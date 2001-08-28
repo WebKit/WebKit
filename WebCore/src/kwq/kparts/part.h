0dbd0e11446d32b9653d812011b481db9aac3b1e
Ken Kocienda <kocienda@apple.com>
2001-08-28 17:43:10 +0000

    Added a bunch of extra declarations, includes, etc. in the process of getting khtml/dom to compile. Several new files were added as well.
    Canonical link: https://commits.webkit.org/24@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@26 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/src/kwq/kparts/part.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2001 Apple Computer, Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+#ifndef PART_H_
+#define PART_H_
+
+#include <kurl.h>
+
+// added to help in compilation of khtml/khtml_part.h:867
+namespace KIO {
+class Job;
+}
+ 
+// forward declaration hack to help in compilation of khtml/khtml_part.h:166
+class QWidget;
+
+// forward declaration hack to help in compilation of khtml/khtml_part.h:249
+class QVariant;
+
+// forward declaration hack to help in compilation of khtml/khtml_part.h:552
+class QCursor;
+
+// forward declaration hack to help in compilation of khtml/khtml_part.h:631
+class QDataStream;
+
+namespace KParts {
+
+class Part {
+};
+
+class ReadOnlyPart : public Part {
+};
+
+// hack to help in compilation of khtml/khtml_part.h:785
+class GUIActivateEvent {
+};
+
+};
+
+#endif
