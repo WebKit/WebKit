381e372d7cfa9ab74919c88b744693ac74d831cd
Don Melton <gramps@apple.com>
2001-08-31 16:32:23 +0000

    More changes to get this beast to compile
    Canonical link: https://commits.webkit.org/57@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@59 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/src/kwq/qt/qcstring.h
+++ WebCore/src/kwq/qt/qcstring.h
@@ -46,9 +46,11 @@ public:
     QCString mid(uint index, uint len=0xffffffff) const;
     
     int find(const char *str, int index=0, bool cs=TRUE) const;
+    int contains(char,) const;
     bool isEmpty() const;
     bool isNull() const;
     uint length() const;
+    bool truncate(uint);
     QCString lower() const;
 
     operator const char *() const;
