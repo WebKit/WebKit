0ca9717930cb370d87fc0fff11640c7a1713dc0e
Don Melton <gramps@apple.com>
2001-09-21 01:35:22 +0000

    Created from QChar implementation in KWQString.mm
    Canonical link: https://commits.webkit.org/160@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@163 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/kwq/KWQChar.mm
@@ -0,0 +1,226 @@
+/*
+ * Copyright (C) 2001 Apple Computer, Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+// FIXME: obviously many functions here can be made inline
+
+#include <qstring.h>
+
+static UniChar scratchUniChar;
+
+static CFMutableStringRef GetScratchUniCharString()
+{
+    static CFMutableStringRef s = NULL;
+
+    if (!s) {
+        // FIXME: this CFMutableString will be leaked exactly once
+        s = CFStringCreateMutableWithExternalCharactersNoCopy(NULL,
+                &scratchUniChar, 1, 1, kCFAllocatorNull);
+    }
+    return s;
+}
+
+QChar::QChar()
+{
+    c = 0;
+}
+
+QChar::QChar(char ch)
+{
+    c = ch;
+}
+
+QChar::QChar(uchar uch)
+{
+    c = uch;
+}
+
+QChar::QChar(short n)
+{
+    c = n;
+}
+
+QChar::QChar(ushort n)
+{
+    c = n;
+}
+
+QChar::QChar(uint n)
+{
+    c = n;
+}
+
+QChar::QChar(int n)
+{
+    c = n;
+}
+
+QChar::QChar(const QChar &qc)
+{
+    c = qc.c;
+}
+
+QChar::~QChar()
+{
+    // do nothing because the single data member is a UniChar
+}
+
+ushort QChar::unicode() const
+{
+    return c;
+}
+
+uchar QChar::cell() const
+{
+    // return least significant byte
+    return c & 0xff;
+}
+
+uchar QChar::row() const
+{
+    // return most significant byte
+    return (c & 0xff00) >> 8;
+}
+
+char QChar::latin1() const
+{
+    return row() ? 0 : cell();
+}
+
+QChar::operator char() const
+{
+    return latin1();
+}
+
+bool QChar::isNull() const
+{
+    return c == 0;
+}
+
+bool QChar::isSpace() const
+{
+    // FIXME: should we use this optimization?
+#if 0
+    if (!row()) {
+	return isspace(c);
+    }
+#endif
+    return CFCharacterSetIsCharacterMember(CFCharacterSetGetPredefined(
+                kCFCharacterSetWhitespaceAndNewline), c);
+}
+
+bool QChar::isDigit() const
+{
+    return CFCharacterSetIsCharacterMember(CFCharacterSetGetPredefined(
+                kCFCharacterSetDecimalDigit), c);
+}
+
+bool QChar::isLetter() const
+{
+    return CFCharacterSetIsCharacterMember(CFCharacterSetGetPredefined(
+                kCFCharacterSetLetter), c);
+}
+
+bool QChar::isLetterOrNumber() const
+{
+    return CFCharacterSetIsCharacterMember(CFCharacterSetGetPredefined(
+                kCFCharacterSetAlphaNumeric), c) || isDigit();
+}
+
+bool QChar::isPunct() const
+{
+    return CFCharacterSetIsCharacterMember(CFCharacterSetGetPredefined(
+                kCFCharacterSetPunctuation), c);
+}
+
+QChar QChar::lower() const
+{
+    scratchUniChar = c;
+    CFStringLowercase(GetScratchUniCharString(), NULL);
+    if (scratchUniChar) {
+	return QChar(scratchUniChar);
+    }
+    return *this;
+}
+
+QChar QChar::upper() const
+{
+    scratchUniChar = c;
+    CFStringUppercase(GetScratchUniCharString(), NULL);
+    if (scratchUniChar) {
+	return QChar(scratchUniChar);
+    }
+    return *this;
+}
+
+QChar::Direction QChar::direction() const
+{
+    // FIXME: unimplemented because we don't do BIDI yet
+    return DirL;
+}
+
+bool QChar::mirrored() const
+{
+    // FIXME: unimplemented because we don't do BIDI yet
+    // return whether character should be reversed if text direction is
+    // reversed
+    return FALSE;
+}
+
+QChar QChar::mirroredChar() const
+{
+    // FIXME: unimplemented because we don't do BIDI yet
+    // return mirrored character if it is mirrored else return itself
+    return *this;
+}
+
+int operator==(QChar qc1, QChar qc2)
+{
+    return qc1.c == qc2.c;
+}
+
+int operator==(QChar qc, char ch)
+{
+    return qc.c == ch;
+}
+
+int operator==(char ch, QChar qc)
+{
+    return ch == qc.c;
+}
+
+int operator!=(QChar qc1, QChar qc2)
+{
+    return qc1.c != qc2.c;
+}
+
+int operator!=(QChar qc, char ch)
+{
+    return qc.c != ch;
+}
+
+int operator!=(char ch, QChar qc)
+{
+    return ch != qc.c;
+}
