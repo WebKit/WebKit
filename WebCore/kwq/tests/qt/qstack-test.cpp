af046f0cf124f57bbfdca143c79f3d8e7576a9be
Chris Blumenberg <cblu@apple.com>
2001-09-26 21:07:46 +0000

    Added tests for isEmpty, cleaned up output for qstack-test
    Canonical link: https://commits.webkit.org/200@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@203 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/kwq/tests/qt/qstack-test.cpp
+++ WebCore/kwq/tests/qt/qstack-test.cpp
@@ -6,36 +6,38 @@ int main() {
 
     QStack<int> s0;
     QStack<int> s1 = QStack<int>();
-
+    QStack<int> s2;
+    
     int i0 = 0;
     int i1 = 1;
     int i2 = 2;
     int i3 = 3;
     int i4 = 4;
 
-    cout << s0 << endl;
+    cout << "s0: " << s0 << endl;
 
     s0.push(&i0);
     s0.push(&i1);
     s0.push(&i2);
     s0.push(&i3);
     s0.push(&i4);
-
-    cout << "push test: " << endl;
-    cout << s0 << endl;
+    cout << "s0 push: " << s0 << endl;
     
     s1 = s0;
-    cout << "pop test: " << endl;
+    cout << "s1 = s0: " << s1 << endl;
+    cout << "s1 count: " << s1.count() << endl;
+    
     int count = s1.count();
     for (int i = 0; i < count; i++) {
-        cout << *(s1.pop()) << endl;
+        cout << "s1 pop: "<< *(s1.pop()) << endl;
     }
-
-    s1 = s0;
-    cout << s1 << endl;
-    cout << "clear test: " << endl;
-    s1.clear();
-    cout << s1 << endl;
+    cout << "s1: " << s1 << endl;
+    
+    cout << "s0 isEmpty: " << s0.isEmpty() << endl;
+    cout << "s1 isEmpty: " << s1.isEmpty() << endl;
+    
+    s0.clear();  // not needed for KWQ?
+    cout << "s0 clear: "<< s0 << endl;
 
     return 0;
 }
