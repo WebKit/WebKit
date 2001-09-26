18cc8337518c055e99c42c7905ef3584e8d0beb2
Chris Blumenberg <cblu@apple.com>
2001-09-26 17:49:00 +0000

    Added tests for at and duplicate to qarray-test. Commented out uneccessary tests.
    Canonical link: https://commits.webkit.org/197@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@200 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/kwq/tests/qt/qarray-test.cpp
+++ WebCore/kwq/tests/qt/qarray-test.cpp
@@ -32,7 +32,7 @@ int main() {
     cout << "a3 is null: " << (a3.isNull()) << endl;
     cout << "a3 is empty: " << (a3.isEmpty()) << endl;
 
-    cout << "a0 count: " << (a3.count()) << endl;
+    cout << "a0 count: " << (a0.count()) << endl;
     
     ch = a0.data();
     cout << "a0 data: " << ch << endl;
@@ -43,33 +43,40 @@ int main() {
     }
     cout << "a0 resize: " << a0 << endl;  
     
-    a0.truncate(10);
-    cout << "a0 truncate: " << a0 << endl;
-    
     QArray<char> a4 = QArray<char>(10);
     a4.fill('c');
     cout << "a4 fill: " << a4 << endl;
     
-    cout << "a0 find 'j': " << a0.find('j') << endl;
+    //cout << "a0 find 'j': " << a0.find('j') << endl;
     
-    cout << "a0 nrefs: " << a0.nrefs() << endl;
+    //cout << "a0 nrefs: " << a0.nrefs() << endl;
     
-    QArray<char> a5 = QArray<char>(10);
-    a5 = a0.copy();
-    cout << "a5 copy a0: " << a5 << endl;
+    //QArray<char> a5 = QArray<char>(10);
+    //a5 = a0.copy();
+    //cout << "a5 copy a0: " << a5 << endl;
     
-    QArray<char> a6 = QArray<char>(10);
-    a6 = a0.assign(a0); 			// hmm. Am I doing this right?
-    cout << "a6 assign a0: " << a6 << endl;
+    //QArray<char> a6 = QArray<char>(10);
+    //a6 = a0.assign(a0); 			// hmm. Am I doing this right?
+    //cout << "a6 assign a0: " << a6 << endl;
     
-    QArray<char> a7 = QArray<char>(10);
-    for (int i = 9; i >= 0; i--) {
-        a7[i] = 'j' - i;
-    }
+    //QArray<char> a7 = QArray<char>(10);
+    //for (int i = 9; i >= 0; i--) {
+    //    a7[i] = 'j' - i;
+    //}
     
-    cout << "a7 unsorted: " << a7 << endl;
-    a7.sort();
-    cout << "a7 sorted: " << a7 << endl;
+    //cout << "a7 unsorted: " << a7 << endl;
+    //a7.sort();
+    //cout << "a7 sorted: " << a7 << endl;
+    
+    
+    char ch2;
+    ch2 = a0.at(4);
+    cout << "a0 at 4: " << ch2 << endl;
+    
+    char *ch3;
+    ch3 = a4.data();
+    a0 = a0.duplicate(ch3, 10);
+    cout << "a0 duplicate from a4: " << a0 << endl;
     
     return 0;
 }
