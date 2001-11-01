14a732f46bdf1472a71c70f982867f70fd867f9e
Richard Williamson <rjw@apple.com>
2001-11-01 23:45:34 +0000

    Added template +initialize for Chris.
    Canonical link: https://commits.webkit.org/394@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@398 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebView.subproj/NSWebPageView.mm
+++ WebKit/WebView.subproj/NSWebPageView.mm
@@ -4,4 +4,17 @@
 #import <WebKit/NSWebPageView.h>
 
 @implementation NSWebPageView
+
++ (void)initialize {
+#if 0
+    NSAutoreleasePool *localPool;
+    NSUserDefaults *userDefaults;
+    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+        @"value", @"key",
+    };
+
+    [[NSUserDefaults standardUserDefaults] registerDefaults:dict];
+#endif
+}
+
 @end
