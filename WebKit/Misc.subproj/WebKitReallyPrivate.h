1abc6082e4607d6cad088a09ab58ff59b15ebffb
Ken Kocienda <kocienda@apple.com>
2001-11-08 21:33:43 +0000

    Added checks to validate arguments passed in through public API methods. Invalid arguments now throw exceptions.
    Canonical link: https://commits.webkit.org/423@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@427 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/Misc.subproj/WebKitReallyPrivate.h
+++ WebKit/Misc.subproj/WebKitReallyPrivate.h
@@ -3,3 +3,19 @@
 */
 
 #import "_NSMonitor.h"
+
+#define WEBKIT_ASSERT_VALID_ARG(arg,expr) \
+    do { \
+        if (!(expr)) { \
+            NSString *reason = [NSString stringWithFormat:@"'%s' fails check: '%s'", #arg, #expr]; \
+            [[NSException exceptionWithName:NSInvalidArgumentException reason:reason userInfo: nil] raise]; \
+        } \
+    } while (0)
+    
+#define WEBKIT_ASSERT_NOT_NIL(arg) \
+    do { \
+        if ((arg) == nil) { \
+            NSString *reason = [NSString stringWithFormat:@"'%s' is nil", #arg]; \
+            [[NSException exceptionWithName:NSInvalidArgumentException reason:reason userInfo: nil] raise]; \
+        } \
+    } while (0)
