7bd1a902eca05e0fed2d3852e94f1648399af8e0
John Sullivan <sullivan@apple.com>
2002-03-06 00:46:11 +0000

    Replaced mechanism for setting array of 7 font sizes from user's chosen     font size with a far simpler one that gets better results.
    	* Preferences.subproj/TextPreferences.m:
    	(-[TextPreferences defaultFontSize]): Renamed from mediumFontSize
    	(-[TextPreferences setDefaultFontSize:]): Renamed from setMediumFontSize;
    	now does everything by computation, no table involved.
    	(-[TextPreferences updateFontSampleField:]), (-[TextPreferences changeFont:]):
    	updated for name changes.
    	* Defaults.plist: Tweak default sizes so they match what algorithm returns.
    
    	* WebView.subproj/IFWebDataSource.mm: (+[IFWebDataSource initialize]):
    	Tweak default font sizes so they match what algorithm used in Alexander
    	returns.
    
    
    Canonical link: https://commits.webkit.org/687@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@697 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebView.subproj/IFWebDataSource.mm
+++ WebKit/WebView.subproj/IFWebDataSource.mm
@@ -26,7 +26,7 @@ static id IFWebDataSourceMake(void *url)
 + (void)initialize {
 
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSArray *fontSizeArray = [NSArray arrayWithObjects:@"6", @"8", @"10", @"12", @"14", @"18", @"30", nil];
+    NSArray *fontSizeArray = [NSArray arrayWithObjects:@"6", @"8", @"9", @"11", @"14", @"16", @"20", nil];
     NSNumber *pluginsEnabled = [NSNumber numberWithBool:TRUE];
     NSNumber *javaEnabled = [NSNumber numberWithBool:FALSE];
     
