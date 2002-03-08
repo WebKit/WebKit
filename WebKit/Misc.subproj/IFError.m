48697cfbf8b4fa3322f5dbca40db785dc6745316
John Sullivan <sullivan@apple.com>
2002-03-08 00:04:40 +0000

    * CacheLoader.subproj/IFURLCacheLoaderConstants.h: 	Fixed two typos in constant names (not used anywhere).
    	* WebFoundation.pbproj/project.pbxproj: Jaguar/Puma
    	version wars.
    
    	First pass at adding error strings to IFError. They are
    	properly localized, but they don't include any parameters,
    	and the strings haven't been made user-friendly.
    
    	* Misc.subproj/IFError.m:
    	(+[IFError initialize]): New method, set up a dictionary
    	mapping error code to localized error string.
    	(-[IFError errorDescription]): Read string from dictionary.
    
    	* English.lproj/IFError.strings: New file, auto-generated
    	by the genstrings tool.
    
    	* WebKit.pbproj/project.pbxproj: Removed stray -F /symroots.
    	This shouldn't have been in there since symroots location is
    	no longer hardwired.
    
    	* BrowserDocument.m: (-[BrowserDocument locationChangeDone:forFrame:]):
    	Make error message include errorDescription. This is not final UI, have
    	no fear. Useful for debugging though.
    
    	* WebBrowser.pbproj/project.pbxproj: Jaguar/Puma version wars
    
    
    Canonical link: https://commits.webkit.org/704@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@716 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/Misc.subproj/IFError.m
+++ WebKit/Misc.subproj/IFError.m
@@ -5,12 +5,114 @@
 #import <IFError.h>
 
 #include <WCError.h>
+#include <WebFoundation/IFURLCacheLoaderConstants.h>
+
 
 @implementation IFError
+
+static NSDictionary *descriptions = nil;
 
 static id IFErrorMake(int code) 
 {
     return [[[IFError alloc] initWithErrorCode: (int)code] autorelease];
+}
+
++(void) initialize
+{
+    NSDictionary *dict;
+    
+    // +initialize is not thread-safe
+    if (descriptions != nil) {
+        return;
+    }
+
+    // Specifying all strings explicitly here, including table name, allows genstrings to work its magic
+    dict = [NSDictionary dictionaryWithObjectsAndKeys:
+        NSLocalizedStringFromTable (@"cancelled", @"IFError", @"IFURLHandleResultCancelled description"),
+        [NSNumber numberWithInt: IFURLHandleResultCancelled],
+        NSLocalizedStringFromTable (@"bad URL", @"IFError", @"IFURLHandleResultBadURLError description"),
+        [NSNumber numberWithInt: IFURLHandleResultBadURLError],
+        NSLocalizedStringFromTable (@"error requesting create", @"IFError", @"IFURLHandleResultRequestCreateError description"),
+        [NSNumber numberWithInt: IFURLHandleResultRequestCreateError],
+        NSLocalizedStringFromTable (@"error reading stream", @"IFError", @"IFURLHandleResultReadStreamError description"),
+        [NSNumber numberWithInt: IFURLHandleResultReadStreamError],
+        NSLocalizedStringFromTable (@"error creating read stream", @"IFError", @"IFURLHandleResultReadStreamCreateError description"),
+        [NSNumber numberWithInt: IFURLHandleResultReadStreamCreateError],
+        NSLocalizedStringFromTable (@"error setting client for read stream", @"IFError", @"IFURLHandleResultReadStreamSetClientError description"),
+        [NSNumber numberWithInt: IFURLHandleResultReadStreamSetClientError],
+        NSLocalizedStringFromTable (@"error opening read stream", @"IFError", @"IFURLHandleResultReadStreamOpenError description"),
+        [NSNumber numberWithInt: IFURLHandleResultReadStreamOpenError],
+        NSLocalizedStringFromTable (@"time out", @"IFError", @"IFURLHandleResultTimedOut description"),
+        [NSNumber numberWithInt: IFURLHandleResultTimedOut],
+        NSLocalizedStringFromTable (@"unsupported URL handle", @"IFError", @"IFURLHandleResultUnsupportedURLHandleError description"),
+        [NSNumber numberWithInt: IFURLHandleResultUnsupportedURLHandleError],
+        NSLocalizedStringFromTable (@"can't connect to host", @"IFError", @"IFURLHandleResultCantConnectToHostError description"),
+        [NSNumber numberWithInt: IFURLHandleResultCantConnectToHostError],
+        NSLocalizedStringFromTable (@"lost network connection", @"IFError", @"IFURLHandleResultNetworkConnectionLostError description"),
+        [NSNumber numberWithInt: IFURLHandleResultNetworkConnectionLostError],
+        NSLocalizedStringFromTable (@"DNS lookup error", @"IFError", @"IFURLHandleResultDNSLookupError description"),
+        [NSNumber numberWithInt: IFURLHandleResultDNSLookupError],
+        NSLocalizedStringFromTable (@"HTTP redirection loop error", @"IFError", @"IFURLHandleResultHTTPRedirectionLoopError description"),
+        [NSNumber numberWithInt: IFURLHandleResultHTTPRedirectionLoopError],
+        NSLocalizedStringFromTable (@"resource unvailable", @"IFError", @"IFURLHandleResultResourceUnavailableError description"),
+        [NSNumber numberWithInt: IFURLHandleResultResourceUnavailableError],
+        NSLocalizedStringFromTable (@"can't load from network", @"IFError", @"IFURLHandleResultCantLoadFromNetworkError description"),
+        [NSNumber numberWithInt: IFURLHandleResultCantLoadFromNetworkError],
+
+        NSLocalizedStringFromTable (@"bad request", @"IFError", @"IFURLHandleResultBadRequest description"),
+        [NSNumber numberWithInt: IFURLHandleResultBadRequest],
+        NSLocalizedStringFromTable (@"unauthorized", @"IFError", @"IFURLHandleResultUnauthorized description"),
+        [NSNumber numberWithInt: IFURLHandleResultUnauthorized],
+        NSLocalizedStringFromTable (@"payment required", @"IFError", @"IFURLHandleResultPaymentRequired description"),
+        [NSNumber numberWithInt: IFURLHandleResultPaymentRequired],
+        NSLocalizedStringFromTable (@"forbidden", @"IFError", @"IFURLHandleResultForbidden description"),
+        [NSNumber numberWithInt: IFURLHandleResultForbidden],
+        NSLocalizedStringFromTable (@"not found", @"IFError", @"IFURLHandleResultNotFound description"),
+        [NSNumber numberWithInt: IFURLHandleResultNotFound],
+        NSLocalizedStringFromTable (@"not allowed", @"IFError", @"IFURLHandleResultMethodNotAllowed description"),
+        [NSNumber numberWithInt: IFURLHandleResultMethodNotAllowed],
+        NSLocalizedStringFromTable (@"not acceptable", @"IFError", @"IFURLHandleResultNotAcceptable description"),
+        [NSNumber numberWithInt: IFURLHandleResultNotAcceptable],
+        NSLocalizedStringFromTable (@"not found", @"IFError", @"IFURLHandleResultNotFound description"),
+        [NSNumber numberWithInt: IFURLHandleResultNotFound],
+        NSLocalizedStringFromTable (@"proxy authentication required", @"IFError", @"IFURLHandleResultProxyAuthenticationRequired description"),
+        [NSNumber numberWithInt: IFURLHandleResultProxyAuthenticationRequired],
+        NSLocalizedStringFromTable (@"request time out", @"IFError", @"IFURLHandleResultRequestTimeOut description"),
+        [NSNumber numberWithInt: IFURLHandleResultRequestTimeOut],
+        NSLocalizedStringFromTable (@"conflict", @"IFError", @"IFURLHandleResultConflict description"),
+        [NSNumber numberWithInt: IFURLHandleResultConflict],
+        NSLocalizedStringFromTable (@"gone", @"IFError", @"IFURLHandleResultGone description"),
+        [NSNumber numberWithInt: IFURLHandleResultGone],
+        NSLocalizedStringFromTable (@"length required", @"IFError", @"IFURLHandleResultLengthRequired description"),
+        [NSNumber numberWithInt: IFURLHandleResultLengthRequired],
+        NSLocalizedStringFromTable (@"request entity too large", @"IFError", @"IFURLHandleResultRequestEntityTooLarge description"),
+        [NSNumber numberWithInt: IFURLHandleResultRequestEntityTooLarge],
+        NSLocalizedStringFromTable (@"request URI too large", @"IFError", @"IFURLHandleResultRequestURITooLarge description"),
+        [NSNumber numberWithInt: IFURLHandleResultRequestURITooLarge],
+        NSLocalizedStringFromTable (@"unsupported media type", @"IFError", @"IFURLHandleResultUnsupportedMediaType description"),
+        [NSNumber numberWithInt: IFURLHandleResultUnsupportedMediaType],
+        NSLocalizedStringFromTable (@"request range not satisfiable", @"IFError", @"IFURLHandleResultRequestRangeNotSatisfiable description"),
+        [NSNumber numberWithInt: IFURLHandleResultRequestRangeNotSatisfiable],
+        NSLocalizedStringFromTable (@"expectation failed", @"IFError", @"IFURLHandleResultExpectationFailed description"),
+        [NSNumber numberWithInt: IFURLHandleResultExpectationFailed],
+
+        NSLocalizedStringFromTable (@"internal server error", @"IFError", @"IFURLHandleResultInternalServerError description"),
+        [NSNumber numberWithInt: IFURLHandleResultInternalServerError],
+        NSLocalizedStringFromTable (@"not implemented", @"IFError", @"IFURLHandleResultNotImplemented description"),
+        [NSNumber numberWithInt: IFURLHandleResultNotImplemented],
+        NSLocalizedStringFromTable (@"bad gateway", @"IFError", @"IFURLHandleResultBadGateway description"),
+        [NSNumber numberWithInt: IFURLHandleResultBadGateway],
+        NSLocalizedStringFromTable (@"service unavailable", @"IFError", @"IFURLHandleResultServiceUnavailable description"),
+        [NSNumber numberWithInt: IFURLHandleResultServiceUnavailable],
+        NSLocalizedStringFromTable (@"gateway time out", @"IFError", @"IFURLHandleResultGatewayTimeOut description"),
+        [NSNumber numberWithInt: IFURLHandleResultGatewayTimeOut],
+        NSLocalizedStringFromTable (@"HTTP version not supported", @"IFError", @"IFURLHandleResultHTTPVersionNotSupported description"),
+        [NSNumber numberWithInt: IFURLHandleResultHTTPVersionNotSupported],
+        nil];
+
+    if (descriptions == nil) {
+        descriptions = [dict retain];
+    }
 }
 
 +(void) load
@@ -32,8 +134,8 @@ static id IFErrorMake(int code)
 
 - (NSString *)errorDescription
 {
-    // FIXME!
-    return nil;
+    NSLog(@"descriptions are %@", descriptions);
+    return [descriptions objectForKey: [NSNumber numberWithInt: errorCode]];
 }
 
 @end
