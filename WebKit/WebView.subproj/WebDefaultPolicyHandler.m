334386e48bb1e75c0fd7fb0ea1147b21b9d4720a
Chris Blumenberg <cblu@apple.com>
2002-07-24 21:07:22 +0000

    Renamed WebDefaultControllerPolicyHandler to WebDefaultPolicyHandler.
            * WebKit.pbproj/project.pbxproj:
            * WebView.subproj/WebDefaultControllerPolicyHandler.h: Removed.
            * WebView.subproj/WebDefaultControllerPolicyHandler.m: Removed.
            * WebView.subproj/WebDefaultPolicyHandler.h: Added.
            * WebView.subproj/WebDefaultPolicyHandler.m: Added.
            (-[WebDefaultControllerPolicyHandler initWithWebController:]):
            (-[WebDefaultControllerPolicyHandler URLPolicyForURL:]):
            (-[WebDefaultControllerPolicyHandler fileURLPolicyForMIMEType:dataSource:isDirectory:]):
            (-[WebDefaultControllerPolicyHandler unableToImplementFileURLPolicy:forDataSource:]):
            (-[WebDefaultControllerPolicyHandler requestContentPolicyForMIMEType:dataSource:]):
            (-[WebDefaultControllerPolicyHandler unableToImplementURLPolicyForURL:error:]):
            (-[WebDefaultControllerPolicyHandler unableToImplementContentPolicy:forDataSource:]):
            (-[WebDefaultControllerPolicyHandler pluginNotFoundForMIMEType:pluginPageURL:]):
    
    
    Canonical link: https://commits.webkit.org/1516@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@1657 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebKit/WebView.subproj/WebDefaultPolicyHandler.m
@@ -0,0 +1,67 @@
+/*	
+        WebDefaultControllerPolicyHandler.h
+	Copyright 2002, Apple Computer, Inc.
+
+        Public header file.
+*/
+#import <WebKit/WebDataSource.h>
+#import <WebKit/WebDefaultControllerPolicyHandler.h>
+#import <WebKit/WebFrame.h>
+
+
+@implementation WebDefaultControllerPolicyHandler
+
+- initWithWebController: (WebController *)wc
+{
+    [super init];
+    webController = wc;  // Non-retained, like a delegate.
+    return self;
+}
+
+- (WebURLPolicy)URLPolicyForURL: (NSURL *)url
+{
+    return [WebController defaultURLPolicyForURL: url];
+}
+
+- (WebFileURLPolicy)fileURLPolicyForMIMEType: (NSString *)type dataSource: (WebDataSource *)dataSource isDirectory:(BOOL)isDirectory
+{
+    if(isDirectory)
+        return WebFileURLPolicyIgnore;
+    if([WebController canShowMIMEType:type])
+        return WebFileURLPolicyUseContentPolicy;
+    return WebFileURLPolicyIgnore;
+}
+
+- (void)unableToImplementFileURLPolicy: (WebError *)error forDataSource: (WebDataSource *)dataSource
+{
+    NSLog (@"unableToImplementFileURLPolicy:forDataSource: - error %@\n", error);
+}
+
+- (void)requestContentPolicyForMIMEType: (NSString *)type dataSource: (WebDataSource *)dataSource
+{
+    if([WebController canShowMIMEType:type]){
+        [webController haveContentPolicy:WebContentPolicyShow andPath:nil forDataSource:dataSource];
+    }
+    else{
+        [webController haveContentPolicy:WebContentPolicyIgnore andPath:nil forDataSource:dataSource];
+    }
+}
+
+- (void)unableToImplementURLPolicyForURL: (NSURL *)url error: (WebError *)error
+{
+    NSLog (@"unableToImplementURLPolicyForURL:error: - URL %@, error %@\n", url, error);
+}
+
+
+- (void)unableToImplementContentPolicy: (WebError *)error forDataSource: (WebDataSource *)dataSource
+{
+    NSLog (@"unableToImplementContentPolicy:forDataSource: - error %@\n", error);
+}
+
+- (void)pluginNotFoundForMIMEType:(NSString *)mime pluginPageURL:(NSURL *)url
+{
+    NSLog (@"pluginNotFoundForMIMEType:pluginPageURL: - MIME %@, URL \n", mime, url);
+}
+
+@end
+
