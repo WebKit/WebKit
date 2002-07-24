d8c5b57336c9c5eb7c8c33810bafe5345f3265c7
Darin Adler <darin@apple.com>
2002-07-24 21:22:50 +0000

    * khtml/khtml_part.cpp: (KHTMLPart::clear): Remove code that reset m_frameNameId, 	to complete Maciej's change to fix bug 3006354.
    Canonical link: https://commits.webkit.org/1518@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@1659 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/khtml_part.cpp
+++ WebCore/khtml/khtml_part.cpp
@@ -967,7 +967,9 @@ void KHTMLPart::clear()
   d->m_redirectLockHistory = true;
   d->m_bHTTPRefresh = false;
   d->m_bClearing = false;
+#ifndef APPLE_CHANGES
   d->m_frameNameId = 1;
+#endif
   d->m_bFirstData = true;
 
   d->m_bMousePressed = false;
