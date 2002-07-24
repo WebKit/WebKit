99bd356e5489a24c1a0d89a9e3da7de3355b8d3e
Richard Williamson <rjw@apple.com>
2002-07-24 22:41:39 +0000

    Implemented find for text views.
    Canonical link: https://commits.webkit.org/1520@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@1661 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebView.subproj/WebTextView.m
+++ WebKit/WebView.subproj/WebTextView.m
@@ -7,6 +7,43 @@
 
 #import <WebKit/WebDataSource.h>
 
+@interface NSString (NSStringTextFinding)
+
+- (NSRange)findString:(NSString *)string selectedRange:(NSRange)selectedRange options:(unsigned)mask wrap:(BOOL)wrapFlag;
+
+@end
+
+@implementation NSString (_Web_StringTextFinding)
+
+- (NSRange)findString:(NSString *)string selectedRange:(NSRange)selectedRange options:(unsigned)options wrap:(BOOL)wrap {
+    BOOL forwards = (options & NSBackwardsSearch) == 0;
+    unsigned length = [self length];
+    NSRange searchRange, range;
+
+    if (forwards) {
+	searchRange.location = NSMaxRange(selectedRange);
+	searchRange.length = length - searchRange.location;
+	range = [self rangeOfString:string options:options range:searchRange];
+        if ((range.length == 0) && wrap) {	/* If not found look at the first part of the string */
+	    searchRange.location = 0;
+            searchRange.length = selectedRange.location;
+            range = [self rangeOfString:string options:options range:searchRange];
+        }
+    } else {
+	searchRange.location = 0;
+	searchRange.length = selectedRange.location;
+        range = [self rangeOfString:string options:options range:searchRange];
+        if ((range.length == 0) && wrap) {
+            searchRange.location = NSMaxRange(selectedRange);
+            searchRange.length = length - searchRange.location;
+            range = [self rangeOfString:string options:options range:searchRange];
+        }
+    }
+    return range;
+}        
+
+@end
+
 @implementation WebTextView
 
 - (id)initWithFrame:(NSRect)frame
@@ -80,7 +117,29 @@
 
 - (BOOL)searchFor: (NSString *)string direction: (BOOL)forward caseSensitive: (BOOL)caseFlag
 {
-    return NO;
+    BOOL lastFindWasSuccessful = NO;
+    NSString *textContents = [self string];
+    unsigned textLength;
+    
+    if (textContents && (textLength = [textContents length])) {
+        NSRange range;
+        unsigned options = 0;
+        
+        if (!forward) 
+            options |= NSBackwardsSearch;
+            
+        if (!caseFlag)
+            options |= NSCaseInsensitiveSearch;
+            
+        range = [textContents findString:string selectedRange:[self selectedRange] options:options wrap:YES];
+        if (range.length) {
+            [self setSelectedRange:range];
+            [self scrollRangeToVisible:range];
+            lastFindWasSuccessful = YES;
+        }
+    }
+
+    return lastFindWasSuccessful;
 }
 
 @end
