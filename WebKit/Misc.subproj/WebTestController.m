5390e683803bf1dbf4b68ec20bb4541573db87f9
Darin Adler <darin@apple.com>
2002-08-16 17:29:14 +0000

    Fixed broken build.
            * Misc.subproj/WebTestController.m: Added empty placeholder methods.
    
    
    Canonical link: https://commits.webkit.org/1701@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@1847 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/Misc.subproj/WebTestController.m
+++ WebKit/Misc.subproj/WebTestController.m
@@ -33,6 +33,31 @@
     return self;
 }
 
+- (void)filledRect:(NSRect)r
+{
+}
+
+- (void)strokedRect:(NSRect)r
+{
+}
+
+- (void)filledOval:(NSRect)r
+{
+}
+
+- (void)strokedOval:(NSRect)r
+{
+}
+
+- (void)strokedArcWithCenter:(NSPoint)p radius:(float)radius startAngle:(float)startAngle endAngle:(float)endAngle
+{
+}
+
+- (void)drewLineFrom:(NSPoint)p1 to:(NSPoint)p2 width:(float)width
+{
+}
+
+
 @end
 
 @implementation WebTestController
