d6f032f9e9a11762b68ca75010490a6a180c9006
Richard Williamson <rjw@apple.com>
2002-09-18 22:19:12 +0000

    Added documentation keywords to header.
            * WebView.subproj/WebDocument.h:
    
    
    Canonical link: https://commits.webkit.org/1938@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@2089 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebView.subproj/WebDocument.h
+++ WebKit/WebView.subproj/WebDocument.h
@@ -1,6 +1,6 @@
 /*	
     WebDocument.h
-	Copyright 2002, Apple, Inc. All rights reserved.
+    Copyright 2002, Apple, Inc. All rights reserved.
 */
 
 #import <Cocoa/Cocoa.h>
@@ -8,30 +8,106 @@
 @class WebDataSource;
 @class WebError;
 
+/*!
+    @protocol WebDocumentView
+*/
 @protocol WebDocumentView <NSObject>
+/*!
+    @method setDataSource:
+    @param dataSource
+*/
 - (void)setDataSource: (WebDataSource *)dataSource;
+
+/*!
+    @method dataSourceUpdated:
+    @param dataSource
+*/
 - (void)dataSourceUpdated: (WebDataSource *)dataSource;
+
+/*!
+    @method layout
+*/
 - (void)layout;
 @end
 
+
+/*!
+    @protocol WebDocumentDragSettings
+*/
 @protocol WebDocumentDragSettings <NSObject>
+/*!
+    @method setCanDragFrom:
+    @param flag
+*/
 - (void)setCanDragFrom: (BOOL)flag;
+
+/*!
+    @method canDragFrom
+*/
 - (BOOL)canDragFrom;
+
+/*!
+    @method setCanDragTo:
+    @param flag
+*/
 - (void)setCanDragTo: (BOOL)flag;
+
+/*!
+    @method canDragTo
+*/
 - (BOOL)canDragTo;
 @end
 
+
+/*!
+    @protocol WebDocumentSearching
+*/
 @protocol WebDocumentSearching <NSObject>
+/*!
+    @method searchFor:direction:caseSensitive:
+*/
 - (BOOL)searchFor: (NSString *)string direction: (BOOL)forward caseSensitive: (BOOL)caseFlag;
 @end
 
+
+/*!
+    @protocol WebDocumentTextEncoding
+*/
 @protocol WebDocumentTextEncoding <NSObject>
+/*!
+    @method supportsTextEncoding
+*/
 - (BOOL)supportsTextEncoding;
 @end
 
+
+/*!
+    @protocol WebDocumentRepresentation
+*/
 @protocol WebDocumentRepresentation <NSObject>
+/*!
+    @method setDataSource:
+    @param dataSource
+*/
 - (void)setDataSource: (WebDataSource *)dataSource;
+
+/*!
+    @method receivedData:withDataSource:
+    @param data
+    @param dataSource
+*/
 - (void)receivedData: (NSData *)data withDataSource: (WebDataSource *)dataSource;
+
+/*!
+    @method receivedError:withDataSource:
+    @param error
+    @param dataSource
+*/
 - (void)receivedError: (WebError *)error withDataSource: (WebDataSource *)dataSource;
+
+/*!
+    @method finishLoadingWithDataSource:
+    @param dataSource
+*/
 - (void)finishedLoadingWithDataSource: (WebDataSource *)dataSource;
 @end
