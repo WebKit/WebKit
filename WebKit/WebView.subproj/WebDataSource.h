346d224fb771933298839620a495b4e5a0991613
Richard Williamson <rjw@apple.com>
2002-09-18 21:41:23 +0000

    Added documentation keywords to headers.
            * WebView.subproj/WebDataSource.h:
    
    
    Canonical link: https://commits.webkit.org/1935@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@2086 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebView.subproj/WebDataSource.h
+++ WebKit/WebView.subproj/WebDataSource.h
@@ -9,154 +9,260 @@
 
 #import <WebKit/WebController.h>
 
-/* 
-    =============================================================================
-   
-    A WebDataSource represents the data associated with a web page.
-
-    ============================================================================= */
-
 @class WebFrame;
 @class WebResourceHandle;
 @class WebController;
-
 @class WebDataSourcePrivate;
 @class WebResourceRequest;
+
 @protocol WebDocumentRepresentation;
 
+/*!
+    @class WebDataSource
+    @discussion A WebDataSource represents the data associated with a web page.
+*/
 @interface WebDataSource : NSObject
 {
 @private
     WebDataSourcePrivate *_private;
 }
 
-// Returns nil if object cannot be initialized due to a malformed URL (RFC 1808).
+/*!
+    @method initWithURL:
+    @discussion Returns nil if object cannot be initialized due to a malformed URL (RFC 1808).
+    @param URL
+*/
 -(id)initWithURL:(NSURL *)URL;
+
+/*!
+    @method initWithRequest:
+    @param request
+*/
 -(id)initWithRequest:(WebResourceRequest *)request;
 
-#ifdef TENTATIVE_API
-- initWithData: (NSData *)data;
-- initWithString: (NSString *)string;
-- initWithLoader: (WebLoader *)loader;
-#endif
-
+/*!
+    @method data
+*/
 - (NSData *)data;
 
+/*!
+    @method representation
+*/
 - (id <WebDocumentRepresentation>)representation;
 
-// Returns YES if this is the main document.  The main document is the 'top'
-// document, typically either a frameset or a normal HTML document.
+/*!
+    @method isMainDocument
+    @discussion Returns YES if this is the main document.  The main document is the 'top'
+    document, typically either a frameset or a normal HTML document.
+*/
 - (BOOL)isMainDocument;
 
-// Returns nil if this data source represents the main document.  Otherwise
-// returns the parent data source.
+/*!
+    @method parent
+    @discussion Returns nil if this data source represents the main document.  Otherwise
+    returns the parent data source.
+*/
 - (WebDataSource *)parent;
 
-// Return the frame that represents this data source. Same as above.
+/*!
+    @method webFrame
+    @discussion Return the frame that represents this data source. Same as above.
+*/
 - (WebFrame *)webFrame;
 
-// Add a child frame.  This should only be called by the data source's controller
-// as a result of a createFrame:inParent:.
-// [Should this be private?]
+/*!
+    @method addFrame:
+    @discussion Add a child frame.  This should only be called by the data source's controller
+    as a result of a createFrame:inParent:.
+    // [Should this be private?]
+*/
 - (void)addFrame: (WebFrame *)frame;
 
-// Returns an array of WebFrame.  The frames in the array are
-// associated with a frame set or iframe.
+/*!
+    @method children
+    @discussion Returns an array of WebFrame.  The frames in the array are
+    associated with a frame set or iframe.
+*/
 - (NSArray *)children;
 
+/*!
+    @method frameNamed:
+    @param frameName
+*/
 - (WebFrame *)frameNamed:(NSString *)frameName;
 
-// Returns an array of NSStrings or nil.  The NSStrings corresponds to
-// frame names.  If this data source is the main document and has no
-// frames then frameNames will return nil.
+/*!
+    @method frameNames
+    @discussion Returns an array of NSStrings or nil.  The NSStrings corresponds to
+    frame names.  If this data source is the main document and has no
+    frames then frameNames will return nil.
+*/
 - (NSArray *)frameNames;
 
-// findDataSourceForFrameNamed: returns the child data source associated with
-// the frame named 'name', or nil. 
+/*!
+    @method findDataSourceForFrameNamed:
+    @discussion findDataSourceForFrameNamed: returns the child data source associated with
+    the frame named 'name', or nil. 
+    @param name
+*/
 - (WebDataSource *)findDataSourceForFrameNamed:(NSString *)name;
 
+/*!
+    @method frameExists:
+    @param name
+*/
 - (BOOL)frameExists: (NSString *)name;
 
+/*!
+    @method openURL:isFrameNamed:
+    @param URL
+    @param frameName
+*/
 - (void)openURL:(NSURL *)URL inFrameNamed:(NSString *)frameName;
 
-// Set the controller for this data source.  NOTE:  The controller is not retained by the
-// data source.  Perhaps setController: should be private?  Perhaps the back pointers
-// can be managed externally, i.e. + controllerForDataSource: as a class method on 
-// WebController?
-//- (void)setController: (WebController *)controller;
+/*!
+    @method controller
+*/
 - (WebController *)controller;
-
+    
+/*!
+    @method request
+*/
 -(WebResourceRequest *)request;
 
-// May return nil if not initialized with a URL.
-// The value of URL will change if a redirect occurs.
-// To monitor change in the URL, override the <WebLocationChangeHandler> 
-// serverRedirectTo:forDataSource: method.
+/*!
+    @method URL
+    @discussion May return nil if not initialized with a URL.
+    The value of URL will change if a redirect occurs.
+    To monitor change in the URL, override the <WebLocationChangeHandler> 
+    serverRedirectTo:forDataSource: method.
+*/
 - (NSURL *)URL;
 
-// The original passed in at initialization time.
-// Starts out same as URL, but doesn't change if a redirect occurs.
+/*!
+    @method originalURL
+    @discussion The original passed in at initialization time.
+    Starts out same as URL, but doesn't change if a redirect occurs.
+*/
 - (NSURL *)originalURL;
 
-// Start actually getting (if initialized with a URL) and parsing data. If the data source
-// is still performing a previous load it will be stopped.
+/*!
+    @method startLoading
+    @discussion Start actually getting (if initialized with a URL) and parsing data. If the data source
+    is still performing a previous load it will be stopped.
+*/
 - (void)startLoading;
 
-// Cancels any pending loads.  A data source is conceptually only ever loading
-// one document at a time, although one document may have many related
-// resources.  stopLoading will stop all loads related to the data source.
+/*!
+    @method stopLoading
+    @discussion Cancels any pending loads.  A data source is conceptually only ever loading
+    one document at a time, although one document may have many related
+    resources.  stopLoading will stop all loads related to the data source.
+*/
 - (void)stopLoading;
 
-// Returns YES if there are any pending loads.
+/*!
+    @method isLoading
+    @discussion Returns YES if there are any pending loads.
+*/
 - (BOOL)isLoading;
 
-
-
-#ifdef TENTATIVE_API
-// Get DOM access to the document.
-- (WebDOMDocument *)document;
-#endif
-
+/*!
+    @method isDocumentHTML
+*/
 - (BOOL)isDocumentHTML;
 
-// Get the actual source of the document.
+/*!
+    @method documentSource
+    @abstract Get the actual source of the document.
+*/
 - (NSString *)documentSource;
 
-// URL reference point, these should probably not be public for 1.0.
+/*!
+    @method base	
+    @abstract URL reference point, these should probably not be public for 1.0.
+*/
 - (NSURL *)base;
+
+/*!
+    @method baseTarget
+*/
 - (NSString *)baseTarget;
 
+/*!
+    @method encoding
+*/
 - (NSString *)encoding;
 
-// Style sheet
+/*!
+    @method setUserStyleSheetFromURL:
+    @param URL
+*/
 - (void)setUserStyleSheetFromURL: (NSURL *)URL;
+
+/*!
+    @method setUserStyleSheetFromString:
+    @param sheet
+*/
 - (void)setUserStyleSheetFromString: (NSString *)sheet;
 
-// a.k.a shortcut icons, http://msdn.microsoft.com/workshop/Author/dhtml/howto/ShortcutIcon.asp.
-// This method may be moved to a category to prevent unnecessary linkage to the AppKit.  Note, however
-// that WebCore also has dependencies on the appkit.
+/*!
+    @method icon
+    @discussion a.k.a shortcut icons, http://msdn.microsoft.com/workshop/Author/dhtml/howto/ShortcutIcon.asp.
+    This method may be moved to a category to prevent unnecessary linkage to the AppKit.  Note, however
+    that WebCore also has dependencies on the appkit.
+*/
 - (NSImage *)icon;
 
-// Is page secure, e.g. https, ftps
+/*!
+    @method isPageSecure
+    @discussion Is page secure, e.g. https, ftps
+*/
 - (BOOL)isPageSecure;
 
-// Returns nil or the page title.
+/*!
+    @method pageTitle
+    @discussion Returns nil or the page title.
+*/
 - (NSString *)pageTitle;
 
+/*!
+    @method frameName
+*/
 - (NSString *)frameName;
 
+/*!
+    @method contentPolicy
+*/
 - (WebContentPolicy *)contentPolicy;
 
-// MIME type
+/*!
+    @method contentType
+    @discussion returns the MIME type for the data source.
+*/
 - (NSString *)contentType;
 
-// extension based on the MIME type 
+/*!
+    @method fileType
+    @discussion extension based on the MIME type 
+*/
 - (NSString *)fileType;
 
+/*!
+    @method errors
+*/
 - (NSDictionary *)errors;
 
+/*!
+    @method mainDocumentError
+*/
 - (WebError *)mainDocumentError;
 
+/*!
+    @method registerRepresentationClass:forMIMEType:
+    @param repClass
+    @param MIMEType
+*/
 + (void) registerRepresentationClass:(Class)repClass forMIMEType:(NSString *)MIMEType;
 
 @end
