6128e53ac8d74f4fff5625e031fc250e021220e4
Maciej Stachowiak <mjs@apple.com>
2002-09-23 22:17:28 +0000

    * Panels.subproj/WebStandardPanels.h: Added inline header docs.
    Canonical link: https://commits.webkit.org/1976@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@2128 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/Panels.subproj/WebStandardPanels.h
+++ WebKit/Panels.subproj/WebStandardPanels.h
@@ -11,6 +11,9 @@
 
 /*!
     @class WebStandardPanels
+    @discussion This class allows the use of standard, WebKit-provided
+    panels for particular UI that requires panel interaction. The only
+    one available currently is authentication.
 */
 @interface WebStandardPanels : NSObject
 {
@@ -20,37 +23,53 @@
 
 /*!
     @method sharedStandardPanels
+    @abstract Get the shared singleton standard panels object
+    @result The shared standard panels instance
 */
 +(WebStandardPanels *)sharedStandardPanels;
 
 /*!
     @method setUseStandardAuthenticationPanel:
-    @param use
+    @abstract Request that the standard authentication panel be used or not used.
+    @param use YES if the standard authentication panel should be used, NO otherwise
 */
 -(void)setUseStandardAuthenticationPanel:(BOOL)use;
 
 /*!
     @method useStandardAuthenticationPanel
+    @abstract Determine whether the standard authentication panelwill  be used or not used.
+    @result YES if the standard authentication panel should be used, NO otherwise
 */
 -(BOOL)useStandardAuthenticationPanel;
 
 /*!
     @method didStartLoadingURL:inWindow:
-    @param URL
-    @param window
+    @abstract Notify the standard panel mechanism of the start of a resource load
+    @param URL The URL that is starting to load
+    @param window The window associated with the load.
+    @discussion This is only needed for resources that are not being
+    loaded via WebKit. WebKit takes care of this bookkeeping
+    automatically, for resources that it loads.
 */
 -(void)didStartLoadingURL:(NSURL *)URL inWindow:(NSWindow *)window;
 
 /*!
     @method didStopLoadingURL:inWindow:
-    @param URL
-    @param window
+    @abstract Notify the standard panel mechanism of the end of a resource load
+    @param URL The URL that finished loading
+    @param window The window associated with the load.
+    @discussion This is only needed for resources that are not being
+    loaded via WebKit. WebKit takes care of this bookkeeping
+    automatically, for resources that it loads.
 */
 -(void)didStopLoadingURL:(NSURL *)URL inWindow:(NSWindow *)window;
 
 /*!
     @method frontmostWindowLoadingURL:
-    @param URL
+    @abstract Get the frontmost window loading a URL
+    @param URL The URL in question
+    @discussion This may be useful for clients that want to implement their
+    own custom panels for special purposes.
 */
 -(NSWindow *)frontmostWindowLoadingURL:(NSURL *)URL;
 
