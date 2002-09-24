b016a33d2d2109a23dd1aec9508f5471e70942a3
Maciej Stachowiak <mjs@apple.com>
2002-09-24 01:56:01 +0000

    Added documentation for these two delegate protocols.
            * WebView.subproj/WebLocationChangeDelegate.h:
            * WebView.subproj/WebResourceProgressDelegate.h:
    
    
    Canonical link: https://commits.webkit.org/1982@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@2134 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebView.subproj/WebFrameLoadDelegate.h
+++ WebKit/WebView.subproj/WebFrameLoadDelegate.h
@@ -13,62 +13,88 @@
 
 /*!
     @protocol WebLocationChangeDelegate
-    @abstract A controller's WebLocationChangeDelegate track changes it's frame's location. 
+    @discussion A controller's WebLocationChangeDelegate track changes it's frame's location. 
 */
 @protocol WebLocationChangeDelegate <NSObject>
 
 /*!
     @method locationChangeStartedForDataSource:
-    @param dataSource
+    @abstract Notify that a location change has started on a given data source
+    @param dataSource The data source for which the location change has started
+    @discussion You can find out the URL and other parameters of the location change
+    from the data source object.
 */
 - (void)locationChangeStartedForDataSource:(WebDataSource *)dataSource;
 
 /*!
     @method locationChangeCommittedForDataSource:
-    @param dataSource
+    @abstract Notify that a location change has been committed on a given data source
+    @param dataSource The data source for which the location change has started
+    @discussion When a location change starts, it is considered
+    "provisional" until at least one byte of the new page is
+    received. This is done so the old page will not be lost if the new
+    page fails to load completely.
+
 */
 - (void)locationChangeCommittedForDataSource:(WebDataSource *)dataSource;
 
 /*!
     @method locationChangeDone:forDataSource
-    @param error
-    @param dataSource
+    @abstract Notify that a location change is done (possibly with an error) for a data source
+    @param error The error, if one occurred, or nil if none
+    @param dataSource The data source that finished changing location
+    @discussion This callback will only be received when all
+    subresources are done loading.
 */
 - (void)locationChangeDone:(WebError *)error forDataSource:(WebDataSource *)dataSource;
 
 /*!
     @method receivedPageTitle:forDataSource:
-    @param title
-    @param dataSource
+    @abstract Notify that the page title has been determined or has changed
+    @param title The new page title
+    @param dataSource The data source for which the title changed
+    @disucssion The title may update during loading; clients should be prepared for this.
 */
 - (void)receivedPageTitle:(NSString *)title forDataSource:(WebDataSource *)dataSource;
 
 /*!
     @method receivedPageIcon:forDataSource:
-    @param image
-    @param dataSource
+    @abstract Notify that a page icon image is available or has changed
+    @param image The icon image
+    @param dataSource The data source for which a page icon is ready
+    @discussion It's possible the image will change, starting out as a
+    default or stored image and changing on a refresh.
+
 */
 - (void)receivedPageIcon:(NSImage *)image forDataSource:(WebDataSource *)dataSource;
 
 /*!
     @method serverRedirectTo:forDataSource:
-    @param URL
-    @param dataSource
+    @abstract Notify that the data source has received a server redirect.
+    @param URL The URL redirected to
+    @param dataSource The data source for which the redirect occurred
 */
 - (void)serverRedirectTo:(NSURL *)URL forDataSource:(WebDataSource *)dataSource;
 
 /*!
     @method clientRedirectTo:delay:fireDate:forFrame:
-    @param URL
-    @param seconds
-    @param date
-    @param frame
+    @abstract Notify that the data source has received a client-side redirect that may trigger soon
+    @param URL The URL to be redirected to
+    @param seconds Seconds in which the redirect will happen
+    @param date The fire date
+    @param frame The frame on which the redirect will occur
+    @discussion This method can be used to keep progress feedback
+    going while a client redirect is pending. A client redirect might
+    be cancelled before it fires - see clientRedirectCancelledForFrame.
 */
 - (void)clientRedirectTo:(NSURL *)URL delay:(NSTimeInterval)seconds fireDate:(NSDate *)date forFrame:(WebFrame *)frame;
 
 /*!
     @method clientRedirectCancelledForFrame:
-    @param frame
+    @abstract Notify that a pending client redirect has been cancelled.
+    @param frame The frame for which the pending redirect was cancelled
+    @discussion A client redirect can be cancelled if the frame
+    changes locations before the timeout.
 */
 - (void)clientRedirectCancelledForFrame:(WebFrame *)frame;
 
--- WebKit/WebView.subproj/WebResourceLoadDelegate.h
+++ WebKit/WebView.subproj/WebResourceLoadDelegate.h
@@ -16,23 +16,25 @@
 
 /*!
     @method receivedProgress:forResourceHandle:fromDataSource:complete:
+    @abstract Notify that a new chunk of data has been received for a resource.
+    @param progress An object representing the amount of progress so far
+    @param resourceHandle The resource handle loading the resource
+    @param dataSource The data source containing the resource
+    @param isComplete YES if the load is now complete, otherwise NO
     @discussion A new chunk of data has been received.  This could be a partial load
-    of a URL.  It may be useful to do incremental layout, although
+    of a URL. It may be useful to do incremental layout, although
     typically for non-base URLs this should be done after a URL (i.e. image)
     has been completely downloaded.
-    @param progress
-    @param resourceHandle
-    @param dataSource
-    @param isComplete
 */
 - (void)receivedProgress: (WebLoadProgress *)progress forResourceHandle: (WebResourceHandle *)resourceHandle fromDataSource: (WebDataSource *)dataSource complete: (BOOL)isComplete;
 
 /*!
     @method receivedError:forResourceHandle:partialProgress:fromDataSource:
-    @param error
-    @param resourceHandle
-    @param progress
-    @param dataSource
+    @abstract Notify that an error took place loading a resource.
+    @param error An object representing the error that occurred
+    @param resourceHandle The resource handle that was loading the resource
+    @param progress The amoung of progress made before the error
+    @param dataSource The data source responsible for the resource
 */
 - (void)receivedError: (WebError *)error forResourceHandle: (WebResourceHandle *)resourceHandle partialProgress: (WebLoadProgress *)progress fromDataSource: (WebDataSource *)dataSource;
 
