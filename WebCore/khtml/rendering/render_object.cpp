ea54a3e78bd94e4514d19784754ec4e677a4bf6d
David Hyatt <hyatt@apple.com>
2003-02-07 18:16:31 +0000

    *** empty log message ***
    Canonical link: https://commits.webkit.org/3348@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@3596 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/rendering/render_object.cpp
+++ WebCore/khtml/rendering/render_object.cpp
@@ -1000,8 +1000,16 @@ RenderObject *RenderObject::container() const
 {
     EPosition pos = m_style->position();
     RenderObject *o = 0;
-    if( pos == FIXED )
-	o = root();
+    if( pos == FIXED ) {
+        // container() can be called on an object that is not in the
+        // tree yet.  We don't call root() since it will assert if it
+        // can't get back to the root.  Instead we just walk as high up
+        // as we can.  If we're in the tree, we'll get the root.  If we
+        // aren't we'll get the root of our little subtree (most likely
+        // we'll just return 0).
+        o = parent();
+        while ( o && o->parent() ) o = o->parent();
+    }
     else if ( pos == ABSOLUTE )
 	o = containingBlock();
     else
