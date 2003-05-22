35226193684ed189c3362ed8f927a7c978bb7932
Darin Adler <darin@apple.com>
2003-05-22 16:55:36 +0000

    Reviewed by John.
            - removed all entries except for jaguar.com because:
                a) most of these sites now work fine without the spoofing or have gone away
                b) nj.com and oregonlive.com do not work, but the spoofing committee (Mark,
                   Don, Dave, and me) decided we should stop spoofing and get them to fix
                   the sites instead
              If we can resolve jaguar.com in a similar way, we can remove the spoofing
              feature altogether.
    
            * WebView.subproj/WebUserAgentSpoofTable.gperf: Removed all but jaguar.com.
            * WebView.subproj/WebUserAgentSpoofTable.c: Regenerated.
    
    
    Canonical link: https://commits.webkit.org/4037@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@4413 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebView.subproj/WebUserAgentSpoofTable.c
+++ WebKit/WebView.subproj/WebUserAgentSpoofTable.c
@@ -1,7 +1,7 @@
 /* ANSI-C code produced by gperf version 2.7.2 */
 /* Command-line: gperf -CEot -L ANSI-C -k '*' -N _web_findSpoofTableEntry -F ,0 WebView.subproj/WebUserAgentSpoofTable.gperf  */
 struct UserAgentSpoofTableEntry { const char *name; UserAgentStringType type; };
-/* maximum key range = 26, duplicates = 0 */
+/* maximum key range = 1, duplicates = 0 */
 
 #ifdef __GNUC__
 __inline
@@ -15,58 +15,38 @@ hash (register const char *str, register unsigned int len)
 {
   static const unsigned char asso_values[] =
     {
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32,  5,  0, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32,  0, 10,  0,
-       0,  0,  0,  0,  0,  0,  0, 32,  0,  0,
-       0,  0,  0, 32,  0,  0, 10,  0,  0,  0,
-      32,  5,  0, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      32, 32, 32, 32, 32, 32
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11,  0, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11,  0, 11,  0,
+      11, 11, 11,  0, 11, 11,  0, 11, 11,  0,
+      11,  0, 11, 11,  0, 11, 11,  0, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11
     };
   register int hval = len;
 
   switch (hval)
     {
       default:
-      case 20:
-        hval += asso_values[(unsigned char)str[19]];
-      case 19:
-        hval += asso_values[(unsigned char)str[18]];
-      case 18:
-        hval += asso_values[(unsigned char)str[17]];
-      case 17:
-        hval += asso_values[(unsigned char)str[16]];
-      case 16:
-        hval += asso_values[(unsigned char)str[15]];
-      case 15:
-        hval += asso_values[(unsigned char)str[14]];
-      case 14:
-        hval += asso_values[(unsigned char)str[13]];
-      case 13:
-        hval += asso_values[(unsigned char)str[12]];
-      case 12:
-        hval += asso_values[(unsigned char)str[11]];
-      case 11:
-        hval += asso_values[(unsigned char)str[10]];
       case 10:
         hval += asso_values[(unsigned char)str[9]];
       case 9:
@@ -100,36 +80,18 @@ _web_findSpoofTableEntry (register const char *str, register unsigned int len)
 {
   enum
     {
-      TOTAL_KEYWORDS = 14,
-      MIN_WORD_LENGTH = 6,
-      MAX_WORD_LENGTH = 20,
-      MIN_HASH_VALUE = 6,
-      MAX_HASH_VALUE = 31
+      TOTAL_KEYWORDS = 1,
+      MIN_WORD_LENGTH = 10,
+      MAX_WORD_LENGTH = 10,
+      MIN_HASH_VALUE = 10,
+      MAX_HASH_VALUE = 10
     };
 
   static const struct UserAgentSpoofTableEntry wordlist[] =
     {
-      {"",0}, {"",0}, {"",0}, {"",0}, {"",0}, {"",0},
-      {"nj.com", MacIE},
+      {"",0}, {"",0}, {"",0}, {"",0}, {"",0}, {"",0}, {"",0},
       {"",0}, {"",0}, {"",0},
-      {"jaguar.com", MacIE},
-      {"porsche.com", MacIE},
-      {"mazdausa.com", MacIE},
-      {"hondacars.com", MacIE},
-      {"oregonlive.com", MacIE},
-      {"",0},
-      {"olympic.org", MacIE},
-      {"",0},
-      {"hondaredriders.com", MacIE},
-      {"watch.com", MacIE},
-      {"",0},
-      {"freebsd.org", MacIE},
-      {"firstusa.com", MacIE},
-      {"microsoft.com", MacIE},
-      {"",0},
-      {"wap.sonyericsson.com", MacIE},
-      {"",0}, {"",0}, {"",0}, {"",0}, {"",0},
-      {"bang-olufsen.com", MacIE}
+      {"jaguar.com", MacIE}
     };
 
   if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH)
