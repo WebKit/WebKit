e963420a0d8f5330548103ae77f27fec977a1081
Chris Blumenberg <cblu@apple.com>
2003-06-09 22:40:07 +0000

    <rdar://problem/3283359>: don't load Cocoa Java plug-in if in Carbon app
            Reviewed by darin.
    
            * Plugins.subproj/WebPluginDatabase.h: removed pluginForFilename, wasn't being used
            * Plugins.subproj/WebPluginDatabase.m:
            (-[WebPluginDatabase pluginForKey:withEnumeratorSelector:]): don't use the plug-in if ![self isCocoa] && [[[webPlugin bundle] bundleIdentifier] isEqualToString:JavaCocoaPluginIdentifier]
    
    
    Canonical link: https://commits.webkit.org/4121@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@4507 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/Plugins.subproj/WebPluginDatabase.m
+++ WebKit/Plugins.subproj/WebPluginDatabase.m
@@ -3,7 +3,7 @@
 	Copyright (c) 2002, Apple, Inc. All rights reserved.
 */
 
-
+#import <WebKit/WebAssertions.h>
 #import <WebKit/WebBasePluginPackage.h>
 #import <WebKit/WebDataSource.h>
 #import <WebKit/WebFrame.h>
@@ -16,6 +16,8 @@
 #import <WebKit/WebPluginPackage.h>
 
 #import <CoreGraphics/CPSProcesses.h>
+
+#define JavaCocoaPluginIdentifier 	@"com.apple.JavaPluginCocoa"
 
 @implementation WebPluginDatabase
 
@@ -56,42 +58,45 @@ static BOOL sIsCocoa = FALSE;
 - (WebBasePluginPackage *)pluginForKey:(NSString *)key withEnumeratorSelector:(SEL)enumeratorSelector
 {
     WebBasePluginPackage *plugin, *CFMPlugin=nil, *machoPlugin=nil, *webPlugin=nil;
-    uint i;
     NSString *lowercaseKey = [key lowercaseString];
+    uint i;
 
-    for(i=0; i<[plugins count]; i++){
+    for (i=0; i<[plugins count]; i++) {
         plugin = [plugins objectAtIndex:i];
-        if([[[plugin performSelector:enumeratorSelector] allObjects] containsObject:lowercaseKey]){
-            if([plugin isKindOfClass:[WebPluginPackage class]]){
-                if(webPlugin == nil){
+        if ([[[plugin performSelector:enumeratorSelector] allObjects] containsObject:lowercaseKey]) {
+            if ([plugin isKindOfClass:[WebPluginPackage class]]) {
+                if (webPlugin == nil) {
                     webPlugin = plugin;
                 }
-            }else if([plugin isKindOfClass:[WebNetscapePluginPackage class]]){
+            } else if([plugin isKindOfClass:[WebNetscapePluginPackage class]]) {
                 WebExecutableType executableType = [(WebNetscapePluginPackage *)plugin executableType];
-                if(executableType == WebCFMExecutableType){
-                    if(CFMPlugin == nil){
+                if (executableType == WebCFMExecutableType) {
+                    if (CFMPlugin == nil) {
                         CFMPlugin = plugin;
                     }
-                }else if(executableType == WebMachOExecutableType){
-                    if(machoPlugin == nil){
+                } else if(executableType == WebMachOExecutableType) {
+                    if (machoPlugin == nil) {
                         machoPlugin = plugin;
                     }
-                }else{
-                    [NSException raise:NSInternalInconsistencyException
-                                format:@"Unknown executable type for plugin"];
+                } else {
+                    ASSERT_NOT_REACHED();
                 }
-            }else{
-                [NSException raise:NSInternalInconsistencyException
-                            format:@"Unknown plugin class: %@", [plugin className]];
+            } else {
+                ASSERT_NOT_REACHED();
             }
         }
     }
+
+    // The Cocoa Java plug-in won't work in a Carbon app.
+    if (webPlugin && ![self isCocoa] && [[[webPlugin bundle] bundleIdentifier] isEqualToString:JavaCocoaPluginIdentifier]) {
+        webPlugin = nil;
+    }
     
-    if(webPlugin && [self isCocoa]){
+    if (webPlugin) {
         return webPlugin;
-    }else if(machoPlugin){
+    } else if (machoPlugin) {
         return machoPlugin;
-    }else{
+    } else {
         return CFMPlugin;
     }
 }
@@ -106,20 +111,6 @@ static BOOL sIsCocoa = FALSE;
 {
     return [self pluginForKey:[extension lowercaseString]
        withEnumeratorSelector:@selector(extensionEnumerator)];
-}
-
-- (WebBasePluginPackage *)pluginForFilename:(NSString *)filename
-{
-    uint i;
-    WebBasePluginPackage *plugin;
-    
-    for(i=0; i<[plugins count]; i++){
-        plugin = [plugins objectAtIndex:i];
-        if([[plugin filename] isEqualToString:filename]){
-            return plugin;
-        }
-    }
-    return nil;
 }
 
 - (NSArray *)plugins
