6622c848bfada45fdc5df1a34fa3b0ce865b70a9
Maciej Stachowiak <mjs@apple.com>
2003-08-05 23:06:42 +0000

    Reviewed by John.
            * kwq/KWQPainter.mm:
            (QPainter::drawLine): Print a warning in debug builds when drawing
    	a dashed line. Also, revert accidental commenting of lines that
    	set the dash style.
    
    
    Canonical link: https://commits.webkit.org/4337@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@4773 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/kwq/KWQPainter.mm
+++ WebCore/kwq/KWQPainter.mm
@@ -255,6 +255,8 @@ void QPainter::drawLine(int x1, int y1, int x2, int y2)
     [graphicsContext setShouldAntialias: NO];
     
     if (patWidth) {
+	ERROR("Drawing a dashed line. This may trigger a CoreGraphics memory trasher.");
+
         // Do a rect fill of our endpoints.  This ensures we always have the
         // appearance of being a border.  We then draw the actual dotted/dashed line.
         if (x1 == x2) {
@@ -296,8 +298,8 @@ void QPainter::drawLine(int x1, int y1, int x2, int y2)
             }
         }
         
-        //const float dottedLine[2] = { patWidth, patWidth };
-        //[path setLineDash:dottedLine count:2 phase:patternOffset];
+        const float dottedLine[2] = { patWidth, patWidth };
+        [path setLineDash:dottedLine count:2 phase:patternOffset];
     }
     
     [path moveToPoint:p1];
