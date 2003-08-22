303d842a3b9a8540a067f1a5bebf4010ad54af95
Darin Adler <darin@apple.com>
2003-08-22 04:22:35 +0000

    * Makefile.am: Work around DLCFN issue to prevent constant churn of config.h.         * config.h: Regenerated without DLCFN.
    Canonical link: https://commits.webkit.org/4437@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@4878 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/config.h
+++ WebCore/config.h
@@ -6,7 +6,6 @@
 #define HAVE_DIRENT_H 1
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#define HAVE_DLFCN_H 1
 
 /* Define to 1 if you have the <float.h> header file. */
 #define HAVE_FLOAT_H 1
