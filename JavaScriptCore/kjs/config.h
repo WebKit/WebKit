058bca76bb144de24eb6e6694e0e6935b061bdee
Darin Adler <darin@apple.com>
2003-09-22 15:21:10 +0000

    Reviewed by Ken.
            * kjs/config.h: Added HAVE_SYS_PARAM_H, since KJS does look for this header, and we do
            indeed have it. Just something I noticed in passing while cleaning up configure.in.
    
    
    Canonical link: https://commits.webkit.org/4558@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@5028 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/config.h
+++ JavaScriptCore/kjs/config.h
@@ -3,6 +3,7 @@
 #define HAVE_FUNC_STRTOLL 1
 #define HAVE_STRING_H 1
 #define HAVE_STRINGS_H 1
+#define HAVE_SYS_PARAM_H 1
 #define HAVE_SYS_TIME_H 1
 #define HAVE_PCREPOSIX 1
 #define TIME_WITH_SYS_TIME 1
