b7639dc8999af84717f934e6e76873e7b2dedd75
Darin Adler <darin@apple.com>
2004-02-07 20:56:54 +0000

    * kwq/can-convert.mm: Added test program to see which conversions are actually supported by TEC.
    Canonical link: https://commits.webkit.org/5382@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@6047 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/kwq/can-convert.mm
@@ -0,0 +1,41 @@
+/* cd into the kwq directory, and use this command:
+
+make && g++ can-convert.mm -framework Carbon && ./a.out && rm a.out
+
+*/
+
+#include <Carbon/Carbon.h>
+
+const int NoEncodingFlags = 0;
+const int BigEndian = 0;
+const int IsJapanese = 0;
+const int LittleEndian = 0;
+const int VisualOrdering = 0;
+
+typedef struct {
+    const char *charset;
+    TextEncoding encoding;
+    int flags;
+} CharsetEntry;
+
+#define kCFStringEncodingBig5_DOSVariant (kTextEncodingBig5 | (kBig5_DOSVariant << 16))
+#define kCFStringEncodingEUC_CN_DOSVariant (kTextEncodingEUC_CN | (kEUC_CN_DOSVariant << 16))
+#define kCFStringEncodingEUC_KR_DOSVariant (kTextEncodingEUC_KR | (kEUC_KR_DOSVariant << 16))
+#define kCFStringEncodingISOLatin10 kTextEncodingISOLatin10
+#define kCFStringEncodingKOI8_U kTextEncodingKOI8_U
+#define kCFStringEncodingShiftJIS_DOSVariant (kTextEncodingShiftJIS | (kShiftJIS_DOSVariant << 16))
+
+#include "KWQCharsetData.c"
+
+int main()
+{
+    int i;
+    for (i = 0; table[i].charset; ++i) {
+        TECObjectRef c;
+        if (table[i].encoding != kCFStringEncodingUnicode
+            && TECCreateConverter(&c, table[i].encoding,
+                CreateTextEncoding(kTextEncodingUnicodeDefault, kTextEncodingDefaultVariant, kUnicode16BitFormat)) != noErr) {
+            printf("can't do %s\n", table[i].charset);
+        }
+    }
+}
