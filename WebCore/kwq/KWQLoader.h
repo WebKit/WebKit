97ccc6692e5f028d27e22a2770e600b56ab6fe08
Darin Adler <darin@apple.com>
2004-05-10 16:47:45 +0000

    Reviewed by Vicki.
            - fixed build failure seen in B&I log (due to case sensitive file system?)
    
            * kwq/KWQLoader.h: Include "KWQCString.h" rather than "QCString.h". The latter was
            failing because the forwarding header is called "qcstring.h" and I guess B&I started
            building with a case sensitive file system. I could have changed this to "qcstring.h",
            but it seemed better to use the header's real name since we're inside KWQ here.
    
    
    Canonical link: https://commits.webkit.org/5800@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@6563 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/kwq/KWQLoader.h
+++ WebCore/kwq/KWQLoader.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2004 Apple Computer, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,8 +23,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
+#include "KWQCString.h"
 #include "KWQSignal.h"
-#include "QCString.h"
 
 namespace khtml {
     class CachedObject;
