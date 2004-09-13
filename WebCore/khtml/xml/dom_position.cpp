d6fc6f43718d651ca50365cdad655944e6f3c5f2
Ken Kocienda <kocienda@apple.com>
2004-09-13 18:25:22 +0000

    Reviewed by John
            Fix for this bug:
    
            <rdar://problem/3765519> REGRESSION (Mail): word movement goes too far upstream at start of line
    
            * khtml/xml/dom_position.cpp:
            (DOM::Position::previousWordPosition): Use downstream position here when making final placement of caret.
    
    
    Canonical link: https://commits.webkit.org/6574@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@7528 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/xml/dom_position.cpp
+++ WebCore/khtml/xml/dom_position.cpp
@@ -444,7 +444,9 @@ Position Position::previousWordPosition() const
             pos = Position(node, it.range().startOffset());
         }
     }
-    pos = pos.equivalentDeepPosition().closestRenderedPosition(UPSTREAM);
+    // Use DOWNSTREAM here so that we don't jump past words at the start of lines.
+    // <rdar://problem/3765519> REGRESSION (Mail): word movement goes too far upstream at start of line
+    pos = pos.equivalentDeepPosition().closestRenderedPosition(DOWNSTREAM);
     return pos;
 }
 
