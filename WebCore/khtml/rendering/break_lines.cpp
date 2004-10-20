e857377179cfe5b9f708cc93396d145d8899b2b7
David Hyatt <hyatt@apple.com>
2004-10-20 20:58:12 +0000

    Fix for 3790936, make the unicode-breaking on the layout tests match Panther.
            Reviewed by kocienda
    
            * khtml/rendering/break_lines.cpp:
            (khtml::isBreakable):
    
    
    Canonical link: https://commits.webkit.org/6878@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@7867 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/rendering/break_lines.cpp
+++ WebCore/khtml/rendering/break_lines.cpp
@@ -84,8 +84,12 @@ bool isBreakable(const QChar *s, int pos, int len, bool breakNBSP)
     const QChar *c = s+pos;
     unsigned short ch = c->unicode();
 
-    // Newlines are always breakable, as are non-breaking spaces when breakNBSP is true.
-    if (ch == '\n' || (breakNBSP && ch == 0xa0))
+    // Newlines and spaces are always breakable, as are non-breaking spaces when breakNBSP is true.
+    // FIXME: Tiger is not allowing breaks on spaces after bullet characters like &#183;.  We don't know
+    // at the moment whether this behavior is correct or not.  Since Tiger is also not allowing breaks on spaces
+    // after hyphen-like characters, this does not seem ideal for the Web.  Therefore for now we override space
+    // characters up front and bypass the Unicode line breaking routines.
+    if (ch == '\n' || ch == ' ' || (breakNBSP && ch == 0xa0))
         return true;
 
     // If current character, or the previous character aren't simple latin1 then
