942c777693a64f9c6f4cd105c0527ebba31505f1
David Hyatt <hyatt@apple.com>
2004-11-23 05:50:35 +0000

    Fix mistake made with frameset hit testing from previous checkin.
    Canonical link: https://commits.webkit.org/7061@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@8071 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/rendering/render_frames.cpp
+++ WebCore/khtml/rendering/render_frames.cpp
@@ -94,7 +94,7 @@ bool RenderFrameSet::nodeAtPoint(NodeInfo& info, int _x, int _y, int _tx, int _t
 
     bool inside = RenderContainer::nodeAtPoint(info, _x, _y, _tx, _ty, hitTestAction) || 
                   m_resizing || canResize(_x, _y);
-    if (inside && element() && !element()->noResize() && !info.readonly()){
+    if (inside && element() && !element()->noResize() && !info.readonly() && !info.innerNode()) {
         info.setInnerNode(element());
         info.setInnerNonSharedNode(element());
     }
