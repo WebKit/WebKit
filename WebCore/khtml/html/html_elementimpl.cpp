e0809df5e7dd84282c8870ae433914d56911d27f
Adele Peterson <adele@apple.com>
2005-05-10 01:04:52 +0000

    fix for <rdar://problem/4110775> Crash will occur when double-clicking outerHTML link on W3 DOM test
            Reviewed by Darin.
    
            * khtml/html/html_elementimpl.cpp: (HTMLElementImpl::setOuterHTML): added nil check.
            This was causing a crash when you tried to set the outerHTML on an element that was no longer in the DOM tree.
    
            * layout-tests/fast/dom/outerText-no-element.html: Added. Tests case where you try to set outerText on an element no longer in the DOM tree.
            * layout-tests/fast/dom/outerText-no-element-expected.txt: Added.
            * layout-tests/fast/dynamic/outerHTML-doc.html: Added.  Tests case where someone tries to set outerHTML on the document.
            * layout-tests/fast/dynamic/outerHTML-doc-expected.txt: Added..
            * layout-tests/fast/dynamic/outerHTML-no-element.html: Added. Tests case where you try to set outerHTML on an element no longer in the DOM tree.
            * layout-tests/fast/dynamic/outerHTML-no-element-expected.txt: Added.
            * layout-tests/fast/dynamic/outerHTML-img.html: moved images to resources directory
            * layout-tests/fast/dynamic/resources/apple.gif: Added.
            * layout-tests/fast/dynamic/resources/mozilla.gif: Added.
    
    
    Canonical link: https://commits.webkit.org/8010@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@9146 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/html/html_elementimpl.cpp
+++ WebCore/khtml/html/html_elementimpl.cpp
@@ -861,7 +861,7 @@ bool HTMLElementImpl::setInnerHTML( const DOMString &html )
 bool HTMLElementImpl::setOuterHTML( const DOMString &html )
 {
     NodeImpl *p = parent();
-    if (!p->isHTMLElement())
+    if (!p || !p->isHTMLElement())
         return false;
     HTMLElementImpl *parent = static_cast<HTMLElementImpl *>(p);
     DocumentFragmentImpl *fragment = parent->createContextualFragment( html );
