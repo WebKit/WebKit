bb1530f087e01428b7aa22487e492024f89ada7d
Darin Adler <darin@apple.com>
2005-06-05 17:54:49 +0000

    - added appropriate license headers to most files and updated copyright to reflect publication dates
            * LICENSE: Added.
            * <lots of files>: Added license header.
    
            * WebKit.pbproj/project.pbxproj: Removed references to NP_objc.h.
    
            * API-Issues.rtf: Removed.
            * Misc.subproj/WebException.h: Removed.
            * Misc.subproj/WebException.m: Removed.
            * Plugins.subproj/NP_objc.h: Removed.
    
    
    Canonical link: https://commits.webkit.org/8097@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@9276 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/Carbon.subproj/CarbonWindowContentView.h
+++ WebKit/Carbon.subproj/CarbonWindowContentView.h
@@ -1,17 +1,32 @@
 /*
-        CarbonWindowContentView.h
-        Application Kit
-        Copyright (c) 2000-2001, Apple Computer, Inc.
-        All rights reserved.
-        Original Author: Mark Piccirelli
-        Responsibility: Mark Piccirelli
-
- The class of objects that are meant to be used as _contentViews of NSCarbonWindow objects.  It's really just used to provide the context in which views that are being shown by NSViewCarbonControls can operate.
-*/
-
+ * Copyright (C) 2005 Apple Computer, Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer. 
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution. 
+ * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import <AppKit/NSView.h>
-
 
 @interface CarbonWindowContentView : NSView { }
 
--- WebKit/Carbon.subproj/CarbonWindowFrame.m
+++ WebKit/Carbon.subproj/CarbonWindowFrame.m
@@ -1,14 +1,30 @@
 /*
-        NSCarbonWindowFrame.m
-        Application Kit
-        Copyright (c) 2000-2001, Apple Computer, Inc.
-        All rights reserved.
-        Original Author: Mark Piccirelli
-        Responsibility: Mark Piccirelli
-
-The class of objects that are meant to be used as _borderViews of NSCarbonWindow objects.  It's really only a subclass of NSView for the benefit of embedded NSViewCarbonControls.
-*/
-
+ * Copyright (C) 2005 Apple Computer, Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer. 
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution. 
+ * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "CarbonWindowFrame.h"
 #import "CarbonWindowAdapter.h"
