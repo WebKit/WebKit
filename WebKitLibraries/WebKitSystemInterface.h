9475ed129d3e4fa5b95767db6e72c76f0ae3149b
Maciej Stachowiak <mjs@apple.com>
2005-06-05 21:17:15 +0000

    - initial checkin of WebKitSystemInterface binary
            * WebKitSystemInterface.h: Added.
            * libWebKitSystemInterface.a: Added.
    
    
    Canonical link: https://commits.webkit.org/8100@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@9279 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebKitLibraries/WebKitSystemInterface.h
@@ -0,0 +1,18 @@
+/*      
+    WebKitSystemInterface.h
+    Copyright (C) 2005 Apple Computer, Inc. All rights reserved.    
+    
+    Public header file.
+*/
+
+#import <Cocoa/Cocoa.h>
+
+typedef enum {
+    WKCertificateParseResultSucceeded  = 0,
+    WKCertificateParseResultFailed     = 1,
+    WKCertificateParseResultPKCS7      = 2,
+} WKCertificateParseResult;
+
+CFStringRef WKCopyCFLocalizationPreferredName(CFStringRef localization);
+CFStringRef WKSignedPublicKeyAndChallengeString(unsigned keySize, CFStringRef challenge, CFStringRef keyDescription);
+WKCertificateParseResult WKAddCertificatesToKeychainFromData(const void *bytes, unsigned length);
