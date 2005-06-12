b8699fb5690119a4c72a6b6befa1ac0eb21130d9
Darin Adler <darin@apple.com>
2005-06-12 22:48:34 +0000

    Reviewed by Maciej.         Includes changes by Stuart Morgan as well as my own.
            * Scripts/build-dumprendertree: Call the new setConfiguration function, and use XcodeOptions instead of
            symrootXcodeOptions to set the -buildstyle option.
            * Scripts/build-webkit: Ditto. Also remove the old way of supporting Xcode 2.1 and the old --debug option.
            The new --development option does the same thing.
            * Scripts/run-safari: Ditto.
            * Scripts/run-webkit-tests: Ditto.
            * Scripts/update-webkit: Ditto.
            * Scripts/set-webkit-configuration: Added. Sets the default configuration to Development or Deployment.
    
            * Scripts/webkitdirs.pm: use FindBin to find the WebKit directory; works no matter what the current
            directory is when invoking a script. Add code to determine the Xcode version so we can do the right
            thing for 2.0 and 2.1. Change the productDir function to return the appropriate per-configuration
            product directory. Read the default configuration from a file, overridable by a passed-in command-line
            option.
    
    
    Canonical link: https://commits.webkit.org/8182@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@9366 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/webkitdirs.pm
+++ WebKitTools/Scripts/webkitdirs.pm
@@ -28,69 +28,129 @@
 
 use strict;
 use warnings;
+use FindBin;
 
 BEGIN {
    use Exporter   ();
    our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
    $VERSION     = 1.00;
    @ISA         = qw(Exporter);
-   @EXPORT      = qw(&chdirWebKit &productDir &symrootXcodeOptions);
+   @EXPORT      = qw(&chdirWebKit &baseProductDir &productDir &XcodeOptions &passedConfiguration &setConfiguration &checkFrameworks);
    %EXPORT_TAGS = ( );
    @EXPORT_OK   = ();
 }
 
 our @EXPORT_OK;
 
-my $productDir;
-my @options;
+my $baseProductDir;
+my @baseProductDirOption;
+my $configuration;
+my $configurationProductDir;
+my $didChdirWebKit;
+my $XcodeVersion;
 
 # Check that we're in the right directory.
 sub chdirWebKit
 {
-    if (! -d "WebKitTools") {
-        if (-d "../WebKitTools") {
-            chdir ".." or die;
-        }
-        if (-d "../../WebKitTools") {
-            chdir "../.." or die;
-        }
-        if (! -d "WebKitTools") {
-            die "No WebKitTools directory found. Please run this script from the directory containing WebKitTools.\n";
-        }
-    }
+    return if $didChdirWebKit;
+    $didChdirWebKit = 1;
+    chdir "$FindBin::Bin/../.." or die;
 }
 
-# Check that an Xcode product directory is set, setting the SYMROOT environment variable
-# as a side effect in case it's not so that we will effectively have a temporary Xcode
-# product directory for xcodebuild commands called from the script.
-sub findProductDir
+sub determineXCodeVersion
 {
-    return if defined $productDir;
-    open PRODUCT, "defaults read com.apple.Xcode PBXProductDirectory 2> /dev/null |" or die;
-    $productDir = <PRODUCT>;
-    close PRODUCT;
-    if ($productDir) {
-        chomp $productDir;
-        @options = ();
-    } else {
-        $productDir = "$ENV{HOME}/WebKitBuild";
-        @options = ("SYMROOT=$productDir");
-    }
-    $productDir =~ s|^~/|$ENV{HOME}/|;
+    return if defined $XcodeVersion;
+    # Could use "xcodebuild -version" instead.
+    open VERSION, "defaults read /Developer/Applications/Xcode.app/Contents/Info CFBundleShortVersionString 2> /dev/null |" or die;
+    $XcodeVersion = <VERSION>;
+    close VERSION;
+    chomp $XcodeVersion;
 }
 
-# Get product directory.
+sub determineBaseProductDir
+{
+    return if defined $baseProductDir;
+    open PRODUCT, "defaults read com.apple.Xcode PBXProductDirectory 2> /dev/null |" or die;
+    $baseProductDir = <PRODUCT>;
+    close PRODUCT;
+    if ($baseProductDir) {
+        chomp $baseProductDir;
+        @baseProductDirOption = ();
+    } else {
+        chdirWebKit();
+        $baseProductDir = `pwd` . "/WebKitBuild";
+        @baseProductDirOption = ("SYMROOT=$baseProductDir");
+    }
+    $baseProductDir =~ s|^~/|$ENV{HOME}/|;
+}
+
+sub determineConfiguration
+{
+    return if defined $configuration;
+    determineBaseProductDir();
+    open CONFIGURATION, "$baseProductDir/Configuration" or die;
+    $configuration = <CONFIGURATION>;
+    close CONFIGURATION;
+    if ($configuration) {
+        chomp $configuration;
+    } else {
+        $configuration = "Deployment";
+    }
+}
+
+sub determineConfigurationProductDir
+{
+    determineConfiguration();
+    determineXCodeVersion();
+    if ($XcodeVersion eq "2.0") {
+        $configurationProductDir = $baseProductDir;
+    } else {
+        $configurationProductDir = "$baseProductDir/$configuration";
+    }
+}
+
+sub baseProductDir
+{
+    determineBaseProductDir();
+    return $baseProductDir;
+}
+
 sub productDir
 {
-    findProductDir();
-    return $productDir;
+    determineConfigurationProductDir();
+    return $configurationProductDir;
 }
 
-# Get SYMROOT options for Xcode.
-sub symrootXcodeOptions
+sub XcodeOptions
 {
-    findProductDir();
-    return @options;
+    determineBaseProductDir();
+    determineConfiguration();
+    return (@baseProductDirOption, "-buildstyle", $configuration);
+}
+
+sub passedConfiguration
+{
+    for my $opt (@ARGV) {
+        return "Development" if $opt =~ /^--devel/i;
+        return "Deployment" if $opt =~ /^--deploy/i;
+    }
+    return undef;
+}
+
+sub setConfiguration
+{
+    my $passed = passedConfiguration();
+    $configuration = $passed if $passed;
+}
+
+# Check to see that all the frameworks are built.
+sub checkFrameworks
+{
+    determineConfigurationProductDir();
+    for my $framework ("JavaScriptCore", "WebCore", "WebKit") {
+        my $path = "$configurationProductDir/$framework.framework/Versions/A/$framework";
+        die "Can't find built framework at \"$path\".\n" unless -x $path;
+    }
 }
 
 1;
