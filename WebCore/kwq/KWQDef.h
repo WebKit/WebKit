d82fee4512e56e824a4027e7cc35f31cc0464767
Geoffrey Garen <ggaren@apple.com>
2005-07-05 18:37:32 +0000

    -rolled out patch for         http://bugzilla.opendarwin.org/show_bug.cgi?id=3826         because it fails to build w/gcc3.3 (previous fix didn't completely work)
            * kwq/KWQDef.h:
    
    
    Canonical link: https://commits.webkit.org/8361@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@9591 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/kwq/KWQDef.h
+++ WebCore/kwq/KWQDef.h
@@ -33,9 +33,6 @@ typedef unsigned char uchar;
 typedef unsigned long ulong;
 #endif
 
-typedef int64_t Q_INT64;
-typedef u_int64_t Q_UINT64;
-
 typedef int Q_INT32;
 typedef unsigned int Q_UINT32;
 
@@ -50,8 +47,6 @@ inline const T& kMax ( const T& a, const T& b ) { return b < a ? a : b; }
 
 #define QABS(a) (((a) >= 0) ? (a) : -(a))
 
-inline int qRound(double a) { return lround(a); }
-
 #ifndef TRUE
 #define TRUE 1
 #endif
@@ -64,7 +59,7 @@ void qDebug(const char *msg, ...);
 void qWarning(const char *msg, ...);
 
 /* Silly hack to avoid "unused parameter" warnings */
-#define Q_UNUSED(x) (void)(x)
+#define Q_UNUSED(x) (x)=(x)
 
 #define Q_ASSERT(arg) do {} while(0)
 
