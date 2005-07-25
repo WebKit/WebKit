b8e369cdd194042fd421594663a6df664f0eff88
Justin Garcia <justin.garcia@apple.com>
2005-07-25 21:50:27 +0000

    Reviewed by hyatt
            Backing out a change that made the caret invisible in Mail in certain situations.
    
            * khtml/rendering/render_block.cpp:
            (khtml::RenderBlock::paintObject):
    
    
    Canonical link: https://commits.webkit.org/8523@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@9887 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/rendering/render_block.cpp
+++ WebCore/khtml/rendering/render_block.cpp
@@ -1324,7 +1324,7 @@ void RenderBlock::paintObject(PaintInfo& i, int _tx, int _ty)
     // 6. paint caret.
     // If the caret's node's render object's containing block is this block, and the paint action is PaintActionForeground,
     // then paint the caret.
-    if (!inlineFlow && paintAction == PaintActionForeground && element() && element()->isContentEditable()) {        
+    if (!inlineFlow && paintAction == PaintActionForeground) {        
         paintCaret(i, CursorCaret);
         paintCaret(i, DragCaret);
     }
