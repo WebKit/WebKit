b10a8fadd674aa7a97fcab747e3a8a27b39390ec
Adele Peterson <adele@apple.com>
2005-08-04 21:33:47 +0000

    Reviewed by John.
            - fixed <rdar://problem/4204496> REGRESSION(denver): repro crash in KHTMLParser::finished() downloading attachment at webmail.mac.com
            This regression was introduced by the fix for <rdar://problem/4084029>.  It was just missing a nil check.
    
            * khtml/html/htmlparser.cpp: (KHTMLParser::finished): added nil check for current.
    
    
    Canonical link: https://commits.webkit.org/8622@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@10041 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/html/htmlparser.cpp
+++ WebCore/khtml/html/htmlparser.cpp
@@ -1360,7 +1360,7 @@ void HTMLParser::startBody()
 void HTMLParser::finished()
 {
     // In the case of a completely empty document, here's the place to create the HTML element.
-    if (current->isDocumentNode() && current->firstChild() == 0) {
+    if (current && current->isDocumentNode() && current->firstChild() == 0) {
         insertNode(new HTMLHtmlElementImpl(document));
     }
 
