19a7d95c0a3be6b4dfcb39e6fc57211be2beb131
Anders Carlsson <andersca@apple.com>
2005-08-26 19:14:27 +0000

    Fix copyright header
    Canonical link: https://commits.webkit.org/8815@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@10343 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/ecma/domparser.cpp
+++ WebCore/khtml/ecma/domparser.cpp
@@ -1,7 +1,7 @@
 // -*- c-basic-offset: 2 -*-
 /*
  *  This file is part of the KDE libraries
- *  Copyright (C) 2003 Apple Computer, Inc.
+ *  Copyright (C) 2005 Anders Carlsson (andersca@mac.com)
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
