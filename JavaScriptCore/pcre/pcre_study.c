9b1f5040e84546855efb6b97b1935165a975d290
Darin Adler <darin@apple.com>
2005-09-09 00:51:07 +0000

    Reviewed by Geoff.
            - updated to PCRE 6.1
    
            The original PCRE 6.1 sources are checked into the tree with the tag
            "pcre-6-1" for reference. What we're checking in right now is the original
            plus our changes to make it support UTF-16 and at least one other tweak
            (vertical tab considered whitespace). Our work to get our changes was
            done on "pcre-6-1-branch", with an anchor at "pcre-6-1-anchor" so you can
            see the evolution of the UTF-16 changes.
    
            Note also that there was one small change made here that's not on the branch
            in pcre_compile.c.
    
            * Info.plist: Updated the part of the copyright message that's about PCRE.
    
            * JavaScriptCore.xcodeproj/project.pbxproj: Added new PCRE source files,
            removed obsolete ones.
    
            * pcre/AUTHORS: Updated to PCRE 6.1. Includes credits for Apple's UTF-16
            changes, but not the credits for Google's C++ wrapper, since we don't include that.
            * pcre/COPYING: Updated to PCRE 6.1.
            * pcre/LICENCE: Ditto.
            * pcre/dftables.c: Ditto.
            * pcre/pcre-config.h: Ditto.
            * pcre/pcre.h: Ditto.
    
            * pcre/pcre_compile.c: Added for PCRE 6.1.
            * pcre/pcre_config.c: Ditto.
            * pcre/pcre_exec.c: Ditto.
            * pcre/pcre_fullinfo.c: Ditto.
            * pcre/pcre_get.c: Ditto.
            * pcre/pcre_globals.c: Ditto.
            * pcre/pcre_info.c: Ditto.
            * pcre/pcre_internal.h: Ditto.
            * pcre/pcre_maketables.c: Ditto.
            * pcre/pcre_ord2utf8.c: Ditto.
            * pcre/pcre_printint.c: Ditto.
            * pcre/pcre_refcount.c: Ditto.
            * pcre/pcre_study.c: Ditto.
            * pcre/pcre_tables.c: Ditto.
            * pcre/pcre_try_flipped.c: Ditto.
            * pcre/pcre_ucp_findchar.c: Ditto.
            * pcre/pcre_version.c: Ditto.
            * pcre/pcre_xclass.c: Ditto.
            * pcre/ucp.h: Ditto.
            * pcre/ucp_findchar.c: Ditto.
            * pcre/ucpinternal.h: Ditto.
            * pcre/ucptable.c: Ditto.
    
            * pcre/get.c: Removed.
            * pcre/internal.h: Removed.
            * pcre/maketables.c: Removed.
            * pcre/pcre.c: Removed.
            * pcre/study.c: Removed.
    
    
    Canonical link: https://commits.webkit.org/8940@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@10495 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/pcre/pcre_study.c
+++ JavaScriptCore/pcre/pcre_study.c
@@ -1,45 +1,48 @@
 /*************************************************
 *      Perl-Compatible Regular Expressions       *
-*  extended to UTF-16 for use in JavaScriptCore  *
 *************************************************/
 
-/*
-This is a library of functions to support regular expressions whose syntax
-and semantics are as close as possible to those of the Perl 5 language. See
-the file Tech.Notes for some information on the internals.
+/* PCRE is a library of functions to support regular expressions whose syntax
+and semantics are as close as possible to those of the Perl 5 language.
 
-Written by: Philip Hazel <ph10@cam.ac.uk>
-
-           Copyright (c) 1997-2001 University of Cambridge
-           Copyright (C) 2004 Apple Computer, Inc.
+                       Written by Philip Hazel
+           Copyright (c) 1997-2005 University of Cambridge
 
 -----------------------------------------------------------------------------
-Permission is granted to anyone to use this software for any purpose on any
-computer system, and to redistribute it freely, subject to the following
-restrictions:
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
-1. This software is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
 
-2. The origin of this software must not be misrepresented, either by
-   explicit claim or by omission.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
 
-3. Altered versions must be plainly marked as such, and must not be
-   misrepresented as being the original software.
+    * Neither the name of the University of Cambridge nor the names of its
+      contributors may be used to endorse or promote products derived from
+      this software without specific prior written permission.
 
-4. If PCRE is embedded in any software that is released under the GNU
-   General Purpose Licence (GPL), then the terms of that licence shall
-   supersede any condition above with which it is incompatible.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 -----------------------------------------------------------------------------
 */
 
 
-/* Include the internals header, which itself includes Standard C headers plus
-the external pcre header. */
+/* This module contains the external function pcre_study(), along with local
+supporting functions. */
 
-#include "internal.h"
 
+#include "pcre_internal.h"
 
 
 /*************************************************
@@ -59,14 +62,11 @@ Returns:        nothing
 */
 
 static void
-set_bit(uschar *start_bits, int c, BOOL caseless, compile_data *cd)
+set_bit(uschar *start_bits, unsigned int c, BOOL caseless, compile_data *cd)
 {
 start_bits[c/8] |= (1 << (c&7));
 if (caseless && (cd->ctypes[c] & ctype_letter) != 0)
-  {
-  ichar flipped = MAPCHAR(cd->fcc, c);
-  start_bits[flipped/8] |= (1 << (flipped&7));
-  }
+  start_bits[cd->fcc[c]/8] |= (1 << (cd->fcc[c]&7));
 }
 
 
@@ -81,8 +81,9 @@ goes by, we may be able to get more clever at doing this.
 
 Arguments:
   code         points to an expression
-  start_bits   points to a table (32 bytes for 8 bit characters), initialized to 0
+  start_bits   points to a 32-byte table, initialized to 0
   caseless     the current state of the caseless flag
+  utf8         TRUE if in UTF-8 mode
   cd           the block with char table pointers
 
 Returns:       TRUE if table built, FALSE otherwise
@@ -90,7 +91,7 @@ Returns:       TRUE if table built, FALSE otherwise
 
 static BOOL
 set_start_bits(const uschar *code, uschar *start_bits, BOOL caseless,
-  compile_data *cd)
+  BOOL utf8, compile_data *cd)
 {
 register int c;
 
@@ -104,7 +105,7 @@ volatile int dummy;
 
 do
   {
-  const uschar *tcode = code + 3;
+  const uschar *tcode = code + 1 + LINK_SIZE;
   BOOL try_next = TRUE;
 
   while (try_next)
@@ -114,7 +115,7 @@ do
 
     if ((int)*tcode >= OP_BRA || *tcode == OP_ASSERT)
       {
-      if (!set_start_bits(tcode, start_bits, caseless, cd))
+      if (!set_start_bits(tcode, start_bits, caseless, utf8, cd))
         return FALSE;
       try_next = FALSE;
       }
@@ -123,6 +124,12 @@ do
       {
       default:
       return FALSE;
+
+      /* Skip over callout */
+
+      case OP_CALLOUT:
+      tcode += 2 + 2*LINK_SIZE;
+      break;
 
       /* Skip over extended extraction bracket number */
 
@@ -135,8 +142,8 @@ do
       case OP_ASSERT_NOT:
       case OP_ASSERTBACK:
       case OP_ASSERTBACK_NOT:
-      do tcode += (tcode[1] << 8) + tcode[2]; while (*tcode == OP_ALT);
-      tcode += 3;
+      do tcode += GET(tcode, 1); while (*tcode == OP_ALT);
+      tcode += 1+LINK_SIZE;
       break;
 
       /* Skip over an option setting, changing the caseless flag */
@@ -150,11 +157,11 @@ do
 
       case OP_BRAZERO:
       case OP_BRAMINZERO:
-      if (!set_start_bits(++tcode, start_bits, caseless, cd))
+      if (!set_start_bits(++tcode, start_bits, caseless, utf8, cd))
         return FALSE;
       dummy = 1;
-      do tcode += (tcode[1] << 8) + tcode[2]; while (*tcode == OP_ALT);
-      tcode += 3;
+      do tcode += GET(tcode,1); while (*tcode == OP_ALT);
+      tcode += 1+LINK_SIZE;
       break;
 
       /* Single-char * or ? sets the bit and tries the next item */
@@ -163,44 +170,34 @@ do
       case OP_MINSTAR:
       case OP_QUERY:
       case OP_MINQUERY:
-#if PCRE_UTF16
-      if (tcode[1]) return FALSE;
-      set_bit(start_bits, tcode[2], caseless, cd);
-#else
       set_bit(start_bits, tcode[1], caseless, cd);
+      tcode += 2;
+#ifdef SUPPORT_UTF8
+      if (utf8) while ((*tcode & 0xc0) == 0x80) tcode++;
 #endif
-      tcode += sizeof(ichar) + 1;
       break;
 
       /* Single-char upto sets the bit and tries the next */
 
       case OP_UPTO:
       case OP_MINUPTO:
-#if PCRE_UTF16
-      if (tcode[3]) return FALSE;
-      set_bit(start_bits, tcode[4], caseless, cd);
-#else
       set_bit(start_bits, tcode[3], caseless, cd);
+      tcode += 4;
+#ifdef SUPPORT_UTF8
+      if (utf8) while ((*tcode & 0xc0) == 0x80) tcode++;
 #endif
-      tcode += sizeof(ichar) + 3;
       break;
 
       /* At least one single char sets the bit and stops */
 
       case OP_EXACT:       /* Fall through */
-      tcode++;
+      tcode += 2;
 
-      case OP_CHARS:       /* Fall through */
-      tcode++;
-
+      case OP_CHAR:
+      case OP_CHARNC:
       case OP_PLUS:
       case OP_MINPLUS:
-#if PCRE_UTF16
-      if (tcode[1]) return FALSE;
-      set_bit(start_bits, tcode[2], caseless, cd);
-#else
       set_bit(start_bits, tcode[1], caseless, cd);
-#endif
       try_next = FALSE;
       break;
 
@@ -267,6 +264,9 @@ do
       case OP_TYPEMINQUERY:
       switch(tcode[1])
         {
+        case OP_ANY:
+        return FALSE;
+
         case OP_NOT_DIGIT:
         for (c = 0; c < 32; c++)
           start_bits[c] |= ~cd->cbits[c+cbit_digit];
@@ -301,13 +301,53 @@ do
       tcode += 2;
       break;
 
-      /* Character class: set the bits and either carry on or not,
-      according to the repeat count. */
+      /* Character class where all the information is in a bit map: set the
+      bits and either carry on or not, according to the repeat count. If it was
+      a negative class, and we are operating with UTF-8 characters, any byte
+      with a value >= 0xc4 is a potentially valid starter because it starts a
+      character with a value > 255. */
+
+      case OP_NCLASS:
+      if (utf8)
+        {
+        start_bits[24] |= 0xf0;              /* Bits for 0xc4 - 0xc8 */
+        memset(start_bits+25, 0xff, 7);      /* Bits for 0xc9 - 0xff */
+        }
+      /* Fall through */
 
       case OP_CLASS:
         {
         tcode++;
-        for (c = 0; c < 32; c++) start_bits[c] |= tcode[c];
+
+        /* In UTF-8 mode, the bits in a bit map correspond to character
+        values, not to byte values. However, the bit map we are constructing is
+        for byte values. So we have to do a conversion for characters whose
+        value is > 127. In fact, there are only two possible starting bytes for
+        characters in the range 128 - 255. */
+
+        if (utf8)
+          {
+          for (c = 0; c < 16; c++) start_bits[c] |= tcode[c];
+          for (c = 128; c < 256; c++)
+            {
+            if ((tcode[c/8] && (1 << (c&7))) != 0)
+              {
+              int d = (c >> 6) | 0xc0;            /* Set bit for this starter */
+              start_bits[d/8] |= (1 << (d&7));    /* and then skip on to the */
+              c = (c & 0xc0) + 0x40 - 1;          /* next relevant character. */
+              }
+            }
+          }
+
+        /* In non-UTF-8 mode, the two bit maps are completely compatible. */
+
+        else
+          {
+          for (c = 0; c < 32; c++) start_bits[c] |= tcode[c];
+          }
+
+        /* Advance past the bit map, and act on what follows */
+
         tcode += 32;
         switch (*tcode)
           {
@@ -329,12 +369,12 @@ do
           break;
           }
         }
-      break; /* End of class handling */
+      break; /* End of bitmap class handling */
 
       }      /* End of switch */
     }        /* End of try_next loop */
 
-  code += (code[1] << 8) + code[2];   /* Advance to next branch */
+  code += GET(code, 1);   /* Advance to next branch */
   }
 while (*code == OP_ALT);
 return TRUE;
@@ -356,16 +396,21 @@ Arguments:
   errorptr  points to where to place error messages;
             set NULL unless error
 
-Returns:    pointer to a pcre_extra block,
+Returns:    pointer to a pcre_extra block, with study_data filled in and the
+              appropriate flag set;
             NULL on error or if no optimization possible
 */
 
-pcre_extra *
+EXPORT pcre_extra *
 pcre_study(const pcre *external_re, int options, const char **errorptr)
 {
 uschar start_bits[32];
-real_pcre_extra *extra;
+pcre_extra *extra;
+pcre_study_data *study;
+const uschar *tables;
 const real_pcre *re = (const real_pcre *)external_re;
+uschar *code = (uschar *)re + re->name_table_offset +
+  (re->name_count * re->name_entry_size);
 compile_data compile_block;
 
 *errorptr = NULL;
@@ -382,29 +427,40 @@ if ((options & ~PUBLIC_STUDY_OPTIONS) != 0)
   return NULL;
   }
 
-/* For an anchored pattern, or an unchored pattern that has a first char, or a
-multiline pattern that matches only at "line starts", no further processing at
-present. */
+/* For an anchored pattern, or an unanchored pattern that has a first char, or
+a multiline pattern that matches only at "line starts", no further processing
+at present. */
 
 if ((re->options & (PCRE_ANCHORED|PCRE_FIRSTSET|PCRE_STARTLINE)) != 0)
   return NULL;
 
-/* Set the character tables in the block which is passed around */
+/* Set the character tables in the block that is passed around */
 
-compile_block.lcc = (const ichar *)(re->tables + lcc_offset);
-compile_block.fcc = (const ichar *)(re->tables + fcc_offset);
-compile_block.cbits = re->tables + cbits_offset;
-compile_block.ctypes = re->tables + ctypes_offset;
+tables = re->tables;
+if (tables == NULL)
+  (void)pcre_fullinfo(external_re, NULL, PCRE_INFO_DEFAULT_TABLES,
+  (void *)(&tables));
+
+compile_block.lcc = tables + lcc_offset;
+compile_block.fcc = tables + fcc_offset;
+compile_block.cbits = tables + cbits_offset;
+compile_block.ctypes = tables + ctypes_offset;
 
 /* See if we can find a fixed set of initial characters for the pattern. */
 
 memset(start_bits, 0, 32 * sizeof(uschar));
-if (!set_start_bits(re->code, start_bits, (re->options & PCRE_CASELESS) != 0,
-  &compile_block)) return NULL;
+if (!set_start_bits(code, start_bits, (re->options & PCRE_CASELESS) != 0,
+  (re->options & PCRE_UTF8) != 0, &compile_block)) return NULL;
 
-/* Get an "extra" block and put the information therein. */
+/* Get a pcre_extra block and a pcre_study_data block. The study data is put in
+the latter, which is pointed to by the former, which may also get additional
+data set later by the calling program. At the moment, the size of
+pcre_study_data is fixed. We nevertheless save it in a field for returning via
+the pcre_fullinfo() function so that if it becomes variable in the future, we
+don't have to change that code. */
 
-extra = (real_pcre_extra *)(pcre_malloc)(sizeof(real_pcre_extra));
+extra = (pcre_extra *)(pcre_malloc)
+  (sizeof(pcre_extra) + sizeof(pcre_study_data));
 
 if (extra == NULL)
   {
@@ -412,10 +468,15 @@ if (extra == NULL)
   return NULL;
   }
 
-extra->options = PCRE_STUDY_MAPPED;
-memcpy(extra->start_bits, start_bits, sizeof(start_bits));
+study = (pcre_study_data *)((char *)extra + sizeof(pcre_extra));
+extra->flags = PCRE_EXTRA_STUDY_DATA;
+extra->study_data = study;
 
-return (pcre_extra *)extra;
+study->size = sizeof(pcre_study_data);
+study->options = PCRE_STUDY_MAPPED;
+memcpy(study->start_bits, start_bits, sizeof(start_bits));
+
+return extra;
 }
 
-/* End of study.c */
+/* End of pcre_study.c */
