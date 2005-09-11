97ded8500fbcb22a483caea8ec989a6274def8df
Darin Adler <darin@apple.com>
2005-09-11 17:04:19 +0000

    WebKit:
            Reviewed, tweaked, and landed by Darin.
    
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=4286
              .Mac prefpane crashes when Safari using CVS WebKit is running
    
            * WebView.subproj/WebView.m: (-[WebView initWithFrame:frameName:groupName:]):
            If ENABLE_WEBKIT_UNSET_DYLD_FRAMEWORK_PATH, and WEBKIT_UNSET_DYLD_FRAMEWORK_PATH
            is set in the environment, then unset DYLD_FRAMEWORK_PATH.
    
            * WebKit.xcodeproj/project.pbxproj: Set ENABLE_WEBKIT_UNSET_DYLD_FRAMEWORK_PATH
            in configurations other than Default -- we don't want that code in production
            builds, but we want it in builds we do ourselves and nightly builds.
    
    WebKitTools:
    
            Reviewed, tweaked, and landed by Darin.
    
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=4286
              .Mac prefpane crashes when Safari using CVS WebKit is running
    
            * Scripts/run-safari: Set WEBKIT_UNSET_DYLD_FRAMEWORK_PATH.
            * Scripts/run-webkit-app: Ditto.
    
    
    Canonical link: https://commits.webkit.org/8958@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@10521 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebView.subproj/WebView.m
+++ WebKit/WebView.subproj/WebView.m
@@ -1596,7 +1596,19 @@ NSMutableDictionary *countInvocations;
     self = [super initWithFrame:f];
     if (!self)
         return nil;
-    
+
+#if ENABLE_WEBKIT_UNSET_DYLD_FRAMEWORK_PATH
+    // DYLD_FRAMEWORK_PATH is used so Safari will load the development version of WebKit, which
+    // may not work with other WebKit applications.  Unsetting DYLD_FRAMEWORK_PATH removes the
+    // need for Safari to unset it to prevent it from being passed to applications it launches.
+    // Unsetting it when a WebView is first created is as good a place as any.
+    // See <http://bugzilla.opendarwin.org/show_bug.cgi?id=4286> for more details.
+    if (getenv("WEBKIT_UNSET_DYLD_FRAMEWORK_PATH")) {
+        unsetenv("DYLD_FRAMEWORK_PATH");
+        unsetenv("WEBKIT_UNSET_DYLD_FRAMEWORK_PATH");
+    }
+#endif
+
     _private = [[WebViewPrivate alloc] init];
     [self _commonInitializationWithFrameName:frameName groupName:groupName];
     [self setMaintainsBackForwardList: YES];
