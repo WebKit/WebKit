1c4bd2166233ad285abe40fb862497042ebd0724
Darin Adler <darin@apple.com>
2005-10-09 04:25:00 +0000

    Reviewed, tweaked, and landed by Darin.
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=5189
              pcre_exec.c fails to compile using MSVC
            - fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=5190
              KJS config.h adjustment for Win32
    
            * kjs/config.h: Make sure HAVE_MMAP and HAVE_SBRK are off for Win32.
            Turn HAVE_ERRNO_H on for Mac OS X. Sort defines so they are easy to compare
            with each other. Remove #undef of DEBUG_COLLECTOR.
            * pcre/pcre_exec.c: (match): Work around strange MSVC complaint by splitting
            the definition of a local variable into a separate declaration and
            initialization.
    
    
    Canonical link: https://commits.webkit.org/9153@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@10798 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/config.h
+++ JavaScriptCore/kjs/config.h
@@ -1,7 +1,10 @@
 #if __APPLE__
 
+#define HAVE_ERRNO_H 1
 #define HAVE_FUNC_ISINF 1
 #define HAVE_FUNC_ISNAN 1
+#define HAVE_MMAP 1
+#define HAVE_SBRK 1
 #define HAVE_STRINGS_H 1
 #define HAVE_SYS_PARAM_H 1
 #define HAVE_SYS_TIME_H 1
@@ -15,31 +18,27 @@
 
 #else
 
+#define HAVE_ERRNO_H 1
 #define HAVE_FUNC_ISINF 1
 #define HAVE_FUNC_ISNAN 1
+#define HAVE_MMAP 1
+#define HAVE_SBRK 1
 #define HAVE_STRINGS_H 1
 #define HAVE_SYS_PARAM_H 1
 #define HAVE_SYS_TIME_H 1
 #define TIME_WITH_SYS_TIME 1
-#define HAVE_ERRNO_H 1
 
 #endif
 
 #define HAVE_FUNC_STRTOLL 1
 #define HAVE_ICU 1
 #define HAVE_PCREPOSIX 1
-#define HAVE_STRING_H 1
 #define HAVE_STDINT_H 1
-
-#define HAVE_MMAP 1
-#define HAVE_SBRK 1
+#define HAVE_STRING_H 1
 
 #ifdef __ppc__
 #define WORDS_BIGENDIAN 1
 #endif
-
-/* define to debug garbage collection */
-#undef DEBUG_COLLECTOR
 
 #define KXC_CHANGES 1
 
