79c330d397ebacc527541aa28da9df83ace16301
Geoffrey Garen <ggaren@apple.com>
2005-11-07 21:18:03 +0000

    Darin reviewed this a while back.
            - Fixed <rdar://problem/4161660> window.close followed by window.print
            in onload handler crashes Safari in KJS::ScopeChain::bottom
            (redmccombstoyota.com)
    
            Added a call to stopLoading inside closeWindowSoon to prevent load
            events from firing after a window has torn down.
    
            Manual test case: WebCore/manual-tests/window-close-during-parsing.html
    
            * WebCoreSupport.subproj/WebBridge.m:
            (-[WebBridge closeWindowSoon]):
    
    
    Canonical link: https://commits.webkit.org/9384@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@11089 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebCoreSupport.subproj/WebBridge.m
+++ WebKit/WebCoreSupport.subproj/WebBridge.m
@@ -351,14 +351,20 @@ NSString *WebPluginContainerKey =   @"WebPluginContainer";
 
     // We need to remove the parent WebView from WebViewSets here, before it actually
     // closes, to make sure that JavaScript code that executes before it closes
-    // can't find it. Otherwise, window.open will select a closed WebView instead of opening a new one.
+    // can't find it. Otherwise, window.open will select a closed WebView instead of 
+    // opening a new one <rdar://problem/3572585>.
 
+    // We also need to stop the load to prevent further parsing or JavaScript execution
+    // after the window has torn down <rdar://problem/4161660>.
+  
     // FIXME: This code assumes that the UI delegate will respond to a webViewClose
     // message by actually closing the WebView. Safari guarantees this behavior, but other apps might not.
     // This approach is an inherent limitation of not making a close execute immediately
     // after a call to window.close.
     
     [WebViewSets removeWebView:parentWebView fromSetNamed:[parentWebView groupName]];
+
+    [parentWebView stopLoading:self];
     
     [parentWebView performSelector:@selector(_closeWindow) withObject:nil afterDelay:0.0];
 }
