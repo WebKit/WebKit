3542d68bb349b34287e69c6598ba0aed34d61414
Eric Seidel <eric@webkit.org>
2005-12-30 05:43:23 +0000

    Bug #: 6290 Submitted by: eseidel Reviewed by: mjs         XML documents with "html" in dtd name use XHTML entities when they shouldn't         http://bugzilla.opendarwin.org/show_bug.cgi?id=6290
            * khtml/xml/xml_tokenizer.cpp:
            (khtml::externalSubsetHandler): check for exact xhtml types
    
    
    Canonical link: https://commits.webkit.org/9892@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@11822 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/xml/xml_tokenizer.cpp
+++ WebCore/khtml/xml/xml_tokenizer.cpp
@@ -666,8 +666,16 @@ static void internalSubsetHandler(void *closure, const xmlChar *name, const xmlC
 
 static void externalSubsetHandler(void *closure, const xmlChar *name, const xmlChar *externalId, const xmlChar *systemId)
 {
-    if (toQString(name).contains("html"))
-        getTokenizer(closure)->setIsXHTMLDocument(true);
+    QString extId = toQString(externalId);
+    if ((extId == "-//W3C//DTD XHTML 1.0 Transitional//EN")
+        || (extId == "-//W3C//DTD XHTML 1.1//EN")
+        || (extId == "-//W3C//DTD XHTML 1.0 Strict//EN")
+        || (extId == "-//W3C//DTD XHTML 1.0 Frameset//EN")
+        || (extId == "-//W3C//DTD XHTML Basic 1.0//EN")
+        || (extId == "-//W3C//DTD XHTML 1.1 plus MathML 2.0//EN")
+        || (extId == "-//W3C//DTD XHTML 1.1 plus MathML 2.0 plus SVG 1.1//EN")
+        || (extId == "-//WAPFORUM//DTD XHTML Mobile 1.0//EN"))
+        getTokenizer(closure)->setIsXHTMLDocument(true); // controls if we replace entities or not.
 }
 
 void XMLTokenizer::finish()
