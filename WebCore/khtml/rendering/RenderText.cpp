50b00518f7ec99267adc2599480ab2ea629731e9
David Hyatt <hyatt@apple.com>
2006-01-11 00:20:36 +0000

    Fix other gnuc spot.
    Canonical link: https://commits.webkit.org/10046@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@11992 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/khtml/rendering/RenderText.cpp
+++ WebCore/khtml/rendering/RenderText.cpp
@@ -282,7 +282,7 @@ VisiblePosition RenderText::positionForCoordinates(int _x, int _y)
     return VisiblePosition(element(), 0, DOWNSTREAM);
 }
 
-#if __GNUC
+#if __GNUC__
 static RenderObject *firstRendererOnNextLine(InlineBox *box) __attribute__ ((unused));
 #endif
 static RenderObject *firstRendererOnNextLine(InlineBox *box)
