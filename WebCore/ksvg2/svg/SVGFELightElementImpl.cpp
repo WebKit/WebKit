d94661dd1fd0b77c2f5cdc75eeadec5646bbfb2f
Eric Seidel <eric@webkit.org>
2006-01-16 00:38:47 +0000

    2006-01-15  Eric Seidel  <eseidel@apple.com>
            Rubber-stamped by darin.
    
            Updated files w/ missing copyrights from Oliver.
    
            * kcanvas/device/quartz/filters/WKArithmeticFilter.cikernel:
            * kcanvas/device/quartz/filters/WKDistantLightFilter.cikernel:
            * kcanvas/device/quartz/filters/WKPointLightFilter.cikernel:
            * kcanvas/device/quartz/filters/WKSpotLightFilter.cikernel:
            * ksvg2/svg/SVGAnimatedColorImpl.cpp:
            * ksvg2/svg/SVGAnimatedColorImpl.h:
            * ksvg2/svg/SVGFEDiffuseLightingElementImpl.cpp:
            * ksvg2/svg/SVGFEDiffuseLightingElementImpl.h:
            * ksvg2/svg/SVGFEDistantLightElementImpl.cpp:
            * ksvg2/svg/SVGFEDistantLightElementImpl.h:
            * ksvg2/svg/SVGFELightElementImpl.cpp:
            * ksvg2/svg/SVGFELightElementImpl.h:
            * ksvg2/svg/SVGFEPointLightElementImpl.cpp:
            * ksvg2/svg/SVGFEPointLightElementImpl.h:
            * ksvg2/svg/SVGFESpecularLightingElementImpl.cpp:
            * ksvg2/svg/SVGFESpecularLightingElementImpl.h:
            * ksvg2/svg/SVGFESpotLightElementImpl.cpp:
            * ksvg2/svg/SVGFESpotLightElementImpl.h:
    
    
    Canonical link: https://commits.webkit.org/10174@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@12120 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/ksvg2/svg/SVGFELightElementImpl.cpp
+++ WebCore/ksvg2/svg/SVGFELightElementImpl.cpp
@@ -1,22 +1,22 @@
 /*
- Copyright (C) 2004, 2005 Nikolas Zimmermann <wildfox@kde.org>
- 2004, 2005 Rob Buis <buis@kde.org>
- 2005 Oliver Hunt <ojh16@student.canterbury.ac.nz>
- 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Library General Public
- License as published by the Free Software Foundation; either
- version 2 of the License, or (at your option) any later version.
- 
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Library General Public License for more details.
- 
- You should have received a copy of the GNU Library General Public License
- along with this library; see the file COPYING.LIB.  If not, write to
- the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- Boston, MA 02111-1307, USA.
+    Copyright (C) 2004, 2005 Nikolas Zimmermann <wildfox@kde.org>
+                  2004, 2005 Rob Buis <buis@kde.org>
+                  2005 Oliver Hunt <ojh16@student.canterbury.ac.nz>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
  */
 
 #include "config.h"
