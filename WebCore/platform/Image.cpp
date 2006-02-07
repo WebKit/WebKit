9291aabd6d5910a652c01c10473e0c0b612b123c
David Hyatt <hyatt@apple.com>
2006-02-07 07:06:00 +0000

    Fix build bustage on Mac.
    Canonical link: https://commits.webkit.org/10647@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@12633 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/Image.cpp
+++ WebCore/platform/Image.cpp
@@ -46,7 +46,7 @@ Image::Image(ImageAnimationObserver* observer, bool isPDF)
 {
     m_data = new ImageData(this);
 #if __APPLE__
-    m_data->setIsPDF(isPDF);
+    m_data->setIsPDF();
 #endif
     m_animationObserver = observer;
 }
