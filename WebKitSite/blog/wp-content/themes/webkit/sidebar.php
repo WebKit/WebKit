48f4b0101b82780867f81453d40a709c5b38bd0f
Geoffrey Garen <ggaren@apple.com>
2006-03-06 23:05:27 +0000

    Oops, accidentally deleted the archives links.
            * blog/wp-content/themes/webkit/sidebar.php: Bring them back.
    
    
    
    Canonical link: https://commits.webkit.org/11149@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@13175 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitSite/blog/wp-content/themes/webkit/sidebar.php
+++ WebKitSite/blog/wp-content/themes/webkit/sidebar.php
@@ -1,41 +1,44 @@
 <div id="nav">
-<ul id="navlist">
-<li><a href="/">Home</a></li>
-<li><a href="/blog">Surfin' Safari Blog</a></li>
-<li><a href="/contact.html">Keeping in Touch</a></li>
+    <ul id="navlist">
+        <li><a href="/">Home</a></li>
+        <li><a href="/blog">Surfin' Safari Blog</a></li>
+        <li><a href="/contact.html">Keeping in Touch</a></li>
 
-<li class="subtitle">Working with the Code</li>
+        <li class="subtitle">Working with the Code</li>
 
-<li><a href="/building/checkout.html">Getting the Code</a></li>
-<li><a href="/building/build.html">Building WebKit</a></li>
-<li><a href="/coding/contributing.html">Contributing Code</a></li>
+        <li><a href="/building/checkout.html">Getting the Code</a></li>
+        <li><a href="/building/build.html">Building WebKit</a></li>
+        <li><a href="/coding/contributing.html">Contributing Code</a></li>
 
-<li class="subtitle">Documentation</li>
+        <li class="subtitle">Documentation</li>
 
-<li><a href="/projects/">Projects</a></li>
-<li><a href="/coding/coding-style.html">Code Style Guidelines</a></li>
+        <li><a href="/projects/">Projects</a></li>
+        <li><a href="/coding/coding-style.html">Code Style Guidelines</a></li>
 
-<li class="subtitle">Testing</li>
+        <li class="subtitle">Testing</li>
 
-<li><a href="/quality/testing.html">Regression Testing</a></li>
-<li><a href="/quality/leakhunting.html">Leak Hunting</a></li>
-<li><a href="/quality/testwriting.html">Writing New Tests</a></li>
+        <li><a href="/quality/testing.html">Regression Testing</a></li>
+        <li><a href="/quality/leakhunting.html">Leak Hunting</a></li>
+        <li><a href="/quality/testwriting.html">Writing New Tests</a></li>
 
-<li class="subtitle">Bugs</li>
+        <li class="subtitle">Bugs</li>
 
-<li><a href="/projects/compat/hitlist.html">Compatibility Hit List</a></li>
-<li><a href="/quality/reporting.html">Reporting Bugs</a></li>
-<li><a href="/quality/bugwriting.html">Bug Report Guidelines</a></li>
-<li><a href="/quality/bugpriorities.html">Bug Prioritization</a></li>
-<li><a href="/quality/reduction.html">Test Case Reduction</a></li>
-<li><a href="/quality/lifecycle.html">Bug Life Cycle</a></li>
-</ul>
-<div id="notice">
-WebKit is open source software with portions licensed under the
-<a href="http://www.opensource.org/licenses/lgpl-license.php">LGPL</a> and
-<a href="http://www.opensource.org/licenses/bsd-license.php">BSD licenses</a>.
-Complete license and copyright information can be found within the code.
-</div>
+        <li><a href="/projects/compat/hitlist.html">Compatibility Hit List</a></li>
+        <li><a href="/quality/reporting.html">Reporting Bugs</a></li>
+        <li><a href="/quality/bugwriting.html">Bug Report Guidelines</a></li>
+        <li><a href="/quality/bugpriorities.html">Bug Prioritization</a></li>
+        <li><a href="/quality/reduction.html">Test Case Reduction</a></li>
+        <li><a href="/quality/lifecycle.html">Bug Life Cycle</a></li>
+        
+        <li class="subtitle">Archives</li>
+        <?php wp_get_archives('type=monthly'); ?>    
+    </ul>
+    <div id="notice">
+        WebKit is open source software with portions licensed under the
+        <a href="http://www.opensource.org/licenses/lgpl-license.php">LGPL</a> and
+        <a href="http://www.opensource.org/licenses/bsd-license.php">BSD licenses</a>.
+        Complete license and copyright information can be found within the code.
+    </div>
 </div>
 
 <script>setCurrentLink();</script>
