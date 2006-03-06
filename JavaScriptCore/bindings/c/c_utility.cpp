2019370f0eac2a50de343f1d4d5840ac5585cb22
Alexey Proskuryakov <ap@webkit.org>
2006-03-06 19:59:09 +0000

    Patch by David Carson, reviewed by Darin.
            - Fixed http://bugzilla.opendarwin.org/show_bug.cgi?id=7582
            c_utility.cpp contains CFString OS X platform-dependent code; should use ICU
    
            Tested with test case from:
            http://bugzilla.opendarwin.org/show_bug.cgi?id=5163
    
            * bindings/c_utility.cpp
            (convertUTF8ToUTF16): Changed to using Unicode converter from ICU, and manual Latin-1 conversion.
            * icu/unicode/ucnv.h: Copied from WebCore.
            * icu/unicode/ucnv_err.h: Ditto.
            * icu/unicode/uenum.h: Ditto.
    
    
    
    Canonical link: https://commits.webkit.org/11144@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@13170 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/bindings/c/c_utility.cpp
+++ JavaScriptCore/bindings/c/c_utility.cpp
@@ -26,6 +26,7 @@
 
 #include "config.h"
 #include "c_utility.h"
+#include <unicode/ucnv.h>
 
 #include "JSType.h"
 #include "NP_jsobject.h"
@@ -45,27 +46,38 @@ void convertNPStringToUTF16(const NPString *string, NPUTF16 **UTF16Chars, unsign
 void convertUTF8ToUTF16(const NPUTF8 *UTF8Chars, int UTF8Length, NPUTF16 **UTF16Chars, unsigned int *UTF16Length)
 {
     assert(UTF8Chars || UTF8Length == 0);
+    assert(UTF16Chars);
     
     if (UTF8Length == -1)
         UTF8Length = strlen(UTF8Chars);
-
-    CFStringRef stringRef = CFStringCreateWithBytes(NULL, (const UInt8*)UTF8Chars, (CFIndex)UTF8Length, kCFStringEncodingUTF8, false);
+        
+    // UTF16Length maximum length is the length of the UTF8 string, plus one to include terminator
+    // Without the plus one, it will convert ok, but a warning is generated from the converter as
+    // there is not enough room for a terminating character.
+    *UTF16Length = UTF8Length + 1; 
+        
+    *UTF16Chars = 0;
+    UErrorCode status = U_ZERO_ERROR;
+    UConverter* conv = ucnv_open("utf8", &status);
+    if (U_SUCCESS(status)) { 
+        *UTF16Chars = (NPUTF16 *)malloc(sizeof(NPUTF16) * (*UTF16Length));
+        ucnv_setToUCallBack(conv, UCNV_TO_U_CALLBACK_STOP, 0, 0, 0, &status);
+        ucnv_toUChars(conv, *UTF16Chars, *UTF16Length, UTF8Chars, UTF8Length, &status); 
+        ucnv_close(conv);
+    } 
     
+    // Check to see if the conversion was successful
     // Some plugins return invalid UTF-8 in NPVariantType_String, see <http://bugzilla.opendarwin.org/show_bug.cgi?id=5163>
-    if (!stringRef)
-        stringRef = CFStringCreateWithBytes(NULL, (const UInt8*)UTF8Chars, (CFIndex)UTF8Length, kCFStringEncodingISOLatin1, false);
-
-    // There is no "bad data" for kCFStringEncodingISOLatin1. It is unlikely that the plugin was really sending text in this encoding,
+    // There is no "bad data" for latin1. It is unlikely that the plugin was really sending text in this encoding,
     // but it should have used UTF-8, and now we are simply avoiding a crash.
-    assert(stringRef);
+    if (!U_SUCCESS(status)) {
+        if (!*UTF16Chars)   // If the memory wasn't allocated, allocate it.
+            *UTF16Chars = (NPUTF16 *)malloc(sizeof(NPUTF16) * (*UTF16Length));
+ 
+        for (unsigned i = 0; i < *UTF16Length; i++)
+            (*UTF16Chars)[i] = UTF8Chars[i] & 0xFF;
 
-    *UTF16Length = (unsigned int)CFStringGetLength(stringRef);
-    *UTF16Chars = (NPUTF16 *)malloc(sizeof(NPUTF16) * (*UTF16Length));
-
-    // Convert the string to UTF16.
-    CFRange range = { 0, *UTF16Length };
-    CFStringGetCharacters(stringRef, range, (UniChar *)*UTF16Chars);
-    CFRelease(stringRef);
+    }
 }
 
 // Variant value must be released with NPReleaseVariantValue()
