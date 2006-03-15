f002e5b2a93cdc9c3f8792508626ada57905d97d
Geoffrey Garen <ggaren@apple.com>
2006-03-15 02:07:09 +0000

    Oops, forgot to add this file.
            * fast/js/resources/string-sort.js: Added.
    
    
    
    Canonical link: https://commits.webkit.org/11255@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@13296 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/js/resources/string-sort.js
@@ -0,0 +1,19 @@
+description("This page tests sorting of string values");
+
+function shouldBeInThisOrder(a)
+{
+    shouldBeTrue('"' + a[0] + '"' + " < " + '"' + a[1] + '"');
+    shouldBe('[' + '"' + a[0] + '"' + ',' + '"' + a[1] + '"' + '].sort().toString()', '"' + a + '"');
+}
+
+shouldBeInThisOrder(["12", "123"]);
+shouldBeInThisOrder(["123", "13"]);
+shouldBeInThisOrder(["", " "]);
+shouldBeInThisOrder(["close", "closed"]);
+shouldBeInThisOrder(["a", "~"]);
+shouldBeInThisOrder(["_", "a"]);
+shouldBeInThisOrder(["0", "A"]);
+shouldBeInThisOrder(["-", "0"]);
+shouldBeInThisOrder([" ", "-"]);
+
+var successfullyParsed = true;
