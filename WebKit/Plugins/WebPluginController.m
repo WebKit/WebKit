9863465060004a970e17c8873d72afc6c1bb0159
Darin Adler <darin@apple.com>
2006-03-16 01:15:20 +0000

    Fix by Patrick Beard, reviewed by me.
            - fix <rdar://problem/4478181> WebPluginController leaks NSArray and NSMutableSet objects
    
            * Plugins/WebPluginController.m:
            (-[WebPluginController initWithDocumentView:]): Call CFMakeCollectable on the CFSet
            so we can act like it's an NSSet.
            (-[WebPluginController dealloc]): Release _views and _checksInProgress.
    
    
    
    Canonical link: https://commits.webkit.org/11278@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@13319 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/Plugins/WebPluginController.m
+++ WebKit/Plugins/WebPluginController.m
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2005, 2006 Apple Computer, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -102,9 +102,15 @@ static NSMutableSet *pluginViews = nil;
     [super init];
     _documentView = view;
     _views = [[NSMutableArray alloc] init];
-    _checksInProgress = (NSMutableSet *)CFSetCreateMutable(NULL, 0, NULL);
-
+    _checksInProgress = (NSMutableSet *)CFMakeCollectable(CFSetCreateMutable(NULL, 0, NULL));
     return self;
+}
+
+- (void)dealloc
+{
+    [_views release];
+    [_checksInProgress release];
+    [super dealloc];
 }
 
 - (void)startAllPlugins
