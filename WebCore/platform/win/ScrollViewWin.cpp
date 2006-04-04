5607b8a9e70075517f6f20b351c49b3b63de01b7
Eric Seidel <eric@webkit.org>
2006-04-04 11:54:46 +0000

    2006-04-03  Justin Haygood <jhaygood@spsu.edu>
            Reviewed by eseidel.  Landed by eseidel.
    
            - WIN32: maximumScroll() is the maximum scroll delta, not the maximum scroll position. Update to use
              the real maximum scroll position.
            http://bugzilla.opendarwin.org/show_bug.cgi?id=8160
    
            * platform/win/ScrollViewWin.cpp:
            (WebCore::ScrollView::updateScrollBars):
    
    
    Canonical link: https://commits.webkit.org/11614@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@13679 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/win/ScrollViewWin.cpp
+++ WebCore/platform/win/ScrollViewWin.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2006 Justin Haygood <jhaygood@spsu.edu>.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -246,7 +247,8 @@ int ScrollView::updateScrollInfo(short type, int current, int max, int pageSize)
 
 void ScrollView::updateScrollBars()
 { 
-    IntSize scroll = scrollOffset().shrunkTo(maximumScroll());
+    IntSize maxScrollPosition(contentsWidth(), contentsHeight());
+    IntSize scroll = scrollOffset().shrunkTo(maxScrollPosition);
     scroll.clampNegativeToZero();
 
     m_data->scrollOffset = 
