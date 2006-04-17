702d30dbef47c5b6e7ccf98ceafd4155e0563745
jdevalk <jdevalk>
2006-04-17 21:15:06 +0000

    Removed passwords and username for blog from SVN and included from elsewhere on server.
    Canonical link: https://commits.webkit.org/11762@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@13899 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitSite/blog/wp-config.php
+++ WebKitSite/blog/wp-config.php
@@ -1,19 +1,5 @@
 <?php
-// ** MySQL settings ** //
-define('DB_NAME', 'wordpress_webkit');     // The name of the database
-define('DB_USER', 'webkit');     // Your MySQL username
-define('DB_PASSWORD', 'wordpress'); // ...and password
-define('DB_HOST', 'localhost');     // 99% chance you won't need to change this value
-
-// Change the prefix if you want to have multiple blogs in a single database.
-$table_prefix  = 'wp_';   // example: 'wp_' or 'b2' or 'mylogin_'
-
-// Change this to localize WordPress.  A corresponding MO file for the
-// chosen language must be installed to wp-includes/languages.
-// For example, install de.mo to wp-includes/languages and set WPLANG to 'de'
-// to enable German language support.
-define ('WPLANG', '');
-
+require('/var/opendarwin/secrets/webkit-blog-config.php');
 /* Stop editing */
 
 define('ABSPATH', dirname(__FILE__).'/');
