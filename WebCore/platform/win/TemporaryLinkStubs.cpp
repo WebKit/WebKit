be2b91bbc23799b8bb74dc867e44847a108fc5bc
Steve Falkenburg <sfalken@apple.com>
2006-06-06 03:52:30 +0000

    2006-06-06  Steve Falkenburg  <sfalken@apple.com>
            Fix build break
    
            * platform/win/TemporaryLinkStubs.cpp:
            (GraphicsContext::roundToDevicePixels):
    
    
    
    Canonical link: https://commits.webkit.org/12406@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@14741 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/win/TemporaryLinkStubs.cpp
+++ WebCore/platform/win/TemporaryLinkStubs.cpp
@@ -286,6 +286,7 @@ void GraphicsContext::clip(const Path&) { }
 void GraphicsContext::translate(const FloatSize&) { }
 void GraphicsContext::rotate(float) { }
 void GraphicsContext::scale(const FloatSize&) { }
+FloatRect GraphicsContext::roundToDevicePixels(const FloatRect& rect) { return rect; }
 
 Path::Path(){ }
 Path::~Path(){ }
