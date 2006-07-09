8f5af7141b4fe644a94b35db0af4bf86d23fb3d6
Alexey Proskuryakov <ap@webkit.org>
2006-07-09 07:04:00 +0000

    Reviewed by Darin.
            - http://bugzilla.opendarwin.org/show_bug.cgi?id=9572
            Add application/xhtml+xml to the Accept header
    
            * WebView/WebFrame.m:
            (-[WebFrame _addExtraFieldsToRequest:mainResource:alwaysFromRequest:]): Add an Accept header
            to main resource requests.
            * English.lproj/StringsNotToBeLocalized.txt: Added new strings.
    
    
    
    Canonical link: https://commits.webkit.org/12830@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@15246 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebView/WebFrame.m
+++ WebKit/WebView/WebFrame.m
@@ -2775,6 +2775,9 @@ static CFAbsoluteTime _timeOfLastCompletedLoad;
         else
             [request setMainDocumentURL:[[[[self webView] mainFrame] dataSource] _URL]];
     }
+    
+    if (mainResource)
+        [request setValue:@"text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5" forHTTPHeaderField:@"Accept"];
 }
 
 - (BOOL)_isMainFrame
