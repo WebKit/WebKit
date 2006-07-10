825f9a9bfdafb796000cfd1d4b04522a0412be39
Timothy Hatcher <timothy@apple.com>
2006-07-10 04:05:58 +0000

    Build fix for DumpRenderTree and other clients that don't use AppKit. Update the copyright date on headers I changed earlier.
    Canonical link: https://commits.webkit.org/12865@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@15281 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/bindings/objc/DOMExtensions.h
+++ WebCore/bindings/objc/DOMExtensions.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,7 +27,8 @@
 #import <WebCore/DOMHTML.h>
 #import <WebCore/DOMRange.h>
 
-@class DOMCSSStyleDeclaration;
+@class NSImage;
+@class NSColor;
 
 @interface DOMImplementation (DOMImplementationExtensions)
 - (DOMHTMLDocument *)createHTMLDocument:(NSString *)title;
