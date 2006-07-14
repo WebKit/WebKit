6efc831c432fc7fa8d43c0f8db91a515c531b9b1
Geoffrey Garen <ggaren@apple.com>
2006-07-14 08:07:05 +0000

    Reviewed by Adele.
            - Build fix: don't need BLOCK_OBJC_EXCEPTIONS because we already have
            one surrounding this function, and the nested one makes the compiler
            think our local variable is volatile (seems like a compiler bug to me).
    
            * bridge/mac/FrameMac.mm:
            (WebCore::FrameMac::sendContextMenuEvent):
    
    
    
    Canonical link: https://commits.webkit.org/12976@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@15423 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/bridge/mac/FrameMac.mm
+++ WebCore/bridge/mac/FrameMac.mm
@@ -2174,11 +2174,8 @@ bool FrameMac::sendContextMenuEvent(NSEvent *event)
         // Since we're about to make a new selection in this view,
         // make sure this view is first responder
         NSView *view = d->m_view->getDocumentView();
-        BEGIN_BLOCK_OBJC_EXCEPTIONS;
-        if ([_bridge firstResponder] != view) {
+        if ([_bridge firstResponder] != view)
             [_bridge makeFirstResponder:view];
-        }
-        END_BLOCK_OBJC_EXCEPTIONS;
 
         _mouseDownMayStartSelect = true; // context menu events are always allowed to perform a selection
         selectClosestWordFromMouseEvent(mouseEvent, mev.targetNode());
