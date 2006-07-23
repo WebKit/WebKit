87c67ac05473a6390cd72fd55c1777fe7969776b
Timothy Hatcher <timothy@apple.com>
2006-07-23 01:37:40 +0000

    Rolling out r15572.
            Bug 10062: REGRESSION: dom/xhtml/level2/html/HTMLIFrameElement11.xhtml asserts/crashes
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10062
    
        2006-07-21  Timothy Hatcher  <timothy@apple.com>
    
            Reviewed by Maciej.
    
            <rdar://problem/4609195> Help Viewer loads empty window
            (not getting didFailLoadingWithError: callback)
    
            Call super's didFailWithError before _receivedMainResourceError
            because _receivedMainResourceError will cause the datasource's
            frame to be set to nil before the didFailLoadingWithError delegate
            callback is sent. (This order is needed now that WebDataSource does
            not hold on to the WebView; it uses the WebFrame to get to the WebView.
            If the WebFrame is nil we can't get to the WebView's resource load delegate.)
    
            * WebView/WebMainResourceLoader.m:
            (-[WebMainResourceLoader receivedError:]):
    
    
    
    Canonical link: https://commits.webkit.org/13105@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@15580 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebView/WebMainResourceLoader.m
+++ WebKit/WebView/WebMainResourceLoader.m
@@ -84,9 +84,9 @@
 {
     // Calling _receivedMainResourceError will likely result in a call to release, so we must retain.
     [self retain];
-    WebDataSource *ds = [dataSource retain]; // super's didFailWithError will release the datasource
+    WebDataSource *ds = [dataSource retain];
+    [dataSource _receivedMainResourceError:error complete:YES];
     [super didFailWithError:error];
-    [ds _receivedMainResourceError:error complete:YES];
     [ds release];
     [self release];
 }
