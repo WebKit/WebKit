ab7d264617d2ff709311d21b7fef8c2b01ff4d13
Adam Roben <aroben@webkit.org>
2006-08-02 20:26:11 +0000

    Reviewed by Darin.
            - Fixed build
    
            * kjs/date_object.cpp:
            (KJS::formatTime):
    
    
    
    Canonical link: https://commits.webkit.org/13224@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@15757 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/date_object.cpp
+++ JavaScriptCore/kjs/date_object.cpp
@@ -207,10 +207,16 @@ static UString formatTime(const tm &t, bool utc)
         snprintf(buffer, sizeof(buffer), "%02d:%02d:%02d GMT", t.tm_hour, t.tm_min, t.tm_sec);
     } else {
         int offset = abs(gmtoffset(t));
-        if (t.tm_zone) {
+#if PLATFORM(WIN_OS)
+        char tzname[70];
+        strftime(tzname, sizeof(tzname), "%Z", &t);
+#else
+        const char *tzname = t.tm_zone;
+#endif
+        if (tzname) {
             snprintf(buffer, sizeof(buffer), "%02d:%02d:%02d GMT%c%02d%02d (%s)",
                 t.tm_hour, t.tm_min, t.tm_sec,
-                gmtoffset(t) < 0 ? '-' : '+', offset / (60*60), (offset / 60) % 60, t.tm_zone);
+                gmtoffset(t) < 0 ? '-' : '+', offset / (60*60), (offset / 60) % 60, tzname);
         } else {
             snprintf(buffer, sizeof(buffer), "%02d:%02d:%02d GMT%c%02d%02d",
                 t.tm_hour, t.tm_min, t.tm_sec,
