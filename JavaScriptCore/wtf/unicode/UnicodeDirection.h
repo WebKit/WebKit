49d771c34d977a8507140340ffc1eea39a2356c1
Rob Buis <rbuis@igalia.com>
2006-08-18 19:33:48 +0000

    I hope I got this finally right, learning all the time :|
    Canonical link: https://commits.webkit.org/13349@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@15940 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/unicode/UnicodeDirection.h
+++ JavaScriptCore/wtf/unicode/UnicodeDirection.h
@@ -20,54 +20,6 @@
  *
  */
 
-#include "UnicodeQt4.h"
-
-using namespace WTF::Unicode;
-
-Direction WTF::Unicode::direction(int c)
-{
-  // FIXME: implement support for non-BMP code points
-  if ((c & 0xffff0000) != 0)
-    return LeftToRight;
-
-  switch (QChar((unsigned short)c).direction()) {
-    case QChar::DirL:
-      return LeftToRight;
-    case QChar::DirR:
-      return RightToLeft;
-    case QChar::DirNSM:
-      return NonSpacingMark;
-    case QChar::DirBN:
-      return BoundaryNeutral;
-    case QChar::DirEN:
-      return EuropeanNumber;
-    case QChar::DirES:
-      return EuropeanNumberSeparator;
-    case QChar::DirET:
-      return EuropeanNumberTerminator;
-    case QChar::DirAN:
-      return ArabicNumber;
-    case QChar::DirCS:
- *  This file is part of the KDE libraries
- *  Copyright (C) 2006 George Staikos <staikos@kde.org>
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Library General Public
- *  License as published by the Free Software Foundation; either
- *  version 2 of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Library General Public License for more details.
- *
- *  You should have received a copy of the GNU Library General Public License
- *  along with this library; see the file COPYING.LIB.  If not, write to
- *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *  Boston, MA 02110-1301, USA.
- *
- */
-
 #ifndef KJS_UNICODE_DIRECTION_H
 #define KJS_UNICODE_DIRECTION_H
 
