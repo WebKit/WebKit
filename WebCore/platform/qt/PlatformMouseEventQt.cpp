7225fc2e04a5a8dcc855a18be8daefea23af5c38
Rob Buis <rbuis@igalia.com>
2006-08-18 23:26:51 +0000

    Reviewed by Eric. Landed by rwlbuis.
            Next chunk of the implementation for:
            http://bugzilla.opendarwin.org/show_bug.cgi?id=10467
    
            WebKit should have Qt platform support
    
    
    
    Canonical link: https://commits.webkit.org/13353@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@15944 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/platform/qt/PlatformMouseEventQt.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2006 Zack Rusin <zack@kde.org>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "PlatformMouseEvent.h"
+
+#include <QMouseEvent>
+
+namespace WebCore {
+
+const PlatformMouseEvent::CurrentEventTag PlatformMouseEvent::currentEvent = { };
+
+PlatformMouseEvent::PlatformMouseEvent(QMouseEvent* event, int clickCount)
+{
+    m_position = IntPoint(event->pos());
+    m_globalPosition = IntPoint(event->globalPos());
+
+    if (event->button() == Qt::LeftButton || (event->buttons() & Qt::LeftButton))
+        m_button = LeftButton;
+    else if (event->button() == Qt::RightButton || (event->buttons() & Qt::RightButton))
+        m_button = RightButton;
+    else if (event->button() == Qt::MidButton || (event->buttons() & Qt::MidButton))
+        m_button = MiddleButton;
+
+    m_clickCount = clickCount;
+    m_shiftKey =  (event->modifiers() & Qt::ShiftModifier) != 0;
+    m_ctrlKey = (event->modifiers() & Qt::ControlModifier) != 0;
+    m_altKey =  (event->modifiers() & Qt::AltModifier) != 0;
+    m_metaKey = (event->modifiers() & Qt::MetaModifier) != 0;    
+}
+
+}
+
+// vim: ts=4 sw=4 et
