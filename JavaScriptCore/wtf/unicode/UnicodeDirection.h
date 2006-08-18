b23d9c88b2335c6fca9b4c196647d15cf61ca273
Rob Buis <rbuis@igalia.com>
2006-08-18 19:26:47 +0000

    Accidently doubly applied patch for: Fixes: http://bugzilla.opendarwin.org/show_bug.cgi?id=10463 Sorry for any inconvenience!
    Canonical link: https://commits.webkit.org/13348@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@15939 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/unicode/UnicodeDirection.h
+++ JavaScriptCore/wtf/unicode/UnicodeDirection.h
@@ -20,39 +20,34 @@
  *
  */
 
-#ifndef KJS_UNICODE_DIRECTION_H
-#define KJS_UNICODE_DIRECTION_H
+#include "UnicodeQt4.h"
 
-namespace WTF {
-  namespace Unicode {
-    enum Direction {
-      LeftToRight = 0,
-      RightToLeft = 0x00000001,
-      EuropeanNumber = 0x00000002,
-      EuropeanNumberSeparator = 0x00000004,
-      EuropeanNumberTerminator = 0x00000008,
-      ArabicNumber = 0x00000010,
-      CommonNumberSeparator = 0x00000020,
-      BlockSeparator = 0x00000040,
-      SegmentSeparator = 0x00000080,
-      WhiteSpaceNeutral = 0x00000100,
-      OtherNeutral = 0x00000200,
-      LeftToRightEmbedding = 0x00000400,
-      LeftToRightOverride = 0x00000800,
-      RightToLeftArabic = 0x00001000,
-      RightToLeftEmbedding = 0x00002000,
-      RightToLeftOverride = 0x00004000,
-      PopDirectionalFormat = 0x00008000,
-      NonSpacingMark = 0x00010000,
-      BoundaryNeutral = 0x00020000
-    };
-  }
-}
+using namespace WTF::Unicode;
 
-#endif
-// vim: ts=2 sw=2 et
-// -*- c-basic-offset: 2 -*-
-/*
+Direction WTF::Unicode::direction(int c)
+{
+  // FIXME: implement support for non-BMP code points
+  if ((c & 0xffff0000) != 0)
+    return LeftToRight;
+
+  switch (QChar((unsigned short)c).direction()) {
+    case QChar::DirL:
+      return LeftToRight;
+    case QChar::DirR:
+      return RightToLeft;
+    case QChar::DirNSM:
+      return NonSpacingMark;
+    case QChar::DirBN:
+      return BoundaryNeutral;
+    case QChar::DirEN:
+      return EuropeanNumber;
+    case QChar::DirES:
+      return EuropeanNumberSeparator;
+    case QChar::DirET:
+      return EuropeanNumberTerminator;
+    case QChar::DirAN:
+      return ArabicNumber;
+    case QChar::DirCS:
  *  This file is part of the KDE libraries
  *  Copyright (C) 2006 George Staikos <staikos@kde.org>
  *
