79466249f4e7e977cfe749d8139d7353174f2c86
Timothy Hatcher <timothy@apple.com>
2006-09-13 16:16:51 +0000

    Forgot these added files.
    Canonical link: https://commits.webkit.org/13708@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@16344 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/platform/qt/FileChooserQt.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2006 Nikolas Zimmermann <zimmermann@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "config.h"
+#include "FileChooser.h"
+
+#define notImplemented() do { fprintf(stderr, "FIXME: UNIMPLEMENTED: %s:%d\n", __FILE__, __LINE__); } while(0)
+
+namespace WebCore {
+
+FileChooser::FileChooser(Document*, RenderFileUploadControl*)
+{
+    notImplemented();
+}
+
+FileChooser::~FileChooser()
+{
+    notImplemented();
+}
+
+void FileChooser::openFileChooser()
+{
+    notImplemented();
+}
+
+String FileChooser::basenameForWidth(int width) const
+{
+    notImplemented();
+    return String();
+}
+
+void FileChooser::uploadControlDetaching()
+{
+    notImplemented();
+}
+
+void FileChooser::chooseFile(const String& filename)
+{
+    notImplemented();
+}
+
+}
+
+// vim: ts=4 sw=4 et
---
+++ WebCore/platform/qt/IconQt.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2006 Nikolas Zimmermann <zimmermann@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "config.h"
+
+#include <stdio.h>
+#include "Icon.h"
+
+#define notImplemented() do { fprintf(stderr, "FIXME: UNIMPLEMENTED: %s:%d\n", __FILE__, __LINE__); } while(0)
+
+namespace WebCore {
+
+Icon::Icon()
+{
+    notImplemented();
+}
+
+Icon::~Icon()
+{
+    notImplemented();
+}
+    
+PassRefPtr<Icon> Icon::newIconForFile(const String& filename)
+{
+    notImplemented();
+    return PassRefPtr<Icon>(0);
+}
+
+void Icon::paint(GraphicsContext*, const IntRect&)
+{
+    notImplemented();
+}
+
+}
+
+// vim: ts=4 sw=4 et
