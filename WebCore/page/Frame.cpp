97853fe1a83088f69f23a5e091b2813cb5effea9
Rob Buis <rbuis@igalia.com>
2006-09-21 21:18:32 +0000

    Too tired too do it right :}
    Canonical link: https://commits.webkit.org/13857@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@16508 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/page/Frame.cpp
+++ WebCore/page/Frame.cpp
@@ -767,7 +767,7 @@ void Frame::endIfNotLoading()
         
     
     if (IconDatabase* sharedIconDatabase = IconDatabase::sharedIconDatabase()) {
-        if (!sharedIconDatabase->enabled() || !sharedIconDatabase->enabled())
+        if (!sharedIconDatabase || !sharedIconDatabase->enabled())
             return;
         
         String url(iconURL().url());
