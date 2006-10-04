e6b61fe73b925370ae5069a861b9871bcd2dc44c
David Hyatt <hyatt@apple.com>
2006-10-04 04:45:28 +0000

    Fix mac build bustage.
    Canonical link: https://commits.webkit.org/14071@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@16766 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/Widget.cpp
+++ WebCore/platform/Widget.cpp
@@ -100,11 +100,4 @@ bool Widget::isFrameView() const
     return false;
 }
 
-IntRect Widget::convertToContainingWindow(const IntRect& rect) const
-{
-    IntRect convertedRect = rect;
-    convertedRect.setLocation(convertToContainingWindow(convertedRect.location()));
-    return convertedRect;
-}
-
 }
