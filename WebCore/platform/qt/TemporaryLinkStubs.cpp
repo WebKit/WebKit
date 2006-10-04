a69f8fc4d643577b307c76b9faf5a3e728065d37
David Hyatt <hyatt@apple.com>
2006-10-04 08:10:55 +0000

    Fix Qt bustage.
    Canonical link: https://commits.webkit.org/14077@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@16772 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/qt/TemporaryLinkStubs.cpp
+++ WebCore/platform/qt/TemporaryLinkStubs.cpp
@@ -179,7 +179,7 @@ bool FrameView::passMousePressEventToSubframe(MouseEventWithHitTestResults& mev,
 bool FrameView::passMouseMoveEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe) { return true; }
 bool FrameView::passMouseReleaseEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe) { return true; }
 bool FrameView::passWheelEventToSubframe(PlatformWheelEvent& e, Frame* subframe) { return false; }
-bool FrameView::passMousePressEventToScrollbar(MouseEventWithHitTestResults& mev) { return false; }
+bool FrameView::passMousePressEventToScrollbar(MouseEventWithHitTestResults& mev, PlatformScrollBar*) { return false; }
 
 bool AXObjectCache::gAccessibilityEnabled = false;
 
