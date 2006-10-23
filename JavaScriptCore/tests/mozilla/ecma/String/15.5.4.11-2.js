dc796d3535855c8de1122a8ad48d53fab651d951
kmccullo <kmccullo>
2006-10-23 19:03:23 +0000

    Reviewed by Geof.
            - Sets the lowercase range correctly in the test and consolidates a variable to make the test more readable.
    
           * tests/mozilla/ecma/String/15.5.4.11-2.js:
    
    
    
    Canonical link: https://commits.webkit.org/14485@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@17218 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/tests/mozilla/ecma/String/15.5.4.11-2.js
+++ JavaScriptCore/tests/mozilla/ecma/String/15.5.4.11-2.js
@@ -55,6 +55,18 @@
    Author:             christine@netscape.com
    Date:               12 november 1997
 */
+/*
+    Safari Changes:  This test differs from the mozilla tests in two significant
+    ways.
+        First, the lower case range for Georgian letters in this file is the
+    correct range according to the Unicode 5.0 standard, as opposed to the 
+    Georgian caseless range that mozilla uses.
+        Secondly this test uses an array for expected results with two entries,
+    instead of a single expected result. This allows us to accept Unicode 4.0 or
+    Unicode 5.0 results as correct, as opposed to the mozilla test, which assumes
+    Unicode 5.0.  This allows Safari to pass this test on OS' with different
+    Unicode standards implemented (e.g. Tiger and XP)
+*/
     var SECTION = "15.5.4.11-2";
     var VERSION = "ECMA_1";
     startTest();
@@ -72,8 +84,7 @@ function getTestCases() {
     // Georgian
     // Range: U+10A0 to U+10FF
     for ( var i = 0x10A0; i <= 0x10FF; i++ ) {
-        var U4 = new Unicode4( i );
-        var U5 = new Unicode5( i );
+        var U = new Array(new Unicode( i, 4 ), new Unicode( i, 5 ));
 
 /*
         array[item++] = new TestCase(   SECTION,
@@ -83,8 +94,7 @@ function getTestCases() {
 */
         array[item++] = new TestCaseDualExpected(   SECTION,
                                         "var s = new String( String.fromCharCode("+i+") ); s.toLowerCase().charCodeAt(0)",
-                                        U4.lower,
-                                        U5.lower,
+                                        U,
                                         eval("var s = new String( String.fromCharCode(i) ); s.toLowerCase().charCodeAt(0)") );
     }
 
@@ -96,26 +106,25 @@ function getTestCases() {
  *
  */
 
-function TestCaseDualExpected( n, d, e1, e2, a ) {
+function TestCaseDualExpected( n, d, e, a ) {
     this.name        = n;
     this.description = d;
-    this.expect1     = e1;
-    this.expect2     = e2;
-    this.actual  = a;
+    this.expect      = e;
+    this.actual      = a;
     this.passed      = true;
     this.reason      = "";
-    this.bugnumber    = BUGNUMBER;
+    this.bugnumber   = BUGNUMBER;
 
-    this.passed = getTestCaseResultDualExpected( this.expect1, this.expect2, this.actual );
+    this.passed = getTestCaseResultDualExpected( this.expect, this.actual );
     if ( DEBUG ) {
         writeLineToLog( "added " + this.description );
     }
 }
 
 // Added so that either Unicode 4.0 or 5.0 results will be considered correct.
-function writeTestCaseResultDualExpected( expect1, expect2, actual, string ) {
-        var passed = getTestCaseResultDualExpected(  expect1, expect2, actual );
-        writeFormattedResult( expect2, actual, string, passed );
+function writeTestCaseResultDualExpected( expect, actual, string ) {
+        var passed = getTestCaseResultDualExpected( expect, actual );
+        writeFormattedResult( expect[1].lower, actual, string, passed );
         return passed;
 }
 /*
@@ -123,7 +132,9 @@ function writeTestCaseResultDualExpected( expect1, expect2, actual, string ) {
  * Compare expected result to the actual result and figure out whether
  * the test case passed.
  */
-function getTestCaseResultDualExpected( expect1, expect2, actual ) {
+function getTestCaseResultDualExpected( expect, actual ) {
+    expectedU4 = expect[0].lower;
+    expectedU5 = expect[1].lower;
     //  because ( NaN == NaN ) always returns false, need to do
     //  a special compare to see if we got the right result.
         if ( actual != actual ) {
@@ -134,36 +145,36 @@ function getTestCaseResultDualExpected( expect1, expect2, actual ) {
             }
         }
 
-        if ( expect1 != expect1 )   {
-            if ( typeof expect1 == "object" ) {
-                expect1 = "NaN object";
+        if ( expectedU4 != expectedU4 )   {
+            if ( typeof expectedU4 == "object" ) {
+                expectedU4 = "NaN object";
             } else {
-                expect1 = "NaN number";
+                expectedU4 = "NaN number";
             }
         }
-        if ( expect2 != expect2 )   {
-            if ( typeof expect2 == "object" ) {
-                expect2 = "NaN object";
+        if ( expectedU5 != expectedU5 )   {
+            if ( typeof expectedU5 == "object" ) {
+                expectedU5 = "NaN object";
             } else {
-                expect2 = "NaN number";
+                expectedU5 = "NaN number";
             }
         }
 
-        var passed = ( expect1 == actual || expect2 == actual ) ? true : false;
+        var passed = ( expectedU4 == actual || expectedU5 == actual ) ? true : false;
         
         //  if both objects are numbers
         // need to replace w/ IEEE standard for rounding
         if ( !passed &&
              typeof(actual) == "number" &&
-            (typeof(expect1) == "number" ||
-             typeof(expect2) == "number")) {
-            if (( Math.abs(actual-expect1) < 0.0000001 ) || ( Math.abs(actual-expect2) < 0.0000001 )) {
+            (typeof(expectedU4) == "number" ||
+             typeof(expectedU5) == "number")) {
+            if (( Math.abs(actual-expectedU4) < 0.0000001 ) || ( Math.abs(actual-expectedU5) < 0.0000001 )) {
                 passed = true;
             }
         }
 
         //  verify type is the same
-        if ( typeof(expect1) != typeof(actual) && typeof(expect2) != typeof(actual) )   {
+        if ( typeof(expectedU4) != typeof(actual) && typeof(expectedU5) != typeof(actual) )   {
             passed = false;
         }
 
@@ -173,8 +184,7 @@ function getTestCaseResultDualExpected( expect1, expect2, actual ) {
 function test() {
     for ( tc=0; tc < testcases.length; tc++ ) {
         testcases[tc].passed = writeTestCaseResultDualExpected(
-                            testcases[tc].expect1,
-                            testcases[tc].expect2,
+                            testcases[tc].expect,
                             testcases[tc].actual,
                             testcases[tc].description +" = "+ testcases[tc].actual );
 
@@ -190,21 +200,14 @@ function MyObject( value ) {
     this.toString = new Function ( "return this.value+''" );
 }
 
-function Unicode4( c ) {
-    u = GetUnicodeValues4( c );
+function Unicode( c, version ) {
+    u = GetUnicodeValues( c, version );
     this.upper = u[0];
     this.lower = u[1]
     return this;
 }
 
-function Unicode5( c ) {
-    u = GetUnicodeValues5( c );
-    this.upper = u[0];
-    this.lower = u[1]
-    return this;
-}
-
-function GetUnicodeValues4( c ) {
+function GetUnicodeValues( c, version ) {
     u = new Array();
 
     u[0] = c;
@@ -435,390 +438,17 @@ function GetUnicodeValues4( c ) {
 
     // Georgian
     // Range: U+10A0 to U+10F0
-
-    // Hangul Jamo
-    // Range: U+1100 to U+11FF
-
-    // Greek Extended
-    // Range: U+1F00 to U+1FFF
-    // skip for now
-
-
-    // General Punctuation
-    // Range: U+2000 to U+206F
-
-    // Superscripts and Subscripts
-    // Range: U+2070 to U+209F
-
-    // Currency Symbols
-    // Range: U+20A0 to U+20CF
-
-
-    // Combining Diacritical Marks for Symbols
-    // Range: U+20D0 to U+20FF
-    // skip for now
-
-
-    // Number Forms
-    // Range: U+2150 to U+218F
-    // skip for now
-
-
-    // Arrows
-    // Range: U+2190 to U+21FF
-
-    // Mathematical Operators
-    // Range: U+2200 to U+22FF
-
-    // Miscellaneous Technical
-    // Range: U+2300 to U+23FF
-
-    // Control Pictures
-    // Range: U+2400 to U+243F
-
-    // Optical Character Recognition
-    // Range: U+2440 to U+245F
-
-    // Enclosed Alphanumerics
-    // Range: U+2460 to U+24FF
-
-    // Box Drawing
-    // Range: U+2500 to U+257F
-
-    // Block Elements
-    // Range: U+2580 to U+259F
-
-    // Geometric Shapes
-    // Range: U+25A0 to U+25FF
-
-    // Miscellaneous Symbols
-    // Range: U+2600 to U+26FF
-
-    // Dingbats
-    // Range: U+2700 to U+27BF
-
-    // CJK Symbols and Punctuation
-    // Range: U+3000 to U+303F
-
-    // Hiragana
-    // Range: U+3040 to U+309F
-
-    // Katakana
-    // Range: U+30A0 to U+30FF
-
-    // Bopomofo
-    // Range: U+3100 to U+312F
-
-    // Hangul Compatibility Jamo
-    // Range: U+3130 to U+318F
-
-    // Kanbun
-    // Range: U+3190 to U+319F
-
-
-    // Enclosed CJK Letters and Months
-    // Range: U+3200 to U+32FF
-
-    // CJK Compatibility
-    // Range: U+3300 to U+33FF
-
-    // Hangul Syllables
-    // Range: U+AC00 to U+D7A3
-
-    // High Surrogates
-    // Range: U+D800 to U+DB7F
-
-    // Private Use High Surrogates
-    // Range: U+DB80 to U+DBFF
-
-    // Low Surrogates
-    // Range: U+DC00 to U+DFFF
-
-    // Private Use Area
-    // Range: U+E000 to U+F8FF
-
-    // CJK Compatibility Ideographs
-    // Range: U+F900 to U+FAFF
-
-    // Alphabetic Presentation Forms
-    // Range: U+FB00 to U+FB4F
-
-    // Arabic Presentation Forms-A
-    // Range: U+FB50 to U+FDFF
-
-    // Combining Half Marks
-    // Range: U+FE20 to U+FE2F
-
-    // CJK Compatibility Forms
-    // Range: U+FE30 to U+FE4F
-
-    // Small Form Variants
-    // Range: U+FE50 to U+FE6F
-
-    // Arabic Presentation Forms-B
-    // Range: U+FE70 to U+FEFF
-
-    // Halfwidth and Fullwidth Forms
-    // Range: U+FF00 to U+FFEF
-
-    if ( c >= 0xFF21 && c <= 0xFF3A ) {
-        u[0] = c;
-        u[1] = c + 32;
-        return u;
-    }
-
-    if ( c >= 0xFF41 && c <= 0xFF5A ) {
-        u[0] = c - 32;
-        u[1] = c;
-        return u;
-    }
-
-    // Specials
-    // Range: U+FFF0 to U+FFFF
-
-    return u;
-}
-
-function GetUnicodeValues5( c ) {
-    u = new Array();
-
-    u[0] = c;
-    u[1] = c;
-
-    // upper case Basic Latin
-
-    if ( c >= 0x0041 && c <= 0x005A) {
-        u[0] = c;
-        u[1] = c + 32;
-        return u;
-    }
-
-    // lower case Basic Latin
-    if ( c >= 0x0061 && c <= 0x007a ) {
-        u[0] = c - 32;
-        u[1] = c;
-        return u;
-    }
-
-    // upper case Latin-1 Supplement
-    if ( c == 0x00B5 ) {
-        u[0] = c;
-        u[1] = 0x039C;
-        return u;
-    }
-    if ( (c >= 0x00C0 && c <= 0x00D6) || (c >= 0x00D8 && c<=0x00DE) ) {
-        u[0] = c;
-        u[1] = c + 32;
-        return u;
-    }
-
-    // lower case Latin-1 Supplement
-    if ( (c >= 0x00E0 && c <= 0x00F6) || (c >= 0x00F8 && c <= 0x00FE) ) {
-        u[0] = c - 32;
-        u[1] = c;
-        return u;
-    }
-    if ( c == 0x00FF ) {
-        u[0] = 0x0178;
-        u[1] = c;
-        return u;
-    }
-    // Latin Extended A
-    if ( (c >= 0x0100 && c < 0x0138) || (c > 0x0149 && c < 0x0178) ) {
-        // special case for capital I
-        if ( c == 0x0130 ) {
+    if ( version == 5 ) {
+        if ( c >= 0x10A0 && c <= 0x10C5 ) {
             u[0] = c;
-            u[1] = 0x0069;
+            u[1] = c + 7264; //48;
             return u;
         }
-        if ( c == 0x0131 ) {
-            u[0] = 0x0049;
+        if ( c >= 0x2D00 && c <= 0x2D25 ) {
+            u[0] = c;
             u[1] = c;
             return u;
         }
-
-        if ( c % 2 == 0 ) {
-        // if it's even, it's a capital and the lower case is c +1
-            u[0] = c;
-            u[1] = c+1;
-        } else {
-        // if it's odd, it's a lower case and upper case is c-1
-            u[0] = c-1;
-            u[1] = c;
-        }
-        return u;
-    }
-    if ( c == 0x0178 ) {
-        u[0] = c;
-        u[1] = 0x00FF;
-        return u;
-    }
-
-    if ( (c >= 0x0139 && c < 0x0149) || (c > 0x0178 && c < 0x017F) ) {
-        if ( c % 2 == 1 ) {
-        // if it's odd, it's a capital and the lower case is c +1
-            u[0] = c;
-            u[1] = c+1;
-        } else {
-        // if it's even, it's a lower case and upper case is c-1
-            u[0] = c-1;
-            u[1] = c;
-        }
-        return u;
-    }
-    if ( c == 0x017F ) {
-        u[0] = 0x0053;
-        u[1] = c;
-    }
-
-    // Latin Extended B
-    // need to improve this set
-
-    if ( c >= 0x0200 && c <= 0x0217 ) {
-        if ( c % 2 == 0 ) {
-            u[0] = c;
-            u[1] = c+1;
-        } else {
-            u[0] = c-1;
-            u[1] = c;
-        }
-        return u;
-    }
-
-    // Latin Extended Additional
-    // Range: U+1E00 to U+1EFF
-    // http://www.unicode.org/Unicode.charts/glyphless/U1E00.html
-
-    // Spacing Modifier Leters
-    // Range: U+02B0 to U+02FF
-
-    // Combining Diacritical Marks
-    // Range: U+0300 to U+036F
-
-    // skip Greek for now
-    // Greek
-    // Range: U+0370 to U+03FF
-
-    // Cyrillic
-    // Range: U+0400 to U+04FF
-
-    if ( c >= 0x0400 && c <= 0x040F) {
-        u[0] = c;
-        u[1] = c + 80;
-        return u;
-    }
-
-
-    if ( c >= 0x0410  && c <= 0x042F ) {
-        u[0] = c;
-        u[1] = c + 32;
-        return u;
-    }
-
-    if ( c >= 0x0430 && c<= 0x044F ) {
-        u[0] = c - 32;
-        u[1] = c;
-        return u;
-
-    }
-    if ( c >= 0x0450 && c<= 0x045F ) {
-        u[0] = c -80;
-        u[1] = c;
-        return u;
-    }
-
-    if ( c >= 0x0460 && c <= 0x047F ) {
-        if ( c % 2 == 0 ) {
-            u[0] = c;
-            u[1] = c +1;
-        } else {
-            u[0] = c - 1;
-            u[1] = c;
-        }
-        return u;
-    }
-
-    // Armenian
-    // Range: U+0530 to U+058F
-    if ( c >= 0x0531 && c <= 0x0556 ) {
-        u[0] = c;
-        u[1] = c + 48;
-        return u;
-    }
-    if ( c >= 0x0561 && c < 0x0587 ) {
-        u[0] = c - 48;
-        u[1] = c;
-        return u;
-    }
-
-    // Hebrew
-    // Range: U+0590 to U+05FF
-
-
-    // Arabic
-    // Range: U+0600 to U+06FF
-
-    // Devanagari
-    // Range: U+0900 to U+097F
-
-
-    // Bengali
-    // Range: U+0980 to U+09FF
-
-
-    // Gurmukhi
-    // Range: U+0A00 to U+0A7F
-
-
-    // Gujarati
-    // Range: U+0A80 to U+0AFF
-
-
-    // Oriya
-    // Range: U+0B00 to U+0B7F
-    // no capital / lower case
-
-
-    // Tamil
-    // Range: U+0B80 to U+0BFF
-    // no capital / lower case
-
-
-    // Telugu
-    // Range: U+0C00 to U+0C7F
-    // no capital / lower case
-
-
-    // Kannada
-    // Range: U+0C80 to U+0CFF
-    // no capital / lower case
-
-
-    // Malayalam
-    // Range: U+0D00 to U+0D7F
-
-    // Thai
-    // Range: U+0E00 to U+0E7F
-
-
-    // Lao
-    // Range: U+0E80 to U+0EFF
-
-
-    // Tibetan
-    // Range: U+0F00 to U+0FBF
-
-    // Georgian
-    // Range: U+10A0 to U+10F0
-    if ( c >= 0x10A0 && c <= 0x10C5 ) {
-        u[0] = c;
-        u[1] = c + 48;
-        return u;
-    }
-    if ( c >= 0x10D0 && c <= 0x10F5 ) {
-        u[0] = c;
-        u[1] = c;
-        return u;
     }
 
     // Hangul Jamo
