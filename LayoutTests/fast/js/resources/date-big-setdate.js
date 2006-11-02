a47d5c1463b8b331d0d0a3d49fe64f16e5ec05eb
kmccullo <kmccullo>
2006-11-02 17:44:13 +0000

    Reviewed by Maciej.
            - Changed the test to take into account the JavaScript standard's interesting take on DST.  According to the standard DST should not reflect historically accurate information but should rather interpolate from a comparable year.  This test used to show historically accurate information but now reflects the results of interpolating.
    
            * fast/js/date-big-setdate-expected.txt:
            * fast/js/resources/date-big-setdate.js:
    
    
    
    Canonical link: https://commits.webkit.org/14746@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@17538 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/js/resources/date-big-setdate.js
+++ LayoutTests/fast/js/resources/date-big-setdate.js
@@ -7,8 +7,9 @@ var validVars = false;
 var curValue;
 var success = true;
 var millisecondsPerDay = 1000 * 60 * 60 * 24;
-	
-for (var i = 120; i < 130; i++) {
+var millisecondsPerHour = 1000 * 60 * 60;
+
+for (var i = 116; i < 126; i++) {
     var d = new Date(0);
     d.setDate(i);
     if (validVars)
@@ -17,5 +18,16 @@ for (var i = 120; i < 130; i++) {
     curValue = d.valueOf();        
     validVars = true;
 }
+
+// Added a special case that should represent a change in DST.  DST did not actually
+// change on this date but because of the wierdness of how JavaScriptDates are
+// expected to interpolate DST as opposed to reflect acurate history, this day
+// (April 5th 1970) should show a DST change.
+
+var c = new Date(0);
+var d = new Date(0);
+c.setDate(125);
+d.setDate(126);
+shouldBe("d.valueOf() - c.valueOf()", "millisecondsPerDay - millisecondsPerHour");
 
 var successfullyParsed = true;
