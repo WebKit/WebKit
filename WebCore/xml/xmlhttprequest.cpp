dbc909d7544e3c0410411cfd34e509cb1f4d58d0
beidson <beidson>
2006-11-07 03:25:16 +0000

    Reviewed by Sarge
            <rdar://problem/4822911> - ASSERTION was a little overzealous.
            Toned it down a bit.
    
            * xml/xmlhttprequest.cpp:
            (WebCore::XMLHttpRequest::didFinishLoading):  Toned down assertion
    
    
    
    Canonical link: https://commits.webkit.org/14832@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@17637 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/xml/xmlhttprequest.cpp
+++ WebCore/xml/xmlhttprequest.cpp
@@ -485,7 +485,7 @@ void XMLHttpRequest::processSyncLoadResults(const Vector<char>& data, const Reso
 
 void XMLHttpRequest::didFinishLoading(ResourceHandle* loader)
 {
-    ASSERT(loader == m_loader);
+    ASSERT(m_aborted || loader == m_loader);
 
     if (m_state < Sent)
         changeState(Sent);
