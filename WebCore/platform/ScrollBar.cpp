50593d61f6f579946ab70be897c718e301060900
Mark Rowe <mrowe@bdash.net.nz>
2006-12-08 00:10:35 +0000

    2006-12-07  Don Gibson  <dgibson77@gmail.com>
            Reviewed by Darin.
    
            http://bugs.webkit.org/show_bug.cgi?id=11749
            Don't call updateThumbProportion() unnecessarily.
    
            * platform/ScrollBar.cpp:
            (WebCore::Scrollbar::setProportion):
    
    
    Canonical link: https://commits.webkit.org/15156@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@18066 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/ScrollBar.cpp
+++ WebCore/platform/ScrollBar.cpp
@@ -61,6 +61,9 @@ bool Scrollbar::setValue(int v)
 
 void Scrollbar::setProportion(int visibleSize, int totalSize)
 {
+    if (visibleSize == m_visibleSize && totalSize == m_totalSize)
+        return;
+
     m_visibleSize = visibleSize;
     m_totalSize = totalSize;
 
