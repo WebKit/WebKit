7a63b74fac75c26051ab926abf42984213478d73
staikos <staikos>
2006-12-11 01:23:48 +0000

    Add a default useragent for now.
    Canonical link: https://commits.webkit.org/15236@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@18148 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitQt/WebCoreSupport/FrameLoaderClientQt.cpp
+++ WebKitQt/WebCoreSupport/FrameLoaderClientQt.cpp
@@ -495,8 +495,7 @@ void FrameLoaderClientQt::setTitle(const String& title, const KURL&)
 
 String FrameLoaderClientQt::userAgent()
 {
-    notImplemented();
-    return String();
+    return "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en) AppleWebKit/418.9.1 (KHTML, like Gecko) Safari/419.3 Qt";
 }
 
 void FrameLoaderClientQt::dispatchDidReceiveIcon()
