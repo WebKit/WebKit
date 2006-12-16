580f5a4c4af59978a534167ab72a0849730aa442
Adele Peterson <adele@apple.com>
2006-12-16 10:12:23 +0000

    Reviewed by Adam.
            * platform/PopupMenuClient.h: Added. Broken out of PopupMenu class.
            (WebCore::PopupMenuClient::~PopupMenuClient):
    
    
    
    Canonical link: https://commits.webkit.org/15322@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@18250 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/platform/PopupMenuClient.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2006 Apple Computer, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef POPUPMENUCLIENT_H
+#define POPUPMENUCLIENT_H
+
+namespace WebCore {
+
+class Document;
+class String;
+class RenderStyle;
+
+class PopupMenuClient {
+public:
+    virtual ~PopupMenuClient() {}
+    virtual void valueChanged(unsigned listIndex, bool fireEvents = true) = 0;
+
+    virtual String itemText(unsigned listIndex) const = 0;
+    virtual bool itemIsEnabled(unsigned listIndex) const = 0;
+    virtual RenderStyle* itemStyle(unsigned listIndex) const = 0;
+    virtual RenderStyle* clientStyle() const = 0;
+    virtual Document* clientDocument() const = 0;
+    virtual int clientPaddingLeft() const = 0;
+    virtual int clientPaddingRight() const = 0;
+    virtual unsigned listSize() const = 0;
+    virtual int selectedIndex() const = 0;
+    virtual void hidePopup() = 0;
+    virtual bool itemIsSeparator(unsigned listIndex) const = 0;
+    virtual bool itemIsLabel(unsigned listIndex) const = 0;
+    virtual bool itemIsSelected(unsigned listIndex) const = 0;
+    virtual bool shouldPopOver() const = 0;
+    virtual bool valueShouldChangeOnHotTrack() const = 0;
+    virtual void setTextFromItem(unsigned listIndex) = 0;
+};
+
+}
+
+#endif
