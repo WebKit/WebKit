dca304b55867bf1127720cf25d0af280f6f616f1
Sam Weinig <sam@webkit.org>
2007-01-02 18:04:35 +0000

    LayoutTests:
            Reviewed by Mitz.
    
            Testcase for http://bugs.webkit.org/show_bug.cgi?id=12071
    
            * fast/forms/button-align-expected.checksum: Added.
            * fast/forms/button-align-expected.png: Added.
            * fast/forms/button-align-expected.txt: Added.
            * fast/forms/button-align.html: Added.
    
    WebCore:
    
            Reviewed by Mitz.
    
            Patch for http://bugs.webkit.org/show_bug.cgi?id=12071
            button with align attribute should not affect contained text
    
            Don't map align for button elements.
    
            Test: fast/forms/button-align.html
    
            * html/HTMLButtonElement.cpp:
            (WebCore::HTMLButtonElement::parseMappedAttribute):
    
    
    
    Canonical link: https://commits.webkit.org/15572@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@18528 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/html/HTMLButtonElement.cpp
+++ WebCore/html/HTMLButtonElement.cpp
@@ -6,6 +6,7 @@
  *           (C) 2001 Dirk Mueller (mueller@kde.org)
  * Copyright (C) 2004, 2005, 2006 Apple Computer, Inc.
  *           (C) 2006 Alexey Proskuryakov (ap@nypop.com)
+ * Copyright (C) 2007 Samuel Weinig (sam@webkit.org)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -75,6 +76,9 @@ void HTMLButtonElement::parseMappedAttribute(MappedAttribute *attr)
         m_currValue = m_value;
     } else if (attr->name() == accesskeyAttr) {
         // Do nothing.
+    } else if (attr->name() == alignAttr) {
+        // Don't map 'align' attribute.  This matches what Firefox and IE do, but not Opera.
+        // See http://bugs.webkit.org/show_bug.cgi?id=12071
     } else if (attr->name() == onfocusAttr) {
         setHTMLEventListener(focusEvent, attr);
     } else if (attr->name() == onblurAttr) {
