153b32ed0618d76ba737d41dbf2f3044b1965680
Beth Dakin <bdakin@apple.com>
2007-01-09 01:03:00 +0000

    Reviewed by Hyatt.
            Fix for a bug that Adam, Hyatt, and I noticed when debugging
            another problem.
    
            * rendering/InlineBox.cpp:
            (WebCore::InlineBox::root): Added assertion. If we are casting to a
            root, we should make sure we are root.
            * rendering/RenderFlow.cpp:
            (WebCore::RenderFlow::destroy): It is not enough to remove the
            child boxes for anonymous blocks. We need to be more thorough when
            cleaning these up. Adam's test case illustrates the problem well.
    
    
    Canonical link: https://commits.webkit.org/15709@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@18690 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderFlow.cpp
+++ WebCore/rendering/RenderFlow.cpp
@@ -201,11 +201,8 @@ void RenderFlow::destroy()
             // that will outlast this block. In the non-anonymous block case those
             // children will be destroyed by the time we return from this function.
             if (isAnonymousBlock()) {
-                for (InlineFlowBox* box = m_firstLineBox; box; box = box->nextFlowBox()) {
-                    while (InlineBox* childBox = box->firstChild()) {
-                        childBox->remove();
-                    }
-                }
+                for (InlineFlowBox* box = m_firstLineBox; box; box = box->nextFlowBox())
+                    box->deleteLine(renderArena());
             }
         } else if (isInline() && parent())
             parent()->dirtyLinesFromChangedChild(this);
