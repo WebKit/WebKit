3d5659d2abfae15b91be6c6f3e33f2a1bfeed84a
Justin Garcia <justin.garcia@apple.com>
2007-02-06 22:28:17 +0000

    Backing out some changes I accidently checked in.
    Canonical link: https://commits.webkit.org/16343@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@19442 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/editing/htmlediting.cpp
+++ WebCore/editing/htmlediting.cpp
@@ -304,7 +304,7 @@ Position rangeCompliantEquivalent(const Position& pos)
     Node *node = pos.node();
     
     if (pos.offset() <= 0) {
-        if (node->parentNode() && (node->hasTagName(brTag) || editingIgnoresContent(node)) || isTableElement(node))
+        if (node->parentNode() && (node->hasTagName(brTag) || editingIgnoresContent(node)))
             return positionBeforeNode(node);
         return Position(node, 0);
     }
@@ -327,9 +327,6 @@ Position rangeCompliantEquivalent(const Position& pos)
         ASSERT_NOT_REACHED();
         return node->parentNode() ? positionBeforeNode(node) : Position(node, 0);
     }
-    
-    if (pos.offset() == maxCompliantOffset && (editingIgnoresContent(node) || isTableElement(node)))
-        return positionAfterNode(node);
     
     return Position(pos);
 }
