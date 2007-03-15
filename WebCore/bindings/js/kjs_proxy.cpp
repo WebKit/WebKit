4dd182bbf98a55d58e17490e737a8a4c3ff33245
Geoffrey Garen <ggaren@apple.com>
2007-03-15 07:11:45 +0000

    Added an assert to help catch a bug. Hopefully someone will hit it!
            * bindings/js/kjs_proxy.cpp:
            (WebCore::KJSProxy::~KJSProxy):
    
    
    
    Canonical link: https://commits.webkit.org/17007@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@20211 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/bindings/js/kjs_proxy.cpp
+++ WebCore/bindings/js/kjs_proxy.cpp
@@ -46,6 +46,9 @@ KJSProxy::KJSProxy(Frame* frame)
 
 KJSProxy::~KJSProxy()
 {
+    // Check for <rdar://problem/4876466>. In theory, no JS should be executing
+    // in our interpreter. 
+    ASSERT(!m_script->context());
     m_script = 0;
     
     // It's likely that destroying the interpreter has created a lot of garbage.
