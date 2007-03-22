71c98df92775d9f60fb937b4caa8803783a7cad5
Darin Adler <darin@apple.com>
2007-03-22 18:22:15 +0000

    Reviewed by Brady.
            - use binary_search instead of hash table; slightly better performance and
              less memory use
    
            * platform/network/ResourceHandle.cpp: (WebCore::ResourceHandle::portAllowed):
            Use binary_search.
    
    
    
    Canonical link: https://commits.webkit.org/17171@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@20389 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/network/ResourceHandle.cpp
+++ WebCore/platform/network/ResourceHandle.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2004, 2006, 2007 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,17 +30,18 @@
 #include "Logging.h"
 #include "ResourceHandleClient.h"
 #include "Timer.h"
-
-#include <wtf/HashSet.h>
+#include <algorithm>
 
 namespace WebCore {
 
-ResourceHandle::ResourceHandle(const ResourceRequest& request, ResourceHandleClient* client, bool defersLoading, bool mightDownloadFromHandle)
+ResourceHandle::ResourceHandle(const ResourceRequest& request, ResourceHandleClient* client, bool defersLoading,
+        bool mightDownloadFromHandle)
     : d(new ResourceHandleInternal(this, request, client, defersLoading, mightDownloadFromHandle))
 {
 }
 
-PassRefPtr<ResourceHandle> ResourceHandle::create(const ResourceRequest& request, ResourceHandleClient* client, Frame* frame, bool defersLoading, bool mightDownloadFromHandle)
+PassRefPtr<ResourceHandle> ResourceHandle::create(const ResourceRequest& request, ResourceHandleClient* client,
+    Frame* frame, bool defersLoading, bool mightDownloadFromHandle)
 {
     RefPtr<ResourceHandle> newHandle(new ResourceHandle(request, client, defersLoading, mightDownloadFromHandle));
 
@@ -107,92 +108,80 @@ void ResourceHandle::clearAuthentication()
 
 bool ResourceHandle::portAllowed(const ResourceRequest& request)
 {
-    uint16_t port = request.url().port();
+    unsigned short port = request.url().port();
+
+    // Since most URLs don't have a port, return early for the "no port" case.
     if (!port)
         return true;
-        
-    // The blocked port list matches the port blocking mozilla implements
-    // See http://www.mozilla.org/projects/netlib/PortBanning.html for more information
-    static uint16_t blockedPortList[] = { 
-    1,    // tcpmux          
-    7,    // echo     
-    9,    // discard          
-    11,   // systat   
-    13,   // daytime          
-    15,   // netstat  
-    17,   // qotd             
-    19,   // chargen  
-    20,   // FTP-data 
-    21,   // FTP-control        
-    22,   // SSH              
-    23,   // telnet   
-    25,   // SMTP     
-    37,   // time     
-    42,   // name     
-    43,   // nicname  
-    53,   // domain  
-    77,   // priv-rjs 
-    79,   // finger   
-    87,   // ttylink  
-    95,   // supdup   
-    101,  // hostriame
-    102,  // iso-tsap 
-    103,  // gppitnp  
-    104,  // acr-nema 
-    109,  // POP2     
-    110,  // POP3     
-    111,  // sunrpc   
-    113,  // auth     
-    115,  // SFTP     
-    117,  // uucp-path
-    119,  // nntp     
-    123,  // NTP
-    135,  // loc-srv / epmap         
-    139,  // netbios
-    143,  // IMAP2  
-    179,  // BGP
-    389,  // LDAP
-    465,  // SMTP+SSL
-    512,  // print / exec          
-    513,  // login         
-    514,  // shell         
-    515,  // printer         
-    526,  // tempo         
-    530,  // courier        
-    531,  // Chat         
-    532,  // netnews        
-    540,  // UUCP       
-    556,  // remotefs    
-    563,  // NNTP+SSL
-    587,  // ESMTP
-    601,  // syslog-conn  
-    636,  // LDAP+SSL
-    993,  // IMAP+SSL
-    995,  // POP3+SSL
-    2049, // NFS
-    4045, // lockd
-    6000, // X11        
-    0 };   
-    
-    static HashSet<int>* blockedPortHash = 0;
-    if (!blockedPortHash) {
-        blockedPortHash = new HashSet<int>;
-        
-        for (int i = 0; blockedPortList[i]; ++i)
-            blockedPortHash->add(blockedPortList[i]);
-    }
-    
-    bool restricted = blockedPortHash->contains(port);
-    
-    if (restricted) {
-        // An exception in the mozilla port blocking is they allow 21 and 22 for FTP (and Secure FTP), which we have to do also
-        if ((port == 21 || port == 22) && request.url().url().startsWith("ftp:", false))
-            return true;
-    }
-        
-    return !restricted;
+
+    // This blocked port list matches the port blocking that Mozilla implements.
+    // See http://www.mozilla.org/projects/netlib/PortBanning.html for more information.
+    static const unsigned short blockedPortList[] = {
+        1,    // tcpmux
+        7,    // echo
+        9,    // discard
+        11,   // systat
+        13,   // daytime
+        15,   // netstat
+        17,   // qotd
+        19,   // chargen
+        20,   // FTP-data
+        21,   // FTP-control
+        22,   // SSH
+        23,   // telnet
+        25,   // SMTP
+        37,   // time
+        42,   // name
+        43,   // nicname
+        53,   // domain
+        77,   // priv-rjs
+        79,   // finger
+        87,   // ttylink
+        95,   // supdup
+        101,  // hostriame
+        102,  // iso-tsap
+        103,  // gppitnp
+        104,  // acr-nema
+        109,  // POP2
+        110,  // POP3
+        111,  // sunrpc
+        113,  // auth
+        115,  // SFTP
+        117,  // uucp-path
+        119,  // nntp
+        123,  // NTP
+        135,  // loc-srv / epmap
+        139,  // netbios
+        143,  // IMAP2
+        179,  // BGP
+        389,  // LDAP
+        465,  // SMTP+SSL
+        512,  // print / exec
+        513,  // login
+        514,  // shell
+        515,  // printer
+        526,  // tempo
+        530,  // courier
+        531,  // Chat
+        532,  // netnews
+        540,  // UUCP
+        556,  // remotefs
+        563,  // NNTP+SSL
+        587,  // ESMTP
+        601,  // syslog-conn
+        636,  // LDAP+SSL
+        993,  // IMAP+SSL
+        995,  // POP3+SSL
+        2049, // NFS
+        4045, // lockd
+        6000, // X11
+    };
+    const unsigned short* const blockedPortListEnd = blockedPortList
+        + sizeof(blockedPortList) / sizeof(blockedPortList[0]);
+
+    // Allow ports 21 and 22 for FTP URLs, as Mozilla does.
+    return !std::binary_search(blockedPortList, blockedPortListEnd, port)
+        || ((port == 21 || port == 22) && request.url().url().startsWith("ftp:", false));
 }
-
-
+  
 } // namespace WebCore
-
