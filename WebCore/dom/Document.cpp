e251534f86f0e1bc860c148ff095931d001e4494
Darin Adler <darin@apple.com>
2007-04-19 00:33:29 +0000

    Reviewed by Hyatt.
            - fix http://bugs.webkit.org/show_bug.cgi?id=13375
              REGRESSION (r20901): failing fast/overflow/scrollRevealButton.html
    
            * dom/Document.cpp: (WebCore::Document::updateLayout):
            If we have a parent frame, update its layout too.
    
    
    
    Canonical link: https://commits.webkit.org/17480@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@20936 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/dom/Document.cpp
+++ WebCore/dom/Document.cpp
@@ -1,11 +1,9 @@
-/**
- * This file is part of the DOM implementation for KDE.
- *
+/*
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2001 Dirk Mueller (mueller@kde.org)
  *           (C) 2006 Alexey Proskuryakov (ap@webkit.org)
- * Copyright (C) 2004, 2005, 2006 Apple Computer, Inc.
+ * Copyright (C) 2004, 2005, 2006, 2007 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -1058,6 +1056,9 @@ void Document::updateDocumentsRendering()
 
 void Document::updateLayout()
 {
+    if (Element* oe = ownerElement())
+        oe->document()->updateLayout();
+
     // FIXME: Dave Hyatt's pretty sure we can remove this because layout calls recalcStyle as needed.
     updateRendering();
 
