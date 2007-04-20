c919178838742700fa68ef339667b60897bcaf7e
Anders Carlsson <andersca@apple.com>
2007-04-20 07:13:24 +0000

    Reviewed by Maciej.
            <rdar://problem/5085897> REGRESSION: Some Flash links at www.jumpskyhigh.com just reload the page
    
            Get rid of the check that would prevent plugin requests from being loaded if a new page load was underway.
            www.jumpskyhigh.com had a flash movie that was embedded inside an <a> tag and clicking on the plug-in would cause the
            URL pointed to by the <a> tag to start loading and thus preventing the plug-in from loading the real URL.
    
            This check was added by Maciej and we should be able to remove it with the loader changes that have happened now,
            (mainly the fact that resource loaders are handled by the document loader instead of the frame loader).
    
            * Plugins/WebBaseNetscapePluginView.mm:
            (-[WebBaseNetscapePluginView loadRequest:inTarget:withNotifyData:sendNotification:]):
    
    
    
    Canonical link: https://commits.webkit.org/17505@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@20969 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/Plugins/WebBaseNetscapePluginView.mm
+++ WebKit/Plugins/WebBaseNetscapePluginView.mm
@@ -2148,11 +2148,6 @@ static OSStatus TSMEventHandler(EventHandlerCallRef inHandlerRef, EventRef inEve
     if (!URL) 
         return NPERR_INVALID_URL;
 
-    // don't let a plugin start any loads if it is no longer part of a document that is being
-    // displayed
-    if ([[self dataSource] _documentLoader] != [[self webFrame] _frameLoader]->activeDocumentLoader())
-        return NPERR_GENERIC_ERROR;
-    
     NSString *JSString = [URL _webkit_scriptIfJavaScriptURL];
     if (JSString != nil) {
         if (![[[self webView] preferences] isJavaScriptEnabled]) {
