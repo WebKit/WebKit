2039685050155e8ea744748c8f74ae02aad8cfc9
Mark Rowe <mrowe@bdash.net.nz>
2007-04-24 11:51:45 +0000

    2007-04-24  Benjamin Otte  <otte@gnome.org>
            Reviewed by Mark Rowe.
    
            Gdk build system improvements.
    
            * make-generated-sources.sh: Exit with failure status if make fails.
    
    
    Canonical link: https://commits.webkit.org/17589@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@21066 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/make-generated-sources.sh
+++ WebCore/make-generated-sources.sh
@@ -4,10 +4,9 @@ export SRCROOT=$PWD
 export WebCore=$PWD
 export CREATE_HASH_TABLE="$SRCROOT/../JavaScriptCore/kjs/create_hash_table"
 
-mkdir -p DerivedSources/WebCore
-cd DerivedSources/WebCore
-
 # note ENCODINGS_PREFIX needs to be a ws string so it does not turn into
 # a null value
-make -f ../../DerivedSources.make ENCODINGS_FILE=$1 ENCODINGS_PREFIX="\" \""
-cd ../..
+mkdir -p DerivedSources/WebCore &&
+cd DerivedSources/WebCore &&
+((make -f ../../DerivedSources.make ENCODINGS_FILE=$1 ENCODINGS_PREFIX="\" \"" && cd ../..) || 
+cd ../.. && false)
