6147a6eb4b5568e27b269f6edd1b2921ef7b47f8
Mark Rowe <mrowe@bdash.net.nz>
2007-04-25 13:57:42 +0000

    2007-04-26  Mark Rowe  <mrowe@apple.com>
            Gdk build fix courtesy of Alp Toker.
    
            * make-generated-sources.sh:  Remove conditional logic to ensure the
            script exits with the correct exit code.
    
    
    Canonical link: https://commits.webkit.org/17606@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@21084 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/make-generated-sources.sh
+++ WebCore/make-generated-sources.sh
@@ -7,6 +7,4 @@ export CREATE_HASH_TABLE="$SRCROOT/../JavaScriptCore/kjs/create_hash_table"
 # note ENCODINGS_PREFIX needs to be a ws string so it does not turn into
 # a null value
 mkdir -p DerivedSources/WebCore &&
-cd DerivedSources/WebCore &&
-((make -f ../../DerivedSources.make ENCODINGS_FILE=$1 ENCODINGS_PREFIX="\" \"" && cd ../..) || 
-cd ../.. && false)
+make -C DerivedSources/WebCore -f ../../DerivedSources.make ENCODINGS_FILE=$1 ENCODINGS_PREFIX="\" \""
