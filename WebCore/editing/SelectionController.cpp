0608ab2b217f905a58f46218d6b8503ce938c265
aliceli1 <aliceli1>
2007-04-26 23:21:45 +0000

    Reviewed by Oliver.
            * editing/SelectionController.cpp:
            (WebCore::SelectionController::selectAll):
            adding null check
    
    
    
    Canonical link: https://commits.webkit.org/17641@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@21124 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/editing/SelectionController.cpp
+++ WebCore/editing/SelectionController.cpp
@@ -1097,7 +1097,7 @@ void SelectionController::selectAll()
     }
     
     Node* root = isContentEditable() ? highestEditableRoot(m_sel.start()) : document->documentElement();
-    if (!root->renderer() || !root->renderer()->canSelect())
+    if (!root || !root->renderer() || !root->renderer()->canSelect())
         return;
     Selection newSelection(Selection::selectionFromContentsOfNode(root));
     if (m_frame->shouldChangeSelection(newSelection))
