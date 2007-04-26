d240b025177b4a1f5d970b80ed844a23179cf497
oliver <oliver>
2007-04-26 00:26:45 +0000

    2007-04-25  Oliver Hunt  <oliver@apple.com>
            Rubber stamped by Adele.
    
            Roll out WebKit changes from from r21052 to fix regression noted in
            <rdar://problem/5159556> REGRESSION: In Mail, pressing option-command- ' doesn't decrease block quote in selection
    
            * WebView/WebHTMLView.mm:
            (-[WebHTMLView _interceptEditingKeyEvent:shouldSaveCommand:]):
            (-[WebHTMLView doCommandBySelector:]):
    
    
    
    Canonical link: https://commits.webkit.org/17623@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@21102 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebView/WebHTMLView.mm
+++ WebKit/WebView/WebHTMLView.mm
@@ -5294,13 +5294,11 @@ static CGPoint coreGraphicsScreenPointForAppKitScreenPoint(NSPoint point)
         parameters.event = event;
         _private->interpretKeyEventsParameters = &parameters;
         KeypressCommand command = event->keypressCommand();
+        bool hasKeypressCommand = !command.commandNames.isEmpty() || !command.text.isEmpty();
 
-        if (parameters.shouldSaveCommand || command.isEmpty()) {
+        if (parameters.shouldSaveCommand || !hasKeypressCommand)
             [self interpretKeyEvents:[NSArray arrayWithObject:macEvent]];
-            // If we have no command after calling interpretKeyEvents, we assume the input method handled the key.
-            if (event->keypressCommand().isEmpty())
-                parameters.eventWasHandled = true;
-        } else {
+        else {
             if (!command.text.isEmpty())
                 [self insertText:command.text];
             else {
@@ -5524,6 +5522,9 @@ static CGPoint coreGraphicsScreenPointForAppKitScreenPoint(NSPoint point)
 
 - (void)doCommandBySelector:(SEL)selector
 {
+    if (selector == @selector(noop:))
+        return;
+
     // Use pointer to get parameters passed to us by the caller of interpretKeyEvents.
     // The same call to interpretKeyEvents can do more than one command.
     WebHTMLViewInterpretKeyEventsParameters* parameters = _private->interpretKeyEventsParameters;
@@ -5535,7 +5536,7 @@ static CGPoint coreGraphicsScreenPointForAppKitScreenPoint(NSPoint point)
         KeypressCommand command = event->keypressCommand();
         command.commandNames.append(NSStringFromSelector(selector));
         event->setKeypressCommand(command);
-    } else if (selector != @selector(noop:)) {
+    } else {
         // Make sure that only direct calls to doCommandBySelector: see the parameters by setting to 0.
         _private->interpretKeyEventsParameters = 0;
 
