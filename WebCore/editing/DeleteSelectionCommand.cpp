970cc452e2e8a94d3a27618055ee51b8b2e963e8
Justin Garcia <justin.garcia@apple.com>
2007-04-30 21:14:09 +0000

    LayoutTests:
            Reviewed by darin
    
            <rdar://problem/5168598> Crash when deleting a link inside an input field
    
            * editing/deleting/5168598-expected.checksum: Added.
            * editing/deleting/5168598-expected.png: Added.
            * editing/deleting/5168598-expected.txt: Added.
            * editing/deleting/5168598.html: Added.
    
    WebCore:
    
            Reviewed by darin
    
            <rdar://problem/5168598>
            Crash when deleting a link inside an input field
    
            Deletion code tries to update one of its positions to keep
            it valid and nullifies it.  Later in mergeParagraphs we try
            to find out if it's valid by checking to see if it's still in
            the document and crash.
    
            The fix is just to not bother keeping the position,
            m_upstreamStart, valid, since after the point in the code
            under examination, it doesn't need to be valid anymore.
            Besides, code to update DeleteSelectionCommand's positions
            should go in the functions that handle the content removal
            that might invalid them (there are already FIXMEs about this).
    
            Also, users shouldn't be able to insert links (or any rich content)
            into plaintext-only regions like text fields, even if those
            text fields are inside richly editable regions.  This is a
            separate bug and is filed as 5171552.
    
            * editing/DeleteSelectionCommand.cpp:
            (WebCore::DeleteSelectionCommand::handleGeneralDelete): Don't
            bother updating m_upstreamStart.
    
    
    
    Canonical link: https://commits.webkit.org/17704@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@21195 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/editing/DeleteSelectionCommand.cpp
+++ WebCore/editing/DeleteSelectionCommand.cpp
@@ -406,12 +406,7 @@ void DeleteSelectionCommand::handleGeneralDelete()
                 // need to delete whole node
                 // we can get here if this is the last node in the block
                 // remove an ancestor of m_downstreamEnd.node(), and thus m_downstreamEnd.node() itself
-                if (!m_upstreamStart.node()->inDocument() ||
-                    m_upstreamStart.node() == m_downstreamEnd.node() ||
-                    m_upstreamStart.node()->isDescendantOf(m_downstreamEnd.node())) {
-                    m_upstreamStart = Position(m_downstreamEnd.node()->parentNode(), m_downstreamEnd.node()->nodeIndex());
-                }
-                
+                // FIXME: Shouldn't remove m_downstreamEnd.node() if its offsets refer to children.
                 removeNode(m_downstreamEnd.node());
             } else {
                 if (m_downstreamEnd.node()->isTextNode()) {
