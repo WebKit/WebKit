1cf27645b5a52eea311e9ad8d904e5af27b449fb
Mark Rowe <mrowe@bdash.net.nz>
2007-05-03 22:48:45 +0000

    2007-05-03  Mark Rowe  <mrowe@apple.com>
            Reviewed by Geoff and Kevin.
    
            <rdar://problem/5141290> WebAssertions.h is still needed by some internal clients
    
            Second shot at fixing this error.  Stub out the macros rather than forwarding to JavaScriptCore, which would
            leave clients using this header trying to resolve JavaScriptCore symbols against WebKit when linking.  This
            should only happen in production builds when assertions should be disabled anyway as anyone building a
            development configuration should be in a position to move away from using this header.
    
            * Misc/WebAssertions.h:
    
    
    Canonical link: https://commits.webkit.org/17737@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@21238 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/Misc/WebAssertions.h
+++ WebKit/Misc/WebAssertions.h
@@ -27,4 +27,9 @@
  * which have yet to move away from it: <rdar://problem/5141290>.
  */
 
-#import <JavaScriptCore/Assertions.h>
+#warning <WebKit/WebAssertions.h> is deprecated.  Please move away from this SPI as soon as is possible.
+
+#define ASSERT(...) ((void)0)
+#define ASSERT_NOT_REACHED(...) ((void)0)
+#define ASSERT_ARG(...) ((void)0)
+#define ERROR(...) ((void)0)
