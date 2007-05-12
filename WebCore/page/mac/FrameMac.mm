b68b6ed9080b60c7b8a95bc4823f144d45d79082
Eric Seidel <eric@webkit.org>
2007-05-12 08:35:46 +0000

    2007-05-12  Eric Seidel  <eric@webkit.org>
            Reviewed by mjs.
    
            Crash starting Safari with JavaScript disabled.
            http://bugs.webkit.org/show_bug.cgi?id=13691
    
            Test: none possible.
    
            * page/mac/FrameMac.mm:
            (WebCore::Frame::cleanupPlatformScriptObjects): NULL check d->m_script
    
    
    
    Canonical link: https://commits.webkit.org/17873@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@21427 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/page/mac/FrameMac.mm
+++ WebCore/page/mac/FrameMac.mm
@@ -732,7 +732,7 @@ void Frame::cleanupPlatformScriptObjects()
     // (This problem is unique to m_windowScriptObject because its JS/DOM counterparts
     // persist across page loads.)
     removeDOMWrapper(reinterpret_cast<DOMObjectInternal*>(d->m_domWindow.get()));
-    if (d->m_jscript->haveInterpreter())
+    if (d->m_jscript && d->m_jscript->haveInterpreter())
         removeJSWrapper(KJS::Window::retrieveWindow(this));
     d->m_windowScriptObject = 0;
 }
