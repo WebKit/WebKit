042280d76393f381a167a3c172c79ff5d7b27d8d
staikos <staikos>
2007-05-20 18:36:13 +0000

    add -f to cp for some obscure build systems.
    Canonical link: https://commits.webkit.org/18037@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@21610 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/move-js-headers.sh
+++ WebCore/move-js-headers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p include/JavaScriptCore
-find ../JavaScriptCore/ -name \*.h -exec cp -p {} include/JavaScriptCore/. \;
+find ../JavaScriptCore/ -name \*.h -exec cp -f -p {} include/JavaScriptCore/. \;
 rm include/JavaScriptCore/config.h
 rm include/JavaScriptCore/JavaScriptCorePrefix.h
