b42019a8cb1ef7afc76def32412525daf11123c7
kmccullo <kmccullo>
2007-05-23 23:19:56 +0000

    LayoutTests:
            Reviewed by Darin.
    
            Update tests for http://bugs.webkit.org/show_bug.cgi?id=13830
            Auto-generate JS DOM bindings for HTMLDocument and most of
            the rest of HTMLElement
    
            - Corrects prototype chain for JSHTMLDocument and adds test
              for HTMLFormElement.
    
            * fast/dom/prototype-chain-expected.txt:
            * fast/dom/prototype-chain.html:
    
    WebCore:
    
            Reviewed by Darin.
    
            - rdar://problem/5183523
            - We now check the document's URL to see if it can load a resource in the case of a URL click, instead of checking the referrer, which is blank for local files.
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::load):
            (WebCore::FrameLoader::open):
            * platform/PlatformString.h:
            (WebCore::debugPrintString):
    
    
    
    Canonical link: https://commits.webkit.org/18095@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@21682 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/String.cpp
+++ WebCore/platform/String.cpp
@@ -510,3 +510,11 @@ WebCore::String* string(const char* s)
     return new WebCore::String(s);
 }
 #endif
+
+#ifndef NDEBUG
+void dump(const WebCore::String* s)
+{
+    if(s)
+        printf("%s\n", s->ascii().data());
+}
+#endif
