b80a1056db68c4790f37e276f7f226fc3d931c9e
Maciej Stachowiak <mjs@apple.com>
2007-06-01 00:19:05 +0000

    LayoutTests:
            Reviewed by Hyatt.
    
            - fix layout test editing/style/table-selection.html
    
            * editing/style/table-selection-expected.txt: Updated selection delegate output.
    
    WebCore:
    
            Reviewed by Hyatt.
    
            - fix layout test editing/style/table-selection.html
    
            This was broken by the change to keep whitespace text nodes in tables. They safely didn't render,
            but the font delta style change command would wrap them in spans, creating extra anonymous table
            cells.
    
            * editing/ApplyStyleCommand.cpp:
            (WebCore::ApplyStyleCommand::applyRelativeFontStyleChange): Don't wrap in a span if the text node
            is not rendered.
    
    
    
    Canonical link: https://commits.webkit.org/18282@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@21922 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/editing/ApplyStyleCommand.cpp
+++ WebCore/editing/ApplyStyleCommand.cpp
@@ -507,7 +507,7 @@ void ApplyStyleCommand::applyRelativeFontStyleChange(CSSMutableStyleDeclaration 
             if (!nodeFullySelected(node, start, end))
                 continue;
             elem = static_cast<HTMLElement *>(node);
-        } else if (node->isTextNode() && node->parentNode() != lastStyledNode) {
+        } else if (node->isTextNode() && node->renderer() && node->parentNode() != lastStyledNode) {
             // Last styled node was not parent node of this text node, but we wish to style this
             // text node. To make this possible, add a style span to surround this text node.
             RefPtr<HTMLElement> span = createStyleSpanElement(document());
