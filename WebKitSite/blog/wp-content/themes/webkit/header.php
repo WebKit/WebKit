adb52b85b89500f219230c64f26d0f9338ac6e4a
Mark Rowe <mrowe@bdash.net.nz>
2007-06-08 06:50:59 +0000

    2007-06-08  Mark Rowe  <mrowe@apple.com>
            Rubber-stamped by Hyatt.
    
            Switch to using Internet Explorer's conditional comments to target IE-specific styles rather
            than doing server-side user agent checks.  This resolves an issue some people were seeing where
            caching would lead to a page generated for an Internet Explorer user being served to a Safari
            user.  The visible symptom of this was many of the graphics were missing.
    
            * blog/wp-content/themes/webkit/header.php:
            * header.inc:
    
    
    Canonical link: https://commits.webkit.org/18337@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@22064 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitSite/blog/wp-content/themes/webkit/header.php
+++ WebKitSite/blog/wp-content/themes/webkit/header.php
@@ -11,19 +11,15 @@
     <link rel="stylesheet" type="text/css" href="/css/green.css" title="green">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
 
-<?php
-    $msie = '/msie\s(5\.[5-9]|[6]\.[0-9]*).*(win)/i';
-    $agent = $_SERVER['HTTP_USER_AGENT'];
-    if( isset($agent) && preg_match($msie, $agent) && ! preg_match('/opera/i', $agent)) {
-?>
-    <link rel="stylesheet" type="text/css" href="/css/ie.css">
-<?php } else { ?>
     <link rel="alternate stylesheet" type="text/css" href="/css/blue.css" title="blue">
     <link rel="alternate stylesheet" type="text/css" href="/css/yellow.css" title="yellow">
     <link rel="alternate stylesheet" type="text/css" href="/css/pink.css" title="pink">
     <link rel="alternate stylesheet" type="text/css" href="/css/purple.css" title="purple">
     <link rel="alternate stylesheet" type="text/css" href="/css/gray.css" title="gray">
-<?php } ?>
+
+<!--[if gte IE 5]>
+    <link rel="stylesheet" type="text/css" href="/css/ie.css">
+<![endif]-->
 
     <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>">
     <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>">
