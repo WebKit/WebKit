a0ee3eabae375d50fc6561e42d24c81986e6e4c8
zack <zack>
2007-06-11 07:30:43 +0000

    Forgot to export the plugin exporter. Fixing it.
    Canonical link: https://commits.webkit.org/18352@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@22090 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitQt/Api/qwebobjectpluginconnector.h
+++ WebKitQt/Api/qwebobjectpluginconnector.h
@@ -1,21 +1,21 @@
 /*
   Copyright (C) 2007 Trolltech ASA
-  
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Library General Public
   License as published by the Free Software Foundation; either
   version 2 of the License, or (at your option) any later version.
-  
+
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Library General Public License for more details.
-  
+
   You should have received a copy of the GNU Library General Public License
   along with this library; see the file COPYING.LIB.  If not, write to
   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
   Boston, MA 02111-1307, USA.
-  
+
   This class provides all functionality needed for loading images, style sheets and html
   pages from the web. It has a memory cache for these objects.
 */
@@ -34,22 +34,22 @@ class QWebObjectPluginConnectorPrivate;
 class QWebFactoryLoader;
 class QWebNetworkManager;
 
-class QWebObjectPluginConnector : public QObject
+class QWEBKIT_EXPORT QWebObjectPluginConnector : public QObject
 {
     Q_OBJECT
 public:
     QWebFrame *frame() const;
     QWidget *pluginParentWidget() const;
-    
+
     enum Target {
         Plugin,
-        New, 
+        New,
         Self,
         Parent,
         Top
     };
     QWebNetworkJob *requestUrl(QWebNetworkJob::Method, const QUrl &url, Target target = Plugin);
-    
+
 signals:
     void started(QWebNetworkJob*);
     void data(QWebNetworkJob*, const QByteArray &data);
@@ -62,5 +62,5 @@ private:
 
     QWebObjectPluginConnectorPrivate *d;
 };
-    
+
 #endif
