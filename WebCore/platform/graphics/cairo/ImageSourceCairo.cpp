05b8489a39efa22e55d4733c006e9f3b508661a4
Mark Rowe <mrowe@bdash.net.nz>
2007-07-05 13:43:44 +0000

    2007-07-05  Alp Toker  <alp.toker@collabora.co.uk>
            Reviewed by Mark Rowe.
    
            http://bugs.webkit.org/show_bug.cgi?id=14513
            Cairo animated GIF support is broken
    
            Implement ImageSource::frameIsCompleteAtIndex() to enable animated GIFs.
    
            * platform/graphics/cairo/ImageSourceCairo.cpp:
            (WebCore::ImageSource::frameIsCompleteAtIndex):
    
    
    Canonical link: https://commits.webkit.org/18756@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@24014 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/cairo/ImageSourceCairo.cpp
+++ WebCore/platform/graphics/cairo/ImageSourceCairo.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2007 Alp Toker <alp.toker@collabora.co.uk>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -163,10 +164,13 @@ NativeImagePtr ImageSource::createFrameAtIndex(size_t index)
                                                size().width()*4);
 }
 
-bool ImageSource::frameIsCompleteAtIndex(size_t)
+bool ImageSource::frameIsCompleteAtIndex(size_t index)
 {
-    // FIXME: write me. Returning false makes the animation not run
-    return false;
+    if (!m_decoder)
+        return false;
+
+    RGBA32Buffer* buffer = m_decoder->frameBufferAtIndex(index);
+    return buffer && buffer->status() == RGBA32Buffer::FrameComplete;
 }
 
 float ImageSource::frameDurationAtIndex(size_t index)
