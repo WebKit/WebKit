86ab5396885f5e7c44ff3ba07c4ba5d74931ce38
Alexey Proskuryakov <ap@webkit.org>
2007-08-12 19:20:50 +0000

    Reviewed by Darin.
            http://bugs.webkit.org/show_bug.cgi?id=4648
            Shockwave unable to load GZip'd text resources when server sends Content-Length header
    
            * Plugins/WebBaseNetscapePluginStream.mm:
            (-[WebBaseNetscapePluginStream startStreamWithResponse:]): Don't trust
            -[NSURLResponse expectedContentLength] if Content-Encoding is not identity.
    
    
    
    Canonical link: https://commits.webkit.org/19575@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@25024 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/Plugins/WebBaseNetscapePluginStream.mm
+++ WebKit/Plugins/WebBaseNetscapePluginStream.mm
@@ -295,6 +295,8 @@ static StreamMap& streams()
 - (void)startStreamWithResponse:(NSURLResponse *)r
 {
     NSMutableData *theHeaders = nil;
+    long long expectedContentLength = [r expectedContentLength];
+
     if ([r isKindOfClass:[NSHTTPURLResponse class]]) {
         NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)r;
         theHeaders = [NSMutableData dataWithCapacity:1024];
@@ -331,11 +333,18 @@ static StreamMap& streams()
             [theHeaders appendBytes:"\n" length:1];
         }
 
+        // If the content is encoded (most likely compressed), then don't send its length to the plugin,
+        // which is only interested in the decoded length, not yet known at the moment.
+        // <rdar://problem/4470599> tracks a request for -[NSURLResponse expectedContentLength] to incorporate this logic.
+        NSString *contentEncoding = (NSString *)[[(NSHTTPURLResponse *)r allHeaderFields] objectForKey:@"Content-Encoding"];
+        if (contentEncoding && ![contentEncoding isEqualToString:@"identity"])
+            expectedContentLength = -1;
+
         // startStreamResponseURL:... will null-terminate.
     }
 
     [self startStreamResponseURL:[r URL]
-           expectedContentLength:[r expectedContentLength]
+           expectedContentLength:expectedContentLength
                 lastModifiedDate:WKGetNSURLResponseLastModifiedDate(r)
                         MIMEType:[r MIMEType]
                          headers:theHeaders];
