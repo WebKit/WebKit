bf44567c764ea591c441569cb9786ebbee8c41e8
beidson <beidson>
2007-09-07 17:39:24 +0000

    Reviewed by Darin
            Add some AutoreleasePool and Threading utilities in preparation for the multi-threaded
            fix for <rdar://problem/5434431>
    
            AutodrainedPool wraps an NSAutoreleasePool and tracks a "cycle" count.  This allows clients
            in WebCore to simply call ::cycle() and after a preset number of iterations the pool will drain
            and recreate the underlying NSAutoreleasePool
    
            Threading utilities include C++ wrappers for pthread mutexes and conditions, as well as a generic
            "callOnMainThread()" function for any non-main thread to use.
    
            * platform/AutodrainedPool.h: Added.
            (WebCore::AutodrainedPool::AutodrainedPool): Empty implementations for all non-mac platforms
            (WebCore::AutodrainedPool::~AutodrainedPool): ditto
            (WebCore::AutodrainedPool::cycle): ditto
            * platform/mac/AutodrainedPool.mm: Added.
            (WebCore::AutodrainedPool::AutodrainedPool):
            (WebCore::AutodrainedPool::~AutodrainedPool):
            (WebCore::AutodrainedPool::cycle): Keep track of number of cycles, and drain/recreate the pool if the
              cycle limit is hit
    
            * platform/Threading.h: Added.
            (WebCore::Mutex::Mutex): C++ wrapper for pthread_mutex
            (WebCore::Mutex::~Mutex):
            (WebCore::Mutex::lock):
            (WebCore::Mutex::tryLock):
            (WebCore::Mutex::unlock):
            (WebCore::MutexLocker::MutexLocker): Handles automatically locking/unlocking a Mutex (for early returns
              from a function, for example)
            (WebCore::MutexLocker::~MutexLocker):
            (WebCore::ThreadCondition::ThreadCondition): C++ wrapper for pthread_condition
            (WebCore::ThreadCondition::~ThreadCondition):
            (WebCore::ThreadCondition::wait):
            (WebCore::ThreadCondition::signal):
            (WebCore::ThreadCondition::broadcast):
            * platform/mac/Threading.mm: Added.
            (-[WebCoreFunctionWrapper initWithFunction:]): Obj-C implementation of "callOnMainThread"
            (-[WebCoreFunctionWrapper _call]):
            (WebCore::callOnMainThread):
    
    
    
    Canonical link: https://commits.webkit.org/19893@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@25416 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/platform/Threading.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2007 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer. 
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution. 
+ * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef Threading_h
+#define Threading_h
+
+#include <wtf/Noncopyable.h>
+#include <pthread.h>
+
+namespace WebCore {
+
+class ThreadCondition;
+
+class Mutex : Noncopyable {
+friend class ThreadCondition;
+public:
+    Mutex() { pthread_mutex_init(&m_mutex, NULL); }
+    ~Mutex() { pthread_mutex_destroy(&m_mutex); }
+
+    int lock() { return pthread_mutex_lock(&m_mutex);}
+    int tryLock() { return pthread_mutex_trylock(&m_mutex); }
+    int unlock() { return pthread_mutex_unlock(&m_mutex); }
+    
+private:
+    pthread_mutex_t m_mutex;
+};
+
+class MutexLocker : Noncopyable {
+public:
+    MutexLocker(Mutex& mutex) : m_mutex(mutex) { m_mutex.lock(); }
+    ~MutexLocker() { m_mutex.unlock(); }
+private:
+    Mutex& m_mutex;
+};
+
+class ThreadCondition : Noncopyable {
+public:
+    ThreadCondition() { pthread_cond_init(&m_condition, NULL); }
+    ~ThreadCondition() { pthread_cond_destroy(&m_condition); }
+    
+    int wait(Mutex& mutex) { return pthread_cond_wait(&m_condition, &mutex.m_mutex); }
+    int signal() { return pthread_cond_signal(&m_condition); }
+    int broadcast() { return pthread_cond_broadcast(&m_condition); }
+    
+private:
+    pthread_cond_t m_condition;
+};
+
+void callOnMainThread(void (*)());
+
+} // namespace WebCore
+
+#endif // Threading_h
