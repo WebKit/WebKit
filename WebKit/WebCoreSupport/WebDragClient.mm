120c440166a5e56b7a6d87ad93b8c6e146b4784d
Timothy Hatcher <timothy@apple.com>
2007-09-07 19:07:48 +0000

    Attempt to fix the PPC build.
    Canonical link: https://commits.webkit.org/19897@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@25420 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/WebCoreSupport/WebDragClient.mm
+++ WebKit/WebCoreSupport/WebDragClient.mm
@@ -99,19 +99,22 @@ void WebDragClient::startDrag(DragImageRef dragImage, const IntPoint& at, const 
     [topHTMLView _stopAutoscrollTimer];
     NSPasteboard *pasteboard = static_cast<ClipboardMac*>(clipboard)->pasteboard();
 
+    NSImage *dragNSImage = dragImage.get();
+    WebHTMLView *sourceHTMLView = htmlView.get();
+
     id delegate = [m_webView UIDelegate];
     SEL selector = @selector(webView:dragImage:at:offset:event:pasteboard:source:slideBack:forView:);
     if ([delegate respondsToSelector:selector]) {
         if ([m_webView _catchesDelegateExceptions]) {
             @try {
-                [delegate webView:m_webView dragImage:dragImage.get() at:at offset:NSZeroSize event:event pasteboard:pasteboard source:htmlView.get() slideBack:YES forView:topHTMLView];
+                [delegate webView:m_webView dragImage:dragNSImage at:at offset:NSZeroSize event:event pasteboard:pasteboard source:sourceHTMLView slideBack:YES forView:topHTMLView];
             } @catch (id exception) {
                 ReportDiscardedDelegateException(selector, exception);
             }
         } else
-            [delegate webView:m_webView dragImage:dragImage.get() at:at offset:NSZeroSize event:event pasteboard:pasteboard source:htmlView.get() slideBack:YES forView:topHTMLView];
+            [delegate webView:m_webView dragImage:dragNSImage at:at offset:NSZeroSize event:event pasteboard:pasteboard source:sourceHTMLView slideBack:YES forView:topHTMLView];
     } else
-        [topHTMLView dragImage:dragImage.get() at:at offset:NSZeroSize event:event pasteboard:pasteboard source:htmlView.get() slideBack:YES];
+        [topHTMLView dragImage:dragNSImage at:at offset:NSZeroSize event:event pasteboard:pasteboard source:sourceHTMLView slideBack:YES];
 }
 
 DragImageRef WebDragClient::createDragImageForLink(KURL& url, const String& title, Frame* frame)
