3673b57d6432574b759754c83acb535ba4fd8158
Holger Freyther <zecke@selfish.org>
2007-09-09 13:23:22 +0000

    2007-09-09  Holger Hans Peter Freyther  <zecke@selfish.org>
            Build fix for Gtk+ by adding WebCore::callOnMainThread to the TemporaryLinkStubs.
    
            * platform/gdk/TemporaryLinkStubs.cpp:
            (WebCore::callOnMainThread):
    
    
    Canonical link: https://commits.webkit.org/19934@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@25459 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/gdk/TemporaryLinkStubs.cpp
+++ WebCore/platform/gdk/TemporaryLinkStubs.cpp
@@ -237,5 +237,6 @@ String signedPublicKeyAndChallengeString(unsigned keySizeIndex, const String &ch
 float userIdleTime() { notImplemented(); return 0.0; }
 bool fileExists(const String& path) { notImplemented(); return false; }
 bool deleteFile(const String& path) { notImplemented(); return false; }
+void callOnMainThread(void (*)()) { notImplemented(); }
 }
 
