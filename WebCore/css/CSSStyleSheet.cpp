006be4097569492c85e1cab073f34d04cabf3c28
antti <antti>
2007-09-17 20:42:14 +0000

    WebCore:
            Reviewed by Darin.
    
            Fix <rdar://problem/5480050>
            Leopard9A551 with Dashcode 112: Dashcode crash when dragging image into canvas.
    
            If @import stylesheet was modified through CSS DOM style selector would not get updated. This
            could lead to crashes.
    
            Test: fast/css/import-style-update.html
    
            * css/CSSStyleSheet.cpp:
            (WebCore::CSSStyleSheet::styleSheetChanged):
    
    LayoutTests:
    
            Reviewed by Darin.
    
            Test for <rdar://problem/5480050>
            Leopard9A551 with Dashcode 112: Dashcode crash when dragging image into canvas.
    
            Non-crashing test case, but it demonstrates the same problem that would crash
            under some circumstances.
    
            * fast/css/import-style-update-expected.txt: Added.
            * fast/css/import-style-update.html: Added.
    
    
    
    Canonical link: https://commits.webkit.org/20061@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@25598 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/css/CSSStyleSheet.cpp
+++ WebCore/css/CSSStyleSheet.cpp
@@ -190,12 +190,17 @@ DocLoader *CSSStyleSheet::docLoader()
 
 void CSSStyleSheet::styleSheetChanged()
 {
+    StyleBase* root = this;
+    while (StyleBase* parent = root->parent())
+        root = parent;
+    Document* documentToUpdate = (root && root->isCSSStyleSheet()) ? static_cast<CSSStyleSheet*>(root)->doc() : 0;
+    
     /* FIXME: We don't need to do everything updateStyleSelector does,
      * basically we just need to recreate the document's selector with the
      * already existing style sheets.
      */
-    if (m_doc)
-        m_doc->updateStyleSelector();
+    if (documentToUpdate)
+        documentToUpdate->updateStyleSelector();
 }
 
 }
