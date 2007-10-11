f847622231b1994bd4e5fbace5ab8d074c0d5c4a
Justin Garcia <justin.garcia@apple.com>
2007-10-11 22:31:36 +0000

    WebCore:
            Reviewed by Maciej.
    
            <rdar://problem/5521237> Pasting a word copied from TextEdit after a regular, breaking space, adds a leading newline
    
            * editing/CompositeEditCommand.cpp:
            (WebCore::CompositeEditCommand::prepareWhitespaceAtPositionForSplit):
            Fixed a typo.
    
    LayoutTests:
    
            Reviewed by Maciej.
    
            <rdar://problem/5521237> Pasting a word copied from TextEdit after a regular, breaking space, adds a leading newline
    
            * editing/pasteboard/5521237-expected.txt: Added.
            * editing/pasteboard/5521237.html: Added.
    
    
    
    Canonical link: https://commits.webkit.org/20366@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@26369 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/editing/CompositeEditCommand.cpp
+++ WebCore/editing/CompositeEditCommand.cpp
@@ -411,7 +411,7 @@ void CompositeEditCommand::prepareWhitespaceAtPositionForSplit(Position& positio
     position = upstreamPos.downstream();
 
     VisiblePosition visiblePos(position);
-    VisiblePosition previousVisiblePos(visiblePos.next());
+    VisiblePosition previousVisiblePos(visiblePos.previous());
     Position previous(previousVisiblePos.deepEquivalent());
     
     if (isCollapsibleWhitespace(previousVisiblePos.characterAfter()) && previous.node()->isTextNode() && !previous.node()->hasTagName(brTag))
