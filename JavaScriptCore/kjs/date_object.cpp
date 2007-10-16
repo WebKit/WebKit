f293bf1cbd4553e18d49e7797358ea72fbf33811
Darin Adler <darin@apple.com>
2007-10-16 20:44:28 +0000

    - try to fix the Windows build
            * kjs/date_object.cpp: (KJS::parseDate): A couple instances of isspace were
            in here. Not sure why it wasn't failing elsewhere. Changed to isASCIISpace.
    
    
    
    Canonical link: https://commits.webkit.org/20637@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@26679 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/date_object.cpp
+++ JavaScriptCore/kjs/date_object.cpp
@@ -886,14 +886,14 @@ static double parseDate(const UString &date)
             if (month == -1)
                 return NaN;
 
-            while (*dateString && *dateString != '-' && *dateString != ',' && !isspace(*dateString))
+            while (*dateString && *dateString != '-' && *dateString != ',' && !isASCIISpace(*dateString))
                 dateString++;
 
             if (!*dateString)
                 return NaN;
 
             // '-99 23:12:40 GMT'
-            if (*dateString != '-' && *dateString != '/' && *dateString != ',' && !isspace(*dateString))
+            if (*dateString != '-' && *dateString != '/' && *dateString != ',' && !isASCIISpace(*dateString))
                 return NaN;
             dateString++;
         }
@@ -917,7 +917,7 @@ static double parseDate(const UString &date)
         dateString = newPosStr;
     else {
         // ' 23:12:40 GMT'
-        if (!(isspace(*newPosStr) || *newPosStr == ',')) {
+        if (!(isASCIISpace(*newPosStr) || *newPosStr == ',')) {
             if (*newPosStr != ':')
                 return NaN;
             // There was no year; the number was the hour.
