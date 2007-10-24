51441dcfc5ae6a1882d7992d9273b587cf38ae59
Adam Roben <aroben@webkit.org>
2007-10-24 01:30:00 +0000

    Fix the behavior of pathByAppendingComponent when path is empty
            We now use the Windows Shell API function PathAppendW instead of
            rolling our own broken implementation.
    
            Reviewed by Anders.
    
            * platform/win/FileSystemWin.cpp:
            (WebCore::pathByAppendingComponent):
    
    
    Canonical link: https://commits.webkit.org/20865@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@26937 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/win/FileSystemWin.cpp
+++ WebCore/platform/win/FileSystemWin.cpp
@@ -36,6 +36,7 @@
 #include <windows.h>
 #include <winbase.h>
 #include <shlobj.h>
+#include <shlwapi.h>
 
 namespace WebCore {
 
@@ -65,10 +66,21 @@ bool deleteFile(const String& path)
 
 String pathByAppendingComponent(const String& path, const String& component)
 {
-    if (path.endsWith("\\"))
-        return path + component;
-    else
-        return path + "\\" + component;
+    Vector<UChar> buffer(MAX_PATH);
+
+    if (path.length() + 1 > buffer.size())
+        return String();
+
+    memcpy(buffer.data(), path.characters(), path.length() * sizeof(UChar));
+    buffer[path.length()] = '\0';
+
+    String componentCopy = component;
+    if (!PathAppendW(buffer.data(), componentCopy.charactersWithNullTermination()))
+        return String();
+
+    buffer.resize(wcslen(buffer.data()));
+
+    return String::adopt(buffer);
 }
 
 CString fileSystemRepresentation(const String&)
