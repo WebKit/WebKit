6bbf69a88b52aa14d6caf51ceff5b0c8ac8f8446
kmccullo <kmccullo>
2007-10-27 23:26:58 +0000

    Reviewed by Adam.
            - Stubbed out IWebScriptCallFrame for Drosera.
    
            * Interfaces/IWebScriptCallFrame.idl: Added function declarations.
            * WebKit.vcproj/WebKit.vcproj: Added .h/.cpp files to the project.
            * WebScriptCallFrame.cpp: Added.
            (WebScriptCallFrame::WebScriptCallFrame):
            (WebScriptCallFrame::~WebScriptCallFrame):
            (WebScriptCallFrame::createInstance):
            (WebScriptCallFrame::QueryInterface):
            (WebScriptCallFrame::AddRef):
            (WebScriptCallFrame::Release):
            (WebScriptCallFrame::caller):
            (WebScriptCallFrame::scopeChain):
            (WebScriptCallFrame::functionName):
            (WebScriptCallFrame::evaluateWebScript):
            * WebScriptCallFrame.h: Added.
    
    
    
    Canonical link: https://commits.webkit.org/21077@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@27156 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebKit/win/WebScriptCallFrame.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2007 Apple Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer. 
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution. 
+ * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef WebScriptCallFrame_h
+#define WebScriptCallFrame_h
+
+#include "IWebScriptCallFrame.h"
+
+class WebScriptCallFrame : public IWebScriptCallFrame {
+public:
+    static WebScriptCallFrame* createInstance();
+
+private:
+    WebScriptCallFrame();
+    ~WebScriptCallFrame();
+
+public:
+    // IUnknown
+    virtual HRESULT STDMETHODCALLTYPE QueryInterface( 
+        /* [in] */ REFIID riid,
+        /* [retval][out] */ void** ppvObject);
+    
+    virtual ULONG STDMETHODCALLTYPE AddRef( void);
+
+    virtual ULONG STDMETHODCALLTYPE Release( void);
+
+    // IWebScriptCallFrame
+    virtual HRESULT STDMETHODCALLTYPE caller(
+        /* [out, retval] */ IWebScriptCallFrame**);
+
+    virtual HRESULT STDMETHODCALLTYPE scopeChain(
+        /* [out, retval] */ VARIANT*);
+
+    virtual HRESULT STDMETHODCALLTYPE functionName(
+        /* [out, retval] */ BSTR*);
+
+    virtual HRESULT STDMETHODCALLTYPE evaluateWebScript(
+        /* [in] */ BSTR script,
+        /* [out, retval] */ VARIANT*);
+
+private:
+    ULONG m_refCount;
+};
+
+#endif
