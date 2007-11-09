bdf21468f6f024b1bf6e4169574b65384a464969
Steve Falkenburg <sfalken@apple.com>
2007-11-09 17:16:39 +0000

    Roll out r27641 since the same bug was already fixed by r27568.
    Canonical link: https://commits.webkit.org/21568@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@27647 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/page/ContextMenuController.cpp
+++ WebCore/page/ContextMenuController.cpp
@@ -110,7 +110,6 @@ static void openNewWindow(const KURL& urlToLoad, Frame* frame)
 {
     if (Page* oldPage = frame->page()) {
         WindowFeatures features;
-        memset(&features, 0, sizeof(WindowFeatures));
         if (Page* newPage = oldPage->chrome()->createWindow(frame,
                 FrameLoadRequest(ResourceRequest(urlToLoad, frame->loader()->outgoingReferrer())), features))
             newPage->chrome()->show();
