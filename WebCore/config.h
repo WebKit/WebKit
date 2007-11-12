74666865fe75b66b8e96e1b0da89305690907555
Adam Roben <aroben@apple.com>
2007-11-12 11:02:47 +0000

    Windows build fix
            * config.h: Touch because VS apparently can't figure out which files
            depend on ResourceResponse.h.
            * WebCore.vcproj/WebCore.vcproj: Add ResourceResponseBase files to
            project and remove old ResourceResponse files.
    
    
    Canonical link: https://commits.webkit.org/21637@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@27716 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/config.h
+++ WebCore/config.h
@@ -37,7 +37,7 @@
 #endif
 
 // If we don't define these, they get defined in windef.h. 
-// We want to use std::min and std::max
+// We want to use std::min and std::max.
 #ifndef max
 #define max max
 #endif
