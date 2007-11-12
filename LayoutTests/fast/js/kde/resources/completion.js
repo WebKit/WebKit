b0bf7e6e362e77880534f4cf97be3209f9ab4ea7
Geoffrey Garen <ggaren@apple.com>
2007-11-12 22:06:46 +0000

    Rubber-stamped by Oliver Hunt.
            Landing a few layout tests from http://bugs.webkit.org/show_bug.cgi?id=14868
            Import variable lookup optimizations from KJS
    
            * fast/js/kde/completion-expected.txt:
            * fast/js/kde/resources/completion.js:
    
    
    
    Canonical link: https://commits.webkit.org/21648@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@27727 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/js/kde/resources/completion.js
+++ LayoutTests/fast/js/kde/resources/completion.js
@@ -12,4 +12,16 @@ var caught = false;
 try { eval("return;"); } catch(e) { caught = true; }
 shouldBeTrue("caught");
 
+// value completions take precedence
+var val = eval("11; { }");
+shouldBe("val", "11");
+val = eval("12; ;");
+shouldBe("val", "12");
+val = eval("13; if(false);");
+shouldBe("val", "13");
+val = eval("14; function f() {}");
+shouldBe("val", "14");
+val = eval("15; var v = 0");
+shouldBe("val", "15");
+
 successfullyParsed = true
