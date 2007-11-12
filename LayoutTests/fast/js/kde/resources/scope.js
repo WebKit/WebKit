7dda155d1985cfdeca5a6d5b6739db49b53581aa
Geoffrey Garen <ggaren@apple.com>
2007-11-12 21:48:55 +0000

    Rubber-stamped by Oliver Hunt.
            Landing a few layout tests from http://bugs.webkit.org/show_bug.cgi?id=14868
            Import variable lookup optimizations from KJS
    
            * fast/js/kde/arguments-scope-expected.txt: Added.
            * fast/js/kde/arguments-scope.html: Added.
            * fast/js/kde/resources/arguments-scope.js: Added.
            * fast/js/kde/resources/scope.js:
            * fast/js/kde/scope-expected.txt:
    
    
    
    Canonical link: https://commits.webkit.org/21643@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@27722 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/js/kde/resources/scope.js
+++ LayoutTests/fast/js/kde/resources/scope.js
@@ -6,4 +6,31 @@ with (b) {
 }
 
 shouldBe("f(2)", "22");
-successfullyParsed = true
+
+var OBJECT = new MyObject( "hello" );
+function MyObject(value) {
+    this.value = value;
+    this.toString = new Function( "return this.value+''" );
+    return this;
+}
+shouldBe("OBJECT.toString()", "'hello'");
+var s;
+with (OBJECT) {
+    s = toString();
+}
+shouldBe("s", "'hello'");
+
+
+// Make sure that for ... in reevaluates the scoping every time!
+P = { foo : 1, bar : 2, baz : 3 }
+
+function testForIn() {
+   for (g in P) {
+        eval("var g;") //Change the scope of g half-ways through the loop
+   }
+}
+
+testForIn();
+shouldBe("g", "'foo'"); //Before the eval, g was in outer scope, but not after!
+
+var successfullyParsed = true;
