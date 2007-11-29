77798f629ca8c92ccecedd8bd06aa814b18731f1
Eric Seidel <eric@webkit.org>
2007-11-29 11:07:57 +0000

    2007-11-24  Eric Seidel  <eric@webkit.org>
            Reviewed by Sam.
    
            Fix spacing for read_repeat_counts
    
            * pcre/pcre_compile.cpp:
            (read_repeat_counts):
    
    
    Canonical link: https://commits.webkit.org/22061@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@28142 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/pcre/pcre_compile.cpp
+++ JavaScriptCore/pcre/pcre_compile.cpp
@@ -352,47 +352,46 @@ Returns:         pointer to '}' on success;
 
 static const UChar* read_repeat_counts(const UChar* p, int* minp, int* maxp, ErrorCode* errorcodeptr)
 {
-int min = 0;
-int max = -1;
-
-/* Read the minimum value and do a paranoid check: a negative value indicates
-an integer overflow. */
-
-while (isASCIIDigit(*p)) min = min * 10 + *p++ - '0';
-if (min < 0 || min > 65535)
-  {
-  *errorcodeptr = ERR5;
-  return p;
-  }
-
-/* Read the maximum value if there is one, and again do a paranoid on its size.
-Also, max must not be less than min. */
-
-if (*p == '}') max = min; else
-  {
-  if (*(++p) != '}')
-    {
-    max = 0;
-    while (isASCIIDigit(*p)) max = max * 10 + *p++ - '0';
-    if (max < 0 || max > 65535)
-      {
-      *errorcodeptr = ERR5;
-      return p;
-      }
-    if (max < min)
-      {
-      *errorcodeptr = ERR4;
-      return p;
-      }
+    int min = 0;
+    int max = -1;
+    
+    /* Read the minimum value and do a paranoid check: a negative value indicates
+     an integer overflow. */
+    
+    while (isASCIIDigit(*p))
+        min = min * 10 + *p++ - '0';
+    if (min < 0 || min > 65535) {
+        *errorcodeptr = ERR5;
+        return p;
     }
-  }
-
-/* Fill in the required variables, and pass back the pointer to the terminating
-'}'. */
-
-*minp = min;
-*maxp = max;
-return p;
+    
+    /* Read the maximum value if there is one, and again do a paranoid on its size.
+     Also, max must not be less than min. */
+    
+    if (*p == '}')
+        max = min;
+    else {
+        if (*(++p) != '}') {
+            max = 0;
+            while (isASCIIDigit(*p))
+                max = max * 10 + *p++ - '0';
+            if (max < 0 || max > 65535) {
+                *errorcodeptr = ERR5;
+                return p;
+            }
+            if (max < min) {
+                *errorcodeptr = ERR4;
+                return p;
+            }
+        }
+    }
+    
+    /* Fill in the required variables, and pass back the pointer to the terminating
+     '}'. */
+    
+    *minp = min;
+    *maxp = max;
+    return p;
 }
 
 
