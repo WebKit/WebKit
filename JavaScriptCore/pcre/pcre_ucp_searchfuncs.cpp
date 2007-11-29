791e7b519d2601d773916fe119facfc1003e09a8
Eric Seidel <eric@webkit.org>
2007-11-29 11:21:50 +0000

    2007-11-24  Eric Seidel  <eric@webkit.org>
            Reviewed by Sam.
    
            cleanup _pcre_ucp_othercase
    
            * pcre/pcre_ucp_searchfuncs.cpp:
            (_pcre_ucp_othercase):
    
    
    Canonical link: https://commits.webkit.org/22078@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@28159 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/pcre/pcre_ucp_searchfuncs.cpp
+++ JavaScriptCore/pcre/pcre_ucp_searchfuncs.cpp
@@ -59,39 +59,42 @@ Arguments:
 Returns:      the other case or -1 if none
 */
 
-int
-_pcre_ucp_othercase(const unsigned int c)
+int _pcre_ucp_othercase(const unsigned c)
 {
-int bot = 0;
-int top = sizeof(ucp_table)/sizeof(cnode);
-int mid, offset;
-
-/* The table is searched using a binary chop. You might think that using
-intermediate variables to hold some of the common expressions would speed
-things up, but tests with gcc 3.4.4 on Linux showed that, on the contrary, it
-makes things a lot slower. */
-
-for (;;)
-  {
-  if (top <= bot) return -1;
-  mid = (bot + top) >> 1;
-  if (c == (ucp_table[mid].f0 & f0_charmask)) break;
-  if (c < (ucp_table[mid].f0 & f0_charmask)) top = mid;
-  else
-    {
-    if ((ucp_table[mid].f0 & f0_rangeflag) != 0 &&
-        c <= (ucp_table[mid].f0 & f0_charmask) +
-             (ucp_table[mid].f1 & f1_rangemask)) break;
-    bot = mid + 1;
+    int bot = 0;
+    int top = sizeof(ucp_table) / sizeof(cnode);
+    int mid;
+    
+    /* The table is searched using a binary chop. You might think that using
+     intermediate variables to hold some of the common expressions would speed
+     things up, but tests with gcc 3.4.4 on Linux showed that, on the contrary, it
+     makes things a lot slower. */
+    
+    for (;;) {
+        if (top <= bot)
+            return -1;
+        mid = (bot + top) >> 1;
+        if (c == (ucp_table[mid].f0 & f0_charmask))
+            break;
+        if (c < (ucp_table[mid].f0 & f0_charmask))
+            top = mid;
+        else {
+            if ((ucp_table[mid].f0 & f0_rangeflag) != 0 &&
+                c <= (ucp_table[mid].f0 & f0_charmask) +
+                (ucp_table[mid].f1 & f1_rangemask))
+                break;
+            bot = mid + 1;
+        }
     }
-  }
-
-/* Found an entry in the table. Return -1 for a range entry. Otherwise return
-the other case if there is one, else -1. */
-
-if ((ucp_table[mid].f0 & f0_rangeflag) != 0) return -1;
-
-offset = ucp_table[mid].f1 & f1_casemask;
-if ((offset & f1_caseneg) != 0) offset |= f1_caseneg;
-return (offset == 0)? -1 : c + offset;
+    
+    /* Found an entry in the table. Return -1 for a range entry. Otherwise return
+     the other case if there is one, else -1. */
+    
+    if (ucp_table[mid].f0 & f0_rangeflag)
+        return -1;
+    
+    int offset = ucp_table[mid].f1 & f1_casemask;
+    if (offset & f1_caseneg)
+        offset |= f1_caseneg;
+    return !offset ? -1 : c + offset;
 }
