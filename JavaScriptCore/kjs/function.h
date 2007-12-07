14ac728fdc6f517b92040d0e34565d0493a080e4
Geoffrey Garen <ggaren@apple.com>
2007-12-07 22:23:41 +0000

    Build fix: removed stray name qualification.
            * kjs/function.h:
            (KJS::ActivationImp::ActivationImp):
    
    
    
    Canonical link: https://commits.webkit.org/22412@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@28529 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/function.h
+++ JavaScriptCore/kjs/function.h
@@ -155,7 +155,7 @@ namespace KJS {
     };
 
   public:
-    ActivationImp::ActivationImp(ExecState* exec)
+    ActivationImp(ExecState* exec)
         : JSVariableObject(new ActivationImpData(exec))
     {
     }
