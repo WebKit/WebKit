86aefbf0c9005faaea8850371c45a8fc4d3ca36a
Ada Chan <adachan@apple.com>
2007-12-07 07:27:40 +0000

    Fixed the if statement (ERROR_SUCCESS is 0 and we were actually         returning true when there's an error).
            Reviewed by Steve.
    
            * platform/win/FileSystemWin.cpp:
            (WebCore::makeAllDirectories):
    
    
    
    Canonical link: https://commits.webkit.org/22397@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@28514 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/win/FileSystemWin.cpp
+++ WebCore/platform/win/FileSystemWin.cpp
@@ -91,7 +91,7 @@ CString fileSystemRepresentation(const String&)
 bool makeAllDirectories(const String& path)
 {
     String fullPath = path;
-    if (!SHCreateDirectoryEx(0, fullPath.charactersWithNullTermination(), 0)) {
+    if (SHCreateDirectoryEx(0, fullPath.charactersWithNullTermination(), 0) != ERROR_SUCCESS) {
         DWORD error = GetLastError();
         if (error != ERROR_FILE_EXISTS && error != ERROR_ALREADY_EXISTS) {
             LOG_ERROR("Failed to create path %s", path.ascii().data());
