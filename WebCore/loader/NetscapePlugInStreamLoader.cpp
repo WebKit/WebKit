724daf4ef27c245b9fdf49f0ddcc1c3997b86840
Mark Rowe <mrowe@apple.com>
2007-12-17 01:50:18 +0000

    2007-12-16  Rodney Dawes  <dobey@wayofthemonkey.com>
            Reviewed by Maciej Stachowiak.
    
            http://bugs.webkit.org/show_bug.cgi?id=16389
            Bug 16389: Common Implementation of NetscapePlugInStreamLoader
    
            * WebCore.vcproj/WebCore.vcproj: Remove NetscapePlugInStreamLoaderWin.cpp.
            * loader/NetscapePlugInStreamLoader.cpp: Copy method implementations from NetscapePlugInStreamLoaderWin.cpp.
            * loader/win/NetscapePlugInStreamLoaderWin.cpp: Removed.
    
    
    Canonical link: https://commits.webkit.org/22655@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@28790 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/NetscapePlugInStreamLoader.cpp
+++ WebCore/loader/NetscapePlugInStreamLoader.cpp
@@ -29,8 +29,91 @@
 #include "config.h"
 #include "NetscapePlugInStreamLoader.h"
 
+#include "FrameLoader.h"
 #include "DocumentLoader.h"
 
 namespace WebCore {
+
+NetscapePlugInStreamLoader::NetscapePlugInStreamLoader(Frame* frame, NetscapePlugInStreamLoaderClient* client)
+    : ResourceLoader(frame, true, true)
+    , m_client(client)
+{
+}
+
+NetscapePlugInStreamLoader::~NetscapePlugInStreamLoader()
+{
+}
+
+PassRefPtr<NetscapePlugInStreamLoader> NetscapePlugInStreamLoader::create(Frame* frame, NetscapePlugInStreamLoaderClient* client)
+{
+    return new NetscapePlugInStreamLoader(frame, client);
+}
+
+bool NetscapePlugInStreamLoader::isDone() const
+{
+    return !m_client;
+}
+
+void NetscapePlugInStreamLoader::releaseResources()
+{
+    m_client = 0;
+    ResourceLoader::releaseResources();
+}
+
+void NetscapePlugInStreamLoader::didReceiveResponse(const ResourceResponse& response)
+{
+    RefPtr<NetscapePlugInStreamLoader> protect(this);
+
+    m_client->didReceiveResponse(this, response);
+
+    // Don't continue if the stream is cancelled
+    if (!m_client)
+        return;
+
+    ResourceLoader::didReceiveResponse(response);
+
+    // Don't continue if the stream is cancelled
+    if (!m_client)
+        return;
+
+    if (response.isHTTP() && (response.httpStatusCode() < 100 || response.httpStatusCode() >= 400))
+        didCancel(frameLoader()->fileDoesNotExistError(response));
+}
+
+void NetscapePlugInStreamLoader::didReceiveData(const char* data, int length, long long lengthReceived, bool allAtOnce)
+{
+    RefPtr<NetscapePlugInStreamLoader> protect(this);
+
+    m_client->didReceiveData(this, data, length);
+    
+    ResourceLoader::didReceiveData(data, length, lengthReceived, allAtOnce);
+}
+
+void NetscapePlugInStreamLoader::didFinishLoading()
+{
+    RefPtr<NetscapePlugInStreamLoader> protect(this);
+
+    m_documentLoader->removePlugInStreamLoader(this);
+    m_client->didFinishLoading(this);
+    ResourceLoader::didFinishLoading();
+}
+
+void NetscapePlugInStreamLoader::didFail(const ResourceError& error)
+{
+    RefPtr<NetscapePlugInStreamLoader> protect(this);
+
+    m_documentLoader->removePlugInStreamLoader(this);
+    m_client->didFail(this, error);
+    ResourceLoader::didFail(error);
+}
+
+void NetscapePlugInStreamLoader::didCancel(const ResourceError& error)
+{
+    RefPtr<NetscapePlugInStreamLoader> protect(this);
+
+    m_documentLoader->removePlugInStreamLoader(this);
+    m_client->didFail(this, error);
+    ResourceLoader::didCancel(error);
+}
 
 }
