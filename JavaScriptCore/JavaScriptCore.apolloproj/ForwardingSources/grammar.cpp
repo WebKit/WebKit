4df4ac1bd3c69c4182b63bdf65f5cf9d7f9f0a59
Mark Rowe <mrowe@apple.com>
2007-12-19 02:12:08 +0000

    Remove outdated and non-functioning project files for the Apollo port.  Rubber-stamped by Maciej Stachowiak and Chris Brichford.
    Canonical link: https://commits.webkit.org/22703@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@28846 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/JavaScriptCore.apolloproj/ForwardingSources/grammar.cpp
+++
@@ -1,5 +0,0 @@
-// This file includes a derived source file from the build output tree.
-// Since the location of the derived sources depends on which configuration
-// we are building, we need to use the include path ( which can be
-// different for each configuration ) to reference the derived source file.
-#include <DerivedSources/grammar.cpp>
