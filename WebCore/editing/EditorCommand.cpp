eb7cd59288ba1f352cca30c91243c7e6d7d32c0a
Alexey Proskuryakov <ap@webkit.org>
2008-01-02 21:07:26 +0000

    Fixed a typo (pointed out in review, but I somehow missed it at first).
            * editing/EditorCommand.cpp: (WebCore::executeDelete):
    
    
    
    Canonical link: https://commits.webkit.org/22929@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@29090 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/editing/EditorCommand.cpp
+++ WebCore/editing/EditorCommand.cpp
@@ -270,7 +270,7 @@ static bool executeDelete(Frame* frame, Event*, EditorCommandSource source, cons
             return true;
         case CommandFromDOM:
         case CommandFromDOMWithUserInterface:
-            // If the curernt selection is a caret, delete the preceding character. IE performs forwardDelete, but we currently side with Firefox.
+            // If the current selection is a caret, delete the preceding character. IE performs forwardDelete, but we currently side with Firefox.
             // Doesn't scroll to make the selection visible, or modify the kill ring (this time, siding with IE, not Firefox).
             TypingCommand::deleteKeyPressed(frame->document(), frame->selectionGranularity() == WordGranularity);
             return true;
