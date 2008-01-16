WebKit/WebKit

commit 74c8babf4d4580c0a65ec8cda910c28d8bb9e35e

WebCore:
        Reviewed by Mitz.

        - fix http://bugs.webkit.org/show_bug.cgi?id=14955
          Implement getElementsByClassName.

        This patch also renames AtomicStringList to ClassNames to better reflect its actual use,
        and takes advantage of admitting that it's class-specific to encapsulate class attribute
        parsing so it can be shared.  It also changes the class to use a Vector, rather than a linked
        list to store the class names.

        Tests: fast/dom/getElementsByClassName/001.html
               fast/dom/getElementsByClassName/002.html
               fast/dom/getElementsByClassName/003.html
               fast/dom/getElementsByClassName/004.html
               fast/dom/getElementsByClassName/005.html
               fast/dom/getElementsByClassName/006.html
               fast/dom/getElementsByClassName/007.html
               fast/dom/getElementsByClassName/008.html
               fast/dom/getElementsByClassName/009.html
               fast/dom/getElementsByClassName/010.xml
               fast/dom/getElementsByClassName/011.xml
               fast/dom/getElementsByClassName/012.html
               fast/dom/getElementsByClassName/013.html
               fast/dom/getElementsByClassName/014.html
               fast/dom/getElementsByClassName/array/001.html
               fast/dom/getElementsByClassName/array/002.html
               fast/dom/getElementsByClassName/array/003.html
               fast/dom/getElementsByClassName/array/004.html
               fast/dom/getElementsByClassName/dumpNodeList.html

        * WebCore.xcodeproj/project.pbxproj:
        * css/CSSStyleSelector.cpp:
        (WebCore::CSSStyleSelector::matchRules):
        (WebCore::CSSStyleSelector::checkOneSelector):
        * dom/AtomicStringList.h: Removed.
        * dom/ClassNames.cpp: Added.
        (WebCore::ClassNames::contains):
        (WebCore::ClassNames::parseClassAttribute):

WebCore/css/CSSStyleSelector.cpp
@@ -390,8 +390,9 @@ void CSSStyleSelector::matchRules(CSSRuleSet* rules, int& firstRuleIndex, int& l
     if (element->hasID())
         matchRulesForList(rules->getIDRules(element->getIDAttribute().impl()), firstRuleIndex, lastRuleIndex);
     if (element->hasClass()) {
-        for (const AtomicStringList* singleClass = element->getClassList(); singleClass; singleClass = singleClass->next())
-            matchRulesForList(rules->getClassRules(singleClass->string().impl()), firstRuleIndex, lastRuleIndex);
+        const ClassNames& classNames = *element->getClassNames();
+        for (size_t i = 0; i < classNames.size(); ++i)
+            matchRulesForList(rules->getClassRules(classNames[i].impl()), firstRuleIndex, lastRuleIndex);
     }
     matchRulesForList(rules->getTagRules(element->localName().impl()), firstRuleIndex, lastRuleIndex);
     matchRulesForList(rules->getUniversalRules(), firstRuleIndex, lastRuleIndex);
@@ -1431,12 +1432,13 @@ bool CSSStyleSelector::checkOneSelector(CSSSelector* sel, Element* e, bool isAnc
         if (sel->m_match == CSSSelector::Class) {
             if (!e->hasClass())
                 return false;
-            for (const AtomicStringList* c = e->getClassList(); c; c = c->next())
-                if (c->string() == sel->m_value)
+            const ClassNames& classNames = *e->getClassNames();
+            for (size_t i = 0; i < classNames.size(); ++i) {
+                if (classNames[i] == sel->m_value)
                     return true;
+            }
             return false;
-        }
-        else if (sel->m_match == CSSSelector::Id)
+        } else if (sel->m_match == CSSSelector::Id)
             return e->hasID() && e->getIDAttribute() == sel->m_value;
         else if (style && (e != element || !styledElement || (!styledElement->isMappedAttribute(sel->m_attr) && sel->m_attr != typeAttr && sel->m_attr != readonlyAttr))) {
             style->setAffectedByAttributeSelectors(); // Special-case the "type" and "readonly" attributes so input form controls can share style.

WebCore/dom/ClassNames.cpp
@@ -1,0 +1,69 @@
+/*
+ * Copyright (C) 2007 David Smith (catfish.man@gmail.com)
+ * Copyright (C) 2007 Apple Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "config.h"
+#include "ClassNames.h"
+
+namespace WebCore {
+    
+bool ClassNames::contains(const AtomicString& str) const 
+{
+    if (!m_nameVector)
+        return false;
+
+    for (size_t i = 0; i < m_nameVector->size(); ++i) {
+        if ((*m_nameVector)[i] == str)
+            return true;
+    }
+
+    return false;
+}
+
+void ClassNames::parseClassAttribute(const String& classStr, const bool inCompatMode)
+{
+    if (!m_nameVector)
+        m_nameVector.set(new Vector<AtomicString>);
+    else
+        m_nameVector->clear();
+
+    if (classStr.isEmpty())
+        return;
+
+    String classAttr = inCompatMode ? classStr.foldCase() : classStr;
+
+    const UChar* str = classAttr.characters();
+    const int length = classAttr.length();
+    int start = 0;
+    while (true) {
+        while (start < length && isClassWhitespace(str[start]))
+            ++start;
+        if (start >= length)
+            break;
+        int end = start + 1;
+        while (end < length && !isClassWhitespace(str[end]))
+            ++end;
+
+        m_nameVector->append(AtomicString(str + start, end - start));
+
+        start = end + 1;
+    }
+}
+
+} // namespace WebCore

WebCore/dom/ClassNames.h
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2007 Apple Computer, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef ClassNames_h
+#define ClassNames_h
+
+#include "AtomicString.h"
+#include <wtf/Vector.h>
+#include <wtf/OwnPtr.h>
+
+namespace WebCore {
+
+    class ClassNames {
+    public:
+        ClassNames()
+        {
+        }
+
+        bool contains(const AtomicString&) const;
+        void parseClassAttribute(const String&, const bool inCompatMode);
+
+        size_t size() const { return m_nameVector ? m_nameVector->size() : 0; }
+        void clear() { if (m_nameVector) m_nameVector->clear(); }
+        const AtomicString& operator[](size_t i) const { return m_nameVector ? (*m_nameVector)[i] : nullAtom; }
+
+    private:
+        OwnPtr<Vector<AtomicString> > m_nameVector;
+    };
+
+    inline static bool isClassWhitespace(UChar c)
+    {
+        return c == ' ' || c == '\r' || c == '\n' || c == '\t' || c == '\f';
+    }
+
+} // namespace WebCore
+
+#endif // ClassNames_h

commit 37efa10b122ebd920221912438d1328a015626ec

Reviewed by Darin.
        Fix for <rdar://problem/5671040>
        REGRESSION: 6% HTML iBench regression from r28722 (getElementsByClassName)

        On my most consistent tests, this brings the HTML iBench from 1.46 -> 1.41,
        which does not completely make up reported regression, but I was not able to
        reproduce those findings either.

        * css/CSSStyleSelector.cpp:
        (WebCore::CSSStyleSelector::matchRules):
        (WebCore::CSSStyleSelector::checkOneSelector):
        * dom/ClassNames.cpp:
        (WebCore::ClassNames::parseClassAttribute):
        * dom/ClassNames.h:
        (WebCore::ClassNames::contains):
        (WebCore::ClassNames::operator[]):



Canonical link: https://commits.webkit.org/23334@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@29502 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/css/CSSStyleSelector.cpp
@@ -390,7 +390,8 @@ void CSSStyleSelector::matchRules(CSSRuleSet* rules, int& firstRuleIndex, int& l
         matchRulesForList(rules->getIDRules(m_element->getIDAttribute().impl()), firstRuleIndex, lastRuleIndex);
     if (m_element->hasClass()) {
         const ClassNames& classNames = *m_element->getClassNames();
-        for (size_t i = 0; i < classNames.size(); ++i)
+        size_t classNamesSize = classNames.size();
+        for (size_t i = 0; i < classNamesSize; ++i)
             matchRulesForList(rules->getClassRules(classNames[i].impl()), firstRuleIndex, lastRuleIndex);
     }
     matchRulesForList(rules->getTagRules(m_element->localName().impl()), firstRuleIndex, lastRuleIndex);
@@ -1500,12 +1501,7 @@ bool CSSStyleSelector::checkOneSelector(CSSSelector* sel, Element* e, bool isAnc
         if (sel->m_match == CSSSelector::Class) {
             if (!e->hasClass())
                 return false;
-            const ClassNames& classNames = *e->getClassNames();
-            for (size_t i = 0; i < classNames.size(); ++i) {
-                if (classNames[i] == sel->m_value)
-                    return true;
-            }
-            return false;
+            return e->getClassNames()->contains(sel->m_value);
         } else if (sel->m_match == CSSSelector::Id)
             return e->hasID() && e->getIDAttribute() == sel->m_value;
         else if (m_style && (e != m_element || !m_styledElement || (!m_styledElement->isMappedAttribute(sel->m_attr) && sel->m_attr != typeAttr && sel->m_attr != readonlyAttr))) {

WebCore/dom/ClassNames.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2007 David Smith (catfish.man@gmail.com)
- * Copyright (C) 2007 Apple Inc. All rights reserved.
+ * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -22,24 +22,11 @@
 #include "ClassNames.h"
 
 namespace WebCore {
-    
-bool ClassNames::contains(const AtomicString& str) const 
-{
-    if (!m_nameVector)
-        return false;
-
-    for (size_t i = 0; i < m_nameVector->size(); ++i) {
-        if ((*m_nameVector)[i] == str)
-            return true;
-    }
-
-    return false;
-}
 
 void ClassNames::parseClassAttribute(const String& classStr, const bool inCompatMode)
 {
     if (!m_nameVector)
-        m_nameVector.set(new Vector<AtomicString>);
+        m_nameVector.set(new ClassNameVector);
     else
         m_nameVector->clear();
 

WebCore/dom/ClassNames.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007 Apple Computer, Inc.
+ * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -28,20 +28,34 @@
 namespace WebCore {
 
     class ClassNames {
+        typedef Vector<AtomicString, 8> ClassNameVector;
     public:
         ClassNames()
         {
         }
 
-        bool contains(const AtomicString&) const;
+        bool contains(const AtomicString& str) const
+        {
+            if (!m_nameVector)
+                return false;
+
+            size_t size = m_nameVector->size();
+            for (size_t i = 0; i < size; ++i) {
+                if (m_nameVector->at(i) == str)
+                    return true;
+            }
+
+            return false;
+        }
+
         void parseClassAttribute(const String&, const bool inCompatMode);
 
         size_t size() const { return m_nameVector ? m_nameVector->size() : 0; }
         void clear() { if (m_nameVector) m_nameVector->clear(); }
-        const AtomicString& operator[](size_t i) const { return m_nameVector ? (*m_nameVector)[i] : nullAtom; }
+        const AtomicString& operator[](size_t i) const { ASSERT(m_nameVector); return m_nameVector->at(i); }
 
     private:
-        OwnPtr<Vector<AtomicString> > m_nameVector;
+        OwnPtr<ClassNameVector> m_nameVector;
     };
 
     inline static bool isClassWhitespace(UChar c)