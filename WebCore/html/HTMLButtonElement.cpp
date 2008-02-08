9d58980d1607a8791b6fa719a87419a4de42df97
David Hyatt <hyatt@apple.com>
2008-02-08 19:23:08 +0000

    Fix for bug 16798, button should default to type=submit.
            Reviewed by darin
    
            * html/HTMLButtonElement.cpp:
            (WebCore::HTMLButtonElement::type):
            (WebCore::HTMLButtonElement::parseMappedAttribute):
    
    
    
    Canonical link: https://commits.webkit.org/23906@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@30095 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/html/HTMLButtonElement.cpp
+++ WebCore/html/HTMLButtonElement.cpp
@@ -56,18 +56,34 @@ RenderObject* HTMLButtonElement::createRenderer(RenderArena* arena, RenderStyle*
 
 const AtomicString& HTMLButtonElement::type() const
 {
-    return getAttribute(typeAttr);
+    switch (m_type) {
+        case SUBMIT: {
+            static const AtomicString submit("submit");
+            return submit;
+        }
+        case BUTTON: {
+            static const AtomicString button("button");
+            return button;
+        }
+        case RESET: {
+            static const AtomicString reset("reset");
+            return reset;
+        }
+    }
+
+    ASSERT_NOT_REACHED();
+    return emptyAtom;
 }
 
 void HTMLButtonElement::parseMappedAttribute(MappedAttribute* attr)
 {
     if (attr->name() == typeAttr) {
-        if (equalIgnoringCase(attr->value(), "submit"))
-            m_type = SUBMIT;
-        else if (equalIgnoringCase(attr->value(), "reset"))
+        if (equalIgnoringCase(attr->value(), "reset"))
             m_type = RESET;
         else if (equalIgnoringCase(attr->value(), "button"))
             m_type = BUTTON;
+        else
+            m_type = SUBMIT;
     } else if (attr->name() == alignAttr) {
         // Don't map 'align' attribute.  This matches what Firefox and IE do, but not Opera.
         // See http://bugs.webkit.org/show_bug.cgi?id=12071
