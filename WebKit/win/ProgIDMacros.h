7937bfbf89fc4978acaac99bf10efb94cb277281
Steve Falkenburg <sfalken@apple.com>
2008-02-14 00:15:30 +0000

    Versioning.
    Canonical link: https://commits.webkit.org/24025@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@30217 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/win/ProgIDMacros.h
+++ WebKit/win/ProgIDMacros.h
@@ -29,7 +29,7 @@
 #define STRINGIFIED_VERSION(version) STRINGIFY(version)
 #define STRINGIFY(s) L###s
 
-#define CURRENT_PROGID_VERSION 525
+#define CURRENT_PROGID_VERSION 526
 #define VERSION_INDEPENDENT_PRODUCTION_PROGID(className) L##"WebKit." L###className
 #define VERSION_INDEPENDENT_OPENSOURCE_PROGID(className) L##"OpenSource" VERSION_INDEPENDENT_PRODUCTION_PROGID(className)
 #define VERSIONED_PROGID(versionIndependentProgID, version) versionIndependentProgID L##"." STRINGIFIED_VERSION(version)
