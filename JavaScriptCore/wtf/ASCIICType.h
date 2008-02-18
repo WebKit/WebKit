7771e2d6e0b46833bd5da294cf7bf3624c9910b6
Darin Adler <darin@apple.com>
2008-02-18 21:28:21 +0000

    Reviewed by Sam.
            * wtf/ASCIICType.h:
            (WTF::toASCIIHexValue): Added.
    
    
    
    Canonical link: https://commits.webkit.org/24114@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@30380 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/ASCIICType.h
+++ JavaScriptCore/wtf/ASCIICType.h
@@ -86,19 +86,18 @@ namespace WTF {
     inline bool isASCIIUpper(int c) { return c >= 'A' && c <= 'Z'; }
 
     /*
-        Statistics from a run of the PLT on the usage of isASCIISpace:
-        Hex  Name               Count
-        ---  ----               -----
-           ALL OTHER VALUES     689383
-        x20  SPACE              294720
-        x0A  NEWLINE            89059
-        x09  TAB                28320
-        x0D  CARRIAGE RETURN    0
-        x0C  FORMFEED           0
-        x0B  VERTICAL TAB       0
+        Statistics from a run of Apple's page load test for callers of isASCIISpace:
 
+            character          count
+            ---------          -----
+            non-spaces         689383
+        20  space              294720
+        0A  \n                 89059
+        09  \t                 28320
+        0D  \r                 0
+        0C  \f                 0
+        0B  \v                 0
     */
-
     inline bool isASCIISpace(char c) { return c <= ' ' && (c == ' ' || (c <= 0xD && c >= 0x9)); }
     inline bool isASCIISpace(unsigned short c) { return c <= ' ' && (c == ' ' || (c <= 0xD && c >= 0x9)); }
 #if !COMPILER(MSVC) || defined(_NATIVE_WCHAR_T_DEFINED)
@@ -119,6 +118,13 @@ namespace WTF {
     inline wchar_t toASCIIUpper(wchar_t c) { return static_cast<wchar_t>(c & ~((c >= 'a' && c <= 'z') << 5)); }
 #endif
     inline int toASCIIUpper(int c) { return static_cast<int>(c & ~((c >= 'a' && c <= 'z') << 5)); }
+
+    inline int toASCIIHexValue(char c) { ASSERT(isASCIIHexDigit(c)); return c < 'A' ? c - '0' : (c - 'A' + 10) & 0xF; }
+    inline int toASCIIHexValue(unsigned short c) { ASSERT(isASCIIHexDigit(c)); return c < 'A' ? c - '0' : (c - 'A' + 10) & 0xF; }
+#if !COMPILER(MSVC) || defined(_NATIVE_WCHAR_T_DEFINED)
+    inline int toASCIIHexValue(wchar_t c) { ASSERT(isASCIIHexDigit(c)); return c < 'A' ? c - '0' : (c - 'A' + 10) & 0xF; }
+#endif
+    inline int toASCIIHexValue(int c) { ASSERT(isASCIIHexDigit(c)); return c < 'A' ? c - '0' : (c - 'A' + 10) & 0xF; }
 
 }
 
