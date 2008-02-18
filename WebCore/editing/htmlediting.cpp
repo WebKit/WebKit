2e5993296219aa6d15d3e12a2788bb43d9a0662e
Darin Adler <darin@apple.com>
2008-02-18 21:20:32 +0000

    Reviewed by Sam.
            * editing/htmlediting.cpp:
            (WebCore::stringWithRebalancedWhitespace): Changed to use String instead of
            DeprecatedString.
    
    
    
    Canonical link: https://commits.webkit.org/24113@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@30379 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/editing/htmlediting.cpp
+++ WebCore/editing/htmlediting.cpp
@@ -367,11 +367,11 @@ int maxDeepOffset(const Node *node)
 
 String stringWithRebalancedWhitespace(const String& string, bool startIsStartOfParagraph, bool endIsEndOfParagraph)
 {
-    static DeprecatedString twoSpaces("  ");
-    static DeprecatedString nbsp("\xa0");
-    static DeprecatedString pattern(" \xa0");
+    static String twoSpaces("  ");
+    static String nbsp("\xa0");
+    static String pattern(" \xa0");
 
-    DeprecatedString rebalancedString = string.deprecatedString();
+    String rebalancedString = string;
 
     rebalancedString.replace(noBreakSpace, ' ');
     rebalancedString.replace('\n', ' ');
@@ -385,7 +385,7 @@ String stringWithRebalancedWhitespace(const String& string, bool startIsStartOfP
     if (endIsEndOfParagraph && rebalancedString[end] == ' ')
         rebalancedString.replace(end, 1, nbsp);    
 
-    return String(rebalancedString);
+    return rebalancedString;
 }
 
 bool isTableStructureNode(const Node *node)
