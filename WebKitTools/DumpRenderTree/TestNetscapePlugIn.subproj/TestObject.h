034f99b630d710c4e565818849ee44022f13e86c
David Kilzer <ddkilzer@webkit.org>
2008-02-23 17:08:57 +0000

    WebCore:
            Please clarify licensing for some files
            <http://bugs.webkit.org/show_bug.cgi?id=14970>
    
            Reviewed by Darin.
    
            * bindings/objc/WebScriptObject.h: Added Apple BSD-style license.
            * bindings/objc/WebScriptObjectPrivate.h: Ditto.
            * platform/text/mac/ShapeArabic.c: Added ICU license from WebCore/icu/LICENSE.
    
    WebKit/mac:
    
            Please clarify licensing for some files
            <http://bugs.webkit.org/show_bug.cgi?id=14970>
    
            Reviewed by Darin.
    
            * Plugins/WebNetscapeDeprecatedFunctions.c: Updated copyright statement
            and added Apple BSD-style license.
            * Plugins/WebNetscapeDeprecatedFunctions.h: Ditto.
    
    WebKitTools:
    
            Please clarify licensing for some files
            <http://bugs.webkit.org/show_bug.cgi?id=14970>
    
            Reviewed by Darin.
    
            * DumpRenderTree/TestNetscapePlugIn.subproj/PluginObject.cpp: Added
            copyright statement.  Replaced license with newer Apple BSD-style license.
            * DumpRenderTree/TestNetscapePlugIn.subproj/PluginObject.h: Ditto.
            * DumpRenderTree/TestNetscapePlugIn.subproj/TestObject.cpp: Ditto.
            * DumpRenderTree/TestNetscapePlugIn.subproj/TestObject.h: Ditto.
            * DumpRenderTree/TestNetscapePlugIn.subproj/main.cpp: Ditto.
            * DumpRenderTree/win/TestNetscapePlugin/main.c: Ditto.
            * mangleme/LICENSE: Added (LGPL).
    
    
    
    Canonical link: https://commits.webkit.org/24231@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@30520 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/DumpRenderTree/TestNetscapePlugIn.subproj/TestObject.h
+++ WebKitTools/DumpRenderTree/TestNetscapePlugIn.subproj/TestObject.h
@@ -1,34 +1,26 @@
 /*
- IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc. ("Apple") in
- consideration of your agreement to the following terms, and your use, installation, 
- modification or redistribution of this Apple software constitutes acceptance of these 
- terms.  If you do not agree with these terms, please do not use, install, modify or 
- redistribute this Apple software.
- 
- In consideration of your agreement to abide by the following terms, and subject to these 
- terms, Apple grants you a personal, non-exclusive license, under Apple’s copyrights in 
- this original Apple software (the "Apple Software"), to use, reproduce, modify and 
- redistribute the Apple Software, with or without modifications, in source and/or binary 
- forms; provided that if you redistribute the Apple Software in its entirety and without 
- modifications, you must retain this notice and the following text and disclaimers in all 
- such redistributions of the Apple Software.  Neither the name, trademarks, service marks 
- or logos of Apple Computer, Inc. may be used to endorse or promote products derived from 
- the Apple Software without specific prior written permission from Apple. Except as expressly
- stated in this notice, no other rights or licenses, express or implied, are granted by Apple
- herein, including but not limited to any patent rights that may be infringed by your 
- derivative works or by other works in which the Apple Software may be incorporated.
- 
- The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO WARRANTIES, 
- EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, 
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS 
- USE AND OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
- 
- IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL 
- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS 
- OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, 
- REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND 
- WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR 
- OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2007 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
 #include <WebKit/npapi.h>
