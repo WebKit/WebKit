0a667f2bfedafe7fabae476991a8065431ddec5f
Alp Toker <alp@webkit.org>
2008-03-06 03:49:21 +0000

    2008-03-05  Alp Toker  <alp@atoker.com>
            Attempt to get the autotools build working again on the bot with
            build-webkit.
    
            Force the use of /bin/bash since we seem to have bash-isms in the
            configure script right now.
    
            * Scripts/webkitdirs.pm:
    
    
    Canonical link: https://commits.webkit.org/24506@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@30818 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/webkitdirs.pm
+++ WebKitTools/Scripts/webkitdirs.pm
@@ -764,10 +764,19 @@ sub buildAutotoolsProject($@)
         return 0;
     }
 
+    print "Calling autogen.sh in " . $dir . "\n\n";
+    print "Installation directory: $prefix\n" if(defined($prefix));
+
+    $result = system "$sourceDir/autogen.sh --help";
+    if ($result ne 0) {
+        die "Failed to setup build environment using 'autotools'!\n";
+    }
+
     print "Calling configure in " . $dir . "\n\n";
     print "Installation directory: $prefix\n" if(defined($prefix));
-     
-    $result = system "$sourceDir/autogen.sh", @buildArgs;
+
+    # The autotools configure script requires bash explicitly at time of writing
+    $result = system "/bin/bash", "$sourceDir/configure", @buildArgs;
     if ($result ne 0) {
         die "Failed to setup build environment using 'autotools'!\n";
     }
