57e67c9a7d73dfee49a2c71fc82adcfc1f8e2bd2
Antti Koivisto <antti@apple.com>
2008-04-11 05:26:49 +0000

    2008-04-10  Antti Koivisto  <antti@apple.com>
            Reviewed by Eric.
    
            Move CurveData out from AnimationController.cpp and rename
            it to UnitBezier. This makes it possible to reuse this nice class from the
            SVG animation code.
    
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * page/AnimationController.cpp:
            (WebCore::solveCubicBezierFunction):
            * platform/graphics/UnitBezier.h: Added.
            (WebCore::UnitBezier::UnitBezier):
            (WebCore::UnitBezier::sampleCurveX):
            (WebCore::UnitBezier::sampleCurveY):
            (WebCore::UnitBezier::sampleCurveDerivativeX):
            (WebCore::UnitBezier::solveCurveX):
            (WebCore::UnitBezier::solve):
    
    
    
    Canonical link: https://commits.webkit.org/25339@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@31797 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/page/AnimationController.cpp
+++ WebCore/page/AnimationController.cpp
@@ -37,97 +37,13 @@
 #include "RenderStyle.h"
 #include "SystemTime.h"
 #include "Timer.h"
+#include "UnitBezier.h"
 
 namespace WebCore {
 
 static const double cAnimationTimerDelay = 0.025;
 
-struct CurveData {
-    CurveData(double p1x, double p1y, double p2x, double p2y)
-    {
-        // Calculate the polynomial coefficients, implicit first and last control points are (0,0) and (1,1).
-        cx = 3.0 * p1x;
-        bx = 3.0 * (p2x - p1x) - cx;
-        ax = 1.0 - cx -bx;
-         
-        cy = 3.0 * p1y;
-        by = 3.0 * (p2y - p1y) - cy;
-        ay = 1.0 - cy - by;
-    }
-    
-    double sampleCurveX(double t)
-    {
-        // `ax t^3 + bx t^2 + cx t' expanded using Horner's rule.
-        return ((ax * t + bx) * t + cx) * t;
-    }
-    
-    double sampleCurveY(double t)
-    {
-        return ((ay * t + by) * t + cy) * t;
-    }
-    
-    double sampleCurveDerivativeX(double t)
-    {
-        return (3.0 * ax * t + 2.0 * bx) * t + cx;
-    }
-    
-    // Given an x value, find a parametric value it came from.
-    double solveCurveX(double x, double epsilon)
-    {
-        double t0;
-        double t1;
-        double t2;
-        double x2;
-        double d2;
-        int i;
-
-        // First try a few iterations of Newton's method -- normally very fast.
-        for (t2 = x, i = 0; i < 8; i++) {
-            x2 = sampleCurveX(t2) - x;
-            if (fabs (x2) < epsilon)
-                return t2;
-            d2 = sampleCurveDerivativeX(t2);
-            if (fabs(d2) < 1e-6)
-                break;
-            t2 = t2 - x2 / d2;
-        }
-
-        // Fall back to the bisection method for reliability.
-        t0 = 0.0;
-        t1 = 1.0;
-        t2 = x;
-
-        if (t2 < t0)
-            return t0;
-        if (t2 > t1)
-            return t1;
-
-        while (t0 < t1) {
-            x2 = sampleCurveX(t2);
-            if (fabs(x2 - x) < epsilon)
-                return t2;
-            if (x > x2)
-                t0 = t2;
-            else
-                t1 = t2;
-            t2 = (t1 - t0) * .5 + t0;
-        }
-
-        // Failure.
-        return t2;
-    }
-    
-private:
-    double ax;
-    double bx;
-    double cx;
-    
-    double ay;
-    double by;
-    double cy;
-};
-
-// The epsilon value we pass to solveCurveX given that the animation is going to run over |dur| seconds. The longer the
+// The epsilon value we pass to UnitBezier::solve given that the animation is going to run over |dur| seconds. The longer the
 // animation, the more precision we need in the timing function result to avoid ugly discontinuities.
 static inline double solveEpsilon(double duration) { return 1. / (200. * duration); }
 
@@ -135,10 +51,8 @@ static inline double solveCubicBezierFunction(double p1x, double p1y, double p2x
 {
     // Convert from input time to parametric value in curve, then from
     // that to output time.
-    CurveData c(p1x, p1y, p2x, p2y);
-    t = c.solveCurveX(t, solveEpsilon(duration));
-    t = c.sampleCurveY(t);
-    return t;
+    UnitBezier bezier(p1x, p1y, p2x, p2y);
+    return bezier.solve(t, solveEpsilon(duration));
 }
 
 class CompositeImplicitAnimation;
