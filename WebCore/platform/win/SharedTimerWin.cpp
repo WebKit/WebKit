6b0b32e175bb13a646936b2bf7528eee36a7936b
Steve Falkenburg <sfalken@apple.com>
2008-04-16 20:32:31 +0000

    2008-04-16  Steve Falkenburg  <sfalken@apple.com>
            Improve timer latency/resolution.
    
            The primary timing mechanism is now CreateTimerQueueTimer, with a PostMessage from
            the callback proc.  This event will be delivered sooner than if we used SetTimer(),
            since it is higher priority than other messages.
    
            If there is an input message pending, we'll use we SetTimer() based timers to avoid
            starving user input.
    
            Don't call PostMessage immediately on all timers < 10ms.  Only call immediately if
            timer is already expired.  This avoids the case where no timers would be processed,
            causing the timer to be re-posted via the slower SetTimer method.
    
            Don't call PostMessage immediately if there's a pending WM_PAINT, since
            we don't want to starve all paints when bombarded with a continuous stream of low
            duration timers.
    
            Removed reentrancy check, since we're now looking for specific starvation cases,
            and will typically defer non-expired timers through reateTimerQueueTimer.
    
            The reason Windows wouldn't starve paints in favor of timers with the old code:
            When we went to reset the timer when it fired, the timer reset code treated this as a
            reentrant timer, causing us to hit the slow SetTimer code rather than calling PostMessage,
            even for timers expiring in < 10ms. I corrected that, exposing the paint starvation problem.
    
            Use timeBeginPeriod/timeEndPeriod to improve resolution of system timers.
            Limit this resolution change to within 20ms of the last high resolution timer request.
    
            Corrected GetQueueStatus to check for specific input (and paint) messages rather than
            checking QS_ALLINPUT.  Contrary to its name, QS_ALLINPUT actually indicates interest
            in ALL queued messages, not just input messages.
    
            Fix SetTimer/KillTimer calls to pass our window handle.
    
            Reviewed by Brady.
    
            * platform/win/SharedTimerWin.cpp:
            (WebCore::):
            (WebCore::TimerWindowWndProc):
            (WebCore::queueTimerProc):
            (WebCore::setSharedTimerFireTime):
            (WebCore::stopSharedTimer):
    
    
    
    Canonical link: https://commits.webkit.org/25475@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@31960 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/win/SharedTimerWin.cpp
+++ WebCore/platform/win/SharedTimerWin.cpp
@@ -39,15 +39,17 @@
 #endif
 
 #include <windows.h>
+#include <mmsystem.h>
 
 // These aren't in winuser.h with the MSVS 2003 Platform SDK, 
 // so use default values in that case.
-#ifndef USER_TIMER_MINIMUM
-#define USER_TIMER_MINIMUM 0x0000000A
-#endif
 
 #ifndef USER_TIMER_MAXIMUM
 #define USER_TIMER_MAXIMUM 0x7FFFFFFF
+#endif
+
+#ifndef QS_RAWINPUT
+#define QS_RAWINPUT         0x0400
 #endif
 
 #if PLATFORM(WIN)
@@ -61,9 +63,20 @@ static void (*sharedTimerFiredFunction)();
 
 static HWND timerWindowHandle = 0;
 static UINT timerFiredMessage = 0;
+static HANDLE timerQueue;
+static HANDLE timer;
+static bool highResTimerActive;
+
 const LPCWSTR kTimerWindowClassName = L"TimerWindowClass";
-static bool processingCustomTimerMessage = false;
-const int sharedTimerID = 1000;
+const int timerResolution = 1;
+const int highResolutionThresholdMsec = 16;
+const int stopHighResTimerInMsec = 20;
+
+enum {
+    sharedTimerID = 1000,
+    endHighResTimerID = 1001
+};
+
 
 LRESULT CALLBACK TimerWindowWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -78,14 +91,20 @@ LRESULT CALLBACK TimerWindowWndProc(HWND hWnd, UINT message, WPARAM wParam, LPAR
     }
 #endif
 
-    if (message == timerFiredMessage) {
-        processingCustomTimerMessage = true;
+    if (message == timerFiredMessage)
         sharedTimerFiredFunction();
-        processingCustomTimerMessage = false;
-    } else if (message == WM_TIMER && wParam == sharedTimerID)
-        sharedTimerFiredFunction();
-    else
+    else if (message == WM_TIMER) {
+        if (wParam == sharedTimerID) {
+            KillTimer(timerWindowHandle, sharedTimerID);
+            sharedTimerFiredFunction();
+        } else if (wParam == endHighResTimerID) {
+            KillTimer(timerWindowHandle, endHighResTimerID);
+            highResTimerActive = false;
+            timeEndPeriod(timerResolution);
+        }
+    } else
         return DefWindowProc(hWnd, message, wParam, lParam);
+
     return 0;
 }
 
@@ -112,6 +131,12 @@ void setSharedTimerFiredFunction(void (*f)())
     sharedTimerFiredFunction = f;
 }
 
+static void NTAPI queueTimerProc(PVOID, BOOLEAN)
+{
+    timer = 0;
+    PostMessage(timerWindowHandle, timerFiredMessage, 0, 0);
+}
+
 void setSharedTimerFireTime(double fireTime)
 {
     ASSERT(sharedTimerFiredFunction);
@@ -128,29 +153,56 @@ void setSharedTimerFireTime(double fireTime)
             intervalInMS = (unsigned)interval;
     }
 
-    if (timerID) {
-        KillTimer(0, timerID);
-        timerID = 0;
+    if (interval < highResolutionThresholdMsec) {
+        if (!highResTimerActive) {
+            highResTimerActive = true;
+            timeBeginPeriod(timerResolution);
+        }
+        SetTimer(timerWindowHandle, endHighResTimerID, stopHighResTimerInMsec, 0);
     }
 
-    // We don't allow nested PostMessages, since the custom messages will effectively starve
-    // painting and user input. (Win32 has a tri-level queue with application messages > 
-    // user input > WM_PAINT/WM_TIMER.)
-    // In addition, if the queue contains input events that have been there since the last call to
-    // GetQueueStatus, PeekMessage or GetMessage we favor timers.
     initializeOffScreenTimerWindow();
-    if (intervalInMS < USER_TIMER_MINIMUM && !processingCustomTimerMessage && 
-        !LOWORD(::GetQueueStatus(QS_ALLINPUT))) {
-        // Windows SetTimer does not allow timeouts smaller than 10ms (USER_TIMER_MINIMUM)
-        PostMessage(timerWindowHandle, timerFiredMessage, 0, 0);
+    bool timerSet = false;
+    DWORD queueStatus = LOWORD(GetQueueStatus(QS_PAINT | QS_MOUSEBUTTON | QS_KEY | QS_RAWINPUT));
+
+    // Win32 has a tri-level queue with application messages > user input > WM_PAINT/WM_TIMER.
+
+    // If the queue doesn't contains input events, we use a higher priorty timer event posting mechanism.
+    if (!(queueStatus & (QS_MOUSEBUTTON | QS_KEY | QS_RAWINPUT))) {
+
+        if (!intervalInMS && !(queueStatus & QS_PAINT)) {
+            // Call PostMessage immediately if the timer is already expired, unless a paint is pending.
+            // (we prioritize paints over timers)
+            PostMessage(timerWindowHandle, timerFiredMessage, 0, 0);
+            timerSet = true;
+        } else {
+            // Otherwise, delay the PostMessage via a CreateTimerQueueTimer
+            if (!timerQueue)
+                timerQueue = CreateTimerQueue();
+            if (timer)
+                timerSet = ChangeTimerQueueTimer(timerQueue, timer, intervalInMS, 0);
+            else
+                timerSet = CreateTimerQueueTimer(&timer, timerQueue, queueTimerProc, 0, intervalInMS, 0, WT_EXECUTEINTIMERTHREAD);
+        }
+    }
+
+    if (timerSet) {
+        if (timerID) {
+            KillTimer(timerWindowHandle, timerID);
+            timerID = 0;
+        }
     } else
         timerID = SetTimer(timerWindowHandle, sharedTimerID, intervalInMS, 0);
 }
 
 void stopSharedTimer()
 {
+    if (timer) {
+        DeleteTimerQueueTimer(timerQueue, timer, 0);
+        timer = 0;
+    }
     if (timerID) {
-        KillTimer(0, timerID);
+        KillTimer(timerWindowHandle, timerID);
         timerID = 0;
     }
 }
