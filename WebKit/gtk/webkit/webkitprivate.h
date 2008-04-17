d453f65e8ac805be8ef78d7450c1d4d9d477ee67
Alp Toker <alp@webkit.org>
2008-04-17 18:49:31 +0000

    2008-04-17  Daniele Metilli  <daniele.metilli@gmail.com>
            Reviewed by Eric.
    
            Fixed a typo in the copyright header.
    
            * webkit/webkitprivate.h:
    
    
    Canonical link: https://commits.webkit.org/25512@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@32005 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/gtk/webkit/webkitprivate.h
+++ WebKit/gtk/webkit/webkitprivate.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2007, 2008 Holger Hans Peter Freyther
- * Copyrifht (C) 2008 Jan Michael C. Alonzo
+ * Copyright (C) 2008 Jan Michael C. Alonzo
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
