0ab4301c02edc88ed5efcf820deed99f57943170
Julien Chaffraix <jchaffraix@webkit.org>
2008-04-21 15:39:56 +0000

    2008-04-21  Julien Chaffraix  <jchaffraix@webkit.org>
            Not reviewed, add missing files in my previous commit.
    
    
    
    Canonical link: https://commits.webkit.org/25659@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@32318 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/xml/XMLHttpRequestProgressEvent.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2008 Julien Chaffraix <jchaffraix@webkit.org>. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef XMLHttpRequestProgressEvent_h
+#define XMLHttpRequestProgressEvent_h
+
+#include "ProgressEvent.h"
+
+namespace WebCore {
+
+    class XMLHttpRequestProgressEvent : public ProgressEvent {
+    public:
+        XMLHttpRequestProgressEvent(const AtomicString& type, bool lengthComputable, unsigned loaded, unsigned total);
+        XMLHttpRequestProgressEvent();
+
+        virtual bool isXMLHttpRequestProgressEvent() const { return true; }
+
+        // Avoid matching ProgressEvent.
+        virtual bool isProgressEvent() const { return false; }
+
+        bool lengthComputable() const { return m_lengthComputable; }
+        unsigned loaded() const { return m_loaded; }
+        unsigned total() const { return m_total; }
+
+        // Those 2 methods are to be compatible with Firefox and are only a wrapper on top of the real implementation.
+        // FIXME: We should print a warning as they are deprecated.
+        unsigned position();
+        unsigned totalSize();
+
+    }; // class XMLHttpRequestProgressEvent
+
+} // Namespace WebCore
+
+#endif // XMLHttpRequestProgressEvent_h
