WebKit/WebKit

commit 235ca84d4bb60f76a2dd85d6356e54e8e9c87ebc

2008-05-21  Maciej Stachowiak  <mjs@apple.com>
        Reviewed by Oliver.
        
        - check property map before symbol table in JSGlobalObject::getOwnPropertySlot
        0.5% speedup on SunSpider

        * kjs/JSGlobalObject.h:
        (KJS::JSGlobalObject::getOwnPropertySlot): Check property map before symbol table
        because symbol table access is likely to have been optimized.



git-svn-id: http://svn.webkit.org/repository/webkit/branches/squirrelfish@33961 268f45cc-cd09-0410-ab3c-d52691b4dbfc

JavaScriptCore/kjs/JSGlobalObject.h
@@ -302,16 +302,16 @@ namespace KJS {
 
     inline bool JSGlobalObject::getOwnPropertySlot(ExecState* exec, const Identifier& propertyName, PropertySlot& slot)
     {
-        if (symbolTableGet(propertyName, slot))
+        if (JSVariableObject::getOwnPropertySlot(exec, propertyName, slot))
             return true;
-        return JSVariableObject::getOwnPropertySlot(exec, propertyName, slot);
+        return symbolTableGet(propertyName, slot);
     }
 
     inline bool JSGlobalObject::getOwnPropertySlot(ExecState* exec, const Identifier& propertyName, PropertySlot& slot, bool& slotIsWriteable)
     {
-        if (symbolTableGet(propertyName, slot, slotIsWriteable))
+        if (JSVariableObject::getOwnPropertySlotForWrite(exec, propertyName, slot, slotIsWriteable))
             return true;
-        return JSVariableObject::getOwnPropertySlotForWrite(exec, propertyName, slot, slotIsWriteable);
+        return symbolTableGet(propertyName, slot, slotIsWriteable);
     }
 
     inline bool JSGlobalObject::timedOut()

commit 2a73d054fb3631eb6fb2fb12e3cb420d54cce1de

2008-05-21  Geoffrey Garen  <ggaren@apple.com>
        Reviewed by Mark Rowe.

        Fix layout test failure in fast/dom/getter-on-window-object2 introduced in r33961.

        * JavaScriptCore.exp:
        * kjs/JSGlobalObject.cpp:
        (KJS::JSGlobalObject::defineGetter):
        (KJS::JSGlobalObject::defineSetter):
        * kjs/JSGlobalObject.h:


Canonical link: https://commits.webkit.org/26495@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@33980 268f45cc-cd09-0410-ab3c-d52691b4dbfc

JavaScriptCore/JavaScriptCore.exp
@@ -112,6 +112,8 @@ __ZN3KJS13CodeGenerator21setDumpsGeneratedCodeEb
 __ZN3KJS13StatementNode6setLocEii
 __ZN3KJS13jsOwnedStringERKNS_7UStringE
 __ZN3KJS14JSGlobalObject10globalExecEv
+__ZN3KJS14JSGlobalObject12defineGetterEPNS_9ExecStateERKNS_10IdentifierEPNS_8JSObjectE
+__ZN3KJS14JSGlobalObject12defineSetterEPNS_9ExecStateERKNS_10IdentifierEPNS_8JSObjectE
 __ZN3KJS14JSGlobalObject16stopTimeoutCheckEv
 __ZN3KJS14JSGlobalObject17putWithAttributesEPNS_9ExecStateERKNS_10IdentifierEPNS_7JSValueEj
 __ZN3KJS14JSGlobalObject17startTimeoutCheckEv
@@ -198,6 +200,8 @@ __ZN3KJS8JSObject4callEPNS_9ExecStateEPS0_RKNS_4ListE
 __ZN3KJS8JSObject4markEv
 __ZN3KJS8JSObject9constructEPNS_9ExecStateERKNS_4ListE
 __ZN3KJS8JSObject9constructEPNS_9ExecStateERKNS_4ListERKNS_10IdentifierERKNS_7UStringEi
+__ZN3KJS8Profiler13stopProfilingEPNS_9ExecStateERKNS_7UStringE
+__ZN3KJS8Profiler14startProfilingEPNS_9ExecStateERKNS_7UStringE
 __ZN3KJS8Profiler8profilerEv
 __ZN3KJS8jsStringEPKc
 __ZN3KJS8jsStringERKNS_7UStringE
@@ -207,8 +211,6 @@ __ZN3KJS9Collector17globalObjectCountEv
 __ZN3KJS9Collector20protectedObjectCountEv
 __ZN3KJS9Collector23collectOnMainThreadOnlyEPNS_7JSValueE
 __ZN3KJS9Collector25protectedObjectTypeCountsEv
-__ZN3KJS8Profiler13stopProfilingEPNS_9ExecStateERKNS_7UStringE
-__ZN3KJS8Profiler14startProfilingEPNS_9ExecStateERKNS_7UStringE
 __ZN3KJS9Collector26protectedGlobalObjectCountEv
 __ZN3KJS9Collector4sizeEv
 __ZN3KJS9Collector7collectEv

JavaScriptCore/kjs/JSGlobalObject.cpp
@@ -234,6 +234,20 @@ void JSGlobalObject::putWithAttributes(ExecState* exec, const Identifier& proper
     }
 }
 
+void JSGlobalObject::defineGetter(ExecState* exec, const Identifier& propertyName, JSObject* getterFunc)
+{
+    PropertySlot slot;
+    if (!symbolTableGet(propertyName, slot))
+        JSVariableObject::defineGetter(exec, propertyName, getterFunc);
+}
+
+void JSGlobalObject::defineSetter(ExecState* exec, const Identifier& propertyName, JSObject* setterFunc)
+{
+    PropertySlot slot;
+    if (!symbolTableGet(propertyName, slot))
+        JSVariableObject::defineSetter(exec, propertyName, setterFunc);
+}
+
 static inline JSObject* lastInPrototypeChain(JSObject* object)
 {
     JSObject* o = object;

JavaScriptCore/kjs/JSGlobalObject.h
@@ -171,6 +171,10 @@ namespace KJS {
         virtual void put(ExecState*, const Identifier&, JSValue*);
         virtual void putWithAttributes(ExecState*, const Identifier& propertyName, JSValue* value, unsigned attributes);
 
+
+        virtual void defineGetter(ExecState*, const Identifier& propertyName, JSObject* getterFunc);
+        virtual void defineSetter(ExecState*, const Identifier& propertyName, JSObject* setterFunc);
+
         // Linked list of all global objects.
         static JSGlobalObject* head() { return s_head; }
         JSGlobalObject* next() { return d()->next; }

LayoutTests/fast/dom/getter-on-window-object2-expected.txt
@@ -1,9 +1,9 @@
 This page tests what happens when a getter / setter on the window object conflicts with another property or declared variable.
 
 window.x is: 1
-typeof window.__lookupGetter__("x") is: function
+typeof window.__lookupGetter__("x") is: undefined
 window.x is: 2
-typeof window.__lookupSetter__("x") is: function
+typeof window.__lookupSetter__("x") is: undefined
 window.y is: window.y __getter__
 typeof window.__lookupGetter__("y") is: function
 window.y is: 2