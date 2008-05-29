7576d5fbbf307ad3afd5de82e4b02f7f095df259
Ada Chan <adachan@apple.com>
2008-05-29 00:51:06 +0000

    <rdar://problem/5957036> REGRESSION (r31960): 20-30% slowdown in i-Bench JavaScript test on XP Home         Conditionalize the timer latency change (r31960) only for vista since it does not         help xp anyway.
            Reviewed by Steve Falkenburg.
    
            * platform/win/SharedTimerWin.cpp:
            (WebCore::isRunningOnVistaOrLater):
            (WebCore::TimerWindowWndProc):
            (WebCore::setSharedTimerFireTime):
    
    
    
    Canonical link: https://commits.webkit.org/26695@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@34190 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/win/SharedTimerWin.cpp
+++ WebCore/platform/win/SharedTimerWin.cpp
@@ -43,6 +43,9 @@
 
 // These aren't in winuser.h with the MSVS 2003 Platform SDK, 
 // so use default values in that case.
+#ifndef USER_TIMER_MINIMUM
+#define USER_TIMER_MINIMUM 0x0000000A
+#endif
 
 #ifndef USER_TIMER_MAXIMUM
 #define USER_TIMER_MAXIMUM 0x7FFFFFFF
@@ -66,6 +69,7 @@ static UINT timerFiredMessage = 0;
 static HANDLE timerQueue;
 static HANDLE timer;
 static bool highResTimerActive;
+static bool processingCustomTimerMessage = false;
 
 const LPCWSTR kTimerWindowClassName = L"TimerWindowClass";
 const int timerResolution = 1;
@@ -77,6 +81,22 @@ enum {
     endHighResTimerID = 1001
 };
 
+static bool isRunningOnVistaOrLater()
+{
+    static bool os = false;
+    static bool initialized = false;
+    if (!initialized) {
+        OSVERSIONINFOEX vi = {sizeof(vi), 0};
+        GetVersionEx((OSVERSIONINFO*)&vi);
+
+        // NOTE: This does not work under a debugger - Vista shims Visual Studio, 
+        // making it believe it is xpsp2, which is inherited by debugged applications
+        os = vi.dwMajorVersion >= 6;
+        initialized = true;
+    }
+
+    return os;
+}
 
 LRESULT CALLBACK TimerWindowWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -91,9 +111,11 @@ LRESULT CALLBACK TimerWindowWndProc(HWND hWnd, UINT message, WPARAM wParam, LPAR
     }
 #endif
 
-    if (message == timerFiredMessage)
+    if (message == timerFiredMessage) {
+        processingCustomTimerMessage = true;
         sharedTimerFiredFunction();
-    else if (message == WM_TIMER) {
+        processingCustomTimerMessage = false;
+    } else if (message == WM_TIMER) {
         if (wParam == sharedTimerID) {
             KillTimer(timerWindowHandle, sharedTimerID);
             sharedTimerFiredFunction();
@@ -140,6 +162,7 @@ static void NTAPI queueTimerProc(PVOID, BOOLEAN)
 void setSharedTimerFireTime(double fireTime)
 {
     ASSERT(sharedTimerFiredFunction);
+    bool isRunningVista = isRunningOnVistaOrLater();
 
     double interval = fireTime - currentTime();
     unsigned intervalInMS;
@@ -153,7 +176,7 @@ void setSharedTimerFireTime(double fireTime)
             intervalInMS = (unsigned)interval;
     }
 
-    if (interval < highResolutionThresholdMsec) {
+    if (isRunningVista && interval < highResolutionThresholdMsec) {
         if (!highResTimerActive) {
             highResTimerActive = true;
             timeBeginPeriod(timerResolution);
@@ -169,13 +192,13 @@ void setSharedTimerFireTime(double fireTime)
 
     // If the queue doesn't contains input events, we use a higher priorty timer event posting mechanism.
     if (!(queueStatus & (QS_MOUSEBUTTON | QS_KEY | QS_RAWINPUT))) {
-
-        if (!intervalInMS && !(queueStatus & QS_PAINT)) {
+        if (((isRunningVista && !intervalInMS) || (!isRunningVista && intervalInMS < USER_TIMER_MINIMUM && !processingCustomTimerMessage))
+            && !(queueStatus & QS_PAINT)) {
             // Call PostMessage immediately if the timer is already expired, unless a paint is pending.
             // (we prioritize paints over timers)
             PostMessage(timerWindowHandle, timerFiredMessage, 0, 0);
             timerSet = true;
-        } else {
+        } else if (isRunningVista) {
             // Otherwise, delay the PostMessage via a CreateTimerQueueTimer
             if (!timerQueue)
                 timerQueue = CreateTimerQueue();
