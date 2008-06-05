f0a7ebb86c6d571042c43d154c16eaf3dabb83f8
Eric Seidel <eric@webkit.org>
2008-06-05 22:01:34 +0000

    Reviewed by mjs.
            Get rid of Uninitialized Memory Reads (UMRs) in the XPath::Value copy constructor.
    
            No functional changes, thus no tests.
    
            * xml/XPathValue.h:
            (WebCore::XPath::Value::Value): initialize all values in each constructor
    
    
    Canonical link: https://commits.webkit.org/26834@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@34383 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/xml/XPathValue.h
+++ WebCore/xml/XPathValue.h
@@ -56,20 +56,20 @@ namespace WebCore {
         public:
             enum Type { NodeSetValue, BooleanValue, NumberValue, StringValue };
             
-            Value(unsigned value) : m_type(NumberValue), m_number(value) {}
-            Value(unsigned long value) : m_type(NumberValue), m_number(value) {}
-            Value(double value) : m_type(NumberValue), m_number(value) {}
+            Value(unsigned value) : m_type(NumberValue), m_bool(false), m_number(value) {}
+            Value(unsigned long value) : m_type(NumberValue), m_bool(false), m_number(value) {}
+            Value(double value) : m_type(NumberValue), m_bool(false), m_number(value) {}
 
-            Value(const char* value) : m_type(StringValue), m_data(ValueData::create(value)) {}
-            Value(const String& value) : m_type(StringValue), m_data(ValueData::create(value)) {}
-            Value(const NodeSet& value) : m_type(NodeSetValue), m_data(ValueData::create(value)) {}
-            Value(Node* value) : m_type(NodeSetValue), m_data(ValueData::create()) { m_data->m_nodeSet.append(value); }
+            Value(const char* value) : m_type(StringValue), m_bool(false), m_number(0), m_data(ValueData::create(value)) {}
+            Value(const String& value) : m_type(StringValue), m_bool(false), m_number(0), m_data(ValueData::create(value)) {}
+            Value(const NodeSet& value) : m_type(NodeSetValue), m_bool(false), m_number(0), m_data(ValueData::create(value)) {}
+            Value(Node* value) : m_type(NodeSetValue), m_bool(false), m_number(0), m_data(ValueData::create()) { m_data->m_nodeSet.append(value); }
 
             // This is needed to safely implement constructing from bool - with normal function overloading, any pointer type would match.
             template<typename T> Value(T);
 
             static const struct AdoptTag {} adopt;
-            Value(NodeSet& value, const AdoptTag&) : m_type(NodeSetValue), m_data(ValueData::create()) { value.swap(m_data->m_nodeSet); }
+            Value(NodeSet& value, const AdoptTag&) : m_type(NodeSetValue), m_bool(false), m_number(0),  m_data(ValueData::create()) { value.swap(m_data->m_nodeSet); }
 
             Type type() const { return m_type; }
 
@@ -95,6 +95,7 @@ namespace WebCore {
         inline Value::Value(bool value)
             : m_type(BooleanValue)
             , m_bool(value)
+            , m_number(0)
         {
         }
     }
