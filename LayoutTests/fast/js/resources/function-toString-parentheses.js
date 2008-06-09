b3ccfc12a6228ffafaf6eea994ecde2f8df84795
Cameron Zwarich <cwzwarich@webkit.org>
2008-06-09 05:20:32 +0000

    2008-06-08  Cameron Zwarich  <cwzwarich@uwaterloo.ca>
            Reviewed by Oliver.
    
            Bug 17931: Incorrect decompilation with |new|
            <https://bugs.webkit.org/show_bug.cgi?id=17931>
    
            This bug was fixed by SquirrelFish, but we should add a test.
    
            * fast/js/function-toString-parentheses-expected.txt:
            * fast/js/resources/function-toString-parentheses.js:
    
    
    Canonical link: https://commits.webkit.org/26913@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@34462 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/js/resources/function-toString-parentheses.js
+++ LayoutTests/fast/js/resources/function-toString-parentheses.js
@@ -220,4 +220,6 @@ shouldBe("compileAndSerializeLeftmostTest('const a = (1, 2), b = 1')", "'const a
 shouldBe("compileAndSerializeLeftmostTest('const a = 1, b = (1, 2)')", "'const a = 1, b = (1, 2)'");
 shouldBe("compileAndSerializeLeftmostTest('const a = (1, 2), b = (1, 2)')", "'const a = (1, 2), b = (1, 2)'");
 
+shouldBe("compileAndSerialize('(function () { new (a.b()).c })')", "'(function () { new (a.b()).c })'");
+
 var successfullyParsed = true;
