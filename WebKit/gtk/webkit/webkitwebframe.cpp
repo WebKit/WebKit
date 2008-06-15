704cafa07fd15086e416a690aae1ce100d5a1257
Darin Adler <darin@apple.com>
2008-06-15 03:52:51 +0000

    2008-06-14  Darin Adler  <darin@apple.com>
            - fix build
    
            * webkit/webkitwebframe.cpp: Remove a bogus release() call.
    
    
    
    Canonical link: https://commits.webkit.org/26994@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@34545 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/gtk/webkit/webkitwebframe.cpp
+++ WebKit/gtk/webkit/webkitwebframe.cpp
@@ -234,7 +234,7 @@ WebKitWebFrame* webkit_web_frame_init_with_web_view(WebKitWebView* webView, HTML
 
     priv->webView = webView;
     priv->client = new WebKit::FrameLoaderClient(frame);
-    priv->coreFrame = Frame::create(viewPriv->corePage, element, priv->client).release().releaseRef();
+    priv->coreFrame = Frame::create(viewPriv->corePage, element, priv->client).releaseRef();
     priv->coreFrame->init();
 
     return frame;
