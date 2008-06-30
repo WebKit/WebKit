bd591c4e1d9d735856708fb39ef4d5fcfeac34bf
Dan Bernstein <mitz@apple.com>
2008-06-30 00:58:11 +0000

    2008-06-29  Dan Bernstein  <mitz@apple.com>
            - add a missing file from the last check-in
    
            * fast/css/resources/font-face-descriptor-multiple-values-parsing.js: Added.
    
    
    
    Canonical link: https://commits.webkit.org/27324@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@34880 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/css/resources/font-face-descriptor-multiple-values-parsing.js
@@ -0,0 +1,39 @@
+description("This tests that the font-weight, font-style and font-variant properties accept value lists when they appear inside @font-face rules, and that the value 'all' is allowed only by itself.");
+
+function test(property, value)
+{
+    var style = document.createElement("style");
+    style.appendChild(document.createTextNode("@font-face{" + property + ":" + value + ";}"));
+    var head = document.getElementsByTagName("head")[0];
+    head.appendChild(style);
+    var result = style.sheet.rules[0].style.getPropertyCSSValue(property).cssText;
+    head.removeChild(style);
+    return result;
+}
+
+shouldBe('test("font-weight", "100")', '"100"');
+shouldBe('test("font-weight", "all")', '"all"');
+shouldBe('test("font-weight", "100, 200")', '"100, 200"');
+shouldBe('test("font-weight", "bold, normal")', '"bold, normal"');
+shouldBe('test("font-weight", "100, 200, 300, 400, 500, 600, 700, 100")', '"100, 200, 300, 400, 500, 600, 700, 100"');
+shouldThrow('test("font-weight", "all, 100")', '"TypeError: Null value"');
+shouldThrow('test("font-weight", "bold, normal, all")', '"TypeError: Null value"');
+shouldThrow('test("font-weight", "")', '"TypeError: Null value"');
+
+shouldBe('test("font-style", "normal")', '"normal"');
+shouldBe('test("font-style", "italic")', '"italic"');
+shouldBe('test("font-style", "normal, oblique")', '"normal, oblique"');
+shouldBe('test("font-style", "all")', '"all"');
+shouldThrow('test("font-style", "all, normal")', '"TypeError: Null value"');
+shouldThrow('test("font-style", "italic, all")', '"TypeError: Null value"');
+shouldThrow('test("font-style", "")', '"TypeError: Null value"');
+
+shouldBe('test("font-variant", "normal")', '"normal"');
+shouldBe('test("font-variant", "small-caps")', '"small-caps"');
+shouldBe('test("font-variant", "normal, small-caps")', '"normal, small-caps"');
+shouldBe('test("font-variant", "all")', '"all"');
+shouldThrow('test("font-variant", "all, normal")', '"TypeError: Null value"');
+shouldThrow('test("font-variant", "small-caps, all")', '"TypeError: Null value"');
+shouldThrow('test("font-variant", "")', '"TypeError: Null value"');
+
+var successfullyParsed = true;
