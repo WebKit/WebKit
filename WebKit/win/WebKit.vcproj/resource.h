a7c2ebf28068461a5d0a44e49ad427fc0ae6056a
Adam Roben <aroben@apple.com>
2008-07-10 18:08:24 +0000

    Windows build fix
            * WebKit.vcproj/resource.h: Revert some changes that VS made to this
            file in r35083.
    
    Canonical link: https://commits.webkit.org/27537@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@35099 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/win/WebKit.vcproj/resource.h
+++ WebKit/win/WebKit.vcproj/resource.h
@@ -2,30 +2,24 @@
 // Microsoft Visual C++ generated include file.
 // Used by WebKit.rc
 //
-#define __BUILD_NUMBER_MINOR__          0
-#define __BUILD_NUMBER_VARIANT__        0
 #define IDR_WEBKIT                      1
-#define BUILD_NUMBER                    1
-#define __VERSION_MINOR__               1
-#define __VERSION_TINY__                1
-#define VS_VERSION_INFO                 1
 #define IDR_RESIZE_CORNER               2
 #define IDR_MISSING_IMAGE               3
-#define __VERSION_MAJOR__               3
 #define IDR_URL_ICON                    4
 #define IDR_NULL_PLUGIN                 5
 #define IDR_ZOOM_IN_CURSOR              6
 #define IDR_ZOOM_OUT_CURSOR             7
 #define IDR_VERTICAL_TEXT_CURSOR        8
-#define IDR_PAN_SCROLL_ICON             12
-#define __BUILD_NUMBER_MAJOR__          526
+#define IDR_PAN_SCROLL_ICON             9
 #define IDC_STATIC                      -1
+
+#define BUILD_NUMBER                    1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        13
+#define _APS_NEXT_RESOURCE_VALUE        12
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
