0e45d9450452762668d1d1e262d048401640ea6d
Kevin McCullough <kmccullough@apple.com>
2008-07-16 20:37:35 +0000

    2008-07-16  Kevin McCullough  <kmccullough@apple.com>
            Reviewed by Sam and Geoff.
    
            <rdar://problem/5958840> Navigating to another page while profiler is
            attached results in slow JavaScript for all time.
    
            - The UNLIKELY keeps this from being a sunspider performance regression.
    
            * kjs/JSGlobalObject.cpp:
            (KJS::JSGlobalObject::~JSGlobalObject): Stop the profiler associated
            with this exec state.
    
    
    
    Canonical link: https://commits.webkit.org/27631@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@35200 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/JSGlobalObject.cpp
+++ JavaScriptCore/kjs/JSGlobalObject.cpp
@@ -81,6 +81,12 @@ JSGlobalObject::~JSGlobalObject()
     if (d()->debugger)
         d()->debugger->detach(this);
 
+    Profiler** profiler = Profiler::enabledProfilerReference();
+    if (UNLIKELY(*profiler != 0)) {
+        (*profiler)->stopProfiling(globalExec(), UString());
+        (*profiler)->didFinishAllExecution(globalExec());
+    }
+
     d()->next->d()->prev = d()->prev;
     d()->prev->d()->next = d()->next;
     JSGlobalObject*& headObject = head();
