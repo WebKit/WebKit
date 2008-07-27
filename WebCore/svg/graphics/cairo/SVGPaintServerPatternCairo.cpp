4564d32ad77767c6aab5bfa62e341f3fae11066e
Mark Rowe <mrowe@apple.com>
2008-07-27 05:38:28 +0000

    2008-07-26  Dirk Schulze  <vbs85@gmx.de>
            Reviewed by Nikolas Zimmermann.
    
            Added pattern-support for SVG in Cairo.
    
            * svg/graphics/cairo/SVGPaintServerPatternCairo.cpp:
            (WebCore::applyStrokeStyleToContext):
            (WebCore::SVGPaintServerPattern::setup):
    
    Canonical link: https://commits.webkit.org/27816@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@35390 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/svg/graphics/cairo/SVGPaintServerPatternCairo.cpp
+++ WebCore/svg/graphics/cairo/SVGPaintServerPatternCairo.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2007 Alp Toker <alp@atoker.com>
+ * Copyright (C) 2008 Dirk Schulze <vbs85@gmx.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -20,14 +21,75 @@
 #include "config.h"
 
 #if ENABLE(SVG)
-#include "NotImplemented.h"
 #include "SVGPaintServerPattern.h"
+
+#include "GraphicsContext.h"
+#include "ImageBuffer.h"
+#include "RenderObject.h"
+#include "SVGPatternElement.h"
+#include <wtf/OwnArrayPtr.h>
 
 namespace WebCore {
 
+// TODO: Share this code with all SVGPaintServers
+static void applyStrokeStyleToContext(GraphicsContext* context, const SVGRenderStyle* style, const RenderObject* object)
+{
+    cairo_t* cr = context->platformContext();
+
+    context->setStrokeThickness(SVGRenderStyle::cssPrimitiveToLength(object, style->strokeWidth(), 1.0));
+    context->setLineCap(style->capStyle());
+    context->setLineJoin(style->joinStyle());
+    if (style->joinStyle() == MiterJoin)
+        context->setMiterLimit(style->strokeMiterLimit());
+
+    const DashArray& dashes = dashArrayFromRenderingStyle(object->style());
+    OwnArrayPtr<double> dsh(new double[dashes.size()]);
+    for (size_t i = 0; i < dashes.size() ; i++)
+        dsh[i] = dashes[i];
+    double dashOffset = SVGRenderStyle::cssPrimitiveToLength(object, style->strokeDashOffset(), 0.0);
+    cairo_set_dash(cr, dsh.get(), dashes.size(), dashOffset);
+}
+
 bool SVGPaintServerPattern::setup(GraphicsContext*& context, const RenderObject* object, SVGPaintTargetType type, bool isPaintingText) const
 {
-    notImplemented();
+    FloatRect targetRect = object->relativeBBox(false);
+
+    const SVGRenderStyle* style = object->style()->svgStyle();
+
+    float strokeWidth = SVGRenderStyle::cssPrimitiveToLength(object, style->strokeWidth(), 1.0);
+
+    if (targetRect.width() == 0)
+        targetRect = FloatRect(targetRect.x(), targetRect.y(), strokeWidth, targetRect.height());
+    if (targetRect.height() == 0)
+        targetRect = FloatRect(targetRect.x(), targetRect.y(), targetRect.width(), strokeWidth);
+
+    m_ownerElement->buildPattern(targetRect);
+
+    cairo_surface_t* image = tile()->surface();
+    if (!image)
+        return false;
+
+    cairo_t* cr = context->platformContext();
+
+    cairo_pattern_t* pattern = cairo_pattern_create_for_surface(image);
+    cairo_pattern_set_extend(pattern, CAIRO_EXTEND_REPEAT);
+
+    cairo_pattern_set_filter(pattern, CAIRO_FILTER_BEST);
+    cairo_matrix_t pattern_matrix = patternTransform();
+    cairo_matrix_t matrix = {1, 0, 0, 1, patternBoundaries().x(), patternBoundaries().y()};
+    cairo_matrix_multiply(&matrix, &matrix, &pattern_matrix);
+    cairo_matrix_invert(&matrix);
+    cairo_pattern_set_matrix(pattern, &matrix);
+
+    if ((type & ApplyToFillTargetType) && style->hasFill())
+        cairo_set_fill_rule(cr, style->fillRule() == RULE_EVENODD ? CAIRO_FILL_RULE_EVEN_ODD : CAIRO_FILL_RULE_WINDING);
+
+    if ((type & ApplyToStrokeTargetType) && style->hasStroke())
+        applyStrokeStyleToContext(context, style, object);
+
+    cairo_set_source(cr, pattern);
+    cairo_pattern_destroy(pattern);
+
     return true;
 }
 
