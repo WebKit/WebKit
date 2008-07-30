c3c173494107d403b4e19c022e1e9063d740778f
Anders Carlsson <andersca@apple.com>
2008-07-30 16:22:39 +0000

    2008-07-30  Anders Carlsson  <andersca@apple.com>
            * API/WebKitAvailability.h:
            Fix Windows (and other non-Mac builds).
    
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.vcproj:
            Add WebKitAvailability.h to the project.
    
    
    
    Canonical link: https://commits.webkit.org/27873@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@35449 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/API/WebKitAvailability.h
+++ JavaScriptCore/API/WebKitAvailability.h
@@ -26,8 +26,6 @@
 #ifndef __WebKitAvailability__
 #define __WebKitAvailability__
 
-#import <AvailabilityMacros.h>
-
 /* The structure of this header is based on AvailabilityMacros.h.  The major difference is that the availability
    macros are defined in terms of WebKit version numbers rather than Mac OS X system version numbers, as WebKit
    releases span multiple versions of Mac OS X.
@@ -42,6 +40,12 @@
 #define WEBKIT_VERSION_3_1    0x0310
 #define WEBKIT_VERSION_LATEST 0x9999
 
+#ifdef __APPLE__
+#import <AvailabilityMacros.h>
+else
+// For non-Mac platforms, require the newest version.
+#define WEBKIT_VERSION_MIN_REQUIRED WEBKIT_VERSION_LATEST
+#endif
 
 /* The versions of GCC that shipped with Xcode prior to 3.0 (GCC build number < 5400) did not support attributes on methods
    declared in a category.  If we are building with one of these versions, we need to omit the attribute.  We achieve this
