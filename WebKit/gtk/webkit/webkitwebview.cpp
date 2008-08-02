6996ef34ccf1ba1594371d82a486484a640e9079
Jan Alonzo <jmalonzo@webkit.org>
2008-08-02 05:12:11 +0000

    2008-08-01  Wouter Bolsterlee  <uws@xs4all.nl>
            https://bugs.webkit.org/show_bug.cgi?id=20035
            [GTK] Fix return value for webkit_web_view_go_to_back_forward_item()
    
            * webkit/webkitwebview.cpp:
    
    Canonical link: https://commits.webkit.org/27940@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@35522 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/gtk/webkit/webkitwebview.cpp
+++ WebKit/gtk/webkit/webkitwebview.cpp
@@ -1520,7 +1520,10 @@ gboolean webkit_web_view_go_to_back_forward_item(WebKitWebView* webView, WebKitW
     g_return_val_if_fail(WEBKIT_IS_WEB_VIEW(webView), FALSE);
     g_return_val_if_fail(WEBKIT_IS_WEB_HISTORY_ITEM(item), FALSE);
 
-    // FIXME: This always returns TRUE
+    WebKitWebBackForwardList* backForwardList = webkit_web_view_get_back_forward_list(webView);
+    if (!webkit_web_back_forward_list_contains_item(backForwardList, item))
+        return FALSE;
+
     core(webView)->goToItem(core(item), FrameLoadTypeIndexedBackForward);
     return TRUE;
 }
