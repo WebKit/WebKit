80a19d1abd9357e0c1b2c0a70b49502772d25f94
Jan Alonzo <jmalonzo@webkit.org>
2008-08-02 09:38:55 +0000

    2008-08-02  Wouter Bolsterlee  <wbolster@svn.gnome.org>
            Reviewed by Eric Seidel.
    
            * plugins/gtk/gtk2xtbin.h:
    
            https://bugs.webkit.org/show_bug.cgi?id=20034
            Fix malformed Vim modeline.
    
    Canonical link: https://commits.webkit.org/27944@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@35526 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/plugins/gtk/gtk2xtbin.h
+++ WebCore/plugins/gtk/gtk2xtbin.h
@@ -1,5 +1,5 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * vim:expandtab:shiftwidth=2:tabstop=2: */
+ * vim: set expandtab shiftwidth=2 tabstop=2: */
  
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
