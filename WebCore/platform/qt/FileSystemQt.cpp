cb27d27a390b07ad16219b7fcb3ce003fd7ca38c
Simon Hausmann <hausmann@webkit.org>
2008-08-08 09:32:51 +0000

    2008-08-08  Adam Treat  <treat@kde.org>
            Reviewed by Simon.
    
            Return the canonical file path in Qt's listDirectory as WebCore::PluginDatabase
            uses this method to retrieve a list of possible plugins.  Many linux systems
            have nested symbolic links scattered throughout the plugin directories which
            resulted in the PluginDatabase loading the same plugin N times depending on
            the number of symbolic links.
    
    Canonical link: https://commits.webkit.org/28048@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@35638 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/qt/FileSystemQt.cpp
+++ WebCore/platform/qt/FileSystemQt.cpp
@@ -109,7 +109,7 @@ Vector<String> listDirectory(const String& path, const String& filter)
         nameFilters.append(filter);
     QFileInfoList fileInfoList = QDir(path).entryInfoList(nameFilters, QDir::AllEntries | QDir::NoDotAndDotDot);
     foreach (const QFileInfo fileInfo, fileInfoList) {
-        String entry = String(fileInfo.absoluteFilePath());
+        String entry = String(fileInfo.canonicalFilePath());
         entries.append(entry);
     }
 
