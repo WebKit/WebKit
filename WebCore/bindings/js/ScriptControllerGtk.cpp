WebKit/WebKit

commit 9612c01eb8546c4612cb8002377621971991fc61

Reviewed by Cameron Zwarich.
        Move more methods from Frame into ScriptController
        https://bugs.webkit.org/show_bug.cgi?id=20294

        Finally finish more of the work which Darin started long ago
        Move a few more Frame methods into ScriptController.
        I also took this opportunity to clean up some of the plugin
        code as well, and moved some of that into ScriptController.

        I removed Frame::clearScriptController() and ScriptController::clear() is now clearWindowShell()

        No functional changes, thus no test cases.

        * GNUmakefile.am:
        * WebCore.NPAPI.exp:
        * WebCore.base.exp:
        * WebCore.pro:
        * WebCore.vcproj/WebCore.vcproj:
        * WebCore.xcodeproj/project.pbxproj:
        * bindings/js/ScriptController.cpp:
        (WebCore::ScriptController::ScriptController):
        (WebCore::ScriptController::~ScriptController):
        (WebCore::ScriptController::bindingRootObject):
        (WebCore::ScriptController::createRootObject):
        (WebCore::ScriptController::windowScriptNPObject):
        (WebCore::ScriptController::createScriptObjectForPluginElement): New method.
        (WebCore::ScriptController::clearPlatformScriptObjects):
        (WebCore::ScriptController::disconnectPlatformScriptObjects):
        (WebCore::ScriptController::cleanupScriptObjectsForPlugin):
        (WebCore::ScriptController::clearScriptObjects):
        * bindings/js/ScriptController.h:
        (WebCore::ScriptController::sourceURL):
        (WebCore::ScriptController::clearFormerWindow):
        * bindings/js/ScriptControllerGtk.cpp: Copied from WebCore/page/gtk/FrameGtk.cpp.
        (WebCore::Frame::createScriptInstanceForWidget):
        * bindings/js/ScriptControllerMac.mm: Added.
        (WebCore::ScriptController::createScriptInstanceForWidget):
        (WebCore::ScriptController::windowScriptObject):
        (WebCore::ScriptController::clearPlatformScriptObjects):

WebCore/bindings/js/ScriptControllerGtk.cpp
@@ -1,4 +1,11 @@
 /*
+ * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2006 Michael Emmel mike.emmel@gmail.com
+ * Copyright (C) 2007 Holger Hans Peter Freyther
+ * Copyright (C) 2008 Collabora Ltd.  All rights reserved.
+ * Copyright (C) 2008 Eric Seidel <eric@webkit.org>
+ * All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -22,16 +29,20 @@
  */
 
 #include "config.h"
-#include "Frame.h"
+#include "ScriptController.h"
 
-#include "NotImplemented.h"
+#include "PluginView.h"
+#include "runtime_root.h"
+#include "runtime.h"
 
 namespace WebCore {
 
-DragImageRef Frame::dragImageForSelection()
+PassRefPtr<KJS::Bindings::Instance> Frame::createScriptInstanceForWidget(Widget* widget)
 {
-    notImplemented();
-    return 0;
+    if (widget->isPluginView())
+        return 0;
+
+    return static_cast<PluginView*>(widget)->bindingInstance();
 }
 
 }

WebCore/bindings/js/ScriptControllerQt.cpp
@@ -1,4 +1,16 @@
 /*
+ * Copyright (C) 2006 Dirk Mueller <mueller@kde.org>
+ * Copyright (C) 2006 Zack Rusin <zack@kde.org>
+ * Copyright (C) 2006 George Staikos <staikos@kde.org>
+ * Copyright (C) 2006 Simon Hausmann <hausmann@kde.org>
+ * Copyright (C) 2006 Rob Buis <buis@kde.org>
+ * Copyright (C) 2006 Nikolas Zimmermann <zimmermann@kde.org>
+ * Copyright (C) 2007 Trolltech ASA
+ * Copyright (C) 2008 Collabora Ltd.  All rights reserved.
+ * Copyright (C) 2008 Eric Seidel <eric@webkit.org>
+ *
+ * All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -22,16 +34,28 @@
  */
 
 #include "config.h"
-#include "Frame.h"
+#include "ScriptController.h"
 
-#include "NotImplemented.h"
+#include "DOMWindow.h"
+#include "PluginView.h"
+#include "qt_instance.h"
+#include "runtime_root.h"
+#include "runtime.h"
 
 namespace WebCore {
 
-DragImageRef Frame::dragImageForSelection()
+PassRefPtr<KJS::Bindings::Instance> ScriptController::createScriptInstanceForWidget(WebCore::Widget* widget)
 {
-    notImplemented();
-    return 0;
+    if (widget->isFrameView())
+        return 0;
+    if (widget->isNPAPIPlugin())
+        return static_cast<PluginView*>(widget)->bindingInstance();
+
+    QWidget* nativeWidget = widget->nativeWidget();
+    if (!nativeWidget)
+        return 0;
+    return KJS::Bindings::QtInstance::create(nativeWidget, bindingRootObject());
 }
 
 }
+// vim: ts=4 sw=4 et

WebCore/bindings/js/ScriptControllerWin.cpp
@@ -1,4 +1,7 @@
 /*
+ * Copyright (C) 2006, 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2008 Eric Seidel <eric@webkit.org>
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -18,20 +21,25 @@
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
 #include "config.h"
-#include "Frame.h"
+#include "ScriptController.h"
 
-#include "NotImplemented.h"
+#include "PluginView.h"
+#include "runtime_root.h"
+
+using namespace KJS::Bindings;
 
 namespace WebCore {
 
-DragImageRef Frame::dragImageForSelection()
+PassRefPtr<KJS::Bindings::Instance> ScriptController::createScriptInstanceForWidget(Widget* widget)
 {
-    notImplemented();
-    return 0;
+    if (widget->isPluginView())
+        return 0;
+
+    return static_cast<PluginView*>(widget)->bindingInstance();
 }
 
-}
+} // namespace WebCore

WebCore/GNUmakefile.am
@@ -1050,6 +1050,7 @@ webcore_sources += \
 	WebCore/xml/XMLSerializer.cpp
 
 webcoregtk_sources += \
+	WebCore/bindings/js/ScriptControllerGtk.cpp \
 	WebCore/plugins/gtk/PluginDataGtk.cpp \
 	WebCore/plugins/gtk/PluginPackageGtk.cpp \
 	WebCore/plugins/gtk/PluginViewGtk.cpp

WebCore/WebCore.NPAPI.exp
@@ -16,4 +16,5 @@ __NPN_RetainObject
 __NPN_SetException
 __NPN_SetProperty
 __NPN_UTF8FromIdentifier
-__ZN7WebCore5Frame20windowScriptNPObjectEv
+__ZN7WebCore16ScriptController20windowScriptNPObjectEv
+__ZN7WebCore16ScriptController29cleanupScriptObjectsForPluginEPv

WebCore/WebCore.base.exp
@@ -167,7 +167,6 @@ __ZN7WebCore11FrameLoader4loadEPNS_14DocumentLoaderE
 __ZN7WebCore11FrameLoader4loadERKNS_15ResourceRequestE
 __ZN7WebCore11FrameLoader4loadERKNS_15ResourceRequestERKNS_14SubstituteDataE
 __ZN7WebCore11FrameLoader4loadERKNS_15ResourceRequestERKNS_6StringE
-__ZN7WebCore11FrameLoader5clearEbb
 __ZN7WebCore11FrameLoader6reloadEv
 __ZN7WebCore11FrameLoader7addDataEPKci
 __ZN7WebCore11FrameLoader7canLoadERKNS_4KURLERKNS_6StringEPKNS_8DocumentE
@@ -362,6 +361,7 @@ __ZN7WebCore16NavigationActionC1ERKNS_4KURLENS_13FrameLoadTypeEb
 __ZN7WebCore16NavigationActionC1ERKNS_4KURLENS_14NavigationTypeE
 __ZN7WebCore16NavigationActionC1Ev
 __ZN7WebCore16ScriptController10initScriptEv
+__ZN7WebCore16ScriptController18windowScriptObjectEv
 __ZN7WebCore16colorFromNSColorEP7NSColor
 __ZN7WebCore16createFullMarkupEPKNS_4NodeE
 __ZN7WebCore16createFullMarkupEPKNS_5RangeE
@@ -461,16 +461,13 @@ __ZN7WebCore5Frame13setZoomFactorEfb
 __ZN7WebCore5Frame14frameForWidgetEPKNS_6WidgetE
 __ZN7WebCore5Frame15sendScrollEventEv
 __ZN7WebCore5Frame16adjustPageHeightEPffff
-__ZN7WebCore5Frame17bindingRootObjectEv
 __ZN7WebCore5Frame17setIsDisconnectedEb
-__ZN7WebCore5Frame18windowScriptObjectEv
 __ZN7WebCore5Frame20setSelectionFromNoneEv
 __ZN7WebCore5Frame21markAllMatchesForTextERKNS_6StringEbj
 __ZN7WebCore5Frame21setProhibitsScrollingEb
 __ZN7WebCore5Frame24computeAndSetTypingStyleEPNS_19CSSStyleDeclarationENS_10EditActionE
 __ZN7WebCore5Frame25matchLabelsAgainstElementEP7NSArrayPNS_7ElementE
 __ZN7WebCore5Frame28searchForLabelsBeforeElementEP7NSArrayPNS_7ElementE
-__ZN7WebCore5Frame29cleanupScriptObjectsForPluginEPv
 __ZN7WebCore5Frame29forceLayoutWithPageWidthRangeEffb
 __ZN7WebCore5Frame34setMarkedTextMatchesAreHighlightedEb
 __ZN7WebCore5Frame4initEv
@@ -561,6 +558,7 @@ __ZN7WebCore8FormDataD1Ev
 __ZN7WebCore8IntPointC1ERK8_NSPoint
 __ZN7WebCore8Settings14setJavaEnabledEb
 __ZN7WebCore8Settings16setUsesPageCacheEb
+__ZN7WebCore8Settings16setZoomsTextOnlyEb
 __ZN7WebCore8Settings17setPluginsEnabledEb
 __ZN7WebCore8Settings18setDOMPasteAllowedEb
 __ZN7WebCore8Settings18setDefaultFontSizeEi
@@ -576,7 +574,6 @@ __ZN7WebCore8Settings22setShowsURLsInToolTipsEb
 __ZN7WebCore8Settings23setDefaultFixedFontSizeEi
 __ZN7WebCore8Settings23setEditableLinkBehaviorENS_20EditableLinkBehaviorE
 __ZN7WebCore8Settings23setUpdatesWhenOffscreenEb
-__ZN7WebCore8Settings16setZoomsTextOnlyEb
 __ZN7WebCore8Settings24setTextAreasAreResizableEb
 __ZN7WebCore8Settings25setDeveloperExtrasEnabledEb
 __ZN7WebCore8Settings25setMinimumLogicalFontSizeEi

WebCore/WebCore.pro
@@ -922,6 +922,7 @@ HEADERS += \
     $$PWD/platform/network/qt/QNetworkReplyHandler.h
 
 SOURCES += \
+    bindings/js/ScriptControllerQt.cpp \
     bridge/qt/qt_class.cpp \
     bridge/qt/qt_instance.cpp \
     bridge/qt/qt_runtime.cpp \

WebCore/WebCore.vcproj/WebCore.vcproj
@@ -14183,6 +14183,10 @@
 					>
 				</File>
 				<File
+					RelativePath="..\bindings\js\ScriptControllerWin.cpp"
+					>
+				</File>
+				<File
 					RelativePath="..\bindings\js\StringSourceProvider.h"
 					>
 				</File>

commit 029ea68710f2a21d99f466ad9f1c93258415a619

Fix crash in Acid3 in Windows and Gtk ports, introduced in r35590.
Reviewed by Cameron Zwarich.

* bindings/js/ScriptControllerGtk.cpp: Fix the logic.
* bindings/js/ScriptControllerWin.cpp: Ditto.
* bindings/js/ScriptControllerQt.cpp: Switch to isPluginView for consistency with Gtk and Windows.

Canonical link: https://commits.webkit.org/28069@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@35660 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/bindings/js/ScriptControllerGtk.cpp
@@ -39,7 +39,7 @@ namespace WebCore {
 
 PassRefPtr<KJS::Bindings::Instance> ScriptController::createScriptInstanceForWidget(Widget* widget)
 {
-    if (widget->isPluginView())
+    if (!widget->isPluginView())
         return 0;
 
     return static_cast<PluginView*>(widget)->bindingInstance();

WebCore/bindings/js/ScriptControllerQt.cpp
@@ -48,7 +48,7 @@ namespace WebCore {
 
 PassRefPtr<KJS::Bindings::Instance> ScriptController::createScriptInstanceForWidget(WebCore::Widget* widget)
 {
-    if (widget->isFrameView())
+    if (!widget->isPluginView())
         return 0;
     if (widget->isNPAPIPlugin())
         return static_cast<PluginView*>(widget)->bindingInstance();

WebCore/bindings/js/ScriptControllerWin.cpp
@@ -36,7 +36,7 @@ namespace WebCore {
 
 PassRefPtr<KJS::Bindings::Instance> ScriptController::createScriptInstanceForWidget(Widget* widget)
 {
-    if (widget->isPluginView())
+    if (!widget->isPluginView())
         return 0;
 
     return static_cast<PluginView*>(widget)->bindingInstance();