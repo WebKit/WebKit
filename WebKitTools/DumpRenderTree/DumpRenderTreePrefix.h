7f23b6776af312be92e21d24202633e471285139
Stephanie Lewis <slewis@apple.com>
2008-09-07 07:12:45 +0000

    2008-09-07  Stephanie Lewis  <slewis@apple.com>
            Reviewed by Mark Rowe.
    
            Fix DRT build
    
            * DumpRenderTree/DumpRenderTreePrefix.h:
            * DumpRenderTree/win/DumpRenderTree.vcproj:
    
    
    Canonical link: https://commits.webkit.org/28540@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@36248 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/DumpRenderTree/DumpRenderTreePrefix.h
+++ WebKitTools/DumpRenderTree/DumpRenderTreePrefix.h
@@ -31,3 +31,8 @@
 #import <Foundation/Foundation.h>
 
 #endif
+
+// If we don't define these, they get defined in windef.h. 
+// We want to use std::min and std::max
+#define max max
+#define min min
