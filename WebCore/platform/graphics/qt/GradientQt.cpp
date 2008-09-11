0fd4c0c0920c11d557d0f158f26d923afa542fb6
Simon Hausmann <hausmann@webkit.org>
2008-09-11 07:37:42 +0000

    2008-09-11  Dirk Schulze  <vbs85@gmx.de>
            Reviewed by Simon.
    
            https://bugs.webkit.org/show_bug.cgi?id=20729
    
            Fixed canvas gradients for Qt
    
    Canonical link: https://commits.webkit.org/28622@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@36330 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/qt/GradientQt.cpp
+++ WebCore/platform/graphics/qt/GradientQt.cpp
@@ -46,15 +46,23 @@ QGradient* Gradient::platformGradient()
         return m_gradient;
 
     if (m_radial)
-        m_gradient = new QRadialGradient(m_p0.x(), m_p0.y(), m_r0, m_p1.x(), m_p1.y());
+        m_gradient = new QRadialGradient(m_p1.x(), m_p1.y(), m_r1, m_p0.x(), m_p0.y());
     else
         m_gradient = new QLinearGradient(m_p0.x(), m_p0.y(), m_p1.x(), m_p1.y());
 
     QColor stopColor;
     Vector<ColorStop>::iterator stopIterator = m_stops.begin();;
+    qreal lastStop;
+    const qreal lastStopDiff = 0.0000001;
     while (stopIterator != m_stops.end()) {
         stopColor.setRgbF(stopIterator->red, stopIterator->green, stopIterator->blue, stopIterator->alpha);
-        m_gradient->setColorAt(stopIterator->stop, stopColor);
+        if (qFuzzyCompare(lastStop, qreal(stopIterator->stop)))
+            lastStop = stopIterator->stop + lastStopDiff;
+        else
+            lastStop = stopIterator->stop;
+        if (m_radial && m_r0)
+            lastStop = m_r0 / m_r1 + lastStop * (1.0f - m_r0 / m_r1);
+        m_gradient->setColorAt(lastStop, stopColor);
         ++stopIterator;
     }
 
