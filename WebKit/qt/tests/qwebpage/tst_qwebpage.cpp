b93e18ceca81ecee065ab8cfab3491d56104c09d
Tor Arne Vestbø <vestbo@webkit.org>
2008-09-11 09:53:13 +0000

    2008-09-11  Tor Arne Vestbø  <tavestbo@trolltech.com>
            Reviewed by Simon
    
            Fix QtWebKit autotest
    
    Canonical link: https://commits.webkit.org/28624@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@36332 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/qt/tests/qwebpage/tst_qwebpage.cpp
+++ WebKit/qt/tests/qwebpage/tst_qwebpage.cpp
@@ -258,13 +258,13 @@ void tst_QWebPage::userStyleSheet()
     m_page->setNetworkAccessManager(networkManager);
     networkManager->requestedUrls.clear();
 
-    m_page->settings()->setUserStyleSheetUrl(QUrl::fromEncoded("data:text/css,p { background-image: url(qrc:/does/not/exist.png);}"));
+    m_page->settings()->setUserStyleSheetUrl(QUrl("data:text/css,p { background-image: url('http://does.not/exist.png');}"));
     m_view->setHtml("<p>hello world</p>");
     QVERIFY(::waitForSignal(m_view, SIGNAL(loadFinished(bool))));
 
     QVERIFY(networkManager->requestedUrls.count() >= 2);
-    QCOMPARE(networkManager->requestedUrls.at(0), QUrl::fromEncoded("data:text/css,p { background-image: url(qrc:/does/not/exist.png);}"));
-    QCOMPARE(networkManager->requestedUrls.at(1), QUrl::fromEncoded("qrc:/does/not/exist.png"));
+    QCOMPARE(networkManager->requestedUrls.at(0), QUrl("data:text/css,p { background-image: url('http://does.not/exist.png');}"));
+    QCOMPARE(networkManager->requestedUrls.at(1), QUrl("http://does.not/exist.png"));
 }
 
 void tst_QWebPage::modified()
