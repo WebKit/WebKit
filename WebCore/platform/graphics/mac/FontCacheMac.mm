93a6cf97a1595800d6c72a3392f19afb829c4fd0
Dan Bernstein <mitz@apple.com>
2008-09-15 02:23:24 +0000

    WebCore:
            Rubber-stamped by Sam Weinig.
    
            - revert <http://trac.webkit.org/changeset/36382> because it exposed
              <https://bugs.webkit.org/show_bug.cgi?id=20846> and resulted in crashes
              on Tiger.
    
            * platform/graphics/mac/FontCacheMac.mm:
            (WebCore::FontCache::getFontDataForCharacters):
            (WebCore::FontCache::getLastResortFallbackFont):
    
    LayoutTests:
    
            Rubber-stamped by Sam Weinig.
    
            - revert <http://trac.webkit.org/changeset/36382> because it exposed
              <https://bugs.webkit.org/show_bug.cgi?id=20846> and resulted in crashes
              on Tiger.
    
            * platform/mac/fast/text/last-resort-font-expected.checksum: Removed.
            * platform/mac/fast/text/last-resort-font-expected.png: Removed.
            * platform/mac/fast/text/last-resort-font-expected.txt: Removed.
            * platform/mac/fast/text/last-resort-font.html: Removed.
    
    
    
    Canonical link: https://commits.webkit.org/28709@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@36420 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/mac/FontCacheMac.mm
+++ WebCore/platform/graphics/mac/FontCacheMac.mm
@@ -87,7 +87,7 @@ const SimpleFontData* FontCache::getFontDataForCharacters(const Font& font, cons
     if (!substituteFont && length == 1)
         substituteFont = wkGetFontInLanguageForCharacter(nsFont, characters[0]);
     if (!substituteFont)
-        return getCachedFontData(getLastResortFallbackFont(font.fontDescription()));
+        return 0;
 
     // Use the family name from the AppKit-supplied substitute font, requesting the
     // traits, weight, and size we want. One way this does better than the original
@@ -153,9 +153,20 @@ FontPlatformData* FontCache::getSimilarFontPlatformData(const Font& font)
 
 FontPlatformData* FontCache::getLastResortFallbackFont(const FontDescription& fontDescription)
 {
-    static AtomicString lastResort("LastResort");
+    static AtomicString timesStr("Times");
+    static AtomicString lucidaGrandeStr("Lucida Grande");
 
-    return getCachedFontPlatformData(fontDescription, lastResort);
+    // FIXME: Would be even better to somehow get the user's default font here.  For now we'll pick
+    // the default that the user would get without changing any prefs.
+    FontPlatformData* platformFont = getCachedFontPlatformData(fontDescription, timesStr);
+    if (!platformFont)
+        // The Times fallback will almost always work, but in the highly unusual case where
+        // the user doesn't have it, we fall back on Lucida Grande because that's
+        // guaranteed to be there, according to Nathan Taylor. This is good enough
+        // to avoid a crash at least.
+        platformFont = getCachedFontPlatformData(fontDescription, lucidaGrandeStr);
+
+    return platformFont;
 }
 
 void FontCache::getTraitsInFamily(const AtomicString& familyName, Vector<unsigned>& traitsMasks)
