81a57cbbacdd75bece12b68421cbd6c327ebcb02
Mark Rowe <mrowe@apple.com>
2008-09-16 23:04:56 +0000

    Tweak garbage-collect-after-string-appends.js to not fail when run twice or more in a row. This will hopefully address the intermittent failures seen on the build bot.
    Rubber-stamped by Sam Weinig.
    
    * fast/js/resources/garbage-collect-after-string-appends.js:
    
    Canonical link: https://commits.webkit.org/28812@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@36524 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/js/resources/garbage-collect-after-string-appends.js
+++ LayoutTests/fast/js/resources/garbage-collect-after-string-appends.js
@@ -2,6 +2,10 @@ description(
 "This test checks whether the GC collects after string appends."
 );
 
+// FIXME: This test appears to be highly tied to the details of how JS strings report memory
+// cost to the garbage collector.  It should be improved to be less tied to these implementation details.
+// <https://bugs.webkit.org/show_bug.cgi?id=20871>
+
 if (window.layoutTestController)
     layoutTestController.dumpAsText();
 
@@ -18,7 +22,7 @@ for (var i = 0; i < count; ++i) {
 
 // str has 128 chars in it.
 str = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
-count = 10;
+count = 11;
 for (var i = 0; i < count; ++i) {
     str += str;
 }
