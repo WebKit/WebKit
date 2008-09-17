23f8df92d5a6607e0a82734b3f1768d15caaeb11
Beth Dakin <bdakin@apple.com>
2008-09-17 21:35:02 +0000

    WebCore:
    2008-09-17  Beth Dakin  <bdakin@apple.com>
    
            Reviewed by Adam Roben.
    
            Fix for https://bugs.webkit.org/show_bug.cgi?id=20512 Invalid CSS
            code crashes Safari
            and corresponding: <rdar://problem/6173832>
    
            Reading through the spec, it seems like a function is not valid
            input for a counter. So this patch checks for that and bails in the
            case of invalid input.
    
            * css/CSSParser.cpp:
            (WebCore::CSSParser::parseCounterContent):
    
    LayoutTests:
    
    2008-09-17  Beth Dakin  <bdakin@apple.com>
    
            Reviewed by Adam Roben.
    
            Test for https://bugs.webkit.org/show_bug.cgi?id=20512 Invalid CSS
            code crashes Safari
            and corresponding: <rdar://problem/6173832>
    
            * fast/css/counters/counter-function-input-2-expected.txt: Added.
            * fast/css/counters/counter-function-input-2.html: Added.
            * fast/css/counters/counter-function-input-expected.txt: Added.
            * fast/css/counters/counter-function-input.html: Added.
    
    
    
    Canonical link: https://commits.webkit.org/28841@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@36559 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/css/CSSParser.cpp
+++ WebCore/css/CSSParser.cpp
@@ -2657,6 +2657,8 @@ PassRefPtr<CSSValue> CSSParser::parseCounterContent(CSSParserValueList* args, bo
         return 0;
     
     CSSParserValue* i = args->current();
+    if (i->unit == CSSParserValue::Function)
+        return 0;
     RefPtr<CSSPrimitiveValue> identifier = CSSPrimitiveValue::create(i->string, CSSPrimitiveValue::CSS_STRING);
 
     RefPtr<CSSPrimitiveValue> separator;
