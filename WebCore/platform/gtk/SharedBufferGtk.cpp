3a4832594c96ee5a9c09d2643004be8c713529aa
Jan Alonzo <jmalonzo@webkit.org>
2008-09-23 21:07:50 +0000

    2008-09-23  Marco Barisione  <marco.barisione@collabora.co.uk>
            Reviewed by Holger Freyther.
    
            http://bugs.webkit.org/show_bug.cgi?id=18987
            [GTK] Implement SharedBuffer::createWithContentsOfFile and
            KURL::fileSystemPath
    
            * GNUmakefile.am: Add KURLGtk.cpp and SharedBufferGtk.cpp.
            * platform/gtk/KURLGtk.cpp: Added.
            (WebCore::KURL::fileSystemPath): Implemented.
            * platform/gtk/SharedBufferGtk.cpp: Added.
            (WebCore::SharedBuffer::createWithContentsOfFile): Implemented.
            * platform/gtk/TemporaryLinkStubs.cpp: Remove the old stubs.
    
    Canonical link: https://commits.webkit.org/29086@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@36812 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/platform/gtk/SharedBufferGtk.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2008 Collabora Ltd.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "config.h"
+#include "SharedBuffer.h"
+
+#include "CString.h"
+
+#include <glib.h>
+
+
+namespace WebCore {
+
+PassRefPtr<SharedBuffer> SharedBuffer::createWithContentsOfFile(const String& filePath)
+{
+    if (filePath.isEmpty())
+        return 0;
+
+    gchar* filename = g_filename_from_utf8(filePath.utf8().data(), -1, 0, 0, 0);
+    if (!filename)
+        return 0;
+
+    gchar* contents;
+    gsize size;
+    GError* error = 0;
+    if (!g_file_get_contents(filename, &contents, &size, &error)) {
+        LOG_ERROR("Failed to fully read contents of file %s - %s", filePath.utf8().data(), error->message);
+        g_error_free(error);
+        g_free(filename);
+        return 0;
+    }
+
+    RefPtr<SharedBuffer> result = SharedBuffer::create(contents, size);
+    g_free(filename);
+    g_free(contents);
+
+    return result.release();
+}
+
+} // namespace WebCore
