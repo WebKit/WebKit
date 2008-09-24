910d0fb446a9aa9776bda9b6d3011d59175d0a3e
Kevin McCullough <kmccullough@apple.com>
2008-09-24 22:24:00 +0000

    2008-09-24  Kevin McCullough  <kmccullough@apple.com>
            Style change.
    
            * kjs/nodes.cpp:
            (JSC::statementListEmitCode):
    
    
    
    Canonical link: https://commits.webkit.org/29146@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@36872 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/nodes.cpp
+++ JavaScriptCore/kjs/nodes.cpp
@@ -1084,7 +1084,7 @@ static inline RegisterID* statementListEmitCode(StatementVector& statements, Cod
     StatementVector::iterator end = statements.end();
     for (StatementVector::iterator it = statements.begin(); it != end; ++it) {
         StatementNode* n = it->get();
-        if(!n->isLoop())
+        if (!n->isLoop())
             generator.emitDebugHook(WillExecuteStatement, n->firstLine(), n->lastLine());
         generator.emitNode(dst, n);
     }
