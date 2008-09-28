2730503cfb8e74d02dfe5ce33a0e1fecbe6f08d8
David Hyatt <hyatt@apple.com>
2008-09-28 09:33:46 +0000

    Fix wx bustage.
    Canonical link: https://commits.webkit.org/29304@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@37036 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/wx/MouseWheelEventWx.cpp
+++ WebCore/platform/wx/MouseWheelEventWx.cpp
@@ -34,6 +34,7 @@ namespace WebCore {
 PlatformWheelEvent::PlatformWheelEvent(const wxMouseEvent& event, const wxPoint& globalPoint)
     : m_position(event.GetPosition())
     , m_globalPosition(globalPoint)
+    , m_granularity(ScrollByLineWheelEvent)
     , m_shiftKey(event.ShiftDown())
     , m_ctrlKey(event.ControlDown())
     , m_altKey(event.AltDown())
@@ -41,15 +42,9 @@ PlatformWheelEvent::PlatformWheelEvent(const wxMouseEvent& event, const wxPoint&
     , m_deltaX(0) // wx doesn't support horizontal mouse wheel scrolling
     , m_deltaY(event.GetWheelRotation() / event.GetWheelDelta())
     , m_isAccepted(false)
-    , m_isContinuous(false)
-    , m_continuousDeltaX(0)
-    , m_continuousDeltaY(0)
 {
     // FIXME: retrieve the user setting for the number of lines to scroll on each wheel event
-    m_charsToScrollPerDelta = 1;
-    m_linesToScrollPerDelta = 1;
-    m_pageXScrollMode = false;
-    m_pageYScrollMode = false;
+    m_deltaY *= horizontalLineMultiplier();
 }
 
 }
