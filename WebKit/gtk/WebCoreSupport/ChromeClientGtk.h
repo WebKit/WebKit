de2611ce59c58dc6976a071d6b4673f6ee4f067e
Alp Toker <alp@webkit.org>
2008-10-03 12:59:50 +0000

    2008-10-03  Alp Toker  <alp@nuanti.com>
            Build fix following r37234. Remove addToDirtyRegion from the header
            too.
    
            * WebCoreSupport/ChromeClientGtk.h:
    
    
    Canonical link: https://commits.webkit.org/29496@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@37237 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/gtk/WebCoreSupport/ChromeClientGtk.h
+++ WebKit/gtk/WebCoreSupport/ChromeClientGtk.h
@@ -83,8 +83,7 @@ namespace WebKit {
         virtual bool tabsToLinks() const;
 
         virtual WebCore::IntRect windowResizerRect() const;
-        virtual void addToDirtyRegion(const WebCore::IntRect&);
-        
+
         virtual void repaint(const WebCore::IntRect&, bool contentChanged, bool immediate = false, bool repaintContentOnly = false);
         virtual void scroll(const WebCore::IntSize& scrollDelta, const WebCore::IntRect& rectToScroll, const WebCore::IntRect& clipRect);
         virtual WebCore::IntPoint screenToWindow(const WebCore::IntPoint&) const;
