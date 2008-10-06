818bfcdcaa07eae3ca15ffb315a9250f36885e71
Maciej Stachowiak <mjs@apple.com>
2008-10-06 18:19:20 +0000

    2008-10-06  Maciej Stachowiak  <mjs@apple.com>
            Not reviewed.
    
            - fix build; toBoolean no longer takes an ExecState
    
            * JSUtils.cpp:
            (KJSValueToCFTypeInternal):
    
    
    
    Canonical link: https://commits.webkit.org/29592@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@37336 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptGlue/JSUtils.cpp
+++ JavaScriptGlue/JSUtils.cpp
@@ -207,7 +207,7 @@ CFTypeRef KJSValueToCFTypeInternal(JSValue *inValue, ExecState *exec, ObjectImpL
 
         if (inValue->isBoolean())
             {
-                result = inValue->toBoolean(exec) ? kCFBooleanTrue : kCFBooleanFalse;
+                result = inValue->toBoolean() ? kCFBooleanTrue : kCFBooleanFalse;
                 RetainCFType(result);
                 return result;
             }
