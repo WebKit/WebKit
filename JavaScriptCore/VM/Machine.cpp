c5a762faedf52b8ca6cdd19b4e16e9bc69a0e04f
Maciej Stachowiak <mjs@apple.com>
2008-10-08 03:46:34 +0000

    2008-10-07  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Mark Rowe.
    
            - optimize away multiplication by constant 1.0
    
            2.3% speedup on v8 RayTrace benchmark
    
            Apparently it's not uncommon for JavaScript code to multiply by
            constant 1.0 in the mistaken belief that this converts integer to
            floating point and that there is any operational difference.
    
            * VM/CTI.cpp:
            (JSC::CTI::privateCompileMainPass): Optimize to_jsnumber for
            case where parameter is already number.
            (JSC::CTI::privateCompileSlowCases): ditto
            * VM/Machine.cpp:
            (JSC::Machine::privateExecute): ditto
            * kjs/grammar.y:
            (makeMultNode): Transform as follows:
            +FOO * BAR ==> FOO * BAR
            FOO * +BAR ==> FOO * BAR
            FOO * 1 ==> +FOO
            1 * FOO ==> +FOO
            (makeDivNode): Transform as follows:
            +FOO / BAR ==> FOO / BAR
            FOO / +BAR ==> FOO / BAR
            (makeSubNode): Transform as follows:
            +FOO - BAR ==> FOO - BAR
            FOO - +BAR ==> FOO - BAR
            * kjs/nodes.h:
            (JSC::ExpressionNode::stripUnaryPlus): Helper for above
            grammar.y changes
            (JSC::UnaryPlusNode::stripUnaryPlus): ditto
    
    
    
    Canonical link: https://commits.webkit.org/29652@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@37405 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/VM/Machine.cpp
+++ JavaScriptCore/VM/Machine.cpp
@@ -1815,10 +1815,16 @@ JSValue* Machine::privateExecute(ExecutionFlag flag, RegisterFile* registerFile,
         */
         int dst = (++vPC)->u.operand;
         int src = (++vPC)->u.operand;
-        JSValue* result = r[src].jsValue(exec)->toJSNumber(exec);
-        VM_CHECK_EXCEPTION();
 
-        r[dst] = result;
+        JSValue* srcVal = r[src].jsValue(exec);
+
+        if (LIKELY(JSImmediate::isNumber(srcVal) || static_cast<JSCell*>(srcVal)->structureID()->typeInfo().type() == NumberType))
+            r[dst] = r[src];
+        else {
+            JSValue* result = srcVal->toJSNumber(exec);
+            VM_CHECK_EXCEPTION();
+            r[dst] = result;
+        }
 
         ++vPC;
         NEXT_OPCODE;
