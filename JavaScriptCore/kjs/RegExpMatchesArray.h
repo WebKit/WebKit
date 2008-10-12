d75ef1009d1cce7b4ed2648384214d98a9ca436a
Cameron Zwarich <cwzwarich@webkit.org>
2008-10-12 05:00:28 +0000

    2008-10-11  Cameron Zwarich  <zwarich@apple.com>
            Reviewed by Sam Weinig.
    
            Clean up RegExpMatchesArray.h to match our coding style.
    
            * kjs/RegExpMatchesArray.h:
            (JSC::RegExpMatchesArray::getOwnPropertySlot):
            (JSC::RegExpMatchesArray::put):
            (JSC::RegExpMatchesArray::deleteProperty):
            (JSC::RegExpMatchesArray::getPropertyNames):
    
    
    Canonical link: https://commits.webkit.org/29764@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@37520 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/kjs/RegExpMatchesArray.h
+++ JavaScriptCore/kjs/RegExpMatchesArray.h
@@ -24,21 +24,62 @@
 
 namespace JSC {
 
-class RegExpMatchesArray : public JSArray {
-public:
-    RegExpMatchesArray(ExecState*, RegExpConstructorPrivate*);
-    virtual ~RegExpMatchesArray();
+    class RegExpMatchesArray : public JSArray {
+    public:
+        RegExpMatchesArray(ExecState*, RegExpConstructorPrivate*);
+        virtual ~RegExpMatchesArray();
 
-private:
-    virtual bool getOwnPropertySlot(ExecState* exec, const Identifier& propertyName, PropertySlot& slot) { if (lazyCreationData()) fillArrayInstance(exec); return JSArray::getOwnPropertySlot(exec, propertyName, slot); }
-    virtual bool getOwnPropertySlot(ExecState* exec, unsigned propertyName, PropertySlot& slot) { if (lazyCreationData()) fillArrayInstance(exec); return JSArray::getOwnPropertySlot(exec, propertyName, slot); }
-    virtual void put(ExecState* exec, const Identifier& propertyName, JSValue* v, PutPropertySlot& slot) { if (lazyCreationData()) fillArrayInstance(exec); JSArray::put(exec, propertyName, v, slot); }
-    virtual void put(ExecState* exec, unsigned propertyName, JSValue* v) { if (lazyCreationData()) fillArrayInstance(exec); JSArray::put(exec, propertyName, v); }
-    virtual bool deleteProperty(ExecState* exec, const Identifier& propertyName) { if (lazyCreationData()) fillArrayInstance(exec); return JSArray::deleteProperty(exec, propertyName); }
-    virtual bool deleteProperty(ExecState* exec, unsigned propertyName) { if (lazyCreationData()) fillArrayInstance(exec); return JSArray::deleteProperty(exec, propertyName); }
-    virtual void getPropertyNames(ExecState* exec, PropertyNameArray& arr) { if (lazyCreationData()) fillArrayInstance(exec); JSArray::getPropertyNames(exec, arr); }
+    private:
+        virtual bool getOwnPropertySlot(ExecState* exec, const Identifier& propertyName, PropertySlot& slot)
+        {
+            if (lazyCreationData())
+                fillArrayInstance(exec);
+            return JSArray::getOwnPropertySlot(exec, propertyName, slot);
+        }
 
-    void fillArrayInstance(ExecState*);
+        virtual bool getOwnPropertySlot(ExecState* exec, unsigned propertyName, PropertySlot& slot)
+        {
+            if (lazyCreationData())
+                fillArrayInstance(exec);
+            return JSArray::getOwnPropertySlot(exec, propertyName, slot);
+        }
+
+        virtual void put(ExecState* exec, const Identifier& propertyName, JSValue* v, PutPropertySlot& slot)
+        {
+            if (lazyCreationData())
+                fillArrayInstance(exec);
+            JSArray::put(exec, propertyName, v, slot);
+        }
+
+        virtual void put(ExecState* exec, unsigned propertyName, JSValue* v)
+        {
+            if (lazyCreationData())
+                fillArrayInstance(exec);
+            JSArray::put(exec, propertyName, v);
+        }
+
+        virtual bool deleteProperty(ExecState* exec, const Identifier& propertyName)
+        {
+            if (lazyCreationData())
+                fillArrayInstance(exec);
+            return JSArray::deleteProperty(exec, propertyName);
+        }
+
+        virtual bool deleteProperty(ExecState* exec, unsigned propertyName)
+        {
+            if (lazyCreationData())
+                fillArrayInstance(exec);
+            return JSArray::deleteProperty(exec, propertyName);
+        }
+
+        virtual void getPropertyNames(ExecState* exec, PropertyNameArray& arr)
+        {
+            if (lazyCreationData())
+                fillArrayInstance(exec);
+            JSArray::getPropertyNames(exec, arr);
+        }
+
+        void fillArrayInstance(ExecState*);
 };
 
 }
