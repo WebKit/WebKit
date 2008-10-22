ed3066a7927111fc7358fdd5bac3e5fd11b7baee
Brady Eidson <beidson@apple.com>
2008-10-22 23:06:53 +0000

    Reviewed by Adam Roben
            <rdar://6261773> - autocomplete="off" doesn't work on Windows
    
            Visual Studio makes a poor decision regarding the combination of enums and bitfields, such that a
            statement like "m_autocomplete = Off" followed by "return m_autocomplete == Off" would return "false"
            instead of the much more correct "true."
    
            In the past we have worked around this by declaring the bitfield member as an unsigned instead
            of the enum type.
    
            For more discussion, see http://trac.webkit.org/changeset/25329
    
            * html/HTMLInputElement.h:  Work around insane Visual Studio enum issue  *sigh*
    
    
    Canonical link: https://commits.webkit.org/30018@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@37793 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/html/HTMLInputElement.h
+++ WebCore/html/HTMLInputElement.h
@@ -244,7 +244,7 @@ private:
     bool m_indeterminate : 1;
     bool m_haveType : 1;
     bool m_activeSubmit : 1;
-    AutoCompleteSetting m_autocomplete : 2;
+    unsigned m_autocomplete : 2; // AutoCompleteSetting
     bool m_autofilled : 1;
     bool m_inited : 1;
     bool m_placeholderShouldBeVisible : 1;
