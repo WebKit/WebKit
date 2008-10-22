16660d2d03024304500166922edd582a85462ccc
Darin Fisher <darin@chromium.org>
2008-10-22 16:47:02 +0000

    2008-10-22  Darin Fisher  <darin@chromium.org>
            Reviewed by Eric Seidel.
    
            Should not define PLATFORM(WIN,MAC,GTK) when PLATFORM(CHROMIUM) is defined
            https://bugs.webkit.org/show_bug.cgi?id=21757
    
            PLATFORM(CHROMIUM) implies HAVE_ACCESSIBILITY
    
            * wtf/Platform.h:
    
    
    
    Canonical link: https://commits.webkit.org/30008@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@37783 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/Platform.h
+++ JavaScriptCore/wtf/Platform.h
@@ -91,18 +91,16 @@
 #define WTF_PLATFORM_UNIX 1
 #endif
 
-/* PLATFORM(CHROMIUM) */
-#if defined(BUILDING_CHROMIUM__)
-#define WTF_PLATFORM_CHROMIUM 1
-#endif
-
 /* Operating environments */
 
+/* PLATFORM(CHROMIUM) */
 /* PLATFORM(QT) */
 /* PLATFORM(GTK) */
 /* PLATFORM(MAC) */
 /* PLATFORM(WIN) */
-#if defined(BUILDING_QT__)
+#if defined(BUILDING_CHROMIUM__)
+#define WTF_PLATFORM_CHROMIUM 1
+#elif defined(BUILDING_QT__)
 #define WTF_PLATFORM_QT 1
 
 /* PLATFORM(KDE) */
@@ -281,7 +279,7 @@
 #endif
 #endif
 
-#if PLATFORM(MAC) || PLATFORM(WIN) || PLATFORM(GTK)
+#if PLATFORM(MAC) || PLATFORM(WIN) || PLATFORM(GTK) || PLATFORM(CHROMIUM)
 #define HAVE_ACCESSIBILITY 1
 #endif
 
