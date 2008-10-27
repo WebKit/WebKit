f4524d4facab8792e77b80724e2d82592b1f0d78
Anders Carlsson <andersca@apple.com>
2008-10-27 18:52:49 +0000

    2008-10-27  Anders Carlsson  <andersca@apple.com>
            Reviewed by Kevin Decker.
    
            <rdar://problem/6322650> Crash in fast/loader/simultaneous-reloads-assert.html
    
            Make sure to null check the NSURLConnection object.
    
            * platform/network/mac/ResourceHandleMac.mm:
            (WebCore::ResourceHandle::start):
            (WebCore::ResourceHandle::setDefersLoading):
    
    
    
    Canonical link: https://commits.webkit.org/30119@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@37897 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/network/mac/ResourceHandleMac.mm
+++ WebCore/platform/network/mac/ResourceHandleMac.mm
@@ -199,11 +199,13 @@ bool ResourceHandle::start(Frame* frame)
 #endif
     d->m_connection = connection;
     [connection release];
-    if (d->m_defersLoading)
-        wkSetNSURLConnectionDefersCallbacks(d->m_connection.get(), YES);
 
-    if (d->m_connection)
+    if (d->m_connection) {
+        if (d->m_defersLoading)
+            wkSetNSURLConnectionDefersCallbacks(d->m_connection.get(), YES);
+
         return true;
+    }
 
     END_BLOCK_OBJC_EXCEPTIONS;
 
@@ -220,7 +222,8 @@ void ResourceHandle::cancel()
 void ResourceHandle::setDefersLoading(bool defers)
 {
     d->m_defersLoading = defers;
-    wkSetNSURLConnectionDefersCallbacks(d->m_connection.get(), defers);
+    if (d->m_connection)
+        wkSetNSURLConnectionDefersCallbacks(d->m_connection.get(), defers);
 }
 
 void ResourceHandle::schedule(SchedulePair* pair)
