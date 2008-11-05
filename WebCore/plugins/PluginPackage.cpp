d25eb00624b8803820f5cf3dbc2097c3e6db2ea8
Steve Falkenburg <sfalken@apple.com>
2008-11-05 18:23:10 +0000

    2008-11-05  Steve Falkenburg  <sfalken@apple.com>
            Build fix.
    
            * plugins/PluginPackage.cpp:
    
    
    
    Canonical link: https://commits.webkit.org/30345@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@38127 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/plugins/PluginPackage.cpp
+++ WebCore/plugins/PluginPackage.cpp
@@ -187,6 +187,7 @@ void PluginPackage::determineQuirks(const String& mimeType)
 }
 #endif
 
+#if !PLATFORM(WIN)
 void PluginPackage::determineModuleVersionFromDescription()
 {
     // It's a bit lame to detect the plugin version by parsing it
@@ -234,5 +235,6 @@ void PluginPackage::determineModuleVersionFromDescription()
         }
     }
 }
+#endif
 
 }
