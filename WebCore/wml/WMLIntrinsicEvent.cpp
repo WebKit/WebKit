661faf90e41b9e3936cd68be0990549309d567da
Nikolas Zimmermann <zimmermann@webkit.org>
2008-11-22 18:47:35 +0000

    Reviewed by Holger Freyther. Fixes: https://bugs.webkit.org/show_bug.cgi?id=22426
    Add WMLInstrinsicEvent/EventHandler implementation. Code not yet used.
    
    
    Canonical link: https://commits.webkit.org/30902@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@38693 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/wml/WMLIntrinsicEvent.cpp
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2008 Torch Mobile Inc. All rights reserved.
+ *               http://www.torchmobile.com/
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "config.h"
+
+#if ENABLE(WML)
+#include "WMLIntrinsicEvent.h"
+
+#include "WMLElementFactory.h"
+#include "WMLNames.h"
+#include "WMLTaskElement.h"
+
+namespace WebCore {
+
+static PassRefPtr<WMLTaskElement> createTaskElement(Document* document)
+{
+    return adoptRef(static_cast<WMLTaskElement*>(WMLElementFactory::createWMLElement(WMLNames::goTag, document, false)));
+}
+
+WMLIntrinsicEvent::WMLIntrinsicEvent(Document* document)
+    : m_taskElement(createTaskElement(document))
+{
+}
+
+WMLIntrinsicEvent::WMLIntrinsicEvent(WMLTaskElement* taskElement)
+    : m_taskElement(taskElement)
+{
+}
+
+}
+
+#endif
---
+++ WebCore/wml/WMLIntrinsicEventHandler.cpp
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2008 Torch Mobile Inc. All rights reserved.
+ *               http://www.torchmobile.com/
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "config.h"
+
+#if ENABLE(WML)
+#include "WMLIntrinsicEventHandler.h"
+
+#include "WMLIntrinsicEvent.h"
+#include "WMLTaskElement.h"
+
+namespace WebCore {
+
+WMLIntrinsicEventHandler::WMLIntrinsicEventHandler()
+{
+}
+
+void WMLIntrinsicEventHandler::registerIntrinsicEvent(WMLIntrinsicEventType type, PassRefPtr<WMLIntrinsicEvent> event)
+{
+    if (m_events.contains(type)) {
+        // FIXME: error reporting
+        // WMLHelper::tokenizer()->reportError(ConflictingEventBindingError);
+        return;
+    }
+
+    m_events.set(type, event);
+}
+
+void WMLIntrinsicEventHandler::triggerIntrinsicEvent(WMLIntrinsicEventType type) const
+{
+    RefPtr<WMLIntrinsicEvent> event = m_events.get(type);
+    ASSERT(event->taskElement());
+    event->taskElement()->executeTask(0);
+}
+
+bool WMLIntrinsicEventHandler::hasIntrinsicEvent(WMLIntrinsicEventType type) const
+{
+    return m_events.contains(type);
+}
+
+}
+
+#endif
