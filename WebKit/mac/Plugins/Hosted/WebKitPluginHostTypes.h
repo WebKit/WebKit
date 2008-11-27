efa819e3b70208d4cd95a23603050e9962d31a96
Anders Carlsson <andersca@apple.com>
2008-11-27 01:23:23 +0000

    2008-11-26  Anders Carlsson  <andersca@apple.com>
            Reviewed by Dan Bernstein.
    
            * Plugins/Hosted/WebKitPluginHost.defs:
            Add copyright headers.
    
            * Plugins/Hosted/WebKitPluginHostTypes.h: Added.
            Forgot to add this.
    
    
    
    Canonical link: https://commits.webkit.org/31011@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@38803 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebKit/mac/Plugins/Hosted/WebKitPluginHostTypes.h
@@ -0,0 +1,9 @@
+#ifndef WebKitPluginHostTypes_h
+#define WebKitPluginHostTypes_h
+
+typedef uint8_t * plist_bytes_t;
+typedef uint8_t * application_name_t;
+
+typedef char * data_t;
+
+#endif // WebKitPluginHostTypes_h
