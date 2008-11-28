543fc7cf1ba4d5d89544b1c9326f15f3a5a9a1bc
Simon Hausmann <hausmann@webkit.org>
2008-11-28 15:19:01 +0000

    2008-11-28  Joerg Bornemann  <joerg.bornemann@trolltech.com>
            Reviewed by Simon Hausmann.
    
            Implement lowResUTCTime() on Windows CE using GetSystemTime as _ftime() is not available.
    
    Canonical link: https://commits.webkit.org/31038@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@38830 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/runtime/DateMath.cpp
+++ JavaScriptCore/runtime/DateMath.cpp
@@ -341,9 +341,24 @@ static double highResUpTime()
 
 static double lowResUTCTime()
 {
+#if PLATFORM(WIN_CE)
+    SYSTEMTIME systemTime;
+    GetSystemTime(&systemTime);
+    struct tm tmtime;
+    tmtime.tm_year = systemTime.wYear - 1900;
+    tmtime.tm_mon = systemTime.wMonth - 1;
+    tmtime.tm_mday = systemTime.wDay;
+    tmtime.tm_wday = systemTime.wDayOfWeek;
+    tmtime.tm_hour = systemTime.wHour;
+    tmtime.tm_min = systemTime.wMinute;
+    tmtime.tm_sec = systemTime.wSecond;
+    time_t timet = mktime(&tmtime);
+    return timet * msPerSecond + systemTime.wMilliseconds;
+#else
     struct _timeb timebuffer;
     _ftime(&timebuffer);
     return timebuffer.time * msPerSecond + timebuffer.millitm;
+#endif
 }
 
 static bool qpcAvailable()
