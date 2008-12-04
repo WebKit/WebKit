f8e972ce97b786d920b179009af82d4833192ba5
Nikolas Zimmermann <zimmermann@webkit.org>
2008-12-04 00:37:27 +0000

    Reviewed by George Staikos. Forgot to rerun make-js-test-wrappers, and update some fast/wml results.
    Canonical link: https://commits.webkit.org/31184@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@38977 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/wml/resources/variable-reference-invalid-character.js
+++ LayoutTests/wml/resources/variable-reference-invalid-character.js
@@ -58,3 +58,5 @@ function executeTest() {
 
     completeTest();
 }
+
+var successfullyParsed = true;
--- LayoutTests/wml/resources/variable-reference-valid.js
+++ LayoutTests/wml/resources/variable-reference-valid.js
@@ -64,3 +64,5 @@ function executeTest() {
 
     completeTest();
 }
+
+var successfullyParsed = true;
