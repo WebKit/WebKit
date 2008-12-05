bce3510376c35d91b6c00fd0dc80b3b6127cd052
Kevin Ollivier <kevino@webkit.org>
2008-12-05 03:42:15 +0000

    Reviewed by Kevin Ollivier.
    Add a MIME mapping for the .htm extension to wx and GTK ports.
    
    https://bugs.webkit.org/show_bug.cgi?id=22668
    
    
    Canonical link: https://commits.webkit.org/31221@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@39014 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/wx/MimeTypeRegistryWx.cpp
+++ WebCore/platform/wx/MimeTypeRegistryWx.cpp
@@ -38,6 +38,7 @@ static const ExtensionMap extensionMap [] = {
     { "bmp", "image/bmp" },
     { "css", "text/css" },
     { "gif", "image/gif" },
+    { "htm", "text/html" },
     { "html", "text/html" },
     { "ico", "image/x-icon" },   
     { "jpeg", "image/jpeg" },
