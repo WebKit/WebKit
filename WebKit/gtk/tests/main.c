488df5c298598c80bcf96a7844eafb7b62ac6e25
Holger Freyther <zecke@webkit.org>
2008-12-07 19:16:17 +0000

    [Gtk+] Use glib's unit test facilities to test the WebKit/Gtk+ API
        https://bugs.webkit.org/show_bug.cgi?id=22491
    
        Add the skeleton to WebKit/gtk/tests and integrate that into the
        buildsystem. Testing support was added in glib 2.16. For versions
        using glib < 2.16 we compile an empty application.
    
    Canonical link: https://commits.webkit.org/31286@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@39082 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebKit/gtk/tests/main.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2008 Holger Hans Peter Freyther
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include <glib.h>
+#include <webkit/webkit.h>
+
+#if GLIB_CHECK_VERSION(2, 16, 0)
+
+int main(int argc, char** argv)
+{
+    g_thread_init(NULL);
+    gtk_test_init(&argc, &argv, NULL);
+
+    g_test_bug_base("https://bugs.webkit.org/");
+
+    return g_test_run ();
+}
+
+#else
+int main(int argc, char** argv)
+{
+    g_critical("You will need at least glib-2.16.0 to run the unit tests. Doing nothing now.");
+    return 0;
+}
+
+#endif
