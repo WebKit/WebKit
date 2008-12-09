53ac1ffe8d96d88dfc7c5736a958ee11dee66fb1
Darin Fisher <darin@chromium.org>
2008-12-09 20:43:56 +0000

    2008-12-09  Darin Fisher  <darin@chromium.org>
            Fixes bustages.
    
            https://bugs.webkit.org/show_bug.cgi?id=22631
            Adding missing files from previous commit.
    
            * bindings/js/ScriptCallFrame.cpp: Added.
            (WebCore::ScriptCallFrame::ScriptCallFrame):
            (WebCore::ScriptCallFrame::~ScriptCallFrame):
            (WebCore::ScriptCallFrame::argumentAt):
            * bindings/js/ScriptCallFrame.h: Added.
            (WebCore::ScriptCallFrame::functionName):
            (WebCore::ScriptCallFrame::sourceURL):
            (WebCore::ScriptCallFrame::lineNumber):
            (WebCore::ScriptCallFrame::argumentCount):
            * bindings/js/ScriptCallStack.cpp: Added.
            (WebCore::ScriptCallStack::ScriptCallStack):
            (WebCore::ScriptCallStack::~ScriptCallStack):
            (WebCore::ScriptCallStack::at):
            (WebCore::ScriptCallStack::size):
            (WebCore::ScriptCallStack::initialize):
            * bindings/js/ScriptCallStack.h: Added.
            (WebCore::ScriptCallStack::state):
    
    
    
    Canonical link: https://commits.webkit.org/31347@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@39143 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/bindings/js/ScriptCallStack.cpp
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2008, Google Inc. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "ScriptCallStack.h"
+
+#include <interpreter/CallFrame.h>
+#include <interpreter/Interpreter.h>
+#include <runtime/InternalFunction.h>
+#include <runtime/JSValue.h>
+#include <runtime/UString.h>
+#include <runtime/JSGlobalData.h>
+
+using namespace JSC;
+
+namespace WebCore {
+
+ScriptCallStack::ScriptCallStack(ExecState* exec, const ArgList& args, unsigned skipArgumentCount)
+    : m_initialized(false)
+    , m_exec(exec)
+    , m_caller(0)
+{
+    int signedLineNumber;
+    intptr_t sourceID;
+    UString urlString;
+    JSValue* function;
+
+    exec->interpreter()->retrieveLastCaller(exec, signedLineNumber, sourceID, urlString, function);
+
+    if (function) {
+        m_caller = asInternalFunction(function);
+        unsigned lineNumber = signedLineNumber >= 0 ? signedLineNumber : 0;
+        m_frames.append(ScriptCallFrame(m_caller->name(&m_exec->globalData()), urlString, lineNumber, exec, args, skipArgumentCount));
+    } else {
+        // Caller is unknown, but we should still add the frame, because
+        // something called us, and gave us arguments.
+        m_frames.append(ScriptCallFrame(UString(), UString(), 0, exec, args, skipArgumentCount));
+    }
+}
+
+ScriptCallStack::~ScriptCallStack()
+{
+}
+
+const ScriptCallFrame &ScriptCallStack::at(unsigned index)
+{
+    // First frame is pre-populated in constructor, so don't trigger
+    // initialization unless looking beyond the first frame.
+    if (index > 0)
+        initialize();
+    ASSERT(m_frames.size() > index);
+    return m_frames[index];
+}
+
+unsigned ScriptCallStack::size()
+{
+    initialize();
+    return m_frames.size();
+}
+
+void ScriptCallStack::initialize()
+{
+    if (!m_caller || m_initialized)
+        return;
+
+    JSValue* func = m_exec->interpreter()->retrieveCaller(m_exec, m_caller);
+    while (!func->isNull()) {
+        InternalFunction* internalFunction = asInternalFunction(func);
+        ArgList emptyArgList;
+        m_frames.append(ScriptCallFrame(internalFunction->name(&m_exec->globalData()), UString(), 0, 0, emptyArgList, 0));
+        func = m_exec->interpreter()->retrieveCaller(m_exec, internalFunction);
+    }
+    m_initialized = true;
+}
+
+} // namespace WebCore
