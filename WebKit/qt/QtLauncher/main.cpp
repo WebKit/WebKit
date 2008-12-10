8e581c345c1ca306b45599ab55e4a01d8d8ba291
Simon Hausmann <hausmann@webkit.org>
2008-12-10 15:59:24 +0000

    2008-12-09  Balazs Kelemen  <Kelemen.Balazs.3@stud.u-szeged.hu>
            Reviewed by Simon Hausmann.
    
            Extended QtLauncher so now it can load pages from an url list
            one by one. This can be useful for testing purposes (eq. leak hunting).
            The -r option activates this feature.
    
            https://bugs.webkit.org/show_bug.cgi?id=20932
    
    Canonical link: https://commits.webkit.org/31374@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@39170 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/qt/QtLauncher/main.cpp
+++ WebKit/qt/QtLauncher/main.cpp
@@ -42,6 +42,11 @@
 
 #include <QtUiTools/QUiLoader>
 
+#include <QVector>
+#include <QTextStream>
+#include <QFile>
+#include <cstdio>
+
 class WebPage : public QWebPage
 {
 public:
@@ -83,8 +88,12 @@ public:
         }
     }
 
-    QWebPage *webPage() const {
+    QWebPage* webPage() const {
         return view->page();
+    }
+
+    QWebView* webView() const {
+        return view;
     }
 
 protected slots:
@@ -322,6 +331,69 @@ QObject *WebPage::createPlugin(const QString &classId, const QUrl &url, const QS
     return loader.createWidget(classId, view());
 }
 
+class URLLoader : public QObject
+{
+    Q_OBJECT
+public:
+    URLLoader(QWebView* view, const QString& inputFileName)
+        : m_view(view)
+        , m_stdOut(stdout)
+    {
+        init(inputFileName);
+    }
+
+public slots:
+    void loadNext()
+    {
+        QString qstr;
+        if (getUrl(qstr)) {
+            QUrl url(qstr, QUrl::StrictMode);
+            if (url.isValid()) {
+                m_stdOut << "Loading " << qstr << " ......" << endl;
+                m_view->load(url);
+            } else
+                loadNext();
+        } else
+            disconnect(m_view, 0, this, 0);
+    }
+
+private:
+    void init(const QString& inputFileName)
+    {
+        QFile inputFile(inputFileName);
+        if (inputFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QTextStream stream(&inputFile);
+            QString line;
+            while (true) {
+                line = stream.readLine();
+                if (line.isNull())
+                    break;
+                m_urls.append(line);
+            }
+        } else {
+            qDebug() << "Cant't open list file";
+            exit(0);
+        }
+        m_index = 0;
+        inputFile.close();
+    }
+
+    bool getUrl(QString& qstr)
+    {
+        if (m_index == m_urls.size())
+            return false;
+
+        qstr = m_urls[m_index++];
+        return true;
+    }
+
+private:
+    QVector<QString> m_urls;
+    int m_index;
+    QWebView* m_view;
+    QTextStream m_stdOut;
+};
+
 #include "main.moc"
 
 int main(int argc, char **argv)
@@ -342,12 +414,26 @@ int main(int argc, char **argv)
     QWebSettings::globalSettings()->setAttribute(QWebSettings::DeveloperExtrasEnabled, true);
 
     const QStringList args = app.arguments();
-    if (args.count() > 1)
-        url = args.at(1);
 
-    MainWindow window(url);
-    window.show();
-
-    return app.exec();
+    // robotized
+    if (args.contains(QLatin1String("-r"))) {
+        QString listFile = args.at(2);
+        if (!(args.count() == 3) && QFile::exists(listFile)) {
+            qDebug() << "Usage: QtLauncher -r listfile";
+            exit(0);
+        }
+        MainWindow window(url);
+        QWebView *view = window.webView();
+        URLLoader loader(view, listFile);
+        QObject::connect(view, SIGNAL(loadFinished(bool)), &loader, SLOT(loadNext()));
+        window.show();
+        return app.exec();
+    } else {
+        if (args.count() > 1)
+            url = args.at(1);
+            
+        MainWindow window(url);
+        window.show();
+        return app.exec();
+    }
 }
-
