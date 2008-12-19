a5988cddc1f655be420e371fc48d095a603205bc
Pierre-Olivier Latour <pol@apple.com>
2008-12-19 00:39:21 +0000

    2008-12-18  Pierre-Olivier Latour  <pol@apple.com>
            Reviewed by Darin Adler.
    
            Rewrote some of the transition layout tests that were using setTimeout() to use the transition pause API in DRT when available.
    
            Rewrote the transition layout tests that were specifically testing the transition end event to use common JS code from transition-end-event-helpers.js.
    
            Fixed some typos.
    
            https://bugs.webkit.org/show_bug.cgi?id=22890
    
            * transitions/end-event-helpers.js: Removed.
            * transitions/interrupt-transform-transition-expected.txt:
            * transitions/interrupt-transform-transition.html:
            * transitions/opacity-transition-zindex.html:
            * transitions/start-transform-transition.html:
            * transitions/transform-op-list-match-expected.txt:
            * transitions/transform-op-list-match.html:
            * transitions/transform-op-list-no-match-expected.txt:
            * transitions/transform-op-list-no-match.html:
            * transitions/transition-end-event-all-properties-expected.txt:
            * transitions/transition-end-event-all-properties.html:
            * transitions/transition-end-event-attributes-expected.txt:
            * transitions/transition-end-event-attributes.html:
            * transitions/transition-end-event-container-expected.txt:
            * transitions/transition-end-event-container.html:
            * transitions/transition-end-event-helpers.js: Copied from LayoutTests/transitions/end-event-helpers.js.
            (recordTransitionEndEvent):
            (runTransitionTest.processEndEvents.compareEventInfo):
            (runTransitionTest.processEndEvents.examineResults):
            (runTransitionTest.processEndEvents):
            (runTransitionTest.startTest):
            (runTransitionTest.window):
            (runTransitionTest):
            * transitions/transition-end-event-left-expected.txt:
            * transitions/transition-end-event-left.html:
            * transitions/transition-end-event-multiple-01-expected.txt:
            * transitions/transition-end-event-multiple-01.html:
            * transitions/transition-end-event-multiple-02-expected.txt:
            * transitions/transition-end-event-multiple-02.html:
            * transitions/transition-end-event-multiple-03-expected.txt:
            * transitions/transition-end-event-multiple-03.html:
            * transitions/transition-end-event-multiple-04-expected.txt:
            * transitions/transition-end-event-multiple-04.html:
            * transitions/transition-end-event-nested-expected.txt:
            * transitions/transition-end-event-nested.html:
            * transitions/transition-end-event-set-none-expected.txt:
            * transitions/transition-end-event-set-none.html:
            * transitions/transition-end-event-transform-expected.txt:
            * transitions/transition-end-event-transform.html:
            * transitions/transition-end-event-window-expected.txt:
            * transitions/transition-end-event-window.html:
            * transitions/transition-test-helpers.js: Added.
            (runTransitionTest.isCloseEnough):
            (runTransitionTest.checkExpectedValue):
            (runTransitionTest.endTest):
            (runTransitionTest.runTest):
            (runTransitionTest.startTest):
            (runTransitionTest.if):
            * transitions/transition-timing-function-expected.txt: Copied from LayoutTests/transitions/transition-timing-function-software-expected.txt.
            * transitions/transition-timing-function-software-expected.txt: Removed.
            * transitions/transition-timing-function-software.html: Removed.
            * transitions/transition-timing-function.html: Copied from LayoutTests/transitions/transition-timing-function-software.html.
            * transitions/zero-duration-in-list-expected.txt:
            * transitions/zero-duration-in-list.html:
    
    
    
    Canonical link: https://commits.webkit.org/31587@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@39384 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/transitions/transition-end-event-helpers.js
@@ -0,0 +1,140 @@
+var _recordedEvents = new Array();
+
+/* Call this function to record manually transition end events:
+
+Function parameters:
+    event [required]: the event passed with "webkitTransitionEnd"
+
+*/
+function recordTransitionEndEvent(event)
+{
+  if (event.type != "webkitTransitionEnd")
+    throw("Invalid transition end event!");
+
+  _recordedEvents.push([
+    event.propertyName,
+    event.target.id,
+    Math.round(event.elapsedTime * 1000) / 1000 // round to ms to avoid floating point imprecision
+    ]);
+}
+
+/* This is the helper function to run transition end event tests:
+
+Test page requirements:
+- The body must contain an empty div with id "result"
+- The body must contain a div with with id "container"
+- Call this function directly from the <script> inside the test page
+
+Function parameters:
+    expected [required]: an array of arrays defining the expected parameter values for the recorded transition end events (see below)
+    timeout [required]: 
+    callback [optional]: a function to be executed just before the test starts (none by default)
+
+    Each sub-array must contain these items in this order:
+    - the name of the CSS property that was transitioning
+    - the id of the element on which the CSS property was transitioning
+    - the elapsed time in seconds at which the CSS property finished transitioning
+    - a boolean indicating if an event listener should be automatically added to the element to record the transition end event or if the script calls recordTransitionEndEvent() directly
+
+*/
+function runTransitionTest(expected, timeout, callback)
+{
+  
+  if (window.layoutTestController) {
+    layoutTestController.dumpAsText();
+    layoutTestController.waitUntilDone();
+  }
+  
+  function processEndEvents(expected)
+  {
+
+    function compareEventInfo(e1, e2)
+    {
+      // Sort by property name then event target id
+
+      // Index 0 is the property name
+      if (e1[0]<e2[0]) return -1;
+      if (e1[0]>e2[0]) return +1;
+
+      // Index 1 is the target id
+      if (e1[1]<e2[1]) return -1;
+      if (e1[1]>e2[1]) return +1;
+
+      return 0;
+    }
+
+    function examineResults(results, expected)
+    {
+      // Sort recorded and expected events arrays so they have the same ordering
+      expected.sort(compareEventInfo);
+      results.sort(compareEventInfo);
+
+      var result = '<p>';
+      for (var i=0; i < results.length && i < expected.length; ++i) {
+        var pass = expected[i][0] == results[i][0] && expected[i][1] == results[i][1] && expected[i][2] == results[i][2];
+
+        if (pass)
+          result += "PASS --- ";
+        else
+          result += "FAIL --- ";
+
+        result += "[Expected] Property: " + expected[i][0] + " ";
+        result += "Target: " + expected[i][1] + " ";
+        result += "Elapsed Time: " + expected[i][2];
+
+        if (!pass) {
+          result += " --- ";
+          result += "[Received] Property: " + results[i][0] + " ";
+          result += "Target: " + results[i][1] + " ";
+          result += "Elapsed Time: " + results[i][2];
+        }
+
+        result += "<br>";
+      }
+      result += "</p>";
+
+      if (expected.length > results.length) {
+        result += "<p>FAIL - Missing events<br>";
+        for (i=results.length; i < expected.length; ++i) {
+          result += "[Missing] Property: " + expected[i][0] + " ";
+          result += "Target: " + expected[i][1] + " ";
+          result += "Elapsed Time: " + expected[i][2] + "<br>";
+        }
+        result += "</p>";
+      } else if (expected.length < results.length) {
+        result += "<p>FAIL - Unexpected events<br>";
+        for (i=expected.length; i < results.length; ++i) {
+          result += "[Unexpected] Property: " + results[i][0] + " ";
+          result += "Target: " + results[i][1] + " ";
+          result += "Elapsed Time: " + results[i][2] + "<br>";
+        }
+        result += "</p>";
+      }
+
+      return result;
+    }
+
+    document.body.removeChild(document.getElementById('container'));
+    document.getElementById('result').innerHTML = examineResults(_recordedEvents, expected);
+
+    if (window.layoutTestController)
+        layoutTestController.notifyDone();
+  }
+
+  function startTest(expected, timeout, callback)
+  {
+    if (callback)
+      callback();
+    
+    for (var i=0; i < expected.length; ++i) {
+      if (expected[i][3]) {
+        var box = document.getElementById(expected[i][1]);
+        box.addEventListener("webkitTransitionEnd", recordTransitionEndEvent, false);
+      }
+    }
+    
+    window.setTimeout(function() { processEndEvents(expected); }, timeout * 1000);
+  }
+  
+  window.addEventListener('load', function() { startTest(expected, timeout, callback) }, false);
+}
---
+++ LayoutTests/transitions/transition-test-helpers.js
@@ -0,0 +1,137 @@
+/* This is the helper function to run transition tests:
+
+Test page requirements:
+- The body must contain an empty div with id "result"
+- Call this function directly from the <script> inside the test page
+
+Function parameters:
+    expected [required]: an array of arrays defining a set of CSS properties that must have given values at specific times (see below)
+    callback [optional]: a function to be executed just before the test starts (none by default)
+    
+    Each sub-array must contain these items in this order:
+    - the time in seconds at which to snapshot the CSS property
+    - the id of the element on which to get the CSS property value
+    - the name of the CSS property to get [1]
+    - the expected value for the CSS property
+    - the tolerance to use when comparing the effective CSS property value with its expected value
+    
+    [1] If the CSS property name is "-webkit-transform", expected value must be an array of 1 or more numbers corresponding to the matrix elements,
+    or a string which will be compared directly (useful if the expected value is "none")
+    If the CSS property name is "-webkit-transform.N", expected value must be a number corresponding to the Nth element of the matrix
+
+*/
+function runTransitionTest(expected, callback)
+{
+    var result = "";
+    var hasPauseTransitionAPI = window.layoutTestController && layoutTestController.pauseTransitionAtTimeOnElementWithId;
+
+    function isCloseEnough(actual, desired, tolerance)
+    {
+        var diff = Math.abs(actual - desired);
+        return diff <= tolerance;
+    }
+
+    function checkExpectedValue(expected, index)
+    {
+        var time = expected[index][0];
+        var elementId = expected[index][1];
+        var property = expected[index][2];
+        var expectedValue = expected[index][3];
+        var tolerance = expected[index][4];
+
+        var computedValue;
+        var pass;
+        if (!property.indexOf("-webkit-transform")) {
+            computedValue = window.getComputedStyle(document.getElementById(elementId)).webkitTransform;
+
+            if (typeof expectedValue == "string")
+                pass = (computedValue == expectedValue);
+            else if (typeof expectedValue == "number") {
+                var m = computedValue.split("(");
+                var m = m[1].split(",");
+                pass = isCloseEnough(parseFloat(m[parseInt(property.substring(18))]), expectedValue, tolerance);
+            } else {
+                var m = computedValue.split("(");
+                var m = m[1].split(",");
+                for (i = 0; i < expectedValue.length; ++i) {
+                    pass = isCloseEnough(parseFloat(m[i]), expectedValue[i], tolerance);
+                    if (!pass)
+                        break;
+                }
+            }
+        } else if (property == "lineHeight") {
+            computedValue = parseInt(window.getComputedStyle(document.getElementById(elementId)).lineHeight);
+            pass = isCloseEnough(computedValue, expectedValue, tolerance);
+        } else {    
+            var computedStyle = window.getComputedStyle(document.getElementById(elementId)).getPropertyCSSValue(property);
+            computedValue = computedStyle.getFloatValue(CSSPrimitiveValue.CSS_NUMBER);
+            pass = isCloseEnough(computedValue, expectedValue, tolerance);
+        }
+
+        if (pass)
+            result += "PASS - \"" + property + "\" property for \"" + elementId + "\" element at " + time + "s saw something close to: " + expectedValue + "<br>";
+        else
+            result += "FAIL - \"" + property + "\" property for \"" + elementId + "\" element at " + time + "s expected: " + expectedValue + " but saw: " + computedValue + "<br>";
+    }
+
+    function endTest()
+    {
+        document.getElementById('result').innerHTML = result;
+
+        if (window.layoutTestController)
+            layoutTestController.notifyDone();
+    }
+
+    function checkExpectedValueCallback(expected, index)
+    {
+        return function() { checkExpectedValue(expected, index); };
+    }
+
+    function runTest(expected)
+    {
+        var maxTime = 0;
+
+        for (var i = 0; i < expected.length; ++i) {
+            var time = expected[i][0];
+            var elementId = expected[i][1];
+            var property = expected[i][2];
+            if (!property.indexOf("-webkit-transform"))
+            property = "-webkit-transform";
+
+            // We can only use the transition fast-forward mechanism if DRT implements pauseTransitionAtTimeOnElementWithId()
+            if (hasPauseTransitionAPI) {
+                layoutTestController.pauseTransitionAtTimeOnElementWithId(property, time, elementId);
+                checkExpectedValue(expected, i);
+            }
+            else {
+                if (time > maxTime)
+                    maxTime = time;
+
+                window.setTimeout(checkExpectedValueCallback(expected, i), time * 1000);
+            }
+        }
+
+        if (maxTime > 0)
+            window.setTimeout(endTest, maxTime * 1000 + 50);
+        else
+            endTest();
+    }
+    
+    function startTest(expected, callback)
+    {
+        if (callback)
+            callback();
+
+        window.setTimeout(function() { runTest(expected); }, 0);
+    }
+    
+    if (window.layoutTestController) {
+        layoutTestController.dumpAsText();
+        layoutTestController.waitUntilDone();
+    }
+    
+    if (!expected)
+        throw("Expected results are missing!");
+    
+    window.addEventListener("load", function() { startTest(expected, callback); }, false);
+}
