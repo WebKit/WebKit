60e7d1abcf012f7c2c94feb2afdb9e09e0741cc1
Nikolas Zimmermann <zimmermann@webkit.org>
2008-12-22 01:42:03 +0000

    Rubber-stamped by George Staikos. Fix copyright of the new RandomNumber* files.
    Canonical link: https://commits.webkit.org/31632@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@39429 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/RandomNumber.cpp
+++ JavaScriptCore/wtf/RandomNumber.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2006, 2007, 2008 Apple Inc. All rights reserved.
- *           (C) 2008 Nikolas Zimmermann <nikolas.zimmermann@torchmobile.com>
+ *           (C) 2008 Torch Mobile Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
--- JavaScriptCore/wtf/RandomNumber.h
+++ JavaScriptCore/wtf/RandomNumber.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 Nikolas Zimmermann <nikolas.zimmermann@torchmobile.com>
+ * Copyright (C) 2008 Torch Mobile Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
