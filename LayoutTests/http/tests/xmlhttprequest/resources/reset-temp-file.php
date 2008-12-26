3076caacd314ff82c40226550dd0198db84b73ae
Alexey Proskuryakov <ap@webkit.org>
2008-12-26 11:03:56 +0000

    2008-12-26  David Levin  <levin@chromium.org>
            Reviewed by Alexey Proskuryakov.
    
            https://bugs.webkit.org/show_bug.cgi?id=22950
            Added more logging to reset-temp-file to help expose potential issues that could cause test failures (on the tiger buildbot).
    
            If there are issues in reset-temp-file, they are printed from the test access-control-basic-allow-preflight-cache.html
    
            * http/tests/xmlhttprequest/access-control-basic-allow-preflight-cache.html:
            * http/tests/xmlhttprequest/resources/reset-temp-file.php:
    
    
    
    Canonical link: https://commits.webkit.org/31675@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@39476 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/xmlhttprequest/resources/reset-temp-file.php
+++ LayoutTests/http/tests/xmlhttprequest/resources/reset-temp-file.php
@@ -1,6 +1,20 @@
 <?php
 require_once 'portabilityLayer.php';
 
+if (!sys_get_temp_dir()) {
+    echo "FAIL: No temp dir was returned.\n";
+    exit();
+}
+
 $tmpFile = ensureTrailingSlash(sys_get_temp_dir()) . $_GET['filename'];
-unlink($tmpFile)
+
+
+if (!file_put_contents($tmpFile, "test")) {
+    echo "FAIL: unable to write to file" . $tmpFile . "\n";
+    exit();
+}
+if (!unlink($tmpFile)) {
+    echo "FAIL: Unable to delete file: " . $tmpFile . "\n";
+    exit();
+}
 ?>
