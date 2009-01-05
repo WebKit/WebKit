31d09e67500fa8f9d024cc58dd7199c29c6b1453
Adam Treat <treat@webkit.org>
2009-01-05 23:09:46 +0000

    Fix win build
    Canonical link: https://commits.webkit.org/31818@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@39619 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/win/WebCoreSupport/WebChromeClient.h
+++ WebKit/win/WebCoreSupport/WebChromeClient.h
@@ -97,7 +97,7 @@ public:
     virtual WebCore::IntPoint screenToWindow(const WebCore::IntPoint& p) const;
     virtual WebCore::IntRect windowToScreen(const WebCore::IntRect& r) const;
     virtual PlatformWidget platformWindow() const;
-    virtual void contentsSizeChanged(Frame*, const IntSize&) const;
+    virtual void contentsSizeChanged(WebCore::Frame*, const WebCore::IntSize&) const;
 
     virtual void mouseDidMoveOverElement(const WebCore::HitTestResult&, unsigned modifierFlags);
 
