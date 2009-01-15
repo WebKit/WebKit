9825dcc5ef8c9300100ccc0e5bed13e27d6b4830
Eric Seidel <eric@webkit.org>
2009-01-15 00:55:06 +0000

    2009-01-14  Michael Moss  <mmoss@chromium.org>
            Reviewed by Eric Seidel.
    
            Workaround a skia limitation on repeated patterns, and remove extra
            transformations applied to pattern.
            https://bugs.webkit.org/show_bug.cgi?id=23332
    
            Fixes:
            LayoutTests/fast/canvas/patternfill-repeat.html
            LayoutTests/svg/W3C-SVG-1.1/pservers-grad-06-b.svg
    
            * platform/graphics/skia/PatternSkia.cpp:
            (WebCore::Pattern::createPlatformPattern):
    
    Canonical link: https://commits.webkit.org/32119@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@39921 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/skia/PatternSkia.cpp
+++ WebCore/platform/graphics/skia/PatternSkia.cpp
@@ -34,22 +34,44 @@
 #include "TransformationMatrix.h"
 
 #include "SkShader.h"
+#include "SkCanvas.h"
 
 namespace WebCore {
 
-static inline SkShader::TileMode shaderRule(bool shouldRepeat)
-{
-    // FIXME: Skia does not have a "draw the tile only once" option
-    // Clamp draws the last line of the image after stopping repeating
-    return shouldRepeat ? SkShader::kRepeat_TileMode : SkShader::kClamp_TileMode;
-}
-
 PlatformPatternPtr Pattern::createPlatformPattern(const TransformationMatrix& patternTransform) const
 {
+    // Note: patternTransform is ignored since it seems to be applied elsewhere
+    // (when the pattern is used?). Applying it to the pattern (i.e.
+    // shader->setLocalMatrix) results in a double transformation. This can be
+    // seen, for instance, as an extra offset in:
+    // LayoutTests/fast/canvas/patternfill-repeat.html
+    // and expanded scale and skew in:
+    // LayoutTests/svg/W3C-SVG-1.1/pservers-grad-06-b.svg
+
     SkBitmap* bm = m_tileImage->nativeImageForCurrentFrame();
-    SkShader* shader = SkShader::CreateBitmapShader(*bm, shaderRule(m_repeatX), shaderRule(m_repeatY));
-    shader->setLocalMatrix(patternTransform);
-    return shader;
+    if (m_repeatX && m_repeatY)
+        return SkShader::CreateBitmapShader(*bm, SkShader::kRepeat_TileMode, SkShader::kRepeat_TileMode);
+
+    // Skia does not have a "draw the tile only once" option. Clamp_TileMode
+    // repeats the last line of the image after drawing one tile. To avoid
+    // filling the space with arbitrary pixels, this workaround forces the
+    // image to have a line of transparent pixels on the "repeated" edge(s),
+    // thus causing extra space to be transparent filled.
+    SkShader::TileMode tileModeX = m_repeatX ? SkShader::kRepeat_TileMode : SkShader::kClamp_TileMode;
+    SkShader::TileMode tileModeY = m_repeatY ? SkShader::kRepeat_TileMode : SkShader::kClamp_TileMode;
+    int expandW = m_repeatX ? 0 : 1;
+    int expandH = m_repeatY ? 0 : 1;
+
+    // Create a transparent bitmap 1 pixel wider and/or taller than the
+    // original, then copy the orignal into it.
+    // FIXME: Is there a better way to pad (not scale) an image in skia?
+    SkBitmap bm2;
+    bm2.setConfig(bm->config(), bm->width() + expandW, bm->height() + expandH);
+    bm2.allocPixels();
+    bm2.eraseARGB(0x00, 0x00, 0x00, 0x00);
+    SkCanvas canvas(bm2);
+    canvas.drawBitmap(*bm, 0, 0);
+    return SkShader::CreateBitmapShader(bm2, tileModeX, tileModeY);
 }
 
 } // namespace WebCore
