89724d6caef10bac97e46bf0ca068a82ffdd840c
Darin Fisher <darin@chromium.org>
2009-01-28 18:52:15 +0000

    2009-01-28  Adam Langley  <agl@google.com>
            Reviewed by Sam Weinig.
    
            https://bugs.webkit.org/show_bug.cgi?id=23573
            Make PlatformWidget on chromium an intptr_t to match changes made in Chromium
    
            * platform/chromium/PlatformWidget.h:
    
    
    
    Canonical link: https://commits.webkit.org/32502@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@40314 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/chromium/PlatformWidget.h
+++ WebCore/platform/chromium/PlatformWidget.h
@@ -31,10 +31,12 @@
 #ifndef PlatformWidget_h
 #define PlatformWidget_h
 
+#include <wtf/StdLibExtras.h>  // for intptr_t
+
 // PlatformWidget is an opaque identifier corresponding to whatever native
 // view type the embedder may use.  PlatformWidget CANNOT be assumed to be
 // a valid pointer.  Some embedders may not use this identifier at all.
 
-typedef void* PlatformWidget;
+typedef intptr_t PlatformWidget;
 
 #endif
