2a36db0e2659edd51dfbd6c5c8a903de6960a5fa
Alexey Proskuryakov <ap@webkit.org>
2009-01-30 13:13:28 +0000

    Reviewed by Mark Rowe.
            https://bugs.webkit.org/show_bug.cgi?id=23640
            Test that non-HTML main resources work with application cache correctly
    
            * http/tests/appcache/non-html-expected.txt: Added.
            * http/tests/appcache/non-html.xhtml: Added.
            * http/tests/appcache/resources/abe.png: Added.
            * http/tests/appcache/resources/non-html.manifest: Added.
    
            * http/tests/resources/network-simulator.php: Support more file extensions.
    
    
    
    Canonical link: https://commits.webkit.org/32584@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@40401 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/resources/network-simulator.php
+++ LayoutTests/http/tests/resources/network-simulator.php
@@ -30,6 +30,20 @@ function contentType($path)
         return "text/cache-manifest";
     if (preg_match("/\.js$/", $path))
         return "text/javascript";
+    if (preg_match("/\.xml$/", $path))
+        return "application/xml";
+    if (preg_match("/\.xhtml$/", $path))
+        return "application/xhtml+xml";
+    if (preg_match("/\.svg$/", $path))
+        return "application/svg+xml";
+    if (preg_match("/\.xsl$/", $path))
+        return "application/xslt+xml";
+    if (preg_match("/\.gif$/", $path))
+        return "image/gif";
+    if (preg_match("/\.jpg$/", $path))
+        return "image/jpeg";
+    if (preg_match("/\.png$/", $path))
+        return "image/png";
     return "text/plain";
 }
 
