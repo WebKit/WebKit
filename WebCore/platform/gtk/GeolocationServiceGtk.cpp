8324ca463d89cd68c089ce0b41a20b446aa9a66a
Holger Freyther <zecke@webkit.org>
2009-01-31 03:51:00 +0000

    [GTK] Implement GeolocationService using the Geoclue library
    https://bugs.webkit.org/show_bug.cgi?id=22022
    
    Untested implementation of the GeolocationService using the geoclue
    library. Velocity handling is completely missing and the accuracy
    handling might be wrong.
    
    Canonical link: https://commits.webkit.org/32624@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@40441 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/gtk/GeolocationServiceGtk.cpp
+++ WebCore/platform/gtk/GeolocationServiceGtk.cpp
@@ -20,6 +20,21 @@
 #include "config.h"
 #include "GeolocationServiceGtk.h"
 
+#include "CString.h"
+#include "GOwnPtr.h"
+#include "NotImplemented.h"
+#include "PositionOptions.h"
+
+namespace WTF {
+    template<> void freeOwnedGPtr<GeoclueAccuracy>(GeoclueAccuracy* accuracy)
+    {
+        if (!accuracy)
+            return;
+
+        geoclue_accuracy_free(accuracy);
+    }
+}
+
 namespace WebCore {
 
 GeolocationService* GeolocationService::create(GeolocationServiceClient* client)
@@ -29,33 +44,172 @@ GeolocationService* GeolocationService::create(GeolocationServiceClient* client)
 
 GeolocationServiceGtk::GeolocationServiceGtk(GeolocationServiceClient* client)
     : GeolocationService(client)
-{}
-
-bool GeolocationServiceGtk::startUpdating(PositionOptions*)
+    , m_geoclueClient(0)
+    , m_geocluePosition(0)
+    , m_latitude(0.0)
+    , m_longitude(0.0)
+    , m_altitude(0.0)
+    , m_altitudeAccuracy(0.0)
+    , m_timestamp(0)
 {
-    return false;
+}
+
+GeolocationServiceGtk::~GeolocationServiceGtk()
+{
+    if (m_geoclueClient)
+        g_object_unref(m_geoclueClient);
+
+    if (m_geocluePosition)
+        g_object_unref(m_geocluePosition);
+}
+
+//
+// 1.) Initialize Geoclue with our requirements
+// 2.) Try to get a GeocluePosition
+// 3.) Update the Information and get the current position
+//
+// TODO: Also get GeoclueVelocity but there is no master client
+//       API for that.
+//
+bool GeolocationServiceGtk::startUpdating(PositionOptions* options)
+{
+    ASSERT(!m_geoclueClient);
+
+    m_lastPosition = 0;
+    m_lastError = 0;
+
+    GOwnPtr<GError> error;
+    GeoclueMaster* master = geoclue_master_get_default();
+    GeoclueMasterClient* client = geoclue_master_create_client(master, 0, 0);
+    g_object_unref(master);
+
+    if (!client) {
+        setError(PositionError::LOCATION_PROVIDER_ERROR, "Could not connect to location provider.");
+        return false;
+    }
+
+    GeoclueAccuracyLevel accuracyLevel = GEOCLUE_ACCURACY_LEVEL_LOCALITY;
+    int timeout = 0;
+    if (options) {
+        accuracyLevel = options->enableHighAccuracy() ? GEOCLUE_ACCURACY_LEVEL_DETAILED : GEOCLUE_ACCURACY_LEVEL_LOCALITY;
+        timeout = options->timeout();
+    }
+
+    gboolean result = geoclue_master_client_set_requirements(client, accuracyLevel, timeout,
+                                                             true, GEOCLUE_RESOURCE_ALL, &error.outPtr());
+
+    if (!result) {
+        setError(PositionError::LOCATION_PROVIDER_ERROR, error->message);
+        g_object_unref(client);
+        return false;
+    }
+
+    m_geocluePosition = geoclue_master_client_create_position(client, &error.outPtr());
+    if (!m_geocluePosition) {
+        setError(PositionError::LOCATION_PROVIDER_ERROR, error->message);
+        g_object_unref(client);
+        return false;
+    }
+
+    g_signal_connect(G_OBJECT(m_geocluePosition), "position-changed",
+                     G_CALLBACK(position_changed), this);
+
+    m_geoclueClient = client;
+    updateLocationInformation();
+
+    return result;
 }
 
 void GeolocationServiceGtk::stopUpdating()
 {
+    if (!m_geoclueClient)
+        return;
+
+    g_object_unref(m_geocluePosition);
+    g_object_unref(m_geoclueClient);
+
+    m_geocluePosition = 0;
+    m_geoclueClient = 0;
 }
 
 void GeolocationServiceGtk::suspend()
 {
+    // not available with geoclue
+    notImplemented();
 }
 
 void GeolocationServiceGtk::resume()
 {
+    // not available with geoclue
+    notImplemented();
 }
 
 Geoposition* GeolocationServiceGtk::lastPosition() const
 {
-    return 0;
+    return m_lastPosition.get();
 }
 
 PositionError* GeolocationServiceGtk::lastError() const
 {
-    return 0;
+    return m_lastError.get();
+}
+
+void GeolocationServiceGtk::updateLocationInformation()
+{
+    ASSERT(m_geocluePosition);
+
+    GOwnPtr<GError> error;
+    GOwnPtr<GeoclueAccuracy> accuracy;
+
+    GeocluePositionFields fields = geoclue_position_get_position(m_geocluePosition, &m_timestamp,
+                                                                 &m_latitude, &m_longitude,
+                                                                 &m_altitude, &accuracy.outPtr(),
+                                                                 &error.outPtr());
+    if (error) {
+        setError(PositionError::POSITION_NOT_FOUND_ERROR, error->message);
+        return;
+    } else if (fields & GEOCLUE_POSITION_FIELDS_LATITUDE && fields & GEOCLUE_POSITION_FIELDS_LONGITUDE) {
+        setError(PositionError::POSITION_NOT_FOUND_ERROR, "Position could not be determined.");
+        return;
+    }
+
+
+    // The m_altitudeAccuracy accuracy is likely to be wrong
+    GeoclueAccuracyLevel level;
+    geoclue_accuracy_get_details(accuracy.get(), &level, &m_accuracy, &m_altitudeAccuracy);
+    updatePosition();
+}
+
+void GeolocationServiceGtk::updatePosition()
+{
+    m_lastError = 0;
+    m_lastPosition = Geoposition::create(m_latitude, m_longitude, m_altitude,
+                                         m_accuracy, m_altitudeAccuracy, 0.0,
+                                         0.0, m_timestamp*1000.0);
+    positionChanged();
+}
+
+void GeolocationServiceGtk::position_changed(GeocluePosition*, GeocluePositionFields fields, int timestamp, double latitude, double longitude, double altitude, GeoclueAccuracy* accuracy, GeolocationServiceGtk* that)
+{
+    if (fields & GEOCLUE_POSITION_FIELDS_LATITUDE && fields & GEOCLUE_POSITION_FIELDS_LONGITUDE) {
+        that->setError(PositionError::POSITION_NOT_FOUND_ERROR, "Position could not be determined.");
+        return;
+    }
+
+    that->m_timestamp = timestamp;
+    that->m_latitude = latitude;
+    that->m_longitude = longitude;
+    that->m_altitude = altitude;
+
+    GeoclueAccuracyLevel level;
+    geoclue_accuracy_get_details(accuracy, &level, &that->m_accuracy, &that->m_altitudeAccuracy);
+    that->updatePosition();
+}
+
+void GeolocationServiceGtk::setError(PositionError::ErrorCode errorCode, const char* message)
+{
+    m_lastPosition = 0;
+    m_lastError = PositionError::create(errorCode, String::fromUTF8(message));
 }
 
 }
--- WebCore/platform/gtk/GeolocationServiceGtk.h
+++ WebCore/platform/gtk/GeolocationServiceGtk.h
@@ -21,11 +21,18 @@
 #define GeolocationServiceGtk_h
 
 #include "GeolocationService.h"
+#include "Geoposition.h"
+#include "PositionError.h"
+#include "RefPtr.h"
+
+#include <geoclue/geoclue-master.h>
+#include <geoclue/geoclue-position.h>
 
 namespace WebCore {
     class GeolocationServiceGtk : public GeolocationService {
     public:
         GeolocationServiceGtk(GeolocationServiceClient*);
+        ~GeolocationServiceGtk();
 
         virtual bool startUpdating(PositionOptions*);
         virtual void stopUpdating();
@@ -35,6 +42,29 @@ namespace WebCore {
 
         Geoposition* lastPosition() const;
         PositionError* lastError() const;
+
+    private:
+        void updateLocationInformation();
+        void setError(PositionError::ErrorCode, const char* message);
+        void updatePosition();
+
+        static void position_changed(GeocluePosition*, GeocluePositionFields, int, double, double, double, GeoclueAccuracy*, GeolocationServiceGtk*);
+
+    private:
+        RefPtr<Geoposition> m_lastPosition;
+        RefPtr<PositionError> m_lastError;
+
+        // state objects
+        GeoclueMasterClient* m_geoclueClient;
+        GeocluePosition* m_geocluePosition;
+
+        // Error and Position state
+        double m_latitude;
+        double m_longitude;
+        double m_altitude;
+        double m_accuracy;
+        double m_altitudeAccuracy;
+        int m_timestamp;
     };
 }
 
