eefd908e0fb2c49f491c33370fd334e0d3ba9d85
Anders Carlsson <andersca@apple.com>
2009-02-02 23:44:23 +0000

    2009-02-02  Anders Carlsson  <andersca@apple.com>
            Build fix.
    
            * WebCoreSupport/FrameLoaderClientGtk.h:
    
    
    
    Canonical link: https://commits.webkit.org/32679@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@40496 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.h
+++ WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.h
@@ -109,7 +109,7 @@ namespace WebKit {
 
         virtual PassRefPtr<WebCore::Frame> createFrame(const WebCore::KURL& url, const WebCore::String& name, WebCore::HTMLFrameOwnerElement* ownerElement,
                                    const WebCore::String& referrer, bool allowsScrolling, int marginWidth, int marginHeight);
-        virtual WebCore::Widget* createPlugin(const WebCore::IntSize&, WebCore::Element*, const WebCore::KURL&, const WTF::Vector<WebCore::String>&, const WTF::Vector<WebCore::String>&, const WebCore::String&, bool);
+        virtual WebCore::Widget* createPlugin(const WebCore::IntSize&, WebCore::HTMLPlugInElement*, const WebCore::KURL&, const WTF::Vector<WebCore::String>&, const WTF::Vector<WebCore::String>&, const WebCore::String&, bool);
         virtual void redirectDataToPlugin(WebCore::Widget* pluginWidget);
         virtual WebCore::Widget* createJavaAppletWidget(const WebCore::IntSize&, WebCore::HTMLAppletElement*, const WebCore::KURL& baseURL, const WTF::Vector<WebCore::String>& paramNames, const WTF::Vector<WebCore::String>& paramValues);
         virtual WebCore::String overrideMediaType() const;
