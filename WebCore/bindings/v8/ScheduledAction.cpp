6e21924cee83dd61a97019c6114ec230c1038178
Dimitri Glazkov <dglazkov@chromium.org>
2009-02-27 16:23:38 +0000

    2009-02-27  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=24211
            Add ScheduledAction for V8.
    
            * bindings/v8/ScheduledAction.cpp: Added.
            (WebCore::ScheduledAction::ScheduledAction):
            (WebCore::ScheduledAction::~ScheduledAction):
            (WebCore::ScheduledAction::execute):
            * bindings/v8/ScheduledAction.h: Added.
            (WebCore::ScheduledAction::ScheduledAction):
    
    Canonical link: https://commits.webkit.org/33358@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@41286 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/bindings/v8/ScheduledAction.cpp
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2007-2009 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "ScheduledAction.h"
+
+#include "Document.h"
+#include "ScriptExecutionContext.h"
+#include "ScriptSourceCode.h"
+
+#include "V8Binding.h"
+#include "V8Proxy.h"
+
+namespace WebCore {
+
+ScheduledAction::ScheduledAction(v8::Handle<v8::Function> func, int argc, v8::Handle<v8::Value> argv[])
+    : m_code(String(), KURL(), 0)
+{
+    m_function = v8::Persistent<v8::Function>::New(func);
+
+#ifndef NDEBUG
+    V8Proxy::RegisterGlobalHandle(SCHEDULED_ACTION, this, m_function);
+#endif
+
+    m_argc = argc;
+    if (argc > 0) {
+        m_argv = new v8::Persistent<v8::Value>[argc];
+        for (int i = 0; i < argc; i++) {
+            m_argv[i] = v8::Persistent<v8::Value>::New(argv[i]);
+
+#ifndef NDEBUG
+    V8Proxy::RegisterGlobalHandle(SCHEDULED_ACTION, this, m_argv[i]);
+#endif
+        }
+    } else
+        m_argv = 0;
+}
+
+ScheduledAction::~ScheduledAction()
+{
+    if (m_function.IsEmpty())
+        return;
+
+#ifndef NDEBUG
+    V8Proxy::UnregisterGlobalHandle(this, m_function);
+#endif
+    m_function.Dispose();
+
+    for (int i = 0; i < m_argc; i++) {
+#ifndef NDEBUG
+        V8Proxy::UnregisterGlobalHandle(this, m_argv[i]);
+#endif
+        m_argv[i].Dispose();
+    }
+
+    if (m_argc > 0)
+        delete[] m_argv;
+}
+
+void ScheduledAction::execute(ScriptExecutionContext* context)
+{
+    // FIXME: Timeouts for running the javascript code are not set.
+    V8Proxy* proxy = V8Proxy::retrieve(context);
+    if (!proxy)
+        return;
+
+    v8::HandleScope handleScope;
+    v8::Local<v8::Context> v8Context = proxy->GetContext();
+    if (v8Context.IsEmpty())
+        return; // JS may not be enabled.
+
+    v8::Context::Scope scope(v8Context);
+
+    proxy->setTimerCallback(true);
+
+    if (!m_function.IsEmpty() && m_function->IsFunction())
+        proxy->CallFunction(v8::Persistent<v8::Function>::Cast(m_function), v8Context->Global(), m_argc, m_argv);
+    else
+        proxy->Evaluate(m_code.url(), m_code.startLine() - 1, m_code.source(), 0);
+
+    if (context->isDocument())
+        static_cast<Document*>(context)->updateRendering();
+
+    proxy->setTimerCallback(false);
+}
+
+} // namespace WebCore
