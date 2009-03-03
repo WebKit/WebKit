1b9d7de6de562b51218d6cd755fe0ccc0d5c18bb
Simon Fraser <simon.fraser@apple.com>
2009-03-03 17:43:34 +0000

    2009-03-02  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dave Hyatt
    
            https://bugs.webkit.org/show_bug.cgi?id=24317
    
            Always return |true| from RenderTextControlMultiLine::nodeAtPoint() if
            the superclass found a node, otherwise we set the innerNode of the
            HitTestResult, but don't actually report that we found a hit.
    
            * rendering/RenderTextControlMultiLine.cpp:
            (WebCore::RenderTextControlMultiLine::nodeAtPoint):
    
    Canonical link: https://commits.webkit.org/33455@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@41385 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderTextControlMultiLine.cpp
+++ WebCore/rendering/RenderTextControlMultiLine.cpp
@@ -83,12 +83,10 @@ bool RenderTextControlMultiLine::nodeAtPoint(const HitTestRequest& request, HitT
     if (!RenderTextControl::nodeAtPoint(request, result, x, y, tx, ty, hitTestAction))
         return false;
 
-    if (result.innerNode() == node()) {
+    if (result.innerNode() == node())
         hitInnerTextBlock(result, x, y, tx, ty);
-        return true;
-    }
 
-    return false;
+    return true;
 }
 
 void RenderTextControlMultiLine::forwardEvent(Event* event)
