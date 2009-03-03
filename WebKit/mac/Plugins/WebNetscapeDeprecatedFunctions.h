c9b82498e154fa5a5de41d73e593dcb076f3d806
Anders Carlsson <andersca@apple.com>
2009-03-03 00:00:46 +0000

    2009-03-02  Anders Carlsson  <andersca@apple.com>
            Build fix.
    
            * Plugins/WebNetscapeDeprecatedFunctions.h:
    
    
    
    Canonical link: https://commits.webkit.org/33441@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@41371 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/mac/Plugins/WebNetscapeDeprecatedFunctions.h
+++ WebKit/mac/Plugins/WebNetscapeDeprecatedFunctions.h
@@ -27,9 +27,17 @@
 
 #import <CoreServices/CoreServices.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern OSErr WebGetDiskFragment(const FSSpec *fileSpec, UInt32 offset, UInt32 length, ConstStr63Param fragName, CFragLoadOptions options, CFragConnectionID *connID, Ptr *mainAddr, Str255 errMessage);
 extern OSErr WebCloseConnection(CFragConnectionID *connID);
 extern SInt16 WebLMGetCurApRefNum(void);
 extern void WebLMSetCurApRefNum(SInt16 value);
 
+#ifdef __cplusplus
+}
+#endif
+        
 #endif /* ENABLE(NETSCAPE_PLUGIN_API) && !defined(__LP64__) */
