8316e26bb769977263bebb7834c6352de3d54c96
Alexey Proskuryakov <ap@webkit.org>
2009-03-10 07:46:09 +0000

    Reviewed by Darin Adler.
            https://bugs.webkit.org/show_bug.cgi?id=19797
            Bring the list of forbidden headers in sync with XMLHttpRequest spec draft
    
            Added new checks to http/tests/xmlhttprequest/set-dangerous-headers.html.
    
            * xml/XMLHttpRequest.cpp:
            (WebCore::XMLHttpRequestStaticData::XMLHttpRequestStaticData): Added all headers from
            XMLHttpRequest 2 draft.
    
    
    
    Canonical link: https://commits.webkit.org/33613@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@41547 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/xml/XMLHttpRequest.cpp
+++ WebCore/xml/XMLHttpRequest.cpp
@@ -72,18 +72,25 @@ XMLHttpRequestStaticData::XMLHttpRequestStaticData()
 {
     m_forbiddenRequestHeaders.add("accept-charset");
     m_forbiddenRequestHeaders.add("accept-encoding");
+    m_forbiddenRequestHeaders.add("access-control-request-headers");
+    m_forbiddenRequestHeaders.add("access-control-request-method");
+    m_forbiddenRequestHeaders.add("authorization");
     m_forbiddenRequestHeaders.add("connection");
     m_forbiddenRequestHeaders.add("content-length");
     m_forbiddenRequestHeaders.add("content-transfer-encoding");
+    m_forbiddenRequestHeaders.add("cookie");
+    m_forbiddenRequestHeaders.add("cookie2");
     m_forbiddenRequestHeaders.add("date");
     m_forbiddenRequestHeaders.add("expect");
     m_forbiddenRequestHeaders.add("host");
     m_forbiddenRequestHeaders.add("keep-alive");
+    m_forbiddenRequestHeaders.add("origin");
     m_forbiddenRequestHeaders.add("referer");
     m_forbiddenRequestHeaders.add("te");
     m_forbiddenRequestHeaders.add("trailer");
     m_forbiddenRequestHeaders.add("transfer-encoding");
     m_forbiddenRequestHeaders.add("upgrade");
+    m_forbiddenRequestHeaders.add("user-agent");
     m_forbiddenRequestHeaders.add("via");
 
     m_allowedCrossSiteResponseHeaders.add("cache-control");
