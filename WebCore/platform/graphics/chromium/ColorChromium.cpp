2b14d571b4dd6eccf3f8629a0946c9f214a1e330
Dimitri Glazkov <dglazkov@chromium.org>
2009-03-19 16:43:30 +0000

    2009-03-19  Jeremy Moskovich  <jeremy@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            https://bugs.webkit.org/show_bug.cgi?id=24456
            Split ColorChromium.cpp into Mac & Windows variants.
            Remove Chromium Dependency on platform/graphics/mac/ColorMac.mm since we
            ultimately need to take a different approach.  For now, createCGColor()
            is copied from ColorMac.mm.
    
            No observable change in behavior, so no test.
    
            * platform/graphics/chromium/ColorChromium.cpp:
            (WebCore::focusRingColor):
            * platform/graphics/chromium/ColorChromiumMac.mm: Added.
    
    Canonical link: https://commits.webkit.org/33896@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@41839 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/chromium/ColorChromium.cpp
+++ WebCore/platform/graphics/chromium/ColorChromium.cpp
@@ -28,35 +28,15 @@
 
 namespace WebCore {
 
+#if !PLATFORM(DARWIN)
+// On OS X, there's code to monitor changes in the focus color system setting.
+// On Windows/Linux there is no equivalent system setting and therefore a static
+// color is all we need.
 Color focusRingColor()
 {
-// FIXME: This should be split up to ColorChromiumWin and ColorChromiumMac.
-#if PLATFORM(DARWIN)
-    // To avoid the Mac Chromium build having to rebasline 500+ layout tests and
-    // continue to do this w/ new tests that get landed in WebKit, we want to
-    // run the layout tests w/ the same color that stock WebKit uses.
-    // 
-    // TODO: For now we've hard coded the color that WebKit uses for layout
-    // tests.  We need to revisit this and do either of the following:
-    //  A. Fully honor the color from the UI, which means collecting the color
-    //     (and change notifications) in the browser process, and messaging the
-    //     color to the render process.
-    //  B. Adding a "layout tests" flag, to control the orage vs. blue colors
-    //     depending if we're running layout tests.
-    // To see the WebKit implementation of using the UI color and/or a flag for
-    // layout tests see WebKit/WebCore/platform/graphics/mac/ColorMac.mm.
-    // (Reality is we just need an api to override the focus color and both
-    // of the above are covered for what this file needs to provide, the
-    // two options would be details that happen in other places.)
-
-    // From WebKit:
-    // static RGBA32 oldAquaFocusRingColorRGBA = 0xFF7DADD9;
-    static Color oldAquaFocusRingColor(0x7D, 0xAD, 0xD9, 0xFF);
-    return oldAquaFocusRingColor;
-#else
     static Color focusRingColor(229, 151, 0, 255);
     return focusRingColor;
-#endif
 }
+#endif
 
 } // namespace WebCore
