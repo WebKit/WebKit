b21b98143e3f53b1263972a2fa7e9d71399e290c
Steve Falkenburg <sfalken@apple.com>
2009-04-02 04:51:53 +0000

    https://bugs.webkit.org/show_bug.cgi?id=24997         Race conditions in icon database threading code
            m_syncThreadRunning set was mistakenly eliminated in r27717.
            This reintroduces it.
    
            Reviewed by Oliver Hunt.
    
            * loader/icon/IconDatabase.cpp:
            (WebCore::IconDatabase::open): Set m_syncThreadRunning.
    
    
    
    Canonical link: https://commits.webkit.org/34218@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@42163 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/icon/IconDatabase.cpp
+++ WebCore/loader/icon/IconDatabase.cpp
@@ -150,6 +150,7 @@ bool IconDatabase::open(const String& databasePath)
     // completes and m_syncThreadRunning is properly set
     m_syncLock.lock();
     m_syncThread = createThread(IconDatabase::iconDatabaseSyncThreadStart, this, "WebCore: IconDatabase");
+    m_syncThreadRunning = m_syncThread;
     m_syncLock.unlock();
     if (!m_syncThread)
         return false;
