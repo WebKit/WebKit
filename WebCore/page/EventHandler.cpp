484a5e349e07f51106dcb75faf8d5252454a6640
Justin Garcia <justin.garcia@apple.com>
2009-04-13 20:57:04 +0000

    Adding a comment to my previous code change.
    Canonical link: https://commits.webkit.org/34514@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@42460 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/page/EventHandler.cpp
+++ WebCore/page/EventHandler.cpp
@@ -1068,6 +1068,8 @@ bool EventHandler::handleMousePressEvent(const PlatformMouseEvent& mouseEvent)
     m_mouseDownWasInSubframe = false;
 
     HitTestRequest request(HitTestRequest::Active);
+    // Save the document point we generate in case the window coordinate is invalidated by what happens 
+    // when we dispatch the event.
     IntPoint documentPoint = documentPointForWindowPoint(m_frame, mouseEvent.pos());
     MouseEventWithHitTestResults mev = m_frame->document()->prepareMouseEvent(request, documentPoint, mouseEvent);
 
