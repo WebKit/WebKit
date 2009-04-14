8c7c5c7077c19c3288019b35c0bfac564ef4b77b
Ada Chan <adachan@apple.com>
2009-04-14 00:19:46 +0000

    Forgot to capitalize the "US".
            * platform/text/win/TextBreakIteratorInternalICUWin.cpp:
            (WebCore::currentTextBreakLocaleID):
    
    
    
    Canonical link: https://commits.webkit.org/34526@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@42472 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/text/win/TextBreakIteratorInternalICUWin.cpp
+++ WebCore/platform/text/win/TextBreakIteratorInternalICUWin.cpp
@@ -25,11 +25,11 @@ namespace WebCore {
 
 const char* currentTextBreakLocaleID()
 {
-    // Using en_us_POSIX now so word selection in address field works as expected as before (double-clicking
+    // Using en_US_POSIX now so word selection in address field works as expected as before (double-clicking
     // in a URL selects a word delimited by periods rather than selecting the entire URL).
     // However, this is not entirely correct - we should honor the system locale in the normal case.
     // FIXME: <rdar://problem/6786703> Should use system locale for text breaking
-    return "en_us_POSIX";
+    return "en_US_POSIX";
 }
 
 }
