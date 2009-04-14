9ad53215f4c69a680c3220f0d4109dcbb7711e01
Ada Chan <adachan@apple.com>
2009-04-14 00:10:14 +0000

    <rdar://problem/6564956> URL field double-click selects entire domain, rather than single word or entire URL         Revert back to the old word breaking behavior by using en_US_POSIX locale.
            Reviewed by Steve Falkenburg.
    
            * platform/text/win/TextBreakIteratorInternalICUWin.cpp:
            (WebCore::currentTextBreakLocaleID):
    
    
    
    Canonical link: https://commits.webkit.org/34525@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@42471 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/text/win/TextBreakIteratorInternalICUWin.cpp
+++ WebCore/platform/text/win/TextBreakIteratorInternalICUWin.cpp
@@ -25,7 +25,11 @@ namespace WebCore {
 
 const char* currentTextBreakLocaleID()
 {
-    return "en_us";
+    // Using en_us_POSIX now so word selection in address field works as expected as before (double-clicking
+    // in a URL selects a word delimited by periods rather than selecting the entire URL).
+    // However, this is not entirely correct - we should honor the system locale in the normal case.
+    // FIXME: <rdar://problem/6786703> Should use system locale for text breaking
+    return "en_us_POSIX";
 }
 
 }
