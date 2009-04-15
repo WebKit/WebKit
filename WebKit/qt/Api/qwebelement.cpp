c16440eb459f4bb86fd23c3a196b0d0078935699
Simon Hausmann <hausmann@webkit.org>
2009-04-15 13:42:34 +0000

    2009-04-15  Antonio Gomes   <antonio.gomes@openbossa.org>
            Reviewed by Simon Hausmann.
    
            Fixed nit/typo in QWebElement documentation.
    
    Canonical link: https://commits.webkit.org/34591@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@42539 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/qt/Api/qwebelement.cpp
+++ WebKit/qt/Api/qwebelement.cpp
@@ -58,8 +58,8 @@ public:
 
     The element's attributes can be read using attribute() and changed using setAttribute().
 
-    The content of the child elements can be converted to toPlainText() and html(), and it is possible to
-    replace the content using setPlainText() and setHtml().
+    The content of the child elements can be converted to plain text using toPlainText() and to 
+    x(html) using toXml(), and it is possible to replace the content using setPlainText() and setXml().
 
     Depending on the type of the underlying element there may be extra functionality available, not
     covered through QWebElement's API. For example a HTML form element can be triggered to submit the
