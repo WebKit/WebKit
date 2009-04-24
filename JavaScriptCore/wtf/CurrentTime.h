4d5a57459d1c2e8e23f7d4da2ac9975f63276f4e
Mark Rowe <mrowe@apple.com>
2009-04-24 23:28:45 +0000

    2009-04-24  Rob Raguet-Schofield  <ragfield@gmail.com>
            Rubber-stamped by Mark Rowe.
    
            * wtf/CurrentTime.h: Fix a typo in a comment.
    
    Canonical link: https://commits.webkit.org/34901@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@42851 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/CurrentTime.h
+++ JavaScriptCore/wtf/CurrentTime.h
@@ -36,7 +36,7 @@ namespace WTF {
 
     // Returns the current system (UTC) time in seconds, starting January 1, 1970.
     // Precision varies depending on a platform but usually is as good or better 
-    // then a millisecond.
+    // than a millisecond.
     double currentTime();
 
 } // namespace WTF
