6a1098b821c47b4b0716382d4f1e20e5cb3bfeaf
Gustavo Noronha Silva <kov@webkit.org>
2009-05-02 19:39:17 +0000

    2009-05-02  Gustavo Noronha Silva  <gns@gnome.org>
            Reviewed by Holger Freyther.
    
            https://bugs.webkit.org/show_bug.cgi?id=25466
            WebKitGtk+ 1.1.6 prints weird error messages in Liferea
    
            Use strlen instead of g_utf8_strlen when calculating the size of
            the data that is being given to webkit_web_frame_load_string to be
            loaded, so that we don't miss any of the content.
    
            * webkit/webkitwebframe.cpp:
    
    Canonical link: https://commits.webkit.org/35181@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@43146 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/gtk/webkit/webkitwebframe.cpp
+++ WebKit/gtk/webkit/webkitwebframe.cpp
@@ -416,7 +416,7 @@ static void webkit_web_frame_load_data(WebKitWebFrame* frame, const gchar* conte
 
     ResourceRequest request(baseKURL);
 
-    RefPtr<SharedBuffer> sharedBuffer = SharedBuffer::create(content, g_utf8_strlen(content, -1));
+    RefPtr<SharedBuffer> sharedBuffer = SharedBuffer::create(content, strlen(content));
     SubstituteData substituteData(sharedBuffer.release(),
                                   mimeType ? String::fromUTF8(mimeType) : String::fromUTF8("text/html"),
                                   encoding ? String::fromUTF8(encoding) : String::fromUTF8("UTF-8"),
