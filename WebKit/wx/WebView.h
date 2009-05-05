3699062cda95da8e4182eb18728d189509d7528e
Kevin Ollivier <kevino@webkit.org>
2009-05-05 23:11:34 +0000

    Reviewed by Kevin Ollivier.
    Have wxWebkit make use of gcc(4 and greater) hidden visibility feature
    
    https://bugs.webkit.org/show_bug.cgi?id=24345
    
    
    Canonical link: https://commits.webkit.org/35283@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@43260 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/wx/WebView.h
+++ WebKit/wx/WebView.h
@@ -49,15 +49,15 @@ namespace WebCore {
 
 #ifndef SWIG
 
-#if WXMAKINGDLL_WEBKIT
+#if !wxCHECK_VERSION(2,9,0) && wxCHECK_GCC_VERSION(4,0)
+#define WXDLLIMPEXP_WEBKIT __attribute__ ((visibility("default")))
+#elif WXMAKINGDLL_WEBKIT
 #define WXDLLIMPEXP_WEBKIT WXEXPORT
 #elif defined(WXUSINGDLL_WEBKIT)
 #define WXDLLIMPEXP_WEBKIT WXIMPORT
-#else
-#define WXDLLIMPEXP_WEBKIT
 #endif
 
-#else 
+#else
 #define WXDLLIMPEXP_WEBKIT
 #endif // SWIG
 
