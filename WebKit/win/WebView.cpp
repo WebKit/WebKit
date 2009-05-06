e7741511d4574521b9cdb2a9b190a9a44549e289
Adam Roben <aroben@apple.com>
2009-05-06 02:20:22 +0000

    Add a call to notify the UI delegate when the WebView gets invalidated
            Reviewed by Steve Falkenburg.
    
            * Interfaces/IWebUIDelegatePrivate.idl: Added webViewDidInvalidate.
            * WebView.cpp:
            (WebView::addToDirtyRegion): Call webViewDidInvalidate.
    
    Canonical link: https://commits.webkit.org/35294@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@43271 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/win/WebView.cpp
+++ WebKit/win/WebView.cpp
@@ -727,6 +727,10 @@ void WebView::addToDirtyRegion(HRGN newRegion)
         m_backingStoreDirtyRegion.set(combinedRegion);
     } else
         m_backingStoreDirtyRegion.set(newRegion);
+
+    COMPtr<IWebUIDelegatePrivate5> delegate(Query, m_uiDelegatePrivate);
+    if (delegate)
+        delegate->webViewDidInvalidate(this);
 }
 
 void WebView::scrollBackingStore(FrameView* frameView, int dx, int dy, const IntRect& scrollViewRect, const IntRect& clipRect)
