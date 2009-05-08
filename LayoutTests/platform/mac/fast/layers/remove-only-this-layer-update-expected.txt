WebKit/WebKit

commit 17f1025947e4055c3edd53e9127451c817e4990a

Reviewed by David Hyatt.
        Changes to RenderLayer destruction to hopefully help catch an elusive crasher
        https://bugs.webkit.org/show_bug.cgi?id=24409

        Added a new RenderBoxModelObject::destroyLayer() call which is
        now the only way which RenderLayers should ever be destroyed.
        This ensures that the pointer to the layer is cleared in the
        RenderObject after destruction, allowing us to ASSERT in the
        RenderBoxModelObject destructor.

        * rendering/RenderBox.cpp:
        (WebCore::RenderBox::calcAbsoluteHorizontalReplaced):
        * rendering/RenderBoxModelObject.cpp:
        (WebCore::RenderBoxModelObject::~RenderBoxModelObject):
        (WebCore::RenderBoxModelObject::destroyLayer):
        (WebCore::RenderBoxModelObject::destroy):
        (WebCore::RenderBoxModelObject::styleDidChange):
        * rendering/RenderBoxModelObject.h:
        * rendering/RenderLayer.cpp:
        (WebCore::RenderLayer::stackingContext):
        (WebCore::RenderLayer::destroy):
        (WebCore::RenderLayer::removeOnlyThisLayer):
        * rendering/RenderLayer.h:
        * rendering/RenderObject.cpp:
        (WebCore::RenderObject::destroy):
        * rendering/RenderWidget.cpp:
        (WebCore::RenderWidget::destroy):

Canonical link: https://commits.webkit.org/33539@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@41469 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/rendering/RenderBoxModelObject.cpp
@@ -50,6 +50,18 @@ RenderBoxModelObject::RenderBoxModelObject(Node* node)
 
 RenderBoxModelObject::~RenderBoxModelObject()
 {
+    // Our layer should have been destroyed and cleared by now
+    ASSERT(!hasLayer());
+    ASSERT(!m_layer);
+}
+
+void RenderBoxModelObject::destroyLayer()
+{
+    ASSERT(hasLayer());
+    ASSERT(m_layer);
+    m_layer->destroy(renderArena());
+    m_layer = 0;
+    setHasLayer(false);
 }
 
 void RenderBoxModelObject::destroy()
@@ -57,6 +69,8 @@ void RenderBoxModelObject::destroy()
     // This must be done before we destroy the RenderObject.
     if (m_layer)
         m_layer->clearClipRects();
+
+    // RenderObject::destroy calls back to destroyLayer() for layer destruction
     RenderObject::destroy();
 }
 
@@ -100,13 +114,9 @@ void RenderBoxModelObject::styleDidChange(StyleDifference diff, const RenderStyl
                 m_layer->updateLayerPositions();
         }
     } else if (layer() && layer()->parent()) {
-        
-        RenderLayer* layer = m_layer;
-        m_layer = 0;
-        setHasLayer(false);
         setHasTransform(false); // Either a transform wasn't specified or the object doesn't support transforms, so just null out the bit.
         setHasReflection(false);
-        layer->removeOnlyThisLayer();
+        m_layer->removeOnlyThisLayer(); // calls destroyLayer() which clears m_layer
         if (s_wasFloating && isFloating())
             setChildNeedsLayout(true);
     }

WebCore/rendering/RenderLayer.cpp
@@ -610,13 +610,12 @@ FloatPoint RenderLayer::perspectiveOrigin() const
                       style->perspectiveOriginY().calcFloatValue(borderBox.height()));
 }
 
-RenderLayer *RenderLayer::stackingContext() const
+RenderLayer* RenderLayer::stackingContext() const
 {
-    RenderLayer* curr = parent();
-    for ( ; curr && !curr->renderer()->isRenderView() && !curr->renderer()->isRoot() &&
-          curr->renderer()->style()->hasAutoZIndex();
-          curr = curr->parent()) { }
-    return curr;
+    RenderLayer* layer = parent();
+    while (layer && !layer->renderer()->isRenderView() && !layer->renderer()->isRoot() && layer->renderer()->style()->hasAutoZIndex())
+        layer = layer->parent();
+    return layer;
 }
 
 RenderLayer* RenderLayer::enclosingPositionedAncestor() const
@@ -770,7 +769,7 @@ void RenderLayer::operator delete(void* ptr, size_t sz)
 }
 
 void RenderLayer::destroy(RenderArena* renderArena)
-{    
+{
     delete this;
 
     // Recover the size left there for us by operator delete and free the memory.
@@ -880,8 +879,8 @@ void RenderLayer::removeOnlyThisLayer()
         current->updateLayerPositions();
         current = next;
     }
-    
-    destroy(renderer()->renderArena());
+
+    m_renderer->destroyLayer();
 }
 
 void RenderLayer::insertOnlyThisLayer()

WebCore/rendering/RenderObject.cpp
@@ -1839,10 +1839,9 @@ void RenderObject::destroy()
 
     // FIXME: Would like to do this in RenderBoxModelObject, but the timing is so complicated that this can't easily
     // be moved into RenderBoxModelObject::destroy.
-    RenderArena* arena = renderArena();
     if (hasLayer())
-        toRenderBoxModelObject(this)->layer()->destroy(arena);
-    arenaDelete(arena, this);
+        toRenderBoxModelObject(this)->destroyLayer();
+    arenaDelete(renderArena(), this);
 }
 
 void RenderObject::arenaDelete(RenderArena* arena, void* base)

WebCore/rendering/RenderWidget.cpp
@@ -91,19 +91,18 @@ void RenderWidget::destroy()
     if (hasOverrideSize())
         setOverrideSize(-1);
 
-    RenderArena* arena = renderArena();
-
-    if (hasLayer())
-        layer()->clearClipRects();
-
     if (style() && (style()->height().isPercent() || style()->minHeight().isPercent() || style()->maxHeight().isPercent()))
         RenderBlock::removePercentHeightDescendant(this);
 
+    if (hasLayer()) {
+        layer()->clearClipRects();
+        destroyLayer();
+    }
+
+    // Grab the arena from node()->document()->renderArena() before clearing the node pointer.
+    // Clear the node before deref-ing, as this may be deleted when deref is called.
+    RenderArena* arena = renderArena();
     setNode(0);
-
-    if (hasLayer())
-        layer()->destroy(arena);
-
     deref(arena);
 }
 

WebCore/rendering/RenderBox.cpp
@@ -2354,7 +2354,7 @@ void RenderBox::calcAbsoluteHorizontalReplaced()
         } else {
             RenderObject* po = parent();
             // 'staticX' should already have been set through layout of the parent.
-            int staticPosition = layer()->staticX() + containerWidth + toRenderBoxModelObject(containerBlock)->borderRight();
+            int staticPosition = layer()->staticX() + containerWidth + containerBlock->borderRight();
             for ( ; po && po != containerBlock; po = po->parent()) {
                 if (po->isBox())
                     staticPosition += toRenderBox(po)->x();

WebCore/rendering/RenderBoxModelObject.h
@@ -96,6 +96,9 @@ public:
     // The difference between this inline's baseline position and the line's baseline position.
     int verticalPosition(bool firstLine) const;
 
+    // Called by RenderObject::destroy() (and RenderWidget::destroy()) and is the only way layers should ever be destroyed
+    void destroyLayer();
+
 protected:
     void calculateBackgroundImageGeometry(const FillLayer*, int tx, int ty, int w, int h, IntRect& destRect, IntPoint& phase, IntSize& tileSize);
     IntSize calculateBackgroundSize(const FillLayer*, int scaledWidth, int scaledHeight) const;
@@ -123,7 +126,7 @@ inline const RenderBoxModelObject* toRenderBoxModelObject(const RenderObject* o)
 }
 
 // This will catch anyone doing an unnecessary cast.
-void toRenderBoxModelObject(const RenderBox*);
+void toRenderBoxModelObject(const RenderBoxModelObject*);
 
 } // namespace WebCore
 

WebCore/rendering/RenderLayer.h
@@ -414,8 +414,6 @@ public:
     bool preserves3D() const { return renderer()->style()->transformStyle3D() == TransformStyle3DPreserve3D; }
     bool has3DTransform() const { return m_transform && !m_transform->isAffine(); }
 
-    void destroy(RenderArena*);
-
      // Overloaded new operator.  Derived classes must override operator new
     // in order to allocate out of the RenderArena.
     void* operator new(size_t, RenderArena*) throw();
@@ -519,6 +517,10 @@ private:
 private:
     friend class RenderLayerBacking;
     friend class RenderLayerCompositor;
+    friend class RenderBoxModelObject;
+
+    // Only safe to call from RenderBoxModelObject::destroyLayer(RenderArena*)
+    void destroy(RenderArena*);
 
 protected:
     RenderBoxModelObject* m_renderer;

commit 4592251a9538ee322696fba4f24b7936c7ee0929

WebCore:
2009-05-07  Eric Seidel  <eric@webkit.org>

        Reviewed by Beth Dakin.

        Fix regression caused by r41469, add test case to prevent it from
        happening again.
        https://bugs.webkit.org/show_bug.cgi?id=25252

        hasLayer() was true during removeOnlyThisLayer()/
        updateLayerPositions()
        which caused updateLayerPosition()'s walk up the render tree to
        include offsets from the layer we were about to remove.

        I'm not 100% convinced that this wasn't a bug in
        updateLayerPosition() or in RenderBoxModelObject::styleDidChange,
        because the layer in question is not the containing block for the
        block which gets laid out wrong. But this restores the previous
        behavior and adds a test.  So the next time someone is in here re-
        factoring, they will at least know if they break something.

        Test: fast/layers/remove-only-this-layer-update.html

        * rendering/RenderBoxModelObject.cpp:
        (WebCore::RenderBoxModelObject::destroyLayer):
        * rendering/RenderLayer.cpp:
        (WebCore::RenderLayer::removeOnlyThisLayer):
        * rendering/RenderObject.cpp:
        (WebCore::RenderObject::destroy):
        * rendering/RenderWidget.cpp:
        (WebCore::RenderWidget::destroy):

LayoutTests:

2009-05-07  Eric Seidel  <eric@webkit.org>

        Reviewed by Beth Dakin.

        Fix regression caused by r41469, add test case to prevent it from
        happening again.
        https://bugs.webkit.org/show_bug.cgi?id=25252

        * fast/layers/remove-only-this-layer-update.html: Added.
        * platform/mac/fast/layers/remove-only-this-layer-update-expected.checksum: Added.
        * platform/mac/fast/layers/remove-only-this-layer-update-expected.png: Added.
        * platform/mac/fast/layers/remove-only-this-layer-update-expected.txt: Added.



Canonical link: https://commits.webkit.org/35401@main

WebCore/rendering/RenderBoxModelObject.cpp
@@ -57,11 +57,10 @@ RenderBoxModelObject::~RenderBoxModelObject()
 
 void RenderBoxModelObject::destroyLayer()
 {
-    ASSERT(hasLayer());
+    ASSERT(!hasLayer()); // Callers should have already called setHasLayer(false)
     ASSERT(m_layer);
     m_layer->destroy(renderArena());
     m_layer = 0;
-    setHasLayer(false);
 }
 
 void RenderBoxModelObject::destroy()

WebCore/rendering/RenderLayer.cpp
@@ -861,7 +861,11 @@ void RenderLayer::removeOnlyThisLayer()
 {
     if (!m_parent)
         return;
-    
+
+    // Mark that we are about to lose our layer. This makes render tree
+    // walks ignore this layer while we're removing it.
+    m_renderer->setHasLayer(false);
+
 #if USE(ACCELERATED_COMPOSITING)
     compositor()->layerWillBeRemoved(m_parent, this);
 #endif
@@ -883,7 +887,7 @@ void RenderLayer::removeOnlyThisLayer()
         RenderLayer* next = current->nextSibling();
         removeChild(current);
         parent->addChild(current, nextSib);
-        current->updateLayerPositions();
+        current->updateLayerPositions(); // Depends on hasLayer() already being false for proper layout.
         current = next;
     }
 

WebCore/rendering/RenderObject.cpp
@@ -1860,8 +1860,10 @@ void RenderObject::destroy()
 
     // FIXME: Would like to do this in RenderBoxModelObject, but the timing is so complicated that this can't easily
     // be moved into RenderBoxModelObject::destroy.
-    if (hasLayer())
+    if (hasLayer()) {
+        setHasLayer(false);
         toRenderBoxModelObject(this)->destroyLayer();
+    }
     arenaDelete(renderArena(), this);
 }
 

WebCore/rendering/RenderWidget.cpp
@@ -96,6 +96,7 @@ void RenderWidget::destroy()
 
     if (hasLayer()) {
         layer()->clearClipRects();
+        setHasLayer(false);
         destroyLayer();
     }
 

LayoutTests/fast/layers/remove-only-this-layer-update.html
@@ -1,0 +1,19 @@
+<body style="margin:0px">
+    <div style="width: 100px; height: 100px; position: absolute; left: 100px; top: 100px; background-color: red">FAIL</div>
+    <div id="outer" style="opacity: 0.5; margin: 100px">
+        <div style="position: relative; background-color: green; width: 100px; height: 100px">PASS</div>
+    </div>
+You should see a 100x100 green rect at 100x100 above with the word PASS.  There should be no red on this page.  This is a test case for https://bugs.webkit.org/show_bug.cgi?id=25252
+
+    <script>
+        function doTest() {
+            document.getElementById("outer").style.opacity = 1.0;
+            if (window.layoutTestController)
+                layoutTestController.notifyDone();
+        }
+        if (window.layoutTestController)
+            layoutTestController.waitUntilDone();
+        // Delay until after the first paint
+        setTimeout(doTest, 0);
+    </script>
+</body>

LayoutTests/platform/mac/fast/layers/remove-only-this-layer-update-expected.checksum
@@ -1,0 +1,1 @@
+472ca17a009ad9a51e760c9f8a5b542d

LayoutTests/platform/mac/fast/layers/remove-only-this-layer-update-expected.txt
@@ -1,0 +1,22 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (0,100) size 800x500
+      RenderBlock {DIV} at (100,0) size 600x100
+      RenderBlock (anonymous) at (0,200) size 800x36
+        RenderText {#text} at (0,0) size 782x36
+          text run at (0,0) width 495: "You should see a 100x100 green rect at 100x100 above with the word PASS. "
+          text run at (495,0) width 234: "There should be no red on this page. "
+          text run at (729,0) width 53: "This is a"
+          text run at (0,18) width 383: "test case for https://bugs.webkit.org/show_bug.cgi?id=25252"
+        RenderText {#text} at (0,0) size 0x0
+        RenderText {#text} at (0,0) size 0x0
+layer at (100,100) size 100x100
+  RenderBlock (positioned) {DIV} at (100,100) size 100x100 [bgcolor=#FF0000]
+    RenderText {#text} at (0,0) size 36x18
+      text run at (0,0) width 36: "FAIL"
+layer at (100,100) size 100x100
+  RenderBlock (relative positioned) {DIV} at (0,0) size 100x100 [bgcolor=#008000]
+    RenderText {#text} at (0,0) size 39x18
+      text run at (0,0) width 39: "PASS"