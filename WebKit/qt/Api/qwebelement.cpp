70dfb2586b6530ec8c14932f0ab7396ac61307cf
Ariya Hidayat <ariya@webkit.org>
2009-05-13 14:20:35 +0000

    2009-05-13  Kenneth Rohde Christiansen  <kenneth.christiansen@openbossa.org>
        Rubberstamped by Ariya Hidayat.
    
        Fix coding style: Add {} to contents of if, containing a for loop.
    
    Canonical link: https://commits.webkit.org/35579@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@43625 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/qt/Api/qwebelement.cpp
+++ WebKit/qt/Api/qwebelement.cpp
@@ -908,7 +908,7 @@ QString QWebElement::styleProperty(const QString &name, const ResolveRule rule) 
         // declarations, as well as embedded and inline style declarations.
 
         DOMWindow* domWindow = m_element->document()->frame()->domWindow();
-        if (RefPtr<CSSRuleList> rules = domWindow->getMatchedCSSRules(m_element, ""))
+        if (RefPtr<CSSRuleList> rules = domWindow->getMatchedCSSRules(m_element, "")) {
             for (int i = rules->length(); i > 0; --i) {
                 CSSStyleRule* rule = static_cast<CSSStyleRule*>(rules->item(i - 1));
 
@@ -918,6 +918,7 @@ QString QWebElement::styleProperty(const QString &name, const ResolveRule rule) 
                 if (style->getPropertyValue(propID).isEmpty())
                     style = rule->style();
             }
+        }
 
         return style->getPropertyValue(propID);
     }
