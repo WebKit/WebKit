cae97b2e787c7bf48590b18e76828d3decb95a08
Dimitri Glazkov <dglazkov@chromium.org>
2009-05-15 19:40:08 +0000

    2009-05-14  Dimitri Glazkov  <dglazkov@chromium.org>
            Reviewed by Eric Seidel.
    
            https://bugs.webkit.org/show_bug.cgi?id=25810
            Upstream V8 DOM Wrapper map declarations.
    
            No behavior change, so no tests.
    
            * bindings/v8/V8DOMMap.h:
            (WebCore::WeakReferenceMap::WeakReferenceMap): Added.
            (WebCore::WeakReferenceMap::~WeakReferenceMap): Added.
            (WebCore::WeakReferenceMap::get): Added.
            (WebCore::WeakReferenceMap::set): Added.
            (WebCore::WeakReferenceMap::forget): Added.
            (WebCore::WeakReferenceMap::contains): Added.
            (WebCore::WeakReferenceMap::impl): Added.
            (WebCore::DOMWrapperMap::DOMWrapperMap): Added.
    
    Canonical link: https://commits.webkit.org/35697@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@43778 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/bindings/v8/V8DOMMap.h
+++ WebCore/bindings/v8/V8DOMMap.h
@@ -31,8 +31,7 @@
 #ifndef V8DOMMap_h
 #define V8DOMMap_h
 
-#include "dom_wrapper_map.h"
-
+#include <wtf/HashMap.h>
 #include <v8.h>
 
 namespace WebCore {
@@ -40,6 +39,61 @@ namespace WebCore {
 #if ENABLE(SVG)
     class SVGElementInstance;
 #endif
+
+    // A table of wrappers with weak pointers.
+    // This table allows us to avoid track wrapped objects for debugging
+    // and for ensuring that we don't double wrap the same object.
+    template<class KeyType, class ValueType> class WeakReferenceMap {
+    public:
+        WeakReferenceMap(v8::WeakReferenceCallback callback) : m_weakReferenceCallback(callback) { }
+    #ifndef NDEBUG
+        virtual ~WeakReferenceMap()
+        {
+            if (m_map.size() > 0)
+                fprintf(stderr, "Leak %d JS wrappers.\n", m_map.size());
+        }
+    #endif
+
+        // Get the JS wrapper object of an object.
+        virtual v8::Persistent<ValueType> get(KeyType* obj)
+        {
+            ValueType* wrapper = m_map.get(obj);
+            return wrapper ? v8::Persistent<ValueType>(wrapper) : v8::Persistent<ValueType>();
+        }
+
+        virtual void set(KeyType* obj, v8::Persistent<ValueType> wrapper)
+        {
+            ASSERT(!m_map.contains(obj));
+            wrapper.MakeWeak(obj, m_weakReferenceCallback);
+            m_map.set(obj, *wrapper);
+        }
+
+        virtual void forget(KeyType* obj)
+        {
+            ASSERT(obj);
+            ValueType* wrapper = m_map.take(obj);
+            if (!wrapper)
+                return;
+
+            v8::Persistent<ValueType> handle(wrapper);
+            handle.Dispose();
+            handle.Clear();
+        }
+
+        bool contains(KeyType* obj) { return m_map.contains(obj); }
+
+        HashMap<KeyType*, ValueType*>& impl() { return m_map; }
+
+    protected:
+        HashMap<KeyType*, ValueType*> m_map;
+        v8::WeakReferenceCallback m_weakReferenceCallback;
+    };
+
+
+    template <class KeyType> class DOMWrapperMap : public WeakReferenceMap<KeyType, v8::Object> {
+    public:
+        DOMWrapperMap(v8::WeakReferenceCallback callback) : WeakReferenceMap<KeyType, v8::Object>(callback) { }
+    };
 
     // Callback when JS wrapper of active DOM object is dead.
     void weakActiveDOMObjectCallback(v8::Persistent<v8::Value> v8Object, void* domObject);
