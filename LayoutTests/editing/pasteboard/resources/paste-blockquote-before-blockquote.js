f01a43457d7490946baa19198cab3072ca501a84
Eric Seidel <eric@webkit.org>
2009-05-15 05:19:28 +0000

    No review, rollout only.
            Roll out r43753 as it caused a test to fail
            Turns out more than one test was using the modified JS file.
    
            * editing/pasteboard/paste-blockquote-before-blockquote-expected.txt:
            * editing/pasteboard/paste-blockquote-before-blockquote.html:
            * editing/pasteboard/resources/TEMPLATE.html: Removed.
            * editing/pasteboard/resources/paste-blockquote-before-blockquote.js:
            (copyAndPasteNode):
    
    Canonical link: https://commits.webkit.org/35674@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@43754 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/editing/pasteboard/resources/paste-blockquote-before-blockquote.js
+++ LayoutTests/editing/pasteboard/resources/paste-blockquote-before-blockquote.js
@@ -1,36 +1,3 @@
-description('This tests pasting a singly nested blockquote before, and outside of, another blockquote.  This test does not work outside of DRT.');
-
-var createLine = function(num, parent) {
-  var line = document.createElement('div');
-  line.innerHTML = 'line ' + num;
-  parent.appendChild(line);
-  return line;
-}
-
-var pasteDiv = document.createElement("div");
-pasteDiv.id = "pasteDiv";
-pasteDiv.contentEditable = true;
-document.body.appendChild(pasteDiv);
-
-var outerBlockquote = document.createElement('blockquote');
-outerBlockquote.type = 'cite';
-
-createLine(1, outerBlockquote);
-var line2 = createLine(2, outerBlockquote);
-line2.id = 'singleNestedNodeToCopy';
-createLine(3, outerBlockquote);
-
-var innerBlockquote = document.createElement('blockquote');
-innerBlockquote.type = 'cite';
-createLine(4, innerBlockquote);
-outerBlockquote.appendChild(innerBlockquote);
-
-createLine(5, outerBlockquote);
-
-var div = document.createElement("div");
-div.appendChild(outerBlockquote);
-div.appendChild(document.createElement('br'));
-document.body.appendChild(div);
 
 function copyAndPasteNode(nodeName) {
     var range = document.createRange();
@@ -48,12 +15,8 @@ function copyAndPasteNode(nodeName) {
     sel.setPosition(pasteDiv, 0);
     document.execCommand("Paste");
 
-    if (pasteDiv.children[1] == undefined)
-        testFailed("Pasting " + nodeName + " DID keep the newline within the blockquote");
-    else
-        testPassed("Pasting " + nodeName + " DID NOT keep the newline within the blockquote.");
+    console.log(pasteDiv.children[1] == undefined
+        ? "FAILED: pasting " + nodeName + " DID keep the newline within the blockquote"
+        : "SUCCESS: pasting " + nodeName + " DID NOT keep the newline within the blockquote.");
 }
 
-copyAndPasteNode('singleNestedNodeToCopy');
-
-var successfullyParsed = true;
