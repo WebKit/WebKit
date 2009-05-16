d1cf3de8c1b6eabb1178d3fc244610a5635ec487
Holger Freyther <zecke@webkit.org>
2009-05-16 09:11:17 +0000

    [GTK] Merge r41819 into Gtk to log through the JS console
    https://bugs.webkit.org/show_bug.cgi?id=25787
    
    Copy over the log routine from the OSX TestNetscapePlugin to
    the copy of Gtk.
    
    Canonical link: https://commits.webkit.org/35719@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@43801 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/DumpRenderTree/gtk/TestNetscapePlugin/TestNetscapePlugin.cpp
+++ WebKitTools/DumpRenderTree/gtk/TestNetscapePlugin/TestNetscapePlugin.cpp
@@ -32,10 +32,51 @@
 #include "npruntime.h"
 #include "npfunctions.h"
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <X11/Xlib.h>
+
+static void log(NPP instance, const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    char message[2048] = "PLUGIN: ";
+    vsprintf(message + strlen(message), format, args);
+    va_end(args);
+
+    NPObject* windowObject = 0;
+    NPError error = browser->getvalue(instance, NPNVWindowNPObject, &windowObject);
+    if (error != NPERR_NO_ERROR) {
+        fprintf(stderr, "Failed to retrieve window object while logging: %s\n", message);
+        return;
+    }
+
+    NPVariant consoleVariant;
+    if (!browser->getproperty(instance, windowObject, browser->getstringidentifier("console"), &consoleVariant)) {
+        fprintf(stderr, "Failed to retrieve console object while logging: %s\n", message);
+        browser->releaseobject(windowObject);
+        return;
+    }
+
+    NPObject* consoleObject = NPVARIANT_TO_OBJECT(consoleVariant);
+
+    NPVariant messageVariant;
+    STRINGZ_TO_NPVARIANT(message, messageVariant);
+
+    NPVariant result;
+    if (!browser->invoke(instance, consoleObject, browser->getstringidentifier("log"), &messageVariant, 1, &result)) {
+        fprintf(stderr, "Failed to invoke console.log while logging: %s\n", message);
+        browser->releaseobject(consoleObject);
+        browser->releaseobject(windowObject);
+        return;
+    }
+
+    browser->releasevariantvalue(&result);
+    browser->releaseobject(consoleObject);
+    browser->releaseobject(windowObject);
+}
 
 extern "C" {
     NPError NP_Initialize (NPNetscapeFuncs *aMozillaVTable, NPPluginFuncs *aPluginVTable);
@@ -93,7 +134,7 @@ webkit_test_plugin_destroy_instance(NPP instance, NPSavedData **save)
             free(obj->onURLNotify);
 
         if (obj->logDestroy)
-            printf("PLUGIN: NPP_Destroy\n");
+            log(instance, "NPP_Destroy");
 
         browser->releaseobject(&obj->header);
     }
@@ -108,7 +149,7 @@ webkit_test_plugin_set_window(NPP instance, NPWindow *window)
 
     if (obj) {
         if (obj->logSetWindow) {
-            printf("PLUGIN: NPP_SetWindow: %d %d\n", (int)window->width, (int)window->height);
+            log(instance, "NPP_SetWindow: %d %d", (int)window->width, (int)window->height);
             obj->logSetWindow = false;
         }
     }
@@ -198,7 +239,7 @@ webkit_test_plugin_handle_event(NPP instance, void* event)
         return 0;
 
     XEvent* evt = static_cast<XEvent*>(event);
-    fprintf(stderr, "PLUGIN: event %d\n", evt->type);
+    log(instance, "event %d", evt->type);
 
     return 0;
 }
