331a448cf7dd5f59faedb78058c73f2ad2041988
Christian Dywan <christian@webkit.org>
2009-05-20 22:18:34 +0000

    2009-05-21  Christian Dywan  <christian@twotoasts.de>
            Rubberstamped by Gustavo Noronha.
    
            * platform/network/curl/ResourceHandleManager.cpp:
            (WebCore::parseDataUrl): Remove Gtk/ Glib Base64 decoding.
    
    Canonical link: https://commits.webkit.org/35844@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@43935 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/network/curl/ResourceHandleManager.cpp
+++ WebCore/platform/network/curl/ResourceHandleManager.cpp
@@ -46,12 +46,6 @@
 #include <stdio.h>
 #include <wtf/Vector.h>
 
-#if PLATFORM(GTK)
-    #if GLIB_CHECK_VERSION(2,12,0)
-        #define USE_GLIB_BASE64
-    #endif
-#endif
-
 namespace WebCore {
 
 const int selectTimeoutMS = 5;
@@ -512,20 +506,10 @@ static void parseDataUrl(ResourceHandle* handle)
         response.setTextEncodingName(charset);
         client->didReceiveResponse(handle, response);
 
-        // Use the GLib Base64 if available, since WebCore's decoder isn't
-        // general-purpose and fails on Acid3 test 97 (whitespace).
-#ifdef USE_GLIB_BASE64
-        size_t outLength = 0;
-        char* outData = 0;
-        outData = reinterpret_cast<char*>(g_base64_decode(data.utf8().data(), &outLength));
-        if (outData && outLength > 0)
-            client->didReceiveData(handle, outData, outLength, 0);
-        g_free(outData);
-#else
+        // WebCore's decoder fails on Acid3 test 97 (whitespace).
         Vector<char> out;
         if (base64Decode(data.latin1().data(), data.latin1().length(), out) && out.size() > 0)
             client->didReceiveData(handle, out.data(), out.size(), 0);
-#endif
     } else {
         // We have to convert to UTF-16 early due to limitations in KURL
         data = decodeURLEscapeSequences(data, TextEncoding(charset));
