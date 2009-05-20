674aac64ea65130f71a806d5d62d7cc0d7d22281
Xan Lopez <xan@webkit.org>
2009-05-20 14:33:19 +0000

    2009-05-20  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Jan Alonzo.
    
            https://bugs.webkit.org/show_bug.cgi?id=25414
            [GTK] ROLE_PANEL should not be used for paragraphs and list items.
    
            Implement Atk paragraph role for paragraph elements.
    
            * accessibility/gtk/AccessibilityObjectWrapperAtk.cpp:
            (webkit_accessible_get_role):
    
    Canonical link: https://commits.webkit.org/35820@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@43907 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/accessibility/gtk/AccessibilityObjectWrapperAtk.cpp
+++ WebCore/accessibility/gtk/AccessibilityObjectWrapperAtk.cpp
@@ -41,6 +41,7 @@
 #include "Editor.h"
 #include "Frame.h"
 #include "FrameView.h"
+#include "HTMLNames.h"
 #include "IntRect.h"
 #include "NotImplemented.h"
 
@@ -267,6 +268,11 @@ static AtkRole webkit_accessible_get_role(AtkObject* object)
 
     if (!AXObject)
         return ATK_ROLE_UNKNOWN;
+
+    // WebCore does not know about paragraph role
+    Node* node = static_cast<AccessibilityRenderObject*>(AXObject)->renderer()->node();
+    if (node && node->hasTagName(HTMLNames::pTag))
+        return ATK_ROLE_PARAGRAPH;
 
     // Note: Why doesn't WebCore have a password field for this
     if (AXObject->isPasswordField())
