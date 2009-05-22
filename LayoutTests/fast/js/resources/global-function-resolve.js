fad8ed6fba81fab030d408cacc9d53e42ad88dae
Dan Bernstein <mitz@apple.com>
2009-05-22 20:48:54 +0000

    2009-05-22  Darin Adler  <darin@apple.com>
            Reviewed by Dan Bernstein.
    
            - Made the global-function-resolve test independent of hashing order
              by using a fixed set of properties. This also removes the need for us
              to update this test every time we add something to the window object or
              the js-test-pre.js family of scripts.
    
            * fast/js/global-function-resolve-expected.txt: Updated.
            * fast/js/resources/global-function-resolve.js: Use a fixed array instead
            of using all property names from the global object; also made other small
            changes that don't have an effect on the substance of the test.
    
    
    
    Canonical link: https://commits.webkit.org/35938@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@44061 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/js/resources/global-function-resolve.js
+++ LayoutTests/fast/js/resources/global-function-resolve.js
@@ -1,16 +1,73 @@
 description("Test to make sure cached lookups on the global object are performed correctly.");
 
-var funcNames = [];
+var functionNames = [
+    'addEventListener',
+    'alert',
+    'clearInterval',
+    'clearTimeout',
+    'clientInformation',
+    'close',
+    'closed',
+    'confirm',
+    'console',
+    'crypto',
+    'defaultStatus',
+    'defaultstatus',
+    'description',
+    'devicePixelRatio',
+    'dispatchEvent',
+    'document',
+    'getComputedStyle',
+    'getMatchedCSSRules',
+    'getSelection',
+    'history',
+    'innerHeight',
+    'innerWidth',
+    'location',
+    'locationbar',
+    'menubar',
+    'moveBy',
+    'moveTo',
+    'name',
+    'navigator',
+    'open',
+    'openDatabase',
+    'opener',
+    'outerHeight',
+    'outerWidth',
+    'pageXOffset',
+    'pageYOffset',
+    'parent',
+    'prompt',
+    'releaseEvents',
+    'removeEventListener',
+    'resizeBy',
+    'resizeTo',
+    'screen',
+    'screenLeft',
+    'screenTop',
+    'screenX',
+    'screenY',
+    'scroll',
+    'scrollBy',
+    'scrollTo',
+    'scrollX',
+    'scrollY',
+    'setInterval',
+    'setTimeout',
+    'showModalDialog',
+    'status',
+    'stop',
+    'window',
+];
+
 var cachedFunctions = [];
+for (var i = 0; i < functionNames.length; i++)
+    cachedFunctions[i] = new Function("return " + functionNames[i]);
 
-for (var i in this) {
-    funcNames.push(i);
-    cachedFunctions.push(new Function("return " + i));
-}
-
-for (var i = 0; i < funcNames.length; i++) {
-    shouldBe("cachedFunctions["+i+"]()", funcNames[i]);
-    shouldBe("cachedFunctions["+i+"]()", funcNames[i]);
+for (var i = 0; i < functionNames.length; i++) {
+    shouldBe("cachedFunctions["+i+"]()", functionNames[i]);
+    shouldBe("cachedFunctions["+i+"]()", functionNames[i]);
 }
 
 successfullyParsed = true;
