e23fe1d59e92b3ecb511aea7ef26f7f2f794e4c6
David Kilzer <ddkilzer@webkit.org>
2009-05-23 17:16:24 +0000

    Windows build fix for PassOwnPtr
            * WebHistory.cpp:
            (WebHistory::visitedURL): Changed
            std::auto_ptr<Vector<String> >() to 0.
            * WebHistoryItem.cpp:
            (WebHistoryItem::initFromDictionaryRepresentation): Changed type
            of redirectURLsVector from std::auto_ptr<Vector<String> > to
            OwnPtr<Vector<String> >.
    
    Canonical link: https://commits.webkit.org/35968@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@44098 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/win/WebHistory.cpp
+++ WebKit/win/WebHistory.cpp
@@ -740,7 +740,7 @@ void WebHistory::visitedURL(const KURL& url, const String& title, const String& 
         entryPrivate->setLastVisitWasHTTPNonGet(!equalIgnoringCase(httpMethod, "GET") && url.protocolInHTTPFamily());
 
     COMPtr<WebHistoryItem> item(Query, entry);
-    item->historyItem()->setRedirectURLs(std::auto_ptr<Vector<String> >());
+    item->historyItem()->setRedirectURLs(0);
 
     CFDictionaryPropertyBag* userInfo = createUserInfoFromHistoryItem(
         getNotificationString(kWebHistoryItemsAddedNotification), entry);
--- WebKit/win/WebHistoryItem.cpp
+++ WebKit/win/WebHistoryItem.cpp
@@ -39,6 +39,7 @@
 #include <WebCore/KURL.h>
 #pragma warning(pop)
 
+#include <wtf/PassOwnPtr.h>
 #include <wtf/RetainPtr.h>
 
 using namespace WebCore;
@@ -143,10 +144,10 @@ HRESULT STDMETHODCALLTYPE WebHistoryItem::initFromDictionaryRepresentation(void*
         return E_FAIL;
     bool lastVisitWasHTTPNonGet = lastVisitWasHTTPNonGetRef && CFBooleanGetValue(lastVisitWasHTTPNonGetRef);
 
-    std::auto_ptr<Vector<String> > redirectURLsVector;
+    OwnPtr<Vector<String> > redirectURLsVector;
     if (CFArrayRef redirectURLsRef = static_cast<CFArrayRef>(CFDictionaryGetValue(dictionaryRef, redirectURLsKey))) {
         CFIndex size = CFArrayGetCount(redirectURLsRef);
-        redirectURLsVector.reset(new Vector<String>(size));
+        redirectURLsVector = PassOwnPtr<Vector<String> >(new Vector<String>(size));
         for (CFIndex i = 0; i < size; ++i)
             (*redirectURLsVector)[i] = String(static_cast<CFStringRef>(CFArrayGetValueAtIndex(redirectURLsRef, i)));
     }
@@ -193,8 +194,8 @@ HRESULT STDMETHODCALLTYPE WebHistoryItem::initFromDictionaryRepresentation(void*
     if (lastVisitWasHTTPNonGet && (protocolIs(m_historyItem->urlString(), "http") || protocolIs(m_historyItem->urlString(), "https")))
         m_historyItem->setLastVisitWasHTTPNonGet(true);
 
-    if (redirectURLsVector.get())
-        m_historyItem->setRedirectURLs(redirectURLsVector);
+    if (redirectURLsVector)
+        m_historyItem->setRedirectURLs(redirectURLsVector.release());
 
     if (dailyVector.get())
         m_historyItem->adoptVisitCounts(*dailyVector, *weeklyVector);
