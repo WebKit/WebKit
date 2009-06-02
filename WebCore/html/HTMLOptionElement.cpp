86fc3759a5a11d7793ea18f77510192875028fcf
Adam Barth <abarth@webkit.org>
2009-06-02 08:37:41 +0000

    WebCore:
    2009-06-02  Grace Kloba  <klobag@gmail.com>
    
            Reviewed by Darin Adler.  Landed by Adam Barth.
    
            https://bugs.webkit.org/show_bug.cgi?id=26016
    
            As HTMLKeygenElement is derived from HTMLSelectElement, HTMLOptionElement::ownerSelectElement() needs to check both selectTag and keygenTag.
            Test: fast/dom/HTMLKeygenElement/keygen-option-select.html
    
            * html/HTMLOptionElement.cpp:
            (WebCore::HTMLOptionElement::ownerSelectElement):
    
    LayoutTests:
    
    2009-06-02  Grace Kloba  <klobag@gmail.com>
    
            Reviewed by Darin Adler.  Landed by Adam Barth.
    
            https://bugs.webkit.org/show_bug.cgi?id=26016
    
            As HTMLKeygenElement is derived from HTMLSelectElement, HTMLOptionElement::ownerSelectElement() needs to check both selectTag and keygenTag. Added layout test to make sure set selected on option will affect keygen element.
    
            * fast/dom/HTMLKeygenElement: Added.
            * fast/dom/HTMLKeygenElement/keygen-option-select-expected.txt: Added.
            * fast/dom/HTMLKeygenElement/keygen-option-select.html: Added.
    
    
    
    Canonical link: https://commits.webkit.org/36189@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@44354 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/html/HTMLOptionElement.cpp
+++ WebCore/html/HTMLOptionElement.cpp
@@ -161,7 +161,7 @@ void HTMLOptionElement::childrenChanged(bool changedByParser, Node* beforeChange
 HTMLSelectElement* HTMLOptionElement::ownerSelectElement() const
 {
     Node* select = parentNode();
-    while (select && !select->hasTagName(selectTag))
+    while (select && !(select->hasTagName(selectTag) || select->hasTagName(keygenTag)))
         select = select->parentNode();
 
     if (!select)
