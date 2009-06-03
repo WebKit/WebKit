ea5e846bb960dd40aa43e2fca294aa7b030eec94
Kevin Ollivier <kevino@webkit.org>
2009-06-03 17:07:34 +0000

    Reviewed by Kevin Ollivier.
    Use CGContextShowGlyphsWithAdvances to get more accurate text rendering on Mac.
    
    https://bugs.webkit.org/show_bug.cgi?id=26161
    
    
    Canonical link: https://commits.webkit.org/36222@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@44387 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/wx/wxcode/mac/carbon/non-kerned-drawing.cpp
+++ WebCore/platform/wx/wxcode/mac/carbon/non-kerned-drawing.cpp
@@ -24,44 +24,95 @@
  */
 
 #include "config.h"
+#include "FloatSize.h"
 #include "GlyphBuffer.h"
 #include "GraphicsContext.h"
 #include "SimpleFontData.h"
+#include <wtf/Vector.h>
+
+#include <ApplicationServices/ApplicationServices.h>
+
+#include <dlfcn.h>
 
 #include <wx/defs.h>
 #include <wx/dcclient.h>
 #include <wx/dcgraph.h>
 #include <wx/gdicmn.h>
-#include <vector>
+
+
+// Unfortunately we need access to a private function to get the character -> glyph conversion needed to
+// allow us to use CGContextShowGlyphsWithAdvances
+// Note that on < 10.5, the function is called CGFontGetGlyphsForUnicodes, so we need to detect and deal
+// with this.
+typedef void (*CGFontGetGlyphsForUnicharsPtr)(CGFontRef, const UniChar[], const CGGlyph[], size_t);
+static CGFontGetGlyphsForUnicharsPtr CGFontGetGlyphsForUnichars = (CGFontGetGlyphsForUnicharsPtr)dlsym(RTLD_DEFAULT, "CGFontGetGlyphsForUnichars");
 
 namespace WebCore {
 
 void drawTextWithSpacing(GraphicsContext* graphicsContext, const SimpleFontData* font, const wxColour& color, const GlyphBuffer& glyphBuffer, int from, int numGlyphs, const FloatPoint& point)
 {
-#if USE(WXGC)
+    graphicsContext->save();
+    
     wxGCDC* dc = static_cast<wxGCDC*>(graphicsContext->platformContext());
-#else
-    wxDC* dc = graphicsContext->platformContext();
-#endif
 
     wxFont* wxfont = font->getWxFont();
-    if (wxfont->IsOk())
-        dc->SetFont(*wxfont);
-    dc->SetTextForeground(color);
+    graphicsContext->setFillColor(graphicsContext->fillColor());
 
-    // convert glyphs to wxString
-    GlyphBufferGlyph* glyphs = const_cast<GlyphBufferGlyph*>(glyphBuffer.glyphs(from));
-    int offset = point.x();
-    wxString text = wxEmptyString;
-    for (unsigned i = 0; i < numGlyphs; i++) {
-        text = text.Append((wxChar)glyphs[i]);
-        offset += glyphBuffer.advanceAt(from + i);
+    CGContextRef cgContext = static_cast<CGContextRef>(dc->GetGraphicsContext()->GetNativeContext());
+
+    CGFontRef cgFont;
+
+#ifdef wxOSX_USE_CORE_TEXT && wxOSX_USE_CORE_TEXT
+    cgFont = CTFontCopyGraphicsFont((CTFontRef)font->OSXGetCTFont(), NULL);
+#else
+    ATSFontRef fontRef;
+    
+    fontRef = FMGetATSFontRefFromFont(wxfont->MacGetATSUFontID());
+    
+    if (fontRef)
+        cgFont = CGFontCreateWithPlatformFont((void*)&fontRef);
+#endif
+    
+    CGContextSetFont(cgContext, cgFont);
+
+    CGContextSetFontSize(cgContext, wxfont->GetPointSize());
+
+    CGFloat red, green, blue, alpha;
+    graphicsContext->fillColor().getRGBA(red, green, blue, alpha);
+    CGContextSetRGBFillColor(cgContext, red, green, blue, alpha);
+
+    CGAffineTransform matrix = CGAffineTransformIdentity;
+    matrix.b = -matrix.b;
+    matrix.d = -matrix.d;
+    
+    CGContextSetTextMatrix(cgContext, matrix);
+
+    CGContextSetTextPosition(cgContext, point.x(), point.y());
+    
+    const FloatSize* advanceSizes = static_cast<const FloatSize*>(glyphBuffer.advances(from));
+    int size = glyphBuffer.size() - from;
+    CGSize sizes[size];
+    CGGlyph glyphs[numGlyphs];
+    
+    // if the function doesn't exist, we're probably on tiger and need to grab the
+    // function under its old name, CGFontGetGlyphsForUnicodes
+    if (!CGFontGetGlyphsForUnichars)
+        CGFontGetGlyphsForUnichars = (CGFontGetGlyphsForUnicharsPtr)dlsym(RTLD_DEFAULT, "CGFontGetGlyphsForUnicodes");
+    
+    // Let's make sure we got the function under one name or another!
+    ASSERT(CGFontGetGlyphsForUnichars);
+    CGFontGetGlyphsForUnichars(cgFont, glyphBuffer.glyphs(from), glyphs, numGlyphs);
+    
+    for (int i = 0; i < size; i++) {
+        FloatSize fsize = advanceSizes[i];
+        sizes[i] = CGSizeMake(fsize.width(), fsize.height());
     }
     
-    // NOTE: The wx API actually adds the ascent to the y value internally,
-    // so we have to subtract it from the y point here so that the ascent
-    // isn't added twice. 
-    dc->DrawText(text, (wxCoord)point.x(), int(point.y() - font->ascent()));
+    CGContextShowGlyphsWithAdvances(cgContext, glyphs, sizes, numGlyphs);
+    
+    if (cgFont)
+        CGFontRelease(cgFont);
+    graphicsContext->restore();
 }
 
 }
