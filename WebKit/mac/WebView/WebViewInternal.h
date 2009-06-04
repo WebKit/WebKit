56a504f4ee824f5c747204ad9a30454a0a39f694
Sam Weinig <weinig@apple.com>
2009-06-04 03:32:38 +0000

    WebKit:
    2009-06-03  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Mark Rowe.
    
            Added WebDelegateImplementationCaching.h/mm
    
            * WebKit.xcodeproj/project.pbxproj:
    
    WebKit/mac:
    
    2009-06-03  Sam Weinig  <sam@webkit.org>
    
            Reviewed by Mark Rowe.
    
            Move delegate implementation caching logic into its own files.
    
            * WebView/WebDelegateImplementationCaching.h: Copied from mac/WebView/WebViewInternal.h.
            * WebView/WebDelegateImplementationCaching.mm: Copied from mac/WebView/WebView.mm.
            (WebViewGetResourceLoadDelegateImplementations):
            (WebViewGetFrameLoadDelegateImplementations):
            (WebViewGetScriptDebugDelegateImplementations):
            * WebView/WebView.mm:
            * WebView/WebViewInternal.h:
    
    
    
    Canonical link: https://commits.webkit.org/36240@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@44409 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/mac/WebView/WebViewInternal.h
+++ WebKit/mac/WebView/WebViewInternal.h
@@ -33,6 +33,7 @@
 #import "WebTypesInternal.h"
 
 #ifdef __cplusplus
+#import "WebDelegateImplementationCaching.h"
 #import <WebCore/PlatformString.h>
 #import <WebCore/WebCoreKeyboardUIMode.h>
 #import <wtf/HashMap.h>
@@ -176,93 +177,6 @@ namespace WebCore {
 @end
 
 #ifdef __cplusplus
-
-struct WebResourceDelegateImplementationCache {
-    IMP didCancelAuthenticationChallengeFunc;
-    IMP didReceiveAuthenticationChallengeFunc;
-    IMP identifierForRequestFunc;
-    IMP willSendRequestFunc;
-    IMP didReceiveResponseFunc;
-    IMP didReceiveContentLengthFunc;
-    IMP didFinishLoadingFromDataSourceFunc;
-    IMP didFailLoadingWithErrorFromDataSourceFunc;
-    IMP didLoadResourceFromMemoryCacheFunc;
-    IMP willCacheResponseFunc;
-    IMP plugInFailedWithErrorFunc;
-    IMP shouldUseCredentialStorageFunc;
-};
-
-struct WebFrameLoadDelegateImplementationCache {
-    IMP didClearWindowObjectForFrameFunc;
-    IMP windowScriptObjectAvailableFunc;
-    IMP didHandleOnloadEventsForFrameFunc;
-    IMP didReceiveServerRedirectForProvisionalLoadForFrameFunc;
-    IMP didCancelClientRedirectForFrameFunc;
-    IMP willPerformClientRedirectToURLDelayFireDateForFrameFunc;
-    IMP didChangeLocationWithinPageForFrameFunc;
-    IMP willCloseFrameFunc;
-    IMP didStartProvisionalLoadForFrameFunc;
-    IMP didReceiveTitleForFrameFunc;
-    IMP didCommitLoadForFrameFunc;
-    IMP didFailProvisionalLoadWithErrorForFrameFunc;
-    IMP didFailLoadWithErrorForFrameFunc;
-    IMP didFinishLoadForFrameFunc;
-    IMP didFirstLayoutInFrameFunc;
-    IMP didFirstVisuallyNonEmptyLayoutInFrameFunc;
-    IMP didReceiveIconForFrameFunc;
-    IMP didFinishDocumentLoadForFrameFunc;
-};
-
-struct WebScriptDebugDelegateImplementationCache {
-    BOOL didParseSourceExpectsBaseLineNumber;
-    IMP didParseSourceFunc;
-    IMP failedToParseSourceFunc;
-    IMP didEnterCallFrameFunc;
-    IMP willExecuteStatementFunc;
-    IMP willLeaveCallFrameFunc;
-    IMP exceptionWasRaisedFunc;
-};
-
-WebResourceDelegateImplementationCache* WebViewGetResourceLoadDelegateImplementations(WebView *);
-WebFrameLoadDelegateImplementationCache* WebViewGetFrameLoadDelegateImplementations(WebView *);
-WebScriptDebugDelegateImplementationCache* WebViewGetScriptDebugDelegateImplementations(WebView *);
-
-id CallFormDelegate(WebView *, SEL, id, id);
-id CallFormDelegate(WebView *self, SEL selector, id object1, id object2, id object3, id object4, id object5);
-BOOL CallFormDelegateReturningBoolean(BOOL, WebView *, SEL, id, SEL, id);
-
-id CallUIDelegate(WebView *, SEL);
-id CallUIDelegate(WebView *, SEL, id);
-id CallUIDelegate(WebView *, SEL, NSRect);
-id CallUIDelegate(WebView *, SEL, id, id);
-id CallUIDelegate(WebView *, SEL, id, BOOL);
-id CallUIDelegate(WebView *, SEL, id, id, id);
-id CallUIDelegate(WebView *, SEL, id, NSUInteger);
-float CallUIDelegateReturningFloat(WebView *, SEL);
-BOOL CallUIDelegateReturningBoolean(BOOL, WebView *, SEL);
-BOOL CallUIDelegateReturningBoolean(BOOL, WebView *, SEL, id);
-BOOL CallUIDelegateReturningBoolean(BOOL, WebView *, SEL, id, id);
-BOOL CallUIDelegateReturningBoolean(BOOL, WebView *, SEL, id, BOOL);
-
-id CallFrameLoadDelegate(IMP, WebView *, SEL);
-id CallFrameLoadDelegate(IMP, WebView *, SEL, id);
-id CallFrameLoadDelegate(IMP, WebView *, SEL, id, id);
-id CallFrameLoadDelegate(IMP, WebView *, SEL, id, id, id);
-id CallFrameLoadDelegate(IMP, WebView *, SEL, id, id, id, id);
-id CallFrameLoadDelegate(IMP, WebView *, SEL, id, NSTimeInterval, id, id);
-
-id CallResourceLoadDelegate(IMP, WebView *, SEL, id, id);
-id CallResourceLoadDelegate(IMP, WebView *, SEL, id, id, id);
-id CallResourceLoadDelegate(IMP, WebView *, SEL, id, id, id, id);
-id CallResourceLoadDelegate(IMP, WebView *, SEL, id, NSInteger, id);
-id CallResourceLoadDelegate(IMP, WebView *, SEL, id, id, NSInteger, id);
-
-BOOL CallResourceLoadDelegateReturningBoolean(BOOL, IMP, WebView *, SEL, id, id);
-
-id CallScriptDebugDelegate(IMP, WebView *, SEL, id, id, NSInteger, id);
-id CallScriptDebugDelegate(IMP, WebView *, SEL, id, NSInteger, id, NSInteger, id);
-id CallScriptDebugDelegate(IMP, WebView *, SEL, id, NSInteger, id, id, id);
-id CallScriptDebugDelegate(IMP, WebView *, SEL, id, NSInteger, NSInteger, id);
 
 extern BOOL applicationIsTerminating;
 extern int pluginDatabaseClientCount;
