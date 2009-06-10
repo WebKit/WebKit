87a940b2550006fb468991ec4dda4a625336cf7d
Brent Fulgham <bfulgham@webkit.org>
2009-06-10 19:05:11 +0000

    2009-06-10  Feng Qian  <feng@chromium.org>
            Reviewed by Eric Seidel.
    
            Add Android port files to WebCore/platform (part 3).
            https://bugs.webkit.org/show_bug.cgi?id=23296
    
    
    Canonical link: https://commits.webkit.org/36406@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@44581 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/platform/android/FileSystemAndroid.cpp
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2009, The Android Open Source Project
+ * Copyright (C) 2007 Holger Hans Peter Freyther
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+#include "config.h"
+#include "FileSystem.h"
+
+#include "CString.h"
+#include "StringBuilder.h"
+#include <fnmatch.h>
+#include <dlfcn.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include "cutils/log.h"
+
+namespace WebCore {
+
+// Global static used to store the base to the plugin path.
+// This is set in WebSettings.cpp
+String sPluginPath;
+
+CString fileSystemRepresentation(const String& path)
+{
+    return path.utf8();
+}
+
+CString openTemporaryFile(const char* prefix, PlatformFileHandle& handle)
+{
+    int number = rand() % 10000 + 1;
+    CString filename;
+    do {
+        StringBuilder builder;
+        builder.append(sPluginPath);
+        builder.append('/');
+        builder.append(prefix);
+        builder.append(String::number(number));
+        filename = builder.toString().utf8();
+        const char* fstr = filename.data();
+        handle = open(filename.data(), O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+        number++;
+        
+        if (handle != -1)
+            return filename;
+    } while (errno == EEXIST);
+    
+    return CString();
+}
+
+bool unloadModule(PlatformModule module)
+{
+    return dlclose(module) == 0;
+}
+
+void closeFile(PlatformFileHandle& handle)
+{
+    if (isHandleValid(handle)) {
+        close(handle);
+        handle = invalidPlatformFileHandle;
+    }
+}
+
+int writeToFile(PlatformFileHandle handle, const char* data, int length)
+{
+    int totalBytesWritten = 0;
+    while (totalBytesWritten < length) {
+        int bytesWritten = write(handle, data, (size_t)(length - totalBytesWritten));
+        if (bytesWritten < 0 && errno != EINTR)
+            return -1;
+        else if (bytesWritten > 0)
+            totalBytesWritten += bytesWritten;
+    }
+
+    return totalBytesWritten;
+}
+
+String homeDirectoryPath() 
+{
+    return sPluginPath;
+}
+
+Vector<String> listDirectory(const String& path, const String& filter)
+{
+    Vector<String> entries;
+    CString cpath = path.utf8();
+    CString cfilter = filter.utf8();
+    DIR* dir = opendir(cpath.data());
+    if (dir) {
+        struct dirent* dp;
+        while (dp = readdir(dir)) {
+            const char* name = dp->d_name;
+            if (!strcmp(name, ".") || !strcmp(name, ".."))
+                continue;
+            if (fnmatch(cfilter.data(), name, 0))
+                continue;
+            char filePath[1024];
+            if ((int) (sizeof(filePath) - 1) < snprintf(filePath,
+                    sizeof(filePath), "%s/%s", cpath.data(), name)) {
+                continue; // buffer overflow
+            }
+            entries.append(filePath);
+        }
+        closedir(dir);
+    }
+    return entries;
+}
+
+} // namespace WebCore
