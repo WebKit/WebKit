130d82624a57ec6b65ff943b28633931674195e8
Dimitri Glazkov <dglazkov@chromium.org>
2009-06-16 20:31:45 +0000

    2009-06-16  Tony Chang  <tony@chromium.org>
            Reviewed by Darin Fisher.
    
            Fix a UMR in WebCore::BitStack by initializing new memory to 0.
            https://bugs.webkit.org/show_bug.cgi?id=26449
            No new tests, covered by purify.
    
            * editing/TextIterator.cpp:
            (WebCore::BitStack::push):
    
    Canonical link: https://commits.webkit.org/36552@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@44735 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/editing/TextIterator.cpp
+++ WebCore/editing/TextIterator.cpp
@@ -109,8 +109,10 @@ void BitStack::push(bool bit)
 {
     unsigned index = m_size / bitsInWord;
     unsigned shift = m_size & bitInWordMask;
-    if (!shift && index == m_words.size())
+    if (!shift && index == m_words.size()) {
         m_words.grow(index + 1);
+        m_words[index] = 0;
+    }
     unsigned& word = m_words[index];
     unsigned mask = 1U << shift;
     if (bit)
