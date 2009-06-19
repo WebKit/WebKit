6a6bc97fb7f9e16b389de81352fa8db5849e4a31
Adam Treat <treat@webkit.org>
2009-06-19 17:01:47 +0000

    2009-06-19  Yong Li  <yong.li@torchmobile.com>
            Reviewed by George Staikos.
    
            https://bugs.webkit.org/show_bug.cgi?id=26543
            Windows CE uses 'GetLastError' instead of 'errno.'
    
            * interpreter/RegisterFile.h:
            (JSC::RegisterFile::RegisterFile):
            (JSC::RegisterFile::grow):
    
    Canonical link: https://commits.webkit.org/36670@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@44857 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/interpreter/RegisterFile.h
+++ JavaScriptCore/interpreter/RegisterFile.h
@@ -176,19 +176,31 @@ namespace JSC {
     #if HAVE(MMAP)
         m_buffer = static_cast<Register*>(mmap(0, bufferLength, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANON, VM_TAG_FOR_REGISTERFILE_MEMORY, 0));
         if (m_buffer == MAP_FAILED) {
+#if PLATFORM(WINCE)
+            fprintf(stderr, "Could not allocate register file: %d\n", GetLastError());
+#else
             fprintf(stderr, "Could not allocate register file: %d\n", errno);
+#endif
             CRASH();
         }
     #elif HAVE(VIRTUALALLOC)
         m_buffer = static_cast<Register*>(VirtualAlloc(0, roundUpAllocationSize(bufferLength, commitSize), MEM_RESERVE, PAGE_READWRITE));
         if (!m_buffer) {
+#if PLATFORM(WINCE)
+            fprintf(stderr, "Could not allocate register file: %d\n", GetLastError());
+#else
             fprintf(stderr, "Could not allocate register file: %d\n", errno);
+#endif
             CRASH();
         }
         size_t committedSize = roundUpAllocationSize(maxGlobals * sizeof(Register), commitSize);
         void* commitCheck = VirtualAlloc(m_buffer, committedSize, MEM_COMMIT, PAGE_READWRITE);
         if (commitCheck != m_buffer) {
+#if PLATFORM(WINCE)
+            fprintf(stderr, "Could not allocate register file: %d\n", GetLastError());
+#else
             fprintf(stderr, "Could not allocate register file: %d\n", errno);
+#endif
             CRASH();
         }
         m_commitEnd = reinterpret_cast<Register*>(reinterpret_cast<char*>(m_buffer) + committedSize);
@@ -222,7 +234,11 @@ namespace JSC {
         if (newEnd > m_commitEnd) {
             size_t size = roundUpAllocationSize(reinterpret_cast<char*>(newEnd) - reinterpret_cast<char*>(m_commitEnd), commitSize);
             if (!VirtualAlloc(m_commitEnd, size, MEM_COMMIT, PAGE_READWRITE)) {
+#if PLATFORM(WINCE)
+                fprintf(stderr, "Could not allocate register file: %d\n", GetLastError());
+#else
                 fprintf(stderr, "Could not allocate register file: %d\n", errno);
+#endif
                 CRASH();
             }
             m_commitEnd = reinterpret_cast<Register*>(reinterpret_cast<char*>(m_commitEnd) + size);
