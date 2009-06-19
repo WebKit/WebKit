005899a2cd60e2715906d9312423db88483aaee9
Oliver Hunt <oliver@apple.com>
2009-06-19 09:23:24 +0000

    2009-06-18  Shinichiro Hamaji  <hamaji@chromium.org>
            Reviewed by Oliver Hunt.
    
            Bug 26426: Canvas: rotation of 'no-repeat' pattern is weird
            <https://bugs.webkit.org/show_bug.cgi?id=26426>
    
            Use 1<<23-1 as steps of no-repeat patterns instead of 100000000.0f.
            The original number cannot be represented by mantissa of float
            (23bit) so that it caused some error.
    
            Test: fast/canvas/image-object-in-canvas.html:
    
            * platform/graphics/cg/PatternCG.cpp:
            (WebCore::Pattern::createPlatformPattern):
    
    
    Canonical link: https://commits.webkit.org/36663@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@44850 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/cg/PatternCG.cpp
+++ WebCore/platform/graphics/cg/PatternCG.cpp
@@ -62,10 +62,11 @@ CGPatternRef Pattern::createPlatformPattern(const TransformationMatrix& userSpac
     // If FLT_MAX should also be used for xStep or yStep, nothing is rendered. Using fractions of FLT_MAX also
     // result in nothing being rendered.
     // INT_MAX is almost correct, but there seems to be some number wrapping occuring making the fill
-    // pattern is not filled correctly. 
-    // So, just pick a really large number that works. 
-    float xStep = m_repeatX ? tileRect.width() : (100000000.0f);
-    float yStep = m_repeatY ? tileRect.height() : (100000000.0f);
+    // pattern is not filled correctly.
+    // To make error of floating point less than 1, we need to use a big number which is smaller than the
+    // number of mantissa of float (1 << 23).
+    CGFloat xStep = m_repeatX ? tileRect.width() : (1 << 23 - 1);
+    CGFloat yStep = m_repeatY ? tileRect.height() : (1 << 23 - 1);
 
     // The pattern will release the tile when it's done rendering in patternReleaseCallback
     tileImage()->ref();
