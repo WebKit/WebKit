ddb6308eb957dd8e3d062b23e977d73a43eadadd
Eric Seidel <eric@webkit.org>
2009-07-02 06:54:37 +0000

    2009-07-01  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            bugzilla-tool fails for SVN users
            https://bugs.webkit.org/show_bug.cgi?id=26914
    
            To fix this I moved svn from -F - to using -m
            In order for -m to work I had to move us off of shell=True
            To move off of shell=True all call sites for run_command
            which take args, need to pass their args as an list instead of a string.
    
            In order for the final bug update to work correctly, I had to
            abstract the way that we parse out revision numbers from the commit text.
    
            * Scripts/bugzilla-tool:
            * Scripts/modules/scm.py:
    
    Canonical link: https://commits.webkit.org/37131@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@45469 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/modules/scm.py
+++ WebKitTools/Scripts/modules/scm.py
@@ -59,9 +59,9 @@ class SCM:
         self.dryrun = dryrun
 
     @staticmethod
-    def run_command(command, cwd=None, input=None, raise_on_failure=True, return_exit_code=False):
+    def run_command(args, cwd=None, input=None, raise_on_failure=True, return_exit_code=False):
         stdin = subprocess.PIPE if input else None
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stdin=stdin, shell=True, cwd=cwd)
+        process = subprocess.Popen(args, stdout=subprocess.PIPE, stdin=stdin, cwd=cwd)
         output = process.communicate(input)[0].rstrip()
         exit_code = process.wait()
         if raise_on_failure and exit_code:
@@ -120,6 +120,10 @@ class SCM:
     def find_checkout_root(path):
         raise NotImplementedError, "subclasses must implement"
 
+    @staticmethod
+    def commit_success_regexp():
+        raise NotImplementedError, "subclasses must implement"
+
     def working_directory_is_clean(self):
         raise NotImplementedError, "subclasses must implement"
 
@@ -146,7 +150,8 @@ class SCM:
     
     # Subclasses must indicate if they support local commits,
     # but the SCM baseclass will only call local_commits methods when this is true.
-    def supports_local_commits(self):
+    @staticmethod
+    def supports_local_commits():
         raise NotImplementedError, "subclasses must implement"
 
     def commit_locally_with_message(self, message):
@@ -178,23 +183,27 @@ class SVN(SCM):
             if last_path == path:
                 return None
     
+    @staticmethod
+    def commit_success_regexp():
+        return "^Committed revision (?P<svn_revision>\d+)\.$"
+    
     def svn_version(self):
         if not self.cached_version:
-            self.cached_version = self.run_command("svn --version --quiet")
+            self.cached_version = self.run_command(['svn', '--version', '--quiet'])
         
         return self.cached_version
 
     def working_directory_is_clean(self):
-        return self.run_command("svn diff") == ""
+        return self.run_command(['svn', 'diff']) == ""
 
     def clean_working_directory(self):
-        self.run_command("svn revert -R .")
+        self.run_command(['svn', 'revert', '-R', '.'])
 
     def update_webkit(self):
         self.run_command(self.script_path("update-webkit"))
 
     def status_command(self):
-        return 'svn status'
+        return ['svn', 'status']
 
     def changed_files(self):
         if self.svn_version() > "1.6":
@@ -203,7 +212,8 @@ class SVN(SCM):
             status_regexp = "^(?P<status>[ACDMR]).{5} (?P<filename>.+)$"
         return self.run_status_and_extract_filenames(self.status_command(), status_regexp)
 
-    def supports_local_commits(self):
+    @staticmethod
+    def supports_local_commits():
         return False
 
     def display_name(self):
@@ -215,7 +225,7 @@ class SVN(SCM):
     def commit_with_message(self, message):
         if self.dryrun:
             return "Dry run, no remote commit."
-        return self.run_command('svn commit -F -', input=message)
+        return self.run_command(['svn', 'commit', '-m', message])
 
 # All git-specific logic should go here.
 class Git(SCM):
@@ -224,51 +234,56 @@ class Git(SCM):
     
     @classmethod
     def in_working_directory(cls, path):
-        return cls.run_command("git rev-parse --is-inside-work-tree 2>&1", cwd=path) == "true"
+        return cls.run_command(['git', 'rev-parse', '--is-inside-work-tree'], cwd=path) == "true"
 
     @classmethod
     def find_checkout_root(cls, path):
         # "git rev-parse --show-cdup" would be another way to get to the root
-        (checkout_root, dot_git) = os.path.split(cls.run_command("git rev-parse --git-dir", cwd=path))
+        (checkout_root, dot_git) = os.path.split(cls.run_command(['git', 'rev-parse', '--git-dir'], cwd=path))
         # If we were using 2.6 # checkout_root = os.path.relpath(checkout_root, path)
         if not os.path.isabs(checkout_root): # Sometimes git returns relative paths
             checkout_root = os.path.join(path, checkout_root)
         return checkout_root
     
+    @staticmethod
+    def commit_success_regexp():
+        return "^Committed r(?P<svn_revision>\d+)$"
+    
     def discard_local_commits(self):
-        self.run_command("git reset --hard trunk")
+        self.run_command(['git', 'reset', '--hard', 'trunk'])
     
     def local_commits(self):
-        return self.run_command("git log --pretty=oneline head...trunk").splitlines()
-    
+        return self.run_command('git', 'log', '--pretty=oneline', 'HEAD...trunk').splitlines()
+
     def working_directory_is_clean(self):
-        return self.run_command("git diff-index HEAD") == ""
+        return self.run_command(['git', 'diff-index', 'HEAD']) == ""
     
     def clean_working_directory(self):
         # Could run git clean here too, but that wouldn't match working_directory_is_clean
-        self.run_command("git reset --hard HEAD")
+        self.run_command(['git', 'reset', '--hard', 'HEAD'])
     
     def update_webkit(self):
         # FIXME: Should probably call update-webkit, no?
         log("Updating working directory")
-        self.run_command("git svn rebase")
+        self.run_command(['git', 'svn', 'rebase'])
 
     def status_command(self):
-        return 'git status'
+        return ['git', 'status']
 
     def changed_files(self):
-        status_command = 'git diff -r --name-status -C -M HEAD'
+        status_command = ['git', 'diff', '-r', '--name-status', '-C', '-M', 'HEAD']
         status_regexp = '^(?P<status>[ADM])\t(?P<filename>.+)$'
         return self.run_status_and_extract_filenames(status_command, status_regexp)
     
-    def supports_local_commits(self):
+    @staticmethod
+    def supports_local_commits():
         return True
 
     def display_name(self):
         return "git"
 
     def create_patch(self):
-        return self.run_command("git diff HEAD")
+        return self.run_command(['git', 'diff', 'HEAD'])
 
     def commit_with_message(self, message):
         self.commit_locally_with_message(message)
@@ -277,31 +292,29 @@ class Git(SCM):
     # Git-specific methods:
     
     def commit_locally_with_message(self, message):
-        self.run_command('git commit -a -F -', input=message)
+        self.run_command(['git', 'commit', '--all', '-F', '-'], input=message)
         
     def push_local_commits_to_server(self):
         if self.dryrun:
             return "Dry run, no remote commit."
-        return self.run_command('git svn dcommit')
+        return self.run_command(['git', 'svn', 'dcommit'])
 
     def commit_ids_from_range_arguments(self, args, cherry_pick=False):
         # First get the commit-ids for the passed in revisions.
-        rev_parse_args = ['git', 'rev-parse', '--revs-only'] + args
-        revisions = self.run_command(" ".join(rev_parse_args)).splitlines()
-        
+        revisions = self.run_command(['git', 'rev-parse', '--revs-only'] + args).splitlines()
+
         if cherry_pick:
             return revisions
-        
+
         # If we're not cherry picking and were only passed one revision, assume "^revision head" aka "revision..head".
         if len(revisions) < 2:
             revisions[0] = "^" + revisions[0]
             revisions.append("HEAD")
-        
-        rev_list_args = ['git', 'rev-list'] + revisions
-        return self.run_command(" ".join(rev_list_args)).splitlines()
+
+        return self.run_command(['git', 'rev-list'] + revisions).splitlines()
 
     def commit_message_for_commit(self, commit_id):
-        commit_lines = self.run_command("git cat-file commit " + commit_id).splitlines()
+        commit_lines = self.run_command(['git', 'cat-file', 'commit', commit_id]).splitlines()
 
         # Skip the git headers.
         first_line_after_headers = 0
@@ -311,8 +324,5 @@ class Git(SCM):
                 break
         return "\n".join(commit_lines[first_line_after_headers:])
 
-    def show_diff_command_for_commit(self, commit_id):
-        return "git diff-tree -p " + commit_id
-
     def files_changed_summary_for_commit(self, commit_id):
-        return self.run_command("git diff-tree --shortstat --no-commit-id " + commit_id)
+        return self.run_command(['git', 'diff-tree', '--shortstat', '--no-commit-id', commit_id])
