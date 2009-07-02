a15c3a8b65894f72a5761eccef03341998b58873
Sam Weinig <weinig@apple.com>
2009-07-02 23:45:32 +0000

    2009-07-02  Sam Weinig  <sam@webkit.org>
            Reviewed by Dave Hyatt.
    
            Fix for <rdar://problem/5230700>
            Remove local .xhtml file workaround
    
            * platform/network/mac/ResourceResponseMac.mm:
            (WebCore::ResourceResponse::platformLazyInit): Work around is no longer necessary.
    
    
    
    Canonical link: https://commits.webkit.org/37165@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@45504 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/network/mac/ResourceResponseMac.mm
+++ WebCore/platform/network/mac/ResourceResponseMac.mm
@@ -86,20 +86,8 @@ void ResourceResponse::platformLazyInit()
         NSEnumerator *e = [headers keyEnumerator];
         while (NSString *name = [e nextObject])
             m_httpHeaderFields.set(name, [headers objectForKey:name]);
-    } else {
+    } else
         m_httpStatusCode = 0;
-
-#ifndef BUILDING_ON_TIGER
-        // FIXME: This is a work around for <rdar://problem/5230154> (-[NSURLConnection initWithRequest:delegate:] 
-        // is returning incorrect MIME type for local .xhtml files) which is only required in Leopard.
-        if (m_url.isLocalFile() && m_mimeType == "text/html") {
-            const String& path = m_url.path();
-            DEFINE_STATIC_LOCAL(const String, xhtmlExt, (".xhtml"));
-            if (path.endsWith(xhtmlExt, false))
-                m_mimeType = "application/xhtml+xml";
-        }
-#endif
-    }
 }
 
 bool ResourceResponse::platformCompare(const ResourceResponse& a, const ResourceResponse& b)
