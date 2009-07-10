de0d1120704ae7b339fde3b8b848aca346e849e4
Dan Bernstein <mitz@apple.com>
2009-07-10 17:45:29 +0000

    - fix the build by reverting the ill-advised r45711
            * page/FrameView.cpp:
            (WebCore::FrameView::scrollToAnchor):
    
    
    
    Canonical link: https://commits.webkit.org/37366@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@45713 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/page/FrameView.cpp
+++ WebCore/page/FrameView.cpp
@@ -1141,7 +1141,7 @@ void FrameView::scrollToAnchor()
 
     IntRect rect;
     if (anchorNode != m_frame->document())
-        rect = anchorNode->getRect(true);
+        rect = anchorNode->getRect();
 
     // Scroll nested layers and frames to reveal the anchor.
     // Align to the top and to the closest side (this matches other browsers).
