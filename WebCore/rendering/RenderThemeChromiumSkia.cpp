32ff2620bc799a70d8400fe9c4e86c144d113bf8
Adam Langley <agl@chromium.org>
2009-07-10 19:10:26 +0000

    2009-07-10  Adam Langley  <agl@google.com>
            Reviewed by Darin Fisher.
    
            Chromium Linux: use disabled images for disabled widgets.
    
            https://bugs.webkit.org/show_bug.cgi?id=27106
    
            Previously, checkboxes and radio controls rendered the same even if disabled.
            The Chromium side of this change is r20224.
    
            * rendering/RenderThemeChromiumSkia.cpp:
            (WebCore::RenderThemeChromiumSkia::paintCheckbox):
            (WebCore::RenderThemeChromiumSkia::paintRadio):
    
    
    Canonical link: https://commits.webkit.org/37369@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@45716 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderThemeChromiumSkia.cpp
+++ WebCore/rendering/RenderThemeChromiumSkia.cpp
@@ -212,8 +212,16 @@ bool RenderThemeChromiumSkia::paintCheckbox(RenderObject* o, const RenderObject:
 {
     static Image* const checkedImage = Image::loadPlatformResource("linuxCheckboxOn").releaseRef();
     static Image* const uncheckedImage = Image::loadPlatformResource("linuxCheckboxOff").releaseRef();
+    static Image* const disabledCheckedImage = Image::loadPlatformResource("linuxCheckboxDisabledOn").releaseRef();
+    static Image* const disabledUncheckedImage = Image::loadPlatformResource("linuxCheckboxDisabledOff").releaseRef();
 
-    Image* image = this->isChecked(o) ? checkedImage : uncheckedImage;
+    Image* image;
+
+    if (this->isEnabled(o))
+        image = this->isChecked(o) ? checkedImage : uncheckedImage;
+    else
+        image = this->isChecked(o) ? disabledCheckedImage : disabledUncheckedImage;
+
     i.context->drawImage(image, rect);
     return false;
 }
@@ -237,8 +245,15 @@ bool RenderThemeChromiumSkia::paintRadio(RenderObject* o, const RenderObject::Pa
 {
     static Image* const checkedImage = Image::loadPlatformResource("linuxRadioOn").releaseRef();
     static Image* const uncheckedImage = Image::loadPlatformResource("linuxRadioOff").releaseRef();
+    static Image* const disabledCheckedImage = Image::loadPlatformResource("linuxRadioDisabledOn").releaseRef();
+    static Image* const disabledUncheckedImage = Image::loadPlatformResource("linuxRadioDisabledOff").releaseRef();
 
-    Image* image = this->isChecked(o) ? checkedImage : uncheckedImage;
+    Image* image;
+    if (this->isEnabled(o))
+        image = this->isChecked(o) ? checkedImage : uncheckedImage;
+    else
+        image = this->isChecked(o) ? disabledCheckedImage : disabledUncheckedImage;
+
     i.context->drawImage(image, rect);
     return false;
 }
