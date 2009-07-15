8ac5b02685f64c1b657767a08357fac3882bb806
Brent Fulgham <bfulgham@webkit.org>
2009-07-15 06:00:24 +0000

    2009-07-14  Steve Falkenburg  <sfalken@apple.com>
            Reorganize JavaScriptCore headers into:
            API: include/JavaScriptCore/
            Private: include/private/JavaScriptCore/
    
            Reviewed by Darin Adler.
    
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.vcproj:
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCoreCommon.vsprops:
            * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCoreGenerated.make:
            * JavaScriptCore.vcproj/testapi/testapi.vcproj:
            * JavaScriptCore.vcproj/testapi/testapiCommon.vsprops:
    
    
    
    Canonical link: https://commits.webkit.org/37527@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@45897 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/runtime/JSCell.h
+++ JavaScriptCore/runtime/JSCell.h
@@ -31,7 +31,7 @@
 
 namespace JSC {
 
-    class JSCell : public Noncopyable {
+    class JSCell : public NoncopyableCustomAllocated {
         friend class GetterSetter;
         friend class Heap;
         friend class JIT;
