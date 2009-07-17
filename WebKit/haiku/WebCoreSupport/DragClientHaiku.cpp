88cc900c84fb55f6ead38afbba48d54aa8610f9c
Adam Barth <abarth@webkit.org>
2009-07-17 06:02:38 +0000

    2009-07-16  Maxime Simon  <simon.maxime@gmail.com>
            Reviewed by Eric Seidel.
    
            Added DragClient for Haiku WebCore support.
            https://bugs.webkit.org/show_bug.cgi?id=26952
    
            Adding two files, DragClientHaiku.h and DragClientHaiku.cpp
    
            * haiku/WebCoreSupport/DragClientHaiku.cpp: Added.
            (WebCore::DragClientHaiku::actionMaskForDrag):
            (WebCore::DragClientHaiku::willPerformDragDestinationAction):
            (WebCore::DragClientHaiku::dragControllerDestroyed):
            (WebCore::DragClientHaiku::dragSourceActionMaskForPoint):
            (WebCore::DragClientHaiku::willPerformDragSourceAction):
            (WebCore::DragClientHaiku::startDrag):
            (WebCore::DragClientHaiku::createDragImageForLink):
            * haiku/WebCoreSupport/DragClientHaiku.h: Added.
    
    Canonical link: https://commits.webkit.org/37638@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@46014 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebKit/haiku/WebCoreSupport/DragClientHaiku.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2007 Apple Inc.  All rights reserved.
+ * Copyright (C) 2007 Ryan Leavengood <leavengood@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+#include "config.h"
+#include "DragClientHaiku.h"
+
+#include "NotImplemented.h"
+
+
+namespace WebCore {
+
+DragDestinationAction DragClientHaiku::actionMaskForDrag(DragData*)
+{
+    notImplemented();
+    return DragDestinationActionAny;
+}
+
+void DragClientHaiku::willPerformDragDestinationAction(DragDestinationAction, DragData*)
+{
+    notImplemented();
+}
+
+void DragClientHaiku::dragControllerDestroyed()
+{
+    notImplemented();
+}
+
+DragSourceAction DragClientHaiku::dragSourceActionMaskForPoint(const IntPoint&)
+{
+    notImplemented();
+    return DragSourceActionAny;
+}
+
+void DragClientHaiku::willPerformDragSourceAction(DragSourceAction, const IntPoint&, Clipboard*)
+{
+    notImplemented();
+}
+
+void DragClientHaiku::startDrag(DragImageRef dragImage, const IntPoint&, const IntPoint&, Clipboard*, Frame*, bool)
+{
+    notImplemented();
+}
+
+DragImageRef DragClientHaiku::createDragImageForLink(KURL&, const String&, Frame*)
+{
+    notImplemented();
+    return 0;
+}
+
+} // namespace WebCore
+
