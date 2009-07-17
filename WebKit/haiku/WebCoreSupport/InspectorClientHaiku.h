164ea0c97aaa71750dae981bca40d7878638d391
Adam Barth <abarth@webkit.org>
2009-07-17 06:03:09 +0000

    2009-07-16  Maxime Simon  <simon.maxime@gmail.com>
            Reviewed by Eric Seidel.
    
            Added InspectorClient for Haiku WebCore support.
            https://bugs.webkit.org/show_bug.cgi?id=26952
    
            Adding two files, InspectorClientHaiku.h and InspectorClientHaiku.cpp
    
            * haiku/WebCoreSupport/InspectorClientHaiku.cpp: Added.
            (WebCore::InspectorClientHaiku::inspectorDestroyed):
            (WebCore::InspectorClientHaiku::createPage):
            (WebCore::InspectorClientHaiku::localizedStringsURL):
            (WebCore::InspectorClientHaiku::hiddenPanels):
            (WebCore::InspectorClientHaiku::showWindow):
            (WebCore::InspectorClientHaiku::closeWindow):
            (WebCore::InspectorClientHaiku::attachWindow):
            (WebCore::InspectorClientHaiku::detachWindow):
            (WebCore::InspectorClientHaiku::setAttachedWindowHeight):
            (WebCore::InspectorClientHaiku::highlight):
            (WebCore::InspectorClientHaiku::hideHighlight):
            (WebCore::InspectorClientHaiku::inspectedURLChanged):
            (WebCore::InspectorClientHaiku::populateSetting):
            (WebCore::InspectorClientHaiku::storeSetting):
            (WebCore::InspectorClientHaiku::removeSetting):
            * haiku/WebCoreSupport/InspectorClientHaiku.h: Added.
    
    Canonical link: https://commits.webkit.org/37640@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@46016 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebKit/haiku/WebCoreSupport/InspectorClientHaiku.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2007 Apple Inc.  All rights reserved.
+ * Copyright (C) 2007 Ryan Leavengood <leavengood@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer. 
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution. 
+ * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef InspectorClientHaiku_h
+#define InspectorClientHaiku_h
+
+#include "InspectorClient.h"
+
+
+namespace WebCore {
+    class Node;
+    class Page;
+    class String;
+
+    class InspectorClientHaiku : public InspectorClient {
+    public:
+        virtual void inspectorDestroyed();
+
+        virtual Page* createPage();
+
+        virtual String localizedStringsURL();
+
+        virtual String hiddenPanels();
+
+        virtual void showWindow();
+        virtual void closeWindow();
+
+        virtual void attachWindow();
+        virtual void detachWindow();
+
+        virtual void setAttachedWindowHeight(unsigned height);
+
+        virtual void highlight(Node*);
+        virtual void hideHighlight();
+
+        virtual void inspectedURLChanged(const String& newURL);
+
+        virtual void populateSetting(const String& key, InspectorController::Setting&);
+        virtual void storeSetting(const String& key, const InspectorController::Setting&);
+        virtual void removeSetting(const String& key);
+    };
+} // namespace WebCore
+
+#endif // InspectorClientHaiku_h
+
