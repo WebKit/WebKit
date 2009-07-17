04085f8a71b8c0349aaab6c6cd2c2d4747fd20a2
Adam Barth <abarth@webkit.org>
2009-07-17 05:47:08 +0000

    2009-07-16  Maxime Simon  <simon.maxime@gmail.com>
            Reviewed by Eric Seidel.
    
            Added Haiku-specific files for WebCore/page/.
            https://bugs.webkit.org/show_bug.cgi?id=26949
    
            Adding three new files, DragControllerHaiku.cpp, EventHandlerHaiku.cpp
            and FrameHaiku.cpp
    
            * page/haiku/DragControllerHaiku.cpp: Added.
            (WebCore::DragController::isCopyKeyDown):
            (WebCore::DragController::dragOperation):
            (WebCore::DragController::maxDragImageSize):
            (WebCore::DragController::cleanupAfterSystemDrag):
            * page/haiku/EventHandlerHaiku.cpp: Added.
            (WebCore::isKeyboardOptionTab):
            (WebCore::EventHandler::invertSenseOfTabsToLinks):
            (WebCore::EventHandler::tabsToAllControls):
            (WebCore::EventHandler::focusDocumentView):
            (WebCore::EventHandler::passWidgetMouseDownEventToWidget):
            (WebCore::EventHandler::passMouseDownEventToWidget):
            (WebCore::EventHandler::eventActivatedView):
            (WebCore::EventHandler::passSubframeEventToSubframe):
            (WebCore::EventHandler::passWheelEventToWidget):
            (WebCore::EventHandler::createDraggingClipboard):
            (WebCore::EventHandler::passMousePressEventToSubframe):
            (WebCore::EventHandler::passMouseMoveEventToSubframe):
            (WebCore::EventHandler::passMouseReleaseEventToSubframe):
            (WebCore::EventHandler::accessKeyModifiers):
            * page/haiku/FrameHaiku.cpp: Added.
            (WebCore::Frame::dragImageForSelection):
    
    Canonical link: https://commits.webkit.org/37635@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@46011 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/page/haiku/EventHandlerHaiku.cpp
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2006 Zack Rusin <zack@kde.org>
+ * Copyright (C) 2007 Ryan Leavengood <leavengood@gmail.com>
+ * Copyright (C) 2009 Maxime Simon <simon.maxime@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+#include "config.h"
+#include "EventHandler.h"
+
+#include "ClipboardHaiku.h"
+#include "EventNames.h"
+#include "FocusController.h"
+#include "Frame.h"
+#include "FrameView.h"
+#include "HitTestResult.h"
+#include "KeyboardEvent.h"
+#include "MouseEventWithHitTestResults.h"
+#include "Page.h"
+#include "PlatformKeyboardEvent.h"
+#include "PlatformScrollBar.h"
+#include "PlatformWheelEvent.h"
+#include "RenderWidget.h"
+
+#include "NotImplemented.h"
+
+#include <interface/View.h>
+
+
+namespace WebCore {
+
+const double EventHandler::TextDragDelay = 0.0;
+
+static bool isKeyboardOptionTab(KeyboardEvent* event)
+{
+    return event
+        && (event->type() == eventNames().keydownEvent
+            || event->type() == eventNames().keypressEvent)
+        && event->altKey()
+        && event->keyIdentifier() == "U+000009";
+}
+
+bool EventHandler::invertSenseOfTabsToLinks(KeyboardEvent* event) const
+{
+    return isKeyboardOptionTab(event);
+}
+
+bool EventHandler::tabsToAllControls(KeyboardEvent* event) const
+{
+    bool handlingOptionTab = isKeyboardOptionTab(event);
+
+    return handlingOptionTab;
+}
+
+void EventHandler::focusDocumentView()
+{
+    BView* view = m_frame->view()->platformWidget();
+    if (view)
+        view->MakeFocus();
+
+    Page* page = m_frame->page();
+    if (page)
+        page->focusController()->setFocusedFrame(m_frame);
+}
+
+bool EventHandler::passWidgetMouseDownEventToWidget(const MouseEventWithHitTestResults& event)
+{
+    // Figure out which view to send the event to.
+    RenderObject* target = event.targetNode() ? event.targetNode()->renderer() : 0;
+    if (!target || !target->isWidget())
+        return false;
+
+    return passMouseDownEventToWidget(static_cast<RenderWidget*>(target)->widget());
+}
+
+bool EventHandler::passWidgetMouseDownEventToWidget(RenderWidget* renderWidget)
+{
+    return passMouseDownEventToWidget(renderWidget->widget());
+}
+
+bool EventHandler::passMouseDownEventToWidget(Widget* widget)
+{
+    notImplemented();
+    return false;
+}
+
+bool EventHandler::eventActivatedView(const PlatformMouseEvent&) const
+{
+    notImplemented();
+    return false;
+}
+
+bool EventHandler::passSubframeEventToSubframe(MouseEventWithHitTestResults& event, Frame* subframe, HitTestResult*)
+{
+    notImplemented();
+    return true;
+}
+
+bool EventHandler::passWheelEventToWidget(PlatformWheelEvent& event, Widget* widget)
+{
+    notImplemented();
+    return false;
+}
+
+PassRefPtr<Clipboard> EventHandler::createDraggingClipboard() const
+{
+    return new ClipboardHaiku(ClipboardWritable, true);
+}
+
+bool EventHandler::passMousePressEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe)
+{
+    return passSubframeEventToSubframe(mev, subframe);
+}
+
+bool EventHandler::passMouseMoveEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe, HitTestResult* hoveredNode)
+{
+    return passSubframeEventToSubframe(mev, subframe, hoveredNode);
+}
+
+bool EventHandler::passMouseReleaseEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe)
+{
+    return passSubframeEventToSubframe(mev, subframe);
+}
+
+unsigned EventHandler::accessKeyModifiers()
+{
+    return PlatformKeyboardEvent::AltKey;
+}
+
+} // namespace WebCore
+
