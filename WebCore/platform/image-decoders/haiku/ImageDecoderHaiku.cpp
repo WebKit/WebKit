8cb06c635f4466ee4bdccf231820d6c589d75181
Adam Barth <abarth@webkit.org>
2009-07-17 05:47:24 +0000

    2009-07-16  Maxime Simon  <simon.maxime@gmail.com>
            Reviewed by Oliver Hunt.
    
            Added Haiku-specific files for WebCore/platform/image-decoders/.
            https://bugs.webkit.org/show_bug.cgi?id=26949
    
            Adding a new file, ImageDecoderHaiku.cpp.
    
            * platform/image-decoders/haiku/ImageDecoderHaiku.cpp: Added.
            (WebCore::RGBA32Buffer::RGBA32Buffer):
            (WebCore::RGBA32Buffer::clear):
            (WebCore::RGBA32Buffer::zeroFill):
            (WebCore::RGBA32Buffer::copyBitmapData):
            (WebCore::RGBA32Buffer::setSize):
            (WebCore::RGBA32Buffer::asNewNativeImage):
            (WebCore::RGBA32Buffer::hasAlpha):
            (WebCore::RGBA32Buffer::setHasAlpha):
            (WebCore::RGBA32Buffer::setStatus):
            (WebCore::RGBA32Buffer::operator=):
            (WebCore::RGBA32Buffer::width):
            (WebCore::RGBA32Buffer::height):
    
    Canonical link: https://commits.webkit.org/37636@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@46012 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/platform/image-decoders/haiku/ImageDecoderHaiku.cpp
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+#include "config.h"
+#include "ImageDecoder.h"
+
+#include <Bitmap.h>
+
+
+namespace WebCore {
+
+RGBA32Buffer::RGBA32Buffer()
+    : m_hasAlpha(false)
+    , m_status(FrameEmpty)
+    , m_duration(0)
+    , m_disposalMethod(DisposeNotSpecified)
+{
+} 
+
+void RGBA32Buffer::clear()
+{
+    m_bytes.clear();
+    m_status = FrameEmpty;
+    // NOTE: Do not reset other members here; clearFrameBufferCache()
+    // calls this to free the bitmap data, but other functions like
+    // initFrameBuffer() and frameComplete() may still need to read
+    // other metadata out of this frame later.
+}
+
+void RGBA32Buffer::zeroFill()
+{
+    m_bytes.fill(0);
+    m_hasAlpha = true;
+}
+
+void RGBA32Buffer::copyBitmapData(const RGBA32Buffer& other)
+{
+    if (this == &other)
+        return;
+
+    m_bytes = other.m_bytes;
+    setHasAlpha(other.m_hasAlpha);
+}
+
+bool RGBA32Buffer::setSize(int newWidth, int newHeight)
+{
+    // NOTE: This has no way to check for allocation failure if the
+    // requested size was too big...
+    m_bytes.resize(newWidth * newHeight);
+    m_size = IntSize(newWidth, newHeight);
+
+    // Zero the image.
+    zeroFill();
+
+    return true;
+}
+
+NativeImagePtr RGBA32Buffer::asNewNativeImage() const
+{
+    const void* bytes = m_bytes.data();
+
+    BBitmap* bmp = new BBitmap(BRect(0, 0, width(), height()), B_RGB32);
+    bmp->SetBits(bytes, m_size.width() * m_size.height(), 0, B_RGB32);
+
+    return bmp;
+}
+
+bool RGBA32Buffer::hasAlpha() const
+{
+    return m_hasAlpha;
+}
+
+void RGBA32Buffer::setHasAlpha(bool alpha)
+{
+    m_hasAlpha = alpha;
+}
+
+void RGBA32Buffer::setStatus(FrameStatus status)
+{
+    m_status = status;
+}
+
+RGBA32Buffer& RGBA32Buffer::operator=(const RGBA32Buffer& other)
+{
+    if (this == &other)
+        return *this;
+
+    m_bytes = other.m_bytes;
+    m_size = other.m_size;
+    setHasAlpha(other.hasAlpha());
+    setRect(other.rect());
+    setStatus(other.status());
+    setDuration(other.duration());
+    setDisposalMethod(other.disposalMethod());
+    return *this;
+}
+
+int RGBA32Buffer::width() const
+{
+    return m_size.width();
+}
+
+int RGBA32Buffer::height() const
+{
+    return m_size.height();
+}
+
+} // namespace WebCore
+
