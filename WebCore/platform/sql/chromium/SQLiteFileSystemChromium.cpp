92c028ee8e4886f1f1f06e5528563774def6ee0d
Eric Roman <eroman@chromium.org>
2009-07-20 17:56:55 +0000

    2009-07-20  Dumitru Daniliuc  <dumi@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Adding the Win SQLite VFS implementation for Chromium, and stubs
            for the Mac and Linux VFSs.
    
            https://bugs.webkit.org/show_bug.cgi?id=26940
    
            * WebCore.gypi:
            * platform/chromium/ChromiumBridge.h:
            * platform/sql/chromium/SQLiteFileSystemChromium.cpp: Added.
            * platform/sql/chromium/SQLiteFileSystemChromiumLinux.cpp: Added.
            * platform/sql/chromium/SQLiteFileSystemChromiumMac.cpp: Added.
            * platform/sql/chromium/SQLiteFileSystemChromiumWin.cpp: Added.
    
    
    
    Canonical link: https://commits.webkit.org/37747@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@46126 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/platform/sql/chromium/SQLiteFileSystemChromium.cpp
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2009 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "SQLiteFileSystem.h"
+
+#include "ChromiumBridge.h"
+#include "CString.h"
+#include "SQLiteDatabase.h"
+#include <sqlite3.h>
+
+#ifndef SQLITE_OPEN_FULLMUTEX
+#define SQLITE_OPEN_FULLMUTEX 0x00010000
+#endif
+
+// SQLiteFileSystem::registerSQLiteVFS() is implemented in the
+// platform-specific files SQLiteFileSystemChromium{Win|Posix}.cpp
+namespace WebCore {
+
+SQLiteFileSystem::SQLiteFileSystem()
+{
+}
+
+int SQLiteFileSystem::openDatabase(const String& fileName, sqlite3** database)
+{
+    // open databases using Chromium's VFS
+    return sqlite3_open_v2(fileName.utf8().data(), database,
+                           SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_FULLMUTEX,
+                           "chromium_vfs");
+}
+
+String SQLiteFileSystem::getFileNameForNewDatabase(
+  const String&, const String& dbName, const String &originIdentifier, SQLiteDatabase*)
+{
+    // Chromium names DB files based on origin and DB name only
+    return originIdentifier + "_" + dbName + ".db";
+}
+
+String SQLiteFileSystem::appendDatabaseFileNameToPath(const String&, const String& fileName)
+{
+    // Chromium saves all DB files in the same directory (known by
+    // the browser process only); as far as the renderer processes
+    // are concerned, all DB files are saved in the "current" directory
+    return fileName;
+}
+
+bool SQLiteFileSystem::ensureDatabaseDirectoryExists(const String&)
+{
+    // if the directory where Chromium stores the databases does not exist,
+    // it will be automatically created by the browser process;
+    // so as far as the WebKit code is concerned, this directory always exists
+    return true;
+}
+
+bool SQLiteFileSystem::ensureDatabaseFileExists(const String&, bool)
+{
+    // all database directories will be created as needed by the browser process
+    return true;
+}
+
+bool SQLiteFileSystem::deleteEmptyDatabaseDirectory(const String&)
+{
+    // Chromium does not use a separate directory for each database,
+    // so there's nothing to do here
+    return true;
+}
+
+bool SQLiteFileSystem::deleteDatabaseFile(const String& fileName)
+{
+    // return true if and only if the error code returned by
+    // ChromiumBridge::deleteDatabase() is 0
+    return (!ChromiumBridge::databaseDeleteFile(fileName));
+}
+
+long long SQLiteFileSystem::getDatabaseFileSize(const String& fileName)
+{
+    return ChromiumBridge::databaseGetFileSize(fileName);
+}
+
+} // namespace WebCore
