5a4dab68fc6a9b552bbf0eb6d003182315e4b2cd
Adam Treat <treat@webkit.org>
2009-07-21 22:42:21 +0000

    2009-07-21  Yong Li  <yong.li@torchmobile.com>
            Reviewed by George Staikos.
    
            https://bugs.webkit.org/show_bug.cgi?id=27509
            Add font-related files for the WinCE port.
    
            Written by Yong Li <yong.li@torchmobile.com>
    
            * platform/graphics/wince/FontCacheWince.cpp: Added.
            * platform/graphics/wince/FontCustomPlatformData.cpp: Added.
            * platform/graphics/wince/FontCustomPlatformData.h: Added.
            * platform/graphics/wince/FontPlatformData.cpp: Added.
            * platform/graphics/wince/FontPlatformData.h: Added.
            * platform/graphics/wince/FontWince.cpp: Added.
            * platform/graphics/wince/GlyphPageTreeNodeWince.cpp: Added.
            * platform/graphics/wince/SimpleFontDataWince.cpp: Added.
    
    Canonical link: https://commits.webkit.org/37812@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@46196 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/platform/graphics/wince/FontCustomPlatformData.cpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2007, 2008, 2009 Apple Inc. All rights reserved.
+ * Copyright (C) 2009 Torch Mobile Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "config.h"
+#include "FontCustomPlatformData.h"
+
+#include "Base64.h"
+#include "CachedFont.h"
+#include "FontPlatformData.h"
+#include <wtf/RandomNumber.h>
+
+namespace WebCore {
+
+static CustomFontCache* g_customFontCache = 0;
+
+bool renameFont(SharedBuffer* fontData, const String& fontName);
+
+FontCustomPlatformData::~FontCustomPlatformData()
+{
+    if (g_customFontCache && !m_name.isEmpty())
+        g_customFontCache->unregisterFont(m_name);
+}
+
+FontPlatformData FontCustomPlatformData::fontPlatformData(int size, bool bold, bool italic, FontRenderingMode renderingMode)
+{
+    FontDescription fontDesc;
+    fontDesc.setComputedSize(size);
+    fontDesc.setSpecifiedSize(size);
+    fontDesc.setItalic(italic);
+    fontDesc.setWeight(bold ? FontWeightBold : FontWeightNormal);
+    return FontPlatformData(fontDesc, m_name, false);
+}
+
+// Creates a unique and unpredictable font name, in order to avoid collisions and to
+// not allow access from CSS.
+static String createUniqueFontName()
+{
+    Vector<char> fontUuid(sizeof(GUID));
+
+    unsigned int* ptr = reinterpret_cast<unsigned int*>(fontUuid.data());
+    for (int i = 0; i < sizeof(GUID) / sizeof(int) ; ++i)
+        *(ptr + i) = static_cast<unsigned int>(WTF::randomNumber() * (std::numeric_limits<unsigned>::max() + 1.0));
+
+    Vector<char> fontNameVector;
+    base64Encode(fontUuid, fontNameVector);
+    ASSERT(fontNameVector.size() < LF_FACESIZE);
+    String fontName(fontNameVector.data(), fontNameVector.size());
+    return fontName.replace('/', '_');
+}
+
+FontCustomPlatformData* createFontCustomPlatformData(CachedFont* cachedFont)
+{
+    if (g_customFontCache && cachedFont->CachedResource::data()) {
+        String fontName = createUniqueFontName();
+        if (renameFont(cachedFont->CachedResource::data(), fontName) && g_customFontCache->registerFont(fontName, cachedFont))
+            return new FontCustomPlatformData(fontName);
+    }
+    return 0;
+}
+
+}
---
+++ WebCore/platform/graphics/wince/SimpleFontDataWince.cpp
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2006, 2007 Apple Inc.  All rights reserved.
+ * Copyright (C) 2007-2009 Torch Mobile, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer. 
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution. 
+ * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include "config.h"
+#include "SimpleFontData.h"
+
+#include "FloatRect.h"
+#include "Font.h"
+#include "FontCache.h"
+#include "FontDescription.h"
+#include <wtf/MathExtras.h>
+#include <mlang.h>
+#include <tchar.h>
+
+namespace WebCore {
+
+extern HDC g_screenDC;
+
+void SimpleFontData::platformInit()
+{
+    if (!m_platformData.isValid())
+        return;
+
+    const TEXTMETRIC& tm = m_platformData.metrics();
+    m_isSystemFont = m_platformData.isSystemFont();
+
+    m_ascent = (tm.tmAscent * m_platformData.size() + 36) / 72;
+    m_descent = (tm.tmDescent * m_platformData.size() + 36) / 72;
+    m_lineGap = (tm.tmExternalLeading * m_platformData.size() + 36) / 72;
+    m_lineSpacing = m_ascent + m_descent + m_lineGap;
+    m_xHeight = m_ascent * 0.56f;
+}
+
+void SimpleFontData::platformDestroy()
+{
+    delete m_smallCapsFontData;
+    m_smallCapsFontData = 0;
+}
+
+SimpleFontData* SimpleFontData::smallCapsFontData(const FontDescription& fontDescription) const
+{
+    if (!m_smallCapsFontData) {
+        FontDescription fontDesc(fontDescription);
+        fontDesc.setComputedSize(lroundf(0.70f * fontDesc.computedSize()));
+        fontDesc.setSpecifiedSize(lroundf(0.70f * fontDesc.specifiedSize()));
+        fontDesc.setKeywordSize(lroundf(0.70f * fontDesc.keywordSize()));
+        FontPlatformData* result = fontCache()->getCachedFontPlatformData(fontDesc, m_platformData.family());
+        if (result)
+            m_smallCapsFontData = new SimpleFontData(*result);
+    }
+    return m_smallCapsFontData;
+}
+
+DWORD getKnownFontCodePages(const wchar_t* family);
+
+bool SimpleFontData::containsCharacters(const UChar* characters, int length) const
+{
+    if (m_platformData.isDisabled())
+        return true;
+
+    // FIXME: Microsoft documentation seems to imply that characters can be output using a given font and DC
+    // merely by testing code page intersection.  This seems suspect though.  Can't a font only partially
+    // cover a given code page?
+
+    // FIXME: in the case that we failed to get the interface, still use the font.
+#if defined(IMLANG_FONT_LINK) && (IMLANG_FONT_LINK == 2)
+    IMLangFontLink2* langFontLink = fontCache()->getFontLinkInterface();
+#else
+    IMLangFontLink* langFontLink = fontCache()->getFontLinkInterface();
+#endif
+    if (!langFontLink)
+        return true;
+
+    DWORD fontCodePages = m_platformData.codePages();
+    if (!fontCodePages)
+        return false;
+
+    DWORD acpCodePages = 0;
+    langFontLink->CodePageToCodePages(CP_ACP, &acpCodePages);
+
+    DWORD actualCodePages;
+    long numCharactersProcessed;
+    while (length) {
+        langFontLink->GetStrCodePages(characters, length, acpCodePages, &actualCodePages, &numCharactersProcessed);
+        if (actualCodePages && !(actualCodePages & fontCodePages))
+            return false;
+
+        length -= numCharactersProcessed;
+        characters += numCharactersProcessed;
+    }
+
+    return true;
+}
+
+void SimpleFontData::determinePitch()
+{
+    if (!m_platformData.isValid())
+        return;
+
+    const TEXTMETRIC& tm = m_platformData.metrics();
+
+    // Yes, this looks backwards, but the fixed pitch bit is actually set if the font
+    // is *not* fixed pitch.  Unbelievable but true.
+    m_treatAsFixedPitch = !(tm.tmPitchAndFamily & TMPF_FIXED_PITCH);
+}
+
+float SimpleFontData::platformWidthForGlyph(Glyph glyph) const
+{
+    if (m_platformData.isDisabled())
+        return 0;
+
+    HGDIOBJ hOldFont = SelectObject(g_screenDC, m_platformData.hfont());
+
+    SIZE fontSize;
+    wchar_t c = glyph;
+    GetTextExtentPoint32(g_screenDC, &c, 1, &fontSize);
+
+    SelectObject(g_screenDC, hOldFont);
+
+    return (float)fontSize.cx * (float)m_platformData.size() / 72.f;
+}
+
+
+void SimpleFontData::platformCharWidthInit()
+{
+    if (!m_platformData.isValid())
+        return;
+
+    const TEXTMETRIC& tm = m_platformData.metrics();
+    m_avgCharWidth = (tm.tmAveCharWidth * m_platformData.size() + 36) / 72;
+    m_maxCharWidth = (tm.tmMaxCharWidth * m_platformData.size() + 36) / 72;
+}
+
+}
