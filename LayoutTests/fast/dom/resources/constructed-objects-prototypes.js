2ad2d2dd5120ae2b04d2bdf6ce164b1ebb51c12e
Eric Seidel <eric@webkit.org>
2009-07-24 07:42:09 +0000

    2009-07-24  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            x-frame constructor calls are instantiated with the wrong prototype chain
            https://bugs.webkit.org/show_bug.cgi?id=27566
    
            Add the test.  Soon to be fixed by bug 27634.
    
            * fast/dom/constructed-objects-prototypes-expected.txt: Added.
            * fast/dom/constructed-objects-prototypes.html: Added.
            * fast/dom/resources/constructed-objects-prototypes.js: Added.
            (classNameForObject):
            (constructorPropertiesOnWindow):
    
    Canonical link: https://commits.webkit.org/37936@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@46326 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/dom/resources/constructed-objects-prototypes.js
@@ -0,0 +1,54 @@
+description("Make sure prototypes are set up using the window a property came from, instead of the lexical global object.")
+
+var subframe = document.createElement("iframe");
+document.body.appendChild(subframe);
+var inner = subframe.contentWindow; // Call it "inner" to make shouldBe output shorter
+
+// Stash a property on the prototypes.
+window.Object.prototype.isInner = false;
+inner.Object.prototype.isInner = true;
+
+function classNameForObject(object)
+{
+    // call will use the global object if passed null or undefined, so special case those:
+    if (object == null)
+        return null;
+    var result = Object.prototype.toString.call(object);
+    // remove '[object ' and ']'
+    return result.split(' ')[1].split(']')[0];
+}
+
+function constructorPropertiesOnWindow(globalObject)
+{
+    var constructorNames = [];
+    for (var property in globalObject) {
+        var value = inner[property];
+        if (value == null)
+            continue;
+        var type = classNameForObject(value);
+        if (!type.match('Constructor$'))
+            continue;
+        constructorNames.push(property);
+    }
+    return constructorNames.sort();
+}
+
+var constructorNames = constructorPropertiesOnWindow(inner);
+
+for (var x = 0; x < constructorNames.length; x++) {
+    var constructorName = constructorNames[x];
+    // Test first to see if the object is constructable
+    var constructedObject;
+    try {
+        constructedObject = eval("new inner." + constructorName);
+    } catch(e) {
+        continue;
+    }
+
+    shouldBeTrue("(new inner." + constructorName + ").isInner");
+    shouldBeTrue("(new inner." + constructorName + ").constructor.isInner");
+}
+
+document.body.removeChild(subframe);
+
+var successfullyParsed = true;
