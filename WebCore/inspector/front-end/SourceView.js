861a343e164213d867830d685df45b9c06a8101c
Adam Barth <abarth@webkit.org>
2009-07-24 07:51:39 +0000

    2009-07-24  Joseph Pecoraro  <joepeck02@gmail.com>
            Reviewed by Timothy Hatcher.
    
            Inspector: Should Syntax Highlight JSON
            https://bugs.webkit.org/show_bug.cgi?id=27503
    
            * inspector/front-end/SourceView.js:
            (WebInspector.SourceView.prototype._contentLoaded):
    
    Canonical link: https://commits.webkit.org/37941@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@46331 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/inspector/front-end/SourceView.js
+++ WebCore/inspector/front-end/SourceView.js
@@ -104,8 +104,11 @@ WebInspector.SourceView.prototype = {
     {
         delete this._frameNeedsSetup;
         this.sourceFrame.removeEventListener("content loaded", this._contentLoaded, this);
-
-        if (this.resource.type === WebInspector.Resource.Type.Script) {
+        
+        if (this.resource.type === WebInspector.Resource.Type.Script
+            || this.resource.mimeType === 'application/json'
+            || this.resource.mimeType === 'application/javascript'
+            || /\.js(on)?$/.test(this.resource.lastPathComponent) ) {
             this.sourceFrame.addEventListener("syntax highlighting complete", this._syntaxHighlightingComplete, this);
             this.sourceFrame.syntaxHighlightJavascript();
         } else
