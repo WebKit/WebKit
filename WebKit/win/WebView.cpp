71f8f3d8f6b386d3eea17999a26466649d9fa2bd
Brian Weinstein <bweinstein@apple.com>
2009-07-27 23:21:53 +0000

    Build fix for Windows for JSC recent breakage.
    Canonical link: https://commits.webkit.org/38051@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@46441 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/win/WebView.cpp
+++ WebKit/win/WebView.cpp
@@ -2820,7 +2820,7 @@ HRESULT STDMETHODCALLTYPE WebView::stringByEvaluatingJavaScriptFromString(
     if (!scriptExecutionResult)
         return E_FAIL;
     else if (scriptExecutionResult.isString()) {
-        JSLock lock(SilenceAssertionsOnly);
+        JSLock lock(JSC::SilenceAssertionsOnly);
         *result = BString(String(scriptExecutionResult.getString()));
     }
 
