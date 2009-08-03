41a659e45630ab4ae31a2da9dc52383dabca79ff
Sam Weinig <weinig@apple.com>
2009-08-03 20:00:47 +0000

    WebCore:
    2009-08-03  Dirk Pranke  <dpranke@chromium.org>
    
            Reviewed by Sam Weinig.
    
            Fix for https://bugs.webkit.org/show_bug.cgi?id=27748
            crash w/ stack overflow when same CSS file loaded repeatedly
    
            Fix crash caused by a stack overflow when repeatedly loading very large
            CSS files. CSSRuleData in CSSStyleSelector.h destroyed itself
            recursively rather than iteratively.
    
            Test: fast/css/large-list-of-rules-crash.html
    
            * css/CSSStyleSelector.h:
            (WebCore::CSSRuleData::~CSSRuleData):
            (WebCore::CSSRuleDataList::~CSSRuleDataList):
    
    LayoutTests:
    
    2009-08-03  Dirk Pranke  <dpranke@chromium.org>
    
            Reviewed by Sam Weinig.
    
            Test for https://bugs.webkit.org/show_bug.cgi?id=27748
            crash w/ stack overflow when same CSS file loaded repeatedly
    
            Fix crash caused by a stack overflow when repeatedly loading very large
            CSS files. CSSRuleData in CSSStyleSelector.h destroyed itself
            recursively rather than iteratively.
    
            * fast/css/large-list-of-rules-crash-expected.txt: Added.
            * fast/css/large-list-of-rules-crash.html: Added.
            * fast/css/resources/large-list-of-rules-crash.css: Added.
    
    
    
    Canonical link: https://commits.webkit.org/38334@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@46728 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/css/CSSStyleSelector.h
+++ WebCore/css/CSSStyleSelector.h
@@ -296,7 +296,9 @@ public:
                 prev->m_next = this;
         }
 
-        ~CSSRuleData() { delete m_next; }
+        ~CSSRuleData() 
+        { 
+        }
 
         unsigned position() { return m_position; }
         CSSStyleRule* rule() { return m_rule; }
@@ -318,7 +320,17 @@ public:
         {
         }
 
-        ~CSSRuleDataList() { delete m_first; }
+        ~CSSRuleDataList() 
+        { 
+            CSSRuleData* ptr;
+            CSSRuleData* next;
+            ptr = m_first;
+            while (ptr) {
+                next = ptr->next();
+                delete ptr;
+                ptr = next;
+            }
+        }
 
         CSSRuleData* first() { return m_first; }
         CSSRuleData* last() { return m_last; }
