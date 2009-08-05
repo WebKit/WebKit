f4a590b9139a6818d668b8e7224f36e44ca5267d
Eric Seidel <eric@webkit.org>
2009-08-05 04:36:06 +0000

    2009-08-04  Eric Seidel  <eric@webkit.org>
            Reviewed by Simon Fraser.
    
            REGRESSION (r45064): Drop caret remains in text control after the mouse exits
            https://bugs.webkit.org/show_bug.cgi?id=26787
    
            Fix some bogus comments noticed during testing.
    
            * fast/forms/drag-into-textarea.html:
            * fast/forms/drag-out-of-textarea.html:
    
    2009-08-04  Eric Seidel  <eric@webkit.org>
    
            Reviewed by Simon Fraser.
    
            REGRESSION (r45064): Drop caret remains in text control after the mouse exits
            https://bugs.webkit.org/show_bug.cgi?id=26787
    
            Add back missing dragController()->clear() call.
    
            * manual-tests/drag-caret.html: Added.
            * page/DragController.cpp:
            (WebCore::DragController::tryDocumentDrag):
    
    Canonical link: https://commits.webkit.org/38398@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@46792 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/page/DragController.cpp
+++ WebCore/page/DragController.cpp
@@ -281,9 +281,7 @@ bool DragController::tryDocumentDrag(DragData* dragData, DragDestinationAction a
     if (m_isHandlingDrag) {
         m_page->dragCaretController()->clear();
         return true;
-    }
-
-    if ((actionMask & DragDestinationActionEdit) && !m_isHandlingDrag && canProcessDrag(dragData)) {
+    } else if ((actionMask & DragDestinationActionEdit) && canProcessDrag(dragData)) {
         if (dragData->containsColor()) {
             operation = DragOperationGeneric;
             return true;
@@ -302,6 +300,8 @@ bool DragController::tryDocumentDrag(DragData* dragData, DragDestinationAction a
         operation = dragIsMove(innerFrame->selection()) ? DragOperationMove : DragOperationCopy;
         return true;
     }
+    // If we're not over an editable region, make sure we're clearing any prior drag cursor.
+    m_page->dragCaretController()->clear();
     return false;
 }
 
