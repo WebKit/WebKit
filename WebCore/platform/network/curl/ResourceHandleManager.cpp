a4c3968da48fd402cc3a24fe603bef84d71b8c77
Brent Fulgham <bfulgham@webkit.org>
2009-08-10 22:37:10 +0000

    Unreviewed build correction after http://trac.webkit.org/changeset/46978
    * platform/network/curl/ResourceHandleManager.cpp:
    (WebCore::ResourceHandleManager::initializeHandle):  Correct
     deprecated removeRef to new removeFragmentIdentifier.
    
    
    
    Canonical link: https://commits.webkit.org/38612@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@47009 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/network/curl/ResourceHandleManager.cpp
+++ WebCore/platform/network/curl/ResourceHandleManager.cpp
@@ -582,7 +582,7 @@ void ResourceHandleManager::initializeHandle(ResourceHandle* job)
     KURL kurl = job->request().url();
 
     // Remove any fragment part, otherwise curl will send it as part of the request.
-    kurl.removeRef();
+    kurl.removeFragmentIdentifier();
 
     ResourceHandleInternal* d = job->getInternal();
     String url = kurl.string();
