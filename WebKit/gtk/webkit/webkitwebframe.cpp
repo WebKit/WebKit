4c43000cf6fd14529c523d978a1876cdffed79da
Xan Lopez <xan@webkit.org>
2009-08-11 18:29:20 +0000

    2009-08-11  Benjamin Otte  <otte@gnome.org>
            Reviewed by Xan Lopez.
    
            When instructed to print an out-of-range page, print an empty page
            instead of crashing.
    
            * webkit/webkitwebframe.cpp:
            (draw_page_callback):
    
    Canonical link: https://commits.webkit.org/38644@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@47041 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/gtk/webkit/webkitwebframe.cpp
+++ WebKit/gtk/webkit/webkitwebframe.cpp
@@ -696,6 +696,9 @@ static void draw_page_callback(GtkPrintOperation* op, GtkPrintContext* context, 
 {
     PrintContext* printContext = reinterpret_cast<PrintContext*>(user_data);
 
+    if (page_nr >= printContext->pageCount())
+        return;
+
     cairo_t* cr = gtk_print_context_get_cairo_context(context);
     GraphicsContext ctx(cr);
     float width = gtk_print_context_get_width(context);
