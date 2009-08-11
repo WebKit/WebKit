688a7602d20362fcd0bf82f42fdd9065e89ff290
Dmitry Titov <dimich@chromium.org>
2009-08-11 19:09:12 +0000

    Fix for layout tests failures. Need to initialize some preferences early because WebView on Windows uses them during create time.
    Reviewed by NOBODY (Windows layout tests fix).
    
    * DumpRenderTree/win/DumpRenderTree.cpp:
    (main):
    
    Canonical link: https://commits.webkit.org/38646@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@47043 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/DumpRenderTree/win/DumpRenderTree.cpp
+++ WebKitTools/DumpRenderTree/win/DumpRenderTree.cpp
@@ -1121,6 +1121,20 @@ int main(int argc, char* argv[])
     sharedEditingDelegate.adoptRef(new EditingDelegate);
     sharedResourceLoadDelegate.adoptRef(new ResourceLoadDelegate);
 
+    // FIXME - need to make DRT pass with Windows native controls <http://bugs.webkit.org/show_bug.cgi?id=25592>
+    COMPtr<IWebPreferences> tmpPreferences;
+    if (FAILED(WebKitCreateInstance(CLSID_WebPreferences, 0, IID_IWebPreferences, reinterpret_cast<void**>(&tmpPreferences))))
+        return -1;
+    COMPtr<IWebPreferences> standardPreferences;
+    if (FAILED(tmpPreferences->standardPreferences(&standardPreferences)))
+        return -1;
+    COMPtr<IWebPreferencesPrivate> standardPreferencesPrivate;
+    if (FAILED(standardPreferences->QueryInterface(&standardPreferencesPrivate)))
+        return -1;
+    standardPreferencesPrivate->setShouldPaintNativeControls(FALSE);
+    standardPreferences->setJavaScriptEnabled(TRUE);
+    standardPreferences->setDefaultFontSize(16);
+
     COMPtr<IWebView> webView(AdoptCOM, createWebViewAndOffscreenWindow(&webViewWindow));
     if (!webView)
         return -1;
