fa57b312ba032c2cb8ef75dab7d579bc4d9740e0
Eric Seidel <eric@webkit.org>
2009-08-11 18:27:47 +0000

    2009-08-11  Eric Seidel  <eric@webkit.org>
            Reviewed by Darin Adler.
    
            Exception in land-patches
            https://bugs.webkit.org/show_bug.cgi?id=27962
    
            Use ("%s" % object) instead of ("" + object).
            Added unit tests for logging.py.
    
            * Scripts/modules/logging.py:
            * Scripts/modules/logging_unittest.py: Added.
            * Scripts/run-webkit-unittests:
    
    Canonical link: https://commits.webkit.org/38643@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@47040 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/modules/logging_unittest.py
+++ WebKitTools/Scripts/modules/logging_unittest.py
@@ -1,20 +1,19 @@
-# Copyright (c) 2009, Google Inc. All rights reserved.
-# Copyright (c) 2009 Apple Inc. All rights reserved.
-# 
+# Copyright (C) 2009 Google Inc. All rights reserved.
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-# 
-#     * Redistributions of source code must retain the above copyright
+#
+#    * Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above
+#    * Redistributions in binary form must reproduce the above
 # copyright notice, this list of conditions and the following disclaimer
 # in the documentation and/or other materials provided with the
 # distribution.
-#     * Neither the name of Google Inc. nor the names of its
+#    * Neither the name of Google Inc. nor the names of its
 # contributors may be used to endorse or promote products derived from
 # this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -26,14 +25,37 @@
 # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# WebKit's Python module for logging
 
-import sys
+import os
+import subprocess
+import StringIO
+import tempfile
+import unittest
 
-def log(string):
-    print >> sys.stderr, string
+from modules.logging import *
+from modules.scm import ScriptError
 
-def error(string):
-    log("ERROR: %s" % string)
-    exit(1)
+class LoggingTest(unittest.TestCase):
+
+    def assert_log_equals(self, log_input, expected_output):
+        original_stderr = sys.stderr
+        test_stderr = StringIO.StringIO()
+        sys.stderr = test_stderr
+
+        try:
+            log(log_input)
+            actual_output = test_stderr.getvalue()
+        finally:
+            original_stderr = original_stderr
+
+        self.assertEquals(actual_output, expected_output, "log(\"%s\") expected: %s actual: %s" % (log_input, expected_output, actual_output))
+
+    def test_log(self):
+        self.assert_log_equals("test", "test\n")
+
+        # Test that log() does not throw an exception when passed an object instead of a string.
+        self.assert_log_equals(ScriptError("ScriptError"), "ScriptError\n")
+
+
+if __name__ == '__main__':
+    unittest.main()
