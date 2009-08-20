0e93713fbc1fd4585b4de15335ebb404c680770a
Aaron Boodman <aa@chromium.org>
2009-08-20 08:22:47 +0000

    One more speculative build for gtk.
    Patch by Aaron Boodman <aa@chromium.org> on 2009-08-20
    * DumpRenderTree/gtk/LayoutTestControllerGtk.cpp:
    
    Canonical link: https://commits.webkit.org/39145@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@47559 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/DumpRenderTree/gtk/LayoutTestControllerGtk.cpp
+++ WebKitTools/DumpRenderTree/gtk/LayoutTestControllerGtk.cpp
@@ -49,7 +49,7 @@ bool webkit_web_frame_pause_transition(WebKitWebFrame* frame, const gchar* name,
 unsigned int webkit_web_frame_number_of_active_animations(WebKitWebFrame* frame);
 void webkit_application_cache_set_maximum_size(unsigned long long size);
 unsigned int webkit_worker_thread_count(void);
-void webkit_white_list_access_to_origin(const gchar* sourceOrigin, const gchar* destinationProtocol, const gchar* destinationHost, bool allowDestinationSubdomains);
+void webkit_white_list_access_from_origin(const gchar* sourceOrigin, const gchar* destinationProtocol, const gchar* destinationHost, bool allowDestinationSubdomains);
 }
 
 LayoutTestController::~LayoutTestController()
