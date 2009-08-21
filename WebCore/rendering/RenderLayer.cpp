38b6af7d7d2453298480b0e11bba72ff980d338e
Brian Weinstein <bweinstein@apple.com>
2009-08-21 00:27:15 +0000

    Mac Build Fix.
    Canonical link: https://commits.webkit.org/39197@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@47611 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderLayer.cpp
+++ WebCore/rendering/RenderLayer.cpp
@@ -975,9 +975,9 @@ static inline int adjustedScrollDelta(int beginningDelta) {
 
     int adjustedDelta = beginningDelta / speedReducer;
     if (adjustedDelta > 1)
-        adjustedDelta = adjustedDelta * sqrt(static_cast<double>(adjustedDelta)) - 1;
+        adjustedDelta = static_cast<int>(adjustedDelta * sqrt(static_cast<double>(adjustedDelta))) - 1;
     else if (adjustedDelta < -1)
-        adjustedDelta = adjustedDelta * sqrt(static_cast<double>(-adjustedDelta)) + 1;
+        adjustedDelta = static_cast<int>(adjustedDelta * sqrt(static_cast<double>(-adjustedDelta))) + 1;
 
     return adjustedDelta;
 }
