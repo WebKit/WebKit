b10810ffc275ec4b15481f1a9a37e0e729c30a98
Eric Seidel <eric@webkit.org>
2009-08-21 19:16:21 +0000

    2009-08-21  Maxime Simon  <simon.maxime@gmail.com>
            Reviewed by Oliver Hunt.
    
            [Haiku] Adding three font-specific files to WebCore:
            FontCacheHaiku.cpp, FontHaiku.cpp, and SimpleFontDataHaiku.cpp
            https://bugs.webkit.org/show_bug.cgi?id=28131
    
            * platform/graphics/haiku/FontCacheHaiku.cpp: Added.
            (WebCore::FontCache::platformInit):
            (WebCore::FontCache::getFontDataForCharacters):
            (WebCore::FontCache::getSimilarFontPlatformData):
            (WebCore::FontCache::getLastResortFallbackFont):
            (WebCore::FontCache::createFontPlatformData):
            (WebCore::FontCache::getTraitsInFamily):
            * platform/graphics/haiku/FontHaiku.cpp: Added.
            (charUnicodeToUTF8HACK):
            (WebCore::Font::canReturnFallbackFontsForComplexText):
            (WebCore::Font::drawGlyphs):
            (WebCore::Font::drawComplexText):
            (WebCore::Font::floatWidthForComplexText):
            (WebCore::Font::selectionRectForComplexText):
            (WebCore::Font::offsetForPositionForComplexText):
            * platform/graphics/haiku/SimpleFontDataHaiku.cpp: Added.
            (WebCore::SimpleFontData::platformInit):
            (WebCore::SimpleFontData::platformCharWidthInit):
            (WebCore::SimpleFontData::platformDestroy):
            (WebCore::SimpleFontData::smallCapsFontData):
            (WebCore::SimpleFontData::containsCharacters):
            (WebCore::SimpleFontData::determinePitch):
            (WebCore::SimpleFontData::platformWidthForGlyph):
    
    Canonical link: https://commits.webkit.org/39220@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@47637 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/platform/graphics/haiku/FontHaiku.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2007 Ryan Leavengood <leavengood@gmail.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "Font.h"
+
+#include "FontData.h"
+#include "FontDescription.h"
+#include "FontSelector.h"
+#include "GraphicsContext.h"
+#include "NotImplemented.h"
+#include <Font.h>
+#include <String.h>
+#include <View.h>
+
+
+// FIXME: Temp routine to convert unicode character to UTF8.
+int charUnicodeToUTF8HACK(unsigned short glyph, char* out)
+{
+    int i = 0;
+
+    if (glyph < 0x0080)
+        out[i++] = static_cast<char>(glyph);
+    else if (glyph < 0x0800) {  // 2 bytes
+        out[i++] = 0xc0 | (glyph >> 6);
+        out[i++] = 0x80 | (glyph & 0x3F);
+    } else if (glyph > 0x0800) {  // 3 bytes
+        out[i++] = 0xe0 | (glyph >> 12);
+        out[i++] = 0x80 | ((glyph >> 6) & 0x3F);
+        out[i++] = 0x80 | (glyph & 0x3F);
+    }
+
+    out[i] = '\0';
+    return i;
+}
+
+namespace WebCore {
+
+bool Font::canReturnFallbackFontsForComplexText()
+{
+    return false;
+}
+
+void Font::drawGlyphs(GraphicsContext* graphicsContext, const SimpleFontData* font,
+                      const GlyphBuffer& glyphBuffer, int from, int numGlyphs, const FloatPoint& point) const
+{
+    Color color = graphicsContext->fillColor();
+    BView* view = graphicsContext->platformContext();
+    BFont* m_font = font->platformData().font();
+
+    graphicsContext->setCompositeOperation(CompositeSourceOver);
+    view->SetHighColor(color);
+    view->SetFont(m_font);
+
+    GlyphBufferGlyph* glyphs = const_cast<GlyphBufferGlyph*>(glyphBuffer.glyphs(from));
+    float offset = point.x();
+    for (int i = 0; i < numGlyphs; i++) {
+        char out[4];
+        charUnicodeToUTF8HACK(glyphs[i], out);
+
+        view->DrawString(out, sizeof(out), BPoint(offset, point.y()));
+        offset += glyphBuffer.advanceAt(from + i);
+    }
+}
+
+void Font::drawComplexText(GraphicsContext* ctx, const TextRun& run, const FloatPoint& point,
+                           int from, int to) const
+{
+    notImplemented();
+}
+
+
+float Font::floatWidthForComplexText(const TextRun& run, HashSet<const SimpleFontData*>* fallbackFonts) const
+{
+    notImplemented();
+    return 0;
+}
+
+FloatRect Font::selectionRectForComplexText(const TextRun&, const IntPoint&, int, int, int) const
+{
+    notImplemented();
+    return FloatRect();
+}
+
+int Font::offsetForPositionForComplexText(const TextRun&, int, bool) const
+{
+    notImplemented();
+    return 0;
+}
+
+} // namespace WebCore
+
---
+++ WebCore/platform/graphics/haiku/SimpleFontDataHaiku.cpp
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2006 Apple Computer, Inc.  All rights reserved.
+ * Copyright (C) 2009 Maxime Simon <simon.maxime@gmail.com> All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "SimpleFontData.h"
+
+#include "FloatRect.h"
+#include "FontCache.h"
+#include "FontDescription.h"
+#include "NotImplemented.h"
+#include <Rect.h>
+#include <unicode/uchar.h>
+#include <unicode/unorm.h>
+
+
+namespace WebCore {
+
+void SimpleFontData::platformInit()
+{
+    BFont* font = m_platformData.font();
+    if (!font)
+        return;
+
+    font_height height;
+    font->GetHeight(&height);
+    m_ascent = static_cast<int>(height.ascent);
+    m_descent = static_cast<int>(height.descent);
+    m_lineSpacing = m_ascent + m_descent;
+    m_xHeight = height.ascent * 0.56f; // Hack taken from the win port.
+    m_lineGap = height.leading;
+}
+
+void SimpleFontData::platformCharWidthInit()
+{
+    m_avgCharWidth = 0.f;
+    m_maxCharWidth = 0.f;
+    initCharWidths();
+}
+
+void SimpleFontData::platformDestroy()
+{
+    delete m_smallCapsFontData;
+    m_smallCapsFontData = 0;
+}
+
+SimpleFontData* SimpleFontData::smallCapsFontData(const FontDescription& fontDescription) const
+{
+    if (!m_smallCapsFontData) {
+        FontDescription desc = FontDescription(fontDescription);
+        desc.setSpecifiedSize(0.70f * fontDescription.computedSize());
+        const FontPlatformData* fontPlatformData = new FontPlatformData(desc, desc.family().family());
+        m_smallCapsFontData = new SimpleFontData(*fontPlatformData);
+    }
+    return m_smallCapsFontData;
+}
+
+bool SimpleFontData::containsCharacters(const UChar* characters, int length) const
+{
+    // FIXME: We will need to implement this to load non-ASCII encoding sites
+    return true;
+}
+
+void SimpleFontData::determinePitch()
+{
+    m_treatAsFixedPitch = m_platformData.font() && m_platformData.font()->IsFixed();
+}
+
+float SimpleFontData::platformWidthForGlyph(Glyph glyph) const
+{
+    const char charArray[1] = { glyph };
+    float escapements[1];
+
+    if (m_platformData.font()) {
+        m_platformData.font()->GetEscapements(charArray, 1, escapements);
+        return escapements[0] * m_platformData.font()->Size();
+    }
+
+    return 0;
+}
+
+} // namespace WebCore
+
