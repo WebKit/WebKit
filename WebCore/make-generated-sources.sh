2fc1efb578469b7bf18685e188a7f05134ea7fc3
Darin Adler <darin@apple.com>
2009-08-22 06:53:16 +0000

    * WebCore.vcproj/build-generated-files.sh: * make-generated-sources.sh: Removed obsolete code to set up CREATE_HASH_TABLE.
    Canonical link: https://commits.webkit.org/39249@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@47666 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/make-generated-sources.sh
+++ WebCore/make-generated-sources.sh
@@ -2,7 +2,6 @@
 export SOURCE_ROOT=$PWD
 export SRCROOT=$PWD
 export WebCore=$PWD
-export CREATE_HASH_TABLE="$SRCROOT/../JavaScriptCore/create_hash_table"
 
 mkdir -p DerivedSources/WebCore &&
 make -C DerivedSources/WebCore -f ../../DerivedSources.make $@
