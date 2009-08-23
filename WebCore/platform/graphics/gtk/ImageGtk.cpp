a9f4ecd4b6e3fcb0ef344a15937a78a5f552d1f1
Xan Lopez <xan@webkit.org>
2009-08-23 07:18:17 +0000

    2009-08-22  Martin Robinson  <martin.james.robinson@gmail.com>
            Reviewed by Xan Lopez.
    
            [GTK] BitmapImage::getGdkPixbuf does not handle alpha channels properly
            https://bugs.webkit.org/show_bug.cgi?id=28345
    
            When doing the conversion between cairo_surface_t* and GdkPixbuf*
            account for the differences in the respective formats' in-memory
            image format.
    
            * platform/graphics/gtk/ImageGtk.cpp:
            (WebCore::getCairoSurfacePixel):
            (WebCore::getGdkPixbufPixel):
            (WebCore::BitmapImage::getGdkPixbuf):
    
    Canonical link: https://commits.webkit.org/39268@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@47689 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/gtk/ImageGtk.cpp
+++ WebCore/platform/graphics/gtk/ImageGtk.cpp
@@ -88,26 +88,67 @@ PassRefPtr<Image> Image::loadPlatformResource(const char* name)
     return img.release();
 }
 
+static inline unsigned char* getCairoSurfacePixel(unsigned char* data, uint x, uint y, uint rowStride)
+{
+    return data + (y * rowStride) + x * 4;
+}
+
+static inline guchar* getGdkPixbufPixel(guchar* data, uint x, uint y, uint rowStride)
+{
+    return data + (y * rowStride) + x * 4;
+}
+
 GdkPixbuf* BitmapImage::getGdkPixbuf()
 {
     int width = cairo_image_surface_get_width(frameAtIndex(currentFrame()));
     int height = cairo_image_surface_get_height(frameAtIndex(currentFrame()));
+    unsigned char* surfaceData = cairo_image_surface_get_data(frameAtIndex(currentFrame()));
+    int surfaceRowStride = cairo_image_surface_get_stride(frameAtIndex(currentFrame()));
 
-    int bestDepth = gdk_visual_get_best_depth();
-    GdkColormap* cmap = gdk_colormap_new(gdk_visual_get_best_with_depth(bestDepth), true);
+    GdkPixbuf* dest = gdk_pixbuf_new(GDK_COLORSPACE_RGB, TRUE, 8, width, height);
+    if (!dest)
+        return 0;
 
-    GdkPixmap* pixmap = gdk_pixmap_new(0, width, height, bestDepth);
-    gdk_drawable_set_colormap(GDK_DRAWABLE(pixmap), cmap);
-    cairo_t* cr = gdk_cairo_create(GDK_DRAWABLE(pixmap));
-    cairo_set_source_surface(cr, frameAtIndex(currentFrame()), 0, 0);
-    cairo_paint(cr);
-    cairo_destroy(cr);
+    guchar* pixbufData = gdk_pixbuf_get_pixels(dest);
+    int pixbufRowStride = gdk_pixbuf_get_rowstride(dest);
 
-    GdkPixbuf* pixbuf = gdk_pixbuf_get_from_drawable(0, GDK_DRAWABLE(pixmap), 0, 0, 0, 0, 0, width, height);
-    g_object_unref(pixmap);
-    g_object_unref(cmap);
+    /* From: http://cairographics.org/manual/cairo-image-surface.html#cairo-format-t
+     * "CAIRO_FORMAT_ARGB32: each pixel is a 32-bit quantity, with alpha in
+     * the upper 8 bits, then red, then green, then blue. The 32-bit
+     * quantities are stored native-endian. Pre-multiplied alpha is used.
+     * (That is, 50% transparent red is 0x80800000, not 0x80ff0000.)"
+     *
+     * See http://developer.gimp.org/api/2.0/gdk-pixbuf/gdk-pixbuf-gdk-pixbuf.html#GdkPixbuf
+     * for information on the structure of GdkPixbufs stored with GDK_COLORSPACE_RGB.
+     *
+     * RGB color channels in CAIRO_FORMAT_ARGB32 are stored based on the
+     * endianness of the machine and are also multiplied by the alpha channel.
+     * To properly transfer the data from the Cairo surface we must divide each
+     * of the RGB channels by the alpha channel and then reorder all channels
+     * if this machine is little-endian.
+     */
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < height; x++) {
+            unsigned char* source = getCairoSurfacePixel(surfaceData, x, y, surfaceRowStride);
+            guchar* dest = getGdkPixbufPixel(pixbufData, x, y, pixbufRowStride);
 
-    return pixbuf;
+#if G_BYTE_ORDER == G_LITTLE_ENDIAN
+            guchar alpha = source[3];
+            dest[0] = alpha ? ((source[2] * 255) / alpha) : 0;
+            dest[1] = alpha ? ((source[1] * 255) / alpha) : 0;
+            dest[2] = alpha ? ((source[0] * 255) / alpha) : 0;
+            dest[3] = alpha;
+#else
+            guchar alpha = source[0];
+            dest[0] = alpha ? ((source[1] * 255) / alpha) : 0;
+            dest[1] = alpha ? ((source[2] * 255) / alpha) : 0;
+            dest[2] = alpha ? ((source[3] * 255) / alpha) : 0;
+            dest[3] = alpha;
+#endif
+        }
+    }
+
+    return dest;
 }
 
 }
