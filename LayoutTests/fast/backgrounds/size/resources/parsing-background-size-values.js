b4b3eaee1f2e8ac6dd6e30bee408c2240d76512c
Dan Bernstein <mitz@apple.com>
2009-08-24 16:26:47 +0000

    WebCore: background-size fails to parse if a single length/percentage/auto is followed by a comma https://bugs.webkit.org/show_bug.cgi?id=28674
    Reviewed by Simon Fraser.
    
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseFillSize): Added an allowComma parameter,
        which is set to false when this method consumes a comma. Moved
        handling of 'contain' and 'cover' from parseFillProperty into this
        method.
    (WebCore::CSSParser::parseFillProperty): Set allowComma to true before
        processing the next value, and pass allowComma to parseFillSize.
    * css/CSSParser.h:
    
    LayoutTests:
    background-size fails to parse if a single length/percentage/auto is followed by a comma
    https://bugs.webkit.org/show_bug.cgi?id=28674
    
    Reviewed by Simon Fraser.
    
    * fast/backgrounds/size/parsing-background-size-values-expected.txt:
    * fast/backgrounds/size/resources/parsing-background-size-values.js:
    
    
    
    Canonical link: https://commits.webkit.org/39295@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@47716 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/backgrounds/size/resources/parsing-background-size-values.js
+++ LayoutTests/fast/backgrounds/size/resources/parsing-background-size-values.js
@@ -25,4 +25,8 @@ shouldBe('test("background-size: 5em ;")', '"5em 5em"');
 shouldBe('test("background-size: 100 100 100;")', 'null');
 shouldBe('test("background-size: coconut;")', 'null');
 
+shouldBe('test("background-size: 100px,;")', 'null');
+shouldBe('test("background-size: 100px, 50%;")', '"100px 100px, 50% 50%"');
+shouldBe('test("background-size: 50% 100px, 2em, 100px 50%;")', '"50% 100px, 2em 2em, 100px 50%"');
+
 var successfullyParsed = true;
--- WebCore/css/CSSParser.cpp
+++ WebCore/css/CSSParser.cpp
@@ -2236,9 +2236,14 @@ void CSSParser::parseFillPosition(RefPtr<CSSValue>& value1, RefPtr<CSSValue>& va
         value1.swap(value2);
 }
 
-PassRefPtr<CSSValue> CSSParser::parseFillSize()
+PassRefPtr<CSSValue> CSSParser::parseFillSize(bool& allowComma)
 {
+    allowComma = true;
     CSSParserValue* value = m_valueList->current();
+
+    if (value->id == CSSValueContain || value->id == CSSValueCover)
+        return CSSPrimitiveValue::createIdentifier(value->id);
+
     RefPtr<CSSPrimitiveValue> parsedValue1;
     
     if (value->id == CSSValueAuto)
@@ -2253,6 +2258,8 @@ PassRefPtr<CSSValue> CSSParser::parseFillSize()
     if ((value = m_valueList->next())) {
         if (value->id == CSSValueAuto)
             parsedValue2 = CSSPrimitiveValue::create(0, CSSPrimitiveValue::CSS_UNKNOWN);
+        else if (value->unit == CSSParserValue::Operator && value->iValue == ',')
+            allowComma = false;
         else {
             if (!validUnit(value, FLength|FPercent, m_strict))
                 return 0;
@@ -2295,6 +2302,7 @@ bool CSSParser::parseFillProperty(int propId, int& propId1, int& propId2,
             m_valueList->next();
             allowComma = false;
         } else {
+            allowComma = true;
             switch (propId) {
                 case CSSPropertyBackgroundColor:
                     currValue = parseBackgroundColor();
@@ -2377,14 +2385,9 @@ bool CSSParser::parseFillProperty(int propId, int& propId1, int& propId2,
                     break;
                 case CSSPropertyBackgroundSize:
                 case CSSPropertyWebkitMaskSize: {
-                    if (val->id == CSSValueContain || val->id == CSSValueCover) {
-                        currValue = CSSPrimitiveValue::createIdentifier(val->id);
+                    currValue = parseFillSize(allowComma);
+                    if (currValue)
                         m_valueList->next();
-                    } else {
-                        currValue = parseFillSize();
-                        if (currValue)
-                            m_valueList->next();
-                    }
                     break;
                 }
             }
@@ -2411,7 +2414,6 @@ bool CSSParser::parseFillProperty(int propId, int& propId1, int& propId2,
                 else
                     value2 = currValue2.release();
             }
-            allowComma = true;
         }
         
         // When parsing any fill shorthand property, we let it handle building up the lists for all
