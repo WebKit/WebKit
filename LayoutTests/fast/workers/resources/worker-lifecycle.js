98f90bc1c3bd3d84444bfbfb9148b78e4264b451
Drew Wilson <atwilson@chromium.org>
2009-08-27 21:57:07 +0000

    Need to write tests for worker lifecycle mechanisms. https://bugs.webkit.org/show_bug.cgi?id=28714
    Reviewed by David Levin.
    
    * fast/workers/dedicated-worker-lifecycle-expected.txt: Added.
    * fast/workers/dedicated-worker-lifecycle.html: Added.
    * fast/workers/resources/create-shared-worker-frame.html: Added.
    * fast/workers/resources/dedicated-worker-lifecycle.js: Added.
    Broke out "orphaned worker" lifecycle tests because they only apply to dedicated workers.
    (runTests.worker.onmessage):
    (runTests):
    (orphanedWorkerExited.worker.onmessage):
    (orphanedWorkerExited):
    (orphanedTimeoutWorkerExited):
    * fast/workers/resources/shared-worker-lifecycle.js: Added.
    Added tests for the various shared worker lifecycle cases (makes sure that refcounts are properly updated when documents close).
    (runTests):
    (createWorkerFrame):
    (closeFrame):
    (closeFrame1):
    (closeFrame2):
    (closeFrame3):
    (closeFrame4):
    (complete):
    * fast/workers/resources/worker-lifecycle.js:
    Updated test so it can run in a browser (with incorrect output) to aid in debugging.
    (runTests.worker.onmessage):
    (runTests):
    (workerExited):
    * fast/workers/resources/worker-util.js: Added.
    Moved some common utilities out of worker-lifecycle.js and into this file so they can be shared with other tests.
    (log):
    (gc):
    (waitUntilWorkerThreadsExit):
    (waitUntilThreadCountMatches.return):
    (waitUntilThreadCountMatches):
    (ensureThreadCountMatches):
    (done):
    * fast/workers/shared-worker-frame-lifecycle-expected.txt: Added.
    * fast/workers/shared-worker-frame-lifecycle.html-disabled: Added.
    * fast/workers/shared-worker-lifecycle.html-disabled:
    * fast/workers/worker-lifecycle.html:
    
    Canonical link: https://commits.webkit.org/39413@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@47837 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/workers/resources/worker-lifecycle.js
+++ LayoutTests/fast/workers/resources/worker-lifecycle.js
@@ -1,38 +1,26 @@
-function log(message)
-{
-    document.getElementById("result").innerHTML += message + "<br>";
-}
 
 if (window.layoutTestController) {
     layoutTestController.dumpAsText();
     layoutTestController.waitUntilDone();
     waitUntilWorkerThreadsExit(runTests);
 } else {
-    log("FAIL: Test relies on window.layoutTestController. Please run this test via DumpRenderTree");
-    done();
-}
-
-function waitUntilWorkerThreadsExit(callback)
-{
-    if (layoutTestController.workerThreadCount == 0) {
-        // Worker threads have exited.
-        callback();
-    } else {
-        // Poll until worker threads have exited.
-        setTimeout(function() { waitUntilWorkerThreadsExit(callback); }, 100);
-    }
+    log("NOTE: Test relies on window.layoutTestController to detect when worker threads have exited. Please run this test via DumpRenderTree");
+    waitUntilWorkerThreadsExit(runTests);
 }
 
 function runTests()
 {
-    log("PASS: workerThreadCount = " + layoutTestController.workerThreadCount);
+    if (window.layoutTestController)
+        log("PASS: workerThreadCount = " + layoutTestController.workerThreadCount);
     var worker = createWorker();
     worker.postMessage("ping");
     worker.onmessage = function(event) {
-        if (layoutTestController.workerThreadCount == 1)
-            log("PASS: Worker thread created");
-        else
-            log("FAIL: After thread creation: layoutTestController.workerThreadCount = " + layoutTestController.workerThreadCount);
+        if (window.layoutTestController) {
+            if (layoutTestController.workerThreadCount == 1)
+                log("PASS: Worker thread created");
+            else
+                log("FAIL: After thread creation: layoutTestController.workerThreadCount = " + layoutTestController.workerThreadCount);
+        }
 
         // Shutdown the worker.
         worker.postMessage("close");
@@ -45,11 +33,3 @@ function workerExited()
     log("PASS: Worker exited when close() called.");
     done();
 }
-
-function done()
-{
-    log("DONE");
-    if (window.layoutTestController)
-        layoutTestController.notifyDone();
-}
-
---
+++ LayoutTests/fast/workers/resources/worker-util.js
@@ -0,0 +1,58 @@
+// Useful utilities for worker tests
+
+function log(message)
+{
+    document.getElementById("result").innerHTML += message + "<br>";
+}
+
+function gc()
+{
+    if (window.GCController)
+        return GCController.collect();
+
+    for (var i = 0; i < 10000; i++) { // force garbage collection (FF requires about 9K allocations before a collect)
+        var s = new String("abc");
+    }
+}
+
+function waitUntilWorkerThreadsExit(callback)
+{
+    waitUntilThreadCountMatches(callback, 0);
+}
+
+function waitUntilThreadCountMatches(callback, count)
+{
+    // When running in a browser, just wait for one second then call the callback.
+    if (!window.layoutTestController) {
+        setTimeout(function() { gc(); callback(); }, 1000);
+        return;
+    }
+
+    if (layoutTestController.workerThreadCount == count) {
+        // Worker threads have exited.
+        callback();
+    } else {
+        // Poll until worker threads have been GC'd/exited.
+        gc();
+        setTimeout(function() { waitUntilThreadCountMatches(callback, count); }, 10);
+    }
+}
+
+function ensureThreadCountMatches(callback, count)
+{
+    // Just wait until the thread count matches, then wait another 100ms to see if it changes, then fire the callback.
+    waitUntilThreadCountMatches(function() {
+            setTimeout(function() { waitUntilThreadCountMatches(callback, count); }, 100);
+        }, count);
+}
+
+function done()
+{
+    if (window.debug)
+        debug('<br><span class="pass">TEST COMPLETE</span>');
+    else
+        log("DONE");
+
+    if (window.layoutTestController)
+        layoutTestController.notifyDone();
+}
