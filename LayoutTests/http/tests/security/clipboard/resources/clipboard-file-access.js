ee79c2cf92beaf8adbd133d0f48e723d0efef7f6
Eric Seidel <eric@webkit.org>
2009-08-27 20:29:54 +0000

    2009-08-27  Noel Gordon  <noel.gordon@gmail.com>
            Reviewed by Eric Seidel.
    
            clipboard file access tests use "dragentered" instead of "dragenter"
            https://bugs.webkit.org/show_bug.cgi?id=28761
    
            Use dragenter and log these events.  Add a dragleave test to check
            that drag files are not visible in dragleave events.
    
            * http/tests/security/clipboard/clipboard-file-access-expected.txt:
            * http/tests/security/clipboard/resources/clipboard-file-access.js:
    
    
    Canonical link: https://commits.webkit.org/39408@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@47832 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/security/clipboard/resources/clipboard-file-access.js
+++ LayoutTests/http/tests/security/clipboard/resources/clipboard-file-access.js
@@ -8,15 +8,23 @@ dragTarget.style.height = "100px";
 // Important that we put this at the top of the doc so that logging does not cause it to go out of view (and be undragable)
 document.body.insertBefore(dragTarget, document.body.firstChild);
 
-dragTarget.addEventListener("dragentered", function() {
+dragTarget.addEventListener("dragenter", function() {
+    debug("On dragenter:")
     event.dataTransfer.dropEffect = "copy";
+    fileListShouldBe("event.dataTransfer.files", []);
     event.preventDefault();
 }, false);
 
 dragTarget.addEventListener("dragover", function() {
     debug("On dragover:")
+    event.dataTransfer.dropEffect = "copy";
     fileListShouldBe("event.dataTransfer.files", []);
     event.preventDefault();
+}, false);
+
+dragTarget.addEventListener("dragleave", function() {
+    debug("On dragleave:")
+    fileListShouldBe("event.dataTransfer.files", []);
 }, false);
 
 var expectedFilesOnDrop;
@@ -26,16 +34,23 @@ dragTarget.addEventListener("drop", function() {
     event.preventDefault();
 }, false);
 
-function moveMouseToCenterOfElement(element)
-{
+function moveMouseToCenterOfElement(element) {
     var centerX = element.offsetLeft + element.offsetWidth / 2;
     var centerY = element.offsetTop + element.offsetHeight / 2;
     eventSender.mouseMoveTo(centerX, centerY);
 }
 
-function dragFilesOntoDragTarget(files) {
+function moveMouseToOutsideOfElement(element) {
+    var outsideX = element.offsetLeft + element.offsetWidth + 42;
+    var outsideY = element.offsetTop + element.offsetHeight + 42;
+    eventSender.mouseMoveTo(outsideX, outsideY);
+}
+
+function dragFilesOntoDragTarget(files, leave) {
     eventSender.beginDragWithFiles(files);
     moveMouseToCenterOfElement(dragTarget);
+    if (leave && leave === true)
+      moveMouseToOutsideOfElement(dragTarget);
     eventSender.mouseUp();
 }
 
@@ -49,18 +64,18 @@ function fileListShouldBe(fileListString, filesArray)
     }
 }
 
-function draggingPathsShouldResultInFiles(pathsArray, filesArray)
+function draggingPathsShouldResultInFiles(pathsArray, filesArray, leave)
 {
     expectedFilesOnDrop = filesArray;
-    dragFilesOntoDragTarget(pathsArray);
+    dragFilesOntoDragTarget(pathsArray, leave || false);
 }
 
-function testDraggingFiles(filesArray)
+function testDraggingFiles(filesArray, leave)
 {
     // We could make a way to parse the filename from the path, and then only need to pass
     // the path in the filesArray.
     var pathsOnly = filesArray.map(function(fileSpec) { return fileSpec['path']; });
-    draggingPathsShouldResultInFiles(pathsOnly, filesArray);
+    draggingPathsShouldResultInFiles(pathsOnly, filesArray, leave || false);
 }
 
 function runTest()
@@ -68,25 +83,30 @@ function runTest()
     debug("Dragging no files should return an empty file list (arbitrary implementation detail):");
     testDraggingFiles([]);
 
-    debug("Dragging a single (non-existant) file onto an element:");
+    debug("Drag drop a single (non-existant) file onto an element:");
     testDraggingFiles([
         { 'path': 'DRTFakeFile', 'name' : 'DRTFakeFile', 'size' : 0 }
     ]);
 
     debug("FIXME: File.fileSize always returns 0 for files dropped by eventSender.beginDragWithFiles from http tests:  https://bugs.webkit.org/show_bug.cgi?id=25909");
 
-    debug("Dragging a real file onto an element:");
+    debug("Drag files over an element, leave that element and release the mouse:");   
+    testDraggingFiles([
+        { 'path': 'resources/apple.gif', 'name' : 'apple.gif', 'size' : 1476 }
+    ], true);
+
+    debug("Drag drop a real file onto an element:");
     testDraggingFiles([
         { 'path': 'resources/apple.gif', 'name' : 'apple.gif', 'size' : 1476 }
     ]);
-
-    debug("Dragging two files onto an element:");
+    
+    debug("Drag drop two files onto an element:");
     testDraggingFiles([
         { 'path': 'resources/apple.gif', 'name' : 'apple.gif', 'size' : 1476 },
         { 'path': 'resources/mozilla.gif', 'name' : 'mozilla.gif', 'size' : 2593 }
     ]);
 
-    debug("Dragging two files in reverse alphabetical order onto an element:");
+    debug("Drag drop two files in reverse alphabetical order onto an element:");
     testDraggingFiles([
         { 'path': 'resources/mozilla.gif', 'name' : 'mozilla.gif', 'size' : 2593 },
         { 'path': 'resources/apple.gif', 'name' : 'apple.gif', 'size' : 1476 }
@@ -94,16 +114,16 @@ function runTest()
 
     // Directory dragging behavior is covered by https://bugs.webkit.org/show_bug.cgi?id=25852 and https://bugs.webkit.org/show_bug.cgi?id=25879
     debug("FIXME: We should not allow element to accept drops including directories unless https://bugs.webkit.org/show_bug.cgi?id=25879 is fixed to make directory File objects useful from JavaScript.  The page is given File objects corresponding to directories, but form submission and xhr.send() will fail.");
-    debug("Dragging a directory onto an element:");
+    debug("Drag drop a directory onto an element:");
     draggingPathsShouldResultInFiles(['resources/directory-for-dragging'], []);
 
     // Note: The order of selection in the Finder changes the order of file paths in the pasteboard
     // thus it's important that we test different orders here as well (at least on the Mac)
     // Both drops should be refused or succeed based on how https://bugs.webkit.org/show_bug.cgi?id=25879 is resolved.  Currently we expect drops to be refused.
-    debug("Dragging a file and a directory onto an element:");
+    debug("Drag drop a file and a directory onto an element:");
     draggingPathsShouldResultInFiles(['resources/apple.gif', 'resources/directory-for-dragging'], []);
 
-    debug("Dragging a directory and a file onto an element:")
+    debug("Drag drop a directory and a file onto an element:")
     draggingPathsShouldResultInFiles(['resources/directory-for-dragging', 'resources/apple.gif'], []);
 }
 
