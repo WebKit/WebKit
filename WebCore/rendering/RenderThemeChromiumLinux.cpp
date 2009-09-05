cc6db565cf5e54f5c8ebbf8dea1808563fb1d0f0
Eric Seidel <eric@webkit.org>
2009-09-05 09:05:57 +0000

    2009-09-05  Joel Stanley  <joel@jms.id.au>
            Reviewed by David Levin.
    
            Expose functions to set the caret blink interval for Linux Chromium.
            https://bugs.webkit.org/show_bug.cgi?id=28931
    
            * rendering/RenderThemeChromiumLinux.cpp:
            (WebCore::RenderThemeChromiumLinux::setCaretBlinkInterval):
            (WebCore::RenderThemeChromiumLinux::caretBlinkInterval):
            * rendering/RenderThemeChromiumLinux.h:
    
    Canonical link: https://commits.webkit.org/39664@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@48094 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderThemeChromiumLinux.cpp
+++ WebCore/rendering/RenderThemeChromiumLinux.cpp
@@ -125,4 +125,14 @@ bool RenderThemeChromiumLinux::supportsControlTints() const
     return true;
 }
 
+void RenderThemeChromiumLinux::setCaretBlinkInterval(double interval)
+{
+    m_caretBlinkInterval = interval;
+}
+
+double RenderThemeChromiumLinux::caretBlinkInterval() const
+{
+    return m_caretBlinkInterval;
+}
+
 } // namespace WebCore
