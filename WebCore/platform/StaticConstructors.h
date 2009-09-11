add94300920cdf25f1cc6560eca356e11860e79b
Eric Seidel <eric@webkit.org>
2009-09-11 08:35:50 +0000

    2009-09-11  Yongjun Zhang  <yongjun.zhang@nokia.com>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=29131
            Use a different variadic macro format for Symbian compiler.
    
            Use arg... instead of ellipsis in variadic macro.
    
            * platform/StaticConstructors.h:
    
    Canonical link: https://commits.webkit.org/39858@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@48289 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/StaticConstructors.h
+++ WebCore/platform/StaticConstructors.h
@@ -56,6 +56,9 @@
 #if COMPILER(MSVC7)
 #define DEFINE_GLOBAL(type, name) \
     const type name;
+#elif COMPILER(WINSCW)
+#define DEFINE_GLOBAL(type, name, arg...) \
+    const type name;
 #else
 #define DEFINE_GLOBAL(type, name, ...) \
     const type name;
@@ -66,6 +69,9 @@
 // Use an array of pointers instead of an array of char in case there is some alignment issue.
 #if COMPILER(MSVC7)
 #define DEFINE_GLOBAL(type, name) \
+    void * name[(sizeof(type) + sizeof(void *) - 1) / sizeof(void *)];
+#elif COMPILER(WINSCW)
+#define DEFINE_GLOBAL(type, name, arg...) \
     void * name[(sizeof(type) + sizeof(void *) - 1) / sizeof(void *)];
 #else
 #define DEFINE_GLOBAL(type, name, ...) \
