3bb0b36bf285963734b0ee49b541a049ce54ccd9
Janne Koskinen <janne.p.koskinen@digia.com>
2009-09-17 10:54:42 +0000

    Symbian/WINSCW build fox.
    Patch by Janne Koskinen <janne.p.koskinen@digia.com> on 2009-09-17
    Reviewed by Simon Hausmann.
    
    Repeat Q_OS_WIN wchar_t hack for WINSCW, similar to
    revision 24774.
    
    WINSCW defines wchar_t, thus UChar has to be wchar_t
    
    * wtf/unicode/qt4/UnicodeQt4.h:
    
    Canonical link: https://commits.webkit.org/40028@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@48463 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/unicode/qt4/UnicodeQt4.h
+++ JavaScriptCore/wtf/unicode/qt4/UnicodeQt4.h
@@ -58,7 +58,7 @@ QT_END_NAMESPACE
 #endif
 
 // ugly hack to make UChar compatible with JSChar in API/JSStringRef.h
-#if defined(Q_OS_WIN)
+#if defined(Q_OS_WIN) || COMPILER(WINSCW)
 typedef wchar_t UChar;
 #else
 typedef uint16_t UChar;
