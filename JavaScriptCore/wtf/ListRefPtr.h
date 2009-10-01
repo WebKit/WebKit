345586897c1e625eb8f2fda79bd20c693df16387
Eric Seidel <eric@webkit.org>
2009-10-01 19:18:02 +0000

    2009-10-01  Yongjun Zhang  <yongjun.zhang@nokia.com>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=29187
    
            Don't inline ~ListRefPtr() to work around winscw compiler forward declaration
            bug regarding templated classes.
    
            The compiler bug is reported at:
            https://xdabug001.ext.nokia.com/bugzilla/show_bug.cgi?id=9812
    
            The change will be reverted when the above bug is fixed in winscw compiler.
    
            * wtf/ListRefPtr.h:
            (WTF::::~ListRefPtr):
    
    Canonical link: https://commits.webkit.org/40547@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@48988 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/ListRefPtr.h
+++ JavaScriptCore/wtf/ListRefPtr.h
@@ -34,13 +34,8 @@ namespace WTF {
         ListRefPtr(const RefPtr<T>& o) : RefPtr<T>(o) {}
         // see comment in PassRefPtr.h for why this takes const reference
         template <typename U> ListRefPtr(const PassRefPtr<U>& o) : RefPtr<T>(o) {}
-
-        ~ListRefPtr()
-        {
-            RefPtr<T> reaper = this->release();
-            while (reaper && reaper->hasOneRef())
-                reaper = reaper->releaseNext(); // implicitly protects reaper->next, then derefs reaper
-        }
+        
+        ~ListRefPtr();
         
         ListRefPtr& operator=(T* optr) { RefPtr<T>::operator=(optr); return *this; }
         ListRefPtr& operator=(const RefPtr<T>& o) { RefPtr<T>::operator=(o); return *this; }
@@ -48,6 +43,20 @@ namespace WTF {
         template <typename U> ListRefPtr& operator=(const RefPtr<U>& o) { RefPtr<T>::operator=(o); return *this; }
         template <typename U> ListRefPtr& operator=(const PassRefPtr<U>& o) { RefPtr<T>::operator=(o); return *this; }
     };
+
+    // Remove inline for winscw compiler to prevent the compiler agressively resolving
+    // T::ref() in RefPtr<T>'s copy constructor.  The bug is reported at:
+    // https://xdabug001.ext.nokia.com/bugzilla/show_bug.cgi?id=9812.
+    template <typename T> 
+#if !COMPILER(WINSCW)
+    inline
+#endif
+    ListRefPtr<T>::~ListRefPtr()
+    {
+        RefPtr<T> reaper = this->release();
+        while (reaper && reaper->hasOneRef())
+            reaper = reaper->releaseNext(); // implicitly protects reaper->next, then derefs reaper
+    }
 
     template <typename T> inline T* getPtr(const ListRefPtr<T>& p)
     {
