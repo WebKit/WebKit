c97c4976494d01d0d1790736c036c69ee5a684e8
Eric Seidel <eric@webkit.org>
2009-10-06 11:57:17 +0000

    2009-10-06  Alejandro G. Castro  <alex@igalia.com>
            Reviewed by Xan Lopez.
    
            ASSERT executing the webhistory unittest in a debug build
            https://bugs.webkit.org/show_bug.cgi?id=28947
    
            We have to call g_object_new first to initialize webkit threading
            before creating the KURL.
    
            * WebKit/gtk/webkit/webkitwebhistoryitem.cpp:
    
    Canonical link: https://commits.webkit.org/40731@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@49172 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/gtk/webkit/webkitwebhistoryitem.cpp
+++ WebKit/gtk/webkit/webkitwebhistoryitem.cpp
@@ -297,12 +297,11 @@ WebKitWebHistoryItem* webkit_web_history_item_new()
  */
 WebKitWebHistoryItem* webkit_web_history_item_new_with_data(const gchar* uri, const gchar* title)
 {
-    WebCore::KURL historyUri(WebCore::KURL(), uri);
-    WebCore::String historyTitle = WebCore::String::fromUTF8(title);
-
     WebKitWebHistoryItem* webHistoryItem = WEBKIT_WEB_HISTORY_ITEM(g_object_new(WEBKIT_TYPE_WEB_HISTORY_ITEM, NULL));
     WebKitWebHistoryItemPrivate* priv = webHistoryItem->priv;
 
+    WebCore::KURL historyUri(WebCore::KURL(), uri);
+    WebCore::String historyTitle = WebCore::String::fromUTF8(title);
     RefPtr<WebCore::HistoryItem> item = WebCore::HistoryItem::create(historyUri, historyTitle, 0);
     priv->historyItem = item.release().releaseRef();
     webkit_history_item_add(webHistoryItem, priv->historyItem);
