5f686f8e8e6f48501677a34e9efb98a8b61c1ff0
Xan Lopez <xan@webkit.org>
2009-10-07 12:44:02 +0000

    2009-10-07  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Gustavo Noronha.
    
            "delete" in EventSender is the backspace key, not the delete one.
    
            * DumpRenderTree/gtk/EventSender.cpp:
            (keyDownCallback):
    
    Canonical link: https://commits.webkit.org/40800@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@49241 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/DumpRenderTree/gtk/EventSender.cpp
+++ WebKitTools/DumpRenderTree/gtk/EventSender.cpp
@@ -381,7 +381,7 @@ static JSValueRef keyDownCallback(JSContextRef context, JSObjectRef function, JS
     else if (JSStringIsEqualToUTF8CString(character, "end"))
         gdkKeySym = GDK_End;
     else if (JSStringIsEqualToUTF8CString(character, "delete"))
-        gdkKeySym = GDK_Delete;
+        gdkKeySym = GDK_BackSpace;
     else if (JSStringIsEqualToUTF8CString(character, "F1"))
         gdkKeySym = GDK_F1;
     else if (JSStringIsEqualToUTF8CString(character, "F2"))
