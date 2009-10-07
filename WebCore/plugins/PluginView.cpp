b85b60e90ce0df19f5be6c34ebe88b12f00490c9
Adam Roben <aroben@apple.com>
2009-10-07 15:11:49 +0000

    Fix typo in PluginView::load that was causing cross-origin loads to be allowed
    This typo was introduced in the build fix in r49213.
    
    Fixes <http://webkit.org/b/30168> REGRESSION (r49213):
    http/tests/plugins/local-geturl-from-remote.html is failing on Windows
    
    Reviewed by Sam Weinig.
    
    * plugins/PluginView.cpp:
    (WebCore::PluginView::load): Removed a comma operator that was making
    a condition always evaluate to true.
    
    Canonical link: https://commits.webkit.org/40803@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@49244 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/plugins/PluginView.cpp
+++ WebCore/plugins/PluginView.cpp
@@ -517,9 +517,8 @@ NPError PluginView::load(const FrameLoadRequest& frameLoadRequest, bool sendNoti
         // For security reasons, only allow JS requests to be made on the frame that contains the plug-in.
         if (!targetFrameName.isNull() && m_parentFrame->tree()->find(targetFrameName) != m_parentFrame)
             return NPERR_INVALID_PARAM;
-    } else if (!SecurityOrigin::canLoad(url, String(), m_parentFrame->document()), 0) {
+    } else if (!SecurityOrigin::canLoad(url, String(), m_parentFrame->document()))
             return NPERR_GENERIC_ERROR;
-    }
 
     PluginRequest* request = new PluginRequest(frameLoadRequest, sendNotification, notifyData, arePopupsAllowed());
     scheduleRequest(request);
