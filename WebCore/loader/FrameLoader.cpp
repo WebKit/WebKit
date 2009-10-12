3ebd4f473782e5926196df013221f6540464b6f8
Eric Seidel <eric@webkit.org>
2009-10-12 03:15:10 +0000

    2009-10-11  Collin Jackson  <collinj@webkit.org>
            Reviewed by Adam Barth.
    
            Log mixed content warnings to console
    
            https://bugs.webkit.org/show_bug.cgi?id=30290
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::checkIfDisplayInsecureContent):
            (WebCore::FrameLoader::checkIfRunInsecureContent):
    
    Canonical link: https://commits.webkit.org/40953@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@49431 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/FrameLoader.cpp
+++ WebCore/loader/FrameLoader.cpp
@@ -1408,6 +1408,10 @@ void FrameLoader::checkIfDisplayInsecureContent(SecurityOrigin* context, const K
     if (!isMixedContent(context, url))
         return;
 
+    String message = String::format("The page at %s displayed insecure content from %s.\n",
+        m_URL.string().utf8().data(), url.string().utf8().data());
+    m_frame->domWindow()->console()->addMessage(HTMLMessageSource, LogMessageType, WarningMessageLevel, message, 1, String());
+
     m_client->didDisplayInsecureContent();
 }
 
@@ -1415,6 +1419,10 @@ void FrameLoader::checkIfRunInsecureContent(SecurityOrigin* context, const KURL&
 {
     if (!isMixedContent(context, url))
         return;
+
+    String message = String::format("The page at %s ran insecure content from %s.\n",
+        m_URL.string().utf8().data(), url.string().utf8().data());
+    m_frame->domWindow()->console()->addMessage(HTMLMessageSource, LogMessageType, WarningMessageLevel, message, 1, String());
 
     m_client->didRunInsecureContent(context);
 }
