0f3b3d9b1435bbda004ee7b8cd47e7545b5f1994
Jan Alonzo <jmalonzo@webkit.org>
2009-10-19 19:38:36 +0000

    2009-10-19  Jan Michael Alonzo  <jmalonzo@webkit.org>
            Reviewed by Alexey Proskuryakov.
    
            [Gtk] ApplicationCacheStorage error while compiling WebKit/gtk/webkit/webkitapplicationcache.cpp
            https://bugs.webkit.org/show_bug.cgi?id=30358
    
            Guard calls to cacheStorage() with ENABLE(OFFLINE_WEB_APPLICATIONS).
    
            * webkit/webkitapplicationcache.cpp:
    
    Canonical link: https://commits.webkit.org/41255@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@49801 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/gtk/webkit/webkitapplicationcache.cpp
+++ WebKit/gtk/webkit/webkitapplicationcache.cpp
@@ -20,13 +20,17 @@
 #include "config.h"
 #include "webkitprivate.h"
 
+#include <wtf/UnusedParam.h>
+
 #include "ApplicationCacheStorage.h"
 
 void webkit_application_cache_set_maximum_size(unsigned long long size)
 {
+#if ENABLE(OFFLINE_WEB_APPLICATIONS)   
     WebCore::cacheStorage().empty();
     WebCore::cacheStorage().vacuumDatabaseFile();
     WebCore::cacheStorage().setMaximumSize(size);
+#else
+    UNUSED_PARAM(size);
+#endif
 }
-
-
