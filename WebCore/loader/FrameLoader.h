ece761b138a0a5a1cbc501d140998f9b624f9df7
Nate Chapin <japhet@chromium.org>
2009-10-20 18:17:01 +0000

    2009-10-20  Nate Chapin  <japhet@chromium.org>
            Reviewed by Adam Barth.
    
            Added getter for FrameLoader:m_suppressOpenerInNewFrame.
            This will allow Chromium to more intelligently detect
            noreferrer links and therefore open them in a new process.
    
            https://bugs.webkit.org/show_bug.cgi?id=30581
    
            * loader/FrameLoader.h:
            (WebCore::FrameLoader::suppressOpenerInNewFrame): Added.
    
    
    Canonical link: https://commits.webkit.org/41326@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@49872 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/FrameLoader.h
+++ WebCore/loader/FrameLoader.h
@@ -322,6 +322,8 @@ public:
     // uses the policy machinery (and therefore is called via the PolicyChecker).  Once we
     // introduce a proper callback type for this function, we should make it private again.
     void continueLoadAfterWillSubmitForm();
+    
+    bool suppressOpenerInNewFrame() const { return m_suppressOpenerInNewFrame; }
 
 private:
     bool canCachePageContainingThisFrame();
