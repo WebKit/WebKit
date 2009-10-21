4290c3dfe323cb87823de01d01c66159cbb0cc01
Brian Weinstein <bweinstein@apple.com>
2009-10-21 21:51:20 +0000

    WebCore: Fixes part of <http://webkit.org/b/30522>. Web Inspector: DOM Exceptions throughout the Inspector should be more human readable.
    Reviewed by Eric Seidel.
    
    Expose the description attribute that is now a member of
    ExceptionBase as of r49723, so when a user logs an exception, they
    can see the description in the Web Inspector.
    
    Test: fast/dom/dom-exception-description.html
    
    * dom/DOMCoreException.idl:
    * dom/EventException.idl:
    * dom/RangeException.idl:
    * svg/SVGException.idl:
    * xml/XMLHttpRequestException.idl:
    * xml/XPathException.idl:
    
    LayoutTests: Fixes part of <http://webkit.org/b/30522>.
    Web Inspector: DOM Exceptions throughout the Inspector should be more human readable.
    
    Reviewed by Eric Seidel.
    
    Adds a test to check the name and description of some DOM Exceptions.
    
    * fast/dom/dom-exception-description-expected.txt: Added.
    * fast/dom/dom-exception-description.html: Added.
    * fast/dom/script-tests/dom-exception-description.js: Added.
    
    
    
    Canonical link: https://commits.webkit.org/41376@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@49922 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/dom/script-tests/dom-exception-description.js
@@ -0,0 +1,30 @@
+description("This test checks the names and descriptions of DOM exceptions matches what they should be.");
+
+try {
+    document.appendChild();
+} catch (e) {
+    // FIXME: Setting window.exception because shouldBe needs a global variable
+    window.exception = e;
+    shouldBeEqualToString("window.exception.name", "NOT_FOUND_ERR");
+    debug(e.description);
+}
+
+try {
+    document.appendChild(document.createElement());
+} catch (e) {
+    // FIXME: Setting window.exception because shouldBe needs a global variable
+    window.exception = e;
+    shouldBeEqualToString("window.exception.name", "HIERARCHY_REQUEST_ERR");
+    debug(e.description);
+}
+
+try {
+    document.createTextNode("foo").splitText(10000);
+} catch (e) {
+    // FIXME: Setting window.exception because shouldBe needs a global variable
+    window.exception = e;
+    shouldBeEqualToString("window.exception.name", "INDEX_SIZE_ERR");
+    debug(e.description);
+}
+
+var successfullyParsed = true;
