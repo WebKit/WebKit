4124fb45b0f8048ab06b49a11758335e69f40018
Tor Arne Vestbø <vestbo@webkit.org>
2009-10-23 14:30:09 +0000

    Qt build fix
    Canonical link: https://commits.webkit.org/41432@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@49978 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/qt/Api/qwebframe.cpp
+++ WebKit/qt/Api/qwebframe.cpp
@@ -420,7 +420,7 @@ void QWebFrame::addToJavaScriptWindowObject(const QString &name, QObject *object
         return;
 
     JSC::JSLock lock(JSC::SilenceAssertionsOnly);
-    JSDOMWindow* window = toJSDOMWindow(d->frame);
+    JSDOMWindow* window = toJSDOMWindow(d->frame, mainThreadNormalWorld());
     JSC::Bindings::RootObject* root = d->frame->script()->bindingRootObject();
     if (!window) {
         qDebug() << "Warning: couldn't get window object";
