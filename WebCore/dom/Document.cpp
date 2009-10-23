2b7db6b52048797baa7d7885df975d444fb108bd
Eric Seidel <eric@webkit.org>
2009-10-23 19:48:03 +0000

    2009-10-23  Xiaomei Ji  <xji@chromium.org>
            Reviewed by Sam Weinig.
    
            Fix caretRangeFromPoint()/elementFromPoint() returns wrong result
            for a zoomed and scrolled page.
            https://bugs.webkit.org/show_bug.cgi?id=30689.
    
            The real cause is the wrong calculation of event.clientX/Y when
            page is zoomed and scolled.
            After Sam fixed the event.clientX/Y in r49551, the hit test point
            should be recalculated (revert r49490).
    
            * fast/dom/Document/CaretRangeFromPoint/caretRangeFromPoint-in-zoom-and-scroll-expected.txt: Added.
            * fast/dom/Document/CaretRangeFromPoint/caretRangeFromPoint-in-zoom-and-scroll.html: Added.
            * fast/dom/Document/CaretRangeFromPoint/hittest-relative-to-viewport-expected.txt:
            * fast/dom/Document/CaretRangeFromPoint/hittest-relative-to-viewport.html:
            * fast/dom/elementFromPoint-relative-to-viewport-expected.txt:
            * fast/dom/elementFromPoint-relative-to-viewport.html:
    2009-10-23  Xiaomei Ji  <xji@chromium.org>
    
            Reviewed by Sam Weinig.
    
            Fix caretRangeFromPoint()/elementFromPoint() returns wrong result
            for a zoomed and scrolled page.
            https://bugs.webkit.org/show_bug.cgi?id=30689.
    
            The real cause is the wrong calculation of event.clientX/Y when
            page is zoomed and scolled.
            After Sam fixed the event.clientX/Y in r49551, the hit test point
            should be recalculated (revert r49490).
    
            Test: fast/dom/Document/CaretRangeFromPoint/caretRangeFromPoint-in-zoom-and-scroll.html
    
            * dom/Document.cpp:
            (WebCore::Document::elementFromPoint): calculate the correct point
            relative to document when page is zoomed and scrolled.
            (WebCore::Document::caretRangeFromPoint): calculate the correct point
            relative to document when page is zoomed and scrolled.
    
    Canonical link: https://commits.webkit.org/41444@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@49990 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/dom/Document.cpp
+++ WebCore/dom/Document.cpp
@@ -951,7 +951,7 @@ Element* Document::elementFromPoint(int x, int y) const
         return 0;
 
     float zoomFactor = frame->pageZoomFactor();
-    IntPoint point = roundedIntPoint(FloatPoint((x + view()->scrollX()) * zoomFactor, (y + view()->scrollY()) * zoomFactor));
+    IntPoint point = roundedIntPoint(FloatPoint(x * zoomFactor  + view()->scrollX(), y * zoomFactor + view()->scrollY()));
 
     if (!frameView->visibleContentRect().contains(point))
         return 0;
@@ -981,7 +981,7 @@ PassRefPtr<Range> Document::caretRangeFromPoint(int x, int y)
         return 0;
 
     float zoomFactor = frame->pageZoomFactor();
-    IntPoint point = roundedIntPoint(FloatPoint((x + view()->scrollX()) * zoomFactor, (y + view()->scrollY()) * zoomFactor));
+    IntPoint point = roundedIntPoint(FloatPoint(x * zoomFactor + view()->scrollX(), y * zoomFactor + view()->scrollY()));
 
     if (!frameView->visibleContentRect().contains(point))
         return 0;
