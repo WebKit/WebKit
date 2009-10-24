3ec8445504c324bf38e397074367dd7d063dfdec
Mark Rowe <mrowe@apple.com>
2009-10-24 03:01:26 +0000

    Build fix.  Include config.h before including other files.
    * AccessibleImage.cpp:
    
    Canonical link: https://commits.webkit.org/41469@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50019 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/win/AccessibleImage.cpp
+++ WebKit/win/AccessibleImage.cpp
@@ -23,6 +23,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
+#include "config.h"
 #include "AccessibleImage.h"
 
 #include <WebCore/AccessibilityRenderObject.h>
