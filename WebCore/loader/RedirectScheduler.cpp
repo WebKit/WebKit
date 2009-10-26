86bcbc76f03650294fe2123b270bf20b6afded4c
Brady Eidson <beidson@apple.com>
2009-10-26 23:20:25 +0000

    <rdar://problem/7308952> and http://webkit.org/b/30424 - REGRESSION (r48687): Pages on ucas.com appear blank.
    Reviewed by Darin Adler.
    
    WebCore:
    
    Test: fast/loader/history-forward-in-head.html
    
    * loader/RedirectScheduler.cpp:
    (WebCore::RedirectScheduler::scheduleHistoryNavigation): Restore the moved canGoBackOrForward() check.
      This restores the side effect of an invalid history navigation canceling previous scheduled redirects
      and also fixes the bug of scheduling an invalid history navigation canceling parsing of the current document.
    (WebCore::RedirectScheduler::timerFired): Removed the canGoBackOrForward() check as it is now made before
      the redirect is scheduled.
    
    LayoutTests:
    
    * fast/loader/history-forward-in-head-expected.txt: Added.
    * fast/loader/history-forward-in-head.html: Added.
    
    
    
    Canonical link: https://commits.webkit.org/41557@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50111 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/RedirectScheduler.cpp
+++ WebCore/loader/RedirectScheduler.cpp
@@ -255,6 +255,13 @@ void RedirectScheduler::scheduleHistoryNavigation(int steps)
     if (!m_frame->page())
         return;
 
+    // Invalid history navigations (such as history.forward() during a new load) have the side effect of cancelling any scheduled
+    // redirects. We also avoid the possibility of cancelling the current load by avoiding the scheduled redirection altogether.
+    if (!m_frame->page()->canGoBackOrForward(steps)) { 
+        cancel(); 
+        return; 
+    } 
+
     schedule(new ScheduledRedirection(steps));
 }
 
@@ -282,8 +289,7 @@ void RedirectScheduler::timerFired(Timer<RedirectScheduler>*)
             }
             // go(i!=0) from a frame navigates into the history of the frame only,
             // in both IE and NS (but not in Mozilla). We can't easily do that.
-            if (m_frame->page()->canGoBackOrForward(redirection->historySteps))
-                m_frame->page()->goBackOrForward(redirection->historySteps);
+            m_frame->page()->goBackOrForward(redirection->historySteps);
             return;
         case ScheduledRedirection::formSubmission:
             // The submitForm function will find a target frame before using the redirection timer.
