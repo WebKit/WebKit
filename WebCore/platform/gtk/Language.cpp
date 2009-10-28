4562675e3898ba53b071e384477d9abb4dceb84b
Gustavo Noronha Silva <kov@webkit.org>
2009-10-28 22:42:42 +0000

    LayoutTests
            Reviewed by Xan Lopez.
    
            [GTK] Fails new test fast/js/navigator-language.html
            https://bugs.webkit.org/show_bug.cgi?id=30440
    
            Unskip the test that now passes.
    
            * platform/gtk/Skipped:
    WebCore
    
            Reviewed by Xan Lopez.
    
            [GTK] Fails new test fast/js/navigator-language.html
            https://bugs.webkit.org/show_bug.cgi?id=30440
    
            Reimplement WebCore::defaultLanguage to account for changes in
            locale done by setLocale.
    
            Already existing test: fast/js/navigator-language.html
    
            * platform/gtk/Language.cpp:
            (WebCore::defaultLanguage):
    
    Canonical link: https://commits.webkit.org/41686@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50244 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/gtk/Language.cpp
+++ WebCore/platform/gtk/Language.cpp
@@ -21,16 +21,37 @@
 #include "Language.h"
 
 #include "CString.h"
+#include "GOwnPtr.h"
 #include "PlatformString.h"
 
 #include <gtk/gtk.h>
+#include <locale.h>
 #include <pango/pango.h>
 
 namespace WebCore {
 
+// Using pango_language_get_default() here is not an option, because
+// it doesn't support changing the locale in runtime, so it returns
+// always the same value.
 String defaultLanguage()
 {
-    return pango_language_to_string(gtk_get_default_language());
+    char* localeDefault = setlocale(LC_CTYPE, NULL);
+
+    if (!localeDefault)
+        return String("c");
+
+    GOwnPtr<gchar> normalizedDefault(g_ascii_strdown(localeDefault, -1));
+    char* ptr = strchr(normalizedDefault.get(), '_');
+
+    if(ptr)
+        *ptr = '-';
+
+    ptr = strchr(normalizedDefault.get(), '.');
+
+    if(ptr)
+        *ptr = '\0';
+
+    return String(normalizedDefault.get());
 }
 
 }
