a51de1c163bb440a6a45a2597baa08e909828bde
Gavin Barraclough <barraclough@apple.com>
2009-10-28 11:06:19 +0000

    * jit/JIT.h:
    Reviewed by NOBODY (build fix!).
    
    
    
    Canonical link: https://commits.webkit.org/41653@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50211 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/jit/JIT.h
+++ JavaScriptCore/jit/JIT.h
@@ -38,7 +38,7 @@
 #define JIT_CLASS_ALIGNMENT
 #endif
 
-#define ASSERT_JIT_OFFSET(actual, expected) ASSERT_WITH_MESSAGE(actual == expected, "JIT Offset \"%s\" shoud be %d, not %d.\n", #expected, actual, expected);
+#define ASSERT_JIT_OFFSET(actual, expected) ASSERT_WITH_MESSAGE(actual == expected, "JIT Offset \"%s\" should be %d, not %d.\n", #expected, static_cast<int>(actual), static_cast<int>(expected));
 
 #include "CodeBlock.h"
 #include "Interpreter.h"
