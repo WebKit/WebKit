cc548658e12b98fd51a7715ad3384b8227752eed
Kenneth Rohde Christiansen <kenneth@webkit.org>
2009-10-30 13:29:02 +0000

    If the owner widget of the page has a palette set, we should use that one. This was only working when the owner was a QWebView. This patch fixes that.
    Patch by Kenneth Rohde Christiansen <kenneth@webkit.org> on 2009-10-30
    Reviewed by Holger Hans Peter Freyther.
    
    * platform/qt/RenderThemeQt.cpp:
    (WebCore::RenderThemeQt::applyTheme):
    
    Canonical link: https://commits.webkit.org/41772@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50330 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/qt/RenderThemeQt.cpp
+++ WebCore/platform/qt/RenderThemeQt.cpp
@@ -45,6 +45,7 @@
 #include "RenderBox.h"
 #include "RenderTheme.h"
 #include "UserAgentStyleSheets.h"
+#include "QWebPageClient.h"
 #include "qwebpage.h"
 
 #include <QApplication>
@@ -757,12 +758,13 @@ ControlPart RenderThemeQt::applyTheme(QStyleOption& option, RenderObject* o) con
     if (result == RadioPart || result == CheckboxPart)
         option.state |= (isChecked(o) ? QStyle::State_On : QStyle::State_Off);
 
-    // If the webview has a custom palette, use it
+    // If the owner widget has a custom palette, use it
     Page* page = o->document()->page();
     if (page) {
-        QWidget* view = static_cast<ChromeClientQt*>(page->chrome()->client())->m_webPage->view();
-        if (view)
-            option.palette = view->palette();
+        ChromeClient* client = page->chrome()->client();
+        QWebPageClient* pageClient = client->platformPageClient();
+        if (pageClient)
+            option.palette = pageClient->palette();
     }
 
     return result;
