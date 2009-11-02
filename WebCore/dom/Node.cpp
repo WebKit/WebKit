4dbe2953d25592da01dbd7d92cae850a89b8a15a
Brian Weinstein <bweinstein@apple.com>
2009-11-02 05:57:39 +0000

    Change ssize_t to unsigned to try and fix Windows build.
    Canonical link: https://commits.webkit.org/41836@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50403 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/dom/Node.cpp
+++ WebCore/dom/Node.cpp
@@ -2451,7 +2451,7 @@ static bool eventHasListeners(const AtomicString& eventType, Node* node, Vector<
     if (node->hasEventListeners(eventType))
         return true;
 
-    for (ssize_t i = (ancestors.size() - 1); i >= 0; --i) {
+    for (unsigned i = (ancestors.size() - 1); i >= 0; --i) {
         ContainerNode* ancestor = ancestors[i].get();
         if (ancestor->hasEventListeners(eventType))
             return true;
