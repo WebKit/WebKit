8c9694efa0c414559a075c04b598de062ccc0828
Eric Seidel <eric@webkit.org>
2009-11-09 21:14:20 +0000

    2009-11-09  Yaar Schnitman  <yaar@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            Upstreaming the Chromium WebKit API: WebURL and friends.
    
            https://bugs.webkit.org/show_bug.cgi?id=28394
    
            * public/WebURL.h: Added.
            (WebKit::WebURL::~WebURL):
            (WebKit::WebURL::WebURL):
            (WebKit::WebURL::operator=):
            (WebKit::WebURL::assign):
            (WebKit::WebURL::spec):
            (WebKit::WebURL::parsed):
            (WebKit::WebURL::isValid):
            (WebKit::WebURL::isEmpty):
            (WebKit::WebURL::isNull):
            (WebKit::WebURL::operator GURL):
            * public/WebURLError.h: Added.
            (WebKit::WebURLError::WebURLError):
            * public/WebURLLoader.h: Added.
            (WebKit::WebURLLoader::~WebURLLoader):
            * public/WebURLLoaderClient.h: Added.
            (WebKit::WebURLLoaderClient::~WebURLLoaderClient):
            * public/WebURLRequest.h: Added.
            (WebKit::WebURLRequest::):
            (WebKit::WebURLRequest::~WebURLRequest):
            (WebKit::WebURLRequest::WebURLRequest):
            (WebKit::WebURLRequest::operator=):
            * public/WebURLResponse.h: Added.
            (WebKit::WebURLResponse::~WebURLResponse):
            (WebKit::WebURLResponse::WebURLResponse):
            (WebKit::WebURLResponse::operator=):
    
    Canonical link: https://commits.webkit.org/42112@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50682 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebKit/chromium/public/WebURLRequest.h
@@ -0,0 +1,155 @@
+/*
+ * Copyright (C) 2009 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef WebURLRequest_h
+#define WebURLRequest_h
+
+#include "WebCommon.h"
+#include "WebHTTPBody.h"
+
+#if defined(WEBKIT_IMPLEMENTATION)
+namespace WebCore { struct ResourceRequest; }
+#endif
+
+namespace WebKit {
+
+class WebCString;
+class WebHTTPBody;
+class WebHTTPHeaderVisitor;
+class WebString;
+class WebURL;
+class WebURLRequestPrivate;
+
+class WebURLRequest {
+public:
+    enum CachePolicy {
+        UseProtocolCachePolicy,  // normal load
+        ReloadIgnoringCacheData, // reload
+        ReturnCacheDataElseLoad, // back/forward or encoding change - allow stale data
+        ReturnCacheDataDontLoad, // results of a post - allow stale data and only use cache
+    };
+
+    enum TargetType {
+        TargetIsMainFrame,
+        TargetIsSubFrame,
+        TargetIsSubResource,
+        TargetIsObject,
+        TargetIsMedia
+    };
+
+    ~WebURLRequest() { reset(); }
+
+    WebURLRequest() : m_private(0) { }
+    WebURLRequest(const WebURLRequest& r) : m_private(0) { assign(r); }
+    WebURLRequest& operator=(const WebURLRequest& r)
+    {
+        assign(r);
+        return *this;
+    }
+
+    explicit WebURLRequest(const WebURL& url) : m_private(0)
+    {
+        initialize();
+        setURL(url);
+    }
+
+    WEBKIT_API void initialize();
+    WEBKIT_API void reset();
+    WEBKIT_API void assign(const WebURLRequest&);
+
+    WEBKIT_API bool isNull() const;
+
+    WEBKIT_API WebURL url() const;
+    WEBKIT_API void setURL(const WebURL&);
+
+    // Used to implement third-party cookie blocking.
+    WEBKIT_API WebURL firstPartyForCookies() const;
+    WEBKIT_API void setFirstPartyForCookies(const WebURL&);
+
+    WEBKIT_API bool allowCookies() const;
+    WEBKIT_API void setAllowCookies(bool allowCookies);
+
+    // Controls whether user name, password, and cookies may be sent with the
+    // request. (If false, this overrides allowCookies.)
+    WEBKIT_API bool allowStoredCredentials() const;
+    WEBKIT_API void setAllowStoredCredentials(bool allowStoredCredentials);
+
+    WEBKIT_API CachePolicy cachePolicy() const;
+    WEBKIT_API void setCachePolicy(CachePolicy);
+
+    WEBKIT_API WebString httpMethod() const;
+    WEBKIT_API void setHTTPMethod(const WebString&);
+
+    WEBKIT_API WebString httpHeaderField(const WebString& name) const;
+    WEBKIT_API void setHTTPHeaderField(const WebString& name, const WebString& value);
+    WEBKIT_API void addHTTPHeaderField(const WebString& name, const WebString& value);
+    WEBKIT_API void clearHTTPHeaderField(const WebString& name);
+    WEBKIT_API void visitHTTPHeaderFields(WebHTTPHeaderVisitor*) const;
+
+    WEBKIT_API WebHTTPBody httpBody() const;
+    WEBKIT_API void setHTTPBody(const WebHTTPBody&);
+
+    // Controls whether upload progress events are generated when a request
+    // has a body.
+    WEBKIT_API bool reportUploadProgress() const;
+    WEBKIT_API void setReportUploadProgress(bool);
+
+    WEBKIT_API TargetType targetType() const;
+    WEBKIT_API void setTargetType(TargetType);
+
+    // A consumer controlled value intended to be used to identify the
+    // requestor.
+    WEBKIT_API int requestorID() const;
+    WEBKIT_API void setRequestorID(int);
+
+    // A consumer controlled value intended to be used to identify the
+    // process of the requestor.
+    WEBKIT_API int requestorProcessID() const;
+    WEBKIT_API void setRequestorProcessID(int);
+
+    // Allows the request to be matched up with its app cache host.
+    WEBKIT_API int appCacheHostID() const;
+    WEBKIT_API void setAppCacheHostID(int id);
+
+#if defined(WEBKIT_IMPLEMENTATION)
+    WebCore::ResourceRequest& toMutableResourceRequest();
+    const WebCore::ResourceRequest& toResourceRequest() const;
+#endif
+
+protected:
+    void assign(WebURLRequestPrivate*);
+
+private:
+    WebURLRequestPrivate* m_private;
+};
+
+} // namespace WebKit
+
+#endif
---
+++ WebKit/chromium/public/WebURLResponse.h
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2009 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef WebURLResponse_h
+#define WebURLResponse_h
+
+#include "WebCommon.h"
+
+#if defined(WEBKIT_IMPLEMENTATION)
+namespace WebCore { class ResourceResponse; }
+#endif
+
+namespace WebKit {
+
+class WebCString;
+class WebHTTPHeaderVisitor;
+class WebString;
+class WebURL;
+class WebURLResponsePrivate;
+
+class WebURLResponse {
+public:
+    ~WebURLResponse() { reset(); }
+
+    WebURLResponse() : m_private(0) { }
+    WebURLResponse(const WebURLResponse& r) : m_private(0) { assign(r); }
+    WebURLResponse& operator=(const WebURLResponse& r)
+    {
+        assign(r);
+        return *this;
+    }
+
+    explicit WebURLResponse(const WebURL& url) : m_private(0)
+    {
+        initialize();
+        setURL(url);
+    }
+
+    WEBKIT_API void initialize();
+    WEBKIT_API void reset();
+    WEBKIT_API void assign(const WebURLResponse&);
+
+    WEBKIT_API bool isNull() const;
+
+    WEBKIT_API WebURL url() const;
+    WEBKIT_API void setURL(const WebURL&);
+
+    WEBKIT_API WebString mimeType() const;
+    WEBKIT_API void setMIMEType(const WebString&);
+
+    WEBKIT_API long long expectedContentLength() const;
+    WEBKIT_API void setExpectedContentLength(long long);
+
+    WEBKIT_API WebString textEncodingName() const;
+    WEBKIT_API void setTextEncodingName(const WebString&);
+
+    WEBKIT_API WebString suggestedFileName() const;
+    WEBKIT_API void setSuggestedFileName(const WebString&);
+
+    WEBKIT_API int httpStatusCode() const;
+    WEBKIT_API void setHTTPStatusCode(int);
+
+    WEBKIT_API WebString httpStatusText() const;
+    WEBKIT_API void setHTTPStatusText(const WebString&);
+
+    WEBKIT_API WebString httpHeaderField(const WebString& name) const;
+    WEBKIT_API void setHTTPHeaderField(const WebString& name, const WebString& value);
+    WEBKIT_API void addHTTPHeaderField(const WebString& name, const WebString& value);
+    WEBKIT_API void clearHTTPHeaderField(const WebString& name);
+    WEBKIT_API void visitHTTPHeaderFields(WebHTTPHeaderVisitor*) const;
+
+    WEBKIT_API double lastModifiedDate() const;
+    WEBKIT_API void setLastModifiedDate(double);
+
+    WEBKIT_API bool isContentFiltered() const;
+    WEBKIT_API void setIsContentFiltered(bool);
+
+    WEBKIT_API long long appCacheID() const;
+    WEBKIT_API void setAppCacheID(long long);
+
+    WEBKIT_API WebURL appCacheManifestURL() const;
+    WEBKIT_API void setAppCacheManifestURL(const WebURL&);
+
+    // A consumer controlled value intended to be used to record opaque
+    // security info related to this request.
+    WEBKIT_API WebCString securityInfo() const;
+    WEBKIT_API void setSecurityInfo(const WebCString&);
+
+#if defined(WEBKIT_IMPLEMENTATION)
+    WebCore::ResourceResponse& toMutableResourceResponse();
+    const WebCore::ResourceResponse& toResourceResponse() const;
+#endif
+
+protected:
+    void assign(WebURLResponsePrivate*);
+
+private:
+    WebURLResponsePrivate* m_private;
+};
+
+} // namespace WebKit
+
+#endif
