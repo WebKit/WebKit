b3173aacfac07b4d840c17e283264144e9d4dffb
Kevin Ollivier <kevino@webkit.org>
2009-11-09 20:32:38 +0000

    Reviewed by Kevin Ollivier.
    [wx] Fix handling of alpha channel when using wxWidgets 2.9: it was
    simply ignored before resulting in transparent areas being black in PNG
    images for example.
    
    https://bugs.webkit.org/show_bug.cgi?id=30823
    
    
    Canonical link: https://commits.webkit.org/42107@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50677 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/image-decoders/wx/ImageDecoderWx.cpp
+++ WebCore/platform/image-decoders/wx/ImageDecoderWx.cpp
@@ -40,35 +40,41 @@ namespace WebCore {
 NativeImagePtr RGBA32Buffer::asNewNativeImage() const
 {
     wxBitmap* bmp = new wxBitmap(width(), height(), 32);
-    typedef wxPixelData<wxBitmap, wxAlphaPixelFormat> WxPixelData;
-    WxPixelData data(*bmp);
-    
-    // NB: It appears that the data is in BGRA format instead of RGBA format.
-    // This code works properly on both ppc and intel, meaning the issue is
-    // likely not an issue of byte order getting mixed up on different archs. 
-    const unsigned char* bytes = (const unsigned char*)m_bytes.data();
-    int rowCounter = 0;
-    long pixelCounter = 0;
-    WxPixelData::Iterator p(data);
-    WxPixelData::Iterator rowStart = p; 
-    for (size_t i = 0; i < m_bytes.size() * sizeof(PixelData); i += sizeof(PixelData)) {
-        p.Red() = bytes[i+2];
-        p.Green() = bytes[i+1];
-        p.Blue() = bytes[i+0];
-        p.Alpha() = bytes[i+3];
-        
-        p++;
 
-        pixelCounter++;
-        if ((pixelCounter % width()) == 0) {
-            rowCounter++;
-            p = rowStart;
-            p.MoveTo(data, 0, rowCounter);
+    {
+        typedef wxPixelData<wxBitmap, wxAlphaPixelFormat> WxPixelData;
+        WxPixelData data(*bmp);
+        
+        // NB: It appears that the data is in BGRA format instead of RGBA format.
+        // This code works properly on both ppc and intel, meaning the issue is
+        // likely not an issue of byte order getting mixed up on different archs. 
+        const unsigned char* bytes = (const unsigned char*)m_bytes.data();
+        int rowCounter = 0;
+        long pixelCounter = 0;
+        WxPixelData::Iterator p(data);
+        WxPixelData::Iterator rowStart = p; 
+        for (size_t i = 0; i < m_bytes.size() * sizeof(PixelData); i += sizeof(PixelData)) {
+                p.Red() = bytes[i + 2];
+                p.Green() = bytes[i + 1];
+                p.Blue() = bytes[i + 0];
+                p.Alpha() = bytes[i + 3];
+            
+            p++;
+
+            pixelCounter++;
+            if ((pixelCounter % width()) == 0) {
+                rowCounter++;
+                p = rowStart;
+                p.MoveTo(data, 0, rowCounter);
+            }
         }
-    }
 #if !wxCHECK_VERSION(2,9,0)
-    bmp->UseAlpha();
+        bmp->UseAlpha();
 #endif
+    } // ensure that WxPixelData is destroyed as it unlocks the bitmap data in
+      // its dtor and we can't access it (notably in CreateBitmap() below)
+      // before this is done
+
     ASSERT(bmp->IsOk());
 
 #if USE(WXGC)
