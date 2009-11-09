f434711f2ac9ed35c0ce78a783f41f1caee2d881
Zoltan Horvath <zoltan@webkit.org>
2009-11-09 14:27:00 +0000

    Allow custom memory allocation control for WebCore's DocLoader https://bugs.webkit.org/show_bug.cgi?id=31163
    Reviewed by Darin Adler.
    
    Inherits DocLoader class from Noncopyable because it is
    instantiated by 'new' in WebCore/dom/Document.cpp:370 and
    it is no need to be copyable.
    
    * loader/DocLoader.h:
    
    
    
    Canonical link: https://commits.webkit.org/42085@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50655 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/DocLoader.h
+++ WebCore/loader/DocLoader.h
@@ -47,7 +47,7 @@ class ImageLoader;
 class KURL;
 
 // The DocLoader manages the loading of scripts/images/stylesheets for a single document.
-class DocLoader
+class DocLoader : public Noncopyable
 {
 friend class Cache;
 friend class ImageLoader;
