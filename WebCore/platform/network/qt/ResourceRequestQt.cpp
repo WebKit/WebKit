50a35ccbda4a368f9ef0e73f53548f9d61c8d8fc
Eric Seidel <eric@webkit.org>
2009-11-09 20:46:06 +0000

    2009-11-09  Yael Aharon  <yael.aharon@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Allow setting HTTP headers with empty value in XMLHTTPRequest
            https://bugs.webkit.org/show_bug.cgi?id=31140
    
            Test that XHR request can set http headers with no value.
    
            * http/tests/xmlhttprequest/xmlhttprequest-setrequestheader-no-value-expected.txt: Added.
            * http/tests/xmlhttprequest/xmlhttprequest-setrequestheader-no-value.html: Added.
    2009-11-09  Yael Aharon  <yael.aharon@nokia.com>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Allow setting HTTP headers with empty value in XMLHTTPRequest
            https://bugs.webkit.org/show_bug.cgi?id=31140
    
            QtNetwork interprets null string as request to remove the header, not add it.
            Replace null values with empty values before passing them to QtNetwork.
    
            Test: http/tests/xmlhttprequest/xmlhttprequest-setrequestheader-no-value.html
    
            * platform/network/qt/ResourceRequestQt.cpp:
            (WebCore::ResourceRequest::toNetworkRequest):
    
    Canonical link: https://commits.webkit.org/42109@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50679 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/network/qt/ResourceRequestQt.cpp
+++ WebCore/platform/network/qt/ResourceRequestQt.cpp
@@ -41,7 +41,12 @@ QNetworkRequest ResourceRequest::toNetworkRequest(QObject* originatingFrame) con
          it != end; ++it) {
         QByteArray name = QString(it->first).toAscii();
         QByteArray value = QString(it->second).toAscii();
-        request.setRawHeader(name, value);
+        // QNetworkRequest::setRawHeader() would remove the header if the value is null
+        // Make sure to set an empty header instead of null header.
+        if (!value.isNull())
+            request.setRawHeader(name, value);
+        else
+            request.setRawHeader(name, "");
     }
 
     switch (cachePolicy()) {
