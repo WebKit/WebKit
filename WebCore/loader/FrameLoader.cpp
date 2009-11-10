7f87982fe01bc1f98bf81a676551d39de5f709a7
Anders Carlsson <andersca@apple.com>
2009-11-10 00:46:37 +0000

    WebCore: <rdar://problem/7328395> https://bugs.webkit.org/show_bug.cgi?id=31277
    Reviewed by Darin Adler and Dan Bernstein.
    
    When an object tag's style changes (for example when child nodes are added/removed),
    reuse its Frame (if it has one) instead of creating multiple Frames.
    
    Test: fast/dom/HTMLObjectElement/children-changed.html
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::requestObject):
    
    LayoutTests: <rdar://problem/7328395>
    https://bugs.webkit.org/show_bug.cgi?id=31277
    
    Reviewed by Darin Adler and Dan Bernstein.
    
    When an object tag's style changes (for example when child nodes are added/removed),
    reuse its Frame (if it has one) instead of creating multiple Frames.
    
    * fast/dom/HTMLObjectElement/children-changed-expected.txt: Added.
    * fast/dom/HTMLObjectElement/children-changed.html: Added.
    
    
    
    Canonical link: https://commits.webkit.org/42128@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50698 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/FrameLoader.cpp
+++ WebCore/loader/FrameLoader.cpp
@@ -1260,9 +1260,11 @@ bool FrameLoader::requestObject(RenderPart* renderer, const String& url, const A
 
     ASSERT(renderer->node()->hasTagName(objectTag) || renderer->node()->hasTagName(embedTag));
     HTMLPlugInElement* element = static_cast<HTMLPlugInElement*>(renderer->node());
-    
-    // FIXME: OK to always make a new frame? When does the old frame get removed?
-    return loadSubframe(element, completedURL, frameName, m_outgoingReferrer);
+
+    // If the plug-in element already contains a subframe, requestFrame will re-use it. Otherwise,
+    // it will create a new frame and set it as the RenderPart's widget, causing what was previously 
+    // in the widget to be torn down.
+    return requestFrame(element, completedURL, frameName);
 }
 
 bool FrameLoader::shouldUsePlugin(const KURL& url, const String& mimeType, bool hasFallback, bool& useFallback)
