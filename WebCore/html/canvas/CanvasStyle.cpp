c0bdb33a01f7280d4bf9b190efebddab20bd3fac
Beth Dakin <bdakin@apple.com>
2009-11-10 22:35:53 +0000

    Second attempted gtk build fix. No review needed.
    * html/canvas/CanvasStyle.cpp:
    (WebCore::CanvasStyle::applyStrokeColor):
    
    
    
    Canonical link: https://commits.webkit.org/42200@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50771 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/html/canvas/CanvasStyle.cpp
+++ WebCore/html/canvas/CanvasStyle.cpp
@@ -154,7 +154,7 @@ void CanvasStyle::applyStrokeColor(GraphicsContext* context)
             currentPen.setColor(clr);
             context->platformContext()->setPen(currentPen);
 #else
-            context->setStrokeColor(Color(m_cyan, m_magenta, m_yellow, m_black, m_alpha));
+            context->setStrokeColor(Color(m_cyan, m_magenta, m_yellow, m_black, m_alpha), DeviceColorSpace);
 #endif
             break;
         }
