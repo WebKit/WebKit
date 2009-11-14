1b7c9bb55352bcfb50a3952fedd7a719e4f90fd5
Shinichiro Hamaji <hamaji@chromium.org>
2009-11-14 00:07:03 +0000

    2009-11-13  Shinichiro Hamaji  <hamaji@chromium.org>
            Reviewed by Darin Adler.
    
            WebCore::externalRepresentation should update layout before getting render object
            https://bugs.webkit.org/show_bug.cgi?id=31459
    
            Before this change, the render tree output for this test was wrong.
            Note that #FF0000 (red) becomes #008000 (green) now.
    
            * platform/mac/svg/custom/gradient-stop-style-change-expected.txt:
    2009-11-13  Shinichiro Hamaji  <hamaji@chromium.org>
    
            Reviewed by Darin Adler.
    
            WebCore::externalRepresentation should update layout before getting render object
            https://bugs.webkit.org/show_bug.cgi?id=31459
    
            * rendering/RenderTreeAsText.cpp:
            (WebCore::externalRepresentation):
    
    Canonical link: https://commits.webkit.org/42405@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50976 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderTreeAsText.cpp
+++ WebCore/rendering/RenderTreeAsText.cpp
@@ -541,6 +541,8 @@ static void writeSelection(TextStream& ts, const RenderObject* o)
 
 String externalRepresentation(Frame* frame)
 {
+    frame->document()->updateLayout();
+
     RenderObject* o = frame->contentRenderer();
     if (!o)
         return String();
@@ -549,8 +551,6 @@ String externalRepresentation(Frame* frame)
 #if ENABLE(SVG)
     writeRenderResources(ts, o->document());
 #endif
-    if (o->view()->frameView())
-        o->view()->frameView()->layout();
     if (o->hasLayer()) {
         RenderLayer* l = toRenderBox(o)->layer();
         writeLayers(ts, l, l, IntRect(l->x(), l->y(), l->width(), l->height()));
