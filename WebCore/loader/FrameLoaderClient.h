01464fa8890bf486d8286ec70cd9b65bc6176591
Aaron Boodman <aa@chromium.org>
2009-11-14 03:34:08 +0000

    2009-11-13  Aaron Boodman  <aa@chromium.org>
            Unreviewed fix for Chromium build.
    
            * loader/FrameLoaderClient.h:
            (WebCore::FrameLoaderClient::dispatchDidClearWindowObjectInWorld):
            Provide an empty implementation of this method because I don't know
            what it is supposed to do on Chromium.
    
    Canonical link: https://commits.webkit.org/42421@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@50992 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/FrameLoaderClient.h
+++ WebCore/loader/FrameLoaderClient.h
@@ -223,7 +223,7 @@ namespace WebCore {
         virtual ObjectContentType objectContentType(const KURL& url, const String& mimeType) = 0;
         virtual String overrideMediaType() const = 0;
 
-        virtual void dispatchDidClearWindowObjectInWorld(DOMWrapperWorld*) = 0;
+        virtual void dispatchDidClearWindowObjectInWorld(DOMWrapperWorld*) {}
         virtual void documentElementAvailable() = 0;
         virtual void didPerformFirstNavigation() const = 0; // "Navigation" here means a transition from one page to another that ends up in the back/forward list.
 
