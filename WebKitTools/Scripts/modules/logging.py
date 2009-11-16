ab2189c4c7bb5de56aa2aa60a71daf26a416b3c3
Adam Barth <abarth@webkit.org>
2009-11-16 08:11:58 +0000

    2009-11-16  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Siedel.
    
            Move OutputTee to logging.py.
    
            * Scripts/bugzilla-tool:
            * Scripts/modules/logging.py:
    
    Canonical link: https://commits.webkit.org/42442@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@51013 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/modules/logging.py
+++ WebKitTools/Scripts/modules/logging.py
@@ -46,3 +46,38 @@ class tee:
     def write(self, string):
         for file in self.files:
             file.write(string)
+
+class OutputTee:
+    def __init__(self):
+        self._original_stdout = None
+        self._original_stderr = None
+        self._files_for_output = []
+
+    def add_log(self, path):
+        log_file = self._open_log_file(path)
+        self._files_for_output.append(log_file)
+        self._tee_outputs_to_files(self._files_for_output)
+        return log_file
+
+    def remove_log(self, log_file):
+        self._files_for_output.remove(log_file)
+        self._tee_outputs_to_files(self._files_for_output)
+        log_file.close()
+
+    @staticmethod
+    def _open_log_file(log_path):
+        (log_directory, log_name) = os.path.split(log_path)
+        if log_directory and not os.path.exists(log_directory):
+            os.makedirs(log_directory)
+        return open(log_path, 'a+')
+
+    def _tee_outputs_to_files(self, files):
+        if not self._original_stdout:
+            self._original_stdout = sys.stdout
+            self._original_stderr = sys.stderr
+        if files and len(files):
+            sys.stdout = tee(self._original_stdout, *files)
+            sys.stderr = tee(self._original_stderr, *files)
+        else:
+            sys.stdout = self._original_stdout
+            sys.stderr = self._original_stderr
