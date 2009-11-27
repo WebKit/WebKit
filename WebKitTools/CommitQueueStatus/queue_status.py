84e3dd13ec5505079bd8fb442c947b784f9d1862
Eric Seidel <eric@webkit.org>
2009-11-27 18:49:01 +0000

    2009-11-27  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Make commit-queue status not throw an exception where there is not
            status
            https://bugs.webkit.org/show_bug.cgi?id=31936
    
            We need to actually fetch the results of the query to see what's in the
            datastore.
    
            * CommitQueueStatus/queue_status.py:
    
    Canonical link: https://commits.webkit.org/42873@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@51444 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/CommitQueueStatus/queue_status.py
+++ WebKitTools/CommitQueueStatus/queue_status.py
@@ -66,9 +66,10 @@ class MainPage(webapp.RequestHandler):
 
 class PatchStatus(webapp.RequestHandler):
     def get(self, queue_name, attachment_id):
-        statuses = QueueStatus.all().filter('queue_name =', queue_name).filter('active_patch_id =', attachment_id).order('-date')
+        statuses = QueueStatus.all().filter('queue_name =', queue_name).filter('active_patch_id =', attachment_id).order('-date').fetch(1)
         if not statuses:
             self.error(404)
+            return
         self.response.out.write(statuses[0].message)
 
 
