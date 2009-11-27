4c17edf8efc9c8e15545d03b564735df28bfa587
Adam Barth <abarth@webkit.org>
2009-11-27 23:34:04 +0000

    2009-11-27  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            PatchStatus does not return status
            https://bugs.webkit.org/show_bug.cgi?id=31938
    
            We need to parse attachment_id as an int.  Otherwise, we can't find
            anything in the datastore.
    
            * CommitQueueStatus/queue_status.py:
    
    Canonical link: https://commits.webkit.org/42875@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@51446 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/CommitQueueStatus/queue_status.py
+++ WebKitTools/CommitQueueStatus/queue_status.py
@@ -66,7 +66,7 @@ class MainPage(webapp.RequestHandler):
 
 class PatchStatus(webapp.RequestHandler):
     def get(self, queue_name, attachment_id):
-        statuses = QueueStatus.all().filter('queue_name =', queue_name).filter('active_patch_id =', attachment_id).order('-date').fetch(1)
+        statuses = QueueStatus.all().filter('queue_name =', queue_name).filter('active_patch_id =', int(attachment_id)).order('-date').fetch(1)
         if not statuses:
             self.error(404)
             return
