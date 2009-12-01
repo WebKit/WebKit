73a86efb516b5b2c4d81936fc608f75cc99124c3
Adam Roben <aroben@apple.com>
2009-12-01 18:35:29 +0000

    Fix tests that use the TestNetscapePlugin in Debug_Internal builds
    Reviewed by Ada Chan.
    
    Fixes <http://webkit.org/b/32027> REGRESSION (r49705): Tests that use
    TestNetscapePlugin fail in Debug_Internal builds
    
    * DumpRenderTree/win/DumpRenderTree.cpp: Use the "_Debug" plugin
    directory in Debug and Debug_All builds, but not in Debug_Internal
    builds.
    
    Canonical link: https://commits.webkit.org/42973@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@51546 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/DumpRenderTree/win/DumpRenderTree.cpp
+++ WebKitTools/DumpRenderTree/win/DumpRenderTree.cpp
@@ -66,7 +66,7 @@
 
 using namespace std;
 
-#ifndef NDEBUG
+#if !defined(NDEBUG) && (!defined(DEBUG_INTERNAL) || defined(DEBUG_ALL))
 const LPWSTR TestPluginDir = L"TestNetscapePlugin_Debug";
 #else
 const LPWSTR TestPluginDir = L"TestNetscapePlugin";
