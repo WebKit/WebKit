59d449ecbc650f8da47915d1502d1f2c5674db34
Alexey Proskuryakov <ap@apple.com>
2009-12-04 00:07:11 +0000

    Reviewed by Darin Adler.
            https://bugs.webkit.org/show_bug.cgi?id=32132
            websocket/tests/simple.html fails on Windows
    
            * websocket/tests/script-tests/simple.js:
            * websocket/tests/simple-expected.txt:
            Make the test weaker - readyState could already grow by the time a posted event gets dispatched.
    
            * platform/win/Skipped: Un-skip the test.
    
    
    
    Canonical link: https://commits.webkit.org/43094@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@51667 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/websocket/tests/script-tests/simple.js
+++ LayoutTests/websocket/tests/script-tests/simple.js
@@ -14,21 +14,21 @@ function endTest()
 }
 
 var ws = new WebSocket("ws://localhost:8880/websocket/tests/simple");
-debug("Created a socket to '" + ws.URL + "'; readyState = " + ws.readyState + ".");
+debug("Created a socket to '" + ws.URL + "'; readyState " + ws.readyState + ".");
 
 ws.onopen = function()
 {
-    debug("Connected; readyState = " + ws.readyState + ".");
+    debug("Connected; readyState " + ((ws.readyState >= 0) ? ">= 0." : ws.readyState));
 };
 
 ws.onmessage = function(messageEvent)
 {
-    debug("Received: '" + messageEvent.data + "'; readyState = " + ws.readyState + ".");
+    debug("Received: '" + messageEvent.data + "'; readyState " + ((ws.readyState >= 1) ? ">= 1." : ws.readyState));
 };
 
 ws.onclose = function()
 {
-    debug("Closed; readyState = " + ws.readyState + ".");
+    debug("Closed; readyState " + ws.readyState + ".");
     endTest();
 };
 
