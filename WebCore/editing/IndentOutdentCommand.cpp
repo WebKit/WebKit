78a54fefd0a8d845367c25573d98f6b81b8675a3
Enrica Casucci <enrica@apple.com>
2009-12-04 19:49:37 +0000

    Mail.app crashes when indenting table pasted from Numbers.app. <rdar://problem/7209507> https://bugs.webkit.org/show_bug.cgi?id=32166
    Reviewed by John Sullivan.
    
    WebCore:
    
    Test: LayoutTests/editing/execCommand/indent-partial-table.html
    
    * editing/IndentOutdentCommand.cpp:
    (WebCore::IndentOutdentCommand::indentRegion): Added test to verify that the end of the paragraph
    after the selection has not been moved.
    
    LayoutTests:
    
    * editing/execCommand/indent-partial-table-expected.txt: Added.
    * editing/execCommand/indent-partial-table.html: Added.
    
    
    
    Canonical link: https://commits.webkit.org/43131@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@51704 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/editing/IndentOutdentCommand.cpp
+++ WebCore/editing/IndentOutdentCommand.cpp
@@ -153,6 +153,12 @@ void IndentOutdentCommand::indentRegion(const VisiblePosition& startOfSelection,
             blockquoteForNextIndent = 0;
         else
             indentIntoBlockquote(endOfCurrentParagraph, endOfNextParagraph, blockquoteForNextIndent);
+
+        // indentIntoBlockquote could move more than one paragraph if the paragraph
+        // is in a list item or a table. As a result, endAfterSelection could refer to a position
+        // no longer in the document.
+        if (endAfterSelection.isNotNull() && !endAfterSelection.deepEquivalent().node()->inDocument())
+            break;
         // Sanity check: Make sure our moveParagraph calls didn't remove endOfNextParagraph.deepEquivalent().node()
         // If somehow we did, return to prevent crashes.
         if (endOfNextParagraph.isNotNull() && !endOfNextParagraph.deepEquivalent().node()->inDocument()) {
