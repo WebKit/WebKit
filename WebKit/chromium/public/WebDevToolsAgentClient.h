865995d94cc4be7a2ef2c6669cabaa57380fdb04
Yury Semikhatsky <yurys@chromium.org>
2009-12-09 15:26:49 +0000

    2009-12-09  Yury Semikhatsky  <yurys@chromium.org>
            Unreviewed, build fix.
    
            Add return statement to default implementation of hostIdentifier method
            introduced in http://trac.webkit.org/changeset/51897
    
            * public/WebDevToolsAgentClient.h:
            (WebKit::WebDevToolsAgentClient::hostIdentifier):
    
    
    Canonical link: https://commits.webkit.org/43325@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@51900 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/chromium/public/WebDevToolsAgentClient.h
+++ WebKit/chromium/public/WebDevToolsAgentClient.h
@@ -51,7 +51,7 @@ public:
     virtual void forceRepaint() { }
 
     // Returns the identifier of the entity hosting this agent.
-    virtual int hostIdentifier() { }
+    virtual int hostIdentifier() { return -1; }
 
     // Notifies host upon runtime feature being enabled/disabled.
     virtual void runtimeFeatureStateChanged(const WebString& feature, bool enabled) { }
