d9f2a52bf8ecd734d7ad7d2c23b2604328abe5b9
Adam Barth <abarth@webkit.org>
2009-12-11 03:12:04 +0000

    2009-12-10  Adam Barth  <abarth@webkit.org>
            Reviewed by Darin Adler.
    
            Mixed content shouldn't trigger for plug-ins without URLs
            https://bugs.webkit.org/show_bug.cgi?id=32384
    
            * http/tests/security/mixedContent/empty-url-plugin-in-frame-expected.txt: Added.
            * http/tests/security/mixedContent/empty-url-plugin-in-frame.html: Added.
            * http/tests/security/mixedContent/resources/frame-with-empty-url-plugin.html: Added.
    2009-12-10  Adam Barth  <abarth@webkit.org>
    
            Reviewed by Darin Adler.
    
            Mixed content shouldn't trigger for plug-ins without URLs
            https://bugs.webkit.org/show_bug.cgi?id=32384
    
            These plug-ins cannot be controlled by active network attackers, so
            there's no reason to trigger a mixed content warning.
    
            Test: http/tests/security/mixedContent/empty-url-plugin-in-frame.html
    
            * loader/FrameLoader.cpp:
            (WebCore::FrameLoader::isMixedContent):
    
    Canonical link: https://commits.webkit.org/43402@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@51977 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/FrameLoader.cpp
+++ WebCore/loader/FrameLoader.cpp
@@ -1382,7 +1382,7 @@ bool FrameLoader::isMixedContent(SecurityOrigin* context, const KURL& url)
     if (context->protocol() != "https")
         return false;  // We only care about HTTPS security origins.
 
-    if (url.protocolIs("https") || url.protocolIs("about") || url.protocolIs("data"))
+    if (!url.isValid() || url.protocolIs("https") || url.protocolIs("about") || url.protocolIs("data"))
         return false;  // Loading these protocols is secure.
 
     return true;
