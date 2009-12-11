a35f99525274ae23284519996b85386933a1d330
Adam Roben <aroben@apple.com>
2009-12-11 19:45:59 +0000

    Windows build fix for systems with spaces in their paths
    * JavaScriptCore.vcproj/JavaScriptCore/build-generated-files.sh: Quote some paths.
    
    Canonical link: https://commits.webkit.org/43436@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@52011 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/JavaScriptCore.vcproj/JavaScriptCore/build-generated-files.sh
+++ JavaScriptCore/JavaScriptCore.vcproj/JavaScriptCore/build-generated-files.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 # Determine if we have QuartzCore so we can turn on
-if [ -f ${WEBKITLIBRARIESDIR}/include/QuartzCore/QuartzCore.h ]
+if [ -f "${WEBKITLIBRARIESDIR}/include/QuartzCore/QuartzCore.h" ]
 then
-	echo "#define QUARTZCORE_PRESENT 1" > ${WEBKITOUTPUTDIR}/Include/JavaScriptCore/QuartzCorePresent.h
+	echo "#define QUARTZCORE_PRESENT 1" > "${WEBKITOUTPUTDIR}/Include/JavaScriptCore/QuartzCorePresent.h"
 else
-	echo "#define QUARTZCORE_PRESENT 0" > ${WEBKITOUTPUTDIR}/Include/JavaScriptCore/QuartzCorePresent.h
+	echo "#define QUARTZCORE_PRESENT 0" > "${WEBKITOUTPUTDIR}/Include/JavaScriptCore/QuartzCorePresent.h"
 fi
 
 NUMCPUS=`../../../WebKitTools/Scripts/num-cpus`
