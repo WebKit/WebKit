6c0c14536e1f82e36e6b0915d1880f55cfc91aa8
Brady Eidson <beidson@apple.com>
2009-12-12 01:40:50 +0000

    Rubberstamped by Sam Weinig.
    https://webkit.org/b/22851 - Remove defunct FIXME.
    
    * html/HTMLPlugInElement.cpp:
    (WebCore::HTMLPlugInElement::HTMLPlugInElement): This flag was removed completely
      from the HTMLFrameOwnerElement chain of classes.
    
    
    
    Canonical link: https://commits.webkit.org/43459@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@52035 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/html/HTMLPlugInElement.cpp
+++ WebCore/html/HTMLPlugInElement.cpp
@@ -45,7 +45,6 @@ namespace WebCore {
 using namespace HTMLNames;
 
 HTMLPlugInElement::HTMLPlugInElement(const QualifiedName& tagName, Document* doc)
-    // FIXME: Always passing false as createdByParser is odd (see bug22851).
     : HTMLFrameOwnerElement(tagName, doc)
 #if ENABLE(NETSCAPE_PLUGIN_API)
     , m_NPObject(0)
