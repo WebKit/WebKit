8158d5216ffbc4e72975bc4889f0d222500571f9
Philippe Normand <philn@igalia.com>
2009-12-18 14:01:19 +0000

    2009-12-18  Philippe Normand  <pnormand@igalia.com>
            Reviewed by Xan Lopez.
    
            [GTK] segfault during controls initialization if not GTK theme is present.
            https://bugs.webkit.org/show_bug.cgi?id=32705
    
            Create empty images for the controls if no GTK theme is set.
    
            * platform/graphics/gtk/ImageGtk.cpp:
            (WebCore::getThemeIconFileName):
            (WebCore::loadImageFromFile):
    
    Canonical link: https://commits.webkit.org/43732@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@52309 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/gtk/ImageGtk.cpp
+++ WebCore/platform/graphics/gtk/ImageGtk.cpp
@@ -48,13 +48,19 @@ static CString getThemeIconFileName(const char* name, int size)
 {
     GtkIconInfo* iconInfo = gtk_icon_theme_lookup_icon(gtk_icon_theme_get_default(),
                                                        name, size, GTK_ICON_LOOKUP_NO_SVG);
+    // Try to fallback on MISSING_IMAGE.
     if (!iconInfo)
         iconInfo = gtk_icon_theme_lookup_icon(gtk_icon_theme_get_default(),
                                               GTK_STOCK_MISSING_IMAGE, size,
                                               GTK_ICON_LOOKUP_NO_SVG);
+    if (iconInfo) {
+        GOwnPtr<GtkIconInfo> info(iconInfo);
+        return CString(gtk_icon_info_get_filename(info.get()));
+    }
 
-    GOwnPtr<GtkIconInfo> info(iconInfo);
-    return CString(gtk_icon_info_get_filename(info.get()));
+    // No icon was found, this can happen if not GTK theme is set. In
+    // that case an empty Image will be created.
+    return CString();
 }
 
 static PassRefPtr<SharedBuffer> loadResourceSharedBuffer(CString name)
@@ -78,8 +84,10 @@ void BitmapImage::invalidatePlatformData()
 PassRefPtr<Image> loadImageFromFile(CString fileName)
 {
     RefPtr<BitmapImage> img = BitmapImage::create();
-    RefPtr<SharedBuffer> buffer = loadResourceSharedBuffer(fileName);
-    img->setData(buffer.release(), true);
+    if (!fileName.isNull()) {
+        RefPtr<SharedBuffer> buffer = loadResourceSharedBuffer(fileName);
+        img->setData(buffer.release(), true);
+    }
     return img.release();
 }
 
