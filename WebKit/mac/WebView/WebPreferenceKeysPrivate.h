cb13b4dd020aa26a8eca5fdb6ca30b9749ff3884
Sam Weinig <weinig@apple.com>
2009-12-18 22:44:16 +0000

    Fix preference key name.
    Reviewed by Anders Carlsson.
    
    * WebView/WebPreferenceKeysPrivate.h:
    
    
    
    Canonical link: https://commits.webkit.org/43767@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@52344 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/mac/WebView/WebPreferenceKeysPrivate.h
+++ WebKit/mac/WebView/WebPreferenceKeysPrivate.h
@@ -88,7 +88,7 @@
 #define WebKitShowDebugBordersPreferenceKey @"WebKitShowDebugBorders"
 #define WebKitShowRepaintCounterPreferenceKey @"WebKitShowRepaintCounter"
 #define WebKitWebGLEnabledPreferenceKey @"WebKitWebGLEnabled"
-#define WebKitUsesProxiedOpenPanelPreferenceKey @"WebKitUsesProxiedOpenPanelPreferenceKey"
+#define WebKitUsesProxiedOpenPanelPreferenceKey @"WebKitUsesProxiedOpenPanel"
 #define WebKitPluginAllowedRunTimePreferenceKey @"WebKitPluginAllowedRunTime"
 
 // These are private both because callers should be using the cover methods and because the
