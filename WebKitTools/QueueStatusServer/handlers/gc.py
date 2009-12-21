b26aa61fbcc21a796dc369a3e342c6d23bda31d8
Eric Seidel <eric@webkit.org>
2009-12-21 20:19:08 +0000

    2009-12-21  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Delete the boring "Empty queue" status messages in QueueStatusServer
            https://bugs.webkit.org/show_bug.cgi?id=32818
    
            * QueueStatusServer/app.yaml:
            * QueueStatusServer/cron.yaml:
            * QueueStatusServer/handlers/gc.py:
            * QueueStatusServer/main.py:
    
    Canonical link: https://commits.webkit.org/43866@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@52453 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/QueueStatusServer/handlers/gc.py
+++ WebKitTools/QueueStatusServer/handlers/gc.py
@@ -26,40 +26,19 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# Request a modern Django
-from google.appengine.dist import use_library
-use_library('django', '1.1')
-
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
 
-from handlers.dashboard import Dashboard
-from handlers.gc import GC
-from handlers.patch import Patch
-from handlers.patchstatus import PatchStatus
-from handlers.recentstatus import RecentStatus
-from handlers.showresults import ShowResults
-from handlers.statusbubble import StatusBubble
-from handlers.updatestatus import UpdateStatus
+from model.queuestatus import QueueStatus
 
-webapp.template.register_template_library('filters.webkit_extras')
 
-routes = [
-    ('/', RecentStatus),
-    ('/dashboard', Dashboard),
-    ('/gc', GC),
-    (r'/patch-status/(.*)/(.*)', PatchStatus),
-    (r'/patch/(.*)', Patch),
-    (r'/results/(.*)', ShowResults),
-    (r'/status-bubble/(.*)', StatusBubble),
-    (r'/queue-status/(.*)', RecentStatus),
-    ('/update-status', UpdateStatus),
-]
-
-application = webapp.WSGIApplication(routes, debug=True)
-
-def main():
-    run_wsgi_app(application)
-
-if __name__ == "__main__":
-    main()
+class GC(webapp.RequestHandler):
+    def get(self):
+        statuses = QueueStatus.all().order("-date")
+        seen_queues = set()
+        for status in statuses:
+            if status.active_patch_id or status.active_bug_id:
+                continue
+            if status.queue_name in seen_queues:
+                status.delete()
+            seen_queues.add(status.queue_name)
+        self.response.out.write("Done!")
