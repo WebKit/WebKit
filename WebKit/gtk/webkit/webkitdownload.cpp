372449a900314798888a9d8d3314f414990b86f6
Eric Seidel <eric@webkit.org>
2009-12-28 16:48:55 +0000

    2009-12-28  Estêvão Samuel Procópio  <tevaum@gmail.com>
            Reviewed by Gustavo Noronha Silva.
    
            Bug 32940: [GTK] Changing the download throttle conditions.
            https://bugs.webkit.org/show_bug.cgi?id=32716
    
            The WebKitDownload progress notification was taking long to
            update. This fix makes notification happens each 0.7 secs
            or when the progress ups in 1%.
    
            * WebKit/gtk/webkit/webkitdownload.cpp:
    
    
    Canonical link: https://commits.webkit.org/44010@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@52597 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/gtk/webkit/webkitdownload.cpp
+++ WebKit/gtk/webkit/webkitdownload.cpp
@@ -854,22 +854,24 @@ static void webkit_download_received_data(WebKitDownload* download, const gchar*
     if (priv->currentSize > webkit_download_get_total_size(download))
         g_object_notify(G_OBJECT(download), "total-size");
 
-    gdouble lastProgress = webkit_download_get_progress(download);
-
     // Throttle progress notification to not consume high amounts of
-    // CPU on fast links, except when the progress is >= 3%, or we
-    // reached the end.
+    // CPU on fast links, except when the last notification occured
+    // in more then 0.7 secs from now, or the last notified progress
+    // is passed in 1% or we reached the end.
+    static gdouble lastProgress = 0;
     static gdouble lastElapsed = 0;
     gdouble currentElapsed = g_timer_elapsed(priv->timer, NULL);
+    gdouble currentProgress = webkit_download_get_progress(download);
 
     if (lastElapsed
-        && (currentElapsed - lastElapsed) < 0.1
-        && (webkit_download_get_progress(download) - lastProgress) < 0.03
-        && webkit_download_get_progress(download) < 1.0) {
-        lastElapsed = currentElapsed;
+        && lastProgress
+        && (currentElapsed - lastElapsed) < 0.7
+        && (currentProgress - lastProgress) < 0.01
+        && currentProgress < 1.0) {
         return;
     }
     lastElapsed = currentElapsed;
+    lastProgress = currentProgress;
 
     g_object_notify(G_OBJECT(download), "progress");
 }
