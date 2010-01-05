3bedec09aecc1e6bc3fb8246476cec097715fd68
Chris Marrin <cmarrin@apple.com>
2010-01-05 23:42:03 +0000

    New test for texImage2d and texSubImage2d cases         New test for texImage2d using ImageData which tests rendering.         https://bugs.webkit.org/show_bug.cgi?id=31562         Also bug: https://bugs.webkit.org/show_bug.cgi?id=31493
    Canonical link: https://commits.webkit.org/44246@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@52834 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/canvas/webgl/script-tests/texImageTest.js
@@ -0,0 +1,28 @@
+description("Test of texImage2d and texSubImage2d");
+
+var context = create3DContext();
+var image = document.createElement("img");
+var video = document.createElement("video");
+var canvas2d = document.createElement("canvas");
+var context2d = canvas2d.getContext("2d");
+var imageData = context2d.createImageData(64, 64);
+var array = new WebGLUnsignedByteArray([ 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0, 0, 255 ]);
+
+shouldThrow("context.texImage2D(context.TEXTURE_2D)");
+shouldBeUndefined("context.texImage2D(context.TEXTURE_2D, 0, context.RGBA, 64, 64, 0, context.RGBA, context.UNSIGNED_BYTE, 0)");
+shouldThrow("context.texImage2D(context.TEXTURE_2D, 0, context.RGBA, 0, context.RGBA, context.UNSIGNED_BYTE, 0)");
+shouldBeUndefined("context.texImage2D(context.TEXTURE_2D, 0, context.RGBA, 2, 2, 0, context.RGBA, context.UNSIGNED_BYTE, array)");
+shouldBeUndefined("context.texImage2D(context.TEXTURE_2D, 0, imageData, true)");
+shouldBeUndefined("context.texImage2D(context.TEXTURE_2D, 0, image)");
+shouldBeUndefined("context.texImage2D(context.TEXTURE_2D, 0, canvas2d, false)");
+shouldBeUndefined("context.texImage2D(context.TEXTURE_2D, 0, video, true, false)");
+
+shouldThrow("context.texSubImage2D(context.TEXTURE_2D)");
+shouldBeUndefined("context.texSubImage2D(context.TEXTURE_2D, 0, 10, 20, 0, 0, context.RGBA, context.UNSIGNED_BYTE, 0)");
+shouldThrow("context.texSubImage2D(context.TEXTURE_2D, 0, 10, 20, 0, context.UNSIGNED_BYTE, 0)");
+shouldBeUndefined("context.texSubImage2D(context.TEXTURE_2D, 0, 10, 20, imageData, false)");
+shouldBeUndefined("context.texSubImage2D(context.TEXTURE_2D, 0, 10, 20, image)");
+shouldBeUndefined("context.texSubImage2D(context.TEXTURE_2D, 0, 10, 20, canvas2d, true)");
+shouldBeUndefined("context.texSubImage2D(context.TEXTURE_2D, 0, 10, 20, video, false, true)");
+
+successfullyParsed = true;
