281bafd9af99f67db09b5f10d51c73863243bf04
Adam Roben <aroben@apple.com>
2010-01-05 18:14:35 +0000

    Don't hang in WebKitAPITest if no messages have been posted
    Part of <http://webkit.org/b/33212> Small fixes/improvements to
    WebKitAPITest
    
    Reviewed by Sam Weinig.
    
    * WebKitAPITest/tests/WebViewDestruction.cpp:
    (WebKitAPITest::runMessagePump): Use PeekMessage instead of GetMessage
    to avoid hanging if no messages have been posted.
    
    Canonical link: https://commits.webkit.org/44216@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@52803 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/WebKitAPITest/tests/WebViewDestruction.cpp
+++ WebKitTools/WebKitAPITest/tests/WebViewDestruction.cpp
@@ -68,7 +68,7 @@ static void runMessagePump(DWORD timeoutMilliseconds)
     DWORD startTickCount = GetTickCount();
     MSG msg;
     BOOL result;
-    while ((result = GetMessage(&msg, 0, 0, 0)) && GetTickCount() - startTickCount <= timeoutMilliseconds) {
+    while ((result = PeekMessageW(&msg, 0, 0, 0, PM_REMOVE)) && GetTickCount() - startTickCount <= timeoutMilliseconds) {
         if (result == -1)
             break;
         TranslateMessage(&msg);
