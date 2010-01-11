9722ca8523238996cedff897bd39063b093f85a6
Oliver Hunt <oliver@apple.com>
2010-01-11 02:59:02 +0000

    Bad DOM performance in large SVG files https://bugs.webkit.org/show_bug.cgi?id=30055
    Reviewed by Nikolas Zimmerman
    
    Cull RenderPaths before passing on to the underlying graphics system.
    
    Canonical link: https://commits.webkit.org/44465@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@53057 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderPath.cpp
+++ WebCore/rendering/RenderPath.cpp
@@ -214,32 +214,36 @@ void RenderPath::paint(PaintInfo& paintInfo, int, int)
 {
     if (paintInfo.context->paintingDisabled() || style()->visibility() == HIDDEN || m_path.isEmpty())
         return;
-            
-    paintInfo.context->save();
-    paintInfo.context->concatCTM(localToParentTransform());
+    PaintInfo childPaintInfo(paintInfo);
+    childPaintInfo.context->save();
+    applyTransformToPaintInfo(childPaintInfo, m_localTransform);
+    FloatRect boundingBox = repaintRectInLocalCoordinates();
+    if (!boundingBox.intersects(childPaintInfo.rect)) {
+        childPaintInfo.context->restore();
+        return;
+    }
 
     SVGResourceFilter* filter = 0;
 
-    FloatRect boundingBox = repaintRectInLocalCoordinates();
-    if (paintInfo.phase == PaintPhaseForeground) {
-        PaintInfo savedInfo(paintInfo);
+    if (childPaintInfo.phase == PaintPhaseForeground) {
+        PaintInfo savedInfo(childPaintInfo);
 
-        if (prepareToRenderSVGContent(this, paintInfo, boundingBox, filter)) {
+        if (prepareToRenderSVGContent(this, childPaintInfo, boundingBox, filter)) {
             if (style()->svgStyle()->shapeRendering() == SR_CRISPEDGES)
-                paintInfo.context->setShouldAntialias(false);
-            fillAndStrokePath(m_path, paintInfo.context, style(), this);
+                childPaintInfo.context->setShouldAntialias(false);
+            fillAndStrokePath(m_path, childPaintInfo.context, style(), this);
 
             if (static_cast<SVGStyledElement*>(node())->supportsMarkers())
-                m_markerLayoutInfo.drawMarkers(paintInfo);
+                m_markerLayoutInfo.drawMarkers(childPaintInfo);
         }
-        finishRenderSVGContent(this, paintInfo, filter, savedInfo.context);
+        finishRenderSVGContent(this, childPaintInfo, filter, savedInfo.context);
     }
 
-    if ((paintInfo.phase == PaintPhaseOutline || paintInfo.phase == PaintPhaseSelfOutline) && style()->outlineWidth())
-        paintOutline(paintInfo.context, static_cast<int>(boundingBox.x()), static_cast<int>(boundingBox.y()),
+    if ((childPaintInfo.phase == PaintPhaseOutline || childPaintInfo.phase == PaintPhaseSelfOutline) && style()->outlineWidth())
+        paintOutline(childPaintInfo.context, static_cast<int>(boundingBox.x()), static_cast<int>(boundingBox.y()),
             static_cast<int>(boundingBox.width()), static_cast<int>(boundingBox.height()), style());
     
-    paintInfo.context->restore();
+    childPaintInfo.context->restore();
 }
 
 // This method is called from inside paintOutline() since we call paintOutline()
@@ -257,7 +261,7 @@ bool RenderPath::nodeAtFloatPoint(const HitTestRequest&, HitTestResult& result, 
     if (hitTestAction != HitTestForeground)
         return false;
 
-    FloatPoint localPoint = localToParentTransform().inverse().mapPoint(pointInParent);
+    FloatPoint localPoint = m_localTransform.inverse().mapPoint(pointInParent);
 
     PointerEventsHitRules hitRules(PointerEventsHitRules::SVG_PATH_HITTESTING, style()->pointerEvents());
 
