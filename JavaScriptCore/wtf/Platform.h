34f26728dde5ba81f524112af21a0b48c342652b
Csaba Osztrogonác <ossy@webkit.org>
2010-01-11 20:33:53 +0000

    [Qt] Enable JIT and YARR_JIT if (CPU(X86_64) && OS(LINUX) && GCC_VERSION >= 40100)
    Reviewed by Ariya Hidayat.
    
    * wtf/Platform.h:
    
    
    
    Canonical link: https://commits.webkit.org/44502@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@53094 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/Platform.h
+++ JavaScriptCore/wtf/Platform.h
@@ -852,6 +852,8 @@ on MinGW. See https://bugs.webkit.org/show_bug.cgi?id=29268 */
 #elif CPU(X86) && OS(LINUX) && GCC_VERSION >= 40100
     #define ENABLE_JIT 1
     #define WTF_USE_JIT_STUB_ARGUMENT_VA_LIST 1
+#elif CPU(X86_64) && OS(LINUX) && GCC_VERSION >= 40100
+    #define ENABLE_JIT 1
 #elif CPU(ARM_TRADITIONAL) && OS(LINUX)
     #define ENABLE_JIT 1
 #endif
@@ -908,6 +910,7 @@ on MinGW. See https://bugs.webkit.org/show_bug.cgi?id=29268 */
 #if (CPU(X86) && OS(WINDOWS) && COMPILER(MINGW) && GCC_VERSION >= 40100) \
     || (CPU(X86) && OS(WINDOWS) && COMPILER(MSVC)) \
     || (CPU(X86) && OS(LINUX) && GCC_VERSION >= 40100) \
+    || (CPU(X86_64) && OS(LINUX) && GCC_VERSION >= 40100) \
     || (CPU(ARM_TRADITIONAL) && OS(LINUX))
 #define ENABLE_YARR 1
 #define ENABLE_YARR_JIT 1
