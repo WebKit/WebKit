da52d8d622b3cd3d54ceb51879eb5c8a97c3859c
Eric Seidel <eric@webkit.org>
2010-01-12 21:15:28 +0000

    2010-01-12  Jungshik Shin  <jshin@chromium.org>
            Reviewed by Darin Adler
    
            https://bugs.webkit.org/show_bug.cgi?id=31597
    
            Make ICU's text breakiterator and string search use Chrome's UI
            language instead of 'en-US' or the OS UI language.
    
            As long as the webkit layout tests are run in en-US locale, there'd be
            no visible change.
    
            * platform/text/chromium/TextBreakIteratorInternalICUChromium.cpp:
            (UILanguage):
            (WebCore::currentSearchLocaleID):
            (WebCore::currentTextBreakLocaleID):
    
    Canonical link: https://commits.webkit.org/44567@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@53159 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/text/chromium/TextBreakIteratorInternalICUChromium.cpp
+++ WebCore/platform/text/chromium/TextBreakIteratorInternalICUChromium.cpp
@@ -22,18 +22,29 @@
 #include "config.h"
 #include "TextBreakIteratorInternalICU.h"
 
+#include "CString.h"
+#include "Language.h"
+#include "PlatformString.h"
+#include <wtf/StdLibExtras.h>
+
 namespace WebCore {
+
+static const char* UILanguage()
+{
+    // Chrome's UI language can be different from the OS UI language on Windows.
+    // We want to return Chrome's UI language here.
+    DEFINE_STATIC_LOCAL(CString, locale, (defaultLanguage().latin1()));
+    return locale.data();
+}
 
 const char* currentSearchLocaleID()
 {
-    // FIXME: Should use system locale.
-    return "";
+    return UILanguage();
 }
 
 const char* currentTextBreakLocaleID()
 {
-    // FIXME: Should use system locale.
-    return "en_us";
+    return UILanguage();
 }
 
 } // namespace WebCore
