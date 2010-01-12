6a130af5b5206add98ff4600648e125197dc1697
Gustavo Noronha Silva <kov@webkit.org>
2010-01-12 13:48:08 +0000

    Reviewed by Xan Lopez.
            [GTK] couple fixes for signal emissions, and property notifications
            https://bugs.webkit.org/show_bug.cgi?id=33428
    
            Fix commitedLoad being called where it should not, causing it to
            be called one time too many in some cases. We now match Qt's and
            Mac's behavior here.
    
            * WebCoreSupport/FrameLoaderClientGtk.cpp:
            (WebKit::FrameLoaderClient::finishedLoading):
            * tests/testloading.c:
            (load_error_status_changed_cb):
    
    Canonical link: https://commits.webkit.org/44546@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@53138 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
+++ WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
@@ -840,9 +840,10 @@ String FrameLoaderClient::generatedMIMETypeForURLScheme(const String&) const
 
 void FrameLoaderClient::finishedLoading(WebCore::DocumentLoader* documentLoader)
 {
-    if (!m_pluginView)
-        committedLoad(documentLoader, 0, 0);
-    else {
+    if (!m_pluginView) {
+        FrameLoader* loader = documentLoader->frameLoader();
+        loader->setEncoding(m_response.textEncodingName(), false);
+    } else {
         m_pluginView->didFinishLoading();
         m_pluginView = 0;
         m_hasSentResponseToPlugin = false;
