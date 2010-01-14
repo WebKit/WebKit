47ddb4da9c8c4c478652497b86d796907b7caba5
Adam Barth <abarth@webkit.org>
2010-01-14 00:55:17 +0000

    2010-01-13  Adam Barth  <abarth@webkit.org>
            Unreviewed.  Repeat the below for CloseBugForLandDiff.
    
            * Scripts/test-webkitpy:
            * Scripts/webkitpy/steps/closebugforlanddiff.py:
            * Scripts/webkitpy/steps/closebugforlanddiff_unittest.py: Added.
    
    Canonical link: https://commits.webkit.org/44625@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@53217 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/webkitpy/steps/closebugforlanddiff.py
+++ WebKitTools/Scripts/webkitpy/steps/closebugforlanddiff.py
@@ -41,7 +41,9 @@ class CloseBugForLandDiff(AbstractStep):
 
     def run(self, state):
         comment_text = bug_comment_from_commit_text(self._tool.scm(), state["commit_text"])
-        bug_id = state.get("bug_id") or state["patch"].bug_id()
+        bug_id = state.get("bug_id")
+        if not bug_id and state.get("patch"):
+            bug_id = state.get("patch").bug_id()
 
         if bug_id:
             log("Updating bug %s" % bug_id)
