ac6be69fa99e5c472ace13c4dface5af620884c6
Oliver Hunt <oliver@apple.com>
2010-01-16 00:16:23 +0000

    2010-01-15  Oliver Hunt  <oliver@apple.com>
            Reviewed by Sam Weinig.
    
            Bad DOM performance in large SVG files
            https://bugs.webkit.org/show_bug.cgi?id=30055
    
            Improve the performance of the early culling check by avoiding save/restore
            of graphics state in culled case.
    
            * rendering/RenderPath.cpp:
            (WebCore::RenderPath::paint):
    
    Canonical link: https://commits.webkit.org/44753@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@53349 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderPath.cpp
+++ WebCore/rendering/RenderPath.cpp
@@ -216,18 +216,17 @@ void RenderPath::paint(PaintInfo& paintInfo, int, int)
     if (paintInfo.context->paintingDisabled() || style()->visibility() == HIDDEN || m_path.isEmpty())
         return;
 
-    PaintInfo childPaintInfo(paintInfo);
-    childPaintInfo.context->save();
-    applyTransformToPaintInfo(childPaintInfo, m_localTransform);
     FloatRect boundingBox = repaintRectInLocalCoordinates();
+    FloatRect nonLocalBoundingBox = m_localTransform.mapRect(boundingBox);
     // FIXME: The empty rect check is to deal with incorrect initial clip in renderSubtreeToImage
     // unfortunately fixing that problem is fairly complex unless we were willing to just futz the
     // rect to something "close enough"
-    if (!boundingBox.intersects(childPaintInfo.rect) && !childPaintInfo.rect.isEmpty()) {
-        childPaintInfo.context->restore();
+    if (!nonLocalBoundingBox.intersects(paintInfo.rect) && !paintInfo.rect.isEmpty())
         return;
-    }
 
+    PaintInfo childPaintInfo(paintInfo);
+    childPaintInfo.context->save();
+    applyTransformToPaintInfo(childPaintInfo, m_localTransform);
     SVGResourceFilter* filter = 0;
 
     if (childPaintInfo.phase == PaintPhaseForeground) {
