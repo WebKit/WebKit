02754ba73b2e0642e8f579ca9bd84848981566f8
Dan Bernstein <mitz@apple.com>
2010-01-16 19:29:26 +0000

    <rdar://problem/7529519> DumpRenderTree crashes in makeLargeMallocFailSilently()
    Reviewed by Mark Rowe.
    
    * DumpRenderTree/mac/CheckedMalloc.cpp:
    (makeLargeMallocFailSilently): Temporarily allow writing to the default
    malloc zone structure while modifying it.
    
    
    
    Canonical link: https://commits.webkit.org/44766@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@53362 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/DumpRenderTree/mac/CheckedMalloc.cpp
+++ WebKitTools/DumpRenderTree/mac/CheckedMalloc.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005, 2006, 2007 Apple, Inc.  All rights reserved.
+ * Copyright (C) 2005, 2006, 2007, 2010 Apple Inc. All rights reserved.
  *           (C) 2007 Graham Dennis (graham.dennis@gmail.com)
  *           (C) 2007 Eric Seidel <eric@webkit.org>
  *
@@ -32,6 +32,7 @@
 #import "CheckedMalloc.h"
 
 #import <malloc/malloc.h>
+#import <sys/mman.h>
 
 static void* (*savedMalloc)(malloc_zone_t*, size_t);
 static void* (*savedRealloc)(malloc_zone_t*, void*, size_t);
@@ -53,8 +54,19 @@ static void* checkedRealloc(malloc_zone_t* zone, void* ptr, size_t size)
 void makeLargeMallocFailSilently()
 {
     malloc_zone_t* zone = malloc_default_zone();
+
+#if !defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD) && !defined(BUILDING_ON_SNOW_LEOPARD)
+    vm_address_t pageStart = reinterpret_cast<vm_address_t>(zone) & static_cast<vm_size_t>(~(getpagesize() - 1));
+    vm_size_t len = reinterpret_cast<vm_address_t>(zone) - pageStart + sizeof(malloc_zone_t);
+    mprotect(reinterpret_cast<void*>(pageStart), len, PROT_READ | PROT_WRITE);
+#endif
+
     savedMalloc = zone->malloc;
     savedRealloc = zone->realloc;
     zone->malloc = checkedMalloc;
     zone->realloc = checkedRealloc;
+
+#if !defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD) && !defined(BUILDING_ON_SNOW_LEOPARD)
+    mprotect(reinterpret_cast<void*>(pageStart), len, PROT_READ);
+#endif
 }
