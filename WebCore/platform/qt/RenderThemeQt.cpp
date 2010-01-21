2f6238c4dffccbeecc8ba7f6f260e7e3705b3b4f
Csaba Osztrogonác <ossy@webkit.org>
2010-01-21 18:39:32 +0000

    [Qt] Unreviewed buildfix after r53625.
    Cleanup MediaPlayer and MediaPlayerPrivateInterface to remove dead methods
    https://bugs.webkit.org/show_bug.cgi?id=30106
    
    * platform/qt/RenderThemeQt.cpp:
    (WebCore::RenderThemeQt::paintMediaSliderTrack):
    
    
    
    Canonical link: https://commits.webkit.org/45031@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@53632 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/qt/RenderThemeQt.cpp
+++ WebCore/platform/qt/RenderThemeQt.cpp
@@ -979,16 +979,6 @@ bool RenderThemeQt::paintMediaSliderTrack(RenderObject* o, const RenderObject::P
 
     paintMediaBackground(p.painter, r);
 
-    if (MediaPlayer* player = mediaElement->player()) {
-        if (player->totalBytesKnown()) {
-            float percentLoaded = static_cast<float>(player->bytesLoaded()) / player->totalBytes();
-
-            WorldMatrixTransformer transformer(p.painter, o, r);
-            p.painter->setBrush(getMediaControlForegroundColor());
-            p.painter->drawRect(0, 37, 100 * percentLoaded, 26);
-        }
-    }
-
     return false;
 }
 
