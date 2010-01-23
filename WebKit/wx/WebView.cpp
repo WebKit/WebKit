056197e2b176de1a9b9878e9248c4e2dcd5653c1
Kevin Ollivier <kevino@webkit.org>
2010-01-23 04:33:11 +0000

    Build fix after r53718.
    Canonical link: https://commits.webkit.org/45155@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@53756 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/wx/WebView.cpp
+++ WebKit/wx/WebView.cpp
@@ -821,11 +821,11 @@ void wxWebView::OnKeyEvents(wxKeyEvent& event)
             return;
         case WXK_PAGEUP:
         case WXK_NUMPAD_PAGEUP:
-            frame->view()->scrollBy(WebCore::IntSize(0, -frame->view()->visibleHeight() + WebCore::cAmountToKeepWhenPaging));
+            frame->view()->scrollBy(WebCore::IntSize(0, -frame->view()->visibleHeight() * WebCore::cFractionToStepWhenPaging));
             return;
         case WXK_PAGEDOWN:
         case WXK_NUMPAD_PAGEDOWN:
-            frame->view()->scrollBy(WebCore::IntSize(0, frame->view()->visibleHeight() - WebCore::cAmountToKeepWhenPaging));
+            frame->view()->scrollBy(WebCore::IntSize(0, frame->view()->visibleHeight() * WebCore::cFractionToStepWhenPaging));
             return;
         //These we don't want turning into char events, stuff 'em
         case WXK_ESCAPE:
