609c1c64068b8fde597091dbc22aee37dc0d4e58
Dan Bernstein <mitz@apple.com>
2010-01-24 01:50:42 +0000

    <rdar://problem/7569820> svg/custom/text-zoom.xhtml fails on Windows https://bugs.webkit.org/show_bug.cgi?id=34006
    Reviewed by Maciej Stachowiak.
    
    WebKit/win:
    
    * WebPreferences.cpp:
    (WebPreferences::initializeDefaultSettings): Added WebKitZoomsTextOnlyPreferenceKey
    with a default value of true.
    
    LayoutTests:
    
    * platform/win/svg/custom/text-zoom-expected.txt: Removed.
    
    
    
    Canonical link: https://commits.webkit.org/45172@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@53773 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/win/WebPreferences.cpp
+++ WebKit/win/WebPreferences.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006, 2007, 2008, 2009 Apple Inc.  All rights reserved.
+ * Copyright (C) 2006, 2007, 2008, 2009, 2010 Apple Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -212,6 +212,7 @@ void WebPreferences::initializeDefaultSettings()
     CFDictionaryAddValue(defaults, CFSTR(WebKitDatabasesEnabledPreferenceKey), kCFBooleanTrue);
     CFDictionaryAddValue(defaults, CFSTR(WebKitLocalStorageEnabledPreferenceKey), kCFBooleanTrue);
     CFDictionaryAddValue(defaults, CFSTR(WebKitExperimentalNotificationsEnabledPreferenceKey), kCFBooleanFalse);
+    CFDictionaryAddValue(defaults, CFSTR(WebKitZoomsTextOnlyPreferenceKey), kCFBooleanTrue);
     CFDictionaryAddValue(defaults, CFSTR(WebKitAllowAnimatedImagesPreferenceKey), kCFBooleanTrue);
     CFDictionaryAddValue(defaults, CFSTR(WebKitAllowAnimatedImageLoopingPreferenceKey), kCFBooleanTrue);
     CFDictionaryAddValue(defaults, CFSTR(WebKitDisplayImagesKey), kCFBooleanTrue);
