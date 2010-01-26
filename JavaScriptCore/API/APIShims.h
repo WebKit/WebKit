6fe320030f2b7f4f2e89d5163e041cfeed2f95da
Oliver Hunt <oliver@apple.com>
2010-01-26 19:22:51 +0000

    2010-01-26  Oliver Hunt  <oliver@apple.com>
            Reviewed by Geoffrey Garen.
    
            Using JavaScriptCore API with a webkit vended context can result in slow script dialog
            https://bugs.webkit.org/show_bug.cgi?id=34172
    
            Make the APIShim correctly increment and decrement the timeout
            entry counter.
    
            * API/APIShims.h:
            (JSC::APIEntryShimWithoutLock::APIEntryShimWithoutLock):
            (JSC::APIEntryShimWithoutLock::~APIEntryShimWithoutLock):
            (JSC::APICallbackShim::APICallbackShim):
            (JSC::APICallbackShim::~APICallbackShim):
    
    Canonical link: https://commits.webkit.org/45243@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@53860 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/API/APIShims.h
+++ JavaScriptCore/API/APIShims.h
@@ -34,18 +34,22 @@ namespace JSC {
 class APIEntryShimWithoutLock {
 protected:
     APIEntryShimWithoutLock(JSGlobalData* globalData, bool registerThread)
-        : m_entryIdentifierTable(setCurrentIdentifierTable(globalData->identifierTable))
+        : m_globalData(globalData)
+        , m_entryIdentifierTable(setCurrentIdentifierTable(globalData->identifierTable))
     {
         if (registerThread)
             globalData->heap.registerThread();
+        m_globalData->timeoutChecker.start();
     }
 
     ~APIEntryShimWithoutLock()
     {
+        m_globalData->timeoutChecker.stop();
         setCurrentIdentifierTable(m_entryIdentifierTable);
     }
 
 private:
+    JSGlobalData* m_globalData;
     IdentifierTable* m_entryIdentifierTable;
 };
 
@@ -76,10 +80,12 @@ public:
         , m_globalData(&exec->globalData())
     {
         resetCurrentIdentifierTable();
+        m_globalData->timeoutChecker.start();
     }
 
     ~APICallbackShim()
     {
+        m_globalData->timeoutChecker.stop();
         setCurrentIdentifierTable(m_globalData->identifierTable);
     }
 
