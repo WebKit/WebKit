d4c104ee5d012307ddc2a264e3f6aa811cec571d
Csaba Osztrogonác <ossy@webkit.org>
2010-01-28 23:50:29 +0000

    [Qt] Unreviewed buildfix after r54022.
    * bridge/qt/qt_instance.cpp:
    (JSC::Bindings::QtRuntimeObjectImp::createStructure):
    * bridge/qt/qt_pixmapruntime.cpp:
    (JSC::Bindings::QtPixmapRuntimeObjectImp::createStructure):
    * bridge/qt/qt_runtime.h:
    (JSC::Bindings::QtRuntimeMethod::createStructure):
    
    
    
    Canonical link: https://commits.webkit.org/45393@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@54025 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/bridge/qt/qt_pixmapruntime.cpp
+++ WebCore/bridge/qt/qt_pixmapruntime.cpp
@@ -162,7 +162,7 @@ public:
 
     static PassRefPtr<Structure> createStructure(JSValue prototype)
     {
-        return Structure::create(prototype, TypeInfo(ObjectType,  StructureFlags));
+        return Structure::create(prototype, TypeInfo(ObjectType,  StructureFlags), AnonymousSlotCount);
     }
 
 protected:
