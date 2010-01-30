7b05f00fd7cf59825ce27823b7142732498cc6f8
Chris Marrin <cmarrin@apple.com>
2010-01-30 02:17:20 +0000

    Fixed changed virtual function in GraphicsLayerCACF and call order issues         https://bugs.webkit.org/show_bug.cgi?id=34348
            The correct virtual function in GraphicsLayerCACF is now being
            called. We also fixed an issue in QTMovieWin where the size
            of the movie was not being set correctly because the call order
            was changed.
    
    
    
    Canonical link: https://commits.webkit.org/45461@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@54095 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/win/GraphicsLayerCACF.h
+++ WebCore/platform/graphics/win/GraphicsLayerCACF.h
@@ -82,7 +82,7 @@ public:
     virtual void setContentsRect(const IntRect&);
 
     virtual void setContentsToImage(Image*);
-    virtual void setContentsToVideo(PlatformLayer*);
+    virtual void setContentsToMedia(PlatformLayer*);
     
     virtual PlatformLayer* platformLayer() const;
 
@@ -115,7 +115,7 @@ private:
     void updateLayerBackgroundColor();
 
     void updateContentsImage();
-    void updateContentsVideo();
+    void updateContentsMedia();
     void updateContentsRect();
     void updateGeometryOrientation();
     
@@ -129,7 +129,7 @@ private:
     enum ContentsLayerPurpose {
         NoContentsLayer = 0,
         ContentsLayerForImage,
-        ContentsLayerForVideo
+        ContentsLayerForMedia
     };
     
     ContentsLayerPurpose m_contentsLayerPurpose;
