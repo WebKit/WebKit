1bae23752985c8ce45e66a5b35733a26fa787f82
Eric Seidel <eric@webkit.org>
2010-02-02 12:10:54 +0000

    2010-02-02  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Eric Seidel.
    
            [BREWMP] Port LocalizedStrings
            https://bugs.webkit.org/show_bug.cgi?id=34257
    
            Port LocalizedStrings to BREWMP.
    
            * platform/brew/LocalizedStringsBrew.cpp: Added.
            (WebCore::submitButtonDefaultLabel):
            (WebCore::inputElementAltText):
            (WebCore::resetButtonDefaultLabel):
            (WebCore::defaultLanguage):
            (WebCore::searchableIndexIntroduction):
            (WebCore::fileButtonChooseFileLabel):
            (WebCore::fileButtonNoFileSelectedLabel):
            (WebCore::contextMenuItemTagOpenLinkInNewWindow):
            (WebCore::contextMenuItemTagDownloadLinkToDisk):
            (WebCore::contextMenuItemTagCopyLinkToClipboard):
            (WebCore::contextMenuItemTagOpenImageInNewWindow):
            (WebCore::contextMenuItemTagDownloadImageToDisk):
            (WebCore::contextMenuItemTagCopyImageToClipboard):
            (WebCore::contextMenuItemTagOpenFrameInNewWindow):
            (WebCore::contextMenuItemTagCopy):
            (WebCore::contextMenuItemTagGoBack):
            (WebCore::contextMenuItemTagGoForward):
            (WebCore::contextMenuItemTagStop):
            (WebCore::contextMenuItemTagReload):
            (WebCore::contextMenuItemTagCut):
            (WebCore::contextMenuItemTagPaste):
            (WebCore::contextMenuItemTagNoGuessesFound):
            (WebCore::contextMenuItemTagIgnoreSpelling):
            (WebCore::contextMenuItemTagLearnSpelling):
            (WebCore::contextMenuItemTagSearchWeb):
            (WebCore::contextMenuItemTagLookUpInDictionary):
            (WebCore::contextMenuItemTagOpenLink):
            (WebCore::contextMenuItemTagIgnoreGrammar):
            (WebCore::contextMenuItemTagSpellingMenu):
            (WebCore::contextMenuItemTagShowSpellingPanel):
            (WebCore::contextMenuItemTagCheckSpelling):
            (WebCore::contextMenuItemTagCheckSpellingWhileTyping):
            (WebCore::contextMenuItemTagCheckGrammarWithSpelling):
            (WebCore::contextMenuItemTagFontMenu):
            (WebCore::contextMenuItemTagBold):
            (WebCore::contextMenuItemTagItalic):
            (WebCore::contextMenuItemTagUnderline):
            (WebCore::contextMenuItemTagOutline):
            (WebCore::contextMenuItemTagWritingDirectionMenu):
            (WebCore::contextMenuItemTagDefaultDirection):
            (WebCore::contextMenuItemTagLeftToRight):
            (WebCore::contextMenuItemTagRightToLeft):
            (WebCore::contextMenuItemTagInspectElement):
            (WebCore::searchMenuNoRecentSearchesText):
            (WebCore::searchMenuRecentSearchesText):
            (WebCore::searchMenuClearRecentSearchesText):
            (WebCore::unknownFileSizeText):
            (WebCore::AXWebAreaText):
            (WebCore::AXLinkText):
            (WebCore::AXListMarkerText):
            (WebCore::AXImageMapText):
            (WebCore::AXHeadingText):
            (WebCore::imageTitle):
            (WebCore::contextMenuItemTagTextDirectionMenu):
            (WebCore::AXButtonActionVerb):
            (WebCore::AXTextFieldActionVerb):
            (WebCore::AXRadioButtonActionVerb):
            (WebCore::AXCheckedCheckBoxActionVerb):
            (WebCore::AXUncheckedCheckBoxActionVerb):
            (WebCore::AXLinkActionVerb):
            (WebCore::AXMenuListPopupActionVerb):
            (WebCore::AXMenuListActionVerb):
            (WebCore::AXDefinitionListTermText):
            (WebCore::AXDefinitionListDefinitionText):
            (WebCore::validationMessageValueMissingText):
            (WebCore::validationMessageTypeMismatchText):
            (WebCore::validationMessagePatternMismatchText):
            (WebCore::validationMessageTooLongText):
            (WebCore::validationMessageRangeUnderflowText):
            (WebCore::validationMessageRangeOverflowText):
            (WebCore::validationMessageStepMismatchText):
    
    Canonical link: https://commits.webkit.org/45569@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@54225 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/platform/brew/LocalizedStringsBrew.cpp
@@ -0,0 +1,397 @@
+/*
+ * Copyright (C) 2009 Company 100, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "LocalizedStrings.h"
+
+#include "NotImplemented.h"
+#include "PlatformString.h"
+
+namespace WebCore {
+
+String submitButtonDefaultLabel()
+{
+    return "Submit";
+}
+
+String inputElementAltText()
+{
+    return String();
+}
+
+String resetButtonDefaultLabel()
+{
+    return "Reset";
+}
+
+String defaultLanguage()
+{
+    return "en";
+}
+
+String searchableIndexIntroduction()
+{
+    return "Searchable Index";
+}
+
+String fileButtonChooseFileLabel()
+{
+    return "Choose File";
+}
+
+String fileButtonNoFileSelectedLabel()
+{
+    return "No file selected";
+}
+
+String contextMenuItemTagOpenLinkInNewWindow()
+{
+    return "Open in new tab";
+}
+
+String contextMenuItemTagDownloadLinkToDisk()
+{
+    return "Download link to disk";
+}
+
+String contextMenuItemTagCopyLinkToClipboard()
+{
+    return "Copy link to clipboard";
+}
+
+String contextMenuItemTagOpenImageInNewWindow()
+{
+    return "Open image in new window";
+}
+
+String contextMenuItemTagDownloadImageToDisk()
+{
+    return "Download image to disk";
+}
+
+String contextMenuItemTagCopyImageToClipboard()
+{
+    return "Copy image to clipboard";
+}
+
+String contextMenuItemTagOpenFrameInNewWindow()
+{
+    return "Open frame in new window";
+}
+
+String contextMenuItemTagCopy()
+{
+    return "Copy";
+}
+
+String contextMenuItemTagGoBack()
+{
+    return "Go back";
+}
+
+String contextMenuItemTagGoForward()
+{
+    return "Go forward";
+}
+
+String contextMenuItemTagStop()
+{
+    return "Stop";
+}
+
+String contextMenuItemTagReload()
+{
+    return "Reload";
+}
+
+String contextMenuItemTagCut()
+{
+    return "Cut";
+}
+
+String contextMenuItemTagPaste()
+{
+    return "Paste";
+}
+
+String contextMenuItemTagNoGuessesFound()
+{
+    return "No guesses found";
+}
+
+String contextMenuItemTagIgnoreSpelling()
+{
+    return "Ignore spelling";
+}
+
+String contextMenuItemTagLearnSpelling()
+{
+    return "Learn spelling";
+}
+
+String contextMenuItemTagSearchWeb()
+{
+    return "Search web";
+}
+
+String contextMenuItemTagLookUpInDictionary()
+{
+    return "Lookup in dictionary";
+}
+
+String contextMenuItemTagOpenLink()
+{
+    return "Open link";
+}
+
+String contextMenuItemTagIgnoreGrammar()
+{
+    return "Ignore grammar";
+}
+
+String contextMenuItemTagSpellingMenu()
+{
+    return "Spelling menu";
+}
+
+String contextMenuItemTagShowSpellingPanel(bool show)
+{
+    return "Show spelling panel";
+}
+
+String contextMenuItemTagCheckSpelling()
+{
+    return "Check spelling";
+}
+
+String contextMenuItemTagCheckSpellingWhileTyping()
+{
+    return "Check spelling while typing";
+}
+
+String contextMenuItemTagCheckGrammarWithSpelling()
+{
+    return "Check for grammar with spelling";
+}
+
+String contextMenuItemTagFontMenu()
+{
+    return "Font menu";
+}
+
+String contextMenuItemTagBold()
+{
+    return "Bold";
+}
+
+String contextMenuItemTagItalic()
+{
+    return "Italic";
+}
+
+String contextMenuItemTagUnderline()
+{
+    return "Underline";
+}
+
+String contextMenuItemTagOutline()
+{
+    return "Outline";
+}
+
+String contextMenuItemTagWritingDirectionMenu()
+{
+    return "Writing direction menu";
+}
+
+String contextMenuItemTagDefaultDirection()
+{
+    return "Default direction";
+}
+
+String contextMenuItemTagLeftToRight()
+{
+    return "Left to right";
+}
+
+String contextMenuItemTagRightToLeft()
+{
+    return "Right to left";
+}
+
+String contextMenuItemTagInspectElement()
+{
+    return "Inspect";
+}
+
+String searchMenuNoRecentSearchesText()
+{
+    return "No recent text searches";
+}
+
+String searchMenuRecentSearchesText()
+{
+    return "Recent text searches";
+}
+
+String searchMenuClearRecentSearchesText()
+{
+    return "Clear recent text searches";
+}
+
+String unknownFileSizeText()
+{
+    return "Unknown";
+}
+
+String AXWebAreaText()
+{
+    return String();
+}
+
+String AXLinkText()
+{
+    return String();
+}
+
+String AXListMarkerText()
+{
+    return String();
+}
+
+String AXImageMapText()
+{
+    return String();
+}
+
+String AXHeadingText()
+{
+    return String();
+}
+
+String imageTitle(const String& filename, const IntSize& size)
+{
+    return String(filename);
+}
+
+String contextMenuItemTagTextDirectionMenu()
+{
+    return String();
+}
+
+String AXButtonActionVerb()
+{
+    return String();
+}
+
+String AXTextFieldActionVerb()
+{
+    return String();
+}
+
+String AXRadioButtonActionVerb()
+{
+    return String();
+}
+
+String AXCheckedCheckBoxActionVerb()
+{
+    return String();
+}
+
+String AXUncheckedCheckBoxActionVerb()
+{
+    return String();
+}
+
+String AXLinkActionVerb()
+{
+    return String();
+}
+
+String AXMenuListPopupActionVerb()
+{
+    return String();
+}
+
+String AXMenuListActionVerb()
+{
+    return String();
+}
+
+String AXDefinitionListTermText()
+{
+    return String();
+}
+
+String AXDefinitionListDefinitionText()
+{
+    return String();
+}
+
+String validationMessageValueMissingText()
+{
+    notImplemented();
+    return String();
+}
+
+String validationMessageTypeMismatchText()
+{
+    notImplemented();
+    return String();
+}
+
+String validationMessagePatternMismatchText()
+{
+    notImplemented();
+    return String();
+}
+
+String validationMessageTooLongText()
+{
+    notImplemented();
+    return String();
+}
+
+String validationMessageRangeUnderflowText()
+{
+    notImplemented();
+    return String();
+}
+
+String validationMessageRangeOverflowText()
+{
+    notImplemented();
+    return String();
+}
+
+String validationMessageStepMismatchText()
+{
+    notImplemented();
+    return String();
+}
+
+} // namespace WebCore
+
