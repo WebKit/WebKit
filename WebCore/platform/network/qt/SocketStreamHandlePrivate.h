1ede92df973101509d446a52b365995e135ddc12
Yael Aharon <yael.aharon@nokia.com>
2010-02-03 22:00:21 +0000

    [Qt] WebSockets : Buffer the data in WebKit instead of QtNetwork https://bugs.webkit.org/show_bug.cgi?id=34425
    Reviewed by Kenneth Rohde Christiansen.
    
    Reverting r54279, it was a misunderstanding.
    
    * platform/network/qt/SocketStreamHandlePrivate.h:
    * platform/network/qt/SocketStreamHandleQt.cpp:
    
    
    Canonical link: https://commits.webkit.org/45641@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@54298 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/network/qt/SocketStreamHandlePrivate.h
+++ WebCore/platform/network/qt/SocketStreamHandlePrivate.h
@@ -55,7 +55,6 @@ public slots:
     int send(const char* data, int len);
     void close();
     void socketSentdata();
-    void socketBytesWritten(qint64);
     void socketClosed();
     void socketError(QAbstractSocket::SocketError);
     void socketClosedCallback();
@@ -66,7 +65,6 @@ public slots:
 public:
     QTcpSocket* m_socket;
     SocketStreamHandle* m_streamHandle;
-    QByteArray m_data;
 };
 
 }
