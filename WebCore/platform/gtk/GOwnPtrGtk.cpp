7b701238e8073d98e7f32fe2439fdfd8cfebf897
Christian Dywan <christian@webkit.org>
2010-02-04 22:50:59 +0000

    2010-02-04  Christian Dywan  <christian@twotoasts.de>
            Rubber-stamped by Gustavo Noronha Silva.
    
            Add ENABLE(VIDEO) guards around freeOwnedGPtr<GstElement> implementation.
    
            * platform/gtk/GOwnPtrGtk.cpp:
    
    Canonical link: https://commits.webkit.org/45706@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@54374 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/gtk/GOwnPtrGtk.cpp
+++ WebCore/platform/gtk/GOwnPtrGtk.cpp
@@ -20,7 +20,9 @@
 #include "config.h"
 #include "GOwnPtrGtk.h"
 
+#if ENABLE(VIDEO)
 #include <gst/gstelement.h>
+#endif
 #include <libsoup/soup-uri.h>
 
 namespace WTF {
@@ -31,10 +33,12 @@ template <> void freeOwnedGPtr<SoupURI>(SoupURI* ptr)
         soup_uri_free(ptr);
 }
 
+#if ENABLE(VIDEO)
 template <> void freeOwnedGPtr<GstElement>(GstElement* ptr)
 {
     if (ptr)
         gst_object_unref(ptr);
 }
+#endif
 
 }
