43fe921544a6c2b0da6cfdbececfec37780e574e
Dirk Pranke <dpranke@chromium.org>
2010-02-04 07:54:37 +0000

    2010-02-03  Dirk Pranke  <dpranke@chromium.org>
            Rubber-stamped by Eric Seidel.
    
            Change "the Chromium name" to "the name of Google Inc." in the licenses
    
            https://bugs.webkit.org/show_bug.cgi?id=34511
    
            * Scripts/webkitpy/layout_tests/layout_package/json_layout_results_generator.py:
            * Scripts/webkitpy/layout_tests/layout_package/json_results_generator.py:
            * Scripts/webkitpy/layout_tests/layout_package/metered_stream.py:
            * Scripts/webkitpy/layout_tests/layout_package/test_expectations.py:
            * Scripts/webkitpy/layout_tests/layout_package/test_failures.py:
            * Scripts/webkitpy/layout_tests/layout_package/test_files.py:
            * Scripts/webkitpy/layout_tests/layout_package/test_shell_thread.py:
            * Scripts/webkitpy/layout_tests/port/__init__.py:
            * Scripts/webkitpy/layout_tests/port/apache_http_server.py:
            * Scripts/webkitpy/layout_tests/port/chromium_linux.py:
            * Scripts/webkitpy/layout_tests/port/chromium_mac.py:
            * Scripts/webkitpy/layout_tests/port/chromium_win.py:
            * Scripts/webkitpy/layout_tests/port/http_server.py:
            * Scripts/webkitpy/layout_tests/port/http_server_base.py:
            * Scripts/webkitpy/layout_tests/port/path_utils.py:
            * Scripts/webkitpy/layout_tests/port/websocket_server.py:
            * Scripts/webkitpy/layout_tests/rebaseline_chromium_webkit_tests.py:
            * Scripts/webkitpy/layout_tests/run_chromium_webkit_tests.py:
            * Scripts/webkitpy/layout_tests/test_types/fuzzy_image_diff.py:
            * Scripts/webkitpy/layout_tests/test_types/image_diff.py:
            * Scripts/webkitpy/layout_tests/test_types/test_type_base.py:
            * Scripts/webkitpy/layout_tests/test_types/text_diff.py:
    
    
    
    
    Canonical link: https://commits.webkit.org/45664@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@54328 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/webkitpy/layout_tests/port/websocket_server.py
+++ WebKitTools/Scripts/webkitpy/layout_tests/port/websocket_server.py
@@ -11,7 +11,7 @@
 # copyright notice, this list of conditions and the following disclaimer
 # in the documentation and/or other materials provided with the
 # distribution.
-#     * Neither the Chromium name nor the names of its
+#     * Neither the name of Google Inc. nor the names of its
 # contributors may be used to endorse or promote products derived from
 # this software without specific prior written permission.
 #
