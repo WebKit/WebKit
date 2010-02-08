e7e93f6ba85e67ee6d7a0290e98b98af7c570bc9
Simon Hausmann <hausmann@webkit.org>
2010-02-08 09:33:12 +0000

    Unreviewed RVCT build fix.
    Similar to r54391, don't import the cmath functions from std:: for RVCT.
    
    * wtf/MathExtras.h:
    
    Canonical link: https://commits.webkit.org/45790@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@54476 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/MathExtras.h
+++ JavaScriptCore/wtf/MathExtras.h
@@ -186,7 +186,7 @@ inline float deg2turn(float d) { return d / 360.0f; }
 inline float rad2grad(float r) { return r * 200.0f / piFloat; }
 inline float grad2rad(float g) { return g * piFloat / 200.0f; }
 
-#if !COMPILER(MSVC)
+#if !COMPILER(MSVC) && !COMPILER(RVCT)
 using std::isfinite;
 using std::isinf;
 using std::isnan;
