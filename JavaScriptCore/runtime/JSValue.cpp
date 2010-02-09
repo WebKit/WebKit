11639778b69dbc5ff4739158b2906fbebffa790d
Eric Seidel <eric@webkit.org>
2010-02-09 09:51:35 +0000

    2010-02-09  Janne Koskinen  <janne.p.koskinen@digia.com>
            Reviewed by Laszlo Gombos.
    
            [Qt] use nanval() for Symbian as nonInlineNaN
            https://bugs.webkit.org/show_bug.cgi?id=34170
    
            numeric_limits<double>::quiet_NaN is broken in Symbian
            causing NaN to be evaluated as a number.
    
            * runtime/JSValue.cpp:
            (JSC::nonInlineNaN):
    
    Canonical link: https://commits.webkit.org/45852@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@54539 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/runtime/JSValue.cpp
+++ JavaScriptCore/runtime/JSValue.cpp
@@ -174,7 +174,11 @@ uint32_t toUInt32SlowCase(double d, bool& ok)
 
 NEVER_INLINE double nonInlineNaN()
 {
+#if OS(SYMBIAN)
+    return nanval();
+#else
     return std::numeric_limits<double>::quiet_NaN();
+#endif
 }
 
 } // namespace JSC
