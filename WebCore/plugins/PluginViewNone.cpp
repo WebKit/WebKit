18c166c719c7e91f6c138736fb16647da68aade0
Eric Seidel <eric@webkit.org>
2010-02-10 22:01:03 +0000

    2010-02-10  Eric Seidel  <eric@webkit.org>
            No review, Mac build fix.
    
            Crash in Flash at http://www.cctv.com/
            https://bugs.webkit.org/show_bug.cgi?id=34673
    
            * plugins/PluginViewNone.cpp:
            (WebCore::PluginView::getValueStatic):
    
    Canonical link: https://commits.webkit.org/45933@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@54622 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/plugins/PluginViewNone.cpp
+++ WebCore/plugins/PluginViewNone.cpp
@@ -73,7 +73,7 @@ NPError PluginView::getValue(NPNVariable, void*)
 }
 
 #if ENABLE(NETSCAPE_PLUGIN_API)
-NPError PluginView::getValueStatic(NPNVariable variable, void* value)
+NPError PluginView::getValueStatic(NPNVariable, void*)
 {
     return 0;
 }
