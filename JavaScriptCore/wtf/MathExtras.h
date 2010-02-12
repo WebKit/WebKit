ad32d96dabc9e21a3bd9de6eb87f9684c028b4c4
Janne Koskinen <janne.p.koskinen@digia.com>
2010-02-12 13:48:54 +0000

    Don't import the cmath functions from std:: for WINSCW.
    Patch by Janne Koskinen <janne.p.koskinen@digia.com> on 2010-02-12
    Reviewed by Simon Hausmann.
    
    * wtf/MathExtras.h:
    
    Canonical link: https://commits.webkit.org/46032@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@54722 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/MathExtras.h
+++ JavaScriptCore/wtf/MathExtras.h
@@ -186,7 +186,7 @@ inline float deg2turn(float d) { return d / 360.0f; }
 inline float rad2grad(float r) { return r * 200.0f / piFloat; }
 inline float grad2rad(float g) { return g * piFloat / 200.0f; }
 
-#if !COMPILER(MSVC) && !COMPILER(RVCT) && !OS(ANDROID)
+#if !COMPILER(MSVC) && !COMPILER(RVCT) && !OS(ANDROID) && !COMPILER(WINSCW)
 using std::isfinite;
 using std::isinf;
 using std::isnan;
