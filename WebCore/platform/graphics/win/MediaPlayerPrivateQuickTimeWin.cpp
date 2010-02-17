6234cf59d39ab1601f159c8e61c1ee032b58a22c
Brian Weinstein <bweinstein@apple.com>
2010-02-17 04:55:21 +0000

    Try using m_qtVideoLayer instead of platformLayer() to fix the build
    Canonical link: https://commits.webkit.org/46175@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@54868 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeWin.cpp
+++ WebCore/platform/graphics/win/MediaPlayerPrivateQuickTimeWin.cpp
@@ -863,7 +863,7 @@ void MediaPlayerPrivate::acceleratedRenderingStateChanged()
 
 void MediaPlayerPrivate::notifySyncRequired(const GraphicsLayer*)
 {
-    GraphicsLayerCACF* videoGraphicsLayer = static_cast<GraphicsLayerCACF*>(platformLayer());
+    GraphicsLayerCACF* videoGraphicsLayer = static_cast<GraphicsLayerCACF*>(m_qtVideoLayer);
     if (videoGraphicsLayer)
         videoGraphicsLayer->notifySyncRequired();
  }
