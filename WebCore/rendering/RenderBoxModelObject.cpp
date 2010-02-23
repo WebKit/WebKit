4869785c656c31671d923fb50ddf29cba1173d89
Eric Seidel <eric@webkit.org>
2010-02-23 12:04:39 +0000

    2010-02-23  Yuta Kitamura  <yutak@chromium.org>
            Reviewed by Dan Bernstein.
    
            Fix alignment of vertical-align: text-bottom inside an inline-block.
    
            This patch is based on a fix provided by Takahito Hirano.
    
            display: inline-block + vertical-align: text-bottom causes misalignment.
            https://bugs.webkit.org/show_bug.cgi?id=30846
    
            * fast/inline-block/inline-block-vertical-align-2.html: Added.
            * fast/inline-block/inline-block-vertical-align-2-expected.txt: Added.
    2010-02-23  Yuta Kitamura  <yutak@chromium.org>
    
            Reviewed by Dan Bernstein.
    
            Fix alignment of vertical-align: text-bottom inside an inline-block.
    
            This patch is based on a fix provided by Takahito Hirano.
    
            display: inline-block + vertical-align: text-bottom causes misalignment.
            https://bugs.webkit.org/show_bug.cgi?id=30846
    
            Test: fast/inline-block/inline-block-vertical-align-2.html
    
            * rendering/RenderBoxModelObject.cpp:
            (WebCore::RenderBoxModelObject::verticalPosition): Fixed vpos calculation.
            We cannot cut corners for inline blocks, since they may have their own
            text metrics.
    
    Canonical link: https://commits.webkit.org/46443@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@55141 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderBoxModelObject.cpp
+++ WebCore/rendering/RenderBoxModelObject.cpp
@@ -793,7 +793,8 @@ int RenderBoxModelObject::verticalPosition(bool firstLine) const
             vpos += -static_cast<int>(f.xHeight() / 2) - lineHeight(firstLine) / 2 + baselinePosition(firstLine);
         else if (va == TEXT_BOTTOM) {
             vpos += f.descent();
-            if (!isReplaced()) // lineHeight - baselinePosition is always 0 for replaced elements, so don't bother wasting time in that case.
+            // lineHeight - baselinePosition is always 0 for replaced elements (except inline blocks), so don't bother wasting time in that case.
+            if (!isReplaced() || style()->display() == INLINE_BLOCK)
                 vpos -= (lineHeight(firstLine) - baselinePosition(firstLine));
         } else if (va == BASELINE_MIDDLE)
             vpos += -lineHeight(firstLine) / 2 + baselinePosition(firstLine);
