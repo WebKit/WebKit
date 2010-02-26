e53e19347517ce76faae9fe5594532d49b4e2f23
Shinichiro Hamaji <hamaji@chromium.org>
2010-02-26 10:40:17 +0000

    2010-02-26  Shinichiro Hamaji  <hamaji@chromium.org>
            Reviewed by Eric Seidel.
    
            Refine LayoutTests/printing/css2.1 with numberOfPages
            https://bugs.webkit.org/show_bug.cgi?id=35334
    
            Small refinements were done in paged-media-test-utils.js.
    
            Note that WebKit doesn't pass page-break-after-003 for now.
    
            * printing/css2.1/README.txt:
            * printing/css2.1/page-break-after-000-expected.txt:
            * printing/css2.1/page-break-after-000.html:
            * printing/css2.1/page-break-after-001-expected.txt:
            * printing/css2.1/page-break-after-001.html:
            * printing/css2.1/page-break-after-002-expected.txt:
            * printing/css2.1/page-break-after-002.html:
            * printing/css2.1/page-break-after-003-expected.txt: Added.
            * printing/css2.1/page-break-after-003.html: Added.
            * printing/css2.1/page-break-after-004-expected.txt:
            * printing/css2.1/page-break-after-004.html:
            * printing/css2.1/page-break-before-000-expected.txt:
            * printing/css2.1/page-break-before-000.html:
            * printing/css2.1/page-break-before-001-expected.txt:
            * printing/css2.1/page-break-before-001.html:
            * printing/css2.1/page-break-before-002-expected.txt:
            * printing/css2.1/page-break-before-002.html:
            * printing/css2.1/page-break-inside-000-expected.txt:
            * printing/css2.1/page-break-inside-000.html:
            * printing/resources/paged-media-test-utils.js:
            (appendResult):
            (pageNumberForElementShouldBe):
            (numberOfPagesShouldBe):
            (runPrintingTest):
    
    Canonical link: https://commits.webkit.org/46576@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@55275 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/printing/css2.1/README.txt
+++ LayoutTests/printing/css2.1/README.txt
@@ -10,12 +10,8 @@ We need the following modifications to add W3C's test into this directory:
 - Eliminate tabs.
 - Add dumpAsText() call.
 - Add test() function and implement it using layoutTestController functions.
+- Call test() function using runPrintintTest(test) from body's onload.
 - Add id attributes for elements in question.
-- Add <div id="results"> to report the test results.
-
-FIXME: We have no way to get the number of last page for now.
-       Though page-break-before-003 is actually failing, we cannot add
-       failing test due to the lack of this feature.
 
 FIXME: page-margin-* aren't added yet. We may need
        layoutTestController APIs to test margins.
--- LayoutTests/printing/resources/paged-media-test-utils.js
+++ LayoutTests/printing/resources/paged-media-test-utils.js
@@ -1,10 +1,54 @@
-function pageNumberShouldBe(id, expectedPageNumber)
+// The buffer to store the results.  We output the results after all
+// tests finish.   Note that we cannot have a DOM element where the
+// results are stored in HTMLs because the DOM element to store
+// results may change the number of pages.
+var _results = '';
+var _errored = false;
+
+function appendResult(result)
+{
+    _results += '<br>' + result;
+}
+
+function pageNumberForElementShouldBe(id, expectedPageNumber)
 {
     var actualPageNumber = layoutTestController.pageNumberForElementById(id);
-    var result = '';
     if (actualPageNumber == expectedPageNumber)
-        result = 'PASS';
-    else
-        result = 'FAIL expect page number is ' + expectedPageNumber + '. Was ' + actualPageNumber;
-    document.getElementById('results').innerHTML += result + '<br>';
+        appendResult('PASS: page number of "' + id + '" is ' + actualPageNumber);
+    else {
+        appendResult('FAIL: expected page number of "' + id + '" is ' + expectedPageNumber + '. Was ' + actualPageNumber);
+        _errored = true;
+    }
+}
+
+function numberOfPagesShouldBe(expectedNumberOfPages)
+{
+    var actualNumberOfPages = layoutTestController.numberOfPages();
+    if (actualNumberOfPages == expectedNumberOfPages)
+        appendResult('PASS: number of pages is ' + actualNumberOfPages);
+    else {
+        appendResult('FAIL: expected number of pages is ' + expectedNumberOfPages + '. Was ' + actualNumberOfPages);
+        _errored = true;
+    }
+}
+
+function runPrintingTest(testFunction)
+{
+    if (window.layoutTestController) {
+        try {
+            testFunction();
+        } catch (err) {
+            _results += '<p>Exception: ' + err.toString();
+            _errored = true;
+        }
+
+        if (!_errored)
+            _results += '<br>All tests passed';
+    } else {
+        _results += 'This test requires layoutTestController. You can test this manually with the above description.';
+    }
+
+    var resultElement = document.createElement('p');
+    resultElement.innerHTML = _results;
+    document.body.appendChild(resultElement);
 }
