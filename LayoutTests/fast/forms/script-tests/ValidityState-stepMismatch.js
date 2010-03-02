ce4325c6d15647cbdc0ef33d6ba001550b589b2f
Eric Seidel <eric@webkit.org>
2010-03-02 20:31:57 +0000

    2010-03-02  Kent Tamura  <tkent@chromium.org>
            Reviewed by Shinichiro Hamaji.
    
            Merge ValidityState-stepMismatch-{number,range,unsupported}.html and
            input-step-{number,range,unsupported}.html to other tests
            https://bugs.webkit.org/show_bug.cgi?id=35435
    
            - Merge ValidityState-stepMismatch-{number,range,unsupported}.html to
            ValidityState-stepMismatch.html
            - Merge input-step--{number,range,unsupported}.html to input-step.html
    
            * fast/forms/ValidityState-stepMismatch-expected.txt:
            * fast/forms/ValidityState-stepMismatch-number-expected.txt: Removed.
            * fast/forms/ValidityState-stepMismatch-number.html: Removed.
            * fast/forms/ValidityState-stepMismatch-range-expected.txt: Removed.
            * fast/forms/ValidityState-stepMismatch-range.html: Removed.
            * fast/forms/ValidityState-stepMismatch-unsupported-expected.txt: Removed.
            * fast/forms/ValidityState-stepMismatch-unsupported.html: Removed.
            * fast/forms/input-step-number-expected.txt: Removed.
            * fast/forms/input-step-number.html: Removed.
            * fast/forms/input-step-range-expected.txt: Removed.
            * fast/forms/input-step-range.html: Removed.
            * fast/forms/input-step-unsupported-expected.txt: Removed.
            * fast/forms/input-step-unsupported.html: Removed.
            * fast/forms/input-stepup-stepdown-expected.txt:
            * fast/forms/script-tests/ValidityState-stepMismatch-number.js: Removed.
            * fast/forms/script-tests/ValidityState-stepMismatch-range.js: Removed.
            * fast/forms/script-tests/ValidityState-stepMismatch-unsupported.js: Removed.
            * fast/forms/script-tests/ValidityState-stepMismatch.js:
            * fast/forms/script-tests/input-step-number.js: Removed.
            * fast/forms/script-tests/input-step-range.js: Removed.
            * fast/forms/script-tests/input-step-unsupported.js: Removed.
            * fast/forms/script-tests/input-stepup-stepdown.js:
    
    Canonical link: https://commits.webkit.org/46724@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@55423 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/forms/script-tests/ValidityState-stepMismatch.js
+++ LayoutTests/fast/forms/script-tests/ValidityState-stepMismatch.js
@@ -1,12 +1,12 @@
 description('Check stepMismatch results for type=date, datetime, datetime-local, month, time, week.');
-// FIXME: Merge other ValidityState-stepMismatch-* into this.
 
 var input = document.createElement('input');
+document.body.appendChild(input);
 
 function stepMismatchFor(value, step, min) {
-    input.value = value;
-    input.step = step;
     input.min = min;
+    input.step = step;
+    input.value = value;
     return input.validity.stepMismatch;
 }
 
@@ -128,6 +128,66 @@ debug('Special step value');
 shouldBeFalse('stepMismatchFor("2010-03", "any", "2010-02")');
 
 debug('');
+debug('Number type');
+input.type = 'number';
+debug('Empty values');
+shouldBe('stepMismatchFor("", null, null)', 'false');
+shouldBe('stepMismatchFor("", "1.0", "0.1")', 'false');
+debug('Integers');
+shouldBe('stepMismatchFor("1", "2", "0")', 'true');
+shouldBe('stepMismatchFor("-3", "2", "-4")', 'true');
+shouldBe('input.max = "5"; stepMismatchFor("5", "3", "0")', 'true');
+shouldBe('input.value', '"5"');
+debug('Invalid step values');
+input.max = '';
+shouldBe('stepMismatchFor("-3", "-2", "-4")', 'false');
+shouldBe('stepMismatchFor("-3", null, "-4")', 'false');
+shouldBe('stepMismatchFor("-3", undefined, "-4")', 'false');
+debug('Huge numbers and small step; uncomparable');
+shouldBe('stepMismatchFor("1.7976931348623157e+308", "3", "")', 'false');
+shouldBe('stepMismatchFor("1.7976931348623156e+308", "3", "")', 'false');
+shouldBe('stepMismatchFor("1.7976931348623155e+308", "3", "")', 'false');
+debug('Huge numbers and huge step');
+shouldBe('stepMismatchFor("1.60e+308", "0.20e+308", "")', 'false');
+shouldBe('stepMismatchFor("1.60e+308", "0.22e+308", "")', 'true');
+debug('Fractional numbers');
+shouldBe('stepMismatchFor("0.9", "0.1", "")', 'false');
+shouldBe('stepMismatchFor("0.9", "0.1000001", "")', 'true');
+shouldBe('stepMismatchFor("0.9", "0.1000000000000001", "")', 'false');
+shouldBe('stepMismatchFor("1.0", "0.3333333333333333", "")', 'false');
+
+debug('');
+debug('Range type');
+input.type = 'range';
+// The following test inputs are the same as inputs for type=numbe,
+// but all expected results should be 'false'.
+debug('Empty values');
+shouldBe('stepMismatchFor("", null, null)', 'false');
+shouldBe('stepMismatchFor("", "1.0", "0.1")', 'false');
+debug('Integers');
+shouldBe('stepMismatchFor("1", "2", "0")', 'false');
+shouldBe('stepMismatchFor("-3", "2", "-4")', 'false');
+shouldBe('input.max = "5"; stepMismatchFor("5", "3", "0")', 'false');
+shouldBe('input.value', '"3"'); // Different from type=number.
+debug('Invalid step values');
+input.max = '';
+shouldBe('stepMismatchFor("-3", "-2", "-4")', 'false');
+shouldBe('stepMismatchFor("-3", null, "-4")', 'false');
+shouldBe('stepMismatchFor("-3", undefined, "-4")', 'false');
+debug('Huge numbers and small step; uncomparable');
+shouldBe('stepMismatchFor("1.7976931348623157e+308", "3", "")', 'false');
+shouldBe('stepMismatchFor("1.7976931348623156e+308", "3", "")', 'false');
+shouldBe('stepMismatchFor("1.7976931348623155e+308", "3", "")', 'false');
+debug('Huge numbers and huge step');
+shouldBe('stepMismatchFor("1.60e+308", "0.20e+308", "")', 'false');
+shouldBe('stepMismatchFor("1.60e+308", "0.22e+308", "")', 'false');
+debug('Fractional numbers');
+shouldBe('stepMismatchFor("0.9", "0.1", "")', 'false');
+shouldBe('stepMismatchFor("0.9", "0.1000001", "")', 'false');
+shouldBe('stepMismatchFor("0.9", "0.1000000000000001", "")', 'false');
+shouldBe('stepMismatchFor("1.0", "0.3333333333333333", "")', 'false');
+
+debug('');
 debug('Time type');
 input.type = 'time';
 debug('Empty values');
@@ -187,5 +247,24 @@ shouldBeFalse('stepMismatchFor("2010-W03", "-1", "2010-W02")');
 shouldBeFalse('stepMismatchFor("2010-W03", "foo", "2010-W02")');
 debug('Special step value');
 shouldBeFalse('stepMismatchFor("2010-W03", "any", "2010-W02")');
+
+debug('');
+debug('Unsupported types');
+shouldBe('input.type = "text"; input.step = "3"; input.min = ""; input.value = "2"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "button"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "checkbox"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "color"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "email"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "hidden"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "image"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "khtml_isindex"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "passwd"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "radio"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "reset"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "search"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "submit"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "tel"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "url"; input.validity.stepMismatch', 'false');
+shouldBe('input.type = "file"; input.validity.stepMismatch', 'false');
 
 var successfullyParsed = true;
