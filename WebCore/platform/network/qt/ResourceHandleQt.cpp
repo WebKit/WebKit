adae2a1a460bae52aa08afd18346a2bfd154cb74
Mark Rowe <mrowe@apple.com>
2010-03-08 02:27:15 +0000

    Qt build fix.
    * platform/network/qt/ResourceHandleQt.cpp: Include SharedBuffer.h since we're using WebCore::SharedBuffer.
    
    Canonical link: https://commits.webkit.org/46943@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@55646 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/network/qt/ResourceHandleQt.cpp
+++ WebCore/platform/network/qt/ResourceHandleQt.cpp
@@ -28,20 +28,22 @@
  */
 
 #include "config.h"
-
-#include "Frame.h"
-#include "DocLoader.h"
 #include "ResourceHandle.h"
-#include "ResourceHandleClient.h"
-#include "ResourceHandleInternal.h"
-#include "qwebpage_p.h"
-#include "qwebframe_p.h"
+
 #include "ChromeClientQt.h"
+#include "DocLoader.h"
+#include "Frame.h"
 #include "FrameLoaderClientQt.h"
+#include "NotImplemented.h"
 #include "Page.h"
 #include "QNetworkReplyHandler.h"
+#include "ResourceHandleClient.h"
+#include "ResourceHandleInternal.h"
+#include "SharedBuffer.h"
 
-#include "NotImplemented.h"
+// FIXME: WebCore including these headers from WebKit is a massive layering violation.
+#include "qwebframe_p.h"
+#include "qwebpage_p.h"
 
 #if QT_VERSION >= 0x040500
 #include <QAbstractNetworkCache>
