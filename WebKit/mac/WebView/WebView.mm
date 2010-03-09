ab18affd3f440f683c1e02b2431c48b242a20d1c
Darin Adler <darin@apple.com>
2010-03-09 01:14:55 +0000

    WebCore: Added a test for crash when you quit inside an unload handler. rdar://problem/6958347
    Reviewed by Dan Bernstein.
    
    * manual-tests/quit-inside-unload.html: Added.
    
    WebKit/mac: Fix crash when you quit inside an unload handler.
    rdar://problem/6958347
    
    Reviewed by Dan Bernstein.
    
    Test: manual-tests/quit-inside-unload.html
    
    * WebView/WebView.mm:
    (-[WebView _closeWithFastTeardown]): Removed code to set
    _private->closed since _close now does this earlier, before
    calling this method.
    (-[WebView _close]): Moved code to set _private->closed to the
    top of this method.
    (-[WebView setHostWindow:]): Rewrote the code that forbade
    setting the host window after closing so that it only forbids
    non-nil host windows. That way, the code to clear away the host
    window can run safely after setting _private->closed, yet client
    code cannot set a new host window after closing.
    
    
    
    Canonical link: https://commits.webkit.org/46998@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@55701 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/mac/WebView/WebView.mm
+++ WebKit/mac/WebView/WebView.mm
@@ -975,8 +975,6 @@ static bool shouldEnableLoadDeferring()
     WTF::RefCountedLeakCounter::suppressMessages("At least one WebView was closed with fast teardown.");
 #endif
 
-    _private->closed = YES;
-    
     [[NSDistributedNotificationCenter defaultCenter] removeObserver:self];
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 
@@ -1008,6 +1006,8 @@ static bool fastDocumentTeardownEnabled()
 {
     if (!_private || _private->closed)
         return;
+
+    _private->closed = YES;
 
     [self _closingEventHandling];
 
@@ -1041,9 +1041,6 @@ static bool fastDocumentTeardownEnabled()
     [self setUIDelegate:nil];
 
     [_private->inspector webViewClosed];
-
-    // setHostWindow:nil must be called before this value is set (see 5408186)
-    _private->closed = YES;
 
     // To avoid leaks, call removeDragCaret in case it wasn't called after moveDragCaretToPoint.
     [self removeDragCaret];
@@ -3339,7 +3336,7 @@ static bool needsWebViewInitThreadWorkaround()
 
 - (void)setHostWindow:(NSWindow *)hostWindow
 {
-    if (_private->closed)
+    if (_private->closed && hostWindow)
         return;
     if (hostWindow == _private->hostWindow)
         return;
