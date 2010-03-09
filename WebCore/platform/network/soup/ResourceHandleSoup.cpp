7ea859a9632f9c7c34fe2052449d8e14c30520af
Csaba Osztrogonác <ossy@webkit.org>
2010-03-09 00:26:48 +0000

    [GTK] Unreviewed buildfix after r55688.
    * platform/network/soup/ResourceHandleSoup.cpp:
    (WebCore::):
    
    
    
    Canonical link: https://commits.webkit.org/46989@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@55692 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/network/soup/ResourceHandleSoup.cpp
+++ WebCore/platform/network/soup/ResourceHandleSoup.cpp
@@ -648,7 +648,7 @@ bool ResourceHandle::willLoadFromCache(ResourceRequest&, Frame*)
 void ResourceHandle::loadResourceSynchronously(const ResourceRequest& request, StoredCredentials /*storedCredentials*/, ResourceError& error, ResourceResponse& response, Vector<char>& data, Frame* frame)
 {
     WebCoreSynchronousLoader syncLoader(error, response, data);
-    ResourceHandle handle(request, &syncLoader, true, false, true);
+    ResourceHandle handle(request, &syncLoader, true, false);
 
     handle.start(frame);
     syncLoader.run();
