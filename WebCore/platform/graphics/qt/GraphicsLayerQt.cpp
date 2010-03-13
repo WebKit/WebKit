1452722abf84b61ee7b77f640f8b57c65749b197
Eric Seidel <eric@webkit.org>
2010-03-13 19:28:45 +0000

    2010-03-13  Kim Grönholm  <kim.gronholm@nomovok.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] GraphicsLayer: Opacity change from zero to non-zero doesn't always have effect with AC
            https://bugs.webkit.org/show_bug.cgi?id=36034
    
            * platform/graphics/qt/GraphicsLayerQt.cpp:
            (WebCore::OpacityAnimationQt::applyFrame):
    
    Canonical link: https://commits.webkit.org/47262@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@55967 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/qt/GraphicsLayerQt.cpp
+++ WebCore/platform/graphics/qt/GraphicsLayerQt.cpp
@@ -1134,7 +1134,14 @@ public:
 
     virtual void applyFrame(const qreal& fromValue, const qreal& toValue, qreal progress)
     {
-        m_layer.data()->setOpacity(qMin<qreal>(qMax<qreal>(fromValue + (toValue-fromValue)*progress, 0), 1));
+        qreal opacity = qBound(qreal(0), fromValue + (toValue-fromValue)*progress, qreal(1));
+
+        // FIXME: this is a hack, due to a probable QGraphicsScene bug.
+        // Without this the opacity change doesn't always have immediate effect.
+        if (!m_layer.data()->opacity() && opacity)
+            m_layer.data()->scene()->update();
+
+        m_layer.data()->setOpacity(opacity);
     }
 
     virtual void updateState(QAbstractAnimation::State newState, QAbstractAnimation::State oldState)
