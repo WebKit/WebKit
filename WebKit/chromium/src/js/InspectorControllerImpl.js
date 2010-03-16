9d3b47fdc0c6bf6e997ab6ac8add1807a56cb927
Pavel Feldman <pfeldman@chromium.org>
2010-03-16 10:17:21 +0000

    2010-03-16  Pavel Feldman  <pfeldman@chromium.org>
            Not reviewed: chromium tests fix.
    
            * src/js/InspectorControllerImpl.js:
            (devtools.InspectorBackendImpl):
    
    
    Canonical link: https://commits.webkit.org/47345@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@56050 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/chromium/src/js/InspectorControllerImpl.js
+++ WebKit/chromium/src/js/InspectorControllerImpl.js
@@ -74,6 +74,7 @@ devtools.InspectorBackendImpl = function()
 
     this.installInspectorControllerDelegate_("getStyles");
     this.installInspectorControllerDelegate_("getComputedStyle");
+    this.installInspectorControllerDelegate_("getInlineStyle");
     this.installInspectorControllerDelegate_("applyStyleText");
     this.installInspectorControllerDelegate_("setStyleText");
     this.installInspectorControllerDelegate_("setStyleProperty");
