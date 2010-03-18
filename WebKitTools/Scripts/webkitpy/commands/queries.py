c353d45650c6fdd559c28ef63e65d33438210499
Eric Seidel <eric@webkit.org>
2010-03-18 18:33:35 +0000

    2010-03-18  Eric Seidel  <eric@webkit.org>
            Just fixing missing parenthesis typo, no review.
    
            * Scripts/webkitpy/commands/queries.py: '%' has higher precedence than 'or', use parentheses.
    
    Canonical link: https://commits.webkit.org/47470@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@56176 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/webkitpy/commands/queries.py
+++ WebKitTools/Scripts/webkitpy/commands/queries.py
@@ -156,8 +156,8 @@ and displayes the status of each builder."""
         changelog_entries = self._changelog_entries_for_revision(revision)
         # Assume for now that the first entry has everything we need:
         changelog_entry = changelog_entries[0]
-        # FIXME: The commit info dictionary here should probably be its own class?
         committer_list = CommitterList()
+        # FIXME: This should be a CommitInfo object.
         return {
             "bug_id" : parse_bug_id(changelog_entry.contents()),
             "revision" : revision,
@@ -170,13 +170,15 @@ and displayes the status of each builder."""
             "committer" : committer_list.committer_by_email(committer_email) if committer_email else None
         }
 
+    # FIXME: This should be on some CommitInfo object.
     def _print_blame_information_for_commit(self, commit_info):
         print "r%s:" % commit_info["revision"]
         print "  %s" % view_source_url(commit_info["revision"])
         print "  Bug: %s (%s)" % (commit_info["bug_id"], self.tool.bugs.bug_url_for_bug_id(commit_info["bug_id"]))
-        print "  Author: %s" % commit_info["author"] or ("\"%s\" <%s> %" % (commit_info["author_name"], commit_info["author_email"]))
-        print "  Reviewer: %s" % commit_info["reviewer"] or commit_info["reviewer_text"]
-        print "  Committer: %s" % commit_info["committer"] or commit_info["committer_email"]
+        author_line = "\"%s\" <%s>" % (commit_info["author_name"], commit_info["author_email"])
+        print "  Author: %s" % (commit_info["author"] or author_line)
+        print "  Reviewer: %s" % (commit_info["reviewer"] or commit_info["reviewer_text"])
+        print "  Committer: %s" % (commit_info["committer"] or commit_info["committer_email"])
 
     def _print_blame_information_for_builder(self, builder_status, name_width):
         (last_green_build, first_red_build) = self._find_green_to_red_transition(builder_status)
