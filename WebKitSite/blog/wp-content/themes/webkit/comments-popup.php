706a7a39e82a3ab881aae9bacb21c61ec4a98b23
Maciej Stachowiak <mjs@apple.com>
2010-03-20 09:07:15 +0000

    2010-03-20  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Mark Rowe.
    
            WebKit blog should be valid HTML5
            https://bugs.webkit.org/show_bug.cgi?id=36410
    
            * blog/wp-content/themes/webkit/comments-popup.php:
            * blog/wp-content/themes/webkit/footer.php:
            * blog/wp-content/themes/webkit/header.php:
    
    
    Canonical link: https://commits.webkit.org/47596@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@56302 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitSite/blog/wp-content/themes/webkit/comments-popup.php
+++ WebKitSite/blog/wp-content/themes/webkit/comments-popup.php
@@ -3,7 +3,7 @@
 add_filter('comment_text', 'popuplinks');
 foreach ($posts as $post) { start_wp();
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
 <html>
 <head>
      <title><?php echo get_settings('blogname'); ?> - Comments on <?php the_title(); ?></title>
