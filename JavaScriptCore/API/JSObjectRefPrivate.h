67e4f41f2eabaef278267aebac407788292bfb37
Oliver Hunt <oliver@apple.com>
2010-03-21 08:35:09 +0000

    Documentation fix for previous patch.
    Reviewed by Maciej Stachowiak.
    
    * API/JSObjectRefPrivate.h:
    
    Canonical link: https://commits.webkit.org/47609@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@56315 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/API/JSObjectRefPrivate.h
+++ JavaScriptCore/API/JSObjectRefPrivate.h
@@ -39,8 +39,10 @@ extern "C" {
  @param object The JSObject whose private property you want to set.
  @param propertyName A JSString containing the property's name.
  @param value A JSValue to use as the property's value.  This may be NULL.
- @result The default object class does not allocate storage for private data. Only objects created with a non-NULL JSClass can store private properties.  If this method is used on an object with a NULL JSClass it will return false.
+ @result true if object can store private data, otherwise false.
  @discussion This API allows you to store JS values directly an object in a way that will be ensure that they are kept alive without exposing them to JavaScript code and without introducing the reference cycles that may occur when using JSValueProtect.
+
+ The default object class does not allocate storage for private data. Only objects created with a non-NULL JSClass can store private properties.
  */
 JS_EXPORT bool JSObjectSetPrivateProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef value);
 
@@ -60,7 +62,8 @@ JS_EXPORT JSValueRef JSObjectGetPrivateProperty(JSContextRef ctx, JSObjectRef ob
  @param ctx The execution context to use.
  @param object The JSObject whose private property you want to delete.
  @param propertyName A JSString containing the property's name.
- @result The default object class does not allocate storage for private data. Only objects created with a non-NULL JSClass can store private properties.  If this method is used on an object with a NULL JSClass it will return false.
+ @result true if object can store private data, otherwise false.
+ @discussion The default object class does not allocate storage for private data. Only objects created with a non-NULL JSClass can store private data.
  */
 JS_EXPORT bool JSObjectDeletePrivateProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName);
 
