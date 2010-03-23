5028555cbd97c87dfb3440bc915c6357cc09090d
Eric Seidel <eric@webkit.org>
2010-03-23 16:34:15 +0000

    2010-03-23  Qi Zhang  <qi.2.zhang@nokia.com>
            Reviewed by Simon Hausmann.
    
            Qt] LayoutTests/fast/canvas/fillrect_gradient.html failed
            https://bugs.webkit.org/show_bug.cgi?id=36444
    
            Enable LayoutTests/fast/canvas/fillrect_gradient.html
    
            * platform/qt/Skipped:
            * platform/qt/fast/canvas/fillrect_gradient-expected.checksum: Added.
            * platform/qt/fast/canvas/fillrect_gradient-expected.png: Added.
            * platform/qt/fast/canvas/fillrect_gradient-expected.txt:
    2010-03-23  Qi Zhang  <qi.2.zhang@nokia.com>
    
            Reviewed by Simon Hausmann.
    
            [Qt] LayoutTests/fast/canvas/fillrect_gradient.html failed
            https://bugs.webkit.org/show_bug.cgi?id=36444
    
            Handle duplicated or unsorted colorStop at platformDestory
    
            * platform/graphics/qt/GradientQt.cpp:
            (WebCore::Gradient::platformGradient):
    
    Canonical link: https://commits.webkit.org/47692@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@56398 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/qt/GradientQt.cpp
+++ WebCore/platform/graphics/qt/GradientQt.cpp
@@ -51,6 +51,8 @@ QGradient* Gradient::platformGradient()
     else
         m_gradient = new QLinearGradient(m_p0.x(), m_p0.y(), m_p1.x(), m_p1.y());
 
+    sortStopsIfNecessary();
+
     QColor stopColor;
     Vector<ColorStop>::iterator stopIterator = m_stops.begin();
     qreal lastStop(0.0);
@@ -64,6 +66,8 @@ QGradient* Gradient::platformGradient()
         if (m_radial && m_r0)
             lastStop = m_r0 / m_r1 + lastStop * (1.0f - m_r0 / m_r1);
         m_gradient->setColorAt(lastStop, stopColor);
+        // Keep the lastStop as orginal value, since the following stopColor depend it
+        lastStop = stopIterator->stop;
         ++stopIterator;
     }
 
