e1fe27fd77a69cc44ca78331ca54f349edba7346
Olivier Goffart <ogoffart@trolltech.com>
2010-03-26 13:44:49 +0000

    [Qt] Fix compilation on Windows
    Patch by Olivier Goffart <ogoffart@trolltech.com> on 2010-03-26
    Reviewed by Simon Hausmann.
    
    For some reason, the MSVC compiler choose the
    operator+(const QString &, const QString &) instead of
    operator+(const WebCore::String &, const WebCore::String &)
    resulting in errors when QT_USE_FAST_OPERATOR_PLUS is used.
    
    * dom/XMLTokenizerQt.cpp:
    (WebCore::handleElementNamespaces):
    
    Canonical link: https://commits.webkit.org/47916@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@56622 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/dom/XMLTokenizerQt.cpp
+++ WebCore/dom/XMLTokenizerQt.cpp
@@ -328,7 +328,7 @@ static inline void handleElementNamespaces(Element* newElement, const QXmlStream
     for (int i = 0; i < ns.count(); ++i) {
         const QXmlStreamNamespaceDeclaration &decl = ns[i];
         String namespaceURI = decl.namespaceUri();
-        String namespaceQName = decl.prefix().isEmpty() ? String("xmlns") : String("xmlns:") + decl.prefix();
+        String namespaceQName = decl.prefix().isEmpty() ? String("xmlns") : String("xmlns:") + String(decl.prefix());
         newElement->setAttributeNS("http://www.w3.org/2000/xmlns/", namespaceQName, namespaceURI, ec, scriptingPermission);
         if (ec) // exception setting attributes
             return;
