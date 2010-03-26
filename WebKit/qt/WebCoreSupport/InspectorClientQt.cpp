d88133433bc7f8b71d47d1d6e2a6c7d0cd4ee6c2
Olivier Goffart <ogoffart@trolltech.com>
2010-03-26 14:37:39 +0000

    [Qt] Compile with QT_USE_FAST_OPERATOR_PLUS
    Patch by Olivier Goffart <ogoffart@trolltech.com> on 2010-03-26
    Reviewed by Simon Hausmann.
    
    * WebCoreSupport/InspectorClientQt.cpp:
    (WebCore::InspectorClientQt::populateSetting):
    (WebCore::InspectorClientQt::storeSetting):
    
    Canonical link: https://commits.webkit.org/47920@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@56626 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/qt/WebCoreSupport/InspectorClientQt.cpp
+++ WebKit/qt/WebCoreSupport/InspectorClientQt.cpp
@@ -118,7 +118,7 @@ void InspectorClientQt::populateSetting(const String& key, String* setting)
         return;
     }
 
-    QString settingKey(settingStoragePrefix + key);
+    QString settingKey(settingStoragePrefix + QString(key));
     QString storedValueType = qsettings.value(settingKey + settingStorageTypeSuffix).toString();
     QVariant storedValue = qsettings.value(settingKey);
     storedValue.convert(QVariant::nameToType(storedValueType.toAscii().data()));
@@ -135,7 +135,7 @@ void InspectorClientQt::storeSetting(const String& key, const String& setting)
     }
 
     QVariant valueToStore = settingToVariant(setting);
-    QString settingKey(settingStoragePrefix + key);
+    QString settingKey(settingStoragePrefix + QString(key));
     qsettings.setValue(settingKey, valueToStore);
     qsettings.setValue(settingKey + settingStorageTypeSuffix, QVariant::typeToName(valueToStore.type()));
 }
