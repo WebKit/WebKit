57396474476444db172666d2ed3112f4f1853dfe
Eric Seidel <eric@webkit.org>
2010-03-29 17:50:17 +0000

    2010-03-29  Kenneth Rohde Christiansen  <kenneth@webkit.org>
            Reviewed by Antti Koivisto.
    
            Use 'Mobile Safari' instead of 'Safari' on mobile Qt platforms.
    
            * Api/qwebpage.cpp:
            (QWebPage::userAgentForUrl):
    
    Canonical link: https://commits.webkit.org/48025@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@56731 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/qt/Api/qwebpage.cpp
+++ WebKit/qt/Api/qwebpage.cpp
@@ -3154,6 +3154,8 @@ QWebPluginFactory *QWebPage::pluginFactory() const
 
     "Mozilla/5.0 (%Platform%; %Security%; %Subplatform%; %Locale%) AppleWebKit/%WebKitVersion% (KHTML, like Gecko) %AppVersion Safari/%WebKitVersion%"
 
+    On mobile platforms such as Symbian S60 and Maemo, "Mobile Safari" is used instead of "Safari".
+
     In this string the following values are replaced at run-time:
     \list
     \o %Platform% and %Subplatform% are expanded to the windowing system and the operation system.
@@ -3385,9 +3387,11 @@ QString QWebPage::userAgentForUrl(const QUrl& url) const
         ua.append(QLatin1String(qVersion()));
     }
 
-    ua.append(QString(QLatin1String(" Safari/%1"))
-                      .arg(qWebKitVersion()));
-
+#if defined(Q_WS_S60) || defined(Q_WS_MAEMO_5)
+    ua.append(QString(QLatin1String(" Mobile Safari/%1")).arg(qWebKitVersion()));
+#else
+    ua.append(QString(QLatin1String(" Safari/%1")).arg(qWebKitVersion()));
+#endif
     return ua;
 }
 
