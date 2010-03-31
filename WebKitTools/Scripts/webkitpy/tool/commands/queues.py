7be2d1ba86fad75b6dfbcb08582437a850b0771e
Adam Barth <abarth@webkit.org>
2010-03-31 19:32:28 +0000

    2010-03-31  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            webkit-patch commit-queue should land patches optimistically
            https://bugs.webkit.org/show_bug.cgi?id=34187
    
            This patch adds an optimistic path to commit-queue and a "fail twice"
            requirement for rejecting patches.  That means we'll land good patches
            faster (via the optmistic first run) and we'll reject many fewer
            patches due to flaky tests.
    
            * Scripts/webkitpy/tool/commands/queues.py:
    
    Canonical link: https://commits.webkit.org/48150@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@56857 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/webkitpy/tool/commands/queues.py
+++ WebKitTools/Scripts/webkitpy/tool/commands/queues.py
@@ -176,7 +176,13 @@ class CommitQueue(AbstractPatchQueue, StepSequenceErrorHandler):
 
     def _can_build_and_test(self):
         try:
-            self.run_webkit_patch(["build-and-test", "--force-clean", "--non-interactive", "--build-style=both", "--quiet"])
+            self.run_webkit_patch([
+                "build-and-test",
+                "--force-clean",
+                "--non-interactive",
+                "--no-update",
+                "--build-style=both",
+                "--quiet"])
         except ScriptError, e:
             self._update_status("Unable to successfully build and test", None)
             return False
@@ -194,40 +200,56 @@ class CommitQueue(AbstractPatchQueue, StepSequenceErrorHandler):
         if not patch.is_rollout():
             if not self._builders_are_green():
                 return False
-            if not self._can_build_and_test():
-                return False
-            if not self._builders_are_green():
-                return False
         self._update_status("Landing patch", patch)
         return True
 
-    def process_work_item(self, patch):
+    def _land(self, patch, first_run=False):
         try:
-            self._cc_watchers(patch.bug_id())
             args = [
                 "land-attachment",
                 "--force-clean",
                 "--non-interactive",
-                "--parent-command=commit-queue",
                 "--build-style=both",
                 "--quiet",
                 patch.id()
             ]
+            if not first_run:
+                # The first time through, we don't reject the patch from the
+                # commit queue because we want to make sure we can build and
+                # test ourselves. However, the second time through, we
+                # register ourselves as the parent-command so we can reject
+                # the patch on failure.
+                args.append("--parent-command=commit-queue")
+                # The second time through, we also don't want to update so we
+                # know we're testing the same revision that we successfully
+                # built and tested.
+                args.append("--no-update")
             if patch.is_rollout():
                 # We need to ignore the builders when landing a rollout
                 # because they're probably red.
                 args.append("--ignore-builders")
-            else:
-                # We pass --no-update in the normal (non-rollout) case
-                # because we've already validated that the current revision
-                # actually builds and passes the tests.  If we update, we risk
-                # moving to a revision that doesn't!
-                args.append("--no-update")
             self.run_webkit_patch(args)
             self._did_pass(patch)
+            return True
         except ScriptError, e:
+            if first_run:
+                return False
             self._did_fail(patch)
             raise e
+
+    def process_work_item(self, patch):
+        self._cc_watchers(patch.bug_id())
+        if not self._land(patch, first_run=True):
+            # The patch failed to land, but the bots were green. It's possible
+            # that the bots were behind. To check that case, we try to build and
+            # test ourselves.
+            if not self._can_build_and_test():
+                return
+            # Hum, looks like the patch is actually bad. Of course, we could
+            # have been bitten by a flaky test the first time around.  We try
+            # to land again.  If it fails a second time, we're pretty sure its
+            # a bad test and re can reject it outright.
+            self._land(patch)
 
     def handle_unexpected_error(self, patch, message):
         self.committer_validator.reject_patch_from_commit_queue(patch.id(), message)
