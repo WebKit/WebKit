1a4d6c1ea27c5f97d42a00fd2b894de67534bdf7
Adam Barth <abarth@webkit.org>
2010-04-03 06:15:04 +0000

    2010-04-02  Adam Barth  <abarth@webkit.org>
            Unreviewed.
    
            REGRESSION(56394): http/tests/xmlhttprequest/xmlhttprequest-50ms-download-dispatch.html fails on Tiger
            https://bugs.webkit.org/show_bug.cgi?id=36934
    
            Attempt Alexey's solution of using a non-sniffable MIME type.
    
            * http/tests/xmlhttprequest/resources/download-with-delay.php:
    
    Canonical link: https://commits.webkit.org/48337@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@57044 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/xmlhttprequest/resources/download-with-delay.php
+++ LayoutTests/http/tests/xmlhttprequest/resources/download-with-delay.php
@@ -2,6 +2,7 @@
 header("Expires: Thu, 01 Dec 2003 16:00:00 GMT");
 header("Cache-Control: no-cache, no-store, must-revalidate");
 header("Pragma: no-cache");
+header("Content-Type: application/x-no-buffering-please");
 
 $iteration = $_GET['iteration'];
 $delay = $_GET['delay'];
