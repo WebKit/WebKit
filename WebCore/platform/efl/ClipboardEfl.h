17619246eb0f59663831615da05d4f9df46280b1
Eric Seidel <eric@webkit.org>
2010-04-06 00:34:32 +0000

    2010-04-05  Leandro Pereira  <leandro@profusion.mobi>
            Reviewed by Kenneth Rohde Christiansen.
    
            Add missing ClipboardEfl.h to platform/efl.
            http://webkit.org/b/36242
    
            * platform/efl/ClipboardEfl.h: Added.
    
    Canonical link: https://commits.webkit.org/48400@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@57107 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebCore/platform/efl/ClipboardEfl.h
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (C) 2008 INdT - Instituto Nokia de Tecnologia
+ *  Copyright (C) 2009-2010 ProFUSION embedded systems
+ *  Copyright (C) 2009-2010 Samsung Electronics
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef ClipboardEfl_h
+#define ClipboardEfl_h
+
+#include "Clipboard.h"
+
+namespace WebCore {
+class CachedImage;
+
+class ClipboardEfl : public Clipboard {
+public:
+    ClipboardEfl(ClipboardAccessPolicy, bool);
+    ~ClipboardEfl();
+
+    void clearData(const String&);
+    void clearAllData();
+    String getData(const String&, bool&) const;
+    bool setData(const String&, const String&);
+
+    HashSet<String> types() const;
+    virtual PassRefPtr<FileList> files() const;
+
+    IntPoint dragLocation() const;
+    CachedImage* dragImage() const;
+    void setDragImage(CachedImage*, const IntPoint&);
+    Node* dragImageElement();
+    void setDragImageElement(Node*, const IntPoint&);
+
+    virtual DragImageRef createDragImage(IntPoint&) const;
+    virtual void declareAndWriteDragImage(Element*, const KURL&, const String&, Frame*);
+    virtual void writeURL(const KURL&, const String&, Frame*);
+    virtual void writeRange(Range*, Frame*);
+
+    virtual bool hasData();
+
+    virtual void writePlainText(const WebCore::String&);
+};
+}
+
+#endif
