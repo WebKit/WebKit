63cb6166e3d829881f5a1afeffc54b7c8ca5f159
Steve Falkenburg <sfalken@apple.com>
2010-04-13 00:47:10 +0000

    2010-04-12  Steve Falkenburg  <sfalken@apple.com>
            Reviewed by Dan Bernstein.
    
            WebKit should have more robust last-chance font fallback on Windows
            https://bugs.webkit.org/show_bug.cgi?id=37473
            <rdar://problem/7789438> Crash in FontFallbackList::determinePitch
            <rdar://problem/7233762> Crash in FontFallbackList::fontDataAt
    
            Look harder for a suitable last-resort font. Previously, we checked for
            "Times New Roman" followed by DEFAULT_GUI_FONT.
    
            We now look for:
            - Typically installed Unicode-capable fonts, in order of coverage
            - DEFAULT_GUI_FONT
            - SPI_GETNONCLIENTMETRICS fonts
    
            * platform/graphics/win/FontCacheWin.cpp:
            (WebCore::FontCache::fontDataFromDescriptionAndLogFont):
            (WebCore::FontCache::getLastResortFallbackFont):
    
    
    Canonical link: https://commits.webkit.org/48778@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@57490 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/graphics/win/FontCacheWin.cpp
+++ WebCore/platform/graphics/win/FontCacheWin.cpp
@@ -301,22 +301,62 @@ SimpleFontData* FontCache::getSimilarFontPlatformData(const Font& font)
     return 0;
 }
 
+static SimpleFontData* fontDataFromDescriptionAndLogFont(FontCache* fontCache, const FontDescription& fontDescription, const LOGFONT& font)
+{
+    String fontFamily = String(font.lfFaceName, wcsnlen(font.lfFaceName, LF_FACESIZE));
+    return fontCache->getCachedFontData(fontDescription, fontFamily);
+}
+
 SimpleFontData* FontCache::getLastResortFallbackFont(const FontDescription& fontDescription)
 {
-    // FIXME: Would be even better to somehow get the user's default font here.  For now we'll pick
-    // the default that the user would get without changing any prefs.
-    static AtomicString timesStr("Times New Roman");
-    if (SimpleFontData* simpleFont = getCachedFontData(fontDescription, timesStr))
+    DEFINE_STATIC_LOCAL(SimpleFontData*, simpleFont, ());
+    if (simpleFont)
         return simpleFont;
 
-    DEFINE_STATIC_LOCAL(String, defaultGUIFontFamily, ());
-    if (defaultGUIFontFamily.isEmpty()) {
-        HFONT defaultGUIFont = static_cast<HFONT>(GetStockObject(DEFAULT_GUI_FONT));
-        LOGFONT logFont;
-        GetObject(defaultGUIFont, sizeof(logFont), &logFont);
-        defaultGUIFontFamily = String(logFont.lfFaceName, wcsnlen(logFont.lfFaceName, LF_FACESIZE));
+    // FIXME: Would be even better to somehow get the user's default font here.  For now we'll pick
+    // the default that the user would get without changing any prefs.
+
+    // Search all typical Windows-installed full Unicode fonts.
+    // Sorted by most to least glyphs according to http://en.wikipedia.org/wiki/Unicode_typefaces
+    // Start with Times New Roman also since it is the default if the user doesn't change prefs.
+    static AtomicString fallbackFonts[] = {
+        AtomicString("Times New Roman"),
+        AtomicString("Microsoft Sans Serif"),
+        AtomicString("Tahoma"),
+        AtomicString("Lucida Sans Unicode"),
+        AtomicString("Arial")
+    };
+    for (int i = 0; i < ARRAYSIZE(fallbackFonts); ++i) {
+        if (simpleFont = getCachedFontData(fontDescription, fallbackFonts[i]))
+            return simpleFont;
     }
-    return getCachedFontData(fontDescription, defaultGUIFontFamily);
+
+    // Fall back to the DEFAULT_GUI_FONT if no known Unicode fonts are available.
+    if (HFONT defaultGUIFont = static_cast<HFONT>(GetStockObject(DEFAULT_GUI_FONT))) {
+        LOGFONT defaultGUILogFont;
+        GetObject(defaultGUIFont, sizeof(defaultGUILogFont), &defaultGUILogFont);
+        if (simpleFont = fontDataFromDescriptionAndLogFont(this, fontDescription, defaultGUILogFont))
+            return simpleFont;
+    }
+
+    // Fall back to Non-client metrics fonts.
+    NONCLIENTMETRICS nonClientMetrics = {0};
+    nonClientMetrics.cbSize = sizeof(nonClientMetrics);
+    if (SystemParametersInfo(SPI_GETNONCLIENTMETRICS, sizeof(nonClientMetrics), &nonClientMetrics, 0)) {
+        if (simpleFont = fontDataFromDescriptionAndLogFont(this, fontDescription, nonClientMetrics.lfMessageFont))
+            return simpleFont;
+        if (simpleFont = fontDataFromDescriptionAndLogFont(this, fontDescription, nonClientMetrics.lfMenuFont))
+            return simpleFont;
+        if (simpleFont = fontDataFromDescriptionAndLogFont(this, fontDescription, nonClientMetrics.lfStatusFont))
+            return simpleFont;
+        if (simpleFont = fontDataFromDescriptionAndLogFont(this, fontDescription, nonClientMetrics.lfCaptionFont))
+            return simpleFont;
+        if (simpleFont = fontDataFromDescriptionAndLogFont(this, fontDescription, nonClientMetrics.lfSmCaptionFont))
+            return simpleFont;
+    }
+    
+    ASSERT_NOT_REACHED();
+    return 0;
 }
 
 static LONG toGDIFontWeight(FontWeight fontWeight)
