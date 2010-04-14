de2945d7014613169ba02b17b526732ec86da88b
Eric Seidel <eric@webkit.org>
2010-04-14 17:42:16 +0000

    2010-04-14  Adam Barth  <abarth@webkit.org>
            Reviewed by Darin Fisher.
    
            Add port URL tests
            https://bugs.webkit.org/show_bug.cgi?id=37524
    
            This went pretty well.
    
            * fast/url/port-expected.txt: Added.
            * fast/url/port.html: Added.
            * fast/url/script-tests/port.js: Added.
    
    Canonical link: https://commits.webkit.org/48876@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@57588 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/url/script-tests/port.js
@@ -0,0 +1,21 @@
+description("Test URLs that have a port number.");
+
+cases = [ 
+  // Invalid input should be copied w/ failure.
+  ["as df", ":as%20df"],
+  ["-2", ":-2"],
+  // Default port should be omitted.
+  ["80", ""],
+  ["8080", ":8080"],
+];
+
+for (var i = 0; i < cases.length; ++i) {
+  shouldBe("canonicalize('http://www.example.com:" + cases[i][0] + "/')",
+           "'http://www.example.com" + cases[i][1] + "/'");
+}
+
+// Unspecified port should mean always keep the port.
+shouldBe("canonicalize('foobar://www.example.com:80/')",
+         "'foobar://www.example.com:80/'");
+
+var successfullyParsed = true;
