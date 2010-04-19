9985dc9ff7933629af5ada15009e326dba539120
Eric Seidel <eric@webkit.org>
2010-04-19 05:13:27 +0000

    2010-04-18  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Laszlo Gombos.
    
            [WINCE] Don't define WTF_CPU_MIDDLE_ENDIAN=1
            https://bugs.webkit.org/show_bug.cgi?id=37434
    
            Windows CE supports little-endian format only, so don't define
            WTF_CPU_MIDDLE_ENDIAN=1.
    
            * wtf/Platform.h:
    
    Canonical link: https://commits.webkit.org/49085@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@57804 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/Platform.h
+++ JavaScriptCore/wtf/Platform.h
@@ -202,6 +202,7 @@
 #elif !defined(__ARM_EABI__) \
     && !defined(__EABI__) \
     && !defined(__VFP_FP__) \
+    && !defined(_WIN32_WCE) \
     && !defined(ANDROID)
 #define WTF_CPU_MIDDLE_ENDIAN 1
 
