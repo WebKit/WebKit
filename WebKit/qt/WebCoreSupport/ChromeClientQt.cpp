489ab0e87d015666a7e22aa36940504a8f3da2f4
Eric Seidel <eric@webkit.org>
2010-04-21 15:08:37 +0000

    2010-04-21  Yi Shen  <yi.4.shen@nokia.com>
            Reviewed by Simon Hausmann.
    
            [Qt] Check the request empty or not in ChromeClientQt::createWindow()
            https://bugs.webkit.org/show_bug.cgi?id=37821
    
            * WebCoreSupport/ChromeClientQt.cpp:
            (WebCore::ChromeClientQt::createWindow):
    
    Canonical link: https://commits.webkit.org/49263@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@57982 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/qt/WebCoreSupport/ChromeClientQt.cpp
+++ WebKit/qt/WebCoreSupport/ChromeClientQt.cpp
@@ -166,7 +166,9 @@ Page* ChromeClientQt::createWindow(Frame*, const FrameLoadRequest& request, cons
     QWebPage *newPage = m_webPage->createWindow(features.dialog ? QWebPage::WebModalDialog : QWebPage::WebBrowserWindow);
     if (!newPage)
         return 0;
-    newPage->mainFrame()->load(request.resourceRequest().url());
+
+    if (!request.isEmpty())
+        newPage->mainFrame()->load(request.resourceRequest().url());
     return newPage->d->page;
 }
 
