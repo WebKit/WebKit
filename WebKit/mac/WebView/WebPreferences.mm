a183f0013ad6c547bfecc636883880841a540dce
Alexey Proskuryakov <ap@apple.com>
2010-04-22 04:45:43 +0000

    Reviewed by Shinichiro Hamaji.
            https://bugs.webkit.org/show_bug.cgi?id=37964
            Fix a typo in comments - Korean encoding name is windows-949, not windows-939
    
            * WebView/WebPreferences.mm: (+[WebPreferences _setInitialDefaultTextEncodingToSystemEncoding]):
    
    
    
    Canonical link: https://commits.webkit.org/49333@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@58052 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/mac/WebView/WebPreferences.mm
+++ WebKit/mac/WebView/WebPreferences.mm
@@ -1097,8 +1097,8 @@ static WebCacheModel cacheModelForMainBundle(void)
 {
     NSString *systemEncodingName = (NSString *)CFStringConvertEncodingToIANACharSetName([self _systemCFStringEncoding]);
 
-    // CFStringConvertEncodingToIANACharSetName() returns cp949 for kTextEncodingDOSKorean AKA "extended EUC-KR" AKA windows-939.
-    // ICU uses this name for a different encoding, so we need to change the name to a value that actually gives us windows-939.
+    // CFStringConvertEncodingToIANACharSetName() returns cp949 for kTextEncodingDOSKorean AKA "extended EUC-KR" AKA windows-949.
+    // ICU uses this name for a different encoding, so we need to change the name to a value that actually gives us windows-949.
     // In addition, this value must match what is used in Safari, see <rdar://problem/5579292>.
     // On some OS versions, the result is CP949 (uppercase).
     if ([systemEncodingName _webkit_isCaseInsensitiveEqualToString:@"cp949"])
