a1e5a4553bcdbf5be3c5a251c3e163ebdd297749
Adam Barth <abarth@webkit.org>
2010-04-22 06:49:50 +0000

    2010-04-21  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            URL LayoutTests don't run on Firefox
            https://bugs.webkit.org/show_bug.cgi?id=37974
    
            It turns out Firefox doesn't handle base URLs properly if we go through
            the DOM to create the <base> and <a> elements.  If we switch to
            document.write, we can run the tests in Firefox (even though
            document.write is lame).
    
            * fast/url/resources/utilities.js:
            (canonicalize):
            (setBaseURL):
    
    Canonical link: https://commits.webkit.org/49345@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@58064 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/url/resources/utilities.js
+++ LayoutTests/fast/url/resources/utilities.js
@@ -1,13 +1,16 @@
 function canonicalize(url)
 {
-  var a = document.createElement("a");
-  a.href = url;
-  return a.href;
+  // It would be more elegant to use the DOM here, but we use document.write()
+  // so the tests run correctly in Firefox.
+  var id = Math.random();
+  document.write("<a id='" + id + "' href='" + url + "'></a>");
+  return document.getElementById(id).href;
 }
 
 function setBaseURL(url)
 {
-  var base = document.createElement("base");
-  base.href = url;
-  document.documentElement.appendChild(base);
+  // According to the HTML5 spec, we're only supposed to honor <base> elements
+  // in the <head>, but we use document.write() here to make the test run in
+  // Firefox.
+  document.write('<base href="' + url + '">');
 }
