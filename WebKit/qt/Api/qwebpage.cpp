4376c022f35ac3b82428f977e526a066d838248f
Andreas Kling <andreas.kling@nokia.com>
2010-04-29 10:42:59 +0000

    Remove unnecessary call to FrameView::forceLayout() in setViewportSize() This prevents a double relayout on resize.
    Patch by Andreas Kling <andreas.kling@nokia.com> on 2010-04-29
    Reviewed by Simon Hausmann.
    
    https://bugs.webkit.org/show_bug.cgi?id=38179
    
    Thanks to Nate Whetsell <nathan.whetsell@gmail.com> for spotting this.
    
    * Api/qwebpage.cpp:
    (QWebPage::setViewportSize):
    
    Canonical link: https://commits.webkit.org/49752@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@58497 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/qt/Api/qwebpage.cpp
+++ WebKit/qt/Api/qwebpage.cpp
@@ -2068,7 +2068,6 @@ void QWebPage::setViewportSize(const QSize &size) const
     if (frame->d->frame && frame->d->frame->view()) {
         WebCore::FrameView* view = frame->d->frame->view();
         view->setFrameRect(QRect(QPoint(0, 0), size));
-        view->forceLayout();
         view->adjustViewSize();
     }
 }
