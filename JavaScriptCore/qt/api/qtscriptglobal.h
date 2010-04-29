9ca0749b119e1f5db1107e0ed5f7a5a10109abb4
Eric Seidel <eric@webkit.org>
2010-04-29 10:25:16 +0000

    2010-04-29  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Reviewed by Simon Hausmann.
    
            [Qt] Refactor qtscriptglobal.h
            https://bugs.webkit.org/show_bug.cgi?id=37953
    
            Use the same pattern in qtscriptglobal.h as in
            qwebkitglobal.h without checking for specific OSs.
    
            * qt/api/qtscriptglobal.h:
    
    Canonical link: https://commits.webkit.org/49749@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@58494 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/qt/api/qtscriptglobal.h
+++ JavaScriptCore/qt/api/qtscriptglobal.h
@@ -22,23 +22,22 @@
 
 #include <QtCore/qglobal.h>
 
-#if defined(Q_OS_WIN) || defined(Q_OS_SYMBIAN)
-#  if defined(QT_NODLL)
-#  elif defined(QT_MAKEDLL)        /* create a Qt DLL library */
-#    if defined(QT_BUILD_JAVASCRIPT_LIB)
-#      define Q_JAVASCRIPT_EXPORT Q_DECL_EXPORT
-#    else
-#      define Q_JAVASCRIPT_EXPORT Q_DECL_IMPORT
-#    endif
-#  elif defined(QT_DLL) /* use a Qt DLL library */
-#    define Q_JAVASCRIPT_EXPORT
+#if defined(QT_MAKEDLL)        /* create a Qt DLL library */
+#  if defined(QT_BUILD_JAVASCRIPT_LIB)
+#    define Q_JAVASCRIPT_EXPORT Q_DECL_EXPORT
+#  else
+#    define Q_JAVASCRIPT_EXPORT Q_DECL_IMPORT
 #  endif
+#elif defined(QT_DLL) /* use a Qt DLL library */
+#  define Q_JAVASCRIPT_EXPORT
 #endif
 
-#if defined(QT_SHARED)
-#  define Q_JAVASCRIPT_EXPORT Q_DECL_EXPORT
-#else
-#  define Q_JAVASCRIPT_EXPORT
+#if !defined(Q_JAVASCRIPT_EXPORT)
+#  if defined(QT_SHARED)
+#    define Q_JAVASCRIPT_EXPORT Q_DECL_EXPORT
+#  else
+#    define Q_JAVASCRIPT_EXPORT
+#  endif
 #endif
 
 #endif
