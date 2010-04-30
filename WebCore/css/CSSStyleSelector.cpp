8dc56e8ea56672b8ec0f2f3f5391106d0515c66d
Shinichiro Hamaji <hamaji@chromium.org>
2010-04-30 15:46:01 +0000

    2010-04-30  Shinichiro Hamaji  <hamaji@chromium.org>
            Reviewed by Simon Fraser.
    
            SHOULD NEVER BE REACHED assertion loading forbes.com
            https://bugs.webkit.org/show_bug.cgi?id=38272
    
            * printing/pseudo-class-outside-page-expected.txt: Added.
            * printing/pseudo-class-outside-page.html: Added.
    2010-04-30  Shinichiro Hamaji  <hamaji@chromium.org>
    
            Reviewed by Simon Fraser.
    
            SHOULD NEVER BE REACHED assertion loading forbes.com
            https://bugs.webkit.org/show_bug.cgi?id=38272
    
            Ignore page media related pseudo classes.
    
            Test: printing/pseudo-class-outside-page.html
    
            * css/CSSStyleSelector.cpp:
            (WebCore::CSSStyleSelector::SelectorChecker::checkOneSelector):
    
    Canonical link: https://commits.webkit.org/49833@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@58578 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/css/CSSStyleSelector.cpp
+++ WebCore/css/CSSStyleSelector.cpp
@@ -2556,6 +2556,11 @@ bool CSSStyleSelector::SelectorChecker::checkOneSelector(CSSSelector* sel, Eleme
                     break;
                 return true;
             }
+            case CSSSelector::PseudoLeftPage:
+            case CSSSelector::PseudoRightPage:
+            case CSSSelector::PseudoFirstPage:
+                // Page media related pseudo-classes are not handled yet.
+                return false;
             case CSSSelector::PseudoUnknown:
             case CSSSelector::PseudoNotParsed:
             default:
