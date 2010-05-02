3f81d32b17f3d76c3acfccbf8b146d7a74c07a20
Eric Seidel <eric@webkit.org>
2010-05-02 15:27:53 +0000

    2010-05-02  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Timothy Hatcher.
    
            Web Inspector: debugger shortcuts are processed twice if source frame has focus.
    
            https://bugs.webkit.org/show_bug.cgi?id=38431
    
            * inspector/front-end/SourceFrame.js:
            (WebInspector.SourceFrame):
            (WebInspector.SourceFrame.prototype._createViewerIfNeeded):
    
    Canonical link: https://commits.webkit.org/49910@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@58655 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/inspector/front-end/SourceFrame.js
+++ WebCore/inspector/front-end/SourceFrame.js
@@ -39,7 +39,6 @@ WebInspector.SourceFrame = function(parentElement, addBreakpointDelegate, remove
     this._rowMessages = {};
     this._messageBubbles = {};
     this.breakpoints = [];
-    this._shortcuts = {};
 
     this._loaded = false;
 
@@ -172,7 +171,6 @@ WebInspector.SourceFrame.prototype = {
 
         this._textViewer = new WebInspector.TextViewer(this._textModel, WebInspector.platform, this._url);
         var element = this._textViewer.element;
-        element.addEventListener("keydown", this._keyDown.bind(this), true);
         element.addEventListener("contextmenu", this._contextMenu.bind(this), true);
         element.addEventListener("mousedown", this._mouseDown.bind(this), true);
         element.addEventListener("mousemove", this._mouseMove.bind(this), true);
@@ -675,17 +673,6 @@ WebInspector.SourceFrame.prototype = {
         this._conditionEditorElement = editorElement;
 
         return conditionElement;
-    },
-
-    _keyDown: function(event)
-    {
-        var shortcut = WebInspector.KeyboardShortcut.makeKeyFromEvent(event);
-        var handler = this._shortcuts[shortcut];
-        if (handler) {
-            handler(event);
-            event.preventDefault();
-        } else
-            WebInspector.documentKeyDown(event);
     },
 
     _evalSelectionInCallFrame: function(event)
