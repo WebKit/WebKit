3af7a803c92949ec2060548b8e9c4f9c0ac41e9d
Eric Seidel <eric@webkit.org>
2010-05-06 05:11:55 +0000

    2010-05-05  Adam Barth  <abarth@webkit.org>
            Reviewed by Alexey Proskuryakov.
    
            Add a test of URL segmentation
            https://bugs.webkit.org/show_bug.cgi?id=38600
    
            These tests are based on URLParser.Standard from
            http://code.google.com/p/google-url/source/browse/trunk/src/url_parse_unittest.cc
    
            * fast/url/resources/utilities.js:
            (canonicalize):
                - I changed this to use deterministic IDs because (theoretically)
                  the old way was flaky in case the IDs collided.
            (segments):
                - Here we're using HTMLAnchorElement's attributes to infer the
                  internal segmentation.  Unforunately, there don't seem to be
                  accessors for usernames and passwords of URLs.
            * fast/url/script-tests/segments.js: Added.
                - I'm not 100% sure all the test cases here represent the intent of
                  the original test author.  The tricky bit is we don't have a good
                  way to test absolute URL parsing from the web platform, which
                  means a bunch of these become tests of how we handle relative
                  URLs.  However, I'm not one to turn down more test cases.
            * fast/url/script-tests/trivial-segments.js: Added.
                - This is more of a test of the testing harness to make sure its
                  working properly when running on other browsers.
            * fast/url/segments-expected.txt: Added.
            * fast/url/trivial-segments-expected.txt: Added.
                - As usual, these have their "PASS" expectations set to the
                  Chromium behavior but have their -expected.txt set to the normal
                  WebKit behavior, letting us track differences between the two.
            * fast/url/segments.html: Added.
            * fast/url/trivial-segments.html: Added.
    
    Canonical link: https://commits.webkit.org/50106@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@58859 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/url/resources/utilities.js
+++ LayoutTests/fast/url/resources/utilities.js
@@ -1,8 +1,11 @@
+// Start the bidding at 42 for no particular reason.
+var lastID = 42;
+
 function canonicalize(url)
 {
   // It would be more elegant to use the DOM here, but we use document.write()
   // so the tests run correctly in Firefox.
-  var id = Math.random();
+  var id = ++lastID;
   document.write("<a id='" + id + "' href='" + url + "'></a>");
   return document.getElementById(id).href;
 }
@@ -13,4 +16,21 @@ function setBaseURL(url)
   // in the <head>, but we use document.write() here to make the test run in
   // Firefox.
   document.write('<base href="' + url + '">');
+}
+
+function segments(url)
+{
+  // It would be more elegant to use the DOM here, but we use document.write()
+  // so the tests run correctly in Firefox.
+  var id = ++lastID;
+  document.write("<a id='" + id + "' href='" + url + "'></a>");
+  var elmt = document.getElementById(id);
+  return JSON.stringify([
+    elmt.protocol,
+    elmt.hostname,
+    elmt.port,
+    elmt.pathname,
+    elmt.search,
+    elmt.hash
+  ]);
 }
