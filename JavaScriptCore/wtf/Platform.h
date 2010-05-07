89ed03685b4eb54eb4f16929999894a26f855a28
Laszlo Gombos <laszlo.1.gombos@nokia.com>
2010-05-07 06:22:48 +0000

    2010-05-06  Laszlo Gombos  <laszlo.1.gombos@nokia.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Enable YARR_JIT for X86 Mac for QtWebKit
            https://bugs.webkit.org/show_bug.cgi?id=38668
    
            * wtf/Platform.h:
    
    
    
    Canonical link: https://commits.webkit.org/50185@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@58939 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/Platform.h
+++ JavaScriptCore/wtf/Platform.h
@@ -1027,7 +1027,9 @@ on MinGW. See https://bugs.webkit.org/show_bug.cgi?id=29268 */
     || (CPU(X86_64) && OS(LINUX) && GCC_VERSION >= 40100) \
     || (CPU(ARM_TRADITIONAL) && OS(LINUX)) \
     || (CPU(ARM_TRADITIONAL) && OS(SYMBIAN) && COMPILER(RVCT)) \
-    || (CPU(MIPS) && OS(LINUX))
+    || (CPU(MIPS) && OS(LINUX)) \
+    || (CPU(X86) && OS(DARWIN)) \
+    || (CPU(X86_64) && OS(DARWIN))
 #define ENABLE_YARR 1
 #define ENABLE_YARR_JIT 1
 #endif
