fc2a26288d6ba0a8f3c67a9ba0f5d173b63ba909
Simon Hausmann <hausmann@webkit.org>
2010-05-08 07:08:09 +0000

    [Qt] Unreviewed, QtWebKit versioning for the trunk.
    The next version is going to be 2.1, based off the trunk.
    
    * Api/qwebkitglobal.h:
    
    Canonical link: https://commits.webkit.org/50245@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@59004 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/qt/Api/qwebkitglobal.h
+++ WebKit/qt/Api/qwebkitglobal.h
@@ -22,9 +22,9 @@
 
 #include <QtCore/qglobal.h>
 
-#define QTWEBKIT_VERSION_STR "2.0.0"
+#define QTWEBKIT_VERSION_STR "2.1.0"
 // QTWEBKIT_VERSION is (major << 16) + (minor << 8) + patch. Similar to Qt.
-#define QTWEBKIT_VERSION 0x020000
+#define QTWEBKIT_VERSION 0x020100
 // Use: #if (QTWEBKIT_VERSION >= QTWEBKIT_VERSION_CHECK(2, 0, 0)). Similar to Qt.
 #define QTWEBKIT_VERSION_CHECK(major, minor, patch) ((major<<16)|(minor<<8)|(patch))
 
