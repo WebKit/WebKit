d3798c3aa6803bb6f891912bd3e7f8fb2d056ced
Xan Lopez <xan@webkit.org>
2010-06-01 18:08:31 +0000

    2010-06-01  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Gustavo Noronha.
    
            Plug a small leak.
    
            * WebCoreSupport/FrameLoaderClientGtk.cpp:
            (WebKit::FrameLoaderClient::assignIdentifierToInitialRequest):
    
    Canonical link: https://commits.webkit.org/51529@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@60490 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
+++ WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
@@ -235,7 +235,8 @@ void FrameLoaderClient::dispatchWillSendRequest(WebCore::DocumentLoader* loader,
 
 void FrameLoaderClient::assignIdentifierToInitialRequest(unsigned long identifier, WebCore::DocumentLoader*, const ResourceRequest& request)
 {
-    webkit_web_view_add_resource(getViewFromFrame(m_frame), toString(identifier),
+    GOwnPtr<gchar> identifierString(toString(identifier));
+    webkit_web_view_add_resource(getViewFromFrame(m_frame), identifierString.get(),
                                  WEBKIT_WEB_RESOURCE(g_object_new(WEBKIT_TYPE_WEB_RESOURCE, "uri", request.url().string().utf8().data(), 0)));
 }
 
