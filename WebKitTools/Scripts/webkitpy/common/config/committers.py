aebcaa9d5837d9c0a1e4a2c687aec23707be0a68
Mike Fenton <mike.fenton@torchmobile.com>
2010-06-03 13:02:23 +0000

    2010-06-03  Mike Fenton  <mifenton@rim.com>
            Unreviewed.
    
            Reverse e-mail for myself in committers.py to correspond with bugzilla account.
    
            * Scripts/webkitpy/common/config/committers.py:
    
    Canonical link: https://commits.webkit.org/51647@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@60620 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/webkitpy/common/config/committers.py
+++ WebKitTools/Scripts/webkitpy/common/config/committers.py
@@ -140,7 +140,7 @@ committers_unable_to_review = [
     Committer("Martin Robinson", ["mrobinson@webkit.org", "martin.james.robinson@gmail.com"]),
     Committer("Michelangelo De Simone", "michelangelo@webkit.org", "michelangelo"),
     Committer("Mike Belshe", ["mbelshe@chromium.org", "mike@belshe.com"]),
-    Committer("Mike Fenton", ["mike.fenton@torchmobile.com", "mifenton@rim.com"], "mfenton"),
+    Committer("Mike Fenton", ["mifenton@rim.com", "mike.fenton@torchmobile.com"], "mfenton"),
     Committer("Mike Thole", ["mthole@mikethole.com", "mthole@apple.com"]),
     Committer("Mikhail Naganov", "mnaganov@chromium.org"),
     Committer("MORITA Hajime", "morrita@google.com", "morrita"),
