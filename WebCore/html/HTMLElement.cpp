c36a473ff4af8bb895e95e5542c6065bd1a22259
Maciej Stachowiak <mjs@apple.com>
2010-06-12 01:28:18 +0000

    2010-06-11  Maciej Stachowiak  <mjs@apple.com>
            Reviewed by Ojan Vafai.
    
            Implement HTML5 hidden attribute
            https://bugs.webkit.org/show_bug.cgi?id=40511
    
            Test: fast/html/hidden-attr.html
    
            Note: I used the mapped attribute mechanism for this instead of a rule in the UA stylesheet
            to avoid a performance hit from adding a global attribute rule to the UA stylesheet.
    
            * html/HTMLElement.cpp:
            (WebCore::HTMLElement::mapToEntry): Pick up hidden as a global mapped attribute.
            (WebCore::HTMLElement::parseMappedAttribute): Map hidden attribute to display: none.
    2010-06-11  Maciej Stachowiak  <mjs@apple.com>
    
            Reviewed by Ojan Vafai.
    
            Implement HTML5 hidden attribute
            https://bugs.webkit.org/show_bug.cgi?id=40511
    
            * fast/html/hidden-attr-expected.txt: Added.
            * fast/html/hidden-attr.html: Added. Test static and dynamic cases of hidden attribute.
    
    
    Canonical link: https://commits.webkit.org/52067@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@61052 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/html/HTMLElement.cpp
+++ WebCore/html/HTMLElement.cpp
@@ -129,8 +129,9 @@ int HTMLElement::tagPriority() const
 
 bool HTMLElement::mapToEntry(const QualifiedName& attrName, MappedAttributeEntry& result) const
 {
-    if (attrName == alignAttr ||
-        attrName == contenteditableAttr) {
+    if (attrName == alignAttr
+        || attrName == contenteditableAttr
+        || attrName == hiddenAttr) {
         result = eUniversal;
         return false;
     }
@@ -153,6 +154,8 @@ void HTMLElement::parseMappedAttribute(Attribute* attr)
             addCSSProperty(attr, CSSPropertyTextAlign, "center");
         else
             addCSSProperty(attr, CSSPropertyTextAlign, attr->value());
+    } else if (attr->name() == hiddenAttr) {
+        addCSSProperty(attr, CSSPropertyDisplay, CSSValueNone);
     } else if (attr->name() == contenteditableAttr) {
         setContentEditable(attr);
     } else if (attr->name() == tabindexAttr) {
