adb22ca30cb133e9753620ca39a952b5bc3c591f
Eric Seidel <eric@webkit.org>
2010-06-13 05:39:00 +0000

    2010-06-12  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r61031.
            http://trac.webkit.org/changeset/61031
            https://bugs.webkit.org/show_bug.cgi?id=40539
    
            Crashes Dromaeo jslib test in Release Chromium (Requested by
            dimich_ on #webkit).
    
            * bindings/v8/V8Binding.cpp:
            (WebCore::v8ExternalString):
            * bindings/v8/V8Binding.h:
    
    Canonical link: https://commits.webkit.org/52095@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@61082 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/bindings/v8/V8Binding.cpp
+++ WebCore/bindings/v8/V8Binding.cpp
@@ -442,29 +442,25 @@ static void cachedStringCallback(v8::Persistent<v8::Value> wrapper, void* parame
     stringImpl->deref();
 }
 
-RefPtr<StringImpl> lastStringImpl = 0;
-v8::Persistent<v8::String> lastV8String;
-
-v8::Local<v8::String> v8ExternalStringSlow(StringImpl* stringImpl)
+v8::Local<v8::String> v8ExternalString(const String& string)
 {
-    if (!stringImpl->length())
+    StringImpl* stringImpl = string.impl();
+    if (!stringImpl || !stringImpl->length())
         return v8::String::Empty();
 
     if (!stringImplCacheEnabled)
-        return makeExternalString(String(stringImpl));
+        return makeExternalString(string);
 
     StringCache& stringCache = getStringCache();
     v8::String* cachedV8String = stringCache.get(stringImpl);
-    if (cachedV8String) {
+    if (cachedV8String)
+    {
         v8::Persistent<v8::String> handle(cachedV8String);
-        if (!handle.IsNearDeath() && !handle.IsEmpty()) {
-            lastStringImpl = stringImpl;
-            lastV8String = handle;
+        if (!handle.IsNearDeath() && !handle.IsEmpty())
             return v8::Local<v8::String>::New(handle);
-        }
     }
 
-    v8::Local<v8::String> newString = makeExternalString(String(stringImpl));
+    v8::Local<v8::String> newString = makeExternalString(string);
     if (newString.IsEmpty())
         return newString;
 
@@ -475,9 +471,6 @@ v8::Local<v8::String> v8ExternalStringSlow(StringImpl* stringImpl)
     stringImpl->ref();
     wrapper.MakeWeak(stringImpl, cachedStringCallback);
     stringCache.set(stringImpl, *wrapper);
-
-    lastStringImpl = stringImpl;
-    lastV8String = wrapper;
 
     return newString;
 }
