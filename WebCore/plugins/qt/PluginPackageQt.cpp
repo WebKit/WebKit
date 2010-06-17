5a9a77b9a3a7247788d4cd3fdd72d4543e73f41f
Dawit Alemayehu <adawit@kde.org>
2010-06-17 05:38:10 +0000

    [Qt] QtWebKit crashes while initializing flash plugin 10.1.53.64. https://bugs.webkit.org/show_bug.cgi?id=40567
    Patch by Dawit Alemayehu <adawit@kde.org> on 2010-06-16
    Reviewed by Simon Hausmann.
    
    Avoid preventable crashes by ensuring gtk_init() is called in the
    flash viewer plugins before calling NP_Initialize.
    
    * plugins/qt/PluginPackageQt.cpp:
    (WebCore::PluginPackage::load):
    
    Canonical link: https://commits.webkit.org/52317@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@61307 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/plugins/qt/PluginPackageQt.cpp
+++ WebCore/plugins/qt/PluginPackageQt.cpp
@@ -35,6 +35,8 @@
 
 namespace WebCore {
 
+typedef void gtkInitFunc(int *argc, char ***argv);
+
 bool PluginPackage::fetchInfo()
 {
     if (!load())
@@ -109,6 +111,7 @@ bool PluginPackage::load()
 
     NP_InitializeFuncPtr NP_Initialize;
     NPError npErr;
+    gtkInitFunc* gtkInit;
 
     NP_Initialize = (NP_InitializeFuncPtr)m_module->resolve("NP_Initialize");
     m_NPP_Shutdown = (NPP_ShutdownProcPtr)m_module->resolve("NP_Shutdown");
@@ -125,6 +128,26 @@ bool PluginPackage::load()
         // nspluginwrapper relies on the toolkit value to know if glib is available
         // It does so in NP_Initialize with a null instance, therefore it is done this way:
         m_browserFuncs.getvalue = staticPluginQuirkRequiresGtkToolKit_NPN_GetValue;
+    }
+
+    // WORKAROUND: Prevent gtk based plugin crashes such as BR# 40567 by
+    // explicitly forcing the initializing of Gtk, i.e. calling gtk_init,
+    // whenver the symbol is present in the plugin library loaded above.
+    // Note that this workaround is based on code from the NSPluginClass ctor
+    // in KDE's kdebase/apps/nsplugins/viewer/nsplugin.cpp file.
+    gtkInit = (gtkInitFunc*)m_module->resolve("gtk_init");
+    if (gtkInit) {
+        // Prevent gtk_init() from replacing the X error handlers, since the Gtk
+        // handlers abort when they receive an X error, thus killing the viewer.
+#ifdef Q_WS_X11
+        int (*old_error_handler)(Display*, XErrorEvent*) = XSetErrorHandler(0);
+        int (*old_io_error_handler)(Display*) = XSetIOErrorHandler(0);
+#endif
+        gtkInit(0, 0);
+#ifdef Q_WS_X11
+        XSetErrorHandler(old_error_handler);
+        XSetIOErrorHandler(old_io_error_handler);
+#endif
     }
 
 #if defined(XP_UNIX)
