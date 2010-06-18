07427df3881d8f1953ea52efbe05444859f0abfb
Adam Barth <abarth@webkit.org>
2010-06-18 08:58:22 +0000

    2010-06-18  Adam Barth  <abarth@webkit.org>
            Rubber-stamped by Eric Seidel.
    
            Fix the namespace indent for HTML5Lexer.  This patch changes
            white-space only.
    
            * html/HTML5Lexer.h:
            (WebCore::HTML5Lexer::):
            (WebCore::HTML5Lexer::lineNumber):
            (WebCore::HTML5Lexer::columnNumber):
            (WebCore::HTML5Lexer::state):
            (WebCore::HTML5Lexer::setState):
            (WebCore::HTML5Lexer::skipLeadingNewLineForListing):
            (WebCore::HTML5Lexer::InputStreamPreprocessor::InputStreamPreprocessor):
            (WebCore::HTML5Lexer::InputStreamPreprocessor::nextInputCharacter):
            (WebCore::HTML5Lexer::InputStreamPreprocessor::peek):
            (WebCore::HTML5Lexer::InputStreamPreprocessor::advance):
    
    Canonical link: https://commits.webkit.org/52396@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@61386 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/html/HTML5Lexer.h
+++ WebCore/html/HTML5Lexer.h
@@ -34,199 +34,199 @@
 
 namespace WebCore {
 
-    class HTML5Token;
+class HTML5Token;
 
-    class HTML5Lexer : public Noncopyable {
+class HTML5Lexer : public Noncopyable {
+public:
+    enum State {
+        DataState,
+        CharacterReferenceInDataState,
+        RCDATAState,
+        CharacterReferenceInRCDATAState,
+        RAWTEXTState,
+        ScriptDataState,
+        PLAINTEXTState,
+        TagOpenState,
+        EndTagOpenState,
+        TagNameState,
+        RCDATALessThanSignState,
+        RCDATAEndTagOpenState,
+        RCDATAEndTagNameState,
+        RAWTEXTLessThanSignState,
+        RAWTEXTEndTagOpenState,
+        RAWTEXTEndTagNameState,
+        ScriptDataLessThanSignState,
+        ScriptDataEndTagOpenState,
+        ScriptDataEndTagNameState,
+        ScriptDataEscapeStartState,
+        ScriptDataEscapeStartDashState,
+        ScriptDataEscapedState,
+        ScriptDataEscapedDashState,
+        ScriptDataEscapedDashDashState,
+        ScriptDataEscapedLessThanSignState,
+        ScriptDataEscapedEndTagOpenState,
+        ScriptDataEscapedEndTagNameState,
+        ScriptDataDoubleEscapeStartState,
+        ScriptDataDoubleEscapedState,
+        ScriptDataDoubleEscapedDashState,
+        ScriptDataDoubleEscapedDashDashState,
+        ScriptDataDoubleEscapedLessThanSignState,
+        ScriptDataDoubleEscapeEndState,
+        BeforeAttributeNameState,
+        AttributeNameState,
+        AfterAttributeNameState,
+        BeforeAttributeValueState,
+        AttributeValueDoubleQuotedState,
+        AttributeValueSingleQuotedState,
+        AttributeValueUnquotedState,
+        CharacterReferenceInAttributeValueState,
+        AfterAttributeValueQuotedState,
+        SelfClosingStartTagState,
+        BogusCommentState,
+        MarkupDeclarationOpenState,
+        CommentStartState,
+        CommentStartDashState,
+        CommentState,
+        CommentEndDashState,
+        CommentEndState,
+        CommentEndBangState,
+        CommentEndSpaceState,
+        DOCTYPEState,
+        BeforeDOCTYPENameState,
+        DOCTYPENameState,
+        AfterDOCTYPENameState,
+        AfterDOCTYPEPublicKeywordState,
+        BeforeDOCTYPEPublicIdentifierState,
+        DOCTYPEPublicIdentifierDoubleQuotedState,
+        DOCTYPEPublicIdentifierSingleQuotedState,
+        AfterDOCTYPEPublicIdentifierState,
+        BetweenDOCTYPEPublicAndSystemIdentifiersState,
+        AfterDOCTYPESystemKeywordState,
+        BeforeDOCTYPESystemIdentifierState,
+        DOCTYPESystemIdentifierDoubleQuotedState,
+        DOCTYPESystemIdentifierSingleQuotedState,
+        AfterDOCTYPESystemIdentifierState,
+        BogusDOCTYPEState,
+        CDATASectionState,
+    };
+
+    HTML5Lexer();
+    ~HTML5Lexer();
+
+    void reset();
+
+    // This function returns true if it emits a token.  Otherwise, callers
+    // must provide the same (in progress) token on the next call (unless
+    // they call reset() first).
+    bool nextToken(SegmentedString&, HTML5Token&);
+
+    int lineNumber() const { return m_lineNumber; }
+    int columnNumber() const { return 1; } // Matches HTMLDocumentParser.h behavior.
+
+    State state() const { return m_state; }
+    void setState(State state) { m_state = state; }
+
+    // Hack to skip leading newline in <pre>/<listing> for authoring ease.
+    // http://www.whatwg.org/specs/web-apps/current-work/multipage/tokenization.html#parsing-main-inbody
+    void skipLeadingNewLineForListing() { m_skipLeadingNewLineForListing = true; }
+
+private:
+    // http://www.whatwg.org/specs/web-apps/current-work/#preprocessing-the-input-stream
+    class InputStreamPreprocessor : public Noncopyable {
     public:
-        enum State {
-            DataState,
-            CharacterReferenceInDataState,
-            RCDATAState,
-            CharacterReferenceInRCDATAState,
-            RAWTEXTState,
-            ScriptDataState,
-            PLAINTEXTState,
-            TagOpenState,
-            EndTagOpenState,
-            TagNameState,
-            RCDATALessThanSignState,
-            RCDATAEndTagOpenState,
-            RCDATAEndTagNameState,
-            RAWTEXTLessThanSignState,
-            RAWTEXTEndTagOpenState,
-            RAWTEXTEndTagNameState,
-            ScriptDataLessThanSignState,
-            ScriptDataEndTagOpenState,
-            ScriptDataEndTagNameState,
-            ScriptDataEscapeStartState,
-            ScriptDataEscapeStartDashState,
-            ScriptDataEscapedState,
-            ScriptDataEscapedDashState,
-            ScriptDataEscapedDashDashState,
-            ScriptDataEscapedLessThanSignState,
-            ScriptDataEscapedEndTagOpenState,
-            ScriptDataEscapedEndTagNameState,
-            ScriptDataDoubleEscapeStartState,
-            ScriptDataDoubleEscapedState,
-            ScriptDataDoubleEscapedDashState,
-            ScriptDataDoubleEscapedDashDashState,
-            ScriptDataDoubleEscapedLessThanSignState,
-            ScriptDataDoubleEscapeEndState,
-            BeforeAttributeNameState,
-            AttributeNameState,
-            AfterAttributeNameState,
-            BeforeAttributeValueState,
-            AttributeValueDoubleQuotedState,
-            AttributeValueSingleQuotedState,
-            AttributeValueUnquotedState,
-            CharacterReferenceInAttributeValueState,
-            AfterAttributeValueQuotedState,
-            SelfClosingStartTagState,
-            BogusCommentState,
-            MarkupDeclarationOpenState,
-            CommentStartState,
-            CommentStartDashState,
-            CommentState,
-            CommentEndDashState,
-            CommentEndState,
-            CommentEndBangState,
-            CommentEndSpaceState,
-            DOCTYPEState,
-            BeforeDOCTYPENameState,
-            DOCTYPENameState,
-            AfterDOCTYPENameState,
-            AfterDOCTYPEPublicKeywordState,
-            BeforeDOCTYPEPublicIdentifierState,
-            DOCTYPEPublicIdentifierDoubleQuotedState,
-            DOCTYPEPublicIdentifierSingleQuotedState,
-            AfterDOCTYPEPublicIdentifierState,
-            BetweenDOCTYPEPublicAndSystemIdentifiersState,
-            AfterDOCTYPESystemKeywordState,
-            BeforeDOCTYPESystemIdentifierState,
-            DOCTYPESystemIdentifierDoubleQuotedState,
-            DOCTYPESystemIdentifierSingleQuotedState,
-            AfterDOCTYPESystemIdentifierState,
-            BogusDOCTYPEState,
-            CDATASectionState,
-        };
+        InputStreamPreprocessor()
+            : m_nextInputCharacter('\0')
+            , m_skipNextNewLine(false)
+        {
+        }
 
-        HTML5Lexer();
-        ~HTML5Lexer();
+        UChar nextInputCharacter() const { return m_nextInputCharacter; }
 
-        void reset();
-
-        // This function returns true if it emits a token.  Otherwise, callers
-        // must provide the same (in progress) token on the next call (unless
-        // they call reset() first).
-        bool nextToken(SegmentedString&, HTML5Token&);
-
-        int lineNumber() const { return m_lineNumber; }
-        int columnNumber() const { return 1; } // Matches HTMLDocumentParser.h behavior.
-
-        State state() const { return m_state; }
-        void setState(State state) { m_state = state; }
-
-        // Hack to skip leading newline in <pre>/<listing> for authoring ease.
-        // http://www.whatwg.org/specs/web-apps/current-work/multipage/tokenization.html#parsing-main-inbody
-        void skipLeadingNewLineForListing() { m_skipLeadingNewLineForListing = true; }
-
-    private:
-        // http://www.whatwg.org/specs/web-apps/current-work/#preprocessing-the-input-stream
-        class InputStreamPreprocessor : public Noncopyable {
-        public:
-            InputStreamPreprocessor()
-                : m_nextInputCharacter('\0')
-                , m_skipNextNewLine(false)
-            {
-            }
-
-            UChar nextInputCharacter() const { return m_nextInputCharacter; }
-
-            // Returns whether we succeeded in peeking at the next character.
-            // The only way we can fail to peek is if there are no more
-            // characters in |source| (after collapsing \r\n, etc).
-            bool peek(SegmentedString& source, int& lineNumber)
-            {
-                m_nextInputCharacter = *source;
-                if (m_nextInputCharacter == '\n' && m_skipNextNewLine) {
-                    m_skipNextNewLine = false;
-                    source.advancePastNewline(lineNumber);
-                    if (source.isEmpty())
-                        return false;
-                    m_nextInputCharacter = *source;
-                }
-                if (m_nextInputCharacter == '\r') {
-                    m_nextInputCharacter = '\n';
-                    m_skipNextNewLine = true;
-                } else {
-                    m_skipNextNewLine = false;
-                    // FIXME: The spec indicates that the surrogate pair range as well as
-                    // a number of specific character values are parse errors and should be replaced
-                    // by the replacement character. We suspect this is a problem with the spec as doing
-                    // that filtering breaks surrogate pair handling and causes us not to match Minefield.
-                    if (m_nextInputCharacter == '\0')
-                        m_nextInputCharacter = 0xFFFD;
-                }
-                return true;
-            }
-
-            // Returns whether there are more characters in |source| after advancing.
-            bool advance(SegmentedString& source, int& lineNumber)
-            {
-                source.advance(lineNumber);
+        // Returns whether we succeeded in peeking at the next character.
+        // The only way we can fail to peek is if there are no more
+        // characters in |source| (after collapsing \r\n, etc).
+        bool peek(SegmentedString& source, int& lineNumber)
+        {
+            m_nextInputCharacter = *source;
+            if (m_nextInputCharacter == '\n' && m_skipNextNewLine) {
+                m_skipNextNewLine = false;
+                source.advancePastNewline(lineNumber);
                 if (source.isEmpty())
                     return false;
-                return peek(source, lineNumber);
+                m_nextInputCharacter = *source;
             }
+            if (m_nextInputCharacter == '\r') {
+                m_nextInputCharacter = '\n';
+                m_skipNextNewLine = true;
+            } else {
+                m_skipNextNewLine = false;
+                // FIXME: The spec indicates that the surrogate pair range as well as
+                // a number of specific character values are parse errors and should be replaced
+                // by the replacement character. We suspect this is a problem with the spec as doing
+                // that filtering breaks surrogate pair handling and causes us not to match Minefield.
+                if (m_nextInputCharacter == '\0')
+                    m_nextInputCharacter = 0xFFFD;
+            }
+            return true;
+        }
 
-        private:
-            // http://www.whatwg.org/specs/web-apps/current-work/#next-input-character
-            UChar m_nextInputCharacter;
-            bool m_skipNextNewLine;
-        };
+        // Returns whether there are more characters in |source| after advancing.
+        bool advance(SegmentedString& source, int& lineNumber)
+        {
+            source.advance(lineNumber);
+            if (source.isEmpty())
+                return false;
+            return peek(source, lineNumber);
+        }
 
-        inline void emitCharacter(UChar);
-        inline void emitParseError();
-        inline void emitCurrentToken();
-        inline void emitCodePoint(unsigned);
-
-        inline bool processEntity(SegmentedString& source);
-
-        inline bool temporaryBufferIs(const String&);
-
-        // Sometimes we speculatively consume input characters and we don't
-        // know whether they represent end tags or RCDATA, etc.  These
-        // functions help manage these state.
-        inline void addToPossibleEndTag(UChar cc);
-        inline bool isAppropriateEndTag();
-
-        inline bool shouldEmitBufferedCharacterToken(const SegmentedString&);
-
-        State m_state;
-
-        Vector<UChar, 32> m_appropriateEndTagName;
-
-        // m_token is owned by the caller.  If nextToken is not on the stack,
-        // this member might be pointing to unallocated memory.
-        HTML5Token* m_token;
-        int m_lineNumber;
-
-        bool m_skipLeadingNewLineForListing;
-
-        // http://www.whatwg.org/specs/web-apps/current-work/#temporary-buffer
-        Vector<UChar, 32> m_temporaryBuffer;
-
-        // We occationally want to emit both a character token and an end tag
-        // token (e.g., when lexing script).  We buffer the name of the end tag
-        // token here so we remember it next time we re-enter the lexer.
-        Vector<UChar, 32> m_bufferedEndTagName;
-
-        // http://www.whatwg.org/specs/web-apps/current-work/#additional-allowed-character
-        UChar m_additionalAllowedCharacter;
-
-        // http://www.whatwg.org/specs/web-apps/current-work/#preprocessing-the-input-stream
-        InputStreamPreprocessor m_inputStreamPreprocessor;
+    private:
+        // http://www.whatwg.org/specs/web-apps/current-work/#next-input-character
+        UChar m_nextInputCharacter;
+        bool m_skipNextNewLine;
     };
+
+    inline void emitCharacter(UChar);
+    inline void emitParseError();
+    inline void emitCurrentToken();
+    inline void emitCodePoint(unsigned);
+
+    inline bool processEntity(SegmentedString& source);
+
+    inline bool temporaryBufferIs(const String&);
+
+    // Sometimes we speculatively consume input characters and we don't
+    // know whether they represent end tags or RCDATA, etc.  These
+    // functions help manage these state.
+    inline void addToPossibleEndTag(UChar cc);
+    inline bool isAppropriateEndTag();
+
+    inline bool shouldEmitBufferedCharacterToken(const SegmentedString&);
+
+    State m_state;
+
+    Vector<UChar, 32> m_appropriateEndTagName;
+
+    // m_token is owned by the caller.  If nextToken is not on the stack,
+    // this member might be pointing to unallocated memory.
+    HTML5Token* m_token;
+    int m_lineNumber;
+
+    bool m_skipLeadingNewLineForListing;
+
+    // http://www.whatwg.org/specs/web-apps/current-work/#temporary-buffer
+    Vector<UChar, 32> m_temporaryBuffer;
+
+    // We occationally want to emit both a character token and an end tag
+    // token (e.g., when lexing script).  We buffer the name of the end tag
+    // token here so we remember it next time we re-enter the lexer.
+    Vector<UChar, 32> m_bufferedEndTagName;
+
+    // http://www.whatwg.org/specs/web-apps/current-work/#additional-allowed-character
+    UChar m_additionalAllowedCharacter;
+
+    // http://www.whatwg.org/specs/web-apps/current-work/#preprocessing-the-input-stream
+    InputStreamPreprocessor m_inputStreamPreprocessor;
+};
 
 }
 
