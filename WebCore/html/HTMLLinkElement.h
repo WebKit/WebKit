87c40ac67d931f8636fb0890f57fb9299aac0577
Andy Estes <aestes@apple.com>
2010-06-18 19:11:26 +0000

    Update copyright header.
    Rubber-stamped by Sam Weinig.
    
    * html/HTMLLinkElement.h:
    
    
    
    Canonical link: https://commits.webkit.org/52435@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@61425 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/html/HTMLLinkElement.h
+++ WebCore/html/HTMLLinkElement.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
- * Copyright (C) 2003, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2003, 2008, 2010 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
