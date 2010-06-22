fa3d1316606f41f7a8a266e3bb019760aa7e1ef4
Darin Adler <darin@apple.com>
2010-06-22 20:30:42 +0000

    Alexey asked me to take out the autorelease pools because he believes that there is no code that should be running using Objective-C autorelease. Most likely, the code that is triggering these stderr messages is incorrect, and that bug should be fixed instead.
    Reviewed by Alexey Proskuryakov.
    
    * workers/WorkerRunLoop.cpp:
    (WebCore::WorkerRunLoop::run): Roll out use of AutodrainedPool.
    
    
    
    Canonical link: https://commits.webkit.org/52635@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@61627 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/workers/WorkerRunLoop.cpp
+++ WebCore/workers/WorkerRunLoop.cpp
@@ -32,7 +32,6 @@
 
 #if ENABLE(WORKERS)
 
-#include "AutodrainedPool.h"
 #include "ScriptExecutionContext.h"
 #include "SharedTimer.h"
 #include "ThreadGlobalData.h"
@@ -127,13 +126,11 @@ private:
 
 void WorkerRunLoop::run(WorkerContext* context)
 {
-    AutodrainedPool pool;
     RunLoopSetup setup(*this);
     ModePredicate modePredicate(defaultMode());
     MessageQueueWaitResult result;
     do {
         result = runInMode(context, modePredicate);
-        pool.cycle();
     } while (result != MessageQueueTerminated);
 }
 
