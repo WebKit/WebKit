9adcc16618fc136e175f9f8aa6eedcb912e86c8e
Bea Lam <bea.lam@nokia.com>
2010-07-01 20:00:42 +0000

    [Qt] Doc improvements for QDeclarativeWebView
    Patch by Bea Lam <bea.lam@nokia.com> on 2010-07-01
    Reviewed by Simon Hausmann.
    
    * declarative/qdeclarativewebview.cpp:
    
    Canonical link: https://commits.webkit.org/53239@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@62289 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/qt/declarative/qdeclarativewebview.cpp
+++ WebKit/qt/declarative/qdeclarativewebview.cpp
@@ -180,6 +180,8 @@ void GraphicsWebView::mouseMoveEvent(QGraphicsSceneMouseEvent* event)
 
     When this item has keyboard focus, all keyboard input will be sent directly to the
     web page within.
+
+    \sa {declarative/modelviews/webview}{WebView example}, {demos/declarative/webbrowser}{Web Browser demo}
 */
 
 /*!
