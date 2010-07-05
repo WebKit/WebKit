1a017b957ee30c0ac04e65aabe84fba26f59d62b
Nikolas Zimmermann <zimmermann@webkit.org>
2010-07-05 12:48:28 +0000

    2010-07-05  Nikolas Zimmermann  <nzimmermann@rim.com>
            Reviewed by Dirk Schulze / Darin Adler.
    
            Node.cloneNode does not work on SVG nodes
            https://bugs.webkit.org/show_bug.cgi?id=41421
    
            Be sure to synchronize animated SVG properties before checking whether NamedNodeMap exists.
            When creating a SVG element from JS, and setting all attributes via SVG DOM, and not using setAttribute
            the NamedNodeMap does not exist. When cloning such an element, be sure to synchronize SVG <-> XML DOM
            attributes before attempting to clone, otherwhise the SVG animated properties are lost while cloning.
    
            Test: svg/custom/clone-element-with-animated-svg-properties.html
    
            * dom/Element.cpp:
            (WebCore::Element::cloneElementWithoutChildren):
    
    
    Canonical link: https://commits.webkit.org/53399@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@62484 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/dom/Element.cpp
+++ WebCore/dom/Element.cpp
@@ -154,10 +154,10 @@ PassRefPtr<Element> Element::cloneElementWithoutChildren()
     // This is a sanity check as HTML overloads some of the DOM methods.
     ASSERT(isHTMLElement() == clone->isHTMLElement());
 
-    // Clone attributes.
-    if (namedAttrMap)
-        clone->attributes()->setAttributes(*attributes(true)); // Call attributes(true) to force attribute synchronization to occur (for svg and style) before cloning happens.
-    
+    // Call attributes(true) to force attribute synchronization to occur for SVG and style attributes.
+    if (NamedNodeMap* attributeMap = attributes(true))
+        clone->attributes()->setAttributes(*attributeMap);
+
     clone->copyNonAttributeProperties(this);
 
     return clone.release();
