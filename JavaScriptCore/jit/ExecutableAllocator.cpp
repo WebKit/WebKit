c5ccb7b248bbd1b90dbb31efe306253f5c16caac
Gavin Barraclough <barraclough@apple.com>
2010-07-06 00:22:42 +0000

    https://bugs.webkit.org/show_bug.cgi?id=41641
    Reviewed by Sam Weinig.
    
    Update compile flags to allow use of ExecutableAllocatorFixedVMPool on platforms
    other than x86-64 (this may be useful on 32-bit platforms, too).
    
    Simplify ifdefs by dividing into thwo broad allocation strategies
    (ENABLE_EXECUTABLE_ALLOCATOR_FIXED & ENABLE_EXECUTABLE_ALLOCATOR_DEMAND).
    
    Rename constant used in the code to have names descriptive of their purpose,
    rather than their specific value on a given platform.
    
    * jit/ExecutableAllocator.cpp:
    (JSC::ExecutableAllocator::reprotectRegion):
    (JSC::ExecutableAllocator::cacheFlush):
    * jit/ExecutableAllocatorFixedVMPool.cpp:
    (JSC::FixedVMPoolAllocator::FixedVMPoolAllocator):
    (JSC::FixedVMPoolAllocator::free):
    (JSC::ExecutablePool::systemAlloc):
    * jit/ExecutableAllocatorPosix.cpp:
    * jit/ExecutableAllocatorSymbian.cpp:
    * jit/ExecutableAllocatorWin.cpp:
    * wtf/Platform.h:
    
    
    
    Canonical link: https://commits.webkit.org/53426@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@62511 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/jit/ExecutableAllocator.cpp
+++ JavaScriptCore/jit/ExecutableAllocator.cpp
@@ -33,6 +33,42 @@ namespace JSC {
 
 size_t ExecutableAllocator::pageSize = 0;
 
+#if ENABLE(ASSEMBLER_WX_EXCLUSIVE)
+void ExecutableAllocator::reprotectRegion(void* start, size_t size, ProtectionSeting setting)
+{
+    if (!pageSize)
+        intializePageSize();
+
+    // Calculate the start of the page containing this region,
+    // and account for this extra memory within size.
+    intptr_t startPtr = reinterpret_cast<intptr_t>(start);
+    intptr_t pageStartPtr = startPtr & ~(pageSize - 1);
+    void* pageStart = reinterpret_cast<void*>(pageStartPtr);
+    size += (startPtr - pageStartPtr);
+
+    // Round size up
+    size += (pageSize - 1);
+    size &= ~(pageSize - 1);
+
+    mprotect(pageStart, size, (setting == Writable) ? PROTECTION_FLAGS_RW : PROTECTION_FLAGS_RX);
+}
+#endif
+
+#if CPU(ARM_TRADITIONAL) && OS(LINUX) && COMPILER(RVCT)
+__asm void ExecutableAllocator::cacheFlush(void* code, size_t size)
+{
+    ARM
+    push {r7}
+    add r1, r1, r0
+    mov r7, #0xf0000
+    add r7, r7, #0x2
+    mov r2, #0x0
+    svc #0x0
+    pop {r7}
+    bx lr
+}
+#endif
+
 }
 
 #endif // HAVE(ASSEMBLER)
