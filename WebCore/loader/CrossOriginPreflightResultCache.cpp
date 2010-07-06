be2daf33e62fad9493cd458a6a6fd21e7866b186
Alexey Proskuryakov <ap@apple.com>
2010-07-06 20:51:46 +0000

    Not reviewed, fixing a simple copy/paste mistake.
            https://bugs.webkit.org/show_bug.cgi?id=41156
            Cross origin XMLHttpRequest should log the reason why connection failed
    
            Covered by xmlhttprequest/access-control-basic-non-simple-deny-cached.html
    
            * loader/CrossOriginPreflightResultCache.cpp:
            (WebCore::CrossOriginPreflightResultCacheItem::allowsCrossOriginHeaders): Fix incorrectly
            copy/pasted header field name.
    
    
    
    Canonical link: https://commits.webkit.org/53507@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@62592 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/CrossOriginPreflightResultCache.cpp
+++ WebCore/loader/CrossOriginPreflightResultCache.cpp
@@ -128,7 +128,7 @@ bool CrossOriginPreflightResultCacheItem::allowsCrossOriginHeaders(const HTTPHea
     HTTPHeaderMap::const_iterator end = requestHeaders.end();
     for (HTTPHeaderMap::const_iterator it = requestHeaders.begin(); it != end; ++it) {
         if (!m_headers.contains(it->first) && !isOnAccessControlSimpleRequestHeaderWhitelist(it->first, it->second)) {
-            errorDescription = "Request header field " + it->first + " is not allowed by Access-Control-Allow-Methods response header field.";
+            errorDescription = "Request header field " + it->first + " is not allowed by Access-Control-Allow-Headers response header field.";
             return false;
         }
     }
