6f77ba406c6fee6f0ffa7e208d9082f674d6e65b
Vitaly Repeshko <vitalyr@chromium.org>
2010-07-09 17:06:04 +0000

    2010-07-09  Vitaly Repeshko  <vitalyr@chromium.org>
            Reviewed by Pavel Feldman.
    
            [v8] Call JS gc in a fresh context to avoid retaining the current one.
            https://bugs.webkit.org/show_bug.cgi?id=41963
            http://crbug.com/46571
    
            * bindings/v8/ScriptController.cpp:
            (WebCore::ScriptController::collectGarbage):
    
    
    Canonical link: https://commits.webkit.org/53852@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@62962 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/bindings/v8/ScriptController.cpp
+++ WebCore/bindings/v8/ScriptController.cpp
@@ -290,13 +290,19 @@ void ScriptController::bindToWindowObject(Frame* frame, const String& key, NPObj
 void ScriptController::collectGarbage()
 {
     v8::HandleScope handleScope;
-    v8::Handle<v8::Context> v8Context = V8Proxy::mainWorldContext(m_proxy->frame());
+
+    v8::Persistent<v8::Context> v8Context = v8::Context::New();
     if (v8Context.IsEmpty())
         return;
-
-    v8::Context::Scope scope(v8Context);
-
-    m_proxy->evaluate(ScriptSourceCode("if (window.gc) void(gc());"), 0);
+    {
+        v8::Context::Scope scope(v8Context);
+        v8::Local<v8::String> source = v8::String::New("if (gc) gc();");
+        v8::Local<v8::String> name = v8::String::New("gc");
+        v8::Handle<v8::Script> script = v8::Script::Compile(source, name);
+        if (!script.IsEmpty())
+            script->Run();
+    }
+    v8Context.Dispose();
 }
 
 void ScriptController::lowMemoryNotification()
