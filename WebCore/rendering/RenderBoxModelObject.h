2f6a4799bf69b98ff1adc4521e2760d720867854
Mark Rowe <mrowe@apple.com>
2010-07-10 01:54:21 +0000

    Merge r61341.
    Canonical link: https://commits.webkit.org/50175.254@safari-533.17-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-533.17-branch@63029 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderBoxModelObject.h
+++ WebCore/rendering/RenderBoxModelObject.h
@@ -2,6 +2,7 @@
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  * Copyright (C) 2003, 2006, 2007, 2009 Apple Inc. All rights reserved.
+ * Copyright (C) 2010 Google Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -105,6 +106,8 @@ public:
 
 protected:
     void calculateBackgroundImageGeometry(const FillLayer*, int tx, int ty, int w, int h, IntRect& destRect, IntPoint& phase, IntSize& tileSize);
+
+    bool shouldPaintAtLowQuality(GraphicsContext*, Image*, const IntSize&);
 
 private:
     virtual bool isBoxModelObject() const { return true; }
