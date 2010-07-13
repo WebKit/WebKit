52176cdd90af28e156767a0d281bb8c67ab60a46
Philippe Normand <philn@igalia.com>
2010-07-13 10:05:42 +0000

    2010-07-13  Philippe Normand  <pnormand@igalia.com>
            Rubber-stamped by Xan Lopez.
    
            Updated the copyright headers of the files modified in r62476.
    
            * WebView/WebView.mm:
            * WebView/WebViewData.h:
            * WebView/WebViewInternal.h:
    
    Canonical link: https://commits.webkit.org/54044@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@63188 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/mac/WebView/WebView.mm
+++ WebKit/mac/WebView/WebView.mm
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Apple Inc. All rights reserved.
  * Copyright (C) 2006 David Smith (catfish.man@gmail.com)
+ * Copyright (C) 2010 Igalia S.L
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
--- WebKit/mac/WebView/WebViewInternal.h
+++ WebKit/mac/WebView/WebViewInternal.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2005, 2006, 2007, 2008, 2009 Apple Inc. All rights reserved.
+ * Copyright (C) 2010 Igalia S.L
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
