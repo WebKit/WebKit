b652685736c35c64148fe9ce58d8d15ca757032a
Commit Queue <commit-queue@webkit.org>
2010-07-14 08:49:57 +0000

    2010-07-14  Lucas De Marchi  <lucas.demarchi@profusion.mobi>
            Reviewed by Antonio Gomes.
    
            [EFL] Add eina_safety to popup menu deletion.
            https://bugs.webkit.org/show_bug.cgi?id=41873
    
            Since this is a public call, it's better to have an eina_safety on
            public calls. It would crash if browser calls this function to select
            an item of a nonexistent popup.
    
            * efl/ewk/ewk_view.cpp: add eina_safety to return if popup menu client does
            not exist.
    
    Canonical link: https://commits.webkit.org/54139@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@63290 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/efl/ewk/ewk_view.cpp
+++ WebKit/efl/ewk/ewk_view.cpp
@@ -3737,6 +3737,7 @@ void ewk_view_popup_selected_set(Evas_Object* o, int index)
     INF("o=%p", o);
     EWK_VIEW_SD_GET_OR_RETURN(o, sd);
     EWK_VIEW_PRIV_GET_OR_RETURN(sd, priv);
+    EINA_SAFETY_ON_NULL_RETURN(priv->popup.menu_client);
 
     priv->popup.menu_client->valueChanged(index);
 }
