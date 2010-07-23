bb4b65649b1cdea603749cbc0966cc09770a326d
Jon Honeycutt <jhoneycutt@apple.com>
2010-07-23 03:20:31 +0000

    Windows build fix.
    Commit some changes missed by the commit-queue in r63944.
    
    * platform/win/BitmapInfo.h:
    (WebCore::BitmapInfo::is16bit):
    (WebCore::BitmapInfo::is32bit):
    (WebCore::BitmapInfo::width):
    (WebCore::BitmapInfo::height):
    (WebCore::BitmapInfo::size):
    (WebCore::BitmapInfo::paddedWidth):
    (WebCore::BitmapInfo::numPixels):
    (WebCore::BitmapInfo::paddedBytesPerLine):
    (WebCore::BitmapInfo::bytesPerLine):
    
    Canonical link: https://commits.webkit.org/54778@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@63945 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/win/BitmapInfo.h
+++ WebCore/platform/win/BitmapInfo.h
@@ -1,6 +1,8 @@
 /*
  * Copyright (C) 2009 Apple Inc. All Rights Reserved.
  * Copyright (C) 2009 Brent Fulgham
+ * Copyright (C) 2007-2009 Torch Mobile, Inc. All Rights Reserved.
+ * Copyright (C) 2010 Patrick Gansterer <paroga@paroga.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,17 +29,25 @@
 #ifndef BitmapInfo_h
 #define BitmapInfo_h
 
-#include <windows.h>
 #include "IntSize.h"
+#include <windows.h>
 
 namespace WebCore {
 
 struct BitmapInfo : public BITMAPINFO {
-    BitmapInfo ();
-    static BitmapInfo create(const IntSize&);
-    static BitmapInfo createBottomUp(const IntSize&);
-};
+    BitmapInfo();
+    static BitmapInfo create(const IntSize&, WORD bitCount = 32);
+    static BitmapInfo createBottomUp(const IntSize&, WORD bitCount = 32);
 
+    bool is16bit() const { return bmiHeader.biBitCount == 16; }
+    bool is32bit() const { return bmiHeader.biBitCount == 32; }
+    unsigned width() const { return abs(bmiHeader.biWidth); }
+    unsigned height() const { return abs(bmiHeader.biHeight); }
+    IntSize size() const { return IntSize(width(), height()); }
+    unsigned paddedWidth() const { return is16bit() ? (width() + 1) & ~0x1 : width(); }
+    unsigned numPixels() const { return paddedWidth() * height(); }
+    unsigned paddedBytesPerLine() const { return is16bit() ? paddedWidth() * 2 : width() * 4; }
+    unsigned bytesPerLine() const { return width() * bmiHeader.biBitCount / 8; }};
 } // namespace WebCore
 
 #endif // BitmapInfo_h
