3d463fc566c4fd9e84898d9cc85885b0e0137a88
Gavin Barraclough <barraclough@apple.com>
2010-08-04 18:14:00 +0000

    Enable JSVALUE64 for CPU(PPC64). Basic browsing seems to work.
    Rubber stamped by Sam Weinig.
    
    * wtf/Platform.h:
    
    
    
    Canonical link: https://commits.webkit.org/55480@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@64658 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/Platform.h
+++ JavaScriptCore/wtf/Platform.h
@@ -910,9 +910,10 @@
 #if (CPU(X86_64) && (OS(UNIX) || OS(WINDOWS))) \
     || (CPU(IA64) && !CPU(IA64_32)) \
     || CPU(ALPHA) \
-    || CPU(SPARC64)
+    || CPU(SPARC64) \
+    || CPU(PPC64)
 #define WTF_USE_JSVALUE64 1
-#elif CPU(PPC64) || CPU(MIPS) || (CPU(ARM_TRADITIONAL) && !COMPILER(GCC))
+#elif CPU(MIPS) || (CPU(ARM_TRADITIONAL) && !COMPILER(GCC))
 #define WTF_USE_JSVALUE32 1
 #elif OS(WINDOWS) && COMPILER(MINGW)
 /* Using JSVALUE32_64 causes padding/alignement issues for JITStubArg
