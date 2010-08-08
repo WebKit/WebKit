0969c6b6fcd1c1d4156f8ea14ab99e66b32fdeff
Eric Seidel <eric@webkit.org>
2010-08-08 02:38:22 +0000

    2010-08-07  Gyuyoung Kim  <gyuyoung.kim@samsung.com>
            Reviewed by Eric Seidel.
    
            [WML] Change DocumentParser with ScriptableDocumentParser in WMLDocument
            https://bugs.webkit.org/show_bug.cgi?id=43174
    
            * wml/WMLDocument.cpp:
            (WebCore::WMLDocument::finishedParsing): Change DocumentParser with ScriptableDocumentParser.
    
    Canonical link: https://commits.webkit.org/55743@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@64930 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/wml/WMLDocument.cpp
+++ WebCore/wml/WMLDocument.cpp
@@ -26,7 +26,7 @@
 #include "BackForwardList.h"
 #include "Frame.h"
 #include "Page.h"
-#include "DocumentParser.h"
+#include "ScriptableDocumentParser.h"
 #include "WMLCardElement.h"
 #include "WMLErrorHandling.h"
 #include "WMLPageState.h"
@@ -47,7 +47,7 @@ WMLDocument::~WMLDocument()
 
 void WMLDocument::finishedParsing()
 {
-    if (DocumentParser* parser = this->parser()) {
+    if (ScriptableDocumentParser* parser = this->scriptableDocumentParser()) {
         if (!parser->wellFormed()) {
             Document::finishedParsing();
             return;
