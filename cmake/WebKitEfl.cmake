a2cf1bf8744393d35e4399dc1400a64ed1a5f09f
Commit Queue <commit-queue@webkit.org>
2010-08-12 17:16:22 +0000

    2010-08-12  Rafael Antognolli  <antognolli@profusion.mobi>
            Reviewed by Kenneth Rohde Christiansen.
    
            [EFL] Making CMake keep track of theme dependencies.
            https://bugs.webkit.org/show_bug.cgi?id=43862
    
            Removing unused macro GENERATE_EDJ.
    
            * cmake/WebKitEfl.cmake:
    2010-08-12  Rafael Antognolli  <antognolli@profusion.mobi>
    
            Reviewed by Kenneth Rohde Christiansen.
    
            [EFL] Making CMake keep track of theme dependencies.
            https://bugs.webkit.org/show_bug.cgi?id=43862
    
            Adding theme source files and images as dependencies of the theme.
    
            * CMakeListsEfl.txt:
    
    Canonical link: https://commits.webkit.org/56053@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@65253 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- cmake/WebKitEfl.cmake
+++ cmake/WebKitEfl.cmake
@@ -4,14 +4,3 @@ FIND_PROGRAM(EDJE_CC_EXECUTABLE edje_cc)
 IF (NOT EDJE_CC_EXECUTABLE)
   MESSAGE(FATAL_ERROR "Missing edje_cc")
 ENDIF ()
-
-# - Generate Edje compiled from the given source
-# GENERATE_EDJ(source binary edje_cc_options)
-#
-# This runs edje_cc -v ${edje_cc_options} ${source} ${binary}
-MACRO(GENERATE_EDJ _edc _edj _edje_cc_opts)
-  ADD_CUSTOM_COMMAND(
-    OUTPUT  ${_edj}
-    COMMAND ${EDJE_CC_EXECUTABLE} -v ${_edje_cc_opts} ${_edc} ${_edj}
-    VERBATIM)
-ENDMACRO()
