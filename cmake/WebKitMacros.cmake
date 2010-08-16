8e5146201b41f8c05910e7b7305670e61ed26027
Commit Queue <commit-queue@webkit.org>
2010-08-16 03:28:58 +0000

    2010-08-15  Ryuan Choi  <ryuan.choi@samsung.com>
            Reviewed by Antonio Gomes.
    
            [EFL] Build error on r65378
            https://bugs.webkit.org/show_bug.cgi?id=44019
    
            Add GENERATE_JSON macro for HTMLEntityNames.json instead of HTMLEntityNames.gperf
    
            * cmake/WebKitMacros.cmake:
    2010-08-15  Ryuan Choi  <ryuan.choi@samsung.com>
    
            Reviewed by Antonio Gomes.
    
            [EFL] Build error on r65378
            https://bugs.webkit.org/show_bug.cgi?id=44019
    
            Change build script for HTMLEntityNames.json instead of HTMLEntityNames.gperf
    
            * CMakeLists.txt:
    
    Canonical link: https://commits.webkit.org/56191@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@65391 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- cmake/WebKitMacros.cmake
+++ cmake/WebKitMacros.cmake
@@ -110,3 +110,12 @@ MACRO (WEBKIT_WRAP_SOURCELIST _input)
         ENDFOREACH ()
     ENDIF ()
 ENDMACRO ()
+
+
+MACRO (GENERATE_JSON _json _output _script)
+    ADD_CUSTOM_COMMAND(
+        OUTPUT ${_output}
+        MAIN_DEPENDENCY ${_input} ${_script}
+        COMMAND ${PYTHON_EXECUTABLE} ${_script} -o ${_output} ${_json}
+        VERBATIM)
+ENDMACRO ()
