d4b70ad3230cf745baba204e6eeb2dce64fac23f
Steve Block <steveblock@google.com>
2010-08-17 17:13:22 +0000

    2010-08-17  Steve Block  <steveblock@google.com>
            Reviewed by Jeremy Orlow.
    
            [Qt] fast/dom/Geolocation/disconnected-frame-permission-denied.html fails
            https://bugs.webkit.org/show_bug.cgi?id=44059
    
            * fast/dom/Geolocation/disconnected-frame-permission-denied-expected.txt:
            * fast/dom/Geolocation/script-tests/disconnected-frame-permission-denied.js:
            (onIframeLoaded):
            ():
            * platform/chromium/test_expectations.txt:
            * platform/qt/Skipped:
    
    Canonical link: https://commits.webkit.org/56313@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@65513 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/dom/Geolocation/script-tests/disconnected-frame-permission-denied.js
+++ LayoutTests/fast/dom/Geolocation/script-tests/disconnected-frame-permission-denied.js
@@ -1,15 +1,30 @@
 description("Tests that when a request is made on a Geolocation object, permission is denied and its Frame is disconnected before a callback is made, the error callback is invoked with PERMISSION_DENIED.");
 
-if (window.layoutTestController)
+// Prime the Geolocation instance by denying permission. This makes sure that we execute the
+// same code path for both preemptive and non-preemtive permissions policies.
+if (window.layoutTestController) {
     layoutTestController.setGeolocationPermission(false);
-
-function onIframeLoaded() {
-    iframeGeolocation = iframe.contentWindow.navigator.geolocation;
-    iframe.src = 'data:text/html,This frame should be visible when the test completes';
-}
+    layoutTestController.setMockGeolocationPosition(51.478, -0.166, 100);
+} else
+    debug('This test can not be run without the LayoutTestController');
 
 var error;
+function onIframeLoaded() {
+    iframeGeolocation = iframe.contentWindow.navigator.geolocation;
+    iframeGeolocation.getCurrentPosition(function() {
+        testFailed('Success callback invoked unexpectedly');
+        finishJSTest();
+    }, function(e) {
+        error = e;
+        shouldBe('error.code', 'error.PERMISSION_DENIED');
+        shouldBe('error.message', '"User denied Geolocation"');
+        debug('');
+        iframe.src = 'data:text/html,This frame should be visible when the test completes';
+    });
+}
+
 function onIframeUnloaded() {
+    // Make another request, with permission already denied.
     iframeGeolocation.getCurrentPosition(function () {
         testFailed('Success callback invoked unexpectedly');
         finishJSTest();
@@ -17,6 +32,7 @@ function onIframeUnloaded() {
         error = e;
         shouldBe('error.code', 'error.PERMISSION_DENIED');
         shouldBe('error.message', '"User denied Geolocation"');
+        debug('');
         finishJSTest();
     });
 }
