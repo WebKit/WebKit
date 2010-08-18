7b5bcec278424b50e2fbf0d0b2eccb99e09d9ecd
Ilya Tikhonovsky <loislo@chromium.org>
2010-08-18 10:00:06 +0000

    2010-08-16  Ilya Tikhonovsky  <loislo@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: replace hand written InspectorBackendStub.js by generated one.
            New command-line flag was added to combine-javascript-resources.
            It specifies the location of generated scripts.
            https://bugs.webkit.org/show_bug.cgi?id=43791
    
            * GNUmakefile.am:
            * WebCore.gyp/WebCore.gyp:
            * WebCore.gypi:
            * WebCore.pri:
            * WebCore.pro:
            * WebCore.vcproj/WebCore.vcproj:
            * WebCore.xcodeproj/project.pbxproj:
            * combine-javascript-resources:
            * inspector/CodeGeneratorInspector.pm:
            * inspector/front-end/InspectorBackendStub.js: Removed.
            * inspector/front-end/WebKit.qrc:
    
    2010-08-16  Ilya Tikhonovsky  <loislo@chromium.org>
    
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: replace hand written InspectorBackendStub.js by generated one.
            https://bugs.webkit.org/show_bug.cgi?id=43791
    
            * GNUmakefile.am:
    
    2010-08-17  Ilya Tikhonovsky  <loislo@chromium.org>
    
            Reviewed by Yury Semikhatsky.
    
            WebInspector: inspector tests for GTK were added to Skip list.
            They should be enabled later.
            Bug for GTK https://bugs.webkit.org/show_bug.cgi?id=43977
            https://bugs.webkit.org/show_bug.cgi?id=43791
    
            * platform/gtk/Skipped:
    
    2010-08-17  Ilya Tikhonovsky  <loislo@chromium.org>
    
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: replace hand written InspectorBackendStub.js by generated one.
            https://bugs.webkit.org/show_bug.cgi?id=43791
    
            * WebKit.gyp:
    
    2010-08-17  Ilya Tikhonovsky  <loislo@chromium.org>
    
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: replace hand written InspectorBackendStub.js by generated one.
            https://bugs.webkit.org/show_bug.cgi?id=43791
    
            * WebCoreSupport/InspectorClientGtk.cpp:
            (WebKit::InspectorClient::openInspectorFrontend):
    
    
    
    
    Canonical link: https://commits.webkit.org/56378@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@65595 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/inspector/CodeGeneratorInspector.pm
+++ WebCore/inspector/CodeGeneratorInspector.pm
@@ -138,6 +138,7 @@ my $verbose;
 my $namespace;
 
 my $backendClassName;
+my $backendJSStubName;
 my %backendTypes;
 my %backendMethods;
 my @backendMethodsImpl;
@@ -145,6 +146,7 @@ my $backendConstructor;
 my @backendConstantDeclarations;
 my @backendConstantDefinitions;
 my $backendFooter;
+my @backendStubJS;
 
 my $frontendClassName;
 my %frontendTypes;
@@ -204,6 +206,7 @@ sub GenerateInterface
     $frontendTypes{"PassRefPtr"} = 1;
 
     $backendClassName = $className . "BackendDispatcher";
+    $backendJSStubName = $className . "BackendStub";
     my @backendHead;
     push(@backendHead, "    ${backendClassName}(InspectorController* inspectorController) : m_inspectorController(inspectorController) { }");
     push(@backendHead, "    void reportProtocolError(const long callId, const String& method, const String& errorText) const;");
@@ -238,6 +241,8 @@ sub generateFunctions
     }
     push(@backendMethodsImpl, generateBackendDispatcher());
     push(@backendMethodsImpl, generateBackendReportProtocolError());
+
+    @backendStubJS = generateBackendStubJS($interface);
 }
 
 sub generateFrontendFunction
@@ -462,6 +467,40 @@ EOF
     return split("\n", $messageParserBody);
 }
 
+sub generateBackendStubJS
+{
+    my $interface = shift;
+    my @backendFunctions = grep(!$_->signature->extendedAttributes->{"notify"}, @{$interface->functions});
+    my @JSStubs = map("    this._registerDelegate(\"" . $_->signature->name . "\");", @backendFunctions);
+
+    my $JSStubs = join("\n", @JSStubs);
+    my $inspectorBackendStubJS = << "EOF";
+$licenseTemplate
+
+WebInspector.InspectorBackendStub = function()
+{
+$JSStubs
+}
+
+WebInspector.InspectorBackendStub.prototype = {
+    _registerDelegate: function(methodName)
+    {
+        this[methodName] = this.sendMessageToBackend.bind(this, methodName);
+    },
+
+    sendMessageToBackend: function()
+    {
+        var message = JSON.stringify(Array.prototype.slice.call(arguments));
+        InspectorFrontendHost.sendMessageToBackend(message);
+    }
+}
+
+InspectorBackend = new WebInspector.InspectorBackendStub();
+
+EOF
+    return split("\n", $inspectorBackendStubJS);
+}
+
 sub generateHeader
 {
     my $className = shift;
@@ -563,6 +602,11 @@ sub finish
     print $HEADER join("\n", generateHeader($backendClassName, \%backendTypes, $backendConstructor, \@backendConstantDeclarations, \%backendMethods, $backendFooter));
     close($HEADER);
     undef($HEADER);
+
+    open(my $JS_STUB, ">$outputDir/$backendJSStubName.js") || die "Couldn't open file $outputDir/$backendJSStubName.js";
+    print $JS_STUB join("\n", @backendStubJS);
+    close($JS_STUB);
+    undef($JS_STUB);
 }
 
 1;
