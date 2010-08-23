29eb84252214220f04b717050dc1e9fc428bc49d
Commit Queue <commit-queue@webkit.org>
2010-08-23 19:18:01 +0000

    2010-08-23  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r65814.
            http://trac.webkit.org/changeset/65814
            https://bugs.webkit.org/show_bug.cgi?id=44443
    
            broke svg/custom/missing-xlink.svg (Requested by kling on
            #webkit).
    
            * fast/dom/script-tests/setAttributeNS-prefix-and-null-namespace.js: Removed.
            * fast/dom/setAttributeNS-prefix-and-null-namespace-expected.txt: Removed.
            * fast/dom/setAttributeNS-prefix-and-null-namespace.html: Removed.
    2010-08-23  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r65814.
            http://trac.webkit.org/changeset/65814
            https://bugs.webkit.org/show_bug.cgi?id=44443
    
            broke svg/custom/missing-xlink.svg (Requested by kling on
            #webkit).
    
            * dom/Element.cpp:
            (WebCore::Element::setAttributeNS):
    
    Canonical link: https://commits.webkit.org/56602@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@65823 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/dom/Element.cpp
+++ WebCore/dom/Element.cpp
@@ -1213,11 +1213,6 @@ void Element::setAttributeNS(const AtomicString& namespaceURI, const AtomicStrin
     if (!Document::parseQualifiedName(qualifiedName, prefix, localName, ec))
         return;
 
-    if (namespaceURI.isNull() && !prefix.isNull()) {
-        ec = NAMESPACE_ERR;
-        return;
-    }
-
     QualifiedName qName(prefix, localName, namespaceURI);
 
     if (scriptingPermission == FragmentScriptingNotAllowed && (isEventHandlerAttribute(qName) || isAttributeToRemove(qName, value)))
