8a43ea91a58c533de4264b0bd9b7ba1db28d4c4c
Lucas De Marchi <demarchi@webkit.org>
2010-08-25 16:39:36 +0000

    2010-08-25  Rafael Antognolli  <antognolli@profusion.mobi>
            Unreviewed build fix.
    
            [EFL] Build fix for revision 65332
            https://bugs.webkit.org/show_bug.cgi?id=44543
    
            Generated variables are now returned from FindPkgConfig, so no need
            to document the custom ones.
    
            * cmake/FindGthread.cmake:
    2010-08-25  Rafael Antognolli  <antognolli@profusion.mobi>
    
            Unreviewed build fix.
    
            [EFL] Build fix for revision 65332
            https://bugs.webkit.org/show_bug.cgi?id=44543
    
            Use Gthread_LIBRARIES instead of GTHREAD_LIBRARIES. This fix
            the build when using ENABLE_GLIB_SUPPORT=ON and NETWORK_BACKEND=curl.
    
            * CMakeListsEfl.txt:
    
    Canonical link: https://commits.webkit.org/56797@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@66018 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- cmake/FindGthread.cmake
+++ cmake/FindGthread.cmake
@@ -1,8 +1,4 @@
-# Find include and libraries for GTHREAD library
-# GTHREAD_INCLUDE     Directories to include to use GTHREAD
-# GTHREAD_INCLUDE-I   Directories to include to use GTHREAD (with -I)
-# GTHREAD_LIBRARIES   Libraries to link against to use GTHREAD
-# GTHREAD_FOUND       GTHREAD was found
+# Find include and libraries for Gthread library
 
 INCLUDE(FindPkgConfig)
 PKG_CHECK_MODULES (Gthread REQUIRED gthread-2.0>=2.20.0)
