a839dee093c9bb68db1776e8aec6394a21eeb70e
Adam Roben <aroben@apple.com>
2010-08-27 17:14:34 +0000

    Make the web process pause on launch when the WEBKIT2_PAUSE_WEB_PROCESS_ON_LAUNCH environment variable is set
    This is a bit easier to use than holding Ctrl-Alt-Shift during launch,
    since the process can sometimes take a long time to launch under the
    debugger. Ctrl-Alt-Shift still works, however.
    
    Fixes <http://webkit.org/b/44774> Would like an easier way to pause
    the web process on launch
    
    Reviewed by Sam Weinig.
    
    * WebProcess/WebKitMain.cpp:
    (WebKitMain):
    
    Canonical link: https://commits.webkit.org/57001@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@66229 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit2/WebProcess/WebKitMain.cpp
+++ WebKit2/WebProcess/WebKitMain.cpp
@@ -86,7 +86,7 @@ int WebKitMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpstrCmdLine
     // Show an alert when Ctrl-Alt-Shift is held down during launch to give the user time to attach a
     // debugger. This is useful for debugging problems that happen early in the web process's lifetime.
     const unsigned short highBitMaskShort = 0x8000;
-    if ((::GetKeyState(VK_CONTROL) & highBitMaskShort) && (::GetKeyState(VK_MENU) & highBitMaskShort) && (::GetKeyState(VK_SHIFT) & highBitMaskShort))
+    if (getenv("WEBKIT2_PAUSE_WEB_PROCESS_ON_LAUNCH") || (::GetKeyState(VK_CONTROL) & highBitMaskShort) && (::GetKeyState(VK_MENU) & highBitMaskShort) && (::GetKeyState(VK_SHIFT) & highBitMaskShort))
         ::MessageBoxW(0, L"You can now attach a debugger to " PROCESS_NAME L". You can use\nthe same debugger for WebKit2WebProcessand the UI process, if desired.\nClick OK when you are ready for WebKit2WebProcess to continue.", L"WebKit2WebProcess has launched", MB_OK | MB_ICONINFORMATION);
 #endif
 
