b3fbf77800547937f5591b87ff7d94b75fef042b
Commit Queue <commit-queue@webkit.org>
2010-08-31 17:37:41 +0000

    2010-08-31  Patrick Gansterer  <paroga@paroga.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            Add ContextMenuClientWinCE
            https://bugs.webkit.org/show_bug.cgi?id=44820
    
            * WebCoreSupport/ContextMenuClientWinCE.cpp: Added.
            (WebKit::ContextMenuClient::ContextMenuClient):
            (WebKit::ContextMenuClient::contextMenuDestroyed):
            (WebKit::ContextMenuClient::getCustomMenuFromDefaultItems):
            (WebKit::ContextMenuClient::contextMenuItemSelected):
            (WebKit::ContextMenuClient::downloadURL):
            (WebKit::ContextMenuClient::copyImageToClipboard):
            (WebKit::ContextMenuClient::searchWithGoogle):
            (WebKit::ContextMenuClient::lookUpInDictionary):
            (WebKit::ContextMenuClient::speak):
            (WebKit::ContextMenuClient::stopSpeaking):
            (WebKit::ContextMenuClient::isSpeaking):
            * WebCoreSupport/ContextMenuClientWinCE.h: Added.
    
    Canonical link: https://commits.webkit.org/57263@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@66503 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebKit/wince/WebCoreSupport/ContextMenuClientWinCE.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2010 Patrick Gansterer <paroga@paroga.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "ContextMenuClientWinCE.h"
+
+#include "ContextMenu.h"
+#include "NotImplemented.h"
+
+using namespace WebCore;
+
+namespace WebKit {
+
+ContextMenuClient::ContextMenuClient(WebView *webView)
+    : m_webView(webView)
+{
+}
+
+void ContextMenuClient::contextMenuDestroyed()
+{
+    delete this;
+}
+
+PlatformMenuDescription ContextMenuClient::getCustomMenuFromDefaultItems(ContextMenu* menu)
+{
+    return menu->releasePlatformDescription();
+}
+
+void ContextMenuClient::contextMenuItemSelected(ContextMenuItem*, const ContextMenu*)
+{
+    notImplemented();
+}
+
+void ContextMenuClient::downloadURL(const KURL& url)
+{
+    notImplemented();
+}
+
+void ContextMenuClient::copyImageToClipboard(const HitTestResult&)
+{
+    notImplemented();
+}
+
+void ContextMenuClient::searchWithGoogle(const Frame*)
+{
+    notImplemented();
+}
+
+void ContextMenuClient::lookUpInDictionary(Frame*)
+{
+    notImplemented();
+}
+
+void ContextMenuClient::speak(const String&)
+{
+    notImplemented();
+}
+
+void ContextMenuClient::stopSpeaking()
+{
+    notImplemented();
+}
+
+bool ContextMenuClient::isSpeaking()
+{
+    notImplemented();
+    return false;
+}
+
+} // namespace WebKit
