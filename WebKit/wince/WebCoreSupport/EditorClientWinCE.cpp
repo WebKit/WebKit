502b1e142c563eb28598d803702bb45c7d24b6a6
Commit Queue <commit-queue@webkit.org>
2010-08-31 18:39:45 +0000

    2010-08-31  Patrick Gansterer  <paroga@paroga.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            Add EditorClientWinCE
            https://bugs.webkit.org/show_bug.cgi?id=44822
    
            * WebCoreSupport/EditorClientWinCE.cpp: Added.
            (WebKit::EditorClient::EditorClient):
            (WebKit::EditorClient::~EditorClient):
            (WebKit::EditorClient::setInputMethodState):
            (WebKit::EditorClient::shouldDeleteRange):
            (WebKit::EditorClient::shouldShowDeleteInterface):
            (WebKit::EditorClient::isContinuousSpellCheckingEnabled):
            (WebKit::EditorClient::isGrammarCheckingEnabled):
            (WebKit::EditorClient::spellCheckerDocumentTag):
            (WebKit::EditorClient::shouldBeginEditing):
            (WebKit::EditorClient::shouldEndEditing):
            (WebKit::EditorClient::shouldInsertText):
            (WebKit::EditorClient::shouldChangeSelectedRange):
            (WebKit::EditorClient::shouldApplyStyle):
            (WebKit::EditorClient::shouldMoveRangeAfterDelete):
            (WebKit::EditorClient::didBeginEditing):
            (WebKit::EditorClient::respondToChangedContents):
            (WebKit::EditorClient::respondToChangedSelection):
            (WebKit::EditorClient::didEndEditing):
            (WebKit::EditorClient::didWriteSelectionToPasteboard):
            (WebKit::EditorClient::didSetSelectionTypesForPasteboard):
            (WebKit::EditorClient::isEditable):
            (WebKit::EditorClient::registerCommandForUndo):
            (WebKit::EditorClient::registerCommandForRedo):
            (WebKit::EditorClient::clearUndoRedoOperations):
            (WebKit::EditorClient::canUndo):
            (WebKit::EditorClient::canRedo):
            (WebKit::EditorClient::undo):
            (WebKit::EditorClient::redo):
            (WebKit::EditorClient::shouldInsertNode):
            (WebKit::EditorClient::pageDestroyed):
            (WebKit::EditorClient::smartInsertDeleteEnabled):
            (WebKit::EditorClient::isSelectTrailingWhitespaceEnabled):
            (WebKit::EditorClient::toggleContinuousSpellChecking):
            (WebKit::EditorClient::toggleGrammarChecking):
            (WebKit::EditorClient::handleKeyboardEvent):
            (WebKit::EditorClient::handleInputMethodKeydown):
            (WebKit::EditorClient::textFieldDidBeginEditing):
            (WebKit::EditorClient::textFieldDidEndEditing):
            (WebKit::EditorClient::textDidChangeInTextField):
            (WebKit::EditorClient::doTextFieldCommandFromEvent):
            (WebKit::EditorClient::textWillBeDeletedInTextField):
            (WebKit::EditorClient::textDidChangeInTextArea):
            (WebKit::EditorClient::ignoreWordInSpellDocument):
            (WebKit::EditorClient::learnWord):
            (WebKit::EditorClient::checkSpellingOfString):
            (WebKit::EditorClient::getAutoCorrectSuggestionForMisspelledWord):
            (WebKit::EditorClient::checkGrammarOfString):
            (WebKit::EditorClient::updateSpellingUIWithGrammarString):
            (WebKit::EditorClient::updateSpellingUIWithMisspelledWord):
            (WebKit::EditorClient::showSpellingUI):
            (WebKit::EditorClient::spellingUIIsShowing):
            (WebKit::EditorClient::getGuessesForWord):
            (WebKit::EditorClient::willSetInputMethodState):
            * WebCoreSupport/EditorClientWinCE.h: Added.
    
    Canonical link: https://commits.webkit.org/57272@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@66512 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ WebKit/wince/WebCoreSupport/EditorClientWinCE.cpp
@@ -0,0 +1,315 @@
+/*
+ * Copyright (C) 2010 Patrick Gansterer <paroga@paroga.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "EditorClientWinCE.h"
+
+#include "EditCommand.h"
+#include "NotImplemented.h"
+
+using namespace WebCore;
+
+namespace WebKit {
+
+EditorClient::EditorClient(WebView* webView)
+    : m_webView(webView)
+{
+}
+
+EditorClient::~EditorClient()
+{
+}
+
+void EditorClient::setInputMethodState(bool active)
+{
+    notImplemented();
+}
+
+bool EditorClient::shouldDeleteRange(Range*)
+{
+    notImplemented();
+    return true;
+}
+
+bool EditorClient::shouldShowDeleteInterface(HTMLElement*)
+{
+    return false;
+}
+
+bool EditorClient::isContinuousSpellCheckingEnabled()
+{
+    notImplemented();
+    return false;
+}
+
+bool EditorClient::isGrammarCheckingEnabled()
+{
+    notImplemented();
+    return false;
+}
+
+int EditorClient::spellCheckerDocumentTag()
+{
+    notImplemented();
+    return 0;
+}
+
+bool EditorClient::shouldBeginEditing(WebCore::Range*)
+{
+    notImplemented();
+    return true;
+}
+
+bool EditorClient::shouldEndEditing(WebCore::Range*)
+{
+    notImplemented();
+    return true;
+}
+
+bool EditorClient::shouldInsertText(const String&, Range*, EditorInsertAction)
+{
+    notImplemented();
+    return true;
+}
+
+bool EditorClient::shouldChangeSelectedRange(Range*, Range*, EAffinity, bool)
+{
+    notImplemented();
+    return true;
+}
+
+bool EditorClient::shouldApplyStyle(WebCore::CSSStyleDeclaration*, WebCore::Range*)
+{
+    notImplemented();
+    return true;
+}
+
+bool EditorClient::shouldMoveRangeAfterDelete(WebCore::Range*, WebCore::Range*)
+{
+    notImplemented();
+    return true;
+}
+
+void EditorClient::didBeginEditing()
+{
+    notImplemented();
+}
+
+void EditorClient::respondToChangedContents()
+{
+    notImplemented();
+}
+
+void EditorClient::respondToChangedSelection()
+{
+    notImplemented();
+}
+
+void EditorClient::didEndEditing()
+{
+    notImplemented();
+}
+
+void EditorClient::didWriteSelectionToPasteboard()
+{
+    notImplemented();
+}
+
+void EditorClient::didSetSelectionTypesForPasteboard()
+{
+    notImplemented();
+}
+
+bool EditorClient::isEditable()
+{
+    notImplemented();
+    return false;
+}
+
+void EditorClient::registerCommandForUndo(WTF::PassRefPtr<WebCore::EditCommand> command)
+{
+    notImplemented();
+}
+
+void EditorClient::registerCommandForRedo(WTF::PassRefPtr<WebCore::EditCommand> command)
+{
+    notImplemented();
+}
+
+void EditorClient::clearUndoRedoOperations()
+{
+    notImplemented();
+}
+
+bool EditorClient::canUndo() const
+{
+    notImplemented();
+    return false;
+}
+
+bool EditorClient::canRedo() const
+{
+    notImplemented();
+    return false;
+}
+
+void EditorClient::undo()
+{
+    notImplemented();
+}
+
+void EditorClient::redo()
+{
+    notImplemented();
+}
+
+bool EditorClient::shouldInsertNode(Node*, Range*, EditorInsertAction)
+{
+    notImplemented();
+    return true;
+}
+
+void EditorClient::pageDestroyed()
+{
+    delete this;
+}
+
+bool EditorClient::smartInsertDeleteEnabled()
+{
+    notImplemented();
+    return false;
+}
+
+bool EditorClient::isSelectTrailingWhitespaceEnabled()
+{
+    notImplemented();
+    return false;
+}
+
+void EditorClient::toggleContinuousSpellChecking()
+{
+    notImplemented();
+}
+
+void EditorClient::toggleGrammarChecking()
+{
+    notImplemented();
+}
+
+void EditorClient::handleKeyboardEvent(KeyboardEvent* event)
+{
+    notImplemented();
+}
+
+void EditorClient::handleInputMethodKeydown(KeyboardEvent* event)
+{
+    notImplemented();
+}
+
+void EditorClient::textFieldDidBeginEditing(Element*)
+{
+}
+
+void EditorClient::textFieldDidEndEditing(Element*)
+{
+}
+
+void EditorClient::textDidChangeInTextField(Element*)
+{
+}
+
+bool EditorClient::doTextFieldCommandFromEvent(Element*, KeyboardEvent*)
+{
+    return false;
+}
+
+void EditorClient::textWillBeDeletedInTextField(Element*)
+{
+    notImplemented();
+}
+
+void EditorClient::textDidChangeInTextArea(Element*)
+{
+    notImplemented();
+}
+
+void EditorClient::ignoreWordInSpellDocument(const String& text)
+{
+    notImplemented();
+}
+
+void EditorClient::learnWord(const String& text)
+{
+    notImplemented();
+}
+
+void EditorClient::checkSpellingOfString(const UChar* text, int length, int* misspellingLocation, int* misspellingLength)
+{
+    notImplemented();
+}
+
+String EditorClient::getAutoCorrectSuggestionForMisspelledWord(const String& inputWord)
+{
+    // This method can be implemented using customized algorithms for the particular browser.
+    // Currently, it computes an empty string.
+    return String();
+}
+
+void EditorClient::checkGrammarOfString(const UChar*, int, Vector<GrammarDetail>&, int*, int*)
+{
+    notImplemented();
+}
+
+void EditorClient::updateSpellingUIWithGrammarString(const String&, const GrammarDetail&)
+{
+    notImplemented();
+}
+
+void EditorClient::updateSpellingUIWithMisspelledWord(const String&)
+{
+    notImplemented();
+}
+
+void EditorClient::showSpellingUI(bool)
+{
+    notImplemented();
+}
+
+bool EditorClient::spellingUIIsShowing()
+{
+    notImplemented();
+    return false;
+}
+
+void EditorClient::getGuessesForWord(const String& word, WTF::Vector<String>& guesses)
+{
+    notImplemented();
+}
+
+void EditorClient::willSetInputMethodState()
+{
+    notImplemented();
+}
+
+} // namespace WebKit
