c9bc64333e9f5ccb360522e1a1d7907088fc42fe
Jian Li <jianli@chromium.org>
2010-09-02 23:14:50 +0000

    Improve tests for blob URL. https://bugs.webkit.org/show_bug.cgi?id=45132
    Reviewed by Darin Fisher.
    
    Change apply-blob-url-to-xhr.html to produce text based result. Also
    add the test case for revokeBlobURL in apply-blob-url-to-xhr.html
    and work-apply-blob-url-to-xhr.html.
    
    * fast/files/apply-blob-url-to-img-expected.txt: Added.
    * fast/files/apply-blob-url-to-img.html:
    * fast/files/apply-blob-url-to-xhr-expected.txt:
    * fast/files/apply-blob-url-to-xhr.html:
    * fast/files/workers/resources/worker-apply-blob-url-to-xhr.js:
    (sendXMLHttpRequest):
    (onmessage):
    * fast/files/workers/worker-apply-blob-url-to-xhr-expected.txt:
    * platform/chromium/test_expectations.txt:
    * platform/mac/Skipped:
    * platform/mac/fast/files/apply-blob-url-to-img-expected.checksum: Removed.
    * platform/mac/fast/files/apply-blob-url-to-img-expected.png: Removed.
    * platform/mac/fast/files/apply-blob-url-to-img-expected.txt: Removed.
    
    Canonical link: https://commits.webkit.org/57449@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@66690 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/files/workers/resources/worker-apply-blob-url-to-xhr.js
+++ LayoutTests/fast/files/workers/resources/worker-apply-blob-url-to-xhr.js
@@ -3,26 +3,34 @@ function log(message)
     postMessage(message);
 }
 
+function sendXMLHttpRequest(method, url)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url, false);
+    try {
+        xhr.send();
+        log("Status: " + xhr.status);
+        log("Response: " + xhr.responseText);
+    } catch (error) {
+        log("Received exception " + error.code + ": " + error.name);
+    }
+}
+
 onmessage = function(event)
 {
     var file = event.data;
     var fileURL = createBlobURL(file);
 
     log("Test that XMLHttpRequest GET succeeds.");
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", fileURL, false);
-    xhr.send();
-    log(xhr.responseText);
+    sendXMLHttpRequest("GET", fileURL);
 
     log("Test that XMLHttpRequest POST fails.");
     xhr = new XMLHttpRequest();
-    xhr.open("POST", fileURL, false);
-    try {
-        xhr.send();
-    } catch (error) {
-        log("Received exception " + error.code + ": " + error.name);
-    }
-    log(xhr.responseText);
+    sendXMLHttpRequest("POST", fileURL);
+
+    log("Test that XMLHttpRequest GET fails after the blob URL is revoked.");
+    revokeBlobURL(fileURL);
+    sendXMLHttpRequest("GET", fileURL);
 
     log("DONE");
 }
