ba9b6e7ea748a5205cdd981c28fd076b4bf8f4b7
Andrey Kosyakov <caseq@chromium.org>
2010-09-02 16:30:09 +0000

    2010-09-02  Andrey Kosyakov  <caseq@chromium.org>
            Unreviewed test fix.
            Add size & bodySize to list of non-deterministic fields for HAR tests, as those tend to vary by platform
            in error responses.
            Remove platform-specific expectations that were previously added because of this.
    
            * http/tests/inspector/resource-har-conversion-expected.txt:
            * http/tests/inspector/resource-har-conversion.html:
            * http/tests/inspector/resource-test2.js:
            * platform/chromium-win/inspector/resource-har-conversion-expected.txt: Removed.
            * platform/win/http/tests/inspector/resource-har-conversion-expected.txt: Removed.
    
    
    Canonical link: https://commits.webkit.org/57431@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@66672 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/inspector/resource-test2.js
+++ LayoutTests/http/tests/inspector/resource-test2.js
@@ -8,6 +8,12 @@ InspectorTest.HARNondeterministicProperties = {
     headers: 1,
 };
 
+// addObject checks own properties only, so make a deep copy rather than use prototype.
+
+InspectorTest.HARNondeterministicPropertiesWithSize = JSON.parse(JSON.stringify(InspectorTest.HARNondeterministicProperties));
+InspectorTest.HARNondeterministicPropertiesWithSize.size = 1;
+InspectorTest.HARNondeterministicPropertiesWithSize.bodySize = 1;
+
 InspectorTest.resourceURLComparer = function(r1, r2)
 {
     return r1.request.url.localeCompare(r2.request.url);
