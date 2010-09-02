12998e6ff3f7e8a1b2c3cf23bcd922709cded619
Ilya Tikhonovsky <loislo@chromium.org>
2010-09-02 17:10:38 +0000

    2010-09-02  Ilya Tikhonovsky  <loislo@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            WebInspector: Timeline instrumentation code can crash browser.
    
            Steps:
            1) load the site
            2) open inspector
            3) start timeline recording
            4) reload inspected page
            got NPE
    
            https://bugs.webkit.org/show_bug.cgi?id=45098
    
            * loader/ResourceLoader.cpp:
            (WebCore::ResourceLoader::didReceiveResponse):
            (WebCore::ResourceLoader::didReceiveData):
    
    Canonical link: https://commits.webkit.org/57433@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@66674 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/ResourceLoader.cpp
+++ WebCore/loader/ResourceLoader.cpp
@@ -401,7 +401,7 @@ void ResourceLoader::didReceiveResponse(ResourceHandle*, const ResourceResponse&
 {
 #if ENABLE(INSPECTOR)
     if (InspectorTimelineAgent::instanceCount()) {
-        InspectorTimelineAgent* timelineAgent = m_frame->page() ? m_frame->page()->inspectorTimelineAgent() : 0;
+        InspectorTimelineAgent* timelineAgent = (m_frame && m_frame->page()) ? m_frame->page()->inspectorTimelineAgent() : 0;
         if (timelineAgent)
             timelineAgent->willReceiveResourceResponse(identifier(), response);
     }
@@ -413,7 +413,7 @@ void ResourceLoader::didReceiveResponse(ResourceHandle*, const ResourceResponse&
     didReceiveResponse(response);
 #if ENABLE(INSPECTOR)
     if (InspectorTimelineAgent::instanceCount()) {
-        InspectorTimelineAgent* timelineAgent = m_frame->page() ? m_frame->page()->inspectorTimelineAgent() : 0;
+        InspectorTimelineAgent* timelineAgent = (m_frame && m_frame->page()) ? m_frame->page()->inspectorTimelineAgent() : 0;
         if (timelineAgent)
             timelineAgent->didReceiveResourceResponse();
     }
@@ -424,7 +424,7 @@ void ResourceLoader::didReceiveData(ResourceHandle*, const char* data, int lengt
 {
 #if ENABLE(INSPECTOR)
     if (InspectorTimelineAgent::instanceCount()) {
-        InspectorTimelineAgent* timelineAgent = m_frame->page() ? m_frame->page()->inspectorTimelineAgent() : 0;
+        InspectorTimelineAgent* timelineAgent = (m_frame && m_frame->page()) ? m_frame->page()->inspectorTimelineAgent() : 0;
         if (timelineAgent)
             timelineAgent->willReceiveResourceData(identifier());
     }
@@ -432,7 +432,7 @@ void ResourceLoader::didReceiveData(ResourceHandle*, const char* data, int lengt
     didReceiveData(data, length, lengthReceived, false);
 #if ENABLE(INSPECTOR)
     if (InspectorTimelineAgent::instanceCount()) {
-        InspectorTimelineAgent* timelineAgent = m_frame->page() ? m_frame->page()->inspectorTimelineAgent() : 0;
+        InspectorTimelineAgent* timelineAgent = (m_frame && m_frame->page()) ? m_frame->page()->inspectorTimelineAgent() : 0;
         if (timelineAgent)
             timelineAgent->didReceiveResourceData();
     }
