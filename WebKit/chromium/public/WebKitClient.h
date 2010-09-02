1bd3e659f390cd528a561b651f4dbc4ae11bda8a
Kent Tamura <tkent@chromium.org>
2010-09-02 00:25:40 +0000

    2010-09-01  Kent Tamura  <tkent@chromium.org>
            Unreviewed, rolling out r66576.
            http://trac.webkit.org/changeset/66576
            https://bugs.webkit.org/show_bug.cgi?id=45016
    
            This change is not needed.
    
            * public/WebKitClient.h:
    
    Canonical link: https://commits.webkit.org/57386@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@66627 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/chromium/public/WebKitClient.h
+++ WebKit/chromium/public/WebKitClient.h
@@ -260,7 +260,6 @@ public:
 
     // Callable from a background WebKit thread.
     virtual void callOnMainThread(void (*func)(void*), void* context) { }
-    virtual void callOnMainThreadWithDelay(void (*func)(void*), void* context, long long delayMilliseconds) { }
 
     // WebGL --------------------------------------------------------------
 
