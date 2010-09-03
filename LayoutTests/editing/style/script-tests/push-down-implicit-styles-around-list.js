1b2b697acd8587159a47e6759ddedc9c43c736a0
Ryosuke Niwa <rniwa@webkit.org>
2010-09-03 20:08:28 +0000

    2010-09-03  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Tony Chang.
    
            Fixed a test committed in r66743.
    
            * editing/style/script-tests/push-down-implicit-styles-around-list.js:
            (selectLastWord):
    
    
    Canonical link: https://commits.webkit.org/57523@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@66764 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/editing/style/script-tests/push-down-implicit-styles-around-list.js
+++ LayoutTests/editing/style/script-tests/push-down-implicit-styles-around-list.js
@@ -17,54 +17,36 @@ function testSingleToggle(toggleCommand, selector, initialContents, expectedCont
         testFailed(action + ", expected " + expectedContents);
 }
 
-function selectAll(container) {
-    window.getSelection().selectAllChildren(container);
-    return 'all';
-}
-
-function selectTest(container) {
-    window.getSelection().selectAllChildren(document.getElementById('test'));
-    return 'test';
-}
-
 function selectFirstWord(container) {
     window.getSelection().setPosition(container, 0);
     window.getSelection().modify('extend', 'forward', 'word');
     return 'first word';
 }
 
-function selectSecondWord(container) {
-    window.getSelection().setPosition(container, 0);
-    window.getSelection().modify('move', 'forward', 'word');
-    window.getSelection().modify('extend', 'forward', 'word');
-    return 'second word';
-}
-
-function selectLastTwoWords(container) {
+function selectLastWord(container) {
     window.getSelection().setPosition(container, container.childNodes.length);
     window.getSelection().modify('extend', 'backward', 'word');
-    window.getSelection().modify('extend', 'backward', 'word');
-    return 'last two words';
+    return 'last word';
 }
 
 testSingleToggle("bold", selectFirstWord, '<b><ul><li><b>a</b></li></ul></b>', '<ul><li>a</li></ul>');
 testSingleToggle("bold", selectFirstWord, '<b><ul><li>hello</li><li>world</li></ul></b>', '<ul><li>hello</li><li style="font-weight: bold; ">world</li></ul>');
-testSingleToggle("bold", selectSecondWord, '<ul><li>hello</li><li style="font-weight: bold; ">world</li></ul>', '<ul><li>hello</li><li>world</li></ul>');
+testSingleToggle("bold", selectLastWord, '<ul><li>hello</li><li style="font-weight: bold; ">world</li></ul>', '<ul><li>hello</li><li>world</li></ul>');
 testSingleToggle("bold", selectFirstWord, '<b><ul><li>hello world</li><li>webkit</li></ul></b>', '<ul><li>hello<b> world</b></li><li style="font-weight: bold; ">webkit</li></ul>');
 
 testSingleToggle("italic", selectFirstWord, '<i><ul><li><i>a</i></li></ul></i>', '<ul><li>a</li></ul>');
 testSingleToggle("italic", selectFirstWord, '<i><ul><li>hello</li><li>world</li></ul></i>', '<ul><li>hello</li><li style="font-style: italic; ">world</li></ul>');
-testSingleToggle("italic", selectSecondWord, '<ul><li>hello</li><li style="font-style: italic; ">world</li></ul>', '<ul><li>hello</li><li>world</li></ul>');
+testSingleToggle("italic", selectLastWord, '<ul><li>hello</li><li style="font-style: italic; ">world</li></ul>', '<ul><li>hello</li><li>world</li></ul>');
 testSingleToggle("italic", selectFirstWord, '<i><ul><li>hello world</li><li>webkit</li></ul></i>', '<ul><li>hello<i> world</i></li><li style="font-style: italic; ">webkit</li></ul>');
 
 testSingleToggle("underline", selectFirstWord, '<u><ul><li><u>a</u></li></ul></u>', '<ul><li>a</li></ul>');
 testSingleToggle("underline", selectFirstWord, '<u><ul><li>hello</li><li>world</li></ul></u>', '<ul><li>hello</li><li style="text-decoration: underline; ">world</li></ul>');
-testSingleToggle("underline", selectSecondWord, '<ul><li>hello</li><li style="text-decoration: underline; ">world</li></ul>', '<ul><li>hello</li><li>world</li></ul>');
+testSingleToggle("underline", selectLastWord, '<ul><li>hello</li><li style="text-decoration: underline; ">world</li></ul>', '<ul><li>hello</li><li>world</li></ul>');
 testSingleToggle("underline", selectFirstWord, '<u><ul><li>hello world</li><li>webkit</li></ul></u>', '<ul><li>hello<u> world</u></li><li style="text-decoration: underline; ">webkit</li></ul>');
 
 testSingleToggle("strikethrough", selectFirstWord, '<s><ul><li><s>a</s></li></ul></s>', '<ul><li>a</li></ul>');
 testSingleToggle("strikethrough", selectFirstWord, '<s><ul><li>hello</li><li>world</li></ul></s>', '<ul><li>hello</li><li style="text-decoration: line-through; ">world</li></ul>');
-testSingleToggle("strikethrough", selectSecondWord, '<ul><li>hello</li><li style="text-decoration: line-through; ">world</li></ul>', '<ul><li>hello</li><li>world</li></ul>');
+testSingleToggle("strikethrough", selectLastWord, '<ul><li>hello</li><li style="text-decoration: line-through; ">world</li></ul>', '<ul><li>hello</li><li>world</li></ul>');
 testSingleToggle("strikethrough", selectFirstWord, '<s><ul><li>hello world</li><li>webkit</li></ul></s>', '<ul><li>hello<s> world</s></li><li style="text-decoration: line-through; ">webkit</li></ul>');
 
 document.body.removeChild(testContainer);
