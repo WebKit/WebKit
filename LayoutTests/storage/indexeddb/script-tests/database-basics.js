4bb0c4ac759175c0068f6485f5c17f880c03b53a
Jeremy Orlow <jorlow@chromium.org>
2010-09-03 13:15:28 +0000

    2010-09-03  Jeremy Orlow  <jorlow@chromium.org>
            Reviewed by Steve Block.
    
            Fix and beef up IndexedDB layout test
            https://bugs.webkit.org/show_bug.cgi?id=45035
    
            Implement some fixmes and such.
    
            * storage/indexeddb/database-basics-expected.txt:
            * storage/indexeddb/script-tests/database-basics.js:
            (openSuccess):
            (setVersionAgain):
            (createObjectStore):
            (checkObjectStore):
    
    Canonical link: https://commits.webkit.org/57487@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@66728 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/storage/indexeddb/script-tests/database-basics.js
+++ LayoutTests/storage/indexeddb/script-tests/database-basics.js
@@ -7,15 +7,13 @@ function openSuccess()
     verifySuccessEvent(event);
 
     var db = evalAndLog("db = event.result");
-    shouldBeEqualToString("db.name", "name");
-    shouldBe("db.objectStores", "[]");
-    shouldBe("db.objectStores.length", "0");
-    shouldBe("db.objectStores.contains('')", "false");
-    // FIXME: Test .item() once it's possible to get back a non-empty list.
+    deleteAllObjectStores(db);
 
-    // FIXME: Test the other properties of IDBDatabase as they're written.
+    // We must do something asynchronous before anything synchronous since
+    // deleteAllObjectStores only schedules the object stores to be removed.
+    // We don't know for sure whether it's happened until an IDBRequest object
+    // that was created after the removes fires.
 
-    debug("");
     debug("Testing setVersion.");
     result = evalAndLog('db.setVersion("version a")');
     verifyResult(result);
@@ -25,15 +23,37 @@ function openSuccess()
 
 function setVersionAgain()
 {
+    verifySuccessEvent(event);
+
     result = evalAndLog('db.setVersion("version b")');
     verifyResult(result);
-    result.onsuccess = checkVersion;
+    result.onsuccess = createObjectStore;
     result.onError = unexpectedErrorCallback;
 }
 
-function checkVersion()
+function createObjectStore()
 {
+    verifySuccessEvent(event);
     shouldBeEqualToString("db.version", "version b");
+    shouldBeEqualToString("db.name", "name");
+    shouldBe("db.objectStores", "[]");
+    shouldBe("db.objectStores.length", "0");
+    shouldBe("db.objectStores.contains('')", "false");
+
+    result = evalAndLog('db.createObjectStore("test123")');
+    verifyResult(result);
+    result.onsuccess = checkObjectStore;
+    result.onError = unexpectedErrorCallback;
+}
+
+function checkObjectStore()
+{
+    verifySuccessEvent(event);
+    shouldBe("db.objectStores", "['test123']");
+    shouldBe("db.objectStores.length", "1");
+    shouldBe("db.objectStores.contains('')", "false");
+    shouldBe("db.objectStores.contains('test456')", "false");
+    shouldBe("db.objectStores.contains('test123')", "true");
 
     done();
 }
