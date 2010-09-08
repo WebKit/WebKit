cbe581f0b0aee077b1f78400a49e85bedfd07c1a
Peter Kasting <pkasting@chromium.org>
2010-09-08 20:33:06 +0000

    WebCore: Not reviewed, fallout from http://trac.webkit.org/changeset/67001
    Fix compile failures and add svn:eol-style on two new files.
    
    * platform/ScrollAnimator.h: Added property svn:eol-style.
    * platform/ScrollAnimatorWin.cpp: Added property svn:eol-style.
    * platform/win/PopupMenuWin.cpp: Fix compile errors.
    (WebCore::PopupMenuWin::scrollToRevealSelection):
    (WebCore::PopupMenuWin::scrollSize):
    
    LayoutTests: Not reviewed, skip failing layout tests.
    
    https://bugs.webkit.org/show_bug.cgi?id=45411
    
    * platform/qt/Skipped:
    
    
    
    Canonical link: https://commits.webkit.org/57759@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@67009 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/win/PopupMenuWin.cpp
+++ WebCore/platform/win/PopupMenuWin.cpp
@@ -531,12 +531,12 @@ bool PopupMenuWin::scrollToRevealSelection()
     int index = focusedIndex();
 
     if (index < m_scrollOffset) {
-        m_scrollbar->setValue(index);
+        m_scrollbar->setValue(index, Scrollbar::NotFromScrollAnimator);
         return true;
     }
 
     if (index >= m_scrollOffset + visibleItems()) {
-        m_scrollbar->setValue(index - visibleItems() + 1);
+        m_scrollbar->setValue(index - visibleItems() + 1, Scrollbar::NotFromScrollAnimator);
         return true;
     }
 
@@ -666,7 +666,7 @@ void PopupMenuWin::paint(const IntRect& damageRect, HDC hdc)
 
 int PopupMenuWin::scrollSize(ScrollbarOrientation orientation) const
 {
-    return ((orientation == VerticallScrollbar) && m_scrollbar) ? (m_scrollbar->totalSize() - m_scrollbar->visibleSize()) : 0;
+    return ((orientation == VerticalScrollbar) && m_scrollbar) ? (m_scrollbar->totalSize() - m_scrollbar->visibleSize()) : 0;
 }
 
 void PopupMenuWin::setScrollOffsetFromAnimation(const IntPoint& offset)
