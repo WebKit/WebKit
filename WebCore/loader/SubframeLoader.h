385ec2c7c16387d48b1fb6c04374f8d2cad002c2
Dirk Pranke <dpranke@chromium.org>
2010-09-10 07:45:15 +0000

    2010-09-10  Dirk Pranke  <dpranke@chromium.org>
            Reviewed by Kent Tamura.
    
            roll out r66992 and r66997 - possibly introduced a perf slowdown in chromium tests
            https://bugs.webkit.org/show_bug.cgi?id=45524
    
            * fast/replaced/object-param-url-control-char.html:
            * fast/replaced/object-with-embed-url-param-expected.txt:
            * fast/replaced/object-with-embed-url-param.html:
    2010-09-10  Dirk Pranke  <dpranke@chromium.org>
    
            Reviewed by Kent Tamura.
    
            roll out r66992 and r66997 - possibly introduced a perf slowdown in chromium tests
            https://bugs.webkit.org/show_bug.cgi?id=45524
    
            * html/HTMLObjectElement.cpp:
            (WebCore::HTMLObjectElement::parametersForPlugin):
            (WebCore::HTMLObjectElement::updateWidget):
            * loader/SubframeLoader.cpp:
            (WebCore::SubframeLoader::requestFrame):
            * loader/SubframeLoader.h:
    
    Canonical link: https://commits.webkit.org/57914@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@67178 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/SubframeLoader.h
+++ WebCore/loader/SubframeLoader.h
@@ -75,8 +75,6 @@ public:
     bool allowPlugins(ReasonForCallingAllowPlugins);
 
     bool containsPlugins() const { return m_containsPlugins; }
-    
-    bool resourceWillUsePlugin(const String& url, const String& mimeType);
 
 private:
     Frame* loadOrRedirectSubframe(HTMLFrameOwnerElement*, const KURL&, const AtomicString& frameName, bool lockHistory, bool lockBackForwardList);
