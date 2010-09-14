0a592a78f10c2b0f8014af4d5fb4f5582df9cd51
Commit Queue <commit-queue@webkit.org>
2010-09-14 16:49:55 +0000

    2010-09-14  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Darin Adler.
    
            Share UnicodeMacrosFromICU.h
            https://bugs.webkit.org/show_bug.cgi?id=45710
    
            glib, qt4 and wince use the same macros from ICU.
            Remove the code duplication and use the same header file.
    
            * wtf/unicode/UnicodeMacrosFromICU.h: Copied from JavaScriptCore/wtf/unicode/glib/UnicodeMacrosFromICU.h.
            * wtf/unicode/glib/UnicodeMacrosFromICU.h: Removed.
            * wtf/unicode/qt4/UnicodeQt4.h:
            * wtf/unicode/wince/UnicodeWince.h:
    
    Canonical link: https://commits.webkit.org/58206@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@67475 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/unicode/qt4/UnicodeQt4.h
+++ JavaScriptCore/wtf/unicode/qt4/UnicodeQt4.h
@@ -23,6 +23,8 @@
 #ifndef WTF_UNICODE_QT4_H
 #define WTF_UNICODE_QT4_H
 
+#include "UnicodeMacrosFromICU.h"
+
 #include <QChar>
 #include <QString>
 
@@ -62,49 +64,6 @@ typedef wchar_t UChar;
 typedef uint16_t UChar;
 #endif
 typedef uint32_t UChar32;
-
-// some defines from ICU
-// FIXME: This should use UnicodeMacrosFromICU.h instead!
-
-#define U_IS_BMP(c) ((UChar32)(c)<=0xffff)
-#define U16_IS_LEAD(c) (((c)&0xfffffc00)==0xd800)
-#define U16_IS_TRAIL(c) (((c)&0xfffffc00)==0xdc00)
-#define U16_SURROGATE_OFFSET ((0xd800<<10UL)+0xdc00-0x10000)
-#define U16_GET_SUPPLEMENTARY(lead, trail) \
-    (((UChar32)(lead)<<10UL)+(UChar32)(trail)-U16_SURROGATE_OFFSET)
-
-#define U16_LEAD(supplementary) (UChar)(((supplementary)>>10)+0xd7c0)
-#define U16_TRAIL(supplementary) (UChar)(((supplementary)&0x3ff)|0xdc00)
-#define U16_LENGTH(c) ((uint32_t)(c) <= 0xffff ? 1 : 2)
-
-#define U_IS_SURROGATE(c) (((c)&0xfffff800)==0xd800)
-#define U16_IS_SINGLE(c) !U_IS_SURROGATE(c)
-#define U16_IS_SURROGATE(c) U_IS_SURROGATE(c)
-#define U16_IS_SURROGATE_LEAD(c) (((c)&0x400)==0)
-
-#define U16_NEXT(s, i, length, c) { \
-    (c)=(s)[(i)++]; \
-    if(U16_IS_LEAD(c)) { \
-        uint16_t __c2; \
-        if((i)<(length) && U16_IS_TRAIL(__c2=(s)[(i)])) { \
-            ++(i); \
-            (c)=U16_GET_SUPPLEMENTARY((c), __c2); \
-        } \
-    } \
-}
-
-#define U16_PREV(s, start, i, c) { \
-    (c)=(s)[--(i)]; \
-    if(U16_IS_TRAIL(c)) { \
-        uint16_t __c2; \
-        if((i)>(start) && U16_IS_LEAD(__c2=(s)[(i)-1])) { \
-            --(i); \
-            (c)=U16_GET_SUPPLEMENTARY(__c2, (c)); \
-        } \
-    } \
-}
-
-#define U_MASK(x) ((uint32_t)1<<(x))
 
 namespace WTF {
 namespace Unicode {
--- JavaScriptCore/wtf/unicode/wince/UnicodeWince.h
+++ JavaScriptCore/wtf/unicode/wince/UnicodeWince.h
@@ -24,51 +24,11 @@
 #ifndef UNICODE_WINCE_H
 #define UNICODE_WINCE_H
 
+#include "UnicodeMacrosFromICU.h"
+
 #include "ce_unicode.h"
 
 #define TO_MASK(x) (1 << (x))
-
-// some defines from ICU needed one or two places
-// FIXME: This should use UnicodeMacrosFromICU.h instead!
-
-#define U_IS_BMP(c) ((UChar32)(c)<=0xffff)
-#define U16_IS_LEAD(c) (((c) & 0xfffffc00) == 0xd800)
-#define U16_IS_TRAIL(c) (((c) & 0xfffffc00) == 0xdc00)
-#define U16_SURROGATE_OFFSET ((0xd800 << 10UL) + 0xdc00 - 0x10000)
-#define U16_GET_SUPPLEMENTARY(lead, trail) \
-    (((UChar32)(lead) << 10UL) + (UChar32)(trail) - U16_SURROGATE_OFFSET)
-
-#define U16_LEAD(supplementary) (UChar)(((supplementary) >> 10) + 0xd7c0)
-#define U16_TRAIL(supplementary) (UChar)(((supplementary) & 0x3ff) | 0xdc00)
-#define U16_LENGTH(c) ((uint32_t)(c) <= 0xffff ? 1 : 2)
-
-#define U_IS_SURROGATE(c) (((c) & 0xfffff800) == 0xd800)
-#define U16_IS_SURROGATE(c) U_IS_SURROGATE(c)
-#define U16_IS_SURROGATE_LEAD(c) (((c) & 0x400) == 0)
-
-#define U16_NEXT(s, i, length, c) { \
-    (c)=(s)[(i)++]; \
-    if (U16_IS_LEAD(c)) { \
-        uint16_t __c2; \
-        if ((i) < (length) && U16_IS_TRAIL(__c2 = (s)[(i)])) { \
-            ++(i); \
-            (c) = U16_GET_SUPPLEMENTARY((c), __c2); \
-        } \
-    } \
-}
-
-#define U16_PREV(s, start, i, c) { \
-    (c)=(s)[--(i)]; \
-    if (U16_IS_TRAIL(c)) { \
-        uint16_t __c2; \
-        if ((i) > (start) && U16_IS_LEAD(__c2 = (s)[(i) - 1])) { \
-            --(i); \
-            (c) = U16_GET_SUPPLEMENTARY(__c2, (c)); \
-        } \
-    } \
-}
-
-#define U16_IS_SINGLE(c) !U_IS_SURROGATE(c)
 
 namespace WTF {
 
