bae4581bba09f846d8bbdb8c2f56236f0cf44c85
Steve Block <steveblock@google.com>
2010-09-15 15:52:58 +0000

    2010-09-15  Steve Block  <steveblock@google.com>
            Reviewed by Jeremy Orlow.
    
            fast/dom/DeviceOrientation/add-listener-from-callback flaky
            https://bugs.webkit.org/show_bug.cgi?id=45814
    
            When we call addEventListener(), an event is sent to all registered
            event listeners on that page. So in the existing test, both the calls
            to setMockDeviceOrientation() and addEventListener() will trigger events
            to fire asynchronously on both listeners (both will be registered by the
            time the event fires), giving a total of five events. A race condition
            exists as to whether the test finishes after the third event, before
            the fourth event fires.
    
            This patch fixes the problem by expecting the correct number of events
            to fire. We also remove the call to setMockDeviceOrientation() as this
            adds extra complication and is tested in update.html.
    
            * fast/dom/DeviceOrientation/add-listener-from-callback-expected.txt:
            * fast/dom/DeviceOrientation/script-tests/add-listener-from-callback.js:
            (checkOrientation):
            (firstListener):
    
    
    Canonical link: https://commits.webkit.org/58282@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@67557 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/dom/DeviceOrientation/script-tests/add-listener-from-callback.js
+++ LayoutTests/fast/dom/DeviceOrientation/script-tests/add-listener-from-callback.js
@@ -1,30 +1,29 @@
 description('Tests that adding a new event listener from a callback works as expected.');
 
-var mockEvent;
-function setMockOrientation(alpha, beta, gamma) {
-    mockEvent = {alpha: alpha, beta: beta, gamma: gamma};
-    if (window.layoutTestController)
-        layoutTestController.setMockDeviceOrientation(true, mockEvent.alpha, true, mockEvent.beta, true, mockEvent.gamma);
-    else
-        debug('This test can not be run without the LayoutTestController');
-}
+var mockAlpha = 1.1;
+var mockBeta = 2.2;
+var mockGamma = 3.3;
+
+if (window.layoutTestController)
+    layoutTestController.setMockDeviceOrientation(true, mockAlpha, true, mockBeta, true, mockGamma);
+else
+    debug('This test can not be run without the LayoutTestController');
 
 var deviceOrientationEvent;
 function checkOrientation(event) {
     deviceOrientationEvent = event;
-    shouldBe('deviceOrientationEvent.alpha', 'mockEvent.alpha');
-    shouldBe('deviceOrientationEvent.beta', 'mockEvent.beta');
-    shouldBe('deviceOrientationEvent.gamma', 'mockEvent.gamma');
+    shouldBe('deviceOrientationEvent.alpha', 'mockAlpha');
+    shouldBe('deviceOrientationEvent.beta', 'mockBeta');
+    shouldBe('deviceOrientationEvent.gamma', 'mockGamma');
 }
 
 var firstListenerEvents = 0;
 function firstListener(event) {
     checkOrientation(event);
     if (++firstListenerEvents == 1)
-        setMockOrientation(11.1, 22.2, 33.3);
+        window.addEventListener('deviceorientation', secondListener);
     else if (firstListenerEvents > 2)
         testFailed('Too many events for first listener.');
-    window.addEventListener('deviceorientation', secondListener);
     maybeFinishTest();
 }
 
@@ -41,7 +40,6 @@ function maybeFinishTest() {
         finishJSTest();
 }
 
-setMockOrientation(1.1, 2.2, 3.3);
 window.addEventListener('deviceorientation', firstListener);
 
 window.jsTestIsAsync = true;
