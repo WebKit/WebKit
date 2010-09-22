ec261d25caa41080faff83e3e3954bb2d3f460e0
Dirk Pranke <dpranke@chromium.org>
2010-09-22 19:38:45 +0000

    2010-09-22  Dirk Pranke  <dpranke@chromium.org>
            Reviewed by Tony Chang.
    
            new-run-webkit-tests: r68063 broke linux python tests
    
            Add a missing "from __future__ import with_statement" to this
            new file; the perils of testing only on the Mac and by review :(
    
            https://bugs.webkit.org/show_bug.cgi?id=46293
    
            * Scripts/webkitpy/layout_tests/port/chromium_gpu.py:
    
    Canonical link: https://commits.webkit.org/58775@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@68073 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/webkitpy/layout_tests/port/chromium_gpu.py
+++ WebKitTools/Scripts/webkitpy/layout_tests/port/chromium_gpu.py
@@ -24,6 +24,8 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import with_statement
+
 import codecs
 import os
 import sys
