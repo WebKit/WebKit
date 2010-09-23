c7ca0c8eeafeacd8e27825c060f7684e40a6752a
Abhishek Arya <inferno@chromium.org>
2010-09-23 17:21:07 +0000

    2010-09-23  Abhishek Arya  <inferno@chromium.org>
            Reviewed by Dave Hyatt.
    
            Fix the memory safety issue by checking containerObject is a RenderBox
            before doing the transforms.
            https://bugs.webkit.org/show_bug.cgi?id=46365
    
            Test: compositing/overflow/get-transform-from-non-box-container.html
    
            * rendering/RenderObject.cpp:
            (WebCore::RenderObject::getTransformFromContainer):
    2010-09-23  Abhishek Arya  <inferno@chromium.org>
    
            Reviewed by Dave Hyatt.
    
            Tests that we donot assert on object->isBox.
            https://bugs.webkit.org/show_bug.cgi?id=46365
    
            * compositing/overflow/get-transform-from-non-box-container-expected.txt: Added.
            * compositing/overflow/get-transform-from-non-box-container.html: Added.
    
    
    Canonical link: https://commits.webkit.org/58857@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@68158 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderObject.cpp
+++ WebCore/rendering/RenderObject.cpp
@@ -1988,7 +1988,7 @@ void RenderObject::getTransformFromContainer(const RenderObject* containerObject
         transform.multLeft(layer->currentTransform());
     
 #if ENABLE(3D_RENDERING)
-    if (containerObject && containerObject->hasLayer() && containerObject->style()->hasPerspective()) {
+    if (containerObject && containerObject->isBox() && containerObject->hasLayer() && containerObject->style()->hasPerspective()) {
         // Perpsective on the container affects us, so we have to factor it in here.
         ASSERT(containerObject->hasLayer());
         FloatPoint perspectiveOrigin = toRenderBox(containerObject)->layer()->perspectiveOrigin();
