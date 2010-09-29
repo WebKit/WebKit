e4ce039ab8647177c7522765115312402ce4e1d0
Mark Rowe <mrowe@apple.com>
2010-09-29 23:20:52 +0000

    Merge r62873.
    Canonical link: https://commits.webkit.org/50175.326@safari-533.19-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-533.19-branch@68710 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/editing/SelectionController.cpp
+++ WebCore/editing/SelectionController.cpp
@@ -1434,6 +1434,9 @@ void SelectionController::updateAppearance()
     }
 #endif
 
+    // We need to update style in case the node containing the selection is made display:none.
+    m_frame->document()->updateStyleIfNeeded();
+
     RenderView* view = m_frame->contentRenderer();
     if (!view)
         return;
