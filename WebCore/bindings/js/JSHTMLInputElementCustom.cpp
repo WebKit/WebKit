042406f6593bb0ddd48642f72bb2c2f600987471
Dan Bernstein <mitz@apple.com>
2010-09-29 21:12:44 +0000

    Removed input type site-specific quirk for mail.google.com https://bugs.webkit.org/show_bug.cgi?id=46836
    Reviewed by Sam Weinig.
    
    * bindings/js/JSHTMLInputElementCustom.cpp:
    (WebCore::needsGmailQuirk): Removed.
    (WebCore::JSHTMLInputElement::type): Removed this custom getter.
    * html/HTMLInputElement.idl: Removed the JSCCustomGetter custom attribute
    from the type attribute.
    
    
    
    Canonical link: https://commits.webkit.org/59342@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@68683 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/bindings/js/JSHTMLInputElementCustom.cpp
+++ WebCore/bindings/js/JSHTMLInputElementCustom.cpp
@@ -26,46 +26,12 @@
 #include "config.h"
 #include "JSHTMLInputElement.h"
 
-#include "Document.h"
 #include "HTMLInputElement.h"
-#include "Settings.h"
 #include <runtime/Error.h>
 
 using namespace JSC;
 
 namespace WebCore {
-
-static bool needsGmailQuirk(HTMLInputElement* input)
-{
-    Document* document = input->document();
-
-    const KURL& url = document->url();
-    if (url.host() != "mail.google.com")
-        return false;
-
-    // As with other site-specific quirks, allow website developers to turn this off.
-    // In theory, this allows website developers to check if their fixes are effective.
-    Settings* settings = document->settings();
-    if (!settings)
-        return false;
-    if (!settings->needsSiteSpecificQuirks())
-        return false;
-
-    return true;
-}
-
-JSValue JSHTMLInputElement::type(ExecState* exec) const
-{
-    HTMLInputElement* input = static_cast<HTMLInputElement*>(impl());
-    const AtomicString& type = input->type();
-
-    DEFINE_STATIC_LOCAL(const AtomicString, url, ("url"));
-    DEFINE_STATIC_LOCAL(const AtomicString, text, ("text"));
-
-    if (type == url && needsGmailQuirk(input))
-        return jsString(exec, text);
-    return jsString(exec, type);
-}
 
 JSValue JSHTMLInputElement::selectionStart(ExecState* exec) const
 {
