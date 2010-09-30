145f84d6ca4c51c3374dd5d486d7d323855af171
Commit Queue <commit-queue@webkit.org>
2010-09-30 10:29:34 +0000

    2010-09-30  Kwang Yul Seo  <skyul@company100.net>
            Reviewed by Andreas Kling.
    
            [BREWMP] Leave initializeRandomNumberGenerator empty.
            https://bugs.webkit.org/show_bug.cgi?id=46851
    
            On Brew MP, AEECLSID_RANDOM initializes itself.
    
            * wtf/RandomNumberSeed.h:
            (WTF::initializeRandomNumberGenerator):
    
    Canonical link: https://commits.webkit.org/59402@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@68770 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/RandomNumberSeed.h
+++ JavaScriptCore/wtf/RandomNumberSeed.h
@@ -64,6 +64,8 @@ inline void initializeRandomNumberGenerator()
     init_by_array(initializationBuffer, 4);
 #elif COMPILER(MSVC) && defined(_CRT_RAND_S)
     // On Windows we use rand_s which initialises itself
+#elif PLATFORM(BREWMP)
+    // On Brew MP we use AEECLSID_RANDOM which initialises itself
 #elif OS(UNIX)
     // srandomdev is not guaranteed to exist on linux so we use this poor seed, this should be improved
     timeval time;
