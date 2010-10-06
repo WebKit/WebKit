9a624142766bd1bdcabbca0ed7c48e63aa47c4e2
Pavel Podivilov <podivilov@chromium.org>
2010-10-06 08:17:57 +0000

    2010-10-06  Pavel Podivilov  <podivilov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: add "Set Breakpoint" item to XHR resource context menu.
            https://bugs.webkit.org/show_bug.cgi?id=47085
    
            * English.lproj/localizedStrings.js:
            * inspector/front-end/ResourcesPanel.js:
            (WebInspector.ResourcesPanel.prototype._contextMenu):
    
    Canonical link: https://commits.webkit.org/59786@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@69180 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/inspector/front-end/ResourcesPanel.js
+++ WebCore/inspector/front-end/ResourcesPanel.js
@@ -1250,17 +1250,27 @@ WebInspector.ResourcesPanel.prototype = {
 
     _contextMenu: function(event)
     {
-        // createBlobURL is enabled conditionally, do not expose resource export if it's not available.
-        if (typeof window.createBlobURL !== "function" || !Preferences.resourceExportEnabled)
-            return;
-
         var contextMenu = new WebInspector.ContextMenu();
         var resourceTreeItem = event.target.enclosingNodeOrSelfWithClass("resource-sidebar-tree-item");
-        if (resourceTreeItem && resourceTreeItem.treeElement) {
-            var resource = resourceTreeItem.treeElement.representedObject;
-            contextMenu.appendItem(WebInspector.UIString("Export to HAR"), this._exportResource.bind(this, resource));
+        var resource;
+        if (resourceTreeItem && resourceTreeItem.treeElement)
+            resource = resourceTreeItem.treeElement.representedObject;
+
+        var needSeparator = false;
+        // createBlobURL is enabled conditionally, do not expose resource export if it's not available.
+        if (typeof window.createBlobURL === "function" && Preferences.resourceExportEnabled) {
+            if (resource)
+                contextMenu.appendItem(WebInspector.UIString("Export to HAR"), this._exportResource.bind(this, resource));
+            contextMenu.appendItem(WebInspector.UIString("Export all to HAR"), this._exportAll.bind(this));
+            needSeparator = true;
         }
-        contextMenu.appendItem(WebInspector.UIString("Export all to HAR"), this._exportAll.bind(this));
+
+        if (resource && resource.category === WebInspector.resourceCategories.xhr) {
+            if (needSeparator)
+                contextMenu.appendSeparator();
+            contextMenu.appendItem(WebInspector.UIString("Set Breakpoint"), WebInspector.breakpointManager.createXHRBreakpoint.bind(WebInspector.breakpointManager, resource.url));
+        }
+
         contextMenu.show(event);
     },
 
