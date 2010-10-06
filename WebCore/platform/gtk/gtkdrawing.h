982fe8fbb13f592834dbbe1a13b4ebf7760a1a8d
Commit Queue <commit-queue@webkit.org>
2010-10-06 17:04:39 +0000

    2010-10-06  Carlos Garcia Campos  <cgarcia@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] Remove unused code from gtk2drawing.c
            https://bugs.webkit.org/show_bug.cgi?id=47086
    
            gtk2drawing.c was copied from mozilla and contains a lot of code that is not
            used by WebKit. Most of that unused code is uncompatible with gtk3, so
            removing it will make easier porting to gtk3.
    
            * platform/gtk/gtk2drawing.c:
            (moz_gtk_get_widget_border):
            (moz_gtk_widget_paint):
            (moz_gtk_destroy_theme_parts_widgets):
            * platform/gtk/gtkdrawing.h:
    
    Canonical link: https://commits.webkit.org/59811@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@69205 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/gtk/gtkdrawing.h
+++ WebCore/platform/gtk/gtkdrawing.h
@@ -91,7 +91,6 @@ typedef struct _GtkThemeParts {
     GtkWidget* radiobuttonWidget;
     GtkWidget* horizScrollbarWidget;
     GtkWidget* vertScrollbarWidget;
-    GtkWidget* spinWidget;
     GtkWidget* hScaleWidget;
     GtkWidget* vScaleWidget;
     GtkWidget* entryWidget;
@@ -103,28 +102,7 @@ typedef struct _GtkThemeParts {
     GtkWidget* comboBoxEntryTextareaWidget;
     GtkWidget* comboBoxEntryButtonWidget;
     GtkWidget* comboBoxEntryArrowWidget;
-    GtkWidget* handleBoxWidget;
-    GtkWidget* toolbarWidget;
-    GtkWidget* frameWidget;
-    GtkWidget* statusbarWidget;
     GtkWidget* progresWidget;
-    GtkWidget* tabWidget;
-    GtkWidget* tooltipWidget;
-    GtkWidget* menuBarWidget;
-    GtkWidget* menuBarItemWidget;
-    GtkWidget* menuPopupWidget;
-    GtkWidget* menuItemWidget;
-    GtkWidget* imageMenuItemWidget;
-    GtkWidget* checkMenuItemWidget;
-    GtkWidget* treeViewWidget;
-    GtkTreeViewColumn* middleTreeViewColumn;
-    GtkWidget* treeHeaderCellWidget;
-    GtkWidget* treeHeaderSortArrowWidget;
-    GtkWidget* expanderWidget;
-    GtkWidget* toolbarSeparatorWidget;
-    GtkWidget* menuSeparatorWidget;
-    GtkWidget* hpanedWidget;
-    GtkWidget* vpanedWidget;
     GtkWidget* scrolledWindowWidget;
 } GtkThemeParts;
 
@@ -133,24 +111,6 @@ typedef enum {
   MOZ_GTK_STEPPER_BOTTOM      = 1 << 1,
   MOZ_GTK_STEPPER_VERTICAL    = 1 << 2
 } GtkScrollbarButtonFlags;
-
-/** flags for tab state **/
-typedef enum {
-  /* first eight bits are used to pass a margin */
-  MOZ_GTK_TAB_MARGIN_MASK     = 0xFF,
-  /* bottom tabs */
-  MOZ_GTK_TAB_BOTTOM          = 1 << 8,
-  /* the first tab in the group */
-  MOZ_GTK_TAB_FIRST           = 1 << 9,
-  /* the selected tab */
-  MOZ_GTK_TAB_SELECTED        = 1 << 10
-} GtkTabFlags;
-
-/** flags for menuitems **/
-typedef enum {
-  /* menuitem is part of the menubar */
-  MOZ_TOPLEVEL_MENU_ITEM      = 1 << 0
-} GtkMenuItemFlags;
 
 /* function type for moz_gtk_enable_style_props */
 typedef gint (*style_prop_t)(GtkStyle*, const gchar*, gint);
@@ -191,80 +151,13 @@ typedef enum {
   /* Paints a GtkScale thumb. */
   MOZ_GTK_SCALE_THUMB_HORIZONTAL,
   MOZ_GTK_SCALE_THUMB_VERTICAL,
-  /* Paints a GtkSpinButton */
-  MOZ_GTK_SPINBUTTON,
-  MOZ_GTK_SPINBUTTON_UP,
-  MOZ_GTK_SPINBUTTON_DOWN,
-  MOZ_GTK_SPINBUTTON_ENTRY,
-  /* Paints the gripper of a GtkHandleBox. */
-  MOZ_GTK_GRIPPER,
-  /* Paints a GtkEntry. */
   MOZ_GTK_ENTRY,
-  /* Paints the native caret (or in GTK-speak: insertion cursor) */
-  MOZ_GTK_ENTRY_CARET,
   /* Paints a GtkOptionMenu. */
   MOZ_GTK_DROPDOWN,
-  /* Paints a dropdown arrow (a GtkButton containing a down GtkArrow). */
-  MOZ_GTK_DROPDOWN_ARROW,
-  /* Paints an entry in an editable option menu */
-  MOZ_GTK_DROPDOWN_ENTRY,
-  /* Paints the container part of a GtkCheckButton. */
-  MOZ_GTK_CHECKBUTTON_CONTAINER,
-  /* Paints the container part of a GtkRadioButton. */
-  MOZ_GTK_RADIOBUTTON_CONTAINER,
-  /* Paints the label of a GtkCheckButton (focus outline) */
-  MOZ_GTK_CHECKBUTTON_LABEL,
-  /* Paints the label of a GtkRadioButton (focus outline) */
-  MOZ_GTK_RADIOBUTTON_LABEL,
-  /* Paints the background of a GtkHandleBox. */
-  MOZ_GTK_TOOLBAR,
-  /* Paints a toolbar separator */
-  MOZ_GTK_TOOLBAR_SEPARATOR,
-  /* Paints a GtkToolTip */
-  MOZ_GTK_TOOLTIP,
-  /* Paints a GtkFrame (e.g. a status bar panel). */
-  MOZ_GTK_FRAME,
-  /* Paints a resize grip for a GtkWindow */
-  MOZ_GTK_RESIZER,
   /* Paints a GtkProgressBar. */
   MOZ_GTK_PROGRESSBAR,
   /* Paints a progress chunk of a GtkProgressBar. */
-  MOZ_GTK_PROGRESS_CHUNK,
-  /* Paints a tab of a GtkNotebook. flags is a GtkTabFlags, defined above. */
-  MOZ_GTK_TAB,
-  /* Paints the background and border of a GtkNotebook. */
-  MOZ_GTK_TABPANELS,
-  /* Paints a GtkArrow for a GtkNotebook. flags is a GtkArrowType. */
-  MOZ_GTK_TAB_SCROLLARROW,
-  /* Paints the background and border of a GtkTreeView */
-  MOZ_GTK_TREEVIEW,
-  /* Paints treeheader cells */
-  MOZ_GTK_TREE_HEADER_CELL,
-  /* Paints sort arrows in treeheader cells */
-  MOZ_GTK_TREE_HEADER_SORTARROW,
-  /* Paints an expander for a GtkTreeView */
-  MOZ_GTK_TREEVIEW_EXPANDER,
-  /* Paints a GtkExpander */
-  MOZ_GTK_EXPANDER,
-  /* Paints the background of the menu bar. */
-  MOZ_GTK_MENUBAR,
-  /* Paints the background of menus, context menus. */
-  MOZ_GTK_MENUPOPUP,
-  /* Paints the arrow of menuitems that contain submenus */
-  MOZ_GTK_MENUARROW,
-  /* Paints an arrow that points down */
-  MOZ_GTK_TOOLBARBUTTON_ARROW,
-  /* Paints items of menubar and popups. */
-  MOZ_GTK_MENUITEM,
-  MOZ_GTK_CHECKMENUITEM,
-  MOZ_GTK_RADIOMENUITEM,
-  MOZ_GTK_MENUSEPARATOR,
-  /* Paints a GtkVPaned separator */
-  MOZ_GTK_SPLITTER_HORIZONTAL,
-  /* Paints a GtkHPaned separator */
-  MOZ_GTK_SPLITTER_VERTICAL,
-  /* Paints the background of a window, dialog or page. */
-  MOZ_GTK_WINDOW
+  MOZ_GTK_PROGRESS_CHUNK
 } GtkThemeWidgetType;
 
 /*** General library functions ***/
@@ -362,16 +255,6 @@ moz_gtk_checkbox_get_metrics(gint* indicator_size, gint* indicator_spacing);
 gint
 moz_gtk_radio_get_metrics(gint* indicator_size, gint* indicator_spacing);
 
-/**
- * Get the inner-border value for a GtkButton widget (button or tree header)
- * widget:             [IN]  the widget to get the border value for 
- * inner_border:       [OUT] the inner border
- *
- * returns:   MOZ_GTK_SUCCESS if there was no error, an error code otherwise
- */
-gint
-moz_gtk_button_get_inner_border(GtkWidget* widget, GtkBorder* inner_border);
-
 /** Get the focus metrics for a treeheadercell, button, checkbox, or radio button.
  * widget:             [IN]  the widget to get the focus metrics for    
  * interior_focus:     [OUT] whether the focus is drawn around the
@@ -406,89 +289,10 @@ gint
 moz_gtk_get_scrollbar_metrics(MozGtkScrollbarMetrics* metrics);
 
 /**
- * Get the desired size of a dropdown arrow button
- * width:   [OUT] the desired width
- * height:  [OUT] the desired height
- *
- * returns:    MOZ_GTK_SUCCESS if there was no error, an error code otherwise
- */
-gint moz_gtk_get_combo_box_entry_button_size(gint* width, gint* height);
-
-/**
- * Get the desired size of a scroll arrow widget
- * width:   [OUT] the desired width
- * height:  [OUT] the desired height
- *
- * returns:    MOZ_GTK_SUCCESS if there was no error, an error code otherwise
- */
-gint moz_gtk_get_tab_scroll_arrow_size(gint* width, gint* height);
-
-/**
- * Get the desired size of a toolbar button dropdown arrow
- * width:   [OUT] the desired width
- * height:  [OUT] the desired height
- *
- * returns:    MOZ_GTK_SUCCESS if there was no error, an error code otherwise
- */
-gint moz_gtk_get_downarrow_size(gint* width, gint* height);
-
-/**
- * Get the desired size of a toolbar separator
- * size:    [OUT] the desired width
- *
- * returns: MOZ_GTK_SUCCESS if there was no error, an error code otherwise
- */
-gint moz_gtk_get_toolbar_separator_width(gint* size);
-
-/**
- * Get the size of a regular GTK expander that shows/hides content
- * size:    [OUT] the size of the GTK expander, size = width = height.
- *
- * returns:    MOZ_GTK_SUCCESS if there was no error, an error code otherwise
- */
-gint moz_gtk_get_expander_size(gint* size);
-
-/**
- * Get the size of a treeview's expander (we call them twisties)
- * size:    [OUT] the size of the GTK expander, size = width = height.
- *
- * returns:    MOZ_GTK_SUCCESS if there was no error, an error code otherwise
- */
-gint moz_gtk_get_treeview_expander_size(gint* size);
-
-/**
- * Get the desired height of a menu separator
- * size:    [OUT] the desired height
- *
- * returns: MOZ_GTK_SUCCESS if there was no error, an error code otherwise
- */
-gint moz_gtk_get_menu_separator_height(gint* size);
-
-/**
- * Get the desired size of a splitter
- * orientation:   [IN]  GTK_ORIENTATION_HORIZONTAL or GTK_ORIENTATION_VERTICAL
- * size:          [OUT] width or height of the splitter handle
- *
- * returns:    MOZ_GTK_SUCCESS if there was no error, an error code otherwise
- */
-gint moz_gtk_splitter_get_metrics(gint orientation, gint* size);
-
-/**
  * Retrieve an actual GTK scrollbar widget for style analysis. It will not
  * be modified.
  */
 GtkWidget* moz_gtk_get_scrollbar_widget(void);
-
-/**
- * Get the YTHICKNESS of a tab (notebook extension).
- */
-gint moz_gtk_get_tab_thickness(void);
-
-/**
- * Get a boolean which indicates whether or not to use images in menus.
- * If TRUE, use images in menus.
- */
-gboolean moz_gtk_images_in_menus(void);
 
 /**
  * Retrieve an actual GTK progress bar widget for style analysis. It will not
