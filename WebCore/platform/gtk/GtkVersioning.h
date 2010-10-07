f8f60c6381f282b1f58b5a8dc3912351667121d6
Commit Queue <commit-queue@webkit.org>
2010-10-07 11:46:38 +0000

    2010-10-07  Carlos Garcia Campos  <cgarcia@igalia.com>
            Reviewed by Xan Lopez.
    
            [GTK] Fix the build for GTK+ 3
            https://bugs.webkit.org/show_bug.cgi?id=47249
    
            Do not use GdkDrawable deprecated API. Some methods of GdkDrawable
            are deprecated in gtk2 and have been removed in gtk3. Equivalent
            API has been added to GdkWindow.
    
            * platform/gtk/GtkVersioning.c:
            (getDefaultGDKPointerDevice):
            * platform/gtk/GtkVersioning.h:
            * platform/gtk/PlatformScreenGtk.cpp:
            (WebCore::getVisual):
            (WebCore::screenAvailableRect):
    2010-10-07  Carlos Garcia Campos  <cgarcia@igalia.com>
    
            Reviewed by Xan Lopez.
    
            [GTK] Fix the build for GTK+ 3
            https://bugs.webkit.org/show_bug.cgi?id=47249
    
            Do not use GdkDrawable deprecated API. Some methods of GdkDrawable
            are deprecated in gtk2 and have been removed in gtk3. Equivalent
            API has been added to GdkWindow.
    
            * tests/testcopyandpaste.c:
            (runPasteTestCallback):
            * webkit/webkitwebview.cpp:
            (webkit_web_view_button_press_event):
    
    Canonical link: https://commits.webkit.org/59894@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@69293 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/gtk/GtkVersioning.h
+++ WebCore/platform/gtk/GtkVersioning.h
@@ -37,6 +37,13 @@ GdkPixbuf* gdk_pixbuf_get_from_surface(cairo_surface_t* surface, int srcX, int s
                                        int width, int height);
 #endif
 
+#if !GTK_CHECK_VERSION(2, 24, 0)
+#define gdk_window_get_display(window) gdk_drawable_get_display(window)
+#ifdef GDK_DISABLE_DEPRECATED
+#define gdk_window_get_visual gdk_drawable_get_visual
+#endif
+#endif // GTK_CHECK_VERSION(2, 24, 0)
+
 #if !GTK_CHECK_VERSION(2, 21, 2)
 #define gdk_visual_get_depth(visual) (visual)->depth
 #define gdk_visual_get_bits_per_rgb(visual) (visual)->bits_per_rgb
