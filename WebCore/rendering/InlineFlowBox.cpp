b5dd85a638a68dcdc364f832984cb1d4e564f547
Abhishek Arya <inferno@chromium.org>
2010-10-07 22:00:02 +0000

    2010-10-07  Abhishek Arya  <inferno@chromium.org>
            Reviewed by Dave Hyatt.
    
            Set the m_selectedChildren flag on the root inline box if one of the leaf
            inline box from the bidi runs walk has a selection state. Remove the flag
            setting logic from addToLine, since line boxes created in createLinesBoxes
            should not be propagating selection state to root inline box.
            https://bugs.webkit.org/show_bug.cgi?id=47201
    
            Test: editing/selection/root-inlinebox-selected-children-crash.html
    
            * rendering/InlineFlowBox.cpp:
            (WebCore::InlineFlowBox::addToLine):
            * rendering/RenderBlockLineLayout.cpp:
            (WebCore::RenderBlock::constructLine):
    2010-10-07  Abhishek Arya  <inferno@chromium.org>
    
            Reviewed by Dave Hyatt.
    
            Tests that we don't crash while setting selected children on the root inline box.
            https://bugs.webkit.org/show_bug.cgi?id=47201
    
            * editing/selection/root-inlinebox-selected-children-crash-expected.txt: Added.
            * editing/selection/root-inlinebox-selected-children-crash.html: Added.
    
    
    Canonical link: https://commits.webkit.org/59946@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@69345 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/InlineFlowBox.cpp
+++ WebCore/rendering/InlineFlowBox.cpp
@@ -83,8 +83,6 @@ void InlineFlowBox::addToLine(InlineBox* child)
     child->setIsVertical(m_isVertical);
     if (child->isText())
         m_hasTextChildren = true;
-    if (child->renderer()->selectionState() != RenderObject::SelectionNone)
-        root()->setHasSelectedChildren(true);
 
     checkConsistency();
 }
