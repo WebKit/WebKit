WebKit/WebKit

commit 6aa978bac10ca5915263cdb9ff835efc89f42d0d

2010-09-30  Daniel Cheng  <dcheng@chromium.org>
        Reviewed by Tony Chang.

        [chromium] Refactor ChromiumDataObject to use getters/setters.
        https://bugs.webkit.org/show_bug.cgi?id=46559

        This is an intermediate step to converting ChromiumDataObject to use
        callbacks to the browser to retrieve data.

        This is covered by existing tests.

        * editing/chromium/EditorChromium.cpp:
        (WebCore::Editor::newGeneralClipboard):
        * page/chromium/EventHandlerChromium.cpp:
        (WebCore::EventHandler::createDraggingClipboard):
        * platform/chromium/ChromiumDataObject.cpp:
        (WebCore::ChromiumDataObject::clearData):
        (WebCore::ChromiumDataObject::clearAll):
        (WebCore::ChromiumDataObject::clearAllExceptFiles):
        (WebCore::ChromiumDataObject::hasData):
        (WebCore::ChromiumDataObject::types):
        (WebCore::ChromiumDataObject::getData):
        (WebCore::ChromiumDataObject::setData):
        (WebCore::ChromiumDataObject::ChromiumDataObject):
        * platform/chromium/ChromiumDataObject.h:
        (WebCore::ChromiumDataObject::create):
        (WebCore::ChromiumDataObject::urlTitle):
        (WebCore::ChromiumDataObject::setUrlTitle):
        (WebCore::ChromiumDataObject::htmlBaseUrl):
        (WebCore::ChromiumDataObject::setHtmlBaseUrl):
        (WebCore::ChromiumDataObject::containsFilenames):
        (WebCore::ChromiumDataObject::filenames):
        (WebCore::ChromiumDataObject::setFilenames):
        (WebCore::ChromiumDataObject::fileExtension):
        (WebCore::ChromiumDataObject::setFileExtension):
        (WebCore::ChromiumDataObject::fileContentFilename):
        (WebCore::ChromiumDataObject::setFileContentFilename):
        (WebCore::ChromiumDataObject::fileContent):
        (WebCore::ChromiumDataObject::setFileContent):
        * platform/chromium/ClipboardChromium.cpp:
        (WebCore::normalizeType):
        (WebCore::ClipboardChromium::clearData):

WebCore/platform/chromium/ClipboardMimeTypes.cpp
@@ -33,8 +33,12 @@
 
 namespace WebCore {
 
-const char textPlainType[] = "text/plain";
-const char textHtmlType[] = "text/html";
-const char textUriListType[] = "text/uri-list";
+const char mimeTypeText[] = "text/plain";
+const char mimeTypeTextPlain[] = "text/plain";
+const char mimeTypeTextPlainEtc[] = "text/plain;";
+const char mimeTypeTextHTML[] = "text/html";
+const char mimeTypeURL[] = "url";
+const char mimeTypeTextURIList[] = "text/uri-list";
+const char mimeTypeDownloadURL[] = "downloadurl";
 
 } // namespace WebCore

WebCore/editing/chromium/EditorChromium.cpp
@@ -39,7 +39,7 @@ namespace WebCore {
 
 PassRefPtr<Clipboard> Editor::newGeneralClipboard(ClipboardAccessPolicy policy, Frame* frame)
 {
-    return ClipboardChromium::create(Clipboard::CopyAndPaste, ChromiumDataObject::create(), policy, frame);
+    return ClipboardChromium::create(Clipboard::CopyAndPaste, ChromiumDataObject::create(Clipboard::CopyAndPaste), policy, frame);
 }
 
 } // namespace WebCore

WebCore/page/chromium/EventHandlerChromium.cpp
@@ -128,7 +128,7 @@ bool EventHandler::eventActivatedView(const PlatformMouseEvent& event) const
 
 PassRefPtr<Clipboard> EventHandler::createDraggingClipboard() const
 {
-    RefPtr<ChromiumDataObject> dataObject = ChromiumDataObject::create();
+    RefPtr<ChromiumDataObject> dataObject = ChromiumDataObject::create(Clipboard::DragAndDrop);
     return ClipboardChromium::create(Clipboard::DragAndDrop, dataObject.get(), ClipboardWritable, m_frame);
 }
 

WebCore/platform/chromium/ChromiumDataObject.cpp
@@ -31,56 +31,235 @@
 #include "config.h"
 #include "ChromiumDataObject.h"
 
+#include "ChromiumBridge.h"
+#include "ClipboardMimeTypes.h"
+#include "Pasteboard.h"
+
 namespace WebCore {
 
-void ChromiumDataObject::clear()
+// Per RFC 2483, the line separator for "text/..." MIME types is CR-LF.
+static char const* const textMIMETypeLineSeparator = "\r\n";
+
+void ChromiumDataObject::clearData(const String& type)
+{
+    if (type == mimeTypeTextPlain) {
+        m_plainText = "";
+        return;
+    }
+
+    if (type == mimeTypeURL || type == mimeTypeTextURIList) {
+        m_uriList.clear();
+        m_url = KURL();
+        m_urlTitle = "";
+        return;
+    }
+
+    if (type == mimeTypeTextHTML) {
+        m_textHtml = "";
+        m_htmlBaseUrl = KURL();
+        return;
+    }
+
+    if (type == mimeTypeDownloadURL) {
+        m_downloadMetadata = "";
+        return;
+    }
+}
+
+void ChromiumDataObject::clearAll()
 {
     clearAllExceptFiles();
-    filenames.clear();
+    m_filenames.clear();
 }
 
 void ChromiumDataObject::clearAllExceptFiles()
 {
-    url = KURL();
-    urlTitle = "";
-    uriList.clear();
-    downloadMetadata = "";
-    fileExtension = "";
-    plainText = "";
-    textHtml = "";
-    htmlBaseUrl = KURL();
-    fileContentFilename = "";
-    if (fileContent)
-        fileContent->clear();
+    m_urlTitle = "";
+    m_url = KURL();
+    m_uriList.clear();
+    m_downloadMetadata = "";
+    m_fileExtension = "";
+    m_plainText = "";
+    m_textHtml = "";
+    m_htmlBaseUrl = KURL();
+    m_fileContentFilename = "";
+    if (m_fileContent)
+        m_fileContent->clear();
 }
 
 bool ChromiumDataObject::hasData() const
 {
-    return !url.isEmpty()
-        || !uriList.isEmpty()
-        || !downloadMetadata.isEmpty()
-        || !fileExtension.isEmpty()
-        || !filenames.isEmpty()
-        || !plainText.isEmpty()
-        || !textHtml.isEmpty()
-        || fileContent;
+    return !m_url.isEmpty()
+        || !m_uriList.isEmpty()
+        || !m_downloadMetadata.isEmpty()
+        || !m_fileExtension.isEmpty()
+        || !m_filenames.isEmpty()
+        || !m_plainText.isEmpty()
+        || !m_textHtml.isEmpty()
+        || m_fileContent;
+}
+
+HashSet<String> ChromiumDataObject::types() const
+{
+    // This is currently broken for pasteboard events, and always has been.
+    HashSet<String> results;
+
+    if (!m_plainText.isEmpty()) {
+        results.add(mimeTypeText);
+        results.add(mimeTypeTextPlain);
+    }
+
+    if (!m_url.isEmpty()) {
+        results.add(mimeTypeURL);
+        results.add(mimeTypeTextURIList);
+    }
+
+    if (!m_textHtml.isEmpty())
+        results.add(mimeTypeTextHTML);
+
+    if (!m_filenames.isEmpty())
+        results.add("Files");
+
+    return results;
+}
+
+String ChromiumDataObject::getData(const String& type, bool& success)
+{
+    if (type == mimeTypeTextPlain) {
+        if (m_clipboardType == Clipboard::CopyAndPaste) {
+            PasteboardPrivate::ClipboardBuffer buffer =
+                Pasteboard::generalPasteboard()->isSelectionMode() ?
+                PasteboardPrivate::SelectionBuffer :
+                PasteboardPrivate::StandardBuffer;
+            String text = ChromiumBridge::clipboardReadPlainText(buffer);
+            success = !text.isEmpty();
+            return text;
+        }
+        success = !m_plainText.isEmpty();
+        return m_plainText;
+    }
+
+    if (type == mimeTypeURL) {
+        ASSERT(m_url.isEmpty() == m_uriList.isEmpty());
+        success = !m_url.isEmpty();
+        return m_url.string();
+    }
+
+    if (type == mimeTypeTextURIList) {
+        ASSERT(m_url.isEmpty() == m_uriList.isEmpty());
+        if (m_uriList.isEmpty()) {
+            success = false;
+            return "";
+        }
+        String uriListString(m_uriList[0]);
+        for (size_t i = 1; i < m_uriList.size(); i++) {
+            uriListString.append(textMIMETypeLineSeparator);
+            uriListString.append(m_uriList[i]);
+        }
+        success = true;
+        return uriListString;
+    }
+
+    if (type == mimeTypeTextHTML) {
+        if (m_clipboardType == Clipboard::CopyAndPaste) {
+            PasteboardPrivate::ClipboardBuffer buffer =
+                Pasteboard::generalPasteboard()->isSelectionMode() ?
+                PasteboardPrivate::SelectionBuffer :
+                PasteboardPrivate::StandardBuffer;
+            String htmlText;
+            KURL sourceURL;
+            ChromiumBridge::clipboardReadHTML(buffer, &htmlText, &sourceURL);
+            success = !htmlText.isEmpty();
+            return htmlText;
+        }
+        success = !m_textHtml.isEmpty();
+        return m_textHtml;
+    }
+
+    if (type == mimeTypeDownloadURL) {
+        success = !m_downloadMetadata.isEmpty();
+        return m_downloadMetadata;
+    }
+
+    success = false;
+    return String();
+}
+
+bool ChromiumDataObject::setData(const String& type, const String& data)
+{
+    if (type == mimeTypeTextPlain) {
+        m_plainText = data;
+        return true;
+    }
+
+    if (type == mimeTypeURL || type == mimeTypeTextURIList) {
+        m_url = KURL();
+        // Line separator is \r\n per RFC 2483 - however, for compatibility reasons
+        // we also allow just \n here.
+        data.split('\n', m_uriList);
+        // Strip white space on all lines, including trailing \r from above split.
+        // If this leaves a line empty, remove it completely.
+        //
+        // Also, copy the first valid URL into the 'url' member as well.
+        // In case no entry is a valid URL (i.e., remarks only), then we leave 'url' empty.
+        // I.e., in that case subsequent calls to getData("URL") will get an empty string.
+        // This is in line with the HTML5 spec (see "The DragEvent and DataTransfer interfaces").
+        for (size_t i = 0; i < m_uriList.size(); /**/) {
+            String& line = m_uriList[i];
+            line = line.stripWhiteSpace();
+            if (line.isEmpty()) {
+                m_uriList.remove(i);
+                continue;
+            }
+            ++i;
+            // Only copy the first valid URL.
+            if (m_url.isValid())
+                continue;
+            if (line[0] == '#')
+                continue;
+            KURL url = KURL(ParsedURLString, line);
+            if (url.isValid())
+                m_url = url;
+        }
+        ASSERT(m_url.isEmpty() == m_uriList.isEmpty());
+        return true;
+    }
+
+    if (type == mimeTypeTextHTML) {
+        m_textHtml = data;
+        m_htmlBaseUrl = KURL();
+        return true;
+    }
+
+    if (type == mimeTypeDownloadURL) {
+        m_downloadMetadata = data;
+        return true;
+    }
+
+    return false;
+}
+
+ChromiumDataObject::ChromiumDataObject(Clipboard::ClipboardType clipboardType)
+    : m_clipboardType(clipboardType)
+{
 }
 
 ChromiumDataObject::ChromiumDataObject(const ChromiumDataObject& other)
     : RefCounted<ChromiumDataObject>()
-    , urlTitle(other.urlTitle)
-    , downloadMetadata(other.downloadMetadata)
-    , fileExtension(other.fileExtension)
-    , filenames(other.filenames)
-    , plainText(other.plainText)
-    , textHtml(other.textHtml)
-    , htmlBaseUrl(other.htmlBaseUrl)
-    , fileContentFilename(other.fileContentFilename)
-    , url(other.url)
-    , uriList(other.uriList)
+    , m_clipboardType(other.m_clipboardType)
+    , m_urlTitle(other.m_urlTitle)
+    , m_downloadMetadata(other.m_downloadMetadata)
+    , m_fileExtension(other.m_fileExtension)
+    , m_filenames(other.m_filenames)
+    , m_plainText(other.m_plainText)
+    , m_textHtml(other.m_textHtml)
+    , m_htmlBaseUrl(other.m_htmlBaseUrl)
+    , m_fileContentFilename(other.m_fileContentFilename)
+    , m_url(other.m_url)
+    , m_uriList(other.m_uriList)
 {
-    if (other.fileContent.get())
-        fileContent = other.fileContent->copy();
+    if (other.m_fileContent.get())
+        m_fileContent = other.m_fileContent->copy();
 }
 
 } // namespace WebCore

WebCore/platform/chromium/ChromiumDataObject.h
@@ -31,11 +31,14 @@
 #ifndef ChromiumDataObject_h
 #define ChromiumDataObject_h
 
+#include "Clipboard.h"
 #include "KURL.h"
 #include "PlatformString.h"
 #include "SharedBuffer.h"
+#include <wtf/HashSet.h>
 #include <wtf/RefPtr.h>
 #include <wtf/Vector.h>
+#include <wtf/text/StringHash.h>
 
 namespace WebCore {
 
@@ -44,9 +47,9 @@ namespace WebCore {
     // of and is not specific to a platform.
     class ChromiumDataObject : public RefCounted<ChromiumDataObject> {
     public:
-        static PassRefPtr<ChromiumDataObject> create()
+        static PassRefPtr<ChromiumDataObject> create(Clipboard::ClipboardType clipboardType)
         {
-            return adoptRef(new ChromiumDataObject);
+            return adoptRef(new ChromiumDataObject(clipboardType));
         }
 
         PassRefPtr<ChromiumDataObject> copy() const
@@ -54,60 +57,61 @@ namespace WebCore {
             return adoptRef(new ChromiumDataObject(*this));
         }
 
-        void clear();
+        void clearData(const String& type);
+        void clearAll();
         void clearAllExceptFiles();
+
         bool hasData() const;
 
-        void clearURL()
-        {
-            url = KURL();
-            uriList.clear();
-            urlTitle = "";
-        }
+        HashSet<String> types() const;
+        String getData(const String& type, bool& success);
+        bool setData(const String& type, const String& data);
 
-        bool hasValidURL() const
-        {
-            return url.isValid();
-        }
+        // Special handlers for URL/HTML metadata.
+        String urlTitle() const { return m_urlTitle; }
+        void setUrlTitle(const String& urlTitle) { m_urlTitle = urlTitle; }
+        KURL htmlBaseUrl() const { return m_htmlBaseUrl; }
+        void setHtmlBaseUrl(const KURL& url) { m_htmlBaseUrl = url; }
 
-        KURL getURL() const
-        {
-            return url;
-        }
+        // Used to handle files being dragged in.
+        bool containsFilenames() const { return !m_filenames.isEmpty(); }
+        Vector<String> filenames() const { return m_filenames; }
+        void setFilenames(const Vector<String>& filenames) { m_filenames = filenames; }
 
-        void setURL(const KURL& newURL)
-        {
-            url = newURL;
-            uriList.clear();
-            if (newURL.isEmpty())
-                return;
-            uriList.append(newURL.string());
-        }
-
-        String urlTitle;
-
-        String downloadMetadata;
-
-        String fileExtension;
-        Vector<String> filenames;
-
-        String plainText;
-
-        String textHtml;
-        KURL htmlBaseUrl;
-
-        String fileContentFilename;
-        RefPtr<SharedBuffer> fileContent;
+        // Used to handle files (images) being dragged out.
+        String fileExtension() const { return m_fileExtension; }
+        void setFileExtension(const String& fileExtension) { m_fileExtension = fileExtension; }
+        String fileContentFilename() const { return m_fileContentFilename; }
+        void setFileContentFilename(const String& fileContentFilename) { m_fileContentFilename = fileContentFilename; }
+        PassRefPtr<SharedBuffer> fileContent() const { return m_fileContent; }
+        void setFileContent(PassRefPtr<SharedBuffer> fileContent) { m_fileContent = fileContent; }
 
     private:
-        // URL and uri-list are linked, so they should not be accessed individually.
-        KURL url;
-        Vector<String> uriList;
-
-        ChromiumDataObject() {}
+        ChromiumDataObject(Clipboard::ClipboardType);
         ChromiumDataObject(const ChromiumDataObject&);
 
-        friend class ClipboardChromium;
+        Clipboard::ClipboardType m_clipboardType;
+
+        String m_urlTitle;
+
+        String m_downloadMetadata;
+
+        String m_fileExtension;
+        Vector<String> m_filenames;
+
+        String m_plainText;
+
+        String m_textHtml;
+        KURL m_htmlBaseUrl;
+
+        String m_fileContentFilename;
+        RefPtr<SharedBuffer> m_fileContent;
+
+        // These two are linked. Setting m_url will set m_uriList to the same
+        // string value; setting m_uriList will cause its contents to be parsed
+        // according to RFC 2483 and the first URL found will be set in m_url.
+        KURL m_url;
+        Vector<String> m_uriList;
     };
 
 } // namespace WebCore

commit 2b1e43558da8d13f528b7f830c7d41e84d1fbf14

2010-10-10  Daniel Cheng  <dcheng@chromium.org>
        Reviewed by Adam Barth.

        [chromium] Clipboard no longer accepts 'text' as a synonym for 'text/plain'
        https://bugs.webkit.org/show_bug.cgi?id=47419

        This was a regression introduced in r68807. Updated the layout test to
        check for this case.

        * editing/pasteboard/dataTransfer-setData-getData-expected.txt:
        * editing/pasteboard/script-tests/dataTransfer-setData-getData.js:
        (runTest):
2010-10-10  Daniel Cheng  <dcheng@chromium.org>

        Reviewed by Adam Barth.

        [chromium] Clipboard no longer accepts 'text' as a synonym for 'text/plain'
        https://bugs.webkit.org/show_bug.cgi?id=47419

        This was a regression introduced in r68807. mimeTypeText had the incorrect
        value.

        Test: editing/pasteboard/dataTransfer-setData-getData.html

        * platform/chromium/ClipboardMimeTypes.cpp:

Canonical link: https://commits.webkit.org/60070@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@69470 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/platform/chromium/ClipboardMimeTypes.cpp
@@ -33,7 +33,7 @@
 
 namespace WebCore {
 
-const char mimeTypeText[] = "text/plain";
+const char mimeTypeText[] = "text";
 const char mimeTypeTextPlain[] = "text/plain";
 const char mimeTypeTextPlainEtc[] = "text/plain;";
 const char mimeTypeTextHTML[] = "text/html";

LayoutTests/editing/pasteboard/dataTransfer-setData-getData-expected.txt
@@ -36,6 +36,15 @@ FAIL getDataResult should be  (of type string). Was undefined (of type undefined
 --- Test set/get 'text/plain':
 PASS getDataResultType is "string"
 PASS getDataResult is "Lorem ipsum dolor sit amet."
+--- Test set 'text/plain', get 'text':
+PASS getDataResultType is "string"
+PASS getDataResult is "Lorem ipsum dolor sit amet."
+--- Test set 'text', get 'text/plain':
+PASS getDataResultType is "string"
+PASS getDataResult is "Lorem ipsum dolor sit amet."
+--- Test set/get 'text':
+PASS getDataResultType is "string"
+PASS getDataResult is "Lorem ipsum dolor sit amet."
 PASS successfullyParsed is true
 
 TEST COMPLETE

LayoutTests/editing/pasteboard/script-tests/dataTransfer-setData-getData.js
@@ -117,6 +117,18 @@ function runTest()
     debug("--- Test set/get 'text/plain':");
     test("text/plain", "Lorem ipsum dolor sit amet.", 
          "text/plain", "Lorem ipsum dolor sit amet.");
+
+    debug("--- Test set 'text/plain', get 'text':");
+    test("text/plain", "Lorem ipsum dolor sit amet.",
+         "text", "Lorem ipsum dolor sit amet.");
+
+    debug("--- Test set 'text', get 'text/plain':");
+    test("text", "Lorem ipsum dolor sit amet.",
+         "text/plain", "Lorem ipsum dolor sit amet.");
+
+    debug("--- Test set/get 'text':");
+    test("text", "Lorem ipsum dolor sit amet.",
+         "text", "Lorem ipsum dolor sit amet.");
 }
 
 if (window.eventSender) {