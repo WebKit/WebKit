f072db6205fe55a6f866d81375ce6be5087df91c
Steve Block <steveblock@google.com>
2010-10-14 14:14:19 +0000

    2010-10-14  Steve Block  <steveblock@google.com>
            Reviewed by Jeremy Orlow.
    
            DeviceOrientation crash when page is in page cache and is navigated while listener is registered
            https://bugs.webkit.org/show_bug.cgi?id=47257
    
            This crash is avoided now that pages using DeviceOrientation are not put in
            the page cache. See https://bugs.webkit.org/show_bug.cgi?id=47408.
    
            This test serves to document the fact that the underlying problem needs to
            be addressed before the page cache is enabled for DeviceOrientation.
    
            * fast/dom/DeviceOrientation/event-after-navigation-expected.txt: Added.
            * fast/dom/DeviceOrientation/event-after-navigation.html: Added.
            * fast/dom/DeviceOrientation/resources/event-after-navigation-new.html: Added.
            * fast/dom/DeviceOrientation/script-tests/event-after-navigation.js: Added.
    
    
    Canonical link: https://commits.webkit.org/60351@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@69764 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/dom/DeviceOrientation/script-tests/event-after-navigation.js
@@ -0,0 +1,12 @@
+description('Tests for a crash where an event is fired after the page has been navigated away when the original page is in the page cache.');
+
+if (window.layoutTestController)
+    layoutTestController.overridePreference('WebKitUsesPageCachePreferenceKey', 1);
+else
+    debug('This test can not be run without the LayoutTestController');
+
+window.addEventListener('deviceorientation', function() { } );
+window.location = "resources/event-after-navigation-new.html";
+
+window.jsTestIsAsync = true;
+window.successfullyParsed = true;
