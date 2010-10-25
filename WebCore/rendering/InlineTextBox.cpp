9fea22545c167edb4b0d40a5f8e60a335c3f362d
David Hyatt <hyatt@apple.com>
2010-10-25 16:05:03 +0000

    https://bugs.webkit.org/show_bug.cgi?id=48246
    Reviewed by Sam Weinig.
    
    Make selectionRect() on InlineTextBox give the right value for a vertical line.  Selection is still too broken
    to test anything though.
    
    * rendering/InlineTextBox.cpp:
    (WebCore::InlineTextBox::selectionRect):
    
    
    
    Canonical link: https://commits.webkit.org/60992@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@70456 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/InlineTextBox.cpp
+++ WebCore/rendering/InlineTextBox.cpp
@@ -154,12 +154,19 @@ IntRect InlineTextBox::selectionRect(int tx, int ty, int startPos, int endPos)
     }
 
     IntRect r = enclosingIntRect(f.selectionRectForText(TextRun(characters, len, textObj->allowTabs(), textPos(), m_toAdd, !isLeftToRightDirection(), m_dirOverride),
-                                                        IntPoint(tx + m_x, ty + selTop), selHeight, sPos, ePos));
-    if (r.x() > tx + m_x + m_logicalWidth)
-        r.setWidth(0);
-    else if (r.right() - 1 > tx + m_x + m_logicalWidth)
-        r.setWidth(tx + m_x + m_logicalWidth - r.x());
-    return r;
+                                                        IntPoint(), selHeight, sPos, ePos));
+                                                        
+    int logicalWidth = r.width();
+    if (r.x() > m_logicalWidth)
+        logicalWidth  = 0;
+    else if (r.right() > m_logicalWidth)
+        logicalWidth = m_logicalWidth - r.x();
+    
+    IntPoint topPoint = m_isVertical ? IntPoint(tx + selTop, ty + m_y + r.x()) : IntPoint(tx + m_x + r.x(), ty + selTop);
+    int width = m_isVertical ? selHeight : logicalWidth;
+    int height = m_isVertical ? logicalWidth : selHeight;
+    
+    return IntRect(topPoint, IntSize(width, height));
 }
 
 void InlineTextBox::deleteLine(RenderArena* arena)
