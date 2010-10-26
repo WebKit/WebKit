5e424798b73a470f49cbebafcbb5d1a2e1c727ed
Adam Roben <aroben@apple.com>
2010-10-26 20:56:40 +0000

    Clear up confusion between "3D rendering" and "accelerated compositing"
    WebKit has no software-based 3D rendering implementation, so 3D
    rendering can only be enabled when accelerated compositing is. But DRT
    was falsely reporting that 3D rendering was available on machines that
    don't support accelerated compositing, leading to much confusion.
    
    Reviewed by Darin Adler.
    
    Fixes <http://webkit.org/b/48370> REGRESSION (r70540): Many
    transforms/3d tests are failing on the XP bots
    
    * DumpRenderTree/win/DumpRenderTree.cpp:
    (main): When ENABLE(3D_RENDERING) is turned on, only report that 3D
    rendering is available when acclerated compositing is also available.
    
    * Scripts/old-run-webkit-tests: Skip tests that use the
    -webkit-transform-3d media query when 3D rendering is disabled,
    rather than when accelerated compositing is disabled.
    
    Canonical link: https://commits.webkit.org/61104@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@70572 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/DumpRenderTree/win/DumpRenderTree.cpp
+++ WebKitTools/DumpRenderTree/win/DumpRenderTree.cpp
@@ -1307,11 +1307,14 @@ int main(int argc, char* argv[])
     if (printSupportedFeatures) {
         BOOL acceleratedCompositingAvailable;
         standardPreferences->acceleratedCompositingEnabled(&acceleratedCompositingAvailable);
-        BOOL threeDRenderingAvailable = 
+
 #if ENABLE(3D_RENDERING)
-            true;
+        // In theory, we could have a software-based 3D rendering implementation that we use when
+        // hardware-acceleration is not available. But we don't have any such software
+        // implementation, so 3D rendering is only available when hardware-acceleration is.
+        BOOL threeDRenderingAvailable = acceleratedCompositingAvailable;
 #else
-            false;
+        BOOL threeDRenderingAvailable = FALSE;
 #endif
 
         printf("SupportedFeatures:%s %s\n", acceleratedCompositingAvailable ? "AcceleratedCompositing" : "", threeDRenderingAvailable ? "3DRendering" : "");
