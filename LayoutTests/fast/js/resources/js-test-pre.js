f2d9c0be1cc957ce3e19a532cbc12369fd8c0093
Commit Queue <commit-queue@webkit.org>
2010-10-28 19:05:41 +0000

    2010-10-28  Adrienne Walker  <enne@google.com>
            Reviewed by Kenneth Russell.
    
            Add shouldBeDefined function to js-test-pre.js and update
            context-attributes-etc test to use it.
            https://bugs.webkit.org/show_bug.cgi?id=47853
    
            * fast/canvas/webgl/context-attributes-alpha-depth-stencil-antialias-expected.txt:
            * fast/canvas/webgl/context-attributes-alpha-depth-stencil-antialias.html:
            * fast/dom/Window/window-property-descriptors-expected.txt:
            * fast/js/resources/js-test-pre.js:
            (shouldBeDefined):
    
    Canonical link: https://commits.webkit.org/61308@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@70794 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/js/resources/js-test-pre.js
+++ LayoutTests/fast/js/resources/js-test-pre.js
@@ -219,6 +219,24 @@ function shouldBeUndefined(_a)
     testFailed(_a + " should be undefined. Was " + _av);
 }
 
+function shouldBeDefined(_a)
+{
+  var exception;
+  var _av;
+  try {
+     _av = eval(_a);
+  } catch (e) {
+     exception = e;
+  }
+
+  if (exception)
+    testFailed(_a + " should be defined. Threw exception " + exception);
+  else if (_av !== undefined)
+    testPassed(_a + " is defined.");
+  else
+    testFailed(_a + " should be defined. Was " + _av);
+}
+
 function shouldBeGreaterThanOrEqual(_a, _b) {
     if (typeof _a != "string" || typeof _b != "string")
         debug("WARN: shouldBeGreaterThanOrEqual expects string arguments");
