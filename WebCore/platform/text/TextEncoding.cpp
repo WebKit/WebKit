35620803cf9f06a4e51c2145dcde04a256b411b9
Commit Queue <commit-queue@webkit.org>
2010-10-29 22:15:03 +0000

    2010-10-29  Carlos Garcia Campos  <cgarcia@igalia.com>
            Reviewed by Andreas Kling.
    
            [GTK] Crash in TextEncoding.cpp when using glib unicode
            https://bugs.webkit.org/show_bug.cgi?id=48521
    
            When the conversion to UTF-8 fails, try to encode the original string
            without normalizing. Fixes tests cases:
             fast/url/anchor.html
             fast/url/query.html
             fast/url/standard-url.html
    
            * platform/text/TextEncoding.cpp:
            (WebCore::TextEncoding::encode):
    
    Canonical link: https://commits.webkit.org/61439@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@70935 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/text/TextEncoding.cpp
+++ WebCore/platform/text/TextEncoding.cpp
@@ -120,6 +120,10 @@ CString TextEncoding::encode(const UChar* characters, size_t length, Unencodable
 #elif USE(GLIB_UNICODE)
     GOwnPtr<char> UTF8Source;
     UTF8Source.set(g_utf16_to_utf8(characters, length, 0, 0, 0));
+    if (!UTF8Source) {
+        // If conversion to UTF-8 failed, try with the string without normalization
+        return newTextCodec(*this)->encode(characters, length, handling);
+    }
 
     GOwnPtr<char> UTF8Normalized;
     UTF8Normalized.set(g_utf8_normalize(UTF8Source.get(), -1, G_NORMALIZE_NFC));
