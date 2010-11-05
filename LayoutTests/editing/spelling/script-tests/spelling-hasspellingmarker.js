4253f367baa8225cfd7cc954bace827e7d8f5b21
Hajime Morrita <morrita@google.com>
2010-11-05 04:58:51 +0000

    2010-11-04  MORITA Hajime  <morrita@google.com>
            Reviewed by Kent Tamura.
    
            Expectation of /editing/spelling/spelling-textarea.html is confusing
            https://bugs.webkit.org/show_bug.cgi?id=48818
    
            - Turned spelling-contenteditable.html and spelling-textarea.html into
              single script-test named spelling-hasspellingmarker.html,
              which uses shouldBe() family for the assertion. The original tests
              misused testPassed() for printing.
            - Added a test for <input>, which possibly goes different path from
              editing <textarea> and @contentEditable.
            - Added a "false" case test to catch the error that results true for anything.
    
            * editing/spelling/script-tests/spelling-hasspellingmarker.js: Added.
            * editing/spelling/spelling-contenteditable-expected.txt: Removed.
            * editing/spelling/spelling-contenteditable.html: Removed.
            * editing/spelling/spelling-hasspellingmarker-expected.txt: Added.
            * editing/spelling/spelling-hasspellingmarker.html: Added.
            * editing/spelling/spelling-textarea-expected.txt: Removed.
            * editing/spelling/spelling-textarea.html: Removed.
            * platform/chromium/test_expectations.txt:
            * platform/gtk/Skipped:
            * platform/qt/Skipped:
    
    Canonical link: https://commits.webkit.org/61879@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@71392 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/editing/spelling/script-tests/spelling-hasspellingmarker.js
@@ -0,0 +1,23 @@
+description('This tests Layouttestcontroller.hasSpellingMarker works for differnt type of elements.');
+
+var parent = document.createElement("div");
+document.body.appendChild(parent);
+
+function hasMarked(markup)
+{
+    parent.innerHTML = markup;
+    document.getElementById('test').focus();
+    document.execCommand("InsertText", false, 'z');
+    document.execCommand("InsertText", false, 'z');
+    document.execCommand("InsertText", false, ' ');
+
+    return layoutTestController.hasSpellingMarker(0, 2);
+}
+
+shouldBeTrue("hasMarked(\"<textarea id='test' cols='80' rows='10'></textarea>\");");
+shouldBeTrue("hasMarked(\"<input id='test' type='text'>\");");
+shouldBeTrue("hasMarked(\"<div id='test' contenteditable='true'></div>\");");
+shouldBeFalse("hasMarked(\"<div id='test' contentEditable='true' spellcheck='false'></div>\");");
+parent.innerHTML = "";
+
+var successfullyParsed = true;
