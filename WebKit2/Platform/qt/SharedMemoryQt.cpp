d433453949537fc870ee350b6483f910c2c98074
Balazs Kelemen <kbalazs@webkit.org>
2010-11-07 01:23:39 +0000

    2010-11-06  Balazs Kelemen  <kbalazs@webkit.org>
            Reviewed by Andreas Kling.
    
            [Qt][WK2] WebKitTestRunner is dead
            https://bugs.webkit.org/show_bug.cgi?id=49129
    
            * Platform/qt/SharedMemoryQt.cpp:
            (WebKit::SharedMemory::~SharedMemory):
            Avoid double deletion when deleteLater has
            already been called.
    
    Canonical link: https://commits.webkit.org/61958@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@71471 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit2/Platform/qt/SharedMemoryQt.cpp
+++ WebKit2/Platform/qt/SharedMemoryQt.cpp
@@ -147,7 +147,11 @@ SharedMemory::~SharedMemory()
 {
     // m_impl must be non-null and it must point to a valid QSharedMemory object.
     ASSERT(qobject_cast<QSharedMemory*>(m_impl));
-    delete m_impl;
+
+    // Avoid double deletion when deleteLater has already been called through the signal-slot connection.
+    if (QCoreApplication::instance()->disconnect(m_impl))
+        delete m_impl;
+
     CrashHandler::instance()->didDelete(m_impl);
 }
 
