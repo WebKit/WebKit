94ae59c6e5e276387cebeb569bec95773f61861c
Martin Robinson <mrobinson@webkit.org>
2010-11-08 17:30:41 +0000

    2010-11-08  Martin Robinson  <mrobinson@igalia.com>
            Reviewed by Xan Lopez.
    
            >=webkitgtk-1.2.5: parallel build fails with libtool: link: cannot find the library `libwebkit-1.0.la' or unhandled argument `libwebkit-1.0.la'
            https://bugs.webkit.org/show_bug.cgi?id=49128
    
            r59042 introduced a C++-style comment in Platform.h, which is often
            included in C source files. Change it to a C-style comment.
    
            * wtf/Platform.h: Fix the C++-style comment.
    
    Canonical link: https://commits.webkit.org/62022@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@71535 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/wtf/Platform.h
+++ JavaScriptCore/wtf/Platform.h
@@ -814,7 +814,7 @@
 /* ENABLE macro defaults */
 
 #if PLATFORM(QT)
-// We must not customize the global operator new and delete for the Qt port.
+/* We must not customize the global operator new and delete for the Qt port. */
 #define ENABLE_GLOBAL_FASTMALLOC_NEW 0
 #endif
 
