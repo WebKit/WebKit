edac8687407c06590c0acf8ecb88b47a866d0ca0
Adam Roben <aroben@apple.com>
2010-11-08 14:18:37 +0000

    Switch back to using kCGImageAlphaPremultipliedFirst when generating pixel dumps on Windows
    I changed this behavior in r71418 thinking that it was required for
    getting plugins to show up in pixel dumps. But it doesn't seem to be
    necessary, and was making it impossible to compare new Windows pixel
    dumps with existing Windows or Mac pixel dumps (because ImageDiff won't
    compare an image with alpha to an image without alpha).
    
    Fixes <http://webkit.org/b/49172> REGRESION (r71418): Can't compare
    new Windows pixel results to existing Windows or Mac results
    
    Reviewed by Antti Koivisto.
    
    * DumpRenderTree/win/PixelDumpSupportWin.cpp:
    (createBitmapContextFromWebView): Replaced kCGImageAlphaNoneSkipFirst
    with kCGImageAlphaPremultipliedFirst.
    
    Canonical link: https://commits.webkit.org/62003@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@71516 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/DumpRenderTree/win/PixelDumpSupportWin.cpp
+++ WebKitTools/DumpRenderTree/win/PixelDumpSupportWin.cpp
@@ -73,7 +73,7 @@ PassRefPtr<BitmapContext> createBitmapContextFromWebView(bool onscreen, bool inc
 #if PLATFORM(CG)
     RetainPtr<CGColorSpaceRef> colorSpace(AdoptCF, CGColorSpaceCreateDeviceRGB());
     CGContextRef context = CGBitmapContextCreate(info.bmBits, info.bmWidth, info.bmHeight, 8,
-                                                info.bmWidthBytes, colorSpace.get(), kCGBitmapByteOrder32Host | kCGImageAlphaNoneSkipFirst);
+                                                info.bmWidthBytes, colorSpace.get(), kCGBitmapByteOrder32Host | kCGImageAlphaPremultipliedFirst);
 #elif PLATFORM(CAIRO) 
     cairo_surface_t* image = cairo_image_surface_create_for_data((unsigned char*)info.bmBits, CAIRO_FORMAT_ARGB32, 
                                                       info.bmWidth, info.bmHeight, info.bmWidthBytes); 
