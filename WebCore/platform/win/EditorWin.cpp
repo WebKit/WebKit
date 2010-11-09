ffb4510bb3dcc662260f41a8c481072093e6842f
Patrick Gansterer <paroga@webkit.org>
2010-11-09 00:23:47 +0000

    2010-11-08  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Adam Roben.
    
            Merge EditorWinCE.cpp into EditorWin.cpp
            https://bugs.webkit.org/show_bug.cgi?id=49095
    
            * platform/win/EditorWin.cpp:
            (WebCore::Editor::newGeneralClipboard):
            * platform/wince/EditorWinCE.cpp: Removed.
    
    Canonical link: https://commits.webkit.org/62071@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@71584 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/win/EditorWin.cpp
+++ WebCore/platform/win/EditorWin.cpp
@@ -42,8 +42,10 @@ namespace WebCore {
 PassRefPtr<Clipboard> Editor::newGeneralClipboard(ClipboardAccessPolicy policy, Frame* frame)
 {
     COMPtr<IDataObject> clipboardData;
+#if !OS(WINCE)
     if (!SUCCEEDED(OleGetClipboard(&clipboardData)))
         clipboardData = 0;
+#endif
 
     return ClipboardWin::create(Clipboard::CopyAndPaste, clipboardData.get(), policy, frame);
 }
