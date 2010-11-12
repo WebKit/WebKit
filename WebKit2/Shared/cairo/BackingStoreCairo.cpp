cb45b4c7dcfa3e565256784bc4b6c5c5bb719329
Brent Fulgham <bfulgham@webkit.org>
2010-11-12 00:19:47 +0000

    Windows [Cairo] build fix. Unreviewed.
    Provide stubs for two BackingStore methods.
    
    * Shared/cairo/BackingStoreCairo.cpp:
    (WebKit::BackingStore::createFlippedGraphicsContext):
    (WebKit::BackingStore::paint):
    
    
    
    Canonical link: https://commits.webkit.org/62339@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@71863 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit2/Shared/cairo/BackingStoreCairo.cpp
+++ WebKit2/Shared/cairo/BackingStoreCairo.cpp
@@ -38,7 +38,13 @@ PassOwnPtr<GraphicsContext> BackingStore::createGraphicsContext()
     return 0;
 }
 
-void BackingStore::paint(GraphicsContext*, const IntRect&)
+PassOwnPtr<GraphicsContext> BackingStore::createFlippedGraphicsContext()
+{
+    notImplemented();
+    return 0;
+}
+
+void BackingStore::paint(GraphicsContext&, const IntPoint&, const IntRect&)
 {
     notImplemented();
 }
