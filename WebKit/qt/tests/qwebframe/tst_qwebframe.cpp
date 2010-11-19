61b06504c3a906053f81ede6166d10c292c03979
Gavin Barraclough <barraclough@apple.com>
2010-11-19 07:02:00 +0000

    Build fix - update error messages folllowing r72360.
    * tests/qwebframe/tst_qwebframe.cpp:
    
    
    
    Canonical link: https://commits.webkit.org/62830@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@72368 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit/qt/tests/qwebframe/tst_qwebframe.cpp
+++ WebKit/qt/tests/qwebframe/tst_qwebframe.cpp
@@ -1566,26 +1566,26 @@ void tst_QWebFrame::connectAndDisconnect()
         QString type;
         QString ret = evalJS("(function() { }).connect()", type);
         QCOMPARE(type, sError);
-        QCOMPARE(ret, QLatin1String("TypeError: 'undefined' is not a function (evaluating '(function() { }).connect()')"));
+        QCOMPARE(ret, QLatin1String("TypeError: 'undefined' is not a function"));
     }
     {
         QString type;
         QString ret = evalJS("var o = { }; o.connect = Function.prototype.connect;  o.connect()", type);
         QCOMPARE(type, sError);
-        QCOMPARE(ret, QLatin1String("TypeError: 'undefined' is not a function (evaluating 'o.connect()')"));
+        QCOMPARE(ret, QLatin1String("TypeError: 'undefined' is not a function"));
     }
 
     {
         QString type;
         QString ret = evalJS("(function() { }).connect(123)", type);
         QCOMPARE(type, sError);
-        QCOMPARE(ret, QLatin1String("TypeError: 'undefined' is not a function (evaluating '(function() { }).connect(123)')"));
+        QCOMPARE(ret, QLatin1String("TypeError: 'undefined' is not a function"));
     }
     {
         QString type;
         QString ret = evalJS("var o = { }; o.connect = Function.prototype.connect;  o.connect(123)", type);
         QCOMPARE(type, sError);
-        QCOMPARE(ret, QLatin1String("TypeError: 'undefined' is not a function (evaluating 'o.connect(123)')"));
+        QCOMPARE(ret, QLatin1String("TypeError: 'undefined' is not a function"));
     }
 
     {
