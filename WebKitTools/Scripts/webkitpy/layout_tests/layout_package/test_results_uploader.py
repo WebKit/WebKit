6ed62373d19d7fc2b6c2ae9fa7327aeda1a3cac6
Ojan Vafai <ojan@chromium.org>
2010-11-23 22:59:58 +0000

    2010-11-22  Ojan Vafai  <ojan@chromium.org>
            Reviewed by Tony Chang.
    
            speculative fix for upload errors: stop using mechanize to upload to test-results.appspot.com
            https://bugs.webkit.org/show_bug.cgi?id=49944
    
            * Scripts/webkitpy/layout_tests/layout_package/test_results_uploader.py:
    
    Canonical link: https://commits.webkit.org/63094@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@72633 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/webkitpy/layout_tests/layout_package/test_results_uploader.py
+++ WebKitTools/Scripts/webkitpy/layout_tests/layout_package/test_results_uploader.py
@@ -27,45 +27,81 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import with_statement
+
+import codecs
 import mimetypes
 import socket
+import urllib2
 
 from webkitpy.common.net.networktransaction import NetworkTransaction
-from webkitpy.thirdparty.autoinstalled.mechanize import Browser
-
 
 def get_mime_type(filename):
-    return mimetypes.guess_type(filename)[0] or "text/plain"
+    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+
+
+def _encode_multipart_form_data(fields, files):
+    """Encode form fields for multipart/form-data.
+
+    Args:
+      fields: A sequence of (name, value) elements for regular form fields.
+      files: A sequence of (name, filename, value) elements for data to be
+             uploaded as files.
+    Returns:
+      (content_type, body) ready for httplib.HTTP instance.
+
+    Source:
+      http://code.google.com/p/rietveld/source/browse/trunk/upload.py
+    """
+    BOUNDARY = '-M-A-G-I-C---B-O-U-N-D-A-R-Y-'
+    CRLF = '\r\n'
+    lines = []
+
+    for key, value in fields:
+        lines.append('--' + BOUNDARY)
+        lines.append('Content-Disposition: form-data; name="%s"' % key)
+        lines.append('')
+        if isinstance(value, unicode):
+            value = value.encode('utf-8')
+        lines.append(value)
+
+    for key, filename, value in files:
+        lines.append('--' + BOUNDARY)
+        lines.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
+        lines.append('Content-Type: %s' % get_mime_type(filename))
+        lines.append('')
+        if isinstance(value, unicode):
+            value = value.encode('utf-8')
+        lines.append(value)
+
+    lines.append('--' + BOUNDARY + '--')
+    lines.append('')
+    body = CRLF.join(lines)
+    content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
+    return content_type, body
 
 
 class TestResultsUploader:
     def __init__(self, host):
         self._host = host
-        self._browser = Browser()
 
     def _upload_files(self, attrs, file_objs):
-        self._browser.open("http://%s/testfile/uploadform" % self._host)
-        self._browser.select_form("test_result_upload")
-        for (name, data) in attrs:
-            self._browser[name] = str(data)
-
-        for (filename, handle) in file_objs:
-            self._browser.add_file(handle, get_mime_type(filename), filename,
-                "file")
-
-        self._browser.submit()
+        url = "http://%s/testfile/upload" % self._host
+        content_type, data = _encode_multipart_form_data(attrs, file_objs)
+        headers = {"Content-Type": content_type}
+        request = urllib2.Request(url, data, headers)
+        urllib2.urlopen(request, timeout=60)
 
     def upload(self, params, files, timeout_seconds):
-        orig_timeout = socket.getdefaulttimeout()
         file_objs = []
-        try:
-            file_objs = [(filename, open(path, "rb")) for (filename, path)
-                in files]
+        for filename, path in files:
+            with codecs.open(path, "rb") as file:
+                file_objs.append(('file', filename, file.read()))
 
+        orig_timeout = socket.getdefaulttimeout()
+        try:
             socket.setdefaulttimeout(timeout_seconds)
             NetworkTransaction(timeout_seconds=timeout_seconds).run(
                 lambda: self._upload_files(params, file_objs))
         finally:
             socket.setdefaulttimeout(orig_timeout)
-            for (filename, handle) in file_objs:
-                handle.close()
