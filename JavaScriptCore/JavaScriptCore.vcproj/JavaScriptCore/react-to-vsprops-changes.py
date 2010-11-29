5d32b4e5388b5df3773027f46d02bef54ceef100
Adam Roben <aroben@apple.com>
2010-11-29 21:08:31 +0000

    Touch wtf/Platform.h whenever any .vsprops file changes
    This will cause all files to be recompiled, which will make changes to
    preprocessor macros (e.g., ENABLE_*) actually take effect.
    
    Fixes <http://webkit.org/b/50167> Windows build fails when ENABLE_*
    macros are changed (because not enough files are rebuilt)
    
    Reviewed by John Sullivan.
    
    * JavaScriptCore.vcproj/JavaScriptCore/react-to-vsprops-changes.py:
    (main): Touch wtf/Platform.h if it's older than any .vsprops file. Also
    added some comments and logging to make it clearer what the script is
    doing and why.
    
    Canonical link: https://commits.webkit.org/63281@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@72822 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/JavaScriptCore.vcproj/JavaScriptCore/react-to-vsprops-changes.py
+++ JavaScriptCore/JavaScriptCore.vcproj/JavaScriptCore/react-to-vsprops-changes.py
@@ -20,11 +20,21 @@ def main():
     vsprops_directory = os.path.join(top_level_directory, 'WebKitLibraries', 'win', 'tools', 'vsprops')
     newest_vsprops_time = max(file_modification_times(vsprops_directory))
 
+    # Delete any manifest-related files because Visual Studio isn't smart
+    # enough to figure out that it might need to rebuild them.
     obj_directory = os.path.join(os.environ['WEBKITOUTPUTDIR'], 'obj')
     for manifest_file in glob.iglob(os.path.join(obj_directory, '*', '*', '*.manifest*')):
         manifest_time = os.path.getmtime(manifest_file)
         if manifest_time < newest_vsprops_time:
+            print 'Deleting %s' % manifest_file
             os.remove(manifest_file)
+
+    # Touch wtf/Platform.h so all files will be recompiled. This is necessary
+    # to pick up changes to preprocessor macros (e.g., ENABLE_*).
+    wtf_platform_h = os.path.join(top_level_directory, 'JavaScriptCore', 'wtf', 'Platform.h')
+    if os.path.getmtime(wtf_platform_h) < newest_vsprops_time:
+        print 'Touching wtf/Platform.h'
+        os.utime(wtf_platform_h, None)
 
 
 if __name__ == '__main__':
