571966b9a2b749c2e167f22cc17d84d0000137bc
Adam Roben <aroben@apple.com>
2010-11-29 21:09:00 +0000

    Robustify react-to-vsprops-changes.py against changes to its location or the location of the .vsprops files
    Suggested by John Sullivan.
    
    * JavaScriptCore.vcproj/JavaScriptCore/react-to-vsprops-changes.py:
    Removed file_modification_times.
    (main): Use glob.glob to find the .vsprops files and assert that we found some.
    
    Canonical link: https://commits.webkit.org/63282@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@72823 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/JavaScriptCore.vcproj/JavaScriptCore/react-to-vsprops-changes.py
+++ JavaScriptCore/JavaScriptCore.vcproj/JavaScriptCore/react-to-vsprops-changes.py
@@ -6,19 +6,15 @@ import re
 import sys
 
 
-def file_modification_times(directory):
-    for dirpath, dirnames, filenames in os.walk(directory):
-        for filename in filenames:
-            yield os.path.getmtime(os.path.join(dirpath, filename))
-
-
 def main():
     # It's a little unfortunate that we're relying on the location of this
     # script to find the top-level source directory.
     top_level_directory = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
     vsprops_directory = os.path.join(top_level_directory, 'WebKitLibraries', 'win', 'tools', 'vsprops')
-    newest_vsprops_time = max(file_modification_times(vsprops_directory))
+    vsprops_files = glob.glob(os.path.join(vsprops_directory, '*.vsprops'))
+    assert len(vsprops_files), "Couldn't find any .vsprops files in %s" % vsprops_directory
+    newest_vsprops_time = max(map(os.path.getmtime, vsprops_files))
 
     # Delete any manifest-related files because Visual Studio isn't smart
     # enough to figure out that it might need to rebuild them.
