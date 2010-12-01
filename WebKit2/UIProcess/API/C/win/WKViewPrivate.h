ef569f39a30b488afe6abfcac403c39f7c2d9965
Sam Weinig <weinig@apple.com>
2010-12-01 22:08:49 +0000

    Attempt to fix Windows build.
    * UIProcess/API/C/win/WKViewPrivate.cpp:
    * UIProcess/API/C/win/WKViewPrivate.h:
    
    
    
    Canonical link: https://commits.webkit.org/63514@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@73067 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit2/UIProcess/API/C/win/WKViewPrivate.h
+++ WebKit2/UIProcess/API/C/win/WKViewPrivate.h
@@ -33,8 +33,6 @@
 extern "C" {
 #endif
 
-WK_EXPORT WKViewRef WKViewCreateForWebInspector(RECT rect, WKPageNamespaceRef pageNamespace, HWND parentWindow);
-
 WK_EXPORT void WKViewSetOverrideCursor(WKViewRef viewRef, HCURSOR overrideCursor);
 
 #ifdef __cplusplus
