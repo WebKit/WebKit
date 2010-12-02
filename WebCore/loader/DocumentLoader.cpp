84b5cd06a80f272d32dd93ececae7135448e6981
Joseph Pecoraro <joepeck@webkit.org>
2010-12-02 23:49:54 +0000

    2010-12-02  Joseph Pecoraro  <joepeck@webkit.org>
            Reviewed by Darin Adler.
    
            Update ResourceLoaderSet Enumeration
            https://bugs.webkit.org/show_bug.cgi?id=50405
    
            Use size_t when working with Vectors and when comparing between size_t types.
    
            * loader/DocumentLoader.cpp:
            (WebCore::cancelAll):
            (WebCore::setAllDefersLoading):
    
    Canonical link: https://commits.webkit.org/63637@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@73210 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/loader/DocumentLoader.cpp
+++ WebCore/loader/DocumentLoader.cpp
@@ -61,7 +61,7 @@ static void cancelAll(const ResourceLoaderSet& loaders)
     Vector<RefPtr<ResourceLoader> > loadersCopy;
     copyToVector(loaders, loadersCopy);
     size_t size = loadersCopy.size();
-    for (unsigned i = 0; i < size; ++i)
+    for (size_t i = 0; i < size; ++i)
         loadersCopy[i]->cancel();
 }
 
@@ -70,7 +70,7 @@ static void setAllDefersLoading(const ResourceLoaderSet& loaders, bool defers)
     Vector<RefPtr<ResourceLoader> > loadersCopy;
     copyToVector(loaders, loadersCopy);
     size_t size = loadersCopy.size();
-    for (unsigned i = 0; i < size; ++i)
+    for (size_t i = 0; i < size; ++i)
         loadersCopy[i]->setDefersLoading(defers);
 }
 
