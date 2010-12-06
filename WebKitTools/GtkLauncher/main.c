a4a460cba927dc010cb6e90b6d7b98baad4370ce
Alejandro G. Castro <alex@webkit.org>
2010-12-06 15:41:08 +0000

    2010-12-06  Alejandro G. Castro  <alex@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] Fix GtkLauncher.c style, use webkit style
            https://bugs.webkit.org/show_bug.cgi?id=50542
    
            * GtkLauncher/main.c:
            (activateUriEntryCb):
            (updateTitle):
            (linkHoverCb):
            (notifyTitleCb):
            (notifyLoadStatusCb):
            (notifyProgressCb):
            (destroyCb):
            (goBackCb):
            (goForwardCb):
            (create_webViewCb):
            (webViewReadyCb):
            (closeWebViewCb):
            (createBrowser):
            (createStatusbar):
            (createToolbar):
            (createWindow):
            (main):
    
    Canonical link: https://commits.webkit.org/63788@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@73367 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/GtkLauncher/main.c
+++ WebKitTools/GtkLauncher/main.c
@@ -27,208 +27,193 @@
 #include <gtk/gtk.h>
 #include <webkit/webkit.h>
 
-static gint window_count = 0;
+static gint windowCount = 0;
 
-static GtkWidget* create_window (WebKitWebView** out_web_view);
+static GtkWidget* createWindow(WebKitWebView** outWebView);
 
-static void
-activate_uri_entry_cb (GtkWidget* entry, gpointer data)
+static void activateUriEntryCb(GtkWidget* entry, gpointer data)
 {
-    WebKitWebView *web_view = g_object_get_data (G_OBJECT (entry), "web-view");
-    const gchar* uri = gtk_entry_get_text (GTK_ENTRY (entry));
-    g_assert (uri);
-    webkit_web_view_load_uri (web_view, uri);
+    WebKitWebView *webView = g_object_get_data(G_OBJECT(entry), "web-view");
+    const gchar* uri = gtk_entry_get_text(GTK_ENTRY(entry));
+    g_assert(uri);
+    webkit_web_view_load_uri(webView, uri);
 }
 
-static void
-update_title (GtkWindow* window, WebKitWebView* web_view)
+static void updateTitle(GtkWindow* window, WebKitWebView* webView)
 {
-    GString *string = g_string_new (webkit_web_view_get_title(web_view));
-    gdouble load_progress = webkit_web_view_get_progress (web_view) * 100;
-    g_string_append (string, " - WebKit Launcher");
-    if (load_progress < 100)
-        g_string_append_printf (string, " (%f%%)", load_progress);
-    gchar* title = g_string_free (string, FALSE);
-    gtk_window_set_title (window, title);
-    g_free (title);
+    GString *string = g_string_new(webkit_web_view_get_title(webView));
+    gdouble loadProgress = webkit_web_view_get_progress(webView) * 100;
+    g_string_append(string, " - WebKit Launcher");
+    if (loadProgress < 100)
+        g_string_append_printf(string, " (%f%%)", loadProgress);
+    gchar *title = g_string_free(string, FALSE);
+    gtk_window_set_title(window, title);
+    g_free(title);
 }
 
-static void
-link_hover_cb (WebKitWebView* page, const gchar* title, const gchar* link, GtkStatusbar* statusbar)
+static void linkHoverCb(WebKitWebView* page, const gchar* title, const gchar* link, GtkStatusbar* statusbar)
 {
-    guint status_context_id =
-      GPOINTER_TO_UINT (g_object_get_data (G_OBJECT (statusbar), "link-hover-context"));
+    guint statusContextId =
+      GPOINTER_TO_UINT(g_object_get_data(G_OBJECT(statusbar), "link-hover-context"));
     /* underflow is allowed */
-    gtk_statusbar_pop (statusbar, status_context_id);
+    gtk_statusbar_pop(statusbar, statusContextId);
     if (link)
-        gtk_statusbar_push (statusbar, status_context_id, link);
+        gtk_statusbar_push(statusbar, statusContextId, link);
 }
 
-static void
-notify_title_cb (WebKitWebView* web_view, GParamSpec* pspec, GtkWidget* window)
+static void notifyTitleCb(WebKitWebView* webView, GParamSpec* pspec, GtkWidget* window)
 {
-    update_title (GTK_WINDOW (window), web_view);
+    updateTitle(GTK_WINDOW(window), webView);
 }
 
-static void
-notify_load_status_cb (WebKitWebView* web_view, GParamSpec* pspec, GtkWidget* uri_entry)
+static void notifyLoadStatusCb(WebKitWebView* webView, GParamSpec* pspec, GtkWidget* uriEntry)
 {
-    if (webkit_web_view_get_load_status (web_view) == WEBKIT_LOAD_COMMITTED) {
-        WebKitWebFrame* frame = webkit_web_view_get_main_frame (web_view);
-        const gchar* uri = webkit_web_frame_get_uri (frame);
+    if (webkit_web_view_get_load_status(webView) == WEBKIT_LOAD_COMMITTED) {
+        WebKitWebFrame *frame = webkit_web_view_get_main_frame(webView);
+        const gchar *uri = webkit_web_frame_get_uri(frame);
         if (uri)
-            gtk_entry_set_text (GTK_ENTRY (uri_entry), uri);
+            gtk_entry_set_text(GTK_ENTRY(uriEntry), uri);
     }
 }
 
-static void
-notify_progress_cb (WebKitWebView* web_view, GParamSpec* pspec, GtkWidget* window)
+static void notifyProgressCb(WebKitWebView* webView, GParamSpec* pspec, GtkWidget* window)
 {
-    update_title (GTK_WINDOW (window), web_view);
+    updateTitle(GTK_WINDOW(window), webView);
 }
 
-static void
-destroy_cb (GtkWidget* widget, GtkWidget* window)
+static void destroyCb(GtkWidget* widget, GtkWidget* window)
 {
-    if (g_atomic_int_dec_and_test (&window_count))
-      gtk_main_quit ();
+    if (g_atomic_int_dec_and_test(&windowCount))
+      gtk_main_quit();
 }
 
-static void
-go_back_cb (GtkWidget* widget,  WebKitWebView* web_view)
+static void goBackCb(GtkWidget* widget,  WebKitWebView* webView)
 {
-    webkit_web_view_go_back (web_view);
+    webkit_web_view_go_back(webView);
 }
 
-static void
-go_forward_cb (GtkWidget* widget, WebKitWebView* web_view)
+static void goForwardCb(GtkWidget* widget, WebKitWebView* webView)
 {
-    webkit_web_view_go_forward (web_view);
+    webkit_web_view_go_forward(webView);
 }
 
 static WebKitWebView*
-create_web_view_cb (WebKitWebView* web_view, WebKitWebFrame* web_frame, GtkWidget* window)
+createWebViewCb(WebKitWebView* webView, WebKitWebFrame* web_frame, GtkWidget* window)
 {
-    WebKitWebView *new_web_view;
-    create_window (&new_web_view);
-    return new_web_view;
+    WebKitWebView *newWebView;
+    createWindow(&newWebView);
+    return newWebView;
 }
 
-static gboolean
-web_view_ready_cb (WebKitWebView* web_view, GtkWidget* window)
+static gboolean webViewReadyCb(WebKitWebView* webView, GtkWidget* window)
 {
-    gtk_widget_grab_focus (GTK_WIDGET (web_view));
-    gtk_widget_show_all (window);
+    gtk_widget_grab_focus(GTK_WIDGET(webView));
+    gtk_widget_show_all(window);
     return FALSE;
 }
 
-static gboolean
-close_web_view_cb (WebKitWebView* web_view, GtkWidget* window)
+static gboolean closeWebViewCb(WebKitWebView* webView, GtkWidget* window)
 {
-    gtk_widget_destroy (window);
+    gtk_widget_destroy(window);
     return TRUE;
 }
 
-static GtkWidget*
-create_browser (GtkWidget* window, GtkWidget* uri_entry, GtkWidget* statusbar, WebKitWebView* web_view)
+static GtkWidget* createBrowser(GtkWidget* window, GtkWidget* uriEntry, GtkWidget* statusbar, WebKitWebView* webView)
 {
-    GtkWidget* scrolled_window = gtk_scrolled_window_new (NULL, NULL);
-    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+    GtkWidget *scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolledWindow), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
 
-    gtk_container_add (GTK_CONTAINER (scrolled_window), GTK_WIDGET (web_view));
+    gtk_container_add(GTK_CONTAINER(scrolledWindow), GTK_WIDGET(webView));
 
-    g_signal_connect (web_view, "notify::title", G_CALLBACK (notify_title_cb), window);
-    g_signal_connect (web_view, "notify::load-status", G_CALLBACK (notify_load_status_cb), uri_entry);
-    g_signal_connect (web_view, "notify::progress", G_CALLBACK (notify_progress_cb), window);
-    g_signal_connect (web_view, "hovering-over-link", G_CALLBACK (link_hover_cb), statusbar);
-    g_signal_connect (web_view, "create-web-view", G_CALLBACK (create_web_view_cb), window);
-    g_signal_connect (web_view, "web-view-ready", G_CALLBACK (web_view_ready_cb), window);
-    g_signal_connect (web_view, "close-web-view", G_CALLBACK (close_web_view_cb), window);
+    g_signal_connect(webView, "notify::title", G_CALLBACK(notifyTitleCb), window);
+    g_signal_connect(webView, "notify::load-status", G_CALLBACK(notifyLoadStatusCb), uriEntry);
+    g_signal_connect(webView, "notify::progress", G_CALLBACK(notifyProgressCb), window);
+    g_signal_connect(webView, "hovering-over-link", G_CALLBACK(linkHoverCb), statusbar);
+    g_signal_connect(webView, "create-web-view", G_CALLBACK(createWebViewCb), window);
+    g_signal_connect(webView, "web-view-ready", G_CALLBACK(webViewReadyCb), window);
+    g_signal_connect(webView, "close-web-view", G_CALLBACK(closeWebViewCb), window);
 
-    return scrolled_window;
+    return scrolledWindow;
 }
 
-static GtkWidget*
-create_statusbar ()
+static GtkWidget* createStatusbar()
 {
-    GtkStatusbar *statusbar = GTK_STATUSBAR (gtk_statusbar_new ());
-    guint status_context_id = gtk_statusbar_get_context_id (statusbar, "Link Hover");
-    g_object_set_data (G_OBJECT (statusbar), "link-hover-context",
-        GUINT_TO_POINTER(status_context_id));
+    GtkStatusbar *statusbar = GTK_STATUSBAR(gtk_statusbar_new());
+    guint statusContextId = gtk_statusbar_get_context_id(statusbar, "Link Hover");
+    g_object_set_data(G_OBJECT(statusbar), "link-hover-context",
+        GUINT_TO_POINTER(statusContextId));
 
-    return GTK_WIDGET (statusbar);
+    return GTK_WIDGET(statusbar);
 }
 
-static GtkWidget*
-create_toolbar (GtkWidget* uri_entry, WebKitWebView* web_view)
+static GtkWidget* createToolbar(GtkWidget* uriEntry, WebKitWebView* webView)
 {
-    GtkWidget* toolbar = gtk_toolbar_new ();
+    GtkWidget *toolbar = gtk_toolbar_new();
 
-#if GTK_CHECK_VERSION(2,15,0)
-    gtk_orientable_set_orientation (GTK_ORIENTABLE (toolbar), GTK_ORIENTATION_HORIZONTAL);
+#if GTK_CHECK_VERSION(2, 15, 0)
+    gtk_orientable_set_orientation(GTK_ORIENTABLE(toolbar), GTK_ORIENTATION_HORIZONTAL);
 #else
-    gtk_toolbar_set_orientation (GTK_TOOLBAR (toolbar), GTK_ORIENTATION_HORIZONTAL);
+    gtk_toolbar_set_orientation(GTK_TOOLBAR(toolbar), GTK_ORIENTATION_HORIZONTAL);
 #endif
-    gtk_toolbar_set_style (GTK_TOOLBAR (toolbar), GTK_TOOLBAR_BOTH_HORIZ);
+    gtk_toolbar_set_style(GTK_TOOLBAR(toolbar), GTK_TOOLBAR_BOTH_HORIZ);
 
-    GtkToolItem* item;
+    GtkToolItem *item;
 
     /* the back button */
-    item = gtk_tool_button_new_from_stock (GTK_STOCK_GO_BACK);
-    g_signal_connect (G_OBJECT (item), "clicked", G_CALLBACK (go_back_cb), web_view);
-    gtk_toolbar_insert (GTK_TOOLBAR (toolbar), item, -1);
+    item = gtk_tool_button_new_from_stock(GTK_STOCK_GO_BACK);
+    g_signal_connect(G_OBJECT(item), "clicked", G_CALLBACK(goBackCb), webView);
+    gtk_toolbar_insert(GTK_TOOLBAR(toolbar), item, -1);
 
     /* The forward button */
-    item = gtk_tool_button_new_from_stock (GTK_STOCK_GO_FORWARD);
-    g_signal_connect (G_OBJECT (item), "clicked", G_CALLBACK (go_forward_cb), web_view);
-    gtk_toolbar_insert (GTK_TOOLBAR (toolbar), item, -1);
+    item = gtk_tool_button_new_from_stock(GTK_STOCK_GO_FORWARD);
+    g_signal_connect(G_OBJECT(item), "clicked", G_CALLBACK(goForwardCb), webView);
+    gtk_toolbar_insert(GTK_TOOLBAR(toolbar), item, -1);
 
     /* The URL entry */
-    item = gtk_tool_item_new ();
-    gtk_tool_item_set_expand (item, TRUE);
-    gtk_container_add (GTK_CONTAINER (item), uri_entry);
-    g_signal_connect (G_OBJECT (uri_entry), "activate", G_CALLBACK (activate_uri_entry_cb), NULL);
-    gtk_toolbar_insert (GTK_TOOLBAR (toolbar), item, -1);
+    item = gtk_tool_item_new();
+    gtk_tool_item_set_expand(item, TRUE);
+    gtk_container_add(GTK_CONTAINER(item), uriEntry);
+    g_signal_connect(G_OBJECT(uriEntry), "activate", G_CALLBACK(activateUriEntryCb), NULL);
+    gtk_toolbar_insert(GTK_TOOLBAR(toolbar), item, -1);
 
     /* The go button */
-    g_object_set_data (G_OBJECT (uri_entry), "web-view", web_view);
-    item = gtk_tool_button_new_from_stock (GTK_STOCK_OK);
-    g_signal_connect_swapped (G_OBJECT (item), "clicked", G_CALLBACK (activate_uri_entry_cb), (gpointer)uri_entry);
-    gtk_toolbar_insert (GTK_TOOLBAR (toolbar), item, -1);
+    g_object_set_data(G_OBJECT(uriEntry), "web-view", webView);
+    item = gtk_tool_button_new_from_stock(GTK_STOCK_OK);
+    g_signal_connect_swapped(G_OBJECT(item), "clicked", G_CALLBACK(activateUriEntryCb), (gpointer)uriEntry);
+    gtk_toolbar_insert(GTK_TOOLBAR(toolbar), item, -1);
 
     return toolbar;
 }
 
-static GtkWidget*
-create_window (WebKitWebView** out_web_view)
+static GtkWidget* createWindow(WebKitWebView** outWebView)
 {
-    WebKitWebView *web_view;
+    WebKitWebView *webView;
     GtkWidget *vbox;
     GtkWidget *window;
-    GtkWidget *uri_entry;
+    GtkWidget *uriEntry;
     GtkWidget *statusbar;
 
-    g_atomic_int_inc (&window_count);
-   
-    window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
-    gtk_widget_set_name (window, "GtkLauncher");
+    g_atomic_int_inc(&windowCount);
 
-    web_view = WEBKIT_WEB_VIEW (webkit_web_view_new ());
-    uri_entry = gtk_entry_new ();
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    gtk_widget_set_name(window, "GtkLauncher");
 
-    vbox = gtk_vbox_new (FALSE, 0);
-    statusbar = create_statusbar ();
-    gtk_box_pack_start (GTK_BOX (vbox), create_toolbar (uri_entry, web_view), FALSE, FALSE, 0);
-    gtk_box_pack_start (GTK_BOX (vbox), create_browser (window, uri_entry, statusbar, web_view), TRUE, TRUE, 0);
-    gtk_box_pack_start (GTK_BOX (vbox), statusbar, FALSE, FALSE, 0);
+    webView = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    uriEntry = gtk_entry_new();
 
-    gtk_container_add (GTK_CONTAINER (window), vbox);
+    vbox = gtk_vbox_new(FALSE, 0);
+    statusbar = createStatusbar(webView);
+    gtk_box_pack_start(GTK_BOX(vbox), createToolbar(uriEntry, webView), FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), createBrowser(window, uriEntry, statusbar, webView), TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), statusbar, FALSE, FALSE, 0);
 
-    g_signal_connect (window, "destroy", G_CALLBACK (destroy_cb), NULL);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
 
-    if (out_web_view)
-        *out_web_view = web_view;
+    g_signal_connect(window, "destroy", G_CALLBACK(destroyCb), NULL);
+
+    if (outWebView)
+        *outWebView = webView;
 
     return window;
 }
@@ -245,27 +230,26 @@ static gchar* filenameToURL(const char* filename)
     return fileURL;
 }
 
-int
-main (int argc, char* argv[])
+int main(int argc, char* argv[])
 {
-    WebKitWebView *web_view;
+    WebKitWebView *webView;
     GtkWidget *main_window;
 
-    gtk_init (&argc, &argv);
-    if (!g_thread_supported ())
-        g_thread_init (NULL);
+    gtk_init(&argc, &argv);
+    if (!g_thread_supported())
+        g_thread_init(NULL);
 
-    main_window = create_window (&web_view);
+    main_window = createWindow(&webView);
 
-    gchar *uri = (gchar*) (argc > 1 ? argv[1] : "http://www.google.com/");
+    gchar *uri =(gchar*)(argc > 1 ? argv[1] : "http://www.google.com/");
     gchar *fileURL = filenameToURL(uri);
 
-    webkit_web_view_load_uri(web_view, fileURL ? fileURL : uri);
+    webkit_web_view_load_uri(webView, fileURL ? fileURL : uri);
     g_free(fileURL);
 
-    gtk_widget_grab_focus (GTK_WIDGET (web_view));
-    gtk_widget_show_all (main_window);
-    gtk_main ();
+    gtk_widget_grab_focus(GTK_WIDGET(webView));
+    gtk_widget_show_all(main_window);
+    gtk_main();
 
     return 0;
 }
