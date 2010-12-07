cf4e86018f249a549879b90b5ff568e9f352e916
Yury Semikhatsky <yurys@chromium.org>
2010-12-07 14:24:51 +0000

    2010-12-07  Yury Semikhatsky  <yurys@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: switch http/tests/inspector/console-resource-errors.html to inspector-tests2
            https://bugs.webkit.org/show_bug.cgi?id=50551
    
            We need to be sure that front-end has already opened by the time the messages are
            recorded in console because the data we collect for errors may differ depending
            on whether the front-end is open.
    
            * http/tests/inspector/console-resource-errors-expected.txt:
            * http/tests/inspector/console-resource-errors.html:
            * http/tests/inspector/console-tests2.js:
            (initialize_ConsoleTest.InspectorTest.dumpConsoleMessagesWithClasses):
    
    Canonical link: https://commits.webkit.org/63856@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@73440 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/inspector/console-tests2.js
+++ LayoutTests/http/tests/inspector/console-tests2.js
@@ -11,6 +11,19 @@ InspectorTest.dumpConsoleMessages = function()
     return result;
 }
 
+InspectorTest.dumpConsoleMessagesWithClasses = function(sortMessages) {
+    var result = [];
+    var messages = WebInspector.console.messages;
+    for (var i = 0; i < messages.length; ++i) {
+        var element = messages[i].toMessageElement();
+        result.push(element.textContent.replace(/\u200b/g, "") + " " + element.getAttribute("class"));
+    }
+    if (sortMessages)
+        result.sort();
+    for (var i = 0; i < messages.length; ++i)
+        InspectorTest.addResult(result[i]);
+}
+
 InspectorTest.expandConsoleMessages = function()
 {
     var messages = WebInspector.console.messages;
