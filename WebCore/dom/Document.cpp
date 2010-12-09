be5cce03b92e1e958f89cdf5ff53d562aa3b22a4
Abhishek Arya <inferno@chromium.org>
2010-12-09 17:51:07 +0000

    2010-12-09  Abhishek Arya  <inferno@chromium.org>
            Reviewed by Dimitri Glazkov.
    
            As part of r73559, I added the referenceNode check to validate whether the root
            node of the iterator matches the node which is getting moved to other document.
            referenceNode is initialized to root, however can get moved using previousNode
            and nextNode methods, so it is required to use root directly.
            https://bugs.webkit.org/show_bug.cgi?id=50764
    
            Test: fast/dom/node-iterator-reference-node-moved-crash.html
    
            * dom/Document.cpp:
            (WebCore::Document::moveNodeIteratorsToNewDocument): change referenceNode to root.
    2010-12-09  Abhishek Arya  <inferno@chromium.org>
    
            Reviewed by Dimitri Glazkov.
    
            Tests that we do not crash when we move the referenceNode using node iterator
            methods previousNode and nextNode.
            https://bugs.webkit.org/show_bug.cgi?id=50764
    
            * fast/dom/node-iterator-reference-node-moved-crash-expected.txt: Added.
            * fast/dom/node-iterator-reference-node-moved-crash.html: Added.
    
    
    Canonical link: https://commits.webkit.org/64028@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@73620 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/dom/Document.cpp
+++ WebCore/dom/Document.cpp
@@ -3308,7 +3308,7 @@ void Document::moveNodeIteratorsToNewDocument(Node* node, Document* newDocument)
     HashSet<NodeIterator*> nodeIteratorsList = m_nodeIterators;
     HashSet<NodeIterator*>::const_iterator nodeIteratorsEnd = nodeIteratorsList.end();
     for (HashSet<NodeIterator*>::const_iterator it = nodeIteratorsList.begin(); it != nodeIteratorsEnd; ++it) {
-        if ((*it)->referenceNode() == node) {
+        if ((*it)->root() == node) {
             detachNodeIterator(*it);
             newDocument->attachNodeIterator(*it);
         }
