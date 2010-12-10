619fc3bfd545f865538f2433a882d70015a47fed
Eric Seidel <eric@webkit.org>
2010-12-10 17:00:11 +0000

    2010-12-10  Eric Seidel  <eric@webkit.org>
            Reviewed by Ojan Vafai.
    
            webkit-patch: not possible to use build-and-test with local commits
            https://bugs.webkit.org/show_bug.cgi?id=33378
    
            Make --no-clean not even check if we have local commits.
            It's unclear to me why the code was originally written this way.
            I was unable to dig up a reason from svn history.
    
            * Scripts/webkitpy/tool/steps/cleanworkingdirectory.py:
            * Scripts/webkitpy/tool/steps/cleanworkingdirectory_unittest.py: Copied from WebKitTools/Scripts/webkitpy/tool/steps/cleanworkingdirectory.py.
    
    Canonical link: https://commits.webkit.org/64140@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@73733 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitTools/Scripts/webkitpy/tool/steps/cleanworkingdirectory_unittest.py
+++ WebKitTools/Scripts/webkitpy/tool/steps/cleanworkingdirectory_unittest.py
@@ -1,9 +1,9 @@
 # Copyright (C) 2010 Google Inc. All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above
@@ -13,7 +13,7 @@
 #     * Neither the name of Google Inc. nor the names of its
 # contributors may be used to endorse or promote products derived from
 # this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -26,29 +26,24 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import os
+import unittest
 
-from webkitpy.tool.steps.abstractstep import AbstractStep
-from webkitpy.tool.steps.options import Options
+from webkitpy.thirdparty.mock import Mock
+from webkitpy.tool.mocktool import MockOptions, MockTool
+from webkitpy.tool.steps.cleanworkingdirectory import CleanWorkingDirectory
 
 
-class CleanWorkingDirectory(AbstractStep):
-    def __init__(self, tool, options, allow_local_commits=False):
-        AbstractStep.__init__(self, tool, options)
-        self._allow_local_commits = allow_local_commits
+class CleanWorkingDirectoryTest(unittest.TestCase):
+    def test_run(self):
+        tool = MockTool()
+        step = CleanWorkingDirectory(tool, MockOptions(clean=True, force_clean=False))
+        step.run({})
+        self.assertEqual(tool._scm.ensure_no_local_commits.call_count, 1)
+        self.assertEqual(tool._scm.ensure_clean_working_directory.call_count, 1)
 
-    @classmethod
-    def options(cls):
-        return AbstractStep.options() + [
-            Options.force_clean,
-            Options.clean,
-        ]
-
-    def run(self, state):
-        if not self._options.clean:
-            return
-        # FIXME: This chdir should not be necessary.
-        os.chdir(self._tool.scm().checkout_root)
-        if not self._allow_local_commits:
-            self._tool.scm().ensure_no_local_commits(self._options.force_clean)
-        self._tool.scm().ensure_clean_working_directory(force_clean=self._options.force_clean)
+    def test_no_clean(self):
+        tool = MockTool()
+        step = CleanWorkingDirectory(tool, MockOptions(clean=False))
+        step.run({})
+        self.assertEqual(tool._scm.ensure_no_local_commits.call_count, 0)
+        self.assertEqual(tool._scm.ensure_clean_working_directory.call_count, 0)
