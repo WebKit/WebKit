81b95f3a7d5b9ca3e4786ab22578cba4ca96e02d
Alexey Proskuryakov <ap@apple.com>
2010-12-11 07:21:02 +0000

    Mac build fix.
            * platform/network/cf/ResourceResponseCFNet.cpp: (WebCore::ResourceResponse::platformLazyInit):
            Resolve string[0] ambiguity by passing 0U.
    
    
    
    Canonical link: https://commits.webkit.org/64208@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@73844 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/platform/network/cf/ResourceResponseCFNet.cpp
+++ WebCore/platform/network/cf/ResourceResponseCFNet.cpp
@@ -81,7 +81,7 @@ void ResourceResponse::platformLazyInit()
 
     // Workaround for <rdar://problem/8757088>, can be removed once that is fixed.
     unsigned textEncodingNameLength = m_textEncodingName.length();
-    if (textEncodingNameLength >= 2 && m_textEncodingName[0] == '"' && m_textEncodingName[textEncodingNameLength - 1] == '"')
+    if (textEncodingNameLength >= 2 && m_textEncodingName[0U] == '"' && m_textEncodingName[textEncodingNameLength - 1] == '"')
         m_textEncodingName = m_textEncodingName.substring(1, textEncodingNameLength - 2);
 
     m_lastModifiedDate = toTimeT(CFURLResponseGetLastModifiedDate(m_cfResponse.get()));
