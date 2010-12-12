6de0864d3d9e30b29f43fb695b0b8a7ae70ea0e3
Balazs Kelemen <kbalazs@webkit.org>
2010-12-12 16:25:50 +0000

    [Qt][WK2] Crash in WebPage constructor. https://bugs.webkit.org/show_bug.cgi?id=50892
    Reviewed by Andreas Kling.
    
    * WebProcess/qt/WebProcessQt.cpp:
    (WebKit::WebProcess::platformInitializeWebProcess):
    Disable runtime enabled features that have no WebKit2 implementation yet.
    
    Canonical link: https://commits.webkit.org/64237@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@73876 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit2/WebProcess/qt/WebProcessQt.cpp
+++ WebKit2/WebProcess/qt/WebProcessQt.cpp
@@ -26,6 +26,7 @@
 #include "WebProcess.h"
 
 #include "WebProcessCreationParameters.h"
+#include <WebCore/RuntimeEnabledFeatures.h>
 #include <QNetworkAccessManager>
 
 namespace WebKit {
@@ -42,6 +43,15 @@ void WebProcess::platformClearResourceCaches()
 void WebProcess::platformInitializeWebProcess(const WebProcessCreationParameters& parameters, CoreIPC::ArgumentDecoder* arguments)
 {
     m_networkAccessManager = new QNetworkAccessManager;
+
+    // Disable runtime enabled features that have no WebKit2 implementation yet.
+#if ENABLE(DEVICE_ORIENTATION)
+    WebCore::RuntimeEnabledFeatures::setDeviceMotionEnabled(false);
+    WebCore::RuntimeEnabledFeatures::setDeviceOrientationEnabled(false);
+#endif
+#if ENABLE(SPEECH_INPUT)
+    WebCore::RuntimeEnabledFeatures::setSpeechInputEnabled(false);
+#endif
 }
 
 void WebProcess::platformShutdown()
