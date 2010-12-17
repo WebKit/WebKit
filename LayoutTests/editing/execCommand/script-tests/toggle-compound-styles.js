ccb7489068d556c951e8d6664971f2a5ab7c0b4e
Ryosuke Niwa <rniwa@webkit.org>
2010-12-17 18:58:23 +0000

    2010-12-17  Ryosuke Niwa  <rniwa@webkit.org>
            Reviewed by Antonio Gomes.
    
            editing/execCommand/toggle-compound-styles-expected should use LayoutTestController::setEditingBehavior
            https://bugs.webkit.org/show_bug.cgi?id=51261
    
            Modified the test to run each test case using each editing behavior, eliminating the need for
            platform-specific expected results.
    
            * editing/execCommand/script-tests/toggle-compound-styles.js:
            (testSingleToggle):
            * platform/gtk/editing/execCommand/toggle-compound-styles-expected.txt: Removed.
            * platform/mac/editing/execCommand/toggle-compound-styles-expected.txt: Removed.
            * platform/qt/editing/execCommand/toggle-compound-styles-expected.txt: Removed.
            * platform/win/editing/execCommand/toggle-compound-styles-expected.txt: Removed.
    
    
    Canonical link: https://commits.webkit.org/64621@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@74279 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/editing/execCommand/script-tests/toggle-compound-styles.js
+++ LayoutTests/editing/execCommand/script-tests/toggle-compound-styles.js
@@ -4,33 +4,47 @@ var testContainer = document.createElement("div");
 testContainer.contentEditable = true;
 document.body.appendChild(testContainer);
 
-function testSingleToggle(toggleCommand, initialContents, expectedContents, dependsOnPlatform)
+function testSingleToggle(toggleCommand, initialContents, expectedContents)
 {
     testContainer.innerHTML = initialContents;
     window.getSelection().selectAllChildren(testContainer);
     document.execCommand(toggleCommand, false, null);
-    if (!expectedContents)
-        debug('<span>' + escapeHTML("one " + toggleCommand + " command converted " + initialContents + " to " + testContainer.innerHTML) + '</span>');
-    else if (testContainer.innerHTML === expectedContents)
-        testPassed("one " + toggleCommand + " command converted " + initialContents + " to " + expectedContents);
+    var action = 'one ' + toggleCommand + ' command converted "' + initialContents + '" to "' + expectedContents;
+    if (testContainer.innerHTML === expectedContents)
+        testPassed(action);
     else
-        testFailed("one " + toggleCommand + " command converted " + initialContents + " to " + testContainer.innerHTML + ", expected " + expectedContents);
+        testFailed(action + '", expected "' + expectedContents + '"');
 }
 
-debug('PLATFORM-DEPENDENT TESTS');
-testSingleToggle("bold", "<u><b>hello</b> world</u>");
-testSingleToggle("bold", "<b>hello </b>world");
-testSingleToggle("bold", "<u><b>hello </b></u>world");
-testSingleToggle("italic", "<i>hello</i> <img>");
-testSingleToggle("italic", "<s><b>hello<i> world</i></b></s>");
-debug('PLATFORM-INDEPENDENT TESTS');
-testSingleToggle("bold", "<u><span id='test'><b>hello</b></span><b>world</b></u>", '<u><span id="test">hello</span>world</u>');
-testSingleToggle("bold", "<span id='test' style='font-weight:normal;'><b>hello</b></span>", '<span id="test">hello</span>');
-testSingleToggle("bold", "<div><b>hello</b><br><br><b>world</b></div>", "<div>hello<br><br>world</div>");
-testSingleToggle("italic", "<i>hello </i><img>", "hello <img>");
-testSingleToggle("italic", "<i><b>hello</b>world</i>", "<b>hello</b>world");
-testSingleToggle("italic", "<span style='font-style: normal;'> <i> hello </i> </span>", "  hello  ");
-testSingleToggle("italic", "<p><i>hello</i><span style='font-style:italic;'>world</span></p>", "<p>helloworld</p>");
+platforms = ['mac', 'win', 'unix'];
+
+for (var i = 0; i < platforms.length; i++) {
+    platform = platforms[i];
+    debug('Platform: ' + platform);
+
+    if (window.layoutTestController)
+        layoutTestController.setEditingBehavior(platform);
+
+    if (platform == 'win' || platform == 'unix')
+        platform = 'nonmac';
+
+    testSingleToggle("bold", "<u><b>hello</b> world</u>", {mac: '<u>hello world</u>', nonmac: '<u><b>hello world</b></u>'}[platform]);
+    testSingleToggle("bold", "<b>hello </b>world", {mac: 'hello world', nonmac: '<b>hello world</b>'}[platform]);
+    testSingleToggle("bold", "<u><b>hello </b></u>world", {mac: '<u>hello </u>world', nonmac: '<b><u>hello </u>world</b>'}[platform]);
+    testSingleToggle("italic", "<i>hello</i> <img>", {mac: 'hello <img>', nonmac: '<i>hello <img></i>'}[platform]);
+
+    // Following tests are cross-platform
+    testSingleToggle("bold", "<u><span id='test'><b>hello</b></span><b>world</b></u>", '<u><span id="test">hello</span>world</u>');
+    testSingleToggle("bold", "<span id='test' style='font-weight:normal;'><b>hello</b></span>", '<span id="test">hello</span>');
+    testSingleToggle("bold", "<div><b>hello</b><br><br><b>world</b></div>", "<div>hello<br><br>world</div>");
+    testSingleToggle("italic", "<i>hello </i><img>", "hello <img>");
+    testSingleToggle("italic", "<i><b>hello</b>world</i>", "<b>hello</b>world");
+    testSingleToggle("italic", "<span style='font-style: normal;'> <i> hello </i> </span>", "  hello  ");
+    testSingleToggle("italic", "<p><i>hello</i><span style='font-style:italic;'>world</span></p>", "<p>helloworld</p>");
+    testSingleToggle("italic", "<s><b>hello<i> world</i></b></s>", "<s><b><i>hello world</i></b></s>");
+
+    debug('');
+}
 
 document.body.removeChild(testContainer);
 
