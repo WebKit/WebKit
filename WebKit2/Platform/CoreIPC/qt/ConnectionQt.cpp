31990892c71303705f87eb2124007432db57770f
Balazs Kelemen <kbalazs@webkit.org>
2010-12-20 15:12:06 +0000

    2010-12-20  Balazs Kelemen  <kbalazs@webkit.org>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt][WK2] Crash in web process after the connection had been closed
            https://bugs.webkit.org/show_bug.cgi?id=51115
    
            Do not call connectionDidClose with a connection that has been explicitly
            invalidated on the listener thread.
            * Platform/CoreIPC/Connection.h:
            * Platform/CoreIPC/qt/ConnectionQt.cpp:
            (CoreIPC::Connection::disconnectHandler): Added.
            (CoreIPC::Connection::open):
    
    Canonical link: https://commits.webkit.org/64687@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@74345 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKit2/Platform/CoreIPC/qt/ConnectionQt.cpp
+++ WebKit2/Platform/CoreIPC/qt/ConnectionQt.cpp
@@ -82,6 +82,13 @@ void Connection::readyReadHandler()
     }
 }
 
+void Connection::disconnectHandler()
+{
+    // The connection might have been explicitly invalidated on the listener thread.
+    if (isValid())
+        connectionDidClose();
+}
+
 bool Connection::open()
 {
     ASSERT(!m_socket);
@@ -98,7 +105,7 @@ bool Connection::open()
         m_socket->connectToServer(m_serverName);
         m_connectionQueue.moveSocketToWorkThread(m_socket);
         m_connectionQueue.connectSignal(m_socket, SIGNAL(readyRead()), WorkItem::create(this, &Connection::readyReadHandler));
-        m_connectionQueue.connectSignal(m_socket, SIGNAL(disconnected()), WorkItem::create(this, &Connection::connectionDidClose));
+        m_connectionQueue.connectSignal(m_socket, SIGNAL(disconnected()), WorkItem::create(this, &Connection::disconnectHandler));
         m_isConnected = m_socket->waitForConnected();
     }
     return m_isConnected;
