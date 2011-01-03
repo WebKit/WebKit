f3adcaf796956ef2eefbf859ca5638090c05ec39
Pratik Solanki <psolanki@apple.com>
2011-01-03 18:24:54 +0000

    2011-01-03  Pratik Solanki  <psolanki@apple.com>
            Unreviewed. Adding myself to committers.py.
    
            * Scripts/webkitpy/common/config/committers.py:
    
    
    Canonical link: https://commits.webkit.org/65241@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@74902 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/config/committers.py
+++ Tools/Scripts/webkitpy/common/config/committers.py
@@ -175,6 +175,7 @@ committers_unable_to_review = [
     Committer("Philippe Normand", ["pnormand@igalia.com", "philn@webkit.org"], "philn-tp"),
     Committer("Pierre d'Herbemont", ["pdherbemont@free.fr", "pdherbemont@apple.com"], "pdherbemont"),
     Committer("Pierre-Olivier Latour", "pol@apple.com", "pol"),
+    Committer("Pratik Solanki", "psolanki@apple.com", "psolanki"),
     Committer("Renata Hodovan", "reni@webkit.org", "reni"),
     Committer("Robert Hogan", ["robert@webkit.org", "robert@roberthogan.net", "lists@roberthogan.net"], "mwenge"),
     Committer("Roland Steiner", "rolandsteiner@chromium.org"),
