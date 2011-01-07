a96173f675f27b29060ec354f144d78d71796557
Chris Marrin <cmarrin@apple.com>
2011-01-07 20:44:01 +0000

    2011-01-07  Chris Marrin  <cmarrin@apple.com>
            Unreviewed.
    
            Adding updated WKSI files missed in http://trac.webkit.org/changeset/75262
    
            * win/include/WebKitSystemInterface/WebKitSystemInterface.h:
            * win/lib/WebKitSystemInterface.lib:
            * win/lib/WebKitSystemInterface_debug.lib:
    
    
    
    Canonical link: https://commits.webkit.org/65585@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@75264 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitLibraries/win/include/WebKitSystemInterface/WebKitSystemInterface.h
+++ WebKitLibraries/win/include/WebKitSystemInterface/WebKitSystemInterface.h
@@ -145,6 +145,10 @@ void wkCACFContextSetLayer(WKCACFContext*, CACFLayerRef);
 void wkCACFContextFlush(WKCACFContext*);
 
 CFTimeInterval wkCACFContextGetLastCommitTime(WKCACFContext*);
+CFTimeInterval wkCACFContextGetNextUpdateTime(WKCACFContext*);
+
+void* wkCACFContextGetUserData(WKCACFContext*);
+void wkCACFContextSetUserData(WKCACFContext*, void*);
 
 void wkCACFContextInitializeD3DDevice(WKCACFContext*, IDirect3DDevice9*);
 void wkCACFContextReleaseD3DResources(WKCACFContext*);
