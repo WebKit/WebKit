b344dec114be8c41aed4eddc3e296a9b8996d3c7
Alexey Proskuryakov <ap@apple.com>
2011-01-17 20:53:33 +0000

    Build fix.
            * Platform/Logging.cpp:
    
    
    
    Canonical link: https://commits.webkit.org/66210@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@75968 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/Platform/Logging.cpp
+++ Source/WebKit2/Platform/Logging.cpp
@@ -30,7 +30,7 @@
 WTFLogChannel LogSessionState = { 0x00000001, "WebKit2LogLevel", WTFLogChannelOff };
 WTFLogChannel LogContextMenu  = { 0x00000002, "WebKit2LogLevel", WTFLogChannelOff };
 WTFLogChannel LogTextInput    = { 0x00000004, "WebKit2LogLevel", WTFLogChannelOff };
-WTFLogChannel LogTextView     = { 0x00000008, "WebKit2LogLevel", WTFLogChannelOff };
+WTFLogChannel LogView         = { 0x00000008, "WebKit2LogLevel", WTFLogChannelOff };
 
 static inline void initializeLogChannel(WTFLogChannel* channel)
 {
