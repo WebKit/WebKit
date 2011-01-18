3d9c0deb4ab61679c4d432bff082775768019dec
Kenneth Russell <kbr@google.com>
2011-01-18 22:11:48 +0000

    2011-01-18  Kenneth Russell  <kbr@google.com>
            Reviewed by David Levin.
    
            Must strip comments from WebGL shaders before enforcing character set
            https://bugs.webkit.org/show_bug.cgi?id=52390
    
            Strip comments from incoming shaders, preserving line numbers,
            before validating that they conform to the ESSL character set.
            Revert changes from http://trac.webkit.org/changeset/75735 which
            allowed invalid characters to be passed to certain APIs.
    
            Tested with WebGL layout tests, conformance test suite and several
            WebGL demos in both Safari and Chromium.
    
            * html/canvas/WebGLRenderingContext.cpp:
            (WebCore::StripComments::StripComments::process):
            (WebCore::WebGLRenderingContext::shaderSource):
    2011-01-18  Kenneth Russell  <kbr@google.com>
    
            Reviewed by David Levin.
    
            Must strip comments from WebGL shaders before enforcing character set
            https://bugs.webkit.org/show_bug.cgi?id=52390
    
            Incorporated non-ASCII GLSL conformance tests from Khronos
            repository. Updated and synchronized invalid-passed-params.html
            with Khronos repository, undoing changes from
            http://trac.webkit.org/changeset/75735 .
    
            * fast/canvas/webgl/glsl-conformance-expected.txt:
            * fast/canvas/webgl/invalid-passed-params-expected.txt:
            * fast/canvas/webgl/invalid-passed-params.html:
            * fast/canvas/webgl/shaders/00_shaders.txt:
            * fast/canvas/webgl/shaders/misc: Added.
            * fast/canvas/webgl/shaders/misc/00_shaders.txt: Added.
            * fast/canvas/webgl/shaders/misc/non-ascii-comments.vert: Added.
            * fast/canvas/webgl/shaders/misc/non-ascii.vert: Added.
    
    
    Canonical link: https://commits.webkit.org/66301@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@76063 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/canvas/WebGLRenderingContext.cpp
+++ Source/WebCore/html/canvas/WebGLRenderingContext.cpp
@@ -62,6 +62,7 @@
 #include <wtf/ByteArray.h>
 #include <wtf/OwnArrayPtr.h>
 #include <wtf/PassOwnArrayPtr.h>
+#include <wtf/text/StringBuilder.h>
 
 namespace WebCore {
 
@@ -101,12 +102,11 @@ namespace {
 
     // Return true if a character belongs to the ASCII subset as defined in
     // GLSL ES 1.0 spec section 3.1.
-    // We make exceptions for " ' `.
     bool validateCharacter(unsigned char c)
     {
         // Printing characters are valid except " $ ` @ \ ' DEL.
         if (c >= 32 && c <= 126
-            && c != '$' && c != '@' && c != '\\')
+            && c != '"' && c != '$' && c != '`' && c != '@' && c != '\\' && c != '\'')
             return true;
         // Horizontal tab, line feed, vertical tab, form feed, carriage return
         // are also valid.
@@ -115,6 +115,189 @@ namespace {
         return false;
     }
 
+    // Strips comments from shader text. This allows non-ASCII characters
+    // to be used in comments without potentially breaking OpenGL
+    // implementations not expecting characters outside the GLSL ES set.
+    class StripComments {
+    public:
+        StripComments(const String& str)
+            : m_parseState(BeginningOfLine)
+            , m_sourceString(str)
+            , m_length(str.length())
+            , m_position(0)
+        {
+            parse();
+        }
+
+        String result()
+        {
+            return m_builder.toString();
+        }
+
+    private:
+        bool hasMoreCharacters()
+        {
+            return (m_position < m_length);
+        }
+
+        void parse()
+        {
+            while (hasMoreCharacters()) {
+                process(current());
+                // process() might advance the position.
+                if (hasMoreCharacters())
+                    advance();
+            }
+        }
+
+        void process(UChar);
+
+        bool peek(UChar& character)
+        {
+            if (m_position + 1 >= m_length)
+                return false;
+            character = m_sourceString[m_position + 1];
+            return true;
+        }
+
+        UChar current()
+        {
+            ASSERT(m_position < m_length);
+            return m_sourceString[m_position];
+        }
+
+        void advance()
+        {
+            ++m_position;
+        }
+
+        bool isNewline(UChar character)
+        {
+            // Don't attempt to canonicalize newline related characters.
+            return (character == '\n' || character == '\r');
+        }
+
+        void emit(UChar character)
+        {
+            m_builder.append(character);
+        }
+
+        enum ParseState {
+            // Have not seen an ASCII non-whitespace character yet on
+            // this line. Possible that we might see a preprocessor
+            // directive.
+            BeginningOfLine,
+
+            // Have seen at least one ASCII non-whitespace character
+            // on this line.
+            MiddleOfLine,
+
+            // Handling a preprocessor directive. Passes through all
+            // characters up to the end of the line. Disables comment
+            // processing.
+            InPreprocessorDirective,
+
+            // Handling a single-line comment. The comment text is
+            // replaced with a single space.
+            InSingleLineComment,
+
+            // Handling a multi-line comment. Newlines are passed
+            // through to preserve line numbers.
+            InMultiLineComment
+        };
+
+        ParseState m_parseState;
+        String m_sourceString;
+        unsigned m_length;
+        unsigned m_position;
+        StringBuilder m_builder;
+    };
+
+    void StripComments::process(UChar c)
+    {
+        if (isNewline(c)) {
+            // No matter what state we are in, pass through newlines
+            // so we preserve line numbers.
+            emit(c);
+
+            if (m_parseState != InMultiLineComment)
+                m_parseState = BeginningOfLine;
+
+            return;
+        }
+
+        UChar temp = 0;
+        switch (m_parseState) {
+        case BeginningOfLine:
+            if (WTF::isASCIISpace(c)) {
+                emit(c);
+                break;
+            }
+
+            if (c == '#') {
+                m_parseState = InPreprocessorDirective;
+                emit(c);
+                break;
+            }
+
+            // Transition to normal state and re-handle character.
+            m_parseState = MiddleOfLine;
+            process(c);
+            break;
+
+        case MiddleOfLine:
+            if (c == '/' && peek(temp)) {
+                if (temp == '/') {
+                    m_parseState = InSingleLineComment;
+                    emit(' ');
+                    advance();
+                    break;
+                }
+
+                if (temp == '*') {
+                    m_parseState = InMultiLineComment;
+                    // Emit the comment start in case the user has
+                    // an unclosed comment and we want to later
+                    // signal an error.
+                    emit('/');
+                    emit('*');
+                    advance();
+                    break;
+                }
+            }
+
+            emit(c);
+            break;
+
+        case InPreprocessorDirective:
+            // No matter what the character is, just pass it
+            // through. Do not parse comments in this state. This
+            // might not be the right thing to do long term, but it
+            // should handle the #error preprocessor directive.
+            emit(c);
+            break;
+
+        case InSingleLineComment:
+            // The newline code at the top of this function takes care
+            // of resetting our state when we get out of the
+            // single-line comment. Swallow all other characters.
+            break;
+
+        case InMultiLineComment:
+            if (c == '*' && peek(temp) && temp == '/') {
+                emit('*');
+                emit('/');
+                m_parseState = MiddleOfLine;
+                advance();
+                break;
+            }
+
+            // Swallow all other characters. Unclear whether we may
+            // want or need to just emit a space per character to try
+            // to preserve column numbers for debugging purposes.
+            break;
+        }
+    }
 } // namespace anonymous
 
 class WebGLStateRestorer {
@@ -2547,9 +2730,10 @@ void WebGLRenderingContext::shaderSource(WebGLShader* shader, const String& stri
     UNUSED_PARAM(ec);
     if (isContextLost() || !validateWebGLObject(shader))
         return;
-    if (!validateString(string))
+    String stringWithoutComments = StripComments(string).result();
+    if (!validateString(stringWithoutComments))
         return;
-    m_context->shaderSource(objectOrZero(shader), string);
+    m_context->shaderSource(objectOrZero(shader), stringWithoutComments);
     cleanupAfterGraphicsCall(false);
 }
 
