94f585f7d32b7f222325e7bf8765b61043d44c75
Mikhail Naganov <mnaganov@chromium.org>
2011-01-21 14:41:54 +0000

    2011-01-21  Mikhail Naganov  <mnaganov@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: [Chromium] REGRESSION: Using the "Back" button
            while profiling causes renderer crash.
    
            https://bugs.webkit.org/show_bug.cgi?id=52808
    
            * inspector/InspectorProfilerAgent.cpp:
            (WebCore::InspectorProfilerAgent::addProfileFinishedMessageToConsole):
            (WebCore::InspectorProfilerAgent::addStartProfilingMessageToConsole):
    
    Canonical link: https://commits.webkit.org/66580@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@76345 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/InspectorProfilerAgent.cpp
+++ Source/WebCore/inspector/InspectorProfilerAgent.cpp
@@ -88,6 +88,8 @@ void InspectorProfilerAgent::addProfile(PassRefPtr<ScriptProfile> prpProfile, un
 
 void InspectorProfilerAgent::addProfileFinishedMessageToConsole(PassRefPtr<ScriptProfile> prpProfile, unsigned lineNumber, const String& sourceURL)
 {
+    if (!m_frontend)
+        return;
     RefPtr<ScriptProfile> profile = prpProfile;
     String title = profile->title();
     String message = makeString("Profile \"webkit-profile://", CPUProfileType, '/', encodeWithURLEscapeSequences(title), '#', String::number(profile->uid()), "\" finished.");
@@ -96,6 +98,8 @@ void InspectorProfilerAgent::addProfileFinishedMessageToConsole(PassRefPtr<Scrip
 
 void InspectorProfilerAgent::addStartProfilingMessageToConsole(const String& title, unsigned lineNumber, const String& sourceURL)
 {
+    if (!m_frontend)
+        return;
     String message = makeString("Profile \"webkit-profile://", CPUProfileType, '/', encodeWithURLEscapeSequences(title), "#0\" started.");
     m_inspectorController->consoleAgent()->addMessageToConsole(JSMessageSource, LogMessageType, LogMessageLevel, message, lineNumber, sourceURL);
 }
