8f3b1457c8b90236778d57b3f6425d0dac3ff814
Andreas Kling <andreas.kling@nokia.com>
2011-01-24 11:51:17 +0000

    2011-01-24  Andreas Kling  <kling@webkit.org>
            Reviewed by Tor Arne Vestbø.
    
            [Qt] Use rad2deg() from WTF instead of rolling our own.
            https://bugs.webkit.org/show_bug.cgi?id=52993
    
            * platform/graphics/qt/GraphicsContextQt.cpp:
            (WebCore::GraphicsContext::rotate):
            * platform/graphics/qt/PathQt.cpp:
            (WebCore::Path::addArc):
    
    
    Canonical link: https://commits.webkit.org/66734@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@76501 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/qt/PathQt.cpp
+++ Source/WebCore/platform/graphics/qt/PathQt.cpp
@@ -39,14 +39,8 @@
 #include <QPainterPath>
 #include <QTransform>
 #include <QString>
+#include <wtf/MathExtras.h>
 #include <wtf/OwnPtr.h>
-
-#define _USE_MATH_DEFINES
-#include <math.h>
-
-#ifndef M_PI
-#   define M_PI 3.14159265358979323846
-#endif
 
 namespace WebCore {
 
@@ -263,7 +257,6 @@ void Path::closeSubpath()
     m_path.closeSubpath();
 }
 
-#define DEGREES(t) ((t) * 180.0 / M_PI)
 void Path::addArc(const FloatPoint& p, float r, float sar, float ear, bool anticlockwise)
 {
     qreal xc = p.x();
@@ -280,8 +273,8 @@ void Path::addArc(const FloatPoint& p, float r, float sar, float ear, bool antic
     anticlockwise = !anticlockwise;
     //end hack
 
-    float sa = DEGREES(sar);
-    float ea = DEGREES(ear);
+    float sa = rad2deg(sar);
+    float ea = rad2deg(ear);
 
     double span = 0;
 
