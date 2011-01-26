89a23045e650ce81c41a398cde725e51d4c3bf43
Pavel Feldman <pfeldman@chromium.org>
2011-01-26 17:19:06 +0000

    2011-01-26  Pavel Feldman  <pfeldman@chromium.org>
            Reviewed by Yury Semikhatsky.
    
            Web Inspector: visualize \n in strings as unicode cr
            symbol in stack variables sidebar.
            https://bugs.webkit.org/show_bug.cgi?id=53162
    
            * inspector/front-end/ObjectPropertiesSection.js:
            (WebInspector.ObjectPropertyTreeElement.prototype.update):
    
    
    
    Canonical link: https://commits.webkit.org/66912@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@76692 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/ObjectPropertiesSection.js
+++ Source/WebCore/inspector/front-end/ObjectPropertiesSection.js
@@ -180,7 +180,13 @@ WebInspector.ObjectPropertyTreeElement.prototype = {
         
         this.valueElement = document.createElement("span");
         this.valueElement.className = "value";
-        this.valueElement.textContent = this.property.value.description;
+
+        var description = this.property.value.description;
+        // Render \n as a nice unicode cr symbol.
+        if (this.property.value.type === "string" && typeof description === "string")
+            description = description.replace(/\n/g, "\u21B5");
+        this.valueElement.textContent = description;
+
         if (this.property.isGetter)
             this.valueElement.addStyleClass("dimmed");
         if (this.property.value.isError())
