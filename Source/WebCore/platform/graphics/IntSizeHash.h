df79ebe10b0eed4c5f77b5bdd2f0d05013098a9b
Alexey Proskuryakov <ap@apple.com>
2011-01-26 03:29:52 +0000

    Reviewed by Darin Adler.
            https://bugs.webkit.org/show_bug.cgi?id=53143
            Add IntRectHash
    
            * WebCore.xcodeproj/project.pbxproj:
            * platform/graphics/IntRectHash.h: Added.
    
            * platform/graphics/IntSizeHash.h: Don't do "using WebCore::IntSize"!
    
    
    
    Canonical link: https://commits.webkit.org/66884@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@76660 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/IntSizeHash.h
+++ Source/WebCore/platform/graphics/IntSizeHash.h
@@ -24,22 +24,20 @@
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
 
-using WebCore::IntSize;
-
 namespace WTF {
 
-    template<> struct IntHash<IntSize> {
-        static unsigned hash(const IntSize& key) { return intHash((static_cast<uint64_t>(key.width()) << 32 | key.height())); }
-        static bool equal(const IntSize& a, const IntSize& b) { return a == b; }
+    template<> struct IntHash<WebCore::IntSize> {
+        static unsigned hash(const WebCore::IntSize& key) { return intHash((static_cast<uint64_t>(key.width()) << 32 | key.height())); }
+        static bool equal(const WebCore::IntSize& a, const WebCore::IntSize& b) { return a == b; }
         static const bool safeToCompareToEmptyOrDeleted = true;
     };
-    template<> struct DefaultHash<IntSize> { typedef IntHash<IntSize> Hash; };
+    template<> struct DefaultHash<WebCore::IntSize> { typedef IntHash<WebCore::IntSize> Hash; };
     
-    template<> struct HashTraits<IntSize> : GenericHashTraits<IntSize> {
+    template<> struct HashTraits<WebCore::IntSize> : GenericHashTraits<WebCore::IntSize> {
         static const bool emptyValueIsZero = true;
         static const bool needsDestruction = false;
-        static void constructDeletedValue(IntSize& slot) { new (&slot) IntSize(-1, -1); }
-        static bool isDeletedValue(const IntSize& value) { return value.width() == -1 && value.height() == -1; }
+        static void constructDeletedValue(WebCore::IntSize& slot) { new (&slot) WebCore::IntSize(-1, -1); }
+        static bool isDeletedValue(const WebCore::IntSize& value) { return value.width() == -1 && value.height() == -1; }
     };
 } // namespace WTF
 
