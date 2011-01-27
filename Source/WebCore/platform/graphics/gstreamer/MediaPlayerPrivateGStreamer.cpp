7b4a2243ac98b6b955b5706bebd7ea7cac58855a
Philippe Normand <philn@igalia.com>
2011-01-27 08:55:04 +0000

    2011-01-26  Philippe Normand  <pnormand@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] LayoutTests/media/audio-mpeg4-supported.html fails
            https://bugs.webkit.org/show_bug.cgi?id=53125
    
            * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
            (WebCore::mimeTypeCache): Add audio/x-m4a mimetype in the cache.
    
    2011-01-26  Philippe Normand  <pnormand@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] LayoutTests/media/audio-mpeg4-supported.html fails
            https://bugs.webkit.org/show_bug.cgi?id=53125
    
            * platform/gtk/Skipped: Unskip fixed test.
    
    Canonical link: https://commits.webkit.org/66991@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@76773 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -1384,11 +1384,15 @@ static HashSet<String> mimeTypeCache()
 
                 // These formats are supported by GStreamer, but not
                 // correctly advertised.
-                if (g_str_equal(name, "video/x-h264")
-                    || g_str_equal(name, "audio/x-m4a")) {
+                if (g_str_equal(name, "video/x-h264")) {
                     cache.add(String("video/mp4"));
+                    cached = true;
+                }
+
+                if (g_str_equal(name, "audio/x-m4a")) {
                     cache.add(String("audio/aac"));
                     cache.add(String("audio/mp4"));
+                    cache.add(String("audio/x-m4a"));
                     cached = true;
                 }
 
