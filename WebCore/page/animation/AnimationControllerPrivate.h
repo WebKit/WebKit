699887dee5cc34d4723b7e19d6f078e02e6068cf
Mark Rowe <mrowe@apple.com>
2011-01-28 01:52:41 +0000

    Merge r74286.
    Canonical link: https://commits.webkit.org/50175.380@safari-533.20-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-533.20-branch@76884 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/page/animation/AnimationControllerPrivate.h
+++ WebCore/page/animation/AnimationControllerPrivate.h
@@ -54,11 +54,10 @@ public:
     AnimationControllerPrivate(Frame*);
     ~AnimationControllerPrivate();
 
+    void updateAnimationTimer(bool callSetChanged = false);
+
     PassRefPtr<CompositeAnimation> accessCompositeAnimation(RenderObject*);
     bool clear(RenderObject*);
-
-    void animationTimerFired(Timer<AnimationControllerPrivate>*);
-    void updateAnimationTimer(bool callSetChanged = false);
 
     void updateStyleIfNeededDispatcherFired(Timer<AnimationControllerPrivate>*);
     void startUpdateStyleIfNeededDispatcher();
@@ -88,11 +87,13 @@ public:
     
     void addToStartTimeResponseWaitList(AnimationBase*, bool willGetResponse);
     void removeFromStartTimeResponseWaitList(AnimationBase*);    
-    void startTimeResponse(double t);
     
 private:
+    void animationTimerFired(Timer<AnimationControllerPrivate>*);
+
     void styleAvailable();
     void fireEventsAndUpdateStyle();
+    void startTimeResponse(double t);
 
     typedef HashMap<RenderObject*, RefPtr<CompositeAnimation> > RenderObjectAnimationMap;
 
@@ -116,9 +117,9 @@ private:
     AnimationBase* m_styleAvailableWaiters;
     AnimationBase* m_lastStyleAvailableWaiter;
     
-    AnimationBase* m_responseWaiters;
-    AnimationBase* m_lastResponseWaiter;
-    bool m_waitingForResponse;
+    AnimationBase* m_startTimeResponseWaiters;
+    AnimationBase* m_lastStartTimeResponseWaiter;
+    bool m_waitingForStartTimeResponse;
 };
 
 } // namespace WebCore
