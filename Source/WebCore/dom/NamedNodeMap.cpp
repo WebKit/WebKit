e366f134c55ed8f9610af7b6d0f295ac98c7a749
Antti Koivisto <antti@apple.com>
2011-01-29 00:23:39 +0000

    CSS styles are shared based on uninitialized property values https://bugs.webkit.org/show_bug.cgi?id=53285
    Reviewed by Simon Fraser.
    
    Null test.
    
    * dom/NamedNodeMap.cpp:
    (WebCore::NamedNodeMap::mappedMapsEquivalent):
    
    
    
    Canonical link: https://commits.webkit.org/67177@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77019 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/NamedNodeMap.cpp
+++ Source/WebCore/dom/NamedNodeMap.cpp
@@ -357,7 +357,7 @@ bool NamedNodeMap::mappedMapsEquivalent(const NamedNodeMap* otherMap) const
             ASSERT(attr->isMappedAttribute());
 
             Attribute* otherAttr = otherMap->getAttributeItem(attr->name());
-            if (!otherAttr || attr->value() != otherAttr->value())
+            if (!otherAttr || !otherAttr->decl() || attr->value() != otherAttr->value())
                 return false;
             if (!attr->decl()->propertiesEqual(otherAttr->decl()))
                 return false;
