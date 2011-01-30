de35b4ab2c590f9663e75d301ab5c48a6d678e28
Commit Queue <commit-queue@webkit.org>
2011-01-30 11:54:48 +0000

    2011-01-30  Leo Yang  <leo.yang@torchmobile.com.cn>
            Reviewed by Daniel Bates.
    
            Code style issue in JavaScriptCore/wtf/CurrentTime.h
            https://bugs.webkit.org/show_bug.cgi?id=53394
    
            According to rule #3 at http://webkit.org/coding/coding-style.html,
            This patch fix style issue in CurrentTime.h.
    
            No functionality change, no new tests.
    
            * wtf/CurrentTime.h:
            (WTF::currentTimeMS):
            (WTF::getLocalTime):
    
    Canonical link: https://commits.webkit.org/67240@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77087 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/wtf/CurrentTime.h
+++ Source/JavaScriptCore/wtf/CurrentTime.h
@@ -36,27 +36,27 @@
 
 namespace WTF {
 
-    // Returns the current UTC time in seconds, counted from January 1, 1970.
-    // Precision varies depending on platform but is usually as good or better 
-    // than a millisecond.
-    double currentTime();
+// Returns the current UTC time in seconds, counted from January 1, 1970.
+// Precision varies depending on platform but is usually as good or better
+// than a millisecond.
+double currentTime();
 
-    // Same thing, in milliseconds.
-    inline double currentTimeMS()
-    {
-        return currentTime() * 1000.0; 
-    }
+// Same thing, in milliseconds.
+inline double currentTimeMS()
+{
+    return currentTime() * 1000.0;
+}
 
-    inline void getLocalTime(const time_t* localTime, struct tm* localTM)
-    {
-    #if COMPILER(MSVC7_OR_LOWER) || COMPILER(MINGW) || OS(WINCE)
-        *localTM = *localtime(localTime);
-    #elif COMPILER(MSVC)
-        localtime_s(localTM, localTime);
-    #else
-        localtime_r(localTime, localTM);
-    #endif
-    }
+inline void getLocalTime(const time_t* localTime, struct tm* localTM)
+{
+#if COMPILER(MSVC7_OR_LOWER) || COMPILER(MINGW) || OS(WINCE)
+    *localTM = *localtime(localTime);
+#elif COMPILER(MSVC)
+    localtime_s(localTM, localTime);
+#else
+    localtime_r(localTime, localTM);
+#endif
+}
 
 } // namespace WTF
 
