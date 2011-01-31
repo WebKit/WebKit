c491df30278437b8a938b59bd176198545cac42b
Jessie Berlin <jberlin@webkit.org>
2011-01-31 19:25:46 +0000

    WebKit2: the cookies used by the WebKit2 Web Process should not go in a top-level directory. https://bugs.webkit.org/show_bug.cgi?id=53278
    Reviewed by Steve Falkenburg.
    
    Use the name WebKit2WebProcess since the default location for the cookies is determined
    based on the process name (which is WebKit2WebProcess.exe on Windows).
    
    * win/WebKit2.make:
    Copy the WebKit2WebProcess.resources directory into the AppleInternal bin.
    
    * win/WebKit2WebProcess.resources: Added.
    * win/WebKit2WebProcess.resources/Info.plist: Added.
    Copied from the WebKit version of Info.plist.
    
    * win/WebKit2WebProcessPostBuild.cmd:
    Copy the WebKit2WebProcess.resources directory into the WebKitOutPutDir's bin.
    
    
    
    Canonical link: https://commits.webkit.org/67298@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77147 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/win/WebKit2WebProcessPostBuild.cmd
+++ Source/WebKit2/win/WebKit2WebProcessPostBuild.cmd
@@ -1,1 +1,4 @@
+mkdir 2>NUL "%OUTDIR%\..\bin\WebKit2WebProcess.resources"
+xcopy /y /d "%PROJECTDIR%\WebKit2WebProcess.resources\*" "%OUTDIR%\..\bin\WebKit2WebProcess.resources"
+
 if exist "%CONFIGURATIONBUILDDIR%\buildfailed" del "%CONFIGURATIONBUILDDIR%\buildfailed"
