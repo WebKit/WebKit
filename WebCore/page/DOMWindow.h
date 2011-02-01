c7e2b31f70d9d7afec0b863b9215bca1a54025bd
Mark Rowe <mrowe@apple.com>
2011-02-01 21:26:56 +0000

    Merge r74854.
    Canonical link: https://commits.webkit.org/50175.454@safari-533.20-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-533.20-branch@77280 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/page/DOMWindow.h
+++ WebCore/page/DOMWindow.h
@@ -133,7 +133,7 @@ namespace WebCore {
 
         void focus();
         void blur();
-        void close();
+        void close(ScriptExecutionContext* = 0);
         void print();
         void stop();
 
