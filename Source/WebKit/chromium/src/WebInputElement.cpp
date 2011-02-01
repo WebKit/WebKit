WebKit/WebKit

commit 361d4543805be04abb128c8b461d3dda8bb8ed36

2011-02-01  Naoki Takano  <takano.naoki@gmail.com>
        Reviewed by Darin Fisher.

        [Chromium] Autofill should work with HTML5 form elements
        https://bugs.webkit.org/show_bug.cgi?id=51809
        http://crbug.com/65654

        No new tests, because this fix is for Chromium project and hard to test only in WebKit project.

        * html/InputType.h: Insert comment for canSetSuggestedValue().
        * html/TextFieldInputType.cpp:
        (WebCore::TextFieldInputType::canSetSuggestedValue): Implemented to return always true for that all text filed inputs can be completed.
        * html/TextFieldInputType.h: Declare canSetSuggestedValue().
        * html/TextInputType.cpp: Delete canSetSuggestedValue() not to return true anymore.
        * html/TextInputType.h: Delete canSetSuggestedValue() not to return true anymore.
2011-02-01  Naoki Takano  <takano.naoki@gmail.com>

        Reviewed by Darin Fisher.

        [Chromium] Autofill should work with HTML5 form elements
        https://bugs.webkit.org/show_bug.cgi?id=51809
        http://crbug.com/65654

        No new tests, because this fix is for Chromium project and hard to test only in WebKit project.

        * public/WebInputElement.h: Added toWebInputElement() declarations and implemented const version of toWebInputElement().
        * src/WebInputElement.cpp:
        (WebKit::WebInputElement::toWebInputElement): Implemented a convert function to cast no-const WebElement* to no-const WebInputElement*. Because Chrome needs safe cast for autofill completion.

Canonical link: https://commits.webkit.org/67342@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77228 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/chromium/src/WebInputElement.cpp
@@ -173,4 +173,15 @@ WebInputElement::operator PassRefPtr<HTMLInputElement>() const
     return static_cast<HTMLInputElement*>(m_private.get());
 }
 
+WebInputElement* toWebInputElement(WebElement* webElement)
+{
+    InputElement* inputElement = toInputElement(webElement->unwrap<Element>());
+    if (!inputElement)
+        return 0;
+
+    ASSERT(inputElement->isHTMLElement());
+
+    return static_cast<WebInputElement*>(webElement);
+}
+
 } // namespace WebKit

Source/WebCore/html/InputType.h
@@ -192,6 +192,7 @@ public:
     virtual void willMoveToNewOwnerDocument();
     virtual bool shouldRespectAlignAttribute();
     virtual FileList* files();
+    // Should return true if the corresponding renderer for a type can display a suggested value.
     virtual bool canSetSuggestedValue();
     virtual bool shouldSendChangeEventAfterCheckedChanged();
     virtual bool canSetValue(const String&);

Source/WebCore/html/TextFieldInputType.cpp
@@ -52,6 +52,11 @@ bool TextFieldInputType::valueMissing(const String& value) const
     return value.isEmpty();
 }
 
+bool TextFieldInputType::canSetSuggestedValue()
+{
+    return true;
+}
+
 void TextFieldInputType::handleKeydownEvent(KeyboardEvent* event)
 {
     if (!element()->focused())

Source/WebCore/html/TextFieldInputType.h
@@ -40,6 +40,7 @@ namespace WebCore {
 class TextFieldInputType : public InputType {
 protected:
     TextFieldInputType(HTMLInputElement* element) : InputType(element) { }
+    virtual bool canSetSuggestedValue();
     virtual void handleKeydownEvent(KeyboardEvent*);
     void handleKeydownEventForSpinButton(KeyboardEvent*);
     void handleWheelEventForSpinButton(WheelEvent*);

Source/WebCore/html/TextInputType.cpp
@@ -45,12 +45,6 @@ const AtomicString& TextInputType::formControlType() const
     return InputTypeNames::text();
 }
 
-bool TextInputType::canSetSuggestedValue()
-{
-    // FIXME: Should this really be restricted to plain text? What about search, for example?
-    return true;
-}
-
 bool TextInputType::shouldRespectSpeechAttribute()
 {
     return true;

Source/WebCore/html/TextInputType.h
@@ -42,7 +42,6 @@ public:
 private:
     TextInputType(HTMLInputElement* element) : BaseTextInputType(element) { }
     virtual const AtomicString& formControlType() const;
-    virtual bool canSetSuggestedValue();
     virtual bool shouldRespectSpeechAttribute();
 };
 

Source/WebKit/chromium/public/WebInputElement.h
@@ -89,6 +89,13 @@ namespace WebKit {
 #endif
     };
 
+    WEBKIT_API WebInputElement* toWebInputElement(WebElement*);
+
+    inline const WebInputElement* toWebInputElement(const WebElement* element)
+    {
+        return toWebInputElement(const_cast<WebElement*>(element));
+    }
+
 } // namespace WebKit
 
 #endif

commit 07204974bd5be8914cacbe4233e070943ab54d6d

2011-02-01  Yury Semikhatsky  <yurys@chromium.org>
        Unreviewed. Fix Chromium Debug compilation broken by r77228.

        [Chromium] Autofill should work with HTML5 form elements
        https://bugs.webkit.org/show_bug.cgi?id=51809

        * src/WebInputElement.cpp:
        (WebKit::toWebInputElement):


Canonical link: https://commits.webkit.org/67346@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77232 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/chromium/src/WebInputElement.cpp
@@ -179,7 +179,7 @@ WebInputElement* toWebInputElement(WebElement* webElement)
     if (!inputElement)
         return 0;
 
-    ASSERT(inputElement->isHTMLElement());
+    ASSERT(webElement->unwrap<Element>()->isHTMLElement());
 
     return static_cast<WebInputElement*>(webElement);
 }