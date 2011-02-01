99758a854bf355e7e54acbca83b799e7be9f1067
Mark Rowe <mrowe@apple.com>
2011-02-01 22:59:11 +0000

    Merge r73559.
    Canonical link: https://commits.webkit.org/50175.468@safari-533.20-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-533.20-branch@77318 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/dom/Document.h
+++ WebCore/dom/Document.h
@@ -619,6 +619,7 @@ public:
 
     void attachNodeIterator(NodeIterator*);
     void detachNodeIterator(NodeIterator*);
+    void moveNodeIteratorsToNewDocument(Node*, Document*);
 
     void attachRange(Range*);
     void detachRange(Range*);
