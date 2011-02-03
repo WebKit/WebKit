15f80da66cae96f3915cb65f3a169b79d3d9bee4
Patrick Gansterer <paroga@webkit.org>
2011-02-03 01:14:04 +0000

    2011-02-02  Patrick Gansterer  <paroga@webkit.org>
            Unreviewed WinCE build fix for r77397.
    
            * page/wince/FrameWinCE.cpp:
            (WebCore::computePageRectsForFrame):
    
    Canonical link: https://commits.webkit.org/67522@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77438 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/wince/FrameWinCE.cpp
+++ Source/WebCore/page/wince/FrameWinCE.cpp
@@ -76,7 +76,7 @@ void computePageRectsForFrame(Frame* frame, const IntRect& printRect, float head
 
     float ratio = (float)printRect.height() / (float)printRect.width();
 
-    float pageWidth  = (float) root->rightLayoutOverflow();
+    float pageWidth  = (float) root->maxXLayoutOverflow();
     float pageHeight = pageWidth * ratio;
     outPageHeight = (int) pageHeight;   // this is the height of the page adjusted by margins
     pageHeight -= (headerHeight + footerHeight);
