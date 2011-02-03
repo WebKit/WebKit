d65e015a69a795b925755d4d66c40237346b15d2
Stephanie Lewis <slewis@apple.com>
2011-02-03 01:58:03 +0000

    https://bugs.webkit.org/show_bug.cgi?id=53361 <rdar://problem/8921729> REGRESSION: Membuster accumulates 1GB+ memory due to autorelease pools not being drained. Call into AppKit to add observers that push and pop autorelease pools on entry and exit from the CFRunLoop.  This will prevent memory from accumulating.
    Reviewed by Geoff Garen.
    
    * WebProcess/mac/WebProcessMainMac.mm:
    (WebKit::WebProcessMain):
    
    
    
    Canonical link: https://commits.webkit.org/67535@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77452 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/mac/WebProcessMainMac.mm
+++ Source/WebKit2/WebProcess/mac/WebProcessMainMac.mm
@@ -46,6 +46,10 @@
 // FIXME: We should be doing this another way.
 extern "C" kern_return_t bootstrap_look_up2(mach_port_t, const name_t, mach_port_t*, pid_t, uint64_t);
 
+@interface NSApplication (WebNSApplicationDetails)
+-(void)_installAutoreleasePoolsOnCurrentThreadIfNecessary;
+@end
+
 #define SHOW_CRASH_REPORTER 1
 
 using namespace WebCore;
@@ -88,6 +92,10 @@ int WebProcessMain(const CommandLine& commandLine)
 
      // Initialize AppKit.
     [NSApplication sharedApplication];
+
+    // Installs autorelease pools on the current CFRunLoop which prevents memory from accumulating between user events.
+    // FIXME: Remove when <rdar://problem/8929426> is fixed.
+    [[NSApplication sharedApplication] _installAutoreleasePoolsOnCurrentThreadIfNecessary];
 
 #if !defined(BUILDING_ON_SNOW_LEOPARD)
     WKAXRegisterRemoteApp();
