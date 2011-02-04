36bed992436862b78065b82b00be49ad5e9ca63d
Steve Falkenburg <sfalken@apple.com>
2011-02-04 20:35:42 +0000

    Windows build fix. Correct prebuild build-stopping rule to allow project only rebuilds.
    * win/WebKit2WebProcessPreBuild.cmd:
    
    
    
    Canonical link: https://commits.webkit.org/67727@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77656 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/win/WebKit2WebProcessPreBuild.cmd
+++ Source/WebKit2/win/WebKit2WebProcessPreBuild.cmd
@@ -3,6 +3,6 @@ if errorlevel 1 set PATH=%SystemDrive%\cygwin\bin;%PATH%
 cmd /c
 if exist "%CONFIGURATIONBUILDDIR%\buildfailed" grep XX%PROJECTNAME%XX "%CONFIGURATIONBUILDDIR%\buildfailed"
 if errorlevel 1 exit 1
-echo XX$%PROJECTNAME%XX > "%CONFIGURATIONBUILDDIR%\buildfailed"
+echo XX%PROJECTNAME%XX > "%CONFIGURATIONBUILDDIR%\buildfailed"
 
 bash "%WEBKITLIBRARIESDIR%\tools\scripts\auto-version.sh" "%INTDIR%"
