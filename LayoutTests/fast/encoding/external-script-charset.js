7481d253c5723ec9366ea5202240fe63fbb0af57
Patrick Gansterer <paroga@webkit.org>
2011-02-05 20:30:15 +0000

    2011-02-05  Patrick Gansterer  <paroga@webkit.org>
            Reviewed by Alexey Proskuryakov.
    
            Add addition test for inherited encoding
            https://bugs.webkit.org/show_bug.cgi?id=53398
    
            Check if the right encoding is used, after document.charset was set.
    
            * fast/dom/Document/document-charset.html: Added doctype to make it pass in IE9 out of the box.
            * fast/encoding/css-charset-default-expected.txt:
            * fast/encoding/css-charset-default.xhtml:
            * fast/encoding/external-script-charset-expected.txt:
            * fast/encoding/external-script-charset-koi-8.js: Added.
            * fast/encoding/external-script-charset.js:
            * fast/encoding/external-script-charset.xhtml:
    
    Canonical link: https://commits.webkit.org/67814@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77749 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/encoding/external-script-charset.js
+++ LayoutTests/fast/encoding/external-script-charset.js
@@ -1,4 +1,4 @@
 if (window.layoutTestController)
     layoutTestController.dumpAsText();
 
-document.getElementById("result").innerHTML = ("Я" == "\u042F") ? "PASS" : "FAIL";
+document.getElementById("result1").innerHTML += ("Я" == "\u042F") ? "PASS" : "FAIL";
