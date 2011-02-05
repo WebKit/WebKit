WebKit/WebKit

commit c2b8276802173391c0c0324512a69054c8cad202

2011-02-04  Adam Barth  <abarth@webkit.org>
        Reviewed by Eric Seidel.

        PluginDocuments don't create widgets for plugins on back/forward
        https://bugs.webkit.org/show_bug.cgi?id=53474

        Test that plugin loads when in a plugin document on back-forward.

        * plugins/plugin-document-back-forward-expected.txt: Added.
        * plugins/plugin-document-back-forward.html: Added.
2011-02-04  Adam Barth  <abarth@webkit.org>

        Reviewed by Eric Seidel.

        PluginDocuments don't create widgets for plugins on back/forward
        https://bugs.webkit.org/show_bug.cgi?id=53474

        Long ago, PluginDocument always caused the HTMLEmbedElement to create
        its widget synchronously during a post-layout task.  Recently, however,
        some changes to the HistroyController caused layout on back/forward to
        become slightly more complicated (and added an extra level of recursion
        to layout).  This extra level of recursion triggered the "I've recursed
        too many times" condition in the post-layout task queue, causing the
        FrameView to run the remainder of the tasks asynchronously.
        Unfortunately, that broke PluginDocument because it needs its the
        HTMLEmbedElement's updateWidget task to run synchronously.

        This patch adds a mechanism for "kicking off" the pending post-layout
        tasks synchronously (instead of waiting for the timer to fire).
        PluginDocument then uses that facility to ensure that the
        HTMLEmbedElement's updateWidget task happens.

        Test: plugins/plugin-document-back-forward.html

        * html/PluginDocument.cpp:
        (WebCore::PluginDocumentParser::appendBytes):
        * page/FrameView.cpp:
        (WebCore::FrameView::flushAnyPendingPostLayoutTasks):
        * page/FrameView.h:
2011-02-04  Adam Barth  <abarth@webkit.org>

        Reviewed by Eric Seidel.

        PluginDocuments don't create widgets for plugins on back/forward

LayoutTests/plugins/plugin-document-back-forward-expected.txt
@@ -1,0 +1,3 @@
+ALERT: Plugin Loaded!
+ALERT: Plugin Loaded!
+

LayoutTests/plugins/plugin-document-back-forward.html
@@ -1,0 +1,44 @@
+<html>
+<script>
+if (window.layoutTestController) {
+    layoutTestController.waitUntilDone();
+    layoutTestController.dumpAsText();
+    layoutTestController.dumpChildFramesAsText();
+}
+
+state = "initial";
+
+function next() {
+    switch (state) {
+    case "initial":
+        state = "loading-plugin";
+        var frame = document.getElementsByTagName("iframe")[0]
+        frame.addEventListener("load", next, false);
+        frame.src = "data:application/x-webkit-test-netscape,alertwhenloaded";
+        break;
+    case "loading-plugin":
+        state = "moving-backward";
+        history.back();
+        break;
+    case "moving-backward":
+        state = "moving-forward";
+        history.forward();
+        break;
+    case "moving-forward":
+        state = "done";
+        layoutTestController.notifyDone();
+        break;
+    default:
+        alert('FAIL');
+        break;
+    }
+}
+
+window.onload = function() {
+    // We need to wait for after the load event so that we get a new history
+    // entry when navigating the frame.
+    setTimeout(next, 0);
+}
+</script>
+<iframe src="data:text/html,Waiting..."></iframe>
+</html>

Source/WebCore/html/PluginDocument.cpp
@@ -28,6 +28,7 @@
 #include "DocumentLoader.h"
 #include "Frame.h"
 #include "FrameLoaderClient.h"
+#include "FrameView.h"
 #include "HTMLEmbedElement.h"
 #include "HTMLHtmlElement.h"
 #include "HTMLNames.h"
@@ -114,6 +115,13 @@ void PluginDocumentParser::appendBytes(DocumentWriter*, const char*, int, bool)
         return;
 
     document()->updateLayout();
+
+    // Below we assume that renderer->widget() to have been created by
+    // document()->updateLayout(). However, in some cases, updateLayout() will 
+    // recurse too many times and delay its post-layout tasks (such as creating
+    // the widget). Here we kick off the pending post-layout tasks so that we
+    // can synchronously redirect data to the plugin.
+    frame->view()->flushAnyPendingPostLayoutTasks();
 
     if (RenderPart* renderer = m_embedElement->renderPart()) {
         if (Widget* widget = renderer->widget()) {

Source/WebCore/page/FrameView.cpp
@@ -1835,7 +1835,16 @@ bool FrameView::updateWidgets()
     
     return m_widgetUpdateSet->isEmpty();
 }
-    
+
+void FrameView::flushAnyPendingPostLayoutTasks()
+{
+    if (!m_hasPendingPostLayoutTasks)
+        return;
+
+    m_postLayoutTasksTimer.stop();
+    performPostLayoutTasks();
+}
+
 void FrameView::performPostLayoutTasks()
 {
     m_hasPendingPostLayoutTasks = false;

Source/WebCore/page/FrameView.h
@@ -258,6 +258,9 @@ public:
 
     virtual IntPoint currentMousePosition() const;
 
+    // FIXME: Remove this method once plugin loading is decoupled from layout.
+    void flushAnyPendingPostLayoutTasks();
+
 protected:
     virtual bool scrollContentsFastPath(const IntSize& scrollDelta, const IntRect& rectToScroll, const IntRect& clipRect);
     virtual void scrollContentsSlowPath(const IntRect& updateRect);

Tools/DumpRenderTree/TestNetscapePlugIn/main.cpp
@@ -200,6 +200,9 @@ NPError NPP_New(NPMIMEType pluginType, NPP instance, uint16_t mode, int16_t argc
         else if (strcasecmp(argn[i], "src") == 0 &&
                  strcasecmp(argv[i], "data:application/x-webkit-test-netscape,returnerrorfromnewstream") == 0)
             obj->returnErrorFromNewStream = TRUE;
+        else if (strcasecmp(argn[i], "src") == 0 &&
+                 strcasecmp(argv[i], "data:application/x-webkit-test-netscape,alertwhenloaded") == 0)
+            executeScript(obj, "alert('Plugin Loaded!')");
         else if (strcasecmp(argn[i], "onSetWindow") == 0 && !obj->onSetWindow)
             obj->onSetWindow = strdup(argv[i]);
         else if (strcasecmp(argn[i], "onNew") == 0 && !onNewScript)

commit 3ee2c2a57741161d5a8f310ef1b5e7895b2be386

[Windows 7 Release Tests] plugins/plugin-document-back-forward.html failing since introduction in r77706. https://bugs.webkit.org/show_bug.cgi?id=53831
Unreviewed. Add expected (failing?) results to get the Windows 7 Release Test bots green.

* platform/win/plugins/plugin-document-back-forward-expected.txt: Added.



Canonical link: https://commits.webkit.org/67786@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77718 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/win/plugins/plugin-document-back-forward-expected.txt
@@ -1,0 +1,8 @@
+ALERT: Plugin Loaded!
+ALERT: Plugin Loaded!
+
+
+--------
+Frame: '<!--framePath //<!--frame0-->-->'
+--------
+