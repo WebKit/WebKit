b3028a4adea8fda553367a84a62da22e926a0a1c
Commit Queue <commit-queue@webkit.org>
2011-02-06 23:48:37 +0000

    2011-02-06  Benjamin Poulain  <ikipou@gmail.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [Qt] Fix coding style errors in ClipboardQt.h
            https://bugs.webkit.org/show_bug.cgi?id=43520
    
            Fix coding style: order of header and indentation.
    
            * platform/qt/ClipboardQt.h:
            (WebCore::ClipboardQt::create):
            (WebCore::ClipboardQt::clipboardData):
            (WebCore::ClipboardQt::invalidateWritableData):
    
    Canonical link: https://commits.webkit.org/67841@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77779 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/qt/ClipboardQt.h
+++ Source/WebCore/platform/qt/ClipboardQt.h
@@ -26,8 +26,8 @@
 #ifndef ClipboardQt_h
 #define ClipboardQt_h
 
-#include "Clipboard.h"
 #include "CachedResourceClient.h"
+#include "Clipboard.h"
 
 QT_BEGIN_NAMESPACE
 class QMimeData;
@@ -35,56 +35,56 @@ QT_END_NAMESPACE
 
 namespace WebCore {
 
-    class CachedImage;
+class CachedImage;
 
-    // State available during IE's events for drag and drop and copy/paste
-    class ClipboardQt : public Clipboard, public CachedResourceClient {
-        WTF_MAKE_FAST_ALLOCATED;
-    public:
-        static PassRefPtr<ClipboardQt> create(ClipboardAccessPolicy policy, const QMimeData* readableClipboard)
-        {
-            return adoptRef(new ClipboardQt(policy, readableClipboard));
-        }
-        static PassRefPtr<ClipboardQt> create(ClipboardAccessPolicy policy, ClipboardType clipboardType = CopyAndPaste)
-        {
-            return adoptRef(new ClipboardQt(policy, clipboardType));
-        }
-        virtual ~ClipboardQt();
+// State available during IE's events for drag and drop and copy/paste
+class ClipboardQt : public Clipboard, public CachedResourceClient {
+    WTF_MAKE_FAST_ALLOCATED;
+public:
+    static PassRefPtr<ClipboardQt> create(ClipboardAccessPolicy policy, const QMimeData* readableClipboard)
+    {
+        return adoptRef(new ClipboardQt(policy, readableClipboard));
+    }
+    static PassRefPtr<ClipboardQt> create(ClipboardAccessPolicy policy, ClipboardType clipboardType = CopyAndPaste)
+    {
+        return adoptRef(new ClipboardQt(policy, clipboardType));
+    }
+    virtual ~ClipboardQt();
 
-        void clearData(const String& type);
-        void clearAllData();
-        String getData(const String& type, bool& success) const;
-        bool setData(const String& type, const String& data);
+    void clearData(const String& type);
+    void clearAllData();
+    String getData(const String& type, bool& success) const;
+    bool setData(const String& type, const String& data);
 
-        // extensions beyond IE's API
-        virtual HashSet<String> types() const;
-        virtual PassRefPtr<FileList> files() const;
+    // extensions beyond IE's API
+    virtual HashSet<String> types() const;
+    virtual PassRefPtr<FileList> files() const;
 
-        void setDragImage(CachedImage*, const IntPoint&);
-        void setDragImageElement(Node*, const IntPoint&);
+    void setDragImage(CachedImage*, const IntPoint&);
+    void setDragImageElement(Node*, const IntPoint&);
 
-        virtual DragImageRef createDragImage(IntPoint& dragLoc) const;
-        virtual void declareAndWriteDragImage(Element*, const KURL&, const String& title, Frame*);
-        virtual void writeURL(const KURL&, const String&, Frame*);
-        virtual void writeRange(Range*, Frame*);
-        virtual void writePlainText(const String&);
+    virtual DragImageRef createDragImage(IntPoint& dragLoc) const;
+    virtual void declareAndWriteDragImage(Element*, const KURL&, const String& title, Frame*);
+    virtual void writeURL(const KURL&, const String&, Frame*);
+    virtual void writeRange(Range*, Frame*);
+    virtual void writePlainText(const String&);
 
-        virtual bool hasData();
+    virtual bool hasData();
 
-        QMimeData* clipboardData() const { return m_writableData; }
-        void invalidateWritableData() { m_writableData = 0; }
+    QMimeData* clipboardData() const { return m_writableData; }
+    void invalidateWritableData() { m_writableData = 0; }
 
-    private:
-        ClipboardQt(ClipboardAccessPolicy, const QMimeData* readableClipboard);
+private:
+    ClipboardQt(ClipboardAccessPolicy, const QMimeData* readableClipboard);
 
-        // Clipboard is writable so it will create its own QMimeData object
-        ClipboardQt(ClipboardAccessPolicy, ClipboardType);
+    // Clipboard is writable so it will create its own QMimeData object
+    ClipboardQt(ClipboardAccessPolicy, ClipboardType);
 
-        void setDragImage(CachedImage*, Node*, const IntPoint& loc);
+    void setDragImage(CachedImage*, Node*, const IntPoint& loc);
 
-        const QMimeData* m_readableData;
-        QMimeData* m_writableData;
-    };
+    const QMimeData* m_readableData;
+    QMimeData* m_writableData;
+};
 }
 
 #endif // ClipboardQt_h
