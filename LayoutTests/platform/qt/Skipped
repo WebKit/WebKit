WebKit/WebKit

commit ef4a67e42976462033c48d9a04101ed1577028c9

2011-02-05  Nate Chapin  <japhet@chromium.org>
        Reviewed by Adam Barth.

        Refactor: NotificationCenter shouldn't hold its own copy of the ScriptExecutionContext*
        when it inherits one from ActiveDOMObject.
        https://bugs.webkit.org/show_bug.cgi?id=53815

        * bindings/js/JSDesktopNotificationsCustom.cpp:
        (WebCore::JSNotificationCenter::requestPermission):
        * bindings/v8/custom/V8NotificationCenterCustom.cpp:
        (WebCore::V8NotificationCenter::requestPermissionCallback):
        * notifications/NotificationCenter.cpp:
        (WebCore::NotificationCenter::NotificationCenter):
        (WebCore::NotificationCenter::checkPermission):
        (WebCore::NotificationCenter::requestPermission):
        (WebCore::NotificationCenter::disconnectFrame):
        * notifications/NotificationCenter.h:
        (WebCore::NotificationCenter::createHTMLNotification):
        (WebCore::NotificationCenter::createNotification):

Canonical link: https://commits.webkit.org/67803@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77738 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/bindings/js/JSDesktopNotificationsCustom.cpp
@@ -47,7 +47,7 @@ namespace WebCore {
 
 JSValue JSNotificationCenter::requestPermission(ExecState* exec)
 {
-    ScriptExecutionContext* context = impl()->context();
+    ScriptExecutionContext* context = impl()->scriptExecutionContext();
 
     // Make sure that script execution context is valid.
     if (!context) {

Source/WebCore/bindings/v8/custom/V8NotificationCenterCustom.cpp
@@ -81,7 +81,7 @@ v8::Handle<v8::Value> V8NotificationCenter::requestPermissionCallback(const v8::
 {
     INC_STATS(L"DOM.NotificationCenter.RequestPermission()");
     NotificationCenter* notificationCenter = V8NotificationCenter::toNative(args.Holder());
-    ScriptExecutionContext* context = notificationCenter->context();
+    ScriptExecutionContext* context = notificationCenter->scriptExecutionContext();
 
     // Make sure that script execution context is valid.
     if (!context)

Source/WebCore/notifications/NotificationCenter.cpp
@@ -42,21 +42,20 @@ namespace WebCore {
 
 NotificationCenter::NotificationCenter(ScriptExecutionContext* context, NotificationPresenter* presenter)
     : ActiveDOMObject(context, this)
-    , m_scriptExecutionContext(context)
     , m_notificationPresenter(presenter) {}
 
 int NotificationCenter::checkPermission()
 {
-    if (!presenter())
+    if (!presenter() || !scriptExecutionContext())
         return NotificationPresenter::PermissionDenied;
-    return m_notificationPresenter->checkPermission(m_scriptExecutionContext);
+    return m_notificationPresenter->checkPermission(scriptExecutionContext());
 }
 
 void NotificationCenter::requestPermission(PassRefPtr<VoidCallback> callback)
 {
-    if (!presenter())
+    if (!presenter() || !scriptExecutionContext())
         return;
-    m_notificationPresenter->requestPermission(m_scriptExecutionContext, callback);
+    m_notificationPresenter->requestPermission(scriptExecutionContext(), callback);
 }
 
 void NotificationCenter::disconnectFrame()
@@ -66,9 +65,8 @@ void NotificationCenter::disconnectFrame()
     ASSERT(m_notificationPresenter);
     if (!m_notificationPresenter)
         return;
-    m_notificationPresenter->cancelRequestsForPermission(m_scriptExecutionContext);
+    m_notificationPresenter->cancelRequestsForPermission(scriptExecutionContext());
     m_notificationPresenter = 0;
-    m_scriptExecutionContext = 0;
 }
 
 } // namespace WebCore

Source/WebCore/notifications/NotificationCenter.h
@@ -58,7 +58,7 @@ namespace WebCore {
                 ec = SYNTAX_ERR;
                 return 0;
             }
-            return Notification::create(m_scriptExecutionContext->completeURL(URI), context(), ec, this);
+            return Notification::create(scriptExecutionContext()->completeURL(URI), scriptExecutionContext(), ec, this);
         }
 
         PassRefPtr<Notification> createNotification(const String& iconURI, const String& title, const String& body, ExceptionCode& ec)
@@ -67,11 +67,10 @@ namespace WebCore {
                 ec = INVALID_STATE_ERR;
                 return 0;
             }
-            NotificationContents contents(iconURI.isEmpty() ? KURL() : m_scriptExecutionContext->completeURL(iconURI), title, body);
-            return Notification::create(contents, context(), ec, this);
+            NotificationContents contents(iconURI.isEmpty() ? KURL() : scriptExecutionContext()->completeURL(iconURI), title, body);
+            return Notification::create(contents, scriptExecutionContext(), ec, this);
         }
 
-        ScriptExecutionContext* context() const { return m_scriptExecutionContext; }
         NotificationPresenter* presenter() const { return m_notificationPresenter; }
 
         int checkPermission();
@@ -82,7 +81,6 @@ namespace WebCore {
     private:
         NotificationCenter(ScriptExecutionContext*, NotificationPresenter*);
 
-        ScriptExecutionContext* m_scriptExecutionContext;
         NotificationPresenter* m_notificationPresenter;
     };
 

commit 0ffe3ba7448f490c0682e0a7a220233a5cd3163a

2011-02-06  Robert Hogan  <robert@webkit.org>
        Unreviewed, skip failing test on Qt bot.

        [Qt]  fast/notifications/notifications-document-close-crash.html fails after r77738
        https://bugs.webkit.org/show_bug.cgi?id=53868

        * platform/qt/Skipped: Skip fast/notifications/notifications-document-close-crash.html

Canonical link: https://commits.webkit.org/67826@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77764 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/qt/Skipped
@@ -5016,3 +5016,8 @@ svg/zoom/page/zoom-zoom-coords.xhtml
 # [Qt] media/video-frame-accurate-seek.html fails
 # https://bugs.webkit.org/show_bug.cgi?id=53843
 media/video-frame-accurate-seek.html
+
+# [Qt]  fast/notifications/notifications-document-close-crash.html fails after r77738
+# https://bugs.webkit.org/show_bug.cgi?id=53868
+fast/notifications/notifications-document-close-crash.html
+