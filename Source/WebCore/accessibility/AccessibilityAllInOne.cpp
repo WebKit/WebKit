31d439651abb828f9142a327ba78e6e177279330
Lucas Forschler <lforschler@apple.com>
2011-02-08 02:04:37 +0000

    2011-02-07  Lucas Forschler  <lforschler@apple.com>
            Reviewed by Geoff Garen.
    
            Tr #2, Fix the Leopard 32bit debug build.
    
            No new tests required.
    
            * WebCore.xcodeproj/project.pbxproj:
            * accessibility/AccessibilityAllInOne.cpp:
    
    
    Canonical link: https://commits.webkit.org/67937@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@77878 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/accessibility/AccessibilityAllInOne.cpp
+++ Source/WebCore/accessibility/AccessibilityAllInOne.cpp
@@ -25,22 +25,22 @@
 
 // This all-in-one cpp file cuts down on template bloat to allow us to build our Windows release build.
 
-#include <AXObjectCache.cpp>
-#include <AccessibilityARIAGrid.cpp>
-#include <AccessibilityARIAGridCell.cpp>
-#include <AccessibilityARIAGridRow.cpp>
-#include <AccessibilityImageMapLink.cpp>
-#include <AccessibilityList.cpp>
-#include <AccessibilityListBox.cpp>
-#include <AccessibilityListBoxOption.cpp>
-#include <AccessibilityMediaControls.cpp>
-#include <AccessibilityObject.cpp>
-#include <AccessibilityRenderObject.cpp>
-#include <AccessibilityScrollView.cpp>
-#include <AccessibilityScrollbar.cpp>
-#include <AccessibilitySlider.cpp>
-#include <AccessibilityTable.cpp>
-#include <AccessibilityTableCell.cpp>
-#include <AccessibilityTableColumn.cpp>
-#include <AccessibilityTableHeaderContainer.cpp>
-#include <AccessibilityTableRow.cpp>
+#include "AXObjectCache.cpp"
+#include "AccessibilityARIAGrid.cpp"
+#include "AccessibilityARIAGridCell.cpp"
+#include "AccessibilityARIAGridRow.cpp"
+#include "AccessibilityImageMapLink.cpp"
+#include "AccessibilityList.cpp"
+#include "AccessibilityListBox.cpp"
+#include "AccessibilityListBoxOption.cpp"
+#include "AccessibilityMediaControls.cpp"
+#include "AccessibilityObject.cpp"
+#include "AccessibilityRenderObject.cpp"
+#include "AccessibilityScrollView.cpp"
+#include "AccessibilityScrollbar.cpp"
+#include "AccessibilitySlider.cpp"
+#include "AccessibilityTable.cpp"
+#include "AccessibilityTableCell.cpp"
+#include "AccessibilityTableColumn.cpp"
+#include "AccessibilityTableHeaderContainer.cpp"
+#include "AccessibilityTableRow.cpp"
