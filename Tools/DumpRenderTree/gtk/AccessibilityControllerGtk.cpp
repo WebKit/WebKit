aca76a11bdda51f22d31dd0a06523f823fc68394
Mario Sanchez Prada <mario@webkit.org>
2011-02-10 18:42:18 +0000

    2011-02-10  Mario Sanchez Prada  <msanchez@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] Log signals from AtkDocument interface also in AccessibilityController
            https://bugs.webkit.org/show_bug.cgi?id=54198
    
            Trace AtkDocument's signals emission through AccessibilityController.
    
            Also, taken the code related to adding and removing global
            listeners for ATK signals out to a separate file, for the sake of
            clarity and to ease future additions.
    
            * DumpRenderTree/gtk/AccessibilityCallbacks.h: Added.
            * DumpRenderTree/gtk/AccessibilityCallbacks.cpp: Added
            (printAccessibilityEvent): Print information about an event.
            (axObjectEventListener): Global listener for AtkObject's signals.
            (axDocumentEventListener): Global listener for AtkDocument's signals.
            (connectAccessibilityCallbacks): Connect all global listeners.
            (disconnectAccessibilityCallbacks): Disconnect all global listeners.
    
            * DumpRenderTree/gtk/AccessibilityControllerGtk.cpp:
            (AccessibilityController::setLogAccessibilityEvents): Call to
            connectAccessibilityCallbacks and disconnectAccessibilityCallbacks.
    
            * GNUmakefile.am: Added new files.
    
    Canonical link: https://commits.webkit.org/68256@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@78244 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/DumpRenderTree/gtk/AccessibilityControllerGtk.cpp
+++ Tools/DumpRenderTree/gtk/AccessibilityControllerGtk.cpp
@@ -27,6 +27,7 @@
 #include "config.h"
 #include "AccessibilityController.h"
 
+#include "AccessibilityCallbacks.h"
 #include "AccessibilityUIElement.h"
 #include "DumpRenderTree.h"
 #include "WebCoreSupport/DumpRenderTreeSupportGtk.h"
@@ -36,13 +37,6 @@
 #include <webkit/webkit.h>
 
 static bool loggingAccessibilityEvents = false;
-
-static guint state_change_listener_id = 0;
-static guint focus_event_listener_id = 0;
-static guint active_descendant_changed_listener_id = 0;
-static guint children_changed_listener_id = 0;
-static guint property_changed_listener_id = 0;
-static guint visible_data_changed_listener_id = 0;
 
 AccessibilityController::AccessibilityController()
 {
@@ -88,103 +82,18 @@ void AccessibilityController::setLogValueChangeEvents(bool)
 {
 }
 
-static gboolean accessibility_event_listener(GSignalInvocationHint *signal_hint,
-                                            guint n_param_values,
-                                            const GValue *param_values,
-                                            gpointer data)
-{
-    // At least we should receive the instance emitting the signal.
-    if (n_param_values < 1)
-        return TRUE;
-
-    AtkObject* accessible = ATK_OBJECT(g_value_get_object(&param_values[0]));
-    const gchar* axObjectName = 0;
-    guint axObjectRole = 0;
-    if (accessible) {
-        axObjectName = atk_object_get_name(accessible);
-        axObjectRole = atk_object_get_role(accessible);
-    }
-
-    GSignalQuery signal_query;
-    gchar* signalName = 0;
-
-    g_signal_query(signal_hint->signal_id, &signal_query);
-
-    if (!g_strcmp0(signal_query.signal_name, "state-change")) {
-        signalName = g_strdup_printf("state-change:%s = %d",
-                                     g_value_get_string(&param_values[1]),
-                                     g_value_get_boolean(&param_values[2]));
-    } else if (!g_strcmp0(signal_query.signal_name, "focus-event")) {
-        signalName = g_strdup_printf("focus-event = %d",
-                                     g_value_get_boolean(&param_values[1]));
-    } else if (!g_strcmp0(signal_query.signal_name, "children-changed")) {
-        signalName = g_strdup_printf("children-changed = %d",
-                                     g_value_get_uint(&param_values[1]));
-    } else {
-        signalName = g_strdup(signal_query.signal_name);
-    }
-
-    // Try to provide always a name to be logged for the object.
-    gchar* objectName = 0;
-    if (!axObjectName || *axObjectName == '\0')
-        objectName = g_strdup("(No name)");
-    else
-        objectName = g_strdup(axObjectName);
-
-    printf("Accessibility object emitted \"%s\" / Name: \"%s\" / Role: %d\n", signalName, objectName, axObjectRole);
-    g_free(signalName);
-    g_free(objectName);
-
-    return TRUE;
-}
-
 void AccessibilityController::setLogAccessibilityEvents(bool logAccessibilityEvents)
 {
     if (logAccessibilityEvents == loggingAccessibilityEvents)
         return;
 
-    if (state_change_listener_id) {
-        atk_remove_global_event_listener(state_change_listener_id);
-        state_change_listener_id = 0;
-    }
-    if (focus_event_listener_id) {
-        atk_remove_global_event_listener(focus_event_listener_id);
-        focus_event_listener_id = 0;
-    }
-    if (active_descendant_changed_listener_id) {
-        atk_remove_global_event_listener(active_descendant_changed_listener_id);
-        active_descendant_changed_listener_id = 0;
-    }
-    if (children_changed_listener_id) {
-        atk_remove_global_event_listener(children_changed_listener_id);
-        children_changed_listener_id = 0;
-    }
-    if (property_changed_listener_id) {
-        atk_remove_global_event_listener(property_changed_listener_id);
-        property_changed_listener_id = 0;
-    }
-    if (visible_data_changed_listener_id) {
-        atk_remove_global_event_listener(visible_data_changed_listener_id);
-        visible_data_changed_listener_id = 0;
-    }
-
     if (!logAccessibilityEvents) {
+        disconnectAccessibilityCallbacks();
         loggingAccessibilityEvents = false;
         return;
     }
 
-    // Ensure that accessibility is initialized for the WebView by querying for
-    // the root accessible object.
-    rootElement();
-
-    // Add global listeners
-    state_change_listener_id = atk_add_global_event_listener(accessibility_event_listener, "Gtk:AtkObject:state-change");
-    focus_event_listener_id = atk_add_global_event_listener(accessibility_event_listener, "Gtk:AtkObject:focus-event");
-    active_descendant_changed_listener_id = atk_add_global_event_listener(accessibility_event_listener, "Gtk:AtkObject:active-descendant-changed");
-    children_changed_listener_id = atk_add_global_event_listener(accessibility_event_listener, "Gtk:AtkObject:children-changed");
-    property_changed_listener_id = atk_add_global_event_listener(accessibility_event_listener, "Gtk:AtkObject:property-change");
-    visible_data_changed_listener_id = atk_add_global_event_listener(accessibility_event_listener, "Gtk:AtkObject:visible-data-changed");
-
+    connectAccessibilityCallbacks();
     loggingAccessibilityEvents = true;
 }
 
