fbff66dc5d9a230f9cfc161139f695a00c376d63
Mario Sanchez Prada <mario@webkit.org>
2011-02-16 17:12:38 +0000

    2011-02-16  Mario Sanchez Prada  <msanchez@igalia.com>
            Reviewed by Martin Robinson.
    
            [GTK] events missing when a document is (re)loaded
            https://bugs.webkit.org/show_bug.cgi?id=25831
    
            Added new layout test to check the right signals are emitted.
    
            * platform/gtk/accessibility/document-reload-events-expected.txt: Added.
            * platform/gtk/accessibility/document-reload-events.html: Added.
    2011-02-16  Mario Sanchez Prada  <msanchez@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] events missing when a document is (re)loaded
            https://bugs.webkit.org/show_bug.cgi?id=25831
    
            Make sure webArea returns a proper name and that a signal
            'state-change::defunct' is emitted when detaching the wrapper.
    
            Test: platform/gtk/accessibility/document-reload-events.html
    
            * accessibility/gtk/AccessibilityObjectWrapperAtk.cpp:
            (webkit_accessible_get_name): Returns the current document's title
            as fallback mechanism for webArea objects.
            (webkit_accessible_detach): Emit 'state-change::defunct' function
            as soon as the wrapper is detached from the related core object.
    2011-02-16  Mario Sanchez Prada  <msanchez@igalia.com>
    
            Reviewed by Martin Robinson.
    
            [GTK] events missing when a document is (re)loaded
            https://bugs.webkit.org/show_bug.cgi?id=25831
    
            Emit the right signals when reloading a document.
    
            * WebCoreSupport/FrameLoaderClientGtk.cpp:
            (WebKit::notifyAccessibilityStatus): New function, to make sure
            the signals involved in reloading a document are properly emitted.
            (WebKit::notifyStatus): Also notify accessibility if enabled.
    
    Canonical link: https://commits.webkit.org/68695@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@78715 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
+++ Source/WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
@@ -25,6 +25,8 @@
 #include "config.h"
 #include "FrameLoaderClientGtk.h"
 
+#include "AXObjectCache.h"
+#include "AccessibilityObject.h"
 #include "ArchiveResource.h"
 #include "CachedFrame.h"
 #include "Color.h"
@@ -55,6 +57,7 @@
 #include "PluginDatabase.h"
 #include "ProgressTracker.h"
 #include "RenderPart.h"
+#include "RenderView.h"
 #include "ResourceHandle.h"
 #include "ResourceRequest.h"
 #include "ScriptController.h"
@@ -222,6 +225,51 @@ String FrameLoaderClient::userAgent(const KURL& url)
     return String::fromUTF8(webkit_web_settings_get_user_agent(settings));
 }
 
+static void notifyAccessibilityStatus(WebKitWebFrame* frame, WebKitLoadStatus loadStatus)
+{
+    if (loadStatus != WEBKIT_LOAD_PROVISIONAL
+        && loadStatus != WEBKIT_LOAD_FAILED
+        && loadStatus != WEBKIT_LOAD_FINISHED)
+        return;
+
+    WebKitWebFramePrivate* priv = frame->priv;
+    if (!priv->coreFrame || !priv->coreFrame->document())
+        return;
+
+    RenderView* contentRenderer = priv->coreFrame->contentRenderer();
+    if (!contentRenderer)
+        return;
+
+    AXObjectCache* axObjectCache = priv->coreFrame->document()->axObjectCache();
+    if (!axObjectCache)
+        return;
+
+    AccessibilityObject* coreAxObject = axObjectCache->getOrCreate(contentRenderer);
+    if (!coreAxObject)
+        return;
+
+    AtkObject* axObject = coreAxObject->wrapper();
+    if (!axObject || !ATK_IS_DOCUMENT(axObject))
+        return;
+
+    switch (loadStatus) {
+    case WEBKIT_LOAD_PROVISIONAL:
+        g_signal_emit_by_name(axObject, "state-change", "busy", true);
+        if (core(frame)->loader()->loadType() == FrameLoadTypeReload)
+            g_signal_emit_by_name(axObject, "reload");
+        break;
+    case WEBKIT_LOAD_FAILED:
+        g_signal_emit_by_name(axObject, "load-stopped");
+        g_signal_emit_by_name(axObject, "state-change", "busy", false);
+        break;
+    case WEBKIT_LOAD_FINISHED:
+        g_signal_emit_by_name(axObject, "load-complete");
+        g_signal_emit_by_name(axObject, "state-change", "busy", false);
+    default:
+        break;
+    }
+}
+
 static void notifyStatus(WebKitWebFrame* frame, WebKitLoadStatus loadStatus)
 {
     frame->priv->loadStatus = loadStatus;
@@ -231,6 +279,9 @@ static void notifyStatus(WebKitWebFrame* frame, WebKitLoadStatus loadStatus)
     if (frame == webkit_web_view_get_main_frame(webView)) {
         webView->priv->loadStatus = loadStatus;
         g_object_notify(G_OBJECT(webView), "load-status");
+
+        if (AXObjectCache::accessibilityEnabled())
+            notifyAccessibilityStatus(frame, loadStatus);
     }
 }
 
