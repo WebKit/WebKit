3d182f793f39d3e11c8edee058d908d09e844262
Steve Block <steveblock@google.com>
2011-02-18 12:27:38 +0000

    2011-02-18  Steve Block  <steveblock@google.com>
            Reviewed by Andreas Kling
    
            Memory allocation error in convertV8ObjectToNPVariant() for strings
            https://bugs.webkit.org/show_bug.cgi?id=54737
    
            Include the termination character in the length when allocating memory
            and copying the string. This fixes a crashing bug on Android.
    
            This should be tested by the java/ tests on Chromium, but these
            tests are currently skipped.
    
            * bindings/v8/V8NPUtils.cpp:
            (WebCore::convertV8ObjectToNPVariant):
    
    
    Canonical link: https://commits.webkit.org/68949@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@78994 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/V8NPUtils.cpp
+++ Source/WebCore/bindings/v8/V8NPUtils.cpp
@@ -63,8 +63,9 @@ void convertV8ObjectToNPVariant(v8::Local<v8::Value> object, NPObject* owner, NP
         VOID_TO_NPVARIANT(*result);
     else if (object->IsString()) {
         v8::String::Utf8Value utf8(object);
-        char* utf8Chars = reinterpret_cast<char*>(malloc(utf8.length()));
-        memcpy(utf8Chars, *utf8, utf8.length());
+        int length = utf8.length() + 1;
+        char* utf8Chars = reinterpret_cast<char*>(malloc(length));
+        memcpy(utf8Chars, *utf8, length);
         STRINGN_TO_NPVARIANT(utf8Chars, utf8.length(), *result);
     } else if (object->IsObject()) {
         DOMWindow* window = V8Proxy::retrieveWindow(V8Proxy::currentContext());
