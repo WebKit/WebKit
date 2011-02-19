aaf2e3c0dc24befbb92c905db1a1cb48e94594f3
Drew Wilson <atwilson@chromium.org>
2011-02-19 01:15:48 +0000

    2011-02-18  Andrew Wilson  <atwilson@chromium.org>
            Unreviewed, rolling out r79047.
            http://trac.webkit.org/changeset/79047
            https://bugs.webkit.org/show_bug.cgi?id=54596
    
            Broke canary bots - please watch the canaries the next time
            you land this
    
            * Scripts/webkitpy/layout_tests/port/chromium_mac.py:
            * Scripts/webkitpy/layout_tests/port/mac.py:
    
    Canonical link: https://commits.webkit.org/69010@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@79055 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/port/chromium_mac.py
+++ Tools/Scripts/webkitpy/layout_tests/port/chromium_mac.py
@@ -111,9 +111,6 @@ class ChromiumMacPort(chromium.ChromiumPort):
 
         return chromium.ChromiumPort.default_child_processes(self)
 
-    def default_worker_model(self):
-        return 'processes'
-
     def driver_name(self):
         return "DumpRenderTree"
 
