b0ee3219c525638c84146cceff15546c4a1f9d7b
Simon Fraser <simon.fraser@apple.com>
2011-02-21 03:26:11 +0000

    2011-02-20  Simon Fraser  <simon.fraser@apple.com>
            Reviewed by Dirk Schulze.
    
            REGRESSION (r73369-r73405): transform animation interpolates incorrectly
            https://bugs.webkit.org/show_bug.cgi?id=54793
    
            After the refactoring in r73380, the ending state for some types of
            accelerated animations was computed incorrectly, because the TransformationMatrix
            wasn't reset to identity before the ending value transformations were applied.
    
            Fix by cleaning up the code to use explicit, separate values for
            starting and ending values. Only matrix-type animations had this
            issue, but cleaned up other clauses similarly.
    
            Test: animations/3d/matrix-transform-type-animation.html
    
            * platform/graphics/ca/GraphicsLayerCA.cpp:
            (WebCore::GraphicsLayerCA::setTransformAnimationEndpoints):
    
    Canonical link: https://commits.webkit.org/69100@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@79168 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
+++ Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -1759,23 +1759,29 @@ bool GraphicsLayerCA::setTransformAnimationEndpoints(const KeyframeValueList& va
         basicAnim->setToValue(toTransform);
     } else {
         if (isTransformTypeNumber(transformOpType)) {
-            float value;
-            getTransformFunctionValue(startValue->value()->at(functionIndex), transformOpType, boxSize, value);
-            basicAnim->setFromValue(value);
-            getTransformFunctionValue(endValue->value()->at(functionIndex), transformOpType, boxSize, value);
-            basicAnim->setToValue(value);
+            float fromValue;
+            getTransformFunctionValue(startValue->value()->at(functionIndex), transformOpType, boxSize, fromValue);
+            basicAnim->setFromValue(fromValue);
+            
+            float toValue;
+            getTransformFunctionValue(endValue->value()->at(functionIndex), transformOpType, boxSize, toValue);
+            basicAnim->setToValue(toValue);
         } else if (isTransformTypeFloatPoint3D(transformOpType)) {
-            FloatPoint3D value;
-            getTransformFunctionValue(startValue->value()->at(functionIndex), transformOpType, boxSize, value);
-            basicAnim->setFromValue(value);
-            getTransformFunctionValue(endValue->value()->at(functionIndex), transformOpType, boxSize, value);
-            basicAnim->setToValue(value);
+            FloatPoint3D fromValue;
+            getTransformFunctionValue(startValue->value()->at(functionIndex), transformOpType, boxSize, fromValue);
+            basicAnim->setFromValue(fromValue);
+            
+            FloatPoint3D toValue;
+            getTransformFunctionValue(endValue->value()->at(functionIndex), transformOpType, boxSize, toValue);
+            basicAnim->setToValue(toValue);
         } else {
-            TransformationMatrix value;
-            getTransformFunctionValue(startValue->value()->at(functionIndex), transformOpType, boxSize, value);
-            basicAnim->setFromValue(value);
-            getTransformFunctionValue(endValue->value()->at(functionIndex), transformOpType, boxSize, value);
-            basicAnim->setToValue(value);
+            TransformationMatrix fromValue;
+            getTransformFunctionValue(startValue->value()->at(functionIndex), transformOpType, boxSize, fromValue);
+            basicAnim->setFromValue(fromValue);
+
+            TransformationMatrix toValue;
+            getTransformFunctionValue(endValue->value()->at(functionIndex), transformOpType, boxSize, toValue);
+            basicAnim->setToValue(toValue);
         }
     }
 
