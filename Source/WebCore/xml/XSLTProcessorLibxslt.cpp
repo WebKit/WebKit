08dbaaebc939b015fe4415c5108d23797b569f20
Alexey Proskuryakov <ap@apple.com>
2011-02-21 01:07:34 +0000

    Reviewed by Eric Seidel.
            Tighten up access permissions by using libxslt API
            https://bugs.webkit.org/show_bug.cgi?id=52688
            <rdar://problem/8909191>
    
            * xml/XSLTProcessorLibxslt.cpp: (WebCore::XSLTProcessor::transformToString): We are only
            interested in a string result, so let libxslt know about that.
    
    
    
    Canonical link: https://commits.webkit.org/69092@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@79159 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/xml/XSLTProcessorLibxslt.cpp
+++ Source/WebCore/xml/XSLTProcessorLibxslt.cpp
@@ -41,6 +41,7 @@
 #include "XSLTUnicodeSort.h"
 #include "markup.h"
 #include <libxslt/imports.h>
+#include <libxslt/security.h>
 #include <libxslt/variables.h>
 #include <libxslt/xsltutils.h>
 #include <wtf/Assertions.h>
@@ -61,6 +62,12 @@ SOFT_LINK(libxslt, xsltSetCtxtSortFunc, void, (xsltTransformContextPtr ctxt, xsl
 SOFT_LINK(libxslt, xsltSetLoaderFunc, void, (xsltDocLoaderFunc f), (f))
 SOFT_LINK(libxslt, xsltSaveResultTo, int, (xmlOutputBufferPtr buf, xmlDocPtr result, xsltStylesheetPtr style), (buf, result, style))
 SOFT_LINK(libxslt, xsltNextImport, xsltStylesheetPtr, (xsltStylesheetPtr style), (style))
+SOFT_LINK(libxslt, xsltNewSecurityPrefs, xsltSecurityPrefsPtr, (), ())
+SOFT_LINK(libxslt, xsltFreeSecurityPrefs, void, (xsltSecurityPrefsPtr sec), (sec))
+SOFT_LINK(libxslt, xsltSetSecurityPrefs, int, (xsltSecurityPrefsPtr sec, xsltSecurityOption option, xsltSecurityCheck func), (sec, option, func))
+SOFT_LINK(libxslt, xsltSetCtxtSecurityPrefs, int, (xsltSecurityPrefsPtr sec, xsltTransformContextPtr ctxt), (sec, ctxt))
+SOFT_LINK(libxslt, xsltSecurityForbid, int, (xsltSecurityPrefsPtr sec, xsltTransformContextPtr ctxt, const char* value), (sec, ctxt, value))
+
 #endif
 
 namespace WebCore {
@@ -315,6 +322,17 @@ bool XSLTProcessor::transformToString(Node* sourceNode, String& mimeType, String
         xsltTransformContextPtr transformContext = xsltNewTransformContext(sheet, sourceDoc);
         registerXSLTExtensions(transformContext);
 
+        xsltSecurityPrefsPtr securityPrefs = xsltNewSecurityPrefs();
+        // Read permissions are checked by docLoaderFunc.
+        if (0 != xsltSetSecurityPrefs(securityPrefs, XSLT_SECPREF_WRITE_FILE, xsltSecurityForbid))
+            CRASH();
+        if (0 != xsltSetSecurityPrefs(securityPrefs, XSLT_SECPREF_CREATE_DIRECTORY, xsltSecurityForbid))
+            CRASH();
+        if (0 != xsltSetSecurityPrefs(securityPrefs, XSLT_SECPREF_WRITE_NETWORK, xsltSecurityForbid))
+            CRASH();
+        if (0 != xsltSetCtxtSecurityPrefs(securityPrefs, transformContext))
+            CRASH();
+
         // <http://bugs.webkit.org/show_bug.cgi?id=16077>: XSLT processor <xsl:sort> algorithm only compares by code point.
         xsltSetCtxtSortFunc(transformContext, xsltUnicodeSortFunction);
 
@@ -328,6 +346,7 @@ bool XSLTProcessor::transformToString(Node* sourceNode, String& mimeType, String
         xmlDocPtr resultDoc = xsltApplyStylesheetUser(sheet, sourceDoc, 0, 0, 0, transformContext);
 
         xsltFreeTransformContext(transformContext);
+        xsltFreeSecurityPrefs(securityPrefs);
         freeXsltParamArray(params);
 
         if (shouldFreeSourceDoc)
