18a2b12a08b45a3dbd3e17a8f88e8a8919b19818
Commit Queue <commit-queue@webkit.org>
2011-02-26 12:12:13 +0000

    2011-02-26  Alice Boxhall  <aboxhall@chromium.org>
            Reviewed by Ojan Vafai.
    
            Tests that typing enter in the input element does not fire textInput.
            https://bugs.webkit.org/show_bug.cgi?id=54152
    
            * fast/forms/textinput-not-fired-on-enter-in-input-expected.txt: Added.
            * fast/forms/textinput-not-fired-on-enter-in-input.html: Added.
    2011-02-26  Alice Boxhall  <aboxhall@chromium.org>
    
            Reviewed by Ojan Vafai.
    
            typing enter in the input element should not fire textInput
            https://bugs.webkit.org/show_bug.cgi?id=54152
    
            Stop textInput event propagation in HTMLInputElement::preDispatchEventHandler if the event target should submit implicitly.
    
            Test: fast/forms/textinput-not-fired-on-enter-in-input.html
    
            * html/HTMLInputElement.cpp:
            (WebCore::HTMLInputElement::preDispatchEventHandler): Check m_inputType->shouldSubmitImplicitly for textInputEvents and stop propagation if true.
    
    Canonical link: https://commits.webkit.org/69694@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@79777 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLInputElement.cpp
+++ Source/WebCore/html/HTMLInputElement.cpp
@@ -961,6 +961,10 @@ void HTMLInputElement::setFileListFromRenderer(const Vector<String>& paths)
 
 void* HTMLInputElement::preDispatchEventHandler(Event* event)
 {
+    if (event->type() == eventNames().textInputEvent && m_inputType->shouldSubmitImplicitly(event)) {
+        event->stopPropagation();
+        return 0;
+    }
     if (event->type() != eventNames().clickEvent)
         return 0;
     if (!event->isMouseEvent() || static_cast<MouseEvent*>(event)->button() != LeftButton)
