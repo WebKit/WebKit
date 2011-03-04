ff710549f5295c9fb53c20cf03e3c683fb3bfd43
Jeff Miller <jeffm@apple.com>
2011-03-04 05:36:03 +0000

    2011-03-03  Jeff Miller  <jeffm@apple.com>
            Reviewed by Darin Adler.
    
            Refactor classes in GenericCallback.h
            https://bugs.webkit.org/show_bug.cgi?id=55732
    
            Since we're going to need to add additional callback classes, make a CallbackBase class that manages
            the context and the callback ID.  The existing callback classes (VoidCallback, GenericCallback, and
            ComputedPagesCallback) now all derive from CallbackBase.
    
            Fix bug in VoidCallback where we meant to implement the destructor, but we were implementing the default
            constructor instead (we forgot the leading ~).
    
            * UIProcess/GenericCallback.h:
            (WebKit::CallbackBase::~CallbackBase):
            (WebKit::CallbackBase::callbackID):
            (WebKit::CallbackBase::CallbackBase):
            (WebKit::CallbackBase::context):
            (WebKit::CallbackBase::generateCallbackID):
            (WebKit::VoidCallback::~VoidCallback):
            (WebKit::VoidCallback::performCallback):
            (WebKit::VoidCallback::invalidate):
            (WebKit::VoidCallback::VoidCallback):
            (WebKit::GenericCallback::create):
            (WebKit::GenericCallback::~GenericCallback):
            (WebKit::GenericCallback::performCallbackWithReturnValue):
            (WebKit::GenericCallback::invalidate):
            (WebKit::GenericCallback::GenericCallback):
            (WebKit::ComputedPagesCallback::create):
            (WebKit::ComputedPagesCallback::~ComputedPagesCallback):
            (WebKit::ComputedPagesCallback::performCallbackWithReturnValue):
            (WebKit::ComputedPagesCallback::invalidate):
            (WebKit::ComputedPagesCallback::ComputedPagesCallback):
    
    
    Canonical link: https://commits.webkit.org/70216@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@80322 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/GenericCallback.h
+++ Source/WebKit2/UIProcess/GenericCallback.h
@@ -35,7 +35,35 @@
 
 namespace WebKit {
 
-class VoidCallback : public RefCounted<VoidCallback> {
+class CallbackBase : public RefCounted<CallbackBase> {
+public:
+    virtual ~CallbackBase()
+    {
+    }
+
+    uint64_t callbackID() const { return m_callbackID; }
+
+protected:
+    CallbackBase(void* context)
+        : m_context(context)
+        , m_callbackID(generateCallbackID())
+    {
+    }
+
+    void* context() const { return m_context; }
+
+private:
+    static uint64_t generateCallbackID()
+    {
+        static uint64_t uniqueCallbackID = 1;
+        return uniqueCallbackID++;
+    }
+
+    void* m_context;
+    uint64_t m_callbackID;
+};
+
+class VoidCallback : public CallbackBase {
 public:
     typedef void (*CallbackFunction)(WKErrorRef, void*);
 
@@ -44,7 +72,7 @@ public:
         return adoptRef(new VoidCallback(context, callback));
     }
 
-    VoidCallback()
+    virtual ~VoidCallback()
     {
         ASSERT(!m_callback);
     }
@@ -53,7 +81,7 @@ public:
     {
         ASSERT(m_callback);
 
-        m_callback(0, m_context);
+        m_callback(0, context());
 
         m_callback = 0;
     }
@@ -63,89 +91,23 @@ public:
         ASSERT(m_callback);
 
         RefPtr<WebError> error = WebError::create();
-        m_callback(toAPI(error.get()), m_context);
+        m_callback(toAPI(error.get()), context());
         
         m_callback = 0;
     }
 
-    uint64_t callbackID() const { return m_callbackID; }
-
 private:
-    static uint64_t generateCallbackID()
-    {
-        static uint64_t uniqueCallbackID = 1;
-        return uniqueCallbackID++;
-    }
-
     VoidCallback(void* context, CallbackFunction callback)
-        : m_context(context)
+        : CallbackBase(context)
         , m_callback(callback)
-        , m_callbackID(generateCallbackID())
     {
     }
 
-    void* m_context;
     CallbackFunction m_callback;
-    uint64_t m_callbackID;
-};
-
-// FIXME: Make a version of GenericCallback with two arguments, and define ComputedPagesCallback as a specialization.
-class ComputedPagesCallback : public RefCounted<ComputedPagesCallback> {
-public:
-    typedef void (*CallbackFunction)(const Vector<WebCore::IntRect>&, double, WKErrorRef, void*);
-
-    static PassRefPtr<ComputedPagesCallback> create(void* context, CallbackFunction callback)
-    {
-        return adoptRef(new ComputedPagesCallback(context, callback));
-    }
-
-    ~ComputedPagesCallback()
-    {
-        ASSERT(!m_callback);
-    }
-
-    void performCallbackWithReturnValue(const Vector<WebCore::IntRect>& returnValue1, double returnValue2)
-    {
-        ASSERT(m_callback);
-
-        m_callback(returnValue1, returnValue2, 0, m_context);
-
-        m_callback = 0;
-    }
-    
-    void invalidate()
-    {
-        ASSERT(m_callback);
-
-        RefPtr<WebError> error = WebError::create();
-        m_callback(Vector<WebCore::IntRect>(), 0, toAPI(error.get()), m_context);
-        
-        m_callback = 0;
-    }
-
-    uint64_t callbackID() const { return m_callbackID; }
-
-private:
-    static uint64_t generateCallbackID()
-    {
-        static uint64_t uniqueCallbackID = 1;
-        return uniqueCallbackID++;
-    }
-
-    ComputedPagesCallback(void* context, CallbackFunction callback)
-        : m_context(context)
-        , m_callback(callback)
-        , m_callbackID(generateCallbackID())
-    {
-    }
-
-    void* m_context;
-    CallbackFunction m_callback;
-    uint64_t m_callbackID;
 };
 
 template<typename APIReturnValueType, typename InternalReturnValueType = typename APITypeInfo<APIReturnValueType>::ImplType>
-class GenericCallback : public RefCounted<GenericCallback<APIReturnValueType, InternalReturnValueType> > {
+class GenericCallback : public CallbackBase {
 public:
     typedef void (*CallbackFunction)(APIReturnValueType, WKErrorRef, void*);
 
@@ -154,7 +116,7 @@ public:
         return adoptRef(new GenericCallback(context, callback));
     }
 
-    ~GenericCallback()
+    virtual ~GenericCallback()
     {
         ASSERT(!m_callback);
     }
@@ -163,7 +125,7 @@ public:
     {
         ASSERT(m_callback);
 
-        m_callback(toAPI(returnValue), 0, m_context);
+        m_callback(toAPI(returnValue), 0, context());
 
         m_callback = 0;
     }
@@ -173,30 +135,64 @@ public:
         ASSERT(m_callback);
 
         RefPtr<WebError> error = WebError::create();
-        m_callback(0, toAPI(error.get()), m_context);
+        m_callback(0, toAPI(error.get()), context());
         
         m_callback = 0;
     }
 
-    uint64_t callbackID() const { return m_callbackID; }
+private:
+    GenericCallback(void* context, CallbackFunction callback)
+        : CallbackBase(context)
+        , m_callback(callback)
+    {
+    }
+
+    CallbackFunction m_callback;
+};
+
+// FIXME: Make a version of CallbackBase with two arguments, and define ComputedPagesCallback as a specialization.
+class ComputedPagesCallback : public CallbackBase {
+public:
+    typedef void (*CallbackFunction)(const Vector<WebCore::IntRect>&, double, WKErrorRef, void*);
+
+    static PassRefPtr<ComputedPagesCallback> create(void* context, CallbackFunction callback)
+    {
+        return adoptRef(new ComputedPagesCallback(context, callback));
+    }
+
+    virtual ~ComputedPagesCallback()
+    {
+        ASSERT(!m_callback);
+    }
+
+    void performCallbackWithReturnValue(const Vector<WebCore::IntRect>& returnValue1, double returnValue2)
+    {
+        ASSERT(m_callback);
+
+        m_callback(returnValue1, returnValue2, 0, context());
+
+        m_callback = 0;
+    }
+    
+    void invalidate()
+    {
+        ASSERT(m_callback);
+
+        RefPtr<WebError> error = WebError::create();
+        m_callback(Vector<WebCore::IntRect>(), 0, toAPI(error.get()), context());
+        
+        m_callback = 0;
+    }
 
 private:
-    static uint64_t generateCallbackID()
-    {
-        static uint64_t uniqueCallbackID = 1;
-        return uniqueCallbackID++;
-    }
 
-    GenericCallback(void* context, CallbackFunction callback)
-        : m_context(context)
+    ComputedPagesCallback(void* context, CallbackFunction callback)
+        : CallbackBase(context)
         , m_callback(callback)
-        , m_callbackID(generateCallbackID())
     {
     }
 
-    void* m_context;
     CallbackFunction m_callback;
-    uint64_t m_callbackID;
 };
 
 template<typename T>
