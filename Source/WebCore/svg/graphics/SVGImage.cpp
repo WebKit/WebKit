eacccd38319a91239a8d620c6828bb91753b53c5
Commit Queue <commit-queue@webkit.org>
2011-03-04 21:28:09 +0000

    2011-03-04  Cosmin Truta  <ctruta@chromium.org>
            Reviewed by Adam Barth.
    
            Clarify comment about potential memory leak in SVGImage
            https://bugs.webkit.org/show_bug.cgi?id=55362
    
            No functionality change. No new tests.
    
            * svg/graphics/SVGImage.cpp:
            (WebCore::SVGImage::dataChanged):
    
    Canonical link: https://commits.webkit.org/70265@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@80371 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/svg/graphics/SVGImage.cpp
+++ Source/WebCore/svg/graphics/SVGImage.cpp
@@ -263,8 +263,11 @@ bool SVGImage::dataChanged(bool allDataReceived)
 #endif
 
         // FIXME: If this SVG ends up loading itself, we might leak the world.
-        // The comment said that the Cache code does not know about CachedImages
-        // holding Frames and won't know to break the cycle. But 
+        // The Cache code does not know about CachedImages holding Frames and
+        // won't know to break the cycle.
+        // This will become an issue when SVGImage will be able to load other
+        // SVGImage objects, but we're safe now, because SVGImage can only be
+        // loaded by a top-level document.
         m_page.set(new Page(pageClients));
         m_page->settings()->setMediaEnabled(false);
         m_page->settings()->setJavaScriptEnabled(false);
