87017f99d993ee45880cfd0b1ce3f66e51df7117
Anders Carlsson <andersca@apple.com>
2011-03-12 21:42:48 +0000

    * WebProcess/WebPage/WebFrame.cpp: (WebKit::WebFrame::url): Get the URL from the loader, not from the document.
    Reviewed by Sam Weinig.
    
    (WebKit::WebFrame::suggestedFilenameForResourceWithURL):
    Return the correct suggested filename for the main resource.
    
    (WebKit::WebFrame::mimeTypeForResourceWithURL):
    Return the correct mime type for the main resource.
    
    
    
    Canonical link: https://commits.webkit.org/70830@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@80945 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/WebPage/WebFrame.cpp
+++ Source/WebKit2/WebProcess/WebPage/WebFrame.cpp
@@ -335,7 +335,11 @@ String WebFrame::url() const
     if (!m_coreFrame)
         return String();
 
-    return m_coreFrame->document()->url().string();
+    DocumentLoader* documentLoader = m_coreFrame->loader()->documentLoader();
+    if (!documentLoader)
+        return String();
+
+    return documentLoader->url().string();
 }
 
 String WebFrame::innerText() const
@@ -611,7 +615,12 @@ String WebFrame::suggestedFilenameForResourceWithURL(const KURL& url) const
     DocumentLoader* loader = m_coreFrame->loader()->documentLoader();
     if (!loader)
         return String();
-    
+
+    // First, try the main resource.
+    if (loader->url() == url)
+        return loader->response().suggestedFilename();
+
+    // Next, try subresources.
     RefPtr<ArchiveResource> resource = loader->subresource(url);
     if (!resource)
         return String();
@@ -627,7 +636,12 @@ String WebFrame::mimeTypeForResourceWithURL(const KURL& url) const
     DocumentLoader* loader = m_coreFrame->loader()->documentLoader();
     if (!loader)
         return String();
-    
+
+    // First, try the main resource.
+    if (loader->url() == url)
+        return loader->response().mimeType();
+
+    // Next, try subresources.
     RefPtr<ArchiveResource> resource = loader->subresource(url);
     if (resource)
         return resource->mimeType();
