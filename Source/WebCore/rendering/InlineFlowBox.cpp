1f68c70f35cac4b7bdb1fe5b02ccab249ac46165
David Hyatt <hyatt@apple.com>
2011-03-14 20:56:18 +0000

    https://bugs.webkit.org/show_bug.cgi?id=45164
    Reviewed by Dan Bernstein.
    
    REGRESSION: <a><img align=top></a> Clickable area too large
    
    Make sure to clamp hit testing of quirky inline flow boxes the same way we already clamped
    painting.
    
    Source/WebCore:
    
    * rendering/InlineFlowBox.cpp:
    (WebCore::InlineFlowBox::nodeAtPoint):
    
    LayoutTests:
    
    * fast/inline/inline-position-top-align-expected.txt: Added.
    * fast/inline/inline-position-top-align.html: Added.
    
    
    
    Canonical link: https://commits.webkit.org/70935@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@81055 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/InlineFlowBox.cpp
+++ Source/WebCore/rendering/InlineFlowBox.cpp
@@ -920,10 +920,29 @@ bool InlineFlowBox::nodeAtPoint(const HitTestRequest& request, HitTestResult& re
         }
     }
 
-    // Now check ourselves.
-    FloatPoint boxOrigin = locationIncludingFlipping();
-    boxOrigin.move(tx, ty);
-    FloatRect rect(boxOrigin, IntSize(width(), height()));
+    // Now check ourselves. Pixel snap hit testing.
+    IntRect frameRect = roundedFrameRect();
+    int minX = frameRect.x();
+    int minY = frameRect.y();
+    int width = frameRect.width();
+    int height = frameRect.height();
+
+    // Constrain our hit testing to the line top and bottom if necessary.
+    bool noQuirksMode = renderer()->document()->inNoQuirksMode();
+    if (!hasTextChildren() && !noQuirksMode) {
+        RootInlineBox* rootBox = root();
+        int& top = isHorizontal() ? minY : minX;
+        int& logicalHeight = isHorizontal() ? height : width;
+        int bottom = min(rootBox->lineBottom(), top + logicalHeight);
+        top = max(rootBox->lineTop(), top);
+        logicalHeight = bottom - top;
+    }
+    
+    // Move x/y to our coordinates.
+    IntRect rect(minX, minY, width, height);
+    flipForWritingMode(rect);
+    rect.move(tx, ty);
+
     if (visibleToHitTesting() && rect.intersects(result.rectForPoint(x, y))) {
         renderer()->updateHitTestResult(result, flipForWritingMode(IntPoint(x - tx, y - ty))); // Don't add in m_x or m_y here, we want coords in the containing block's space.
         if (!result.addNodeToRectBasedTestResult(renderer()->node(), x, y, rect))
