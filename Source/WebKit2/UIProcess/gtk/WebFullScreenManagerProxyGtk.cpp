ee7512764e25826336b46c503cb83b77198ec6c8
Alejandro G. Castro <alex@webkit.org>
2011-03-18 16:40:20 +0000

    2011-03-18  Alejandro G. Castro  <alex@igalia.com>
            Reviewed by Martin Robinson.
    
            Fixed compilation after r80925.
    
            * GNUmakefile.am:
            * UIProcess/gtk/WebFullScreenManagerProxyGtk.cpp: Added.
            (WebKit::WebFullScreenManagerProxy::enterFullScreen):
            (WebKit::WebFullScreenManagerProxy::exitFullScreen):
            (WebKit::WebFullScreenManagerProxy::beganEnterFullScreenAnimation):
            (WebKit::WebFullScreenManagerProxy::finishedEnterFullScreenAnimation):
            (WebKit::WebFullScreenManagerProxy::beganExitFullScreenAnimation):
            (WebKit::WebFullScreenManagerProxy::finishedExitFullScreenAnimation):
            (WebKit::WebFullScreenManagerProxy::enterAcceleratedCompositingMode):
            (WebKit::WebFullScreenManagerProxy::exitAcceleratedCompositingMode):
            (WebKit::WebFullScreenManagerProxy::getFullScreenRect):
            * WebProcess/FullScreen/gtk/WebFullScreenManagerGtk.cpp: Added.
            (WebKit::WebFullScreenManagerGtk::WebFullScreenManagerGtk):
            (WebKit::WebFullScreenManager::create):
            (WebKit::WebFullScreenManagerGtk::setRootFullScreenLayer):
            (WebKit::WebFullScreenManagerGtk::beginEnterFullScreenAnimation):
            (WebKit::WebFullScreenManagerGtk::beginExitFullScreenAnimation):
            * WebProcess/FullScreen/gtk/WebFullScreenManagerGtk.h: Added.
    
    Canonical link: https://commits.webkit.org/71338@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@81478 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit2/UIProcess/gtk/WebFullScreenManagerProxyGtk.cpp
@@ -0,0 +1,81 @@
+/*
+ *  Copyright (C) 2011 Igalia S.L.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
+
+#include "config.h"
+#include "WebFullScreenManagerProxy.h"
+
+#if ENABLE(FULLSCREEN_API)
+
+#include "WebContext.h"
+#include "WebFullScreenManagerMessages.h"
+#include "WebFullScreenManagerProxyMessages.h"
+#include "WebProcess.h"
+
+#include <WebCore/NotImplemented.h>
+
+namespace WebKit {
+
+void WebFullScreenManagerProxy::enterFullScreen()
+{
+    notImplemented();
+}
+
+void WebFullScreenManagerProxy::exitFullScreen()
+{
+    notImplemented();
+}
+
+void WebFullScreenManagerProxy::beganEnterFullScreenAnimation()
+{
+    notImplemented();
+}
+
+void WebFullScreenManagerProxy::finishedEnterFullScreenAnimation(bool completed)
+{
+    notImplemented();
+}
+
+void WebFullScreenManagerProxy::beganExitFullScreenAnimation()
+{
+    notImplemented();
+}
+
+void WebFullScreenManagerProxy::finishedExitFullScreenAnimation(bool completed)
+{
+    notImplemented();
+}
+
+void WebFullScreenManagerProxy::enterAcceleratedCompositingMode(const LayerTreeContext& context)
+{
+    notImplemented();
+}
+
+void WebFullScreenManagerProxy::exitAcceleratedCompositingMode()
+{
+    notImplemented();
+}
+
+void WebFullScreenManagerProxy::getFullScreenRect(WebCore::IntRect& rect)
+{
+    notImplemented();
+}
+
+} // namespace WebKit
+
+#endif // ENABLE(FULLSCREEN_API)
