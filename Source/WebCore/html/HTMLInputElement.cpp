aebce5a4ccb7ebe95b14ac91ad34f4f03d39e4ac
Commit Queue <commit-queue@webkit.org>
2011-03-22 09:48:43 +0000

    2011-03-22  Ilya Sherman  <isherman@chromium.org>
            Reviewed by Kent Tamura.
    
            Clear autofilled flag when reseting a form
            https://bugs.webkit.org/show_bug.cgi?id=56802
    
            * fast/forms/reset-autofilled-expected.txt: Added.
            * fast/forms/reset-autofilled.html: Added.
            * platform/chromium/fast/forms/reset-autofilled-expected.txt: Added.
            * platform/win/fast/forms/reset-autofilled-expected.txt: Added.
    2011-03-22  Ilya Sherman  <isherman@chromium.org>
    
            Reviewed by Kent Tamura.
    
            Clear autofilled flag when reseting a form
            In service of https://code.google.com/p/chromium/issues/detail?id=70037
            https://bugs.webkit.org/show_bug.cgi?id=56802
    
            Test: fast/forms/reset-autofilled.html
    
            * html/HTMLInputElement.cpp:
            (WebCore::HTMLInputElement::reset): Clear the autofilled flag.
    
    Canonical link: https://commits.webkit.org/71484@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@81652 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLInputElement.cpp
+++ Source/WebCore/html/HTMLInputElement.cpp
@@ -750,6 +750,7 @@ void HTMLInputElement::reset()
     if (m_inputType->storesValueSeparateFromAttribute())
         setValue(String());
 
+    setAutofilled(false);
     setChecked(hasAttribute(checkedAttr));
     m_reflectsCheckedAttribute = true;
 }
