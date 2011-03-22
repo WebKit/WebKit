2784409cae54fbc824c65874a19fe12364c28f89
Jay Civelli <jcivelli@chromium.org>
2011-03-22 19:17:58 +0000

    2011-03-22  Jay Civelli  <jcivelli@chromium.org>
            Reviewed by David Levin.
    
            Adding a contains method to Vector.
            https://bugs.webkit.org/show_bug.cgi?id=55859
    
            * wtf/Vector.h:
            (WTF::Vector::contains):
    2011-03-22  Jay Civelli  <jcivelli@chromium.org>
    
            Reviewed by David Levin.
    
            Relanding:
            Moving the method that is used to retrieve all the resources in
            a page from the Chromium code (dom_operations.cc) to WebKit.
            https://bugs.webkit.org/show_bug.cgi?id=55859
    
            * WebKit.gyp:
            * public/WebPageSerializer.h:
            * public/WebURL.h:
            (WebKit::operator==):
            (WebKit::operator!=):
            * public/WebVector.h:
            (WebKit::WebVector::contains):
            * src/WebDataSourceImpl.cpp:
            (WebKit::WebDataSourceImpl::WebDataSourceImpl):
            * src/WebPageSerializer.cpp:
            (WebCore::getSubResourceURLFromElement):
            (WebCore::retrieveResourcesForElement):
            (WebCore::retrieveResourcesForFrame):
            (WebKit::WebPageSerializer::retrieveAllResources):
            * tests/WebFrameTest.cpp:
            * tests/WebPageSerializerTest.cpp: Added.
            * tests/data/pageserialization/awesome.png: Added.
            * tests/data/pageserialization/embed_iframe.html: Added.
            * tests/data/pageserialization/object_iframe.html: Added.
            * tests/data/pageserialization/simple_iframe.html: Added.
            * tests/data/pageserialization/simple_page.html: Added.
            * tests/data/pageserialization/top_frame.html: Added.
    
    Canonical link: https://commits.webkit.org/71516@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@81686 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/chromium/public/WebURL.h
+++ Source/WebKit/chromium/public/WebURL.h
@@ -143,6 +143,16 @@ inline bool operator<(const WebURL& a, const WebURL& b)
     return a.spec() < b.spec();
 }
 
+inline bool operator==(const WebURL& a, const WebURL& b)
+{
+    return !a.spec().compare(b.spec());
+}
+
+inline bool operator!=(const WebURL& a, const WebURL& b)
+{
+    return !(a == b);
+}
+
 } // namespace WebKit
 
 #endif
