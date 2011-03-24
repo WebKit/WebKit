fc3e1ea9aff159be837224fca7e704c1db4d5af0
Andy Estes <aestes@apple.com>
2011-03-24 19:58:35 +0000

    2011-03-24  Andy Estes  <aestes@apple.com>
            Reviewed by Eric Seidel.
    
            REGRESSION (r80231): Bad cast in HTMLTreeBuilder with closed </form> tags
            https://bugs.webkit.org/show_bug.cgi?id=56836
    
            During fragment parsing, HTMLConstructionSite holds a reference to the
            fragment context's closest form ancestor. If a misnested form end tag is
            then encountered as the first node of the fragment, we will check to see
            if a corresponding form start tag is in scope even though no such tag
            exists. This led to isScope() walking the HTMLElementStack all the way
            to the root DocumentFragment and attempting to cast it to Element*.
    
            Fix this by ensuring that the inScope() family of functions operate in
            terms of ContainerNodes to account for the fragment case.
    
            Test: fast/parser/fragment-closest-form-ancestor.html
    
            * html/parser/HTMLElementStack.cpp:
            (WebCore::HTMLNames::isRootNode):
            (WebCore::HTMLNames::isScopeMarker):
            (WebCore::HTMLNames::isTableScopeMarker):
            (WebCore::HTMLNames::isTableBodyScopeMarker):
            (WebCore::HTMLNames::isTableRowScopeMarker):
            (WebCore::HTMLElementStack::hasOnlyHTMLElementsInScope):
            (WebCore::HTMLElementStack::inScope):
    2011-03-24  Andy Estes  <aestes@apple.com>
    
            Reviewed by Eric Seidel.
    
            REGRESSION (r80231): Bad cast in HTMLTreeBuilder with closed </form> tags
            https://bugs.webkit.org/show_bug.cgi?id=56836
    
            * fast/parser/fragment-closest-form-ancestor-expected.txt: Added.
            * fast/parser/fragment-closest-form-ancestor.html: Added.
    
    
    Canonical link: https://commits.webkit.org/71701@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@81889 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/parser/HTMLElementStack.cpp
+++ Source/WebCore/html/parser/HTMLElementStack.cpp
@@ -50,7 +50,7 @@ inline bool isNumberedHeaderElement(ContainerNode* node)
         || node->hasTagName(h6Tag);
 }
     
-inline bool isRootMarker(ContainerNode* node)
+inline bool isRootNode(ContainerNode* node)
 {
     return node->nodeType() == Node::DOCUMENT_FRAGMENT_NODE
         || node->hasTagName(htmlTag);
@@ -74,7 +74,7 @@ inline bool isScopeMarker(ContainerNode* node)
         || node->hasTagName(SVGNames::foreignObjectTag)
         || node->hasTagName(SVGNames::descTag)
         || node->hasTagName(SVGNames::titleTag)
-        || isRootMarker(node);
+        || isRootNode(node);
 }
 
 inline bool isListItemScopeMarker(ContainerNode* node)
@@ -87,7 +87,7 @@ inline bool isListItemScopeMarker(ContainerNode* node)
 inline bool isTableScopeMarker(ContainerNode* node)
 {
     return node->hasTagName(tableTag)
-        || isRootMarker(node);
+        || isRootNode(node);
 }
 
 inline bool isTableBodyScopeMarker(ContainerNode* node)
@@ -95,13 +95,13 @@ inline bool isTableBodyScopeMarker(ContainerNode* node)
     return node->hasTagName(tbodyTag)
         || node->hasTagName(tfootTag)
         || node->hasTagName(theadTag)
-        || isRootMarker(node);
+        || isRootNode(node);
 }
 
 inline bool isTableRowScopeMarker(ContainerNode* node)
 {
     return node->hasTagName(trTag)
-        || isRootMarker(node);
+        || isRootNode(node);
 }
 
 inline bool isForeignContentScopeMarker(ContainerNode* node)
@@ -434,10 +434,10 @@ bool inScopeCommon(HTMLElementStack::ElementRecord* top, const AtomicString& tar
 bool HTMLElementStack::hasOnlyHTMLElementsInScope() const
 {
     for (ElementRecord* record = m_top.get(); record; record = record->next()) {
-        Element* element = record->element();
-        if (!isInHTMLNamespace(element))
+        ContainerNode* node = record->node();
+        if (!isInHTMLNamespace(node))
             return false;
-        if (isScopeMarker(element))
+        if (isScopeMarker(node))
             return true;
     }
     ASSERT_NOT_REACHED(); // <html> is always on the stack and is a scope marker.
@@ -460,10 +460,10 @@ bool HTMLElementStack::hasNumberedHeaderElementInScope() const
 bool HTMLElementStack::inScope(Element* targetElement) const
 {
     for (ElementRecord* pos = m_top.get(); pos; pos = pos->next()) {
-        Element* element = pos->element();
-        if (element == targetElement)
+        ContainerNode* node = pos->node();
+        if (node == targetElement)
             return true;
-        if (isScopeMarker(element))
+        if (isScopeMarker(node))
             return false;
     }
     ASSERT_NOT_REACHED(); // <html> is always on the stack and is a scope marker.
