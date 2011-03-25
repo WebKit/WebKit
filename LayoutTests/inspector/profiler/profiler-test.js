df0160c244fb8f71b9f967ee4af774ff82a2e2d3
Mikhail Naganov <mnaganov@chromium.org>
2011-03-25 07:17:45 +0000

    2011-03-24  Mikhail Naganov  <mnaganov@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: Rework cpu-profiler-profiling.html to use capabilities of testing framework.
            https://bugs.webkit.org/show_bug.cgi?id=57006
    
            * http/tests/inspector/profiler-test.js: Added.
            (initialize_ProfilerTest.InspectorTest.startProfilerTest.startTest):
            (initialize_ProfilerTest.InspectorTest.startProfilerTest):
            (initialize_ProfilerTest.InspectorTest.completeProfilerTest):
            (initialize_ProfilerTest.InspectorTest.runProfilerTestSuite.runner):
            (initialize_ProfilerTest.InspectorTest.runProfilerTestSuite):
            (initialize_ProfilerTest):
            * inspector/cpu-profiler-profiling-expected.txt: Removed.
            * inspector/cpu-profiler-profiling.html: Removed.
            * inspector/profiler/cpu-profiler-profiling-expected.txt: Added.
            * inspector/profiler/cpu-profiler-profiling.html: Added.
    
    Canonical link: https://commits.webkit.org/71753@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@81945 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/inspector/profiler/profiler-test.js
@@ -0,0 +1,94 @@
+var initialize_ProfilerTest = function() {
+
+InspectorTest.startProfilerTest = function(callback)
+{
+    WebInspector.showPanel("profiles");
+
+    function startTest()
+    {
+        InspectorTest.addResult("Profiler was enabled.");
+        InspectorTest.addSniffer(WebInspector.panels.profiles, "_addProfileHeader", InspectorTest._profileHeaderAdded, true);
+        InspectorTest.addSniffer(WebInspector.CPUProfileView.prototype, "refresh", InspectorTest._profileViewRefresh, true);
+        InspectorTest.safeWrap(callback)();
+    }
+
+    if (WebInspector.panels.profiles._profilerEnabled)
+        startTest();
+    else {
+        InspectorTest.addSniffer(WebInspector.panels.profiles, "_profilerWasEnabled", startTest);
+        WebInspector.panels.profiles._toggleProfiling(false);
+    }
+};
+
+InspectorTest.completeProfilerTest = function()
+{
+    function completeTest()
+    {
+        InspectorTest.addResult("");
+        InspectorTest.addResult("Profiler was disabled.");
+        InspectorTest.completeTest();
+    }
+
+    var profilesPanel = WebInspector.panels.profiles;
+    if (!profilesPanel._profilerEnabled)
+        completeTest();
+    else {
+        InspectorTest.addSniffer(WebInspector.panels.profiles, "_profilerWasDisabled", completeTest);
+        profilesPanel._toggleProfiling(false);
+    }
+};
+
+InspectorTest.runProfilerTestSuite = function(testSuite)
+{
+    var testSuiteTests = testSuite.slice();
+
+    function runner()
+    {
+        if (!testSuiteTests.length) {
+            InspectorTest.completeProfilerTest();
+            return;
+        }
+
+        var nextTest = testSuiteTests.shift();
+        InspectorTest.addResult("");
+        InspectorTest.addResult("Running: " + /function\s([^(]*)/.exec(nextTest)[1]);
+        InspectorTest.safeWrap(nextTest)(runner, runner);
+    }
+
+    InspectorTest.startProfilerTest(runner);
+};
+
+InspectorTest.showProfileWhenAdded = function(title)
+{
+    InspectorTest._showProfileWhenAdded = title;
+};
+
+InspectorTest._profileHeaderAdded = function(profile)
+{
+    if (InspectorTest._showProfileWhenAdded === profile.title) {
+        WebInspector.panels.profiles.showProfile(profile);
+    }
+};
+
+InspectorTest.waitUntilProfileViewIsShown = function(title, callback)
+{
+    callback = InspectorTest.safeWrap(callback);
+
+    var profilesPanel = WebInspector.panels.profiles;
+    if (profilesPanel.visibleView && profilesPanel.visibleView.profile.title === title)
+        callback(profilesPanel.visibleView);
+    else
+        InspectorTest._waitUntilProfileViewIsShownCallback = { title: title, callback: callback };
+}
+
+InspectorTest._profileViewRefresh = function()
+{
+    // Called in the context of ProfileView.
+    if (InspectorTest._waitUntilProfileViewIsShownCallback && InspectorTest._waitUntilProfileViewIsShownCallback.title === this.profile.title) {
+        var callback = InspectorTest._waitUntilProfileViewIsShownCallback;
+        delete InspectorTest._waitUntilProfileViewIsShownCallback;
+        callback.callback(this);
+    }
+};
+
+};
