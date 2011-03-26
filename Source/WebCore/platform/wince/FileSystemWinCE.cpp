71d22884fbeafab53f5d501b4f43d147148a18fd
Patrick Gansterer <paroga@webkit.org>
2011-03-26 07:22:44 +0000

    2011-03-26  Patrick Gansterer  <paroga@webkit.org>
            Unreviewed WinCE build fix for r82000.
    
            * platform/wince/FileSystemWinCE.cpp:
            (WebCore::openFile): Added missing function.
    
    Canonical link: https://commits.webkit.org/71830@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@82025 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/wince/FileSystemWinCE.cpp
+++ Source/WebCore/platform/wince/FileSystemWinCE.cpp
@@ -234,6 +234,25 @@ CString openTemporaryFile(const char*, PlatformFileHandle& handle)
     return proposedPath.latin1();
 }
 
+PlatformFileHandle openFile(const String& path, FileOpenMode mode)
+{
+    DWORD desiredAccess = 0;
+    DWORD creationDisposition = 0;
+    switch (mode) {
+        case OpenForRead:
+            desiredAccess = GENERIC_READ;
+            creationDisposition = OPEN_EXISTING;
+        case OpenForWrite:
+            desiredAccess = GENERIC_WRITE;
+            creationDisposition = CREATE_ALWAYS;
+        default:
+            ASSERT_NOT_REACHED();
+    }
+
+    String destination = path;
+    return CreateFile(destination.charactersWithNullTermination(), desiredAccess, 0, 0, creationDisposition, FILE_ATTRIBUTE_NORMAL, 0);
+}
+
 void closeFile(PlatformFileHandle& handle)
 {
     if (isHandleValid(handle)) {
