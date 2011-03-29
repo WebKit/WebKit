836c7647706515c2897f026a6d0732a6c9ad8819
Commit Queue <commit-queue@webkit.org>
2011-03-29 07:59:16 +0000

    2011-03-29  Ben Taylor  <bentaylor.solx86@gmail.com>
            Reviewed by Darin Adler.
    
            https://bugs.webkit.org/show_bug.cgi?id=57256
    
            Fix crash on misaligned reads on Solaris 10/Sparc
    
            * wtf/text/AtomicString.cpp:
            (WTF::equal):
    
    Canonical link: https://commits.webkit.org/72009@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@82207 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/wtf/text/AtomicString.cpp
+++ Source/JavaScriptCore/wtf/text/AtomicString.cpp
@@ -142,7 +142,7 @@ static inline bool equal(StringImpl* string, const UChar* characters, unsigned l
 
     // FIXME: perhaps we should have a more abstract macro that indicates when
     // going 4 bytes at a time is unsafe
-#if CPU(ARM) || CPU(SH4) || CPU(MIPS)
+#if CPU(ARM) || CPU(SH4) || CPU(MIPS) || CPU(SPARC)
     const UChar* stringCharacters = string->characters();
     for (unsigned i = 0; i != length; ++i) {
         if (*stringCharacters++ != *characters++)
