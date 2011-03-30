WebKit/WebKit

commit 51566ba5fc8e70f56d28080f0c3523e6d94195db

2011-03-29  Nikolas Zimmermann  <nzimmermann@rim.com>
        Reviewed by Eric Seidel.

        REGRESSION (r68976): Incorrect bidi rendering in SVG text
        https://bugs.webkit.org/show_bug.cgi?id=53980

        Deconvolute SVGTextLayoutEngine code, which was confusing due to the simultaneous processing of the rendered text
        in visual and logical order. Added several helper methods to make the code more readable.

        Fix Unicode directional formatting characters support, now works as expected.

        Test: svg/text/bidi-embedded-direction.svg

        * editing/visible_units.cpp: Refactor getLeafBoxesInLogicalOrder(), move to InlineFlowBox.
        (WebCore::getLogicalStartBoxAndNode): Use new collectLeafBoxesInLogicalOrder() method in InlineFlowBox.
        (WebCore::getLogicalEndBoxAndNode): Ditto.
        * rendering/InlineFlowBox.cpp: Add new helper function, that returns a list of all leaf boxes in logical order.
        (WebCore::InlineFlowBox::collectLeafBoxesInLogicalOrder):
        * rendering/InlineFlowBox.h:
        * rendering/svg/RenderSVGText.cpp: Actually trigger reordering the x/y/dx/dy/rotate value lists, if needed.
        (WebCore::RenderSVGText::RenderSVGText):
        (WebCore::RenderSVGText::layout):
        * rendering/svg/RenderSVGText.h: Ditto.
        (WebCore::RenderSVGText::layoutAttributes):
        (WebCore::RenderSVGText::needsReordering):
        * rendering/svg/SVGRootInlineBox.cpp: Use new InlineFlowBox::collectLeafBoxesINLogicalOrder(), with a custom "inline box reverse" implementation,
                                              which not only reverses the order of InlineBoxes, but also the order of the x/y/dx/dy/rotate value lists, if needed.
        (WebCore::SVGRootInlineBox::computePerCharacterLayoutInformation):
        (WebCore::SVGRootInlineBox::layoutCharactersInTextBoxes):
        (WebCore::swapItems):
        (WebCore::reverseInlineBoxRangeAndValueListsIfNeeded):

LayoutTests/platform/mac/svg/text/bidi-embedded-direction-expected.txt
@@ -1,0 +1,31 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 150x275
+  RenderSVGRoot {svg} at (9,50) size 113x200
+    RenderSVGPath {line} at (9,50) size 2x200 [stroke={[type=SOLID] [color=#000000]}] [fill={[type=SOLID] [color=#000000]}] [x1=10.00] [y1=25.00] [x2=10.00] [y2=225.00]
+    RenderSVGText {text} at (10,28) size 111x28 contains 1 chunk(s)
+      RenderSVGInlineText {#text} at (56,0) size 55x28
+        chunk 1 text run 1 at (66.00,50.00) startOffset 0 endOffset 5 width 55.00 RTL: "\x{5E9}\x{5DC}\x{5D5}\x{5DD} "
+      RenderSVGTSpan {tspan} at (0,0) size 56x28
+        RenderSVGInlineText {#text} at (0,0) size 56x28
+          chunk 1 text run 1 at (10.00,50.00) startOffset 0 endOffset 5 width 56.00: "world"
+      RenderSVGInlineText {#text} at (0,0) size 0x0
+    RenderSVGText {text} at (10,78) size 111x28 contains 1 chunk(s)
+      RenderSVGInlineText {#text} at (-10,-78) size 111x28
+        chunk 1 text run 1 at (10.00,100.00) startOffset 0 endOffset 1 width 0.00 RTL: "\x{202C}"
+        chunk 1 text run 1 at (10.00,100.00) startOffset 0 endOffset 6 width 56.00: "\x{202A}world"
+        chunk 1 text run 1 at (66.00,100.00) startOffset 0 endOffset 6 width 55.00 RTL: "\x{202B}\x{5E9}\x{5DC}\x{5D5}\x{5DD} "
+        chunk 1 text run 1 at (121.00,100.00) startOffset 0 endOffset 1 width 0.00: "\x{202C}"
+    RenderSVGText {text} at (10,128) size 112x28 contains 1 chunk(s)
+      RenderSVGInlineText {#text} at (56,0) size 56x28
+        chunk 1 text run 1 at (66.00,150.00) startOffset 0 endOffset 6 width 56.00 RTL override: "hello "
+      RenderSVGTSpan {tspan} at (0,0) size 56x28
+        RenderSVGInlineText {#text} at (0,0) size 56x28
+          chunk 1 text run 1 at (10.00,150.00) startOffset 0 endOffset 5 width 56.00: "world"
+      RenderSVGInlineText {#text} at (0,0) size 0x0
+    RenderSVGText {text} at (10,178) size 112x28 contains 1 chunk(s)
+      RenderSVGInlineText {#text} at (-10,-178) size 112x28
+        chunk 1 text run 1 at (10.00,200.00) startOffset 0 endOffset 1 width 0.00 RTL override: "\x{202C}"
+        chunk 1 text run 1 at (10.00,200.00) startOffset 0 endOffset 6 width 56.00: "\x{202A}world"
+        chunk 1 text run 1 at (66.00,200.00) startOffset 0 endOffset 7 width 56.00 RTL override: "\x{202E}hello "
+        chunk 1 text run 1 at (122.00,200.00) startOffset 0 endOffset 1 width 0.00: "\x{202C}"

LayoutTests/platform/mac/svg/text/bidi-embedded-direction-expected.checksum
@@ -1,0 +1,1 @@
+3e4c13dd888e9dab2a250e82caca31cf

LayoutTests/platform/mac/svg/text/bidi-reorder-value-lists-expected.checksum
@@ -1,1 +1,1 @@
-b5700b4960426f2c6994d4d54f163f7f
+dcef2f4c8ed6fdd9483bd4070cf5864b

LayoutTests/platform/mac/svg/text/bidi-reorder-value-lists-expected.txt
@@ -1,25 +1,52 @@
 layer at (0,0) size 800x600
   RenderView at (0,0) size 800x600
 layer at (0,0) size 400x400
-  RenderSVGRoot {svg} at (40,60) size 360x196
-    RenderSVGContainer {g} at (40,60) size 360x196 [transform={m=((4.00,0.00)(0.00,4.00)) t=(0.00,0.00)}]
-      RenderSVGText {text} at (10,15) size 90x19 contains 1 chunk(s)
+  RenderSVGRoot {svg} at (30,15) size 270x237
+    RenderSVGContainer {g} at (30,15) size 270x237 [transform={m=((3.00,0.00)(0.00,3.00)) t=(0.00,0.00)}]
+      RenderSVGText {text} at (10,5) size 90x19 contains 1 chunk(s)
         RenderSVGInlineText {#text} at (0,0) size 90x19
-          chunk 1 text run 1 at (10.00,30.00) startOffset 0 endOffset 1 width 9.75: "T"
-          chunk 1 text run 1 at (20.00,30.00) startOffset 0 endOffset 1 width 7.00: "e"
-          chunk 1 text run 1 at (30.00,30.00) startOffset 0 endOffset 1 width 6.25: "s"
-          chunk 1 text run 1 at (40.00,30.00) startOffset 0 endOffset 1 width 4.50: "t"
-          chunk 1 text run 1 at (50.00,30.00) startOffset 0 endOffset 1 width 4.00: " "
-          chunk 1 text run 1 at (54.00,30.00) startOffset 0 endOffset 5 width 45.57 RTL: "\x{5D0}\x{5D1}\x{5D2}\x{5D3}\x{5D4}"
+          chunk 1 text run 1 at (10.00,20.00) startOffset 0 endOffset 1 width 9.67: "T"
+          chunk 1 text run 1 at (20.00,20.00) startOffset 0 endOffset 1 width 7.00: "e"
+          chunk 1 text run 1 at (30.00,20.00) startOffset 0 endOffset 1 width 6.33: "s"
+          chunk 1 text run 1 at (40.00,20.00) startOffset 0 endOffset 1 width 4.33: "t"
+          chunk 1 text run 1 at (50.00,20.00) startOffset 0 endOffset 1 width 4.00: " "
+          chunk 1 text run 1 at (54.00,20.00) startOffset 0 endOffset 5 width 45.57 RTL: "\x{5D0}\x{5D1}\x{5D2}\x{5D3}\x{5D4}"
+      RenderSVGText {text} at (10,25) size 89x19 contains 1 chunk(s)
+        RenderSVGTSpan {tspan} at (0,0) size 89x19
+          RenderSVGInlineText {#text} at (0,0) size 89x19
+            chunk 1 text run 1 at (10.00,40.00) startOffset 0 endOffset 1 width 9.67: "T"
+            chunk 1 text run 1 at (20.00,40.00) startOffset 0 endOffset 1 width 7.00: "e"
+            chunk 1 text run 1 at (30.00,40.00) startOffset 0 endOffset 1 width 6.33: "s"
+            chunk 1 text run 1 at (40.00,40.00) startOffset 0 endOffset 1 width 4.33: "t"
+            chunk 1 text run 1 at (50.00,40.00) startOffset 0 endOffset 1 width 4.00: " "
+            chunk 1 text run 1 at (55.00,40.00) startOffset 0 endOffset 1 width 11.39 RTL: "\x{5D4}"
+            chunk 1 text run 1 at (64.00,40.00) startOffset 0 endOffset 1 width 8.02 RTL: "\x{5D3}"
+            chunk 1 text run 1 at (72.00,40.00) startOffset 0 endOffset 1 width 6.84 RTL: "\x{5D2}"
+            chunk 1 text run 1 at (79.00,40.00) startOffset 0 endOffset 1 width 9.01 RTL: "\x{5D1}"
+            chunk 1 text run 1 at (88.00,40.00) startOffset 0 endOffset 1 width 10.32 RTL: "\x{5D0}"
       RenderSVGText {text} at (10,45) size 89x19 contains 1 chunk(s)
-        RenderSVGInlineText {#text} at (0,0) size 89x19
-          chunk 1 text run 1 at (10.00,60.00) startOffset 0 endOffset 1 width 9.75: "T"
+        RenderSVGInlineText {#text} at (0,0) size 44x19
+          chunk 1 text run 1 at (10.00,60.00) startOffset 0 endOffset 1 width 9.67: "T"
           chunk 1 text run 1 at (20.00,60.00) startOffset 0 endOffset 1 width 7.00: "e"
-          chunk 1 text run 1 at (30.00,60.00) startOffset 0 endOffset 1 width 6.25: "s"
-          chunk 1 text run 1 at (40.00,60.00) startOffset 0 endOffset 1 width 4.50: "t"
+          chunk 1 text run 1 at (30.00,60.00) startOffset 0 endOffset 1 width 6.33: "s"
+          chunk 1 text run 1 at (40.00,60.00) startOffset 0 endOffset 1 width 4.33: "t"
           chunk 1 text run 1 at (50.00,60.00) startOffset 0 endOffset 1 width 4.00: " "
-          chunk 1 text run 1 at (55.00,60.00) startOffset 0 endOffset 1 width 11.39 RTL: "\x{5D4}"
-          chunk 1 text run 1 at (64.00,60.00) startOffset 0 endOffset 1 width 8.02 RTL: "\x{5D3}"
-          chunk 1 text run 1 at (72.00,60.00) startOffset 0 endOffset 1 width 6.84 RTL: "\x{5D2}"
-          chunk 1 text run 1 at (79.00,60.00) startOffset 0 endOffset 1 width 9.01 RTL: "\x{5D1}"
-          chunk 1 text run 1 at (88.00,60.00) startOffset 0 endOffset 1 width 10.32 RTL: "\x{5D0}"
+        RenderSVGTSpan {tspan} at (0,0) size 44x19
+          RenderSVGInlineText {#text} at (45,0) size 44x19
+            chunk 1 text run 1 at (55.00,60.00) startOffset 0 endOffset 1 width 11.39 RTL: "\x{5D4}"
+            chunk 1 text run 1 at (64.00,60.00) startOffset 0 endOffset 1 width 8.02 RTL: "\x{5D3}"
+            chunk 1 text run 1 at (72.00,60.00) startOffset 0 endOffset 1 width 6.84 RTL: "\x{5D2}"
+            chunk 1 text run 1 at (79.00,60.00) startOffset 0 endOffset 1 width 9.01 RTL: "\x{5D1}"
+            chunk 1 text run 1 at (88.00,60.00) startOffset 0 endOffset 1 width 10.32 RTL: "\x{5D0}"
+      RenderSVGText {text} at (10,65) size 89x19 contains 1 chunk(s)
+        RenderSVGInlineText {#text} at (0,0) size 89x19
+          chunk 1 text run 1 at (10.00,80.00) startOffset 0 endOffset 1 width 9.67: "T"
+          chunk 1 text run 1 at (20.00,80.00) startOffset 0 endOffset 1 width 7.00: "e"
+          chunk 1 text run 1 at (30.00,80.00) startOffset 0 endOffset 1 width 6.33: "s"
+          chunk 1 text run 1 at (40.00,80.00) startOffset 0 endOffset 1 width 4.33: "t"
+          chunk 1 text run 1 at (50.00,80.00) startOffset 0 endOffset 1 width 4.00: " "
+          chunk 1 text run 1 at (55.00,80.00) startOffset 0 endOffset 1 width 11.39 RTL: "\x{5D4}"
+          chunk 1 text run 1 at (64.00,80.00) startOffset 0 endOffset 1 width 8.02 RTL: "\x{5D3}"
+          chunk 1 text run 1 at (72.00,80.00) startOffset 0 endOffset 1 width 6.84 RTL: "\x{5D2}"
+          chunk 1 text run 1 at (79.00,80.00) startOffset 0 endOffset 1 width 9.01 RTL: "\x{5D1}"
+          chunk 1 text run 1 at (88.00,80.00) startOffset 0 endOffset 1 width 10.32 RTL: "\x{5D0}"

LayoutTests/platform/mac/svg/text/font-size-below-point-five-expected.txt
@@ -30,7 +30,6 @@ layer at (0,0) size 800x600
         chunk 1 text run 1 at (35.47,10.00) startOffset 0 endOffset 1 width 4.06: " "
       RenderSVGTSpan {tspan} at (0,0) size 0x0
         RenderSVGInlineText {#text} at (-10,5) size 0x0
-          chunk 1 text run 1 at (39.53,10.00) startOffset 0 endOffset 1 width 0.00: "6"
       RenderSVGInlineText {#text} at (0,0) size 0x0
     RenderSVGText {text} at (63,42) size 124x11 contains 1 chunk(s)
       RenderSVGInlineText {#text} at (0,0) size 124x11

LayoutTests/svg/text/bidi-embedded-direction.svg
@@ -1,0 +1,17 @@
+<svg xmlns="http://www.w3.org/2000/svg" 
+    width="150px" height="275px" viewBox="0 -25 150 275">
+  <line x1="10" y1="25" x2="10" y2="225" stroke="black" stroke-width="1"/>
+  <text x="10" y="50" font-size="24" font-family="Arial" direction="rtl" unicode-bidi="embed">
+    שלום <tspan direction="ltr" unicode-bidi="embed">world</tspan>
+  </text>
+  <text x="10" y="100" font-size="24" font-family="Arial">
+    &#x202B;שלום &#x202A;world&#x202C;&#x202C;
+  </text>
+
+  <text x="10" y="150" font-size="24" font-family="Arial" direction="rtl" unicode-bidi="bidi-override">
+    hello <tspan direction="ltr" unicode-bidi="embed">world</tspan>
+  </text>
+  <text x="10" y="200" font-size="24" font-family="Arial">
+    &#x202E;hello &#x202A;world&#x202C;&#x202C;
+  </text>
+</svg>

commit 01f30abceb3428d03e67bf83053b8db19fa8c699

[Leopard Intel Release Tests] svg/text/bidi-embedded-direction.svg failing since introduction in r82411. https://bugs.webkit.org/show_bug.cgi?id=57458
Unreviewed, add the platform-specific failing results to get the bots green.

NOTE: The platform-specific failing pixel test results still need to be added.

* platform/mac-leopard/svg/text/bidi-embedded-direction-expected.txt: Added.



Canonical link: https://commits.webkit.org/72249@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@82451 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/mac-leopard/svg/text/bidi-embedded-direction-expected.txt
@@ -1,0 +1,31 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 150x275
+  RenderSVGRoot {svg} at (9,50) size 119x200
+    RenderSVGPath {line} at (9,50) size 2x200 [stroke={[type=SOLID] [color=#000000]}] [fill={[type=SOLID] [color=#000000]}] [x1=10.00] [y1=25.00] [x2=10.00] [y2=225.00]
+    RenderSVGText {text} at (10,28) size 118x28 contains 1 chunk(s)
+      RenderSVGInlineText {#text} at (56,0) size 62x28
+        chunk 1 text run 1 at (66.00,50.00) startOffset 0 endOffset 5 width 61.48 RTL: "\x{5E9}\x{5DC}\x{5D5}\x{5DD} "
+      RenderSVGTSpan {tspan} at (0,0) size 56x28
+        RenderSVGInlineText {#text} at (0,0) size 56x28
+          chunk 1 text run 1 at (10.00,50.00) startOffset 0 endOffset 5 width 56.00: "world"
+      RenderSVGInlineText {#text} at (0,0) size 0x0
+    RenderSVGText {text} at (10,78) size 118x28 contains 1 chunk(s)
+      RenderSVGInlineText {#text} at (-10,-78) size 118x28
+        chunk 1 text run 1 at (10.00,100.00) startOffset 0 endOffset 1 width 0.00 RTL: "\x{202C}"
+        chunk 1 text run 1 at (10.00,100.00) startOffset 0 endOffset 6 width 56.00: "\x{202A}world"
+        chunk 1 text run 1 at (66.00,100.00) startOffset 0 endOffset 6 width 61.48 RTL: "\x{202B}\x{5E9}\x{5DC}\x{5D5}\x{5DD} "
+        chunk 1 text run 1 at (127.48,100.00) startOffset 0 endOffset 1 width 0.00: "\x{202C}"
+    RenderSVGText {text} at (10,128) size 112x28 contains 1 chunk(s)
+      RenderSVGInlineText {#text} at (56,0) size 56x28
+        chunk 1 text run 1 at (66.00,150.00) startOffset 0 endOffset 6 width 56.00 RTL override: "hello "
+      RenderSVGTSpan {tspan} at (0,0) size 56x28
+        RenderSVGInlineText {#text} at (0,0) size 56x28
+          chunk 1 text run 1 at (10.00,150.00) startOffset 0 endOffset 5 width 56.00: "world"
+      RenderSVGInlineText {#text} at (0,0) size 0x0
+    RenderSVGText {text} at (10,178) size 112x28 contains 1 chunk(s)
+      RenderSVGInlineText {#text} at (-10,-178) size 112x28
+        chunk 1 text run 1 at (10.00,200.00) startOffset 0 endOffset 1 width 0.00 RTL override: "\x{202C}"
+        chunk 1 text run 1 at (10.00,200.00) startOffset 0 endOffset 6 width 56.00: "\x{202A}world"
+        chunk 1 text run 1 at (66.00,200.00) startOffset 0 endOffset 7 width 56.00 RTL override: "\x{202E}hello "
+        chunk 1 text run 1 at (122.00,200.00) startOffset 0 endOffset 1 width 0.00: "\x{202C}"