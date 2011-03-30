WebKit/WebKit

commit 821c2ea10af8d27ee186ad314379d69f8d7da877

Added platform-specific expected results for a new test.
* platform/mac-snowleopard/fast/text/justify-ideograph-leading-expansion-expected.txt: Added.
* platform/win/fast/text/justify-ideograph-leading-expansion-expected.txt: Added.



Canonical link: https://commits.webkit.org/71831@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@82026 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/mac-snowleopard/fast/text/justify-ideograph-leading-expansion-expected.txt
@@ -1,0 +1,41 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {DIV} at (0,0) size 784x178
+        RenderBlock {DIV} at (0,0) size 106x54 [border: (3px solid #000000)]
+          RenderText {#text} at (3,6) size 65x17
+            text run at (3,6) width 65: "a. b. "
+          RenderInline {SPAN} at (0,0) size 5x17
+            RenderText {#text} at (68,6) size 5x17
+              text run at (68,6) width 5: "i"
+          RenderText {#text} at (73,6) size 99x41
+            text run at (73,6) width 29: "\x{306F}"
+            text run at (3,30) width 63: "xxxxxxx"
+        RenderBlock {DIV} at (0,62) size 106x54 [border: (3px solid #000000)]
+          RenderText {#text} at (3,6) size 100x41
+            text run at (3,6) width 100: "a. b. i\x{306F}"
+            text run at (3,30) width 63: "xxxxxxx"
+        RenderBlock {DIV} at (0,124) size 106x54 [border: (3px solid #000000)]
+          RenderText {#text} at (3,6) size 189x41
+            text run at (3,6) width 189: "xxxxxxxxxxxxxxxxxxxxx"
+            text run at (3,30) width 48: "\x{FF08}\x{624B}\x{FF09}"
+      RenderBlock {DIV} at (0,186) size 784x178
+        RenderBlock {DIV} at (0,0) size 106x54 [border: (3px solid #000000)]
+          RenderText {#text} at (3,6) size 65x17
+            text run at (3,6) width 65: "a. b. "
+          RenderInline {SPAN} at (0,0) size 5x17
+            RenderText {#text} at (68,6) size 5x17
+              text run at (68,6) width 5: "i"
+          RenderText {#text} at (73,6) size 99x41
+            text run at (73,6) width 29: "\x{306F}"
+            text run at (3,30) width 63: "xxxxxxx"
+        RenderBlock {DIV} at (0,62) size 106x54 [border: (3px solid #000000)]
+          RenderText {#text} at (3,6) size 100x41
+            text run at (3,6) width 100: "a. b. i\x{306F}"
+            text run at (3,30) width 63: "xxxxxxx"
+        RenderBlock {DIV} at (0,124) size 106x54 [border: (3px solid #000000)]
+          RenderText {#text} at (3,6) size 189x41
+            text run at (3,6) width 189: "xxxxxxxxxxxxxxxxxxxxx"
+            text run at (3,30) width 48: "\x{FF08}\x{624B}\x{FF09}"

LayoutTests/platform/win/fast/text/justify-ideograph-leading-expansion-expected.txt
@@ -1,0 +1,41 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {DIV} at (0,0) size 784x142
+        RenderBlock {DIV} at (0,0) size 106x42 [border: (3px solid #000000)]
+          RenderText {#text} at (3,3) size 80x18
+            text run at (3,3) width 80: "a. b. "
+          RenderInline {SPAN} at (0,0) size 4x18
+            RenderText {#text} at (83,3) size 4x18
+              text run at (83,3) width 4: "i"
+          RenderText {#text} at (87,3) size 100x36
+            text run at (87,3) width 16: "\x{306F}"
+            text run at (3,21) width 56: "xxxxxxx"
+        RenderBlock {DIV} at (0,50) size 106x42 [border: (3px solid #000000)]
+          RenderText {#text} at (3,3) size 100x36
+            text run at (3,3) width 100: "a. b. i\x{306F}"
+            text run at (3,21) width 56: "xxxxxxx"
+        RenderBlock {DIV} at (0,100) size 106x42 [border: (3px solid #000000)]
+          RenderText {#text} at (3,3) size 168x36
+            text run at (3,3) width 168: "xxxxxxxxxxxxxxxxxxxxx"
+            text run at (3,21) width 32: "\x{FF08}\x{624B}\x{FF09}"
+      RenderBlock {DIV} at (0,150) size 784x142
+        RenderBlock {DIV} at (0,0) size 106x42 [border: (3px solid #000000)]
+          RenderText {#text} at (3,3) size 80x18
+            text run at (3,3) width 80: "a. b. "
+          RenderInline {SPAN} at (0,0) size 4x18
+            RenderText {#text} at (83,3) size 4x18
+              text run at (83,3) width 4: "i"
+          RenderText {#text} at (87,3) size 100x36
+            text run at (87,3) width 16: "\x{306F}"
+            text run at (3,21) width 56: "xxxxxxx"
+        RenderBlock {DIV} at (0,50) size 106x42 [border: (3px solid #000000)]
+          RenderText {#text} at (3,3) size 100x36
+            text run at (3,3) width 100: "a. b. i\x{306F}"
+            text run at (3,21) width 56: "xxxxxxx"
+        RenderBlock {DIV} at (0,100) size 106x42 [border: (3px solid #000000)]
+          RenderText {#text} at (3,3) size 168x36
+            text run at (3,3) width 168: "xxxxxxxxxxxxxxxxxxxxx"
+            text run at (3,21) width 32: "\x{FF08}\x{624B}\x{FF09}"

commit 0790fc3562efebad806eb1e57f6f1f6c8bdfa544

[Leopard Intel Release Tests] fast/text/justify-ideograph-leading-expansion.html failing since r82026. https://bugs.webkit.org/show_bug.cgi?id=57461
The Leopard results match the other Mac results, but when it started inheriting from the
Snow Leopard specific results it started failing.

Copy the mac results to the mac-leopard results to get the bots green.

* platform/mac-leopard/fast/text/justify-ideograph-leading-expansion-expected.checksum: Added.
* platform/mac-leopard/fast/text/justify-ideograph-leading-expansion-expected.png: Added.
* platform/mac-leopard/fast/text/justify-ideograph-leading-expansion-expected.txt: Added.



Canonical link: https://commits.webkit.org/72255@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@82457 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/mac-leopard/fast/text/justify-ideograph-leading-expansion-expected.checksum
@@ -1,0 +1,1 @@
+43c65ac302cdf4c6e0fa39bbb5e28dd0

LayoutTests/platform/mac-leopard/fast/text/justify-ideograph-leading-expansion-expected.txt
@@ -1,0 +1,39 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {DIV} at (0,0) size 784x154
+        RenderBlock {DIV} at (0,0) size 106x54 [border: (3px solid #000000)]
+          RenderText {#text} at (3,6) size 65x17
+            text run at (3,6) width 65: "a. b. "
+          RenderInline {SPAN} at (0,0) size 5x17
+            RenderText {#text} at (68,6) size 5x17
+              text run at (68,6) width 5: "i"
+          RenderText {#text} at (73,6) size 99x41
+            text run at (73,6) width 29: "\x{306F}"
+            text run at (3,30) width 63: "xxxxxxx"
+        RenderBlock {DIV} at (0,62) size 106x54 [border: (3px solid #000000)]
+          RenderText {#text} at (3,6) size 100x41
+            text run at (3,6) width 100: "a. b. i\x{306F}"
+            text run at (3,30) width 63: "xxxxxxx"
+        RenderBlock {DIV} at (0,124) size 106x30 [border: (3px solid #000000)]
+          RenderText {#text} at (3,6) size 237x17
+            text run at (3,6) width 237: "xxxxxxxxxxxxxxxxxxxxx\x{FF08}\x{624B}\x{FF09}"
+      RenderBlock {DIV} at (0,162) size 784x154
+        RenderBlock {DIV} at (0,0) size 106x54 [border: (3px solid #000000)]
+          RenderText {#text} at (3,6) size 65x17
+            text run at (3,6) width 65: "a. b. "
+          RenderInline {SPAN} at (0,0) size 5x17
+            RenderText {#text} at (68,6) size 5x17
+              text run at (68,6) width 5: "i"
+          RenderText {#text} at (73,6) size 99x41
+            text run at (73,6) width 29: "\x{306F}"
+            text run at (3,30) width 63: "xxxxxxx"
+        RenderBlock {DIV} at (0,62) size 106x54 [border: (3px solid #000000)]
+          RenderText {#text} at (3,6) size 100x41
+            text run at (3,6) width 100: "a. b. i\x{306F}"
+            text run at (3,30) width 63: "xxxxxxx"
+        RenderBlock {DIV} at (0,124) size 106x30 [border: (3px solid #000000)]
+          RenderText {#text} at (3,6) size 237x17
+            text run at (3,6) width 237: "xxxxxxxxxxxxxxxxxxxxx\x{FF08}\x{624B}\x{FF09}"