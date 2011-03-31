daab0b0a69adf402dc5783f6bd22a61919272c9e
Commit Queue <commit-queue@webkit.org>
2011-03-31 16:07:25 +0000

    2011-03-31  Nancy Piedra  <nancy.piedra@nokia.com>
            Reviewed by Benjamin Poulain.
    
            [Qt] ChromeClientQt.h has coding-style errors
            https://bugs.webkit.org/show_bug.cgi?id=40239
    
            * WebCoreSupport/ChromeClientQt.h:
            (WebCore::ChromeClientQt::scrollbarsModeDidChange):
            (WebCore::ChromeClientQt::needTouchEvents):
            (WebCore::ChromeClientQt::formStateDidChange):
            (WebCore::ChromeClientQt::scrollRectIntoView):
            (WebCore::ChromeClientQt::requestGeolocationPermissionForFrame):
            (WebCore::ChromeClientQt::cancelGeolocationPermissionRequestForFrame):
    
    Canonical link: https://commits.webkit.org/72381@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@82585 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/qt/WebCoreSupport/ChromeClientQt.h
+++ Source/WebKit/qt/WebCoreSupport/ChromeClientQt.h
@@ -26,15 +26,15 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef ChromeClientQt_H
-#define ChromeClientQt_H
+#ifndef ChromeClientQt_h
+#define ChromeClientQt_h
 
 #include "ChromeClient.h"
 #include "FloatRect.h"
-#include "RefCounted.h"
 #include "KURL.h"
 #include "PlatformString.h"
 #include "QtPlatformPlugin.h"
+#include <wtf/RefCounted.h>
 
 QT_BEGIN_NAMESPACE
 class QEventLoop;
@@ -44,176 +44,172 @@ class QWebPage;
 
 namespace WebCore {
 
-    class FileChooser;
-    class FloatRect;
-    class Page;
-    struct FrameLoadRequest;
-    class QtAbstractWebPopup;
-    struct ViewportArguments;
+class FileChooser;
+class FloatRect;
+class Page;
+struct FrameLoadRequest;
+class QtAbstractWebPopup;
+struct ViewportArguments;
 #if ENABLE(VIDEO) && ENABLE(QT_MULTIMEDIA)
-    class FullScreenVideoQt;
+class FullScreenVideoQt;
 #endif
 
-    class ChromeClientQt : public ChromeClient
-    {
-    public:
-        ChromeClientQt(QWebPage* webPage);
-        virtual ~ChromeClientQt();
-        virtual void chromeDestroyed();
+class ChromeClientQt : public ChromeClient {
+public:
+    ChromeClientQt(QWebPage*);
+    virtual ~ChromeClientQt();
+    virtual void chromeDestroyed();
 
-        virtual void setWindowRect(const FloatRect&);
-        virtual FloatRect windowRect();
+    virtual void setWindowRect(const FloatRect&);
+    virtual FloatRect windowRect();
 
-        virtual FloatRect pageRect();
+    virtual FloatRect pageRect();
 
-        virtual float scaleFactor();
+    virtual float scaleFactor();
 
-        virtual void focus();
-        virtual void unfocus();
+    virtual void focus();
+    virtual void unfocus();
 
-        virtual bool canTakeFocus(FocusDirection);
-        virtual void takeFocus(FocusDirection);
+    virtual bool canTakeFocus(FocusDirection);
+    virtual void takeFocus(FocusDirection);
 
-        virtual void focusedNodeChanged(Node*);
-        virtual void focusedFrameChanged(Frame*);
+    virtual void focusedNodeChanged(Node*);
+    virtual void focusedFrameChanged(Frame*);
 
-        virtual Page* createWindow(Frame*, const FrameLoadRequest&, const WindowFeatures&, const NavigationAction&);
-        virtual void show();
+    virtual Page* createWindow(Frame*, const FrameLoadRequest&, const WindowFeatures&, const NavigationAction&);
+    virtual void show();
 
-        virtual bool canRunModal();
-        virtual void runModal();
+    virtual bool canRunModal();
+    virtual void runModal();
 
-        virtual void setToolbarsVisible(bool);
-        virtual bool toolbarsVisible();
+    virtual void setToolbarsVisible(bool);
+    virtual bool toolbarsVisible();
 
-        virtual void setStatusbarVisible(bool);
-        virtual bool statusbarVisible();
+    virtual void setStatusbarVisible(bool);
+    virtual bool statusbarVisible();
 
-        virtual void setScrollbarsVisible(bool);
-        virtual bool scrollbarsVisible();
+    virtual void setScrollbarsVisible(bool);
+    virtual bool scrollbarsVisible();
 
-        virtual void setMenubarVisible(bool);
-        virtual bool menubarVisible();
+    virtual void setMenubarVisible(bool);
+    virtual bool menubarVisible();
 
-        virtual void setResizable(bool);
+    virtual void setResizable(bool);
 
-        virtual void addMessageToConsole(MessageSource, MessageType, MessageLevel, const String& message,
-                                         unsigned int lineNumber, const String& sourceID);
+    virtual void addMessageToConsole(MessageSource, MessageType, MessageLevel, const String& message, unsigned int lineNumber, const String& sourceID);
 
-        virtual bool canRunBeforeUnloadConfirmPanel();
-        virtual bool runBeforeUnloadConfirmPanel(const String& message, Frame* frame);
+    virtual bool canRunBeforeUnloadConfirmPanel();
+    virtual bool runBeforeUnloadConfirmPanel(const String& message, Frame*);
 
-        virtual void closeWindowSoon();
+    virtual void closeWindowSoon();
 
-        virtual void runJavaScriptAlert(Frame*, const String&);
-        virtual bool runJavaScriptConfirm(Frame*, const String&);
-        virtual bool runJavaScriptPrompt(Frame*, const String& message, const String& defaultValue, String& result);
-        virtual bool shouldInterruptJavaScript();
+    virtual void runJavaScriptAlert(Frame*, const String&);
+    virtual bool runJavaScriptConfirm(Frame*, const String&);
+    virtual bool runJavaScriptPrompt(Frame*, const String& message, const String& defaultValue, String& result);
+    virtual bool shouldInterruptJavaScript();
 
-        virtual void setStatusbarText(const String&);
+    virtual void setStatusbarText(const String&);
 
-        virtual WebCore::KeyboardUIMode keyboardUIMode();
-        virtual IntRect windowResizerRect() const;
+    virtual KeyboardUIMode keyboardUIMode();
+    virtual IntRect windowResizerRect() const;
 
-        virtual void invalidateWindow(const IntRect&, bool);
-        virtual void invalidateContentsAndWindow(const IntRect&, bool);
-        virtual void invalidateContentsForSlowScroll(const IntRect&, bool);
-        virtual void scroll(const IntSize& scrollDelta, const IntRect& rectToScroll, const IntRect& clipRect);
+    virtual void invalidateWindow(const IntRect&, bool);
+    virtual void invalidateContentsAndWindow(const IntRect&, bool);
+    virtual void invalidateContentsForSlowScroll(const IntRect&, bool);
+    virtual void scroll(const IntSize& scrollDelta, const IntRect& rectToScroll, const IntRect& clipRect);
 #if ENABLE(TILED_BACKING_STORE)
-        virtual void delegatedScrollRequested(const IntPoint& scrollPoint);
+    virtual void delegatedScrollRequested(const IntPoint& scrollPoint);
 #endif
 
-        virtual IntPoint screenToWindow(const IntPoint&) const;
-        virtual IntRect windowToScreen(const IntRect&) const;
-        virtual PlatformPageClient platformPageClient() const;
-        virtual void contentsSizeChanged(Frame*, const IntSize&) const;
+    virtual IntPoint screenToWindow(const IntPoint&) const;
+    virtual IntRect windowToScreen(const IntRect&) const;
+    virtual PlatformPageClient platformPageClient() const;
+    virtual void contentsSizeChanged(Frame*, const IntSize&) const;
 
-        virtual void scrollbarsModeDidChange() const { }
-        virtual void mouseDidMoveOverElement(const HitTestResult&, unsigned modifierFlags);
+    virtual void scrollbarsModeDidChange() const { }
+    virtual void mouseDidMoveOverElement(const HitTestResult&, unsigned modifierFlags);
 
-        virtual void setToolTip(const String&, TextDirection);
+    virtual void setToolTip(const String&, TextDirection);
 
-        virtual void print(Frame*);
+    virtual void print(Frame*);
 #if ENABLE(DATABASE)
-        virtual void exceededDatabaseQuota(Frame*, const String&);
+    virtual void exceededDatabaseQuota(Frame*, const String&);
 #endif
 #if ENABLE(OFFLINE_WEB_APPLICATIONS)
-        virtual void reachedMaxAppCacheSize(int64_t spaceNeeded);
-        virtual void reachedApplicationCacheOriginQuota(SecurityOrigin*);
+    virtual void reachedMaxAppCacheSize(int64_t spaceNeeded);
+    virtual void reachedApplicationCacheOriginQuota(SecurityOrigin*);
 #endif
 #if ENABLE(CONTEXT_MENUS)
     virtual void showContextMenu() { }
 #endif
 
 #if ENABLE(NOTIFICATIONS)
-        virtual NotificationPresenter* notificationPresenter() const;
+    virtual NotificationPresenter* notificationPresenter() const;
 #endif
 
 #if USE(ACCELERATED_COMPOSITING)
-        // see ChromeClient.h
-        // this is a hook for WebCore to tell us what we need to do with the GraphicsLayers
-        virtual void attachRootGraphicsLayer(Frame*, GraphicsLayer*);
-        virtual void setNeedsOneShotDrawingSynchronization();
-        virtual void scheduleCompositingLayerSync();
-        virtual CompositingTriggerFlags allowedCompositingTriggers() const;
+    // This is a hook for WebCore to tell us what we need to do with the GraphicsLayers.
+    virtual void attachRootGraphicsLayer(Frame*, GraphicsLayer*);
+    virtual void setNeedsOneShotDrawingSynchronization();
+    virtual void scheduleCompositingLayerSync();
+    virtual CompositingTriggerFlags allowedCompositingTriggers() const;
 #endif
 
 #if ENABLE(TILED_BACKING_STORE)
-        virtual IntRect visibleRectForTiledBackingStore() const;
+    virtual IntRect visibleRectForTiledBackingStore() const;
 #endif
 
 #if ENABLE(TOUCH_EVENTS)
-        virtual void needTouchEvents(bool) { }
+    virtual void needTouchEvents(bool) { }
 #endif
  
 #if ENABLE(VIDEO) && ENABLE(QT_MULTIMEDIA)
-        virtual bool supportsFullscreenForNode(const Node*);
-        virtual void enterFullscreenForNode(Node*);
-        virtual void exitFullscreenForNode(Node*);
-        virtual bool requiresFullscreenForVideoPlayback();
-        FullScreenVideoQt* fullScreenVideo();
+    virtual bool supportsFullscreenForNode(const Node*);
+    virtual void enterFullscreenForNode(Node*);
+    virtual void exitFullscreenForNode(Node*);
+    virtual bool requiresFullscreenForVideoPlayback();
+    FullScreenVideoQt* fullScreenVideo();
 #endif
+     virtual void runOpenPanel(Frame*, PassRefPtr<FileChooser>);
+     virtual void chooseIconForFiles(const Vector<String>&, FileChooser*);
 
-        virtual void runOpenPanel(Frame*, PassRefPtr<FileChooser>);
-        virtual void chooseIconForFiles(const Vector<String>&, FileChooser*);
+    virtual void formStateDidChange(const Node*) { }
 
-        virtual void formStateDidChange(const Node*) { }
+    virtual void setCursor(const Cursor&);
 
-        virtual void setCursor(const Cursor&);
+    virtual void scrollRectIntoView(const IntRect&, const ScrollView*) const { }
 
-        virtual void scrollRectIntoView(const IntRect&, const ScrollView*) const { }
+    virtual void requestGeolocationPermissionForFrame(Frame*, Geolocation*) { }
+    virtual void cancelGeolocationPermissionRequestForFrame(Frame*, Geolocation*) { }
 
-        virtual void requestGeolocationPermissionForFrame(Frame*, Geolocation*) { }
-        virtual void cancelGeolocationPermissionRequestForFrame(Frame*, Geolocation*) { }
+    virtual bool selectItemWritingDirectionIsNatural();
+    virtual bool selectItemAlignmentFollowsMenuWritingDirection();
+    virtual PassRefPtr<PopupMenu> createPopupMenu(PopupMenuClient*) const;
+    virtual PassRefPtr<SearchPopupMenu> createSearchPopupMenu(PopupMenuClient*) const;
+    virtual void populateVisitedLinks();
 
-        virtual bool selectItemWritingDirectionIsNatural();
-        virtual bool selectItemAlignmentFollowsMenuWritingDirection();
-        virtual PassRefPtr<PopupMenu> createPopupMenu(PopupMenuClient*) const;
-        virtual PassRefPtr<SearchPopupMenu> createSearchPopupMenu(PopupMenuClient*) const;
-        virtual void populateVisitedLinks();
+    QWebSelectMethod* createSelectPopup() const;
 
-        QWebSelectMethod* createSelectPopup() const;
+    virtual void dispatchViewportDataDidChange(const ViewportArguments&) const;
 
-        virtual void dispatchViewportDataDidChange(const ViewportArguments&) const;
+    QWebPage* m_webPage;
+    KURL lastHoverURL;
+    String lastHoverTitle;
+    String lastHoverContent;
 
-        QWebPage* m_webPage;
-        WebCore::KURL lastHoverURL;
-        WTF::String lastHoverTitle;
-        WTF::String lastHoverContent;
-
-        bool toolBarsVisible;
-        bool statusBarVisible;
-        bool menuBarVisible;
-        QEventLoop* m_eventLoop;
+    bool toolBarsVisible;
+    bool statusBarVisible;
+    bool menuBarVisible;
+    QEventLoop* m_eventLoop;
 
 #if ENABLE(VIDEO) && ENABLE(QT_MULTIMEDIA)
-        FullScreenVideoQt* m_fullScreenVideo;
+    FullScreenVideoQt* m_fullScreenVideo;
 #endif
 
-        static bool dumpVisitedLinksCallbacks;
+    static bool dumpVisitedLinksCallbacks;
 
-        mutable QtPlatformPlugin m_platformPlugin;
-    };
+    mutable QtPlatformPlugin m_platformPlugin;
+};
 }
 
 #endif
