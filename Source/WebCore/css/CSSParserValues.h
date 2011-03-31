5ecae42f6fa10895ff0c5be54a63bf62a6b10a07
Abhishek Arya <inferno@chromium.org>
2011-03-31 23:29:29 +0000

    2011-03-31  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r81849.
            http://trac.webkit.org/changeset/81849
            https://bugs.webkit.org/show_bug.cgi?id=57588
    
            CSS Parsing broke with -webkit-calc and introduced crashes
            (Requested by inferno-sec on #webkit).
    
            * css/CSSGrammar.y:
            * css/CSSParserValues.cpp:
            * css/CSSParserValues.h:
            * css/tokenizer.flex:
    2011-03-31  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r81849.
            http://trac.webkit.org/changeset/81849
            https://bugs.webkit.org/show_bug.cgi?id=57588
    
            CSS Parsing broke with -webkit-calc and introduced crashes
            (Requested by inferno-sec on #webkit).
    
            * css3/calc/calc-errors-expected.txt: Removed.
            * css3/calc/calc-errors.html: Removed.
            * css3/calc/minmax-errors-expected.txt: Removed.
            * css3/calc/minmax-errors.html: Removed.
            * css3/calc/simple-calcs-expected.txt: Removed.
            * css3/calc/simple-calcs.html: Removed.
            * css3/calc/simple-minmax-expected.txt: Removed.
            * css3/calc/simple-minmax.html: Removed.
    
    
    Canonical link: https://commits.webkit.org/72430@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@82636 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/CSSParserValues.h
+++ Source/WebCore/css/CSSParserValues.h
@@ -71,9 +71,7 @@ public:
     ~CSSParserValueList();
     
     void addValue(const CSSParserValue&);
-    void insertValueAt(unsigned, const CSSParserValue&);
     void deleteValueAt(unsigned);
-    void extend(CSSParserValueList&);
 
     unsigned size() const { return m_values.size(); }
     CSSParserValue* current() { return m_current < m_values.size() ? &m_values[m_current] : 0; }
