07da76826d728ac32d697b12c89093a33f9609a2
Martin Robinson <mrobinson@webkit.org>
2011-04-04 14:34:04 +0000

    2011-04-04  Martin Robinson  <mrobinson@igalia.com>
            Reviewed by Xan Lopez.
    
            [GTK] http/tests/plugins/post-url-file.html fails on GTK+
            https://bugs.webkit.org/show_bug.cgi?id=55826
    
            * http/tests/plugins/resources/dump-post.pl: Instead of just printing out the "keywords" parameter,
            print out the POSTDATA parameter as well. Printing "keywords" did not seem to work on the version of
            Apache/Perl/CGI that existed on my machine. According to the Perl CGI module documentation
            using 'POSTDATA' is the correct way to print the entire POSTDATA for more recent versions of the
            module.
            * platform/gtk/Skipped: Unskip the test which is now passing.
    2011-04-04  Martin Robinson  <mrobinson@igalia.com>
    
            Reviewed by Xan Lopez.
    
            [GTK] http/tests/plugins/post-url-file.html fails on GTK+
            https://bugs.webkit.org/show_bug.cgi?id=55826
    
            Correct the implementation of handlePostReadFile which uses GIO APIs and
            actually resizes the buffer to fit the entire size of the file data. This
            was likely leading to memory corruption until now.
    
            * plugins/gtk/PluginViewGtk.cpp:
            (WebCore::PluginView::handlePostReadFile): Fix this method.
    
    Canonical link: https://commits.webkit.org/72620@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@82833 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/plugins/resources/dump-post.pl
+++ LayoutTests/http/tests/plugins/resources/dump-post.pl
@@ -7,4 +7,9 @@ my $cgi = new CGI;
 # Just dump whatever was POSTed to us as text/plain.
 
 print $cgi->header('text/plain');
+
+# Different versions of the CGI module use different conventions for accessing
+# the POST data. Because at least one of them will be empty, there is no harm
+# in printing both.
+print $cgi->param('POSTDATA');
 print $cgi->param('keywords');
