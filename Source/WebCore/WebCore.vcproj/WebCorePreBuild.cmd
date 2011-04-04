90d792beecbbd642a4d711173314d1f2e1a63e1b
Steve Falkenburg <sfalken@apple.com>
2011-04-04 20:27:08 +0000

    Remove unused AnalyzeWithLargeStack code from Windows build files https://bugs.webkit.org/show_bug.cgi?id=57771
    Reviewed by Adam Roben.
    
    This was used for us to build with prefast automatically,
    but it is out-of-date and hasn't been used for some time.
    Removing completely for now.
    
    Source/WebCore:
    
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.vcproj/WebCorePreBuild.cmd:
    
    Source/WebKit/win:
    
    * WebKit.vcproj/WebKitLibPreBuild.cmd:
    
    Source/WebKit2:
    
    * win/WebKitPreBuild.cmd:
    
    WebKitLibraries:
    
    * win/tools/vsprops/common.vsprops:
    
    
    
    Canonical link: https://commits.webkit.org/72646@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@82861 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/WebCore.vcproj/WebCorePreBuild.cmd
+++ Source/WebCore/WebCore.vcproj/WebCorePreBuild.cmd
@@ -5,8 +5,4 @@ if exist "%CONFIGURATIONBUILDDIR%\buildfailed" grep XX%PROJECTNAME%XX "%CONFIGUR
 if errorlevel 1 exit 1
 echo XX%PROJECTNAME%XX > "%CONFIGURATIONBUILDDIR%\buildfailed"
 
-touch "%CONFIGURATIONBUILDDIR%\tmp.cpp"
-cl /analyze /nologo /c "%CONFIGURATIONBUILDDIR%\tmp.cpp" /Fo"%INTDIR%\tmp.obj" 2>&1 | findstr D9040
-if ERRORLEVEL 1 (set EnablePREfast="true") else (set EnablePREfast="false")
-if ERRORLEVEL 1 (set AnalyzeWithLargeStack="/analyze:65536") else (set AnalyzeWithLargeStack="")
 exit /b
--- Source/WebKit2/win/WebKitPreBuild.cmd
+++ Source/WebKit2/win/WebKitPreBuild.cmd
@@ -5,11 +5,6 @@ if exist "%CONFIGURATIONBUILDDIR%\buildfailed" grep XX%PROJECTNAME%XX "%CONFIGUR
 if errorlevel 1 exit 1
 echo XX%PROJECTNAME%XX > "%CONFIGURATIONBUILDDIR%\buildfailed"
 
-touch "%CONFIGURATIONBUILDDIR%\tmp.cpp"
-cl /analyze /nologo /c "%CONFIGURATIONBUILDDIR%\tmp.cpp" /Fo"%INTDIR%\tmp.obj" 2>&1 | findstr D9040
-if ERRORLEVEL 1 (set EnablePREfast="true") else (set EnablePREfast="false")
-if ERRORLEVEL 1 (set AnalyzeWithLargeStack="/analyze:65536") else (set AnalyzeWithLargeStack="")
-
 mkdir 2>NUL "%CONFIGURATIONBUILDDIR%\include\JavaScriptCore"
 xcopy /y /d "%WEBKITLIBRARIESDIR%\include\JavaScriptCore\*" "%CONFIGURATIONBUILDDIR%\include\JavaScriptCore"
 
