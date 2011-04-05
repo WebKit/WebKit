3b50abd89cb992ee481828c3ba49eb75e16b5f6c
Anders Carlsson <andersca@apple.com>
2011-04-05 16:22:28 +0000

    Didn't mean to land this.
    * Plugins/WebNetscapePluginView.mm:
    (-[WebNetscapePluginView getVariable:value:]):
    
    
    
    Canonical link: https://commits.webkit.org/72729@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@82945 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/mac/Plugins/WebNetscapePluginView.mm
+++ Source/WebKit/mac/Plugins/WebNetscapePluginView.mm
@@ -2063,7 +2063,7 @@ static inline void getNPRect(const NSRect& nr, NPRect& npr)
 #ifdef BUILDING_ON_TIGER
             *(NPBool *)value = FALSE;
 #else
-            *(NPBool *)value = FALSE;
+            *(NPBool *)value = TRUE;
 #endif
             return NPERR_NO_ERROR;
         }
