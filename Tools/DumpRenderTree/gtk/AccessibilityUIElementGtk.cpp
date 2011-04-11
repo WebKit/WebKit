6e3a74cb110f1ecfe4add30a507d6f9dd7d4cbd0
Mario Sanchez Prada <mario@webkit.org>
2011-04-11 18:57:35 +0000

    2011-04-11  Mario Sanchez Prada  <msanchez@igalia.com>
            Reviewed by Xan Lopez.
    
            [GTK] Implement AccessibilityUIElement::{row|column}IndexRange in DRT
            https://bugs.webkit.org/show_bug.cgi?id=57854
    
            Unskipped table-cell-spans.html and updated GTK expectations.
    
            * platform/gtk/Skipped: Unskipped test.
            * platform/gtk/accessibility/table-cell-spans-expected.txt: Added.
    2011-04-11  Mario Sanchez Prada  <msanchez@igalia.com>
    
            Reviewed by Xan Lopez.
    
            [GTK] Implement AccessibilityUIElement::{row|column}IndexRange in DRT
            https://bugs.webkit.org/show_bug.cgi?id=57854
    
            Implement missing features in GTK's DRT.
    
            * DumpRenderTree/gtk/AccessibilityUIElementGtk.cpp:
            (indexRangeInTable): New helper function to get the range string
            for the current cell inside the parent table, either from the
            point of view of rows or columns.
            (AccessibilityUIElement::rowIndexRange): Implemented by relying on
            the new helper function indexRangeInTable().
            (AccessibilityUIElement::columnIndexRange): Ditto.
    
    Canonical link: https://commits.webkit.org/73237@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@83466 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/DumpRenderTree/gtk/AccessibilityUIElementGtk.cpp
+++ Tools/DumpRenderTree/gtk/AccessibilityUIElementGtk.cpp
@@ -487,16 +487,56 @@ int AccessibilityUIElement::indexInTable()
     return 0;
 }
 
+static JSStringRef indexRangeInTable(PlatformUIElement element, bool isRowRange)
+{
+    GOwnPtr<gchar> rangeString(g_strdup("{0, 0}"));
+
+    if (!element)
+        return JSStringCreateWithUTF8CString(rangeString.get());
+
+    ASSERT(ATK_IS_OBJECT(element));
+
+    AtkObject* axTable = atk_object_get_parent(ATK_OBJECT(element));
+    if (!axTable || !ATK_IS_TABLE(axTable))
+        return JSStringCreateWithUTF8CString(rangeString.get());
+
+    // Look for the cell in the table.
+    gint indexInParent = atk_object_get_index_in_parent(ATK_OBJECT(element));
+    if (indexInParent == -1)
+        return JSStringCreateWithUTF8CString(rangeString.get());
+
+    int row = -1;
+    int column = -1;
+    row = atk_table_get_row_at_index(ATK_TABLE(axTable), indexInParent);
+    column = atk_table_get_column_at_index(ATK_TABLE(axTable), indexInParent);
+
+    // Get the actual values, if row and columns are valid values.
+    if (row != -1 && column != -1) {
+        int base = 0;
+        int length = 0;
+        if (isRowRange) {
+            base = row;
+            length = atk_table_get_row_extent_at(ATK_TABLE(axTable), row, column);
+        } else {
+            base = column;
+            length = atk_table_get_column_extent_at(ATK_TABLE(axTable), row, column);
+        }
+        rangeString.set(g_strdup_printf("{%d, %d}", base, length));
+    }
+
+    return JSStringCreateWithUTF8CString(rangeString.get());
+}
+
 JSStringRef AccessibilityUIElement::rowIndexRange()
 {
-    // FIXME: implement
-    return JSStringCreateWithCharacters(0, 0);
+    // Range in table for rows.
+    return indexRangeInTable(m_element, true);
 }
 
 JSStringRef AccessibilityUIElement::columnIndexRange()
 {
-    // FIXME: implement
-    return JSStringCreateWithCharacters(0, 0);
+    // Range in table for columns.
+    return indexRangeInTable(m_element, false);
 }
 
 int AccessibilityUIElement::lineForIndex(int)
