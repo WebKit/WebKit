42020cf46b69c1f58492f7484c9894d1169b8473
Ojan Vafai <ojan@chromium.org>
2011-04-21 23:24:13 +0000

    2011-04-21  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            ValidateChangeLogs doesn't work on SVN
            https://bugs.webkit.org/show_bug.cgi?id=59115
    
            svn-create-patch lies about the diff in the working copy by moving
            ChangeLog entries to the top of the diff.  That's fine on most cases,
            but causes problems for ValidateChangeLogs, which is trying validate
            the where the ChangeLog entry appears.
    
            I haven't added a test for this change because I couldn't figure out
            how to write one.  The issue is more of an integration issue, which
            we're not really set up to test in our unit testing framework.  If this
            patch had worked around the output from svn-create-patch, then I could
            have tested that we behave correctly on sample svn-create-patch output,
            but, in this case, I've removed the dependency on svn-create-patch.  I
            could test that we behave correctly on "svn diff" output, but we
            already have those tests.
    
            * Scripts/webkitpy/tool/commands/download_unittest.py:
            * Scripts/webkitpy/tool/mocktool.py:
            * Scripts/webkitpy/tool/steps/validatechangelogs.py:
    
    Canonical link: https://commits.webkit.org/74297@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@84560 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/layout_package/json_results_test.js
+++ Tools/Scripts/webkitpy/layout_tests/layout_package/json_results_test.js
@@ -1,9 +1,12 @@
 // To run these tests, load json_results.html in a browser.
 // You should see a series of PASS lines.
 
+var testStyles = document.createElement('style');
+testStyles.innerText = ".test-pass { color: green; } .test-fail { color: red; }";
+document.querySelector('head').appendChild(testStyles);
+
 var g_testIndex = 0;
 var g_log = ["You should see a serios of PASS lines."];
-var g_currentTestSucceeded;
 
 // Make async actually be sync for the sake of simpler testing.
 function async(func, args)
@@ -21,10 +24,10 @@ function mockResults()
         "num_passes": 0,
         "fixable": 0,
         "num_flaky": 0,
-        "layout_tests_dir":"/WEBKITROOT",
-        "uses_expectations_file":true,
-        "has_pretty_patch":true,
-        "has_wdiff":true
+        "layout_tests_dir": "/WEBKITROOT",
+        "uses_expectations_file": true,
+        "has_pretty_patch": false,
+        "has_wdiff": false
     };
 }
 
@@ -38,15 +41,22 @@ function mockExpectation(expected, actual)
     };
 }
 
-function log(msg)
+function logPass(msg)
 {
-    g_log.push('TEST-' + g_testIndex + ': ' + msg);
+    g_log.push('TEST-' + g_testIndex + ': <span class="test-pass">' + msg + '</span>')
+}
+
+function logFail(msg)
+{
+    g_log.push('TEST-' + g_testIndex + ': <span class="test-fail">' + msg + '</span>')
 }
 
 function assertTrue(bool)
 {
-    g_currentTestSucceeded &= bool;
-    log(bool ? 'PASS' : 'FAIL')
+    if (bool)
+        logPass('PASS');
+    else
+        logFail('FAIL');
 }
 
 function runTest(results, assertions)
@@ -58,21 +68,43 @@ function runTest(results, assertions)
     try {
         ADD_RESULTS(results);
         originalGeneratePage();
-        g_currentTestSucceeded = true;
     } catch (e) {
-        log("FAIL: uncaught exception " + e.toString());
-        g_currentTestSucceeded = false;
+        logFail("FAIL: uncaught exception " + e.toString());
     }
     
     try {
         assertions();
     } catch (e) {
-        log("FAIL: uncaught exception executing assertions " + e.toString());
-        g_currentTestSucceeded = false;
+        logFail("FAIL: uncaught exception executing assertions " + e.toString());
     }
+}
 
-    if (g_currentTestSucceeded)
-        log('PASS');
+function runDefaultSingleRowTest(test, expected, actual, isExpected, textResults, imageResults)
+{
+    results = mockResults();
+    results.tests[test] = mockExpectation(expected, actual);
+    runSingleRowTest(results, isExpected, textResults, imageResults);
+}
+
+function runSingleRowTest(results, isExpected, textResults, imageResults)
+{
+    for (var key in results.tests)
+        var test = key;
+    var expected = results.tests[test].expected;
+    var actual = results.tests[test].actual;
+    runTest(results, function() {
+        if (isExpected)
+            assertTrue(document.querySelector('tbody').className == 'expected');
+        else
+            assertTrue(document.querySelector('tbody').className.indexOf('expected') == -1);
+
+        assertTrue(document.querySelector('tbody td:nth-child(1)').textContent == '+' + test);
+        assertTrue(document.querySelector('tbody td:nth-child(2)').textContent == textResults);
+        assertTrue(document.querySelector('tbody td:nth-child(3)').textContent == imageResults);
+        assertTrue(document.querySelector('tbody td:nth-child(4)').textContent == actual);
+        assertTrue(document.querySelector('tbody td:nth-child(5)').textContent == expected);
+    });
+    
 }
 
 function runTests()
@@ -174,7 +206,99 @@ function runTests()
         expandAllExpectations();
         assertTrue(document.querySelectorAll('.results-row').length == 2);
     });
+  
+    runDefaultSingleRowTest('foo/bar-skip.html', 'TEXT', 'SKIP', true, '', '');
+    runDefaultSingleRowTest('foo/bar-flaky-fail.html', 'PASS FAIL', 'TEXT', true, 'expected actual diff ', '');
+    runDefaultSingleRowTest('foo/bar-flaky-fail-unexpected.html', 'PASS TEXT', 'IMAGE', false, '', 'expected actual diff ');
+    runDefaultSingleRowTest('foo/bar-crash.html', 'TEXT', 'CRASH', false, 'stack ', '');
+    runDefaultSingleRowTest('foo/bar-audio.html', 'TEXT', 'AUDIO', false, 'expected actual ', '');
+    runDefaultSingleRowTest('foo/bar-timeout.html', 'TEXT', 'TIMEOUT', false, 'expected actual diff ', '');
+    runDefaultSingleRowTest('foo/bar-image.html', 'TEXT', 'IMAGE', false, '', 'expected actual diff ');
+    runDefaultSingleRowTest('foo/bar-image-plus-text.html', 'TEXT', 'IMAGE+TEXT', false, 'expected actual diff ', 'expected actual diff ');
+
+    results = mockResults();
+    results.tests['foo/bar-reftest.html'] = mockExpectation('PASS', 'IMAGE');
+    results.tests['foo/bar-reftest.html'].is_reftest = true;
+    runSingleRowTest(results, false, '', 'ref html expected actual diff ');
+
+    results = mockResults();
+    results.tests['foo/bar-reftest-mismatch.html'] = mockExpectation('PASS', 'IMAGE');
+    results.tests['foo/bar-reftest-mismatch.html'].is_mismatch_reftest = true;
+    runSingleRowTest(results, false, '', 'ref mismatch html actual ');
+
+    results = mockResults();
+    results.tests['foo/bar-flaky-pass.html'] = mockExpectation('PASS FAIL', 'PASS');
+    runTest(results, function() {
+        // FIXME: should a PASS FAIL test that passes go in the unexpected passes table?
+        assertTrue(document.body.textContent.indexOf('foo/bar-flaky-pass.html') == -1);
+    });
+
+    results = mockResults();
+    results.tests['foo/bar.html'] = mockExpectation('TEXT', 'TEXT');
+    results.uses_expectations_file = false;
+    runTest(results, function() {
+        assertTrue(document.querySelectorAll('tbody td').length == 4);
+        assertTrue(document.querySelector('tbody').className.indexOf('expected') == -1);
+    });
+
+    results = mockResults();
+    results.tests['foo/bar.html'] = mockExpectation('TEXT', 'TEXT');
+    results.has_pretty_patch = true;
+    runTest(results, function() {
+        assertTrue(document.querySelector('tbody td:nth-child(2)').textContent.indexOf('pretty diff') != -1);
+        assertTrue(document.querySelector('tbody td:nth-child(2)').textContent.indexOf('wdiff') == -1);
+    });
+
+    results = mockResults();
+    results.tests['foo/bar.html'] = mockExpectation('TEXT', 'TEXT');
+    results.has_wdiff = true;
+    runTest(results, function() {
+        assertTrue(document.querySelector('tbody td:nth-child(2)').textContent.indexOf('wdiff') != -1);
+        assertTrue(document.querySelector('tbody td:nth-child(2)').textContent.indexOf('pretty diff') == -1);
+    });
     
+    results = mockResults();
+    results.tests['foo/bar.html'] = mockExpectation('TEXT', 'PASS');
+    results.tests['foo/bar-1.html'] = mockExpectation('TEXT', 'CRASH');
+    results.tests['foo/bar-5.html'] = mockExpectation('TEXT', 'IMAGE+TEXT');
+    results.tests['foo/bar-3.html'] = mockExpectation('PASS', 'TEXT');
+    results.tests['foo/bar-2.html'] = mockExpectation('PASS', 'IMAGE');
+    runTest(results, function() {
+        // FIXME: This just ensures we don't get a JS error.
+        // Verify that the sort is correct and that inline expanded expectations
+        // move along with the test they're attached to.
+        TableSorter.sortColumn(0);
+        TableSorter.sortColumn(0);
+        TableSorter.sortColumn(1);
+        TableSorter.sortColumn(1);
+        TableSorter.sortColumn(2);
+        TableSorter.sortColumn(2);
+        TableSorter.sortColumn(3);
+        TableSorter.sortColumn(3);
+        TableSorter.sortColumn(4);
+        TableSorter.sortColumn(4);
+        TableSorter.sortColumn(0);
+        logPass('PASS');
+    });
+
+    results = mockResults();
+    results.tests['foo/bar-5.html'] = mockExpectation('TEXT', 'IMAGE+TEXT');
+    runTest(results, function() {
+        expandAllExpectations();
+        var png = document.querySelector('[src*="bar-5-expected.png"]');
+        var x = png.offsetLeft + 1;
+        var y = png.offsetTop + 1;
+        var mockEvent = {
+            target: png,
+            clientX: x,
+            clientY: y
+        }
+        PixelZoomer.showOnDelay = false;
+        PixelZoomer.handleMouseMove(mockEvent);
+        assertTrue(!!document.querySelector('.pixel-zoom-container'));
+        assertTrue(document.querySelectorAll('.zoom-image-container').length == 3);
+    });
+
     document.body.innerHTML = '<pre>' + g_log.join('\n') + '</pre>';
 }
 
