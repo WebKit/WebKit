8676eb253188a9356fe1f03f22317ca0b735469a
Adam Barth <abarth@webkit.org>
2011-04-21 15:38:37 +0000

    2011-04-21  Adam Barth  <abarth@webkit.org>
            Reviewed by Tony Gentilcore.
    
            Make test robust to PHP magic quotes
            https://bugs.webkit.org/show_bug.cgi?id=59104
    
            * http/tests/security/contentSecurityPolicy/report-uri-expected.txt:
            * http/tests/security/contentSecurityPolicy/report-uri-from-child-frame-expected.txt:
            * http/tests/security/contentSecurityPolicy/resources/save-report.php:
    
    
    Canonical link: https://commits.webkit.org/74241@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@84502 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/security/contentSecurityPolicy/resources/save-report.php
+++ LayoutTests/http/tests/security/contentSecurityPolicy/resources/save-report.php
@@ -1,13 +1,23 @@
 <?php
+function undoMagicQuotes($value) {
+    if (get_magic_quotes_gpc())
+        return stripslashes($value);
+    return $value;
+}
+
 $reportFile = fopen("csp-report.txt.tmp", 'w');
 $httpHeaders = $_SERVER;
 ksort($httpHeaders, SORT_STRING);
 foreach ($httpHeaders as $name => $value) {
-    if ($name === "CONTENT_TYPE" || $name === "HTTP_REFERER" || $name === "REQUEST_METHOD")
+    if ($name === "CONTENT_TYPE" || $name === "HTTP_REFERER" || $name === "REQUEST_METHOD") {
+        $value = undoMagicQuotes($value);
         fwrite($reportFile, "$name: $value\n");
+    }
 }
 fwrite($reportFile, "=== POST DATA ===\n");
 foreach ($_POST as $name => $value) {
+    $name = undoMagicQuotes($name);
+    $value = undoMagicQuotes($value);
     fwrite($reportFile, "$name: $value\n");
 }
 fclose($reportFile);
