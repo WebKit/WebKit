9ee48858ab79ef25e9143140fc5f61ee8abc914e
Enrica Casucci <enrica@apple.com>
2011-04-27 21:17:46 +0000

    REGRESSION (r84311): Copy should preserve background color if specified in the body only if the entire content is selected. https://bugs.webkit.org/show_bug.cgi?id=59251 <rdar://problem/9327044>
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    When we are looking for wrapping elements that are presentational, we should
    include elements that have a non transparent background color only if they
    are not blocks.
    
    Test: editing/pasteboard/do-not-copy-body-color.html
    
    * editing/markup.cpp:
    (WebCore::isElementPresentational):
    
    LayoutTests:
    
    * editing/pasteboard/do-not-copy-body-color-expected.txt: Added.
    * editing/pasteboard/do-not-copy-body-color.html: Added.
    
    
    
    Canonical link: https://commits.webkit.org/74808@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@85090 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/editing/markup.cpp
+++ Source/WebCore/editing/markup.cpp
@@ -467,7 +467,7 @@ static bool isElementPresentational(const Node* node)
     RefPtr<CSSMutableStyleDeclaration> style = styleFromMatchedRulesAndInlineDecl(node);
     if (!style)
         return false;
-    return !propertyMissingOrEqualToNone(style.get(), CSSPropertyTextDecoration) || !Editor::hasTransparentBackgroundColor(style.get());
+    return !propertyMissingOrEqualToNone(style.get(), CSSPropertyTextDecoration) || (!node->isBlockFlow() && !Editor::hasTransparentBackgroundColor(style.get()));
 }
 
 static bool shouldIncludeWrapperForFullySelectedRoot(Node* fullySelectedRoot, CSSMutableStyleDeclaration* style)
