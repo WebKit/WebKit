376b7e53c61b9d30fea162722f8d6f860b3357f0
Commit Queue <commit-queue@webkit.org>
2011-04-27 10:14:35 +0000

    2011-04-27  Lucas De Marchi  <lucas.demarchi@profusion.mobi>
            Reviewed by Darin Adler.
    
            Change the email used on bugzilla
            https://bugs.webkit.org/show_bug.cgi?id=59582
    
            * Scripts/webkitpy/common/config/committers.py:
    
    Canonical link: https://commits.webkit.org/74762@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@85043 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/config/committers.py
+++ Tools/Scripts/webkitpy/common/config/committers.py
@@ -188,7 +188,7 @@ committers_unable_to_review = [
     Committer("Leandro Pereira", ["leandro@profusion.mobi", "leandro@webkit.org"], "acidx"),
     Committer("Leo Yang", ["leo.yang@torchmobile.com.cn", "leoyang@webkit.org", "leoyang.webkit@gmail.com"], "leoyang"),
     Committer("Levi Weintraub", ["leviw@chromium.org", "leviw@google.com", "lweintraub@apple.com"], "leviw"),
-    Committer("Lucas De Marchi", ["lucas.demarchi@profusion.mobi", "demarchi@webkit.org"], "demarchi"),
+    Committer("Lucas De Marchi", ["demarchi@webkit.org", "lucas.demarchi@profusion.mobi"], "demarchi"),
     Committer("Lucas Forschler", ["lforschler@apple.com"], "lforschler"),
     Committer("Luiz Agostini", ["luiz@webkit.org", "luiz.agostini@openbossa.org"], "lca"),
     Committer("Mads Ager", "ager@chromium.org"),
