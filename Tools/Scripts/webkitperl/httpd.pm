23097985c3c28e286f80bfc33ad5261064c72f70
Eric Seidel <eric@webkit.org>
2011-04-28 05:38:00 +0000

    2011-04-27  Eric Seidel  <eric@webkit.org>
            Reviewed by Adam Barth.
    
            error_log.txt should include PHP errors
            https://bugs.webkit.org/show_bug.cgi?id=59643
    
            Turns out this was really easy after I finally learned
            enough about PHP to do it...
    
            I'm adding this in hopes of better tracking down
            our random http failures.
    
            * Scripts/webkitperl/httpd.pm:
    
    Canonical link: https://commits.webkit.org/74872@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@85155 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitperl/httpd.pm
+++ Tools/Scripts/webkitperl/httpd.pm
@@ -108,6 +108,7 @@ sub getDefaultConfigForTestDirectory
         "-c", "TypesConfig \"$typesConfig\"",
         "-c", "PidFile \"$httpdPidFile\"",
         "-c", "ScoreBoardFile \"$httpdScoreBoardFile\"",
+        "-c", "php_value log_errors 1", # PHP errors should show up in Apache's error log.
     );
 
     push @httpdArgs, (
