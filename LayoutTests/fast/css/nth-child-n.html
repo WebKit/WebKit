WebKit/WebKit

commit ddfbb639f2fd8651b56bc3d4f66838838e090103

WebCore: document.querySelector(':nth-child(foo)') should throw a syntax error https://bugs.webkit.org/show_bug.cgi?id=51231
Reviewed by Darin Adler.

Also fixes
CSSNthSelector parsing code is illegible and buggy (allows "n3" as a valid selector)
https://bugs.webkit.org/show_bug.cgi?id=21815

Add a check in the parser that other than an+b, only "odd" or "even" would be allowed
as parameters to nth selector.

Tests: fast/css/css3-nth-tokens-script.html
       fast/css/css3-nth-tokens-style.html

* css/CSSGrammar.y:
* css/CSSParser.cpp:
(WebCore::isValidNthToken):
* css/CSSParser.h:

LayoutTests: document.querySelector(':nth-child(foo)') should throw a syntax error
https://bugs.webkit.org/show_bug.cgi?id=51231
CSSNthSelector parsing code is illegible and buggy (allows "n3" as a valid selector)
https://bugs.webkit.org/show_bug.cgi?id=21815

Reviewed by Darin Adler.

* fast/css/css3-nth-tokens-script-expected.txt: Added.
* fast/css/css3-nth-tokens-script.html: Added.
* fast/css/css3-nth-tokens-style-expected.txt: Added.
* fast/css/css3-nth-tokens-style.html: Added.



Canonical link: https://commits.webkit.org/65481@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@75158 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/css/CSSParser.cpp
@@ -6309,6 +6309,13 @@ String quoteCSSURLIfNeeded(const String& string)
     return isCSSTokenizerURL(string) ? string : quoteCSSString(string);
 }
 
+bool isValidNthToken(const CSSParserString& token)
+{
+    // The tokenizer checks for the construct of an+b.
+    // nth can also accept "odd" or "even" but should not accept any other token.
+    return equalIgnoringCase(token, "odd") || equalIgnoringCase(token, "even");
+}
+
 #define YY_DECL int CSSParser::lex()
 #define yyconst const
 typedef int yy_state_type;

LayoutTests/fast/css/css3-nth-tokens-script-expected.txt
@@ -1,0 +1,26 @@
+This test passes if the nth-of-type can accept a parameter that is an+b, odd or even. But it does not accept any other parameter.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+debug("These spans should alternate red and green");
+span span span span span span span span
+debug("These spans should alternate red and green");
+span span span span span span span span
+debug("These spans should be black");
+span span span span span span span span
+debug("These spans should be black");
+span span span span span span span span
+
+PASS document.defaultView.getComputedStyle(el, null).getPropertyValue('color') is 'rgb(255, 0, 0)'
+PASS document.defaultView.getComputedStyle(el, null).getPropertyValue('color') is 'rgb(0, 128, 0)'
+PASS document.defaultView.getComputedStyle(el, null).getPropertyValue('color') is 'rgb(255, 0, 0)'
+PASS document.defaultView.getComputedStyle(el, null).getPropertyValue('color') is 'rgb(0, 128, 0)'
+PASS document.querySelector("span.c3:nth-of-type(n3)") threw exception Error: SYNTAX_ERR: DOM Exception 12.
+PASS document.querySelector("span.c3:nth-of-type(foo)") threw exception Error: SYNTAX_ERR: DOM Exception 12.
+PASS document.querySelector("span.c3:nth-of-type(2n3)") threw exception Error: SYNTAX_ERR: DOM Exception 12.
+PASS document.querySelector("span.c3:nth-of-type(foon + bar)") threw exception Error: SYNTAX_ERR: DOM Exception 12.
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/css/css3-nth-tokens-script.html
@@ -1,0 +1,72 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<link rel="stylesheet" href="../js/resources/js-test-style.css">
+<script src="../js/resources/js-test-pre.js"></script>
+<style>
+span.c1:nth-of-type(2n -1){ color: red; }
+span.c1:nth-of-type(2n){ color: green; }
+span.c2:nth-of-type(odd){ color: red; }
+span.c2:nth-of-type(even){ color: green; }
+span.c3:nth-of-type(n3){ color: red; }
+span.c3:nth-of-type(foo){ color: green; }
+span.c4:nth-of-type(2n3){ color: red; }
+span.c4:nth-of-type(foon + bar ){ color: green; }
+
+</style>
+
+</head>
+<body>
+<p id="description"></p>
+debug("These spans should alternate red and green");
+<div id="test1"></div>
+debug("These spans should alternate red and green");
+<div id="test2"></div>
+debug("These spans should be black");
+<div id="test3"></div>
+debug("These spans should be black");
+<div id="test4"></div>
+<br>
+<script>
+var i=1;
+for(; i < 5; i++) {
+    var str = "";
+    var j=1;
+    for (; j < 9; j++) {
+        str += '<span class="c' + i + '" id="span' + i + j + '"> span </span>';
+    }
+    document.getElementById("test"+i).innerHTML = str;
+}
+
+</script>
+
+<div id="console"></div>
+<script>
+description('This test passes if the nth-of-type can accept a parameter that is an+b, odd or even. But it does not accept any other parameter.');
+
+el = document.querySelector("span.c1:nth-of-type(2n -1)");
+shouldBe("document.defaultView.getComputedStyle(el, null).getPropertyValue('color')", "'rgb(255, 0, 0)'");
+
+el = document.querySelector("span.c1:nth-of-type(2n)");
+shouldBe("document.defaultView.getComputedStyle(el, null).getPropertyValue('color')", "'rgb(0, 128, 0)'");
+
+el = document.querySelector("span.c2:nth-of-type(odd)");
+shouldBe("document.defaultView.getComputedStyle(el, null).getPropertyValue('color')", "'rgb(255, 0, 0)'");
+
+el = document.querySelector("span.c2:nth-of-type(even)");
+shouldBe("document.defaultView.getComputedStyle(el, null).getPropertyValue('color')", "'rgb(0, 128, 0)'");
+
+shouldThrow('document.querySelector("span.c3:nth-of-type(n3)")', '"Error: SYNTAX_ERR: DOM Exception 12"');
+
+shouldThrow('document.querySelector("span.c3:nth-of-type(foo)")', '"Error: SYNTAX_ERR: DOM Exception 12"');
+
+shouldThrow('document.querySelector("span.c3:nth-of-type(2n3)")', '"Error: SYNTAX_ERR: DOM Exception 12"');
+
+shouldThrow('document.querySelector("span.c3:nth-of-type(foon + bar)")', '"Error: SYNTAX_ERR: DOM Exception 12"');
+
+
+var successfullyParsed = true;
+</script>
+<script src="../js/resources/js-test-post.js"></script>
+</body>
+</html>

LayoutTests/fast/css/css3-nth-tokens-style-expected.txt
@@ -1,0 +1,26 @@
+This test passes if the nth-of-type can accept a parameter that is an+b, odd or even. But it does not accept any other parameter.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+debug("These spans should alternate red and green");
+span span span span span span span span
+debug("These spans should alternate red and green");
+span span span span span span span span
+debug("These spans should be black");
+span span span span span span span span
+debug("These spans should be black");
+span span span span span span span span
+
+PASS document.defaultView.getComputedStyle(el, null).getPropertyValue('color') is 'rgb(255, 0, 0)'
+PASS document.defaultView.getComputedStyle(el, null).getPropertyValue('color') is 'rgb(0, 128, 0)'
+PASS document.defaultView.getComputedStyle(el, null).getPropertyValue('color') is 'rgb(255, 0, 0)'
+PASS document.defaultView.getComputedStyle(el, null).getPropertyValue('color') is 'rgb(0, 128, 0)'
+PASS document.defaultView.getComputedStyle(el, null).getPropertyValue('color') is 'rgb(0, 0, 0)'
+PASS document.defaultView.getComputedStyle(el, null).getPropertyValue('color') is 'rgb(0, 0, 0)'
+PASS document.defaultView.getComputedStyle(el, null).getPropertyValue('color') is 'rgb(0, 0, 0)'
+PASS document.defaultView.getComputedStyle(el, null).getPropertyValue('color') is 'rgb(0, 0, 0)'
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/css/css3-nth-tokens-style.html
@@ -1,0 +1,77 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<link rel="stylesheet" href="../js/resources/js-test-style.css">
+<script src="../js/resources/js-test-pre.js"></script>
+<style>
+span.c1:nth-of-type(2n + 1){ color: red; }
+span.c1:nth-of-type(2n - 2){ color: green; }
+span.c2:nth-of-type(odd){ color: red; }
+span.c2:nth-of-type(even){ color: green; }
+span.c3:nth-of-type(n3){ color: red; }
+span.c3:nth-of-type(foo){ color: green; }
+span.c4:nth-of-type(2n3){ color: red; }
+span.c4:nth-of-type(foon + bar ){ color: green; }
+
+</style>
+
+</head>
+<body>
+<p id="description"></p>
+debug("These spans should alternate red and green");
+<div id="test1"></div>
+debug("These spans should alternate red and green");
+<div id="test2"></div>
+debug("These spans should be black");
+<div id="test3"></div>
+debug("These spans should be black");
+<div id="test4"></div>
+<br>
+<script>
+var i=1;
+for(; i < 5; i++) {
+    var str = "";
+    var j=1;
+    for (; j < 9; j++) {
+        str += '<span class="c' + i + '" id="span' + i + j + '"> span </span>';
+    }
+    document.getElementById("test"+i).innerHTML = str;
+}
+
+</script>
+
+<div id="console"></div>
+<script>
+description('This test passes if the nth-of-type can accept a parameter that is an+b, odd or even. But it does not accept any other parameter.');
+
+
+
+el = document.getElementById("span11");
+shouldBe("document.defaultView.getComputedStyle(el, null).getPropertyValue('color')", "'rgb(255, 0, 0)'");
+
+el = document.getElementById("span12");
+shouldBe("document.defaultView.getComputedStyle(el, null).getPropertyValue('color')", "'rgb(0, 128, 0)'");
+
+el = document.getElementById("span21");
+shouldBe("document.defaultView.getComputedStyle(el, null).getPropertyValue('color')", "'rgb(255, 0, 0)'");
+
+el = document.getElementById("span22");
+shouldBe("document.defaultView.getComputedStyle(el, null).getPropertyValue('color')", "'rgb(0, 128, 0)'");
+
+el = document.getElementById("span31");
+shouldBe("document.defaultView.getComputedStyle(el, null).getPropertyValue('color')", "'rgb(0, 0, 0)'");
+
+el = document.getElementById("span32");
+shouldBe("document.defaultView.getComputedStyle(el, null).getPropertyValue('color')", "'rgb(0, 0, 0)'");
+
+el = document.getElementById("span41");
+shouldBe("document.defaultView.getComputedStyle(el, null).getPropertyValue('color')", "'rgb(0, 0, 0)'");
+
+el = document.getElementById("span42");
+shouldBe("document.defaultView.getComputedStyle(el, null).getPropertyValue('color')", "'rgb(0, 0, 0)'");
+
+var successfullyParsed = true;
+</script>
+<script src="../js/resources/js-test-post.js"></script>
+</body>
+</html>

WebCore/css/CSSGrammar.y
@@ -1205,8 +1205,11 @@ pseudo:
                  type == CSSSelector::PseudoNthOfType ||
                  type == CSSSelector::PseudoNthLastChild ||
                  type == CSSSelector::PseudoNthLastOfType) {
-            if (p->document())
-                p->document()->setUsesSiblingRules(true);
+            if (isValidNthToken($4)) {
+                if (p->document())
+                    p->document()->setUsesSiblingRules(true);
+            } else
+                $$ = 0;
         }
     }
     // used by :not

WebCore/css/CSSParser.h
@@ -359,6 +359,7 @@ namespace WebCore {
     String quoteCSSStringIfNeeded(const String&);
     String quoteCSSURLIfNeeded(const String&);
 
+    bool isValidNthToken(const CSSParserString&);
 } // namespace WebCore
 
 #endif // CSSParser_h

commit 090597d6b322178e2cfa74b2824dca43adc53f16

2011-05-01  Yael Aharon  <yael.aharon@nokia.com>
        Reviewed by Eric Seidel.

        CSS3 nth-child(n) selector fails
        https://bugs.webkit.org/show_bug.cgi?id=56943

        * fast/css/nth-child-n-expected.txt: Added.
        * fast/css/nth-child-n.html: Added.
2011-05-01  Yael Aharon  <yael.aharon@nokia.com>

        Reviewed by Eric Seidel.

        CSS3 nth-child(n) selector fails
        https://bugs.webkit.org/show_bug.cgi?id=56943

        Regression from r75158.
        "n" is a valid parameter to nth() and should be allowed.

        Test: fast/css/nth-child-n.html

        * css/CSSParser.cpp:
        (WebCore::isValidNthToken):

Canonical link: https://commits.webkit.org/75121@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@85416 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/css/CSSParser.cpp
@@ -6772,8 +6772,8 @@ String quoteCSSURLIfNeeded(const String& string)
 bool isValidNthToken(const CSSParserString& token)
 {
     // The tokenizer checks for the construct of an+b.
-    // nth can also accept "odd" or "even" but should not accept any other token.
-    return equalIgnoringCase(token, "odd") || equalIgnoringCase(token, "even");
+    // nth can also accept "n", "odd" or "even" but should not accept any other token.
+    return equalIgnoringCase(token, "odd") || equalIgnoringCase(token, "even") || equalIgnoringCase(token, "n");
 }
 
 #define YY_DECL int CSSParser::lex()

LayoutTests/fast/css/nth-child-n-expected.txt
@@ -1,0 +1,5 @@
+The test passes if all DIVs are green.
+
+DIV1
+DIV2
+Pass

LayoutTests/fast/css/nth-child-n.html
@@ -1,0 +1,57 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+<html>
+<head>
+<title>CSS Test: :nth-child(n) selector</title>
+<style type="text/css">
+div div:nth-child(n) { color: green; }
+</style>
+<script>
+if (window.layoutTestController)
+  window.layoutTestController.dumpAsText()
+</script>
+</head>
+<body>
+<p>The test passes if all DIVs are green.</p>
+<div id="test">
+<div>DIV1</div>
+<div>DIV2</div>
+</div>
+<div id="testresult" style="color: blue">Running</div>
+<script type="text/javascript">
+function ArrayContains(array, value, ci)
+{
+  ci = ci == true ? true : false;
+  for (var i = 0; i < array.length; i++)
+  {
+    if (ci && array[i].toLowerCase() == value.toLowerCase())
+    {
+      return true;
+    }
+    else if (array[i] == value)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+function TestCase()
+{
+  try
+  {
+    var greenValues = new Array("green", "#008000", "rgb(0, 128, 0)");
+
+    var elem = document.getElementById("test");
+    var val = getComputedStyle(elem.children[0], null).getPropertyValue("color");
+    if (ArrayContains(greenValues, val, false))
+    {
+      document.getElementById("testresult").innerHTML = "Pass";
+      return;
+    }
+  }catch(e){}
+  document.getElementById("testresult").innerHTML = "Fail";
+}
+TestCase();
+</script>
+</body>
+</html>