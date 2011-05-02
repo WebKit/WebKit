7c88311b4ec030b0a59551656e0de100e8d59cd6
Adam Barth <abarth@webkit.org>
2011-05-02 19:42:27 +0000

    2011-05-02  Adam Barth  <abarth@webkit.org>
            Reviewed by Eric Seidel.
    
            Add base case for a test-running EWS
            https://bugs.webkit.org/show_bug.cgi?id=59920
    
            Later I will subclass this to run the Chromium Linux EWS.
    
            * Scripts/webkitpy/tool/bot/earlywarningsystemtask.py: Added.
            * Scripts/webkitpy/tool/commands/earlywarningsystem.py:
            * Scripts/webkitpy/tool/commands/earlywarningsystem_unittest.py:
    
    
    Canonical link: https://commits.webkit.org/75207@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@85504 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/tool/commands/earlywarningsystem.py
+++ Tools/Scripts/webkitpy/tool/commands/earlywarningsystem.py
@@ -26,11 +26,15 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from webkitpy.tool.commands.queues import AbstractReviewQueue
 from webkitpy.common.config.committers import CommitterList
 from webkitpy.common.config.ports import WebKitPort
+from webkitpy.common.system.deprecated_logging import error, log
 from webkitpy.common.system.executive import ScriptError
+from webkitpy.tool.bot.expectedfailures import ExpectedFailures
+from webkitpy.tool.bot.layouttestresultsreader import LayoutTestResultsReader
 from webkitpy.tool.bot.queueengine import QueueEngine
+from webkitpy.tool.bot.earlywarningsystemtask import EarlyWarningSystemTask, EarlyWarningSystemTaskDelegate, UnableToApplyPatch
+from webkitpy.tool.commands.queues import AbstractReviewQueue
 
 
 class AbstractEarlyWarningSystem(AbstractReviewQueue):
@@ -104,6 +108,73 @@ class AbstractEarlyWarningSystem(AbstractReviewQueue):
         message = "Attachment %s did not build on %s:\nBuild output: %s" % (state["patch"].id(), cls.port_name, results_link)
         tool.bugs.post_comment_to_bug(state["patch"].bug_id(), message, cc=cls.watchers)
         exit(1)
+
+# FIXME: This should merge with AbstractEarlyWarningSystem once all the EWS
+# bots run tests.
+class AbstractTestingEWS(AbstractEarlyWarningSystem):
+    def begin_work_queue(self):
+        # FIXME: This violates abstraction
+        self._tool._port = self.port
+        AbstractEarlyWarningSystem.begin_work_queue(self)
+        self._expected_failures = ExpectedFailures()
+        self._layout_test_results_reader = LayoutTestResultsReader(self._tool, self._log_directory())
+
+    def review_patch(self, patch):
+        task = EarlyWarningSystemTask(self, patch)
+        if not task.validate():
+            self._did_error(patch, "%s did not process patch." % self.name)
+            return False
+        try:
+            return task.run()
+        except UnableToApplyPatch, e:
+            self._did_error(patch, "%s unable to apply patch." % self.name)
+            return False
+        except ScriptError, e:
+            # FIXME: We should upload the results archive on failure.
+            results_link = self._tool.status_server.results_url_for_status(task.failure_status_id)
+            message = "Attachment %s did not pass %s:\nOutput: %s" % (patch.id(), self.name, results_link)
+            results = task.results_from_patch_test_run(patch)
+            unexpected_failures = self._expected_failures.unexpected_failures(results)
+            if unexpected_failures:
+                message += "\nNew failing tests:\n%s" % "\n".join(unexpected_failures)
+            self._tool.bugs.post_comment_to_bug(patch.bug_id(), message, cc=self.watchers)
+            raise
+
+    # EarlyWarningSystemDelegate methods
+
+    def parent_command(self):
+        return self.name
+
+    def run_command(self, command):
+        self.run_webkit_patch(command + [self.port.flag()])
+
+    def command_passed(self, message, patch):
+        pass
+
+    def command_failed(self, message, script_error, patch):
+        failure_log = self._log_from_script_error_for_upload(script_error)
+        return self._update_status(message, patch=patch, results_file=failure_log)
+
+    def expected_failures(self):
+        return self._expected_failures
+
+    def layout_test_results(self):
+        return self._layout_test_results_reader.results()
+
+    def archive_last_layout_test_results(self, patch):
+        return self._layout_test_results_reader.archive(patch)
+
+    def refetch_patch(self, patch):
+        return self._tool.bugs.fetch_attachment(patch.id())
+
+    def report_flaky_tests(self, patch, flaky_test_results, results_archive):
+        pass
+
+    # StepSequenceErrorHandler methods
+
+    @classmethod
+    def handle_script_error(cls, tool, state, script_error):
+        log(script_error.message_with_output())
 
 
 class GtkEWS(AbstractEarlyWarningSystem):
