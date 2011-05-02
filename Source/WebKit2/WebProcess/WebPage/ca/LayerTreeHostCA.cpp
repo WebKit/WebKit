86f712900cd0d972d7be80f870f66b4c1f90bb80
Chris Marrin <cmarrin@apple.com>
2011-05-02 19:55:15 +0000

    2011-04-29  Chris Marrin  <cmarrin@apple.com>
            Reviewed by Simon Fraser.
    
            Pages with accelerated compositing do not redraw unless scrolled
            https://bugs.webkit.org/show_bug.cgi?id=59851
    
            Added a setNeedDisplay on the non-composited content layer after
            the size is changed so it paints the full size of the layer. Called
            only for the areas newly exposed, if any.
    
            * WebProcess/WebPage/ca/LayerTreeHostCA.cpp:
            (WebKit::LayerTreeHostCA::sizeDidChange):
    
    
    
    Canonical link: https://commits.webkit.org/75211@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@85508 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/WebPage/ca/LayerTreeHostCA.cpp
+++ Source/WebKit2/WebProcess/WebPage/ca/LayerTreeHostCA.cpp
@@ -128,7 +128,19 @@ void LayerTreeHostCA::scrollNonCompositedContents(const IntRect& scrollRect, con
 void LayerTreeHostCA::sizeDidChange(const IntSize& newSize)
 {
     m_rootLayer->setSize(newSize);
+
+    // If the newSize exposes new areas of the non-composited content a setNeedsDisplay is needed
+    // for those newly exposed areas.
+    FloatSize oldSize = m_nonCompositedContentLayer->size();
     m_nonCompositedContentLayer->setSize(newSize);
+
+    if (newSize.width() > oldSize.width()) {
+        float height = std::min(static_cast<float>(newSize.height()), oldSize.height());
+        m_nonCompositedContentLayer->setNeedsDisplayInRect(FloatRect(oldSize.width(), 0, newSize.width() - oldSize.width(), height));
+    }
+
+    if (newSize.height() > oldSize.height())
+        m_nonCompositedContentLayer->setNeedsDisplayInRect(FloatRect(0, oldSize.height(), newSize.width(), newSize.height() - oldSize.height()));
 
     if (m_pageOverlayLayer)
         m_pageOverlayLayer->setSize(newSize);
