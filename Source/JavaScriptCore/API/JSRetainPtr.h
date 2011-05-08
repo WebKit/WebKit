79c5372e5d38e9bd4aa976cff98a75e5b53635fe
Jessie Berlin <jberlin@webkit.org>
2011-05-08 20:56:30 +0000

    Make JSRetainPtr work with JSGlobalContextRefs. https://bugs.webkit.org/show_bug.cgi?id=60452
    Reviewed by Dan Bernstein.
    
    Add specialized functions for JSRetain and JSRelease when dealing with JSGlobalContextRefs.
    
    * API/JSRetainPtr.h:
    (JSRetain):
    (JSRelease):
    
    
    
    Canonical link: https://commits.webkit.org/75709@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@86036 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/API/JSRetainPtr.h
+++ Source/JavaScriptCore/API/JSRetainPtr.h
@@ -29,11 +29,14 @@
 #ifndef JSRetainPtr_h
 #define JSRetainPtr_h
 
+#include <JavaScriptCore/JSContextRef.h>
 #include <JavaScriptCore/JSStringRef.h>
 #include <algorithm>
 
 inline void JSRetain(JSStringRef string) { JSStringRetain(string); }
 inline void JSRelease(JSStringRef string) { JSStringRelease(string); }
+inline void JSRetain(JSGlobalContextRef context) { JSGlobalContextRetain(context); }
+inline void JSRelease(JSGlobalContextRef context) { JSGlobalContextRelease(context); }
 
 enum AdoptTag { Adopt };
 
