33727255c6dafa952c7fcdf043e536a5d340ccd9
Adam Barth <abarth@webkit.org>
2011-05-16 07:43:22 +0000

    2011-05-16  Adam Barth  <abarth@webkit.org>
            Attempt to fix Qt build.  (Strict PassOwnPtr fix.)
    
            This patch requires some slightly fancy footwork.
    
            * WebCoreSupport/InspectorClientQt.cpp:
            (WebCore::InspectorClientQt::openInspectorFrontend):
            (WebCore::InspectorFrontendClientQt::InspectorFrontendClientQt):
    
    
    Canonical link: https://commits.webkit.org/76192@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@86540 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/qt/WebCoreSupport/InspectorClientQt.cpp
+++ Source/WebKit/qt/WebCoreSupport/InspectorClientQt.cpp
@@ -203,8 +203,9 @@ void InspectorClientQt::inspectorDestroyed()
 void InspectorClientQt::openInspectorFrontend(WebCore::InspectorController* inspectorController)
 {
 #if ENABLE(INSPECTOR)
-    QWebView* inspectorView = new QWebView;
-    InspectorClientWebPage* inspectorPage = new InspectorClientWebPage(inspectorView);
+    OwnPtr<QWebView> inspectorView = adoptPtr(new QWebView);
+    // FIXME: Where does inspectorPage get deleted?
+    InspectorClientWebPage* inspectorPage = new InspectorClientWebPage(inspectorView.get());
     inspectorView->setPage(inspectorPage);
 
     QWebInspector* inspector = m_inspectedWebPage->d->getOrCreateInspector();
@@ -229,11 +230,13 @@ void InspectorClientQt::openInspectorFrontend(WebCore::InspectorController* insp
         inspectorPage->setProperty("_q_inspectorJavaScriptWindowObjects", inspectorJavaScriptWindowObjects);
 #endif
     inspectorView->page()->mainFrame()->load(inspectorUrl);
-    m_inspectedWebPage->d->inspectorFrontend = inspectorView;
-    inspector->d->setFrontend(inspectorView);
+    m_inspectedWebPage->d->inspectorFrontend = inspectorView.get();
+    inspector->d->setFrontend(inspectorView.get());
 
-    m_frontendClient = new InspectorFrontendClientQt(m_inspectedWebPage, inspectorView, this);
-    inspectorView->page()->d->page->inspectorController()->setInspectorFrontendClient(m_frontendClient);
+    InspectorController* inspectorController = inspectorView->page()->d->page->inspectorController();
+    OwnPtr<InspectorFrontendClientQt> frontendClient = adoptPtr(new InspectorFrontendClientQt(m_inspectedWebPage, inspectorView.release(), this));
+    m_frontendClient = frontendClient.get();
+    inspectorController->setInspectorFrontendClient(frontendClient.release());
     m_frontendWebPage = inspectorPage;
 #endif
 }
@@ -297,7 +300,7 @@ bool InspectorClientQt::sendMessageToFrontend(const String& message)
 
 #if ENABLE(INSPECTOR)
 InspectorFrontendClientQt::InspectorFrontendClientQt(QWebPage* inspectedWebPage, PassOwnPtr<QWebView> inspectorView, InspectorClientQt* inspectorClient)
-    : InspectorFrontendClientLocal(inspectedWebPage->d->page->inspectorController(), inspectorView->page()->d->page, new InspectorFrontendSettingsQt())
+    : InspectorFrontendClientLocal(inspectedWebPage->d->page->inspectorController(), inspectorView->page()->d->page, adoptPtr(new InspectorFrontendSettingsQt()))
     , m_inspectedWebPage(inspectedWebPage)
     , m_inspectorView(inspectorView)
     , m_destroyingInspectorView(false)
