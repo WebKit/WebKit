7d5319cf8ea30d3f7fc1103f51c87f13efd59443
Commit Queue <commit-queue@webkit.org>
2011-05-24 18:24:56 +0000

    2011-05-24  Mike Reed  <reed@google.com>
            Reviewed by Kenneth Russell.
    
            skia: fix stroking of zero-height rectangles
            https://bugs.webkit.org/show_bug.cgi?id=61284
    
            Tests: canvas/philip/tests/2d.line.miter.lineedge.html
                   canvas/philip/tests/2d.strokeRect.zero.4.html
    
            * platform/graphics/skia/GraphicsContextSkia.cpp:
            (WebCore::GraphicsContext::strokeRect):
    
    Canonical link: https://commits.webkit.org/76754@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@87174 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
+++ Source/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
@@ -1230,7 +1230,24 @@ void GraphicsContext::strokeRect(const FloatRect& rect, float lineWidth)
     SkPaint paint;
     platformContext()->setupPaintForStroking(&paint, 0, 0);
     paint.setStrokeWidth(WebCoreFloatToSkScalar(lineWidth));
-    platformContext()->canvas()->drawRect(rect, paint);
+    // strokerect has special rules for CSS when the rect is degenerate:
+    // if width==0 && height==0, do nothing
+    // if width==0 || height==0, then just draw line for the other dimension
+    SkRect r(rect);
+    bool validW = r.width() > 0;
+    bool validH = r.height() > 0;
+    SkCanvas* canvas = platformContext()->canvas();
+    if (validW && validH)
+        canvas->drawRect(r, paint);
+    else if (validW || validH) {
+        // we are expected to respect the lineJoin, so we can't just call
+        // drawLine -- we have to create a path that doubles back on itself.
+        SkPath path;
+        path.moveTo(r.fLeft, r.fTop);
+        path.lineTo(r.fRight, r.fBottom);
+        path.close();
+        canvas->drawPath(path, paint);
+    }
 }
 
 void GraphicsContext::rotate(float angleInRadians)
