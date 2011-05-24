0a45945fa2fee1d3cf0a354ef18fee803a9ee1d2
Lucas Forschler <lforschler@apple.com>
2011-05-24 23:13:46 +0000

    Merge radar 9489360.
    Canonical link: https://commits.webkit.org/76367.55@safari-534-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-534-branch@87226 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/wtf/Platform.h
+++ Source/JavaScriptCore/wtf/Platform.h
@@ -1022,9 +1022,9 @@
 
 /* Currently only implemented for JSVALUE64, only tested on PLATFORM(MAC) */
 #if ENABLE(JIT) && USE(JSVALUE64) && PLATFORM(MAC)
-#define ENABLE_DFG_JIT 1
+#define ENABLE_DFG_JIT 0
 /* Enabled with restrictions to circumvent known performance regressions. */
-#define ENABLE_DFG_JIT_RESTRICTIONS 1
+#define ENABLE_DFG_JIT_RESTRICTIONS 0
 #endif
 
 /* Ensure that either the JIT or the interpreter has been enabled. */
