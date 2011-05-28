1a93090142f7f69cb294aaaca5c0e5dfb393ea72
Antonio Gomes <tonikitoo@webkit.org>
2011-05-28 17:02:18 +0000

    2011-05-26  Antonio Gomes  <agomes@rim.com>
            Reviewed by Andreas Kling.
    
            Logic error in WebCore/Page/SpatialNavigation.cpp::areRectsPartial      lyAligned
            https://bugs.webkit.org/show_bug.cgi?id=61606
    
            Removed duplicated statement within if condition.
    
            No new tests. Obvious clean up patch.
    
            * page/SpatialNavigation.cpp:
            (WebCore::areRectsPartiallyAligned):
    
    Canonical link: https://commits.webkit.org/77124@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@87616 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/SpatialNavigation.cpp
+++ Source/WebCore/page/SpatialNavigation.cpp
@@ -234,7 +234,6 @@ static bool areRectsPartiallyAligned(FocusDirection direction, const IntRect& a,
     //
     // ... and variants of the above cases.
     return ((bStart >= aStart && bStart <= aEnd)
-            || (bStart >= aStart && bStart <= aEnd)
             || (bEnd >= aStart && bEnd <= aEnd)
             || (bMiddle >= aStart && bMiddle <= aEnd)
             || (bEnd >= aStart && bEnd <= aEnd));
