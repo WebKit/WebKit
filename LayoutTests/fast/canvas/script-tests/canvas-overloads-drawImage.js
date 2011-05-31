ee9aafa8bbbc69d3f5678ae397b56228f2701ae0
Andreas Kling <andreas.kling@nokia.com>
2011-05-31 16:22:31 +0000

    2011-05-31  Andreas Kling  <kling@webkit.org>
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for drawImage()
            https://bugs.webkit.org/show_bug.cgi?id=61703
    
            Move CanvasRenderingContext2D.drawImage() to auto-generated JSC bindings.
    
            There is a subtle difference to the previous behavior: invalid numbers of
            arguments now raise TypeError instead of SyntaxError. This is in accordance
            with Web IDL, and matches the existing V8 bindings.
    
            Test: fast/canvas/canvas-overloads-drawImage.html
    
            * bindings/js/JSCanvasRenderingContext2DCustom.cpp:
            * html/canvas/CanvasRenderingContext2D.idl:
    2011-05-31  Andreas Kling  <kling@webkit.org>
    
            Reviewed by Antti Koivisto.
    
            Canvas/JSC: Auto-generate overloads for drawImage()
            https://bugs.webkit.org/show_bug.cgi?id=61703
    
            Add a test to verify the behavior of drawImage() when called with
            different numbers of arguments.
    
            * fast/canvas/canvas-overloads-drawImage-expected.txt: Added.
            * fast/canvas/canvas-overloads-drawImage.html: Added.
            * fast/canvas/script-tests/canvas-overloads-drawImage.js: Added.
    
    
    Canonical link: https://commits.webkit.org/77214@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@87734 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/canvas/script-tests/canvas-overloads-drawImage.js
@@ -0,0 +1,34 @@
+description("Test the behavior of CanvasRenderingContext2D.drawImage() when called with different numbers of arguments.");
+
+var ctx = document.createElement('canvas').getContext('2d');
+
+var TypeError = "TypeError: Type error";
+
+var imageElement = document.createElement("img");
+shouldThrow("ctx.drawImage()", "TypeError");
+shouldThrow("ctx.drawImage(imageElement)", "TypeError");
+shouldThrow("ctx.drawImage(imageElement, 0)", "TypeError");
+shouldBe("ctx.drawImage(imageElement, 0, 0)", "undefined");
+shouldThrow("ctx.drawImage(imageElement, 0, 0, 0)", "TypeError");
+shouldBe("ctx.drawImage(imageElement, 0, 0, 0, 0)", "undefined");
+shouldThrow("ctx.drawImage(imageElement, 0, 0, 0, 0, 0)", "TypeError");
+shouldThrow("ctx.drawImage(imageElement, 0, 0, 0, 0, 0, 0)", "TypeError");
+shouldThrow("ctx.drawImage(imageElement, 0, 0, 0, 0, 0, 0, 0)", "TypeError");
+shouldBe("ctx.drawImage(imageElement, 0, 0, 0, 0, 0, 0, 0, 0)", "undefined");
+shouldThrow("ctx.drawImage(imageElement, 0, 0, 0, 0, 0, 0, 0, 0, 0)", "TypeError");
+shouldThrow("ctx.drawImage(imageElement, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)", "TypeError");
+
+var canvasElement = document.createElement("canvas");
+shouldThrow("ctx.drawImage(canvasElement)", "TypeError");
+shouldThrow("ctx.drawImage(canvasElement, 0)", "TypeError");
+shouldBe("ctx.drawImage(canvasElement, 0, 0)", "undefined");
+shouldThrow("ctx.drawImage(canvasElement, 0, 0, 0)", "TypeError");
+shouldBe("ctx.drawImage(canvasElement, 0, 0, 0, 0)", "undefined");
+shouldThrow("ctx.drawImage(canvasElement, 0, 0, 0, 0, 0)", "TypeError");
+shouldThrow("ctx.drawImage(canvasElement, 0, 0, 0, 0, 0, 0)", "TypeError");
+shouldThrow("ctx.drawImage(canvasElement, 0, 0, 0, 0, 0, 0, 0)", "TypeError");
+shouldThrow("ctx.drawImage(canvasElement, 0, 0, 0, 0, 0, 0, 0, 0)", "'Error: INDEX_SIZE_ERR: DOM Exception 1'");
+shouldThrow("ctx.drawImage(canvasElement, 0, 0, 0, 0, 0, 0, 0, 0, 0)", "TypeError");
+shouldThrow("ctx.drawImage(canvasElement, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)", "TypeError");
+
+var successfullyParsed = true;
