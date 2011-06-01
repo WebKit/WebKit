4bf996be2900d83ff8ad7b8e786b43547e4f8eeb
Xan Lopez <xan@webkit.org>
2011-06-01 18:38:35 +0000

    2011-06-01  Xan Lopez  <xlopez@igalia.com>
            Reviewed by Martin Robinson.
    
            Explictly request the size of a pointer in malloc. Otherwise this
            explodes in 64bit architectures, at least.
    
            * webkit/webkitwebplugin.cpp:
            (webkit_web_plugin_get_mimetypes): ditto.
    
    Canonical link: https://commits.webkit.org/77307@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@87830 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/gtk/webkit/webkitwebplugin.cpp
+++ Source/WebKit/gtk/webkit/webkitwebplugin.cpp
@@ -241,7 +241,7 @@ GSList* webkit_web_plugin_get_mimetypes(WebKitWebPlugin* plugin)
         mimeType->description = g_strdup(it->second.utf8().data());
 
         Vector<String> extensions = priv->corePlugin->mimeToExtensions().get(it->first);
-        mimeType->extensions = static_cast<gchar**>(g_malloc0(extensions.size() + 1));
+        mimeType->extensions = static_cast<gchar**>(g_malloc0(sizeof(char*) * (extensions.size() + 1)));
         for (unsigned i = 0; i < extensions.size(); i++)
             mimeType->extensions[i] = g_strdup(extensions[i].utf8().data());
 
