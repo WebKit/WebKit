99223f32f0e9bcd4bafffaad8bb1aa96a7dc027a
Kinuko Yasuda <kinuko@chromium.org>
2011-06-01 05:09:52 +0000

    2011-05-31  Kinuko Yasuda  <kinuko@chromium.org>
            Reviewed by Kent Tamura.
    
            [Chromium] webkitStorageInfo methods (for quota API) should not crash when error callback is not given.
            https://bugs.webkit.org/show_bug.cgi?id=61823
    
            Added a new test for the fix marked the new test 'skipped' on unsupported platforms.
    
            * platform/chromium/test_expectations.txt:
            * platform/gtk/Skipped:
            * platform/mac/Skipped:
            * platform/qt/Skipped:
            * platform/win/Skipped:
            * storage/storageinfo-no-callbacks-expected.txt: Added.
            * storage/storageinfo-no-callbacks.html: Added.
    2011-05-31  Kinuko Yasuda  <kinuko@chromium.org>
    
            Reviewed by Kent Tamura.
    
            [Chromium] webkitStorageInfo methods (for quota API) should not crash when error callback is not given.
            https://bugs.webkit.org/show_bug.cgi?id=61823
    
            Fixed assertion on errorCallback parameter.
    
            Test: storage/quotainfo-no-callbacks.html
    
            * src/StorageInfoChromium.cpp:
            (WebCore::StorageInfo::queryUsageAndQuota):
            (WebCore::StorageInfo::requestQuota):
    
    
    Canonical link: https://commits.webkit.org/77250@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@87772 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/chromium/src/StorageInfoChromium.cpp
+++ Source/WebKit/chromium/src/StorageInfoChromium.cpp
@@ -52,7 +52,8 @@ namespace WebCore {
 namespace {
 void fireStorageInfoErrorCallback(PassRefPtr<StorageInfoErrorCallback> errorCallback, ExceptionCode ec)
 {
-    ASSERT(errorCallback);
+    if (!errorCallback)
+        return;
     ExceptionCodeDescription description;
     getExceptionCodeDescription(ec, description);
     errorCallback->handleEvent(DOMCoreException::create(description).get());
@@ -71,7 +72,7 @@ void StorageInfo::queryUsageAndQuota(ScriptExecutionContext* context, int storag
         Document* document = static_cast<Document*>(context);
         WebFrameImpl* webFrame = WebFrameImpl::fromFrame(document->frame());
         webFrame->client()->queryStorageUsageAndQuota(webFrame, static_cast<WebStorageQuotaType>(storageType), new WebStorageQuotaCallbacksImpl(successCallback, errorCallback));
-    } else if (errorCallback) {
+    } else {
         // FIXME: calling this on worker is not yet supported.
         fireStorageInfoErrorCallback(errorCallback, NOT_SUPPORTED_ERR);
     }
@@ -89,7 +90,7 @@ void StorageInfo::requestQuota(ScriptExecutionContext* context, int storageType,
         Document* document = static_cast<Document*>(context);
         WebFrameImpl* webFrame = WebFrameImpl::fromFrame(document->frame());
         webFrame->client()->requestStorageQuota(webFrame, static_cast<WebStorageQuotaType>(storageType), newQuotaInBytes, new WebStorageQuotaCallbacksImpl(successCallback, errorCallback));
-    } else if (errorCallback) {
+    } else {
         // FIXME: calling this on worker is not yet supported.
         fireStorageInfoErrorCallback(errorCallback, NOT_SUPPORTED_ERR);
     }
