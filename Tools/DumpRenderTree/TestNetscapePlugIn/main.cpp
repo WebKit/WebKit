9b9323f70e988058754f241c6714cd682922d46b
Martin Robinson <mrobinson@webkit.org>
2011-06-09 19:30:55 +0000

    2011-06-09  Martin Robinson  <mrobinson@igalia.com>
            Reviewed by Andreas Kling.
    
            [GTK] [Qt] Eliminate duplicate TestNetscapePlugin implementation
            https://bugs.webkit.org/show_bug.cgi?id=62385
    
            Remove a cr-linux expectation that no longer differs from the default
            expectation.
    
            * platform/chromium-linux/plugins/mouse-events-expected.txt: Removed.
    2011-06-09  Martin Robinson  <mrobinson@igalia.com>
    
            Reviewed by Andreas Kling.
    
            [GTK] [Qt] Eliminate duplicate TestNetscapePlugin implementation
            https://bugs.webkit.org/show_bug.cgi?id=62385
    
            Remove duplicate TestNetscapePlugIn implementation. Having two copies
            of this code makes keeping it in sync much more difficult. The files are
            almost identical and this change ports the X11 changes to main.cpp.
    
            * DumpRenderTree/TestNetscapePlugIn/main.cpp:
            (keyEventToChar): Added this helper which converts a X11 keycode into a char.
            (handleEventX11): Use the handler to properly convert the keycode. Do not print
            adjustCursorEvent output. The tests currently do not trigger this for Cocoa and thus
            it should not be in the results.
            (NPP_GetValue): Properly handle NPPVpluginNeedsXEmbed.
            * DumpRenderTree/qt/TestNetscapePlugin/TestNetscapePlugin.pro: Update source list.
            * DumpRenderTree/unix/TestNetscapePlugin/TestNetscapePlugin.cpp: Removed.
            * GNUmakefile.am: Update source list.
    
    Canonical link: https://commits.webkit.org/77888@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@88471 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/DumpRenderTree/TestNetscapePlugIn/main.cpp
+++ Tools/DumpRenderTree/TestNetscapePlugIn/main.cpp
@@ -27,10 +27,12 @@
 
 #include "PluginTest.h"
 #include <cstdlib>
+#include <cstring>
 #include <string>
 
 #ifdef XP_UNIX
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 #endif
 
 #if !defined(NP_NO_CARBON) && defined(QD_HEADERS_ARE_PRIVATE) && QD_HEADERS_ARE_PRIVATE
@@ -622,32 +624,38 @@ static int16_t handleEventCocoa(NPP instance, PluginObject* obj, NPCocoaEvent* e
 #endif // XP_MACOSX
 
 #ifdef XP_UNIX
+
+static char keyEventToChar(XKeyEvent* event)
+{
+    char c = ' ';
+    XLookupString(event, &c, sizeof(c), 0, 0);
+    return c;
+}
+
 static int16_t handleEventX11(NPP instance, PluginObject* obj, XEvent* event)
 {
-    XButtonPressedEvent* buttonPressEvent = reinterpret_cast<XButtonPressedEvent*>(event);
-    XButtonReleasedEvent* buttonReleaseEvent = reinterpret_cast<XButtonReleasedEvent*>(event);
     switch (event->type) {
     case ButtonPress:
         if (obj->eventLogging)
-            pluginLog(instance, "mouseDown at (%d, %d)", buttonPressEvent->x, buttonPressEvent->y);
+            pluginLog(instance, "mouseDown at (%d, %d)", event->xbutton.x, event->xbutton.y);
         if (obj->evaluateScriptOnMouseDownOrKeyDown && obj->mouseDownForEvaluateScript)
             executeScript(obj, obj->evaluateScriptOnMouseDownOrKeyDown);
         break;
     case ButtonRelease:
         if (obj->eventLogging)
-            pluginLog(instance, "mouseUp at (%d, %d)", buttonReleaseEvent->x, buttonReleaseEvent->y);
+            pluginLog(instance, "mouseUp at (%d, %d)", event->xbutton.x, event->xbutton.y);
         break;
     case KeyPress:
         // FIXME: extract key code
         if (obj->eventLogging)
-            pluginLog(instance, "NOTIMPLEMENTED: keyDown '%c'", ' ');
+            pluginLog(instance, "keyDown '%c'", keyEventToChar(&event->xkey));
         if (obj->evaluateScriptOnMouseDownOrKeyDown && !obj->mouseDownForEvaluateScript)
             executeScript(obj, obj->evaluateScriptOnMouseDownOrKeyDown);
         break;
     case KeyRelease:
         // FIXME: extract key code
         if (obj->eventLogging)
-            pluginLog(instance, "NOTIMPLEMENTED: keyUp '%c'", ' ');
+            pluginLog(instance, "keyUp '%c'", keyEventToChar(&event->xkey));
         break;
     case GraphicsExpose:
         if (obj->eventLogging)
@@ -667,8 +675,6 @@ static int16_t handleEventX11(NPP instance, PluginObject* obj, XEvent* event)
     case EnterNotify:
     case LeaveNotify:
     case MotionNotify:
-        if (obj->eventLogging)
-            pluginLog(instance, "adjustCursorEvent");
         break;
     default:
         if (obj->eventLogging)
@@ -778,6 +784,10 @@ NPError NPP_GetValue(NPP instance, NPPVariable variable, void *value)
     }
     if (variable == NPPVpluginDescriptionString) {
         *((char **)value) = const_cast<char*>("Simple Netscape plug-in that handles test content for WebKit");
+        return NPERR_NO_ERROR;
+    }
+    if (variable == NPPVpluginNeedsXEmbed) {
+        *((NPBool *)value) = TRUE;
         return NPERR_NO_ERROR;
     }
 #endif
