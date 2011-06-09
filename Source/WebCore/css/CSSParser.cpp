3e0c7ab1f7744d3ba18fa79fe9cc5b9bb34b846c
Mike Lawther <mikelawther@chromium.org>
2011-06-09 14:12:56 +0000

    2011-06-09  Mike Lawther  <mikelawther@chromium.org>
            Reviewed by Kent Tamura.
    
            Parsing issue with -webkit-calc
            https://bugs.webkit.org/show_bug.cgi?id=62276
    
            Set the CSSParserString for the calc functions.
    
            * css3/calc/regression-62276-expected.txt: Added.
            * css3/calc/regression-62276.html: Added.
    2011-06-09  Mike Lawther  <mikelawther@chromium.org>
    
            Reviewed by Kent Tamura.
    
            Parsing issue with -webkit-calc
            https://bugs.webkit.org/show_bug.cgi?id=62276
    
            Set the CSSParserString for the calc functions.
    
            Test: css3/calc/regression-62276.html
    
            * css/CSSParser.cpp:
            (WebCore::CSSParser::lex):
    
    Canonical link: https://commits.webkit.org/77867@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@88448 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/CSSParser.cpp
+++ Source/WebCore/css/CSSParser.cpp
@@ -5959,6 +5959,9 @@ int CSSParser::lex(void* yylvalWithoutType)
     case FUNCTION:
     case ANYFUNCTION:
     case NOTFUNCTION:
+    case CALCFUNCTION:
+    case MINFUNCTION:
+    case MAXFUNCTION:
         yylval->string.characters = t;
         yylval->string.length = length;
         break;
