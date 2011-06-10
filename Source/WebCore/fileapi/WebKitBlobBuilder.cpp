297eefda6722738efc66baea793b59f7710da827
Jian Li <jianli@chromium.org>
2011-06-10 01:09:11 +0000

    Calling WebKitBlobBuilder.append with null argument should not crash https://bugs.webkit.org/show_bug.cgi?id=62419
    Reviewed by David Levin.
    
    Source/WebCore:
    
    Test: fast/files/blob-builder-crash.html
    
    * fileapi/WebKitBlobBuilder.cpp:
    (WebCore::WebKitBlobBuilder::append):
    
    LayoutTests:
    
    * fast/files/blob-builder-crash-expected.txt: Added.
    * fast/files/blob-builder-crash.html: Added.
    
    Canonical link: https://commits.webkit.org/77917@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@88510 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/fileapi/WebKitBlobBuilder.cpp
+++ Source/WebCore/fileapi/WebKitBlobBuilder.cpp
@@ -88,6 +88,8 @@ void WebKitBlobBuilder::append(const String& text, ExceptionCode& ec)
 #if ENABLE(BLOB)
 void WebKitBlobBuilder::append(ArrayBuffer* arrayBuffer)
 {
+    if (!arrayBuffer)
+        return;
     Vector<char>& buffer = getBuffer();
     size_t oldSize = buffer.size();
     buffer.append(static_cast<const char*>(arrayBuffer->data()), arrayBuffer->byteLength());
@@ -97,6 +99,8 @@ void WebKitBlobBuilder::append(ArrayBuffer* arrayBuffer)
 
 void WebKitBlobBuilder::append(Blob* blob)
 {
+    if (!blob)
+        return;
     if (blob->isFile()) {
         // If the blob is file that is not snapshoted, capture the snapshot now.
         // FIXME: This involves synchronous file operation. We need to figure out how to make it asynchronous.
