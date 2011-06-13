22ceed9370f54998fe7e863fde2c392c4a98f3b4
Commit Queue <commit-queue@webkit.org>
2011-06-13 22:44:59 +0000

    2011-06-13  Ryuan Choi  <ryuan.choi@samsung.com>
            Reviewed by Kenneth Rohde Christiansen.
    
            [EFL][WK2] Add dummy files at below of WebProcess.
            https://bugs.webkit.org/show_bug.cgi?id=62445
    
            Add Efl related directory and files in WebKit2/WebProcess/.
    
            * WebProcess/Cookies/efl/WebCookieManagerEfl.cpp: Added.
            (WebKit::WebCookieManager::platformSetHTTPCookieAcceptPolicy):
            (WebKit::WebCookieManager::platformGetHTTPCookieAcceptPolicy):
            * WebProcess/InjectedBundle/efl/InjectedBundleEfl.cpp: Added.
            (WebKit::InjectedBundle::load):
            (WebKit::InjectedBundle::activateMacFontAscentHack):
            * WebProcess/WebPage/efl/WebInspectorEfl.cpp: Added.
            (WebKit::WebInspector::localizedStringsURL):
            * WebProcess/WebPage/efl/WebPageEfl.cpp: Added.
            (WebKit::WebPage::platformInitialize):
            (WebKit::WebPage::platformPreferencesDidChange):
            (WebKit::scroll):
            (WebKit::WebPage::performDefaultBehaviorForKeyEvent):
            (WebKit::WebPage::platformHasLocalDataForURL):
            (WebKit::WebPage::cachedResponseMIMETypeForURL):
            (WebKit::WebPage::platformCanHandleRequest):
            (WebKit::WebPage::cachedSuggestedFilenameForURL):
            (WebKit::WebPage::cachedResponseDataForURL):
            * WebProcess/efl/WebProcessEfl.cpp: Added.
            (WebKit::WebProcess::platformSetCacheModel):
            (WebKit::WebProcess::platformClearResourceCaches):
            (WebKit::WebProcess::platformInitializeWebProcess):
            (WebKit::WebProcess::platformTerminate):
            * WebProcess/efl/WebProcessMainEfl.cpp: Added.
            (WebKit::WebProcessMainEfl):
            * WebProcess/efl/WebProcessMainEfl.h: Added.
            * efl/MainEfl.cpp: Added.
            (main):
    
    Canonical link: https://commits.webkit.org/78124@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@88722 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit2/WebProcess/WebPage/efl/WebPageEfl.cpp
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2010 Apple Inc. All rights reserved.
+ * Portions Copyright (c) 2010 Motorola Mobility, Inc.  All rights reserved.
+ * Copyright (C) 2011 Igalia S.L.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "WebPage.h"
+
+#include "NotImplemented.h"
+#include "WebEvent.h"
+#include "WindowsKeyboardCodes.h"
+#include <WebCore/FocusController.h>
+#include <WebCore/Frame.h>
+#include <WebCore/KeyboardEvent.h>
+#include <WebCore/Page.h>
+#include <WebCore/PlatformKeyboardEvent.h>
+#include <WebCore/Settings.h>
+
+using namespace WebCore;
+
+namespace WebKit {
+
+void WebPage::platformInitialize()
+{
+    notImplemented();
+}
+
+void WebPage::platformPreferencesDidChange(const WebPreferencesStore&)
+{
+    notImplemented();
+}
+
+static inline void scroll(Page* page, ScrollDirection direction, ScrollGranularity granularity)
+{
+    page->focusController()->focusedOrMainFrame()->eventHandler()->scrollRecursively(direction, granularity);
+}
+
+bool WebPage::performDefaultBehaviorForKeyEvent(const WebKeyboardEvent& keyboardEvent)
+{
+    notImplemented();
+    return false;
+}
+
+bool WebPage::platformHasLocalDataForURL(const KURL&)
+{
+    notImplemented();
+    return false;
+}
+
+String WebPage::cachedResponseMIMETypeForURL(const KURL&)
+{
+    notImplemented();
+    return String();
+}
+
+bool WebPage::platformCanHandleRequest(const ResourceRequest&)
+{
+    notImplemented();
+    return true;
+}
+
+String WebPage::cachedSuggestedFilenameForURL(const KURL&)
+{
+    notImplemented();
+    return String();
+}
+
+PassRefPtr<SharedBuffer> WebPage::cachedResponseDataForURL(const KURL&)
+{
+    notImplemented();
+    return 0;
+}
+
+} // namespace WebKit
---
+++ Source/WebKit2/WebProcess/efl/WebProcessMainEfl.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2011 Samsung Electronics. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "WebProcessMainEfl.h"
+
+#include "WKBase.h"
+#include <Ecore.h>
+#include <WebCore/ResourceHandle.h>
+#include <WebKit2/RunLoop.h>
+#include <WebKit2/WebProcess.h>
+#include <runtime/InitializeThreading.h>
+#include <unistd.h>
+#include <wtf/Threading.h>
+
+namespace WebKit {
+
+WK_EXPORT int WebProcessMainEfl(int argc, char* argv[])
+{
+    // WebProcess should be launched with an option.
+    if (argc != 2)
+        return 1;
+
+    if (!eina_init())
+        return 1;
+
+    if (!ecore_init()) {
+        // Could not init ecore.
+        eina_shutdown();
+        return 1;
+    }
+
+#if ENABLE(GLIB_SUPPORT)
+    g_type_init();
+#endif
+
+    JSC::initializeThreading();
+    WTF::initializeMainThread();
+
+    RunLoop::initializeMainRunLoop();
+
+#if USE(SOUP)
+    SoupSession* session = WebCore::ResourceHandle::defaultSession();
+    const char* httpProxy = g_getenv("http_proxy");
+    if (httpProxy) {
+        SoupURI* proxyUri = soup_uri_new(httpProxy);
+        g_object_set(session, SOUP_SESSION_PROXY_URI, proxyUri, NULL);
+        soup_uri_free(proxyUri);
+    }
+
+    soup_session_add_feature_by_type(session, SOUP_TYPE_CONTENT_SNIFFER);
+    soup_session_add_feature_by_type(session, SOUP_TYPE_CONTENT_DECODER);
+#endif
+
+    int socket = atoi(argv[1]);
+    WebProcess::shared().initialize(socket, RunLoop::main());
+    RunLoop::run();
+
+    return 0;
+
+}
+
+} // namespace WebKit
