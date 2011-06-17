9a578d45806a433c16113eb5b280937ecf5c1501
Commit Queue <commit-queue@webkit.org>
2011-06-17 00:56:17 +0000

    2011-06-16  Sheriff Bot  <webkit.review.bot@gmail.com>
            Unreviewed, rolling out r89055.
            http://trac.webkit.org/changeset/89055
            https://bugs.webkit.org/show_bug.cgi?id=62835
    
            Caused failures on all ports except GTK (Requested by
            mrobinson on #webkit).
    
            * http/tests/loading/resources/status-code-error-with-response-body.php: Removed.
            * http/tests/loading/resources/status-code-error-without-response-body.php: Removed.
            * http/tests/loading/status-code-error-without-response-body-expected.txt: Removed.
            * http/tests/loading/status-code-error-without-response-body.html: Removed.
            * platform/chromium/test_expectations.txt:
    2011-06-16  Sheriff Bot  <webkit.review.bot@gmail.com>
    
            Unreviewed, rolling out r89055.
            http://trac.webkit.org/changeset/89055
            https://bugs.webkit.org/show_bug.cgi?id=62835
    
            Caused failures on all ports except GTK (Requested by
            mrobinson on #webkit).
    
            * platform/network/soup/ResourceHandleSoup.cpp:
            (WebCore::statusWillBeHandledBySoup):
            (WebCore::soupErrorShouldCauseLoadFailure):
            (WebCore::convertSoupErrorToResourceError):
            (WebCore::sendRequestCallback):
    
    Canonical link: https://commits.webkit.org/78425@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@89094 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/loading/resources/status-code-error-with-response-body.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: text/html", TRUE, (int)$_REQUEST['statusCode']);
-?>
-<html>
-<body>
-<p>status code is <?php echo $_REQUEST['statusCode'];?></p>
-</body>
-</html>
--- LayoutTests/http/tests/loading/resources/status-code-error-without-response-body.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("Content-Length: 0", TRUE, (int)$_REQUEST['statusCode']);
-?>
