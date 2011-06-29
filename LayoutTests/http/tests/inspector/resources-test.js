fa9e4c5007daa252025f364afa234dc7ddde9bca
Pavel Feldman <pfeldman@chromium.org>
2011-06-29 12:43:45 +0000

    2011-06-29  Mike West  <mkwst@chromium.org>
            Reviewed by Pavel Feldman.
    
            Web Inspector: Missing fields in HAR
            https://bugs.webkit.org/show_bug.cgi?id=58127
    
            Setting correct expectations for the following fields newly added to
            the HAR implementation:
    
            - entry.request.httpVersion
            - entry.request.headersSize
            - entry.request.bodySize
            - entry.response.httpVersion
            - entry.response.headersSize
            - entry.response.bodySize
            - entry.response.content.compression
    
            * http/tests/inspector/network/network-size.html:
            * http/tests/inspector/resource-har-conversion-expected.txt:
            * http/tests/inspector/resource-har-conversion.html:
            * http/tests/inspector/resource-har-headers-expected.txt: Added.
            * http/tests/inspector/resource-har-headers.html: Added.
            * http/tests/inspector/resource-parameters-expected.txt:
            * http/tests/inspector/resources-test.js:
    2011-06-29  Mike West  <mkwst@chromium.org>
    
            Reviewed by Pavel Feldman.
    
            Web Inspector: Missing fields in HAR
            https://bugs.webkit.org/show_bug.cgi?id=58127
    
            Specifically, this patch adds:
            - entry.request.httpVersion
            - entry.request.headersSize
            - entry.request.bodySize
            - entry.response.httpVersion
            - entry.response.headersSize
            - entry.response.bodySize
            - entry.response.content.compression
    
            Tests: http/tests/inspector/resource-har-conversion.html
                   http/tests/inspector/resource-har-headers.html
                   http/tests/inspector/resource-parameters.html
                   http/tests/inspector/network/network-size.html
    
            * inspector/front-end/HAREntry.js:
            (WebInspector.HAREntry.prototype.build):
            (WebInspector.HAREntry.prototype._buildRequest):
            (WebInspector.HAREntry.prototype._buildResponse):
            (WebInspector.HAREntry.prototype._buildContent):
            (WebInspector.HAREntry.prototype._buildCookie):
            (WebInspector.HAREntry.prototype._interval):
            (WebInspector.HAREntry.prototype.get requestBodySize):
            (WebInspector.HAREntry.prototype.get responseBodySize):
            (WebInspector.HAREntry.prototype.get responseCompression):
            (WebInspector.HARLog.prototype.build):
            * inspector/front-end/Resource.js:
            (WebInspector.Resource.prototype.set requestHeaders):
            (WebInspector.Resource.prototype.get requestHeadersText):
            (WebInspector.Resource.prototype.set requestHeadersText):
            (WebInspector.Resource.prototype.get requestHeadersSize):
            (WebInspector.Resource.prototype.get requestHttpVersion):
            (WebInspector.Resource.prototype.set responseHeaders):
            (WebInspector.Resource.prototype.get responseHeadersText):
            (WebInspector.Resource.prototype.set responseHeadersText):
            (WebInspector.Resource.prototype.get responseHeadersSize):
            (WebInspector.Resource.prototype.get responseHttpVersion):
    
    Canonical link: https://commits.webkit.org/79294@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@90010 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/inspector/resources-test.js
+++ LayoutTests/http/tests/inspector/resources-test.js
@@ -1,24 +1,26 @@
 var initialize_ResourceTest = function() {
 
 InspectorTest.HARNondeterministicProperties = {
-    startedDateTime: 1,
-    time: 1,
-    wait: 1,
-    receive: 1,
+    bodySize: 1,
+    compression: 1,
     headers: 1,
-    timings: 1,
-    version: 1,
-    startedDateTime: 1,
+    headersSize: 1,
     id: 1,
     onContentLoad: 1,
-    onLoad: 1
+    onLoad: 1,
+    receive: 1,
+    startedDateTime: 1,
+    time: 1,
+    timings: 1,
+    version: 1,
+    wait: 1,
 };
 
 // addObject checks own properties only, so make a deep copy rather than use prototype.
 
 InspectorTest.HARNondeterministicPropertiesWithSize = JSON.parse(JSON.stringify(InspectorTest.HARNondeterministicProperties));
 InspectorTest.HARNondeterministicPropertiesWithSize.size = 1;
-InspectorTest.HARNondeterministicPropertiesWithSize.bodySize = 1;
+
 
 InspectorTest.resourceURLComparer = function(r1, r2)
 {
