feb061e3c03cefddc278a23d038fa6254f2ae84f
Yuta Kitamura <yutak@chromium.org>
2011-07-01 02:55:49 +0000

    2011-06-30  Yuta Kitamura  <yutak@chromium.org>
            Reviewed by Kent Tamura.
    
            WebSocket: Don't use old Hixie75 handshake in pywebsocket handlers
            https://bugs.webkit.org/show_bug.cgi?id=63704
    
            * http/tests/websocket/tests/bad-handshake-crash_wsh.py:
            Use handshake format of Hixie's protocol draft 76 which we are currently supporting.
            * http/tests/websocket/tests/long-invalid-header_wsh.py:
            Content of web_socket_transfer_data() is completely bogus. It's not necessary.
    
    Canonical link: https://commits.webkit.org/79466@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@90196 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/websocket/tests/bad-handshake-crash_wsh.py
+++ LayoutTests/http/tests/websocket/tests/bad-handshake-crash_wsh.py
@@ -1,9 +1,12 @@
 def web_socket_do_extra_handshake(request):
-    msg = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
+    msg = "HTTP/1.1 101 WebSocket Protocol Handshake\r\n"
     msg += "Upgrade: WebSocket\r\n"
     msg += "Connection: Upgrade\r\n"
+    msg += "Sec-WebSocket-Location: " + request.ws_location + "\r\n"
+    msg += "Sec-WebSocket-Origin: " + request.ws_origin + "\r\n"
     msg += "\xa5:\r\n"
     msg += "\r\n"
+    msg += request.ws_challenge_md5
     request.connection.write(msg)
     print msg
 
--- LayoutTests/http/tests/websocket/tests/long-invalid-header_wsh.py
+++ LayoutTests/http/tests/websocket/tests/long-invalid-header_wsh.py
@@ -1,10 +1,8 @@
 def web_socket_do_extra_handshake(request):
-    msg = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
+    msg = "HTTP/1.1 101 WebSocket Protocol Handshake\r\n"
     msg += ("p" * 1024) + "\r\n"
     request.connection.write(msg)
 
 
 def web_socket_transfer_data(request):
-    msg = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
-    msg += ("p" * 1024) + "\r\n"
-    request.connection.write(msg)
+    pass
