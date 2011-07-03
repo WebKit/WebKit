1eaa035dd7b9cbae276aaafb2af72dec458e694a
Adam Barth <abarth@webkit.org>
2011-07-03 10:45:21 +0000

    2011-07-03  Adam Barth  <abarth@webkit.org>
            Move the rebaseline server out of the commands package and into the
            (new) servers package.  This patch prepares for refactoring this code
            to share server infrastructure with the (forthcoming) garden-o-matic
            command.
    
            * Scripts/webkitpy/tool/commands/rebaselineserver.py:
            * Scripts/webkitpy/tool/servers/__init__.py: Added.
            * Scripts/webkitpy/tool/servers/data/rebaselineserver/loupe.js:
            * Scripts/webkitpy/tool/servers/data/rebaselineserver/main.css:
            * Scripts/webkitpy/tool/servers/data/rebaselineserver/queue.js:
            * Scripts/webkitpy/tool/servers/rebaselineserver.py: Added.
    
    Canonical link: https://commits.webkit.org/79602@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@90336 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/tool/commands/rebaselineserver.py
+++ Tools/Scripts/webkitpy/tool/commands/rebaselineserver.py
@@ -29,22 +29,13 @@
 """Starts a local HTTP server which displays layout test failures (given a test
 results directory), provides comparisons of expected and actual results (both
 images and text) and allows one-click rebaselining of tests."""
-from __future__ import with_statement
 
-import codecs
-import datetime
 import fnmatch
-import mimetypes
 import os
 import os.path
-import shutil
 import threading
-import time
-import urlparse
-import BaseHTTPServer
 
 from optparse import make_option
-from wsgiref.handlers import format_date_time
 
 from webkitpy.common import system
 from webkitpy.common.net import resultsjsonparser
@@ -52,170 +43,7 @@ from webkitpy.layout_tests.layout_package import json_results_generator
 from webkitpy.layout_tests.port import factory
 from webkitpy.layout_tests.port.webkit import WebKitPort
 from webkitpy.tool.multicommandtool import AbstractDeclarativeCommand
-from webkitpy.thirdparty import simplejson
-
-STATE_NEEDS_REBASELINE = 'needs_rebaseline'
-STATE_REBASELINE_FAILED = 'rebaseline_failed'
-STATE_REBASELINE_SUCCEEDED = 'rebaseline_succeeded'
-
-class RebaselineHTTPServer(BaseHTTPServer.HTTPServer):
-    def __init__(self, httpd_port, test_config, results_json, platforms_json):
-        BaseHTTPServer.HTTPServer.__init__(self, ("", httpd_port), RebaselineHTTPRequestHandler)
-        self.test_config = test_config
-        self.results_json = results_json
-        self.platforms_json = platforms_json
-
-
-class RebaselineHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    STATIC_FILE_NAMES = frozenset([
-        "index.html",
-        "loupe.js",
-        "main.js",
-        "main.css",
-        "queue.js",
-        "util.js",
-    ])
-
-    STATIC_FILE_DIRECTORY = os.path.join(
-        os.path.dirname(__file__), "data", "rebaselineserver")
-
-    def do_GET(self):
-        self._handle_request()
-
-    def do_POST(self):
-        self._handle_request()
-
-    def _handle_request(self):
-        # Parse input.
-        if "?" in self.path:
-            path, query_string = self.path.split("?", 1)
-            self.query = urlparse.parse_qs(query_string)
-        else:
-            path = self.path
-            self.query = {}
-        function_or_file_name = path[1:] or "index.html"
-
-        # See if a static file matches.
-        if function_or_file_name in RebaselineHTTPRequestHandler.STATIC_FILE_NAMES:
-            self._serve_static_file(function_or_file_name)
-            return
-
-        # See if a class method matches.
-        function_name = function_or_file_name.replace(".", "_")
-        if not hasattr(self, function_name):
-            self.send_error(404, "Unknown function %s" % function_name)
-            return
-        if function_name[0] == "_":
-            self.send_error(
-                401, "Not allowed to invoke private or protected methods")
-            return
-        function = getattr(self, function_name)
-        function()
-
-    def _serve_static_file(self, static_path):
-        self._serve_file(os.path.join(
-            RebaselineHTTPRequestHandler.STATIC_FILE_DIRECTORY, static_path))
-
-    def rebaseline(self):
-        test = self.query['test'][0]
-        baseline_target = self.query['baseline-target'][0]
-        baseline_move_to = self.query['baseline-move-to'][0]
-        test_json = self.server.results_json['tests'][test]
-
-        if test_json['state'] != STATE_NEEDS_REBASELINE:
-            self.send_error(400, "Test %s is in unexpected state: %s" %
-                (test, test_json["state"]))
-            return
-
-        log = []
-        success = _rebaseline_test(
-            test,
-            baseline_target,
-            baseline_move_to,
-            self.server.test_config,
-            log=lambda l: log.append(l))
-
-        if success:
-            test_json['state'] = STATE_REBASELINE_SUCCEEDED
-            self.send_response(200)
-        else:
-            test_json['state'] = STATE_REBASELINE_FAILED
-            self.send_response(500)
-
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('\n'.join(log))
-
-    def quitquitquit(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write("Quit.\n")
-
-        # Shutdown has to happen on another thread from the server's thread,
-        # otherwise there's a deadlock
-        threading.Thread(target=lambda: self.server.shutdown()).start()
-
-    def test_result(self):
-        test_name, _ = os.path.splitext(self.query['test'][0])
-        mode = self.query['mode'][0]
-        if mode == 'expected-image':
-            file_name = test_name + '-expected.png'
-        elif mode == 'actual-image':
-            file_name = test_name + '-actual.png'
-        if mode == 'expected-checksum':
-            file_name = test_name + '-expected.checksum'
-        elif mode == 'actual-checksum':
-            file_name = test_name + '-actual.checksum'
-        elif mode == 'diff-image':
-            file_name = test_name + '-diff.png'
-        if mode == 'expected-text':
-            file_name = test_name + '-expected.txt'
-        elif mode == 'actual-text':
-            file_name = test_name + '-actual.txt'
-        elif mode == 'diff-text':
-            file_name = test_name + '-diff.txt'
-        elif mode == 'diff-text-pretty':
-            file_name = test_name + '-pretty-diff.html'
-
-        file_path = os.path.join(self.server.test_config.results_directory, file_name)
-
-        # Let results be cached for 60 seconds, so that they can be pre-fetched
-        # by the UI
-        self._serve_file(file_path, cacheable_seconds=60)
-
-    def results_json(self):
-        self._serve_json(self.server.results_json)
-
-    def platforms_json(self):
-        self._serve_json(self.server.platforms_json)
-
-    def _serve_json(self, json):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        simplejson.dump(json, self.wfile)
-
-    def _serve_file(self, file_path, cacheable_seconds=0):
-        if not os.path.exists(file_path):
-            self.send_error(404, "File not found")
-            return
-        with codecs.open(file_path, "rb") as static_file:
-            self.send_response(200)
-            self.send_header("Content-Length", os.path.getsize(file_path))
-            mime_type, encoding = mimetypes.guess_type(file_path)
-            if mime_type:
-                self.send_header("Content-type", mime_type)
-
-            if cacheable_seconds:
-                expires_time = (datetime.datetime.now() +
-                    datetime.timedelta(0, cacheable_seconds))
-                expires_formatted = format_date_time(
-                    time.mktime(expires_time.timetuple()))
-                self.send_header("Expires", expires_formatted)
-            self.end_headers()
-
-            shutil.copyfileobj(static_file, self.wfile)
+from webkitpy.tool.servers.rebaselineserver import RebaselineHTTPServer, STATE_NEEDS_REBASELINE
 
 
 class TestConfig(object):
