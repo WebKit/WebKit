5a743ad49882cd4a8645c3c9da58117db1df70d2
Nate Chapin <japhet@chromium.org>
2011-07-11 18:48:34 +0000

    [Chromium] Ensure we don't crash when an HTMLSelectElement mouse release event removes the element from the DOM. https://bugs.webkit.org/show_bug.cgi?id=64295
    Reviewed by Dimitri Glazkov.
    
    Manual test only because the crash requires a
    real popup menu, which DRT doesn't provide.
    
    * manual-tests/chromium/popup-menu-crash.html: Added.
    * platform/chromium/PopupMenuChromium.cpp:
    (WebCore::PopupContainer::handleMouseReleaseEvent):
    
    
    Canonical link: https://commits.webkit.org/80034@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@90769 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/chromium/PopupMenuChromium.cpp
+++ Source/WebCore/platform/chromium/PopupMenuChromium.cpp
@@ -496,6 +496,7 @@ bool PopupContainer::handleMouseMoveEvent(const PlatformMouseEvent& event)
 
 bool PopupContainer::handleMouseReleaseEvent(const PlatformMouseEvent& event)
 {
+    RefPtr<PopupContainer> protect(this);
     UserGestureIndicator gestureIndicator(DefinitelyProcessingUserGesture);
     return m_listBox->handleMouseReleaseEvent(
         constructRelativeMouseEvent(event, this, m_listBox.get()));
