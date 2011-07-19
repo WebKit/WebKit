db56f48b890edbd7d83b17843e1d4d459e26ed6c
Ojan Vafai <ojan@chromium.org>
2011-07-19 22:48:55 +0000

    remove the concept of platform fallbacks https://bugs.webkit.org/show_bug.cgi?id=64829
    Reviewed by Adam Barth.
    
    In the process, wrote tests for this code and fixed a pretty major
    bug (now covered by the test).
    
    * TestResultServer/static-dashboards/flakiness_dashboard.html:
    * TestResultServer/static-dashboards/flakiness_dashboard_tests.js:
    
    Canonical link: https://commits.webkit.org/80556@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@91313 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestResultServer/static-dashboards/flakiness_dashboard_tests.js
+++ Tools/TestResultServer/static-dashboards/flakiness_dashboard_tests.js
@@ -228,6 +228,80 @@ function testAllTestsWithSamePlatformAndBuildType()
     }
 }
 
+function testGetExpectations()
+{
+    g_builders['WebKit Win'] = true;
+    g_resultsByBuilder = {
+        'WebKit Win': {
+            'tests': {
+                'foo/test1.html': {'results': [[100, 'F']], 'times': [[100, 0]]},
+                'foo/test3.html': {'results': [[100, 'F']], 'times': [[100, 0]]},
+                'test1.html': {'results': [[100, 'F']], 'times': [[100, 0]]}
+            }
+        }
+    }
+
+    g_expectationsByTest = {
+        'foo': [
+            {'modifiers': '', 'expectations': 'FAIL PASS CRASH'}
+        ], 
+        'foo/test1.html': [
+            {'modifiers': 'RELEASE BUGFOO', 'expectations': 'FAIL'},
+            {'modifiers': 'DEBUG', 'expectations': 'CRASH'}
+        ],
+        'foo/test2.html': [
+            {'modifiers': '', 'expectations': 'FAIL'},
+            {'modifiers': 'LINUX DEBUG', 'expectations': 'CRASH'}
+        ],
+        'test1.html': [
+            {'modifiers': 'RELEASE', 'expectations': 'FAIL'},
+            {'modifiers': 'DEBUG', 'expectations': 'CRASH'}
+        ],
+        'http/tests/appcache/interrupted-update.html': [
+            {'modifiers': 'WIN7', 'expectations': 'TIMEOUT'},
+            {'modifiers': 'MAC LINUX XP VISTA', 'expectations': 'FAIL'}
+        ]
+    }
+
+    processExpectations();
+    
+    var expectations = getExpectations('foo/test1.html', 'XP', 'DEBUG');
+    assertEquals(JSON.stringify(expectations), '{"modifiers":"DEBUG","expectations":"CRASH"}');
+
+    var expectations = getExpectations('foo/test1.html', 'LUCID', 'RELEASE');
+    assertEquals(JSON.stringify(expectations), '{"modifiers":"RELEASE BUGFOO","expectations":"FAIL"}');
+
+    var expectations = getExpectations('foo/test2.html', 'LUCID', 'RELEASE');
+    assertEquals(JSON.stringify(expectations), '{"modifiers":"","expectations":"FAIL"}');
+
+    var expectations = getExpectations('foo/test2.html', 'LEOPARD', 'DEBUG');
+    assertEquals(JSON.stringify(expectations), '{"modifiers":"","expectations":"FAIL"}');
+
+    var expectations = getExpectations('foo/test2.html', 'LUCID', 'DEBUG');
+    assertEquals(JSON.stringify(expectations), '{"modifiers":"LINUX DEBUG","expectations":"CRASH"}');
+
+    var expectations = getExpectations('foo/test3.html', 'LUCID', 'DEBUG');
+    assertEquals(JSON.stringify(expectations), '{"modifiers":"","expectations":"FAIL PASS CRASH"}');
+
+    var expectations = getExpectations('test1.html', 'XP', 'DEBUG');
+    assertEquals(JSON.stringify(expectations), '{"modifiers":"DEBUG","expectations":"CRASH"}');
+
+    var expectations = getExpectations('test1.html', 'LUCID', 'RELEASE');
+    assertEquals(JSON.stringify(expectations), '{"modifiers":"RELEASE","expectations":"FAIL"}');
+
+    var expectations = getExpectations('http/tests/appcache/interrupted-update.html', 'WIN7', 'RELEASE');
+    assertEquals(JSON.stringify(expectations), '{"modifiers":"WIN7","expectations":"TIMEOUT"}');
+
+    var expectations = getExpectations('http/tests/appcache/interrupted-update.html', 'LEOPARD', 'RELEASE');
+    assertEquals(JSON.stringify(expectations), '{"modifiers":"MAC LINUX XP VISTA","expectations":"FAIL"}');
+
+    var expectations = getExpectations('http/tests/appcache/interrupted-update.html', 'LUCID', 'RELEASE');
+    assertEquals(JSON.stringify(expectations), '{"modifiers":"MAC LINUX XP VISTA","expectations":"FAIL"}');
+
+    var expectations = getExpectations('http/tests/appcache/interrupted-update.html', 'VISTA', 'RELEASE');
+    assertEquals(JSON.stringify(expectations), '{"modifiers":"MAC LINUX XP VISTA","expectations":"FAIL"}');
+}
+
 function runTests()
 {
     document.body.innerHTML = '<pre id=unittest-results></pre>';
