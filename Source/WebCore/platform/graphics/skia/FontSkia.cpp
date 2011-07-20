43261f595f39064fcab89ac35164279070774063
Cary Clark <caryclark@google.com>
2011-07-20 19:31:06 +0000

    update FontSkia to match platform context convention https://bugs.webkit.org/show_bug.cgi?id=64795
    Reviewed by James Robinson.
    
    No new tests. This platform variant is not
    built at this time.
    
    * platform/graphics/skia/FontSkia.cpp:
    (WebCore::Font::drawGlyphs):
    Call makeGrContextCurrent instead of the obsolete prepareForSoftwareDraw.
    
    
    Canonical link: https://commits.webkit.org/80623@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@91388 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/skia/FontSkia.cpp
+++ Source/WebCore/platform/graphics/skia/FontSkia.cpp
@@ -129,7 +129,7 @@ void Font::drawGlyphs(GraphicsContext* gc, const SimpleFontData* font,
         y += SkFloatToScalar(adv[i].height);
     }
 
-    gc->platformContext()->prepareForSoftwareDraw();
+    gc->platformContext()->makeGrContextCurrent();
 
     SkCanvas* canvas = gc->platformContext()->canvas();
     TextDrawingModeFlags textMode = gc->platformContext()->getTextDrawingMode();
