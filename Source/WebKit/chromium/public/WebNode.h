ebd8c547b5a2a2729244b81f7bbf3ed2ebe76281
Ilya Sherman <isherman@chromium.org>
2011-07-20 19:39:02 +0000

    Remove TODO in WebNode.h from a two-sided patch that has now landed https://bugs.webkit.org/show_bug.cgi?id=64840
    Reviewed by Darin Fisher.
    
    * public/WebNode.h:
    
    
    Canonical link: https://commits.webkit.org/80624@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@91390 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/chromium/public/WebNode.h
+++ Source/WebKit/chromium/public/WebNode.h
@@ -35,9 +35,6 @@
 #include "WebPrivatePtr.h"
 #include "WebString.h"
 
-// TODO(isherman): Remove this once the Chromium side is updated.
-#define CRBUG_72758_FIXED
-
 namespace WebCore { class Node; }
 
 namespace WebKit {
