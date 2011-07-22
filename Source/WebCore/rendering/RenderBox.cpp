7167831677e11fdee60efbce3618cc61f4eec537
Kulanthaivel Palanichamy <kulanthaivel@codeaurora.org>
2011-07-22 00:14:42 +0000

    Source/WebCore: Fix for bug 64046 - Wrong image height in absolutely positioned div in relatively positioned parent with bottom padding. https://bugs.webkit.org/show_bug.cgi?id=64046
    Patch by Kulanthaivel Palanichamy <kulanthaivel@codeaurora.org> on 2011-07-21
    Reviewed by David Hyatt.
    
    Test: fast/css/absolute-child-with-percent-height-inside-relative-parent.html
    
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::availableLogicalHeightUsing):
    
    LayoutTests: Test to cover absolutely positioned child with percentage height
    in relatively positioned parent with bottom padding.
    https://bugs.webkit.org/show_bug.cgi?id=64046
    
    Patch by Kulanthaivel Palanichamy <kulanthaivel@codeaurora.org> on 2011-07-21
    Reviewed by David Hyatt.
    
    * fast/css/absolute-child-with-percent-height-inside-relative-parent-expected.txt: Added.
    * fast/css/absolute-child-with-percent-height-inside-relative-parent.html: Added.
    
    Canonical link: https://commits.webkit.org/80734@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@91533 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderBox.cpp
+++ Source/WebCore/rendering/RenderBox.cpp
@@ -2055,8 +2055,17 @@ LayoutUnit RenderBox::availableLogicalHeightUsing(const Length& h) const
     if (isTableCell() && (h.isAuto() || h.isPercent()))
         return overrideHeight() - borderAndPaddingLogicalWidth();
 
-    if (h.isPercent())
-       return computeContentBoxLogicalHeight(h.calcValue(containingBlock()->availableLogicalHeight()));
+    if (h.isPercent()) {
+        LayoutUnit availableHeight;
+        // https://bugs.webkit.org/show_bug.cgi?id=64046
+        // For absolutely positioned elements whose containing block is based on a block-level element,
+        // the percentage is calculated with respect to the height of the padding box of that element
+        if (isPositioned())
+            availableHeight = containingBlockLogicalHeightForPositioned(containingBlock());
+        else
+            availableHeight = containingBlock()->availableLogicalHeight();
+        return computeContentBoxLogicalHeight(h.calcValue(availableHeight));
+    }
 
     // FIXME: We can't just check top/bottom here.
     // https://bugs.webkit.org/show_bug.cgi?id=46500
