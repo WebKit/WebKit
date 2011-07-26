5ca62fb368b0b10dc5ce8a455c62838008e0c21e
Dan Bernstein <mitz@apple.com>
2011-07-26 03:21:52 +0000

    <rdar://problem/9835028> Font loading during layout can cause layout code to be re-entered via resource load delegate https://bugs.webkit.org/show_bug.cgi?id=65123
    Source/WebCore:
    
    Reviewed by Anders Carlsson and Darin Adler.
    
    Since CSSFontFaceSource::getFontData() can get called during layout, avoid calling out to loader
    code from under it, and instead defer that work using a zero-delay timer.
    
    * css/CSSFontFaceSource.cpp:
    (WebCore::CSSFontFaceSource::CSSFontFaceSource):
    (WebCore::CSSFontFaceSource::~CSSFontFaceSource):
    (WebCore::CSSFontFaceSource::getFontData): Rather than starting the font load here, schedule
    a zero-delay timer to do it.
    (WebCore::CSSFontFaceSource::startLoadingTimerFired): Added. Starts loading the font if needed.
    * css/CSSFontFaceSource.h:
    
    LayoutTests:
    
    Reviewed by Darin Adler.
    
    Updated tests that depended on fonts loading synchronously during layout.
    Unfortunately, font loading does not fire any DOM events, so in most cases
    a constant timeout had to be introduced.
    
    * fast/blockflow/broken-ideograph-small-caps.html:
    * fast/css/color-leakage.html:
    * fast/css/custom-font-xheight.html:
    * fast/css/font-face-multiple-faces.html:
    * fast/css/font-face-multiple-remote-sources.html:
    * fast/css/font-face-remote.html:
    * fast/css/font-face-woff.html:
    * svg/W3C-SVG-1.1-SE/text-intro-09-b.svg:
    * svg/W3C-SVG-1.1/fonts-elem-07-b.svg:
    * svg/custom/svg-fonts-fallback.xhtml:
    * svg/custom/svg-fonts-in-html.html:
    * svg/custom/svg-fonts-segmented.xhtml:
    * svg/custom/svg-fonts-with-no-element-reference.html:
    * svg/custom/svg-fonts-without-missing-glyph.xhtml:
    * svg/text/text-overflow-ellipsis-svgfont.html:
    
    
    
    Canonical link: https://commits.webkit.org/80909@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@91738 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/CSSFontFaceSource.cpp
+++ Source/WebCore/css/CSSFontFaceSource.cpp
@@ -50,6 +50,7 @@ CSSFontFaceSource::CSSFontFaceSource(const String& str, CachedFont* font)
     : m_string(str)
     , m_font(font)
     , m_face(0)
+    , m_loadStartTimer(this, &CSSFontFaceSource::startLoadingTimerFired)
 #if ENABLE(SVG_FONTS)
     , m_hasExternalSVGFont(false)
 #endif
@@ -60,6 +61,7 @@ CSSFontFaceSource::CSSFontFaceSource(const String& str, CachedFont* font)
 
 CSSFontFaceSource::~CSSFontFaceSource()
 {
+    m_loadStartTimer.stop();
     if (m_font)
         m_font->removeClient(this);
     pruneTable();
@@ -172,9 +174,12 @@ SimpleFontData* CSSFontFaceSource::getFontData(const FontDescription& fontDescri
 #endif
         }
     } else {
-        // Kick off the load now.
-        if (CachedResourceLoader* cachedResourceLoader = fontSelector->cachedResourceLoader())
-            m_font->beginLoadIfNeeded(cachedResourceLoader);
+        // Kick off the load now. Do it on a zero-delay timer rather than synchronously, because we may be in
+        // the middle of layout, and the loader may invoke arbitrary delegate or event handler code.
+        m_fontSelector = fontSelector;
+        if (!m_loadStartTimer.isActive())
+            m_loadStartTimer.startOneShot(0);
+
         // FIXME: m_string is a URL so it makes no sense to pass it as a family name.
         SimpleFontData* tempData = fontCache()->getCachedFontData(fontDescription, m_string);
         if (!tempData)
@@ -187,6 +192,17 @@ SimpleFontData* CSSFontFaceSource::getFontData(const FontDescription& fontDescri
     m_fontDataTable.set(hashKey, fontDataRawPtr);
 
     return fontDataRawPtr;
+}
+
+void CSSFontFaceSource::startLoadingTimerFired(Timer<WebCore::CSSFontFaceSource>*)
+{
+    ASSERT(m_font);
+    ASSERT(m_fontSelector);
+
+    if (CachedResourceLoader* cachedResourceLoader = m_fontSelector->cachedResourceLoader())
+        m_font->beginLoadIfNeeded(cachedResourceLoader);
+
+    m_fontSelector = nullptr;
 }
 
 #if ENABLE(SVG_FONTS)
