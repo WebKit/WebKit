1d604f072a39f52c0dbe048692a8229462fda9da
James Simonsen <simonjam@chromium.org>
2011-07-30 02:11:24 +0000

    Remove zipcar.com site specific hack added in r87361 https://bugs.webkit.org/show_bug.cgi?id=65405
    Reviewed by Adam Barth.
    
    * html/HTMLScriptElement.cpp:
    (WebCore::HTMLScriptElement::insertedIntoDocument):
    
    
    Canonical link: https://commits.webkit.org/81184@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@92041 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLScriptElement.cpp
+++ Source/WebCore/html/HTMLScriptElement.cpp
@@ -29,7 +29,6 @@
 #include "EventNames.h"
 #include "HTMLNames.h"
 #include "ScriptEventListener.h"
-#include "Settings.h"
 #include "Text.h"
 
 namespace WebCore {
@@ -82,33 +81,8 @@ void HTMLScriptElement::parseMappedAttribute(Attribute* attr)
         HTMLElement::parseMappedAttribute(attr);
 }
 
-static bool needsOldRequirejsQuirk(HTMLScriptElement* element)
-{
-    if (element->fastGetAttribute(typeAttr) != "script/cache")
-        return false;
-
-    Document* document = element->document();
-
-    const KURL& url = document->url();
-    if (!equalIgnoringCase(url.host(), "www.zipcar.com"))
-        return false;
-
-    Settings* settings = document->settings();
-    if (!settings)
-        return false;
-    if (!settings->needsSiteSpecificQuirks())
-        return false;
-
-    return true;
-}
-
 void HTMLScriptElement::insertedIntoDocument()
 {
-    if (needsOldRequirejsQuirk(this)) {
-        if (!asyncAttributeValue())
-            handleAsyncAttribute(); // Clear forceAsync, so this script loads in parallel, but executes in order.
-        setAttribute(typeAttr, "text/javascript");
-    }
     HTMLElement::insertedIntoDocument();
     ScriptElement::insertedIntoDocument();
 }
