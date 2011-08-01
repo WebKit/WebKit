e228cf07aa4d7b102d02a54fc672b6875936b98d
Zhenyao Mo <zmo@google.com>
2011-08-01 23:12:49 +0000

    Unreviewed, rolling out r92022. http://trac.webkit.org/changeset/92022 https://bugs.webkit.org/show_bug.cgi?id=65203
    try to see if this is causing a bunch of browser tests failues
    on win bots - will recommit if not
    
    Source/WebCore:
    
    * platform/graphics/chromium/FontChromiumWin.cpp:
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareFontPainter::TransparencyAwareFontPainter):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareFontPainter::init):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareFontPainter::initializeForGDI):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareFontPainter::~TransparencyAwareFontPainter):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareGlyphPainter::TransparencyAwareGlyphPainter):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareGlyphPainter::~TransparencyAwareGlyphPainter):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareGlyphPainter::drawGlyphs):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareUniscribePainter::hdc):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareUniscribePainter::TransparencyAwareUniscribePainter):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareUniscribePainter::~TransparencyAwareUniscribePainter):
    (WebCore::drawGlyphsWin):
    (WebCore::Font::drawComplexText):
    * platform/graphics/chromium/UniscribeHelper.cpp:
    (WebCore::UniscribeHelper::draw):
    * platform/graphics/skia/PlatformContextSkia.cpp:
    (WebCore::PlatformContextSkia::isNativeFontRenderingAllowed):
    * platform/graphics/skia/PlatformContextSkia.h:
    * platform/graphics/skia/SkiaFontWin.cpp:
    (WebCore::windowsCanHandleDrawTextShadow):
    (WebCore::windowsCanHandleTextDrawing):
    (WebCore::windowsCanHandleTextDrawingWithoutShadow):
    * platform/graphics/skia/SkiaFontWin.h:
    
    LayoutTests:
    
    * platform/chromium/test_expectations.txt:
    
    
    Canonical link: https://commits.webkit.org/81293@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@92150 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/skia/PlatformContextSkia.cpp
+++ Source/WebCore/platform/graphics/skia/PlatformContextSkia.cpp
@@ -569,6 +569,17 @@ const SkBitmap* PlatformContextSkia::bitmap() const
     return &m_canvas->getDevice()->accessBitmap(false);
 }
 
+bool PlatformContextSkia::isNativeFontRenderingAllowed()
+{
+#if ENABLE(SKIA_TEXT)
+    return false;
+#else
+    if (useSkiaGPU())
+        return false;
+    return skia::SupportsPlatformPaint(m_canvas);
+#endif
+}
+
 void PlatformContextSkia::getImageResamplingHint(IntSize* srcSize, FloatSize* dstSize) const
 {
     *srcSize = m_imageResamplingHintSrcSize;
--- Source/WebCore/platform/graphics/skia/SkiaFontWin.h
+++ Source/WebCore/platform/graphics/skia/SkiaFontWin.h
@@ -39,6 +39,33 @@ class SkPoint;
 namespace WebCore {
 
 class GraphicsContext;
+class PlatformContextSkia;
+
+// The functions below are used for more complex font drawing (effects such as
+// stroking and more complex transforms) than Windows supports directly.  Since 
+// Windows drawing is faster you should use windowsCanHandleTextDrawing first to 
+// check if using Skia is required at all.
+// Note that the text will look different (no ClearType) so this should only be
+// used when necessary.
+//
+// When you call a Skia* text drawing function, various glyph outlines will be
+// cached. As a result, you should call SkiaWinOutlineCache::removePathsForFont
+// when the font is destroyed so that the cache does not outlive the font (since
+// the HFONTs are recycled).
+//
+// Remember that Skia's text drawing origin is the baseline, like WebKit, not
+// the top, like Windows.
+
+// Returns true if the fillColor and shadowColor are opaque and the text-shadow
+// is not blurred.
+bool windowsCanHandleDrawTextShadow(GraphicsContext*);
+
+// Returns true if advanced font rendering is recommended.
+bool windowsCanHandleTextDrawing(GraphicsContext*);
+
+// Returns true if advanced font rendering is recommended if shadows are
+// disregarded.
+bool windowsCanHandleTextDrawingWithoutShadow(GraphicsContext*);
 
 // Note that the offsets parameter is optional.  If not NULL it represents a
 // per glyph offset (such as returned by ScriptPlace Windows API function).
