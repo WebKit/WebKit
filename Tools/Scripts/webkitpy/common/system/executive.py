6d2570961e5629e3e59250a24011d6a1d4e7c350
Patrick Gansterer <paroga@webkit.org>
2011-08-02 14:11:02 +0000

    Fix _win32_check_running_pid on 64bit windows python https://bugs.webkit.org/show_bug.cgi?id=64950
    Reviewed by Adam Roben.
    
    Use the correct types for the PROCESSENTRY32 struct.
    th32DefaultHeapID is declared as ULONG_PTR and not as DWORD.
    Using 32bit for pointers instead of 64bit results in an ERROR_BAD_LENGTH
    failure, when calling the Process32First function.
    This is covered by the executive unittest.
    
    * Scripts/webkitpy/common/system/executive.py:
    
    
    Canonical link: https://commits.webkit.org/81331@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@92188 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/system/executive.py
+++ Tools/Scripts/webkitpy/common/system/executive.py
@@ -243,7 +243,7 @@ class Executive(object):
             _fields_ = [("dwSize", ctypes.c_ulong),
                         ("cntUsage", ctypes.c_ulong),
                         ("th32ProcessID", ctypes.c_ulong),
-                        ("th32DefaultHeapID", ctypes.c_ulong),
+                        ("th32DefaultHeapID", ctypes.POINTER(ctypes.c_ulong)),
                         ("th32ModuleID", ctypes.c_ulong),
                         ("cntThreads", ctypes.c_ulong),
                         ("th32ParentProcessID", ctypes.c_ulong),
