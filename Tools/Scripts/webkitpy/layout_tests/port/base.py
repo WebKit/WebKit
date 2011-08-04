114baa286fe07212ac9318a5ad07518ce98f8cd6
Adam Barth <abarth@webkit.org>
2011-08-04 20:38:37 +0000

    More logging to help debug the "no SVN rev on Chromium Windows bots" bug.  My current theory is that we're trying to detect the SCM system in the wrong directory.
    * Scripts/webkitpy/layout_tests/port/base.py:
    
    
    Canonical link: https://commits.webkit.org/81528@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@92407 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/port/base.py
+++ Tools/Scripts/webkitpy/layout_tests/port/base.py
@@ -631,7 +631,11 @@ class Port(object):
         # FIXME: Due to Chromium's insane DEPS system, the root of the WebKit
         # checkout isn't actually from svn.webkit.org. Instead, we detect the
         # scm from the Source directory, which really does map to svn.webkit.org.
-        return detect_scm_system(self.path_from_webkit_base('Source'))
+        path = self.path_from_webkit_base('Source')
+        _log.debug('---webkit_source_scm---')
+        _log.debug(path)
+        _log.debug(os.getcwd())
+        return detect_scm_system(path)
 
     def path_to_test_expectations_file(self):
         """Update the test expectations to the passed-in string.
