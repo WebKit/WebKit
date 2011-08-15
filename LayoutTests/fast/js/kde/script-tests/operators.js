9cefd8c8a12e088f0c726fbf58a4a11b44e0a455
Gavin Barraclough <barraclough@apple.com>
2011-08-15 23:44:41 +0000

    https://bugs.webkit.org/show_bug.cgi?id=66263 DFG JIT does not always zero extend boolean result of DFG operations
    Reviewed by Sam Weinig.
    
    Source/JavaScriptCore:
    
    * dfg/DFGOperations.cpp:
    * dfg/DFGOperations.h:
        - Change bool return values to a 64-bit type.
    
    LayoutTests:
    
    * fast/js/kde/script-tests/operators.js:
    (nonSpeculativeNotInner):
    (nonSpeculativeNot):
    (nonSpeculativeLessInner):
    (nonSpeculativeLess):
    (nonSpeculativeLessEqInner):
    (nonSpeculativeLessEq):
    (nonSpeculativeGreaterInner):
    (nonSpeculativeGreater):
    (nonSpeculativeGreaterEqInner):
    (nonSpeculativeGreaterEq):
    (nonSpeculativeEqualInner):
    (nonSpeculativeEqual):
    (nonSpeculativeNotEqualInner):
    (nonSpeculativeNotEqual):
    (nonSpeculativeStrictEqualInner):
    (nonSpeculativeStrictEqual):
    (nonSpeculativeStrictNotEqualInner):
    (nonSpeculativeStrictNotEqual):
        - Add tests cases that will hit the non-spec path.
    
    
    
    Canonical link: https://commits.webkit.org/82089@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@93070 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/js/kde/script-tests/operators.js
+++ LayoutTests/fast/js/kde/script-tests/operators.js
@@ -1,3 +1,102 @@
+function nonSpeculativeNotInner(argument, o1, o2)
+{
+    // The + operator on objects is a reliable way to avoid the speculative JIT path for now at least.
+    o1 + o2;
+    return !argument;
+}
+function nonSpeculativeNot(argument)
+{
+    return nonSpeculativeNotInner(argument, {}, {});
+}
+
+function nonSpeculativeLessInner(a, b, o1, o2)
+{
+    // The + operator on objects is a reliable way to avoid the speculative JIT path for now at least.
+    o1 + o2;
+    return a < b;
+}
+function nonSpeculativeLess(a, b)
+{
+    return nonSpeculativeLessInner(a, b, {}, {});
+}
+
+function nonSpeculativeLessEqInner(a, b, o1, o2)
+{
+    // The + operator on objects is a reliable way to avoid the speculative JIT path for now at least.
+    o1 + o2;
+    return a <= b;
+}
+function nonSpeculativeLessEq(a, b)
+{
+    return nonSpeculativeLessEqInner(a, b, {}, {});
+}
+
+function nonSpeculativeGreaterInner(a, b, o1, o2)
+{
+    // The + operator on objects is a reliable way to avoid the speculative JIT path for now at least.
+    o1 + o2;
+    return a > b;
+}
+function nonSpeculativeGreater(a, b)
+{
+    return nonSpeculativeGreaterInner(a, b, {}, {});
+}
+
+function nonSpeculativeGreaterEqInner(a, b, o1, o2)
+{
+    // The + operator on objects is a reliable way to avoid the speculative JIT path for now at least.
+    o1 + o2;
+    return a >= b;
+}
+function nonSpeculativeGreaterEq(a, b)
+{
+    return nonSpeculativeGreaterEqInner(a, b, {}, {});
+}
+
+function nonSpeculativeEqualInner(a, b, o1, o2)
+{
+    // The + operator on objects is a reliable way to avoid the speculative JIT path for now at least.
+    o1 + o2;
+    return a == b;
+}
+function nonSpeculativeEqual(a, b)
+{
+    return nonSpeculativeEqualInner(a, b, {}, {});
+}
+
+function nonSpeculativeNotEqualInner(a, b, o1, o2)
+{
+    // The + operator on objects is a reliable way to avoid the speculative JIT path for now at least.
+    o1 + o2;
+    return a != b;
+}
+function nonSpeculativeNotEqual(a, b)
+{
+    return nonSpeculativeNotEqualInner(a, b, {}, {});
+}
+
+function nonSpeculativeStrictEqualInner(a, b, o1, o2)
+{
+    // The + operator on objects is a reliable way to avoid the speculative JIT path for now at least.
+    o1 + o2;
+    return a === b;
+}
+function nonSpeculativeStrictEqual(a, b)
+{
+    return nonSpeculativeStrictEqualInner(a, b, {}, {});
+}
+
+function nonSpeculativeStrictNotEqualInner(a, b, o1, o2)
+{
+    // The + operator on objects is a reliable way to avoid the speculative JIT path for now at least.
+    o1 + o2;
+    return a !== b;
+}
+function nonSpeculativeStrictNotEqual(a, b)
+{
+    return nonSpeculativeStrictNotEqualInner(a, b, {}, {});
+}
+
 // operator !
 shouldBeTrue("!undefined");
 shouldBeTrue("!null");
@@ -7,6 +106,15 @@ shouldBeTrue("!!1");
 shouldBeTrue("!0");
 shouldBeTrue("!!'a'");
 shouldBeTrue("!''");
+
+shouldBeTrue("nonSpeculativeNot(undefined)");
+shouldBeTrue("nonSpeculativeNot(null)");
+shouldBeTrue("nonSpeculativeNot(!true)");
+shouldBeTrue("nonSpeculativeNot(false)");
+shouldBeTrue("nonSpeculativeNot(!1)");
+shouldBeTrue("nonSpeculativeNot(0)");
+shouldBeTrue("nonSpeculativeNot(!'a')");
+shouldBeTrue("nonSpeculativeNot('')");
 
 // unary plus
 shouldBe("+9", "9");
@@ -148,18 +256,31 @@ shouldBe("'-6' % 4", "-2");
 shouldBe("2==2", "true");
 shouldBe("1==2", "false");
 
+shouldBe("nonSpeculativeEqual(2,2)", "true");
+shouldBe("nonSpeculativeEqual(1,2)", "false");
+
 shouldBe("1<2", "true");
 shouldBe("1<=2", "true");
 shouldBe("2<1", "false");
 shouldBe("2<=1", "false");
+
+shouldBe("nonSpeculativeLess(1,2)", "true");
+shouldBe("nonSpeculativeLessEq(1,2)", "true");
+shouldBe("nonSpeculativeLess(2,1)", "false");
+shouldBe("nonSpeculativeLessEq(2,1)", "false");
 
 shouldBe("2>1", "true");
 shouldBe("2>=1", "true");
 shouldBe("1>=2", "false");
 shouldBe("1>2", "false");
 
-shouldBe("'abc' == 'abc'", "true");
-shouldBe("'abc' != 'xyz'", "true");
+shouldBe("nonSpeculativeGreater(2,1)", "true");
+shouldBe("nonSpeculativeGreaterEq(2,1)", "true");
+shouldBe("nonSpeculativeGreaterEq(1,2)", "false");
+shouldBe("nonSpeculativeGreater(1,2)", "false");
+
+shouldBeTrue("'abc' == 'abc'");
+shouldBeTrue("'abc' != 'xyz'");
 shouldBeTrue("true == true");
 shouldBeTrue("false == false");
 shouldBeTrue("true != false");
@@ -184,11 +305,43 @@ shouldBeTrue("1 == true");
 shouldBeTrue("false == 0");
 shouldBeTrue("0 == false");
 
+shouldBeTrue("nonSpeculativeEqual('abc', 'abc')");
+shouldBeTrue("nonSpeculativeNotEqual('abc', 'xyz')");
+shouldBeTrue("nonSpeculativeEqual(true, true)");
+shouldBeTrue("nonSpeculativeEqual(false, false)");
+shouldBeTrue("nonSpeculativeNotEqual(true, false)");
+shouldBeTrue("nonSpeculativeNotEqual('a', null)");
+shouldBeTrue("nonSpeculativeNotEqual('a', undefined)");
+shouldBeTrue("nonSpeculativeEqual(null, null)");
+shouldBeTrue("nonSpeculativeEqual(null, undefined)");
+shouldBeTrue("nonSpeculativeEqual(undefined, undefined)");
+shouldBeTrue("nonSpeculativeNotEqual(NaN, NaN)");
+shouldBeTrue("nonSpeculativeNotEqual(true, undefined)");
+shouldBeTrue("nonSpeculativeNotEqual(true, null)");
+shouldBeTrue("nonSpeculativeNotEqual(false, undefined)");
+shouldBeTrue("nonSpeculativeNotEqual(false, null)");
+shouldBeTrue("nonSpeculativeEqual('0', 0)");
+shouldBeTrue("nonSpeculativeEqual(1, '1')");
+shouldBeTrue("nonSpeculativeNotEqual(NaN, NaN)");
+shouldBeTrue("nonSpeculativeNotEqual(NaN, 0)");
+shouldBeTrue("nonSpeculativeNotEqual(NaN, undefined)");
+shouldBeTrue("nonSpeculativeEqual(true, 1)");
+shouldBeTrue("nonSpeculativeNotEqual(true, 2)");
+shouldBeTrue("nonSpeculativeEqual(1, true)");
+shouldBeTrue("nonSpeculativeEqual(false, 0)");
+shouldBeTrue("nonSpeculativeEqual(0, false)");
+
 shouldBe("'abc' < 'abx'", "true");
 shouldBe("'abc' < 'abcd'", "true");
 shouldBe("'abc' < 'abc'", "false");
 shouldBe("'abcd' < 'abcd'", "false");
 shouldBe("'abx' < 'abc'", "false");
+
+shouldBe("nonSpeculativeLess('abc', 'abx')", "true");
+shouldBe("nonSpeculativeLess('abc', 'abcd')", "true");
+shouldBe("nonSpeculativeLess('abc', 'abc')", "false");
+shouldBe("nonSpeculativeLess('abcd', 'abcd')", "false");
+shouldBe("nonSpeculativeLess('abx', 'abc')", "false");
 
 shouldBe("'abc' <= 'abc'", "true");
 shouldBe("'abc' <= 'abx'", "true");
@@ -196,11 +349,23 @@ shouldBe("'abx' <= 'abc'", "false");
 shouldBe("'abcd' <= 'abc'", "false");
 shouldBe("'abc' <= 'abcd'", "true");
 
+shouldBe("nonSpeculativeLessEq('abc', 'abc')", "true");
+shouldBe("nonSpeculativeLessEq('abc', 'abx')", "true");
+shouldBe("nonSpeculativeLessEq('abx', 'abc')", "false");
+shouldBe("nonSpeculativeLessEq('abcd', 'abc')", "false");
+shouldBe("nonSpeculativeLessEq('abc', 'abcd')", "true");
+
 shouldBe("'abc' > 'abx'", "false");
 shouldBe("'abc' > 'abc'", "false");
 shouldBe("'abcd' > 'abc'", "true");
 shouldBe("'abx' > 'abc'", "true");
 shouldBe("'abc' > 'abcd'", "false");
+
+shouldBe("nonSpeculativeGreater('abc', 'abx')", "false");
+shouldBe("nonSpeculativeGreater('abc', 'abc')", "false");
+shouldBe("nonSpeculativeGreater('abcd', 'abc')", "true");
+shouldBe("nonSpeculativeGreater('abx', 'abc')", "true");
+shouldBe("nonSpeculativeGreater('abc', 'abcd')", "false");
 
 shouldBe("'abc' >= 'abc'", "true");
 shouldBe("'abcd' >= 'abc'", "true");
@@ -208,6 +373,13 @@ shouldBe("'abx' >= 'abc'", "true");
 shouldBe("'abc' >= 'abx'", "false");
 shouldBe("'abc' >= 'abx'", "false");
 shouldBe("'abc' >= 'abcd'", "false");
+
+shouldBe("nonSpeculativeGreaterEq('abc', 'abc')", "true");
+shouldBe("nonSpeculativeGreaterEq('abcd', 'abc')", "true");
+shouldBe("nonSpeculativeGreaterEq('abx', 'abc')", "true");
+shouldBe("nonSpeculativeGreaterEq('abc', 'abx')", "false");
+shouldBe("nonSpeculativeGreaterEq('abc', 'abx')", "false");
+shouldBe("nonSpeculativeGreaterEq('abc', 'abcd')", "false");
 
 // mixed strings and numbers - results validated in NS+moz+IE5
 shouldBeFalse("'abc' <= 0"); // #35246
@@ -220,31 +392,67 @@ shouldBeTrue("0 <= null");
 shouldBeTrue("null <= null");
 shouldBeTrue("6 < '52'");
 shouldBeTrue("6 < '72'"); // #36087
-shouldBe("NaN < 0", "false");
-shouldBe("NaN <= 0", "false");
-shouldBe("NaN > 0", "false");
-shouldBe("NaN >= 0", "false");
+shouldBeFalse("NaN < 0");
+shouldBeFalse("NaN <= 0");
+shouldBeFalse("NaN > 0");
+shouldBeFalse("NaN >= 0");
+
+shouldBeFalse("nonSpeculativeLessEq('abc', 0)"); // #35246
+shouldBeTrue("nonSpeculativeLessEq('', 0)");
+shouldBeTrue("nonSpeculativeLessEq(' ', 0)");
+shouldBeTrue("nonSpeculativeLessEq(null, 0)");
+shouldBeFalse("nonSpeculativeLessEq(0, 'abc')");
+shouldBeTrue("nonSpeculativeLessEq(0, '')");
+shouldBeTrue("nonSpeculativeLessEq(0, null)");
+shouldBeTrue("nonSpeculativeLessEq(null, null)");
+shouldBeTrue("nonSpeculativeLess(6, '52')");
+shouldBeTrue("nonSpeculativeLess(6, '72')"); // #36087
+shouldBeFalse("nonSpeculativeLess(NaN, 0)");
+shouldBeFalse("nonSpeculativeLessEq(NaN, 0)");
+shouldBeFalse("nonSpeculativeGreater(NaN, 0)");
+shouldBeFalse("nonSpeculativeGreaterEq(NaN, 0)");
 
 // strict comparison ===
-shouldBe("0 === false", "false");
+shouldBeFalse("0 === false");
 //shouldBe("undefined === undefined", "true"); // aborts in IE5 (undefined is not defined ;)
-shouldBe("null === null", "true");
-shouldBe("NaN === NaN", "false");
-shouldBe("0.0 === 0", "true");
-shouldBe("'abc' === 'abc'", "true");
-shouldBe("'a' === 'x'", "false");
-shouldBe("1 === '1'", "false");
-shouldBe("'1' === 1", "false");
-shouldBe("true === true", "true");
-shouldBe("false === false", "true");
-shouldBe("true === false", "false");
-shouldBe("Math === Math", "true");
-shouldBe("Math === Boolean", "false");
-shouldBe("Infinity === Infinity", "true");
+shouldBeTrue("null === null");
+shouldBeFalse("NaN === NaN");
+shouldBeTrue("0.0 === 0");
+shouldBeTrue("'abc' === 'abc'");
+shouldBeFalse("'a' === 'x'");
+shouldBeFalse("1 === '1'");
+shouldBeFalse("'1' === 1");
+shouldBeTrue("true === true");
+shouldBeTrue("false === false");
+shouldBeFalse("true === false");
+shouldBeTrue("Math === Math");
+shouldBeFalse("Math === Boolean");
+shouldBeTrue("Infinity === Infinity");
+
+// strict comparison ===
+shouldBeFalse("nonSpeculativeStrictEqual(0, false)");
+//shouldBe("undefined === undefined", "true"); // aborts in IE5 (undefined is not defined ;)
+shouldBeTrue("nonSpeculativeStrictEqual(null, null)");
+shouldBeFalse("nonSpeculativeStrictEqual(NaN, NaN)");
+shouldBeTrue("nonSpeculativeStrictEqual(0.0, 0)");
+shouldBeTrue("nonSpeculativeStrictEqual('abc', 'abc')");
+shouldBeFalse("nonSpeculativeStrictEqual('a', 'x')");
+shouldBeFalse("nonSpeculativeStrictEqual(1, '1')");
+shouldBeFalse("nonSpeculativeStrictEqual('1', 1)");
+shouldBeTrue("nonSpeculativeStrictEqual(true, true)");
+shouldBeTrue("nonSpeculativeStrictEqual(false, false)");
+shouldBeFalse("nonSpeculativeStrictEqual(true, false)");
+shouldBeTrue("nonSpeculativeStrictEqual(Math, Math)");
+shouldBeFalse("nonSpeculativeStrictEqual(Math, Boolean)");
+shouldBeTrue("nonSpeculativeStrictEqual(Infinity, Infinity)");
 
 // !==
 shouldBe("0 !== 0", "false");
 shouldBe("0 !== 1", "true");
+
+// !==
+shouldBe("nonSpeculativeStrictNotEqual(0, 0)", "false");
+shouldBe("nonSpeculativeStrictNotEqual(0, 1)", "true");
 
 shouldBe("typeof undefined", "'undefined'");
 shouldBe("typeof null", "'object'");
