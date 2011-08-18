34442534f31ec97d4e8b5a18e2aea1ec072ef494
Wyatt Carss <wcarss@chromium.org>
2011-08-18 08:44:02 +0000

    Selecting all and inserting text into a page with a frameset leads to a NULL ptr https://bugs.webkit.org/show_bug.cgi?id=66288
    Source/WebCore:
    
    Patch by Wyatt Carss <wcarss@chromium.org> on 2011-08-18
    Reviewed by Tony Chang.
    
    VisibleSelection returns a NoSelection to InsertTextCommand::doApply as
    a new endingSelection, which isn't a sane state to be in. The code
    tries to get a position on a higher node for a better selection, but
    cannot go above the frameset (which exists in place of the body tag),
    which lacks a renderer, and therefore cannot have a non-NoSelection
    VisibleSelection.
    
    Because this is a rare corner case, it seemed more reasonable to bail
    out in this circumstance than to change the way VisibleSelection
    canonicalization works. The new behavior is to delete the content, but
    insert nothing. I investigated the possibility of trying to move above
    or below the frameset in this particular case, but there still isn't a
    renderer, so it didn't prevent the crash.
    
    Test: editing/inserting/insert-text-into-empty-frameset-crash.html
    
    * editing/InsertTextCommand.cpp:
    (WebCore::InsertTextCommand::doApply):
    
    LayoutTests:
    
    This test selects all and inserts text in a page which contains a frameset.
    It tests for a crash caused by a NULL ptr which can occur in InsertText::doApply.
    
    Patch by Wyatt Carss <wcarss@chromium.org> on 2011-08-18
    Reviewed by Tony Chang.
    
    * editing/inserting/insert-text-into-empty-frameset-crash-expected.txt: Added.
    * editing/inserting/insert-text-into-empty-frameset-crash.html: Added.
    
    Canonical link: https://commits.webkit.org/82286@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@93290 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/editing/InsertTextCommand.cpp
+++ Source/WebCore/editing/InsertTextCommand.cpp
@@ -108,6 +108,11 @@ void InsertTextCommand::doApply()
         if (performTrivialReplace(m_text, m_selectInsertedText))
             return;
         deleteSelection(false, true, true, false);
+        // deleteSelection eventually makes a new endingSelection out of a Position. If that Position doesn't have
+        // a renderer (e.g. it is on a <frameset> in the DOM), the VisibleSelection cannot be canonicalized to 
+        // anything other than NoSelection. The rest of this function requires a real endingSelection, so bail out.
+        if (endingSelection().isNone())
+            return;
     }
 
     Position startPosition(endingSelection().start());
