b3ad52bf420333dadadba7d244412586998c1b24
Ryuan Choi <ryuan.choi@samsung.com>
2011-08-19 01:04:12 +0000

    [EFL] Broken rendering occured when resized in ewk_view_single. https://bugs.webkit.org/show_bug.cgi?id=66271
    Force rendering when ewk_view resized.
    It's because Evas_Object doesn't generate any rendering request although
    it was changed.
    
    Reviewed by Tony Chang.
    
    * ewk/ewk_view.cpp:
    (_ewk_view_smart_calculate):
    
    
    Canonical link: https://commits.webkit.org/82369@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@93379 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/efl/ewk/ewk_view.cpp
+++ Source/WebKit/efl/ewk/ewk_view.cpp
@@ -839,6 +839,8 @@ static void _ewk_view_smart_calculate(Evas_Object *o)
         sd->view.w = w;
         sd->view.h = h;
 
+        _ewk_view_repaint_add(priv, 0, 0, w, h);
+
         // This callback is a good place e.g. to change fixed layout size (ewk_view_fixed_layout_size_set).
         evas_object_smart_callback_call(o, "view,resized", 0);
     }
