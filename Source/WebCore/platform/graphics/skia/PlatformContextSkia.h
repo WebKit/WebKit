edf2e6fa5b8b433909d9ad4abbae93194be9f848
Pavel Podivilov <podivilov@chromium.org>
2011-08-26 16:26:04 +0000

    Unreviewed, rolling out r93870. http://trac.webkit.org/changeset/93870 https://bugs.webkit.org/show_bug.cgi?id=65203
    Broke i18n chromium tests
    
    Source/WebCore:
    
    * platform/graphics/chromium/FontChromiumWin.cpp:
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareFontPainter::TransparencyAwareFontPainter):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareFontPainter::init):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareFontPainter::initializeForGDI):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareFontPainter::~TransparencyAwareFontPainter):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareGlyphPainter::TransparencyAwareGlyphPainter):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareGlyphPainter::~TransparencyAwareGlyphPainter):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareGlyphPainter::drawGlyphs):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareUniscribePainter::hdc):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareUniscribePainter::TransparencyAwareUniscribePainter):
    (WebCore::TransparencyAwareFontPainter::TransparencyAwareUniscribePainter::~TransparencyAwareUniscribePainter):
    (WebCore::drawGlyphsWin):
    (WebCore::Font::drawComplexText):
    * platform/graphics/chromium/UniscribeHelper.cpp:
    (WebCore::UniscribeHelper::draw):
    * platform/graphics/skia/PlatformContextSkia.cpp:
    (WebCore::PlatformContextSkia::isNativeFontRenderingAllowed):
    * platform/graphics/skia/PlatformContextSkia.h:
    * platform/graphics/skia/SkiaFontWin.cpp:
    (WebCore::windowsCanHandleDrawTextShadow):
    (WebCore::windowsCanHandleTextDrawing):
    (WebCore::windowsCanHandleTextDrawingWithoutShadow):
    * platform/graphics/skia/SkiaFontWin.h:
    
    LayoutTests:
    
    * platform/chromium/test_expectations.txt:
    
    Canonical link: https://commits.webkit.org/82826@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@93881 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/skia/PlatformContextSkia.h
+++ Source/WebCore/platform/graphics/skia/PlatformContextSkia.h
@@ -168,6 +168,11 @@ public:
     bool printing() const { return m_printing; }
     void setPrinting(bool p) { m_printing = p; }
 
+    // Returns if the context allows rendering of fonts using native platform
+    // APIs. If false is returned font rendering is performed using the skia
+    // text drawing APIs.
+    bool isNativeFontRenderingAllowed();
+
     void getImageResamplingHint(IntSize* srcSize, FloatSize* dstSize) const;
     void setImageResamplingHint(const IntSize& srcSize, const FloatSize& dstSize);
     void clearImageResamplingHint();
