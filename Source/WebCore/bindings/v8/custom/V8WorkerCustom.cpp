2869d411d42436cc6107ccd5df184f6f1ea25910
Kentaro Hara <haraken@google.com>
2011-09-02 01:37:55 +0000

    Generate a Worker constructor of V8 using the IDL 'Constructor' extended attribute https://bugs.webkit.org/show_bug.cgi?id=67447
    Patch by Kentaro Hara <haraken@google.com> on 2011-09-01
    Reviewed by Dimitri Glazkov.
    
    Test: fast/workers/worker-constructor.html
          fast/workers/worker-event-listener.html
    
    * bindings/v8/custom/V8WorkerCustom.cpp: Removed constructorCallback().
    * workers/Worker.idl: Added the 'Constructor' extended attribute.
    
    Canonical link: https://commits.webkit.org/83290@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@94381 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/custom/V8WorkerCustom.cpp
+++ Source/WebCore/bindings/v8/custom/V8WorkerCustom.cpp
@@ -47,46 +47,6 @@
 
 namespace WebCore {
 
-v8::Handle<v8::Value> V8Worker::constructorCallback(const v8::Arguments& args)
-{
-    INC_STATS(L"DOM.Worker.Constructor");
-
-    if (!args.IsConstructCall())
-        return throwError("DOM object constructor cannot be called as a function.", V8Proxy::TypeError);
-
-    if (!args.Length())
-        return throwError("Not enough arguments", V8Proxy::TypeError);
-
-    v8::TryCatch tryCatch;
-    v8::Handle<v8::String> scriptUrl = args[0]->ToString();
-    if (tryCatch.HasCaught())
-        return throwError(tryCatch.Exception());
-
-    if (scriptUrl.IsEmpty())
-        return v8::Undefined();
-
-    // Get the script execution context.
-    ScriptExecutionContext* context = getScriptExecutionContext();
-    if (!context)
-        return v8::Undefined();
-
-    // Create the worker object.
-    // Note: it's OK to let this RefPtr go out of scope because we also call setDOMWrapper(), which effectively holds a reference to obj.
-    ExceptionCode ec = 0;
-    RefPtr<Worker> obj = Worker::create(toWebCoreString(scriptUrl), context, ec);
-    if (ec)
-        return throwError(ec);
-
-    // Setup the standard wrapper object internal fields.
-    v8::Handle<v8::Object> wrapperObject = args.Holder();
-    V8DOMWrapper::setDOMWrapper(wrapperObject, &info, obj.get());
-
-    obj->ref();
-    V8DOMWrapper::setJSWrapperForActiveDOMObject(obj.get(), v8::Persistent<v8::Object>::New(wrapperObject));
-
-    return wrapperObject;
-}
-
 v8::Handle<v8::Value> V8Worker::postMessageCallback(const v8::Arguments& args)
 {
     INC_STATS("DOM.Worker.postMessage");
