d028039ebf825090fe085866a8e34e1fc449ac0e
Andrew Wason <rectalogic@rectalogic.com>
2011-09-03 16:13:57 +0000

    [Qt] Enable support for WebGL OES_standard_derivatives for Qt https://bugs.webkit.org/show_bug.cgi?id=67430
    Patch by Andrew Wason <rectalogic@rectalogic.com> on 2011-09-03
    Reviewed by Noam Rosenthal.
    
    Tested using https://cvs.khronos.org/svn/repos/registry/trunk/public/webgl/sdk/tests/conformance/extensions/oes-standard-derivatives.html
    
    Enable existing support for OES_standard_derivatives for Qt.
    
    * platform/graphics/opengl/Extensions3DOpenGL.cpp:
    (WebCore::Extensions3DOpenGL::ensureEnabled):
    (WebCore::Extensions3DOpenGL::isEnabled):
    
    Canonical link: https://commits.webkit.org/83396@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@94489 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/opengl/Extensions3DOpenGL.cpp
+++ Source/WebCore/platform/graphics/opengl/Extensions3DOpenGL.cpp
@@ -105,7 +105,7 @@ bool Extensions3DOpenGL::supports(const String& name)
 
 void Extensions3DOpenGL::ensureEnabled(const String& name)
 {
-#if PLATFORM(MAC)
+#if PLATFORM(MAC) || PLATFORM(QT)
     if (name == "GL_OES_standard_derivatives") {
         // Enable support in ANGLE (if not enabled already)
         ANGLEWebKitBridge& compiler = m_context->m_compiler;
@@ -122,7 +122,7 @@ void Extensions3DOpenGL::ensureEnabled(const String& name)
 
 bool Extensions3DOpenGL::isEnabled(const String& name)
 {
-#if PLATFORM(MAC)
+#if PLATFORM(MAC) || PLATFORM(QT)
     if (name == "GL_OES_standard_derivatives") {
         ANGLEWebKitBridge& compiler = m_context->m_compiler;
         return compiler.getResources().OES_standard_derivatives;
