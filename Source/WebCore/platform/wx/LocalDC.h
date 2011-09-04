6cc3da225ac8e391cd6e49e9a01932862ab053cd
Kevin Ollivier <kevino@webkit.org>
2011-09-04 17:51:35 +0000

    [wx] Unreviewed build fix. Add new / moved files missing from last commit.
    Canonical link: https://commits.webkit.org/83409@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@94502 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebCore/platform/wx/LocalDC.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2011 Kevin Ollivier <kevino@theolliviers.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+#include "IntRect.h"
+
+#include <wtf/Assertions.h>
+
+#include <wx/defs.h>
+
+#include <wx/dc.h>
+#include <wx/dcmemory.h>
+#include <wx/rawbmp.h>
+
+namespace WebCore {
+
+wxBitmap* transparentBitmap(int width, int height);
+
+class LocalDC {
+
+public:
+    LocalDC(wxDC* host, const IntRect& r)
+    {
+#ifndef __WXMAC__
+        m_host = host;
+        int width = r.width();
+        int height = r.height();
+        m_bitmap = new wxBitmap(width, height, 32);
+        wxAlphaPixelData pixData(*m_bitmap, wxPoint(0,0), wxSize(width, height));
+        ASSERT(pixData);
+        if (pixData) {
+            wxAlphaPixelData::Iterator p(pixData);
+            for (int y=0; y<height; y++) {
+                wxAlphaPixelData::Iterator rowStart = p;
+                for (int x=0; x<width; x++) {
+                    p.Alpha() = 0;
+                    ++p; 
+                }
+                p = rowStart;
+                p.OffsetY(pixData, 1);
+            }
+        }
+
+        m_context = new wxMemoryDC(*m_bitmap);
+        m_context->SetDeviceOrigin(-r.x(), -r.y());
+        m_rect = r;
+#else
+        m_context = host;
+#endif
+    }
+
+    wxDC* context() { return m_context; }
+
+    ~LocalDC()
+    {
+#ifndef __WXMAC__
+        delete m_context;
+        m_host->DrawBitmap(*m_bitmap, m_rect.x(), m_rect.y());
+        delete m_bitmap;
+#endif
+    }
+
+private:
+    wxDC* m_host;
+    wxDC* m_context;
+    wxBitmap* m_bitmap;
+    IntRect m_rect;
+            
+};
+
+}
+
---
+++ Source/WebCore/platform/wx/wxcode/gdiplus/non-kerned-drawing.cpp
@@ -0,0 +1,172 @@
+/*
+ * Copyright (C) 2007 Kevin Watters, Kevin Ollivier.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+
+#include "AffineTransform.h"
+#include "GlyphBuffer.h"
+#include "GraphicsContext.h"
+#include "SimpleFontData.h"
+
+#include <wx/defs.h>
+
+#if 1 // !wxUSE_CAIRO
+
+#include <wx/dcclient.h>
+#include <wx/dcgraph.h>
+#include <wx/gdicmn.h>
+#include <vector>
+
+using namespace std;
+
+//-----------------------------------------------------------------------------
+// constants
+//-----------------------------------------------------------------------------
+
+const double RAD2DEG = 180.0 / M_PI;
+
+//-----------------------------------------------------------------------------
+// Local functions
+//-----------------------------------------------------------------------------
+
+static inline double dmin(double a, double b) { return a < b ? a : b; }
+static inline double dmax(double a, double b) { return a > b ? a : b; }
+
+static inline double DegToRad(double deg) { return (deg * M_PI) / 180.0; }
+static inline double RadToDeg(double deg) { return (deg * 180.0) / M_PI; }
+
+#include "wx/msw/private.h"
+
+// TODO remove this dependency (gdiplus needs the macros)
+
+#ifndef max
+#define max(a,b)            (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef min
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+#endif
+
+#include "gdiplus.h"
+ 
+#if wxUSE_CAIRO
+#include <cairo.h>
+#include <cairo-win32.h>
+#endif
+
+namespace WebCore {
+
+void drawTextWithSpacing(GraphicsContext* graphicsContext, const SimpleFontData* font, const wxColour& color, const GlyphBuffer& glyphBuffer, int from, int numGlyphs, const FloatPoint& point)
+{
+
+
+#if USE(WXGC)
+    wxGCDC* dc = static_cast<wxGCDC*>(graphicsContext->platformContext());
+#else
+    wxDC* dc = graphicsContext->platformContext();
+#endif
+
+    // get the native HDC handle to draw using native APIs
+    HDC hdc = 0;
+    float y = point.y() - font->fontMetrics().ascent();
+    float x = point.x();
+
+
+#if USE(WXGC)
+
+    wxGraphicsContext* gc = dc->GetGraphicsContext();
+#if wxUSE_CAIRO
+    cairo_t* context = (cairo_t*)gc->GetNativeContext();
+    if (context) {
+        cairo_surface_t* surface = cairo_get_target(context);
+        hdc = cairo_win32_surface_get_dc(surface);
+    }
+#else
+    // when going from GdiPlus -> Gdi, any GdiPlus transformations are lost
+    // so we need to alter the coordinates to reflect their transformed point.
+    double xtrans = 0;
+    double ytrans = 0;
+    
+    gc->GetTransform().TransformPoint(&xtrans, &ytrans);
+    Gdiplus::Graphics* g;
+    if (gc) {
+        g = (Gdiplus::Graphics*)gc->GetNativeContext();
+        hdc = g->GetHDC();
+    }
+    x += (int)xtrans;
+    y += (int)ytrans;
+#endif // wxUSE_CAIRO
+#else
+    hdc = static_cast<HDC>(dc->GetHDC());
+#endif
+
+    // if the context has been scaled, we must manually re-apply that scale
+    // to the HDC.
+    FloatSize scale = graphicsContext->currentScale();
+    if (scale != FloatSize(1.0, 1.0)) {
+        SetGraphicsMode(hdc, GM_ADVANCED);
+        XFORM xForm;
+        xForm.eM11 = scale.width();
+        xForm.eM12 = 0.0;
+        xForm.eM21 = 0.0;
+        xForm.eM22 = scale.height();
+        xForm.eDx = 0.0;
+        xForm.eDy = 0.0;
+        SetWorldTransform(hdc, &xForm);
+    }
+    // ExtTextOut wants the offsets as an array of ints, so extract them
+    // from the glyph buffer
+    const GlyphBufferGlyph*   glyphs   = glyphBuffer.glyphs(from);
+    const GlyphBufferAdvance* advances = glyphBuffer.advances(from);
+
+    int* spacing = new int[numGlyphs - from];
+    for (unsigned i = 0; i < numGlyphs; ++i)
+        spacing[i] = advances[i].width();
+
+    wxFont* wxfont = font->getWxFont();
+    if (wxfont && wxfont->IsOk())
+        ::SelectObject(hdc, GetHfontOf(*wxfont));
+
+    if (color.Ok())
+        ::SetTextColor(hdc, color.GetPixel());
+
+    // do not draw background behind characters
+    ::SetBkMode(hdc, TRANSPARENT);
+
+    // draw text with optional character widths array
+    wxString string = wxString((wxChar*)(&glyphs[from]), numGlyphs);
+    ::ExtTextOut(hdc, x, y,  ETO_GLYPH_INDEX, 0, reinterpret_cast<const WCHAR*>(glyphs), numGlyphs, spacing);
+
+    ::SetBkMode(hdc, TRANSPARENT);
+
+    #if USE(WXGC) && !wxUSE_CAIRO
+        g->ReleaseHDC(hdc);
+    #endif
+
+    delete [] spacing;
+}
+
+}
+#endif
