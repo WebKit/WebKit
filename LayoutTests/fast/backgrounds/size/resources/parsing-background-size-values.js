2ec4200ecf1521c85d472181490f7d02c6a33d5b
David Hyatt <hyatt@apple.com>
2011-09-06 20:41:58 +0000

    Update some layout test results following the improved Pair parsing code that landed in r94593.
    * fast/backgrounds/size/parsing-background-size-values-expected.txt:
    * fast/backgrounds/size/resources/parsing-background-size-values.js:
    * fast/reflections/reflection-computed-style-expected.txt:
    * inspector/elements/elements-panel-styles-expected.txt:
    
    
    
    Canonical link: https://commits.webkit.org/83500@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@94595 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/backgrounds/size/resources/parsing-background-size-values.js
+++ LayoutTests/fast/backgrounds/size/resources/parsing-background-size-values.js
@@ -13,23 +13,23 @@ function test(value)
 
 shouldBe('test("background-size: contain;")', '"contain"');
 shouldBe('test("background-size: cover;")', '"cover"');
-shouldBe('test("background-size: 100 100;")', '"100px 100px"');
-shouldBe('test("background-size: 100px 100px;")', '"100px 100px"');
+shouldBe('test("background-size: 100 100;")', '"100px"');
+shouldBe('test("background-size: 100px 100px;")', '"100px"');
 shouldBe('test("background-size: auto 50px;")', '" 50px"');
 shouldBe('test("background-size: 50px auto;")', '"50px "');
 shouldBe('test("background-size: auto auto;")', '" "');
 shouldBe('test("background-size: 30% 20%;")', '"30% 20%"');
 shouldBe('test("background-size: 4em auto;")', '"4em "');
 shouldBe('test("background-size: 5em;")', '"5em "');
-shouldBe('test("-webkit-background-size: 5em ;")', '"5em 5em"');
+shouldBe('test("-webkit-background-size: 5em ;")', '"5em"');
 
 shouldBe('test("background-size: 100 100 100;")', 'null');
 shouldBe('test("background-size: coconut;")', 'null');
 
 shouldBe('test("background-size: 100px,;")', 'null');
 shouldBe('test("background-size: 100px, 50%;")', '"100px , 50% "');
-shouldBe('test("-webkit-background-size: 100px, 50%;")', '"100px 100px, 50% 50%"');
+shouldBe('test("-webkit-background-size: 100px, 50%;")', '"100px, 50%"');
 shouldBe('test("background-size: 50% 100px, 2em, 100px 50%;")', '"50% 100px, 2em , 100px 50%"');
-shouldBe('test("-webkit-background-size: 50% 100px, 2em, 100px 50%;")', '"50% 100px, 2em 2em, 100px 50%"');
+shouldBe('test("-webkit-background-size: 50% 100px, 2em, 100px 50%;")', '"50% 100px, 2em, 100px 50%"');
 
 var successfullyParsed = true;
