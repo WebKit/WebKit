1b7b7c2ac24815d67cfbba5d97cc614632bc4c48
Eric Seidel <eric@webkit.org>
2011-09-12 20:06:40 +0000

    Reshuffle some code in WebKitDriver._read_block in preparation for reading stderr/stdout separately https://bugs.webkit.org/show_bug.cgi?id=67530
    Reviewed by Adam Barth.
    
    I believe I've fixed the bug in the original patch which prompted the rollout.
    The previous patch was using the wrong deadline for the initial read,
    subtracting time.time() twice from the deadline value.
    
    * Scripts/webkitpy/layout_tests/port/webkit.py:
    * Scripts/webkitpy/layout_tests/port/webkit_unittest.py:
    
    Canonical link: https://commits.webkit.org/83858@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@94973 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/port/webkit_unittest.py
+++ Tools/Scripts/webkitpy/layout_tests/port/webkit_unittest.py
@@ -31,7 +31,7 @@ from webkitpy.common.system.filesystem_mock import MockFileSystem
 from webkitpy.common.system.outputcapture import OutputCapture
 
 from webkitpy.layout_tests.models.test_configuration import TestConfiguration
-from webkitpy.layout_tests.port.webkit import WebKitPort
+from webkitpy.layout_tests.port.webkit import WebKitPort, WebKitDriver
 from webkitpy.layout_tests.port import port_testcase
 
 from webkitpy.tool.mocktool import MockExecutive, MockOptions, MockUser
@@ -197,3 +197,29 @@ BUG_SKIPPED SKIP : media = FAIL""")
         # Mock out _apache_config_file_name_for_platform to ignore the passed sys.platform value.
         port._apache_config_file_name_for_platform = lambda platform: 'httpd.conf'
         self.assertEquals(port._path_to_apache_config_file(), '/mock-checkout/LayoutTests/http/conf/httpd.conf')
+
+
+class MockServerProcess(object):
+    def __init__(self, lines=None):
+        self.timed_out = False
+        self.crashed = False
+        self.lines = lines or []
+
+    def read_line(self, timeout):
+        return self.lines.pop(0)
+
+
+class WebKitDriverTest(unittest.TestCase):
+    def test_read_block(self):
+        port = TestWebKitPort()
+        driver = WebKitDriver(port, 0)
+        driver._server_process = MockServerProcess([
+            'ActualHash: foobar',
+            'Content-Type: my_type',
+            'Content-Transfer-Encoding: none',
+            "#EOF",
+        ])
+        content_block = driver._read_block(0)
+        self.assertEquals(content_block.content_type, 'my_type')
+        self.assertEquals(content_block.encoding, 'none')
+        self.assertEquals(content_block.content_hash, 'foobar')
