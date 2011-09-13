c7aa7cd5d6b4541375821ac08d6f0a40516bd43e
Raphael Kubo da Costa <kubo@profusion.mobi>
2011-09-13 17:54:15 +0000

    [EFL] Do not always return the cached frame name. https://bugs.webkit.org/show_bug.cgi?id=66856
    Patch by Raphael Kubo da Costa <kubo@profusion.mobi> on 2011-09-13
    Reviewed by Antonio Gomes.
    
    When a frame has its parent changed (via adoptNode and appendChild,
    for example), the frame name will change, so we need to account for
    that case and change the cached name when needed.
    
    This should make fast/frames/iframe-reparenting-unique-name.html pass.
    
    * ewk/ewk_frame.cpp:
    (ewk_frame_name_get):
    
    Canonical link: https://commits.webkit.org/83917@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@95033 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/efl/ewk/ewk_frame.cpp
+++ Source/WebKit/efl/ewk/ewk_frame.cpp
@@ -349,17 +349,18 @@ const char *ewk_frame_name_get(const Evas_Object *o)
 {
     EWK_FRAME_SD_GET_OR_RETURN(o, sd, 0);
 
-    if (sd->name)
-        return sd->name;
-
     if (!sd->frame) {
         ERR("could not get name of uninitialized frame.");
         return 0;
     }
 
-    WTF::String s = sd->frame->tree()->uniqueName();
-    WTF::CString cs = s.utf8();
-    sd->name = eina_stringshare_add_length(cs.data(), cs.length());
+    const WTF::String frameName = sd->frame->tree()->uniqueName();
+
+    if ((sd->name) && (sd->name == frameName))
+        return sd->name;
+
+    eina_stringshare_replace_length(&(sd->name), frameName.utf8().data(), frameName.length());
+
     return sd->name;
 }
 
