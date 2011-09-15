0910e029befc32a9a69247bb0a076083cf45d7d2
Tony Chang <tony@chromium.org>
2011-09-15 19:46:39 +0000

    Merge r91386.
    Canonical link: https://commits.webkit.org/76367.384@safari-534.51-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-534.51-branch@95217 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderBox.cpp
+++ Source/WebCore/rendering/RenderBox.cpp
@@ -3203,7 +3203,7 @@ bool RenderBox::shrinkToAvoidFloats() const
 
 bool RenderBox::avoidsFloats() const
 {
-    return isReplaced() || hasOverflowClip() || isHR() || isLegend() || isWritingModeRoot();
+    return isReplaced() || hasOverflowClip() || isHR() || isLegend() || isWritingModeRoot() || isDeprecatedFlexItem();
 }
 
 void RenderBox::addShadowOverflow()
--- Source/WebCore/rendering/RenderBox.h
+++ Source/WebCore/rendering/RenderBox.h
@@ -384,6 +384,8 @@ public:
     virtual void markForPaginationRelayoutIfNeeded() { }
 
     bool isWritingModeRoot() const { return !parent() || parent()->style()->writingMode() != style()->writingMode(); }
+
+    bool isDeprecatedFlexItem() const { return !isInline() && !isFloatingOrPositioned() && parent() && parent()->isFlexibleBox(); }
     
     virtual int lineHeight(bool firstLine, LineDirectionMode, LinePositionMode = PositionOnContainingLine) const;
     virtual int baselinePosition(FontBaseline, bool firstLine, LineDirectionMode, LinePositionMode = PositionOnContainingLine) const;
