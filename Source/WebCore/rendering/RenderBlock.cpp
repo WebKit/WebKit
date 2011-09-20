eaa2f0c255395e706151bfb6f7a1c0e29ab2e1a3
Ryosuke Niwa <rniwa@webkit.org>
2011-09-20 02:48:04 +0000

    Hit testing on margins of body and head elements doesn't recur https://bugs.webkit.org/show_bug.cgi?id=40753
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    The bug was caused by positionForPointRespectingEditingBoundaries's comparing the editability
    of head/body and html elements when hit testing was done inside margins of head and body elements.
    
    Fixed the bug by special-casing html element (any immediate child of render view with a render layer)
    since margins of head and body elements are special.
    
    Tests: editing/selection/click-on-body-margin.html
           editing/selection/click-on-head-margin.html
    
    * rendering/RenderBlock.cpp:
    (WebCore::positionForPointRespectingEditingBoundaries):
    
    LayoutTests:
    
    Add tests to click on margins of head and body elements. WebKit should not
    (attempt to) place the caret after or before head and body elements.
    
    * editing/selection/click-on-body-margin-expected.txt: Added.
    * editing/selection/click-on-body-margin.html: Added.
    * editing/selection/click-on-head-margin-expected.txt: Added.
    * editing/selection/click-on-head-margin.html: Added.
    
    
    Canonical link: https://commits.webkit.org/84356@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@95509 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderBlock.cpp
+++ Source/WebCore/rendering/RenderBlock.cpp
@@ -4232,7 +4232,7 @@ static VisiblePosition positionForPointRespectingEditingBoundaries(RenderBlock* 
         ancestor = ancestor->parent();
 
     // If we can't find an ancestor to check editability on, or editability is unchanged, we recur like normal
-    if (!ancestor || ancestor->node()->rendererIsEditable() == childNode->rendererIsEditable())
+    if (!ancestor || (ancestor->hasLayer() && ancestor->parent()->isRenderView()) || ancestor->node()->rendererIsEditable() == childNode->rendererIsEditable())
         return child->positionForPoint(pointInChildCoordinates);
 
     // Otherwise return before or after the child, depending on if the click was to the logical left or logical right of the child
