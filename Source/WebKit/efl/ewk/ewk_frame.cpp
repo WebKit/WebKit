931001ef0fb54a83fba5607b062b8b9a55d10016
Raphael Kubo da Costa <kubo@profusion.mobi>
2011-10-10 20:48:17 +0000

    [EFL] Do not cache a frame's contents size. https://bugs.webkit.org/show_bug.cgi?id=69772
    Patch by Raphael Kubo da Costa <kubo@profusion.mobi> on 2011-10-10
    Reviewed by Antonio Gomes.
    
    As part of the process of experimenting with having scrollbars with
    an actual size, it became clear that setting the contents size via
    ChromeClient::contentsSizeChanged() and keeping the value around does
    not work.
    
    Case in point: FrameView::adjustViewSize() can result in nested
    ScrollView::updateScrollbars() calls in which the innermost ones run
    ChromeClient::contentsSizeChanged() before the outermost ones. This
    means the ewk_frame's smart data will store an old value.
    
    * ewk/ewk_frame.cpp:
    (ewk_frame_contents_size_get):
    (ewk_frame_contents_size_changed):
    
    Canonical link: https://commits.webkit.org/85822@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@97080 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/efl/ewk/ewk_frame.cpp
+++ Source/WebKit/efl/ewk/ewk_frame.cpp
@@ -72,9 +72,6 @@ struct Ewk_Frame_Smart_Data {
     const char* title;
     const char* uri;
     const char* name;
-    struct {
-        Evas_Coord w, h;
-    } contents_size;
     Eina_Bool editable : 1;
 };
 
@@ -371,10 +368,12 @@ Eina_Bool ewk_frame_contents_size_get(const Evas_Object* o, Evas_Coord* w, Evas_
     if (h)
         *h = 0;
     EWK_FRAME_SD_GET_OR_RETURN(o, sd, EINA_FALSE);
+    if (!sd->frame || !sd->frame->view())
+        return EINA_FALSE;
     if (w)
-        *w = sd->contents_size.w;
+        *w = sd->frame->view()->contentsWidth();
     if (h)
-        *h = sd->contents_size.h;
+        *h = sd->frame->view()->contentsHeight();
     return EINA_TRUE;
 }
 
@@ -1418,14 +1417,6 @@ void ewk_frame_load_progress_changed(Evas_Object* o)
  */
 void ewk_frame_contents_size_changed(Evas_Object* o, Evas_Coord w, Evas_Coord h)
 {
-    DBG("o=%p: %dx%d", o, w, h);
-    EWK_FRAME_SD_GET_OR_RETURN(o, sd);
-    if (sd->contents_size.w == w && sd->contents_size.h == h)
-        return;
-    sd->contents_size.w = w;
-    sd->contents_size.h = h;
-    // TODO: update something else internally?
-
     Evas_Coord size[2] = {w, h};
     evas_object_smart_callback_call(o, "contents,size,changed", size);
 }
