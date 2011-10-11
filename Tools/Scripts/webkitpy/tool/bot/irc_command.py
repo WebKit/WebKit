cc29cb9c3ec551050415c8ac78b452309d3b9ac4
Adam Barth <abarth@webkit.org>
2011-10-11 23:50:33 +0000

    sheriffbot takes too long to acknowledge rollout commands https://bugs.webkit.org/show_bug.cgi?id=69871
    Reviewed by Eric Seidel.
    
    We used to update the working copy before acknowledging the command
    because we wanted to ping all the relevant IRC nicks.  That's caused a
    bunch of frustration because folks don't know whether the bot has heard
    their commands.
    
    This patch makes the bot reply immediately before updating the working
    copy.  All the relevenat folks are still pinged when the bot finishes
    preparing the rollout.
    
    * Scripts/webkitpy/tool/bot/irc_command.py:
    * Scripts/webkitpy/tool/bot/sheriffircbot_unittest.py:
    
    
    Canonical link: https://commits.webkit.org/85931@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@97198 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/tool/bot/irc_command.py
+++ Tools/Scripts/webkitpy/tool/bot/irc_command.py
@@ -127,14 +127,14 @@ class Rollout(IRCCommand):
             # return is equivalent to an irc().post(), but makes for easier unit testing.
             return "%s: Usage: rollout SVN_REVISION [SVN_REVISIONS] REASON" % nick
 
+        revision_urls_string = join_with_separators([urls.view_revision_url(revision) for revision in svn_revision_list])
+        tool.irc().post("%s: Preparing rollout for %s..." % (nick, revision_urls_string))
+
         self._update_working_copy(tool)
 
         # FIXME: IRCCommand should bind to a tool and have a self._tool like Command objects do.
         # Likewise we should probably have a self._sheriff.
         nicks_string = self._nicks_string(tool, sheriff, nick, svn_revision_list)
-
-        revision_urls_string = join_with_separators([urls.view_revision_url(revision) for revision in svn_revision_list])
-        tool.irc().post("%s: Preparing rollout for %s..." % (nicks_string, revision_urls_string))
 
         try:
             complete_reason = "%s (Requested by %s on %s)." % (
