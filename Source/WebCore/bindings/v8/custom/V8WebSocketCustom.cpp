0d0c4dcc7552d02ed3fd79db9e0b350e7bb83c06
Adam Barth <abarth@webkit.org>
2011-10-14 05:12:32 +0000

    Spelling error in v8WebSocket::constructorCallback error https://bugs.webkit.org/show_bug.cgi?id=63943
    Reviewed by Kent Tamura.
    
    Source/WebCore:
    
    Fix typo.
    
    * bindings/v8/custom/V8WebSocketCustom.cpp:
    (WebCore::V8WebSocket::constructorCallback):
    
    LayoutTests:
    
    * platform/chromium/fast/dom/call-a-constructor-as-a-function-expected.txt:
    
    
    Canonical link: https://commits.webkit.org/86170@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@97441 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/custom/V8WebSocketCustom.cpp
+++ Source/WebCore/bindings/v8/custom/V8WebSocketCustom.cpp
@@ -56,7 +56,7 @@ v8::Handle<v8::Value> V8WebSocket::constructorCallback(const v8::Arguments& args
     INC_STATS("DOM.WebSocket.Constructor");
 
     if (!args.IsConstructCall())
-        return throwError("DOM object custructor cannot be called as a function.", V8Proxy::TypeError);
+        return throwError("DOM object constructor cannot be called as a function.", V8Proxy::TypeError);
     if (args.Length() == 0)
         return throwError("Not enough arguments", V8Proxy::SyntaxError);
 
