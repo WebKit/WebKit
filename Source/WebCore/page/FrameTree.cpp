WebKit/WebKit

commit 32e944ce4536694420eddf639037a84026d56d69

Add a way to print the Frame tree from the debugger.
Reviewed by Sam Weinig.

Add a showFrameTree(frame) method, callable from gdb,
which dumps the Frame tree, including the frame's view,
document, owner element and URI.

* page/FrameTree.cpp:
(printIndent):
(printFrames):
(showFrameTree):
* page/FrameTree.h:
* rendering/RenderLayer.cpp:
(WebCore::RenderLayer::updateClipRects):
* rendering/RenderLayerCompositor.cpp:
(WebCore::RenderLayerCompositor::requiresCompositingForFrame):

Canonical link: https://commits.webkit.org/86722@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@98008 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/page/FrameTree.cpp
@@ -335,3 +335,46 @@ Frame* FrameTree::top(bool checkForDisconnectedFrame) const
 }
 
 } // namespace WebCore
+
+#ifndef NDEBUG
+
+static void printIndent(int indent)
+{
+    for (int i = 0; i < indent; ++i)
+        printf("    ");
+}
+
+static void printFrames(const WebCore::Frame* frame, const WebCore::Frame* targetFrame, int indent)
+{
+    if (frame == targetFrame) {
+        printf("--> ");
+        printIndent(indent - 1);
+    } else
+        printIndent(indent);
+
+    WebCore::FrameView* view = frame->view();
+    printf("Frame %p %dx%d\n", frame, view ? view->width() : 0, view ? view->height() : 0);
+    printIndent(indent);
+    printf("  ownerElement=%p\n", frame->ownerElement());
+    printIndent(indent);
+    printf("  frameView=%p\n", view);
+    printIndent(indent);
+    printf("  document=%p\n", frame->document());
+    printIndent(indent);
+    printf("  uri=%s\n\n", frame->document()->documentURI().utf8().data());
+
+    for (WebCore::Frame* child = frame->tree()->firstChild(); child; child = child->tree()->nextSibling())
+        printFrames(child, targetFrame, indent + 1);
+}
+
+void showFrameTree(const WebCore::Frame* frame)
+{
+    if (!frame) {
+        printf("Null input frame\n");
+        return;
+    }
+
+    printFrames(frame->tree()->top(), frame, 0);
+}
+
+#endif

Source/WebCore/page/FrameTree.h
@@ -90,4 +90,9 @@ namespace WebCore {
 
 } // namespace WebCore
 
+#ifndef NDEBUG
+// Outside the WebCore namespace for ease of invocation from gdb.
+void showFrameTree(const WebCore::Frame*);
+#endif
+
 #endif // FrameTree_h

commit 07ebddd24f1334c5ef2a4b9c17a1f9c8afc2a336

Fix build breakage on some platforms after r98008.
* page/FrameTree.cpp:

Canonical link: https://commits.webkit.org/86732@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@98020 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/page/FrameTree.cpp
@@ -25,6 +25,7 @@
 #include "FrameView.h"
 #include "Page.h"
 #include "PageGroup.h"
+#include "Document.h"
 #include <stdarg.h>
 #include <wtf/StringExtras.h>
 #include <wtf/Vector.h>