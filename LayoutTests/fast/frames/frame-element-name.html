WebKit/WebKit

commit 66a94f267299e24c4e1e3f311c1b15408ac03854

JS Test Harness: Insert the stylesheet dynamically https://bugs.webkit.org/show_bug.cgi?id=70432
Reviewed by Ojan Vafai.

This injects the js-test-style.css file dynamically so that the test files can be simpler.
This patch also removes the markup from the existing files in LayoutTests/fast/

* fast/*.html: Remove <link rel=:"stylesheet" href="...js-test-style.css">
* fast/js/resources/js-test-pre.js: Inject the stylesheet dynamically.

Canonical link: https://commits.webkit.org/86596@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@97881 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/fast/frames/resources/frame-element-name.html
@@ -1,5 +1,4 @@
 <head>
-<link rel="stylesheet" href="../../js/resources/js-test-style.css">
 <script src="../../js/resources/js-test-pre.js"></script>
 </head>
 <body>

commit 00947da4e25693f96c9851ae875c996051c61522

fast/frames/frame-element-name.html fails intermittently after r97881 https://bugs.webkit.org/show_bug.cgi?id=70487
Reviewed by Zoltán Herczeg.

r97881 made js/resources/js-test-pre.js (used in fast/frames/resource/frame-element-name.html)
slower with loading css dynamically. We have to wait for loading all frames to get correct results.

* fast/frames/frame-element-name.html:

Canonical link: https://commits.webkit.org/86678@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@97963 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/fast/frames/frame-element-name.html
@@ -1,11 +1,20 @@
 <html>
 <head>
 <script>
-if (window.layoutTestController)
+if (window.layoutTestController) {
     layoutTestController.dumpChildFramesAsText();
+    layoutTestController.waitUntilDone();
+}
+
+function framesLoaded()
+{
+    if (window.layoutTestController)
+        layoutTestController.notifyDone();
+}
+
 </script>
 </head>
- <frameset border="1" cols="150,150,150,150,150,*" style='border: solid 1px;'>
+ <frameset border="1" cols="150,150,150,150,150,*" style='border: solid 1px;' onload="framesLoaded()">
   <frame marginwidth="1" src="resources/frame-element-name.html?expectedName=left" name="left">
   <frame src="resources/frame-element-name.html?expectedName=">
   <frame src="resources/frame-element-name.html?expectedName=_blank" name="_blank">