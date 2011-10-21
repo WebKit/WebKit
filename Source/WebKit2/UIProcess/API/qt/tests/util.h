8ea26bd0614552df711cfd2531b6a0a08b08c793
Csaba Osztrogonác <ossy@webkit.org>
2011-10-21 08:40:06 +0000

    [Qt][WK2] Unreviewed buildfix for newer Qt5. Remove duplicated defines.
    Source/WebKit/qt:
    
    * tests/util.h:
    (waitForSignal):
    
    Source/WebKit2:
    
    * UIProcess/API/qt/tests/util.h:
    
    Canonical link: https://commits.webkit.org/86792@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@98085 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/qt/tests/util.h
+++ Source/WebKit2/UIProcess/API/qt/tests/util.h
@@ -28,31 +28,3 @@
 
 void addQtWebProcessToPath();
 bool waitForSignal(QObject*, const char* signal, int timeout = 10000);
-
-// Will try to wait for the condition while allowing event processing
-#define QTRY_VERIFY(__expr) \
-    do { \
-        const int __step = 50; \
-        const int __timeout = 5000; \
-        if (!(__expr)) { \
-            QTest::qWait(0); \
-        } \
-        for (int __i = 0; __i < __timeout && !(__expr); __i+=__step) { \
-            QTest::qWait(__step); \
-        } \
-        QVERIFY(__expr); \
-    } while(0)
-
-// Will try to wait for the condition while allowing event processing
-#define QTRY_COMPARE(__expr, __expected) \
-    do { \
-        const int __step = 50; \
-        const int __timeout = 5000; \
-        if ((__expr) != (__expected)) { \
-            QTest::qWait(0); \
-        } \
-        for (int __i = 0; __i < __timeout && ((__expr) != (__expected)); __i+=__step) { \
-            QTest::qWait(__step); \
-        } \
-        QCOMPARE(__expr, __expected); \
-    } while(0)
