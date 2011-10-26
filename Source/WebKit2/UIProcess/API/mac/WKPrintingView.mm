4c796060116f05c9c558d9a0a2f2ac37218677c3
Lucas Forschler <lforschler@apple.com>
2011-10-26 13:03:27 +0000

    Merged r98153.
    Canonical link: https://commits.webkit.org/76367.419@safari-534.52-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-534.52-branch@98468 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/mac/WKPrintingView.mm
+++ Source/WebKit2/UIProcess/API/mac/WKPrintingView.mm
@@ -90,6 +90,9 @@ static BOOL isForcingPreviewUpdate;
     
     _autodisplayResumeTimer = nil;
     [self _setAutodisplay:YES];
+
+    // Enabling autodisplay normally implicitly calls endPrinting() via -[WKView drawRect:], but not when content is in accelerated compositing mode.
+    _webFrame->page()->endPrinting();
 }
 
 - (void)_delayedResumeAutodisplay
