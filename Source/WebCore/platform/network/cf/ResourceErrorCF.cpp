03b67a2c29ee7b0835665a638a6531b40e84a722
Lucas Forschler <lforschler@apple.com>
2011-10-27 18:13:04 +0000

    Merged r98530.
    Canonical link: https://commits.webkit.org/76367.431@safari-534.52-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-534.52-branch@98610 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/network/cf/ResourceErrorCF.cpp
+++ Source/WebCore/platform/network/cf/ResourceErrorCF.cpp
@@ -84,6 +84,8 @@ void ResourceError::platformLazyInit()
         m_domain = "NSOSStatusErrorDomain";
     else if (domain == kCFErrorDomainWinSock)
         m_domain = "kCFErrorDomainWinSock";
+    else
+        m_domain = domain;
 
     m_errorCode = CFErrorGetCode(m_platformError.get());
 
