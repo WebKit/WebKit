ca09f5eea59e3d1f903c4ea41f6e55a74344bbac
Andy Estes <aestes@apple.com>
2011-11-01 17:35:24 +0000

    Merge r98936 and update Versioning.
    Canonical link: https://commits.webkit.org/76367.438@safari-534.52-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-534.52-branch@98977 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/FrameView.cpp
+++ Source/WebCore/page/FrameView.cpp
@@ -2236,7 +2236,9 @@ bool FrameView::isOnActivePage() const
 {
     if (m_frame->view() != this)
         return false;
-    return !m_frame->document()->inPageCache();
+    if (Document* document = m_frame->document())
+        return !document->inPageCache();
+    return false;
 }
 
 bool FrameView::shouldSuspendScrollAnimations() const
