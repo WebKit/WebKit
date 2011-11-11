7c3e32b34e970e50e3eae353e629f1341d278a5a
Sadrul Habib Chowdhury <sadrul@chromium.org>
2011-11-11 18:35:20 +0000

    Speculative fix for a few worker tests. https://bugs.webkit.org/show_bug.cgi?id=72134
    Looking at the recent failures, it looks like the test starts and completes
    before the page is done loading completely (the test output is correct except
    for the HTML on top of the files). So start the tests from an onload callback so
    that the plain HTML text also gets printed correctly.
    
    Patch by Sadrul Habib Chowdhury <sadrul@chromium.org> on 2011-11-11
    Reviewed by Tony Chang.
    
    * fast/workers/resources/worker-replace-self.js:
    (startTest.worker.onmessage):
    (startTest):
    * fast/workers/worker-call.html:
    * fast/workers/worker-close.html:
    * fast/workers/worker-messageport-gc.html:
    * fast/workers/worker-replace-self.html:
    
    Canonical link: https://commits.webkit.org/88550@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@99990 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/workers/resources/worker-replace-self.js
+++ LayoutTests/fast/workers/resources/worker-replace-self.js
@@ -8,17 +8,19 @@ if (window.layoutTestController) {
     layoutTestController.waitUntilDone();
 }
 
-var worker = createWorker();
-worker.postMessage("eval self='PASS'");
-worker.postMessage("eval self");
-worker.postMessage("eval foo//bar");
+function startTest() {
+    var worker = createWorker();
+    worker.postMessage("eval self='PASS'");
+    worker.postMessage("eval self");
+    worker.postMessage("eval foo//bar");
 
-worker.onmessage = function(evt) {
-    if (!/foo\/\/bar/.test(evt.data))
-        log(evt.data.replace(new RegExp("/.*LayoutTests"), "<...>"));
-    else {
-        log("DONE");
-        if (window.layoutTestController)
-            layoutTestController.notifyDone();
+    worker.onmessage = function(evt) {
+        if (!/foo\/\/bar/.test(evt.data))
+            log(evt.data.replace(new RegExp("/.*LayoutTests"), "<...>"));
+        else {
+            log("DONE");
+            if (window.layoutTestController)
+                layoutTestController.notifyDone();
+        }
     }
 }
