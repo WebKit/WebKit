17f9c4cf497b064f96b1d2661775e1ffa445b0aa
Patrick Gansterer <paroga@webkit.org>
2011-11-12 17:30:48 +0000

    [CMake] Move list of DFG source files into correct file https://bugs.webkit.org/show_bug.cgi?id=72212
    Reviewed by Daniel Bates.
    
    The DFG files are platform independent. So move them from
    the EFL specific file into the general CMakeLists.txt.
    
    * CMakeLists.txt:
    * PlatformEfl.cmake:
    
    Canonical link: https://commits.webkit.org/88638@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@100081 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/PlatformEfl.cmake
+++ Source/JavaScriptCore/PlatformEfl.cmake
@@ -16,33 +16,3 @@ ENDIF ()
 LIST(APPEND JavaScriptCore_LINK_FLAGS
     ${ECORE_LDFLAGS}
 )
-
-LIST(APPEND JavaScriptCore_INCLUDE_DIRECTORIES
-    ${JAVASCRIPTCORE_DIR}/dfg
-)
-
-LIST(APPEND JavaScriptCore_SOURCES
-    dfg/DFGAbstractState.cpp
-    dfg/DFGAssemblyHelpers.cpp
-    dfg/DFGByteCodeParser.cpp
-    dfg/DFGCapabilities.cpp
-    dfg/DFGCorrectableJumpPoint.cpp
-    dfg/DFGDriver.cpp
-    dfg/DFGGraph.cpp
-    dfg/DFGJITCodeGenerator.cpp
-    dfg/DFGJITCodeGenerator64.cpp
-    dfg/DFGJITCodeGenerator32_64.cpp
-    dfg/DFGJITCompiler.cpp
-    dfg/DFGOperations.cpp
-    dfg/DFGOSREntry.cpp
-    dfg/DFGOSRExit.cpp
-    dfg/DFGOSRExitCompiler.cpp
-    dfg/DFGOSRExitCompiler64.cpp
-    dfg/DFGOSRExitCompiler32_64.cpp
-    dfg/DFGPropagator.cpp
-    dfg/DFGRepatch.cpp
-    dfg/DFGSpeculativeJIT.cpp
-    dfg/DFGSpeculativeJIT64.cpp
-    dfg/DFGSpeculativeJIT32_64.cpp
-    dfg/DFGThunks.cpp
-)
