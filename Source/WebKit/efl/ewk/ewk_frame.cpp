b3944fe2fd07663b7846909f482b26562714f4c2
Grzegorz Czajkowski <g.czajkowski@samsung.com>
2011-11-15 11:03:40 +0000

    [EFL] Use EINA_SAFETY_ON_NULL_RETURN_VAL for NULL checks instead of EINA_SAFETY_ON_FALSE_RETURN_VAL. https://bugs.webkit.org/show_bug.cgi?id=70091
    Patch by Grzegorz Czajkowski <g.czajkowski@samsung.com> on 2011-11-15
    Reviewed by Filip Pizlo.
    
    Replaces EINA_SAFETY_ON_FALSE_RETURN_VAL by EINA_SAFETY_ON_NULL_RETURN_VAL for NULL checks in compliance
    with WebKit-EFL functions.
    
    * ewk/ewk_frame.cpp:
    (ewk_frame_contents_set):
    (ewk_frame_contents_alternate_set):
    
    Canonical link: https://commits.webkit.org/88809@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@100261 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/efl/ewk/ewk_frame.cpp
+++ Source/WebKit/efl/ewk/ewk_frame.cpp
@@ -399,7 +399,7 @@ static Eina_Bool _ewk_frame_contents_set_internal(Ewk_Frame_Smart_Data* smartDat
 Eina_Bool ewk_frame_contents_set(Evas_Object* ewkFrame, const char* contents, size_t contentsSize, const char* mimeType, const char* encoding, const char* baseUri)
 {
     EWK_FRAME_SD_GET_OR_RETURN(ewkFrame, smartData, false);
-    EINA_SAFETY_ON_FALSE_RETURN_VAL(smartData->frame, false);
+    EINA_SAFETY_ON_NULL_RETURN_VAL(smartData->frame, false);
     EINA_SAFETY_ON_NULL_RETURN_VAL(contents, false);
     return _ewk_frame_contents_set_internal
                (smartData, contents, contentsSize, mimeType, encoding, baseUri, 0);
@@ -408,7 +408,7 @@ Eina_Bool ewk_frame_contents_set(Evas_Object* ewkFrame, const char* contents, si
 Eina_Bool ewk_frame_contents_alternate_set(Evas_Object* ewkFrame, const char* contents, size_t contentsSize, const char* mimeType, const char* encoding, const char* baseUri, const char* unreachableUri)
 {
     EWK_FRAME_SD_GET_OR_RETURN(ewkFrame, smartData, false);
-    EINA_SAFETY_ON_FALSE_RETURN_VAL(smartData->frame, false);
+    EINA_SAFETY_ON_NULL_RETURN_VAL(smartData->frame, false);
     EINA_SAFETY_ON_NULL_RETURN_VAL(contents, false);
     EINA_SAFETY_ON_NULL_RETURN_VAL(unreachableUri, false);
     return _ewk_frame_contents_set_internal
