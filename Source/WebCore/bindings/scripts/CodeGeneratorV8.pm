2c2893a6f0b01bc7e6024c73b2200485f161090a
Oliver Hunt <oliver@apple.com>
2011-11-15 01:58:46 +0000

    Fix V8 build
    Canonical link: https://commits.webkit.org/88776@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@100228 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -2704,7 +2704,7 @@ END
         push(@implContent, <<END);
     }
     namespace WTF {
-    void ${nativeType}::neuterBinding(ScriptExecutionContext*) {
+    void ${nativeType}::neuterBinding(WebCore::ScriptExecutionContext*) {
         v8::Handle<v8::Value> bound = toV8(this);
         v8::Handle<v8::Object> object(bound.As<v8::Object>());
         object->SetIndexedPropertiesToExternalArrayData(0, v8::kExternalByteArray, 0);
