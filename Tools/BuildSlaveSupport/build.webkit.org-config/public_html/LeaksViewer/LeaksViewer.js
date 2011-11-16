c97f3d29b11eca02294cd72f4e2326d01f7d4903
Adam Roben <aroben@apple.com>
2011-11-16 16:50:07 +0000

    Make Leaks Viewer load builds from the new Lion Leaks bot
    Fixes <http://webkit.org/b/72507> REGRESSION: Leaks Viewer no longer shows recent leaky builds
    
    Reviewed by Andreas Kling.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/LeaksViewer/LeaksViewer.js:
    (LeaksViewer._displayURLPrompt): Use the Lion Leaks bot instead of the nonexistent
    SnowLeopard one.
    
    Canonical link: https://commits.webkit.org/88987@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@100456 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/BuildSlaveSupport/build.webkit.org-config/public_html/LeaksViewer/LeaksViewer.js
+++ Tools/BuildSlaveSupport/build.webkit.org-config/public_html/LeaksViewer/LeaksViewer.js
@@ -120,7 +120,7 @@ var LeaksViewer = {
         document.getElementById("url-prompt-container").removeStyleClass("hidden");
         document.getElementById("url").focus();
         var loader = new RecentBuildsLoader(this._didLoadRecentBuilds.bind(this));
-        loader.start("SnowLeopard Intel Leaks", this._numberOfRecentBuildsToLoad);
+        loader.start("Lion Intel Leaks", this._numberOfRecentBuildsToLoad);
     },
 
     _loadLeaksFromURL: function(url) {
