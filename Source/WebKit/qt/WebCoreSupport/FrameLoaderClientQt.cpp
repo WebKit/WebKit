e0c1d06c5dde6938b0a5e40b9182fbd6108ac10b
Jesus Sanchez-Palencia <jesus.palencia@openbossa.org>
2011-11-16 15:12:30 +0000

    [Qt] FrameLoaderClientQt::canShowMIMEType should always return true https://bugs.webkit.org/show_bug.cgi?id=72169
    Patch by Jesus Sanchez-Palencia <jesus.palencia@openbossa.org> on 2011-11-16
    Reviewed by Simon Hausmann.
    
    Currently FrameLoaderClientQt::canShowMIMEType is called after
    dispatchDecidePolicyForResponse. If decidePolicyForResponse returns
    PolicyUse and canShowMIMEType returns false, then we have a bug.
    Therefore, FrameLoaderClientQt::canShowMIMEType now will always
    return true, following the implemenation of WebKit2.
    
    * WebCoreSupport/FrameLoaderClientQt.cpp:
    (WebCore::FrameLoaderClientQt::canShowMIMEType):
    
    Canonical link: https://commits.webkit.org/88979@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@100448 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
+++ Source/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
@@ -650,25 +650,10 @@ bool FrameLoaderClientQt::canShowMIMETypeAsHTML(const String& MIMEType) const
     notImplemented();
     return false;
 }
-    
+
 bool FrameLoaderClientQt::canShowMIMEType(const String& MIMEType) const
 {
-    String type = MIMEType;
-    type.makeLower();
-    if (MIMETypeRegistry::isSupportedImageMIMEType(type))
-        return true;
-
-    if (MIMETypeRegistry::isSupportedNonImageMIMEType(type))
-        return true;
-
-    if (MIMETypeRegistry::isSupportedMediaMIMEType(type))
-        return true;
-
-    if (m_frame && m_frame->settings()  && m_frame->settings()->arePluginsEnabled()
-        && PluginDatabase::installedPlugins()->isMIMETypeRegistered(type))
-        return true;
-
-    return false;
+    return true;
 }
 
 bool FrameLoaderClientQt::representationExistsForURLScheme(const String&) const
