225c3dead9265747ab683875b5297d71cd5fa0a8
Steve Falkenburg <sfalken@apple.com>
2011-12-02 17:04:45 +0000

    Changes to localStorageDatabasePath don't have any effect on Windows https://bugs.webkit.org/show_bug.cgi?id=73606 <rdar://problem/10491296>
    Reviewed by Darin Adler.
    
    * WebView.cpp:
    (WebView::notifyPreferencesChanged): Propagate local storage path change to m_page's settings.
    
    
    
    Canonical link: https://commits.webkit.org/90279@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@101808 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/win/WebView.cpp
+++ Source/WebKit/win/WebView.cpp
@@ -4584,6 +4584,15 @@ HRESULT WebView::notifyPreferencesChanged(IWebNotification* notification)
     settings->setFixedFontFamily(AtomicString(str, SysStringLen(str)));
     SysFreeString(str);
 
+    COMPtr<IWebPreferencesPrivate> prefsPrivate(Query, preferences);
+    if (prefsPrivate) {
+        hr = prefsPrivate->localStorageDatabasePath(&str);
+        if (FAILED(hr))
+            return hr;
+        settings->setLocalStorageDatabasePath(String(str, SysStringLen(str)));
+        SysFreeString(str);
+    }
+
     hr = preferences->pictographFontFamily(&str);
     if (FAILED(hr))
         return hr;
@@ -4737,7 +4746,6 @@ HRESULT WebView::notifyPreferencesChanged(IWebNotification* notification)
     settings->setAVFoundationEnabled(enabled);
 #endif
 
-    COMPtr<IWebPreferencesPrivate> prefsPrivate(Query, preferences);
     if (prefsPrivate) {
         hr = prefsPrivate->authorAndUserStylesEnabled(&enabled);
         if (FAILED(hr))
