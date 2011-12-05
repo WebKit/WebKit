4d167ae34d1182dd0f60eda483e88b969519a599
Raphael Kubo da Costa <kubo@profusion.mobi>
2011-12-05 21:22:10 +0000

    Add myself as a committer.
    Canonical link: https://commits.webkit.org/90491@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@102036 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/config/committers.py
+++ Tools/Scripts/webkitpy/common/config/committers.py
@@ -303,6 +303,7 @@ committers_unable_to_review = [
     Committer("Pratik Solanki", "psolanki@apple.com", "psolanki"),
     Committer("Qi Zhang", ["qi.2.zhang@nokia.com", "qi.zhang02180@gmail.com"], "qi"),
     Committer("Rafael Antognolli", "antognolli@profusion.mobi", "antognolli"),
+    Committer("Raphael Kubo da Costa", ["kubo@profusion.mobi", "rakuco@FreeBSD.org"], "rakuco"),
     Committer("Ravi Kasibhatla", "ravi.kasibhatla@motorola.com", "kphanee"),
     Committer("Renata Hodovan", "reni@webkit.org", "reni"),
     Committer("Robert Hogan", ["robert@webkit.org", "robert@roberthogan.net", "lists@roberthogan.net"], "mwenge"),
