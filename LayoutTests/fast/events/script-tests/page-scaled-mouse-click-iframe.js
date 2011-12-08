991aa6b66a417dc25d3c9e73e006c70273ec340b
Fady Samuel <fsamuel@chromium.org>
2011-12-08 09:18:01 +0000

    Move scalePageBy from eventSender to window.internals https://bugs.webkit.org/show_bug.cgi?id=64512
    Reviewed by Simon Fraser.
    
    .:
    
    * Source/autotools/symbols.filter:
    
    Source/WebCore:
    
    Added setPageScaleFactor to window.internals.
    Renamed window.internals.getPageScaleFactor to window.internals.pageScaleFactor
    to match the webkit style.
    
    * testing/Internals.cpp:
    (WebCore::Internals::pageScaleFactor):
    (WebCore::Internals::setPageScaleFactor):
    * testing/Internals.h:
    * testing/Internals.idl:
    
    Source/WebKit2:
    
    * win/WebKit2.def:
    * win/WebKit2CFLite.def:
    
    LayoutTests:
    
    Updated layout tests to use window.internals.setPageScaleFactor.
    
    * compositing/geometry/fixed-position-composited-page-scale-down.html:
    * compositing/geometry/fixed-position-composited-page-scale.html:
    * compositing/geometry/fixed-position-iframe-composited-page-scale-down.html:
    * compositing/geometry/fixed-position-iframe-composited-page-scale.html:
    * compositing/geometry/fixed-position-transform-composited-page-scale-down.html:
    * compositing/geometry/fixed-position-transform-composited-page-scale.html:
    * compositing/scaling/tiled-layer-recursion.html:
    * fast/dom/Element/scale-page-bounding-client-rect.html:
    * fast/dom/Element/scale-page-client-rects.html:
    * fast/dom/Range/scale-page-bounding-client-rect.html:
    * fast/dom/Range/scale-page-client-rects.html:
    * fast/events/scale-and-scroll-body.html:
    * fast/events/scale-and-scroll-iframe-body.html:
    * fast/events/scale-and-scroll-iframe-window.html:
    * fast/events/scale-and-scroll-window.html:
    * fast/events/script-tests/page-scaled-mouse-click-iframe.js:
    (iframeLoaded):
    * fast/events/script-tests/page-scaled-mouse-click.js:
    * fast/events/scroll-in-scaled-page-with-overflow-hidden.html:
    * fast/events/touch/page-scaled-touch-gesture-click.html:
    * fast/frames/frame-set-rotation-hit.html:
    * fast/frames/frame-set-scaling-hit.html:
    * fast/frames/iframe-double-scale-contents.html:
    * fast/repaint/background-scaling.html:
    * fast/repaint/scale-page-shrink.html:
    * fast/transforms/selection-bounds-in-transformed-view.html:
    
    
    Canonical link: https://commits.webkit.org/90738@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@102323 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/events/script-tests/page-scaled-mouse-click-iframe.js
+++ LayoutTests/fast/events/script-tests/page-scaled-mouse-click-iframe.js
@@ -52,7 +52,7 @@ function iframeLoaded() {
   div.addEventListener("click", appendEventLog, false);
   iframe.contentWindow.document.body.insertBefore(div, iframe.contentWindow.document.body.firstChild);
 
-  if (window.eventSender) {
+  if (window.eventSender && window.internals) {
     eventSender.mouseMoveTo(10, 10);
     // We are clicking in the same position on screen. As we scale or transform the page,
     // we expect the pageX and pageY event coordinates to change because different
@@ -60,15 +60,15 @@ function iframeLoaded() {
 
     testEvents(0, "Unscaled", "click(10, 10)");
 
-    eventSender.scalePageBy(0.5, 0, 0);
+    window.internals.setPageScaleFactor(document, 0.5, 0, 0);
     testEvents(0, "setPageScale(0.5)", "click(20, 20)");
 
-    eventSender.scalePageBy(1.0, 0, 0);
+    window.internals.setPageScaleFactor(document, 1.0, 0, 0);
     html.style["-webkit-transform"] = "scale(0.5, 2.0)";
     html.style["-webkit-transform-origin"] = "0 0";
     testEvents(0, "CSS scale(0.5, 2.0)", "click(20, 5)");
 
-    eventSender.scalePageBy(0.5, 0, 0);
+    window.internals.setPageScaleFactor(document, 0.5, 0, 0);
     testEvents(0, "setPageScale(0.5), CSS scale(0.5, 2.0)", "click(40, 10)");
   }
 
