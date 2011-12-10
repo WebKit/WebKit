9cc9011334999b328ad40d1f9250bac7c5441783
Konrad Piascik <kpiascik@rim.com>
2011-12-10 01:57:24 +0000

    Remove unnecessary file DissasemblerARM.cpp from build system https://bugs.webkit.org/show_bug.cgi?id=74184
    Patch by Konrad Piascik <kpiascik@rim.com> on 2011-12-09
    Reviewed by Daniel Bates.
    
    * PlatformBlackBerry.cmake:
    
    Canonical link: https://commits.webkit.org/90906@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@102505 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/PlatformBlackBerry.cmake
+++ Source/JavaScriptCore/PlatformBlackBerry.cmake
@@ -2,8 +2,4 @@ LIST(INSERT JavaScriptCore_INCLUDE_DIRECTORIES 0
     "${BLACKBERRY_THIRD_PARTY_DIR}/icu"
 )
 
-LIST(APPEND JavaScriptCore_SOURCES
-    DisassemblerARM.cpp
-)
-
 INSTALL(FILES "wtf/Forward.h" DESTINATION usr/include/browser/webkit/wtf)
