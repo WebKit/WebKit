07fd72db29d626452ee6b9b18af0e429dc6038d7
Mary Wu <mawu@blackberry.com>
2011-12-12 16:47:49 +0000

    Upstream 3 files into WebCore/platform/blackberry https://bugs.webkit.org/show_bug.cgi?id=74275
    Patch by Mary Wu <mary.wu@torchmobile.com.cn> on 2011-12-12
    Reviewed by Rob Buis.
    
    Initial upstream, no new tests.
    
    * platform/blackberry/ContextMenuBlackBerry.cpp: Added.
    * platform/blackberry/ContextMenuItemBlackBerry.cpp: Added.
    * platform/blackberry/TemporaryLinkStubs.cpp: Added.
    
    Canonical link: https://commits.webkit.org/90988@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@102590 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebCore/platform/blackberry/ContextMenuItemBlackBerry.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2009 Research In Motion Limited. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "config.h"
+#include "ContextMenuItem.h"
+
+#include "ContextMenu.h"
+#include "NotImplemented.h"
+
+namespace WebCore {
+
+ContextMenuItem::ContextMenuItem(ContextMenuItemType, ContextMenuAction, String const&, ContextMenu*)
+{
+    notImplemented();
+}
+
+ContextMenuItem::~ContextMenuItem()
+{
+    notImplemented();
+}
+
+void ContextMenuItem::setChecked(bool)
+{
+    notImplemented();
+}
+
+void ContextMenuItem::setEnabled(bool)
+{
+    notImplemented();
+}
+
+void ContextMenuItem::setSubMenu(ContextMenu*)
+{
+    notImplemented();
+}
+
+void ContextMenuItem::setTitle(String const&)
+{
+    notImplemented();
+}
+
+ContextMenuAction ContextMenuItem::action() const
+{
+    notImplemented();
+    return ContextMenuItemTagNoAction;
+}
+
+ContextMenuItemType ContextMenuItem::type() const
+{
+    notImplemented();
+    return ActionType;
+}
+
+String ContextMenuItem::title() const
+{
+    notImplemented();
+    return String();
+}
+
+} // namespace WebCore
