2c58c0a161b7d65d5b4f1d91dba54e6e7ac52fe9
Kenneth Russell <kbr@google.com>
2011-12-13 03:13:40 +0000

    Unreviewed, rolling out r102648. http://trac.webkit.org/changeset/102648 https://bugs.webkit.org/show_bug.cgi?id=74313
    Broke the Snow Leopard and Windows builds
    
    * bindings/js/ScriptEventListener.cpp:
    (WebCore::eventListenerHandlerLocation):
    
    
    Canonical link: https://commits.webkit.org/91045@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@102653 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/js/ScriptEventListener.cpp
+++ Source/WebCore/bindings/js/ScriptEventListener.cpp
@@ -34,8 +34,6 @@
 #include "Attribute.h"
 #include "Document.h"
 #include "EventListener.h"
-#include "Executable.h"
-#include "JSFunction.h"
 #include "JSNode.h"
 #include "Frame.h"
 #include <runtime/JSLock.h>
@@ -105,23 +103,10 @@ String eventListenerHandlerBody(Document* document, EventListener* eventListener
     return ustringToString(jsFunction->toString(scriptStateFromNode(jsListener->isolatedWorld(), document)));
 }
 
-bool eventListenerHandlerLocation(Document* document, EventListener* eventListener, String& sourceName, int& lineNumber)
+bool eventListenerHandlerLocation(Document*, EventListener*, String&, int&)
 {
-    const JSEventListener* jsListener = JSEventListener::cast(eventListener);
-    if (!jsListener)
-        return false;
-    JSC::JSObject* jsObject = jsListener->jsFunction(document);
-    if (!jsObject)
-        return false;
-    JSC::JSFunction* jsFunction = static_cast<JSFunction*>(jsObject);
-    if (!jsFunction || jsFunction->isHostFunction())
-        return false;
-    JSC::FunctionExecutable* funcExecutable = jsFunction->jsExecutable();
-    if (!funcExecutable)
-        return false;
-    lineNumber = funcExecutable->lineNo();
-    sourceName = ustringToString(funcExecutable->sourceURL());
-    return true;
+    // FIXME: Add support for getting function location.
+    return false;
 }
 
 } // namespace WebCore
