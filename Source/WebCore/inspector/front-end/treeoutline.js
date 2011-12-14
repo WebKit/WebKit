28b8b595edab337f82c3d3813f8b7eca4a94daf8
Vsevolod Vlasov <vsevik@chromium.org>
2011-12-14 10:06:20 +0000

    Web Inspector: TreeOutline's is broken when li elements have padding-left different from 14px. https://bugs.webkit.org/show_bug.cgi?id=74445
    Reviewed by Pavel Feldman.
    
    * inspector/front-end/treeoutline.js:
    (TreeElement.prototype.isEventWithinDisclosureTriangle):
    
    Canonical link: https://commits.webkit.org/91143@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@102757 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/treeoutline.js
+++ Source/WebCore/inspector/front-end/treeoutline.js
@@ -951,7 +951,8 @@ TreeElement.prototype.traversePreviousTreeElement = function(skipUnrevealed, don
 
 TreeElement.prototype.isEventWithinDisclosureTriangle = function(event)
 {
-    const leftPadding = 14;
-    var left = this._listItemNode.totalOffsetLeft() + leftPadding;
+    // FIXME: We should not use getComputedStyle(). For that we need to get rid of using ::before for disclosure triangle. (http://webk.it/74446) 
+    var computedLeftPadding = window.getComputedStyle(this._listItemNode).getPropertyCSSValue("padding-left").getFloatValue(CSSPrimitiveValue.CSS_PX);
+    var left = this._listItemNode.totalOffsetLeft() + computedLeftPadding;
     return event.pageX >= left && event.pageX <= left + this.arrowToggleWidth && this.hasChildren;
 }
