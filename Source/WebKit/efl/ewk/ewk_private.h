aa1087aa953cc584ea030367ca167ebef2e42289
Gyuyoung Kim <gyuyoung.kim@samsung.com>
2011-12-15 06:36:25 +0000

    [EFL] Change efl style variable name with WebKit style for internal functions. https://bugs.webkit.org/show_bug.cgi?id=74474
    Reviewed by Ryosuke Niwa.
    
    In order to be more compliant with WebKit coding style, variable names of internal functions
    should be changed with WebKit coding style.
    
    * ewk/ewk_private.h:
    * ewk/ewk_view.cpp:
    (ewk_view_scroll):
    
    
    Canonical link: https://commits.webkit.org/91262@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@102892 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/efl/ewk/ewk_private.h
+++ Source/WebKit/efl/ewk/ewk_private.h
@@ -84,72 +84,72 @@ Evas_Object* kitFrame(const WebCore::Frame* coreFrame);
 
 } // namespace EWKPrivate
 
-void ewk_view_ready(Evas_Object* o);
-void ewk_view_input_method_state_set(Evas_Object* o, bool active);
-void ewk_view_title_set(Evas_Object* o, const char* title);
-void ewk_view_uri_changed(Evas_Object* o);
-void ewk_view_load_document_finished(Evas_Object* o, Evas_Object* frame);
-void ewk_view_load_started(Evas_Object* o);
-void ewk_view_load_provisional(Evas_Object* o);
-void ewk_view_frame_main_load_started(Evas_Object* o);
-void ewk_view_frame_main_cleared(Evas_Object* o);
-void ewk_view_frame_main_icon_received(Evas_Object* o);
-void ewk_view_load_finished(Evas_Object* o, const Ewk_Frame_Load_Error* error);
-void ewk_view_load_error(Evas_Object* o, const Ewk_Frame_Load_Error* error);
-void ewk_view_load_progress_changed(Evas_Object* o);
-void ewk_view_load_show(Evas_Object* o);
-void ewk_view_restore_state(Evas_Object* o, Evas_Object* frame);
-Evas_Object* ewk_view_window_create(Evas_Object* o, bool javascript, const WebCore::WindowFeatures* coreFeatures);
-void ewk_view_window_close(Evas_Object* o);
+void ewk_view_ready(Evas_Object* ewkView);
+void ewk_view_input_method_state_set(Evas_Object* ewkView, bool active);
+void ewk_view_title_set(Evas_Object* ewkView, const char* title);
+void ewk_view_uri_changed(Evas_Object* ewkView);
+void ewk_view_load_document_finished(Evas_Object* ewkView, Evas_Object* frame);
+void ewk_view_load_started(Evas_Object* ewkView);
+void ewk_view_load_provisional(Evas_Object* ewkView);
+void ewk_view_frame_main_load_started(Evas_Object* ewkView);
+void ewk_view_frame_main_cleared(Evas_Object* ewkView);
+void ewk_view_frame_main_icon_received(Evas_Object* ewkView);
+void ewk_view_load_finished(Evas_Object* ewkView, const Ewk_Frame_Load_Error* error);
+void ewk_view_load_error(Evas_Object* ewkView, const Ewk_Frame_Load_Error* error);
+void ewk_view_load_progress_changed(Evas_Object* ewkView);
+void ewk_view_load_show(Evas_Object* ewkView);
+void ewk_view_restore_state(Evas_Object* ewkView, Evas_Object* frame);
+Evas_Object* ewk_view_window_create(Evas_Object* ewkView, bool javascript, const WebCore::WindowFeatures* coreFeatures);
+void ewk_view_window_close(Evas_Object* ewkView);
 
-void ewk_view_mouse_link_hover_in(Evas_Object* o, void* data);
-void ewk_view_mouse_link_hover_out(Evas_Object* o);
+void ewk_view_mouse_link_hover_in(Evas_Object* ewkView, void* data);
+void ewk_view_mouse_link_hover_out(Evas_Object* ewkView);
 
-void ewk_view_toolbars_visible_set(Evas_Object* o, bool visible);
-void ewk_view_toolbars_visible_get(Evas_Object* o, bool* visible);
+void ewk_view_toolbars_visible_set(Evas_Object* ewkView, bool visible);
+void ewk_view_toolbars_visible_get(Evas_Object* ewkView, bool* visible);
 
-void ewk_view_statusbar_visible_set(Evas_Object* o, bool visible);
-void ewk_view_statusbar_visible_get(Evas_Object* o, bool* visible);
-void ewk_view_statusbar_text_set(Evas_Object* o, const char* text);
+void ewk_view_statusbar_visible_set(Evas_Object* ewkView, bool visible);
+void ewk_view_statusbar_visible_get(Evas_Object* ewkView, bool* visible);
+void ewk_view_statusbar_text_set(Evas_Object* ewkView, const char* text);
 
-void ewk_view_scrollbars_visible_set(Evas_Object* o, bool visible);
-void ewk_view_scrollbars_visible_get(Evas_Object* o, bool* visible);
+void ewk_view_scrollbars_visible_set(Evas_Object* ewkView, bool visible);
+void ewk_view_scrollbars_visible_get(Evas_Object* ewkView, bool* visible);
 
-void ewk_view_menubar_visible_set(Evas_Object* o, bool visible);
-void ewk_view_menubar_visible_get(Evas_Object* o, bool* visible);
+void ewk_view_menubar_visible_set(Evas_Object* ewkView, bool visible);
+void ewk_view_menubar_visible_get(Evas_Object* ewkView, bool* visible);
 
-void ewk_view_tooltip_text_set(Evas_Object* o, const char* text);
+void ewk_view_tooltip_text_set(Evas_Object* ewkView, const char* text);
 
-void ewk_view_add_console_message(Evas_Object* o, const char* message, unsigned int lineNumber, const char* sourceID);
+void ewk_view_add_console_message(Evas_Object* ewkView, const char* message, unsigned int lineNumber, const char* sourceID);
 
-void ewk_view_run_javascript_alert(Evas_Object* o, Evas_Object* frame, const char* message);
-bool ewk_view_run_javascript_confirm(Evas_Object* o, Evas_Object* frame, const char* message);
-bool ewk_view_run_javascript_prompt(Evas_Object* o, Evas_Object* frame, const char* message, const char* defaultValue, char** value);
-bool ewk_view_should_interrupt_javascript(Evas_Object* o);
-uint64_t ewk_view_exceeded_database_quota(Evas_Object* o, Evas_Object* frame, const char* databaseName, uint64_t current_size, uint64_t expected_size);
+void ewk_view_run_javascript_alert(Evas_Object* ewkView, Evas_Object* frame, const char* message);
+bool ewk_view_run_javascript_confirm(Evas_Object* ewkView, Evas_Object* frame, const char* message);
+bool ewk_view_run_javascript_prompt(Evas_Object* ewkView, Evas_Object* frame, const char* message, const char* defaultValue, char** value);
+bool ewk_view_should_interrupt_javascript(Evas_Object* ewkView);
+uint64_t ewk_view_exceeded_database_quota(Evas_Object* ewkView, Evas_Object* frame, const char* databaseName, uint64_t currentSize, uint64_t expectedSize);
 
 bool ewk_view_run_open_panel(Evas_Object* ewkView, Evas_Object* frame, bool allowsMultipleFiles, const Vector<String>& acceptMIMETypes, Eina_List** selectedFilenames);
 
-void ewk_view_repaint(Evas_Object* o, Evas_Coord x, Evas_Coord y, Evas_Coord w, Evas_Coord h);
-void ewk_view_scroll(Evas_Object* o, Evas_Coord dx, Evas_Coord dy, Evas_Coord sx, Evas_Coord sy, Evas_Coord sw, Evas_Coord sh, Evas_Coord cx, Evas_Coord cy, Evas_Coord cw, Evas_Coord ch, bool main_frame);
-WebCore::Page* ewk_view_core_page_get(const Evas_Object* o);
+void ewk_view_repaint(Evas_Object* ewkView, Evas_Coord x, Evas_Coord y, Evas_Coord width, Evas_Coord height);
+void ewk_view_scroll(Evas_Object* ewkView, Evas_Coord deltaX, Evas_Coord deltaY, Evas_Coord scrollX, Evas_Coord scrollY, Evas_Coord scrollWidth, Evas_Coord scrollHeight, Evas_Coord centerX, Evas_Coord centerY, Evas_Coord centerW, Evas_Coord centerHeight, bool mainFrame);
+WebCore::Page* ewk_view_core_page_get(const Evas_Object* ewkView);
 
-WTF::PassRefPtr<WebCore::Frame> ewk_view_frame_create(Evas_Object* o, Evas_Object* frame, const WTF::String& name, WebCore::HTMLFrameOwnerElement* ownerElement, const WebCore::KURL& url, const WTF::String& referrer);
+WTF::PassRefPtr<WebCore::Frame> ewk_view_frame_create(Evas_Object* ewkView, Evas_Object* frame, const WTF::String& name, WebCore::HTMLFrameOwnerElement* ownerElement, const WebCore::KURL& url, const WTF::String& referrer);
 
-WTF::PassRefPtr<WebCore::Widget> ewk_view_plugin_create(Evas_Object* o, Evas_Object* frame, const WebCore::IntSize& pluginSize, WebCore::HTMLPlugInElement* element, const WebCore::KURL& url, const WTF::Vector<WTF::String>& paramNames, const WTF::Vector<WTF::String>& paramValues, const WTF::String& mimeType, bool loadManually);
+WTF::PassRefPtr<WebCore::Widget> ewk_view_plugin_create(Evas_Object* ewkView, Evas_Object* frame, const WebCore::IntSize& pluginSize, WebCore::HTMLPlugInElement* element, const WebCore::KURL& url, const WTF::Vector<WTF::String>& paramNames, const WTF::Vector<WTF::String>& paramValues, const WTF::String& mimeType, bool loadManually);
 
-void ewk_view_popup_new(Evas_Object* o, WebCore::PopupMenuClient* client, int selected, const WebCore::IntRect& rect);
-void ewk_view_viewport_attributes_set(Evas_Object* o, const WebCore::ViewportArguments& arguments);
+void ewk_view_popup_new(Evas_Object* ewkView, WebCore::PopupMenuClient* client, int selected, const WebCore::IntRect& rect);
+void ewk_view_viewport_attributes_set(Evas_Object* ewkView, const WebCore::ViewportArguments& arguments);
 
-void ewk_view_download_request(Evas_Object* o, Ewk_Download* download);
+void ewk_view_download_request(Evas_Object* ewkView, Ewk_Download* download);
 
-void ewk_view_editor_client_contents_changed(Evas_Object* o);
-void ewk_view_editor_client_selection_changed(Evas_Object* o);
+void ewk_view_editor_client_contents_changed(Evas_Object* ewkView);
+void ewk_view_editor_client_selection_changed(Evas_Object* ewkView);
 
-bool ewk_view_focus_can_cycle(Evas_Object *o, Ewk_Focus_Direction direction);
+bool ewk_view_focus_can_cycle(Evas_Object* ewkView, Ewk_Focus_Direction direction);
 
 #if ENABLE(NETSCAPE_PLUGIN_API)
-void ewk_view_js_window_object_clear(Evas_Object* o, Evas_Object* frame);
+void ewk_view_js_window_object_clear(Evas_Object* ewkView, Evas_Object* frame);
 #endif
 
 int ewk_util_dpi_get();
@@ -165,62 +165,62 @@ void ewk_history_free(Ewk_History* history);
 
 #if ENABLE(CONTEXT_MENUS)
 Ewk_Context_Menu* ewk_context_menu_new(Evas_Object* view, WebCore::ContextMenuController* controller);
-bool ewk_context_menu_free(Ewk_Context_Menu* o);
-void ewk_context_menu_item_append(Ewk_Context_Menu* o, WebCore::ContextMenuItem& core);
-Ewk_Context_Menu* ewk_context_menu_customize(Ewk_Context_Menu* o);
-void ewk_context_menu_show(Ewk_Context_Menu* o);
+bool ewk_context_menu_free(Ewk_Context_Menu* menu);
+void ewk_context_menu_item_append(Ewk_Context_Menu* menu, WebCore::ContextMenuItem& core);
+Ewk_Context_Menu* ewk_context_menu_customize(Ewk_Context_Menu* menu);
+void ewk_context_menu_show(Ewk_Context_Menu* menu);
 #endif
 
 const Eina_Rectangle* ewk_view_repaints_get(const Ewk_View_Private_Data* priv, size_t* count);
 const Ewk_Scroll_Request* ewk_view_scroll_requests_get(const Ewk_View_Private_Data* priv, size_t* count);
 
-void ewk_view_repaint_add(Ewk_View_Private_Data* priv, Evas_Coord x, Evas_Coord y, Evas_Coord w, Evas_Coord h);
+void ewk_view_repaint_add(Ewk_View_Private_Data* priv, Evas_Coord x, Evas_Coord y, Evas_Coord width, Evas_Coord height);
 
 void ewk_view_layout_if_needed_recursive(Ewk_View_Private_Data* priv);
 
 Ewk_Window_Features* ewk_window_features_new_from_core(const WebCore::WindowFeatures* core);
 
-Evas_Object* ewk_frame_add(Evas* e);
-bool ewk_frame_init(Evas_Object* o, Evas_Object* view, WebCore::Frame* frame);
-bool ewk_frame_child_add(Evas_Object* o, WTF::PassRefPtr<WebCore::Frame> child, const WTF::String& name, const WebCore::KURL& url, const WTF::String& referrer);
-void ewk_frame_view_set(Evas_Object* o, Evas_Object* newParent);
+Evas_Object* ewk_frame_add(Evas* canvas);
+bool ewk_frame_init(Evas_Object* ewkFrame, Evas_Object* view, WebCore::Frame* frame);
+bool ewk_frame_child_add(Evas_Object* ewkFrame, WTF::PassRefPtr<WebCore::Frame> child, const WTF::String& name, const WebCore::KURL& url, const WTF::String& referrer);
+void ewk_frame_view_set(Evas_Object* ewkFrame, Evas_Object* newParent);
 
-void ewk_frame_core_gone(Evas_Object* o);
+void ewk_frame_core_gone(Evas_Object* ewkFrame);
 
-void ewk_frame_load_started(Evas_Object* o);
-void ewk_frame_load_provisional(Evas_Object* o);
-void ewk_frame_load_firstlayout_finished(Evas_Object* o);
-void ewk_frame_load_firstlayout_nonempty_finished(Evas_Object* o);
-void ewk_frame_load_document_finished(Evas_Object* o);
-void ewk_frame_load_finished(Evas_Object* o, const char* error_domain, int error_code, bool is_cancellation, const char* error_description, const char* failing_url);
-void ewk_frame_load_error(Evas_Object* o, const char* error_domain, int error_code, bool is_cancellation, const char* error_description, const char* failing_url);
-void ewk_frame_load_progress_changed(Evas_Object* o);
+void ewk_frame_load_started(Evas_Object* ewkFrame);
+void ewk_frame_load_provisional(Evas_Object* ewkFrame);
+void ewk_frame_load_firstlayout_finished(Evas_Object* ewkFrame);
+void ewk_frame_load_firstlayout_nonempty_finished(Evas_Object* ewkFrame);
+void ewk_frame_load_document_finished(Evas_Object* ewkFrame);
+void ewk_frame_load_finished(Evas_Object* ewkFrame, const char* errorDomain, int errorCode, bool isCancellation, const char* errorDescription, const char* failingUrl);
+void ewk_frame_load_error(Evas_Object* ewkFrame, const char* errorDomain, int errorCode, bool isCancellation, const char* errorDescription, const char* failingUrl);
+void ewk_frame_load_progress_changed(Evas_Object* ewkFrame);
 
-void ewk_frame_request_will_send(Evas_Object* o, Ewk_Frame_Resource_Request* request);
-void ewk_frame_request_assign_identifier(Evas_Object* o, const Ewk_Frame_Resource_Request* request);
-void ewk_frame_view_state_save(Evas_Object* o, WebCore::HistoryItem* item);
+void ewk_frame_request_will_send(Evas_Object* ewkFrame, Ewk_Frame_Resource_Request* request);
+void ewk_frame_request_assign_identifier(Evas_Object* ewkFrame, const Ewk_Frame_Resource_Request* request);
+void ewk_frame_view_state_save(Evas_Object* ewkFrame, WebCore::HistoryItem* item);
 
-void ewk_frame_did_perform_first_navigation(Evas_Object* o);
+void ewk_frame_did_perform_first_navigation(Evas_Object* ewkFrame);
 
-void ewk_frame_contents_size_changed(Evas_Object* o, Evas_Coord w, Evas_Coord h);
-void ewk_frame_title_set(Evas_Object* o, const char* title);
+void ewk_frame_contents_size_changed(Evas_Object* ewkFrame, Evas_Coord width, Evas_Coord height);
+void ewk_frame_title_set(Evas_Object* ewkFrame, const char* title);
 
-void ewk_frame_view_create_for_view(Evas_Object* o, Evas_Object* view);
-bool ewk_frame_uri_changed(Evas_Object* o);
-void ewk_frame_force_layout(Evas_Object* o);
+void ewk_frame_view_create_for_view(Evas_Object* ewkFrame, Evas_Object* view);
+bool ewk_frame_uri_changed(Evas_Object* ewkFrame);
+void ewk_frame_force_layout(Evas_Object* ewkFrame);
 
-WTF::PassRefPtr<WebCore::Widget> ewk_frame_plugin_create(Evas_Object* o, const WebCore::IntSize& pluginSize, WebCore::HTMLPlugInElement* element, const WebCore::KURL& url, const WTF::Vector<WTF::String>& paramNames, const WTF::Vector<WTF::String>& paramValues, const WTF::String& mimeType, bool loadManually);
+WTF::PassRefPtr<WebCore::Widget> ewk_frame_plugin_create(Evas_Object* ewkFrame, const WebCore::IntSize& pluginSize, WebCore::HTMLPlugInElement* element, const WebCore::KURL& url, const WTF::Vector<WTF::String>& paramNames, const WTF::Vector<WTF::String>& paramValues, const WTF::String& mimeType, bool loadManually);
 
-bool ewk_view_navigation_policy_decision(Evas_Object* o, Ewk_Frame_Resource_Request* request);
+bool ewk_view_navigation_policy_decision(Evas_Object* ewkView, Ewk_Frame_Resource_Request* request);
 
-void ewk_view_contents_size_changed(Evas_Object* o, Evas_Coord w, Evas_Coord h);
+void ewk_view_contents_size_changed(Evas_Object* ewkView, Evas_Coord width, Evas_Coord height);
 
-WebCore::FloatRect ewk_view_page_rect_get(const Evas_Object* o);
+WebCore::FloatRect ewk_view_page_rect_get(const Evas_Object* ewkView);
 
 const char* ewk_settings_default_user_agent_get();
 
-void ewk_frame_editor_client_contents_changed(Evas_Object* o);
-void ewk_frame_editor_client_selection_changed(Evas_Object* o);
+void ewk_frame_editor_client_contents_changed(Evas_Object* ewkFrame);
+void ewk_frame_editor_client_selection_changed(Evas_Object* ewkFrame);
 
 void ewk_frame_mixed_content_displayed_set(Evas_Object* ewkFrame, bool hasDisplayed);
 void ewk_frame_mixed_content_run_set(Evas_Object* ewkFrame, bool hasRun);
--- Source/WebKit/efl/ewk/ewk_view.cpp
+++ Source/WebKit/efl/ewk/ewk_view.cpp
@@ -3335,10 +3335,10 @@ void ewk_view_repaint(Evas_Object* ewkView, Evas_Coord x, Evas_Coord y, Evas_Coo
     _ewk_view_smart_changed(smartData);
 }
 
-void ewk_view_scroll(Evas_Object* ewkView, Evas_Coord dx, Evas_Coord dy, Evas_Coord scrollX, Evas_Coord scrollY, Evas_Coord scrollWidth, Evas_Coord scrollHeight, Evas_Coord centerX, Evas_Coord centerY, Evas_Coord centerWidth, Evas_Coord centerHeight, bool mainFrame)
+void ewk_view_scroll(Evas_Object* ewkView, Evas_Coord deltaX, Evas_Coord deltaY, Evas_Coord scrollX, Evas_Coord scrollY, Evas_Coord scrollWidth, Evas_Coord scrollHeight, Evas_Coord centerX, Evas_Coord centerY, Evas_Coord centerWidth, Evas_Coord centerHeight, bool mainFrame)
 {
     DBG("ewkView=%p, delta: %d,%d, scroll: %d,%d+%dx%d, clip: %d,%d+%dx%d",
-        ewkView, dx, dy, scrollX, scrollY, scrollWidth, scrollHeight, centerX, centerY, centerWidth, centerHeight);
+        ewkView, deltaX, deltaY, scrollX, scrollY, scrollWidth, scrollHeight, centerX, centerY, centerWidth, centerHeight);
 
     if ((scrollX != centerX) || (scrollY != centerY) || (scrollWidth != centerWidth) || (scrollHeight != centerHeight))
         WRN("scroll region and clip are different! %d,%d+%dx%d and %d,%d+%dx%d",
@@ -3346,9 +3346,9 @@ void ewk_view_scroll(Evas_Object* ewkView, Evas_Coord dx, Evas_Coord dy, Evas_Co
 
     EWK_VIEW_SD_GET_OR_RETURN(ewkView, smartData);
     EWK_VIEW_PRIV_GET_OR_RETURN(smartData, priv);
-    EINA_SAFETY_ON_TRUE_RETURN(!dx && !dy);
+    EINA_SAFETY_ON_TRUE_RETURN(!deltaX && !deltaY);
 
-    _ewk_view_scroll_add(priv, dx, dy, scrollX, scrollY, scrollWidth, scrollHeight, mainFrame);
+    _ewk_view_scroll_add(priv, deltaX, deltaY, scrollX, scrollY, scrollWidth, scrollHeight, mainFrame);
 
     _ewk_view_smart_changed(smartData);
 }
