e338182f8a125ffaca7811da5579d8a32463cb6a
Ryuan Choi <ryuan.choi@samsung.com>
2011-12-20 01:13:37 +0000

    [EFL] Change the behavior of ewk_view_scale_set. https://bugs.webkit.org/show_bug.cgi?id=70078
    Reviewed by Eric Seidel.
    
    Remove center point basis zoom alignment from ewk_view_scale_set to call
    Page::setPageScaleFactor without any adjustment.
    
    * ewk/ewk_view.cpp:
    (ewk_view_scale_set):
    * ewk/ewk_view.h:
    
    
    Canonical link: https://commits.webkit.org/91650@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@103288 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/efl/ewk/ewk_view.cpp
+++ Source/WebKit/efl/ewk/ewk_view.cpp
@@ -1625,7 +1625,7 @@ float ewk_view_scale_get(const Evas_Object* ewkView)
     return priv->page->pageScaleFactor();
 }
 
-Eina_Bool ewk_view_scale_set(Evas_Object* ewkView, float scaleFactor, Evas_Coord centerX, Evas_Coord centerY)
+Eina_Bool ewk_view_scale_set(Evas_Object* ewkView, float scaleFactor, Evas_Coord scrollX, Evas_Coord scrollY)
 {
     EWK_VIEW_SD_GET_OR_RETURN(ewkView, smartData, false);
     EWK_VIEW_PRIV_GET_OR_RETURN(smartData, priv, false);
@@ -1634,12 +1634,7 @@ Eina_Bool ewk_view_scale_set(Evas_Object* ewkView, float scaleFactor, Evas_Coord
     if (currentScaleFactor == -1)
         return false;
 
-    int x, y;
-    ewk_frame_scroll_pos_get(smartData->main_frame, &x, &y);
-
-    x = static_cast<int>(((x + centerX) / currentScaleFactor) * scaleFactor) - centerX;
-    y = static_cast<int>(((y + centerY) / currentScaleFactor) * scaleFactor) - centerY;
-    priv->page->setPageScaleFactor(scaleFactor, WebCore::LayoutPoint(x, y));
+    priv->page->setPageScaleFactor(scaleFactor, WebCore::LayoutPoint(scrollX, scrollY));
     return true;
 }
 
