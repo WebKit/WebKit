87381e1be890ad34a4721283dbbf3bb3f2abdc8b
Geoffrey Garen <ggaren@apple.com>
2011-12-20 23:30:50 +0000

    Tightened up Vector<T>::append https://bugs.webkit.org/show_bug.cgi?id=74906
    Reviewed by Sam Weinig.
    
    Not a measurable speedup, but code inspection shows better code generated,
    and I believe this is a step toward turning off -fomit-frame-pointer.
    
    * wtf/Vector.h:
    (WTF::::append):
    (WTF::::appendSlowCase): Split out the slow case into a separate function
    to keep unnecessary instructions off the hot path. This means the hot
    path can now be inlined more often.
    
    Removed some old MSVC7 cruft. Hopefully, we don't need to hang on to a
    compiler work-around from 2007.
    
    
    Canonical link: https://commits.webkit.org/91715@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@103356 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/wtf/Vector.h
+++ Source/JavaScriptCore/wtf/Vector.h
@@ -607,6 +607,7 @@ namespace WTF {
         bool tryExpandCapacity(size_t newMinCapacity);
         const T* tryExpandCapacity(size_t newMinCapacity, const T*);
         template<typename U> U* expandCapacity(size_t newMinCapacity, U*); 
+        template<typename U> void appendSlowCase(const U&);
 
         size_t m_size;
         Buffer m_buffer;
@@ -953,24 +954,26 @@ namespace WTF {
     template<typename T, size_t inlineCapacity> template<typename U>
     ALWAYS_INLINE void Vector<T, inlineCapacity>::append(const U& val)
     {
-        const U* ptr = &val;
-        if (size() == capacity()) {
-            ptr = expandCapacity(size() + 1, ptr);
-            if (!begin())
-                return;
+        if (size() != capacity()) {
+            new (NotNull, end()) T(val);
+            ++m_size;
+            return;
         }
-            
-#if COMPILER(MSVC7_OR_LOWER)
-        // FIXME: MSVC7 generates compilation errors when trying to assign
-        // a pointer to a Vector of its base class (i.e. can't downcast). So far
-        // I've been unable to determine any logical reason for this, so I can
-        // only assume it is a bug with the compiler. Casting is a bad solution,
-        // however, because it subverts implicit conversions, so a better 
-        // one is needed. 
-        new (NotNull, end()) T(static_cast<T>(*ptr));
-#else
+
+        appendSlowCase(val);
+    }
+
+    template<typename T, size_t inlineCapacity> template<typename U>
+    void Vector<T, inlineCapacity>::appendSlowCase(const U& val)
+    {
+        ASSERT(size() == capacity());
+
+        const U* ptr = &val;
+        ptr = expandCapacity(size() + 1, ptr);
+        if (!begin())
+            return;
+
         new (NotNull, end()) T(*ptr);
-#endif
         ++m_size;
     }
 
