a1ef57dedd0730d92f4f58688044485b6695735b
Eric Carlson <eric.carlson@apple.com>
2011-12-21 01:14:23 +0000

    WebVTT cues sometimes render when they should not https://bugs.webkit.org/show_bug.cgi?id=74873
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    Test: media/track/track-cue-nothing-to-render.html
    
    * html/shadow/MediaControlRootElement.cpp:
    (WebCore::MediaControlRootElement::updateTextTrackDisplay): Don't return early if the current
        cue is empty so the previous cue is removed.
    
    LayoutTests:
    
    * media/track/captions-webvtt/captions-gaps.vtt: Added.
    * media/track/track-cue-nothing-to-render-expected.txt: Added.
    * media/track/track-cue-nothing-to-render.html: Added.
    
    
    Canonical link: https://commits.webkit.org/91725@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@103371 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/shadow/MediaControlRootElement.cpp
+++ Source/WebCore/html/shadow/MediaControlRootElement.cpp
@@ -634,9 +634,6 @@ void MediaControlRootElement::updateTextTrackDisplay()
         createTextTrackDisplay();
 
     CueList activeCues = toParentMediaElement(m_textDisplayContainer)->currentlyActiveCues();
-    if (activeCues.isEmpty())
-        return;
-
     m_textTrackDisplay->removeChildren();
     bool nothingToDisplay = true;
     for (size_t i = 0; i < activeCues.size(); ++i) {
