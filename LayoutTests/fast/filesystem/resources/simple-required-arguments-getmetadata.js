885aa055d6fdb95400a4c7bb865ce4739c9c29b5
Mark Pilgrim <pilgrim@chromium.org>
2011-12-22 08:48:01 +0000

    [FileSystem API] Entry.getMetadata successCallback is required https://bugs.webkit.org/show_bug.cgi?id=69638
    Patch by Mark Pilgrim <pilgrim@chromium.org> on 2011-12-22
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    Test: fast/filesystem/simple-required-arguments-getmetadata.html
    
    * fileapi/Entry.idl: remove [Optional] flag from successCallback parameter
    
    LayoutTests:
    
    * fast/filesystem/resources/simple-required-arguments-getmetadata.js: Added.
    (errorCallback):
    (successCallback):
    * fast/filesystem/simple-required-arguments-getmetadata-expected.txt: Added.
    * fast/filesystem/simple-required-arguments-getmetadata.html: Added.
    
    Canonical link: https://commits.webkit.org/91813@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@103501 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/filesystem/resources/simple-required-arguments-getmetadata.js
@@ -0,0 +1,25 @@
+if (this.importScripts) {
+    importScripts('../resources/fs-worker-common.js');
+    importScripts('../resources/fs-test-util.js');
+}
+
+description("DirectoryEntry.getMetadata required arguments test.");
+
+var fileSystem = null;
+
+function errorCallback(error) {
+    debug("Error occured while requesting a TEMPORARY file system:" + error.code);
+    finishJSTest();
+}
+
+function successCallback(fs) {
+    fileSystem = fs;
+    debug("Successfully obtained TEMPORARY FileSystem:" + fileSystem.name);
+    root = evalAndLog("root = fileSystem.root");
+    shouldThrow("root.getMetadata()");
+    finishJSTest();
+}
+
+var jsTestIsAsync = true;
+evalAndLog("webkitRequestFileSystem(TEMPORARY, 100, successCallback, errorCallback);");
+var successfullyParsed = true;
