8bda2fe39ca6d474c3740b4fe5308bf3a358cc54
Leo Yang <leoyang@rim.com>
2011-12-22 14:13:06 +0000

    [BlackBerry] Upstream the BlackBerry change to ResourceHandle.h https://bugs.webkit.org/show_bug.cgi?id=75061
    Reviewed by George Staikos.
    
    No functionality change to the existing code, no new tests.
    
    * platform/network/ResourceHandle.h:
    
    
    Canonical link: https://commits.webkit.org/91850@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@103538 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/network/ResourceHandle.h
+++ Source/WebCore/platform/network/ResourceHandle.h
@@ -171,7 +171,7 @@ public:
     static void CALLBACK internetStatusCallback(HINTERNET, DWORD_PTR, DWORD, LPVOID, DWORD);
 #endif
 
-#if PLATFORM(QT) || USE(CURL) || USE(SOUP)
+#if PLATFORM(QT) || USE(CURL) || USE(SOUP) || PLATFORM(BLACKBERRY)
     ResourceHandleInternal* getInternal() { return d.get(); }
 #endif
 
