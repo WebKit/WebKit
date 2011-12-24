60084ea2f23b7c4634c7c9ad7c7708bb5b5cd6cc
Ryosuke Niwa <rniwa@webkit.org>
2011-12-24 06:12:35 +0000

    Chromium Mac build fix.
    * WebCoreSupport/WebSystemInterface.mm:
    (InitWebCoreSystemInterface):
    
    
    Canonical link: https://commits.webkit.org/91957@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@103652 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/mac/WebCoreSupport/WebSystemInterface.mm
+++ Source/WebKit/mac/WebCoreSupport/WebSystemInterface.mm
@@ -67,7 +67,9 @@ void InitWebCoreSystemInterface(void)
     INIT(SignedPublicKeyAndChallengeString);
     INIT(GetPreferredExtensionForMIMEType);
     INIT(GetWheelEventDeltas);
+#if PLATFORM(MAC)
     INIT(GetNSEventKeyChar);
+#endif
     INIT(HitTestMediaUIPart);
     INIT(InitializeMaximumHTTPConnectionCountPerHost);
     INIT(MeasureMediaUIPart);
