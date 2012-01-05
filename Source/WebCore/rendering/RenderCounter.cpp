e36280af82cea8a3e798db52ecb79ffe6652a3dd
Lucas Forschler <lforschler@apple.com>
2012-01-05 21:19:26 +0000

    Merged r97927.
    Canonical link: https://commits.webkit.org/76367.541@safari-534.54-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-534.54-branch@104204 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderCounter.cpp
+++ Source/WebCore/rendering/RenderCounter.cpp
@@ -47,6 +47,14 @@ static CounterMaps& counterMaps()
     return staticCounterMaps;
 }
 
+static RenderObject* rendererOfAfterPseudoElement(RenderObject* renderer)
+{
+    RenderObject* lastContinuation = renderer;
+    while (RenderObject* continuation = lastContinuation->virtualContinuation())
+        lastContinuation = continuation;
+    return lastContinuation->afterPseudoElementRenderer();
+}
+
 // This function processes the renderer tree in the order of the DOM tree
 // including pseudo elements as defined in CSS 2.1.
 // Anonymous renderers are skipped except for those representing pseudo elements.
@@ -73,7 +81,7 @@ static RenderObject* previousInPreOrder(const RenderObject* object)
     }
     while (sibling) {
         if (RenderObject* renderer = sibling->renderer()) {
-            if (RenderObject* after = renderer->afterPseudoElementRenderer())
+            if (RenderObject* after = rendererOfAfterPseudoElement(renderer))
                 return after;
             parent = sibling;
             sibling = sibling->lastElementChild();
@@ -185,7 +193,7 @@ static RenderObject* nextInPreOrder(const RenderObject* object, const Element* s
                 return result;
             child = child->nextElementSibling();
         }
-        result = self->renderer()->afterPseudoElementRenderer();
+        result = rendererOfAfterPseudoElement(self->renderer());
         if (result)
             return result;
 nextsibling:
