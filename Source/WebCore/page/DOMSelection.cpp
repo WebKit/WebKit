0ee02c4a804cf91eb824298356e89f23f9bd6562
Abhishek Arya <inferno@chromium.org>
2012-01-06 20:15:59 +0000

    Crash with range selection across different documents. https://bugs.webkit.org/show_bug.cgi?id=74285
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Test: fast/dom/Range/range-selection-across-documents-crash.html
    
    * page/DOMSelection.cpp:
    (WebCore::DOMSelection::addRange):
    
    LayoutTests:
    
    * fast/dom/Range/range-selection-across-documents-crash-expected.txt: Added.
    * fast/dom/Range/range-selection-across-documents-crash.html: Added.
    
    
    Canonical link: https://commits.webkit.org/92526@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@104317 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/DOMSelection.cpp
+++ Source/WebCore/page/DOMSelection.cpp
@@ -419,7 +419,7 @@ void DOMSelection::addRange(Range* r)
         }
     } else {
         // We don't support discontiguous selection. We don't do anything if r and range don't intersect.
-        if (r->compareBoundaryPoints(Range::END_TO_START, range.get(), ec) < 1) {
+        if (r->compareBoundaryPoints(Range::END_TO_START, range.get(), ec) < 1 && !ec) {
             if (r->compareBoundaryPoints(Range::END_TO_END, range.get(), ec) == -1)
                 // The original range contains r.
                 selection->setSelection(VisibleSelection(range.get()));
