36422cf3cc6260ee4cf5b88f862aaaee01906a11
Lucas Forschler <lforschler@apple.com>
2012-01-10 01:02:14 +0000

    Merged r102377.
    Canonical link: https://commits.webkit.org/76367.577@safari-534.54-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-534.54-branch@104515 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/mac/WKFullScreenWindowController.mm
+++ Source/WebKit2/UIProcess/mac/WKFullScreenWindowController.mm
@@ -185,8 +185,13 @@ using namespace WebCore;
         screen = [NSScreen mainScreen];
     NSRect screenFrame = [screen frame];
     
+#if defined(BUILDING_ON_LEOPARD) || defined(BUILDING_ON_SNOW_LEOPARD)
     NSRect webViewFrame = [_webView convertRectToBase:[_webView frame]];
     webViewFrame.origin = [[_webView window] convertBaseToScreen:webViewFrame.origin];
+#else
+    NSRect webViewFrame = [[_webView window] convertRectToScreen:
+        [_webView convertRect:[_webView frame] toView:nil]];
+#endif
         
     // In the case of a multi-monitor setup where the webView straddles two
     // monitors, we must create a window large enough to contain the destination
