2f9e26a3f89ce238f4c8840ee0a7505f5d8af191
Mike Reed <reed@google.com>
2012-01-10 19:50:40 +0000

    [skia] not all convex paths are convex, so recompute convexity for the problematic ones https://bugs.webkit.org/show_bug.cgi?id=75960
    Reviewed by Stephen White.
    
    No new tests.
    See related chrome issue
    http://code.google.com/p/chromium/issues/detail?id=108605
    
    * platform/graphics/skia/GraphicsContextSkia.cpp:
    (WebCore::setPathFromConvexPoints):
    
    
    Canonical link: https://commits.webkit.org/92787@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@104609 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
+++ Source/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
@@ -440,7 +440,17 @@ static void setPathFromConvexPoints(SkPath* path, size_t numPoints, const FloatP
         path->lineTo(WebCoreFloatToSkScalar(points[i].x()),
                      WebCoreFloatToSkScalar(points[i].y()));
     }
-    path->setIsConvex(true);
+
+    /*  The code used to just blindly call this
+            path->setIsConvex(true);
+        But webkit can sometimes send us non-convex 4-point values, so we mark the path's
+        convexity as unknown, so it will get computed by skia at draw time.
+        See crbug.com 108605
+    */
+    SkPath::Convexity convexity = SkPath::kConvex_Convexity;
+    if (numPoints == 4)
+        convexity = SkPath::kUnknown_Convexity;
+    path->setConvexity(convexity);
 }
 
 void GraphicsContext::drawConvexPolygon(size_t numPoints,
