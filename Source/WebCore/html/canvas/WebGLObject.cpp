75d9aa68559a009c231cc64ed0fb091cb683c331
Kenneth Russell <kbr@google.com>
2012-01-13 19:19:24 +0000

    Unreviewed, build fix for unused argument warning after r104954. https://bugs.webkit.org/show_bug.cgi?id=75906
    Also fixed up somebody's bad merge in Source/WebCore/ChangeLog.
    
    * html/canvas/WebGLObject.cpp:
    (WebCore::WebGLObject::WebGLObject):
    
    Canonical link: https://commits.webkit.org/93105@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@104959 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/canvas/WebGLObject.cpp
+++ Source/WebCore/html/canvas/WebGLObject.cpp
@@ -41,7 +41,7 @@
 
 namespace WebCore {
 
-WebGLObject::WebGLObject(WebGLRenderingContext* context)
+WebGLObject::WebGLObject(WebGLRenderingContext*)
     : m_object(0)
     , m_attachmentCount(0)
     , m_deleted(false)
