dfc9ad784a7200bcd970e3cd2b54d0694f84a59c
Lucas Forschler <lforschler@apple.com>
2012-01-13 00:24:58 +0000

    Merge pre-reqs for 102024.
    Canonical link: https://commits.webkit.org/76367.586@safari-534.54-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-534.54-branch@104875 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebCore/platform/mac/DisplaySleepDisabler.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2011 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "DisplaySleepDisabler.h"
+
+#include <IOKit/pwr_mgt/IOPMLib.h>
+#include <wtf/RetainPtr.h>
+
+#ifdef BUILDING_ON_LEOPARD
+#include <wtf/UnusedParam.h>
+#endif
+
+namespace WebCore {
+
+static const double systemActivityInterval = 1;
+
+DisplaySleepDisabler::DisplaySleepDisabler(const char* reason)
+    : m_disableDisplaySleepAssertion(0)
+#ifdef BUILDING_ON_LEOPARD
+    , m_systemActivityTimer(this, &DisplaySleepDisabler::systemActivityTimerFired)
+#endif
+{
+#ifndef BUILDING_ON_LEOPARD
+    RetainPtr<CFStringRef> reasonCF(AdoptCF, CFStringCreateWithCString(kCFAllocatorDefault, reason, kCFStringEncodingUTF8));
+    IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep, kIOPMAssertionLevelOn, reasonCF.get(), &m_disableDisplaySleepAssertion);
+#else
+    UNUSED_PARAM(reason);
+    IOPMAssertionCreate(kIOPMAssertionTypeNoDisplaySleep, kIOPMAssertionLevelOn, &_idleDisplaySleepAssertion);
+    m_systemActivityTimer.startRepeating(systemActivityInterval);
+#endif
+}
+
+DisplaySleepDisabler::~DisplaySleepDisabler()
+{
+    IOPMAssertionRelease(m_disableDisplaySleepAssertion);
+}
+    
+#ifdef BUILDING_ON_LEOPARD
+void DisplaySleepDisabler::systemActivityTimerFired(Timer<DisplaySleepDisabler>*)
+{
+    UpdateSystemActivity(OverallAct);
+}
+#endif
+
+}
