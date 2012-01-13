9e59a5514843041084ecd1a4de0bc5135720a8c0
Lucas Forschler <lforschler@apple.com>
2012-01-13 18:35:01 +0000

    Merged r103482.
    Canonical link: https://commits.webkit.org/76367.590@safari-534.54-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-534.54-branch@104950 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/js/script-tests/dfg-convert-this-dom-window.js
@@ -0,0 +1,24 @@
+description(
+"This tests that we can correctly call Function.prototype.call in the DFG, but more precisely, that we give the correct this object in case it is undefined"
+);
+
+var myObject = { call: function() { return [myObject, "myObject.call"] } };
+var myFunction = function (arg1) { return [this, "myFunction", arg1] };
+var myFunctionWithCall = function (arg1) { return [this, "myFunctionWithCall", arg1] };
+myFunctionWithCall.call = function (arg1) { return [this, "myFunctionWithCall.call", arg1] };
+Function.prototype.aliasedCall = Function.prototype.call;
+
+for (var i = 0; i < 100; ++i) {
+    shouldBe("myObject.call()", '[myObject, "myObject.call"]');
+    shouldBe("myFunction('arg1')", '[this, "myFunction", "arg1"]');
+    shouldBe("myFunction.call(myObject, 'arg1')", '[myObject, "myFunction", "arg1"]');
+    shouldBe("myFunction.call()", '[this, "myFunction", undefined]');
+    shouldBe("myFunction.call(null)", '[this, "myFunction", undefined]');
+    shouldBe("myFunction.call(undefined)", '[this, "myFunction", undefined]');
+    shouldBe("myFunction.aliasedCall(myObject, 'arg1')", '[myObject, "myFunction", "arg1"]');
+    shouldBe("myFunction.aliasedCall()", '[this, "myFunction", undefined]');
+    shouldBe("myFunction.aliasedCall(null)", '[this, "myFunction", undefined]');
+    shouldBe("myFunction.aliasedCall(undefined)", '[this, "myFunction", undefined]');
+    shouldBe("myFunctionWithCall.call(myObject, 'arg1')", '[myFunctionWithCall, "myFunctionWithCall.call", myObject]');
+    shouldBe("myFunctionWithCall.aliasedCall(myObject, 'arg1')", '[myObject, "myFunctionWithCall", "arg1"]');
+}
