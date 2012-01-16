25048bee14ae4eb070026809df217b3ccd4ffc86
Pavel Heimlich <tropikhajma@gmail.com>
2012-01-16 10:23:05 +0000

    Solaris Studio supports alignment macros too https://bugs.webkit.org/show_bug.cgi?id=75453
    Patch by Pavel Heimlich <tropikhajma@gmail.com> on 2012-01-16
    Reviewed by Hajime Morita.
    
    * wtf/Alignment.h:
    
    Canonical link: https://commits.webkit.org/93177@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@105044 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/wtf/Alignment.h
+++ Source/JavaScriptCore/wtf/Alignment.h
@@ -26,7 +26,7 @@
 
 namespace WTF {
 
-#if COMPILER(GCC) || COMPILER(MINGW) || COMPILER(RVCT) || COMPILER(GCCE)
+#if COMPILER(GCC) || COMPILER(MINGW) || COMPILER(RVCT) || COMPILER(GCCE) || (COMPILER(SUNCC) && __SUNPRO_CC > 0x590)
     #define WTF_ALIGN_OF(type) __alignof__(type)
     #define WTF_ALIGNED(variable_type, variable, n) variable_type variable __attribute__((__aligned__(n)))
 #elif COMPILER(MSVC)
