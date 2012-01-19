d1dac5f576694e4cfcfea314f1a9bc005c594da6
Ojan Vafai <ojan@chromium.org>
2012-01-19 00:07:00 +0000

    Fix tests from http://trac.webkit.org/changeset/105332.
    The code assumed that if you passed an options object with a configuration
    attribute that it would have a valid value. A bunch of the testing mocks
    do things like having a configuration value of None.
    
    * Scripts/webkitpy/layout_tests/port/base.py:
    (Port.__init__):
    
    Canonical link: https://commits.webkit.org/93409@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@105335 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/port/base.py
+++ Tools/Scripts/webkitpy/layout_tests/port/base.py
@@ -146,7 +146,7 @@ class Port(object):
         self._pretty_patch_path = self.path_from_webkit_base("Websites", "bugs.webkit.org", "PrettyPatch", "prettify.rb")
         self._pretty_patch_available = None
 
-        if not options or not options.configuration:
+        if not hasattr(options, 'configuration') or not options.configuration:
             self.set_option_default('configuration', self.default_configuration())
         self._test_configuration = None
         self._reftest_list = {}
