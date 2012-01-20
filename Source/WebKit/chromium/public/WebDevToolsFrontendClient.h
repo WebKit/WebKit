7f22238a30c286440c4ac72801724943b7f13876
Pavel Feldman <pfeldman@chromium.org>
2012-01-20 12:31:01 +0000

    Web Inspector: [chromium] add WebDevToolsFrontendClient::openInNewTab for upcoming WebCore change. https://bugs.webkit.org/show_bug.cgi?id=76698
    Reviewed by Yury Semikhatsky.
    
    * public/WebDevToolsFrontendClient.h:
    (WebKit::WebDevToolsFrontendClient::openInNewTab):
    
    Canonical link: https://commits.webkit.org/93545@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@105507 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/chromium/public/WebDevToolsFrontendClient.h
+++ Source/WebKit/chromium/public/WebDevToolsFrontendClient.h
@@ -49,6 +49,7 @@ public:
     virtual void requestUndockWindow() { }
     virtual void requestSetDockSide(const WebString& side) { }
     virtual void moveWindowBy(const WebFloatPoint&) { }
+    virtual void openInNewTab(const WebString& side) { }
     virtual void saveAs(const WebString& fileName, const WebString& content) { }
 
 protected:
