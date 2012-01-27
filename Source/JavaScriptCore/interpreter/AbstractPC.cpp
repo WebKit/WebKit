335f0fc38a04dc12d41b030f537f5c91ac754f79
Lucas Forschler <lforschler@apple.com>
2012-01-27 22:05:56 +0000

    build fix
    Canonical link: https://commits.webkit.org/76367.672@safari-534.54-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-534.54-branch@106149 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/interpreter/AbstractPC.cpp
+++ Source/JavaScriptCore/interpreter/AbstractPC.cpp
@@ -35,6 +35,7 @@ namespace JSC {
 
 AbstractPC::AbstractPC(JSGlobalData& globalData, ExecState* exec)
 {
+    UNUSED_PARAM(globalData);
 #if ENABLE(JIT)
     if (globalData.canUseJIT()) {
         m_pointer = exec->returnPC().value();
