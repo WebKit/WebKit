773fd72e90dbe31881bd442ccbb30137e075a64b
Mark Rowe <mrowe@apple.com>
2012-01-27 01:30:33 +0000

    Merge r102507.
    Canonical link: https://commits.webkit.org/76367.664@safari-534.54-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-534.54-branch@106070 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/Launcher/mac/DynamicLinkerEnvironmentExtractor.mm
+++ Source/WebKit2/UIProcess/Launcher/mac/DynamicLinkerEnvironmentExtractor.mm
@@ -23,10 +23,10 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef BUILDING_ON_SNOW_LEOPARD
-
 #include "config.h"
 #include "DynamicLinkerEnvironmentExtractor.h"
+
+#ifndef BUILDING_ON_SNOW_LEOPARD
 
 #include "EnvironmentVariables.h"
 #include <mach-o/loader.h>
