1af62b5718d0339da0cf37ff848d60ad0ff25501
Ojan Vafai <ojan@chromium.org>
2012-01-31 01:04:43 +0000

    run-webkit-tests calls out to webkit-build-directory twice https://bugs.webkit.org/show_bug.cgi?id=77248
    Reviewed by Dirk Pranke.
    
    This reduces to calls to webkit-build-directory to one call and
    saves 500ms for run-webkit-tests over a single file.
    
    * Scripts/webkit-build-directory:
    If called with no arguments, print out both the top-level directory and the
    configuration directory.
    
    * Scripts/webkit-build-directory:
    * Scripts/webkitpy/layout_tests/port/config.py:
    (Config.build_directory):
    When called with no arguments, get and cache both the top-level and configuration directories.
    
    * Scripts/webkitpy/layout_tests/port/config_standalone.py:
    (main):
    * Scripts/webkitpy/layout_tests/port/config_unittest.py:
    (ConfigTest.assert_configuration):
    (ConfigTest.test_build_directory.mock_webkit_build_directory):
    (ConfigTest.test_build_directory):
    (ConfigTest.test_default_configuration__notfound):
    * Scripts/webkitpy/layout_tests/port/webkit.py:
    (WebKitPort._build_path):
    Cache the root directory in the options object so that other users
    of the options object don't need to call out to webkit-build-directory.
    
    
    Canonical link: https://commits.webkit.org/94264@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@106302 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/port/config_unittest.py
+++ Tools/Scripts/webkitpy/layout_tests/port/config_unittest.py
@@ -51,18 +51,21 @@ class ConfigTest(unittest.TestCase):
     def assert_configuration(self, contents, expected):
         # This tests that a configuration file containing
         # _contents_ ends up being interpreted as _expected_.
-        c = self.make_config('foo', {'foo/Configuration': contents})
+        output = 'foo\nfoo/%s' % contents
+        c = self.make_config(output, {'foo/Configuration': contents})
         self.assertEqual(c.default_configuration(), expected)
 
     def test_build_directory(self):
         # --top-level
         def mock_webkit_build_directory(arg_list):
             if arg_list == ['--top-level']:
-                return '/WebKitBuild'
+                return '/WebKitBuild/'
             elif arg_list == ['--configuration', '--debug']:
                 return '/WebKitBuild/Debug'
             elif arg_list == ['--configuration', '--release']:
                 return '/WebKitBuild/Release'
+            elif arg_list == []:
+                return '/WebKitBuild/\n/WebKitBuild//Debug\n'
             return 'Error'
 
         def mock_run_command(arg_list):
@@ -71,10 +74,10 @@ class ConfigTest(unittest.TestCase):
             return 'Error'
 
         c = self.make_config(run_command_fn=mock_run_command)
-        self.assertTrue(c.build_directory(None).endswith('WebKitBuild'))
+        self.assertEqual(c.build_directory(None), '/WebKitBuild/')
 
         # Test again to check caching
-        self.assertTrue(c.build_directory(None).endswith('WebKitBuild'))
+        self.assertEqual(c.build_directory(None), '/WebKitBuild/')
 
         # Test other values
         self.assertTrue(c.build_directory('Release').endswith('/Release'))
@@ -95,7 +98,7 @@ class ConfigTest(unittest.TestCase):
 
     def test_default_configuration__notfound(self):
         # This tests what happens if the default configuration file doesn't exist.
-        c = self.make_config(output='foo', files={'foo/Configuration': None})
+        c = self.make_config(output='foo\nfoo/Release', files={'foo/Configuration': None})
         self.assertEqual(c.default_configuration(), "Release")
 
     def test_default_configuration__unknown(self):
