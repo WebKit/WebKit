e9425afaf636e5058aa19581c979d1e965697b79
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2012-01-31 23:11:13 +0000

    Fix the update_webgl_conformance_tests.py script in order to enable it to detect the SCM being used correctly. https://bugs.webkit.org/show_bug.cgi?id=77462
    Patch by Ehsan Akhgari <ehsan.akhgari@gmail.com> on 2012-01-31
    Reviewed by Kenneth Russell.
    
    * Scripts/webkitpy/to_be_moved/update_webgl_conformance_tests.py:
    (default_out_dir):
    
    Canonical link: https://commits.webkit.org/94358@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@106404 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/to_be_moved/update_webgl_conformance_tests.py
+++ Tools/Scripts/webkitpy/to_be_moved/update_webgl_conformance_tests.py
@@ -32,6 +32,8 @@ import os
 import re
 import sys
 from webkitpy.common.checkout import scm
+from webkitpy.common.system.filesystem import FileSystem
+from webkitpy.common.system.executive import Executive
 
 
 _log = logging.getLogger(__name__)
@@ -94,7 +96,8 @@ def update_directory(in_dir, out_dir):
 
 
 def default_out_dir():
-    current_scm = scm.detect_scm_system(os.path.dirname(sys.argv[0]))
+    detector = scm.SCMDetector(FileSystem(), Executive())
+    current_scm = detector.detect_scm_system(os.path.dirname(sys.argv[0]))
     if not current_scm:
         return os.getcwd()
     root_dir = current_scm.checkout_root
