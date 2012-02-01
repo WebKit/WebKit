ba478ba09a44364d46c2ddcdcef136080aa33572
Lucas Forschler <lforschler@apple.com>
2012-02-01 21:46:51 +0000

    Merged r106271.
    Canonical link: https://commits.webkit.org/76367.673@safari-534.54-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-534.54-branch@106489 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/PlatformScreen.h
+++ Source/WebCore/platform/PlatformScreen.h
@@ -43,20 +43,21 @@
 namespace WebCore {
 
     class FloatRect;
+    class FrameView;
     class Widget;
 
     int screenDepth(Widget*);
     int screenDepthPerComponent(Widget*);
     bool screenIsMonochrome(Widget*);
 
-    FloatRect screenRect(Widget*);
-    FloatRect screenAvailableRect(Widget*);
+    FloatRect screenRect(FrameView*);
+    FloatRect screenAvailableRect(FrameView*);
 
 #if PLATFORM(MAC)
     NSScreen *screenForWindow(NSWindow *);
 
-    FloatRect toUserSpace(const NSRect&, NSWindow *destination);
-    NSRect toDeviceSpace(const FloatRect&, NSWindow *source);
+    FloatRect toUserSpace(const NSRect&, NSWindow *destination, float deviceScaleFactor);
+    NSRect toDeviceSpace(const FloatRect&, NSWindow *source, float deviceScaleFactor);
 
     NSPoint flipScreenPoint(const NSPoint&, NSScreen *);
 #endif
