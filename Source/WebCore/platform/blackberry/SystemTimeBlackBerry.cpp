d9799324f6168cb1d0d48ad7a39bd19582d4ad1d
Yong Li <yoli@rim.com>
2012-02-03 22:52:10 +0000

    [BlackBerry] Let userIdleTime() return maximum number instead of 0 as most of other ports do. https://bugs.webkit.org/show_bug.cgi?id=77769
    Patch by Yong Li <yoli@rim.com> on 2012-02-03
    Reviewed by Rob Buis.
    
    userIdleTime() should return a big number so it won't block page cache
    from releasing cached pages. See PageCache::releaseAutoreleasedPagesNowOrReschedule().
    
    No new tests as no visible functional changes.
    
    * platform/blackberry/SystemTimeBlackBerry.cpp:
    (WebCore::userIdleTime):
    
    Canonical link: https://commits.webkit.org/94633@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@106697 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/blackberry/SystemTimeBlackBerry.cpp
+++ Source/WebCore/platform/blackberry/SystemTimeBlackBerry.cpp
@@ -21,13 +21,15 @@
 #include "SystemTime.h"
 
 #include "NotImplemented.h"
+#include <limits>
 
 namespace WebCore {
 
 float userIdleTime()
 {
     notImplemented();
-    return 0;
+    // return an arbitrarily high userIdleTime so that releasing pages from the page cache isn't postponed.
+    return std::numeric_limits<float>::max();
 }
 
 } // namespace WebCore
