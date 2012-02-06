WebKit/WebKit

commit b81d7197e6e7d392728b10d2e167e6ffd7e1f48f

Kill CSSMappedAttributeDeclaration. <http://webkit.org/b/77820>
Reviewed by Antti Koivisto.

Replace all use of CSSMappedAttributeDeclaration by StylePropertySet.
Moved the setNeedsStyleRecalc() calls from CSSMappedAttributeDeclaration
to the add/remove-CSS-property helpers in StyledElement.

This removes one step of indirection for attribute styles and reduces
the size of elements that have presentational attributes by one pointer.

* CMakeLists.txt:
* GNUmakefile.list.am:
* Target.pri:
* WebCore.gypi:
* WebCore.order:
* WebCore.vcproj/WebCore.vcproj:
* WebCore.xcodeproj/project.pbxproj:
* dom/CSSMappedAttributeDeclaration.cpp: Removed.
* dom/CSSMappedAttributeDeclaration.h: Removed.
* dom/DOMAllInOne.cpp:

    Remove CSSMappedAttributeDeclaration.{cpp,h}

* css/CSSParser.h:
* css/CSSParser.cpp:
(WebCore::parseSimpleLengthValue):

    Remove CSSParser::parseMappedAttributeValue(), we now use parseValue()
    directly instead. We lose the benefit of caching new CSSValues in the
    document's CSSValuePool but this optimization can be added back later.

* css/CSSStyleSelector.cpp:
(WebCore::CSSStyleSelector::matchAllRules):
* dom/ElementAttributeData.h:
(ElementAttributeData):
* dom/NamedNodeMap.cpp:
(WebCore::NamedNodeMap::ensureAttributeStyle):
* dom/NamedNodeMap.h:
(WebCore::NamedNodeMap::attributeStyle):
(NamedNodeMap):
* dom/StyledElement.cpp:
(WebCore::StyledElement::removeCSSProperties):
(WebCore::StyledElement::addCSSProperty):
(WebCore::StyledElement::addCSSImageProperty):
(WebCore::StyledElement::addCSSLength):
(WebCore::StyledElement::addCSSColor):
* dom/StyledElement.h:
(WebCore):
(WebCore::StyledElement::attributeStyle):
(WebCore::StyledElement::ensureAttributeStyle):
* inspector/InspectorCSSAgent.cpp:
(WebCore::InspectorCSSAgent::buildArrayForAttributeStyles):


Canonical link: https://commits.webkit.org/94691@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@106756 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/ElementAttributeData.h
@@ -26,7 +26,6 @@
 #ifndef ElementAttributeData_h
 #define ElementAttributeData_h
 
-#include "CSSMappedAttributeDeclaration.h"
 #include "SpaceSplitString.h"
 #include "StylePropertySet.h"
 
@@ -51,7 +50,7 @@ private:
     }
 
     RefPtr<StylePropertySet> m_inlineStyleDecl;
-    RefPtr<CSSMappedAttributeDeclaration> m_attributeStyle;
+    RefPtr<StylePropertySet> m_attributeStyle;
     SpaceSplitString m_classNames;
     AtomicString m_idForStyleResolution;
 };

Source/WebCore/dom/StyledElement.cpp
@@ -25,6 +25,7 @@
 #include "StyledElement.h"
 
 #include "Attribute.h"
+#include "CSSImageValue.h"
 #include "CSSStyleSelector.h"
 #include "CSSStyleSheet.h"
 #include "CSSValueKeywords.h"
@@ -121,49 +122,56 @@ void StyledElement::parseMappedAttribute(Attribute* attr)
 
 void StyledElement::removeCSSProperties(int id1, int id2, int id3, int id4, int id5, int id6, int id7, int id8)
 {
-    CSSMappedAttributeDeclaration* style = attributeStyle();
+    StylePropertySet* style = attributeStyle();
     if (!style)
         return;
 
+    setNeedsStyleRecalc(FullStyleChange);
+
     ASSERT(id1 != CSSPropertyInvalid);
-    style->removeMappedProperty(this, id1);
+    style->removeProperty(id1);
 
     if (id2 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id2);
+    style->removeProperty(id2);
     if (id3 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id3);
+    style->removeProperty(id3);
     if (id4 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id4);
+    style->removeProperty(id4);
     if (id5 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id5);
+    style->removeProperty(id5);
     if (id6 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id6);
+    style->removeProperty(id6);
     if (id7 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id7);
+    style->removeProperty(id7);
     if (id8 == CSSPropertyInvalid)
         return;
-    style->removeMappedProperty(this, id8);
+    style->removeProperty(id8);
 }
 
 void StyledElement::addCSSProperty(int id, const String &value)
 {
-    ensureAttributeStyle()->setMappedProperty(this, id, value);
+    if (!ensureAttributeStyle()->setProperty(id, value))
+        removeCSSProperty(id);
+    else
+        setNeedsStyleRecalc(FullStyleChange);
 }
 
 void StyledElement::addCSSProperty(int id, int value)
 {
-    ensureAttributeStyle()->setMappedProperty(this, id, value);
+    ensureAttributeStyle()->setProperty(id, value);
+    setNeedsStyleRecalc(FullStyleChange);
 }
 
 void StyledElement::addCSSImageProperty(int id, const String& url)
 {
-    ensureAttributeStyle()->setMappedImageProperty(this, id, url);
+    ensureAttributeStyle()->setProperty(CSSProperty(id, CSSImageValue::create(url)));
+    setNeedsStyleRecalc(FullStyleChange);
 }
 
 void StyledElement::addCSSLength(int id, const String &value)
@@ -192,12 +200,12 @@ void StyledElement::addCSSLength(int id, const String &value)
         }
 
         if (l != v->length()) {
-            ensureAttributeStyle()->setMappedLengthProperty(this, id, v->substring(0, l));
+            addCSSProperty(id, v->substring(0, l));
             return;
         }
     }
 
-    ensureAttributeStyle()->setMappedLengthProperty(this, id, value);
+    addCSSProperty(id, value);
 }
 
 static String parseColorStringWithCrazyLegacyRules(const String& colorString)
@@ -272,11 +280,11 @@ void StyledElement::addCSSColor(int id, const String& attributeValue)
     // If the string is a named CSS color or a 3/6-digit hex color, use that.
     Color parsedColor(colorString);
     if (parsedColor.isValid()) {
-        ensureAttributeStyle()->setMappedProperty(this, id, colorString);
+        addCSSProperty(id, colorString);
         return;
     }
 
-    ensureAttributeStyle()->setMappedProperty(this, id, parseColorStringWithCrazyLegacyRules(colorString));
+    addCSSProperty(id, parseColorStringWithCrazyLegacyRules(colorString));
 }
 
 void StyledElement::copyNonAttributeProperties(const Element* sourceElement)

Source/WebCore/dom/StyledElement.h
@@ -32,7 +32,6 @@
 namespace WebCore {
 
 class Attribute;
-class CSSMappedAttributeDeclaration;
 
 class StyledElement : public Element {
 public:
@@ -55,8 +54,8 @@ public:
     StylePropertySet* ensureInlineStyleDecl() { return ensureAttributeMap()->ensureInlineStyleDecl(); }
     virtual CSSStyleDeclaration* style() OVERRIDE { return ensureInlineStyleDecl()->ensureCSSStyleDeclaration(); }
 
-    CSSMappedAttributeDeclaration* attributeStyle() const { return attributeMap() ? attributeMap()->attributeStyle() : 0; }
-    CSSMappedAttributeDeclaration* ensureAttributeStyle() { return ensureAttributeMap()->ensureAttributeStyle(); }
+    StylePropertySet* attributeStyle() const { return attributeMap() ? attributeMap()->attributeStyle() : 0; }
+    StylePropertySet* ensureAttributeStyle() { return ensureAttributeMap()->ensureAttributeStyle(); }
 
     const SpaceSplitString& classNames() const;
 

Source/WebCore/CMakeLists.txt
@@ -520,7 +520,6 @@ SET(WebCore_SOURCES
     dom/Comment.cpp
     dom/CompositionEvent.cpp
     dom/ContainerNode.cpp
-    dom/CSSMappedAttributeDeclaration.cpp
     dom/CustomEvent.cpp
     dom/DatasetDOMStringMap.cpp
     dom/DecodedDataDocumentParser.cpp

Source/WebCore/GNUmakefile.list.am
@@ -1474,8 +1474,6 @@ webcore_sources += \
 	Source/WebCore/dom/ContainerNode.cpp \
 	Source/WebCore/dom/ContainerNode.h \
 	Source/WebCore/dom/CrossThreadTask.h \
-	Source/WebCore/dom/CSSMappedAttributeDeclaration.cpp \
-	Source/WebCore/dom/CSSMappedAttributeDeclaration.h \
 	Source/WebCore/dom/CustomEvent.cpp \
 	Source/WebCore/dom/CustomEvent.h \
 	Source/WebCore/dom/DatasetDOMStringMap.cpp \

Source/WebCore/Target.pri
@@ -484,7 +484,6 @@ SOURCES += \
     dom/Comment.cpp \
     dom/CompositionEvent.cpp \
     dom/ContainerNode.cpp \
-    dom/CSSMappedAttributeDeclaration.cpp \
     dom/CustomEvent.cpp \
     dom/DecodedDataDocumentParser.cpp \
     dom/DeviceMotionController.cpp \
@@ -1596,7 +1595,6 @@ HEADERS += \
     dom/Clipboard.h \
     dom/Comment.h \
     dom/ContainerNode.h \
-    dom/CSSMappedAttributeDeclaration.h \
     dom/CustomEvent.h \
     dom/default/PlatformMessagePortChannel.h \
     dom/DeviceMotionClient.h \

Source/WebCore/WebCore.gypi
@@ -575,7 +575,6 @@
             'dom/ActiveDOMObject.h',
             'dom/Attr.h',
             'dom/Attribute.h',
-            'dom/CSSMappedAttributeDeclaration.h',
             'dom/CharacterData.h',
             'dom/CheckedRadioButtons.h',
             'dom/Clipboard.h',
@@ -4928,7 +4927,6 @@
             'dom/BeforeUnloadEvent.h',
             'dom/CDATASection.cpp',
             'dom/CDATASection.h',
-            'dom/CSSMappedAttributeDeclaration.cpp',
             'dom/CharacterData.cpp',
             'dom/CheckedRadioButtons.cpp',
             'dom/ChildListMutationScope.cpp',

Source/WebCore/WebCore.order
@@ -4915,7 +4915,6 @@ __ZN7WebCore16HTMLTableElement29additionalAttributeStyleDeclsERN3WTF6VectorIPNS_
 __ZN7WebCore13StyledElement22getMappedAttributeDeclENS_20MappedAttributeEntryERKNS_13QualifiedNameERKN3WTF12AtomicStringE
 __ZN7WebCore26CSSMutableStyleDeclaration11setPropertyEiibb
 __ZN7WebCore26CSSMutableStyleDeclaration19setPropertyInternalERKNS_11CSSPropertyEPS1_
-__ZN7WebCore13StyledElement22setMappedAttributeDeclENS_20MappedAttributeEntryERKNS_13QualifiedNameERKN3WTF12AtomicStringEPNS_29CSSMappedAttributeDeclarationE
 __ZN3WTF6VectorIPN7WebCore26CSSMutableStyleDeclarationELm0EE14expandCapacityEm
 __ZN7WebCore11RenderStyle26setHorizontalBorderSpacingEs
 __ZN7WebCore11RenderStyle24setVerticalBorderSpacingEs
@@ -5366,7 +5365,6 @@ __ZNK7WebCore25AccessibilityRenderObject16linkedUIElementsERN3WTF6VectorINS1_6Re
 __ZNK7WebCore25AccessibilityRenderObject18ariaFlowToElementsERN3WTF6VectorINS1_6RefPtrINS_19AccessibilityObjectEEELm0EEE
 __ZN3WTF6VectorINS_6RefPtrIN7WebCore19AccessibilityObjectEEELm0EED1Ev
 __ZN7WebCore16HTMLTableElementD0Ev
-__ZN7WebCore29CSSMappedAttributeDeclarationD0Ev
 __ZN7WebCore13StyledElement25removeMappedAttributeDeclENS_20MappedAttributeEntryERKNS_13QualifiedNameERKN3WTF12AtomicStringE
 __ZN7WebCore26CSSMutableStyleDeclarationD2Ev
 __ZN7WebCore17HTMLSelectElementD0Ev
@@ -7918,10 +7916,6 @@ __ZNK7WebCore17CSSPrimitiveValue14getDoubleValueEt
 __ZNK7WebCore17CSSPrimitiveValue22getDoubleValueInternalENS0_9UnitTypesEPd
 __ZN7WebCore23HTMLTableCaptionElementD0Ev
 __ZN7WebCore13StyledElement11addCSSColorEPNS_9AttributeEiRKN3WTF6StringE
-__ZN3WTF9HashTableIN7WebCore18MappedAttributeKeyESt4pairIS2_PNS1_29CSSMappedAttributeDeclarationEENS_18PairFirstExtractorIS6_EENS1_19MappedAttributeHashENS_14PairHashTraitsINS1_24MappedAttributeKeyTraitsENS_10HashTraitsIS5_EEEESB_E16lookupForWritingIS2_NS_22IdentityHashTranslatorIS2_S6_S9_EEEES3_IPS6_bERKT_
-__ZN3WTF9HashTableIN7WebCore18MappedAttributeKeyESt4pairIS2_PNS1_29CSSMappedAttributeDeclarationEENS_18PairFirstExtractorIS6_EENS1_19MappedAttributeHashENS_14PairHashTraitsINS1_24MappedAttributeKeyTraitsENS_10HashTraitsIS5_EEEESB_E3addIS2_S5_NS_17HashMapTranslatorIS6_SE_S9_EEEES3_INS_17HashTableIteratorIS2_S6_S8_S9_SE_SB_EEbERKT_RKT0_
-__ZN3WTF9HashTableIN7WebCore18MappedAttributeKeyESt4pairIS2_PNS1_29CSSMappedAttributeDeclarationEENS_18PairFirstExtractorIS6_EENS1_19MappedAttributeHashENS_14PairHashTraitsINS1_24MappedAttributeKeyTraitsENS_10HashTraitsIS5_EEEESB_E6lookupIS2_NS_22IdentityHashTranslatorIS2_S6_S9_EEEEPS6_RKT_
-__ZN3WTF9HashTableIN7WebCore18MappedAttributeKeyESt4pairIS2_PNS1_29CSSMappedAttributeDeclarationEENS_18PairFirstExtractorIS6_EENS1_19MappedAttributeHashENS_14PairHashTraitsINS1_24MappedAttributeKeyTraitsENS_10HashTraitsIS5_EEEESB_E6rehashEi
 __ZN7WebCore16HTMLStyleElement20parseMappedAttributeEPNS_9AttributeE
 __ZN7WebCore15FontFamilyValueC1ERKN3WTF6StringE
 __ZN7WebCore15FontFamilyValueC2ERKN3WTF6StringE

commit 1a9f6f5b6d0b2aadef7d4aed798c4965ba2913ed

REGRESSION(r106756): 10% performance hit on DOM/Template. <http://webkit.org/b/77831>
Reviewed by Ryosuke Niwa.

Let the StylePropertySet used for element attribute style have the element as its parent.
This is accomplished by adding an m_parentIsElement bit to StylePropertySet and sharing
some of the internal logic with inline styles.

In the end, this means that CSSParser will now pick up the document's CSSValuePool when
parsing properties for attribute styles, which fixes the perf regression from r106756.

* css/StylePropertySet.cpp:
(WebCore::StylePropertySet::StylePropertySet):
(WebCore::StylePropertySet::contextStyleSheet):

    Find contextStyleSheet via the parentElement() when there is one.

(WebCore::StylePropertySet::setNeedsStyleRecalc):

    Always set FullStyleChange for attribute style mutations. We can probably use the
    same lighter invalidation as inline styles, but that's a topic for another patch.

* css/StylePropertySet.h:
(WebCore::StylePropertySet::createInline):
(WebCore::StylePropertySet::createAttributeStyle):
(WebCore::StylePropertySet::parentRuleInternal):
(WebCore::StylePropertySet::clearParentRule):
(StylePropertySet):
(WebCore::StylePropertySet::parentElement):
(WebCore::StylePropertySet::clearParentElement):

    Added m_parentIsElement bit and update assertions as appropriate to not just
    cover the inline style case. Added a createAttributeStyle() helper to create
    a StylePropertySet for use as Element::attributeStyle().

* dom/StyledElement.h:
* dom/ElementAttributeData.h:
* dom/ElementAttributeData.cpp:
(WebCore::ElementAttributeData::ensureAttributeStyle):

    Use StylePropertySet::createAttributeStyle().

* dom/StyledElement.cpp:
(WebCore::StyledElement::removeCSSProperties):
(WebCore::StyledElement::addCSSProperty):
(WebCore::StyledElement::addCSSImageProperty):

    Remove setNeedsStyleRecalc() calls since that is now handled automatically by
    StylePropertySet's mutation methods.

Source/WebCore/css/StylePropertySet.cpp
@@ -169,6 +169,7 @@ bool StyleAttributeMutationScope::s_shouldDeliver = false;
 
 StylePropertySet::StylePropertySet()
     : m_strictParsing(false)
+    , m_parentIsElement(false)
     , m_isInlineStyleDeclaration(false)
     , m_parent(static_cast<CSSRule*>(0))
 {
@@ -176,6 +177,7 @@ StylePropertySet::StylePropertySet()
 
 StylePropertySet::StylePropertySet(CSSRule* parentRule)
     : m_strictParsing(!parentRule || parentRule->useStrictParsing())
+    , m_parentIsElement(false)
     , m_isInlineStyleDeclaration(false)
     , m_parent(parentRule)
 {
@@ -184,6 +186,7 @@ StylePropertySet::StylePropertySet(CSSRule* parentRule)
 StylePropertySet::StylePropertySet(CSSRule* parentRule, const Vector<CSSProperty>& properties)
     : m_properties(properties)
     , m_strictParsing(!parentRule || parentRule->useStrictParsing())
+    , m_parentIsElement(false)
     , m_isInlineStyleDeclaration(false)
     , m_parent(parentRule)
 {
@@ -192,6 +195,7 @@ StylePropertySet::StylePropertySet(CSSRule* parentRule, const Vector<CSSProperty
 
 StylePropertySet::StylePropertySet(CSSRule* parentRule, const CSSProperty* const * properties, int numProperties)
     : m_strictParsing(!parentRule || parentRule->useStrictParsing())
+    , m_parentIsElement(false)
     , m_isInlineStyleDeclaration(false)
     , m_parent(parentRule)
 {
@@ -214,11 +218,12 @@ StylePropertySet::StylePropertySet(CSSRule* parentRule, const CSSProperty* const
     }
 }
 
-StylePropertySet::StylePropertySet(StyledElement* parentElement) 
+StylePropertySet::StylePropertySet(StyledElement* parentElement, bool isInlineStyle)
     : m_strictParsing(false)
-    , m_isInlineStyleDeclaration(true)
+    , m_parentIsElement(true)
+    , m_isInlineStyleDeclaration(isInlineStyle)
     , m_parent(parentElement)
-{ 
+{
 }
 
 StylePropertySet::~StylePropertySet()
@@ -239,7 +244,7 @@ void StylePropertySet::deref()
 
 CSSStyleSheet* StylePropertySet::contextStyleSheet() const
 {
-    if (m_isInlineStyleDeclaration) {
+    if (m_parentIsElement) {
         Document* document = m_parent.element ? m_parent.element->document() : 0;
         return document ? document->elementSheet() : 0;
     }
@@ -705,10 +710,16 @@ String StylePropertySet::removeProperty(int propertyID, bool notifyChanged, bool
 
 void StylePropertySet::setNeedsStyleRecalc()
 {
-    if (isInlineStyleDeclaration()) {
+    if (m_parentIsElement) {
         StyledElement* element = parentElement();
         if (!element)
             return;
+
+        if (!m_isInlineStyleDeclaration) {
+            element->setNeedsStyleRecalc();
+            return;
+        }
+
         element->setNeedsStyleRecalc(InlineStyleChange);
         element->invalidateStyleAttribute();
         StyleAttributeMutationScope(this).didInvalidateStyleAttr();

Source/WebCore/css/StylePropertySet.h
@@ -55,8 +55,12 @@ public:
         return adoptRef(new StylePropertySet(0, properties));
     }
     static PassRefPtr<StylePropertySet> createInline(StyledElement* element)
-    { 
-        return adoptRef(new StylePropertySet(element));
+    {
+        return adoptRef(new StylePropertySet(element, /*isInlineStyle*/ true));
+    }
+    static PassRefPtr<StylePropertySet> createAttributeStyle(StyledElement* element)
+    {
+        return adoptRef(new StylePropertySet(element, /*isInlineStyle*/ false));
     }
 
     void deref();
@@ -109,12 +113,12 @@ public:
 
     PassRefPtr<StylePropertySet> copyPropertiesInSet(const int* set, unsigned length) const;
 
-    CSSRule* parentRuleInternal() const { return m_isInlineStyleDeclaration ? 0 : m_parent.rule; }
-    void clearParentRule() { ASSERT(!m_isInlineStyleDeclaration); m_parent.rule = 0; }
-    
-    StyledElement* parentElement() const { ASSERT(m_isInlineStyleDeclaration); return m_parent.element; }
-    void clearParentElement() { ASSERT(m_isInlineStyleDeclaration); m_parent.element = 0; }
-    
+    CSSRule* parentRuleInternal() const { return m_parentIsElement ? 0 : m_parent.rule; }
+    void clearParentRule() { ASSERT(!m_parentIsElement); m_parent.rule = 0; }
+
+    StyledElement* parentElement() const { ASSERT(m_parentIsElement); return m_parent.element; }
+    void clearParentElement() { ASSERT(m_parentIsElement); m_parent.element = 0; }
+
     CSSStyleSheet* contextStyleSheet() const;
     
     String asText() const;
@@ -126,7 +130,7 @@ private:
     StylePropertySet(CSSRule* parentRule);
     StylePropertySet(CSSRule* parentRule, const Vector<CSSProperty>&);
     StylePropertySet(CSSRule* parentRule, const CSSProperty* const *, int numProperties);
-    StylePropertySet(StyledElement*);
+    StylePropertySet(StyledElement*, bool isInlineStyle);
 
     void setNeedsStyleRecalc();
 
@@ -155,6 +159,7 @@ private:
     Vector<CSSProperty, 4> m_properties;
 
     bool m_strictParsing : 1;
+    bool m_parentIsElement : 1;
     bool m_isInlineStyleDeclaration : 1;
 
     union Parent {

Source/WebCore/dom/ElementAttributeData.cpp
@@ -53,12 +53,10 @@ void ElementAttributeData::destroyInlineStyleDecl()
     m_inlineStyleDecl = 0;
 }
 
-StylePropertySet* ElementAttributeData::ensureAttributeStyle()
+StylePropertySet* ElementAttributeData::ensureAttributeStyle(StyledElement* element)
 {
-    if (!m_attributeStyle) {
-        m_attributeStyle = StylePropertySet::create();
-        m_attributeStyle->setStrictParsing(false);
-    }
+    if (!m_attributeStyle)
+        m_attributeStyle = StylePropertySet::createAttributeStyle(element);
     return m_attributeStyle.get();
 }
 

Source/WebCore/dom/ElementAttributeData.h
@@ -47,7 +47,7 @@ public:
     void destroyInlineStyleDecl();
 
     StylePropertySet* attributeStyle() const { return m_attributeStyle.get(); }
-    StylePropertySet* ensureAttributeStyle();
+    StylePropertySet* ensureAttributeStyle(StyledElement*);
 
 private:
     friend class NamedNodeMap;

Source/WebCore/dom/StyledElement.cpp
@@ -110,8 +110,6 @@ void StyledElement::removeCSSProperties(int id1, int id2, int id3, int id4, int 
     if (!style)
         return;
 
-    setNeedsStyleRecalc(FullStyleChange);
-
     ASSERT(id1 != CSSPropertyInvalid);
     style->removeProperty(id1);
 
@@ -142,20 +140,16 @@ void StyledElement::addCSSProperty(int id, const String &value)
 {
     if (!ensureAttributeStyle()->setProperty(id, value))
         removeCSSProperty(id);
-    else
-        setNeedsStyleRecalc(FullStyleChange);
 }
 
 void StyledElement::addCSSProperty(int id, int value)
 {
     ensureAttributeStyle()->setProperty(id, value);
-    setNeedsStyleRecalc(FullStyleChange);
 }
 
 void StyledElement::addCSSImageProperty(int id, const String& url)
 {
     ensureAttributeStyle()->setProperty(CSSProperty(id, CSSImageValue::create(url)));
-    setNeedsStyleRecalc(FullStyleChange);
 }
 
 void StyledElement::addCSSLength(int id, const String &value)

Source/WebCore/dom/StyledElement.h
@@ -52,7 +52,7 @@ public:
     virtual CSSStyleDeclaration* style() OVERRIDE { return ensureInlineStyleDecl()->ensureCSSStyleDeclaration(); }
 
     StylePropertySet* attributeStyle() const { return attributeData() ? attributeData()->attributeStyle() : 0; }
-    StylePropertySet* ensureAttributeStyle() { return ensureAttributeData()->ensureAttributeStyle(); }
+    StylePropertySet* ensureAttributeStyle() { return ensureAttributeData()->ensureAttributeStyle(this); }
 
     const SpaceSplitString& classNames() const;
 