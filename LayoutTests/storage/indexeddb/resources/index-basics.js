de2c597e4ac7f917a22403f5b9a18a8b1de3dbea
David Grogan <dgrogan@chromium.org>
2012-02-07 01:39:09 +0000

    Port some indexeddb layout tests to workers. https://bugs.webkit.org/show_bug.cgi?id=77805
    Reviewed by Tony Chang.
    
    * storage/indexeddb/index-basics-workers-expected.txt: Added.
    * storage/indexeddb/index-basics-workers.html: Added.
    * storage/indexeddb/index-basics.html:
    * storage/indexeddb/objectstore-basics-expected.txt:
    * storage/indexeddb/objectstore-basics-workers-expected.txt: Added.
    * storage/indexeddb/objectstore-basics-workers.html: Added.
    * storage/indexeddb/objectstore-basics.html:
    * storage/indexeddb/resources/index-basics.js: Copied from LayoutTests/storage/indexeddb/index-basics.html.
    (test):
    (setVersion):
    (deleteExisting):
    (addData):
    (addData2):
    (addData3):
    (getData):
    (getObjectData):
    (getObjectData2):
    (getObjectData3):
    (getDataFail):
    (getObjectDataFail):
    (getObjectData4):
    (openKeyCursor):
    (cursor1Continue):
    (cursor1Continue2):
    (cursor1Continue3):
    (openObjectCursor):
    (cursor2Continue):
    (cursor2Continue2):
    (cursor2Continue3):
    (last.try.get testFailed):
    (last):
    * storage/indexeddb/resources/objectstore-basics.js: Copied from LayoutTests/storage/indexeddb/objectstore-basics.html.
    (test):
    (openSuccess):
    (setVersionSuccess):
    (createIndex):
    (testSetVersionAbort):
    (createAnotherIndex):
    (checkMetadata):
    (addData):
    (addDateSuccess):
    (addSuccess):
    (addAgainFailure):
    (getSuccess):
    (getSuccessDateKey):
    (removeSuccess):
    (removeSuccessButFalse):
    (testPreConditions.request.onsuccess):
    (testPreConditions):
    
    
    Canonical link: https://commits.webkit.org/94804@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@106886 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/storage/indexeddb/resources/index-basics.js
@@ -0,0 +1,280 @@
+if (this.importScripts) {
+    importScripts('../../../fast/js/resources/js-test-pre.js');
+    importScripts('idb-worker-common.js');
+    importScripts('shared.js');
+}
+
+description("Test the basics of IndexedDB's webkitIDBIndex.");
+
+function test()
+{
+    request = evalAndLog("webkitIndexedDB.open('index-basics')");
+    request.onsuccess = setVersion;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function setVersion(evt)
+{
+    event = evt;
+    db = evalAndLog("db = event.target.result");
+
+    request = evalAndLog("db.setVersion('new version')");
+    request.onsuccess = deleteExisting;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function deleteExisting(evt)
+{
+    event = evt;
+    debug("setVersionSuccess():");
+    self.trans = evalAndLog("trans = event.target.result");
+    shouldBeTrue("trans !== null");
+    trans.onabort = unexpectedAbortCallback;
+
+    deleteAllObjectStores(db);
+
+    self.store = evalAndLog("db.createObjectStore('storeName', null)");
+    self.indexObject = evalAndLog("store.createIndex('indexName', 'x')");
+    self.indexObject2 = evalAndLog("store.createIndex('indexName2', 'y', {unique: false})");
+    self.indexObject3 = evalAndLog("store.createIndex('zIndex', 'z', {unique: true})");
+    addData();
+}
+
+function addData()
+{
+    shouldBeTrue("'name' in indexObject");
+    shouldBeEqualToString("indexObject.name", "indexName");
+    shouldBeTrue("'objectStore' in indexObject");
+    shouldBeEqualToString("indexObject.objectStore.name", "storeName");
+    shouldBeTrue("'keyPath' in indexObject");
+    shouldBeEqualToString("indexObject.keyPath", "x");
+    shouldBeTrue("'unique' in indexObject");
+    shouldBeTrue("'multiEntry' in indexObject");
+    shouldBeFalse("indexObject.unique");
+    shouldBeFalse("indexObject.multiEntry");
+    shouldBeTrue("'openKeyCursor' in indexObject");
+    shouldBeEqualToString("typeof indexObject.openKeyCursor", "function");
+    shouldBeTrue("'openCursor' in indexObject");
+    shouldBeEqualToString("typeof indexObject.openCursor", "function");
+    shouldBeTrue("'getKey' in indexObject");
+    shouldBeEqualToString("typeof indexObject.getKey", "function");
+    shouldBeTrue("'get' in indexObject");
+    shouldBeEqualToString("typeof indexObject.get", "function");
+    shouldBeTrue("'count' in indexObject");
+    shouldBeEqualToString("typeof indexObject.count", "function");
+
+    request = evalAndLog("store.add({x: 'value', y: 'zzz', z: 2.72}, 'key')");
+    request.onsuccess = addData2;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function addData2(evt)
+{
+    event = evt;
+    request = evalAndLog("event.target.source.add({x: 'value2', y: 'zzz2', z: 2.71, foobar: 12}, 'key2')");
+    request.onsuccess = addData3;
+    request.onerror = unexpectedErrorCallback;
+    self.indexObject4 = evalAndLog("store.createIndex('indexWhileAddIsInFlight', 'x')");
+    self.indexObject5 = evalAndLog("store.createIndex('indexWithWeirdKeyPath', 'foobar')");
+}
+
+function addData3(evt)
+{
+    event = evt;
+    // Add data which doesn't have a key in the zIndex.
+    request = evalAndLog("event.target.source.add({x: 'value3', y: '456'}, 'key3')");
+    request.onsuccess = getData;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function getData(evt)
+{
+    event = evt;
+    request = evalAndLog("indexObject.getKey('value')");
+    request.onsuccess = getObjectData;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function getObjectData(evt)
+{
+    event = evt;
+    shouldBeEqualToString("event.target.result", "key");
+
+    request = evalAndLog("indexObject2.getKey('zzz')");
+    request.onsuccess = getObjectData2;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function getObjectData2(evt)
+{
+    event = evt;
+    shouldBeEqualToString("event.target.result", "key");
+
+    request = evalAndLog("indexObject3.get(2.71)");
+    request.onsuccess = getObjectData3;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function getObjectData3(evt)
+{
+    event = evt;
+    shouldBeEqualToString("event.target.result.x", "value2");
+
+    request = evalAndLog("indexObject.get('value')");
+    request.onsuccess = getDataFail;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function getDataFail(evt)
+{
+    event = evt;
+    shouldBeEqualToString("event.target.result.x", "value");
+    shouldBeEqualToString("event.target.result.y", "zzz");
+
+    request = evalAndLog("indexObject.getKey('does not exist')");
+    request.onsuccess = getObjectDataFail;
+    request.onerror = unexpectedSuccessCallback;
+}
+
+function getObjectDataFail(evt)
+{
+    event = evt;
+    shouldBe("event.target.result", "undefined");
+
+    request = evalAndLog("indexObject.get('does not exist')");
+    request.onsuccess = getObjectData4;
+    request.onerror = unexpectedSuccessCallback;
+}
+
+function getObjectData4(evt)
+{
+    event = evt;
+    shouldBe("event.target.result", "undefined");
+
+    request = evalAndLog("indexObject4.getKey('value2')");
+    request.onsuccess = openKeyCursor;
+    request.onerror =  unexpectedErrorCallback;
+}
+
+function openKeyCursor(evt)
+{
+    event = evt;
+    shouldBeEqualToString("event.target.result", "key2");
+
+    self.request = evalAndLog("indexObject.openKeyCursor()");
+    request.onsuccess = cursor1Continue;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function cursor1Continue(evt)
+{
+    event = evt;
+    shouldBe("event.target.source", "indexObject");
+    shouldBeFalse("event.target.result === null");
+    shouldBeEqualToString("event.target.result.key", "value");
+    shouldBeEqualToString("event.target.result.primaryKey", "key");
+
+    // We re-use the last request object.
+    evalAndLog("event.target.result.continue()");
+    self.request.onsuccess = cursor1Continue2;
+}
+
+function cursor1Continue2(evt)
+{
+    event = evt;
+    shouldBeFalse("event.target.result === null");
+    shouldBeEqualToString("event.target.result.key", "value2");
+    shouldBeEqualToString("event.target.result.primaryKey", "key2");
+
+    // We re-use the last request object.
+    evalAndLog("event.target.result.continue()");
+    self.request.onsuccess = cursor1Continue3;
+}
+
+function cursor1Continue3(evt)
+{
+    event = evt;
+    shouldBeFalse("event.target.result === null");
+    shouldBeEqualToString("event.target.result.key", "value3");
+    shouldBeEqualToString("event.target.result.primaryKey", "key3");
+
+    // We re-use the last request object.
+    evalAndLog("event.target.result.continue()");
+    self.request.onsuccess = openObjectCursor;
+}
+
+function openObjectCursor(evt)
+{
+    event = evt;
+    shouldBeTrue("event.target.result === null");
+
+    self.request = evalAndLog("indexObject.openCursor()");
+    request.onsuccess = cursor2Continue;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function cursor2Continue(evt)
+{
+    event = evt;
+    shouldBe("event.target.source", "indexObject");
+    shouldBeFalse("event.target.result === null");
+    shouldBeEqualToString("event.target.result.key", "value");
+    shouldBeEqualToString("event.target.result.value.x", "value");
+    shouldBeEqualToString("event.target.result.value.y", "zzz");
+
+    // We re-use the last request object.
+    evalAndLog("event.target.result.continue()");
+    self.request.onsuccess = cursor2Continue2;
+}
+
+function cursor2Continue2(evt)
+{
+    event = evt;
+    shouldBeFalse("event.target.result === null");
+    shouldBeEqualToString("event.target.result.key", "value2");
+    shouldBeEqualToString("event.target.result.value.x", "value2");
+    shouldBeEqualToString("event.target.result.value.y", "zzz2");
+
+    // We re-use the last request object.
+    evalAndLog("event.target.result.continue()");
+    self.request.onsuccess = cursor2Continue3;
+}
+
+function cursor2Continue3(evt)
+{
+    event = evt;
+    shouldBeFalse("event.target.result === null");
+    shouldBeEqualToString("event.target.result.key", "value3");
+    shouldBeEqualToString("event.target.result.value.x", "value3");
+    shouldBeEqualToString("event.target.result.value.y", "456");
+
+    // We re-use the last request object.
+    evalAndLog("event.target.result.continue()");
+    self.request.onsuccess = last;
+}
+
+function last(evt)
+{
+    event = evt;
+    shouldBeTrue("event.target.result === null");
+
+    try {
+        debug("Passing an invalid key into indexObject.get({}).");
+        indexObject.get({});
+        testFailed("No exception thrown");
+    } catch (e) {
+        testPassed("Caught exception: " + e.toString());
+    }
+
+    try {
+        debug("Passing an invalid key into indexObject.getKey({}).");
+        indexObject.getKey({});
+        testFailed("No exception thrown");
+    } catch (e) {
+        testPassed("Caught exception: " + e.toString());
+    }
+    finishJSTest();
+}
+
+var jsTestIsAsync = true;
+test();
---
+++ LayoutTests/storage/indexeddb/resources/objectstore-basics.js
@@ -0,0 +1,339 @@
+if (this.importScripts) {
+    importScripts('../../../fast/js/resources/js-test-pre.js');
+    importScripts('idb-worker-common.js');
+    importScripts('shared.js');
+}
+
+description("Test the basics of IndexedDB's IDBObjectStore.");
+
+function test()
+{
+    request = evalAndLog("webkitIndexedDB.open('objectstore-basics')");
+    request.onsuccess = openSuccess;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function openSuccess(evt)
+{
+    event = evt;
+    debug("openSuccess():");
+    self.db = evalAndLog("db = event.target.result");
+
+    request = evalAndLog("db.setVersion('new version')");
+    request.onsuccess = setVersionSuccess;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function setVersionSuccess(evt)
+{
+    event = evt;
+    debug("setVersionSuccess():");
+    self.trans = evalAndLog("trans = event.target.result");
+    shouldBeTrue("trans !== null");
+    trans.onabort = unexpectedAbortCallback;
+
+    deleteAllObjectStores(db);
+
+    debug("createObjectStore():");
+    self.store = evalAndLog("store = db.createObjectStore('storeName', null)");
+    var storeNames = evalAndLog("storeNames = db.objectStoreNames");
+
+    shouldBeTrue("'name' in store");
+    shouldBeTrue("'keyPath' in store");
+    shouldBeTrue("'indexNames' in store");
+    shouldBeTrue("'transaction' in store");
+    shouldBeTrue("'put' in store");
+    shouldBeEqualToString("typeof store.put", "function");
+    shouldBeTrue("'add' in store");
+    shouldBeEqualToString("typeof store.add", "function");
+    shouldBeTrue("'delete' in store");
+    shouldBeEqualToString("typeof store.delete", "function");
+    shouldBeTrue("'get' in store");
+    shouldBeEqualToString("typeof store.get", "function");
+    shouldBeTrue("'clear' in store");
+    shouldBeEqualToString("typeof store.clear", "function");
+    shouldBeTrue("'openCursor' in store");
+    shouldBeEqualToString("typeof store.openCursor", "function");
+    shouldBeTrue("'createIndex' in store");
+    shouldBeEqualToString("typeof store.createIndex", "function");
+    shouldBeTrue("'index' in store");
+    shouldBeEqualToString("typeof store.index", "function");
+    shouldBeTrue("'deleteIndex' in store");
+    shouldBeEqualToString("typeof store.deleteIndex", "function");
+    shouldBeTrue("'count' in store");
+    shouldBeEqualToString("typeof store.count", "function");
+
+    shouldBeEqualToString("store.name", "storeName");
+    shouldBeNull("store.keyPath");
+    shouldBe("storeNames.contains('storeName')", "true");
+    shouldBe("storeNames.length", "1");
+    // FIXME: test all of object store's methods.
+
+    debug("Ask for an index that doesn't exist:");
+    try {
+        debug("index = store.index('asdf')");
+        index = store.index('asdf');
+        testFailed("Asking for a store that doesn't exist should have thrown.");
+    } catch (err) {
+        testPassed("Exception thrown.");
+        code = err.code;
+        shouldBe("code", "webkitIDBDatabaseException.NOT_FOUND_ERR");
+    }
+
+    createIndex();
+}
+
+function createIndex()
+{
+    debug("createIndex():");
+    var index = evalAndLog("index = store.createIndex('indexName', 'x', {unique: true})"); // true == unique requirement.
+    shouldBeTrue("index !== null");
+    shouldBeTrue("store.indexNames.contains('indexName')");
+    index = evalAndLog("index = store.index('indexName')");
+    shouldBeTrue("index !== null");
+ 
+    debug("Ask for an index that doesn't exist:");
+    try {
+        debug("index = store.index('asdf')");
+        index = store.index('asdf');
+        testFailed("Asking for a store that doesn't exist should have thrown.");
+    } catch (err) {
+        testPassed("Exception thrown.");
+        code = err.code
+        shouldBe("code", "webkitIDBDatabaseException.NOT_FOUND_ERR");
+    }
+    trans.oncomplete = testSetVersionAbort;
+}
+
+function testSetVersionAbort()
+{
+    request = evalAndLog('db.setVersion("version fail")');
+    request.onsuccess = createAnotherIndex;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function createAnotherIndex(evt)
+{
+    event = evt;
+    shouldBeEqualToString("db.version", "version fail");
+
+    var setVersionTrans = evalAndLog("setVersionTrans = event.target.result");
+    shouldBeTrue("setVersionTrans !== null");
+    setVersionTrans.oncomplete = unexpectedCompleteCallback;
+    setVersionTrans.onabort = checkMetadata;
+    self.store = evalAndLog("store = setVersionTrans.objectStore('storeName')");
+    var index = evalAndLog("index = store.createIndex('indexFail', 'x')");
+
+    setVersionTrans.abort();
+}
+
+function checkMetadata()
+{
+    shouldBeEqualToString("db.version", "new version");
+    shouldBe("store.transaction", "setVersionTrans");
+    shouldBe("store.indexNames", "['indexName']");
+    shouldBe("store.indexNames.length", "1");
+    shouldBe("store.indexNames.contains('')", "false");
+    shouldBe("store.indexNames.contains('indexFail')", "false");
+    shouldBe("store.indexNames.contains('indexName')", "true");
+    shouldBeEqualToString("store.indexNames[0]", "indexName");
+    shouldBeNull("store.indexNames[1]");
+    shouldBeNull("store.indexNames[100]");
+    shouldBeNull("store.indexNames.item(1)");
+    shouldBeNull("store.indexNames.item(100)");
+    addData();
+}
+
+var testDate = new Date("August 25, 1991 20:57:08");
+var testDateB = new Date("Wed Jan 05 2011 15:54:49");
+
+function addData()
+{
+    var transaction = evalAndLog("transaction = db.transaction(['storeName'], webkitIDBTransaction.READ_WRITE)");
+    transaction.onabort = unexpectedAbortCallback;
+    self.store = evalAndLog("store = transaction.objectStore('storeName')");
+
+    debug("Try to insert data with a Date key:");
+    request = evalAndLog("store.add({x: 'foo'}, testDate)");
+    request.onsuccess = addDateSuccess;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function addDateSuccess(evt)
+{
+    event = evt;
+    debug("Try to insert a value not handled by structured clone:");
+    try {
+        debug("store.add({x: 'bar', y: self}, 'bar')");
+        store.add({x: 'bar', y: self}, 'bar');
+        testFailed("Passing a DOM node as value should have thrown.");
+    } catch (err) {
+        testPassed("Exception thrown");
+        code = err.code;
+        // FIXME: When we move to DOM4-style exceptions this should look for the
+        // name "DataCloneError".
+        shouldBe("code", "25");
+    }
+
+    debug("Try to insert data where key path yields a Date key:");
+    request = evalAndLog("store.add({x: testDateB, y: 'value'}, 'key')");
+    request.onsuccess = addSuccess;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function addSuccess(evt)
+{
+    event = evt;
+    debug("addSuccess():");
+    shouldBeEqualToString("event.target.result", "key");
+
+    request = evalAndLog("event.target.source.add({x: 'foo'}, 'zzz')");
+    request.onsuccess = unexpectedSuccessCallback;
+    request.addEventListener('error', addAgainFailure, false);
+}
+
+function addAgainFailure(evt)
+{
+    event = evt;
+    debug("addAgainFailure():");
+    shouldBe("event.target.errorCode", "webkitIDBDatabaseException.CONSTRAINT_ERR");
+
+    evalAndLog("event.preventDefault()");
+
+    transaction = evalAndLog("db.transaction(['storeName'], webkitIDBTransaction.READ_WRITE)");
+    transaction.onabort = unexpectedErrorCallback;
+    var store = evalAndLog("store = transaction.objectStore('storeName')");
+
+    evalAndLog("store.add({x: 'somevalue'}, 'somekey')");
+    evalAndExpectException("store.add({x: 'othervalue'}, null)", "webkitIDBDatabaseException.DATA_ERR");
+
+    transaction = evalAndLog("db.transaction(['storeName'], webkitIDBTransaction.READ_WRITE)");
+    transaction.onabort = unexpectedErrorCallback;
+    var store = evalAndLog("store = transaction.objectStore('storeName')");
+
+    evalAndExpectException("store.add({x: null}, 'validkey')", "webkitIDBDatabaseException.DATA_ERR");
+
+    transaction = evalAndLog("db.transaction(['storeName'], webkitIDBTransaction.READ_WRITE)");
+    transaction.onabort = unexpectedErrorCallback;
+    var store = evalAndLog("store = transaction.objectStore('storeName')");
+
+    request = evalAndLog("store.get('key')");
+    request.addEventListener('success', getSuccess, true);
+    request.onerror = unexpectedErrorCallback;
+}
+
+function getSuccess(evt)
+{
+    event = evt;
+    debug("getSuccess():");
+    shouldBeEqualToString("event.target.result.y", "value");
+
+    var store = evalAndLog("store = event.target.source");
+    request = evalAndLog("store.get(testDate)");
+    request.addEventListener('success', getSuccessDateKey, false);
+    request.onerror = unexpectedErrorCallback;
+}
+
+function getSuccessDateKey(evt)
+{
+    event = evt;
+    debug("getSuccessDateKey():");
+    shouldBeEqualToString("event.target.result.x", "foo");
+
+    request = evalAndLog("store.delete('key')");
+    request.onsuccess = removeSuccess;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function removeSuccess(evt)
+{
+    event = evt;
+    debug("removeSuccess():");
+    shouldBe("event.target.result", "true");
+
+    request = evalAndLog("store.delete('key')");
+    request.onsuccess = removeSuccessButFalse;
+    request.onerror = unexpectedErrorCallback;
+}
+
+function removeSuccessButFalse(evt)
+{
+    event = evt;
+    debug("removeSuccessButFalse():");
+    shouldBe("event.target.result", "false");
+    var store = evalAndLog("store = event.target.source");
+
+    debug("Passing an invalid key into store.get().");
+    evalAndExpectException("store.get({})", "webkitIDBDatabaseException.DATA_ERR");
+
+    debug("Passing an invalid key into store.delete().");
+    evalAndExpectException("store.delete({})", "webkitIDBDatabaseException.DATA_ERR");
+
+    debug("Passing an invalid key into store.add().");
+    evalAndExpectException("store.add(null, {})", "webkitIDBDatabaseException.DATA_ERR");
+
+    debug("Passing an invalid key into store.put().");
+    evalAndExpectException("store.put(null, {})", "webkitIDBDatabaseException.DATA_ERR");
+
+    testPreConditions();
+}
+
+function testPreConditions()
+{
+    debug("");
+    debug("testPreConditions():");
+    request = evalAndLog("db.setVersion('precondition version')");
+    request.onerror = unexpectedErrorCallback;
+    request.onsuccess = function() {
+        storeWithInLineKeys = evalAndLog("storeWithInLineKeys = db.createObjectStore('storeWithInLineKeys', {keyPath: 'key'})");
+        storeWithOutOfLineKeys = evalAndLog("storeWithOutOfLineKeys = db.createObjectStore('storeWithOutIOfLineKeys')");
+        storeWithIndex = evalAndLog("storeWithIndex = db.createObjectStore('storeWithIndex')");
+        index = evalAndLog("index = storeWithIndex.createIndex('indexName', 'indexKey')");
+
+        debug("");
+        debug("IDBObjectStore.put()");
+        debug("The object store uses in-line keys and the key parameter was provided.");
+        evalAndExpectException("storeWithInLineKeys.put({key: 1}, 'key')", "webkitIDBDatabaseException.DATA_ERR");
+
+        debug("The object store uses out-of-line keys and has no key generator and the key parameter was not provided.");
+        evalAndExpectException("storeWithOutOfLineKeys.put({})", "webkitIDBDatabaseException.DATA_ERR");
+
+        debug("The object store uses in-line keys and the result of evaluating the object store's key path yields a value and that value is not a valid key.");
+        evalAndExpectException("storeWithInLineKeys.put({key: null})", "webkitIDBDatabaseException.DATA_ERR");
+
+        debug("The object store uses in-line keys but no key generator and the result of evaluating the object store's key path does not yield a value.");
+        evalAndExpectException("storeWithInLineKeys.put({})", "webkitIDBDatabaseException.DATA_ERR");
+
+        debug("The key parameter was provided but does not contain a valid key.");
+        evalAndExpectException("storeWithOutOfLineKeys.put({}, null)", "webkitIDBDatabaseException.DATA_ERR");
+
+        debug("If there are any indexes referencing this object store whose key path is a string, evaluating their key path on the value parameter yields a value, and that value is not a valid key.");
+        evalAndExpectException("storeWithIndex.put({indexKey: null}, 'key')", "webkitIDBDatabaseException.DATA_ERR");
+
+        debug("");
+        debug("IDBObjectStore.add()");
+        debug("The object store uses in-line keys and the key parameter was provided.");
+        evalAndExpectException("storeWithInLineKeys.add({key: 1}, 'key')", "webkitIDBDatabaseException.DATA_ERR");
+
+        debug("The object store uses out-of-line keys and has no key generator and the key parameter was not provided.");
+        evalAndExpectException("storeWithOutOfLineKeys.add({})", "webkitIDBDatabaseException.DATA_ERR");
+
+        debug("The object store uses in-line keys and the result of evaluating the object store's key path yields a value and that value is not a valid key.");
+        evalAndExpectException("storeWithInLineKeys.add({key: null})", "webkitIDBDatabaseException.DATA_ERR");
+
+        debug("The object store uses in-line keys but no key generator and the result of evaluating the object store's key path does not yield a value.");
+        evalAndExpectException("storeWithInLineKeys.add({})", "webkitIDBDatabaseException.DATA_ERR");
+
+        debug("The key parameter was provided but does not contain a valid key.");
+        evalAndExpectException("storeWithOutOfLineKeys.add({}, null)", "webkitIDBDatabaseException.DATA_ERR");
+
+        debug("If there are any indexes referencing this object store whose key path is a string, evaluating their key path on the value parameter yields a value, and that value is not a valid key.");
+        evalAndExpectException("storeWithIndex.add({indexKey: null}, 'key')", "webkitIDBDatabaseException.DATA_ERR");
+
+        finishJSTest();
+    };
+}
+
+var jsTestIsAsync = true;
+
+test();
