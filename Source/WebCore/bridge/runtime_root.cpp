0ae7f963b5f4f2b163eb49fccc7ca4108b028c09
Mark Hahnenberg <mhahnenberg@apple.com>
2012-02-15 21:59:36 +0000

    RootObject::finalize can cause a crash in object->invalidate() https://bugs.webkit.org/show_bug.cgi?id=78645
    Reviewed by Geoffrey Garen.
    
    No new tests.
    
    * bridge/runtime_root.cpp:
    (JSC::Bindings::RootObject::finalize): Added a stack-allocated RefPtr to protect the RootObject
    during the call to invalidate().
    
    
    Canonical link: https://commits.webkit.org/95678@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@107837 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bridge/runtime_root.cpp
+++ Source/WebCore/bridge/runtime_root.cpp
@@ -199,6 +199,7 @@ void RootObject::finalize(JSC::Handle<JSC::Unknown> handle, void*)
     RuntimeObject* object = static_cast<RuntimeObject*>(asObject(handle.get()));
     ASSERT(m_runtimeObjects.contains(object));
 
+    RefPtr<RootObject> protect(this);
     object->invalidate();
     m_runtimeObjects.remove(object);
 }
