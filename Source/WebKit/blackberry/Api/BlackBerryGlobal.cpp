03f6b80c878fbba0bdaa6360157e70121f54e5b8
Rob Buis <rwlbuis@webkit.org>
2012-02-15 00:11:57 +0000

    [BlackBerry] Upstream remaining classes in Api https://bugs.webkit.org/show_bug.cgi?id=78644
    Reviewed by Antonio Gomes.
    
    Initial upstream, no new tests.
    
    * blackberry/Api/BlackBerryContext.h: Added.
    * blackberry/Api/BlackBerryGlobal.cpp: Added.
    * blackberry/Api/BlackBerryGlobal.h: Added.
    * blackberry/Api/Version.cpp: Added.
    * blackberry/Api/Version.h: Added.
    
    Canonical link: https://commits.webkit.org/95594@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@107750 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit/blackberry/Api/BlackBerryGlobal.cpp
@@ -0,0 +1,153 @@
+/*
+ * Copyright (C) 2009, 2010, 2011, 2012 Research In Motion Limited. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "config.h"
+#include "BlackBerryGlobal.h"
+
+#include "ApplicationCacheStorage.h"
+#include "CacheClientBlackBerry.h"
+#include "CookieManager.h"
+#include "CrossOriginPreflightResultCache.h"
+#include "FontCache.h"
+#include "ImageSource.h"
+#include "InitializeThreading.h"
+#include "JSDOMWindow.h"
+#include "JSGlobalData.h"
+#include "Logging.h"
+#include "MainThread.h"
+#include "MemoryCache.h"
+#include "NetworkStateNotifier.h"
+#include "PageCache.h"
+#include "PageGroup.h"
+#include "TextureCacheCompositingThread.h"
+#include "WebString.h"
+#include "bindings/js/GCController.h"
+#include "runtime/JSLock.h"
+#include <BlackBerryPlatformExecutableMessage.h>
+#include <BlackBerryPlatformMessageClient.h>
+#include <BlackBerryPlatformSettings.h>
+
+using namespace WebCore;
+
+namespace BlackBerry {
+namespace WebKit {
+
+static bool gIsGlobalInitialized = false;
+
+// Global initialization of various WebKit singletons.
+void globalInitialize()
+{
+    if (gIsGlobalInitialized)
+        return;
+    gIsGlobalInitialized = true;
+
+#if ENABLE(BLACKBERRY_DEBUG_MEMORY)
+    blackberryDebugInitialize();
+#endif
+
+    // Turn on logging.
+    initializeLoggingChannelsIfNecessary();
+
+    // Initialize threading/
+    JSC::initializeThreading();
+
+    // Normally this is called from initializeThreading, but we're using ThreadingNone
+    // we're grabbing callOnMainThread without using the rest of the threading support.
+    WTF::initializeMainThread();
+
+    // Track visited links.
+    PageGroup::setShouldTrackVisitedLinks(true);
+
+    CacheClientBlackBerry::get()->initialize();
+
+    BlackBerry::Platform::Settings* settings = BlackBerry::Platform::Settings::get();
+
+    ImageSource::setMaxPixelsPerDecodedImage(settings->maxPixelsPerDecodedImage());
+}
+
+void collectJavascriptGarbageNow()
+{
+    if (gIsGlobalInitialized)
+        gcController().garbageCollectNow();
+}
+
+void clearCookieCache()
+{
+    cookieManager().removeAllCookies(RemoveFromBackingStore);
+}
+
+#if USE(ACCELERATED_COMPOSITING)
+static void clearMemoryCachesInCompositingThread()
+{
+    textureCacheCompositingThread()->prune(0);
+}
+#endif
+
+void clearMemoryCaches()
+{
+#if USE(ACCELERATED_COMPOSITING)
+    // Call textureCacheCompositingThread()->prune(0) in UI thread.
+    BlackBerry::Platform::userInterfaceThreadMessageClient()->dispatchMessage(BlackBerry::Platform::createFunctionCallMessage(clearMemoryCachesInCompositingThread));
+#endif
+
+    {
+        JSC::JSLock lock(JSC::SilenceAssertionsOnly);
+        // This function also performs a GC.
+        JSC::releaseExecutableMemory(*JSDOMWindow::commonJSGlobalData());
+    }
+
+    // Clean caches after JS garbage collection because JS GC can
+    // generate more dead resources.
+    int capacity = pageCache()->capacity();
+    pageCache()->setCapacity(0);
+    pageCache()->setCapacity(capacity);
+    pageCache()->releaseAutoreleasedPagesNow();
+
+    CrossOriginPreflightResultCache::shared().empty();
+
+    if (!memoryCache()->disabled()) {
+        // Evict all dead resources and prune live resources.
+        memoryCache()->setCapacities(0, 0, 0);
+
+        // Update cache capacity based on current memory status.
+        CacheClientBlackBerry::get()->updateCacheCapacity();
+    }
+
+    fontCache()->invalidate();
+}
+
+void clearAppCache(const WebString& pageGroupName)
+{
+    cacheStorage().empty();
+}
+
+void clearLocalStorage(const WebString& pageGroupName)
+{
+}
+
+void clearDatabase(const WebString& pageGroupName)
+{
+}
+
+void updateOnlineStatus(bool online)
+{
+    networkStateNotifier().networkStateChange(online);
+}
+
+}
+}
