f177aa3a8b26514e282a9aa0bfeea13e0d8ad207
Patrick Gansterer <paroga@webkit.org>
2012-02-16 07:50:48 +0000

    Windows build fix for !ENABLE(CSS_FILTERS) after r106593.
    * platform/graphics/ca/win/PlatformCALayerWin.cpp:
    
    Canonical link: https://commits.webkit.org/95736@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@107900 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/ca/win/PlatformCALayerWin.cpp
+++ Source/WebCore/platform/graphics/ca/win/PlatformCALayerWin.cpp
@@ -566,6 +566,8 @@ void PlatformCALayer::setOpacity(float value)
     setNeedsCommit();
 }
 
+#if ENABLE(CSS_FILTERS)
+
 void PlatformCALayer::setFilters(const FilterOperations&)
 {
 }
@@ -574,6 +576,8 @@ bool PlatformCALayer::filtersCanBeComposited(const FilterOperations&)
 {
     return false;
 }
+
+#endif // ENABLE(CSS_FILTERS)
 
 String PlatformCALayer::name() const
 {
