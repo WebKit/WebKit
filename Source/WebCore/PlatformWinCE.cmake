2444c563b303ee618062a8739ecffcdce25d2987
Raphael Kubo da Costa <kubo@profusion.mobi>
2012-02-17 14:19:21 +0000

    [CMake, EFL] Unreviewed, fix the build when building with SHARED_CORE=ON after r107820.
    RunLoopEfl.cpp has not been upstreamed yet, and building only
    RunLoop.cpp created an .so with some missing, unimplemented
    symbols. The BlackBerry port seems to be in the same situation.
    
    The best solution for now is to build RunLoop.cpp only on the
    WinCE port.
    
    * CMakeLists.txt: Remove RunLoop.cpp from the list of files to build.
    * PlatformWinCE.cmake: Add RunLoop.cpp to the list of files to build.
    
    Canonical link: https://commits.webkit.org/95901@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@108067 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/PlatformWinCE.cmake
+++ Source/WebCore/PlatformWinCE.cmake
@@ -32,6 +32,7 @@ LIST(APPEND WebCore_SOURCES
     platform/Cursor.cpp
     platform/LocalizedStrings.cpp
     platform/PlatformStrategies.cpp
+    platform/RunLoop.cpp
     platform/ScrollAnimatorWin.cpp
 
     platform/graphics/ImageSource.cpp
