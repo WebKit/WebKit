88244bda6a2abf072ecf1698e906ae9689783af9
Csaba Osztrogonác <ossy@webkit.org>
2012-02-21 12:34:21 +0000

    [Qt] tst_QWebPage::infiniteLoopJS() timeouts with DFG JIT https://bugs.webkit.org/show_bug.cgi?id=79040
    Reviewed by Simon Hausmann.
    
    * tests/qwebpage/tst_qwebpage.cpp:
    (tst_QWebPage): Disable tst_QWebPage::infiniteLoopJS() until proper fix.
    
    Canonical link: https://commits.webkit.org/96149@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@108341 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/qt/tests/qwebpage/tst_qwebpage.cpp
+++ Source/WebKit/qt/tests/qwebpage/tst_qwebpage.cpp
@@ -160,7 +160,9 @@ private slots:
     void testStopScheduledPageRefresh();
     void findText();
     void supportedContentType();
-    void infiniteLoopJS();
+    // [Qt] tst_QWebPage::infiniteLoopJS() timeouts with DFG JIT
+    // https://bugs.webkit.org/show_bug.cgi?id=79040
+    // void infiniteLoopJS();
     void navigatorCookieEnabled();
     void deleteQWebViewTwice();
     void renderOnRepaintRequestedShouldNotRecurse();
@@ -284,6 +286,9 @@ private:
     bool m_allowGeolocation;
 };
 
+// [Qt] tst_QWebPage::infiniteLoopJS() timeouts with DFG JIT
+// https://bugs.webkit.org/show_bug.cgi?id=79040
+/*
 void tst_QWebPage::infiniteLoopJS()
 {
     JSTestPage* newPage = new JSTestPage(m_view);
@@ -292,6 +297,7 @@ void tst_QWebPage::infiniteLoopJS()
     m_view->page()->mainFrame()->evaluateJavaScript("var run = true;var a = 1;while(run){a++;}");
     delete newPage;
 }
+*/
 
 void tst_QWebPage::geolocationRequestJS()
 {
