16d5cea36e5528b77ea78c46a8f0d61eeab78812
Patrick Gansterer <paroga@webkit.org>
2012-02-21 15:52:06 +0000

    Use WTF::cryptographicallyRandomValues in FileSystemWin.cpp https://bugs.webkit.org/show_bug.cgi?id=79089
    Reviewed by Adam Roben.
    
    Avoid loading and unloading of the crypto library during every
    call to openTemporaryFile() and make the code easier.
    
    * platform/win/FileSystemWin.cpp:
    (WebCore::openTemporaryFile):
    
    
    Canonical link: https://commits.webkit.org/96160@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@108353 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/win/FileSystemWin.cpp
+++ Source/WebCore/platform/win/FileSystemWin.cpp
@@ -32,6 +32,7 @@
 
 #include "NotImplemented.h"
 #include "PathWalker.h"
+#include <wtf/CryptographicallyRandomNumber.h>
 #include <wtf/HashMap.h>
 #include <wtf/text/CString.h>
 #include <wtf/text/WTFString.h>
@@ -220,16 +221,11 @@ String openTemporaryFile(const String&, PlatformFileHandle& handle)
     if (tempPathLength <= 0 || tempPathLength > WTF_ARRAY_LENGTH(tempPath))
         return String();
 
-    HCRYPTPROV hCryptProv = 0;
-    if (!CryptAcquireContext(&hCryptProv, 0, 0, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
-        return String();
-
     String proposedPath;
     do {
         wchar_t tempFile[] = L"XXXXXXXX.tmp"; // Use 8.3 style name (more characters aren't helpful due to 8.3 short file names)
         const int randomPartLength = 8;
-        if (!CryptGenRandom(hCryptProv, randomPartLength * sizeof(wchar_t), reinterpret_cast<BYTE*>(tempFile)))
-            break;
+        cryptographicallyRandomValues(tempFile, randomPartLength * sizeof(wchar_t));
 
         // Limit to valid filesystem characters, also excluding others that could be problematic, like punctuation.
         // don't include both upper and lowercase since Windows file systems are typically not case sensitive.
@@ -246,8 +242,6 @@ String openTemporaryFile(const String&, PlatformFileHandle& handle)
         // use CREATE_NEW to avoid overwriting an existing file with the same name
         handle = ::CreateFileW(proposedPath.charactersWithNullTermination(), GENERIC_READ | GENERIC_WRITE, 0, 0, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, 0);
     } while (!isHandleValid(handle) && GetLastError() == ERROR_ALREADY_EXISTS);
-
-    CryptReleaseContext(hCryptProv, 0);
 
     if (!isHandleValid(handle))
         return String();
