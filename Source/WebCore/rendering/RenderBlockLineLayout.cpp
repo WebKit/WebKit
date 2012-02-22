f564ea2ad2c6ca59397ef0c70c8d9f7d0c7c4e32
Abhishek Arya <inferno@chromium.org>
2012-02-22 21:33:33 +0000

    Source/WebCore: Crash due to accessing removed parent lineboxes when clearing view selection. https://bugs.webkit.org/show_bug.cgi?id=79264
    Reviewed by Eric Seidel.
    
    When our block needed a full layout, we were deleting our own lineboxes
    and letting descendant children (at any level in hierarchy and not just
    immediate children) clear their own lineboxes as we keep laying them out.
    This was problematic because those descendant children lineboxes were
    pointing to removed parent lineboxes in the meantime. An example scenario
    where this would go wrong is first-letter object removal, which can cause
    clearing view selection, leading to accessing parent lineboxes. The patch
    modifies clearing the entire linebox tree upfront. It shouldn't introduce
    performance issues since it will eventually happen as we are laying out
    those children.
    
    Test: fast/css-generated-content/first-letter-textbox-parent-crash.html
    
    * rendering/RenderBlockLineLayout.cpp:
    (WebCore::RenderBlock::layoutInlineChildren):
    
    LayoutTests: Crash due to accessing removed parent lineboxes when clearing view selection.
    https://bugs.webkit.org/show_bug.cgi?id=79264
    
    Reviewed by Eric Seidel.
    
    * fast/css-generated-content/first-letter-textbox-parent-crash-expected.txt: Added.
    * fast/css-generated-content/first-letter-textbox-parent-crash.html: Added.
    
    
    Canonical link: https://commits.webkit.org/96337@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@108547 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderBlockLineLayout.cpp
+++ Source/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -1462,7 +1462,7 @@ void RenderBlock::layoutInlineChildren(bool relayoutChildren, LayoutUnit& repain
     LineLayoutState layoutState(isFullLayout, repaintLogicalTop, repaintLogicalBottom);
 
     if (isFullLayout)
-        lineBoxes()->deleteLineBoxes(renderArena());
+        deleteLineBoxTree();
 
     // Text truncation only kicks in if your overflow isn't visible and your text-overflow-mode isn't
     // clip.
