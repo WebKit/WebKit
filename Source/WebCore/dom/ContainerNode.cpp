46731d41e48fab30586353bd9d05d2ee19ea25ab
Adam Klein <adamk@chromium.org>
2012-02-22 00:34:19 +0000

    ContainerNode::childrenChanged must be called immediately after removing children https://bugs.webkit.org/show_bug.cgi?id=79162
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    In r108152, a call to childrenChanged() was erroneously moved
    below the call to child->removedFromDocument(). This breaks, at the
    least, the behavior of the <title> element. This patch corrects the
    mistake and adds a test.
    
    Test: fast/dom/title-directionality-removeChild.html
    
    * dom/ContainerNode.cpp:
    (WebCore::ContainerNode::removeChild):
    
    LayoutTests:
    
    * fast/dom/title-directionality-removeChild-expected.txt: Added.
    * fast/dom/title-directionality-removeChild.html: Added.
    
    Canonical link: https://commits.webkit.org/96219@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@108415 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/ContainerNode.cpp
+++ Source/WebCore/dom/ContainerNode.cpp
@@ -478,13 +478,13 @@ bool ContainerNode::removeChild(Node* oldChild, ExceptionCode& ec)
     Node* next = child->nextSibling();
     removeBetween(prev, next, child.get());
 
+    childrenChanged(false, prev, next, -1);
+
     if (child->inDocument())
         child->removedFromDocument();
     else
         child->removedFromTree(true);
 
-    // Dispatch post-removal mutation events
-    childrenChanged(false, prev, next, -1);
     dispatchSubtreeModifiedEvent();
 
     return child;
