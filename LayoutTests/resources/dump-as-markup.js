ede2f705200a155016902c13f5fb979d52ca7d40
Ryosuke Niwa <rniwa@webkit.org>
2012-02-23 21:17:27 +0000

    REGRESSION(r99076): WebKit pastes the trailing newline into a single-line text field https://bugs.webkit.org/show_bug.cgi?id=79305
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    The bug was caused by ReplacementFragment::m_hasInterchangeNewlineAtEnd not reset even when
    text field's beforeTextInserted event handler removed interchange new lines at the end.
    Because the event handler is responsible for trimming new lines, we need to recompute the values
    for m_hasInterchangeNewlineAt* after the event dispatch.
    
    Test: editing/input/paste-text-ending-with-interchange-newline.html
    
    * editing/ReplaceSelectionCommand.cpp:
    (WebCore::ReplacementFragment::ReplacementFragment):
    
    LayoutTests:
    
    Add a regression test. Also make Markup.dump() dump the shadow DOM of the specififed node.
    
    * editing/input/paste-text-ending-with-interchange-newline-expected.txt: Added.
    * editing/input/paste-text-ending-with-interchange-newline.html: Added.
    * resources/dump-as-markup.js:
    (Markup._get):
    (Markup._getShadowHostIfPossible):
    
    
    Canonical link: https://commits.webkit.org/96450@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@108668 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/resources/dump-as-markup.js
+++ LayoutTests/resources/dump-as-markup.js
@@ -126,14 +126,16 @@ Markup.useHTML5libOutputFormat = function()
 
 Markup.get = function(node)
 {
+    var markup = Markup._getShadowHostIfPossible(node, 0);
+    if (markup)
+        return markup.substring(1);
+
     if (!node.firstChild)
         return '| ';
 
     // Don't print any markup for the root node.
-    var markup = '';
-    for (var i = 0, len = node.childNodes.length; i < len; i++) {
+    for (var i = 0, len = node.childNodes.length; i < len; i++)
         markup += Markup._get(node.childNodes[i], 0);
-    }
     return markup.substring(1);
 }
 
@@ -228,17 +230,23 @@ Markup._get = function(node, depth)
         str += Markup._get(node.childNodes[i], depth + 1);
     }
 
-    if (!Markup._useHTML5libOutputFormat && node.nodeType == Node.ELEMENT_NODE && window.internals) {
-        var root = window.internals.shadowRoot(node);
-        if (root)
-            str += Markup._get(root, depth + 1);
-    }
+    str += Markup._getShadowHostIfPossible(node, depth);
     
     var selection = Markup._getSelectionMarker(node, i);
     if (selection)
         str += Markup._indent(depth + 1) + selection;
 
     return str;
+}
+
+Markup._getShadowHostIfPossible = function (node, depth)
+{
+    if (!Markup._useHTML5libOutputFormat && node.nodeType == Node.ELEMENT_NODE && window.internals) {
+        var root = window.internals.shadowRoot(node);
+        if (root)
+            return Markup._get(root, depth + 1);
+    }
+    return '';
 }
 
 Markup._namespace = function(node)
