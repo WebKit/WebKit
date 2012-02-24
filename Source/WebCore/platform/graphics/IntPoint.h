5008027572d012c5ef811947e9c311dc66a4c2a8
Leo Yang <leoyang@rim.com>
2012-02-24 03:05:58 +0000

    [BlackBerry] Upstream the BlackBerry change to platform/graphics/IntPoint.h https://bugs.webkit.org/show_bug.cgi?id=79094
    Reviewed by Antonio Gomes.
    
    Add conversion convenience between WebCore::IntPoint and BlackBerry::Platform::IntPoint.
    
    The porting can't be built yet, no new tests.
    
    * platform/graphics/IntPoint.h:
    (Platform):
    (IntPoint):
    
    
    Canonical link: https://commits.webkit.org/96491@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@108715 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/IntPoint.h
+++ Source/WebCore/platform/graphics/IntPoint.h
@@ -55,6 +55,12 @@ class QPoint;
 QT_END_NAMESPACE
 #elif PLATFORM(GTK)
 typedef struct _GdkPoint GdkPoint;
+#elif PLATFORM(BLACKBERRY)
+namespace BlackBerry {
+namespace Platform {
+class IntPoint;
+}
+}
 #elif PLATFORM(EFL)
 typedef struct _Evas_Point Evas_Point;
 #endif
@@ -136,6 +142,9 @@ public:
 #elif PLATFORM(GTK)
     IntPoint(const GdkPoint&);
     operator GdkPoint() const;
+#elif PLATFORM(BLACKBERRY)
+    IntPoint(const BlackBerry::Platform::IntPoint&);
+    operator BlackBerry::Platform::IntPoint() const;
 #elif PLATFORM(EFL)
     explicit IntPoint(const Evas_Point&);
     operator Evas_Point() const;
