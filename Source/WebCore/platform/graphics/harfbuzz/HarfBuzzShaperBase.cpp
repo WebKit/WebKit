2657f56c8c713b2b3c38e30a6e39a618a5da55e6
Adrienne Walker <enne@google.com>
2012-02-25 00:54:07 +0000

    Fix uninitialized variables in HarfBuzzShaperBase https://bugs.webkit.org/show_bug.cgi?id=79546
    Reviewed by Dirk Pranke.
    
    These were introduced in r108733.
    
    * platform/graphics/harfbuzz/HarfBuzzShaperBase.cpp:
    (WebCore::HarfBuzzShaperBase::HarfBuzzShaperBase):
    
    Canonical link: https://commits.webkit.org/96644@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@108871 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/harfbuzz/HarfBuzzShaperBase.cpp
+++ Source/WebCore/platform/graphics/harfbuzz/HarfBuzzShaperBase.cpp
@@ -42,8 +42,12 @@ namespace WebCore {
 
 HarfBuzzShaperBase::HarfBuzzShaperBase(const Font* font, const TextRun& run)
     : m_font(font)
+    , m_normalizedBufferLength(0)
     , m_run(run)
     , m_wordSpacingAdjustment(font->wordSpacing())
+    , m_padding(0)
+    , m_padPerWordBreak(0)
+    , m_padError(0)
     , m_letterSpacing(font->letterSpacing())
 {
 }
