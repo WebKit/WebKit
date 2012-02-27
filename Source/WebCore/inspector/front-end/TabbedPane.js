3db70f475b1a3ae2dc63548f4ea77239eb9c1506
Dan Beam <dbeam@chromium.org>
2012-02-27 08:51:14 +0000

    Web Inspector: Close TabbedPanes on middle click of tab handle https://bugs.webkit.org/show_bug.cgi?id=79518
    Patch by Dan Beam <dbeam@chromium.org> on 2012-02-27
    Reviewed by Pavel Feldman.
    
    * inspector/front-end/TabbedPane.js:
    (WebInspector.TabbedPaneTab.prototype._createTabElement):
    (WebInspector.TabbedPaneTab.prototype._tabClicked):
    
    Canonical link: https://commits.webkit.org/96736@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@108966 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/TabbedPane.js
+++ Source/WebCore/inspector/front-end/TabbedPane.js
@@ -596,9 +596,7 @@ WebInspector.TabbedPaneTab.prototype = {
             tabElement.addStyleClass("measuring");
         else {
             this._tabElement = tabElement;
-            tabElement.addEventListener("click", this._tabSelected.bind(this), false);
-            if (this._closeable)
-                closeButtonSpan.addEventListener("click", this._tabClosed.bind(this), false);
+            tabElement.addEventListener("click", this._tabClicked.bind(this), false);
         }
         
         return tabElement;
@@ -612,13 +610,14 @@ WebInspector.TabbedPaneTab.prototype = {
         this._measureElement.removeChild(measuringTabElement);
     },
 
-    _tabSelected: function()
+    /**
+     * @param {Event} event
+     */
+    _tabClicked: function(event)
     {
-        this._tabbedPane.selectTab(this.id, true);        
-    },
-
-    _tabClosed: function()
-    {
-        this._tabbedPane.closeTab(this.id, true);        
+        if (this._closeable && (event.button === 1 || event.target.hasStyleClass("tabbed-pane-header-tab-close-button")))
+            this._tabbedPane.closeTab(this.id, true);
+        else
+            this._tabbedPane.selectTab(this.id, true);
     }
 }
