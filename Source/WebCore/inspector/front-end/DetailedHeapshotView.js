200c6fe3104b150028a855ccf46af508dda46f0e
Ilya Tikhonovsky <loislo@chromium.org>
2012-02-27 11:56:14 +0000

    Web Inspector: [chromium] Profiles - Tooltip with object/property types stays on screen when another tab selected https://bugs.webkit.org/show_bug.cgi?id=79654
    Reviewed by Yury Semikhatsky.
    
    * inspector/front-end/DetailedHeapshotView.js:
    (WebInspector.DetailedHeapshotView.prototype.willHide):
    
    Canonical link: https://commits.webkit.org/96753@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@108983 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/DetailedHeapshotView.js
+++ Source/WebCore/inspector/front-end/DetailedHeapshotView.js
@@ -623,6 +623,8 @@ WebInspector.DetailedHeapshotView.prototype = {
     {
         this._currentSearchResultIndex = -1;
         this._popoverHelper.hidePopover();
+        if (this.helpPopover.visible)
+            this.helpPopover.hide();
     },
 
     onResize: function()
