47afd6f0ab37314171248955a7100b3a9031f583
Dirk Schulze <krit@webkit.org>
2012-02-27 04:16:09 +0000

    2012-02-26  Dirk Schulze  <krit@webkit.org>
            Cleanup of Adobes copyright text. The text got harmonized with copyright texts of other companies.
    
            Rubber stamped by Adam Barth.
    
            * css/CSSWrapShapes.cpp:
            * css/CSSWrapShapes.h:
            * css/WebKitCSSRegionRule.cpp:
            * css/WebKitCSSRegionRule.h:
            * css/WebKitCSSRegionRule.idl:
            * css/WebKitCSSShaderValue.cpp:
            * css/WebKitCSSShaderValue.h:
            * dom/WebKitNamedFlow.cpp:
            * dom/WebKitNamedFlow.h:
            * dom/WebKitNamedFlow.idl:
            * loader/cache/CachedShader.cpp:
            * loader/cache/CachedShader.h:
            * platform/graphics/filters/CustomFilterMesh.cpp:
            * platform/graphics/filters/CustomFilterMesh.h:
            * platform/graphics/filters/CustomFilterNumberParameter.h:
            * platform/graphics/filters/CustomFilterOperation.cpp:
            * platform/graphics/filters/CustomFilterOperation.h:
            * platform/graphics/filters/CustomFilterParameter.h:
            * platform/graphics/filters/CustomFilterProgram.cpp:
            * platform/graphics/filters/CustomFilterProgram.h:
            * platform/graphics/filters/CustomFilterProgramClient.h:
            * platform/graphics/filters/CustomFilterShader.cpp:
            * platform/graphics/filters/CustomFilterShader.h:
            * platform/graphics/filters/FECustomFilter.cpp:
            * platform/graphics/filters/FECustomFilter.h:
            * rendering/FilterEffectObserver.h:
            * rendering/RenderFlowThread.cpp:
            * rendering/RenderFlowThread.h:
            * rendering/RenderRegion.cpp:
            * rendering/RenderRegion.h:
            * rendering/style/StyleCachedShader.cpp:
            * rendering/style/StyleCachedShader.h:
            * rendering/style/StyleCustomFilterProgram.h:
            * rendering/style/StylePendingShader.h:
            * rendering/style/StyleShader.h:
    
    Canonical link: https://commits.webkit.org/96722@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@108952 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/style/StyleCachedShader.cpp
+++ Source/WebCore/rendering/style/StyleCachedShader.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Adobe Systems Incorporated. All Rights Reserved.
+ * Copyright (C) 2011 Adobe Systems Incorporated. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
