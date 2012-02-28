95ae24970084db504598e98474a13c21f60e0715
Brady Eidson <beidson@apple.com>
2012-02-28 01:59:09 +0000

    <rdar://problem/10924993> and https://bugs.webkit.org/show_bug.cgi?id=79725 <a ping> doesn't work in WebKit2
    Reviewed by Alexey Proskuryakov.
    
    Source/WebKit2:
    
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::updatePreferences): Push the WK2 <a ping> setting to WebCore::Settings.
    
    LayoutTests:
    
    * platform/wk2/Skipped: Unskip some tests that now pass.
    
    Canonical link: https://commits.webkit.org/96817@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@109055 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -1893,6 +1893,7 @@ void WebPage::updatePreferences(const WebPreferencesStore& store)
     settings->setMediaPlaybackRequiresUserGesture(store.getBoolValueForKey(WebPreferencesKey::mediaPlaybackRequiresUserGestureKey()));
     settings->setMediaPlaybackAllowsInline(store.getBoolValueForKey(WebPreferencesKey::mediaPlaybackAllowsInlineKey()));
     settings->setMockScrollbarsEnabled(store.getBoolValueForKey(WebPreferencesKey::mockScrollbarsEnabledKey()));
+    settings->setHyperlinkAuditingEnabled(store.getBoolValueForKey(WebPreferencesKey::hyperlinkAuditingEnabledKey()));
 
     // <rdar://problem/10697417>: It is necessary to force compositing when accelerate drawing
     // is enabled on Mac so that scrollbars are always in their own layers.
