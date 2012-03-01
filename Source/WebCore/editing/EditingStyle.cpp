20325de3c57d7e2208f7cfa999c0ff0f96bafd86
Kenichi Ishibashi <bashi@chromium.org>
2012-03-01 19:47:44 +0000

    REGRESSION: Outlook 2007 doesn't display fonts correctly on emails composed by WebKit https://bugs.webkit.org/show_bug.cgi?id=79448
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Don't produce single quotes around face attribute of font elements.
    
    Test: editing/style/font-face-unquote.html
    
    * editing/EditingStyle.cpp:
    (WebCore::StyleChange::extractTextStyles): Remove single quotes from m_applyFontFace.
    
    LayoutTests:
    
    Add a test that makes sure WebKit doesn't produce single quotes around
    face attribute of font element.
    
    * editing/style/font-face-unquote-expected.txt: Added.
    * editing/style/font-face-unquote.html: Added.
    
    
    Canonical link: https://commits.webkit.org/97111@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@109382 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/editing/EditingStyle.cpp
+++ Source/WebCore/editing/EditingStyle.cpp
@@ -1379,6 +1379,8 @@ void StyleChange::extractTextStyles(Document* document, StylePropertySet* style,
     }
 
     m_applyFontFace = style->getPropertyValue(CSSPropertyFontFamily);
+    // Remove single quotes for Outlook 2007 compatibility. See https://bugs.webkit.org/show_bug.cgi?id=79448
+    m_applyFontFace.replace('\'', "");
     style->removeProperty(CSSPropertyFontFamily);
 
     if (RefPtr<CSSValue> fontSize = style->getPropertyCSSValue(CSSPropertyFontSize)) {
