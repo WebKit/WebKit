d6efcbe6ceff40c103bfebbe0a4d3e128714a1cf
Enrica Casucci <enrica@apple.com>
2012-03-09 17:59:30 +0000

    REGRESSION (r109022): Files dragged onto input controls cannot be read due to sandbox violation. https://bugs.webkit.org/show_bug.cgi?id=80203 <rdar://problem/10976643>
    Reviewed by Alexey Proskuryakov.
    
    This patch reverts a small part fo r109022, leaving access to
    NSPasteboard in the WebProcess when retrieving pathnames for
    files being dragged. This avoid the sandbox violation until we
    implement a mechanism to provide a sandbox extension to the WebProcess.
    
    * WebProcess/WebCoreSupport/WebPlatformStrategies.cpp:
    (WebKit::WebPlatformStrategies::getPathnamesForType):
    
    
    
    Canonical link: https://commits.webkit.org/97899@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@110303 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/WebCoreSupport/WebPlatformStrategies.cpp
+++ Source/WebKit2/WebProcess/WebCoreSupport/WebPlatformStrategies.cpp
@@ -155,8 +155,11 @@ PassRefPtr<WebCore::SharedBuffer> WebPlatformStrategies::bufferForType(const Str
 
 void WebPlatformStrategies::getPathnamesForType(Vector<String>& pathnames, const String& pasteboardType, const String& pasteboardName)
 {
-    WebProcess::shared().connection()->sendSync(Messages::WebContext::GetPasteboardPathnamesForType(pasteboardName, pasteboardType),
-                                                Messages::WebContext::GetPasteboardPathnamesForType::Reply(pathnames), 0);
+    // FIXME: this needs to be replaced with the code below, once we resolve the sandboxing issue.
+    // WebProcess::shared().connection()->sendSync(Messages::WebContext::GetPasteboardPathnamesForType(pasteboardName, pasteboardType),
+    //                                             Messages::WebContext::GetPasteboardPathnamesForType::Reply(pathnames), 0);
+    
+    PlatformPasteboard(pasteboardName).getPathnamesForType(pathnames, pasteboardType);
 }
 
 String WebPlatformStrategies::stringForType(const String& pasteboardType, const String& pasteboardName)
