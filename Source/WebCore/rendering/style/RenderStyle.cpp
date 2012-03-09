65c2898af27354ae6896bba30d9a9491adb05b59
Emil A Eklund <eae@chromium.org>
2012-03-09 05:00:41 +0000

    Change calcRadiiFor to IntSize https://bugs.webkit.org/show_bug.cgi?id=80655
    Reviewed by Simon Fraser.
    
    Borders and RoundedRect are both represented with pixel precision. As
    such it doesn't make sense for calcRadiiFor to use subpixel units, it
    just adds unnecessary type conversions.
    
    No new tests, no new functionality.
    
    * rendering/style/RenderStyle.cpp:
    (WebCore::calcRadiiFor):
    
    
    Canonical link: https://commits.webkit.org/97856@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@110260 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/style/RenderStyle.cpp
+++ Source/WebCore/rendering/style/RenderStyle.cpp
@@ -845,14 +845,14 @@ void RenderStyle::setBoxShadow(PassOwnPtr<ShadowData> shadowData, bool add)
 static RoundedRect::Radii calcRadiiFor(const BorderData& border, LayoutSize size)
 {
     return RoundedRect::Radii(
-        LayoutSize(border.topLeft().width().calcValue(size.width()), 
-                   border.topLeft().height().calcValue(size.height())),
-        LayoutSize(border.topRight().width().calcValue(size.width()),
-                   border.topRight().height().calcValue(size.height())),
-        LayoutSize(border.bottomLeft().width().calcValue(size.width()), 
-                   border.bottomLeft().height().calcValue(size.height())),
-        LayoutSize(border.bottomRight().width().calcValue(size.width()), 
-                   border.bottomRight().height().calcValue(size.height())));
+        IntSize(border.topLeft().width().calcValue(size.width()), 
+                border.topLeft().height().calcValue(size.height())),
+        IntSize(border.topRight().width().calcValue(size.width()),
+                border.topRight().height().calcValue(size.height())),
+        IntSize(border.bottomLeft().width().calcValue(size.width()), 
+                border.bottomLeft().height().calcValue(size.height())),
+        IntSize(border.bottomRight().width().calcValue(size.width()), 
+                border.bottomRight().height().calcValue(size.height())));
 }
 
 static float calcConstraintScaleFor(const IntRect& rect, const RoundedRect::Radii& radii)
