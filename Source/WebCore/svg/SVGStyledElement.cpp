9ddf43729da5d4fd11ddf37af024df9ea4f31db9
Stephen Chenney <schenney@chromium.org>
2012-03-09 20:07:11 +0000

    Crash in WebCore::SVGUseElement::instanceForShadowTreeElement https://bugs.webkit.org/show_bug.cgi?id=80406
    Reviewed by Nikolas Zimmermann.
    
    Code assumes that an object that is an SVG Element and in a shadow
    tree must be in an SVG use shadow tree, and casts the shadow host with
    a static_cast. It may be that an SVG element appears in a non-use
    shadow tree, in which case bad things happen. While it appears that
    the current code prevents such a situation from arising (checks are
    made within the shadow tree code to prevent it) there are also
    indications that the situation may change.
    
    No new tests. I believe that the problem here cannot currently be
    reproduced. That is, other code prevents SVG elements from appearing
    in non-svg shadow trees.
    
    * dom/EventDispatcher.cpp:
    (WebCore::eventTargetRespectingSVGTargetRules):
    * svg/SVGStyledElement.cpp:
    (WebCore::SVGStyledElement::title):
    
    
    Canonical link: https://commits.webkit.org/97910@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@110314 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/svg/SVGStyledElement.cpp
+++ Source/WebCore/svg/SVGStyledElement.cpp
@@ -90,13 +90,19 @@ String SVGStyledElement::title() const
 
     // Walk up the tree, to find out whether we're inside a <use> shadow tree, to find the right title.
     if (isInShadowTree()) {
-        SVGUseElement* useElement = static_cast<SVGUseElement*>(treeScope()->rootNode()->shadowHost());
-        ASSERT(useElement);
-
-        // If the <use> title is not empty we found the title to use.
-        String useTitle(useElement->title());
-        if (!useTitle.isEmpty())
-            return useTitle;
+        Element* shadowHostElement = treeScope()->rootNode()->shadowHost();
+        // At this time, SVG nodes are not allowed in non-<use> shadow trees, so any shadow root we do
+        // have should be a use. The assert and following test is here to catch future shadow DOM changes
+        // that do enable SVG in a shadow tree.
+        ASSERT(!shadowHostElement || shadowHostElement->hasTagName(SVGNames::useTag));
+        if (shadowHostElement && shadowHostElement->hasTagName(SVGNames::useTag)) {
+            SVGUseElement* useElement = static_cast<SVGUseElement*>(shadowHostElement);
+ 
+            // If the <use> title is not empty we found the title to use.
+            String useTitle(useElement->title());
+            if (!useTitle.isEmpty())
+               return useTitle;
+        }
     }
 
     // If we aren't an instance in a <use> or the <use> title was not found, then find the first
