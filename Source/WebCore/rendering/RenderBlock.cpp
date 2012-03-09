a371303df4646aa097cfc6f88d5c2758d32a1bf4
Abhishek Arya <inferno@chromium.org>
2012-03-09 21:14:59 +0000

    Crash when splitting an anonymous block in multi-column layout. https://bugs.webkit.org/show_bug.cgi?id=80432
    Reviewed by David Hyatt.
    
    Source/WebCore:
    
    Calculating currChild->nextSibling() is risky after destroying :after content
    because it can blow away currChild if it is a left over empty anonymous block.
    We need to calculate next sibling upfront, using the same trick, we do in
    RenderBlock::addChildIgnoringAnonymousColumnBlock to reset beforeChild (check
    out the line before splitFlow call).
    
    Test: fast/multicol/anonymous-block-split-crash.html
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::splitBlocks):
    
    LayoutTests:
    
    * fast/multicol/anonymous-block-split-crash-expected.txt: Added.
    * fast/multicol/anonymous-block-split-crash.html: Added.
    
    Canonical link: https://commits.webkit.org/97920@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@110324 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderBlock.cpp
+++ Source/WebCore/rendering/RenderBlock.cpp
@@ -575,6 +575,7 @@ void RenderBlock::splitBlocks(RenderBlock* fromBlock, RenderBlock* toBlock,
     // Once we hit the anonymous columns block we're done.
     RenderBoxModelObject* curr = toRenderBoxModelObject(parent());
     RenderBoxModelObject* currChild = this;
+    RenderObject* currChildNextSibling = currChild->nextSibling();
     
     while (curr && curr != fromBlock) {
         ASSERT(curr->isRenderBlock());
@@ -601,15 +602,20 @@ void RenderBlock::splitBlocks(RenderBlock* fromBlock, RenderBlock* toBlock,
         // Someone may have indirectly caused a <q> to split.  When this happens, the :after content
         // has to move into the inline continuation.  Call updateBeforeAfterContent to ensure that the inline's :after
         // content gets properly destroyed.
+        bool isLastChild = (currChildNextSibling == blockCurr->lastChild());
         if (document()->usesBeforeAfterRules())
             blockCurr->children()->updateBeforeAfterContent(blockCurr, AFTER);
+        if (isLastChild && currChildNextSibling != blockCurr->lastChild())
+            currChildNextSibling = 0; // We destroyed the last child, so now we need to update
+                                      // the value of currChildNextSibling.
 
         // Now we need to take all of the children starting from the first child
         // *after* currChild and append them all to the clone.
-        blockCurr->moveChildrenTo(cloneBlock, currChild->nextSibling(), 0, true);
+        blockCurr->moveChildrenTo(cloneBlock, currChildNextSibling, 0, true);
 
         // Keep walking up the chain.
         currChild = curr;
+        currChildNextSibling = currChild->nextSibling();
         curr = toRenderBoxModelObject(curr->parent());
     }
 
@@ -618,7 +624,7 @@ void RenderBlock::splitBlocks(RenderBlock* fromBlock, RenderBlock* toBlock,
 
     // Now take all the children after currChild and remove them from the fromBlock
     // and put them in the toBlock.
-    fromBlock->moveChildrenTo(toBlock, currChild->nextSibling(), 0, true);
+    fromBlock->moveChildrenTo(toBlock, currChildNextSibling, 0, true);
 }
 
 void RenderBlock::splitFlow(RenderObject* beforeChild, RenderBlock* newBlockBox,
