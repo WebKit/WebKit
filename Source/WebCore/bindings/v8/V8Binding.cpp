8f207b4f7cc92ce46a5c2b604873b44de25e870b
Erik Arvidsson <arv@chromium.org>
2012-03-10 05:04:55 +0000

    [V8] Fix a fixme in v8 bindings https://bugs.webkit.org/show_bug.cgi?id=80734
    Reviewed by Kentaro Hara.
    
    No new tests. Covered by existing tests.
    
    * bindings/v8/V8Binding.cpp:
    (WebCore::toInt32):
    (WebCore::toUInt32):
    
    
    Canonical link: https://commits.webkit.org/97961@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@110369 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/V8Binding.cpp
+++ Source/WebCore/bindings/v8/V8Binding.cpp
@@ -210,7 +210,7 @@ int toInt32(v8::Handle<v8::Value> value, bool& ok)
 {
     ok = true;
     
-    // Fast case.  The value is already a 32-bit integer.
+    // Fast case. The value is already a 32-bit integer.
     if (value->IsInt32())
         return value->Int32Value();
     
@@ -243,11 +243,10 @@ uint32_t toUInt32(v8::Handle<v8::Value> value, bool& ok)
 {
     ok = true;
 
-    // FIXME: there is currently no Value::IsUint32(). This code does
-    // some contortions to avoid silently converting out-of-range
-    // values to uint32_t.
+    // Fast case. The value is already a 32-bit unsigned integer.
+    if (value->IsUint32())
+        return value->Uint32Value();
 
-    // Fast case.  The value is already a 32-bit positive integer.
     if (value->IsInt32()) {
         int32_t result = value->Int32Value();
         if (result >= 0)
@@ -275,15 +274,7 @@ uint32_t toUInt32(v8::Handle<v8::Value> value, bool& ok)
         return 0;
     }
 
-    // FIXME: v8::Uint32::Value is not defined!
-    // http://code.google.com/p/v8/issues/detail?id=624
-    v8::Local<v8::Int32> intValue = value->ToInt32();
-    if (intValue.IsEmpty()) {
-        ok = false;
-        return 0;
-    }
-
-    return static_cast<uint32_t>(intValue->Value());
+    return uintValue->Value();
 }
 
 template <class S> struct StringTraits
