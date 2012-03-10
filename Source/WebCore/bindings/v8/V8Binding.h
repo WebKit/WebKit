5a4c3ee7c9939fc5109c9c67fe3bfbeb6daa1e7e
Kentaro Hara <haraken@chromium.org>
2012-03-10 00:13:13 +0000

    [V8][Performance] Inline hot methods in V8Bindings.h https://bugs.webkit.org/show_bug.cgi?id=80685
    Reviewed by Adam Barth.
    
    This patch slightly improves DOM binding performance by inlining hot
    methods in V8Binding.cpp, e.g. isUndefinedOrNull(), v8StringOrNull(), v8Boolean().
    For example, this patch improves div.nodeName by 5.0%, and div.nodeValue by 4.1%.
    
    Performance tests: https://bugs.webkit.org/attachment.cgi?id=131006
    
    The performance test results in my Mac environment are as follows:
    
    Chromium/V8 without this patch:
    div.nodeName : 3417.4 ms
    div.nodeValue : 2069.6 ms
    
    Chromium/V8 with this patch:
    div.nodeName : 3245.6 ms
    div.nodeValue : 1983.1 ms
    
    No tests. No change in behavior.
    
    * bindings/v8/V8Binding.cpp:
    * bindings/v8/V8Binding.h:
    (WebCore::toWebCoreString):
    (WebCore::isUndefinedOrNull):
    (WebCore::isHostObject):
    (WebCore::v8Boolean):
    (WebCore::toWebCoreStringWithNullCheck):
    (WebCore::toAtomicWebCoreStringWithNullCheck):
    (WebCore::toWebCoreStringWithNullOrUndefinedCheck):
    (WebCore::v8UndetectableString):
    (WebCore::v8StringOrNull):
    (WebCore::v8StringOrUndefined):
    (WebCore::v8StringOrFalse):
    (WebCore::toWebCoreDate):
    (WebCore::v8DateOrNull):
    
    
    Canonical link: https://commits.webkit.org/97939@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@110347 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/V8Binding.h
+++ Source/WebCore/bindings/v8/V8Binding.h
@@ -329,7 +329,10 @@ namespace WebCore {
         return v8ValueToWebCoreString(object);
     }
 
-    String toWebCoreString(const v8::Arguments&, int index);
+    inline String toWebCoreString(const v8::Arguments& args, int index)
+    {
+        return v8ValueToWebCoreString(args[index]);
+    }
 
     // The string returned by this function is still owned by the argument
     // and will be deallocated when the argument is deallocated.
@@ -338,27 +341,62 @@ namespace WebCore {
         return reinterpret_cast<const uint16_t*>(str.characters());
     }
 
-    bool isUndefinedOrNull(v8::Handle<v8::Value> value);
+    inline bool isUndefinedOrNull(v8::Handle<v8::Value> value)
+    {
+        return value->IsNull() || value->IsUndefined();
+    }
 
     // Returns true if the provided object is to be considered a 'host object', as used in the
     // HTML5 structured clone algorithm.
-    bool isHostObject(v8::Handle<v8::Object>);
+    inline bool isHostObject(v8::Handle<v8::Object> object)
+    {
+        // If the object has any internal fields, then we won't be able to serialize or deserialize
+        // them; conveniently, this is also a quick way to detect DOM wrapper objects, because
+        // the mechanism for these relies on data stored in these fields. We should
+        // catch external array data and external pixel data as a special case (noting that CanvasPixelArrays
+        // can't be serialized without being wrapped by ImageData according to the standard).
+        return object->InternalFieldCount() || object->HasIndexedPropertiesInPixelData() || object->HasIndexedPropertiesInExternalArrayData();
+    }
 
-    v8::Handle<v8::Boolean> v8Boolean(bool value);
+    inline v8::Handle<v8::Boolean> v8Boolean(bool value)
+    {
+        return value ? v8::True() : v8::False();
+    }
 
-    String toWebCoreStringWithNullCheck(v8::Handle<v8::Value> value);
+    inline String toWebCoreStringWithNullCheck(v8::Handle<v8::Value> value)
+    {
+        return value->IsNull() ? String() : v8ValueToWebCoreString(value);
+    }
 
-    AtomicString toAtomicWebCoreStringWithNullCheck(v8::Handle<v8::Value> value);
+    inline AtomicString toAtomicWebCoreStringWithNullCheck(v8::Handle<v8::Value> value)
+    {
+        return value->IsNull() ? AtomicString() : v8ValueToAtomicWebCoreString(value);
+    }
 
-    String toWebCoreStringWithNullOrUndefinedCheck(v8::Handle<v8::Value> value);
+    inline String toWebCoreStringWithNullOrUndefinedCheck(v8::Handle<v8::Value> value)
+    {
+        return (value->IsNull() || value->IsUndefined()) ? String() : toWebCoreString(value);
+    }
 
-    v8::Handle<v8::String> v8UndetectableString(const String& str);
+    inline v8::Handle<v8::String> v8UndetectableString(const String& str)
+    {
+        return v8::String::NewUndetectable(fromWebCoreString(str), str.length());
+    }
 
-    v8::Handle<v8::Value> v8StringOrNull(const String& str);
+    inline v8::Handle<v8::Value> v8StringOrNull(const String& str)
+    {
+        return str.isNull() ? v8::Handle<v8::Value>(v8::Null()) : v8::Handle<v8::Value>(v8String(str));
+    }
 
-    v8::Handle<v8::Value> v8StringOrUndefined(const String& str);
+    inline v8::Handle<v8::Value> v8StringOrUndefined(const String& str)
+    {
+        return str.isNull() ? v8::Handle<v8::Value>(v8::Undefined()) : v8::Handle<v8::Value>(v8String(str));
+    }
 
-    v8::Handle<v8::Value> v8StringOrFalse(const String& str);
+    inline v8::Handle<v8::Value> v8StringOrFalse(const String& str)
+    {
+        return str.isNull() ? v8::Handle<v8::Value>(v8::False()) : v8::Handle<v8::Value>(v8String(str));
+    }
 
     template <class T> v8::Handle<v8::Value> v8NumberArray(const Vector<T>& values)
     {
@@ -369,9 +407,15 @@ namespace WebCore {
         return result;
     }
 
-    double toWebCoreDate(v8::Handle<v8::Value> object);
+    inline double toWebCoreDate(v8::Handle<v8::Value> object)
+    {
+        return (object->IsDate() || object->IsNumber()) ? object->NumberValue() : std::numeric_limits<double>::quiet_NaN();
+    }
 
-    v8::Handle<v8::Value> v8DateOrNull(double value);
+    inline v8::Handle<v8::Value> v8DateOrNull(double value)
+    {
+        return isfinite(value) ? v8::Date::New(value) : v8::Handle<v8::Value>(v8::Null());
+    }
 
     v8::Persistent<v8::FunctionTemplate> createRawTemplate();
 
