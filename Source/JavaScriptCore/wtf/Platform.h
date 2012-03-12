521109e2689ea666006c240edd8be0aed42bf76c
Zeno Albisser <zeno.albisser@nokia.com>
2012-03-12 15:34:50 +0000

    [Qt][Mac] Build fails after enabling LLINT when JIT is disabled (r109863) https://bugs.webkit.org/show_bug.cgi?id=80827
    Qt on Mac uses OS(DARWIN) as well, but we do not want to enable LLINT.
    
    Reviewed by Simon Hausmann.
    
    * wtf/Platform.h:
    
    Canonical link: https://commits.webkit.org/98020@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@110430 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/wtf/Platform.h
+++ Source/JavaScriptCore/wtf/Platform.h
@@ -935,7 +935,7 @@
 
 /* On some of the platforms where we have a JIT, we want to also have the 
    low-level interpreter. */
-#if !defined(ENABLE_LLINT) && ENABLE(JIT) && OS(DARWIN) && (CPU(X86) || CPU(X86_64) || CPU(ARM_THUMB2))
+#if !defined(ENABLE_LLINT) && ENABLE(JIT) && (OS(DARWIN) && !PLATFORM(QT)) && (CPU(X86) || CPU(X86_64) || CPU(ARM_THUMB2))
 #define ENABLE_LLINT 1
 #endif
 
