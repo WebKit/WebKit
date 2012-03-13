55780c55deded3a117c4f97d8cd26ea56cdfeecb
Yoshifumi Inoue <yosin@chromium.org>
2012-03-13 07:24:42 +0000

    [Forms] Re-factor label.for tests for extending test coverage https://bugs.webkit.org/show_bug.cgi?id=80574
    Patch by Yosifumi Inoue <yosin@chromium.org> on 2012-03-13
    Reviewed by Kent Tamura.
    
    This patch changes label.labels attribute testing with using shared
    utility functions, createFormControlDataSet for representing form
    related information of HTML5 specification and setupLabelsTeset for
    populating test sample dom tree and data set.
    
    This patch extends coverage of existing label.labels test to cover
    defined in the specification, labelable-elements.html, and adds
    multiple label entries tests, labels-multiple-*.html.
    
    Note: test failure of hidden input type will be fixed by
    https://bugs.webkit.org/show_bug.cgi?id=80403
    
    Note: test failure of output element will be fixed by
    https://bugs.webkit.org/show_bug.cgi?id=80466
    
    * fast/forms/label/labelable-elements-expected.txt: Added.
    * fast/forms/label/labelable-elements.html: Added.
    * fast/forms/label/labels-add-htmlFor-label-expected.txt:
    * fast/forms/label/labels-add-htmlFor-label.html:
    * fast/forms/label/labels-add-parent-label-expected.txt:
    * fast/forms/label/labels-add-parent-label.html:
    * fast/forms/label/labels-change-htmlFor-attribute-expected.txt:
    * fast/forms/label/labels-change-htmlFor-attribute.html:
    * fast/forms/label/labels-multiple-sibling-labels-expected.txt: Added.
    * fast/forms/label/labels-multiple-sibling-labels.html: Added.
    * fast/forms/label/labels-parent-and-sibling-labels-expected.txt: Added.
    * fast/forms/label/labels-parent-and-sibling-labels.html: Added.
    * fast/forms/label/labels-remove-htmlFor-attribute-expected.txt:
    * fast/forms/label/labels-remove-htmlFor-attribute.html:
    * fast/forms/label/labels-remove-htmlFor-label-expected.txt:
    * fast/forms/label/labels-remove-htmlFor-label.html:
    * fast/forms/label/labels-remove-parent-label-expected.txt:
    * fast/forms/label/labels-remove-parent-label.html:
    * fast/forms/label/labels-set-htmlFor-attribute-expected.txt:
    * fast/forms/label/labels-set-htmlFor-attribute.html:
    * fast/forms/resources/common.js:
    (createFormControlDataSet): Returns form control data set for providing
    labelable elements and supported elements information.
    * fast/forms/resources/label-test-util.js: Added.
    (setupLabelsTest.return.dataSet.dataSet.getLabelableElementData):
    (setupLabelsTest): Populate dom tree for unit tests.
    
    Canonical link: https://commits.webkit.org/98119@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@110542 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/forms/resources/common.js
+++ LayoutTests/fast/forms/resources/common.js
@@ -2,6 +2,90 @@ function $(id) {
     return document.getElementById(id);
 }
 
+function createFormControlDataSet() {
+    // A list of labelable elements resides in http://www.whatwg.org/specs/web-apps/current-work/multipage/forms.html#category-label
+    var formControlClassNames = [
+        'HTMLButtonElement',
+        'HTMLDataListElement',
+        'HTMLFieldSetElement',
+        'HTMLInputElement',
+        'HTMLKeygenElement',
+        'HTMLLabelElement',
+        'HTMLLegendElement',
+        'HTMLMeterElement',
+        'HTMLObjectElement',
+        'HTMLOptGroupElement',
+        'HTMLOptionElement',
+        'HTMLOutputElement',
+        'HTMLProgressElement',
+        'HTMLSelectElement',
+        'HTMLTextAreaElement'
+    ];
+    var formControlDataSet = {};
+    for (var i = 0; i < formControlClassNames.length; i++) {
+        var className = formControlClassNames[i];
+        var tagName = className.toLowerCase().substring(4, className.length - 7);
+        var element = document.createElement(tagName);
+        formControlDataSet[tagName] = {
+            inputType: null,
+            isLabelable: true,
+            isSupported: element.toString() == '[object ' + className + ']',
+            name: tagName,
+            tagName: tagName,
+        };
+    }
+    formControlDataSet.datalist.isLabelable = false;
+    formControlDataSet.fieldset.isLabelable = false;
+    formControlDataSet.label.isLabelable = false;
+    formControlDataSet.legend.isLabelable = false;
+    formControlDataSet.object.isLabelable = false;
+    formControlDataSet.optgroup.isLabelable = false;
+    formControlDataSet.option.isLabelable = false;
+
+    // Input type names reside in http://www.whatwg.org/specs/web-apps/current-work/multipage/the-input-element.html
+    var inputTypeNames = [
+        'button',
+        'checkbox',
+        'color',
+        'date',
+        'datetime',
+        'datetime-local',
+        'email',
+        'file',
+        'hidden',
+        'image',
+        'month',
+        'number',
+        'password',
+        'radio',
+        'range',
+        'reset',
+        'search',
+        'submit',
+        'tel',
+        'text',
+        'time',
+        'url',
+        'week',
+    ];
+    for (var i = 0; i < inputTypeNames.length; i++) {
+        var typeName = inputTypeNames[i];
+        var name = typeName + 'Type';
+        var element = document.createElement('input');
+        element.type = typeName;
+        formControlDataSet[name] = {
+            inputType: typeName,
+            isLabelable: true,
+            isSupported: element.type == typeName,
+            name: name,
+            tagName: 'input',
+      };
+    }
+    formControlDataSet.hiddenType.isLabelable = false;
+
+    return formControlDataSet;
+}
+
 function getValidationMessageBubbleNode(host) {
     // FIXME: We should search for a pseudo ID.
     return internals.shadowRoot(host).lastChild;
