777ef783c36bae5ea3e1da65feef790b129fdd9b
Rob Buis <rwlbuis@webkit.org>
2012-03-15 14:34:16 +0000

    Fix cast-align GCC warnings https://bugs.webkit.org/show_bug.cgi?id=80790
    Reviewed by Nikolas Zimmermann.
    
    * platform/graphics/WOFFFileFormat.cpp:
    (WebCore::writeUInt32):
    (WebCore::writeUInt16):
    (WebCore::convertWOFFToSfnt):
    
    Canonical link: https://commits.webkit.org/98406@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@110844 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/WOFFFileFormat.cpp
+++ Source/WebCore/platform/graphics/WOFFFileFormat.cpp
@@ -84,13 +84,13 @@ static bool readUInt16(SharedBuffer* buffer, size_t& offset, uint16_t& value)
 static bool writeUInt32(Vector<char>& vector, uint32_t value)
 {
     uint32_t bigEndianValue = htonl(value);
-    return vector.tryAppend(reinterpret_cast<char*>(&bigEndianValue), sizeof(bigEndianValue));
+    return vector.tryAppend(reinterpret_cast_ptr<char*>(&bigEndianValue), sizeof(bigEndianValue));
 }
 
 static bool writeUInt16(Vector<char>& vector, uint16_t value)
 {
     uint16_t bigEndianValue = htons(value);
-    return vector.tryAppend(reinterpret_cast<char*>(&bigEndianValue), sizeof(bigEndianValue));
+    return vector.tryAppend(reinterpret_cast_ptr<char*>(&bigEndianValue), sizeof(bigEndianValue));
 }
 
 static const uint32_t woffSignature = 0x774f4646; /* 'wOFF' */
@@ -210,7 +210,7 @@ bool convertWOFFToSfnt(SharedBuffer* woff, Vector<char>& sfnt)
             return false;
 
         // Write an sfnt table directory entry.
-        uint32_t* sfntTableDirectoryPtr = reinterpret_cast<uint32_t*>(sfnt.data() + sfntTableDirectoryCursor);
+        uint32_t* sfntTableDirectoryPtr = reinterpret_cast_ptr<uint32_t*>(sfnt.data() + sfntTableDirectoryCursor);
         *sfntTableDirectoryPtr++ = htonl(tableTag);
         *sfntTableDirectoryPtr++ = htonl(tableOrigChecksum);
         *sfntTableDirectoryPtr++ = htonl(sfnt.size());
