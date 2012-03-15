a086708133cfe213f0d4d56b637b23f952d37f3f
Yuta Kitamura <yutak@chromium.org>
2012-03-15 23:17:31 +0000

    [Qt] Change default WebSocket protocol to the latest one https://bugs.webkit.org/show_bug.cgi?id=73630
    Reviewed by Simon Hausmann.
    
    * Api/qwebpage.cpp:
    (QWebPagePrivate::QWebPagePrivate):
    Use RFC6455 WebSocket protocol by default.
    
    
    Canonical link: https://commits.webkit.org/98467@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@110905 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/qt/Api/qwebpage.cpp
+++ Source/WebKit/qt/Api/qwebpage.cpp
@@ -358,6 +358,10 @@ QWebPagePrivate::QWebPagePrivate(QWebPage *qq)
 #endif
     settings = new QWebSettings(page->settings());
 
+#if ENABLE(WEB_SOCKETS)
+    page->settings()->setUseHixie76WebSocketProtocol(false);
+#endif
+
     history.d = new QWebHistoryPrivate(static_cast<WebCore::BackForwardListImpl*>(page->backForwardList()));
     memset(actions, 0, sizeof(actions));
 
