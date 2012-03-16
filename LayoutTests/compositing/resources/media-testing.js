8a86ee56a9e41bf9b177851837de6f6823b170ac
Aaron Colwell <acolwell@chromium.org>
2012-03-16 18:21:16 +0000

    [chromium] compositing tests with videos fail in chromium DumpRenderTree, seeking doesn't appear to work https://bugs.webkit.org/show_bug.cgi?id=76511
    Patch by Aaron Colwell <acolwell@chromium.org> on 2012-03-16
    Reviewed by Eric Carlson.
    
    * compositing/resources/media-testing.js:
    (ensureFrameVisible.playingHandler):
    (ensureFrameVisible.seekedHandler):
    (ensureFrameVisible):
    
    Canonical link: https://commits.webkit.org/98585@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@111029 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/compositing/resources/media-testing.js
+++ LayoutTests/compositing/resources/media-testing.js
@@ -4,13 +4,18 @@ if (window.layoutTestController)
 
 function ensureFrameVisible(video, videoRenderedCallback)
 {
-    video.addEventListener('timeupdate', function() {
+    var playingHandler = function() {
         video.pause();
         video.currentTime = 0;
+    };
+
+    var seekedHandler = function() {
         if (videoRenderedCallback)
             videoRenderedCallback();
-    }, false);
-    
+    };
+
+    video.addEventListener('playing', playingHandler, false);
+    video.addEventListener('seeked', seekedHandler, false);
     video.play();
 }
 
