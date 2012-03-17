e218bd07dd9bfb276710088e7ed89a3a3f5eb60c
Leo Yang <leoyang@rim.com>
2012-03-17 06:39:49 +0000

    [BlackBerry] Add typedef of DragDataRef for blackberry https://bugs.webkit.org/show_bug.cgi?id=81301
    Reviewed by Rob Buis.
    
    * platform/DragData.h:
    
    
    Canonical link: https://commits.webkit.org/98660@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@111113 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/DragData.h
+++ Source/WebCore/platform/DragData.h
@@ -63,7 +63,7 @@ class DataObjectGtk;
 typedef WebCore::DataObjectGtk* DragDataRef;
 #elif PLATFORM(CHROMIUM)
 #include "DragDataRef.h"
-#elif PLATFORM(EFL)
+#elif PLATFORM(EFL) || PLATFORM(BLACKBERRY)
 typedef void* DragDataRef;
 #endif
 
