f0446e23b65edb221dfa6289d2042a63ac1c42cc
Anders Carlsson <andersca@apple.com>
2012-03-20 03:02:12 +0000

    Fix Windows build.
    * FullscreenVideoController.cpp:
    (FullscreenVideoController::LayerClient::platformCALayerDeviceScaleFactor):
    
    Canonical link: https://commits.webkit.org/98854@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@111331 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/win/FullscreenVideoController.cpp
+++ Source/WebKit/win/FullscreenVideoController.cpp
@@ -196,6 +196,7 @@ private:
     virtual bool platformCALayerDrawsContent() const { return false; }
     virtual void platformCALayerLayerDidDisplay(PlatformLayer*) { }
     virtual void platformCALayerDidCreateTiles() { }
+    virtual float platformCALayerDeviceScaleFactor() { return 1; }
 
     FullscreenVideoController* m_parent;
 };
