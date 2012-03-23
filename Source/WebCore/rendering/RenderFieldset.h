4c8996ee44cc8edbe43e42d78884b34d2095a8f8
SravanKumar Sandela <ssandela@innominds.com>
2012-03-23 03:07:24 +0000

    Fieldset unexpectedly stretches to minimum intrinsic width https://bugs.webkit.org/show_bug.cgi?id=79128
    Patch by SravanKumar Sandela <ssandela@innominds.com> on 2012-03-22
    Reviewed by Julien Chaffraix.
    
    Source/WebCore:
    
    Fieldset element width will now check if css width is specified explicitly
    before stretching to minimum intrinsic width. The reference
    can be taken from IE9, instead of FF(FF acknowledged the broken behavior).
    
    Tests: fast/forms/fieldset-width-nostretch-ifspecified-expected.html
           fast/forms/fieldset-width-nostretch-ifspecified.html
    
    * rendering/RenderFieldset.cpp:
    (WebCore::RenderFieldset::stretchesToMinIntrinsicLogicalWidth):
    (WebCore):
    * rendering/RenderFieldset.h:
    (RenderFieldset):
    
    LayoutTests:
    
    Ref tests to check no stretching to intrinsic width if width is explicitly specified.
    Currently, calc width is Work-In-Progress, so it wont be supported now, and these ref
    tests should automatically take care once Webkit enables it. Need to keep an eye on it.
    
    * fast/forms/fieldset-width-nostretch-ifspecified-expected.html: Added.
    * fast/forms/fieldset-width-nostretch-ifspecified.html: Added.
    
    Canonical link: https://commits.webkit.org/99304@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@111818 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderFieldset.h
+++ Source/WebCore/rendering/RenderFieldset.h
@@ -42,7 +42,7 @@ private:
 
     virtual void computePreferredLogicalWidths();
     virtual bool avoidsFloats() const { return true; }
-    virtual bool stretchesToMinIntrinsicLogicalWidth() const { return true; }
+    virtual bool stretchesToMinIntrinsicLogicalWidth() const OVERRIDE;
 
     virtual void paintBoxDecorations(PaintInfo&, const LayoutPoint&);
     virtual void paintMask(PaintInfo&, const LayoutPoint&);
