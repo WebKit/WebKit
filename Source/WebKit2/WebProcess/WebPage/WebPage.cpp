96fc41c99094db7edda6154312d47374ff12248e
Martin Robinson <mrobinson@webkit.org>
2012-03-24 01:25:20 +0000

    Merging r110888
    Canonical link: https://commits.webkit.org/96014.85@webkitgtk/1.8
    git-svn-id: https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-1.8@111962 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -3036,7 +3036,8 @@ static bool pageContainsAnyHorizontalScrollbars(Frame* mainFrame)
 
         for (HashSet<ScrollableArea*>::const_iterator it = scrollableAreas->begin(), end = scrollableAreas->end(); it != end; ++it) {
             ScrollableArea* scrollableArea = *it;
-            ASSERT(scrollableArea->isOnActivePage());
+            if (!scrollableArea->isOnActivePage())
+                continue;
 
             if (hasEnabledHorizontalScrollbar(scrollableArea))
                 return true;
