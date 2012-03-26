b22596c70d22eafd32f2a37685356516ad7def61
Abhishek Arya <inferno@chromium.org>
2012-03-26 06:11:53 +0000

    Crash in ContainerNode::resumePostAttachCallbacks. https://bugs.webkit.org/show_bug.cgi?id=82159
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    Test: plugins/object-onfocus-mutation-crash.html
    
    * dom/ContainerNode.cpp:
    (WebCore::ContainerNode::resumePostAttachCallbacks): dispatching post attach
    callbacks when our attach depth is 1 can fire mutation events such as onfocus
    which can blow away |this|. Need to protect it with a RefPtr.
    * html/HTMLPlugInImageElement.cpp:
    (WebCore::HTMLPlugInImageElement::attach): add calls to suspend attach callbacks
    until the function completes.
    
    LayoutTests:
    
    * plugins/object-onfocus-mutation-crash-expected.txt: Added.
    * plugins/object-onfocus-mutation-crash.html: Added.
    
    
    Canonical link: https://commits.webkit.org/99498@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@112051 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/ContainerNode.cpp
+++ Source/WebCore/dom/ContainerNode.cpp
@@ -673,6 +673,8 @@ void ContainerNode::suspendPostAttachCallbacks()
 void ContainerNode::resumePostAttachCallbacks()
 {
     if (s_attachDepth == 1) {
+        RefPtr<ContainerNode> protect(this);
+
         if (s_postAttachCallbackQueue)
             dispatchPostAttachCallbacks();
         if (s_shouldReEnableMemoryCacheCallsAfterAttach) {
