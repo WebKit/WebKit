527135d9ffd202c033ec12082078065376724313
Jer Noble <jer.noble@apple.com>
2012-03-29 23:03:58 +0000

    Heap-use-after-free in WebCore::InlineFlowBox::deleteLine due to fullscreen issues. https://bugs.webkit.org/show_bug.cgi?id=82055
    Reviewed by David Hyatt.
    
    No new tests; fixes fuzz test crasher which is not reproducible in DRT or WKTR.
    
    When a RenderFullScreen object is inserted between a child and parent renderer, make sure the
    parent renderer deletes its line boxes by calling setNeedsLayoutAndPrefWidthsRecalc().  This
    forces its InlineBox renderers to be removed from the line boxes and their parents in the correct
    order, fixing a double-delete crash.
    
    The same is true when unwrapping the RenderFullScreen object, and when creating and inserting
    the full screen placeholder.
    
    * rendering/RenderFullScreen.cpp:
    (RenderFullScreen::wrapRenderer):
    (RenderFullScreen::unwrapRenderer):
    (RenderFullScreen::createPlaceholder):
    
    Canonical link: https://commits.webkit.org/100003@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@112596 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderFullScreen.cpp
+++ Source/WebCore/rendering/RenderFullScreen.cpp
@@ -113,8 +113,10 @@ RenderObject* RenderFullScreen::wrapRenderer(RenderObject* object, Document* doc
         if (RenderObject* parent = object->parent()) {
             parent->addChild(fullscreenRenderer, object);
             object->remove();
+            parent->setNeedsLayoutAndPrefWidthsRecalc();
         }
         fullscreenRenderer->addChild(object);
+        fullscreenRenderer->setNeedsLayoutAndPrefWidthsRecalc();
     }
     document->setFullScreenRenderer(fullscreenRenderer);
     return fullscreenRenderer;
@@ -127,6 +129,7 @@ void RenderFullScreen::unwrapRenderer()
         while ((child = firstChild())) {
             child->remove();
             parent()->addChild(child, this);
+            parent()->setNeedsLayoutAndPrefWidthsRecalc();
         }
     }
     if (placeholder())
@@ -150,8 +153,10 @@ void RenderFullScreen::createPlaceholder(PassRefPtr<RenderStyle> style, const La
     if (!m_placeholder) {
         m_placeholder = new (document()->renderArena()) RenderFullScreenPlaceholder(this);
         m_placeholder->setStyle(style);
-        if (parent())
+        if (parent()) {
             parent()->addChild(m_placeholder, this);
+            parent()->setNeedsLayoutAndPrefWidthsRecalc();
+        }
     } else
         m_placeholder->setStyle(style);
 }
