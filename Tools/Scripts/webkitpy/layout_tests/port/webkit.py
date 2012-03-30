994480b3d4b33f117b24b65364498bee91712841
Simon Fraser <simon.fraser@apple.com>
2012-03-30 23:00:46 +0000

    run-webkit-tests needs to set DYLD_LIBRARY_PATH as well as DYLD_FRAMEWORK_PATH, so that libWebCoreTestSupport.dylib is found.
    <rdar://problem/11158581>
    
    Reviewed by Mark Rowe.
    
    * Scripts/webkitpy/layout_tests/port/webkit.py:
    (WebKitDriver._start):
    
    Canonical link: https://commits.webkit.org/100118@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@112739 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/port/webkit.py
+++ Tools/Scripts/webkitpy/layout_tests/port/webkit.py
@@ -480,6 +480,7 @@ class WebKitDriver(Driver):
     def _start(self, pixel_tests, per_test_args):
         server_name = self._port.driver_name()
         environment = self._port.setup_environ_for_server(server_name)
+        environment['DYLD_LIBRARY_PATH'] = self._port._build_path()
         environment['DYLD_FRAMEWORK_PATH'] = self._port._build_path()
         # FIXME: We're assuming that WebKitTestRunner checks this DumpRenderTree-named environment variable.
         environment['DUMPRENDERTREE_TEMP'] = str(self._driver_tempdir)
