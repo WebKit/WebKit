e0ca3a166d4200b8bb1447c3182c1d75c4be1275
Simon Fraser <simon.fraser@apple.com>
2012-04-02 21:10:28 +0000

    Fix issue with reflections and composited layers https://bugs.webkit.org/show_bug.cgi?id=82636
    Source/WebCore:
    
    Reviewed by Alexey Proskuryakov
    
    When tearing down GraphicsLayers which referene eachother via m_replicatedLayer/m_replicaLayer,
    we need to clean up the replica layer pointers.
    
    No new tests; tested by existing compositing and repaint tests.
    
    * platform/graphics/GraphicsLayer.cpp:
    (WebCore::GraphicsLayer::~GraphicsLayer):
    (WebCore::GraphicsLayer::setReplicatedByLayer):
    
    LayoutTests:
    
    Reviewed by Alexey Proskuryakov
    
    Unskip some compositing tests that should pass now.
    
    * platform/mac-wk2/Skipped:
    
    Canonical link: https://commits.webkit.org/100298@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@112939 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/GraphicsLayer.cpp
+++ Source/WebCore/platform/graphics/GraphicsLayer.cpp
@@ -99,6 +99,10 @@ GraphicsLayer::~GraphicsLayer()
     if (m_client)
         m_client->verifyNotPainting();
 #endif
+
+    if (m_replicatedLayer)
+        m_replicatedLayer->setReplicatedByLayer(0);
+
     removeAllChildren();
     removeFromParent();
 }
@@ -259,6 +263,12 @@ void GraphicsLayer::noteDeviceOrPageScaleFactorChangedIncludingDescendants()
 
 void GraphicsLayer::setReplicatedByLayer(GraphicsLayer* layer)
 {
+    if (m_replicaLayer == layer)
+        return;
+
+    if (m_replicaLayer)
+        m_replicaLayer->setReplicatedLayer(0);
+
     if (layer)
         layer->setReplicatedLayer(this);
 
