78ebc863ec608b80974f2e554db0e2ea94bbc168
Patrick Gansterer <paroga@webkit.org>
2012-04-02 08:24:13 +0000

    Build fix for ENABLE(SVG) && !ENABLE(FILTERS) after r112813.
    * svg/SVGAnimatedEnumeration.cpp:
    (WebCore::enumerationValueForTargetAttribute):
    
    Canonical link: https://commits.webkit.org/100208@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@112837 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/svg/SVGAnimatedEnumeration.cpp
+++ Source/WebCore/svg/SVGAnimatedEnumeration.cpp
@@ -53,10 +53,20 @@ static inline unsigned short enumerationValueForTargetAttribute(SVGElement* targ
         || attrName == SVGNames::primitiveUnitsAttr)
         return SVGPropertyTraits<SVGUnitTypes::SVGUnitType>::fromString(value);
 
-    if (attrName == SVGNames::edgeModeAttr)
-        return SVGPropertyTraits<EdgeModeType>::fromString(value);
     if (attrName == SVGNames::lengthAdjustAttr)
         return SVGPropertyTraits<SVGLengthAdjustType>::fromString(value);
+    if (attrName == SVGNames::markerUnitsAttr)
+        return SVGPropertyTraits<SVGMarkerUnitsType>::fromString(value);
+    if (attrName == SVGNames::methodAttr)
+        return SVGPropertyTraits<SVGTextPathMethodType>::fromString(value);
+    if (attrName == SVGNames::spacingAttr)
+        return SVGPropertyTraits<SVGTextPathSpacingType>::fromString(value);
+    if (attrName == SVGNames::spreadMethodAttr)
+        return SVGPropertyTraits<SVGSpreadMethodType>::fromString(value);
+
+#if ENABLE(FILTERS)
+    if (attrName == SVGNames::edgeModeAttr)
+        return SVGPropertyTraits<EdgeModeType>::fromString(value);
 
     if (attrName == SVGNames::operatorAttr) {
         if (targetElement->hasTagName(SVGNames::feCompositeTag))
@@ -78,22 +88,15 @@ static inline unsigned short enumerationValueForTargetAttribute(SVGElement* targ
         return SVGPropertyTraits<ComponentTransferType>::fromString(value);
     }
 
-    if (attrName == SVGNames::markerUnitsAttr)
-        return SVGPropertyTraits<SVGMarkerUnitsType>::fromString(value);
-    if (attrName == SVGNames::methodAttr)
-        return SVGPropertyTraits<SVGTextPathMethodType>::fromString(value);
     if (attrName == SVGNames::modeAttr)
         return SVGPropertyTraits<BlendModeType>::fromString(value);
-    if (attrName == SVGNames::spacingAttr)
-        return SVGPropertyTraits<SVGTextPathSpacingType>::fromString(value);
-    if (attrName == SVGNames::spreadMethodAttr)
-        return SVGPropertyTraits<SVGSpreadMethodType>::fromString(value);
     if (attrName == SVGNames::stitchTilesAttr)
         return SVGPropertyTraits<SVGStitchOptions>::fromString(value);
     if (attrName == SVGNames::xChannelSelectorAttr)
         return SVGPropertyTraits<ChannelSelectorType>::fromString(value);
     if (attrName == SVGNames::yChannelSelectorAttr)
         return SVGPropertyTraits<ChannelSelectorType>::fromString(value);
+#endif // ENABLE(FILTERS)
 
     ASSERT_NOT_REACHED();
     return 0;
