463847c751b6aaf6ec6f803d6f2918a24df73548
Justin Novosad <junov@chromium.org>
2012-04-05 22:01:58 +0000

    [Chromium] Correct misleading trace event names in Canvas2DLayerChromium https://bugs.webkit.org/show_bug.cgi?id=83310
    Patch by Justin Novosad <junov@chromium.org> on 2012-04-05
    Reviewed by Adrienne Walker.
    
    * platform/graphics/chromium/Canvas2DLayerChromium.cpp:
    (WebCore::Canvas2DLayerChromium::paintContentsIfDirty):
    
    Canonical link: https://commits.webkit.org/100716@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@113384 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/chromium/Canvas2DLayerChromium.cpp
+++ Source/WebCore/platform/graphics/chromium/Canvas2DLayerChromium.cpp
@@ -121,11 +121,11 @@ void Canvas2DLayerChromium::paintContentsIfDirty(const CCOcclusionTracker* /* oc
         return;
 
     if (m_canvas) {
-        TRACE_EVENT("SkDeferredCanvas::flush", m_canvas, 0);
+        TRACE_EVENT("SkCanvas::flush", m_canvas, 0);
         m_canvas->flush();
     }
 
-    TRACE_EVENT("GrContext::flush", m_context, 0);
+    TRACE_EVENT("GraphicsContext3D::flush", m_context, 0);
     m_context->flush();
 }
 
