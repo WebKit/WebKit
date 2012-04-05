78cd32ebcb32666fabc255d273f5aecdfa25a093
Erik Arvidsson <arv@chromium.org>
2012-04-05 19:08:30 +0000

    HTMLSelectElement does not have a getter for namedItem. This fixes the invalid test https://bugs.webkit.org/show_bug.cgi?id=83289
    Reviewed by Ryosuke Niwa.
    
    http://www.whatwg.org/specs/web-apps/current-work/multipage/the-button-element.html#the-select-element
    
    * fast/dom/HTMLSelectElement/named-options-expected.txt:
    * fast/dom/HTMLSelectElement/script-tests/named-options.js:
    * platform/chromium-mac/fast/dom/HTMLSelectElement/named-options-expected.txt: Removed.
    * platform/chromium-win/fast/dom/HTMLSelectElement/named-options-expected.txt: Removed.
    
    Canonical link: https://commits.webkit.org/100686@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@113351 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/dom/HTMLSelectElement/script-tests/named-options.js
+++ LayoutTests/fast/dom/HTMLSelectElement/script-tests/named-options.js
@@ -1,4 +1,4 @@
-description("This tests that options elements are accessible by name from both a select element and the options collection, per HTML5.");
+description("This tests that option elements are accessible using namedItem from both HTMLSelectElement and HTMLOptionsCollection as well as using the getter from the HTMLOptionsCollection");
 
 var select1 = document.createElement("select");
 document.body.appendChild(select1);
@@ -9,20 +9,19 @@ document.body.appendChild(select2);
 select2.innerHTML = '<option value="Value1" name="test"><option value="Value2" name="test">';
 
 debug("Confirm that the option named 'test' is accessible from the select element");
-shouldBeEqualToString("select1.test.toString()", "[object HTMLOptionElement]");
-shouldBeEqualToString("select1.test.value", "Value");
+shouldBeEqualToString("select1.namedItem('test').toString()", "[object HTMLOptionElement]");
+shouldBeEqualToString("select1.namedItem('test').value", "Value");
 
 debug("Confirm that the option named 'test' is accessible from the options collection");
-shouldBeEqualToString("select1.options.test.toString()", "[object HTMLOptionElement]");
-shouldBeEqualToString("select1.options.test.value", "Value");
-
-debug("Confirm that both options named 'test' are accessible from the select element");
-shouldBe("select2.test.length", "2");
-shouldBeEqualToString("select2.test.toString()", "[object NodeList]");
-shouldBeEqualToString("select2.test[0].value", "Value1");
-shouldBeEqualToString("select2.test[1].value", "Value2");
+shouldBeEqualToString("select1.options.namedItem('test').toString()", "[object HTMLOptionElement]");
+shouldBeEqualToString("select1.options.namedItem('test').value", "Value");
 
 debug("Confirm that both options named 'test' are accessible from the options collection");
+shouldBe("select2.options.namedItem('test').length", "2");
+shouldBeEqualToString("select2.options.namedItem('test').toString()", "[object NodeList]");
+shouldBeEqualToString("select2.options.namedItem('test')[0].value", "Value1");
+shouldBeEqualToString("select2.options.namedItem('test')[1].value", "Value2");
+
 shouldBe("select2.options.test.length", "2");
 shouldBeEqualToString("select2.options.test.toString()", "[object NodeList]");
 shouldBeEqualToString("select2.options.test[0].value", "Value1");
