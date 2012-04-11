4df289039ff9edbf955ff9cd2972d967599a089c
Mark Rowe <mrowe@apple.com>
2012-04-11 23:08:13 +0000

    Remove a fprintf from LayoutTestController.
    This fprintf is causing 100+MB of data to be written to stderr during a single run of the tests.
    It was added for no apparent reason by Ryosuke in r113651.
    
    * DumpRenderTree/LayoutTestController.cpp:
    (LayoutTestController::redirectionDestinationForURL):
    
    Canonical link: https://commits.webkit.org/101215@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@113920 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/DumpRenderTree/LayoutTestController.cpp
+++ Tools/DumpRenderTree/LayoutTestController.cpp
@@ -2552,7 +2552,6 @@ void LayoutTestController::addURLToRedirect(std::string origin, std::string dest
 
 const std::string& LayoutTestController::redirectionDestinationForURL(std::string origin)
 {
-    fprintf(stderr, "[%s]\n", origin.c_str());
     return m_URLsToRedirect[origin];
 }
 
