f3d98311c1d211e645be0a7a323ca648d8acc432
Nico Weber <thakis@chromium.org>
2012-04-11 20:16:47 +0000

    [mac] Suppress deprecation warning for GlobalToLocal in the carbon parts of the test plugin. https://bugs.webkit.org/show_bug.cgi?id=83709
    Reviewed by Anders Carlsson.
    
    * DumpRenderTree/TestNetscapePlugIn/main.cpp:
    (handleEventCarbon):
    
    
    Canonical link: https://commits.webkit.org/101198@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@113901 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/DumpRenderTree/TestNetscapePlugIn/main.cpp
+++ Tools/DumpRenderTree/TestNetscapePlugIn/main.cpp
@@ -457,7 +457,14 @@ static int16_t handleEventCarbon(NPP instance, PluginObject* obj, EventRecord* e
             break;
         case mouseDown:
             if (obj->eventLogging) {
+#if __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
                 GlobalToLocal(&pt);
+#if __clang__
+#pragma clang diagnostic pop
+#endif
                 pluginLog(instance, "mouseDown at (%d, %d)", pt.h, pt.v);
             }
             if (obj->evaluateScriptOnMouseDownOrKeyDown && obj->mouseDownForEvaluateScript)
@@ -465,7 +472,14 @@ static int16_t handleEventCarbon(NPP instance, PluginObject* obj, EventRecord* e
             break;
         case mouseUp:
             if (obj->eventLogging) {
+#if __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
                 GlobalToLocal(&pt);
+#if __clang__
+#pragma clang diagnostic pop
+#endif
                 pluginLog(instance, "mouseUp at (%d, %d)", pt.h, pt.v);
             }
             break;
