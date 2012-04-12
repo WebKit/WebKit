76c4e10d8edb81b3accf378bc451807a7c16e689
Yury Semikhatsky <yurys@chromium.org>
2012-04-12 12:19:23 +0000

    2012-04-12  Yury Semikhatsky  <yurys@chromium.org>
            Unreviewed. Fix a couple of compiler annotations after r113963.
    
            * inspector/front-end/TopDownProfileDataGridTree.js:
            (WebInspector.TopDownProfileDataGridTree):
            (WebInspector.TopDownProfileDataGridTree.prototype.exclude):
    
    
    Canonical link: https://commits.webkit.org/101259@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@113964 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/TopDownProfileDataGridTree.js
+++ Source/WebCore/inspector/front-end/TopDownProfileDataGridTree.js
@@ -81,7 +81,7 @@ WebInspector.TopDownProfileDataGridTree = function(/*ProfileView*/ profileView, 
     this._remainingChildren = profileNode.children;
 
     var any = /** @type{*} */this;
-    var node = /** @type{WebInspector.ProfileDataGridNode} */
+    var node = /** @type{WebInspector.ProfileDataGridNode} */any;
     WebInspector.TopDownProfileDataGridNode.prototype._populate.call(node);
 }
 
@@ -108,7 +108,7 @@ WebInspector.TopDownProfileDataGridTree.prototype = {
         var excludedCallUID = profileDataGrideNode.callUID;
 
         var any = /** @type{*} */this;
-        var node = /** @type{WebInspector.ProfileDataGridNode} */
+        var node = /** @type{WebInspector.ProfileDataGridNode} */any;
         WebInspector.TopDownProfileDataGridNode.prototype._exclude.call(node, excludedCallUID);
 
         if (this.lastComparator)
