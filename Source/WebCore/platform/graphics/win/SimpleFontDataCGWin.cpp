d6007a5a7d5f28fbe081d5cc848284d60fdb7157
Pravinin D <pravind.2k4@gmail.com>
2012-04-12 16:00:09 +0000

    font-size:0 text runs has non zero width in case of Windows Safari. https://bugs.webkit.org/show_bug.cgi?id=83559
    Patch by Pravinin D <pravind.2k4@gmail.com> on 2012-04-12
    Reviewed by Dan Bernstein.
    
    Test case already exits.
    LayoutTest/fast/text/font-size-zero.html
    
    * platform/graphics/win/SimpleFontDataCGWin.cpp:
    (WebCore):
    (WebCore::SimpleFontData::platformBoundsForGlyph):
    (WebCore::SimpleFontData::platformWidthForGlyph):
    
    Canonical link: https://commits.webkit.org/101284@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@113989 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/win/SimpleFontDataCGWin.cpp
+++ Source/WebCore/platform/graphics/win/SimpleFontDataCGWin.cpp
@@ -29,9 +29,9 @@
 #include "config.h"
 #include "SimpleFontData.h"
 
+#include "FloatRect.h"
 #include "Font.h"
 #include "FontCache.h"
-#include "FloatRect.h"
 #include "FontDescription.h"
 #include "HWndDC.h"
 #include "PlatformString.h"
@@ -46,7 +46,7 @@
 
 namespace WebCore {
 
-using std::max;
+using namespace std;
 
 void SimpleFontData::platformInit()
 {
@@ -103,6 +103,9 @@ void SimpleFontData::platformInit()
 
 FloatRect SimpleFontData::platformBoundsForGlyph(Glyph glyph) const
 {
+    if (!platformData().size())
+        return FloatRect();
+
     if (m_platformData.useGDI())
         return boundsForGDIGlyph(glyph);
 
@@ -119,6 +122,9 @@ FloatRect SimpleFontData::platformBoundsForGlyph(Glyph glyph) const
 
 float SimpleFontData::platformWidthForGlyph(Glyph glyph) const
 {
+    if (!platformData().size())
+        return 0;
+
     if (m_platformData.useGDI())
         return widthForGDIGlyph(glyph);
 
