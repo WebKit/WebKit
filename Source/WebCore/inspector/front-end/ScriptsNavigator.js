c04933247a0c00ac66e6a8457db2907ace95fc92
Pavel Feldman <pfeldman@chromium.org>
2012-04-17 13:03:55 +0000

    Web Inspector: [regression r112413]content scripts tree outline is not added to the tab. https://bugs.webkit.org/show_bug.cgi?id=84146
    Reviewed by Vsevolod Vlasov.
    
    Adding the tree to the container.
    
    * inspector/front-end/ScriptsNavigator.js:
    (WebInspector.ScriptsNavigator):
    
    Canonical link: https://commits.webkit.org/101638@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@114368 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/ScriptsNavigator.js
+++ Source/WebCore/inspector/front-end/ScriptsNavigator.js
@@ -47,27 +47,34 @@ WebInspector.ScriptsNavigator = function()
     this._tabbedPane.element.appendChild(this._treeSearchBox);
 
     var scriptsTreeElement = document.createElement("ol");
-    var scriptsView = new WebInspector.View();
-    scriptsView.element.addStyleClass("fill");
-    scriptsView.element.addStyleClass("navigator-container");
+    this._scriptsTree = new WebInspector.NavigatorTreeOutline(this, scriptsTreeElement);
+
     var scriptsOutlineElement = document.createElement("div");
     scriptsOutlineElement.addStyleClass("outline-disclosure");
     scriptsOutlineElement.addStyleClass("navigator");
     scriptsOutlineElement.appendChild(scriptsTreeElement);
+
+    var scriptsView = new WebInspector.View();
+    scriptsView.element.addStyleClass("fill");
+    scriptsView.element.addStyleClass("navigator-container");
     scriptsView.element.appendChild(scriptsOutlineElement);
-    this._scriptsTree = new WebInspector.NavigatorTreeOutline(this, scriptsTreeElement);
+
     this._tabbedPane.appendTab(WebInspector.ScriptsNavigator.ScriptsTab, WebInspector.UIString("Scripts"), scriptsView);
     this._tabbedPane.selectTab(WebInspector.ScriptsNavigator.ScriptsTab);
 
     var contentScriptsTreeElement = document.createElement("ol");
-    var contentScriptsView = new WebInspector.View();
-    contentScriptsView.element.addStyleClass("fill");
-    contentScriptsView.element.addStyleClass("navigator-container");
+    this._contentScriptsTree = new WebInspector.NavigatorTreeOutline(this, contentScriptsTreeElement);
+
     var contentScriptsOutlineElement = document.createElement("div");
     contentScriptsOutlineElement.addStyleClass("outline-disclosure");
     contentScriptsOutlineElement.addStyleClass("navigator");
     contentScriptsOutlineElement.appendChild(contentScriptsTreeElement);
-    this._contentScriptsTree = new WebInspector.NavigatorTreeOutline(this, contentScriptsTreeElement);
+
+    var contentScriptsView = new WebInspector.View();
+    contentScriptsView.element.addStyleClass("fill");
+    contentScriptsView.element.addStyleClass("navigator-container");
+    contentScriptsView.element.appendChild(contentScriptsOutlineElement);
+
     this._tabbedPane.appendTab(WebInspector.ScriptsNavigator.ContentScriptsTab, WebInspector.UIString("Content scripts"), contentScriptsView);
 
     this._snippetsTree = this._createSnippetsTree();
@@ -472,7 +479,7 @@ WebInspector.ScriptsNavigator.prototype = {
         
         if ((domain === "" && folderName === "") || !showScriptFolders)
             return isContentScript ? this._contentScriptsTree : this._scriptsTree;
-        
+
         var parentFolderElement;
         if (folderName === "")
             parentFolderElement = isContentScript ? this._contentScriptsTree : this._scriptsTree;
