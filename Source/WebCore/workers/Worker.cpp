0fe88bb6c8ffcc19e1ab382bff5afdaafe3e6425
Li Yin <li.yin@intel.com>
2012-04-25 14:52:06 +0000

    [chromium][workers] setTargetType(ResourceRequest::TargetIsWorker) is repeatedly called in chromium https://bugs.webkit.org/show_bug.cgi?id=84542
    Patch by Li Yin <li.yin@intel.com> on 2012-04-25
    Reviewed by David Levin.
    
    In chromium platform, the default value of m_targetType is ResourceRequest::TargetIsWorker
    it isn't necessary to call
    worker->m_scriptLoader->setTargetType(ResourceRequest::TargetIsWorker) again in Worker.cpp
    In chromium platform, the m_targettype value determines the priority of resource loaded.
    
    No new tests because this patch just deletes a repeated code, it can reduce code workload and can't impact any feature.
    If the target type isn't set correctly, the following test maybe fail because of timeout in the chromium.
    fast/workers/storage/multiple-transactions-on-different-handles.html
    fast/workers/storage/multiple-transactions-on-different-handles-sync.html
    fast/workers/storage/interrupt-database.html
    
    * workers/Worker.cpp:
    (WebCore::Worker::create):
    
    Canonical link: https://commits.webkit.org/102361@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@115207 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/workers/Worker.cpp
+++ Source/WebCore/workers/Worker.cpp
@@ -70,7 +70,7 @@ PassRefPtr<Worker> Worker::create(ScriptExecutionContext* context, const String&
     worker->setPendingActivity(worker.get());
 
     worker->m_scriptLoader = WorkerScriptLoader::create();
-#if PLATFORM(CHROMIUM) || PLATFORM(BLACKBERRY)
+#if PLATFORM(BLACKBERRY)
     worker->m_scriptLoader->setTargetType(ResourceRequest::TargetIsWorker);
 #endif
     worker->m_scriptLoader->loadAsynchronously(context, scriptURL, DenyCrossOriginRequests, worker.get());
