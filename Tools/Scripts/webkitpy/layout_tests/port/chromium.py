73361a6b21802fbd9ea521714d18b3be156b7188
Dirk Pranke <dpranke@chromium.org>
2012-04-25 00:28:14 +0000

    nrwt: add ASAN handling to chromium crash logs https://bugs.webkit.org/show_bug.cgi?id=84664
    Reviewed by Ojan Vafai.
    
    * Scripts/webkitpy/layout_tests/port/chromium.py:
    (ChromiumPort._get_crash_log):
    
    Canonical link: https://commits.webkit.org/102300@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@115146 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/port/chromium.py
+++ Tools/Scripts/webkitpy/layout_tests/port/chromium.py
@@ -333,6 +333,16 @@ class ChromiumPort(Port):
         repos.append(('chromium', self.path_from_chromium_base('build')))
         return repos
 
+    def _get_crash_log(self, name, pid, stdout, stderr, newer_than):
+        new_stderr = stderr
+        if stderr and 'AddressSanitizer' in stderr:
+            asan_filter_path = self.path_from_chromium_base('third_party', 'asan', 'scripts', 'asan_symbolize.py')
+            if self._filesystem.exists(asan_filter_path):
+                output = self._executive.run_command([asan_filter_path], input=stderr)
+                new_stderr = self._executive.run_command(['c++filt'], input=output)
+
+        return super(ChromiumPort, self)._get_crash_log(name, pid, stdout, new_stderr, newer_than)
+
     def virtual_test_suites(self):
         return [
             VirtualTestSuite('platform/chromium/virtual/gpu/fast/canvas',
