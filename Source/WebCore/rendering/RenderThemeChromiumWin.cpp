45b0c5e37c9be632f2552c9d0d018d830c2b60b9
Nico Weber <thakis@chromium.org>
2012-04-27 03:32:42 +0000

    [chromium] Fix C++ language use. https://bugs.webkit.org/show_bug.cgi?id=85015
    Reviewed by James Robinson.
    
    Even though MSVC allows it, a sizeof followed by a non-parenthesized
    typename is not valid C++.
    
    No functionality change.
    
    * rendering/RenderThemeChromiumWin.cpp:
    (WebCore):
    (WebCore::getNonClientMetrics):
    
    
    Canonical link: https://commits.webkit.org/102541@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@115404 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderThemeChromiumWin.cpp
+++ Source/WebCore/rendering/RenderThemeChromiumWin.cpp
@@ -137,7 +137,7 @@ bool ThemePainter::s_hasInstance = false;
 static void getNonClientMetrics(NONCLIENTMETRICS* metrics)
 {
     static UINT size = (windowsVersion() >= WindowsVista) ?
-        (sizeof NONCLIENTMETRICS) : NONCLIENTMETRICS_SIZE_PRE_VISTA;
+        sizeof(NONCLIENTMETRICS) : NONCLIENTMETRICS_SIZE_PRE_VISTA;
     metrics->cbSize = size;
     bool success = !!SystemParametersInfo(SPI_GETNONCLIENTMETRICS, size, metrics, 0);
     ASSERT(success);
