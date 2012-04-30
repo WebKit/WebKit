c7353cf82c239433a8a3208d1987db2ec70d6585
Raphael Kubo da Costa <rakuco@webkit.org>
2012-04-30 21:02:51 +0000

    [CMake] Add a proper license to FindSqlite.cmake. https://bugs.webkit.org/show_bug.cgi?id=84901
    Reviewed by Daniel Bates.
    
    FindSqlite.cmake referenced COPYING-CMAKE-SCRIPTS, which was
    supposed to contain its license. This file, however, was not
    imported with FindSqlite.cmake into the tree.
    
    Add it from kdelibs (where the Find file came from) at git
    revision [1]. Also worth mentioning is that Alexander Neundorf
    added Gilles Caulier as the script author in revision [2].
    
    [1] https://projects.kde.org/projects/kde/kdelibs/repository/revisions/c27925edf98b9952aeada677dfc74ce9c809c48a
    [2] https://projects.kde.org/projects/kde/kdelibs/repository/revisions/14d0a92e04e1a200d6e58397e57a29194a0819cf
    
    * Source/cmake/FindSqlite.cmake:
    
    Canonical link: https://commits.webkit.org/102767@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@115663 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/FindSqlite.cmake
+++ Source/cmake/FindSqlite.cmake
@@ -5,15 +5,31 @@
 #  SQLITE_INCLUDE_DIR - the Sqlite include directory
 #  SQLITE_LIBRARIES - Link these to use Sqlite
 #  SQLITE_DEFINITIONS - Compiler switches required for using Sqlite
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
-
-
 # Copyright (c) 2008, Gilles Caulier, <caulier.gilles@gmail.com>
 #
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 if ( SQLITE_INCLUDE_DIR AND SQLITE_LIBRARIES )
    # in cache already
