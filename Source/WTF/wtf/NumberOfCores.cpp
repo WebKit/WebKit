aea6fed118135999588e0ea5c74cbf976f9e676b
Jeff Rogers <jrogers@rim.com>
2012-05-04 20:13:10 +0000

    [BlackBerry] Implement numberOfProcessorCores() for QNX https://bugs.webkit.org/show_bug.cgi?id=85638
    Patch by Jeff Rogers <jrogers@rim.com> on 2012-05-04
    Reviewed by Antonio Gomes.
    
    * wtf/NumberOfCores.cpp:
    (WTF::numberOfProcessorCores):
    
    Canonical link: https://commits.webkit.org/103225@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@116157 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/NumberOfCores.cpp
+++ Source/WTF/wtf/NumberOfCores.cpp
@@ -34,9 +34,13 @@
 #include <sys/sysctl.h>
 #elif OS(LINUX) || OS(AIX) || OS(SOLARIS)
 #include <unistd.h>
-#elif OS(WINDOWS)
-#include <windows.h>
+#elif OS(WINDOWS) || OS(QNX)
 #include <wtf/UnusedParam.h>
+#if OS(WINDOWS)
+#include <windows.h>
+#elif OS(QNX)
+#include <sys/syspage.h>
+#endif
 #endif
 
 namespace WTF {
@@ -69,6 +73,10 @@ int numberOfProcessorCores()
     GetSystemInfo(&sysInfo);
 
     s_numberOfCores = sysInfo.dwNumberOfProcessors;
+#elif OS(QNX)
+    UNUSED_PARAM(defaultIfUnavailable);
+
+    s_numberOfCores = _syspage_ptr->num_cpu;
 #else
     s_numberOfCores = defaultIfUnavailable;
 #endif
