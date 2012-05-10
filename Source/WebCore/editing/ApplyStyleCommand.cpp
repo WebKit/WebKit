76ba4fffbde00aa4b81967111d7b01ea10aa307f
Abhishek Arya <inferno@chromium.org>
2012-05-10 18:39:43 +0000

    Crash in ApplyStyleCommand::joinChildTextNodes. https://bugs.webkit.org/show_bug.cgi?id=85939
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Test: editing/style/apply-style-join-child-text-nodes-crash.html
    
    * editing/ApplyStyleCommand.cpp:
    (WebCore::ApplyStyleCommand::applyRelativeFontStyleChange): add conditions
    to bail out if our start and end position nodes are removed due to
    mutation events in joinChildTextNodes.
    (WebCore::ApplyStyleCommand::applyInlineStyle): this executes after
    applyRelativeFontStyleChange in ApplyStyleCommand::doApply. So, need
    to bail out if our start and end position nodes are removed due to
    mutation events.
    (WebCore::ApplyStyleCommand::joinChildTextNodes): hold all the children
    in a ref vector to prevent them from getting destroyed due to mutation events.
    
    LayoutTests:
    
    * editing/style/apply-style-join-child-text-nodes-crash-expected.txt: Added.
    * editing/style/apply-style-join-child-text-nodes-crash.html: Added.
    
    Canonical link: https://commits.webkit.org/103710@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@116669 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/editing/ApplyStyleCommand.cpp
+++ Source/WebCore/editing/ApplyStyleCommand.cpp
@@ -320,11 +320,18 @@ void ApplyStyleCommand::applyRelativeFontStyleChange(EditingStyle* style)
         start = startPosition();
         end = endPosition();
     }
+    
+    if (start.isNull() || end.isNull())
+        return;
+
     if (end.deprecatedNode()->isTextNode() && start.deprecatedNode()->parentNode() != end.deprecatedNode()->parentNode()) {
         joinChildTextNodes(end.deprecatedNode()->parentNode(), start, end);
         start = startPosition();
         end = endPosition();
     }
+
+    if (start.isNull() || end.isNull())
+        return;
 
     // Split the start text nodes if needed to apply style.
     if (isValidCaretPositionInTextNode(start)) {
@@ -542,6 +549,10 @@ void ApplyStyleCommand::applyInlineStyle(EditingStyle* style)
     // adjust to the positions we want to use for applying style
     Position start = startPosition();
     Position end = endPosition();
+
+    if (start.isNull() || end.isNull())
+        return;
+
     if (comparePositions(end, start) < 0) {
         Position swap = start;
         start = end;
@@ -1427,24 +1438,29 @@ void ApplyStyleCommand::joinChildTextNodes(Node* node, const Position& start, co
     Position newStart = start;
     Position newEnd = end;
 
-    Node* child = node->firstChild();
-    while (child) {
-        Node* next = child->nextSibling();
-        if (child->isTextNode() && next && next->isTextNode()) {
-            Text* childText = toText(child);
-            Text* nextText = toText(next);
-            if (start.anchorType() == Position::PositionIsOffsetInAnchor && next == start.containerNode())
-                newStart = Position(childText, childText->length() + start.offsetInContainerNode());
-            if (end.anchorType() == Position::PositionIsOffsetInAnchor && next == end.containerNode())
-                newEnd = Position(childText, childText->length() + end.offsetInContainerNode());
-            String textToMove = nextText->data();
-            insertTextIntoNode(childText, childText->length(), textToMove);
-            removeNode(next);
-            // don't move child node pointer. it may want to merge with more text nodes.
-        }
-        else {
-            child = child->nextSibling();
-        }
+    Vector<RefPtr<Text> > textNodes;
+    for (Node* curr = node->firstChild(); curr; curr = curr->nextSibling()) {
+        if (!curr->isTextNode())
+            continue;
+        
+        textNodes.append(toText(curr));
+    }
+
+    for (size_t i = 0; i < textNodes.size(); ++i) {
+        Text* childText = textNodes[i].get();
+        Node* next = childText->nextSibling();
+        if (!next || !next->isTextNode())
+            continue;
+    
+        Text* nextText = toText(next);
+        if (start.anchorType() == Position::PositionIsOffsetInAnchor && next == start.containerNode())
+            newStart = Position(childText, childText->length() + start.offsetInContainerNode());
+        if (end.anchorType() == Position::PositionIsOffsetInAnchor && next == end.containerNode())
+            newEnd = Position(childText, childText->length() + end.offsetInContainerNode());
+        String textToMove = nextText->data();
+        insertTextIntoNode(childText, childText->length(), textToMove);
+        removeNode(next);
+        // don't move child node pointer. it may want to merge with more text nodes.
     }
 
     updateStartEnd(newStart, newEnd);
