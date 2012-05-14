6144059ddaa57dd3b16454aa4b289b1c3d71f55f
Darin Adler <darin@apple.com>
2012-05-14 05:34:41 +0000

    [CG] Unneeded CFRetain/Release of CGImageRef in BitmapImage::draw https://bugs.webkit.org/show_bug.cgi?id=86332
    Reviewed by Dan Bernstein.
    
    * platform/graphics/cg/ImageCG.cpp:
    (WebCore::BitmapImage::draw): Don't use RetainPtr since the result of
    the function is used before anything could call CFRelease on it.
    
    
    Canonical link: https://commits.webkit.org/103916@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@116919 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/cg/ImageCG.cpp
+++ Source/WebCore/platform/graphics/cg/ImageCG.cpp
@@ -192,7 +192,7 @@ void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& destRect, const F
 {
     startAnimation();
 
-    RetainPtr<CGImageRef> image = frameAtIndex(m_currentFrame);
+    CGImageRef image = frameAtIndex(m_currentFrame);
     if (!image) // If it's too early we won't have an image yet.
         return;
     
@@ -207,7 +207,7 @@ void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& destRect, const F
     if (shouldRespectImageOrientation == RespectImageOrientation)
         orientation = frameOrientationAtIndex(m_currentFrame);
 
-    ctxt->drawNativeImage(image.get(), selfSize, styleColorSpace, destRect, srcRect, compositeOp, orientation);
+    ctxt->drawNativeImage(image, selfSize, styleColorSpace, destRect, srcRect, compositeOp, orientation);
 
     if (imageObserver())
         imageObserver()->didDraw(this);
