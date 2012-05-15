90029467a75f108ffe3497424ba2e62038c83427
Mike West <mkwst@chromium.org>
2012-05-15 19:51:45 +0000

    Fixing the flaky 'script-src-redirect.html' test. https://bugs.webkit.org/show_bug.cgi?id=86433
    Patch by Mike West <mkwst@chromium.org> on 2012-05-15
    Reviewed by Adam Barth.
    
    * http/tests/security/contentSecurityPolicy/resources/source-list-parsing-base.js:
    (test):
    * http/tests/security/contentSecurityPolicy/script-src-redirect.html:
    
    Canonical link: https://commits.webkit.org/104094@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@117123 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/security/contentSecurityPolicy/resources/source-list-parsing-base.js
+++ LayoutTests/http/tests/security/contentSecurityPolicy/resources/source-list-parsing-base.js
@@ -8,10 +8,13 @@ function test() {
     if (tests.length === 0)
         return finishTesting();
 
+    var defaultScript = 'http://127.0.0.1:8000/security/contentSecurityPolicy/resources/script.js'
     var current = tests.shift();
     var iframe = document.createElement('iframe');
-    iframe.src = "http://127.0.0.1:8000/security/contentSecurityPolicy/resources/echo-script-src.pl?should_run=" + current[0] +
-                 "&q=http://127.0.0.1:8000/security/contentSecurityPolicy/resources/script.js&csp=" + escape(current[1]);
+    iframe.src = "http://127.0.0.1:8000/security/contentSecurityPolicy/resources/echo-script-src.pl?" +
+                 "should_run=" + escape(current[0]) +
+                 "&csp=" + escape(current[1]) +
+                 "&q=" + escape(current[2] ? current[2] : defaultScript);
     iframe.onload = test;
     document.body.appendChild(iframe);
 }
