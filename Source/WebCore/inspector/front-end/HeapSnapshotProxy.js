6556aee22628a31f0d1ffc7f58563cd71f0b835f
Yury Semikhatsky <yurys@chromium.org>
2012-05-15 11:10:22 +0000

    Web Inspector: exception when switching to heap profiler comparison view https://bugs.webkit.org/show_bug.cgi?id=86224
    Reviewed by Pavel Feldman.
    
    Make sure the messages are dispatched in the same order as they are sent in
    case a fake worker is used for heap snapshot processing.
    
    * inspector/front-end/HeapSnapshotProxy.js:
    (WebInspector.TimerQueue):
    (WebInspector.TimerQueue.prototype.addTask):
    (WebInspector.TimerQueue.prototype._onTimeout):
    (WebInspector.TimerQueue.prototype._scheduleTimer):
    (WebInspector.HeapSnapshotFakeWorker):
    (WebInspector.HeapSnapshotFakeWorker.prototype.postMessage):
    (WebInspector.HeapSnapshotFakeWorker.prototype._postMessageFromWorker):
    
    Canonical link: https://commits.webkit.org/104041@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@117050 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/HeapSnapshotProxy.js
+++ Source/WebCore/inspector/front-end/HeapSnapshotProxy.js
@@ -78,11 +78,51 @@ WebInspector.HeapSnapshotRealWorker.prototype.__proto__ = WebInspector.HeapSnaps
 
 /**
  * @constructor
+ */
+WebInspector.AsyncTaskQueue = function()
+{
+    this._queue = [];
+    this._isTimerSheduled = false;
+}
+
+WebInspector.AsyncTaskQueue.prototype = {
+    /**
+     * @param {function()} task
+     */
+    addTask: function(task)
+    {
+        this._queue.push(task);
+        this._scheduleTimer();
+    },
+
+    _onTimeout: function()
+    {
+        this._isTimerSheduled = false;
+        var task = this._queue.shift();
+        try {
+            task();
+        } finally {
+            this._scheduleTimer();
+        }
+    },
+
+    _scheduleTimer: function()
+    {
+        if (this._queue.length && !this._isTimerSheduled) {
+            setTimeout(this._onTimeout.bind(this), 0);
+            this._isTimerSheduled = true;
+        }
+    }
+}
+
+/**
+ * @constructor
  * @extends {WebInspector.HeapSnapshotWorkerWrapper}
  */
 WebInspector.HeapSnapshotFakeWorker = function()
 {
     this._dispatcher = new WebInspector.HeapSnapshotWorkerDispatcher(window, this._postMessageFromWorker.bind(this));
+    this._asyncTaskQueue = new WebInspector.AsyncTaskQueue();
 }
 
 WebInspector.HeapSnapshotFakeWorker.prototype = {
@@ -93,7 +133,7 @@ WebInspector.HeapSnapshotFakeWorker.prototype = {
             if (this._dispatcher)
                 this._dispatcher.dispatchMessage({data: message});
         }
-        setTimeout(dispatch.bind(this), 0);
+        this._asyncTaskQueue.addTask(dispatch.bind(this));
     },
 
     terminate: function()
@@ -107,7 +147,7 @@ WebInspector.HeapSnapshotFakeWorker.prototype = {
         {
             this.dispatchEventToListeners("message", message);
         }
-        setTimeout(send.bind(this), 0);
+        this._asyncTaskQueue.addTask(send.bind(this));
     }
 };
 
