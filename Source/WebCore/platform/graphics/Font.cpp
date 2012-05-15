e85d918971d8a3fe6f858c3214c0426318444c32
Darin Adler <darin@apple.com>
2012-05-15 18:53:21 +0000

    Font copy constructor unnecessarily recalculates transcoding https://bugs.webkit.org/show_bug.cgi?id=86490
    Reviewed by Dan Bernstein.
    
    * platform/graphics/Font.cpp:
    (WebCore::Font::Font): Copy m_needsTranscoding instead of recomputing it.
    
    
    Canonical link: https://commits.webkit.org/104086@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@117109 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/Font.cpp
+++ Source/WebCore/platform/graphics/Font.cpp
@@ -93,7 +93,7 @@ Font::Font(const Font& other)
     , m_letterSpacing(other.m_letterSpacing)
     , m_wordSpacing(other.m_wordSpacing)
     , m_isPlatformFont(other.m_isPlatformFont)
-    , m_needsTranscoding(fontTranscoder().needsTranscoding(other.m_fontDescription))
+    , m_needsTranscoding(other.m_needsTranscoding)
 {
 }
 
