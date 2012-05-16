154081268efea0943043bc7010f37eee26a547da
Gustavo Noronha Silva <kov@webkit.org>
2012-05-16 13:57:24 +0000

    Merge 113099 - Crash in SelectorChecker::checkOneSelector. https://bugs.webkit.org/show_bug.cgi?id=83040
    Reviewed by Antti Koivisto.
    
    Source/WebCore:
    
    Test: fast/css/css-set-selector-text-crash.html
    
    Removing the early bail when we detect that our selector text
    hasn't changed, and we don't notify the styleSelectorChanged.
    In fact, when we adopt the new selector list, the old one will
    get destroyed and the styleSelectorChanged call needs to be made.
    
    * css/CSSStyleRule.cpp:
    (WebCore::CSSStyleRule::setSelectorText):
    
    LayoutTests:
    
    * fast/css/css-set-selector-text-crash-expected.txt: Added.
    * fast/css/css-set-selector-text-crash.html: Added.
    
    Canonical link: https://commits.webkit.org/96014.183@webkitgtk/1.8
    git-svn-id: https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-1.8@117283 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/CSSStyleRule.cpp
+++ Source/WebCore/css/CSSStyleRule.cpp
@@ -113,9 +113,6 @@ void CSSStyleRule::setSelectorText(const String& selectorText)
         selectorTextCache().set(this, generateSelectorText());
     }
 
-    if (this->selectorText() == oldSelectorText)
-        return;
-
     doc->styleSelectorChanged(DeferRecalcStyle);
 }
 
