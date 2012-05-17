WebKit/WebKit

commit 3a25c30e33712835c4cd490e478eebccfbbd7746

Malformed GIF can cause decoder to read off end of heap buffer https://bugs.webkit.org/show_bug.cgi?id=86531
Reviewed by Adam Barth.

Source/WebCore:

Test: fast/images/read-past-end-of-buffer.html
This test is only expected to catch problems if run under Address
Sanitizer or a similar memory-checking utility.

* platform/image-decoders/gif/GIFImageReader.cpp:
(GIFImageReader::read):

LayoutTests:

The test here is only expected to catch problems if run under Address
Sanitizer or a similar memory-checking utility.

* fast/images/read-past-end-of-buffer-expected.txt: Added.
* fast/images/read-past-end-of-buffer.html: Added.
* fast/images/resources/wrong-block-length.gif: Added.



Canonical link: https://commits.webkit.org/104251@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@117333 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/image-decoders/gif/GIFImageReader.cpp
@@ -581,21 +581,30 @@ bool GIFImageReader::read(const unsigned char *buf, unsigned len,
 
     case gif_extension:
     {
-      int len = count = q[1];
+      count = q[1];
       gstate es = gif_skip_block;
 
+      // NOTE: We use the spec-mandated block lengths below where applicable,
+      // instead of the value in q[1]. If the two disagree, the GIF is
+      // malformed; the main thing we want to avoid is having the GIF specify a
+      // smaller number in q[1] than the spec requires and then having our
+      // processing code for these extensions blindly read off the end of a
+      // too-short heap buffer.
       switch (*q)
       {
       case 0xf9:
         es = gif_control_extension;
+        count = 4;
         break;
 
       case 0x01:
         // ignoring plain text extension
+        count = 12;
         break;
 
       case 0xff:
         es = gif_application_extension;
+        count = 11;
         break;
 
       case 0xfe:
@@ -603,8 +612,8 @@ bool GIFImageReader::read(const unsigned char *buf, unsigned len,
         break;
       }
 
-      if (len)
-        GETN(len, es);
+      if (count)
+        GETN(count, es);
       else
         GETN(1, gif_image_start);
     }

LayoutTests/fast/images/read-past-end-of-buffer-expected.txt
@@ -1,0 +1,2 @@
+This test passes if no heap errors are detected during decoding.
+

LayoutTests/fast/images/read-past-end-of-buffer.html
@@ -1,0 +1,6 @@
+<script>
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+</script>
+This test passes if no heap errors are detected during decoding.<br>
+<img src=resources/wrong-block-length.gif>

commit 073153a4d38bcaf683a251169799a88b1b86da4f

Correctly display malformed GIFs which specify bogus extension block sizes. https://bugs.webkit.org/show_bug.cgi?id=86531
Reviewed by James Robinson.

This was broken by r117333, which was an attempt to avoid memory errors
on GIFs that were malformed in a similar way.  It turns out some GIFs
in the wild (i.e. "our LayoutTests directory") relied on some of the
effects of the old code.  This refixes in a way that doesn't break
these.

No new tests, covered by existing tests.

* platform/image-decoders/gif/GIFImageReader.cpp:
(GIFImageReader::read):



Canonical link: https://commits.webkit.org/104283@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@117373 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/image-decoders/gif/GIFImageReader.cpp
@@ -584,27 +584,36 @@ bool GIFImageReader::read(const unsigned char *buf, unsigned len,
       count = q[1];
       gstate es = gif_skip_block;
 
-      // NOTE: We use the spec-mandated block lengths below where applicable,
-      // instead of the value in q[1]. If the two disagree, the GIF is
-      // malformed; the main thing we want to avoid is having the GIF specify a
-      // smaller number in q[1] than the spec requires and then having our
-      // processing code for these extensions blindly read off the end of a
-      // too-short heap buffer.
+      // The GIF spec mandates lengths for three of the extensions below.
+      // However, it's possible for GIFs in the wild to deviate. For example,
+      // some GIFs that embed ICC color profiles using gif_application_extension
+      // violate the spec and treat this extension block like a sort of
+      // "extension + data" block, giving a size greater than 11 and filling the
+      // remaining bytes with data (then following with more data blocks as
+      // needed), instead of placing a true data block just after the 11 byte
+      // extension block.
+      //
+      // Accordingly, if the specified length is larger than the required value,
+      // we use it. If it's smaller, then we enforce the spec value, because the
+      // parsers for these extensions expect to have the specified number of
+      // bytes available, and if we don't ensure that, they could read off the
+      // end of the heap buffer. (In this case, it's likely the GIF is corrupt
+      // and we'll soon fail to decode anyway.)
       switch (*q)
       {
       case 0xf9:
         es = gif_control_extension;
-        count = 4;
+        count = std::max(count, 4);
         break;
 
       case 0x01:
         // ignoring plain text extension
-        count = 12;
+        count = std::max(count, 12);
         break;
 
       case 0xff:
         es = gif_application_extension;
-        count = 11;
+        count = std::max(count, 11);
         break;
 
       case 0xfe: