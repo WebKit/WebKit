341b937feb7303074b64c5437f3bfa30925228f2
Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
2012-05-17 07:30:03 +0000

    [EFL] fast/frames/frame-crash-with-page-cache.html is crashing https://bugs.webkit.org/show_bug.cgi?id=85879
    Patch by Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com> on 2012-05-17
    Reviewed by Noam Rosenthal.
    
    Source/WebKit/efl:
    
    _ewk_frame_smart_del() is considering now that the frame can be present in cache.
    loader()->detachFromParent() is only applied for the main frame.
    loader()->cancelAndClear() is not used anymore.
    
    * ewk/ewk_frame.cpp:
    (_ewk_frame_smart_del):
    
    LayoutTests:
    
    * platform/efl/test_expectations.txt: Removed fast/frames/frame-crash-with-page-cache.html.
    
    Canonical link: https://commits.webkit.org/104318@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@117409 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/efl/ewk/ewk_frame.cpp
+++ Source/WebKit/efl/ewk/ewk_frame.cpp
@@ -233,8 +233,9 @@ static void _ewk_frame_smart_del(Evas_Object* ewkFrame)
         if (smartData->frame) {
             WebCore::FrameLoaderClientEfl* flc = _ewk_frame_loader_efl_get(smartData->frame);
             flc->setWebFrame(0);
-            smartData->frame->loader()->detachFromParent();
-            smartData->frame->loader()->cancelAndClear();
+            EWK_FRAME_SD_GET(ewk_view_frame_main_get(smartData->view), mainSmartData);
+            if (mainSmartData->frame == smartData->frame) // applying only for main frame is enough (will traverse through frame tree)
+                smartData->frame->loader()->detachFromParent();
             smartData->frame = 0;
         }
 
