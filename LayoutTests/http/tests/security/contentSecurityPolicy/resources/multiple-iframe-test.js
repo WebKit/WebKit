960ddcd4834286188b01df63be4598c1d7aebb01
Mike West <mkwst@chromium.org>
2012-05-18 13:46:47 +0000

    Deflaking http/tests/security/contentSecurityPolicy/script-src-redirect.html https://bugs.webkit.org/show_bug.cgi?id=86433
    Patch by Mike West <mkwst@chromium.org> on 2012-05-18
    Reviewed by Adam Barth.
    
    As it turns out, this was a straightforward typo on my part. Loading
    the wrong JS file is a surefire way to break tests. I've rewritten the
    test function to make it more difficult to make such a mistake in the
    future, and pushed that change to source-list-parsing-* tests that use
    the same methodology.
    
    * http/tests/security/contentSecurityPolicy/resources/multiple-iframe-test.js: Renamed from LayoutTests/http/tests/security/contentSecurityPolicy/resources/source-list-parsing-base.js.
    (test):
    (finishTesting):
    * http/tests/security/contentSecurityPolicy/script-src-redirect-expected.txt:
    * http/tests/security/contentSecurityPolicy/script-src-redirect.html:
    * http/tests/security/contentSecurityPolicy/source-list-parsing-01.html:
    * http/tests/security/contentSecurityPolicy/source-list-parsing-02.html:
    * http/tests/security/contentSecurityPolicy/source-list-parsing-03.html:
    * http/tests/security/contentSecurityPolicy/source-list-parsing-04.html:
    * platform/chromium/test_expectations.txt: Unskipping.
    * platform/mac-lion/Skipped: Unskipping.
    * platform/gtk/test_expectations.txt: Unskipping.
    * platform/efl/test_expectations.txt: Unskipping.
    * platform/qt/Skipped: Unskipping.
    * platform/mac/Skipped: Unskipping.
    
    Canonical link: https://commits.webkit.org/104483@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@117583 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/security/contentSecurityPolicy/resources/multiple-iframe-test.js
+++ LayoutTests/http/tests/security/contentSecurityPolicy/resources/multiple-iframe-test.js
@@ -1,20 +1,20 @@
 if (window.layoutTestController) {
-  layoutTestController.waitUntilDone();
-  layoutTestController.dumpAsText();
-  layoutTestController.dumpChildFramesAsText();
+    layoutTestController.waitUntilDone();
+    layoutTestController.dumpAsText();
+    layoutTestController.dumpChildFramesAsText();
 }
 
 function test() {
     if (tests.length === 0)
         return finishTesting();
 
-    var defaultScript = 'http://127.0.0.1:8000/security/contentSecurityPolicy/resources/script.js'
+    var baseURL = "http://127.0.0.1:8000/security/contentSecurityPolicy/";
     var current = tests.shift();
-    var iframe = document.createElement('iframe');
-    iframe.src = "http://127.0.0.1:8000/security/contentSecurityPolicy/resources/echo-script-src.pl?" +
+    var iframe = document.createElement("iframe");
+    iframe.src = baseURL + "resources/echo-script-src.pl?" +
                  "should_run=" + escape(current[0]) +
                  "&csp=" + escape(current[1]) +
-                 "&q=" + escape(current[2] ? current[2] : defaultScript);
+                 "&q=" + baseURL + escape(current[2]);
     iframe.onload = test;
     document.body.appendChild(iframe);
 }
