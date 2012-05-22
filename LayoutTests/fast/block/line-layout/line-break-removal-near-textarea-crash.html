WebKit/WebKit

commit a8a5159e3730fb0f8d4c8199c82e646b7fbf4500

Crash from removal of line break object after layout https://bugs.webkit.org/show_bug.cgi?id=75461
Source/WebCore:

Patch by Ken Buchanan <kenrb@chromium.org> on 2012-04-26
Reviewed by David Hyatt.

There is a condition where objects can get removed from underneath
inlines while they represent a line break object in a RootInlineBox
of an ancestor block. If an intermediary inline has already been
marked as needing layout, then the line box will not get dirtied
because dirtyLineFromChangedChild thinks it already has been.

This patch introduces a new set in RenderObject to indicate whether
an ancestral line box corresponding to the current line has been
marked dirty or not. dirtyLinesFromChangedChild() can use this set
rather than m_selfNeedsLayout, so it will not be confused if a
container was dirtied for some other reason that did not affect the
line box.

* rendering/RenderLineBoxList.cpp:
(WebCore::RenderLineBoxList::dirtyLinesFromChangedChild): Use the new
set rather than m_selfNeedsLayout in the container to determine
whether to continue propagating upward.
* rendering/RenderObject.cpp:
(WebCore::RenderObject::s_ancestorLineboxDirtySet): Instantiate the
static member.
(WebCore::RenderObject::willBeDestroyed): Clears the object from the
linebox set when it is being destroyed.
* rendering/RenderObject.h:
(WebCore::RenderObject::s_ancestorLineboxDirtySet): Added static
member set.
(WebCore::RenderObject::setNeedsLayout): Clears the
object from the linebox set when layout bits are getting cleared.
(WebCore::RenderObject::ancestorLineBoxDirty): Added.
(WebCore::RenderObject::setAncestorLineBoxDirty): Added.

LayoutTests:

Patch by Ken Buchanan <kenrb@chromium.org> on 2012-04-25
Reviewed by David Hyatt.

Test exercising crashing condition in bug 75461.

* fast/block/line-layout/line-break-obj-removal-crash-expected.txt: Added
* fast/block/line-layout/line-break-obj-removal-crash.html: Added

Canonical link: https://commits.webkit.org/102487@main

Source/WebCore/rendering/RenderObject.h
@@ -38,6 +38,7 @@
 #include "ScrollBehavior.h"
 #include "TextAffinity.h"
 #include "TransformationMatrix.h"
+#include <wtf/HashSet.h>
 #include <wtf/UnusedParam.h>
 
 #if USE(CG) || USE(CAIRO) || USE(SKIA) || PLATFORM(QT)
@@ -122,6 +123,8 @@ struct DashboardRegionValue {
     int type;
 };
 #endif
+
+typedef WTF::HashSet<const RenderObject*> RenderObjectAncestorLineboxDirtySet;
 
 #ifndef NDEBUG
 const int showTreeCharacterOffset = 39;
@@ -378,6 +381,22 @@ public:
     void setChildrenInline(bool b) { m_bitfields.setChildrenInline(b); }
     bool hasColumns() const { return m_bitfields.hasColumns(); }
     void setHasColumns(bool b = true) { m_bitfields.setHasColumns(b); }
+
+    bool ancestorLineBoxDirty() const { return s_ancestorLineboxDirtySet && s_ancestorLineboxDirtySet->contains(this); }
+    void setAncestorLineBoxDirty(bool b = true)
+    {
+        if (b) {
+            if (!s_ancestorLineboxDirtySet)
+                s_ancestorLineboxDirtySet = new RenderObjectAncestorLineboxDirtySet;
+            s_ancestorLineboxDirtySet->add(this);
+        } else if (s_ancestorLineboxDirtySet) {
+            s_ancestorLineboxDirtySet->remove(this);
+            if (s_ancestorLineboxDirtySet->isEmpty()) {
+                delete s_ancestorLineboxDirtySet;
+                s_ancestorLineboxDirtySet = 0;
+            }
+        }
+    }
 
     bool inRenderFlowThread() const { return m_bitfields.inRenderFlowThread(); }
     void setInRenderFlowThread(bool b = true) { m_bitfields.setInRenderFlowThread(b); }
@@ -904,6 +923,8 @@ private:
     RenderObject* m_previous;
     RenderObject* m_next;
 
+    static RenderObjectAncestorLineboxDirtySet* s_ancestorLineboxDirtySet;
+
 #ifndef NDEBUG
     bool m_hasAXObject             : 1;
     bool m_setNeedsLayoutForbidden : 1;
@@ -1064,6 +1085,7 @@ inline void RenderObject::setNeedsLayout(bool needsLayout, MarkingBehavior markP
         setNeedsSimplifiedNormalFlowLayout(false);
         setNormalChildNeedsLayout(false);
         setNeedsPositionedMovementLayout(false);
+        setAncestorLineBoxDirty(false);
     }
 }
 

LayoutTests/fast/block/line-layout/line-break-obj-removal-crash-expected.txt
@@ -1,0 +1,2 @@
+
+PASS, if no crash or assert in debug

LayoutTests/fast/block/line-layout/line-break-obj-removal-crash.html
@@ -1,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<style>
+.style1:nth-last-child(odd) { padding-right: 100px; }
+.styleCounterBefore:before { position: absolute; content: counter(section); }
+</style>
+<script>
+window.onload = function() {
+    divElem = document.createElement('div');
+    divElem.setAttribute('class', 'styleCounterBefore');
+    document.documentElement.appendChild(divElem);
+    brElem = document.createElement('br');
+    document.documentElement.appendChild(brElem);
+    sampElem = document.createElement('samp');
+    sampElem.setAttribute('class', 'style1');
+    document.documentElement.appendChild(sampElem);
+    spanElem = document.createElement('span');
+    spanElem.setAttribute('class', 'styleCounterBefore');
+    trElem = document.createElement('tr');
+    document.documentElement.appendChild(trElem);
+    sampElem.appendChild(spanElem);
+    document.documentElement.offsetHeight;
+    document.documentElement.removeChild(trElem);
+
+    document.documentElement.appendChild(document.createTextNode('PASS, if no crash or assert in debug'));
+
+    if (window.layoutTestController)
+        layoutTestController.dumpAsText();
+}
+</script>
+</html>

Source/WebCore/rendering/RenderLineBoxList.cpp
@@ -321,9 +321,9 @@ void RenderLineBoxList::dirtyLinesFromChangedChild(RenderObject* container, Rend
     if (!firstBox) {
         // For an empty inline, go ahead and propagate the check up to our parent, unless the parent
         // is already dirty.
-        if (container->isInline() && !container->parent()->selfNeedsLayout()) {
+        if (container->isInline() && !container->parent()->ancestorLineBoxDirty()) {
             container->parent()->dirtyLinesFromChangedChild(container);
-            container->setNeedsLayout(true); // Mark the container as needing layout to avoid dirtying the same lines again across multiple destroy() calls of the same subtree.
+            container->setAncestorLineBoxDirty(); // Mark the container to avoid dirtying the same lines again across multiple destroy() calls of the same subtree.
         }
         return;
     }
@@ -361,9 +361,9 @@ void RenderLineBoxList::dirtyLinesFromChangedChild(RenderObject* container, Rend
             // we won't find a previous sibling, but firstBox can be pointing to a following sibling.
             // This isn't good enough, since we won't locate the root line box that encloses the removed
             // <br>. We have to just over-invalidate a bit and go up to our parent.
-            if (!inlineContainer->parent()->selfNeedsLayout()) {
+            if (!inlineContainer->parent()->ancestorLineBoxDirty()) {
                 inlineContainer->parent()->dirtyLinesFromChangedChild(inlineContainer);
-                inlineContainer->setNeedsLayout(true); // Mark the container as needing layout to avoid dirtying the same lines again across multiple destroy() calls of the same subtree.
+                inlineContainer->setAncestorLineBoxDirty(); // Mark the container to avoid dirtying the same lines again across multiple destroy() calls of the same subtree.
             }
             return;
         }

Source/WebCore/rendering/RenderObject.cpp
@@ -103,6 +103,8 @@ COMPILE_ASSERT(sizeof(RenderObject) == sizeof(SameSizeAsRenderObject), RenderObj
 
 bool RenderObject::s_affectsParentBlock = false;
 
+RenderObjectAncestorLineboxDirtySet* RenderObject::s_ancestorLineboxDirtySet = 0;
+
 void* RenderObject::operator new(size_t sz, RenderArena* renderArena)
 {
     return renderArena->allocate(sz);
@@ -2323,6 +2325,8 @@ void RenderObject::willBeDestroyed()
         setHasLayer(false);
         toRenderBoxModelObject(this)->destroyLayer();
     }
+
+    setAncestorLineBoxDirty(false);
 
     clearLayoutRootIfNeeded();
 }

commit 72e7254ab3aca523f4b4ab97147ba9e12c1d984f

Merge 116551 - Crash from removal of a line break object https://bugs.webkit.org/show_bug.cgi?id=85997
Patch by Ken Buchanan <kenrb@chromium.org> on 2012-05-09
Reviewed by David Hyatt.

Source/WebCore:

Regression from r115343. That replaced a call to setNeedsLayout()
with a separate call that used a different bit during linebox
invalidation after renderer child removal. There are special cases
where layout isn't marked on parent nodes just from the removal, so
line dirtying needs to explicitly mark ancestors for layout.

* rendering/RenderObject.h:
(WebCore::RenderObject::setAncestorLineBoxDirty):

LayoutTests:

Test case to exercise crashing condition in bug 85997.

* fast/block/line-layout/line-break-removal-near-textarea-crash-expected.txt: Added
* fast/block/line-layout/line-break-removal-near-textarea-crash.html: Added

Canonical link: https://commits.webkit.org/96014.199@webkitgtk/1.8
git-svn-id: https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-1.8@117886 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderObject.h
@@ -381,6 +381,7 @@ public:
             if (!s_ancestorLineboxDirtySet)
                 s_ancestorLineboxDirtySet = new RenderObjectAncestorLineboxDirtySet;
             s_ancestorLineboxDirtySet->add(this);
+            setNeedsLayout(true);
         } else if (s_ancestorLineboxDirtySet) {
             s_ancestorLineboxDirtySet->remove(this);
             if (s_ancestorLineboxDirtySet->isEmpty()) {

LayoutTests/fast/block/line-layout/line-break-removal-near-textarea-crash-expected.txt
@@ -1,0 +1,2 @@
+PASS, if no crash or assert in debug
+abc

LayoutTests/fast/block/line-layout/line-break-removal-near-textarea-crash.html
@@ -1,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<style>
+.style1 { display: table-row; content: counter(section); }
+.style2 { position: fixed; }
+.style3 { vertical-align: super; -webkit-appearance: button; -webkit-column-width: 100px; }
+.style3:nth-of-type(2n) { display: list-item; }
+</style>
+<script>
+window.onload = function() {
+    div1 = document.createElement('div');
+    document.documentElement.appendChild(div1);
+
+    spanElem1 = document.createElement('span');
+    spanElem1.setAttribute('class', 'style3');
+    document.documentElement.appendChild(spanElem1);
+    
+    spanElem2 = document.createElement('span');
+    spanElem1.appendChild(spanElem2);
+    spanElem1.appendChild(document.createTextNode('abc'));
+
+    textareaElem = document.createElement('textarea');
+    textareaElem.setAttribute('class', 'style1');
+    spanElem2.appendChild(textareaElem);
+
+    canvasElem = document.createElement('canvas');
+    spanElem2.appendChild(canvasElem);
+
+    paragraphElem = document.createElement('p');
+    paragraphElem.setAttribute('class', 'style2');
+    spanElem2.appendChild(paragraphElem);
+
+    document.execCommand("SelectAll", false, "");
+    window.scrollBy(-15, -48);
+    document.documentElement.offsetHeight;
+
+    div1.setAttribute('class', 'style3');
+    spanElem2.removeChild(paragraphElem);
+
+    if (window.layoutTestController)
+        layoutTestController.dumpAsText();
+}
+</script>
+<body>
+PASS, if no crash or assert in debug
+</body>
+</html>