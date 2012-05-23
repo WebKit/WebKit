207f2c249f879b76e2afd64aa78e168ffcbf2d7b
Chris Rogers <crogers@google.com>
2012-05-23 21:46:15 +0000

    Source/WebCore: AudioParam must support fan-in (multiple audio connections) (take 2) https://bugs.webkit.org/show_bug.cgi?id=83610
    Reviewed by Kenneth Russell.
    
    This re-lands bug: https://bugs.webkit.org/show_bug.cgi?id=83610
    Now that a fix has been landed: http://trac.webkit.org/changeset/118099
    
    Test: webaudio/audioparam-summingjunction.html
    
    * Modules/webaudio/AudioParam.cpp:
    * Modules/webaudio/AudioParam.h:
    (WebCore::AudioParam::calculateSampleAccurateValues):
    (WebCore::AudioParam::calculateAudioRateSignalValues):
    Sums intrinsic parameter value with all audio-rate connections.
    
    (WebCore::AudioParam::connect):
    (WebCore::AudioParam::disconnect):
    Support multiple connections.
    
    (WebCore::AudioParam::hasSampleAccurateValues):
    If we have one or more audio-rate connections.
    
    (WebCore::AudioParam::AudioParam):
    AudioParam now sub-classes AudioSummingJunction.
    
    LayoutTests: AudioParam must support fan-in (multiple audio connections)
    https://bugs.webkit.org/show_bug.cgi?id=83610
    
    Reviewed by Kenneth Russell.
    
    * webaudio/audioparam-connect-audioratesignal.html:
    * webaudio/audioparam-summingjunction-expected.txt: Added.
    * webaudio/audioparam-summingjunction.html: Added.
    
    
    Canonical link: https://commits.webkit.org/105040@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@118247 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/webaudio/AudioParam.h
+++ Source/WebCore/Modules/webaudio/AudioParam.h
@@ -31,6 +31,7 @@
 
 #include "AudioContext.h"
 #include "AudioParamTimeline.h"
+#include "AudioSummingJunction.h"
 #include "PlatformString.h"
 #include <sys/types.h>
 #include <wtf/Float32Array.h>
@@ -41,7 +42,7 @@ namespace WebCore {
 
 class AudioNodeOutput;
 
-class AudioParam : public RefCounted<AudioParam> {
+class AudioParam : public AudioSummingJunction, public RefCounted<AudioParam> {
 public:
     static const double DefaultSmoothingConstant;
     static const double SnapThreshold;
@@ -51,7 +52,9 @@ public:
         return adoptRef(new AudioParam(context, name, defaultValue, minValue, maxValue, units));
     }
 
-    AudioContext* context() { return m_context.get(); }
+    // AudioSummingJunction
+    virtual bool canUpdateState() OVERRIDE { return true; }
+    virtual void didUpdate() OVERRIDE { }
 
     float value();
     void setValue(float);
@@ -84,7 +87,7 @@ public:
     void setValueCurveAtTime(Float32Array* curve, float time, float duration) { m_timeline.setValueCurveAtTime(curve, time, duration); }
     void cancelScheduledValues(float startTime) { m_timeline.cancelScheduledValues(startTime); }
 
-    bool hasSampleAccurateValues() { return m_timeline.hasValues() || m_audioRateSignal; }
+    bool hasSampleAccurateValues() { return m_timeline.hasValues() || numberOfRenderingConnections(); }
     
     // Calculates numberOfValues parameter values starting at the context's current time.
     // Must be called in the context's render thread.
@@ -96,7 +99,7 @@ public:
 
 protected:
     AudioParam(AudioContext* context, const String& name, double defaultValue, double minValue, double maxValue, unsigned units = 0)
-        : m_context(context)
+        : AudioSummingJunction(context)
         , m_name(name)
         , m_value(defaultValue)
         , m_defaultValue(defaultValue)
@@ -105,7 +108,6 @@ protected:
         , m_units(units)
         , m_smoothedValue(defaultValue)
         , m_smoothingConstant(DefaultSmoothingConstant)
-        , m_audioRateSignal(0)
     {
     }
 
@@ -113,7 +115,6 @@ private:
     void calculateAudioRateSignalValues(float* values, unsigned numberOfValues);
     void calculateTimelineValues(float* values, unsigned numberOfValues);
 
-    RefPtr<AudioContext> m_context;
     String m_name;
     double m_value;
     double m_defaultValue;
@@ -126,11 +127,6 @@ private:
     double m_smoothingConstant;
     
     AudioParamTimeline m_timeline;
-
-    // An audio-rate signal directly providing parameter values.
-    // FIXME: support fan-in (multiple audio connections to this parameter with unity-gain summing).
-    // https://bugs.webkit.org/show_bug.cgi?id=83610
-    AudioNodeOutput* m_audioRateSignal;
 };
 
 } // namespace WebCore
