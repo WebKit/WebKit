0f30ffae7c1d06817268983e6ad3e99b5fa117ee
Mary Wu <mawu@blackberry.com>
2012-05-25 15:04:01 +0000

    [BlackBerry] Pass http headers to loader in download request https://bugs.webkit.org/show_bug.cgi?id=87449
    Reviewed by Rob Buis.
    
    PR# 149283
    
    This is to support byte-range download and we could pass http headers like
    "Range" in download request to loader.
    
    Reviewed internally by Lyon Chen.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPage::download):
    
    
    Canonical link: https://commits.webkit.org/105278@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@118526 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/blackberry/Api/WebPage.cpp
+++ Source/WebKit/blackberry/Api/WebPage.cpp
@@ -638,7 +638,13 @@ void WebPage::loadFile(const char* path, const char* overrideContentType)
 
 void WebPage::download(const Platform::NetworkRequest& request)
 {
-    d->load(request.getUrlRef().c_str(), 0, "GET", Platform::NetworkRequest::UseProtocolCachePolicy, 0, 0, 0, 0, false, false, true, "", request.getSuggestedSaveName().c_str());
+    vector<const char*> headers;
+    Platform::NetworkRequest::HeaderList& list = request.getHeaderListRef();
+    for (unsigned i = 0; i < list.size(); i++) {
+        headers.push_back(list[i].first.c_str());
+        headers.push_back(list[i].second.c_str());
+    }
+    d->load(request.getUrlRef().c_str(), 0, "GET", Platform::NetworkRequest::UseProtocolCachePolicy, 0, 0, headers.empty() ? 0 : &headers[0], headers.size(), false, false, true, "", request.getSuggestedSaveName().c_str());
 }
 
 void WebPagePrivate::loadString(const char* string, const char* baseURL, const char* contentType, const char* failingURL)
