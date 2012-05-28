6474e9278fadc49bb4c1047a8d69ff62732ddc52
Noam Rosenthal <noam@webkit.org>
2012-05-28 17:48:01 +0000

    Remove unused argument coders for animation https://bugs.webkit.org/show_bug.cgi?id=87435
    Reviewed by Sam Weinig.
    
    Left the used argument coders only, behind a proper USE(UI_SIDE_COMPOSITING) flag.
    
    * Shared/WebCoreArgumentCoders.cpp:
    (CoreIPC):
    * Shared/WebCoreArgumentCoders.h:
    
    
    Canonical link: https://commits.webkit.org/105439@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@118697 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/Shared/WebCoreArgumentCoders.h
+++ Source/WebKit2/Shared/WebCoreArgumentCoders.h
@@ -234,7 +234,7 @@ template<> struct ArgumentCoder<WebCore::KURL> {
     static bool decode(ArgumentDecoder*, WebCore::KURL&);
 };
 
-#if PLATFORM(QT)
+#if USE(UI_SIDE_COMPOSITING)
 template<> struct ArgumentCoder<WebCore::FloatPoint3D> {
     static void encode(ArgumentEncoder*, const WebCore::FloatPoint3D&);
     static bool decode(ArgumentDecoder*, WebCore::FloatPoint3D&);
@@ -250,76 +250,14 @@ template<> struct ArgumentCoder<WebCore::TransformationMatrix> {
     static bool decode(ArgumentDecoder*, WebCore::TransformationMatrix&);
 };
 
-template<> struct ArgumentCoder<RefPtr<WebCore::MatrixTransformOperation> > {
-    static void encode(ArgumentEncoder*, const WebCore::MatrixTransformOperation*);
-    static bool decode(ArgumentDecoder*, RefPtr<WebCore::MatrixTransformOperation>&);
-};
-
-template<> struct ArgumentCoder<RefPtr<WebCore::Matrix3DTransformOperation> > {
-    static void encode(ArgumentEncoder*, const WebCore::Matrix3DTransformOperation*);
-    static bool decode(ArgumentDecoder*, RefPtr<WebCore::Matrix3DTransformOperation>&);
-};
-
-template<> struct ArgumentCoder<RefPtr<WebCore::PerspectiveTransformOperation> > {
-    static void encode(ArgumentEncoder*, const WebCore::PerspectiveTransformOperation*);
-    static bool decode(ArgumentDecoder*, RefPtr<WebCore::PerspectiveTransformOperation>&);
-};
-
-template<> struct ArgumentCoder<RefPtr<WebCore::RotateTransformOperation> > {
-    static void encode(ArgumentEncoder*, const WebCore::RotateTransformOperation*);
-    static bool decode(ArgumentDecoder*, RefPtr<WebCore::RotateTransformOperation>&);
-};
-
-template<> struct ArgumentCoder<RefPtr<WebCore::ScaleTransformOperation> > {
-    static void encode(ArgumentEncoder*, const WebCore::ScaleTransformOperation*);
-    static bool decode(ArgumentDecoder*, RefPtr<WebCore::ScaleTransformOperation>&);
-};
-
-template<> struct ArgumentCoder<RefPtr<WebCore::SkewTransformOperation> > {
-    static void encode(ArgumentEncoder*, const WebCore::SkewTransformOperation*);
-    static bool decode(ArgumentDecoder*, RefPtr<WebCore::SkewTransformOperation>&);
-};
-
-template<> struct ArgumentCoder<RefPtr<WebCore::TranslateTransformOperation> > {
-    static void encode(ArgumentEncoder*, const WebCore::TranslateTransformOperation*);
-    static bool decode(ArgumentDecoder*, RefPtr<WebCore::TranslateTransformOperation>&);
-};
-
-template<> struct ArgumentCoder<RefPtr<WebCore::TimingFunction> > {
-    static void encode(ArgumentEncoder*, const RefPtr<WebCore::TimingFunction>&);
-    static void encode(ArgumentEncoder*, const WebCore::TimingFunction*);
-    static bool decode(ArgumentDecoder*, RefPtr<WebCore::TimingFunction>&);
-};
-
-template<> struct ArgumentCoder<RefPtr<WebCore::TransformOperation> > {
-    static void encode(ArgumentEncoder*, const RefPtr<WebCore::TransformOperation>&);
-    static bool decode(ArgumentDecoder*, RefPtr<WebCore::TransformOperation>&);
-};
-
-template<> struct ArgumentCoder<WebCore::TransformOperations> {
-    static void encode(ArgumentEncoder*, const WebCore::TransformOperations&);
-    static bool decode(ArgumentDecoder*, WebCore::TransformOperations&);
-};
-
-template<> struct ArgumentCoder<RefPtr<WebCore::Animation> > {
-    static void encode(ArgumentEncoder*, const RefPtr<WebCore::Animation>&);
-    static bool decode(ArgumentDecoder*, RefPtr<WebCore::Animation>&);
-};
-#endif
-
-#if USE(UI_SIDE_COMPOSITING) && ENABLE(CSS_FILTERS)
+#if ENABLE(CSS_FILTERS)
 template<> struct ArgumentCoder<WebCore::FilterOperations> {
     static void encode(ArgumentEncoder*, const WebCore::FilterOperations&);
     static bool decode(ArgumentDecoder*, WebCore::FilterOperations&);
 };
 #endif
-
-#if USE(ACCELERATED_COMPOSITING)
-template<> struct ArgumentCoder<WebCore::KeyframeValueList> {
-    static void encode(ArgumentEncoder*, const WebCore::KeyframeValueList& keyframes);
-    static bool decode(ArgumentDecoder*, WebCore::KeyframeValueList& keyframes);
-};
 #endif
+
 
 } // namespace CoreIPC
 
