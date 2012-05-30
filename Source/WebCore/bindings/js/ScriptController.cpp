bd0effd2e8a3ee6b9f0413494a967019242e3907
Chris Dumez <christophe.dumez@intel.com>
2012-05-30 00:25:12 +0000

    Some Content Security Policy console messages are missing line numbers in JSC https://bugs.webkit.org/show_bug.cgi?id=87558
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-05-29
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Make ScriptController::eventHandlerPosition() behave the same way on
    JSC than V8. This means that TextPosition::minimumPosition() is
    returned if parser is null, instead of
    TextPosition::belowRangePosition(). The line numbers are now correctly
    printed in the output of Content Security Policy tests.
    
    Tests: http/tests/security/contentSecurityPolicy/inline-script-blocked-javascript-url.html
           http/tests/security/contentSecurityPolicy/javascript-url-blocked.html
    
    * bindings/js/ScriptController.cpp:
    (WebCore::ScriptController::eventHandlerPosition):
    
    LayoutTests:
    
    Remove Mac/EFL/GTK/Qt-specific expectations for Content Security Policy
    tests now that the line numbers are correctly printed.
    
    * platform/efl/http/tests/security/contentSecurityPolicy/inline-script-blocked-javascript-url-expected.txt: Removed.
    * platform/efl/http/tests/security/contentSecurityPolicy/javascript-url-blocked-expected.txt: Removed.
    * platform/gtk/http/tests/security/contentSecurityPolicy/inline-script-blocked-javascript-url-expected.txt: Removed.
    * platform/gtk/http/tests/security/contentSecurityPolicy/javascript-url-blocked-expected.txt: Removed.
    * platform/mac/http/tests/security/contentSecurityPolicy/inline-script-blocked-javascript-url-expected.txt: Removed.
    * platform/mac/http/tests/security/contentSecurityPolicy/javascript-url-blocked-expected.txt: Removed.
    * platform/qt/http/tests/security/contentSecurityPolicy/inline-script-blocked-javascript-url-expected.txt: Removed.
    * platform/qt/http/tests/security/contentSecurityPolicy/javascript-url-blocked-expected.txt: Removed.
    
    Canonical link: https://commits.webkit.org/105599@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@118861 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/js/ScriptController.cpp
+++ Source/WebCore/bindings/js/ScriptController.cpp
@@ -230,7 +230,7 @@ TextPosition ScriptController::eventHandlerPosition() const
     ScriptableDocumentParser* parser = m_frame->document()->scriptableDocumentParser();
     if (parser)
         return parser->textPosition();
-    return TextPosition::belowRangePosition();
+    return TextPosition::minimumPosition();
 }
 
 void ScriptController::disableEval()
