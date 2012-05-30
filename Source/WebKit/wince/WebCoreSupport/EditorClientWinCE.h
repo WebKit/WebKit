fdda68a648c2ada146d7520862b832975498b8d2
Patrick Gansterer <paroga@webkit.org>
2012-05-30 15:49:48 +0000

    Unreviewed WinCE build fix after r117470.
    * WebCoreSupport/EditorClientWinCE.h:
    (EditorClientWinCE):
    
    Canonical link: https://commits.webkit.org/105651@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@118927 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/wince/WebCoreSupport/EditorClientWinCE.h
+++ Source/WebKit/wince/WebCoreSupport/EditorClientWinCE.h
@@ -90,7 +90,7 @@ public:
     virtual void textWillBeDeletedInTextField(WebCore::Element*);
     virtual void textDidChangeInTextArea(WebCore::Element*);
 
-    virtual bool shouldEraseMarkersAfterChangeSelection(TextCheckingType) const;
+    virtual bool shouldEraseMarkersAfterChangeSelection(WebCore::TextCheckingType) const;
     virtual void ignoreWordInSpellDocument(const WTF::String&);
     virtual void learnWord(const WTF::String&);
     virtual void checkSpellingOfString(const UChar*, int, int*, int*);
