0136205c849beca3dcad8f81db3016c7387c9ccb
Filip Pizlo <fpizlo@apple.com>
2012-05-30 23:09:45 +0000

    LLInt broken on x86-32 with JIT turned off https://bugs.webkit.org/show_bug.cgi?id=87906
    Reviewed by Geoffrey Garen.
    
    Fixed the code to not clobber registers that contain important things, like the call frame.
    
    * llint/LowLevelInterpreter32_64.asm:
    
    
    
    Canonical link: https://commits.webkit.org/105712@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@118992 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/llint/LowLevelInterpreter32_64.asm
+++ Source/JavaScriptCore/llint/LowLevelInterpreter32_64.asm
@@ -1668,8 +1668,8 @@ macro nativeCallTrampoline(executableOffsetToFunction)
     storei CellTag, ScopeChain + TagOffset[cfr]
     storei t1, ScopeChain + PayloadOffset[cfr]
     if X86
-        loadp JITStackFrame::globalData + 4[sp], t0 # Additional offset for return address
-        storep cfr, JSGlobalData::topCallFrame[t0]
+        loadp JITStackFrame::globalData + 4[sp], t3 # Additional offset for return address
+        storep cfr, JSGlobalData::topCallFrame[t3]
         peek 0, t1
         storep t1, ReturnPC[cfr]
         move cfr, t2  # t2 = ecx
@@ -1681,8 +1681,8 @@ macro nativeCallTrampoline(executableOffsetToFunction)
         addp 16 - 4, sp
         loadp JITStackFrame::globalData + 4[sp], t3
     elsif ARMv7
-        loadp JITStackFrame::globalData[sp], t1
-        storep cfr, JSGlobalData::topCallFrame[t1]
+        loadp JITStackFrame::globalData[sp], t3
+        storep cfr, JSGlobalData::topCallFrame[t3]
         move t0, t2
         preserveReturnAddressAfterCall(t3)
         storep t3, ReturnPC[cfr]
