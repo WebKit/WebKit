8e28d938ab436b2b75ab724dd7d78358f67ad29b
Brady Eidson <beidson@apple.com>
2012-06-01 19:32:21 +0000

    <rdar://problem/11335622> and https://bugs.webkit.org/show_bug.cgi?id=88119 REGRESSION (r99448) Zero-sized plug-ins no longer get an NPP_SetWindow call
    Followup to r119260
    
    Rubber-stamped by Anders Carlsson.
    
    * WebProcess/Plugins/Netscape/NetscapePlugin.cpp:
    (WebKit::NetscapePlugin::callSetWindow): Set the m_hasCalledSetWindow here...
    (WebKit::NetscapePlugin::geometryDidChange): ...instead of here.
    
    Canonical link: https://commits.webkit.org/105939@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@119261 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/Plugins/Netscape/NetscapePlugin.cpp
+++ Source/WebKit2/WebProcess/Plugins/Netscape/NetscapePlugin.cpp
@@ -507,6 +507,7 @@ void NetscapePlugin::callSetWindow()
     m_npWindow.clipRect.bottom = m_npWindow.clipRect.top + m_clipRect.height();
 
     NPP_SetWindow(&m_npWindow);
+    m_hasCalledSetWindow = true;
 }
 
 bool NetscapePlugin::shouldLoadSrcURL()
@@ -712,7 +713,6 @@ void NetscapePlugin::geometryDidChange(const IntSize& pluginSize, const IntRect&
         return;
 
     callSetWindow();
-    m_hasCalledSetWindow = true;
 }
 
 void NetscapePlugin::visibilityDidChange()
