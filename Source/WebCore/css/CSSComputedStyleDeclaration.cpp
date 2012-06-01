02637d0b29823eaf2b596037093a75f112f245e3
Joe Thomas <joethomas@motorola.com>
2012-06-01 19:00:25 +0000

    getComputedStyle for background shorthand property does not return background-origin and background-clip. https://bugs.webkit.org/show_bug.cgi?id=86155
    Patch by Joe Thomas <joethomas@motorola.com> on 2012-06-01
    Reviewed by Tony Chang.
    
    Added background-origin and background-clip CSS Property values to the background shorthand CSSValueList.
    
    Source/WebCore:
    
    * css/CSSComputedStyleDeclaration.cpp:
    (WebCore::CSSComputedStyleDeclaration::getBackgroundShorthandValue):
    
    LayoutTests:
    
    * fast/backgrounds/background-shorthand-with-backgroundSize-style-expected.txt:
    * fast/backgrounds/background-shorthand-with-backgroundSize-style.html:
    * fast/css/getComputedStyle/getComputedStyle-background-shorthand-expected.txt:
    * fast/css/getComputedStyle/getComputedStyle-background-shorthand.html:
    
    Canonical link: https://commits.webkit.org/105937@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@119259 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/CSSComputedStyleDeclaration.cpp
+++ Source/WebCore/css/CSSComputedStyleDeclaration.cpp
@@ -2715,14 +2715,15 @@ void CSSComputedStyleDeclaration::setPropertyInternal(CSSPropertyID, const Strin
 
 PassRefPtr<CSSValueList> CSSComputedStyleDeclaration::getBackgroundShorthandValue() const
 {
-    // CSSPropertyBackgroundPosition should be at the end of the array so that CSSPropertyBackgroundSize can be appended followed by '/'.
-    static const CSSPropertyID properties[5] = { CSSPropertyBackgroundColor, CSSPropertyBackgroundImage,
-                                                 CSSPropertyBackgroundRepeat, CSSPropertyBackgroundAttachment,
-                                                 CSSPropertyBackgroundPosition };
+    static const CSSPropertyID propertiesBeforeSlashSeperator[5] = { CSSPropertyBackgroundColor, CSSPropertyBackgroundImage,
+                                                                     CSSPropertyBackgroundRepeat, CSSPropertyBackgroundAttachment,  
+                                                                     CSSPropertyBackgroundPosition };
+    static const CSSPropertyID propertiesAfterSlashSeperator[3] = { CSSPropertyBackgroundSize, CSSPropertyBackgroundOrigin, 
+                                                                    CSSPropertyBackgroundClip };
 
     RefPtr<CSSValueList> list = CSSValueList::createSlashSeparated();
-    list->append(getCSSPropertyValuesForShorthandProperties(StylePropertyShorthand(properties, WTF_ARRAY_LENGTH(properties))));
-    list->append(getPropertyCSSValue(CSSPropertyBackgroundSize, DoNotUpdateLayout));
+    list->append(getCSSPropertyValuesForShorthandProperties(StylePropertyShorthand(propertiesBeforeSlashSeperator, WTF_ARRAY_LENGTH(propertiesBeforeSlashSeperator))));
+    list->append(getCSSPropertyValuesForShorthandProperties(StylePropertyShorthand(propertiesAfterSlashSeperator, WTF_ARRAY_LENGTH(propertiesAfterSlashSeperator))));
     return list.release();
 }
 
