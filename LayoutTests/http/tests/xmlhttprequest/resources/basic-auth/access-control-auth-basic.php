81274fd71242712ae4650401789cb4e234bf9455
Pablo Flouret <pablof@motorola.com>
2012-06-09 18:42:41 +0000

    The value in Access-Control-Allow-Origin is not being matched correctly for CORS-enabled requests https://bugs.webkit.org/show_bug.cgi?id=88139
    Patch by Pablo Flouret <pablof@motorola.com> on 2012-06-09
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Compare a request's origin with the value given in any
    Access-Control-Allow-Origin headers in an exact, case-sensitive manner,
    instead of using SecurityOrigin::isSameSchemeHostPort(). Per step 3 of
    the resource sharing check algorithm in
    http://dvcs.w3.org/hg/cors/raw-file/tip/Overview.html#resource-sharing-check
    
    Test: http/tests/xmlhttprequest/origin-exact-matching.html
    
    * loader/CrossOriginAccessControl.cpp:
    (WebCore::passesAccessControlCheck):
    
    LayoutTests:
    
    * http/tests/xmlhttprequest/origin-exact-matching-expected.txt: Added.
    * http/tests/xmlhttprequest/origin-exact-matching.html: Added.
    * http/tests/xmlhttprequest/resources/basic-auth/access-control-auth-basic.php: Get rid of a trailing slash in the origin.
    * http/tests/xmlhttprequest/resources/origin-exact-matching-iframe.html: Added.
    
    Canonical link: https://commits.webkit.org/106547@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@119911 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/xmlhttprequest/resources/basic-auth/access-control-auth-basic.php
+++ LayoutTests/http/tests/xmlhttprequest/resources/basic-auth/access-control-auth-basic.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://127.0.0.1:8000/");
+header("Access-Control-Allow-Origin: http://127.0.0.1:8000");
 header("Access-Control-Allow-Credentials: true");
 
 if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
