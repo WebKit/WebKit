94823f54b249d5eb8670e52ae61aec2ff341d52d
Balazs Kelemen <kbalazs@webkit.org>
2012-06-10 13:44:32 +0000

    [Qt][Win] Fix building Source/WebCore/plugins/win/PluginViewWin.cpp https://bugs.webkit.org/show_bug.cgi?id=88308
    Reviewed by Simon Hausmann.
    
    No new tests, just a build fix.
    
    Use ownerWindow() from page client. It has been
    introduced for x11 plugins to solve exactly the
    same problem.
    
    * plugins/win/PluginViewWin.cpp:
    (windowHandleForPageClient):
    
    Canonical link: https://commits.webkit.org/106563@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@119927 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/plugins/win/PluginViewWin.cpp
+++ Source/WebCore/plugins/win/PluginViewWin.cpp
@@ -88,7 +88,11 @@
 
 #if PLATFORM(QT)
 #include "QWebPageClient.h"
+#if HAVE(QT5)
+#include <QWindow>
+#else
 #include <QWidget>
+#endif
 #endif
 
 #if PLATFORM(WX)
@@ -101,8 +105,13 @@ static inline HWND windowHandleForPageClient(PlatformPageClient client)
 #if PLATFORM(QT)
     if (!client)
         return 0;
+#if HAVE(QT5)
+    if (QWindow* window = client->ownerWindow())
+        return reinterpret_cast<HWND>(window->winId());
+#else
     if (QWidget* pluginParent = qobject_cast<QWidget*>(client->pluginParent()))
         return pluginParent->winId();
+#endif
     return 0;
 #elif PLATFORM(WX)
     if (!client)
