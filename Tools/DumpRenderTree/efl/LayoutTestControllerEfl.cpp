98b0a89b065f5367ad5fbc30e97242156eef5abe
Sudarsana Nagineni <sudarsana.nagineni@linux.intel.com>
2012-06-11 19:45:20 +0000

    [EFL] REGRESSION (r119788): tests rely on pathToLocalResource are failing after r119788 https://bugs.webkit.org/show_bug.cgi?id=88661
    Patch by Sudarsana Nagineni <sudarsana.nagineni@linux.intel.com> on 2012-06-11
    Reviewed by Dirk Pranke.
    
    Tools:
    
    Pass string length explicitly when creating String object from
    non-null-terminated UChar* returned by characters().
    
    * DumpRenderTree/efl/LayoutTestControllerEfl.cpp:
    (LayoutTestController::pathToLocalResource):
    
    LayoutTests:
    
    Unskip test cases which rely on pathToLocalResource().
    
    * platform/efl/TestExpectations:
    
    Canonical link: https://commits.webkit.org/106632@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@119998 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/DumpRenderTree/efl/LayoutTestControllerEfl.cpp
+++ Tools/DumpRenderTree/efl/LayoutTestControllerEfl.cpp
@@ -178,7 +178,7 @@ void LayoutTestController::notifyDone()
 
 JSStringRef LayoutTestController::pathToLocalResource(JSContextRef context, JSStringRef url)
 {
-    String requestedUrl(url->characters());
+    String requestedUrl(url->characters(), url->length());
     String resourceRoot;
     String requestedRoot;
 
