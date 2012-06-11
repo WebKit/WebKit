479b09b4c8cec119d23e0fa6e110eee066f376ca
Mark Pilgrim <pilgrim@chromium.org>
2012-06-11 18:36:12 +0000

    [Chromium] Remove some dead code in PasteboardChromium https://bugs.webkit.org/show_bug.cgi?id=88782
    Reviewed by Adam Barth.
    
    Part of a refactoring series. See tracking bug 82948.
    Leftover detritus from bug 88038.
    
    * platform/chromium/PasteboardChromium.cpp:
    (WebCore::Pasteboard::documentFragment):
    
    
    Canonical link: https://commits.webkit.org/106629@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@119994 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/chromium/PasteboardChromium.cpp
+++ Source/WebCore/platform/chromium/PasteboardChromium.cpp
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) 2008, 2009, Google Inc. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above
@@ -14,7 +14,7 @@
  *     * Neither the name of Google Inc. nor the names of its
  * contributors may be used to endorse or promote products derived from
  * this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -186,10 +186,6 @@ PassRefPtr<DocumentFragment> Pasteboard::documentFragment(Frame* frame, PassRefP
         unsigned fragmentEnd = 0;
         WebKit::WebURL url;
         markup = WebKit::Platform::current()->clipboard()->readHTML(buffer, &url, &fragmentStart, &fragmentEnd);
-
-        
-        //        PlatformSupport::clipboardReadHTML(buffer, &markup, &srcURL, &fragmentStart, &fragmentEnd);
-
         if (!markup.isEmpty()) {
           RefPtr<DocumentFragment> fragment =
               createFragmentFromMarkupWithContext(frame->document(), markup, fragmentStart, fragmentEnd, KURL(url), DisallowScriptingContent);
