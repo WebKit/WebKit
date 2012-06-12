746e53e94795bd29432c531bfc94b6817e82ad49
Mary Wu <mawu@blackberry.com>
2012-06-12 03:44:38 +0000

    [BlackBerry] Wrong mimetype for empty file link https://bugs.webkit.org/show_bug.cgi?id=88642
    Reviewed by Rob Buis.
    
    RIM PR# 164164
    
    For empty file link, we shouldn't give "application/octet-stream"
    which will lead to download.
    
    Reviewed internally by Charles Wei.
    
    * platform/network/blackberry/NetworkJob.cpp:
    (WebCore::NetworkJob::sendResponseIfNeeded):
    
    
    Canonical link: https://commits.webkit.org/106668@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@120035 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/network/blackberry/NetworkJob.cpp
+++ Source/WebCore/platform/network/blackberry/NetworkJob.cpp
@@ -618,10 +618,15 @@ void NetworkJob::sendResponseIfNeeded()
     String mimeType = m_sniffedMimeType;
     if (m_isFTP && m_isFTPDir)
         mimeType = "application/x-ftp-directory";
-    if (mimeType.isNull())
+    else if (mimeType.isNull())
         mimeType = extractMIMETypeFromMediaType(m_contentType);
-    if (mimeType.isNull())
-        mimeType = MIMETypeRegistry::getMIMETypeForPath(urlFilename);
+    if (mimeType.isNull()) {
+        if (m_dataReceived)
+            mimeType = MIMETypeRegistry::getMIMETypeForPath(urlFilename);
+        else
+            // For empty content, we shouldn't download.
+            mimeType = "text/plain";
+    }
     m_response.setMimeType(mimeType);
 
     // Set encoding from Content-Type header.
