d4c704776cb2da54e867f36f295c1e12271f662b
Zoltan Horvath <zoltan@webkit.org>
2012-06-13 15:15:44 +0000

    check-webkit-style needs to respect Qt API coding style https://bugs.webkit.org/show_bug.cgi?id=88995
    Reviewed by Csaba Osztrogonác.
    
    Add -whitespace/declaration exception to Qt API files.
    
    * Scripts/webkitpy/style/checker.py:
    
    Canonical link: https://commits.webkit.org/106833@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@120212 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/style/checker.py
+++ Tools/Scripts/webkitpy/style/checker.py
@@ -140,11 +140,17 @@ _PATH_RULES_SPECIFIER = [
       # Qt code uses '_' in some places (such as private slots
       # and on test xxx_data methos on tests)
       "Source/JavaScriptCore/qt/",
-      "Source/WebKit/qt/Api/",
       "Source/WebKit/qt/tests/",
       "Source/WebKit/qt/declarative/",
       "Source/WebKit/qt/examples/"],
      ["-readability/naming"]),
+
+    ([# The Qt APIs use Qt declaration style, it puts the * to
+      # the variable name, not to the class.
+      "Source/WebKit/qt/Api/"],
+     ["-readability/naming",
+      "-whitespace/declaration"]),
+
      ([# Qt's MiniBrowser has no config.h
        "Tools/MiniBrowser/qt"],
       ["-build/include"]),
