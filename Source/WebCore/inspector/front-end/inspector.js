17a02e93870bd1a4dddac57213a7a59922fd0d2f
Pavel Feldman <pfeldman@chromium.org>
2012-06-14 09:52:50 +0000

    Web Inspector: render dock-to-right icon in status bar. https://bugs.webkit.org/show_bug.cgi?id=89040
    Reviewed by Vsevolod Vlasov.
    
    Today we render dock to bottom icon for both actions (dock to bottom and dock to right).
    
    * inspector/front-end/Images/statusbarButtonGlyphs.png:
    * inspector/front-end/inspector.css:
    (button.dock-status-bar-item.status-bar-item.toggled-undock .glyph):
    (button.dock-status-bar-item.status-bar-item.toggled-bottom .glyph):
    (button.dock-status-bar-item.status-bar-item.toggled-right .glyph):
    * inspector/front-end/inspector.js:
    (WebInspector._createGlobalStatusBarItems):
    (WebInspector._installDockToRight.listener):
    (windowLoaded):
    (WebInspector.setDockingUnavailable):
    
    Canonical link: https://commits.webkit.org/106920@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@120302 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/inspector.js
+++ Source/WebCore/inspector/front-end/inspector.js
@@ -70,10 +70,9 @@ var WebInspector = {
 
     _createGlobalStatusBarItems: function()
     {
-        this._dockToggleButton = new WebInspector.StatusBarButton(this._dockButtonTitle(), "dock-status-bar-item");
+        this._dockToggleButton = new WebInspector.StatusBarButton("", "dock-status-bar-item", 3);
         this._dockToggleButton.addEventListener("click", this._toggleAttach.bind(this), false);
-        this._dockToggleButton.toggled = !this.attached;
-        WebInspector.updateDockToggleButton();
+        this._updateDockButtonState();
 
         var anchoredStatusBar = document.getElementById("anchored-status-bar-items");
         anchoredStatusBar.appendChild(this._dockToggleButton.element);
@@ -87,9 +86,20 @@ var WebInspector = {
         anchoredStatusBar.appendChild(this.settingsController.statusBarItem);
     },
 
-    _dockButtonTitle: function()
+    _updateDockButtonState: function()
     {
-        return this.attached ? WebInspector.UIString("Undock into separate window.") : WebInspector.UIString("Dock to main window.");
+        if (!this._dockToggleButton)
+            return;
+
+        if (this.attached) {
+            this._dockToggleButton.disabled = false;
+            this._dockToggleButton.state = "undock";
+            this._dockToggleButton.title = WebInspector.UIString("Undock into separate window.");
+        } else {
+            this._dockToggleButton.disabled = this._isDockingUnavailable;
+            this._dockToggleButton.state = WebInspector.settings.dockToRight.get() ? "right" : "bottom";
+            this._dockToggleButton.title = WebInspector.UIString("Dock to main window.");
+        }
     },
 
     _toggleAttach: function()
@@ -188,17 +198,13 @@ var WebInspector = {
 
         this._attached = x;
 
-        if (this._dockToggleButton) {
-            this._dockToggleButton.title = this._dockButtonTitle();
-            this._dockToggleButton.toggled = !x;
-        }
-
         if (x)
             document.body.removeStyleClass("detached");
         else
             document.body.addStyleClass("detached");
 
         this._setCompactMode(x && !WebInspector.settings.dockToRight.get());
+        this._updateDockButtonState();
     },
 
     isCompactMode: function()
@@ -519,6 +525,8 @@ WebInspector._installDockToRight = function()
         }
         if (WebInspector.attached)
             WebInspector._setCompactMode(!value);
+        else
+            WebInspector._updateDockButtonState();
     }
 }
 
@@ -539,7 +547,7 @@ var windowLoaded = function()
     } else
         WebInspector.loaded();
 
-    WebInspector.setAttachedWindow(WebInspector.queryParamsObject.docked === "true");
+    WebInspector.attached = WebInspector.queryParamsObject.docked === "true";
 
     window.removeEventListener("DOMContentLoaded", windowLoaded, false);
     delete windowLoaded;
@@ -578,23 +586,10 @@ WebInspector.windowResize = function(event)
     WebInspector.toolbar.resize();
 }
 
-WebInspector.setAttachedWindow = function(attached)
-{
-    this.attached = attached;
-    WebInspector.updateDockToggleButton();
-}
-
 WebInspector.setDockingUnavailable = function(unavailable)
 {
     this._isDockingUnavailable = unavailable;
-    WebInspector.updateDockToggleButton();
-}
-
-WebInspector.updateDockToggleButton = function()
-{
-    if (!this._dockToggleButton)
-        return;
-    this._dockToggleButton.disabled = this.attached ? false : this._isDockingUnavailable;
+    this._updateDockButtonState();
 }
 
 WebInspector.close = function(event)
