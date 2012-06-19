c13b45a40ad43c274c1f21bc392c5dccffa0169b
Joone Hur <joone@webkit.org>
2012-06-19 05:09:39 +0000

    Unreviewed. Updating email for committers.py script.
    * Scripts/webkitpy/common/config/committers.py:
    
    Canonical link: https://commits.webkit.org/107281@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@120671 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/config/committers.py
+++ Tools/Scripts/webkitpy/common/config/committers.py
@@ -282,7 +282,7 @@ committers_unable_to_review = [
     Committer("John Knottenbelt", "jknotten@chromium.org", "jknotten"),
     Committer("Johnny Ding", ["jnd@chromium.org", "johnnyding.webkit@gmail.com"], "johnnyding"),
     Committer("Jon Lee", "jonlee@apple.com", "jonlee"),
-    Committer("Joone Hur", "joone@webkit.org", "joone"),
+    Committer("Joone Hur", ["joone@webkit.org", "joone.hur@intel.com"], "joone"),
     Committer("Joost de Valk", ["joost@webkit.org", "webkit-dev@joostdevalk.nl"], "Altha"),
     Committer("Joshua Bell", ["jsbell@chromium.org", "jsbell@google.com"], "jsbell"),
     Committer("Julie Parent", ["jparent@google.com", "jparent@chromium.org"], "jparent"),
