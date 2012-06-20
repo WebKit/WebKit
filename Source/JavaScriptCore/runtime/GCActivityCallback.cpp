d4c8446760e589ab0fecda62522a9789d5064b2a
Mark Hahnenberg <mhahnenberg@apple.com>
2012-06-20 00:20:30 +0000

    [Qt][Mac] REGRESSION(r120742): It broke the build https://bugs.webkit.org/show_bug.cgi?id=89516
    Reviewed by Geoffrey Garen.
    
    Removing GCActivityCallbackCF.cpp because it doesn't mesh well with cross-platform
    code on Darwin (e.g. Qt). We now use plain ol' vanilla ifdefs to handle platforms
    without CF support. These if-defs will probably disappear in the future when we
    use cross-platform timers in HeapTimer.
    
    * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.vcproj:
    * JavaScriptCore.xcodeproj/project.pbxproj:
    * runtime/GCActivityCallback.cpp:
    (JSC):
    (JSC::DefaultGCActivityCallback::DefaultGCActivityCallback):
    (JSC::DefaultGCActivityCallback::doWork):
    (JSC::DefaultGCActivityCallback::scheduleTimer):
    (JSC::DefaultGCActivityCallback::cancelTimer):
    (JSC::DefaultGCActivityCallback::didAllocate):
    (JSC::DefaultGCActivityCallback::willCollect):
    (JSC::DefaultGCActivityCallback::cancel):
    * runtime/GCActivityCallbackCF.cpp: Removed.
    
    
    Canonical link: https://commits.webkit.org/107382@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@120778 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/GCActivityCallback.cpp
+++ Source/JavaScriptCore/runtime/GCActivityCallback.cpp
@@ -29,9 +29,93 @@
 #include "config.h"
 #include "GCActivityCallback.h"
 
+#include "APIShims.h"
 #include "Heap.h"
+#include "JSGlobalData.h"
+#include "JSLock.h"
+#include "JSObject.h"
+#include "ScopeChain.h"
+#include <wtf/RetainPtr.h>
+#include <wtf/WTFThreadData.h>
 
 namespace JSC {
+
+#if USE(CF)
+
+const double gcTimeSlicePerMB = 0.01; // Percentage of CPU time we will spend to reclaim 1 MB
+const double maxGCTimeSlice = 0.05; // The maximum amount of CPU time we want to use for opportunistic timer-triggered collections.
+const double timerSlop = 2.0; // Fudge factor to avoid performance cost of resetting timer.
+const double pagingTimeOut = 0.1; // Time in seconds to allow opportunistic timer to iterate over all blocks to see if the Heap is paged out.
+const CFTimeInterval hour = 60 * 60;
+
+DefaultGCActivityCallback::DefaultGCActivityCallback(Heap* heap)
+    : GCActivityCallback(heap->globalData(), CFRunLoopGetCurrent())
+    , m_delay(s_decade)
+{
+}
+
+DefaultGCActivityCallback::DefaultGCActivityCallback(Heap* heap, CFRunLoopRef runLoop)
+    : GCActivityCallback(heap->globalData(), runLoop)
+    , m_delay(s_decade)
+{
+}
+
+void DefaultGCActivityCallback::doWork()
+{
+    Heap* heap = &m_globalData->heap;
+    if (!isEnabled())
+        return;
+    
+    APIEntryShim shim(m_globalData);
+#if !PLATFORM(IOS)
+    double startTime = WTF::monotonicallyIncreasingTime();
+    if (heap->isPagedOut(startTime + pagingTimeOut)) {
+        heap->activityCallback()->cancel();
+        heap->increaseLastGCLength(pagingTimeOut);
+        return;
+    }
+#endif
+    heap->collectAllGarbage();
+}
+    
+void DefaultGCActivityCallback::scheduleTimer(double newDelay)
+{
+    if (newDelay * timerSlop > m_delay)
+        return;
+    double delta = m_delay - newDelay;
+    m_delay = newDelay;
+    CFRunLoopTimerSetNextFireDate(m_timer.get(), CFRunLoopTimerGetNextFireDate(m_timer.get()) - delta);
+}
+
+void DefaultGCActivityCallback::cancelTimer()
+{
+    m_delay = s_decade;
+    CFRunLoopTimerSetNextFireDate(m_timer.get(), CFAbsoluteTimeGetCurrent() + s_decade);
+}
+
+void DefaultGCActivityCallback::didAllocate(size_t bytes)
+{
+    // The first byte allocated in an allocation cycle will report 0 bytes to didAllocate. 
+    // We pretend it's one byte so that we don't ignore this allocation entirely.
+    if (!bytes)
+        bytes = 1;
+    Heap* heap = static_cast<Heap*>(&m_globalData->heap);
+    double gcTimeSlice = std::min((static_cast<double>(bytes) / MB) * gcTimeSlicePerMB, maxGCTimeSlice);
+    double newDelay = heap->lastGCLength() / gcTimeSlice;
+    scheduleTimer(newDelay);
+}
+
+void DefaultGCActivityCallback::willCollect()
+{
+    cancelTimer();
+}
+
+void DefaultGCActivityCallback::cancel()
+{
+    cancelTimer();
+}
+
+#else
 
 DefaultGCActivityCallback::DefaultGCActivityCallback(Heap* heap)
     : GCActivityCallback(heap->globalData())
@@ -53,6 +137,8 @@ void DefaultGCActivityCallback::willCollect()
 void DefaultGCActivityCallback::cancel()
 {
 }
+
+#endif
 
 }
 
