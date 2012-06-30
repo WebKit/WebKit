5d44e09c20caf69576cae583ec180495bd2aea35
Krzysztof Czech <k.czech@samsung.com>
2012-06-30 01:20:20 +0000

    Source/WebKit: [EFL] Add support for Unit Tests, based on the gtest library. https://bugs.webkit.org/show_bug.cgi?id=68509
    Patch by Krzysztof Czech <k.czech@samsung.com> on 2012-06-29
    Reviewed by Chang Shu.
    
    Add configuration for building gtest library, testing framework and unit tests.
    
    * PlatformEfl.cmake:
    
    Source/WebKit/efl: [EFL] Implementation of testing framework and unit tests for WebKit-EFL port.
    https://bugs.webkit.org/show_bug.cgi?id=68509
    
    Patch by Krzysztof Czech  <k.czech@samsung.com>, Tomasz Morawski <t.morawski@samsung.com> on 2012-06-29
    Reviewed by Chang Shu.
    
    Testing framework is based on gtest library. Gtest library is part of the WebKit project. Framework is devided into base part and view part.
    Base part takes care of efl initialization, defines test macros and prepares test to run. View part is a context of each test.
    
    * tests/UnitTestUtils/EWKTestBase.cpp: Added.
    (EWKUnitTests):
    (EWKUnitTests::EWKTestBase::init):
    (EWKUnitTests::EWKTestBase::shutdown):
    (EWKUnitTests::EWKTestBase::shutdownAll):
    (EWKUnitTests::EWKTestBase::startTest):
    (EWKUnitTests::EWKTestBase::endTest):
    (EWKUnitTests::EWKTestBase::createTest):
    (EWKUnitTests::EWKTestBase::runTest):
    * tests/UnitTestUtils/EWKTestBase.h: Added.
    (EWKUnitTests):
    (EWKTestBase):
    * tests/UnitTestUtils/EWKTestConfig.h: Added.
    (Config):
    * tests/UnitTestUtils/EWKTestView.cpp: Added.
    (EWKUnitTests):
    (EWKUnitTests::EWKTestEcoreEvas::EWKTestEcoreEvas):
    (EWKUnitTests::EWKTestEcoreEvas::evas):
    (EWKUnitTests::EWKTestEcoreEvas::show):
    (EWKUnitTests::EWKTestView::EWKTestView):
    (EWKUnitTests::EWKTestView::init):
    (EWKUnitTests::EWKTestView::show):
    (EWKUnitTests::EWKTestView::mainFrame):
    (EWKUnitTests::EWKTestView::evas):
    (EWKUnitTests::EWKTestView::bindEvents):
    * tests/UnitTestUtils/EWKTestView.h: Added.
    (EWKUnitTests):
    (EWKTestEcoreEvas):
    (EWKTestView):
    (EWKUnitTests::EWKTestView::webView):
    * tests/resources/default_test_page.html: Added.
    * tests/test_ewk_view.cpp: Added.
    (ewkViewEditableGetCb):
    (TEST):
    (ewkViewUriGetCb):
    * tests/test_runner.cpp: Added.
    (parseCustomArguments):
    (main):
    
    Canonical link: https://commits.webkit.org/108173@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@121608 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit/efl/tests/UnitTestUtils/EWKTestBase.h
@@ -0,0 +1,70 @@
+/*
+    Copyright (C) 2012 Samsung Electronics
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library; if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#ifndef EWKTestBase_h
+#define EWKTestBase_h
+
+#include <Evas.h>
+#include <gtest/gtest.h>
+
+#define RUN_TEST(args...)   \
+    do {    \
+        ASSERT_EQ(true, EWKTestBase::runTest(args));    \
+    } while (0)
+
+#define START_TEST()    \
+    do {    \
+        EWKTestBase::startTest();   \
+    } while (0)
+
+#define END_TEST()    \
+    do {    \
+        EWKTestBase::endTest(); \
+    } while (0)
+
+#define EFL_INIT_RET()  \
+    do {    \
+        if (!EWKTestBase::init())   \
+            return false;    \
+    } while (0)
+
+#define EFL_INIT()  \
+    do {    \
+        EWKTestBase::init();    \
+    } while (0)
+
+namespace EWKUnitTests {
+
+class EWKTestBase {
+    static bool createTest(const char* url, void (*event_callback)(void*, Evas_Object*, void*), const char* event_name, void* event_data);
+public:
+    static bool init();
+    static void shutdown();
+    static void shutdownAll();
+    static void startTest();
+    static void endTest();
+
+    static bool runTest(const char* url, void (*event_callback)(void*, Evas_Object*, void*), const char* event_name, void* event_data);
+    static bool runTest(void (*event_callback)(void*, Evas_Object*, void*), const char* event_name, void* event_data);
+
+    static int useX11Window;
+};
+
+}
+
+#endif
---
+++ Source/WebKit/efl/tests/UnitTestUtils/EWKTestView.h
@@ -0,0 +1,75 @@
+/*
+    Copyright (C) 2012 Samsung Electronics
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library; if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#ifndef EWKTestView_h
+#define EWKTestView_h
+
+#include <Ecore_Evas.h>
+#include <Evas.h>
+#include <string>
+#include <wtf/OwnPtr.h>
+
+namespace EWKUnitTests {
+
+class EWKTestEcoreEvas {
+public:
+    EWKTestEcoreEvas(int useX11Window);
+    EWKTestEcoreEvas(const char* engine_name, int viewport_x, int viewport_y, int viewport_w, int viewport_h, const char* extra_options, int useX11Window);
+
+    Evas* evas();
+    void show();
+
+private:
+    OwnPtr<Ecore_Evas> m_ecoreEvas;
+};
+
+class EWKTestView {
+public:
+    enum EwkViewType {
+        SingleView = 0,
+        TiledView,
+    };
+
+    explicit EWKTestView(Evas*);
+    EWKTestView(Evas*, const char* url);
+    EWKTestView(Evas*, EwkViewType, const char* url);
+    EWKTestView(Evas*, EwkViewType, const char* url, int width, int height);
+
+    Evas_Object* webView() { return m_webView.get(); }
+    Evas_Object* mainFrame();
+    Evas* evas();
+    void show();
+
+    bool init();
+    void bindEvents(void (*callback)(void*, Evas_Object*, void*), const char* eventName, void* ptr);
+
+private:
+    EWKTestView(const EWKTestView&);
+    EWKTestView operator=(const EWKTestView&);
+
+    Evas* m_evas;
+    OwnPtr<Evas_Object> m_webView;
+
+    int m_width, m_height;
+    EwkViewType m_defaultViewType;
+    std::string m_url;
+};
+
+}
+
+#endif
