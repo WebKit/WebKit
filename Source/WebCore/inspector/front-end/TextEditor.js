873c051ae3156dc74702e92452b2c6233c3424e9
Pavel Feldman <pfeldman@chromium.org>
2012-07-05 19:44:04 +0000

    Web Inspector: move cursor to the current search match. https://bugs.webkit.org/show_bug.cgi?id=90621
    Reviewed by Vsevolod Vlasov.
    
    After closing the search dialog, the cursor should be at the latest search match location.
    
    * inspector/front-end/TextEditor.js:
    (WebInspector.TextEditor.prototype.markAndRevealRange):
    (WebInspector.TextEditor.prototype._handleFocused):
    
    
    Canonical link: https://commits.webkit.org/108476@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@121918 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/TextEditor.js
+++ Source/WebCore/inspector/front-end/TextEditor.js
@@ -158,6 +158,8 @@ WebInspector.TextEditor.prototype = {
      */
     markAndRevealRange: function(range)
     {
+        if (range)
+            this._lastMarkedRange = range;
         this._mainPanel.markAndRevealRange(range);
     },
 
@@ -410,6 +412,12 @@ WebInspector.TextEditor.prototype = {
 
     _handleFocused: function()
     {
+        // Convert last marked range into a selection upon focus. This is needed to focus the search result.
+        if (this._lastMarkedRange) {
+            this._lastSelection = this._lastMarkedRange;
+            delete this._lastMarkedRange; 
+        }
+
         if (this._lastSelection) {
             // We do not restore selection after focus lost to avoid selection blinking. We restore only cursor position instead.
             // FIXME: consider adding selection decoration to blurred editor.
