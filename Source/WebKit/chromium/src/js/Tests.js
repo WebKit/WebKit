79cfe925ed889894e17ce17947eef887078735a9
Vsevolod Vlasov <vsevik@chromium.org>
2012-07-09 08:36:51 +0000

    Unreviewed chromium inspector test fix.
    * src/js/Tests.js:
    (.TestSuite.prototype.nonAnonymousUISourceCodes_):
    
    Canonical link: https://commits.webkit.org/108610@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@122098 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/chromium/src/js/Tests.js
+++ Source/WebKit/chromium/src/js/Tests.js
@@ -649,7 +649,7 @@ TestSuite.prototype.nonAnonymousUISourceCodes_ = function()
         return !!uiSourceCode.url;
     }
 
-    var uiSourceCodes = WebInspector.panels.scripts._uiSourceCodeProvider.uiSourceCodes();
+    var uiSourceCodes = WebInspector.workspace.uiSourceCodes();
     return uiSourceCodes.filter(filterOutAnonymous);
 };
 
