3534c9e546d746db389e1fc745c4d878a4913e36
Vincent Scheib <scheib@chromium.org>
2012-07-09 21:19:26 +0000

    Refactor common Pointer Lock test code to a harness. https://bugs.webkit.org/show_bug.cgi?id=90813
    Reviewed by Adrienne Walker.
    
    * pointer-lock/bug90391-move-then-window-open-crash-expected.txt:
    * pointer-lock/bug90391-move-then-window-open-crash.html:
    * pointer-lock/lock-already-locked-expected.txt:
    * pointer-lock/lock-already-locked.html:
    * pointer-lock/lock-fail-responses-expected.txt:
    * pointer-lock/lock-fail-responses.html:
    * pointer-lock/mouse-event-delivery-expected.txt:
    * pointer-lock/mouse-event-delivery.html:
    * pointer-lock/pointer-lock-api-expected.txt:
    * pointer-lock/pointer-lock-api.html:
    * pointer-lock/pointerlockchange-pointerlockerror-events-expected.txt:
    * pointer-lock/pointerlockchange-pointerlockerror-events.html:
    * pointer-lock/pointerlocklost-event-expected.txt:
    * pointer-lock/pointerlocklost-event.html:
    * pointer-lock/resources/pointer-lock-test-harness.js: Added.
    (doNextStep.else):
    (doNextStepWithUserGesture):
    (eventExpected.targetHanderNode.eventHandlerName):
    (eventExpected):
    (expectOnlyChangeEvent):
    (expectOnlyErrorEvent):
    (expectNoEvents):
    
    Canonical link: https://commits.webkit.org/108650@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@122158 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/pointer-lock/resources/pointer-lock-test-harness.js
@@ -0,0 +1,64 @@
+// Automatically add doNextStepButton to document for manual tests.
+if (!window.testRunner) {
+    setTimeout(function () {
+        doNextStepButton = document.body.insertBefore(document.createElement("button"), document.body.firstChild);
+        doNextStepButton.onclick = doNextStep;
+        doNextStepButton.innerText = "doNextStep button for manual testing. Use keyboard to select button and press (TAB, then SPACE).";
+    }, 0);
+}
+
+function doNextStep()
+{
+    if (typeof(currentStep) == "undefined")
+        currentStep = 0;
+
+    if (currentStep < todo.length)
+        setTimeout(function () { todo[currentStep++](); }, 0);
+    else if (currentStep++ == todo.length)
+        setTimeout(function () { finishJSTest(); }, 0);
+}
+
+function doNextStepWithUserGesture()
+{
+    if (!window.testRunner)
+        return; // Wait for human to press doNextStep button.
+    doNextStep();
+}
+
+function eventExpected(eventHandlerName, message, expectedCalls, targetHanderNode)
+{
+    targetHanderNode[eventHandlerName] = function () {
+        switch (expectedCalls--) {
+        case 0:
+            testFailed(eventHandlerName + " received after: " + message);
+            finishJSTest();
+            break;
+        case 1:
+            doNextStep();
+        default:
+            testPassed(eventHandlerName + " received after: " + message);
+        };
+    };
+};
+
+function expectOnlyChangeEvent(message, targetDocument) {
+    debug("     " + message);
+    targetDocument = targetDocument !== undefined ? targetDocument : document;
+    eventExpected("onwebkitpointerlockchange", message, 1, targetDocument);
+    eventExpected("onwebkitpointerlockerror", message, 0, targetDocument);
+};
+
+function expectOnlyErrorEvent(message, targetDocument) {
+    debug("     " + message);
+    targetDocument = targetDocument !== undefined ? targetDocument : document;
+    eventExpected("onwebkitpointerlockchange", message, 0, targetDocument);
+    eventExpected("onwebkitpointerlockerror", message, 1, targetDocument);
+};
+
+function expectNoEvents(message, targetDocument) {
+    debug("     " + message);
+    targetDocument = targetDocument !== undefined ? targetDocument : document;
+    eventExpected("onwebkitpointerlockchange", message, 0, targetDocument);
+    eventExpected("onwebkitpointerlockerror", message, 0, targetDocument);
+};
+
