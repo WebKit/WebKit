8bd213898ca9b0112467ba1e4b6a551f5bd28745
Carlos Garcia Campos <cgarcia@igalia.com>
2012-07-09 15:11:46 +0000

    [GTK] Fix inspector detach when inspector was attached by the client https://bugs.webkit.org/show_bug.cgi?id=90763
    Reviewed by Martin Robinson.
    
    When the inspector is detached, we are unconditionally removing it
    from the inspected view, but if the inspector was attached by the
    client, the parent might be another widget.
    
    * UIProcess/gtk/WebInspectorProxyGtk.cpp:
    (WebKit::WebInspectorProxy::platformDetach): Remove the inspector
    view from its parent widget.
    
    Canonical link: https://commits.webkit.org/108620@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@122108 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/gtk/WebInspectorProxyGtk.cpp
+++ Source/WebKit2/UIProcess/gtk/WebInspectorProxyGtk.cpp
@@ -195,8 +195,11 @@ void WebInspectorProxy::platformDetach()
         return;
 
     GRefPtr<GtkWidget> inspectorView = m_inspectorView;
-    if (!m_client.detach(this))
-        gtk_container_remove(GTK_CONTAINER(m_page->viewWidget()), m_inspectorView);
+    if (!m_client.detach(this)) {
+        GtkWidget* parent = gtk_widget_get_parent(m_inspectorView);
+        ASSERT(parent);
+        gtk_container_remove(GTK_CONTAINER(parent), m_inspectorView);
+    }
 
     if (!m_isVisible)
         return;
