78c86824e34f8ec390bde0e89a0f93925129aabb
Philip Rogers <pdr@google.com>
2012-07-10 23:36:07 +0000

    Crash due to SVG animation element not removed from target (before reset) https://bugs.webkit.org/show_bug.cgi?id=90750
    Reviewed by Abhishek Arya.
    
    Source/WebCore:
    
    Previously we were not removing an animation element from
    SVGDocumentExtensions::m_animatedElements which led to a crash.
    This change properly removes animation elements in resetTargetElement
    which both fixes this bug and will prevent others from hitting it in
    the future.
    
    Test: svg/animations/dynamic-modify-attributename-crash2.svg
    
    * svg/SVGDocumentExtensions.cpp:
    (WebCore::SVGDocumentExtensions::removeAllAnimationElementsFromTarget):
    
    removeAllAnimationElementsFromTarget now adds all the animation elements
    to a vector and iterates over it because the changes to resetTargetElement
    would have caused us to modify the underlying hashset as we iterated. Note that
    before we deleted animationElementsForTarget in removeAllAnimationElementsFromTarget
    but that logic is now handled in removeAnimationElementFromTarget which is called
    during resetTargetElement.
    
    * svg/animation/SVGSMILElement.cpp:
    (WebCore::SVGSMILElement::removedFrom):
    
    Because of the changes in resetTargetElement, removedFrom was able to be
    refactored. This patch changes removedFrom to call resetTargetElement rather
    than have duplicated logic. There is a very small change in logic here:
    animationAttributeChanged() is now called in removedFrom().
    
    (WebCore::SVGSMILElement::resetTargetElement):
    
    resetTargetElement now fully resets the target, including removing it from
    m_animatedElements. This will prevent future instances of this bug.
    
    LayoutTests:
    
    * svg/animations/dynamic-modify-attributename-crash2-expected.txt: Added.
    * svg/animations/dynamic-modify-attributename-crash2.svg: Added.
    
    
    Canonical link: https://commits.webkit.org/108756@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@122278 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/svg/animation/SVGSMILElement.cpp
+++ Source/WebCore/svg/animation/SVGSMILElement.cpp
@@ -233,11 +233,8 @@ void SVGSMILElement::removedFrom(ContainerNode* rootParent)
         disconnectConditions();
 
         // Clear target now, because disconnectConditions calls targetElement() which will recreate the target if we removed it sooner. 
-        if (m_targetElement) {
-            document()->accessSVGExtensions()->removeAnimationElementFromTarget(this, m_targetElement);
-            targetElementWillChange(m_targetElement, 0);
-            m_targetElement = 0;
-        }
+        if (m_targetElement)
+            resetTargetElement();
 
         m_attributeName = anyQName();
     }
@@ -582,6 +579,7 @@ void SVGSMILElement::targetElementWillChange(SVGElement* currentTarget, SVGEleme
 
 void SVGSMILElement::resetTargetElement()
 {
+    document()->accessSVGExtensions()->removeAnimationElementFromTarget(this, m_targetElement);
     targetElementWillChange(m_targetElement, 0);
     m_targetElement = 0;
     animationAttributeChanged();
