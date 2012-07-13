df9af48cc2808d9c6f41c05731745576b91b2ef4
Dongwoo Joshua Im <dw.im@samsung.com>
2012-07-13 06:50:15 +0000

    CodeGeneratorJS.pm need to handle the attribute which has "CallWith=ScriptExecutionContext" option. https://bugs.webkit.org/show_bug.cgi?id=91185
    Patch by Dongwoo Im <dw.im@samsung.com> on 2012-07-12
    Reviewed by Kentaro Hara.
    
    When an attribute has "CallWith=ScriptExecutionContext" option, 'ScriptExecutionContext*' parameter should be the last parameter.
    
    No new tests. Covered by existing tests.
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateImplementation):
    
    Canonical link: https://commits.webkit.org/109018@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@122545 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -1796,7 +1796,7 @@ sub GenerateImplementation
                             $functionName = "impl->${functionName}";
                         }
 
-                        unshift(@arguments, @callWithArgs);
+                        push(@arguments, @callWithArgs);
 
                         my $jsType = NativeToJSValue($attribute->signature, 0, $implClassName, "${functionName}(" . join(", ", @arguments) . ")", "castedThis");
                         push(@implContent, "    $implClassName* impl = static_cast<$implClassName*>(castedThis->impl());\n");
