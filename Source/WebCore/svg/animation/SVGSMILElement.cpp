b377b0fef5a4684db76dbd466cfc2b320edfc161
Florin Malita <fmalita@chromium.org>
2012-07-16 20:31:54 +0000

    SVGAnimationElement::currentValuesForValuesAnimation crash https://bugs.webkit.org/show_bug.cgi?id=91326
    Reviewed by Simon Fraser.
    
    SVGSMILElement::progress() assumes that seekToIntervalCorrespondingToTime() always
    lands inside a defined interval, but one can force arbitrary time offsets using
    setCurrentTime(). This patch adds logic for handling non-interval time offsets
    gracefully.
    
    Source/WebCore:
    
    Test: svg/animations/smil-setcurrenttime-crash.svg
    
    * svg/animation/SVGSMILElement.cpp:
    (WebCore::SVGSMILElement::progress):
    
    LayoutTests:
    
    * svg/animations/smil-setcurrenttime-crash-expected.txt: Added.
    * svg/animations/smil-setcurrenttime-crash.svg: Added.
    
    
    Canonical link: https://commits.webkit.org/109225@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@122755 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/svg/animation/SVGSMILElement.cpp
+++ Source/WebCore/svg/animation/SVGSMILElement.cpp
@@ -1043,7 +1043,11 @@ bool SVGSMILElement::progress(SMILTime elapsed, SVGSMILElement* resultElement, b
     // This call may obtain a new interval -- never call calculateAnimationPercentAndRepeat() before!
     if (seekToTime) {
         seekToIntervalCorrespondingToTime(elapsed);
-        ASSERT(elapsed >= m_intervalBegin);
+        if (elapsed < m_intervalBegin) {
+            // elapsed is not within an interval.
+            m_nextProgressTime = m_intervalBegin;
+            return false;
+        }
     }
 
     unsigned repeat = 0;
