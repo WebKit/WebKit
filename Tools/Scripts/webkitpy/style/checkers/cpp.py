WebKit/WebKit

commit 31992bf9c8b23650fe01b00bc49a2bbd5af6d48c

[EFL] Replace 0 by NULL in public headers documentation https://bugs.webkit.org/show_bug.cgi?id=91470
Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-07-17
Reviewed by Dirk Pranke.

Source/WebKit/efl:

Use NULL instead of 0 for pointer types in public
C headers.

* ewk/ewk_contextmenu.h:
* ewk/ewk_cookies.h:
* ewk/ewk_frame.h:
* ewk/ewk_intent.h:
* ewk/ewk_intent_request.h:
* ewk/ewk_network.h:
* ewk/ewk_security_origin.h:
* ewk/ewk_settings.h:
* ewk/ewk_view.h:
* ewk/ewk_window_features.h:

Source/WebKit2:

Use NULL instead of 0 for pointer types in public
C headers.

* UIProcess/API/efl/ewk_intent.h:
* UIProcess/API/efl/ewk_intent_service.h:
* UIProcess/API/efl/ewk_url_request.h:
* UIProcess/API/efl/ewk_url_response.h:
* UIProcess/API/efl/ewk_view.h:
* UIProcess/API/efl/ewk_web_resource.h:

Tools:

Fix style checking to properly detect C headers as C
files. Without this change, the style script would
complain if we use NULL (instead of 0 / null) in C
headers.

* Scripts/webkitpy/style/checkers/cpp.py:
(_FileState.__init__):
(_FileState.is_c):
(_FileState.is_c_or_objective_c):

Canonical link: https://commits.webkit.org/109330@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@122868 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/webkitpy/style/checkers/cpp.py
@@ -1161,14 +1161,18 @@ class _FileState(object):
         self._clean_lines = clean_lines
         if file_extension in ['m', 'mm']:
             self._is_objective_c = True
+            self._is_c = False
         elif file_extension == 'h':
             # In the case of header files, it is unknown if the file
-            # is objective c or not, so set this value to None and then
+            # is c / objective c or not, so set this value to None and then
             # if it is requested, use heuristics to guess the value.
             self._is_objective_c = None
+            self._is_c = None
+        elif file_extension == 'c':
+            self._is_c = True
         else:
             self._is_objective_c = False
-        self._is_c = file_extension == 'c'
+            self._is_c = False
 
     def set_did_inside_namespace_indent_warning(self):
         self._did_inside_namespace_indent_warning = True
@@ -1188,9 +1192,21 @@ class _FileState(object):
                 self._is_objective_c = False
         return self._is_objective_c
 
+    def is_c(self):
+        if self._is_c is None:
+            for line in self._clean_lines.lines:
+                # if extern "C" is found, then it is a good indication
+                # that we have a C header file.
+                if line.startswith('extern "C"'):
+                    self._is_c = True
+                    break
+            else:
+                self._is_c = False
+        return self._is_c
+
     def is_c_or_objective_c(self):
         """Return whether the file extension corresponds to C or Objective-C."""
-        return self._is_c or self.is_objective_c()
+        return self.is_c() or self.is_objective_c()
 
 
 def check_for_non_standard_constructs(clean_lines, line_number,

Source/WebKit/efl/ewk/ewk_contextmenu.h
@@ -163,7 +163,7 @@ EAPI void                        ewk_context_menu_ref(Ewk_Context_Menu *menu);
 /**
  * Decreases the reference count of the given object, possibly freeing it.
  *
- * When the reference count it's reached 0, the menu with all items are freed.
+ * When the reference count reaches 0, the menu with all its items are freed.
  *
  * @param menu the context menu object to decrease the reference count
  */
@@ -183,7 +183,7 @@ EAPI Eina_Bool                   ewk_context_menu_destroy(Ewk_Context_Menu *menu
  * Gets the list of items.
  *
  * @param o the context menu object to get list of the items
- * @return the list of the items on success or @c 0 on failure
+ * @return the list of the items on success or @c NULL on failure
  */
 EAPI const Eina_List            *ewk_context_menu_item_list_get(const Ewk_Context_Menu *o);
 
@@ -196,7 +196,7 @@ EAPI const Eina_List            *ewk_context_menu_item_list_get(const Ewk_Contex
  * @param title specifies a title of the item
  * @param checked @c EINA_TRUE if the item should be toggled or @c EINA_FALSE if not
  * @param enabled @c EINA_TRUE to enable the item or @c EINA_FALSE to disable
- * @return the pointer to the new item on success or @c 0 on failure
+ * @return the pointer to the new item on success or @c NULL on failure
  *
  * @note The return value @b should @b be freed after use.
  */
@@ -267,7 +267,7 @@ EAPI Eina_Bool                   ewk_context_menu_item_action_set(Ewk_Context_Me
  * Gets a title of the item.
  *
  * @param o the item to get the title
- * @return a title of the item on success, or @c 0 on failure
+ * @return a title of the item on success, or @c NULL on failure
  *
  * @see ewk_context_menu_item_title_set
  */
@@ -278,7 +278,7 @@ EAPI const char                 *ewk_context_menu_item_title_get(const Ewk_Conte
  *
  * @param o the item to set the title
  * @param title a new title for the item object
- * @return a new title of the item on success or @c 0 on failure
+ * @return a new title of the item on success or @c NULL on failure
  *
  * @see ewk_context_menu_item_title_get
  */

Source/WebKit/efl/ewk/ewk_cookies.h
@@ -50,7 +50,7 @@ struct _Ewk_Cookie {
     char *value;
     /// the "domain" attribute, or else the hostname that the cookie came from
     char *domain;
-    /// the "path" attribute, or @c 0
+    /// the "path" attribute, or @c NULL
     char *path;
     /// the cookie expiration time, or @c 0 for a session cookie
     time_t expires;
@@ -83,7 +83,7 @@ typedef enum _Ewk_Cookie_Policy Ewk_Cookie_Policy;
 /**
  * Sets the path where the cookies are going to be stored. 
  *
- * @param filename path to the cookies.txt file, use @c 0 for keep
+ * @param filename path to the cookies.txt file, use @c NULL for keep
  *        cookies just in memory.
  *
  * @return @c EINA_FALSE if it wasn't possible to create the cookie jar,

Source/WebKit/efl/ewk/ewk_frame.h
@@ -293,7 +293,7 @@ typedef enum {
  *
  * @param o frame object to get view object
  *
- * @return view object or @c 0 on failure
+ * @return view object or @c NULL on failure
  */
 EAPI Evas_Object *ewk_frame_view_get(const Evas_Object *o);
 
@@ -321,7 +321,7 @@ EAPI Ewk_Security_Origin *ewk_frame_security_origin_get(const Evas_Object *o);
  *
  * @param o frame object to create the iterator
  *
- * @return a newly allocated iterator on sucess, or @c 0 if not possible to
+ * @return a newly allocated iterator on sucess, or @c NULL if not possible to
  *      create the iterator
  */
 EAPI Eina_Iterator *ewk_frame_children_iterator_new(Evas_Object *o);
@@ -343,7 +343,7 @@ EAPI Eina_Iterator *ewk_frame_children_iterator_new(Evas_Object *o);
  * @param o frame object to find a child frame
  * @param name child frame name
  *
- * @return child frame of the given frame, or @c 0 if the the child wasn't found
+ * @return child frame of the given frame, or @c NULL if the the child wasn't found
  */
 EAPI Evas_Object   *ewk_frame_child_find(Evas_Object *o, const char *name);
 
@@ -365,7 +365,7 @@ EAPI Eina_Bool    ewk_frame_uri_set(Evas_Object *o, const char *uri);
  *
  * @param o frame object to get uri
  *
- * @return frame uri on success or @c 0 on failure
+ * @return frame uri on success or @c NULL on failure
  */
 EAPI const char  *ewk_frame_uri_get(const Evas_Object *o);
 
@@ -377,7 +377,7 @@ EAPI const char  *ewk_frame_uri_get(const Evas_Object *o);
  *
  * @param o frame object to get title
  *
- * @return frame title on success or @c 0 on failure
+ * @return frame title on success or @c NULL on failure
  */
 EAPI const Ewk_Text_With_Direction  *ewk_frame_title_get(const Evas_Object *o);
 
@@ -389,7 +389,7 @@ EAPI const Ewk_Text_With_Direction  *ewk_frame_title_get(const Evas_Object *o);
  *
  * @param o frame object to get name
  *
- * @return frame name on success or @c 0 on failure
+ * @return frame name on success or @c NULL on failure
  */
 EAPI const char  *ewk_frame_name_get(const Evas_Object *o);
 
@@ -397,8 +397,8 @@ EAPI const char  *ewk_frame_name_get(const Evas_Object *o);
  * Gets last known contents size.
  *
  * @param o frame object to get contents size
- * @param w pointer to store contents size width, may be @c 0
- * @param h pointer to store contents size height, may be @c 0
+ * @param w pointer to store contents size width, may be @c NULL
+ * @param h pointer to store contents size height, may be @c NULL
  *
  * @return @c EINA_TRUE on success or @c EINA_FALSE on failure and
  *         @a w and @a h will be zeroed
@@ -412,9 +412,9 @@ EAPI Eina_Bool    ewk_frame_contents_size_get(const Evas_Object *o, Evas_Coord *
  * @param contents what to load into frame
  * @param contents_size size of @a contents (in bytes),
  *        if @c 0 is given, length of @a contents is used
- * @param mime_type type of @a contents data, if @c 0 is given "text/html" is assumed
- * @param encoding encoding for @a contents data, if @c 0 is given "UTF-8" is assumed
- * @param base_uri base uri to use for relative resources, may be @c 0,
+ * @param mime_type type of @a contents data, if @c NULL is given "text/html" is assumed
+ * @param encoding encoding for @a contents data, if @c NULL is given "UTF-8" is assumed
+ * @param base_uri base uri to use for relative resources, may be @c NULL,
  *        if provided @b must be an absolute uri
  *
  * @return @c EINA_TRUE on successful request, @c EINA_FALSE on errors
@@ -429,15 +429,15 @@ EAPI Eina_Bool    ewk_frame_contents_set(Evas_Object *o, const char *contents, s
  * difference is that back-forward navigation list is not changed.
  *
  * @param o frame object to load alternative content
- * @param contents what to load into frame, must @b not be @c 0
+ * @param contents what to load into frame, must @b not be @c NULL
  * @param contents_size size of @a contents (in bytes),
  *        if @c 0 is given, length of @a contents is used
- * @param mime_type type of @a contents data, if @c 0 is given "text/html" is assumed
- * @param encoding encoding used for @a contents data, if @c 0 is given "UTF-8" is assumed
- * @param base_uri base URI to use for relative resources, may be @c 0,
+ * @param mime_type type of @a contents data, if @c NULL is given "text/html" is assumed
+ * @param encoding encoding used for @a contents data, if @c NULL is given "UTF-8" is assumed
+ * @param base_uri base URI to use for relative resources, may be @c NULL,
  *        if provided must be an absolute uri
  * @param unreachable_uri the URI that failed to load and is getting the
- *        alternative representation, must @b not be @c 0
+ *        alternative representation, must @b not be @c NULL
  *
  * @return @c EINA_TRUE on successful request, @c EINA_FALSE on errors
  */
@@ -451,7 +451,7 @@ EAPI Eina_Bool    ewk_frame_contents_alternate_set(Evas_Object *o, const char *c
  * @param o frame object to execute script
  * @param script Java Script to execute
  *
- * @return newly allocated string for result or @c 0 if the result cannot be converted to string or failure
+ * @return newly allocated string for result or @c NULL if the result cannot be converted to string or failure
  */
 EAPI char        *ewk_frame_script_execute(Evas_Object *o, const char *script);
 
@@ -481,7 +481,7 @@ EAPI Eina_Bool    ewk_frame_editable_set(Evas_Object *o, Eina_Bool editable);
  *
  * @param o the frame object to get selected text
  *
- * @return a newly allocated string or @c 0 if nothing is selected or on failure
+ * @return a newly allocated string or @c NULL if nothing is selected or on failure
  */
 EAPI char        *ewk_frame_selection_get(const Evas_Object *o);
 
@@ -545,8 +545,8 @@ EAPI Eina_Bool    ewk_frame_text_matches_highlight_get(const Evas_Object *o);
  *
  * @param o frame object where matches are marked
  * @param n index of element 
- * @param x the pointer to store the horizontal position of @a n matched text, may be @c 0
- * @param y the pointer to store the vertical position of @a n matched text, may be @c 0
+ * @param x the pointer to store the horizontal position of @a n matched text, may be @c NULL
+ * @param y the pointer to store the vertical position of @a n matched text, may be @c NULL
  *
  * @return @c EINA_TRUE on success, @c EINA_FALSE when no matches were found or
  *         @a n is bigger than search results or on failure
@@ -710,7 +710,7 @@ EAPI void          ewk_frame_hit_test_free(Ewk_Hit_Test *hit_test);
  * @param x the horizontal position to query
  * @param y the vertical position to query
  *
- * @return a newly allocated hit test on success, @c 0 otherwise
+ * @return a newly allocated hit test on success, @c NULL otherwise
  */
 EAPI Ewk_Hit_Test *ewk_frame_hit_test_new(const Evas_Object *o, int x, int y);
 
@@ -758,9 +758,9 @@ EAPI Eina_Bool    ewk_frame_scroll_set(Evas_Object *o, int x, int y);
  *
  * @param o frame object to get scroll size
  * @param w the pointer to store the horizontal size that is possible to scroll,
- *        may be @c 0
+ *        may be @c NULL
  * @param h the pointer to store the vertical size that is possible to scroll,
- *        may be @c 0
+ *        may be @c NULL
  *
  * @return @c EINA_TRUE on success, @c EINA_FALSE otherwise and
  *         values are zeroed
@@ -771,8 +771,8 @@ EAPI Eina_Bool    ewk_frame_scroll_size_get(const Evas_Object *o, int *w, int *h
  * Gets the current scroll position of given frame.
  *
  * @param o frame object to get the current scroll position
- * @param x the pointer to store the horizontal position, may be @c 0
- * @param y the pointer to store the vertical position. may be @c 0
+ * @param x the pointer to store the horizontal position, may be @c NULL
+ * @param y the pointer to store the vertical position. may be @c NULL
  *
  * @return @c EINA_TRUE on success, @c EINA_FALSE otherwise and
  *         values are zeroed.
@@ -784,10 +784,10 @@ EAPI Eina_Bool    ewk_frame_scroll_pos_get(const Evas_Object *o, int *x, int *y)
  *
  * @param o frame object to query visible content geometry
  * @param include_scrollbars whenever to include scrollbars size
- * @param x the pointer to store the horizontal position, may be @c 0
- * @param y the pointer to store the vertical position, may be @c 0
- * @param w the pointer to store width, may be @c 0
- * @param h the pointer to store height, may be @c 0
+ * @param x the pointer to store the horizontal position, may be @c NULL
+ * @param y the pointer to store the vertical position, may be @c NULL
+ * @param w the pointer to store width, may be @c NULL
+ * @param h the pointer to store height, may be @c NULL
  *
  * @return @c EINA_TRUE on success, @c EINA_FALSE otherwise and
  *         values are zeroed
@@ -842,10 +842,10 @@ EAPI Eina_Bool    ewk_frame_feed_focus_out(Evas_Object *o);
  * document.
  *
  * @param o frame object containing the focused element
- * @param x pointer where to store the X value of the geometry, may be @c 0
- * @param x pointer where to store the Y value of the geometry, may be @c 0
- * @param x pointer where to store width of the geometry, may be @c 0
- * @param x pointer where to store height of the geometry, may be @c 0
+ * @param x pointer where to store the X value of the geometry, may be @c NULL
+ * @param x pointer where to store the Y value of the geometry, may be @c NULL
+ * @param x pointer where to store width of the geometry, may be @c NULL
+ * @param x pointer where to store height of the geometry, may be @c NULL
  *
  * @return @c EINA_TRUE if the frame contains the currently focused element and
  * its geometry was correctly fetched, @c EINA_FALSE in any other case
@@ -939,7 +939,7 @@ EAPI Ewk_Text_Selection_Type ewk_frame_text_selection_type_get(const Evas_Object
  *
  * @param o frame smart object to get the frame source
  * @param frame_source a pointer to store the source of frame,
- *        must @b not be @c 0, this value @b should be freed after use
+ *        must @b not be @c NULL, this value @b should be freed after use
  *
  * @return @c length of @a frame_source on success, or @c -1 on failure
  *
@@ -955,7 +955,7 @@ EAPI ssize_t ewk_frame_source_get(const Evas_Object *o, char **frame_source);
  * replace them to the local paths. Values are not duplicated and they are decoded.
  *
  * @param o frame smart object to get the resources list
- * @return @c Eina_List with location of resources on success, or @c 0 on failure,
+ * @return @c Eina_List with location of resources on success, or @c NULL on failure,
  *         the Eina_List should be freed after use
  *
  * @see ewk_frame_source_get()
@@ -971,7 +971,7 @@ EAPI Eina_List *ewk_frame_resources_location_get(const Evas_Object *o);
  * @param ewkFrame Frame object whose contents to retrieve.
  *
  * @return A newly allocated string (which must be freed by the caller with @c free())
- *         or @c 0 in case of failure.
+ *         or @c NULL in case of failure.
  */
 EAPI char *ewk_frame_plain_text_get(const Evas_Object *o);
 

Source/WebKit/efl/ewk/ewk_intent.h
@@ -76,7 +76,7 @@ EAPI const char *ewk_intent_service_get(const Ewk_Intent *intent);
  *
  * @param intent intent item to query.
  *
- * @return @c Eina_List with suggested service URLs on success, or @c 0 on failure,
+ * @return @c Eina_List with suggested service URLs on success, or @c NULL on failure,
  *         the Eina_List and its items should be freed after use. Use free() to free the
  *         items.
  */
@@ -97,7 +97,7 @@ EAPI char *ewk_intent_extra_get(const Ewk_Intent *intent, const char *key);
  *
  * @param intent intent item to query.
  *
- * @return @c Eina_List with names of extra metadata on success, or @c 0 on failure,
+ * @return @c Eina_List with names of extra metadata on success, or @c NULL on failure,
  *         the Eina_List and its items should be freed after use. Use free() to free the
  *         items.
  */

Source/WebKit/efl/ewk/ewk_intent_request.h
@@ -41,7 +41,7 @@ EAPI void ewk_intent_request_ref(Ewk_Intent_Request *request);
 /**
  * Decreases the reference count of the given object, possibly freeing it.
  *
- * When the reference count it's reached 0, the intent request is freed.
+ * When the reference count reaches 0, the intent request is freed.
  *
  * @param request the intent request object to decrease the reference count
  */

Source/WebKit/efl/ewk/ewk_network.h
@@ -49,7 +49,7 @@ EAPI void             ewk_network_proxy_uri_set(const char *proxy);
  *
  * The returned string should be freed by eina_stringshare_del() after use.
  *
- * @return current proxy URI or @c 0 if it's not set
+ * @return current proxy URI or @c NULL if it's not set
  *
  * @note If the libsoup backend is being used, this function has effect on
  * the @b default SoupSession, returned by ewk_network_default_soup_session_get().

Source/WebKit/efl/ewk/ewk_security_origin.h
@@ -49,7 +49,7 @@ typedef struct _Ewk_Security_Origin Ewk_Security_Origin;
  * It returns a internal string which should not
  * be modified. The string is guaranteed to be stringshared.
  *
- * @return the protocol scheme or @c 0 if there is not a protocol scheme
+ * @return the protocol scheme or @c NULL if there is not a protocol scheme
  */
 EAPI const char          *ewk_security_origin_protocol_get(const Ewk_Security_Origin *o);
 
@@ -61,7 +61,7 @@ EAPI const char          *ewk_security_origin_protocol_get(const Ewk_Security_Or
  *
  * @param o security origin object
  *
- * @return the host domain or @c 0 if there is not a host scheme
+ * @return the host domain or @c NULL if there is not a host scheme
  */
 EAPI const char          *ewk_security_origin_host_get(const Ewk_Security_Origin *o);
 
@@ -72,7 +72,7 @@ EAPI const char          *ewk_security_origin_host_get(const Ewk_Security_Origin
  *
  * @param o security origin object
  *
- * @return the string representation of security origin or @c 0 if there is not a proper security origin scheme
+ * @return the string representation of security origin or @c NULL if there is not a proper security origin scheme
  */
 EAPI const char          *ewk_security_origin_string_get(const Ewk_Security_Origin *o);
 
@@ -142,7 +142,7 @@ EAPI void                 ewk_security_origin_application_cache_clear(const Ewk_
  * use ewk_web_database_list_free() as convenience.
  *
  * This function won't work if Web SQL Database was not enabled when
- * building WebKit and will just return 0.
+ * building WebKit and will just return @c NULL.
  *
  * @param o security origin object
  *

commit 1e7d5b7988cda35da33222f6833c154abd0873ae

Fix regression in style checker introduced in r122868. https://bugs.webkit.org/show_bug.cgi?id=91470
Unreviewed, build fix.

* Scripts/webkitpy/style/checkers/cpp.py:
(_FileState.__init__):

Canonical link: https://commits.webkit.org/109348@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@122889 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/webkitpy/style/checkers/cpp.py
@@ -1170,6 +1170,7 @@ class _FileState(object):
             self._is_c = None
         elif file_extension == 'c':
             self._is_c = True
+            self._is_objective_c = False
         else:
             self._is_objective_c = False
             self._is_c = False