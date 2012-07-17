ed6a15cfbf6621b6e4c2f8dd51fc194a77012d61
Ojan Vafai <ojan@chromium.org>
2012-07-17 18:44:37 +0000

    results.html should handle flaky tests differently https://bugs.webkit.org/show_bug.cgi?id=90892
    Reviewed by Dirk Pranke.
    
    -Put tests that failed both runs into the main failures table.
    -Put flaky passes below the list of timeout tests.
    
    * fast/harness/resources/results-test.js:
    * fast/harness/results-expected.txt:
    * fast/harness/results.html:
    
    Canonical link: https://commits.webkit.org/109327@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@122864 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/harness/resources/results-test.js
+++ LayoutTests/fast/harness/resources/results-test.js
@@ -685,7 +685,7 @@ function runTests()
     runTest(results, function() {
         var titles = document.getElementsByTagName('h1');
         assertTrue(titles[0].textContent == 'Tests that crashed (1):');
-        assertTrue(titles[1].textContent == 'Tests where results did not match expected results (3):');
+        assertTrue(titles[1].textContent == 'Tests that failed text/pixel/audio diff (3):');
         assertTrue(titles[2].textContent == 'Tests that had no expected results (probably new) (1):');
         assertTrue(titles[3].textContent == 'Tests that timed out (0):');
         assertTrue(titles[4].textContent == 'Tests that had stderr output (1):');
@@ -695,11 +695,26 @@ function runTests()
         document.getElementById('unexpected-results').onchange();
 
         assertTrue(titles[0].textContent == 'Tests that crashed (2):');
-        assertTrue(titles[1].textContent == 'Tests where results did not match expected results (5):');
+        assertTrue(titles[1].textContent == 'Tests that failed text/pixel/audio diff (5):');
         assertTrue(titles[2].textContent == 'Tests that had no expected results (probably new) (1):');
         assertTrue(titles[3].textContent == 'Tests that timed out (1):');
         assertTrue(titles[4].textContent == 'Tests that had stderr output (1):');
         assertTrue(titles[5].textContent == 'Tests expected to fail but passed (1):');
+    });
+
+    results = mockResults();
+    var subtree = results.tests['foo'] = {}
+    subtree['bar.html'] = mockExpectation('', 'PASS TEXT');
+    subtree['bar-1.html'] = mockExpectation('', 'TEXT IMAGE');
+    subtree['bar-2.html'] = mockExpectation('IMAGE TEXT', 'TEXT IMAGE');
+    subtree['bar-3.html'] = mockExpectation('PASS TEXT', 'TEXT PASS');
+    runTest(results, function() {
+        var titles = document.getElementsByTagName('h1');
+        assertTrue(titles[0].textContent == 'Tests that failed text/pixel/audio diff (1):');
+        assertTrue(titles[1].textContent =='Flaky tests (failed the first run and passed on retry) (1):');
+
+        assertTrue(document.querySelectorAll('#results-table tbody').length == 2);
+        assertTrue(document.querySelectorAll('#flaky-tests-table tbody').length == 2);
     });
 
     document.body.innerHTML = '<pre>' + g_log.join('\n') + '</pre>';
