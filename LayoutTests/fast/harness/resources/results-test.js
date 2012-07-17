473b3f80b155c3ce88229632262c1a6cea93828e
Ojan Vafai <ojan@chromium.org>
2012-07-17 18:42:15 +0000

    results.html doesn't hide the right data when not showing expected failures https://bugs.webkit.org/show_bug.cgi?id=90889
    Reviewed by Dirk Pranke.
    
    -Properly hide test lists that have no unexpected items.
    -Show the correct counts of tests in each test list when only showing unexpected.
    -Always show tests with stderr output.
    -Put tests with expected crash/timeout in the crash/timeout lists.
    
    * fast/harness/resources/results-test.js:
    * fast/harness/results.html:
    
    Canonical link: https://commits.webkit.org/109326@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@122863 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/harness/resources/results-test.js
+++ LayoutTests/fast/harness/resources/results-test.js
@@ -495,15 +495,15 @@ function runTests()
         var testLinks = document.querySelectorAll('.test-link');
         assertTrue(testLinks[0].innerText == 'foo/expected-to-pass-or-crash-and-crashed.html');
         assertTrue(enclosingNodeWithTagNameHasClassName(testLinks[0], 'tbody', 'expected'));
-        assertTrue(enclosingNodeWithTagNameHasClassName(testLinks[0], 'table', 'expected'));
+        assertTrue(enclosingNodeWithTagNameHasClassName(testLinks[0], 'div', 'expected'));
 
         assertTrue(testLinks[1].innerText == 'foo/expected-to-pass-or-timeout-and-timeouted.html');
         assertTrue(enclosingNodeWithTagNameHasClassName(testLinks[1], 'tbody', 'expected'));
-        assertTrue(enclosingNodeWithTagNameHasClassName(testLinks[1], 'table', 'expected'));
+        assertTrue(enclosingNodeWithTagNameHasClassName(testLinks[1], 'div', 'expected'));
 
         assertTrue(testLinks[2].innerText == 'foo/expected-pass-or-fail-and-passed.html');
         assertTrue(enclosingNodeWithTagNameHasClassName(testLinks[2], 'tbody', 'expected'));
-        assertTrue(enclosingNodeWithTagNameHasClassName(testLinks[2], 'table', 'expected'));
+        assertTrue(enclosingNodeWithTagNameHasClassName(testLinks[2], 'div', 'expected'));
     });
 
     results = mockResults();
@@ -676,15 +676,30 @@ function runTests()
     subtree['bar-3.html'] = mockExpectation('PASS', 'TEXT');
     subtree['bar-4.html'] = mockExpectation('TEXT', 'TEXT');
     subtree['bar-5.html'] = mockExpectation('TEXT', 'IMAGE+TEXT');
+    subtree['bar-stderr-expected.html'] = mockExpectation('IMAGE', 'IMAGE');
+    subtree['bar-stderr-expected.html'].has_stderr = true;
+    subtree['bar-expected-timeout.html'] = mockExpectation('TIMEOUT', 'TIMEOUT');
+    subtree['bar-expected-crash.html'] = mockExpectation('CRASH', 'CRASH');
     subtree['bar-missing.html'] = mockExpectation('TEXT', 'MISSING');
     subtree['bar-missing.html'].is_missing_text = true;
     runTest(results, function() {
         var titles = document.getElementsByTagName('h1');
         assertTrue(titles[0].textContent == 'Tests that crashed (1):');
         assertTrue(titles[1].textContent == 'Tests where results did not match expected results (3):');
-        assertTrue(titles[2].textContent =='Tests that had no expected results (probably new) (1):');
-        assertTrue(titles[3].textContent =='Tests expected to fail but passed (1):');
-        logPass('PASS');
+        assertTrue(titles[2].textContent == 'Tests that had no expected results (probably new) (1):');
+        assertTrue(titles[3].textContent == 'Tests that timed out (0):');
+        assertTrue(titles[4].textContent == 'Tests that had stderr output (1):');
+        assertTrue(titles[5].textContent == 'Tests expected to fail but passed (1):');
+
+        document.getElementById('unexpected-results').checked = false;
+        document.getElementById('unexpected-results').onchange();
+
+        assertTrue(titles[0].textContent == 'Tests that crashed (2):');
+        assertTrue(titles[1].textContent == 'Tests where results did not match expected results (5):');
+        assertTrue(titles[2].textContent == 'Tests that had no expected results (probably new) (1):');
+        assertTrue(titles[3].textContent == 'Tests that timed out (1):');
+        assertTrue(titles[4].textContent == 'Tests that had stderr output (1):');
+        assertTrue(titles[5].textContent == 'Tests expected to fail but passed (1):');
     });
 
     document.body.innerHTML = '<pre>' + g_log.join('\n') + '</pre>';
