27765dbde39467e0c5e77bbd0281048a64e4c389
Dmitry Titov <dimich@chromium.org>
2012-07-19 21:40:37 +0000

    [Chromium] Out of Memory is observed when a large object is passed to a Web Worker. https://bugs.webkit.org/show_bug.cgi?id=91535.
    Reviewed by Dave Levin.
    
    No new tests because of no new behavior and lack of mechanism to monitor used memory.
    
    * bindings/v8/SerializedScriptValue.cpp:
    (WebCore::SerializedScriptValue::SerializedScriptValue):
    (WebCore::SerializedScriptValue::registerMemoryAllocatedWithCurrentScriptContext):
    (WebCore):
    (WebCore::SerializedScriptValue::~SerializedScriptValue):
    * bindings/v8/SerializedScriptValue.h:
    (SerializedScriptValue):
    Added a method to SerializedScriptValue to add memory pressure on GC proportional to the size of memory allocated in serialized data.
    
    * dom/MessageEvent.cpp:
    (WebCore::MessageEvent::MessageEvent):
    (WebCore::MessageEvent::initMessageEvent):
    Call the new method of SerializedScriptValue during creation of MessageEvent (presumably in the thread which contains the right V8 context).
    
    Canonical link: https://commits.webkit.org/109607@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@123149 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/SerializedScriptValue.cpp
+++ Source/WebCore/bindings/v8/SerializedScriptValue.cpp
@@ -2201,6 +2201,7 @@ PassRefPtr<SerializedScriptValue> SerializedScriptValue::release()
 }
 
 SerializedScriptValue::SerializedScriptValue()
+    : m_externallyAllocatedMemory(0)
 {
 }
 
@@ -2252,6 +2253,7 @@ SerializedScriptValue::SerializedScriptValue(v8::Handle<v8::Value> value,
                                              MessagePortArray* messagePorts, ArrayBufferArray* arrayBuffers,
                                              bool& didThrow,
                                              v8::Isolate* isolate)
+    : m_externallyAllocatedMemory(0)
 {
     didThrow = false;
     Writer writer(isolate);
@@ -2298,6 +2300,7 @@ SerializedScriptValue::SerializedScriptValue(v8::Handle<v8::Value> value,
 }
 
 SerializedScriptValue::SerializedScriptValue(const String& wireData)
+    : m_externallyAllocatedMemory(0)
 {
     m_data = wireData.isolatedCopy();
 }
@@ -2321,5 +2324,18 @@ ScriptValue SerializedScriptValue::deserializeForInspector(ScriptState* scriptSt
     return ScriptValue(deserialize(0, isolate));
 }
 #endif
+
+void SerializedScriptValue::registerMemoryAllocatedWithCurrentScriptContext()
+{
+    if (m_externallyAllocatedMemory)
+        return;
+    m_externallyAllocatedMemory = static_cast<intptr_t>(m_data.length());
+    v8::V8::AdjustAmountOfExternalAllocatedMemory(m_externallyAllocatedMemory);
+}
+
+SerializedScriptValue::~SerializedScriptValue()
+{
+    v8::V8::AdjustAmountOfExternalAllocatedMemory(-m_externallyAllocatedMemory);
+}
 
 } // namespace WebCore
