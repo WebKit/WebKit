df4c884686268786915c6db6549def4d75b2a5df
Gyuyoung Kim <gyuyoung.kim@samsung.com>
2012-07-19 04:36:23 +0000

    [CMAKE] Remove duplicated #ifdef guard in CMakeLists.txt https://bugs.webkit.org/show_bug.cgi?id=91575
    Reviewed by Dirk Pranke.
    
    CMake files have used ENABLE_XXX macro. However, almost source and idl files are already protected by #ifdef.
    So, we don't need to guard them in CMake files again.
    
    In SVG idl files case, blackberry port doesn't support it yet. So, SVG idl files are handled by WebCore_SVG_IDL_FILES macro.
    
    No new tests. This patch isn't for functionality.
    
    * CMakeLists.txt:
    * PlatformBlackBerry.cmake: Change SVG_IDL_FILES with WebCore_SVG_IDL_FILES.
    
    
    Canonical link: https://commits.webkit.org/109526@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@123068 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/PlatformBlackBerry.cmake
+++ Source/WebCore/PlatformBlackBerry.cmake
@@ -312,7 +312,7 @@ ENDFOREACH ()
 
 # FIXME: We need to add the IDLs for SQL storage and Web Workers. See PR #123484.
 SET(WebCore_NO_CPP_IDL_FILES
-    ${SVG_IDL_FILES}
+    ${WebCore_SVG_IDL_FILES}
     dom/CustomEvent.idl
     dom/PopStateEvent.idl
     inspector/ScriptProfile.idl
