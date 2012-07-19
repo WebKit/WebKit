9efdcdf9a99a8bd3b5a0b440c4853b8f1930318e
Andrey Kosyakov <caseq@chromium.org>
2012-07-19 16:44:19 +0000

    Web Inspector: CPU bars are not cleared along with the rest of the timeline panel https://bugs.webkit.org/show_bug.cgi?id=91753
    Reviewed by Pavel Feldman.
    
    - remove early bail-outs in  refreshMainThreadBars; let it run to clear bars for the non-existent / invisible events;
    
    * inspector/front-end/TimelinePanel.js:
    (WebInspector.TimelinePanel.prototype._refreshMainThreadBars):
    
    Canonical link: https://commits.webkit.org/109571@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@123113 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/TimelinePanel.js
+++ Source/WebCore/inspector/front-end/TimelinePanel.js
@@ -785,8 +785,6 @@ WebInspector.TimelinePanel.prototype = {
         var endTime = startTime + width * scale;
 
         var tasks = this._mainThreadTasks;
-        if (!tasks.length)
-            return;
 
         function compareEndTime(value, task)
         {
@@ -794,8 +792,6 @@ WebInspector.TimelinePanel.prototype = {
         }
 
         var taskIndex = insertionIndexForObjectInListSortedByFunction(startTime, tasks, compareEndTime);
-        if (taskIndex === tasks.length)
-            return;
 
         var container = this._cpuBarsElement;
         var element = container.firstChild.nextSibling;
