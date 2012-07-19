d0a380cf39c6a15c7961b7b0617f0768b4dd0b8e
Raymond Toy <rtoy@google.com>
2012-07-19 19:06:11 +0000

    Limit maximum delay of DelayNode. https://bugs.webkit.org/show_bug.cgi?id=91675
    Patch by Raymond Toy <rtoy@google.com> on 2012-07-19
    Reviewed by Kenneth Russell.
    
    Source/WebCore:
    
    Clip the maximum delay of a DelayNode to a reasonable maximum.
    
    Test: webaudio/delaynode-maxdelaylimit.html
    
    * Modules/webaudio/DelayNode.cpp:
    (WebCore): Add maximumAllowedDelayTime.
    (WebCore::DelayNode::DelayNode): Clip max delay.
    
    LayoutTests:
    
    Test that a DelayNode limits the specified max delay to the
    expected maximum allowed delay.
    
    * webaudio/delaynode-maxdelaylimit-expected.txt: Added.
    * webaudio/delaynode-maxdelaylimit.html: Added.
    
    Canonical link: https://commits.webkit.org/109589@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@123131 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/webaudio/DelayNode.cpp
+++ Source/WebCore/Modules/webaudio/DelayNode.cpp
@@ -30,9 +30,13 @@
 
 namespace WebCore {
 
+// FIXME: What should be the maximum allowed delay? Arbitrarily set to 300 sec (5 minutes).
+const double maximumAllowedDelayTime = 300;
+
 DelayNode::DelayNode(AudioContext* context, float sampleRate, double maxDelayTime)
     : AudioBasicProcessorNode(context, sampleRate)
 {
+    maxDelayTime = std::max(std::min(maxDelayTime, maximumAllowedDelayTime), 0.0);
     m_processor = adoptPtr(new DelayProcessor(context, sampleRate, 1, maxDelayTime));
     setNodeType(NodeTypeDelay);
 }
