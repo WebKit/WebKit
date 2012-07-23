456ca7b66327f859a9ca345fa0ba707b621a2a8d
Scott Graham <scottmg@google.com>
2012-07-23 21:52:00 +0000

    Use native (non-cygwin) binaries for perl, gperf, and bison in Chromium https://bugs.webkit.org/show_bug.cgi?id=91667
    Patch by Scott Graham <scottmg@google.com> on 2012-07-23
    Reviewed by Tony Chang.
    
    Using native tools instead of cygwin version improves build time
    performance by roughly 50% (on top of previous cl-instead-of-gcc
    change).
    
    Also, use - instead of / for cl flags because a layer of project
    generator converts them to \ otherwise, which causes the preprocessing
    to fail (very slowly because of the cygwin-loop with a sleep 1).
    
    No new tests.
    
    * WebCore.gyp/WebCore.gyp:
    * WebCore.gyp/scripts/rule_bison.py:
    * bindings/scripts/preprocessor.pm:
    (applyPreprocessor):
    * make-hash-tools.pl:
    
    Canonical link: https://commits.webkit.org/109838@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@123387 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/make-hash-tools.pl
+++ Source/WebCore/make-hash-tools.pl
@@ -29,10 +29,10 @@ my $option = basename($ARGV[0],".gperf");
 
 if ($option eq "ColorData") {
     my $colorDataGenerated         = "$outdir/ColorData.cpp";
-    my $colorDataGperf             = $ARGV[0];
-    shift;
+    my $colorDataGperf             = shift;
+    my $customGperf                = shift;
 
-    my $gperf = $ENV{GPERF} ? $ENV{GPERF} : "gperf";
+    my $gperf = $ENV{GPERF} ? $ENV{GPERF} : ($customGperf ? $customGperf : "gperf");
     system("\"$gperf\" --key-positions=\"*\" -D -s 2 $colorDataGperf --output-file=$colorDataGenerated") == 0 || die "calling gperf failed: $?";
 
 } else {
