ae104578d69009db36b8ae66676d06ca97924859
Julien Chaffraix <jchaffraix@webkit.org>
2012-07-24 17:44:29 +0000

    inline-table wrapper should be generated for display: inline element only https://bugs.webkit.org/show_bug.cgi?id=92054
    Reviewed by Abhishek Arya.
    
    Source/WebCore:
    
    CSS 2.1 only takes into account the display when determining if we should generate
    an inline-table. My misreading of the spec would make us generate an inline-table
    wrapper for any inline formatting context.
    
    Test: fast/table/inline-block-generates-table-wrapper.html
    
    * rendering/RenderTable.cpp:
    (WebCore::RenderTable::createAnonymousWithParentRenderer):
    Changed to use display instead of isInline to properly match the spec.
    
    LayoutTests:
    
    For some reason, using dumpAsText didn't dump the 2 lines properly so the test
    is a reftest to work-around that.
    
    * fast/table/inline-block-generates-table-wrapper-expected.html: Added.
    * fast/table/inline-block-generates-table-wrapper.html: Added.
    
    
    Canonical link: https://commits.webkit.org/109942@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@123492 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderTable.cpp
+++ Source/WebCore/rendering/RenderTable.cpp
@@ -1297,7 +1297,7 @@ bool RenderTable::nodeAtPoint(const HitTestRequest& request, HitTestResult& resu
 RenderTable* RenderTable::createAnonymousWithParentRenderer(const RenderObject* parent)
 {
     // CSS 2.1, section 17.2.1: "If a proper table child's parent is an 'inline' box, then the generated anonymous table must be an 'inline-table' box".
-    EDisplay tableDisplay = parent->isInline() ? INLINE_TABLE : TABLE;
+    EDisplay tableDisplay = parent->style()->display() == INLINE ? INLINE_TABLE : TABLE;
     RefPtr<RenderStyle> newStyle = RenderStyle::createAnonymousStyleWithDisplay(parent->style(), tableDisplay);
     RenderTable* newTable = new (parent->renderArena()) RenderTable(parent->document() /* is anonymous */);
     newTable->setStyle(newStyle.release());
