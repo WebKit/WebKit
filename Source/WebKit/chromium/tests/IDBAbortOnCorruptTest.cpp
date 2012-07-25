ea793f1f08277fb0ef2115def54cd9292857bf82
Hajime Morrita <morrita@google.com>
2012-07-25 08:42:44 +0000

    Another unreviewed win build fix against r123569.
    * tests/IDBAbortOnCorruptTest.cpp:
    
    Canonical link: https://commits.webkit.org/110029@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@123589 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/chromium/tests/IDBAbortOnCorruptTest.cpp
+++ Source/WebKit/chromium/tests/IDBAbortOnCorruptTest.cpp
@@ -25,6 +25,7 @@
 
 #include "config.h"
 #include "IDBCursorBackendInterface.h"
+#include "IDBDatabaseBackendInterface.h"
 #include "IDBFactoryBackendImpl.h"
 #include "IDBFakeBackingStore.h"
 #include "SecurityOrigin.h"
