d7dad4593da512c66ca96d609386ae4496908a71
Vsevolod Vlasov <vsevik@chromium.org>
2012-07-27 09:26:24 +0000

    Source/WebCore: Web Inspector: Move formatting support from JavaScriptSource to UISourceCode. https://bugs.webkit.org/show_bug.cgi?id=92373
    Reviewed by Pavel Feldman.
    
    Moved formatting support from JavaScriptSource to UISourceCode.
    Extracted Formatter interface from ScriptFormatter and created IdentityFormatter
    implememntation and Formatter.createFormatter(contentType) formatter factory.
    Introduced virtual formattedChanged() method on UISourceCode.
    
    * inspector/front-end/JavaScriptSource.js:
    (WebInspector.JavaScriptSource):
    (WebInspector.JavaScriptSource.prototype.uiLocationToRawLocation):
    (WebInspector.JavaScriptSource.prototype.breakpointStorageId):
    (WebInspector.JavaScriptSource.prototype.searchInContent):
    (WebInspector.JavaScriptSource.prototype.formattedChanged):
    * inspector/front-end/ScriptFormatter.js:
    (WebInspector.Formatter):
    (WebInspector.Formatter.createFormatter):
    (WebInspector.Formatter.locationToPosition):
    (WebInspector.Formatter.positionToLocation):
    (WebInspector.Formatter.prototype.formatContent):
    (WebInspector.ScriptFormatter):
    (WebInspector.IdentityFormatter):
    (WebInspector.IdentityFormatter.prototype.formatContent):
    (WebInspector.FormatterSourceMappingImpl.prototype.originalToFormatted):
    (WebInspector.FormatterSourceMappingImpl.prototype.formattedToOriginal):
    * inspector/front-end/UISourceCode.js:
    (WebInspector.UISourceCode):
    (WebInspector.UISourceCode.prototype.requestContent):
    (WebInspector.UISourceCode.prototype._fireContentAvailable):
    (WebInspector.UISourceCode.prototype.uiLocationToRawLocation):
    (WebInspector.UISourceCode.prototype.overrideLocation):
    (WebInspector.UISourceCode.prototype.togglingFormatter):
    (WebInspector.UISourceCode.prototype.formatted):
    (WebInspector.UISourceCode.prototype.setFormatted.if):
    (WebInspector.UISourceCode.prototype.setFormatted.didGetContent.formattedChanged):
    (WebInspector.UISourceCode.prototype.setFormatted.didGetContent):
    (WebInspector.UISourceCode.prototype.setFormatted):
    (WebInspector.UISourceCode.prototype.createFormatter):
    (WebInspector.UISourceCode.prototype.formattedChanged):
    
    Source/WebKit2: [EFL][WK2] Add unit tests for Ewk_View form client
    https://bugs.webkit.org/show_bug.cgi?id=92468
    
    Reviewed by Simon Hausmann.
    
    Add unit test for form client functionality in
    Ewk_View.
    
    * UIProcess/API/efl/tests/test_ewk2_view.cpp:
    (onFormAboutToBeSubmitted):
    (TEST_F):
    
    LayoutTests: Web Inspector: Move formatting support from JavaScriptSource to UISourceCode.
    https://bugs.webkit.org/show_bug.cgi?id=92373
    
    Reviewed by Pavel Feldman.
    
    * inspector/debugger/script-formatter.html:
    
    
    Canonical link: https://commits.webkit.org/110288@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@123853 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/efl/tests/test_ewk2_view.cpp
+++ Source/WebKit2/UIProcess/API/efl/tests/test_ewk2_view.cpp
@@ -60,3 +60,54 @@ TEST_F(EWK2UnitTestBase, ewk_view_setting_encoding_custom)
     ASSERT_TRUE(ewk_view_setting_encoding_custom_set(webView(), 0));
     ASSERT_FALSE(ewk_view_setting_encoding_custom_get(webView()));
 }
+
+static void onFormAboutToBeSubmitted(void* userData, Evas_Object*, void* eventInfo)
+{
+    Ewk_Form_Submission_Request* request = static_cast<Ewk_Form_Submission_Request*>(eventInfo);
+    bool* handled = static_cast<bool*>(userData);
+
+    ASSERT_TRUE(request);
+
+    Eina_List* fieldNames = ewk_form_submission_request_field_names_get(request);
+    ASSERT_TRUE(fieldNames);
+    ASSERT_EQ(eina_list_count(fieldNames), 3);
+    void* data;
+    EINA_LIST_FREE(fieldNames, data)
+        eina_stringshare_del(static_cast<char*>(data));
+
+    const char* value1 = ewk_form_submission_request_field_value_get(request, "text1");
+    ASSERT_STREQ(value1, "value1");
+    eina_stringshare_del(value1);
+    const char* value2 = ewk_form_submission_request_field_value_get(request, "text2");
+    ASSERT_STREQ(value2, "value2");
+    eina_stringshare_del(value2);
+    const char* password = ewk_form_submission_request_field_value_get(request, "password");
+    ASSERT_STREQ(password, "secret");
+    eina_stringshare_del(password);
+
+    *handled = true;
+}
+
+TEST_F(EWK2UnitTestBase, ewk_view_form_submission_request)
+{
+    const char* formHTML =
+        "<html><head><script type='text/javascript'>function submitForm() { document.getElementById('myform').submit(); }</script></head>"
+        "<body onload='submitForm()'>"
+        " <form id='myform' action='#'>"
+        "  <input type='text' name='text1' value='value1'>"
+        "  <input type='text' name='text2' value='value2'>"
+        "  <input type='password' name='password' value='secret'>"
+        "  <textarea cols='5' rows='5' name='textarea'>Text</textarea>"
+        "  <input type='hidden' name='hidden1' value='hidden1'>"
+        "  <input type='submit' value='Submit'>"
+        " </form>"
+        "</body></html>";
+
+    ewk_view_html_string_load(webView(), formHTML, "file:///", 0);
+    bool handled = false;
+    evas_object_smart_callback_add(webView(), "form,submission,request", onFormAboutToBeSubmitted, &handled);
+    while (!handled)
+        ecore_main_loop_iterate();
+    ASSERT_TRUE(handled);
+    evas_object_smart_callback_del(webView(), "form,submission,request", onFormAboutToBeSubmitted);
+}
