197c256e354da411efc8d69bfcc2557b4bbf16c2
Alexander Pavlov <apavlov@chromium.org>
2012-07-30 12:51:04 +0000

    Web Inspector: [Styles] No color swatch for the SVG "fill" and "stroke" property values https://bugs.webkit.org/show_bug.cgi?id=92638
    Reviewed by Vsevolod Vlasov.
    
    Added the "fill" and "stroke" properties to the list of color-aware properties.
    
    * inspector/front-end/CSSKeywordCompletions.js:
    
    Canonical link: https://commits.webkit.org/110450@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@124016 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/CSSKeywordCompletions.js
+++ Source/WebCore/inspector/front-end/CSSKeywordCompletions.js
@@ -87,7 +87,7 @@ WebInspector.CSSKeywordCompletions._colors = [
 WebInspector.CSSKeywordCompletions._colorAwareProperties = [
     "background", "background-color", "background-image", "border", "border-color", "border-top", "border-right", "border-bottom",
     "border-left", "border-top-color", "border-right-color", "border-bottom-color", "border-left-color", "box-shadow", "color",
-    "outline", "outline-color", "text-line-through", "text-line-through-color", "text-overline", "text-overline-color",
+    "fill", "outline", "outline-color", "stroke", "text-line-through", "text-line-through-color", "text-overline", "text-overline-color",
     "text-shadow", "text-underline", "text-underline-color", "-webkit-box-shadow", "-webkit-text-emphasis", "-webkit-text-emphasis-color"
 ].keySet();
 
