5b5207b3cf078397383b9d48f7fc7d9fa536f8f9
Mario Sanchez Prada <mario@webkit.org>
2012-07-31 15:51:29 +0000

    [GTK] Add a new and reusable enchant-based spellchecker in WebCore https://bugs.webkit.org/show_bug.cgi?id=90269
    Reviewed by Martin Robinson.
    
    Source/WebCore:
    
    Move enchant specific code down to WebCore, into a new class
    TextCheckerEnchant, that we can use from WK1 and WK2.
    
    * GNUmakefile.am: Add flags to handle the SPELLCHECK feature.
    * GNUmakefile.list.am: Added new files.
    * platform/text/gtk/TextCheckerEnchant.cpp: Added.
    (getAvailableDictionariesCallback):
    (TextCheckerEnchant::TextCheckerEnchant):
    (TextCheckerEnchant::~TextCheckerEnchant):
    (TextCheckerEnchant::ignoreWord):
    (TextCheckerEnchant::learnWord):
    (TextCheckerEnchant::checkSpellingOfString):
    (TextCheckerEnchant::getGuessesForWord):
    (TextCheckerEnchant::updateSpellCheckingLanguages):
    (TextCheckerEnchant::freeEnchantBrokerDictionaries):
    * platform/text/gtk/TextCheckerEnchant.h: Added.
    (WebCore):
    (TextCheckerEnchant):
    (WebCore::TextCheckerEnchant::create):
    
    Source/WebKit/gtk:
    
    Remove enchant specific code from WebKitSpellCheckerEnchant and
    implement it relying in the new TextCheckerEnchant class in WebCore.
    
    * webkit/webkitspellcheckerenchant.cpp:
    (_WebKitSpellCheckerEnchantPrivate):
    (webkit_spell_checker_enchant_finalize):
    (webkit_spell_checker_enchant_class_init):
    (webkit_spell_checker_enchant_init):
    (checkSpellingOfString):
    (getGuessesForWord):
    (updateSpellCheckingLanguages):
    (learnWord):
    (ignoreWord):
    
    Canonical link: https://commits.webkit.org/110582@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@124207 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebCore/platform/text/gtk/TextCheckerEnchant.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2012 Igalia S.L.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef TextCheckerEnchant_h
+#define TextCheckerEnchant_h
+
+#if ENABLE(SPELLCHECK)
+
+#include <enchant.h>
+#include <wtf/FastAllocBase.h>
+#include <wtf/PassOwnPtr.h>
+#include <wtf/Vector.h>
+#include <wtf/text/WTFString.h>
+
+namespace WebCore {
+
+class TextCheckerEnchant {
+    WTF_MAKE_FAST_ALLOCATED;
+
+public:
+    static PassOwnPtr<TextCheckerEnchant> create() { return adoptPtr(new TextCheckerEnchant); }
+    virtual ~TextCheckerEnchant();
+
+    void ignoreWord(const String&);
+    void learnWord(const String&);
+    void checkSpellingOfString(const String&, int& misspellingLocation, int& misspellingLength);
+    Vector<String> getGuessesForWord(const String&);
+    void updateSpellCheckingLanguages(const String& languages);
+
+private:
+    TextCheckerEnchant();
+    void freeEnchantBrokerDictionaries();
+
+    EnchantBroker* m_broker;
+    Vector<EnchantDict*> m_enchantDictionaries;
+};
+
+} // namespace WebCore
+
+#endif // ENABLE(SPELLCHECK)
+
+#endif
