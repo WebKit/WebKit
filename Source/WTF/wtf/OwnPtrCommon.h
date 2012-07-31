0a2bc8f0bda63a9984e66a4c0e16b7fc4c1a4d39
Patrick Gansterer <paroga@webkit.org>
2012-07-31 01:13:48 +0000

    Add WTF_EXPORT_PRIVATE to deleteOwnedPtr() https://bugs.webkit.org/show_bug.cgi?id=92668
    Reviewed by Hajime Morita.
    
    When using export macros all non-inlined deleteOwnedPtr()
    functions need to be declared with WTF_EXPORT_PRIVATE.
    
    * wtf/OwnPtrCommon.h:
    (WTF):
    
    
    Canonical link: https://commits.webkit.org/110521@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@124134 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/OwnPtrCommon.h
+++ Source/WTF/wtf/OwnPtrCommon.h
@@ -57,22 +57,22 @@ namespace WTF {
     }
 
 #if OS(WINDOWS)
-    void deleteOwnedPtr(HBITMAP);
-    void deleteOwnedPtr(HBRUSH);
-    void deleteOwnedPtr(HDC);
-    void deleteOwnedPtr(HFONT);
-    void deleteOwnedPtr(HPALETTE);
-    void deleteOwnedPtr(HPEN);
-    void deleteOwnedPtr(HRGN);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(HBITMAP);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(HBRUSH);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(HDC);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(HFONT);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(HPALETTE);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(HPEN);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(HRGN);
 #endif
 
 #if PLATFORM(EFL)
-    void deleteOwnedPtr(Ecore_Evas*);
-    void deleteOwnedPtr(Ecore_Pipe*);
-    void deleteOwnedPtr(Ecore_Timer*);
-    void deleteOwnedPtr(Eina_Hash*);
-    void deleteOwnedPtr(Eina_Module*);
-    void deleteOwnedPtr(Evas_Object*);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(Ecore_Evas*);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(Ecore_Pipe*);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(Ecore_Timer*);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(Eina_Hash*);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(Eina_Module*);
+    WTF_EXPORT_PRIVATE void deleteOwnedPtr(Evas_Object*);
 #endif
 
 } // namespace WTF
