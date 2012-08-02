9692889b4e2ecc936ebe50e495ed9c5a14e595ff
Erik Arvidsson <arv@chromium.org>
2012-08-02 21:28:12 +0000

    DOM4: className should be defined on Element and not on HTMLElement https://bugs.webkit.org/show_bug.cgi?id=93014
    Reviewed by Adam Barth.
    
    DOM4 specs that Element should have the className WebIDL attribute. This moves the
    attribute to the correct IDL file.
    
    Source/WebCore:
    
    Test: fast/dom/Element/class-name.html
    
    * dom/Element.idl:
    * html/HTMLElement.idl:
    
    LayoutTests:
    
    * fast/dom/Element/class-name-expected.txt: Added.
    * fast/dom/Element/class-name.html: Added.
    
    
    Canonical link: https://commits.webkit.org/110869@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@124499 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/objc/PublicDOMInterfaces.h
+++ Source/WebCore/bindings/objc/PublicDOMInterfaces.h
@@ -174,6 +174,7 @@
 @interface DOMElement : DOMNode WEBKIT_VERSION_1_3
 @property(readonly, copy) NSString *tagName;
 @property(readonly, retain) DOMCSSStyleDeclaration *style;
+@property(copy) NSString *className;
 @property(readonly) int offsetLeft;
 @property(readonly) int offsetTop;
 @property(readonly) int offsetWidth;
@@ -472,7 +473,6 @@
 @property(copy) NSString *idName;
 @property(copy) NSString *lang;
 @property(copy) NSString *dir;
-@property(copy) NSString *className;
 @property(copy) NSString *innerHTML;
 @property(copy) NSString *innerText;
 @property(copy) NSString *outerHTML;
