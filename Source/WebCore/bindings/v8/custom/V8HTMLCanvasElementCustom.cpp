d2204a5c6207f9bcca5444a44869190a4685d1f9
Kenneth Russell <kbr@google.com>
2012-08-03 21:19:36 +0000

    [Chromium] Web Inspector: Win Dbg tests timing out (r123556 or r123560?) https://bugs.webkit.org/show_bug.cgi?id=92570
    Reviewed by Pavel Feldman.
    
    Try restricting the use of the GC hint for canvas contexts to only
    WebGL contexts, where it is most needed, to see whether this
    clears up the timeouts.
    
    * bindings/v8/custom/V8HTMLCanvasElementCustom.cpp:
    (WebCore::V8HTMLCanvasElement::getContextCallback):
    
    
    Canonical link: https://commits.webkit.org/110999@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@124653 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/custom/V8HTMLCanvasElementCustom.cpp
+++ Source/WebCore/bindings/v8/custom/V8HTMLCanvasElementCustom.cpp
@@ -87,16 +87,16 @@ v8::Handle<v8::Value> V8HTMLCanvasElement::getContextCallback(const v8::Argument
     if (!result)
         return v8::Null(args.GetIsolate());
 
-    // Both 2D and 3D canvas contexts can hold on to lots of GPU resources, and we
-    // want to take an opportunity to get rid of them as soon as possible when we
-    // navigate away from pages using them.
-    V8BindingPerIsolateData* perIsolateData = V8BindingPerIsolateData::current(args.GetIsolate());
-    perIsolateData->setShouldCollectGarbageSoon();
-
     if (result->is2d())
         return toV8(static_cast<CanvasRenderingContext2D*>(result), args.GetIsolate());
 #if ENABLE(WEBGL)
     else if (result->is3d()) {
+        // 3D canvas contexts can hold on to lots of GPU resources, and we want to take an
+        // opportunity to get rid of them as soon as possible when we navigate away from pages using
+        // them.
+        V8BindingPerIsolateData* perIsolateData = V8BindingPerIsolateData::current(args.GetIsolate());
+        perIsolateData->setShouldCollectGarbageSoon();
+
         v8::Handle<v8::Value> v8Result = toV8(static_cast<WebGLRenderingContext*>(result), args.GetIsolate());
         if (InspectorInstrumentation::hasFrontends()) {
             ScriptState* scriptState = ScriptState::forContext(v8::Context::GetCurrent());
