78bfaee6a56f9e6d2e1b5ee661b29bc58bcb0209
Joone Hur <joone.hur@intel.com>
2012-08-03 04:42:15 +0000

    [GTK] Build break when building DumpRenderTree/gtk/EditingCallbacks.cpp https://bugs.webkit.org/show_bug.cgi?id=93061
    Unreviewed, Fix build break for WebKitGtk+.
    
    Use webkit_dom_element_get_class_name instead of webkit_dom_html_element_get_class_name.
    
    Patch by Joone Hur <joone.hur@intel.com> on 2012-08-02
    
    * DumpRenderTree/gtk/EditingCallbacks.cpp:
    (shouldShowDeleteInterfaceForElement):
    
    Canonical link: https://commits.webkit.org/110908@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@124559 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/DumpRenderTree/gtk/EditingCallbacks.cpp
+++ Tools/DumpRenderTree/gtk/EditingCallbacks.cpp
@@ -138,7 +138,7 @@ gboolean shouldDeleteRange(WebKitWebView* webView, WebKitDOMRange* range)
 
 gboolean shouldShowDeleteInterfaceForElement(WebKitWebView* webView, WebKitDOMHTMLElement* element)
 {
-    GOwnPtr<gchar> elementClassName(webkit_dom_html_element_get_class_name(element));
+    GOwnPtr<gchar> elementClassName(webkit_dom_element_get_class_name(WEBKIT_DOM_ELEMENT(element)));
     return g_str_equal(elementClassName.get(), "needsDeletionUI");
 }
 
