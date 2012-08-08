8bbaceea916df676e67f470f6ee7ba6e3a840a70
Yury Semikhatsky <yurys@chromium.org>
2012-08-08 09:16:30 +0000

    Web Inspector: cached images memory instrumentation regression after r124744 https://bugs.webkit.org/show_bug.cgi?id=93366
    Reviewed by Vsevolod Vlasov.
    
    Source/WebCore:
    
    CachedImage memory usage data no includes size of both encoded and
    decoded data.
    
    Test: inspector/profiler/memory-instrumentation-cached-images.html
    
    * loader/cache/CachedImage.cpp:
    (WebCore::CachedImage::reportMemoryUsage):
    
    LayoutTests:
    
    Added a sanity test checking that memory usage data includes decoded image size.
    
    * inspector/profiler/memory-instrumentation-cached-images-expected.txt: Added.
    * inspector/profiler/memory-instrumentation-cached-images.html: Added.
    
    
    Canonical link: https://commits.webkit.org/111319@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@125011 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/cache/CachedImage.cpp
+++ Source/WebCore/loader/cache/CachedImage.cpp
@@ -471,7 +471,11 @@ void CachedImage::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const
 {
     MemoryClassInfo info(memoryObjectInfo, this, MemoryInstrumentation::CachedResourceImage);
     CachedResource::reportMemoryUsage(memoryObjectInfo);
-    info.addMember(m_image);
+    if (m_image) {
+        if (m_image->data())
+            info.addRawBuffer(m_image->data(), m_image->data()->size());
+        info.addRawBuffer(m_image.get(), decodedSize());
+    }
 #if ENABLE(SVG)
     info.addMember(m_svgImageCache);
 #endif
