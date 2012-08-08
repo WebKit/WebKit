WebKit/WebKit

commit c510476da814db7dfc0ed79905a149aefc7b08eb

Don't re-use the same EventDispatcher instance to dispatch events. https://bugs.webkit.org/show_bug.cgi?id=93322
Reviewed by Dimitri Glazkov.

It is potentially dangerous to call
EventDispatcher::dispatchEvent(PassRefPtr<Event>) twice for the
same EventDispatcher instance.  Some member functions in
EventDispatcher assume that dispatchEvent(PassRefPtr<Event>) is
never called more than once in its life cycle.

For example, EventDispatcher::ensureEventAncestor never
recalculates ancestors of node even when
dispatchEvent(PassRefPtr<Event)) is called again with a different
event parameter.

A 'dblclick' event violates this rule. A 'dblclick' dispatching
reuses the same dispatcher instance.  So stop re-using the same
dispatcher and add ASSERT to make sure dispatchEvent() is never
called more than once.

No new tests, no change in functionality.

* dom/EventDispatcher.cpp:
(WebCore::EventDispatcher::EventDispatcher):
(WebCore::EventDispatcher::dispatchEvent):
* dom/EventDispatcher.h:
(EventDispatcher):
* dom/MouseEvent.cpp:
(WebCore::MouseEventDispatchMediator::dispatchEvent):


Canonical link: https://commits.webkit.org/111283@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@124975 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/EventDispatcher.cpp
@@ -201,6 +201,9 @@ void EventDispatcher::adjustRelatedTarget(Event* event, PassRefPtr<EventTarget> 
 EventDispatcher::EventDispatcher(Node* node)
     : m_node(node)
     , m_ancestorsInitialized(false)
+#ifndef NDEBUG
+    , m_eventDispatched(false)
+#endif
 {
     ASSERT(node);
     m_view = node->document()->view();
@@ -238,6 +241,10 @@ void EventDispatcher::ensureEventAncestors(Event* event)
 
 bool EventDispatcher::dispatchEvent(PassRefPtr<Event> prpEvent)
 {
+#ifndef NDEBUG
+    ASSERT(!m_eventDispatched);
+    m_eventDispatched = true;
+#endif
     RefPtr<Event> event = prpEvent;
     event->setTarget(eventTargetRespectingSVGTargetRules(m_node.get()));
     ASSERT(!eventDispatchForbidden());

Source/WebCore/dom/EventDispatcher.h
@@ -96,6 +96,9 @@ private:
     RefPtr<Node> m_node;
     RefPtr<FrameView> m_view;
     bool m_ancestorsInitialized;
+#ifndef NDEBUG
+    bool m_eventDispatched;
+#endif
 };
 
 inline Node* EventDispatcher::node() const

Source/WebCore/dom/MouseEvent.cpp
@@ -207,22 +207,21 @@ bool MouseEventDispatchMediator::dispatchEvent(EventDispatcher* dispatcher) cons
     dispatcher->dispatchEvent(event());
     bool swallowEvent = event()->defaultHandled() || event()->defaultPrevented();
 
+    if (event()->type() != eventNames().clickEvent || event()->detail() != 2)
+        return swallowEvent;
     // Special case: If it's a double click event, we also send the dblclick event. This is not part
     // of the DOM specs, but is used for compatibility with the ondblclick="" attribute. This is treated
     // as a separate event in other DOM-compliant browsers like Firefox, and so we do the same.
-    if (event()->type() == eventNames().clickEvent && event()->detail() == 2) {
-        RefPtr<MouseEvent> doubleClickEvent = MouseEvent::create();
-        doubleClickEvent->initMouseEvent(eventNames().dblclickEvent, event()->bubbles(), event()->cancelable(), event()->view(),
-                event()->detail(), event()->screenX(), event()->screenY(), event()->clientX(), event()->clientY(),
-                event()->ctrlKey(), event()->altKey(), event()->shiftKey(), event()->metaKey(),
-                event()->button(), relatedTarget);
-        if (event()->defaultHandled())
-            doubleClickEvent->setDefaultHandled();
-        dispatcher->dispatchEvent(doubleClickEvent);
-        if (doubleClickEvent->defaultHandled() || doubleClickEvent->defaultPrevented())
-            swallowEvent = true;
-    }
-
+    RefPtr<MouseEvent> doubleClickEvent = MouseEvent::create();
+    doubleClickEvent->initMouseEvent(eventNames().dblclickEvent, event()->bubbles(), event()->cancelable(), event()->view(),
+                                     event()->detail(), event()->screenX(), event()->screenY(), event()->clientX(), event()->clientY(),
+                                     event()->ctrlKey(), event()->altKey(), event()->shiftKey(), event()->metaKey(),
+                                     event()->button(), relatedTarget);
+    if (event()->defaultHandled())
+        doubleClickEvent->setDefaultHandled();
+    EventDispatcher::dispatchEvent(dispatcher->node(), MouseEventDispatchMediator::create(doubleClickEvent));
+    if (doubleClickEvent->defaultHandled() || doubleClickEvent->defaultPrevented())
+        return true;
     return swallowEvent;
 }
 

commit 2abde8f2820fa7a8ebbb8e7162a349b2880997d1

Unreviewed build fix after http://trac.webkit.org/changeset/124975
Remove assertion introduced in r124975.

* dom/EventDispatcher.cpp:
(WebCore::EventDispatcher::EventDispatcher):
(WebCore::EventDispatcher::dispatchEvent):
* dom/EventDispatcher.h:
(EventDispatcher):

Canonical link: https://commits.webkit.org/111300@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@124992 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/EventDispatcher.cpp
@@ -202,9 +202,6 @@ void EventDispatcher::adjustRelatedTarget(Event* event, PassRefPtr<EventTarget> 
 EventDispatcher::EventDispatcher(Node* node)
     : m_node(node)
     , m_ancestorsInitialized(false)
-#ifndef NDEBUG
-    , m_eventDispatched(false)
-#endif
 {
     ASSERT(node);
     m_view = node->document()->view();
@@ -242,10 +239,6 @@ void EventDispatcher::ensureEventAncestors(Event* event)
 
 bool EventDispatcher::dispatchEvent(PassRefPtr<Event> prpEvent)
 {
-#ifndef NDEBUG
-    ASSERT(!m_eventDispatched);
-    m_eventDispatched = true;
-#endif
     RefPtr<Event> event = prpEvent;
     ChildNodesLazySnapshot::takeChildNodesLazySnapshot();
 

Source/WebCore/dom/EventDispatcher.h
@@ -96,9 +96,6 @@ private:
     RefPtr<Node> m_node;
     RefPtr<FrameView> m_view;
     bool m_ancestorsInitialized;
-#ifndef NDEBUG
-    bool m_eventDispatched;
-#endif
 };
 
 inline Node* EventDispatcher::node() const