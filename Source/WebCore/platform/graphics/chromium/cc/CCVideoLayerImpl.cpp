36866cd87ef63b91156dcf1366c16ced7fada694
Ami Fischman <fischman@chromium.org>
2012-08-12 17:40:25 +0000

    Stop Y-flipping CROS/ARM video textures https://bugs.webkit.org/show_bug.cgi?id=93769
    Reviewed by Eric Seidel.
    
    No new tests; there's no cros/arm bot yet, sadly.
    
    * platform/graphics/chromium/cc/CCVideoLayerImpl.cpp:
    (WebCore::CCVideoLayerImpl::appendQuads): drop the single-platform Y-flipping in prep for the platform doing the flip natively.
    
    
    Canonical link: https://commits.webkit.org/111653@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@125378 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/chromium/cc/CCVideoLayerImpl.cpp
+++ Source/WebCore/platform/graphics/chromium/cc/CCVideoLayerImpl.cpp
@@ -215,13 +215,7 @@ void CCVideoLayerImpl::appendQuads(CCQuadSink& quadList, const CCSharedQuadState
         // NativeTexture hardware decoder.
         bool premultipliedAlpha = true;
         FloatRect uvRect(0, 0, 1, 1);
-#if defined(OS_CHROMEOS) && defined(__ARMEL__)
-        bool flipped = true; // Under the covers, implemented by OpenMAX IL.
-#elif OS(WINDOWS)
-        bool flipped = false; // Under the covers, implemented by DXVA.
-#else
-        bool flipped = false; // LibVA (cros/intel), MacOS.
-#endif
+        bool flipped = false;
         OwnPtr<CCTextureDrawQuad> textureQuad = CCTextureDrawQuad::create(sharedQuadState, quadRect, m_externalTextureResource, premultipliedAlpha, uvRect, flipped);
         quadList.append(textureQuad.release());
         break;
