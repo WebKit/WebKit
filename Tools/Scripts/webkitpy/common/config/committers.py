c32d0db310613ea9e3ce9ed84d28652d20b5e85b
Hajime Morrita <morrita@google.com>
2012-08-13 05:21:54 +0000

    Unreviewed, added a proxy address to contributors_who_are_not_committers.
    * Scripts/webkitpy/common/config/committers.py:
    
    Canonical link: https://commits.webkit.org/111665@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@125390 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/config/committers.py
+++ Tools/Scripts/webkitpy/common/config/committers.py
@@ -161,6 +161,7 @@ contributors_who_are_not_committers = [
     Contributor("Terry Anderson", "tdanderson@chromium.org", "tdanderson"),
     Contributor("Tien-Ren Chen", "trchen@chromium.org", "trchen"),
     Contributor("WebKit Review Bot", "webkit.review.bot@gmail.com", "sheriff-bot"),
+    Contributor("Web Components Team", "webcomponents-bugzilla@google.com"),
     Contributor("Wyatt Carss", ["wcarss@chromium.org", "wcarss@google.com"], "wcarss"),
     Contributor("Zeev Lieber", "zlieber@chromium.org"),
     Contributor("Zoltan Arvai", "zarvai@inf.u-szeged.hu", "azbest_hu"),
