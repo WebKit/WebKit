34874ae29bc494c8752466d023815c6aaaf410fe
Filip Pizlo <fpizlo@apple.com>
2012-08-16 20:35:12 +0000

    All op_resolve_global instructions should end up in the list of global resolve instructions https://bugs.webkit.org/show_bug.cgi?id=94247 <rdar://problem/12103500>
    Reviewed by Mark Hahnenberg.
    
    * bytecompiler/BytecodeGenerator.cpp:
    (JSC::BytecodeGenerator::emitResolveWithBase):
    
    
    
    Canonical link: https://commits.webkit.org/112061@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@125806 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
+++ Source/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
@@ -1464,9 +1464,7 @@ RegisterID* BytecodeGenerator::emitResolveWithBase(RegisterID* baseDst, Register
 #if ENABLE(JIT)
         m_codeBlock->addGlobalResolveInfo(instructions().size());
 #endif
-#if ENABLE(CLASSIC_INTERPRETER)
         m_codeBlock->addGlobalResolveInstruction(instructions().size());
-#endif
         ValueProfile* profile = emitProfiledOpcode(op_resolve_global);
         instructions().append(propDst->index());
         instructions().append(addConstant(property));
