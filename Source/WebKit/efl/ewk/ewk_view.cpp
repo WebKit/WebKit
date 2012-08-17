4ddef71fe596953c97d4ef0b97f5ba7e7df52067
Thiago Marcos P. Santos <thiago.santos@intel.com>
2012-08-17 12:23:18 +0000

    [EFL] Change font family/script on layout tests causes flakiness https://bugs.webkit.org/show_bug.cgi?id=94327
    Patch by Thiago Marcos P. Santos <thiago.santos@intel.com> on 2012-08-17
    Reviewed by Kenneth Rohde Christiansen.
    
    Always set the font on WebCore even if the font family is the same. It
    has to be done because the script value might have also changed.
    Settings::set*FontFamily() has an implicit UScriptCode
    parameter that will set the script back to the defaults.
    
    * ewk/ewk_view.cpp:
    (ewk_view_font_family_name_set):
    
    Canonical link: https://commits.webkit.org/112136@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@125885 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/efl/ewk/ewk_view.cpp
+++ Source/WebKit/efl/ewk/ewk_view.cpp
@@ -2502,28 +2502,28 @@ Eina_Bool ewk_view_font_family_name_set(Evas_Object* ewkView, Ewk_Font_Family fo
 
     switch (fontFamily) {
     case EWK_FONT_FAMILY_STANDARD:
-        if (eina_stringshare_replace(&priv->settings.fontStandard, name))
-            priv->pageSettings->setStandardFontFamily(AtomicString::fromUTF8(name));
+        eina_stringshare_replace(&priv->settings.fontStandard, name);
+        priv->pageSettings->setStandardFontFamily(AtomicString::fromUTF8(name));
         break;
     case EWK_FONT_FAMILY_CURSIVE:
-        if (eina_stringshare_replace(&priv->settings.fontCursive, name))
-            priv->pageSettings->setCursiveFontFamily(AtomicString::fromUTF8(name));
+        eina_stringshare_replace(&priv->settings.fontCursive, name);
+        priv->pageSettings->setCursiveFontFamily(AtomicString::fromUTF8(name));
         break;
     case EWK_FONT_FAMILY_FANTASY:
-        if (eina_stringshare_replace(&priv->settings.fontFantasy, name))
-            priv->pageSettings->setFantasyFontFamily(AtomicString::fromUTF8(name));
+        eina_stringshare_replace(&priv->settings.fontFantasy, name);
+        priv->pageSettings->setFantasyFontFamily(AtomicString::fromUTF8(name));
         break;
     case EWK_FONT_FAMILY_MONOSPACE:
-        if (eina_stringshare_replace(&priv->settings.fontMonospace, name))
-            priv->pageSettings->setFixedFontFamily(AtomicString::fromUTF8(name));
+        eina_stringshare_replace(&priv->settings.fontMonospace, name);
+        priv->pageSettings->setFixedFontFamily(AtomicString::fromUTF8(name));
         break;
     case EWK_FONT_FAMILY_SERIF:
-        if (eina_stringshare_replace(&priv->settings.fontSerif, name))
-            priv->pageSettings->setSerifFontFamily(AtomicString::fromUTF8(name));
+        eina_stringshare_replace(&priv->settings.fontSerif, name);
+        priv->pageSettings->setSerifFontFamily(AtomicString::fromUTF8(name));
         break;
     case EWK_FONT_FAMILY_SANS_SERIF:
-        if (eina_stringshare_replace(&priv->settings.fontSansSerif, name))
-            priv->pageSettings->setSansSerifFontFamily(AtomicString::fromUTF8(name));
+        eina_stringshare_replace(&priv->settings.fontSansSerif, name);
+        priv->pageSettings->setSansSerifFontFamily(AtomicString::fromUTF8(name));
         break;
     default:
         return false;
