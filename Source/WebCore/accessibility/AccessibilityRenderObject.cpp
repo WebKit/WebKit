5f66246773e0850c5d520958beb9f3da40642f6a
Mario Sanchez Prada <msanchez@igalia.com>
2012-08-20 16:18:36 +0000

    Merge 125321 - [GTK] Broken implementation of AtkText and AtkEditableText for password fields https://bugs.webkit.org/show_bug.cgi?id=93621
    Patch by Mario Sanchez Prada <msanchez@igalia.com> on 2012-08-10
    Reviewed by Chris Fleizach.
    
    Source/WebCore:
    
    Fix broken implementation of AtkText and AtkEditableText
    interfaces in the GTK port for password input fields.
    
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::stringValue): Call the new
    function passwordFieldValue() for password input fields.
    (WebCore::AccessibilityRenderObject::text): Ditto.
    (WebCore::AccessibilityRenderObject::textLength): Return the
    actual length of the password in the field for GTK, return -1
    otherwise (not to break current behavior in other platforms).
    (WebCore::AccessibilityRenderObject::doAXStringForRange): Don't
    early return for password fields in GTK (will rely on text()).
    (WebCore::AccessibilityRenderObject::passwordFieldValue): New, it
    returns the text being actually rendered for a password input
    field (normally a masked string) in GTK. It returns String() in
    the rest of platforms, to ensure we don't break anything there.
    (WebCore):
    * accessibility/AccessibilityRenderObject.h:
    (AccessibilityRenderObject):
    * accessibility/AccessibilityObject.h:
    (WebCore::AccessibilityObject::passwordFieldValue): Default
    implementation of this new method, returning an empty string.
    (AccessibilityObject):
    
    Ensure the 'text-insert' and 'text-remove' signals for the AtkText
    interface are properly emitted for password input fields.
    
    * accessibility/gtk/AXObjectCacheAtk.cpp:
    (WebCore::AXObjectCache::nodeTextChangePlatformNotification): Make
    sure we never emit the password value for an input field in plain
    text when inserting or removing text. Emit the masked text instead.
    
    Source/WebKit/gtk:
    
    Update unit test to ensure that password input fields behave
    as expected when inserting and removing characters in them.
    
    * tests/testatk.c:
    (testWebkitAtkTextChangedNotifications): Updated unit test to
    cover the special case of password input fields.
    
    Canonical link: https://commits.webkit.org/96014.264@webkitgtk/1.8

--- Source/WebCore/accessibility/AccessibilityRenderObject.cpp
+++ Source/WebCore/accessibility/AccessibilityRenderObject.cpp
@@ -1154,8 +1154,11 @@ float AccessibilityRenderObject::minValueForRange() const
 
 String AccessibilityRenderObject::stringValue() const
 {
-    if (!m_renderer || isPasswordField())
+    if (!m_renderer)
         return String();
+
+    if (isPasswordField())
+        return passwordFieldValue();
 
     RenderBoxModelObject* cssBox = renderBoxModelObject();
 
@@ -1975,8 +1978,11 @@ String AccessibilityRenderObject::text() const
     if (ariaRoleAttribute() == StaticTextRole)
         return ariaAccessibilityDescription();
     
-    if (!isTextControl() || isPasswordField())
+    if (!isTextControl())
         return String();
+
+    if (isPasswordField())
+        return passwordFieldValue();
 
     Node* node = m_renderer->node();
     if (!node)
@@ -1996,8 +2002,12 @@ int AccessibilityRenderObject::textLength() const
     ASSERT(isTextControl());
     
     if (isPasswordField())
+#if PLATFORM(GTK)
+        return passwordFieldValue().length();
+#else
         return -1; // need to return something distinct from 0
-    
+#endif
+
     return text().length();
 }
 
@@ -2792,16 +2802,13 @@ PlainTextRange AccessibilityRenderObject::doAXRangeForIndex(unsigned index) cons
 // specified by the given character range.
 String AccessibilityRenderObject::doAXStringForRange(const PlainTextRange& range) const
 {
-    if (isPasswordField())
-        return String();
-    
     if (!range.length)
         return String();
     
     if (!isTextControl())
         return String();
     
-    String elementText = text();
+    String elementText = isPasswordField() ? passwordFieldValue() : text();
     if (range.start + range.length > elementText.length())
         return String();
     
@@ -3938,6 +3945,27 @@ AccessibilityRole AccessibilityRenderObject::roleValueForMSAA() const
         m_roleForMSAA = roleValue();
 
     return m_roleForMSAA;
+}
+
+String AccessibilityRenderObject::passwordFieldValue() const
+{
+#if !PLATFORM(GTK)
+    // It seems only GTK is interested in this at the moment.
+    return String();
+#endif
+
+    ASSERT(isPasswordField());
+
+    // Look for the RenderText object in the RenderObject tree for this input field.
+    RenderObject* renderer = node()->renderer();
+    while (renderer && !renderer->isText())
+        renderer = renderer->firstChild();
+
+    if (!renderer || !renderer->isText())
+        return String();
+
+    // Return the text that is actually being rendered in the input field.
+    return static_cast<RenderText*>(renderer)->textWithoutTranscoding();
 }
 
 ScrollableArea* AccessibilityRenderObject::getScrollableAreaIfScrollable() const
--- Source/WebCore/accessibility/AccessibilityRenderObject.h
+++ Source/WebCore/accessibility/AccessibilityRenderObject.h
@@ -258,6 +258,8 @@ public:
     virtual String descriptionForMSAA() const;
     virtual AccessibilityRole roleValueForMSAA() const;
 
+    virtual String passwordFieldValue() const;
+
 protected:
     RenderObject* m_renderer;
     AccessibilityRole m_ariaRole;
