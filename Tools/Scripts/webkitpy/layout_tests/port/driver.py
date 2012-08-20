10ece63ca777c66fe1433a3d91dc2edfaa470e28
Dirk Pranke <dpranke@chromium.org>
2012-08-20 21:38:42 +0000

    Fix change for timeout detection to not crash if we have no output :) https://bugs.webkit.org/show_bug.cgi?id=94505
    Unreviewed, build fix.
    
    * Scripts/webkitpy/layout_tests/port/driver.py:
    (Driver.run_test):
    
    Canonical link: https://commits.webkit.org/112313@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@126073 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/port/driver.py
+++ Tools/Scripts/webkitpy/layout_tests/port/driver.py
@@ -160,7 +160,7 @@ class Driver(object):
 
         crashed = self.has_crashed()
         timed_out = self._server_process.timed_out
-        if 'Timed out waiting for final message from web process' in text:
+        if text and ('Timed out waiting for final message from web process' in text):
             # FIXME: This is a hack to work around the issues in https://bugs.webkit.org/show_bug.cgi?id=94505.
             # We need to either fix the underlying problem in WTR or return a more canonical error.
             if not timed_out:
