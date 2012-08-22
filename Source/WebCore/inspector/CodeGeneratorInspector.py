1382e0ebd703e397f3dfa0d2e8e8965f3da91ec7
Andrey Adaykin <aandrey@chromium.org>
2012-08-22 08:50:22 +0000

    Web Inspector: [WebGL] Add minimum transport protocol from backend to frontend https://bugs.webkit.org/show_bug.cgi?id=88973
    Patch by Andrey Adaikin <aandrey@chromium.org> on 2012-08-22
    Reviewed by Pavel Feldman.
    
    Added the following protocol methods to communicate with the WebGL injected
    module: captureFrame, getTraceLog, dropTraceLog, replayTraceLog.
    
    * inspector/CodeGeneratorInspector.py:
    * inspector/InjectedScriptWebGLModule.cpp:
    (WebCore::InjectedScriptWebGLModule::captureFrame):
    (WebCore):
    (WebCore::InjectedScriptWebGLModule::dropTraceLog):
    (WebCore::InjectedScriptWebGLModule::getTraceLog):
    (WebCore::InjectedScriptWebGLModule::replayTraceLog):
    * inspector/InjectedScriptWebGLModule.h:
    (InjectedScriptWebGLModule):
    * inspector/Inspector.json:
    * inspector/InspectorController.cpp:
    (WebCore::InspectorController::InspectorController):
    * inspector/InspectorWebGLAgent.cpp:
    (WebCore::InspectorWebGLAgent::InspectorWebGLAgent):
    (WebCore::InspectorWebGLAgent::dropTraceLog):
    (WebCore):
    (WebCore::InspectorWebGLAgent::captureFrame):
    (WebCore::InspectorWebGLAgent::getTraceLog):
    (WebCore::InspectorWebGLAgent::replayTraceLog):
    * inspector/InspectorWebGLAgent.h:
    (WebCore):
    (WebCore::InspectorWebGLAgent::create):
    (InspectorWebGLAgent):
    
    Canonical link: https://commits.webkit.org/112512@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@126280 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/CodeGeneratorInspector.py
+++ Source/WebCore/inspector/CodeGeneratorInspector.py
@@ -59,7 +59,7 @@ TYPE_NAME_FIX_MAP = {
 
 
 TYPES_WITH_RUNTIME_CAST_SET = frozenset(["Runtime.RemoteObject", "Runtime.PropertyDescriptor",
-                                         "Debugger.FunctionDetails", "Debugger.CallFrame",
+                                         "Debugger.FunctionDetails", "Debugger.CallFrame", "WebGL.TraceLog",
                                          # This should be a temporary hack. TimelineEvent should be created via generated C++ API.
                                          "Timeline.TimelineEvent"])
 
