4adbfaade2a31b391682be201632a5b468089547
Julien Chaffraix <jchaffraix@webkit.org>
2012-08-22 02:16:02 +0000

    Crash in RenderTableSection::setCellLogicalWidths https://bugs.webkit.org/show_bug.cgi?id=94291
    Reviewed by Abhishek Arya.
    
    Source/WebCore:
    
    This issue was that splitAnonymousBoxesAroundChild would move a table section
    into a newly created table *without* marking it as needing cell recalc. The table
    would thus never build its structure to match its sections. The fix is to hop on
    the new willBeRemovedFromTree signal so that the section invalidates itself properly.
    
    Test: fast/table/crash-split-table-section-no-cell-recalc.html
    
    * rendering/RenderTableSection.cpp:
    (WebCore::RenderTableSection::willBeRemovedFromTree):
    * rendering/RenderTableSection.h:
    Replaced willBeDestroyed by willBeRemovedFromTree in RenderTableSection. This ensures that it is called
    when moving sections in the tree to mark them as needing cell recalc.
    
    LayoutTests:
    
    * fast/table/crash-split-table-section-no-cell-recalc-expected.txt: Added.
    * fast/table/crash-split-table-section-no-cell-recalc.html: Added.
    
    
    Canonical link: https://commits.webkit.org/112483@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@126251 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderTableSection.h
+++ Source/WebCore/rendering/RenderTableSection.h
@@ -206,7 +206,7 @@ private:
 
     virtual bool isTableSection() const { return true; }
 
-    virtual void willBeDestroyed();
+    virtual void willBeRemovedFromTree() OVERRIDE;
 
     virtual void layout();
 
