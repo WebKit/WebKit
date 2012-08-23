c1e3dcf99a0725245c8e8ea9645d207c433b0ebd
Adam Barth <abarth@webkit.org>
2012-08-23 01:23:12 +0000

    [V8] OwnHandle doesn't need to support weak handles https://bugs.webkit.org/show_bug.cgi?id=94760
    Reviewed by Kentaro Hara.
    
    No code uses makeWeak(). We can drop support for it and simplify this
    class.
    
    * bindings/v8/OwnHandle.h:
    (WebCore):
    (OwnHandle):
    (WebCore::OwnHandle::OwnHandle):
    (WebCore::OwnHandle::~OwnHandle):
    (WebCore::OwnHandle::get):
    (WebCore::OwnHandle::set):
    (WebCore::OwnHandle::clear):
    
    
    Canonical link: https://commits.webkit.org/112607@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@126376 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/OwnHandle.h
+++ Source/WebCore/bindings/v8/OwnHandle.h
@@ -35,45 +35,41 @@
 
 namespace WebCore {
 
-    template<typename T>
-    class OwnHandle {
-    public:
-        OwnHandle() { }
-        explicit OwnHandle(v8::Handle<T> handle) : m_handle(v8::Persistent<T>::New(handle)) { }
-        ~OwnHandle() { clear(); }
+template<typename T>
+class OwnHandle {
+public:
+    OwnHandle() { }
 
-        v8::Handle<T> get() const { return m_handle; }
-        void set(v8::Handle<T> handle) { clear(); m_handle = v8::Persistent<T>::New(handle); }
+    explicit OwnHandle(v8::Handle<T> handle)
+        : m_handle(v8::Persistent<T>::New(handle))
+    {
+    }
 
-        // Note: This is clear in the OwnPtr sense, not the v8::Handle sense.
-        void clear()
-        {
-            if (m_handle.IsEmpty())
-                return;
-            if (m_handle.IsWeak())
-                m_handle.ClearWeak();
-            m_handle.Dispose();
-            m_handle.Clear();
-        }
+    ~OwnHandle()
+    {
+        clear();
+    }
 
-        // Make the underlying handle weak.  The client doesn't get a callback,
-        // we just make the handle empty.
-        void makeWeak()
-        {
-            if (m_handle.IsEmpty())
-                return;
-            m_handle.MakeWeak(this, &OwnHandle<T>::weakCallback);
-        }
+    v8::Handle<T> get() const { return m_handle; }
 
-    private:
-        static void weakCallback(v8::Persistent<v8::Value> object, void* ownHandle)
-        {
-            OwnHandle<T>* handle = static_cast<OwnHandle<T>*>(ownHandle);
-            handle->clear();
-        }
+    void set(v8::Handle<T> handle)
+    {
+        clear();
+        m_handle = v8::Persistent<T>::New(handle);
+    }
 
-        v8::Persistent<T> m_handle;
-    };
+    // Note: This is clear in the OwnPtr sense, not the v8::Handle sense.
+    void clear()
+    {
+        if (m_handle.IsEmpty())
+            return;
+        m_handle.Dispose();
+        m_handle.Clear();
+    }
+
+private:
+    v8::Persistent<T> m_handle;
+};
 
 } // namespace WebCore
 
