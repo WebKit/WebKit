06ce44e8a63452b86b9bcce622a425e4aee0bf4c
Mark Rowe <mrowe@apple.com>
2012-08-23 20:57:47 +0000

    Roll out part of r126475 that breaks the build with older versions of Clang.
    * wtf/FastMalloc.cpp:
    
    Canonical link: https://commits.webkit.org/112703@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@126481 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/FastMalloc.cpp
+++ Source/WTF/wtf/FastMalloc.cpp
@@ -2469,20 +2469,11 @@ class TCMalloc_Central_FreeList {
   int32_t cache_size_;
 };
 
-#if COMPILER(CLANG)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-private-field"
-#endif
-
 // Pad each CentralCache object to multiple of 64 bytes
 class TCMalloc_Central_FreeListPadded : public TCMalloc_Central_FreeList {
  private:
   char pad_[(64 - (sizeof(TCMalloc_Central_FreeList) % 64)) % 64];
 };
-
-#if COMPILER(CLANG)
-#pragma clang diagnostic pop
-#endif
 
 //-------------------------------------------------------------------
 // Global variables
