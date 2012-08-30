14a6b778cfa152ad63cb6e0545c2610f91815038
Dirk Pranke <dpranke@chromium.org>
2012-08-30 20:22:29 +0000

    decide what (and how) we should set the tolerance for ref test pixel compares and test for that https://bugs.webkit.org/show_bug.cgi?id=94746
    Reviewed by Ojan Vafai.
    
    Add an assertion to diff_image() to check that we are passing
    tolerance=0 explicitly when diffing ref test results.
    
    * Scripts/webkitpy/layout_tests/port/test.py:
    (TestPort.diff_image):
    
    
    Canonical link: https://commits.webkit.org/113351@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@127180 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/port/test.py
+++ Tools/Scripts/webkitpy/layout_tests/port/test.py
@@ -405,6 +405,8 @@ class TestPort(Port):
 
     def diff_image(self, expected_contents, actual_contents, tolerance=None):
         diffed = actual_contents != expected_contents
+        if 'ref' in expected_contents:
+            assert tolerance == 0
         if diffed:
             return ("< %s\n---\n> %s\n" % (expected_contents, actual_contents), 1, None)
         return (None, 0, None)
