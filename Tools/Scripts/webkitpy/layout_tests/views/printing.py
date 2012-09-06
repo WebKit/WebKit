023780dbc3683b0b9bd4c68a5709d7bf2c766854
Dirk Pranke <dpranke@chromium.org>
2012-09-06 04:37:42 +0000

    NRWT lies about the number of tests that it's skipping when used with --repeat-each https://bugs.webkit.org/show_bug.cgi?id=95789
    Reviewed by Ojan Vafai.
    
    Fix miscalculation in log message.
    
    * Scripts/webkitpy/layout_tests/views/printing.py:
    (Printer.print_found):
    
    
    Canonical link: https://commits.webkit.org/113790@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@127688 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/views/printing.py
+++ Tools/Scripts/webkitpy/layout_tests/views/printing.py
@@ -109,10 +109,11 @@ class Printer(object):
         self._print_default('')
 
     def print_found(self, num_all_test_files, num_to_run, repeat_each, iterations):
-        found_str = 'Found %s; running %d' % (grammar.pluralize('test', num_all_test_files), num_to_run)
+        num_unique_tests = num_to_run / (repeat_each * iterations)
+        found_str = 'Found %s; running %d' % (grammar.pluralize('test', num_all_test_files), num_unique_tests)
         if repeat_each * iterations > 1:
             found_str += ' (%d times each: --repeat-each=%d --iterations=%d)' % (repeat_each * iterations, repeat_each, iterations)
-        found_str += ', skipping %d' % (num_all_test_files - num_to_run)
+        found_str += ', skipping %d' % (num_all_test_files - num_unique_tests)
         self._print_default(found_str + '.')
 
     def print_expected(self, result_summary, tests_with_result_type_callback):
