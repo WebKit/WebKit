ef4e7315b6daca34db6824f33b467db74e8ce393
Csaba Osztrogonác <ossy@webkit.org>
2012-09-07 04:14:57 +0000

    Move RenderView::setFixedPositionedObjectsNeedLayout to FrameView https://bugs.webkit.org/show_bug.cgi?id=96017
    Unreviewed Qt-WK2 buildfix after r127783.
    
    * WebProcess/WebPage/CoordinatedGraphics/LayerTreeCoordinator.cpp:
    (WebKit::LayerTreeCoordinator::syncFixedLayers):
    
    Canonical link: https://commits.webkit.org/113917@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@127816 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/LayerTreeCoordinator.cpp
+++ Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/LayerTreeCoordinator.cpp
@@ -367,7 +367,7 @@ void LayerTreeCoordinator::syncFixedLayers()
     if (!m_webPage->corePage()->settings() || !m_webPage->corePage()->settings()->acceleratedCompositingForFixedPositionEnabled())
         return;
 
-    if (!m_webPage->mainFrame()->view()->hasFixedObjects())
+    if (!m_webPage->mainFrame()->view()->hasViewportConstrainedObjects())
         return;
 
     RenderLayer* rootRenderLayer = m_webPage->mainFrame()->contentRenderer()->compositor()->rootRenderLayer();
