159e7a0892159c9068f67b4514586647be7de003
Csaba Osztrogonác <ossy@webkit.org>
2012-09-07 11:45:24 +0000

    Unreviewed. Add Gábor Ballabás as contributor.
    * Scripts/webkitpy/common/config/committers.py:
    
    Canonical link: https://commits.webkit.org/113954@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@127853 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/config/committers.py
+++ Tools/Scripts/webkitpy/common/config/committers.py
@@ -130,6 +130,7 @@ contributors_who_are_not_committers = [
     Contributor("Felician Marton", ["felician@inf.u-szeged.hu", "marton.felician.zoltan@stud.u-szeged.hu"], "Felician"),
     Contributor("Finnur Thorarinsson", ["finnur@chromium.org", "finnur.webkit@gmail.com"], "finnur"),
     Contributor("Forms Bugs", "forms-bugs@chromium.org"),
+    Contributor("Gabor Ballabas", "gaborb@inf.u-szeged.hu", "bgabor"),
     Contributor("Grace Kloba", "klobag@chromium.org", "klobag"),
     Contributor("Greg Simon", "gregsimon@chromium.org", "gregsimon"),
     Contributor("Gregg Tavares", ["gman@google.com", "gman@chromium.org"], "gman"),
