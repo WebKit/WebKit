6bea43f17146eafaf0ccd1b2ae38b830061bcad1
Simon Hausmann <hausmann@webkit.org>
2012-09-10 12:34:52 +0000

    [TexMap] Fix compilation on Windows https://bugs.webkit.org/show_bug.cgi?id=96257
    Patch by Simon Hausmann <simon.hausmann@nokia.com> on 2012-09-10
    Reviewed by Noam Rosenthal.
    
    TextureMapperGL uses near and far as variable names and unfortunately
    WinDefs.h on Windows has the nasty habit of unconditionally(!) defining
    near and far as empty macros. There's no way to prevent WinDefs.h from
    doing so. Instead we rename the variables. (Another option would've been to
    #undef)
    
    * platform/graphics/texmap/TextureMapperGL.cpp:
    
    Canonical link: https://commits.webkit.org/114142@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@128047 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/texmap/TextureMapperGL.cpp
+++ Source/WebCore/platform/graphics/texmap/TextureMapperGL.cpp
@@ -750,13 +750,13 @@ PassRefPtr<BitmapTexture> BitmapTextureGL::applyFilters(const BitmapTexture& con
 
 static inline TransformationMatrix createProjectionMatrix(const IntSize& size, bool mirrored)
 {
-    const float near = 9999999;
-    const float far = -99999;
+    const float nearValue = 9999999;
+    const float farValue = -99999;
 
     return TransformationMatrix(2.0 / float(size.width()), 0, 0, 0,
                                 0, (mirrored ? 2.0 : -2.0) / float(size.height()), 0, 0,
-                                0, 0, -2.f / (far - near), 0,
-                                -1, mirrored ? -1 : 1, -(far + near) / (far - near), 1);
+                                0, 0, -2.f / (farValue - nearValue), 0,
+                                -1, mirrored ? -1 : 1, -(farValue + nearValue) / (farValue - nearValue), 1);
 }
 
 void BitmapTextureGL::initializeStencil()
