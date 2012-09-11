fa391880fe3e3040b960e1e0c4f84cc17028d753
Simon Hausmann <hausmann@webkit.org>
2012-09-11 09:57:38 +0000

    [QT] Fix build with newer qmake from Qt 5
    Rubber-stamped by Tor Arne Vestbø.
    
    Support for "qmake -query QMAKE_MKSPECS" was removed from qmake. Replace
    it with the use of QT_HOST_DATA/mkspecs.
    Based on patch by Oswald Buddenhagen <oswald.buddenhagen@nokia.com>.
    
    * Scripts/webkitdirs.pm:
    (builtDylibPathForName):
    (qtMakeCommand):
    
    Canonical link: https://commits.webkit.org/114255@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@128162 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -735,8 +735,8 @@ sub builtDylibPathForName
                 $libraryName .= "d";
             }
 
-            my $mkspec = `$qmakebin -query QMAKE_MKSPECS`;
-            $mkspec =~ s/[\n|\r]$//g;
+            chomp(my $mkspec = `$qmakebin -query QT_HOST_DATA`);
+            $mkspec .= "/mkspecs";
             my $qtMajorVersion = retrieveQMakespecVar("$mkspec/qconfig.pri", "QT_MAJOR_VERSION");
             if (not $qtMajorVersion) {
                 $qtMajorVersion = "";
@@ -1872,8 +1872,8 @@ sub retrieveQMakespecVar
 sub qtMakeCommand($)
 {
     my ($qmakebin) = @_;
-    chomp(my $mkspec = `$qmakebin -query QMAKE_MKSPECS`);
-    $mkspec .= "/default";
+    chomp(my $mkspec = `$qmakebin -query QT_HOST_DATA`);
+    $mkspec .= "/mkspecs/default";
     my $compiler = retrieveQMakespecVar("$mkspec/qmake.conf", "QMAKE_CC");
 
     #print "default spec: " . $mkspec . "\n";
