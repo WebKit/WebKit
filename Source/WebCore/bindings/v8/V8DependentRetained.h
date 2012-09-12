f178b26dbaf3e1d3f7e14b79404c5e721090a05f
Elliott Sprehn <esprehn@chromium.org>
2012-09-12 00:41:06 +0000

    Add new JSDependentRetained that allows keeping a JSObject alive as long as another is alive https://bugs.webkit.org/show_bug.cgi?id=96034
    Patch by Elliott Sprehn <esprehn@chromium.org> on 2012-09-11
    Reviewed by Geoffrey Garen.
    
    Add new JSDependentRetained (with the same API as V8DependentRetained) that allows keeping
    a JSObject alive as long as another is alive. This is useful for keeping callbacks on
    wrappers without keeping strong references into the JS heap in C++ which can result in
    cycles that create memory leaks.
    
    No new tests needed, this will be used to fix MutationObservers and UndoManager which will have tests.
    
    * GNUmakefile.list.am:
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.xcodeproj/project.pbxproj:
    * bindings/js/JSDependentRetained.h: Added.
    (WebCore):
    (JSDependentRetained):
    (WebCore::JSDependentRetained::JSDependentRetained):
    (WebCore::JSDependentRetained::~JSDependentRetained):
    (WebCore::JSDependentRetained::get): Get the JSObject value.
    (WebCore::JSDependentRetained::isEmpty): Check if the value is still alive.
    (WebCore::JSDependentRetained::retain): Sets the owner of the object, should only be used once.
    (WebCore::JSDependentRetained::release):
    * bindings/v8/V8DependentRetained.h:
    (WebCore::V8DependentRetained::V8DependentRetained):
    (WebCore::V8DependentRetained::retain): Added this method so the API for JSC and V8 are identical.
    
    Canonical link: https://commits.webkit.org/114341@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@128249 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/V8DependentRetained.h
+++ Source/WebCore/bindings/v8/V8DependentRetained.h
@@ -1,29 +1,25 @@
 /*
- * Copyright (C) 2012 Google Inc. All rights reserved.
+ * Copyright (C) 2012 Google Inc. All Rights Reserved.
+ * Copyright (C) 2005, 2006, 2007, 2008, 2009 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *     * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
@@ -44,15 +40,13 @@ namespace WebCore {
 class V8DependentRetained {
 public:
     V8DependentRetained(v8::Handle<v8::Object> owner, v8::Handle<v8::Object> value)
-        : m_owner(owner)
-        , m_value(value)
+        : m_value(value)
         , m_propertyName(createPropertyName())
     {
-        ASSERT(!m_owner.IsEmpty());
-        ASSERT(!m_value.IsEmpty());
-        owner->SetHiddenValue(m_propertyName.get(), value);
-        m_owner.get().MakeWeak(this, &V8DependentRetained::ownerWeakCallback);
+        ASSERT(!m_value.isEmpty());
         m_value.get().MakeWeak(this, &V8DependentRetained::valueWeakCallback);
+        if (!owner.IsEmpty())
+            retain(owner);
     }
 
     ~V8DependentRetained()
@@ -70,9 +64,14 @@ public:
         return m_owner.isEmpty() || m_value.isEmpty();
     }
 
-    // FIXME: We might add an explicit retain(v8::Handle<v8::Object>) method to allow
-    // creating one of these and choosing the owner later. Such behavior is required
-    // in JSC, but not in v8 right now.
+    void retain(v8::Handle<v8::Object> owner)
+    {
+        ASSERT(m_owner.isEmpty() && !owner.IsEmpty());
+        ASSERT(!m_value.isEmpty());
+        owner->SetHiddenValue(m_propertyName.get(), get());
+        m_owner = owner;
+        m_owner.get().MakeWeak(this, &V8DependentRetained::ownerWeakCallback);
+    }
 
 private:
     static v8::Handle<v8::String> createPropertyName()
