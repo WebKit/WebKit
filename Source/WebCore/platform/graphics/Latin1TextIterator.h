40baaa2bb9fe11e12a54385f374aa5eb92f07f55
Anders Carlsson <andersca@apple.com>
2012-09-13 22:13:07 +0000

    Fix build.
    * platform/graphics/Latin1TextIterator.h:
    (WebCore::Latin1TextIterator::Latin1TextIterator):
    (Latin1TextIterator):
    m_endCharacter is an unused private member variable, so just remove it to appease newer versions of clang.
    
    Canonical link: https://commits.webkit.org/114598@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@128512 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/Latin1TextIterator.h
+++ Source/WebCore/platform/graphics/Latin1TextIterator.h
@@ -30,11 +30,10 @@ class Latin1TextIterator {
 public:
     // The passed in LChar pointer starts at 'currentCharacter'. The iterator operates on the range [currentCharacter, lastCharacter].
     // 'endCharacter' denotes the maximum length of the UChar array, which might exceed 'lastCharacter'.
-    Latin1TextIterator(const LChar* characters, int currentCharacter, int lastCharacter, int endCharacter)
+    Latin1TextIterator(const LChar* characters, int currentCharacter, int lastCharacter, int /*endCharacter*/)
         : m_characters(characters)
         , m_currentCharacter(currentCharacter)
         , m_lastCharacter(lastCharacter)
-        , m_endCharacter(endCharacter)
     {
     }
 
@@ -61,7 +60,6 @@ private:
     const LChar* m_characters;
     int m_currentCharacter;
     int m_lastCharacter;
-    int m_endCharacter;
 };
 
 }
