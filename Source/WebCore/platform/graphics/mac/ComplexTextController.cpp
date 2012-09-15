1d2124c4cd134458ebad72de5bf239ce8b778240
Dan Bernstein <mitz@apple.com>
2012-09-15 16:27:08 +0000

    REGRESSION (r125578): The Mac complex text code path disagrees with the fast path on word spacing https://bugs.webkit.org/show_bug.cgi?id=96857
    Reviewed by Anders Carlsson.
    
    Source/WebCore:
    
    Fixes the following tests when run with run-webkit-tests --complex-text:
    css2.1/20110323/c541-word-sp-001.htm
    css2.1/20110323/word-spacing-remove-space-002.htm
    css2.1/20110323/word-spacing-remove-space-003.htm
    css2.1/20110323/word-spacing-remove-space-004.htm
    css2.1/20110323/word-spacing-remove-space-005.htm
    css2.1/20110323/word-spacing-remove-space-006.htm
    
    Fixes fast/css/word-spacing-characters-complex-text.html
    
    * platform/graphics/mac/ComplexTextController.cpp:
    (WebCore::ComplexTextController::adjustGlyphsAndAdvances): Made the conditions for adding
    word spacing match the ones in WidthIterator again.
    
    LayoutTests:
    
    * fast/text/atsui-negative-spacing-features.html: Removed some newlines from this test,
    because now that the complex text code path matches the fast path, it too suffers from a bug
    where word spacing is not added at a newline.
    * fast/text/atsui-spacing-features.html: Ditto.
    * platform/mac/TestExpectations: Removed fast/css/word-spacing-characters-complex-text.html.
    
    
    
    Canonical link: https://commits.webkit.org/114777@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@128691 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/mac/ComplexTextController.cpp
+++ Source/WebCore/platform/graphics/mac/ComplexTextController.cpp
@@ -635,7 +635,7 @@ void ComplexTextController::adjustGlyphsAndAdvances()
                         m_afterExpansion = false;
 
                     // Account for word-spacing.
-                    if (treatAsSpace && characterIndex > 0 && !Font::treatAsSpace(m_run[characterIndex - 1]) && m_font.wordSpacing())
+                    if ((ch == noBreakSpace || ch == ' ') && (characterIndex > 0 || r > 0) && m_font.wordSpacing())
                         advance.width += m_font.wordSpacing();
                 } else
                     m_afterExpansion = false;
