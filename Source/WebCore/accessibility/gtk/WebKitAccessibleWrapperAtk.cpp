b201cc17883e332b12fa57011fb56d524b3e9021
Dominic Mazzoni <dmazzoni@google.com>
2012-09-17 11:03:44 +0000

    Merge r127084 - AX: Canvas should have a distinct role https://bugs.webkit.org/show_bug.cgi?id=95248
    Reviewed by Chris Fleizach.
    
    Source/WebCore:
    
    Add new role for a canvas element, and a method to determine if
    a canvas has fallback content, so each platform can decide on the
    appropriate role mapping to use.
    
    Test: accessibility/canvas-description-and-role.html
    
    * accessibility/AccessibilityNodeObject.cpp:
    (WebCore::AccessibilityNodeObject::canvasHasFallbackContent):
    (WebCore):
    * accessibility/AccessibilityNodeObject.h:
    (AccessibilityNodeObject):
    * accessibility/AccessibilityObject.h:
    (WebCore::AccessibilityObject::isCanvas):
    (WebCore::AccessibilityObject::canvasHasFallbackContent):
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::accessibilityIsIgnored):
    (WebCore::AccessibilityRenderObject::determineAccessibilityRole):
    (WebCore::AccessibilityRenderObject::canHaveChildren):
    * accessibility/gtk/WebKitAccessibleWrapperAtk.cpp:
    (atkRole):
    * accessibility/mac/WebAccessibilityObjectWrapper.mm:
    (createAccessibilityRoleMap):
    (-[WebAccessibilityObjectWrapper role]):
    
    Source/WebKit/chromium:
    
    Add support for canvas accessibility role.
    
    * public/WebAccessibilityRole.h:
    * src/AssertMatchingEnums.cpp:
    
    Source/WebKit/win:
    
    Map new CanvasRole to the same as ImageRole.
    
    * AccessibleBase.cpp:
    (MSAARole):
    
    Tools:
    
    Add support for canvas accessibility role.
    
    * DumpRenderTree/chromium/TestRunner/AccessibilityUIElementChromium.cpp:
    (roleToString):
    
    LayoutTests:
    
    Add new tests for canvas role.
    
    * accessibility/canvas.html: Deleted.
    * accessibility/canvas-expected.txt: Deleted.
    * accessibility/canvas-description-and-role.html: Added.
    * platform/chromium/accessibility/canvas-description-and-role-expected.txt: Added.
    * platform/gtk/TestExpectations:
    * platform/mac/accessibility/canvas.html: Added.
    * platform/mac/accessibility/canvas-expected.txt: Added.
    * platform/mac/accessibility/canvas-description-and-role-expected.txt: Added.

--- Source/WebCore/accessibility/gtk/WebKitAccessibleWrapperAtk.cpp
+++ Source/WebCore/accessibility/gtk/WebKitAccessibleWrapperAtk.cpp
@@ -501,6 +501,7 @@ static AtkRole atkRole(AccessibilityRole role)
     case WebCoreLinkRole:
     case ImageMapLinkRole:
         return ATK_ROLE_LINK;
+    case CanvasRole:
     case ImageMapRole:
     case ImageRole:
         return ATK_ROLE_IMAGE;
