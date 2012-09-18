03c05087e129956c035a39fc4b0f7c62784c658c
Dirk Pranke <dpranke@chromium.org>
2012-09-18 01:20:23 +0000

    [chromium] ASAN bot is crashing at the end of the run https://bugs.webkit.org/show_bug.cgi?id=96967
    Reviewed by Abhishek Arya.
    
    The ASAN bot is crashing attempting to decode some output into
    UTF-8; there's no reason to do this, so let's not do this and
    see if something else is going on as well.
    
    * Scripts/webkitpy/layout_tests/port/chromium.py:
    (ChromiumPort._get_crash_log):
    
    Canonical link: https://commits.webkit.org/114900@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@128834 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/port/chromium.py
+++ Tools/Scripts/webkitpy/layout_tests/port/chromium.py
@@ -354,8 +354,8 @@ class ChromiumPort(Port):
         if stderr and 'AddressSanitizer' in stderr:
             asan_filter_path = self.path_from_chromium_base('tools', 'valgrind', 'asan', 'asan_symbolize.py')
             if self._filesystem.exists(asan_filter_path):
-                output = self._executive.run_command([asan_filter_path], input=stderr)
-                stderr = self._executive.run_command(['c++filt'], input=output)
+                output = self._executive.run_command([asan_filter_path], input=stderr, decode_output=False)
+                stderr = self._executive.run_command(['c++filt'], input=output, decode_output=False)
 
         return super(ChromiumPort, self)._get_crash_log(name, pid, stdout, stderr, newer_than)
 
