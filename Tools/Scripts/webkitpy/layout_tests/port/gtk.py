73654b885b6fafbf70d838152b8c6d4cdd603736
Sergio Villar Senin <sergio@webkit.org>
2012-09-18 11:33:03 +0000

    [GTK] run-webkit-tests unable to find TestExpectations for WK2 https://bugs.webkit.org/show_bug.cgi?id=96998
    Reviewed by Philippe Normand.
    
    We should look for TestExpectations files in all the locations where
    we currently look for Skipped files. This will allow
    run-webkit-tests to look for TestExpectations files in
    platform/gtk-wk2 and platform/wk2 if the "-2" flag is used.
    
    * Scripts/webkitpy/layout_tests/port/gtk.py:
    (GtkPort.expectations_files):
    
    Canonical link: https://commits.webkit.org/114940@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@128874 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/port/gtk.py
+++ Tools/Scripts/webkitpy/layout_tests/port/gtk.py
@@ -38,6 +38,9 @@ from webkitpy.layout_tests.port.xvfbdriver import XvfbDriver
 class GtkPort(Port, PulseAudioSanitizer):
     port_name = "gtk"
 
+    def expectations_files(self):
+        return [self._filesystem.join(self._webkit_baseline_path(d), 'TestExpectations') for d in self._skipped_file_search_paths()]
+
     def _port_flag_for_scripts(self):
         return "--gtk"
 
