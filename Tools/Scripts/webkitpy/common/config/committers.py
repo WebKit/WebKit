7ebfe71a4a16f63caa993264ec6e5ed29d47b4b2
Andras Becsi <abecsi@webkit.org>
2012-09-18 14:03:48 +0000

    Update my e-mail address.
    Unreviewed.
    
    * Scripts/webkitpy/common/config/committers.py:
    
    Canonical link: https://commits.webkit.org/114954@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@128888 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/config/committers.py
+++ Tools/Scripts/webkitpy/common/config/committers.py
@@ -198,7 +198,7 @@ committers_unable_to_review = [
     Committer("Andrew Wellington", ["andrew@webkit.org", "proton@wiretapped.net"], "proton"),
     Committer("Andrew Scherkus", "scherkus@chromium.org", "scherkus"),
     Committer("Andrey Kosyakov", "caseq@chromium.org", "caseq"),
-    Committer("Andras Becsi", ["abecsi@webkit.org", "andras.becsi@nokia.com"], "bbandix"),
+    Committer("Andras Becsi", ["abecsi@webkit.org", "andras.becsi@digia.com"], "bbandix"),
     Committer("Andy Wingo", "wingo@igalia.com", "wingo"),
     Committer("Anna Cavender", "annacc@chromium.org", "annacc"),
     Committer("Anthony Ricaud", "rik@webkit.org", "rik"),
