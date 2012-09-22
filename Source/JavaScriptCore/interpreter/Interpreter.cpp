45ea944a565e4d30855352c4661cde1b559db7af
Geoffrey Garen <ggaren@apple.com>
2012-09-22 06:43:04 +0000

    Unreviewed, rolled out a line I committed by accident.
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::execute):
    
    
    
    Canonical link: https://commits.webkit.org/115340@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@129298 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/interpreter/Interpreter.cpp
+++ Source/JavaScriptCore/interpreter/Interpreter.cpp
@@ -834,7 +834,6 @@ JSValue Interpreter::execute(ProgramExecutable* program, CallFrame* callFrame, J
 {
     JSScope* scope = callFrame->scope();
     ASSERT(isValidThisObject(thisObj, callFrame));
-    scope->globalData()->exception = JSValue();
     ASSERT(!scope->globalData()->exception);
     ASSERT(!callFrame->globalData().isCollectorBusy());
     if (callFrame->globalData().isCollectorBusy())
