65a39a879fca96c7a67144c36874de8cbd4f3c18
Paweł Forysiuk <tuxator@o2.pl>
2012-09-25 19:11:28 +0000

    [GTK] Webkit 1.8.2 fails to build with MinGW with spellchecking enabled https://bugs.webkit.org/show_bug.cgi?id=93255
    Patch by Paweł Forysiuk <tuxator@o2.pl> on 2012-09-25
    Reviewed by Martin Robinson.
    
    Build dies because of invalid cast. Additionaly word "interface" is in use with MinGW compiler.
    
    * WebCoreSupport/TextCheckerClientGtk.cpp:
    (WebKit::TextCheckerClientGtk::checkSpellingOfString): Use String to avoid unneded casting
    * webkit/webkitspellcheckerenchant.cpp:
    (webkit_spell_checker_enchant_spell_checker_interface_init): rename interface -> checkerInterface
    
    Canonical link: https://commits.webkit.org/115555@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@129537 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/gtk/WebCoreSupport/TextCheckerClientGtk.cpp
+++ Source/WebKit/gtk/WebCoreSupport/TextCheckerClientGtk.cpp
@@ -61,8 +61,8 @@ void TextCheckerClientGtk::learnWord(const String& text)
 
 void TextCheckerClientGtk::checkSpellingOfString(const UChar* text, int length, int* misspellingLocation, int* misspellingLength)
 {
-    GOwnPtr<gchar> utf8Text(g_utf16_to_utf8(const_cast<gunichar2*>(text), length, 0, 0, 0));
-    webkit_spell_checker_check_spelling_of_string(m_spellChecker.get(), utf8Text.get(), misspellingLocation, misspellingLength);
+    String textAsString(text, length);
+    webkit_spell_checker_check_spelling_of_string(m_spellChecker.get(), textAsString.utf8().data(), misspellingLocation, misspellingLength);
 }
 
 String TextCheckerClientGtk::getAutoCorrectSuggestionForMisspelledWord(const String& inputWord)
