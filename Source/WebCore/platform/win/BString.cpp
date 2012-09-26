0a3a198ff4e7215d7bfec5dd5951c0e8bb073c89
Patrick Gansterer <paroga@webkit.org>
2012-09-26 06:55:27 +0000

    [WIN] Fix ASSERT in BString after r128988.
    * platform/win/BString.cpp:
    (WebCore::BString::~BString):
    (WebCore::BString::adoptBSTR):
    (WebCore::BString::clear): Set pointer to 0 after freeing.
    
    Canonical link: https://commits.webkit.org/115614@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@129598 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/win/BString.cpp
+++ Source/WebCore/platform/win/BString.cpp
@@ -106,7 +106,7 @@ BString::BString(CFStringRef cfstr)
 
 BString::~BString()
 {
-    clear();
+    SysFreeString(m_bstr);
 }
 
 BString::BString(const BString& other)
@@ -119,13 +119,14 @@ BString::BString(const BString& other)
 
 void BString::adoptBSTR(BSTR bstr)
 {
-    clear();
+    SysFreeString(m_bstr);
     m_bstr = bstr;
 }
 
 void BString::clear()
 {
     SysFreeString(m_bstr);
+    m_bstr = 0;
 }
 
 BString& BString::operator=(const BString& other)
