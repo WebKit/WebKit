5e3e6bcb292e2e8f8d8374d38adc1ec269c94218
Mike Fenton <mifenton@rim.com>
2012-09-26 16:33:31 +0000

    [BlackBerry] Update padding in ensureFocusTextElementVisible to be DPI independent. https://bugs.webkit.org/show_bug.cgi?id=97684
    Reviewed by Rob Buis.
    
    Update padding in ensureFocusTextElementVisible to be DPI
    independent and relative to the current zoom level.
    
    Reviewed Internally by Gen Mak.
    
    * WebKitSupport/InputHandler.cpp:
    (BlackBerry::WebKit::InputHandler::ensureFocusTextElementVisible):
    
    
    Canonical link: https://commits.webkit.org/115674@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@129658 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/blackberry/WebKitSupport/InputHandler.cpp
+++ Source/WebKit/blackberry/WebKitSupport/InputHandler.cpp
@@ -1125,16 +1125,14 @@ void InputHandler::ensureFocusTextElementVisible(CaretScrollType scrollType)
             }
 
             // Pad the rect to improve the visual appearance.
-            // Padding must be large enough to expose the selection / FCC should they exist. Dragging the handle offscreen and releasing
-            // will not trigger an automatic scroll. Using a padding of 40 will fully exposing the width of the current handle and half of
-            // the height making it usable.
-            // FIXME: This will need to be updated when the graphics change.
-            // FIXME: The value of 40 should be calculated as a unit of measure using Graphics::Screen::primaryScreen()->heightInMMToPixels
-            // using a relative value to the size of the handle. We should also consider expanding different amounts horizontally vs vertically.
-            selectionFocusRect.inflate(40 /* padding in pixels */);
-            WebCore::IntRect revealRect = layer->getRectToExpose(actualScreenRect, selectionFocusRect,
+            // Convert the padding back from transformed to ensure a consistent padding regardless of
+            // zoom level as controls do not zoom.
+            static const int s_focusRectPaddingSize = Graphics::Screen::primaryScreen()->heightInMMToPixels(3);
+            selectionFocusRect.inflate(m_webPage->mapFromTransformed(WebCore::IntSize(0, s_focusRectPaddingSize)).height());
+
+            WebCore::IntRect revealRect(layer->getRectToExpose(actualScreenRect, selectionFocusRect,
                                                                  horizontalScrollAlignment,
-                                                                 verticalScrollAlignment);
+                                                                 verticalScrollAlignment));
 
             mainFrameView->setConstrainsScrollingToContentEdge(false);
             // In order to adjust the scroll position to ensure the focused input field is visible,
