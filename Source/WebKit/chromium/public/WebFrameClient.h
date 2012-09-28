bcf573b91184d70e5ff00f0cf01d21c26434f9eb
Mark Pilgrim <pilgrim@chromium.org>
2012-09-28 04:04:44 +0000

    [Chromium] Update comment in WebFrameClient to point to new userAgent() function https://bugs.webkit.org/show_bug.cgi?id=97854
    Reviewed by Adam Barth.
    
    Part of a refactoring series. See tracking bug 82948.
    
    * public/WebFrameClient.h:
    (WebFrameClient):
    
    
    Canonical link: https://commits.webkit.org/115854@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@129846 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/chromium/public/WebFrameClient.h
+++ Source/WebKit/chromium/public/WebFrameClient.h
@@ -411,7 +411,7 @@ public:
 
     // Asks the embedder if a specific user agent should be used for the given
     // URL. Non-empty strings indicate an override should be used. Otherwise,
-    // WebKitPlatformSupport::userAgent() will be called to provide one.
+    // Platform::current()->userAgent() will be called to provide one.
     virtual WebString userAgentOverride(WebFrame*, const WebURL& url) { return WebString(); }
 
 protected:
