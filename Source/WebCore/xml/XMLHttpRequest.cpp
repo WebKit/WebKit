4988f8f05d3baa3669d7e5d6a4d9606c245c7840
Chris Dumez <christophe.dumez@intel.com>
2012-10-02 12:46:38 +0000

    [XMLHttpRequest] overrideMimeType(mime) does not update the response's "Content-Type" header https://bugs.webkit.org/show_bug.cgi?id=98137
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-10-02
    Reviewed by Kentaro Hara.
    
    Source/WebCore:
    
    According to the XMLHttpRequest specification, overrideMimeType(mime) sets the
    "Content-Type" header for the response to mime. However, with the current
    implementation, calling overrideMimeType(mime) does not affect the value
    returned by client.getResponseHeader("Content-Type"). This patch makes sure
    the response's "Content-Type" header is properly updated with the override
    MIME type.
    
    Test: http/tests/xmlhttprequest/xmlhttprequest-overridemimetype-content-type-header.html
    
    * xml/XMLHttpRequest.cpp:
    (WebCore::XMLHttpRequest::didReceiveResponse):
    
    LayoutTests:
    
    Add layout test to check that calling overrideMimeType(mime) properly
    sets the response's "Content-Type" header to mime.
    
    * http/tests/xmlhttprequest/xmlhttprequest-overridemimetype-content-type-header-expected.txt: Added.
    * http/tests/xmlhttprequest/xmlhttprequest-overridemimetype-content-type-header.html: Added.
    
    Canonical link: https://commits.webkit.org/116157@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@130158 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/xml/XMLHttpRequest.cpp
+++ Source/WebCore/xml/XMLHttpRequest.cpp
@@ -1116,7 +1116,11 @@ void XMLHttpRequest::didReceiveResponse(unsigned long identifier, const Resource
     InspectorInstrumentation::didReceiveXHRResponse(scriptExecutionContext(), identifier);
 
     m_response = response;
-    m_responseEncoding = extractCharsetFromMediaType(m_mimeTypeOverride);
+    if (!m_mimeTypeOverride.isEmpty()) {
+        m_response.setHTTPHeaderField("Content-Type", m_mimeTypeOverride);
+        m_responseEncoding = extractCharsetFromMediaType(m_mimeTypeOverride);
+    }
+
     if (m_responseEncoding.isEmpty())
         m_responseEncoding = response.textEncodingName();
 }
