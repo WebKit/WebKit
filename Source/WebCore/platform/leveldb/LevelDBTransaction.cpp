057fa8e0c7fa23f1f8dac77f3ad8c850526aeb1d
Joshua Bell <jsbell@chromium.org>
2012-10-03 23:13:56 +0000

    IndexedDB: Don't do full commit for empty transactions https://bugs.webkit.org/show_bug.cgi?id=89239
    Reviewed by Tony Chang.
    
    Don't bother creating a leveldb write batch if there's nothing in the transaction
    to commit. Note that a read-only transaction may still have index cleanup so may
    not be an empty transaction.
    
    This cuts the Lookup2 benchmark in http://reyesr.github.com/html5-storage-benchmark/
    from 70s to 2s.
    
    Covered by existing tests, e.g. storage/indexeddb/transaction-basics.html
    
    * platform/leveldb/LevelDBTransaction.cpp:
    (WebCore::LevelDBTransaction::commit):
    
    
    Canonical link: https://commits.webkit.org/116335@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@130338 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/leveldb/LevelDBTransaction.cpp
+++ Source/WebCore/platform/leveldb/LevelDBTransaction.cpp
@@ -125,6 +125,12 @@ bool LevelDBTransaction::get(const LevelDBSlice& key, Vector<char>& value)
 bool LevelDBTransaction::commit()
 {
     ASSERT(!m_finished);
+
+    if (m_tree.is_empty()) {
+        m_finished = true;
+        return true;
+    }
+
     OwnPtr<LevelDBWriteBatch> writeBatch = LevelDBWriteBatch::create();
 
     TreeType::Iterator iterator;
