105a53ce06214055ed5b0177383cd1adb942d2b3
Glenn Hartmann <hartmanng@chromium.org>
2012-10-03 22:33:00 +0000

    Adding total pixels painted and rasterized stats. https://bugs.webkit.org/show_bug.cgi?id=98269
    Patch by Glenn Hartmann <hartmanng@chromium.org> on 2012-10-03
    Reviewed by James Robinson.
    
    * chromium/public/WebRenderingStats.h:
    (WebRenderingStats):
    (WebKit::WebRenderingStats::WebRenderingStats):
    (WebKit::WebRenderingStats::enumerateFields):
    
    Canonical link: https://commits.webkit.org/116329@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@130332 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/Platform/chromium/public/WebRenderingStats.h
+++ Source/Platform/chromium/public/WebRenderingStats.h
@@ -36,6 +36,8 @@ struct WebRenderingStats {
     double totalRasterizeTimeInSeconds;
     double totalCommitTimeInSeconds;
     size_t totalCommitCount;
+    size_t totalPixelsPainted;
+    size_t totalPixelsRasterized;
     size_t numImplThreadScrolls;
     size_t numMainThreadScrolls;
 
@@ -47,6 +49,8 @@ struct WebRenderingStats {
         , totalRasterizeTimeInSeconds(0)
         , totalCommitTimeInSeconds(0)
         , totalCommitCount(0)
+        , totalPixelsPainted(0)
+        , totalPixelsRasterized(0)
         , numImplThreadScrolls(0)
         , numMainThreadScrolls(0)
     {
@@ -74,6 +78,8 @@ struct WebRenderingStats {
         enumerator->addDouble("totalRasterizeTimeInSeconds", totalRasterizeTimeInSeconds);
         enumerator->addDouble("totalCommitTimeInSeconds", totalCommitTimeInSeconds);
         enumerator->addInt("totalCommitCount", totalCommitCount);
+        enumerator->addInt("totalPixelsPainted", totalPixelsPainted);
+        enumerator->addInt("totalPixelsRasterized", totalPixelsRasterized);
         enumerator->addInt("numImplThreadScrolls", numImplThreadScrolls);
         enumerator->addInt("numMainThreadScrolls", numMainThreadScrolls);
     }
