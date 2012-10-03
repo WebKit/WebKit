d81152e39a8c74333dab16c51ea1347d9dbd509a
Anders Carlsson <andersca@apple.com>
2012-10-03 21:00:29 +0000

    Use the XPC service by default on newer systems https://bugs.webkit.org/show_bug.cgi?id=98297 <rdar://problem/12424641>
    Reviewed by Sam Weinig.
    
    * UIProcess/mac/WebProcessProxyMac.mm:
    (WebKit::shouldUseXPC):
    Return false for older systems.
    
    (WebKit::WebProcessProxy::platformConnect):
    Call shouldUseXPC().
    
    Canonical link: https://commits.webkit.org/116315@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@130318 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/mac/WebProcessProxyMac.mm
+++ Source/WebKit2/UIProcess/mac/WebProcessProxyMac.mm
@@ -149,6 +149,20 @@ bool WebProcessProxy::fullKeyboardAccessEnabled()
     return [WKFullKeyboardAccessWatcher fullKeyboardAccessEnabled];
 }
 
+#if HAVE(XPC)
+static bool shouldUseXPC()
+{
+    if (id value = [[NSUserDefaults standardUserDefaults] objectForKey:@"WebKit2UseXPCServiceForWebProcess"])
+        return [value boolValue];
+
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
+    return true;
+#else
+    return false;
+#endif
+}
+#endif
+
 void WebProcessProxy::platformConnect(ProcessLauncher::LaunchOptions& launchOptions)
 {
     // We want the web process to match the architecture of the UI process.
@@ -156,7 +170,7 @@ void WebProcessProxy::platformConnect(ProcessLauncher::LaunchOptions& launchOpti
     launchOptions.executableHeap = false;
 
 #if HAVE(XPC)
-    launchOptions.useXPC = [[NSUserDefaults standardUserDefaults] boolForKey:@"WebKit2UseXPCServiceForWebProcess"];
+    launchOptions.useXPC = shouldUseXPC();
 #endif
 }
 
