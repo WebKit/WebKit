4fe8d68ddac4411e025237a13e4d976a5ea61984
Benjamin Poulain <bpoulain@apple.com>
2012-10-04 19:34:34 +0000

    Use copyLCharsFromUCharSource() for IdentifierLCharFromUCharTranslator translation https://bugs.webkit.org/show_bug.cgi?id=98335
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2012-10-04
    Reviewed by Michael Saboff.
    
    Michael Saboff added an optimized version of UChar->LChar conversion in r125846.
    Use this function in JSC::Identifier.
    
    * runtime/Identifier.cpp:
    (JSC::IdentifierLCharFromUCharTranslator::translate):
    
    
    Canonical link: https://commits.webkit.org/116415@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@130418 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/Identifier.cpp
+++ Source/JavaScriptCore/runtime/Identifier.cpp
@@ -30,6 +30,7 @@
 #include <wtf/Assertions.h>
 #include <wtf/FastMalloc.h>
 #include <wtf/HashSet.h>
+#include <wtf/text/ASCIIFastPath.h>
 #include <wtf/text/StringHash.h>
 
 using WTF::ThreadSpecific;
@@ -80,11 +81,7 @@ struct IdentifierLCharFromUCharTranslator {
     {
         LChar* d;
         StringImpl* r = StringImpl::createUninitialized(buf.length, d).leakRef();
-        for (unsigned i = 0; i != buf.length; i++) {
-            UChar c = buf.s[i];
-            ASSERT(c <= 0xff);
-            d[i] = c;
-        }
+        WTF::copyLCharsFromUCharSource(d, buf.s, buf.length);
         r->setHash(hash);
         location = r; 
     }
