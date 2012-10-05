baf83309d4e0f2c7566131a4110e9c0384afa5af
Rob Buis <rwlbuis@webkit.org>
2012-10-05 14:33:11 +0000

    [BlackBerry] Sync up CMake files https://bugs.webkit.org/show_bug.cgi?id=98442
    Reviewed by Gyuyoung Kim.
    
    .:
    
    Amongst others some internal paths changed.
    
    * Source/cmake/OptionsBlackBerry.cmake:
    
    Source/WebKit:
    
    Amongst others use InspectorOverlayBlackBerry.
    
    * PlatformBlackBerry.cmake:
    
    Canonical link: https://commits.webkit.org/116495@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@130503 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/PlatformBlackBerry.cmake
+++ Source/WebKit/PlatformBlackBerry.cmake
@@ -4,19 +4,20 @@ LIST(INSERT WebKit_INCLUDE_DIRECTORIES 0
 )
 
 LIST(APPEND WebKit_INCLUDE_DIRECTORIES
-    "${JAVASCRIPTCORE_DIR}/wtf/text"
+    "${JAVASCRIPTCORE_DIR}/dfg"
     "${WEBCORE_DIR}/bindings/cpp"
+    "${WEBCORE_DIR}/fileapi"
     "${WEBCORE_DIR}/history/blackberry"
-    "${WEBCORE_DIR}/html/canvas"
     "${WEBCORE_DIR}/html/parser" # For HTMLParserIdioms.h
     "${WEBCORE_DIR}/loader/appcache"
     "${WEBCORE_DIR}/platform/blackberry"
+    "${WEBCORE_DIR}/platform/graphics/gpu"
     "${WEBCORE_DIR}/platform/graphics/blackberry"
-    "${WEBCORE_DIR}/platform/graphics/blackberry/skia"
-    "${WEBCORE_DIR}/platform/graphics/skia"
     "${WEBCORE_DIR}/platform/network/blackberry"
+    "${WEBCORE_DIR}/testing/js"
+    "${WEBCORE_DIR}/Modules/filesystem"
     "${WEBCORE_DIR}/Modules/geolocation"
-    "${WEBCORE_DIR}/Modules/notifications"
+    "${WEBCORE_DIR}/Modules/indexeddb"
     "${WEBCORE_DIR}/Modules/networkinfo"
     "${WEBCORE_DIR}/Modules/vibration"
     "${WEBCORE_DIR}/Modules/websockets"
@@ -24,11 +25,29 @@ LIST(APPEND WebKit_INCLUDE_DIRECTORIES
     "${WEBKIT_DIR}/blackberry/WebCoreSupport"
     "${WEBKIT_DIR}/blackberry/WebKitSupport"
     "${CMAKE_SOURCE_DIR}/Source" # For JavaScriptCore API headers
+    "${CMAKE_SOURCE_DIR}"
 )
+
+IF (ENABLE_NOTIFICATIONS)
+    LIST(APPEND WebKit_INCLUDE_DIRECTORIES
+        "${WEBCORE_DIR}/Modules/notifications"
+    )
+ENDIF ()
+IF (WTF_USE_SKIA)
+    LIST(APPEND WebKit_INCLUDE_DIRECTORIES
+        "${WEBCORE_DIR}/platform/graphics/chromium"
+        "${WEBCORE_DIR}/platform/graphics/blackberry/skia"
+        "${WEBCORE_DIR}/platform/graphics/skia"
+    )
+ELSE ()
+    LIST(APPEND WebKit_INCLUDE_DIRECTORIES
+        "${WEBCORE_DIR}/platform/image-encoders"
+    )
+ENDIF ()
+
 IF (NOT PUBLIC_BUILD)
     LIST(APPEND WebKit_INCLUDE_DIRECTORIES
         # needed for DRT for now
-        "${JAVASCRIPTCORE_DIR}/wtf"
         "${WEBCORE_DIR}/platform/mock"
         "${WEBCORE_DIR}/svg/animation"
         "${WEBCORE_DIR}/workers"
@@ -41,6 +60,12 @@ ENDIF ()
 IF (ENABLE_BATTERY_STATUS)
     LIST(APPEND WebKit_INCLUDE_DIRECTORIES ${WEBCORE_DIR}/Modules/battery)
     LIST(APPEND WebKit_SOURCES blackberry/WebCoreSupport/BatteryClientBlackBerry.cpp)
+ENDIF ()
+
+IF (ENABLE_REGISTER_PROTOCOL_HANDLER OR ENABLE_CUSTOM_SCHEME_HANDLER)
+  LIST(APPEND WebKit_INCLUDE_DIRECTORIES
+    "${WEBCORE_DIR}/Modules/protocolhandler"
+  )
 ENDIF ()
 
 IF (ENABLE_MEDIA_STREAM)
@@ -91,9 +116,10 @@ LIST(APPEND WebKit_SOURCES
     blackberry/WebCoreSupport/JavaScriptDebuggerBlackBerry.cpp
     blackberry/WebCoreSupport/NetworkInfoClientBlackBerry.cpp
     blackberry/WebCoreSupport/NotificationPresenterImpl.cpp
-    blackberry/WebCoreSupport/VibrationClientBlackBerry.cpp
     blackberry/WebCoreSupport/PagePopupBlackBerry.cpp
+    blackberry/WebCoreSupport/RegisterProtocolHandlerClientBlackBerry.cpp
     blackberry/WebCoreSupport/SelectPopupClient.cpp
+    blackberry/WebCoreSupport/VibrationClientBlackBerry.cpp
     blackberry/WebCoreSupport/DatePickerClient.cpp
     blackberry/WebCoreSupport/ColorPickerClient.cpp
     blackberry/WebKitSupport/AboutData.cpp
@@ -105,7 +131,7 @@ LIST(APPEND WebKit_SOURCES
     blackberry/WebKitSupport/InPageSearchManager.cpp
     blackberry/WebKitSupport/InputHandler.cpp
     blackberry/WebKitSupport/InRegionScrollableArea.cpp
-    blackberry/WebKitSupport/InspectorOverlay.cpp
+    blackberry/WebKitSupport/InspectorOverlayBlackBerry.cpp
     blackberry/WebKitSupport/RenderQueue.cpp
     blackberry/WebKitSupport/SelectionHandler.cpp
     blackberry/WebKitSupport/SelectionOverlay.cpp
@@ -147,6 +173,7 @@ IF (NOT PUBLIC_BUILD)
         ${TOOLS_DIR}/DumpRenderTree/PixelDumpSupport.cpp
         ${TOOLS_DIR}/DumpRenderTree/WorkQueue.cpp
         ${TOOLS_DIR}/DumpRenderTree/GCController.cpp
+        ${WTF_DIR}/wtf/MD5.cpp
     )
 ENDIF ()
 
@@ -155,27 +182,37 @@ SET(WebKit_LINK_FLAGS ${BLACKBERRY_LINK_FLAGS})
 LIST(APPEND WebKit_LIBRARIES
     ${CURL_LIBRARY}
     ${FONTCONFIG_LIBRARY}
-    ${FREETYPE_LIBRARY}
-    ${HARFBUZZ_LIBRARY}
     ${ICUData_LIBRARY}
     ${ICUI18N_LIBRARY}
     ${ICUUC_LIBRARY}
     ${INTL_LIBRARY}
     ${JPEG_LIBRARY}
     ${JavaScriptCore_LIBRARY_NAME}
+    ${LEVELDB_LIBRARY}
     ${MMR_LIBRARY}
     ${M_LIBRARY}
     ${OTS_LIBRARY}
     ${PNG_LIBRARY}
     ${SQLITE3_LIBRARY}
-    ${Skia_LIBRARY}
-    ${Skia_QNX_LIBRARY}
-    ${UUID_LIBRARY}
     ${WebKitPlatform_LIBRARY}
     ${XML2_LIBRARY}
     ${XSLT_LIBRARY}
     ${Z_LIBRARY}
 )
+
+IF (WTF_USE_SKIA)
+    LIST(APPEND WebKit_LIBRARIES
+        ${FREETYPE_LIBRARY}
+        ${HARFBUZZ_LIBRARY}
+        ${Skia_LIBRARY}
+        ${Skia_QNX_LIBRARY}
+    )
+ELSE ()
+    LIST(APPEND WebKit_LIBRARIES
+        ${ITYPE_LIBRARY}
+        ${WTLE_LIBRARY}
+    )
+ENDIF ()
 
 IF (PROFILING)
     LIST(APPEND WebKit_LIBRARIES
@@ -198,14 +235,19 @@ ENDIF ()
 
 FILE(GLOB BBWebKit_HEADERS "${CMAKE_CURRENT_SOURCE_DIR}/blackberry/Api/*.h")
 
-INSTALL(FILES ${BBWebKit_HEADERS} DESTINATION usr/include/browser/webkit)
+INSTALL(FILES ${BBWebKit_HEADERS}
+        DESTINATION ../../usr/include/browser/webkit)
 
 IF (NOT PUBLIC_BUILD)
     INSTALL(FILES ${TOOLS_DIR}/DumpRenderTree/blackberry/DumpRenderTreeBlackBerry.h
-            DESTINATION usr/include/browser/webkit)
+            DESTINATION ../../usr/include/browser/webkit)
 ENDIF ()
 
-SET(WebKit_INSTALL_DIR "${CMAKE_SYSTEM_PROCESSOR}/usr/lib/torch-webkit")
+IF (ENABLE_VIDEO_TRACK)
+    LIST(APPEND WebKit_INCLUDE_DIRECTORIES
+        "${WEBCORE_DIR}/html/track"
+    )
+ENDIF ()
 
 ADD_CUSTOM_TARGET (
     inspector ALL
@@ -217,19 +259,22 @@ ADD_CUSTOM_TARGET (
 # Generate contents for AboutData.cpp
 ADD_CUSTOM_COMMAND(
     OUTPUT ${DERIVED_SOURCES_WEBCORE_DIR}/AboutDataHaveFeatures.cpp
-    MAIN_DEPENDENCY ${WEBKIT_DIR}/blackberry/WebCoreSupport/AboutDataHaveFeatures.in ${WEBKIT_DIR}/blackberry/WebCoreSupport/generateAboutDataFeatures.pl
+    MAIN_DEPENDENCY ${WEBKIT_DIR}/blackberry/WebCoreSupport/AboutDataHaveFeatures.in
+    DEPENDS ${WEBKIT_DIR}/blackberry/WebCoreSupport/AboutDataHaveFeatures.in ${WEBKIT_DIR}/blackberry/WebCoreSupport/generateAboutDataFeatures.pl
     COMMAND ${PERL_EXECUTABLE} ${WEBKIT_DIR}/blackberry/WebCoreSupport/generateAboutDataFeatures.pl HAVE ${WEBKIT_DIR}/blackberry/WebCoreSupport/AboutDataHaveFeatures.in ${DERIVED_SOURCES_WEBCORE_DIR}/AboutDataHaveFeatures.cpp
 )
 
 ADD_CUSTOM_COMMAND(
     OUTPUT ${DERIVED_SOURCES_WEBCORE_DIR}/AboutDataEnableFeatures.cpp
-    MAIN_DEPENDENCY ${WEBKIT_DIR}/blackberry/WebCoreSupport/AboutDataEnableFeatures.in ${WEBKIT_DIR}/blackberry/WebCoreSupport/generateAboutDataFeatures.pl
+    MAIN_DEPENDENCY ${WEBKIT_DIR}/blackberry/WebCoreSupport/AboutDataEnableFeatures.in
+    DEPENDS ${WEBKIT_DIR}/blackberry/WebCoreSupport/AboutDataEnableFeatures.in ${WEBKIT_DIR}/blackberry/WebCoreSupport/generateAboutDataFeatures.pl
     COMMAND ${PERL_EXECUTABLE} ${WEBKIT_DIR}/blackberry/WebCoreSupport/generateAboutDataFeatures.pl ENABLE ${WEBKIT_DIR}/blackberry/WebCoreSupport/AboutDataEnableFeatures.in ${DERIVED_SOURCES_WEBCORE_DIR}/AboutDataEnableFeatures.cpp
 )
 
 ADD_CUSTOM_COMMAND(
     OUTPUT ${DERIVED_SOURCES_WEBCORE_DIR}/AboutDataUseFeatures.cpp
-    MAIN_DEPENDENCY ${WEBKIT_DIR}/blackberry/WebCoreSupport/AboutDataUseFeatures.in ${WEBKIT_DIR}/blackberry/WebCoreSupport/generateAboutDataFeatures.pl
+    MAIN_DEPENDENCY ${WEBKIT_DIR}/blackberry/WebCoreSupport/AboutDataUseFeatures.in
+    DEPENDS ${WEBKIT_DIR}/blackberry/WebCoreSupport/AboutDataUseFeatures.in ${WEBKIT_DIR}/blackberry/WebCoreSupport/generateAboutDataFeatures.pl
     COMMAND ${PERL_EXECUTABLE} ${WEBKIT_DIR}/blackberry/WebCoreSupport/generateAboutDataFeatures.pl USE ${WEBKIT_DIR}/blackberry/WebCoreSupport/AboutDataUseFeatures.in ${DERIVED_SOURCES_WEBCORE_DIR}/AboutDataUseFeatures.cpp
 )
 
