225513924ec9a0d7b863a4b3842fa129c8efc0f0
Bear Travis <betravis@adobe.com>
2012-10-05 22:01:04 +0000

    [CSS Exclusions] Add shape-inside tests for rounded rectangles https://bugs.webkit.org/show_bug.cgi?id=96669
    Patch by Bear Travis <betravis@adobe.com> on 2012-10-05
    Reviewed by Levi Weintraub.
    
    Adding multiple rounded rectangle tests. The more complex tests use float "sandbags"
    to simulate the shape-inside area in the test expectations.
    (See http://www.alistapart.com/articles/sandbags/)
    In general, each line has its own pair of left and right floats (both lineHeight tall),
    to position the text and appropriately limit its width. In the simple, 0 corner radius
    cases, these floats can be merged because they all have the same width on the left and
    right.
    
    * fast/exclusions/resources/rounded-rectangle.js: Contains functionality for generating
    a shape-inside, simulating one, and generating content that should roughly fill the
    shape-inside.
    (xFromEllipseCenter): The x distance from the center of an ellipse, based on its y
    distance from center.
    (xInset): The inset of a rounded rectangle from its bounding box.
    (generateString): Generate a string that roughly fills a shape-inside's content area.
    (simulateShape): Create floats to simulate a shape-inside area.
    (simulateShapeOutline): Create an outline for the rounded rectangle shape-inside.
    (generateSimulatedShapeElement): Simulate a shape-inside and appropriately fill it
    with content.
    (generateShapeElement): Create a shape-inside and fill it with content.
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-001-expected.html:
    Renamed from LayoutTests/fast/exclusions/shape-inside/shape-inside-rounded-rectangle-expected.html.
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-001.html:
    Renamed from LayoutTests/fast/exclusions/shape-inside/shape-inside-rounded-rectangle.html.
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-002-expected.html: Added.
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-002.html: Added.
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-003-expected.html: Added.
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-003.html: Added.
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-004-expected.html: Added.
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-004.html: Added.
    
    Canonical link: https://commits.webkit.org/116547@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@130557 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/exclusions/resources/rounded-rectangle.js
@@ -0,0 +1,111 @@
+function xFromEllipseCenter(yFromEllipseCenter, radiusX, radiusY) {
+    return radiusX * Math.sqrt(1 - Math.pow(yFromEllipseCenter / radiusY, 2));
+}
+
+function xInset(dimensions, lineTop, lineBottom) {
+    var left;
+    if (lineTop < dimensions.shapeHeight && lineBottom > dimensions.shapeHeight)
+        lineBottom = dimensions.shapeHeight;
+    if (lineTop >= dimensions.shapeHeight || (lineTop >= dimensions.shapeRadiusY && lineBottom <= dimensions.shapeHeight - dimensions.shapeRadiusY)) {
+        left = 0;
+    } else {
+        var yFromEllipseCenter = Math.max((dimensions.shapeRadiusY - lineTop), lineBottom - (dimensions.shapeHeight - dimensions.shapeRadiusY));
+        left = dimensions.shapeRadiusX - xFromEllipseCenter(yFromEllipseCenter, dimensions.shapeRadiusX, dimensions.shapeRadiusY);
+    }
+    return left;
+}
+
+function generateString(dimensions, lineHeight) {
+    var resultLength = 0;
+    if (dimensions.shapeRadiusX == 0 || dimensions.shapeRadiusY == 0)
+        resultLength = dimensions.shapeWidth * dimensions.shapeHeight / (lineHeight * lineHeight);
+    else {
+        for (var lineTop = 0; lineTop < dimensions.shapeHeight; lineTop += lineHeight) {
+            var width = dimensions.shapeWidth - 2 * xInset(dimensions, lineTop, lineTop + lineHeight);
+            resultLength += width / lineHeight;
+        }
+    }
+    return (new Array(Math.ceil(resultLength / 2)).join("x "));
+}
+
+function simulateShape(elementId, stylesheet, dimensions, lineHeight) {
+    var simpleRectangle = dimensions.shapeRadiusX == 0 || dimensions.shapeRadiusY == 0;
+    var floatHeight = simpleRectangle ? dimensions.shapeHeight : lineHeight;
+
+    stylesheet.insertRule("#" + elementId + " { width: " + dimensions.width + "px; height: " + dimensions.height + "px; font: " + lineHeight + "px/1 Ahem, sans-serif; }");
+    stylesheet.insertRule("#" + elementId + " .float { height: " + floatHeight + "px; }");
+    stylesheet.insertRule("#" + elementId + " .left { float: left; clear: left; }");
+    stylesheet.insertRule("#" + elementId + " .right { float: right; clear: right; }");
+    stylesheet.insertRule("#" + elementId + " p { -webkit-margin-before: 0; word-wrap: break-word; letter-spacing: 0; }");
+
+    var element = document.getElementById(elementId);
+    if (dimensions.shapeY > 0) {
+        var paddingTop = document.createElement("div");
+        paddingTop.setAttribute("class", "float left");
+        paddingTop.style.width = dimensions.width + "px";
+        paddingTop.style.height = dimensions.shapeY + "px";
+        element.appendChild(paddingTop);
+    }
+
+    for (var y = 0; y < dimensions.shapeHeight; y+= floatHeight) {
+        var inset = simpleRectangle ? 0 : xInset(dimensions, y, y + lineHeight);
+
+        var paddingLeft = document.createElement("div");
+        paddingLeft.setAttribute("class", "float left");
+        paddingLeft.style.width = dimensions.shapeX + inset + "px";
+        element.appendChild(paddingLeft);
+
+        var paddingRight = document.createElement("div");
+        paddingRight.setAttribute("class", "float right");
+        paddingRight.style.width = Math.floor((dimensions.width - dimensions.shapeWidth - dimensions.shapeX) + inset) + "px";
+        element.appendChild(paddingRight);
+    }
+}
+
+function simulateShapeOutline(elementId, stylesheet, dimensions) {
+    stylesheet.insertRule("#" + elementId + "{ position: relative; }");
+    stylesheet.insertRule("#" + elementId + "::before { "
+                            + "content: ' '; "
+                            + "display: block; "
+                            + "position: absolute; "
+                            + "top: " + (dimensions.shapeY - 1) + "px; "
+                            + "left: " + (dimensions.shapeX - 1) + "px; "
+                            + "width: " + dimensions.shapeWidth + "px; "
+                            + "height: " + dimensions.shapeHeight + "px; "
+                            + "border: 1px solid blue; "
+                            + "border-radius: " + dimensions.shapeRadiusX + "px / " + dimensions.shapeRadiusY + "px; }");
+}
+
+function generateSimulatedShapeElement(elementId, stylesheet, dimensions, lineHeight) {
+    simulateShape(elementId, stylesheet, dimensions, lineHeight);
+    simulateShapeOutline(elementId, stylesheet, dimensions);
+    var text = document.createTextNode(generateString(dimensions, lineHeight));
+    var p = document.createElement("p");
+    p.appendChild(text);
+
+    var element = document.getElementById(elementId);
+    element.appendChild(p);
+}
+
+function generateShapeElement(elementId, stylesheet, dimensions, lineHeight) {
+    stylesheet.insertRule("#" + elementId + " { "
+        + "-webkit-shape-inside: rectangle("
+        + dimensions.shapeX + "px, "
+        + dimensions.shapeY + "px, "
+        + dimensions.shapeWidth + "px, "
+        + dimensions.shapeHeight + "px, "
+        + dimensions.shapeRadiusX + "px, "
+        + dimensions.shapeRadiusY + "px); "
+        + "width: " + dimensions.width + "px; "
+        + "height: " + dimensions.height + "px; "
+        + "font: " + lineHeight + "px/1 Ahem, sans-serif; "
+        + "word-wrap: break-word; }");
+    stylesheet.insertRule("#" + elementId + " p { -webkit-margin-before: 0; word-wrap: break-word; letter-spacing: 0; }");
+    simulateShapeOutline(elementId, stylesheet, dimensions);
+    var text = document.createTextNode(generateString(dimensions, lineHeight));
+    var p = document.createElement("p");
+    p.appendChild(text);
+
+    var element = document.getElementById(elementId);
+    element.appendChild(p);
+}
