38c036a51ebab93c35db515614d398ae6636aee8
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2012-10-08 10:45:19 +0000

    [Qt] Apply QQuickWebViewExperimental::devicePixelRatio to the desktop mode as well https://bugs.webkit.org/show_bug.cgi?id=98428
    Reviewed by Kenneth Rohde Christiansen.
    
    This is handled by the PageViewportController when in flickable mode.
    
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebViewLegacyPrivate::updateViewportSize):
    
    Canonical link: https://commits.webkit.org/116617@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@130630 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/qt/qquickwebview.cpp
+++ Source/WebKit2/UIProcess/API/qt/qquickwebview.cpp
@@ -820,12 +820,17 @@ void QQuickWebViewLegacyPrivate::updateViewportSize()
     QSizeF viewportSize = q->boundingRect().size();
     if (viewportSize.isEmpty())
         return;
+
+    float devicePixelRatio = webPageProxy->deviceScaleFactor();
     pageView->setContentsSize(viewportSize);
+    // Make sure that our scale matches the one passed to setVisibleContentsRect.
+    pageView->setContentsScale(devicePixelRatio);
+
     // The fixed layout is handled by the FrameView and the drawing area doesn't behave differently
     // whether its fixed or not. We still need to tell the drawing area which part of it
     // has to be rendered on tiles, and in desktop mode it's all of it.
-    webPageProxy->drawingArea()->setSize(viewportSize.toSize(), IntSize());
-    webPageProxy->drawingArea()->setVisibleContentsRect(FloatRect(FloatPoint(), FloatSize(viewportSize)), 1, FloatPoint());
+    webPageProxy->drawingArea()->setSize((viewportSize / devicePixelRatio).toSize(), IntSize());
+    webPageProxy->drawingArea()->setVisibleContentsRect(FloatRect(FloatPoint(), viewportSize / devicePixelRatio), devicePixelRatio, FloatPoint());
 }
 
 qreal QQuickWebViewLegacyPrivate::zoomFactor() const
