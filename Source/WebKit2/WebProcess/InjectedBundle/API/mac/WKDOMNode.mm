9d38781bcccf007bc0fc4d545b555e0bcaaa2dab
Sam Weinig <weinig@apple.com>
2012-10-10 22:31:08 +0000

    Add skeleton of a WebKit2 ObjC DOM SPI (not intended to be the full DOM) https://bugs.webkit.org/show_bug.cgi?id=98955
    Reviewed by Anders Carlsson.
    
    Add a skeleton DOM implementation only exposing Node, Element and Document
    for now. It is currently only available in 64-bit as it relies on the non-fragile
    ABI for now.
    
    * WebKit2.xcodeproj/project.pbxproj:
    * WebProcess/InjectedBundle/API/mac/WKDOMDocument.h: Added.
    * WebProcess/InjectedBundle/API/mac/WKDOMDocument.mm: Added.
    * WebProcess/InjectedBundle/API/mac/WKDOMElement.h: Added.
    * WebProcess/InjectedBundle/API/mac/WKDOMElement.mm: Added.
    * WebProcess/InjectedBundle/API/mac/WKDOMInternals.h: Added.
    * WebProcess/InjectedBundle/API/mac/WKDOMInternals.mm: Added.
    * WebProcess/InjectedBundle/API/mac/WKDOMNode.h: Added.
    * WebProcess/InjectedBundle/API/mac/WKDOMNode.mm: Added.
    
    
    Canonical link: https://commits.webkit.org/116943@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@130971 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit2/WebProcess/InjectedBundle/API/mac/WKDOMNode.mm
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2012 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "config.h"
+
+#if __LP64__
+
+#import "WKDOMNode.h"
+
+#import "WKDOMInternals.h"
+
+@implementation WKDOMNode
+
+- (id)_initWithNode:(WebCore::Node*)node
+{
+    self = [super init];
+    if (!self)
+        return nil;
+
+    WebKit::WKDOMNodeCacheAdd(node, self);
+    return self;
+}
+
+- (void)dealloc
+{
+    WebKit::WKDOMNodeCacheRemove(_node.get());
+    [super dealloc];
+}
+
+- (WKDOMDocument *)document
+{
+    return WebKit::toWKDOMDocument(_node->document());
+}
+
+@end
+
+#endif // __LP64__
