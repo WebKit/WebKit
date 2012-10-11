293729017d1e12537e8cc81eeef91eac40578137
Kunihiko Sakamoto <ksakamoto@chromium.org>
2012-10-11 09:23:53 +0000

    Final newline (LF or CRLF) in paste buffer is converted to space https://bugs.webkit.org/show_bug.cgi?id=80838
    Patch by Kunihiko Sakamoto <ksakamoto@chromium.org> on 2012-10-11
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Currently newline characters are replaced with spaces when pasted
    to text input. This behavior was introduced in r37539, with the
    intention to follow the FireFox's behavior (see
    https://bugs.webkit.org/show_bug.cgi?id=20461). However, FireFox
    truncates newlines at the end of pasted text, instead of replacing
    them with spaces. So this patch changes TextFieldInputType to cut
    trailing newlines of inserted text.
    
    Test: fast/forms/paste-multiline-text-input.html
    
    * html/TextFieldInputType.cpp:
    (WebCore::TextFieldInputType::handleBeforeTextInsertedEvent):
    Truncates trailing newlines.
    
    LayoutTests:
    
    Verify that trailing newlines are stripped when pasted into text inputs.
    
    * editing/input/paste-text-ending-with-interchange-newline-expected.txt:
    Updated the expectation.
    * fast/forms/paste-multiline-text-input.html:
    Changed to use document.execCommand to mimic pasting, because setting
    input.value attribute does not trigger beforeTextInserted event.
    Added a test case to test trailing newlines.
    
    Canonical link: https://commits.webkit.org/117002@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@131032 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/TextFieldInputType.cpp
+++ Source/WebCore/html/TextFieldInputType.cpp
@@ -381,6 +381,10 @@ void TextFieldInputType::handleBeforeTextInsertedEvent(BeforeTextInsertedEvent* 
 
     // Truncate the inserted text to avoid violating the maxLength and other constraints.
     String eventText = event->text();
+    unsigned textLength = eventText.length();
+    while (textLength > 0 && isASCIILineBreak(eventText[textLength - 1]))
+        textLength--;
+    eventText.truncate(textLength);
     eventText.replace("\r\n", " ");
     eventText.replace('\r', ' ');
     eventText.replace('\n', ' ');
