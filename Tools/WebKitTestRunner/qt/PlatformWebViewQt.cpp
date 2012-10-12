b3ed48fc15799653e1bb0e31603f13a29596bb43
Balazs Kelemen <kbalazs@webkit.org>
2012-10-12 08:44:24 +0000

    [Qt][WTR] Do a forced repaint before generating pixel results https://bugs.webkit.org/show_bug.cgi?id=98654
    Reviewed by Jocelyn Turcotte.
    
    Source/WebKit2:
    
    Added API to convert a QImage to a WKImage so we can
    pass it to cross-platform code.
    
    * Shared/API/c/qt/WKImageQt.cpp:
    (WKImageCreateFromQImage):
    * Shared/API/c/qt/WKImageQt.h:
    
    Tools:
    
    Do a forced repaint before grabbing the pixel snapshot. This extra
    synchronisation is necessary with the CoordinatedGraphics rendering
    backend because it has a fully asynchronous nature. This patch make
    us using the window snapshot for pixel results which is necessary to
    capture animations and other dynamic content. The actual grabbing of
    the window has not been implemented in this patch. It will come in
    a follow-up.
    
    * WebKitTestRunner/TestInvocation.cpp:
    (WTR::TestInvocation::invoke):
    (WTR::TestInvocation::dump): Store results in member variables.
    Don't close output channels yet.
    (WTR::TestInvocation::dumpResults): Added. This is where we dump
    the results if no error happened.
    (WTR):
    (WTR::TestInvocation::didReceiveMessageFromInjectedBundle):
    * WebKitTestRunner/TestInvocation.h:
    (TestInvocation):
    * WebKitTestRunner/qt/PlatformWebViewQt.cpp:
    (WTR::PlatformWebView::windowSnapshotImage):
    * WebKitTestRunner/qt/TestInvocationQt.cpp:
    (WTR::TestInvocation::forceRepaintDoneCallback):
    (WTR):
    (WTR::TestInvocation::dumpPixelsAndCompareWithExpected):
    
    Canonical link: https://commits.webkit.org/117129@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@131160 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/WebKitTestRunner/qt/PlatformWebViewQt.cpp
+++ Tools/WebKitTestRunner/qt/PlatformWebViewQt.cpp
@@ -153,7 +153,7 @@ WKRetainPtr<WKImageRef> PlatformWebView::windowSnapshotImage()
 {
     // FIXME: implement to capture pixels in the UI process,
     // which may be necessary to capture things like 3D transforms.
-    return 0;
+    return WKImageCreateFromQImage(QImage());
 }
 
 } // namespace WTR
