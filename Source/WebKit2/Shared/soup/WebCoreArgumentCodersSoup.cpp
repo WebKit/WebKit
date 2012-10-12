6ed8577b9d10749d58481a245022adf59a98ff8f
Balazs Kelemen <kbalazs@webkit.org>
2012-10-12 11:59:40 +0000

    [WK2] Serialization of Resource[Request,Response,Error] should be shared across ports https://bugs.webkit.org/show_bug.cgi?id=90142
    Unreviewed buildfix 2.
    
    * Shared/soup/WebCoreArgumentCodersSoup.cpp:
    (CoreIPC::::encodePlatformData):
    (CoreIPC::::decodePlatformData):
    
    Canonical link: https://commits.webkit.org/117142@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@131174 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/Shared/soup/WebCoreArgumentCodersSoup.cpp
+++ Source/WebKit2/Shared/soup/WebCoreArgumentCodersSoup.cpp
@@ -66,11 +66,11 @@ bool ArgumentCoder<ResourceResponse>::decodePlatformData(ArgumentDecoder* decode
 }
 
 
-void ArgumentCoder<ResourceError>::encode(ArgumentEncoder* encoder, const ResourceError& resourceError)
+void ArgumentCoder<ResourceError>::encodePlatformData(ArgumentEncoder* encoder, const ResourceError& resourceError)
 {
 }
 
-bool ArgumentCoder<ResourceError>::decode(ArgumentDecoder* decoder, ResourceError& resourceError)
+bool ArgumentCoder<ResourceError>::decodePlatformData(ArgumentDecoder* decoder, ResourceError& resourceError)
 {
     return true;
 }
