5a3f9f787d32f6cb63c06bc1304f7f94ae1921f2
Simon Fraser <simon.fraser@apple.com>
2012-10-13 01:16:04 +0000

    Add some initial visible rect tests and tweak the visible wash layer https://bugs.webkit.org/show_bug.cgi?id=99095
    Reviewed by Tim Horton.
    
    Source/WebCore:
    
    Make the m_visibleTileWashLayer more useful by showing it for all
    layers with transforms, not just tiled layers. This is a debugging
    layer normally disabled, but enabled by #defining VISIBLE_TILE_WASH.
    
    Tests: compositing/visible-rect/2d-transformed.html
           compositing/visible-rect/3d-transformed.html
           compositing/visible-rect/clipped-by-viewport copy.html
           compositing/visible-rect/clipped-by-viewport.html
           compositing/visible-rect/clipped-visible-rect.html
    
    * platform/graphics/ca/GraphicsLayerCA.cpp:
    (WebCore::GraphicsLayerCA::recursiveCommitChanges):
    (WebCore::GraphicsLayerCA::swapFromOrToTiledLayer):
    * platform/graphics/ca/GraphicsLayerCA.h:
    
    LayoutTests:
    
    Add some tests of the GraphicsLayerCA visibleRect for simple cases.
    
    Skip these tests on platforms that don't log layer visible rects
    (all non-Mac).
    
    * compositing/visible-rect/2d-transformed-expected.txt: Added.
    * compositing/visible-rect/2d-transformed.html: Added.
    * compositing/visible-rect/clipped-by-viewport-expected.txt: Added.
    * compositing/visible-rect/clipped-by-viewport.html: Added.
    * compositing/visible-rect/clipped-visible-rect-expected.txt: Added.
    * compositing/visible-rect/clipped-visible-rect.html: Added.
    * platform/chromium-android/TestExpectations:
    * platform/chromium/TestExpectations:
    * platform/chromium/virtual/softwarecompositing/TestExpectations: Added.
    * platform/efl/TestExpectations:
    * platform/gtk/TestExpectations:
    * platform/qt/TestExpectations:
    * platform/win/TestExpectations:
    * platform/wincairo/TestExpectations:
    
    Canonical link: https://commits.webkit.org/117214@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@131246 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
+++ Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -945,6 +945,22 @@ void GraphicsLayerCA::recursiveCommitChanges(const TransformState& state, float 
     m_visibleRect = clipRectForSelf;
     
 #ifdef VISIBLE_TILE_WASH
+    // Use having a transform as a key to making the tile wash layer. If every layer gets a wash,
+    // they start to obscure useful information.
+    if (!m_transform.isIdentity() && !m_visibleTileWashLayer) {
+        static Color washFillColor(255, 0, 0, 50);
+        static Color washBorderColor(255, 0, 0, 100);
+        
+        m_visibleTileWashLayer = PlatformCALayer::create(PlatformCALayer::LayerTypeLayer, this);
+        String name = String::format("Visible Tile Wash Layer %p", m_visibleTileWashLayer->platformLayer());
+        m_visibleTileWashLayer->setName(name);
+        m_visibleTileWashLayer->setAnchorPoint(FloatPoint3D(0, 0, 0));
+        m_visibleTileWashLayer->setBorderColor(washBorderColor);
+        m_visibleTileWashLayer->setBorderWidth(8);
+        m_visibleTileWashLayer->setBackgroundColor(washFillColor);
+        noteSublayersChanged();
+    }
+
     if (m_visibleTileWashLayer)
         m_visibleTileWashLayer->setFrame(clipRectForSelf);
 #endif
@@ -2470,27 +2486,6 @@ void GraphicsLayerCA::swapFromOrToTiledLayer(bool useTiledLayer, float pageScale
     m_layer = PlatformCALayer::create(useTiledLayer ? PlatformCALayer::LayerTypeWebTiledLayer : PlatformCALayer::LayerTypeWebLayer, this);
     m_usingTiledLayer = useTiledLayer;
     
-#ifdef VISIBLE_TILE_WASH
-    if (useTiledLayer) {
-        if (!m_visibleTileWashLayer) {
-            static Color washFillColor(255, 0, 0, 50);
-            static Color washBorderColor(255, 0, 0, 100);
-            
-            m_visibleTileWashLayer = PlatformCALayer::create(PlatformCALayer::LayerTypeLayer, this);
-            String name = String::format("Visible Tile Wash Layer %p", m_visibleTileWashLayer->platformLayer());
-            m_visibleTileWashLayer->setName(name);
-            m_visibleTileWashLayer->setAnchorPoint(FloatPoint3D(0, 0, 0));
-            m_visibleTileWashLayer->setBorderColor(washBorderColor);
-            m_visibleTileWashLayer->setBorderWidth(8);
-            m_visibleTileWashLayer->setBackgroundColor(washFillColor);
-        }
-    } else {
-        if (m_visibleTileWashLayer)
-            m_visibleTileWashLayer->removeFromSuperlayer();
-        m_visibleTileWashLayer = 0;
-    }
-#endif
-
     m_layer->adoptSublayers(oldLayer.get());
 
 #ifdef VISIBLE_TILE_WASH
