81f4cdb536f814a2b963c13aefc2538d57916c4d
Peter Wang <peter.wang@torchmobile.com.cn>
2012-10-16 04:42:00 +0000

    [BlackBerry] Web Inspector: Remove the useless preferences items in "inspectorBB.js" https://bugs.webkit.org/show_bug.cgi?id=99404
    Patch by Peter Wang <peter.wang@torchmobile.com.cn> on 2012-10-15
    Reviewed by George Staikos.
    
    In "inspectorBB.js", remove the useless preferences items to catch up the new code of frontend.
    
    * WebCoreSupport/inspectorBB.js:
    
    Canonical link: https://commits.webkit.org/117374@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@131409 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/blackberry/WebCoreSupport/inspectorBB.js
+++ Source/WebKit/blackberry/WebCoreSupport/inspectorBB.js
@@ -17,17 +17,8 @@
  */
 var context = {};
 (function () {
-    Preferences.ignoreWhitespace = false;
-    Preferences.samplingCPUProfiler = true;
-    Preferences.debuggerAlwaysEnabled = true;
-    Preferences.profilerAlwaysEnabled = true;
-    Preferences.canEditScriptSource = false;
-    Preferences.onlineDetectionEnabled = false;
-    Preferences.nativeInstrumentationEnabled = true;
     // FIXME: Turn this to whatever the value of --enable-file-system for chrome.
     Preferences.fileSystemEnabled = false;
-    Preferences.canClearCacheAndCookies = true;
-    Preferences.showCookiesTab = true;
 })();
 InspectorFrontendHost.copyText = function(tmp) {
     var encoded = encodeURI(tmp);
