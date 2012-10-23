5240c1b6318442a854efe5752b93694cc6e1b410
Pavel Feldman <pfeldman@chromium.org>
2012-10-23 17:04:39 +0000

    Web Inspector: array grouping does not work for arrays with exactly 10000 elements. https://bugs.webkit.org/show_bug.cgi?id=100131
    Reviewed by Vsevolod Vlasov.
    
    Source/WebCore:
    
    Using ceil() - 1 instead of floor() in bucket size calculation.
    
    * inspector/front-end/ObjectPropertiesSection.js:
    
    LayoutTests:
    
    * inspector/console/console-big-array-expected.txt:
    * inspector/console/console-big-array.html:
    
    
    Canonical link: https://commits.webkit.org/118157@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@132238 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/ObjectPropertiesSection.js
+++ Source/WebCore/inspector/front-end/ObjectPropertiesSection.js
@@ -603,8 +603,7 @@ WebInspector.ArrayGroupingTreeElement._populateRanges = function(treeElement, ob
     {
         var count = 0;
         for (var i = fromIndex; i <= toIndex; ++i) {
-            var value = this[i];
-            if (typeof value !== "undefined")
+            if (i in this)
                 ++count;
         }
 
@@ -612,15 +611,14 @@ WebInspector.ArrayGroupingTreeElement._populateRanges = function(treeElement, ob
         if (count <= bucketThreshold)
             bucketSize = count;
         else
-            bucketSize = Math.pow(bucketThreshold, Math.floor(Math.log(count) / Math.log(bucketThreshold)));
+            bucketSize = Math.pow(bucketThreshold, Math.ceil(Math.log(count) / Math.log(bucketThreshold)) - 1);
 
         var ranges = [];
         count = 0;
         var groupStart = -1;
         var groupEnd = 0;
         for (var i = fromIndex; i <= toIndex; ++i) {
-            var value = this[i];
-            if (typeof value === "undefined")
+            if (!(i in this))
                 continue;
 
             if (groupStart === -1)
@@ -678,9 +676,8 @@ WebInspector.ArrayGroupingTreeElement._populateAsFragment = function(treeElement
     {
         var result = Object.create(null);
         for (var i = fromIndex; i <= toIndex; ++i) {
-            var value = this[i];
-            if (typeof value !== "undefined")
-                result[i] = value;
+            if (i in this)
+                result[i] = this[i];
         }
         return result;
     }
