f8da3b628b2ff6892d8af050f3b1ba298420312f
Alexey Proskuryakov <ap@apple.com>
2012-10-29 23:18:26 +0000

    [WK2] Add a NetworkingContext for NetworkProcess         https://bugs.webkit.org/show_bug.cgi?id=100708
            Reviewed by Anders Carlsson.
    
            Each request will have a context, because these are so lightweight on Mac, and
            other platforms will need ResourceHandle refactored anyway to use NetworkProcess.
    
            * NetworkProcess/mac/RemoteNetworkingContext.h: Added.
            * NetworkProcess/mac/RemoteNetworkingContext.mm: Added.
            These go mac/ subdirectory, because NetworkingContext interface is different on
            each platform, as ResourceHandle needs dictate.
    
            * WebKit2.xcodeproj/project.pbxproj:
    
    
    
    Canonical link: https://commits.webkit.org/118741@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@132857 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit2/NetworkProcess/mac/RemoteNetworkingContext.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2012 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef RemoteNetworkingContext_h
+#define RemoteNetworkingContext_h
+
+#include <WebCore/NetworkingContext.h>
+
+namespace WebKit {
+
+class RemoteNetworkingContext : public WebCore::NetworkingContext {
+public:
+    virtual ~RemoteNetworkingContext();
+
+private:
+    virtual bool isValid() const OVERRIDE;
+
+    virtual bool needsSiteSpecificQuirks() const OVERRIDE;
+    virtual bool localFileContentSniffingEnabled() const OVERRIDE;
+    virtual WebCore::SchedulePairHashSet* scheduledRunLoopPairs() const OVERRIDE;
+    virtual WebCore::ResourceError blockedError(const WebCore::ResourceRequest&) const OVERRIDE;
+
+    bool m_needsSiteSpecificQuirks;
+    bool m_localFileContentSniffingEnabled;
+};
+
+}
+
+#endif // RemoteNetworkingContext_h
