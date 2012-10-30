921a06cca32f1b64489e0b8773ff4d0edcfafed4
Emil A Eklund <eae@chromium.org>
2012-10-30 13:36:31 +0000

    [subixel] Change LineWidth::shrinkAvailableWidthForNewFloatIfNeeded to not pixel snap https://bugs.webkit.org/show_bug.cgi?id=100742
    Reviewed by Levi Weintraub.
    
    Source/WebCore:
    
    As we no longer pixel snap values when computing the current width for a
    line we should not do it in shrinkAvailableWidthForNewFloatIfNeeded
    either.
    
    Test: fast/sub-pixel/float-wrap-zoom.html
    
    * rendering/RenderBlockLineLayout.cpp:
    (WebCore::LineWidth::LineWidth): Remove unnecessary ifdef.
    (WebCore::LineWidth::shrinkAvailableWidthForNewFloatIfNeeded): Use float values instead of pixel snapped/floored values as the line width calculations uses floats.
    
    LayoutTests:
    
    Add test for wrapping when combining float and inline elements.
    
    * fast/sub-pixel/float-wrap-zoom-expected.html: Added.
    * fast/sub-pixel/float-wrap-zoom.html: Added.
    
    
    Canonical link: https://commits.webkit.org/118788@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@132904 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderBlockLineLayout.cpp
+++ Source/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -94,13 +94,8 @@ public:
 #endif
         updateAvailableWidth();
     }
-#if ENABLE(SUBPIXEL_LAYOUT)
     bool fitsOnLine() const { return currentWidth() <= m_availableWidth; }
     bool fitsOnLine(float extra) const { return currentWidth() + extra <= m_availableWidth; }
-#else
-    bool fitsOnLine() const { return currentWidth() <= m_availableWidth; }
-    bool fitsOnLine(float extra) const { return currentWidth() + extra <= m_availableWidth; }
-#endif
     float currentWidth() const { return m_committedWidth + m_uncommittedWidth; }
 
     // FIXME: We should eventually replace these three functions by ones that work on a higher abstraction.
@@ -177,12 +172,12 @@ inline void LineWidth::shrinkAvailableWidthForNewFloatIfNeeded(RenderBlock::Floa
         return;
 
     if (newFloat->type() == RenderBlock::FloatingObject::FloatLeft) {
-        float newLeft = m_block->pixelSnappedLogicalRightForFloat(newFloat);
+        float newLeft = m_block->logicalRightForFloat(newFloat);
         if (m_isFirstLine && m_block->style()->isLeftToRightDirection())
             newLeft += floorToInt(m_block->textIndentOffset());
         m_left = max<float>(m_left, newLeft);
     } else {
-        float newRight = m_block->pixelSnappedLogicalLeftForFloat(newFloat);
+        float newRight = m_block->logicalLeftForFloat(newFloat);
         if (m_isFirstLine && !m_block->style()->isLeftToRightDirection())
             newRight -= floorToInt(m_block->textIndentOffset());
         m_right = min<float>(m_right, newRight);
