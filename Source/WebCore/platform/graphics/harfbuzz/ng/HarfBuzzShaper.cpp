23a20fb02682313f56b4e84b7784c1f768072913
Kenichi Ishibashi <bashi@chromium.org>
2012-11-06 03:04:17 +0000

    [Chromium] Unicode combining diacritical aren't always combined on Linux https://bugs.webkit.org/show_bug.cgi?id=101009
    Reviewed by Kent Tamura.
    
    Source/WebCore:
    
    Add a space character to harfbuzzBuffer as pre-context. This will prevent
    harfbuzz from inserting dotted-circle.
    
    Test: fast/text/international/combining-marks-position.html
    
    * platform/graphics/harfbuzz/ng/HarfBuzzShaper.cpp:
    (WebCore::HarfBuzzShaper::shapeHarfBuzzRuns):
    
    LayoutTests:
    
    Add a test that checks whether combining marks are combined with base characters.
    
    * fast/text/international/combining-marks-position-expected.txt: Added.
    * fast/text/international/combining-marks-position.html: Added.
    
    
    Canonical link: https://commits.webkit.org/119368@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@133550 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/harfbuzz/ng/HarfBuzzShaper.cpp
+++ Source/WebCore/platform/graphics/harfbuzz/ng/HarfBuzzShaper.cpp
@@ -322,6 +322,11 @@ bool HarfBuzzShaper::shapeHarfBuzzRuns()
         HarfBuzzRun* currentRun = m_harfbuzzRuns[runIndex].get();
         const SimpleFontData* currentFontData = currentRun->fontData();
 
+        // Add a space as pre-context to the buffer. This prevents showing dotted-circle
+        // for combining marks at the beginning of runs.
+        static const uint16_t preContext = ' ';
+        hb_buffer_add_utf16(harfbuzzBuffer.get(), &preContext, 1, 1, 0);
+
         if (m_font->isSmallCaps() && u_islower(m_normalizedBuffer[currentRun->startIndex()])) {
             String upperText = String(m_normalizedBuffer.get() + currentRun->startIndex(), currentRun->numCharacters());
             upperText.makeUpper();
