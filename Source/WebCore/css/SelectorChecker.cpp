de6e2251f3acfe9a8f96138083ef3a8e1cd24e7d
Shinya Kawanaka <shinyak@chromium.org>
2012-11-07 04:40:33 +0000

    [Shadow] Pseudo custom-elements should start with 'x-'. https://bugs.webkit.org/show_bug.cgi?id=100919
    Reviewed by Dimitri Glazkov.
    
    Source/WebCore:
    
    Pseuco custom-elements should start with 'x-'. 'Pseudo' starting with '-webkit-' should work
    only in UserAgent ShadowDOM. If it's used in Author ShadowDOM, it should not work.
    
    Test: fast/dom/shadow/pseudo-attribute-rendering.html
    
    * css/SelectorChecker.cpp:
    (WebCore::SelectorChecker::checkSelector): Added ShadowRoot type check.
    
    LayoutTests:
    
    * fast/dom/shadow/pseudo-attribute-rendering-expected.html: Added.
    * fast/dom/shadow/pseudo-attribute-rendering.html: Added.
    
    
    Canonical link: https://commits.webkit.org/119517@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@133715 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/SelectorChecker.cpp
+++ Source/WebCore/css/SelectorChecker.cpp
@@ -51,6 +51,7 @@
 #include "RenderStyle.h"
 #include "ScrollableArea.h"
 #include "ScrollbarTheme.h"
+#include "ShadowRoot.h"
 #include "SiblingTraversalStrategies.h"
 #include "StyledElement.h"
 #include "Text.h"
@@ -447,7 +448,13 @@ SelectorChecker::SelectorMatch SelectorChecker::checkSelector(const SelectorChec
 
     if (context.selector->m_match == CSSSelector::PseudoElement) {
         if (context.selector->isCustomPseudoElement()) {
-            if (context.element->shadowPseudoId() != context.selector->value())
+            if (ShadowRoot* root = context.element->shadowRoot()) {
+                if (context.element->shadowPseudoId() != context.selector->value())
+                    return SelectorFailsLocally;
+
+                if (context.selector->pseudoType() == CSSSelector::PseudoWebKitCustomElement && root->type() != ShadowRoot::UserAgentShadowRoot)
+                    return SelectorFailsLocally;
+            } else
                 return SelectorFailsLocally;
         } else {
             if ((!context.elementStyle && m_mode == ResolvingStyle) || m_mode == QueryingRules)
