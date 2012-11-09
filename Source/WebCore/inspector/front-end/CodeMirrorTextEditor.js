a19626e22c6c16a52ee185ef866c858054a71dd2
Jan Keromnes <janx@linux.com>
2012-11-09 06:46:26 +0000

    Web Inspector: stop using cursorCoords in CodeMirrorTextEditor https://bugs.webkit.org/show_bug.cgi?id=101607
    Patch by Jan Keromnes <janx@linux.com> on 2012-11-08
    Reviewed by Vsevolod Vlasov.
    
    API changes completing migration to v3.
    
    * inspector/front-end/CodeMirrorTextEditor.js:
    (WebInspector.CodeMirrorTextEditor):
    (WebInspector.CodeMirrorTextEditor.prototype.revealLine):
    (WebInspector.CodeMirrorTextEditor.prototype.selection):
    
    Canonical link: https://commits.webkit.org/119800@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@134014 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/CodeMirrorTextEditor.js
+++ Source/WebCore/inspector/front-end/CodeMirrorTextEditor.js
@@ -54,8 +54,9 @@ WebInspector.CodeMirrorTextEditor = function(url, delegate)
         lineNumbers: true,
         gutters: ["CodeMirror-linenumbers", "breakpoints"]
     });
-    CodeMirror.on(this._codeMirror, "change", this._change.bind(this));
-    CodeMirror.on(this._codeMirror, "gutterClick", this._gutterClick.bind(this));
+
+    this._codeMirror.on("change", this._change.bind(this));
+    this._codeMirror.on("gutterClick", this._gutterClick.bind(this));
 
     this._lastRange = this.range();
 
@@ -117,8 +118,7 @@ WebInspector.CodeMirrorTextEditor.prototype = {
     revealLine: function(lineNumber)
     {
         this._codeMirror.setCursor({ line: lineNumber, ch: 0 });
-        var coords = this._codeMirror.cursorCoords();
-        this._codeMirror.scrollTo(coords.x, coords.y);
+        this._codeMirror.scrollIntoView();
     },
 
     _gutterClick: function(instance, lineNumber, gutter, event)
@@ -276,8 +276,8 @@ WebInspector.CodeMirrorTextEditor.prototype = {
      */
     selection: function(textRange)
     {
-        var start = this._codeMirror.cursorCoords(true);
-        var end = this._codeMirror.cursorCoords(false);
+        var start = this._codeMirror.getCursor(true);
+        var end = this._codeMirror.getCursor(false);
 
         if (start.line > end.line || (start.line == end.line && start.ch > end.ch))
             return this._toRange(end, start);
