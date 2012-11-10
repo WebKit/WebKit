ebe4627af926a99c891a5c725b83a5bde21849c8
Filip Pizlo <fpizlo@apple.com>
2012-11-10 00:15:43 +0000

    Fix indentation of BooleanPrototype.h
    Rubber stamped by Mark Hahnenberg.
    
    * runtime/BooleanPrototype.h:
    
    
    
    Canonical link: https://commits.webkit.org/119908@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@134134 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/BooleanPrototype.h
+++ Source/JavaScriptCore/runtime/BooleanPrototype.h
@@ -25,34 +25,34 @@
 
 namespace JSC {
 
-    class BooleanPrototype : public BooleanObject {
-    public:
-        typedef BooleanObject Base;
+class BooleanPrototype : public BooleanObject {
+public:
+    typedef BooleanObject Base;
 
-        static BooleanPrototype* create(ExecState* exec, JSGlobalObject* globalObject, Structure* structure)
-        {
-            BooleanPrototype* prototype = new (NotNull, allocateCell<BooleanPrototype>(*exec->heap())) BooleanPrototype(exec, structure);
-            prototype->finishCreation(exec, globalObject);
-            return prototype;
-        }
+    static BooleanPrototype* create(ExecState* exec, JSGlobalObject* globalObject, Structure* structure)
+    {
+        BooleanPrototype* prototype = new (NotNull, allocateCell<BooleanPrototype>(*exec->heap())) BooleanPrototype(exec, structure);
+        prototype->finishCreation(exec, globalObject);
+        return prototype;
+    }
         
-        static const ClassInfo s_info;
+    static const ClassInfo s_info;
 
-        static Structure* createStructure(JSGlobalData& globalData, JSGlobalObject* globalObject, JSValue prototype)
-        {
-            return Structure::create(globalData, globalObject, prototype, TypeInfo(ObjectType, StructureFlags), &s_info);
-        }
+    static Structure* createStructure(JSGlobalData& globalData, JSGlobalObject* globalObject, JSValue prototype)
+    {
+        return Structure::create(globalData, globalObject, prototype, TypeInfo(ObjectType, StructureFlags), &s_info);
+    }
 
-    protected:
-        void finishCreation(ExecState*, JSGlobalObject*);
-        static const unsigned StructureFlags = OverridesGetOwnPropertySlot | BooleanObject::StructureFlags;
+protected:
+    void finishCreation(ExecState*, JSGlobalObject*);
+    static const unsigned StructureFlags = OverridesGetOwnPropertySlot | BooleanObject::StructureFlags;
 
-    private:
-        BooleanPrototype(ExecState*, Structure*);
-        static bool getOwnPropertySlot(JSCell*, ExecState*, PropertyName, PropertySlot&);
+private:
+    BooleanPrototype(ExecState*, Structure*);
+    static bool getOwnPropertySlot(JSCell*, ExecState*, PropertyName, PropertySlot&);
 
-        static bool getOwnPropertyDescriptor(JSObject*, ExecState*, PropertyName, PropertyDescriptor&);
-    };
+    static bool getOwnPropertyDescriptor(JSObject*, ExecState*, PropertyName, PropertyDescriptor&);
+};
 
 } // namespace JSC
 
