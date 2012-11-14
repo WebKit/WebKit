ad31dee155794be04b7b91e6245fd68e3e72e6fa
Kent Tamura <tkent@chromium.org>
2012-11-14 10:05:15 +0000

    Support for localization tests of calendar picker https://bugs.webkit.org/show_bug.cgi?id=102181
    Reviewed by Kentaro Hara.
    
    Source/WebCore:
    
    Introduce DateTimeChooserParameters::locale to inform locale to
    DateTimeChooser implementations. However we pass defaultLanguage
    unless tests calls internals.settings.
    setLangAttributeAwareFormControlUIEnabled(true) explicitly.
    
    Test: platform/chromium/fast/forms/calendar-picker/calendar-picker-appearance-ru.html
    
    * html/HTMLInputElement.cpp:
    (WebCore::HTMLInputElement::setupDateTimeChooserParameters):
    Set DateTimeChooserParameters::locale up.
    * platform/DateTimeChooser.h:
    (DateTimeChooserParameters): Add 'locale' member.
    
    Source/WebKit/chromium:
    
    * src/DateTimeChooserImpl.cpp:
    Use DateTimeChooserParameters::locale.
    (WebKit::DateTimeChooserImpl::DateTimeChooserImpl):
    (WebKit::DateTimeChooserImpl::writeDocument):
    
    LayoutTests:
    
    * platform/chromium-mac/platform/chromium/fast/forms/calendar-picker/calendar-picker-appearance-ru-expected.png: Added.
    * platform/chromium/TestExpectations:
    * platform/chromium/fast/forms/calendar-picker/calendar-picker-appearance-ru-expected.txt: Added.
    * platform/chromium/fast/forms/calendar-picker/calendar-picker-appearance-ru.html: Added.
    
    Canonical link: https://commits.webkit.org/120304@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@134579 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLInputElement.cpp
+++ Source/WebCore/html/HTMLInputElement.cpp
@@ -51,11 +51,13 @@
 #include "IdTargetObserver.h"
 #include "InputType.h"
 #include "KeyboardEvent.h"
+#include "Language.h"
 #include "LocalizedStrings.h"
 #include "MouseEvent.h"
 #include "NumberInputType.h"
 #include "RenderTextControlSingleLine.h"
 #include "RenderTheme.h"
+#include "RuntimeEnabledFeatures.h"
 #include "ScopedEventQueue.h"
 #include "SearchInputType.h"
 #include "ShadowRoot.h"
@@ -1913,6 +1915,12 @@ bool HTMLInputElement::setupDateTimeChooserParameters(DateTimeChooserParameters&
     parameters.minimum = minimum();
     parameters.maximum = maximum();
     parameters.required = required();
+    if (!RuntimeEnabledFeatures::langAttributeAwareFormControlUIEnabled())
+        parameters.locale = defaultLanguage();
+    else {
+        AtomicString computedLocale = computeInheritedLanguage();
+        parameters.locale = computedLocale.isEmpty() ? AtomicString(defaultLanguage()) : computedLocale;
+    }
 
     StepRange stepRange = createStepRange(RejectAny);
     if (stepRange.hasStep()) {
