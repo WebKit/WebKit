2d2ab06e421ab350e7114ddb3fbc3a67da3d0078
Alec Flett <alecflett@chromium.org>
2012-11-16 01:07:26 +0000

    Add tests for explicit serialization values https://bugs.webkit.org/show_bug.cgi?id=96818
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Expose direct access to the serialization/deserialization mechanisms
    of SerializedScriptValue to DumpRenderTree.
    
    * testing/Internals.cpp:
    (WebCore::Internals::serializeObject):
    (WebCore):
    (WebCore::Internals::deserializeBuffer):
    * testing/Internals.h:
    (WebCore):
    * testing/Internals.idl:
    
    Source/WebKit2:
    
    Add exports for SerializedScriptValue changes.
    
    * win/WebKit2.def:
    * win/WebKit2CFLite.def:
    
    LayoutTests:
    
    Make sure SerializedScriptValues are backward compatible with future changes.
    
    * platform/chromium/fast/storage/serialized-script-value.html: Added.
    * platform/chromium/fast/storage/serialized-script-value.txt: Added.
    
    
    Canonical link: https://commits.webkit.org/120564@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@134865 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/js/SerializedScriptValue.h
+++ Source/WebCore/bindings/js/SerializedScriptValue.h
@@ -73,6 +73,7 @@ public:
     static PassRefPtr<SerializedScriptValue> create(JSContextRef, JSValueRef, JSValueRef* exception);
 
     static PassRefPtr<SerializedScriptValue> create(const String&);
+    static PassRefPtr<SerializedScriptValue> createFromWire(const String& value) { return create(value); }
     static PassRefPtr<SerializedScriptValue> adopt(Vector<uint8_t>& buffer)
     {
         return adoptRef(new SerializedScriptValue(buffer));
@@ -84,6 +85,7 @@ public:
     static PassRefPtr<SerializedScriptValue> booleanValue(bool value);
 
     String toString();
+    String toWireString() { return toString(); }
     
     JSC::JSValue deserialize(JSC::ExecState*, JSC::JSGlobalObject*, MessagePortArray*, SerializationErrorMode = Throwing);
     JSValueRef deserialize(JSContextRef, JSValueRef* exception, MessagePortArray*);
