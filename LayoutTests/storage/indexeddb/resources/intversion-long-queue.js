71f762d585cb5f2b1e8cba39074c550900fb4694
David Grogan <dgrogan@chromium.org>
2012-11-16 00:10:21 +0000

    IndexedDB: remove setVersion from intversion-long-queue https://bugs.webkit.org/show_bug.cgi?id=102322
    Reviewed by Tony Chang.
    
    This test probably doesn't provide as much value as it used to but
    tests like this, where there's a long sequence of interdependent
    events, have caught stuff for us in the past. There are endless
    variants that will all exercise our code differently.
    
    * storage/indexeddb/intversion-long-queue-expected.txt:
    * storage/indexeddb/resources/intversion-long-queue.js:
    (prepareDatabase):
    (connection1Success):
    (connection1VersionChangeCallback):
    (connection2Blocked):
    (deleteDatabaseBlockedCallback):
    (deleteDatabaseSuccessCallback):
    (connection2UpgradeNeeded):
    (connection2Success):
    (connection2TransactionComplete):
    (connection2VersionChangeEvent):
    (connection3UpgradeNeeded):
    (connection3Success):
    
    
    Canonical link: https://commits.webkit.org/120544@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@134844 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/storage/indexeddb/resources/intversion-long-queue.js
+++ LayoutTests/storage/indexeddb/resources/intversion-long-queue.js
@@ -3,44 +3,25 @@ if (this.importScripts) {
     importScripts('shared.js');
 }
 
-description("Test that a database is recreated correctly when an open-with-version call is queued behind both a deleteDatabase and setVersion call");
+description("Test that a database is recreated correctly when an open-with-version call is queued behind both a deleteDatabase and an open-with-version call");
 
-function test()
+indexedDBTest(prepareDatabase, connection1Success);
+function prepareDatabase(evt)
 {
-    removeVendorPrefixes();
-    setDBNameFromPath();
-    request = evalAndLog("indexedDB.deleteDatabase(dbname)");
-    request.onsuccess = deleteSuccess;
-    request.onerror = unexpectedErrorCallback;
-}
-
-function deleteSuccess(evt) {
     preamble(evt);
-    evalAndLog("request = indexedDB.open(dbname)");
-    evalAndLog("request.onsuccess = firstSuccessCallback");
-    request.onerror = unexpectedErrorCallback;
-    request.onblocked = unexpectedBlockedCallback;
+    evalAndLog("db = event.target.result");
 }
 
-function firstSuccessCallback(evt)
+function connection1Success(evt)
 {
     preamble(evt);
     evalAndLog("connection1 = event.target.result");
+    shouldBe("db", "connection1");
     evalAndLog("connection1.onversionchange = connection1VersionChangeCallback");
-    evalAndLog("request = indexedDB.open(dbname)");
-    evalAndLog("request.onsuccess = secondSuccessCallback");
-    request.onerror = unexpectedErrorCallback;
-    request.onblocked = unexpectedBlockedCallback;
-}
-
-function secondSuccessCallback(evt)
-{
-    preamble(evt);
-    evalAndLog("connection2 = event.target.result");
-    evalAndLog("connection2.onversionchange = connection2VersionChangeCallback");
-    evalAndLog("request = connection2.setVersion('version 2')");
-    evalAndLog("request.onblocked = setVersionBlockedCallback");
-    evalAndLog("request.onsuccess = setVersionSuccessCallback");
+    evalAndLog("request = indexedDB.open(dbname, 2)");
+    evalAndLog("request.onsuccess = connection2Success");
+    evalAndLog("request.onupgradeneeded = connection2UpgradeNeeded");
+    evalAndLog("request.onblocked = connection2Blocked");
     request.onerror = unexpectedErrorCallback;
 }
 
@@ -48,58 +29,31 @@ function connection1VersionChangeCallback(evt)
 {
     preamble(evt);
     shouldBeEqualToString("event.type", "versionchange");
-    shouldBeEqualToString("event.version", "version 2");
+    shouldBe("event.oldVersion", "1");
+    shouldBe("event.newVersion", "2");
+    shouldBeUndefined("event.version");
 }
 
-function setVersionBlockedCallback(evt)
+function connection2Blocked(evt)
 {
-    event = evt;
-    debug("");
-    debug("setVersionBlockedCallback():");
+    preamble(evt);
     evalAndLog("request = indexedDB.deleteDatabase(dbname)");
     evalAndLog("request.onblocked = deleteDatabaseBlockedCallback");
     evalAndLog("request.onsuccess = deleteDatabaseSuccessCallback");
     request.onerror = unexpectedErrorCallback;
 
-    evalAndLog("request = indexedDB.open(dbname, 1)");
-    evalAndLog("request.onupgradeneeded = upgradeNeededCallback");
-    evalAndLog("request.onsuccess = openWithVersionSuccessCallback");
-    request.onblocked = unexpectedBlockedCallback;
+    evalAndLog("request = indexedDB.open(dbname, 3)");
+    evalAndLog("request.onupgradeneeded = connection3UpgradeNeeded");
+    evalAndLog("request.onsuccess = connection3Success");
     request.onerror = unexpectedErrorCallback;
     evalAndLog("connection1.close()");
-}
-
-function setVersionSuccessCallback(evt)
-{
-    preamble(evt);
-    evalAndLog("transaction = event.target.result");
-    evalAndLog("db = transaction.db");
-    evalAndLog("db.createObjectStore('some object store')");
-    evalAndLog("transaction.oncomplete = setVersionTransactionCompleteCallback");
-    transaction.onabort = unexpectedAbortCallback;
-}
-
-function setVersionTransactionCompleteCallback(evt)
-{
-    preamble(evt);
-    shouldBeEqualToString("event.target.db.version", "version 2");
-}
-
-function connection2VersionChangeCallback(evt)
-{
-    preamble(evt);
-    shouldBeEqualToString("event.type", "versionchange");
-    debug("FIXME: Fire a versionchange event that has oldVersion and newVersion");
-    shouldBeEqualToString("event.oldVersion", "version 2");
-    shouldBeNull("event.newVersion");
-    shouldBeUndefined("event.version");
 }
 
 function deleteDatabaseBlockedCallback(evt)
 {
     preamble(evt);
-    evalAndLog("connection2.close()");
-    shouldBeEqualToString("event.oldVersion", "version 2");
+    debug("FIXME: http://crbug.com/153122");
+    shouldBe("event.oldVersion", "1");
     shouldBeNull("event.newVersion");
     shouldBeUndefined("event.version");
 }
@@ -107,26 +61,60 @@ function deleteDatabaseBlockedCallback(evt)
 function deleteDatabaseSuccessCallback(evt)
 {
     preamble(evt);
-    shouldBeNonNull("event.oldVersion");
-    shouldBeNull("event.newVersion");
     shouldBeUndefined("event.target.result");
+    debug("FIXME: http://crbug.com/153122");
+    shouldBe("event.oldVersion", "1");
+    shouldBeNull("event.newVersion");
+}
+
+function connection2UpgradeNeeded(evt)
+{
+    preamble(evt);
+    shouldBe("event.oldVersion", "0");
+    shouldBe("event.newVersion", "2");
+    evalAndLog("db = event.target.result");
+    shouldBe("db.objectStoreNames.length", "0");
+    evalAndLog("db.createObjectStore('some object store')");
+    evalAndLog("transaction = event.target.transaction");
+    evalAndLog("transaction.oncomplete = connection2TransactionComplete");
+}
+
+function connection2Success(evt)
+{
+    preamble(evt);
+    evalAndLog("connection2 = event.target.result");
+    evalAndLog("connection2.onversionchange = connection2VersionChangeEvent");
+}
+
+function connection2TransactionComplete(evt)
+{
+    preamble(evt);
+    shouldBe("db.version", "2");
+}
+
+function connection2VersionChangeEvent(evt)
+{
+    preamble(evt);
+    shouldBeEqualToString("event.type", "versionchange");
+    shouldBe("event.oldVersion", "2");
+    shouldBe("event.newVersion", "3");
+    shouldBeUndefined("event.version");
+    evalAndLog("connection2.close()");
 }
 
 var gotUpgradeNeededEvent = false;
-function upgradeNeededCallback(evt)
+function connection3UpgradeNeeded(evt)
 {
     preamble(evt);
     evalAndLog("gotUpgradeNeededEvent = true");
-    shouldBe("event.newVersion", "1");
-    shouldBe("event.oldVersion", "0");
+    shouldBe("event.newVersion", "3");
+    shouldBe("event.oldVersion", "2");
 }
 
-function openWithVersionSuccessCallback(evt)
+function connection3Success(evt)
 {
     preamble(evt);
     shouldBeTrue("gotUpgradeNeededEvent");
-    shouldBe("event.target.result.objectStoreNames.length", "0");
+    shouldBe("event.target.result.objectStoreNames.length", "1");
     finishJSTest();
 }
-
-test();
