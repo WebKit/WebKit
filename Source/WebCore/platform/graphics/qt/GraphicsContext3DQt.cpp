93bbce37a47cea4e83d7800e658ae0f231eab730
Zeno Albisser <zeno.albisser@digia.com>
2012-11-16 14:55:36 +0000

    [Qt] Adding a null pointer check for currentContext to GraphicsContext3DQt. https://bugs.webkit.org/show_bug.cgi?id=102360
    QOpenGLContext::currentContext() will return null, in case there is
    no current context. Therefore currentContext must be null-checked
    before it can be reused.
    Making a context current on a null-surface on the other hand is
    perfectly possible.
    
    Reviewed by Kenneth Rohde Christiansen.
    
    * platform/graphics/qt/GraphicsContext3DQt.cpp:
    (WebCore::GraphicsContext3DPrivate::blitMultisampleFramebufferAndRestoreContext):
    
    Canonical link: https://commits.webkit.org/120637@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@134945 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
+++ Source/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
@@ -319,12 +319,12 @@ void GraphicsContext3DPrivate::blitMultisampleFramebufferAndRestoreContext() con
 
     const QOpenGLContext* currentContext = QOpenGLContext::currentContext();
     QSurface* currentSurface = 0;
-    if (currentContext != m_platformContext) {
+    if (currentContext && currentContext != m_platformContext) {
         currentSurface = currentContext->surface();
         m_platformContext->makeCurrent(m_surface);
     }
     blitMultisampleFramebuffer();
-    if (currentSurface)
+    if (currentContext)
         const_cast<QOpenGLContext*>(currentContext)->makeCurrent(currentSurface);
 }
 
