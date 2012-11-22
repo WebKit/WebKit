27731d8d0773c0644fa3ce8b0ae32c738fb59745
James Simonsen <simonjam@chromium.org>
2012-11-22 00:13:31 +0000

    [Resource Timing] Create W3C-style test suite https://bugs.webkit.org/show_bug.cgi?id=84883
    Reviewed by Tony Gentilcore.
    
    The tests are designed to be upstreamed. Eventually, the local ones will be replaced by the official upstream versions.
    
    * http/tests/w3c/webperf/resources/Ahem.ttf: Added.
    * http/tests/w3c/webperf/resources/all_resource_types.htm: Added.
    * http/tests/w3c/webperf/resources/blank_image.png: Added.
    * http/tests/w3c/webperf/resources/empty_script.js: Added.
    * http/tests/w3c/webperf/resources/generate_resource.php: Added.
    * http/tests/w3c/webperf/resources/gray_bg.css: Added.
    (body):
    * http/tests/w3c/webperf/resources/green_frame.htm: Added.
    * http/tests/w3c/webperf/resources/inject_resource_test.html: Added.
    * http/tests/w3c/webperf/resources/nested.css: Added.
    (@font-face):
    (ol):
    * http/tests/w3c/webperf/resources/webperftestharness.js:
    (test_greater_or_equals):
    (test_fail):
    (test_resource_entries):
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_attribute_order-expected.txt: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_attribute_order.html: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_dynamic_insertion-expected.txt: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_dynamic_insertion.html: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_frame_initiator_type-expected.txt: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_frame_initiator_type.html: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_initiator_types-expected.txt: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_initiator_types.html: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_redirects-expected.txt: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_redirects.html: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_reparenting-expected.txt: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_reparenting.html: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_script_types-expected.txt: Added.
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_script_types.html: Added.
    * platform/chromium/TestExpectations: Skip these new tests until the flag is flipped.
    
    
    Canonical link: https://commits.webkit.org/121111@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@135450 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/http/tests/w3c/webperf/resources/generate_resource.php
@@ -0,0 +1,48 @@
+<?php
+  $type = $_GET["type"];
+
+  $response_code = 200;
+  $body = "";
+  $content_type = "text/plain";
+
+  switch ($type) {
+  case "audio":
+      $body = file_get_contents("silence.wav");
+      $content_type = "audio/wav";
+      break;
+
+  case "css":
+      $body = file_get_contents("gray_bg.css");
+      $content_type = "text/css";
+      break;
+
+  case "font":
+      $body = file_get_contents("Ahem.ttf");
+      $content_type = "application/octet-stream";
+      break;
+
+  case "iframe":
+      $body = file_get_contents("blank_page_green.htm");
+      $content_type = "text/html";
+      break;
+
+  case "image":
+      $body = file_get_contents("blank_image.png");
+      $content_type = "image/png";
+      break;
+
+  case "script":
+      $body = file_get_contents("empty_script.js");
+      $content_type = "text/javascript";
+      break;
+
+  default:
+      $response_code = 404;
+      break;
+  }
+
+  header("HTTP/1.1 $response_code");
+  header("Content-type: $content_type");
+  print($body);
+  exit;
+?>
