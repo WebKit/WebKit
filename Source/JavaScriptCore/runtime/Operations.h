0996cfa05e4bce64e57320cab5498baf005ce34a
Daniel Bates <dbates@webkit.org>
2012-11-22 00:25:51 +0000

    JavaScript fails to concatenate large strings <https://bugs.webkit.org/show_bug.cgi?id=102963>
    Reviewed by Michael Saboff.
    
    Source/JavaScriptCore:
    
    Fixes an issue where we inadvertently didn't check the length of
    a JavaScript string for overflow.
    
    * runtime/Operations.h:
    (JSC::jsString):
    (JSC::jsStringFromArguments):
    
    LayoutTests:
    
    Add tests to ensure that we handle concatenating large strings.
    
    * fast/js/concat-large-strings-crash-expected.txt: Added.
    * fast/js/concat-large-strings-crash.html: Added.
    * fast/js/concat-large-strings-crash2-expected.txt: Added.
    * fast/js/concat-large-strings-crash2.html: Added.
    
    
    Canonical link: https://commits.webkit.org/121115@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@135454 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/Operations.h
+++ Source/JavaScriptCore/runtime/Operations.h
@@ -86,6 +86,7 @@ namespace JSC {
 
             if (ropeBuilder.length() < oldLength) // True for overflow
                 return throwOutOfMemoryError(exec);
+            oldLength = ropeBuilder.length();
         }
 
         return ropeBuilder.release();
@@ -105,6 +106,7 @@ namespace JSC {
 
             if (ropeBuilder.length() < oldLength) // True for overflow
                 return throwOutOfMemoryError(exec);
+            oldLength = ropeBuilder.length();
         }
 
         return ropeBuilder.release();
