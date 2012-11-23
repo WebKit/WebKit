286c70fa3da4073d2c0e5dcf434c396412c2ad42
Rafael Brandao <rafael.lobo@openbossa.org>
2012-11-23 05:36:38 +0000

    Remove PLATFORM(HAIKU) leftovers code https://bugs.webkit.org/show_bug.cgi?id=103086
    Reviewed by Antti Koivisto.
    
    * platform/PlatformWheelEvent.h:
    (PlatformWheelEvent):
    
    
    Canonical link: https://commits.webkit.org/121218@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@135557 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/PlatformWheelEvent.h
+++ Source/WebCore/platform/PlatformWheelEvent.h
@@ -45,10 +45,6 @@ class wxMouseEvent;
 class wxPoint;
 #endif
 
-#if PLATFORM(HAIKU)
-class BMessage;
-#endif
-
 namespace WebCore {
 
     class FloatPoint;
@@ -181,10 +177,6 @@ namespace WebCore {
 
 #if PLATFORM(WX)
         PlatformWheelEvent(const wxMouseEvent&, const wxPoint&);
-#endif
-
-#if PLATFORM(HAIKU)
-        explicit PlatformWheelEvent(BMessage*);
 #endif
 
     protected:
