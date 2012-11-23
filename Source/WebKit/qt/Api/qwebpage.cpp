WebKit/WebKit

commit 2a2ab5549f7838a99f83cdb1948d50b05dafc18f

[Qt] Separate QWidget dependant code into separate WebKitWidgets static library https://bugs.webkit.org/show_bug.cgi?id=102800
Patch by Simon Hausmann  <simon.hausmann@digia.com>, Pierre Rossi <pierre.rossi@digia.com> on 2012-11-22
Reviewed by Tor Arne Vestbø.

This patch separates code that needs to use QWidget related APIs in
WebKit/qt/WebCoreSupport and Api from code that doesn't. This means for
example FrameLoaderClientQt.cpp remains in the WebKit1 static library,
while qwebpage.cpp and qwebframe.cpp become part of the WebKitWidgets
static library. WebKit1 is compiled without QT += widgets and therefore
any widget related dependency has been moved "up" and out of WebKit1 into
the WebKitWidgets library.

Between the code in WebKit.a and WebKitWidgets.a new adapters and
interfaces have been introduced, such as QWebPageAdapter and
QWebFrameAdapter. QWebPageAdapter, when used from WebKit1, is a way to
call out into the API layer, implemented by QWebPage (QWebPagePrivate).
The other way around if QWebPage wants to access WebCore or
WebCoreSupport related functionality, it will go through
QWebPageAdapater (as base class). The separation in the direction up
into the API layer is complete with this patch, no code in WebKit1
depends on QtWidgets. The separation the other way around, code in the
API layer not using any WebCore types, is not complete yet.

Some classes such as QWebSettings, QWebElement or
DumpRenderTreeSupportQt remain in WebKit1. While they are API layer,
they do not depend on widget related Qt APIs and they make much more
use of WebCore internal APIs and therefore are easier to keep in
WebKit1.

In the future we plan to place a real shared library boundary between
WebKit1 and WebKitWidgets, by keeping the WebKit1 static library as
part of the QtWebKit shared library and by turning the WebKitWidgets
static library into a shared one.

.:

* Source/api.pri:
* WebKit.pro:

Source/WebCore:

* platform/qt/QWebPageClient.h:
(QWebPageClient):

Source/WebKit:

Source/WebKit/qt/WebCoreSupport/InitWebCoreQt.cpp
@@ -44,19 +44,31 @@
 
 #include "qwebelement_p.h"
 #include <JavaScriptCore/runtime/InitializeThreading.h>
-#include <QApplication>
-#include <QStyle>
 #include <wtf/MainThread.h>
 
 namespace WebKit {
 
-// Called also from WebKit2's WebProcess.
-Q_DECL_EXPORT void initializeWebKit2Theme()
+static QtStyleFactoryFunction initCallback = 0;
+
+void setWebKitWidgetsInitCallback(QtStyleFactoryFunction callback)
 {
-    if (qgetenv("QT_WEBKIT_THEME_NAME") == "qstyle") {
-        WebCore::RenderThemeQStyle::setStyleFactoryFunction(WebKit::QStyleFacadeImp::create);
+    initCallback = callback;
+}
+
+// Called also from WebKit2's WebProcess
+Q_DECL_EXPORT void initializeWebKitQt()
+{
+    if (initCallback) {
+        WebCore::RenderThemeQStyle::setStyleFactoryFunction(initCallback);
         WebCore::RenderThemeQt::setCustomTheme(WebCore::RenderThemeQStyle::create, new WebCore::ScrollbarThemeQStyle);
     }
+
+    WebCore::initializeWebCoreQt();
+}
+
+Q_DECL_EXPORT void setImagePlatformResource(const char* name, const QPixmap& pixmap)
+{
+    WebCore::Image::setPlatformResource(name, pixmap);
 }
 
 }
@@ -79,17 +91,9 @@ void initializeWebCoreQt()
     PlatformStrategiesQt::initialize();
     QtWebElementRuntime::initialize();
 
-    RenderThemeQStyle::setStyleFactoryFunction(WebKit::QStyleFacadeImp::create);
-    RenderThemeQt::setCustomTheme(RenderThemeQStyle::create, new ScrollbarThemeQStyle);
-
 #if USE(QTKIT)
     InitWebCoreSystemInterface();
 #endif
-
-    // QWebSettings::SearchCancelButtonGraphic
-    Image::setPlatformResource("searchCancelButton", QApplication::style()->standardPixmap(QStyle::SP_DialogCloseButton));
-    // QWebSettings::SearchCancelButtonPressedGraphic
-    Image::setPlatformResource("searchCancelButtonPressed", QApplication::style()->standardPixmap(QStyle::SP_DialogCloseButton));
 
     initialized = true;
 }

Source/WebKit2/qt/MainQt.cpp
@@ -42,9 +42,10 @@ typedef QApplication ApplicationType;
 
 namespace WebKit {
 Q_DECL_IMPORT int WebProcessMainQt(QGuiApplication*);
-#if defined(HAVE_WEBKIT1)
-Q_DECL_IMPORT void initializeWebKit2Theme();
+#if !defined(QT_NO_WIDGETS)
+Q_DECL_IMPORT void initializeWebKitWidgets();
 #endif
+Q_DECL_IMPORT void initializeWebKitQt();
 }
 
 #if !defined(NDEBUG) && defined(Q_OS_UNIX)
@@ -82,15 +83,23 @@ int main(int argc, char** argv)
     }
 #endif
 
-#if defined(HAVE_WEBKIT1)
-    WebKit::initializeWebKit2Theme();
-#endif
-
     // Has to be done before QApplication is constructed in case
     // QApplication itself produces debug output.
     QByteArray suppressOutput = qgetenv("QT_WEBKIT_SUPPRESS_WEB_PROCESS_OUTPUT");
     if (!suppressOutput.isEmpty() && suppressOutput != "0")
         qInstallMessageHandler(messageHandler);
 
-    return WebKit::WebProcessMainQt(new ApplicationType(argc, argv));
+    // QApplication must be created before we call initializeWebKitWidgets() so that
+    // the standard pixmaps can be fetched from the style.
+    ApplicationType* appInstance = new ApplicationType(argc, argv);
+
+#if !defined(QT_NO_WIDGETS)
+    if (qgetenv("QT_WEBKIT_THEME_NAME") == "qstyle")
+        WebKit::initializeWebKitWidgets();
+    else
+#endif
+        WebKit::initializeWebKitQt();
+
+
+    return WebKit::WebProcessMainQt(appInstance);
 }

commit 031dddb636d8eb5494d2006ea2589d211e7cfc37

[Qt] Avoid initializing PlatformStrategies twice.
Rubber-stamped by Simon Hausmann.

This fixes another regression introduced in r135515:
initializeWebKitQt shouldn't implicitely call initializeWebCoreQt
since it can be called from WebKit2 to initialize QStyle for testing.
This would then lead to things such as PlatformStrategies being
initialized twice.

Source/WebKit/qt:

* Api/qwebpage.cpp: Explicitely call initializeWebCoreQt().
(QWebPagePrivate::QWebPagePrivate):
* WebCoreSupport/InitWebCoreQt.cpp:
(WebKit::initializeWebKitQt):

Source/WebKit2:

* qt/MainQt.cpp: No need to initialize anything if we're not using QStyle.
(WebKit):
(main):

Canonical link: https://commits.webkit.org/121236@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@135575 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/qt/Api/qwebpage.cpp
@@ -68,6 +68,7 @@
 #include "HTMLNames.h"
 #include "HitTestResult.h"
 #include "Image.h"
+#include "InitWebCoreQt.h"
 #include "InitWebKitQt.h"
 #include "InspectorClientQt.h"
 #include "InspectorClientWebPage.h"
@@ -325,6 +326,7 @@ QWebPagePrivate::QWebPagePrivate(QWebPage *qq)
 #endif
 
     WebKit::initializeWebKitWidgets();
+    WebCore::initializeWebCoreQt();
 
     Page::PageClients pageClients;
     pageClients.chromeClient = new ChromeClientQt(this);

Source/WebKit/qt/WebCoreSupport/InitWebCoreQt.cpp
@@ -62,8 +62,6 @@ Q_DECL_EXPORT void initializeWebKitQt()
         WebCore::RenderThemeQStyle::setStyleFactoryFunction(initCallback);
         WebCore::RenderThemeQt::setCustomTheme(WebCore::RenderThemeQStyle::create, new WebCore::ScrollbarThemeQStyle);
     }
-
-    WebCore::initializeWebCoreQt();
 }
 
 Q_DECL_EXPORT void setImagePlatformResource(const char* name, const QPixmap& pixmap)

Source/WebKit2/qt/MainQt.cpp
@@ -45,7 +45,6 @@ Q_DECL_IMPORT int WebProcessMainQt(QGuiApplication*);
 #if !defined(QT_NO_WIDGETS)
 Q_DECL_IMPORT void initializeWebKitWidgets();
 #endif
-Q_DECL_IMPORT void initializeWebKitQt();
 }
 
 #if !defined(NDEBUG) && defined(Q_OS_UNIX)
@@ -96,10 +95,7 @@ int main(int argc, char** argv)
 #if !defined(QT_NO_WIDGETS)
     if (qgetenv("QT_WEBKIT_THEME_NAME") == "qstyle")
         WebKit::initializeWebKitWidgets();
-    else
 #endif
-        WebKit::initializeWebKitQt();
-
 
     return WebKit::WebProcessMainQt(appInstance);
 }