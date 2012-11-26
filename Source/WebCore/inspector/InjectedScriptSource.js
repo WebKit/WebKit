934cde3993d3af5a3349614cbe710c4a32825639
Pavel Feldman <pfeldman@chromium.org>
2012-11-26 15:17:17 +0000

    Web Inspector: object preview does not render node id, className; logs too many functions for jQuery. https://bugs.webkit.org/show_bug.cgi?id=103222
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    - Added node class name and id into the preview
    - Now keeps track of properties separately from array indexes.
    
    * inspector/InjectedScriptSource.js:
    (.):
    * inspector/InspectorOverlayPage.html:
    * inspector/front-end/ConsoleMessage.js:
    (WebInspector.ConsoleMessageImpl.prototype._appendObjectPreview):
    (WebInspector.ConsoleMessageImpl.prototype._appendPropertyPreview):
    
    LayoutTests:
    
    * inspector/console/command-line-api-expected.txt:
    * inspector/console/command-line-api-inspect-expected.txt:
    * inspector/console/console-dir-expected.txt:
    * inspector/console/console-format-collections-expected.txt:
    * inspector/console/console-format-expected.txt:
    * inspector/elements/event-listener-sidebar-expected.txt:
    * inspector/elements/event-listeners-about-blank-expected.txt:
    * platform/chromium-linux/inspector/console/console-format-collections-expected.txt: Removed.
    * platform/chromium/inspector/console/command-line-api-inspect-expected.txt:
    * platform/chromium/inspector/console/console-dir-expected.txt:
    * platform/chromium/inspector/console/console-format-collections-expected.txt:
    
    Canonical link: https://commits.webkit.org/121380@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@135720 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/InjectedScriptSource.js
+++ Source/WebCore/inspector/InjectedScriptSource.js
@@ -723,7 +723,9 @@ InjectedScript.prototype = {
             var description = obj.nodeName.toLowerCase();
             switch (obj.nodeType) {
             case 1 /* Node.ELEMENT_NODE */:
-                description =  "<" + description + ">";
+                description += obj.id ? "#" + obj.id : "";
+                var className = obj.className;
+                description += className ? "." + className : "";
                 break;
             case 10 /*Node.DOCUMENT_TYPE_NODE */:
                 description = "<!DOCTYPE " + description + ">";
@@ -820,22 +822,25 @@ InjectedScript.RemoteObject.prototype = {
         this.preview.properties = [];
 
         var isArray = this.subtype === "array";
-        var elementsToDump = isArray ? 100 : 5;
+        var propertiesThreshold = {
+            properties: 5,
+            indexes: 100
+        };
 
         for (var o = object; injectedScript._isDefined(o); o = o.__proto__)
-            this._generateProtoPreview(o, elementsToDump);
+            this._generateProtoPreview(o, propertiesThreshold);
     },
 
     /**
      * @param {Object} object
-     * @param {number} elementsToDump
+     * @param {Object} propertiesThreshold
      */
-    _generateProtoPreview: function(object, elementsToDump)
+    _generateProtoPreview: function(object, propertiesThreshold)
     {
         var propertyNames = Object.keys(/** @type {!Object} */(object));
         try {
             for (var i = 0; i < propertyNames.length; ++i) {
-                if (this.preview.properties.length >= elementsToDump) {
+                if (!propertiesThreshold.properties || !propertiesThreshold.indexes) {
                     this.preview.overflow = true;
                     this.preview.lossless = false;
                     break;
@@ -852,7 +857,7 @@ InjectedScript.RemoteObject.prototype = {
 
                 var value = descriptor.value;
                 if (value === null) {
-                    this.preview.properties.push({ name: name, type: "object", value: "null" });
+                    this._appendPropertyPreview({ name: name, type: "object", value: "null" }, propertiesThreshold);
                     continue;
                 }
     
@@ -867,7 +872,7 @@ InjectedScript.RemoteObject.prototype = {
                         }
                         value = "\"" + value.replace(/\n/g, "\u21B5") + "\"";
                     }
-                    this.preview.properties.push({ name: name, type: type, value: value + "" });
+                    this._appendPropertyPreview({ name: name, type: type, value: value + "" }, propertiesThreshold);
                     continue;
                 }
     
@@ -881,10 +886,23 @@ InjectedScript.RemoteObject.prototype = {
                 var property = { name: name, type: type, value: description };
                 if (subtype)
                     property.subtype = subtype;
-                this.preview.properties.push(property);
+                this._appendPropertyPreview(property, propertiesThreshold);
             }
         } catch (e) {
         }
+    },
+
+    /**
+     * @param {Object} property
+     * @param {Object} propertiesThreshold
+     */
+    _appendPropertyPreview: function(property, propertiesThreshold)
+    {
+        if (isNaN(property.name))
+            propertiesThreshold.properties--;
+        else
+            propertiesThreshold.indexes--;
+        this.preview.properties.push(property);
     },
 
     /**
