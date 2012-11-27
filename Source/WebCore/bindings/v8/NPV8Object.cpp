dbdba25c6f3a0f7bac2f16123faefb42d077c3e4
Istiaque Ahmed <lazyboy@chromium.org>
2012-11-27 03:40:59 +0000

    Check for empty perContextData while creating NP V8 Object. https://bugs.webkit.org/show_bug.cgi?id=98448
    Patch by Istiaque Ahmed <lazyboy@chromium.org> on 2012-11-26
    Reviewed by Adam Barth.
    
    Fixes crash in npCreateV8ScriptObject(), if NP Invoke is called from a document
    that is no longer displayed in frame (isCurrentlyDisplayedInFrame() ==
    false), we have empty perContextData and this results in invalid memory access.
    
    Source/WebCore:
    
    Test: platform/chromium/plugins/empty-per-context-data.html
    
    * bindings/v8/NPV8Object.cpp:
    (WebCore::npCreateV8ScriptObject):
    
    LayoutTests:
    
    * platform/chromium/plugins/empty-per-context-data-expected.txt: Added.
    * platform/chromium/plugins/empty-per-context-data.html: Added.
    * platform/chromium/plugins/resources/script-container.html: Added.
    
    Canonical link: https://commits.webkit.org/121454@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@135804 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/NPV8Object.cpp
+++ Source/WebCore/bindings/v8/NPV8Object.cpp
@@ -144,29 +144,33 @@ NPObject* npCreateV8ScriptObject(NPP npp, v8::Handle<v8::Object> object, DOMWind
         }
     }
 
-    int v8ObjectHash = object->GetIdentityHash();
-    ASSERT(v8ObjectHash);
-    V8NPObjectMap* v8NPObjectMap = V8PerContextData::from(object->CreationContext())->v8NPObjectMap();
-    V8NPObjectMap::iterator iter = v8NPObjectMap->find(v8ObjectHash);
-    if (iter != v8NPObjectMap->end()) {
-        V8NPObjectVector& objects = iter->value;
-        for (size_t index = 0; index < objects.size(); ++index) {
-            V8NPObject* v8npObject = objects.at(index);
-            if (v8npObject->rootObject == root) {
-                ASSERT(v8npObject->v8Object == object);
-                _NPN_RetainObject(&v8npObject->object);
-                return reinterpret_cast<NPObject*>(v8npObject);
+    V8NPObjectVector* objectVector = 0;
+    if (V8PerContextData* perContextData = V8PerContextData::from(object->CreationContext())) {
+        int v8ObjectHash = object->GetIdentityHash();
+        ASSERT(v8ObjectHash);
+        V8NPObjectMap* v8NPObjectMap = perContextData->v8NPObjectMap();
+        V8NPObjectMap::iterator iter = v8NPObjectMap->find(v8ObjectHash);
+        if (iter != v8NPObjectMap->end()) {
+            V8NPObjectVector& objects = iter->value;
+            for (size_t index = 0; index < objects.size(); ++index) {
+                V8NPObject* v8npObject = objects.at(index);
+                if (v8npObject->rootObject == root) {
+                    ASSERT(v8npObject->v8Object == object);
+                    _NPN_RetainObject(&v8npObject->object);
+                    return reinterpret_cast<NPObject*>(v8npObject);
+                }
             }
+        } else {
+            iter = v8NPObjectMap->set(v8ObjectHash, V8NPObjectVector()).iterator;
+            objectVector = &iter->value;
         }
-    } else {
-        iter = v8NPObjectMap->set(v8ObjectHash, V8NPObjectVector()).iterator;
     }
-
     V8NPObject* v8npObject = reinterpret_cast<V8NPObject*>(_NPN_CreateObject(npp, &V8NPObjectClass));
     v8npObject->v8Object = v8::Persistent<v8::Object>::New(object);
     v8npObject->rootObject = root;
 
-    iter->value.append(v8npObject);
+    if (objectVector)
+        objectVector->append(v8npObject);
 
     return reinterpret_cast<NPObject*>(v8npObject);
 }
