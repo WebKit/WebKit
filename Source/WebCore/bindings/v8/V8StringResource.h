456ae1e497032eb12307b72e91f9e30df8f9c351
Kentaro Hara <haraken@chromium.org>
2012-11-27 15:50:07 +0000

    [V8] Remove V8StringResource::m_string https://bugs.webkit.org/show_bug.cgi?id=103367
    Reviewed by Adam Barth.
    
    This patch cleans up V8StringResource and removes V8StringResource::m_string.
    This patch won't change performance. (This patch just adds one redundant IsString()
    check and one IsInt32() check.) I confirmed no performance regression in micro benchmarks
    (https://bugs.webkit.org/attachment.cgi?id=176178).
    
    No tests. No change in behavior.
    
    * bindings/v8/V8Binding.cpp:
    (WebCore::v8NonStringValueToWebCoreString):
    * bindings/v8/V8StringResource.cpp:
    (WebCore::int32ToWebCoreStringFast):
    (WebCore::String):
    (WebCore::AtomicString):
    (WebCore):
    * bindings/v8/V8StringResource.h:
    (WebCore::V8StringResource::V8StringResource):
    (WebCore::V8StringResource::prepareBase):
    (WebCore::V8StringResource::toString):
    (V8StringResource):
    (WebCore::::prepare):
    
    
    Canonical link: https://commits.webkit.org/121509@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@135862 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/V8StringResource.h
+++ Source/WebCore/bindings/v8/V8StringResource.h
@@ -142,7 +142,8 @@ enum ExternalMode {
 
 template <typename StringType>
 StringType v8StringToWebCoreString(v8::Handle<v8::String>, ExternalMode);
-String int32ToWebCoreString(int value);
+template <typename StringType>
+StringType int32ToWebCoreString(int value);
 
 // V8StringResource is an adapter class that converts V8 values to Strings
 // or AtomicStrings as appropriate, using multiple typecast operators.
@@ -158,7 +159,6 @@ public:
     V8StringResource(v8::Local<v8::Value> object)
         : m_v8Object(object)
         , m_mode(Externalize)
-        , m_string()
     {
     }
 
@@ -169,16 +169,9 @@ public:
 private:
     bool prepareBase()
     {
-        if (m_v8Object.IsEmpty())
+        ASSERT(!m_v8Object.IsEmpty());
+        if (LIKELY(m_v8Object->IsString() || m_v8Object->IsInt32()))
             return true;
-
-        if (LIKELY(m_v8Object->IsString()))
-            return true;
-
-        if (LIKELY(m_v8Object->IsInt32())) {
-            setString(int32ToWebCoreString(m_v8Object->Int32Value()));
-            return true;
-        }
 
         m_mode = DoNotExternalize;
         v8::TryCatch block;
@@ -191,35 +184,32 @@ private:
         return true;
     }
 
-    void setString(const String& string)
-    {
-        m_string = string;
-        m_v8Object.Clear(); // To signal that String is ready.
-    }
-
     template <class StringType>
     StringType toString()
     {
-        if (LIKELY(!m_v8Object.IsEmpty()))
-            return v8StringToWebCoreString<StringType>(m_v8Object.As<v8::String>(), m_mode);
-
-        return StringType(m_string);
+        if (m_v8Object.IsEmpty())
+            return StringType();
+        if (m_v8Object->IsInt32())
+            return int32ToWebCoreString<StringType>(m_v8Object->Int32Value());
+        ASSERT(m_v8Object->IsString());
+        return v8StringToWebCoreString<StringType>(m_v8Object.As<v8::String>(), m_mode);
     }
 
     v8::Local<v8::Value> m_v8Object;
     ExternalMode m_mode;
-    String m_string;
 };
 
 template<> inline bool V8StringResource<DefaultMode>::prepare()
 {
+    if (m_v8Object.IsEmpty())
+        return true;
     return prepareBase();
 }
 
 template<> inline bool V8StringResource<WithNullCheck>::prepare()
 {
     if (m_v8Object.IsEmpty() || m_v8Object->IsNull()) {
-        setString(String());
+        m_v8Object.Clear();
         return true;
     }
     return prepareBase();
@@ -228,7 +218,7 @@ template<> inline bool V8StringResource<WithNullCheck>::prepare()
 template<> inline bool V8StringResource<WithUndefinedOrNullCheck>::prepare()
 {
     if (m_v8Object.IsEmpty() || m_v8Object->IsNull() || m_v8Object->IsUndefined()) {
-        setString(String());
+        m_v8Object.Clear();
         return true;
     }
     return prepareBase();
