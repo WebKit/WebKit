8e919ba892ab7c058b5c471381f5f1e08a10c532
Robert Hogan <robert@webkit.org>
2012-11-27 19:15:04 +0000

    REGRESSION (r125578): word-wrapping in absolute position with nbsp, word-spacing and custom font https://bugs.webkit.org/show_bug.cgi?id=101443
    Reviewed by David Hyatt.
    
    Source/WebCore:
    
    Leading non-breaking space in a text run should always get word-spacing applied. When RenderText
    calculates the preferred with of a run of text it breaks it up into word segments that contain
    no breaking spaces and calculates the width of each. So when calculating the width of '&nbsp;b'
    it needs to add word-spacing to the leading non-breaking space as it does not get collapsed away.
    
    Test: fast/text/word-space-nbsp.html
    
    * platform/graphics/WidthIterator.cpp:
    (WebCore::WidthIterator::advanceInternal):
    
    LayoutTests:
    
    * fast/text/word-space-nbsp-expected.html: Added.
    * fast/text/word-space-nbsp.html: Added.
    
    
    Canonical link: https://commits.webkit.org/121530@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@135884 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/WidthIterator.cpp
+++ Source/WebCore/platform/graphics/WidthIterator.cpp
@@ -241,7 +241,7 @@ inline unsigned WidthIterator::advanceInternal(TextIterator& textIterator, Glyph
 
                 // Account for word spacing.
                 // We apply additional space between "words" by adding width to the space character.
-                if (treatAsSpace && (character != '\t' || !m_run.allowTabs()) && textIterator.currentCharacter() && m_font->wordSpacing())
+                if (treatAsSpace && (character != '\t' || !m_run.allowTabs()) && (textIterator.currentCharacter() || character == noBreakSpace) && m_font->wordSpacing())
                     width += m_font->wordSpacing();
             } else
                 m_isAfterExpansion = false;
