c777391f8c27947d870737edd07335e1543b8612
Alexandru Chiculita <achicu@adobe.com>
2012-11-28 20:05:47 +0000

    [CSS Regions] Auto-height regions will not calculate the height correctly when the content changes dynamically https://bugs.webkit.org/show_bug.cgi?id=102954
    Reviewed by David Hyatt.
    
    Source/WebCore:
    
    Whenever the flow changes, we need to reset the region computed auto-heights.
    However, when we reset the region heights to "auto" or "un-computed", we also need to make
    sure that all the region breaks are counted again, so that we split regions correctly during the new layout.
    I've reused the m_regionsInvalidated property to force the flow do a complete layout.
    
    Test: fast/regions/autoheight-dynamic-update.html
    
    * rendering/RenderFlowThread.cpp:
    (WebCore::RenderFlowThread::resetRegionsOverrideLogicalContentHeight):
    
    LayoutTests:
    
    Added test to check for auto-height region size update after an element is added to the flow.
    In this case the first region will need to resize to accommodate the size of the added elements.
    
    * fast/regions/autoheight-dynamic-update-expected.html: Added.
    * fast/regions/autoheight-dynamic-update.html: Added.
    
    
    Canonical link: https://commits.webkit.org/121668@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@136039 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderFlowThread.cpp
+++ Source/WebCore/rendering/RenderFlowThread.cpp
@@ -737,7 +737,7 @@ void RenderFlowThread::resetRegionsOverrideLogicalContentHeight()
 
     // We need to reset the override logical content height for regions with auto logical height
     // only if the flow thread content needs layout.
-    if (!selfNeedsLayout())
+    if (!needsLayout())
         return;
 
     // FIXME: optimize this to iterate the region chain only if the flow thread has auto logical height
@@ -753,6 +753,9 @@ void RenderFlowThread::resetRegionsOverrideLogicalContentHeight()
         // as we are already inside layout.
         region->setNeedsLayout(true);
     }
+    // Make sure we don't skip any region breaks when we do the layout again.
+    // Using m_regionsInvalidated to force all the RenderFlowThread children do the layout again.
+    m_regionsInvalidated = true;
 }
 
 void RenderFlowThread::markAutoLogicalHeightRegionsForLayout()
