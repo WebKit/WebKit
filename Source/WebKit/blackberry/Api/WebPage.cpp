28a940f757bfc2b3a77bcdc0a6c260f604bf34e7
Andrew Lo <anlo@rim.com>
2012-11-29 21:25:25 +0000

    [BlackBerry] Do not adjust scroll position on viewport resizes that are not orientation change related https://bugs.webkit.org/show_bug.cgi?id=103659
    Patch by Andrew Lo <anlo@rim.com> on 2012-11-29
    Reviewed by Rob Buis.
    Internally reviewed by Gen Mak.
    
    Internal PR253893.
    Only adjust scroll position during an orientation change
    when setting viewport size.
    We don't want the scroll position jumping around when opening
    & closing the VKB.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::setViewportSize):
    
    Canonical link: https://commits.webkit.org/121777@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@136157 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/blackberry/Api/WebPage.cpp
+++ Source/WebKit/blackberry/Api/WebPage.cpp
@@ -3635,9 +3635,11 @@ void WebPagePrivate::setViewportSize(const IntSize& transformedActualVisibleSize
 
     IntSize viewportSizeAfter = actualVisibleSize();
 
-    IntSize offset(
-        roundf((viewportSizeBefore.width() - viewportSizeAfter.width()) / 2.0),
-        roundf((viewportSizeBefore.height() - viewportSizeAfter.height()) / 2.0));
+    IntSize offset;
+    if (hasPendingOrientation) {
+        offset = IntSize(roundf((viewportSizeBefore.width() - viewportSizeAfter.width()) / 2.0),
+            roundf((viewportSizeBefore.height() - viewportSizeAfter.height()) / 2.0));
+    }
 
     // As a special case, if we were anchored to the top left position at
     // the beginning of the rotation then preserve that anchor.
