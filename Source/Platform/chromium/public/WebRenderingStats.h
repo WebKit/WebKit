db7fc6a79940a26021039b109bd9fa97a57e6ee4
Glenn Hartmann <hartmanng@chromium.org>
2012-12-04 16:52:01 +0000

    Gut WebRenderingStats since we no longer use its member variables. https://bugs.webkit.org/show_bug.cgi?id=103460
    Patch by Glenn Hartmann <hartmanng@chromium.org> on 2012-12-04
    Reviewed by Darin Fisher.
    
    This was used as an intermediate location to move stats from
    chromium's RenderStats. The chromium code has been updated to
    remove this dependency (see https://codereview.chromium.org/11364221/).
    
    * chromium/public/WebRenderingStats.h:
    (WebKit::WebRenderingStats::WebRenderingStats):
    
    Canonical link: https://commits.webkit.org/122099@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@136511 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/Platform/chromium/public/WebRenderingStats.h
+++ Source/Platform/chromium/public/WebRenderingStats.h
@@ -29,59 +29,8 @@
 namespace WebKit {
 
 struct WebRenderingStats {
-    int numAnimationFrames;
-    int numFramesSentToScreen;
-    int droppedFrameCount;
-    double totalPaintTimeInSeconds;
-    double totalRasterizeTimeInSeconds;
-    double totalCommitTimeInSeconds;
-    size_t totalCommitCount;
-    size_t totalPixelsPainted;
-    size_t totalPixelsRasterized;
-    size_t numImplThreadScrolls;
-    size_t numMainThreadScrolls;
-
     WebRenderingStats()
-        : numAnimationFrames(0)
-        , numFramesSentToScreen(0)
-        , droppedFrameCount(0)
-        , totalPaintTimeInSeconds(0)
-        , totalRasterizeTimeInSeconds(0)
-        , totalCommitTimeInSeconds(0)
-        , totalCommitCount(0)
-        , totalPixelsPainted(0)
-        , totalPixelsRasterized(0)
-        , numImplThreadScrolls(0)
-        , numMainThreadScrolls(0)
     {
-    }
-
-    // In conjunction with enumerateFields, this allows the embedder to
-    // enumerate the values in this structure without
-    // having to embed references to its specific member variables. This
-    // simplifies the addition of new fields to this type.
-    class Enumerator {
-    public:
-        virtual void addInt(const char* name, int value) = 0;
-        virtual void addDouble(const char* name, double value) = 0;
-    protected:
-        virtual ~Enumerator() { }
-    };
-
-    // Outputs the fields in this structure to the provided enumerator.
-    void enumerateFields(Enumerator* enumerator) const
-    {
-        enumerator->addInt("numAnimationFrames", numAnimationFrames);
-        enumerator->addInt("numFramesSentToScreen", numFramesSentToScreen);
-        enumerator->addInt("droppedFrameCount", droppedFrameCount);
-        enumerator->addDouble("totalPaintTimeInSeconds", totalPaintTimeInSeconds);
-        enumerator->addDouble("totalRasterizeTimeInSeconds", totalRasterizeTimeInSeconds);
-        enumerator->addDouble("totalCommitTimeInSeconds", totalCommitTimeInSeconds);
-        enumerator->addInt("totalCommitCount", totalCommitCount);
-        enumerator->addInt("totalPixelsPainted", totalPixelsPainted);
-        enumerator->addInt("totalPixelsRasterized", totalPixelsRasterized);
-        enumerator->addInt("numImplThreadScrolls", numImplThreadScrolls);
-        enumerator->addInt("numMainThreadScrolls", numMainThreadScrolls);
     }
 };
 
