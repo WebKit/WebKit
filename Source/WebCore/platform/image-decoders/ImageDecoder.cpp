82a202f538546662c0112a4757b453562d2ebfd6
Pierre Rossi <pierre.rossi@gmail.com>
2012-12-04 00:11:16 +0000

    [Qt] Unreviewed build fix after r136410
    When building without libjpeg, take JPEGImageDecoder's header out, since it now includes libjpeg.h.
    
    * Target.pri: conditionally add JPEGImageDecoder.h to HEADERS
    * platform/image-decoders/ImageDecoder.cpp: guard JPEGImageDecoder.h's include
    
    Canonical link: https://commits.webkit.org/122042@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@136453 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/image-decoders/ImageDecoder.cpp
+++ Source/WebCore/platform/image-decoders/ImageDecoder.cpp
@@ -28,7 +28,9 @@
 #if PLATFORM(QT)
 #include "ImageDecoderQt.h"
 #endif
+#if !PLATFORM(QT) || USE(LIBJPEG)
 #include "JPEGImageDecoder.h"
+#endif
 #include "PNGImageDecoder.h"
 #include "PlatformMemoryInstrumentation.h"
 #include "SharedBuffer.h"
