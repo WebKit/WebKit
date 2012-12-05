bd94918a090b11887b27fc41e2c05c8f1f19b0c1
Krzysztof Czech <k.czech@samsung.com>
2012-12-05 17:56:12 +0000

    [EFL][WK2] Introduce WebKit-EFL to Assistive Technologies https://bugs.webkit.org/show_bug.cgi?id=104000
    Patch by Krzysztof Czech <k.czech@samsung.com> on 2012-12-05
    Reviewed by Chris Fleizach.
    
    Introduce Webkit-EFL so that Assistive Technologies can distinguish it.
    
    * accessibility/atk/WebKitAccessibleWrapperAtk.cpp:
    (webkitAccessibleGetAttributes):
    
    Canonical link: https://commits.webkit.org/122292@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@136710 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/accessibility/atk/WebKitAccessibleWrapperAtk.cpp
+++ Source/WebCore/accessibility/atk/WebKitAccessibleWrapperAtk.cpp
@@ -436,6 +436,8 @@ static AtkAttributeSet* webkitAccessibleGetAttributes(AtkObject* object)
     AtkAttributeSet* attributeSet = 0;
 #if PLATFORM(GTK)
     attributeSet = addToAtkAttributeSet(attributeSet, "toolkit", "WebKitGtk");
+#elif PLATFORM(EFL)
+    attributeSet = addToAtkAttributeSet(attributeSet, "toolkit", "WebKitEfl");
 #endif
 
     AccessibilityObject* coreObject = core(object);
