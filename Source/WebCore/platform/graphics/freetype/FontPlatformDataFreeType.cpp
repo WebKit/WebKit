42f1f508cdf62553094707fd28e42812704490bd
Martin Robinson <mrobinson@webkit.org>
2012-12-10 14:50:44 +0000

    [GTK] Bring Harfbuzz-ng support to Gtk https://bugs.webkit.org/show_bug.cgi?id=92098
    Reviewed by Gustavo Noronha Silva.
    
    .:
    
    Add support for detecting HarfBuzz during configuration phase. Add these
    flags to the FreeType ones since HarfBuzz support is part of the FreeType
    backend.
    
    * configure.ac: Detect HarfBuzz.
    
    Source/WebCore:
    
    Enable HarfBuzz support by default. With the Freetype backend we no longer
    fall back to Pango for rendering complex text.
    
    No new tests. This is covered by existing tests.
    
    * GNUmakefile.am: Add HarfBuzz include directories and define.
    * GNUmakefile.list.am: Add HarfBuzz files to source list and remove
    Pango ones.
    * platform/graphics/freetype/FontPlatformDataFreeType.cpp:
    (WebCore::FontPlatformData::operator==): To properly render fallback
    fonts via HarfBuzz , it's important that we actually ask Fontconfig
    whether or not two patterns are the same. This ensures that when patterns
    are just copies of one another they only take up one slot in the font cache
    and appear to be the same font to HarfBuzzShaper.
    
    Source/WebKit/gtk:
    
    Add HarfBuzz dependency.
    
    * GNUmakefile.am: Add FreeType/HarfBuzz CFLAGS to the library.
    
    Source/WebKit2:
    
    Add a HarfBuzz dependency.
    
    * GNUmakefile.am: Add FreeType/HarfBuzz CFLAGS/LIBS to the libraries.
    
    Tools:
    
    Add HarfBuzz dependency.
    
    * GNUmakefile.am: Add FreeType/HarfBuzz CFLAGS to the WebCore Internals library.
    * gtk/jhbuild.modules: Add HarfBuzz to the JHbuild and remove Pango, since it
    should no longer affect layout test results.
    
    LayoutTests:
    
    Update tests results that have changed due to the switch to HarfBuzz.
    In general, these are progressions or harmless metrics differences.
    
    * platform/gtk: Update test results that have changed.
    
    Canonical link: https://commits.webkit.org/122705@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@137146 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/freetype/FontPlatformDataFreeType.cpp
+++ Source/WebCore/platform/graphics/freetype/FontPlatformDataFreeType.cpp
@@ -244,8 +244,13 @@ bool FontPlatformData::isFixedPitch()
 
 bool FontPlatformData::operator==(const FontPlatformData& other) const
 {
-    return m_pattern == other.m_pattern
-        && m_scaledFont == other.m_scaledFont
+    // FcPatternEqual does not support null pointers as arguments.
+    if ((m_pattern && !other.m_pattern)
+        || (!m_pattern && other.m_pattern)
+        || (m_pattern != other.m_pattern && !FcPatternEqual(m_pattern.get(), other.m_pattern.get())))
+        return false;
+
+    return m_scaledFont == other.m_scaledFont
         && m_size == other.m_size
         && m_syntheticOblique == other.m_syntheticOblique
         && m_syntheticBold == other.m_syntheticBold; 
