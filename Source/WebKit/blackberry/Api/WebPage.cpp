eb45baac2bb5bf98765c02267727f8797abcfa2f
Yong Li <yoli@rim.com>
2012-12-13 23:41:31 +0000

    [BlackBerry] Possible JS re-entrancy caused by UI event handler https://bugs.webkit.org/show_bug.cgi?id=84862
    Reviewed by Rob Buis.
    
    Fix an obvious bug in previous patch.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::deferredTasksTimerFired):
    
    
    Canonical link: https://commits.webkit.org/123197@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@137671 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/blackberry/Api/WebPage.cpp
+++ Source/WebKit/blackberry/Api/WebPage.cpp
@@ -1419,7 +1419,7 @@ void WebPagePrivate::didResumeLoading()
 void WebPagePrivate::deferredTasksTimerFired(WebCore::Timer<WebPagePrivate>*)
 {
     ASSERT(!m_deferredTasks.isEmpty());
-    if (!m_deferredTasks.isEmpty())
+    if (m_deferredTasks.isEmpty())
         return;
 
     OwnPtr<DeferredTaskBase> task = m_deferredTasks[0].release();
