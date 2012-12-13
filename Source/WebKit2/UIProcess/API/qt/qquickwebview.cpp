a1665f553797a6331805b20aa0ef0c463e17f422
Jerome Pasion <jerome.pasion@digia.com>
2012-12-13 15:04:36 +0000

    [Qt] Doc: Fixing Qt WebKit reference documentation.
    Patch by Jerome Pasion <jerome.pasion@digia.com> on 2012-12-13
    Reviewed by Simon Hausmann.
    
    Fixes:
    -added \module for C++ classes and \qmlmodule for QML types
    -added links to the Qt WebKit Examples pages
    -fixed the qhp settings for Qt Creator
    
    Task-number: QTBUG-28583
    Task-number: QTBUG-28418
    Task-number: QTBUG-27646
    
    .:
    
    * Source/qtwebkit.qdocconf:
    
    Source/WebKit/qt:
    
    * docs/qtwebkit.qdoc:
    
    Source/WebKit2:
    
    * UIProcess/API/qt/qquickwebview.cpp:
    
    Canonical link: https://commits.webkit.org/123135@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@137590 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/qt/qquickwebview.cpp
+++ Source/WebKit2/UIProcess/API/qt/qquickwebview.cpp
@@ -1460,6 +1460,12 @@ QQuickWebPage* QQuickWebViewExperimental::page()
         }
     }
     \endcode
+
+    \section1 Examples
+
+    There are several Qt WebKit examples located in the
+    \l{Qt WebKit Examples} page.
+
 */
 
 
