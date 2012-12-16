794e28f565e82f6447134420b9f662199b045eb5
Jacky Jiang <zhajiang@rim.com>
2012-12-16 18:36:45 +0000

    [BlackBerry] GUIMark3 page is rendered at a large scale during history navigation back https://bugs.webkit.org/show_bug.cgi?id=105130
    Patch by Jacky Jiang  <zhajiang@rim.com>.
    Reviewed by George Staikos.
    
    PR: 265999
    During history navigation, UI thread can blit the contents at a bad
    view state before WebKit thread restores the view state.
    Suspend screen updates before goBackOrForward() and resume thereafter
    so that screen can be updated after the correct view state is updated.
    No need to render or blit here, as goBackOrForward() can dispatch a
    message to WebKit thread to do that in WebPagePrivate::restoreHistoryViewState().
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPage::goBackOrForward):
    
    Canonical link: https://commits.webkit.org/123356@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@137843 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/blackberry/Api/WebPage.cpp
+++ Source/WebKit/blackberry/Api/WebPage.cpp
@@ -3130,7 +3130,9 @@ bool WebPage::canGoBackOrForward(int delta) const
 bool WebPage::goBackOrForward(int delta)
 {
     if (d->m_page->canGoBackOrForward(delta)) {
+        d->m_backingStore->d->suspendScreenUpdates();
         d->m_page->goBackOrForward(delta);
+        d->m_backingStore->d->resumeScreenUpdates(BackingStore::None);
         return true;
     }
     return false;
