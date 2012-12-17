396f345605440cc3f281edfa9da13a77a28da381
Adam Klein <adamk@chromium.org>
2012-12-17 21:40:59 +0000

    build-webkit: rename --template-tag to --template-element to match ENABLE #define name https://bugs.webkit.org/show_bug.cgi?id=105072
    Reviewed by Laszlo Gombos.
    
    * Scripts/webkitperl/FeatureList.pm:
    
    
    Canonical link: https://commits.webkit.org/123450@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@137938 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitperl/FeatureList.pm
+++ Tools/Scripts/webkitperl/FeatureList.pm
@@ -127,7 +127,7 @@ my (
     $svgFontsSupport,
     $svgSupport,
     $systemMallocSupport,
-    $templateTagSupport,
+    $templateElementSupport,
     $textAutosizingSupport,
     $tiledBackingStoreSupport,
     $touchEventsSupport,
@@ -395,8 +395,8 @@ my @features = (
     { option => "system-malloc", desc => "Toggle system allocator instead of TCmalloc",
       define => "USE_SYSTEM_MALLOC", default => isWinCE(), value => \$systemMallocSupport },
 
-    { option => "template-tag", desc => "Toggle Templates Tag support",
-      define => "ENABLE_TEMPLATE_ELEMENT", default => 0, value => \$templateTagSupport },
+    { option => "template-element", desc => "Toggle HTMLTemplateElement support",
+      define => "ENABLE_TEMPLATE_ELEMENT", default => 0, value => \$templateElementSupport },
 
     { option => "text-autosizing", desc => "Toggle Text Autosizing support",
       define => "ENABLE_TEXT_AUTOSIZING", default => 0, value => \$textAutosizingSupport },
