2173545ae2e62d121bb389c6810cbcbf17961b3b
Eugene Klyuchnikov <eustas@chromium.org>
2012-12-17 14:53:49 +0000

    Web Inspector: Make popover content accessible for clicks. https://bugs.webkit.org/show_bug.cgi?id=105167
    Patch by Eugene Klyuchnikov <eustas@chromium.org> on 2012-12-17
    Reviewed by Vsevolod Vlasov.
    
    Currently it is very hard to move mouse pointer inside of popover area.
    The only maneuver is to precisely jerk the mouse in direction of popover.
    
    This path makes anchor and popover "active" areas overlap, so it becomes
    possible to smoothly move mouse to popover interior.
    
    * inspector/front-end/Popover.js:
    (WebInspector.Popover.prototype._innerShow): Make popover "outer" border
    an be "active" area.
    * inspector/front-end/popover.css: Allow popover "outer" border receive
    mouse events.
    
    Canonical link: https://commits.webkit.org/123418@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@137905 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/Popover.js
+++ Source/WebCore/inspector/front-end/Popover.js
@@ -106,7 +106,7 @@ WebInspector.Popover.prototype = {
         this._positionElement(anchor, preferredWidth, preferredHeight);
 
         if (this._popoverHelper) {
-            contentElement.addEventListener("mousemove", this._popoverHelper._killHidePopoverTimer.bind(this._popoverHelper), true);
+            this.element.addEventListener("mousemove", this._popoverHelper._killHidePopoverTimer.bind(this._popoverHelper), true);
             this.element.addEventListener("mouseout", this._popoverHelper._popoverMouseOut.bind(this._popoverHelper), true);
         }
     },
