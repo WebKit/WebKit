d6bc5baaa8d7d25e6f56b9861f2183684b4f5bd9
Andrey Kosyakov <caseq@chromium.org>
2012-12-17 14:14:13 +0000

    Web Inspector: Persist Audit selections across sessions https://bugs.webkit.org/show_bug.cgi?id=103944
    Reviewed by Alexander Pavlov.
    
    Make selected audit categories a persistent setting, to simplify life for those
    repeatedly running same set of categories.
    
    * inspector/front-end/AuditLauncherView.js:
    (WebInspector.AuditLauncherView):
    (WebInspector.AuditLauncherView.prototype.addCategory.get var):
    (WebInspector.AuditLauncherView.prototype.addCategory):
    (WebInspector.AuditLauncherView.prototype._selectAllClicked):
    (WebInspector.AuditLauncherView.prototype._categoryClicked):
    (WebInspector.AuditLauncherView.prototype._createCategoryElement):
    
    Canonical link: https://commits.webkit.org/123416@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@137903 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/AuditLauncherView.js
+++ Source/WebCore/inspector/front-end/AuditLauncherView.js
@@ -60,7 +60,13 @@ WebInspector.AuditLauncherView = function(runnerCallback)
 
     WebInspector.networkManager.addEventListener(WebInspector.NetworkManager.EventTypes.RequestStarted, this._onRequestStarted, this);
     WebInspector.networkManager.addEventListener(WebInspector.NetworkManager.EventTypes.RequestFinished, this._onRequestFinished, this);
+
+    var defaultSelectedAuditCategory = {};
+    defaultSelectedAuditCategory[WebInspector.AuditLauncherView.AllCategoriesKey] = true;
+    this._selectedCategoriesSetting = WebInspector.settings.createSetting("selectedAuditCategories", defaultSelectedAuditCategory);
 }
+
+WebInspector.AuditLauncherView.AllCategoriesKey = "__AllCategories";
 
 WebInspector.AuditLauncherView.prototype = {
     _resetResourceCount: function()
@@ -97,9 +103,10 @@ WebInspector.AuditLauncherView.prototype = {
         if (!this._sortedCategories.length)
             this._createLauncherUI();
 
+        var selectedCategories = this._selectedCategoriesSetting.get();
         var categoryElement = this._createCategoryElement(category.displayName, category.id);
         category._checkboxElement = categoryElement.firstChild;
-        if (this._selectAllCheckboxElement.checked) {
+        if (this._selectAllCheckboxElement.checked || selectedCategories[category.displayName]) {
             category._checkboxElement.checked = true;
             ++this._currentCategoriesCount;
         }
@@ -113,7 +120,7 @@ WebInspector.AuditLauncherView.prototype = {
         var insertBefore = insertionIndexForObjectInListSortedByFunction(category, this._sortedCategories, compareCategories);
         this._categoriesElement.insertBefore(categoryElement, this._categoriesElement.children[insertBefore]);
         this._sortedCategories.splice(insertBefore, 0, category);
-        this._updateButton();
+        this._selectedCategoriesUpdated();
     },
 
     /**
@@ -166,21 +173,22 @@ WebInspector.AuditLauncherView.prototype = {
 
     /**
      * @param {boolean} checkCategories
+     * @param {boolean=} userGesture
      */
-    _selectAllClicked: function(checkCategories)
+    _selectAllClicked: function(checkCategories, userGesture)
     {
         var childNodes = this._categoriesElement.childNodes;
         for (var i = 0, length = childNodes.length; i < length; ++i)
             childNodes[i].firstChild.checked = checkCategories;
         this._currentCategoriesCount = checkCategories ? this._sortedCategories.length : 0;
-        this._updateButton();
+        this._selectedCategoriesUpdated(userGesture);
     },
 
     _categoryClicked: function(event)
     {
         this._currentCategoriesCount += event.target.checked ? 1 : -1;
         this._selectAllCheckboxElement.checked = this._currentCategoriesCount === this._sortedCategories.length;
-        this._updateButton();
+        this._selectedCategoriesUpdated(true);
     },
 
     /**
@@ -198,6 +206,7 @@ WebInspector.AuditLauncherView.prototype = {
             element.addEventListener("click", this._boundCategoryClickListener, false);
         labelElement.appendChild(element);
         labelElement.appendChild(document.createTextNode(title));
+        labelElement.__displayName = title;
 
         return labelElement;
     },
@@ -214,12 +223,12 @@ WebInspector.AuditLauncherView.prototype = {
 
         function handleSelectAllClick(event)
         {
-            this._selectAllClicked(event.target.checked);
+            this._selectAllClicked(event.target.checked, true);
         }
         var categoryElement = this._createCategoryElement(WebInspector.UIString("Select All"), "");
         categoryElement.id = "audit-launcher-selectall";
         this._selectAllCheckboxElement = categoryElement.firstChild;
-        this._selectAllCheckboxElement.checked = true;
+        this._selectAllCheckboxElement.checked = this._selectedCategoriesSetting.get()[WebInspector.AuditLauncherView.AllCategoriesKey];
         this._selectAllCheckboxElement.addEventListener("click", handleSelectAllClick.bind(this), false);
         this._contentElement.appendChild(categoryElement);
 
@@ -249,13 +258,29 @@ WebInspector.AuditLauncherView.prototype = {
         this._launchButton.addEventListener("click", this._launchButtonClicked.bind(this), false);
 
         this._selectAllClicked(this._selectAllCheckboxElement.checked);
-        this._updateButton();
     },
 
     _updateResourceProgress: function()
     {
         if (this._displayResourceLoadingProgress)
             this._progressIndicator.setTitle(WebInspector.UIString("Loading (%d of %d)", this._loadedResources, this._totalResources));
+    },
+
+    /**
+     * @param {boolean=} userGesture
+     */
+    _selectedCategoriesUpdated: function(userGesture)
+    {
+        // Save present categories only upon user gesture to clean up junk from past versions and removed extensions.
+        // Do not remove old categories if not handling a user gesture, as there's chance categories will be added
+        // later during start-up.
+        var selectedCategories = userGesture ? {} : this._selectedCategoriesSetting.get();
+        var childNodes = this._categoriesElement.childNodes;
+        for (var i = 0, length = childNodes.length; i < length; ++i)
+            selectedCategories[childNodes[i].__displayName] = childNodes[i].firstChild.checked;
+        selectedCategories[WebInspector.AuditLauncherView.AllCategoriesKey] = this._selectAllCheckboxElement.checked;
+        this._selectedCategoriesSetting.set(selectedCategories);
+        this._updateButton();
     },
 
     _updateButton: function()
