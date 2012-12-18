885f0116d1846cde366c89391fa4267938ab4d63
Beth Dakin <bdakin@apple.com>
2012-12-18 22:25:11 +0000

    https://bugs.webkit.org/show_bug.cgi?id=102579 [mac] Enable scaled cursors
    Reviewed by Dean Jackson.
    
    Source/JavaScriptCore:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebCore:
    
    Not much is required to get this turned on. We just need the feature to be
    defined, and we need to initialize m_imageScaleFactor in the Cursor constructors.
    
    * Configurations/FeatureDefines.xcconfig:
    * platform/mac/CursorMac.mm:
    (WebCore::Cursor::Cursor):
    (WebCore::Cursor::operator=):
    
    Source/WebKit/mac:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebKit2:
    
    * Configurations/FeatureDefines.xcconfig:
    
    LayoutTests:
    
    Un-skip these tests now that the feature is enabled.
    * platform/mac/TestExpectations:
    
    
    
    Canonical link: https://commits.webkit.org/123573@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@138069 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/mac/CursorMac.mm
+++ Source/WebCore/platform/mac/CursorMac.mm
@@ -340,6 +340,7 @@ Cursor::Cursor(const Cursor& other)
     : m_type(other.m_type)
     , m_image(other.m_image)
     , m_hotSpot(other.m_hotSpot)
+    , m_imageScaleFactor(other.m_imageScaleFactor)
     , m_platformCursor(other.m_platformCursor)
 {
 }
@@ -349,6 +350,7 @@ Cursor& Cursor::operator=(const Cursor& other)
     m_type = other.m_type;
     m_image = other.m_image;
     m_hotSpot = other.m_hotSpot;
+    m_imageScaleFactor = other.m_imageScaleFactor;
     m_platformCursor = other.m_platformCursor;
     return *this;
 }
