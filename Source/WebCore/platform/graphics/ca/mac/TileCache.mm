cad47dd61ab6b355bbf707fc6d5b9185cea10a73
Simon Fraser <simon.fraser@apple.com>
2012-12-19 17:49:45 +0000

    Fix regression from r137923 that caused all tiles to paint when scrolling https://bugs.webkit.org/show_bug.cgi?id=105394
    Reviewed by Dean Jackson.
    
    r137923 removed a 'continue' from the loop that ensures we have tile coverage,
    but this caused us to issue repaints for all existing tiles.
    
    Fix to 'continue' when the tile size is unchanged. Also remove 'primaryLayerCount'
    which was unused.
    
    * platform/graphics/ca/mac/TileCache.mm:
    (WebCore::TileCache::revalidateTiles):
    
    Canonical link: https://commits.webkit.org/123676@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@138177 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/ca/mac/TileCache.mm
+++ Source/WebCore/platform/graphics/ca/mac/TileCache.mm
@@ -601,7 +601,6 @@ void TileCache::revalidateTiles(TileValidationPolicy validationPolicy)
     
     // Ensure primary tile coverage tiles.
     m_primaryTileCoverageRect = IntRect();
-    int primaryLayerCount = 0;
     
     for (int y = topLeft.y(); y <= bottomRight.y(); ++y) {
         for (int x = topLeft.x(); x <= bottomRight.x(); ++x) {
@@ -617,12 +616,12 @@ void TileCache::revalidateTiles(TileValidationPolicy validationPolicy)
             } else {
                 // We already have a layer for this tile. Ensure that its size is correct.
                 FloatSize tileLayerSize([tileInfo.layer.get() frame].size);
-                if (tileLayerSize != FloatSize(tileRect.size()))
-                    [tileInfo.layer.get() setFrame:tileRect];
+                if (tileLayerSize == FloatSize(tileRect.size()))
+                    continue;
+
+                [tileInfo.layer.get() setFrame:tileRect];
             }
             
-            ++primaryLayerCount;
-
             FloatRect scaledTileRect = tileRect;
             scaledTileRect.scale(1 / m_scale);
             dirtyRects.append(scaledTileRect);
