b03d4ee0e9d1b28547ce38142fd2f8a950e881ee
Andrey Lushnikov <lushnikov@chromium.com>
2012-12-20 10:06:56 +0000

    Web Inspector: fix |_repaintAll| behaviour of a DefaultTextEditor https://bugs.webkit.org/show_bug.cgi?id=105429
    Patch by Andrey Lushnikov <lushnikov@chromium.com> on 2012-12-20
    Reviewed by Pavel Feldman.
    
    Override |_repainAll| method in TextEditorMainPanel and manually
    repaint visible chunks.
    
    * inspector/front-end/DefaultTextEditor.js:
    (WebInspector.TextEditorChunkedPanel.prototype._splitChunkOnALine): Call |_repaintAll| method because |expand()| does not do any repaints anymore.
    (WebInspector.TextEditorChunkedPanel.prototype._repaintAll): Return |false| in case of fast return, and |true| otherwise.
    (WebInspector.TextEditorMainPanel.prototype._repaintAll): Added.
    (WebInspector.TextEditorMainChunk.prototype.expand): Remove painting operations.
    
    Canonical link: https://commits.webkit.org/123727@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@138237 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/DefaultTextEditor.js
+++ Source/WebCore/inspector/front-end/DefaultTextEditor.js
@@ -813,6 +813,7 @@ WebInspector.TextEditorChunkedPanel.prototype = {
             lineChunk.expand();
             if (suffixChunk)
                 suffixChunk.expand();
+            this._repaintAll();
         }
 
         this.endDomUpdates();
@@ -932,7 +933,7 @@ WebInspector.TextEditorChunkedPanel.prototype = {
         delete this._repaintAllTimer;
 
         if (this._paintCoalescingLevel)
-            return;
+            return false;
 
         var visibleFrom = this._scrollTop();
         var visibleTo = visibleFrom + this._clientHeight();
@@ -941,6 +942,7 @@ WebInspector.TextEditorChunkedPanel.prototype = {
             var result = this._findVisibleChunks(visibleFrom, visibleTo);
             this._expandChunks(result.start, result.end);
         }
+        return true;
     },
 
     _scrollTop: function()
@@ -1359,6 +1361,25 @@ WebInspector.TextEditorMainPanel.prototype = {
     {
         this._detachMutationObserver();
         this._isShowing = false;
+    },
+
+    _repaintAll: function()
+    {
+        // calling overridden |_repaintAll| method
+        if (!WebInspector.TextEditorChunkedPanel.prototype._repaintAll.call(this))
+            return false;
+
+        var visibleFrom = this._scrollTop();
+        var visibleTo = visibleFrom + this._clientHeight();
+
+        if (visibleTo) {
+            var result = this._findVisibleChunks(visibleFrom, visibleTo);
+            for(var i = result.start; i < result.end; i++) {
+                var chunk = this._textChunks[i];
+                this._paintLines(chunk.startLine, chunk.startLine + chunk.linesCount, true);
+            }
+        }
+        return true;
     },
 
     _attachMutationObserver: function()
@@ -2658,10 +2679,8 @@ WebInspector.TextEditorMainChunk.prototype = {
 
         this._expanded = true;
 
-        if (this.linesCount === 1) {
-            this._chunkedPanel._paintLine(this.element);
+        if (this.linesCount === 1)
             return;
-        }
 
         this._chunkedPanel.beginDomUpdates();
 
@@ -2673,7 +2692,6 @@ WebInspector.TextEditorMainChunk.prototype = {
             this._expandedLineRows.push(lineRow);
         }
         parentElement.removeChild(this.element);
-        this._chunkedPanel._paintLines(this.startLine, this.startLine + this.linesCount);
 
         this._chunkedPanel.endDomUpdates();
     },
