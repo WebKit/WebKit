863cc9666126ece37fb881ad1a611b1d31c5b3a2
Dongseong Hwang <luxtella@company100.net>
2012-12-26 20:59:23 +0000

    [TexMap] Remove unused members and methods in TextureMapperLayer. https://bugs.webkit.org/show_bug.cgi?id=105755
    Patch by Huang Dongsung <luxtella@company100.net> on 2012-12-26
    Reviewed by Noam Rosenthal.
    
    No new tests. Refactoring only.
    
    * platform/graphics/texmap/TextureMapperLayer.h:
    (TextureMapperLayer):
    (State):
    (WebCore::TextureMapperLayer::State::State):
    
    Canonical link: https://commits.webkit.org/123967@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@138483 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/texmap/TextureMapperLayer.h
+++ Source/WebCore/platform/graphics/texmap/TextureMapperLayer.h
@@ -99,7 +99,6 @@ public:
 
     TextureMapper* textureMapper() const;
     void flushCompositingStateForThisLayerOnly(GraphicsLayerTextureMapper*);
-    IntSize size() const { return IntSize(m_size.width(), m_size.height()); }
     void setTransform(const TransformationMatrix&);
     void setOpacity(float value) { m_opacity = value; }
 #if ENABLE(CSS_FILTERS)
@@ -121,14 +120,8 @@ public:
 private:
     const TextureMapperLayer* rootLayer() const;
     void computeTransformsRecursive();
-    void computeOverlapsIfNeeded();
-    void computeTiles();
     IntRect intermediateSurfaceRect(const TransformationMatrix&);
     IntRect intermediateSurfaceRect();
-    void swapContentsBuffers();
-    FloatRect targetRectForTileRect(const FloatRect& totalTargetRect, const FloatRect& tileRect) const;
-    void invalidateViewport(const FloatRect&);
-    void notifyChange(ChangeMask);
 
     static int compareGraphicsLayersZValue(const void* a, const void* b);
     static void sortByZOrder(Vector<TextureMapperLayer* >& array, int first, int last);
@@ -165,12 +158,12 @@ private:
     ContentsLayerCount countPotentialLayersWithContents() const;
     bool shouldPaintToIntermediateSurface() const;
 
-    GraphicsLayerTransform m_transform;
-
     inline FloatRect layerRect() const
     {
         return FloatRect(FloatPoint::zero(), m_size);
     }
+
+    GraphicsLayerTransform m_transform;
 
     Vector<TextureMapperLayer*> m_children;
     TextureMapperLayer* m_parent;
@@ -183,7 +176,6 @@ private:
     FilterOperations m_filters;
 #endif
     float m_centerZ;
-    String m_name;
 
     struct State {
         FloatPoint pos;
@@ -193,12 +185,11 @@ private:
         TransformationMatrix childrenTransform;
         float opacity;
         FloatRect contentsRect;
-        int descendantsWithContent;
         TextureMapperLayer* maskLayer;
         TextureMapperLayer* replicaLayer;
         Color solidColor;
 #if ENABLE(CSS_FILTERS)
-         FilterOperations filters;
+        FilterOperations filters;
 #endif
 
         bool preserves3D : 1;
@@ -208,8 +199,7 @@ private:
         bool contentsOpaque : 1;
         bool backfaceVisibility : 1;
         bool visible : 1;
-        bool mightHaveOverlaps : 1;
-        bool needsRepaint;
+
         State()
             : opacity(1.f)
             , maskLayer(0)
@@ -221,8 +211,6 @@ private:
             , contentsOpaque(false)
             , backfaceVisibility(false)
             , visible(true)
-            , mightHaveOverlaps(false)
-            , needsRepaint(false)
         {
         }
     };
