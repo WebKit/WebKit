a5a8efd994ee9f8711254af3c8a1e67c218c2a6b
Stuart Eichert <stuart@fivemicro.com>
2012-12-28 08:00:58 +0000

    instance-of-immediates.js : the word "passing" is spelled incorrectly https://bugs.webkit.org/show_bug.cgi?id=105669
    Patch by Stuart Eichert <stuart@fivemicro.com> on 2012-12-27
    Reviewed by Julien Chaffraix.
    
    Corrected spelling of the word "passing" in instance-of-immediates.js
    
    * fast/js/script-tests/instance-of-immediates.js:
    
    Canonical link: https://commits.webkit.org/124008@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@138524 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/js/script-tests/instance-of-immediates.js
+++ LayoutTests/fast/js/script-tests/instance-of-immediates.js
@@ -12,29 +12,29 @@ function testSet(constructor, testMethod)
     testMethod["obj"]("(obj instanceof " + constructor + ")");
 }
 
-// Test set 1, test passsing the integer 1 as the constructor to be tested for.
+// Test set 1, test passing the integer 1 as the constructor to be tested for.
 // The constructor being an object is the first thing tested, so these should all throw.
 testSet("1", { "1":shouldThrow, "{}":shouldThrow, "obj":shouldThrow });
 
-// Test set 2, test passsing an empty object ({}) as the constructor to be tested for.
+// Test set 2, test passing an empty object ({}) as the constructor to be tested for.
 // As well as being an object, the constructor must implement 'HasInstance' (i.e. be a function), so these should all throw too.
 testSet("{}", { "1":shouldThrow, "{}":shouldThrow, "obj":shouldThrow });
 
-// Test set 3, test passsing Constructor as the constructor to be tested for.
+// Test set 3, test passing Constructor as the constructor to be tested for.
 // Nothing should except, the third test should pass, since obj is an instance of Constructor.
 testSet("Constructor", { "1":shouldBeFalse, "{}":shouldBeFalse, "obj":shouldBeTrue });
 
-// Test set 4, test passsing Constructor as the constructor to be tested for - with Constructor.prototype set to the integer 1.
+// Test set 4, test passing Constructor as the constructor to be tested for - with Constructor.prototype set to the integer 1.
 // Constructor.prototype being a non-object will cause an exception, /unless/ value is also a non-object, since this is checked first.
 Constructor.prototype = 1;
 testSet("Constructor", { "1":shouldBeFalse, "{}":shouldThrow, "obj":shouldThrow });
 
-// Test set 5, test passsing Constructor as the constructor to be tested for - with Constructor.prototype set to an empty object ({}).
+// Test set 5, test passing Constructor as the constructor to be tested for - with Constructor.prototype set to an empty object ({}).
 // All test fail, no reason to throw.  (obj instanceof Constructor) is now false, since Constructor.prototype has changed.
 Constructor.prototype = {};
 testSet("Constructor", { "1":shouldBeFalse, "{}":shouldBeFalse, "obj":shouldBeFalse });
 
-// Test set 6, test passsing Constructor as the constructor to be tested for - with Constructor.prototype set to null.
+// Test set 6, test passing Constructor as the constructor to be tested for - with Constructor.prototype set to null.
 // Test that behaviour is the same as for test set 4.
 Constructor.prototype = null;
 testSet("Constructor", { "1":shouldBeFalse, "{}":shouldThrow, "obj":shouldThrow });
