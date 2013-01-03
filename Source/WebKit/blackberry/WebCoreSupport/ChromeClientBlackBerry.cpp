1063a58e7f953a15d2bcd9022f5309ca7e3a0bc4
Otto Derek Cheung <otcheung@rim.com>
2013-01-03 04:44:09 +0000

    [BlackBerry] Do not determine db quota limit using db file size https://bugs.webkit.org/show_bug.cgi?id=105991
    PR 267191
    
    Patch by Otto Derek Cheung <otcheung@rim.com> on 2013-01-02
    Reviewed by George Staikos.
    
    Passing the current quota to WebPageClientImpl::databaseQuota so we can determine a better
    quota limit without using the db file size.
    
    * Api/WebPageClient.h:
    * WebCoreSupport/ChromeClientBlackBerry.cpp:
    (WebCore::ChromeClientBlackBerry::exceededDatabaseQuota):
    
    Canonical link: https://commits.webkit.org/124175@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@138696 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/blackberry/WebCoreSupport/ChromeClientBlackBerry.cpp
+++ Source/WebKit/blackberry/WebCoreSupport/ChromeClientBlackBerry.cpp
@@ -498,8 +498,9 @@ void ChromeClientBlackBerry::exceededDatabaseQuota(Frame* frame, const String& n
 
     DatabaseManager& manager = DatabaseManager::manager();
 
-    unsigned long long totalUsage = manager.totalDatabaseUsage();
-    unsigned long long originUsage = manager.usageForOrigin(origin);
+    unsigned long long totalUsage = tracker.totalDatabaseUsage();
+    unsigned long long originUsage = tracker.usageForOrigin(origin);
+    unsigned long long currentQuota = tracker.quotaForOrigin(origin);
 
     DatabaseDetails details = manager.detailsForNameAndOrigin(name, origin);
     unsigned long long estimatedSize = details.expectedUsage();
@@ -508,7 +509,7 @@ void ChromeClientBlackBerry::exceededDatabaseQuota(Frame* frame, const String& n
     String originStr = origin->databaseIdentifier();
 
     unsigned long long quota = m_webPagePrivate->m_client->databaseQuota(originStr.characters(), originStr.length(),
-        nameStr.characters(), nameStr.length(), totalUsage, originUsage, estimatedSize);
+        nameStr.characters(), nameStr.length(), originUsage, currentQuota, estimatedSize);
 
     manager.setQuota(origin, quota);
 #endif
