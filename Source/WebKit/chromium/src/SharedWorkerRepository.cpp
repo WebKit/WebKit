16c84292adfed25fa4ba4c153d3b562847a3d0a3
Mark Pilgrim <pilgrim@chromium.org>
2013-01-03 22:26:02 +0000

    [Chromium] Remove all references to sharedWorkerRepository() https://bugs.webkit.org/show_bug.cgi?id=104704
    Reviewed by Adam Barth.
    
    Now that Chromium calls setSharedWorkerRepository upon
    initializing WebKit (
    https://codereview.chromium.org/10990121/ and
    https://codereview.chromium.org/11576028/ ) and no longer
    tries to override sharedWorkerRepository, it is safe to remove
    all references to this function.
    
    * public/platform/WebKitPlatformSupport.h:
    (WebKit):
    (WebKit::WebKitPlatformSupport::idbFactory):
    * src/SharedWorkerRepository.cpp:
    (WebKit::sharedWorkerRepository):
    (WebCore::SharedWorkerRepository::isAvailable):
    
    
    Canonical link: https://commits.webkit.org/124220@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@138742 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/chromium/src/SharedWorkerRepository.cpp
+++ Source/WebKit/chromium/src/SharedWorkerRepository.cpp
@@ -69,15 +69,8 @@ void setSharedWorkerRepository(WebSharedWorkerRepository* repository)
 
 static WebSharedWorkerRepository* sharedWorkerRepository()
 {
-    WebSharedWorkerRepository* repository;
-
-    repository = s_sharedWorkerRepository;
-    if (!repository) {
-        repository = webKitPlatformSupport()->sharedWorkerRepository();
-        setSharedWorkerRepository(repository);
-    }
-
-    return repository;
+    // Will only be non-zero if the embedder has set the shared worker repository upon initialization. Nothing in WebKit sets this.
+    return s_sharedWorkerRepository;
 }
 
 }
@@ -218,8 +211,6 @@ void SharedWorkerScriptLoader::connected()
 
 bool SharedWorkerRepository::isAvailable()
 {
-    // Allow the WebKitPlatformSupport to determine if SharedWorkers
-    // are available.
     return WebKit::sharedWorkerRepository();
 }
 
