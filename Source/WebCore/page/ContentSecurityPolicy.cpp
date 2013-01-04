2de7b8c300a0358dc9a6479a930f6d6595990534
Mike West <mkwst@chromium.org>
2013-01-04 20:38:26 +0000

    CSP 1.1: Experiment with adding line numbers to violation reports. https://bugs.webkit.org/show_bug.cgi?id=105374
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Line numbers and URLs were added to the CSP 1.1 spec in
    https://dvcs.w3.org/hg/content-security-policy/rev/e0270baace3d, and
    already exist (and have for quite some time) in Firefox's
    implementation. Based on developer feedback, it seems reasonable to
    experiment a bit with adding them to WebKit's violation reports.
    This patch generates a stack trace when constructing the violation
    report object (iff a violation report object ought to be generated)
    and adds a line number and URL to the object if possible.
    
    Tests: http/tests/security/contentSecurityPolicy/report-uri-from-inline-javascript.html
           http/tests/security/contentSecurityPolicy/report-uri-from-javascript.html
    
    * page/ContentSecurityPolicy.cpp:
    (WebCore::ContentSecurityPolicy::reportViolation):
        Generate a stack trace when building the violation report: if it
        has relevant detail, add the line number and URL to the report
        object.
    
    LayoutTests:
    
    * http/tests/security/contentSecurityPolicy/report-uri-from-inline-javascript-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/report-uri-from-inline-javascript.html: Added.
    * http/tests/security/contentSecurityPolicy/report-uri-from-javascript-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/report-uri-from-javascript.html: Added.
    
    
    Canonical link: https://commits.webkit.org/124309@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@138834 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/ContentSecurityPolicy.cpp
+++ Source/WebCore/page/ContentSecurityPolicy.cpp
@@ -39,6 +39,8 @@
 #include "PingLoader.h"
 #include "RuntimeEnabledFeatures.h"
 #include "SchemeRegistry.h"
+#include "ScriptCallStack.h"
+#include "ScriptCallStackFactory.h"
 #include "ScriptState.h"
 #include "SecurityOrigin.h"
 #include "TextEncoding.h"
@@ -155,6 +157,15 @@ FeatureObserver::Feature getFeatureObserverType(ContentSecurityPolicy::HeaderTyp
     }
     ASSERT_NOT_REACHED();
     return FeatureObserver::NumberOfFeatures;
+}
+
+const ScriptCallFrame& getFirstNonNativeFrame(PassRefPtr<ScriptCallStack> stack)
+{
+    int frameNumber = 0;
+    if (!stack->at(0).lineNumber() && stack->size() > 1 && stack->at(1).lineNumber())
+        frameNumber = 1;
+
+    return stack->at(frameNumber);
 }
 
 } // namespace
@@ -1604,6 +1615,17 @@ void ContentSecurityPolicy::reportViolation(const String& directiveText, const S
         cspReport->setString("blocked-uri", document->securityOrigin()->canRequest(blockedURL) ? blockedURL.strippedForUseAsReferrer() : SecurityOrigin::create(blockedURL)->toString());
     else
         cspReport->setString("blocked-uri", String());
+
+    RefPtr<ScriptCallStack> stack = createScriptCallStack(2, false);
+    if (stack) {
+        const ScriptCallFrame& callFrame = getFirstNonNativeFrame(stack);
+
+        if (callFrame.lineNumber()) {
+            KURL source = KURL(KURL(), callFrame.sourceURL());
+            cspReport->setString("source-file", document->securityOrigin()->canRequest(source) ? source.strippedForUseAsReferrer() : SecurityOrigin::create(source)->toString());
+            cspReport->setNumber("line-number", callFrame.lineNumber());
+        }
+    }
 
     RefPtr<InspectorObject> reportObject = InspectorObject::create();
     reportObject->setObject("csp-report", cspReport.release());
