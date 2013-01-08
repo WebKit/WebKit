c9a057dbca1c972985b2dc3df3047d3fefa26c0b
Alexey Proskuryakov <ap@apple.com>
2013-01-08 01:17:44 +0000

    [WK2] Merge SecItemShimMethods files into SecItemShim         https://bugs.webkit.org/show_bug.cgi?id=106278
            Reviewed by Anders Carlsson.
    
            Now that we have a SecItemShim class, there is no reason to split implementation
            into a separate file.
    
            * Shared/mac/SecItemShim.cpp:
            (WebKit::responseMap):
            (WebKit::generateSecItemRequestID):
            (WebKit::sendSecItemRequest):
            (WebKit::webSecItemCopyMatching):
            (WebKit::webSecItemAdd):
            (WebKit::webSecItemUpdate):
            (WebKit::webSecItemDelete):
            (WebKit::SecItemShim::secItemResponse):
            (WebKit::SecItemShim::install):
            * WebKit2.xcodeproj/project.pbxproj:
            * WebProcess/mac/SecItemShimMethods.h: Removed.
            * WebProcess/mac/SecItemShimMethods.mm: Removed.
    
    
    
    Canonical link: https://commits.webkit.org/124478@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@139018 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/Shared/mac/SecItemShim.cpp
+++ Source/WebKit2/Shared/mac/SecItemShim.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Apple Inc. All rights reserved.
+ * Copyright (C) 2011, 2013 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -28,9 +28,22 @@
 
 #if USE(SECURITY_FRAMEWORK)
 
-#include "SecItemShimMethods.h"
+#include "BlockingResponseMap.h"
+#include "SecItemRequestData.h"
+#include "SecItemResponseData.h"
+#include "WebProcess.h"
+#include "SecItemShimProxyMessages.h"
+#include "WebProcessShim.h"
+#include <Security/Security.h>
+#include <dlfcn.h>
 
 namespace WebKit {
+
+static BlockingResponseMap<SecItemResponseData>& responseMap()
+{
+    AtomicallyInitializedStatic(BlockingResponseMap<SecItemResponseData>*, responseMap = new BlockingResponseMap<SecItemResponseData>);
+    return *responseMap;
+}
 
 SecItemShim& SecItemShim::shared()
 {
@@ -42,14 +55,76 @@ SecItemShim::SecItemShim()
 {
 }
 
+static uint64_t generateSecItemRequestID()
+{
+    static int64_t uniqueSecItemRequestID;
+    return atomicIncrement(&uniqueSecItemRequestID);
+}
+
+static PassOwnPtr<SecItemResponseData> sendSecItemRequest(SecItemRequestData::Type requestType, CFDictionaryRef query, CFDictionaryRef attributesToMatch = 0)
+{
+    uint64_t requestID = generateSecItemRequestID();
+    if (!WebProcess::shared().connection()->send(Messages::SecItemShimProxy::SecItemRequest(requestID, SecItemRequestData(requestType, query, attributesToMatch)), 0))
+        return nullptr;
+
+    return responseMap().waitForResponse(requestID);
+}
+
+static OSStatus webSecItemCopyMatching(CFDictionaryRef query, CFTypeRef* result)
+{
+    OwnPtr<SecItemResponseData> response = sendSecItemRequest(SecItemRequestData::CopyMatching, query);
+    if (!response)
+        return errSecInteractionNotAllowed;
+
+    *result = response->resultObject().leakRef();
+    return response->resultCode();
+}
+
+static OSStatus webSecItemAdd(CFDictionaryRef query, CFTypeRef* result)
+{
+    OwnPtr<SecItemResponseData> response = sendSecItemRequest(SecItemRequestData::Add, query);
+    if (!response)
+        return errSecInteractionNotAllowed;
+
+    if (result)
+        *result = response->resultObject().leakRef();
+    return response->resultCode();
+}
+
+static OSStatus webSecItemUpdate(CFDictionaryRef query, CFDictionaryRef attributesToUpdate)
+{
+    OwnPtr<SecItemResponseData> response = sendSecItemRequest(SecItemRequestData::Update, query, attributesToUpdate);
+    if (!response)
+        return errSecInteractionNotAllowed;
+    
+    return response->resultCode();
+}
+
+static OSStatus webSecItemDelete(CFDictionaryRef query)
+{
+    OwnPtr<SecItemResponseData> response = sendSecItemRequest(SecItemRequestData::Delete, query);
+    if (!response)
+        return errSecInteractionNotAllowed;
+    
+    return response->resultCode();
+}
+
 void SecItemShim::secItemResponse(CoreIPC::Connection*, uint64_t requestID, const SecItemResponseData& response)
 {
-    didReceiveSecItemResponse(requestID, response);
+    responseMap().didReceiveResponse(requestID, adoptPtr(new SecItemResponseData(response)));
 }
 
 void SecItemShim::install()
 {
-    initializeSecItemShim();
+    const WebProcessSecItemShimCallbacks callbacks = {
+        webSecItemCopyMatching,
+        webSecItemAdd,
+        webSecItemUpdate,
+        webSecItemDelete
+    };
+    
+    WebProcessSecItemShimInitializeFunc func = reinterpret_cast<WebProcessSecItemShimInitializeFunc>(dlsym(RTLD_DEFAULT, "WebKitWebProcessSecItemShimInitialize"));
+    func(callbacks);
 }
 
 void SecItemShim::didReceiveMessageOnConnectionWorkQueue(CoreIPC::Connection* connection, CoreIPC::MessageID messageID, CoreIPC::MessageDecoder& decoder, bool& didHandleMessage)
