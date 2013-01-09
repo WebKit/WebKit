3df0368b48d862050274719a6bce16ab8353310c
Zeno Albisser <zeno.albisser@digia.com>
2013-01-09 13:31:47 +0000

    [Qt] WebGL content is incomplete when using multiple canvas https://bugs.webkit.org/show_bug.cgi?id=106313
    While we are using double buffering for WebGL,
    we do not use a drawable that implements a mechanism
    for swapping buffers.
    Therefore we have to make sure that all GL commands
    have been executed properly before copying the texture
    onto the GraphicsSurface.
    
    Reviewed by Simon Hausmann.
    
    * platform/graphics/qt/GraphicsContext3DQt.cpp:
    (WebCore::GraphicsContext3DPrivate::blitMultisampleFramebufferAndRestoreContext):
    
    Canonical link: https://commits.webkit.org/124635@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@139186 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
+++ Source/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
@@ -322,16 +322,20 @@ void GraphicsContext3DPrivate::blitMultisampleFramebuffer() const
 
 void GraphicsContext3DPrivate::blitMultisampleFramebufferAndRestoreContext() const
 {
-    if (!m_context->m_attrs.antialias)
-        return;
-
     const QOpenGLContext* currentContext = QOpenGLContext::currentContext();
     QSurface* currentSurface = 0;
     if (currentContext && currentContext != m_platformContext) {
         currentSurface = currentContext->surface();
         m_platformContext->makeCurrent(m_surface);
     }
-    blitMultisampleFramebuffer();
+
+    if (m_context->m_attrs.antialias)
+        blitMultisampleFramebuffer();
+
+    // While the context is still bound, make sure all the Framebuffer content is in finished state.
+    // This is necessary as we are doing our own double buffering instead of using a drawable that provides swapBuffers.
+    glFinish();
+
     if (currentContext && currentContext != m_platformContext)
         const_cast<QOpenGLContext*>(currentContext)->makeCurrent(currentSurface);
 }
