0236f6446eadb5910793df7a7355876b4dd917bc
Zeno Albisser <zeno.albisser@digia.com>
2013-01-09 11:25:12 +0000

    [Qt][Mac] GraphicsSurface does not need glEnable/glDisable for texture targets. https://bugs.webkit.org/show_bug.cgi?id=106310
    glEnable/glDisable for texture targets is only necessary
    when using the fixed function pipeline.
    Enabling or disabling the target might cause unexpected
    behavior to texture bindings and is therefore considered harmful.
    
    Reviewed by Noam Rosenthal.
    
    * platform/graphics/surfaces/mac/GraphicsSurfaceMac.cpp:
    (WebCore::createTexture):
    (WebCore::GraphicsSurfacePrivate::copyFromTexture):
    (WebCore::GraphicsSurface::platformCopyToGLTexture):
    
    Canonical link: https://commits.webkit.org/124631@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@139182 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/surfaces/mac/GraphicsSurfaceMac.cpp
+++ Source/WebCore/platform/graphics/surfaces/mac/GraphicsSurfaceMac.cpp
@@ -50,10 +50,7 @@ static uint32_t createTexture(IOSurfaceRef handle)
         return 0;
 
     GLint prevTexture;
-    GLboolean wasEnabled = glIsEnabled(GL_TEXTURE_RECTANGLE_ARB);
     glGetIntegerv(GL_TEXTURE_RECTANGLE_ARB, &prevTexture);
-    if (!wasEnabled)
-        glEnable(GL_TEXTURE_RECTANGLE_ARB);
 
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_RECTANGLE_ARB, texture);
@@ -64,8 +61,6 @@ static uint32_t createTexture(IOSurfaceRef handle)
     }
 
     glBindTexture(GL_TEXTURE_RECTANGLE_ARB, prevTexture);
-    if (!wasEnabled)
-        glDisable(GL_TEXTURE_RECTANGLE_ARB);
 
     return texture;
 }
@@ -216,7 +211,6 @@ public:
         glFlush(); // Make sure the texture has actually been completely written in the original context.
 
         makeCurrent();
-        glEnable(GL_TEXTURE_RECTANGLE_ARB);
 
         int x = sourceRect.x();
         int y = sourceRect.y();
@@ -319,8 +313,6 @@ void GraphicsSurface::platformCopyToGLTexture(uint32_t target, uint32_t id, cons
     if (!m_fbo)
         glGenFramebuffers(1, &m_fbo);
     glBindTexture(GL_TEXTURE_RECTANGLE_ARB, 0);
-    glEnable(GL_TEXTURE_RECTANGLE_ARB);
-    glEnable(target);
     glBindTexture(target, id);
     glBindFramebuffer(GL_READ_FRAMEBUFFER, m_fbo);
     glFramebufferTexture2D(GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_RECTANGLE_ARB, m_private->frontBufferTextureID(), 0);
