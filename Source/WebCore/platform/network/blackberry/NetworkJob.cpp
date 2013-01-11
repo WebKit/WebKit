30788f779cf181a366e4a1fcba8c6cd116f6947c
Mary Wu <mawu@blackberry.com>
2013-01-11 11:12:18 +0000

    [BlackBerry] Enable concatenating headers with same field name https://bugs.webkit.org/show_bug.cgi?id=106625
    Reviewed by Rob Buis.
    
    RFC 2616 specifies that headers could concatenate with comma if they have
    same field name. We should enable this if the header allows multiple values.
    
    RIM PR# 275508, internally reviewed by Joe Mason
    
    (WebCore):
    (WebCore::isAppendableHeader):
    (WebCore::NetworkJob::handleNotifyHeaderReceived):
    
    
    Canonical link: https://commits.webkit.org/124847@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@139423 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/network/blackberry/NetworkJob.cpp
+++ Source/WebCore/platform/network/blackberry/NetworkJob.cpp
@@ -63,6 +63,22 @@ inline static bool isUnauthorized(int statusCode)
     return statusCode == 401;
 }
 
+static char* const appendableHeaders[] = {"access-control-allow-origin", "allow",
+    "set-cookie", "set-cookie2", "vary", "via", "warning"};
+
+static bool isAppendableHeader(const String& key)
+{
+    // Non-standard header fields are conventionally marked by prefixing the field name with X-.
+    if (key.startsWith("x-"))
+        return true;
+
+    for (int i = 0; i < sizeof(appendableHeaders) /sizeof(char*); i++)
+        if (key == appendableHeaders[i])
+            return true;
+
+    return false;
+}
+
 NetworkJob::NetworkJob()
     : FrameDestructionObserver(0)
     , m_playerId(0)
@@ -336,20 +352,17 @@ void NetworkJob::handleNotifyHeaderReceived(const String& key, const String& val
         m_contentType = value.lower();
     else if (lowerKey == "content-disposition")
         m_contentDisposition = value;
-    else if (lowerKey == "set-cookie") {
-        if (m_frame && m_frame->loader() && m_frame->loader()->client()
-            && static_cast<FrameLoaderClientBlackBerry*>(m_frame->loader()->client())->cookiesEnabled()) {
-            handleSetCookieHeader(value);
-            // If there are several "Set-Cookie" headers, we should combine the following ones with the first.
-            if (m_response.httpHeaderFields().contains("Set-Cookie")) {
-                m_response.setHTTPHeaderField(key, m_response.httpHeaderField(key) + ", " + value);
-                return;
-            }
-        }
-    } else if (equalIgnoringCase(key, BlackBerry::Platform::NetworkRequest::HEADER_BLACKBERRY_FTP))
+    else if (lowerKey == "set-cookie" && m_frame && m_frame->loader() && m_frame->loader()->client()
+        && static_cast<FrameLoaderClientBlackBerry*>(m_frame->loader()->client())->cookiesEnabled())
+        handleSetCookieHeader(value);
+    else if (equalIgnoringCase(key, BlackBerry::Platform::NetworkRequest::HEADER_BLACKBERRY_FTP))
         handleFTPHeader(value);
 
-    m_response.setHTTPHeaderField(key, value);
+    if (m_response.httpHeaderFields().contains(key.utf8().data()) && isAppendableHeader(lowerKey)) {
+        // If there are several headers with same key, we should combine the following ones with the first.
+        m_response.setHTTPHeaderField(key, m_response.httpHeaderField(key) + ", " + value);
+    } else
+        m_response.setHTTPHeaderField(key, value);
 }
 
 void NetworkJob::handleNotifyMultipartHeaderReceived(const String& key, const String& value)
