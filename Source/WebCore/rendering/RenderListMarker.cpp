7fc6cec94c9bf8654bbe29065fea26fa3918ccec
Ojan Vafai <ojan@chromium.org>
2013-01-15 22:15:35 +0000

    Unreviewed, rolling out r139782. http://trac.webkit.org/changeset/139782 https://bugs.webkit.org/show_bug.cgi?id=106939
    Source/WebCore:
    
    Broke preferred widths on list markers.
    
    * rendering/RenderListMarker.cpp:
    (WebCore::RenderListMarker::layout):
    (WebCore::RenderListMarker::computePreferredLogicalWidths):
    
    LayoutTests:
    
    * fast/lists/marker-preferred-margins-expected.txt: Added.
    * fast/lists/marker-preferred-margins.html: Added.
    Adding a new test to cover the case that broke.
    
    Canonical link: https://commits.webkit.org/125172@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@139789 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderListMarker.cpp
+++ Source/WebCore/rendering/RenderListMarker.cpp
@@ -1316,8 +1316,6 @@ void RenderListMarker::layout()
 {
     StackStats::LayoutCheckPoint layoutCheckPoint;
     ASSERT(needsLayout());
-
-    updateMargins();
  
     if (isImage()) {
         setWidth(m_image->imageSize(this, style()->effectiveZoom()).width());
@@ -1369,6 +1367,7 @@ void RenderListMarker::computePreferredLogicalWidths()
         LayoutSize imageSize = m_image->imageSize(this, style()->effectiveZoom());
         m_minPreferredLogicalWidth = m_maxPreferredLogicalWidth = style()->isHorizontalWritingMode() ? imageSize.width() : imageSize.height();
         setPreferredLogicalWidthsDirty(false);
+        updateMargins();
         return;
     }
 
@@ -1479,6 +1478,8 @@ void RenderListMarker::computePreferredLogicalWidths()
     m_maxPreferredLogicalWidth = logicalWidth;
 
     setPreferredLogicalWidthsDirty(false);
+
+    updateMargins();
 }
 
 void RenderListMarker::updateMargins()
