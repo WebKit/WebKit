76a04c70f0b7da2c29a9d12edac82171528a4527
Seokju Kwon <seokju.kwon@gmail.com>
2013-01-17 18:37:54 +0000

    [EFL][CMAKE] Compress resource files of inspector https://bugs.webkit.org/show_bug.cgi?id=106210
    Patch by Seokju Kwon <seokju.kwon@gmail.com> on 2013-01-17
    Reviewed by Gyuyoung Kim.
    
    Add compressing JavaScript
    for smaller package and faster connection of remote web inspector.
    
    * Source/PlatformEfl.cmake:
    
    Canonical link: https://commits.webkit.org/125366@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@139992 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/PlatformEfl.cmake
+++ Source/PlatformEfl.cmake
@@ -17,4 +17,22 @@ if (ENABLE_INSPECTOR)
                        PATTERN "*.css"
                        PATTERN "*.gif"
                        PATTERN "*.png")
+
+    find_program(UGLIFYJS_EXECUTABLE uglifyjs)
+    if (UGLIFYJS_EXECUTABLE AND (NOT ${CMAKE_BUILD_TYPE} STREQUAL "Debug"))
+        file(GLOB frontend_js_files "${WEBCORE_DIR}/inspector/front-end/*.js")
+        set(all_js_files
+            ${frontend_js_files}
+            "${WEBCORE_DIR}/English.lproj/localizedStrings.js"
+            "${DERIVED_SOURCES_WEBCORE_DIR}/InspectorBackendCommands.js"
+        )
+
+        foreach (js_file ${all_js_files})
+            get_filename_component(filename ${js_file} NAME)
+            install(CODE
+                "execute_process(
+                    COMMAND ${UGLIFYJS_EXECUTABLE} --overwrite ${filename}
+                    WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/${WEB_INSPECTOR_DIR})")
+        endforeach ()
+    endif ()
 endif ()
