aa614f4b16f39631cd55aac18facac7f73e328ef
Chris Dumez <christophe.dumez@intel.com>
2013-01-21 13:55:36 +0000

    [EFL][WKTR] platformRunUntil() should keep the main loop running https://bugs.webkit.org/show_bug.cgi?id=107442
    Reviewed by Noam Rosenthal.
    
    EFL's implementation of platformRunUntil() should keep the main loop
    running and avoid calling sleep() between calls to
    ecore_main_loop_iterate(). This call to sleep seems to be a source
    of deadlocks (see Bug 106884).
    
    * WebKitTestRunner/efl/TestControllerEfl.cpp:
    (WTR::TestController::platformRunUntil):
    
    Canonical link: https://commits.webkit.org/125680@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@140327 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/WebKitTestRunner/efl/TestControllerEfl.cpp
+++ Tools/WebKitTestRunner/efl/TestControllerEfl.cpp
@@ -65,10 +65,9 @@ void TestController::platformRunUntil(bool& condition, double timeout)
 {
     if (timeout == m_noTimeout) {
         // Never timeout if we are debugging or not meant to timeout.
-        while (!condition) {
+        while (!condition)
             ecore_main_loop_iterate();
-            sleep(1);
-        }
+
         return;
     }
     timer = ecore_timer_loop_add(timeout, timerFired, 0);
