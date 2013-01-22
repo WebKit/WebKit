de0c6d7c7176333a04934c5de3b0c0673a35b048
Michał Pakuła vel Rutka <m.pakula@samsung.com>
2013-01-22 06:42:23 +0000

    [EFL] Missing context menus strings https://bugs.webkit.org/show_bug.cgi?id=107104
    Patch by Michał Pakuła vel Rutka <m.pakula@samsung.com> on 2013-01-21
    Reviewed by Kenneth Rohde Christiansen.
    
    Add 5 missing localization strings for text direction context menus.
    
    * platform/efl/LocalizedStringsEfl.cpp:
    (WebCore::contextMenuItemTagRightToLeft):
    (WebCore::contextMenuItemTagLeftToRight):
    (WebCore::contextMenuItemTagWritingDirectionMenu):
    (WebCore::contextMenuItemTagTextDirectionMenu):
    (WebCore::contextMenuItemTagDefaultDirection):
    
    Canonical link: https://commits.webkit.org/125735@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@140386 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/efl/LocalizedStringsEfl.cpp
+++ Source/WebCore/platform/efl/LocalizedStringsEfl.cpp
@@ -331,27 +331,27 @@ String contextMenuItemTagInspectElement()
 
 String contextMenuItemTagRightToLeft()
 {
-    return String();
+    return String::fromUTF8("Right to Left");
 }
 
 String contextMenuItemTagLeftToRight()
 {
-    return String();
+    return String::fromUTF8("Left to Right");
 }
 
 String contextMenuItemTagWritingDirectionMenu()
 {
-    return String();
+    return String::fromUTF8("Writing Direction");
 }
 
 String contextMenuItemTagTextDirectionMenu()
 {
-    return String();
+    return String::fromUTF8("Text Direction");
 }
 
 String contextMenuItemTagDefaultDirection()
 {
-    return String();
+    return String::fromUTF8("Default");
 }
 
 String searchMenuNoRecentSearchesText()
