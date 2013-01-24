40d38faeefb4cb17425de15825a37a97c80d0af6
Anders Carlsson <andersca@apple.com>
2013-01-24 21:00:27 +0000

    Add stubbed out StorageNamespaceProxy class https://bugs.webkit.org/show_bug.cgi?id=107846
    Reviewed by Sam Weinig.
    
    Source/WebCore:
    
    Make StorageNamespace.h and StorageArea.h private headers.
    
    * WebCore.xcodeproj/project.pbxproj:
    
    Source/WebKit2:
    
    Add new class.
    
    * WebKit2.xcodeproj/project.pbxproj:
    * WebProcess/Storage/StorageNamespaceProxy.cpp: Added.
    * WebProcess/Storage/StorageNamespaceProxy.h: Added.
    
    Canonical link: https://commits.webkit.org/126037@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@140712 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit2/WebProcess/Storage/StorageNamespaceProxy.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2013 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef StorageNamespaceProxy_h
+#define StorageNamespaceProxy_h
+
+#include <WebCore/StorageArea.h>
+#include <WebCore/StorageNamespace.h>
+
+namespace WebKit {
+
+class StorageNamespaceProxy : public WebCore::StorageNamespace {
+public:
+    // FIXME: Figure out what else to pass here.
+    static PassRefPtr<StorageNamespaceProxy> create();
+    virtual ~StorageNamespaceProxy();
+
+private:
+    StorageNamespaceProxy();
+
+    virtual PassRefPtr<WebCore::StorageArea> storageArea(PassRefPtr<WebCore::SecurityOrigin>) OVERRIDE;
+    virtual PassRefPtr<WebCore::StorageNamespace> copy() OVERRIDE;
+    virtual void close() OVERRIDE;
+    virtual void clearOriginForDeletion(WebCore::SecurityOrigin*) OVERRIDE;
+    virtual void clearAllOriginsForDeletion() OVERRIDE;
+    virtual void sync() OVERRIDE;
+    virtual void closeIdleLocalStorageDatabases() OVERRIDE;
+};
+
+} // namespace WebKit
+
+#endif // StorageNamespaceProxy_h
