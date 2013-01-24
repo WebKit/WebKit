4049b0a5714d318f76e6deba23c76911fa211038
Anders Carlsson <andersca@apple.com>
2013-01-24 22:32:39 +0000

    Add stubbed out StorageAreaProxy class https://bugs.webkit.org/show_bug.cgi?id=107864
    Reviewed by Beth Dakin.
    
    * WebKit2.xcodeproj/project.pbxproj:
    * WebProcess/Storage/StorageAreaProxy.cpp: Added.
    * WebProcess/Storage/StorageAreaProxy.h: Added.
    
    Canonical link: https://commits.webkit.org/126048@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@140723 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit2/WebProcess/Storage/StorageAreaProxy.cpp
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2013 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "StorageAreaProxy.h"
+
+namespace WebKit {
+
+PassRefPtr<StorageAreaProxy> StorageAreaProxy::create()
+{
+    return adoptRef(new StorageAreaProxy);
+}
+
+StorageAreaProxy::StorageAreaProxy()
+{
+}
+
+StorageAreaProxy::~StorageAreaProxy()
+{
+}
+
+unsigned StorageAreaProxy::length(WebCore::ExceptionCode&, WebCore::Frame* sourceFrame) const
+{
+    // FIXME: Implement this.
+    ASSERT_NOT_REACHED();
+}
+
+String StorageAreaProxy::key(unsigned index, WebCore::ExceptionCode&, WebCore::Frame* sourceFrame) const
+{
+    // FIXME: Implement this.
+    ASSERT_NOT_REACHED();
+    return String();
+}
+
+String StorageAreaProxy::getItem(const String& key, WebCore::ExceptionCode&, WebCore::Frame* sourceFrame) const
+{
+    // FIXME: Implement this.
+    ASSERT_NOT_REACHED();
+    return String();
+}
+
+void StorageAreaProxy::setItem(const String& key, const String& value, WebCore::ExceptionCode&, WebCore::Frame* sourceFrame)
+{
+    // FIXME: Implement this.
+    ASSERT_NOT_REACHED();
+}
+
+void StorageAreaProxy::removeItem(const String& key, WebCore::ExceptionCode&, WebCore::Frame* sourceFrame)
+{
+    // FIXME: Implement this.
+    ASSERT_NOT_REACHED();
+}
+
+void StorageAreaProxy::clear(WebCore::ExceptionCode&, WebCore::Frame* sourceFrame)
+{
+    // FIXME: Implement this.
+    ASSERT_NOT_REACHED();
+}
+
+bool StorageAreaProxy::contains(const String& key, WebCore::ExceptionCode&, WebCore::Frame* sourceFrame) const
+{
+    // FIXME: Implement this.
+    ASSERT_NOT_REACHED();
+    return false;
+}
+
+bool StorageAreaProxy::canAccessStorage(WebCore::Frame*) const
+{
+    // FIXME: Implement this.
+    ASSERT_NOT_REACHED();
+    return false;
+}
+
+size_t StorageAreaProxy::memoryBytesUsedByCache() const
+{
+    // FIXME: Implement this.
+    ASSERT_NOT_REACHED();
+    return 0;
+}
+
+void StorageAreaProxy::incrementAccessCount()
+{
+    // FIXME: Implement this.
+    ASSERT_NOT_REACHED();
+}
+
+void StorageAreaProxy::decrementAccessCount()
+{
+    // FIXME: Implement this.
+    ASSERT_NOT_REACHED();
+}
+
+void StorageAreaProxy::closeDatabaseIfIdle()
+{
+    // FIXME: Implement this.
+    ASSERT_NOT_REACHED();
+}
+
+} // namespace WebKit
---
+++ Source/WebKit2/WebProcess/Storage/StorageAreaProxy.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2013 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef StorageAreaProxy_h
+#define StorageAreaProxy_h
+
+#include <WebCore/StorageArea.h>
+
+namespace WebKit {
+
+class StorageAreaProxy : public WebCore::StorageArea {
+public:
+    static PassRefPtr<StorageAreaProxy> create();
+    virtual ~StorageAreaProxy();
+
+private:
+    StorageAreaProxy();
+
+    // WebCore::StorageArea.
+    virtual unsigned length(WebCore::ExceptionCode&, WebCore::Frame* sourceFrame) const OVERRIDE;
+    virtual String key(unsigned index, WebCore::ExceptionCode&, WebCore::Frame* sourceFrame) const OVERRIDE;
+    virtual String getItem(const String& key, WebCore::ExceptionCode&, WebCore::Frame* sourceFrame) const OVERRIDE;
+    virtual void setItem(const String& key, const String& value, WebCore::ExceptionCode&, WebCore::Frame* sourceFrame) OVERRIDE;
+    virtual void removeItem(const String& key, WebCore::ExceptionCode&, WebCore::Frame* sourceFrame) OVERRIDE;
+    virtual void clear(WebCore::ExceptionCode&, WebCore::Frame* sourceFrame) OVERRIDE;
+    virtual bool contains(const String& key, WebCore::ExceptionCode&, WebCore::Frame* sourceFrame) const OVERRIDE;
+    virtual bool canAccessStorage(WebCore::Frame*) const OVERRIDE;
+    virtual size_t memoryBytesUsedByCache() const OVERRIDE;
+    virtual void incrementAccessCount() OVERRIDE;
+    virtual void decrementAccessCount() OVERRIDE;
+    virtual void closeDatabaseIfIdle() OVERRIDE;
+};
+
+} // namespace WebKit
+
+#endif // StorageAreaProxy_h
