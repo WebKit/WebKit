8799239f2f00f999da50651877f5d75e98d015f5
Tim Volodine <timvolodine@chromium.org>
2013-01-26 19:29:18 +0000

    Text Autosizing: simplify and clean-up preOrderTraversal skipping containers https://bugs.webkit.org/show_bug.cgi?id=107446
    Patch by Tim Volodine <timvolodine@chromium.org> on 2013-01-26
    Reviewed by Julien Chaffraix.
    
    Simplify nextInPreOrderSkippingDescendantsOfContainers implementation
    using RenderObject tree traversal methods.
    
    No new tests because functionality is unchanged.
    
    * rendering/TextAutosizer.cpp:
    (WebCore::TextAutosizer::nextInPreOrderSkippingDescendantsOfContainers):
    
    Canonical link: https://commits.webkit.org/126222@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@140913 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/TextAutosizer.cpp
+++ Source/WebCore/rendering/TextAutosizer.cpp
@@ -407,17 +407,8 @@ void TextAutosizer::measureDescendantTextWidth(const RenderBlock* container, con
 RenderObject* TextAutosizer::nextInPreOrderSkippingDescendantsOfContainers(const RenderObject* current, const RenderObject* stayWithin)
 {
     if (current == stayWithin || !isAutosizingContainer(current))
-        for (RenderObject* child = current->firstChild(); child; child = child->nextSibling())
-            return child;
-
-    for (const RenderObject* ancestor = current; ancestor; ancestor = ancestor->parent()) {
-        if (ancestor == stayWithin)
-            return 0;
-        for (RenderObject* sibling = ancestor->nextSibling(); sibling; sibling = sibling->nextSibling())
-            return sibling;
-    }
-
-    return 0;
+        return current->nextInPreOrder(stayWithin);
+    return current->nextInPreOrderAfterChildren(stayWithin);
 }
 
 const RenderBlock* TextAutosizer::findDeepestBlockContainingAllText(const RenderBlock* cluster)
