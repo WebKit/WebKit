c52dd7aefbac8c5f7eb7bfc3b1b4260206120333
Kentaro Hara <haraken@chromium.org>
2013-01-26 01:28:40 +0000

    [V8] Keep a RefPtr<SerializedScriptValue*> when we call serialize()/deserialize() (part 1) https://bugs.webkit.org/show_bug.cgi?id=107891
    Reviewed by Abhishek Arya.
    
    If you use a raw SerializedScriptValue* for serialize()/deserialize(),
    it can potentially cause a use-after-free. This is because serialize()/
    deserialize() can destruct a RefPtr of the SerializedScriptValue*,
    depending on data that is serialized/deserialized. So we should keep a
    RefPtr<SerializedScriptValue*> when we call serialize()/deserialize().
    (See https://bugs.webkit.org/show_bug.cgi?id=107792 for more details.)
    
    No tests. This is just a just-in-case fix. I couldn't find any bug
    even in an ASAN build.
    
    * bindings/v8/custom/V8CustomEventCustom.cpp:
    (WebCore::V8CustomEvent::detailAccessorGetter):
    
    
    Canonical link: https://commits.webkit.org/126200@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@140887 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/custom/V8CustomEventCustom.cpp
+++ Source/WebCore/bindings/v8/custom/V8CustomEventCustom.cpp
@@ -49,7 +49,7 @@ namespace WebCore {
 v8::Handle<v8::Value> V8CustomEvent::detailAccessorGetter(v8::Local<v8::String> name, const v8::AccessorInfo& info)
 {
     CustomEvent* imp = V8CustomEvent::toNative(info.Holder());
-    SerializedScriptValue* serialized = imp->serializedScriptValue().get();
+    RefPtr<SerializedScriptValue> serialized = imp->serializedScriptValue();
     if (serialized) {
         v8::Handle<v8::Value> value = info.Holder()->GetHiddenValue(V8HiddenPropertyName::detail());
         if (value.IsEmpty()) {
