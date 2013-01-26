202025b65fdf12e27ce69fa4e26c7fa22744ec11
Dan Carney <dcarney@google.com>
2013-01-26 16:08:41 +0000

    [v8] prepare SerializedScriptValue for transition to Latin-1 https://bugs.webkit.org/show_bug.cgi?id=107655
    Patch by Dan Carney <dcarney@google.com> on 2013-01-26
    Reviewed by Kentaro Hara.
    
    No new tests. Covered by existing tests.
    
    * bindings/v8/SerializedScriptValue.cpp:
    
    Canonical link: https://commits.webkit.org/126220@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@140911 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/SerializedScriptValue.cpp
+++ Source/WebCore/bindings/v8/SerializedScriptValue.cpp
@@ -315,15 +315,22 @@ public:
 
     void writeOneByteString(v8::Handle<v8::String>& string)
     {
-        int length = string->Length();
-        ASSERT(length >= 0);
+        int stringLength = string->Length();
+        int utf8Length = string->Utf8Length();
+        ASSERT(stringLength >= 0 && utf8Length >= 0);
 
         append(StringTag);
-        doWriteUint32(static_cast<uint32_t>(length));
-        ensureSpace(length);
+        doWriteUint32(static_cast<uint32_t>(utf8Length));
+        ensureSpace(utf8Length);
 
-        string->WriteOneByte(byteAt(m_position), 0, length, v8StringWriteOptions());
-        m_position += length;
+        // ASCII fast path.
+        if (stringLength == utf8Length)
+            string->WriteOneByte(byteAt(m_position), 0, utf8Length, v8StringWriteOptions());
+        else {
+            char* buffer = reinterpret_cast<char*>(byteAt(m_position));
+            string->WriteUtf8(buffer, utf8Length, 0, v8StringWriteOptions());
+        }
+        m_position += utf8Length;
     }
 
     void writeUCharString(v8::Handle<v8::String>& string)
