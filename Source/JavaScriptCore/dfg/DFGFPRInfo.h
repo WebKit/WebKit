5e9fe5ee1726e5e146d059d1dcc24c2d0a35008a
Michael Saboff <msaboff@apple.com>
2013-01-28 18:27:06 +0000

    Cleanup ARM version of debugName() in DFGFPRInfo.h https://bugs.webkit.org/show_bug.cgi?id=108090
    Reviewed by David Kilzer.
    
    Fixed debugName() so it will compile by adding static_cast<int> and missing commas.
    
    * dfg/DFGFPRInfo.h:
    (JSC::DFG::FPRInfo::debugName):
    
    
    Canonical link: https://commits.webkit.org/126285@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@140976 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGFPRInfo.h
+++ Source/JavaScriptCore/dfg/DFGFPRInfo.h
@@ -147,15 +147,15 @@ public:
     static const char* debugName(FPRReg reg)
     {
         ASSERT(reg != InvalidFPRReg);
-        ASSERT(reg < 32);
+        ASSERT(static_cast<int>(reg) < 32);
         static const char* nameForRegister[32] = {
             "d0", "d1", "d2", "d3",
             "d4", "d5", "d6", "d7",
             "d8", "d9", "d10", "d11",
-            "d12", "d13", "d14", "d15"
-            "d16", "d17", "d18", "d19"
-            "d20", "d21", "d22", "d23"
-            "d24", "d25", "d26", "d27"
+            "d12", "d13", "d14", "d15",
+            "d16", "d17", "d18", "d19",
+            "d20", "d21", "d22", "d23",
+            "d24", "d25", "d26", "d27",
             "d28", "d29", "d30", "d31"
         };
         return nameForRegister[reg];
