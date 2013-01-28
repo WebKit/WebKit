8e39e5a20d755dd4caad69ca72a5b23bef928a70
Matt Falkenhagen <falken@chromium.org>
2013-01-28 01:55:04 +0000

    Elements must be reattached when inserted/removed from top layer https://bugs.webkit.org/show_bug.cgi?id=105489
    Relanding r139402 as the apparent perf regression has been explained as not real (bug 106726).
    
    Reviewed by Julien Chaffraix.
    
    Source/WebCore:
    
    Ensure a reattach occurs when an element is inserted/removed from top layer, so its renderer can be inserted correctly:
    as a child of RenderView in top layer sibling order if it's in the top layer, and in the usual place otherwise.
    
    We previously relied on style recalc to catch when an element is inserted/removed from the top layer, because it
    only happens on dialog.show/close which toggle display: none. But that is incorrect because, for example, close()
    followed immediately by show() results in no style change.
    
    Tests: fast/dom/HTMLDialogElement/removed-element-is-removed-from-top-layer.html
           fast/dom/HTMLDialogElement/top-layer-stacking-correct-order-remove-readd.html
    
    * dom/Element.cpp:
    (WebCore::Element::removedFrom): Call Document::removeFromTopLayer to let the element be removed from the top layer vector.
    removeFromTopLayer calls Element::setIsInTopLayer(false) itself if needed.
    (WebCore::Element::setIsInTopLayer): Ensure a reattach occurs if the element is already attached.
    
    LayoutTests:
    
    * fast/dom/HTMLDialogElement/removed-element-is-removed-from-top-layer-expected.html: Added.
    * fast/dom/HTMLDialogElement/removed-element-is-removed-from-top-layer.html: Added.
    This tests that a top layer element removed from the document does not reappear in the top layer if readded.
    This test actually would pass before this patch, but just by good fortune (see bug).
    * fast/dom/HTMLDialogElement/top-layer-stacking-correct-order-remove-readd-expected.html: Added.
    * fast/dom/HTMLDialogElement/top-layer-stacking-correct-order-remove-readd.html: Added.
    This tests that top layer ordering is correct after removing and readding an element to the top layer.
    
    
    Canonical link: https://commits.webkit.org/126240@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@140931 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/Element.cpp
+++ Source/WebCore/dom/Element.cpp
@@ -1183,7 +1183,7 @@ void Element::removedFrom(ContainerNode* insertionPoint)
         after->removedFrom(insertionPoint);
 
 #if ENABLE(DIALOG_ELEMENT)
-    setIsInTopLayer(false);
+    document()->removeFromTopLayer(this);
 #endif
 #if ENABLE(FULLSCREEN_API)
     if (containsFullScreenElement())
@@ -2404,7 +2404,10 @@ void Element::setIsInTopLayer(bool inTopLayer)
     if (isInTopLayer() == inTopLayer)
         return;
     ensureElementRareData()->setIsInTopLayer(inTopLayer);
-    setNeedsStyleRecalc(SyntheticStyleChange);
+
+    // We must ensure a reattach occurs so the renderer is inserted in the correct sibling order under RenderView according to its
+    // top layer position, or in its usual place if not in the top layer.
+    reattachIfAttached();
 }
 #endif
 
