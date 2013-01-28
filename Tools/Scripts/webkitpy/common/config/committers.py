05c37cd3ffa6d44a80d42f74d519efe3cc7fa9c6
Raymond Toy <rtoy@google.com>
2013-01-28 23:15:26 +0000

    Add myself to committers.py https://bugs.webkit.org/show_bug.cgi?id=107992
    Reviewed by kbr@google.com.
    
    * Scripts/webkitpy/common/config/committers.py:
    
    Canonical link: https://commits.webkit.org/126324@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@141016 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/config/committers.py
+++ Tools/Scripts/webkitpy/common/config/committers.py
@@ -404,6 +404,7 @@ committers_unable_to_review = [
     Committer("Rafael Weinstein", "rafaelw@chromium.org", "rafaelw"),
     Committer("Raphael Kubo da Costa", ["rakuco@webkit.org", "rakuco@FreeBSD.org", "raphael.kubo.da.costa@intel.com"], "rakuco"),
     Committer("Ravi Kasibhatla", "ravi.kasibhatla@motorola.com", "kphanee"),
+    Committer("Raymond Toy", "rtoy@google.com", "rtoy"),
     Committer("Renata Hodovan", "reni@webkit.org", "reni"),
     Committer("Robert Hogan", ["robert@webkit.org", "robert@roberthogan.net", "lists@roberthogan.net"], "mwenge"),
     Committer("Robert Kroeger", "rjkroege@chromium.org", "rjkroege"),
