5de0226524ce3c8fab415ae2ec11ac393e33c198
Kiran Muppala <cmuppala@apple.com>
2013-01-30 00:43:55 +0000

    Fix erroneous semicolon causing build failure: if statement has empty body [-Werror,-Wempty-body] https://bugs.webkit.org/show_bug.cgi?id=108241
    Patch by Kiran Muppala <cmuppala@apple.com> on 2013-01-29
    Reviewed by Anders Carlsson.
    
    * UIProcess/WebProcessProxy.cpp:
    (WebKit::WebProcessProxy::addExistingWebPage): Remove erroneous
    semicolon following the if condition.
    
    Canonical link: https://commits.webkit.org/126467@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@141180 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/WebProcessProxy.cpp
+++ Source/WebKit2/UIProcess/WebProcessProxy.cpp
@@ -188,7 +188,7 @@ void WebProcessProxy::addExistingWebPage(WebPageProxy* webPage, uint64_t pageID)
     m_pageMap.set(pageID, webPage);
     globalPageMap().set(pageID, webPage);
 #if PLATFORM(MAC)
-    if (pageIsProcessSuppressible(webPage));
+    if (pageIsProcessSuppressible(webPage))
         m_processSuppressiblePages.add(pageID);
     updateProcessSuppressionState();
 #endif
