4bb9420ae38edd767b067c1781be9a7981d70f05
Chris Hopman <cjhopman@chromium.org>
2013-01-31 20:55:33 +0000

    [chromium] Make selection handling work in applyPageScaleInCompositor mode https://bugs.webkit.org/show_bug.cgi?id=107831
    Patch by Chris Hopman <cjhopman@chromium.org> on 2013-01-31
    Reviewed by Ryosuke Niwa.
    
    These functions expect a window point. When in
    applyPageScaleFactorInCompositor mode, the points need to be unscaled
    by the page scale factor.
    
    * src/WebFrameImpl.cpp:
    (WebKit::WebFrameImpl::selectRange):
    (WebKit::WebFrameImpl::moveCaretSelectionTowardsWindowPoint):
    
    Canonical link: https://commits.webkit.org/126739@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@141470 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/chromium/src/WebFrameImpl.cpp
+++ Source/WebKit/chromium/src/WebFrameImpl.cpp
@@ -1383,8 +1383,14 @@ bool WebFrameImpl::selectWordAroundCaret()
 
 void WebFrameImpl::selectRange(const WebPoint& base, const WebPoint& extent)
 {
-    VisiblePosition basePosition = visiblePositionForWindowPoint(base);
-    VisiblePosition extentPosition = visiblePositionForWindowPoint(extent);
+    IntPoint unscaledBase = base;
+    IntPoint unscaledExtent = extent;
+    if (frame()->page()->settings()->applyPageScaleFactorInCompositor()) {
+        unscaledExtent.scale(1 / view()->pageScaleFactor(), 1 / view()->pageScaleFactor());
+        unscaledBase.scale(1 / view()->pageScaleFactor(), 1 / view()->pageScaleFactor());
+    }
+    VisiblePosition basePosition = visiblePositionForWindowPoint(unscaledBase);
+    VisiblePosition extentPosition = visiblePositionForWindowPoint(unscaledExtent);
     VisibleSelection newSelection = VisibleSelection(basePosition, extentPosition);
     if (frame()->selection()->shouldChangeSelection(newSelection))
         frame()->selection()->setSelection(newSelection, CharacterGranularity);
@@ -1398,8 +1404,12 @@ void WebFrameImpl::selectRange(const WebRange& webRange)
 
 void WebFrameImpl::moveCaretSelectionTowardsWindowPoint(const WebPoint& point)
 {
+    IntPoint unscaledPoint(point);
+    if (frame()->page()->settings()->applyPageScaleFactorInCompositor())
+        unscaledPoint.scale(1 / view()->pageScaleFactor(), 1 / view()->pageScaleFactor());
+
     Element* editable = frame()->selection()->rootEditableElement();
-    IntPoint contentsPoint = frame()->view()->windowToContents(IntPoint(point));
+    IntPoint contentsPoint = frame()->view()->windowToContents(unscaledPoint);
     LayoutPoint localPoint(editable->convertFromPage(contentsPoint));
     VisiblePosition position = editable->renderer()->positionForPoint(localPoint);
     if (frame()->selection()->shouldChangeSelection(position))
