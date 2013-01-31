928191f7b57d357ecc443bddc0ae05d6e68bac93
Florin Malita <fmalita@chromium.org>
2013-01-31 23:38:04 +0000

    [Chromium] Unreviewed gardening.
    WebViewTest.SetCompositionFromExistingText failing after r141479.
    
    * tests/WebViewTest.cpp:
    
    Canonical link: https://commits.webkit.org/126763@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@141496 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/chromium/tests/WebViewTest.cpp
+++ Source/WebKit/chromium/tests/WebViewTest.cpp
@@ -434,7 +434,8 @@ TEST_F(WebViewTest, ExtendSelectionAndDelete)
     webView->close();
 }
 
-TEST_F(WebViewTest, SetCompositionFromExistingText)
+// Disabled, https://bugs.webkit.org/show_bug.cgi?id=108543
+TEST_F(WebViewTest, DISABLED_SetCompositionFromExistingText)
 {
     URLTestHelpers::registerMockedURLFromBaseURL(WebString::fromUTF8(m_baseURL.c_str()), WebString::fromUTF8("input_field_populated.html"));
     WebView* webView = FrameTestHelpers::createWebViewAndLoad(m_baseURL + "input_field_populated.html");
