049ff837da21f3cdc85c0e27d5a8dbf9ffe21de8
Grzegorz Czajkowski <g.czajkowski@samsung.com>
2013-01-31 17:59:22 +0000

    On Linux, can't get spelling suggestions without selection when unified text checker is enabled https://bugs.webkit.org/show_bug.cgi?id=107684
    Reviewed by Ryosuke Niwa.
    
    No new tests, covered by context-menu-suggestions.html.
    
    * editing/Editor.cpp:
    (WebCore::Editor::guessesForMisspelledOrUngrammatical):
    For Linux, extend selection range to the closest word to get
    the spelling suggestions when unified text checker is enabled.
    
    
    Canonical link: https://commits.webkit.org/126699@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@141429 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/editing/Editor.cpp
+++ Source/WebCore/editing/Editor.cpp
@@ -1776,7 +1776,14 @@ Vector<String> Editor::guessesForMisspelledWord(const String& word) const
 Vector<String> Editor::guessesForMisspelledOrUngrammatical(bool& misspelled, bool& ungrammatical)
 {
     if (unifiedTextCheckerEnabled()) {
-        RefPtr<Range> range = frame()->selection()->toNormalizedRange();
+        RefPtr<Range> range;
+        FrameSelection* frameSelection = frame()->selection();
+        if (frameSelection->isCaret() && behavior().shouldAllowSpellingSuggestionsWithoutSelection()) {
+            VisibleSelection wordSelection = VisibleSelection(frameSelection->base());
+            wordSelection.expandUsingGranularity(WordGranularity);
+            range = wordSelection.toNormalizedRange();
+        } else
+            range = frameSelection->toNormalizedRange();
         if (!range)
             return Vector<String>();
         return TextCheckingHelper(client(), range).guessesForMisspelledOrUngrammaticalRange(isGrammarCheckingEnabled(), misspelled, ungrammatical);
