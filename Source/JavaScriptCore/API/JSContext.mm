6db3421f55305be7bc1e6639f881ce74b0edfd0b
Mark Hahnenberg <mhahnenberg@apple.com>
2013-01-31 22:33:55 +0000

    Objective-C API: JSContext exception property causes reference cycle https://bugs.webkit.org/show_bug.cgi?id=107778
    Reviewed by Darin Adler.
    
    JSContext has a (retain) JSValue * exception property which, when non-null, creates a
    reference cycle (since the JSValue * holds a strong reference back to the JSContext *).
    
    * API/JSContext.mm: Instead of JSValue *, we now use a plain JSValueRef, which eliminates the reference cycle.
    (-[JSContext initWithVirtualMachine:]):
    (-[JSContext setException:]):
    (-[JSContext exception]):
    
    
    Canonical link: https://commits.webkit.org/126757@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@141490 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/API/JSContext.mm
+++ Source/JavaScriptCore/API/JSContext.mm
@@ -34,6 +34,7 @@
 #import "JavaScriptCore.h"
 #import "ObjcRuntimeExtras.h"
 #import "Operations.h"
+#import "StrongInlines.h"
 #import <wtf/HashSet.h>
 
 #if JS_OBJC_API_ENABLED
@@ -42,9 +43,9 @@
     JSVirtualMachine *m_virtualMachine;
     JSGlobalContextRef m_context;
     JSWrapperMap *m_wrapperMap;
+    JSC::Strong<JSC::JSObject> m_exception;
 }
 
-@synthesize exception;
 @synthesize exceptionHandler;
 
 - (id)init
@@ -62,7 +63,6 @@
     m_context = JSGlobalContextCreateInGroup(getGroupFromVirtualMachine(virtualMachine), 0);
     m_wrapperMap = [[JSWrapperMap alloc] initWithContext:self];
 
-    self.exception = nil;
     self.exceptionHandler = ^(JSContext *context, JSValue *exceptionValue) {
         context.exception = exceptionValue;
     };
@@ -82,6 +82,21 @@
         return [self valueFromNotifyException:exceptionValue];
 
     return [JSValue valueWithValue:result inContext:self];
+}
+
+- (void)setException:(JSValue *)value
+{
+    if (value)
+        m_exception.set(toJS(m_context)->globalData(), toJS(JSValueToObject(m_context, valueInternalValue(value), 0)));
+    else
+        m_exception.clear();
+}
+
+- (JSValue *)exception
+{
+    if (!m_exception)
+        return nil;
+    return [JSValue valueWithValue:toRef(m_exception.get()) inContext:self];
 }
 
 - (JSWrapperMap *)wrapperMap
