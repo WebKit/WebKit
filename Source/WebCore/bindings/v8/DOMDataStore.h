fe1a7a526fa23f7d90bb1ad9a146f01298cea538
Kentaro Hara <haraken@chromium.org>
2013-02-01 08:27:10 +0000

    [V8] Remove V8GCController::m_edenNodes https://bugs.webkit.org/show_bug.cgi?id=108579
    Reviewed by Adam Barth.
    
    Currently V8GCController::m_edenNodes stores a list of nodes whose
    wrappers have been created since the latest GC. The reason why we
    needed m_edenNodes is that there was no way to know a list of wrappers
    in the new space of V8. By using m_edenNodes, we had been approximating
    'wrappers in the new space' by 'wrappers that have been created since
    the latest GC'.
    
    Now V8 provides VisitHandlesForPartialDependence(), with which WebKit
    can know a list of wrappers in the new space. By using the API, we can
    remove V8GCController::m_edenNodes. The benefit is that (1) we no longer
    need to keep m_edenNodes and that (2) it enables more precise minor
    DOM GC (Remember that m_edenNodes was just an approximation).
    
    Performance benchmark: https://bugs.webkit.org/attachment.cgi?id=185940
    The benchmark runs 300 iterations, each of which creates 100000 elements.
    The benchmark measures average, min, median, max and stdev of execution times
    of the 300 iterations. This will tell us the worst-case overhead of this change.
    
    Before:
      mean=59.91ms, min=39ms, median=42ms, max=258ms, stdev=47.48ms
    
    After:
      mean=58.75ms, min=35ms, median=41ms, max=250ms, stdev=47.32ms
    
    As shown above, I couldn't observe any performance regression.
    
    No tests. No change in behavior.
    
    * bindings/v8/DOMDataStore.h:
    (WebCore::DOMDataStore::setWrapperInObject):
    * bindings/v8/V8GCController.cpp:
    (WebCore::gcTree):
    (WebCore):
    (MinorGCWrapperVisitor):
    (WebCore::MinorGCWrapperVisitor::notifyFinished):
    (WebCore::V8GCController::gcPrologue):
    (WebCore::V8GCController::minorGCPrologue):
    (WebCore::V8GCController::majorGCPrologue):
    * bindings/v8/V8GCController.h:
    (V8GCController):
    
    
    Canonical link: https://commits.webkit.org/126813@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@141548 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/DOMDataStore.h
+++ Source/WebCore/bindings/v8/DOMDataStore.h
@@ -164,13 +164,6 @@ private:
         object->setWrapper(wrapper);
         wrapper.MakeWeak(object, weakCallback);
     }
-    static void setWrapperInObject(Node* object, v8::Persistent<v8::Object> wrapper)
-    {
-        ASSERT(object->wrapper().IsEmpty());
-        object->setWrapper(wrapper);
-        V8GCController::didCreateWrapperForNode(object);
-        wrapper.MakeWeak(static_cast<ScriptWrappable*>(object), weakCallback);
-    }
 
     static void weakCallback(v8::Persistent<v8::Value>, void* context);
 
--- Source/WebCore/bindings/v8/V8GCController.h
+++ Source/WebCore/bindings/v8/V8GCController.h
@@ -52,7 +52,6 @@ public:
     static void collectGarbage();
 
     static Node* opaqueRootForGC(Node*);
-    static void didCreateWrapperForNode(Node*);
 
 private:
     static Vector<Node*>* m_edenNodes;
