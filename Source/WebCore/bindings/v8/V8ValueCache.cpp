2989d30a622561dcf6837b5ed40d778ea3cded71
Dan Carney <dcarney@google.com>
2013-02-04 11:34:31 +0000

    [v8] disable ascii check once latin-1 is enabled in v8 https://bugs.webkit.org/show_bug.cgi?id=108805
    Patch by Dan Carney <dcarney@google.com> on 2013-02-04
    Reviewed by Kentaro Hara.
    
    No new tests. No change in functionality.
    
    * bindings/v8/V8ValueCache.cpp:
    (WebCore::makeExternalString):
    
    Canonical link: https://commits.webkit.org/127014@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@141752 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/v8/V8ValueCache.cpp
+++ Source/WebCore/bindings/v8/V8ValueCache.cpp
@@ -42,7 +42,11 @@ namespace WebCore {
 
 static v8::Local<v8::String> makeExternalString(const String& string)
 {
+#ifdef V8_ONE_BYTE_STRINGS_ENABLED
+    if (string.is8Bit()) {
+#else
     if (string.is8Bit() && string.containsOnlyASCII()) {
+#endif
         WebCoreStringResource8* stringResource = new WebCoreStringResource8(string);
         v8::Local<v8::String> newString = v8::String::NewExternal(stringResource);
         if (newString.IsEmpty())
