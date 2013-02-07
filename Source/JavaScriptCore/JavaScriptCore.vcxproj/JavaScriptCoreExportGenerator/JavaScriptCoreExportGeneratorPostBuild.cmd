8679290d136f1da0ece72c5ba1c4470adc8cba95
Roger Fong <roger_fong@apple.com>
2013-02-07 02:00:12 +0000

    Unreviewed. Touchups to VS2010 WebKit solution. Fix an export generator script, modify some property sheets, add resouce file. Add WinLauncher projects to solution.
    * JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorDebug.props:
    * JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorPostBuild.cmd:
    * JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorRelease.props:
    * JavaScriptCore.vcxproj/resource.h: Added.
    * WebKit.vcxproj/WebKit.sln:
    
    
    Canonical link: https://commits.webkit.org/127308@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@142060 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorPostBuild.cmd
+++ Source/JavaScriptCore/JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorPostBuild.cmd
@@ -1,8 +1,9 @@
 set GeneratorDirectory=%CONFIGURATIONBUILDDIR%\obj\JavaScriptCoreExportGenerator
-echo Exporting link definition output (%GeneratorDirectory%\JavaScriptCoreExports.def)
-if exist "%OUTDIR%\JavaScriptCoreExportGenerator.exe" "%OUTDIR%\JavaScriptCoreExportGenerator.exe" > "%GeneratorDirectory%\JavaScriptCoreExports.def"
 
 mkdir 2>NUL "%CONFIGURATIONBUILDDIR%\bin"
 if exist "%WebKit_Libraries%\bin\pthreadVC2.dll" xcopy /y /d "%WebKit_Libraries%\bin\pthreadVC2.dll" "%CONFIGURATIONBUILDDIR%\bin"
+
+echo Exporting link definition output (%GeneratorDirectory%\JavaScriptCoreExports.def)
+if exist "%OUTDIR%\JavaScriptCoreExportGenerator.exe" "%OUTDIR%\JavaScriptCoreExportGenerator.exe" > "%GeneratorDirectory%\JavaScriptCoreExports.def"
 
 if exist "%CONFIGURATIONBUILDDIR%\buildfailed" del "%CONFIGURATIONBUILDDIR%\buildfailed"
