b0ae72f5765597e3648098e91270ddeaaf584a0c
Andreas Kling <akling@apple.com>
2013-02-10 06:41:34 +0000

    Shrink-wrap UnlinkedCodeBlock members. <http://webkit.org/b/109368>
    Reviewed by Oliver Hunt.
    
    Rearrange the members of UnlinkedCodeBlock to avoid unnecessary padding on 64-bit.
    Knocks ~600 KB off of the Membuster3 peak.
    
    * bytecode/UnlinkedCodeBlock.cpp:
    (JSC::UnlinkedCodeBlock::UnlinkedCodeBlock):
    * bytecode/UnlinkedCodeBlock.h:
    (UnlinkedCodeBlock):
    
    Canonical link: https://commits.webkit.org/127628@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@142387 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/bytecode/UnlinkedCodeBlock.h
+++ Source/JavaScriptCore/bytecode/UnlinkedCodeBlock.h
@@ -486,8 +486,6 @@ private:
     RefCountedArray<UnlinkedInstruction> m_unlinkedInstructions;
 
     int m_numParameters;
-    JSGlobalData* m_globalData;
-
     int m_thisRegister;
     int m_argumentsRegister;
     int m_activationRegister;
@@ -498,11 +496,21 @@ private:
     bool m_isStrictMode : 1;
     bool m_isConstructor : 1;
     bool m_hasCapturedVariables : 1;
+    CodeType m_codeType : 2;
+    CodeFeatures m_features : 16;
+
     unsigned m_firstLine;
     unsigned m_lineCount;
 
-    CodeFeatures m_features;
-    CodeType m_codeType;
+    unsigned m_resolveOperationCount;
+    unsigned m_putToBaseOperationCount;
+    unsigned m_arrayProfileCount;
+    unsigned m_arrayAllocationProfileCount;
+    unsigned m_objectAllocationProfileCount;
+    unsigned m_valueProfileCount;
+    unsigned m_llintCallLinkInfoCount;
+
+    JSGlobalData* m_globalData;
 
     Vector<unsigned> m_jumpTargets;
 
@@ -523,14 +531,6 @@ private:
     Vector<Comment>  m_bytecodeComments;
     size_t m_bytecodeCommentIterator;
 #endif
-
-    unsigned m_resolveOperationCount;
-    unsigned m_putToBaseOperationCount;
-    unsigned m_arrayProfileCount;
-    unsigned m_arrayAllocationProfileCount;
-    unsigned m_objectAllocationProfileCount;
-    unsigned m_valueProfileCount;
-    unsigned m_llintCallLinkInfoCount;
 
 public:
     struct RareData {
