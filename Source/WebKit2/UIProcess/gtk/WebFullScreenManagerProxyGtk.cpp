73da7e033ea98ec12fb5621f93e1933487afdc44
Žan Doberšek <zdobersek@igalia.com>
2013-02-10 10:42:48 +0000

    [WebKit2][Gtk] Remove the fullscreen manager proxy message receiver upon invalidating https://bugs.webkit.org/show_bug.cgi?id=109352
    Patch by Zan Dobersek <zdobersek@igalia.com> on 2013-02-10
    Reviewed by Sam Weinig.
    
    As added for the Mac port in r142160 due to the changes in the same revision, remove
    the fullscreen manager proxy as a message receiver. Also fixes a failing unit test.
    
    * UIProcess/gtk/WebFullScreenManagerProxyGtk.cpp:
    (WebKit::WebFullScreenManagerProxy::invalidate):
    
    
    Canonical link: https://commits.webkit.org/127634@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@142393 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/gtk/WebFullScreenManagerProxyGtk.cpp
+++ Source/WebKit2/UIProcess/gtk/WebFullScreenManagerProxyGtk.cpp
@@ -35,6 +35,7 @@ namespace WebKit {
 
 void WebFullScreenManagerProxy::invalidate()
 {
+    m_page->process()->removeMessageReceiver(Messages::WebFullScreenManagerProxy::messageReceiverName(), m_page->pageID());
     m_webView = 0;
 }
 
