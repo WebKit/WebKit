70e6a19e8b9b1973542a235dfcfc191e7644f583
Zhenyao Mo <zmo@google.com>
2013-02-12 00:31:15 +0000

    WEBGL_compressed_texture_s3tc extension can be enabled even when not supported https://bugs.webkit.org/show_bug.cgi?id=109508
    Reviewed by Kenneth Russell.
    
    * html/canvas/WebGLRenderingContext.cpp:
    (WebCore):
    (WebCore::WebGLRenderingContext::getExtension): Check whether the extension support is there before returning the extension pointer.
    
    
    Canonical link: https://commits.webkit.org/127784@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@142545 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/canvas/WebGLRenderingContext.cpp
+++ Source/WebCore/html/canvas/WebGLRenderingContext.cpp
@@ -2405,9 +2405,10 @@ WebGLExtension* WebGLRenderingContext::getExtension(const String& name)
             m_webglLoseContext = WebGLLoseContext::create(this);
         return m_webglLoseContext.get();
     }
-    if (equalIgnoringCase(name, "WEBGL_compressed_texture_s3tc")
-        // FIXME: remove this after a certain grace period.
-        || equalIgnoringCase(name, "WEBKIT_WEBGL_compressed_texture_s3tc")) {
+    if ((equalIgnoringCase(name, "WEBGL_compressed_texture_s3tc")
+         // FIXME: remove this after a certain grace period.
+         || equalIgnoringCase(name, "WEBKIT_WEBGL_compressed_texture_s3tc"))
+        && WebGLCompressedTextureS3TC::supported(this)) {
         if (!m_webglCompressedTextureS3TC)
             m_webglCompressedTextureS3TC = WebGLCompressedTextureS3TC::create(this);
         return m_webglCompressedTextureS3TC.get();
