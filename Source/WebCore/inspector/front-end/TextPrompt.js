a7fab625b55c573f52c1c2bc167b8afc5bb948ed
Alexander Pavlov <apavlov@chromium.org>
2013-02-12 14:15:45 +0000

    Web Inspector: [SuggestBox] SuggestBox not hidden when prefix is empty and there is preceding input https://bugs.webkit.org/show_bug.cgi?id=109568
    Reviewed by Vsevolod Vlasov.
    
    The suggestbox would get hidden in the case of empty input, yet it should get hidden
    in the case of empty user-entered prefix (which is a wider notion.)
    
    * inspector/front-end/TextPrompt.js:
    (WebInspector.TextPrompt.prototype.complete):
    
    
    Canonical link: https://commits.webkit.org/127850@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@142611 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/TextPrompt.js
+++ Source/WebCore/inspector/front-end/TextPrompt.js
@@ -371,7 +371,8 @@ WebInspector.TextPrompt.prototype = {
             return;
 
         var selectionRange = selection.getRangeAt(0);
-        var isEmptyInput = selectionRange.commonAncestorContainer === this._element; // this._element has no child Text nodes.
+        var wordPrefixRange = selectionRange.startContainer.rangeOfWord(selectionRange.startOffset, this._completionStopCharacters, this._element, "backward");
+        var isEmptyInput = wordPrefixRange.collapsed;
 
         var shouldExit;
 
@@ -395,7 +396,6 @@ WebInspector.TextPrompt.prototype = {
             return;
         }
 
-        var wordPrefixRange = selectionRange.startContainer.rangeOfWord(selectionRange.startOffset, this._completionStopCharacters, this._element, "backward");
         this._waitingForCompletions = true;
         this._loadCompletions(this.proxyElement, wordPrefixRange, force, this._completionsReady.bind(this, selection, auto, wordPrefixRange, !!reverse));
     },
