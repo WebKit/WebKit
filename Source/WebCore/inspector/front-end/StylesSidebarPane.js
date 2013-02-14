7254f14d893978658c843307db66f0a200817f64
Vladislav Kaznacheev <kaznacheev@chromium.org>
2013-02-14 15:15:17 +0000

    Web Inspector: Color picker should not be available in Computed Styles pane. https://bugs.webkit.org/show_bug.cgi?id=109697
    Patch by Vladislav Kaznacheev <kaznacheev@chromium.org> on 2013-02-14
    Reviewed by Alexander Pavlov.
    
    Changed the parentPane parameter of WebInspector.ComputedStylePropertiesSection to the correct value
    (the ComputedStyleSidebarPane instance).
    
    * inspector/front-end/StylesSidebarPane.js:
    (WebInspector.StylesSidebarPane.prototype._rebuildSectionsForStyleRules):
    
    Canonical link: https://commits.webkit.org/128096@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@142876 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/StylesSidebarPane.js
+++ Source/WebCore/inspector/front-end/StylesSidebarPane.js
@@ -605,7 +605,7 @@ WebInspector.StylesSidebarPane.prototype = {
                 editable = true;
 
             if (computedStyle)
-                var section = new WebInspector.ComputedStylePropertiesSection(this, styleRule, usedProperties);
+                var section = new WebInspector.ComputedStylePropertiesSection(this._computedStylePane, styleRule, usedProperties);
             else {
                 var section = new WebInspector.StylePropertiesSection(this, styleRule, editable, styleRule.isInherited, lastWasSeparator);
                 section._markSelectorMatches();
@@ -1353,7 +1353,7 @@ WebInspector.StylePropertiesSection.prototype = {
 /**
  * @constructor
  * @extends {WebInspector.PropertiesSection}
- * @param {!WebInspector.StylesSidebarPane} parentPane
+ * @param {!WebInspector.ComputedStyleSidebarPane} parentPane
  * @param {!Object} styleRule
  * @param {!Object.<string, boolean>} usedProperties
  */
