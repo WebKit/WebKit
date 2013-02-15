a35ff738297aae8a9e86c7a6387fd687233da1f9
Roger Fong <roger_fong@apple.com>
2013-02-15 19:17:32 +0000

    Get Win EWS startup script checked into tree so we can make changes to all the EWS bots more easily.
    * EWSTools/start-queue-win.sh: Added.
    
    
    Canonical link: https://commits.webkit.org/128237@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@143022 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Tools/EWSTools/start-queue-win.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+export WEBKIT_BUGZILLA_USERNAME=buildbot@hotmail.com
+export WEBKIT_BUGZILLA_PASSWORD=intrigue
+while :
+do
+	date
+	echo "TASK: cleaning up old log files."
+    cd ~/win-ews-logs
+    rm -rf win-ews.old
+    mv win-ews.log win-ews.old
+    find ~/win-ews-logs -mtime +7 -exec rm -f {} \;
+    echo "TASK: Starting up"
+	cd ~/WebKit
+	echo "TASK: Cleaning WebKitBuild"
+	rm -rf WebKitBuild
+	date
+	echo "TASK: Cleaning up"
+	svn cleanup
+	date
+	echo "TASK: svn revert -R *"
+	svn revert -R *
+	date
+	echo "TASK: svn status"
+	svn status | grep "?" | awk '{print $2}' | xargs rm -rf
+	date
+	echo "TASK: webkit-patch clean"
+	~/WebKit/Tools/Scripts/webkit-patch clean
+	date
+	echo "TASK: svn up --non-interactive"
+	svn up --non-interactive
+	date
+	echo "TASK: kill orphaned tasks"
+	taskkill.exe /f /im cl.exe
+	echo "TASK: webkit-patch win-ews"
+	~/WebKit/Tools/Scripts/webkit-patch win-ews --bot-id=APPLE-EWS-4 --no-confirm --exit-after-iteration 10
+	echo "TASK: reboot"
+	shutdown /r /t 0
+done
