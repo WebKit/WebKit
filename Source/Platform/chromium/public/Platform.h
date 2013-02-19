77a1adc0172e40a65d856b767186366032d75c7d
Tommy Widenflycht <tommyw@google.com>
2013-02-19 20:36:36 +0000

    WebAudio API: Change the Platform::createAudioDevice function to take a DeviceId parameter https://bugs.webkit.org/show_bug.cgi?id=110192
    Reviewed by Adam Barth.
    
    * chromium/public/Platform.h:
    (WebKit::Platform::createAudioDevice):
    (Platform):
    
    
    Canonical link: https://commits.webkit.org/128555@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@143372 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/Platform/chromium/public/Platform.h
+++ Source/Platform/chromium/public/Platform.h
@@ -117,10 +117,11 @@ public:
 
     // Creates a device for audio I/O.
     // Pass in (numberOfInputChannels > 0) if live/local audio input is desired.
-    virtual WebAudioDevice* createAudioDevice(size_t bufferSize, unsigned numberOfInputChannels, unsigned numberOfChannels, double sampleRate, WebAudioDevice::RenderCallback*) { return 0; }
+    virtual WebAudioDevice* createAudioDevice(size_t bufferSize, unsigned numberOfInputChannels, unsigned numberOfChannels, double sampleRate, WebAudioDevice::RenderCallback*, const WebString& deviceId) { return 0; }
 
-    // FIXME: remove deprecated API once chromium switches over to new method.
+    // FIXME: remove deprecated APIs once chromium switches over to new method.
     virtual WebAudioDevice* createAudioDevice(size_t bufferSize, unsigned numberOfChannels, double sampleRate, WebAudioDevice::RenderCallback*) { return 0; }
+    virtual WebAudioDevice* createAudioDevice(size_t bufferSize, unsigned numberOfInputChannels, unsigned numberOfChannels, double sampleRate, WebAudioDevice::RenderCallback*) { return 0; }
 
 
     // Blob ----------------------------------------------------------------
