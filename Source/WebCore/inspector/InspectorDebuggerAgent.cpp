175df9daed5f80b427a44122d931d6a43bc73fdb
Pavel Podivilov <podivilov@chromium.org>
2013-02-19 16:12:47 +0000

    Web Inspector: release backtrace object group on each step. https://bugs.webkit.org/show_bug.cgi?id=110223
    Reviewed by Pavel Feldman.
    
    * inspector/InspectorDebuggerAgent.cpp:
    (WebCore):
    (WebCore::InspectorDebuggerAgent::stepOver):
    (WebCore::InspectorDebuggerAgent::stepInto):
    (WebCore::InspectorDebuggerAgent::stepOut):
    
    Canonical link: https://commits.webkit.org/128521@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@143334 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/InspectorDebuggerAgent.cpp
+++ Source/WebCore/inspector/InspectorDebuggerAgent.cpp
@@ -60,7 +60,7 @@ static const char javaScriptBreakpoints[] = "javaScriptBreakopints";
 static const char pauseOnExceptionsState[] = "pauseOnExceptionsState";
 };
 
-const char* InspectorDebuggerAgent::backtraceObjectGroup = "backtrace-object-group";
+const char* InspectorDebuggerAgent::backtraceObjectGroup = "backtrace";
 
 InspectorDebuggerAgent::InspectorDebuggerAgent(InstrumentingAgents* instrumentingAgents, InspectorCompositeState* inspectorState, InjectedScriptManager* injectedScriptManager)
     : InspectorBaseAgent<InspectorDebuggerAgent>("Debugger", instrumentingAgents, inspectorState)
@@ -455,6 +455,7 @@ void InspectorDebuggerAgent::stepOver(ErrorString* errorString)
 {
     if (!assertPaused(errorString))
         return;
+    m_injectedScriptManager->releaseObjectGroup(InspectorDebuggerAgent::backtraceObjectGroup);
     scriptDebugServer().stepOverStatement();
 }
 
@@ -462,6 +463,7 @@ void InspectorDebuggerAgent::stepInto(ErrorString* errorString)
 {
     if (!assertPaused(errorString))
         return;
+    m_injectedScriptManager->releaseObjectGroup(InspectorDebuggerAgent::backtraceObjectGroup);
     scriptDebugServer().stepIntoStatement();
     m_listener->stepInto();
 }
@@ -470,6 +472,7 @@ void InspectorDebuggerAgent::stepOut(ErrorString* errorString)
 {
     if (!assertPaused(errorString))
         return;
+    m_injectedScriptManager->releaseObjectGroup(InspectorDebuggerAgent::backtraceObjectGroup);
     scriptDebugServer().stepOutOfFunction();
 }
 
