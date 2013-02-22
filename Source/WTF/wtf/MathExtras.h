8186c931e21f38cb444bb51bc2b199f84455d40f
Brady Eidson <beidson@apple.com>
2013-02-22 05:56:17 +0000

    Move fastlog2() to WTF/MathExtras.h so it can be used from multiple projects.
    Rubberstamped by Geoff Garen.
    
    Source/WebCore:
    
    * loader/cache/MemoryCache.cpp:
    (WebCore::MemoryCache::lruListFor): Remove the inline fastlog2 and use WTF::fastlog2.
    
    Source/WTF:
    
    * wtf/MathExtras.h:
    (WTF::fastLog2):
    
    Canonical link: https://commits.webkit.org/128829@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@143687 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/MathExtras.h
+++ Source/WTF/wtf/MathExtras.h
@@ -431,6 +431,24 @@ inline uint32_t roundUpToPowerOfTwo(uint32_t v)
     return v;
 }
 
+inline unsigned fastLog2(unsigned i)
+{
+    unsigned log2 = 0;
+    if (i & (i - 1))
+        log2 += 1;
+    if (i >> 16)
+        log2 += 16, i >>= 16;
+    if (i >> 8)
+        log2 += 8, i >>= 8;
+    if (i >> 4)
+        log2 += 4, i >>= 4;
+    if (i >> 2)
+        log2 += 2, i >>= 2;
+    if (i >> 1)
+        log2 += 1;
+    return log2;
+}
+
 } // namespace WTF
 
 #endif // #ifndef WTF_MathExtras_h
--- Source/WebCore/loader/cache/MemoryCache.cpp
+++ Source/WebCore/loader/cache/MemoryCache.cpp
@@ -45,6 +45,7 @@
 #include "WorkerThread.h"
 #include <stdio.h>
 #include <wtf/CurrentTime.h>
+#include <wtf/MathExtras.h>
 #include <wtf/MemoryInstrumentationHashMap.h>
 #include <wtf/MemoryInstrumentationVector.h>
 #include <wtf/MemoryObjectInfo.h>
@@ -412,28 +413,10 @@ void MemoryCache::evict(CachedResource* resource)
     resource->deleteIfPossible();
 }
 
-static inline unsigned fastLog2(unsigned i)
-{
-    unsigned log2 = 0;
-    if (i & (i - 1))
-        log2 += 1;
-    if (i >> 16)
-        log2 += 16, i >>= 16;
-    if (i >> 8)
-        log2 += 8, i >>= 8;
-    if (i >> 4)
-        log2 += 4, i >>= 4;
-    if (i >> 2)
-        log2 += 2, i >>= 2;
-    if (i >> 1)
-        log2 += 1;
-    return log2;
-}
-
 MemoryCache::LRUList* MemoryCache::lruListFor(CachedResource* resource)
 {
     unsigned accessCount = max(resource->accessCount(), 1U);
-    unsigned queueIndex = fastLog2(resource->size() / accessCount);
+    unsigned queueIndex = WTF::fastLog2(resource->size() / accessCount);
 #ifndef NDEBUG
     resource->m_lruIndex = queueIndex;
 #endif
