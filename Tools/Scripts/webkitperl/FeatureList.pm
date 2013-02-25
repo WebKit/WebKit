9a838bac0358631cd264ff9b0a2336fc3ab61075
Laszlo Gombos <l.gombos@samsung.com>
2013-02-25 12:36:00 +0000

    [EFL] Align feature defaults https://bugs.webkit.org/show_bug.cgi?id=110715
    Enable 3D_RENDERING in FeatureList.pm as it is enabled by default in
    OptionsEfl.cmake.
    
    Patch by Laszlo Gombos <l.gombos@samsung.com> on 2013-02-25
    Reviewed by Gyuyoung Kim.
    
    * Scripts/webkitperl/FeatureList.pm:
    
    Canonical link: https://commits.webkit.org/129020@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@143905 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitperl/FeatureList.pm
+++ Tools/Scripts/webkitperl/FeatureList.pm
@@ -153,7 +153,7 @@ my (
 
 my @features = (
     { option => "3d-rendering", desc => "Toggle 3D Rendering support",
-      define => "ENABLE_3D_RENDERING", default => (isAppleMacWebKit() || isQt() || isGtk()), value => \$threeDRenderingSupport },
+      define => "ENABLE_3D_RENDERING", default => (isAppleMacWebKit() || isQt() || isGtk() || isEfl()), value => \$threeDRenderingSupport },
 
     { option => "accelerated-2d-canvas", desc => "Toggle Accelerated 2D Canvas support",
       define => "ENABLE_ACCELERATED_2D_CANVAS", default => 0, value => \$accelerated2DCanvasSupport },
