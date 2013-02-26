13e00f6c746c10e21c8deec6ba4864d52d8f108d
Vivek Galatage <vivek.vg@samsung.com>
2013-02-26 13:43:03 +0000

    Web Inspector: DataGrid should reveal and select next/previous DataGridNode upon deletion of selected node https://bugs.webkit.org/show_bug.cgi?id=110862
    Reviewed by Alexander Pavlov.
    
    While deleting entries in the DataGrid, deleting of selected node should trigger selection of
    next possible (backward/forward) DataGridNode.
    
    * inspector/front-end/DataGrid.js:
    (WebInspector.DataGrid.prototype._keyDown):
    (WebInspector.DataGridNode.prototype.get leftPadding):
    (WebInspector.DataGridNode.prototype.isEventWithinDisclosureTriangle):
    
    
    Canonical link: https://commits.webkit.org/129150@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@144047 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/DataGrid.js
+++ Source/WebCore/inspector/front-end/DataGrid.js
@@ -808,6 +808,16 @@ WebInspector.DataGrid.prototype = {
             if (this._deleteCallback) {
                 handled = true;
                 this._deleteCallback(this.selectedNode);
+
+                nextSelectedNode = this.selectedNode.traverseNextNode(true);
+                while (nextSelectedNode && !nextSelectedNode.selectable)
+                    nextSelectedNode = nextSelectedNode.traverseNextNode(true);
+
+                if (!nextSelectedNode || nextSelectedNode.isCreationNode) {
+                    nextSelectedNode = this.selectedNode.traversePreviousNode(true);
+                    while (nextSelectedNode && !nextSelectedNode.selectable)
+                        nextSelectedNode = nextSelectedNode.traversePreviousNode(true);
+                }
             }
         } else if (isEnterKey(event)) {
             if (this._editCallback) {
@@ -1192,7 +1202,7 @@ WebInspector.DataGridNode.prototype = {
     {
         if (typeof this._leftPadding === "number")
             return this._leftPadding;
-        
+
         this._leftPadding = this.depth * this.dataGrid.indentWidth;
         return this._leftPadding;
     },
@@ -1636,7 +1646,7 @@ WebInspector.DataGridNode.prototype = {
         var cell = event.target.enclosingNodeOrSelfWithNodeName("td");
         if (!cell.hasStyleClass("disclosure"))
             return false;
-        
+
         var left = cell.totalOffsetLeft() + this.leftPadding;
         return event.pageX >= left && event.pageX <= left + this.disclosureToggleWidth;
     },
