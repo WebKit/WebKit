d71fcb037747bded33f6dad16654fb5e15d0dc03
Kentaro Hara <haraken@chromium.org>
2013-02-28 18:00:51 +0000

    Unreviewed, rolling out r144157. http://trac.webkit.org/changeset/144157 https://bugs.webkit.org/show_bug.cgi?id=110794
    It broke chromium windows build
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateNamedConstructor):
    * bindings/scripts/test/V8/V8TestNamedConstructor.cpp:
    (WebCore::V8TestNamedConstructorConstructorCallback):
    (WebCore::V8TestNamedConstructorConstructor::GetTemplate):
    * bindings/v8/custom/V8HTMLImageElementConstructor.cpp:
    (WebCore::v8HTMLImageElementConstructorMethodCustom):
    (WebCore::V8HTMLImageElementConstructor::GetTemplate):
    
    Canonical link: https://commits.webkit.org/129401@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@144322 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -2157,8 +2157,11 @@ sub GenerateNamedConstructor
         }
     }
 
+    my $maybeObserveFeature = GenerateFeatureObservation($function->signature->extendedAttributes->{"V8MeasureAs"});
+
     my @beforeArgumentList;
     my @afterArgumentList;
+
     my $toActiveDOMObject = "0";
     if ($codeGenerator->InheritsExtendedAttribute($interface, "ActiveDOMObject")) {
         $toActiveDOMObject = "${v8InterfaceName}::toActiveDOMObject";
@@ -2173,10 +2176,10 @@ sub GenerateNamedConstructor
     push(@implContent, <<END);
 WrapperTypeInfo ${v8InterfaceName}Constructor::info = { ${v8InterfaceName}Constructor::GetTemplate, ${v8InterfaceName}::derefObject, $toActiveDOMObject, $toEventTarget, 0, ${v8InterfaceName}::installPerContextPrototypeProperties, 0, WrapperTypeObjectPrototype };
 
-static v8::Handle<v8::Value> namedConstructor(const v8::Arguments& args)
+static v8::Handle<v8::Value> ${v8InterfaceName}ConstructorCallback(const v8::Arguments& args)
 {
+    ${maybeObserveFeature}
 END
-    push(@implContent, GenerateFeatureObservation($function->signature->extendedAttributes->{"V8MeasureAs"}));
     push(@implContent, GenerateConstructorHeader());
     push(@implContent, <<END);
     Document* document = currentDocument(BindingState::instance());
@@ -2231,17 +2234,15 @@ END
     V8DOMWrapper::associateObjectWithWrapper(impl.release(), &${v8InterfaceName}Constructor::info, wrapper, args.GetIsolate(), WrapperConfiguration::Dependent);
     return wrapper;
 END
+
     if ($raisesExceptions) {
         push(@implContent, "    fail:\n");
         push(@implContent, "    return setDOMException(ec, args.GetIsolate());\n");
     }
-    push(@implContent, <<END);
-}
 
-static v8::Handle<v8::Value> namedConstructorCallback(const v8::Arguments& args)
-{
-    return namedConstructor(args);
-}
+    push(@implContent, "}\n");
+
+    push(@implContent, <<END);
 
 v8::Persistent<v8::FunctionTemplate> ${v8InterfaceName}Constructor::GetTemplate(v8::Isolate* isolate)
 {
@@ -2250,7 +2251,7 @@ v8::Persistent<v8::FunctionTemplate> ${v8InterfaceName}Constructor::GetTemplate(
         return cachedTemplate;
 
     v8::HandleScope scope;
-    v8::Local<v8::FunctionTemplate> result = v8::FunctionTemplate::New(namedConstructorCallback);
+    v8::Local<v8::FunctionTemplate> result = v8::FunctionTemplate::New(${v8InterfaceName}ConstructorCallback);
 
     v8::Local<v8::ObjectTemplate> instance = result->InstanceTemplate();
     instance->SetInternalFieldCount(${v8InterfaceName}::internalFieldCount);
