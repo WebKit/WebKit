f53b829933168fac7833c80044f49b30e23d0ad5
Ryosuke Niwa <rniwa@webkit.org>
2013-02-28 22:02:09 +0000

    Revert an erroneous change in r144336.
    Rubber-stamped by Eric Carlson.
    
    * page/PageGroup.cpp:
    (WebCore::PageGroup::captionPreferences):
    
    
    
    Canonical link: https://commits.webkit.org/129439@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@144367 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/PageGroup.cpp
+++ Source/WebCore/page/PageGroup.cpp
@@ -39,7 +39,7 @@
 #include "StorageNamespace.h"
 
 #if ENABLE(VIDEO_TRACK)
-#if HAVE(MEDIA_ACCESSIBILITY_FRAMEWORK)
+#if PLATFORM(MAC) && !PLATFORM(IOS)
 #include "CaptionUserPreferencesMac.h"
 #else
 #include "CaptionUserPreferences.h"
@@ -420,7 +420,7 @@ void PageGroup::invalidatedInjectedStyleSheetCacheInAllFrames()
 CaptionUserPreferences* PageGroup::captionPreferences()
 {
     if (!m_captionPreferences)
-#if HAVE(MEDIA_ACCESSIBILITY_FRAMEWORK)
+#if PLATFORM(MAC) && !PLATFORM(IOS)
         m_captionPreferences = CaptionUserPreferencesMac::create(this);
 #else
         m_captionPreferences = CaptionUserPreferences::create(this);
