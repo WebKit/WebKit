076968b92f4aba2a68871df485d375125da7eb68
Laszlo Gombos <l.gombos@samsung.com>
2013-03-04 21:11:33 +0000

    JSC_OBJC_API_ENABLED should be removed from FeatureDefines.h https://bugs.webkit.org/show_bug.cgi?id=111269
    Reviewed by Mark Hahnenberg.
    
    Source/WebKit/mac:
    
    Include JavaScriptCore/JSBase.h for the definition of JSC_OBJC_API_ENABLED.
    
    * WebView/WebDelegateImplementationCaching.h:
    
    Source/WTF:
    
    JSC_OBJC_API_ENABLED flag was added to FeatureDefines.h (r143637)
    so that it could be used in WebCore.exp.in to determine whether
    or not we should be exporting certain symbols based on whether
    the WebKit-related portion of the JSC ObjC API was enabled.
    
    It turned out conditionally including certain symbols didn't
    work when building fat binaries, so it's not even necessary any more.
    
    * wtf/FeatureDefines.h: Remove JSC_OBJC_API_ENABLED.
    
    
    Canonical link: https://commits.webkit.org/129702@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@144662 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/mac/WebView/WebDelegateImplementationCaching.h
+++ Source/WebKit/mac/WebView/WebDelegateImplementationCaching.h
@@ -29,6 +29,7 @@
 // This header contains WebView declarations that can be used anywhere in WebKit, but are neither SPI nor API.
 
 #import "WebTypesInternal.h"
+#import <JavaScriptCore/JSBase.h>
 
 @class WebView;
 
