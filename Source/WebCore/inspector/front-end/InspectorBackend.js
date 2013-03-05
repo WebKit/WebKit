55a8d0e02efc7875f2ff01dee13051334bfc999c
Eugene Klyuchnikov <eustas@chromium.org>
2013-03-05 06:12:18 +0000

    Web Inspector: Exceptions when WebInspector run in hosted mode. https://bugs.webkit.org/show_bug.cgi?id=111326
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    There is bug in commands code generation in InspectorBackend.js
    As a consequence all protocol enums have wrong names and cause
    exception when refererenced.
    
    Currently protocol enums are referenced only from NetworkManager.
    
    Test: inspector/inspector-backend-commands-generation.html
    
    * inspector/front-end/InspectorBackend.js:
    (InspectorBackendClass.prototype.loadFromJSONIfNeeded):
    Use static method to generate code.
    (InspectorBackendClass._generateCommands):
    Extracted from loadFromJSONIfNeeded. Typo fixed.
    
    LayoutTests:
    
    Test runtime commands code generator output.
    
    * inspector/inspector-backend-commands-generation-expected.txt: Added.
    * inspector/inspector-backend-commands-generation.html: Added.
    
    Canonical link: https://commits.webkit.org/129754@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@144722 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/front-end/InspectorBackend.js
+++ Source/WebCore/inspector/front-end/InspectorBackend.js
@@ -56,7 +56,7 @@ InspectorBackendClass.prototype = {
         callback.methodName = method;
         if (this.dumpInspectorTimeStats)
             callback.sendRequestTime = Date.now();
-        
+
         return callbackId;
     },
 
@@ -280,101 +280,110 @@ InspectorBackendClass.prototype = {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", jsonUrl, false);
         xhr.send(null);
-    
+
         var schema = JSON.parse(xhr.responseText);
-        var jsTypes = { integer: "number", array: "object" };
-        var rawTypes = {};
-    
-        var domains = schema["domains"];
-        for (var i = 0; i < domains.length; ++i) {
-            var domain = domains[i];
-            for (var j = 0; domain.types && j < domain.types.length; ++j) {
-                var type = domain.types[j];
-                rawTypes[domain.domain + "." + type.id] = jsTypes[type.type] || type.type;
-            }
-        }
-
-        function toUpperCase(groupIndex, group0, group1)
-        {
-            return [group0, group1][groupIndex].toUpperCase();
-        }
-        function generateEnum(enumName, items)
-        {
-            var members = []
-            for (var m = 0; m < items.length; ++m) {
-                var value = items[m];
-                var name = value.replace(/-(\w)/g, toUpperCase.bind(null, 1)).toTitleCase();
-                name = name.replace(/HTML|XML|WML|API/ig, toUpperCase.bind(null, 0));
-                members.push(name + ": \"" + value +"\"");
-            }
-            return "InspectorBackend.registerEnum(\"" + enumName + "s\", {" + members.join(", ") + "});\n";
-        }
-    
-        var result = [];
-        for (var i = 0; i < domains.length; ++i) {
-            var domain = domains[i];
-
-            var types = domain["types"] || [];
-            for (var j = 0; j < types.length; ++j) {
-                var type = types[j];
-                if ((type["type"] === "string") && type["enum"])
-                    result.push(generateEnum(domain.domain + "." + type.id, type["enum"]));
-                else if (type["type"] === "object") {
-                    var properties = type["properties"] || [];
-                    for (var k = 0; k < properties.length; ++k) {
-                        var property = properties[k];
-                        if ((property["type"] === "string") && property["enum"])
-                            result.push(generateEnum(domain.domain + "." + type.id + property["name"].toTitleCase(), property["enum"]));
-                    }
-                }
-            }
-
-            var commands = domain["commands"] || [];    
-            for (var j = 0; j < commands.length; ++j) {
-                var command = commands[j];
-                var parameters = command["parameters"];
-                var paramsText = [];
-                for (var k = 0; parameters && k < parameters.length; ++k) {
-                    var parameter = parameters[k];
-    
-                    var type;
-                    if (parameter.type)
-                        type = jsTypes[parameter.type] || parameter.type;
-                    else {
-                        var ref = parameter["$ref"];
-                        if (ref.indexOf(".") !== -1)
-                            type = rawTypes[ref];
-                        else
-                            type = rawTypes[domain.domain + "." + ref];
-                    }
-    
-                    var text = "{\"name\": \"" + parameter.name + "\", \"type\": \"" + type + "\", \"optional\": " + (parameter.optional ? "true" : "false") + "}";
-                    paramsText.push(text);
-                }
-    
-                var returnsText = [];
-                var returns = command["returns"] || [];
-                for (var k = 0; k < returns.length; ++k) {
-                    var parameter = returns[k];
-                    returnsText.push("\"" + parameter.name + "\"");
-                }
-                result.push("InspectorBackend.registerCommand(\"" + domain.domain + "." + command.name + "\", [" + paramsText.join(", ") + "], [" + returnsText.join(", ") + "]);");
-            }
-    
-            for (var j = 0; domain.events && j < domain.events.length; ++j) {
-                var event = domain.events[j];
-                var paramsText = [];
-                for (var k = 0; event.parameters && k < event.parameters.length; ++k) {
-                    var parameter = event.parameters[k];
-                    paramsText.push("\"" + parameter.name + "\"");
-                }
-                result.push("InspectorBackend.registerEvent(\"" + domain.domain + "." + event.name + "\", [" + paramsText.join(", ") + "]);");
-            }
-    
-            result.push("InspectorBackend.register" + domain.domain + "Dispatcher = InspectorBackend.registerDomainDispatcher.bind(InspectorBackend, \"" + domain.domain + "\");");
-        }
-        eval(result.join("\n"));
+        var code = InspectorBackendClass._generateCommands(schema);
+        eval(code);
     }
+}
+
+/**
+ * @param {*} schema
+ * @return {string}
+ */
+InspectorBackendClass._generateCommands = function(schema) {
+    var jsTypes = { integer: "number", array: "object" };
+    var rawTypes = {};
+    var result = [];
+
+    var domains = schema["domains"] || [];
+    for (var i = 0; i < domains.length; ++i) {
+        var domain = domains[i];
+        for (var j = 0; domain.types && j < domain.types.length; ++j) {
+            var type = domain.types[j];
+            rawTypes[domain.domain + "." + type.id] = jsTypes[type.type] || type.type;
+        }
+    }
+
+    function toUpperCase(groupIndex, group0, group1)
+    {
+        return [group0, group1][groupIndex].toUpperCase();
+    }
+    function generateEnum(enumName, items)
+    {
+        var members = []
+        for (var m = 0; m < items.length; ++m) {
+            var value = items[m];
+            var name = value.replace(/-(\w)/g, toUpperCase.bind(null, 1)).toTitleCase();
+            name = name.replace(/HTML|XML|WML|API/ig, toUpperCase.bind(null, 0));
+            members.push(name + ": \"" + value +"\"");
+        }
+        return "InspectorBackend.registerEnum(\"" + enumName + "\", {" + members.join(", ") + "});";
+    }
+
+    for (var i = 0; i < domains.length; ++i) {
+        var domain = domains[i];
+
+        var types = domain["types"] || [];
+        for (var j = 0; j < types.length; ++j) {
+            var type = types[j];
+            if ((type["type"] === "string") && type["enum"])
+                result.push(generateEnum(domain.domain + "." + type.id, type["enum"]));
+            else if (type["type"] === "object") {
+                var properties = type["properties"] || [];
+                for (var k = 0; k < properties.length; ++k) {
+                    var property = properties[k];
+                    if ((property["type"] === "string") && property["enum"])
+                        result.push(generateEnum(domain.domain + "." + type.id + property["name"].toTitleCase(), property["enum"]));
+                }
+            }
+        }
+
+        var commands = domain["commands"] || [];
+        for (var j = 0; j < commands.length; ++j) {
+            var command = commands[j];
+            var parameters = command["parameters"];
+            var paramsText = [];
+            for (var k = 0; parameters && k < parameters.length; ++k) {
+                var parameter = parameters[k];
+
+                var type;
+                if (parameter.type)
+                    type = jsTypes[parameter.type] || parameter.type;
+                else {
+                    var ref = parameter["$ref"];
+                    if (ref.indexOf(".") !== -1)
+                        type = rawTypes[ref];
+                    else
+                        type = rawTypes[domain.domain + "." + ref];
+                }
+
+                var text = "{\"name\": \"" + parameter.name + "\", \"type\": \"" + type + "\", \"optional\": " + (parameter.optional ? "true" : "false") + "}";
+                paramsText.push(text);
+            }
+
+            var returnsText = [];
+            var returns = command["returns"] || [];
+            for (var k = 0; k < returns.length; ++k) {
+                var parameter = returns[k];
+                returnsText.push("\"" + parameter.name + "\"");
+            }
+            result.push("InspectorBackend.registerCommand(\"" + domain.domain + "." + command.name + "\", [" + paramsText.join(", ") + "], [" + returnsText.join(", ") + "]);");
+        }
+
+        for (var j = 0; domain.events && j < domain.events.length; ++j) {
+            var event = domain.events[j];
+            var paramsText = [];
+            for (var k = 0; event.parameters && k < event.parameters.length; ++k) {
+                var parameter = event.parameters[k];
+                paramsText.push("\"" + parameter.name + "\"");
+            }
+            result.push("InspectorBackend.registerEvent(\"" + domain.domain + "." + event.name + "\", [" + paramsText.join(", ") + "]);");
+        }
+
+        result.push("InspectorBackend.register" + domain.domain + "Dispatcher = InspectorBackend.registerDomainDispatcher.bind(InspectorBackend, \"" + domain.domain + "\");");
+    }
+    return result.join("\n");
 }
 
 InspectorBackend = new InspectorBackendClass();
