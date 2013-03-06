fcea13f082bb90dc52b6e19e6e03ddec02495d43
Aaron Colwell <acolwell@chromium.org>
2013-03-06 02:11:03 +0000

    Heap-use-after-free in WebCore::HTMLMediaElement::~HTMLMediaElement https://bugs.webkit.org/show_bug.cgi?id=110623
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    Test: http/tests/misc/delete-frame-during-readystatechange-with-gc-after-video-removal.html
    
    * html/HTMLAudioElement.h:
    (HTMLAudioElement):
    * html/HTMLAudioElement.idl:
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::hasPendingActivity):
    * html/HTMLMediaElement.idl:
    
    LayoutTests:
    
    * http/tests/misc/delete-frame-during-readystatechange-with-gc-after-video-removal-expected.txt: Added.
    * http/tests/misc/delete-frame-during-readystatechange-with-gc-after-video-removal.html: Added.
    * http/tests/misc/resources/delete-frame-during-readystatechange-frame-with-gc-after-video-removal.html: Added.
    
    
    Canonical link: https://commits.webkit.org/129880@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@144859 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLAudioElement.h
+++ Source/WebCore/html/HTMLAudioElement.h
@@ -39,8 +39,6 @@ public:
     static PassRefPtr<HTMLAudioElement> create(const QualifiedName&, Document*, bool);
     static PassRefPtr<HTMLAudioElement> createForJSConstructor(Document*, const String& src);
 
-    virtual bool hasPendingActivity() const { return isPlaying() || HTMLMediaElement::hasPendingActivity(); }
-
     virtual bool isActiveNode() const { return true; }
 
 private:
