f03ef3736d9cce8d3a74a952c6b72ce90a017c7f
Julien Chaffraix <jchaffraix@webkit.org>
2013-03-06 15:48:25 +0000

    Remove the (dead) code for handling shorthands in StyleResolver / StyleBuilder https://bugs.webkit.org/show_bug.cgi?id=111505
    Unreviewed minimal build fix.
    
    * css/StyleResolver.cpp:
    (WebCore::StyleResolver::applyProperty):
    Put CSSPropertyWebkitWrap under the proper if-guard.
    
    Canonical link: https://commits.webkit.org/129946@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@144930 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/StyleResolver.cpp
+++ Source/WebCore/css/StyleResolver.cpp
@@ -3136,7 +3136,9 @@ void StyleResolver::applyProperty(CSSPropertyID id, CSSValue* value)
     case CSSPropertyWebkitTextStroke:
     case CSSPropertyWebkitTransition:
     case CSSPropertyWebkitTransformOrigin:
+#if ENABLE(CSS_EXCLUSIONS)
     case CSSPropertyWebkitWrap:
+#endif
         ASSERT(isExpandedShorthand(id));
         ASSERT_NOT_REACHED();
         break;
