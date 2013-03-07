4e625a2c2cc286b6262167bee0720e609b5b302a
Arunprasad Rajkumar <arunprasadr@nds.com>
2013-03-07 17:11:57 +0000

    [Qt] QWebSettings::clearMemoryCaches should clear JS garbage https://bugs.webkit.org/show_bug.cgi?id=111094
    Patch by Arunprasad Rajkumar <arunprasadr@nds.com> on 2013-03-07
    Reviewed by Jocelyn Turcotte.
    
    * Api/qwebsettings.cpp:
    (QWebSettings::clearMemoryCaches):
    
    Canonical link: https://commits.webkit.org/130086@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@145085 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/qt/Api/qwebsettings.cpp
+++ Source/WebKit/qt/Api/qwebsettings.cpp
@@ -27,6 +27,7 @@
 #include "DatabaseManager.h"
 #include "FileSystem.h"
 #include "FontCache.h"
+#include "GCController.h"
 #include "IconDatabase.h"
 #include "Image.h"
 #if ENABLE(ICONDATABASE)
@@ -813,7 +814,7 @@ QPixmap QWebSettings::webGraphic(WebGraphic type)
 }
 
 /*!
-    Frees up as much memory as possible by cleaning all memory caches such
+    Frees up as much memory as possible by calling the JavaScript garbage collector and cleaning all memory caches such
     as page, object and font cache.
 
     \since 4.6
@@ -839,6 +840,11 @@ void QWebSettings::clearMemoryCaches()
 
     // Empty the Cross-Origin Preflight cache
     WebCore::CrossOriginPreflightResultCache::shared().empty();
+
+    // Drop JIT compiled code from ExecutableAllocator.
+    WebCore::gcController().discardAllCompiledCode();
+    // Garbage Collect to release the references of CachedResource from dead objects.
+    WebCore::gcController().garbageCollectNow();
 }
 
 /*!
