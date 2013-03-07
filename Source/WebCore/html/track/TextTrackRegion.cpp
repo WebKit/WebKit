df42080e3c8152859e40e9d362c8f3299a82b3a8
Victor Carbune <vcarbune@chromium.org>
2013-03-07 09:29:10 +0000

    TextTrackRegion Constructor https://bugs.webkit.org/show_bug.cgi?id=110511
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    This is an initial patch for implementing the TextTrackRegion
    defined in the "WebVTT Extension: Regions for rendering cue groups":
    https://dvcs.w3.org/hg/text-tracks/raw-file/default/608toVTT/region.html
    
    The feature components are hidden behind the WEBVTT_REGIONS flag
    and is currently disabled by default in all ports and tests skipped.
    
    Test: media/track/regions-webvtt/text-track-region-constructor.html
    
    * WebCore.gypi: Added files to be included for building Chromium with
    feature enabled (it is disabled by default).
    * html/track/TextTrackRegion.cpp: Added.
    (WebCore):
    (WebCore::TextTrackRegion::TextTrackRegion):
    (WebCore::TextTrackRegion::~TextTrackRegion):
    (WebCore::TextTrackRegion::setId): Setter.
    (WebCore::TextTrackRegion::setWidth): Setter.
    (WebCore::TextTrackRegion::setHeight): Setter.
    (WebCore::TextTrackRegion::setRegionAnchorX): Setter.
    (WebCore::TextTrackRegion::setRegionAnchorY): Setter.
    (WebCore::TextTrackRegion::setViewportAnchorX): Setter.
    (WebCore::TextTrackRegion::setViewportAnchorY): Setter.
    (WebCore::TextTrackRegion::scroll): Getter. Internally, the scroll
    value is stored as a boolean, but when returned is converted to either
    the empty string or "up".
    (WebCore::TextTrackRegion::setScroll): Setter.
    (WebCore::TextTrackRegion::setRegionSettings): Not used yet (see b/109818)
    * html/track/TextTrackRegion.h: Added.
    (WebCore):
    (TextTrackRegion):
    (WebCore::TextTrackRegion::create):
    (WebCore::TextTrackRegion::track): Getter.
    (WebCore::TextTrackRegion::id): Getter.
    (WebCore::TextTrackRegion::width): Getter.
    (WebCore::TextTrackRegion::height): Getter.
    (WebCore::TextTrackRegion::regionAnchorX): Getter.
    (WebCore::TextTrackRegion::regionAnchorY): Getter.
    (WebCore::TextTrackRegion::viewportAnchorX): Getter.
    (WebCore::TextTrackRegion::viewportAnchorY): Getter.
    (WebCore::TextTrackRegion::regionSettings): Getter, used only internally (see b/109818).
    * html/track/TextTrackRegion.idl: Added.
    * page/DOMWindow.idl: Constructor support.
    
    LayoutTests:
    
    * TestExpectations:
    * media/track/regions-webvtt/text-track-region-constructor-expected.txt: Added.
    * media/track/regions-webvtt/text-track-region-constructor.html: Added.
    
    
    Canonical link: https://commits.webkit.org/130056@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@145053 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebCore/html/track/TextTrackRegion.cpp
@@ -0,0 +1,185 @@
+/*
+ * Copyright (C) 2013 Google Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source ec must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+
+#if ENABLE(VIDEO_TRACK) && ENABLE(WEBVTT_REGIONS)
+
+#include "TextTrackRegion.h"
+
+namespace WebCore {
+
+// The region occupies by default 100% of the width of the video viewport.
+static const float defaultWidth = 100;
+
+// The region has, by default, 3 lines of text.
+static const long defaultHeightInLines = 3;
+
+// The region and viewport are anchored in the bottom left corner.
+static const float defaultAnchorPointX = 0;
+static const float defaultAnchorPointY = 100;
+
+// The region doesn't have scrolling text, by default.
+static const bool defaultScroll = false;
+
+TextTrackRegion::TextTrackRegion()
+    : m_id(emptyString())
+    , m_width(defaultWidth)
+    , m_heightInLines(defaultHeightInLines)
+    , m_regionAnchor(FloatPoint(defaultAnchorPointX, defaultAnchorPointY))
+    , m_viewportAnchor(FloatPoint(defaultAnchorPointX, defaultAnchorPointY))
+    , m_scroll(defaultScroll)
+{
+}
+
+TextTrackRegion::~TextTrackRegion()
+{
+}
+
+void TextTrackRegion::setId(const String& id)
+{
+    m_id = id;
+}
+
+void TextTrackRegion::setWidth(double value, ExceptionCode& ec)
+{
+    if (std::isinf(value) || std::isnan(value)) {
+        ec = TypeError;
+        return;
+    }
+
+    if (value < 0 || value > 100) {
+        ec = INDEX_SIZE_ERR;
+        return;
+    }
+
+    m_width = value;
+}
+
+void TextTrackRegion::setHeight(long value, ExceptionCode& ec)
+{
+    if (value < 0) {
+        ec = INDEX_SIZE_ERR;
+        return;
+    }
+
+    m_heightInLines = value;
+}
+
+void TextTrackRegion::setRegionAnchorX(double value, ExceptionCode& ec)
+{
+    if (std::isinf(value) || std::isnan(value)) {
+        ec = TypeError;
+        return;
+    }
+
+    if (value < 0 || value > 100) {
+        ec = INDEX_SIZE_ERR;
+        return;
+    }
+
+    m_regionAnchor.setX(value);
+}
+
+void TextTrackRegion::setRegionAnchorY(double value, ExceptionCode& ec)
+{
+    if (std::isinf(value) || std::isnan(value)) {
+        ec = TypeError;
+        return;
+    }
+
+    if (value < 0 || value > 100) {
+        ec = INDEX_SIZE_ERR;
+        return;
+    }
+
+    m_regionAnchor.setY(value);
+}
+
+void TextTrackRegion::setViewportAnchorX(double value, ExceptionCode& ec)
+{
+    if (std::isinf(value) || std::isnan(value)) {
+        ec = TypeError;
+        return;
+    }
+
+    if (value < 0 || value > 100) {
+        ec = INDEX_SIZE_ERR;
+        return;
+    }
+
+    m_viewportAnchor.setX(value);
+}
+
+void TextTrackRegion::setViewportAnchorY(double value, ExceptionCode& ec)
+{
+    if (std::isinf(value) || std::isnan(value)) {
+        ec = TypeError;
+        return;
+    }
+
+    if (value < 0 || value > 100) {
+        ec = INDEX_SIZE_ERR;
+        return;
+    }
+
+    m_viewportAnchor.setY(value);
+}
+
+const AtomicString TextTrackRegion::scroll() const
+{
+    DEFINE_STATIC_LOCAL(const AtomicString, upScrollValueKeyword, ("up", AtomicString::ConstructFromLiteral));
+
+    if (m_scroll)
+        return upScrollValueKeyword;
+
+    return "";
+}
+
+void TextTrackRegion::setScroll(const AtomicString& value, ExceptionCode& ec)
+{
+    DEFINE_STATIC_LOCAL(const AtomicString, upScrollValueKeyword, ("up", AtomicString::ConstructFromLiteral));
+
+    if (value != emptyString() && value != upScrollValueKeyword) {
+        ec = SYNTAX_ERR;
+        return;
+    }
+
+    m_scroll = value == upScrollValueKeyword;
+}
+
+void TextTrackRegion::setRegionSettings(const String& input)
+{
+    // FIXME(109818): Parse region header metadata.
+}
+
+} // namespace WebCore
+
+#endif
