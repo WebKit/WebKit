9db9caeee38251bdaf6d475f1acee99c4252568b
Geoffrey Garen <ggaren@apple.com>
2013-03-08 21:11:53 +0000

    Added a regression test for raw parsing speed https://bugs.webkit.org/show_bug.cgi?id=111808
    Reviewed by Oliver Hunt.
    
    * fast/js/regress/nested-function-parsing-random.html: Added.
    * fast/js/regress/script-tests/nested-function-parsing-random.js: Added.
    (randomIdent): Use randomness to defeat caching.
    
    
    Canonical link: https://commits.webkit.org/130241@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@145261 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/js/regress/script-tests/nested-function-parsing-random.js
@@ -0,0 +1,26 @@
+function randomIdent(prefix)
+{
+    var random = Math.random();
+    var truncated = Math.floor(random * 1000000000);
+    return String(prefix) + String(truncated);
+}
+
+(function() {
+    var result = 0;
+
+    for (var i = 0; i < 100; ++i) {
+        var program = "(function " + randomIdent("f") + "() {\n";
+        for (var j = 0; j < 1000; ++j) {
+            program += "function " + randomIdent("g") + "() { return 0 && 1 && 2 && 3 && 4 && 5 && 6 && 7 && 8 && 9 && 10; }\n";
+        }
+        program += "})();\n";
+        program += "return 0;\n";
+
+        result += new Function(program)();
+    }
+
+    if (result != 0) {
+        print("Bad result: " + result);
+        throw "Error";
+    }
+})();
