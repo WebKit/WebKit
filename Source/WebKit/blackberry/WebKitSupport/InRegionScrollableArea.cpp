520ec316c3a65a4075d0808c6188590bf40d9208
Genevieve Mak <gmak@rim.com>
2013-03-11 20:40:50 +0000

    [BlackBerry] Set the documentViewportRect and viewport size. https://bugs.webkit.org/show_bug.cgi?id=112056
    Patch by Genevieve Mak <gmak@rim.com> on 2013-03-11
    Reviewed by Rob Buis.
    
    Internally reviewed by Mike Fenton and Jakob Petsovits.
    PR #278490
    
    * WebKitSupport/InRegionScrollableArea.cpp:
    (BlackBerry::WebKit::InRegionScrollableArea::InRegionScrollableArea):
    
    Canonical link: https://commits.webkit.org/130348@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@145396 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/blackberry/WebKitSupport/InRegionScrollableArea.cpp
+++ Source/WebKit/blackberry/WebKitSupport/InRegionScrollableArea.cpp
@@ -81,7 +81,8 @@ InRegionScrollableArea::InRegionScrollableArea(WebPagePrivate* webPage, RenderLa
 
         m_scrollPosition = m_webPage->mapToTransformed(view->scrollPosition());
         m_contentsSize = m_webPage->mapToTransformed(view->contentsSize());
-        m_viewportSize = m_webPage->mapToTransformed(view->visibleContentRect()).size();
+        m_viewportSize = m_webPage->mapToTransformed(view->visibleContentRect(false /*includeScrollbars*/)).size();
+        m_documentViewportRect = view->frameRect();
 
         m_scrollsHorizontally = view->contentsWidth() > view->visibleWidth();
         m_scrollsVertically = view->contentsHeight() > view->visibleHeight();
@@ -104,7 +105,8 @@ InRegionScrollableArea::InRegionScrollableArea(WebPagePrivate* webPage, RenderLa
         ScrollableArea* scrollableArea = static_cast<ScrollableArea*>(m_layer);
         m_scrollPosition = m_webPage->mapToTransformed(scrollableArea->scrollPosition());
         m_contentsSize = m_webPage->mapToTransformed(scrollableArea->contentsSize());
-        m_viewportSize = m_webPage->mapToTransformed(scrollableArea->visibleContentRect()).size();
+        m_viewportSize = m_webPage->mapToTransformed(scrollableArea->visibleContentRect(false /*includeScrollbars*/)).size();
+        m_documentViewportRect = enclosingIntRect(box->absoluteClippedOverflowRect());
 
         m_scrollsHorizontally = box->scrollWidth() != box->clientWidth();
         m_scrollsVertically = box->scrollHeight() != box->clientHeight();
