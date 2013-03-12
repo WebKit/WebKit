71bbbf444d0c518a4c17c7dcec94af3487fbae1e
Alberto Garcia <agarcia@igalia.com>
2013-03-12 15:16:03 +0000

    [BlackBerry] FontPlatformData: HarfBuzz renaming https://bugs.webkit.org/show_bug.cgi?id=112142
    Patch by Alberto Garcia <agarcia@igalia.com> on 2013-03-12
    Reviewed by Rob Buis.
    
    When the old HarfBuzz code was removed in r141291,
    HarfBuzzNGFace.h was renamed to HarfBuzzFace.h and m_harfbuzzFace
    was renamed to m_harfBuzzFace.
    
    These changes need to be reflected in the BlackBerry port.
    
    * platform/graphics/blackberry/FontPlatformDataBlackBerry.cpp:
    (WebCore::FontPlatformData::FontPlatformData):
    (WebCore::FontPlatformData::platformDataInit):
    (WebCore::FontPlatformData::platformDataAssign):
    (WebCore::FontPlatformData::harfBuzzFace):
    
    Canonical link: https://commits.webkit.org/130472@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@145546 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/blackberry/FontPlatformDataBlackBerry.cpp
+++ Source/WebCore/platform/graphics/blackberry/FontPlatformDataBlackBerry.cpp
@@ -19,7 +19,7 @@
 #include "config.h"
 #include "FontPlatformData.h"
 
-#include "HarfBuzzNGFace.h"
+#include "HarfBuzzFace.h"
 #include "ITypeUtils.h"
 
 #include <BlackBerryPlatformGraphicsContext.h>
@@ -38,7 +38,7 @@ FontPlatformData::FontPlatformData(FILECHAR* name, float size, bool syntheticBol
     , m_font(0)
     , m_name(fastStrDup(name))
     , m_scaledFont(0)
-    , m_harfbuzzFace()
+    , m_harfBuzzFace()
     , m_isColorBitmapFont(false)
 {
     ASSERT(name);
@@ -109,7 +109,7 @@ bool FontPlatformData::applyState(FS_STATE* font, float scale) const
 
 void FontPlatformData::platformDataInit(const FontPlatformData& source)
 {
-    m_harfbuzzFace = source.m_harfbuzzFace;
+    m_harfBuzzFace = source.m_harfBuzzFace;
     m_scaledFont = 0;
     if (source.m_font && source.m_font != hashTableDeletedFontValue()) {
         m_font = FS_new_client(source.m_font, 0);
@@ -122,7 +122,7 @@ void FontPlatformData::platformDataInit(const FontPlatformData& source)
 
 const FontPlatformData& FontPlatformData::platformDataAssign(const FontPlatformData& other)
 {
-    m_harfbuzzFace = other.m_harfbuzzFace;
+    m_harfBuzzFace = other.m_harfBuzzFace;
     m_scaledFont = 0;
     if (other.m_font && other.m_font != hashTableDeletedFontValue()) {
         m_font = FS_new_client(other.m_font, 0);
@@ -154,14 +154,14 @@ String FontPlatformData::description() const
 }
 #endif
 
-HarfBuzzNGFace* FontPlatformData::harfbuzzFace()
+HarfBuzzFace* FontPlatformData::harfBuzzFace()
 {
-    if (!m_harfbuzzFace) {
+    if (!m_harfBuzzFace) {
         uint64_t uniqueID = reinterpret_cast<uintptr_t>(m_font);
-        m_harfbuzzFace = HarfBuzzNGFace::create(const_cast<FontPlatformData*>(this), uniqueID);
+        m_harfBuzzFace = HarfBuzzFace::create(const_cast<FontPlatformData*>(this), uniqueID);
     }
 
-    return m_harfbuzzFace.get();
+    return m_harfBuzzFace.get();
 }
 
 FS_STATE* FontPlatformData::scaledFont(float scale) const
