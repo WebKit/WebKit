adf257ab4fad4f56ccac28db8a610d285b06be3a
Matt Falkenhagen <falken@chromium.org>
2013-03-12 11:13:02 +0000

    Refactoring: Pull Node::disabled() and Node::isInert() down to Element. https://bugs.webkit.org/show_bug.cgi?id=112085
    Reviewed by Hajime Morrita.
    
    Node is too low a level for these methods.
    
    No new tests, just refactoring.
    
    * dom/Element.cpp:
    (WebCore::Element::disabled):
    (WebCore):
    (WebCore::Element::isInert):
    * dom/Element.h:
    (Element):
    * dom/EventDispatcher.cpp:
    (WebCore::EventDispatcher::dispatchSimulatedClick):
    * dom/GestureEvent.cpp:
    (WebCore::GestureEventDispatchMediator::dispatchEvent):
    * dom/MouseEvent.cpp:
    (WebCore::MouseEventDispatchMediator::dispatchEvent):
    * dom/Node.cpp:
    (WebCore::Node::handleLocalEvents):
    (WebCore::Node::willRespondToMouseMoveEvents):
    (WebCore::Node::willRespondToMouseClickEvents):
    (WebCore::Node::willRespondToTouchEvents):
    * dom/Node.h:
    (Node):
    
    
    Canonical link: https://commits.webkit.org/130446@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@145514 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/EventDispatcher.cpp
+++ Source/WebCore/dom/EventDispatcher.cpp
@@ -79,7 +79,7 @@ void EventDispatcher::dispatchScopedEvent(Node* node, PassRefPtr<EventDispatchMe
 
 void EventDispatcher::dispatchSimulatedClick(Node* node, Event* underlyingEvent, SimulatedClickMouseEventOptions mouseEventOptions, SimulatedClickVisualOptions visualOptions)
 {
-    if (node->disabled())
+    if (node->isElementNode() && toElement(node)->disabled())
         return;
 
     if (!gNodesDispatchingSimulatedClicks)
--- Source/WebCore/dom/Node.cpp
+++ Source/WebCore/dom/Node.cpp
@@ -902,14 +902,6 @@ Node* Node::focusDelegate()
     return this;
 }
 
-#if ENABLE(DIALOG_ELEMENT)
-bool Node::isInert() const
-{
-    Element* dialog = document()->activeModalDialog();
-    return dialog && !containsIncludingShadowDOM(dialog) && !dialog->containsIncludingShadowDOM(this);
-}
-#endif
-
 unsigned Node::nodeIndex() const
 {
     Node *_tempNode = previousSibling();
@@ -2312,7 +2304,7 @@ void Node::handleLocalEvents(Event* event)
     if (!hasEventTargetData())
         return;
 
-    if (disabled() && event->isMouseEvent())
+    if (isElementNode() && toElement(this)->disabled() && event->isMouseEvent())
         return;
 
     fireEventListeners(event);
@@ -2452,15 +2444,6 @@ void Node::dispatchInputEvent()
     dispatchScopedEvent(Event::create(eventNames().inputEvent, true, false));
 }
 
-bool Node::disabled() const
-{
-#if ENABLE(DIALOG_ELEMENT)
-    if (isInert())
-        return true;
-#endif
-    return false;
-}
-
 void Node::defaultEventHandler(Event* event)
 {
     if (event->target() != this)
@@ -2520,14 +2503,14 @@ void Node::defaultEventHandler(Event* event)
 
 bool Node::willRespondToMouseMoveEvents()
 {
-    if (disabled())
+    if (isElementNode() && toElement(this)->disabled())
         return false;
     return hasEventListeners(eventNames().mousemoveEvent) || hasEventListeners(eventNames().mouseoverEvent) || hasEventListeners(eventNames().mouseoutEvent);
 }
 
 bool Node::willRespondToMouseClickEvents()
 {
-    if (disabled())
+    if (isElementNode() && toElement(this)->disabled())
         return false;
     return isContentEditable(UserSelectAllIsAlwaysNonEditable) || hasEventListeners(eventNames().mouseupEvent) || hasEventListeners(eventNames().mousedownEvent) || hasEventListeners(eventNames().clickEvent) || hasEventListeners(eventNames().DOMActivateEvent);
 }
@@ -2535,7 +2518,7 @@ bool Node::willRespondToMouseClickEvents()
 bool Node::willRespondToTouchEvents()
 {
 #if ENABLE(TOUCH_EVENTS)
-    if (disabled())
+    if (isElementNode() && toElement(this)->disabled())
         return false;
     return hasEventListeners(eventNames().touchstartEvent) || hasEventListeners(eventNames().touchmoveEvent) || hasEventListeners(eventNames().touchcancelEvent) || hasEventListeners(eventNames().touchendEvent);
 #else
