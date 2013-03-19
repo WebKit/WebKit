98fceb5ae05c4bbf6d38049c0a4dbf1a1677e25d
Beth Dakin <bdakin@apple.com>
2013-03-19 21:30:59 +0000

    RenderLayer::scrollTo() should call FrameLoaderClient::didChangeScrollOffset() https://bugs.webkit.org/show_bug.cgi?id=110673
    Reviewed by Simon Fraser.
    
    FrameLoaderClient::didChangeScrollOffset() should be called whenever something is
    actually being scrolled.
    
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::scrollTo):
    
    
    
    Canonical link: https://commits.webkit.org/131055@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@146260 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderLayer.cpp
+++ Source/WebCore/rendering/RenderLayer.cpp
@@ -2214,6 +2214,8 @@ void RenderLayer::scrollTo(int x, int y)
         renderer()->node()->document()->eventQueue()->enqueueOrDispatchScrollEvent(renderer()->node(), DocumentEventQueue::ScrollEventElementTarget);
 
     InspectorInstrumentation::didScrollLayer(frame);
+    if (scrollsOverflow())
+        frame->loader()->client()->didChangeScrollOffset(); 
 }
 
 static inline bool frameElementAndViewPermitScroll(HTMLFrameElement* frameElement, FrameView* frameView) 
