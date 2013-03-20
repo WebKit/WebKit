f270c4bdee9a257ea4957f9cf266f8c5fa12453d
Dominic Mazzoni <dmazzoni@google.com>
2013-03-20 00:27:16 +0000

    Security: Heap-use-after-free in WebCore::AXObjectCache::getOrCreate https://bugs.webkit.org/show_bug.cgi?id=112044
    Reviewed by Chris Fleizach.
    
    Source/WebCore:
    
    Always call recalcSectionsIfNeeded before accessing
    table sections.
    
    Test: accessibility/table-remove-cell-crash.html
    
    * accessibility/AccessibilityTable.cpp:
    (WebCore::AccessibilityTable::addChildren):
    
    LayoutTests:
    
    Adds test showing a crash / assertion failure if a cell is
    deleted from a table and the table's AX object is accessed.
    
    * accessibility/table-remove-cell-crash-expected.txt: Added.
    * accessibility/table-remove-cell-crash.html: Added.
    
    
    Canonical link: https://commits.webkit.org/131077@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@146282 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/accessibility/AccessibilityTable.cpp
+++ Source/WebCore/accessibility/AccessibilityTable.cpp
@@ -345,6 +345,7 @@ void AccessibilityTable::addChildren()
     AXObjectCache* axCache = m_renderer->document()->axObjectCache();
 
     // Go through all the available sections to pull out the rows and add them as children.
+    table->recalcSectionsIfNeeded();
     RenderTableSection* tableSection = table->topSection();
     if (!tableSection)
         return;
