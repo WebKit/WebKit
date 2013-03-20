3fe8510d24586019305383099f9d62f927f975ea
Žan Doberšek <zdobersek@igalia.com>
2013-03-20 20:07:17 +0000

    [GTK] Clean up TemporaryLinkStubs.cpp https://bugs.webkit.org/show_bug.cgi?id=112820
    Reviewed by Philippe Normand.
    
    Clean up the TemporaryLinkStubs.cpp file, removing unnecessary includes and prettifying the code.
    
    No new tests - no new functionality.
    
    * platform/gtk/TemporaryLinkStubs.cpp:
    (WebCore):
    (WebCore::getSupportedKeySizes):
    (WebCore::signedPublicKeyAndChallengeString):
    (WebCore::userIdleTime):
    
    
    Canonical link: https://commits.webkit.org/131166@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@146379 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/gtk/TemporaryLinkStubs.cpp
+++ Source/WebCore/platform/gtk/TemporaryLinkStubs.cpp
@@ -27,25 +27,32 @@
 
 #include "config.h"
 
-#include "AXObjectCache.h"
-#include "CookieStorage.h"
-#include "DNS.h"
-#include "Editor.h"
-#include "FrameView.h"
-#include "FTPDirectoryDocument.h"
+#include "KURL.h"
 #include "NotImplemented.h"
-#include "PluginView.h"
 #include <float.h>
-
-using namespace WebCore;
+#include <wtf/text/WTFString.h>
 
 /********************************************************/
 /* Completely empty stubs (mostly to allow DRT to run): */
 /********************************************************/
 
 namespace WebCore {
-void getSupportedKeySizes(Vector<String>&) { notImplemented(); }
-String signedPublicKeyAndChallengeString(unsigned keySizeIndex, const String &challengeString, const KURL &url) { return String(); }
-float userIdleTime() { notImplemented(); return FLT_MAX; } // return an arbitrarily high userIdleTime so that releasing pages from the page cache isn't postponed
+
+void getSupportedKeySizes(Vector<String>&)
+{
+    notImplemented();
+}
+
+String signedPublicKeyAndChallengeString(unsigned, const String&, const KURL&)
+{
+    return String();
+}
+
+float userIdleTime()
+{
+    // Return an arbitrarily high userIdleTime so that releasing pages from the page cache isn't postponed.
+    notImplemented();
+    return FLT_MAX;
+}
 
 }
