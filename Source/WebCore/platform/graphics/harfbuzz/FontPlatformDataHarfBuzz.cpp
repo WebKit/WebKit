d363d5b936136dc4b3b11ef1b2838e6d3c45916b
Mike Reed <reed@google.com>
2013-03-21 12:49:44 +0000

    Remove obsolete conditionals around calling SkTypeface::getUnitsPerEm() https://bugs.webkit.org/show_bug.cgi?id=112846
    Reviewed by Stephen White.
    
    Existing complex-text layouttests exercise this code.
    
    * platform/graphics/harfbuzz/FontPlatformDataHarfBuzz.cpp:
    (WebCore::FontPlatformData::emSizeInFontUnits):
    
    
    Canonical link: https://commits.webkit.org/131239@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@146456 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/harfbuzz/FontPlatformDataHarfBuzz.cpp
+++ Source/WebCore/platform/graphics/harfbuzz/FontPlatformDataHarfBuzz.cpp
@@ -165,17 +165,7 @@ int FontPlatformData::emSizeInFontUnits() const
     if (m_emSizeInFontUnits)
         return m_emSizeInFontUnits;
 
-    // FIXME: Switch to the SkTypeface::GetUnitsPerEm API once this becomes available.
-    // https://bugs.webkit.org/show_bug.cgi?id=75961
-#if OS(ANDROID)
-    // Android doesn't currently support Skia's getAdvancedTypefaceMetrics(),
-    // but it has access to another method to replace this functionality.
     m_emSizeInFontUnits = m_typeface->getUnitsPerEm();
-#else
-    SkAdvancedTypefaceMetrics* metrics = m_typeface->getAdvancedTypefaceMetrics(SkAdvancedTypefaceMetrics::kNo_PerGlyphInfo);
-    m_emSizeInFontUnits = metrics->fEmSize;
-    metrics->unref();
-#endif
     return m_emSizeInFontUnits;
 }
 
