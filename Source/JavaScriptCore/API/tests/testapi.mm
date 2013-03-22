4dfaa7b3683f9e20d79063e8669fa07463677ed6
David Kilzer <ddkilzer@webkit.org>
2013-03-22 13:53:37 +0000

    Revert "BUILD FIX (r146558): Call [super dealloc] from -[TinyDOMNode dealloc]"
    This fixes a build failure introduced by this change:
    
        Source/JavaScriptCore/API/tests/testapi.mm:206:6: error: ARC forbids explicit message send of 'dealloc'
            [super dealloc];
             ^     ~~~~~~~
        1 error generated.
    
    Not sure why this didn't fail locally on my Mac Pro.
    
    * API/tests/testapi.mm:
    (-[TinyDOMNode dealloc]): Remove call to [super dealloc].
    
    Canonical link: https://commits.webkit.org/131381@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@146603 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/API/tests/testapi.mm
+++ Source/JavaScriptCore/API/tests/testapi.mm
@@ -202,8 +202,6 @@ static JSVirtualMachine *sharedInstance = nil;
     id nextChild;
     while ((nextChild = [enumerator nextObject]))
         [[TinyDOMNode sharedVirtualMachine] removeManagedReference:nextChild withOwner:self];
-
-    [super dealloc];
 }
 
 - (void)appendChild:(TinyDOMNode *)child
