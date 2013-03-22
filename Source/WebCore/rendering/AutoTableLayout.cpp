7aee4527b872816296275ba7bea1cf2f4cbce340
Emil A Eklund <eae@chromium.org>
2013-03-22 13:40:57 +0000

    Floor cell widths in AutoTableLayout::recalcColumn https://bugs.webkit.org/show_bug.cgi?id=112922
    Reviewed by Levi Weintraub.
    
    Source/WebCore:
    
    Table cells widths are floored to ensure even distribution of available
    space across columns. The code path that computes the width of the
    _table_ failed to take this into account and thus can over report the
    total width of the table.
    
    Test: fast/sub-pixel/table-with-subpixel-cell-size.html
    
    * rendering/AutoTableLayout.cpp:
    (WebCore::AutoTableLayout::recalcColumn): Floor width to match column sizing logic.
    
    LayoutTests:
    
    Add test for table with subpixel cells and update existing baselines as needed.
    
    * fast/sub-pixel/table-with-subpixel-cell-size-expected.html: Added.
    * fast/sub-pixel/table-with-subpixel-cell-size.html: Added.
    * platform/chromium-linux/fast/text/international/bidi-layout-across-linebreak-expected.png:
    * platform/chromium-linux/tables/mozilla_expected_failures/bugs/bug89315-expected.png:
    * platform/chromium-win/fast/text/international/bidi-layout-across-linebreak-expected.txt:
    * platform/chromium-win/fast/transforms/bounding-rect-zoom-expected.txt:
    * platform/chromium-win/tables/mozilla_expected_failures/bugs/bug89315-expected.txt:
    
    
    Canonical link: https://commits.webkit.org/131378@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@146600 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/AutoTableLayout.cpp
+++ Source/WebCore/rendering/AutoTableLayout.cpp
@@ -94,7 +94,7 @@ void AutoTableLayout::recalcColumn(unsigned effCol)
                     case Fixed:
                         // ignore width=0
                         if (cellLogicalWidth.isPositive() && !columnLayout.logicalWidth.isPercent()) {
-                            LayoutUnit logicalWidth = cell->adjustBorderBoxLogicalWidthForBoxSizing(cellLogicalWidth.value());
+                            int logicalWidth = cell->adjustBorderBoxLogicalWidthForBoxSizing(cellLogicalWidth.value());
                             if (columnLayout.logicalWidth.isFixed()) {
                                 // Nav/IE weirdness
                                 if ((logicalWidth > columnLayout.logicalWidth.value()) 
