ecc8ef0bf5a2ced26f32d9293de7146bb415fc92
Arpita Bahuguna <a.bah@samsung.com>
2013-03-28 09:13:21 +0000

    Regression: Crash when selecting Hebrew and numbers in a list https://bugs.webkit.org/show_bug.cgi?id=111894
    Patch by Arpita Bahuguna <a.bah@samsung.com> on 2013-03-28
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    There is a crash when trying to click/select an anonymous
    renderer.
    
    This is a regression due to r143313 which tries to add border/padding
    to the computed caret rect in RenderBox::localCaretRect().
    The patch calls on the node() method for the corresponding
    renderer. For an anonymous renderer though, node() returns
    null. Since no check had been added for this, a crash occurs when
    caret rect for an anonymous renderer is being computed.
    
    Test: editing/selection/click-on-anonymous-content-crash.html
    
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::localCaretRect):
    Added a check for validating the return from the node() call.
    
    LayoutTests:
    
    * editing/selection/click-on-anonymous-content-crash-expected.txt: Added.
    * editing/selection/click-on-anonymous-content-crash.html: Added.
    Added a testcase for verifying that no crash occurs when trying to
    select/click an anonymous renderer, in this case the list marker's
    renderer.
    
    Canonical link: https://commits.webkit.org/131814@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@147087 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderBox.cpp
+++ Source/WebCore/rendering/RenderBox.cpp
@@ -3992,7 +3992,7 @@ LayoutRect RenderBox::localCaretRect(InlineBox* box, int caretOffset, LayoutUnit
     // FIXME: Border/padding should be added for all elements but this workaround
     // is needed because we use offsets inside an "atomic" element to represent
     // positions before and after the element in deprecated editing offsets.
-    if (!(editingIgnoresContent(node()) || isTableElement(node()))) {
+    if (node() && !(editingIgnoresContent(node()) || isTableElement(node()))) {
         rect.setX(rect.x() + borderLeft() + paddingLeft());
         rect.setY(rect.y() + paddingTop() + borderTop());
     }
