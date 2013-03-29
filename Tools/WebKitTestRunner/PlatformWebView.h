0ecd43f464f0182d13ddbb89df170d0f6baa7be1
Ryuan Choi <ryuan.choi@samsung.com>
2013-03-29 05:53:14 +0000

    [EFL] Build break with latest EFL libraries after r146265 https://bugs.webkit.org/show_bug.cgi?id=113558
    Unreviewed build fix.
    
    
    * TestWebKitAPI/PlatformWebView.h:
    * WebKitTestRunner/PlatformWebView.h:
    
    
    Canonical link: https://commits.webkit.org/131912@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@147197 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/WebKitTestRunner/PlatformWebView.h
+++ Tools/WebKitTestRunner/PlatformWebView.h
@@ -55,7 +55,11 @@ typedef WKViewRef PlatformWKView;
 typedef GtkWidget* PlatformWindow;
 #elif PLATFORM(EFL)
 typedef struct _Ecore_Evas Ecore_Evas;
+#if USE(EO)
+typedef struct _Eo Evas_Object;
+#else
 typedef struct _Evas_Object Evas_Object;
+#endif
 typedef Evas_Object* PlatformWKView;
 typedef Ecore_Evas* PlatformWindow;
 #endif
