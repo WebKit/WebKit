55fad9950e11efb7f053d0855524dcc9cb434936
Benjamin Poulain <benjamin@webkit.org>
2013-04-01 20:01:41 +0000

    Remove a couple of malloc from ExceptionBase construction https://bugs.webkit.org/show_bug.cgi?id=113681
    Reviewed by Darin Adler.
    
    * dom/ExceptionBase.cpp:
    (WebCore::ExceptionBase::ExceptionBase):
    When the condition is true, we were creating a new String for
    description.name. The constructor had already allocated a string for
    that: m_name. Use that string instead of creating a new one.
    
    When the condition is false, we were creating a String for typeName
    just to use the string operators. This is a waste of time, we can use
    makeString() to invoke the string concatenation functions directly.
    
    
    Canonical link: https://commits.webkit.org/132039@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@147353 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/ExceptionBase.cpp
+++ Source/WebCore/dom/ExceptionBase.cpp
@@ -40,9 +40,9 @@ ExceptionBase::ExceptionBase(const ExceptionCodeDescription& description)
     , m_description(description.description)
 {
     if (description.name)
-        m_message = String(description.name) + ": " + description.typeName + " Exception " + String::number(description.code);
+        m_message = m_name + ": " + description.typeName + " Exception " + String::number(description.code);
     else
-        m_message = String(description.typeName) + " Exception " + String::number(description.code);
+        m_message = makeString(description.typeName, " Exception ", String::number(description.code));
 }
 
 String ExceptionBase::toString() const
