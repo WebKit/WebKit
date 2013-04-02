ba2d3327c24f8fce4f94fb6a45b259024519aca5
Emil A Eklund <eae@chromium.org>
2013-04-02 23:17:18 +0000

    Padding applied twice for empty generated RenderInlines https://bugs.webkit.org/show_bug.cgi?id=113800
    Source/WebCore:
    
    Reviewed by Levi Weintraub.
    
    The padding for empty generated content render objects is
    incorrectly applied twice. Regression from r140693.
    
    Test: fast/css-generated-content/margin-empty-content.html
    
    * rendering/RenderBlockLineLayout.cpp:
    (WebCore::inlineLogicalWidth):
    
    LayoutTests:
    
    Reviewed by Levi Weintraub.
    
    Add test for empty generated content with padding.
    
    * fast/css-generated-content/margin-empty-content-expected.html: Added.
    * fast/css-generated-content/margin-empty-content.html: Added.
    
    
    Canonical link: https://commits.webkit.org/132174@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@147505 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderBlockLineLayout.cpp
+++ Source/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -322,12 +322,14 @@ static LayoutUnit inlineLogicalWidth(RenderObject* child, bool start = true, boo
     RenderObject* parent = child->parent();
     while (parent->isRenderInline() && lineDepth++ < cMaxLineDepth) {
         RenderInline* parentAsRenderInline = toRenderInline(parent);
-        if (start && shouldAddBorderPaddingMargin(child->previousSibling(), start))
-            extraWidth += borderPaddingMarginStart(parentAsRenderInline);
-        if (end && shouldAddBorderPaddingMargin(child->nextSibling(), end))
-            extraWidth += borderPaddingMarginEnd(parentAsRenderInline);
-        if (!start && !end)
-            return extraWidth;
+        if (!isEmptyInline(parentAsRenderInline)) {
+            if (start && shouldAddBorderPaddingMargin(child->previousSibling(), start))
+                extraWidth += borderPaddingMarginStart(parentAsRenderInline);
+            if (end && shouldAddBorderPaddingMargin(child->nextSibling(), end))
+                extraWidth += borderPaddingMarginEnd(parentAsRenderInline);
+            if (!start && !end)
+                return extraWidth;
+        }
         child = parent;
         parent = child->parent();
     }
