9ff4b76a5ccbd0d9e259992419b60f72523741c2
David Vest <davve@opera.com>
2013-04-02 15:18:36 +0000

    Hang when calling setCurrentTime on SVG with cyclic animation dependency chain https://bugs.webkit.org/show_bug.cgi?id=113485
    Patch by David Vest <davve@opera.com> on 2013-04-02
    Reviewed by Stephen Chenney.
    
    Source/WebCore:
    
    If no exact match can be found, approximateBinarySearch may return
    either adjacent value to the key. Selecting the lesser value when
    there is a greater one may cause interval creation to fail in a
    way that causes infinite loops during seeking. Detect and amend
    this condition by selecting the greater value when possible.
    
    Test: svg/animations/animation-dependency-hang.html
    
    * svg/animation/SVGSMILElement.cpp:
    (WebCore::SVGSMILElement::findInstanceTime):
    
    LayoutTests:
    
    * svg/animations/animation-dependency-hang-expected.txt: Added.
    * svg/animations/animation-dependency-hang.html: Added.
    
    Canonical link: https://commits.webkit.org/132116@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@147434 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/svg/animation/SVGSMILElement.cpp
+++ Source/WebCore/svg/animation/SVGSMILElement.cpp
@@ -743,6 +743,10 @@ SMILTime SVGSMILElement::findInstanceTime(BeginOrEnd beginOrEnd, SMILTime minimu
     const SMILTimeWithOrigin* result = approximateBinarySearch<const SMILTimeWithOrigin, SMILTime>(list, sizeOfList, minimumTime, extractTimeFromVector);
     int indexOfResult = result - list.begin();
     ASSERT_WITH_SECURITY_IMPLICATION(indexOfResult < sizeOfList);
+
+    if (list[indexOfResult].time() < minimumTime && indexOfResult < sizeOfList - 1)
+        ++indexOfResult;
+
     const SMILTime& currentTime = list[indexOfResult].time();
 
     // The special value "indefinite" does not yield an instance time in the begin list.
