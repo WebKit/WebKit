7bc8d9d7d4ac8a65731e7b429dce2dc14ad2dbe2
Tim Horton <timothy_horton@apple.com>
2013-04-02 00:26:22 +0000

    Autosize should use documentRect height instead of scrollHeight https://bugs.webkit.org/show_bug.cgi?id=112770
    Reviewed by David Levin.
    
    Autosizing fails to compute the correct height if the root element is very
    small but the document has significant overflow.
    
    Also, unconditionally start laying out from the minimum height, so that the
    documentRect can shrink below its previous height if needed.
    
    No new tests; autosizing is not currently exposed in a testable way on Mac.
    
    * page/FrameView.cpp:
    (WebCore::FrameView::autoSizeIfEnabled):
    
    
    Canonical link: https://commits.webkit.org/132056@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@147373 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/FrameView.cpp
+++ Source/WebCore/page/FrameView.cpp
@@ -2778,14 +2778,8 @@ void FrameView::autoSizeIfEnabled()
     if (!documentView || !documentElement)
         return;
 
-    RenderBox* documentRenderBox = documentElement->renderBox();
-    if (!documentRenderBox)
-        return;
-
-    // If this is the first time we run autosize, start from small height and
-    // allow it to grow.
-    if (!m_didRunAutosize)
-        resize(frameRect().width(), m_minAutoSize.height());
+    // Start from the minimum height and allow it to grow.
+    resize(frameRect().width(), m_minAutoSize.height());
 
     IntSize size = frameRect().size();
 
@@ -2795,7 +2789,7 @@ void FrameView::autoSizeIfEnabled()
         // Update various sizes including contentsSize, scrollHeight, etc.
         document->updateLayoutIgnorePendingStylesheets();
         int width = documentView->minPreferredLogicalWidth();
-        int height = documentRenderBox->scrollHeight();
+        int height = documentView->documentRect().height();
         IntSize newSize(width, height);
 
         // Check to see if a scrollbar is needed for a given dimension and
