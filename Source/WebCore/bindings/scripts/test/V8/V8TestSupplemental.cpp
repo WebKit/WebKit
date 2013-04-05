efaaa1140c5654ff3868191d15d2e877768d26ce
Geoffrey Garen <ggaren@apple.com>
2013-04-05 19:34:04 +0000

    Removed a chunk of the v8 bindings.  Reviewed by Sam 'Anders Carlsson' Weinig.  I'm using a direct http commit to reduce email traffic.
    Canonical link: https://commits.webkit.org/132419@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@147780 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/scripts/test/V8/V8TestSupplemental.cpp
+++
@@ -1,6 +0,0 @@
-/*
-    This file is generated just to tell build scripts that V8TestSupplemental.h and
-    V8TestSupplemental.cpp are created for TestSupplemental.idl, and thus
-    prevent the build scripts from trying to generate V8TestSupplemental.h and
-    V8TestSupplemental.cpp at every build. This file must not be tried to compile.
-*/
