2a27b2fb7947a41912a006cc277767a01a0342c1
Sukolsak Sakshuwong <sukolsak@gmail.com>
2013-04-14 03:55:35 +0000

    Selection direction is not preserved when applying styles https://bugs.webkit.org/show_bug.cgi?id=110979
    Patch by Sukolsak Sakshuwong <sukolsak@gmail.com> on 2013-04-13
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    The patch is based on the one submitted by Shezan Baig.
    
    If the starting selection was directional and its base was not first,
    switch newStart and newEnd when setting the ending direction. Otherwise,
    leave the order unchanged.
    
    Test: editing/style/preserve-selection-direction.html
    
    * editing/ApplyStyleCommand.cpp:
    (WebCore::ApplyStyleCommand::updateStartEnd):
    
    LayoutTests:
    
    Add a test, based on the work by Shezan Baig
    
    * editing/style/preserve-selection-direction-expected.txt: Added.
    * editing/style/preserve-selection-direction.html: Added.
    
    Canonical link: https://commits.webkit.org/132957@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@148378 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/editing/ApplyStyleCommand.cpp
+++ Source/WebCore/editing/ApplyStyleCommand.cpp
@@ -184,7 +184,8 @@ void ApplyStyleCommand::updateStartEnd(const Position& newStart, const Position&
     if (!m_useEndingSelection && (newStart != m_start || newEnd != m_end))
         m_useEndingSelection = true;
 
-    setEndingSelection(VisibleSelection(newStart, newEnd, VP_DEFAULT_AFFINITY, endingSelection().isDirectional()));
+    bool wasBaseFirst = startingSelection().isBaseFirst() || !startingSelection().isDirectional();
+    setEndingSelection(VisibleSelection(wasBaseFirst ? newStart : newEnd, wasBaseFirst ? newEnd : newStart, VP_DEFAULT_AFFINITY, endingSelection().isDirectional()));
     m_start = newStart;
     m_end = newEnd;
 }
