c0139bc6e625b34dac4196d800af4d5b3aa9d915
Roger Fong <roger_fong@apple.com>
2013-04-17 21:30:52 +0000

    Unreviewed. VS2010 Windows build fix.
    * JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorPostBuild.cmd:
    
    
    Canonical link: https://commits.webkit.org/133162@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@148638 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorPostBuild.cmd
+++ Source/JavaScriptCore/JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorPostBuild.cmd
@@ -2,6 +2,8 @@ set GeneratorDirectory=%CONFIGURATIONBUILDDIR%\obj\JavaScriptCoreExportGenerator
 
 mkdir 2>NUL "%CONFIGURATIONBUILDDIR%\bin"
 if exist "%WebKit_Libraries%\bin\pthreadVC2.dll" xcopy /y /d "%WebKit_Libraries%\bin\pthreadVC2.dll" "%CONFIGURATIONBUILDDIR%\bin"
+if exist "%WebKit_Libraries%\bin\libicuuc.dll" xcopy /y /d "%WebKit_Libraries%\bin\libicuuc.dll" "%CONFIGURATIONBUILDDIR%\bin"
+if exist "%WebKit_Libraries%\bin\icudt46.dll" xcopy /y /d "%WebKit_Libraries%\bin\icudt46.dll" "%CONFIGURATIONBUILDDIR%\bin"
 
 echo Exporting link definition output (%GeneratorDirectory%\JavaScriptCoreExports.def)
 if exist "%OUTDIR%\JavaScriptCoreExportGenerator%DEBUGSUFFIX%.exe" "%OUTDIR%\JavaScriptCoreExportGenerator%DEBUGSUFFIX%.exe" > "%GeneratorDirectory%\JavaScriptCoreExports.def"
