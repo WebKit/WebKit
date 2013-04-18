WebKit/WebKit

commit 7f01b45488f405da7679d8ded8fd48127ca06b75

Move side-effects on hover/active state out of hit-testing https://bugs.webkit.org/show_bug.cgi?id=98168
Reviewed by Julien Chaffraix.

Original patch by Allan Sandfeld Jensen; I'm just tweaking things.

Document::updateHoverActiveState is currently called during hit testing
to update the hover and active states of elements effected by mouse
movements (or their keyboard equivalents). This conflates the hit test
algorithm itself with side-effects associated with specific use-cases.

This conflation makes it very difficult to reuse the hover/active logic
for things other than hit testing. 'mouseenter'/'mouseleave' events[1]
are one example of a feature that would be simple to implement on top of
this existing logic if we split it out from the hit testing path, and
instead call it explicitly when necessary. An explicit split between
hit testing and its side-effects will also enable us to simplify the
logic in future patches with well-named parameters, rather than relying
on stuffing properties into HitTestRequest.

This patch drops the call to Document::updateHoverActiveState from
RenderView::hitTest, and adjusts the three call-sites in EventHandler
to explicitly call out to it rather than Document::updateStyleIfNeeded.
The latter call is still necessary but has been folded into
updateHoverActiveState, as the former is never called without calling
the latter.

[1]: http://wkbug.com/18930

* dom/Document.h:
* dom/Document.cpp:
(WebCore::Document::updateHoverActiveState):
    First, this function must now only be called from contexts that were
    performing a read/write hit-test: the code asserts this
    precondition.

    Second, rather than accepting a HitTestResult, the function accepts
    an Element* from which to begin the hover/active chain changes.

    Third, we have to explicitly update the hover/active states for
    documents between the updated element and the top-level document.
    The hit-testing logic was taking care of this for us, now we need to

Source/WebCore/dom/Document.cpp
@@ -2997,7 +2997,7 @@ MouseEventWithHitTestResults Document::prepareMouseEvent(const HitTestRequest& r
     renderView()->hitTest(request, result);
 
     if (!request.readOnly())
-        updateStyleIfNeeded();
+        updateHoverActiveState(request, result.innerElement());
 
     return MouseEventWithHitTestResults(event, result);
 }
@@ -5830,15 +5830,15 @@ static RenderObject* nearestCommonHoverAncestor(RenderObject* obj1, RenderObject
     return 0;
 }
 
-void Document::updateHoverActiveState(const HitTestRequest& request, HitTestResult& result)
+void Document::updateHoverActiveState(const HitTestRequest& request, Element* innerElement)
 {
-    // We don't update :hover/:active state when the result is marked as readOnly.
-    if (request.readOnly())
-        return;
+    ASSERT(!request.readOnly());
 
-    Element* innerElementInDocument = result.innerElement();
-    while (innerElementInDocument && innerElementInDocument->document() != this)
+    Element* innerElementInDocument = innerElement;
+    while (innerElementInDocument && innerElementInDocument->document() != this) {
+        innerElementInDocument->document()->updateHoverActiveState(request, innerElementInDocument);
         innerElementInDocument = innerElementInDocument->document()->ownerElement();
+    }
 
     Element* oldActiveElement = activeElement();
     if (oldActiveElement && !request.active()) {
@@ -5923,6 +5923,8 @@ void Document::updateHoverActiveState(const HitTestRequest& request, HitTestResu
             nodesToAddToChain[i]->setActive(true);
         nodesToAddToChain[i]->setHovered(true);
     }
+
+    updateStyleIfNeeded();
 }
 
 void Document::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const

Source/WebCore/page/EventHandler.cpp
@@ -1039,6 +1039,8 @@ HitTestResult EventHandler::hitTestResultAtPoint(const LayoutPoint& point, HitTe
     // hitTestResultAtPoint is specifically used to hitTest into all frames, thus it always allows child frame content.
     HitTestRequest request(hitType | HitTestRequest::AllowChildFrameContent);
     m_frame->contentRenderer()->hitTest(request, result);
+    if (!request.readOnly())
+        m_frame->document()->updateHoverActiveState(request, result.innerElement());
 
     if (!request.allowsShadowContent())
         result.setToNonShadowAncestor();
@@ -2935,10 +2937,8 @@ bool EventHandler::sendContextMenuEventForKey()
     // Use the focused node as the target for hover and active.
     HitTestResult result(position);
     result.setInnerNode(targetNode);
-    HitTestRequest request(HitTestRequest::Active);
-    doc->updateHoverActiveState(request, result);
-    doc->updateStyleIfNeeded();
-   
+    doc->updateHoverActiveState(HitTestRequest::Active, result.innerElement());
+
     // The contextmenu event is a mouse event even when invoked using the keyboard.
     // This is required for web compatibility.
 
@@ -3074,7 +3074,7 @@ void EventHandler::hoverTimerFired(Timer<EventHandler>*)
             HitTestRequest request(HitTestRequest::Move);
             HitTestResult result(view->windowToContents(m_lastKnownMousePosition));
             renderer->hitTest(request, result);
-            m_frame->document()->updateStyleIfNeeded();
+            m_frame->document()->updateHoverActiveState(request, result.innerElement());
         }
     }
 }

Source/WebCore/dom/Document.h
@@ -689,7 +689,7 @@ public:
     void hoveredNodeDetached(Node*);
     void activeChainNodeDetached(Node*);
 
-    void updateHoverActiveState(const HitTestRequest&, HitTestResult&);
+    void updateHoverActiveState(const HitTestRequest&, Element*);
 
     // Updates for :target (CSS3 selector).
     void setCSSTarget(Element*);

Source/WebCore/rendering/RenderView.cpp
@@ -92,12 +92,7 @@ bool RenderView::hitTest(const HitTestRequest& request, HitTestResult& result)
 
 bool RenderView::hitTest(const HitTestRequest& request, const HitTestLocation& location, HitTestResult& result)
 {
-    bool inside = layer()->hitTest(request, location, result);
-
-    // Next set up the correct :hover/:active state along the new chain.
-    document()->updateHoverActiveState(request, result);
-
-    return inside;
+    return layer()->hitTest(request, location, result);
 }
 
 void RenderView::computeLogicalHeight(LayoutUnit logicalHeight, LayoutUnit, LogicalExtentComputedValues& computedValues) const

commit 7ae02acf1b7e1db39fd836ad4ce067e70cdec156

Unset :hover in inner documents https://bugs.webkit.org/show_bug.cgi?id=114446
Reviewed by Antonio Gomes.

Source/WebCore:

Fixes a regression from r145126 where hover nodes in inner documents was sometimes
not unset. Additionally it uses the new api from r145126 to avoid an unnecessary
hit test in touch-event handling.

Test: fast/events/touch/frame-hover-update.html

* dom/Document.cpp:
(WebCore::Document::updateHoverActiveState):
* page/EventHandler.cpp:
(WebCore::shouldGesturesTriggerActive):
(WebCore::EventHandler::handleTouchEvent):

LayoutTests:

Test expected effect of touch events on hover state.

* fast/events/touch/frame-hover-update-expected.txt: Added.
* fast/events/touch/frame-hover-update.html: Added.

Canonical link: https://commits.webkit.org/133194@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@148672 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/Document.cpp
@@ -5981,6 +5981,11 @@ void Document::updateHoverActiveState(const HitTestRequest& request, Element* in
             if (curr->node() && !curr->isText() && (!mustBeInActiveChain || curr->node()->inActiveChain()))
                 nodesToRemoveFromChain.append(curr->node());
         }
+        // Unset hovered nodes in sub frame documents if the old hovered node was a frame owner.
+        if (oldHoverNode && oldHoverNode->isFrameOwnerElement()) {
+            if (Document* contentDocument = toFrameOwnerElement(oldHoverNode.get())->contentDocument())
+                contentDocument->updateHoverActiveState(request, 0);
+        }
     }
 
     // Now set the hover state for our new object up to the root.

Source/WebCore/page/EventHandler.cpp
@@ -281,7 +281,6 @@ static inline bool scrollNode(float delta, ScrollGranularity granularity, Scroll
     return enclosingBox->scroll(delta < 0 ? negativeDirection : positiveDirection, granularity, absDelta, stopNode);
 }
 
-#if ENABLE(GESTURE_EVENTS)
 static inline bool shouldGesturesTriggerActive()
 {
     // If the platform we're on supports GestureTapDown and GestureTapCancel then we'll
@@ -289,7 +288,6 @@ static inline bool shouldGesturesTriggerActive()
     // know in advance what event types are supported.
     return false;
 }
-#endif
 
 #if !PLATFORM(MAC)
 
@@ -3932,10 +3930,8 @@ bool EventHandler::handleTouchEvent(const PlatformTouchEvent& event)
             break;
         }
 
-#if ENABLE(GESTURE_EVENTS)
         if (shouldGesturesTriggerActive())
             hitType |= HitTestRequest::ReadOnly;
-#endif
 
         // Increment the platform touch id by 1 to avoid storing a key of 0 in the hashmap.
         unsigned touchPointTargetKey = point.id() + 1;
@@ -3975,14 +3971,12 @@ bool EventHandler::handleTouchEvent(const PlatformTouchEvent& event)
             m_originatingTouchPointTargets.set(touchPointTargetKey, node);
             touchTarget = node;
         } else if (pointState == PlatformTouchPoint::TouchReleased || pointState == PlatformTouchPoint::TouchCancelled) {
-            // We only perform a hittest on release or cancel to unset :active or :hover state.
-            if (touchPointTargetKey == m_originatingTouchPointTargetKey) {
-                hitTestResultAtPoint(pagePoint, hitType);
+            // No need to perform a hit-test since we only need to unset :hover and :active states.
+            if (!shouldGesturesTriggerActive() && allTouchReleased)
+                m_frame->document()->updateHoverActiveState(hitType, 0);
+            if (touchPointTargetKey == m_originatingTouchPointTargetKey)
                 m_originatingTouchPointTargetKey = 0;
-            } else if (m_originatingTouchPointDocument.get() && m_originatingTouchPointDocument->frame()) {
-                LayoutPoint pagePointInOriginatingDocument = documentPointForWindowPoint(m_originatingTouchPointDocument->frame(), point.pos());
-                hitTestResultInFrame(m_originatingTouchPointDocument->frame(), pagePointInOriginatingDocument, hitType);
-            }
+
             // The target should be the original target for this touch, so get it from the hashmap. As it's a release or cancel
             // we also remove it from the map.
             touchTarget = m_originatingTouchPointTargets.take(touchPointTargetKey);

LayoutTests/fast/events/touch/frame-hover-update-expected.txt
@@ -1,0 +1,21 @@
+PASS [object HTMLDocument] is non-null.
+PASS [object HTMLDocument] is non-null.
+PASS hoveredTop.length is 1
+PASS hoveredIframe1.length is 1
+PASS hoveredIframe2.length is 0
+PASS hoveredTop.length is 1
+PASS hoveredIframe1.length is 0
+PASS hoveredIframe2.length is 1
+PASS hoveredTop.length is 0
+PASS hoveredIframe1.length is 0
+PASS hoveredIframe2.length is 0
+PASS hoveredTop.length is 1
+PASS hoveredIframe1.length is 0
+PASS hoveredIframe2.length is 0
+PASS hoveredTop.length is 1
+PASS hoveredIframe1.length is 0
+PASS hoveredIframe2.length is 0
+PASS successfullyParsed is true
+
+TEST COMPLETE
+  

LayoutTests/fast/events/touch/frame-hover-update.html
@@ -1,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test that hover is unset in inner documents</title>
+    <style>
+        #sandbox {
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            width: 300px;
+            height: 100px;
+        }
+        #iframe1 {
+            left: 0px;
+            top: 0px;
+            width: 100px;
+            height: 100px;
+            border: none;
+        }
+        #iframe2 {
+            left: 100px;
+            top: 0px;
+            width: 100px;
+            height: 100px;
+            border: none;
+        }
+    </style>
+    <script src="../js/resources/js-test-pre.js"></script>
+</head>
+<body>
+    <div id='sandbox'>
+        <iframe id="iframe1" src="data:text/html,<body>iframe1</body>"></iframe>
+        <iframe id="iframe2" src="data:text/html,<body>iframe2</body>"></iframe>
+    </div>
+
+    <script>
+        var hoveredTop, hoveredIframe1, hoveredIframe2;
+
+        function runTest() {
+            if (window.testRunner) {
+                var idoc1 = document.getElementById('iframe1').contentDocument;
+                var idoc2 = document.getElementById('iframe2').contentDocument;
+                shouldBeNonNull(idoc1);
+                shouldBeNonNull(idoc2);
+                eventSender.clearTouchPoints();
+
+                eventSender.addTouchPoint(50, 50);
+                eventSender.touchStart();
+                hoveredTop = document.querySelectorAll('iframe:hover');
+                hoveredIframe1 = idoc1.querySelectorAll('body:hover');
+                hoveredIframe2 = idoc2.querySelectorAll('body:hover');
+                shouldBe('hoveredTop.length', '1');
+                shouldBe('hoveredIframe1.length', '1');
+                shouldBe('hoveredIframe2.length', '0');
+                eventSender.releaseTouchPoint(0);
+                eventSender.touchEnd();
+
+                eventSender.addTouchPoint(150, 50);
+                eventSender.touchStart();
+                hoveredTop = document.querySelectorAll('iframe:hover');
+                hoveredIframe1 = idoc1.querySelectorAll('body:hover');
+                hoveredIframe2 = idoc2.querySelectorAll('body:hover');
+                shouldBe('hoveredTop.length', '1');
+                shouldBe('hoveredIframe1.length', '0');
+                shouldBe('hoveredIframe2.length', '1');
+                eventSender.releaseTouchPoint(0);
+                eventSender.touchCancel();
+
+                eventSender.addTouchPoint(250, 50);
+                eventSender.touchStart();
+                hoveredTop = document.querySelectorAll('iframe:hover');
+                hoveredIframe1 = idoc1.querySelectorAll('body:hover');
+                hoveredIframe2 = idoc2.querySelectorAll('body:hover');
+                shouldBe('hoveredTop.length', '0');
+                shouldBe('hoveredIframe1.length', '0');
+                shouldBe('hoveredIframe2.length', '0');
+                eventSender.addTouchPoint(150, 50);
+                eventSender.touchMove();
+                hoveredTop = document.querySelectorAll('#sandbox:hover');
+                hoveredIframe1 = idoc1.querySelectorAll('body:hover');
+                hoveredIframe2 = idoc2.querySelectorAll('body:hover');
+                shouldBe('hoveredTop.length', '1');
+                shouldBe('hoveredIframe1.length', '0');
+                shouldBe('hoveredIframe2.length', '0');
+                eventSender.releaseTouchPoint(0);
+                eventSender.touchMove();
+                hoveredTop = document.querySelectorAll('#sandbox:hover');
+                hoveredIframe1 = idoc1.querySelectorAll('body:hover');
+                hoveredIframe2 = idoc2.querySelectorAll('body:hover');
+                shouldBe('hoveredTop.length', '1');
+                shouldBe('hoveredIframe1.length', '0');
+                shouldBe('hoveredIframe2.length', '0');
+                eventSender.releaseTouchPoint(0);
+                eventSender.touchEnd();
+            }
+        }
+        runTest();
+    </script>
+    <script src="../js/resources/js-test-post.js"></script>
+</body>
+</html>