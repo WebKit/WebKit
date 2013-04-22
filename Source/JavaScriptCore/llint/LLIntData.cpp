ff7efc93b287d75c3db10f961eb19cd30268098f
Oliver Hunt <oliver@apple.com>
2013-04-22 18:19:06 +0000

    2013-04-22  Oliver Hunt  <oliver@apple.com>
            Fix assertions to account for new Vector layout
    
            RS=Gavin
    
            * llint/LLIntData.cpp:
            (JSC::LLInt::Data::performAssertions):
    
    Canonical link: https://commits.webkit.org/133397@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@148896 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/llint/LLIntData.cpp
+++ Source/JavaScriptCore/llint/LLIntData.cpp
@@ -116,8 +116,8 @@ void Data::performAssertions(VM& vm)
 #if !ASSERT_DISABLED
     Vector<int> testVector;
     testVector.resize(42);
-    ASSERT(bitwise_cast<size_t*>(&testVector)[0] == 42);
-    ASSERT(bitwise_cast<int**>(&testVector)[1] == testVector.begin());
+    ASSERT(bitwise_cast<uint32_t*>(&testVector)[sizeof(void*)/sizeof(uint32_t) + 1] == 42);
+    ASSERT(bitwise_cast<int**>(&testVector)[0] == testVector.begin());
 #endif
 
     ASSERT(StringImpl::s_hashFlag8BitBuffer == 64);
