9864bd401ce2e157a74c0fbc5be37b3add04fa1d
Seokju Kwon <seokju.kwon@gmail.com>
2013-04-25 00:51:29 +0000

    Add efl to JSInspectorFrontendHost::port() https://bugs.webkit.org/show_bug.cgi?id=115137
    Patch by Seokju Kwon <seokju.kwon@gmail.com> on 2013-04-24
    Reviewed by Timothy Hatcher.
    
    No new tests, because there is no change in behavior.
    
    * bindings/js/JSInspectorFrontendHostCustom.cpp:
    (WebCore::JSInspectorFrontendHost::port):
    
    Canonical link: https://commits.webkit.org/133579@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@149081 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/js/JSInspectorFrontendHostCustom.cpp
+++ Source/WebCore/bindings/js/JSInspectorFrontendHostCustom.cpp
@@ -79,6 +79,8 @@ JSValue JSInspectorFrontendHost::port(ExecState* execState)
     DEFINE_STATIC_LOCAL(const String, port, (ASCIILiteral("gtk")));
 #elif PLATFORM(WX)
     DEFINE_STATIC_LOCAL(const String, port, (ASCIILiteral("wx")));
+#elif PLATFORM(EFL)
+    DEFINE_STATIC_LOCAL(const String, port, (ASCIILiteral("efl")));
 #else
     DEFINE_STATIC_LOCAL(const String, port, (ASCIILiteral("unknown")));
 #endif
