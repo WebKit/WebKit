2a194875d962e478c7c4d45123215b202c1ee7b8
Andreas Kling <akling@apple.com>
2013-04-25 20:37:50 +0000

    Remove ENABLE(PARSED_STYLE_SHEET_CACHING) and make it always-on.
    Rubber-stamped by Anders Koivisto.
    
    Source/WebCore:
    
    * PlatformWinCE.cmake:
    * accessibility/AccessibilityObject.h:
    * html/HTMLLinkElement.cpp:
    (WebCore::HTMLLinkElement::setCSSStyleSheet):
    
    Source/WebKit:
    
    * PlatformWinCE.cmake:
    
    Source/WebKit/blackberry:
    
    * WebCoreSupport/AboutDataEnableFeatures.in:
    
    Source/WTF:
    
    * wtf/FeatureDefines.h:
    
    Canonical link: https://commits.webkit.org/133637@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@149140 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLLinkElement.cpp
+++ Source/WebCore/html/HTMLLinkElement.cpp
@@ -300,7 +300,6 @@ void HTMLLinkElement::setCSSStyleSheet(const String& href, const KURL& baseURL, 
 
     CSSParserContext parserContext(document(), baseURL, charset);
 
-#if ENABLE(PARSED_STYLE_SHEET_CACHING)
     if (RefPtr<StyleSheetContents> restoredSheet = const_cast<CachedCSSStyleSheet*>(cachedStyleSheet)->restoreParsedStyleSheet(parserContext)) {
         ASSERT(restoredSheet->isCacheable());
         ASSERT(!restoredSheet->isLoading());
@@ -314,7 +313,6 @@ void HTMLLinkElement::setCSSStyleSheet(const String& href, const KURL& baseURL, 
         notifyLoadedSheetAndAllCriticalSubresources(false);
         return;
     }
-#endif
 
     RefPtr<StyleSheetContents> styleSheet = StyleSheetContents::create(href, parserContext);
 
@@ -328,10 +326,8 @@ void HTMLLinkElement::setCSSStyleSheet(const String& href, const KURL& baseURL, 
     styleSheet->notifyLoadedSheet(cachedStyleSheet);
     styleSheet->checkLoaded();
 
-#if ENABLE(PARSED_STYLE_SHEET_CACHING)
     if (styleSheet->isCacheable())
         const_cast<CachedCSSStyleSheet*>(cachedStyleSheet)->saveParsedStyleSheet(styleSheet);
-#endif
 }
 
 bool HTMLLinkElement::styleSheetIsLoading() const
