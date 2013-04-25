ed03a3d18b3a70b7dcef3df4c156be7970e2acd7
Andreas Kling <akling@apple.com>
2013-04-25 12:32:49 +0000

    Remove isPluginElement hack in Document::setFocusedNode() <http://webkit.org/b/115171>
    From Blink r148800 by <tkent@chromium.org>:
    
    This hack is no longer necessary because r147591 made isFocusable for plugin
    elements work correctly.
    
    * dom/Document.cpp:
    (WebCore::Document::setFocusedNode):
    
    Canonical link: https://commits.webkit.org/133598@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@149101 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/Document.cpp
+++ Source/WebCore/dom/Document.cpp
@@ -3419,7 +3419,7 @@ bool Document::setFocusedNode(PassRefPtr<Node> prpNewFocusedNode, FocusDirection
         }
     }
 
-    if (newFocusedNode && (newFocusedNode->isPluginElement() || newFocusedNode->isFocusable())) {
+    if (newFocusedNode && newFocusedNode->isFocusable()) {
         if (newFocusedNode->isRootEditableElement() && !acceptsEditingFocus(newFocusedNode.get())) {
             // delegate blocks focus change
             focusChangeBlocked = true;
