628161f963dd521282730d6542da284371e7b9b3
Alexey Proskuryakov <ap@apple.com>
2013-04-26 17:51:13 +0000

    Blobs are not associated with connection when there are no sandbox extensions         https://bugs.webkit.org/show_bug.cgi?id=115264
            Reviewed by Brady Eidson.
    
            Covered by multiple fast/files tests when using NetworkProcess.
    
            * NetworkProcess/FileAPI/NetworkBlobRegistry.cpp:
            (WebKit::NetworkBlobRegistry::registerBlobURL): Fixed a logic error.
    
    
    
    Canonical link: https://commits.webkit.org/133692@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@149196 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/NetworkProcess/FileAPI/NetworkBlobRegistry.cpp
+++ Source/WebKit2/NetworkProcess/FileAPI/NetworkBlobRegistry.cpp
@@ -75,13 +75,12 @@ void NetworkBlobRegistry::registerBlobURL(NetworkConnectionToWebProcess* connect
 {
     blobRegistry().registerBlobURL(url, srcURL);
     SandboxExtensionMap::iterator iter = m_sandboxExtensions.find(srcURL.string());
-    if (iter != m_sandboxExtensions.end()) {
+    if (iter != m_sandboxExtensions.end())
         m_sandboxExtensions.add(url.string(), iter->value);
 
-        ASSERT(m_blobsForConnection.contains(connection));
-        ASSERT(m_blobsForConnection.find(connection)->value.contains(srcURL));
-        m_blobsForConnection.find(connection)->value.add(url);
-    }
+    ASSERT(m_blobsForConnection.contains(connection));
+    ASSERT(m_blobsForConnection.find(connection)->value.contains(srcURL));
+    m_blobsForConnection.find(connection)->value.add(url);
 }
 
 void NetworkBlobRegistry::unregisterBlobURL(NetworkConnectionToWebProcess* connection, const WebCore::KURL& url)
