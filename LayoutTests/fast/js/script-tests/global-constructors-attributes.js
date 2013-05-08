04e5945d9aaec64b74fa35eb8fe43827902fb3b8
Chris Dumez <ch.dumez@sisa.samsung.com>
2013-05-08 19:28:24 +0000

    Add layout test that lists all global constructors https://bugs.webkit.org/show_bug.cgi?id=115724
    Reviewed by Ryosuke Niwa.
    
    Update fast/js/global-constructors-attributes.html so that it lists all
    global constructors instead of just a few.
    
    * fast/js/global-constructors-attributes-expected.txt:
    * fast/js/script-tests/global-constructors-attributes.js:
    (classNameForObject):
    (constructorPropertiesOnWindow):
    * platform/efl/fast/js/global-constructors-attributes-expected.txt: Added.
    * platform/gtk/fast/js/global-constructors-attributes-expected.txt: Added.
    
    
    Canonical link: https://commits.webkit.org/134219@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@149758 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/js/script-tests/global-constructors-attributes.js
+++ LayoutTests/fast/js/script-tests/global-constructors-attributes.js
@@ -1,28 +1,52 @@
 description("Test to ensure that global constructors have the right attributes");
 
 function descriptorShouldBe(object, property, expected) {
-    var test = 'Object.getOwnPropertyDescriptor('+object+", "+property+')';
+    var test = "Object.getOwnPropertyDescriptor(" + object + ", '" + eval(property) + "')";
     if ("writable" in expected) {
-      //  shouldBe(test+'.writable', '' + expected.writable);
-        shouldBe(test+'.value', '' + expected.value);
-        shouldBeFalse(test+".hasOwnProperty('get')");
-        shouldBeFalse(test+".hasOwnProperty('set')");
+        shouldBe(test + ".value", "" + expected.value);
+        shouldBeFalse(test + ".hasOwnProperty('get')");
+        shouldBeFalse(test + ".hasOwnProperty('set')");
     } else {
-        shouldBe(test+'.get', '' + expected.get);
-        shouldBe(test+'.set', '' + expected.set);
-        shouldBeFalse(test+".hasOwnProperty('value')");
-        shouldBeFalse(test+".hasOwnProperty('writable')");
+        shouldBe(test + ".get", "" + expected.get);
+        shouldBe(test + ".set", "" + expected.set);
+        shouldBeFalse(test + ".hasOwnProperty('value')");
+        shouldBeFalse(test + ".hasOwnProperty('writable')");
     }
-    shouldBe(test+'.enumerable', '' + expected.enumerable);
-    shouldBe(test+'.configurable', '' + expected.configurable);
+    shouldBe(test + ".enumerable", "" + expected.enumerable);
+    shouldBe(test + ".configurable", "" + expected.configurable);
+}
+
+function classNameForObject(object)
+{
+    // call will use the global object if passed null or undefined, so special case those:
+    if (object == null)
+        return null;
+    var result = Object.prototype.toString.call(object);
+    // remove "[object " and "]"
+    return result.split(" ")[1].split("]")[0];
+}
+
+function constructorPropertiesOnWindow(globalObject)
+{
+    var constructorNames = [];
+    var propertyNames = Object.getOwnPropertyNames(global);
+    for (var i = 0; i < propertyNames.length; i++) {
+        var value = global[propertyNames[i]];
+        if (value == null)
+            continue;
+        var type = classNameForObject(value);
+        if (!type.match("Constructor$"))
+            continue;
+        constructorNames.push(propertyNames[i]);
+    }
+    return constructorNames.sort();
 }
 
 var global = this;
+var constructorNames = constructorPropertiesOnWindow(global);
 
-descriptorShouldBe("global", "'CSSRuleList'", {writable: true, enumerable: false, configurable: true, value:"CSSRuleList"});
-descriptorShouldBe("global", "'Document'", {writable: true, enumerable: false, configurable: true, value:"Document"});
-descriptorShouldBe("global", "'Element'", {writable: true, enumerable: false, configurable: true, value:"Element"});
-descriptorShouldBe("global", "'HTMLDivElement'", {writable: true, enumerable: false, configurable: true, value:"HTMLDivElement"});
-descriptorShouldBe("global", "'ProgressEvent'", {writable: true, enumerable: false, configurable: true, value:"ProgressEvent"});
-descriptorShouldBe("global", "'Selection'", {writable: true, enumerable: false, configurable: true, value:"Selection"});
-descriptorShouldBe("global", "'XMLHttpRequest'", {writable: true, enumerable: false, configurable: true, value:"XMLHttpRequest"});
+var constructorName;
+for (var i = 0; i < constructorNames.length; i++) {
+    constructorName = constructorNames[i];
+    descriptorShouldBe("global", "constructorName", {writable: true, enumerable: false, configurable: true, value: constructorName});
+}
