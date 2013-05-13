01ba267c6227372d861501348af7eccbf1dd611a
Manuel Rego Casasnovas <rego@igalia.com>
2013-05-13 06:48:55 +0000

    Merge r148788 - [GTK] Fix unit test webkit2/WebKitFindController/hide https://bugs.webkit.org/show_bug.cgi?id=89810
    Patch by Manuel Rego Casasnovas <rego@igalia.com> on 2013-04-20
    Reviewed by Carlos Garcia Campos.
    
    Source/WebKit2:
    
    The test had some hacks in order to compare a page with highlighted
    results after using the find command with the original page. Now it uses
    the snapshots API that allows to make the test simpler and more
    reliable.
    
    * UIProcess/API/gtk/tests/TestMain.h:
    (Test::cairoSurfacesEqual): Moved helper function to compare two cairo
    surfaces from TestWebKitWebView.
    (Test):
    * UIProcess/API/gtk/tests/TestWebKitFindController.cpp:
    (testFindControllerHide): Modify test to use snapshots.
    * UIProcess/API/gtk/tests/TestWebKitWebView.cpp:
    (testWebViewSnapshot): Move basic snapshop methods to WebViewTest to
    share them with TestWebKitFindController test.
    * UIProcess/API/gtk/tests/WebViewTest.cpp:
    (WebViewTest::WebViewTest): Initialize cairo surface.
    (WebViewTest::~WebViewTest): Destroy cairo surface.
    (onSnapshotReady): Callback to set the cairo surface when the snapshot
    is ready.
    (WebViewTest::getSnapshotAndWaitUntilReady): Method that takes a
    snapshot and returns the cairo surface when it is ready.
    * UIProcess/API/gtk/tests/WebViewTest.h: Add new method headers and
    attribute for the cairo surface.
    
    Tools:
    
    * Scripts/run-gtk-tests:
    (TestRunner): Unskip test webkit2/WebKitFindController/hide.

--- Source/WebKit2/UIProcess/API/gtk/tests/WebViewTest.h
+++ Source/WebKit2/UIProcess/API/gtk/tests/WebViewTest.h
@@ -65,6 +65,8 @@ public:
     static bool javascriptResultIsNull(WebKitJavascriptResult*);
     static bool javascriptResultIsUndefined(WebKitJavascriptResult*);
 
+    cairo_surface_t* getSnapshotAndWaitUntilReady(WebKitSnapshotRegion, WebKitSnapshotOptions);
+
     WebKitWebView* m_webView;
     GMainLoop* m_mainLoop;
     CString m_activeURI;
@@ -74,6 +76,7 @@ public:
     GError** m_javascriptError;
     GOwnPtr<char> m_resourceData;
     size_t m_resourceDataSize;
+    cairo_surface_t* m_surface;
 
 private:
     void doMouseButtonEvent(GdkEventType, int, int, unsigned int, unsigned int);
