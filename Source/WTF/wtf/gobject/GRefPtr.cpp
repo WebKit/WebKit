387bb66d55ff5874ceed73ad1c3877a398d7034e
Carlos Garcia Campos <cgarcia@igalia.com>
2013-05-13 16:19:48 +0000

    Unreviewed. Fix Qt Linux MIPS32R2 build after r149879.
    Bring back GLIB_CHECK_VERSION for GBytes since Qt Linux MIPS32R2
    depends on glib 2.24.2.
    
    * wtf/gobject/GRefPtr.cpp:
    (_GBytes):
    (WTF::refGPtr):
    (WTF::derefGPtr):
    
    Canonical link: https://commits.webkit.org/134452@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@150019 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/gobject/GRefPtr.cpp
+++ Source/WTF/wtf/gobject/GRefPtr.cpp
@@ -63,6 +63,7 @@ template <> void derefGPtr(GMainLoop* ptr)
         g_main_loop_unref(ptr);
 }
 
+#if GLIB_CHECK_VERSION(2, 32, 0)
 template <> GBytes* refGPtr(GBytes* ptr)
 {
     if (ptr)
@@ -75,6 +76,23 @@ template <> void derefGPtr(GBytes* ptr)
     if (ptr)
         g_bytes_unref(ptr);
 }
+
+# else
+
+typedef struct _GBytes {
+    bool fake;
+} GBytes;
+
+template <> GBytes* refGPtr(GBytes* ptr)
+{
+    return ptr;
+}
+
+template <> void derefGPtr(GBytes* ptr)
+{
+}
+
+#endif
 
 template <> GVariant* refGPtr(GVariant* ptr)
 {
