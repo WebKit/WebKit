3b11d68589f14ca3f1593d7f3f96a4da1634304a
Gustavo Noronha Silva <gustavo.noronha@collabora.com>
2013-05-17 14:33:39 +0000

    Unreviewed build fix. Remove assertion that doesn't make sense anymore since the page does not give us access to the Chrome pointer directly anymore.
    Patch by Gustavo Noronha Silva <gustavo.noronha@collabora.com> on 2013-05-17
    
    * webkit/webkitwebview.cpp:
    (WebKit::kit):
    
    Canonical link: https://commits.webkit.org/134678@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@150256 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/gtk/webkit/webkitwebview.cpp
+++ Source/WebKit/gtk/webkit/webkitwebview.cpp
@@ -5537,7 +5537,6 @@ WebKitWebView* kit(WebCore::Page* corePage)
     if (!corePage)
         return 0;
 
-    ASSERT(corePage->chrome());
     WebKit::ChromeClient* client = static_cast<WebKit::ChromeClient*>(corePage->chrome().client());
     return client ? static_cast<WebKitWebView*>(client->webView()) : 0;
 }
