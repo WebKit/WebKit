ce6fae89d07667fbb33b4a8852fb6db4867affb7
Hans Muller <hmuller@adobe.com>
2013-05-22 21:22:56 +0000

    [CSS Exclusions] property parsing tests should be revised https://bugs.webkit.org/show_bug.cgi?id=116446
    Reviewed by Alexandru Chiculita.
    
    This is just a cleanup of the existing parsing tests. None of the tests have effectively
    changed. The cleaned-up version should be simpler, more regular, and shorter:
    - Moved all of parsing tests into a fast/exclusions/parsing subdirectory.
    - Remove the obsolete "wrap-" prefix from file names.
    - Eiminated excessive and needless stringification in many of the test functions.
    - Moved common code into parsing-test-utils.js and refactored redundant code.
    
    * fast/exclusions/parsing-wrap-shape-inside-expected.txt: Removed.
    * fast/exclusions/parsing-wrap-shape-inside.html: Removed.
    * fast/exclusions/parsing-wrap-shape-lengths-expected.txt: Removed.
    * fast/exclusions/parsing-wrap-shape-lengths.html: Removed.
    * fast/exclusions/parsing-wrap-shape-outside-expected.txt: Removed.
    * fast/exclusions/parsing-wrap-shape-outside.html: Removed.
    * fast/exclusions/parsing/parsing-shape-inside-expected.txt: Added.
    * fast/exclusions/parsing/parsing-shape-inside.html: Added.
    * fast/exclusions/parsing/parsing-shape-lengths-expected.txt: Added.
    * fast/exclusions/parsing/parsing-shape-lengths.html: Added.
    * fast/exclusions/parsing/parsing-shape-margin-expected.txt: Added.
    * fast/exclusions/parsing/parsing-shape-margin.html: Added.
    * fast/exclusions/parsing/parsing-shape-outside-expected.txt: Added.
    * fast/exclusions/parsing/parsing-shape-outside.html: Added.
    * fast/exclusions/parsing/parsing-shape-padding-expected.txt: Added.
    * fast/exclusions/parsing/parsing-shape-padding.html: Added.
    * fast/exclusions/parsing/parsing-wrap-flow-expected.txt: Added.
    * fast/exclusions/parsing/parsing-wrap-flow.html: Added.
    * fast/exclusions/parsing/parsing-wrap-through-expected.txt: Added.
    * fast/exclusions/parsing/parsing-wrap-through.html: Added.
    * fast/exclusions/parsing/script-tests/parsing-shape-inside.js: Added.
    (string_appeared_here):
    * fast/exclusions/parsing/script-tests/parsing-shape-lengths.js: Added.
    (getStyleValue):
    (testStyleValue):
    (testComputedStyleValue):
    (testInvalidValue):
    * fast/exclusions/parsing/script-tests/parsing-shape-margin.js: Added.
    (testShapeSpecifiedProperty):
    * fast/exclusions/parsing/script-tests/parsing-shape-outside.js: Added.
    (testShapePropertyParsingFailure):
    * fast/exclusions/parsing/script-tests/parsing-shape-padding.js: Added.
    (testShapeSpecifiedProperty):
    * fast/exclusions/parsing/script-tests/parsing-test-utils.js: Added.
    (getCSSText):
    (getComputedStyleValue):
    (getParentAndChildComputedStyles):
    (getParentAndChildComputedStylesString):
    (getChildComputedStyle):
    (testShapeSpecifiedProperty):
    (testShapeComputedProperty):
    (testNotInheritedShapeChildProperty):
    * fast/exclusions/parsing/script-tests/parsing-wrap-flow.js: Added.
    * fast/exclusions/parsing/script-tests/parsing-wrap-through.js: Added.
    * fast/exclusions/shape-margin-parsing-expected.txt: Removed.
    * fast/exclusions/shape-margin-parsing.html: Removed.
    * fast/exclusions/shape-padding-parsing-expected.txt: Removed.
    * fast/exclusions/shape-padding-parsing.html: Removed.
    * fast/exclusions/wrap-flow-parsing-expected.txt: Removed.
    * fast/exclusions/wrap-flow-parsing.html: Removed.
    * fast/exclusions/wrap-through-parsing-expected.txt: Removed.
    * fast/exclusions/wrap-through-parsing.html: Removed.
    
    
    Canonical link: https://commits.webkit.org/134935@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@150542 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/fast/exclusions/parsing/script-tests/parsing-shape-outside.js
@@ -0,0 +1,32 @@
+description('Testing parsing of the -webkit-shape-outside property.');
+
+if (window.internals)
+    window.internals.settings.setCSSExclusionsEnabled(true);
+
+// The test functions and globals used here are defined parsing-test-utils.js.
+
+validShapeValues.forEach(function(elt, i, a) { 
+    var value = (elt instanceof Array) ? elt[0] : elt;
+    var expectedValue = (elt instanceof Array) ? elt[1] : elt;
+    testShapeSpecifiedProperty("-webkit-shape-outside", value, expectedValue);
+    testShapeComputedProperty("-webkit-shape-outside", value, expectedValue);
+});
+
+testLocalURLShapeProperty("-webkit-shape-outside", "url(\'image\')", "url(image)");
+
+invalidShapeValues.forEach(function(value, i, a) { 
+    testShapePropertyParsingFailure("-webkit-shape-outside", value, "auto") 
+});
+
+testShapePropertyParsingFailure("-webkit-shape-outside", "outside-shape", "auto");
+
+applyToEachArglist(
+    testNotInheritedShapeProperty,
+    [// [property, parentValue, childValue, expectedValue]
+     ["-webkit-shape-outside", "auto", "rectangle(10px, 20px, 30px, 40px)", "parent: auto, child: rectangle(10px, 20px, 30px, 40px)"],
+     ["-webkit-shape-outside", "rectangle(10px, 20px, 30px, 40px)", "initial", "parent: rectangle(10px, 20px, 30px, 40px), child: auto"],
+     ["-webkit-shape-outside", "rectangle(10px, 20px, 30px, 40px)", "", "parent: rectangle(10px, 20px, 30px, 40px), child: auto"],
+     ["-webkit-shape-outside", "rectangle(10px, 20px, 30px, 40px)", "inherit", "parent: rectangle(10px, 20px, 30px, 40px), child: rectangle(10px, 20px, 30px, 40px)"],
+     ["-webkit-shape-outside", "", "inherit", "parent: auto, child: auto"],
+     ["-webkit-shape-outside", "auto", "inherit", "parent: auto, child: auto"]]
+);
