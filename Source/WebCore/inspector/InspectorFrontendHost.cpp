d2780104b00447e4a71eb8dcfc8b5d8ae77bb2a8
Joseph Pecoraro <pecoraro@apple.com>
2013-05-22 02:13:58 +0000

    Web Inspector: InspectorFrontendHost::loadResourceSynchronously() builds ASCII-only results https://bugs.webkit.org/show_bug.cgi?id=116569
    Merge: https://chromium.googlesource.com/chromium/blink/+/7f1cfecfe34f4dbc6ce1ef0250b13f2058128eeb
    
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2013-05-21
    Reviewed by Timothy Hatcher.
    
    Source/WebCore:
    
    Test: inspector/load-resource-synchronously-utf8.html
    
    * inspector/InspectorFrontendHost.cpp:
    (WebCore::InspectorFrontendHost::loadResourceSynchronously):
    
    LayoutTests:
    
    * inspector/load-resource-synchronously-utf8-expected.txt: Added.
    * inspector/load-resource-synchronously-utf8.html: Added.
    
    Canonical link: https://commits.webkit.org/134897@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@150496 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/InspectorFrontendHost.cpp
+++ Source/WebCore/inspector/InspectorFrontendHost.cpp
@@ -293,7 +293,7 @@ String InspectorFrontendHost::loadResourceSynchronously(const String& url)
     ResourceError error;
     ResourceResponse response;
     m_frontendPage->mainFrame()->loader()->loadResourceSynchronously(request, DoNotAllowStoredCredentials, DoNotAskClientForCrossOriginCredentials, error, response, data);
-    return String(data.data(), data.size());
+    return String::fromUTF8(data.data(), data.size());
 }
 
 bool InspectorFrontendHost::supportsFileSystems()
