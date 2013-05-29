c8394ce44a43e29d15d6045a078ef19f9e88cca4
Adam Barth <abarth@webkit.org>
2013-05-29 19:26:16 +0000

    Optimize RenderText::offsetNext for 8 bit strings https://bugs.webkit.org/show_bug.cgi?id=116877
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    Previously, this function was always upconverting strings to 16 bits in
    order to create a cursorMovementIterator. This patch adds a fast path
    for ASCII and Latin-1 strings, which don't need this complicated cursor
    movement logic.
    
    I ran into this code because nextOffset showed up as a memory pig on a
    memory profile of Mobile Gmail.
    
    Test: editing/selection/move-by-character-brute-force.html
    
    * rendering/RenderText.cpp:
    (WebCore::RenderText::previousOffset):
    (WebCore::RenderText::nextOffset):
    
    LayoutTests:
    
    Test that caret movement works correctly for many code points.
    
    * editing/selection/move-by-character-brute-force-expected.txt: Added.
    * editing/selection/move-by-character-brute-force.html: Added.
    
    
    Canonical link: https://commits.webkit.org/135257@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@150922 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderText.cpp
+++ Source/WebCore/rendering/RenderText.cpp
@@ -1725,8 +1725,11 @@ unsigned RenderText::renderedTextLength() const
 
 int RenderText::previousOffset(int current) const
 {
-    StringImpl* si = m_text.impl();
-    TextBreakIterator* iterator = cursorMovementIterator(si->characters(), si->length());
+    if (isAllASCII() || m_text.is8Bit())
+        return current - 1;
+
+    StringImpl* textImpl = m_text.impl();
+    TextBreakIterator* iterator = cursorMovementIterator(textImpl->characters16(), textImpl->length());
     if (!iterator)
         return current - 1;
 
@@ -1878,15 +1881,17 @@ int RenderText::previousOffsetForBackwardDeletion(int current) const
 
 int RenderText::nextOffset(int current) const
 {
-    StringImpl* si = m_text.impl();
-    TextBreakIterator* iterator = cursorMovementIterator(si->characters(), si->length());
+    if (isAllASCII() || m_text.is8Bit())
+        return current + 1;
+
+    StringImpl* textImpl = m_text.impl();
+    TextBreakIterator* iterator = cursorMovementIterator(textImpl->characters16(), textImpl->length());
     if (!iterator)
         return current + 1;
 
     long result = textBreakFollowing(iterator, current);
     if (result == TextBreakDone)
         result = current + 1;
-
 
     return result;
 }
