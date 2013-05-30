d03494000ace11cb6688b65bff20e0c5eedda0cb
Praveen R Jadhav <praveen.j@samsung.com>
2013-05-30 06:04:29 +0000

    ProgressEvent should not be cancelable https://bugs.webkit.org/show_bug.cgi?id=114444
    Patch by Praveen R Jadhav <praveen.j@samsung.com> on 2013-05-29
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    Parameter "cancelable" in ProgressEvent is set to false, by default, as mentioned in the specification. The spec link is given below.
    
    Spec: http://www.w3.org/TR/2011/CR-progress-events-20110922/#suggested-names-for-events-using-the-progressevent-interface
    
    No new tests. file-reader-event-listener.html and infoOnProgressEvent-expected.txt are updated.
    
    * dom/ProgressEvent.cpp:
    (WebCore::ProgressEvent::ProgressEvent):
    (WebCore):
    
    LayoutTests:
    
    file-reader-event-listener.html, xmlhttprequest-get-expected.txt and infoOnProgressEvent-expected.txt are updated.
    
    * fast/files/file-reader-event-listener-expected.txt:
    * fast/files/file-reader-event-listener.html:
    * fast/xmlhttprequest/xmlhttprequest-get-expected.txt:
    * http/tests/xmlhttprequest/infoOnProgressEvent-expected.txt:
    * platform/efl/TestExpectations:
    
    Canonical link: https://commits.webkit.org/135281@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@150949 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/ProgressEvent.cpp
+++ Source/WebCore/dom/ProgressEvent.cpp
@@ -53,13 +53,13 @@ ProgressEvent::ProgressEvent(const AtomicString& type, const ProgressEventInit& 
 }
 
 ProgressEvent::ProgressEvent(const AtomicString& type, bool lengthComputable, unsigned long long loaded, unsigned long long total)
-    : Event(type, false, true)
+    : Event(type, false, false)
     , m_lengthComputable(lengthComputable)
     , m_loaded(loaded)
     , m_total(total)
 {
 }
-    
+
 const AtomicString& ProgressEvent::interfaceName() const
 {
     return eventNames().interfaceForProgressEvent;
