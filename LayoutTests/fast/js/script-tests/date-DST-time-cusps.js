d4b4cae8b75647b7da012b934012f22fed29bdfe
Gavin Barraclough <barraclough@apple.com>
2013-05-31 01:52:47 +0000

    fast/js/date-DST-time-cusps.html fails after http://trac.webkit.org/changeset/150833 https://bugs.webkit.org/show_bug.cgi?id=116895 <rdar://problem/4777813> Safari incorrectly handles times that shouldn't exist https://bugs.webkit.org/show_bug.cgi?id=11157
    Reviewed by Alexey Proskuryakov.
    
    Accidental progression, updating results to expect success.
    
    * TestExpectations:
        - Remove expectations of failure.
    * fast/js/script-tests/date-DST-time-cusps.js:
        - Prefer the latter option for non-existent time (due to clock rolling forward).
    
    
    
    Canonical link: https://commits.webkit.org/135321@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@151000 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/js/script-tests/date-DST-time-cusps.js
+++ LayoutTests/fast/js/script-tests/date-DST-time-cusps.js
@@ -3,17 +3,13 @@ description(
 );
 
 description(
-"For times that shouldn't happen we currently go back an hour, but in reality we would like to go forward an hour.  This has been filed as a radar: 4777813"
-);
-
-description(
-"For times that happen twice the behavior of all major browsers seems to be to pick the second occurance, i.e. Standard Time not Daylight Time"
+"For times that happen twice the behavior of all major browsers seems to be to pick the second occurrance, i.e. Standard Time not Daylight Time"
 );
 
 var testCases = [];
 if ((new Date(2009, 9, 1)).toString().match("PDT")) {
-    testCases.push(["(new Date(1982, 2, 14, 2, 10)).getHours()", "1"]);
-    testCases.push(["(new Date(1982, 2, 14, 2)).getHours()", "1"]);
+    testCases.push(["(new Date(1982, 2, 14, 2, 10)).getHours()", "3"]);
+    testCases.push(["(new Date(1982, 2, 14, 2)).getHours()", "3"]);
     testCases.push(["(new Date(1982, 11, 7, 1, 10)).getTimezoneOffset()", "480"]);
     testCases.push(["(new Date(1982, 11, 7, 1)).getTimezoneOffset()", "480"]);
 }
