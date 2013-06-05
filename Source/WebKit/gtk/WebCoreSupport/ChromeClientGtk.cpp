2a9a5bdf5970ce3a686517430bec68a6ead74bc0
Alberto Garcia <agarcia@igalia.com>
2013-06-05 04:38:44 +0000

    [GTK] Make precedence of logical operators explicit https://bugs.webkit.org/show_bug.cgi?id=117216
    Patch by Alberto Garcia <agarcia@igalia.com> on 2013-06-04
    Reviewed by Martin Robinson.
    
    There's a couple of cases where we mix && and || in the same
    expression. In ChromeClient::contentsSizeChanged() the lack of
    parentheses actually makes the expression wrong.
    
    * WebCoreSupport/AcceleratedCompositingContextGL.cpp:
    (WebKit::AcceleratedCompositingContext::renderLayersToWindow):
    * WebCoreSupport/ChromeClientGtk.cpp:
    (WebKit::ChromeClient::contentsSizeChanged):
    
    Canonical link: https://commits.webkit.org/135478@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@151198 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/gtk/WebCoreSupport/ChromeClientGtk.cpp
+++ Source/WebKit/gtk/WebCoreSupport/ChromeClientGtk.cpp
@@ -722,8 +722,8 @@ void ChromeClient::contentsSizeChanged(Frame* frame, const IntSize& size) const
     GtkRequisition requisition;
     gtk_widget_get_requisition(widget, &requisition);
     if (gtk_widget_get_realized(widget)
-        && (requisition.height != size.height())
-        || (requisition.width != size.width()))
+        && (requisition.height != size.height()
+        || requisition.width != size.width()))
         gtk_widget_queue_resize_no_redraw(widget);
 
     // If this was a main frame size change, update the scrollbars.
