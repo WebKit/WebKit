13b3b651622bdff2d4ba707d0e42e36d34425e33
Allan Sandfeld Jensen <allan.jensen@digia.com>
2013-06-06 17:10:09 +0000

    ENABLE(PAN_SCROLLING) AutoscrollController::updateAutoscrollRenderer calls hitTestResultAtPoint with `true` for HitTestRequestType https://bugs.webkit.org/show_bug.cgi?id=116896
    Reviewed by Andreas Kling.
    
    Update hitTestResultAtPoint call to new API.
    
    * page/AutoscrollController.cpp:
    (WebCore::AutoscrollController::updateAutoscrollRenderer):
    
    
    Canonical link: https://commits.webkit.org/135554@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@151281 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/AutoscrollController.cpp
+++ Source/WebCore/page/AutoscrollController.cpp
@@ -133,7 +133,7 @@ void AutoscrollController::updateAutoscrollRenderer()
     RenderObject* renderer = m_autoscrollRenderer;
 
 #if ENABLE(PAN_SCROLLING)
-    HitTestResult hitTest = m_autoscrollRenderer->frame()->eventHandler()->hitTestResultAtPoint(m_panScrollStartPos, true);
+    HitTestResult hitTest = m_autoscrollRenderer->frame()->eventHandler()->hitTestResultAtPoint(m_panScrollStartPos, HitTestRequest::ReadOnly | HitTestRequest::Active);
 
     if (Node* nodeAtPoint = hitTest.innerNode())
         renderer = nodeAtPoint->renderer();
