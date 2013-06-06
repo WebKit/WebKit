fb71184919a14798a2ea163d80d05684727e4e6c
Roger Fong <roger_fong@apple.com>
2013-06-06 21:56:47 +0000

    Stop copying AAS binaries into build folder. https://bugs.webkit.org/show_bug.cgi?id=117319.
    Rubberstamped by Darin Adler.
    
    * JavaScriptCore.vcxproj/JavaScriptCorePreLink.cmd:
    * JavaScriptCore.vcxproj/jsc/jscPostBuild.cmd:
    * JavaScriptCore.vcxproj/jsc/jscPreLink.cmd:
    * JavaScriptCore.vcxproj/testRegExp/testRegExpPostBuild.cmd:
    * JavaScriptCore.vcxproj/testRegExp/testRegExpPreLink.cmd:
    * JavaScriptCore.vcxproj/testapi/testapiPreLink.cmd:
    * DumpRenderTree/DumpRenderTree.vcxproj/DumpRenderTree/DumpRenderTreePostBuild.cmd:
    
    Canonical link: https://commits.webkit.org/135562@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@151292 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/JavaScriptCore.vcxproj/testRegExp/testRegExpPreLink.cmd
+++
@@ -1,9 +0,0 @@
-mkdir 2>NUL "%IntDir%\lib32"
-
-if exist "%WebKit_Libraries%\lib32\icuuc%DebugSuffix%.lib" copy /y "%WebKit_Libraries%\lib32\icuuc%DebugSuffix%.lib" "%IntDir%\lib32\libicuuc%DebugSuffix%.lib"
-if exist "%WebKit_Libraries%\lib32\icuin%DebugSuffix%.lib" copy /y "%WebKit_Libraries%\lib32\icuin%DebugSuffix%.lib" "%IntDir%\lib32\libicuin%DebugSuffix%.lib"
-
-if exist "%WebKit_Libraries%\lib32\libicuuc%DebugSuffix%.lib" copy /y "%WebKit_Libraries%\lib32\libicuuc%DebugSuffix%.lib" "%IntDir%\lib32"
-if exist "%WebKit_Libraries%\lib32\libicuin%DebugSuffix%.lib" copy /y "%WebKit_Libraries%\lib32\libicuin%DebugSuffix%.lib" "%IntDir%\lib32"
-
-cmd /c
