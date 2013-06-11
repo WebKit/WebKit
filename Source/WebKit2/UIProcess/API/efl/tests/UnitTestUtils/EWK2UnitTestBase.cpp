64fb5bbd33fcdc66c3a72e7ae5bf95b399d37f93
Changhyup Jwa <ch.jwa@samsung.com>
2013-06-11 12:12:31 +0000

    [EFL][WK2] Add fallback for EWK2UnitTestBase if opengl is not available. https://bugs.webkit.org/show_bug.cgi?id=117504
    Patch by Changhyup Jwa <ch.jwa@samsung.com> on 2013-06-11
    Reviewed by Christophe Dumez.
    
    There're many fails on efl webkit2 unit test if opengl is not available.
    So, add fallback to non-opengl evas engine.
    
    * UIProcess/API/efl/tests/UnitTestUtils/EWK2UnitTestBase.cpp:
    (EWK2UnitTest::EWK2UnitTestBase::SetUp):
    
    Canonical link: https://commits.webkit.org/135652@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@151433 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/efl/tests/UnitTestUtils/EWK2UnitTestBase.cpp
+++ Source/WebKit2/UIProcess/API/efl/tests/UnitTestUtils/EWK2UnitTestBase.cpp
@@ -50,9 +50,10 @@ void EWK2UnitTestBase::SetUp()
 #if defined(WTF_USE_ACCELERATED_COMPOSITING) && defined(HAVE_ECORE_X)
     const char* engine = "opengl_x11";
     m_ecoreEvas = ecore_evas_new(engine, 0, 0, width, height, 0);
-#else
-    m_ecoreEvas = ecore_evas_new(0, 0, 0, width, height, 0);
+    // Graceful fallback to software rendering if evas_gl engine is not available.
+    if (!m_ecoreEvas)
 #endif
+    m_ecoreEvas = ecore_evas_new(0, 0, 0, width, height, 0);
 
     ecore_evas_show(m_ecoreEvas);
     Evas* evas = ecore_evas_get(m_ecoreEvas);
