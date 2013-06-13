b26989e3e1ecda95180f4138f66f6a23c67ced93
Mihai Tica <mitica@adobe.com>
2013-06-13 18:24:21 +0000

    Updating the -webkit-background-blend-mode property dynamically does not trigger a redraw of the element.
    Source/WebCore:
    
    Adding krit's fix: actual blending values should be compared, not the sets.
    https://bugs.webkit.org/show_bug.cgi?id=117223
    
    Patch by Mihai Tica <mitica@adobe.com> on 2013-06-13
    Reviewed by Dirk Schulze.
    
    Test: css3/compositing/background-blend-mode-image-color-dynamic.html
    
    * rendering/style/FillLayer.cpp:
    (WebCore::FillLayer::operator==):
    
    LayoutTests:
    
    Adding test that sets background blend modes from script, after the page is loaded.
    https://bugs.webkit.org/show_bug.cgi?id=117223
    
    Patch by Mihai Tica <mitica@adobe.com> on 2013-06-13
    Reviewed by Dirk Schulze.
    
    * fast/repaint/background-blend-mode-image-color-dynamic-expected.html: Added.
    * fast/repaint/background-blend-mode-image-color-dynamic.html: Added.
    
    Canonical link: https://commits.webkit.org/135769@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@151565 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/style/FillLayer.cpp
+++ Source/WebCore/rendering/style/FillLayer.cpp
@@ -153,7 +153,7 @@ bool FillLayer::operator==(const FillLayer& o) const
     return StyleImage::imagesEquivalent(m_image.get(), o.m_image.get()) && m_xPosition == o.m_xPosition && m_yPosition == o.m_yPosition
             && m_backgroundXOrigin == o.m_backgroundXOrigin && m_backgroundYOrigin == o.m_backgroundYOrigin
             && m_attachment == o.m_attachment && m_clip == o.m_clip && m_composite == o.m_composite
-            && m_blendModeSet == o.m_blendModeSet && m_origin == o.m_origin && m_repeatX == o.m_repeatX
+            && m_blendMode == o.m_blendMode && m_origin == o.m_origin && m_repeatX == o.m_repeatX
             && m_repeatY == o.m_repeatY && m_sizeType == o.m_sizeType && m_sizeLength == o.m_sizeLength
             && m_type == o.m_type && ((m_next && o.m_next) ? *m_next == *o.m_next : m_next == o.m_next);
 }
