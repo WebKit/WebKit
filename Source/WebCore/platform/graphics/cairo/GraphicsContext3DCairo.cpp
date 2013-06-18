23d03876e323fe9c2fed0c7337e6f21727cac4aa
Mario Sanchez Prada <mario@webkit.org>
2013-06-18 08:35:20 +0000

    Shader compiler not properly configured for GLES on cairo based ports https://bugs.webkit.org/show_bug.cgi?id=117705
    Reviewed by Martin Robinson.
    
    Configure the shader compiler with SH_ESSL_OUTPUT for GLESv2
    compliant platforms, and with SH_GLSL_OUTPUT otherwise.
    
    * platform/graphics/cairo/GraphicsContext3DCairo.cpp:
    (WebCore::GraphicsContext3D::GraphicsContext3D):
    
    Canonical link: https://commits.webkit.org/135869@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@151676 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/cairo/GraphicsContext3DCairo.cpp
+++ Source/WebCore/platform/graphics/cairo/GraphicsContext3DCairo.cpp
@@ -75,6 +75,7 @@ PassRefPtr<GraphicsContext3D> GraphicsContext3D::create(GraphicsContext3D::Attri
 GraphicsContext3D::GraphicsContext3D(GraphicsContext3D::Attributes attributes, HostWindow*, GraphicsContext3D::RenderStyle renderStyle)
     : m_currentWidth(0)
     , m_currentHeight(0)
+    , m_compiler(isGLES2Compliant() ? SH_ESSL_OUTPUT : SH_GLSL_OUTPUT)
     , m_attrs(attributes)
     , m_texture(0)
     , m_fbo(0)
