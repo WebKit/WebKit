aaa80a0e72832ffd7e15bbc7870aef408103efae
Brent Fulgham <bfulgham@apple.com>
2013-06-19 20:51:36 +0000

    [Windows] Unreviewed build correction.
    * WebCoreSupport/WebFrameNetworkingContext.cpp:
    (WebFrameNetworkingContext::ensurePrivateBrowsingSession): Make sure returned generic
    type is a CFStringRef before casting.
    
    
    
    Canonical link: https://commits.webkit.org/135939@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@151752 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/win/WebCoreSupport/WebFrameNetworkingContext.cpp
+++ Source/WebKit/win/WebCoreSupport/WebFrameNetworkingContext.cpp
@@ -74,7 +74,11 @@ void WebFrameNetworkingContext::ensurePrivateBrowsingSession()
 
     String base;
     if (!identifierBase)
-        base = (CFStringRef)CFBundleGetValueForInfoDictionaryKey(CFBundleGetMainBundle(), kCFBundleIdentifierKey);
+    {
+        if (CFTypeRef bundleValue = CFBundleGetValueForInfoDictionaryKey(CFBundleGetMainBundle(), kCFBundleIdentifierKey))
+            if (CFGetTypeID(bundleValue) == CFStringGetTypeID())
+                base = reinterpret_cast<CFStringRef>(bundleValue);
+    }
     else
         base = *identifierBase;
 
