09c9a6bea17e7ab5d1d1a8eac3d3af8c22924004
Ruth Fong <ruth_fong@apple.com>
2013-06-27 18:13:47 +0000

    Polish context menus for media elements https://bugs.webkit.org/show_bug.cgi?id=118078 <rdar://problem/14278960>
    Patch by Ruth Fong <ruth_fong@apple.com> on 2013-06-27
    Reviewed by Eric Carlson.
    
    No new tests: media/context-menu-action.html,
    which has been disabled by bug 116651, is used to test context menus.
    
    * English.lproj/Localizable.strings: Changed "Fullscreen" to "Full Screen"
    and added strings "Show Controls" and "Hide Controls".
    
    * page/ContextMenuController.cpp: Updated to include a new context menu item
    that toggles betweeen "Show Controls" and "Hide Controls".
    
    * platform/ContextMenuItem.h:
    
    * platform/LocalizedStrings.cpp:
    * platform/LocalizedStrings.h:
    * platform/efl/LocalizedStringsEfl.cpp:
    * platform/gtk/LocalizedStringsGtk.cpp:
    * platform/qt/LocalizedStringsQt.cpp:
    Added equivalents of localized strings "Show Controls" and "Hide Controls".
    
    Canonical link: https://commits.webkit.org/136187@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@152117 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/efl/LocalizedStringsEfl.cpp
+++ Source/WebCore/platform/efl/LocalizedStringsEfl.cpp
@@ -153,6 +153,16 @@ String contextMenuItemTagToggleMediaControls()
     return String::fromUTF8("Toggle Media Controls");
 }
 
+String contextMenuItemTagShowMediaControls()
+{
+    return String::fromUTF8("Show Media Controls");
+}
+
+String contextMenuitemTagHideMediaControls()
+{
+    return String::fromUTF8("Hide Media Controls");
+}
+
 String contextMenuItemTagToggleMediaLoop()
 {
     return String::fromUTF8("Toggle Media Loop Playback");
