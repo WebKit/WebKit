37eb58dcb228a84dffbcc73eb6e41c15222ab32d
Brady Eidson <beidson@apple.com>
2013-07-10 18:34:19 +0000

    Some Java plugin instances can deadlock with the WebProcess on NPP_Destroy. <rdar://problem/14286390> and https://bugs.webkit.org/show_bug.cgi?id=118535
    Reviewed by Alexey Proskuryakov.
    
    * Shared/Plugins/NPObjectProxy.cpp:
    (WebKit::NPObjectProxy::~NPObjectProxy): ASSERT this is the main thread.
    (WebKit::NPObjectProxy::NP_Deallocate): If this isn't the main thread, call it again on the main thread.
    
    Canonical link: https://commits.webkit.org/136492@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@152539 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/Shared/Plugins/NPObjectProxy.cpp
+++ Source/WebKit2/Shared/Plugins/NPObjectProxy.cpp
@@ -35,6 +35,10 @@
 #include "NPRemoteObjectMap.h"
 #include "NPRuntimeUtilities.h"
 #include "NPVariantData.h"
+#include <WebCore/RunLoop.h>
+#include <wtf/MainThread.h>
+
+using namespace WebCore;
 
 namespace WebKit {
 
@@ -55,6 +59,8 @@ NPObjectProxy::NPObjectProxy()
 
 NPObjectProxy::~NPObjectProxy()
 {
+    ASSERT(isMainThread());
+
     if (!m_npRemoteObjectMap)
         return;
 
@@ -293,6 +299,16 @@ NPObject* NPObjectProxy::NP_Allocate(NPP npp, NPClass*)
 
 void NPObjectProxy::NP_Deallocate(NPObject* npObject)
 {
+    // http://webkit.org/b/118535 - The Java Netscape Plug-in has a background thread do some of their NPP_Destroy work.
+    // That background thread can call NP_Deallocate, and this leads to a WebProcess <-> PluginProcess deadlock.
+    // Since NPAPI behavior on a background thread is undefined, it is okay to limit this workaround to the one API
+    // that is known to be misused during plugin teardown, and to not be concerned about change in behavior if this
+    // occured at any other time.
+    if (!isMainThread()) {
+        RunLoop::main()->dispatch(bind(&NPObjectProxy::NP_Deallocate, npObject));
+        return;
+    }
+    
     NPObjectProxy* npObjectProxy = toNPObjectProxy(npObject);
     delete npObjectProxy;
 }
