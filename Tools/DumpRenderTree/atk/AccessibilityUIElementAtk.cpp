99165d6d6b2b977de7f1b2d832c1cc8a14db454e
Brian Holt <brian.holt@samsung.com>
2013-07-15 16:55:19 +0000

    Merge r152298 - [ATK] Leak: leaks in AccessibilityUIElement https://bugs.webkit.org/show_bug.cgi?id=118297
    Fixed memory leak.
    
    Patch by Brian Holt <brian.holt@samsung.com> on 2013-07-02
    Reviewed by Christophe Dumez.
    
    * DumpRenderTree/atk/AccessibilityUIElementAtk.cpp:
    (AccessibilityUIElement::stringValue):

--- Tools/DumpRenderTree/atk/AccessibilityUIElementAtk.cpp
+++ Tools/DumpRenderTree/atk/AccessibilityUIElementAtk.cpp
@@ -411,9 +411,9 @@ JSStringRef AccessibilityUIElement::stringValue()
     if (!m_element || !ATK_IS_TEXT(m_element))
         return JSStringCreateWithCharacters(0, 0);
 
-    gchar* text = atk_text_get_text(ATK_TEXT(m_element), 0, -1);
-    GOwnPtr<gchar> axValue(g_strdup_printf("AXValue: %s", replaceCharactersForResults(text)));
-    g_free(text);
+    GOwnPtr<gchar> text(atk_text_get_text(ATK_TEXT(m_element), 0, -1));
+    GOwnPtr<gchar> textWithReplacedCharacters(replaceCharactersForResults(text.get()));
+    GOwnPtr<gchar> axValue(g_strdup_printf("AXValue: %s", textWithReplacedCharacters.get()));
 
     return JSStringCreateWithUTF8CString(axValue.get());
 }
