00420be8e9766b1b084f8a566b65c4c333b96acc
Dong-Gwan Kim <donggwan.kim@samsung.com>
2013-07-17 07:22:43 +0000

    [EFL][WK2] EWK2PopupMenuTest should be defined by inheriting from EWK2UnitTestBase. https://bugs.webkit.org/show_bug.cgi?id=118775
    Patch by Dong-Gwan Kim <donggwan.kim@samsung.com> on 2013-07-17
    Reviewed by Christophe Dumez.
    
    It should be defined as relevant test class specific to each test file for more readability.
    It could be helpful to remove unnecessary static methods.
    
    * UIProcess/API/efl/tests/test_ewk2_popup_menu.cpp:
    (EWK2PopupMenuTest::checkBasicPopupMenuItem):
    (EWK2PopupMenuTest::selectItemAfterDelayed):
    (EWK2PopupMenuTest::showPopupMenu):
    (TEST_F):
    
    Canonical link: https://commits.webkit.org/136649@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@152769 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/efl/tests/test_ewk2_popup_menu.cpp
+++ Source/WebKit2/UIProcess/API/efl/tests/test_ewk2_popup_menu.cpp
@@ -27,62 +27,65 @@ extern EWK2UnitTestEnvironment* environment;
 
 static Ewk_Popup_Menu* s_popupMenu = 0;
 
-static inline void checkBasicPopupMenuItem(Ewk_Popup_Menu_Item* item, const char* title, bool enabled)
-{
-    EXPECT_EQ(EWK_POPUP_MENU_ITEM, ewk_popup_menu_item_type_get(item));
-    EXPECT_STREQ(title, ewk_popup_menu_item_text_get(item));
-    EXPECT_EQ(enabled, ewk_popup_menu_item_enabled_get(item));
-}
+class EWK2PopupMenuTest : public EWK2UnitTestBase {
+public:
+    static void checkBasicPopupMenuItem(Ewk_Popup_Menu_Item* item, const char* title, bool enabled)
+    {
+        EXPECT_EQ(EWK_POPUP_MENU_ITEM, ewk_popup_menu_item_type_get(item));
+        EXPECT_STREQ(title, ewk_popup_menu_item_text_get(item));
+        EXPECT_EQ(enabled, ewk_popup_menu_item_enabled_get(item));
+    }
 
-static Eina_Bool selectItemAfterDelayed(void* data)
-{
-    EXPECT_TRUE(ewk_popup_menu_selected_index_set(static_cast<Ewk_Popup_Menu*>(data), 0));
-    return ECORE_CALLBACK_CANCEL;
-}
+    static Eina_Bool selectItemAfterDelayed(void* data)
+    {
+        EXPECT_TRUE(ewk_popup_menu_selected_index_set(static_cast<Ewk_Popup_Menu*>(data), 0));
+        return ECORE_CALLBACK_CANCEL;
+    }
 
-static Eina_Bool showPopupMenu(Ewk_View_Smart_Data* smartData, Eina_Rectangle, Ewk_Text_Direction, double, Ewk_Popup_Menu* popupMenu)
-{
-    s_popupMenu = popupMenu;
+    static Eina_Bool showPopupMenu(Ewk_View_Smart_Data* smartData, Eina_Rectangle, Ewk_Text_Direction, double, Ewk_Popup_Menu* popupMenu)
+    {
+        s_popupMenu = popupMenu;
 
-    EXPECT_EQ(2, ewk_popup_menu_selected_index_get(popupMenu));
+        EXPECT_EQ(2, ewk_popup_menu_selected_index_get(popupMenu));
 
-    const Eina_List* list = ewk_popup_menu_items_get(popupMenu);
+        const Eina_List* list = ewk_popup_menu_items_get(popupMenu);
 
-    Ewk_Popup_Menu_Item* item = static_cast<Ewk_Popup_Menu_Item*>(eina_list_nth(list, 0));
-    checkBasicPopupMenuItem(item, "first", true);
-    EXPECT_EQ(EWK_TEXT_DIRECTION_LEFT_TO_RIGHT, ewk_popup_menu_item_text_direction_get(item));
-    EXPECT_STREQ("", ewk_popup_menu_item_tooltip_get(item));
-    EXPECT_STREQ("", ewk_popup_menu_item_accessibility_text_get(item));
-    EXPECT_FALSE(ewk_popup_menu_item_is_label_get(item));
-    EXPECT_FALSE(ewk_popup_menu_item_selected_get(item));
+        Ewk_Popup_Menu_Item* item = static_cast<Ewk_Popup_Menu_Item*>(eina_list_nth(list, 0));
+        checkBasicPopupMenuItem(item, "first", true);
+        EXPECT_EQ(EWK_TEXT_DIRECTION_LEFT_TO_RIGHT, ewk_popup_menu_item_text_direction_get(item));
+        EXPECT_STREQ("", ewk_popup_menu_item_tooltip_get(item));
+        EXPECT_STREQ("", ewk_popup_menu_item_accessibility_text_get(item));
+        EXPECT_FALSE(ewk_popup_menu_item_is_label_get(item));
+        EXPECT_FALSE(ewk_popup_menu_item_selected_get(item));
 
-    item = static_cast<Ewk_Popup_Menu_Item*>(eina_list_nth(list, 1));
-    checkBasicPopupMenuItem(item, "second", false);
-    EXPECT_FALSE(ewk_popup_menu_item_enabled_get(item));
+        item = static_cast<Ewk_Popup_Menu_Item*>(eina_list_nth(list, 1));
+        checkBasicPopupMenuItem(item, "second", false);
+        EXPECT_FALSE(ewk_popup_menu_item_enabled_get(item));
 
-    item = static_cast<Ewk_Popup_Menu_Item*>(eina_list_nth(list, 2));
-    checkBasicPopupMenuItem(item, "third", true);
-    EXPECT_EQ(EWK_TEXT_DIRECTION_RIGHT_TO_LEFT, ewk_popup_menu_item_text_direction_get(item));
-    EXPECT_STREQ("tooltip", ewk_popup_menu_item_tooltip_get(item));
-    EXPECT_STREQ("aria", ewk_popup_menu_item_accessibility_text_get(item));
-    EXPECT_TRUE(ewk_popup_menu_item_selected_get(item));
+        item = static_cast<Ewk_Popup_Menu_Item*>(eina_list_nth(list, 2));
+        checkBasicPopupMenuItem(item, "third", true);
+        EXPECT_EQ(EWK_TEXT_DIRECTION_RIGHT_TO_LEFT, ewk_popup_menu_item_text_direction_get(item));
+        EXPECT_STREQ("tooltip", ewk_popup_menu_item_tooltip_get(item));
+        EXPECT_STREQ("aria", ewk_popup_menu_item_accessibility_text_get(item));
+        EXPECT_TRUE(ewk_popup_menu_item_selected_get(item));
 
-    item = static_cast<Ewk_Popup_Menu_Item*>(eina_list_nth(list, 3));
-    checkBasicPopupMenuItem(item, "label", false);
-    EXPECT_TRUE(ewk_popup_menu_item_is_label_get(item));
+        item = static_cast<Ewk_Popup_Menu_Item*>(eina_list_nth(list, 3));
+        checkBasicPopupMenuItem(item, "label", false);
+        EXPECT_TRUE(ewk_popup_menu_item_is_label_get(item));
 
-    item = static_cast<Ewk_Popup_Menu_Item*>(eina_list_nth(list, 4));
-    checkBasicPopupMenuItem(item, "    forth", true);
+        item = static_cast<Ewk_Popup_Menu_Item*>(eina_list_nth(list, 4));
+        checkBasicPopupMenuItem(item, "    forth", true);
 
-    item = static_cast<Ewk_Popup_Menu_Item*>(eina_list_nth(list, 5));
-    EXPECT_EQ(EWK_POPUP_MENU_UNKNOWN, ewk_popup_menu_item_type_get(item));
-    EXPECT_STREQ(0, ewk_popup_menu_item_text_get(item));
+        item = static_cast<Ewk_Popup_Menu_Item*>(eina_list_nth(list, 5));
+        EXPECT_EQ(EWK_POPUP_MENU_UNKNOWN, ewk_popup_menu_item_type_get(item));
+        EXPECT_STREQ(0, ewk_popup_menu_item_text_get(item));
 
-    ecore_timer_add(0, selectItemAfterDelayed, popupMenu);
-    return true;
-}
+        ecore_timer_add(0, selectItemAfterDelayed, popupMenu);
+        return true;
+    }
+};
 
-TEST_F(EWK2UnitTestBase, ewk_popup_menu_select_item)
+TEST_F(EWK2PopupMenuTest, ewk_popup_menu_select_item)
 {
     const char* selectHTML =
         "<!doctype html><body><select onchange=\"document.title=this.value;\">"
