89e29e89b6023bdd1e160cdfc4c18cf41e8a6317
Dong-Gwan Kim <donggwan.kim@samsung.com>
2013-07-17 07:06:18 +0000

    [EFL][WK2] EWK2ContextTest should be defined by inheriting from EWK2UnitTestBase. https://bugs.webkit.org/show_bug.cgi?id=118763
    Patch by Dong-Gwan Kim <donggwan.kim@samsung.com> on 2013-07-17
    Reviewed by Christophe Dumez.
    
    It should be defined as relevant test class specific to each test file for more readability.
    It could be helpful to remove unnecessary static methods.
    
    * UIProcess/API/efl/tests/test_ewk2_context.cpp:
    (EWK2ContextTest::schemeRequestCallback):
    (TEST_F):
    
    Canonical link: https://commits.webkit.org/136640@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@152760 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/efl/tests/test_ewk2_context.cpp
+++ Source/WebKit2/UIProcess/API/efl/tests/test_ewk2_context.cpp
@@ -34,14 +34,28 @@ extern EWK2UnitTestEnvironment* environment;
 
 static const char htmlReply[] = "<html><head><title>Foo</title></head><body>Bar</body></html>";
 
-TEST_F(EWK2UnitTestBase, ewk_context_default_get)
+class EWK2ContextTest : public EWK2UnitTestBase {
+public:
+    static void schemeRequestCallback(Ewk_Url_Scheme_Request* request, void* userData)
+    {
+        const char* scheme = ewk_url_scheme_request_scheme_get(request);
+        ASSERT_STREQ("fooscheme", scheme);
+        const char* url = ewk_url_scheme_request_url_get(request);
+        ASSERT_STREQ("fooscheme:MyPath", url);
+        const char* path = ewk_url_scheme_request_path_get(request);
+        ASSERT_STREQ("MyPath", path);
+        ASSERT_TRUE(ewk_url_scheme_request_finish(request, htmlReply, strlen(htmlReply), "text/html"));
+    }
+};
+
+TEST_F(EWK2ContextTest, ewk_context_default_get)
 {
     Ewk_Context* defaultContext = ewk_context_default_get();
     ASSERT_TRUE(defaultContext);
     ASSERT_EQ(defaultContext, ewk_context_default_get());
 }
 
-TEST_F(EWK2UnitTestBase, ewk_context_cookie_manager_get)
+TEST_F(EWK2ContextTest, ewk_context_cookie_manager_get)
 {
     Ewk_Context* context = ewk_view_context_get(webView());
     Ewk_Cookie_Manager* cookieManager = ewk_context_cookie_manager_get(context);
@@ -49,7 +63,7 @@ TEST_F(EWK2UnitTestBase, ewk_context_cookie_manager_get)
     ASSERT_EQ(cookieManager, ewk_context_cookie_manager_get(context));
 }
 
-TEST_F(EWK2UnitTestBase, ewk_context_database_manager_get)
+TEST_F(EWK2ContextTest, ewk_context_database_manager_get)
 {
     Ewk_Context* context = ewk_view_context_get(webView());
     Ewk_Database_Manager* databaseManager = ewk_context_database_manager_get(context);
@@ -57,7 +71,7 @@ TEST_F(EWK2UnitTestBase, ewk_context_database_manager_get)
     ASSERT_EQ(databaseManager, ewk_context_database_manager_get(context));
 }
 
-TEST_F(EWK2UnitTestBase, ewk_context_favicon_database_get)
+TEST_F(EWK2ContextTest, ewk_context_favicon_database_get)
 {
     Ewk_Context* context = ewk_view_context_get(webView());
     Ewk_Favicon_Database* faviconDatabase = ewk_context_favicon_database_get(context);
@@ -65,7 +79,7 @@ TEST_F(EWK2UnitTestBase, ewk_context_favicon_database_get)
     ASSERT_EQ(faviconDatabase, ewk_context_favicon_database_get(context));
 }
 
-TEST_F(EWK2UnitTestBase, ewk_context_storage_manager_get)
+TEST_F(EWK2ContextTest, ewk_context_storage_manager_get)
 {
     Ewk_Context* context = ewk_view_context_get(webView());
     Ewk_Storage_Manager* storageManager = ewk_context_storage_manager_get(context);
@@ -73,25 +87,14 @@ TEST_F(EWK2UnitTestBase, ewk_context_storage_manager_get)
     ASSERT_EQ(storageManager, ewk_context_storage_manager_get(context));
 }
 
-static void schemeRequestCallback(Ewk_Url_Scheme_Request* request, void* userData)
-{
-    const char* scheme = ewk_url_scheme_request_scheme_get(request);
-    ASSERT_STREQ("fooscheme", scheme);
-    const char* url = ewk_url_scheme_request_url_get(request);
-    ASSERT_STREQ("fooscheme:MyPath", url);
-    const char* path = ewk_url_scheme_request_path_get(request);
-    ASSERT_STREQ("MyPath", path);
-    ASSERT_TRUE(ewk_url_scheme_request_finish(request, htmlReply, strlen(htmlReply), "text/html"));
-}
-
-TEST_F(EWK2UnitTestBase, ewk_context_url_scheme_register)
+TEST_F(EWK2ContextTest, ewk_context_url_scheme_register)
 {
     ewk_context_url_scheme_register(ewk_view_context_get(webView()), "fooscheme", schemeRequestCallback, 0);
     ASSERT_TRUE(loadUrlSync("fooscheme:MyPath"));
     ASSERT_STREQ("Foo", ewk_view_title_get(webView()));
 }
 
-TEST_F(EWK2UnitTestBase, ewk_context_cache_model)
+TEST_F(EWK2ContextTest, ewk_context_cache_model)
 {
     Ewk_Context* context = ewk_view_context_get(webView());
 
@@ -107,21 +110,21 @@ TEST_F(EWK2UnitTestBase, ewk_context_cache_model)
     ASSERT_EQ(EWK_CACHE_MODEL_DOCUMENT_VIEWER, ewk_context_cache_model_get(context));
 }
 
-TEST_F(EWK2UnitTestBase, ewk_context_new)
+TEST_F(EWK2ContextTest, ewk_context_new)
 {
     Ewk_Context* context = ewk_context_new();
     ASSERT_TRUE(context);
     ewk_object_unref(context);
 }
 
-TEST_F(EWK2UnitTestBase, ewk_context_new_with_injected_bundle_path)
+TEST_F(EWK2ContextTest, ewk_context_new_with_injected_bundle_path)
 {
     Ewk_Context* context = ewk_context_new_with_injected_bundle_path(environment->injectedBundleSample());
     ASSERT_TRUE(context);
     ewk_object_unref(context);
 }
 
-TEST_F(EWK2UnitTestBase, ewk_context_additional_plugin_path_set)
+TEST_F(EWK2ContextTest, ewk_context_additional_plugin_path_set)
 {
     Ewk_Context* context = ewk_view_context_get(webView());
 
