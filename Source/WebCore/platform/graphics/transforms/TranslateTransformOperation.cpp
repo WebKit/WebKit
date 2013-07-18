3228235bb0c02af58693a87fda046418112068f4
Jacky Jiang <zhajiang@blackberry.com>
2013-07-18 01:02:56 +0000

    Dereference null pointer crash in Length::decrementCalculatedRef() https://bugs.webkit.org/show_bug.cgi?id=118686
    Patch by Jacky Jiang <zhajiang@blackberry.com> on 2013-07-17
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    Length(Calculated) won't insert any CalculationValue to CalculationValueHandleMap;
    therefore, we dereference null CalculationValue pointer when the temporary
    Length object goes out of the scope.
    Length(Calculated) is not allowed as it doesn't make sense that we construct
    a Calculated Length object with uninitialized calc expression.
    The code just wants to blend with zero. To fix the bug, we can just blend
    with Length(0, Fixed) here as we currently can blend different type units
    and zero has the same behavior regardless of unit.
    
    Test: transitions/transition-transform-translate-calculated-length-crash.html
    
    * platform/graphics/transforms/TranslateTransformOperation.cpp:
    (WebCore::TranslateTransformOperation::blend):
    
    LayoutTests:
    
    * transitions/transition-transform-translate-calculated-length-crash-expected.txt: Added.
    * transitions/transition-transform-translate-calculated-length-crash.html: Added.
    
    Canonical link: https://commits.webkit.org/136695@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@152825 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/transforms/TranslateTransformOperation.cpp
+++ Source/WebCore/platform/graphics/transforms/TranslateTransformOperation.cpp
@@ -29,16 +29,15 @@ PassRefPtr<TransformOperation> TranslateTransformOperation::blend(const Transfor
 {
     if (from && !from->isSameType(*this))
         return this;
-    
+
+    Length zeroLength(0, Fixed);
     if (blendToIdentity)
-        return TranslateTransformOperation::create(Length(m_x.type()).blend(m_x, progress), 
-                                                   Length(m_y.type()).blend(m_y, progress), 
-                                                   Length(m_z.type()).blend(m_z, progress), m_type);
+        return TranslateTransformOperation::create(zeroLength.blend(m_x, progress), zeroLength.blend(m_y, progress), zeroLength.blend(m_z, progress), m_type);
 
     const TranslateTransformOperation* fromOp = static_cast<const TranslateTransformOperation*>(from);
-    Length fromX = fromOp ? fromOp->m_x : Length(m_x.type());
-    Length fromY = fromOp ? fromOp->m_y : Length(m_y.type());
-    Length fromZ = fromOp ? fromOp->m_z : Length(m_z.type());
+    Length fromX = fromOp ? fromOp->m_x : zeroLength;
+    Length fromY = fromOp ? fromOp->m_y : zeroLength;
+    Length fromZ = fromOp ? fromOp->m_z : zeroLength;
     return TranslateTransformOperation::create(m_x.blend(fromX, progress), m_y.blend(fromY, progress), m_z.blend(fromZ, progress), m_type);
 }
 
