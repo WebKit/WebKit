56124a393ec87d6dfb53ad786824ad3f5e47101a
Dean Jackson <dino@apple.com>
2013-07-22 20:54:46 +0000

    PlugIn content can disappear after restarting https://bugs.webkit.org/show_bug.cgi?id=118982
    Reviewed by Simon Fraser.
    
    When a snapshotted plug-in is restarted, we inserted its compositing
    layer back into the tree, but didn't recalculate style. This meant
    that a subsequent compositing tree operation (such as any hardware
    animation) could cause the content to disappear.
    
    * html/HTMLPlugInImageElement.cpp:
    (WebCore::HTMLPlugInImageElement::setDisplayState): Force a style recalc.
    (WebCore::HTMLPlugInImageElement::removeSnapshotTimerFired): Ditto.
    
    Canonical link: https://commits.webkit.org/136819@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@152989 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLPlugInImageElement.cpp
+++ Source/WebCore/html/HTMLPlugInImageElement.cpp
@@ -130,6 +130,7 @@ void HTMLPlugInImageElement::setDisplayState(DisplayState state)
     if (state == RestartingWithPendingMouseClick || state == Restarting) {
         m_isRestartedPlugin = true;
         m_snapshotDecision = NeverSnapshot;
+        setNeedsStyleRecalc(SyntheticStyleChange);
         if (displayState() == DisplayingSnapshot)
             m_removeSnapshotTimer.startOneShot(removeSnapshotTimerDelay);
     }
@@ -423,6 +424,7 @@ void HTMLPlugInImageElement::removeSnapshotTimerFired(Timer<HTMLPlugInImageEleme
 {
     m_snapshotImage = nullptr;
     m_isRestartedPlugin = false;
+    setNeedsStyleRecalc(SyntheticStyleChange);
     if (renderer())
         renderer()->repaint();
 }
