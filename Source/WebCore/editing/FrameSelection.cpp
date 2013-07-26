4f13c7364565163e4d56e805f326bb95f14f534a
Ryosuke Niwa <rniwa@webkit.org>
2013-07-26 03:59:22 +0000

    Fix document leak when selection is created inside the document https://bugs.webkit.org/show_bug.cgi?id=119122
    Reviewed by Andreas Kling.
    
    Source/WebCore:
    
    Merge https://chromium.googlesource.com/chromium/blink/+/b908cb4c8da93316d787de31c93f2a43de332a10
    
    The bug was caused by FrameSelection::m_previousCaretNode holding onto a Node, leaking its document.
    Fixed the bug by explicitly clearing it in FrameSelection::prepareForDestruction.
    
    Test: editing/selection/leak-document-with-selection-inside.html
    
    * editing/FrameSelection.cpp:
    (WebCore::FrameSelection::prepareForDestruction):
    
    LayoutTests:
    
    Added a regression test.
    
    * editing/selection/leak-document-with-selection-inside-expected.txt: Added.
    * editing/selection/leak-document-with-selection-inside.html: Added.
    
    
    Canonical link: https://commits.webkit.org/137149@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@153366 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/editing/FrameSelection.cpp
+++ Source/WebCore/editing/FrameSelection.cpp
@@ -1209,6 +1209,7 @@ void FrameSelection::prepareForDestruction()
         view->clearSelection();
 
     setSelection(VisibleSelection(), CloseTyping | ClearTypingStyle | DoNotUpdateAppearance);
+    m_previousCaretNode.clear();
 }
 
 void FrameSelection::setStart(const VisiblePosition &pos, EUserTriggered trigger)
