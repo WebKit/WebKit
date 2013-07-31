d0838eee48f55df2c8e42abe8d2d37dfcc2f0f4e
Chris Fleizach <cfleizach@apple.com>
2013-07-31 01:50:16 +0000

    Don't post accessibility notification on object in detached document. https://bugs.webkit.org/show_bug.cgi?id=119286
    Reviewed by Ryosuke Niwa.
    
    Merge https://chromium.googlesource.com/chromium/blink/+/ef9fc9e70202dcf33e5cf2f0f0a2135945ffe17e%5E%21/#F0
    
    Don't post accessibility notification on object in detached document.
    
    This can happen if an accessibility notification is queued on a node,
    then that node is reparented to a different document that's not attached
    anywhere before the accessibility notification is fired.
    
    * accessibility/AXObjectCache.cpp:
    (WebCore::AXObjectCache::notificationPostTimerFired):
    
    
    
    Canonical link: https://commits.webkit.org/137268@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@153507 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/accessibility/AXObjectCache.cpp
+++ Source/WebCore/accessibility/AXObjectCache.cpp
@@ -640,6 +640,9 @@ void AXObjectCache::notificationPostTimerFired(Timer<AXObjectCache>*)
         if (!obj->axObjectID())
             continue;
 
+        if (!obj->axObjectCache())
+            continue;
+        
 #ifndef NDEBUG
         // Make sure none of the render views are in the process of being layed out.
         // Notifications should only be sent after the renderer has finished
