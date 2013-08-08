15b811fdd8e2d41caaa8bbca4f31e67a52fe16b8
Sam Weinig <weinig@apple.com>
2013-08-08 22:01:05 +0000

    Hashing SecurityOrigin's can lead to trouble if you mutate them :( https://bugs.webkit.org/show_bug.cgi?id=119533 <rdar://problem/12978338>
    Reviewed by Andreas Kling.
    
    We are getting into trouble in the following circumstance:
    - You have a HashMap<RefPtr<SecurityOrigin>, Value> map.
    - You add security origin A (http, www.webkit.org, 80) to the map.
    - You mutate security origin A by domain relaxation, so that it has the domain webkit.org.
    - You add security origin B (http, www.webkit.org, 80) to the map.
    - You mutate security origin B by domain relaxation, so that it has the domain webkit.org.
        You now have two identical keys in the map.
    - Add few more items to the map causing a rehash.
    - When you try to add A and B back into the map, you will have a collision, because they now equal each other.
    
    We should probably stop using SecurityOrigins as keys in HashMaps (and move to using a non-mutable SecurityOriginTuple,
    or something), but for now, we can just only use the scheme / host / port part for equality, which is what all the users
    really want.
    
    * page/SecurityOriginHash.h:
    (WebCore::SecurityOriginHash::equal):
    Switch to using isSameSchemeHostPort() for SecurityOriginHash::equal().
    
    
    Canonical link: https://commits.webkit.org/137556@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@153850 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/SecurityOriginHash.h
+++ Source/WebCore/page/SecurityOriginHash.h
@@ -52,13 +52,9 @@ struct SecurityOriginHash {
 
     static bool equal(SecurityOrigin* a, SecurityOrigin* b)
     {
-        // FIXME: The hash function above compares three specific fields.
-        // This code to compare those three specific fields should be moved here from
-        // SecurityOrigin as mentioned in SecurityOrigin.h so we don't accidentally change
-        // equal without changing hash to match it.
         if (!a || !b)
             return a == b;
-        return a->equal(b);
+        return a->isSameSchemeHostPort(b);
     }
     static bool equal(SecurityOrigin* a, const RefPtr<SecurityOrigin>& b)
     {
