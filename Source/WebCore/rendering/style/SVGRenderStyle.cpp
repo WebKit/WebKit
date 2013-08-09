fb89993995f54cca13017d035f92654c9977c86e
Rob Buis <rwlbuis@webkit.org>
2013-08-09 23:30:32 +0000

    ASSERTION FAILED: stroke->opacity != other->stroke->opacity in WebCore::SVGRenderStyle::diff https://bugs.webkit.org/show_bug.cgi?id=119623
    Reviewed by Dirk Schulze.
    
    Source/WebCore:
    
    Include all the stroke attributes in the style diff comparison, the visited links ones were missing.
    
    Test: svg/animations/animate-stroke-crasher.html
    
    * rendering/style/SVGRenderStyle.cpp:
    (WebCore::SVGRenderStyle::diff):
    
    LayoutTests:
    
    Add testcase from bug with small adjustments.
    
    * svg/animations/animate-stroke-crasher-expected.txt: Added.
    * svg/animations/animate-stroke-crasher.html: Added.
    
    Canonical link: https://commits.webkit.org/137607@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@153914 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/style/SVGRenderStyle.cpp
+++ Source/WebCore/rendering/style/SVGRenderStyle.cpp
@@ -183,7 +183,10 @@ StyleDifference SVGRenderStyle::diff(const SVGRenderStyle* other) const
             || stroke->paintUri != other->stroke->paintUri
             || stroke->miterLimit != other->stroke->miterLimit
             || stroke->dashArray != other->stroke->dashArray
-            || stroke->dashOffset != other->stroke->dashOffset)
+            || stroke->dashOffset != other->stroke->dashOffset
+            || stroke->visitedLinkPaintColor != other->stroke->visitedLinkPaintColor
+            || stroke->visitedLinkPaintUri != other->stroke->visitedLinkPaintUri
+            || stroke->visitedLinkPaintType != other->stroke->visitedLinkPaintType)
             return StyleDifferenceLayout;
 
         // Only the stroke-opacity case remains, where we only need a repaint.
