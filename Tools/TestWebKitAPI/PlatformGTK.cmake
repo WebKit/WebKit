6ed8c6081b2db7b22ecc1f4fe0e2af5d59733bb6
Martin Robinson <mrobinson@webkit.org>
2013-08-14 21:00:39 +0000

    [GTK] [CMake] Add support for building TestWebKitAPI https://bugs.webkit.org/show_bug.cgi?id=116987
    Reviewed by Philippe Normand.
    
    .:
    
    * Source/cmake/OptionsGTK.cmake: Turn on the API tests in the settings.
    
    Tools:
    
    * TestWebKitAPI/CMakeLists.txt: Move the library listings to before the inclusion of the
    platform-specific file, so that GTK+ can override them.
    * TestWebKitAPI/PlatformGTK.cmake: Added.
    
    Canonical link: https://commits.webkit.org/137755@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@154071 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Tools/TestWebKitAPI/PlatformGTK.cmake
@@ -0,0 +1,114 @@
+# This is necessary because it is possible to build TestWebKitAPI with WebKit2
+# disabled and this triggers the inclusion of the WebKit2 headers.
+add_definitions(-DBUILDING_WEBKIT2__)
+
+add_custom_target(forwarding-headersGTKForTestWebKitAPI
+    COMMAND ${PERL_EXECUTABLE} ${WEBKIT2_DIR}/Scripts/generate-forwarding-headers.pl ${WEBKIT2_DIR} ${DERIVED_SOURCES_WEBKIT2_DIR}/include gtk
+    COMMAND ${PERL_EXECUTABLE} ${WEBKIT2_DIR}/Scripts/generate-forwarding-headers.pl ${TESTWEBKITAPI_DIR} ${DERIVED_SOURCES_WEBKIT2_DIR}/include gtk
+)
+set(ForwardingHeadersForTestWebKitAPI_NAME forwarding-headersGTKForTestWebKitAPI)
+
+add_custom_target(forwarding-headersSoupForTestWebKitAPI
+    COMMAND ${PERL_EXECUTABLE} ${WEBKIT2_DIR}/Scripts/generate-forwarding-headers.pl ${WEBKIT2_DIR} ${DERIVED_SOURCES_WEBKIT2_DIR}/include soup
+    COMMAND ${PERL_EXECUTABLE} ${WEBKIT2_DIR}/Scripts/generate-forwarding-headers.pl ${TESTWEBKITAPI_DIR} ${DERIVED_SOURCES_WEBKIT2_DIR}/include soup
+)
+set(ForwardingNetworkHeadersForTestWebKitAPI_NAME forwarding-headersSoupForTestWebKitAPI)
+
+include_directories(
+    ${WEBKIT2_DIR}/UIProcess/API/C/soup
+    ${WEBKIT2_DIR}/UIProcess/API/C/gtk
+    ${WEBKIT2_DIR}/UIProcess/API/gtk
+    ${GDK3_INCLUDE_DIRS}
+    ${GLIB_INCLUDE_DIRS}
+    ${GTK3_INCLUDE_DIRS}
+    ${LIBSOUP_INCLUDE_DIRS}
+)
+set(test_main_SOURCES
+    ${TESTWEBKITAPI_DIR}/gtk/main.cpp
+)
+
+set(bundle_harness_SOURCES
+    ${TESTWEBKITAPI_DIR}/gtk/InjectedBundleControllerGtk.cpp
+    ${TESTWEBKITAPI_DIR}/gtk/PlatformUtilitiesGtk.cpp
+)
+
+set(webkit2_api_harness_SOURCES
+    ${TESTWEBKITAPI_DIR}/gtk/PlatformUtilitiesGtk.cpp
+    ${TESTWEBKITAPI_DIR}/gtk/PlatformWebViewGtk.cpp
+    ${TESTWEBKITAPI_DIR}/gtk/PlatformWebViewGtk.cpp
+    ../../Source/WebCore/platform/gtk/GOwnPtrGtk.cpp
+)
+
+list(APPEND test_wtf_LIBRARIES
+    ${GDK3_LIBRARIES}
+    ${GTK3_LIBRARIES}
+)
+
+list(APPEND test_webkit2_api_LIBRARIES
+    ${GDK3_LIBRARIES}
+    ${GTK3_LIBRARIES}
+)
+
+list(APPEND test_webcore_LIBRARIES
+    ${GDK3_LIBRARIES}
+    ${GTK3_LIBRARIES}
+)
+
+# The list below works like a test expectation. Tests in the
+# test_{webkit2_api|webcore}_BINARIES list are added to the test runner and
+# tried on the bots on every build. Tests in test_{webkit2_api|webcore}_BINARIES
+# are compiled and suffixed with fail and skipped from the test runner.
+#
+# Make sure that the tests are passing on both Debug and
+# Release builds before adding it to test_{webkit2_api|webcore}_BINARIES.
+
+set(test_webcore_BINARIES
+    LayoutUnit
+    KURL
+)
+
+set(test_webkit2_api_BINARIES
+    AboutBlankLoad
+    CanHandleRequest
+    CookieManager
+    DocumentStartUserScriptAlertCrash
+    DOMWindowExtensionBasic
+    DOMWindowExtensionNoCache
+    DownloadDecideDestinationCrash
+    EvaluateJavaScript
+    FailedLoad
+    Find
+    ForceRepaint
+    FrameMIMETypeHTML
+    FrameMIMETypePNG
+    GetInjectedBundleInitializationUserDataCallback
+    HitTestResultNodeHandle
+    InjectedBundleBasic
+    InjectedBundleFrameHitTest
+    InjectedBundleInitializationUserDataCallbackWins
+    LoadAlternateHTMLStringWithNonDirectoryURL
+    LoadCanceledNoServerRedirectCallback
+    LoadPageOnCrash
+    MouseMoveAfterCrash
+    NewFirstVisuallyNonEmptyLayout
+    NewFirstVisuallyNonEmptyLayoutFails
+    NewFirstVisuallyNonEmptyLayoutForImages
+    NewFirstVisuallyNonEmptyLayoutFrames
+    PageLoadBasic
+    PageLoadDidChangeLocationWithinPageForFrame
+    ParentFrame
+    PreventEmptyUserAgent
+    PrivateBrowsingPushStateNoHistoryCallback
+    ReloadPageAfterCrash
+    ResizeWindowAfterCrash
+    RestoreSessionStateContainingFormData
+    ShouldGoToBackForwardListItem
+    UserMessage
+    WillSendSubmitEvent
+    WKConnection
+    WKPageGetScaleFactorNotZero
+    WKPreferences
+    WKString
+    WKStringJSString
+    WKURL
+)
