a1e2afbe2dd1f1973632011855330222b2f82021
Brent Fulgham <bfulgham@apple.com>
2013-08-21 16:56:11 +0000

    <https://webkit.org/b/120113> [Windows] Enable CSS_IMAGE_SET
    Reviewed by Anders Carlsson.
    
    We can now activate:
    fast/css/image-set-parsing.html
    fast/css/image-set-parsing-invalid.html
    
    * WebCore.vcxproj/WebCore.vcxproj: Add missing image set implementation files.
    * WebCore.vcxproj/WebCore.vcxproj.filters: Ditto.
    * css/CSSAllInOne.cpp: Add missing CSSImageSetValue.cpp
    * rendering/style/StyleAllInOne.cpp: Add missing StyeCachedImageSet.cpp
    
    
    
    Canonical link: https://commits.webkit.org/138055@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@154391 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/style/StyleAllInOne.cpp
+++ Source/WebCore/rendering/style/StyleAllInOne.cpp
@@ -38,6 +38,7 @@
 #include "StyleBackgroundData.cpp"
 #include "StyleBoxData.cpp"
 #include "StyleCachedImage.cpp"
+#include "StyleCachedImageSet.cpp"
 #include "StyleDeprecatedFlexibleBoxData.cpp"
 #include "StyleFilterData.cpp"
 #include "StyleFlexibleBoxData.cpp"
