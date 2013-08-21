eb0893f74feaf98c84a299893c65f6b2b117607b
Brent Fulgham <bfulgham@apple.com>
2013-08-21 23:18:21 +0000

    [Windows] Clean up project file after %PlatformArchitecture% change landed.
    * WebInspectorUI.vcxproj/WebInspectorUI.vcxproj: Remove hard-coded settings covered
    by our configuration files.
    * WebInspectorUI.vcxproj/WebInspectorUIPostBuild.cmd: Use the %PlatformArchitecture%
    macro to support 32- and 64-bit builds.
    
    
    
    Canonical link: https://commits.webkit.org/138090@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@154427 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/WebInspectorUI.vcxproj/WebInspectorUIPostBuild.cmd
+++ Source/WebInspectorUI/WebInspectorUI.vcxproj/WebInspectorUIPostBuild.cmd
@@ -1,4 +1,4 @@
-mkdir 2>NUL "%CONFIGURATIONBUILDDIR%\bin32\WebKit.resources\WebInspectorUI"
-xcopy /y /e "%PROJECTDIR%..\UserInterface\*" "%CONFIGURATIONBUILDDIR%\bin32\WebKit.resources\WebInspectorUI"
-xcopy /y /d "%PROJECTDIR%..\Localizations\en.lproj\*" "%CONFIGURATIONBUILDDIR%\bin32\WebKit.resources\WebInspectorUI"
+mkdir 2>NUL "%CONFIGURATIONBUILDDIR%\bin%PlatformArchitecture%\WebKit.resources\WebInspectorUI"
+xcopy /y /e "%PROJECTDIR%..\UserInterface\*" "%CONFIGURATIONBUILDDIR%\bin%PlatformArchitecture%\WebKit.resources\WebInspectorUI"
+xcopy /y /d "%PROJECTDIR%..\Localizations\en.lproj\*" "%CONFIGURATIONBUILDDIR%\bin%PlatformArchitecture%\WebKit.resources\WebInspectorUI"
 if exist "%CONFIGURATIONBUILDDIR%\buildfailed" del "%CONFIGURATIONBUILDDIR%\buildfailed"
