658ac4269beab0b5408483c3cacf1b47a9ed87ff
Žan Doberšek <zdobersek@igalia.com>
2013-08-21 07:03:24 +0000

    <https://webkit.org/b/119836> [GTK] ChromeClient::paint is susceptible to system time changes
    Reviewed by Martin Robinson.
    
    Instead of using WTF::currentTime() as the value of the last display occurrence,
    WTF::monotonicallyIncreasingTime() should be used. The latter is not affected by
    the changes is the system's time, which can cause a halt in the display updating.
    
    * WebCoreSupport/ChromeClientGtk.cpp:
    (WebKit::ChromeClient::paint):
    
    
    Canonical link: https://commits.webkit.org/138044@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@154380 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/gtk/WebCoreSupport/ChromeClientGtk.cpp
+++ Source/WebKit/gtk/WebCoreSupport/ChromeClientGtk.cpp
@@ -588,7 +588,7 @@ void ChromeClient::performAllPendingScrolls()
 void ChromeClient::paint(WebCore::Timer<ChromeClient>*)
 {
     static const double minimumFrameInterval = 1.0 / 60.0; // No more than 60 frames a second.
-    double timeSinceLastDisplay = currentTime() - m_lastDisplayTime;
+    double timeSinceLastDisplay = monotonicallyIncreasingTime() - m_lastDisplayTime;
     double timeUntilNextDisplay = minimumFrameInterval - timeSinceLastDisplay;
 
     if (timeUntilNextDisplay > 0 && !m_forcePaint) {
@@ -617,7 +617,7 @@ void ChromeClient::paint(WebCore::Timer<ChromeClient>*)
     gtk_widget_queue_draw_area(GTK_WIDGET(m_webView), rect.x(), rect.y(), rect.width(), rect.height());
 
     m_dirtyRegion = Region();
-    m_lastDisplayTime = currentTime();
+    m_lastDisplayTime = monotonicallyIncreasingTime();
     m_repaintSoonSourceId = 0;
 
     // We update the IM context window location here, because we want it to be
