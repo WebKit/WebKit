739b395eae62017836aa4941cf3599b3264ed412
Brent Fulgham <bfulgham@apple.com>
2013-08-24 00:30:38 +0000

    [Windows] Forgot to check in the header as well!
    * win/include/WebKitSystemInterface/WebKitSystemInterface.h:
    
    
    
    Canonical link: https://commits.webkit.org/138190@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@154536 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebKitLibraries/win/include/WebKitSystemInterface/WebKitSystemInterface.h
+++ WebKitLibraries/win/include/WebKitSystemInterface/WebKitSystemInterface.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007, 2008, 2009 Apple Inc.  All rights reserved.
+ * Copyright (C) 2007, 2008, 2009, 2013 Apple Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -84,6 +84,7 @@ void wkSetUpFontCache(size_t s);
 void wkSetBaseCTM(CGContextRef, CGAffineTransform);
 void wkSetPatternPhaseInUserSpace(CGContextRef, CGPoint phasePoint);
 CGAffineTransform wkGetUserToBaseCTM(CGContextRef);
+bool wkCGContextIsPDFContext(CGContextRef);
 
 void wkDrawFocusRing(CGContextRef, CGColorRef, float radius);
 
