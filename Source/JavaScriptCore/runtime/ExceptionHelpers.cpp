e55d6a57484853bf22138bd9e7ba39cc3153526b
Chris Curtis <chris_curtis@apple.com>
2013-08-30 17:02:48 +0000

    Cleaning errorDescriptionForValue after r154839 https://bugs.webkit.org/show_bug.cgi?id=120531
    Patch by Chris Curtis <chris_curtis@apple.com> on 2013-08-30
    Reviewed by Darin Adler.
    
    Changed the assert to ASSERT_NOT_REACHED, now that r154839 has landed. errorDescriptionForValue
    can assert again that the parameterized JSValue is !isEmpty().
    
    * runtime/ExceptionHelpers.cpp:
    (JSC::errorDescriptionForValue):
    
    Canonical link: https://commits.webkit.org/138520@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@154892 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/ExceptionHelpers.cpp
+++ Source/JavaScriptCore/runtime/ExceptionHelpers.cpp
@@ -109,8 +109,8 @@ JSString* errorDescriptionForValue(ExecState* exec, JSValue v)
         return jsString(exec, object->methodTable()->className(object));
     }
     
-    ASSERT(v.isEmpty());
-    // FIXME: https://bugs.webkit.org/show_bug.cgi?id=120080 The JSValue should never be empty in this function.
+    // The JSValue should never be empty, so this point in the code should never be reached.
+    ASSERT_NOT_REACHED();
     return vm.smallStrings.emptyString();
 }
     
