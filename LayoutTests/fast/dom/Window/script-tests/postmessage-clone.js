d7906fdd1f3cc9a41c00bdc84c4f987c379a7ec3
Alexey Proskuryakov <ap@apple.com>
2013-09-13 21:32:11 +0000

    Layout Test fast/dom/Window/window-postmessage-clone.html is flaky https://bugs.webkit.org/show_bug.cgi?id=111898
    Reviewed by Oliver Hunt.
    
    * fast/dom/Window/script-tests/postmessage-clone.js: The test was checking
    modification times of non-existent files. When you do this, you get current time,
    and current time changes over time, making comparisons non-reliable.
    
    * fast/dom/Window/resources/file1.txt: Added.
    * fast/dom/Window/resources/file2.txt: Added.
    
    
    
    Canonical link: https://commits.webkit.org/139282@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@155725 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/dom/Window/script-tests/postmessage-clone.js
+++ LayoutTests/fast/dom/Window/script-tests/postmessage-clone.js
@@ -70,7 +70,7 @@ if (window.eventSender) {
     var fileRect = fileInput.getClientRects()[0];
     var targetX = fileRect.left + fileRect.width / 2;
     var targetY = fileRect.top + fileRect.height / 2;
-    eventSender.beginDragWithFiles(['got-file-upload.html', 'got-file-upload-2.html']);
+    eventSender.beginDragWithFiles(['resources/file1.txt', 'resources/file2.txt']);
     eventSender.mouseMoveTo(targetX, targetY);
     eventSender.mouseUp();
 }
