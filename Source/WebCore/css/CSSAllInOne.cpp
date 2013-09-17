b67e7ac326631076879900fc8a02f931a253c0c4
Lucas Forschler <lforschler@apple.com>
2013-09-17 21:07:41 +0000

    Merge 154391.
    Canonical link: https://commits.webkit.org/135908.364@safari-537.60-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-537.60-branch@155997 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/CSSAllInOne.cpp
+++ Source/WebCore/css/CSSAllInOne.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2011, 2013 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -45,6 +45,7 @@
 #include "CSSGroupingRule.cpp"
 #include "CSSHostRule.cpp"
 #include "CSSImageGeneratorValue.cpp"
+#include "CSSImageSetValue.cpp"
 #include "CSSImageValue.cpp"
 #include "CSSImportRule.cpp"
 #include "CSSInheritedValue.cpp"
