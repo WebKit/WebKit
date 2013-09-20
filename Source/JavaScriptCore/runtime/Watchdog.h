526168199ddd8e68a38f79a822be457f1c13529e
Allan Sandfeld Jensen <allan.jensen@digia.com>
2013-09-20 16:35:15 +0000

    Inline method exported https://bugs.webkit.org/show_bug.cgi?id=121664
    Reviewed by Darin Adler.
    
    WatchDog::didFire() is marked as an exported symbol eventhough it is
    defined inline. This breaks the build on MinGW since it results in dllimport
    being declared on a definition.
    
    * runtime/Watchdog.h:
    (JSC::Watchdog::didFire):
    
    
    Canonical link: https://commits.webkit.org/139684@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@156169 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/Watchdog.h
+++ Source/JavaScriptCore/runtime/Watchdog.h
@@ -54,7 +54,7 @@ public:
     // This version of didFire() is a more efficient version for when we want
     // to know if the watchdog has fired in the past, and not whether it should
     // fire right now.
-    JS_EXPORT_PRIVATE bool didFire() { return m_didFire; }
+    bool didFire() { return m_didFire; }
     JS_EXPORT_PRIVATE void fire();
 
     void* timerDidFireAddress() { return &m_timerDidFire; }
