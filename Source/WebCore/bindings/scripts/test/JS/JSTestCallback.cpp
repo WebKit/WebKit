1255423c0c23cee2d0b6ba4bab127111aaab8607
Anders Carlsson <andersca@apple.com>
2013-10-08 01:37:12 +0000

    Update test again.
    * bindings/scripts/test/JS/JSTestCallback.cpp:
    (WebCore::JSTestCallback::~JSTestCallback):
    
    Canonical link: https://commits.webkit.org/140547@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@157076 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/scripts/test/JS/JSTestCallback.cpp
+++ Source/WebCore/bindings/scripts/test/JS/JSTestCallback.cpp
@@ -49,7 +49,7 @@ JSTestCallback::~JSTestCallback()
     if (!context || context->isContextThread())
         delete m_data;
     else
-        context->postTask(std::make_unique<DeleteCallbackDataTask>(m_data));
+        context->postTask(DeleteCallbackDataTask::create(m_data));
 #ifndef NDEBUG
     m_data = 0;
 #endif
