0f8d133feee283fbda648a329b8e1493bbaa13ae
Hurnjoo Lee <hurnjoo.lee@samsung.com>
2013-10-08 15:56:19 +0000

    Composited layer that painted into composited ancestor is not repainted after moving https://bugs.webkit.org/show_bug.cgi?id=114655
    Patch by Hurnjoo Lee <hurnjoo.lee@samsung.com> on 2013-10-08
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    Test: compositing/repaint/absolute-painted-into-composited-ancestor.html
    
    Composited layers are currently skipping repainting after layout,
    if the repaint status is not NeedsFullRepaint. However, in case layers
    were painted into composited ancestor, they need to repaint.
    
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::shouldRepaintAfterLayout):
    
    LayoutTests:
    
    Add a text repaint test to check the repainting of composited layer
    that painted into composited ancestor.
    
    * compositing/repaint/absolute-painted-into-composited-ancestor-expected.txt: Added.
    * compositing/repaint/absolute-painted-into-composited-ancestor.html: Added.
    
    Canonical link: https://commits.webkit.org/140577@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@157108 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderLayer.cpp
+++ Source/WebCore/rendering/RenderLayer.cpp
@@ -1423,7 +1423,7 @@ inline bool RenderLayer::shouldRepaintAfterLayout() const
     // Composited layers that were moved during a positioned movement only
     // layout, don't need to be repainted. They just need to be recomposited.
     ASSERT(m_repaintStatus == NeedsFullRepaintForPositionedMovementLayout);
-    return !isComposited();
+    return !isComposited() || backing()->paintsIntoCompositedAncestor();
 #else
     return true;
 #endif
