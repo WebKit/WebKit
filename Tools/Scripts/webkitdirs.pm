c035b1650b059e4d5686a8b4d495fea8c77968fc
Alex Christensen <achristensen@webkit.org>
2013-10-23 20:55:29 +0000

    Explicitly build Win32 or x64 configuration on Windows. https://bugs.webkit.org/show_bug.cgi?id=123206
    Patch by Alex Christensen <achristensen@webkit.org> on 2013-10-23
    Reviewed by Brent Fulgham.
    
    * Scripts/webkitdirs.pm:
    (jscPath):
    (determineConfigurationForVisualStudio):
    (determinePassedConfiguration):
    Add |Win32 to the end of 32-bit Visual Studio build configurations.
    (safariPath):
    * Scripts/build-webkit:
    * Scripts/old-run-webkit-tests:
    * Scripts/run-api-tests:
    (testToolPath):
    * Scripts/run-javascriptcore-tests:
    (testapiPath):
    * Scripts/run-regexp-tests:
    (testapiPath):
    * Scripts/sunspider-compare-results:
    (pathToBuiltJSC):
    Use configuration instead of configurationForVisualStudio, which are now always different.
    
    Canonical link: https://commits.webkit.org/141325@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@157882 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -361,7 +361,7 @@ sub jscPath($)
 {
     my ($productDir) = @_;
     my $jscName = "jsc";
-    $jscName .= "_debug"  if configurationForVisualStudio() eq "Debug_All";
+    $jscName .= "_debug"  if configuration() eq "Debug_All";
     $jscName .= ".exe" if (isWindows() || isCygwin());
     return "$productDir/$jscName" if -e "$productDir/$jscName";
     return "$productDir/JavaScriptCore.framework/Resources/$jscName";
@@ -460,7 +460,7 @@ sub determineConfigurationForVisualStudio
     return if defined $configurationForVisualStudio;
     determineConfiguration();
     # FIXME: We should detect when Debug_All or Production has been chosen.
-    $configurationForVisualStudio = $configuration . (isWin64() ? "|x64" : "");
+    $configurationForVisualStudio = $configuration . (isWin64() ? "|x64" : "|Win32");
 }
 
 sub usesPerConfigurationBuildDirectory
@@ -621,6 +621,7 @@ sub determinePassedConfiguration
             $passedConfiguration = "Debug";
             $passedConfiguration .= "_WinCairo" if (isWinCairo() && isCygwin());
             $passedConfiguration .= "|x64" if isWin64();
+            $passedConfiguration .= "|Win32" if isWindows() && !isWin64();
             return;
         }
         if ($opt =~ /^--release$/i) {
@@ -628,6 +629,7 @@ sub determinePassedConfiguration
             $passedConfiguration = "Release";
             $passedConfiguration .= "_WinCairo" if (isWinCairo() && isCygwin());
             $passedConfiguration .= "|x64" if isWin64();
+            $passedConfiguration .= "|Win32" if isWindows() && !isWin64();
             return;
         }
         if ($opt =~ /^--profil(e|ing)$/i) {
@@ -635,6 +637,7 @@ sub determinePassedConfiguration
             $passedConfiguration = "Profiling";
             $passedConfiguration .= "_WinCairo" if (isWinCairo() && isCygwin());
             $passedConfiguration .= "|x64" if isWin64();
+            $passedConfiguration .= "|Win32" if isWindows() && !isWin64();
             return;
         }
     }
@@ -769,7 +772,7 @@ sub safariPath
             my $path = "$configurationProductDir/Safari.exe";
             my $debugPath = "$configurationProductDir/Safari_debug.exe";
 
-            if (configurationForVisualStudio() eq "Debug_All" && -x $debugPath) {
+            if (configuration() eq "Debug_All" && -x $debugPath) {
                 $safariBundle = $debugPath;
             } elsif (-x $path) {
                 $safariBundle = $path;
