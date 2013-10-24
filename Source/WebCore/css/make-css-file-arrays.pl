d2b4362b90386416c55270d25ec2ef875ca49991
Peter Molnar <pmolnar.u-szeged@partner.samsung.com>
2013-10-24 13:30:12 +0000

    Removed Qt workaround. https://bugs.webkit.org/show_bug.cgi?id=123258
    Patch by Peter Molnar <pmolnar.u-szeged@partner.samsung.com> on 2013-10-24
    Reviewed by Csaba Osztrogonác.
    
    No change of functionality, no new tests needed.
    
    !$defines case can be removed, because Qt and Android aren't in WebKit trunk,
    all ports (Mac,EFL,GTK,Windows) call this script with --defines option now
    
    * css/make-css-file-arrays.pl:
    
    Canonical link: https://commits.webkit.org/141368@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@157926 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/make-css-file-arrays.pl
+++ Source/WebCore/css/make-css-file-arrays.pl
@@ -46,18 +46,8 @@ for my $in (@ARGV) {
 
     # Slurp in the CSS file.
     my $text;
-    # We should not set --defines option and run "moc" preprocessor on Qt.
-    # See http://webkit.org/b/37296.
-    if (!$defines) {
-        open IN, "<", $in or die;
-        { local $/; $text = <IN>; }
-        close IN;
-        # Remove preprocessor directives.
-        $text =~ s|^#.*?$||mg;
-    } else {
-        require preprocessor;
-        $text = join('', applyPreprocessor($in, $defines, $preprocessor));
-    }
+    require preprocessor;
+    $text = join('', applyPreprocessor($in, $defines, $preprocessor));
 
     # Remove comments in a simple-minded way that will work fine for our files.
     # Could do this a fancier way if we were worried about arbitrary CSS source.
