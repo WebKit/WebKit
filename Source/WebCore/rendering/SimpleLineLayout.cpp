WebKit/WebKit

commit 1964d915abdf125e2d20a926d5662a823d2d7898

Simple line layout https://bugs.webkit.org/show_bug.cgi?id=122458
Reviewed by Darin Adler.

Line box based inline layout is powerful but also rather slow and memory intensive. Simple line layout
is a compact alternative data structure and fast code path to cover common cases without requiring line
boxes.

This patch handles a case single left-aligned text renderer inside flow with no floats. Even this very basic
case is sufficiently common to handle up to 25% of all text lines on some popular new sites. The decision
which path to use is made per block flow (paragraph).

Simple line layout aims to produce pixel-exact rendering result when compared to the line box layout.

The goal is to handle everything that requires line level access in cases that allow use of simple lines.
This is not quite the case yet. For example selections and outline painting are not supported. In these
cases we seamlessly switch to the line boxes.

The simple line data structure currently uses 12 bytes per line. Lineboxes take ~160 bytes minimum per line.
Laying out the lines is also several times faster as is iterating over them.

* CMakeLists.txt:
* GNUmakefile.list.am:
* WebCore.vcxproj/WebCore.vcxproj:
* WebCore.xcodeproj/project.pbxproj:
* dom/Position.cpp:
(WebCore::Position::upstream):
(WebCore::Position::downstream):
(WebCore::Position::getInlineBoxAndOffset):

    Creating positions within a simple line flow causes switch to line boxes.

* editing/TextIterator.cpp:
(WebCore::TextIterator::handleTextNode):

    TextIterator traverses line boxes if available. In case simple line case we need to replicate the
    same results (for compatibility but especially to avoid changing test results). This is done here
    by just traversing the string without actually using the layout.

* rendering/RenderBlock.cpp:
(WebCore::RenderBlock::RenderBlock):
(WebCore::RenderBlock::layoutShapeInsideInfo):
* rendering/RenderBlock.h:
* rendering/RenderBlockFlow.cpp:

Source/WebCore/rendering/SimpleLineLayout.cpp
@@ -1,0 +1,266 @@
+/*
+ * Copyright (C) 2013 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "SimpleLineLayout.h"
+
+#include "FontCache.h"
+#include "GraphicsContext.h"
+#include "HitTestLocation.h"
+#include "HitTestRequest.h"
+#include "HitTestResult.h"
+#include "InlineTextBox.h"
+#include "LineWidth.h"
+#include "PaintInfo.h"
+#include "RenderBlockFlow.h"
+#include "RenderStyle.h"
+#include "RenderText.h"
+#include "RenderView.h"
+#include "SimpleLineLayoutResolver.h"
+#include "Text.h"
+#include "TextPaintStyle.h"
+#include "break_lines.h"
+#include <wtf/unicode/Unicode.h>
+
+namespace WebCore {
+namespace SimpleLineLayout {
+
+static inline bool isWhitespace(UChar character)
+{
+    return character == ' ' || character == '\t' || character == '\n';
+}
+
+bool canUseFor(const RenderBlockFlow& flow)
+{
+    if (!flow.firstChild())
+        return false;
+    // This currently covers <blockflow>#text</blockflow> case.
+    // The <blockflow><inline>#text</inline></blockflow> case is also popular and should be relatively easy to cover.
+    if (flow.firstChild() != flow.lastChild())
+        return false;
+    if (!flow.firstChild()->isText())
+        return false;
+    // Supporting floats would be very beneficial.
+    if (flow.containsFloats())
+        return false;
+    if (!flow.isHorizontalWritingMode())
+        return false;
+    if (flow.flowThreadState() != RenderObject::NotInsideFlowThread)
+        return false;
+    if (flow.hasOutline())
+        return false;
+    if (flow.isRubyText() || flow.isRubyBase())
+        return false;
+    // These tests only works during layout. Outside layout this function may give false positives.
+    if (flow.view().layoutState()) {
+        if (flow.view().layoutState()->shapeInsideInfo())
+            return false;
+        if (flow.view().layoutState()->m_columnInfo)
+            return false;
+    }
+    const RenderStyle& style = *flow.style();
+    // It shoudn't be hard to support other alignments.
+    if (style.textAlign() != LEFT && style.textAlign() != WEBKIT_LEFT && style.textAlign() != TASTART)
+        return false;
+    // Non-visible overflow should be pretty easy to support.
+    if (style.overflowX() != OVISIBLE || style.overflowY() != OVISIBLE)
+        return false;
+    // Pre/no-wrap would be very helpful to support.
+    if (style.whiteSpace() != NORMAL)
+        return false;
+    if (!style.textIndent().isZero())
+        return false;
+    if (style.wordSpacing() || style.letterSpacing())
+        return false;
+    if (style.textTransform() != TTNONE)
+        return false;
+    if (!style.isLeftToRightDirection())
+        return false;
+    if (style.lineBoxContain() != RenderStyle::initialLineBoxContain())
+        return false;
+    if (style.writingMode() != TopToBottomWritingMode)
+        return false;
+    if (style.lineBreak() != LineBreakAuto)
+        return false;
+    if (style.wordBreak() != NormalWordBreak)
+        return false;
+    if (style.unicodeBidi() != UBNormal || style.rtlOrdering() != LogicalOrder)
+        return false;
+    if (style.lineAlign() != LineAlignNone || style.lineSnap() != LineSnapNone)
+        return false;
+    if (style.hyphens() == HyphensAuto)
+        return false;
+    if (style.textEmphasisFill() != TextEmphasisFillFilled || style.textEmphasisMark() != TextEmphasisMarkNone)
+        return false;
+    if (style.textShadow())
+        return false;
+    if (style.resolvedShapeInside())
+        return true;
+    if (style.textOverflow() || (flow.isAnonymousBlock() && flow.parent()->style()->textOverflow()))
+        return false;
+    if (style.hasPseudoStyle(FIRST_LINE) || style.hasPseudoStyle(FIRST_LETTER))
+        return false;
+    if (style.hasTextCombine())
+        return false;
+    if (style.overflowWrap() != NormalOverflowWrap)
+        return false;
+    if (style.backgroundClip() == TextFillBox)
+        return false;
+    if (style.borderFit() == BorderFitLines)
+        return false;
+    const RenderText& textRenderer = toRenderText(*flow.firstChild());
+    if (textRenderer.isCombineText() || textRenderer.isCounter() || textRenderer.isQuote() || textRenderer.isSVGInlineText() || textRenderer.isTextFragment())
+        return false;
+    if (style.font().codePath(TextRun(textRenderer.text())) != Font::Simple)
+        return false;
+    if (!textRenderer.knownToHaveNoOverflowAndNoFallbackFonts())
+        return false;
+
+    unsigned length = textRenderer.textLength();
+    unsigned consecutiveSpaceCount = 0;
+    for (unsigned i = 0; i < length; ++i) {
+        // This rejects anything with more than one consecutive whitespace, except at the beginning or end.
+        // This is because we don't currently do subruns within lines. Fixing this would improve coverage significantly.
+        UChar character = textRenderer.characterAt(i);
+        if (isWhitespace(character))
+            ++consecutiveSpaceCount;
+        else {
+            if (consecutiveSpaceCount != i && consecutiveSpaceCount > 1)
+                return false;
+            consecutiveSpaceCount = 0;
+        }
+        // These would be easy to support.
+        if (character == noBreakSpace)
+            return false;
+        if (character == softHyphen)
+            return false;
+
+        static const UChar lowestRTLCharacter = 0x590;
+        if (character >= lowestRTLCharacter) {
+            UCharDirection direction = u_charDirection(character);
+            if (direction == U_RIGHT_TO_LEFT || direction == U_RIGHT_TO_LEFT_ARABIC
+                || direction == U_RIGHT_TO_LEFT_EMBEDDING || direction == U_RIGHT_TO_LEFT_OVERRIDE
+                || direction == U_LEFT_TO_RIGHT_EMBEDDING || direction == U_LEFT_TO_RIGHT_OVERRIDE)
+                return false;
+        }
+    }
+    return true;
+}
+
+static inline unsigned skipWhitespaces(const RenderText& textRenderer, unsigned offset, unsigned length)
+{
+    for (; offset < length; ++offset) {
+        if (!isWhitespace(textRenderer.characterAt(offset)))
+            break;
+    }
+    return offset;
+}
+
+static float textWidth(const RenderText& text, unsigned from, unsigned length, float xPosition, const RenderStyle& style)
+{
+    if (style.font().isFixedPitch() || (!from && length == text.textLength()))
+        return text.width(from, length, style.font(), xPosition, nullptr, nullptr);
+    // FIXME: Add templated UChar/LChar paths.
+    TextRun run = text.is8Bit() ? TextRun(text.characters8() + from, length) : TextRun(text.characters16() + from, length);
+    run.setCharactersLength(text.textLength() - from);
+    ASSERT(run.charactersLength() >= run.length());
+
+    run.setXPos(xPosition);
+    return style.font().width(run);
+}
+
+std::unique_ptr<Lines> createLines(RenderBlockFlow& flow)
+{
+    auto lines = std::make_unique<Lines>();
+
+    RenderText& textRenderer = toRenderText(*flow.firstChild());
+    ASSERT(!textRenderer.firstTextBox());
+
+    const RenderStyle& style = *flow.style();
+    const unsigned textLength = textRenderer.textLength();
+
+    float wordTrailingSpaceWidth = style.font().width(TextRun(&space, 1));
+
+    LazyLineBreakIterator lineBreakIterator(textRenderer.text(), style.locale());
+    int nextBreakable = -1;
+
+    unsigned lineEndOffset = 0;
+    while (lineEndOffset < textLength) {
+        lineEndOffset = skipWhitespaces(textRenderer, lineEndOffset, textLength);
+        unsigned lineStartOffset = lineEndOffset;
+        unsigned runEndOffset = lineEndOffset;
+        LineWidth lineWidth(flow, false, DoNotIndentText);
+        while (runEndOffset < textLength) {
+            ASSERT(!isWhitespace(textRenderer.characterAt(runEndOffset)));
+
+            bool previousWasSpaceBetweenRuns = runEndOffset > lineStartOffset && isWhitespace(textRenderer.characterAt(runEndOffset - 1));
+            unsigned runStartOffset = previousWasSpaceBetweenRuns ? runEndOffset - 1 : runEndOffset;
+
+            ++runEndOffset;
+            while (runEndOffset < textLength) {
+                if (runEndOffset > lineStartOffset && isBreakable(lineBreakIterator, runEndOffset, nextBreakable, false))
+                    break;
+                ++runEndOffset;
+            }
+
+            unsigned runLength = runEndOffset - runStartOffset;
+            bool includeEndSpace = runEndOffset < textLength && textRenderer.characterAt(runEndOffset) == ' ';
+            float wordWidth;
+            if (includeEndSpace)
+                wordWidth = textWidth(textRenderer, runStartOffset, runLength + 1, lineWidth.committedWidth(), style) - wordTrailingSpaceWidth;
+            else
+                wordWidth = textWidth(textRenderer, runStartOffset, runLength, lineWidth.committedWidth(), style);
+
+            lineWidth.addUncommittedWidth(wordWidth);
+            if (!lineWidth.fitsOnLine()) {
+                if (!lineWidth.committedWidth()) {
+                    lineWidth.commit();
+                    lineEndOffset = runEndOffset;
+                }
+                break;
+            }
+            lineWidth.commit();
+            lineEndOffset = runEndOffset;
+            runEndOffset = skipWhitespaces(textRenderer, runEndOffset, textLength);
+        }
+        if (lineStartOffset == lineEndOffset)
+            continue;
+
+        Line line;
+        line.textOffset = lineStartOffset;
+        line.textLength = lineEndOffset - lineStartOffset;
+        line.width = lineWidth.committedWidth();
+
+        lines->append(line);
+    }
+
+    textRenderer.clearNeedsLayout();
+
+    lines->shrinkToFit();
+    return lines;
+}
+
+}
+}

Source/WebCore/CMakeLists.txt
@@ -2175,6 +2175,8 @@ set(WebCore_SOURCES
     rendering/RenderWidget.cpp
     rendering/RootInlineBox.cpp
     rendering/ScrollBehavior.cpp
+    rendering/SimpleLineLayout.cpp
+    rendering/SimpleLineLayoutFunctions.cpp
     rendering/TextAutosizer.cpp
     rendering/TextPaintStyle.cpp
     rendering/break_lines.cpp

Source/WebCore/GNUmakefile.list.am
@@ -4483,6 +4483,11 @@ webcore_sources += \
 	Source/WebCore/rendering/RootInlineBox.h \
 	Source/WebCore/rendering/ScrollBehavior.cpp \
 	Source/WebCore/rendering/ScrollBehavior.h \
+	Source/WebCore/rendering/SimpleLineLayout.cpp \
+	Source/WebCore/rendering/SimpleLineLayout.h \
+	Source/WebCore/rendering/SimpleLineLayoutFunctions.cpp \
+	Source/WebCore/rendering/SimpleLineLayoutFunctions.h \
+	Source/WebCore/rendering/SimpleLineLayoutResolver.h \
 	Source/WebCore/rendering/TextAutosizer.cpp \
 	Source/WebCore/rendering/TextAutosizer.h \
 	Source/WebCore/rendering/TextPaintStyle.cpp \

Source/WebCore/WebCore.vcxproj/WebCore.vcxproj
@@ -10912,6 +10912,8 @@
       <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='Production|Win32'">true</ExcludedFromBuild>
       <ExcludedFromBuild Condition="'$(Configuration)|$(Platform)'=='Production|x64'">true</ExcludedFromBuild>
     </ClCompile>
+    <ClCompile Include="..\rendering\SimpleLineLayout.cpp" />
+    <ClCompile Include="..\rendering\SimpleLineLayoutFunctions.cpp" />
     <ClCompile Include="..\rendering\TextPaintStyle.cpp" />
     <ClCompile Include="..\rendering\shapes\PolygonShape.cpp" />
     <ClCompile Include="..\rendering\shapes\RasterShape.cpp" />
@@ -19682,6 +19684,9 @@
     <ClInclude Include="..\rendering\RenderWidget.h" />
     <ClInclude Include="..\rendering\RootInlineBox.h" />
     <ClInclude Include="..\rendering\ScrollBehavior.h" />
+    <ClInclude Include="..\rendering\SimpleLineLayout.h" />
+    <ClInclude Include="..\rendering\SimpleLineLayoutFunctions.h" />
+    <ClInclude Include="..\rendering\SimpleLineLayoutResolver.h" />
     <ClInclude Include="..\rendering\svg\SVGMarkerData.h" />
     <ClInclude Include="..\rendering\svg\SVGPathData.h" />
     <ClInclude Include="..\rendering\svg\SVGRenderingContext.h" />

Source/WebCore/WebCore.xcodeproj/project.pbxproj
@@ -5681,6 +5681,8 @@
 		E47B4BE90E71241600038854 /* CachedResourceHandle.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E47B4BE70E71241600038854 /* CachedResourceHandle.cpp */; };
 		E47E276516036ED200EE2AFB /* DocumentStyleSheetCollection.h in Headers */ = {isa = PBXBuildFile; fileRef = E47E276416036ED200EE2AFB /* DocumentStyleSheetCollection.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		E47E276816036EDC00EE2AFB /* DocumentStyleSheetCollection.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E47E276716036EDC00EE2AFB /* DocumentStyleSheetCollection.cpp */; };
+		E48944A2180B57D800F165D8 /* SimpleLineLayout.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E48944A0180B57D800F165D8 /* SimpleLineLayout.cpp */; };
+		E48944A3180B57D800F165D8 /* SimpleLineLayout.h in Headers */ = {isa = PBXBuildFile; fileRef = E48944A1180B57D800F165D8 /* SimpleLineLayout.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		E4946EAE156E64DD00D3297F /* StyleRuleImport.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E4946EAC156E64DD00D3297F /* StyleRuleImport.cpp */; };
 		E4946EAF156E64DD00D3297F /* StyleRuleImport.h in Headers */ = {isa = PBXBuildFile; fileRef = E4946EAD156E64DD00D3297F /* StyleRuleImport.h */; };
 		E49BD9FA131FD2ED003C56F0 /* CSSValuePool.h in Headers */ = {isa = PBXBuildFile; fileRef = E49BD9F9131FD2ED003C56F0 /* CSSValuePool.h */; settings = {ATTRIBUTES = (Private, ); }; };
@@ -5717,6 +5719,9 @@
 		E4D988B617BFEB210084FB88 /* TextNodeTraversal.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E4D988B517BFEB210084FB88 /* TextNodeTraversal.cpp */; };
 		E4DEAA1717A93DC3000E0430 /* StyleResolveTree.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E4DEAA1517A93DC3000E0430 /* StyleResolveTree.cpp */; };
 		E4DEAA1817A93DC3000E0430 /* StyleResolveTree.h in Headers */ = {isa = PBXBuildFile; fileRef = E4DEAA1617A93DC3000E0430 /* StyleResolveTree.h */; settings = {ATTRIBUTES = (Private, ); }; };
+		E4E9B1191810916F003ACCDF /* SimpleLineLayoutResolver.h in Headers */ = {isa = PBXBuildFile; fileRef = E4E9B1181810916F003ACCDF /* SimpleLineLayoutResolver.h */; };
+		E4E9B11B18145692003ACCDF /* SimpleLineLayoutFunctions.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E4E9B11A18145692003ACCDF /* SimpleLineLayoutFunctions.cpp */; };
+		E4E9B11D1814569C003ACCDF /* SimpleLineLayoutFunctions.h in Headers */ = {isa = PBXBuildFile; fileRef = E4E9B11C1814569C003ACCDF /* SimpleLineLayoutFunctions.h */; };
 		E4F9EEF2156D9FFA00D23E7E /* StyleSheetContents.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E4F9EEF0156D84C400D23E7E /* StyleSheetContents.cpp */; };
 		E4F9EEF3156DA00700D23E7E /* StyleSheetContents.h in Headers */ = {isa = PBXBuildFile; fileRef = E4F9EEF1156D84C400D23E7E /* StyleSheetContents.h */; };
 		E51A81DF17298D7700BFCA61 /* JSPerformance.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E51A81DE17298D7700BFCA61 /* JSPerformance.cpp */; };
@@ -12669,6 +12674,8 @@
 		E47B4BE70E71241600038854 /* CachedResourceHandle.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = CachedResourceHandle.cpp; sourceTree = "<group>"; };
 		E47E276416036ED200EE2AFB /* DocumentStyleSheetCollection.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = DocumentStyleSheetCollection.h; sourceTree = "<group>"; };
 		E47E276716036EDC00EE2AFB /* DocumentStyleSheetCollection.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = DocumentStyleSheetCollection.cpp; sourceTree = "<group>"; };
+		E48944A0180B57D800F165D8 /* SimpleLineLayout.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = SimpleLineLayout.cpp; sourceTree = "<group>"; };
+		E48944A1180B57D800F165D8 /* SimpleLineLayout.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = SimpleLineLayout.h; sourceTree = "<group>"; };
 		E4946EAC156E64DD00D3297F /* StyleRuleImport.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = StyleRuleImport.cpp; sourceTree = "<group>"; };
 		E4946EAD156E64DD00D3297F /* StyleRuleImport.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = StyleRuleImport.h; sourceTree = "<group>"; };
 		E49BD9F9131FD2ED003C56F0 /* CSSValuePool.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = CSSValuePool.h; sourceTree = "<group>"; };
@@ -12722,6 +12729,9 @@
 		E4D988B517BFEB210084FB88 /* TextNodeTraversal.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = TextNodeTraversal.cpp; sourceTree = "<group>"; };
 		E4DEAA1517A93DC3000E0430 /* StyleResolveTree.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = StyleResolveTree.cpp; sourceTree = "<group>"; };
 		E4DEAA1617A93DC3000E0430 /* StyleResolveTree.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = StyleResolveTree.h; sourceTree = "<group>"; };
+		E4E9B1181810916F003ACCDF /* SimpleLineLayoutResolver.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = SimpleLineLayoutResolver.h; sourceTree = "<group>"; };
+		E4E9B11A18145692003ACCDF /* SimpleLineLayoutFunctions.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = SimpleLineLayoutFunctions.cpp; sourceTree = "<group>"; };
+		E4E9B11C1814569C003ACCDF /* SimpleLineLayoutFunctions.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = SimpleLineLayoutFunctions.h; sourceTree = "<group>"; };
 		E4F9EEF0156D84C400D23E7E /* StyleSheetContents.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = StyleSheetContents.cpp; sourceTree = "<group>"; };
 		E4F9EEF1156D84C400D23E7E /* StyleSheetContents.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = StyleSheetContents.h; sourceTree = "<group>"; };
 		E51A81DE17298D7700BFCA61 /* JSPerformance.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = JSPerformance.cpp; sourceTree = "<group>"; };
@@ -20768,6 +20778,11 @@
 				5D925B650F64D4DD00B847F0 /* ScrollBehavior.cpp */,
 				5D925B660F64D4DD00B847F0 /* ScrollBehavior.h */,
 				A8CFF04C0A154F09000A4234 /* TableLayout.h */,
+				E48944A0180B57D800F165D8 /* SimpleLineLayout.cpp */,
+				E48944A1180B57D800F165D8 /* SimpleLineLayout.h */,
+				E4E9B11A18145692003ACCDF /* SimpleLineLayoutFunctions.cpp */,
+				E4E9B11C1814569C003ACCDF /* SimpleLineLayoutFunctions.h */,
+				E4E9B1181810916F003ACCDF /* SimpleLineLayoutResolver.h */,
 				E4C91A0F1802343900A17F6D /* TextPaintStyle.cpp */,
 				E4C91A0D1802343100A17F6D /* TextPaintStyle.h */,
 				37FC96DA1104ED71003E1FAD /* TrailingFloatsRootInlineBox.h */,
@@ -22573,6 +22588,7 @@
 				A80D67080E9E9DEB00E420F0 /* GraphicsContextPlatformPrivateCG.h in Headers */,
 				0F580B0D0F12A2690051D689 /* GraphicsLayer.h in Headers */,
 				499B3ED7128CD31400E726C2 /* GraphicsLayerCA.h in Headers */,
+				E4E9B1191810916F003ACCDF /* SimpleLineLayoutResolver.h in Headers */,
 				0F580B0E0F12A2690051D689 /* GraphicsLayerClient.h in Headers */,
 				CDAB6D2E17C814EE00C60B34 /* JSMediaControlsHost.h in Headers */,
 				1AC69593161A1E53003732CB /* GraphicsLayerFactory.h in Headers */,
@@ -23633,6 +23649,7 @@
 				49D5DC2E0F423A73008F20FD /* PerspectiveTransformOperation.h in Headers */,
 				D0FF2A5E11F8C45A007E74E0 /* PingLoader.h in Headers */,
 				377C4CDF1014E9F600B9AE42 /* PlaceholderDocument.h in Headers */,
+				E4E9B11D1814569C003ACCDF /* SimpleLineLayoutFunctions.h in Headers */,
 				499B3EDD128DB50200E726C2 /* PlatformCAAnimation.h in Headers */,
 				0F13163E16ED0CC80035CC04 /* PlatformCAFilters.h in Headers */,
 				499B3EC5128CCC4700E726C2 /* PlatformCALayer.h in Headers */,
@@ -23819,6 +23836,7 @@
 				0854B0171255E4E600B9CDD0 /* RenderSVGInlineText.h in Headers */,
 				CD3A496217A9D01B00274E42 /* SourceBuffer.h in Headers */,
 				436708CB12D9CA4B00044234 /* RenderSVGModelObject.h in Headers */,
+				E48944A3180B57D800F165D8 /* SimpleLineLayout.h in Headers */,
 				ADDF1AD71257CD9A0003A759 /* RenderSVGPath.h in Headers */,
 				A10BB5851484E3A700B2E87A /* RenderSVGRect.h in Headers */,
 				436708CD12D9CA4B00044234 /* RenderSVGResource.h in Headers */,
@@ -26603,6 +26621,7 @@
 				15C77094100D3CA8005BA267 /* JSValidityState.cpp in Sources */,
 				BE8EF04A171C9014009B48C3 /* JSVideoTrack.cpp in Sources */,
 				BE6DF70B171CA2C500DD52B8 /* JSVideoTrackCustom.cpp in Sources */,
+				E4E9B11B18145692003ACCDF /* SimpleLineLayoutFunctions.cpp in Sources */,
 				BE8EF04C171C9014009B48C3 /* JSVideoTrackList.cpp in Sources */,
 				BE6DF70D171CA2C500DD52B8 /* JSVideoTrackListCustom.cpp in Sources */,
 				078E093317D16B0600420AA1 /* MediaStreamCenter.cpp in Sources */,
@@ -26845,6 +26864,7 @@
 				F3820892147D35F90010BC06 /* PageConsoleAgent.cpp in Sources */,
 				F34742DC134362F000531BC2 /* PageDebuggerAgent.cpp in Sources */,
 				9302B0BD0D79F82900C7EE83 /* PageGroup.cpp in Sources */,
+				E48944A2180B57D800F165D8 /* SimpleLineLayout.cpp in Sources */,
 				7A674BDB0F9EBF4E006CF099 /* PageGroupLoadDeferrer.cpp in Sources */,
 				1C26497C0D7E24EC00BD10F2 /* PageMac.cpp in Sources */,
 				FBDB619D16D6034600BB3394 /* PageRuleCollector.cpp in Sources */,

commit 623412071d527f946ce82dd26d4b4a5730f4320f

Non-SVG build broken after r157950 https://bugs.webkit.org/show_bug.cgi?id=123328
Reviewed by Xan Lopez.

The isSVGInlineTest() check should be done only if SVG is enabled.

* rendering/SimpleLineLayout.cpp:
(WebCore::SimpleLineLayout::canUseFor):

Canonical link: https://commits.webkit.org/141424@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@157998 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/SimpleLineLayout.cpp
@@ -135,7 +135,11 @@ bool canUseFor(const RenderBlockFlow& flow)
     if (style.borderFit() == BorderFitLines)
         return false;
     const RenderText& textRenderer = toRenderText(*flow.firstChild());
-    if (textRenderer.isCombineText() || textRenderer.isCounter() || textRenderer.isQuote() || textRenderer.isSVGInlineText() || textRenderer.isTextFragment())
+    if (textRenderer.isCombineText() || textRenderer.isCounter() || textRenderer.isQuote() || textRenderer.isTextFragment()
+#if ENABLE(SVG)
+        || textRenderer.isSVGInlineText()
+#endif
+        )
         return false;
     if (style.font().codePath(TextRun(textRenderer.text())) != Font::Simple)
         return false;