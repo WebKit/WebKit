f55345ca06508c1330c766458cfccc108be0ad1e
Brent Fulgham <bfulgham@apple.com>
2013-10-29 21:41:41 +0000

    Merge r155221.
        2013-09-06  Brent Fulgham  <bfulgham@apple.com>
    
        [Windows] StructuredExceptionHandlerSuppressor Causes Bad Interactions with Support Libraries
        https://bugs.webkit.org/show_bug.cgi?id=120901
    
        Reviewed by Anders Carlsson.
    
        Change from old "crash on any exception" implementation to a new version that leaves normal
        exception handling infrastructure in place for use by support libraries. We check exceptions
        and decide whether to abort or not based on the failure type.
    
        * WebCore.vcxproj/WebCore.vcxproj: Add new implementation files.
        * WebCore.vcxproj/WebCore.vcxproj.filters: Ditto.
        * platform/graphics/ca/win/LayerChangesFlusher.cpp:
        (WebCore::LayerChangesFlusher::hookCallback): Allocate handler struct on stack.
        * platform/win/StructuredExceptionHandlerSuppressor.cpp: Added.
        (exceptionShouldTerminateProgram): New.
        (exceptionHandler): New.
        (WebCore::StructuredExceptionHandlerSuppressor::StructuredExceptionHandlerSuppressor): Moved
        from header, and updated to add our new exception handler.
        (WebCore::StructuredExceptionHandlerSuppressor::~StructuredExceptionHandlerSuppressor): Moved
        from header.
        * platform/win/StructuredExceptionHandlerSuppressor.h: Move implementation of constructor and
        destructor from header file.
        * platform/win/makesafeseh.asm: Added.
    
    
    
    Canonical link: https://commits.webkit.org/135908.454@safari-537.73-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-537.73-branch@158227 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/win/StructuredExceptionHandlerSuppressor.h
+++ Source/WebCore/platform/win/StructuredExceptionHandlerSuppressor.h
@@ -26,50 +26,27 @@
 #ifndef StructuredExceptionHandlerSuppressor_h
 #define StructuredExceptionHandlerSuppressor_h
 
+#include <excpt.h>
+#include <wtf/Noncopyable.h>
+
+extern "C" EXCEPTION_DISPOSITION __stdcall exceptionHandler(struct _EXCEPTION_RECORD* exceptionRecord, void* establisherFrame, struct _CONTEXT* contextRecord, void* dispatcherContext);
+
 namespace WebCore {
 
-#pragma warning(push)
-#pragma warning(disable: 4733) // Disable "not registered as safe handler" warning
+struct ExceptionRegistration {
+    ExceptionRegistration* prev;
+    void* handler;
+};
 
 class StructuredExceptionHandlerSuppressor {
     WTF_MAKE_NONCOPYABLE(StructuredExceptionHandlerSuppressor);
 public:
-    StructuredExceptionHandlerSuppressor()
-    {
-        // Windows puts an __try/__except block around some calls, such as hooks.
-        // The exception handler then ignores system exceptions like invalid addresses
-        // and null pointers. This class can be used to remove this block and prevent
-        // it from catching the exception. Typically this will cause the exception to crash 
-        // which is often desirable to allow crashlogs to be recorded for debugging purposed.
-        // While this class is in scope we replace the Windows exception handler with 0xffffffff, 
-        // which indicates that the exception should not be handled.
-        //
-        // See http://www.microsoft.com/msj/0197/Exception/Exception.aspx
-
-        // Windows doesn't like assigning to member variables, so we need to get the value into
-        // a local variable and store it afterwards.
-        void* registration;
-
-        __asm mov eax, FS:[0]
-        __asm mov [registration], eax
-        __asm mov eax, 0xffffffff
-        __asm mov FS:[0], eax
-
-        m_savedExceptionRegistration = registration;
-    }
-
-    ~StructuredExceptionHandlerSuppressor()
-    {
-        // Restore the exception handler
-        __asm mov eax, [m_savedExceptionRegistration]
-        __asm mov FS:[0], eax
-    }
+    StructuredExceptionHandlerSuppressor(ExceptionRegistration&);
+    ~StructuredExceptionHandlerSuppressor();
 
 private:
     void* m_savedExceptionRegistration;
 };
-
-#pragma warning(pop)
 
 } // namespace WebCore
 
