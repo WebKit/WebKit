3ba5e4d8c6d0532d35c75228f05aba5a6e22d66b
Andreas Kling <akling@apple.com>
2013-10-31 05:51:52 +0000

    Manage EllipsisBox objects with unique_ptr. <https://webkit.org/b/123554>
    Use smart pointers to store ellipsis boxes instead of new/delete.
    
    Reviewed by Anders Carlsson.
    
    
    Canonical link: https://commits.webkit.org/141713@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@158346 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RootInlineBox.cpp
+++ Source/WebCore/rendering/RootInlineBox.cpp
@@ -49,7 +49,7 @@ struct SameSizeAsRootInlineBox : public InlineFlowBox {
 
 COMPILE_ASSERT(sizeof(RootInlineBox) == sizeof(SameSizeAsRootInlineBox), RootInlineBox_should_stay_small);
 
-typedef WTF::HashMap<const RootInlineBox*, EllipsisBox*> EllipsisBoxMap;
+typedef WTF::HashMap<const RootInlineBox*, std::unique_ptr<EllipsisBox>> EllipsisBoxMap;
 static EllipsisBoxMap* gEllipsisBoxMap = 0;
 
 RootInlineBox::RootInlineBox(RenderBlockFlow& block)
@@ -73,9 +73,8 @@ RootInlineBox::~RootInlineBox()
 void RootInlineBox::detachEllipsisBox()
 {
     if (hasEllipsisBox()) {
-        EllipsisBox* box = gEllipsisBoxMap->take(this);
-        box->setParent(0);
-        delete box;
+        auto box = gEllipsisBoxMap->take(this);
+        box->setParent(nullptr);
         setHasEllipsisBox(false);
     }
 }
@@ -129,12 +128,14 @@ bool RootInlineBox::lineCanAccommodateEllipsis(bool ltr, int blockEdge, int line
 
 float RootInlineBox::placeEllipsis(const AtomicString& ellipsisStr,  bool ltr, float blockLeftEdge, float blockRightEdge, float ellipsisWidth, InlineBox* markupBox)
 {
-    // Create an ellipsis box.
-    EllipsisBox* ellipsisBox = new EllipsisBox(blockFlow(), ellipsisStr, this, ellipsisWidth - (markupBox ? markupBox->logicalWidth() : 0), logicalHeight(), y(), !prevRootBox(), isHorizontal(), markupBox);
-
     if (!gEllipsisBoxMap)
         gEllipsisBoxMap = new EllipsisBoxMap();
-    gEllipsisBoxMap->add(this, ellipsisBox);
+
+    // Create an ellipsis box.
+    auto newEllipsisBox = std::make_unique<EllipsisBox>(blockFlow(), ellipsisStr, this, ellipsisWidth - (markupBox ? markupBox->logicalWidth() : 0), logicalHeight(), y(), !prevRootBox(), isHorizontal(), markupBox);
+    auto ellipsisBox = newEllipsisBox.get();
+
+    gEllipsisBoxMap->add(this, std::move(newEllipsisBox));
     setHasEllipsisBox(true);
 
     // FIXME: Do we need an RTL version of this?
