9b891890f6303a2db487969ba5cb8e1fcb2c9ca5
Antoine Quint <graouts@apple.com>
2013-11-01 18:13:42 +0000

    Remove custom Function.prototype.bind() in favor of native version https://bugs.webkit.org/show_bug.cgi?id=123608
    Reviewed by Joseph Pecoraro.
    
    * UserInterface/Utilities.js:
    
    
    Canonical link: https://commits.webkit.org/141796@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@158440 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Utilities.js
+++ Source/WebInspectorUI/UserInterface/Utilities.js
@@ -23,21 +23,6 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-if (!("bind" in Function.prototype)) {
-    Object.defineProperty(Function.prototype, "bind",
-    {
-        value: function(thisObject)
-        {
-            var func = this;
-            var args = Array.prototype.slice.call(arguments, 1);
-            return function bound()
-            {
-                return func.apply(thisObject, args.concat(Array.prototype.slice.call(arguments, 0)));
-            };
-        }
-    });
-}
-
 Object.defineProperty(Object, "shallowCopy",
 {
     value: function(object)
