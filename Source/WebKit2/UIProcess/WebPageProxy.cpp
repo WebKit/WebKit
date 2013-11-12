9a2db4322252491291843cbc75f285a09090291e
Commit Queue <commit-queue@webkit.org>
2013-11-12 06:43:34 +0000

    Unreviewed, rolling out r159088. http://trac.webkit.org/changeset/159088 https://bugs.webkit.org/show_bug.cgi?id=124187
    It broke the EFL build, because it removed a used function
    (rs=thorton) (Requested by Ossy_night on #webkit).
    
    Patch by Commit Queue <commit-queue@webkit.org> on 2013-11-11
    
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::WebPageProxy):
    (WebKit::WebPageProxy::resetStateAfterProcessExited):
    * UIProcess/WebPageProxy.h:
    (WebKit::WebPageProxy::urlAtProcessExit):
    (WebKit::WebPageProxy::loadStateAtProcessExit):
    
    Canonical link: https://commits.webkit.org/142382@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@159096 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/WebPageProxy.cpp
+++ Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -238,6 +238,7 @@ WebPageProxy::WebPageProxy(PageClient* pageClient, PassRefPtr<WebProcessProxy> p
     , m_estimatedProgress(0)
     , m_viewState(ViewState::NoFlags)
     , m_backForwardList(WebBackForwardList::create(this))
+    , m_loadStateAtProcessExit(FrameLoadState::LoadStateFinished)
     , m_temporarilyClosedComposition(false)
     , m_textZoomFactor(1)
     , m_pageZoomFactor(1)
@@ -3784,6 +3785,11 @@ void WebPageProxy::resetStateAfterProcessExited()
     m_isValid = false;
     m_isPageSuspended = false;
     m_waitingForDidUpdateViewState = false;
+
+    if (m_mainFrame) {
+        m_urlAtProcessExit = m_mainFrame->url();
+        m_loadStateAtProcessExit = m_mainFrame->loadState();
+    }
 
     resetState();
 
--- Source/WebKit2/UIProcess/WebPageProxy.h
+++ Source/WebKit2/UIProcess/WebPageProxy.h
@@ -609,6 +609,9 @@ public:
 
     PassRefPtr<ImmutableArray> relatedPages() const;
 
+    const String& urlAtProcessExit() const { return m_urlAtProcessExit; }
+    FrameLoadState::LoadState loadStateAtProcessExit() const { return m_loadStateAtProcessExit; }
+
 #if ENABLE(DRAG_SUPPORT)
     WebCore::DragSession dragSession() const { return m_currentDragSession; }
     void resetDragOperation() { m_currentDragSession = WebCore::DragSession(); }
@@ -1085,6 +1088,9 @@ private:
     bool m_maintainsInactiveSelection;
 
     String m_toolTip;
+
+    String m_urlAtProcessExit;
+    FrameLoadState::LoadState m_loadStateAtProcessExit;
 
     EditorState m_editorState;
     bool m_temporarilyClosedComposition; // Editor state changed from hasComposition to !hasComposition, but that was only with shouldIgnoreCompositionSelectionChange yet.
