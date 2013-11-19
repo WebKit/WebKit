b539caa6855e9cc0b778044be94ede87b012c830
Manuel Rego Casasnovas <rego@igalia.com>
2013-11-19 09:28:38 +0000

    [CSS Regions] Add performance test for selection https://bugs.webkit.org/show_bug.cgi?id=119230
    Reviewed by Ryosuke Niwa.
    
    Add new performance test for selection in CSS Regions. It checks a
    selection from the first region to the last one, passing through all the
    regions.
    
    Test is skipped for now while implementation of selection in CSS Regions
    is still evolving.
    
    * Layout/RegionsSelection.html: Added.
    * Layout/resources/regions.js:
    (.):
    * Skipped:
    
    
    Canonical link: https://commits.webkit.org/142751@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@159488 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- PerformanceTests/Layout/resources/regions.js
+++ PerformanceTests/Layout/resources/regions.js
@@ -88,5 +88,49 @@
         };
     }
 
+    function mouseMoveToRegionCenter(region) {
+        var rect = region.getBoundingClientRect();
+        eventSender.mouseMoveTo(rect.left + (rect.width / 2), rect.top + (rect.height / 2));
+    }
+
+    function performSelection(regionCount) {
+        var regions = document.getElementsByClassName("region");
+
+        if (window.eventSender) {
+            mouseMoveToRegionCenter(regions[0]);
+            eventSender.mouseDown();
+
+            for (var i = 1; i < regionCount; i++)
+                mouseMoveToRegionCenter(regions[i]);
+
+            eventSender.mouseUp();
+        }
+    }
+
+    function createRegionsSelectionTest(regionCount) {
+        var article = createArticle(regionCount, 1);
+        article.className = "articleInFlow";
+        var regions = createRegions("600px", "auto", regionCount, "auto");
+        document.body.appendChild(article);
+        document.body.appendChild(regions);
+        return {
+            description: "Testing selection with " + regionCount + " regions. Select text from first region to last one passing through all the regions.",
+            run: function() {
+                performSelection(regionCount);
+            },
+            setup: function() {
+                window.getSelection().removeAllRanges();
+            },
+            done: function() {
+                document.body.removeChild(article);
+                document.body.removeChild(regions);
+                templateParagraph = null;
+                templateRegion = null;
+            }
+        };
+    }
+
     window.createRegionsTest = createRegionsTest;
+    window.createRegionsSelectionTest = createRegionsSelectionTest;
+
 })();
