a7a768f24bd7074f99f61cae7909b5dbbf4bd1d2
Filip Pizlo <fpizlo@apple.com>
2013-11-22 20:18:18 +0000

    BytecodeGenerator should align the stack according to native conventions https://bugs.webkit.org/show_bug.cgi?id=124735
    Source/JavaScriptCore:
    
    Reviewed by Mark Lam.
    
    Rolling this back in because it actually fixed fast/dom/gc-attribute-node.html, but
    our infrastructure misleads peole into thinking that fixing a test constitutes
    breaking it.
    
    * bytecompiler/BytecodeGenerator.h:
    (JSC::CallArguments::registerOffset):
    (JSC::CallArguments::argumentCountIncludingThis):
    * bytecompiler/NodesCodegen.cpp:
    (JSC::CallArguments::CallArguments):
    
    LayoutTests:
    
    Reviewed by Mark Lam.
    
    * platform/mac/fast/dom/gc-attribute-node-expected.txt: Removed.
    
    
    
    Canonical link: https://commits.webkit.org/142956@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@159705 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/bytecompiler/NodesCodegen.cpp
+++ Source/JavaScriptCore/bytecompiler/NodesCodegen.cpp
@@ -44,6 +44,7 @@
 #include "RegExpCache.h"
 #include "RegExpObject.h"
 #include "SamplingTool.h"
+#include "StackAlignment.h"
 #include <wtf/Assertions.h>
 #include <wtf/RefCountedLeakCounter.h>
 #include <wtf/Threading.h>
@@ -420,6 +421,7 @@ RegisterID* NewExprNode::emitBytecode(BytecodeGenerator& generator, RegisterID* 
 
 CallArguments::CallArguments(BytecodeGenerator& generator, ArgumentsNode* argumentsNode, unsigned additionalArguments)
     : m_argumentsNode(argumentsNode)
+    , m_padding(0)
 {
     if (generator.shouldEmitProfileHooks())
         m_profileHookRegister = generator.newTemporary();
@@ -435,13 +437,11 @@ CallArguments::CallArguments(BytecodeGenerator& generator, ArgumentsNode* argume
         m_argv[i] = generator.newTemporary();
         ASSERT(static_cast<size_t>(i) == m_argv.size() - 1 || m_argv[i]->index() == m_argv[i + 1]->index() - 1);
     }
-}
-
-inline void CallArguments::newArgument(BytecodeGenerator& generator)
-{
-    RefPtr<RegisterID> tmp = generator.newTemporary();
-    ASSERT(m_argv.isEmpty() || tmp->index() == m_argv.last()->index() + 1); // Calling convention assumes that all arguments are contiguous.
-    m_argv.append(tmp.release());
+    
+    while (stackOffset() % stackAlignmentRegisters()) {
+        m_argv.insert(0, generator.newTemporary());
+        m_padding++;
+    }
 }
 
 // ------------------------------ EvalFunctionCallNode ----------------------------------
