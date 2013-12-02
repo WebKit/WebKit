1b0259463473b3b884096e7d5810840667de9716
Victor Jaquez <vjaquez@igalia.com>
2013-12-02 19:58:32 +0000

    [Gstreamer] update webkitvideosink metadata https://bugs.webkit.org/show_bug.cgi?id=125082
    Reviewed by Philippe Normand.
    
    No new tests, no behavior changes.
    
    * platform/graphics/gstreamer/VideoSinkGStreamer.cpp:
    (webkit_video_sink_class_init):
    
    
    Canonical link: https://commits.webkit.org/143190@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@159947 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/gstreamer/VideoSinkGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/VideoSinkGStreamer.cpp
@@ -377,7 +377,7 @@ static void webkit_video_sink_class_init(WebKitVideoSinkClass* klass)
     GstElementClass* elementClass = GST_ELEMENT_CLASS(klass);
 
     gst_element_class_add_pad_template(elementClass, gst_static_pad_template_get(&s_sinkTemplate));
-    gst_element_class_set_metadata(elementClass, "WebKit video sink", "Sink/Video", "Sends video data from a GStreamer pipeline to a Cairo surface", "Alp Toker <alp@atoker.com>");
+    gst_element_class_set_metadata(elementClass, "WebKit video sink", "Sink/Video", "Sends video data from a GStreamer pipeline to WebKit", "Igalia, Alp Toker <alp@atoker.com>");
 
     g_type_class_add_private(klass, sizeof(WebKitVideoSinkPrivate));
 
