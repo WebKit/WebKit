004a6bcefb23f0148f0daddb2186fa66329b4dc1
Brent Fulgham <bfulgham@apple.com>
2013-12-02 20:44:17 +0000

    Merge r159941.
        2013-12-02  Brent Fulgham  <bfulgham@apple.com>
    
        [Win] WebKit Project doesn't copy resource bundle
        https://bugs.webkit.org/show_bug.cgi?id=125078
    
        Reviewed by Jer Noble.
    
        * WebKit.vcxproj/WebKit/WebKitPostBuild.cmd:
    
    
    
    Canonical link: https://commits.webkit.org/135908.394@safari-537.60-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-537.60-branch@159953 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebKit.vcxproj/WebKit/WebKitPostBuild.cmd
+++ Source/WebKit/WebKit.vcxproj/WebKit/WebKitPostBuild.cmd
@@ -1,5 +1,7 @@
 mkdir 2>NUL "%CONFIGURATIONBUILDDIR%\include\WebKit"
 
+set ResourcesDirectory=%CONFIGURATIONBUILDDIR%\bin32\WebKit.resources
+
 xcopy /y /d "%PROJECTDIR%\..\..\win\WebLocalizableStrings.h" "%CONFIGURATIONBUILDDIR%\include\WebKit"
 xcopy /y /d "%PROJECTDIR%\..\..\win\WebKitGraphics.h" "%CONFIGURATIONBUILDDIR%\include\WebKit"
 xcopy /y /d "%PROJECTDIR%\..\..\win\WebKitCOMAPI.h" "%CONFIGURATIONBUILDDIR%\include\WebKit"
@@ -10,6 +12,10 @@ xcopy /y /d "%CONFIGURATIONBUILDDIR%\include\WebCore\npfunctions.h" "%CONFIGURAT
 xcopy /y /d "%CONFIGURATIONBUILDDIR%\include\WebCore\npruntime.h" "%CONFIGURATIONBUILDDIR%\include\WebKit"
 xcopy /y /d "%CONFIGURATIONBUILDDIR%\include\WebCore\npruntime_internal.h" "%CONFIGURATIONBUILDDIR%\include\WebKit"
 xcopy /y /d "%CONFIGURATIONBUILDDIR%\include\WebCore\nptypes.h" "%CONFIGURATIONBUILDDIR%\include\WebKit"
+
+echo Copying resources...
+mkdir "%ResourcesDirectory%" 2>NUL
+xcopy /y /d "%PROJECTDIR%\..\..\win\WebKit.resources\*" "%ResourcesDirectory%" >NUL
 
 if exist "%WEBKIT_LIBRARIES%\tools\VersionStamper\VersionStamper.exe" "%WEBKIT_LIBRARIES%\tools\VersionStamper\VersionStamper.exe" --verbose "%TARGETPATH%"
 
