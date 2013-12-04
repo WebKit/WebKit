240356580baa980a0351170d90312ba3b446b1fc
Antoine Quint <graouts@apple.com>
2013-12-04 23:20:54 +0000

    Web Inspector: "data detectors" menu on hover for actionable tokens https://bugs.webkit.org/show_bug.cgi?id=124363
    Reviewed by Timothy Hatcher.
    
    Add a new WebInspector.HoverMenu class to display an overlay menu that is presented
    with respect to a target frame that it draws itself around adding a customizable
    action button to its right. The menu uses fade animations as it's presented and
    dismissed and a single delegation method is fired when the button is pressed. Finally,
    just like a popover, it automatically dismisses itself upon scrolling anywhere
    outside of its bounds.
    
    * UserInterface/HoverMenu.css: Added.
    * UserInterface/HoverMenu.js: Added.
    (WebInspector.HoverMenu):
    (WebInspector.HoverMenu.prototype.get element):
    (WebInspector.HoverMenu.prototype.dismiss):
    (WebInspector.HoverMenu.prototype.handleEvent):
    (WebInspector.HoverMenu.prototype._handleClickEvent):
    * UserInterface/Images/HoverMenuButton.png: Added.
    * UserInterface/Images/HoverMenuButton@2x.png: Added.
    * UserInterface/Main.html:
    
    Canonical link: https://commits.webkit.org/143360@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@160134 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebInspectorUI/UserInterface/HoverMenu.js
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2013 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+WebInspector.HoverMenu = function(delegate)
+{
+    WebInspector.Object.call(this);
+
+    this.delegate = delegate;
+
+    this._element = document.createElement("div");
+    this._element.className = WebInspector.HoverMenu.StyleClassName;
+    this._element.addEventListener("transitionend", this, true);
+
+    this._button = this._element.appendChild(document.createElement("img"));
+    this._button.addEventListener("click", this);
+}
+
+WebInspector.HoverMenu.StyleClassName = "hover-menu";
+WebInspector.HoverMenu.VisibleClassName = "visible";
+
+WebInspector.HoverMenu.prototype = {
+    constructor: WebInspector.HoverMenu,
+    __proto__: WebInspector.Object.prototype,
+
+    // Public
+
+    get element()
+    {
+        return this._element;
+    },
+
+    present: function(targetFrame)
+    {
+        var style = this._element.style;
+        style.left = Math.ceil(targetFrame.origin.x) + "px";
+        style.top = Math.ceil(targetFrame.origin.y) + "px";
+        style.width = Math.ceil(targetFrame.size.width) + "px";
+        style.height = Math.ceil(targetFrame.size.height) + "px";
+
+        var element = this._element;
+        document.body.appendChild(element);
+        setTimeout(function() {
+            element.classList.add(WebInspector.HoverMenu.VisibleClassName);
+        });
+
+        window.addEventListener("scroll", this, true);
+    },
+
+    dismiss: function()
+    {
+        if (this._element.parentNode !== document.body)
+            return;
+
+        this._element.classList.remove(WebInspector.HoverMenu.VisibleClassName);
+
+        window.removeEventListener("scroll", this, true);
+    },
+
+    // Protected
+
+    handleEvent: function(event)
+    {
+        switch (event.type) {
+        case "scroll":
+            if (!this._element.contains(event.target))
+                this.dismiss();
+            break;
+        case "click":
+            this._handleClickEvent(event);
+            break;
+        case "transitionend":
+            if (!this._element.classList.contains(WebInspector.HoverMenu.VisibleClassName))
+                this._element.remove();
+            break;
+        }
+    },
+
+    // Private
+
+    _handleClickEvent: function(event)
+    {
+        if (this.delegate && typeof this.delegate.hoverMenuWasActivated === "function")
+            this.delegate.hoverMenuWasActivated(this);
+    }
+}
