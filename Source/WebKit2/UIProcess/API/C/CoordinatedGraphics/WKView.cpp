WebKit/WebKit

commit a877f9eadb861038f8e8998dbe455f92601fd424

[EFL][WK2] Move FullScreenManager logic to WebViewEfl https://bugs.webkit.org/show_bug.cgi?id=125438
Reviewed by Gyuyoung Kim.

This patch fixed build break on EFL port Since r160296.

* PlatformEfl.cmake: Removed WebFullScreenManagerProxyEfl.cpp.
* UIProcess/WebFullScreenManagerProxy.cpp: Removed EFL specific code.
(WebKit::WebFullScreenManagerProxy::WebFullScreenManagerProxy):
* UIProcess/WebFullScreenManagerProxy.h: Ditto.
* UIProcess/efl/WebFullScreenManagerProxyEfl.cpp: Removed file and moved logic to WebViewEfl.
* UIProcess/efl/WebViewEfl.cpp:
(WebKit::WebViewEfl::WebViewEfl):
(WebKit::WebViewEfl::setEwkView): Removed call to setWebView().
(WebKit::WebViewEfl::fullScreenManagerProxyClient):
(WebKit::WebViewEfl::isFullScreen):
(WebKit::WebViewEfl::enterFullScreen):
(WebKit::WebViewEfl::exitFullScreen):
* UIProcess/efl/WebViewEfl.h:

Canonical link: https://commits.webkit.org/143516@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@160301 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/UIProcess/efl/WebViewEfl.cpp
@@ -57,16 +57,13 @@ PassRefPtr<WebView> WebView::create(WebContext* context, WebPageGroup* pageGroup
 WebViewEfl::WebViewEfl(WebContext* context, WebPageGroup* pageGroup)
     : WebView(context, pageGroup)
     , m_ewkView(0)
+    , m_hasRequestedFullScreen(false)
 {
 }
 
 void WebViewEfl::setEwkView(EwkView* ewkView)
 {
     m_ewkView = ewkView;
-
-#if ENABLE(FULLSCREEN_API)
-    m_page->fullScreenManager()->setWebView(ewkView->evasObject());
-#endif
 }
 
 void WebViewEfl::paintToCairoSurface(cairo_surface_t* surface)
@@ -142,5 +139,46 @@ void WebViewEfl::sendMouseEvent(const Evas_Event_Mouse_Move* event)
     ASSERT(event);
     m_page->handleMouseEvent(NativeWebMouseEvent(event, transformFromScene(), m_userViewportTransform.toAffineTransform()));
 }
+
+#if ENABLE(FULLSCREEN_API)
+
+WebFullScreenManagerProxyClient& WebViewEfl::fullScreenManagerProxyClient()
+{
+    return *this;
+}
+
+// WebFullScreenManagerProxyClient
+bool WebViewEfl::isFullScreen()
+{
+    return m_hasRequestedFullScreen;
+}
+
+void WebViewEfl::enterFullScreen()
+{
+    if (!m_ewkView || m_hasRequestedFullScreen)
+        return;
+
+    m_hasRequestedFullScreen = true;
+
+    WebFullScreenManagerProxy* manager = m_page->fullScreenManager();
+    manager->willEnterFullScreen();
+    m_ewkView->enterFullScreen();
+    manager->didEnterFullScreen();
+}
+
+void WebViewEfl::exitFullScreen()
+{
+    if (!m_ewkView || !m_hasRequestedFullScreen)
+        return;
+
+    m_hasRequestedFullScreen = false;
+
+    WebFullScreenManagerProxy* manager = m_page->fullScreenManager();
+    manager->willExitFullScreen();
+    m_ewkView->exitFullScreen();
+    manager->didExitFullScreen();
+}
+
+#endif // ENABLE(FULLSCREEN_API)
 
 } // namespace WebKit

Source/WebKit2/UIProcess/efl/WebViewEfl.h
@@ -26,6 +26,7 @@
 #ifndef WebViewEfl_h
 #define WebViewEfl_h
 
+#include "WebFullScreenManagerProxy.h"
 #include "WebView.h"
 
 class EwkView;
@@ -36,7 +37,11 @@ namespace WebKit {
 class EwkTouchEvent;
 #endif
 
-class WebViewEfl : public WebView {
+class WebViewEfl : public WebView
+#if ENABLE(FULLSCREEN_API)
+    , public WebFullScreenManagerProxyClient
+#endif
+    {
 public:
     void setEwkView(EwkView*);
     EwkView* ewkView() { return m_ewkView; }
@@ -60,8 +65,23 @@ private:
     void updateTextInputState() OVERRIDE;
     void handleDownloadRequest(DownloadProxy*) OVERRIDE;
 
+#if ENABLE(FULLSCREEN_API)
+    WebFullScreenManagerProxyClient& fullScreenManagerProxyClient() OVERRIDE;
+#endif
+
+#if ENABLE(FULLSCREEN_API)
+    // WebFullScreenManagerProxyClient
+    virtual void closeFullScreenManager() OVERRIDE FINAL { }
+    virtual bool isFullScreen() OVERRIDE FINAL;
+    virtual void enterFullScreen() OVERRIDE FINAL;
+    virtual void exitFullScreen() OVERRIDE FINAL;
+    virtual void beganEnterFullScreen(const WebCore::IntRect&, const WebCore::IntRect&) OVERRIDE FINAL { }
+    virtual void beganExitFullScreen(const WebCore::IntRect&, const WebCore::IntRect&) OVERRIDE FINAL { }
+#endif
+
 private:
     EwkView* m_ewkView;
+    bool m_hasRequestedFullScreen;
 
     friend class WebView;
 };

Source/WebKit2/PlatformEfl.cmake
@@ -147,7 +147,6 @@ list(APPEND WebKit2_SOURCES
     UIProcess/efl/ViewClientEfl.cpp
     UIProcess/efl/WebContextEfl.cpp
     UIProcess/efl/WebContextMenuProxyEfl.cpp
-    UIProcess/efl/WebFullScreenManagerProxyEfl.cpp
     UIProcess/efl/WebInspectorProxyEfl.cpp
     UIProcess/efl/WebPageProxyEfl.cpp
     UIProcess/efl/WebPopupItemEfl.cpp

Source/WebKit2/UIProcess/WebFullScreenManagerProxy.cpp
@@ -46,9 +46,6 @@ PassRefPtr<WebFullScreenManagerProxy> WebFullScreenManagerProxy::create(WebPageP
 WebFullScreenManagerProxy::WebFullScreenManagerProxy(WebPageProxy& page, WebFullScreenManagerProxyClient& client)
     : m_page(&page)
     , m_client(&client)
-#if PLATFORM(EFL)
-    , m_hasRequestedFullScreen(false)
-#endif
 {
     m_page->process().addMessageReceiver(Messages::WebFullScreenManagerProxy::messageReceiverName(), m_page->pageID(), this);
 }

Source/WebKit2/UIProcess/WebFullScreenManagerProxy.h
@@ -86,10 +86,6 @@ private:
 
     WebPageProxy* m_page;
     WebFullScreenManagerProxyClient* m_client;
-
-#if PLATFORM(EFL)
-    bool m_hasRequestedFullScreen;
-#endif
 };
 
 } // namespace WebKit

Source/WebKit2/UIProcess/efl/WebFullScreenManagerProxyEfl.cpp
@@ -1,91 +1,0 @@
-/*
- * Copyright (C) 2012 Samsung Electronics
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include "config.h"
-#include "WebFullScreenManagerProxy.h"
-#include "WebFullScreenManagerProxyMessages.h"
-
-#if ENABLE(FULLSCREEN_API)
-
-#include "EwkView.h"
-#include <WebCore/NotImplemented.h>
-
-using namespace WebCore;
-
-namespace WebKit {
-
-void WebFullScreenManagerProxy::invalidate()
-{
-    m_page->process().removeMessageReceiver(Messages::WebFullScreenManagerProxy::messageReceiverName(), m_page->pageID());
-    m_webView = 0;
-}
-
-void WebFullScreenManagerProxy::close()
-{
-    notImplemented();
-}
-
-bool WebFullScreenManagerProxy::isFullScreen()
-{
-    return m_hasRequestedFullScreen;
-}
-
-void WebFullScreenManagerProxy::enterFullScreen()
-{
-    if (!m_webView || m_hasRequestedFullScreen)
-        return;
-
-    m_hasRequestedFullScreen = true;
-
-    willEnterFullScreen();
-    toEwkView(m_webView)->enterFullScreen();
-    didEnterFullScreen();
-}
-
-void WebFullScreenManagerProxy::exitFullScreen()
-{
-    if (!m_webView || !m_hasRequestedFullScreen)
-        return;
-
-    m_hasRequestedFullScreen = false;
-
-    willExitFullScreen();
-    toEwkView(m_webView)->exitFullScreen();
-    didExitFullScreen();
-}
-
-void WebFullScreenManagerProxy::beganEnterFullScreen(const IntRect& /*initialFrame*/, const IntRect& /*finalFrame*/)
-{
-    notImplemented();
-}
-
-void WebFullScreenManagerProxy::beganExitFullScreen(const IntRect& /*initialFrame*/, const IntRect& /*finalFrame*/)
-{
-    notImplemented();
-}
-
-} // namespace WebKit
-
-#endif // ENABLE(FULLSCREEN_API)

commit 782164574d2970595c406b5f27f2267bbf32d226

[EFL][WK2] LayoutTests are broken after r160301 https://bugs.webkit.org/show_bug.cgi?id=125447
Reviewed by Darin Adler.

r160301 moved FullScreenManagerProxyClient logic to WebViewEfl, child class
of CoordinatedGraphics::WebView, because implementations are EFL specific.
However, CoordinatedGraphics::WebView creates WebPageProxy in constructor and
WebPageProxy requires FullScreenManagerProxyClient in constructor.
So, All WK2/Efl based applications got crashed

This patch adds virtual methods for FullScreenManagerProxyClient to CoordinatedGraphics::WebView
so that WebPageProxy can get FullScreenManagerProxyClient instance without
pure viertual methods.

* UIProcess/API/C/CoordinatedGraphics/WKView.cpp:
(WKViewExitFullScreen):
* UIProcess/CoordinatedGraphics/WebView.cpp:
(WebKit::WebView::fullScreenManagerProxyClient):
(WebKit::WebView::requestExitFullScreen):
renamed from exitFullScreen not to conflict with methods of FullScreenManagerProxyClient.
* UIProcess/CoordinatedGraphics/WebView.h:
* UIProcess/efl/WebViewEfl.cpp:
* UIProcess/efl/WebViewEfl.h:


Canonical link: https://commits.webkit.org/143536@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@160321 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/UIProcess/API/C/CoordinatedGraphics/WKView.cpp
@@ -182,7 +182,7 @@ bool WKViewGetShowsAsSource(WKViewRef viewRef)
 bool WKViewExitFullScreen(WKViewRef viewRef)
 {
 #if ENABLE(FULLSCREEN_API)
-    return toImpl(viewRef)->exitFullScreen();
+    return toImpl(viewRef)->requestExitFullScreen();
 #else
     UNUSED_PARAM(viewRef);
     return false;

Source/WebKit2/UIProcess/CoordinatedGraphics/WebView.cpp
@@ -194,13 +194,16 @@ bool WebView::showsAsSource() const
 }
 
 #if ENABLE(FULLSCREEN_API)
-bool WebView::exitFullScreen()
+WebFullScreenManagerProxyClient& WebView::fullScreenManagerProxyClient()
 {
-#if PLATFORM(EFL)
-    // FIXME: Implement this for other platforms.
-    if (!m_page->fullScreenManager()->isFullScreen())
+    return *this;
+}
+
+bool WebView::requestExitFullScreen()
+{
+    if (!isFullScreen())
         return false;
-#endif
+
     m_page->fullScreenManager()->requestExitFullScreen();
     return true;
 }

Source/WebKit2/UIProcess/CoordinatedGraphics/WebView.h
@@ -33,6 +33,7 @@
 #include "DefaultUndoController.h"
 #include "PageClient.h"
 #include "WebContext.h"
+#include "WebFullScreenManagerProxy.h"
 #include "WebGeometry.h"
 #include "WebPageGroup.h"
 #include "WebPageProxy.h"
@@ -46,7 +47,11 @@ class CoordinatedGraphicsScene;
 
 namespace WebKit {
 
-class WebView : public API::TypedObject<API::Object::Type::View>, public PageClient {
+class WebView : public API::TypedObject<API::Object::Type::View>, public PageClient
+#if ENABLE(FULLSCREEN_API)
+    , public WebFullScreenManagerProxyClient
+#endif
+    {
 public:
     virtual ~WebView();
 
@@ -93,7 +98,7 @@ public:
     bool showsAsSource() const;
 
 #if ENABLE(FULLSCREEN_API)
-    bool exitFullScreen();
+    bool requestExitFullScreen();
 #endif
 
     void findZoomableAreaForPoint(const WebCore::IntPoint&, const WebCore::IntSize&);
@@ -188,6 +193,18 @@ protected:
     virtual void enterAcceleratedCompositingMode(const LayerTreeContext&) OVERRIDE;
     virtual void exitAcceleratedCompositingMode() OVERRIDE;
     virtual void updateAcceleratedCompositingMode(const LayerTreeContext&) OVERRIDE;
+
+#if ENABLE(FULLSCREEN_API)
+    WebFullScreenManagerProxyClient& fullScreenManagerProxyClient() OVERRIDE;
+
+    // WebFullScreenManagerProxyClient
+    virtual void closeFullScreenManager() OVERRIDE { }
+    virtual bool isFullScreen() OVERRIDE { return false; }
+    virtual void enterFullScreen() OVERRIDE { }
+    virtual void exitFullScreen() OVERRIDE { }
+    virtual void beganEnterFullScreen(const WebCore::IntRect&, const WebCore::IntRect&) OVERRIDE { }
+    virtual void beganExitFullScreen(const WebCore::IntRect&, const WebCore::IntRect&) OVERRIDE { }
+#endif
 
 protected:
     WebViewClient m_client;

Source/WebKit2/UIProcess/efl/WebViewEfl.cpp
@@ -142,11 +142,6 @@ void WebViewEfl::sendMouseEvent(const Evas_Event_Mouse_Move* event)
 
 #if ENABLE(FULLSCREEN_API)
 
-WebFullScreenManagerProxyClient& WebViewEfl::fullScreenManagerProxyClient()
-{
-    return *this;
-}
-
 // WebFullScreenManagerProxyClient
 bool WebViewEfl::isFullScreen()
 {

Source/WebKit2/UIProcess/efl/WebViewEfl.h
@@ -26,7 +26,6 @@
 #ifndef WebViewEfl_h
 #define WebViewEfl_h
 
-#include "WebFullScreenManagerProxy.h"
 #include "WebView.h"
 
 class EwkView;
@@ -38,9 +37,6 @@ class EwkTouchEvent;
 #endif
 
 class WebViewEfl : public WebView
-#if ENABLE(FULLSCREEN_API)
-    , public WebFullScreenManagerProxyClient
-#endif
     {
 public:
     void setEwkView(EwkView*);
@@ -64,10 +60,6 @@ private:
     PassRefPtr<WebContextMenuProxy> createContextMenuProxy(WebPageProxy*) OVERRIDE;
     void updateTextInputState() OVERRIDE;
     void handleDownloadRequest(DownloadProxy*) OVERRIDE;
-
-#if ENABLE(FULLSCREEN_API)
-    WebFullScreenManagerProxyClient& fullScreenManagerProxyClient() OVERRIDE;
-#endif
 
 #if ENABLE(FULLSCREEN_API)
     // WebFullScreenManagerProxyClient