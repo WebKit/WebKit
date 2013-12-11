fe25e8864b0dddc9ddaa83721034d888d9086baf
Lucas Forschler <lforschler@apple.com>
2013-12-11 11:34:08 +0000

    Merged r157279.  <rdar://problem/15560450>
    Canonical link: https://commits.webkit.org/135908.521@safari-537.74-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-537.74-branch@160420 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/mac/WebView/WebView.mm
+++ Source/WebKit/mac/WebView/WebView.mm
@@ -6447,6 +6447,8 @@ bool LayerFlushController::flushLayers()
     if (viewsNeedDisplay)
         return false;
 
+    [m_webView _viewWillDrawInternal];
+
     if ([m_webView _flushCompositingChanges]) {
         // AppKit may have disabled screen updates, thinking an upcoming window flush will re-enable them.
         // In case setNeedsDisplayInRect() has prevented the window from needing to be flushed, re-enable screen
@@ -6456,10 +6458,6 @@ bool LayerFlushController::flushLayers()
 
         return true;
     }
-
-    // Since the WebView does not need display, -viewWillDraw will not be called. Perform pending layout now,
-    // so that the layers draw with up-to-date layout. 
-    [m_webView _viewWillDrawInternal];
 
     return false;
 }
