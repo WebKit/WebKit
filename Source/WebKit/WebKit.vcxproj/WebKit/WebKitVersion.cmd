39d6d5bdb424d71ab5a49efcb5b734d971ce5847
Alex Christensen <achristensen@webkit.org>
2013-12-11 23:38:23 +0000

    [Win] Build fix after r159768. https://bugs.webkit.org/show_bug.cgi?id=125467
    Patch by Alex Christensen <achristensen@webkit.org> on 2013-12-11
    Reviewed by Brent Fulgham.
    
    * WebKit.vcxproj/WebKit/WebKitVersion.cmd:
    Set PATH environment variable to include cygwin bin directory.
    
    Canonical link: https://commits.webkit.org/143654@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@160465 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebKit.vcxproj/WebKit/WebKitVersion.cmd
+++ Source/WebKit/WebKit.vcxproj/WebKit/WebKitVersion.cmd
@@ -2,5 +2,6 @@ set WEBKITVERSIONSCRIPT=%PROJECTDIR%..\..\scripts\generate-webkitversion.pl
 set WEBKITVERSIONCONFIG=%PROJECTDIR%..\..\mac\Configurations\Version.xcconfig
 set WEBKITVERSIONDIR=%CONFIGURATIONBUILDDIR%\include\WebKit
 set WEBKITVERSIONFILE=%WEBKITVERSIONDIR%\WebKitVersion.h
+set PATH=%SystemDrive%\cygwin\bin;%PATH%
 
 bash -c 'perl "%WEBKITVERSIONSCRIPT%" --config "%WEBKITVERSIONCONFIG%"  --outputDir "%WEBKITVERSIONDIR%"'
