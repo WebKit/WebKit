WebKit/WebKit

commit dd7c79cac5c2d239a823ef93b090f3ecd8c18acf

Use std::array when computing MD5 checksum https://bugs.webkit.org/show_bug.cgi?id=125509
Patch by Laszlo Vidacs <lac@inf.u-szeged.hu> on 2013-12-10
Reviewed by Anders Carlsson.

Source/WebCore:

Use MD5::Digest type and MD5::hashSize when computing MD5 checksum.

* platform/network/curl/CurlCacheEntry.cpp:
(WebCore::CurlCacheEntry::generateBaseFilename):

Source/WTF:

Added MD5::Digest type and MD5::hashSize for computing MD5 checksum.

* wtf/MD5.cpp:
(WTF::MD5::checksum):
* wtf/MD5.h:

Tools:

Use MD5::Digest type and MD5::hashSize when computing MD5 checksum.

* DumpRenderTree/blackberry/PixelDumpSupportBlackBerry.cpp:
(computeMD5HashStringForBitmapContext):
* DumpRenderTree/cairo/PixelDumpSupportCairo.cpp:
(computeMD5HashStringForBitmapContext):
* TestWebKitAPI/Tests/WTF/MD5.cpp:
(TestWebKitAPI::expectMD5):
* WebKitTestRunner/cairo/TestInvocationCairo.cpp:
(WTR::computeMD5HashStringForCairoSurface):
* WebKitTestRunner/cg/TestInvocationCG.cpp:
(WTR::computeMD5HashStringForContext):

Canonical link: https://commits.webkit.org/143592@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@160386 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/network/curl/CurlCacheEntry.cpp
@@ -206,11 +206,11 @@ void CurlCacheEntry::generateBaseFilename(const CString& url)
     MD5 md5;
     md5.addBytes(reinterpret_cast<const uint8_t*>(url.data()), url.length());
 
-    Vector<uint8_t, 16> sum;
+    MD5::Digest sum;
     md5.checksum(sum);
     uint8_t* rawdata = sum.data();
 
-    for (unsigned i = 0; i < 16; i++)
+    for (size_t i = 0; i < MD5::hasSize; i++)
         appendByteAsHex(rawdata[i], m_basename, Lowercase);
 }
 

Source/WTF/wtf/MD5.cpp
@@ -219,7 +219,7 @@ void MD5::addBytes(const uint8_t* input, size_t length)
     memcpy(m_in, buf, length);
 }
 
-void MD5::checksum(Vector<uint8_t, 16>& digest)
+void MD5::checksum(Digest& digest)
 {
     // Compute number of bytes mod 64
     unsigned count = (m_bits[0] >> 3) & 0x3F;
@@ -256,9 +256,9 @@ void MD5::checksum(Vector<uint8_t, 16>& digest)
     reverseBytes(reinterpret_cast<uint8_t*>(m_buf), 4);
 
     // Now, m_buf contains checksum result.
-    if (!digest.isEmpty())
-        digest.clear();
-    digest.append(reinterpret_cast<uint8_t*>(m_buf), 16);
+    uint8_t* mBufUInt8 = reinterpret_cast<uint8_t*>(m_buf);
+    for (size_t i = 0; i < hashSize; ++i)
+        digest[i] = mBufUInt8[i];
 
     // In case it's sensitive
     memset(m_buf, 0, sizeof(m_buf));

Source/WTF/wtf/MD5.h
@@ -31,6 +31,7 @@
 #ifndef WTF_MD5_h
 #define WTF_MD5_h
 
+#include <array>
 #include <wtf/Vector.h>
 
 namespace WTF {
@@ -45,8 +46,14 @@ public:
     }
     WTF_EXPORT_PRIVATE void addBytes(const uint8_t* input, size_t length);
 
+    // Size of the SHA1 hash
+    WTF_EXPORT_PRIVATE static const size_t hashSize = 16;
+
+    // type for computing MD5 hash
+    typedef std::array<uint8_t, hashSize> Digest;
+
     // checksum has a side effect of resetting the state of the object.
-    WTF_EXPORT_PRIVATE void checksum(Vector<uint8_t, 16>&);
+    WTF_EXPORT_PRIVATE void checksum(Digest&);
 
 private:
     uint32_t m_buf[4];

Tools/DumpRenderTree/blackberry/PixelDumpSupportBlackBerry.cpp
@@ -84,11 +84,11 @@ void computeMD5HashStringForBitmapContext(BitmapContext* context, char hashStrin
         pixelData += bytesPerRow;
     }
 
-    Vector<uint8_t, 16> hash;
+    MD5::Digest hash;
     md5.checksum(hash);
 
     hashString[0] = '\0';
-    for (int i = 0; i < 16; ++i)
+    for (size_t i = 0; i < MD5::hashSize; ++i)
         snprintf(hashString, 33, "%s%02x", hashString, hash[i]);
 }
 

Tools/DumpRenderTree/cairo/PixelDumpSupportCairo.cpp
@@ -78,7 +78,7 @@ void computeMD5HashStringForBitmapContext(BitmapContext* context, char hashStrin
         md5Context.addBytes(bitmapData, 4 * pixelsWide);
         bitmapData += bytesPerRow;
     }
-    Vector<uint8_t, 16> hash;
+    MD5::Digest hash;
     md5Context.checksum(hash);
 
     snprintf(hashString, 33, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",

Tools/TestWebKitAPI/Tests/WTF/MD5.cpp
@@ -21,12 +21,12 @@ static void expectMD5(CString input, CString expected)
 {
     MD5 md5;
     md5.addBytes(reinterpret_cast<const uint8_t*>(input.data()), input.length());
-    Vector<uint8_t, 16> digest;
+    MD5::Digest digest;
     md5.checksum(digest);
     char* buf = 0;
     CString actual = CString::newUninitialized(32, buf);
-    for (size_t i = 0; i < 16; i++, buf += 2)
-        snprintf(buf, 3, "%02x", digest.at(i));
+    for (size_t i = 0; i < MD5::hashSize; i++, buf += 2)
+        snprintf(buf, 3, "%02x", digest[i]);
 
     ASSERT_EQ(expected.length(), actual.length());
     ASSERT_STREQ(expected.data(), actual.data());

Tools/WebKitTestRunner/cairo/TestInvocationCairo.cpp
@@ -55,7 +55,7 @@ void computeMD5HashStringForCairoSurface(cairo_surface_t* surface, char hashStri
         md5Context.addBytes(bitmapData, 4 * pixelsWide);
         bitmapData += bytesPerRow;
     }
-    Vector<uint8_t, 16> hash;
+    MD5::Digest hash;
     md5Context.checksum(hash);
 
     snprintf(hashString, 33, "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",

Tools/WebKitTestRunner/cg/TestInvocationCG.cpp
@@ -99,11 +99,11 @@ void computeMD5HashStringForContext(CGContextRef bitmapContext, char hashString[
         }
     }
 
-    Vector<uint8_t, 16> hash;
+    MD5::Digest hash;
     md5.checksum(hash);
-    
+
     hashString[0] = '\0';
-    for (int i = 0; i < 16; i++)
+    for (size_t i = 0; i < MD5::hashSize; i++)
         snprintf(hashString, 33, "%s%02x", hashString, hash[i]);
 }
 

commit 6b6a634c28066ff9e84af388d70ee83fcdde31ad

[CURL] Build fails after r160386. https://bugs.webkit.org/show_bug.cgi?id=125571
Patch by Tamas Gergely <tgergely.u-szeged@partner.samsung.com> on 2013-12-11
Reviewed by Zoltan Herczeg.

Need no new tests.

Fix a typo in commit r160386 that prevents building with curl.

* platform/network/curl/CurlCacheEntry.cpp:
(WebCore::CurlCacheEntry::generateBaseFilename):

Canonical link: https://commits.webkit.org/143622@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@160430 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/network/curl/CurlCacheEntry.cpp
@@ -210,7 +210,7 @@ void CurlCacheEntry::generateBaseFilename(const CString& url)
     md5.checksum(sum);
     uint8_t* rawdata = sum.data();
 
-    for (size_t i = 0; i < MD5::hasSize; i++)
+    for (size_t i = 0; i < MD5::hashSize; i++)
         appendByteAsHex(rawdata[i], m_basename, Lowercase);
 }
 