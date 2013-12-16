WebKit/WebKit

commit 1d0456085092707ecbccd818a8a46d8fc898c384

check-webkit-style should check for extra newlines at EOF https://bugs.webkit.org/show_bug.cgi?id=125424
Patch by Brian J. Burg <burg@cs.washington.edu> on 2013-12-09
Reviewed by Darin Adler.

Report a style violation if extraneous newlines are added
to the end of a C++ file. There should only be one newline at EOF.

* Scripts/webkitpy/style/checkers/cpp.py:
(check_for_missing_new_line_at_eof): Renamed from check_for_new_line_at_eof.
(check_for_extra_new_line_at_eof): Added.
(_process_lines): Added new check and renamed existing EOF check.
* Scripts/webkitpy/style/checkers/cpp_unittest.py:
(CppStyleTest.test_extra_newlines_at_eof): Added.
(CppStyleTest.test_extra_newlines_at_eof.do_test): Added.

Canonical link: https://commits.webkit.org/143534@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@160319 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/webkitpy/style/checkers/cpp.py
@@ -957,8 +957,8 @@ def check_for_unicode_replacement_characters(lines, error):
                   'Line contains invalid UTF-8 (or Unicode replacement character).')
 
 
-def check_for_new_line_at_eof(lines, error):
-    """Logs an error if there is no newline char at the end of the file.
+def check_for_missing_new_line_at_eof(lines, error):
+    """Logs an error if there is not a newline character at the end of the file.
 
     Args:
       lines: An array of strings, each representing a line of the file.
@@ -972,6 +972,20 @@ def check_for_new_line_at_eof(lines, error):
     if len(lines) < 3 or lines[-2]:
         error(len(lines) - 2, 'whitespace/ending_newline', 5,
               'Could not find a newline character at the end of the file.')
+
+
+def check_for_extra_new_line_at_eof(lines, error):
+    """Logs an error if there is not a single newline at the end of the file.
+
+    Args:
+      lines: An array of strings, each representing a line of the file.
+      error: The function to call with any errors found.
+    """
+    # The array lines() was created by adding two newlines to the
+    # original file (go figure), then splitting on \n.
+    if len(lines) > 3 and not lines[-3]:
+        error(len(lines) - 2, 'whitespace/ending_newline', 5,
+              'There was more than one newline at the end of the file.')
 
 
 def check_for_multiline_comments_and_strings(clean_lines, line_number, error):
@@ -3635,7 +3649,8 @@ def _process_lines(filename, file_extension, lines, error, min_confidence):
     # lines rather than "cleaned" lines.
     check_for_unicode_replacement_characters(lines, error)
 
-    check_for_new_line_at_eof(lines, error)
+    check_for_missing_new_line_at_eof(lines, error)
+    check_for_extra_new_line_at_eof(lines, error)
 
 
 class CppChecker(object):

Tools/Scripts/webkitpy/style/checkers/cpp_unittest.py
@@ -1937,6 +1937,23 @@ class CppStyleTest(CppStyleTestBase):
         do_test(self, '// Newline\n// at EOF\n', False)
         do_test(self, '// No newline\n// at EOF', True)
 
+    def test_extra_newlines_at_eof(self):
+        def do_test(self, data, too_many_newlines):
+            error_collector = ErrorCollector(self.assertTrue)
+            self.process_file_data('foo.cpp', 'cpp', data.split('\n'),
+                                   error_collector)
+            # The warning appears only once.
+            self.assertEqual(
+                int(too_many_newlines),
+                error_collector.results().count(
+                    'There was more than one newline at the end of the file.'
+                    '  [whitespace/ending_newline] [5]'))
+
+        do_test(self, '// No Newline\n// at EOF', False)
+        do_test(self, '// One Newline\n// at EOF\n', False)
+        do_test(self, '// Two Newlines\n// at EOF\n\n', True)
+        do_test(self, '// Three Newlines\n// at EOF\n\n\n', True)
+
     def test_invalid_utf8(self):
         def do_test(self, raw_bytes, has_invalid_utf8):
             error_collector = ErrorCollector(self.assertTrue)

commit 5a1ff678013ff793a3316d2fd38edc4615d8118d

Resolve inconsistant style warning caused by r160319. https://bugs.webkit.org/show_bug.cgi?id=125772
Patch by Gergo Balogh <geryxyz@inf.u-szeged.hu> on 2013-12-16
Reviewed by Darin Adler.

* Scripts/webkitpy/style/checkers/cpp.py:
(check_for_extra_new_line_at_eof):
* Scripts/webkitpy/style/checkers/cpp_unittest.py:
(CppStyleTest.test_newline_at_eof):
(CppStyleTest.test_extra_newlines_at_eof):

Canonical link: https://commits.webkit.org/143809@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@160647 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/webkitpy/style/checkers/cpp.py
@@ -983,9 +983,14 @@ def check_for_extra_new_line_at_eof(lines, error):
     """
     # The array lines() was created by adding two newlines to the
     # original file (go figure), then splitting on \n.
-    if len(lines) > 3 and not lines[-3]:
-        error(len(lines) - 2, 'whitespace/ending_newline', 5,
-              'There was more than one newline at the end of the file.')
+    # len(lines) < 3 means that the original file contain one or less lines,
+    # so there is no way to be 'more then one newline at the end'.
+    # The case when the -2. line is non-empty should addressed in the
+    # check_for_missing_new_line_at_eof so it can be ignored here.
+    if len(lines) > 3:
+        if not lines[-2] and not lines[-3]:
+            error(len(lines) - 2, 'whitespace/ending_newline', 5,
+                  'There was more than one newline at the end of the file.')
 
 
 def check_for_multiline_comments_and_strings(clean_lines, line_number, error):

Tools/Scripts/webkitpy/style/checkers/cpp_unittest.py
@@ -1936,7 +1936,9 @@ class CppStyleTest(CppStyleTestBase):
                     '  [whitespace/ending_newline] [5]'))
 
         do_test(self, '// Newline\n// at EOF\n', False)
+        do_test(self, '// Newline2\n\n// at EOF\n', False)
         do_test(self, '// No newline\n// at EOF', True)
+        do_test(self, '// No newline2\n\n// at EOF', True)
 
     def test_extra_newlines_at_eof(self):
         def do_test(self, data, too_many_newlines):
@@ -1951,7 +1953,9 @@ class CppStyleTest(CppStyleTestBase):
                     '  [whitespace/ending_newline] [5]'))
 
         do_test(self, '// No Newline\n// at EOF', False)
+        do_test(self, '// No Newline2\n\n// at EOF', False)
         do_test(self, '// One Newline\n// at EOF\n', False)
+        do_test(self, '// One Newline2\n\n// at EOF\n', False)
         do_test(self, '// Two Newlines\n// at EOF\n\n', True)
         do_test(self, '// Three Newlines\n// at EOF\n\n\n', True)
 