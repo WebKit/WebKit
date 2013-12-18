aca7baacdd6f4fb9fbcea556492ebb18d2d05074
Alex Christensen <achristensen@webkit.org>
2013-12-18 23:11:02 +0000

    [WinCairo] Preparation for GStreamer on Windows. https://bugs.webkit.org/show_bug.cgi?id=125946
    Patch by Alex Christensen <achristensen@webkit.org> on 2013-12-18
    Reviewed by Brent Fulgham.
    
    Source/WebCore:
    
    * WebCore.vcxproj/WebCore.vcxproj:
    Use new GStreamer property sheets for WinCairo.
    * WebCore.vcxproj/WebCoreCairo.props:
    Include GStreamer directory.
    
    Source/WebKit:
    
    * WebKit.vcxproj/WebKit/WebKit.vcxproj:
    Use new GStreamer property sheets for WinCairo.
    * WebKit.vcxproj/WebKitExportGenerator/WebKitExports.def.in:
    Fixed 64-bit symbols, added GStreamer symbol.
    
    Source/WTF:
    
    * WTF.vcxproj/WTF.vcxproj:
    Use new GStreamer property sheets for WinCairo.
    
    Tools:
    
    * WinLauncher/WinLauncher.vcxproj/WinLauncherLib.vcxproj:
    Use new GStreamer property sheets for WinCairo.
    * win/DLLLauncher/DLLLauncherMain.cpp:
    (modifyPath):
    (wWinMain):
    Prepend GStreamer bin directory to path if it exists.
    
    WebKitLibraries:
    
    * win/tools/vsprops/FeatureDefinesCairo.props:
    Added ENABLE_MEDIA_CONTROLS_SCRIPT and ENABLE_VIDEO_TRACK for DerivedSources.make.
    * win/tools/vsprops/GStreamer32.props: Added.
    * win/tools/vsprops/GStreamer64.props: Added.
    * win/tools/vsprops/GStreamerCommon.props: Added.
    
    Canonical link: https://commits.webkit.org/143945@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@160798 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/win/DLLLauncher/DLLLauncherMain.cpp
+++ Tools/win/DLLLauncher/DLLLauncherMain.cpp
@@ -130,6 +130,19 @@ static bool directoryExists(const wstring& path)
 
 static bool modifyPath(const wstring& programName)
 {
+#ifdef WIN_CAIRO
+
+#if defined(_M_X64)
+    wstring path = copyEnvironmentVariable(L"GSTREAMER_1_0_ROOT_X86_64") + L"bin";
+#else
+    wstring path = copyEnvironmentVariable(L"GSTREAMER_1_0_ROOT_X86") + L"bin";
+#endif
+    if (directoryExists(path))
+        prependPath(path);
+    return true;
+
+#else
+
 #if defined(_M_X64)
     static const wstring pathPrefix = L"C:\\Program Files\\Common Files\\Apple\\Apple Application Support";
 #else
@@ -146,6 +159,7 @@ static bool modifyPath(const wstring& programName)
 
     fatalError(programName, L"Failed to modify PATH environment variable.");
     return false;
+#endif
 }
 
 static wstring getLastErrorString(HRESULT hr)
@@ -180,10 +194,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpstrCm
 
     wstring programName = ::PathFindFileNameW(exePath);
 
-#ifndef WIN_CAIRO
     if (!modifyPath(programName))
         return 1;
-#endif
 
     // Load our corresponding DLL.
     wstring dllName = programName + L".dll";
