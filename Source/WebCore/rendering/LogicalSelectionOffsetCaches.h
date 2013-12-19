724a2654caf38151573b6a1df36b8b0cfc731339
Ryosuke Niwa <rniwa@webkit.org>
2013-12-19 03:09:56 +0000

    Crash in WebCore::LogicalSelectionOffsetCaches::LogicalSelectionOffsetCaches https://bugs.webkit.org/show_bug.cgi?id=125970
    Reviewed by Antti Koivisto.
    
    Source/WebCore:
    
    The bug was caused by containingBlockForAbsolutePosition returning a non-RenderBlock render object.
    Fixed the bug by obtaining its containg block.
    
    Also changed the return type of containingBlockForFixedPosition, containingBlockForAbsolutePosition,
    containingBlockForObjectInFlow from RenderElement to RenderBlock as all callers of these functions
    had assumed the return value to be an instance of RenderBlock.
    
    Test: svg/text/select-text-inside-non-static-position.html
    
    * rendering/LogicalSelectionOffsetCaches.h:
    (WebCore::containingBlockForFixedPosition):
    (WebCore::containingBlockForAbsolutePosition):
    (WebCore::containingBlockForObjectInFlow):
    (WebCore::LogicalSelectionOffsetCaches::LogicalSelectionOffsetCaches):
    
    LayoutTests:
    
    Added a regression test.
    
    * svg/text/select-text-inside-non-static-position-expected.txt: Added.
    * svg/text/select-text-inside-non-static-position.html: Added.
    
    
    Canonical link: https://commits.webkit.org/143964@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@160819 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/LogicalSelectionOffsetCaches.h
+++ Source/WebCore/rendering/LogicalSelectionOffsetCaches.h
@@ -40,16 +40,16 @@ static inline bool isNonRenderBlockInline(RenderElement& object)
     return (object.isInline() && !object.isReplaced()) || !object.isRenderBlock();
 }
 
-static inline RenderElement* containingBlockForFixedPosition(RenderElement* parent)
+static inline RenderBlock* containingBlockForFixedPosition(RenderElement* parent)
 {
     RenderElement* object = parent;
     while (object && !object->canContainFixedPositionObjects())
         object = object->parent();
     ASSERT(!object || !object->isAnonymousBlock());
-    return object;
+    return toRenderBlock(object);
 }
 
-static inline RenderElement* containingBlockForAbsolutePosition(RenderElement* parent)
+static inline RenderBlock* containingBlockForAbsolutePosition(RenderElement* parent)
 {
     RenderElement* object = parent;
     while (object && !isContainingBlockCandidateForAbsolutelyPositionedObject(*object))
@@ -59,21 +59,21 @@ static inline RenderElement* containingBlockForAbsolutePosition(RenderElement* p
     // not the inline itself, to avoid having a positioned objects list in all RenderInlines
     // and use RenderBlock* as RenderElement::containingBlock's return type.
     // Use RenderBlock::container() to obtain the inline.
-    if (object && object->isRenderInline())
+    if (object && !object->isRenderBlock())
         object = object->containingBlock();
 
     while (object && object->isAnonymousBlock())
         object = object->containingBlock();
 
-    return object;
+    return toRenderBlock(object);
 }
 
-static inline RenderElement* containingBlockForObjectInFlow(RenderElement* parent)
+static inline RenderBlock* containingBlockForObjectInFlow(RenderElement* parent)
 {
     RenderElement* object = parent;
     while (object && isNonRenderBlockInline(*object))
         object = object->parent();
-    return object;
+    return toRenderBlock(object);
 }
 
 class LogicalSelectionOffsetCaches {
@@ -143,9 +143,9 @@ public:
         auto parent = rootBlock.parent();
 
         // LogicalSelectionOffsetCaches should not be used on an orphaned tree.
-        m_containingBlockForFixedPosition.setBlock(toRenderBlock(containingBlockForFixedPosition(parent)), 0);
-        m_containingBlockForAbsolutePosition.setBlock(toRenderBlock(containingBlockForAbsolutePosition(parent)), 0);
-        m_containingBlockForInflowPosition.setBlock(toRenderBlock(containingBlockForObjectInFlow(parent)), 0);
+        m_containingBlockForFixedPosition.setBlock(containingBlockForFixedPosition(parent), 0);
+        m_containingBlockForAbsolutePosition.setBlock(containingBlockForAbsolutePosition(parent), 0);
+        m_containingBlockForInflowPosition.setBlock(containingBlockForObjectInFlow(parent), 0);
     }
 
     LogicalSelectionOffsetCaches(RenderBlock& block, const LogicalSelectionOffsetCaches& cache)
