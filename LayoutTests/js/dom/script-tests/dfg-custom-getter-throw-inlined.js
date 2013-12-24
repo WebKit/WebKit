9e5275c2d7cc0822a69e1cd435077b89583850ac
Ryosuke Niwa <rniwa@webkit.org>
2013-12-24 08:05:56 +0000

    XMLHttpRequest: status and statusText throw DOM Exception 11 when the state is UNSENT or OPENED. https://bugs.webkit.org/show_bug.cgi?id=45994
    Reviewed by Alexey Proskuryakov.
    
    Source/WebCore:
    
    Merged https://chromium.googlesource.com/chromium/blink/+/23c90460de16e04c5aba7ed942fba76cb79fdb9b.
    
    Latest XHR spec says that XHR should return 0 and an empty string when it's in UNSENT or OPENED state
    or error flag is set: http://www.w3.org/TR/2012/WD-XMLHttpRequest-20121206/#the-status-attribute
    
    * xml/XMLHttpRequest.cpp:
    (WebCore::XMLHttpRequest::status):
    (WebCore::XMLHttpRequest::statusText):
    * xml/XMLHttpRequest.h:
    * xml/XMLHttpRequest.idl:
    
    LayoutTests:
    
    Rebaselined and fixed tests as done in https://chromium.googlesource.com/chromium/blink/+/23c90460de16e04c5aba7ed942fba76cb79fdb9b.
    
    * http/tests/xmlhttprequest/exceptions-expected.txt:
    * http/tests/xmlhttprequest/exceptions.html:
    * http/tests/xmlhttprequest/status-after-abort-expected.txt:
    * http/tests/xmlhttprequest/zero-length-response-expected.txt:
    
    * js/dom/dfg-custom-getter-throw-expected.txt:
    * js/dom/script-tests/dfg-custom-getter-throw-inlined.js: Fixed the test.
    (foo): Use responseText instead of status so that it throws when responseType is "arraybuffer".
    (bar): Set responseType to "arraybuffer" to cause an exception to be thrown.
    * js/dom/script-tests/dfg-custom-getter-throw.js: Ditto.
    (foo):
    (bar):
    
    Canonical link: https://commits.webkit.org/144145@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@161051 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/js/dom/script-tests/dfg-custom-getter-throw-inlined.js
+++ LayoutTests/js/dom/script-tests/dfg-custom-getter-throw-inlined.js
@@ -3,17 +3,18 @@ description(
 );
 
 function foo(x) {
-    return x.status;
+    return x.responseText;
 }
 
 function baz(x) {
     return foo(x);
 }
 
-function bar(doOpen) {
+function bar(binary) {
     var x = new XMLHttpRequest();
-    if (doOpen)
-        x.open("GET", "http://foo.bar.com/");
+    x.open("GET", "http://foo.bar.com/");
+    if (binary)
+        x.responseType = "arraybuffer";
     try {
         return "Returned result: " + baz(x);
     } catch (e) {
@@ -25,7 +26,7 @@ noInline(baz);
 silentTestPass = true;
 
 for (var i = 0; i < 200; i = dfgIncrement({f:baz, i:i + 1, n:50})) {
-    shouldBe("bar(i >= 100)", i >= 100 ? "\"Threw exception: Error: InvalidStateError: DOM Exception 11\"" : "\"Returned result: 0\"");
+    shouldBe("bar(i >= 100)", i >= 100 ? "\"Threw exception: Error: InvalidStateError: DOM Exception 11\"" : "\"Returned result: \"");
 }
 
 
