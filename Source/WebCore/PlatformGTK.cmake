19cb9de9405ba2bcee7e405b6da9fc5ffbbd59b8
Martin Robinson <mrobinson@webkit.org>
2014-01-04 17:44:28 +0000

    [GTK] [CMake] Fix the video and audio build https://bugs.webkit.org/show_bug.cgi?id=126464
    Reviewed by Philippe Normand.
    
    .:
    
    * Source/cmake/OptionsGTK.cmake: GStreamer files look for USE(GSTREAMER) and USE(WEBAUDIO_GSTREAMER),
    so define those when appropriate. Web audio is one by default in autotools, so turn it on for cmake
    as well.
    
    Source/WebCore:
    
    * PlatformGTK.cmake: Complete the audio and video source lists.
    
    Tools:
    
    * GtkLauncher/CMakeLists.txt: Add GStreamer includes and libraries, since GtkLauncher uses it.
    
    Canonical link: https://commits.webkit.org/144378@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@161310 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/PlatformGTK.cmake
+++ Source/WebCore/PlatformGTK.cmake
@@ -67,10 +67,21 @@ list(APPEND WebCore_SOURCES
     platform/graphics/freetype/GlyphPageTreeNodeFreeType.cpp
     platform/graphics/freetype/SimpleFontDataFreeType.cpp
 
+    platform/graphics/gstreamer/AudioTrackPrivateGStreamer.cpp
     platform/graphics/gstreamer/GRefPtrGStreamer.cpp
     platform/graphics/gstreamer/GStreamerUtilities.cpp
     platform/graphics/gstreamer/ImageGStreamerCairo.cpp
+    platform/graphics/gstreamer/InbandTextTrackPrivateGStreamer.cpp
+    platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+    platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp
+    platform/graphics/gstreamer/MediaSourceGStreamer.cpp
+    platform/graphics/gstreamer/SourceBufferPrivateGStreamer.cpp
+    platform/graphics/gstreamer/TextCombinerGStreamer.cpp
+    platform/graphics/gstreamer/TextSinkGStreamer.cpp
+    platform/graphics/gstreamer/TrackPrivateBaseGStreamer.cpp
     platform/graphics/gstreamer/VideoSinkGStreamer.cpp
+    platform/graphics/gstreamer/VideoTrackPrivateGStreamer.cpp
+    platform/graphics/gstreamer/WebKitMediaSourceGStreamer.cpp
     platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
 
     platform/graphics/harfbuzz/HarfBuzzFace.cpp
@@ -154,9 +165,6 @@ list(APPEND WebCorePlatformGTK_SOURCES
     platform/graphics/freetype/FontPlatformDataFreeType.cpp
 
     platform/graphics/glx/GLContextGLX.cpp
-
-    platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
-    platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp
 
     platform/graphics/gtk/ColorGtk.cpp
     platform/graphics/gtk/GdkCairoUtilities.cpp
