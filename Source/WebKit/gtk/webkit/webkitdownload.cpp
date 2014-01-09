b1cb0ad3f867220c375cf48a6204df4529b9c628
Claudio Saavedra <csaavedra@igalia.com>
2014-01-09 02:14:46 +0000

    [GTK] Add xdg.origin.url extended attribute to downloads https://bugs.webkit.org/show_bug.cgi?id=126638
    Reviewed by Carlos Garcia Campos.
    
    This is proposed by fdo in
    http://www.freedesktop.org/wiki/CommonExtendedAttributes/
    
    Source/WebKit/gtk:
    
    * webkit/webkitdownload.cpp:
    (webkit_download_open_stream_for_uri):
    
    Source/WebKit2:
    
    * Shared/Downloads/soup/DownloadSoup.cpp:
    (WebKit::DownloadClient::didReceiveResponse):
    
    
    Canonical link: https://commits.webkit.org/144561@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@161536 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/gtk/webkit/webkitdownload.cpp
+++ Source/WebKit/gtk/webkit/webkitdownload.cpp
@@ -477,7 +477,9 @@ static gboolean webkit_download_open_stream_for_uri(WebKitDownload* download, co
     }
 
     GRefPtr<GFileInfo> info = adoptGRef(g_file_info_new());
-    g_file_info_set_attribute_string(info.get(), "metadata::download-uri", webkit_download_get_uri(download));
+    const char* uri_string = webkit_download_get_uri(download);
+    g_file_info_set_attribute_string(info.get(), "metadata::download-uri", uri_string);
+    g_file_info_set_attribute_string(info.get(), "xattr::xdg.origin.url", uri_string);
     g_file_set_attributes_async(file.get(), info.get(), G_FILE_QUERY_INFO_NONE, G_PRIORITY_DEFAULT, 0, 0, 0);
 
     return TRUE;
