add3932efd76fb1b696a3960d7da36e23e59196b
Manuel Rego Casasnovas <rego@igalia.com>
2014-01-10 22:54:16 +0000

    [GTK] Unreviewed build fix after r161644.
    * Modules/webaudio/MediaStreamAudioSourceNode.cpp:
    (WebCore::MediaStreamAudioSourceNode::setFormat):
    
    Canonical link: https://commits.webkit.org/144692@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@161682 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/webaudio/MediaStreamAudioSourceNode.cpp
+++ Source/WebCore/Modules/webaudio/MediaStreamAudioSourceNode.cpp
@@ -78,7 +78,7 @@ void MediaStreamAudioSourceNode::setFormat(size_t numberOfChannels, float source
 
         {
             // The context must be locked when changing the number of output channels.
-            AudioContext::AutoLocker contextLocker(context());
+            AudioContext::AutoLocker contextLocker(*context());
 
             // Do any necesssary re-configuration to the output's number of channels.
             output(0)->setNumberOfChannels(numberOfChannels);
