b27b59ab23c784592962900ab22f94822cce729f
Joseph Pecoraro <joepeck@webkit.org>
2014-01-11 03:46:20 +0000

    Fix DragData.o for iOS. Unreviewed build fix.
    String m_pasteboardName is unused on iOS, so ifdef it out.
    
    * platform/DragData.h:
    
    Canonical link: https://commits.webkit.org/144725@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@161718 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/DragData.h
+++ Source/WebCore/platform/DragData.h
@@ -20,7 +20,7 @@
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifndef DragData_h
@@ -109,7 +109,7 @@ public:
     bool containsFiles() const;
     unsigned numberOfFiles() const;
     int modifierKeyState() const;
-#if PLATFORM(MAC)
+#if PLATFORM(MAC) && !PLATFORM(IOS)
     const String& pasteboardName() const { return m_pasteboardName; }
 #endif
 
@@ -135,7 +135,7 @@ private:
     DragDataRef m_platformDragData;
     DragOperation m_draggingSourceOperationMask;
     DragApplicationFlags m_applicationFlags;
-#if PLATFORM(MAC)
+#if PLATFORM(MAC) && !PLATFORM(IOS)
     String m_pasteboardName;
 #endif
 #if PLATFORM(WIN)
