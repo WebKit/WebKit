dc709a2e189f2097242db478617f24a361b030ff
Jeffrey Pfau <jpfau@apple.com>
2014-01-15 03:56:02 +0000

    Build fix after r162034
    Rubber-stamped by Benjamin Poulain.
    
    Source/WebCore:
    
    * WebCore.exp.in:
    
    Source/WebKit2:
    
    * WebProcess/WebCoreSupport/WebProgressTrackerClient.cpp:
    
    Canonical link: https://commits.webkit.org/145019@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@162041 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/WebCoreSupport/WebProgressTrackerClient.cpp
+++ Source/WebKit2/WebProcess/WebCoreSupport/WebProgressTrackerClient.cpp
@@ -29,6 +29,7 @@
 #include "WebPage.h"
 #include "WebPageProxyMessages.h"
 #include <WebCore/Frame.h>
+#include <WebCore/MainFrame.h>
 #include <WebCore/Page.h>
 #include <WebCore/ProgressTracker.h>
 
