d118871176da7cd0d405b0ac0c4d2fff7ad4b097
Mihai Tica <mitica@adobe.com>
2014-01-15 12:32:03 +0000

    Source/WebCore: Reapplying: Background-blend-mode doesn't work for an element with an SVG image as background and border-style or padding set. The problem consisted in the drawing path using the default blending parameter at all times. https://bugs.webkit.org/show_bug.cgi?id=118894
    Patch by Mihai Tica <mitica@adobe.com> on 2014-01-15
    Reviewed by Dirk Schulze.
    
    Test: css3/compositing/background-blend-mode-data-uri-svg-image.html
    
    * svg/graphics/SVGImage.cpp:
    (WebCore::SVGImage::drawPatternForContainer): Pass blendMode to Image::drawPattern.
    * svg/graphics/SVGImage.h: Add a blendMode parameter to drawPatternForContainer.
    * svg/graphics/SVGImageForContainer.cpp:
    (WebCore::SVGImageForContainer::drawPattern): Pass blendMode to drawPatternForContainer call.
    
    LayoutTests: Reapplying:
    Background-blend-mode doesn't work for an element with an
    SVG image as background and border-style or padding set.
    Tests now use rectangles instead of circles for SVGs.
    https://bugs.webkit.org/show_bug.cgi?id=118894
    
    Patch by Mihai Tica <mitica@adobe.com> on 2014-01-15
    Reviewed by Dirk Schulze.
    
    * css3/compositing/background-blend-mode-data-uri-svg-image-expected.html:
    * css3/compositing/background-blend-mode-data-uri-svg-image.html:
    * platform/efl/TestExpectations: pass tests for efl, blendMode is not implemented.
    
    Canonical link: https://commits.webkit.org/145043@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@162066 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/svg/graphics/SVGImage.h
+++ Source/WebCore/svg/graphics/SVGImage.h
@@ -92,7 +92,7 @@ private:
     virtual void draw(GraphicsContext*, const FloatRect& fromRect, const FloatRect& toRect, ColorSpace styleColorSpace, CompositeOperator, BlendMode, ImageOrientationDescription) OVERRIDE;
     void drawForContainer(GraphicsContext*, const FloatSize, float, const FloatRect&, const FloatRect&, ColorSpace, CompositeOperator, BlendMode);
     void drawPatternForContainer(GraphicsContext*, const FloatSize, float, const FloatRect&, const AffineTransform&, const FloatPoint&, ColorSpace,
-        CompositeOperator, const FloatRect&);
+        CompositeOperator, const FloatRect&, BlendMode);
 
     std::unique_ptr<SVGImageChromeClient> m_chromeClient;
     std::unique_ptr<Page> m_page;
