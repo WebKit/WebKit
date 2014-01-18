3c3d2aa11ea4af35c865096e8debae03b145c8c9
Anders Carlsson <andersca@apple.com>
2014-01-18 21:15:19 +0000

    Remove support for the viewsource attribute https://bugs.webkit.org/show_bug.cgi?id=127232
    Reviewed by Andreas Kling.
    
    Source/WebCore:
    
    The 'viewsource' attribute on frames is nonstandard, not used by anyone (the inspector has
    its own syntax highlighting) and not audited.
    
    This patch removes parsing of the viewsource attribute and the associated tests. A subsequent
    patch will remove the code as well.
    
    * html/HTMLFrameElementBase.cpp:
    (WebCore::HTMLFrameElementBase::parseAttribute):
    
    Source/WTF:
    
    * wtf/FeatureDefines.h:
    
    LayoutTests:
    
    * .gitattributes:
    * fast/frames/resources/viewsource-fake-image-file.png: Removed.
    * fast/frames/resources/viewsource-frame-1.html: Removed.
    * fast/frames/resources/viewsource-frame-2.html: Removed.
    * fast/frames/resources/viewsource-frame-3.html: Removed.
    * fast/frames/resources/viewsource-frame-4.html: Removed.
    * fast/frames/resources/viewsource-frame-5.html: Removed.
    * fast/frames/resources/viewsource-frame-unfinished-script.html: Removed.
    * fast/frames/resources/viewsource-frame-unfinished-textarea.html: Removed.
    * fast/frames/viewsource-attribute.html: Removed.
    * fast/frames/viewsource-empty-attribute-value-expected.txt: Removed.
    * fast/frames/viewsource-empty-attribute-value.html: Removed.
    * fast/frames/viewsource-linebreak-expected.txt: Removed.
    * fast/frames/viewsource-linebreak.html: Removed.
    * fast/frames/viewsource-link-on-href-value-expected.txt: Removed.
    * fast/frames/viewsource-link-on-href-value.html: Removed.
    * fast/frames/viewsource-on-image-file.html: Removed.
    * fast/frames/viewsource-plain-text-tags-expected.txt: Removed.
    * fast/frames/viewsource-plain-text-tags.html: Removed.
    * fast/frames/viewsource-unfinished-tags-expected.txt: Removed.
    * fast/frames/viewsource-unfinished-tags.html: Removed.
    * fast/frames/viewsource-xml-expected.txt: Removed.
    * fast/frames/viewsource-xml.html: Removed.
    * http/tests/security/resources/view-source-no-refresh.php: Removed.
    * http/tests/security/view-source-javascript-url-in-document-expected.txt: Removed.
    * http/tests/security/view-source-javascript-url-in-document.html: Removed.
    * http/tests/security/view-source-no-javascript-url-expected.txt: Removed.
    * http/tests/security/view-source-no-javascript-url.html: Removed.
    * http/tests/security/view-source-no-refresh-expected.txt: Removed.
    * http/tests/security/view-source-no-refresh.html: Removed.
    * platform/efl/fast/frames/viewsource-attribute-expected.png: Removed.
    * platform/efl/fast/frames/viewsource-attribute-expected.txt: Removed.
    * platform/efl/fast/frames/viewsource-on-image-file-expected.png: Removed.
    * platform/efl/fast/frames/viewsource-on-image-file-expected.txt: Removed.
    * platform/mac/fast/frames/viewsource-attribute-expected.png: Removed.
    * platform/mac/fast/frames/viewsource-attribute-expected.txt: Removed.
    * platform/mac/fast/frames/viewsource-on-image-file-expected.png: Removed.
    * platform/mac/fast/frames/viewsource-on-image-file-expected.txt: Removed.
    * platform/win/TestExpectations:
    
    Canonical link: https://commits.webkit.org/145231@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@162272 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLFrameElementBase.cpp
+++ Source/WebCore/html/HTMLFrameElementBase.cpp
@@ -119,12 +119,6 @@ void HTMLFrameElementBase::parseAttribute(const QualifiedName& name, const Atomi
         else if (equalIgnoringCase(value, "no"))
             m_scrolling = ScrollbarAlwaysOff;
         // FIXME: If we are already attached, this has no effect.
-#if ENABLE(VIEWSOURCE_ATTRIBUTE)
-    } else if (name == viewsourceAttr) {
-        m_viewSource = !value.isNull();
-        if (contentFrame())
-            contentFrame()->setInViewSourceMode(viewSourceMode());
-#endif
     } else if (name == onbeforeloadAttr)
         setAttributeEventListener(eventNames().beforeloadEvent, name, value);
     else if (name == onbeforeunloadAttr) {
