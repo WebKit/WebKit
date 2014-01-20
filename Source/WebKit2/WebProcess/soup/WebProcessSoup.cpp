8c9e0441ebe15fa302b48215ca214a0963712b7e
Tomáš Popela <tpopela@redhat.com>
2014-01-20 16:18:27 +0000

    Merge r162059 - [SOUP] [WK2] - Disable MemoryCache when the DOCUMENT_VIEWER cache model is set https://bugs.webkit.org/show_bug.cgi?id=126577
    Patch by Tomas Popela <tpopela@redhat.com> on 2014-01-15
    Reviewed by Carlos Garcia Campos.
    
    As in http://trac.webkit.org/changeset/152483, but this time for
    WebKit2. When WEBKIT_CACHE_MODEL_DOCUMENT_VIEWER is set we need to
    disable the MemoryCache completely to prevent caching of the resources.
    
    * WebProcess/soup/WebProcessSoup.cpp:
    (WebKit::WebProcess::platformSetCacheModel):

--- Source/WebKit2/WebProcess/soup/WebProcessSoup.cpp
+++ Source/WebKit2/WebProcess/soup/WebProcessSoup.cpp
@@ -98,6 +98,7 @@ void WebProcess::platformSetCacheModel(CacheModel cacheModel)
                         cacheTotalCapacity, cacheMinDeadCapacity, cacheMaxDeadCapacity, deadDecodedDataDeletionInterval,
                         pageCacheCapacity, urlCacheMemoryCapacity, urlCacheDiskCapacity);
 
+    WebCore::memoryCache()->setDisabled(cacheModel == CacheModelDocumentViewer);
     WebCore::memoryCache()->setCapacities(cacheMinDeadCapacity, cacheMaxDeadCapacity, cacheTotalCapacity);
     WebCore::memoryCache()->setDeadDecodedDataDeletionInterval(deadDecodedDataDeletionInterval);
     WebCore::pageCache()->setCapacity(pageCacheCapacity);
