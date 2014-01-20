18984c90a160eb5819de8098e422188bd09617b2
Mihai Tica <mitica@adobe.com>
2014-01-20 16:36:19 +0000

    Source/WebCore: [CSS Background Blending] Background layer with -webkit-cross-fade doesn't blend when having -webkit-background-blending applied. Turns out the problem was the blending parameter not being passed to WebCore::CrossfadeGeneratedImage::draw
    https://bugs.webkit.org/show_bug.cgi?id=126887
    
    Patch by Mihai Tica <mitica@adobe.com> on 2014-01-20
    Reviewed by Dirk Schulze.
    
    Test: css3/compositing/background-blend-mode-crossfade-image.html
    
    * platform/graphics/CrossfadeGeneratedImage.cpp:
    (WebCore::CrossfadeGeneratedImage::draw): set blendMode on context.
    
    LayoutTests: Reapply:
    [CSS Background Blending] Background layer with -webkit-cross-fade doesn't blend
    when having -webkit-background-blending applied. Tests now use SVG rects, instead
    of circles.
    
    https://bugs.webkit.org/show_bug.cgi?id=126887
    Patch by Mihai Tica <mitica@adobe.com> on 2014-01-20
    Reviewed by Dirk Schulze.
    
    * css3/compositing/background-blend-mode-crossfade-image-expected.html:
    * css3/compositing/background-blend-mode-crossfade-image.html:
    * platform/efl/TestExpectations: Pass efl as blendMode is not implemented.
    * platform/mac/TestExpectations: Mark added test as ImageOnlyFailure due to slight differences between the result and the blending formula.
    
    Canonical link: https://commits.webkit.org/145275@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@162348 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/CrossfadeGeneratedImage.cpp
+++ Source/WebCore/platform/graphics/CrossfadeGeneratedImage.cpp
@@ -83,10 +83,10 @@ void CrossfadeGeneratedImage::drawCrossfade(GraphicsContext* context)
     context->endTransparencyLayer();
 }
 
-void CrossfadeGeneratedImage::draw(GraphicsContext* context, const FloatRect& dstRect, const FloatRect& srcRect, ColorSpace, CompositeOperator compositeOp, BlendMode, ImageOrientationDescription)
+void CrossfadeGeneratedImage::draw(GraphicsContext* context, const FloatRect& dstRect, const FloatRect& srcRect, ColorSpace, CompositeOperator compositeOp, BlendMode blendMode, ImageOrientationDescription)
 {
     GraphicsContextStateSaver stateSaver(*context);
-    context->setCompositeOperation(compositeOp);
+    context->setCompositeOperation(compositeOp, blendMode);
     context->clip(dstRect);
     context->translate(dstRect.x(), dstRect.y());
     if (dstRect.size() != srcRect.size())
