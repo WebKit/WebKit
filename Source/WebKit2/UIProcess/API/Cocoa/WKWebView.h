09994796778c6650a75c4376eb409c3d1f9cda73
Anders Carlsson <andersca@apple.com>
2014-01-22 22:16:37 +0000

    Add WKWebView class https://bugs.webkit.org/show_bug.cgi?id=127444 <rdar://problem/15884418>
    Reviewed by Dan Bernstein.
    
    * UIProcess/API/Cocoa/WKWebView.h: Added.
    * UIProcess/API/Cocoa/WKWebView.mm: Added.
    (-[WKWebView initWithFrame:configuration:]):
    (-[WKWebView configuration]):
    * WebKit2.xcodeproj/project.pbxproj:
    
    Canonical link: https://commits.webkit.org/145448@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@162554 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit2/UIProcess/API/Cocoa/WKWebView.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2014 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import <Foundation/Foundation.h>
+#import <WebKit2/WKFoundation.h>
+
+#if WK_API_ENABLED
+
+#if TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+#else
+#import <AppKit/AppKit.h>
+#endif
+
+@class WKWebViewConfiguration;
+
+/*!
+ A @link WKWebView @/link displays interactive Web content.
+ @helperclass @link WKWebViewConfiguration @/link
+ Used to configure @link WKWebView @/link instances.
+ */
+#if TARGET_OS_IPHONE
+@interface WKWebView : UIView
+#else
+@interface WKWebView : NSView
+#endif
+
+/*! @abstract A copy of the configuration with which the @link WKWebView @/link was initialized. */
+@property (nonatomic, readonly) WKWebViewConfiguration *configuration;
+
+/*! @abstract Returns a view initialized with the specified frame and configuration.
+ @param frame The frame for the new view.
+ @param configuration The configuration for the new view.
+ @result An initialized view, or nil if the object could not be initialized.
+ @discussion This is a designated initializer. You can use @link -initWithFrame: @/link to
+ initialize an instance with the default configuration.
+
+ The initializer copies
+ @link //apple_ref/doc/methodparam/WKWebView/initWithFrame:configuration:/configuration
+ configuration@/link, so mutating it after initialization has no effect on the
+ @link WKWebView @/link instance.
+ */
+- (instancetype)initWithFrame:(CGRect)frame configuration:(WKWebViewConfiguration *)configuration WK_DESIGNATED_INITIALIZER;
+
+@end
+
+#endif
