f1c844b80de7e2c5e52becaedb760024f1abe684
Brent Fulgham <bfulgham@apple.com>
2014-01-22 22:38:53 +0000

    Back out accidental assembly change.
    Canonical link: https://commits.webkit.org/135908.566@safari-537.75-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-537.75-branch@162558 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/jit/JITStubsMSVC64.asm
+++ Source/JavaScriptCore/jit/JITStubsMSVC64.asm
@@ -23,6 +23,7 @@
 ; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ;*/
 
+EXTERN cti_vm_throw : near 
 PUBLIC ctiTrampoline
 PUBLIC ctiVMThrowTrampoline
 PUBLIC ctiOpThrowNotCaught
