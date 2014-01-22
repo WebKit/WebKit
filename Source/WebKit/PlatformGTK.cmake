84eeb30b4e96559611751785046eac77fc711583
Martin Robinson <mrobinson@webkit.org>
2014-01-22 18:59:26 +0000

    [GTK] Avoid a circular dependency when building webkitenumtypes.h https://bugs.webkit.org/show_bug.cgi?id=127199
    Reviewed by Daniel Bates.
    
    * PlatformGTK.cmake: Create a new list of headers consisting of all installed headers
    except webkitenumtypes.h. Use this list when generating webkitenumtypes.h.
    
    Canonical link: https://commits.webkit.org/145433@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@162535 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/PlatformGTK.cmake
+++ Source/WebKit/PlatformGTK.cmake
@@ -154,14 +154,17 @@ add_custom_command(
     VERBATIM
 )
 
+# To generate webkitenumtypes.h we want to use all installed headers, except webkitenumtypes.h itself.
+set(WebKitGTK_ENUM_GENERATION_HEADERS ${WebKitGTK_INSTALLED_HEADERS})
+list(REMOVE_ITEM WebKitGTK_ENUM_GENERATION_HEADERS ${DERIVED_SOURCES_WEBKIT2GTK_API_DIR}/webkitenumtypes.h)
 add_custom_command(
     OUTPUT ${DERIVED_SOURCES_WEBKITGTK_API_DIR}/webkitenumtypes.h
            ${DERIVED_SOURCES_WEBKITGTK_API_DIR}/webkitenumtypes.cpp
-    DEPENDS ${WebKitGTK_INSTALLED_HEADERS}
+    DEPENDS ${WebKitGTK_ENUM_GENERATION_HEADERS}
 
-    COMMAND glib-mkenums --template ${WEBKIT_DIR}/gtk/webkit/webkitenumtypes.h.template ${WebKitGTK_INSTALLED_HEADERS} | sed s/web_kit/webkit/ | sed s/WEBKIT_TYPE_KIT/WEBKIT_TYPE/ > ${DERIVED_SOURCES_WEBKITGTK_API_DIR}/webkitenumtypes.h
+    COMMAND glib-mkenums --template ${WEBKIT_DIR}/gtk/webkit/webkitenumtypes.h.template ${WebKitGTK_ENUM_GENERATION_HEADERS} | sed s/web_kit/webkit/ | sed s/WEBKIT_TYPE_KIT/WEBKIT_TYPE/ > ${DERIVED_SOURCES_WEBKITGTK_API_DIR}/webkitenumtypes.h
 
-    COMMAND glib-mkenums --template ${WEBKIT_DIR}/gtk/webkit/webkitenumtypes.cpp.template ${WebKitGTK_INSTALLED_HEADERS} | sed s/web_kit/webkit/ > ${DERIVED_SOURCES_WEBKITGTK_API_DIR}/webkitenumtypes.cpp
+    COMMAND glib-mkenums --template ${WEBKIT_DIR}/gtk/webkit/webkitenumtypes.cpp.template ${WebKitGTK_ENUM_GENERATION_HEADERS} | sed s/web_kit/webkit/ > ${DERIVED_SOURCES_WEBKITGTK_API_DIR}/webkitenumtypes.cpp
     VERBATIM
 )
 
