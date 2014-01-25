b8a89e3516e25bb6514565f24e9231e37634396f
Darin Adler <darin@apple.com>
2014-01-25 17:18:37 +0000

    Restore alphabetical order in Compiler.h https://bugs.webkit.org/show_bug.cgi?id=127620
    Reviewed by Sam Weinig.
    
    * wtf/Compiler.h: Resorted some things into alphabetical order
    
    
    Canonical link: https://commits.webkit.org/145630@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@162765 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/Compiler.h
+++ Source/WTF/wtf/Compiler.h
@@ -43,9 +43,9 @@
 #define WTF_COMPILER_CLANG 1
 #define WTF_COMPILER_SUPPORTS_BLOCKS __has_feature(blocks)
 #define WTF_COMPILER_SUPPORTS_C_STATIC_ASSERT __has_feature(c_static_assert)
-#define WTF_COMPILER_SUPPORTS_CXX_REFERENCE_QUALIFIED_FUNCTIONS __has_feature(cxx_reference_qualified_functions)
-#define WTF_COMPILER_SUPPORTS_CXX_GENERALIZED_INITIALIZERS __has_feature(cxx_generalized_initializers)
 #define WTF_COMPILER_SUPPORTS_CXX_CONSTEXPR __has_feature(cxx_constexpr)
+#define WTF_COMPILER_SUPPORTS_CXX_GENERALIZED_INITIALIZERS __has_feature(cxx_generalized_initializers)
+#define WTF_COMPILER_SUPPORTS_CXX_REFERENCE_QUALIFIED_FUNCTIONS __has_feature(cxx_reference_qualified_functions)
 #define WTF_COMPILER_SUPPORTS_CXX_USER_LITERALS __has_feature(cxx_user_literals)
 #endif
 
@@ -57,7 +57,6 @@
 #define WTF_COMPILER_GCC 1
 #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #define GCC_VERSION_AT_LEAST(major, minor, patch) (GCC_VERSION >= (major * 10000 + minor * 100 + patch))
-
 #endif
 
 /* Define GCC_VERSION_AT_LEAST for all compilers, so we can write things like GCC_VERSION_AT_LEAST(4, 1, 0). */
@@ -127,7 +126,7 @@
 #define WTF_COMPILER_SUPPORTS_EABI 1
 #endif
 
-/* ==== Compiler-independent macros for various compiler features ==== */
+/* ==== Compiler-independent macros for various compiler features, in alphabetical order ==== */
 
 /* ALWAYS_INLINE */
 
@@ -141,6 +140,16 @@
 
 #if !defined(ALWAYS_INLINE)
 #define ALWAYS_INLINE inline
+#endif
+
+/* CONSTEXPR */
+
+#if !defined(CONSTEXPR) && COMPILER_SUPPORTS(CXX_CONSTEXPR)
+#define CONSTEXPR constexpr
+#endif
+
+#if !defined(CONSTEXPR)
+#define CONSTEXPR
 #endif
 
 /* LIKELY */
@@ -262,15 +271,6 @@
 
 #if !defined(WARN_UNUSED_RETURN)
 #define WARN_UNUSED_RETURN
-#endif
-
-/* CONSTEXPR */
-#if !defined(CONSTEXPR) && COMPILER_SUPPORTS(CXX_CONSTEXPR)
-#define CONSTEXPR constexpr
-#endif
-
-#if !defined(CONSTEXPR)
-#define CONSTEXPR
 #endif
 
 #endif /* WTF_Compiler_h */
