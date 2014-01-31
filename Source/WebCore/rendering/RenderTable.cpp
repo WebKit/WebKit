5745c03a857b842bed5f9f60c8efdd10d39707cc
László Langó <lango@inf.u-szeged.hu>
2014-01-31 08:09:07 +0000

    Fix table sizing when 'max-width' is used. https://bugs.webkit.org/show_bug.cgi?id=115156
    Patch by László Langó <lango@inf.u-szeged.hu> on 2014-01-31
    Reviewed by Andreas Kling.
    
    Source/WebCore:
    
    r143534 make <table> abide by 'max-width' all the time which is wrong.
    Per the CSS specification, a table should be wide enough to fit its
    content, regardless of 'max-width'.
    
    r140479 fixed part of the regression from that change but made the
    same fatal mistake by constraining min-content to fit 'max-width'.
    
    The fix is to avoid constraining min-content and ensure that the table
    logical width is at least its min-content size.
    
    Backported from Blink:
    https://chromium.googlesource.com/chromium/blink/+/0bca0dec4895aeeb2054ba36316e984e4ebed06f
    
    Test: fast/table/html-table-width-max-width-constrained.html
    
    * rendering/RenderTable.cpp:
    (WebCore::RenderTable::updateLogicalWidth):
    (WebCore::RenderTable::computePreferredLogicalWidths):
    
    LayoutTests:
    
    * fast/table/css-table-max-width-expected.txt:
    * fast/table/css-table-max-width.html:
    * fast/table/html-table-width-max-width-constrained-expected.txt: Added.
    * fast/table/html-table-width-max-width-constrained.html: Added.
    
    Canonical link: https://commits.webkit.org/145986@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@163165 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderTable.cpp
+++ Source/WebCore/rendering/RenderTable.cpp
@@ -292,16 +292,16 @@ void RenderTable::updateLogicalWidth()
         setLogicalWidth(std::min<int>(availableContentLogicalWidth, maxPreferredLogicalWidth()));
     }
 
-    // Ensure we aren't smaller than our min preferred width.
-    setLogicalWidth(std::max<int>(logicalWidth(), minPreferredLogicalWidth()));
-
-    
     // Ensure we aren't bigger than our max-width style.
     Length styleMaxLogicalWidth = style().logicalMaxWidth();
     if ((styleMaxLogicalWidth.isSpecified() && !styleMaxLogicalWidth.isNegative()) || styleMaxLogicalWidth.isIntrinsic()) {
         LayoutUnit computedMaxLogicalWidth = convertStyleLogicalWidthToComputedWidth(styleMaxLogicalWidth, availableLogicalWidth);
         setLogicalWidth(std::min<int>(logicalWidth(), computedMaxLogicalWidth));
     }
+    
+    // Ensure we aren't smaller than our min preferred width. This MUST be done after 'max-width' as
+    // we ignore it if it means we wouldn't accomodate our content.
+    setLogicalWidth(std::max<int>(logicalWidth(), minPreferredLogicalWidth()));
 
     // Ensure we aren't smaller than our min-width style.
     Length styleMinLogicalWidth = style().logicalMinWidth();
@@ -328,6 +328,10 @@ void RenderTable::updateLogicalWidth()
         setMarginStart(minimumValueForLength(style().marginStart(), availableLogicalWidth));
         setMarginEnd(minimumValueForLength(style().marginEnd(), availableLogicalWidth));
     }
+
+    // We should NEVER shrink the table below the min-content logical width, or else the table can't accomodate
+    // its own content which doesn't match CSS nor what authors expect.
+    ASSERT(logicalWidth() >= minPreferredLogicalWidth());
 }
 
 // This method takes a RenderStyle's logical width, min-width, or max-width length and computes its actual value.
@@ -778,8 +782,9 @@ void RenderTable::computePreferredLogicalWidths()
 
     // FIXME: This should probably be checking for isSpecified since you should be able to use percentage, calc or viewport relative values for maxWidth.
     if (styleToUse.logicalMaxWidth().isFixed()) {
+        // We don't constrain m_minPreferredLogicalWidth as the table should be at least the size of its min-content, regardless of 'max-width'.
         m_maxPreferredLogicalWidth = std::min(m_maxPreferredLogicalWidth, adjustContentBoxLogicalWidthForBoxSizing(styleToUse.logicalMaxWidth().value()));
-        m_minPreferredLogicalWidth = std::min(m_minPreferredLogicalWidth, adjustContentBoxLogicalWidthForBoxSizing(styleToUse.logicalMaxWidth().value()));
+        m_minPreferredLogicalWidth = std::max(m_minPreferredLogicalWidth, m_maxPreferredLogicalWidth);
     }
 
     // FIXME: We should be adding borderAndPaddingLogicalWidth here, but m_tableLayout->computePreferredLogicalWidths already does,
