757c6905a656fa9c8b3ded534882dca5a9fb1027
Lucas Forschler <lforschler@apple.com>
2014-02-10 23:40:23 +0000

    Merged r158676.
    Canonical link: https://commits.webkit.org/135908.579@safari-537.75-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-537.75-branch@163827 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/parser/XSSAuditor.cpp
+++ Source/WebCore/html/parser/XSSAuditor.cpp
@@ -484,11 +484,10 @@ bool XSSAuditor::filterIframeToken(const FilterTokenRequest& request)
     ASSERT(request.token.type() == HTMLToken::StartTag);
     ASSERT(hasName(request.token, iframeTag));
 
-    bool didBlockScript = false;
-    if (isContainedInRequest(decodedSnippetForName(request))) {
+    bool didBlockScript = eraseAttributeIfInjected(request, srcdocAttr, String(), ScriptLikeAttribute);
+    if (isContainedInRequest(decodedSnippetForName(request)))
         didBlockScript |= eraseAttributeIfInjected(request, srcAttr, String(), SrcLikeAttribute);
-        didBlockScript |= eraseAttributeIfInjected(request, srcdocAttr, String(), ScriptLikeAttribute);
-    }
+
     return didBlockScript;
 }
 
