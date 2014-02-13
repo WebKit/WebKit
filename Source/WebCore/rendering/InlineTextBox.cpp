c398bf2d3da2688070a33c725f237136b4d8b364
Myles C. Maxfield <mmaxfield@apple.com>
2014-02-13 20:31:36 +0000

    Gaps for text-decoration-skip: ink are calculated assuming the glyph orientation is the same as the run orientation https://bugs.webkit.org/show_bug.cgi?id=128718
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    This is a stop-gap patch to make underlines in CJK text not look terrible. A long-term fix is to refactor
    showGlyphsWithAdvances to move rotation logic into a common place that the text-decoration-skip: ink code
    can use.
    
    Test: fast/css3-text/css3-text-decoration/text-decoration-skip/text-decoration-skip-orientation-upright.html
    
    * rendering/InlineTextBox.cpp:
    (WebCore::InlineTextBox::paintDecoration):
    
    LayoutTests:
    
    This test draws text that has descenders that would be skipped over if the glyph orientation was the same
    as the run orientation. However, it uses text-orientation: upright to make those two orientations not equal.
    It then compares this to no skipping at all.
    
    * fast/css3-text/css3-text-decoration/text-decoration-skip/text-decoration-skip-orientation-upright-expected.html: Added.
    * fast/css3-text/css3-text-decoration/text-decoration-skip/text-decoration-skip-orientation-upright.html: Added.
    
    Canonical link: https://commits.webkit.org/146782@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@164046 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/InlineTextBox.cpp
+++ Source/WebCore/rendering/InlineTextBox.cpp
@@ -1162,7 +1162,7 @@ void InlineTextBox::paintDecoration(GraphicsContext& context, const FloatPoint& 
             }
             default:
 #if ENABLE(CSS3_TEXT_DECORATION_SKIP_INK)
-                if (lineStyle.textDecorationSkip() == TextDecorationSkipInk) {
+                if (lineStyle.textDecorationSkip() == TextDecorationSkipInk && isHorizontal()) {
                     if (!context.paintingDisabled()) {
                         drawSkipInkUnderline(textPainter, context, localOrigin, underlineOffset, width, isPrinting);
 
