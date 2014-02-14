780d2d588b9f7dfe46c483eb001b10a491656122
Grzegorz Czajkowski <g.czajkowski@samsung.com>
2014-02-14 12:26:54 +0000

    Use asynchronous spellchecking in spelling-hasspellingmarker.js https://bugs.webkit.org/show_bug.cgi?id=128526
    Reviewed by Ryosuke Niwa.
    
    Refactoring spelling-hasspellingmarker.js to use asynchronous spellchecking.
    In addition, adds some bits for non-DumpRenderTree users.
    
    * editing/spelling/script-tests/spelling-hasspellingmarker.js:
    (addContainer): creates a new div element based on given markup.
    (typeMisspelling): types 'zz' to the element.
    (verifySpellingMarkers): calls shouldBecomeEqual to verify spelling markers.
    (tests): contains test cases.
    (done): invokes next test cases if any, otherwise calls finishJSTest().
    
    * editing/spelling/spelling-hasspellingmarker-expected.txt:
    Update baseline.
    
    * editing/spelling/spelling-hasspellingmarker.html:
    Include editing.js due to the test started using typeCharacterCommand().
    
    * platform/gtk-wk2/TestExpectations:
    * platform/gtk/TestExpectations:
    Skip spelling-hasspellingmarker.html for GTK (wk1 and wk2).
    
    Canonical link: https://commits.webkit.org/146836@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@164100 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/editing/spelling/script-tests/spelling-hasspellingmarker.js
+++ LayoutTests/editing/spelling/script-tests/spelling-hasspellingmarker.js
@@ -1,23 +1,63 @@
-description('This tests internals.hasSpellingMarker works for differnt type of elements.');
+description('This tests if internals.hasSpellingMarker works for differnt type of elements. '
+    + 'This test succeds when there are four elements having "zz ". '
+    + 'However, only the last one should not contatin spelling marker.');
 
-var parent = document.createElement("div");
-document.body.appendChild(parent);
+jsTestIsAsync = true;
 
-function hasMarked(markup)
-{
-    parent.innerHTML = markup;
-    document.getElementById('test').focus();
-    document.execCommand("InsertText", false, 'z');
-    document.execCommand("InsertText", false, 'z');
-    document.execCommand("InsertText", false, ' ');
-
-    return internals.hasSpellingMarker(0, 2);
+if (window.internals) {
+    internals.settings.setUnifiedTextCheckerEnabled(true);
+    internals.settings.setAsynchronousSpellCheckingEnabled(true);
 }
 
-shouldBeTrue("hasMarked(\"<textarea id='test' cols='80' rows='10'></textarea>\");");
-shouldBeTrue("hasMarked(\"<input id='test' type='text'>\");");
-shouldBeTrue("hasMarked(\"<div id='test' contenteditable='true'></div>\");");
-shouldBeFalse("hasMarked(\"<div id='test' contentEditable='true' spellcheck='false'></div>\");");
-parent.innerHTML = "";
+var testRoot = document.createElement("div");
+document.body.insertBefore(testRoot, document.body.firstChild);
+
+function addContainer(markup)
+{
+    var contatiner = document.createElement("div");
+    contatiner.innerHTML = markup;
+    testRoot.appendChild(contatiner);
+
+    return contatiner;
+}
+
+function typeMisspelling(contatiner)
+{
+    contatiner.firstChild.focus();
+    typeCharacterCommand('z');
+    typeCharacterCommand('z');
+    typeCharacterCommand(' ');
+}
+
+function verifySpellingMarkers(element)
+{
+    var div = addContainer(element.containerMarkup);
+    typeMisspelling(div);
+
+    if (window.internals)
+        shouldBecomeEqual("internals.hasSpellingMarker(0, 2)", element.isMisspelled ? "true" : "false", done);
+    else
+        done();
+}
+
+var tests = [
+    function() { verifySpellingMarkers({ containerMarkup: "<textarea></textarea>", isMisspelled: true }); },
+    function() { verifySpellingMarkers({ containerMarkup: "<input type='text'></input>", isMisspelled: true}); },
+    function() { verifySpellingMarkers({ containerMarkup: "<div contenteditable='true'></div>", isMisspelled: true}); },
+    function() { verifySpellingMarkers({ containerMarkup: "<div contentEditable='true' spellcheck='false'></div>", isMisspelled: false}); }
+];
+
+function done()
+{
+    var next = tests.shift();
+    if (next)
+        return setTimeout(next, 0);
+
+    if (window.internals)
+        testRoot.style.display = "none";
+
+    finishJSTest();
+}
+done();
 
 var successfullyParsed = true;
