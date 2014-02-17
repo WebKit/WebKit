6980a774352759da5b92f4bcc50145fd29354af5
Manuel Rego Casasnovas <rego@igalia.com>
2014-02-17 10:57:43 +0000

    [CSS Grid Layout] Fix missing layout in flexible and content sized columns https://bugs.webkit.org/show_bug.cgi?id=128672
    Reviewed by Sergio Villar Senin.
    
    Source/WebCore:
    
    RenderGrid::logicalContentHeightForChild() is called for some items at the beginning of RenderGrid::layoutGridItems()
    from RenderGrid::computeUsedBreadthOfGridTracks(). This causes that the comparison inside the for loop in
    RenderGrid::layoutGridItems() does not detect width changes, so elements won't be marked as needsLayout.
    
    So the comparison is done in RenderGrid::logicalContentHeightForChild() and the element is marked to perform a layout if
    the width has changed.
    
    The issue can be reproduced easily with a simple grid with one flexible or content sized column, all the available width
    is not used. On top of that, when you resize the window the flexible or content sized columns are not updating their
    size properly.
    
    CSS Grid Layout perftest results are around 4% worse, which is expected as we're adding a missing layout.
    
    Test: fast/css-grid-layout/flex-content-sized-column-use-available-width.html
    
    * rendering/RenderGrid.cpp:
    (WebCore::RenderGrid::logicalContentHeightForChild): Check width changes and mark element as needed layout if required.
    
    LayoutTests:
    
    Add test that reproduce the issue for both cases flexible and content sized columns.
    
    * fast/css-grid-layout/flex-content-sized-column-use-available-width-expected.html: Added.
    * fast/css-grid-layout/flex-content-sized-column-use-available-width.html: Added.
    
    
    Canonical link: https://commits.webkit.org/146940@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@164214 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderGrid.cpp
+++ Source/WebCore/rendering/RenderGrid.cpp
@@ -424,10 +424,12 @@ size_t RenderGrid::explicitGridSizeForSide(GridPositionSide side) const
 
 LayoutUnit RenderGrid::logicalContentHeightForChild(RenderBox* child, Vector<GridTrack>& columnTracks)
 {
-    if (child->style().logicalHeight().isPercent())
+    LayoutUnit oldOverrideContainingBlockContentLogicalWidth = child->hasOverrideContainingBlockLogicalWidth() ? child->overrideContainingBlockContentLogicalWidth() : LayoutUnit();
+    LayoutUnit overrideContainingBlockContentLogicalWidth = gridAreaBreadthForChild(child, ForColumns, columnTracks);
+    if (child->style().logicalHeight().isPercent() || oldOverrideContainingBlockContentLogicalWidth != overrideContainingBlockContentLogicalWidth)
         child->setNeedsLayout(MarkOnlyThis);
 
-    child->setOverrideContainingBlockContentLogicalWidth(gridAreaBreadthForChild(child, ForColumns, columnTracks));
+    child->setOverrideContainingBlockContentLogicalWidth(overrideContainingBlockContentLogicalWidth);
     // If |child| has a percentage logical height, we shouldn't let it override its intrinsic height, which is
     // what we are interested in here. Thus we need to set the override logical height to -1 (no possible resolution).
     child->setOverrideContainingBlockContentLogicalHeight(-1);
