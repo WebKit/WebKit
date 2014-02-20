5f5c960d86a10ed5d0b8527452c8160ada7471f0
Jon Honeycutt <jhoneycutt@apple.com>
2014-02-20 03:03:39 +0000

    Crash in WTF::StringBuilder::append()
    https://bugs.webkit.org/show_bug.cgi?id=125817
    <rdar://problem/15671883>
    
    Reviewed by Oliver Hunt.
    
    * wtf/text/StringBuilder.cpp:
    (WTF::expandedCapacity):
    Ensure that we return a new capacity of at least 'requiredLength' in
    the case where requiredLength is large. Also, use unsigned rather than
    size_t for the parameters and the return value, as callers pass
    unsigned arguments and treat the result as an unsigned int.
    
    Canonical link: https://commits.webkit.org/147117@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@164408 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/text/StringBuilder.cpp
+++ Source/WTF/wtf/text/StringBuilder.cpp
@@ -33,10 +33,10 @@
 
 namespace WTF {
 
-static size_t expandedCapacity(size_t capacity, size_t newLength)
+static unsigned expandedCapacity(unsigned capacity, unsigned requiredLength)
 {
-    static const size_t minimumCapacity = 16;
-    return std::max(capacity, std::max(minimumCapacity, newLength * 2));
+    static const unsigned minimumCapacity = 16;
+    return std::max(requiredLength, std::max(minimumCapacity, capacity * 2));
 }
 
 void StringBuilder::reifyString() const
