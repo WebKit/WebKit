5dffa69714946299210c5be5227323b15efbe132
Jeffrey Pfau <jpfau@apple.com>
2014-02-22 00:31:23 +0000

    [Mac] Cache callbacks for cache partitioning may be passed null https://bugs.webkit.org/show_bug.cgi?id=129175
    Reviewed by Brady Eidson.
    
    Source/WebKit2:
    
    * WebProcess/ResourceCache/cf/WebResourceCacheManagerCFNet.cpp:
    (WebKit::WebResourceCacheManager::cfURLCacheHostNamesWithCallback):
    
    WebKitLibraries:
    
    * libWebKitSystemInterfaceLion.a:
    * libWebKitSystemInterfaceMavericks.a:
    * libWebKitSystemInterfaceMountainLion.a:
    
    Canonical link: https://commits.webkit.org/147224@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@164516 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/ResourceCache/cf/WebResourceCacheManagerCFNet.cpp
+++ Source/WebKit2/WebProcess/ResourceCache/cf/WebResourceCacheManagerCFNet.cpp
@@ -66,8 +66,10 @@ void WebResourceCacheManager::cfURLCacheHostNamesWithCallback(CacheCallback call
     WKCFURLCacheCopyAllPartitionNames(^(CFArrayRef partitionNames) {
         RetainPtr<CFArrayRef> hostNamesInPersistentStore = adoptCF(WKCFURLCacheCopyAllHostNamesInPersistentStoreForPartition(CFSTR("")));
         RetainPtr<CFMutableArrayRef> hostNames = adoptCF(CFArrayCreateMutableCopy(0, 0, hostNamesInPersistentStore.get()));
-        CFArrayAppendArray(hostNames.get(), partitionNames, CFRangeMake(0, CFArrayGetCount(partitionNames)));
-        CFRelease(partitionNames);
+        if (partitionNames) {
+            CFArrayAppendArray(hostNames.get(), partitionNames, CFRangeMake(0, CFArrayGetCount(partitionNames)));
+            CFRelease(partitionNames);
+        }
         callback(std::move(hostNames));
     });
 }
