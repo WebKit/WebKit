WebKit/WebKit

commit 61c19096da457331ff303dee1b988be6d4914980

[Win][LLINT] Many JSC stress test failures. https://bugs.webkit.org/show_bug.cgi?id=129155
Patch by peavo@outlook.com <peavo@outlook.com> on 2014-02-21
Reviewed by Michael Saboff.

Intel syntax has reversed operand order compared to AT&T syntax, so we need to swap the operand order, in this case on floating point operations.
Also avoid using the reverse opcode (e.g. fdivr), as this puts the result at the wrong position in the floating point stack.
E.g. "divd ft0, ft1" would translate to fdivr st, st(1) (Intel syntax) on Windows, but this puts the result in st, when it should be in st(1).

* offlineasm/x86.rb: Swap operand order on Windows.

Canonical link: https://commits.webkit.org/147202@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@164494 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/offlineasm/x86.rb
@@ -806,12 +806,12 @@ class Instruction
 
     def handleX87BinOp(opcode, opcodereverse)
         if (operands[1].x87DefaultStackPosition == 0)
-            $asm.puts "#{opcode} #{operands[0].x87Operand(0)}, #{register("st")}"
+            $asm.puts "#{opcode} #{orderOperands(operands[0].x87Operand(0), register("st"))}"
         elsif (operands[0].x87DefaultStackPosition == 0)
-            $asm.puts "#{opcodereverse} #{register("st")}, #{operands[1].x87Operand(0)}"
+            $asm.puts "#{opcode} #{orderOperands(register("st"), operands[1].x87Operand(0))}"
         else
             $asm.puts "fld #{operands[0].x87Operand(0)}"
-            $asm.puts "#{opcodereverse}p #{register("st")}, #{operands[1].x87Operand(1)}"
+            $asm.puts "#{opcodereverse}p #{orderOperands(register("st"), operands[1].x87Operand(1))}"
         end
     end
 

commit 4512400e229b70e975f8b3f3149fe45448518e5d

JSC regressions after r164494 https://bugs.webkit.org/show_bug.cgi?id=129272
Patch by peavo@outlook.com <peavo@outlook.com> on 2014-02-24
Reviewed by Mark Lam.

* offlineasm/x86.rb: Only avoid reverse opcode (fdivr) for Windows.

Canonical link: https://commits.webkit.org/147320@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@164612 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/offlineasm/x86.rb
@@ -808,7 +808,11 @@ class Instruction
         if (operands[1].x87DefaultStackPosition == 0)
             $asm.puts "#{opcode} #{orderOperands(operands[0].x87Operand(0), register("st"))}"
         elsif (operands[0].x87DefaultStackPosition == 0)
-            $asm.puts "#{opcode} #{orderOperands(register("st"), operands[1].x87Operand(0))}"
+            if !isIntelSyntax
+                $asm.puts "#{opcodereverse} #{register("st")}, #{operands[1].x87Operand(0)}"
+            else
+                $asm.puts "#{opcode} #{operands[1].x87Operand(0)}, #{register("st")}"
+            end
         else
             $asm.puts "fld #{operands[0].x87Operand(0)}"
             $asm.puts "#{opcodereverse}p #{orderOperands(register("st"), operands[1].x87Operand(1))}"