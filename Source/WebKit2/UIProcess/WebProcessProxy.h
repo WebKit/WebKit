WebKit/WebKit

commit dd3a5d7bc79f4473f4c7978357768c8ed1b935d3

[iOS][WebKit2] Adopt SPI for managing tabs https://bugs.webkit.org/show_bug.cgi?id=129188 <rdar://problem/15939827>
Reviewed by Gavin Barraclough.

Call into assertions SPI to mark tabs as foreground and background.

* Configurations/WebKit2.xcconfig:
* Platform/IPC/Connection.h:
(IPC::Connection::xpcConnection): Expose the xpc_connection_t.
* UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::viewStateDidChange):
* UIProcess/WebProcessProxy.cpp:
(WebKit::WebProcessProxy::didFinishLaunching):
* UIProcess/WebProcessProxy.h:
(WebKit::WebProcessProxy::updateProcessState):
* UIProcess/ios/WebProcessProxyIOS.mm:
(WebKit::WebProcessProxy::updateProcessState): Added. This goes through the list of
WebPageProxies and sets the process state to foreground if one of them is in a window.
Otherwise, it sets it to background.

Canonical link: https://commits.webkit.org/147431@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@164737 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/UIProcess/WebProcessProxy.h
@@ -127,6 +127,8 @@ public:
 
     void windowServerConnectionStateChanged();
 
+    void updateProcessState();
+
 private:
     explicit WebProcessProxy(WebContext&);
 
@@ -212,7 +214,11 @@ private:
 
     int m_numberOfTimesSuddenTerminationWasDisabled;
 };
-    
+
+#if !PLATFORM(IOS)
+void WebProcessProxy::updateProcessState() { }
+#endif
+
 } // namespace WebKit
 
 #endif // WebProcessProxy_h

Source/WebKit2/Configurations/WebKit2.xcconfig
@@ -30,7 +30,7 @@ INSTALL_PATH_ACTUAL = $(WEBKIT2_FRAMEWORKS_DIR);
 DYLIB_INSTALL_NAME_BASE = $(NORMAL_WEBKIT2_FRAMEWORKS_DIR);
 
 FRAMEWORK_AND_LIBRARY_LDFLAGS = $(FRAMEWORK_AND_LIBRARY_LDFLAGS_$(PLATFORM_NAME));
-FRAMEWORK_AND_LIBRARY_LDFLAGS_iphonesimulator = -lobjc -framework CFNetwork -framework CoreFoundation -framework CoreGraphics -framework CoreText -framework Foundation -framework GraphicsServices -framework ImageIO -framework UIKit -framework WebKit -lMobileGestalt;
+FRAMEWORK_AND_LIBRARY_LDFLAGS_iphonesimulator = -lobjc -framework CFNetwork -framework CoreFoundation -framework CoreGraphics -framework CoreText -framework Foundation -framework GraphicsServices -framework ImageIO -framework UIKit -framework WebKit -lMobileGestalt -lassertion_extension;
 FRAMEWORK_AND_LIBRARY_LDFLAGS_iphoneos = $(FRAMEWORK_AND_LIBRARY_LDFLAGS_iphonesimulator) -framework IOSurface;
 FRAMEWORK_AND_LIBRARY_LDFLAGS_macosx = -framework ApplicationServices -framework Carbon -framework Cocoa -framework CoreServices -framework IOKit -framework CoreAudio -framework IOSurface;
 

Source/WebKit2/Platform/IPC/Connection.h
@@ -113,6 +113,8 @@ public:
         xpc_connection_t xpcConnection;
     };
     static bool identifierIsNull(Identifier identifier) { return identifier.port == MACH_PORT_NULL; }
+    xpc_connection_t xpcConnection() { return m_xpcConnection; }
+
 #elif USE(UNIX_DOMAIN_SOCKETS)
     typedef int Identifier;
     static bool identifierIsNull(Identifier identifier) { return !identifier; }

Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -1025,6 +1025,9 @@ void WebPageProxy::viewStateDidChange(ViewState::Flags mayHaveChanged, WantsRepl
     }
 #endif
 
+    if (changed & ViewState::IsInWindow)
+        process().updateProcessState();
+
     updateBackingStoreDiscardableState();
 }
 

Source/WebKit2/UIProcess/WebProcessProxy.cpp
@@ -453,6 +453,7 @@ void WebProcessProxy::didFinishLaunching(ProcessLauncher* launcher, IPC::Connect
 #if PLATFORM(COCOA)
     updateProcessSuppressionState();
 #endif
+    updateProcessState();
 }
 
 WebFrameProxy* WebProcessProxy::webFrame(uint64_t frameID) const

Source/WebKit2/UIProcess/ios/WebProcessProxyIOS.mm
@@ -26,6 +26,7 @@
 #import "config.h"
 #import "WebProcessProxy.h"
 
+#import <assertion/extension_private.h>
 #import <WebCore/NotImplemented.h>
 
 namespace WebKit {
@@ -66,6 +67,29 @@ bool WebProcessProxy::allPagesAreProcessSuppressible() const
 void WebProcessProxy::updateProcessSuppressionState()
 {
     notImplemented();
+}
+
+void WebProcessProxy::updateProcessState()
+{
+#if USE(XPC_SERVICES)
+    if (!isValid())
+        return;
+
+    xpc_connection_t xpcConnection = connection()->xpcConnection();
+    if (!xpcConnection)
+        return;
+
+    assertion_extension_state_t extensionState = ASSERTION_EXTENSION_STATE_BACKGROUND;
+    for (const auto& page : m_pageMap.values()) {
+        if (page->isInWindow()) {
+            extensionState = ASSERTION_EXTENSION_STATE_FOREGROUND;
+            break;
+        }
+    }
+
+    errno_t tabStateError = assertion_extension_set_state(xpcConnection, extensionState, NULL);
+    ASSERT_UNUSED(tabStateError, !tabStateError);
+#endif
 }
 
 } // namespace WebKit

commit 03a9fae2c277b926ab835df333b23251b20faf1c

WebProcessProxy::updateProcessState() is causing duplicate symbol linker errors https://bugs.webkit.org/show_bug.cgi?id=129390
Reviewed by Tim Horton.

Caused by r164737. WebProcessProxy::updateProcessState() is defined in a header, outside of
a class, but isn't inlined.

* UIProcess/WebProcessProxy.h:
(WebKit::WebProcessProxy::updateProcessState):

Canonical link: https://commits.webkit.org/147443@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@164749 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/UIProcess/WebProcessProxy.h
@@ -216,7 +216,7 @@ private:
 };
 
 #if !PLATFORM(IOS)
-void WebProcessProxy::updateProcessState() { }
+inline void WebProcessProxy::updateProcessState() { }
 #endif
 
 } // namespace WebKit