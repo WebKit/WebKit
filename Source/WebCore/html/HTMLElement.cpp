f6707f026c2c6451e2ac32084a9f8033e0151142
Andreas Kling <akling@apple.com>
2014-02-27 23:18:26 +0000

    Micro-optimize elementAffectsDirectionality(). <https://webkit.org/b/129444>
    Tweak this function to skip an unnecessary bit check and use
    fastHasAttribute() instead of hasAttribute() for looking up dirAttr.
    
    ~1% speedup on DYEB/AngularJS.
    
    Reviewed by Ryosuke Niwa.
    
    * html/HTMLElement.cpp:
    (WebCore::elementAffectsDirectionality):
    
    
    Canonical link: https://commits.webkit.org/147520@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@164833 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLElement.cpp
+++ Source/WebCore/html/HTMLElement.cpp
@@ -817,7 +817,7 @@ HTMLFormElement* HTMLElement::virtualForm() const
 
 static inline bool elementAffectsDirectionality(const Node& node)
 {
-    return node.isHTMLElement() && (node.hasTagName(bdiTag) || toHTMLElement(node).hasAttribute(dirAttr));
+    return node.isHTMLElement() && (toHTMLElement(node).hasTagName(bdiTag) || toHTMLElement(node).fastHasAttribute(dirAttr));
 }
 
 static void setHasDirAutoFlagRecursively(Node* firstNode, bool flag, Node* lastNode = nullptr)
