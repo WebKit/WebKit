fdbd8f6160c056c82986fb239fb5afda955f13f7
Timothy Hatcher <timothy@apple.com>
2014-02-28 01:17:37 +0000

    Improve how ContentSearchUtilities::lineEndings works by supporting the three common line endings.
    https://bugs.webkit.org/show_bug.cgi?id=129458
    
    Reviewed by Joseph Pecoraro.
    
    Source/JavaScriptCore:
    
    * inspector/ContentSearchUtilities.cpp:
    (Inspector::ContentSearchUtilities::textPositionFromOffset): Remove assumption about line ending length.
    (Inspector::ContentSearchUtilities::getRegularExpressionMatchesByLines): Remove assumption about
    line ending type and don't try to strip the line ending. Use size_t
    (Inspector::ContentSearchUtilities::lineEndings): Use findNextLineStart to find the lines.
    This will include the line ending in the lines, but that is okay.
    (Inspector::ContentSearchUtilities::buildObjectForSearchMatch): Use size_t.
    (Inspector::ContentSearchUtilities::searchInTextByLines): Modernize.
    
    LayoutTests:
    
    * inspector-protocol/debugger/resources/mac-linebreaks.js: Added.
    * inspector-protocol/debugger/resources/mixed-linebreaks.js: Added.
    * inspector-protocol/debugger/resources/unix-linebreaks.js: Added.
    * inspector-protocol/debugger/resources/windows-linebreaks.js: Added.
    * inspector-protocol/debugger/searchInContent-linebreaks-expected.txt: Added.
    * inspector-protocol/debugger/searchInContent-linebreaks.html: Added.
    
    
    Canonical link: https://commits.webkit.org/147532@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@164845 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/inspector/ContentSearchUtilities.cpp
+++ Source/JavaScriptCore/inspector/ContentSearchUtilities.cpp
@@ -67,36 +67,36 @@ static inline size_t sizetExtractor(const size_t* value)
 
 TextPosition textPositionFromOffset(size_t offset, const Vector<size_t>& lineEndings)
 {
-    const size_t* foundLineEnding = approximateBinarySearch<size_t, size_t>(lineEndings, lineEndings.size(), offset, sizetExtractor);
-    size_t lineIndex = foundLineEnding - &lineEndings.at(0);
-    if (offset > *foundLineEnding)
+    const size_t* foundNextStart = approximateBinarySearch<size_t, size_t>(lineEndings, lineEndings.size(), offset, sizetExtractor);
+    size_t lineIndex = foundNextStart - &lineEndings.at(0);
+    if (offset >= *foundNextStart)
         ++lineIndex;
-    size_t lineStartOffset = lineIndex > 0 ? lineEndings.at(lineIndex - 1) + 1 : 0;
+    size_t lineStartOffset = lineIndex > 0 ? lineEndings.at(lineIndex - 1) : 0;
     size_t column = offset - lineStartOffset;
     return TextPosition(OrdinalNumber::fromZeroBasedInt(lineIndex), OrdinalNumber::fromZeroBasedInt(column));
 }
 
-static Vector<std::pair<int, String>> getRegularExpressionMatchesByLines(const JSC::Yarr::RegularExpression& regex, const String& text)
+static Vector<std::pair<size_t, String>> getRegularExpressionMatchesByLines(const JSC::Yarr::RegularExpression& regex, const String& text)
 {
-    Vector<std::pair<int, String>> result;
+    Vector<std::pair<size_t, String>> result;
     if (text.isEmpty())
         return result;
 
     std::unique_ptr<Vector<size_t>> endings(lineEndings(text));
     size_t size = endings->size();
-    unsigned start = 0;
+    size_t start = 0;
+
     for (size_t lineNumber = 0; lineNumber < size; ++lineNumber) {
-        size_t lineEnd = endings->at(lineNumber);
-        String line = text.substring(start, lineEnd - start);
-        if (line.endsWith('\r'))
-            line = line.left(line.length() - 1);
+        size_t nextStart = endings->at(lineNumber);
+        String line = text.substring(start, nextStart - start);
 
         int matchLength;
         if (regex.match(line, 0, &matchLength) != -1)
-            result.append(std::pair<int, String>(lineNumber, line));
+            result.append(std::pair<size_t, String>(lineNumber, line));
 
-        start = lineEnd + 1;
+        start = nextStart;
     }
+
     return result;
 }
 
@@ -104,21 +104,24 @@ std::unique_ptr<Vector<size_t>> lineEndings(const String& text)
 {
     auto result = std::make_unique<Vector<size_t>>();
 
-    unsigned start = 0;
+    size_t start = 0;
     while (start < text.length()) {
-        size_t lineEnd = text.find('\n', start);
-        if (lineEnd == notFound)
+        size_t nextStart = text.findNextLineStart(start);
+        if (nextStart == notFound) {
+            result->append(text.length());
             break;
+        }
 
-        result->append(lineEnd);
-        start = lineEnd + 1;
+        result->append(nextStart);
+        start = nextStart;
     }
+
     result->append(text.length());
 
     return result;
 }
 
-static PassRefPtr<Inspector::TypeBuilder::GenericTypes::SearchMatch> buildObjectForSearchMatch(int lineNumber, const String& lineContent)
+static PassRefPtr<Inspector::TypeBuilder::GenericTypes::SearchMatch> buildObjectForSearchMatch(size_t lineNumber, const String& lineContent)
 {
     return Inspector::TypeBuilder::GenericTypes::SearchMatch::create()
         .setLineNumber(lineNumber)
@@ -156,10 +159,10 @@ PassRefPtr<Inspector::TypeBuilder::Array<Inspector::TypeBuilder::GenericTypes::S
     RefPtr<Inspector::TypeBuilder::Array<Inspector::TypeBuilder::GenericTypes::SearchMatch>> result = Inspector::TypeBuilder::Array<Inspector::TypeBuilder::GenericTypes::SearchMatch>::create();
 
     JSC::Yarr::RegularExpression regex = ContentSearchUtilities::createSearchRegex(query, caseSensitive, isRegex);
-    Vector<std::pair<int, String>> matches = getRegularExpressionMatchesByLines(regex, text);
+    Vector<std::pair<size_t, String>> matches = getRegularExpressionMatchesByLines(regex, text);
 
-    for (Vector<std::pair<int, String>>::const_iterator it = matches.begin(); it != matches.end(); ++it)
-        result->addItem(buildObjectForSearchMatch(it->first, it->second));
+    for (const auto& match : matches)
+        result->addItem(buildObjectForSearchMatch(match.first, match.second));
 
     return result;
 }
