b6b0b1910b8001c9fb8d9f559892dea009b4406a
Brian Burg <bburg@apple.com>
2014-03-03 18:24:22 +0000

    Another unreviewed build fix attempt for Windows after r164986.
    We never told Visual Studio to copy over the web replay code generator scripts
    and the generated headers for JavaScriptCore replay inputs as if they were
    private headers.
    
    * JavaScriptCore.vcxproj/copy-files.cmd:
    
    Canonical link: https://commits.webkit.org/147664@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@164991 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/JavaScriptCore.vcxproj/copy-files.cmd
+++ Source/JavaScriptCore/JavaScriptCore.vcxproj/copy-files.cmd
@@ -54,6 +54,7 @@ for %%d in (
     llint
     parser
     profiler
+    replay
     runtime
     yarr
 ) do (
@@ -72,6 +73,16 @@ xcopy /y "%DerivedSourcesDirectory%\InspectorJS.json" "%PrivateHeadersDirectory%
 xcopy /y "%DerivedSourcesDirectory%\InspectorJSTypeBuilders.h" "%PrivateHeadersDirectory%" >NUL
 xcopy /y "%DerivedSourcesDirectory%\InspectorJSBackendDispatchers.h" "%PrivateHeadersDirectory%" >NUL
 xcopy /y "%DerivedSourcesDirectory%\InspectorJSFrontendDispatchers.h" "%PrivateHeadersDirectory%" >NUL
+
+echo Copying Web Replay scripts as if they were private headers...
+for %%d in (
+    replay\scripts
+) do (
+    xcopy /y /d ..\%%d\* "%PrivateHeadersDirectory%" >NUL
+)
+
+echo Copying Web Replay generated headers as if they were private headers...
+xcopy /y "%DerivedSourcesDirectory%\JSReplayInputs.h" "%PrivateHeadersDirectory%" >NUL
 
 echo Copying builtins header as if it were a private header...
 xcopy /y "%DerivedSourcesDirectory%\JSCBuiltins.h" "%PrivateHeadersDirectory%" >NUL
