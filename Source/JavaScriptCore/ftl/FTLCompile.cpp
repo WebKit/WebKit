b531fad547c2c2d9c59260b249f8de6efdb70cfb
Ryosuke Niwa <rniwa@webkit.org>
2014-03-06 02:30:51 +0000

    Another build fix attempt after r165141.
    * ftl/FTLCompile.cpp:
    (JSC::FTL::fixFunctionBasedOnStackMaps):
    
    
    Canonical link: https://commits.webkit.org/147805@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@165158 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/ftl/FTLCompile.cpp
+++ Source/JavaScriptCore/ftl/FTLCompile.cpp
@@ -346,7 +346,7 @@ static void fixFunctionBasedOnStackMaps(
                 
                 JITPutByIdGenerator gen(
                     codeBlock, putById.codeOrigin(), usedRegisters, JSValueRegs(base),
-                    JSValueRegs(value), GPRInfo::patchpointScratchRegister, false,
+                    JSValueRegs(value), GPRInfo::patchpointScratchRegister, NeedToSpill,
                     putById.ecmaMode(), putById.putKind());
                 
                 MacroAssembler::Label begin = slowPathJIT.label();
