daa2e80376acec824e69d4f896a84295804bd5c1
Mark Lam <mark.lam@apple.com>
2014-03-06 23:37:50 +0000

    Merge r164077, r164089: <rdar://problem/16136637>
        2014-02-13  Mark Lam  <mark.lam@apple.com>
    
        The JSContainerConvertor and ObjcContainerConvertor need to protect JSValueRefs. Part 2.
        <https://webkit.org/b/128764>
    
        Reviewed by Mark Hahnenberg.
    
        toJS() is the wrong cast function to use. We need to use toJSForGC() instead.
        Also we need to acquire the JSLock to prevent concurrent accesses to the
        Strong handle list.
    
        * API/JSValue.mm:
        (JSContainerConvertor::add):
        (containerValueToObject):
        (ObjcContainerConvertor::add):
        (objectToValue):
    
        2014-02-13  Mark Lam  <mark.lam@apple.com>
    
        The JSContainerConvertor and ObjcContainerConvertor need to protect JSValueRefs.
        <https://webkit.org/b/128764>
    
        Reviewed by Mark Hahnenberg.
    
        Added a vector of Strong<Unknown> references in the 2 containers, and append
        the newly created JSValues to those vectors. This will keep all those JS objects
        alive for the duration of the conversion.
    
        * API/JSValue.mm:
        (JSContainerConvertor::add):
        (ObjcContainerConvertor::add):
    
    
    
    Canonical link: https://commits.webkit.org/135908.628@safari-537.75-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-537.75-branch@165224 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/API/JSValue.mm
+++ Source/JavaScriptCore/API/JSValue.mm
@@ -37,6 +37,7 @@
 #import "ObjcRuntimeExtras.h"
 #import "Operations.h"
 #import "JSCJSValue.h"
+#import "StrongInlines.h"
 #import <wtf/HashMap.h>
 #import <wtf/HashSet.h>
 #import <wtf/ObjcRuntimeExtras.h>
@@ -595,6 +596,7 @@ private:
     JSGlobalContextRef m_context;
     HashMap<JSValueRef, id> m_objectMap;
     Vector<Task> m_worklist;
+    Vector<JSC::Strong<JSC::Unknown>> m_jsValues;
 };
 
 inline id JSContainerConvertor::convert(JSValueRef value)
@@ -611,6 +613,8 @@ inline id JSContainerConvertor::convert(JSValueRef value)
 
 void JSContainerConvertor::add(Task task)
 {
+    JSC::ExecState* exec = toJS(m_context);
+    m_jsValues.append(JSC::Strong<JSC::Unknown>(exec->vm(), toJSForGC(exec, task.js)));
     m_objectMap.add(task.js, task.objc);
     if (task.type != ContainerNone)
         m_worklist.append(task);
@@ -667,6 +671,7 @@ static JSContainerConvertor::Task valueToObjectWithoutCopy(JSGlobalContextRef co
 static id containerValueToObject(JSGlobalContextRef context, JSContainerConvertor::Task task)
 {
     ASSERT(task.type != ContainerNone);
+    JSC::APIEntryShim entryShim(toJS(context));
     JSContainerConvertor convertor(context);
     convertor.add(task);
     ASSERT(!convertor.isWorkListEmpty());
@@ -817,6 +822,7 @@ private:
     JSContext *m_context;
     HashMap<id, JSValueRef> m_objectMap;
     Vector<Task> m_worklist;
+    Vector<JSC::Strong<JSC::Unknown>> m_jsValues;
 };
 
 JSValueRef ObjcContainerConvertor::convert(id object)
@@ -834,6 +840,8 @@ JSValueRef ObjcContainerConvertor::convert(id object)
 
 void ObjcContainerConvertor::add(ObjcContainerConvertor::Task task)
 {
+    JSC::ExecState* exec = toJS(m_context.JSGlobalContextRef);
+    m_jsValues.append(JSC::Strong<JSC::Unknown>(exec->vm(), toJSForGC(exec, task.js)));
     m_objectMap.add(task.objc, task.js);
     if (task.type != ContainerNone)
         m_worklist.append(task);
@@ -913,6 +921,7 @@ JSValueRef objectToValue(JSContext *context, id object)
     if (task.type == ContainerNone)
         return task.js;
 
+    JSC::APIEntryShim entryShim(toJS(contextRef));
     ObjcContainerConvertor convertor(context);
     convertor.add(task);
     ASSERT(!convertor.isWorkListEmpty());
