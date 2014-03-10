afe7d7badfa3f60eaa9ddd6fe2e877f7c737e191
Brent Fulgham <bfulgham@apple.com>
2014-03-10 19:28:54 +0000

    Source/WebInspectorUI: Merge r165393.
        2014-03-10  Brent Fulgham  <bfulgham@apple.com>
    
        [Win] Pass environment to Pre-Build, Pre-Link, and Post-Build Stages.
        https://bugs.webkit.org/show_bug.cgi?id=130023
    
        * WebInspectorUI.vcxproj/WebInspectorUIPostBuild.cmd: Use %PlatformArchitecture%
        rather than hard-coded 32-bit path.
    
    Source/WebKit: Merge r165393.
    
        2014-03-10  Brent Fulgham  <bfulgham@apple.com>
    
        [Win] Pass environment to Pre-Build, Pre-Link, and Post-Build Stages.
        https://bugs.webkit.org/show_bug.cgi?id=130023
    
        Reviewed by Dean Jackson.
    
        * WebKit.vcxproj/WebKit/Interfaces/InterfacesPreBuild.cmd: Use %PlatformArchitecture% rather than
        hard-coded 32-bit path.
        * WebKit.vcxproj/WebKit/WebKitPostBuild.cmd: Ditto.
    
    Tools:     Merge r165393.
    
        2014-03-10  Brent Fulgham  <bfulgham@apple.com>
    
        [Win] Pass environment to Pre-Build, Pre-Link, and Post-Build Stages.
        https://bugs.webkit.org/show_bug.cgi?id=130023
    
        Reviewed by Dean Jackson.
    
        * win/AssembleBuildLogs/AssembleBuildLogs.cmd: Use %PlatformArchitecture% rather than
        32-bit hard-coded path.
    
    WebKitLibraries: Merge r165393.
    
        2014-03-10  Brent Fulgham  <bfulgham@apple.com>
    
        [Win] Pass environment to Pre-Build, Pre-Link, and Post-Build Stages.
        https://bugs.webkit.org/show_bug.cgi?id=130023
    
        Reviewed by Dean Jackson.
    
        * win/tools/vsprops/common.props: Pass WEBKIT_LIBRARIES value to subprocesses.
    
    
    
    Canonical link: https://commits.webkit.org/135908.641@safari-537.75-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-537.75-branch@165394 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebKit.vcxproj/WebKit/WebKitPostBuild.cmd
+++ Source/WebKit/WebKit.vcxproj/WebKit/WebKitPostBuild.cmd
@@ -1,5 +1,7 @@
 mkdir 2>NUL "%CONFIGURATIONBUILDDIR%\include\WebKit"
 
+set ResourcesDirectory=%CONFIGURATIONBUILDDIR%\bin%PlatformArchitecture%\WebKit.resources
+
 xcopy /y /d "%PROJECTDIR%\..\..\win\WebLocalizableStrings.h" "%CONFIGURATIONBUILDDIR%\include\WebKit"
 xcopy /y /d "%PROJECTDIR%\..\..\win\WebKitGraphics.h" "%CONFIGURATIONBUILDDIR%\include\WebKit"
 xcopy /y /d "%PROJECTDIR%\..\..\win\WebKitCOMAPI.h" "%CONFIGURATIONBUILDDIR%\include\WebKit"
@@ -11,8 +13,10 @@ xcopy /y /d "%CONFIGURATIONBUILDDIR%\include\WebCore\npruntime.h" "%CONFIGURATIO
 xcopy /y /d "%CONFIGURATIONBUILDDIR%\include\WebCore\npruntime_internal.h" "%CONFIGURATIONBUILDDIR%\include\WebKit"
 xcopy /y /d "%CONFIGURATIONBUILDDIR%\include\WebCore\nptypes.h" "%CONFIGURATIONBUILDDIR%\include\WebKit"
 
-mkdir 2>NUL "%CONFIGURATIONBUILDDIR%\bin%PlatformArchitecture%\WebKit.resources"
-xcopy /y /d "%PROJECTDIR%..\..\WebKit.resources\*" "%CONFIGURATIONBUILDDIR%\bin%PlatformArchitecture%\WebKit.resources"
+echo Copying resources...
+mkdir "%ResourcesDirectory%" 2>NUL
+xcopy /y /d "%PROJECTDIR%\..\..\win\WebKit.resources\Info.plist" "%ResourcesDirectory%" >NUL
+
 if exist "%WEBKIT_LIBRARIES%\tools\VersionStamper\VersionStamper.exe" "%WEBKIT_LIBRARIES%\tools\VersionStamper\VersionStamper.exe" --verbose "%TARGETPATH%"
 
 if exist "%CONFIGURATIONBUILDDIR%\buildfailed" del "%CONFIGURATIONBUILDDIR%\buildfailed"
