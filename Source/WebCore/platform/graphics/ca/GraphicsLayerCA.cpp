ce1b3a907f15ee9cb52d591cbf412936800aed98
David Kilzer <ddkilzer@webkit.org>
2014-03-16 21:41:47 +0000

    Follow-up: Double values passed to fabsf() in maxScaleFromTransform() <http://webkit.org/b/130297>
    Reviewed by Darin Adler.
    
    * platform/graphics/ca/GraphicsLayerCA.cpp:
    (WebCore::maxScaleFromTransform): Use narrowPrecisionToFloat()
    instead of static_cast<float>().
    
    
    Canonical link: https://commits.webkit.org/148296@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@165711 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
+++ Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -284,7 +284,7 @@ static float maxScaleFromTransform(const TransformationMatrix& t)
 
     TransformationMatrix::Decomposed4Type decomposeData;
     t.decompose4(decomposeData);
-    return std::max(fabsf(static_cast<float>(decomposeData.scaleX)), fabsf(static_cast<float>(decomposeData.scaleY)));
+    return std::max(fabsf(narrowPrecisionToFloat(decomposeData.scaleX)), fabsf(narrowPrecisionToFloat(decomposeData.scaleY)));
 }
 
 #if ENABLE(CSS_FILTERS) || !ASSERT_DISABLED
