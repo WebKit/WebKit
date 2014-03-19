e1384a70f3604c5ae3357cb3a7553834b51b6720
Oliver Hunt <oliver@apple.com>
2014-03-19 18:27:51 +0000

    Don't use ES6 features in testcase.
    Canonical link: https://commits.webkit.org/135908.671@safari-537.75-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-537.75-branch@165904 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/workers/resources/worker-copy-shared-blob-url-worker.js
+++ LayoutTests/fast/workers/resources/worker-copy-shared-blob-url-worker.js
@@ -2,8 +2,10 @@ object = null;
 count = 0
 onmessage = function(message) {
     try {
-        if (!object)
-            object = {[message.data.filename]: message.data.filename};
+        if (!object) {
+            object = {};
+            object[message.data.filename] = message.data.filename
+        }
         if (count++ == 10) {
             object = null;
             for (var i = 0; i < 1000; i++)
