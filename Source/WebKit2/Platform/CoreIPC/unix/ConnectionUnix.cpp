bce350eb97a1d017ffff1bfed18daa90a3227f80
Giovanni Campagna <gcampagna@src.gnome.org>
2014-03-19 08:23:38 +0000

    Merge r165745 - [GTK] Don't busy loop when the socket is full https://bugs.webkit.org/show_bug.cgi?id=129802
    Patch by Giovanni Campagna <gcampagna@src.gnome.org> on 2014-03-17
    Reviewed by Carlos Garcia Campos.
    
    When the socket is full and we see EAGAIN or EWOULDBLOCK
    (because the socket is non blocking), don't busy loop by
    tring to write again, instead poll() until the socket
    becomes writable.
    
    * Platform/IPC/unix/ConnectionUnix.cpp:
    (IPC::Connection::sendOutgoingMessage):

--- Source/WebKit2/Platform/CoreIPC/unix/ConnectionUnix.cpp
+++ Source/WebKit2/Platform/CoreIPC/unix/ConnectionUnix.cpp
@@ -34,6 +34,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <poll.h>
 #include <wtf/Assertions.h>
 #include <wtf/Functional.h>
 #include <wtf/OwnArrayPtr.h>
@@ -555,8 +556,17 @@ bool Connection::sendOutgoingMessage(PassOwnPtr<MessageEncoder> encoder)
 
     int bytesSent = 0;
     while ((bytesSent = sendmsg(m_socketDescriptor, &message, 0)) == -1) {
-        if (errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK)
+        if (errno == EINTR)
             continue;
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            struct pollfd pollfd;
+
+            pollfd.fd = m_socketDescriptor;
+            pollfd.events = POLLOUT;
+            pollfd.revents = 0;
+            poll(&pollfd, 1, -1);
+            continue;
+        }
 
         WTFLogAlways("Error sending IPC message: %s", strerror(errno));
         return false;
