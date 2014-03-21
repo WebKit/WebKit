470a52d1556dd3dbfca4fe4d572f880fc404c032
Oliver Hunt <oliver@apple.com>
2014-03-21 19:12:40 +0000

    Source/WebCore: Fix a crash when assigning an object to document.location https://bugs.webkit.org/show_bug.cgi?id=130213
    Reviewed by Geoffrey Garen.
    
    Convert location to string before we make use the document.
    This prevents us from attempting to navigate a frame that
    has already been removed.
    
    Test: fast/dom/navigation-with-sideeffects-crash.html
    
    * bindings/js/JSDocumentCustom.cpp:
    (WebCore::JSDocument::location):
    (WebCore::JSDocument::setLocation):
    
    LayoutTests: Fix semantics of JS execution when assigning an object to document.location
    https://bugs.webkit.org/show_bug.cgi?id=130213
    
    Reviewed by Geoffrey Garen.
    
    * fast/dom/navigation-with-sideeffects-expected.txt: Added.
    * fast/dom/navigation-with-sideeffects.html: Added.
    
    Canonical link: https://commits.webkit.org/148631@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@166090 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/js/JSDocumentCustom.cpp
+++ Source/WebCore/bindings/js/JSDocumentCustom.cpp
@@ -48,30 +48,30 @@ namespace WebCore {
 
 JSValue JSDocument::location(ExecState* exec) const
 {
-    Frame* frame = impl().frame();
+    RefPtr<Frame> frame = impl().frame();
     if (!frame)
         return jsNull();
 
-    Location* location = frame->document()->domWindow()->location();
-    if (JSObject* wrapper = getCachedWrapper(globalObject()->world(), location))
+    RefPtr<Location> location = frame->document()->domWindow()->location();
+    if (JSObject* wrapper = getCachedWrapper(globalObject()->world(), location.get()))
         return wrapper;
 
-    JSLocation* jsLocation = JSLocation::create(getDOMStructure<JSLocation>(exec->vm(), globalObject()), globalObject(), location);
-    cacheWrapper(globalObject()->world(), location, jsLocation);
+    JSLocation* jsLocation = JSLocation::create(getDOMStructure<JSLocation>(exec->vm(), globalObject()), globalObject(), location.get());
+    cacheWrapper(globalObject()->world(), location.get(), jsLocation);
     return jsLocation;
 }
 
 void JSDocument::setLocation(ExecState* exec, JSValue value)
 {
-    Frame* frame = impl().frame();
-    if (!frame)
-        return;
-
     String locationString = value.toString(exec)->value(exec);
     if (exec->hadException())
         return;
 
-    if (Location* location = frame->document()->domWindow()->location())
+    RefPtr<Frame> frame = impl().frame();
+    if (!frame)
+        return;
+
+    if (RefPtr<Location> location = frame->document()->domWindow()->location())
         location->setHref(locationString, activeDOMWindow(exec), firstDOMWindow(exec));
 }
 
