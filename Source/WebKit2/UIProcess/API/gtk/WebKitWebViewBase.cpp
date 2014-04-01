2a6e0a6ff027388d4b7e358fc66c2f1533ffd13c
Žan Doberšek <zdobersek@igalia.com>
2014-04-01 09:11:14 +0000

    Don't copy the children map in webkitWebViewBaseContainerForall https://bugs.webkit.org/show_bug.cgi?id=131016
    Reviewed by Carlos Garcia Campos.
    
    * UIProcess/API/gtk/WebKitWebViewBase.cpp:
    (webkitWebViewBaseContainerForall): Avoid copying the GtkWidget-to-IntRect HashMap object
    by transforming the iteration to a range-based for-loop over that HashMap's keys.
    
    
    Canonical link: https://commits.webkit.org/149083@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@166575 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/gtk/WebKitWebViewBase.cpp
+++ Source/WebKit2/UIProcess/API/gtk/WebKitWebViewBase.cpp
@@ -363,10 +363,8 @@ static void webkitWebViewBaseContainerForall(GtkContainer* container, gboolean i
     WebKitWebViewBase* webView = WEBKIT_WEB_VIEW_BASE(container);
     WebKitWebViewBasePrivate* priv = webView->priv;
 
-    WebKitWebViewChildrenMap children = priv->children;
-    WebKitWebViewChildrenMap::const_iterator end = children.end();
-    for (WebKitWebViewChildrenMap::const_iterator current = children.begin(); current != end; ++current)
-        (*callback)(current->key, callbackData);
+    for (const auto& widget : priv->children.keys())
+        (*callback)(widget, callbackData);
 
     if (includeInternals && priv->inspectorView)
         (*callback)(priv->inspectorView, callbackData);
