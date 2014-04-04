52b7f77653e3a1391d5d420fc0d4c9f03f1eb1af
Alexey Proskuryakov <ap@apple.com>
2014-04-04 22:56:27 +0000

    Improve error checking in WebPageProxy::didReceiveEvent https://bugs.webkit.org/show_bug.cgi?id=131243 <rdar://problem/16529207>
    Reviewed by Anders Carlsson.
    
    * UIProcess/WebPageProxy.cpp: (WebKit::WebPageProxy::didReceiveEvent):
    
    
    
    Canonical link: https://commits.webkit.org/149295@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@166810 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/WebPageProxy.cpp
+++ Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -3692,7 +3692,7 @@ void WebPageProxy::didReceiveEvent(uint32_t opaqueType, bool handled)
         break;
 
     case WebEvent::Wheel: {
-        ASSERT(!m_currentlyProcessedWheelEvents.isEmpty());
+        MESSAGE_CHECK(!m_currentlyProcessedWheelEvents.isEmpty());
 
         OwnPtr<Vector<NativeWebWheelEvent>> oldestCoalescedEvent = m_currentlyProcessedWheelEvents.takeFirst();
 
@@ -3716,10 +3716,10 @@ void WebPageProxy::didReceiveEvent(uint32_t opaqueType, bool handled)
     case WebEvent::Char: {
         LOG(KeyHandling, "WebPageProxy::didReceiveEvent: %s", webKeyboardEventTypeString(type));
 
-        NativeWebKeyboardEvent event = m_keyEventQueue.first();
-        MESSAGE_CHECK(type == event.type());
+        MESSAGE_CHECK(!m_keyEventQueue.isEmpty());
+        NativeWebKeyboardEvent event = m_keyEventQueue.takeFirst();
 
-        m_keyEventQueue.removeFirst();
+        MESSAGE_CHECK(type == event.type());
 
         if (!m_keyEventQueue.isEmpty())
             m_process->send(Messages::WebPage::KeyEvent(m_keyEventQueue.first()), m_pageID);
@@ -3737,9 +3737,10 @@ void WebPageProxy::didReceiveEvent(uint32_t opaqueType, bool handled)
     case WebEvent::TouchMove:
     case WebEvent::TouchEnd:
     case WebEvent::TouchCancel: {
-        QueuedTouchEvents queuedEvents = m_touchEventQueue.first();
+        MESSAGE_CHECK(!m_touchEventQueue.isEmpty());
+        QueuedTouchEvents queuedEvents = m_touchEventQueue.takeFirst();
+
         MESSAGE_CHECK(type == queuedEvents.forwardedEvent.type());
-        m_touchEventQueue.removeFirst();
 
         m_pageClient.doneWithTouchEvent(queuedEvents.forwardedEvent, handled);
         for (size_t i = 0; i < queuedEvents.deferredTouchEvents.size(); ++i) {
