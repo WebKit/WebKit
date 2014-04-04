361b17c12c25babaa0ff396e77f83e6bfdf32467
Jer Noble <jer.noble@apple.com>
2014-04-04 23:15:10 +0000

    [Mac][WK2] Videos do not have access to session cookies https://bugs.webkit.org/show_bug.cgi?id=129687
    Reviewed by Eric Carlson.
    
    Update the CookieStorageShim to track changes in underlying networking stack.
    
    * Shared/mac/CookieStorageShim.mm:
    (WebKit::CookieStorageShim::initialize):
    (-[WKNSURLSessionLocal _getCookieHeadersForTask:completionHandler:]):
    
    
    Canonical link: https://commits.webkit.org/149297@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@166812 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/Shared/mac/CookieStorageShim.mm
+++ Source/WebKit2/Shared/mac/CookieStorageShim.mm
@@ -31,11 +31,11 @@
 #include "CookieStorageShimLibrary.h"
 #include "NetworkConnectionToWebProcess.h"
 #include "NetworkProcessConnection.h"
-#include <WebCore/SessionID.h>
 #include "WebCoreArgumentCoders.h"
 #include "WebProcess.h"
-#include <WebCore/URL.h>
+#include <WebCore/SessionID.h>
 #include <WebCore/SoftLinking.h>
+#include <WebCore/URL.h>
 #include <dlfcn.h>
 #include <wtf/MainThread.h>
 #include <wtf/RetainPtr.h>
@@ -50,6 +50,9 @@ using namespace WebCore;
 
 @interface WKNSURLSessionLocal : NSObject
 - (CFDictionaryRef) _copyCookiesForRequestUsingAllAppropriateStorageSemantics:(CFURLRequestRef) request;
+#if PLATFORM(IOS) || __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
+- (void)_getCookieHeadersForTask:(NSURLSessionTask*)task completionHandler:(void (^)(CFDictionaryRef))completionHandler;
+#endif
 @end
 
 namespace WebKit {
@@ -87,14 +90,19 @@ void CookieStorageShim::initialize()
     if (!__NSURLSessionLocalClass)
         return;
 
-    Method original = class_getInstanceMethod(__NSURLSessionLocalClass, @selector(_copyCookiesForRequestUsingAllAppropriateStorageSemantics:));
-    if (!original)
-        return;
+    if (Method original = class_getInstanceMethod(__NSURLSessionLocalClass, @selector(_copyCookiesForRequestUsingAllAppropriateStorageSemantics:))) {
+        Method replacement = class_getInstanceMethod([WKNSURLSessionLocal class], @selector(_copyCookiesForRequestUsingAllAppropriateStorageSemantics:));
+        ASSERT(replacement);
+        method_exchangeImplementations(original, replacement);
+    }
 
-    Method replacement = class_getInstanceMethod([WKNSURLSessionLocal class], @selector(_copyCookiesForRequestUsingAllAppropriateStorageSemantics:));
-    ASSERT(replacement);
-
-    method_exchangeImplementations(original, replacement);
+#if PLATFORM(IOS) || __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
+    if (Method original = class_getInstanceMethod(__NSURLSessionLocalClass, @selector(_getCookieHeadersForTask:completionHandler:))) {
+        Method replacement = class_getInstanceMethod([WKNSURLSessionLocal class], @selector(_getCookieHeadersForTask:completionHandler:));
+        ASSERT(replacement);
+        method_exchangeImplementations(original, replacement);
+    }
+#endif
 }
 
 }
@@ -104,6 +112,18 @@ void CookieStorageShim::initialize()
 {
     return WebKit::webKitCookieStorageCopyRequestHeaderFieldsForURL(nullptr, CFURLRequestGetURL(request));
 }
+
+#if PLATFORM(IOS) || __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
+- (void)_getCookieHeadersForTask:(NSURLSessionTask*)task completionHandler:(void (^)(CFDictionaryRef))completionHandler
+{
+    if (!completionHandler)
+        return;
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        completionHandler(WebKit::webKitCookieStorageCopyRequestHeaderFieldsForURL(nullptr, (CFURLRef)[[task currentRequest] URL]));
+    });
+}
+#endif
 @end
 
 #endif // ENABLE(NETWORK_PROCESS)
