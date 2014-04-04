d48c80052cbb7f47dd39f9ea2905b29893db1ee7
Tamas Gergely <tgergely.u-szeged@partner.samsung.com>
2014-04-04 10:24:59 +0000

    [EFL] Build error due to changed path of generated js files. https://bugs.webkit.org/show_bug.cgi?id=131215
    Reviewed by Gyuyoung Kim.
    
    r166648 fixed the generation of some .js files on GTK. Due to some
    changes in the paths it causes a build error on EFL.
    
    * Source/PlatformEfl.cmake:
      Replaced path for two derived source files.
    
    
    Canonical link: https://commits.webkit.org/149269@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@166777 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/PlatformEfl.cmake
+++ Source/PlatformEfl.cmake
@@ -4,8 +4,8 @@ if (ENABLE_INSPECTOR)
     add_custom_target(
         web-inspector-resources ALL
         COMMAND ${CMAKE_COMMAND} -E copy_directory ${WEBINSPECTORUI_DIR}/UserInterface ${WEB_INSPECTOR_DIR}
-        COMMAND ${CMAKE_COMMAND} -E copy ${DERIVED_SOURCES_JAVASCRIPTCORE_DIR}/InspectorJSBackendCommands.js ${WEB_INSPECTOR_DIR}/Protocol
-        COMMAND ${CMAKE_COMMAND} -E copy ${DERIVED_SOURCES_WEBCORE_DIR}/InspectorWebBackendCommands.js ${WEB_INSPECTOR_DIR}/Protocol
+        COMMAND ${CMAKE_COMMAND} -E copy ${DERIVED_SOURCES_WEBINSPECTORUI_DIR}/UserInterface/Protocol/InspectorJSBackendCommands.js ${WEB_INSPECTOR_DIR}/Protocol
+        COMMAND ${CMAKE_COMMAND} -E copy ${DERIVED_SOURCES_WEBINSPECTORUI_DIR}/UserInterface/Protocol/InspectorWebBackendCommands.js ${WEB_INSPECTOR_DIR}/Protocol
         COMMAND ${CMAKE_COMMAND} -E copy ${WEBINSPECTORUI_DIR}/Localizations/en.lproj/localizedStrings.js ${WEB_INSPECTOR_DIR}
         COMMAND ${CMAKE_COMMAND} -E copy ${WEBKIT2_DIR}/UIProcess/InspectorServer/front-end/inspectorPageIndex.html ${WEB_INSPECTOR_DIR}
         DEPENDS JavaScriptCore WebCore
