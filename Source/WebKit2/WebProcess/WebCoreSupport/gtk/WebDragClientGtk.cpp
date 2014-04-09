902546beeebf1978b6af8098b1fefb1a55065d7e
Carlos Garcia Campos <cgarcia@igalia.com>
2014-04-09 07:31:08 +0000

    Unreviewed. Fix GTK+ build after r166965.
    * WebProcess/WebCoreSupport/gtk/WebDragClientGtk.cpp:
    (WebKit::WebDragClient::startDrag): Rename Clipboard to DataTransfer.
    
    Canonical link: https://commits.webkit.org/149490@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@167005 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/WebCoreSupport/gtk/WebDragClientGtk.cpp
+++ Source/WebKit2/WebProcess/WebCoreSupport/gtk/WebDragClientGtk.cpp
@@ -32,8 +32,8 @@
 #include "ShareableBitmap.h"
 #include "WebPage.h"
 #include "WebPageProxyMessages.h"
-#include <WebCore/Clipboard.h>
 #include <WebCore/DataObjectGtk.h>
+#include <WebCore/DataTransfer.h>
 #include <WebCore/DragData.h>
 #include <WebCore/GraphicsContext.h>
 #include <WebCore/Pasteboard.h>
@@ -56,7 +56,7 @@ static PassRefPtr<ShareableBitmap> convertCairoSurfaceToShareableBitmap(cairo_su
     return bitmap.release();
 }
 
-void WebDragClient::startDrag(DragImageRef dragImage, const IntPoint& clientPosition, const IntPoint& globalPosition, Clipboard& clipboard, Frame&, bool)
+void WebDragClient::startDrag(DragImageRef dragImage, const IntPoint& clientPosition, const IntPoint& globalPosition, DataTransfer& dataTransfer, Frame&, bool)
 {
     RefPtr<ShareableBitmap> bitmap = convertCairoSurfaceToShareableBitmap(dragImage);
     ShareableBitmap::Handle handle;
@@ -65,8 +65,8 @@ void WebDragClient::startDrag(DragImageRef dragImage, const IntPoint& clientPosi
     if (bitmap && !bitmap->createHandle(handle))
         return;
 
-    RefPtr<DataObjectGtk> dataObject = clipboard.pasteboard().dataObject();
-    DragData dragData(dataObject.get(), clientPosition, globalPosition, clipboard.sourceOperation());
+    RefPtr<DataObjectGtk> dataObject = dataTransfer.pasteboard().dataObject();
+    DragData dragData(dataObject.get(), clientPosition, globalPosition, dataTransfer.sourceOperation());
     m_page->send(Messages::WebPageProxy::StartDrag(dragData, handle));
 }
 
