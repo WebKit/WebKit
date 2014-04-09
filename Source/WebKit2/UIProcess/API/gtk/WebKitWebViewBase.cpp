81757330ecd1469aeefd5e88bdae800e0c9ad49a
Martin Robinson <mrobinson@webkit.org>
2014-04-09 23:37:39 +0000

    Try to fix the GTK+ build after resetDragOperation rename
    * UIProcess/API/gtk/WebKitWebViewBase.cpp:
    (webkitWebViewBaseDragDataReceived): Rename resetDragOperation to resetDragSession.
    (dragExitedCallback): Ditto.
    
    Canonical link: https://commits.webkit.org/149533@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@167051 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/gtk/WebKitWebViewBase.cpp
+++ Source/WebKit2/UIProcess/API/gtk/WebKitWebViewBase.cpp
@@ -777,7 +777,7 @@ static void webkitWebViewBaseDragDataReceived(GtkWidget* widget, GdkDragContext*
         return;
 
     DragData dragData(dataObject, position, convertWidgetPointToScreenPoint(widget, position), gdkDragActionToDragOperation(gdk_drag_context_get_actions(context)));
-    webViewBase->priv->pageProxy->resetDragOperation();
+    webViewBase->priv->pageProxy->resetDragSession();
     webViewBase->priv->pageProxy->dragEntered(dragData);
     DragOperation operation = webViewBase->priv->pageProxy->dragSession().operation;
     gdk_drag_status(context, dragOperationToSingleGdkDragAction(operation), time);
@@ -837,7 +837,7 @@ static void dragExitedCallback(GtkWidget* widget, DragData& dragData, bool dropH
 
     WebKitWebViewBase* webViewBase = WEBKIT_WEB_VIEW_BASE(widget);
     webViewBase->priv->pageProxy->dragExited(dragData);
-    webViewBase->priv->pageProxy->resetDragOperation();
+    webViewBase->priv->pageProxy->resetDragSession();
 }
 
 static void webkitWebViewBaseDragLeave(GtkWidget* widget, GdkDragContext* context, guint /* time */)
