c896b5ccb1233455fc497e75c927a4a7be91a2df
Antti Koivisto <antti@apple.com>
2014-04-11 21:01:16 +0000

    Try to fix windows build.
    Patch by Antti Koivisto <antti@apple.com> on 2014-04-11
    
    * platform/graphics/ca/GraphicsLayerCA.h:
    
    
    
    Canonical link: https://commits.webkit.org/149619@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@167147 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/English.lproj/mediaControlsLocalizedStringsiOS.js
+++ Source/WebCore/English.lproj/mediaControlsLocalizedStringsiOS.js
@@ -1,7 +1,7 @@
 function mediaControlsLocalizedStringsiOS() {
     return {
         '##AIRPLAY_DEVICE_TYPE##': 'AirPlay',
-        '##AIRPLAY_DEVICE_NAME##': 'This video is playing on "##DEVICE_NAME##".',
+        '##AIRPLAY_DEVICE_NAME##': 'This video is playing on “##DEVICE_NAME##”.',
 
         '##TVOUT_DEVICE_TYPE##': 'TV Connected',
         '##TVOUT_DEVICE_NAME##': 'This video is playing on the TV.',
