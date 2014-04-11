8cecaa07a997f92b311baddb9d5ae4fe397e97ac
Brent Fulgham <bfulgham@apple.com>
2014-04-11 20:36:20 +0000

    [Win] Build fix after r167138
    * platform/graphics/ca/GraphicsLayerCA.h: The TileController is
    only available on Mac/Cocoa builds.
    
    
    
    Canonical link: https://commits.webkit.org/149616@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@167144 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
+++ Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
@@ -30,11 +30,14 @@
 #include "PlatformCAAnimation.h"
 #include "PlatformCALayer.h"
 #include "PlatformCALayerClient.h"
-#include "TileController.h"
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
 #include <wtf/RetainPtr.h>
 #include <wtf/text/StringHash.h>
+
+#if PLATFORM(COCOA)
+#include "TileController.h"
+#endif
 
 // Enable this to add a light red wash over the visible portion of Tiled Layers, as computed
 // by flushCompositingState().
