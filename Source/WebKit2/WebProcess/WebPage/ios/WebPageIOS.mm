c367faec61e5c3f0487c286ee86471e1149098e7
Dan Bernstein <mitz@apple.com>
2014-05-16 00:02:53 +0000

    Fixed a typo in a comment and updated previous change log entry.
    * WebProcess/WebPage/ios/WebPageIOS.mm:
    (WebKit::WebPage::elementDidFocus):
    
    
    
    Canonical link: https://commits.webkit.org/151024@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@168917 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/WebPage/ios/WebPageIOS.mm
+++ Source/WebKit2/WebProcess/WebPage/ios/WebPageIOS.mm
@@ -1880,7 +1880,7 @@ void WebPage::elementDidFocus(WebCore::Node* node)
         // FIXME: We check m_userIsInteracting so that we don't begin an input session for a
         // programmatic focus that doesn't cause the keyboard to appear. But this misses the case of
         // a programmatic focus happening while the keyboard is already shown. Once we have a way to
-        // know the keyboard state in the Web Porcess, we should refine the condition.
+        // know the keyboard state in the Web Process, we should refine the condition.
         if (m_userIsInteracting)
             m_formClient->willBeginInputSession(this, toElement(node), WebFrame::fromCoreFrame(*node->document().frame()), userData);
 
