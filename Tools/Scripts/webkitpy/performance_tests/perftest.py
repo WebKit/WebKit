3d3355cc8ec47ae4190d21a26fcbd3d5dd606be7
Geoffrey Garen <ggaren@apple.com>
2014-05-23 22:37:19 +0000

    run-perf-tests should ignore whitespace lines when snarfing test output https://bugs.webkit.org/show_bug.cgi?id=133238
    Reviewed by Ryosuke Niwa.
    
    Required for PerformanceTests/Canvas/terrain.html.
    
    * Scripts/webkitpy/performance_tests/perftest.py:
    (PerfTest._run_with_driver):
    (PerfTest):
    
    
    Canonical link: https://commits.webkit.org/151252@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@169286 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/performance_tests/perftest.py
+++ Tools/Scripts/webkitpy/performance_tests/perftest.py
@@ -189,7 +189,7 @@ class PerfTest(object):
 
             metric_match = self._metrics_regex.match(line)
             if not metric_match:
-                _log.error('ERROR: ' + line)
+                _log.error('ERROR: [' + line + ']')
                 return False
 
             metric = self._ensure_metrics(metric_match.group('metric'), metric_match.group('subtest'), metric_match.group('unit'), metric_match.group('aggregator'))
@@ -242,6 +242,7 @@ class PerfTest(object):
         return False
 
     _lines_to_ignore_in_parser_result = [
+        re.compile("^\s+$"),
         # Following are for handle existing test like Dromaeo
         re.compile(re.escape("""main frame - has 1 onunload handler(s)""")),
         re.compile(re.escape("""frame "<!--framePath //<!--frame0-->-->" - has 1 onunload handler(s)""")),
