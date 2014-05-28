f6a3a8203cb8e360e30dcc6bc57ced81bdc3d27e
Zoltan Horvath <zoltan@webkit.org>
2014-05-28 00:28:52 +0000

    [CSS Shapes] serialization of the computed value should omit the default radii https://bugs.webkit.org/show_bug.cgi?id=132747
    Reviewed by Darin Adler.
    
    Source/WebCore:
    We should omit closest-side value, whereever is possible. Specification:
    http://www.w3.org/TR/2014/CR-css-shapes-1-20140320/#basic-shape-computed-values
    This change aligns the behavior to the specification.
    
    Added new test cases for the shape-outside parsing tests.
    
    * css/CSSBasicShapes.cpp:
    (WebCore::buildCircleString):
    (WebCore::CSSBasicShapeCircle::cssText):
    (WebCore::CSSBasicShapeEllipse::cssText):
    
    LayoutTests:
    * fast/shapes/parsing/parsing-shape-outside-expected.txt:
    * fast/shapes/parsing/parsing-test-utils.js:
    
    
    Canonical link: https://commits.webkit.org/151359@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@169406 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/CSSBasicShapes.cpp
+++ Source/WebCore/css/CSSBasicShapes.cpp
@@ -88,7 +88,7 @@ static String buildCircleString(const String& radius, const String& centerX, con
     char separator[] = " ";
     StringBuilder result;
     result.appendLiteral(opening);
-    if (!radius.isNull()) 
+    if (!radius.isNull())
         result.append(radius);
 
     if (!centerX.isNull() || !centerY.isNull()) {
@@ -113,7 +113,11 @@ String CSSBasicShapeCircle::cssText() const
     RefPtr<CSSPrimitiveValue> normalizedCX = buildSerializablePositionOffset(m_centerX, CSSValueLeft);
     RefPtr<CSSPrimitiveValue> normalizedCY = buildSerializablePositionOffset(m_centerY, CSSValueTop);
 
-    return buildCircleString(m_radius ? m_radius->cssText() : String(),
+    String radius;
+    if (m_radius && m_radius->getValueID() != CSSValueClosestSide)
+        radius = m_radius->cssText();
+
+    return buildCircleString(radius,
         serializePositionOffset(*normalizedCX->getPairValue(), *normalizedCY->getPairValue()),
         serializePositionOffset(*normalizedCY->getPairValue(), *normalizedCX->getPairValue()),
         m_referenceBox ? m_referenceBox->cssText() : String());
@@ -172,8 +176,21 @@ String CSSBasicShapeEllipse::cssText() const
     RefPtr<CSSPrimitiveValue> normalizedCX = buildSerializablePositionOffset(m_centerX, CSSValueLeft);
     RefPtr<CSSPrimitiveValue> normalizedCY = buildSerializablePositionOffset(m_centerY, CSSValueTop);
 
-    return buildEllipseString(m_radiusX ? m_radiusX->cssText() : String(),
-        m_radiusY ? m_radiusY->cssText() : String(),
+    String radiusX;
+    String radiusY;
+    if (m_radiusX) {
+        bool shouldSerializeRadiusXValue = m_radiusX->getValueID() != CSSValueClosestSide;
+        bool shouldSerializeRadiusYValue = false;
+
+        if (m_radiusY) {
+            shouldSerializeRadiusYValue = m_radiusY->getValueID() != CSSValueClosestSide;
+            if (shouldSerializeRadiusYValue)
+                radiusY = m_radiusY->cssText();
+        }
+        if (shouldSerializeRadiusXValue || (!shouldSerializeRadiusXValue && shouldSerializeRadiusYValue))
+            radiusX = m_radiusX->cssText();
+    }
+    return buildEllipseString(radiusX, radiusY,
         serializePositionOffset(*normalizedCX->getPairValue(), *normalizedCY->getPairValue()),
         serializePositionOffset(*normalizedCY->getPairValue(), *normalizedCX->getPairValue()),
         m_referenceBox ? m_referenceBox->cssText() : String());
