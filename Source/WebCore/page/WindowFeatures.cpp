bf9e3bc86c32da395906e69d853828447fbeff48
David Kilzer <ddkilzer@webkit.org>
2014-06-11 22:41:03 +0000

    WindowFeatures arguments shoud ignore invalid characters in values <http://webkit.org/b/133703> <rdar://problem/17254118>
    Reviewed by Andy Estes.
    
    Source/WebCore:
    
    Test: fast/dom/Window/window-property-invalid-characters-ignored.html
    
    * page/WindowFeatures.cpp:
    (WebCore::WindowFeatures::WindowFeatures): Set |length| based on
    |buffer|, not |features|.  Switch to using a for loop.  Switch
    to unsigned types since we are working with positive offsets
    into a String.
    
    LayoutTests:
    
    * fast/dom/Window/resources/window-property-invalid-characters-ignored.html: Added.
    * fast/dom/Window/window-property-invalid-characters-ignored-expected.txt: Added.
    * fast/dom/Window/window-property-invalid-characters-ignored.html: Added.
    
    Canonical link: https://commits.webkit.org/151747@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@169849 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/WindowFeatures.cpp
+++ Source/WebCore/page/WindowFeatures.cpp
@@ -74,13 +74,12 @@ WindowFeatures::WindowFeatures(const String& features)
     scrollbarsVisible = false;
 
     // Tread lightly in this code -- it was specifically designed to mimic Win IE's parsing behavior.
-    int keyBegin, keyEnd;
-    int valueBegin, valueEnd;
+    unsigned keyBegin, keyEnd;
+    unsigned valueBegin, valueEnd;
 
-    int i = 0;
-    int length = features.length();
     String buffer = features.lower();
-    while (i < length) {
+    unsigned length = buffer.length();
+    for (unsigned i = 0; i < length; ) {
         // skip to first non-separator, but don't skip past the end of the string
         while (isWindowFeaturesSeparator(buffer[i])) {
             if (i >= length)
