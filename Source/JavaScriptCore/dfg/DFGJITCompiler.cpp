18eea1fa8b0031be3bb0fb06a201c007be3b267c
Mark Lam <mark.lam@apple.com>
2014-06-17 23:05:48 +0000

    DFGGraph::m_doubleConstantMap will not map 0 values correctly. <https://webkit.org/b/133994>
    Reviewed by Geoffrey Garen.
    
    DFGGraph::m_doubleConstantsMap should not use a double as a key to its HashMap,
    because it means two unfortunate things:
    - It will probably break for zero.
    - It will think that -0 is the same as +0 under some circumstances, size
      -0==+0 even though they are distinct values (for example 1/-0 != 1/+0).
    
    The fix is to use std::unordered_map which does not require special empty
    and deleted values, and to use the raw bits instead of the double value as
    the key.
    
    * dfg/DFGGraph.h:
    * dfg/DFGJITCompiler.cpp:
    (JSC::DFG::JITCompiler::addressOfDoubleConstant):
    
    
    Canonical link: https://commits.webkit.org/151953@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@170082 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGJITCompiler.cpp
+++ Source/JavaScriptCore/dfg/DFGJITCompiler.cpp
@@ -443,8 +443,8 @@ void* JITCompiler::addressOfDoubleConstant(Node* node)
     JSValue jsvalue = node->valueOfJSConstant(codeBlock());
     ASSERT(jsvalue.isDouble());
 
-    double value = jsvalue.asDouble();
-    auto it = m_graph.m_doubleConstantsMap.find(value);
+    int64_t valueBits = jsvalue.asMachineInt();
+    auto it = m_graph.m_doubleConstantsMap.find(valueBits);
     if (it != m_graph.m_doubleConstantsMap.end())
         return it->value;
 
@@ -452,8 +452,8 @@ void* JITCompiler::addressOfDoubleConstant(Node* node)
         m_graph.m_doubleConstants = std::make_unique<Bag<double>>();
 
     double* addressInConstantPool = m_graph.m_doubleConstants->add();
-    *addressInConstantPool = value;
-    m_graph.m_doubleConstantsMap.add(value, addressInConstantPool);
+    *addressInConstantPool = jsvalue.asDouble();
+    m_graph.m_doubleConstantsMap[valueBits] = addressInConstantPool;
     return addressInConstantPool;
 }
 #endif
