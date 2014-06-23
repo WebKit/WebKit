0aaca16e5b3167a6f17eda22f3d3aa6e25b0931e
Andreas Kling <akling@apple.com>
2014-06-23 19:58:19 +0000

    Inline the UnlinkedInstructionStream::Reader logic. <https://webkit.org/b/134203>
    This class is only used by CodeBlock to unpack the unlinked instructions,
    and we were spending 0.5% of total time on PLT calling Reader::next().
    Move the logic to the header file and mark it ALWAYS_INLINE.
    
    Reviewed by Geoffrey Garen.
    
    * bytecode/UnlinkedInstructionStream.cpp:
    * bytecode/UnlinkedInstructionStream.h:
    (JSC::UnlinkedInstructionStream::Reader::Reader):
    (JSC::UnlinkedInstructionStream::Reader::read8):
    (JSC::UnlinkedInstructionStream::Reader::read32):
    (JSC::UnlinkedInstructionStream::Reader::next):
    
    
    Canonical link: https://commits.webkit.org/152171@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@170312 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/bytecode/UnlinkedInstructionStream.h
+++ Source/JavaScriptCore/bytecode/UnlinkedInstructionStream.h
@@ -70,6 +70,81 @@ private:
     unsigned m_instructionCount;
 };
 
+// Unlinked instructions are packed in a simple stream format.
+//
+// The first byte is always the opcode.
+// It's followed by an opcode-dependent number of argument values.
+// The first 3 bits of each value determines the format:
+//
+//     5-bit positive integer (1 byte total)
+//     5-bit negative integer (1 byte total)
+//     13-bit positive integer (2 bytes total)
+//     13-bit negative integer (2 bytes total)
+//     5-bit constant register index, based at 0x40000000 (1 byte total)
+//     13-bit constant register index, based at 0x40000000 (2 bytes total)
+//     32-bit raw value (5 bytes total)
+
+enum PackedValueType {
+    Positive5Bit = 0,
+    Negative5Bit,
+    Positive13Bit,
+    Negative13Bit,
+    ConstantRegister5Bit,
+    ConstantRegister13Bit,
+    Full32Bit
+};
+
+ALWAYS_INLINE UnlinkedInstructionStream::Reader::Reader(const UnlinkedInstructionStream& stream)
+    : m_stream(stream)
+    , m_index(0)
+{
+}
+
+ALWAYS_INLINE unsigned char UnlinkedInstructionStream::Reader::read8()
+{
+    return m_stream.m_data.data()[m_index++];
+}
+
+ALWAYS_INLINE unsigned UnlinkedInstructionStream::Reader::read32()
+{
+    const unsigned char* data = &m_stream.m_data.data()[m_index];
+    unsigned char type = data[0] >> 5;
+
+    switch (type) {
+    case Positive5Bit:
+        m_index++;
+        return data[0];
+    case Negative5Bit:
+        m_index++;
+        return 0xffffffe0 | data[0];
+    case Positive13Bit:
+        m_index += 2;
+        return ((data[0] & 0x1F) << 8) | data[1];
+    case Negative13Bit:
+        m_index += 2;
+        return 0xffffe000 | ((data[0] & 0x1F) << 8) | data[1];
+    case ConstantRegister5Bit:
+        m_index++;
+        return 0x40000000 | (data[0] & 0x1F);
+    case ConstantRegister13Bit:
+        m_index += 2;
+        return 0x40000000 | ((data[0] & 0x1F) << 8) | data[1];
+    default:
+        ASSERT(type == Full32Bit);
+        m_index += 5;
+        return data[1] | data[2] << 8 | data[3] << 16 | data[4] << 24;
+    }
+}
+
+ALWAYS_INLINE const UnlinkedInstruction* UnlinkedInstructionStream::Reader::next()
+{
+    m_unpackedBuffer[0].u.opcode = static_cast<OpcodeID>(read8());
+    unsigned opLength = opcodeLength(m_unpackedBuffer[0].u.opcode);
+    for (unsigned i = 1; i < opLength; ++i)
+        m_unpackedBuffer[i].u.index = read32();
+    return m_unpackedBuffer;
+}
+
 } // namespace JSC
 
 #endif // UnlinkedInstructionStream_h
