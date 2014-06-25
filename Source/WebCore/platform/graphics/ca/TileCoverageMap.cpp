2cb602ffab59295637308c47e2cff14fe847b281
Commit Queue <commit-queue@webkit.org>
2014-06-25 04:30:40 +0000

    Unreviewed, rolling out r170414. https://bugs.webkit.org/show_bug.cgi?id=134291
    the patch added a test that fails on almost every Mac tester
    (Requested by rniwa on #webkit).
    
    Reverted changeset:
    
    "Tiles on bottom of screen are not always allocated when
    necessary"
    https://bugs.webkit.org/show_bug.cgi?id=134272
    http://trac.webkit.org/changeset/170414
    
    Canonical link: https://commits.webkit.org/152267@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@170417 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/ca/TileCoverageMap.cpp
+++ Source/WebCore/platform/graphics/ca/TileCoverageMap.cpp
@@ -63,6 +63,7 @@ void TileCoverageMap::update()
     FloatRect containerBounds = m_controller.bounds();
     FloatRect visibleRect = m_controller.visibleRect();
     visibleRect.contract(4, 4); // Layer is positioned 2px from top and left edges.
+    visibleRect.setHeight(visibleRect.height() - m_controller.topContentInset());
 
     float widthScale = 1;
     float scale = 1;
