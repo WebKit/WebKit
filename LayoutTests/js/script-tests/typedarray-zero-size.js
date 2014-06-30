f72c61b74a42079c7dc1773a8c382b986ea9ef70
Michael Saboff <msaboff@apple.com>
2014-06-30 17:37:06 +0000

    Reduce memory required for js/typedarray-zero-size.js https://bugs.webkit.org/show_bug.cgi?id=134448
    Reviewed by Darin Adler.
    
    Making the loop count down reduces memory usage from ~190MB to ~22MB while
    still testing the original issue.
    
    * js/script-tests/typedarray-zero-size.js:
    
    
    Canonical link: https://commits.webkit.org/152423@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@170586 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/js/script-tests/typedarray-zero-size.js
+++ LayoutTests/js/script-tests/typedarray-zero-size.js
@@ -10,7 +10,7 @@ function foo() {
 
 var result = 0;
 
-for (var i = 1; i < 10001; i++) {
+for (var i = 10000; i > 0; i--) {
     var newArray = foo();
     var otherArray = new Array(i);
     for (var j = 0; j < i; ++j)
