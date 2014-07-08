WebKit/WebKit

commit c6f2e4edceb03e39eb7b1a0113e0bbbad4c19117

Web Inspector: Remove WebInspector.EventHandler in favor of WebInspector.EventListenerSet https://bugs.webkit.org/show_bug.cgi?id=129185
Patch by Diego Pino Garcia <dpino@igalia.com> on 2014-03-04
Reviewed by Timothy Hatcher.

* UserInterface/Base/EventHandler.js: Removed.
* UserInterface/Main.html: Don't include EventHandler.js.
* UserInterface/Views/SourceCodeTextEditor.js:
(WebInspector.SourceCodeTextEditor.prototype._dismissPopover):
(WebInspector.SourceCodeTextEditor.prototype._trackPopoverEvents):
Use EventListenerSet instead of EventHandler.

Canonical link: https://commits.webkit.org/147718@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@165052 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebInspectorUI/UserInterface/Views/SourceCodeTextEditor.js
@@ -1260,20 +1260,16 @@ WebInspector.SourceCodeTextEditor.prototype = {
 
         this._popover.dismiss();
 
-        if (this._popoverEventHandler)
-            this._popoverEventHandler.stopTrackingEvents();
+        if (this._popoverEventListeners)
+            this._popoverEventListeners.unregister();
     },
 
     _trackPopoverEvents: function()
     {
-        if (!this._popoverEventHandler) {
-            this._popoverEventHandler = new WebInspector.EventHandler(this, {
-                "mouseover": this._popoverMouseover,
-                "mouseout": this._popoverMouseout,
-            });
-        }
-
-        this._popoverEventHandler.trackEvents(this._popover.element);
+        if (!this._popoverEventListeners) 
+            this._popoverEventListeners = new WebInspector.EventListenerSet(this, "Popover listeners");
+        this._popoverEventListeners.register(this._popover.element, "mouseover", this._popoverMouseover);
+        this._popoverEventListeners.register(this._popover.element, "mouseout", this._popoverMouseout);
     },
 
     _popoverMouseover: function(event)

Source/WebInspectorUI/UserInterface/Base/EventHandler.js
@@ -1,79 +1,0 @@
-/*
- * Copyright (C) 2013 Apple Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-WebInspector.EventHandler = function(delegate, eventHandlers)
-{
-    this._delegate = delegate;
-    this._eventHandlers = eventHandlers;
-
-    this._tracking = false;
-    this._target = null;
-};
-
-WebInspector.EventHandler.prototype = {
-    constructor: WebInspector.EventHandler,
-
-    // Public
-
-    trackEvents: function(target)
-    {
-        if (this._tracking && target === this._target)
-            return;
-
-        if (this._tracking && this._target)
-            this.stopTrackingEvents();
-
-        Object.getOwnPropertyNames(this._eventHandlers).forEach(function(eventName) {
-            target.addEventListener(eventName, this);
-        }, this);
-
-        this._target = target;
-        this._tracking = true;
-    },
-    
-    stopTrackingEvents: function()
-    {
-        if (!this._tracking)
-            return;
-
-        Object.getOwnPropertyNames(this._eventHandlers).forEach(function(eventType) {
-            this._target.removeEventListener(eventType, this);
-        }, this);
-
-        this._tracking = false;
-    },
-
-    handleEvent: function(event)
-    {
-        if (event.currentTarget !== this._target)
-            return;
-
-        var handler = this._eventHandlers[event.type];
-        if (handler)
-            handler.call(this._delegate, event);
-    }
-}
-
-WebInspector.EventHandler.prototype.__proto__ = WebInspector.Object.prototype;

Source/WebInspectorUI/UserInterface/Main.html
@@ -163,7 +163,6 @@
     <script src="Base/Object.js"></script>
 
     <script src="Base/DOMUtilities.js"></script>
-    <script src="Base/EventHandler.js"></script>
     <script src="Base/EventListenerSet.js"></script>
     <script src="Base/ImageUtilities.js"></script>
     <script src="Base/LoadLocalizedStrings.js"></script>

commit 54d345b2fff0ed8e8fec4b58c2af627960c90022

Web Inspector: Debugger popovers should not dismiss when mousing over them https://bugs.webkit.org/show_bug.cgi?id=134687
Patch by Joseph Pecoraro <pecoraro@apple.com> on 2014-07-08
Reviewed by David Kilzer.

* UserInterface/Views/SourceCodeTextEditor.js:
(WebInspector.SourceCodeTextEditor.prototype._trackPopoverEvents):
Actually install the listeners in the set. Regression from code refactoring in r165052.

Canonical link: https://commits.webkit.org/152689@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@170899 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebInspectorUI/UserInterface/Views/SourceCodeTextEditor.js
@@ -1270,6 +1270,7 @@ WebInspector.SourceCodeTextEditor.prototype = {
             this._popoverEventListeners = new WebInspector.EventListenerSet(this, "Popover listeners");
         this._popoverEventListeners.register(this._popover.element, "mouseover", this._popoverMouseover);
         this._popoverEventListeners.register(this._popover.element, "mouseout", this._popoverMouseout);
+        this._popoverEventListeners.install();
     },
 
     _popoverMouseover: function(event)