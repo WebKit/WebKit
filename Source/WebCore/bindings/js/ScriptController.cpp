60dd7a0831143a1d85cbc47b093af3e55ce46f2a
Joseph Pecoraro <pecoraro@apple.com>
2014-07-24 02:39:53 +0000

    ScriptController::updateDocument ASSERT mutating map while iterating map https://bugs.webkit.org/show_bug.cgi?id=135211
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2014-07-23
    Reviewed by Oliver Hunt.
    
    Avoid iterating over m_windowShells in more places. This prevents
    the possibility of a collection during JSC allocation which might
    cause a mutation to m_windowShells (HTMLMediaElement destruction).
    
    Have ScriptController defriend ScriptCachedFrameData by providing
    a getter for the list of window shells.
    
    * bindings/js/ScriptCachedFrameData.cpp:
    (WebCore::ScriptCachedFrameData::ScriptCachedFrameData):
    (WebCore::ScriptCachedFrameData::restore):
    * bindings/js/ScriptController.cpp:
    (WebCore::ScriptController::windowShells):
    (WebCore::ScriptController::clearWindowShell):
    (WebCore::ScriptController::attachDebugger):
    (WebCore::ScriptController::updateDocument):
    * bindings/js/ScriptController.h:
    
    Canonical link: https://commits.webkit.org/153140@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@171505 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/js/ScriptController.cpp
+++ Source/WebCore/bindings/js/ScriptController.cpp
@@ -170,6 +170,13 @@ PassRefPtr<DOMWrapperWorld> ScriptController::createWorld()
     return DOMWrapperWorld::create(JSDOMWindow::commonVM());
 }
 
+Vector<JSC::Strong<JSDOMWindowShell>> ScriptController::windowShells()
+{
+    Vector<JSC::Strong<JSDOMWindowShell>> windowShells;
+    copyValuesToVector(m_windowShells, windowShells);
+    return windowShells;
+}
+
 void ScriptController::getAllWorlds(Vector<Ref<DOMWrapperWorld>>& worlds)
 {
     static_cast<WebCoreJSClientData*>(JSDOMWindow::commonVM().clientData)->getAllWorlds(worlds);
@@ -182,9 +189,7 @@ void ScriptController::clearWindowShell(DOMWindow* newDOMWindow, bool goingIntoP
 
     JSLockHolder lock(JSDOMWindowBase::commonVM());
 
-    Vector<JSC::Strong<JSDOMWindowShell>> windowShells;
-    copyValuesToVector(m_windowShells, windowShells);
-
+    Vector<JSC::Strong<JSDOMWindowShell>> windowShells = this->windowShells();
     for (size_t i = 0; i < windowShells.size(); ++i) {
         JSDOMWindowShell* windowShell = windowShells[i].get();
 
@@ -288,8 +293,9 @@ bool ScriptController::canAccessFromCurrentOrigin(Frame *frame)
 
 void ScriptController::attachDebugger(JSC::Debugger* debugger)
 {
-    for (ShellMap::iterator iter = m_windowShells.begin(); iter != m_windowShells.end(); ++iter)
-        attachDebugger(iter->value.get(), debugger);
+    Vector<JSC::Strong<JSDOMWindowShell>> windowShells = this->windowShells();
+    for (size_t i = 0; i < windowShells.size(); ++i)
+        attachDebugger(windowShells[i].get(), debugger);
 }
 
 void ScriptController::attachDebugger(JSDOMWindowShell* shell, JSC::Debugger* debugger)
@@ -306,9 +312,11 @@ void ScriptController::attachDebugger(JSDOMWindowShell* shell, JSC::Debugger* de
 
 void ScriptController::updateDocument()
 {
-    for (ShellMap::iterator iter = m_windowShells.begin(); iter != m_windowShells.end(); ++iter) {
-        JSLockHolder lock(iter->key->vm());
-        iter->value->window()->updateDocument();
+    Vector<JSC::Strong<JSDOMWindowShell>> windowShells = this->windowShells();
+    for (size_t i = 0; i < windowShells.size(); ++i) {
+        JSDOMWindowShell* windowShell = windowShells[i].get();
+        JSLockHolder lock(windowShell->world().vm());
+        windowShell->window()->updateDocument();
     }
 }
 
