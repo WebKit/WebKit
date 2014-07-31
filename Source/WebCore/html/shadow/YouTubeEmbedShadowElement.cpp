2e8ddec9b6da70e4fed52a2719521d22b3e7e8c1
Benjamin Poulain <bpoulain@apple.com>
2014-07-31 03:15:10 +0000

    Clean up YouTubeEmbedShadowElement https://bugs.webkit.org/show_bug.cgi?id=135437
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2014-07-30
    Reviewed by Andreas Kling.
    
    * html/shadow/YouTubeEmbedShadowElement.cpp:
    (WebCore::YouTubeEmbedShadowElement::YouTubeEmbedShadowElement):
    (WebCore::YouTubeEmbedShadowElement::pluginElement): Deleted.
    This is unused in WebCore.
    
    (WebCore::YouTubeEmbedShadowElement::shadowPseudoId): Deleted.
    Use the generic pseudo path instead of a custom shadowPseudoId.
    * html/shadow/YouTubeEmbedShadowElement.h:
    
    
    Canonical link: https://commits.webkit.org/153314@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@171831 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/shadow/YouTubeEmbedShadowElement.cpp
+++ Source/WebCore/html/shadow/YouTubeEmbedShadowElement.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Apple Inc. All rights reserved.
+ * Copyright (C) 2012, 2014 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,8 +30,6 @@
 
 namespace WebCore {
 
-using namespace HTMLNames;
-
 PassRefPtr<YouTubeEmbedShadowElement> YouTubeEmbedShadowElement::create(Document& doc)
 {
     return adoptRef(new YouTubeEmbedShadowElement(doc));
@@ -40,25 +38,12 @@ PassRefPtr<YouTubeEmbedShadowElement> YouTubeEmbedShadowElement::create(Document
 YouTubeEmbedShadowElement::YouTubeEmbedShadowElement(Document& document)
     : HTMLDivElement(HTMLNames::divTag, document)
 {
-    setPseudo(shadowPseudoId());
+    setPseudo(AtomicString("-webkit-plugin-replacement", AtomicString::ConstructFromLiteral));
 
     setInlineStyleProperty(CSSPropertyDisplay, CSSValueInlineBlock);
     setInlineStyleProperty(CSSPropertyPosition, CSSValueRelative);
     setInlineStyleProperty(CSSPropertyWidth, CSSValue100);
     setInlineStyleProperty(CSSPropertyHeight, CSSValue100);
-}
-
-HTMLPlugInImageElement* YouTubeEmbedShadowElement::pluginElement() const
-{
-    Node* node = const_cast<YouTubeEmbedShadowElement*>(this)->deprecatedShadowAncestorNode();
-    ASSERT(!node || embedTag == toElement(node)->tagQName() || objectTag == toElement(node)->tagQName());
-    return static_cast<HTMLPlugInImageElement*>(node);
-}
-
-const AtomicString& YouTubeEmbedShadowElement::shadowPseudoId() const
-{
-    DEPRECATED_DEFINE_STATIC_LOCAL(AtomicString, pseudoId, ("-webkit-plugin-replacement"));
-    return pseudoId;
 }
 
 }
--- Source/WebCore/html/shadow/YouTubeEmbedShadowElement.h
+++ Source/WebCore/html/shadow/YouTubeEmbedShadowElement.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Apple Inc. All rights reserved.
+ * Copyright (C) 2012, 2014 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,14 +31,9 @@
 
 namespace WebCore {
 
-class HTMLPlugInImageElement;
-
 class YouTubeEmbedShadowElement final : public HTMLDivElement {
 public:
     static PassRefPtr<YouTubeEmbedShadowElement> create(Document&);
-
-    virtual const AtomicString& shadowPseudoId() const override;
-    HTMLPlugInImageElement* pluginElement() const;
 
 private:
     YouTubeEmbedShadowElement(Document&);
