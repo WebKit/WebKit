7b03ed85b89dbf3263bcdad68947c26bdaaddf89
Dan Bernstein <mitz@apple.com>
2014-07-31 16:07:14 +0000

    Tried to fix building with the OS X 10.9 SDK while targeting OS X 10.8.
    * platform/network/cocoa/CredentialCocoa.mm:
    (WebCore::toCredentialPersistence):
    
    
    
    Canonical link: https://commits.webkit.org/153332@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@171854 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/network/cocoa/CredentialCocoa.mm
+++ Source/WebCore/platform/network/cocoa/CredentialCocoa.mm
@@ -26,6 +26,13 @@
 #import "config.h"
 #import "CredentialCocoa.h"
 
+#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MAX_ALLOWED == 1080
+// This value is available in OS X 10.8 but only appeared in the OS X 10.9 SDK.
+enum {
+    NSURLCredentialPersistenceSynchronizable = 3
+};
+#endif
+
 #if USE(CFNETWORK)
 @interface NSURLCredential (WebDetails)
 - (id)_initWithCFURLCredential:(CFURLCredentialRef)credential;
@@ -58,9 +65,7 @@ static CredentialPersistence toCredentialPersistence(NSURLCredentialPersistence 
     case NSURLCredentialPersistenceForSession:
         return CredentialPersistenceForSession;
     case NSURLCredentialPersistencePermanent:
-#if PLATFORM(IOS) || __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
     case NSURLCredentialPersistenceSynchronizable:
-#endif
         return CredentialPersistencePermanent;
     }
 
