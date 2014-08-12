59b36b852162595ec3a51ca39ee731dbaeef3803
Renata Hodovan <rhodovan.u-szeged@partner.samsung.com>
2014-08-12 22:30:32 +0000

    Make sure that begin time cannot be greater than SMILTime::indefiniteValue unintentionally. https://bugs.webkit.org/show_bug.cgi?id=135838
    Patch by Renata Hodovan <rhodovan.u-szeged@partner.samsung.com> on 2014-08-12
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    When WebCore::SVGSMILElement::resolveInterval creates a SMILTime begin
    node for a SMILInterval, then it only checks if the value of begin is
    indefinite or unresolved but misses the case if it is between these two
    reference values.
    
    This is a backport of my fix in Blink: https://codereview.chromium.org/406263002/.
    
    Test: svg/animations/animateMotion-crash-with-large-begin-time.html
    
    * svg/animation/SVGSMILElement.cpp:
    (WebCore::SVGSMILElement::parseOffsetValue):
    (WebCore::SVGSMILElement::parseClockValue):
    
    LayoutTests:
    
    * svg/animations/animateMotion-crash-with-large-begin-time-expected.txt: Added.
    * svg/animations/animateMotion-crash-with-large-begin-time.html: Added.
    
    Canonical link: https://commits.webkit.org/153729@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@172496 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/svg/animation/SVGSMILElement.cpp
+++ Source/WebCore/svg/animation/SVGSMILElement.cpp
@@ -305,7 +305,7 @@ SMILTime SVGSMILElement::parseOffsetValue(const String& data)
         result = parse.left(parse.length() - 1).toDouble(&ok);
     else
         result = parse.toDouble(&ok);
-    if (!ok)
+    if (!ok || !SMILTime(result).isFinite())
         return SMILTime::unresolved();
     return result;
 }
@@ -341,7 +341,7 @@ SMILTime SVGSMILElement::parseClockValue(const String& data)
     } else
         return parseOffsetValue(parse);
 
-    if (!ok)
+    if (!ok || !SMILTime(result).isFinite())
         return SMILTime::unresolved();
     return result;
 }
