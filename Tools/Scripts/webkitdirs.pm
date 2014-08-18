5c4132f33f1d0f3de4428d1587564f1a8efddbb7
Dan Bernstein <mitz@apple.com>
2014-08-18 21:44:24 +0000

    Added an update-webkit option to override the ../Internal check. Part of https://bugs.webkit.org/show_bug.cgi?id=135815
    Reviewed by Tim Horton.
    
    * Scripts/update-webkit:
    Added an explicit --internal option to control updating ../Internal, defaulting to whether
    ../Internal exists.
    (runSvnUpdate): Moved to webkitdirs.pm.
    (runGitUpdate): Ditto.
    * Scripts/webkitdirs.pm:
    (runSvnUpdateAndResolveChangeLogs): Moved runSvnUpdate from update-webkit and renamed to this.
    (runGitUpdate): Moved from update-webkit.
    
    
    
    Canonical link: https://commits.webkit.org/153902@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@172728 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -2520,4 +2520,41 @@ sub formatBuildTime($)
     return sprintf("%02dm:%02ds", $buildMins, $buildSecs);
 }
 
+sub runSvnUpdateAndResolveChangeLogs(@)
+{
+    my @svnOptions = @_;
+    open UPDATE, "-|", "svn", "update", @svnOptions or die;
+    my @conflictedChangeLogs;
+    while (my $line = <UPDATE>) {
+        print $line;
+        $line =~ m/^C\s+(.+?)[\r\n]*$/;
+        if ($1) {
+          my $filename = normalizePath($1);
+          push @conflictedChangeLogs, $filename if basename($filename) eq "ChangeLog";
+        }
+    }
+    close UPDATE or die;
+
+    if (@conflictedChangeLogs) {
+        print "Attempting to merge conflicted ChangeLogs.\n";
+        my $resolveChangeLogsPath = File::Spec->catfile(dirname($0), "resolve-ChangeLogs");
+        (system($resolveChangeLogsPath, "--no-warnings", @conflictedChangeLogs) == 0)
+            or die "Could not open resolve-ChangeLogs script: $!.\n";
+    }
+}
+
+sub runGitUpdate()
+{
+    # Doing a git fetch first allows setups with svn-remote.svn.fetch = trunk:refs/remotes/origin/master
+    # to perform the rebase much much faster.
+    system("git", "fetch");
+    if (isGitSVN()) {
+        system("git", "svn", "rebase") == 0 or die;
+    } else {
+        # This will die if branch.$BRANCHNAME.merge isn't set, which is
+        # almost certainly what we want.
+        system("git", "pull") == 0 or die;
+    }
+}
+
 1;
