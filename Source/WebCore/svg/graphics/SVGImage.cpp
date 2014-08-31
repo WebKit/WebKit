200b1de1ffe13f3ff70e9cc240804c12e3b9960c
Tim Horton <timothy_horton@apple.com>
2014-08-31 07:41:50 +0000

    SVGImage::drawPatternForContainer creates a buffer without respecting the destination's acceleration setting https://bugs.webkit.org/show_bug.cgi?id=136408 <rdar://problem/12013317>
    Reviewed by Dan Bernstein.
    
    No new tests required, just a performance improvement.
    
    * svg/graphics/SVGImage.cpp:
    (WebCore::SVGImage::drawPatternForContainer):
    Use ImageBuffer::createCompatibleBuffer, which passes the destination's
    acceleration setting through to the ImageBuffer constructor.
    
    I didn't use GraphicsContext::createCompatibleBuffer because adjusting
    how this function applies the destination's CTM is outside the scope of this patch.
    
    
    Canonical link: https://commits.webkit.org/154214@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@173143 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/svg/graphics/SVGImage.cpp
+++ Source/WebCore/svg/graphics/SVGImage.cpp
@@ -194,7 +194,7 @@ void SVGImage::drawPatternForContainer(GraphicsContext* context, const FloatSize
     FloatRect imageBufferSize = zoomedContainerRect;
     imageBufferSize.scale(imageBufferScale.width(), imageBufferScale.height());
 
-    std::unique_ptr<ImageBuffer> buffer = ImageBuffer::create(expandedIntSize(imageBufferSize.size()), 1);
+    std::unique_ptr<ImageBuffer> buffer = ImageBuffer::createCompatibleBuffer(expandedIntSize(imageBufferSize.size()), 1, ColorSpaceDeviceRGB, context, true);
     if (!buffer) // Failed to allocate buffer.
         return;
     drawForContainer(buffer->context(), containerSize, zoom, imageBufferSize, zoomedContainerRect, ColorSpaceDeviceRGB, CompositeSourceOver, BlendModeNormal);
