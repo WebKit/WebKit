a3dab7a153d38f61e16e4b3a5e843ef749959f84
Daniel Bates <dbates@webkit.org>
2014-09-05 00:09:39 +0000

    [iOS] Fix the iOS build after <http://trac.webkit.org/changeset/173258> (https://bugs.webkit.org/show_bug.cgi?id=136494)
    Use ENABLE_TOUCH_EVENT instead of ENABLE(TOUCH_EVENT) as the latter isn't available
    to some clients of this private header.
    
    * platform/ios/wak/WAKResponder.h:
    
    Canonical link: https://commits.webkit.org/154354@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@173292 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/ios/wak/WAKResponder.h
+++ Source/WebCore/platform/ios/wak/WAKResponder.h
@@ -46,7 +46,7 @@
 - (void)mouseExited:(WebEvent *)theEvent;
 - (void)keyDown:(WebEvent *)event;
 - (void)keyUp:(WebEvent *)event;
-#if ENABLE(TOUCH_EVENTS)
+#if defined(ENABLE_TOUCH_EVENTS) && ENABLE_TOUCH_EVENTS
 - (void)touch:(WebEvent *)event;
 #endif
 
