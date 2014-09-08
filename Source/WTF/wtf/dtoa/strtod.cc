404b25fe059d2c04abcaa5cc7bf4a4fb9a0fcca6
Jessie Berlin <jberlin@webkit.org>
2014-09-08 14:45:37 +0000

    More build fixes after r173374.
    * wtf/dtoa/strtod.cc:
    
    Canonical link: https://commits.webkit.org/154436@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@173376 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/dtoa/strtod.cc
+++ Source/WTF/wtf/dtoa/strtod.cc
@@ -38,11 +38,13 @@
 namespace WTF {
 
 namespace double_conversion {
-    
+
+#if defined(DOUBLE_CONVERSION_CORRECT_DOUBLE_OPERATIONS)
     // 2^53 = 9007199254740992.
     // Any integer with at most 15 decimal digits will hence fit into a double
     // (which has a 53bit significand) without loss of precision.
     static const int kMaxExactDoubleIntegerDecimalDigits = 15;
+#endif
     // 2^64 = 18446744073709551616 > 10^19
     static const int kMaxUint64DecimalDigits = 19;
     
@@ -58,7 +60,7 @@ namespace double_conversion {
     // 2^64 = 18446744073709551616
     static const uint64_t kMaxUint64 = UINT64_2PART_C(0xFFFFFFFF, FFFFFFFF);
     
-    
+#if defined(DOUBLE_CONVERSION_CORRECT_DOUBLE_OPERATIONS)
     static const double exact_powers_of_ten[] = {
         1.0,  // 10^0
         10.0,
@@ -86,6 +88,7 @@ namespace double_conversion {
         10000000000000000000000.0
     };
     static const int kExactPowersOfTenSize = ARRAY_SIZE(exact_powers_of_ten);
+#endif
     
     // Maximum number of significant digits in the decimal representation.
     // In fact the value is 772 (see conversions.cc), but to give us some margin
