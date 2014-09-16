708e33489c960c1702312fe2f3b8d3eef425839d
Tibor Meszaros <tmeszaros.u-szeged@partner.samsung.com>
2014-09-16 08:57:12 +0000

    Fix FindICU.cmake https://bugs.webkit.org/show_bug.cgi?id=136820
    Patch by Tibor Meszaros <tmeszaros.u-szeged@partner.samsung.com> on 2014-09-16
    Reviewed by Csaba Osztrogonác.
    
    * Source/cmake/FindICU.cmake:
    
    Canonical link: https://commits.webkit.org/154679@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@173655 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/FindICU.cmake
+++ Source/cmake/FindICU.cmake
@@ -9,10 +9,15 @@
 #  ICU_I18N_LIBRARIES - Libraries to link against for ICU internationaliation
 #                       (note: in addition to ICU_LIBRARIES)
 
+find_package(PkgConfig)
+pkg_check_modules(PC_ICU icu-uc)
+
 # Look for the header file.
 find_path(
     ICU_INCLUDE_DIR
     NAMES unicode/utypes.h
+    HINTS ${PC_ICU_INCLUDE_DIRS}
+          ${PC_ICU_INCLUDEDIR}
     DOC "Include directory for the ICU library")
 mark_as_advanced(ICU_INCLUDE_DIR)
 
@@ -20,6 +25,8 @@ mark_as_advanced(ICU_INCLUDE_DIR)
 find_library(
     ICU_LIBRARY
     NAMES icuuc cygicuuc cygicuuc32
+    HINTS ${PC_ICU_LIBRARY_DIRS}
+          ${PC_ICU_LIBDIR}
     DOC "Libraries to link against for the common parts of ICU")
 mark_as_advanced(ICU_LIBRARY)
 
