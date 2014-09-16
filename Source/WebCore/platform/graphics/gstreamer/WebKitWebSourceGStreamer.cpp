ef309e4c2f2f04ebe7c61e821b9f6fa9654348ac
Youenn Fablet <youennf@gmail.com>
2014-09-16 08:46:10 +0000

    [GStreamer] http/tests/media/video-auth.html is failing https://bugs.webkit.org/show_bug.cgi?id=126619
    Patch by Youenn Fablet <youenn.fablet@crf.canon.fr> on 2014-09-16
    Reviewed by Philippe Normand.
    
    Source/WebCore:
    
    Test: http/tests/media/video-auth-cors.html
    
    Use stored credentials when retrieving videos following image loading behavior
    (i.e. stored credentials are always used except when in cors anonymous mode),
    except that requesting credentials from user remains disabled for cross-origin requests.
    
    Patch is covered by http/tests/media/video-auth.html.
    
    * platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp:
    (CachedResourceStreamingClient::CachedResourceStreamingClient): Enable using stored credentials except when using cors anonymous mode.
    
    LayoutTests:
    
    * platform/efl/TestExpectations: Removed failure expectation for http/tests/media/video-auth.html.
    * platform/efl/http/tests/media/video-auth-expected.txt: Added.
    * platform/gtk/TestExpectations: Removed failure expectation for http/tests/media/video-auth.html.
    * platform/gtk/http/tests/media/video-auth-expected.txt: Added.
    
    Canonical link: https://commits.webkit.org/154678@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@173654 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
@@ -996,7 +996,7 @@ CachedResourceStreamingClient::CachedResourceStreamingClient(WebKitWebSrc* src, 
 {
     DataBufferingPolicy bufferingPolicy = request.url().protocolIs("blob") ? BufferData : DoNotBufferData;
     RequestOriginPolicy corsPolicy = corsMode != MediaPlayerClient::Unspecified ? PotentiallyCrossOriginEnabled : UseDefaultOriginRestrictionsForType;
-    StoredCredentials allowCredentials = corsMode == MediaPlayerClient::UseCredentials ? AllowStoredCredentials : DoNotAllowStoredCredentials;
+    StoredCredentials allowCredentials = corsMode == MediaPlayerClient::Anonymous ? DoNotAllowStoredCredentials : AllowStoredCredentials;
     ResourceLoaderOptions options(SendCallbacks, DoNotSniffContent, bufferingPolicy, allowCredentials, DoNotAskClientForCrossOriginCredentials, DoSecurityCheck, corsPolicy, DoNotIncludeCertificateInfo);
 
     CachedResourceRequest cacheRequest(request, options);
