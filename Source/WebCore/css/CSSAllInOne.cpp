d4ffd089df15d6c93b18b97ca53b60a73395a396
Roger Fong <roger_fong@apple.com>
2014-09-24 00:52:37 +0000

    [Windows] Enable CSS Supports Rule on Windows.
    * win/tools/vsprops/FeatureDefines.props:
    * css/CSSAllInOne.cpp:
    
    Canonical link: https://commits.webkit.org/154877@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@173904 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/CSSAllInOne.cpp
+++ Source/WebCore/css/CSSAllInOne.cpp
@@ -66,6 +66,9 @@
 #include "CSSSegmentedFontFace.cpp"
 #include "CSSSelector.cpp"
 #include "CSSSelectorList.cpp"
+#if ENABLE(CSS3_CONDITIONAL_RULES)
+#include "CSSSupportsRule.cpp"
+#endif
 #include "CSSStyleRule.cpp"
 #include "CSSStyleSheet.cpp"
 #include "CSSTimingFunctionValue.cpp"
