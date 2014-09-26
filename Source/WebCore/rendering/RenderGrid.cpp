e9721ecbfa7ef588cc9fc141723b4ef7cf6d6057
Sergio Villar Senin <svillar@igalia.com>
2014-09-26 13:41:57 +0000

    [CSS Grid Layout] Fix the handling of infinity in track growth limits https://bugs.webkit.org/show_bug.cgi?id=137019
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    The growth limit of content sized tracks is initialized to
    infinity which is internally represented as -1. We were not
    specialcasing this situation, and thus, -1 was used in the
    computations as any other value. This change makes the code aware
    of the existence of infinites (like when sorting tracks by growth
    potential or when initializing the track growth limits).
    
    There was another bug related to infinities. The code that was
    replacing a infinite growth limit by a finite one was not using
    the proper indexes so the tracks that were being updated were the
    wrong ones.
    
    Test: fast/css-grid-layout/grid-content-sized-columns-resolution.html
    
    * rendering/RenderGrid.cpp:
    (WebCore::RenderGrid::computeUsedBreadthOfGridTracks):
    (WebCore::RenderGrid::resolveContentBasedTrackSizingFunctions):
    (WebCore::sortByGridTrackGrowthPotential):
    (WebCore::RenderGrid::distributeSpaceToTracks):
    
    LayoutTests:
    
    * fast/css-grid-layout/grid-content-sized-columns-resolution-expected.txt: Added.
    * fast/css-grid-layout/grid-content-sized-columns-resolution.html: Added.
    * fast/css-grid-layout/grid-item-order-in-content-sized-columns-resolution-expected.txt:
    
    Canonical link: https://commits.webkit.org/154956@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@174006 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderGrid.cpp
+++ Source/WebCore/rendering/RenderGrid.cpp
@@ -307,7 +307,8 @@ void RenderGrid::computeUsedBreadthOfGridTracks(GridTrackSizingDirection directi
         track.m_usedBreadth = computeUsedBreadthOfMinLength(direction, minTrackBreadth);
         track.m_maxBreadth = computeUsedBreadthOfMaxLength(direction, maxTrackBreadth, track.m_usedBreadth);
 
-        track.m_maxBreadth = std::max(track.m_maxBreadth, track.m_usedBreadth);
+        if (track.m_maxBreadth != infinity)
+            track.m_maxBreadth = std::max(track.m_maxBreadth, track.m_usedBreadth);
 
         if (trackSize.isContentSized())
             sizingData.contentSizedTracksIndex.append(i);
@@ -559,8 +560,8 @@ void RenderGrid::resolveContentBasedTrackSizingFunctions(GridTrackSizingDirectio
 {
     // FIXME: Split the grid tracks into groups that doesn't overlap a <flex> grid track.
 
-    for (size_t i = 0; i < sizingData.contentSizedTracksIndex.size(); ++i) {
-        GridIterator iterator(m_grid, direction, sizingData.contentSizedTracksIndex[i]);
+    for (auto trackIndex : sizingData.contentSizedTracksIndex) {
+        GridIterator iterator(m_grid, direction, trackIndex);
         HashSet<RenderBox*> itemsSet;
         Vector<GridItemWithSpan> itemsSortedByIncreasingSpan;
 
@@ -577,7 +578,7 @@ void RenderGrid::resolveContentBasedTrackSizingFunctions(GridTrackSizingDirectio
             resolveContentBasedTrackSizingFunctionsForItems(direction, sizingData, itemWithSpan, &GridTrackSize::hasMaxContentMaxTrackBreadth, &RenderGrid::maxContentForChild, &GridTrack::maxBreadthIfNotInfinite, &GridTrack::growMaxBreadth);
         }
 
-        GridTrack& track = (direction == ForColumns) ? sizingData.columnTracks[i] : sizingData.rowTracks[i];
+        GridTrack& track = (direction == ForColumns) ? sizingData.columnTracks[trackIndex] : sizingData.rowTracks[trackIndex];
         if (track.m_maxBreadth == infinity)
             track.m_maxBreadth = track.m_usedBreadth;
     }
@@ -617,6 +618,14 @@ void RenderGrid::resolveContentBasedTrackSizingFunctionsForItems(GridTrackSizing
 
 static bool sortByGridTrackGrowthPotential(const GridTrack* track1, const GridTrack* track2)
 {
+    // This check ensures that we respect the irreflexivity property of the strict weak ordering required by std::sort
+    // (forall x: NOT x < x).
+    if (track1->m_maxBreadth == infinity && track2->m_maxBreadth == infinity)
+        return false;
+
+    if (track1->m_maxBreadth == infinity || track2->m_maxBreadth == infinity)
+        return track2->m_maxBreadth == infinity;
+
     return (track1->m_maxBreadth - track1->m_usedBreadth) < (track2->m_maxBreadth - track2->m_usedBreadth);
 }
 
@@ -631,11 +640,14 @@ void RenderGrid::distributeSpaceToTracks(Vector<GridTrack*>& tracks, Vector<Grid
     for (size_t i = 0; i < tracksSize; ++i) {
         GridTrack& track = *tracks[i];
         LayoutUnit trackBreadth = (tracks[i]->*trackGetter)();
-        LayoutUnit trackGrowthPotential = track.m_maxBreadth - trackBreadth;
+        bool infiniteGrowthPotential = track.m_maxBreadth == infinity;
+        LayoutUnit trackGrowthPotential = infiniteGrowthPotential ? track.m_maxBreadth : track.m_maxBreadth - trackBreadth;
         sizingData.distributeTrackVector[i] = trackBreadth;
-        if (trackGrowthPotential > 0) {
+        // Let's avoid computing availableLogicalSpaceShare as much as possible as it's a hot spot in performance tests.
+        if (trackGrowthPotential > 0 || infiniteGrowthPotential) {
             LayoutUnit availableLogicalSpaceShare = availableLogicalSpace / (tracksSize - i);
-            LayoutUnit growthShare = std::min(availableLogicalSpaceShare, trackGrowthPotential);
+            LayoutUnit growthShare = infiniteGrowthPotential ? availableLogicalSpaceShare : std::min(availableLogicalSpaceShare, trackGrowthPotential);
+            ASSERT(growthShare > 0);
             // We should never shrink any grid track or else we can't guarantee we abide by our min-sizing function.
             sizingData.distributeTrackVector[i] += growthShare;
             availableLogicalSpace -= growthShare;
