ac346bc7f5efbfb9ced53c4510f4ae253f8732a4
Carlos Garcia Campos <cgarcia@igalia.com>
2014-10-04 07:35:47 +0000

    [GTK] generate-gtkdoc crashes when generating HTML due to encoding issues https://bugs.webkit.org/show_bug.cgi?id=135502
    Reviewed by Martin Robinson.
    
    When writing to stdout/stderr fails due to a UnicodeDecodeError
    exception, try again without encoding the output.
    
    * gtk/gtkdoc.py:
    (GTKDoc._run_command):
    
    Canonical link: https://commits.webkit.org/155223@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@174309 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/gtk/gtkdoc.py
+++ Tools/gtk/gtkdoc.py
@@ -195,9 +195,15 @@ class GTKDoc(object):
 
         if print_output:
             if stdout:
-                sys.stdout.write(stdout.encode("utf-8"))
+                try:
+                    sys.stdout.write(stdout.encode("utf-8"))
+                except UnicodeDecodeError:
+                    sys.stdout.write(stdout)
             if stderr:
-                sys.stderr.write(stderr.encode("utf-8"))
+                try:
+                    sys.stderr.write(stderr.encode("utf-8"))
+                except UnicodeDecodeError:
+                    sys.stderr.write(stderr)
 
         if process.returncode != 0:
             raise Exception('%s produced a non-zero return code %i'
