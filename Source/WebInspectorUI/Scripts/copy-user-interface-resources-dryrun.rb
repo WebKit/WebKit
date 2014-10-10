7b471858b3adc42546446153c042d3513a08afa4
Joseph Pecoraro <pecoraro@apple.com>
2014-10-10 22:38:37 +0000

    Web Inspector: CSS minification should not strip whitespace around calc +/- binary operators https://bugs.webkit.org/show_bug.cgi?id=137624
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2014-10-10
    Reviewed by Timothy Hatcher.
    
    Be careful when stripping whitespace around "+". We want to strip for
    selectors "a + b" but not for calc expressions "calc(1px + 2px)".
    
    This is not a perfect solution, but it does a good job. It may still break
    with more complex calc expressions, like "calc(1px + calc())" but this
    handles the basics.
    
    * Scripts/copy-user-interface-resources-dryrun.rb: Added.
    * Scripts/cssmin.py:
    (cssminify):
    
    Canonical link: https://commits.webkit.org/155481@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@174615 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebInspectorUI/Scripts/copy-user-interface-resources-dryrun.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/ruby
+
+require 'fileutils'
+require 'tmpdir'
+
+if ARGV.size != 1
+  puts "usage: #{File.basename $0} <output-directory>"
+  exit 1
+end
+
+WEB_INSPECTOR_PATH = File.expand_path File.join(File.dirname(__FILE__), "..")
+COPY_USER_INTERFACE_RESOURCES_PATH = File.join WEB_INSPECTOR_PATH, "Scripts", "copy-user-interface-resources.pl"
+
+$output_directory = File.expand_path ARGV[0]
+
+Dir.mktmpdir do |tmpdir|
+
+  # Create the output directory if needed.
+  FileUtils.mkdir_p $output_directory
+  
+  # Create empty derived sources expected to exist.
+  FileUtils.touch(File.join(tmpdir, 'InspectorJSBackendCommands.js'))
+  FileUtils.touch(File.join(tmpdir, 'InspectorWebBackendCommands.js'))
+  
+  # Setup the environment and run.
+  ENV["DERIVED_SOURCES_DIR"] = tmpdir
+  ENV["JAVASCRIPTCORE_PRIVATE_HEADERS_DIR"] = tmpdir
+  ENV["WEBCORE_PRIVATE_HEADERS_DIR"] = tmpdir
+  ENV["SRCROOT"] = WEB_INSPECTOR_PATH
+  ENV["TARGET_BUILD_DIR"] = $output_directory
+  ENV["UNLOCALIZED_RESOURCES_FOLDER_PATH"] = ""
+  ENV["COMBINE_INSPECTOR_RESOURCES"] = "YES"
+  exec COPY_USER_INTERFACE_RESOURCES_PATH
+
+end
