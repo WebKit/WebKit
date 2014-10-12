3905316572ac2a78b532157b51db149f50018d03
Sergio Villar Senin <svillar@igalia.com>
2014-10-12 09:32:10 +0000

    [SOUP] [WK2] WebProcess and NetworkProcess initialization clears cache contents https://bugs.webkit.org/show_bug.cgi?id=137489
    Reviewed by Carlos Garcia Campos.
    
    Temporarily set the SoupCache's maximum size to a huge value
    before calling soup_cache_load() so that the cache is not shrinked
    to the default value.
    
    * NetworkProcess/soup/NetworkProcessSoup.cpp:
    (WebKit::NetworkProcess::platformInitializeNetworkProcess):
    * WebProcess/soup/WebProcessSoup.cpp:
    (WebKit::WebProcess::platformInitializeWebProcess):
    
    
    Canonical link: https://commits.webkit.org/155504@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@174639 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/NetworkProcess/soup/NetworkProcessSoup.cpp
+++ Source/WebKit2/NetworkProcess/soup/NetworkProcessSoup.cpp
@@ -85,7 +85,12 @@ void NetworkProcess::platformInitializeNetworkProcess(const NetworkProcessCreati
     ASSERT(!parameters.diskCacheDirectory.isEmpty());
     GRefPtr<SoupCache> soupCache = adoptGRef(soup_cache_new(parameters.diskCacheDirectory.utf8().data(), SOUP_CACHE_SINGLE_USER));
     SoupNetworkSession::defaultSession().setCache(soupCache.get());
+    // Set an initial huge max_size for the SoupCache so the call to soup_cache_load() won't evict any cached
+    // resource. The final size of the cache will be set by NetworkProcess::platformSetCacheModel().
+    unsigned initialMaxSize = soup_cache_get_max_size(soupCache.get());
+    soup_cache_set_max_size(soupCache.get(), G_MAXUINT);
     soup_cache_load(soupCache.get());
+    soup_cache_set_max_size(soupCache.get(), initialMaxSize);
 
     if (!parameters.cookiePersistentStoragePath.isEmpty()) {
         supplement<WebCookieManager>()->setCookiePersistentStorage(parameters.cookiePersistentStoragePath,
