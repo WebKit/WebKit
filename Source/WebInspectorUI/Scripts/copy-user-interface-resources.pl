7fb92b879eadff6f0e4beba464482790b1241910
Jono Wells <jonowells@apple.com>
2014-10-15 03:28:55 +0000

    Web Inspector: Add ESLint open source library to the inspector https://bugs.webkit.org/show_bug.cgi?id=137714
    Reviewed by Timothy Hatcher.
    
    This adds the eslint.js external library for static analysis and the necessary license.
    
    
    
    Canonical link: https://commits.webkit.org/155582@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@174721 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/Scripts/copy-user-interface-resources.pl
+++ Source/WebInspectorUI/Scripts/copy-user-interface-resources.pl
@@ -100,9 +100,11 @@ my $targetResourcePath = File::Spec->catdir($ENV{'TARGET_BUILD_DIR'}, $ENV{'UNLO
 my $protocolDir = File::Spec->catdir($targetResourcePath, 'Protocol');
 my $codeMirrorPath = File::Spec->catdir($uiRoot, 'External', 'CodeMirror');
 my $esprimaPath = File::Spec->catdir($uiRoot, 'External', 'Esprima');
+my $eslintPath = File::Spec->catdir($uiRoot, 'External', 'ESLint');
 
 my $codeMirrorLicense = readLicenseFile(File::Spec->catfile($codeMirrorPath, 'LICENSE'));
-my $esprimaMirrorLicense = readLicenseFile(File::Spec->catfile($esprimaPath, 'LICENSE'));
+my $esprimaLicense = readLicenseFile(File::Spec->catfile($esprimaPath, 'LICENSE'));
+my $eslintLicense = readLicenseFile(File::Spec->catfile($eslintPath, 'LICENSE'));
 
 make_path($protocolDir, $targetResourcePath);
 
@@ -155,7 +157,11 @@ if (defined $ENV{'COMBINE_INSPECTOR_RESOURCES'} && ($ENV{'COMBINE_INSPECTOR_RESO
 
     # Export the license into Esprima.js.
     my $targetEsprimaJS = File::Spec->catfile($targetResourcePath, 'Esprima.js');
-    seedFile($targetEsprimaJS, $esprimaMirrorLicense);
+    seedFile($targetEsprimaJS, $esprimaLicense);
+
+    # Export the license into ESLint.js.
+    my $targetESLintJS = File::Spec->catfile($targetResourcePath, 'ESLint.js');
+    seedFile($targetESLintJS, $eslintLicense);
 
     # Minify the Main.js and Main.css files, with Main.js appending to the license that was exported above.
     my $jsMinScript = File::Spec->catfile($scriptsRoot, 'jsmin.py');
@@ -172,6 +178,10 @@ if (defined $ENV{'COMBINE_INSPECTOR_RESOURCES'} && ($ENV{'COMBINE_INSPECTOR_RESO
     # Minify the Esprima.js file, appending to the license that was exported above.
     my $derivedSouressEsprimaJS = File::Spec->catfile($derivedSourcesDir, 'Esprima.js');
     system(qq("$python" "$jsMinScript" < "$derivedSouressEsprimaJS" >> "$targetEsprimaJS")) and die "Failed to minify $derivedSouressEsprimaJS: $!";
+
+    # Minify the ESLint.js file, appending to the license that was exported above.
+    my $derivedSouressESLintJS = File::Spec->catfile($derivedSourcesDir, 'ESLint.js');
+    system(qq("$python" "$jsMinScript" < "$derivedSouressESLintJS" >> "$targetESLintJS")) and die "Failed to minify $derivedSouressESLintJS: $!";
 
     # Copy over Main.html and the Images directory.
     copy($derivedSourcesMainHTML, File::Spec->catfile($targetResourcePath, 'Main.html'));
