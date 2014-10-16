d35e86cba38e7bb0fc2af8dc7ca0d31a2fb2d43d
David Kilzer <ddkilzer@webkit.org>
2014-10-16 16:01:16 +0000

    Fix review form after Bugzilla 4.2.5 update
    * code-review.js:
    (fetchHistory):
    - Trim author since new format adds a space before the name.
    - Update comment_marker to match the text now used by Bugzilla.
    
    * template/en/custom/attachment/reviewform.html.tmpl:
    - Add global/header.html.tmpl to make sure we get the standard
      HTML for including CSS, JavaScript, etc.
      - Remove redundant <link> tags.
      - Add CSS in local <style> to hide the header.
      - Change local <script> to be passed as an argument into
        global/header.html.tmpl when it is processed.
      - NOTE: local <style> tag should be passed as an argument into
        global/header.html.tmpl, but that breaks page layout, so it
        was skipped for now.
    - Fix fallout form moving flag_types into the attachment object
      when processing flag/list.html.tmpl.
    
    Canonical link: https://commits.webkit.org/155628@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@174769 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Websites/bugs.webkit.org/code-review.js
+++ Websites/bugs.webkit.org/code-review.js
@@ -555,10 +555,10 @@ var CODE_REVIEW_UNITTEST;
       $.get('show_bug.cgi?id=' + bug_id, function(data) {
         var comments = [];
         $(data).find('.bz_comment').each(function() {
-          var author = $(this).find('.email').text();
+          var author = $(this).find('.email').text().trim();
           var text = $(this).find('.bz_comment_text').text();
 
-          var comment_marker = '(From update of attachment ' + attachment_id + ' .details.)';
+          var comment_marker = 'Comment on attachment ' + attachment_id + ' .details.';
           if (text.match(comment_marker))
             $.merge(comments, scanForComments(author, text));
 
