a2e5783f0dcb524fb21911e1e2de0277c6cd4277
Joseph Pecoraro <joepeck@webkit.org>
2014-10-20 23:22:40 +0000

    Unreviewed add back copyright line that was accidentally removed.
    * inspector/scripts/codegen/generator_templates.py:
    (GeneratorTemplates):
    
    Canonical link: https://commits.webkit.org/155762@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@174912 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/inspector/scripts/codegen/generator_templates.py
+++ Source/JavaScriptCore/inspector/scripts/codegen/generator_templates.py
@@ -31,6 +31,7 @@
 class GeneratorTemplates:
     CopyrightBlock = (
     """/*
+ * Copyright (C) 2013 Google Inc. All rights reserved.
  * Copyright (C) 2013, 2014 Apple Inc. All rights reserved.
  * Copyright (C) 2014 University of Washington. All rights reserved.
  *
