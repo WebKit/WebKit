4c682390fd33c3a9de0ca43d3dbf6c1362fbe9e1
Chris Dumez <cdumez@apple.com>
2014-10-20 19:55:13 +0000

    Unreviewed EFL / GTK build fix after r174898.
    Add missing header include.
    
    * accessibility/AccessibilityARIAGrid.cpp:
    
    Canonical link: https://commits.webkit.org/155754@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@174903 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/accessibility/AccessibilityARIAGrid.cpp
+++ Source/WebCore/accessibility/AccessibilityARIAGrid.cpp
@@ -30,6 +30,7 @@
 #include "AccessibilityARIAGrid.h"
 
 #include "AXObjectCache.h"
+#include "AccessibilityARIAGridRow.h"
 #include "AccessibilityTableCell.h"
 #include "AccessibilityTableColumn.h"
 #include "AccessibilityTableHeaderContainer.h"
