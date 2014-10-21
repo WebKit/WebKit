37952f6c9edb126ba7e84f52553c1654e326e2c7
Joanmarie Diggs <jdiggs@igalia.com>
2014-10-21 18:44:20 +0000

    AX: [ATK] CSS-generated text content not exposed to assistive technologies https://bugs.webkit.org/show_bug.cgi?id=137669
    Reviewed by Chris Fleizach.
    
    Source/WebCore:
    
    Do not use a text iterator to get the text for elements which include
    pseudo elements.
    
    Test: accessibility/css-content-attribute.html
    
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::textUnderElement):
    
    LayoutTests:
    
    Moved the Mac platform test and modified it to dump the accessible tree
    and string values rather than a platform-specific hierarchy so that it
    could be shared across platforms.
    
    * accessibility/css-content-attribute.html: Added.
    * platform/gtk/accessibility/css-content-attribute-expected.txt: Added.
    * platform/mac/accessibility/css-content-attribute-expected.txt: Modified to reflect new output.
    * platform/mac/accessibility/css-content-attribute.html: Removed.
    
    
    Canonical link: https://commits.webkit.org/155793@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@174992 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/accessibility/AccessibilityRenderObject.cpp
+++ Source/WebCore/accessibility/AccessibilityRenderObject.cpp
@@ -637,6 +637,10 @@ String AccessibilityRenderObject::textUnderElement(AccessibilityTextUnderElement
         return downcast<RenderText>(*m_renderer).text();
 #endif
 
+    // rangeOfContents does not include CSS-generated content.
+    if (m_renderer->isBeforeOrAfterContent())
+        return AccessibilityNodeObject::textUnderElement(mode);
+
     // We use a text iterator for text objects AND for those cases where we are
     // explicitly asking for the full text under a given element.
     if (is<RenderText>(*m_renderer) || mode.childrenInclusion == AccessibilityTextUnderElementMode::TextUnderElementModeIncludeAllChildren) {
@@ -645,6 +649,12 @@ String AccessibilityRenderObject::textUnderElement(AccessibilityTextUnderElement
         Document* nodeDocument = nullptr;
         RefPtr<Range> textRange;
         if (Node* node = m_renderer->node()) {
+            // rangeOfContents does not include CSS-generated content.
+            Node* firstChild = node->pseudoAwareFirstChild();
+            Node* lastChild = node->pseudoAwareLastChild();
+            if ((firstChild && firstChild->isPseudoElement()) || (lastChild && lastChild->isPseudoElement()))
+                return AccessibilityNodeObject::textUnderElement(mode);
+
             nodeDocument = &node->document();
             textRange = rangeOfContents(*node);
         } else {
