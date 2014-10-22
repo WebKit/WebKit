7f9441a73633ad8d290eace21ab65687b61113e6
Dana Burkart <dburkart@apple.com>
2014-10-22 01:35:50 +0000

    Merged r173238
    Canonical link: https://commits.webkit.org/152923.430@safari-600.2-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-600.2-branch@175008 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/inspector/remote/RemoteInspector.mm
+++ Source/JavaScriptCore/inspector/remote/RemoteInspector.mm
@@ -39,11 +39,27 @@
 #import <wtf/text/WTFString.h>
 #import <xpc/xpc.h>
 
+#if __has_include(<sandbox/private.h>)
+#import <sandbox/private.h>
+#else
+enum sandbox_filter_type {
+    SANDBOX_FILTER_GLOBAL_NAME,
+};
+#endif
+extern "C" {
+    int sandbox_check(pid_t, const char *operation, enum sandbox_filter_type, ...);
+}
+
 #if PLATFORM(IOS)
 #import <wtf/ios/WebCoreThread.h>
 #endif
 
 namespace Inspector {
+
+static bool canAccessWebInspectorMachPort()
+{
+    return sandbox_check(getpid(), "mach-lookup", SANDBOX_FILTER_GLOBAL_NAME, WIRXPCMachPortName) == 0;
+}
 
 static void dispatchAsyncOnQueueSafeForAnyDebuggable(void (^block)())
 {
@@ -70,9 +86,11 @@ RemoteInspector& RemoteInspector::shared()
 
     static dispatch_once_t once;
     dispatch_once(&once, ^{
-        JSC::initializeThreading();
-        if (RemoteInspector::startEnabled)
-            shared.get().start();
+        if (canAccessWebInspectorMachPort()) {
+            JSC::initializeThreading();
+            if (RemoteInspector::startEnabled)
+                shared.get().start();
+        }
     });
 
     return shared;
