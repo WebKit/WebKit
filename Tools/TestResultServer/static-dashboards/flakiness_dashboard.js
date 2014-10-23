a18709232e65fb81ae9950b95551880df3b59d38
Alexey Proskuryakov <ap@apple.com>
2014-10-23 00:05:03 +0000

    Flakiness dashboard should support OS X Yosemite https://bugs.webkit.org/show_bug.cgi?id=137986
    Reviewed by Ryosuke Niwa.
    
    * TestResultServer/app.yaml:
    * TestResultServer/static-dashboards/builders.jsonp:
    * TestResultServer/static-dashboards/flakiness_dashboard.js:
    
    
    
    Canonical link: https://commits.webkit.org/155845@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@175076 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestResultServer/static-dashboards/flakiness_dashboard.js
+++ Tools/TestResultServer/static-dashboards/flakiness_dashboard.js
@@ -55,6 +55,12 @@ var PLATFORMS = {
                             'WK2': { fallbackPlatforms: ['APPLE_MAC_MAVERICKS', 'APPLE_MAC', 'WK2'], expectationsDirectory: 'mac-wk2'}
                         }
                     },
+                    'YOSEMITE': {
+                        subPlatforms: {
+                            'WK1': { fallbackPlatforms: ['APPLE_MAC_YOSEMITE', 'APPLE_MAC'] },
+                            'WK2': { fallbackPlatforms: ['APPLE_MAC_YOSEMITE', 'APPLE_MAC', 'WK2'], expectationsDirectory: 'mac-wk2'}
+                        }
+                    },
                 }
             },
             'WIN': {
@@ -348,6 +354,8 @@ function determineBuilderPlatform(builderNameUpperCase)
     if (string.contains(builderNameUpperCase, 'WIN XP'))
         return 'APPLE_WIN_XP';
 
+    if (string.contains(builderNameUpperCase, 'YOSEMITE'))
+        return determineWKPlatform(builderNameUpperCase, 'APPLE_YOSEMITE');
     if (string.contains(builderNameUpperCase, 'MAVERICKS'))
         return determineWKPlatform(builderNameUpperCase, 'APPLE_MAVERICKS');
     if (string.contains(builderNameUpperCase, 'MOUNTAINLION'))
