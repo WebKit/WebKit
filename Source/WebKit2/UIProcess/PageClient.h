WebKit/WebKit

commit 3259dcd245f76b3a9346161036d20cb21d29b8b3

Add action menu support for images https://bugs.webkit.org/show_bug.cgi?id=137987 <rdar://problem/18717767>
Reviewed by Anders Carlsson, Sam Weinig, and Beth Dakin.

* Resources/AddImageToPhotos.pdf: Added.
* Resources/CopyImage.pdf: Added.
* Resources/SaveImageToDownloads.pdf: Added.
* Resources/ShareImage.pdf: Added.
Add some temporary artwork.

* Scripts/webkit/messages.py:
(struct_or_class):
ActionMenuHitTestResult is a struct.

* Shared/API/c/WKActionMenuItemTypes.h:
Add new items for image menus.

* Shared/mac/ActionMenuHitTestResult.cpp:
(WebKit::ActionMenuHitTestResult::encode):
(WebKit::ActionMenuHitTestResult::decode):
For now, pass the hit image data across to the UI process as a ShareableBitmap.

* Shared/mac/ActionMenuHitTestResult.h:

* UIProcess/API/mac/WKView.mm:
(-[WKView _createActionMenuItemForTag:]):
Refactor action menu item creation into a single function, so that
callers can share code and be much simpler.

(webKitBundleImageNamed):
Add a helper to acquire an image from the WebKit2 bundle.

(-[WKView _copyImage:]):
(-[WKView _saveImageToDownloads:]):
(temporaryPhotosDirectoryPath):
(pathToPhotoOnDisk):
(-[WKView _addImageToPhotos:]):
Add rudimentary implementations of image actions.

(-[WKView _defaultMenuItemsForImage]):
Support the image menu, plus a submenu acquired from NSSharingServicePicker
for the Share menu.

(-[WKView _defaultMenuItems]):
Call _defaultMenuItemsForImage if needed.

(-[WKView _updateActionMenu]):
Refactor _updateActionMenu out of prepareForMenu:withEvent:.

(-[WKView prepareForMenu:withEvent:]):
Ask the Web process to re-hit-test under the location that the menu
will appear. Mark the menu as pending.

(-[WKView _didPerformActionMenuHitTest:]):
When we get the hit test reply, record the result for future use.

(-[WKView willOpenMenu:withEvent:]):
If we got a hit test reply, rebuild the menu immediately. If not, wait
up to 500ms for the reply to come in. This eventually needs to be

Source/WebKit2/UIProcess/PageClient.h
@@ -61,6 +61,7 @@ class ViewSnapshot;
 class WebContextMenuProxy;
 class WebEditCommandProxy;
 class WebPopupMenuProxy;
+struct ActionMenuHitTestResult;
 
 #if ENABLE(TOUCH_EVENTS)
 class NativeWebTouchEvent;
@@ -308,6 +309,8 @@ public:
     virtual void didFirstVisuallyNonEmptyLayoutForMainFrame() = 0;
     virtual void didFinishLoadForMainFrame() = 0;
     virtual void didSameDocumentNavigationForMainFrame(SameDocumentNavigationType) = 0;
+
+    virtual void didPerformActionMenuHitTest(const ActionMenuHitTestResult&) = 0;
 };
 
 } // namespace WebKit

Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -5252,4 +5252,14 @@ void WebPageProxy::removeNavigationGestureSnapshot()
 }
 #endif
 
+void WebPageProxy::performActionMenuHitTestAtLocation(FloatPoint point)
+{
+    m_process->send(Messages::WebPage::PerformActionMenuHitTestAtLocation(point), m_pageID);
+}
+
+void WebPageProxy::didPerformActionMenuHitTest(const ActionMenuHitTestResult& result)
+{
+    m_pageClient.didPerformActionMenuHitTest(result);
+}
+
 } // namespace WebKit

Source/WebKit2/UIProcess/WebPageProxy.h
@@ -167,6 +167,7 @@ class WebPageGroup;
 class WebProcessProxy;
 class WebUserContentControllerProxy;
 class WebWheelEvent;
+struct ActionMenuHitTestResult;
 struct AttributedString;
 struct ColorSpaceData;
 struct DictionaryPopupInfo;
@@ -923,6 +924,7 @@ public:
 #endif
 
     WebHitTestResult* activeActionMenuHitTestResult() const { return m_activeActionMenuHitTestResult.get(); }
+    void performActionMenuHitTestAtLocation(WebCore::FloatPoint);
 
 private:
     WebPageProxy(PageClient&, WebProcessProxy&, uint64_t pageID, const WebPageConfiguration&);
@@ -1300,6 +1302,8 @@ private:
     void dispatchViewStateChange();
     void viewDidLeaveWindow();
     void viewDidEnterWindow();
+
+    void didPerformActionMenuHitTest(const ActionMenuHitTestResult&);
 
     PageClient& m_pageClient;
     std::unique_ptr<API::LoaderClient> m_loaderClient;

Source/WebKit2/UIProcess/WebPageProxy.messages.in
@@ -408,5 +408,7 @@ messages -> WebPageProxy {
     ContentFilterDidBlockLoadForFrame(WebCore::ContentFilter contentFilter, uint64_t frameID)
 #endif
 
-    IsPlayingAudioDidChange(bool newIsPlayingAudio);
+    IsPlayingAudioDidChange(bool newIsPlayingAudio)
+
+    DidPerformActionMenuHitTest(WebKit::ActionMenuHitTestResult result)
 }

Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -28,6 +28,7 @@
 #include "config.h"
 #include "WebPage.h"
 
+#include "ActionMenuHitTestResult.h"
 #include "Arguments.h"
 #include "DataReference.h"
 #include "DragControllerAction.h"
@@ -4793,6 +4794,33 @@ void WebPage::didChangeScrollOffsetForFrame(Frame* frame)
 void WebPage::willChangeCurrentHistoryItemForMainFrame()
 {
     send(Messages::WebPageProxy::WillChangeCurrentHistoryItemForMainFrame());
+}
+
+void WebPage::performActionMenuHitTestAtLocation(WebCore::FloatPoint locationInViewCooordinates)
+{
+    layoutIfNeeded();
+
+    MainFrame& mainFrame = corePage()->mainFrame();
+    if (!mainFrame.view() || !mainFrame.view()->renderView()) {
+        send(Messages::WebPageProxy::DidPerformActionMenuHitTest(ActionMenuHitTestResult()));
+        return;
+    }
+
+    RenderView& mainRenderView = *mainFrame.view()->renderView();
+
+    HitTestRequest request(HitTestRequest::ReadOnly | HitTestRequest::Active | HitTestRequest::AllowChildFrameContent | HitTestRequest::IgnoreClipping | HitTestRequest::DisallowShadowContent);
+
+    HitTestResult hitTestResult(mainFrame.view()->rootViewToContents(roundedIntPoint(locationInViewCooordinates)));
+    mainRenderView.hitTest(request, hitTestResult);
+
+    ActionMenuHitTestResult actionMenuResult;
+
+    if (Image* image = hitTestResult.image()) {
+        actionMenuResult.image = ShareableBitmap::createShareable(IntSize(image->size()), ShareableBitmap::SupportsAlpha);
+        actionMenuResult.image->createGraphicsContext()->drawImage(image, ColorSpaceDeviceRGB, IntPoint());
+    }
+
+    send(Messages::WebPageProxy::DidPerformActionMenuHitTest(actionMenuResult));
 }
 
 } // namespace WebKit

Source/WebKit2/WebProcess/WebPage/WebPage.h
@@ -1044,6 +1044,8 @@ private:
 
     void reportUsedFeatures();
 
+    void performActionMenuHitTestAtLocation(WebCore::FloatPoint);
+
     uint64_t m_pageID;
 
     std::unique_ptr<WebCore::Page> m_page;

Source/WebKit2/WebProcess/WebPage/WebPage.messages.in
@@ -387,4 +387,5 @@ messages -> WebPage LegacyReceiver {
     GetBytecodeProfile(uint64_t callbackID)
     
     TakeSnapshot(WebCore::IntRect snapshotRect, WebCore::IntSize bitmapSize, uint32_t options, uint64_t callbackID)
+    PerformActionMenuHitTestAtLocation(WebCore::FloatPoint location)
 }

commit 090f1e26d20825048ad6f4f494d6dc4700cbc13c

EFL and GTK ports have broken since r175075 https://bugs.webkit.org/show_bug.cgi?id=137995
Unreviewed, EFL and GTK ports build fix.

Need to use PLATFORM(MAC) guard for ActionMenuHitTest functions.

* UIProcess/PageClient.h:
* UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::removeNavigationGestureSnapshot):
* UIProcess/WebPageProxy.h:
* UIProcess/WebPageProxy.messages.in:
* WebProcess/WebPage/WebPage.cpp:
* WebProcess/WebPage/WebPage.h:
* WebProcess/WebPage/WebPage.messages.in:

Canonical link: https://commits.webkit.org/155848@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@175079 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/UIProcess/PageClient.h
@@ -310,7 +310,9 @@ public:
     virtual void didFinishLoadForMainFrame() = 0;
     virtual void didSameDocumentNavigationForMainFrame(SameDocumentNavigationType) = 0;
 
+#if PLATFORM(MAC)
     virtual void didPerformActionMenuHitTest(const ActionMenuHitTestResult&) = 0;
+#endif
 };
 
 } // namespace WebKit

Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -5250,7 +5250,6 @@ void WebPageProxy::removeNavigationGestureSnapshot()
 {
     m_pageClient.removeNavigationGestureSnapshot();
 }
-#endif
 
 void WebPageProxy::performActionMenuHitTestAtLocation(FloatPoint point)
 {
@@ -5261,5 +5260,6 @@ void WebPageProxy::didPerformActionMenuHitTest(const ActionMenuHitTestResult& re
 {
     m_pageClient.didPerformActionMenuHitTest(result);
 }
+#endif
 
 } // namespace WebKit

Source/WebKit2/UIProcess/WebPageProxy.h
@@ -921,10 +921,10 @@ public:
 
 #if PLATFORM(MAC)
     void removeNavigationGestureSnapshot();
-#endif
 
     WebHitTestResult* activeActionMenuHitTestResult() const { return m_activeActionMenuHitTestResult.get(); }
     void performActionMenuHitTestAtLocation(WebCore::FloatPoint);
+#endif
 
 private:
     WebPageProxy(PageClient&, WebProcessProxy&, uint64_t pageID, const WebPageConfiguration&);
@@ -1303,7 +1303,9 @@ private:
     void viewDidLeaveWindow();
     void viewDidEnterWindow();
 
+#if PLATFORM(MAC)
     void didPerformActionMenuHitTest(const ActionMenuHitTestResult&);
+#endif
 
     PageClient& m_pageClient;
     std::unique_ptr<API::LoaderClient> m_loaderClient;

Source/WebKit2/UIProcess/WebPageProxy.messages.in
@@ -410,5 +410,7 @@ messages -> WebPageProxy {
 
     IsPlayingAudioDidChange(bool newIsPlayingAudio)
 
+#if PLATFORM(MAC)
     DidPerformActionMenuHitTest(WebKit::ActionMenuHitTestResult result)
+#endif
 }

Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -28,7 +28,6 @@
 #include "config.h"
 #include "WebPage.h"
 
-#include "ActionMenuHitTestResult.h"
 #include "Arguments.h"
 #include "DataReference.h"
 #include "DragControllerAction.h"
@@ -199,6 +198,10 @@
 #include <CoreText/CTFontDescriptorPriv.h>
 #include <CoreText/CTFontPriv.h>
 #include <WebCore/Icon.h>
+#endif
+
+#if PLATFORM(MAC)
+#include "ActionMenuHitTestResult.h"
 #endif
 
 #ifndef NDEBUG
@@ -4796,6 +4799,7 @@ void WebPage::willChangeCurrentHistoryItemForMainFrame()
     send(Messages::WebPageProxy::WillChangeCurrentHistoryItemForMainFrame());
 }
 
+#if PLATFORM(MAC)
 void WebPage::performActionMenuHitTestAtLocation(WebCore::FloatPoint locationInViewCooordinates)
 {
     layoutIfNeeded();
@@ -4822,5 +4826,6 @@ void WebPage::performActionMenuHitTestAtLocation(WebCore::FloatPoint locationInV
 
     send(Messages::WebPageProxy::DidPerformActionMenuHitTest(actionMenuResult));
 }
+#endif
 
 } // namespace WebKit

Source/WebKit2/WebProcess/WebPage/WebPage.h
@@ -1044,7 +1044,9 @@ private:
 
     void reportUsedFeatures();
 
+#if PLATFORM(MAC)
     void performActionMenuHitTestAtLocation(WebCore::FloatPoint);
+#endif
 
     uint64_t m_pageID;
 

Source/WebKit2/WebProcess/WebPage/WebPage.messages.in
@@ -387,5 +387,7 @@ messages -> WebPage LegacyReceiver {
     GetBytecodeProfile(uint64_t callbackID)
     
     TakeSnapshot(WebCore::IntRect snapshotRect, WebCore::IntSize bitmapSize, uint32_t options, uint64_t callbackID)
+#if PLATFORM(MAC)
     PerformActionMenuHitTestAtLocation(WebCore::FloatPoint location)
+#endif
 }