95933084af283578bd22336648427a071238b0be
Simon Fraser <simon.fraser@apple.com>
2014-10-31 23:38:12 +0000

    Brace cleanup in StyleResolver::adjustRenderStyle() https://bugs.webkit.org/show_bug.cgi?id=138267
    Reviewed by Chris Dumez.
    
    No braces for single-line clauses.
    
    * css/StyleResolver.cpp:
    (WebCore::StyleResolver::adjustRenderStyle):
    
    
    Canonical link: https://commits.webkit.org/156109@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@175438 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/StyleResolver.cpp
+++ Source/WebCore/css/StyleResolver.cpp
@@ -1341,9 +1341,8 @@ void StyleResolver::adjustRenderStyle(RenderStyle& style, const RenderStyle& par
     }
 
     // Let the theme also have a crack at adjusting the style.
-    if (style.hasAppearance()) {
+    if (style.hasAppearance())
         RenderTheme::defaultTheme()->adjustStyle(*this, style, e, m_state.hasUAAppearance(), m_state.borderData(), m_state.backgroundData(), m_state.backgroundColor());
-    }
 
     // If we have first-letter pseudo style, do not share this style.
     if (style.hasPseudoStyle(FIRST_LETTER))
