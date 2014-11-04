22e896c93ba8de6f957122b15e5de0e19c91c508
Dana Burkart <dburkart@apple.com>
2014-11-04 07:51:58 +0000

    Merge r173061. <rdar://problem/18856115>
    Canonical link: https://commits.webkit.org/152923.509@safari-600.3-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-600.3-branch@175515 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderObject.cpp
+++ Source/WebCore/rendering/RenderObject.cpp
@@ -1891,7 +1891,11 @@ RenderElement* RenderObject::container(const RenderLayerModelObject* repaintCont
 bool RenderObject::isSelectionBorder() const
 {
     SelectionState st = selectionState();
-    return st == SelectionStart || st == SelectionEnd || st == SelectionBoth;
+    return st == SelectionStart
+        || st == SelectionEnd
+        || st == SelectionBoth
+        || view().selectionStart() == this
+        || view().selectionEnd() == this;
 }
 
 inline void RenderObject::clearLayoutRootIfNeeded() const
