248e3f2d2aa07afb5d6de41eba89e0779465046c
Dana Burkart <dburkart@apple.com>
2014-11-04 18:42:28 +0000

    Merge r175345. <rdar://problem/18856263>
    Canonical link: https://commits.webkit.org/152923.519@safari-600.3-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-600.3-branch@175553 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RootInlineBox.h
+++ Source/WebCore/rendering/RootInlineBox.h
@@ -145,6 +145,13 @@ public:
             m_floats = std::make_unique<Vector<RenderBox*>>(1, &floatingBox);
     }
 
+    void removeFloat(RenderBox& floatingBox)
+    {
+        ASSERT(m_floats);
+        ASSERT(m_floats->contains(&floatingBox));
+        m_floats->remove(m_floats->find(&floatingBox));
+    }
+
     Vector<RenderBox*>* floatsPtr() { ASSERT(!isDirty()); return m_floats.get(); }
 
     virtual void extractLineBoxFromRenderObject() override final;
