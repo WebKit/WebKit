67edabe2f8cbf788f861314cc51dc2582cf8fcc3
Mark Lam <mark.lam@apple.com>
2014-11-06 23:53:50 +0000

    Refactor the get-by-pname.js test. <https://webkit.org/b/138483>
    Reviewed by Geoffrey Garen.
    
    Refactored the test code to:
    1. make it easier to add more test cases.
    2. test each case against different tier JITs.
    3. print only one set of results for each tier being tested.
    
    * js/get-by-pname-expected.txt:
    * js/script-tests/get-by-pname.js:
    (getByPnameOnConstant):
    (getByPnameOnVar):
    (testExpr):
    (testExpectedResult):
    (isTierIteration):
    (tierName):
    
    
    
    Canonical link: https://commits.webkit.org/156313@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@175722 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/js/script-tests/get-by-pname.js
+++ LayoutTests/js/script-tests/get-by-pname.js
@@ -15,11 +15,58 @@ var q = {a:1, b:2, c:3, d:4, e:3457};
 var r = {a:1, b:2, c:3, d:4, e:91, f:12};
 var s = {a:1, b:2, c:3, d:4, e:91, f:12, g:69};
 
-for (var i = 0; i < 100; ++i) {
-    shouldBe("foo(o)", "11");
-    shouldBe("foo(p)", "10");
-    shouldBe("foo(q)", "3467");
-    shouldBe("foo(r)", "113");
-    shouldBe("foo(s)", "182");
+var testCases = [
+    [ "foo(o)", "11" ],
+    [ "foo(p)", "10" ],
+    [ "foo(q)", "3467" ],
+    [ "foo(r)", "113" ],
+    [ "foo(s)", "182" ],
+];
+
+function testExpr(index) {
+    return testCases[index][0];
+}
+function testExpectedResult(index) {
+    return testCases[index][1];
+}
+
+// The tiers should be sorted from lowest iterations to highest.
+var tiers = [
+    // name, iterations
+    [ "cold", 0 ],
+    [ "llint", 10 ],
+    [ "baseline", 500 ],
+    [ "dfg", 1000 ],
+    // [ "ftl", 100000 ],
+];
+var highestIteration = tiers[tiers.length - 1][1];
+
+function isTierIteration(iteration) {
+    for (var i = 0; i < tiers.length; i++) {
+        var tierIteration = tiers[i][1];
+        if (iteration < tierIteration)
+            return false;
+        if (iteration == tierIteration)
+            return true;
+    }
+    return false;
+}
+function tierName(iteration) {
+    for (var i = 0; i < tiers.length; i++) {
+        if (iteration == tiers[i][1])
+            return tiers[i][0];
+    }
+}
+
+for (var i = 0; i <= highestIteration; ++i) {
+    if (isTierIteration(i)) {
+        debug("Test tier: " + tierName(i));
+        for (var j = 0; j < testCases.length; j++)
+            shouldBe(testExpr(j), testExpectedResult(j));
+        debug("");
+    } else {
+        for (var j = 0; j < testCases.length; j++)
+            eval(testExpr(j));
+    }
 }
 
