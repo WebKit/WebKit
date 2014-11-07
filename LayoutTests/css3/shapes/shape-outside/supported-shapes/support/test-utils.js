a8381641ebd868578f3fb109e82136eef67866f4
Bem Jones-Bey <bjonesbe@adobe.com>
2014-11-07 20:02:58 +0000

    [CSS Shapes] the sandbag JS utility code isn't producing the correct expected result in the shape-image gradient tests https://bugs.webkit.org/show_bug.cgi?id=135923
    Reviewed by Dirk Schulze.
    
    Import tests from upstream. They have been modified so that they use
    testharness to validate success or failure instead of being reftests,
    allowing for fuzziness in matching to get around small variances
    between implementations.
    
    * TestExpectations:
    * css3/shapes/shape-outside/shape-image/gradients/shape-outside-radial-gradient-001-expected.html: Removed.
    * css3/shapes/shape-outside/shape-image/gradients/shape-outside-radial-gradient-001-expected.txt: Added.
    * css3/shapes/shape-outside/shape-image/gradients/shape-outside-radial-gradient-001.html:
    * css3/shapes/shape-outside/shape-image/gradients/shape-outside-radial-gradient-002-expected.html: Removed.
    * css3/shapes/shape-outside/shape-image/gradients/shape-outside-radial-gradient-002-expected.txt: Added.
    * css3/shapes/shape-outside/shape-image/gradients/shape-outside-radial-gradient-002.html:
    * css3/shapes/shape-outside/shape-image/gradients/shape-outside-radial-gradient-003-expected.html: Removed.
    * css3/shapes/shape-outside/shape-image/gradients/shape-outside-radial-gradient-003-expected.txt: Added.
    * css3/shapes/shape-outside/shape-image/gradients/shape-outside-radial-gradient-003.html:
    * css3/shapes/shape-outside/shape-image/gradients/shape-outside-radial-gradient-004-expected.html: Removed.
    * css3/shapes/shape-outside/shape-image/gradients/shape-outside-radial-gradient-004-expected.txt: Added.
    * css3/shapes/shape-outside/shape-image/gradients/shape-outside-radial-gradient-004.html:
    * css3/shapes/shape-outside/shape-image/gradients/w3c-import.log:
    * css3/shapes/shape-outside/supported-shapes/support/test-utils.js:
    (verifyTextPoints):
    
    
    Canonical link: https://commits.webkit.org/156335@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@175755 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/css3/shapes/shape-outside/supported-shapes/support/test-utils.js
+++ LayoutTests/css3/shapes/shape-outside/supported-shapes/support/test-utils.js
@@ -1,4 +1,5 @@
 function verifyTextPoints(shape, numLines, tolerance, side) {
+    var failed = false;
     if (tolerance === undefined)
         tolerance = 0.5;
     if (side === undefined)
@@ -14,10 +15,15 @@ function verifyTextPoints(shape, numLines, tolerance, side) {
        var actual = line.getBoundingClientRect().left;
        if (side === "right")
             actual = shape.containerWidth - (actual + line.getBoundingClientRect().width);
-       
+
         if( Math.abs( (actual - expected[i])) > tolerance ){
             line.style.setProperty('color', 'red');
             console.log('diff: ' + Math.abs(actual - expected[i]));
+            failed = true;
         }
+    }
+    if (window.done) {
+        assert_false(failed, "Lines positioned properly around the shape.");
+        done();
     }
 }
