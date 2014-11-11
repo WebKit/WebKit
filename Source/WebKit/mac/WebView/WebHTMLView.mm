83c5539542ff81f90e9b1bc94d6e4c957561300d
Beth Dakin <bdakin@apple.com>
2014-11-11 01:21:50 +0000

    WK1: Support default actions for editable text with misspelled word https://bugs.webkit.org/show_bug.cgi?id=138590 -and corresponding- rdar://problem/18877506
    Reviewed by Tim Horton.
    
    Source/WebCore:
    
    * WebCore.exp.in:
    
    Source/WebKit/mac:
    
    If there are misspelled word suggestions for editable text, create a new type of
    menu with a spelling suggestion sub-menu.
    
    * WebView/WebActionMenuController.mm:
    (-[WebActionMenuController isMenuForTextContent]):
    (-[WebActionMenuController _defaultMenuItemsForEditableTextWithSuggestions:]):
    (-[WebActionMenuController _changeSelectionToSuggestion:]):
    (-[WebActionMenuController _createActionMenuItemForTag:withHitTestResult:]):
    (-[WebActionMenuController _defaultMenuItemsForHitTestResult:]):
    * WebView/WebHTMLView.mm:
    (-[WebHTMLView _changeSpellingToWord:]):
    * WebView/WebHTMLViewInternal.h:
    * WebView/WebUIDelegatePrivate.h:
    
    
    
    Canonical link: https://commits.webkit.org/156420@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@175842 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/mac/WebView/WebHTMLView.mm
+++ Source/WebKit/mac/WebView/WebHTMLView.mm
@@ -5179,24 +5179,6 @@ static NSString *fontNameForDescription(NSString *familyName, BOOL italic, BOOL 
     [spellingPanel orderFront:sender];
 }
 
-- (void)_changeSpellingToWord:(NSString *)newWord
-{
-    if (![self _canEdit])
-        return;
-
-    // Don't correct to empty string.  (AppKit checked this, we might as well too.)
-    if (![NSSpellChecker sharedSpellChecker]) {
-        LOG_ERROR("No NSSpellChecker");
-        return;
-    }
-    
-    if ([newWord isEqualToString:@""])
-        return;
-
-    if ([self _shouldReplaceSelectionWithText:newWord givenAction:WebViewInsertActionPasted])
-        [[self _frame] _replaceSelectionWithText:newWord selectReplacement:YES smartReplace:NO];
-}
-
 - (void)changeSpelling:(id)sender
 {
     COMMAND_PROLOGUE
@@ -5978,6 +5960,24 @@ static BOOL writingDirectionKeyBindingsEnabled()
 #else
     return _private->drawingIntoLayer;
 #endif
+}
+
+- (void)_changeSpellingToWord:(NSString *)newWord
+{
+    if (![self _canEdit])
+        return;
+
+    if (![NSSpellChecker sharedSpellChecker]) {
+        LOG_ERROR("No NSSpellChecker");
+        return;
+    }
+
+    // Don't correct to empty string.  (AppKit checked this, we might as well too.)    
+    if ([newWord isEqualToString:@""])
+        return;
+
+    if ([self _shouldReplaceSelectionWithText:newWord givenAction:WebViewInsertActionPasted])
+        [[self _frame] _replaceSelectionWithText:newWord selectReplacement:YES smartReplace:NO];
 }
 
 @end
