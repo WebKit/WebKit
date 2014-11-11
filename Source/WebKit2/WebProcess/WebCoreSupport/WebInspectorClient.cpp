WebKit/WebKit

commit f5b60cfbcfb9355db786e5c111629ccef2ab0133

Implement paint flashing via GraphicsLayers in the WK2 inspector overlay https://bugs.webkit.org/show_bug.cgi?id=136136
Reviewed by Sam Weinig, Joseph Pecoraro.
Source/WebCore:

Allow InspectorClient to have a custom implementation of showPaintRect(). For
WebKit2's WebInspectorClient, implement this by creating a set of GraphicsLayers
which are parented in a document overlay, with 0.25s fade-out animations.

Also change InspectorInstrumentation::didPaintImpl() to no longer take a GraphicsContext;
it makes no sense to paint the paint rects directly into the context of the web page.
Now that the paint rects are painted into an overlay, the rectangles need to be converted
to root document coordinates, which is done in InspectorInstrumentation::didPaintImpl().

Remove the generic InspectorOverlay::drawOutline()-based indicators; they will
be reimplemented in a later patch.

* WebCore.exp.in:
* inspector/InspectorClient.h:
(WebCore::InspectorClient::showPaintRect):
* inspector/InspectorInstrumentation.cpp:
(WebCore::InspectorInstrumentation::didPaintImpl):
* inspector/InspectorInstrumentation.h:
(WebCore::InspectorInstrumentation::didPaint):
* inspector/InspectorPageAgent.cpp:
(WebCore::InspectorPageAgent::didPaint):
* inspector/InspectorPageAgent.h:
* page/FrameView.cpp:
(WebCore::FrameView::didPaintContents):
* rendering/RenderLayerBacking.cpp:
(WebCore::RenderLayerBacking::paintContents):

Source/WebKit2:

Allow InspectorClient to have a custom implementation of showPaintRect(). For
WebKit2's WebInspectorClient, implement this by creating a set of GraphicsLayers
which are parented in a document overlay, with 0.25s fade-out animations.

Also change InspectorInstrumentation::didPaintImpl() to no longer take a GraphicsContext;
it makes no sense to paint the paint rects directly into the context of the web page.
Now that the paint rects are painted into an overlay, the rectangles need to be converted

Source/WebKit2/WebProcess/WebCoreSupport/WebInspectorClient.cpp
@@ -28,10 +28,12 @@
 
 #if ENABLE(INSPECTOR)
 
+#include "DrawingArea.h"
 #include "WebInspector.h"
 #include "WebPage.h"
 #include <WebCore/InspectorController.h>
 #include <WebCore/Page.h>
+#include <wtf/CurrentTime.h>
 
 #if PLATFORM(IOS)
 #include <WebCore/InspectorOverlay.h>
@@ -40,6 +42,39 @@
 using namespace WebCore;
 
 namespace WebKit {
+
+class RepaintIndicatorLayerClient final : public GraphicsLayerClient {
+public:
+    RepaintIndicatorLayerClient(WebInspectorClient& inspectorClient)
+        : m_inspectorClient(inspectorClient)
+    {
+    }
+    virtual ~RepaintIndicatorLayerClient() { }
+private:
+    virtual void notifyAnimationEnded(const GraphicsLayer* layer, const String&) override
+    {
+        m_inspectorClient.animationEndedForLayer(layer);
+    }
+    
+    WebInspectorClient& m_inspectorClient;
+};
+
+WebInspectorClient::WebInspectorClient(WebPage* page)
+    : m_page(page)
+    , m_highlightOverlay(nullptr)
+{
+}
+
+WebInspectorClient::~WebInspectorClient()
+{
+    for (auto layer : m_paintRectLayers) {
+        layer->removeFromParent();
+        delete layer;
+    }
+
+    if (m_paintRectOverlay)
+        m_page->uninstallPageOverlay(m_paintRectOverlay.get());
+}
 
 void WebInspectorClient::inspectorDestroyed()
 {
@@ -98,6 +133,48 @@ void WebInspectorClient::hideHighlight()
 #else
     m_page->hideInspectorHighlight();
 #endif
+}
+
+void WebInspectorClient::showPaintRect(const FloatRect& rect)
+{
+    if (!m_paintRectOverlay) {
+        m_paintRectOverlay = PageOverlay::create(this, PageOverlay::OverlayType::Document);
+        m_page->installPageOverlay(m_paintRectOverlay, PageOverlay::FadeMode::DoNotFade);
+    }
+
+    if (!m_paintIndicatorLayerClient)
+        m_paintIndicatorLayerClient = std::make_unique<RepaintIndicatorLayerClient>(*this);
+
+    std::unique_ptr<GraphicsLayer> paintLayer = GraphicsLayer::create(m_page->drawingArea()->graphicsLayerFactory(), *m_paintIndicatorLayerClient);
+    
+    paintLayer->setAnchorPoint(FloatPoint3D());
+    paintLayer->setPosition(rect.location());
+    paintLayer->setSize(rect.size());
+    paintLayer->setBackgroundColor(Color(1.0f, 0.0f, 0.0f, 0.2f));
+
+    KeyframeValueList fadeKeyframes(AnimatedPropertyOpacity);
+    OwnPtr<AnimationValue> intialValue = FloatAnimationValue::create(0, 1);
+    fadeKeyframes.insert(intialValue.release());
+
+    OwnPtr<AnimationValue> finalValue = FloatAnimationValue::create(0.25, 0);
+    fadeKeyframes.insert(finalValue.release());
+    
+    RefPtr<Animation> opacityAnimation = Animation::create();
+    opacityAnimation->setDuration(0.25);
+
+    paintLayer->addAnimation(fadeKeyframes, FloatSize(), opacityAnimation.get(), ASCIILiteral("opacity"), 0);
+    
+    m_paintRectLayers.add(paintLayer.get());
+
+    GraphicsLayer* overlayRootLayer = m_paintRectOverlay->layer();
+    overlayRootLayer->addChild(paintLayer.release());
+}
+
+void WebInspectorClient::animationEndedForLayer(const GraphicsLayer* layer)
+{
+    const_cast<GraphicsLayer*>(layer)->removeFromParent();
+    m_paintRectLayers.remove(const_cast<GraphicsLayer*>(layer));
+    delete layer;
 }
 
 #if PLATFORM(IOS)

Source/WebCore/WebCore.exp.in
@@ -759,6 +759,7 @@ __ZN7WebCore17HTMLSelectElement20optionSelectedByUserEibb
 __ZN7WebCore17HistoryController26saveDocumentAndScrollStateEv
 __ZN7WebCore17HistoryController33restoreScrollPositionAndViewStateEv
 __ZN7WebCore17JSDOMGlobalObject6s_infoE
+__ZN7WebCore17KeyframeValueList6insertEN3WTF10PassOwnPtrIKNS_14AnimationValueEEE
 __ZN7WebCore17MouseRelatedEvent7offsetXEv
 __ZN7WebCore17MouseRelatedEvent7offsetYEv
 __ZN7WebCore17PlatformCAFilters17setFiltersOnLayerEP7CALayerRKNS_16FilterOperationsE
@@ -1377,6 +1378,8 @@ __ZN7WebCore8openFileERKN3WTF6StringENS_12FileOpenModeE
 __ZN7WebCore8toStringERKN3WTF6VectorINS_11ProxyServerELm0ENS0_15CrashOnOverflowEEE
 __ZN7WebCore8toUInt16EPN3JSC9ExecStateENS0_7JSValueENS_30IntegerConversionConfigurationE
 __ZN7WebCore8toUInt64EPN3JSC9ExecStateENS0_7JSValueENS_30IntegerConversionConfigurationE
+__ZN7WebCore9AnimationC1Ev
+__ZN7WebCore9AnimationD1Ev
 __ZN7WebCore9DOMWindow30dispatchAllPendingUnloadEventsEv
 __ZN7WebCore9DOMWindow36dispatchAllPendingBeforeUnloadEventsEv
 __ZN7WebCore9FloatRect5scaleEff

Source/WebCore/inspector/InspectorClient.h
@@ -34,8 +34,9 @@
 
 namespace WebCore {
 
-class InspectorController;
+class FloatRect;
 class Frame;
+class InspectorController;
 class Page;
 
 class InspectorClient {
@@ -60,8 +61,9 @@ public:
     virtual bool canClearBrowserCookies() { return false; }
     virtual void clearBrowserCookies() { }
 
-    virtual bool overridesShowPaintRects() { return false; }
+    virtual bool overridesShowPaintRects() const { return false; }
     virtual void setShowPaintRects(bool) { }
+    virtual void showPaintRect(const FloatRect&) { }
 
     virtual bool canShowDebugBorders() { return false; }
     virtual void setShowDebugBorders(bool) { }

Source/WebCore/inspector/InspectorInstrumentation.cpp
@@ -485,12 +485,13 @@ void InspectorInstrumentation::willPaintImpl(InstrumentingAgents* instrumentingA
         timelineAgent->willPaint(&renderer->frame());
 }
 
-void InspectorInstrumentation::didPaintImpl(InstrumentingAgents*  instrumentingAgents, RenderObject* renderer, GraphicsContext* context, const LayoutRect& rect)
+void InspectorInstrumentation::didPaintImpl(InstrumentingAgents* instrumentingAgents, RenderObject* renderer, const LayoutRect& rect)
 {
     if (InspectorTimelineAgent* timelineAgent = instrumentingAgents->inspectorTimelineAgent())
         timelineAgent->didPaint(renderer, rect);
+
     if (InspectorPageAgent* pageAgent = instrumentingAgents->inspectorPageAgent())
-        pageAgent->didPaint(context, rect);
+        pageAgent->didPaint(renderer, rect);
 }
 
 void InspectorInstrumentation::willScrollLayerImpl(InstrumentingAgents* instrumentingAgents, Frame* frame)

Source/WebCore/inspector/InspectorInstrumentation.h
@@ -166,7 +166,7 @@ public:
     static void willScrollLayer(Frame*);
     static void didScrollLayer(Frame*);
     static void willPaint(RenderObject*);
-    static void didPaint(RenderObject*, GraphicsContext*, const LayoutRect&);
+    static void didPaint(RenderObject*, const LayoutRect&);
     static InspectorInstrumentationCookie willRecalculateStyle(Document*);
     static void didRecalculateStyle(const InspectorInstrumentationCookie&);
     static void didScheduleStyleRecalculation(Document*);
@@ -364,7 +364,7 @@ private:
     static void willScrollLayerImpl(InstrumentingAgents*, Frame*);
     static void didScrollLayerImpl(InstrumentingAgents*);
     static void willPaintImpl(InstrumentingAgents*, RenderObject*);
-    static void didPaintImpl(InstrumentingAgents*, RenderObject*, GraphicsContext*, const LayoutRect&);
+    static void didPaintImpl(InstrumentingAgents*, RenderObject*, const LayoutRect&);
     static InspectorInstrumentationCookie willRecalculateStyleImpl(InstrumentingAgents*, Frame*);
     static void didRecalculateStyleImpl(const InspectorInstrumentationCookie&);
     static void didScheduleStyleRecalculationImpl(InstrumentingAgents*, Document*);
@@ -1121,15 +1121,14 @@ inline void InspectorInstrumentation::willPaint(RenderObject* renderer)
 #endif
 }
 
-inline void InspectorInstrumentation::didPaint(RenderObject* renderer, GraphicsContext* context, const LayoutRect& rect)
+inline void InspectorInstrumentation::didPaint(RenderObject* renderer, const LayoutRect& rect)
 {
 #if ENABLE(INSPECTOR)
     FAST_RETURN_IF_NO_FRONTENDS(void());
     if (InstrumentingAgents* instrumentingAgents = instrumentingAgentsForRenderer(renderer))
-        didPaintImpl(instrumentingAgents, renderer, context, rect);
+        didPaintImpl(instrumentingAgents, renderer, rect);
 #else
     UNUSED_PARAM(renderer);
-    UNUSED_PARAM(context);
     UNUSED_PARAM(rect);
 #endif
 }

Source/WebCore/inspector/InspectorPageAgent.cpp
@@ -880,21 +880,26 @@ void InspectorPageAgent::didRunJavaScriptDialog()
     m_frontendDispatcher->javascriptDialogClosed();
 }
 
-void InspectorPageAgent::didPaint(GraphicsContext* context, const LayoutRect& rect)
+void InspectorPageAgent::didPaint(RenderObject* renderer, const LayoutRect& rect)
 {
-    if (!m_enabled || m_client->overridesShowPaintRects() || !m_showPaintRects)
+    if (!m_enabled || !m_showPaintRects)
         return;
 
-    static int colorSelector = 0;
-    const Color colors[] = {
-        Color(0xFF, 0, 0, 0x3F),
-        Color(0xFF, 0, 0xFF, 0x3F),
-        Color(0, 0, 0xFF, 0x3F),
-    };
+    LayoutRect absoluteRect = LayoutRect(renderer->localToAbsoluteQuad(FloatRect(rect)).boundingBox());
+    FrameView* view = renderer->document().view();
+    
+    LayoutRect rootRect = absoluteRect;
+    if (!view->frame().isMainFrame()) {
+        IntRect rootViewRect = view->contentsToRootView(pixelSnappedIntRect(absoluteRect));
+        rootRect = view->frame().mainFrame().view()->rootViewToContents(rootViewRect);
+    }
+    
+    if (m_client->overridesShowPaintRects()) {
+        m_client->showPaintRect(rect);
+        return;
+    }
 
-    LayoutRect inflatedRect(rect);
-    inflatedRect.inflate(-1);
-    m_overlay->drawOutline(context, inflatedRect, colors[colorSelector++ % WTF_ARRAY_LENGTH(colors)]);
+    // FIXME: the overlay needs to accumulate paint rects and draw them itself.
 }
 
 void InspectorPageAgent::didLayout()

Source/WebCore/inspector/InspectorPageAgent.h
@@ -55,12 +55,12 @@ class DOMWrapperWorld;
 class DocumentLoader;
 class Frame;
 class Frontend;
-class GraphicsContext;
 class InspectorClient;
 class InspectorOverlay;
 class InstrumentingAgents;
 class URL;
 class Page;
+class RenderObject;
 class SharedBuffer;
 class TextResourceDecoder;
 
@@ -140,7 +140,7 @@ public:
     void willRunJavaScriptDialog(const String& message);
     void didRunJavaScriptDialog();
     void applyEmulatedMedia(String*);
-    void didPaint(GraphicsContext*, const LayoutRect&);
+    void didPaint(RenderObject*, const LayoutRect&);
     void didLayout();
     void didScroll();
     void didRecalculateStyle();

Source/WebCore/page/FrameView.cpp
@@ -3669,7 +3669,7 @@ void FrameView::didPaintContents(GraphicsContext* context, const IntRect& dirtyR
         sCurrentPaintTimeStamp = 0;
 
     if (!context->paintingDisabled()) {
-        InspectorInstrumentation::didPaint(renderView(), context, dirtyRect);
+        InspectorInstrumentation::didPaint(renderView(), dirtyRect);
         // FIXME: should probably not fire milestones for snapshot painting. https://bugs.webkit.org/show_bug.cgi?id=117623
         firePaintRelatedMilestonesIfNeeded();
     }

commit 02434970911aa8645e5e79f076137254063ec079

Fix possible crash when closing the web inspector https://bugs.webkit.org/show_bug.cgi?id=138597 rdar://problem/18872688
Reviewed by Andreas Kling.

I reported a crash when reloading a page after inspecting it, possibly caused by
r172864. Speculatively fix by null-checking the main frame.

* WebProcess/WebCoreSupport/WebInspectorClient.cpp:
(WebKit::WebInspectorClient::~WebInspectorClient):


Canonical link: https://commits.webkit.org/156429@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@175851 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/WebProcess/WebCoreSupport/WebInspectorClient.cpp
@@ -74,7 +74,7 @@ WebInspectorClient::~WebInspectorClient()
         delete layer;
     }
 
-    if (m_paintRectOverlay)
+    if (m_paintRectOverlay && m_page->mainFrame())
         m_page->mainFrame()->pageOverlayController().uninstallPageOverlay(m_paintRectOverlay.get(), PageOverlay::FadeMode::Fade);
 }
 