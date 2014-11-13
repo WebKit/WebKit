bf8380b1ba1c6a3b99586ef3137c9f466e627d72
Gyuyoung Kim <gyuyoung.kim@samsung.com>
2014-11-13 08:53:03 +0000

    [EFL] Unexpected crashes in layout test after r175953 https://bugs.webkit.org/show_bug.cgi?id=138652
    Reviewed by Csaba Osztrogonác.
    
    In r175953, it set reusableNodeIndex value with m_partCache's size(). It causes
    out of bound index crash because the reusableNodeIndex is Vector object.
    
    No new test. This patch is to fix broken tests related to widgets on EFL port.
    
    * platform/efl/RenderThemeEfl.cpp:
    (WebCore::RenderThemeEfl::getThemePartFromCache): Need to set reusableNodeIndex with "size() - 1".
    
    
    Canonical link: https://commits.webkit.org/156535@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@176070 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/efl/RenderThemeEfl.cpp
+++ Source/WebCore/platform/efl/RenderThemeEfl.cpp
@@ -251,7 +251,7 @@ RenderThemeEfl::ThemePartCacheEntry* RenderThemeEfl::getThemePartFromCache(FormT
     // requested size to avoid resizing. If there was none, reuse
     // the last item of the list.
     if (!reusableNodeIndex)
-        reusableNodeIndex = m_partCache.size();
+        reusableNodeIndex = m_partCache.size() - 1;
 
     ThemePartCacheEntry* reusedEntry = m_partCache[reusableNodeIndex].get();
     ASSERT(reusedEntry);
