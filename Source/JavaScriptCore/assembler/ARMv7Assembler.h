026bd596185f7b5523b0d24274e650baeefcd5a3
Benjamin Poulain <bpoulain@apple.com>
2014-11-17 08:23:14 +0000

    Merge r176151 - STRH can store values with the wrong offset https://bugs.webkit.org/show_bug.cgi?id=138723
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2014-11-14
    Reviewed by Michael Saboff.
    
    This is the counterpart of r176083 for the str instruction.
    
    I believe this code is currently unreachable because there is only one client of strh()
    in the MacroAssembler and it always setup the scale explicitely.
    
    * assembler/ARMv7Assembler.h:
    (JSC::ARMv7Assembler::strh):

--- Source/JavaScriptCore/assembler/ARMv7Assembler.h
+++ Source/JavaScriptCore/assembler/ARMv7Assembler.h
@@ -1687,8 +1687,8 @@ public:
         ASSERT(rn != ARMRegisters::pc);
         ASSERT(imm.isUInt12());
         
-        if (!((rt | rn) & 8) && imm.isUInt7())
-            m_formatter.oneWordOp5Imm5Reg3Reg3(OP_STRH_imm_T1, imm.getUInt7() >> 2, rn, rt);
+        if (!((rt | rn) & 8) && imm.isUInt6())
+            m_formatter.oneWordOp5Imm5Reg3Reg3(OP_STRH_imm_T1, imm.getUInt6() >> 1, rn, rt);
         else
             m_formatter.twoWordOp12Reg4Reg4Imm12(OP_STRH_imm_T2, rn, rt, imm.getUInt12());
     }
