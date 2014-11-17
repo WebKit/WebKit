WebKit/WebKit

commit c9e4dffd843f416f1739d6f2ce4f4547ec228237

CSS: Implement the :placeholder-shown pseudo-class from Selectors Level 4 https://bugs.webkit.org/show_bug.cgi?id=118162
Reviewed by Antti Koivisto.

Source/WebCore:

Previously, HTMLTextFormControlElement was using some mix of its own state
and style to change the visibility of the placeholder. That approach was a little
bit too fragile, and we do not want the style to depends on the renderer() since
that creates circular dependencies.

The biggest change here is refactoring HTMLTextFormControlElement to have
1) An explicit "visible placeholder" state.
2) Separate the textUpdate() from the visibilityUpdate().
3) Remove the dependencies between the Element's style and the placeholder's style.
   This is done by simply using display:none; on the placeholder so that its parent's visibility
   is irrelevant.

When matching the selector, the style is set as unique since style sharing does not deal with
the changes of HTMLTextFormControlElement.

Tests: fast/css/placeholder-shown-basics.html
       fast/selectors/placeholder-shown-long-adjacent-backtracking.html
       fast/selectors/placeholder-shown-sibling-style-update.html
       fast/selectors/placeholder-shown-style-update.html
       fast/selectors/placeholder-shown-with-input-basics.html
       fast/selectors/placeholder-shown-with-textarea-basics.html

* css/CSSSelector.cpp:
(WebCore::CSSSelector::selectorText):
Add the CSS Selector description for CSSOM.

* css/CSSSelector.h:
* css/SelectorChecker.cpp:
(WebCore::SelectorChecker::checkOne):
* css/SelectorCheckerTestFunctions.h:
(WebCore::isPlaceholderShown):
* css/SelectorPseudoClassAndCompatibilityElementMap.in:
* css/html.css:
(::-webkit-input-placeholder):
Previously, the display was forced through the UA stylesheet. Since the display is now part
of the placeholder visibility, it is explicitly handled by HTMLTextFormControlElement and
its subclasses.

* cssjit/SelectorCompiler.cpp:
(WebCore::SelectorCompiler::addPseudoClassType):

Source/WebCore/html/TextFieldInputType.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2010 Google Inc. All rights reserved.
- * Copyright (C) 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2011, 2014 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -409,6 +409,7 @@ void TextFieldInputType::updatePlaceholderText()
     if (!m_placeholder) {
         m_placeholder = HTMLDivElement::create(element().document());
         m_placeholder->setPseudo(AtomicString("-webkit-input-placeholder", AtomicString::ConstructFromLiteral));
+        m_placeholder->setInlineStyleProperty(CSSPropertyDisplay, element().isPlaceholderVisible() ? CSSValueBlock : CSSValueNone, true);
         element().userAgentShadowRoot()->insertBefore(m_placeholder, m_container ? m_container.get() : innerTextElement(), ASSERT_NO_EXCEPTION);
     }
     m_placeholder->setInnerText(placeholderText, ASSERT_NO_EXCEPTION);
@@ -438,7 +439,7 @@ void TextFieldInputType::subtreeHasChanged()
     // sanitizeUserInputValue().
     // sanitizeValue() is needed because IME input doesn't dispatch BeforeTextInsertedEvent.
     element().setValueFromRenderer(sanitizeValue(convertFromVisibleValue(element().innerTextValue())));
-    element().updatePlaceholderVisibility(false);
+    element().updatePlaceholderVisibility();
     // Recalc for :invalid change.
     element().setNeedsStyleRecalc();
 
@@ -472,7 +473,7 @@ void TextFieldInputType::updateInnerTextValue()
         // Update the renderer value if the formControlValueMatchesRenderer() flag is false.
         // It protects an unacceptable renderer value from being overwritten with the DOM value.
         element().setInnerTextValue(visibleValue());
-        element().updatePlaceholderVisibility(false);
+        element().updatePlaceholderVisibility();
     }
 }
 

LayoutTests/fast/css/css-selector-text-expected.txt
@@ -40,6 +40,8 @@ PASS parseThenSerializeRule(':focus { }') is ':focus { }'
 PASS parseThenSerializeRule(':hover { }') is ':hover { }'
 PASS parseThenSerializeRule(':indeterminate { }') is ':indeterminate { }'
 PASS parseThenSerializeRule(':link { }') is ':link { }'
+PASS parseThenSerializeRule(':not(:placeholder-shown) { }') is ':not(:placeholder-shown) { }'
+PASS parseThenSerializeRule(':placeholder-shown { }') is ':placeholder-shown { }'
 PASS parseThenSerializeRule(':root { }') is ':root { }'
 PASS parseThenSerializeRule(':target { }') is ':target { }'
 PASS parseThenSerializeRule(':visited { }') is ':visited { }'

LayoutTests/fast/css/css-selector-text.html
@@ -71,6 +71,8 @@ testSelectorRoundTrip(":focus");
 testSelectorRoundTrip(":hover");
 testSelectorRoundTrip(":indeterminate");
 testSelectorRoundTrip(":link");
+testSelectorRoundTrip(":not(:placeholder-shown)");
+testSelectorRoundTrip(":placeholder-shown");
 testSelectorRoundTrip(":root");
 testSelectorRoundTrip(":target");
 testSelectorRoundTrip(":visited");

LayoutTests/fast/css/css-set-selector-text-expected.txt
@@ -50,6 +50,8 @@ PASS setThenReadSelectorText(':focus') is ':focus'
 PASS setThenReadSelectorText(':hover') is ':hover'
 PASS setThenReadSelectorText(':indeterminate') is ':indeterminate'
 PASS setThenReadSelectorText(':link') is ':link'
+PASS setThenReadSelectorText(':not(:placeholder-shown)') is ':not(:placeholder-shown)'
+PASS setThenReadSelectorText(':placeholder-shown') is ':placeholder-shown'
 PASS setThenReadSelectorText(':root') is ':root'
 PASS setThenReadSelectorText(':target') is ':target'
 PASS setThenReadSelectorText(':visited') is ':visited'

LayoutTests/fast/css/css-set-selector-text.html
@@ -88,6 +88,8 @@ testSelectorRoundTrip(":focus");
 testSelectorRoundTrip(":hover");
 testSelectorRoundTrip(":indeterminate");
 testSelectorRoundTrip(":link");
+testSelectorRoundTrip(":not(:placeholder-shown)");
+testSelectorRoundTrip(":placeholder-shown");
 testSelectorRoundTrip(":root");
 testSelectorRoundTrip(":target");
 testSelectorRoundTrip(":visited");

LayoutTests/fast/css/placeholder-shown-basics-expected.html
@@ -1,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        input, textarea {
+            height: 8px;
+        }
+        .placeholder-shown {
+            background-color: green;
+        }
+    </style>
+</head>
+<body>
+    <p>This test checks how various input elements are styled when :placeholder-shown is applied.</p>
+    <div>
+        <textarea></textarea>
+        <textarea placeholder></textarea>
+        <textarea placeholder="WebKit" class="placeholder-shown"></textarea>
+        <textarea placeholder="WebKit">Foobar</textarea>
+    </div>
+    <div>
+        <input>
+        <input placeholder>
+        <input placeholder="WebKit" class="placeholder-shown">
+        <input placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="">
+        <input type="" placeholder>
+        <input type="" placeholder="WebKit" class="placeholder-shown">
+        <input type="" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="hidden">
+        <input type="hidden" placeholder>
+        <input type="hidden" placeholder="WebKit" class="placeholder-shown">
+        <input type="hidden" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="text">
+        <input type="text" placeholder>
+        <input type="text" placeholder="WebKit" class="placeholder-shown">
+        <input type="text" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="tel">
+        <input type="tel" placeholder>
+        <input type="tel" placeholder="WebKit" class="placeholder-shown">
+        <input type="tel" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="url">
+        <input type="url" placeholder>
+        <input type="url" placeholder="WebKit" class="placeholder-shown">
+        <input type="url" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="email">
+        <input type="email" placeholder>
+        <input type="email" placeholder="WebKit" class="placeholder-shown">
+        <input type="email" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="password">
+        <input type="password" placeholder>
+        <input type="password" placeholder="WebKit" class="placeholder-shown">
+        <input type="password" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="datetime">
+        <input type="datetime" placeholder>
+        <input type="datetime" placeholder="WebKit" class="placeholder-shown">
+        <input type="datetime" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="number">
+        <input type="number" placeholder>
+        <input type="number" placeholder="WebKit" class="placeholder-shown">
+        <input type="number" placeholder="WebKit" value="0">
+    </div>
+    <div>
+        <input type="range">
+        <input type="range" placeholder>
+        <input type="range" placeholder="WebKit">
+        <input type="range" placeholder="WebKit" value="1">
+    </div>
+    <div>
+        <input type="checkbox">
+        <input type="checkbox" placeholder>
+        <input type="checkbox" placeholder="WebKit">
+        <input type="checkbox" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="radio">
+        <input type="radio" placeholder>
+        <input type="radio" placeholder="WebKit">
+        <input type="radio" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="submit">
+        <input type="submit" placeholder>
+        <input type="submit" placeholder="WebKit">
+        <input type="submit" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="color">
+        <input type="color" placeholder>
+        <input type="color" placeholder="WebKit">
+        <input type="color" placeholder="WebKit" value="Foobar">
+    </div>
+
+</body>
+</html>

LayoutTests/fast/css/placeholder-shown-basics.html
@@ -1,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        input, textarea {
+            height: 8px;
+        }
+        :placeholder-shown {
+            background-color: green;
+        }
+    </style>
+</head>
+<body>
+    <p>This test checks how various input elements are styled when :placeholder-shown is applied.</p>
+    <div>
+        <textarea></textarea>
+        <textarea placeholder></textarea>
+        <textarea placeholder="WebKit"></textarea>
+        <textarea placeholder="WebKit">Foobar</textarea>
+    </div>
+    <div>
+        <input>
+        <input placeholder>
+        <input placeholder="WebKit">
+        <input placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="">
+        <input type="" placeholder>
+        <input type="" placeholder="WebKit">
+        <input type="" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="hidden">
+        <input type="hidden" placeholder>
+        <input type="hidden" placeholder="WebKit">
+        <input type="hidden" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="text">
+        <input type="text" placeholder>
+        <input type="text" placeholder="WebKit">
+        <input type="text" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="tel">
+        <input type="tel" placeholder>
+        <input type="tel" placeholder="WebKit">
+        <input type="tel" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="url">
+        <input type="url" placeholder>
+        <input type="url" placeholder="WebKit">
+        <input type="url" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="email">
+        <input type="email" placeholder>
+        <input type="email" placeholder="WebKit">
+        <input type="email" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="password">
+        <input type="password" placeholder>
+        <input type="password" placeholder="WebKit">
+        <input type="password" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="datetime">
+        <input type="datetime" placeholder>
+        <input type="datetime" placeholder="WebKit">
+        <input type="datetime" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="number">
+        <input type="number" placeholder>
+        <input type="number" placeholder="WebKit">
+        <input type="number" placeholder="WebKit" value="0">
+    </div>
+    <div>
+        <input type="range">
+        <input type="range" placeholder>
+        <input type="range" placeholder="WebKit">
+        <input type="range" placeholder="WebKit" value="1">
+    </div>
+    <div>
+        <input type="checkbox">
+        <input type="checkbox" placeholder>
+        <input type="checkbox" placeholder="WebKit">
+        <input type="checkbox" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="radio">
+        <input type="radio" placeholder>
+        <input type="radio" placeholder="WebKit">
+        <input type="radio" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="submit">
+        <input type="submit" placeholder>
+        <input type="submit" placeholder="WebKit">
+        <input type="submit" placeholder="WebKit" value="Foobar">
+    </div>
+    <div>
+        <input type="color">
+        <input type="color" placeholder>
+        <input type="color" placeholder="WebKit">
+        <input type="color" placeholder="WebKit" value="Foobar">
+    </div>
+
+</body>
+</html>

LayoutTests/fast/selectors/placeholder-shown-long-adjacent-backtracking-expected.txt
@@ -1,0 +1,75 @@
+Test backtracking of a long chain of :placeholder-shown siblings to catch any issue with register allocation.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS document.querySelectorAll(":placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown~:not(:placeholder-shown)").length is 3
+PASS document.querySelectorAll(":placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown~:not(:placeholder-shown)")[0] is document.getElementById("target1")
+PASS document.querySelectorAll(":placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown~:not(:placeholder-shown)")[1] is document.getElementById("target2")
+PASS document.querySelectorAll(":placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown+:placeholder-shown~:not(:placeholder-shown)")[2] is document.getElementById("target3")
+PASS document.querySelectorAll("input").length is 61
+PASS getComputedStyle(document.querySelectorAll("input")[0]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[1]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[2]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[3]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[4]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[5]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[6]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[7]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[8]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[9]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[10]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[11]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[12]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[13]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[14]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[15]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[16]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[17]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[18]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[19]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[20]).backgroundColor is "rgb(1, 2, 3)"
+PASS getComputedStyle(document.querySelectorAll("input")[21]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[22]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[23]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[24]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[25]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[26]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[27]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[28]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[29]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[30]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[31]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[32]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[33]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[34]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[35]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[36]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[37]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[38]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[39]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[40]).backgroundColor is "rgb(1, 2, 3)"
+PASS getComputedStyle(document.querySelectorAll("input")[41]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[42]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[43]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[44]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[45]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[46]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[47]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[48]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[49]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[50]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[51]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[52]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[53]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[54]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[55]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[56]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[57]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[58]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[59]).backgroundColor is "rgb(255, 255, 255)"
+PASS getComputedStyle(document.querySelectorAll("input")[60]).backgroundColor is "rgb(1, 2, 3)"
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

commit 0084fce98ce7a84526575ff57d35d736bb1b85a4

Merge r176082 - REGRESSION (r172826): Password field placeholder text is missing if placeholder attribute precedes type attribute
<https://bugs.webkit.org/show_bug.cgi?id=138682>
<rdar://problem/18574213>

Source/WebCore:

When the placeholder attribute is encountered, a shadow tree is created
to include the placeholder element. When the type attribute is
encountered, the previous shadow tree is destroyed, and a new shadow
tree is created. TextFieldInputType::createShadowSubtree() did not
handle creating the placeholder element.

This regressed in r172826 because prior to r172826, the placeholder
element would be recreated when
HTMLTextFormControlElement::updatePlaceholderVisibility() was called.
In r172826, updatePlaceholderVisibility() was changed to only show or
hide the placeholder element if it exists.

Reviewed by Benjamin Poulain.

* html/TextFieldInputType.cpp:
(WebCore::TextFieldInputType::createShadowSubtree):
Call updatePlaceholderVisibility() to insert the placeholder element
(after the inner text element has been inserted, as it appears before
it in the shadow DOM).

LayoutTests:

Reviewed by Benjamin Poulain.

* fast/forms/placeholder-attribute-ordering-expected.html: Added.
* fast/forms/placeholder-attribute-ordering.html: Added.

Source/WebCore/html/TextFieldInputType.cpp
@@ -241,6 +241,7 @@ void TextFieldInputType::createShadowSubtree()
     m_innerText = TextControlInnerTextElement::create(document);
     if (!createsContainer) {
         element().userAgentShadowRoot()->appendChild(m_innerText, IGNORE_EXCEPTION);
+        updatePlaceholderText();
         return;
     }
 
@@ -252,6 +253,8 @@ void TextFieldInputType::createShadowSubtree()
     m_innerBlock = TextControlInnerElement::create(document);
     m_innerBlock->appendChild(m_innerText, IGNORE_EXCEPTION);
     m_container->appendChild(m_innerBlock, IGNORE_EXCEPTION);
+
+    updatePlaceholderText();
 
     if (shouldHaveSpinButton) {
         m_innerSpinButton = SpinButtonElement::create(document, *this);

LayoutTests/fast/forms/placeholder-attribute-ordering-expected.html
@@ -1,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <p>This test ensures that placeholder text is shown regardless of the 'type' and 'placeholder' attribute order.</p>
+    <div>
+        <input type="text" placeholder="12334567890">
+        <input type="text" placeholder="12334567890">
+        <input type="text" placeholder="12334567890">
+        <input type="text" placeholder="12334567890">
+        <input type="password" placeholder="12334567890">
+        <input type="number" placeholder="12334567890">
+        <input type="text" placeholder="12334567890">
+    </div>
+    <div>
+        <input type="password" placeholder="12334567890">
+        <input type="password" placeholder="12334567890">
+        <input type="password" placeholder="12334567890">
+        <input type="password" placeholder="12334567890">
+        <input type="text" placeholder="12334567890">
+        <input type="number" placeholder="12334567890">
+        <input type="password" placeholder="12334567890">
+    </div>
+    <div>
+        <input type="number" placeholder="12334567890">
+        <input type="number" placeholder="12334567890">
+        <input type="number" placeholder="12334567890">
+        <input type="number" placeholder="12334567890">
+        <input type="text" placeholder="12334567890">
+        <input type="password" placeholder="12334567890">
+        <input type="number" placeholder="12334567890">
+    </div>
+    <div>
+        <input type="text" placeholder="12334567890">
+        <input type="password" placeholder="12334567890">
+        <input type="number" placeholder="12334567890">
+    </div>
+</body>
+</html>

LayoutTests/fast/forms/placeholder-attribute-ordering.html
@@ -1,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <p>This test ensures that placeholder text is shown regardless of the 'type' and 'placeholder' attribute order.</p>
+    <div>
+        <input type="text" placeholder="12334567890">
+        <input placeholder="12334567890" type="text">
+        <input type="text" id="a" placeholder="12334567890">
+        <input type="text" id="b" placeholder="NA">
+        <input type="text" id="c">
+        <input type="text" id="d">
+        <input id="e">
+    </div>
+    <div>
+        <input type="password" placeholder="12334567890">
+        <input placeholder="12334567890" type="password">
+        <input type="password" id="f" placeholder="12334567890">
+        <input type="password" id="g" placeholder="NA">
+        <input type="password" id="h">
+        <input type="password" id="i">
+        <input id="j">
+    </div>
+    <div>
+        <input type="number" placeholder="12334567890">
+        <input placeholder="12334567890" type="number">
+        <input type="number" id="k" placeholder="12334567890">
+        <input type="number" id="l" placeholder="456">
+        <input type="number" id="m">
+        <input type="number" id="n">
+        <input id="o">
+    </div>
+    <div>
+        <input id="p">
+        <input id="q">
+        <input id="r">
+    </div>
+    <script>
+        for (var id of ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"])
+            document.getElementById(id).placeholder = "12334567890";
+
+        document.getElementById("c").type = "password";
+        document.getElementById("d").type = "number";
+        document.getElementById("e").type = "text";
+
+        document.getElementById("h").type = "text";
+        document.getElementById("i").type = "number";
+        document.getElementById("j").type = "password";
+
+        document.getElementById("m").type = "text";
+        document.getElementById("n").type = "password";
+        document.getElementById("o").type = "number";
+
+        document.getElementById("p").type = "text";
+        document.getElementById("q").type = "password";
+        document.getElementById("r").type = "number";
+        for (var id of ["p", "q", "r"])
+            document.getElementById(id).placeholder = "12334567890";
+    </script>
+</body>
+</html>