042f77f43f4cabeb48181689f8e120040c532554
Joseph Pecoraro <joepeck@webkit.org>
2014-11-19 21:39:33 +0000

    Unreviewed build fixes after r176329.
      - export all of the codegen python files as they are included by the main generator
      - update the imports of the main generator to match __init__.py
      - remove bundling the python scripts as framework resources, just have them PrivateHeaders
    
    * JavaScriptCore.xcodeproj/project.pbxproj:
    * inspector/scripts/generate-inspector-protocol-bindings.py:
    
    Canonical link: https://commits.webkit.org/156742@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@176335 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/inspector/scripts/generate-inspector-protocol-bindings.py
+++ Source/JavaScriptCore/inspector/scripts/generate-inspector-protocol-bindings.py
@@ -57,13 +57,13 @@ except ImportError, e:
     from objc_generator import *
 
     from generate_cpp_alternate_backend_dispatcher_header import *
-    from generate_cpp_backend_commands import *
     from generate_cpp_backend_dispatcher_header import *
     from generate_cpp_backend_dispatcher_implementation import *
     from generate_cpp_frontend_dispatcher_header import *
     from generate_cpp_frontend_dispatcher_implementation import *
     from generate_cpp_protocol_types_header import *
     from generate_cpp_protocol_types_implementation import *
+    from generate_js_backend_commands import *
     from generate_objc_backend_dispatcher_header import *
     from generate_objc_backend_dispatcher_implementation import *
     from generate_objc_configuration_header import *
