dba6c805b087fdcba48c5525a81bd0d4f35793b6
Conrad Shultz <conrad_shultz@apple.com>
2014-11-24 05:21:13 +0000

    Page previews should have titles https://bugs.webkit.org/show_bug.cgi?id=139020
    Reviewed by Tim Horton.
    
    * UIProcess/API/Cocoa/WKViewPrivate.h:
    Declare -_titleForPreviewOfURL:.
    
    * UIProcess/API/mac/WKView.mm:
    (-[WKView _titleForPreviewOfURL:]):
    Stub implementation.
    
    * UIProcess/mac/WKActionMenuController.mm:
    (+[WKPagePreviewViewController previewPadding]):
    Account for the title height.
    (-[WKPagePreviewViewController loadView]):
    Set correct autoresize masks on some views; create and configure a title view that is
    horizontally and vertically centered.
    (-[WKActionMenuController pagePreviewViewController:titleForPreviewOfURL:]):
    WKPagePreviewViewControllerDelegate method; wrap -[WKView _titleForPreviewOfURL:].
    
    Canonical link: https://commits.webkit.org/156891@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@176511 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/Cocoa/WKViewPrivate.h
+++ Source/WebKit2/UIProcess/API/Cocoa/WKViewPrivate.h
@@ -123,6 +123,7 @@
 - (NSArray *)_actionMenuItemsForHitTestResult:(WKHitTestResultRef)hitTestResult withType:(_WKActionMenuType)type defaultActionMenuItems:(NSArray *)defaultMenuItems userData:(WKTypeRef)userData;
 
 - (NSView *)_viewForPreviewingURL:(NSURL *)url initialFrameSize:(NSSize)initialFrameSize;
+- (NSString *)_titleForPreviewOfURL:(NSURL *)url;
 - (void)_finishPreviewingURL:(NSURL *)url withPreviewView:(NSView *)previewView;
 - (void)_handleClickInPreviewView:(NSView *)previewView URL:(NSURL *)url;
 
