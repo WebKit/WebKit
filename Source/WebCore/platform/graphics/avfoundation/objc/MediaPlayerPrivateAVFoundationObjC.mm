WebKit/WebKit

commit aa0613e134f3f3fedba88d64bebb5b895a5e7ded

Fix logging typo https://bugs.webkit.org/show_bug.cgi?id=139182
Reviewed by Alexey Proskuryakov.

No new tests, only logging code is changed.

* platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm:
(WebCore::MediaPlayerPrivateAVFoundationObjC::platformDuration): Don't use %f to log a string.


Canonical link: https://commits.webkit.org/157022@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@176673 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm
@@ -1191,7 +1191,7 @@ MediaTime MediaPlayerPrivateAVFoundationObjC::platformDuration() const
     if (CMTIME_IS_INDEFINITE(cmDuration))
         return MediaTime::positiveInfiniteTime();
 
-    LOG(Media, "MediaPlayerPrivateAVFoundationObjC::platformDuration(%p) - invalid duration, returning %.0f", this, toString(MediaTime::invalidTime()).utf8().data());
+    LOG(Media, "MediaPlayerPrivateAVFoundationObjC::platformDuration(%p) - invalid duration, returning %.s", this, toString(MediaTime::invalidTime()).utf8().data());
     return MediaTime::invalidTime();
 }
 

commit 1080b59cba33b203ea722fbb41e6e22b5933ed29

Unreviewed, fix typo introduced in r176673.
* platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm:
(WebCore::MediaPlayerPrivateAVFoundationObjC::platformDuration):


Canonical link: https://commits.webkit.org/157033@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@176685 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm
@@ -1191,7 +1191,7 @@ MediaTime MediaPlayerPrivateAVFoundationObjC::platformDuration() const
     if (CMTIME_IS_INDEFINITE(cmDuration))
         return MediaTime::positiveInfiniteTime();
 
-    LOG(Media, "MediaPlayerPrivateAVFoundationObjC::platformDuration(%p) - invalid duration, returning %.s", this, toString(MediaTime::invalidTime()).utf8().data());
+    LOG(Media, "MediaPlayerPrivateAVFoundationObjC::platformDuration(%p) - invalid duration, returning %s", this, toString(MediaTime::invalidTime()).utf8().data());
     return MediaTime::invalidTime();
 }
 