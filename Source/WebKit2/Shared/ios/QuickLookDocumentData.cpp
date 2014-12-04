383f59daea7f55a3f81a46ba21434884a62ac424
Commit Queue <commit-queue@webkit.org>
2014-12-04 01:31:37 +0000

    Unreviewed, rolling out r176457. https://bugs.webkit.org/show_bug.cgi?id=139247
    More iOS breakage (Requested by andersca on #webkit).
    
    Reverted changeset:
    
    "Fix iOS build."
    http://trac.webkit.org/changeset/176457
    
    Canonical link: https://commits.webkit.org/157096@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@176769 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/Shared/ios/QuickLookDocumentData.cpp
+++ Source/WebKit2/Shared/ios/QuickLookDocumentData.cpp
@@ -59,7 +59,7 @@ void QuickLookDocumentData::encode(IPC::ArgumentEncoder& encoder) const
     encoder << size;
 
     for (const auto& data : m_data)
-        encoder.encodeFixedLengthData(CFDataGetBytePtr(data.get()), CFDataGetLength(data.get()));
+        encoder.encodeFixedLengthData(CFDataGetBytePtr(data.get()), CFDataGetLength(data.get()), 1);
 }
 
 bool QuickLookDocumentData::decode(IPC::ArgumentDecoder& decoder, QuickLookDocumentData& documentData)
