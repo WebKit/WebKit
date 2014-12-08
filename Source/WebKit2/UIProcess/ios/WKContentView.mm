4549a992154cb2d087ae64e43ff6c1e754510dc3
Dan Bernstein <mitz@apple.com>
2014-12-08 21:53:54 +0000

    <rdar://problem/18046692> [iOS] Safari crashes at -[_NSUndoStack popAndInvoke] when trying to undo typing on closed tab https://bugs.webkit.org/show_bug.cgi?id=139408
    Reviewed by Anders Carlsson.
    
    * UIProcess/ios/WKContentView.mm:
    (-[WKContentView undoManager]): Override to initialize if necessary and return a unique undo
    manager for this instance. This ensures that undoable operations for this webview don’t get
    put in other undo managers, and matches UIWebView behavior.
    
    
    
    Canonical link: https://commits.webkit.org/157254@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@176969 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/ios/WKContentView.mm
+++ Source/WebKit2/UIProcess/ios/WKContentView.mm
@@ -178,6 +178,8 @@ private:
     RetainPtr<WKInspectorHighlightView> _inspectorHighlightView;
 
     HistoricalVelocityData _historicalKinematicData;
+
+    RetainPtr<NSUndoManager> _undoManager;
 }
 
 - (instancetype)initWithFrame:(CGRect)frame context:(WebKit::WebContext&)context configuration:(WebKit::WebPageConfiguration)webPageConfiguration webView:(WKWebView *)webView
@@ -373,6 +375,14 @@ private:
 - (void)didZoomToScale:(CGFloat)scale
 {
     [self _didEndScrollingOrZooming];
+}
+
+- (NSUndoManager *)undoManager
+{
+    if (!_undoManager)
+        _undoManager = adoptNS([[NSUndoManager alloc] init]);
+
+    return _undoManager.get();
 }
 
 #pragma mark Internal
