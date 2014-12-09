8b027ab700887462aa67233dfd3b34787f9f8683
Matt Hanson <matthew_hanson@apple.com>
2014-12-09 00:10:52 +0000

    Merged r176856. rdar://problems/19157955
    Canonical link: https://commits.webkit.org/152923.714@safari-600.3-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-600.3-branch@176988 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/mac/WebView/WebView.mm
+++ Source/WebKit/mac/WebView/WebView.mm
@@ -1061,9 +1061,11 @@ static void WebKitInitializeGamepadProviderIfNecessary()
         SecurityPolicy::setLocalLoadPolicy(SecurityPolicy::AllowLocalLoadsForLocalAndSubstituteData);
     }
 
-#if !PLATFORM(IOS)
+#if PLATFORM(MAC)
     if (!WebKitLinkedOnOrAfter(WEBKIT_FIRST_VERSION_WITHOUT_CONTENT_SNIFFING_FOR_FILE_URLS))
         ResourceHandle::forceContentSniffing();
+
+    _private->page->setDeviceScaleFactor([self _deviceScaleFactor]);
 #endif
 
 #if USE(GLIB)
@@ -1092,9 +1094,6 @@ static void WebKitInitializeGamepadProviderIfNecessary()
     _private = [[WebViewPrivate alloc] init];
     [self _commonInitializationWithFrameName:frameName groupName:groupName];
     [self setMaintainsBackForwardList: YES];
-#if !PLATFORM(IOS)
-    _private->page->setDeviceScaleFactor([self _deviceScaleFactor]);
-#endif
     return self;
 }
 
