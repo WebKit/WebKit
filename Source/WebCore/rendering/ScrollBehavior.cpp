e1ef9528dbe41414b83b7153f025d23f09c9c936
Myles C. Maxfield <mmaxfield@apple.com>
2014-12-10 01:02:29 +0000

    Scrolling to anchor tags does nothing in vertical-rl writing mode https://bugs.webkit.org/show_bug.cgi?id=137838
    Reviewed by David Hyatt.
    
    Source/WebCore:
    
    Scroll to a particular tag, and make sure the viewport is rendered as expected.
    
    Tests: fast/events/scroll-to-anchor-vertical-lr-writing-mode.html
           fast/events/scroll-to-anchor-vertical-writing-mode-contained-2.html
           fast/events/scroll-to-anchor-vertical-writing-mode-contained.html
           fast/events/scroll-to-anchor-vertical-writing-mode.html
    
    * page/FrameView.cpp:
    (WebCore::FrameView::scrollToAnchor):
    * rendering/ScrollBehavior.cpp:
    * rendering/ScrollBehavior.h:
    
    LayoutTests:
    
    When we specify the behavior regarding scrolling to anchor tags, we previously had
    only described behavior regarding horizontal text. When we have vertical text, we
    need to switch the horizontal and vertical scrolling policies.
    
    * fast/events/scroll-to-anchor-vertical-lr-writing-mode-expected.html: Added.
    * fast/events/scroll-to-anchor-vertical-lr-writing-mode.html: Added.
    * fast/events/scroll-to-anchor-vertical-writing-mode-contained-2-expected.html: Added.
    * fast/events/scroll-to-anchor-vertical-writing-mode-contained-2.html: Added.
    * fast/events/scroll-to-anchor-vertical-writing-mode-contained-expected.html: Added.
    * fast/events/scroll-to-anchor-vertical-writing-mode-contained.html: Added.
    * fast/events/scroll-to-anchor-vertical-writing-mode-expected.html: Added.
    * fast/events/scroll-to-anchor-vertical-writing-mode.html: Added.
    
    
    Canonical link: https://commits.webkit.org/157324@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@177050 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/ScrollBehavior.cpp
+++ Source/WebCore/rendering/ScrollBehavior.cpp
@@ -50,6 +50,8 @@ const ScrollAlignment ScrollAlignment::alignCenterIfNeeded = { noScroll, alignCe
 WEBCORE_EXPORT const ScrollAlignment ScrollAlignment::alignToEdgeIfNeeded = { noScroll, alignToClosestEdge, alignToClosestEdge };
 WEBCORE_EXPORT const ScrollAlignment ScrollAlignment::alignCenterAlways = { alignCenter, alignCenter, alignCenter };
 const ScrollAlignment ScrollAlignment::alignTopAlways = { alignTop, alignTop, alignTop };
+const ScrollAlignment ScrollAlignment::alignRightAlways = { alignRight, alignRight, alignRight };
+const ScrollAlignment ScrollAlignment::alignLeftAlways = { alignLeft, alignLeft, alignLeft };
 const ScrollAlignment ScrollAlignment::alignBottomAlways = { alignBottom, alignBottom, alignBottom };
 
 }; // namespace WebCore
