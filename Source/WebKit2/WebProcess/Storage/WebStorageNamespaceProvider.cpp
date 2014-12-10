be75b83f2622eef02455be5f342df68a3bc3ab23
Anders Carlsson <andersca@apple.com>
2014-12-10 19:25:42 +0000

    Add a stubbed out WebStorageNamespaceProvider class to WebKit2 https://bugs.webkit.org/show_bug.cgi?id=139497
    Reviewed by Beth Dakin.
    
    * WebKit2.xcodeproj/project.pbxproj:
    * WebProcess/Storage/WebStorageNamespaceProvider.cpp: Added.
    (WebKit::storageNamespaceProviders):
    (WebKit::WebStorageNamespaceProvider::getOrCreate):
    (WebKit::WebStorageNamespaceProvider::WebStorageNamespaceProvider):
    (WebKit::WebStorageNamespaceProvider::~WebStorageNamespaceProvider):
    (WebKit::WebStorageNamespaceProvider::createLocalStorageNamespace):
    (WebKit::WebStorageNamespaceProvider::createTransientLocalStorageNamespace):
    * WebProcess/Storage/WebStorageNamespaceProvider.h: Added.
    
    Canonical link: https://commits.webkit.org/157342@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@177082 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit2/WebProcess/Storage/WebStorageNamespaceProvider.cpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2014 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "WebStorageNamespaceProvider.h"
+
+#include <WebCore/StorageNamespace.h>
+#include <wtf/HashMap.h>
+#include <wtf/NeverDestroyed.h>
+
+using namespace WebCore;
+
+namespace WebKit {
+
+static HashMap<uint64_t, WebStorageNamespaceProvider*>& storageNamespaceProviders()
+{
+    static NeverDestroyed<HashMap<uint64_t, WebStorageNamespaceProvider*>> storageNamespaceProviders;
+
+    return storageNamespaceProviders;
+}
+
+RefPtr<WebStorageNamespaceProvider> WebStorageNamespaceProvider::getOrCreate(uint64_t identifier)
+{
+    auto& slot = storageNamespaceProviders().add(identifier, nullptr).iterator->value;
+    if (slot)
+        return slot;
+
+    RefPtr<WebStorageNamespaceProvider> storageNamespaceProvider = adoptRef(new WebStorageNamespaceProvider(identifier));
+    slot = storageNamespaceProvider.get();
+
+    return storageNamespaceProvider;
+}
+
+WebStorageNamespaceProvider::WebStorageNamespaceProvider(uint64_t identifier)
+    : m_identifier(identifier)
+{
+}
+
+WebStorageNamespaceProvider::~WebStorageNamespaceProvider()
+{
+    ASSERT(storageNamespaceProviders().contains(m_identifier));
+
+    storageNamespaceProviders().remove(m_identifier);
+}
+
+RefPtr<WebCore::StorageNamespace> WebStorageNamespaceProvider::createLocalStorageNamespace(unsigned quota)
+{
+    // FIXME: Implement.
+    return nullptr;
+}
+
+RefPtr<WebCore::StorageNamespace> WebStorageNamespaceProvider::createTransientLocalStorageNamespace(WebCore::SecurityOrigin&, unsigned quota)
+{
+    // FIXME: Implement.
+    return nullptr;
+}
+
+}
