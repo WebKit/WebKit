4e642ada2bac251c0d0d4ecb93e738f5418b4f59
Babak Shafiei <bshafiei@apple.com>
2014-12-10 17:06:52 +0000

    Merged r175345.  rdar://problem/19132020
    Canonical link: https://commits.webkit.org/152923.273@safari-600.1.4.13-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-600.1.4.13-branch@177069 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderBlockFlow.cpp
+++ Source/WebCore/rendering/RenderBlockFlow.cpp
@@ -2247,6 +2247,7 @@ void RenderBlockFlow::removeFloatingObject(RenderBox& floatBox)
                     logicalBottom = std::max(logicalBottom, logicalTop + 1);
                 }
                 if (floatingObject->originatingLine()) {
+                    floatingObject->originatingLine()->removeFloat(floatBox);
                     if (!selfNeedsLayout()) {
                         ASSERT(&floatingObject->originatingLine()->renderer() == this);
                         floatingObject->originatingLine()->markDirty();
@@ -2652,10 +2653,7 @@ void RenderBlockFlow::markAllDescendantsWithFloatsForLayout(RenderBox* floatToRe
     if (floatToRemove)
         removeFloatingObject(*floatToRemove);
 
-    if (childrenInline())
-        return;
-
-    // Iterate over our children and mark them as needed.
+    // Iterate over our block children and mark them as needed.
     for (auto& block : childrenOfType<RenderBlock>(*this)) {
         if (!floatToRemove && block.isFloatingOrOutOfFlowPositioned())
             continue;
