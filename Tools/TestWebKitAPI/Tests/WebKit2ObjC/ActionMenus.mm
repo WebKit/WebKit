05f09bfb18e42986172f1d62edc898cc453b1a91
Tim Horton <timothy_horton@apple.com>
2014-12-15 19:46:34 +0000

    Implement Data Detectors immediate actions for WebKit2 https://bugs.webkit.org/show_bug.cgi?id=139617 <rdar://problem/19198539>
    Reviewed by Beth Dakin.
    
    * TestWebKitAPI/Tests/WebKit2ObjC/ActionMenus.mm:
    (TestWebKitAPI::TEST):
    Temporarily disable the action menu tests while the
    underlying code is in flux.
    
    
    Canonical link: https://commits.webkit.org/157533@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@177299 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/WebKit2ObjC/ActionMenus.mm
+++ Tools/TestWebKitAPI/Tests/WebKit2ObjC/ActionMenus.mm
@@ -436,7 +436,8 @@ static NSPoint windowPointForTarget(TargetType target)
 
 // FIXME: Ideally, each of these would be able to run as its own subtest in a suite, sharing a WKView (for performance reasons),
 // but we cannot because run-api-tests explicitly runs each test in a separate process. So, we use a single test for many tests instead.
-TEST(WebKit2, ActionMenusTest)
+// FIXME: Temporarily disabled.
+TEST(WebKit2, DISABLED_ActionMenusTest)
 {
     WKRetainPtr<WKContextRef> context = adoptWK(Util::createContextForInjectedBundleTest("ActionMenusTest"));
 
