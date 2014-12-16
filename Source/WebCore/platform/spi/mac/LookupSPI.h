c3fedc1ac9a28458ad2fa11ea3554bfcd5899298
Beth Dakin <bdakin@apple.com>
2014-12-16 01:19:28 +0000

    Speculative build fix.
    * platform/spi/mac/LookupSPI.h:
    
    
    
    Canonical link: https://commits.webkit.org/157555@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@177325 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/spi/mac/LookupSPI.h
+++ Source/WebCore/platform/spi/mac/LookupSPI.h
@@ -23,6 +23,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
+#import "NSImmediateActionGestureRecognizerSPI.h"
 #import "SoftLinking.h"
 #import <objc/runtime.h>
 
