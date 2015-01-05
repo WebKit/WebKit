b4f6197935c55cb5572801cc49e9385928e91793
Alexey Proskuryakov <ap@webkit.org>
2015-01-05 09:07:19 +0000

    Merge r169726 - Properly reset UStringSearch object in SearchBuffer constructor. https://bugs.webkit.org/show_bug.cgi?id=133662 <rdar://problem/17240136>
    Reviewed by Tim Horton.
    
    * editing/TextIterator.cpp: (WebCore::SearchBuffer::~SearchBuffer):

--- Source/WebCore/editing/TextIterator.cpp
+++ Source/WebCore/editing/TextIterator.cpp
@@ -2058,6 +2058,8 @@ inline SearchBuffer::~SearchBuffer()
     UErrorCode status = U_ZERO_ERROR;
     usearch_setPattern(WebCore::searcher(), &newlineCharacter, 1, &status);
     ASSERT(status == U_ZERO_ERROR);
+    usearch_setText(WebCore::searcher(), &newlineCharacter, 1, &status);
+    ASSERT(status == U_ZERO_ERROR);
 
     unlockSearcher();
 }
