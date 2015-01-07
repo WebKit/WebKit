b220e9c3ceaec1c6442eca508157fe8602dffee9
Bem Jones-Bey <bjonesbe@adobe.com>
2015-01-07 02:22:12 +0000

    [CSS Masking][CSS Shapes] Large corner radii use with inset() clip-path are not properly constrained https://bugs.webkit.org/show_bug.cgi?id=140127
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    Constrain large corner radii for inset() clip-path in the same way
    that border-radii are constrained.
    
    Test: fast/masking/clip-path-inset-large-radii.html
    
    * rendering/style/BasicShapes.cpp:
    (WebCore::BasicShapeInset::path): Add radii constraint. Also reformat
        code a little to shut the style bot up.
    
    LayoutTests:
    
    * fast/masking/clip-path-inset-large-radii-expected.html: Added.
    * fast/masking/clip-path-inset-large-radii.html: Added.
    
    
    Canonical link: https://commits.webkit.org/158132@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@178015 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/style/BasicShapes.cpp
+++ Source/WebCore/rendering/style/BasicShapes.cpp
@@ -231,19 +231,15 @@ void BasicShapeInset::path(Path& path, const FloatRect& boundingBox)
     ASSERT(path.isEmpty());
     float left = floatValueForLength(m_left, boundingBox.width());
     float top = floatValueForLength(m_top, boundingBox.height());
-    FloatRoundedRect r = FloatRoundedRect(
-        FloatRect(
-            left + boundingBox.x(),
-            top + boundingBox.y(),
-            std::max<float>(boundingBox.width() - left - floatValueForLength(m_right, boundingBox.width()), 0),
-            std::max<float>(boundingBox.height() - top - floatValueForLength(m_bottom, boundingBox.height()), 0)
-        ),
-        floatSizeForLengthSize(m_topLeftRadius, boundingBox),
+    auto rect = FloatRect(left + boundingBox.x(), top + boundingBox.y(),
+        std::max<float>(boundingBox.width() - left - floatValueForLength(m_right, boundingBox.width()), 0),
+        std::max<float>(boundingBox.height() - top - floatValueForLength(m_bottom, boundingBox.height()), 0));
+    auto radii = FloatRoundedRect::Radii(floatSizeForLengthSize(m_topLeftRadius, boundingBox),
         floatSizeForLengthSize(m_topRightRadius, boundingBox),
         floatSizeForLengthSize(m_bottomLeftRadius, boundingBox),
-        floatSizeForLengthSize(m_bottomRightRadius, boundingBox)
-    );
-    path.addRoundedRect(r);
+        floatSizeForLengthSize(m_bottomRightRadius, boundingBox));
+    radii.scale(calcBorderRadiiConstraintScaleFor(rect, radii));
+    path.addRoundedRect(FloatRoundedRect(rect, radii));
 }
 
 Ref<BasicShape> BasicShapeInset::blend(const BasicShape& other, double progress) const
