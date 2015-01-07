d470ad5f678329e997a96a8ee887088af63b3461
Anders Carlsson <andersca@apple.com>
2015-01-07 20:40:02 +0000

    Give simple HTML documents a database provider https://bugs.webkit.org/show_bug.cgi?id=140192 rdar://problem/19398825
    Reviewed by Tim Horton.
    
    * WebView/WebView.mm:
    (-[WebView initSimpleHTMLDocumentWithStyle:frame:preferences:groupName:]):
    
    Canonical link: https://commits.webkit.org/158160@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@178046 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/mac/WebView/WebView.mm
+++ Source/WebKit/mac/WebView/WebView.mm
@@ -1204,6 +1204,7 @@ static void WebKitInitializeGamepadProviderIfNecessary()
     pageConfiguration.inspectorClient = new WebInspectorClient(self);
     pageConfiguration.loaderClientForMainFrame = new WebFrameLoaderClient;
     pageConfiguration.progressTrackerClient = new WebProgressTrackerClient(self);
+    pageConfiguration.databaseProvider = &WebDatabaseProvider::shared();
     pageConfiguration.storageNamespaceProvider = &_private->group->storageNamespaceProvider();
     pageConfiguration.userContentController = &_private->group->userContentController();
     pageConfiguration.visitedLinkStore = &_private->group->visitedLinkStore();
