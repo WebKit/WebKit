89ee938559f04d437b32a7a676e18557e315d546
Ting-Wei Lan <lantw44@gmail.com>
2015-01-13 09:54:50 +0000

    Merge r177016 - [GTK] LDFLAGS is ignored when running g-ir-scanner https://bugs.webkit.org/show_bug.cgi?id=138832
    Patch by Ting-Wei Lan <lantw44@gmail.com> on 2014-12-09
    Reviewed by Carlos Garcia Campos.
    
    GTK+ requires libintl on FreeBSD because there is no gettext
    implementation in FreeBSD libc. In order to link to libintl, which is
    not located in the default search path of the linker, we need to add
    the required path via the linker command line. However, LDFLAGS is
    ignored when running g-ir-scanner starting from r167873, so we have
    to manually pick required flags from CMAKE_SHARED_LINKER_FLAGS and
    put them in correct order to prevent g-ir-scanner from linking to
    the installed version of libraries.
    
    * PlatformGTK.cmake:

--- Source/WebKit2/PlatformGTK.cmake
+++ Source/WebKit2/PlatformGTK.cmake
@@ -742,6 +742,10 @@ else ()
     )
 endif ()
 
+# Add required -L flags from ${CMAKE_SHARED_LINKER_FLAGS} for g-ir-scanner
+string(REGEX MATCHALL "-L[^ ]*"
+    INTROSPECTION_ADDITIONAL_LINKER_FLAGS ${CMAKE_SHARED_LINKER_FLAGS})
+
 add_custom_command(
     OUTPUT ${CMAKE_BINARY_DIR}/WebKit2-${WEBKITGTK_API_VERSION}.gir
     DEPENDS WebKit2
@@ -762,6 +766,7 @@ add_custom_command(
         --library=webkit2gtk-${WEBKITGTK_API_VERSION}
         --library=javascriptcoregtk-${WEBKITGTK_API_VERSION}
         -L${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
+        ${INTROSPECTION_ADDITIONAL_LINKER_FLAGS}
         --no-libtool
         --pkg=gobject-2.0
         --pkg=gtk+-3.0
@@ -801,6 +806,7 @@ add_custom_command(
         --library=webkit2gtk-${WEBKITGTK_API_VERSION}
         --library=javascriptcoregtk-${WEBKITGTK_API_VERSION}
         -L${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
+        ${INTROSPECTION_ADDITIONAL_LINKER_FLAGS}
         --no-libtool
         --pkg=gobject-2.0
         --pkg=gtk+-3.0
