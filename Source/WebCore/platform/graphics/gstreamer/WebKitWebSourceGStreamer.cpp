1f660684248c5f9c3266c535de30ccedf8ba6a00
Xavier Claessens <xavier.claessens@collabora.com>
2015-01-13 11:40:38 +0000

    Merge r177279 - [GStreamer] the webkitwebsrc element can stale https://bugs.webkit.org/show_bug.cgi?id=138425
    Patch by Xavier Claessens <xavier.claessens@collabora.com> on 2014-12-15
    Reviewed by Philippe Normand.
    
    * platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp:
    (removeTimeoutSources):
    (webKitWebSrcStart):
    (webKitWebSrcChangeState):

--- Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
@@ -139,6 +139,7 @@ struct _WebKitWebSrcPrivate {
 
     guint64 requestedOffset;
 
+    gboolean pendingStart;
     GThreadSafeMainLoopSource startSource;
     GThreadSafeMainLoopSource stopSource;
     GThreadSafeMainLoopSource needDataSource;
@@ -379,7 +380,8 @@ static void removeTimeoutSources(WebKitWebSrc* src)
 {
     WebKitWebSrcPrivate* priv = src->priv;
 
-    priv->startSource.cancel();
+    if (!priv->pendingStart)
+        priv->startSource.cancel();
     priv->needDataSource.cancel();
     priv->enoughDataSource.cancel();
     priv->seekSource.cancel();
@@ -449,6 +451,7 @@ static void webKitWebSrcStart(WebKitWebSrc* src)
 
     GMutexLocker<GMutex> locker(*GST_OBJECT_GET_LOCK(src));
 
+    priv->pendingStart = FALSE;
     priv->corsAccessCheck = CORSNoCheck;
 
     if (!priv->uri) {
@@ -543,12 +546,14 @@ static GstStateChangeReturn webKitWebSrcChangeState(GstElement* element, GstStat
     switch (transition) {
     case GST_STATE_CHANGE_READY_TO_PAUSED:
         GST_DEBUG_OBJECT(src, "READY->PAUSED");
+        priv->pendingStart = TRUE;
         gst_object_ref(src);
         priv->startSource.schedule("[WebKit] webKitWebSrcStart", std::function<void()>(std::bind(webKitWebSrcStart, src)), G_PRIORITY_DEFAULT,
             [src] { gst_object_unref(src); });
         break;
     case GST_STATE_CHANGE_PAUSED_TO_READY:
         GST_DEBUG_OBJECT(src, "PAUSED->READY");
+        priv->pendingStart = FALSE;
         // cancel pending sources
         removeTimeoutSources(src);
         gst_object_ref(src);
