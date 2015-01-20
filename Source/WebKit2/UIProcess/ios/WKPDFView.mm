a9febe71a00b73a4703e367fad26e4162e0235c8
Babak Shafiei <bshafiei@apple.com>
2015-01-20 20:37:35 +0000

    Merged r171775.  rdar://problem/19419960
    Canonical link: https://commits.webkit.org/152923.302@safari-600.1.4.15-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-600.1.4.15-branch@178734 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/ios/WKPDFView.mm
+++ Source/WebKit2/UIProcess/ios/WKPDFView.mm
@@ -239,7 +239,7 @@ typedef struct {
         if (!page)
             continue;
 
-        CGSize pageSize = [page size];
+        CGSize pageSize = [page cropBoxAccountForRotation].size;
         pageFrame.size.height = pageSize.height / pageSize.width * pageFrame.size.width;
         CGRect pageFrameWithMarginApplied = CGRectInset(pageFrame, pdfPageMargin, pdfPageMargin);
 
