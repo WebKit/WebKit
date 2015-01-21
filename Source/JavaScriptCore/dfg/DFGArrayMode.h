300645a1e28a59659b155ccccfd5d685e606851e
Babak Shafiei <bshafiei@apple.com>
2015-01-21 18:43:22 +0000

    Merged r176972.  rdar://problem/19450085
    Canonical link: https://commits.webkit.org/152923.358@safari-600.1.4.15-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-600.1.4.15-branch@178843 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGArrayMode.h
+++ Source/JavaScriptCore/dfg/DFGArrayMode.h
@@ -406,7 +406,7 @@ public:
         case Array::ArrayStorage:
             return arrayModesWithIndexingShape(ArrayStorageShape);
         case Array::SlowPutArrayStorage:
-            return arrayModesWithIndexingShape(SlowPutArrayStorageShape);
+            return arrayModesWithIndexingShapes(SlowPutArrayStorageShape, ArrayStorageShape);
         default:
             return asArrayModes(NonArray);
         }
@@ -462,6 +462,13 @@ private:
         }
     }
     
+    ArrayModes arrayModesWithIndexingShapes(IndexingType shape1, IndexingType shape2) const
+    {
+        ArrayModes arrayMode1 = arrayModesWithIndexingShape(shape1);
+        ArrayModes arrayMode2 = arrayModesWithIndexingShape(shape2);
+        return arrayMode1 | arrayMode2;
+    }
+
     bool alreadyChecked(Graph&, Node*, AbstractValue&, IndexingType shape) const;
     
     union {
