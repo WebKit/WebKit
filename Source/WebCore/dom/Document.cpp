8acb80ebfcc8153122b82687dd39181e9732ddef
Matt Hanson <matthew_hanson@apple.com>
2015-01-22 22:16:28 +0000

    Merge r177360. rdar://problem/19451319
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-600.5-branch@178944 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/Document.cpp
+++ Source/WebCore/dom/Document.cpp
@@ -1772,6 +1772,10 @@ void Document::recalcStyle(Style::Change change)
 
     InspectorInstrumentation::didRecalculateStyle(cookie);
 
+    // Some animated images may now be inside the viewport due to style recalc,
+    // resume them if necessary.
+    frameView.resumeVisibleImageAnimationsIncludingSubframes();
+
     // As a result of the style recalculation, the currently hovered element might have been
     // detached (for example, by setting display:none in the :hover style), schedule another mouseMove event
     // to check if any other elements ended up under the mouse pointer due to re-layout.
