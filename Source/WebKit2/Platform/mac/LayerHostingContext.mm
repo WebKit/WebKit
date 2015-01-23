c842b944052f5b622f05812d22999b028b8a79b0
David Kilzer <ddkilzer@webkit.org>
2015-01-23 10:04:37 +0000

    Merged r173762.  rdar://problem/19420088
    Canonical link: https://commits.webkit.org/152923.366@safari-600.1.4.15-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-600.1.4.15-branch@178995 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/Platform/mac/LayerHostingContext.mm
+++ Source/WebKit2/Platform/mac/LayerHostingContext.mm
@@ -116,9 +116,11 @@ CGColorSpaceRef LayerHostingContext::colorSpace() const
     return [m_context colorSpace];
 }
 
+#if PLATFORM(IOS)
 void LayerHostingContext::setFencePort(mach_port_t fencePort)
 {
     [m_context setFencePort:fencePort];
 }
+#endif
 
 } // namespace WebKit
