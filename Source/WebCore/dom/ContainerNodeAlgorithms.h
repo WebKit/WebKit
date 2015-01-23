397d0f868ece4df08dc43ba9eb7143b19a84a06a
Grzegorz Czajkowski <g.czajkowski@samsung.com>
2015-01-23 08:49:39 +0000

    Rename ChildNodeRemovalNotifier::m_insertionPoint to m_removalPoint https://bugs.webkit.org/show_bug.cgi?id=140766
    Reviewed by Andreas Kling.
    
    ChildNodeRemovalNotifier::ChildNodeRemovalNotifier(...) should take
    a node as removal point.
    It's probably Copy/Paste from ChildNodeInsertionNotifier::m_insertionPoint.
    
    No new tests. No behavior change.
    
    * dom/ContainerNodeAlgorithms.h:
    (WebCore::ChildNodeRemovalNotifier::ChildNodeRemovalNotifier):
    (WebCore::ChildNodeRemovalNotifier::notifyNodeRemovedFromDocument):
    (WebCore::ChildNodeRemovalNotifier::notifyNodeRemovedFromTree):
    
    
    Canonical link: https://commits.webkit.org/158802@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@178988 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/ContainerNodeAlgorithms.h
+++ Source/WebCore/dom/ContainerNodeAlgorithms.h
@@ -55,8 +55,8 @@ private:
 
 class ChildNodeRemovalNotifier {
 public:
-    explicit ChildNodeRemovalNotifier(ContainerNode& insertionPoint)
-        : m_insertionPoint(insertionPoint)
+    explicit ChildNodeRemovalNotifier(ContainerNode& removalPoint)
+        : m_removalPoint(removalPoint)
     {
     }
 
@@ -68,7 +68,7 @@ private:
     void notifyNodeRemovedFromDocument(Node&);
     void notifyNodeRemovedFromTree(ContainerNode&);
 
-    ContainerNode& m_insertionPoint;
+    ContainerNode& m_removalPoint;
 };
 
 namespace Private {
@@ -234,8 +234,8 @@ inline void ChildNodeInsertionNotifier::notify(Node& node)
 
 inline void ChildNodeRemovalNotifier::notifyNodeRemovedFromDocument(Node& node)
 {
-    ASSERT(m_insertionPoint.inDocument());
-    node.removedFrom(m_insertionPoint);
+    ASSERT(m_removalPoint.inDocument());
+    node.removedFrom(m_removalPoint);
 
     if (is<ContainerNode>(node))
         notifyDescendantRemovedFromDocument(downcast<ContainerNode>(node));
@@ -244,9 +244,9 @@ inline void ChildNodeRemovalNotifier::notifyNodeRemovedFromDocument(Node& node)
 inline void ChildNodeRemovalNotifier::notifyNodeRemovedFromTree(ContainerNode& node)
 {
     NoEventDispatchAssertion assertNoEventDispatch;
-    ASSERT(!m_insertionPoint.inDocument());
+    ASSERT(!m_removalPoint.inDocument());
 
-    node.removedFrom(m_insertionPoint);
+    node.removedFrom(m_removalPoint);
     notifyDescendantRemovedFromTree(node);
 }
 
