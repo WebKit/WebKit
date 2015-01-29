20b06c94c57df91468ba89e67c0edb9a47557415
Santosh Mahto <santoshbit2007@gmail.com>
2015-01-29 19:49:21 +0000

    Refactor String::format to handle unreached va_end. https://bugs.webkit.org/show_bug.cgi?id=140938
    Reviewed by Alexey Proskuryakov.
    
    Now va_end is called for all platform after getting length
    of formatted string and again va_start/va_end is called to write
    formatted string in buffer. This way it ensure va_end is always
    reached for each va_start.
    
    * wtf/text/WTFString.cpp:
    
    
    Canonical link: https://commits.webkit.org/159037@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@179354 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/text/WTFString.cpp
+++ Source/WTF/wtf/text/WTFString.cpp
@@ -455,31 +455,25 @@ String String::format(const char *format, ...)
     va_list args;
     va_start(args, format);
 
-    Vector<char, 256> buffer;
-
     // Do the format once to get the length.
 #if COMPILER(MSVC)
     int result = _vscprintf(format, args);
 #else
     char ch;
     int result = vsnprintf(&ch, 1, format, args);
-    // We need to call va_end() and then va_start() again here, as the
-    // contents of args is undefined after the call to vsnprintf
-    // according to http://man.cx/snprintf(3)
-    //
-    // Not calling va_end/va_start here happens to work on lots of
-    // systems, but fails e.g. on 64bit Linux.
-    va_end(args);
-    va_start(args, format);
 #endif
+    va_end(args);
 
     if (result == 0)
         return String("");
     if (result < 0)
         return String();
+
+    Vector<char, 256> buffer;
     unsigned len = result;
     buffer.grow(len + 1);
     
+    va_start(args, format);
     // Now do the formatting again, guaranteed to fit.
     vsnprintf(buffer.data(), buffer.size(), format, args);
 
