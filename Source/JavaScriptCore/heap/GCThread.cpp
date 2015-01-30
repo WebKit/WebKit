33591076daf0c4ac46295648a60eded5d0aa470f
Geoffrey Garen <ggaren@apple.com>
2015-01-30 22:22:57 +0000

    GC marking threads should clear malloc caches https://bugs.webkit.org/show_bug.cgi?id=141097
    Reviewed by Andreas Kling.
    
    This is an attempt to ameliorate a potential memory use regression
    caused by https://bugs.webkit.org/show_bug.cgi?id=140900
    Use FastMalloc (bmalloc) instead of BlockAllocator for GC pages.
    
    FastMalloc may accumulate a per-thread cache on each of the 8-ish
    GC marking threads, which can be expensive.
    
    * heap/GCThread.cpp:
    (JSC::GCThread::waitForNextPhase): Scavenge the current thread before
    going to sleep. There's probably not too much value to keeping our
    per-thread cache between GCs, and it has some memory footprint.
    
    
    Canonical link: https://commits.webkit.org/159089@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@179411 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/heap/GCThread.cpp
+++ Source/JavaScriptCore/heap/GCThread.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Apple Inc. All rights reserved.
+ * Copyright (C) 2012, 2015 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -75,6 +75,8 @@ GCPhase GCThread::waitForNextPhase()
     m_shared.m_numberOfActiveGCThreads--;
     if (!m_shared.m_numberOfActiveGCThreads)
         m_shared.m_activityConditionVariable.notify_one();
+
+    WTF::releaseFastMallocFreeMemoryForThisThread();
 
     m_shared.m_phaseConditionVariable.wait(lock, [this] { return m_shared.m_currentPhase != NoPhase; });
     m_shared.m_numberOfActiveGCThreads++;
