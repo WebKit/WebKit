8e42b70d41a219633f1eaa448ebc903b555d87ce
Youenn Fablet <youennf@gmail.com>
2015-02-04 22:02:15 +0000

    W3C test importer should sort the list of files when generating the w3c-import.log https://bugs.webkit.org/show_bug.cgi?id=141156
    Reviewed by Bem Jones-Bey.
    
    LayoutTests/imported/w3c:
    
    Updated web-platform-tests logs according importer file sorting.
    
    * web-platform-tests/common/w3c-import.log:
    * web-platform-tests/fonts/w3c-import.log:
    * web-platform-tests/images/w3c-import.log:
    * web-platform-tests/tools/runner/css/w3c-import.log:
    * web-platform-tests/tools/runner/fonts/w3c-import.log:
    * web-platform-tests/tools/runner/w3c-import.log:
    * web-platform-tests/tools/scripts/w3c-import.log:
    * web-platform-tests/tools/sslutils/w3c-import.log:
    * web-platform-tests/tools/w3c-import.log:
    * web-platform-tests/tools/webdriver/webdriver/w3c-import.log:
    
    Tools:
    
    Sort explicitly the list of imported files written in the w3c-import.log.
    Removed logging of the import date.
    
    * Scripts/webkitpy/w3c/test_importer.py:
    (TestImporter.write_import_log):
    
    
    Canonical link: https://commits.webkit.org/159274@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@179628 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/w3c/test_importer.py
+++ Tools/Scripts/webkitpy/w3c/test_importer.py
@@ -385,8 +385,6 @@ class TestImporter(object):
     def write_import_log(self, import_directory, file_list, prop_list, property_values_list):
         """ Writes a w3c-import.log file in each directory with imported files. """
 
-        now = datetime.datetime.now()
-
         import_log = open(os.path.join(import_directory, 'w3c-import.log'), 'w')
         import_log.write('The tests in this directory were imported from the W3C repository.\n')
         import_log.write('Do NOT modify these tests directly in Webkit.\n')
@@ -396,8 +394,6 @@ class TestImporter(object):
         import_log.write('\thttps://github.com/w3c/csswg-test\n\n')
         import_log.write('Then run the Tools/Scripts/import-w3c-tests in Webkit to reimport\n\n')
         import_log.write('Do NOT modify or remove this file\n\n')
-        import_log.write('------------------------------------------------------------------------\n')
-        import_log.write('Last Import: ' + now.strftime('%Y-%m-%d %H:%M') + '\n')
         import_log.write('------------------------------------------------------------------------\n')
         import_log.write('Properties requiring vendor prefixes:\n')
         if prop_list:
@@ -413,7 +409,7 @@ class TestImporter(object):
             import_log.write('None\n')
         import_log.write('------------------------------------------------------------------------\n')
         import_log.write('List of files:\n')
-        for item in file_list:
+        for item in sorted(file_list):
             import_log.write(item + '\n')
 
         import_log.close()
