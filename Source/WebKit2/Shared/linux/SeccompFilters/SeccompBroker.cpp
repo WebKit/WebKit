70958dbe02e4a14a6bf607ec7387bc32c4b573ec
Michael Catanzaro <mcatanzaro@igalia.com>
2015-02-04 17:49:05 +0000

    Build with seccomp filters broken since r179409 https://bugs.webkit.org/show_bug.cgi?id=141206
    Patch by Michael Catanzaro <mcatanzaro@igalia.com> on 2015-02-04
    Reviewed by Darin Adler.
    
    Fix build by renaming SeccompBrokerClient::shared to SeccompBrokerClient::singleton.
    
    * Shared/linux/SeccompFilters/SeccompBroker.cpp:
    (WebKit::SeccompBrokerClient::singleton):
    (WebKit::SeccompBroker::initialize):
    (WebKit::SeccompBrokerClient::shared):
    (WebKit::SeccompBroker::runLoop):
    
    Canonical link: https://commits.webkit.org/159256@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@179605 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/Shared/linux/SeccompFilters/SeccompBroker.cpp
+++ Source/WebKit2/Shared/linux/SeccompFilters/SeccompBroker.cpp
@@ -52,7 +52,7 @@ namespace WebKit {
 
 class SeccompBrokerClient {
 public:
-    static SeccompBrokerClient& shared(int socket = -1);
+    static SeccompBrokerClient& singleton(int socket = -1);
     ~SeccompBrokerClient();
 
     void dispatch(Syscall*) const;
@@ -174,7 +174,7 @@ static void registerSIGSYSHandler()
         CRASH();
 }
 
-SeccompBrokerClient& SeccompBrokerClient::shared(int socket)
+SeccompBrokerClient& SeccompBrokerClient::singleton(int socket)
 {
     DEPRECATED_DEFINE_STATIC_LOCAL(SeccompBrokerClient, brokerClient, (socket));
 
@@ -290,7 +290,7 @@ void SeccompBroker::initialize()
     pid_t pid = fork();
     if (pid) { // Sandboxed process.
         close(sockets[1]);
-        SeccompBrokerClient::shared(sockets[0]);
+        SeccompBrokerClient::singleton(sockets[0]);
         registerSIGSYSHandler();
     } else { // Broker.
         // TODO: The broker should setup seccomp filters
