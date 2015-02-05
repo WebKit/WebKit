ca01a1b3ae368a0ff3006f1ae304968afa317b13
Matt Hanson <matthew_hanson@apple.com>
2015-02-05 02:36:22 +0000

    Merge r178490. rdar://problem/19687156
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-600.5-branch@179658 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderBox.cpp
+++ Source/WebCore/rendering/RenderBox.cpp
@@ -1397,6 +1397,8 @@ static bool isCandidateForOpaquenessTest(const RenderBox& childBox)
             return false;
         if (childLayer->hasTransform() || childLayer->isTransparent() || childLayer->hasFilter())
             return false;
+        if (!childBox.scrolledContentOffset().isZero())
+            return false;
     }
     return true;
 }
