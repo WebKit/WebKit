429d86ffafed94a478e5ce1149cfdc43de63a88f
Commit Queue <commit-queue@webkit.org>
2015-02-11 15:34:13 +0000

    Unreviewed, rolling out r179921. https://bugs.webkit.org/show_bug.cgi?id=141473
    caused about 30 test failures on yosemite and mavericks
    (Requested by alexchristensen on #webkit).
    
    Reverted changeset:
    
    "Div having contentEditable and display:flex cannot be edited
    if it is empty."
    https://bugs.webkit.org/show_bug.cgi?id=141218
    http://trac.webkit.org/changeset/179921
    
    Canonical link: https://commits.webkit.org/159505@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@179926 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/Position.cpp
+++ Source/WebCore/dom/Position.cpp
@@ -35,7 +35,6 @@
 #include "Logging.h"
 #include "PositionIterator.h"
 #include "RenderBlock.h"
-#include "RenderFlexibleBox.h"
 #include "RenderInline.h"
 #include "RenderIterator.h"
 #include "RenderLineBreak.h"
@@ -935,8 +934,8 @@ bool Position::isCandidate() const
 
     if (m_anchorNode->hasTagName(htmlTag))
         return false;
-
-    if (is<RenderBlockFlow>(*renderer) || is<RenderFlexibleBox>(*renderer)) {
+        
+    if (is<RenderBlockFlow>(*renderer)) {
         RenderBlockFlow& block = downcast<RenderBlockFlow>(*renderer);
         if (block.logicalHeight() || m_anchorNode->hasTagName(bodyTag)) {
             if (!Position::hasRenderedNonAnonymousDescendantsWithHeight(block))
