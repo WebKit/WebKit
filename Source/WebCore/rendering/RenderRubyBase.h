60590415fb3e8acadc34a5b2bda134b83f0342c4
Lucas Forschler <lforschler@apple.com>
2015-02-12 01:46:51 +0000

    Merged r177377.  rdar://problem/19526158
    Canonical link: https://commits.webkit.org/152923.990@safari-600.5-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-600.5-branch@179976 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderRubyBase.h
+++ Source/WebCore/rendering/RenderRubyBase.h
@@ -50,6 +50,17 @@ public:
 
     virtual void cachePriorCharactersIfNeeded(const LazyLineBreakIterator&) override;
 
+    void setIsAfterExpansion(bool isAfterExpansion) { m_isAfterExpansion = isAfterExpansion; }
+    bool isAfterExpansion() { return m_isAfterExpansion; }
+
+    void setInitialOffset(float initialOffset) { m_initialOffset = initialOffset; }
+
+    void reset()
+    {
+        m_initialOffset = 0;
+        m_isAfterExpansion = true;
+    }
+
 private:
     virtual bool isChildAllowed(const RenderObject&, const RenderStyle&) const;
     virtual ETextAlign textAlignmentForLine(bool endsWithSoftBreak) const;
@@ -62,6 +73,9 @@ private:
 
     // Allow RenderRubyRun to manipulate the children within ruby bases.
     friend class RenderRubyRun;
+
+    float m_initialOffset;
+    unsigned m_isAfterExpansion : 1;
 };
 
 RENDER_OBJECT_TYPE_CASTS(RenderRubyBase, isRubyBase())
