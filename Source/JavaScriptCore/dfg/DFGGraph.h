ea8afeeb6fc97657a81fbbc09977d8b9df1cfed1
Dana Burkart <dburkart@apple.com>
2015-02-20 18:22:36 +0000

    Merged r180237. <rdar://19870991>
    Canonical link: https://commits.webkit.org/152923.1019@safari-600.5-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-600.5-branch@180421 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGGraph.h
+++ Source/JavaScriptCore/dfg/DFGGraph.h
@@ -481,6 +481,14 @@ public:
         return hasExitSite(node->origin.semantic, exitKind);
     }
     
+    bool usesArguments(InlineCallFrame* inlineCallFrame)
+    {
+        if (!inlineCallFrame)
+            return m_profiledBlock->usesArguments();
+        
+        return baselineCodeBlockForInlineCallFrame(inlineCallFrame)->usesArguments();
+    }
+    
     VirtualRegister argumentsRegisterFor(InlineCallFrame* inlineCallFrame)
     {
         if (!inlineCallFrame)
