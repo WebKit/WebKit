d4b30102f4f74d448c9e390119080d98180504a1
Michael Catanzaro <mcatanzaro@igalia.com>
2015-02-28 11:03:37 +0000

    Merge r180565 - Crash loading local file with WebPageProxy::loadAlternateHTMLString https://bugs.webkit.org/show_bug.cgi?id=141867
    Patch by Michael Catanzaro <mcatanzaro@igalia.com> on 2015-02-24
    Reviewed by Anders Carlsson.
    
    Source/WebKit2:
    
    WebPageProxy::loadAlternateHTMLString needs to assume read access to unreachableURL as well
    as baseURL, because unreachableURL will get added to the back/forward list, causing us to
    crash later on when we notice the unexpected URL received in checkURLReceivedFromWebProcess.
    
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::loadAlternateHTMLString):
    
    Tools:
    
    * TestWebKitAPI/Tests/WebKit2/LoadAlternateHTMLStringWithNonDirectoryURL.cpp:
    (TestWebKitAPI::loadAlternateHTMLString): Split most of this test into a function so it can
    be shared with the new test.
    (TestWebKitAPI::TEST): Add a cross-platform test for this crash.
    * TestWebKitAPI/Tests/WebKit2Gtk/TestLoaderClient.cpp: Add a GTK+ test for this crash.
    (testLoadAlternateHTMLForLocalPage):
    (beforeAll):

--- Source/WebKit2/UIProcess/WebPageProxy.cpp
+++ Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -922,6 +922,7 @@ void WebPageProxy::loadAlternateHTMLString(const String& htmlString, const Strin
         m_mainFrame->setUnreachableURL(unreachableURL);
 
     m_process->assumeReadAccessToBaseURL(baseURL);
+    m_process->assumeReadAccessToBaseURL(unreachableURL);
     m_process->send(Messages::WebPage::LoadAlternateHTMLString(htmlString, baseURL, unreachableURL, UserData(process().transformObjectsToHandles(userData).get())), m_pageID);
     m_process->responsivenessTimer()->start();
 }
