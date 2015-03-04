1b5e53e892c371c475db344037c9122a04845384
Chris Dumez <cdumez@apple.com>
2015-03-04 02:51:16 +0000

    [WK2] Fix memory leak in _getCookieHeadersForTask https://bugs.webkit.org/show_bug.cgi?id=142245
    Reviewed by Alexey Proskuryakov.
    
    Fix memory leak in _getCookieHeadersForTask. We are leaking the CFDictionary
    returned by webKitCookieStorageCopyRequestHeaderFieldsForURL().
    
    This patch addresses the issue by storing the return CFDictionary in a
    RetainPtr<CFDictionaryRef>.
    
    * Shared/mac/CookieStorageShim.mm:
    (-[WKNSURLSessionLocal _getCookieHeadersForTask:completionHandler:]):
    
    
    Canonical link: https://commits.webkit.org/160300@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@180982 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/Shared/mac/CookieStorageShim.mm
+++ Source/WebKit2/Shared/mac/CookieStorageShim.mm
@@ -119,8 +119,9 @@ using CompletionHandlerBlock = void(^)(CFDictionaryRef);
 
     RetainPtr<NSURLSessionTask> strongTask = task;
     CompletionHandlerBlock completionHandlerCopy = [completionHandler copy];
-    RunLoop::main().dispatch([strongTask, completionHandlerCopy]{
-        completionHandlerCopy(WebKit::webKitCookieStorageCopyRequestHeaderFieldsForURL(nullptr, (CFURLRef)[[strongTask currentRequest] URL]));
+    RunLoop::main().dispatch([strongTask, completionHandlerCopy] {
+        RetainPtr<CFDictionaryRef> headers = adoptCF(WebKit::webKitCookieStorageCopyRequestHeaderFieldsForURL(nullptr, (CFURLRef)[[strongTask currentRequest] URL]));
+        completionHandlerCopy(headers.get());
         [completionHandlerCopy release];
     });
 }
