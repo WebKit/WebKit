cf6ae29bab986435d108d3c47fc6ed5fa328e22c
Philippe Normand <pnormand@igalia.com>
2015-03-08 09:11:27 +0000

    Merge r180997 - [GStreamer] the GST_SCHEDULING_FLAG_BANDWIDTH_LIMITED should be wrapped by a ifdef https://bugs.webkit.org/show_bug.cgi?id=142274
    Patch by Philippe Normand <pnormand@igalia.com> on 2015-03-04
    Reviewed by Carlos Garcia Campos.
    
    Don't handle scheduling queries if building against versions of
    GStreamer older than 1.2.0.
    
    * platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp:
    (webKitWebSrcQueryWithParent):

--- Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
@@ -589,6 +589,7 @@ static gboolean webKitWebSrcQueryWithParent(GstPad* pad, GstObject* parent, GstQ
         result = TRUE;
         break;
     }
+#if GST_CHECK_VERSION(1, 2, 0)
     case GST_QUERY_SCHEDULING: {
         GstSchedulingFlags flags;
         int minSize, maxSize, align;
@@ -598,6 +599,7 @@ static gboolean webKitWebSrcQueryWithParent(GstPad* pad, GstObject* parent, GstQ
         result = TRUE;
         break;
     }
+#endif
     default: {
         GRefPtr<GstPad> target = adoptGRef(gst_ghost_pad_get_target(GST_GHOST_PAD_CAST(pad)));
 
