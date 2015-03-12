ec30ef397d56ff881f65894935c68a55d2afa58b
Dan Bernstein <mitz@apple.com>
2015-03-12 21:01:37 +0000

    Finish up <rdar://problem/20086546> [Cocoa] Add an option to treat certificate chains with SHA1-signed certificates as insecure
    Added back OS X bits that I couldn’t land initially in r181317 or had to remove in r181327.
    
    * platform/network/mac/CertificateInfoMac.mm:
    (WebCore::CertificateInfo::containsNonRootSHA1SignedCertificate):
    * platform/spi/cocoa/SecuritySPI.h:
    
    
    
    Canonical link: https://commits.webkit.org/160665@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@181455 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/network/mac/CertificateInfoMac.mm
+++ Source/WebCore/platform/network/mac/CertificateInfoMac.mm
@@ -33,7 +33,7 @@ namespace WebCore {
 
 bool CertificateInfo::containsNonRootSHA1SignedCertificate() const
 {
-#if PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 90000
+#if (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101100) || (PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 90000)
     if (!m_certificateChain)
         return false;
 
