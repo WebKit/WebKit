7801ddfce768921c71bfc105018e190dd6813b40
David Kilzer <ddkilzer@webkit.org>
2015-03-16 12:15:22 +0000

    [iOS] REGRESSION (r181511): Most layout tests crash with a NULL deref
    Follow-up fix for:
    [iOS] Presenting a modal sheet on top of a WKWebView causes it to lose focused, active state
    <http://webkit.org/b/142702>
    
    * UIProcess/ios/PageClientImplIOS.mm:
    (WebKit::PageClientImpl::isViewWindowActive): Add NULL check for m_webView.
    (WebKit::PageClientImpl::isViewFocused): Ditto.
    
    Canonical link: https://commits.webkit.org/160732@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@181548 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/ios/PageClientImplIOS.mm
+++ Source/WebKit2/UIProcess/ios/PageClientImplIOS.mm
@@ -171,13 +171,13 @@ IntSize PageClientImpl::viewSize()
 bool PageClientImpl::isViewWindowActive()
 {
     // FIXME: https://bugs.webkit.org/show_bug.cgi?id=133098
-    return isViewVisible() || m_webView->_activeFocusedStateRetainCount;
+    return isViewVisible() || (m_webView && m_webView->_activeFocusedStateRetainCount);
 }
 
 bool PageClientImpl::isViewFocused()
 {
     // FIXME: https://bugs.webkit.org/show_bug.cgi?id=133098
-    return isViewWindowActive() || m_webView->_activeFocusedStateRetainCount;
+    return isViewWindowActive() || (m_webView && m_webView->_activeFocusedStateRetainCount);
 }
 
 bool PageClientImpl::isViewVisible()
