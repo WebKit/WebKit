01f5a8468d9f5d2e4182cd7c816bd6e51d960970
Jer Noble <jer.noble@apple.com>
2015-03-17 03:28:10 +0000

    [Mac] Update missing image UI https://bugs.webkit.org/show_bug.cgi?id=142592
    Reviewed by Darin Adler.
    
    Update the broken image glyphs with new art (including a 3x image).
    
    Test: fast/hidpi/broken-image-icon-very-hidpi.html
    
    * Resources/missingImage.png:
    * Resources/missingImage@2x.png:
    * Resources/missingImage@3x.png: Added.
    * WebCore.xcodeproj/project.pbxproj:
    * loader/cache/CachedImage.cpp:
    (WebCore::CachedImage::brokenImage):
    
    
    Canonical link: https://commits.webkit.org/160779@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@181608 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/cache/CachedImage.cpp
+++ Source/WebCore/loader/cache/CachedImage.cpp
@@ -170,6 +170,11 @@ void CachedImage::allClientsRemoved()
 
 std::pair<Image*, float> CachedImage::brokenImage(float deviceScaleFactor) const
 {
+    if (deviceScaleFactor >= 3) {
+        static NeverDestroyed<Image*> brokenImageVeryHiRes(Image::loadPlatformResource("missingImage@3x").leakRef());
+        return std::make_pair(brokenImageVeryHiRes, 3);
+    }
+
     if (deviceScaleFactor >= 2) {
         static NeverDestroyed<Image*> brokenImageHiRes(Image::loadPlatformResource("missingImage@2x").leakRef());
         return std::make_pair(brokenImageHiRes, 2);
