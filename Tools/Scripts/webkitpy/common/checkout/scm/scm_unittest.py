8a7e847539754c4d776fec5ca4b9cc7240f96a71
Commit Queue <commit-queue@webkit.org>
2015-04-01 19:20:14 +0000

    Unreviewed, rolling out r182174 and r182177. https://bugs.webkit.org/show_bug.cgi?id=143314
    Try to correct EWS failures (Requested by bfulgham on
    #webkit).
    
    Reverted changesets:
    
    "Python SCM should be able to retrieve untracked files"
    https://bugs.webkit.org/show_bug.cgi?id=143135
    http://trac.webkit.org/changeset/182174
    
    "[buildbot] clean-build script should remove untracked files
    and revert local changes too"
    https://bugs.webkit.org/show_bug.cgi?id=142400
    http://trac.webkit.org/changeset/182177
    
    Canonical link: https://commits.webkit.org/161345@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@182248 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/checkout/scm/scm_unittest.py
+++ Tools/Scripts/webkitpy/common/checkout/scm/scm_unittest.py
@@ -309,51 +309,6 @@ class SCMTest(unittest.TestCase):
         self.assertItemsEqual(self.scm.changed_files(), ["test_dir/test_file3", "test_file"])
         os.chdir(old_cwd)
 
-    def _shared_test_untracked_files(self, scm):
-        write_into_file_at_path("test_file_new", "new content")
-        self.assertItemsEqual(scm.untracked_files(), ["test_file_new"])
-
-        os.mkdir("test_dir_new")
-        write_into_file_at_path("test_dir_new/test_file_new", "new stuff")
-        self.assertItemsEqual(scm.untracked_files(), ["test_dir_new", "test_file_new"])
-
-        old_cwd = os.getcwd()
-        os.chdir("test_dir_new")
-        # Validate that untracked_files do not change with our cwd.
-        self.assertItemsEqual(scm.untracked_files(), ["test_dir_new", "test_file_new"])
-
-        os.chdir(old_cwd)
-
-        write_into_file_at_path("test_file_new.pyc", "new ignored file")
-        self.assertItemsEqual(scm.untracked_files(), ["test_dir_new", "test_file_new"])
-        self.assertItemsEqual(scm.untracked_files(include_ignored_files=True), ["test_file_new.pyc", "test_dir_new", "test_file_new"])
-
-        shutil.rmtree("test_dir_new")
-        os.remove("test_file_new")
-        os.remove("test_file_new.pyc")
-
-    def _shared_test_discard_untracked_files(self, scm):
-        write_into_file_at_path("test_file_new", "new content")
-        os.mkdir("test_dir_new")
-        write_into_file_at_path("test_dir_new/test_file_new", "new stuff")
-        self.assertItemsEqual(scm.untracked_files(), ["test_dir_new", "test_file_new"])
-        scm.discard_untracked_files()
-        self.assertItemsEqual(scm.untracked_files(), [])
-
-        write_into_file_at_path("test_file_new.pyc", "new content")
-        self.assertItemsEqual(scm.untracked_files(True), ["test_file_new.pyc"])
-        scm.discard_untracked_files(discard_ignored_files=False)
-        self.assertItemsEqual(scm.untracked_files(True), ["test_file_new.pyc"])
-        scm.discard_untracked_files(discard_ignored_files=True)
-        self.assertItemsEqual(scm.untracked_files(True), [])
-
-        if os.path.isdir("test_dir_new"):
-            shutil.rmtree("test_dir_new")
-        if os.path.isfile("test_file_new"):
-            os.remove("test_file_new")
-        if os.path.isfile("test_file_new.pyc"):
-            os.remove("test_file_new.pyc")
-
     def _shared_test_added_files(self):
         write_into_file_at_path("test_file", "changed content")
         self.assertItemsEqual(self.scm.added_files(), [])
@@ -850,12 +805,6 @@ END
 
     def test_changed_files(self):
         self._shared_test_changed_files()
-
-    def test_untracked_files(self):
-        self._shared_test_untracked_files(self.scm)
-
-    def test_discard_untracked_files(self):
-        self._shared_test_discard_untracked_files(self.scm)
 
     def test_changed_files_for_revision(self):
         self._shared_test_changed_files_for_revision()
@@ -1509,9 +1458,6 @@ class GitSVNTest(SCMTest):
 
     def test_changed_files(self):
         self._shared_test_changed_files()
-
-    def test_untracked_files(self):
-        self._shared_test_untracked_files(self.scm)
 
     def test_changed_files_for_revision(self):
         self._shared_test_changed_files_for_revision()
