37453e096c9e8d84f2a10a5f537230928dd284f0
Milan Crha <mcrha@redhat.com>
2015-04-07 13:03:33 +0000

    Merge r180141 - [GTK] Loading page into WebView shows g_closure_unref warning https://bugs.webkit.org/show_bug.cgi?id=127474
    Patch by Milan Crha <mcrha@redhat.com> on 2015-02-16
    Reviewed by Carlos Garcia Campos.
    
    * bindings/gobject/GObjectEventListener.cpp:
    (WebCore::GObjectEventListener::gobjectDestroyed):

--- Source/WebCore/bindings/gobject/GObjectEventListener.cpp
+++ Source/WebCore/bindings/gobject/GObjectEventListener.cpp
@@ -52,12 +52,13 @@ void GObjectEventListener::gobjectDestroyed()
 {
     ASSERT(m_coreTarget);
 
-    // We must set m_coreTarget to null, because removeEventListener
-    // may call the destructor as a side effect and we must be in the
-    // proper state to prevent g_object_weak_unref.
-    EventTarget* target = m_coreTarget;
+    // Protect 'this' class in case the 'm_coreTarget' holds the last reference,
+    // which may cause, inside removeEventListener(), free of this object
+    // and later use-after-free with the m_handler = 0; assignment.
+    RefPtr<GObjectEventListener> protect(this);
+
+    m_coreTarget->removeEventListener(m_domEventName.data(), this, m_capture);
     m_coreTarget = 0;
-    target->removeEventListener(m_domEventName.data(), this, m_capture);
     m_handler = 0;
 }
 
