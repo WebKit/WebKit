ec0a87215df95e5525c03334115abe9bea115b71
Gyuyoung Kim <gyuyoung.kim@webkit.org>
2015-04-13 00:54:13 +0000

    [EFL] Enable Media Source https://bugs.webkit.org/show_bug.cgi?id=143635
    Reviewed by Csaba Osztrogonác.
    
    .:
    
    * Source/cmake/OptionsEfl.cmake: Add ENABLE_MEDIA_SOURCE switch.
    
    Tools:
    
    * Scripts/webkitperl/FeatureList.pm: Enable media source on efl port.
    
    LayoutTests:
    
    * platform/efl/TestExpectations: Update failing tests.
    
    
    Canonical link: https://commits.webkit.org/161673@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@182675 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitperl/FeatureList.pm
+++ Tools/Scripts/webkitperl/FeatureList.pm
@@ -296,7 +296,7 @@ my @features = (
       define => "ENABLE_MEDIA_CAPTURE", default => isEfl(), value => \$mediaCaptureSupport },
 
     { option => "media-source", desc => "Toggle Media Source support",
-      define => "ENABLE_MEDIA_SOURCE", default => isGtk(), value => \$mediaSourceSupport },
+      define => "ENABLE_MEDIA_SOURCE", default => (isGtk() || isEfl()), value => \$mediaSourceSupport },
 
     { option => "media-statistics", desc => "Toggle Media Statistics support",
       define => "ENABLE_MEDIA_STATISTICS", default => 0, value => \$mediaStatisticsSupport },
