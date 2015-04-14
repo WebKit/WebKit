8bb265a82d0011bc9b01e054480ffbd418db4915
Alexey Proskuryakov <ap@apple.com>
2015-04-14 01:36:03 +0000

    Build fix.
    * UIProcess/mac/PageClientImpl.mm: (WebKit::PageClientImpl::setDragImage):
    
    
    
    Canonical link: https://commits.webkit.org/161729@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@182775 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/mac/PageClientImpl.mm
+++ Source/WebKit2/UIProcess/mac/PageClientImpl.mm
@@ -407,7 +407,10 @@ void PageClientImpl::setDragImage(const IntPoint& clientPosition, PassRefPtr<Sha
     size.scale(1.0 / toImpl([m_wkView pageRef])->deviceScaleFactor());
     [dragNSImage setSize:size];
 
-    [m_wkView _dragImageForView: (m_webView) ? : m_wkView withImage:dragNSImage.get() at:clientPosition linkDrag:isLinkDrag];
+    if (m_webView)
+        [m_wkView _dragImageForView:m_webView withImage:dragNSImage.get() at:clientPosition linkDrag:isLinkDrag];
+    else
+        [m_wkView _dragImageForView:m_wkView withImage:dragNSImage.get() at:clientPosition linkDrag:isLinkDrag];
 }
 
 void PageClientImpl::setPromisedDataForImage(const String& pasteboardName, PassRefPtr<SharedBuffer> imageBuffer, const String& filename, const String& extension, const String& title, const String& url, const String& visibleUrl, PassRefPtr<SharedBuffer> archiveBuffer)
