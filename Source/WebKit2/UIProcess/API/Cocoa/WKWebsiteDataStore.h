3cfce0fa4964798243a426f60d8099eb1c034a7d
Anders Carlsson <andersca@apple.com>
2015-04-24 20:55:22 +0000

    Address a review comment from Joe Pecoraro.
    * UIProcess/API/Cocoa/WKWebsiteDataStore.h:
    
    Canonical link: https://commits.webkit.org/162146@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@183279 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/Cocoa/WKWebsiteDataStore.h
+++ Source/WebKit2/UIProcess/API/Cocoa/WKWebsiteDataStore.h
@@ -32,8 +32,8 @@
 WK_ASSUME_NONNULL_BEGIN
 
 /*! A WKWebsiteDataStore represents various types of data that a website might
- make use of. This includes cookies, disk and memory caches, and persistent data such as
- WebSQL and IndexedDB databases and local storage.
+ make use of. This includes cookies, disk and memory caches, and persistent data such as WebSQL,
+ IndexedDB databases, and local storage.
  */
 WK_CLASS_AVAILABLE(WK_MAC_TBA, WK_IOS_TBA)
 @interface WKWebsiteDataStore : NSObject
