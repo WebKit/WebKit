1dbe5d5051323f61dd841b996e42d93d42bff440
Commit Queue <commit-queue@webkit.org>
2015-04-25 03:40:02 +0000

    Unreviewed, rolling out r183303. https://bugs.webkit.org/show_bug.cgi?id=144181
    Change was incorrect, broke builds (Requested by smfr on
    #webkit).
    
    Reverted changeset:
    
    "Fix the build."
    http://trac.webkit.org/changeset/183303
    
    Canonical link: https://commits.webkit.org/162172@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@183306 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/MiniBrowser/mac/WK2BrowserWindowController.m
+++ Tools/MiniBrowser/mac/WK2BrowserWindowController.m
@@ -78,7 +78,7 @@ static void* keyValueObservingContext = &keyValueObservingContext;
         return nil;
 
     _configuration = [configuration copy];
-    _isPrivateBrowsingWindow = _configuration.websiteDataStore.isNonPersistent;
+    _isPrivateBrowsingWindow = !_configuration.websiteDataStore.isPersistent;
 
     return self;
 }
