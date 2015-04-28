7b1ab3a885d491d62f6455812c3371cc0dc93142
Brent Fulgham <bfulgham@apple.com>
2015-04-28 17:05:30 +0000

    [Win] PHP scripts fail if HTTP_ORIGIN is not set. https://bugs.webkit.org/show_bug.cgi?id=144333
    Rubberstamped by Brady Eidson.
    
    * http/tests/security/resources/cors-post-redirect-target.php: Check
    that HTTP_ORIGIN key exists before using it to avoid PHP error.
    
    
    Canonical link: https://commits.webkit.org/162338@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@183483 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/security/resources/cors-post-redirect-target.php
+++ LayoutTests/http/tests/security/resources/cors-post-redirect-target.php
@@ -2,7 +2,7 @@
 header("Cache-Control: no-store");
 
 $sawOrigin = false;
-$originHeader = $_SERVER['HTTP_ORIGIN'];
+$originHeader = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : null;
 if ($originHeader) {
     echo "Origin header value: $originHeader";
     $sawOrigin = true;
