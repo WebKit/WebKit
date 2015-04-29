b7da5c0178417d234f2498073f6f0a4df91c0ffe
Alex Christensen <achristensen@apple.com>
2015-04-29 19:23:04 +0000

    Fix WinCairo bot.
    * Scripts/webkitdirs.pm:
    (determinePassedConfiguration):
    WinCairo bot doesn't use cygwin, but it needs Release_WinCairo for its configuration.
    
    
    
    Canonical link: https://commits.webkit.org/162409@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@183566 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -716,7 +716,7 @@ sub determinePassedConfiguration
         $passedConfiguration = "Profiling";
     }
 
-    $passedConfiguration .= "_WinCairo" if (defined($passedConfiguration) && isWinCairo() && isCygwin());
+    $passedConfiguration .= "_WinCairo" if (defined($passedConfiguration) && isWinCairo());
 }
 
 sub passedConfiguration
