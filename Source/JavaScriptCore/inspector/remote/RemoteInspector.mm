5453c67dbf89fbbb1f9f90ff12a0b514140b7bc0
Joseph Pecoraro <pecoraro@apple.com>
2015-04-29 03:56:47 +0000

    Deadlock on applications using JSContext on non-main thread https://bugs.webkit.org/show_bug.cgi?id=144370
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2015-04-28
    Reviewed by Timothy Hatcher.
    
    * inspector/remote/RemoteInspector.mm:
    (Inspector::RemoteInspector::singleton):
    Prevent a possible deadlock by assuming we can synchronously
    run something on the main queue at this time.
    
    Canonical link: https://commits.webkit.org/162374@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@183526 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/inspector/remote/RemoteInspector.mm
+++ Source/JavaScriptCore/inspector/remote/RemoteInspector.mm
@@ -93,8 +93,12 @@ RemoteInspector& RemoteInspector::singleton()
 
             if ([NSThread isMainThread])
                 initialize();
-            else
-                dispatch_sync(dispatch_get_main_queue(), initialize);
+            else {
+                // FIXME: This means that we may miss an auto-attach to a JSContext created on a non-main thread.
+                // The main thread initialization is required for certain WTF values that need to be initialized
+                // on the "real" main thread. We should investigate a better way to handle this.
+                dispatch_async(dispatch_get_main_queue(), initialize);
+            }
         }
     });
 
