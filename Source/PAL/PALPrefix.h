e4fb36758ed7be92466444181f72c32e168ce017
Myles C. Maxfield <mmaxfield@apple.com>
2015-05-06 20:32:42 +0000

    Introducing the Platform Abstraction Layer (PAL) https://bugs.webkit.org/show_bug.cgi?id=143358
    Reviewed by Simon Fraser.
    
    .:
    
    * WebKit.xcworkspace/contents.xcworkspacedata: Add it to the workspace.
    
    Source/JavaScriptCore:
    
    * Configurations/FeatureDefines.xcconfig: Updated
    
    Source/PAL:
    
    Introduce a new top-level folder and static library. This is intended to be
    the destination for code that currently resides in WebCore/platform. Code
    will be moved from that folder into PAL file by file. Separating this
    platform code into its own static library enforces layering, as well as
    encourages/aids unit testing of this platform code.
    
    This commit only creates a single symbol which is not required to build
    WebKit. The reason for this is so that we can get all build issues ironed
    out before requiring PAL.
    
    For more information, see
    https://lists.webkit.org/pipermail/webkit-dev/2015-March/027303.html
    
    * PAL.xcodeproj/project.pbxproj: Added.
    * Makefile: Added. Standard Makefile.
    * config.h: Added. Standard header.
    * PALPrefix.h: Added. Copied from WebCore.
    * graphics/Foo.h: Added. Header.
    * graphics/Foo.cpp: Added.
    (foo): Placeholder symbol.
    * Configurations/Base.xcconfig: Added. Standard config files
    * Configurations/DebugRelease.xcconfig: Added. Standard config files
    * Configurations/PAL.xcconfig: Added. Standard config files
    
    Source/WebCore:
    
    * WebCore.xcodeproj/project.pbxproj: Let the project know about the .a
    * Configurations/FeatureDefines.xcconfig: Updated
    
    Source/WebKit/mac:
    
    * Configurations/FeatureDefines.xcconfig: Updated
    
    Source/WebKit2:
    
    * Configurations/FeatureDefines.xcconfig: Updated
    
    Tools:
    
    * Scripts/build-webkit: Add it to build-webkit.
    
    Canonical link: https://commits.webkit.org/162717@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@183883 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/PAL/PALPrefix.h
@@ -0,0 +1,179 @@
+/*
+ * Copyright (C) 2004, 2005, 2006, 2007, 2013 Apple Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
+/* This prefix file should contain only: 
+ *    1) files to precompile for faster builds
+ *    2) in one case at least: OS-X-specific performance bug workarounds
+ *    3) the special trick to catch us using new or delete without including "config.h"
+ * The project should be able to build without this header, although we rarely test that.
+ */
+
+/* Things that need to be defined globally should go into "config.h". */
+
+#include <wtf/Platform.h>
+
+#if defined(__APPLE__)
+#ifdef __cplusplus
+#define NULL __null
+#else
+#define NULL ((void *)0)
+#endif
+#endif
+
+#if OS(WINDOWS)
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0502
+#endif
+
+#ifndef WINVER
+#define WINVER 0x0502
+#endif
+
+#if !USE(CURL)
+#ifndef _WINSOCKAPI_
+#define _WINSOCKAPI_ // Prevent inclusion of winsock.h in windows.h
+#endif
+#endif
+
+#undef PAL_EXPORT
+#define PAL_EXPORT WTF_EXPORT_DECLARATION
+
+#else
+
+#include <pthread.h>
+
+#endif // OS(WINDOWS)
+
+#include <sys/types.h>
+#include <fcntl.h>
+#if defined(__APPLE__)
+#include <regex.h>
+#endif
+
+#include <setjmp.h>
+
+#include <signal.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#if defined(__APPLE__)
+#include <unistd.h>
+#endif
+
+#ifdef __cplusplus
+#include <algorithm>
+#include <cstddef>
+#include <new>
+#endif
+
+#if defined(__APPLE__)
+#include <sys/param.h>
+#endif
+#include <sys/stat.h>
+#if defined(__APPLE__)
+#include <sys/time.h>
+#include <sys/resource.h>
+#endif
+
+#include <CoreFoundation/CoreFoundation.h>
+
+#if OS(WINDOWS)
+#ifndef CF_IMPLICIT_BRIDGING_ENABLED
+#define CF_IMPLICIT_BRIDGING_ENABLED
+#endif
+
+#ifndef CF_IMPLICIT_BRIDGING_DISABLED
+#define CF_IMPLICIT_BRIDGING_DISABLED
+#endif
+
+#include <CoreFoundation/CFBase.h>
+
+#ifndef CF_ENUM
+#define CF_ENUM(_type, _name) _type _name; enum
+#endif
+#ifndef CF_OPTIONS
+#define CF_OPTIONS(_type, _name) _type _name; enum
+#endif
+#ifndef CF_ENUM_DEPRECATED
+#define CF_ENUM_DEPRECATED(_macIntro, _macDep, _iosIntro, _iosDep)
+#endif
+#ifndef CF_ENUM_AVAILABLE
+#define CF_ENUM_AVAILABLE(_mac, _ios)
+#endif
+#endif
+
+#if PLATFORM(WIN_CAIRO)
+#include <ConditionalMacros.h>
+#include <windows.h>
+#else
+
+#if OS(WINDOWS)
+#if USE(CG)
+
+// FIXME <rdar://problem/8208868> Remove support for obsolete ColorSync API, CoreServices header in CoreGraphics
+// We can remove this once the new ColorSync APIs are available in an internal Safari SDK.
+#include <ColorSync/ColorSync.h>
+#ifdef __COLORSYNCDEPRECATED__
+#define COREGRAPHICS_INCLUDES_CORESERVICES_HEADER
+#define OBSOLETE_COLORSYNC_API
+#endif
+#endif
+#if USE(CFNETWORK)
+/* Windows doesn't include CFNetwork.h via CoreServices.h, so we do
+   it explicitly here to make Windows more consistent with Mac. */
+#include <CFNetwork/CFNetwork.h>
+// On Windows, dispatch.h needs to be included before certain CFNetwork headers.
+#include <dispatch/dispatch.h>
+#endif
+#include <windows.h>
+#else
+#if !PLATFORM(IOS)
+#include <CoreServices/CoreServices.h>
+#endif // !PLATFORM(IOS)
+#endif // OS(WINDOWS)
+
+#endif
+
+#ifdef __OBJC__
+#if PLATFORM(IOS)
+#import <Foundation/Foundation.h>
+#else
+#import <Cocoa/Cocoa.h>
+#endif // PLATFORM(IOS)
+#endif
+
+#ifdef __cplusplus
+#define new ("if you use new/delete make sure to include config.h at the top of the file"()) 
+#define delete ("if you use new/delete make sure to include config.h at the top of the file"()) 
+#endif
+
+/* When C++ exceptions are disabled, the C++ library defines |try| and |catch|
+ * to allow C++ code that expects exceptions to build. These definitions
+ * interfere with Objective-C++ uses of Objective-C exception handlers, which
+ * use |@try| and |@catch|. As a workaround, undefine these macros. */
+#ifdef __OBJC__
+#undef try
+#undef catch
+#endif
+
