13efa1b4c0253c22e66954116748a3b4871e1c78
Chris Dumez <cdumez@apple.com>
2015-05-11 13:10:17 +0000

    Merge r182979 - Possible null pointer dereference in WebDiagnosticLoggingClient::logDiagnosticMessageWithValue() https://bugs.webkit.org/show_bug.cgi?id=143899 <rdar://problem/20584215>
    Reviewed by Anders Carlsson.
    
    WebDiagnosticLoggingClient::logDiagnosticMessage*() methods failed to
    check that m_page.corePage() was non-null before dereferencing, thus
    causing crashes when it is null.
    
    * WebProcess/WebCoreSupport/WebDiagnosticLoggingClient.cpp:
    (WebKit::WebDiagnosticLoggingClient::logDiagnosticMessage):
    (WebKit::WebDiagnosticLoggingClient::logDiagnosticMessageWithResult):
    (WebKit::WebDiagnosticLoggingClient::logDiagnosticMessageWithValue):

--- Source/WebKit2/WebProcess/WebCoreSupport/WebDiagnosticLoggingClient.cpp
+++ Source/WebKit2/WebProcess/WebCoreSupport/WebDiagnosticLoggingClient.cpp
@@ -43,7 +43,7 @@ WebDiagnosticLoggingClient::~WebDiagnosticLoggingClient()
 
 void WebDiagnosticLoggingClient::logDiagnosticMessage(const String& message, const String& description)
 {
-    if (!m_page.corePage()->settings().diagnosticLoggingEnabled())
+    if (!m_page.corePage() || !m_page.corePage()->settings().diagnosticLoggingEnabled())
         return;
 
     // FIXME: Remove this injected bundle API.
@@ -53,7 +53,7 @@ void WebDiagnosticLoggingClient::logDiagnosticMessage(const String& message, con
 
 void WebDiagnosticLoggingClient::logDiagnosticMessageWithResult(const String& message, const String& description, WebCore::DiagnosticLoggingResultType result)
 {
-    if (!m_page.corePage()->settings().diagnosticLoggingEnabled())
+    if (!m_page.corePage() || !m_page.corePage()->settings().diagnosticLoggingEnabled())
         return;
 
     // FIXME: Remove this injected bundle API.
@@ -63,7 +63,7 @@ void WebDiagnosticLoggingClient::logDiagnosticMessageWithResult(const String& me
 
 void WebDiagnosticLoggingClient::logDiagnosticMessageWithValue(const String& message, const String& description, const String& value)
 {
-    if (!m_page.corePage()->settings().diagnosticLoggingEnabled())
+    if (!m_page.corePage() || !m_page.corePage()->settings().diagnosticLoggingEnabled())
         return;
 
     // FIXME: Remove this injected bundle API.
