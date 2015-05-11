851f444bae1f42b275acf24d17efc24279f85c81
Bem Jones-Bey <bjonesbe@adobe.com>
2015-05-11 13:08:11 +0000

    Merge r182974 - Large values for line-height cause integer overflow in RenderStyle::computedLineHeight https://bugs.webkit.org/show_bug.cgi?id=143863
    Reviewed by Rob Buis.
    
    Source/WebCore:
    
    When we compute huge values for line-height through percentage or CSS
    calc, we'll overflow the integer and later on
    ShapeOutsideInfo::computeDeltasForContainingBlockLine will ASSERT
    because it expects non-negative line height.  So for the computed
    line-height, clamp to an integer range to avoid overflow. Note that
    the code path for percentages here is safe because LayoutUnit clamps
    to an int on conversion.
    
    This is based on a Blink patch by Rob Buis.
    
    Test: fast/shapes/shape-outside-floats/shape-outside-negative-line-height-crash.html
    
    * rendering/style/RenderStyle.cpp:
    (WebCore::RenderStyle::computedLineHeight): Clamp line-height to an
        int to avoid overflow.
    
    LayoutTests:
    
    Simplified test from a fuzzer.
    
    * fast/shapes/shape-outside-floats/shape-outside-negative-line-height-crash-expected.txt: Added.
    * fast/shapes/shape-outside-floats/shape-outside-negative-line-height-crash.html: Added.

--- Source/WebCore/rendering/style/RenderStyle.cpp
+++ Source/WebCore/rendering/style/RenderStyle.cpp
@@ -1396,7 +1396,7 @@ int RenderStyle::computedLineHeight() const
     if (lh.isPercent())
         return minimumValueForLength(lh, fontSize());
 
-    return lh.value();
+    return clampTo<int>(lh.value());
 }
 
 void RenderStyle::setWordSpacing(Length value)
