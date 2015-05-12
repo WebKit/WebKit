9d162d1bda59772d396034fb32ac1e26e2217d5d
Brent Fulgham <bfulgham@apple.com>
2015-05-12 19:49:16 +0000

    ROLLOUT [Win] Unreviewed build fix for older DirectX build environments.
    * platform/graphics/avfoundation/cf/MediaPlayerPrivateAVFoundationCF.cpp:
    
    
    
    Canonical link: https://commits.webkit.org/162965@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@184214 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/avfoundation/cf/MediaPlayerPrivateAVFoundationCF.cpp
+++ Source/WebCore/platform/graphics/avfoundation/cf/MediaPlayerPrivateAVFoundationCF.cpp
@@ -47,7 +47,6 @@
 #include "PlatformCALayerWin.h"
 #include "TimeRanges.h"
 #include "WebCoreAVCFResourceLoader.h"
-#include "WebKitQuartzCoreAdditions/WKCACFTypes.h"
 
 #include <AVFoundationCF/AVCFPlayerItem.h>
 #if HAVE(AVFOUNDATION_LEGIBLE_OUTPUT_SUPPORT)
@@ -58,6 +57,7 @@
 #include <AVFoundationCF/AVCFAssetResourceLoader.h>
 #endif
 #include <AVFoundationCF/AVFoundationCF.h>
+#include <d3d9.h>
 #include <delayimp.h>
 #include <dispatch/dispatch.h>
 #if HAVE(AVFOUNDATION_LOADER_DELEGATE) && ENABLE(ENCRYPTED_MEDIA_V2)
