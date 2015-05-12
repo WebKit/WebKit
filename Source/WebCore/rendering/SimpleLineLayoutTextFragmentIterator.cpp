85f173699f040282e96cd4aa90d72a17a0fdb56f
Myles C. Maxfield <mmaxfield@apple.com>
2015-05-12 01:06:59 +0000

    Addressing post-review comments after r184037.
    Unreviewed.
    
    No new tests because there is no behavior change.
    
    * rendering/SimpleLineLayoutTextFragmentIterator.cpp:
    (WebCore::SimpleLineLayout::TextFragmentIterator::runWidth):
    
    Canonical link: https://commits.webkit.org/162931@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@184138 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/SimpleLineLayoutTextFragmentIterator.cpp
+++ Source/WebCore/rendering/SimpleLineLayoutTextFragmentIterator.cpp
@@ -197,8 +197,7 @@ float TextFragmentIterator::runWidth(const FlowContents::Segment& segment, unsig
     bool measureWithEndSpace = m_style.collapseWhitespace && segmentTo < segment.text.length() && segment.text[segmentTo] == ' ';
     if (measureWithEndSpace)
         ++segmentTo;
-    auto string = segment.text.substring(segmentFrom, segmentTo - segmentFrom);
-    TextRun run(string);
+    TextRun run(StringView(segment.text).substring(segmentFrom, segmentTo - segmentFrom));
     run.setXPos(xPosition);
     run.setTabSize(!!m_style.tabWidth, m_style.tabWidth);
     float width = m_style.font.width(run);
