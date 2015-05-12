85da21f0f21fcaf5adf5b539fcd100aa857ca17e
Žan Doberšek <zdobersek@igalia.com>
2015-05-12 08:35:26 +0000

    Merge r183855 - EventHandler::m_eventHandlerWillResetCapturingMouseEventsElement is incorrectly initialized https://bugs.webkit.org/show_bug.cgi?id=144583
    Reviewed by Daniel Bates.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::EventHandler): The
    m_eventHandlerWillResetCapturingMouseEventsElement is a boolean,
    so initialize it to false, instead of to nullptr.

--- Source/WebCore/page/EventHandler.cpp
+++ Source/WebCore/page/EventHandler.cpp
@@ -392,7 +392,7 @@ EventHandler::EventHandler(Frame& frame)
 #endif
     , m_svgPan(false)
     , m_resizeLayer(0)
-    , m_eventHandlerWillResetCapturingMouseEventsElement(nullptr)
+    , m_eventHandlerWillResetCapturingMouseEventsElement(false)
     , m_clickCount(0)
 #if ENABLE(IOS_GESTURE_EVENTS)
     , m_gestureInitialDiameter(GestureUnknown)
