55f2c180c082c275da63ef1e002432e70626904b
Benjamin Poulain <benjamin@webkit.org>
2015-05-13 01:52:52 +0000

    Fix the iteration count of arith-modulo-node-behaviors.js
    * tests/stress/arith-modulo-node-behaviors.js:
    No need for big numbers for the real testing.
    
    
    Canonical link: https://commits.webkit.org/162981@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@184230 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/tests/stress/arith-modulo-node-behaviors.js
+++ Source/JavaScriptCore/tests/stress/arith-modulo-node-behaviors.js
@@ -7,13 +7,13 @@ function moduloWithNegativeZeroDividend(a, b, c)
 noInline(moduloWithNegativeZeroDividend);
 
 // Warm up with integers. The test for NegZero should not be eliminated here.
-for (var i = 1; i < 1e7; ++i) {
+for (var i = 1; i < 1e4; ++i) {
     var result = moduloWithNegativeZeroDividend(i, 5, 5);
     if (result !== 0)
         throw "moduloWithNegativeZeroDividend(i, 5, 5), returned: " + result;
 }
 
-for (var i = 1; i < 1e7; ++i) {
+for (var i = 1; i < 1e4; ++i) {
     // Produce negative zero in the multiplication.
     var result = moduloWithNegativeZeroDividend(-i, 0, 2);
     if (!(result === 0 && (1/result) === -Infinity))
@@ -26,7 +26,7 @@ for (var i = 1; i < 1e7; ++i) {
 }
 
 // Edge cases.
-for (var i = 1; i < 1e7; ++i) {
+for (var i = 1; i < 1e4; ++i) {
     var result = moduloWithNegativeZeroDividend(-i, 0, Infinity);
     if (!(result === 0 && (1/result) === -Infinity))
         throw "moduloWithNegativeZeroDividend(-i, 0, Infinity) failed, returned: " + result;
@@ -49,20 +49,20 @@ function moduloWithUnusedNegativeZeroDividend(a, b, c)
 }
 noInline(moduloWithUnusedNegativeZeroDividend);
 
-for (var i = 1; i < 1e7; ++i) {
+for (var i = 1; i < 1e4; ++i) {
     var result = moduloWithUnusedNegativeZeroDividend(i, 5, 5);
     if (result !== 0)
         throw "moduloWithUnusedNegativeZeroDividend(i, 5, 5), returned: " + result;
 }
 
 // Produce negative zero in the multiplication.
-for (var i = 1; i < 1e7; ++i) {
+for (var i = 1; i < 1e4; ++i) {
     var result = moduloWithUnusedNegativeZeroDividend(-i, 0, 2);
     if (!(result === 0 && (1/result) === Infinity))
         throw "moduloWithUnusedNegativeZeroDividend(-i, 0, 2) failed, returned: " + result;
 }
 
-for (var i = 1; i < 1e7; ++i) {
+for (var i = 1; i < 1e4; ++i) {
     var result = moduloWithUnusedNegativeZeroDividend(-i, 0, Infinity);
     if (!(result === 0 && (1/result) === Infinity))
         throw "moduloWithUnusedNegativeZeroDividend(-i, 0, Infinity) failed, returned: " + result;
@@ -86,7 +86,7 @@ function moduloWithNegativeZeroDivisor(a, b, c)
 noInline(moduloWithNegativeZeroDivisor);
 
 // Warm up with integers.
-for (var i = 1; i < 1e7; ++i) {
+for (var i = 1; i < 1e4; ++i) {
     var result = moduloWithNegativeZeroDivisor(i, 2, i);
     if (result !== i)
         throw "moduloWithNegativeZeroDividend(i, 2, i), returned: " + result;
@@ -97,7 +97,7 @@ for (var i = 1; i < 1e7; ++i) {
 }
 
 // Produce negative zero in the multiplication.
-for (var i = 1; i < 1e7; ++i) {
+for (var i = 1; i < 1e4; ++i) {
     var result = moduloWithNegativeZeroDivisor(-i, 0, 2);
     if (result === result)
         throw "moduloWithNegativeZeroDivisor(-i, 0, 2) failed, returned: " + result;
