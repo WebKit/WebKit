fabdceda1b5462564dc620e10b069fb6729df125
David Hyatt <hyatt@apple.com>
2015-05-15 12:04:35 +0000

    Merge r184293 - Don't compute selection painting info when we don't have selection. https://bugs.webkit.org/show_bug.cgi?id=144920 <rdar://problem/20919920>
    Reviewed by Simon Fraser.
    
    * rendering/InlineTextBox.cpp:
    (WebCore::InlineTextBox::paint):
    
    Just set the selection paint style to the text paint style when we don't have a selection
    at all. Computing the selection style takes time in the case where a ::selection pseudo is
    used on the page, so we don't want to waste time computing that info unless it's actually
    needed.

--- Source/WebCore/rendering/InlineTextBox.cpp
+++ Source/WebCore/rendering/InlineTextBox.cpp
@@ -529,14 +529,12 @@ void InlineTextBox::paint(PaintInfo& paintInfo, const LayoutPoint& paintOffset, 
     // Determine the text colors and selection colors.
     TextPaintStyle textPaintStyle = computeTextPaintStyle(renderer().frame(), lineStyle, paintInfo);
 
-    bool paintSelectedTextOnly;
-    bool paintSelectedTextSeparately;
-    const ShadowData* selectionShadow;
-    TextPaintStyle selectionPaintStyle = computeTextSelectionPaintStyle(textPaintStyle, renderer(), lineStyle, paintInfo, paintSelectedTextOnly, paintSelectedTextSeparately, selectionShadow);
-
+    bool paintSelectedTextOnly = false;
+    bool paintSelectedTextSeparately = false;
+    const ShadowData* selectionShadow = nullptr;
+    
     // Text with custom underlines does not have selection background painted, so selection paint style is not appropriate for it.
-    if (useCustomUnderlines)
-        selectionPaintStyle = textPaintStyle;
+    TextPaintStyle selectionPaintStyle = haveSelection && !useCustomUnderlines ? computeTextSelectionPaintStyle(textPaintStyle, renderer(), lineStyle, paintInfo, paintSelectedTextOnly, paintSelectedTextSeparately, selectionShadow) : textPaintStyle;
 
     // Set our font.
     const FontCascade& font = fontToUse(lineStyle, renderer());
