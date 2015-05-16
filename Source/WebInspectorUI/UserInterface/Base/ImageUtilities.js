6a657650c6572835763a87eac69985f52f3e9d41
Joseph Pecoraro <pecoraro@apple.com>
2015-05-16 02:31:34 +0000

    Web Inspector: Bump image format number to force image regeneration https://bugs.webkit.org/show_bug.cgi?id=145074
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2015-05-15
    Reviewed by Timothy Hatcher.
    
    * UserInterface/Base/ImageUtilities.js:
    
    Canonical link: https://commits.webkit.org/163114@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@184426 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Base/ImageUtilities.js
+++ Source/WebInspectorUI/UserInterface/Base/ImageUtilities.js
@@ -28,7 +28,7 @@ const _imageStorageFormatVersion = 1;
 
 // Use as a default where an image version is not otherwise specified.
 // Bump the base version when making changes that affect the result image.
-const baseDefaultImageVersion = 6;
+const baseDefaultImageVersion = 7;
 const defaultImageVersion = baseDefaultImageVersion + 0.01 * WebInspector.Platform.version.base + 0.0001 * WebInspector.Platform.version.release;
 
 try {
