afe8fa618dec9a5e5b95326ce488f5788b8cbd27
Matt Hanson <matthew_hanson@apple.com>
2015-05-18 17:00:45 +0000

    Merge r184341. rdar://problem/20809399
    Canonical link: https://commits.webkit.org/162740.53@safari-601.1.32.2-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601.1.32.2-branch@184494 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/text/StringView.cpp
+++ Source/WTF/wtf/text/StringView.cpp
@@ -73,6 +73,17 @@ bool StringView::endsWithIgnoringASCIICase(const StringView& suffix) const
     return ::WTF::endsWithIgnoringASCIICase(*this, suffix);
 }
 
+bool equalIgnoringASCIICase(StringView a, const char* b, unsigned bLength)
+{
+    if (bLength != a.length())
+        return false;
+
+    if (a.is8Bit())
+        return equalIgnoringASCIICase(a.characters8(), b, bLength);
+
+    return equalIgnoringASCIICase(a.characters16(), b, bLength);
+}
+
 #if CHECK_STRINGVIEW_LIFETIME
 
 // Manage reference count manually so UnderlyingString does not need to be defined in the header.
