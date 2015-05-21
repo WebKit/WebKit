9a20ce9f56a007761ea33d8b06dee9fd1b87d97e
Matt Hanson <matthew_hanson@apple.com>
2015-05-21 06:39:35 +0000

    Merge r184652. rdar://problem/21002666
    Canonical link: https://commits.webkit.org/162740.81@safari-601.1.32.2-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601.1.32.2-branch@184706 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/heap/IncrementalSweeper.cpp
+++ Source/JavaScriptCore/heap/IncrementalSweeper.cpp
@@ -101,18 +101,8 @@ bool IncrementalSweeper::sweepNextBlock()
     return m_vm->heap.sweepNextLogicallyEmptyWeakBlock();
 }
 
-void IncrementalSweeper::startSweeping(Vector<MarkedBlock*>&& blockSnapshot)
+void IncrementalSweeper::startSweeping()
 {
-    m_blocksToSweep = WTF::move(blockSnapshot);
-    scheduleTimer();
-}
-
-void IncrementalSweeper::addBlocksAndContinueSweeping(Vector<MarkedBlock*>&& blockSnapshot)
-{
-    Vector<MarkedBlock*> blocks = WTF::move(blockSnapshot);
-    m_blocksToSweep.appendVector(blocks);
-    std::sort(m_blocksToSweep.begin(), m_blocksToSweep.end());
-    m_blocksToSweep.shrink(std::unique(m_blocksToSweep.begin(), m_blocksToSweep.end()) - m_blocksToSweep.begin());
     scheduleTimer();
 }
 
@@ -134,11 +124,7 @@ void IncrementalSweeper::doWork()
 {
 }
 
-void IncrementalSweeper::startSweeping(Vector<MarkedBlock*>&&)
-{
-}
-
-void IncrementalSweeper::addBlocksAndContinueSweeping(Vector<MarkedBlock*>&&)
+void IncrementalSweeper::startSweeping()
 {
 }
 
