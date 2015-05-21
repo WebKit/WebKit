08f33a31c722e8e6d6eeb3992d80a4d9ca39a6f7
Žan Doberšek <zdobersek@igalia.com>
2015-05-21 05:55:21 +0000

    [GTK] Move MainThreadGtk, RunLoopGtk, WorkQueueGtk under Source/WTF/wtf/glib/ https://bugs.webkit.org/show_bug.cgi?id=145117
    Reviewed by Csaba Osztrogonác.
    
    Source/WTF:
    
    Move the MainThreadGtk, RunLoopGtk and WorkQueueGtk implementation files from
    Source/WTF/wtf/gtk/ to Source/WTF/wtf/glib/. These implementations are not
    GTK-specific and only depend on GLib, so they are renamed as well.
    
    There are other such implementations under Source/WTF/wtf/gobject/ (GObject
    is just a subset of GLib) that will be moved under the new directory in
    future patches.
    
    * wtf/PlatformGTK.cmake:
    * wtf/glib/MainThreadGLib.cpp: Renamed from Source/WTF/wtf/gtk/MainThreadGtk.cpp.
    (WTF::initializeMainThreadPlatform):
    (WTF::scheduleDispatchFunctionsOnMainThread):
    * wtf/glib/RunLoopGLib.cpp: Renamed from Source/WTF/wtf/gtk/RunLoopGtk.cpp.
    (WTF::RunLoop::RunLoop):
    (WTF::RunLoop::~RunLoop):
    (WTF::RunLoop::run):
    (WTF::RunLoop::stop):
    (WTF::RunLoop::wakeUp):
    (WTF::RunLoop::TimerBase::TimerBase):
    (WTF::RunLoop::TimerBase::~TimerBase):
    (WTF::RunLoop::TimerBase::start):
    (WTF::RunLoop::TimerBase::stop):
    (WTF::RunLoop::TimerBase::isActive):
    * wtf/glib/WorkQueueGLib.cpp: Renamed from Source/WTF/wtf/gtk/WorkQueueGtk.cpp.
    (WTF::WorkQueue::platformInitialize):
    (WTF::WorkQueue::platformInvalidate):
    (WTF::WorkQueue::registerSocketEventHandler):
    (WTF::WorkQueue::unregisterSocketEventHandler):
    (WTF::WorkQueue::dispatch):
    (WTF::WorkQueue::dispatchAfter):
    
    Tools:
    
    Rename the WorkQueueGtk.cpp file to WorkQueueGLib.cpp, and move it under
    Tools/TestWebKitAPI/Tests/WTF/glib/.
    
    * TestWebKitAPI/PlatformGTK.cmake:
    * TestWebKitAPI/Tests/WTF/glib/WorkQueueGLib.cpp: Renamed from Tools/TestWebKitAPI/Tests/WTF/gtk/WorkQueueGtk.cpp.
    (TestWebKitAPI::TEST):
    
    
    Canonical link: https://commits.webkit.org/163270@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@184703 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/PlatformGTK.cmake
+++ Source/WTF/wtf/PlatformGTK.cmake
@@ -2,14 +2,14 @@ set(WTF_LIBRARY_TYPE STATIC)
 set(WTF_OUTPUT_NAME WTFGTK)
 
 list(APPEND WTF_SOURCES
+    glib/MainThreadGLib.cpp
+    glib/RunLoopGLib.cpp
+    glib/WorkQueueGLib.cpp
+
     gobject/GMainLoopSource.cpp
     gobject/GRefPtr.cpp
     gobject/GThreadSafeMainLoopSource.cpp
     gobject/GlibUtilities.cpp
-
-    gtk/MainThreadGtk.cpp
-    gtk/RunLoopGtk.cpp
-    gtk/WorkQueueGtk.cpp
 )
 
 list(APPEND WTF_LIBRARIES
