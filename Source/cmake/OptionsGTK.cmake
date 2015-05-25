f79e5ebf774cd183cb435649300003519d14b49d
Carlos Garcia Campos <cgarcia@igalia.com>
2015-05-25 06:55:01 +0000

    [GTK] Bump GCC requirements to 4.9.0 https://bugs.webkit.org/show_bug.cgi?id=145211
    Reviewed by Žan Doberšek.
    
    It's required to build with IndexedDB support when using GCC,
    clang works just fine. See https://bugs.webkit.org/show_bug.cgi?id=98932.
    
    * Source/cmake/OptionsGTK.cmake:
    
    Canonical link: https://commits.webkit.org/163365@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@184849 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/OptionsGTK.cmake
+++ Source/cmake/OptionsGTK.cmake
@@ -7,6 +7,13 @@ set(PROJECT_VERSION_MICRO 1)
 set(PROJECT_VERSION ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_MICRO})
 set(WEBKITGTK_API_VERSION 4.0)
 
+# IndexedDB support requires GCC 4.9, see https://bugs.webkit.org/show_bug.cgi?id=98932.
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.9.0")
+        message(FATAL_ERROR "GCC 4.9.0 is required to build WebKitGTK+, use a newer GCC version or clang")
+    endif ()
+endif ()
+
 # Libtool library version, not to be confused with API version.
 # See http://www.gnu.org/software/libtool/manual/html_node/Libtool-versioning.html
 CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT2 44 0 7)
