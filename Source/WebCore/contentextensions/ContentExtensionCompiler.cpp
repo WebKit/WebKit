5365386ad04d3add9597526cb41b608ffa505c5d
Alex Christensen <achristensen@apple.com>
2015-05-29 21:20:48 +0000

    [Content Extensions] Decrease maxNFASize https://bugs.webkit.org/show_bug.cgi?id=145461
    Reviewed by Gavin Barraclough.
    
    * contentextensions/ContentExtensionCompiler.cpp:
    (WebCore::ContentExtensions::compileRuleList):
    Decrease maxNFASize based on analysis of memory usage and compiling and interpreting time.
    
    
    
    Canonical link: https://commits.webkit.org/163504@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@185004 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/contentextensions/ContentExtensionCompiler.cpp
+++ Source/WebCore/contentextensions/ContentExtensionCompiler.cpp
@@ -238,8 +238,9 @@ std::error_code compileRuleList(ContentExtensionCompilationClient& client, Strin
     double totalNFAToByteCodeBuildTimeStart = monotonicallyIncreasingTime();
 #endif
 
-    // FIXME: This can be tuned. More NFAs take longer to interpret, fewer use more memory and time to compile.
-    const unsigned maxNFASize = 50000;
+    // Smaller maxNFASizes risk high compiling and interpreting times from having too many DFAs,
+    // larger maxNFASizes use too much memory when compiling.
+    const unsigned maxNFASize = 30000;
     
     bool firstNFAWithoutDomainsSeen = false;
     // FIXME: Combine small NFAs to reduce the number of NFAs.
