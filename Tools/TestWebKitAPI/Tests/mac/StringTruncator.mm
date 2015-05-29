95f8bebd7b21d06655dfdf4e2537a04be4fc9875
Alexey Proskuryakov <ap@apple.com>
2015-05-29 04:03:13 +0000

    Update results of WebKit1.StringTruncator after r184965.
    * TestWebKitAPI/Tests/mac/StringTruncator.mm: (TestWebKitAPI::TEST):
    
    * TestWebKitAPI/Tests/mac/StringTruncator.mm:
    (TestWebKitAPI::TEST):
    
    
    
    Canonical link: https://commits.webkit.org/163479@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@184975 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/mac/StringTruncator.mm
+++ Tools/TestWebKitAPI/Tests/mac/StringTruncator.mm
@@ -33,8 +33,8 @@ namespace TestWebKitAPI {
 TEST(WebKit1, StringTruncator)
 {
     @autoreleasepool {
-        EXPECT_STREQ([[WebStringTruncator centerTruncateString:@"abcdefghijklmnopqrstuvwxyz" toWidth:100 withFont:[NSFont fontWithName:@"Helvetica" size:12]] UTF8String], "abcdef…tuvwxyz");
-        EXPECT_STREQ([[WebStringTruncator centerTruncateString:@"abcdefghijklmnopqrstuvwxyz" toWidth:100] UTF8String], "abcde…uvwxyz");
+        EXPECT_STREQ([[WebStringTruncator centerTruncateString:@"abcdefghijklmnopqrstuvwxyz" toWidth:100 withFont:[NSFont fontWithName:@"Helvetica" size:12]] UTF8String], "abcdefg…tuvwxyz");
+        EXPECT_STREQ([[WebStringTruncator centerTruncateString:@"abcdefghijklmnopqrstuvwxyz" toWidth:100] UTF8String], "abcdef…uvwxyz");
         EXPECT_STREQ([[WebStringTruncator rightTruncateString:@"abcdefghijklmnopqrstuvwxyz" toWidth:100 withFont:[NSFont fontWithName:@"Helvetica" size:12]] UTF8String], "abcdefghijklmno…");
         EXPECT_STREQ([[WebStringTruncator centerTruncateString:@"ābcdefghijklmnopqrstuvwxyz" toWidth:100 withFont:[NSFont fontWithName:@"Helvetica" size:12]] UTF8String], "ābcdefg…tuvwxyz");
         EXPECT_STREQ([[WebStringTruncator rightTruncateString:@"ābcdefghijklmnopqrstuvwxyz" toWidth:100 withFont:[NSFont fontWithName:@"Helvetica" size:12]] UTF8String], "ābcdefghijklmno…");
