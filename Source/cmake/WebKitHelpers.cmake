7d52b4677874148e3dfb52d37e5f7addbf2430dd
Csaba Osztrogonác <ossy@webkit.org>
2015-06-01 09:11:05 +0000

    [cmake] Suppress parentheses-equality warnings https://bugs.webkit.org/show_bug.cgi?id=145126
    Reviewed by Darin Adler.
    
    * Source/cmake/WebKitHelpers.cmake:
    
    Canonical link: https://commits.webkit.org/163544@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@185055 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/WebKitHelpers.cmake
+++ Source/cmake/WebKitHelpers.cmake
@@ -15,6 +15,11 @@ macro(WEBKIT_SET_EXTRA_COMPILER_FLAGS _target)
             set(OLD_COMPILE_FLAGS "-fPIC ${OLD_COMPILE_FLAGS}")
         endif ()
 
+        # Suppress -Wparentheses-equality warning of Clang
+        if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+            set(OLD_COMPILE_FLAGS "-Wno-parentheses-equality ${OLD_COMPILE_FLAGS}")
+        endif ()
+
         # Enable warnings by default
         if (NOT ${OPTION_IGNORECXX_WARNINGS})
             set(OLD_COMPILE_FLAGS "-Wall -Wextra -Wcast-align -Wformat-security -Wmissing-format-attribute -Wpointer-arith -Wundef -Wwrite-strings ${OLD_COMPILE_FLAGS}")
