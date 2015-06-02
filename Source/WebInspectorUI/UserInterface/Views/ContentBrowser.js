57df0f881207dfc4521539540c00ca940019111d
Joseph Pecoraro <pecoraro@apple.com>
2015-06-02 21:14:25 +0000

    Web Inspector: ⌘F no longer brings up the find-in-page bar after typing in the quick console https://bugs.webkit.org/show_bug.cgi?id=145546
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2015-06-02
    Reviewed by Timothy Hatcher.
    
    Scope ContentBrowser keyboard events within the ContentBrowser instead
    of global, where they might conflict between ContentBrowsers. The scoped
    shortcuts now no longer need to be enabled/disabled.
    
    * UserInterface/Views/ContentBrowser.js:
    (WebInspector.ContentBrowser):
    Scope the keyboard shortcuts.
    
    (WebInspector.ContentBrowser.prototype.shown):
    (WebInspector.ContentBrowser.prototype.hidden):
    No longer need to control enabling / disabling these non-global shortcuts.
    
    Canonical link: https://commits.webkit.org/163606@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@185121 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Views/ContentBrowser.js
+++ Source/WebInspectorUI/UserInterface/Views/ContentBrowser.js
@@ -42,20 +42,13 @@ WebInspector.ContentBrowser = function(element, delegate, disableBackForward)
     this._findBanner.addEventListener(WebInspector.FindBanner.Event.DidShow, this._findBannerDidShow, this);
     this._findBanner.addEventListener(WebInspector.FindBanner.Event.DidHide, this._findBannerDidHide, this);
 
-    this._findKeyboardShortcut = new WebInspector.KeyboardShortcut(WebInspector.KeyboardShortcut.Modifier.CommandOrControl, "F", this._showFindBanner.bind(this));
-    this._saveKeyboardShortcut = new WebInspector.KeyboardShortcut(WebInspector.KeyboardShortcut.Modifier.CommandOrControl, "S", this._save.bind(this));
-    this._saveAsKeyboardShortcut = new WebInspector.KeyboardShortcut(WebInspector.KeyboardShortcut.Modifier.Shift | WebInspector.KeyboardShortcut.Modifier.CommandOrControl, "S", this._saveAs.bind(this));
-
-    this._findKeyboardShortcut.disabled = true;
-    this._saveKeyboardShortcut.disabled = true;
-    this._saveAsKeyboardShortcut.disabled = true;
+    this._findKeyboardShortcut = new WebInspector.KeyboardShortcut(WebInspector.KeyboardShortcut.Modifier.CommandOrControl, "F", this._showFindBanner.bind(this), this._element);
+    this._saveKeyboardShortcut = new WebInspector.KeyboardShortcut(WebInspector.KeyboardShortcut.Modifier.CommandOrControl, "S", this._save.bind(this), this._element);
+    this._saveAsKeyboardShortcut = new WebInspector.KeyboardShortcut(WebInspector.KeyboardShortcut.Modifier.Shift | WebInspector.KeyboardShortcut.Modifier.CommandOrControl, "S", this._saveAs.bind(this), this._element);
 
     if (!disableBackForward) {
-        this._backKeyboardShortcut = new WebInspector.KeyboardShortcut(WebInspector.KeyboardShortcut.Modifier.CommandOrControl | WebInspector.KeyboardShortcut.Modifier.Control, WebInspector.KeyboardShortcut.Key.Left, this._backButtonClicked.bind(this));
-        this._forwardKeyboardShortcut = new WebInspector.KeyboardShortcut(WebInspector.KeyboardShortcut.Modifier.CommandOrControl | WebInspector.KeyboardShortcut.Modifier.Control, WebInspector.KeyboardShortcut.Key.Right, this._forwardButtonClicked.bind(this));
-
-        this._backKeyboardShortcut.disabled = true;
-        this._forwardKeyboardShortcut.disabled = true;
+        this._backKeyboardShortcut = new WebInspector.KeyboardShortcut(WebInspector.KeyboardShortcut.Modifier.CommandOrControl | WebInspector.KeyboardShortcut.Modifier.Control, WebInspector.KeyboardShortcut.Key.Left, this._backButtonClicked.bind(this), this._element);
+        this._forwardKeyboardShortcut = new WebInspector.KeyboardShortcut(WebInspector.KeyboardShortcut.Modifier.CommandOrControl | WebInspector.KeyboardShortcut.Modifier.Control, WebInspector.KeyboardShortcut.Key.Right, this._forwardButtonClicked.bind(this), this._element);
 
         this._backButtonNavigationItem = new WebInspector.ButtonNavigationItem("back", WebInspector.UIString("Back (%s)").format(this._backKeyboardShortcut.displayName), "Images/BackArrow.svg", 8, 13);
         this._backButtonNavigationItem.addEventListener(WebInspector.ButtonNavigationItem.Event.Clicked, this._backButtonClicked, this);
@@ -277,30 +270,12 @@ WebInspector.ContentBrowser.prototype = {
     {
         this._contentViewContainer.shown();
 
-        if (this._backKeyboardShortcut)
-            this._backKeyboardShortcut.disabled = false;
-        if (this._forwardKeyboardShortcut)
-            this._forwardKeyboardShortcut.disabled = false;
-
-        this._findKeyboardShortcut.disabled = false;
-        this._saveKeyboardShortcut.disabled = false;
-        this._saveAsKeyboardShortcut.disabled = false;
-
         this._findBanner.enableKeyboardShortcuts();
     },
 
     hidden: function()
     {
         this._contentViewContainer.hidden();
-
-        if (this._backKeyboardShortcut)
-            this._backKeyboardShortcut.disabled = false;
-        if (this._forwardKeyboardShortcut)
-            this._forwardKeyboardShortcut.disabled = true;
-
-        this._findKeyboardShortcut.disabled = true;
-        this._saveKeyboardShortcut.disabled = true;
-        this._saveAsKeyboardShortcut.disabled = true;
 
         this._findBanner.disableKeyboardShortcuts();
     },
