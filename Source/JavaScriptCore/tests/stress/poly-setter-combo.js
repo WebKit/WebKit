d293778d0f20892cc033551f4dc1fe0603638548
Michael Saboff <msaboff@apple.com>
2015-06-03 18:48:41 +0000

    Improve test coverage for changes made in 145527 https://bugs.webkit.org/show_bug.cgi?id=145578
    Reviewed by Geoffrey Garen.
    
    Added more complexity to poly-setter-combo.js stress test to create more turmoil in the
    polymorphic get-by-id / put-by-id with getters and setters to exercise the code change in
    https://bugs.webkit.org/show_bug.cgi?id=145527.  By changing the objects that the main test
    function sees, we are able to test those paths.  Verified with temporary logging code.
    
    * tests/stress/poly-setter-combo.js:
    (Cons2):
    (Cons3):
    (Cons4):
    (foo):
    (test):
    (runTestWithConstructors):
    
    
    Canonical link: https://commits.webkit.org/163642@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@185158 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/tests/stress/poly-setter-combo.js
+++ Source/JavaScriptCore/tests/stress/poly-setter-combo.js
@@ -3,12 +3,30 @@ function Cons1() {
 Cons1.prototype.f = 42;
 
 function Cons2() {
+    this._values = []
 }
 Cons2.prototype.__defineSetter__("f", function(value) {
     counter++;
     this._f = value;
+    this._values[value] = 1;
 });
 Cons2.prototype.__defineGetter__("f", function() { return this._f; });
+
+function Cons3() {
+}
+Cons3.prototype.f = 42;
+Cons3.prototype.g = 43;
+
+function Cons4() {
+    this._values = []
+}
+Cons4.prototype.g = 16;
+Cons4.prototype.__defineSetter__("f", function(value) {
+    counter++;
+    this._f = value;
+    this._values[value] = 1;
+});
+Cons4.prototype.__defineGetter__("f", function() { return this._f; });
 
 function foo(o, value) {
     o.f = value;
@@ -27,17 +45,33 @@ function test(o, value, expectedCount) {
         throw new Error("Bad counter value: " + counter);
 }
 
-for (var i = 0; i < 100000; ++i) {
-    test(new Cons1(), i, counter);
-    test(new Cons2(), i, counter + 1);
-    
-    var o = {};
-    o.__defineSetter__("f", function(value) {
-        this._f = value;
-        counter++;
-    });
-    o.__defineGetter__("f", function() { return this._f; });
-    test(o, i, counter + 1);
+function runTestWithConstructors(constructor1, constructor2) {
+    for (var i = 0; i < 5000; ++i) {
+        test(new constructor1(), i, counter);
+        test(new constructor2(), i, counter + 1);
 
-    test({f: 42}, i, counter);
+        var o = {};
+        o.__defineGetter__("f", function() {
+            counter++;
+            return 84;
+        });
+        test(o, 84, counter + 1);
+
+        var o = {};
+        o.__defineSetter__("f", function(value) {
+            this._f = value;
+            counter++;
+        });
+        o.__defineGetter__("f", function() { return this._f; });
+        test(o, i, counter + 1);
+
+        test({f: 42}, i, counter);
+    }
+}
+
+for (var i = 0; i < 2; ++i) {
+    runTestWithConstructors(Cons1, Cons2);
+    runTestWithConstructors(Cons3, Cons2);
+    runTestWithConstructors(Cons1, Cons4);
+    runTestWithConstructors(Cons3, Cons4);
 }
