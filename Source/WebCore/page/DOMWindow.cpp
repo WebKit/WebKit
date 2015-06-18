23c9fdf6bb67aacedba4dadd8678eadd7838c901
Joseph Pecoraro <pecoraro@apple.com>
2015-06-18 18:16:55 +0000

    Crash under WebCore::DOMWindow::dispatchMessageEventWithOriginCheck attempting to log console message https://bugs.webkit.org/show_bug.cgi?id=146093
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2015-06-18
    Reviewed by Timothy Hatcher.
    
    * page/DOMWindow.cpp:
    (WebCore::DOMWindow::dispatchMessageEventWithOriginCheck):
    The console could be null so null check its use.
    
    Canonical link: https://commits.webkit.org/164159@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@185712 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/DOMWindow.cpp
+++ Source/WebCore/page/DOMWindow.cpp
@@ -934,9 +934,10 @@ void DOMWindow::dispatchMessageEventWithOriginCheck(SecurityOrigin* intendedTarg
     if (intendedTargetOrigin) {
         // Check target origin now since the target document may have changed since the timer was scheduled.
         if (!intendedTargetOrigin->isSameSchemeHostPort(document()->securityOrigin())) {
-            String message = "Unable to post message to " + intendedTargetOrigin->toString() +
-                             ". Recipient has origin " + document()->securityOrigin()->toString() + ".\n";
-            console()->addMessage(MessageSource::Security, MessageLevel::Error, message, stackTrace);
+            if (PageConsoleClient* pageConsole = console()) {
+                String message = makeString("Unable to post message to ", intendedTargetOrigin->toString(), ". Recipient has origin ", document()->securityOrigin()->toString(), ".\n");
+                pageConsole->addMessage(MessageSource::Security, MessageLevel::Error, message, stackTrace);
+            }
             return;
         }
     }
