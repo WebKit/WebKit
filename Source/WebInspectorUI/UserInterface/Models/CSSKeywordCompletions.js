204232c01cdababefdb462f5fcff4ea739443681
Jon Lee <jonlee@apple.com>
2015-06-19 16:58:14 +0000

    Update font and font-family keyword completions https://bugs.webkit.org/show_bug.cgi?id=144558 <rdar://problem/20795292>
    Reviewed by Timothy Hatcher.
    
    * UserInterface/Models/CSSKeywordCompletions.js: Replace -webkit-system-font and
    -apple-system-font with -apple-system, which is the recommended token for getting the
    system font. Also add -title{1,2,3} for font, as they were missing, but already exist
    in font-family.
    
    
    Canonical link: https://commits.webkit.org/164193@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@185750 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Models/CSSKeywordCompletions.js
+++ Source/WebInspectorUI/UserInterface/Models/CSSKeywordCompletions.js
@@ -379,11 +379,11 @@ WebInspector.CSSKeywordCompletions._propertyKeywordMap = {
         "-webkit-control", "status-bar", "italic", "oblique", "small-caps", "normal", "bold", "bolder", "lighter",
         "100", "200", "300", "400", "500", "600", "700", "800", "900", "xx-small", "x-small", "small", "medium",
         "large", "x-large", "xx-large", "-webkit-xxx-large", "smaller", "larger", "serif", "sans-serif", "cursive",
-        "fantasy", "monospace", "-webkit-body", "-webkit-pictograph", "-webkit-system-font", "-apple-system-font",
+        "fantasy", "monospace", "-webkit-body", "-webkit-pictograph", "-apple-system",
         "-apple-system-headline", "-apple-system-body", "-apple-system-subheadline", "-apple-system-footnote",
         "-apple-system-caption1", "-apple-system-caption2", "-apple-system-short-headline", "-apple-system-short-body",
         "-apple-system-short-subheadline", "-apple-system-short-footnote", "-apple-system-short-caption1",
-        "-apple-system-tall-body"
+        "-apple-system-tall-body", "-apple-system-title1", "-apple-system-title2", "-apple-system-title3"
     ],
     "dominant-baseline": [
         "middle", "auto", "central", "text-before-edge", "text-after-edge", "ideographic", "alphabetic", "hanging",
@@ -479,7 +479,7 @@ WebInspector.CSSKeywordCompletions._propertyKeywordMap = {
     ],
     "font-family": [
         "serif", "sans-serif", "cursive", "fantasy", "monospace", "-webkit-body", "-webkit-pictograph",
-        "-webkit-system-font", "-apple-system-font", "-apple-system-headline", "-apple-system-body",
+        "-apple-system", "-apple-system-headline", "-apple-system-body",
         "-apple-system-subheadline", "-apple-system-footnote", "-apple-system-caption1", "-apple-system-caption2",
         "-apple-system-short-headline", "-apple-system-short-body", "-apple-system-short-subheadline",
         "-apple-system-short-footnote", "-apple-system-short-caption1", "-apple-system-tall-body",
