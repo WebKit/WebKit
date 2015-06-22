c86b3030f3513374648158b701ff7b9ac55b9664
Filip Pizlo <fpizlo@apple.com>
2015-06-22 17:11:54 +0000

    Run CDjs as part of JSC stress testing https://bugs.webkit.org/show_bug.cgi?id=146174
    Reviewed by Geoffrey Garen.
    
    PerformanceTests:
    
    * JetStream/cdjs/cdjs-tests.yaml: Added. This tells JSC stress tests what tests to run. It uses new syntax ("tests" being a list) that I add in this change.
    * JetStream/cdjs/main.js: Mark this as a slow test.
    * JetStream/create.rb: Don't copy the JSC stress tests artifacts into the JetStream bundle.
    
    Tools:
    
    * Scripts/run-javascriptcore-tests:
    (runJSCStressTests): Make this aware of the cdjs-tests.yaml.
    * Scripts/run-jsc-stress-tests:
    - Teach this about tests that indicate error by returning an error code while also having lots of output even when they succeed.
    - Add the ability to have "tests" be a list of tests rather than just one test. This could also be a list of directories that have tests.
    - Fix a bug with bundle copying: whether the $collection should have the basename appended depends on whether we copy into bundleDir.dirname, not on whether absoluteCollection is a directory.
    
    
    
    Canonical link: https://commits.webkit.org/164273@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@185833 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- PerformanceTests/JetStream/cdjs/main.js
+++ PerformanceTests/JetStream/cdjs/main.js
@@ -23,6 +23,9 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+// This is run as a JSC stress test. Let the harness know that this is a slow test.
+//@ slow!
+
 load("constants.js");
 load("util.js");
 load("red_black_tree.js");
--- PerformanceTests/JetStream/create.rb
+++ PerformanceTests/JetStream/create.rb
@@ -29,12 +29,32 @@ require "shellwords"
 VERSION = "1.1-alpha2"
 DIRECTORY_NAME = "JetStream-#{VERSION}"
 
+CDJS_FILES = [
+    "constants.js",
+    "util.js",
+    "red_black_tree.js",
+    "call_sign.js",
+    "vector_2d.js",
+    "vector_3d.js",
+    "motion.js",
+    "reduce_collision_set.js",
+    "simulator.js",
+    "collision.js",
+    "collision_detector.js",
+    "benchmark.js"
+]
+
 raise unless system("rm -rf " + DIRECTORY_NAME)
 raise unless system("mkdir -p " + DIRECTORY_NAME)
 raise unless system("mkdir -p #{DIRECTORY_NAME}/sunspider")
 raise unless system("mkdir -p #{DIRECTORY_NAME}/sources")
 raise unless system("cp sunspider/*.js #{DIRECTORY_NAME}/sunspider")
-raise unless system("cp -r JetStream.css JetStreamDriver.js LLVM-test-suite-LICENSE.txt simple Octane2 Octane2Setup.js SimpleSetup.js SunSpiderSetup.js Octane OctaneSetup.js CDjsSetup.js cdjs Reference.js TestingSetup.js JetStream-Logo.png JetStream-Logo@2x.png Swoosh.png Swoosh@2x.png " + DIRECTORY_NAME)
+raise unless system("mkdir -p #{DIRECTORY_NAME}/cdjs")
+CDJS_FILES.each {
+    | filename |
+    raise unless system("cp cdjs/#{filename} #{DIRECTORY_NAME}/cdjs")
+}
+raise unless system("cp -r JetStream.css JetStreamDriver.js LLVM-test-suite-LICENSE.txt simple Octane2 Octane2Setup.js SimpleSetup.js SunSpiderSetup.js Octane OctaneSetup.js CDjsSetup.js Reference.js TestingSetup.js JetStream-Logo.png JetStream-Logo@2x.png Swoosh.png Swoosh@2x.png " + DIRECTORY_NAME)
 
 def detemplatize(basename)
     File.open(DIRECTORY_NAME + "/#{basename}.html", "w") {
@@ -116,7 +136,7 @@ transferSource("code-first-load", "Octane2/code-load.js")
 transferSource("box2d", "Octane2/box2d.js")
 transferSource("zlib", "Octane2/zlib.js", "Octane2/zlib-data.js")
 transferSource("typescript", "Octane2/typescript.js", "Octane2/typescript-compiler.js", "Octane2/typescript-input.js")
-transferSource("cdjs", "cdjs/constants.js", "cdjs/util.js", "cdjs/red_black_tree.js", "cdjs/call_sign.js", "cdjs/vector_2d.js", "cdjs/vector_3d.js", "cdjs/motion.js", "cdjs/reduce_collision_set.js", "cdjs/simulator.js", "cdjs/collision.js", "cdjs/collision_detector.js", "cdjs/benchmark.js")
+transferSource("cdjs", *(CDJS_FILES.collect { | filename | "cdjs/#{filename}" }))
 
 puts "You can now run JetStream by navigating to file://" + (Pathname.new(DIRECTORY_NAME) + "index.html").realpath.to_s
 
