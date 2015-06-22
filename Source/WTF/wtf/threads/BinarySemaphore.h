9001811790c16fec9a311b9ab9766b04baeda46d
Anders Carlsson <andersca@apple.com>
2015-06-22 22:07:20 +0000

    Get rid of the Windows specific BinarySemaphore implementation https://bugs.webkit.org/show_bug.cgi?id=146216
    Reviewed by Andreas Kling.
    
    The fact that the Windows implementation uses a HEVENT internally was only useful
    to the Windows port of WebKit2; we can get rid of it now.
    
    * WTF.vcxproj/WTF.vcxproj:
    * WTF.vcxproj/WTF.vcxproj.filters:
    * wtf/threads/BinarySemaphore.h:
    (WTF::BinarySemaphore::event): Deleted.
    * wtf/threads/win/BinarySemaphoreWin.cpp: Removed.
    (WTF::BinarySemaphore::BinarySemaphore): Deleted.
    (WTF::BinarySemaphore::~BinarySemaphore): Deleted.
    (WTF::BinarySemaphore::signal): Deleted.
    (WTF::BinarySemaphore::wait): Deleted.
    
    Canonical link: https://commits.webkit.org/164287@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@185847 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/threads/BinarySemaphore.h
+++ Source/WTF/wtf/threads/BinarySemaphore.h
@@ -41,19 +41,11 @@ public:
     WTF_EXPORT_PRIVATE void signal();
     WTF_EXPORT_PRIVATE bool wait(double absoluteTime);
 
-#if PLATFORM(WIN)
-    HANDLE event() const { return m_event; }
-#endif
-
 private:
-#if PLATFORM(WIN)
-    HANDLE m_event;
-#else
     bool m_isSet;
 
     Mutex m_mutex;
     ThreadCondition m_condition;
-#endif
 };
 
 } // namespace WTF
