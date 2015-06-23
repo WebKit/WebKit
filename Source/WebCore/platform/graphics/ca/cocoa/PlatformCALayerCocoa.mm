f0062f321ea612b6d38d29337a20d611e88f0396
Dean Jackson <dino@apple.com>
2015-06-23 22:47:04 +0000

    Media controls are missing the white backdrop in UIWebViews https://bugs.webkit.org/show_bug.cgi?id=146251 <rdar://problem/20181345>
    Reviewed by Simon Fraser.
    
    Implement two new CALayer subclasses that explicitly set
    something that resembles the system appearance for
    media-controls-dark-bar-background and media-controls-light-bar-background.
    This way, WebKit1 clients get a visible result.
    
    Creating the actual system recipes is tracked by:
    https://bugs.webkit.org/show_bug.cgi?id=146250
    
    * WebCore.xcodeproj/project.pbxproj: Add new files.
    * platform/graphics/ca/cocoa/PlatformCALayerCocoa.mm:
    (PlatformCALayerCocoa::PlatformCALayerCocoa): Use the new CALayer subclasses for
    the appropriate layer types.
    * platform/graphics/ca/cocoa/WebSystemBackdropLayer.h: Added.
    * platform/graphics/ca/cocoa/WebSystemBackdropLayer.mm: Added.
    (-[WebLightSystemBackdropLayer init]):
    (-[WebLightSystemBackdropLayer setBackgroundColor:]): Only set to a light grey.
    (-[WebDarkSystemBackdropLayer init]):
    (-[WebDarkSystemBackdropLayer setBackgroundColor:]): Only set to a dark grey.
    
    Canonical link: https://commits.webkit.org/164327@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@185888 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/ca/cocoa/PlatformCALayerCocoa.mm
+++ Source/WebCore/platform/graphics/ca/cocoa/PlatformCALayerCocoa.mm
@@ -43,6 +43,7 @@
 #import "WebCoreCALayerExtras.h"
 #import "WebGLLayer.h"
 #import "WebLayer.h"
+#import "WebSystemBackdropLayer.h"
 #import "WebTiledBackingLayer.h"
 #import <AVFoundation/AVFoundation.h>
 #import <QuartzCore/QuartzCore.h>
@@ -225,16 +226,24 @@ PlatformCALayerCocoa::PlatformCALayerCocoa(LayerType layerType, PlatformCALayerC
     case LayerTypeTransformLayer:
         layerClass = [CATransformLayer class];
         break;
+#if ENABLE(FILTERS_LEVEL_2)
+    case LayerTypeBackdropLayer:
+        layerClass = [CABackdropLayer class];
+        break;
+    case LayerTypeLightSystemBackdropLayer:
+        layerClass = [WebLightSystemBackdropLayer class];
+        break;
+    case LayerTypeDarkSystemBackdropLayer:
+        layerClass = [WebDarkSystemBackdropLayer class];
+        break;
+#else
     case LayerTypeBackdropLayer:
     case LayerTypeLightSystemBackdropLayer:
     case LayerTypeDarkSystemBackdropLayer:
-#if ENABLE(FILTERS_LEVEL_2)
-        layerClass = [CABackdropLayer class];
-#else
         ASSERT_NOT_REACHED();
         layerClass = [CALayer class];
-#endif
         break;
+#endif
     case LayerTypeWebTiledLayer:
         ASSERT_NOT_REACHED();
         break;
