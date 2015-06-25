f72201ae3b91da16712d43ff0869f6a3912fe249
Jaehun Lim <ljaehun.lim@samsung.com>
2015-06-25 23:38:59 +0000

    Cleanup ENABLE_CSS3_CONDITIONAL_RULES codes. https://bugs.webkit.org/show_bug.cgi?id=146308
    Reviewed by Csaba Osztrogonác.
    
    ENABLE_CSS3_CONDITIONAL_RULES guard was removed from r174536.
    
    * Scripts/webkitperl/FeatureList.pm:
    
    
    Canonical link: https://commits.webkit.org/164407@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@185970 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitperl/FeatureList.pm
+++ Tools/Scripts/webkitperl/FeatureList.pm
@@ -53,7 +53,6 @@ my (
     $classSyntax,
     $templateLiteralSyntax,
     $cspNextSupport,
-    $css3ConditionalRulesSupport,
     $css3TextSupport,
     $css3TextLineBreakSupport,
     $css4ImagesSupport,
@@ -191,9 +190,6 @@ my @features = (
 
     { option => "css-grid-layout", desc => "Toggle CSS Grid Layout support",
       define => "ENABLE_CSS_GRID_LAYOUT", default => 1, value => \$cssGridLayoutSupport },
-
-    { option => "css3-conditional-rules", desc => "Toggle CSS3 Conditional Rules support (i.e. \@supports)",
-      define => "ENABLE_CSS3_CONDITIONAL_RULES", default => 1, value => \$css3ConditionalRulesSupport },
 
     { option => "css3-text", desc => "Toggle CSS3 Text support",
       define => "ENABLE_CSS3_TEXT", default => (isEfl() || isGtk()), value => \$css3TextSupport },
