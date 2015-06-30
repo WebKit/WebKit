7cf794afb7c553a44e99d4d86c30a08c35e65fd1
Philippe Normand <philn@igalia.com>
2015-06-30 07:17:52 +0000

    [CMake] Error when gst-plugins-base is missing is too confusing https://bugs.webkit.org/show_bug.cgi?id=145682
    Reviewed by Carlos Garcia Campos.
    
    * Source/cmake/OptionsGTK.cmake: Error out if the required
    GStreamer libraries are not found on the host.
    
    Canonical link: https://commits.webkit.org/164540@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@186110 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/OptionsGTK.cmake
+++ Source/cmake/OptionsGTK.cmake
@@ -354,17 +354,30 @@ endif ()
 
 if (ENABLE_VIDEO OR ENABLE_WEB_AUDIO)
     set(GSTREAMER_COMPONENTS app pbutils)
-    SET_AND_EXPOSE_TO_BUILD(USE_GSTREAMER TRUE)
+
     if (ENABLE_VIDEO)
         list(APPEND GSTREAMER_COMPONENTS video mpegts tag gl)
     endif ()
 
     if (ENABLE_WEB_AUDIO)
         list(APPEND GSTREAMER_COMPONENTS audio fft)
-        SET_AND_EXPOSE_TO_BUILD(USE_WEBAUDIO_GSTREAMER TRUE)
     endif ()
 
     find_package(GStreamer 1.0.3 REQUIRED COMPONENTS ${GSTREAMER_COMPONENTS})
+
+    if (ENABLE_WEB_AUDIO)
+        if (NOT PC_GSTREAMER_AUDIO_FOUND OR NOT PC_GSTREAMER_FFT_FOUND)
+            message(FATAL_ERROR "WebAudio requires the audio and fft GStreamer libraries. Please check your gst-plugins-base installation.")
+        else ()
+            SET_AND_EXPOSE_TO_BUILD(USE_WEBAUDIO_GSTREAMER TRUE)
+        endif ()
+    endif ()
+
+    if (ENABLE_VIDEO)
+        if (NOT PC_GSTREAMER_APP_FOUND OR NOT PC_GSTREAMER_PBUTILS_FOUND OR NOT PC_GSTREAMER_TAG_FOUND OR NOT PC_GSTREAMER_VIDEO_FOUND)
+            message(FATAL_ERROR "Video playback requires the following GStreamer libraries: app, pbutils, tag, video. Please check your gst-plugins-base installation.")
+        endif ()
+    endif ()
 
     if (USE_GSTREAMER_MPEGTS)
         if (NOT PC_GSTREAMER_MPEGTS_FOUND)
@@ -377,6 +390,8 @@ if (ENABLE_VIDEO OR ENABLE_WEB_AUDIO)
             message(FATAL_ERROR "GStreamerGL is needed for USE_GSTREAMER_GL.")
         endif ()
     endif ()
+
+    SET_AND_EXPOSE_TO_BUILD(USE_GSTREAMER TRUE)
 endif ()
 
 if (ENABLE_X11_TARGET)
