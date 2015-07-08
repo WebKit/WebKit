19d711703b54857a126846178af9d4bdef4387f3
Csaba Osztrogonác <ossy@webkit.org>
2015-07-08 21:38:10 +0000

    Add new sources to StyleAllInOne.cpp https://bugs.webkit.org/show_bug.cgi?id=146584
    Patch by Csaba Osztrogonác <ossy@webkit.org> on 2015-07-08
    Reviewed by Brent Fulgham.
    
    * WebCore.vcxproj/WebCore.vcxproj:
    * rendering/style/StyleAllInOne.cpp:
    
    
    Canonical link: https://commits.webkit.org/164822@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@186528 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/style/StyleAllInOne.cpp
+++ Source/WebCore/rendering/style/StyleAllInOne.cpp
@@ -25,9 +25,11 @@
 
 // This all-in-one cpp file cuts down on template bloat to allow us to build our Windows release build.
 
+#include "BasicShapes.cpp"
 #include "ContentData.cpp"
 #include "CounterDirectives.cpp"
 #include "FillLayer.cpp"
+#include "GridResolvedPosition.cpp"
 #include "KeyframeList.cpp"
 #include "NinePieceImage.cpp"
 #include "QuotesData.cpp"
@@ -35,6 +37,7 @@
 #include "SVGRenderStyle.cpp"
 #include "SVGRenderStyleDefs.cpp"
 #include "ShadowData.cpp"
+#include "ShapeValue.cpp"
 #include "StyleBackgroundData.cpp"
 #include "StyleBoxData.cpp"
 #include "StyleCachedImage.cpp"
@@ -43,20 +46,14 @@
 #include "StyleFilterData.cpp"
 #include "StyleFlexibleBoxData.cpp"
 #include "StyleGeneratedImage.cpp"
+#include "StyleGridData.cpp"
+#include "StyleGridItemData.cpp"
 #include "StyleInheritedData.cpp"
 #include "StyleMarqueeData.cpp"
 #include "StyleMultiColData.cpp"
 #include "StyleRareInheritedData.cpp"
 #include "StyleRareNonInheritedData.cpp"
+#include "StyleScrollSnapPoints.cpp"
 #include "StyleSurroundData.cpp"
 #include "StyleTransformData.cpp"
 #include "StyleVisualData.cpp"
-
-#if ENABLE(CSS_GRID_LAYOUT)
-#include "StyleGridData.cpp"
-#include "StyleGridItemData.cpp"
-#endif
-
-#if ENABLE(CSS_SCROLL_SNAP)
-#include "StyleScrollSnapPoints.cpp"
-#endif
