af265d16bd398cf796d16142964839989e2fe784
Antti Koivisto <antti@apple.com>
2015-07-14 19:50:37 +0000

    REGRESSION (r177876): store.apple.com profile and cart icons are missing https://bugs.webkit.org/show_bug.cgi?id=146894
    Patch by Antti Koivisto <antti@apple.com> on 2015-07-14
    Reviewed by Myles C. Maxfield.
    
    Source/WebCore:
    
    If we had several @font-face rules for the same face covering overlapping ranges we
    would only try to get glyphs from the first one. If it didn't have the glyph we wouldn't
    try the other ones.
    
    Test: fast/css/font-face-multiple-missing-glyphs.html
    
    * platform/graphics/FontCascadeFonts.cpp:
    (WebCore::FontCascadeFonts::glyphDataForVariant):
    (WebCore::FontCascadeFonts::glyphDataForNormalVariant):
    
        Get GlyphData with one call.
    
    * platform/graphics/FontRanges.cpp:
    (WebCore::FontRanges::~FontRanges):
    (WebCore::FontRanges::glyphDataForCharacter):
    
        Check that we actually have a glyph in the font for the character before returning it.
    
    (WebCore::FontRanges::fontForCharacter):
    (WebCore::FontRanges::fontForFirstRange):
    * platform/graphics/FontRanges.h:
    (WebCore::FontRanges::size):
    (WebCore::FontRanges::rangeAt):
    
    LayoutTests:
    
    * fast/css/font-face-multiple-missing-glyphs-expected.html: Added.
    * fast/css/font-face-multiple-missing-glyphs.html: Added.
    
    Canonical link: https://commits.webkit.org/164998@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@186809 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/SimpleLineLayout.cpp
+++ Source/WebCore/rendering/SimpleLineLayout.cpp
@@ -82,16 +82,17 @@ static bool canUseForText(const CharacterType* text, unsigned length, const Font
     }
     return true;
 }
-
+/*
 static bool canUseForText(const RenderText& textRenderer, const Font& font)
 {
     if (textRenderer.is8Bit())
         return canUseForText(textRenderer.characters8(), textRenderer.textLength(), font);
     return canUseForText(textRenderer.characters16(), textRenderer.textLength(), font);
 }
-
-bool canUseFor(const RenderBlockFlow& flow)
+*/
+bool canUseFor(const RenderBlockFlow&)
 {
+    return false; /*
     if (!flow.frame().settings().simpleLineLayoutEnabled())
         return false;
     if (!flow.firstChild())
@@ -211,6 +212,7 @@ bool canUseFor(const RenderBlockFlow& flow)
             return false;
     }
     return true;
+    */
 }
 
 static float computeLineLeft(ETextAlign textAlign, float availableWidth, float committedWidth, float logicalLeftOffset)
