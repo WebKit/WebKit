461dcab7617abfa23f8653e98b70ce451059eaa0
Simon Fraser <simon.fraser@apple.com>
2015-07-16 21:50:50 +0000

    [iOS] Expose contentsSizeRespectingOverflow() via WebView so UIWebView can use it https://bugs.webkit.org/show_bug.cgi?id=146924 WebKit part of rdar://problem/21802456
    Reviewed by Tim Horton.
    
    Expose the FrameView's contentsSizeRespectingOverflow() via WebView, for use
    by UIKit.
    
    * WebView/WebView.mm:
    (-[WebView _contentsSizeRespectingOverflow]):
    * WebView/WebViewPrivate.h:
    
    Canonical link: https://commits.webkit.org/165073@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@186909 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/mac/WebView/WebViewPrivate.h
+++ Source/WebKit/mac/WebView/WebViewPrivate.h
@@ -442,6 +442,7 @@ Could be worth adding to the API.
 - (DOMCSSStyleDeclaration *)styleAtSelectionStart;
 
 - (NSUInteger)_renderTreeSize;
+- (NSSize)_contentsSizeRespectingOverflow;
 
 /*!
  * @method _handleMemoryWarning
