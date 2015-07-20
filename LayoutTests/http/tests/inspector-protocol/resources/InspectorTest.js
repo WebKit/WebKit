ee4a34f8dec1c9510ff0f75718f4a1c56241e62b
Brian Burg <burg@cs.washington.edu>
2015-07-20 23:35:51 +0000

    Web Inspector: start using Promises to handle asynchronous steps in protocol tests https://bugs.webkit.org/show_bug.cgi?id=147096
    Reviewed by Joseph Pecoraro.
    
    Existing protocol tests use lots of reinvented wheels to run
    multiple asynchronous tests. This patch begins consolidation of
    these redundant wheels, using a tiny test framework based on
    promises.
    
    The first change is to introduce InspectorTest.awaitCommand,
    which returns a promise that is fulfilled or rejected if the command
    succeeds or fails. This is the building block for writing protocol
    tests with promises.
    
    The second change is the introduction of InspectorTest.AsyncTestSuite.
    This properly chains together multiple asynchronous test methods
    using the promise interface. A synchronous version is also added.
    
    To demonstrate how async tests can be written using this new approach,
    this patch converts existing protocol test. Test coverage for the
    new frameworks also demonstrates their use.
    
    * http/tests/inspector-protocol/resources/InspectorTest.js:
    (InspectorTest.sendCommand):
    (InspectorTest.awaitCommand):
    (InspectorTest.sendMessage):
    (InspectorFrontendAPI.dispatchMessageAsync):
    (InspectorTest.AsyncTestSuite): Add tests for this micro framework.
    (InspectorTest.AsyncTestSuite.prototype.get passCount):
    (InspectorTest.AsyncTestSuite.prototype.get skipCount):
    (InspectorTest.AsyncTestSuite.prototype.addTestCase):
    (InspectorTest.AsyncTestSuite.prototype.runTestCasesAndFinish.finish):
    (InspectorTest.AsyncTestSuite.prototype.runTestCasesAndFinish):
    (InspectorTest.AsyncTestSuite.prototype.runTestCases):
    (InspectorTest.SyncTestSuite): Add tests for this micro framework.
    (InspectorTest.SyncTestSuite.prototype.get passCount):
    (InspectorTest.SyncTestSuite.prototype.get skipCount):
    (InspectorTest.SyncTestSuite.prototype.addTestCase):
    (InspectorTest.SyncTestSuite.prototype.runTestCasesAndFinish):
    (InspectorTest.SyncTestSuite.prototype.runTestCases):
    (InspectorTest.log):
    (InspectorTest.assert):
    * inspector-protocol/runtime/getProperties-expected.txt:
    * inspector-protocol/runtime/getProperties.html: Rewrite.
    
    Canonical link: https://commits.webkit.org/165171@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187046 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/inspector-protocol/resources/InspectorTest.js
+++ LayoutTests/http/tests/inspector-protocol/resources/InspectorTest.js
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012 Samsung Electronics. All rights reserved.
- * Copyright (C) 2014 Apple Inc. All rights reserved.
+ * Copyright (C) 2014, 2015 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -26,51 +26,76 @@
 InspectorFrontendAPI = {};
 
 InspectorTest = {};
-InspectorTest.dumpInspectorProtocolMessages = false;
 InspectorTest._dispatchTable = [];
 InspectorTest._requestId = -1;
 InspectorTest.eventHandler = {};
 
-/**
- * @param {string} method
- * @param {object} params
- * @param {function({object} messageObject)=} handler
- */
+InspectorTest.dumpInspectorProtocolMessages = false;
+InspectorTest.forceSyncDebugLogging = false;
+
 InspectorTest.sendCommand = function(method, params, handler)
 {
     this._dispatchTable[++this._requestId] = handler;
+    var messageObject = {method, params, "id": this._requestId};
+    this.sendMessage(messageObject);
 
-    var messageObject = { "method": method,
-                          "params": params,
-                          "id": this._requestId };
+    return this._requestId;
+}
 
+InspectorTest.awaitCommand = function(args)
+{
+    var {method, params} = args;
+    return new Promise(function(resolve, reject) {
+        this._dispatchTable[++this._requestId] = {resolve, reject};
+        var messageObject = {method, params, "id": this._requestId};
+        this.sendMessage(messageObject);
+    }.bind(this));
+}
+
+InspectorTest.sendMessage = function(messageObject)
+{
     // This matches the debug dumping in InspectorBackend, which is bypassed
     // by InspectorTest. Return messages should be dumped by InspectorBackend.
     if (this.dumpInspectorProtocolMessages)
         console.log("frontend: " + JSON.stringify(messageObject));
 
     InspectorFrontendHost.sendMessageToBackend(JSON.stringify(messageObject));
-
-    return this._requestId;
 }
 
-/**
- * @param {object} messageObject
- */
 InspectorFrontendAPI.dispatchMessageAsync = function(messageObject)
 {
+    // If the message has an id, then it is a reply to a command.
     var messageId = messageObject["id"];
     if (typeof messageId === "number") {
         var handler = InspectorTest._dispatchTable[messageId];
-        if (handler && typeof handler === "function")
+        if (!handler)
+            return;
+
+        if (typeof handler === "function")
             handler(messageObject);
+        else if (typeof handler === "object") {
+            var {resolve, reject} = handler;
+            if ("error" in messageObject)
+                reject(messageObject.error.message);
+            else
+                resolve(messageObject.result);
+        }
+    // Otherwise, it is an event.
     } else {
         var eventName = messageObject["method"];
-        var eventHandler = InspectorTest.eventHandler[eventName];
-        if (eventHandler)
-            eventHandler(messageObject);
-        else if (InspectorTest.defaultEventHandler)
-            InspectorTest.defaultEventHandler(messageObject);
+        var handler = InspectorTest.eventHandler[eventName];
+        if (!handler)
+            return;
+
+        if (typeof handler == "function")
+            handler(messageObject);
+        else if (typeof handler === "object") {
+            var {resolve, reject} = handler;
+            if ("error" in messageObject)
+                reject(messageObject.error.message);
+            else
+                resolve(messageObject.result);
+        }
     }
 }
 
@@ -100,31 +125,200 @@ InspectorTest.addEventListener = function(eventName, callback, capture)
     };
 }
 
-/**
-* Logs message to document.
-* @param {string} message
-*/
-InspectorTest.log = function(message)
-{
-    this.sendCommand("Runtime.evaluate", { "expression": "log(" + JSON.stringify(message) + ")" } );
+InspectorTest.AsyncTestSuite = class AsyncTestSuite {
+    constructor(name) {
+        if (!name || typeof name !== "string")
+            throw new Error("Tried to create AsyncTestSuite without string suite name.");
+
+        this.name = name;
+
+        this.testcases = [];
+        this.runCount = 0;
+        this.failCount = 0;
+    }
+
+    get passCount()
+    {
+        return this.runCount - this.failCount;
+    }
+
+    get skipCount()
+    {
+        if (this.failCount)
+            return this.testcases.length - this.runCount;
+        else
+            return 0;
+    }
+
+    addTestCase(testcase)
+    {
+        if (!testcase || !(testcase instanceof Object))
+            throw new Error("Tried to add non-object test case.");
+
+        if (typeof testcase.name !== "string")
+            throw new Error("Tried to add test case without a name.");
+
+        if (typeof testcase.test !== "function")
+            throw new Error("Tried to add test case without `test` function.");
+
+        this.testcases.push(testcase);
+    }
+
+    // Use this if the test file only has one suite, and no handling
+    // of the promise returned by runTestCases() is needed.
+    runTestCasesAndFinish()
+    {
+        function finish() {
+            InspectorTest.completeTest();
+        }
+
+        this.runTestCases()
+            .then(finish)
+            .catch(finish);
+    }
+
+    runTestCases()
+    {
+        if (!this.testcases.length)
+            throw new Error("Tried to call runTestCases() for suite with no test cases");
+        if (this._startedRunning)
+            throw new Error("Tried to call runTestCases() more than once.");
+
+        this._startedRunning = true;
+
+        InspectorTest.log("Running test suite: " + this.name);
+
+        var suite = this;
+        var result = this.testcases.reduce(function(chain, testcase) {
+            return chain.then(function() {
+                InspectorTest.log("Running test case: " + testcase.name);
+                suite.runCount++;
+                return new Promise(testcase.test);
+            });
+        }, Promise.resolve());
+
+        return result.catch(function(e) {
+            suite.failCount++;
+            var message = e;
+            if (e instanceof Error)
+                message = e.message;
+
+            if (typeof message !== "string")
+                message = JSON.stringify(message);
+
+            InspectorTest.log("EXCEPTION: " + message);
+            throw e; // Reject this promise by re-throwing the error.
+        });
+    }
 }
 
-/**
-* Logs an assert message to document.
-* @param {boolean} condition
-* @param {string} message
-*/
+InspectorTest.SyncTestSuite = class SyncTestSuite {
+    constructor(name) {
+        if (!name || typeof name !== "string")
+            throw new Error("Tried to create SyncTestSuite without string suite name.");
+
+        this.name = name;
+
+        this.testcases = [];
+        this.runCount = 0;
+        this.failCount = 0;
+    }
+
+    get passCount()
+    {
+        return this.runCount - this.failCount;
+    }
+
+    get skipCount()
+    {
+        if (this.failCount)
+            return this.testcases.length - this.runCount;
+        else
+            return 0;
+    }
+
+    addTestCase(testcase)
+    {
+        if (!testcase || !(testcase instanceof Object))
+            throw new Error("Tried to add non-object test case.");
+
+        if (typeof testcase.name !== "string")
+            throw new Error("Tried to add test case without a name.");
+
+        if (typeof testcase.test !== "function")
+            throw new Error("Tried to add test case without `test` function.");
+
+        this.testcases.push(testcase);
+    }
+
+    // Use this if the test file only has one suite.
+    runTestCasesAndFinish()
+    {
+        this.runTestCases();
+        InspectorTest.completeTest();
+    }
+
+    runTestCases()
+    {
+        if (!this.testcases.length)
+            throw new Error("Tried to call runTestCases() for suite with no test cases");
+        if (this._startedRunning)
+            throw new Error("Tried to call runTestCases() more than once.");
+
+        this._startedRunning = true;
+
+        InspectorTest.log("Running test suite: " + this.name);
+
+        var suite = this;
+        for (var testcase of this.testcases) {
+            InspectorTest.log("Running test case: " + testcase.name);
+            suite.runCount++;
+            try {
+                var result = testcase.test.call(null);
+                if (result === false) {
+                    suite.failCount++;
+                    return false;
+                }
+            } catch (e) {
+                suite.failCount++;
+                var message = e;
+                if (e instanceof Error)
+                    message = e.message;
+                else
+                    e = new Error(e);
+
+                if (typeof message !== "string")
+                    message = JSON.stringify(message);
+
+                InspectorTest.log("EXCEPTION: " + message);
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Logs a message to test document.
+InspectorTest.log = function(message)
+{
+    if (this.forceSyncDebugLogging)
+        this.debugLog(message);
+    else
+        this.sendCommand("Runtime.evaluate", { "expression": "log(" + JSON.stringify(message) + ")" } );
+}
+
+// Logs an assertion result to the test document.
 InspectorTest.assert = function(condition, message)
 {
     var status = condition ? "PASS" : "FAIL";
-    this.sendCommand("Runtime.evaluate", { "expression": "log(" + JSON.stringify(status + ": " + message) + ")" } );
+    var message = typeof message !== "string" ? JSON.stringify(message) : message;
+    var formattedMessage = status + ": " + message;
+    this.log(formattedMessage);
 }
 
-/**
-* Logs message directly to process stdout via alert function (hopefully followed by flush call).
-* This message should survive process crash or kill by timeout.
-* @param {string} message
-*/
+// Logs message a directly to stdout of the test process via alert function.
+// This message should survive process crash or kill by timeout.
 InspectorTest.debugLog = function(message)
 {
     this.sendCommand("Runtime.evaluate", { "expression": "debugLog(" + JSON.stringify(message) + ")" } );
@@ -144,9 +338,6 @@ InspectorTest.checkForError = function(responseObject)
     }
 }
 
-/**
- * @param {string} scriptName
- */
 InspectorTest.importScript = function(scriptName)
 {
     var xhr = new XMLHttpRequest();
