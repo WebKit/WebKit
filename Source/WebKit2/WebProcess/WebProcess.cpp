d1d0f1c693faa436d1e69d608cb6ce3df48724cc
Csaba Osztrogonác <ossy@webkit.org>
2015-07-20 19:50:54 +0000

    Fix the !ENABLE(VIDEO) build after r186396 https://bugs.webkit.org/show_bug.cgi?id=147116
    Reviewed by Brent Fulgham.
    
    * WebProcess/WebProcess.cpp:
    (WebKit::WebProcess::didClose):
    
    
    Canonical link: https://commits.webkit.org/165157@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187029 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/WebProcess.cpp
+++ Source/WebKit2/WebProcess/WebProcess.cpp
@@ -657,10 +657,12 @@ void WebProcess::didClose(IPC::Connection&)
     GCController::singleton().garbageCollectSoon();
     FontCache::singleton().invalidate();
     MemoryCache::singleton().setDisabled(true);
-#endif    
+#endif
 
+#if ENABLE(VIDEO)
     // FIXME(146657): This explicit media stop command should not be necessary
     PlatformMediaSessionManager::sharedManager().stopAllMediaPlaybackForProcess();
+#endif
 
     // The UI process closed this connection, shut down.
     stopRunLoop();
