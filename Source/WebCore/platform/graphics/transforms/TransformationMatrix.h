65569d887344fc24a0fab81c7b689ffd05e675e3
David Kilzer <ddkilzer@webkit.org>
2015-07-20 21:20:34 +0000

    Unexpected ABI diffference between armv7 and armv7s in WebCore::GraphicsLayerCA::~GraphicsLayerCA() <http://webkit.org/b/147101>
    Reviewed by Michael Saboff.
    
    * platform/graphics/transforms/TransformationMatrix.h: Align
    Matrix4 on both armv7 and armv7s.
    
    
    Canonical link: https://commits.webkit.org/165162@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187034 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/transforms/TransformationMatrix.h
+++ Source/WebCore/platform/graphics/transforms/TransformationMatrix.h
@@ -65,7 +65,7 @@ class TransformationMatrix {
     WTF_MAKE_FAST_ALLOCATED;
 public:
 
-#if CPU(APPLE_ARMV7S) || defined(TRANSFORMATION_MATRIX_USE_X86_64_SSE2)
+#if (PLATFORM(IOS) && CPU(ARM_THUMB2)) || defined(TRANSFORMATION_MATRIX_USE_X86_64_SSE2)
 #if COMPILER(MSVC)
     __declspec(align(16)) typedef double Matrix4[4][4];
 #else
