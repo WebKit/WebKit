ab427703872698ed33772660b19faa8903c6dcd9
Julien Brianceau <julien.brianceau@gmail.com>
2015-07-20 07:19:11 +0000

    Use isnan from std namespace in Stopwatch.h. https://bugs.webkit.org/show_bug.cgi?id=146911
    Reviewed by Žan Doberšek.
    
    * wtf/Stopwatch.h:
    (WTF::Stopwatch::isActive):
    (WTF::Stopwatch::start):
    (WTF::Stopwatch::stop):
    (WTF::Stopwatch::elapsedTime):
    
    
    
    Canonical link: https://commits.webkit.org/165147@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187019 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/Stopwatch.h
+++ Source/WTF/wtf/Stopwatch.h
@@ -44,7 +44,7 @@ public:
 
     double elapsedTime();
 
-    bool isActive() const { return !isnan(m_lastStartTime); }
+    bool isActive() const { return !std::isnan(m_lastStartTime); }
 private:
     Stopwatch() { reset(); }
 
@@ -60,14 +60,14 @@ inline void Stopwatch::reset()
 
 inline void Stopwatch::start()
 {
-    ASSERT_WITH_MESSAGE(isnan(m_lastStartTime), "Tried to start the stopwatch, but it is already running.");
+    ASSERT_WITH_MESSAGE(std::isnan(m_lastStartTime), "Tried to start the stopwatch, but it is already running.");
 
     m_lastStartTime = monotonicallyIncreasingTime();
 }
 
 inline void Stopwatch::stop()
 {
-    ASSERT_WITH_MESSAGE(!isnan(m_lastStartTime), "Tried to stop the stopwatch, but it is not running.");
+    ASSERT_WITH_MESSAGE(!std::isnan(m_lastStartTime), "Tried to stop the stopwatch, but it is not running.");
 
     m_elapsedTime += monotonicallyIncreasingTime() - m_lastStartTime;
     m_lastStartTime = NAN;
@@ -75,7 +75,7 @@ inline void Stopwatch::stop()
 
 inline double Stopwatch::elapsedTime()
 {
-    bool shouldSuspend = !isnan(m_lastStartTime);
+    bool shouldSuspend = !std::isnan(m_lastStartTime);
     if (shouldSuspend)
         stop();
 
