2e02ea8996503759e83646f4c27c26c761cb7e46
Matt Hanson <matthew_hanson@apple.com>
2015-07-21 04:37:55 +0000

    Merge r187025. rdar://problem/21878275
    Canonical link: https://commits.webkit.org/164912.77@safari-601.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601.1-branch@187079 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/webaudio/AudioContext.cpp
+++ Source/WebCore/Modules/webaudio/AudioContext.cpp
@@ -1069,7 +1069,12 @@ void AudioContext::pageMutedStateDidChange()
 
 void AudioContext::isPlayingAudioDidChange()
 {
-    document()->updateIsPlayingMedia();
+    // Make sure to call Document::updateIsPlayingMedia() on the main thread, since
+    // we could be on the audio I/O thread here and the call into WebCore could block.
+    RefPtr<AudioContext> strongThis(this);
+    callOnMainThread([strongThis] {
+        strongThis->document()->updateIsPlayingMedia();
+    });
 }
 
 void AudioContext::fireCompletionEvent()
