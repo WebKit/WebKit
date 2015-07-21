d367546093f22f69539e93da786ebeac7d71aafe
Matt Hanson <matthew_hanson@apple.com>
2015-07-21 05:27:55 +0000

    Merge r187053. rdar://problem/21778212
    Canonical link: https://commits.webkit.org/164912.90@safari-601.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601.1-branch@187093 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/accessibility/AccessibilityRenderObject.cpp
+++ Source/WebCore/accessibility/AccessibilityRenderObject.cpp
@@ -1683,9 +1683,15 @@ void AccessibilityRenderObject::setFocused(bool on)
     if (document->focusedElement() == node)
         document->setFocusedElement(nullptr);
 
-    axObjectCache()->setIsSynchronizingSelection(true);
+    // When a node is told to set focus, that can cause it to be deallocated, which means that doing
+    // anything else inside this object will crash. To fix this, we added a RefPtr to protect this object
+    // long enough for duration. We can also locally cache the axObjectCache.
+    RefPtr<AccessibilityObject> protect(this);
+    AXObjectCache* cache = axObjectCache();
+    
+    cache->setIsSynchronizingSelection(true);
     downcast<Element>(*node).focus();
-    axObjectCache()->setIsSynchronizingSelection(false);
+    cache->setIsSynchronizingSelection(false);
 }
 
 void AccessibilityRenderObject::setSelectedRows(AccessibilityChildrenVector& selectedRows)
