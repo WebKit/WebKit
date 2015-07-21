fca61853ac3dc61c2362241252e95e450b07cde0
Matt Hanson <matthew_hanson@apple.com>
2015-07-21 05:27:45 +0000

    Merge r187039. rdar://problem/21474317
    Canonical link: https://commits.webkit.org/164912.88@safari-601.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601.1-branch@187091 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -1732,7 +1732,8 @@ PassRefPtr<WebImage> WebPage::snapshotAtSize(const IntRect& rect, const IntSize&
 
     auto graphicsContext = snapshot->bitmap()->createGraphicsContext();
 
-    Color backgroundColor = coreFrame->settings().backgroundShouldExtendBeyondPage() ? frameView->documentBackgroundColor() : frameView->baseBackgroundColor();
+    Color documentBackgroundColor = frameView->documentBackgroundColor();
+    Color backgroundColor = (coreFrame->settings().backgroundShouldExtendBeyondPage() && documentBackgroundColor.isValid()) ? documentBackgroundColor : frameView->baseBackgroundColor();
     graphicsContext->fillRect(IntRect(IntPoint(), bitmapSize), backgroundColor, ColorSpaceDeviceRGB);
 
     if (!(options & SnapshotOptionsExcludeDeviceScaleFactor)) {
