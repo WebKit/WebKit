b9e89d8e80a49122f1dddfbddb570e68c42949ea
Joseph Pecoraro <pecoraro@apple.com>
2015-07-23 04:45:52 +0000

    Web Inspector: Timeline should immediately start moving play head when starting a new recording https://bugs.webkit.org/show_bug.cgi?id=147210
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2015-07-22
    Reviewed by Timothy Hatcher.
    
    Source/JavaScriptCore:
    
    * inspector/protocol/Timeline.json:
    Add timestamps to recordingStarted and recordingStopped events.
    
    Source/WebCore:
    
    Test: inspector/timeline/recording-start-stop-timestamps.html
    
    * inspector/InspectorTimelineAgent.cpp:
    (WebCore::InspectorTimelineAgent::internalStart):
    (WebCore::InspectorTimelineAgent::internalStop):
    Include the current timestamp when starting / stopping a recording.
    
    Source/WebInspectorUI:
    
    * UserInterface/Protocol/TimelineObserver.js:
    (WebInspector.TimelineObserver.prototype.recordingStarted):
    (WebInspector.TimelineObserver.prototype.recordingStopped):
    Pass on the new timestamps.
    
    * UserInterface/Controllers/TimelineManager.js:
    (WebInspector.TimelineManager.prototype.capturingStarted):
    (WebInspector.TimelineManager.prototype.capturingStopped):
    Pass on the new timestamps in the events.
    
    * UserInterface/Views/TimelineRecordingContentView.js:
    (WebInspector.TimelineRecordingContentView.prototype._capturingStarted):
    Pass on the new timestamp from the event as it can be used to update the currentTime.
    
    (WebInspector.TimelineRecordingContentView.prototype._startUpdatingCurrentTime):
    If provided a startTime we can use that as the new currentTime. Otherwise fallback
    to determining a good currentTime from the next incoming record for legacy backends.
    
    (WebInspector.TimelineRecordingContentView.prototype._recordingTimesUpdated):
    Update the comment, as there is now a solution but this is required for legacy backends.
    
    LayoutTests:
    
    * inspector/timeline/recording-start-stop-timestamps-expected.txt: Added.
    * inspector/timeline/recording-start-stop-timestamps.html: Added.
    Add a test for Timeline.recordingStarted and Timeline.recordingStopped events.
    
    Canonical link: https://commits.webkit.org/165274@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187211 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Protocol/TimelineObserver.js
+++ Source/WebInspectorUI/UserInterface/Protocol/TimelineObserver.js
@@ -32,13 +32,13 @@ WebInspector.TimelineObserver = class TimelineObserver
         WebInspector.timelineManager.eventRecorded(record);
     }
 
-    recordingStarted()
+    recordingStarted(startTime)
     {
-        WebInspector.timelineManager.capturingStarted();
+        WebInspector.timelineManager.capturingStarted(startTime);
     }
 
-    recordingStopped()
+    recordingStopped(endTime)
     {
-        WebInspector.timelineManager.capturingStopped();
+        WebInspector.timelineManager.capturingStopped(endTime);
     }
 };
