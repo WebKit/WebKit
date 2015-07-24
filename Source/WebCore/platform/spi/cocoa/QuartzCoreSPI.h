d44b92639085a8df65a7121a30d58e89e2eddbf9
Matt Hanson <matthew_hanson@apple.com>
2015-07-24 07:54:33 +0000

    Merge r187215. rdar://problem/21032083
    Canonical link: https://commits.webkit.org/164912.140@safari-601.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601.1-branch@187320 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/spi/cocoa/QuartzCoreSPI.h
+++ Source/WebCore/platform/spi/cocoa/QuartzCoreSPI.h
@@ -32,6 +32,7 @@
 #include <QuartzCore/CARenderServer.h>
 
 #ifdef __OBJC__
+#import <QuartzCore/CADisplay.h>
 #import <QuartzCore/CALayerHost.h>
 #import <QuartzCore/CALayerPrivate.h>
 
@@ -149,7 +150,7 @@ EXTERN_C void CARenderServerCaptureLayerWithTransform(mach_port_t serverPort, ui
                                                       uint32_t slotId, int32_t ox, int32_t oy, const CATransform3D *);
 
 #if USE(IOSURFACE)
-EXTERN_C void CARenderServerRenderLayerWithTransform(mach_port_t server_port, uint32_t client_id, uint64_t layer_id, IOSurfaceRef iosurface, int32_t ox, int32_t oy, const CATransform3D *matrix);
+EXTERN_C void CARenderServerRenderDisplayLayerWithTransformAndTimeOffset(mach_port_t server_port, CFStringRef display_name, uint32_t client_id, uint64_t layer_id, IOSurfaceRef iosurface, int32_t ox, int32_t oy, const CATransform3D *matrix, CFTimeInterval offset);
 #endif
 
 
