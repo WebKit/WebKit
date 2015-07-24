652be8b3d807d8bc6c5c6089006e8981281e8ebf
Lucas Forschler <lforschler@apple.com>
2015-07-24 06:33:53 +0000

    Merged r187125.  rdar://problem/21847618
    Canonical link: https://commits.webkit.org/164912.121@safari-601.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601.1-branch@187301 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/heap/GCLogging.h
+++ Source/JavaScriptCore/heap/GCLogging.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Apple Inc. All rights reserved.
+ * Copyright (C) 2014, 2015 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -47,5 +47,13 @@ public:
 typedef GCLogging::Level gcLogLevel;
 
 } // namespace JSC
+
+namespace WTF {
+
+class PrintStream;
+
+void printInternal(PrintStream&, JSC::GCLogging::Level);
+
+} // namespace WTF
 
 #endif // GCLogging_h
