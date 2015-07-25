13b874173d5c79728735324b87da28b2563a80c4
Tim Horton <timothy_horton@apple.com>
2015-07-25 23:11:16 +0000

    Expose TextIndicator-backed snapshot and rect gathering on DOMNode https://bugs.webkit.org/show_bug.cgi?id=147298 <rdar://problem/21905839>
    Reviewed by Sam Weinig.
    
    * bindings/objc/DOM.mm:
    (-[DOMNode getPreviewSnapshotImage:andRects:]):
    * bindings/objc/DOMExtensions.h:
    
    
    Canonical link: https://commits.webkit.org/165383@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187391 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/objc/DOM.mm
+++ Source/WebCore/bindings/objc/DOM.mm
@@ -50,6 +50,7 @@
 #import "Range.h"
 #import "RenderImage.h"
 #import "ScriptController.h"
+#import "TextIndicator.h"
 #import "WebScriptObjectPrivate.h"
 #import <JavaScriptCore/APICast.h>
 #import <wtf/HashMap.h>
@@ -540,6 +541,48 @@ id <DOMEventTarget> kit(WebCore::EventTarget* eventTarget)
 }
 
 #endif // PLATFORM(IOS)
+
+- (void)getPreviewSnapshotImage:(CGImageRef*)cgImage andRects:(NSArray **)rects
+{
+    if (!cgImage || !rects)
+        return;
+
+    *cgImage = nullptr;
+    *rects = nullptr;
+
+    Node* coreNode = core(self);
+
+    Ref<Range> range = rangeOfContents(*coreNode);
+
+    float deviceScaleFactor = coreNode->document().deviceScaleFactor();
+    const float margin = 4 * deviceScaleFactor;
+    RefPtr<TextIndicator> textIndicator = TextIndicator::createWithRange(range, TextIndicatorPresentationTransition::None, margin);
+
+    if (!textIndicator)
+        return;
+
+    if (Image* image = textIndicator->contentImage())
+        *cgImage = (CGImageRef)CFAutorelease(CGImageRetain(image->getCGImageRef()));
+
+    RetainPtr<NSMutableArray> rectArray = adoptNS([[NSMutableArray alloc] init]);
+
+    if (*cgImage) {
+        FloatPoint origin = textIndicator->textBoundingRectInRootViewCoordinates().location();
+        for (const FloatRect& rect : textIndicator->textRectsInBoundingRectCoordinates()) {
+            CGRect cgRect = rect;
+            cgRect.origin.x += origin.x();
+            cgRect.origin.y += origin.y();
+            cgRect = coreNode->document().frame()->view()->contentsToWindow(enclosingIntRect(cgRect));
+            [rectArray addObject:[NSValue value:&cgRect withObjCType:@encode(CGRect)]];
+        }
+    } else {
+        CGRect cgRect = CGRectInset(range->boundingRect(), -margin, -margin);
+        cgRect = coreNode->document().frame()->view()->contentsToWindow(enclosingIntRect(cgRect));
+        [rectArray addObject:[NSValue value:&cgRect withObjCType:@encode(CGRect)]];
+    }
+
+    *rects = rectArray.autorelease();
+}
 
 @end
 
