5ad498cb0a67c24a7f32bbd6ba0d1b0f62fe6811
Alexey Proskuryakov <ap@apple.com>
2015-07-28 16:29:44 +0000

    Rolling out r187463, because it caused memory corruption on multiple tests.
    Original bug:
    Crash happens when calling removeEventListener for an SVG element which has an instance inside a <defs> element of shadow tree
    https://bugs.webkit.org/show_bug.cgi?id=147290
    
    * svg/custom/remove-event-listener-shadow-disallowed-element-expected.txt: Removed.
    * svg/custom/remove-event-listener-shadow-disallowed-element.svg: Removed.
    Source/WebCore:
    
    
    * svg/SVGUseElement.cpp:
    (WebCore::removeDisallowedElementsFromSubtree):
    (WebCore::associateClonesWithOriginals):
    
    
    
    Canonical link: https://commits.webkit.org/165442@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187486 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/svg/SVGUseElement.cpp
+++ Source/WebCore/svg/SVGUseElement.cpp
@@ -329,11 +329,8 @@ static void removeDisallowedElementsFromSubtree(SVGElement& subtree)
         }
         ++it;
     }
-    for (auto* element : disallowedElements) {
+    for (auto* element : disallowedElements)
         element->parentNode()->removeChild(element);
-        for (auto& descendant : descendantsOfType<SVGElement>(*element))
-            descendant.setCorrespondingElement(nullptr);
-    }
 }
 
 static void associateClonesWithOriginals(SVGElement& clone, SVGElement& original)
