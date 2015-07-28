9b738072091db1da1a68ec716dc5c8644f5679cc
Basile Clement <basile_clement@apple.com>
2015-07-28 18:51:27 +0000

    stress/math-pow-with-constants.js fails in cloop https://bugs.webkit.org/show_bug.cgi?id=147167
    Reviewed by Geoffrey Garen.
    
    Baseline JIT, DFG and FTL are using a fast exponentiation fast path
    when computing Math.pow() with an integer exponent that is not taken in
    the LLInt (or the DFG abstract interpreter). This leads to the result
    of pow changing depending on the compilation tier or the fact that
    constant propagation kicks in, which is undesirable.
    
    This patch adds the fast path to the slow operationMathPow in order to
    maintain an illusion of consistency.
    
    * runtime/MathCommon.cpp:
    (JSC::operationMathPow):
    * tests/stress/math-pow-coherency.js: Added.
    (pow42):
    (build42AsDouble.opaqueAdd):
    (build42AsDouble):
    (powDouble42):
    (clobber):
    (pow42NoConstantFolding):
    (powDouble42NoConstantFolding):
    
    Canonical link: https://commits.webkit.org/165453@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187497 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/MathCommon.cpp
+++ Source/JavaScriptCore/runtime/MathCommon.cpp
@@ -417,7 +417,19 @@ double JIT_OPERATION operationMathPow(double x, double y)
         return PNaN;
     if (std::isinf(y) && fabs(x) == 1)
         return PNaN;
-    return mathPowInternal(x, y);
+    int32_t yAsInt = y;
+    if (static_cast<double>(yAsInt) != y || yAsInt < 0)
+        return mathPowInternal(x, y);
+
+    // If the exponent is a positive int32 integer, we do a fast exponentiation
+    double result = 1;
+    while (yAsInt) {
+        if (yAsInt & 1)
+            result *= x;
+        x *= x;
+        yAsInt >>= 1;
+    }
+    return result;
 }
 
 extern "C" {
