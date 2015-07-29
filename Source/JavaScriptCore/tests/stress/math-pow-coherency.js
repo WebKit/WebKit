ae24b05e4a85009563630e7d44d4edcdcd06f73f
Basile Clement <basile_clement@apple.com>
2015-07-29 17:44:16 +0000

    Unreviewed, lower the number of test iterations to prevent timing out on Debug builds https://bugs.webkit.org/show_bug.cgi?id=147167
    * tests/stress/math-pow-coherency.js:
    
    Canonical link: https://commits.webkit.org/165497@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187548 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/tests/stress/math-pow-coherency.js
+++ Source/JavaScriptCore/tests/stress/math-pow-coherency.js
@@ -44,7 +44,7 @@ var funs = [
 ];
 var tiers = ['jit', 'dfg', 'ftl'];
 
-for (var i = 0; i < 1000000; ++i) {
+for (var i = 0; i < 100000; ++i) {
     for (var j in funs) {
         var name = funs[j][0];
         var fun = funs[j][1];
