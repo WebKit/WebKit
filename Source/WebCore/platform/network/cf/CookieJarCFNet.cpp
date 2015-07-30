bf7728f21eae702846133f78069f7a2df43094d8
Matt Hanson <matthew_hanson@apple.com>
2015-07-30 21:50:44 +0000

    Merge r187490. rdar://problem/21995928
    Canonical link: https://commits.webkit.org/164912.187@safari-601.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601.1-branch@187610 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/network/cf/CookieJarCFNet.cpp
+++ Source/WebCore/platform/network/cf/CookieJarCFNet.cpp
@@ -80,6 +80,7 @@ static inline RetainPtr<CFStringRef> cookieValue(CFHTTPCookieRef cookie)
 
 static RetainPtr<CFArrayRef> filterCookies(CFArrayRef unfilteredCookies)
 {
+    ASSERT(unfilteredCookies);
     CFIndex count = CFArrayGetCount(unfilteredCookies);
     RetainPtr<CFMutableArrayRef> filteredCookies = adoptCF(CFArrayCreateMutable(0, count, &kCFTypeArrayCallBacks));
     for (CFIndex i = 0; i < count; ++i) {
@@ -116,10 +117,14 @@ static RetainPtr<CFArrayRef> copyCookiesForURLWithFirstPartyURL(const NetworkSto
 static CFArrayRef createCookies(CFDictionaryRef headerFields, CFURLRef url)
 {
 #if (PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 90000)
-    return _CFHTTPParsedCookiesWithResponseHeaderFields(kCFAllocatorDefault, headerFields, url);
+    CFArrayRef parsedCookies = _CFHTTPParsedCookiesWithResponseHeaderFields(kCFAllocatorDefault, headerFields, url);
 #else
-    return CFHTTPCookieCreateWithResponseHeaderFields(kCFAllocatorDefault, headerFields, url);
+    CFArrayRef parsedCookies = CFHTTPCookieCreateWithResponseHeaderFields(kCFAllocatorDefault, headerFields, url);
 #endif
+    if (!parsedCookies)
+        parsedCookies = CFArrayCreate(kCFAllocatorDefault, 0, 0, &kCFTypeArrayCallBacks);
+
+    return parsedCookies;
 }
 
 void setCookiesFromDOM(const NetworkStorageSession& session, const URL& firstParty, const URL& url, const String& value)
