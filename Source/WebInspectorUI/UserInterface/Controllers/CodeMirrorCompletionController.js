1cefec3f674075f42d809b90974aabe775d9d71f
Devin Rousso <drousso@apple.com>
2015-08-01 04:15:48 +0000

    Web Inspector: Autocomplete: Undo (Cmd+Z) doesn't work as expected https://bugs.webkit.org/show_bug.cgi?id=147316
    Reviewed by Timothy Hatcher.
    
    Instead of replacing the text for a completion, which messes up the undo history, add
    a unique marker that contains the remaining text for the current completion.
    
    * UserInterface/Controllers/CodeMirrorCompletionController.js:
    (WebInspector.CodeMirrorCompletionController.prototype._createCompletionHintMarker):
    (WebInspector.CodeMirrorCompletionController.prototype._applyCompletionHint.update):
    (WebInspector.CodeMirrorCompletionController.prototype._applyCompletionHint):
    (WebInspector.CodeMirrorCompletionController.prototype._removeCompletionHint.update):
    (WebInspector.CodeMirrorCompletionController.prototype._removeCompletionHint):
    
    
    Canonical link: https://commits.webkit.org/165599@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187708 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Controllers/CodeMirrorCompletionController.js
+++ Source/WebInspectorUI/UserInterface/Controllers/CodeMirrorCompletionController.js
@@ -245,6 +245,15 @@ WebInspector.CodeMirrorCompletionController = class CodeMirrorCompletionControll
         this._notifyCompletionsHiddenIfNeededTimeout = setTimeout(notify.bind(this), WebInspector.CodeMirrorCompletionController.CompletionsHiddenDelay);
     }
 
+    _createCompletionHintMarker(position, text)
+    {
+        var container = document.createElement("span");
+        container.classList.add(WebInspector.CodeMirrorCompletionController.CompletionHintStyleClassName);
+        container.textContent = text;
+
+        this._completionHintMarker = this._codeMirror.setUniqueBookmark(position, {widget: container, insertLeft: true});
+    }
+
     _applyCompletionHint(completionText)
     {
         console.assert(completionText);
@@ -262,14 +271,11 @@ WebInspector.CodeMirrorCompletionController = class CodeMirrorCompletionControll
             var from = {line: this._lineNumber, ch: this._startOffset};
             var cursor = {line: this._lineNumber, ch: this._endOffset};
             var to = {line: this._lineNumber, ch: this._startOffset + replacementText.length};
+            var currentText = this._codeMirror.getRange(from, cursor);
 
-            this._codeMirror.replaceRange(replacementText, from, cursor, WebInspector.CodeMirrorCompletionController.CompletionOrigin);
-            this._removeLastChangeFromHistory();
-
-            this._codeMirror.setCursor(cursor);
-
+            this._createCompletionHintMarker(cursor, replacementText.replace(currentText, ""));
             if (cursor.ch !== to.ch)
-                this._completionHintMarker = this._codeMirror.markText(cursor, to, {className: WebInspector.CodeMirrorCompletionController.CompletionHintStyleClassName});
+                this._codeMirror.markText(cursor, to, {className: WebInspector.CodeMirrorCompletionController.CompletionHintStyleClassName});
         }
 
         this._ignoreChange = true;
@@ -339,12 +345,8 @@ WebInspector.CodeMirrorCompletionController = class CodeMirrorCompletionControll
         function update()
         {
             var range = this._completionHintMarker.find();
-            if (range) {
+            if (range)
                 this._completionHintMarker.clear();
-
-                this._codeMirror.replaceRange("", range.from, range.to, WebInspector.CodeMirrorCompletionController.DeleteCompletionOrigin);
-                this._removeLastChangeFromHistory();
-            }
 
             this._completionHintMarker = null;
 
