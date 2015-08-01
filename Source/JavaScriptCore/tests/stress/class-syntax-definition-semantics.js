8dfd24d7c9373ed5112862ee8714db985f65ef90
Saam Barati <saambarati1@gmail.com>
2015-08-01 15:27:11 +0000

    Unreviewed. Remove unintentional "print" statement in test case. https://bugs.webkit.org/show_bug.cgi?id=142567
    * tests/stress/class-syntax-definition-semantics.js:
    (shouldBeSyntaxError):
    
    
    
    Canonical link: https://commits.webkit.org/165607@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187717 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/tests/stress/class-syntax-definition-semantics.js
+++ Source/JavaScriptCore/tests/stress/class-syntax-definition-semantics.js
@@ -6,7 +6,6 @@ function shouldBeSyntaxError(s) {
     } catch(e) {
         if (e instanceof SyntaxError)
             isSyntaxError = true;
-        print(e);
     }
     if (!isSyntaxError)
         throw new Error("expected a syntax error");
