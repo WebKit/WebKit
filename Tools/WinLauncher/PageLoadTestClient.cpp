ffd9479ae7f7adcf977932a6dfcaeffbaf42083f
Alex Christensen <achristensen@apple.com>
2015-08-03 15:55:08 +0000

    [Win] Build fix after r187733.
    * WinLauncher/PageLoadTestClient.cpp:
    (PageLoadTestClient::dumpRunStatistics):
    wtf/MathExtras.h is now included in HashTable.h, which #defines pow to wtf_pow.
    
    
    
    Canonical link: https://commits.webkit.org/165623@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187738 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/WinLauncher/PageLoadTestClient.cpp
+++ Tools/WinLauncher/PageLoadTestClient.cpp
@@ -234,7 +234,7 @@ void PageLoadTestClient::dumpRunStatistics()
     if (m_pagesTimed) {
         meanTime = m_totalTime / m_pagesTimed;
         squareMeanRootTime = (m_totalSquareRootsOfTime / m_pagesTimed) * (m_totalSquareRootsOfTime / m_pagesTimed);
-        geometricMeanTime = std::pow(m_geometricMeanProductSum, (1.0 / m_pagesTimed));
+        geometricMeanTime = pow(m_geometricMeanProductSum, (1.0 / m_pagesTimed));
     }
 
     file->printf("FINISHED:    Total Time = %.1f ms\n", m_totalTime * 1000.0);
