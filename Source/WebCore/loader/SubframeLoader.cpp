4bc8b5800a44297a8f0c40aa4131a508a4afdcdb
Alan Bujtas <zalan@apple.com>
2015-08-04 09:55:04 +0000

    Merge r186666 - Plugin create can end up destroying its renderer. https://bugs.webkit.org/show_bug.cgi?id=146824 rdar://problem/18921429
    Reviewed by Andreas Kling.
    
    Plugins can run arbitrary code during initialization. If the plugin
    happens to destroy the associated node, its renderer becomes invalid.
    This patch checks whether the renderer survived the createPlugin() call.
    (This WeakPtr pattern is also used in RenderWidget to avoid dangling pointers.)
    
    Speculative fix. Not reproducible.
    
    * loader/SubframeLoader.cpp:
    (WebCore::SubframeLoader::loadPlugin):

--- Source/WebCore/loader/SubframeLoader.cpp
+++ Source/WebCore/loader/SubframeLoader.cpp
@@ -386,10 +386,12 @@ Document* SubframeLoader::document() const
 
 bool SubframeLoader::loadPlugin(HTMLPlugInImageElement& pluginElement, const URL& url, const String& mimeType, const Vector<String>& paramNames, const Vector<String>& paramValues, bool useFallback)
 {
-    RenderEmbeddedObject* renderer = pluginElement.renderEmbeddedObject();
+    if (useFallback)
+        return false;
 
+    RenderEmbeddedObject* renderer = pluginElement.renderEmbeddedObject();
     // FIXME: This code should not depend on renderer!
-    if (!renderer || useFallback)
+    if (!renderer)
         return false;
 
     pluginElement.subframeLoaderWillCreatePlugIn(url);
@@ -403,7 +405,11 @@ bool SubframeLoader::loadPlugin(HTMLPlugInImageElement& pluginElement, const URL
         loadManually = false;
 #endif
 
+    WeakPtr<RenderWidget> weakRenderer = renderer->createWeakPtr();
+    // createPlugin *may* cause this renderer to disappear from underneath.
     RefPtr<Widget> widget = m_frame.loader().client().createPlugin(contentSize, &pluginElement, url, paramNames, paramValues, mimeType, loadManually);
+    if (!weakRenderer)
+        return false;
 
     if (!widget) {
         if (!renderer->isPluginUnavailable())
