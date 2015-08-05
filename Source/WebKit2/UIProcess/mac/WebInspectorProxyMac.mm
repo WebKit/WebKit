a5daed3b0216dbd8491ed756a756ff082a4d901f
Ryosuke Niwa <rniwa@webkit.org>
2015-08-05 19:21:25 +0000

    WebInspectorProxy should make WKWebView first responder https://bugs.webkit.org/show_bug.cgi?id=147676
    Reviewed by Timothy Hatcher.
    
    Revert r181927 now that WKWebView forwards the first responder role to WKView as needed.
    
    * UIProcess/mac/WebInspectorProxyMac.mm:
    (WebKit::WebInspectorProxy::platformBringToFront):
    (WebKit::WebInspectorProxy::platformAttach):
    
    
    Canonical link: https://commits.webkit.org/165763@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@187973 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/mac/WebInspectorProxyMac.mm
+++ Source/WebKit2/UIProcess/mac/WebInspectorProxyMac.mm
@@ -587,7 +587,7 @@ void WebInspectorProxy::platformBringToFront()
 
     // FIXME <rdar://problem/10937688>: this will not bring a background tab in Safari to the front, only its window.
     [m_inspectorView.get().window makeKeyAndOrderFront:nil];
-    [m_inspectorView.get().window makeFirstResponder:m_inspectorView->_page->wkView()];
+    [m_inspectorView.get().window makeFirstResponder:m_inspectorView.get()];
 }
 
 bool WebInspectorProxy::platformIsFront()
@@ -810,7 +810,7 @@ void WebInspectorProxy::platformAttach()
     inspectedViewFrameDidChange(currentDimension);
 
     [[inspectedView superview] addSubview:m_inspectorView.get() positioned:NSWindowBelow relativeTo:inspectedView];
-    [m_inspectorView.get().window makeFirstResponder:m_inspectorView->_page->wkView()];
+    [m_inspectorView.get().window makeFirstResponder:m_inspectorView.get()];
 }
 
 void WebInspectorProxy::platformDetach()
