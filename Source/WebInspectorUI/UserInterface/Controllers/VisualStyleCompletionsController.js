1448a3bc3e26f6144d61882ee588b5046df35d52
Devin Rousso <drousso@apple.com>
2015-08-14 18:43:46 +0000

    Web Inspector: Add autocomplete controller for Visual property editors https://bugs.webkit.org/show_bug.cgi?id=147579
    Reviewed by Timothy Hatcher.
    
    * UserInterface/Controllers/VisualStyleCompletionsController.js: Added.
    (WebInspector.VisualStyleCompletionsController):
    Takes in a CSSCompletions and displays a list of suggestions based on a given prefix in a popover.
    (WebInspector.VisualStyleCompletionsController.prototype.get visible):
    (WebInspector.VisualStyleCompletionsController.prototype.get hasCompletions):
    (WebInspector.VisualStyleCompletionsController.prototype.get currentCompletion):
    (WebInspector.VisualStyleCompletionsController.prototype.set completions):
    (WebInspector.VisualStyleCompletionsController.prototype.completionSuggestionsViewCustomizeCompletionElement):
    (WebInspector.VisualStyleCompletionsController.prototype.previous):
    (WebInspector.VisualStyleCompletionsController.prototype.next):
    (WebInspector.VisualStyleCompletionsController.prototype.update):
    (WebInspector.VisualStyleCompletionsController.prototype.show):
    (WebInspector.VisualStyleCompletionsController.prototype.hide):
    
    * UserInterface/Models/CSSKeywordCompletions.js:
    (WebInspector.CSSKeywordCompletions.forProperty):
    Make sure that the cssNameCompletions exist before trying to add them.
    
    * UserInterface/Views/CompletionSuggestionsView.js:
    (WebInspector.CompletionSuggestionsView.prototype.update):
    Allow the delegate to modify the newly created completion suggestion item.
    
    
    Canonical link: https://commits.webkit.org/166149@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@188480 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebInspectorUI/UserInterface/Controllers/VisualStyleCompletionsController.js
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2015 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+WebInspector.VisualStyleCompletionsController = class VisualStyleCompletionsController extends WebInspector.Object
+{
+    constructor(delegate)
+    {
+        super();
+
+        this._delegate = delegate || null;
+        this._suggestionsView = new WebInspector.CompletionSuggestionsView(this);
+        this._completions = null;
+        this._currentCompletions = [];
+        this._selectedCompletionIndex = 0;
+    }
+
+    // Public
+
+    get visible()
+    {
+        return this._completions && this._currentCompletions.length && this._suggestionsView.visible;
+    }
+
+    get hasCompletions()
+    {
+        return !!this._completions;
+    }
+
+    get currentCompletion()
+    {
+        if (!this.hasCompletions)
+            return;
+
+        return this._currentCompletions[this._selectedCompletionIndex] || null;
+    }
+
+    set completions(completions)
+    {
+        this._completions = completions || null;
+    }
+
+    completionSuggestionsViewCustomizeCompletionElement(suggestionsView, element, item)
+    {
+        if (this._delegate && typeof this._delegate.visualStyleCompletionsControllerCustomizeCompletionElement === "function")
+            this._delegate.visualStyleCompletionsControllerCustomizeCompletionElement(suggestionsView, element, item);
+    }
+
+    completionSuggestionsClickedCompletion(suggestionsView, text)
+    {
+        suggestionsView.hide();
+        this.dispatchEventToListeners(WebInspector.VisualStyleCompletionsController.Event.CompletionSelected, {text});
+    }
+
+    previous()
+    {
+        this._suggestionsView.selectPrevious();
+        this._selectedCompletionIndex = this._suggestionsView.selectedIndex;
+    }
+
+    next()
+    {
+        this._suggestionsView.selectNext();
+        this._selectedCompletionIndex = this._suggestionsView.selectedIndex;
+    }
+
+    update(value)
+    {
+        if (!this.hasCompletions)
+            return false;
+
+        this._currentCompletions = this._completions.startsWith(value);
+
+        var currentCompletionsLength = this._currentCompletions.length;
+        if (currentCompletionsLength) {
+            if (currentCompletionsLength === 1 && this._currentCompletions[0] === value) {
+                this.hide();
+                return false;
+            }
+
+            if (this._selectedCompletionIndex >= currentCompletionsLength)
+                this._selectedCompletionIndex = 0;
+
+            this._suggestionsView.update(this._currentCompletions, this._selectedCompletionIndex);
+            return true;
+        }
+
+        this.hide();
+        return false;
+    }
+
+    show(bounds, padding)
+    {
+        if (!bounds)
+            return;
+
+        this._suggestionsView.show(bounds.pad(padding || 0));
+    }
+
+    hide()
+    {
+        if (this._suggestionsView.isHandlingClickEvent())
+            return;
+
+        this._suggestionsView.hide();
+    }
+};
+
+WebInspector.VisualStyleCompletionsController.Event = {
+    CompletionSelected: "visual-style-completions-controller-completion-selected"
+}
