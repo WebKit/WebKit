6007938e48147db0055bb8be8b8a0679bbd4a108
Brent Fulgham <bfulgham@apple.com>
2015-08-22 01:26:50 +0000

    [Win] Unreviewed test correction after r188709.
    I removed a function argument that happened to be mirrored by a global
    value with the same name. This prevented a compile error, but testing
    showed that the wrong value was being used in the function.
    
    * DumpRenderTree/win/FrameLoadDelegate.cpp:
    (FrameLoadDelegate::willPerformClientRedirectToURL): Put function argument
    back
    
    
    
    Canonical link: https://commits.webkit.org/166446@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@188812 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/DumpRenderTree/win/FrameLoadDelegate.cpp
+++ Tools/DumpRenderTree/win/FrameLoadDelegate.cpp
@@ -268,7 +268,7 @@ HRESULT FrameLoadDelegate::didFailLoadWithError(_In_opt_ IWebView*, _In_opt_ IWe
     return S_OK;
 }
 
-HRESULT FrameLoadDelegate::willPerformClientRedirectToURL(_In_opt_ IWebView*, _In_ BSTR url, double /*delaySeconds*/, DATE /*fireDate*/, _In_opt_ IWebFrame*)
+HRESULT FrameLoadDelegate::willPerformClientRedirectToURL(_In_opt_ IWebView*, _In_ BSTR url, double /*delaySeconds*/, DATE /*fireDate*/, _In_opt_ IWebFrame* frame)
 {
     if (!done && gTestRunner->dumpFrameLoadCallbacks())
         printf("%s - willPerformClientRedirectToURL: %S \n", descriptionSuitableForTestResult(frame).c_str(),
