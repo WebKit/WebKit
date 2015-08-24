WebKit/WebKit

commit f90f688781b38121ee16bb88bc408307ba0f6da7

[Cairo] Improve image quality when using newer versions of cairo/pixman https://bugs.webkit.org/show_bug.cgi?id=147826
Reviewed by Martin Robinson.

Since cairo 1.14 the image filters changed a bit:

 - CAIRO_FILTER_GOOD uses a box filter when downscaling if the
   scale factor is less than 0.75, otherwise it uses a filter
   equivalent to CAIRO_FILTER_BILINEAR.
 - CAIRO_FILTER_BEST uses always a Catmull-Rom filter.

We are currently using CAIRO_FILTER_BILINEAR for medium, high and
default interpolation levels. We could use CAIRO_FILTER_GOOD for
medium and default, and CAIRO_FILTER_BEST for high. This will not
have any effect in previous versions of cairo because before 1.14
CAIRO_FILTER_GOOD, CAIRO_FILTER_BILINEAR and CAIRO_FILTER_BEST had
the same implementation in pixman.

* platform/graphics/cairo/PlatformContextCairo.cpp:
(WebCore::PlatformContextCairo::drawSurfaceToContext):

Canonical link: https://commits.webkit.org/166077@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@188379 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/cairo/PlatformContextCairo.cpp
@@ -198,11 +198,10 @@ void PlatformContextCairo::drawSurfaceToContext(cairo_surface_t* surface, const 
         cairo_pattern_set_filter(pattern.get(), CAIRO_FILTER_FAST);
         break;
     case InterpolationMedium:
-    case InterpolationHigh:
-        cairo_pattern_set_filter(pattern.get(), CAIRO_FILTER_BILINEAR);
-        break;
     case InterpolationDefault:
-        cairo_pattern_set_filter(pattern.get(), CAIRO_FILTER_BILINEAR);
+        cairo_pattern_set_filter(pattern.get(), CAIRO_FILTER_GOOD);
+    case InterpolationHigh:
+        cairo_pattern_set_filter(pattern.get(), CAIRO_FILTER_BEST);
         break;
     }
     cairo_pattern_set_extend(pattern.get(), CAIRO_EXTEND_PAD);

commit 2e7b700982647fe638c67666104f6c971562da9f

Unreviewed. Fix cairo performance regression introduced in r188379.
A missing break in a switch was making us to use High image
interpolation quality by default.

* platform/graphics/cairo/PlatformContextCairo.cpp:
(WebCore::PlatformContextCairo::drawSurfaceToContext): Add the
missing break.

Canonical link: https://commits.webkit.org/166492@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@188858 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/cairo/PlatformContextCairo.cpp
@@ -200,6 +200,7 @@ void PlatformContextCairo::drawSurfaceToContext(cairo_surface_t* surface, const 
     case InterpolationMedium:
     case InterpolationDefault:
         cairo_pattern_set_filter(pattern.get(), CAIRO_FILTER_GOOD);
+        break;
     case InterpolationHigh:
         cairo_pattern_set_filter(pattern.get(), CAIRO_FILTER_BEST);
         break;