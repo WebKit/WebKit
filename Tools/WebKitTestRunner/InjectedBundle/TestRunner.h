b64556c3823219dc9fccf2aac11cef22193a3974
Andy Estes <aestes@apple.com>
2015-08-26 23:36:58 +0000

    Merge r188988.
        2015-08-26  Andy Estes  <aestes@apple.com>
    
        Crash when following a Google search link to Twitter with Limit Adult Content enabled
        https://bugs.webkit.org/show_bug.cgi?id=147651
    
        Rubber-stamped by Brady Eidson.
    
    Tools:
    
        Taught TestRunner how to decide the navigation policy after a delay.
    
        * WebKitTestRunner/InjectedBundle/Bindings/TestRunner.idl:
        * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp:
        (WTR::InjectedBundlePage::decidePolicyForNavigationAction):
        * WebKitTestRunner/InjectedBundle/TestRunner.cpp:
        (WTR::TestRunner::setShouldDecideNavigationPolicyAfterDelay):
        * WebKitTestRunner/InjectedBundle/TestRunner.h:
        (WTR::TestRunner::shouldDecideNavigationPolicyAfterDelay):
        * WebKitTestRunner/TestController.cpp:
        (WTR::TestController::initialize):
        (WTR::TestController::resetStateToConsistentValues):
        (WTR::TestController::decidePolicyForNavigationAction):
        * WebKitTestRunner/TestController.h:
        (WTR::TestController::setShouldDecideNavigationPolicyAfterDelay):
        * WebKitTestRunner/TestInvocation.cpp:
        (WTR::TestInvocation::didReceiveMessageFromInjectedBundle):
    
    LayoutTests:
    
        Added a layout test.
    
        * http/tests/contentfiltering/load-substitute-data-from-appcache-expected.txt: Added.
        * http/tests/contentfiltering/load-substitute-data-from-appcache.html: Added.
        * http/tests/contentfiltering/resources/appcache.html: Added.
        * http/tests/contentfiltering/resources/appcache.manifest: Added.
        * platform/mac-wk1/TestExpectations:
    
    Canonical link: https://commits.webkit.org/164912.283@safari-601.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601.1-branch@189001 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/WebKitTestRunner/InjectedBundle/TestRunner.h
+++ Tools/WebKitTestRunner/InjectedBundle/TestRunner.h
@@ -281,6 +281,9 @@ public:
     JSValueRef numberOfDFGCompiles(JSValueRef theFunction);
     JSValueRef neverInlineFunction(JSValueRef theFunction);
 
+    bool shouldDecideNavigationPolicyAfterDelay() const { return m_shouldDecideNavigationPolicyAfterDelay; }
+    void setShouldDecideNavigationPolicyAfterDelay(bool);
+
 private:
     TestRunner();
 
@@ -330,6 +333,8 @@ private:
 
     double m_databaseDefaultQuota;
     double m_databaseMaxQuota;
+
+    bool m_shouldDecideNavigationPolicyAfterDelay { false };
 
     bool m_userStyleSheetEnabled;
     WKRetainPtr<WKStringRef> m_userStyleSheetLocation;
--- Tools/WebKitTestRunner/TestController.h
+++ Tools/WebKitTestRunner/TestController.h
@@ -121,6 +121,8 @@ public:
 
     bool isCurrentInvocation(TestInvocation* invocation) const { return invocation == m_currentInvocation.get(); }
 
+    void setShouldDecideNavigationPolicyAfterDelay(bool value) { m_shouldDecideNavigationPolicyAfterDelay = value; }
+
 private:
     void initialize(int argc, const char* argv[]);
     void createWebViewWithOptions(WKDictionaryRef);
@@ -272,6 +274,8 @@ private:
 
     bool m_shouldLogHistoryClientCallbacks;
     bool m_shouldShowWebView;
+
+    bool m_shouldDecideNavigationPolicyAfterDelay { false };
 
     std::unique_ptr<EventSenderProxy> m_eventSenderProxy;
 
