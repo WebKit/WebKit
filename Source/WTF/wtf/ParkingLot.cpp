38a8316215f60982a68bb7f205387fff1c6b23dd
Žan Doberšek <zdobersek@igalia.com>
2015-09-14 13:22:08 +0000

    Merge r189151 - [WTF] Improve a ParkingLot::parkConditionally() comment for a libstdc++ workaround https://bugs.webkit.org/show_bug.cgi?id=148571
    Reviewed by Filip Pizlo.
    
    * wtf/ParkingLot.cpp:
    (WTF::ParkingLot::parkConditionally): Adjust the comment about the workaround for
    the libstdc++ std::condition_variable implementation, linking to the WebKit bug
    that dissected the problem and the GCC bug that originally reported the problem.
    
    Canonical link: https://commits.webkit.org/166077.73@webkitgtk/2.10
    git-svn-id: https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-2.10@189724 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/ParkingLot.cpp
+++ Source/WTF/wtf/ParkingLot.cpp
@@ -540,9 +540,9 @@ bool ParkingLot::parkConditionally(
     {
         std::unique_lock<std::mutex> locker(me->parkingLock);
         while (me->address && Clock::now() < timeout) {
-            // This is pretty funny. On Linux, if you wait until the max time, it immediately reports that you timed
-            // out. What's particularly bad about this is that it never releases the lock in that case. So, you loop
-            // forever without yielding to the thread that would wake you up.
+            // Falling back to wait() works around a bug in libstdc++ implementation of std::condition_variable. See:
+            // - https://bugs.webkit.org/show_bug.cgi?id=148027
+            // - https://gcc.gnu.org/bugzilla/show_bug.cgi?id=58931
             if (timeout == Clock::time_point::max())
                 me->parkingCondition.wait(locker);
             else
