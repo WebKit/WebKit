516a16ec4b65a1b04ef4158fb54985e445d6e4dd
Saam Barati <sbarati@apple.com>
2015-09-21 22:42:12 +0000

    Web Inspector: Basic Block Annotations and Type Profiler annotations wrong for script with "class" with default constructor https://bugs.webkit.org/show_bug.cgi?id=149248
    Reviewed by Mark Lam.
    
    We keep track of which functions have and have not
    executed so we can show visually, inside the inspector,
    which functions have and have not executed. With a default
    constructor, our parser parses code that isn't in the actual
    JavaScript source code of the user. Our parser would then
    give us a range of starting at "1" to "1 + default constructor length"
    as being the text range of a function. But, this would then pollute
    actual source code that was at these ranges.
    
    Therefore, we should treat these default constructor source
    codes as having "invalid" ranges. We use [UINT_MAX, UINT_MAX]
    as the invalid range. This range has the effect of not polluting
    valid ranges inside the source code.
    
    * bytecode/UnlinkedFunctionExecutable.cpp:
    (JSC::UnlinkedFunctionExecutable::unlinkedCodeBlockFor):
    (JSC::UnlinkedFunctionExecutable::setInvalidTypeProfilingOffsets):
    * bytecode/UnlinkedFunctionExecutable.h:
    * bytecompiler/BytecodeGenerator.cpp:
    (JSC::BytecodeGenerator::emitNewDefaultConstructor):
    
    
    Canonical link: https://commits.webkit.org/167517@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@190086 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
+++ Source/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
@@ -2503,6 +2503,7 @@ RegisterID* BytecodeGenerator::emitNewArrowFunctionExpression(RegisterID* dst, A
 RegisterID* BytecodeGenerator::emitNewDefaultConstructor(RegisterID* dst, ConstructorKind constructorKind, const Identifier& name)
 {
     UnlinkedFunctionExecutable* executable = m_vm->builtinExecutables()->createDefaultConstructor(constructorKind, name);
+    executable->setInvalidTypeProfilingOffsets();
 
     unsigned index = m_codeBlock->addFunctionExpr(executable);
 
