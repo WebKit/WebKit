a0fa226ecd032b2ed13fdccc4625efa476574743
Carlos Garcia Campos <cgarcia@igalia.com>
2015-09-25 09:42:20 +0000

    [GTK] ASSERTION FAILED: !m_inUpdateBackingStoreState in DrawingAreaImpl::display() after DrawingAreaImpl::forceRepaint() https://bugs.webkit.org/show_bug.cgi?id=148956
    Reviewed by Žan Doberšek.
    
    Source/WebKit2:
    
    This is because those tests call notifyDone in the onresize event
    handler. InjectedBundlePage::dump() always calls WKBundlePageForceRepaint()
    before dumping. When the view is resized DrawingAreaImpl::updateBackingStoreState()
    is called, so if the size has changed the FrameView::resize()
    method is called and all children are resized, so the onresize
    handlers happen at that point, before the
    m_inUpdateBackingStoreState is set to false again. For WTR we
    could probably just return early from froceReapaint() when
    m_inUpdateBackingStoreState is true, because in that case we know
    the layout is updated because of the resize and the actual display
    is not really needed. But the UI process can also request a force
    repaint, so we could wait until the backing store update is done
    and then force the repaint. For WTR it will happen after the
    dump, but it shouldn't be a problem.
    
    * WebProcess/WebPage/DrawingAreaImpl.cpp:
    (WebKit::DrawingAreaImpl::forceRepaint):
    (WebKit::DrawingAreaImpl::updateBackingStoreState):
    * WebProcess/WebPage/DrawingAreaImpl.h:
    
    LayoutTests:
    
    Unskip tests that should pass now.
    
    * platform/gtk/TestExpectations:
    
    Canonical link: https://commits.webkit.org/167657@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@190238 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
+++ Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
@@ -197,6 +197,12 @@ void DrawingAreaImpl::setLayerTreeStateIsFrozen(bool isFrozen)
 
 void DrawingAreaImpl::forceRepaint()
 {
+    if (m_inUpdateBackingStoreState) {
+        m_forceRepaintAfterBackingStoreStateUpdate = true;
+        return;
+    }
+
+    m_forceRepaintAfterBackingStoreStateUpdate = false;
     setNeedsDisplay();
 
     m_webPage.layoutIfNeeded();
@@ -375,6 +381,8 @@ void DrawingAreaImpl::updateBackingStoreState(uint64_t stateID, bool respondImme
     }
 
     m_inUpdateBackingStoreState = false;
+    if (m_forceRepaintAfterBackingStoreStateUpdate)
+        forceRepaint();
 }
 
 void DrawingAreaImpl::sendDidUpdateBackingStoreState()
