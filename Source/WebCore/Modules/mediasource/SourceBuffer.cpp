6e19d8543dad0ac954eba6d8e984f84f09b9cac7
Jer Noble <jer.noble@apple.com>
2015-10-02 19:51:21 +0000

    [MSE] Browser crashes when appending invalid data to MSE source buffer https://bugs.webkit.org/show_bug.cgi?id=149689
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    Test: media/media-source/media-source-stpp-crash.html
    
    Bail out early (as specced) after disconnecting the SourceBuffer from its MediaSource.
    
    * Modules/mediasource/SourceBuffer.cpp:
    (WebCore::SourceBuffer::sourceBufferPrivateDidReceiveInitializationSegment):
    
    LayoutTests:
    
    * media/content/stpp.mp4: Added.
    * media/media-source/media-source-stpp-crash-expected.txt: Added.
    * media/media-source/media-source-stpp-crash.html: Added.
    
    
    Canonical link: https://commits.webkit.org/167872@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@190516 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/mediasource/SourceBuffer.cpp
+++ Source/WebCore/Modules/mediasource/SourceBuffer.cpp
@@ -1022,8 +1022,10 @@ void SourceBuffer::sourceBufferPrivateDidReceiveInitializationSegment(SourceBuff
 
     // 2. If the initialization segment has no audio, video, or text tracks, then run the append error algorithm
     // with the decode error parameter set to true and abort these steps.
-    if (!segment.audioTracks.size() && !segment.videoTracks.size() && !segment.textTracks.size())
+    if (segment.audioTracks.isEmpty() && segment.videoTracks.isEmpty() && segment.textTracks.isEmpty()) {
         appendError(true);
+        return;
+    }
 
     // 3. If the first initialization segment flag is true, then run the following steps:
     if (m_receivedFirstInitializationSegment) {
