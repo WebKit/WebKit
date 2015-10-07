684f92ec81b964dc59e2e65bb5b18a40088ae357
Nan Wang <n_wang@apple.com>
2015-10-07 18:17:56 +0000

    AX: ARIA 1.1 @aria-placeholder https://bugs.webkit.org/show_bug.cgi?id=148970
    Reviewed by Chris Fleizach.
    
    Source/WebCore:
    
    Added support for aria-placeholder attribute.
    
    Modified accessibility/placeholder.html test.
    
    * accessibility/AccessibilityObject.cpp:
    (WebCore::AccessibilityObject::placeholderValue):
    * html/HTMLAttributeNames.in:
    
    LayoutTests:
    
    * accessibility/placeholder-expected.txt:
    * accessibility/placeholder.html:
    
    
    Canonical link: https://commits.webkit.org/168019@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@190674 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/accessibility/AccessibilityObject.cpp
+++ Source/WebCore/accessibility/AccessibilityObject.cpp
@@ -2120,6 +2120,10 @@ bool AccessibilityObject::isValueAutofilled() const
 
 const AtomicString& AccessibilityObject::placeholderValue() const
 {
+    const AtomicString& ariaPlaceholder = getAttribute(aria_placeholderAttr);
+    if (!ariaPlaceholder.isEmpty())
+        return ariaPlaceholder;
+    
     const AtomicString& placeholder = getAttribute(placeholderAttr);
     if (!placeholder.isEmpty())
         return placeholder;
