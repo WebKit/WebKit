5ad2798addc6b53efac4c1abaeea2a7ec1e67f50
ChangSeok Oh <changseok@webkit.org>
2015-10-07 07:32:14 +0000

    [GTK] Progress bar is broken on recent GTK+ https://bugs.webkit.org/show_bug.cgi?id=149831
    Reviewed by Carlos Garcia Campos.
    
    Source/WebCore:
    
    The gtk progress bar has been broken after bumping up to Gtk+-3.16. This is because
    the way of rendering progress bar changed after gtk+-3.13.7. See more
    https://mail.gnome.org/archives/commits-list/2014-August/msg03865.html
    gtk_render_activity is no longer valid to paint a progress bar on a newer gtk+.
    It should be done with gtk_render_background and gtk_render_frame.
    
    Test: fast/dom/HTMLProgressElement/native-progress-bar.html
    
    * rendering/RenderThemeGtk.cpp:
    (WebCore::RenderThemeGtk::paintProgressBar):
    
    LayoutTests:
    
    * fast/dom/HTMLProgressElement/native-progress-bar.html: Added.
    * platform/gtk/fast/dom/HTMLProgressElement/native-progress-bar-expected.png: Added.
    * platform/gtk/fast/dom/HTMLProgressElement/native-progress-bar-expected.txt: Added.
    * platform/mac/fast/dom/HTMLProgressElement/native-progress-bar-expected.png: Added.
    * platform/mac/fast/dom/HTMLProgressElement/native-progress-bar-expected.txt: Added.
    
    
    Canonical link: https://commits.webkit.org/168007@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@190662 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderThemeGtk.cpp
+++ Source/WebCore/rendering/RenderThemeGtk.cpp
@@ -1192,8 +1192,14 @@ bool RenderThemeGtk::paintProgressBar(const RenderObject& renderObject, const Pa
         rect.height() - (padding.top + padding.bottom));
     progressRect = RenderThemeGtk::calculateProgressRect(renderObject, progressRect);
 
-    if (!progressRect.isEmpty())
+    if (!progressRect.isEmpty()) {
+#if GTK_CHECK_VERSION(3, 13, 7)
+        gtk_render_background(context, paintInfo.context().platformContext()->cr(), progressRect.x(), progressRect.y(), progressRect.width(), progressRect.height());
+        gtk_render_frame(context, paintInfo.context().platformContext()->cr(), progressRect.x(), progressRect.y(), progressRect.width(), progressRect.height());
+#else
         gtk_render_activity(context, paintInfo.context().platformContext()->cr(), progressRect.x(), progressRect.y(), progressRect.width(), progressRect.height());
+#endif
+    }
 
     gtk_style_context_restore(context);
     return false;
