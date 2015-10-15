95f1415405a6f9ecf2e3bdd7ae685ab36d94d80a
Matt Hanson <matthew_hanson@apple.com>
2015-10-15 06:46:33 +0000

    Merge r190339. rdar://problem/23075839
    Canonical link: https://commits.webkit.org/164912.359@safari-601.1.46-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601.1.46-branch@191093 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/xml/XSLStyleSheetLibxslt.cpp
+++ Source/WebCore/xml/XSLStyleSheetLibxslt.cpp
@@ -245,12 +245,19 @@ xsltStylesheetPtr XSLStyleSheet::compileStyleSheet()
     if (m_embedded)
         return xsltLoadStylesheetPI(document());
 
+    // Certain libxslt versions are corrupting the xmlDoc on compilation
+    // failures - hence attempting to recompile after a failure is unsafe.
+    if (m_compilationFailed)
+        return 0;
+
     // xsltParseStylesheetDoc makes the document part of the stylesheet
     // so we have to release our pointer to it.
     ASSERT(!m_stylesheetDocTaken);
     xsltStylesheetPtr result = xsltParseStylesheetDoc(m_stylesheetDoc);
     if (result)
         m_stylesheetDocTaken = true;
+    else
+        m_compilationFailed = true;
     return result;
 }
 
