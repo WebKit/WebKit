ac39d175ea9c75ac1cd6dead10bb2cf8725a6814
Dan Bernstein <mitz@apple.com>
2015-10-17 21:32:28 +0000

    [Cocoa] Stop using WKAXRegisterRemoteApp https://bugs.webkit.org/show_bug.cgi?id=150283
    Reviewed by Alexey Proskuryakov.
    
    Source/WebCore:
    
    * platform/spi/ios/GraphicsServicesSPI.h: Added declaration of GSSystemRootDirectory.
    
    Source/WebKit2:
    
    * Platform/spi/mac: Added.
    * Platform/spi/mac/AppKitSPI.h: Added. Includes declaration of
      +[NSAccessibilityRemoteUIElement setRemoteUIApp:].
    
    * WebKit2.xcodeproj/project.pbxproj: Added reference to new header. Also changed the path of
      the Platform/spi/Cocoa group to the corresponding path in the source tree.
    
    * WebProcess/cocoa/WebProcessCocoa.mm:
    (WebKit::registerWithAccessibility): Added.
    (WebKit::WebProcess::platformInitializeProcess): Use new registerWithAccessibility instead
      of WKAXRegisterRemoteApp.
    
    
    
    Canonical link: https://commits.webkit.org/168419@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@191249 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/spi/ios/GraphicsServicesSPI.h
+++ Source/WebCore/platform/spi/ios/GraphicsServicesSPI.h
@@ -40,6 +40,7 @@ WTF_EXTERN_C_BEGIN
 
 void GSInitialize(void);
 uint64_t GSCurrentEventTimestamp(void);
+CFStringRef GSSystemRootDirectory(void);
 
 WTF_EXTERN_C_END
 
--- Source/WebKit2/WebProcess/cocoa/WebProcessCocoa.mm
+++ Source/WebKit2/WebProcess/cocoa/WebProcessCocoa.mm
@@ -61,6 +61,14 @@
 #import <stdio.h>
 #import <wtf/RAMSize.h>
 
+#if USE(APPKIT)
+#import "AppKitSPI.h"
+#endif
+
+#if PLATFORM(IOS)
+#import <WebCore/GraphicsServicesSPI.h>
+#endif
+
 using namespace WebCore;
 
 namespace WebKit {
@@ -190,9 +198,22 @@ void WebProcess::initializeProcessName(const ChildProcessInitializationParameter
 #endif
 }
 
+static void registerWithAccessibility()
+{
+#if USE(APPKIT)
+    [NSAccessibilityRemoteUIElement setRemoteUIApp:YES];
+#endif
+#if PLATFORM(IOS)
+    NSString *accessibilityBundlePath = [(NSString *)GSSystemRootDirectory() stringByAppendingString:@"/System/Library/AccessibilityBundles/WebProcessLoader.axbundle"];
+    NSError *error;
+    if (![[NSBundle bundleWithPath:accessibilityBundlePath] loadAndReturnError:&error])
+        LOG_ERROR("Failed to load accessibility bundle at %@: %@", accessibilityBundlePath, error);
+#endif
+}
+
 void WebProcess::platformInitializeProcess(const ChildProcessInitializationParameters&)
 {
-    WKAXRegisterRemoteApp();
+    registerWithAccessibility();
 
 #if ENABLE(SEC_ITEM_SHIM)
     SecItemShim::singleton().initialize(this);
