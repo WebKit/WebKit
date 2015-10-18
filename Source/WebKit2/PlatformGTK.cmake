1b369132a3bd27bdb0d0c9635683575e934af680
Joonghun Park <jh718.park@samsung.com>
2015-10-18 23:47:23 +0000

    [GTK] Remove WebKitContextMenu related missing annotation warnings https://bugs.webkit.org/show_bug.cgi?id=150299
    Patch by Joonghun Park <jh718.park@samsung.com> on 2015-10-18
    Reviewed by Darin Adler.
    
    The warnings occur currently because when executing g-ir-scanner,
    it doesn't consider WebKitContextMenu related .cpp files,
    but only .h files.
    
    * PlatformGTK.cmake: Add WebKitContextMenu related cpp files.
    
    Canonical link: https://commits.webkit.org/168442@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@191272 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/PlatformGTK.cmake
+++ Source/WebKit2/PlatformGTK.cmake
@@ -973,8 +973,10 @@ add_custom_command(
         ${GObjectDOMBindings_GIR_HEADERS}
         ${WebKit2WebExtension_INSTALLED_HEADERS}
         ${WEBKIT2_DIR}/UIProcess/API/gtk/WebKitContextMenu.h
+        ${WEBKIT2_DIR}/UIProcess/API/gtk/WebKitContextMenu.cpp
         ${WEBKIT2_DIR}/UIProcess/API/gtk/WebKitContextMenuActions.h
         ${WEBKIT2_DIR}/UIProcess/API/gtk/WebKitContextMenuItem.h
+        ${WEBKIT2_DIR}/UIProcess/API/gtk/WebKitContextMenuItem.cpp
         ${WEBKIT2_DIR}/UIProcess/API/gtk/WebKitHitTestResult.h
         ${WEBKIT2_DIR}/UIProcess/API/gtk/WebKitURIRequest.h
         ${WEBKIT2_DIR}/UIProcess/API/gtk/WebKitURIResponse.h
