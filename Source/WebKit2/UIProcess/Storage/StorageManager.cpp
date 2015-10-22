2506e7c4b8512a3526fbcb7f8842d5f98d132858
Carlos Garcia Campos <cgarcia@igalia.com>
2015-10-22 22:25:14 +0000

    ERROR: Unhandled web process message 'StorageAreaMap:DispatchStorageEvent' https://bugs.webkit.org/show_bug.cgi?id=150315
    Patch by Carlos Garcia Campos <cgarcia@igalia.com> on 2015-10-22
    Reviewed by Andreas Kling.
    
    It was introduced in r184930, that keeps the session storage area
    maps alive in the UI process when they are destroyed by the web
    process. The problem is that we also keep the listeners, so that
    events are also dispatched to the listeners of destroyed areas,
    and the message handlers were removed in the web process.
    
    * UIProcess/Storage/StorageManager.cpp:
    (WebKit::StorageManager::createTransientLocalStorageMap): Add a
    listener for the new storage map ID when the map is reused.
    (WebKit::StorageManager::destroyStorageMap): Always remove
    listeners of destroyed maps.
    
    Canonical link: https://commits.webkit.org/168635@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@191481 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/Storage/StorageManager.cpp
+++ Source/WebKit2/UIProcess/Storage/StorageManager.cpp
@@ -199,7 +199,7 @@ void StorageManager::StorageArea::addListener(IPC::Connection& connection, uint6
 
 void StorageManager::StorageArea::removeListener(IPC::Connection& connection, uint64_t storageMapID)
 {
-    ASSERT(m_eventListeners.contains(std::make_pair(&connection, storageMapID)));
+    ASSERT(isSessionStorage() || m_eventListeners.contains(std::make_pair(&connection, storageMapID)));
     m_eventListeners.remove(std::make_pair(&connection, storageMapID));
 }
 
@@ -736,6 +736,7 @@ void StorageManager::createTransientLocalStorageMap(IPC::Connection& connection,
             continue;
         if (!origin->isSameSchemeHostPort(&area->securityOrigin()))
             continue;
+        area->addListener(connection, storageMapID);
         m_storageAreasByConnection.remove(it);
         m_storageAreasByConnection.add({ &connection, storageMapID }, WTF::move(area));
         return;
@@ -796,11 +797,12 @@ void StorageManager::destroyStorageMap(IPC::Connection& connection, uint64_t sto
         return;
     }
 
+    it->value->removeListener(connection, storageMapID);
+
     // Don't remove session storage maps. The web process may reconnect and expect the data to still be around.
     if (it->value->isSessionStorage())
         return;
 
-    it->value->removeListener(connection, storageMapID);
     m_storageAreasByConnection.remove(connectionAndStorageMapIDPair);
 }
 
