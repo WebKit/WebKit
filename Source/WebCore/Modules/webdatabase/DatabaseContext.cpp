72c88aad9354a200ebc7eb73223464c7f4eab658
Gyuyoung Kim <gyuyoung.kim@webkit.org>
2015-10-22 02:02:04 +0000

    Remove unnecessary default quota setting in DatabaseContext::databaseExceededQuota https://bugs.webkit.org/show_bug.cgi?id=150356
    Reviewed by Darin Adler.
    
    All ports have supported DatabaseContext::databaseExceededQuota(). Thus we don't need to
    keep a test code to extend database quota anymore.
    
    * Modules/webdatabase/DatabaseContext.cpp:
    (WebCore::DatabaseContext::databaseExceededQuota): Deleted.
    
    
    Canonical link: https://commits.webkit.org/168590@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@191427 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/webdatabase/DatabaseContext.cpp
+++ Source/WebCore/Modules/webdatabase/DatabaseContext.cpp
@@ -216,9 +216,6 @@ void DatabaseContext::databaseExceededQuota(const String& name, DatabaseDetails 
         return;
     }
     ASSERT(m_scriptExecutionContext->isWorkerGlobalScope());
-    // FIXME: This needs a real implementation; this is a temporary solution for testing.
-    const unsigned long long defaultQuota = 5 * 1024 * 1024;
-    DatabaseManager::singleton().setQuota(m_scriptExecutionContext->securityOrigin(), defaultQuota);
 }
 
 SecurityOrigin* DatabaseContext::securityOrigin() const
