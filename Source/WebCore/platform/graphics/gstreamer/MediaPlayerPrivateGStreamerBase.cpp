71dbeb0927b5c1fad023f1c9711a305f2f7c5465
Gwang Yoon Hwang <yoon@igalia.com>
2015-10-25 15:10:29 +0000

    [TexMap] Fix a misused flag for GstGL https://bugs.webkit.org/show_bug.cgi?id=150545
    Reviewed by Žan Doberšek.
    
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp:
    (WebCore::MediaPlayerPrivateGStreamerBase::paintToTextureMapper):
    We should pass TextureMapperGL::Flags to the TextureMapperGL::drawTexture instead of
    BitmapTexture::Flags.
    
    
    Canonical link: https://commits.webkit.org/168696@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@191549 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp
@@ -560,9 +560,7 @@ void MediaPlayerPrivateGStreamerBase::paintToTextureMapper(TextureMapper* textur
         return;
 
     unsigned textureID = *reinterpret_cast<unsigned*>(videoFrame.data[0]);
-    BitmapTexture::Flags flags = BitmapTexture::NoFlag;
-    if (GST_VIDEO_INFO_HAS_ALPHA(&videoInfo))
-        flags |= BitmapTexture::SupportsAlpha;
+    TextureMapperGL::Flags flags = GST_VIDEO_INFO_HAS_ALPHA(&videoInfo) ? TextureMapperGL::ShouldBlend : 0;
 
     IntSize size = IntSize(GST_VIDEO_INFO_WIDTH(&videoInfo), GST_VIDEO_INFO_HEIGHT(&videoInfo));
     TextureMapperGL* textureMapperGL = reinterpret_cast<TextureMapperGL*>(textureMapper);
