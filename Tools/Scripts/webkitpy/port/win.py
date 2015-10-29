abc18f34402b5c82c94f4adabe6b67d30cf3a0fc
Per Arne Vollan <peavo@outlook.com>
2015-10-29 19:13:56 +0000

    [WinCairo] Fall back to AppleWin results in layout tests. https://bugs.webkit.org/show_bug.cgi?id=150672
    Reviewed by Alex Christensen.
    
    * Scripts/webkitpy/port/win.py:
    (WinCairoPort):
    (WinCairoPort.default_baseline_search_path):
    
    
    Canonical link: https://commits.webkit.org/168847@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@191743 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/port/win.py
+++ Tools/Scripts/webkitpy/port/win.py
@@ -354,3 +354,10 @@ class WinCairoPort(WinPort):
     port_name = "wincairo"
 
     VERSION_FALLBACK_ORDER = ["wincairo-xp", "wincairo-vista", "wincairo-7sp0", "wincairo-win10", "wincairo"]
+
+    def default_baseline_search_path(self):
+        name = self._name.replace('-wk2', '')
+        fallback_names = self.VERSION_FALLBACK_ORDER[self.VERSION_FALLBACK_ORDER.index(name):-1] + [self.port_name]
+        fallback_names.append('win')
+        fallback_names.append('mac')
+        return map(self._webkit_baseline_path, fallback_names)
