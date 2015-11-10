588a7680735189320c91d70f273852c5f1948062
Youenn Fablet <youennf@gmail.com>
2015-11-10 17:57:32 +0000

    Remove WebKit specific XHR timeout tests in favor of WPT version https://bugs.webkit.org/show_bug.cgi?id=151091
    Reviewed by Alexey Proskuryakov.
    
    Removing XHR timeout WebKit specific tests as they have WPT counterparts and WebKit specific tests are less stable.
    Updated test expectations accordingly.
    
    * TestExpectations:
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-aborted-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-aborted.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-aborted.js: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-abortedonmain-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-abortedonmain.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-abortedonmain.js: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-overrides-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-overrides.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-overrides.js: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-overridesexpires-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-overridesexpires.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-overridesexpires.js: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-runner.js: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-simple-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-simple.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-simple.js: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-synconmain-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-synconmain.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-synconmain.js: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-synconworker.js: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-twice-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-twice.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-twice.js: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-worker-aborted-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-worker-aborted.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-worker-overrides-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-worker-overrides.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-worker-overridesexpires-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-worker-overridesexpires.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-worker-simple-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-worker-simple.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-worker-synconworker-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-worker-synconworker.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-worker-twice-expected.txt: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-worker-twice.html: Removed.
    * http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout.js: Removed.
    * platform/gtk/TestExpectations:
    * platform/ios-simulator/TestExpectations:
    * platform/mac-wk2/TestExpectations:
    * platform/win/TestExpectations:
    
    
    Canonical link: https://commits.webkit.org/169291@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@192251 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/xmlhttprequest/timeout/xmlhttprequest-timeout-aborted.js
+++
@@ -1,6 +0,0 @@
-if (this.document === undefined)
-  importScripts("xmlhttprequest-timeout.js");
-
-runTestRequests([ new AbortedRequest(false),
-		  new AbortedRequest(true, -1),
-		  new AbortedRequest(true, TIME_NORMAL_LOAD) ]);
