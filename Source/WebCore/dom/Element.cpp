a08777a817d1d4d053561fe9eeb0ad741d3fdfe6
Jiewen Tan <jiewen_tan@apple.com>
2015-11-13 18:36:31 +0000

    Element::focus() should acquire the ownership of Frame. https://bugs.webkit.org/show_bug.cgi?id=150204 <rdar://problem/23136794>
    Reviewed by Brent Fulgham.
    
    Source/WebCore:
    
    The FrameSelection::setSelection method sometimes releases the last reference to a frame.
    When this happens, the Element::updateFocusAppearance would attempt to use dereferenced memory.
    Instead, we should ensure that the Frame lifetime is guaranteed to extend through the duration
    of the method call.
    
    Test: editing/selection/focus-iframe-removal-crash.html
    
    * dom/Element.cpp:
    (WebCore::Element::updateFocusAppearance):
    
    LayoutTests:
    
    * editing/selection/focus-iframe-removal-crash-expected.txt: Added.
    * editing/selection/focus-iframe-removal-crash.html: Added.
    
    Canonical link: https://commits.webkit.org/169434@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@192433 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/Element.cpp
+++ Source/WebCore/dom/Element.cpp
@@ -2252,7 +2252,8 @@ void Element::updateFocusAppearanceAfterAttachIfNeeded()
 void Element::updateFocusAppearance(SelectionRestorationMode, SelectionRevealMode revealMode)
 {
     if (isRootEditableElement()) {
-        Frame* frame = document().frame();
+        // Keep frame alive in this method, since setSelection() may release the last reference to |frame|.
+        RefPtr<Frame> frame = document().frame();
         if (!frame)
             return;
         
