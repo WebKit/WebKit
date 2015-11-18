ca896f2143d00c360123c5c2b3747eda81d043be
Adam Bergkvist <adam.bergkvist@ericsson.com>
2015-11-18 14:52:29 +0000

    WebRTC: Initial testing of updated RTCPeerConnection API https://bugs.webkit.org/show_bug.cgi?id=151304
    Reviewed by Eric Carlson.
    
    Source/WebCore:
    
    Add an empty implementation of MediaEndpointPeerConnection which
    realizes the PeerConnectionBackend interface. This makes it possible
    to construct an RTCPeerConnection and do initial testing.
    
    Tests: fast/mediastream/RTCPeerConnection-overloaded-operations-params.html
           fast/mediastream/RTCPeerConnection-overloaded-operations.html
    
    Also unskip three existing RTCPeerConnection tests on GTK.
    
    * CMakeLists.txt:
    * Modules/mediastream/MediaEndpointPeerConnection.cpp: Added.
    (WebCore::createMediaEndpointPeerConnection):
    (WebCore::MediaEndpointPeerConnection::MediaEndpointPeerConnection):
    (WebCore::MediaEndpointPeerConnection::createOffer):
    (WebCore::MediaEndpointPeerConnection::createAnswer):
    (WebCore::MediaEndpointPeerConnection::setLocalDescription):
    (WebCore::MediaEndpointPeerConnection::localDescription):
    (WebCore::MediaEndpointPeerConnection::currentLocalDescription):
    (WebCore::MediaEndpointPeerConnection::pendingLocalDescription):
    (WebCore::MediaEndpointPeerConnection::setRemoteDescription):
    (WebCore::MediaEndpointPeerConnection::remoteDescription):
    (WebCore::MediaEndpointPeerConnection::currentRemoteDescription):
    (WebCore::MediaEndpointPeerConnection::pendingRemoteDescription):
    (WebCore::MediaEndpointPeerConnection::setConfiguration):
    (WebCore::MediaEndpointPeerConnection::addIceCandidate):
    (WebCore::MediaEndpointPeerConnection::getStats):
    (WebCore::MediaEndpointPeerConnection::stop):
    (WebCore::MediaEndpointPeerConnection::markAsNeedingNegotiation):
    * Modules/mediastream/MediaEndpointPeerConnection.h: Added.
    * WebCore.xcodeproj/project.pbxproj:
    
    LayoutTests:
    
    Add two new tests and unskip three old ones (details below).
    
    * fast/mediastream/RTCPeerConnection-overloaded-operations-expected.txt: Added.
    * fast/mediastream/RTCPeerConnection-overloaded-operations-params-expected.txt: Added.
    * fast/mediastream/RTCPeerConnection-overloaded-operations-params.html: Added.
    Feeds some good but mostly bad input to the overloaded operations to verify JS built-ins
    argument checking.
    * fast/mediastream/RTCPeerConnection-overloaded-operations.html: Added.
    Verifies that the correct overloaded operation is invoked. For examaple, createOffer()
    without arguments should invoke the promise based function, but the same operation with
    two function arguments should invoke the legacy callback version.
    * platform/gtk/TestExpectations:
    Unskip fast/mediastream/RTCIceCandidate.html, fast/mediastream/RTCPeerConnection.html
    and fast/mediastream/RTCSessionDescription.html.
    
    
    Canonical link: https://commits.webkit.org/169546@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@192575 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebCore/Modules/mediastream/MediaEndpointPeerConnection.cpp
@@ -0,0 +1,168 @@
+/*
+ * Copyright (C) 2015 Ericsson AB. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer
+ *    in the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of Ericsson nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+
+#if ENABLE(MEDIA_STREAM)
+#include "MediaEndpointPeerConnection.h"
+
+#include "DOMError.h"
+#include "JSDOMError.h"
+
+namespace WebCore {
+
+using namespace PeerConnection;
+
+static std::unique_ptr<PeerConnectionBackend> createMediaEndpointPeerConnection(PeerConnectionBackendClient* client)
+{
+    return std::unique_ptr<PeerConnectionBackend>(new MediaEndpointPeerConnection(client));
+}
+
+CreatePeerConnectionBackend PeerConnectionBackend::create = createMediaEndpointPeerConnection;
+
+MediaEndpointPeerConnection::MediaEndpointPeerConnection(PeerConnectionBackendClient* client)
+{
+    UNUSED_PARAM(client);
+}
+
+void MediaEndpointPeerConnection::createOffer(RTCOfferOptions& options, SessionDescriptionPromise&& promise)
+{
+    UNUSED_PARAM(options);
+
+    notImplemented();
+
+    promise.reject(DOMError::create("NotSupportedError"));
+}
+
+void MediaEndpointPeerConnection::createAnswer(RTCAnswerOptions& options, SessionDescriptionPromise&& promise)
+{
+    UNUSED_PARAM(options);
+
+    notImplemented();
+
+    promise.reject(DOMError::create("NotSupportedError"));
+}
+
+void MediaEndpointPeerConnection::setLocalDescription(RTCSessionDescription& description, VoidPromise&& promise)
+{
+    UNUSED_PARAM(description);
+
+    notImplemented();
+
+    promise.reject(DOMError::create("NotSupportedError"));
+}
+
+RefPtr<RTCSessionDescription> MediaEndpointPeerConnection::localDescription() const
+{
+    notImplemented();
+
+    return nullptr;
+}
+
+RefPtr<RTCSessionDescription> MediaEndpointPeerConnection::currentLocalDescription() const
+{
+    notImplemented();
+
+    return nullptr;
+}
+
+RefPtr<RTCSessionDescription> MediaEndpointPeerConnection::pendingLocalDescription() const
+{
+    notImplemented();
+
+    return nullptr;
+}
+
+void MediaEndpointPeerConnection::setRemoteDescription(RTCSessionDescription& description, VoidPromise&& promise)
+{
+    UNUSED_PARAM(description);
+
+    notImplemented();
+
+    promise.reject(DOMError::create("NotSupportedError"));
+}
+
+RefPtr<RTCSessionDescription> MediaEndpointPeerConnection::remoteDescription() const
+{
+    notImplemented();
+
+    return nullptr;
+}
+
+RefPtr<RTCSessionDescription> MediaEndpointPeerConnection::currentRemoteDescription() const
+{
+    notImplemented();
+
+    return nullptr;
+}
+
+RefPtr<RTCSessionDescription> MediaEndpointPeerConnection::pendingRemoteDescription() const
+{
+    notImplemented();
+
+    return nullptr;
+}
+
+void MediaEndpointPeerConnection::setConfiguration(RTCConfiguration& configuration)
+{
+    UNUSED_PARAM(configuration);
+
+    notImplemented();
+}
+
+void MediaEndpointPeerConnection::addIceCandidate(RTCIceCandidate& rtcCandidate, PeerConnection::VoidPromise&& promise)
+{
+    UNUSED_PARAM(rtcCandidate);
+
+    notImplemented();
+
+    promise.reject(DOMError::create("NotSupportedError"));
+}
+
+void MediaEndpointPeerConnection::getStats(MediaStreamTrack&, PeerConnection::StatsPromise&& promise)
+{
+    notImplemented();
+
+    promise.reject(DOMError::create("NotSupportedError"));
+}
+
+void MediaEndpointPeerConnection::stop()
+{
+    notImplemented();
+}
+
+void MediaEndpointPeerConnection::markAsNeedingNegotiation()
+{
+    notImplemented();
+}
+
+} // namespace WebCore
+
+#endif // ENABLE(MEDIA_STREAM)
---
+++ Source/WebCore/Modules/mediastream/MediaEndpointPeerConnection.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2015 Ericsson AB. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer
+ *    in the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of Ericsson nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef MediaEndpointPeerConnection_h
+#define MediaEndpointPeerConnection_h
+
+#if ENABLE(MEDIA_STREAM)
+
+#include "NotImplemented.h"
+#include "PeerConnectionBackend.h"
+#include "RTCSessionDescription.h"
+#include <wtf/RefPtr.h>
+
+namespace WebCore {
+
+class MediaStreamTrack;
+
+class MediaEndpointPeerConnection : public PeerConnectionBackend {
+public:
+    MediaEndpointPeerConnection(PeerConnectionBackendClient*);
+
+    void createOffer(RTCOfferOptions&, PeerConnection::SessionDescriptionPromise&&) override;
+    void createAnswer(RTCAnswerOptions&, PeerConnection::SessionDescriptionPromise&&) override;
+
+    void setLocalDescription(RTCSessionDescription&, PeerConnection::VoidPromise&&) override;
+    RefPtr<RTCSessionDescription> localDescription() const override;
+    RefPtr<RTCSessionDescription> currentLocalDescription() const override;
+    RefPtr<RTCSessionDescription> pendingLocalDescription() const override;
+
+    void setRemoteDescription(RTCSessionDescription&, PeerConnection::VoidPromise&&) override;
+    RefPtr<RTCSessionDescription> remoteDescription() const override;
+    RefPtr<RTCSessionDescription> currentRemoteDescription() const override;
+    RefPtr<RTCSessionDescription> pendingRemoteDescription() const override;
+
+    void setConfiguration(RTCConfiguration&) override;
+    void addIceCandidate(RTCIceCandidate&, PeerConnection::VoidPromise&&) override;
+
+    void getStats(MediaStreamTrack&, PeerConnection::StatsPromise&&) override;
+
+    void stop() override;
+
+    bool isNegotiationNeeded() const override { return false; };
+    void markAsNeedingNegotiation() override;
+    void clearNegotiationNeededState() override { notImplemented(); };
+};
+
+} // namespace WebCore
+
+#endif // ENABLE(MEDIA_STREAM)
+
+#endif // MediaEndpointPeerConnection_h
