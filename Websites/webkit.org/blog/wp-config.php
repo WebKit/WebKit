c0c918b9db5099a493e028812a29c2e26a801985
Timothy Hatcher <timothy@apple.com>
2015-12-04 04:39:04 +0000

    Yet another attempt to fix the http blog redirects.
    Removing the blog folder should do it.
    
    * .htaccess:
    * blog: Removed.
    
    Canonical link: https://commits.webkit.org/169989@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@193405 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Websites/webkit.org/blog/wp-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('/var/www/config/webkit-blog-config.php');
-
-define('WP_CACHE', true); //Added by WP-Cache Manager
-define('ABSPATH', dirname(__FILE__).'/');
-require_once(ABSPATH.'wp-settings.php');
-?>
