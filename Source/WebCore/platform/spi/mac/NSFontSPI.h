a5d3055f9ef37f7555f0f8f7869f495276d3098f
Matt Hanson <matthew_hanson@apple.com>
2015-12-05 18:29:59 +0000

    Merge r187982. rdar://problem/23769732
    Canonical link: https://commits.webkit.org/164912.781@safari-601-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601-branch@193515 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/spi/mac/NSFontSPI.h
+++ Source/WebCore/platform/spi/mac/NSFontSPI.h
@@ -31,9 +31,6 @@
 #else
 
 @interface NSFont (Private)
-+ (NSFont *)findFontLike:(NSFont *)aFont forCharacter:(UInt32)c inLanguage:(id) language;
-+ (NSFont *)findFontLike:(NSFont *)aFont forString:(NSString *)string withRange:(NSRange)range inLanguage:(id) language;
-
 + (NSFont *)systemFontOfSize:(CGFloat)size weight:(CGFloat)weight;
 @end
 
