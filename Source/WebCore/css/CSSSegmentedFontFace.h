6b39891be8811699a885c02b42bf975c84cdd294
David Kilzer <ddkilzer@apple.com>
2015-12-06 22:20:25 +0000

    Merge r188114. rdar://problem/23221171
    Canonical link: https://commits.webkit.org/164912.505@safari-601.1.46-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601.1.46-branch@193595 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/CSSSegmentedFontFace.h
+++ Source/WebCore/css/CSSSegmentedFontFace.h
@@ -26,6 +26,7 @@
 #ifndef CSSSegmentedFontFace_h
 #define CSSSegmentedFontFace_h
 
+#include "FontCache.h"
 #include "FontRanges.h"
 #include <wtf/HashMap.h>
 #include <wtf/PassRefPtr.h>
@@ -73,7 +74,7 @@ private:
 #endif
 
     CSSFontSelector* m_fontSelector;
-    HashMap<unsigned, FontRanges> m_descriptionToRangesMap;
+    HashMap<FontDescriptionFontDataCacheKey, FontRanges, FontDescriptionKeyHash, WTF::SimpleClassHashTraits<FontDescriptionFontDataCacheKey>> m_descriptionToRangesMap; 
     Vector<RefPtr<CSSFontFace>, 1> m_fontFaces;
 #if ENABLE(FONT_LOAD_EVENTS)
     Vector<RefPtr<LoadFontCallback>> m_callbacks;
