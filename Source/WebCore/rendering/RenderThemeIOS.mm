02d8a0fc10187ea6a2018b3139bee9fd7ffc10f8
David Kilzer <ddkilzer@apple.com>
2015-12-06 01:54:22 +0000

    Merge r188146. rdar://problem/23769683
    Canonical link: https://commits.webkit.org/164912.470@safari-601.1.46-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601.1.46-branch@193539 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderThemeIOS.mm
+++ Source/WebCore/rendering/RenderThemeIOS.mm
@@ -1271,6 +1271,8 @@ void RenderThemeIOS::updateCachedSystemFontDescription(CSSValueID valueID, FontD
 
     ASSERT(fontDescriptor);
     RetainPtr<CTFontRef> font = adoptCF(CTFontCreateWithFontDescriptor(fontDescriptor.get(), 0, nullptr));
+    if (font && fontDescription.featureSettings() && fontDescription.featureSettings()->size())
+        font = applyFontFeatureSettings(font.get(), *fontDescription.featureSettings());
     fontDescription.setIsAbsoluteSize(true);
     fontDescription.setOneFamily(textStyle);
     fontDescription.setSpecifiedSize(CTFontGetSize(font.get()));
