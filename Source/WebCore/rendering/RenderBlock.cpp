5be2bdc541a1fc2078b1ebdfd3b0b63c6f9fa4af
Zalan Bujtas <zalan@apple.com>
2015-12-08 20:44:51 +0000

    Do not insert positioned renderers to multiple gPositionedDescendantsMap. https://bugs.webkit.org/show_bug.cgi?id=151878 rdar://problem/22229889
    Reviewed by Simon Fraser.
    
    We insert positioned renderers into a static map (RenderBlock::gPositionedDescendantsMap) to keep track of them.
    This static map is at block level. A particular absolute positioned object is added to its closest ancestor that
    returns true for RenderElement::canContainAbsolutelyPositionedObjects().
    canContainAbsolutelyPositionedObjects() returns true if the ancestor is either positioned or has transform.
    If this container's style changes so that it's no longer positioned and it has no transform anymore,
    we need to clear its static map of positioned objects (they'll get re-inserted to another ancestor at next layout).
    
    This patch addresses the case when the renderer does not have transforms anymore.
    
    Source/WebCore:
    
    Test: fast/block/positioning/crash-when-transform-is-removed.html
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::styleWillChange):
    
    LayoutTests:
    
    * fast/block/positioning/crash-when-transform-is-removed-expected.txt: Added.
    * fast/block/positioning/crash-when-transform-is-removed.html: Added.
    
    Canonical link: https://commits.webkit.org/170156@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@193773 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderBlock.cpp
+++ Source/WebCore/rendering/RenderBlock.cpp
@@ -242,11 +242,14 @@ void RenderBlock::styleWillChange(StyleDifference diff, const RenderStyle& newSt
 
     setReplaced(newStyle.isDisplayInlineType());
 
+    if (oldStyle && oldStyle->hasTransformRelatedProperty() && !newStyle.hasTransformRelatedProperty())
+        removePositionedObjects(nullptr, NewContainingBlock);
+
     if (oldStyle && parent() && diff == StyleDifferenceLayout && oldStyle->position() != newStyle.position()) {
         if (newStyle.position() == StaticPosition)
             // Clear our positioned objects list. Our absolutely positioned descendants will be
             // inserted into our containing block's positioned objects list during layout.
-            removePositionedObjects(0, NewContainingBlock);
+            removePositionedObjects(nullptr, NewContainingBlock);
         else if (oldStyle->position() == StaticPosition) {
             // Remove our absolutely positioned descendants from their current containing block.
             // They will be inserted into our positioned objects list during layout.
