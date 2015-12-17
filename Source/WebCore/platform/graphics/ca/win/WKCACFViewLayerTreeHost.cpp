ca016219df85bc7def8249138a99a2fb5c2775d1
Brent Fulgham <bfulgham@apple.com>
2015-12-17 22:26:15 +0000

    [Win] Prevent flashing/strobing repaints on certain hardware https://bugs.webkit.org/show_bug.cgi?id=152394 <rdar://problem/23875302>
    Reviewed by Simon Fraser.
    
    This patch reverts a change I made in r192166, where I always set the
    m_viewNeedsUpdate flag to true when a 'flushContext' call was made. Instead,
    we should go back to letting the view decide when it needs to paint.
    
    * platform/graphics/ca/win/WKCACFViewLayerTreeHost.cpp:
    (WebCore::WKCACFViewLayerTreeHost::flushContext): Don't just claim that
    the view needs to be updated any time we are asked to flush.
    
    
    Canonical link: https://commits.webkit.org/170505@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@194235 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/ca/win/WKCACFViewLayerTreeHost.cpp
+++ Source/WebCore/platform/graphics/ca/win/WKCACFViewLayerTreeHost.cpp
@@ -162,7 +162,6 @@ CFTimeInterval WKCACFViewLayerTreeHost::lastCommitTime() const
 
 void WKCACFViewLayerTreeHost::flushContext()
 {
-    m_viewNeedsUpdate = true;
     WKCACFViewFlushContext(m_view.get());
 }
 
