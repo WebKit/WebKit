28294a24793e1558e3bcb16d928810d014c39ff1
Alex Christensen <achristensen@webkit.org>
2015-12-17 07:49:45 +0000

    Make SessionID use intHash https://bugs.webkit.org/show_bug.cgi?id=152356
    Patch by Alex Christensen <achristensen@webkit.org> on 2015-12-16
    Reviewed by Darin Adler.
    
    * page/SessionID.h:
    (WTF::SessionIDHash::hash):
    (WTF::SessionIDHash::equal):
    (WTF::HashTraits<WebCore::SessionID>::emptyValue):
    (WTF::HashTraits<WebCore::SessionID>::constructDeletedValue):
    (WTF::HashTraits<WebCore::SessionID>::isDeletedValue):
    Use intHash instead of just casting a uint64_t to an unsigned.
    This reduces hash collisions in HashTables that are usually quite small so it isn't much of a problem.
    Also, needsDestruction hasn't been in HashTraits since r156507.
    
    Canonical link: https://commits.webkit.org/170491@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@194213 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/SessionID.h
+++ Source/WebCore/page/SessionID.h
@@ -56,13 +56,12 @@ namespace WTF {
 
 // The empty value is emptySessionID(), the deleted value is (-1)
 struct SessionIDHash {
-    static unsigned hash(const WebCore::SessionID& p) { return (unsigned)p.sessionID(); }
+    static unsigned hash(const WebCore::SessionID& p) { return intHash(p.sessionID()); }
     static bool equal(const WebCore::SessionID& a, const WebCore::SessionID& b) { return a == b; }
     static const bool safeToCompareToEmptyOrDeleted = true;
 };
 template<> struct HashTraits<WebCore::SessionID> : GenericHashTraits<WebCore::SessionID> {
-    static const uint64_t deletedValueIdentifier = 0xffffffffffffffff;
-    static const bool needsDestruction = false;
+    static const uint64_t deletedValueIdentifier = std::numeric_limits<uint64_t>::max();
     static WebCore::SessionID emptyValue() { return WebCore::SessionID::emptySessionID(); }
 
     static void constructDeletedValue(WebCore::SessionID& slot) { slot = WebCore::SessionID(deletedValueIdentifier); }
