3d20eb88c7bcf001621e49f7f221f08b2f821c87
Matt Hanson <matthew_hanson@apple.com>
2015-12-18 09:59:41 +0000

    Merge r188606. rdar://problem/23905024
    Canonical link: https://commits.webkit.org/164912.877@safari-601-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601-branch@194266 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/C/WKPage.cpp
+++ Source/WebKit2/UIProcess/API/C/WKPage.cpp
@@ -1332,6 +1332,7 @@ void WKPageSetPagePolicyClient(WKPageRef pageRef, const WKPagePolicyClientBase* 
     toImpl(pageRef)->setPolicyClient(std::make_unique<PolicyClient>(wkClient));
 }
 
+#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED <= 101000
 static void fixUpBotchedPageUIClient(WKPageRef pageRef, const WKPageUIClientBase& wkClient)
 {
     struct BotchedWKPageUIClientV4 {
@@ -1463,6 +1464,7 @@ static void fixUpBotchedPageUIClient(WKPageRef pageRef, const WKPageUIClientBase
 
     WKPageSetPageUIClient(pageRef, &fixedPageUIClient.base);
 }
+#endif
 
 namespace WebKit {
 
@@ -1579,7 +1581,7 @@ void WKPageRunJavaScriptPromptResultListenerCall(WKPageRunJavaScriptPromptResult
 
 void WKPageSetPageUIClient(WKPageRef pageRef, const WKPageUIClientBase* wkClient)
 {
-#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED <= 101100
+#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED <= 101000
     if (wkClient && wkClient->version == 4) {
         fixUpBotchedPageUIClient(pageRef, *wkClient);
         return;
