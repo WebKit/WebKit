7db019ad46b35aeeae6cbc3a53d50c5592e78268
Mark Lam <mark.lam@apple.com>
2015-12-21 18:40:10 +0000

    Add unary operator tests to compare JIT and LLINT results. https://bugs.webkit.org/show_bug.cgi?id=152453
    Reviewed by Benjamin Poulain.
    
    Also fixed a few things in the binary-op-test.js.
    
    * tests/stress/op_negate.js: Added.
    (o1.valueOf):
    * tests/stress/op_postdec.js: Added.
    (o1.valueOf):
    * tests/stress/op_postinc.js: Added.
    (o1.valueOf):
    * tests/stress/op_predec.js: Added.
    (o1.valueOf):
    * tests/stress/op_preinc.js: Added.
    (o1.valueOf):
    * tests/stress/resources/binary-op-test.js:
    (stringifyIfNeeded):
    (isIdentical):
    (run):
    * tests/stress/resources/unary-op-test.js: Added.
    (stringifyIfNeeded):
    (generateBinaryTests):
    (isIdentical):
    (runTest):
    (run):
    
    
    
    Canonical link: https://commits.webkit.org/170580@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@194333 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/JavaScriptCore/tests/stress/op_preinc.js
@@ -0,0 +1,75 @@
+//@ runFTLNoCJIT
+
+// If all goes well, this test module will terminate silently. If not, it will print
+// errors. See unary-op-test.js for debugging options if needed.
+
+load("./resources/unary-op-test.js");
+
+//============================================================================
+// Test configuration data:
+
+var opName = "preinc";
+var op = "++";
+var operatorType = "Prefix";
+
+var o1 = {
+    valueOf: function() { return 10; }
+};
+
+var posInfinity = 1 / 0;
+var negInfinity = -1 / 0;
+
+var values = [
+    'o1',
+    'null',
+    'undefined',
+    'true',
+    'false',
+
+    'NaN',
+    'posInfinity',
+    'negInfinity',
+    '100.2', // Some random small double value.
+    '-100.2',
+    '54294967296.2923', // Some random large double value.
+    '-54294967296.2923',
+
+    '0',
+    '-0',
+    '1',
+    '-1',
+    '0x3fff',
+    '-0x3fff',
+    '0x7fff',
+    '-0x7fff',
+    '0x10000',
+    '-0x10000',
+    '0x7ffffff',
+    '-0x7ffffff',
+    '0x80000000',
+    '-0x80000000',
+    '0x100000000',
+    '-0x100000000',
+
+    '"abc"',
+    '"0"',
+    '"-0"',
+    '"1"',
+    '"-1"',
+    '"0x3fff"',
+    '"-0x3fff"',
+    '"0x7fff"',
+    '"-0x7fff"',
+    '"0x10000"',
+    '"-0x10000"',
+    '"0x7ffffff"',
+    '"-0x7ffffff"',
+    '"0x100000000"',
+    '"-0x100000000"',
+];
+
+tests = [];
+generateBinaryTests(tests, opName, operatorType, "ImmediateResult", op, values);
+generateBinaryTests(tests, opName, operatorType, "PostResult", op, values);
+
+run();
--- Source/JavaScriptCore/tests/stress/resources/unary-op-test.js
+++ Source/JavaScriptCore/tests/stress/resources/unary-op-test.js
@@ -1,16 +1,13 @@
-// This test module provides infrastructure for generating and running tests on a binary
+// This test module provides infrastructure for generating and running tests on a unary
 // operator.
 //
-// It works by generating test functions to exercise the specified operator using
-// from permutations of operand value pairs. For each test, it computes the expected
-// result by exercising the test function once (using the LLINT) at test generation time.
+// It works by generating test functions to exercise the specified operator on operand
+// values in a provided set. For each test, it computes the expected result by exercising
+// the test function once (using the LLINT) at test generation time.
 // The test runner later compares the result produced by the function (as it tiers up)
 // against the expected result.
 //
-// The generated tests can exercise the operator using the following operand types.
-//     variable * variable
-//     constant * variable
-//     variable * constant
+// The generated tests will exercise the operator on a variable.
 //
 // If all goes well, this test module will terminate silently. If not, it will print
 // errors.
@@ -35,40 +32,42 @@ function stringifyIfNeeded(x) {
     return x;
 }
 
-// operandTypes are "VarVar", "VarConst", and "ConstVar".
+// operatorTypes can be "Prefix" or "Postfix".
+// resultTypes can be "ImmediateResult" or "PostResult". PostResult is mainly for
+// checking the value of a variable on subsequent inspection, so we can confirm that the
+// postfix operator did do its job.
 var funcIndex = 0;
-function generateBinaryTests(tests, opName, op, operandTypes, leftValues, rightValues) {
-    var funcNamePrefix = opName + operandTypes;
-    for (var i = 0; i < leftValues.length; i++) {
-        for (var j = 0; j < rightValues.length; j++) {
-            var test = { };
-            xStr = leftValues[i];
-            yStr = rightValues[j];
-            test.x = eval(xStr);
-            test.y = eval(yStr);
+function generateBinaryTests(tests, opName, operatorType, resultType, op, inValues) {
+    var funcNamePrefix = opName + resultType;
+    for (var i = 0; i < inValues.length; i++) {
+        var test = { };
+        xStr = inValues[i];
+        test.x = eval(xStr);
 
-            var funcName = funcNamePrefix + funcIndex++;
-            if (operandTypes == "VarVar") {
-                test.signature = funcName + "(x, y) { return x " + op + " y }";
-                test.name = test.signature + " with x:" + xStr + ", y:" + yStr;
-            } else if (operandTypes == "VarConst") {
-                test.signature = funcName + "(x, _) { return x " + op + " " + yStr + " }";
-                test.name = test.signature + " with x:" + xStr;
-            } else if (operandTypes == "ConstVar") {
-                test.signature = funcName + "(_, y) { return " + xStr + " " + op + " y }";
-                test.name = test.signature + " with y:" + yStr;
-            }
-
-            test.func = eval("(function " + test.signature + ")");
-            noInline(test.func);
-
-            test.expectedResult = test.func(test.x, test.y);
-            test.name += ", expected:" + stringifyIfNeeded(test.expectedResult);
-
-            tests.push(test);
-            if (verboseTestGeneration)
-                print("Generated " + test.name);
+        var funcName = funcNamePrefix + funcIndex++;
+        if (operatorType == "Prefix") {
+            if (resultType == "ImmediateResult")
+                test.signature = funcName + "(x) { return " + op + "x }";
+            else if (resultType == "PostResult")
+                test.signature = funcName + "(x) { " + op + "x; return x; }";
+        } else if (operatorType == "Postfix") {
+            if (resultType == "ImmediateResult")
+                test.signature = funcName + "(x) { return x" + op + " }";
+            else if (resultType == "PostResult")
+                test.signature = funcName + "(x) { x" + op + "; return x; }";
         }
+
+        test.name = test.signature + " with x:" + xStr;
+
+        test.func = eval("(function " + test.signature + ")");
+        noInline(test.func);
+
+        test.expectedResult = test.func(test.x);
+        test.name += ", expected:" + stringifyIfNeeded(test.expectedResult);
+
+        tests.push(test);
+        if (verboseTestGeneration)
+            print("Generated " + test.name);
     }
 }
 
@@ -102,7 +101,7 @@ function runTest(test) {
         if (verbose)
             print(test.name);
         for (var i = 0; i < 10000; i++) {
-            var result = test.func(test.x, test.y);
+            var result = test.func(test.x);
             if (isIdentical(result, test.expectedResult))
                 continue;
             if (firstFailed < 0) {
