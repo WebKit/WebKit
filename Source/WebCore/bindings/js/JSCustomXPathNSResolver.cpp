310c64892712d876bab9de0984de750186f5b932
Joseph Pecoraro <pecoraro@apple.com>
2016-01-05 00:22:33 +0000

    JSCustomXPathNSResolver::lookupNamespaceURI Console Message should render with a column number https://bugs.webkit.org/show_bug.cgi?id=114312
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2016-01-04
    Reviewed by Brent Fulgham.
    
    * bindings/js/JSCustomXPathNSResolver.cpp:
    (WebCore::JSCustomXPathNSResolver::lookupNamespaceURI): Deleted.
    This now behaves as expected.
    
    Canonical link: https://commits.webkit.org/170802@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@194564 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/js/JSCustomXPathNSResolver.cpp
+++ Source/WebCore/bindings/js/JSCustomXPathNSResolver.cpp
@@ -80,7 +80,6 @@ String JSCustomXPathNSResolver::lookupNamespaceURI(const String& prefix)
     if (callType == CallTypeNone) {
         callType = m_customResolver->methodTable()->getCallData(m_customResolver.get(), callData);
         if (callType == CallTypeNone) {
-            // FIXME: <http://webkit.org/b/114312> JSCustomXPathNSResolver::lookupNamespaceURI Console Message should include Line, Column, and SourceURL
             if (PageConsoleClient* console = m_globalObject->wrapped().console())
                 console->addMessage(MessageSource::JS, MessageLevel::Error, ASCIILiteral("XPathNSResolver does not have a lookupNamespaceURI method."));
             return String();
