808ed7073e68fd05d651a77a117af6179aafcc60
Ryosuke Niwa <rniwa@webkit.org>
2016-01-06 20:26:58 +0000

    v3 UI's time range slider is harder to use than that of v2 UI https://bugs.webkit.org/show_bug.cgi?id=152780
    Reviewed by Antti Koivisto.
    
    Improved the time range slider by using a cubic mapping to time range and providing a text field
    to directly edit the number of days to show.
    
    Now an user can enter the text mode to directly edit the number of days to show by clicking on
    the number of days (text field is always there with opacity=0).
    
    * public/v3/pages/charts-toolbar.js:
    (ChartsToolbar): Store the minimum and maximum number of days allowed. Also rename _inputElement
    to _slider and added a new type=number text field as _editor.
    (ChartsToolbar.prototype.render):
    (ChartsToolbar.prototype.setStartTime): Exit the text mode when the number of days is changed by
    an URL state transition (i.e. back/forward navigation).
    (ChartsToolbar.prototype._setInputElementValue): Added. Updates the values of _slider and _editor.
    (ChartsToolbar.prototype._enterTextMode): Added. Hide the elements used by the slider mode and
    show the text field.
    (ChartsToolbar.prototype._exitTextMode): Added. Does the opposite.
    (ChartsToolbar.prototype._sliderValueMayHaveChanged): Renamed from _inputValueMayHaveChanged.
    (ChartsToolbar.prototype._editorValueMayHaveChanged): Added. Similar to _sliderValueMayHaveChanged
    but also corrects the value of _editor if needed.
    (ChartsToolbar.prototype._callNumberOfDaysCallback): Extracted from _inputValueMayHaveChanged.
    Also fixed a bug that we didn't update the URL state when the change event was fired without
    modifying the effective number of days.
    (ChartsToolbar.cssTemplate): Tweaked the style to support the new mode. Also set a fixed width on
    the span showing the number of days in the slider mode.
    
    
    Canonical link: https://commits.webkit.org/170880@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@194651 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Websites/perf.webkit.org/public/v3/pages/charts-toolbar.js
+++ Websites/perf.webkit.org/public/v3/pages/charts-toolbar.js
@@ -4,12 +4,21 @@ class ChartsToolbar extends DomainControlToolbar {
     {
         super('chars-toolbar', 7);
 
-        this._numberOfDaysCallback = null;
-        this._inputElement = this.content().querySelector('input');
-        this._labelSpan = this.content().querySelector('.day-count');
+        this._minDayCount = 1;
+        this._maxDayCount = 366;
 
-        this._inputElement.addEventListener('change', this._inputValueMayHaveChanged.bind(this));
-        this._inputElement.addEventListener('mousemove', this._inputValueMayHaveChanged.bind(this));
+        this._numberOfDaysCallback = null;
+        this._slider = this.content().querySelector('.slider');
+        this._slider.addEventListener('change', this._sliderValueMayHaveChanged.bind(this));
+        this._slider.addEventListener('mousemove', this._sliderValueMayHaveChanged.bind(this));
+
+        this._editor = this.content().querySelector('.editor');
+        this._editor.addEventListener('focus', this._enterTextMode.bind(this));
+        this._editor.addEventListener('blur', this._exitTextMode.bind(this));
+        this._editor.addEventListener('input', this._editorValueMayHaveChanged.bind(this));
+        this._editor.addEventListener('change', this._editorValueMayHaveChanged.bind(this));
+
+        this._labelSpan = this.content().querySelector('.day-count');
 
         this._addPaneCallback = null;
         this._paneSelector = this.content().querySelector('pane-selector').component();
@@ -27,7 +36,7 @@ class ChartsToolbar extends DomainControlToolbar {
         super.render();
         this._paneSelector.render();
         this._labelSpan.textContent = this._numberOfDays;
-        this._inputElement.value = this._numberOfDays;
+        this._setInputElementValue(this._numberOfDays);
     }
 
     setNumberOfDaysCallback(callback)
@@ -44,17 +53,61 @@ class ChartsToolbar extends DomainControlToolbar {
 
     setStartTime(startTime)
     {
+        this._exitTextMode();
         if (startTime)
             super.setStartTime(startTime);
         else
             super.setNumberOfDays(7);
     }
 
-    _inputValueMayHaveChanged(event)
+    _setInputElementValue(value)
     {
-        var numberOfDays = parseInt(this._inputElement.value);
-        if (this.numberOfDays() != numberOfDays && this._numberOfDaysCallback)
-            this._numberOfDaysCallback(numberOfDays, event.type == 'change');
+        this._slider.value = Math.pow(value, 1/3);
+        this._slider.min = Math.pow(this._minDayCount, 1/3);
+        this._slider.max = Math.pow(this._maxDayCount, 1/3);
+        this._slider.step = 'any';
+        this._editor.value = value;
+    }
+
+    _enterTextMode(event)
+    {
+        event.preventDefault();
+        this._editor.style.opacity = 1;
+        this._editor.style.marginLeft = '-2.5rem';
+        this._labelSpan.style.opacity = 0;
+        this._slider.style.opacity = 0;
+    }
+
+    _exitTextMode(event)
+    {
+        if (event)
+            event.preventDefault();
+        this._editor.style.opacity = 0;
+        this._editor.style.marginLeft = null;
+        this._labelSpan.style.opacity = 1;
+        this._slider.style.opacity = 1;
+    }
+
+    _sliderValueMayHaveChanged(event)
+    {
+        var numberOfDays = Math.round(Math.pow(parseFloat(this._slider.value), 3));
+        this._callNumberOfDaysCallback(event, numberOfDays);
+    }
+
+    _editorValueMayHaveChanged(event)
+    {
+        var rawNumber = Math.round(parseFloat(this._editor.value));
+        var numberOfDays = Math.max(this._minDayCount, Math.min(this._maxDayCount, rawNumber));
+        if (this._editor.value != numberOfDays)
+            this._editor.value = numberOfDays;
+        this._callNumberOfDaysCallback(event, numberOfDays);
+    }
+
+    _callNumberOfDaysCallback(event, numberOfDays)
+    {
+        var shouldUpdateState = event.type == 'change';
+        if ((this.numberOfDays() != numberOfDays || shouldUpdateState) && this._numberOfDaysCallback)
+            this._numberOfDaysCallback(numberOfDays, shouldUpdateState);
     }
 
     _togglePaneSelector(event)
@@ -101,7 +154,13 @@ class ChartsToolbar extends DomainControlToolbar {
                     <li><a href="#" class="pane-selector-opener">Add pane</a></li>
                 </ul>
                 <ul class="buttoned-toolbar">
-                    <li class="start-time-slider"><label><input type="range" min="7" max="365" step="1"><span class="day-count">?</span> days</label></li>
+                    <li class="start-time-slider">
+                        <label>
+                            <input class="slider" type="range">
+                            <input class="editor" type="number">
+                            <span class="label"><span class="day-count" tabindex="0">?</span> days</span>
+                        </label>
+                    </li>
                 </ul>
                 <div class="pane-selector-container">
                     <pane-selector></pane-selector>
@@ -134,8 +193,8 @@ class ChartsToolbar extends DomainControlToolbar {
                 -webkit-backdrop-filter: blur(0.5rem);
             }
 
-            .start-time-slider {
-                padding-left: 1rem;
+            .buttoned-toolbar .start-time-slider {
+                margin-left: 2rem;
                 line-height: 1em;
                 font-size: 0.9rem;
             }
@@ -144,14 +203,26 @@ class ChartsToolbar extends DomainControlToolbar {
                 display: inline-block;
             }
 
-            .start-time-slider input {
+            .start-time-slider .slider {
                 height: 0.8rem;
             }
 
-            .start-time-slider .numberOfDays {
+            .start-time-slider .editor {
+                position: absolute;
+                opacity: 0;
+                width: 4rem;
+                font-weight: inherit;
+                font-size: 0.8rem;
+                height: 0.9rem;
+                outline: none;
+                border: solid 1px #ccc;
+                z-index: 5;
+            }
+
+            .start-time-slider .day-count {
                 display: inline-block;
                 text-align: right;
-                width: 1.5rem;
+                width: 2rem;
             }
         `;
     }
