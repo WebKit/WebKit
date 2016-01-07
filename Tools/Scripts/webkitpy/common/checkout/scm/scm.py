a56cb662d5816e5010f556b7a5a09328fe70d5a7
Youenn Fablet <youennf@gmail.com>
2016-01-07 14:00:00 +0000

    [buildbot] clean-build script should remove untracked files and revert local changes too https://bugs.webkit.org/show_bug.cgi?id=142400
    Reviewed by Ryosuke Niwa.
    
    This patch cleans the WebKit folder by reverting tracked files changes and deleting SCM untracked files, including SCM ignored files.
    A helper routine SCM.discard_untracked_files is added for that purpose.
    
    * BuildSlaveSupport/clean-build:
    (main): Making call to Scripts/clean-webkit
    * Scripts/clean-webkit: Added.
    (main): Revert changes and delete untracked files.
    * Scripts/webkitpy/common/checkout/scm/scm.py:
    (SCM.discard_untracked_files): Helper function to discard untracked files or folders found by SCM.
    * Scripts/webkitpy/common/checkout/scm/scm_unittest.py:
    (SCMTest._shared_test_discard_untracked_files):
    (test_discard_untracked_files): Tests that untracked file and untracked folder get discarded correctly.
    
    
    Canonical link: https://commits.webkit.org/170916@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@194696 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/checkout/scm/scm.py
+++ Tools/Scripts/webkitpy/common/checkout/scm/scm.py
@@ -207,6 +207,13 @@ class SCM:
     def untracked_files(self, include_ignored_files=False):
         self._subclass_must_implement()
 
+    def discard_untracked_files(self, discard_ignored_files=False):
+        for filename in self.untracked_files(discard_ignored_files):
+            if self._filesystem.isdir(filename):
+                self._filesystem.rmtree(filename)
+            else:
+                self._filesystem.remove(filename)
+
     def discard_working_directory_changes(self):
         self._subclass_must_implement()
 
