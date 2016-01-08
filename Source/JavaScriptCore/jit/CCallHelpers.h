2d9e28003f2761456bcc971a7972ed7dffa74672
Konstantin Tokarev <annulen@yandex.ru>
2016-01-08 16:55:08 +0000

    [mips] Fixed unused parameter warnings https://bugs.webkit.org/show_bug.cgi?id=152885
    Patch by Konstantin Tokarev <annulen@yandex.ru> on 2016-01-08
    Reviewed by Mark Lam.
    
    * jit/CCallHelpers.h:
    (JSC::CCallHelpers::setupArgumentsWithExecState):
    
    Canonical link: https://commits.webkit.org/170980@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@194766 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/jit/CCallHelpers.h
+++ Source/JavaScriptCore/jit/CCallHelpers.h
@@ -1098,12 +1098,12 @@ public:
         poke(arg3, 4);
     }
 
-    ALWAYS_INLINE void setupArgumentsWithExecState(TrustedImm32 arg1, FPRReg arg2, GPRReg arg3)
+    ALWAYS_INLINE void setupArgumentsWithExecState(TrustedImm32, FPRReg arg2, GPRReg arg3)
     {
         setupArgumentsWithExecState(arg2, arg3);
     }
 
-    ALWAYS_INLINE void setupArgumentsWithExecState(GPRReg arg1, GPRReg arg2, TrustedImm32 arg3, FPRReg arg4)
+    ALWAYS_INLINE void setupArgumentsWithExecState(GPRReg arg1, GPRReg arg2, TrustedImm32, FPRReg arg4)
     {
         setupArgumentsWithExecState(arg1, arg2, arg4);
     }
