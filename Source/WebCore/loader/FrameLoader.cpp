0f9abdb3de15b1d7daf2763a96c5884a19c305ae
Matt Hanson <matthew_hanson@apple.com>
2016-01-13 01:41:37 +0000

    Merge r194910. rdar://problem/24101255
    Canonical link: https://commits.webkit.org/164912.898@safari-601-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601-branch@194945 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/FrameLoader.cpp
+++ Source/WebCore/loader/FrameLoader.cpp
@@ -1464,6 +1464,15 @@ void FrameLoader::reportLocalLoadFailed(Frame* frame, const String& url)
     frame->document()->addConsoleMessage(MessageSource::Security, MessageLevel::Error, "Not allowed to load local resource: " + url);
 }
 
+void FrameLoader::reportBlockedPortFailed(Frame* frame, const String& url)
+{
+    ASSERT(!url.isEmpty());
+    if (!frame)
+        return;
+    
+    frame->document()->addConsoleMessage(MessageSource::Security, MessageLevel::Error, "Not allowed to use restricted network port: " + url);
+}
+
 const ResourceRequest& FrameLoader::initialRequest() const
 {
     return activeDocumentLoader()->originalRequest();
@@ -3346,6 +3355,13 @@ void FrameLoader::retryAfterFailedCacheOnlyMainResourceLoad()
 ResourceError FrameLoader::cancelledError(const ResourceRequest& request) const
 {
     ResourceError error = m_client.cancelledError(request);
+    error.setIsCancellation(true);
+    return error;
+}
+
+ResourceError FrameLoader::blockedError(const ResourceRequest& request) const
+{
+    ResourceError error = m_client.blockedError(request);
     error.setIsCancellation(true);
     return error;
 }
