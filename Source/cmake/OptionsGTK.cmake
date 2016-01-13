6171c1402c4dba7ba3941bbbf3ca5d0b1bd3e4bf
Carlos Garcia Campos <cgarcia@igalia.com>
2016-01-13 08:12:57 +0000

    Unreviewed. Update OptionsGTK.cmake and NEWS for 2.11.3 release.
    .:
    
    * Source/cmake/OptionsGTK.cmake: Bump version numbers.
    
    Source/WebKit2:
    
    * gtk/NEWS: Add release notes for 2.11.3.
    
    Canonical link: https://commits.webkit.org/171142@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@194958 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/OptionsGTK.cmake
+++ Source/cmake/OptionsGTK.cmake
@@ -2,7 +2,7 @@ include(GNUInstallDirs)
 
 set(PROJECT_VERSION_MAJOR 2)
 set(PROJECT_VERSION_MINOR 11)
-set(PROJECT_VERSION_MICRO 2)
+set(PROJECT_VERSION_MICRO 3)
 set(PROJECT_VERSION ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_MICRO})
 set(WEBKITGTK_API_VERSION 4.0)
 
@@ -15,8 +15,8 @@ endif ()
 
 # Libtool library version, not to be confused with API version.
 # See http://www.gnu.org/software/libtool/manual/html_node/Libtool-versioning.html
-CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT2 49 1 12)
-CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(JAVASCRIPTCORE 21 1 3)
+CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT2 50 0 13)
+CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(JAVASCRIPTCORE 21 2 3)
 
 # These are shared variables, but we special case their definition so that we can use the
 # CMAKE_INSTALL_* variables that are populated by the GNUInstallDirs macro.
