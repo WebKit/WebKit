cbeadab325ae75e4762cc5ebce862d0b474510fb
Nikos Andronikos <nikos.andronikos-webkit@cisra.canon.com.au>
2016-01-14 16:31:58 +0000

    [CMake][GTK] CMake Error: Could not create named generator Eclipse CDT4 - Ninja https://bugs.webkit.org/show_bug.cgi?id=132599
    Patch by Nikos Andronikos <nikos.andronikos-webkit@cisra.canon.com.au> on 2016-01-14
    Reviewed by Michael Catanzaro.
    
    Check if cmake Ninja generators are installed.
    
    * Scripts/webkitdirs.pm:
    (canUseNinjaGenerator):
    (canUseEclipseNinjaGenerator):
    (generateBuildSystemFromCMakeProject):
    (canUseEclipse): Deleted.
    
    Canonical link: https://commits.webkit.org/171184@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@195057 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -1909,9 +1909,18 @@ sub canUseNinja(@)
     return commandExists("ninja") || commandExists("ninja-build");
 }
 
-sub canUseEclipse(@)
+sub canUseNinjaGenerator(@)
 {
-    return commandExists("eclipse");
+    # Check that a Ninja generator is installed
+    my $devnull = File::Spec->devnull();
+    return exitStatus(system("cmake -N -G Ninja >$devnull 2>&1")) == 0;
+}
+
+sub canUseEclipseNinjaGenerator(@)
+{
+    # Check that eclipse and eclipse Ninja generator is installed
+    my $devnull = File::Spec->devnull();
+    return commandExists("eclipse") && exitStatus(system("cmake -N -G 'Eclipse CDT4 - Ninja' >$devnull 2>&1")) == 0;
 }
 
 sub cmakeGeneratedBuildfile(@)
@@ -1936,7 +1945,7 @@ sub generateBuildSystemFromCMakeProject
     chdir($buildPath) or die;
 
     # We try to be smart about when to rerun cmake, so that we can have faster incremental builds.
-    my $willUseNinja = canUseNinja();
+    my $willUseNinja = canUseNinja() && canUseNinjaGenerator();
     if (-e cmakeCachePath() && -e cmakeGeneratedBuildfile($willUseNinja)) {
         return 0;
     }
@@ -1953,7 +1962,7 @@ sub generateBuildSystemFromCMakeProject
 
     if ($willUseNinja) {
         push @args, "-G";
-        if (canUseEclipse()) {
+        if (canUseEclipseNinjaGenerator()) {
             push @args, "'Eclipse CDT4 - Ninja'";
         } else {
             push @args, "Ninja";
