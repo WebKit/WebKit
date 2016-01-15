fd4f2fb55dc82b5de3536931ef20d5a26fc23597
Youenn Fablet <youennf@gmail.com>
2016-01-15 09:11:52 +0000

    CORS: Fix the handling of redirected request containing Origin null. https://bugs.webkit.org/show_bug.cgi?id=128816
    Reviewed by Brent Fulgham.
    
    Source/WebCore:
    
    Merging Blink patch from George Ancil (https://chromiumcodereview.appspot.com/20735002).
    
    This patch removes the check for securityOrigin->isUnique() in passesAccessControlCheck().
    This check prevented a redirected request with "Origin: null" from being
    successful even when the response contains "Access-Control-Allow-Origin: null"
    
    Tests: http/tests/xmlhttprequest/access-control-sandboxed-iframe-allow-origin-null.html
           http/tests/xmlhttprequest/redirect-cors-origin-null.html
    
    * loader/CrossOriginAccessControl.cpp:
    (WebCore::passesAccessControlCheck):
    
    LayoutTests:
    
    Merging Blink patch from George Ancil (https://chromiumcodereview.appspot.com/20735002)
    
    Added two tests to check CORS with Origin null in HTTP redirect and iframe cases.
    Updated two test sandboxed iframes test expectations (requests are still denied but error messages are different).
    
    * http/tests/xmlhttprequest/access-control-sandboxed-iframe-allow-origin-null-expected.txt: Added.
    * http/tests/xmlhttprequest/access-control-sandboxed-iframe-allow-origin-null.html: Added.
    * http/tests/xmlhttprequest/access-control-sandboxed-iframe-denied-expected.txt:
    * http/tests/xmlhttprequest/access-control-sandboxed-iframe-denied-without-wildcard-expected.txt:
    * http/tests/xmlhttprequest/redirect-cors-origin-null-expected.txt: Added.
    * http/tests/xmlhttprequest/redirect-cors-origin-null.html: Added.
    * http/tests/xmlhttprequest/resources/access-control-sandboxed-iframe-allow-origin-null-iframe.html: Added.
    * http/tests/xmlhttprequest/resources/access-control-sandboxed-iframe-allow-origin-null.cgi: Added.
    * http/tests/xmlhttprequest/resources/redirect-cors-origin-null-pass.php: Added.
    * http/tests/xmlhttprequest/resources/redirect-cors-origin-null.php: Added.
    
    
    Canonical link: https://commits.webkit.org/171216@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@195100 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/CrossOriginAccessControl.cpp
+++ Source/WebCore/loader/CrossOriginAccessControl.cpp
@@ -141,11 +141,6 @@ bool passesAccessControlCheck(const ResourceResponse& response, StoredCredential
     if (accessControlOriginString == "*" && includeCredentials == DoNotAllowStoredCredentials)
         return true;
 
-    if (securityOrigin->isUnique()) {
-        errorDescription = "Cannot make any requests from " + securityOrigin->toString() + ".";
-        return false;
-    }
-
     // FIXME: Access-Control-Allow-Origin can contain a list of origins.
     if (accessControlOriginString != securityOrigin->toString()) {
         if (accessControlOriginString == "*")
