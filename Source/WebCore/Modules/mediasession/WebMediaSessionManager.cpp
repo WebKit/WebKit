2c941f14e64b0af7500ce4ff2acfb6567d498d76
Eric Carlson <eric.carlson@apple.com>
2016-01-26 17:17:49 +0000

    LayoutTest media/airplay-target-availability.html is flaky https://bugs.webkit.org/show_bug.cgi?id=153100 <rdar://problem/24346796>
    Reviewed by Daniel Bates.
    
    Source/WebCore:
    
    No new tests, media/airplay-target-availability.html was updated
    
    * Modules/mediasession/WebMediaSessionManager.cpp:
    (WebCore::WebMediaSessionManager::clientStateDidChange): Schedule a configuration scan if
      any of the config flags have changed.
    (WebCore::WebMediaSessionManager::configurePlaybackTargetMonitoring): Update logging.
    
    LayoutTests:
    
    * media/airplay-target-availability-expected.txt:
    * media/airplay-target-availability.html:
    
    
    Canonical link: https://commits.webkit.org/171546@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@195592 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/mediasession/WebMediaSessionManager.cpp
+++ Source/WebCore/Modules/mediasession/WebMediaSessionManager.cpp
@@ -215,7 +215,7 @@ void WebMediaSessionManager::clientStateDidChange(WebMediaSessionManagerClient& 
     changedClientState->flags = newFlags;
 
     MediaProducer::MediaStateFlags updateConfigurationFlags = MediaProducer::RequiresPlaybackTargetMonitoring | MediaProducer::HasPlaybackTargetAvailabilityListener | MediaProducer::HasAudioOrVideo;
-    if (!flagsAreSet(oldFlags, updateConfigurationFlags) && flagsAreSet(newFlags, updateConfigurationFlags))
+    if ((oldFlags & updateConfigurationFlags) != (newFlags & updateConfigurationFlags))
         scheduleDelayedTask(TargetMonitoringConfigurationTask);
 
     MediaProducer::MediaStateFlags playingToTargetFlags = MediaProducer::IsPlayingToExternalDevice | MediaProducer::IsPlayingVideo;
@@ -358,7 +358,7 @@ void WebMediaSessionManager::configurePlaybackTargetMonitoring()
             haveClientWithMedia = true;
     }
 
-    LOG(Media, "WebMediaSessionManager::configurePlaybackTargetMonitoring - monitoringRequired = %i", (int)monitoringRequired);
+    LOG(Media, "WebMediaSessionManager::configurePlaybackTargetMonitoring - monitoringRequired = %i", static_cast<int>(monitoringRequired || (hasAvailabilityListener && haveClientWithMedia)));
 
     if (monitoringRequired || (hasAvailabilityListener && haveClientWithMedia))
         targetPicker().startingMonitoringPlaybackTargets();
