1db177c10b9281f7d5af387bee2af7da86e4a5e7
Simon Fraser <simon.fraser@apple.com>
2016-01-27 13:21:05 +0000

    Merge r195445 - REGRESSION (r168244): Content in horizontal-bt page is offset such that only the end is viewable and there is a white gap at the top https://bugs.webkit.org/show_bug.cgi?id=136019
    Reviewed by Dan Bernstein.
    
    Source/WebCore:
    
    In horizontal-bt documents (where the page starts scrolled to the bottom, and scrolling up goes into negative scroll positions),
    the position of the root content layer would be set incorrectly by the scrolling thread, resulting in misplaced
    content.
    
    Fix by having the renamed "yPositionForRootContentLayer" take scroll origin into
    account, and being more consistent about using scrollOrigin to position this layer.
    
    Test: fast/scrolling/programmatic-horizontal-bt-document-scroll.html
    
    * page/FrameView.cpp:
    (WebCore::FrameView::yPositionForFooterLayer): Moved
    (WebCore::FrameView::positionForRootContentLayer): Take scrollOrigin, and subtract it from the computed value.
    (WebCore::FrameView::yPositionForRootContentLayer): Renamed.
    * page/FrameView.h:
    * page/scrolling/AsyncScrollingCoordinator.cpp:
    (WebCore::AsyncScrollingCoordinator::updateScrollPositionAfterAsyncScroll): We've already pushed the new scrollPosition onto the FrameView,
    so we can just use the member function to compute the positionForContentsLayer.
    * page/scrolling/mac/ScrollingTreeFrameScrollingNodeMac.mm:
    (WebCore::ScrollingTreeFrameScrollingNodeMac::setScrollLayerPosition): This is the bug fix; FrameView::positionForRootContentLayer()
    now takes scrollOrigin into account.
    * rendering/RenderLayerCompositor.cpp:
    (WebCore::RenderLayerCompositor::updateRootLayerPosition): Rather than using the documentRect, position the root content layer
    in terms of the scroll origin (which is -documentRect.location()).
    
    Source/WebKit2:
    
    Now call frameView.positionForRootContentLayer(), and add a FIXME questioning the
    behavior in horizontal b-t documents. However, this code isn't hit now that we always
    do extended backgrounds, so never have shadow layers.
    
    * WebProcess/WebPage/mac/TiledCoreAnimationDrawingArea.mm:
    (WebKit::shadowLayerPositionForFrame):
    
    LayoutTests:
    
    Test that scrolls a horizontal-bt document.
    
    * fast/scrolling/programmatic-horizontal-bt-document-scroll-expected.html: Added.
    * fast/scrolling/programmatic-horizontal-bt-document-scroll.html: Added.
    
    Canonical link: https://commits.webkit.org/166077.420@webkitgtk/2.10
    git-svn-id: https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-2.10@195664 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/scrolling/AsyncScrollingCoordinator.cpp
+++ Source/WebCore/page/scrolling/AsyncScrollingCoordinator.cpp
@@ -329,10 +329,9 @@ void AsyncScrollingCoordinator::updateScrollPositionAfterAsyncScroll(ScrollingNo
 
             float topContentInset = frameView.topContentInset();
             FloatPoint positionForInsetClipLayer = FloatPoint(0, FrameView::yPositionForInsetClipLayer(scrollPosition, topContentInset));
-            FloatPoint positionForContentsLayer = FloatPoint(scrolledContentsLayer->position().x(),
-                FrameView::yPositionForRootContentLayer(scrollPosition, topContentInset, frameView.headerHeight()));
-            FloatPoint positionForHeaderLayer = FloatPoint(scrollOffsetForFixed.width(), FrameView::yPositionForHeaderLayer(scrollPosition, topContentInset));
-            FloatPoint positionForFooterLayer = FloatPoint(scrollOffsetForFixed.width(),
+            FloatPoint positionForContentsLayer = frameView.positionForRootContentLayer();
+            FloatPoint positionForHeaderLayer = FloatPoint(scrollPositionForFixed.x(), FrameView::yPositionForHeaderLayer(scrollPosition, topContentInset));
+            FloatPoint positionForFooterLayer = FloatPoint(scrollPositionForFixed.x(),
                 FrameView::yPositionForFooterLayer(scrollPosition, topContentInset, frameView.totalContentsSize().height(), frameView.footerHeight()));
 
             if (programmaticScroll || scrollingLayerPositionAction == SetScrollingLayerPosition) {
