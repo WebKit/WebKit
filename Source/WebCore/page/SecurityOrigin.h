bb464fcdaeb1391bf0d3f8f9cde7486ed187dfc8
Matt Hanson <matthew_hanson@apple.com>
2016-01-28 01:34:12 +0000

    Merge r195075. rdar://problem/24302727
    Canonical link: https://commits.webkit.org/164912.928@safari-601-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-601-branch@195715 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/SecurityOrigin.h
+++ Source/WebCore/page/SecurityOrigin.h
@@ -156,6 +156,7 @@ public:
     bool canAccessPluginStorage(const SecurityOrigin* topOrigin) const { return canAccessStorage(topOrigin); }
     bool canAccessApplicationCache(const SecurityOrigin* topOrigin) const { return canAccessStorage(topOrigin); }
     bool canAccessCookies() const { return !isUnique(); }
+    bool canRequestGeolocation() const { return !isUnique(); }
     Policy canShowNotifications() const;
 
     // The local SecurityOrigin is the most privileged SecurityOrigin.
