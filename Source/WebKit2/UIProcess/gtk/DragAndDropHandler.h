1649729af88eff4bc6660cc53249e3e33b1e3133
Mario Sanchez Prada <mario@webkit.org>
2016-01-29 16:53:29 +0000

    Merge r195811 - [GTK] WebProcess crashes when quickly attempting many DnD operations https://bugs.webkit.org/show_bug.cgi?id=138468
    Reviewed by Michael Catanzaro.
    
    Guard all the new DnD-related code under GTK_CHECK_VERSION #if's to
    make sure we don't bump the required version of GTK+ up to 3.16, and
    it's buildable again with GTK+ >= 3.6.
    
    * UIProcess/gtk/DragAndDropHandler.cpp:
    (WebKit::DragAndDropHandler::DragAndDropHandler):
    (WebKit::DragAndDropHandler::startDrag):
    (WebKit::DragAndDropHandler::fillDragData):
    (WebKit::DragAndDropHandler::finishDrag):
    * UIProcess/gtk/DragAndDropHandler.h:
    
    Canonical link: https://commits.webkit.org/166077.426@webkitgtk/2.10
    git-svn-id: https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-2.10@195812 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/gtk/DragAndDropHandler.h
+++ Source/WebKit2/UIProcess/gtk/DragAndDropHandler.h
@@ -30,6 +30,7 @@
 
 #include <WebCore/DataObjectGtk.h>
 #include <WebCore/IntPoint.h>
+#include <gtk/gtk.h>
 #include <wtf/HashMap.h>
 #include <wtf/Noncopyable.h>
 
@@ -74,9 +75,16 @@ private:
     WebCore::DataObjectGtk* requestDragData(GdkDragContext*, const WebCore::IntPoint& position, unsigned time);
 
     WebPageProxy& m_page;
+    HashMap<GdkDragContext*, std::unique_ptr<DroppingContext>> m_droppingContexts;
+
+#if GTK_CHECK_VERSION(3, 16, 0)
     GRefPtr<GdkDragContext> m_dragContext;
     RefPtr<WebCore::DataObjectGtk> m_draggingDataObject;
-    HashMap<GdkDragContext*, std::unique_ptr<DroppingContext>> m_droppingContexts;
+#else
+    // We don't have gtk_drag_cancel() in GTK+ < 3.16, so we use the old code.
+    // See https://bugs.webkit.org/show_bug.cgi?id=138468
+    HashMap<GdkDragContext*, RefPtr<WebCore::DataObjectGtk>> m_draggingDataObjects;
+#endif
 };
 
 } // namespace WebKit
