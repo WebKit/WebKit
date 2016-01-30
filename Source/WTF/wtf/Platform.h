3001c4e5e646c8647305d83b54df2a07bd2ec3ef
Ada Chan <adachan@apple.com>
2016-01-30 19:30:49 +0000

    Enable VIDEO_PRESENTATION_MODE only in Debug and Release builds on Mac https://bugs.webkit.org/show_bug.cgi?id=153665
    Reviewed by Dan Bernstein.
    
    Source/JavaScriptCore:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebCore:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebKit/mac:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebKit2:
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WTF:
    
    * wtf/Platform.h:
    Remove this as the flag is already defined in FeatureDefines.xcconfig files.
    
    Canonical link: https://commits.webkit.org/171779@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@195901 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/Platform.h
+++ Source/WTF/wtf/Platform.h
@@ -1144,10 +1144,6 @@
 #define USE_MEDIATOOLBOX 1
 #endif
 
-#if PLATFORM(IOS) || (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101200)
-#define ENABLE_VIDEO_PRESENTATION_MODE 1
-#endif
-
 /* While 10.10 has support for fences, it is missing some API important for our integration of them. */
 #if PLATFORM(IOS) || (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101100)
 #define HAVE_COREANIMATION_FENCES 1
