c34972d5a49b41979803ae5e030f8f55ea13533b
Philippe Normand <philn@igalia.com>
2016-02-22 11:49:09 +0000

    Merge r196809 - [GStreamer] clean-up various leaks https://bugs.webkit.org/show_bug.cgi?id=154285
    Reviewed by Carlos Garcia Campos.
    
    * platform/audio/gstreamer/WebKitWebAudioSourceGStreamer.cpp:
    (webkit_web_audio_src_init): Take full ownership of the GstTask.
    * platform/graphics/gstreamer/GRefPtrGStreamer.cpp:
    (WTF::adoptGRef): Null pointer support in ASSERTs.
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
    (WebCore::initializeGStreamerAndRegisterWebKitElements): Take full ownership of the GstElementFactory pointers.
    (WebCore::MediaPlayerPrivateGStreamer::isAvailable): Ditto.
    
    Canonical link: https://commits.webkit.org/166077.462@webkitgtk/2.10
    git-svn-id: https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-2.10@196933 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/gstreamer/GRefPtrGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/GRefPtrGStreamer.cpp
@@ -27,7 +27,7 @@ namespace WTF {
 
 template <> GRefPtr<GstElement> adoptGRef(GstElement* ptr)
 {
-    ASSERT(!ptr || !g_object_is_floating(G_OBJECT(ptr)));
+    ASSERT(!ptr || !g_object_is_floating(ptr));
     return GRefPtr<GstElement>(ptr, GRefPtrAdopt);
 }
 
@@ -47,7 +47,7 @@ template <> void derefGPtr<GstElement>(GstElement* ptr)
 
 template <> GRefPtr<GstPad> adoptGRef(GstPad* ptr)
 {
-    ASSERT(!ptr || !g_object_is_floating(G_OBJECT(ptr)));
+    ASSERT(!ptr || !g_object_is_floating(ptr));
     return GRefPtr<GstPad>(ptr, GRefPtrAdopt);
 }
 
@@ -67,7 +67,7 @@ template <> void derefGPtr<GstPad>(GstPad* ptr)
 
 template <> GRefPtr<GstPadTemplate> adoptGRef(GstPadTemplate* ptr)
 {
-    ASSERT(!ptr || !g_object_is_floating(G_OBJECT(ptr)));
+    ASSERT(!ptr || !g_object_is_floating(ptr));
     return GRefPtr<GstPadTemplate>(ptr, GRefPtrAdopt);
 }
 
@@ -103,10 +103,10 @@ template <> void derefGPtr<GstCaps>(GstCaps* ptr)
         gst_caps_unref(ptr);
 }
 
-
 template <> GRefPtr<GstTask> adoptGRef(GstTask* ptr)
 {
-    ASSERT(!g_object_is_floating(G_OBJECT(ptr)));
+    // There is no need to check the object reference is floating here because
+    // gst_task_init() always sinks it.
     return GRefPtr<GstTask>(ptr, GRefPtrAdopt);
 }
 
@@ -126,7 +126,7 @@ template <> void derefGPtr<GstTask>(GstTask* ptr)
 
 template <> GRefPtr<GstBus> adoptGRef(GstBus* ptr)
 {
-    ASSERT(!g_object_is_floating(G_OBJECT(ptr)));
+    ASSERT(!ptr || !g_object_is_floating(ptr));
     return GRefPtr<GstBus>(ptr, GRefPtrAdopt);
 }
 
@@ -146,7 +146,7 @@ template <> void derefGPtr<GstBus>(GstBus* ptr)
 
 template <> GRefPtr<GstElementFactory> adoptGRef(GstElementFactory* ptr)
 {
-    ASSERT(!g_object_is_floating(G_OBJECT(ptr)));
+    ASSERT(!ptr || !g_object_is_floating(ptr));
     return GRefPtr<GstElementFactory>(ptr, GRefPtrAdopt);
 }
 
