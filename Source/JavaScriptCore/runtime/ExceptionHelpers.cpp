3de09d0e17f0c214b8464745639c75675a99b2c6
Joseph Pecoraro <pecoraro@apple.com>
2016-03-02 04:30:45 +0000

    Simplify some StringBuilder appends https://bugs.webkit.org/show_bug.cgi?id=154902
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2016-03-01
    Reviewed by Mark Lam.
    
    * runtime/ExceptionHelpers.cpp:
    (JSC::notAFunctionSourceAppender):
    * runtime/SamplingProfiler.cpp:
    (JSC::SamplingProfiler::stackTracesAsJSON):
    Use StringBuilder::append(char) instead of append(char*) where possible.
    
    Canonical link: https://commits.webkit.org/172992@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@197438 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/ExceptionHelpers.cpp
+++ Source/JavaScriptCore/runtime/ExceptionHelpers.cpp
@@ -186,7 +186,7 @@ static String notAFunctionSourceAppender(const String& originalMessage, const St
     if (type == TypeObject)
         builder.appendLiteral("an instance of ");
     builder.append(displayValue);
-    builder.appendLiteral(")");
+    builder.append(')');
 
     return builder.toString();
 }
--- Source/JavaScriptCore/runtime/SamplingProfiler.cpp
+++ Source/JavaScriptCore/runtime/SamplingProfiler.cpp
@@ -731,29 +731,29 @@ String SamplingProfiler::stackTracesAsJSON()
     }
 
     StringBuilder json;
-    json.appendLiteral("[");
+    json.append('[');
 
     bool loopedOnce = false;
     auto comma = [&] {
         if (loopedOnce)
-            json.appendLiteral(",");
+            json.append(',');
     };
     for (StackTrace& stackTrace : m_stackTraces) {
         comma();
-        json.appendLiteral("[");
+        json.append('[');
         loopedOnce = false;
         for (StackFrame& stackFrame : stackTrace.frames) {
             comma();
-            json.appendLiteral("\"");
+            json.append('"');
             json.append(stackFrame.displayNameForJSONTests(m_vm));
-            json.appendLiteral("\"");
+            json.append('"');
             loopedOnce = true;
         }
-        json.appendLiteral("]");
+        json.append(']');
         loopedOnce = true;
     }
 
-    json.appendLiteral("]");
+    json.append(']');
 
     clearData(locker);
 
