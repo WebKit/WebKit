6119b2e06c0ea576788af57dc8d586576e8f05f7
Gavin Barraclough <barraclough@apple.com>
2016-03-07 20:28:30 +0000

    Last opened tab does not receive SetHiddenPageTimerThrottlingIncreaseLimit message https://bugs.webkit.org/show_bug.cgi?id=155126
    Reviewed by Chris Dumez.
    
    WebPageProxy will increment the hidden page throttling counter from within its constructor,
    which currently causes the WebProcessPool to try to synchronously message all processes.
    The web page triggering the increment then fails to receive the message, since it was
    still being constructed. Make the update of the limit async, such that the page is fully
    constructed before the WebProcessPool processes the update.
    
    * UIProcess/WebProcessPool.cpp:
    (WebKit::m_hiddenPageThrottlingAutoIncreasesCounter):
        - This now schedules a zero-delay timer to handle the update.
    (WebKit::m_hiddenPageThrottlingTimer):
        - This is now responsible for calling updateHiddenPageThrottlingAutoIncreaseLimit.
    * UIProcess/WebProcessPool.h:
        - Added m_hiddenPageThrottlingTimer.
    
    
    
    Canonical link: https://commits.webkit.org/173210@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@197698 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/WebProcessPool.h
+++ Source/WebKit2/UIProcess/WebProcessPool.h
@@ -522,6 +522,7 @@ private:
     UserObservablePageCounter m_userObservablePageCounter;
     ProcessSuppressionDisabledCounter m_processSuppressionDisabledForPageCounter;
     HiddenPageThrottlingAutoIncreasesCounter m_hiddenPageThrottlingAutoIncreasesCounter;
+    WebCore::Timer m_hiddenPageThrottlingTimer;
 
 #if PLATFORM(COCOA)
     RetainPtr<NSMutableDictionary> m_bundleParameters;
