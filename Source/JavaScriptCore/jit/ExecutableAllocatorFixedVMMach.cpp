03544a74ebf66f3dd0dc60e4701a81014f01b131
Oliver Hunt <oliver@apple.com>
2016-03-08 20:53:11 +0000

    Start moving to separated writable and executable mappings in the JIT https://bugs.webkit.org/show_bug.cgi?id=155178
    Reviewed by Filip Pizlo.
    
    Source/JavaScriptCore:
    
    Start moving to a separate writable and executable heap for the various
    JITs.
    
    As part of our work to harden the JIT against various attacks, we're
    moving away from our current RWX heap and on to using separate RW and X
    mappings. This means that simply leaking the location of the executable
    mapping is not sufficient to compromise JSC, so we can continue to
    use direct executable pointers in our GC objects (which we need for
    performance), but keep the writable pointer in only a single location
    so that we are less likely to leak the address. To further obscure the
    address of the writable region we place it in an execute only region
    of memory so that it is not possible to read the location from
    anywhere. That means an attacker must have at least partial control
    of PC (to call jitMemCopy) before they can start to attack the JIT.
    
    This work is initially ARM64 only, as we use as the jitMemCopy is
    currently specific to that platform's calling conventions and layout.
    We're just landing it in the current form so that we can at least
    ensure it doesn't regress.
    
    * Configurations/FeatureDefines.xcconfig:
    * assembler/ARM64Assembler.h:
    (JSC::ARM64Assembler::ldp):
    (JSC::ARM64Assembler::ldnp):
    (JSC::ARM64Assembler::fillNops):
    (JSC::ARM64Assembler::stp):
    (JSC::ARM64Assembler::stnp):
    (JSC::ARM64Assembler::replaceWithJump):
    (JSC::ARM64Assembler::replaceWithLoad):
    (JSC::ARM64Assembler::replaceWithAddressComputation):
    (JSC::ARM64Assembler::setPointer):
    (JSC::ARM64Assembler::repatchInt32):
    (JSC::ARM64Assembler::repatchCompact):
    (JSC::ARM64Assembler::linkJumpOrCall):
    (JSC::ARM64Assembler::linkCompareAndBranch):
    (JSC::ARM64Assembler::linkConditionalBranch):
    (JSC::ARM64Assembler::linkTestAndBranch):
    (JSC::ARM64Assembler::loadStoreRegisterPairOffset):
    (JSC::ARM64Assembler::loadStoreRegisterPairNonTemporal):
    * assembler/LinkBuffer.cpp:
    (JSC::LinkBuffer::copyCompactAndLinkCode):
    (JSC::LinkBuffer::allocate):
    * assembler/LinkBuffer.h:
    (JSC::LinkBuffer::LinkBuffer):
    * assembler/MacroAssemblerARM64.h:
    (JSC::MacroAssemblerARM64::sub64):
    (JSC::MacroAssemblerARM64::load64):
    (JSC::MacroAssemblerARM64::loadPair64):
    (JSC::MacroAssemblerARM64::loadPair64WithNonTemporalAccess):
    (JSC::MacroAssemblerARM64::load8):
    (JSC::MacroAssemblerARM64::store64):
    (JSC::MacroAssemblerARM64::storePair64):
    (JSC::MacroAssemblerARM64::storePair64WithNonTemporalAccess):
    (JSC::MacroAssemblerARM64::store8):
    (JSC::MacroAssemblerARM64::branchAdd64):
    (JSC::MacroAssemblerARM64::branchSub64):
    * jit/ExecutableAllocator.h:
    (JSC::performJITMemcpy):
    * jit/ExecutableAllocatorFixedVMPool.cpp:
    (JSC::FixedVMPoolExecutableAllocator::FixedVMPoolExecutableAllocator):
    (JSC::FixedVMPoolExecutableAllocator::initializeBulletproofJIT):
    (JSC::FixedVMPoolExecutableAllocator::jitWriteThunkGenerator):
    * runtime/Options.cpp:
    (JSC::recomputeDependentOptions):
    * runtime/Options.h:
    
    Source/WebCore:
    
    Update feature defines.
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebKit/mac:
    
    Update feature defines.
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebKit2:
    
    Update feature defines.
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WTF:
    
    Update feature defines.
    
    * wtf/FeatureDefines.h:
    * wtf/Platform.h: ARM64 for now.
    
    Tools:
    
    Making run-jsc-benchmarks slightly happier on my machine.
    
    * Scripts/run-jsc-benchmarks:
    
    Canonical link: https://commits.webkit.org/173283@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@197793 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/JavaScriptCore/jit/ExecutableAllocatorFixedVMMach.cpp
@@ -0,0 +1,9 @@
+//
+//  ExecutableAllocatorFixedVMMach.cpp
+//  JavaScriptCore
+//
+//  Created by Oliver Hunt on 3/8/16.
+//
+//
+
+#include "ExecutableAllocatorFixedVMMach.hpp"
--- Source/JavaScriptCore/jit/ExecutableAllocatorFixedVMPool.cpp
+++ Source/JavaScriptCore/jit/ExecutableAllocatorFixedVMPool.cpp
@@ -48,11 +48,27 @@
 #include <stdio.h>
 #endif
 
+#if ENABLE(SEPARATE_WX_HEAPPROOF_JIT)
+#include "LinkBuffer.h"
+#include "MacroAssembler.h"
+
+#if OS(DARWIN)
+#include <mach/mach.h>
+#include <mach/mach_vm.h>
+#endif
+
+#endif
+
 using namespace WTF;
 
 namespace JSC {
-    
-uintptr_t startOfFixedExecutableMemoryPool;
+
+JS_EXPORT_PRIVATE uintptr_t startOfFixedExecutableMemoryPool;
+JS_EXPORT_PRIVATE uintptr_t endOfFixedExecutableMemoryPool;
+
+#if ENABLE(SEPARATE_WX_HEAPPROOF_JIT)
+JS_EXPORT_PRIVATE uintptr_t jitWriteFunctionAddress;
+#endif
 
 class FixedVMPoolExecutableAllocator : public MetaAllocator {
     WTF_MAKE_FAST_ALLOCATED;
@@ -69,9 +85,22 @@ public:
         m_reservation = PageReservation::reserveWithGuardPages(reservationSize, OSAllocator::JSJITCodePages, EXECUTABLE_POOL_WRITABLE, true);
         if (m_reservation) {
             ASSERT(m_reservation.size() == reservationSize);
-            addFreshFreeSpace(m_reservation.base(), m_reservation.size());
-            
-            startOfFixedExecutableMemoryPool = reinterpret_cast<uintptr_t>(m_reservation.base());
+            void* reservationBase = m_reservation.base();
+
+#if ENABLE(SEPARATE_WX_HEAPPROOF_JIT)
+            if (Options::useBulletproofJIT()) {
+                // First page of our JIT allocation is reserved.
+                ASSERT(reservationSize >= pageSize() * 2);
+                reservationBase = (void*)((uintptr_t)reservationBase + pageSize());
+                reservationSize -= pageSize();
+                initializeBulletproofJIT(m_reservation.base(), pageSize(), reservationBase, reservationSize);
+            }
+#endif
+
+            addFreshFreeSpace(reservationBase, reservationSize);
+
+            startOfFixedExecutableMemoryPool = reinterpret_cast<uintptr_t>(reservationBase);
+            endOfFixedExecutableMemoryPool = startOfFixedExecutableMemoryPool + reservationSize;
         }
     }
 
@@ -110,6 +139,113 @@ protected:
         m_reservation.decommit(page, pageSize());
 #endif
     }
+
+private:
+#if ENABLE(SEPARATE_WX_HEAPPROOF_JIT)
+    void initializeBulletproofJIT(void* stubBase, size_t stubSize, void* jitBase, size_t jitSize)
+    {
+        mach_vm_address_t writableAddr = 0;
+
+        // Create a second mapping of the JIT region at a random address.
+        vm_prot_t cur, max;
+        kern_return_t ret = mach_vm_remap(mach_task_self(), &writableAddr, jitSize, 0,
+            VM_FLAGS_ANYWHERE | VM_FLAGS_RANDOM_ADDR,
+            mach_task_self(), (mach_vm_address_t)jitBase, FALSE,
+            &cur, &max, VM_INHERIT_DEFAULT);
+
+        RELEASE_ASSERT(ret == KERN_SUCCESS);
+
+
+        // Assemble a thunk that will serve as the means for writing into the JIT region.
+        MacroAssemblerCodeRef writeThunk = jitWriteThunkGenerator(writableAddr, stubBase, stubSize);
+
+#if defined(VM_PROT_EXECUTE_ONLY)
+        // Prevent reading the write thunk code.
+        int result = mprotect(stubBase, stubSize, VM_PROT_EXECUTE_ONLY);
+        RELEASE_ASSERT(!result);
+#endif
+        
+        // Prevent writing into the executable JIT mapping.
+        result = mprotect(jitBase, jitSize, VM_PROT_READ | VM_PROT_EXECUTE);
+        RELEASE_ASSERT(!result);
+
+        // Prevent execution in the writable JIT mapping.
+        result = mprotect((void*)writableAddr, jitSize, VM_PROT_READ | VM_PROT_WRITE);
+        RELEASE_ASSERT(!result);
+
+        // Zero out writableAddr to avoid leaking the address of the writable mapping.
+        memset_s(&writableAddr, sizeof(writableAddr), 0, sizeof(writableAddr));
+
+        jitWriteFunctionAddress = (uintptr_t)writeThunk.code().executableAddress();
+    }
+
+#if CPU(ARM64)
+    MacroAssemblerCodeRef jitWriteThunkGenerator(mach_vm_address_t writableAddr, void* stubBase, size_t stubSize)
+    {
+        using namespace ARM64Registers;
+        using TrustedImm32 = MacroAssembler::TrustedImm32;
+
+        MacroAssembler jit;
+
+        jit.move(MacroAssembler::TrustedImmPtr((const void*)writableAddr), x7);
+        jit.addPtr(x7, x0);
+
+        jit.move(x0, x3);
+        MacroAssembler::Jump smallCopy = jit.branch64(MacroAssembler::Below, x2, MacroAssembler::TrustedImm64(64));
+
+        jit.add64(TrustedImm32(32), x3);
+        jit.and64(TrustedImm32(-32), x3);
+        jit.loadPair64(x1, x12, x13);
+        jit.loadPair64(x1, TrustedImm32(16), x14, x15);
+        jit.sub64(x3, x0, x5);
+        jit.addPtr(x5, x1);
+
+        jit.loadPair64(x1, x8, x9);
+        jit.loadPair64(x1, TrustedImm32(16), x10, x11);
+        jit.add64(TrustedImm32(32), x1);
+        jit.sub64(x5, x2);
+        jit.storePair64(x12, x13, x0);
+        jit.storePair64(x14, x15, x0, TrustedImm32(16));
+        MacroAssembler::Jump cleanup = jit.branchSub64(MacroAssembler::BelowOrEqual, TrustedImm32(64), x2);
+
+        MacroAssembler::Label copyLoop = jit.label();
+        jit.storePair64WithNonTemporalAccess(x8, x9, x3);
+        jit.storePair64WithNonTemporalAccess(x10, x11, x3, TrustedImm32(16));
+        jit.add64(TrustedImm32(32), x3);
+        jit.loadPair64WithNonTemporalAccess(x1, x8, x9);
+        jit.loadPair64WithNonTemporalAccess(x1, TrustedImm32(16), x10, x11);
+        jit.add64(TrustedImm32(32), x1);
+        jit.branchSub64(MacroAssembler::Above, TrustedImm32(32), x2).linkTo(copyLoop, &jit);
+
+        cleanup.link(&jit);
+        jit.add64(x2, x1);
+        jit.loadPair64(x1, x12, x13);
+        jit.loadPair64(x1, TrustedImm32(16), x14, x15);
+        jit.storePair64(x8, x9, x3);
+        jit.storePair64(x10, x11, x3, TrustedImm32(16));
+        jit.addPtr(x2, x3);
+        jit.storePair64(x12, x13, x3, TrustedImm32(32));
+        jit.storePair64(x14, x15, x3, TrustedImm32(48));
+        jit.ret();
+
+        MacroAssembler::Label local0 = jit.label();
+        jit.load64(x1, PostIndex(8), x6);
+        jit.store64(x6, x3, PostIndex(8));
+        smallCopy.link(&jit);
+        jit.branchSub64(MacroAssembler::AboveOrEqual, TrustedImm32(8), x2).linkTo(local0, &jit);
+        MacroAssembler::Jump local2 = jit.branchAdd64(MacroAssembler::Equal, TrustedImm32(8), x2);
+        MacroAssembler::Label local1 = jit.label();
+        jit.load8(x1, PostIndex(1), x6);
+        jit.store8(x6, x3, PostIndex(1));
+        jit.branchSub64(MacroAssembler::NotEqual, TrustedImm32(1), x2).linkTo(local1, &jit);
+        local2.link(&jit);
+        jit.ret();
+
+        LinkBuffer linkBuffer(jit, stubBase, stubSize);
+        return FINALIZE_CODE(linkBuffer, ("Bulletproof JIT write thunk"));
+    }
+#endif // CPU(ARM64)
+#endif // ENABLE(SEPARATE_WX_HEAPPROOF_JIT)
 
 private:
     PageReservation m_reservation;
