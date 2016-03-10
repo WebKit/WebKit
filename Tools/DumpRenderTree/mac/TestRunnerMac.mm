d63d0794a29914c5b2e49d3f4b53916e3a7ccf19
David Kilzer <ddkilzer@webkit.org>
2016-03-10 18:23:52 +0000

    [iOS] DumpRenderTree crashes when accessing window.testRunner.inspectorTestStubURL <http://webkit.org/b/155295>
    Reviewed by Brent Fulgham.
    
    Tools:
    
    * DumpRenderTree/mac/TestRunnerMac.mm:
    (TestRunner::inspectorTestStubURL): Always return nullptr for
    PLATFORM(IOS).  This matches what we did in r192604 for
    WebKitTestRunner/InjectedBundle/mac/TestRunnerMac.mm.
    
    LayoutTests:
    
    * platform/ios-simulator-wk1/TestExpectations:
    * platform/ios-simulator-wk2/TestExpectations:
    - Remove all "inspector/" tests.  They are already skipped in
      platform/ios-simulator/TestExpectations, so no need to
      resurrect them here.
    
    
    Canonical link: https://commits.webkit.org/173411@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@197942 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/DumpRenderTree/mac/TestRunnerMac.mm
+++ Tools/DumpRenderTree/mac/TestRunnerMac.mm
@@ -43,7 +43,6 @@
 #import <JavaScriptCore/JSStringRef.h>
 #import <JavaScriptCore/JSStringRefCF.h>
 #import <WebCore/GeolocationPosition.h>
-#import <WebCore/SoftLinking.h>
 #import <WebKit/DOMDocument.h>
 #import <WebKit/DOMElement.h>
 #import <WebKit/DOMHTMLInputElementPrivate.h>
@@ -79,6 +78,7 @@
 #import <wtf/RetainPtr.h>
 
 #if !PLATFORM(IOS)
+#import <WebCore/SoftLinking.h>
 #import <WebKit/WebIconDatabasePrivate.h>
 #endif
 
@@ -89,9 +89,9 @@
 #import <WebKit/WebDOMOperationsPrivate.h>
 #endif
 
+#if !PLATFORM(IOS)
 SOFT_LINK_STAGED_FRAMEWORK(WebInspectorUI, PrivateFrameworks, A)
 
-#if !PLATFORM(IOS)
 @interface CommandValidationTarget : NSObject <NSValidatedUserInterfaceItem>
 {
     SEL _action;
@@ -785,6 +785,9 @@ void TestRunner::evaluateInWebInspector(JSStringRef script)
 
 JSStringRef TestRunner::inspectorTestStubURL()
 {
+#if PLATFORM(IOS)
+    return nullptr;
+#else
     // Call the soft link framework function to dlopen it, then CFBundleGetBundleWithIdentifier will work.
     WebInspectorUILibrary();
 
@@ -798,6 +801,7 @@ JSStringRef TestRunner::inspectorTestStubURL()
 
     CFStringRef urlString = CFURLGetString(url.get());
     return JSStringCreateWithCFString(urlString);
+#endif
 }
 
 typedef HashMap<unsigned, RetainPtr<WebScriptWorld> > WorldMap;
