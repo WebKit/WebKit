WebKit/WebKit

commit 8bcc89e8f968981a4d6253e393604e357456e293

Add a baseURL parameter to _WKUserStyleSheet https://bugs.webkit.org/show_bug.cgi?id=155219
Reviewed by Tim Horton.

Source/WebCore:

- Moves to a model for user content where instead of each page having a WebCore::UserContentController
  object, we have an abstract WebCore::UserContentProvider interface that can be implemented at the WebKit
  level. For now, legacy WebKit continues to use the old UserContentController, which implements
  WebCore::UserContentProvider, and WebKit2 implements its own implementation so it can store additional
  state.

* WebCore.xcodeproj/project.pbxproj:
Add new files.

* dom/ExtensionStyleSheets.cpp:
(WebCore::ExtensionStyleSheets::updateInjectedStyleSheetCache):
Switch to using forEachUserStyleSheet on the UserContentProvider.

* html/HTMLMediaElement.cpp:
(WebCore::HTMLMediaElement::loadResource):
Remove null check now that we always have a UserContentProvider.

* loader/EmptyClients.cpp:
(WebCore::fillWithEmptyClients):
* loader/EmptyClients.h:
Add new EmptyClients.

* loader/FrameLoader.cpp:
(WebCore::FrameLoader::loadResourceSynchronously):
Remove null check now that we always have a UserContentProvider.

* loader/PingLoader.cpp:
(WebCore::processContentExtensionRulesForLoad):
Remove null check now that we always have a UserContentProvider.

* loader/ResourceLoader.cpp:
(WebCore::ResourceLoader::willSendRequestInternal):
Remove null check now that we always have a UserContentProvider.

* loader/cache/CachedResourceLoader.cpp:
(WebCore::CachedResourceLoader::requestResource):
Remove null check now that we always have a UserContentProvider.

* page/DOMWindow.cpp:
(WebCore::DOMWindow::shouldHaveWebKitNamespaceForWorld):
Remove null checks now that we always have a UserContentProvider, and userMessageHandlerDescriptors
returns a reference.

(WebCore::DOMWindow::open):
Remove null check now that we always have a UserContentProvider.

* page/Frame.cpp:
(WebCore::Frame::injectUserScripts):

Source/WebKit2/WebProcess/UserContent/WebUserContentController.h
@@ -28,9 +28,19 @@
 
 #include "MessageReceiver.h"
 #include "WebScriptMessageHandler.h"
-#include <WebCore/UserContentController.h>
+#include "WebUserContentControllerDataTypes.h"
+#include <WebCore/UserContentProvider.h>
 #include <wtf/HashMap.h>
-#include <wtf/RefCounted.h>
+
+#if ENABLE(CONTENT_EXTENSIONS)
+#include <WebCore/ContentExtensionsBackend.h>
+#endif
+
+namespace WebCore {
+namespace ContentExtensions {
+class CompiledContentExtension;
+}
+}
 
 namespace WebKit {
 
@@ -38,17 +48,33 @@ class InjectedBundleScriptWorld;
 class WebCompiledContentExtensionData;
 class WebUserMessageHandlerDescriptorProxy;
 
-class WebUserContentController final : public RefCounted<WebUserContentController>, private IPC::MessageReceiver  {
+class WebUserContentController final : public WebCore::UserContentProvider, private IPC::MessageReceiver {
 public:
     static Ref<WebUserContentController> getOrCreate(uint64_t identifier);
     virtual ~WebUserContentController();
 
-    WebCore::UserContentController& userContentController() { return m_userContentController; }
-
     uint64_t identifier() { return m_identifier; } 
+
+    void addUserScript(InjectedBundleScriptWorld&, WebCore::UserScript&&);
+    void removeUserScriptWithURL(InjectedBundleScriptWorld&, const WebCore::URL&);
+    void removeUserScripts(InjectedBundleScriptWorld&);
+    void addUserStyleSheet(InjectedBundleScriptWorld&, WebCore::UserStyleSheet&&);
+    void removeUserStyleSheetWithURL(InjectedBundleScriptWorld&, const WebCore::URL&);
+    void removeUserStyleSheets(InjectedBundleScriptWorld&);
+    void removeAllUserContent();
 
 private:
     explicit WebUserContentController(uint64_t identifier);
+
+    // WebCore::UserContentProvider
+    void forEachUserScript(const std::function<void(WebCore::DOMWrapperWorld&, const WebCore::UserScript&)>&) const override;
+    void forEachUserStyleSheet(const std::function<void(const WebCore::UserStyleSheet&)>&) const override;
+#if ENABLE(USER_MESSAGE_HANDLERS)
+    const WebCore::UserMessageHandlerDescriptorMap& userMessageHandlerDescriptors() const override { return m_userMessageHandlerDescriptorsMap; }
+#endif
+#if ENABLE(CONTENT_EXTENSIONS)
+    WebCore::ContentExtensions::ContentExtensionsBackend& userContentExtensionBackend() override { return m_contentExtensionBackend; }
+#endif
 
     // IPC::MessageReceiver.
     void didReceiveMessage(IPC::Connection&, IPC::MessageDecoder&) override;
@@ -56,12 +82,12 @@ private:
     void addUserContentWorlds(const Vector<std::pair<uint64_t, String>>&);
     void removeUserContentWorlds(const Vector<uint64_t>&);
 
-    void addUserScripts(const Vector<std::pair<uint64_t, WebCore::UserScript>>&);
-    void removeUserScript(uint64_t worldIdentifier, const String& urlString);
+    void addUserScripts(const Vector<WebUserScriptData>&);
+    void removeUserScript(uint64_t worldIdentifier, uint64_t userScriptIdentifier);
     void removeAllUserScripts(const Vector<uint64_t>&);
 
-    void addUserStyleSheets(const Vector<std::pair<uint64_t, WebCore::UserStyleSheet>>&);
-    void removeUserStyleSheet(uint64_t worldIdentifier, const String& urlString);
+    void addUserStyleSheets(const Vector<WebUserStyleSheetData>&);
+    void removeUserStyleSheet(uint64_t worldIdentifier, uint64_t userScriptIdentifier);
     void removeAllUserStyleSheets(const Vector<uint64_t>&);
 
     void addUserScriptMessageHandlers(const Vector<WebScriptMessageHandlerHandle>&);
@@ -73,11 +99,29 @@ private:
     void removeAllUserContentExtensions();
 #endif
 
+
+    void addUserScriptInternal(InjectedBundleScriptWorld&, uint64_t userScriptIdentifier, WebCore::UserScript&&);
+    void removeUserScriptInternal(InjectedBundleScriptWorld&, uint64_t userScriptIdentifier);
+    void addUserStyleSheetInternal(InjectedBundleScriptWorld&, uint64_t userStyleSheetIdentifier, WebCore::UserStyleSheet&&);
+    void removeUserStyleSheetInternal(InjectedBundleScriptWorld&, uint64_t userStyleSheetIdentifier);
+
+
     uint64_t m_identifier;
-    Ref<WebCore::UserContentController> m_userContentController;
-#if ENABLE(USER_MESSAGE_HANDLERS)
     HashMap<uint64_t, RefPtr<WebUserMessageHandlerDescriptorProxy>> m_userMessageHandlerDescriptors;
+
+    typedef HashMap<RefPtr<InjectedBundleScriptWorld>, Vector<std::pair<uint64_t, WebCore::UserScript>>> WorldToUserScriptMap;
+    WorldToUserScriptMap m_userScripts;
+
+    typedef HashMap<RefPtr<InjectedBundleScriptWorld>, Vector<std::pair<uint64_t, WebCore::UserStyleSheet>>> WorldToUserStyleSheetMap;
+    WorldToUserStyleSheetMap m_userStyleSheets;
+
+#if ENABLE(USER_MESSAGE_HANDLERS)
+    WebCore::UserMessageHandlerDescriptorMap m_userMessageHandlerDescriptorsMap;
 #endif
+#if ENABLE(CONTENT_EXTENSIONS)
+    WebCore::ContentExtensions::ContentExtensionsBackend m_contentExtensionBackend;
+#endif
+
 };
 
 } // namespace WebKit

Source/WebCore/CMakeLists.txt
@@ -2053,6 +2053,7 @@ set(WebCore_SOURCES
     page/SpatialNavigation.cpp
     page/SuspendableTimer.cpp
     page/TextIndicator.cpp
+    page/UserContentProvider.cpp
     page/UserContentController.cpp
     page/UserContentURLPattern.cpp
     page/VisitedLinkStore.cpp

Source/WebCore/WebCore.xcodeproj/project.pbxproj
@@ -2740,6 +2740,8 @@
 		7C3B79721908757B00B47A2D /* UserMessageHandler.h in Headers */ = {isa = PBXBuildFile; fileRef = 7C3B79701908757B00B47A2D /* UserMessageHandler.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		7C3E510A18DF8F3500C112F7 /* HTMLConverter.h in Headers */ = {isa = PBXBuildFile; fileRef = 7C3E510818DF8F3500C112F7 /* HTMLConverter.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		7C3E510B18DF8F3500C112F7 /* HTMLConverter.mm in Sources */ = {isa = PBXBuildFile; fileRef = 7C3E510918DF8F3500C112F7 /* HTMLConverter.mm */; };
+		7C3F01C11C8E5ABA00ADD962 /* UserContentProvider.h in Headers */ = {isa = PBXBuildFile; fileRef = 7C3F01C01C8E5AB100ADD962 /* UserContentProvider.h */; settings = {ATTRIBUTES = (Private, ); }; };
+		7C3F01C21C8E5AC200ADD962 /* UserContentProvider.cpp in Sources */ = {isa = PBXBuildFile; fileRef = 7C3F01BF1C8E5AB100ADD962 /* UserContentProvider.cpp */; };
 		7C48A6D0191C9D6500026674 /* WebKitNamespace.cpp in Sources */ = {isa = PBXBuildFile; fileRef = 7C48A6CE191C9D6500026674 /* WebKitNamespace.cpp */; };
 		7C48A6D1191C9D6500026674 /* WebKitNamespace.h in Headers */ = {isa = PBXBuildFile; fileRef = 7C48A6CF191C9D6500026674 /* WebKitNamespace.h */; };
 		7C4902A218B825F8007D9298 /* DOMWheelEventInternal.h in Copy Generated Headers */ = {isa = PBXBuildFile; fileRef = 85989DCA0ACC8BBD00A0BC51 /* DOMWheelEventInternal.h */; };
@@ -10380,6 +10382,8 @@
 		7C3B79701908757B00B47A2D /* UserMessageHandler.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = UserMessageHandler.h; sourceTree = "<group>"; };
 		7C3E510818DF8F3500C112F7 /* HTMLConverter.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = HTMLConverter.h; sourceTree = "<group>"; };
 		7C3E510918DF8F3500C112F7 /* HTMLConverter.mm */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.objcpp; path = HTMLConverter.mm; sourceTree = "<group>"; };
+		7C3F01BF1C8E5AB100ADD962 /* UserContentProvider.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = UserContentProvider.cpp; sourceTree = "<group>"; };
+		7C3F01C01C8E5AB100ADD962 /* UserContentProvider.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = UserContentProvider.h; sourceTree = "<group>"; };
 		7C4189AB1B07C170000FA757 /* GlobalEventHandlers.idl */ = {isa = PBXFileReference; lastKnownFileType = text; path = GlobalEventHandlers.idl; sourceTree = "<group>"; };
 		7C4189AC1B07CBF2000FA757 /* WindowEventHandlers.idl */ = {isa = PBXFileReference; lastKnownFileType = text; path = WindowEventHandlers.idl; sourceTree = "<group>"; };
 		7C48A6CE191C9D6500026674 /* WebKitNamespace.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = WebKitNamespace.cpp; sourceTree = "<group>"; };
@@ -18116,6 +18120,8 @@
 				2D4F96F21A1ECC240098BF88 /* TextIndicator.h */,
 				1AE79D40188DB61F002239C2 /* UserContentController.cpp */,
 				1AE79D41188DB61F002239C2 /* UserContentController.h */,
+				7C3F01BF1C8E5AB100ADD962 /* UserContentProvider.cpp */,
+				7C3F01C01C8E5AB100ADD962 /* UserContentProvider.h */,
 				003F1FE911E6AB43008258D9 /* UserContentTypes.h */,
 				BCACF3BA1072921A00C0C8A3 /* UserContentURLPattern.cpp */,
 				BCACF3BB1072921A00C0C8A3 /* UserContentURLPattern.h */,
@@ -28249,6 +28255,7 @@
 				088C2F7A12390081003D65CE /* SVGTextLayoutAttributes.h in Headers */,
 				081093DC1255F0E700ED9D29 /* SVGTextLayoutAttributesBuilder.h in Headers */,
 				081668DA125603D5006F25DE /* SVGTextLayoutEngine.h in Headers */,
+				7C3F01C11C8E5ABA00ADD962 /* UserContentProvider.h in Headers */,
 				080E49261255F3BD00EFCA27 /* SVGTextLayoutEngineBaseline.h in Headers */,
 				080E49281255F3BD00EFCA27 /* SVGTextLayoutEngineSpacing.h in Headers */,
 				08F0BFC61255C53C00075185 /* SVGTextMetrics.h in Headers */,
@@ -30425,6 +30432,7 @@
 				511EF2D117F0FDF100E4FA16 /* JSIDBObjectStoreCustom.cpp in Sources */,
 				511EF2C817F0FD3500E4FA16 /* JSIDBOpenDBRequest.cpp in Sources */,
 				511EF2C917F0FD3500E4FA16 /* JSIDBRequest.cpp in Sources */,
+				7C3F01C21C8E5AC200ADD962 /* UserContentProvider.cpp in Sources */,
 				511EF2CA17F0FD3500E4FA16 /* JSIDBTransaction.cpp in Sources */,
 				511EF2CB17F0FD3500E4FA16 /* JSIDBVersionChangeEvent.cpp in Sources */,
 				BC6C49F30D7DBA0500FFA558 /* JSImageConstructor.cpp in Sources */,

Source/WebCore/dom/ExtensionStyleSheets.cpp
@@ -117,6 +117,24 @@ void ExtensionStyleSheets::updateInjectedStyleSheetCache() const
     if (!owningPage)
         return;
 
+    owningPage->userContentProvider().forEachUserStyleSheet([&](const UserStyleSheet& userStyleSheet) {
+        if (userStyleSheet.injectedFrames() == InjectInTopFrameOnly && m_document.ownerElement())
+            return;
+
+        if (!UserContentURLPattern::matchesPatterns(m_document.url(), userStyleSheet.whitelist(), userStyleSheet.blacklist()))
+            return;
+
+        RefPtr<CSSStyleSheet> sheet = CSSStyleSheet::createInline(const_cast<Document&>(m_document), userStyleSheet.url());
+        bool isUserStyleSheet = userStyleSheet.level() == UserStyleUserLevel;
+        if (isUserStyleSheet)
+            m_injectedUserStyleSheets.append(sheet);
+        else
+            m_injectedAuthorStyleSheets.append(sheet);
+
+        sheet->contents().setIsUserStyleSheet(isUserStyleSheet);
+        sheet->contents().parseString(userStyleSheet.source());
+    });
+    
     if (!owningPage->captionUserPreferencesStyleSheet().isEmpty()) {
         // Identify our override style sheet with a unique URL - a new scheme and a UUID.
         static NeverDestroyed<URL> captionsStyleSheetURL(ParsedURLString, "user-captions-override:01F6AF12-C3B0-4F70-AF5E-A3E00234DC23");
@@ -126,34 +144,6 @@ void ExtensionStyleSheets::updateInjectedStyleSheetCache() const
 
         sheet->contents().setIsUserStyleSheet(false);
         sheet->contents().parseString(owningPage->captionUserPreferencesStyleSheet());
-    }
-
-    const auto* userContentController = owningPage->userContentController();
-    if (!userContentController)
-        return;
-
-    const UserStyleSheetMap* userStyleSheets = userContentController->userStyleSheets();
-    if (!userStyleSheets)
-        return;
-
-    for (auto& styleSheets : userStyleSheets->values()) {
-        for (const auto& sheet : *styleSheets) {
-            if (sheet->injectedFrames() == InjectInTopFrameOnly && m_document.ownerElement())
-                continue;
-
-            if (!UserContentURLPattern::matchesPatterns(m_document.url(), sheet->whitelist(), sheet->blacklist()))
-                continue;
-
-            RefPtr<CSSStyleSheet> groupSheet = CSSStyleSheet::createInline(const_cast<Document&>(m_document), sheet->url());
-            bool isUserStyleSheet = sheet->level() == UserStyleUserLevel;
-            if (isUserStyleSheet)
-                m_injectedUserStyleSheets.append(groupSheet);
-            else
-                m_injectedAuthorStyleSheets.append(groupSheet);
-
-            groupSheet->contents().setIsUserStyleSheet(isUserStyleSheet);
-            groupSheet->contents().parseString(sheet->source());
-        }
     }
 }
 

Source/WebCore/html/HTMLMediaElement.cpp
@@ -1258,7 +1258,7 @@ void HTMLMediaElement::loadResource(const URL& initialURL, ContentType& contentT
     ResourceRequest request(url);
     DocumentLoader* documentLoader = frame->loader().documentLoader();
 
-    if (page->userContentController() && documentLoader && page->userContentController()->processContentExtensionRulesForLoad(request, ResourceType::Media, *documentLoader) == ContentExtensions::BlockedStatus::Blocked) {
+    if (documentLoader && page->userContentProvider().processContentExtensionRulesForLoad(request, ResourceType::Media, *documentLoader) == ContentExtensions::BlockedStatus::Blocked) {
         request = { };
         mediaLoadingFailed(MediaPlayer::FormatError);
         return;

Source/WebCore/loader/EmptyClients.cpp
@@ -41,6 +41,7 @@
 #include "StorageArea.h"
 #include "StorageNamespace.h"
 #include "StorageNamespaceProvider.h"
+#include "UserContentProvider.h"
 #include <wtf/NeverDestroyed.h>
 
 #if USE(APPLE_INTERNAL_SDK)
@@ -95,7 +96,18 @@ class EmptyStorageNamespaceProvider final : public StorageNamespaceProvider {
     }
 };
 
-class EmptyVisitedLinkStore : public VisitedLinkStore {
+class EmptyUserContentProvider final : public UserContentProvider {
+    void forEachUserScript(const std::function<void(DOMWrapperWorld&, const UserScript&)>&) const override { }
+    void forEachUserStyleSheet(const std::function<void(const UserStyleSheet&)>&) const override { }
+#if ENABLE(USER_MESSAGE_HANDLERS)
+    const UserMessageHandlerDescriptorMap& userMessageHandlerDescriptors() const override { static NeverDestroyed<UserMessageHandlerDescriptorMap> map; return map.get(); }
+#endif
+#if ENABLE(CONTENT_EXTENSIONS)
+    ContentExtensions::ContentExtensionsBackend& userContentExtensionBackend() override { static NeverDestroyed<ContentExtensions::ContentExtensionsBackend> backend; return backend.get(); };
+#endif
+};
+
+class EmptyVisitedLinkStore final : public VisitedLinkStore {
     bool isLinkVisited(Page&, LinkHash, const URL&, const AtomicString&) override { return false; }
     void addVisitedLink(Page&, LinkHash) override { }
 };
@@ -132,6 +144,7 @@ void fillWithEmptyClients(PageConfiguration& pageConfiguration)
 
     pageConfiguration.databaseProvider = adoptRef(new EmptyDatabaseProvider);
     pageConfiguration.storageNamespaceProvider = adoptRef(new EmptyStorageNamespaceProvider);
+    pageConfiguration.userContentProvider = adoptRef(new EmptyUserContentProvider);
     pageConfiguration.visitedLinkStore = adoptRef(new EmptyVisitedLinkStore);
 
 #if USE(APPLE_INTERNAL_SDK)

Source/WebCore/loader/EmptyClients.h
@@ -36,7 +36,6 @@
 #include "DiagnosticLoggingClient.h"
 #include "DragClient.h"
 #include "EditorClient.h"
-#include "TextCheckerClient.h"
 #include "FloatRect.h"
 #include "FocusDirection.h"
 #include "FrameLoaderClient.h"
@@ -44,8 +43,17 @@
 #include "Page.h"
 #include "ProgressTrackerClient.h"
 #include "ResourceError.h"
+#include "TextCheckerClient.h"
 #include "VisitedLinkStore.h"
 #include <wtf/text/StringView.h>
+
+#if ENABLE(USER_MESSAGE_HANDLERS)
+#include "UserMessageHandlerDescriptor.h"
+#endif
+
+#if ENABLE(CONTENT_EXTENSIONS)
+#include "CompiledContentExtension.h"
+#endif
 
 /*
  This file holds empty Client stubs for use by WebCore.

Source/WebCore/loader/FrameLoader.cpp
@@ -2730,13 +2730,11 @@ unsigned long FrameLoader::loadResourceSynchronously(const ResourceRequest& requ
 #if ENABLE(CONTENT_EXTENSIONS)
     if (error.isNull()) {
         if (auto* page = m_frame.page()) {
-            if (auto* controller = page->userContentController()) {
-                if (m_documentLoader && controller->processContentExtensionRulesForLoad(newRequest, ResourceType::Raw, *m_documentLoader) == ContentExtensions::BlockedStatus::Blocked) {
-                    newRequest = { };
-                    error = ResourceError(errorDomainWebKitInternal, 0, initialRequest.url(), emptyString());
-                    response = { };
-                    data = nullptr;
-                }
+            if (m_documentLoader && page->userContentProvider().processContentExtensionRulesForLoad(newRequest, ResourceType::Raw, *m_documentLoader) == ContentExtensions::BlockedStatus::Blocked) {
+                newRequest = { };
+                error = ResourceError(errorDomainWebKitInternal, 0, initialRequest.url(), emptyString());
+                response = { };
+                data = nullptr;
             }
         }
     }

commit 25744041c511ec917dca1aebb0244f7da9450e59

EFL build has been broken since r198180 https://bugs.webkit.org/show_bug.cgi?id=155488
Unreviewed build fix.

* WebProcess/UserContent/WebUserContentController.h: Use ENABLE(USER_MESSAGE_HANDLERS) guard.

Canonical link: https://commits.webkit.org/173612@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@198208 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/WebProcess/UserContent/WebUserContentController.h
@@ -46,7 +46,10 @@ namespace WebKit {
 
 class InjectedBundleScriptWorld;
 class WebCompiledContentExtensionData;
+
+#if ENABLE(USER_MESSAGE_HANDLERS)
 class WebUserMessageHandlerDescriptorProxy;
+#endif
 
 class WebUserContentController final : public WebCore::UserContentProvider, private IPC::MessageReceiver {
 public:
@@ -107,7 +110,6 @@ private:
 
 
     uint64_t m_identifier;
-    HashMap<uint64_t, RefPtr<WebUserMessageHandlerDescriptorProxy>> m_userMessageHandlerDescriptors;
 
     typedef HashMap<RefPtr<InjectedBundleScriptWorld>, Vector<std::pair<uint64_t, WebCore::UserScript>>> WorldToUserScriptMap;
     WorldToUserScriptMap m_userScripts;
@@ -116,6 +118,7 @@ private:
     WorldToUserStyleSheetMap m_userStyleSheets;
 
 #if ENABLE(USER_MESSAGE_HANDLERS)
+    HashMap<uint64_t, RefPtr<WebUserMessageHandlerDescriptorProxy>> m_userMessageHandlerDescriptors;
     WebCore::UserMessageHandlerDescriptorMap m_userMessageHandlerDescriptorsMap;
 #endif
 #if ENABLE(CONTENT_EXTENSIONS)