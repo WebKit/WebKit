6e310c59ebe952e75ddf2655ec21ccf59ea8d2b5
Zalan Bujtas <zalan@apple.com>
2016-03-17 00:07:58 +0000

    Subpixel rendering: Directly composited image layers need pixelsnapping. https://bugs.webkit.org/show_bug.cgi?id=155558
    Reviewed by Simon Fraser.
    
    In order to match non-composited image size/position, we need to pixelsnap both the contents and the clipping
    layer bounds for directly composited images.
    
    Source/WebCore:
    
    Test: fast/images/hidpi-directly-composited-image-on-subpixel-position.html
    
    * rendering/RenderLayerBacking.cpp:
    (WebCore::RenderLayerBacking::resetContentsRect):
    (WebCore::RenderLayerBacking::updateChildClippingStrategy):
    (WebCore::RenderLayerBacking::updateImageContents):
    
    LayoutTests:
    
    * fast/images/hidpi-directly-composited-image-on-subpixel-position-expected.html: Added.
    * fast/images/hidpi-directly-composited-image-on-subpixel-position.html: Added.
    
    
    Canonical link: https://commits.webkit.org/173685@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@198309 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderLayerBacking.cpp
+++ Source/WebCore/rendering/RenderLayerBacking.cpp
@@ -1203,12 +1203,12 @@ void RenderLayerBacking::updateInternalHierarchy()
 
 void RenderLayerBacking::resetContentsRect()
 {
-    m_graphicsLayer->setContentsRect(snappedIntRect(contentsBox()));
+    m_graphicsLayer->setContentsRect(snapRectToDevicePixels(contentsBox(), deviceScaleFactor()));
     
     if (is<RenderBox>(renderer())) {
         LayoutRect boxRect(LayoutPoint(), downcast<RenderBox>(renderer()).size());
+        boxRect.move(contentOffsetInCompostingLayer());
         FloatRoundedRect contentsClippingRect = renderer().style().getRoundedInnerBorderFor(boxRect).pixelSnappedRoundedRectForPainting(deviceScaleFactor());
-        contentsClippingRect.move(contentOffsetInCompostingLayer());
         m_graphicsLayer->setContentsClippingRect(contentsClippingRect);
     }
 
@@ -1547,8 +1547,8 @@ void RenderLayerBacking::updateChildClippingStrategy(bool needsDescendantsClippi
         if (is<RenderBox>(renderer()) && (renderer().style().clipPath() || renderer().style().hasBorderRadius())) {
             // FIXME: we shouldn't get geometry here as layout may not have been udpated.
             LayoutRect boxRect(LayoutPoint(), downcast<RenderBox>(renderer()).size());
+            boxRect.move(contentOffsetInCompostingLayer());
             FloatRoundedRect contentsClippingRect = renderer().style().getRoundedInnerBorderFor(boxRect).pixelSnappedRoundedRectForPainting(deviceScaleFactor());
-            contentsClippingRect.move(contentOffsetInCompostingLayer());
             if (clippingLayer()->setMasksToBoundsRect(contentsClippingRect)) {
                 if (m_childClippingMaskLayer) 
                     m_childClippingMaskLayer = nullptr;
@@ -2060,11 +2060,11 @@ void RenderLayerBacking::updateImageContents()
         return;
 
     // This is a no-op if the layer doesn't have an inner layer for the image.
-    m_graphicsLayer->setContentsRect(snappedIntRect(contentsBox()));
+    m_graphicsLayer->setContentsRect(snapRectToDevicePixels(contentsBox(), deviceScaleFactor()));
 
     LayoutRect boxRect(LayoutPoint(), imageRenderer.size());
+    boxRect.move(contentOffsetInCompostingLayer());
     FloatRoundedRect contentsClippingRect = renderer().style().getRoundedInnerBorderFor(boxRect).pixelSnappedRoundedRectForPainting(deviceScaleFactor());
-    contentsClippingRect.move(contentOffsetInCompostingLayer());
     m_graphicsLayer->setContentsClippingRect(contentsClippingRect);
 
     m_graphicsLayer->setContentsToImage(image);
