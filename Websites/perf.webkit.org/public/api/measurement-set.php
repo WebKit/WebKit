WebKit/WebKit

commit a336c24715a10fc7b097e19cf9ff4f5925fde4c7

Associated commits don't immediately show up on an analysis task page https://bugs.webkit.org/show_bug.cgi?id=155692
Reviewed by Darin Adler.

The bug was caused by resolveCommits in AnalysisTask._constructAnalysisTasksFromRawData not being
able to find the matching commit log if the commit log had been created by the charts which don't
set the remote identifiers on each CommitLog objects.

Fixed the bug by modifying /api/measurement-set to include the commit ID, and making CommitLog
use the real database ID as its ID instead of a fake ID we create from repository and revision.

Also added a bunch of Mocha unit tests for AnalysisTask.fetchAll.

* public/api/measurement-set.php:
(MeasurementSetFetcher::execute_query): Fetch commit_id.
(MeasurementSetFetcher::format_run): Use pass-by-reference to avoid making a copy of the row.
(MeasurementSetFetcher::parse_revisions_array): Include commit_id as the first item in the result.
* public/v3/instrumentation.js:
* public/v3/models/analysis-task.js:
(AnalysisTask): Fixed a bug that _buildRequestCount and _finishedBuildRequestCount could be kept
as strings and hasPendingRequests() could return a wrong result because it would perform string
inequality instead of numerical inequality.
(AnalysisTask.prototype.updateSingleton): Ditto.
(AnalysisTask.prototype.dissociateCommit):
(AnalysisTask._constructAnalysisTasksFromRawData):
(AnalysisTask._constructAnalysisTasksFromRawData.resolveCommits): Use findById now that CommitLog
objects all use the same id as the database id.
* public/v3/models/commit-log.js:
(CommitLog):
(CommitLog.prototype.remoteId): Deleted since we no longer create a fake id for commit logs for
measurement sets.
(CommitLog.findByRemoteId): Deleted.
(CommitLog.ensureSingleton): Deleted.
(CommitLog.fetchBetweenRevisions):

* public/v3/models/data-model.js:
(DataModelObject.clearStaticMap): Added to aid unit testing.
(DataModelObject.ensureNamedStaticMap): Fixed a typo. Each map is a dictionary, not an array.

Websites/perf.webkit.org/public/api/measurement-set.php
@@ -165,7 +165,7 @@ class MeasurementSetFetcher {
     function execute_query($config_id) {
         return $this->db->query('
             SELECT test_runs.*, builds.*,
-            array_agg((commit_repository, commit_revision, commit_time)) AS revisions,
+            array_agg((commit_id, commit_repository, commit_revision, commit_time)) AS revisions,
             max(commit_time) AS revision_time, max(commit_order) AS revision_order
                 FROM builds
                     LEFT OUTER JOIN build_commits ON commit_build = build_id
@@ -180,7 +180,7 @@ class MeasurementSetFetcher {
             'commitTime', 'build', 'buildTime', 'buildNumber', 'builder');
     }
 
-    private static function format_run($run, &$commit_time) {
+    private static function format_run(&$run, &$commit_time) {
         $commit_time = Database::to_js_time($run['revision_time']);
         $build_time = Database::to_js_time($run['build_time']);
         if (!$commit_time)
@@ -200,16 +200,19 @@ class MeasurementSetFetcher {
             intval($run['build_builder']));
     }
 
-    private static function parse_revisions_array($postgres_array) {
-        // e.g. {"(WebKit,131456,\"2012-10-16 14:53:00\")","(Chromium,162004,)"}
+    private static function parse_revisions_array(&$postgres_array) {
+        // e.g. {"(<commit-id>,<repository-id>,<revision>,\"2012-10-16 14:53:00\")","(<commit-id>,<repository-id>,<revision>,)"}
         $outer_array = json_decode('[' . trim($postgres_array, '{}') . ']');
         $revisions = array();
         foreach ($outer_array as $item) {
             $name_and_revision = explode(',', trim($item, '()'));
             if (!$name_and_revision[0])
                 continue;
-            $time = Database::to_js_time(trim($name_and_revision[2], '"'));
-            array_push($revisions, array(intval(trim($name_and_revision[0], '"')), trim($name_and_revision[1], '"'), $time));
+            $commit_id = intval(trim($name_and_revision[0], '"'));
+            $repository_id = intval(trim($name_and_revision[1], '"'));
+            $revision = trim($name_and_revision[2], '"');
+            $time = Database::to_js_time(trim($name_and_revision[3], '"'));
+            array_push($revisions, array($commit_id, $repository_id, $revision, $time));
         }
         return $revisions;
     }

Websites/perf.webkit.org/public/v3/models/commit-log.js
@@ -5,25 +5,11 @@ class CommitLog extends DataModelObject {
     {
         super(id);
         this._repository = rawData.repository;
+        console.assert(this._repository instanceof Repository);
         this._rawData = rawData;
         this._remoteId = rawData.id;
         if (this._remoteId)
             this.ensureNamedStaticMap('remoteId')[this._remoteId] = this;
-    }
-
-    // FIXME: All this non-sense should go away once measurement-set start returning real commit id.
-    remoteId() { return this._remoteId; }
-    static findByRemoteId(id)
-    {
-        var remoteIdMap = super.namedStaticMap('remoteId');
-        return remoteIdMap ? remoteIdMap[id] : null;
-    }
-
-    static ensureSingleton(repository, rawData)
-    {
-        var id = repository.id() + '-' + rawData['revision'];
-        rawData.repository = repository;
-        return super.ensureSingleton(id, rawData);
     }
 
     updateSingleton(rawData)
@@ -99,7 +85,10 @@ class CommitLog extends DataModelObject {
 
         var self = this;
         return RemoteAPI.getJSONWithStatus(url).then(function (data) {
-            var commits = data['commits'].map(function (rawData) { return CommitLog.ensureSingleton(repository, rawData); });
+            var commits = data['commits'].map(function (rawData) {
+                rawData.repository = repository;
+                return CommitLog.ensureSingleton(rawData.id, rawData);
+            });
             self._cacheCommitLogs(repository, from, to, commits);
             return commits;
         });

Websites/perf.webkit.org/public/v3/models/root-set.js
@@ -14,11 +14,11 @@ class RootSet extends DataModelObject {
 
         for (var row of object.roots) {
             var repositoryId = row.repository;
-            var repository = Repository.findById(repositoryId);
+            row.repository = Repository.findById(repositoryId);
 
             console.assert(!this._repositoryToCommitMap[repositoryId]);
-            this._repositoryToCommitMap[repositoryId] = CommitLog.ensureSingleton(repository, row);
-            this._repositories.push(repository);
+            this._repositoryToCommitMap[repositoryId] = CommitLog.ensureSingleton(row.id, row);
+            this._repositories.push(row.repository);
         }
     }
 
@@ -74,12 +74,16 @@ class MeasurementRootSet extends RootSet {
     {
         super(id, null);
         for (var values of revisionList) {
-            var repositoryId = values[0];
+            // [<commit-id>, <repository-id>, <revision>, <time>]
+            var commitId = values[0];
+            var repositoryId = values[1];
+            var revision = values[2];
+            var time = values[3];
             var repository = Repository.findById(repositoryId);
             if (!repository)
                 continue;
 
-            this._repositoryToCommitMap[repositoryId] = CommitLog.ensureSingleton(repository, {revision: values[1], time: values[2]});
+            this._repositoryToCommitMap[repositoryId] = CommitLog.ensureSingleton(commitId, {repository: repository, revision: revision, time: time});
             this._repositories.push(repository);
         }
     }

Websites/perf.webkit.org/public/v3/instrumentation.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class Instrumentation {
 
     static startMeasuringTime(domain, label)
@@ -53,3 +55,6 @@ class Instrumentation {
     }
 
 }
+
+if (typeof module != 'undefined')
+    module.exports.Instrumentation = Instrumentation;

Websites/perf.webkit.org/public/v3/models/analysis-task.js
@@ -1,3 +1,4 @@
+'use strict';
 
 class AnalysisTask extends LabeledObject {
     constructor(id, object)
@@ -22,8 +23,8 @@ class AnalysisTask extends LabeledObject {
         this._bugs = object.bugs || [];
         this._causes = object.causes || [];
         this._fixes = object.fixes || [];
-        this._buildRequestCount = object.buildRequestCount;
-        this._finishedBuildRequestCount = object.finishedBuildRequestCount;
+        this._buildRequestCount = +object.buildRequestCount;
+        this._finishedBuildRequestCount = +object.finishedBuildRequestCount;
     }
 
     static findByPlatformAndMetric(platformId, metricId)
@@ -50,8 +51,8 @@ class AnalysisTask extends LabeledObject {
         this._bugs = object.bugs || [];
         this._causes = object.causes || [];
         this._fixes = object.fixes || [];
-        this._buildRequestCount = object.buildRequestCount;
-        this._finishedBuildRequestCount = object.finishedBuildRequestCount;
+        this._buildRequestCount = +object.buildRequestCount;
+        this._finishedBuildRequestCount = +object.finishedBuildRequestCount;
     }
 
     hasResults() { return this._finishedBuildRequestCount; }
@@ -138,7 +139,7 @@ class AnalysisTask extends LabeledObject {
         var id = this.id();
         return PrivilegedAPI.sendRequest('associate-commit', {
             task: id,
-            commit: commit.remoteId(),
+            commit: commit.id(),
         }).then(function (data) {
             return AnalysisTask.cachedFetch('../api/analysis-tasks', {id: id}, true)
                 .then(AnalysisTask._constructAnalysisTasksFromRawData.bind(AnalysisTask));
@@ -232,11 +233,11 @@ class AnalysisTask extends LabeledObject {
             rawData.repository = Repository.findById(rawData.repository);
             if (!rawData.repository)
                 continue;
-            CommitLog.ensureSingleton(rawData.repository, rawData);
+            CommitLog.ensureSingleton(rawData.id, rawData);
         }
 
         function resolveCommits(commits) {
-            return commits.map(function (id) { return CommitLog.findByRemoteId(id); }).filter(function (commit) { return !!commit; });
+            return commits.map(function (id) { return CommitLog.findById(id); }).filter(function (commit) { return !!commit; });
         }
 
         var results = [];
@@ -266,3 +267,6 @@ class AnalysisTask extends LabeledObject {
         });
     }
 }
+
+if (typeof module != 'undefined')
+    module.exports.AnalysisTask = AnalysisTask;

Websites/perf.webkit.org/public/v3/models/data-model.js
@@ -20,6 +20,8 @@ class DataModelObject {
 
     updateSingleton(object) { }
 
+    static clearStaticMap() { this[DataModelObject.StaticMapSymbol] = null; }
+
     static namedStaticMap(name)
     {
         var staticMap = this[DataModelObject.StaticMapSymbol];
@@ -32,7 +34,7 @@ class DataModelObject {
             this[DataModelObject.StaticMapSymbol] = {};
         var staticMap = this[DataModelObject.StaticMapSymbol];
         if (!staticMap[name])
-            staticMap[name] = [];
+            staticMap[name] = {};
         return staticMap[name];
     }
 

Websites/perf.webkit.org/public/v3/models/metric.js
@@ -1,3 +1,4 @@
+'use strict';
 
 class Metric extends LabeledObject {
     constructor(id, object)
@@ -86,3 +87,6 @@ class Metric extends LabeledObject {
         }
     };
 }
+
+if (typeof module != 'undefined')
+    module.exports.Metric = Metric;

Websites/perf.webkit.org/public/v3/models/platform.js
@@ -1,3 +1,4 @@
+'use strict';
 
 class Platform extends LabeledObject {
     constructor(id, object)
@@ -34,3 +35,6 @@ class Platform extends LabeledObject {
         return this._lastModifiedByMetric[metric.id()];
     }
 }
+
+if (typeof module != 'undefined')
+    module.exports.Platform = Platform;

commit 0d00714eb3271dcaca9f3fc28d9f95343454d3fe

Analysis task page is broken after r198479 https://bugs.webkit.org/show_bug.cgi?id=155735
Rubber-stamped by Chris Dumez.

* public/api/measurement-set.php:
(AnalysisResultsFetcher::fetch_commits): We need to emit the commit ID as done for regular data.
* public/include/build-requests-fetcher.php:
(BuildRequestsFetcher::fetch_roots_for_set_if_needed): Ditto. Don't use a fake ID after r198479.
* public/v3/models/commit-log.js:
(CommitLog): Assert that all commit log IDs are integers to catch regressions like this in future.
* public/v3/models/root-set.js:
(RootSet): Don't resolve Repository here as doing so would modify the shared "root" entry in the JSON
we fetched, and subsequent construction of RootSet would fail since this line would blow up trying to
find the repository with "[object]" as the ID.
* public/v3/models/test-group.js:
(TestGroup._createModelsFromFetchedTestGroups): Resolve Repository here.


Canonical link: https://commits.webkit.org/173830@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@198503 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Websites/perf.webkit.org/public/api/measurement-set.php
@@ -252,14 +252,14 @@ class AnalysisResultsFetcher {
 
     function fetch_commits()
     {
-        $query = $this->db->query('SELECT commit_build, commit_repository, commit_revision, commit_time
+        $query = $this->db->query('SELECT commit_id, commit_build, commit_repository, commit_revision, commit_time
             FROM commits, build_commits, build_requests, analysis_test_groups
             WHERE commit_id = build_commit AND commit_build = request_build
                 AND request_group = testgroup_id AND testgroup_task = $1', array($this->task_id));
         while ($row = $this->db->fetch_next_row($query)) {
             $commit_time = Database::to_js_time($row['commit_time']);
             array_push(array_ensure_item_has_array($this->build_to_commits, $row['commit_build']),
-                array($row['commit_repository'], $row['commit_revision'], $commit_time));
+                array($row['commit_id'], $row['commit_repository'], $row['commit_revision'], $commit_time));
         }
     }
 

Websites/perf.webkit.org/public/include/build-requests-fetcher.php
@@ -92,14 +92,12 @@ class BuildRequestsFetcher {
 
         $root_ids = array();
         foreach ($root_rows as $row) {
-            $repository = $row['repository_id'];
+            $repository_id = $row['commit_repository'];
             $revision = $row['commit_revision'];
             $commit_time = $row['commit_time'];
-            $root_id = $root_set_id . '-' . $repository;
-            array_push($root_ids, $root_id);
-            $repository_id = $resolve_ids ? $row['repository_name'] : $row['repository_id'];
+            array_push($root_ids, $row['commit_id']);
             array_push($this->roots, array(
-                'id' => $root_id,
+                'id' => $row['commit_id'],
                 'repository' => $repository_id,
                 'revision' => $revision,
                 'time' => Database::to_js_time($commit_time)));

Websites/perf.webkit.org/public/v3/models/commit-log.js
@@ -3,6 +3,7 @@
 class CommitLog extends DataModelObject {
     constructor(id, rawData)
     {
+        console.assert(parseInt(id) == id);
         super(id);
         this._repository = rawData.repository;
         console.assert(this._repository instanceof Repository);

Websites/perf.webkit.org/public/v3/models/root-set.js
@@ -13,9 +13,7 @@ class RootSet extends DataModelObject {
             return;
 
         for (var row of object.roots) {
-            var repositoryId = row.repository;
-            row.repository = Repository.findById(repositoryId);
-
+            var repositoryId = row.repository.id();
             console.assert(!this._repositoryToCommitMap[repositoryId]);
             this._repositoryToCommitMap[repositoryId] = CommitLog.ensureSingleton(row.id, row);
             this._repositories.push(row.repository);

Websites/perf.webkit.org/public/v3/models/test-group.js
@@ -222,8 +222,10 @@ class TestGroup extends LabeledObject {
         });
 
         var rootIdMap = {};
-        for (var root of data['roots'])
+        for (var root of data['roots']) {
             rootIdMap[root.id] = root;
+            root.repository = Repository.findById(root.repository);
+        }
 
         var rootSets = data['rootSets'].map(function (row) {
             row.roots = row.roots.map(function (rootId) { return rootIdMap[rootId]; });