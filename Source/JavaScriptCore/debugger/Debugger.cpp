2ea7c241e89d6b62ab004010e4c5dfdd6f37775b
Joseph Pecoraro <pecoraro@apple.com>
2016-03-29 20:12:18 +0000

    JSC::Debugger cleanup after recent changes https://bugs.webkit.org/show_bug.cgi?id=155982
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2016-03-29
    Reviewed by Mark Lam.
    
    * debugger/Debugger.cpp:
    (JSC::Debugger::Debugger):
    Initialize with breakpoints disabled. Web Inspector always informs
    the backend if it should enable or disable breakpoints on startup.
    
    (JSC::Debugger::setProfilingClient):
    When using the Sampling profiler we do not need to recompile.
    
    Canonical link: https://commits.webkit.org/174092@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@198791 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/debugger/Debugger.cpp
+++ Source/JavaScriptCore/debugger/Debugger.cpp
@@ -118,7 +118,7 @@ Debugger::Debugger(VM& vm)
     , m_pauseOnExceptionsState(DontPauseOnExceptions)
     , m_pauseOnNextStatement(false)
     , m_isPaused(false)
-    , m_breakpointsActivated(true)
+    , m_breakpointsActivated(false)
     , m_hasHandlerForExceptionCallback(false)
     , m_suppressAllPauses(false)
     , m_steppingMode(SteppingModeDisabled)
@@ -232,8 +232,6 @@ void Debugger::setProfilingClient(ProfilingClient* client)
 {
     ASSERT(!!m_profilingClient != !!client);
     m_profilingClient = client;
-
-    recompileAllJSFunctions();
 }
 
 double Debugger::willEvaluateScript()
