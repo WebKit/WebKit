39b64278e09648cc41b9e6f0ae21dd100a4fef5f
Alex Christensen <achristensen@webkit.org>
2016-03-30 20:39:08 +0000

    Fix Windows EWS after r198766 https://bugs.webkit.org/show_bug.cgi?id=156029
    Patch by Alex Christensen <achristensen@webkit.org> on 2016-03-30
    Reviewed by Brent Fulgham.
    
    * CMakeLists.txt:
    * PlatformWin.cmake:
    Copy headers before compiling WebCoreDerivedSources, not just before compiling WebCore.
    
    Canonical link: https://commits.webkit.org/174152@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@198854 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/PlatformWin.cmake
+++ Source/WebCore/PlatformWin.cmake
@@ -295,10 +295,10 @@ endif ()
 
 file(MAKE_DIRECTORY ${DERIVED_SOURCES_DIR}/ForwardingHeaders/WebCore)
 
-set(WebCore_PRE_BUILD_COMMAND "${CMAKE_BINARY_DIR}/DerivedSources/WebCore/preBuild.cmd")
-file(WRITE "${WebCore_PRE_BUILD_COMMAND}" "@xcopy /y /s /d /f \"${WEBCORE_DIR}/ForwardingHeaders/*.h\" \"${DERIVED_SOURCES_DIR}/ForwardingHeaders/WebCore\" >nul 2>nul\n")
+set(WebCore_DERIVED_SOURCES_PRE_BUILD_COMMAND "${CMAKE_BINARY_DIR}/DerivedSources/WebCore/preBuild.cmd")
+file(WRITE "${WebCore_DERIVED_SOURCES_PRE_BUILD_COMMAND}" "@xcopy /y /s /d /f \"${WEBCORE_DIR}/ForwardingHeaders/*.h\" \"${DERIVED_SOURCES_DIR}/ForwardingHeaders/WebCore\" >nul 2>nul\n")
 foreach (_directory ${WebCore_FORWARDING_HEADERS_DIRECTORIES})
-    file(APPEND "${WebCore_PRE_BUILD_COMMAND}" "@xcopy /y /d /f \"${WEBCORE_DIR}/${_directory}/*.h\" \"${DERIVED_SOURCES_DIR}/ForwardingHeaders/WebCore\" >nul 2>nul\n")
+    file(APPEND "${WebCore_DERIVED_SOURCES_PRE_BUILD_COMMAND}" "@xcopy /y /d /f \"${WEBCORE_DIR}/${_directory}/*.h\" \"${DERIVED_SOURCES_DIR}/ForwardingHeaders/WebCore\" >nul 2>nul\n")
 endforeach ()
 
 set(WebCore_POST_BUILD_COMMAND "${CMAKE_BINARY_DIR}/DerivedSources/WebCore/postBuild.cmd")
