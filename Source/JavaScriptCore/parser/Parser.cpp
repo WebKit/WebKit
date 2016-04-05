WebKit/WebKit

commit 5d3124f6b5f4591dac66689f84498303d56a6783

Implement Annex B.3.3 function hoisting rules for function code https://bugs.webkit.org/show_bug.cgi?id=155672
Reviewed by Geoffrey Garen.

Source/JavaScriptCore:

The spec states that functions declared inside a function
inside a block scope are subject to the rules of Annex B.3.3:
https://tc39.github.io/ecma262/#sec-block-level-function-declarations-web-legacy-compatibility-semantics

The rule states that functions declared in such blocks should
be local bindings of the block. If declaring the function's name
as a "var" in the function would not lead to a syntax error (i.e,
if we don't have a let/const/class variable with the same name)
and if we don't have a parameter with the same name, then we
implictly also declare the funcion name as a "var". When evaluating
the block statement we bind the hoisted "var" to be the value
of the local function binding.

There is one more thing we do for web compatibility. We allow
function declarations inside if/else statements that aren't
blocks. For such statements, we transform the code as if the
function were declared inside a block statement. For example:
``` function foo() { if (cond) function baz() { } }```
is transformed into:
``` function foo() { if (cond) { function baz() { } } }```

* bytecompiler/BytecodeGenerator.cpp:
(JSC::BytecodeGenerator::initializeDefaultParameterValuesAndSetupFunctionScopeStack):
(JSC::BytecodeGenerator::initializeBlockScopedFunctions):
* bytecompiler/BytecodeGenerator.h:
* parser/Nodes.cpp:
(JSC::ScopeNode::ScopeNode):
(JSC::ProgramNode::ProgramNode):
(JSC::ModuleProgramNode::ModuleProgramNode):
(JSC::EvalNode::EvalNode):
(JSC::FunctionNode::FunctionNode):
* parser/Nodes.h:
(JSC::ScopeNode::hasCapturedVariables):
(JSC::ScopeNode::captures):
(JSC::ScopeNode::hasSloppyModeHoistedFunction):
(JSC::ScopeNode::varDeclarations):
(JSC::ProgramNode::startColumn):
(JSC::ProgramNode::endColumn):
(JSC::EvalNode::startColumn):
(JSC::EvalNode::endColumn):
(JSC::ModuleProgramNode::startColumn):

Source/JavaScriptCore/parser/Parser.cpp
@@ -203,6 +203,7 @@ Parser<LexerType>::Parser(VM* vm, const SourceCode& source, JSParserBuiltinMode 
     , m_superBinding(superBinding)
     , m_defaultConstructorKind(defaultConstructorKind)
     , m_thisTDZMode(thisTDZMode)
+    , m_immediateParentAllowsFunctionDeclarationInStatement(false)
 {
     m_lexer = std::make_unique<LexerType>(vm, builtinMode);
     m_lexer->setCode(source, &m_parserArena);
@@ -304,9 +305,11 @@ String Parser<LexerType>::parseInner(const Identifier& calleeName, SourceParseMo
     }
 
     IdentifierSet capturedVariables;
+    UniquedStringImplPtrSet sloppyModeHoistedFunctions;
     bool modifiedParameter = false;
     bool modifiedArguments = false;
-    scope->getCapturedVars(capturedVariables, modifiedParameter, modifiedArguments);
+    scope->getSloppyModeHoistedFunctions(sloppyModeHoistedFunctions);
+    scope->getCapturedVars(capturedVariables,  modifiedParameter, modifiedArguments);
 
     VariableEnvironment& varDeclarations = scope->declaredVariables();
     for (auto& entry : capturedVariables)
@@ -341,19 +344,20 @@ String Parser<LexerType>::parseInner(const Identifier& calleeName, SourceParseMo
         }
     }
 #endif // NDEBUG
-    didFinishParsing(sourceElements, scope->takeFunctionDeclarations(), varDeclarations, features, context.numConstants());
+    didFinishParsing(sourceElements, scope->takeFunctionDeclarations(), varDeclarations, WTFMove(sloppyModeHoistedFunctions), features, context.numConstants());
 
     return parseError;
 }
 
 template <typename LexerType>
 void Parser<LexerType>::didFinishParsing(SourceElements* sourceElements, DeclarationStacks::FunctionStack&& funcStack, 
-    VariableEnvironment& varDeclarations, CodeFeatures features, int numConstants)
+    VariableEnvironment& varDeclarations, UniquedStringImplPtrSet&& sloppyModeHoistedFunctions, CodeFeatures features, int numConstants)
 {
     m_sourceElements = sourceElements;
     m_funcDeclarations = WTFMove(funcStack);
     m_varDeclarations.swap(varDeclarations);
     m_features = features;
+    m_sloppyModeHoistedFunctions = WTFMove(sloppyModeHoistedFunctions);
     m_numConstants = numConstants;
 }
 
@@ -562,8 +566,10 @@ template <class TreeBuilder> TreeStatement Parser<LexerType>::parseStatementList
         }
         if (shouldParseVariableDeclaration)
             result = parseVariableDeclaration(context, DeclarationType::LetDeclaration);
-        else
-            result = parseExpressionOrLabelStatement(context); // Treat this as an IDENT. This is how ::parseStatement() handles IDENT.
+        else {
+            bool allowFunctionDeclarationAsStatement = true;
+            result = parseExpressionOrLabelStatement(context, allowFunctionDeclarationAsStatement);
+        }
 
         break;
     }
@@ -575,6 +581,16 @@ template <class TreeBuilder> TreeStatement Parser<LexerType>::parseStatementList
     case FUNCTION:
         result = parseFunctionDeclaration(context);
         break;
+    case IDENT:
+    case YIELD: {
+        // This is a convenient place to notice labeled statements
+        // (even though we also parse them as normal statements)
+        // because we allow the following type of code in sloppy mode:
+        // ``` function foo() { label: function bar() { } } ```
+        bool allowFunctionDeclarationAsStatement = true;
+        result = parseExpressionOrLabelStatement(context, allowFunctionDeclarationAsStatement);
+        break;
+    }
     default:
         m_statementDepth--; // parseStatement() increments the depth.
         result = parseStatement(context, directive, directiveLiteralLength);
@@ -1603,6 +1619,8 @@ template <class TreeBuilder> TreeStatement Parser<LexerType>::parseStatement(Tre
     failIfStackOverflow();
     TreeStatement result = 0;
     bool shouldSetEndOffset = true;
+    bool parentAllowsFunctionDeclarationAsStatement = m_immediateParentAllowsFunctionDeclarationInStatement;
+    m_immediateParentAllowsFunctionDeclarationInStatement = false;
 
     switch (m_token.m_type) {
     case OPENBRACE:
@@ -1612,12 +1630,46 @@ template <class TreeBuilder> TreeStatement Parser<LexerType>::parseStatement(Tre
     case VAR:
         result = parseVariableDeclaration(context, DeclarationType::VarDeclaration);
         break;
-    case FUNCTION:
-        if (!strictMode())
-            result = parseFunctionDeclaration(context);
-        else
+    case FUNCTION: {
+        if (!strictMode()) {
+            failIfFalse(parentAllowsFunctionDeclarationAsStatement, "Function declarations are only allowed inside block statements or at the top level of a program");
+            if (currentScope()->isFunction()) {
+                // Any function declaration that isn't in a block is a syntax error unless it's
+                // in an if/else statement. If it's in an if/else statement, we will magically
+                // treat it as if the if/else statement is inside a block statement.
+                // to the very top like a "var". For example:
+                // function a() {
+                //     if (cond) function foo() { }
+                // }
+                // will be rewritten as:
+                // function a() {
+                //     if (cond) { function foo() { } }
+                // }
+                AutoPopScopeRef blockScope(this, pushScope());
+                blockScope->setIsLexicalScope();
+                blockScope->preventVarDeclarations();
+                JSTokenLocation location(tokenLocation());
+                int start = tokenLine();
+
+                TreeStatement function = parseFunctionDeclaration(context);
+                propagateError();
+                failIfFalse(function, "Expected valid function statement after 'function' keyword");
+                TreeSourceElements sourceElements = context.createSourceElements();
+                context.appendStatement(sourceElements, function);
+                result = context.createBlockStatement(location, sourceElements, start, m_lastTokenEndPosition.line, currentScope()->finalizeLexicalEnvironment(), currentScope()->takeFunctionDeclarations());
+                popScope(blockScope, TreeBuilder::NeedsFreeVariableInfo);
+            } else {
+                // We only implement annex B.3.3 if we're in function mode. Otherwise, we fall back
+                // to hoisting behavior.
+                // FIXME: https://bugs.webkit.org/show_bug.cgi?id=155813
+                DepthManager statementDepth(&m_statementDepth);
+                m_statementDepth = 1;
+                result = parseFunctionDeclaration(context);
+            }
+        } else
             failWithMessage("Function declarations are only allowed inside blocks or switch statements in strict mode");
         break;
+    }
     case SEMICOLON: {
         JSTokenLocation location(tokenLocation());
         next();
@@ -1667,9 +1719,11 @@ template <class TreeBuilder> TreeStatement Parser<LexerType>::parseStatement(Tre
         // These tokens imply the end of a set of source elements
         return 0;
     case IDENT:
-    case YIELD:
-        result = parseExpressionOrLabelStatement(context);
+    case YIELD: {
+        bool allowFunctionDeclarationAsStatement = false;
+        result = parseExpressionOrLabelStatement(context, allowFunctionDeclarationAsStatement);
         break;
+    }
     case STRING:
         directive = m_token.m_data.ident;
         if (directiveLiteralLength)
@@ -2372,7 +2426,7 @@ struct LabelInfo {
 };
 
 template <typename LexerType>
-template <class TreeBuilder> TreeStatement Parser<LexerType>::parseExpressionOrLabelStatement(TreeBuilder& context)
+template <class TreeBuilder> TreeStatement Parser<LexerType>::parseExpressionOrLabelStatement(TreeBuilder& context, bool allowFunctionDeclarationAsStatement)
 {
     
     /* Expression and Label statements are ambiguous at LL(1), so we have a
@@ -2423,6 +2477,7 @@ template <class TreeBuilder> TreeStatement Parser<LexerType>::parseExpressionOrL
         for (size_t i = 0; i < labels.size(); i++)
             pushLabel(labels[i].m_ident, isLoop);
     }
+    m_immediateParentAllowsFunctionDeclarationInStatement = allowFunctionDeclarationAsStatement;
     TreeStatement statement = parseStatement(context, unused);
     if (!m_syntaxAlreadyValidated) {
         for (size_t i = 0; i < labels.size(); i++)
@@ -2475,6 +2530,7 @@ template <class TreeBuilder> TreeStatement Parser<LexerType>::parseIfStatement(T
     handleProductionOrFail(CLOSEPAREN, ")", "end", "'if' condition");
 
     const Identifier* unused = 0;
+    m_immediateParentAllowsFunctionDeclarationInStatement = true;
     TreeStatement trueBlock = parseStatement(context, unused);
     failIfFalse(trueBlock, "Expected a statement as the body of an if block");
 
@@ -2491,6 +2547,7 @@ template <class TreeBuilder> TreeStatement Parser<LexerType>::parseIfStatement(T
         next();
         if (!match(IF)) {
             const Identifier* unused = 0;
+            m_immediateParentAllowsFunctionDeclarationInStatement = true;
             TreeStatement block = parseStatement(context, unused);
             failIfFalse(block, "Expected a statement as the body of an else block");
             statementStack.append(block);
@@ -2507,6 +2564,7 @@ template <class TreeBuilder> TreeStatement Parser<LexerType>::parseIfStatement(T
         int innerEnd = tokenLine();
         handleProductionOrFail(CLOSEPAREN, ")", "end", "'if' condition");
         const Identifier* unused = 0;
+        m_immediateParentAllowsFunctionDeclarationInStatement = true;
         TreeStatement innerTrueBlock = parseStatement(context, unused);
         failIfFalse(innerTrueBlock, "Expected a statement as the body of an if block");
         tokenLocationStack.append(tempLocation);

LayoutTests/js/function-declaration-statement-expected.txt
@@ -5,14 +5,6 @@ On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE
 
 PASS ifTest() is true
 PASS ifElseTest() is true
-PASS doWhileTest() is true
-PASS whileTest() is true
-PASS forTest() is true
-PASS forVarTest() is true
-PASS forInTest() is true
-PASS forInVarTest() is true
-PASS forInVarInitTest() is true
-PASS withTest() is true
 PASS labelTest() is true
 PASS successfullyParsed is true
 

LayoutTests/js/kde/func-decl-expected.txt
@@ -3,8 +3,8 @@ KDE JS Test
 On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
 
 
-PASS Function declaration takes effect at entry
-PASS Decl not yet overwritten
+PASS Function declaration takes effect at declaration block
+PASS Decl already overwritten
 PASS After assign (0)
 PASS function decls have no execution content
 PASS After assign #2 (0)

LayoutTests/js/kde/script-tests/func-decl.js
@@ -17,17 +17,14 @@ f = "global";
 
 function test() {
   try {
-    shouldBeOfType("Function declaration takes effect at entry", f, "function");
+    shouldBeOfType("Function declaration takes effect at declaration block", f, "undefined");
   }
   catch (e) {
     testFailed("Scoping very broken!");
   }
 
   for (var i = 0; i < 3; ++i) {
-    if (i == 0)
-      shouldBeOfType("Decl not yet overwritten", f, 'function');
-    else
-      shouldBeOfType("Decl already overwritten", f, 'number');
+    shouldBeOfType("Decl already overwritten", f, 'function');
 
     f = 3;
     shouldBeVal("After assign ("+i+")", f, 3);

LayoutTests/js/parser-syntax-check-expected.txt
@@ -302,8 +302,8 @@ PASS Invalid: "while if (a) ;"
 PASS Invalid: "function f() { while if (a) ; }"
 PASS Valid:   "if (a) function f() {} else function g() {}"
 PASS Valid:   "function f() { if (a) function f() {} else function g() {} }"
-PASS Valid:   "if (a()) while(0) function f() {} else function g() {}" with TypeError
-PASS Valid:   "function f() { if (a()) while(0) function f() {} else function g() {} }"
+PASS Invalid: "if (a()) while(0) function f() {} else function g() {}"
+PASS Invalid: "function f() { if (a()) while(0) function f() {} else function g() {} }"
 PASS Invalid: "if (a()) function f() { else function g() }"
 PASS Invalid: "function f() { if (a()) function f() { else function g() } }"
 PASS Invalid: "if (a) if (b) ; else function f {}"
@@ -676,16 +676,50 @@ PASS Invalid: "'use strict'; function f1(a) {}; let f1; "
 PASS Invalid: "function f() { 'use strict'; function f1(a) {}; let f1;  }"
 PASS Invalid: "let f1; function f1(a) {};"
 PASS Invalid: "function f() { let f1; function f1(a) {}; }"
-PASS Invalid: "let f1; { function f1(a) {}; }"
-PASS Invalid: "function f() { let f1; { function f1(a) {}; } }"
-PASS Invalid: "{ function f1(a) {}; } let f1;"
-PASS Invalid: "function f() { { function f1(a) {}; } let f1; }"
-PASS Valid:   "{ let f1; function f1(a) {}; }"
-PASS Valid:   "function f() { { let f1; function f1(a) {}; } }"
-PASS Valid:   "{  function f1(a) {}; let f1; }"
-PASS Valid:   "function f() { {  function f1(a) {}; let f1; } }"
+PASS Valid:   "function foo() { let f1; { function f1(a) {}; } }"
+PASS Valid:   "function f() { function foo() { let f1; { function f1(a) {}; } } }"
+PASS Valid:   "function foo() { { function f1(a) {}; } let f1; }"
+PASS Valid:   "function f() { function foo() { { function f1(a) {}; } let f1; } }"
+PASS Valid:   "function foo() { { function foo() { }; function foo() { } } }"
+PASS Valid:   "function f() { function foo() { { function foo() { }; function foo() { } } } }"
+PASS Invalid: "function foo() { 'use strict'; { function foo() { }; function foo() { } } }"
+PASS Invalid: "function f() { function foo() { 'use strict'; { function foo() { }; function foo() { } } } }"
+PASS Invalid: "function foo() { let f1; function f1(a) {}; }"
+PASS Invalid: "function f() { function foo() { let f1; function f1(a) {}; } }"
+PASS Invalid: "let f1; function f1(a) {};"
+PASS Invalid: "function f() { let f1; function f1(a) {}; }"
+PASS Valid:   "{ function f1(a) {}; let f1; }"
+PASS Invalid: "function f() { { function f1(a) {}; let f1; } }"
+PASS Valid:   "{ function f1(a) {}; const f1 = 25; }"
+PASS Invalid: "function f() { { function f1(a) {}; const f1 = 25; } }"
+PASS Valid:   "{ function f1(a) {}; class f1{}; }"
+PASS Invalid: "function f() { { function f1(a) {}; class f1{}; } }"
+PASS Invalid: "function foo() { { let bar; function bar() { } } }"
+PASS Invalid: "function f() { function foo() { { let bar; function bar() { } } } }"
+PASS Invalid: "function foo() { { function bar() { }; let bar; } }"
+PASS Invalid: "function f() { function foo() { { function bar() { }; let bar; } } }"
+PASS Invalid: "function foo() { { const bar; function bar() { } } }"
+PASS Invalid: "function f() { function foo() { { const bar; function bar() { } } } }"
+PASS Invalid: "function foo() { { function bar() { }; const bar; } }"
+PASS Invalid: "function f() { function foo() { { function bar() { }; const bar; } } }"
+PASS Invalid: "function foo() { { class bar{}; function bar() { } } }"
+PASS Invalid: "function f() { function foo() { { class bar{}; function bar() { } } } }"
+PASS Invalid: "function foo() { { function bar() { }; class bar{}; } }"
+PASS Invalid: "function f() { function foo() { { function bar() { }; class bar{}; } } }"
 PASS Valid:   "switch('foo') { case 1: function foo() {}; break; case 2: function foo() {}; break; }"
 PASS Valid:   "function f() { switch('foo') { case 1: function foo() {}; break; case 2: function foo() {}; break; } }"
+PASS Valid:   "switch('foo') { case 1: let foo; function foo() {}; break; case 2: function foo() {}; break; }"
+PASS Invalid: "function f() { switch('foo') { case 1: let foo; function foo() {}; break; case 2: function foo() {}; break; } }"
+PASS Valid:   "switch('foo') { case 1: function foo() {}; let foo; break; case 2: function foo() {}; break; }"
+PASS Invalid: "function f() { switch('foo') { case 1: function foo() {}; let foo; break; case 2: function foo() {}; break; } }"
+PASS Valid:   "switch('foo') { case 1: function foo() {}; const foo = 25; break; case 2: function foo() {}; break; }"
+PASS Invalid: "function f() { switch('foo') { case 1: function foo() {}; const foo = 25; break; case 2: function foo() {}; break; } }"
+PASS Valid:   "switch('foo') { case 1: function foo() {}; class foo {} ; break; case 2: function foo() {}; break; }"
+PASS Invalid: "function f() { switch('foo') { case 1: function foo() {}; class foo {} ; break; case 2: function foo() {}; break; } }"
+PASS Valid:   "switch('foo') { case 1: function foo() {}; break; case 2: function foo() {}; break; case 3: let foo; }"
+PASS Invalid: "function f() { switch('foo') { case 1: function foo() {}; break; case 2: function foo() {}; break; case 3: let foo; } }"
+PASS Valid:   "function foo() { switch('foo') { case 1: function foo() {}; break; case 2: function foo() {}; break; case 3: { let foo; } } }"
+PASS Valid:   "function f() { function foo() { switch('foo') { case 1: function foo() {}; break; case 2: function foo() {}; break; case 3: { let foo; } } } }"
 PASS Invalid: "'use strict'; switch('foo') { case 1: function foo() {}; break; case 2: function foo() {}; break; }"
 PASS Invalid: "function f() { 'use strict'; switch('foo') { case 1: function foo() {}; break; case 2: function foo() {}; break; } }"
 PASS Invalid: "'use strict'; switch('foo') { case 1: function foo() {}; break; case 2: let foo; break; }"
@@ -700,6 +734,74 @@ PASS Invalid: "'use strict'; if (true) function foo() { }; "
 PASS Invalid: "function f() { 'use strict'; if (true) function foo() { };  }"
 PASS Valid:   "if (true) function foo() { }; "
 PASS Valid:   "function f() { if (true) function foo() { };  }"
+PASS Invalid: " let foo; if (true) function foo() { };"
+PASS Valid:   "function f() {  let foo; if (true) function foo() { }; }"
+PASS Valid:   "function baz() { let foo; if (true) function foo() { }; }"
+PASS Valid:   "function f() { function baz() { let foo; if (true) function foo() { }; } }"
+PASS Invalid: "if (true) function foo() { }; let foo;"
+PASS Valid:   "function f() { if (true) function foo() { }; let foo; }"
+PASS Invalid: "{ if (true) function foo() { }; } let foo;"
+PASS Valid:   "function f() { { if (true) function foo() { }; } let foo; }"
+PASS Invalid: "let foo; while (false) function foo() { }; "
+PASS Invalid: "function f() { let foo; while (false) function foo() { };  }"
+PASS Invalid: "let foo;  { while (false) function foo() { }; } "
+PASS Invalid: "function f() { let foo;  { while (false) function foo() { }; }  }"
+PASS Invalid: "while (false) function foo() { }; let foo;"
+PASS Invalid: "function f() { while (false) function foo() { }; let foo; }"
+PASS Invalid: "let foo; while (false) label: function foo() { }; "
+PASS Invalid: "function f() { let foo; while (false) label: function foo() { };  }"
+PASS Invalid: "while (false) label: function foo() { }; let foo;"
+PASS Invalid: "function f() { while (false) label: function foo() { }; let foo; }"
+PASS Invalid: "'use strict'; while (false) function foo() { }; "
+PASS Invalid: "function f() { 'use strict'; while (false) function foo() { };  }"
+PASS Invalid: "'use strict'; if (false) function foo() { }; "
+PASS Invalid: "function f() { 'use strict'; if (false) function foo() { };  }"
+PASS Invalid: "'use strict'; do function foo() { } while (false); "
+PASS Invalid: "function f() { 'use strict'; do function foo() { } while (false);  }"
+PASS Invalid: "while (false) function foo() { }; "
+PASS Invalid: "function f() { while (false) function foo() { };  }"
+PASS Valid:   "if (false) function foo() { }; "
+PASS Valid:   "function f() { if (false) function foo() { };  }"
+PASS Invalid: "do function foo() { } while (false); "
+PASS Invalid: "function f() { do function foo() { } while (false);  }"
+PASS Invalid: "if (cond) label: function foo() { }"
+PASS Invalid: "function f() { if (cond) label: function foo() { } }"
+PASS Invalid: "while (true) { while (true) function bar() { } }"
+PASS Invalid: "function f() { while (true) { while (true) function bar() { } } }"
+PASS Invalid: "with ({}) function bar() { }"
+PASS Invalid: "function f() { with ({}) function bar() { } }"
+PASS Valid:   "function bar() { label: function baz() { } }"
+PASS Valid:   "function f() { function bar() { label: function baz() { } } }"
+PASS Valid:   "function bar() { let: function baz() { } }"
+PASS Valid:   "function f() { function bar() { let: function baz() { } } }"
+PASS Invalid: "function bar() { 'use strict'; let: function baz() { } }"
+PASS Invalid: "function f() { function bar() { 'use strict'; let: function baz() { } } }"
+PASS Valid:   "function bar() { yield: function baz() { } }"
+PASS Valid:   "function f() { function bar() { yield: function baz() { } } }"
+PASS Valid:   "function bar() { label: label2: function baz() { } }"
+PASS Valid:   "function f() { function bar() { label: label2: function baz() { } } }"
+PASS Valid:   "function bar() { label: label2: label3: function baz() { } }"
+PASS Valid:   "function f() { function bar() { label: label2: label3: function baz() { } } }"
+PASS Invalid: "function bar() { label: label2: label weird: function baz() { } }"
+PASS Invalid: "function f() { function bar() { label: label2: label weird: function baz() { } } }"
+PASS Valid:   "function bar() { label: label2: label3: function baz() { } }"
+PASS Valid:   "function f() { function bar() { label: label2: label3: function baz() { } } }"
+PASS Invalid: "function bar() { 'use strict'; label: label2: label 3: function baz() { } }"
+PASS Invalid: "function f() { function bar() { 'use strict'; label: label2: label 3: function baz() { } } }"
+PASS Invalid: "function bar() { if (cond) label: function foo() { } }"
+PASS Invalid: "function f() { function bar() { if (cond) label: function foo() { } } }"
+PASS Invalid: "function bar() { while (cond) label: function foo() { } }"
+PASS Invalid: "function f() { function bar() { while (cond) label: function foo() { } } }"
+PASS Valid:   "label: function foo() { }"
+PASS Valid:   "function f() { label: function foo() { } }"
+PASS Valid:   "let: function foo() { }"
+PASS Valid:   "function f() { let: function foo() { } }"
+PASS Valid:   "yield: function foo() { }"
+PASS Valid:   "function f() { yield: function foo() { } }"
+PASS Valid:   "yield: let: function foo() { }"
+PASS Valid:   "function f() { yield: let: function foo() { } }"
+PASS Invalid: "'use strict'; yield: let: function foo() { }"
+PASS Invalid: "function f() { 'use strict'; yield: let: function foo() { } }"
 PASS Valid:   "var str = "'use strict'; function f1(a) { function f2(b) { return b; } return f2(a); } return f1(arguments[0]);"; var foo = new Function(str); foo(5);"
 PASS Valid:   "function f() { var str = "'use strict'; function f1(a) { function f2(b) { return b; } return f2(a); } return f1(arguments[0]);"; var foo = new Function(str); foo(5); }"
 PASS Valid:   "var str = "'use strict'; function f1(a) { function f2(b) { function f3(c) { return c; } return f3(b); } return f2(a); } return f1(arguments[0]);"; var foo = new Function(str); foo(5);"

LayoutTests/js/script-tests/function-declaration-statement.js
@@ -35,117 +35,6 @@ function ifElseTest()
 
 shouldBeTrue("ifElseTest()");
 
-function doWhileTest()
-{
-    var i = 0;
-    do
-        function f()
-        {
-            return true;
-        }
-    while (i++ < 10)
-
-    return f();
-}
-
-shouldBeTrue("doWhileTest()");
-
-function whileTest()
-{
-    var i = 0;
-    while (i++ < 10)
-        function f()
-        {
-            return true;
-        }
-
-    return f();
-}
-
-shouldBeTrue("whileTest()");
-
-function forTest()
-{
-    var i;
-    for (i = 0; i < 10; ++i)
-        function f()
-        {
-            return true;
-        }
-
-    return f();
-}
-
-shouldBeTrue("forTest()");
-
-function forVarTest()
-{
-    for (var i = 0; i < 10; ++i)
-        function f()
-        {
-            return true;
-        }
-
-    return f();
-}
-
-shouldBeTrue("forVarTest()");
-
-function forInTest()
-{
-    var a;
-    for (a in { field: false })
-        function f()
-        {
-            return true;
-        }
-
-    return f();
-}
-
-shouldBeTrue("forInTest()");
-
-function forInVarTest()
-{
-    var a;
-    for (var a in { field: false })
-        function f()
-        {
-            return true;
-        }
-
-    return f();
-}
-
-shouldBeTrue("forInVarTest()");
-
-function forInVarInitTest()
-{
-    var a;
-    for (var a in { field: false })
-        function f()
-        {
-            return true;
-        }
-
-    return f();
-}
-
-shouldBeTrue("forInVarInitTest()");
-
-function withTest()
-{
-    with ({ })
-        function f()
-        {
-            return true;
-        }
-
-    return f();
-}
-
-shouldBeTrue("withTest()");
-
 function labelTest()
 {
     label:

commit 087d4d292a0ab6669bfe2937196fb130c7e84c24

jsc-layout-tests.yaml/js/script-tests/regress-141098.js failing on Yosemite Debug after r198989 https://bugs.webkit.org/show_bug.cgi?id=156187
Reviewed by Filip Pizlo.

This is a speculative fix. Lets see if the prevents the timeout.

* parser/Parser.cpp:
(JSC::Parser<LexerType>::parseStatementListItem):


Canonical link: https://commits.webkit.org/174343@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@199070 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/parser/Parser.cpp
@@ -542,6 +542,9 @@ template <class TreeBuilder> TreeStatement Parser<LexerType>::parseStatementList
 {
     // The grammar is documented here:
     // http://www.ecma-international.org/ecma-262/6.0/index.html#sec-statements
+
+    failIfStackOverflow();
+
     DepthManager statementDepth(&m_statementDepth);
     m_statementDepth++;
     TreeStatement result = 0;