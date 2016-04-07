57aa2abcfc9b86906b70b91edc8538a18b53f822
Simon Fraser <simon.fraser@apple.com>
2016-04-07 01:46:53 +0000

    Fix Windows build by converting clampToInteger() into a template that only takes integral types.
    * wtf/MathExtras.h:
    (clampToInteger):
    
    Canonical link: https://commits.webkit.org/174405@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@199133 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/MathExtras.h
+++ Source/WTF/wtf/MathExtras.h
@@ -181,18 +181,12 @@ inline int clampToInteger(float value)
     return clampTo<int>(value);
 }
 
-inline int clampToInteger(unsigned x)
+template<typename T>
+inline int clampToInteger(T x)
 {
-    const unsigned intMax = static_cast<unsigned>(std::numeric_limits<int>::max());
+    static_assert(std::numeric_limits<T>::is_integer, "T must be an integer.");
 
-    if (x >= intMax)
-        return std::numeric_limits<int>::max();
-    return static_cast<int>(x);
-}
-
-inline int clampToInteger(size_t x)
-{
-    const size_t intMax = static_cast<size_t>(std::numeric_limits<int>::max());
+    const T intMax = static_cast<unsigned>(std::numeric_limits<int>::max());
 
     if (x >= intMax)
         return std::numeric_limits<int>::max();
