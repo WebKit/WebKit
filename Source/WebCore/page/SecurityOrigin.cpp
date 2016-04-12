b1daeb9a1f579482ecf5dcad8a33374c520b8c1c
Brent Fulgham <bfulgham@webkit.org>
2016-04-12 13:25:54 +0000

    Merge r197858 - Local HTML should be blocked from localStorage access unless "Disable Local File Restrictions" is checked.. https://bugs.webkit.org/show_bug.cgi?id=155185 <rdar://problem/11101440>
    Reviewed by Zalan Bujtas.
    
    Source/WebCore:
    
    Tested by storage/domstorage/localstorage/blocked-file-access.html.
    
    * page/SecurityOrigin.cpp:
    (WebCore::SecurityOrigin::canAccessStorage): If the origin is a local file, and we have not been granted
    universal file access, prevent access to DOM localStorage.
    
    LayoutTests:
    
    * storage/domstorage/localstorage/blocked-file-access-expected.txt: Added.
    * storage/domstorage/localstorage/blocked-file-access.html: Added.
    * storage/domstorage/localstorage/resources/blocked-example.html: Added.

--- Source/WebCore/page/SecurityOrigin.cpp
+++ Source/WebCore/page/SecurityOrigin.cpp
@@ -375,6 +375,9 @@ bool SecurityOrigin::canAccessStorage(const SecurityOrigin* topOrigin, ShouldAll
     if (m_storageBlockingPolicy == BlockAllStorage)
         return false;
 
+    if (isLocal() && !m_universalAccess)
+        return false;
+
     // FIXME: This check should be replaced with an ASSERT once we can guarantee that topOrigin is not null.
     if (!topOrigin)
         return true;
