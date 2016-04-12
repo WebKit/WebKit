d585a5fd6c6f66d87697ca0a62f12dc359dd3c61
Miguel Gomez <magomez@igalia.com>
2016-04-12 16:46:37 +0000

    Merge r198205 - Leak: Accelerated ImageBufferCairo doesn't destroy the used textures https://bugs.webkit.org/show_bug.cgi?id=155431
    Patch by Miguel Gomez <magomez@igalia.com> on 2016-03-15
    Reviewed by Žan Doberšek.
    
    When using the Cairo backend, add a destructor to ImageBufferData and use it to destroy the
    textures created if the buffer is being accelerated.
    
    No new tests, already covered by existing ones.
    
    * platform/graphics/cairo/ImageBufferCairo.cpp:
    (WebCore::ImageBufferData::ImageBufferData):
    Store the renderingMode flag.
    (WebCore::ImageBufferData::~ImageBufferData):
    Destroy gl resources if renderingMode is accelerated.
    (WebCore::ImageBuffer::ImageBuffer):
    Pass renderingMode to the data class and use it fro checks instead of the function parameter.
    * platform/graphics/cairo/ImageBufferDataCairo.h:
    Add destructor and a renderingMode flag.

--- Source/WebCore/platform/graphics/cairo/ImageBufferDataCairo.h
+++ Source/WebCore/platform/graphics/cairo/ImageBufferDataCairo.h
@@ -54,12 +54,14 @@ class ImageBufferData
 #endif
 {
 public:
-    ImageBufferData(const IntSize&);
+    ImageBufferData(const IntSize&, RenderingMode);
+    virtual ~ImageBufferData();
 
     RefPtr<cairo_surface_t> m_surface;
     PlatformContextCairo m_platformContext;
     std::unique_ptr<GraphicsContext> m_context;
     IntSize m_size;
+    RenderingMode m_renderingMode;
 
 #if ENABLE(ACCELERATED_2D_CANVAS)
     void createCairoGLSurface();
