7be7ae1b295b1c745451feeefb0379190276326b
youenn.fablet@crf.canon.fr <youenn.fablet@crf.canon.fr>
2016-04-13 07:25:27 +0000

    Merge r198394 - Move IndexedDB regular test to web-platform-tests https://bugs.webkit.org/show_bug.cgi?id=155581
    Reviewed by Darin Adler.
    
    LayoutTests/imported/w3c:
    
    Moving regular IndexedDB tests from LayoutTests/imported/w3c/indexeddb to LayoutTests/imported/w3c/web-platform-tests/IndexedDB.
    Moving private browsing IndexedDB tests from LayoutTests/imported/w3c/indexeddb to LayoutTests/imported/w3c/IndexedDB-private-browsing.
    Removed private prefix from private browsing IndexedDB tests since they are stored in their own folder.
    
    Modified support.js to remove unnecessary calls to testRunner API.
    Modified also IndexedDB-private-browsing/support.js to always activate testRunner private browsing mode.
    
    LayoutTests:
    
    Renaming indexeddb test expectation according new path and directory name.
    
    * TestExpectations:
    * platform/efl/TestExpectations:
    * platform/gtk/TestExpectations:
    * platform/ios-simulator-wk1/TestExpectations:
    * platform/mac-wk2/TestExpectations:
    * platform/win/TestExpectations:

--- LayoutTests/imported/w3c/web-platform-tests/IndexedDB/support.js
+++ LayoutTests/imported/w3c/web-platform-tests/IndexedDB/support.js
@@ -1,12 +1,3 @@
-if (window.testRunner) {
-    testRunner.dumpAsText();
-    testRunner.waitUntilDone();
-
-    // If the test file URL ends in "-private.htm(l)", enable private browsing.
-    if (window.location.href.endsWith("-private.htm") || window.location.href.endsWith("-private.html"))
-        testRunner.setPrivateBrowsingEnabled(true);
-}
-
 var databaseName = "database";
 var databaseVersion = 1;
 
