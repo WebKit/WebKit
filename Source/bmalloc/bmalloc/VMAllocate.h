b7fc1fabb527e04139196fee5ece2f5b22cc55ae
David Kilzer <ddkilzer@webkit.org>
2016-04-13 09:38:02 +0000

    Merge r198809 - bmalloc: add logging for mmap() failures <http://webkit.org/b/155409> <rdar://problem/24568515>
    Reviewed by Saam Barati.
    
    This patch causes additional logging to be generated on internal
    iOS builds when mmap() fails.  We are trying to track down an
    issue where the WebContent process runs out of VM address space
    before it is killed by jetsam.
    
    * CMakeLists.txt: Add Logging.cpp.
    * bmalloc.xcodeproj/project.pbxproj: Add new files.
    
    * bmalloc/BAssert.h:
    (RELEASE_BASSERT_WITH_MESSAGE): Add macro.
    * bmalloc/Logging.cpp: Added.
    (bmalloc::logVMFailure): Implementation.
    * bmalloc/Logging.h: Added.
    (bmalloc::logVMFailure): Declaration.
    * bmalloc/VMAllocate.h:
    (bmalloc::tryVMAllocate): Call logVMFailure() on mmap() failure.
    * bmalloc/darwin/BSoftLinking.h: Copied from Source/WebCore/platform/mac/SoftLinking.h.

--- Source/bmalloc/bmalloc/VMAllocate.h
+++ Source/bmalloc/bmalloc/VMAllocate.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Apple Inc. All rights reserved.
+ * Copyright (C) 2014-2016 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,6 +27,7 @@
 #define VMAllocate_h
 
 #include "BAssert.h"
+#include "Logging.h"
 #include "Range.h"
 #include "Sizes.h"
 #include "Syscall.h"
@@ -77,8 +78,10 @@ inline void* tryVMAllocate(size_t vmSize)
 {
     vmValidate(vmSize);
     void* result = mmap(0, vmSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, BMALLOC_VM_TAG, 0);
-    if (result == MAP_FAILED)
+    if (result == MAP_FAILED) {
+        logVMFailure();
         return nullptr;
+    }
     return result;
 }
 
