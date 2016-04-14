5aa4d46e9ec06d29f4abff20778056288139e3d1
Geoffrey Garen <ggaren@apple.com>
2016-04-14 17:56:31 +0000

    Some imported ES6 tests are missing __createIterableObject https://bugs.webkit.org/show_bug.cgi?id=156584
    Reviewed by Keith Miller.
    
    These tests were failing because I neglected to include __createIterableObject
    when I first imported them. Now they pass.
    
    * tests/es6.yaml:
    * tests/es6/Array_static_methods_Array.from_generic_iterables.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test):
    * tests/es6/Array_static_methods_Array.from_instances_of_generic_iterables.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test):
    * tests/es6/Array_static_methods_Array.from_iterator_closing.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    * tests/es6/Array_static_methods_Array.from_map_function_generic_iterables.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test):
    * tests/es6/Array_static_methods_Array.from_map_function_instances_of_iterables.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test):
    * tests/es6/Map_iterator_closing.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    * tests/es6/Promise_Promise.all_generic_iterables.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test.asyncTestPassed):
    * tests/es6/Promise_Promise.race_generic_iterables.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test.asyncTestPassed):
    * tests/es6/Set_iterator_closing.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    * tests/es6/WeakMap_iterator_closing.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    * tests/es6/WeakSet_iterator_closing.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    * tests/es6/destructuring_iterator_closing.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    * tests/es6/destructuring_with_generic_iterables.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test):
    * tests/es6/destructuring_with_instances_of_generic_iterables.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test):
    * tests/es6/for..of_loops_iterator_closing_break.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    * tests/es6/for..of_loops_iterator_closing_throw.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    * tests/es6/for..of_loops_with_generic_iterables.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test):
    * tests/es6/for..of_loops_with_instances_of_generic_iterables.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test):
    * tests/es6/generators_yield_star_generic_iterables.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    * tests/es6/generators_yield_star_iterator_closing_via_throw.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    * tests/es6/spread_..._operator_with_generic_iterables_in_arrays.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test):
    * tests/es6/spread_..._operator_with_generic_iterables_in_calls.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test):
    * tests/es6/spread_..._operator_with_instances_of_iterables_in_arrays.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test):
    * tests/es6/spread_..._operator_with_instances_of_iterables_in_calls.js:
    (iterator.next):
    (iterable.Symbol.iterator):
    (__createIterableObject):
    (test):
    
    
    Canonical link: https://commits.webkit.org/174683@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@199543 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/tests/es6/for..of_loops_with_instances_of_generic_iterables.js
+++ Source/JavaScriptCore/tests/es6/for..of_loops_with_instances_of_generic_iterables.js
@@ -1,3 +1,23 @@
+var global = this;
+
+function __createIterableObject(arr, methods) {
+  methods = methods || {};
+  if (typeof Symbol !== 'function' || !Symbol.iterator) {
+    return {};
+  }
+  arr.length++;
+  var iterator = {
+    next: function() {
+      return { value: arr.shift(), done: arr.length <= 0 };
+    },
+    'return': methods['return'],
+    'throw': methods['throw']
+  };
+  var iterable = {};
+  iterable[Symbol.iterator] = function(){ return iterator; }
+  return iterable;
+}
+
 function test() {
 
 var result = "";
--- Source/JavaScriptCore/tests/es6/generators_yield_star_generic_iterables.js
+++ Source/JavaScriptCore/tests/es6/generators_yield_star_generic_iterables.js
@@ -1,22 +1,21 @@
-if (typeof global === 'undefined') {
 var global = this;
-}
+
 function __createIterableObject(arr, methods) {
-methods = methods || {};
-if (typeof Symbol !== 'function' || !Symbol.iterator) {
-  return {};
-}
-arr.length++;
-var iterator = {
-  next: function() {
-    return { value: arr.shift(), done: arr.length <= 0 };
-  },
-  'return': methods['return'],
-  'throw': methods['throw']
-};
-var iterable = {};
-iterable[Symbol.iterator] = function(){ return iterator; }
-return iterable;
+  methods = methods || {};
+  if (typeof Symbol !== 'function' || !Symbol.iterator) {
+    return {};
+  }
+  arr.length++;
+  var iterator = {
+    next: function() {
+      return { value: arr.shift(), done: arr.length <= 0 };
+    },
+    'return': methods['return'],
+    'throw': methods['throw']
+  };
+  var iterable = {};
+  iterable[Symbol.iterator] = function(){ return iterator; }
+  return iterable;
 }
 
 function test() {
