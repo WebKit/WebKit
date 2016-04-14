a62f4b0920d301b8120e9ab4bffdd44577ae2c01
Joseph Pecoraro <pecoraro@apple.com>
2016-04-14 21:46:20 +0000

    Web Inspector: Add a JavaScript Formatting test for template strings https://bugs.webkit.org/show_bug.cgi?id=156600
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2016-04-14
    Reviewed by Brian Burg.
    
    Source/WebInspectorUI:
    
    * Tools/Formatting/index.html:
    
    LayoutTests:
    
    * inspector/formatting/formatting-javascript-expected.txt:
    * inspector/formatting/formatting-javascript.html:
    * inspector/formatting/resources/javascript-tests/template-strings-expected.js: Added.
    * inspector/formatting/resources/javascript-tests/template-strings.js: Added.
    
    Canonical link: https://commits.webkit.org/174696@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@199556 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/inspector/formatting/resources/javascript-tests/template-strings-expected.js
@@ -0,0 +1,10 @@
+`template string`
+`${1 + 1}`
+`${1 + 1}`
+`before ${1 + 1} after`
+`before ${`nested ${1 + 1}`} after`
+`before ${`nested ${1 + 1}`} after`
+
+tag`tagged template string`
+tag`tagged template string`
+tag`before ${1 + 1} after`
