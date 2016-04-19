c8ea1e4817c66eec09f9dd1c27afb73d734db654
Alex Christensen <achristensen@apple.com>
2016-04-19 21:45:44 +0000

    Build fix after r199738
    Source/ThirdParty/ANGLE:
    
    * CMakeLists.txt:
    
    Source/WebCore:
    
    * platform/graphics/efl/GraphicsContext3DEfl.cpp:
    (WebCore::GraphicsContext3D::GraphicsContext3D):
    * platform/graphics/win/GraphicsContext3DWin.cpp:
    (WebCore::GraphicsContext3D::GraphicsContext3D):
    
    
    
    Canonical link: https://commits.webkit.org/174878@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@199743 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/efl/GraphicsContext3DEfl.cpp
+++ Source/WebCore/platform/graphics/efl/GraphicsContext3DEfl.cpp
@@ -39,7 +39,7 @@ PassRefPtr<GraphicsContext3D> GraphicsContext3D::create(GraphicsContext3D::Attri
 GraphicsContext3D::GraphicsContext3D(GraphicsContext3D::Attributes attrs, HostWindow* hostWindow, GraphicsContext3D::RenderStyle renderStyle)
     : m_currentWidth(0)
     , m_currentHeight(0)
-    , m_compiler(isGLES2Compliant() ? SH_ESSL_OUTPUT : SH_GLSL_OUTPUT)
+    , m_compiler(isGLES2Compliant() ? SH_ESSL_OUTPUT : SH_GLSL_COMPATIBILITY_OUTPUT)
     , m_attrs(attrs)
     , m_renderStyle(renderStyle)
     , m_texture(0)
