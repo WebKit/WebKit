928302516b9eb1a1a879ead0ebffcfaf4ce3bb97
Keith Miller <keith_miller@apple.com>
2016-04-21 21:00:02 +0000

    WebScriptObject description swizzler should work in a multi-threaded world https://bugs.webkit.org/show_bug.cgi?id=156808
    Source/WebCore:
    
    Reviewed by Geoffrey Garen.
    
    A WebKit legacy API user might be running Objective-C code on another thread.
    Since we don't want to corrupt other thread's NSObject description method
    we use TLS to record if we are in the stringValue function. As an attempt to
    preserve any user swizzling we update the non-stringValue NSObject description
    method on each call to stringValue if it has changed. Additionally, the TLS
    needs to be a int because the user might call into stringValue, back into JS,
    then back into stringValue. If the TLS was a boolean then it would be unset
    at that point so when we return into the first stringValue call we would call
    the original NSObject description method rather than our override.
    
    Test added to API tests: WebKit1.WebScriptObjectDescription
    
    * bridge/objc/objc_instance.mm:
    (-[NSObject _web_description]):
    (ObjcInstance::stringValue):
    (swizzleNSObjectDescription): Deleted.
    
    Tools:
    
    Add a test for our NSObject swizzling TLS implementation. The test runs on
    two threads. One in JS and another in Objective-C. We expect the JS thread
    to use our NSObject description override and the Objective-C thread to act
    as though it was using the original NSObject description method.
    
    Reviewed by Geoffrey Garen.
    
    * TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
    * TestWebKitAPI/Tests/mac/WebScriptObjectDescription.html: Added.
    * TestWebKitAPI/Tests/mac/WebScriptObjectDescription.mm: Added.
    (nsObjectDescriptionTest):
    (-[WebScriptDescriptionTest webView:didFinishLoadForFrame:]):
    (TestWebKitAPI::TEST):
    
    
    Canonical link: https://commits.webkit.org/174969@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@199834 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bridge/objc/objc_instance.mm
+++ Source/WebCore/bridge/objc/objc_instance.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004, 2008, 2009, 2013 Apple Inc. All rights reserved.
+ * Copyright (C) 2004, 2008, 2009, 2013, 2016 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -37,7 +37,9 @@
 #import <runtime/ObjectPrototype.h>
 #import <wtf/Assertions.h>
 #import <wtf/HashMap.h>
+#import <wtf/MainThread.h>
 #import <wtf/NeverDestroyed.h>
+#import <wtf/ThreadSpecific.h>
 
 #ifdef NDEBUG
 #define OBJC_LOG(formatAndArgs...) ((void)0)
@@ -459,17 +461,42 @@ JSC::JSValue ObjcInstance::defaultValue(ExecState* exec, PreferredPrimitiveType 
     return valueOf(exec);
 }
 
-static NSString* swizzleNSObjectDescription(id self, SEL)
+static WTF::ThreadSpecific<uint32_t>* s_descriptionDepth;
+
+@interface NSObject (WebDescriptionCategory)
+- (NSString *)_web_description;
+@end
+
+@implementation NSObject (WebDescriptionCategory)
+
+- (NSString *)_web_description
 {
-    return [NSString stringWithFormat:@"%@%@%@", @"<", NSStringFromClass([self class]), @">"];
+    ASSERT(s_descriptionDepth);
+    if (s_descriptionDepth->isSet() && **s_descriptionDepth)
+        return [NSString stringWithFormat:@"<%@>", NSStringFromClass([self class])];
+    // We call _web_description here since this method should only be called
+    // once we have already swizzled this method with the one on NSObject.
+    // Thus, _web_description is actually the original description method.
+    return [self _web_description];
 }
+
+@end
 
 JSC::JSValue ObjcInstance::stringValue(ExecState* exec) const
 {
-    auto method = class_getInstanceMethod([NSObject class], @selector(description));
-    IMP originalNSObjectDescription = method_setImplementation(method, (IMP)swizzleNSObjectDescription);
+    static std::once_flag initializeDescriptionDepthOnceFlag;
+    std::call_once(initializeDescriptionDepthOnceFlag, [] {
+        s_descriptionDepth = new WTF::ThreadSpecific<uint32_t>();
+        **s_descriptionDepth = 0;
+
+        auto descriptionMethod = class_getInstanceMethod([NSObject class], @selector(description));
+        auto webDescriptionMethod = class_getInstanceMethod([NSObject class], @selector(_web_description));
+        method_exchangeImplementations(descriptionMethod, webDescriptionMethod);
+    });
+
+    (**s_descriptionDepth)++;
     JSC::JSValue result = convertNSStringToString(exec, [getObject() description]);
-    method_setImplementation(method, originalNSObjectDescription);
+    (**s_descriptionDepth)--;
     return result;
 }
 
