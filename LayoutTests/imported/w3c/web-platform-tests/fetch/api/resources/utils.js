e1f959299fd2ded091bf657cc47d47fd48dccbca
Youenn Fablet <youennf@gmail.com>
2016-04-21 18:11:28 +0000

    [Fetch API] Improve some fetch response streams tests https://bugs.webkit.org/show_bug.cgi?id=156848
    Reviewed by Darin Adler.
    
    Most important changes are for response-stream-disturbed-2.html and response-stream-disturbed-5.html which were broken.
    response-stream-disturbed-2.html was calling an undefined function and was expecting to get a resolved promise while it should be rejected.
    response-stream-disturbed-5.html was expecting to have a null body if data is consumed.
    After rereading the spec, this test is non conformant, as the body should not be null, but getting the reader should throw.
    
    * web-platform-tests/fetch/api/resources/utils.js: Adding delay helper function.
    * web-platform-tests/fetch/api/response/response-cancel-stream.html: Using delay function.
    * web-platform-tests/fetch/api/response/response-stream-disturbed-1.html: Removing unused function.
    * web-platform-tests/fetch/api/response/response-stream-disturbed-2-expected.txt: Rebasing
    * web-platform-tests/fetch/api/response/response-stream-disturbed-2.html: Fixing test.
    * web-platform-tests/fetch/api/response/response-stream-disturbed-5-expected.txt: Rebasing
    * web-platform-tests/fetch/api/response/response-stream-disturbed-5.html: Fixing test.
    
    
    Canonical link: https://commits.webkit.org/174960@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@199825 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/imported/w3c/web-platform-tests/fetch/api/resources/utils.js
+++ LayoutTests/imported/w3c/web-platform-tests/fetch/api/resources/utils.js
@@ -74,3 +74,11 @@ function validateStreamFromString(reader, expectedValue, retrievedArrayBuffer) {
     validateBufferFromString(retrievedArrayBuffer, expectedValue, "Retrieve and verify stream");
   });
 }
+
+// From streams tests
+function delay(milliseconds)
+{
+    return new Promise(function(resolve) {
+        step_timeout(resolve, milliseconds);
+    });
+}
