a8720fd0d03da95118a28d2db70da828626c9f0d
Manuel Rego Casasnovas <rego@igalia.com>
2016-04-22 07:54:22 +0000

    [css-grid] Fix bug with positioned items in vertical writing mode https://bugs.webkit.org/show_bug.cgi?id=156870
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    In RenderGrid::offsetAndBreadthForPositionedChild() we were using
    directly borderLeft(), which is wrong in vertical writing modes.
    
    To fix it we just need to use borderLogicalLeft() which is aware of
    the current writing mode.
    
    Test: fast/css-grid-layout/grid-positioned-children-writing-modes.html
    
    * rendering/RenderGrid.cpp:
    (WebCore::RenderGrid::offsetAndBreadthForPositionedChild):
    
    LayoutTests:
    
    Add new test to check positioned items in different writing modes
    and direction combinations.
    
    * fast/css-grid-layout/grid-positioned-children-writing-modes-expected.html: Added.
    * fast/css-grid-layout/grid-positioned-children-writing-modes.html: Added.
    
    
    Canonical link: https://commits.webkit.org/175004@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@199874 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderGrid.cpp
+++ Source/WebCore/rendering/RenderGrid.cpp
@@ -1583,7 +1583,7 @@ void RenderGrid::offsetAndBreadthForPositionedChild(const RenderBox& child, Grid
         if (endIsAuto)
             offset = LayoutUnit();
         else {
-            offset = translateRTLCoordinate(m_columnPositions[endLine]) - borderLeft();
+            offset = translateRTLCoordinate(m_columnPositions[endLine]) - borderLogicalLeft();
 
             if (endLine > firstExplicitLine && endLine < lastExplicitLine) {
                 offset += guttersSize(direction, 2);
