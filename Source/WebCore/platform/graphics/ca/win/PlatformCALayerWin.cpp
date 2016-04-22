d3d43dff7511fe0512ebfb57522237e3e2ce5e52
Dean Jackson <dino@apple.com>
2016-04-22 19:01:06 +0000

    Attempting to fix Windows build. Add isHidden implementation.
    * platform/graphics/ca/win/PlatformCALayerWin.cpp:
    (PlatformCALayerWin::isHidden):
    
    Canonical link: https://commits.webkit.org/175016@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@199886 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/ca/win/PlatformCALayerWin.cpp
+++ Source/WebCore/platform/graphics/ca/win/PlatformCALayerWin.cpp
@@ -437,6 +437,11 @@ void PlatformCALayerWin::setSublayerTransform(const TransformationMatrix& value)
     setNeedsCommit();
 }
 
+bool PlatformCALayerWin::isHidden(bool value) const
+{
+    return CACFLayerGetHidden(m_layer.get());
+}
+
 void PlatformCALayerWin::setHidden(bool value)
 {
     CACFLayerSetHidden(m_layer.get(), value);
