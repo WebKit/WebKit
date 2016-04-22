1e5f1aea485b29dd20257fbf2fcd831ae5213fc1
Anders Carlsson <andersca@apple.com>
2016-04-22 21:06:50 +0000

    WKWebView WebSQL is not enabled https://bugs.webkit.org/show_bug.cgi?id=156928 rdar://problem/19029603
    Reviewed by Beth Dakin.
    
    Give databases a default quota of 50 MB, matching what we have in UIWebView.
    
    * UIProcess/Cocoa/UIDelegate.mm:
    (WebKit::UIDelegate::UIClient::exceededDatabaseQuota):
    
    Canonical link: https://commits.webkit.org/175027@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@199901 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/Cocoa/UIDelegate.mm
+++ Source/WebKit2/UIProcess/Cocoa/UIDelegate.mm
@@ -239,7 +239,11 @@ void UIDelegate::UIClient::runJavaScriptPrompt(WebKit::WebPageProxy*, const WTF:
 void UIDelegate::UIClient::exceededDatabaseQuota(WebPageProxy*, WebFrameProxy*, API::SecurityOrigin* securityOrigin, const WTF::String& databaseName, const WTF::String& displayName, unsigned long long currentQuota, unsigned long long currentOriginUsage, unsigned long long currentUsage, unsigned long long expectedUsage, std::function<void (unsigned long long)> completionHandler)
 {
     if (!m_uiDelegate.m_delegateMethods.webViewDecideDatabaseQuotaForSecurityOriginCurrentQuotaCurrentOriginUsageCurrentDatabaseUsageExpectedUsageDecisionHandler) {
-        completionHandler(currentQuota);
+
+        // Use 50 MB as the default database quota.
+        unsigned long long defaultPerOriginDatabaseQuota = 50 * 1024 * 1024;
+
+        completionHandler(defaultPerOriginDatabaseQuota);
         return;
     }
 
