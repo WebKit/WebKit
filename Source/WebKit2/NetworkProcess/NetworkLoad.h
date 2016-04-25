cc05da8ccdcab6fbb3b30bf1fb76b5749526ebdc
Chris Dumez <cdumez@apple.com>
2016-04-25 13:31:48 +0000

    Merge r199599 - Mark NetworkLoad as FastAllocated https://bugs.webkit.org/show_bug.cgi?id=156628
    Reviewed by Alex Christensen.
    
    Mark NetworkLoad as FastAllocated for performance.
    
    * NetworkProcess/NetworkLoad.h:

--- Source/WebKit2/NetworkProcess/NetworkLoad.h
+++ Source/WebKit2/NetworkProcess/NetworkLoad.h
@@ -44,6 +44,7 @@ class NetworkLoad : public WebCore::ResourceHandleClient
     , public NetworkDataTaskClient
 #endif
 {
+    WTF_MAKE_FAST_ALLOCATED;
 public:
     NetworkLoad(NetworkLoadClient&, const NetworkLoadParameters&);
     ~NetworkLoad();
