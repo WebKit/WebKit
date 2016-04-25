f2e9fa06235d69e603deda48f5451bb44b51c5d3
Dan Bernstein <mitz@apple.com>
2016-04-25 17:33:08 +0000

    Fix a couple of mistakes from r199700.
    Rubber-stamped by Anders Carlsson.
    
    * UIProcess/API/Cocoa/WKPreferencesPrivate.h: Added availability attributes to new properties.
    * WebKit2.xcodeproj/project.pbxproj: Demoted _WKExperimentalFeatureInternal.h from Private to Project.
    
    
    
    Canonical link: https://commits.webkit.org/175089@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@200033 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/Cocoa/WKPreferencesPrivate.h
+++ Source/WebKit2/UIProcess/API/Cocoa/WKPreferencesPrivate.h
@@ -87,9 +87,9 @@ typedef NS_OPTIONS(NSUInteger, _WKJavaScriptRuntimeFlags) {
 @property (nonatomic, setter=_setOfflineApplicationCacheIsEnabled:) BOOL _offlineApplicationCacheIsEnabled;
 @property (nonatomic, setter=_setFullScreenEnabled:) BOOL _fullScreenEnabled WK_AVAILABLE(10_11, 9_0);
 
-+ (WK_ARRAY(_WKExperimentalFeature *) *)_experimentalFeatures;
-- (BOOL)_isEnabledForFeature:(_WKExperimentalFeature *)feature;
-- (void)_setEnabled:(BOOL)value forFeature:(_WKExperimentalFeature *)feature;
++ (WK_ARRAY(_WKExperimentalFeature *) *)_experimentalFeatures WK_AVAILABLE(WK_MAC_TBA, WK_IOS_TBA);
+- (BOOL)_isEnabledForFeature:(_WKExperimentalFeature *)feature WK_AVAILABLE(WK_MAC_TBA, WK_IOS_TBA);
+- (void)_setEnabled:(BOOL)value forFeature:(_WKExperimentalFeature *)feature WK_AVAILABLE(WK_MAC_TBA, WK_IOS_TBA);
 
 @end
 
