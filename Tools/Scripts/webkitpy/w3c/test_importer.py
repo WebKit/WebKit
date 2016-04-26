5f319040db4f8bc513694fb9f69c947755662859
Ryosuke Niwa <rniwa@webkit.org>
2016-04-26 22:16:00 +0000

    Fix text in log files autogenerated by import-w3c-tests https://bugs.webkit.org/show_bug.cgi?id=157021
    Unreviewed. Fixing the second typo.
    
    * Scripts/webkitpy/w3c/test_importer.py:
    (TestImporter.write_import_log):
    
    
    Canonical link: https://commits.webkit.org/175164@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@200109 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/w3c/test_importer.py
+++ Tools/Scripts/webkitpy/w3c/test_importer.py
@@ -491,7 +491,7 @@ class TestImporter(object):
         import_log.append('Instead, create a pull request on the W3C CSS or WPT github:\n')
         import_log.append('\thttps://github.com/w3c/csswg-test\n')
         import_log.append('\thttps://github.com/w3c/web-platform-tests\n\n')
-        import_log.append('Then run the Tools/Scripts/import-w3c-tests in Webkit to reimport\n\n')
+        import_log.append('Then run the Tools/Scripts/import-w3c-tests in WebKit to reimport\n\n')
         import_log.append('Do NOT modify or remove this file.\n\n')
         import_log.append('------------------------------------------------------------------------\n')
         import_log.append('Properties requiring vendor prefixes:\n')
