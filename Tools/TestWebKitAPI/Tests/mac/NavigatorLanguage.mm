6aae81136da1463d68601e54e61aa27b40687421
Filip Pizlo <fpizlo@apple.com>
2016-04-26 20:01:13 +0000

    WebCore on Mac ignores the user's preferred region (country) while getting the language https://bugs.webkit.org/show_bug.cgi?id=156993
    Reviewed by Geoffrey Garen.
    
    Source/WebCore:
    
    This is tested by the NavigatorLanguage API test.
    
    WebCore was previously getting the list of preferred languages, and for each one, deducing
    the default region. That's wrong, since for example it doesn't respect the user's choice (in
    System Preferences) to display dates/calenders/etc according to a different region (like how
    I have my machine set to en-pl right now).
    
    It might be possible for the country code we get via kCFLocaleCountryCode to be something
    that our ICU doesn't handle. To defend against this, we search for the resulting country
    code in ICU's ISO countries list. If it doesn't appear in that list, we fall back on old
    behavior.
    
    * platform/mac/Language.mm:
    (WebCore::httpStyleLanguageCode):
    (WebCore::isValidICUCountryCode):
    (WebCore::platformUserPreferredLanguages):
    
    Tools:
    
    Change the test expectations for this test. If the variant is not explicitly specified in
    the AppleLanguage, then we use the locale's country code, which this test assumes is US.
    
    * TestWebKitAPI/Tests/mac/NavigatorLanguage.mm:
    (TestWebKitAPI::languageForSystemLanguage):
    (TestWebKitAPI::TEST):
    
    
    
    Canonical link: https://commits.webkit.org/175160@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@200105 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/mac/NavigatorLanguage.mm
+++ Tools/TestWebKitAPI/Tests/mac/NavigatorLanguage.mm
@@ -68,17 +68,21 @@ static NSString *languageForSystemLanguage(WebView* webView, NSString *systemLan
     return [webView stringByEvaluatingJavaScriptFromString:@"navigator.language"];
 }
 
-// These tests document current behavior. Some of the current results may not be right.
+// These tests document current behavior. Some of the current results may not be right. Note that
+// this oddly assumes that the user has set their language to something possibly-foreign but still
+// left their region as US. Hence the "-us" variants.
+// FIXME: These tests should also set the region to see how WebKit will handle that.
+// https://bugs.webkit.org/show_bug.cgi?id=157039
 NSArray *tests = @[
-    @[@"ru", @"ru-ru"], // This does not match other browsers or CFNetwork's Accept-Language, which all use "ru".
+    @[@"ru", @"ru-us"], // This does not match other browsers or CFNetwork's Accept-Language, which all use "ru".
     @[@"en", @"en-us"],
     @[@"en-GB", @"en-gb"],
     @[@"en-US", @"en-us"],
-    @[@"ja", @"ja-jp"],
-    @[@"hi", @"hi-in"],
+    @[@"ja", @"ja-us"],
+    @[@"hi", @"hi-us"],
     @[@"zh-TW", @"zh-tw"], // This should not map to the generic zh-hant, see rdar://problem/21395180.
     @[@"zh-HK", @"zh-tw"],
-    @[@"es", @"es-es"],
+    @[@"es", @"es-us"],
     @[@"es-MX", @"es-xl"],
     @[@"es-ES", @"es-es"],
     @[@"es-419", @"es-xl"],
@@ -86,7 +90,7 @@ NSArray *tests = @[
     @[@"zh-Hant", @"zh-tw"],
     @[@"pt-BR", @"pt-br"],
     @[@"pt-PT", @"pt-pt"],
-    @[@"fr", @"fr-fr"],
+    @[@"fr", @"fr-us"],
     @[@"fr-CA", @"fr-ca"],
 ];
 
