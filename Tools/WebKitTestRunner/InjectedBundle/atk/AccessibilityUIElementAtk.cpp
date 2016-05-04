571317523b961a18574b27813c57cad56087e536
Joanmarie Diggs <jdiggs@igalia.com>
2016-05-04 01:05:05 +0000

    [ATK] accessibility/document-attributes.html is failing https://bugs.webkit.org/show_bug.cgi?id=125348
    Reviewed by Chris Fleizach.
    
    Tools:
    
    The test was failing because the WebArea is now being exposed with the role of
    ATK_ROLE_DOCUMENT_WEB; not ATK_ROLE_DOCUMENT_FRAME. Unfortunately, when that
    change was made, documentEncoding() and documentURI() were not updated accordingly.
    
    * WebKitTestRunner/InjectedBundle/atk/AccessibilityUIElementAtk.cpp:
    (WTR::AccessibilityUIElement::documentEncoding):
    (WTR::AccessibilityUIElement::documentURI):
    
    LayoutTests:
    
    * platform/efl/TestExpectations: Unskipped failing test.
    * platform/gtk/TestExpectations: Unskipped failing test.
    
    
    Canonical link: https://commits.webkit.org/175430@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@200397 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/WebKitTestRunner/InjectedBundle/atk/AccessibilityUIElementAtk.cpp
+++ Tools/WebKitTestRunner/InjectedBundle/atk/AccessibilityUIElementAtk.cpp
@@ -1747,7 +1747,7 @@ JSRetainPtr<JSStringRef> AccessibilityUIElement::documentEncoding()
         return JSStringCreateWithCharacters(0, 0);
 
     AtkRole role = atk_object_get_role(ATK_OBJECT(m_element.get()));
-    if (role != ATK_ROLE_DOCUMENT_FRAME)
+    if (role != ATK_ROLE_DOCUMENT_WEB)
         return JSStringCreateWithCharacters(0, 0);
 
     return JSStringCreateWithUTF8CString(atk_document_get_attribute_value(ATK_DOCUMENT(m_element.get()), "Encoding"));
@@ -1759,7 +1759,7 @@ JSRetainPtr<JSStringRef> AccessibilityUIElement::documentURI()
         return JSStringCreateWithCharacters(0, 0);
 
     AtkRole role = atk_object_get_role(ATK_OBJECT(m_element.get()));
-    if (role != ATK_ROLE_DOCUMENT_FRAME)
+    if (role != ATK_ROLE_DOCUMENT_WEB)
         return JSStringCreateWithCharacters(0, 0);
 
     return JSStringCreateWithUTF8CString(atk_document_get_attribute_value(ATK_DOCUMENT(m_element.get()), "URI"));
