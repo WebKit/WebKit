6c53df80a2d5975d358f7a8eb581e726b81a03fb
Ryosuke Niwa <rniwa@webkit.org>
2016-05-05 21:16:36 +0000

    iOS debug build fix after r200464.
    * dom/EventContext.cpp:
    (WebCore::TouchEventContext::checkReachability):
    
    
    Canonical link: https://commits.webkit.org/175508@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@200477 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/EventContext.cpp
+++ Source/WebCore/dom/EventContext.cpp
@@ -128,7 +128,7 @@ void TouchEventContext::checkReachability(TouchList* touchList) const
 {
     size_t length = touchList->length();
     for (size_t i = 0; i < length; ++i)
-        ASSERT(isReachable(touchList->item(i)->target()->toNode()));
+        ASSERT(!isUnreachableNode(touchList->item(i)->target()->toNode()));
 }
 #endif
 
