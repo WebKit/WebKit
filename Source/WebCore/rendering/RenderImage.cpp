d7c63ed99bbe8e8ea2d2c39508a553c438d19584
Alan Bujtas <zalan@apple.com>
2016-05-13 13:35:08 +0000

    Merge r200486 - Do not attempt to compute min/max width. https://bugs.webkit.org/show_bug.cgi?id=157320
    Reviewed by David Hyatt.
    
    Replaced elements with no intrinsic size (only with ratio) should not call the containing
    block to compute the min/max width when the containing block's min/max width
    depends on the children's intrinsic size. It could lead to infinite recursion.
    
    Source/WebCore:
    
    Test: fast/replaced/before-content-intrinsic-crash.html
    
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::computeReplacedLogicalWidthRespectingMinMaxWidth): Unrelated code change.
    * rendering/RenderImage.cpp: Unrelated code change.
    (WebCore::RenderImage::RenderImage): Deleted.
    * rendering/RenderImage.h:
    * rendering/RenderReplaced.cpp:
    (WebCore::RenderReplaced::computeReplacedLogicalWidth):
    
    LayoutTests:
    
    * fast/replaced/before-content-intrinsic-crash-expected.txt: Added.
    * fast/replaced/before-content-intrinsic-crash.html: Added.

--- Source/WebCore/rendering/RenderImage.cpp
+++ Source/WebCore/rendering/RenderImage.cpp
@@ -121,15 +121,10 @@ using namespace HTMLNames;
 RenderImage::RenderImage(Element& element, Ref<RenderStyle>&& style, StyleImage* styleImage, const float imageDevicePixelRatio)
     : RenderReplaced(element, WTFMove(style), IntSize())
     , m_imageResource(styleImage ? std::make_unique<RenderImageResourceStyleImage>(*styleImage) : std::make_unique<RenderImageResource>())
-    , m_needsToSetSizeForAltText(false)
-    , m_didIncrementVisuallyNonEmptyPixelCount(false)
-    , m_isGeneratedContent(false)
-    , m_hasShadowControls(false)
     , m_imageDevicePixelRatio(imageDevicePixelRatio)
 {
     updateAltText();
     imageResource().initialize(this);
-
     if (is<HTMLImageElement>(element))
         m_hasShadowControls = downcast<HTMLImageElement>(element).hasShadowControls();
 }
@@ -137,11 +132,6 @@ RenderImage::RenderImage(Element& element, Ref<RenderStyle>&& style, StyleImage*
 RenderImage::RenderImage(Document& document, Ref<RenderStyle>&& style, StyleImage* styleImage)
     : RenderReplaced(document, WTFMove(style), IntSize())
     , m_imageResource(styleImage ? std::make_unique<RenderImageResourceStyleImage>(*styleImage) : std::make_unique<RenderImageResource>())
-    , m_needsToSetSizeForAltText(false)
-    , m_didIncrementVisuallyNonEmptyPixelCount(false)
-    , m_isGeneratedContent(false)
-    , m_hasShadowControls(false)
-    , m_imageDevicePixelRatio(1.0f)
 {
     imageResource().initialize(this);
 }
