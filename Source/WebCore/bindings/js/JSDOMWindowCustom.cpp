42de70ec279bba13338ed0e666843978fc0fa16e
Babak Shafiei <bshafiei@apple.com>
2016-05-17 08:47:11 +0000

    Merge r200788. rdar://problem/26143008
    Canonical link: https://commits.webkit.org/175622.27@safari-602.1.32-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-602.1.32-branch@201008 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/js/JSDOMWindowCustom.cpp
+++ Source/WebCore/bindings/js/JSDOMWindowCustom.cpp
@@ -39,11 +39,6 @@
 #include "ScheduledAction.h"
 #include "Settings.h"
 
-#if ENABLE(IOS_TOUCH_EVENTS)
-#include "JSTouchConstructorIOS.h"
-#include "JSTouchListConstructorIOS.h"
-#endif
-
 #if ENABLE(WEB_AUDIO)
 #include "JSAudioContext.h"
 #endif
@@ -437,18 +432,6 @@ JSValue JSDOMWindow::image(ExecState& state) const
 {
     return createImageConstructor(state.vm(), *this);
 }
-
-#if ENABLE(IOS_TOUCH_EVENTS)
-JSValue JSDOMWindow::touch(ExecState& state) const
-{
-    return getDOMConstructor<JSTouchConstructor>(state.vm(), *this);
-}
-
-JSValue JSDOMWindow::touchList(ExecState& state) const
-{
-    return getDOMConstructor<JSTouchListConstructor>(state.vm(), *this);
-}
-#endif
 
 // Custom functions
 
