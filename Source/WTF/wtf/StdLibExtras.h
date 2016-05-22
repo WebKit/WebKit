40307586d38922248e1eaa2cf04b3a8dbd7077b2
Dan Bernstein <mitz@apple.com>
2016-05-22 22:19:33 +0000

    Additional fixes for non-C++14 Apple projects that use WTF.
    * wtf/StdLibExtras.h:
    (std::make_unique):
    (std::move):
    
    
    
    Canonical link: https://commits.webkit.org/176093@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@201258 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/StdLibExtras.h
+++ Source/WTF/wtf/StdLibExtras.h
@@ -320,6 +320,35 @@ inline void* operator new(size_t, NotNullTag, void* location)
 
 // This adds various C++14 features for versions of the STL that may not yet have them.
 namespace std {
+#if __cplusplus < 201400L
+template<class T> struct _Unique_if {
+    typedef unique_ptr<T> _Single_object;
+};
+
+template<class T> struct _Unique_if<T[]> {
+    typedef unique_ptr<T[]> _Unknown_bound;
+};
+
+template<class T, size_t N> struct _Unique_if<T[N]> {
+    typedef void _Known_bound;
+};
+
+template<class T, class... Args> inline typename _Unique_if<T>::_Single_object
+make_unique(Args&&... args)
+{
+    return unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+template<class T> inline typename _Unique_if<T>::_Unknown_bound
+make_unique(size_t n)
+{
+    typedef typename remove_extent<T>::type U;
+    return unique_ptr<T>(new U[n]());
+}
+
+template<class T, class... Args> typename _Unique_if<T>::_Known_bound
+make_unique(Args&&...) = delete;
+#endif
 
 template<WTF::CheckMoveParameterTag, typename T>
 ALWAYS_INLINE constexpr typename remove_reference<T>::type&& move(T&& value)
