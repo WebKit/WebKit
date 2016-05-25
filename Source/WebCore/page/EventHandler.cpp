1ec20569ed7ee8f72a2db68c506539a03d8984b2
Zalan Bujtas <zalan@apple.com>
2016-05-25 16:01:12 +0000

    Addressing post-review comments on r200971.
    Reviewed by Darin Adler.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::hitTestResultAtPoint):
    
    Canonical link: https://commits.webkit.org/176188@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@201384 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/EventHandler.cpp
+++ Source/WebCore/page/EventHandler.cpp
@@ -1135,14 +1135,14 @@ HitTestResult EventHandler::hitTestResultAtPoint(const LayoutPoint& point, HitTe
 
     unsigned nonNegativePaddingWidth = std::max<LayoutUnit>(0, padding.width()).toUnsigned();
     unsigned nonNegativePaddingHeight = std::max<LayoutUnit>(0, padding.height()).toUnsigned();
+    // We should always start hit testing a clean tree.
+    if (m_frame.document())
+        m_frame.document()->updateLayoutIgnorePendingStylesheets();
     HitTestResult result(point, nonNegativePaddingHeight, nonNegativePaddingWidth, nonNegativePaddingHeight, nonNegativePaddingWidth);
-
     RenderView* renderView = m_frame.contentRenderer();
     if (!renderView)
         return result;
-    
-    // We should always start hittesting a clean tree.
-    renderView->document().updateLayoutIgnorePendingStylesheets();
+
     // hitTestResultAtPoint is specifically used to hitTest into all frames, thus it always allows child frame content.
     HitTestRequest request(hitType | HitTestRequest::AllowChildFrameContent);
     renderView->hitTest(request, result);
