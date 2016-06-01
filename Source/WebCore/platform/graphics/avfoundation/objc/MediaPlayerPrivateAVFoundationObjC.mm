1de32f57e65a615cf656b0b1e00a7cb083b5ad7f
Jer Noble <jer.noble@apple.com>
2016-06-01 20:55:30 +0000

    setVideoFullscreenGravity() has no effect on the fullscreen video layer https://bugs.webkit.org/show_bug.cgi?id=158267
    Reviewed by Eric Carlson.
    
    When we moved to a 2-AVPlayerLayer solution, we didn't update setVideoFullscreenGravity()
    to change the correct layer's videoGravity property.
    
    * platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm:
    (WebCore::MediaPlayerPrivateAVFoundationObjC::setVideoFullscreenGravity):
    
    Canonical link: https://commits.webkit.org/176360@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@201567 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm
+++ Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm
@@ -1218,7 +1218,9 @@ void MediaPlayerPrivateAVFoundationObjC::setVideoFullscreenFrame(FloatRect frame
 void MediaPlayerPrivateAVFoundationObjC::setVideoFullscreenGravity(MediaPlayer::VideoGravity gravity)
 {
     m_videoFullscreenGravity = gravity;
-    if (!m_videoLayer)
+
+    auto activeLayer = m_secondaryVideoLayer.get() ?: m_videoLayer.get();
+    if (!activeLayer)
         return;
 
     NSString *videoGravity = AVLayerVideoGravityResizeAspect;
@@ -1231,10 +1233,10 @@ void MediaPlayerPrivateAVFoundationObjC::setVideoFullscreenGravity(MediaPlayer::
     else
         ASSERT_NOT_REACHED();
     
-    if ([m_videoLayer videoGravity] == videoGravity)
+    if ([activeLayer videoGravity] == videoGravity)
         return;
 
-    [m_videoLayer setVideoGravity:videoGravity];
+    [activeLayer setVideoGravity:videoGravity];
     syncTextTrackBounds();
 }
 
