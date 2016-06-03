WebKit/WebKit

commit 267c46898f3d95f5d098b242a87db5f12ae33fa5

Overhaul cross-thread use of ResourceRequest, ResourceResponse, and ResourceError. https://bugs.webkit.org/show_bug.cgi?id=158293
Reviewed by Darin Adler.

Source/WebCore:

No new tests (Refactor, no behavior change).

* loader/WorkerThreadableLoader.cpp:
(WebCore::WorkerThreadableLoader::MainThreadBridge::MainThreadBridge):
(WebCore::WorkerThreadableLoader::MainThreadBridge::didReceiveResponse):
(WebCore::WorkerThreadableLoader::MainThreadBridge::didFail):
(WebCore::WorkerThreadableLoader::MainThreadBridge::didFailAccessControlCheck):

* loader/cache/MemoryCache.cpp:
(WebCore::MemoryCache::removeRequestFromSessionCaches):

* platform/network/ResourceErrorBase.cpp:
(WebCore::ResourceErrorBase::isolatedCopy):
(WebCore::ResourceErrorBase::copy): Deleted.
* platform/network/ResourceErrorBase.h:

* platform/network/ResourceRequestBase.cpp:
(WebCore::ResourceRequestBase::isolatedCopy):
(WebCore::ResourceRequestBase::setAsIsolatedCopy):
(WebCore::ResourceRequestBase::adopt): Deleted.

Made copyData private, and made "adopt" be "isolatedCopy".
Rest of the changes are all re-indenting the file.
* platform/network/ResourceRequestBase.h:
(WebCore::ResourceRequestBase::responseContentDispositionEncodingFallbackArray):
(WebCore::ResourceRequestBase::reportUploadProgress):
(WebCore::ResourceRequestBase::setReportUploadProgress):
(WebCore::ResourceRequestBase::reportLoadTiming):
(WebCore::ResourceRequestBase::setReportLoadTiming):
(WebCore::ResourceRequestBase::reportRawHeaders):
(WebCore::ResourceRequestBase::setReportRawHeaders):
(WebCore::ResourceRequestBase::hiddenFromInspector):
(WebCore::ResourceRequestBase::setHiddenFromInspector):
(WebCore::ResourceRequestBase::requester):
(WebCore::ResourceRequestBase::setRequester):
(WebCore::ResourceRequestBase::encodingRequiresPlatformData):
(WebCore::ResourceRequestBase::ResourceRequestBase):
(WebCore::ResourceRequestBase::platformCompare):
(WebCore::operator==):
(WebCore::operator!=):

* platform/network/ResourceResponseBase.cpp:

Source/WebCore/loader/WorkerThreadableLoader.cpp
@@ -96,17 +96,16 @@ WorkerThreadableLoader::MainThreadBridge::MainThreadBridge(ThreadableLoaderClien
     auto contentSecurityPolicyCopy = std::make_unique<ContentSecurityPolicy>(*securityOrigin);
     contentSecurityPolicyCopy->copyStateFrom(contentSecurityPolicy);
 
-    m_loaderProxy.postTaskToLoader([this, requestData = request.copyData(), options = options.isolatedCopy(), contentSecurityPolicyCopy = WTFMove(contentSecurityPolicyCopy), outgoingReferrer = outgoingReferrer.isolatedCopy()](ScriptExecutionContext& context) mutable {
+    m_loaderProxy.postTaskToLoader([this, request = request.isolatedCopy(), options = options.isolatedCopy(), contentSecurityPolicyCopy = WTFMove(contentSecurityPolicyCopy), outgoingReferrer = outgoingReferrer.isolatedCopy()](ScriptExecutionContext& context) mutable {
         ASSERT(isMainThread());
         Document& document = downcast<Document>(context);
 
-        auto request = ResourceRequest::adopt(WTFMove(requestData));
-        request->setHTTPReferrer(outgoingReferrer);
+        request.setHTTPReferrer(outgoingReferrer);
 
         // FIXME: If the a site requests a local resource, then this will return a non-zero value but the sync path
         // will return a 0 value. Either this should return 0 or the other code path should do a callback with
         // a failure.
-        m_mainThreadLoader = DocumentThreadableLoader::create(document, *this, *request, *options, WTFMove(contentSecurityPolicyCopy));
+        m_mainThreadLoader = DocumentThreadableLoader::create(document, *this, request, *options, WTFMove(contentSecurityPolicyCopy));
         ASSERT(m_mainThreadLoader || m_loadingFinished);
     });
 }
@@ -163,10 +162,9 @@ void WorkerThreadableLoader::MainThreadBridge::didSendData(unsigned long long by
 void WorkerThreadableLoader::MainThreadBridge::didReceiveResponse(unsigned long identifier, const ResourceResponse& response)
 {
     Ref<ThreadableLoaderClientWrapper> protectedWorkerClientWrapper = *m_workerClientWrapper;
-    m_loaderProxy.postTaskForModeToWorkerGlobalScope([protectedWorkerClientWrapper = WTFMove(protectedWorkerClientWrapper), identifier, responseData = response.copyData()] (ScriptExecutionContext& context) mutable {
+    m_loaderProxy.postTaskForModeToWorkerGlobalScope([protectedWorkerClientWrapper = WTFMove(protectedWorkerClientWrapper), identifier, response = response.isolatedCopy()] (ScriptExecutionContext& context) mutable {
         ASSERT_UNUSED(context, context.isWorkerGlobalScope());
-        auto response(ResourceResponse::adopt(WTFMove(responseData)));
-        protectedWorkerClientWrapper->didReceiveResponse(identifier, *response);
+        protectedWorkerClientWrapper->didReceiveResponse(identifier, response);
     }, m_taskMode);
 }
 
@@ -194,20 +192,18 @@ void WorkerThreadableLoader::MainThreadBridge::didFinishLoading(unsigned long id
 void WorkerThreadableLoader::MainThreadBridge::didFail(const ResourceError& error)
 {
     m_loadingFinished = true;
-    Ref<ThreadableLoaderClientWrapper> protectedWorkerClientWrapper = *m_workerClientWrapper;
-    m_loaderProxy.postTaskForModeToWorkerGlobalScope([protectedWorkerClientWrapper = WTFMove(protectedWorkerClientWrapper), error = error.copy()] (ScriptExecutionContext& context) mutable {
+    m_loaderProxy.postTaskForModeToWorkerGlobalScope([workerClientWrapper = Ref<ThreadableLoaderClientWrapper>(*m_workerClientWrapper), error = error.isolatedCopy()] (ScriptExecutionContext& context) mutable {
         ASSERT_UNUSED(context, context.isWorkerGlobalScope());
-        protectedWorkerClientWrapper->didFail(error);
+        workerClientWrapper->didFail(error);
     }, m_taskMode);
 }
 
 void WorkerThreadableLoader::MainThreadBridge::didFailAccessControlCheck(const ResourceError& error)
 {
     m_loadingFinished = true;
-    Ref<ThreadableLoaderClientWrapper> protectedWorkerClientWrapper = *m_workerClientWrapper;
-    m_loaderProxy.postTaskForModeToWorkerGlobalScope([protectedWorkerClientWrapper = WTFMove(protectedWorkerClientWrapper), error = error.copy()] (ScriptExecutionContext& context) mutable {
+    m_loaderProxy.postTaskForModeToWorkerGlobalScope([workerClientWrapper = Ref<ThreadableLoaderClientWrapper>(*m_workerClientWrapper), error = error.isolatedCopy()] (ScriptExecutionContext& context) mutable {
         ASSERT_UNUSED(context, context.isWorkerGlobalScope());
-        protectedWorkerClientWrapper->didFailAccessControlCheck(error);
+        workerClientWrapper->didFailAccessControlCheck(error);
     }, m_taskMode);
 }
 

Source/WebCore/platform/network/ResourceResponseBase.cpp
@@ -63,23 +63,26 @@ ResourceResponseBase::ResourceResponseBase(const URL& url, const String& mimeTyp
 {
 }
 
-std::unique_ptr<ResourceResponse> ResourceResponseBase::adopt(std::unique_ptr<CrossThreadResourceResponseData> data)
+ResourceResponse ResourceResponseBase::isolatedCopy() const
 {
-    auto response = std::make_unique<ResourceResponse>();
-    response->setURL(data->m_url);
-    response->setMimeType(data->m_mimeType);
-    response->setExpectedContentLength(data->m_expectedContentLength);
-    response->setTextEncodingName(data->m_textEncodingName);
+    ResourceResponse response;
+    auto data = copyData();
 
-    response->setHTTPStatusCode(data->m_httpStatusCode);
-    response->setHTTPStatusText(data->m_httpStatusText);
-    response->setHTTPVersion(data->m_httpVersion);
+    response.setURL(data->m_url);
+    response.setMimeType(data->m_mimeType);
+    response.setExpectedContentLength(data->m_expectedContentLength);
+    response.setTextEncodingName(data->m_textEncodingName);
 
-    response->lazyInit(AllFields);
-    response->m_httpHeaderFields.adopt(WTFMove(data->m_httpHeaders));
-    response->m_resourceLoadTiming = data->m_resourceLoadTiming;
-    response->m_type = data->m_type;
-    response->doPlatformAdopt(WTFMove(data));
+    response.setHTTPStatusCode(data->m_httpStatusCode);
+    response.setHTTPStatusText(data->m_httpStatusText);
+    response.setHTTPVersion(data->m_httpVersion);
+
+    response.lazyInit(AllFields);
+    response.m_httpHeaderFields.adopt(WTFMove(data->m_httpHeaders));
+    response.m_resourceLoadTiming = data->m_resourceLoadTiming;
+    response.m_type = data->m_type;
+    response.doPlatformAdopt(WTFMove(data));
+
     return response;
 }
 

Source/WebCore/platform/network/ResourceResponseBase.h
@@ -24,8 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#ifndef ResourceResponseBase_h
-#define ResourceResponseBase_h
+#pragma once
 
 #include "CacheValidation.h"
 #include "CertificateInfo.h"
@@ -47,10 +46,7 @@ struct CrossThreadResourceResponseData;
 class ResourceResponseBase {
     WTF_MAKE_FAST_ALLOCATED;
 public:
-    static std::unique_ptr<ResourceResponse> adopt(std::unique_ptr<CrossThreadResourceResponseData>);
-
-    // Gets a copy of the data suitable for passing to another thread.
-    std::unique_ptr<CrossThreadResourceResponseData> copyData() const;
+    ResourceResponse isolatedCopy() const;
 
     bool isNull() const { return m_isNull; }
     WEBCORE_EXPORT bool isHTTP() const;
@@ -162,6 +158,9 @@ private:
     const ResourceResponse& asResourceResponse() const;
     void parseCacheControlDirectives() const;
     void updateHeaderParsedState(HTTPHeaderName);
+
+    // Gets a copy of the data suitable for passing to another thread.
+    std::unique_ptr<CrossThreadResourceResponseData> copyData() const;
 
 protected:
     bool m_isNull;
@@ -293,5 +292,3 @@ public:
 };
 
 } // namespace WebCore
-
-#endif // ResourceResponseBase_h

Source/WebCore/loader/cache/MemoryCache.cpp
@@ -655,10 +655,8 @@ void MemoryCache::adjustSize(bool live, int delta)
 void MemoryCache::removeRequestFromSessionCaches(ScriptExecutionContext& context, const ResourceRequest& request)
 {
     if (is<WorkerGlobalScope>(context)) {
-        CrossThreadResourceRequestData* requestData = request.copyData().release();
-        downcast<WorkerGlobalScope>(context).thread().workerLoaderProxy().postTaskToLoader([requestData] (ScriptExecutionContext& context) {
-            auto request(ResourceRequest::adopt(std::unique_ptr<CrossThreadResourceRequestData>(requestData)));
-            MemoryCache::removeRequestFromSessionCaches(context, *request);
+        downcast<WorkerGlobalScope>(context).thread().workerLoaderProxy().postTaskToLoader([request = request.isolatedCopy()] (ScriptExecutionContext& context) {
+            MemoryCache::removeRequestFromSessionCaches(context, request);
         });
         return;
     }

Source/WebCore/platform/network/ResourceErrorBase.cpp
@@ -31,7 +31,7 @@ namespace WebCore {
 
 const char* const errorDomainWebKitInternal = "WebKitInternal";
 
-ResourceError ResourceErrorBase::copy() const
+ResourceError ResourceErrorBase::isolatedCopy() const
 {
     lazyInit();
 

Source/WebCore/platform/network/ResourceErrorBase.h
@@ -23,8 +23,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#ifndef ResourceErrorBase_h
-#define ResourceErrorBase_h
+#pragma once
 
 #include "URL.h"
 #include <wtf/text/WTFString.h>
@@ -37,8 +36,7 @@ extern const char* const errorDomainWebKitInternal; // Used for errors that won'
 
 class ResourceErrorBase {
 public:
-    // Makes a deep copy. Useful for when you need to use a ResourceError on another thread.
-    ResourceError copy() const;
+    ResourceError isolatedCopy() const;
 
     bool isNull() const { return m_isNull; }
 
@@ -99,5 +97,3 @@ inline bool operator==(const ResourceError& a, const ResourceError& b) { return 
 inline bool operator!=(const ResourceError& a, const ResourceError& b) { return !(a == b); }
 
 } // namespace WebCore
-
-#endif // ResourceErrorBase_h

Source/WebCore/platform/network/ResourceRequestBase.cpp
@@ -49,19 +49,27 @@ inline const ResourceRequest& ResourceRequestBase::asResourceRequest() const
     return *static_cast<const ResourceRequest*>(this);
 }
 
-std::unique_ptr<ResourceRequest> ResourceRequestBase::adopt(std::unique_ptr<CrossThreadResourceRequestData> data)
+ResourceRequest ResourceRequestBase::isolatedCopy() const
 {
-    auto request = std::make_unique<ResourceRequest>();
-    request->setURL(data->url);
-    request->setCachePolicy(data->cachePolicy);
-    request->setTimeoutInterval(data->timeoutInterval);
-    request->setFirstPartyForCookies(data->firstPartyForCookies);
-    request->setHTTPMethod(data->httpMethod);
-    request->setPriority(data->priority);
-    request->setRequester(data->requester);
+    ResourceRequest request;
+    request.setAsIsolatedCopy(*this);
+    return request;
+}
 
-    request->updateResourceRequest();
-    request->m_httpHeaderFields.adopt(WTFMove(data->httpHeaders));
+void ResourceRequestBase::setAsIsolatedCopy(const ResourceRequestBase& other)
+{
+    auto data = other.copyData();
+
+    setURL(data->url);
+    setCachePolicy(data->cachePolicy);
+    setTimeoutInterval(data->timeoutInterval);
+    setFirstPartyForCookies(data->firstPartyForCookies);
+    setHTTPMethod(data->httpMethod);
+    setPriority(data->priority);
+    setRequester(data->requester);
+
+    updateResourceRequest();
+    m_httpHeaderFields.adopt(WTFMove(data->httpHeaders));
 
     size_t encodingCount = data->responseContentDispositionEncodingFallbackArray.size();
     if (encodingCount > 0) {
@@ -74,12 +82,12 @@ std::unique_ptr<ResourceRequest> ResourceRequestBase::adopt(std::unique_ptr<Cros
                 encoding3 = data->responseContentDispositionEncodingFallbackArray[2];
         }
         ASSERT(encodingCount <= 3);
-        request->setResponseContentDispositionEncodingFallbackArray(encoding1, encoding2, encoding3);
+        setResponseContentDispositionEncodingFallbackArray(encoding1, encoding2, encoding3);
     }
-    request->setHTTPBody(data->httpBody.copyRef());
-    request->setAllowCookies(data->allowCookies);
-    request->doPlatformAdopt(WTFMove(data));
-    return request;
+    setHTTPBody(data->httpBody.copyRef());
+    setAllowCookies(data->allowCookies);
+
+    const_cast<ResourceRequest&>(asResourceRequest()).doPlatformAdopt(WTFMove(data));
 }
 
 std::unique_ptr<CrossThreadResourceRequestData> ResourceRequestBase::copyData() const

commit 24d04488c92f36a18466010cc1f51f7c7a88b2ab

Fix AtomicString regression caused by r201603. https://bugs.webkit.org/show_bug.cgi?id=158338
Reviewed by Chris Dumez.

No new tests (Refactor, plus already caught by existing test configs).

* loader/WorkerThreadableLoader.cpp:
(WebCore::WorkerThreadableLoader::MainThreadBridge::didReceiveResponse):

* platform/network/ResourceResponseBase.cpp:
(WebCore::ResourceResponseBase::crossThreadData): Re-introduce the cross thread data concept,
  but more modern and clean this time.
(WebCore::ResourceResponseBase::fromCrossThreadData):
(WebCore::ResourceResponseBase::isolatedCopy): Deleted.
* platform/network/ResourceResponseBase.h:

Canonical link: https://commits.webkit.org/176423@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@201637 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/loader/WorkerThreadableLoader.cpp
@@ -162,9 +162,9 @@ void WorkerThreadableLoader::MainThreadBridge::didSendData(unsigned long long by
 void WorkerThreadableLoader::MainThreadBridge::didReceiveResponse(unsigned long identifier, const ResourceResponse& response)
 {
     Ref<ThreadableLoaderClientWrapper> protectedWorkerClientWrapper = *m_workerClientWrapper;
-    m_loaderProxy.postTaskForModeToWorkerGlobalScope([protectedWorkerClientWrapper = WTFMove(protectedWorkerClientWrapper), identifier, response = response.isolatedCopy()] (ScriptExecutionContext& context) mutable {
+    m_loaderProxy.postTaskForModeToWorkerGlobalScope([protectedWorkerClientWrapper = WTFMove(protectedWorkerClientWrapper), identifier, responseData = response.crossThreadData()] (ScriptExecutionContext& context) mutable {
         ASSERT_UNUSED(context, context.isWorkerGlobalScope());
-        protectedWorkerClientWrapper->didReceiveResponse(identifier, response);
+        protectedWorkerClientWrapper->didReceiveResponse(identifier, ResourceResponse::fromCrossThreadData(WTFMove(responseData)));
     }, m_taskMode);
 }
 

Source/WebCore/platform/network/ResourceResponseBase.cpp
@@ -63,23 +63,42 @@ ResourceResponseBase::ResourceResponseBase(const URL& url, const String& mimeTyp
 {
 }
 
-ResourceResponse ResourceResponseBase::isolatedCopy() const
+std::unique_ptr<ResourceResponseBase::CrossThreadData> ResourceResponseBase::crossThreadData() const
+{
+    auto data = std::make_unique<CrossThreadData>();
+
+    data->url = url().isolatedCopy();
+    data->mimeType = mimeType().isolatedCopy();
+    data->expectedContentLength = expectedContentLength();
+    data->textEncodingName = textEncodingName().isolatedCopy();
+
+    data->httpStatusCode = httpStatusCode();
+    data->httpStatusText = httpStatusText().isolatedCopy();
+    data->httpVersion = httpVersion().isolatedCopy();
+
+    data->httpHeaderFields = httpHeaderFields().isolatedCopy();
+    data->resourceLoadTiming = m_resourceLoadTiming.isolatedCopy();
+    data->type = m_type;
+
+    return data;
+}
+
+ResourceResponse ResourceResponseBase::fromCrossThreadData(std::unique_ptr<CrossThreadData>&& data)
 {
     ResourceResponse response;
 
-    response.setURL(url().isolatedCopy());
-    response.setMimeType(mimeType().isolatedCopy());
-    response.setExpectedContentLength(expectedContentLength());
-    response.setTextEncodingName(textEncodingName().isolatedCopy());
+    response.setURL(data->url);
+    response.setMimeType(data->mimeType);
+    response.setExpectedContentLength(data->expectedContentLength);
+    response.setTextEncodingName(data->textEncodingName);
 
-    response.setHTTPStatusCode(httpStatusCode());
-    response.setHTTPStatusText(httpStatusText().isolatedCopy());
-    response.setHTTPVersion(httpVersion().isolatedCopy());
+    response.setHTTPStatusCode(data->httpStatusCode);
+    response.setHTTPStatusText(data->httpStatusText);
+    response.setHTTPVersion(data->httpVersion);
 
-    response.lazyInit(AllFields);
-    response.m_httpHeaderFields = httpHeaderFields().isolatedCopy();
-    response.m_resourceLoadTiming = m_resourceLoadTiming.isolatedCopy();
-    response.m_type = m_type;
+    response.m_httpHeaderFields = WTFMove(data->httpHeaderFields);
+    response.m_resourceLoadTiming = data->resourceLoadTiming;
+    response.m_type = data->type;
 
     return response;
 }

Source/WebCore/platform/network/ResourceResponseBase.h
@@ -45,7 +45,23 @@ class ResourceResponse;
 class ResourceResponseBase {
     WTF_MAKE_FAST_ALLOCATED;
 public:
-    ResourceResponse isolatedCopy() const;
+    enum class Type;
+
+    struct CrossThreadData {
+        URL url;
+        String mimeType;
+        long long expectedContentLength;
+        String textEncodingName;
+        int httpStatusCode;
+        String httpStatusText;
+        String httpVersion;
+        HTTPHeaderMap httpHeaderFields;
+        ResourceLoadTiming resourceLoadTiming;
+        Type type;
+    };
+
+    std::unique_ptr<CrossThreadData> crossThreadData() const;
+    static ResourceResponse fromCrossThreadData(std::unique_ptr<CrossThreadData>&&);
 
     bool isNull() const { return m_isNull; }
     WEBCORE_EXPORT bool isHTTP() const;