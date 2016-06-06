c32d5b94f04d707dbb9e537643ecb0d5822f4d18
Aleksandr Skachkov <gskachkov@gmail.com>
2016-06-06 20:33:39 +0000

    Source/JavaScriptCore: [ESNext] Trailing commas in function parameters. https://bugs.webkit.org/show_bug.cgi?id=158020
    Reviewed by Keith Miller.
    
    ESNext allow to add trailing commas in function parameters and function arguments.
    Link to spec - https://jeffmo.github.io/es-trailing-function-commas
    Example of using - (function (a, b,) { return a + b; })(1,2,);
    
    * parser/Parser.cpp:
    (JSC::Parser<LexerType>::parseFormalParameters):
    (JSC::Parser<LexerType>::parseArguments):
    * tests/stress/trailing-comma-in-function-paramters.js: Added.
    
    LayoutTests:
    [ESNext] Support trailing commas in function param lists
    https://bugs.webkit.org/show_bug.cgi?id=158020
    
    Reviewed by Keith Miller.
    
    * js/parser-syntax-check-expected.txt:
    * js/script-tests/parser-syntax-check.js:
    * sputnik/Conformance/13_Function_Definition/S13_A5.html:
    
    
    Canonical link: https://commits.webkit.org/176511@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@201725 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/parser/Parser.cpp
+++ Source/JavaScriptCore/parser/Parser.cpp
@@ -1746,6 +1746,9 @@ template <class TreeBuilder> bool Parser<LexerType>::parseFormalParameters(TreeB
         TreeDestructuringPattern parameter = 0;
         TreeExpression defaultValue = 0;
 
+        if (UNLIKELY(match(CLOSEPAREN)))
+            break;
+        
         if (match(DOTDOTDOT)) {
             next();
             failIfFalse(matchSpecIdentifier(), "Rest parameter '...' should be followed by a variable identifier");
@@ -3826,6 +3829,9 @@ template <class TreeBuilder> TreeArguments Parser<LexerType>::parseArguments(Tre
         JSTokenLocation argumentLocation(tokenLocation());
         next(TreeBuilder::DontBuildStrings);
 
+        if (UNLIKELY(match(CLOSEPAREN)))
+            break;
+        
         TreeExpression arg = parseArgument(context, argType);
         propagateError();
         semanticFailIfTrue(match(DOTDOTDOT), "The '...' operator should come before the target expression");
