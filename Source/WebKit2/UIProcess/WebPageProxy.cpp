30119cd70f51116448953fbd1a30dc4ef4d839c6
Chris Dumez <cdumez@apple.com>
2016-06-22 03:16:14 +0000

    Unreviewed, rolling out r202296.
    Broke basic browsing in Safari
    
    Reverted changeset:
    
    "WKWebView with no WKNavigationDelegate does not follow
    universal links"
    https://bugs.webkit.org/show_bug.cgi?id=158997
    http://trac.webkit.org/changeset/202296
    
    Canonical link: https://commits.webkit.org/177083@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@202316 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/WebPageProxy.cpp
+++ Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -569,9 +569,6 @@ void WebPageProxy::setNavigationClient(std::unique_ptr<API::NavigationClient> na
 
 void WebPageProxy::setLoaderClient(std::unique_ptr<API::LoaderClient> loaderClient)
 {
-    if (m_navigationClient && loaderClient)
-        m_navigationClient = nullptr;
-
     if (!loaderClient) {
         m_loaderClient = std::make_unique<API::LoaderClient>();
         return;
