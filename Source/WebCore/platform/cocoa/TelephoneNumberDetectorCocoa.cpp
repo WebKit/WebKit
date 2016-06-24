cbc79f42392716561d9b9b7f71537674434d4c8e
Enrica Casucci <enrica@apple.com>
2016-06-24 18:52:56 +0000

    Do not use iOS specific telephone detection on macOS. https://bugs.webkit.org/show_bug.cgi?id=159096 rdar://problem/25870571
    Reviewed by Anders Carlsson.
    
    Adding platform guard.
    
    * platform/cocoa/TelephoneNumberDetectorCocoa.cpp:
    (WebCore::TelephoneNumberDetector::phoneNumbersScanner):
    
    
    
    Canonical link: https://commits.webkit.org/177195@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@202436 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/cocoa/TelephoneNumberDetectorCocoa.cpp
+++ Source/WebCore/platform/cocoa/TelephoneNumberDetectorCocoa.cpp
@@ -47,6 +47,7 @@ namespace TelephoneNumberDetector {
 
 static DDDFAScannerRef phoneNumbersScanner()
 {
+#if PLATFORM(IOS)
     if (!DataDetectorsCoreLibrary())
         return nullptr;
 
@@ -56,6 +57,9 @@ static DDDFAScannerRef phoneNumbersScanner()
 
     static DDDFAScannerRef scanner = DDDFAScannerCreateFromCache(cache);
     return scanner;
+#else
+    return nullptr;
+#endif
 }
 
 bool isSupported()
