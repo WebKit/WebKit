887d75d2b510032da2e8e93ed922e547b5a740be
Jer Noble <jer.noble@apple.com>
2016-06-28 18:25:27 +0000

    Cross-domain video loads do not prompt for authorization. https://bugs.webkit.org/show_bug.cgi?id=159195 <rdar://problem/26234612>
    Reviewed by Brent Fulgham.
    
    Source/WebCore:
    
    Test: http/tests/media/video-auth.html (modified)
    
    We should prompt for authorization when a cross-origin <video> is embedded
    in a web page.
    
    * loader/MediaResourceLoader.cpp:
    (WebCore::MediaResourceLoader::requestResource):
    
    LayoutTests:
    
    Add a cross-origin authorization sub-test.
    
    * http/tests/media/video-auth.html:
    * http/tests/media/video-auth-expected.txt:
    
    Canonical link: https://commits.webkit.org/177326@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@202579 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/MediaResourceLoader.cpp
+++ Source/WebCore/loader/MediaResourceLoader.cpp
@@ -78,7 +78,7 @@ RefPtr<PlatformMediaResource> MediaResourceLoader::requestResource(const Resourc
 
     // FIXME: Skip Content Security Policy check if the element that inititated this request
     // is in a user-agent shadow tree. See <https://bugs.webkit.org/show_bug.cgi?id=155505>.
-    CachedResourceRequest cacheRequest(updatedRequest, ResourceLoaderOptions(SendCallbacks, DoNotSniffContent, bufferingPolicy, allowCredentials, DoNotAskClientForCrossOriginCredentials, ClientDidNotRequestCredentials, DoSecurityCheck, corsPolicy, DoNotIncludeCertificateInfo, ContentSecurityPolicyImposition::DoPolicyCheck, DefersLoadingPolicy::AllowDefersLoading, cachingPolicy));
+    CachedResourceRequest cacheRequest(updatedRequest, ResourceLoaderOptions(SendCallbacks, DoNotSniffContent, bufferingPolicy, allowCredentials, AskClientForAllCredentials, ClientDidNotRequestCredentials, DoSecurityCheck, corsPolicy, DoNotIncludeCertificateInfo, ContentSecurityPolicyImposition::DoPolicyCheck, DefersLoadingPolicy::AllowDefersLoading, cachingPolicy));
 
     if (!m_crossOriginMode.isNull())
         updateRequestForAccessControl(cacheRequest.mutableResourceRequest(), m_document->securityOrigin(), allowCredentials);
