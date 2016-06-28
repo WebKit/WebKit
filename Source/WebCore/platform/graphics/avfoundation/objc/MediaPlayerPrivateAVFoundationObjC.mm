41016e6a316c26171af3bb43ec8c9648b66941c2
Commit Queue <commit-queue@webkit.org>
2016-06-28 00:36:40 +0000

    Unreviewed, rolling out r202520. https://bugs.webkit.org/show_bug.cgi?id=159185
    This change broke the 32-bit El Capitan build (Requested by
    ryanhaddad on #webkit).
    
    Reverted changeset:
    
    "REGRESSION?(r202466): http/tests/security/canvas-remote-read-
    remote-video-redirect.html failing on Sierra"
    https://bugs.webkit.org/show_bug.cgi?id=159172
    http://trac.webkit.org/changeset/202520
    
    Patch by Commit Queue <commit-queue@webkit.org> on 2016-06-27
    
    Canonical link: https://commits.webkit.org/177278@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@202527 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm
+++ Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm
@@ -2241,18 +2241,7 @@ bool MediaPlayerPrivateAVFoundationObjC::hasSingleSecurityOrigin() const
     
     Ref<SecurityOrigin> resolvedOrigin(SecurityOrigin::create(resolvedURL()));
     Ref<SecurityOrigin> requestedOrigin(SecurityOrigin::createFromString(assetURL()));
-    if (!resolvedOrigin.get().isSameSchemeHostPort(&requestedOrigin.get()))
-        return false;
-
-#if PLATFORM(IOS) || __MAC_OS_X_VERSION_MIN_REQUIRED > 101100
-    AVAssetResourceLoader *resourceLoader = m_avAsset.get().resourceLoader;
-    if (Settings::isAVFoundationNSURLSessionEnabled() && [resourceLoader respondsToSelector:@selector(URLSession)]) {
-        WebCoreNSURLSession *session = (WebCoreNSURLSession *)resourceLoader.URLSession;
-        if ([session respondsToSelector:@selector(hasSingleSecurityOrigin)])
-            return session.hasSingleSecurityOrigin;
-    }
-#endif
-    return true;
+    return resolvedOrigin.get().isSameSchemeHostPort(&requestedOrigin.get());
 }
 
 bool MediaPlayerPrivateAVFoundationObjC::didPassCORSAccessCheck() const
