ad507bea7c14dfcaa19bf79453b81faca756b01e
David Kilzer <ddkilzer@webkit.org>
2016-06-29 16:50:21 +0000

    Crash when 'input' event handler for input[type=color] changes the input type <https://webkit.org/b/159262> <rdar://problem/27020404>
    Reviewed by Daniel Bates.
    
    Source/WebCore:
    
    Fix based on a Blink change (patch by <tkent@chromium.org>):
    <https://chromium.googlesource.com/chromium/src.git/+/a17cb3ecef49a078657524cdeaba33ad2083646c>
    
    Test: fast/forms/color/color-type-change-on-input-crash.html
    
    * html/ColorInputType.cpp:
    (WebCore::ColorInputType::didChooseColor): Add EventQueueScope
    before setValueFromRenderer() to fix the bug.
    * html/HTMLInputElement.h:
    (WebCore::HTMLInputElement::setValueFromRenderer): Add comment
    about how to use this method.
    
    LayoutTests:
    
    Test based on a Blink change (patch by <tkent@chromium.org>):
    <https://chromium.googlesource.com/chromium/src.git/+/a17cb3ecef49a078657524cdeaba33ad2083646c>
    
    * fast/forms/color/color-type-change-on-input-crash-expected.txt: Added.
    * fast/forms/color/color-type-change-on-input-crash.html: Added.
    
    Canonical link: https://commits.webkit.org/177373@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@202626 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/ColorInputType.cpp
+++ Source/WebCore/html/ColorInputType.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2010 Google Inc. All rights reserved.
- * Copyright (C) 2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2015-2016 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -46,6 +46,7 @@
 #include "MouseEvent.h"
 #include "RenderObject.h"
 #include "RenderView.h"
+#include "ScopedEventQueue.h"
 #include "ScriptController.h"
 #include "ShadowRoot.h"
 
@@ -174,6 +175,7 @@ void ColorInputType::didChooseColor(const Color& color)
 {
     if (element().isDisabledOrReadOnly() || color == valueAsColor())
         return;
+    EventQueueScope scope;
     element().setValueFromRenderer(color.serialized());
     updateColorSwatch();
     element().dispatchFormControlChangeEvent();
--- Source/WebCore/html/HTMLInputElement.h
+++ Source/WebCore/html/HTMLInputElement.h
@@ -2,7 +2,7 @@
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2000 Dirk Mueller (mueller@kde.org)
- * Copyright (C) 2004, 2005, 2006, 2007, 2010, 2016 Apple Inc. All rights reserved.
+ * Copyright (C) 2004-2016 Apple Inc. All rights reserved.
  * Copyright (C) 2012 Samsung Electronics. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
@@ -197,6 +197,9 @@ public:
 
     String valueWithDefault() const;
 
+    // This function dispatches 'input' event for non-textfield types. Callers
+    // need to handle any DOM structure changes by event handlers, or need to
+    // delay the 'input' event with EventQueueScope.
     void setValueFromRenderer(const String&);
 
     bool canHaveSelection() const;
