16fbd486e8463da2e47bca496d87d86a282aed5b
Alex Christensen <achristensen@apple.com>
2016-07-07 22:42:43 +0000

    Fix crash when sending failure message to closed WebProcess https://bugs.webkit.org/show_bug.cgi?id=159526 <rdar://problem/26767037>
    Reviewed by Brady Eidson.
    
    * NetworkProcess/NetworkResourceLoader.cpp:
    (WebKit::NetworkResourceLoader::didFailLoading):
    Check to see if there is a connection before using it to send a message.
    Sometimes it was null.
    I think this could be tested if we had real network loading from TestWebKitAPI
    by cancelling slow loads and closing WebProcesses around the same time, but no such infrastructure exists yet.
    
    
    
    Canonical link: https://commits.webkit.org/177664@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@202939 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/NetworkProcess/NetworkResourceLoader.cpp
+++ Source/WebKit2/NetworkProcess/NetworkResourceLoader.cpp
@@ -405,8 +405,8 @@ void NetworkResourceLoader::didFailLoading(const ResourceError& error)
     if (isSynchronous()) {
         m_synchronousLoadData->error = error;
         sendReplyToSynchronousRequest(*m_synchronousLoadData, nullptr);
-    } else
-        send(Messages::WebResourceLoader::DidFailResourceLoad(error));
+    } else if (auto* connection = messageSenderConnection())
+        connection->send(Messages::WebResourceLoader::DidFailResourceLoad(error), 0, 0);
 
     cleanup();
 }
