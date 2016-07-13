352d7b0abc879a164302863e989b7996fcfc19d4
Chris Dumez <cdumez@apple.com>
2016-07-13 23:54:23 +0000

    Unreviewed, rolling out r203199.
    Broke the build
    
    Reverted changeset:
    
    "Crashes with detached ArrayBuffers"
    https://bugs.webkit.org/show_bug.cgi?id=157088
    http://trac.webkit.org/changeset/203199
    
    Canonical link: https://commits.webkit.org/177891@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@203200 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/tests/stress/typedarray-functions-with-neutered.js
+++ Source/JavaScriptCore/tests/stress/typedarray-functions-with-neutered.js
@@ -77,7 +77,7 @@ for (var i = 0; i < 1000; i++)
 prototypeFunctions = [
     { func:proto.copyWithin, args:["prim", "prim", "prim"] },
     { func:proto.every, args:["func"] },
-    { func:proto.fill, args:["prim", "prim", "prim"] },
+    { func:proto.fill, args:["ins", "prim", "prim"] },
     { func:proto.filter, args:["func"] },
     { func:proto.find, args:["func"] },
     { func:proto.findIndex, args:["func"] },
@@ -95,14 +95,12 @@ prototypeFunctions = [
     { func:proto.subarray, args:["prim", "prim"] },
 ];
 
-function defaultForArg(arg, argNum)
+function defaultForArg(arg)
 {
     if (arg === "func")
-        return () => { return argNum; }
-    if (arg === "array")
-        return [1,2];
+        return () => { return 1; }
 
-    return argNum;
+    return 1;
 }
 
 function callWithArgs(func, array, args) {
@@ -129,40 +127,34 @@ function checkArgumentsForType(func, args, constructor) {
         if (arg === "na")
             continue;
 
-        let array = new constructor(10);
+        let len = 10;
         if (arg === "func") {
+            let array = new constructor(len);
             callArgs[argNum] = () => {
                 transferArrayBuffer(array.buffer);
                 return func === array.every ? 1 : 0;
             };
             callWithArgs(func, array, callArgs);
-        } else if (arg === "prim") {
+        }
+
+        if (arg === "prim") {
+            let array = new constructor(len)
             callArgs[argNum] = { [Symbol.toPrimitive]() {
-                transferArrayBuffer(array.buffer);
-                return argNum;
-            } };
-            callWithArgs(func, array, callArgs);
-        } else if (arg === "array") {
-            callArgs[argNum] = new Array(4);
-            callArgs[argNum].fill(2);
-            let desc = { get: () => {
                 transferArrayBuffer(array.buffer);
                 return 1;
             } };
-            Object.defineProperty(callArgs[argNum], 1, desc);
             callWithArgs(func, array, callArgs);
-        } else
-            throw new Error(arg);
+        }
     }
 }
 
 function checkArguments({func, args}) {
     for (constructor of typedArrays)
-        checkArgumentsForType(func, args, constructor);
+        checkArgumentsForType(func, args, constructor)
 }
 
-function test2() {
+function test() {
     prototypeFunctions.forEach(checkArguments);
 }
 
-test2();
+test();
