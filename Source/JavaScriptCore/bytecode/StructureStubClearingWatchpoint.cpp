c0a51620a5deda703ca687102573f10e56a49c2c
Mark Lam <mark.lam@apple.com>
2016-07-15 18:31:29 +0000

    Stack overflow error for deeply nested classes. https://bugs.webkit.org/show_bug.cgi?id=157086
    Reviewed by Geoffrey Garen.
    
    Changed the StructureStubClearingWatchpoint destructor to iteratively destruct
    its chain of next StructureStubClearingWatchpoints instead of recursively doing
    so.
    
    The added deep-StructureStubClearingWatchpoint-destructor-recursion.js test
    produces a crash before the fix is applied, but takes about 14 minutes to run.
    Hence, it is skipped.
    
    * bytecode/StructureStubClearingWatchpoint.cpp:
    (JSC::StructureStubClearingWatchpoint::~StructureStubClearingWatchpoint):
    * tests/stress/deep-StructureStubClearingWatchpoint-destructor-recursion.js: Added.
    
    
    
    Canonical link: https://commits.webkit.org/177976@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@203286 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/bytecode/StructureStubClearingWatchpoint.cpp
+++ Source/JavaScriptCore/bytecode/StructureStubClearingWatchpoint.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012, 2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2012, 2015-2016 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,7 +34,10 @@
 
 namespace JSC {
 
-StructureStubClearingWatchpoint::~StructureStubClearingWatchpoint() { }
+StructureStubClearingWatchpoint::~StructureStubClearingWatchpoint()
+{
+    for (auto current = WTFMove(m_next); current; current = WTFMove(current->m_next)) { }
+}
 
 StructureStubClearingWatchpoint* StructureStubClearingWatchpoint::push(
     const ObjectPropertyCondition& key,
