6a19050cc91f4af1d87a79eb657ac370f11b2659
Chris Dumez <cdumez@apple.com>
2016-07-22 02:21:23 +0000

    Make parameters mandatory for attribute-related API on Element https://bugs.webkit.org/show_bug.cgi?id=160059
    Reviewed by Ryosuke Niwa.
    
    LayoutTests/imported/w3c:
    
    Rebaseline several W3C tests now that more checks are passing.
    
    * web-platform-tests/dom/interfaces-expected.txt:
    * web-platform-tests/html/dom/interfaces-expected.txt:
    
    Source/WebCore:
    
    Make parameters mandatory for attribute-related API on Element to match
    the specification:
    - https://dom.spec.whatwg.org/#element
    
    Firefox and Chrome agree with the specification. Calling this API
    without the parameters does not make much sense, especially considering
    WebKit uses the string "undefined" when the parameter is omitted.
    
    No new tests, rebaselined existing tests.
    
    * dom/Element.idl:
    
    LayoutTests:
    
    Update existing layout tests to reflect the behavior change.
    
    * fast/dom/Element/script-tests/setAttributeNS-namespace-err.js:
    * fast/dom/Element/setAttributeNS-namespace-err-expected.txt:
    * fast/dom/HTMLHrElement/hr-color-noshade-attribute.html:
    * fast/dom/attribute-downcast-right.html:
    * fast/images/style-access-during-imageChanged-style-freeze.html:
    * fast/selectors/read-only-read-write-input-basics.html:
    * fast/selectors/read-only-read-write-textarea-basics.html:
    * media/video-playsinline-expected.txt:
    * media/video-playsinline.html:
    
    Canonical link: https://commits.webkit.org/178221@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@203546 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/dom/Element/script-tests/setAttributeNS-namespace-err.js
+++ LayoutTests/fast/dom/Element/script-tests/setAttributeNS-namespace-err.js
@@ -124,10 +124,9 @@ function runNSTests()
     }
 }
 
-// Moz throws a "Not enough arguments" exception in these, we don't:
 var element = document.createElement("div");
-shouldBeUndefined("element.setAttributeNS()");
-shouldBeUndefined("element.setAttributeNS(\"http://www.example.com\")");
-shouldBeUndefined("element.setAttributeNS(\"http://www.example.com\", \"foo\")");
+shouldThrow("element.setAttributeNS()");
+shouldThrow("element.setAttributeNS(\"http://www.example.com\")");
+shouldThrow("element.setAttributeNS(\"http://www.example.com\", \"foo\")");
 
 runNSTests();
