ff0b53758856d0f453d1852caa7830776a9796ab
Zalan Bujtas <zalan@apple.com>
2016-07-24 01:38:24 +0000

    Remove unused enum and stale comment from RenderObject. https://bugs.webkit.org/show_bug.cgi?id=160122
    Reviewed by Simon Fraser.
    
    No change in functionality.
    
    * rendering/RenderBox.h:
    
    
    Canonical link: https://commits.webkit.org/178287@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@203656 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderBox.h
+++ Source/WebCore/rendering/RenderBox.h
@@ -280,13 +280,6 @@ public:
         m_marginBox.setEnd(value, styleToUse->writingMode(), styleToUse->direction());
     }
 
-    // The following five functions are used to implement collapsing margins.
-    // All objects know their maximal positive and negative margins.  The
-    // formula for computing a collapsed margin is |maxPosMargin| - |maxNegmargin|.
-    // For a non-collapsing box, such as a leaf element, this formula will simply return
-    // the margin of the element.  Blocks override the maxMarginBefore and maxMarginAfter
-    // methods.
-    enum MarginSign { PositiveMargin, NegativeMargin };
     virtual bool isSelfCollapsingBlock() const { return false; }
     virtual LayoutUnit collapsedMarginBefore() const { return marginBefore(); }
     virtual LayoutUnit collapsedMarginAfter() const { return marginAfter(); }
