c6c4cf5a606d4f98a28269842336fbe3b0e365da
Chris Dumez <cdumez@apple.com>
2016-07-30 01:08:41 +0000

    Window's named properties should be exposed on a WindowProperties object in its prototype https://bugs.webkit.org/show_bug.cgi?id=160354
    Reviewed by Gavin Barraclough.
    
    LayoutTests/imported/w3c:
    
    Rebaseline W3C test now that one more check is passing.
    
    * web-platform-tests/html/dom/interfaces-expected.txt:
    
    Source/WebCore:
    
    Window's named properties should be exposed on a WindowProperties object
    in its prototype:
    - http://heycam.github.io/webidl/#named-properties-object
    
    Firefox and Chrome both comply with the specification. However, WebKit
    had no "WindowProperties" object in the Window prototype chain and the
    named properties are exposed on the Window object itself.
    
    No new tests, rebaselined existing tests.
    
    * CMakeLists.txt:
    * WebCore.xcodeproj/project.pbxproj:
    * bindings/js/JSBindingsAllInOne.cpp:
    * bindings/js/JSDOMWindowCustom.cpp:
    (WebCore::JSDOMWindow::getOwnPropertySlot):
    (WebCore::JSDOMWindow::getOwnPropertySlotByIndex):
    (WebCore::jsDOMWindowGetOwnPropertySlotRestrictedAccess): Deleted.
    (WebCore::JSDOMWindow::put): Deleted.
    (WebCore::JSDOMWindow::putByIndex): Deleted.
    (WebCore::JSDOMWindow::getEnumerableLength): Deleted.
    * bindings/js/JSDOMWindowProperties.cpp: Added.
    (WebCore::jsDOMWindowPropertiesGetOwnPropertySlotNamedItemGetter):
    (WebCore::JSDOMWindowProperties::getOwnPropertySlot):
    (WebCore::JSDOMWindowProperties::getOwnPropertySlotByIndex):
    * bindings/js/JSDOMWindowProperties.h: Added.
    (WebCore::JSDOMWindowProperties::create):
    (WebCore::JSDOMWindowProperties::createStructure):
    (WebCore::JSDOMWindowProperties::JSDOMWindowProperties):
    * bindings/js/JSDOMWindowShell.cpp:
    (WebCore::JSDOMWindowShell::setWindow):
    
    LayoutTests:
    
    * fast/dom/Window/es52-globals-expected.txt:
    Update / Rebaseline test now that named properties are no longer reported as "own"
    properties on the Window object. I have verified that the test gives the
    same result in Firefox and Chrome.
    
    * fast/loader/window-clearing-expected.txt:
    Rebaseline test that prints one more line because there is one more
    object in Window's prototype chain.
    
    * http/tests/security/window-named-proto-expected.txt:
    * http/tests/security/window-named-valueOf-expected.txt:
    Rebaseline 2 security tests that give slightly different output. The new
    output is identical to the one in Firefox and Chrome. The tests are not
    failing since they are not alert'ing content from the other frame.
    The reason those tests were logging a security error is because we would
    previously prevent named property access if the frame name conflicts with
    a property name in the Window prototype, and we now no longer
    differentiate this case.
    
    Canonical link: https://commits.webkit.org/178520@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@203935 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/js/JSDOMWindowShell.cpp
+++ Source/WebCore/bindings/js/JSDOMWindowShell.cpp
@@ -32,6 +32,7 @@
 #include "Frame.h"
 #include "GCController.h"
 #include "JSDOMWindow.h"
+#include "JSDOMWindowProperties.h"
 #include "JSEventTarget.h"
 #include "ScriptController.h"
 #include <heap/StrongInlines.h>
@@ -85,7 +86,11 @@ void JSDOMWindowShell::setWindow(RefPtr<DOMWindow>&& domWindow)
     Structure* structure = JSDOMWindow::createStructure(vm, 0, prototype.get());
     JSDOMWindow* jsDOMWindow = JSDOMWindow::create(vm, structure, *domWindow, this);
     prototype->structure()->setGlobalObject(vm, jsDOMWindow);
-    prototype->structure()->setPrototypeWithoutTransition(vm, JSEventTarget::prototype(vm, jsDOMWindow));
+
+    Structure* windowPropertiesStructure = JSDOMWindowProperties::createStructure(vm, jsDOMWindow, JSEventTarget::prototype(vm, jsDOMWindow));
+    JSDOMWindowProperties* windowProperties = JSDOMWindowProperties::create(windowPropertiesStructure, *jsDOMWindow);
+
+    prototype->structure()->setPrototypeWithoutTransition(vm, windowProperties);
     setWindow(vm, jsDOMWindow);
     ASSERT(jsDOMWindow->globalObject() == jsDOMWindow);
     ASSERT(prototype->globalObject() == jsDOMWindow);
