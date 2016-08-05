6bd42c3e73ae7661653cd23490e8954c84ec5962
Saam Barati <sbarati@apple.com>
2016-08-05 19:16:28 +0000

    Assertion failure when accessing TDZ variable in catch through eval https://bugs.webkit.org/show_bug.cgi?id=160554
    Reviewed by Mark Lam and Keith Miller.
    
    JSTests:
    
    * stress/catch-variables-under-tdz.js: Added.
    (test):
    
    Source/JavaScriptCore:
    
    When we were calculating the variables under TDZ from a JSScope,
    the algorithm was not taking into account that a catch scope
    has variables under TDZ.
    
    * runtime/JSScope.cpp:
    (JSC::JSScope::collectVariablesUnderTDZ):
    
    
    
    Canonical link: https://commits.webkit.org/178720@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@204182 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/JSScope.cpp
+++ Source/JavaScriptCore/runtime/JSScope.cpp
@@ -264,7 +264,7 @@ ResolveOp JSScope::abstractResolve(ExecState* exec, size_t depthOffset, JSScope*
 void JSScope::collectVariablesUnderTDZ(JSScope* scope, VariableEnvironment& result)
 {
     for (; scope; scope = scope->next()) {
-        if (!scope->isLexicalScope() && !scope->isGlobalLexicalEnvironment())
+        if (!scope->isLexicalScope() && !scope->isGlobalLexicalEnvironment() && !scope->isCatchScope())
             continue;
 
         if (scope->isModuleScope()) {
@@ -274,7 +274,7 @@ void JSScope::collectVariablesUnderTDZ(JSScope* scope, VariableEnvironment& resu
         }
 
         SymbolTable* symbolTable = jsCast<JSSymbolTableObject*>(scope)->symbolTable();
-        ASSERT(symbolTable->scopeType() == SymbolTable::ScopeType::LexicalScope || symbolTable->scopeType() == SymbolTable::ScopeType::GlobalLexicalScope);
+        ASSERT(symbolTable->scopeType() == SymbolTable::ScopeType::LexicalScope || symbolTable->scopeType() == SymbolTable::ScopeType::GlobalLexicalScope || symbolTable->scopeType() == SymbolTable::ScopeType::CatchScope);
         ConcurrentJITLocker locker(symbolTable->m_lock);
         for (auto end = symbolTable->end(locker), iter = symbolTable->begin(locker); iter != end; ++iter)
             result.add(iter->key);
