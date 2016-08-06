13347bf4032525ebbf46757eb44e4f1e5b433281
Youenn Fablet <youenn@apple.com>
2016-08-06 08:48:31 +0000

    LayoutTest http/tests/fetch/fetch-in-worker-crash.html is flaky https://bugs.webkit.org/show_bug.cgi?id=160510
    Patch by Youenn Fablet <youenn@apple.com> on 2016-08-06
    Reviewed by Sam Weinig.
    
    * http/tests/fetch/fetch-in-worker-crash.html:
    * http/tests/fetch/fetch-in-worker.js:
    Adding a new test based on a ReadableStream that is not closed and therefore Response.text promise cannot resolve.
    Removing the blob resolve message as there is no guarantee that blob load will finish after the test is done.
    
    Canonical link: https://commits.webkit.org/178756@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@204223 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/fetch/fetch-in-worker.js
+++ LayoutTests/http/tests/fetch/fetch-in-worker.js
@@ -9,10 +9,19 @@ onmessage = function(e) {
         postMessage("FAIL: promise 1 rejected");
     });
 
-    var promise2 = new Request("test", {method: "POST", body: new Blob(["ab"])}).text().then(() => {
+    var stream = new ReadableStream({"start": function(controller) {
+        controller.enqueue("starting but not closing");
+    }});
+    var promise2 = new Response(stream).text().then(() => {
         postMessage("FAIL: promise 2 resolved");
     }, () => {
         postMessage("FAIL: promise 2 rejected");
+    });
+
+    var promise3 = new Request("test", {method: "POST", body: new Blob(["ab"])}).text().then(() => {
+        // Not posting message as it could make the test flaky since we are not sure the test will end before the blob load finishes.
+    }, () => {
+        postMessage("FAIL: promise 3 rejected");
     });
 
     postMessage(counter++);
