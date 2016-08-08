eac2c448867f3a760e67eaa3e7030b1d18e65700
John Wilander <wilander@apple.com>
2016-08-08 21:43:36 +0000

    Popups opened from a sandboxed iframe should themselves be sandboxed https://bugs.webkit.org/show_bug.cgi?id=134850 <rdar://problem/27375388>
    Reviewed by Brent Fulgham.
    
    Source/WebCore:
    
    Test: http/tests/security/window-opened-from-sandboxed-iframe-should-inherit-sandbox.html
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::continueLoadAfterNewWindowPolicy):
        Now copies the opener's frame loader effective sandbox flags to the
        new frame loader.
    
    LayoutTests:
    
    * http/tests/security/resources/anchor-tag-with-blank-target.html: Added.
    * http/tests/security/resources/page-executing-javascript.html: Added.
    * http/tests/security/window-opened-from-sandboxed-iframe-should-inherit-sandbox-expected.txt: Added.
    * http/tests/security/window-opened-from-sandboxed-iframe-should-inherit-sandbox.html: Added.
    
    
    Canonical link: https://commits.webkit.org/178798@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@204266 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/FrameLoader.cpp
+++ Source/WebCore/loader/FrameLoader.cpp
@@ -3118,6 +3118,8 @@ void FrameLoader::continueLoadAfterNewWindowPolicy(const ResourceRequest& reques
     if (!mainFrame)
         return;
 
+    mainFrame->loader().forceSandboxFlags(frame->loader().effectiveSandboxFlags());
+
     if (frameName != "_blank")
         mainFrame->tree().setName(frameName);
 
