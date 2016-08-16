84a6c7fcd55a361bcd26e9aff28c62f8faeeeab6
Chris Dumez <cdumez@apple.com>
2016-08-16 03:32:38 +0000

    Drop unused EventTarget::hasActiveEventListeners() https://bugs.webkit.org/show_bug.cgi?id=160869
    Reviewed by Alex Christensen.
    
    * dom/EventListenerMap.cpp:
    (WebCore::EventListenerMap::containsActive): Deleted.
    * dom/EventListenerMap.h:
    * dom/EventTarget.cpp:
    (WebCore::EventTarget::hasActiveEventListeners): Deleted.
    * dom/EventTarget.h:
    
    
    Canonical link: https://commits.webkit.org/178990@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@204494 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/EventListenerMap.cpp
+++ Source/WebCore/dom/EventListenerMap.cpp
@@ -67,19 +67,6 @@ bool EventListenerMap::containsCapturing(const AtomicString& eventType) const
     return false;
 }
 
-bool EventListenerMap::containsActive(const AtomicString& eventType) const
-{
-    auto* listeners = find(eventType);
-    if (!listeners)
-        return false;
-
-    for (auto& eventListener : *listeners) {
-        if (!eventListener->isPassive())
-            return true;
-    }
-    return false;
-}
-
 void EventListenerMap::clear()
 {
     assertNoActiveIterators();
