eec487b20b472d5d1eaae750f57569274b2bd011
Babak Shafiei <bshafiei@apple.com>
2016-08-17 21:31:33 +0000

    Merge r204572. rdar://problem/27889416
    Canonical link: https://commits.webkit.org/178036.150@safari-602.1.50.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-602.1.50.1-branch@204574 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/ArgList.h
+++ Source/JavaScriptCore/runtime/ArgList.h
@@ -78,7 +78,7 @@ public:
 
     void append(JSValue v)
     {
-        if (m_size >= m_capacity)
+        if (m_size >= m_capacity || mallocBase())
             return slowAppend(v);
 
         slotFor(m_size) = JSValue::encode(v);
@@ -100,6 +100,10 @@ public:
     static void markLists(HeapRootVisitor&, ListSet&);
 
 private:
+    void expandCapacity();
+
+    void addMarkSet(JSValue);
+
     JS_EXPORT_PRIVATE void slowAppend(JSValue);
         
     EncodedJSValue& slotFor(int item) const
@@ -109,7 +113,7 @@ private:
         
     EncodedJSValue* mallocBase()
     {
-        if (m_capacity == static_cast<int>(inlineCapacity))
+        if (m_buffer == m_inlineBuffer)
             return 0;
         return &slotFor(0);
     }
