311676e5a4b341a2c06e863b4cc70f96b68785b8
Per Arne Vollan <pvollan@apple.com>
2016-08-19 17:10:57 +0000

    [Win] Warning fix. https://bugs.webkit.org/show_bug.cgi?id=160994
    Reviewed by Anders Carlsson.
    
    Ignore warning when function declared with __declspec(noreturn) has non void return type.
    
    * Source/cmake/OptionsWin.cmake:
    
    
    Canonical link: https://commits.webkit.org/179113@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@204635 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/OptionsWin.cmake
+++ Source/cmake/OptionsWin.cmake
@@ -124,9 +124,9 @@ if (MSVC)
         /wd4018 /wd4068 /wd4099 /wd4100 /wd4127 /wd4138 /wd4146 /wd4180 /wd4189
         /wd4201 /wd4244 /wd4251 /wd4267 /wd4275 /wd4288 /wd4291 /wd4305 /wd4309
         /wd4344 /wd4355 /wd4389 /wd4396 /wd4456 /wd4457 /wd4458 /wd4459 /wd4481
-        /wd4503 /wd4505 /wd4510 /wd4512 /wd4530 /wd4610 /wd4611 /wd4702 /wd4706
-        /wd4800 /wd4819 /wd4951 /wd4952 /wd4996 /wd6011 /wd6031 /wd6211 /wd6246
-        /wd6255 /wd6387
+        /wd4503 /wd4505 /wd4510 /wd4512 /wd4530 /wd4610 /wd4611 /wd4646 /wd4702
+        /wd4706 /wd4800 /wd4819 /wd4951 /wd4952 /wd4996 /wd6011 /wd6031 /wd6211
+        /wd6246 /wd6255 /wd6387
     )
 
     # Create pdb files for debugging purposes, also for Release builds
