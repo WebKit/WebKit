4cf91ce4cb659ec5ed7334ee45a88b932fd666a4
Geoffrey Garen <ggaren@apple.com>
2016-08-22 13:51:54 +0000

    Merge r204091 - [bmalloc] Merging of XLargeRanges can leak the upper range https://bugs.webkit.org/show_bug.cgi?id=160403
    Reviewed by Michael Saboff.
    
    * bmalloc/Heap.cpp:
    (bmalloc::Heap::scavengeLargeObjects): Don't use removePhysical().
    Recorded physical size is a performance optimization. It is not the
    truth. So it might be zero even if a range contains physical pages.
    
    Instead, iterate each range in the map unconditionally.
    
    The map can shrink when we release the lock, so we must clamp our
    iterator each time through the loop.
    
    The map can grow when we release the lock, but we don't care because
    growth restarts the scavenger from the beginning.
    
    * bmalloc/XLargeMap.cpp:
    (bmalloc::XLargeMap::removePhysical): Deleted. Not used anymore.
    
    * bmalloc/XLargeMap.h:
    (bmalloc::XLargeMap::ranges): Added direct access for the sake of
    scavengeLargeObjects. (This violates our naming conventions -- I'll do
    a rename in a follow-up patch.)

--- Source/bmalloc/bmalloc/Heap.cpp
+++ Source/bmalloc/bmalloc/Heap.cpp
@@ -131,13 +131,16 @@ void Heap::scavengeSmallPages(std::unique_lock<StaticMutex>& lock, std::chrono::
 
 void Heap::scavengeLargeObjects(std::unique_lock<StaticMutex>& lock, std::chrono::milliseconds sleepDuration)
 {
-    while (XLargeRange range = m_largeFree.removePhysical()) {
+    auto& ranges = m_largeFree.ranges();
+    for (size_t i = ranges.size(); i-- > 0; i = std::min(i, ranges.size())) {
+        auto range = ranges.pop(i);
+
         lock.unlock();
         vmDeallocatePhysicalPagesSloppy(range.begin(), range.size());
         lock.lock();
-        
+
         range.setPhysicalSize(0);
-        m_largeFree.add(range);
+        ranges.push(range);
 
         waitUntilFalse(lock, sleepDuration, m_isAllocatingPages);
     }
