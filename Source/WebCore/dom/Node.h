9c7b0448c3b19da4c3f4fb039cce1a0df90df4f4
Ryan Haddad <ryanhaddad@apple.com>
2016-08-23 09:39:53 +0000

    Merge r201292 - Unreviewed, rolling out r200414.
    This change appears to have broken the 'write a reply' field
    on Nextdoor.com
    
    Reverted changeset:
    
    "Clicks inside button elements are sometimes discarded when
    the mouse moves"
    https://bugs.webkit.org/show_bug.cgi?id=39620
    http://trac.webkit.org/changeset/200414

--- Source/WebCore/dom/Node.h
+++ Source/WebCore/dom/Node.h
@@ -791,9 +791,6 @@ inline ContainerNode* Node::parentNodeGuaranteedHostFree() const
     return parentNode();
 }
 
-Node* commonAncestor(Node&, Node&);
-Node* commonAncestorCrossingShadowBoundary(Node&, Node&);
-
 } // namespace WebCore
 
 #if ENABLE(TREE_DEBUGGING)
