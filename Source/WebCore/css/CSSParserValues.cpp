4d84c7c0382b39eccef36346443fbe587dd982b3
Darin Adler <darin@apple.com>
2016-08-23 09:57:31 +0000

    Merge r201608 - Fix a couple of mistakes in CSSParserValue memory management https://bugs.webkit.org/show_bug.cgi?id=158307 <rdar://problem/26127225>
    Source/WebCore:
    
    Patch by Darin Adler <darin@apple.com> on 2016-06-02
    Reviewed by Daniel Bates.
    
    * css/CSSGrammar.y.in: Added a destructor for calc_func_term. This presumably
    fixes some memory leaks in error cases. Removed an assertion about not needing
    a call to destroy that was far too limited. Tweaked formatting of the percentage
    ase in the key production. Indented calc_func_term to make it consistent with
    other productions nearby.
    
    * css/CSSParserValues.cpp:
    (WebCore::CSSParserValueList::~CSSParserValueList): Use a modern for loop.
    (WebCore::CSSParserValueList::deleteValueAt): Deleted. Unused function, and also
    would have resulted in a memory leak unless the code already extracted the value
    from the list.
    (WebCore::CSSParserValueList::extend): Properly transfer ownership from one value
    list to the other by setting the unit to 0 in the donor.
    
    * css/CSSParserValues.h: Removed unused deleteValueAt function.
    
    LayoutTests:
    
    Reviewed by Darin Adler.
    
    * fast/css/calc-with-two-variables-crash-expected.txt: Added.
    * fast/css/calc-with-two-variables-crash.html: Added.

--- Source/WebCore/css/CSSParserValues.cpp
+++ Source/WebCore/css/CSSParserValues.cpp
@@ -46,29 +46,26 @@ void destroy(const CSSParserValue& value)
 
 CSSParserValueList::~CSSParserValueList()
 {
-    for (size_t i = 0, size = m_values.size(); i < size; i++)
-        destroy(m_values[i]);
+    for (auto& value : m_values)
+        destroy(value);
 }
 
-void CSSParserValueList::addValue(const CSSParserValue& v)
+void CSSParserValueList::addValue(const CSSParserValue& value)
 {
-    m_values.append(v);
+    m_values.append(value);
 }
 
-void CSSParserValueList::insertValueAt(unsigned i, const CSSParserValue& v)
+void CSSParserValueList::insertValueAt(unsigned i, const CSSParserValue& value)
 {
-    m_values.insert(i, v);
+    m_values.insert(i, value);
 }
 
-void CSSParserValueList::deleteValueAt(unsigned i)
+void CSSParserValueList::extend(CSSParserValueList& other)
 {
-    m_values.remove(i);
-}
-
-void CSSParserValueList::extend(CSSParserValueList& valueList)
-{
-    for (unsigned int i = 0; i < valueList.size(); ++i)
-        m_values.append(*(valueList.valueAt(i)));
+    for (auto& value : other.m_values) {
+        m_values.append(value);
+        value.unit = 0; // We moved the CSSParserValue from the other list; this acts like std::move.
+    }
 }
 
 bool CSSParserValueList::containsVariables() const
