ca37f6aac8eb6ff8e16f18f443c076a7fb06000f
Jer Noble <jer.noble@apple.com>
2016-08-26 23:28:59 +0000

    Add a test harness for running UI tests on the iOS Simulator https://bugs.webkit.org/show_bug.cgi?id=161265
    .:
    
    Add the new MobileMiniBrowser project to the WebKit workspace.
    
    Reviewed by Simon Fraser.
    
    * WebKit.xcworkspace/contents.xcworkspacedata:
    
    Tools:
    
    Reviewed by Simon Fraser.
    
    Add a new iOS Simulator application which uses WKWebView to implement a basic browser, and which
    can serve as a test harness for WebKit UI tests.
    
    Also add a basic media playback test which verifies that a <video> element will begin playing in
    fullscreen on an iPhone device, but will play inline on an iPad device.
    
    * Makefile: Add MobileMiniBrowser as a Module for iOS builds.
    * MobileMiniBrowser/Configurations/Base.xcconfig: Added.
    * MobileMiniBrowser/Configurations/DebugRelease.xcconfig: Added.
    * MobileMiniBrowser/Configurations/MobileMiniBrowser.xcconfig: Added.
    * MobileMiniBrowser/Makefile: Added.
    * MobileMiniBrowser/MobileMiniBrowser.xcodeproj/project.pbxproj: Added.
    * MobileMiniBrowser/MobileMiniBrowser/AppDelegate.h: Added.
    * MobileMiniBrowser/MobileMiniBrowser/AppDelegate.m: Added.
    (-[AppDelegate application:didFinishLaunchingWithOptions:]):
    (-[AppDelegate applicationWillResignActive:]):
    (-[AppDelegate applicationDidEnterBackground:]):
    (-[AppDelegate applicationWillEnterForeground:]):
    (-[AppDelegate applicationDidBecomeActive:]):
    (-[AppDelegate applicationWillTerminate:]):
    * MobileMiniBrowser/MobileMiniBrowser/Assets.xcassets/AppIcon.appiconset/Contents.json: Added.
    * MobileMiniBrowser/MobileMiniBrowser/Base.lproj/LaunchScreen.storyboard: Added.
    * MobileMiniBrowser/MobileMiniBrowser/Base.lproj/Main.storyboard: Added.
    * MobileMiniBrowser/MobileMiniBrowser/Info.plist: Added.
    * MobileMiniBrowser/MobileMiniBrowser/TabViewController.h: Added.
    * MobileMiniBrowser/MobileMiniBrowser/TabViewController.m: Added.
    (-[TabViewController dealloc]):
    (-[TabViewController viewDidLoad]):
    (-[TabViewController observeValueForKeyPath:ofObject:change:context:]):
    (-[TabViewController tableView:cellForRowAtIndexPath:]):
    (-[TabViewController tableView:numberOfRowsInSection:]):
    (-[TabViewController numberOfSectionsInTableView:]):
    (-[TabViewController tableView:canEditRowAtIndexPath:]):
    (-[TabViewController tableView:canMoveRowAtIndexPath:]):
    (-[TabViewController tableView:commitEditingStyle:forRowAtIndexPath:]):
    (-[TabViewController tableView:didSelectRowAtIndexPath:]):
    * MobileMiniBrowser/MobileMiniBrowser/WebViewController.h: Added.
    * MobileMiniBrowser/MobileMiniBrowser/WebViewController.m: Added.
    (-[WebViewController viewDidLoad]):
    (-[WebViewController didReceiveMemoryWarning]):
    (-[WebViewController reload:]):
    (-[WebViewController goBack:]):
    (-[WebViewController goForward:]):
    (-[WebViewController urlFieldEditingBegan:]):
    (-[WebViewController navigateTo:]):
    (-[WebViewController showTabs:]):
    (-[WebViewController selectWebViewAtIndex:]):
    (-[WebViewController removeWebViewAtIndex:]):
    (-[WebViewController addWebView]):
    (-[WebViewController createWebView]):
    (-[WebViewController removeWebView:]):
    (-[WebViewController setCurrentWebView:]):
    (-[WebViewController webView:didFailNavigation:withError:]):
    (-[WebViewController observeValueForKeyPath:ofObject:change:context:]):
    * MobileMiniBrowser/MobileMiniBrowser/main.m: Added.
    (main):
    * MobileMiniBrowser/MobileMiniBrowserUITests/Info.plist: Added.
    * MobileMiniBrowser/MobileMiniBrowserUITests/MobileMiniBrowserUITests.m: Added.
    (-[MobileMiniBrowserUITests setUp]):
    (-[MobileMiniBrowserUITests tearDown]):
    (-[MobileMiniBrowserUITests testBasicVideoPlayback]):
    
    Canonical link: https://commits.webkit.org/179433@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@205057 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Tools/MobileMiniBrowser/MobileMiniBrowserUITests/MobileMiniBrowserUITests.m
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2016 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import <XCTest/XCTest.h>
+#import <XCTest/XCUIRemote.h>
+
+@interface MobileMiniBrowserUITests : XCTestCase
+
+@end
+
+@implementation MobileMiniBrowserUITests
+
+- (void)setUp
+{
+    [super setUp];
+
+    self.continueAfterFailure = NO;
+
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testBasicVideoPlayback
+{
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    NSPredicate *exists = [NSPredicate predicateWithFormat:@"exists == 1"];
+
+    XCUIElement *urlField = app.textFields[@"URL Field"];
+    [self expectationForPredicate:exists evaluatedWithObject:urlField handler:nil];
+    [self waitForExpectationsWithTimeout:5.0 handler:nil];
+    [urlField tap];
+
+    XCUIElement *clearButton = urlField.buttons[@"Clear text"];
+    if (clearButton.exists)
+        [clearButton tap];
+    [urlField typeText:@"http://test-safari.apple.com/"];
+    [app.buttons[@"Go To URL"] tap];
+
+    XCUIElement *captions = app.links[@"Captions"];
+    [self expectationForPredicate:exists evaluatedWithObject:captions handler:nil];
+    [self waitForExpectationsWithTimeout:5.0 handler:nil];
+    [captions tap];
+
+    XCUIElement *viewInPage = app.links[@"Video in page"];
+    [self expectationForPredicate:exists evaluatedWithObject:viewInPage handler:nil];
+    [self waitForExpectationsWithTimeout:5.0 handler:nil];
+    [viewInPage tap];
+
+    XCUIElement *startPlayback = app.buttons[@"Start Playback"];
+    [self expectationForPredicate:exists evaluatedWithObject:startPlayback handler:nil];
+    [self waitForExpectationsWithTimeout:5.0 handler:nil];
+    [startPlayback tap];
+
+    UIUserInterfaceIdiom idiom = UI_USER_INTERFACE_IDIOM();
+    if (idiom == UIUserInterfaceIdiomPhone) {
+        XCUIElement *pauseButton = app.buttons[@"PauseButton"];
+        [self expectationForPredicate:exists evaluatedWithObject:pauseButton handler:nil];
+        [self waitForExpectationsWithTimeout:5.0 handler:nil];
+        [pauseButton tap];
+
+        XCUIElement *doneButton = app.buttons[@"Done"];
+        [self expectationForPredicate:exists evaluatedWithObject:doneButton handler:nil];
+        [self waitForExpectationsWithTimeout:5.0 handler:nil];
+        [doneButton tap];
+    } else if (idiom == UIUserInterfaceIdiomPad) {
+        XCUIElement *pauseButton = app.buttons[@"Pause"];
+        [self expectationForPredicate:exists evaluatedWithObject:pauseButton handler:nil];
+        [self waitForExpectationsWithTimeout:5.0 handler:nil];
+        [pauseButton tap];
+    }
+}
+
+@end
