e5c991543c3bc932d1700b6ae1ba1f3d2bebe07e
Matt Baker <mattbaker01@gmail.com>
2016-09-01 22:05:00 +0000

    Web Inspector: Value checks of secondsPerPixel cause errors https://bugs.webkit.org/show_bug.cgi?id=161304 <rdar://problem/28050070>
    Reviewed by Brian Burg.
    
    * UserInterface/Views/NetworkTimelineView.js:
    (WebInspector.NetworkTimelineView.prototype.reset):
    Clear resource node map, which is used to determine which nodes
    to refresh during layout.
    
    (WebInspector.NetworkTimelineView.prototype.layout):
    Like OverviewTimelineView, only refresh node graphs when times change.
    This prevents refreshing nodes when (endTime - startTime) === 0.
    
    * UserInterface/Views/TimelineRecordingContentView.js:
    (WebInspector.TimelineRecordingContentView.prototype._recordingReset):
    TimelineViews must be cleared before calling _updateTimes to avoid
    doing layouts in timeline views containing stale data.
    
    
    Canonical link: https://commits.webkit.org/179659@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@205320 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Views/TimelineRecordingContentView.js
+++ Source/WebInspectorUI/UserInterface/Views/TimelineRecordingContentView.js
@@ -618,6 +618,9 @@ WebInspector.TimelineRecordingContentView = class TimelineRecordingContentView e
 
     _recordingReset(event)
     {
+        for (let timelineView of this._timelineViewMap.values())
+            timelineView.reset();
+
         this._currentTime = NaN;
         this._discontinuityStartTime = NaN;
 
@@ -635,8 +638,6 @@ WebInspector.TimelineRecordingContentView = class TimelineRecordingContentView e
 
         this._timelineOverview.reset();
         this._overviewTimelineView.reset();
-        for (var timelineView of this._timelineViewMap.values())
-            timelineView.reset();
         this._clearTimelineNavigationItem.enabled = false;
     }
 
