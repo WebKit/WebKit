c3407e2c9e7298cc604f1f5d0355a3b7eed1efa4
Myles C. Maxfield <mmaxfield@apple.com>
2016-09-06 23:12:15 +0000

    Strikethrough positions are erroneously snapped twice https://bugs.webkit.org/show_bug.cgi?id=161647 <rdar://problem/11542470>
    Reviewed by Zalan Bujtas.
    
    Source/WebCore:
    
    Because we're using an int for the position of the baseline, the math to calculate the
    strikethrough position is snapped. Then, we snap it again inside GraphicsContext.
    
    Test: fast/text/strikethrough-int.html
    
    * rendering/InlineTextBox.cpp:
    (WebCore::InlineTextBox::paintDecoration):
    * rendering/TextDecorationPainter.h:
    (WebCore::TextDecorationPainter::setBaseline):
    
    LayoutTests:
    
    * fast/css3-text/css3-text-decoration/text-decoration-line-through-mispositioned-expected.html: Updated.
    * fast/text/strikethrough-int-expected-mismatch.html: Added.
    * fast/text/strikethrough-int.html: Added.
    
    
    Canonical link: https://commits.webkit.org/179835@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@205519 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/InlineTextBox.cpp
+++ Source/WebCore/rendering/InlineTextBox.cpp
@@ -715,13 +715,12 @@ void InlineTextBox::paintDecoration(GraphicsContext& context, const FontCascade&
         width = renderer().width(m_start, m_truncation, textPos(), isFirstLine());
         mirrorRTLSegment(m_logicalWidth, direction(), start, width);
     }
-    
-    int baseline = lineStyle().fontMetrics().ascent();
+
     TextDecorationPainter decorationPainter(context, decoration, renderer(), isFirstLine());
     decorationPainter.setInlineTextBox(this);
     decorationPainter.setFont(font);
     decorationPainter.setWidth(width);
-    decorationPainter.setBaseline(baseline);
+    decorationPainter.setBaseline(lineStyle().fontMetrics().ascent());
     decorationPainter.setIsHorizontal(isHorizontal());
     decorationPainter.addTextShadow(shadow);
 
