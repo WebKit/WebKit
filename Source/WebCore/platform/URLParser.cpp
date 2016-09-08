f6c571b106dfd940d35af15326552786eed39929
Alex Christensen <achristensen@apple.com>
2016-09-08 01:34:19 +0000

    Unreviewed, revert r205533. https://bugs.webkit.org/show_bug.cgi?id=161668
    Re-landing changesets:
    
    "Punycode encode non-ascii hosts in URLParser"
    https://bugs.webkit.org/show_bug.cgi?id=161655
    http://trac.webkit.org/changeset/205521
    
    "Fix query-only and fragment-only relative URLs when using
    URLParser"
    https://bugs.webkit.org/show_bug.cgi?id=161657
    http://trac.webkit.org/changeset/205526
    
    "URLParser should parse / as a relative URL"
    https://bugs.webkit.org/show_bug.cgi?id=161667
    http://trac.webkit.org/changeset/205532
    
    
    Canonical link: https://commits.webkit.org/179895@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@205580 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/URLParser.cpp
+++ Source/WebCore/platform/URLParser.cpp
@@ -28,6 +28,7 @@
 
 #include "Logging.h"
 #include <array>
+#include <unicode/uidna.h>
 #include <wtf/HashMap.h>
 #include <wtf/NeverDestroyed.h>
 #include <wtf/text/StringBuilder.h>
@@ -450,7 +451,8 @@ URL URLParser::parse(const String& input, const URL& base, const TextEncoding& e
         case State::SchemeEndCheckForSlashes:
             LOG_STATE("SchemeEndCheckForSlashes");
             if (*c == '/') {
-                m_buffer.append('/');
+                m_buffer.append("//");
+                m_url.m_userStart = m_buffer.length();
                 state = State::PathOrAuthority;
                 ++c;
             } else {
@@ -515,11 +517,13 @@ URL URLParser::parse(const String& input, const URL& base, const TextEncoding& e
                 break;
             case '?':
                 copyURLPartsUntil(base, URLPart::PathEnd);
+                m_buffer.append('?');
                 state = State::Query;
                 ++c;
                 break;
             case '#':
                 copyURLPartsUntil(base, URLPart::QueryEnd);
+                m_buffer.append('#');
                 state = State::Fragment;
                 ++c;
                 break;
@@ -841,6 +845,12 @@ URL URLParser::parse(const String& input, const URL& base, const TextEncoding& e
         break;
     case State::RelativeSlash:
         LOG_FINAL_STATE("RelativeSlash");
+        copyURLPartsUntil(base, URLPart::PortEnd);
+        m_buffer.append('/');
+        m_url.m_pathAfterLastSlash = base.m_portEnd + 1;
+        m_url.m_pathEnd = m_url.m_pathAfterLastSlash;
+        m_url.m_queryEnd = m_url.m_pathAfterLastSlash;
+        m_url.m_fragmentEnd = m_url.m_pathAfterLastSlash;
         break;
     case State::SpecialAuthoritySlashes:
         LOG_FINAL_STATE("SpecialAuthoritySlashes");
@@ -1269,11 +1279,44 @@ static String percentDecode(const String& input)
     return output.toStringPreserveCapacity();
 }
 
+static bool containsOnlyASCII(const String& string)
+{
+    if (string.is8Bit())
+        return charactersAreAllASCII(string.characters8(), string.length());
+    return charactersAreAllASCII(string.characters16(), string.length());
+}
+
 static Optional<String> domainToASCII(const String& domain)
 {
-    // FIXME: Implement correctly
-    CString utf8 = domain.utf8();
-    return String(utf8.data(), utf8.length());
+    const unsigned hostnameBufferLength = 2048;
+
+    if (containsOnlyASCII(domain)) {
+        if (domain.is8Bit())
+            return domain;
+        Vector<LChar, hostnameBufferLength> buffer;
+        size_t length = domain.length();
+        buffer.reserveInitialCapacity(length);
+        for (size_t i = 0; i < length; ++i)
+            buffer.append(domain[i]);
+        return String(buffer.data(), length);
+    }
+    
+    UChar hostnameBuffer[hostnameBufferLength];
+    UErrorCode error = U_ZERO_ERROR;
+    
+    int32_t numCharactersConverted = uidna_IDNToASCII(StringView(domain).upconvertedCharacters(), domain.length(), hostnameBuffer, hostnameBufferLength, UIDNA_ALLOW_UNASSIGNED, nullptr, &error);
+
+    if (error == U_ZERO_ERROR) {
+        LChar buffer[hostnameBufferLength];
+        for (int32_t i = 0; i < numCharactersConverted; ++i) {
+            ASSERT(isASCII(hostnameBuffer[i]));
+            buffer[i] = hostnameBuffer[i];
+        }
+        return String(buffer, numCharactersConverted);
+    }
+
+    // FIXME: Check for U_BUFFER_OVERFLOW_ERROR and retry with an allocated buffer.
+    return Nullopt;
 }
 
 static bool hasInvalidDomainCharacter(const String& asciiDomain)
