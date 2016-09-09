880c416b718ea162c8ecd293a9d644e7209ceddd
Carlos Garcia Campos <cgarcia@igalia.com>
2016-09-09 06:26:14 +0000

    Unreviewed. Fix GTK+ build after r205689.
    * WebProcess/InjectedBundle/API/gtk/DOM/WebKitDOMHTMLOListElement.cpp:
    (webkit_dom_html_o_list_element_set_start):
    
    Canonical link: https://commits.webkit.org/179955@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@205696 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/WebProcess/InjectedBundle/API/gtk/DOM/WebKitDOMHTMLOListElement.cpp
+++ Source/WebKit2/WebProcess/InjectedBundle/API/gtk/DOM/WebKitDOMHTMLOListElement.cpp
@@ -216,7 +216,7 @@ void webkit_dom_html_o_list_element_set_start(WebKitDOMHTMLOListElement* self, g
     WebCore::JSMainThreadNullState state;
     g_return_if_fail(WEBKIT_DOM_IS_HTML_O_LIST_ELEMENT(self));
     WebCore::HTMLOListElement* item = WebKit::core(self);
-    item->setStart(value);
+    item->setStartForBindings(value);
 }
 
 gchar* webkit_dom_html_o_list_element_get_type_attr(WebKitDOMHTMLOListElement* self)
