2d0121e51871e2705059db222cc3abf87e188c31
Mark Lam <mark.lam@apple.com>
2016-09-13 22:16:25 +0000

    DFG NewArrayBuffer node should watch for "have a bad time" state change. https://bugs.webkit.org/show_bug.cgi?id=161927 <rdar://problem/27995222>
    Reviewed by Geoffrey Garen.
    
    * dfg/DFGFixupPhase.cpp:
    (JSC::DFG::FixupPhase::fixupNode):
    
    
    
    Canonical link: https://commits.webkit.org/180077@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@205882 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGFixupPhase.cpp
+++ Source/JavaScriptCore/dfg/DFGFixupPhase.cpp
@@ -1096,6 +1096,11 @@ private:
             break;
         }
 
+        case NewArrayBuffer: {
+            watchHavingABadTime(node);
+            break;
+        }
+
         case CallObjectConstructor: {
             if (node->child1()->shouldSpeculateObject()) {
                 fixEdge<ObjectUse>(node->child1());
@@ -1614,7 +1619,6 @@ private:
         case ForwardVarargs:
         case ProfileControlFlow:
         case NewObject:
-        case NewArrayBuffer:
         case NewRegexp:
         case DeleteById:
         case DeleteByVal:
