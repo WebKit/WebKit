06b2814cc4284b6a1b1e188a49b40fc7994b1abc
Jer Noble <jer.noble@apple.com>
2016-09-19 23:30:53 +0000

    Unreviewed gardening; update LayoutTests/media-source to the most recent version (5a011ca).
    * resources/TestRepositories:
    * web-platform-tests/media-source/OWNERS:
    * web-platform-tests/media-source/interfaces.html:
    * web-platform-tests/media-source/manifest.txt:
    * web-platform-tests/media-source/mediasource-appendbuffer-quota-exceeded.html:
    * web-platform-tests/media-source/mediasource-attach-stops-delaying-load-event.html: Added.
    * web-platform-tests/media-source/mediasource-avtracks.html:
    * web-platform-tests/media-source/mediasource-detach.html:
    * web-platform-tests/media-source/mediasource-duration-expected.txt:
    * web-platform-tests/media-source/mediasource-duration.html:
    * web-platform-tests/media-source/mediasource-endofstream.html:
    * web-platform-tests/media-source/mediasource-errors.html:
    * web-platform-tests/media-source/mediasource-getvideoplaybackquality-expected.txt:
    * web-platform-tests/media-source/mediasource-getvideoplaybackquality.html:
    * web-platform-tests/media-source/mediasource-is-type-supported.html:
    * web-platform-tests/media-source/mediasource-liveseekable.html:
    * web-platform-tests/media-source/mediasource-remove-expected.txt:
    * web-platform-tests/media-source/mediasource-remove.html:
    * web-platform-tests/media-source/mediasource-removesourcebuffer.html:
    * web-platform-tests/media-source/mediasource-seekable.html:
    * web-platform-tests/media-source/mediasource-sourcebuffer-mode-timestamps.html: Added.
    * web-platform-tests/media-source/mediasource-sourcebuffer-mode.html:
    * web-platform-tests/media-source/mediasource-sourcebuffer-trackdefaults.html:
    * web-platform-tests/media-source/mediasource-timestamp-offset.html:
    * web-platform-tests/media-source/mediasource-trackdefault.html:
    * web-platform-tests/media-source/mediasource-trackdefaultlist.html:
    * web-platform-tests/media-source/mediasource-util.js:
    
    Canonical link: https://commits.webkit.org/180286@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@206129 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/imported/w3c/web-platform-tests/media-source/mediasource-util.js
+++ LayoutTests/imported/w3c/web-platform-tests/media-source/mediasource-util.js
@@ -303,7 +303,7 @@
 
         var i = startingIndex;
         var onAppendDone = function() {
-            if (eventFired)
+            if (eventFired || (i >= (segmentInfo.media.length - 1)))
                 return;
 
             i++;
