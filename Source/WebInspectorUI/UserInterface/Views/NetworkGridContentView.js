e502ae4326f356bb563ddfcce56b23ad0624baa4
Matt Baker <mattbaker01@gmail.com>
2016-09-27 21:05:50 +0000

    Web Inspector: Refreshing while in Timelines-tab causes negative timestamps in Network-tab https://bugs.webkit.org/show_bug.cgi?id=160051 <rdar://problem/27480122>
    Reviewed by Brian Burg.
    
    * UserInterface/Views/NetworkGridContentView.js:
    (WebInspector.NetworkGridContentView.prototype.get startTime):
    (WebInspector.NetworkGridContentView.prototype.get zeroTime):
    Use the cached start time for graph data source properties instead of
    relying on the ruler, which requires a layout in order to be updated
    for the first time.
    
    
    Canonical link: https://commits.webkit.org/180576@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@206464 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Views/NetworkGridContentView.js
+++ Source/WebInspectorUI/UserInterface/Views/NetworkGridContentView.js
@@ -115,10 +115,10 @@ WebInspector.NetworkGridContentView = class NetworkGridContentView extends WebIn
     // Public
 
     get secondsPerPixel() { return this._timelineRuler.secondsPerPixel; }
-    get startTime() { return this._timelineRuler.startTime; }
+    get startTime() { return this._startTime || 0; }
     get currentTime() { return this.endTime || this.startTime; }
     get endTime() { return this._timelineRuler.endTime; }
-    get zeroTime() { return this._timelineRuler.startTime; }
+    get zeroTime() { return this.startTime; }
 
     get selectionPathComponents()
     {
