5d0000ff7efa5e00230fca7c8b6d69f03196432d
Tim Horton <timothy_horton@apple.com>
2016-10-05 23:12:44 +0000

    Fix the build.
    * UIProcess/Cocoa/WebViewImpl.mm:
    
    
    Canonical link: https://commits.webkit.org/180887@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@206834 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/Cocoa/WebViewImpl.mm
+++ Source/WebKit2/UIProcess/Cocoa/WebViewImpl.mm
@@ -55,6 +55,7 @@
 #import "WKTextInputWindowController.h"
 #import "WKViewLayoutStrategy.h"
 #import "WKWebView.h"
+#import "WebBackForwardList.h"
 #import "WebEditCommandProxy.h"
 #import "WebEventFactory.h"
 #import "WebInspectorProxy.h"
