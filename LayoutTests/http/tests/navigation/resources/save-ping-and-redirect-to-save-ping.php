029543fe65bc16a8a47ca1f140af846101ab356a
Daniel Bates <dbates@webkit.org>
2016-10-05 16:29:37 +0000

    [WK2][NetworkSession] Ping requests should follow redirects if applicable https://bugs.webkit.org/show_bug.cgi?id=162580 <rdar://problem/28631274>
    Rename test anchor-ping-and-do-not-follow-redirect-when-sending-ping.html to anchor-ping-and-follow-redirect-when-sending-ping.html
    to reflect what it is actually testing - that we follow a redirect when sending a ping for a <a ping>.
    
    * http/tests/navigation/ping-attribute/anchor-ping-and-follow-redirect-when-sending-ping-expected.txt: Renamed from LayoutTests/http/tests/navigation/ping-attribute/anchor-ping-and-do-not-follow-redirect-when-sending-ping-expected.txt.
    * http/tests/navigation/ping-attribute/anchor-ping-and-follow-redirect-when-sending-ping.html: Renamed from LayoutTests/http/tests/navigation/ping-attribute/anchor-ping-and-do-not-follow-redirect-when-sending-ping.html.
    * http/tests/navigation/resources/save-ping-and-redirect-to-save-ping.php: Only append the query string
    if we have a non-empty query string to avoid appending a superfluous "?" on redirect.
    
    Canonical link: https://commits.webkit.org/180870@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@206813 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/navigation/resources/save-ping-and-redirect-to-save-ping.php
+++ LayoutTests/http/tests/navigation/resources/save-ping-and-redirect-to-save-ping.php
@@ -5,5 +5,5 @@ $DO_NOT_CLEAR_COOKIES = true; // Used by save-Ping.php
 require_once 'save-Ping.php';
 
 header('HTTP/1.1 307');
-header('Location: save-Ping.php' . (isset($_SERVER['QUERY_STRING']) ? '?' . $_SERVER['QUERY_STRING'] : ''));
+header('Location: save-Ping.php' . (!empty($_SERVER['QUERY_STRING']) ? '?' . $_SERVER['QUERY_STRING'] : ''));
 ?>
