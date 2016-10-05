1ad2579ad58b41ccb8e97ec336e33eb994ecade7
Jeremy Jones <jeremyj@apple.com>
2016-10-05 20:46:15 +0000

    Add MobileMiniBrowser tests for control center and looping fullscreen. https://bugs.webkit.org/show_bug.cgi?id=162748
    Patch by Jeremy Jones <jeremyj@apple.com> on 2016-10-05
    Reviewed by Jer Noble.
    
    Refactor existing MobileMiniBrowserTests and add new tests for
    - fullscreen rotation
    - fullscreen looping (failing)
    - control center interruption (failing)
    
    * MobileMiniBrowser/MobileMiniBrowser.xcodeproj/project.pbxproj:
    * MobileMiniBrowser/MobileMiniBrowserFramework/Resources/looping2s.html: Added.
    * MobileMiniBrowser/MobileMiniBrowserFramework/test2s.mp4: Added.
    * MobileMiniBrowser/MobileMiniBrowserUITests/MobileMiniBrowserUITests.m:
    (-[MobileMiniBrowserUITests setUp]):
    (-[MobileMiniBrowserUITests waitToTapButtonNamed:forApp:]):
    (-[MobileMiniBrowserUITests loadURL:]):
    (-[MobileMiniBrowserUITests requireMinFPS:sampleDurationSeconds:message:]):
    (-[MobileMiniBrowserUITests ensureFullscreenControls]):
    (-[MobileMiniBrowserUITests timeForTimeString:]):
    (-[MobileMiniBrowserUITests testBasicVideoPlayback]):
    (-[MobileMiniBrowserUITests testBasicVideoFullscreen]):
    (-[MobileMiniBrowserUITests testVideoFullscreenAndRotationAnimation]):
    (-[MobileMiniBrowserUITests testVideoFullscreenControlCenter]):
    (-[MobileMiniBrowserUITests testLoopingFullscreenLockup]):
    
    Canonical link: https://commits.webkit.org/180881@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@206827 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/MobileMiniBrowser/MobileMiniBrowserUITests/MobileMiniBrowserUITests.m
+++ Tools/MobileMiniBrowser/MobileMiniBrowserUITests/MobileMiniBrowserUITests.m
@@ -23,14 +23,22 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#import <QuartzCore/CARenderServer.h>
 #import <XCTest/XCTest.h>
 #import <XCTest/XCUIRemote.h>
+
+@interface XCUIApplication ()
+-(instancetype)initPrivateWithPath:(NSString *)path bundleID:(NSString *)bundleID;
+@end
 
 @interface MobileMiniBrowserUITests : XCTestCase
 
 @end
 
-@implementation MobileMiniBrowserUITests
+@implementation MobileMiniBrowserUITests {
+    XCUIApplication *app;
+    NSPredicate *exists;
+}
 
 - (void)setUp
 {
@@ -38,7 +46,8 @@
 
     self.continueAfterFailure = NO;
 
-    XCUIApplication *app = [[XCUIApplication alloc] init];
+    exists = [NSPredicate predicateWithFormat:@"exists == 1"];
+    app = [[XCUIApplication alloc] init];
     [app launch];
 }
 
@@ -47,44 +56,153 @@
     [super tearDown];
 }
 
-- (void)testBasicVideoPlayback
-{
-    XCUIApplication *app = [[XCUIApplication alloc] init];
-    NSPredicate *exists = [NSPredicate predicateWithFormat:@"exists == 1"];
+- (void)waitToTapButtonNamed:(NSString *)name forApp:(XCUIApplication *)targetApp {
+    XCUIElement *button = targetApp.buttons[name];
+    [self expectationForPredicate:exists evaluatedWithObject:button handler:nil];
+    [self waitForExpectationsWithTimeout:5.0 handler:nil];
+    [button tap];
+}
 
+- (void)loadURL:(NSString *)url {
     XCUIElement *urlField = app.textFields[@"URL Field"];
     [self expectationForPredicate:exists evaluatedWithObject:urlField handler:nil];
     [self waitForExpectationsWithTimeout:5.0 handler:nil];
     [urlField tap];
-
+    
     XCUIElement *clearButton = urlField.buttons[@"Clear text"];
     if (clearButton.exists)
         [clearButton tap];
-    [urlField typeText:@"bundle:/looping.html"];
+    [urlField typeText:url];
     [app.buttons[@"Go To URL"] tap];
+}
 
-    XCUIElement *startPlayback = app.buttons[@"Start Playback"];
-    [self expectationForPredicate:exists evaluatedWithObject:startPlayback handler:nil];
-    [self waitForExpectationsWithTimeout:5.0 handler:nil];
-    [startPlayback tap];
+- (void)requireMinFPS:(NSTimeInterval)minFPS sampleDurationSeconds:(NSTimeInterval)sampleSeconds message:(NSString *)message {
+    NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
+    uint32_t initialFrameCount = CARenderServerGetFrameCounter(0);
+    usleep(sampleSeconds*1000000);
+    uint32_t finalFrameCount = CARenderServerGetFrameCounter(0);
+    NSTimeInterval endTime = [NSDate timeIntervalSinceReferenceDate];
+    NSTimeInterval framesPerSecond = (finalFrameCount - initialFrameCount) / (endTime - startTime);
+    
+    XCTAssert(framesPerSecond > 30, @"framesPerSecond < %f: %@", framesPerSecond, message);
+}
+
+- (void)ensureFullscreenControls {
+    if (!app.buttons[@"Done"].exists) {
+        XCUIElement* window = [app.windows allElementsBoundByIndex][0];
+        XCUICoordinate* center = [window coordinateWithNormalizedOffset:CGVectorMake(0.5, 0.5)];
+        [center tap];
+    }
+}
+
+- (NSTimeInterval)timeForTimeString:(NSString *)time {
+    NSArray *components = [time componentsSeparatedByString:@":"];
+    if (components.count == 3)
+        return [components[0] doubleValue]*60*60 + [components[1] doubleValue]*60 + [components[2] doubleValue];
+
+    return [components[0] doubleValue]*60 + [components[1] doubleValue];
+}
+
+- (void)testBasicVideoPlayback
+{
+    [self loadURL:@"bundle:/looping.html"];
+    [self waitToTapButtonNamed:@"Start Playback" forApp:app];
 
     UIUserInterfaceIdiom idiom = UI_USER_INTERFACE_IDIOM();
     if (idiom == UIUserInterfaceIdiomPhone) {
-        XCUIElement *pauseButton = app.buttons[@"PauseButton"];
-        [self expectationForPredicate:exists evaluatedWithObject:pauseButton handler:nil];
-        [self waitForExpectationsWithTimeout:5.0 handler:nil];
-        [pauseButton tap];
+        [self waitToTapButtonNamed:@"PauseButton" forApp:app];
+        [self waitToTapButtonNamed:@"Done" forApp:app];
+    } else if (idiom == UIUserInterfaceIdiomPad)
+        [self waitToTapButtonNamed:@"Pause" forApp:app];
+}
 
-        XCUIElement *doneButton = app.buttons[@"Done"];
-        [self expectationForPredicate:exists evaluatedWithObject:doneButton handler:nil];
-        [self waitForExpectationsWithTimeout:5.0 handler:nil];
-        [doneButton tap];
-    } else if (idiom == UIUserInterfaceIdiomPad) {
-        XCUIElement *pauseButton = app.buttons[@"Pause"];
-        [self expectationForPredicate:exists evaluatedWithObject:pauseButton handler:nil];
-        [self waitForExpectationsWithTimeout:5.0 handler:nil];
-        [pauseButton tap];
-    }
+- (void)testBasicVideoFullscreen
+{
+    [self loadURL:@"bundle:/looping.html"];
+    [self waitToTapButtonNamed:@"Start Playback" forApp:app];
+    
+    UIUserInterfaceIdiom idiom = UI_USER_INTERFACE_IDIOM();
+    if (idiom == UIUserInterfaceIdiomPad)
+        [self waitToTapButtonNamed:@"Display Full Screen" forApp:app];
+    
+    [self waitToTapButtonNamed:@"PauseButton" forApp:app];
+    [self waitToTapButtonNamed:@"Done" forApp:app];
+}
+
+- (void)testVideoFullscreenAndRotationAnimation
+{
+    XCUIDevice *device = [XCUIDevice sharedDevice];
+    device.orientation = UIDeviceOrientationPortrait;
+    
+    [self loadURL:@"bundle:/looping.html"];
+    [self waitToTapButtonNamed:@"Start Playback" forApp:app];
+    
+    UIUserInterfaceIdiom idiom = UI_USER_INTERFACE_IDIOM();
+    if (idiom == UIUserInterfaceIdiomPad)
+        [self waitToTapButtonNamed:@"Display Full Screen" forApp:app];
+    
+    [self requireMinFPS:30 sampleDurationSeconds:1 message:@"Framerate during enter fullscreen animation."];
+    device.orientation = UIDeviceOrientationLandscapeLeft;
+    [self requireMinFPS:30 sampleDurationSeconds:1 message:@"Framerate during rotation animation."];
+    [self waitToTapButtonNamed:@"Done" forApp:app];
+    [self requireMinFPS:30 sampleDurationSeconds:1 message:@"Framerate during exit fullscreen  animation."];
+}
+
+- (void)testVideoFullscreenControlCenter
+{
+    [self loadURL:@"bundle:/looping.html"];
+    [self waitToTapButtonNamed:@"Start Playback" forApp:app];
+    
+    UIUserInterfaceIdiom idiom = UI_USER_INTERFACE_IDIOM();
+    if (idiom == UIUserInterfaceIdiomPad)
+        [self waitToTapButtonNamed:@"Display Full Screen" forApp:app];
+    
+    XCUIElement* window = [app.windows allElementsBoundByIndex][0];
+    XCUICoordinate* top = [window coordinateWithNormalizedOffset:CGVectorMake(0.5, 0.5)];
+    XCUICoordinate* bottom = [window coordinateWithNormalizedOffset:CGVectorMake(0.5, 1)];
+    [bottom pressForDuration:0 thenDragToCoordinate:top];
+    [bottom pressForDuration:0 thenDragToCoordinate:top];
+    
+    XCTAssert(app.buttons[@"PlayButton"].exists, @"Control center should have interrupted");
+    
+    XCUIApplication* springboard = [[XCUIApplication alloc] initPrivateWithPath:nil bundleID:@"com.apple.springboard"];
+    XCUICoordinate* right = [window coordinateWithNormalizedOffset:CGVectorMake(1, 0.9)];
+    XCUICoordinate* left = [window coordinateWithNormalizedOffset:CGVectorMake(0, 0.9)];
+
+    if (springboard.switches[@"Continue"].exists)
+        [springboard.switches[@"Continue"] tap];
+    
+    if (springboard.switches[@"Airplane Mode"].exists)
+        [right pressForDuration:0 thenDragToCoordinate:left];
+
+    XCTAssert(springboard.buttons[@"Play"].exists, @"Control center should have interrupted");
+    
+    NSString* appElapsedString = [app.staticTexts elementBoundByIndex:0].label;
+    NSTimeInterval appElapsed = [self timeForTimeString:appElapsedString];
+    
+    NSString* controlCenterTrackString = springboard.otherElements[@"Track position"].value;
+    NSArray* components = [controlCenterTrackString componentsSeparatedByString:@" of "];
+    NSTimeInterval controlCenterElapsed = [self timeForTimeString:components[0]];
+    
+    XCTAssert(appElapsed == round(controlCenterElapsed), "Elapsed times match");
+}
+
+// rdar://problem/27685077
+- (void)testLoopingFullscreenLockup
+{
+    [self loadURL:@"bundle:/looping2s.html"];
+    
+    XCUIElement* window = [app.windows allElementsBoundByIndex][0];
+    XCUICoordinate* gifLoc = [window coordinateWithNormalizedOffset:CGVectorMake(0.5, 0.75)];
+    [gifLoc pressForDuration:0];
+    
+    [self waitToTapButtonNamed:@"Start Playback" forApp:app];
+    
+    [self waitToTapButtonNamed:@"ExitFullScreenButton" forApp:app];
+    [self waitToTapButtonNamed:@"ExitFullScreenButton" forApp:app];
+    [self waitToTapButtonNamed:@"ExitFullScreenButton" forApp:app];
+    [self waitToTapButtonNamed:@"ExitFullScreenButton" forApp:app];
+    [self waitToTapButtonNamed:@"ExitFullScreenButton" forApp:app];
 }
 
 @end
