02a665332efa154037b8c1d66a241682a5d21bb0
Yusuke Suzuki <utatane.tea@gmail.com>
2016-10-08 02:07:09 +0000

    REGRESSION (r206853?): LayoutTest js/regress-141098.html failing https://bugs.webkit.org/show_bug.cgi?id=163046
    Reviewed by Saam Barati.
    
    This is attempt-to-fix patch since I cannot reproduce this flakiness.
    We reduce the number of frames to back off from the stack overflow to
    catch the closer frame limit to the actual stack limit.
    
    * js/script-tests/regress-141098.js:
    
    
    Canonical link: https://commits.webkit.org/180995@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@206947 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/js/script-tests/regress-141098.js
+++ LayoutTests/js/script-tests/regress-141098.js
@@ -13,7 +13,7 @@ description("Regression test for https://webkit.org/b/141098. Make sure eval() p
 // Under no circumstance should this test ever crash.
 let countStart = 2;
 let countIncrement = 8;
-let numberOfFramesToBackoffFromStackOverflowPoint = 50;
+let numberOfFramesToBackoffFromStackOverflowPoint = 10;
 
 // backoffEverything is chosen to be -1 because a negative number will never be
 // decremented to 0, and hence, will cause probeAndRecurse() to return out of every
