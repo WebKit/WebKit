2f8ccbeb0d0a5fd6c017b392cf9ad48ce5b03edf
Joseph Pecoraro <pecoraro@apple.com>
2016-10-18 20:21:02 +0000

    Web Inspector: CSS Autocompletion sometimes adds extra unexpected characters https://bugs.webkit.org/show_bug.cgi?id=163612 <rdar://problem/28829557>
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2016-10-18
    Reviewed by Timothy Hatcher.
    
    * UserInterface/Controllers/CodeMirrorCompletionController.js:
    (WebInspector.CodeMirrorCompletionController.prototype._generateCSSCompletions):
    Better handle completions in cases where we are in the middle of a property
    to avoid orphaned characters, or at the end of a function name to avoid creating
    duplicate parenthesis.
    
    Canonical link: https://commits.webkit.org/181394@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@207481 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Controllers/CodeMirrorCompletionController.js
+++ Source/WebInspectorUI/UserInterface/Controllers/CodeMirrorCompletionController.js
@@ -537,6 +537,10 @@ WebInspector.CodeMirrorCompletionController = class CodeMirrorCompletionControll
         if (mainToken.state.state === "media" || mainToken.state.state === "top" || mainToken.state.state === "parens")
             return [];
 
+        // Don't complete in the middle of a property name.
+        if (/^[a-z]/i.test(suffix))
+            return [];
+
         var token = mainToken;
         var lineNumber = this._lineNumber;
 
@@ -561,14 +565,24 @@ WebInspector.CodeMirrorCompletionController = class CodeMirrorCompletionControll
             var propertyName = token.string;
 
             // If there is a suffix and it isn't a semicolon, then we should use a space since
-            // the user is editing in the middle.
-            this._implicitSuffix = suffix && suffix !== ";" ? " " : (this._noEndingSemicolon ? "" : ";");
+            // the user is editing in the middle. Likewise if the suffix starts with an open
+            // paren we are changing a function name so don't add a suffix.
+            this._implicitSuffix = " ";
+            if (suffix === ";")
+                this._implicitSuffix = this._noEndingSemicolon ? "" : ";";
+            else if (suffix.startsWith("("))
+                this._implicitSuffix = "";
 
             // Don't use an implicit suffix if it would be the same as the existing suffix.
             if (this._implicitSuffix === suffix)
                 this._implicitSuffix = "";
 
-            return WebInspector.CSSKeywordCompletions.forProperty(propertyName).startsWith(this._prefix);
+            let completions = WebInspector.CSSKeywordCompletions.forProperty(propertyName).startsWith(this._prefix);
+
+            if (suffix.startsWith("("))
+                completions = completions.map((x) => x.replace(/\(\)$/, ""));
+
+            return completions;
         }
 
         this._implicitSuffix = suffix !== ":" ? ": " : "";
