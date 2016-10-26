3022b91f56ebc11ee6820afc928060f445ed9977
Nan Wang <n_wang@apple.com>
2016-10-26 01:15:36 +0000

    LayoutTest accessibility/mac/meter-gauge-value-description.html failing https://bugs.webkit.org/show_bug.cgi?id=163997 <rdar://problem/28948637>
    Reviewed by Chris Fleizach.
    
    Source/WebCore:
    
    I hand-edited the Localizable.strings file in a previous patch and my changes
    were replaced by running update-webkit-localizable-strings script. Changed the
    key to be the same as the value to keep consistency with other entries.
    
    Re-enabled the failing test.
    
    * English.lproj/Localizable.strings:
    * platform/LocalizedStrings.cpp:
    (WebCore::AXMeterGaugeRegionOptimumText):
    (WebCore::AXMeterGaugeRegionSuboptimalText):
    (WebCore::AXMeterGaugeRegionLessGoodText):
    
    LayoutTests:
    
    * platform/mac/TestExpectations:
    
    
    Canonical link: https://commits.webkit.org/181717@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@207860 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/LocalizedStrings.cpp
+++ Source/WebCore/platform/LocalizedStrings.cpp
@@ -789,17 +789,17 @@ String AXMarkText()
 #if ENABLE(METER_ELEMENT)
 String AXMeterGaugeRegionOptimumText()
 {
-    return WEB_UI_STRING("optimum", "The optimum value description for a meter element.");
+    return WEB_UI_STRING("optimal value", "The optimum value description for a meter element.");
 }
 
 String AXMeterGaugeRegionSuboptimalText()
 {
-    return WEB_UI_STRING("suboptimal", "The suboptimal value description for a meter element.");
+    return WEB_UI_STRING("suboptimal value", "The suboptimal value description for a meter element.");
 }
 
 String AXMeterGaugeRegionLessGoodText()
 {
-    return WEB_UI_STRING("less good", "The less good value description for a meter element.");
+    return WEB_UI_STRING("critical value", "The less good value description for a meter element.");
 }
 #endif // ENABLE(METER_ELEMENT)
 #endif // PLATFORM(COCOA)
