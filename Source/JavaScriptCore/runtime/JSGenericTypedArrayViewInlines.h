edffe3b0e738161295bc709cb924f05b951d85ba
Mark Lam <mark.lam@apple.com>
2016-10-26 19:23:55 +0000

    JSGenericTypedArrayView::set() should check for exceptions. https://bugs.webkit.org/show_bug.cgi?id=164007 <rdar://problem/28853775>
    Reviewed by Filip Pizlo.
    
    JSTests:
    
    * stress/typed-array-view-set-should-not-crash-on-exception.js: Added.
    
    Source/JavaScriptCore:
    
    * runtime/JSGenericTypedArrayViewInlines.h:
    (JSC::JSGenericTypedArrayView<Adaptor>::set):
    
    
    
    Canonical link: https://commits.webkit.org/181759@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@207906 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/JSGenericTypedArrayViewInlines.h
+++ Source/JavaScriptCore/runtime/JSGenericTypedArrayViewInlines.h
@@ -242,6 +242,9 @@ template<typename Adaptor>
 bool JSGenericTypedArrayView<Adaptor>::set(
     ExecState* exec, unsigned offset, JSObject* object, unsigned objectOffset, unsigned length, CopyType type)
 {
+    VM& vm = exec->vm();
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
     const ClassInfo* ci = object->classInfo();
     if (ci->typedArrayStorageType == Adaptor::typeValue) {
         // The super fast case: we can just memcpy since we're the same type.
@@ -249,7 +252,9 @@ bool JSGenericTypedArrayView<Adaptor>::set(
         length = std::min(length, other->length());
         
         RELEASE_ASSERT(other->canAccessRangeQuickly(objectOffset, length));
-        if (!validateRange(exec, offset, length))
+        bool success = validateRange(exec, offset, length);
+        ASSERT(!scope.exception() == success);
+        if (!success)
             return false;
 
         memmove(typedVector() + offset, other->typedVector() + objectOffset, length * elementSize);
@@ -286,13 +291,18 @@ bool JSGenericTypedArrayView<Adaptor>::set(
             exec, offset, jsCast<JSFloat64Array*>(object), objectOffset, length, type);
     case NotTypedArray:
     case TypeDataView: {
-        if (!validateRange(exec, offset, length))
+        bool success = validateRange(exec, offset, length);
+        ASSERT(!scope.exception() == success);
+        if (!success)
             return false;
 
         // We could optimize this case. But right now, we don't.
         for (unsigned i = 0; i < length; ++i) {
             JSValue value = object->get(exec, i + objectOffset);
-            if (!setIndex(exec, offset + i, value))
+            RETURN_IF_EXCEPTION(scope, false);
+            bool success = setIndex(exec, offset + i, value);
+            ASSERT(!scope.exception() || !success);
+            if (!success)
                 return false;
         }
         return true;
