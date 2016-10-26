ee522d0a8b14091519b6d3e82aec45111fb69ab5
Antoine Quint <graouts@apple.com>
2016-10-26 20:26:00 +0000

    [Modern Media Controls] Media Controller: playback support https://bugs.webkit.org/show_bug.cgi?id=163678 <rdar://problem/27989479>
    Patch by Antoine Quint <graouts@apple.com> on 2016-10-26
    Reviewed by Dean Jackson.
    
    Source/WebCore:
    
    We introduce the PlaybackSupport class which brings support for playing and pausing
    the media by clicking on the play/pause button in the media controls and correctly
    reflecting the media's playback state as the media is played or paused via the media API.
    
    * Modules/modern-media-controls/js-files:
    * Modules/modern-media-controls/media/media-controller.js:
    (MediaController):
    * Modules/modern-media-controls/media/playback-support.js: Copied from Source/WebCore/Modules/modern-media-controls/media/media-controller.js.
    (PlaybackSupport.prototype.get control):
    (PlaybackSupport.prototype.get mediaEvents):
    (PlaybackSupport.prototype.buttonWasClicked):
    (PlaybackSupport.prototype.syncControl):
    (PlaybackSupport):
    * WebCore.xcodeproj/project.pbxproj:
    
    LayoutTests:
    
    New tests for the PlaybackSupport class and adding the new resource to other MediaController tests.
    
    * http/tests/media/modern-media-controls/skip-back-support/skip-back-support-button-click.html:
    * media/modern-media-controls/elapsed-time-support/elapsed-time-support.html:
    * media/modern-media-controls/media-controller/media-controller-constructor.html:
    * media/modern-media-controls/media-controller/media-controller-resize.html:
    * media/modern-media-controls/mute-support/mute-support-button-click.html:
    * media/modern-media-controls/mute-support/mute-support-media-api.html:
    * media/modern-media-controls/mute-support/mute-support-muted.html:
    * media/modern-media-controls/playback-support/playback-support-autoplay-expected.txt: Added.
    * media/modern-media-controls/playback-support/playback-support-autoplay.html: Copied from LayoutTests/media/modern-media-controls/elapsed-time-support/elapsed-time-support.html.
    * media/modern-media-controls/playback-support/playback-support-button-click-expected.txt: Added.
    * media/modern-media-controls/playback-support/playback-support-button-click.html: Copied from LayoutTests/media/modern-media-controls/remaining-time-support/remaining-time-support.html.
    * media/modern-media-controls/playback-support/playback-support-media-api-expected.txt: Added.
    * media/modern-media-controls/playback-support/playback-support-media-api.html: Copied from LayoutTests/media/modern-media-controls/remaining-time-support/remaining-time-support.html.
    * media/modern-media-controls/remaining-time-support/remaining-time-support.html:
    * media/modern-media-controls/start-support/start-support-audio.html:
    * media/modern-media-controls/start-support/start-support-autoplay.html:
    * media/modern-media-controls/start-support/start-support-click-to-start.html:
    * media/modern-media-controls/start-support/start-support-error.html:
    * media/modern-media-controls/start-support/start-support-fullscreen.html:
    * media/modern-media-controls/start-support/start-support-manual-play.html:
    * media/modern-media-controls/start-support/start-support-no-source.html:
    
    Canonical link: https://commits.webkit.org/181762@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@207909 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/modern-media-controls/media/playback-support.js
+++ Source/WebCore/Modules/modern-media-controls/media/playback-support.js
@@ -23,56 +23,33 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-class MediaController
+class PlaybackSupport extends MediaControllerSupport
 {
-
-    constructor(shadowRoot, media, host)
-    {
-        this.shadowRoot = shadowRoot;
-        this.media = media;
-        this.host = host;
-
-        // FIXME: This should get set dynamically based on the current environment.
-        this.layoutTraits = LayoutTraits.macOS;
-
-        this.controls = new MacOSInlineMediaControls
-        shadowRoot.appendChild(this.controls.element);        
-
-        new ElapsedTimeSupport(this);
-        new MuteSupport(this);
-        new PlaybackSupport(this);
-        new RemainingTimeSupport(this);
-        new SkipBackSupport(this);
-        new StartSupport(this);
-
-        this._updateControlsSize();
-        media.addEventListener("resize", this);
-    }
 
     // Protected
 
-    set pageScaleFactor(pageScaleFactor)
+    get control()
     {
-        // FIXME: To be implemented.
+        return this.mediaController.controls.playPauseButton;
     }
 
-    set usesLTRUserInterfaceLayoutDirection(flag)
+    get mediaEvents()
     {
-        // FIXME: To be implemented.
+        return ["play", "pause"];
     }
 
-    handleEvent(event)
+    buttonWasClicked(control)
     {
-        if (event.type === "resize" && event.currentTarget === this.media)
-            this._updateControlsSize();
+        const media = this.mediaController.media;
+        if (media.paused)
+            media.play();
+        else
+            media.pause();
     }
 
-    // Private
-
-    _updateControlsSize()
+    syncControl()
     {
-        this.controls.width = this.media.offsetWidth;
-        this.controls.height = this.media.offsetHeight;
+        this.control.playing = !this.mediaController.media.paused;
     }
 
 }
