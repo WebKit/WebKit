c8c4fcc86e3dcce79053031b7b651e35e851caa9
Manuel Rego Casasnovas <rego@igalia.com>
2016-10-27 09:34:40 +0000

    Merge r207402 - [css-grid] Disable CSS Grid Layout runtime flag by default https://bugs.webkit.org/show_bug.cgi?id=163432
    Reviewed by Darin Adler.
    
    It was enabled in r201042, but now Safari Technology Preview
    has a UI to switch runtime flags so it doesn't need to be enabled
    by default anymore.
    
    * Shared/WebPreferencesDefinitions.h: Disable grid layout runtime flag
    by default.

--- Source/WebKit2/Shared/WebPreferencesDefinitions.h
+++ Source/WebKit2/Shared/WebPreferencesDefinitions.h
@@ -286,7 +286,7 @@
 // - They should be false by default, although they are currently set to true while we develop client UI.
 
 #define FOR_EACH_WEBKIT_EXPERIMENTAL_FEATURE_PREFERENCE(macro) \
-    macro(CSSGridLayoutEnabled, cssGridLayoutEnabled, Bool, bool, true, "CSS Grid", "CSS Grid Layout Module support") \
+    macro(CSSGridLayoutEnabled, cssGridLayoutEnabled, Bool, bool, false, "CSS Grid", "CSS Grid Layout Module support") \
     macro(CustomElementsEnabled, customElementsEnabled, Bool, bool, false, "Custom Elements", "HTML Custom Elements prototype") \
     macro(GamepadsEnabled, gamepadsEnabled, Bool, bool, false, "Gamepads", "Web Gamepad API support") \
     macro(SpringTimingFunctionEnabled, springTimingFunctionEnabled, Bool, bool, true, "CSS Spring Animations", "CSS Spring Animation prototype") \
