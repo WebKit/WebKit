45434d97b0a7ef9b69b9ceca75954b27d3c334ed
Fujii Hironori <Hironori.Fujii@sony.com>
2016-10-28 12:24:35 +0000

    [CMake] Changing enabled features should trigger recompiling all IDL https://bugs.webkit.org/show_bug.cgi?id=164121
    Patch by Fujii Hironori <Hironori.Fujii@sony.com> on 2016-10-28
    Reviewed by Michael Catanzaro.
    
    Changing enabled features should trigger recompiling all IDL
    because some of them use #if.  generate-bindings-all.pl was
    introduced in <http://trac.webkit.org/changeset/207617>.  Before
    this change, updating supplemental_dependency.tmp triggered
    recompiling all IDL. Changing enabled features usually involves
    adding or removing IDL files. As the result, all IDL would be
    recompiled.  After the change, adding or removing IDL does not
    trigger recompiling all IDL. So, we need to explicitly trigger
    recompiling all IDL if enabled features are changed.
    
    * Source/cmake/WebKitMacros.cmake(GENERATE_BINDINGS): Added
    cmakeconfig.h to extra dependencies.
    
    Canonical link: https://commits.webkit.org/181830@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@208044 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/WebKitMacros.cmake
+++ Source/cmake/WebKitMacros.cmake
@@ -106,6 +106,9 @@ function(GENERATE_BINDINGS target)
     set(common_generator_dependencies
         ${WEBCORE_DIR}/bindings/scripts/generate-bindings.pl
         ${SCRIPTS_BINDINGS}
+        # Changing enabled features should trigger recompiling all IDL files
+        # because some of them use #if.
+        ${CMAKE_BINARY_DIR}/cmakeconfig.h
     )
     if (EXISTS ${WEBCORE_DIR}/bindings/scripts/CodeGenerator${arg_GENERATOR}.pm)
         list(APPEND common_generator_dependencies ${WEBCORE_DIR}/bindings/scripts/CodeGenerator${arg_GENERATOR}.pm)
