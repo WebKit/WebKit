6c5ad9c28115dfabf928e2325e50bb7dcb84fd0d
Mark Lam <mark.lam@apple.com>
2016-10-31 23:18:04 +0000

    Add $vm.codeBlockFor() debugging utility. https://bugs.webkit.org/show_bug.cgi?id=164192
    Reviewed by Saam Barati.
    
    Sometimes, while debugging, it would be nice if we can get the codeBlock info for
    a function.  Amongst other uses, this is useful for checking what optimization
    level the function is currently at.  With $vm.codeBlockFor(), we can now do this.
    For example, when JS_useDollarVM=true:
    
        print("test's codeBlock = " + $vm.codeBlockFor(test)); // prints function test's codeBlock.
    
    Also added some comments to clarify some pre-existing code.
    
    * tools/JSDollarVMPrototype.cpp:
    (JSC::functionCodeBlockForFrame):
    (JSC::codeBlockFromArg):
    (JSC::functionCodeBlockFor):
    (JSC::JSDollarVMPrototype::finishCreation):
    
    
    
    Canonical link: https://commits.webkit.org/181965@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@208189 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/tools/JSDollarVMPrototype.cpp
+++ Source/JavaScriptCore/tools/JSDollarVMPrototype.cpp
@@ -267,7 +267,10 @@ static EncodedJSValue JSC_HOST_CALL functionCodeBlockForFrame(ExecState* exec)
     // function.
     unsigned frameNumber = value.asUInt32() + 1;
     CodeBlock* codeBlock = JSDollarVMPrototype::codeBlockForFrame(exec, frameNumber);
-    return JSValue::encode(JSValue(bitwise_cast<double>(reinterpret_cast<uint64_t>(codeBlock))));
+    // Though CodeBlock is a JSCell, it is not safe to return it directly back to JS code
+    // as it is an internal type that the JS code cannot handle. Hence, we first encode the
+    // CodeBlock* as a double token (which is safe for JS code to handle) before returning it.
+    return JSValue::encode(JSValue(bitwise_cast<double>(static_cast<uint64_t>(reinterpret_cast<uintptr_t>(codeBlock)))));
 }
 
 static CodeBlock* codeBlockFromArg(ExecState* exec)
@@ -276,19 +279,41 @@ static CodeBlock* codeBlockFromArg(ExecState* exec)
         return nullptr;
 
     JSValue value = exec->uncheckedArgument(0);
-    if (!value.isDouble()) {
-        dataLog("Invalid codeBlock: ", value, "\n");
-        return nullptr;
+    CodeBlock* candidateCodeBlock = nullptr;
+    if (value.isCell()) {
+        JSFunction* func = jsDynamicCast<JSFunction*>(value.asCell());
+        if (func) {
+            if (func->isHostFunction())
+                candidateCodeBlock = nullptr;
+            else
+                candidateCodeBlock = func->jsExecutable()->eitherCodeBlock();
+        }
+    } else if (value.isDouble()) {
+        // If the value is a double, it may be an encoded CodeBlock* that came from
+        // $vm.codeBlockForFrame(). We'll treat it as a candidate codeBlock and check if it's
+        // valid below before using.
+        candidateCodeBlock = reinterpret_cast<CodeBlock*>(bitwise_cast<uint64_t>(value.asDouble()));
     }
 
-    CodeBlock* codeBlock = reinterpret_cast<CodeBlock*>(bitwise_cast<uint64_t>(value.asDouble()));
-    if (JSDollarVMPrototype::isValidCodeBlock(exec, codeBlock))
-        return codeBlock;
+    if (candidateCodeBlock && JSDollarVMPrototype::isValidCodeBlock(exec, candidateCodeBlock))
+        return candidateCodeBlock;
 
-    dataLogF("Invalid codeBlock: %p ", codeBlock);
-    dataLog(value, "\n");
+    if (candidateCodeBlock)
+        dataLog("Invalid codeBlock: ", RawPointer(candidateCodeBlock), " ", value, "\n");
+    else
+        dataLog("Invalid codeBlock: ", value, "\n");
     return nullptr;
-    
+}
+
+static EncodedJSValue JSC_HOST_CALL functionCodeBlockFor(ExecState* exec)
+{
+    CodeBlock* codeBlock = codeBlockFromArg(exec);
+    WTF::StringPrintStream stream;
+    if (codeBlock) {
+        stream.print(*codeBlock);
+        return JSValue::encode(jsString(exec, stream.toString()));
+    }
+    return JSValue::encode(jsUndefined());
 }
 
 static EncodedJSValue JSC_HOST_CALL functionPrintSourceFor(ExecState* exec)
@@ -432,6 +457,7 @@ void JSDollarVMPrototype::finishCreation(VM& vm, JSGlobalObject* globalObject)
     addFunction(vm, globalObject, "gc", functionGC, 0);
     addFunction(vm, globalObject, "edenGC", functionEdenGC, 0);
     
+    addFunction(vm, globalObject, "codeBlockFor", functionCodeBlockFor, 1);
     addFunction(vm, globalObject, "codeBlockForFrame", functionCodeBlockForFrame, 1);
     addFunction(vm, globalObject, "printSourceFor", functionPrintSourceFor, 1);
     addFunction(vm, globalObject, "printByteCodeFor", functionPrintByteCodeFor, 1);
