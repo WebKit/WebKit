dddac66180c906d8b8a5074df65df1d61d0d889c
Geoffrey Garen <ggaren@apple.com>
2016-11-01 18:38:08 +0000

    Lots of stderr logging in JSManagedValue https://bugs.webkit.org/show_bug.cgi?id=164279 <rdar://problem/28949886>
    Reviewed by Filip Pizlo.
    
    Revert an accidental change in <https://trac.webkit.org/changeset/205462>.
    
    * API/JSManagedValue.mm:
    (-[JSManagedValue initWithValue:]):
    
    
    Canonical link: https://commits.webkit.org/182001@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@208227 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/API/JSManagedValue.mm
+++ Source/JavaScriptCore/API/JSManagedValue.mm
@@ -213,7 +213,6 @@ private:
     m_owners = [[NSMapTable alloc] initWithKeyOptions:weakIDOptions valueOptions:integerOptions capacity:1];
 
     JSC::JSValue jsValue = toJS(exec, [value JSValueRef]);
-    dataLog("Creating managed value with value ", jsValue, "\n");
     if (jsValue.isObject())
         m_weakValue.setObject(JSC::jsCast<JSC::JSObject*>(jsValue.asCell()), self);
     else if (jsValue.isString())
