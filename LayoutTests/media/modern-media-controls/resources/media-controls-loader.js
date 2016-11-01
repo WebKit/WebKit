0bd4b9afa3a4efb93207d77863380c8ac1a49994
Antoine Quint <graouts@apple.com>
2016-11-01 18:25:16 +0000

    [Modern Media Controls] load all media controller scripts and styles automatically https://bugs.webkit.org/show_bug.cgi?id=164271
    Patch by Antoine Quint <graouts@apple.com> on 2016-11-01
    Reviewed by Dean Jackson.
    
    We now load all media controller assets with the inclusion of a single script,
    obtaining the list of JS files to include through the same file used to build
    the modern-media-controls module, so that we don't need to specify this in two places.
    
    * http/tests/media/modern-media-controls/skip-back-support/skip-back-support-button-click.html:
    * media/modern-media-controls/elapsed-time-support/elapsed-time-support.html:
    * media/modern-media-controls/media-controller/media-controller-constructor.html:
    * media/modern-media-controls/media-controller/media-controller-resize.html:
    * media/modern-media-controls/mute-support/mute-support-button-click.html:
    * media/modern-media-controls/mute-support/mute-support-media-api.html:
    * media/modern-media-controls/mute-support/mute-support-muted.html:
    * media/modern-media-controls/playback-support/playback-support-autoplay.html:
    * media/modern-media-controls/playback-support/playback-support-button-click.html:
    * media/modern-media-controls/playback-support/playback-support-media-api.html:
    * media/modern-media-controls/remaining-time-support/remaining-time-support.html:
    * media/modern-media-controls/resources/media-controls-loader.js: Added.
    * media/modern-media-controls/scrubber-support/scrubber-support-click.html:
    * media/modern-media-controls/scrubber-support/scrubber-support-drag.html:
    * media/modern-media-controls/scrubber-support/scrubber-support-media-api.html:
    * media/modern-media-controls/start-support/start-support-audio.html:
    * media/modern-media-controls/start-support/start-support-autoplay.html:
    * media/modern-media-controls/start-support/start-support-click-to-start.html:
    * media/modern-media-controls/start-support/start-support-error.html:
    * media/modern-media-controls/start-support/start-support-fullscreen.html:
    * media/modern-media-controls/start-support/start-support-manual-play.html:
    * media/modern-media-controls/start-support/start-support-no-source.html:
    * media/modern-media-controls/volume-support/volume-support-click.html:
    * media/modern-media-controls/volume-support/volume-support-drag.html:
    * media/modern-media-controls/volume-support/volume-support-media-api-mute.html:
    * media/modern-media-controls/volume-support/volume-support-media-api.html:
    
    Canonical link: https://commits.webkit.org/182000@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@208226 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/media/modern-media-controls/resources/media-controls-loader.js
@@ -0,0 +1,19 @@
+
+(function() {
+    const layoutTestsPath = window.location.href.substr(0, window.location.href.indexOf("/LayoutTests/"));
+    const modulePath = layoutTestsPath ? layoutTestsPath + "/Source/WebCore/Modules/modern-media-controls" : "/modern-media-controls";
+
+    ["media-controls", "scrubber", "volume-slider", "slider", "button", "start-button", "icon-button", "airplay-button", "time-label", "macos-inline-media-controls", "buttons-container", "placard"].forEach(cssFile => {
+        document.write(`<link rel="stylesheet" type="text/css" href="${modulePath}/controls/${cssFile}.css">`);
+    });
+
+    const request = new XMLHttpRequest;
+    request.open("GET", `${modulePath}/js-files`, false);
+    request.send();
+
+    request.responseText.trim().split("\n").forEach(jsFile => {
+        document.write(`<script type="text/javascript" src="${modulePath}/${jsFile}"></script>`);
+    });
+
+    document.write(`<script type="text/javascript">iconService.directoryPath = "${modulePath}/images";</script>`);
+})();
