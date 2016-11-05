481aedd815f079ba2be3d2b3e0f372c4bc72c02f
Mark Lam <mark.lam@apple.com>
2016-11-05 00:48:01 +0000

    Error description code should be able to handle Symbol values. https://bugs.webkit.org/show_bug.cgi?id=164436 <rdar://problem/29115583>
    Reviewed by Filip Pizlo and Saam Barati.
    
    JSTests:
    
    * stress/error-description-on-symbols-should-not-crash.js: Added.
    
    Source/JavaScriptCore:
    
    Previously, we try to toString() the Symbol value, resulting in it throwing an
    exception in errorDescriptionForValue() which breaks the invariant that
    errorDescriptionForValue() should not throw.
    
    We fixed this by making errorDescriptionForValue() aware of the Symbol type, and
    not so a toString() on Symbol values.  Also fixed notAFunctionSourceAppender()
    to build a nicer message for Symbol values.
    
    * runtime/ExceptionHelpers.cpp:
    (JSC::errorDescriptionForValue):
    (JSC::notAFunctionSourceAppender):
    
    
    
    Canonical link: https://commits.webkit.org/182147@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@208410 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/ExceptionHelpers.cpp
+++ Source/JavaScriptCore/runtime/ExceptionHelpers.cpp
@@ -86,6 +86,8 @@ JSString* errorDescriptionForValue(ExecState* exec, JSValue v)
 {
     if (v.isString())
         return jsNontrivialString(exec, makeString('"',  asString(v)->value(exec), '"'));
+    if (v.isSymbol())
+        return jsNontrivialString(exec, asSymbol(v)->descriptiveString());
     if (v.isObject()) {
         CallData callData;
         JSObject* object = asObject(v);
@@ -182,9 +184,13 @@ static String notAFunctionSourceAppender(const String& originalMessage, const St
     builder.appendLiteral("', '");
     builder.append(base);
     builder.appendLiteral("' is ");
-    if (type == TypeObject)
-        builder.appendLiteral("an instance of ");
-    builder.append(displayValue);
+    if (type == TypeSymbol)
+        builder.appendLiteral("a Symbol");
+    else {
+        if (type == TypeObject)
+            builder.appendLiteral("an instance of ");
+        builder.append(displayValue);
+    }
     builder.append(')');
 
     return builder.toString();
