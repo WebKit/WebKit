c1144f6cbaa0db5f9d6540d3f99f14702d825fb1
Antoine Quint <graouts@webkit.org>
2016-11-09 19:31:25 +0000

    [Modern Media Controls] UI Library: iOS inline controls https://bugs.webkit.org/show_bug.cgi?id=164513 <rdar://problem/27989475>
    Reviewed by Dean Jackson.
    
    We introduce a new IOSInlineMediaControls class which can be used to instantiate media controls
    for inline playback on iOS.
    
    Tests: media/modern-media-controls/ios-inline-media-controls/ios-inline-media-controls-buttons-styles.html
           media/modern-media-controls/ios-inline-media-controls/ios-inline-media-controls-constructor.html
           media/modern-media-controls/ios-inline-media-controls/ios-inline-media-controls-controls-bar-styles.html
           media/modern-media-controls/ios-inline-media-controls/ios-inline-media-controls-layout.html
           media/modern-media-controls/ios-inline-media-controls/ios-inline-media-controls-time-control-styles.html
           media/modern-media-controls/ios-inline-media-controls/ios-inline-media-dropping-controls.html
    
    * Modules/modern-media-controls/controls/ios-inline-media-controls.css: Added.
    (.media-controls.ios.inline > .controls-bar):
    (.media-controls.ios.inline .time-control):
    (.media-controls.ios.inline button):
    (.media-controls.ios.inline button:active):
    (.media-controls.ios.inline > .controls-bar button):
    (.media-controls.ios.inline .buttons-container.right):
    (.media-controls.ios.inline button.play-pause):
    (.media-controls.ios.inline button.skip-back):
    (.media-controls.ios.inline .scrubber.slider):
    (.media-controls.ios.inline button.airplay):
    (.media-controls.ios.inline button.pip):
    (.media-controls.ios.inline button.fullscreen):
    (.media-controls.ios.inline .time-label):
    (.media-controls.ios.inline .scrubber.slider > .fill):
    (.media-controls.ios.inline .scrubber.slider > input::-webkit-slider-thumb):
    * Modules/modern-media-controls/controls/ios-inline-media-controls.js: Added.
    (IOSInlineMediaControls.prototype.layout):
    (IOSInlineMediaControls):
    * Modules/modern-media-controls/images/iOS/slider-thumb@2x.png: Added.
    * Modules/modern-media-controls/js-files:
    * WebCore.xcodeproj/project.pbxproj:
    
    Canonical link: https://commits.webkit.org/182186@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@208454 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebCore/Modules/modern-media-controls/controls/ios-inline-media-controls.js
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2016 Apple Inc. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+class IOSInlineMediaControls extends MediaControls
+{
+
+    constructor(options = {})
+    {
+        options.layoutTraits = LayoutTraits.iOS;
+
+        super(options);
+
+        this.element.classList.add("ios");
+        this.element.classList.add("inline");
+
+        this.element.appendChild(this.startButton.element);
+
+        this._leftContainer = new ButtonsContainer({
+            buttons: [this.playPauseButton, this.skipBackButton],
+            cssClassName: "left",
+            padding: 24,
+            margin: 24,
+        });
+
+        this._rightContainer = new ButtonsContainer({
+            buttons: [this.airplayButton, this.pipButton, this.fullscreenButton],
+            cssClassName: "right",
+            padding: 24,
+            margin: 24
+        });
+
+        this.controlsBar.children = [this._leftContainer, this._rightContainer];
+    }
+
+    // Public
+
+    layout()
+    {
+        super.layout();
+
+        // Reset dropped buttons.
+        for (let button of this._rightContainer.buttons)
+            delete button.dropped;
+
+        this._leftContainer.layout();
+        this._rightContainer.layout();
+
+        this.timeControl.width = this.width - this._leftContainer.width - this._rightContainer.width;
+
+        if (this.timeControl.isSufficientlyWide) {
+            this.controlsBar.insertBefore(this.timeControl, this._rightContainer);
+            this.timeControl.x = this._leftContainer.width;
+        } else {
+            this.timeControl.remove();
+            // Since we don't have enough space to display the scrubber, we may also not have
+            // enough space to display all buttons in the left and right containers, so gradually drop them.
+            for (let control of [this.airplayButton, this.pipButton, this.skipBackButton, this.fullscreenButton]) {
+                // Nothing left to do if the combined container widths is shorter than the available width.
+                if (this._leftContainer.width + this._rightContainer.width < this.width)
+                    break;
+
+                // If the control was already not participating in layout, we can skip it.
+                if (!control.visible)
+                    continue;
+
+                // This control must now be dropped.
+                control.dropped = true;
+
+                this._leftContainer.layout();
+                this._rightContainer.layout();
+            }
+        }
+    }
+
+}
