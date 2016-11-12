fee64adcddecb62ad71a3ff0aaa9b7b21681706c
Joseph Pecoraro <pecoraro@apple.com>
2016-11-12 02:31:31 +0000

    Removed unused INCLUDE_OPTIONS_FOR_DEBUGGING https://bugs.webkit.org/show_bug.cgi?id=164664
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2016-11-11
    Reviewed by Dan Bernstein.
    
    * Scripts/debug-minibrowser:
    * Scripts/debug-safari:
    * Scripts/debug-test-runner:
    * Scripts/webkitdirs.pm:
    (printHelpAndExitForRunAndDebugWebKitAppIfNeeded):
    There are no debug specific options anymore.
    
    Canonical link: https://commits.webkit.org/182354@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@208635 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -107,7 +107,6 @@ use constant {
 };
 
 use constant USE_OPEN_COMMAND => 1; # Used in runMacWebKitApp().
-use constant INCLUDE_OPTIONS_FOR_DEBUGGING => 1;
 use constant SIMULATOR_DEVICE_STATE_SHUTDOWN => "1";
 use constant SIMULATOR_DEVICE_STATE_BOOTED => "3";
 use constant SIMULATOR_DEVICE_SUFFIX_FOR_WEBKIT_DEVELOPMENT  => "For WebKit Development";
@@ -2120,8 +2119,6 @@ sub setPathForRunningWebKitApp
 sub printHelpAndExitForRunAndDebugWebKitAppIfNeeded
 {
     return unless checkForArgumentAndRemoveFromARGV("--help");
-
-    my ($includeOptionsForDebugging) = @_;
 
     print STDERR <<EOF;
 Usage: @{[basename($0)]} [options] [args ...]
