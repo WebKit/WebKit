d381138879a67c32f12dc82c7e4ffd69c9140ca0
David Kilzer <ddkilzer@webkit.org>
2016-11-14 18:20:35 +0000

    Bug 164702: WebContent crash due to checked unsigned overflow in WebCore: WebCore::RenderLayerCompositor::requiresCompositingLayer const + 1104 <https://webkit.org/b/164702> <rdar://problem/29236368>
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    Test: inspector/layers/layers-compositing-reasons.html
    
    * rendering/RenderLayerCompositor.cpp:
    (WebCore::RenderLayerCompositor::requiresCompositingForCanvas):
    Don't composite if the canvas area overflows.
    
    LayoutTests:
    
    * inspector/layers/layers-compositing-reasons-expected.txt:
    Update results.
    * inspector/layers/layers-compositing-reasons.html: Update to
    reproduce the crash.  This does not reproduce the original crash
    stack, but does exercise the same crashing code.
    
    
    Canonical link: https://commits.webkit.org/182409@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@208691 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderLayerCompositor.cpp
+++ Source/WebCore/rendering/RenderLayerCompositor.cpp
@@ -2537,7 +2537,8 @@ bool RenderLayerCompositor::requiresCompositingForCanvas(RenderLayerModelObject&
         bool isCanvasLargeEnoughToForceCompositing = true;
 #else
         HTMLCanvasElement* canvas = downcast<HTMLCanvasElement>(renderer.element());
-        bool isCanvasLargeEnoughToForceCompositing = canvas->size().area().unsafeGet() >= canvasAreaThresholdRequiringCompositing;
+        auto canvasArea = canvas->size().area<RecordOverflow>();
+        bool isCanvasLargeEnoughToForceCompositing = !canvasArea.hasOverflowed() && canvasArea.unsafeGet() >= canvasAreaThresholdRequiringCompositing;
 #endif
         CanvasCompositingStrategy compositingStrategy = canvasCompositingStrategy(renderer);
         return compositingStrategy == CanvasAsLayerContents || (compositingStrategy == CanvasPaintedToLayer && isCanvasLargeEnoughToForceCompositing);
