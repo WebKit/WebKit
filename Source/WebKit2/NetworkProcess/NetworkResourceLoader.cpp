3f63c1253ccae2ec20704c8822f90edc20db881c
Alex Christensen <achristensen@apple.com>
2016-11-18 07:21:37 +0000

    REGRESSION: API test _WKDownload.ConvertResponseToDownload is a flaky timeout https://bugs.webkit.org/show_bug.cgi?id=164631
    Reviewed by Carlos Garcia Campos.
    
    * NetworkProcess/NetworkResourceLoader.cpp:
    (WebKit::NetworkResourceLoader::convertToDownload):
    I could reproduce the flaky failure 100% of the time when not using NETWORK_SESSION
    by adding a usleep(1000000) in NetworkResourceLoader::convertToDownload.  m_networkLoad
    was not being set to nullptr before it was being checked in NetworkResourceLoader::abort
    
    
    
    Canonical link: https://commits.webkit.org/182591@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@208880 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/NetworkProcess/NetworkResourceLoader.cpp
+++ Source/WebKit2/NetworkProcess/NetworkResourceLoader.cpp
@@ -283,7 +283,7 @@ void NetworkResourceLoader::cleanup()
 void NetworkResourceLoader::convertToDownload(DownloadID downloadID, const ResourceRequest& request, const ResourceResponse& response)
 {
     ASSERT(m_networkLoad);
-    NetworkProcess::singleton().downloadManager().convertNetworkLoadToDownload(downloadID, WTFMove(m_networkLoad), WTFMove(m_fileReferences), request, response);
+    NetworkProcess::singleton().downloadManager().convertNetworkLoadToDownload(downloadID, std::exchange(m_networkLoad, nullptr), WTFMove(m_fileReferences), request, response);
 }
 
 void NetworkResourceLoader::abort()
