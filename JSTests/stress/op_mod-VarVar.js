ec607914e0e66e5a5b51c9fb7cfbc8f30a7a57d7
Mark Lam <mark.lam@apple.com>
2016-11-20 03:29:44 +0000

    op_mod-* JSC tests needs a longer timeout too. https://bugs.webkit.org/show_bug.cgi?id=164994
    Reviewed by Yusuke Suzuki.
    
    * stress/op_mod-ConstVar.js:
    * stress/op_mod-VarConst.js:
    * stress/op_mod-VarVar.js:
    
    
    Canonical link: https://commits.webkit.org/182641@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@208932 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/op_mod-VarVar.js
+++ JSTests/stress/op_mod-VarVar.js
@@ -1,4 +1,4 @@
-//@ runFTLNoCJIT
+//@ runFTLNoCJIT("--timeoutMultiplier=1.5")
 
 // If all goes well, this test module will terminate silently. If not, it will print
 // errors. See binary-op-test.js for debugging options if needed.
