67b8c2962add26b0009f4fea38fec933a5192ae4
Philippe Normand <philn@igalia.com>
2016-11-21 09:29:22 +0000

    [WebRTC][OpenWebRTC] RTP bundling support https://bugs.webkit.org/show_bug.cgi?id=162333
    Reviewed by Alejandro G. Castro.
    
    Configure the OpenWebRTC transport agent bundle policy according
    to the RTCConfiguration and pass the receive SSRCs over to
    OpenWebRTC as well. Those are needed so the agent is aware of the
    bundle layout.
    
    * platform/mediastream/openwebrtc/MediaEndpointOwr.cpp:
    (WebCore::MediaEndpointOwr::updateReceiveConfiguration):
    (WebCore::MediaEndpointOwr::updateSendConfiguration):
    (WebCore::MediaEndpointOwr::ensureTransportAgentAndTransceivers):
    
    Canonical link: https://commits.webkit.org/182651@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@208942 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/mediastream/openwebrtc/MediaEndpointOwr.cpp
+++ Source/WebCore/platform/mediastream/openwebrtc/MediaEndpointOwr.cpp
@@ -37,6 +37,7 @@
 #include "MediaPayload.h"
 #include "NotImplemented.h"
 #include "OpenWebRTCUtilities.h"
+#include "PeerConnectionStates.h"
 #include "RTCDataChannelHandler.h"
 #include "RealtimeMediaSourceOwr.h"
 #include <owr/owr.h>
@@ -239,6 +240,7 @@ MediaEndpoint::UpdateResult MediaEndpointOwr::updateReceiveConfiguration(MediaEn
         owr_transport_agent_add_session(m_transportAgent, session);
     }
 
+    owr_transport_agent_start(m_transportAgent);
     m_numberOfReceivePreparedSessions = m_transceivers.size();
 
     return UpdateResult::Success;
@@ -313,6 +315,14 @@ MediaEndpoint::UpdateResult MediaEndpointOwr::updateSendConfiguration(MediaEndpo
 
         owr_media_session_set_send_payload(OWR_MEDIA_SESSION(session), sendPayload);
         owr_media_session_set_send_source(OWR_MEDIA_SESSION(session), source->mediaSource());
+
+        // FIXME: Support for group-ssrc SDP line is missing.
+        const Vector<unsigned> receiveSsrcs = mdesc.ssrcs;
+        if (receiveSsrcs.size()) {
+            g_object_set(session, "receive-ssrc", receiveSsrcs[0], nullptr);
+            if (receiveSsrcs.size() == 2)
+                g_object_set(session, "receive-rtx-ssrc", receiveSsrcs[1], nullptr);
+        }
 
         m_numberOfSendPreparedSessions = i + 1;
     }
@@ -559,7 +569,24 @@ void MediaEndpointOwr::ensureTransportAgentAndTransceivers(bool isInitiator, con
     ASSERT(m_dtlsCertificate);
 
     if (!m_transportAgent) {
-        m_transportAgent = owr_transport_agent_new(false);
+        // FIXME: Handle SDP BUNDLE line from the remote source instead of falling back to balanced.
+        OwrBundlePolicyType bundlePolicy = OWR_BUNDLE_POLICY_TYPE_BALANCED;
+
+        switch (m_configuration->bundlePolicy) {
+        case PeerConnectionStates::BundlePolicy::Balanced:
+            bundlePolicy = OWR_BUNDLE_POLICY_TYPE_BALANCED;
+            break;
+        case PeerConnectionStates::BundlePolicy::MaxCompat:
+            bundlePolicy = OWR_BUNDLE_POLICY_TYPE_MAX_COMPAT;
+            break;
+        case PeerConnectionStates::BundlePolicy::MaxBundle:
+            bundlePolicy = OWR_BUNDLE_POLICY_TYPE_MAX_BUNDLE;
+            break;
+        default:
+            ASSERT_NOT_REACHED();
+        };
+
+        m_transportAgent = owr_transport_agent_new(false, bundlePolicy);
 
         ASSERT(m_configuration);
         for (auto& server : m_configuration->iceServers) {
