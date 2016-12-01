b6297e6f4ed1d9b672d4f7321dd41066629e9a90
Per Arne Vollan <pvollan@apple.com>
2016-12-01 09:22:51 +0000

    [Win] Skip failing JSC tests. https://bugs.webkit.org/show_bug.cgi?id=165140
    Reviewed by Mark Lam.
    
    * ChakraCore.yaml:
    * microbenchmarks/tan.js:
    
    
    Canonical link: https://commits.webkit.org/182864@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@209178 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/microbenchmarks/tan.js
+++ JSTests/microbenchmarks/tan.js
@@ -1,4 +1,7 @@
-(function() {
+//@ skip if $hostOS == "windows"
+// FIXME: unskip this test when https://bugs.webkit.org/show_bug.cgi?id=165194 is fixed.
+
+(function () {
     for (var i = 0; i < 3000000; ++i)
         x = Math.tan(i);
 
