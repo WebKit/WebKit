63a0b538ec5f07a2a4b05a96463aef4c7cf295d6
Joseph Pecoraro <pecoraro@apple.com>
2016-12-05 20:32:36 +0000

    Web Inspector: Remove legacy styles https://bugs.webkit.org/show_bug.cgi?id=165389
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2016-12-05
    Reviewed by Matt Baker.
    
    Remove styles for platforms we don't build on anymore (Mavericks
    and Mountain Lion). There are also no "unknown-mac" styles to
    upgrade to a named platform.
    
    * UserInterface/Base/Platform.js:
    Add sierra and remove older unsupported platforms.
    
    * UserInterface/Views/Main.css:
    (body):
    (body:not(.mavericks)): Deleted.
    * UserInterface/Views/TabBar.css:
    (body.mavericks .tab-bar > .item:not(.disabled).selected): Deleted.
    * UserInterface/Views/Toolbar.css:
    (body .toolbar):
    (body.window-inactive .toolbar):
    (body.mac-platform:not(.docked) .toolbar):
    (body.mac-platform:not(.docked)):
    (body:not(.mavericks) .toolbar,): Deleted.
    (body.window-inactive:not(.mavericks) .toolbar): Deleted.
    (body.mac-platform:not(.docked, .mavericks) .toolbar): Deleted.
    (body.mac-platform:not(.docked, .mavericks)): Deleted.
    Remove mavericks specific styles.
    
    Canonical link: https://commits.webkit.org/183012@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@209342 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Base/Platform.js
+++ Source/WebInspectorUI/UserInterface/Base/Platform.js
@@ -44,21 +44,18 @@ WebInspector.Platform = {
         WebInspector.Platform.version.base = 10;
         WebInspector.Platform.version.release = parseInt(osVersionMatch[2]);
         switch (osVersionMatch[2]) {
-            case "11":
-                WebInspector.Platform.version.name = "el-capitan";
-                break;
-            case "10":
-                WebInspector.Platform.version.name = "yosemite";
-                break;
-            case "9":
-                WebInspector.Platform.version.name = "mavericks";
-                break;
-            case "8":
-                WebInspector.Platform.version.name = "mountain-lion";
-                break;
-            default:
-                WebInspector.Platform.version.name = "unknown-mac";
-                break;
+        case "12":
+            WebInspector.Platform.version.name = "sierra";
+            break;
+        case "11":
+            WebInspector.Platform.version.name = "el-capitan";
+            break;
+        case "10":
+            WebInspector.Platform.version.name = "yosemite";
+            break;
+        default:
+            WebInspector.Platform.version.name = "unknown-mac";
+            break;
         }
     }
 })();
