eaac2665af042d740bd2301e175920ad44e05983
Tomáš Popela <tpopela@redhat.com>
2016-12-06 12:16:06 +0000

    [GTK] Process accelerated compositing env variables only if they are really enabled https://bugs.webkit.org/show_bug.cgi?id=165300
    Don't process the WEBKIT_FORCE_COMPOSITING_MODE and
    WEBKIT_DISABLE_COMPOSITING_MODE env variables if they are disabled
    (eg. WEBKIT_DISABLE_COMPOSITING_MODE=0).
    
    Reviewed by Michael Catanzaro.
    
    * UIProcess/gtk/WebPreferencesGtk.cpp:
    (WebKit::WebPreferences::platformInitializeStore):
    
    Canonical link: https://commits.webkit.org/183058@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@209391 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/gtk/WebPreferencesGtk.cpp
+++ Source/WebKit2/UIProcess/gtk/WebPreferencesGtk.cpp
@@ -47,11 +47,11 @@ void WebPreferences::platformInitializeStore()
 #if USE(COORDINATED_GRAPHICS_THREADED)
     setForceCompositingMode(true);
 #else
-    if (getenv("WEBKIT_FORCE_COMPOSITING_MODE"))
+    if (g_strcmp0(getenv("WEBKIT_FORCE_COMPOSITING_MODE"), "0"))
         setForceCompositingMode(true);
 #endif
 
-    if (getenv("WEBKIT_DISABLE_COMPOSITING_MODE")) {
+    if (g_strcmp0(getenv("WEBKIT_DISABLE_COMPOSITING_MODE"), "0")) {
         setAcceleratedCompositingEnabled(false);
         return;
     }
