4eee6d157212f96b3ebb33f5788a480d33c88b55
Keith Miller <keith_miller@apple.com>
2016-12-09 01:34:05 +0000

    Add 64-bit signed LEB decode method https://bugs.webkit.org/show_bug.cgi?id=165630
    Reviewed by Ryosuke Niwa.
    
    Source/WTF:
    
    Add int64 LEB decode and fix some 64-bit specific issues
    with the decoder. There is also a fix where we would allow
    LEBs with canonical length + 1 size that is fixed by this
    patch.
    
    * wtf/LEBDecoder.h:
    (WTF::LEBDecoder::decodeUInt):
    (WTF::LEBDecoder::decodeInt):
    (WTF::LEBDecoder::decodeUInt32):
    (WTF::LEBDecoder::decodeUInt64):
    (WTF::LEBDecoder::decodeInt32):
    (WTF::LEBDecoder::decodeInt64):
    
    Tools:
    
    Add tests for LEB int64 and uint64 decode since I guess
    I forgot to add tests before.
    
    * TestWebKitAPI/Tests/WTF/LEBDecoder.cpp:
    (TestWebKitAPI::testUInt32LEBDecode):
    (TestWebKitAPI::TEST):
    (TestWebKitAPI::testUInt64LEBDecode):
    (TestWebKitAPI::testInt32LEBDecode):
    (TestWebKitAPI::testInt64LEBDecode):
    (TestWebKitAPI::testUnsignedLEBDecode): Deleted.
    
    Canonical link: https://commits.webkit.org/183245@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@209586 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/LEBDecoder.h
+++ Source/WTF/wtf/LEBDecoder.h
@@ -34,17 +34,19 @@
 
 namespace WTF { namespace LEBDecoder {
 
-template<size_t maxByteLength, typename T>
+template<typename T>
 inline bool WARN_UNUSED_RETURN decodeUInt(const uint8_t* bytes, size_t length, size_t& offset, T& result)
 {
+    const size_t numBits = sizeof(T) * CHAR_BIT;
+    const size_t maxByteLength = (numBits - 1) / 7 + 1; // numBits / 7 rounding up.
     if (length <= offset)
         return false;
     result = 0;
     unsigned shift = 0;
-    size_t last = std::min(maxByteLength, length - offset - 1);
+    size_t last = std::min(maxByteLength, length - offset) - 1;
     for (unsigned i = 0; true; ++i) {
         uint8_t byte = bytes[offset++];
-        result |= (byte & 0x7f) << shift;
+        result |= static_cast<T>(byte & 0x7f) << shift;
         shift += 7;
         if (!(byte & 0x80))
             return true;
@@ -55,30 +57,20 @@ inline bool WARN_UNUSED_RETURN decodeUInt(const uint8_t* bytes, size_t length, s
     return true;
 }
 
-const size_t max32BitLEBByteLength = 5;
-const size_t max64BitLEBByteLength = 10;
-
-inline bool WARN_UNUSED_RETURN decodeUInt32(const uint8_t* bytes, size_t length, size_t& offset, uint32_t& result)
+template<typename T>
+inline bool WARN_UNUSED_RETURN decodeInt(const uint8_t* bytes, size_t length, size_t& offset, T& result)
 {
-    return decodeUInt<max32BitLEBByteLength, uint32_t>(bytes, length, offset, result);
-}
-
-inline bool WARN_UNUSED_RETURN decodeUInt64(const uint8_t* bytes, size_t length, size_t& offset, uint64_t& result)
-{
-    return decodeUInt<max64BitLEBByteLength, uint64_t>(bytes, length, offset, result);
-}
-
-inline bool WARN_UNUSED_RETURN decodeInt32(const uint8_t* bytes, size_t length, size_t& offset, int32_t& result)
-{
+    const size_t numBits = sizeof(T) * CHAR_BIT;
+    const size_t maxByteLength = (numBits - 1) / 7 + 1; // numBits / 7 rounding up.
     if (length <= offset)
         return false;
     result = 0;
     unsigned shift = 0;
-    size_t last = std::min(max32BitLEBByteLength, length - offset - 1);
+    size_t last = std::min(maxByteLength, length - offset) - 1;
     uint8_t byte;
     for (unsigned i = 0; true; ++i) {
         byte = bytes[offset++];
-        result |= (byte & 0x7f) << shift;
+        result |= static_cast<T>(byte & 0x7f) << shift;
         shift += 7;
         if (!(byte & 0x80))
             break;
@@ -86,9 +78,29 @@ inline bool WARN_UNUSED_RETURN decodeInt32(const uint8_t* bytes, size_t length, 
             return false;
     }
 
-    if (shift < 32 && (byte & 0x40))
-        result |= ((-1u) << shift);
+    if (shift < numBits && (byte & 0x40))
+        result |= static_cast<T>(-1) << shift;
     return true;
+}
+
+inline bool WARN_UNUSED_RETURN decodeUInt32(const uint8_t* bytes, size_t length, size_t& offset, uint32_t& result)
+{
+    return decodeUInt<uint32_t>(bytes, length, offset, result);
+}
+
+inline bool WARN_UNUSED_RETURN decodeUInt64(const uint8_t* bytes, size_t length, size_t& offset, uint64_t& result)
+{
+    return decodeUInt<uint64_t>(bytes, length, offset, result);
+}
+
+inline bool WARN_UNUSED_RETURN decodeInt32(const uint8_t* bytes, size_t length, size_t& offset, int32_t& result)
+{
+    return decodeInt<int32_t>(bytes, length, offset, result);
+}
+
+inline bool WARN_UNUSED_RETURN decodeInt64(const uint8_t* bytes, size_t length, size_t& offset, int64_t& result)
+{
+    return decodeInt<int64_t>(bytes, length, offset, result);
 }
 
 } } // WTF::LEBDecoder
