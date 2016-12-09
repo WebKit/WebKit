370e8e1d6b2a7b35c48a3c3552bfdd76c4e626ed
Filip Pizlo <fpizlo@apple.com>
2016-12-09 19:49:07 +0000

    Unreviewed, disable concurrent GC while crashes get investigated.
    * runtime/Options.cpp:
    (JSC::recomputeDependentOptions):
    
    
    
    Canonical link: https://commits.webkit.org/183278@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@209622 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/Options.cpp
+++ Source/JavaScriptCore/runtime/Options.cpp
@@ -332,7 +332,7 @@ static void recomputeDependentOptions()
     Options::useFTLJIT() = false;
 #endif
     
-#if !CPU(X86_64) && !CPU(ARM64)
+#if 0 && !CPU(X86_64) && !CPU(ARM64)
     Options::useConcurrentGC() = false;
 #endif
     
