69f5359baf116496baae916832cd8bd4356a8530
Gustavo Noronha Silva <gns@gnome.org>
2016-12-09 15:30:34 +0000

    [GTK] Force GDK backend to x11 when running with XvfbDriver https://bugs.webkit.org/show_bug.cgi?id=165612
    Reviewed by Michael Catanzaro.
    
    When the developer is running a Wayland session, GDK may get confused
    and try to use its Wayland backend (which is higher priority) instead.
    This ensures it will use the appropriate backend when running under
    Xvfb.
    
    * Scripts/webkitpy/port/gtk.py:
    (GtkPort.setup_environ_for_server): when running with XvfbDriver,
    force GDK_BACKEND to x11.
    
    Canonical link: https://commits.webkit.org/183262@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@209606 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/port/gtk.py
+++ Tools/Scripts/webkitpy/port/gtk.py
@@ -119,6 +119,11 @@ class GtkPort(Port):
         environment['TEST_RUNNER_TEST_PLUGIN_PATH'] = self._build_path('lib', 'plugins')
         environment['OWR_USE_TEST_SOURCES'] = '1'
         self._copy_value_from_environ_if_set(environment, 'WEBKIT_OUTPUTDIR')
+
+        if self._driver_class() is XvfbDriver:
+            # Make sure to use the correct GDK backend - we might have a Wayland session
+            environment['GDK_BACKEND'] = 'x11'
+
         # Configure the software libgl renderer if jhbuild ready and we test inside a virtualized window system
         if self._driver_class() in [XvfbDriver, WestonDriver] and self._should_use_jhbuild():
             llvmpipe_libgl_path = self.host.executive.run_command(self._jhbuild_wrapper + ['printenv', 'LLVMPIPE_LIBGL_PATH'],
