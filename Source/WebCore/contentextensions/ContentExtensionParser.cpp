8b6eef24b92454bd828d9d557b2da5332adfd1f6
David Hyatt <hyatt@apple.com>
2016-12-10 23:36:59 +0000

    [CSS Parser] Make sure content extensions initialize AtomicString https://bugs.webkit.org/show_bug.cgi?id=165737
    Reviewed by Simon Fraser.
    
    Fixes two broken tests in TestWebkitAPI.
    
    * contentextensions/ContentExtensionParser.cpp:
    (WebCore::ContentExtensions::isValidSelector):
    
    
    
    Canonical link: https://commits.webkit.org/183332@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@209676 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/contentextensions/ContentExtensionParser.cpp
+++ Source/WebCore/contentextensions/ContentExtensionParser.cpp
@@ -186,6 +186,7 @@ static std::error_code loadTrigger(ExecState& exec, const JSObject& ruleObject, 
 
 static bool isValidSelector(const String& selector)
 {
+    AtomicString::init(); // Necessary to ensure starAtom is initialized.
     CSSParserContext context(HTMLQuirksMode);
     CSSParser parser(context);
     CSSSelectorList selectorList;
