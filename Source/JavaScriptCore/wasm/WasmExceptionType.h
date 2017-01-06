b639921c7e72b1a3a54a73fb0e6a8795872b90cb
Matt Hanson <matthew_hanson@apple.com>
2017-01-06 01:48:27 +0000

    Merge r210137. rdar://problem/29760386
    Canonical link: https://commits.webkit.org/183607.72@safari-603-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-603-branch@210413 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/wasm/WasmExceptionType.h
+++ Source/JavaScriptCore/wasm/WasmExceptionType.h
@@ -38,6 +38,8 @@ namespace Wasm {
     macro(BadSignature, "call_indirect to a signature that does not match") \
     macro(OutOfBoundsTrunc, "Out of bounds Trunc operation") \
     macro(Unreachable, "Unreachable code should not be executed") \
+    macro(DivisionByZero, "Division by zero") \
+    macro(IntegerOverflow, "Integer overflow")
 
 enum class ExceptionType : uint32_t {
 #define MAKE_ENUM(enumName, error) enumName,
