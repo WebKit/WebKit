3443cf574306a99c3cb564f0f625d287a9cbbb1b
Babak Shafiei <bshafiei@apple.com>
2017-01-10 00:11:05 +0000

    Merge r210458. rdar://problem/29911919
    Canonical link: https://commits.webkit.org/183607.90@safari-603-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-603-branch@210529 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/API/JSVirtualMachineInternal.h
+++ Source/JavaScriptCore/API/JSVirtualMachineInternal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Apple Inc. All rights reserved.
+ * Copyright (C) 2013, 2017 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -46,8 +46,6 @@ JSContextGroupRef getGroupFromVirtualMachine(JSVirtualMachine *);
 
 - (JSContext *)contextForGlobalContextRef:(JSGlobalContextRef)globalContext;
 - (void)addContext:(JSContext *)wrapper forGlobalContextRef:(JSGlobalContextRef)globalContext;
-
-- (NSMapTable *)externalObjectGraph;
 
 @end
 #endif // defined(__OBJC__)
