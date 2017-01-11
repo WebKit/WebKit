744c666d15cf2c3c3c8f25d16e908f2e54e39754
Brent Fulgham <bfulgham@apple.com>
2017-01-11 01:51:25 +0000

    File scheme should not allow access of a resource on a different volume. https://bugs.webkit.org/show_bug.cgi?id=158552 <rdar://problem/15307582>
    Reviewed by Alex Christensen.
    
    Source/WebCore:
    
    Revise SecurityOrigin to prevent files from one storage device (volume) from accessing content
    on a different storage device (volume) unless universal access is enabled.
    
    Pass the current file device as part of the NSURLRequest so that CFNetwork can reject loads
    where the device changes in the midst of a load.
    
    Also properly reflect that SecurityOrigin is never null by passing as a reference,
    rather than as a pointer.
    
    Tests: Tools/TestWebKitAPI/Tests/mac/CrossPartitionFileSchemeAccess.mm
    
    * page/SecurityOrigin.cpp:
    (WebCore::SecurityOrigin::canAccess): Pass argument as reference.
    (WebCore::SecurityOrigin::passesFileCheck): Add check that file URLs refer to files in
    the same storage volume.
    (WebCore::SecurityOrigin::canDisplay): Add check that files share the same volume.
    (WebCore::SecurityOrigin::isSameSchemeHostPort): Pass argument as reference.
    * page/SecurityOrigin.h:
    * platform/FileSystem.cpp:
    (WebCore::filesHaveSameVolume): Added.
    * platform/FileSystem.h:
    * platform/network/cocoa/ResourceRequestCocoa.mm:
    (WebCore::ResourceRequest::doUpdatePlatformRequest): If loading a file URL, tell CFNetwork
    the storage device at the time of the start of the load so we can trigger a failure if this
    changes during the load operation.
    * platform/posix/FileSystemPOSIX.cpp:
    (WebCore::getFileDeviceId): Added.
    * platform/win/FileSystemWin.cpp:
    (WebCore::getFileDeviceId): Added.
    
    Tools:
    
    * TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj: Add new files.
    * TestWebKitAPI/Tests/mac/CrossPartitionFileSchemeAccess.html: Added.
    * TestWebKitAPI/Tests/mac/CrossPartitionFileSchemeAccess.mm: Added.
    
    
    Canonical link: https://commits.webkit.org/184015@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@210571 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/posix/FileSystemPOSIX.cpp
+++ Source/WebCore/platform/posix/FileSystemPOSIX.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2007-2017 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -381,6 +381,15 @@ bool hardLinkOrCopyFile(const String& source, const String& destination)
         unlink(fsDestination.data());
 
     return appendResult;
+}
+
+std::optional<int32_t> getFileDeviceId(PlatformFileHandle handle)
+{
+    struct stat fileStat;
+    if (fstat(handle, &fileStat))
+        return std::nullopt;
+
+    return fileStat.st_dev;
 }
 
 } // namespace WebCore
