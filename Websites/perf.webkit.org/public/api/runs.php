ac820c0869a0530d16486ad558bc283e99107e04
Ryosuke Niwa <rniwa@webkit.org>
2017-01-11 04:51:12 +0000

    Build fixes for older versions of Postgres.
    Also redirect / and /# to /v3/ as intended in r200820.
    
    * public/api/measurement-set.php:
    * public/api/runs.php:
    * public/index.html:
    
    
    
    Canonical link: https://commits.webkit.org/184020@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@210576 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Websites/perf.webkit.org/public/api/runs.php
+++ Websites/perf.webkit.org/public/api/runs.php
@@ -79,7 +79,8 @@ class RunsGenerator {
                     LEFT OUTER JOIN build_commits ON commit_build = build_id
                     LEFT OUTER JOIN commits ON build_commit = commit_id, test_runs
                 WHERE run_build = build_id AND run_config = $1 AND NOT EXISTS (SELECT * FROM build_requests WHERE request_build = build_id)
-                GROUP BY build_id, run_id', array($config_id));
+                GROUP BY build_id, build_builder, build_number, build_time, build_latest_revision, build_slave,
+                    run_id, run_config, run_build, run_iteration_count_cache, run_mean_cache, run_sum_cache, run_square_sum_cache, run_marked_outlier', array($config_id));
     }
 
     private static function format_run($run) {
