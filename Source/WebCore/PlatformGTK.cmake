dc2f7c1e08ea68306b3e4a88ce101b7fc8936147
Csaba Osztrogonác <ossy@webkit.org>
2017-01-11 08:47:42 +0000

    Unreviewed GTK buildfix after r210571.
    * PlatformGTK.cmake:
    
    Canonical link: https://commits.webkit.org/184023@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@210579 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/PlatformGTK.cmake
+++ Source/WebCore/PlatformGTK.cmake
@@ -163,6 +163,8 @@ list(APPEND WebCore_SOURCES
     platform/network/soup/SynchronousLoaderClientSoup.cpp
     platform/network/soup/WebKitSoupRequestGeneric.cpp
 
+    platform/posix/FileSystemPOSIX.cpp
+
     platform/soup/PublicSuffixSoup.cpp
     platform/soup/SharedBufferSoup.cpp
     platform/soup/URLSoup.cpp
