2fca1ce0d3bec7d65181c6b680712345d6be2599
Carlos Garcia Campos <cgarcia@igalia.com>
2017-01-11 08:55:22 +0000

    Unreviewed, rolling out r210579.
    That was not the right fix
    
    Reverted changeset:
    
    "Unreviewed GTK buildfix after r210571."
    http://trac.webkit.org/changeset/210579
    
    Canonical link: https://commits.webkit.org/184026@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@210582 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/PlatformGTK.cmake
+++ Source/WebCore/PlatformGTK.cmake
@@ -163,8 +163,6 @@ list(APPEND WebCore_SOURCES
     platform/network/soup/SynchronousLoaderClientSoup.cpp
     platform/network/soup/WebKitSoupRequestGeneric.cpp
 
-    platform/posix/FileSystemPOSIX.cpp
-
     platform/soup/PublicSuffixSoup.cpp
     platform/soup/SharedBufferSoup.cpp
     platform/soup/URLSoup.cpp
