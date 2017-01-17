962568d51005a502fd9f4b49bb22907f298877d5
Joseph Pecoraro <joepeck@webkit.org>
2017-01-17 19:24:23 +0000

    Crash when closing tab with debugger paused https://bugs.webkit.org/show_bug.cgi?id=161746 <rdar://problem/15607819>
    Reviewed by Brian Burg and Brent Fulgham.
    
    Source/WebCore:
    
    * page/Page.h:
    (WebCore::Page::incrementNestedRunLoopCount):
    (WebCore::Page::decrementNestedRunLoopCount):
    (WebCore::Page::insideNestedRunLoop):
    Keep track of whether or not this Page is inside of a nested run loop.
    Currently the only nested run loop we know about is EventLoop used
    by Web Inspector when debugging JavaScript.
    
    (WebCore::Page::whenUnnested):
    Callback that can be called when we are no longer inside of a nested
    run loop.
    
    (WebCore::Page::~Page):
    Ensure we are not in a known nested run loop when destructing, since
    that could be unsafe.
    
    * inspector/PageScriptDebugServer.cpp:
    (WebCore::PageScriptDebugServer::runEventLoopWhilePausedInternal):
    Increment and decrement as we go into or leave the nested runloop.
    
    * inspector/InspectorController.cpp:
    (WebCore::InspectorController::inspectedPageDestroyed):
    (WebCore::InspectorController::disconnectAllFrontends):
    Rework destruction to allow disconnectAllFrontends to happen earlier
    if necessary. WebKit clients may use this to disconnect remote
    frontends when closing a Page.
    
    Source/WebKit/mac:
    
    * WebView/WebView.mm:
    (WebKit::DeferredPageDestructor::createDeferredPageDestructor):
    (WebKit::DeferredPageDestructor::DeferredPageDestructor):
    (WebKit::DeferredPageDestructor::tryDestruction):
    (-[WebView _close]):
    Defer destruction of the Page if we are in a nested runloop.
    
    Source/WebKit2:
    
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::DeferredPageDestructor::createDeferredPageDestructor):
    (WebKit::DeferredPageDestructor::DeferredPageDestructor):
    (WebKit::DeferredPageDestructor::tryDestruction):
    (WebKit::WebPage::close):
    Defer destruction of the Page and WebPage if we are in a nested runloop.
    Also, proactively close all inspector frontends, including remote frontends.
    
    * WebProcess/WebPage/ios/WebPageIOS.mm:
    (WebKit::WebPage::handleSyntheticClick):
    (WebKit::WebPage::completeSyntheticClick):
    Return early in some cases where a nested run loop may have closed
    the WebPage on us while handling JavaScript events.
    
    Canonical link: https://commits.webkit.org/184176@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@210822 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/Page.h
+++ Source/WebCore/page/Page.h
@@ -192,6 +192,11 @@ public:
     void decrementSubframeCount() { ASSERT(m_subframeCount); --m_subframeCount; }
     int subframeCount() const { checkSubframeCountConsistency(); return m_subframeCount; }
 
+    void incrementNestedRunLoopCount();
+    void decrementNestedRunLoopCount();
+    bool insideNestedRunLoop() const { return m_nestedRunLoopCount > 0; }
+    WEBCORE_EXPORT void whenUnnested(std::function<void()>);
+
 #if ENABLE(REMOTE_INSPECTOR)
     WEBCORE_EXPORT bool remoteInspectionAllowed() const;
     WEBCORE_EXPORT void setRemoteInspectionAllowed(bool);
@@ -622,7 +627,10 @@ private:
     std::unique_ptr<DiagnosticLoggingClient> m_diagnosticLoggingClient;
     std::unique_ptr<WebGLStateTracker> m_webGLStateTracker;
 
-    int m_subframeCount;
+    int m_nestedRunLoopCount { 0 };
+    std::function<void()> m_unnestCallback;
+
+    int m_subframeCount { 0 };
     String m_groupName;
     bool m_openedByDOM;
 
