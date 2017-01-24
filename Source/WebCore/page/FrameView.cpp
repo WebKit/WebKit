d00a399101327d79c52701e9bff669d15d1ae831
Joseph Pecoraro <pecoraro@apple.com>
2017-01-24 11:48:25 +0000

    Remove always true allowCustomScrollbarInMainFrame setting https://bugs.webkit.org/show_bug.cgi?id=167362
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2017-01-24
    Reviewed by Andreas Kling.
    
    Setting added by Chromium and never used by other ports.
    
    * page/FrameView.cpp:
    (WebCore::FrameView::createScrollbar):
    * page/Settings.in:
    
    Canonical link: https://commits.webkit.org/184394@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@211082 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/FrameView.cpp
+++ Source/WebCore/page/FrameView.cpp
@@ -608,9 +608,6 @@ void FrameView::updateCanHaveScrollbars()
 
 Ref<Scrollbar> FrameView::createScrollbar(ScrollbarOrientation orientation)
 {
-    if (!frame().settings().allowCustomScrollbarInMainFrame() && frame().isMainFrame())
-        return ScrollView::createScrollbar(orientation);
-
     // FIXME: We need to update the scrollbar dynamically as documents change (or as doc elements and bodies get discovered that have custom styles).
     Document* doc = frame().document();
 
