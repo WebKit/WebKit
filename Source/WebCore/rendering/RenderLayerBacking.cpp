84554f5953119d08a3c652062182c4c3daa1f4e8
Miguel Gomez <magomez@igalia.com>
2017-01-24 16:25:31 +0000

    [GTK] Do not paint non composited content into the window when using the threaded compositor https://bugs.webkit.org/show_bug.cgi?id=167367
    Reviewed by Carlos Garcia Campos.
    
    When using the threaded compositor we need to send the non composited content for compositing as well,
    not painting it directly into the window.
    
    No new tests.
    
    * rendering/RenderLayerBacking.cpp:
    (WebCore::RenderLayerBacking::paintsIntoWindow):
    
    
    Canonical link: https://commits.webkit.org/184396@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@211084 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/RenderLayerBacking.cpp
+++ Source/WebCore/rendering/RenderLayerBacking.cpp
@@ -2213,6 +2213,10 @@ GraphicsLayer* RenderLayerBacking::childForSuperlayers() const
 
 bool RenderLayerBacking::paintsIntoWindow() const
 {
+#if USE(COORDINATED_GRAPHICS_THREADED)
+        return false;
+#endif
+
     if (m_usingTiledCacheLayer)
         return false;
 
