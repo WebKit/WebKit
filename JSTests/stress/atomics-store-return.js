a4e3df2ea6b86dc455ba722158498f467210406c
Matt Hanson <matthew_hanson@apple.com>
2017-01-26 01:40:48 +0000

    Merge r211129. rdar://problem/30178458
    Canonical link: https://commits.webkit.org/183607.209@safari-603-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-603-branch@211184 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/atomics-store-return.js
+++ JSTests/stress/atomics-store-return.js
@@ -2,10 +2,25 @@ var sab = new SharedArrayBuffer(1);
 var a = new Int8Array(sab);
 var result = Atomics.store(a, 0, 1000);
 if (result != 1000)
-    throw "Error: bad result: " + result;
+    throw new Error("bad result: " + result);
 
 sab = new SharedArrayBuffer(4);
 a = new Uint32Array(sab);
-var result = Atomics.store(a, 0, 4000000000);
+result = Atomics.store(a, 0, 4000000000);
 if (result != 4000000000)
-    throw "Error: bad result: " + result;
+    throw new Error("bad result: " + result);
+if (a[0] != 4000000000)
+    throw new Error("bad value read back: " + a[0]);
+result = Atomics.store(a, 0, -4000000000);
+if (result != -4000000000)
+    throw new Error("bad result: " + result);
+if (a[0] != 294967296)
+    throw new Error("bad value read back: " + a[0]);
+
+var count = 0;
+result = Atomics.store(a, 0, { valueOf() { count++; return 42; } });
+if (result != 42)
+    throw new Error("bad result: " + result);
+if (count != 1)
+    throw new Error("bad count: " + count);
+
