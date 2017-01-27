28cb351f96cc999cad05a8e6b9b282e8ddf4ae26
Matt Hanson <matthew_hanson@apple.com>
2017-01-27 08:02:25 +0000

    Merge r211241. rdar://problem/30099051
    Canonical link: https://commits.webkit.org/183607.236@safari-603-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-603-branch@211272 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/cocoa/ResourceUsageThreadCocoa.mm
+++ Source/WebCore/page/cocoa/ResourceUsageThreadCocoa.mm
@@ -34,21 +34,17 @@
 #include <mach/mach.h>
 #include <mach/vm_statistics.h>
 #include <runtime/VM.h>
-#include <sys/sysctl.h>
 
 namespace WebCore {
 
 static size_t vmPageSize()
 {
-    static size_t pageSize;
-    static std::once_flag onceFlag;
-    std::call_once(onceFlag, [&] {
-        size_t outputSize = sizeof(pageSize);
-        int status = sysctlbyname("vm.pagesize", &pageSize, &outputSize, nullptr, 0);
-        ASSERT_UNUSED(status, status != -1);
-        ASSERT(pageSize);
-    });
-    return pageSize;
+#if PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 100000
+    return vm_kernel_page_size;
+#else
+    static size_t cached = sysconf(_SC_PAGESIZE);
+    return cached;
+#endif
 }
 
 void logFootprintComparison(const std::array<TagInfo, 256>& before, const std::array<TagInfo, 256>& after)
