0fc898a0f949b85bda77db116ec171f508687088
Youenn Fablet <youenn@apple.com>
2017-01-31 18:57:57 +0000

    [WebRTC] Add a libwebrtc AudioModule specific to WebKit https://bugs.webkit.org/show_bug.cgi?id=167572
    Patch by Youenn Fablet <youenn@apple.com> on 2017-01-31
    Reviewed by Alex Christensen.
    
    To be covered by webrtc audio tests.
    Adding an AudioModule to circumvent the default LibWebRTC Audio Module which does the audio directly.
    The implementation of the AudioModule is restricted to enable fueling the incoming audio tracks with incoming data.
    Code inspired from https://chromium.googlesource.com/chromium/src/+/master/remoting/protocol/webrtc_audio_module.h
    
    * WebCore.xcodeproj/project.pbxproj:
    * platform/mediastream/libwebrtc/LibWebRTCAudioModule.cpp: Added.
    (WebCore::LibWebRTCAudioModule::LibWebRTCAudioModule):
    (WebCore::LibWebRTCAudioModule::RegisterAudioCallback):
    (WebCore::LibWebRTCAudioModule::OnMessage):
    (WebCore::LibWebRTCAudioModule::StartPlayout):
    (WebCore::LibWebRTCAudioModule::StopPlayout):
    (WebCore::LibWebRTCAudioModule::StartPlayoutOnAudioThread):
    (WebCore::LibWebRTCAudioModule::PollFromSource):
    * platform/mediastream/libwebrtc/LibWebRTCAudioModule.h: Added.
    * platform/mediastream/libwebrtc/LibWebRTCUtils.cpp:
    (WebCore::ThreadMessageData::ThreadMessageData):
    (WebCore::initializePeerConnectionFactoryAndThreads):
    
    Canonical link: https://commits.webkit.org/184678@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@211439 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/mediastream/libwebrtc/LibWebRTCUtils.cpp
+++ Source/WebCore/platform/mediastream/libwebrtc/LibWebRTCUtils.cpp
@@ -1,29 +1,26 @@
 /*
- * Copyright (C) 2017 Apple Inc.
+ * Copyright (C) 2017 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted, provided that the following conditions
- * are required to be met:
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- * 1.  Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- * 2.  Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- * 3.  Neither the name of Apple Inc. nor the names of
- *     its contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL APPLE INC. AND ITS CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "config.h"
@@ -31,6 +28,7 @@
 
 #if USE(LIBWEBRTC)
 
+#include "LibWebRTCAudioModule.h"
 #include <webrtc/api/peerconnectionfactoryproxy.h>
 #include <webrtc/api/peerconnectionfactory.h>
 #include <webrtc/base/physicalsocketserver.h>
@@ -42,6 +40,7 @@
 namespace WebCore {
 
 struct PeerConnectionFactoryAndThreads : public rtc::MessageHandler {
+    std::unique_ptr<LibWebRTCAudioModule> audioDeviceModule;
     std::unique_ptr<rtc::Thread> networkThread;
     std::unique_ptr<rtc::Thread> signalingThread;
     rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface> factory;
@@ -96,7 +95,9 @@ static void initializePeerConnectionFactoryAndThreads()
     result = factoryAndThreads.signalingThread->Start();
     ASSERT(result);
 
-    factoryAndThreads.factory = webrtc::CreatePeerConnectionFactory(factoryAndThreads.networkThread.get(), factoryAndThreads.networkThread.get(), factoryAndThreads.signalingThread.get(), nullptr, new webrtc::VideoToolboxVideoEncoderFactory() , new webrtc::VideoToolboxVideoDecoderFactory());
+    factoryAndThreads.audioDeviceModule = std::make_unique<LibWebRTCAudioModule>();
+
+    factoryAndThreads.factory = webrtc::CreatePeerConnectionFactory(factoryAndThreads.networkThread.get(), factoryAndThreads.networkThread.get(), factoryAndThreads.signalingThread.get(), factoryAndThreads.audioDeviceModule.get(), new webrtc::VideoToolboxVideoEncoderFactory(), new webrtc::VideoToolboxVideoDecoderFactory());
 
     ASSERT(factoryAndThreads.factory);
 }
