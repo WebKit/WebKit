d549e5d5fd8e7b6f5b8ab2f13fedabe76d627c1f
Antoine Quint <graouts@apple.com>
2017-02-07 10:53:54 +0000

    [Modern Media Controls] Improve handling of <video> with only audio tracks https://bugs.webkit.org/show_bug.cgi?id=167836 <rdar://problem/30255812>
    Patch by Antoine Quint <graouts@apple.com> on 2017-02-07
    Reviewed by Dean Jackson.
    
    Source/WebCore:
    
    We now check for the availability of video tracks before considering a <video>
    element is displaying an actual video file and turning auto-hide on. We also
    check that we have video tracks before enabling the fullscreen button. This
    brings the behavior of a <video> pointing to a resource with no video tracks
    to be the same as an <audio> element.
    
    Test: media/modern-media-controls/media-controller/media-controller-video-with-only-audio.html
    
    * Modules/modern-media-controls/media/controls-visibility-support.js:
    (ControlsVisibilitySupport.prototype.get tracksToMonitor):
    (ControlsVisibilitySupport.prototype._updateControls):
    (ControlsVisibilitySupport):
    * Modules/modern-media-controls/media/fullscreen-support.js:
    (FullscreenSupport.prototype.syncControl):
    (FullscreenSupport):
    
    LayoutTests:
    
    Add a new test to check that a <video> with a resource that only has audio tracks
    does not auto-hide nor show the fullscreen button. We also rebaseline a few existing
    tests for this change of behavior.
    
    * fast/regions/inline-block-inside-anonymous-overflow-with-covered-controls-expected.html:
    * fast/regions/inline-block-inside-anonymous-overflow-with-covered-controls.html:
    * media/modern-media-controls/controls-visibility-support/controls-visibility-support-controls-on-expected.txt:
    * media/modern-media-controls/controls-visibility-support/controls-visibility-support-controls-on.html:
    * media/modern-media-controls/controls-visibility-support/controls-visibility-support-controls-toggle-expected.txt:
    * media/modern-media-controls/controls-visibility-support/controls-visibility-support-controls-toggle.html:
    * media/modern-media-controls/media-controller/media-controller-video-with-only-audio-expected.txt: Added.
    * media/modern-media-controls/media-controller/media-controller-video-with-only-audio.html: Added.
    * media/modern-media-controls/time-label/time-label-white-space-nowrap.html:
    
    Canonical link: https://commits.webkit.org/184954@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@211802 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/modern-media-controls/media/controls-visibility-support.js
+++ Source/WebCore/Modules/modern-media-controls/media/controls-visibility-support.js
@@ -48,6 +48,11 @@ class ControlsVisibilitySupport extends MediaControllerSupport
         return ["loadedmetadata", "play", "pause"];
     }
 
+    get tracksToMonitor()
+    {
+        return [this.mediaController.media.videoTracks];
+    }
+
     handleEvent()
     {
         this._updateControls();
@@ -58,7 +63,7 @@ class ControlsVisibilitySupport extends MediaControllerSupport
     _updateControls()
     {
         const media = this.mediaController.media;
-        const isVideo = media instanceof HTMLVideoElement;
+        const isVideo = media instanceof HTMLVideoElement && media.videoTracks.length > 0;
         let shouldShowControls = this.mediaController.media.controls;
         if (isVideo)
             shouldShowControls = shouldShowControls && media.readyState > HTMLMediaElement.HAVE_NOTHING;
