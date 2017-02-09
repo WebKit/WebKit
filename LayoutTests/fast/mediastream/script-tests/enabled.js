e74a4614cadae64a8fe910e0148f305ed7b51d15
Chris Dumez <cdumez@apple.com>
2017-02-09 00:26:25 +0000

    Unreviewed, rolling out r211907.
    Broke the build
    
    Reverted changeset:
    
    "[MediaStream] Remove legacy Navigator.webkitGetUserMedia"
    https://bugs.webkit.org/show_bug.cgi?id=168016
    http://trac.webkit.org/changeset/211907
    
    Canonical link: https://commits.webkit.org/185048@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@211909 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/mediastream/script-tests/enabled.js
+++ LayoutTests/fast/mediastream/script-tests/enabled.js
@@ -3,22 +3,15 @@ description("Tests that navigator.webkitGetUserMedia is present.");
 function hasGetUserMediaProperty()
 {
     for (var property in navigator) {
-        if (property == "mediaDevices") {
-            var devices = navigator[property];
-            for (property in devices) {
-                if (property == "getUserMedia")
-                    return true;
-            }
-        }
+        if (property == "webkitGetUserMedia")
+            return true;
     }
     return false;
 }
 
-shouldBeTrue("typeof navigator.mediaDevices == 'object'");
-shouldBeTrue("typeof navigator.mediaDevices.getUserMedia == 'function'");
+shouldBeTrue("typeof navigator.webkitGetUserMedia == 'function'");
 shouldBeTrue("hasGetUserMediaProperty()");
-shouldBeTrue("'mediaDevices' in navigator");
-shouldBeTrue("'getUserMedia' in navigator.mediaDevices");
-shouldBeFalse("navigator.hasOwnProperty('mediaDevices')");
+shouldBeTrue("'webkitGetUserMedia' in navigator");
+shouldBeFalse("navigator.hasOwnProperty('webkitGetUserMedia')");
 
 window.jsTestIsAsync = false;
