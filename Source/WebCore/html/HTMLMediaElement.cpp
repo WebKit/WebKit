36700e9ac6934fac1460eba7a734858aed093183
Brent Fulgham <bfulgham@webkit.org>
2017-02-09 08:54:34 +0000

    Merge r208825 - Clear track client when removing a track https://bugs.webkit.org/show_bug.cgi?id=164842 <rdar://problem/29213621>
    Reviewed by Eric Carlson.
    
    Source/WebCore:
    
    Call 'clearClient' when removing a track from an HTMLMediaElement.
    
    Test: media/track/audio-track-add-remove.html
          media/track/video-track-add-remove.html
    
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::removeAudioTrack): Call 'clearClient'
    (WebCore::HTMLMediaElement::removeVideoTrack): Ditto.
    
    LayoutTests:
    
    * media/track/audio-track-add-remove-expected.txt: Added.
    * media/track/audio-track-add-remove.html: Added.
    * media/track/video-track-add-remove-expected.txt: Added.
    * media/track/video-track-add-remove.html: Added.

--- Source/WebCore/html/HTMLMediaElement.cpp
+++ Source/WebCore/html/HTMLMediaElement.cpp
@@ -3654,6 +3654,7 @@ void HTMLMediaElement::addVideoTrack(Ref<VideoTrack>&& track)
 void HTMLMediaElement::removeAudioTrack(AudioTrack& track)
 {
     m_audioTracks->remove(track);
+    track.clearClient();
 }
 
 void HTMLMediaElement::removeTextTrack(TextTrack& track, bool scheduleEvent)
@@ -3671,6 +3672,7 @@ void HTMLMediaElement::removeTextTrack(TextTrack& track, bool scheduleEvent)
 void HTMLMediaElement::removeVideoTrack(VideoTrack& track)
 {
     m_videoTracks->remove(track);
+    track.clearClient();
 }
 
 void HTMLMediaElement::forgetResourceSpecificTracks()
