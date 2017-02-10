29b8a9c55bc93cac626510f21ed45ac3963e9016
Mark Lam <mark.lam@apple.com>
2017-02-10 01:54:24 +0000

    Fix max length check in ArrayPrototype.js' concatSlowPath(). https://bugs.webkit.org/show_bug.cgi?id=167270 <rdar://problem/30128133>
    Patch by Mark Lam <mark.lam@apple.com> on 2017-02-09
    Reviewed by Filip Pizlo.
    
    JSTests:
    
    * stress/array-prototype-concat-of-long-spliced-arrays.js: Added.
    * stress/array-prototype-concat-of-long-spliced-arrays2.js: Added.
    
    Source/JavaScriptCore:
    
    1. Fixed concatSlowPath() to ensure that the result array length does not exceed
       @MAX_ARRAY_INDEX.  The old code was checking against @MAX_SAFE_INTEGER in some
       cases, but this is overly permissive.
    
    2. Changed concatSlowPath() to throw a RangeError instead of a TypeError to be
       consistent with the C++ runtime functions in JSArray.cpp.
    
    3. Changed the RangeError message in concatSlowPath() and JSArray.cpp to "Length
       exceeded the maximum array length" when the error is that the result length
       exceeds MAX_ARRAY_INDEX.  We do this for 2 reasons:
       a. "Length exceeded the maximum array length" is more informative than
          "Invalid array length".
       b. We want to use the same string consistently for the same error.
    
       There are still 2 places in JSArray.cpp that still throws a RangeError with
       message "Invalid array length".  In those cases, the error is not necessarily
       due to the result length exceeding MAX_ARRAY_INDEX, but is due to attempting to
       set a length value that is not an integer that fits in MAX_ARRAY_INDEX e.g.
       an attempt to set a fractional length value.  Hence, "Invalid array length" is
       appropriate for those cases.
    
    4. Fixed JSArray::appendMemcpy() to handle overflows when computing the result
       array length.
    
    * builtins/ArrayPrototype.js:
    (concatSlowPath):
    * bytecode/BytecodeIntrinsicRegistry.cpp:
    (JSC::BytecodeIntrinsicRegistry::BytecodeIntrinsicRegistry):
    * bytecode/BytecodeIntrinsicRegistry.h:
    * runtime/ArrayPrototype.cpp:
    (JSC::concatAppendOne):
    (JSC::arrayProtoPrivateFuncAppendMemcpy):
    * runtime/JSArray.cpp:
    (JSC::JSArray::appendMemcpy):
    (JSC::JSArray::push):
    
    
    Canonical link: https://commits.webkit.org/185126@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@212019 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ JSTests/stress/array-prototype-concat-of-long-spliced-arrays.js
@@ -0,0 +1,36 @@
+function shouldEqual(actual, expected) {
+    if (actual != expected) {
+        throw "ERROR: expect " + expected + ", actual " + actual;
+    }
+}
+
+function test() {
+    var exception;
+    try {
+        var a = [];
+        a.length = 0xffffff00;
+
+        var b = a.splice(0, 0x100000); // Undecided array
+
+        var args = [];
+        args.length = 4094;
+        args.fill(b);
+
+        var q = [];
+        q.length = 0x1000;
+        q.fill(7);
+
+        var c = a.splice(0, 0xfffef); //Shorter undecided array
+
+        args[4094] = c;
+        args[4095] = q;
+
+        b.concat.apply(b, args);
+    } catch (e) {
+        exception = e;
+    }
+
+    shouldEqual(exception, "RangeError: Length exceeded the maximum array length");
+}
+
+test();
--- Source/JavaScriptCore/bytecode/BytecodeIntrinsicRegistry.h
+++ Source/JavaScriptCore/bytecode/BytecodeIntrinsicRegistry.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 Yusuke Suzuki <utatane.tea@gmail.com>.
- * Copyright (C) 2016 Apple Inc. All rights reserved.
+ * Copyright (C) 2016-2017 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -64,6 +64,7 @@ class Identifier;
     macro(iterationKindKey) \
     macro(iterationKindValue) \
     macro(iterationKindKeyValue) \
+    macro(MAX_ARRAY_INDEX) \
     macro(MAX_STRING_LENGTH) \
     macro(MAX_SAFE_INTEGER) \
     macro(ModuleFetch) \
