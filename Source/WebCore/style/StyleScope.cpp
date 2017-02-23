c7cd0f762ffc8b39cde899a40be123bfe7496f6b
Matt Hanson <matthew_hanson@apple.com>
2017-02-23 20:51:42 +0000

    Merge r212828. rdar://problem/30636274
    Canonical link: https://commits.webkit.org/183607.437@safari-603-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-603-branch@212919 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/style/StyleScope.cpp
+++ Source/WebCore/style/StyleScope.cpp
@@ -51,6 +51,7 @@
 #include "UserContentController.h"
 #include "UserContentURLPattern.h"
 #include "UserStyleSheet.h"
+#include <wtf/SetForScope.h>
 
 namespace WebCore {
 
@@ -94,6 +95,7 @@ StyleResolver& Scope::resolver()
         return m_document.userAgentShadowTreeStyleResolver();
 
     if (!m_resolver) {
+        SetForScope<bool> isUpdatingStyleResolver { m_isUpdatingStyleResolver, true };
         m_resolver = std::make_unique<StyleResolver>(m_document);
         m_resolver->appendAuthorStyleSheets(m_activeStyleSheets);
     }
@@ -459,6 +461,7 @@ void Scope::updateStyleResolver(Vector<RefPtr<CSSStyleSheet>>& activeStyleSheets
     }
     auto& styleResolver = resolver();
 
+    SetForScope<bool> isUpdatingStyleResolver { m_isUpdatingStyleResolver, true };
     if (updateType == Reset) {
         styleResolver.ruleSets().resetAuthorStyle();
         styleResolver.appendAuthorStyleSheets(activeStyleSheets);
@@ -529,6 +532,10 @@ void Scope::clearPendingUpdate()
 
 void Scope::scheduleUpdate(UpdateType update)
 {
+    // FIXME: The m_isUpdatingStyleResolver test is here because extension stylesheets can get us here from StyleResolver::appendAuthorStyleSheets.
+    if (update == UpdateType::ContentsOrInterpretation && !m_isUpdatingStyleResolver)
+        clearResolver();
+
     if (!m_pendingUpdate || *m_pendingUpdate < update) {
         m_pendingUpdate = update;
         if (m_shadowRoot)
