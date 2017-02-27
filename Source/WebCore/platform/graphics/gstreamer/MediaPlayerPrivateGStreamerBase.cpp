fa5ece867e7828dd16cc6f49522115beab2f8f23
Xabier Rodriguez-Calvar <calvaris@igalia.com>
2017-02-27 13:59:12 +0000

    Merge r212830 - [GStreamer][EME] Fix issue with allowed systems extraction https://bugs.webkit.org/show_bug.cgi?id=168717
    Reviewed by Carlos Garcia Campos.
    
    The allowed systems were not being extracted from the need-context
    message because the loop was not stopping on the right condition.
    
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp:
    (WebCore::extractEventsAndSystemsFromMessage): Fix wrong
    condition.
    (WebCore::MediaPlayerPrivateGStreamerBase::handleProtectionEvent):
    Add better debug category.

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp
@@ -268,7 +268,7 @@ static std::pair<Vector<GRefPtr<GstEvent>>, Vector<String>> extractEventsAndSyst
     ASSERT(streamEncryptionAllowedSystems);
     Vector<String> streamEncryptionAllowedSystemsVector;
     unsigned i;
-    for (i = 0; !streamEncryptionAllowedSystems[i]; ++i)
+    for (i = 0; streamEncryptionAllowedSystems[i]; ++i)
         streamEncryptionAllowedSystemsVector.append(streamEncryptionAllowedSystems[i]);
 
     const GValue* streamEncryptionEventsList = gst_structure_get_value(structure, "stream-encryption-events");
@@ -1239,7 +1239,7 @@ void MediaPlayerPrivateGStreamerBase::keyAdded()
 void MediaPlayerPrivateGStreamerBase::handleProtectionEvent(GstEvent* event)
 {
     if (m_handledProtectionEvents.contains(GST_EVENT_SEQNUM(event))) {
-        GST_TRACE("event %u already handled", GST_EVENT_SEQNUM(event));
+        GST_DEBUG("event %u already handled", GST_EVENT_SEQNUM(event));
         m_handledProtectionEvents.remove(GST_EVENT_SEQNUM(event));
         return;
     }
