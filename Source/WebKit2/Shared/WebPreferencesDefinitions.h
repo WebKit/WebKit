bfea0bb9fdd2835fad1752765ee70e6ec8c01df7
Youenn Fablet <youenn@apple.com>
2017-03-02 23:39:40 +0000

    Activate some new webrtc tests https://bugs.webkit.org/show_bug.cgi?id=168850
    Patch by Youenn Fablet <youenn@apple.com> on 2017-03-02
    Reviewed by Alex Christensen.
    
    Source/WebKit2:
    
    * Shared/WebPreferencesDefinitions.h: Activating peer connection by default when libwebrtc is on.
    This allows ensuring the network process will have its sandbox extension.
    
    LayoutTests:
    
    * TestExpectations: Marking webrtc/audio-peer-connection-webaudio.html as failing.
    * webrtc/audio-peer-connection-webaudio.html: Making test nicer when failing.
    * webrtc/datachannel/basic-expected.txt: Updated test expectation.
    
    Canonical link: https://commits.webkit.org/186100@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@213310 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/Shared/WebPreferencesDefinitions.h
+++ Source/WebKit2/Shared/WebPreferencesDefinitions.h
@@ -206,7 +206,7 @@
     macro(SubpixelCSSOMElementMetricsEnabled, subpixelCSSOMElementMetricsEnabled, Bool, bool, false, "", "") \
     macro(UseGiantTiles, useGiantTiles, Bool, bool, false, "", "") \
     macro(MediaStreamEnabled, mediaStreamEnabled, Bool, bool, false, "", "") \
-    macro(PeerConnectionEnabled, peerConnectionEnabled, Bool, bool, false, "", "") \
+    macro(PeerConnectionEnabled, peerConnectionEnabled, Bool, bool, WebCore::LibWebRTCProvider::webRTCAvailable(), "", "") \
     macro(UseLegacyTextAlignPositionedElementBehavior, useLegacyTextAlignPositionedElementBehavior, Bool, bool, false, "", "") \
     macro(SpatialNavigationEnabled, spatialNavigationEnabled, Bool, bool, false, "", "") \
     macro(MediaSourceEnabled, mediaSourceEnabled, Bool, bool, true, "", "") \
