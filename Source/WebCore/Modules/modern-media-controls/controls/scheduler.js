834d9cd7e88eeebb91f7ce18b38cbac01a77c5ae
Antoine Quint <graouts@apple.com>
2017-03-10 11:31:56 +0000

    [mac-wk1] LayoutTest media/modern-media-controls/airplay-button/airplay-button.html is a flaky timeout https://bugs.webkit.org/show_bug.cgi?id=168409
    Unreviewed.
    
    Add some debugging in the hope of understanding why airplay-button.html fails once a day on bots.
    
    Patch by Antoine Quint <graouts@apple.com> on 2017-03-10
    
    Source/WebCore:
    
    * Modules/modern-media-controls/controls/scheduler.js:
    (const.scheduler.new.prototype.scheduleLayout):
    (const.scheduler.new.prototype.unscheduleLayout):
    (const.scheduler.new.prototype._requestFrameIfNeeded):
    (const.scheduler.new.prototype._frameDidFire):
    (const.scheduler.new.prototype._layout):
    
    LayoutTests:
    
    * media/modern-media-controls/airplay-button/airplay-button-expected.txt:
    * media/modern-media-controls/airplay-button/airplay-button.html:
    
    Canonical link: https://commits.webkit.org/186462@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@213706 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/modern-media-controls/controls/scheduler.js
+++ Source/WebCore/Modules/modern-media-controls/controls/scheduler.js
@@ -6,6 +6,7 @@ const scheduler = new class
     {
         this._frameID = -1;
         this._layoutCallbacks = new Set;
+        this.debug = new Function;
     }
 
     // Public
@@ -17,31 +18,46 @@ const scheduler = new class
 
     scheduleLayout(callback)
     {
-        if (typeof callback !== "function")
+        this.debug("scheduleLayout() - start");
+        if (typeof callback !== "function") {
+            this.debug("scheduleLayout() - end - callback was not a function");
             return;
+        }
 
         this._layoutCallbacks.add(callback);
         this._requestFrameIfNeeded();
+        this.debug(`scheduleLayout() - _layoutCallbacks.size = ${this._layoutCallbacks.size}`);
+        this.debug("scheduleLayout() - end");
     }
 
     unscheduleLayout(callback)
     {
-        if (typeof callback !== "function")
+        this.debug("unscheduleLayout() - start");
+        if (typeof callback !== "function") {
+            this.debug("unscheduleLayout() - end - callback was not a function");
             return;
+        }
 
         this._layoutCallbacks.delete(callback);
+        this.debug(`unscheduleLayout() - this._layoutCallbacks.size = ${this._layoutCallbacks.size}`);
+        this.debug("unscheduleLayout() - end");
     }
 
     // Private
 
     _requestFrameIfNeeded()
     {
-        if (this._frameID === -1 && this._layoutCallbacks.size > 0)
+        this.debug("_requestFrameIfNeeded()");
+        if (this._frameID === -1 && this._layoutCallbacks.size > 0) {
             this._frameID = window.requestAnimationFrame(this._frameDidFire.bind(this));
+            this.debug(`_requestFrameIfNeeded() - registered rAF, _frameID = ${this._frameID}`);
+        } else
+            this.debug(`_requestFrameIfNeeded() - failed to register rAF call, _frameID = ${this._frameID}, _layoutCallbacks.size = ${this._layoutCallbacks.size}`);
     }
 
     _frameDidFire()
     {
+        this.debug("_frameDidFire() - start");
         if (typeof scheduler.frameWillFire === "function")
             scheduler.frameWillFire();
 
@@ -51,16 +67,21 @@ const scheduler = new class
 
         if (typeof scheduler.frameDidFire === "function")
             scheduler.frameDidFire();
+        this.debug("_frameDidFire() - end");
     }
 
     _layout()
     {
+        this.debug("_layout() - start");
         // Layouts are not re-entrant.
         const layoutCallbacks = this._layoutCallbacks;
         this._layoutCallbacks = new Set;
 
+        this.debug(`_layout() - layoutCallbacks.size = ${layoutCallbacks.size}`);
+
         for (let callback of layoutCallbacks)
             callback();
+        this.debug("_layout() - end");
     }
 
 };
