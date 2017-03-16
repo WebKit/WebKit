cf28d17165df1a78014e62ddff778acbdb836d9d
Matt Hanson <matthew_hanson@apple.com>
2017-03-16 18:28:51 +0000

    Merge r213737. rdar://problem/30983702
    Canonical link: https://commits.webkit.org/183607.450@safari-603-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-603-branch@214056 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/win/WebKitDLL.cpp
+++ Source/WebKit/win/WebKitDLL.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006, 2007, 2014-2015 Apple Inc.  All rights reserved.
+ * Copyright (C) 2006-2017 Apple Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,6 +23,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
+#define DEPRECATED_EXPORT_SYMBOLS 1
 #include "WebKitDLL.h"
 
 #if USE(CG)
@@ -65,6 +66,7 @@ HashCountedSet<String>& gClassNameCount()
     return gClassNameCount.get();
 }
 
+void bindJavaScriptTrampoline();
 
 STDAPI_(BOOL) DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID /*lpReserved*/)
 {
@@ -73,6 +75,7 @@ STDAPI_(BOOL) DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID /*lpRe
             gLockCount = gClassCount = 0;
             gInstance = hModule;
             WebCore::setInstanceHandle(hModule);
+            bindJavaScriptTrampoline();
             return TRUE;
 
         case DLL_PROCESS_DETACH:
@@ -218,4 +221,172 @@ PassRefPtr<WebCore::SharedBuffer> loadResourceIntoBuffer(const char* name)
     unsigned size = SizeofResource(gInstance, resInfo);
 
     return WebCore::SharedBuffer::create(reinterpret_cast<const char*>(resource), size);
+}
+
+// Force symbols to be included so we can export them for legacy clients.
+// DEPRECATED! People should get these symbols from JavaScriptCore.dll, not WebKit.dll!
+// #include <JavaScriptCore/JSObjectRef.h>
+
+typedef JSClassRef (*JSClassCreateFunction)(const JSClassDefinition* definition);
+typedef void* (*JSObjectGetPrivateFunction)(JSObjectRef);
+typedef JSObjectRef (*JSObjectMakeFunctionStub)(JSContextRef, JSClassRef, void*);
+typedef void (*JSObjectSetPropertyFunction)(JSContextRef, JSObjectRef, JSStringRef propertyName, JSValueRef, JSPropertyAttributes, JSValueRef* exception);
+typedef JSStringRef (*JSStringCreateWithCFStringFunction)(CFStringRef);
+typedef JSStringRef (*JSStringCreateWithUTF8CStringFunction)(const char*);
+typedef const JSChar* (*JSStringGetCharactersPtrFunction)(JSStringRef);
+typedef size_t (*JSStringGetLengthFunction)(JSStringRef);
+typedef void (*JSStringReleaseFunction)(JSStringRef);
+typedef bool (*JSValueIsTypeFunction)(JSContextRef, JSValueRef);
+typedef JSValueRef(*JSValueMakeUndefinedFunction)(JSContextRef);
+typedef double(*JSValueToNumberFunction)(JSContextRef, JSValueRef, JSValueRef* exception);
+typedef JSValueRef(*JSValueMakeStringFunction)(JSContextRef, JSStringRef);
+typedef JSStringRef (*JSValueToStringCopyFunction)(JSContextRef, JSValueRef, JSValueRef* exception);
+
+JSClassCreateFunction m_jsClassCreateFunction = nullptr;
+JSObjectGetPrivateFunction m_jsObjectGetPrivateFunction = nullptr;
+JSObjectMakeFunctionStub m_jsObjectMakeFunction = nullptr;
+JSObjectSetPropertyFunction m_jsObjectSetPropertyFunction = nullptr;
+JSStringCreateWithCFStringFunction m_jsStringCreateWithCFStringFunction = nullptr;
+JSStringCreateWithUTF8CStringFunction m_jsStringCreateWithUTF8CStringFunction = nullptr;
+JSStringGetCharactersPtrFunction m_jsStringGetCharactersPtrFunction = nullptr;
+JSStringGetLengthFunction m_jsStringGetLengthFunction = nullptr;
+JSStringReleaseFunction m_jsStringReleaseFunction = nullptr;
+JSValueIsTypeFunction m_jsValueIsNumberFunction = nullptr;
+JSValueIsTypeFunction m_jsValueIsStringFunction = nullptr;
+JSValueMakeUndefinedFunction m_jsValueMakeUndefinedFunction = nullptr;
+JSValueToNumberFunction m_jsValueToNumberFunction = nullptr;
+JSValueMakeStringFunction m_jsValueMakeStringFunction = nullptr;
+JSValueToStringCopyFunction m_jsValueToStringCopyFunction = nullptr;
+
+void bindJavaScriptTrampoline()
+{
+    const wchar_t* libraryName = L"JavaScriptCore.dll";
+
+    HMODULE library = ::LoadLibrary(libraryName);
+    if (!library)
+        return;
+
+    m_jsClassCreateFunction = reinterpret_cast<JSClassCreateFunction>(::GetProcAddress(library, "JSClassCreate"));
+    m_jsObjectGetPrivateFunction = reinterpret_cast<JSObjectGetPrivateFunction>(::GetProcAddress(library, "JSObjectGetPrivate"));
+    m_jsObjectMakeFunction = reinterpret_cast<JSObjectMakeFunctionStub>(::GetProcAddress(library, "JSObjectMake"));
+    m_jsObjectSetPropertyFunction = reinterpret_cast<JSObjectSetPropertyFunction>(::GetProcAddress(library, "JSObjectSetProperty"));;
+    m_jsStringCreateWithCFStringFunction = reinterpret_cast<JSStringCreateWithCFStringFunction>(::GetProcAddress(library, "JSStringCreateWithCFString"));
+    m_jsStringCreateWithUTF8CStringFunction = reinterpret_cast<JSStringCreateWithUTF8CStringFunction>(::GetProcAddress(library, "JSStringCreateWithUTF8CString"));
+    m_jsStringGetCharactersPtrFunction = reinterpret_cast<JSStringGetCharactersPtrFunction>(::GetProcAddress(library, "JSStringGetCharactersPtr"));
+    m_jsStringGetLengthFunction = reinterpret_cast<JSStringGetLengthFunction>(::GetProcAddress(library, "JSStringGetLength"));
+    m_jsStringReleaseFunction = reinterpret_cast<JSStringReleaseFunction>(::GetProcAddress(library, "JSStringRelease"));
+    m_jsValueIsNumberFunction = reinterpret_cast<JSValueIsTypeFunction>(::GetProcAddress(library, "JSValueIsNumber"));
+    m_jsValueIsStringFunction = reinterpret_cast<JSValueIsTypeFunction>(::GetProcAddress(library, "JSValueIsString"));
+    m_jsValueMakeStringFunction = reinterpret_cast<JSValueMakeStringFunction>(::GetProcAddress(library, "JSValueMakeString"));
+    m_jsValueMakeUndefinedFunction = reinterpret_cast<JSValueMakeUndefinedFunction>(::GetProcAddress(library, "JSValueMakeUndefined"));
+    m_jsValueToNumberFunction = reinterpret_cast<JSValueToNumberFunction>(::GetProcAddress(library, "JSValueToNumber"));
+    m_jsValueToStringCopyFunction = reinterpret_cast<JSValueToStringCopyFunction>(::GetProcAddress(library, "JSValueToStringCopy"));
+}
+
+extern "C"
+{
+
+JSClassRef JSClassCreate(const JSClassDefinition* definition)
+{
+    if (m_jsClassCreateFunction)
+        return m_jsClassCreateFunction(definition);
+    return nullptr;
+}
+
+void* JSObjectGetPrivate(JSObjectRef object)
+{
+    if (m_jsObjectGetPrivateFunction)
+        return m_jsObjectGetPrivateFunction(object);
+    return nullptr;
+}
+
+JSObjectRef JSObjectMake(JSContextRef ctx, JSClassRef classRef, void* data)
+{
+    if (m_jsObjectMakeFunction)
+        return m_jsObjectMakeFunction(ctx, classRef, data);
+    return nullptr;
+}
+
+void JSObjectSetProperty(JSContextRef ctx, JSObjectRef object, JSStringRef propertyName, JSValueRef value, JSPropertyAttributes attributes, JSValueRef* exception)
+{
+    if (m_jsObjectSetPropertyFunction)
+        m_jsObjectSetPropertyFunction(ctx, object, propertyName, value, attributes, exception);
+}
+
+JSStringRef JSStringCreateWithCFString(CFStringRef value)
+{
+    if (m_jsStringCreateWithCFStringFunction)
+        return m_jsStringCreateWithCFStringFunction(value);
+    return nullptr;
+}
+
+JSStringRef JSStringCreateWithUTF8CString(const char* value)
+{
+    if (m_jsStringCreateWithUTF8CStringFunction)
+        return m_jsStringCreateWithUTF8CStringFunction(value);
+    return nullptr;
+}
+
+const JSChar* JSStringGetCharactersPtr(JSStringRef value)
+{
+    if (m_jsStringGetCharactersPtrFunction)
+        return m_jsStringGetCharactersPtrFunction(value);
+    return nullptr;
+}
+
+size_t JSStringGetLength(JSStringRef value)
+{
+    if (m_jsStringGetLengthFunction)
+        return m_jsStringGetLengthFunction(value);
+    return 0;
+}
+
+void JSStringRelease(JSStringRef value)
+{
+    if (m_jsStringReleaseFunction)
+        return m_jsStringReleaseFunction(value);
+}
+
+bool JSValueIsNumber(JSContextRef ctx, JSValueRef value)
+{
+    if (m_jsValueIsNumberFunction)
+        return m_jsValueIsNumberFunction(ctx, value);
+    return false;
+}
+
+bool JSValueIsString(JSContextRef ctx, JSValueRef value)
+{
+    if (m_jsValueIsStringFunction)
+        return m_jsValueIsStringFunction(ctx, value);
+    return false;
+}
+
+JSValueRef JSValueMakeString(JSContextRef ctx, JSStringRef value)
+{
+    if (m_jsValueMakeStringFunction)
+        return m_jsValueMakeStringFunction(ctx, value);
+    return nullptr;
+}
+
+JSValueRef JSValueMakeUndefined(JSContextRef ctx)
+{
+    if (m_jsValueMakeUndefinedFunction)
+        return m_jsValueMakeUndefinedFunction(ctx);
+    return nullptr;
+}
+
+double JSValueToNumber(JSContextRef ctx, JSValueRef value, JSValueRef* exception)
+{
+    if (m_jsValueToNumberFunction)
+        return m_jsValueToNumberFunction(ctx, value, exception);
+    return 0;
+}
+
+JSStringRef JSValueToStringCopy(JSContextRef ctx, JSValueRef value, JSValueRef* exception)
+{
+    if (m_jsValueToStringCopyFunction)
+        return m_jsValueToStringCopyFunction(ctx, value, exception);
+    return nullptr;
+}
+
 }
