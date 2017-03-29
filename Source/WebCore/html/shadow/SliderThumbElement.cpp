6ce1e6ce3070cb117d129672853d0ed6545b1473
Jason Marcell <jmarcell@apple.com>
2017-03-29 22:41:23 +0000

    Merge r214291. rdar://problem/30922110
    Canonical link: https://commits.webkit.org/183607.486@safari-603-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-603-branch@214567 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/shadow/SliderThumbElement.cpp
+++ Source/WebCore/html/shadow/SliderThumbElement.cpp
@@ -335,10 +335,6 @@ void SliderThumbElement::stopDragging()
     m_inDragMode = false;
     if (renderer())
         renderer()->setNeedsLayout();
-
-    RefPtr<HTMLInputElement> input = hostInput();
-    if (input)
-        input->dispatchFormControlChangeEvent();
 }
 
 #if !PLATFORM(IOS)
@@ -369,6 +365,7 @@ void SliderThumbElement::defaultEventHandler(Event& event)
         startDragging();
         return;
     } else if (eventType == eventNames().mouseupEvent && isLeftButton) {
+        input->dispatchFormControlChangeEvent();
         stopDragging();
         return;
     } else if (eventType == eventNames().mousemoveEvent) {
@@ -499,6 +496,9 @@ void SliderThumbElement::handleTouchEndAndCancel(TouchEvent& touchEvent)
 
     clearExclusiveTouchIdentifier();
 
+    RefPtr<HTMLInputElement> input = hostInput();
+    if (input)
+        input->dispatchFormControlChangeEvent();
     stopDragging();
 }
 
