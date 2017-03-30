ed72057fe7ee92dae7a620255c1681a9b046fd65
Brent Fulgham <bfulgham@apple.com>
2017-03-30 16:09:09 +0000

    Remove unused MediaControlsApple implementation https://bugs.webkit.org/show_bug.cgi?id=170258 <rdar://problem/31331056>
    Reviewed by Eric Carlson.
    
    Remove dead code related to old media controls. We switched to HTML5-based media controls
    several years ago.
    
    No new tests. Removing this dead code should have no change in behavior.
    
    * WebCore.xcodeproj/project.pbxproj: Remove unused files.
    * dom/EventListener.h: Remove unused event type.
    * html/shadow/MediaControlsApple.cpp: Removed.
    * html/shadow/MediaControlsApple.h: Removed.
    
    
    Canonical link: https://commits.webkit.org/187204@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@214607 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/EventListener.h
+++ Source/WebCore/dom/EventListener.h
@@ -43,7 +43,6 @@ public:
         GObjectEventListenerType,
         NativeEventListenerType,
         SVGTRefTargetEventListenerType,
-        MediaControlsAppleEventListenerType 
     };
 
     virtual ~EventListener() { }
