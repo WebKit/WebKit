c394cd0dd33b826fd9a55edde75ee9961b261d80
Filip Pizlo <fpizlo@apple.com>
2017-04-03 21:07:01 +0000

    Move the Liveness<> adapters from AirLiveness.h to AirLivenessAdapter.h.
    Rubber stamped by Keith Miller.
    
    This will make it easier to write other code that uses those adapters.
    
    * JavaScriptCore.xcodeproj/project.pbxproj:
    * b3/air/AirLiveness.h:
    (JSC::B3::Air::LivenessAdapter::LivenessAdapter): Deleted.
    (JSC::B3::Air::LivenessAdapter::blockSize): Deleted.
    (JSC::B3::Air::LivenessAdapter::forEachUse): Deleted.
    (JSC::B3::Air::LivenessAdapter::forEachDef): Deleted.
    (JSC::B3::Air::TmpLivenessAdapter::TmpLivenessAdapter): Deleted.
    (JSC::B3::Air::TmpLivenessAdapter::numIndices): Deleted.
    (JSC::B3::Air::TmpLivenessAdapter::acceptsBank): Deleted.
    (JSC::B3::Air::TmpLivenessAdapter::acceptsRole): Deleted.
    (JSC::B3::Air::TmpLivenessAdapter::valueToIndex): Deleted.
    (JSC::B3::Air::TmpLivenessAdapter::indexToValue): Deleted.
    (JSC::B3::Air::StackSlotLivenessAdapter::StackSlotLivenessAdapter): Deleted.
    (JSC::B3::Air::StackSlotLivenessAdapter::numIndices): Deleted.
    (JSC::B3::Air::StackSlotLivenessAdapter::acceptsBank): Deleted.
    (JSC::B3::Air::StackSlotLivenessAdapter::acceptsRole): Deleted.
    (JSC::B3::Air::StackSlotLivenessAdapter::valueToIndex): Deleted.
    (JSC::B3::Air::StackSlotLivenessAdapter::indexToValue): Deleted.
    * b3/air/AirLivenessAdapter.h: Added.
    (JSC::B3::Air::LivenessAdapter::LivenessAdapter):
    (JSC::B3::Air::LivenessAdapter::blockSize):
    (JSC::B3::Air::LivenessAdapter::forEachUse):
    (JSC::B3::Air::LivenessAdapter::forEachDef):
    (JSC::B3::Air::TmpLivenessAdapter::TmpLivenessAdapter):
    (JSC::B3::Air::TmpLivenessAdapter::numIndices):
    (JSC::B3::Air::TmpLivenessAdapter::acceptsBank):
    (JSC::B3::Air::TmpLivenessAdapter::acceptsRole):
    (JSC::B3::Air::TmpLivenessAdapter::valueToIndex):
    (JSC::B3::Air::TmpLivenessAdapter::indexToValue):
    (JSC::B3::Air::StackSlotLivenessAdapter::StackSlotLivenessAdapter):
    (JSC::B3::Air::StackSlotLivenessAdapter::numIndices):
    (JSC::B3::Air::StackSlotLivenessAdapter::acceptsBank):
    (JSC::B3::Air::StackSlotLivenessAdapter::acceptsRole):
    (JSC::B3::Air::StackSlotLivenessAdapter::valueToIndex):
    (JSC::B3::Air::StackSlotLivenessAdapter::indexToValue):
    
    
    
    Canonical link: https://commits.webkit.org/187338@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@214838 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/JavaScriptCore/b3/air/AirLivenessAdapter.h
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2015-2017 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#if ENABLE(B3_JIT)
+
+#include "AirBasicBlock.h"
+#include "AirCFG.h"
+#include "AirCode.h"
+#include "AirInstInlines.h"
+#include "AirStackSlot.h"
+#include "AirTmpInlines.h"
+
+namespace JSC { namespace B3 { namespace Air {
+
+template<typename Adapter>
+struct LivenessAdapter {
+    typedef Air::CFG CFG;
+    
+    LivenessAdapter(Code& code)
+        : code(code)
+    {
+    }
+    
+    unsigned blockSize(BasicBlock* block)
+    {
+        return block->size();
+    }
+    
+    template<typename Func>
+    void forEachUse(BasicBlock* block, size_t instBoundaryIndex, const Func& func)
+    {
+        if (Inst* prevInst = block->get(instBoundaryIndex - 1)) {
+            prevInst->forEach<typename Adapter::Thing>(
+                [&] (typename Adapter::Thing& thing, Arg::Role role, Bank bank, Width) {
+                    if (Arg::isLateUse(role)
+                        && Adapter::acceptsBank(bank)
+                        && Adapter::acceptsRole(role))
+                        func(Adapter::valueToIndex(thing));
+                });
+        }
+        
+        if (Inst* nextInst = block->get(instBoundaryIndex)) {
+            nextInst->forEach<typename Adapter::Thing>(
+                [&] (typename Adapter::Thing& thing, Arg::Role role, Bank bank, Width) {
+                    if (Arg::isEarlyUse(role)
+                        && Adapter::acceptsBank(bank)
+                        && Adapter::acceptsRole(role))
+                        func(Adapter::valueToIndex(thing));
+                });
+        }
+    }
+    
+    template<typename Func>
+    void forEachDef(BasicBlock* block, size_t instBoundaryIndex, const Func& func)
+    {
+        if (Inst* prevInst = block->get(instBoundaryIndex - 1)) {
+            prevInst->forEach<typename Adapter::Thing>(
+                [&] (typename Adapter::Thing& thing, Arg::Role role, Bank bank, Width) {
+                    if (Arg::isLateDef(role)
+                        && Adapter::acceptsBank(bank)
+                        && Adapter::acceptsRole(role))
+                        func(Adapter::valueToIndex(thing));
+                });
+        }
+        
+        if (Inst* nextInst = block->get(instBoundaryIndex)) {
+            nextInst->forEach<typename Adapter::Thing>(
+                [&] (typename Adapter::Thing& thing, Arg::Role role, Bank bank, Width) {
+                    if (Arg::isEarlyDef(role)
+                        && Adapter::acceptsBank(bank)
+                        && Adapter::acceptsRole(role))
+                        func(Adapter::valueToIndex(thing));
+                });
+        }
+    }
+
+    Code& code;
+};
+
+template<Bank adapterBank, Arg::Temperature minimumTemperature = Arg::Cold>
+struct TmpLivenessAdapter : LivenessAdapter<TmpLivenessAdapter<adapterBank, minimumTemperature>> {
+    typedef LivenessAdapter<TmpLivenessAdapter<adapterBank, minimumTemperature>> Base;
+    
+    static constexpr const char* name = "TmpLiveness";
+    typedef Tmp Thing;
+
+    TmpLivenessAdapter(Code& code)
+        : Base(code)
+    {
+    }
+
+    unsigned numIndices()
+    {
+        unsigned numTmps = Base::code.numTmps(adapterBank);
+        return AbsoluteTmpMapper<adapterBank>::absoluteIndex(numTmps);
+    }
+    static bool acceptsBank(Bank bank) { return bank == adapterBank; }
+    static bool acceptsRole(Arg::Role role) { return Arg::temperature(role) >= minimumTemperature; }
+    static unsigned valueToIndex(Tmp tmp) { return AbsoluteTmpMapper<adapterBank>::absoluteIndex(tmp); }
+    static Tmp indexToValue(unsigned index) { return AbsoluteTmpMapper<adapterBank>::tmpFromAbsoluteIndex(index); }
+};
+
+struct StackSlotLivenessAdapter : LivenessAdapter<StackSlotLivenessAdapter> {
+    static constexpr const char* name = "StackSlotLiveness";
+    typedef StackSlot* Thing;
+
+    StackSlotLivenessAdapter(Code& code)
+        : LivenessAdapter(code)
+    {
+    }
+
+    unsigned numIndices()
+    {
+        return code.stackSlots().size();
+    }
+    static bool acceptsBank(Bank) { return true; }
+    static bool acceptsRole(Arg::Role) { return true; }
+    static unsigned valueToIndex(StackSlot* stackSlot) { return stackSlot->index(); }
+    StackSlot* indexToValue(unsigned index) { return code.stackSlots()[index]; }
+};
+
+} } } // namespace JSC::B3::Air
+
+#endif // ENABLE(B3_JIT)
+
