5f09958a9729577a485b925c89c2fcafd6d0c6a5
Tomáš Popela <tpopela@redhat.com>
2017-04-06 12:33:31 +0000

    Merge r213638 - Correctly process the return value of gdk_drag_context_get_selected_action()
    Reviewed by Carlos Garcia Campos.
    
    It returns the action itself and not the bitmask.
    
    * UIProcess/gtk/DragAndDropHandler.cpp:
    (WebKit::DragAndDropHandler::drop):

--- Source/WebKit2/UIProcess/gtk/DragAndDropHandler.cpp
+++ Source/WebKit2/UIProcess/gtk/DragAndDropHandler.cpp
@@ -290,7 +290,7 @@ bool DragAndDropHandler::drop(GdkDragContext* context, const IntPoint& position,
     droppingContext->dropHappened = true;
 
     uint32_t flags = 0;
-    if ((gdk_drag_context_get_selected_action(context) & GDK_ACTION_COPY))
+    if (gdk_drag_context_get_selected_action(context) == GDK_ACTION_COPY)
         flags |= WebCore::DragApplicationIsCopyKeyDown;
     DragData dragData(droppingContext->selectionData.ptr(), position, convertWidgetPointToScreenPoint(m_page.viewWidget(), position), gdkDragActionToDragOperation(gdk_drag_context_get_actions(context)), static_cast<WebCore::DragApplicationFlags>(flags));
     SandboxExtension::Handle handle;
