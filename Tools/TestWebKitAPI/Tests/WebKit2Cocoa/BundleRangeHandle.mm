162f76dbfd0995b2dd92975cd6dd778ceaf1e28b
Ryan Haddad <ryanhaddad@apple.com>
2017-04-07 19:28:09 +0000

    [ios-simulator] API test WebKit2.WKWebProcessPlugInRangeHandle timing out https://bugs.webkit.org/show_bug.cgi?id=167594
    Unreviewed test gardening.
    
    Disable this test because it is still timing out on the bots.
    
    * TestWebKitAPI/Tests/WebKit2Cocoa/BundleRangeHandle.mm:
    (TEST):
    
    Canonical link: https://commits.webkit.org/187540@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@215112 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/WebKit2Cocoa/BundleRangeHandle.mm
+++ Tools/TestWebKitAPI/Tests/WebKit2Cocoa/BundleRangeHandle.mm
@@ -61,7 +61,7 @@ static bool didGetBodyInnerHTMLAfterDetectingData;
 
 @end
 
-TEST(WebKit2, WKWebProcessPlugInRangeHandle)
+TEST(WebKit2, DISABLED_WKWebProcessPlugInRangeHandle)
 {
     RetainPtr<WKWebViewConfiguration> configuration = retainPtr([WKWebViewConfiguration testwebkitapi_configurationWithTestPlugInClassName:@"BundleRangeHandlePlugIn"]);
     RetainPtr<WKWebView> webView = adoptNS([[WKWebView alloc] initWithFrame:NSMakeRect(0, 0, 800, 600) configuration:configuration.get()]);
