cf023043c821e37586abe95e2b099ba719d89918
Guillaume Emont <guijemont@igalia.com>
2017-04-11 12:44:47 +0000

    [jsc][mips] Add missing MacroAssembler functions after r214187 https://bugs.webkit.org/show_bug.cgi?id=170089
    Patch by Guillaume Emont <guijemont@igalia.com> on 2017-04-11
    Reviewed by Yusuke Suzuki.
    
    * assembler/MacroAssemblerMIPS.h:
    (JSC::MacroAssemblerMIPS::loadFloat): Added.
    (JSC::MacroAssemblerMIPS::storeFloat): Added.
    
    Canonical link: https://commits.webkit.org/187652@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@215226 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/assembler/MacroAssemblerMIPS.h
+++ Source/JavaScriptCore/assembler/MacroAssemblerMIPS.h
@@ -2403,6 +2403,23 @@ public:
         }
     }
 
+    void loadFloat(ImplicitAddress address, FPRegisterID dest)
+    {
+        if (address.offset >= -32768 && address.offset <= 32767
+            && !m_fixedWidth) {
+            m_assembler.lwc1(dest, address.base, address.offset);
+        } else {
+            /*
+               lui     addrTemp, (offset + 0x8000) >> 16
+               addu    addrTemp, addrTemp, base
+               lwc1    dest, (offset & 0xffff)(addrTemp)
+               */
+            m_assembler.lui(addrTempRegister, (address.offset + 0x8000) >> 16);
+            m_assembler.addu(addrTempRegister, addrTempRegister, address.base);
+            m_assembler.lwc1(dest, addrTempRegister, address.offset);
+        }
+    }
+
     void loadDouble(ImplicitAddress address, FPRegisterID dest)
     {
 #if WTF_MIPS_ISA(1)
@@ -2537,6 +2554,23 @@ public:
             m_assembler.addu(addrTempRegister, addrTempRegister, address.base);
             m_assembler.lui(immTempRegister, (address.offset + 0x8000) >> 16);
             m_assembler.addu(addrTempRegister, addrTempRegister, immTempRegister);
+            m_assembler.swc1(src, addrTempRegister, address.offset);
+        }
+    }
+
+    void storeFloat(FPRegisterID src, ImplicitAddress address)
+    {
+        if (address.offset >= -32768 && address.offset <= 32767
+            && !m_fixedWidth)
+            m_assembler.swc1(src, address.base, address.offset);
+        else {
+            /*
+                lui     addrTemp, (offset + 0x8000) >> 16
+                addu    addrTemp, addrTemp, base
+                swc1    src, (offset & 0xffff)(addrTemp)
+              */
+            m_assembler.lui(addrTempRegister, (address.offset + 0x8000) >> 16);
+            m_assembler.addu(addrTempRegister, addrTempRegister, address.base);
             m_assembler.swc1(src, addrTempRegister, address.offset);
         }
     }
