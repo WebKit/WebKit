f75c07a277345b53e21a920848a07f033d2a07c8
Myles C. Maxfield <mmaxfield@apple.com>
2017-04-13 05:49:14 +0000

    [Cocoa] Suppress font download dialog in new matching algorithm https://bugs.webkit.org/show_bug.cgi?id=169015
    Reviewed by Alexey Proskuryakov.
    
    Not testable.
    
    * platform/graphics/cocoa/FontCacheCoreText.cpp:
    (WebCore::FontDatabase::lookupPostScriptName):
    
    Canonical link: https://commits.webkit.org/187732@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@215314 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/cocoa/FontCacheCoreText.cpp
+++ Source/WebCore/platform/graphics/cocoa/FontCacheCoreText.cpp
@@ -895,8 +895,10 @@ public:
         auto folded = postScriptName.string().foldCase();
         return m_postScriptNameToFontDescriptors.ensure(folded, [&] {
             auto postScriptNameString = folded.createCFString();
-            CFTypeRef keys[] = { kCTFontNameAttribute };
-            CFTypeRef values[] = { postScriptNameString.get() };
+            const int one = 1;
+            static NeverDestroyed<RetainPtr<CFNumberRef>> yes = adoptCF(CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &one));
+            CFTypeRef keys[] = { kCTFontEnabledAttribute, kCTFontNameAttribute };
+            CFTypeRef values[] = { yes.get().get(), postScriptNameString.get() };
             auto attributes = adoptCF(CFDictionaryCreate(kCFAllocatorDefault, keys, values, WTF_ARRAY_LENGTH(keys), &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks));
             auto fontDescriptorToMatch = adoptCF(CTFontDescriptorCreateWithAttributes(attributes.get()));
             auto match = adoptCF(static_cast<CTFontDescriptorRef>(CTFontDescriptorCreateMatchingFontDescriptor(fontDescriptorToMatch.get(), nullptr)));
