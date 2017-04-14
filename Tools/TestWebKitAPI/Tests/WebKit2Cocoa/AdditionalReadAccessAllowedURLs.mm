69c7d8d6a84815ec6544c8cbf2859e61a9fd3739
Dan Bernstein <mitz@apple.com>
2017-04-14 22:17:36 +0000

    [ios-simulator] API test WebKit2.AdditionalReadAccessAllowedURLs is failing https://bugs.webkit.org/show_bug.cgi?id=170856
    Disabled the newly-added test in the Simulator, which doesn’t enforce sandboxing.
    
    * TestWebKitAPI/Tests/WebKit2Cocoa/AdditionalReadAccessAllowedURLs.mm:
    
    
    
    Canonical link: https://commits.webkit.org/187794@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@215377 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/WebKit2Cocoa/AdditionalReadAccessAllowedURLs.mm
+++ Tools/TestWebKitAPI/Tests/WebKit2Cocoa/AdditionalReadAccessAllowedURLs.mm
@@ -26,7 +26,7 @@
 #import "config.h"
 #import <WebKit/WKFoundation.h>
 
-#if WK_API_ENABLED
+#if WK_API_ENABLED && !PLATFORM(IOS_SIMULATOR)
 
 #import "AdditionalReadAccessAllowedURLsProtocol.h"
 #import "PlatformUtilities.h"
