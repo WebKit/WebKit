b8f6523bcd1047baacb55724c632b961ad450fb2
Žan Doberšek <zdobersek@igalia.com>
2017-04-26 07:40:28 +0000

    [GCrypt] CryptoKeyRSA: implement create(), keySizeInBits(), buildAlgorithm(), exportData() https://bugs.webkit.org/show_bug.cgi?id=171213
    Reviewed by Michael Catanzaro.
    
    CryptoKeyRSA::create() validates the specified key type along with the
    provided data. If everything is OK, it then proceeds to build the key
    of the appropriate type through an s-expression that is constructed with
    the necessary data. We don't currently support creating private keys
    with any additional prime information (apart from p and q).
    
    CryptoKeyRSA::keySizeInBits() calls the helper getRSAModulusLength()
    function that find the modulus MPI in the key s-expression and returns
    the modulus data length in bits.
    
    CryptoKeyRSA::buildAlgorithm() returns KeyAlgorithm that's constructed
    from the appropriate name, this key's modulus length, and this key's
    public exponent data. It also specifies the hash if there is such a
    restriction for this key.
    
    CryptoKeyRSA::exportData() returns a properly-constructed
    CryptoKeyDataRSAComponents object. The public modulus and exponent are
    used in case the key is public. If the key is private, the secret
    exponent and the p and q prime numbers are retrieved in MPI format.
    They're then used to calculate the dp, dq and qi parameters that are
    exported in the JWK format. Note that libgcrypt internally switches
    the p and q prime numbers such that p < q, but the Web Crypto
    specification and the dependant RFCs expect that q < p. We address
    this by simply switching the roles of the two primes and follow the
    usual q < p assumption when exporting the primes and computing the
    dependant exponents and coefficients.
    
    No new tests -- existing ones cover this but don't work yet due to missing
    RSA-related CryptoAlgorithm implementations.
    
    * crypto/gcrypt/CryptoKeyRSAGCrypt.cpp:
    (WebCore::getRSAModulusLength):
    (WebCore::getParameterMPIData):
    (WebCore::getRSAKeyParameter):
    (WebCore::CryptoKeyRSA::create):
    (WebCore::CryptoKeyRSA::keySizeInBits):
    (WebCore::CryptoKeyRSA::buildAlgorithm):
    (WebCore::CryptoKeyRSA::exportData):
    
    
    Canonical link: https://commits.webkit.org/188198@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@215796 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/crypto/gcrypt/CryptoKeyRSAGCrypt.cpp
+++ Source/WebCore/crypto/gcrypt/CryptoKeyRSAGCrypt.cpp
@@ -39,17 +39,139 @@
 
 namespace WebCore {
 
-RefPtr<CryptoKeyRSA> CryptoKeyRSA::create(CryptoAlgorithmIdentifier identifier, CryptoAlgorithmIdentifier hash, bool hasHash, const CryptoKeyDataRSAComponents& keyData, bool extractable, CryptoKeyUsageBitmap usage)
+// Retrieve size of the public modulus N of the given RSA key, in bits.
+static size_t getRSAModulusLength(gcry_sexp_t sexp)
 {
-    notImplemented();
-    UNUSED_PARAM(identifier);
-    UNUSED_PARAM(hash);
-    UNUSED_PARAM(hasHash);
-    UNUSED_PARAM(keyData);
-    UNUSED_PARAM(extractable);
-    UNUSED_PARAM(usage);
+    // The s-expression is of roughly the following form:
+    // (private-key|public-key
+    //   (rsa
+    //     (n n-mpi)
+    //     (e e-mpi)
+    //     ...))
+    PAL::GCrypt::Handle<gcry_sexp_t> nSexp(gcry_sexp_find_token(sexp, "n", 0));
+    if (!nSexp)
+        return 0;
 
-    return nullptr;
+    PAL::GCrypt::Handle<gcry_mpi_t> nMPI(gcry_sexp_nth_mpi(nSexp, 1, GCRYMPI_FMT_USG));
+    if (!nMPI)
+        return 0;
+
+    size_t dataLength = 0;
+    gcry_error_t error = gcry_mpi_print(GCRYMPI_FMT_USG, nullptr, 0, &dataLength, nMPI);
+    if (error != GPG_ERR_NO_ERROR) {
+        PAL::GCrypt::logError(error);
+        return 0;
+    }
+
+    return dataLength * 8;
+}
+
+static Vector<uint8_t> getParameterMPIData(gcry_mpi_t paramMPI)
+{
+    size_t dataLength = 0;
+    gcry_error_t error = gcry_mpi_print(GCRYMPI_FMT_USG, nullptr, 0, &dataLength, paramMPI);
+    if (error != GPG_ERR_NO_ERROR) {
+        PAL::GCrypt::logError(error);
+        return { };
+    }
+
+    Vector<uint8_t> parameter(dataLength);
+    error = gcry_mpi_print(GCRYMPI_FMT_USG, parameter.data(), parameter.size(), nullptr, paramMPI);
+    if (error != GPG_ERR_NO_ERROR) {
+        PAL::GCrypt::logError(error);
+        return { };
+    }
+
+    return parameter;
+}
+
+static Vector<uint8_t> getRSAKeyParameter(gcry_sexp_t sexp, const char* name)
+{
+    PAL::GCrypt::Handle<gcry_sexp_t> paramSexp(gcry_sexp_find_token(sexp, name, 0));
+    if (!paramSexp)
+        return { };
+
+    PAL::GCrypt::Handle<gcry_mpi_t> paramMPI(gcry_sexp_nth_mpi(paramSexp, 1, GCRYMPI_FMT_USG));
+    if (!paramMPI)
+        return { };
+
+    return getParameterMPIData(paramMPI);
+}
+
+RefPtr<CryptoKeyRSA> CryptoKeyRSA::create(CryptoAlgorithmIdentifier identifier, CryptoAlgorithmIdentifier hash, bool hasHash, const CryptoKeyDataRSAComponents& keyData, bool extractable, CryptoKeyUsageBitmap usages)
+{
+    // When creating a private key, we require the p and q prime information.
+    if (keyData.type() == CryptoKeyDataRSAComponents::Type::Private && !keyData.hasAdditionalPrivateKeyParameters())
+        return nullptr;
+
+    // But we don't currently support creating keys with any additional prime information.
+    if (!keyData.otherPrimeInfos().isEmpty())
+        return nullptr;
+
+    // Validate the key data.
+    {
+        bool valid = true;
+
+        // For both public and private keys, we need the public modulus and exponent.
+        valid &= !keyData.modulus().isEmpty() && !keyData.exponent().isEmpty();
+
+        // For private keys, we require the private exponent, as well as p and q prime information.
+        if (keyData.type() == CryptoKeyDataRSAComponents::Type::Private)
+            valid &= !keyData.privateExponent().isEmpty() && !keyData.firstPrimeInfo().primeFactor.isEmpty() && !keyData.secondPrimeInfo().primeFactor.isEmpty();
+
+        if (!valid)
+            return nullptr;
+    }
+
+    CryptoKeyType keyType;
+    switch (keyData.type()) {
+    case CryptoKeyDataRSAComponents::Type::Public:
+        keyType = CryptoKeyType::Public;
+        break;
+    case CryptoKeyDataRSAComponents::Type::Private:
+        keyType = CryptoKeyType::Private;
+        break;
+    }
+
+    // Construct the key s-expression, using the data that's available.
+    PAL::GCrypt::Handle<gcry_sexp_t> keySexp;
+    {
+        gcry_error_t error = GPG_ERR_NO_ERROR;
+
+        switch (keyType) {
+        case CryptoKeyType::Public:
+            error = gcry_sexp_build(&keySexp, nullptr, "(public-key(rsa(n %b)(e %b)))",
+                keyData.modulus().size(), keyData.modulus().data(),
+                keyData.exponent().size(), keyData.exponent().data());
+            break;
+        case CryptoKeyType::Private:
+            if (keyData.hasAdditionalPrivateKeyParameters()) {
+                error = gcry_sexp_build(&keySexp, nullptr, "(private-key(rsa(n %b)(e %b)(d %b)(p %b)(q %b)))",
+                    keyData.modulus().size(), keyData.modulus().data(),
+                    keyData.exponent().size(), keyData.exponent().data(),
+                    keyData.privateExponent().size(), keyData.privateExponent().data(),
+                    keyData.secondPrimeInfo().primeFactor.size(), keyData.secondPrimeInfo().primeFactor.data(),
+                    keyData.firstPrimeInfo().primeFactor.size(), keyData.firstPrimeInfo().primeFactor.data());
+                break;
+            }
+
+            error = gcry_sexp_build(&keySexp, nullptr, "(private-key(rsa(n %b)(e %b)(d %b)))",
+                keyData.modulus().size(), keyData.modulus().data(),
+                keyData.exponent().size(), keyData.exponent().data(),
+                keyData.privateExponent().size(), keyData.privateExponent().data());
+            break;
+        case CryptoKeyType::Secret:
+            ASSERT_NOT_REACHED();
+            return nullptr;
+        }
+
+        if (error != GPG_ERR_NO_ERROR) {
+            PAL::GCrypt::logError(error);
+            return nullptr;
+        }
+    }
+
+    return adoptRef(new CryptoKeyRSA(identifier, hash, hasHash, keyType, keySexp.release(), extractable, usages));
 }
 
 CryptoKeyRSA::CryptoKeyRSA(CryptoAlgorithmIdentifier identifier, CryptoAlgorithmIdentifier hash, bool hasHash, CryptoKeyType type, PlatformRSAKey platformKey, bool extractable, CryptoKeyUsageBitmap usage)
@@ -77,8 +199,7 @@ bool CryptoKeyRSA::isRestrictedToHash(CryptoAlgorithmIdentifier& identifier) con
 
 size_t CryptoKeyRSA::keySizeInBits() const
 {
-    notImplemented();
-    return 0;
+    return getRSAModulusLength(m_platformKey);
 }
 
 // Convert the exponent vector to a 32-bit value, if possible.
@@ -177,17 +298,75 @@ ExceptionOr<Vector<uint8_t>> CryptoKeyRSA::exportPkcs8() const
 
 std::unique_ptr<KeyAlgorithm> CryptoKeyRSA::buildAlgorithm() const
 {
-    notImplemented();
-    Vector<uint8_t> publicExponent;
-    return std::make_unique<RsaKeyAlgorithm>(emptyString(), 0, WTFMove(publicExponent));
+    String name = CryptoAlgorithmRegistry::singleton().name(algorithmIdentifier());
+    size_t modulusLength = getRSAModulusLength(m_platformKey);
+    Vector<uint8_t> publicExponent = getRSAKeyParameter(m_platformKey, "e");
+
+    if (m_restrictedToSpecificHash)
+        return std::make_unique<RsaHashedKeyAlgorithm>(name, modulusLength, WTFMove(publicExponent), CryptoAlgorithmRegistry::singleton().name(m_hash));
+    return std::make_unique<RsaKeyAlgorithm>(name, modulusLength, WTFMove(publicExponent));
 }
 
 std::unique_ptr<CryptoKeyData> CryptoKeyRSA::exportData() const
 {
     ASSERT(extractable());
 
-    notImplemented();
-    return nullptr;
+    switch (type()) {
+    case CryptoKeyType::Public:
+        return CryptoKeyDataRSAComponents::createPublic(getRSAKeyParameter(m_platformKey, "n"), getRSAKeyParameter(m_platformKey, "e"));
+    case CryptoKeyType::Private: {
+        auto parameterMPI =
+            [](gcry_sexp_t sexp, const char* name) -> gcry_mpi_t {
+                PAL::GCrypt::Handle<gcry_sexp_t> paramSexp(gcry_sexp_find_token(sexp, name, 0));
+                if (!paramSexp)
+                    return nullptr;
+                return gcry_sexp_nth_mpi(paramSexp, 1, GCRYMPI_FMT_USG);
+            };
+
+        PAL::GCrypt::Handle<gcry_mpi_t> dMPI(parameterMPI(m_platformKey, "d"));
+        // libgcrypt internally uses p and q such that p < q, while usually it's q < p.
+        // Switch the two primes here and continue with assuming the latter.
+        PAL::GCrypt::Handle<gcry_mpi_t> pMPI(parameterMPI(m_platformKey, "q"));
+        PAL::GCrypt::Handle<gcry_mpi_t> qMPI(parameterMPI(m_platformKey, "p"));
+        if (!dMPI || !pMPI || !qMPI)
+            return nullptr;
+
+        CryptoKeyDataRSAComponents::PrimeInfo firstPrimeInfo { getParameterMPIData(pMPI), { }, { } };
+        CryptoKeyDataRSAComponents::PrimeInfo secondPrimeInfo { getParameterMPIData(qMPI), { }, { } };
+
+        // dp -- d mod (p - 1)
+        {
+            PAL::GCrypt::Handle<gcry_mpi_t> dpMPI(gcry_mpi_new(0));
+            PAL::GCrypt::Handle<gcry_mpi_t> pm1MPI(gcry_mpi_new(0));
+            gcry_mpi_sub_ui(pm1MPI, pMPI, 1);
+            gcry_mpi_mod(dpMPI, dMPI, pm1MPI);
+            firstPrimeInfo.factorCRTExponent = getParameterMPIData(dpMPI);
+        }
+
+        // dq -- d mod (q - 1)
+        {
+            PAL::GCrypt::Handle<gcry_mpi_t> dqMPI(gcry_mpi_new(0));
+            PAL::GCrypt::Handle<gcry_mpi_t> qm1MPI(gcry_mpi_new(0));
+            gcry_mpi_sub_ui(qm1MPI, qMPI, 1);
+            gcry_mpi_mod(dqMPI, dMPI, qm1MPI);
+            secondPrimeInfo.factorCRTExponent = getParameterMPIData(dqMPI);
+        }
+
+        // qi -- q^(-1) mod p
+        {
+            PAL::GCrypt::Handle<gcry_mpi_t> qiMPI(gcry_mpi_new(0));
+            gcry_mpi_invm(qiMPI, qMPI, pMPI);
+            secondPrimeInfo.factorCRTCoefficient = getParameterMPIData(qiMPI);
+        }
+
+        return CryptoKeyDataRSAComponents::createPrivateWithAdditionalData(getRSAKeyParameter(m_platformKey, "n"),
+            getRSAKeyParameter(m_platformKey, "e"), getParameterMPIData(dMPI),
+            WTFMove(firstPrimeInfo), WTFMove(secondPrimeInfo), Vector<CryptoKeyDataRSAComponents::PrimeInfo> { });
+    }
+    default:
+        ASSERT_NOT_REACHED();
+        return nullptr;
+    }
 }
 
 } // namespace WebCore
