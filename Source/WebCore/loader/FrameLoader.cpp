bb6cd8511b21421113488a40f47dc62a8776fb0c
Antti Koivisto <antti@apple.com>
2017-05-05 18:39:30 +0000

    ASSERTION FAILED: !frame().document()->inRenderTreeUpdate() in WebCore::FrameView::layout(bool) https://bugs.webkit.org/show_bug.cgi?id=171717
    Reviewed by Brent Fulgham.
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::checkCompleted):
    
        Don't allow frame load to complete in the middle of a render tree update. Instead delay the check.
    
    
    Canonical link: https://commits.webkit.org/188633@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@216253 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/FrameLoader.cpp
+++ Source/WebCore/loader/FrameLoader.cpp
@@ -793,6 +793,13 @@ void FrameLoader::checkCompleted()
     if (m_isComplete)
         return;
 
+    // FIXME: It would be better if resource loads were kicked off after render tree update (or didn't complete synchronously).
+    //        https://bugs.webkit.org/show_bug.cgi?id=171729
+    if (m_frame.document()->inRenderTreeUpdate()) {
+        scheduleCheckCompleted();
+        return;
+    }
+
     // Are we still parsing?
     if (m_frame.document()->parsing())
         return;
