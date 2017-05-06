0d54a4358892a93c9de13e76e95fbdf3fe570446
Nael Ouedraogo <nael.ouedp@gmail.com>
2017-05-06 08:08:39 +0000

    Merge r215001 - [GTK] Build fails when using icecream, ccache and cmake 3.6 https://bugs.webkit.org/show_bug.cgi?id=170498
    Reviewed by Michael Catanzaro.
    
    Disable ninja response file when using icecream, ccache and cmake > 3.5.
    
    * Source/cmake/OptionsCommon.cmake:

--- Source/cmake/OptionsCommon.cmake
+++ Source/cmake/OptionsCommon.cmake
@@ -215,8 +215,8 @@ endif ()
 if (NOT APPLE)
    # If using Ninja with cmake >= 3.6.0 and icecream, then the build is broken
    # if enable the response files. See https://bugs.webkit.org/show_bug.cgi?id=168770
-   if (NOT ((${CMAKE_CXX_COMPILER} MATCHES ".*icecc.*") AND (CMAKE_GENERATOR STREQUAL "Ninja") AND (${CMAKE_VERSION} VERSION_GREATER 3.5)))
-      set(CMAKE_NINJA_FORCE_RESPONSE_FILE 1)
+   if (NOT ((((${CMAKE_CXX_COMPILER} MATCHES ".*ccache.*") AND ($ENV{CCACHE_PREFIX} MATCHES ".*icecc.*")) OR (${CMAKE_CXX_COMPILER} MATCHES ".*icecc.*")) AND (CMAKE_GENERATOR STREQUAL "Ninja") AND (${CMAKE_VERSION} VERSION_GREATER 3.5)))
+       set(CMAKE_NINJA_FORCE_RESPONSE_FILE 1)
    endif ()
 endif ()
 
