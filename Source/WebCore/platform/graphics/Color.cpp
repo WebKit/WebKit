WebKit/WebKit

commit 416b406d687f5eb1c7920c917f8a656207365487

Move the pixel data of ImageFrame to a separate class named ImageBackingStore https://bugs.webkit.org/show_bug.cgi?id=159679
Patch by Said Abou-Hallawa <sabouhallawa@apple.com> on 2016-09-12
Reviewed by Simon Fraser.

Move the pixel data manipulation part in ImageFrame into a separate class
and allocate it on demand.

* PlatformEfl.cmake:
* PlatformGTK.cmake:
* PlatformWinCairo.cmake:
* WebCore.xcodeproj/project.pbxproj:
Add ImageBackingStoreCairo.cpp and remove ImageDecoderCairo.cpp from the
WebKit projects.

* platform/graphics/Color.cpp:
(WebCore::premultipliedChannel): channel = channel * alpha / 255.
(WebCore::unpremultipliedChannel): channel = channel * 255 / alpha.
(WebCore::makePremultipliedRGBA): Un-premultiplied channels to premultiplied RGBA32.
(WebCore::makeUnPremultipliedRGBA): Premultiplied channels to un-premultiplied RGBA32.
(WebCore::colorFromPremultipliedARGB): Use makeUnPremultipliedRGBA.
(WebCore::premultipliedARGBFromColor): Use makePremultipliedRGBA.
* platform/graphics/Color.h:
(WebCore::fastMultiplyBy255): x * 255 = x * 256 - x = x << 8 - x.

* platform/graphics/ImageBackingStore.h: Added.
(WebCore::ImageBackingStore::create): Creates a new ImageBackingStore.
(WebCore::ImageBackingStore::setSize): Changes the pixels size.
(WebCore::ImageBackingStore::setFrameRect): This will always just be the entire buffer except for GIF and PNG frames.
(WebCore::ImageBackingStore::size): Returns the pixels size.
(WebCore::ImageBackingStore::frameRect): Returns the pixels frame rectangle.
(WebCore::ImageBackingStore::clear): Clears the entire image.
(WebCore::ImageBackingStore::clearRect): Clears a rectangle in the image.
(WebCore::ImageBackingStore::repeatFirstRow): Repeats the first row in a rectangle in the image.
(WebCore::ImageBackingStore::pixelAt): Returns a pointer to a pixel data.
(WebCore::ImageBackingStore::setPixel): Sets the color of a pixel in the image.
(WebCore::ImageBackingStore::blendPixel): Blend a color with a pixel in the image.

Source/WebCore/platform/graphics/Color.cpp
@@ -49,6 +49,16 @@ const RGBA32 Color::transparent;
 static const RGBA32 lightenedBlack = 0xFF545454;
 static const RGBA32 darkenedWhite = 0xFFABABAB;
 
+static inline unsigned premultipliedChannel(unsigned c, unsigned a)
+{
+    return fastDivideBy255(c * a + 254);
+}
+
+static inline unsigned unpremultipliedChannel(unsigned c, unsigned a)
+{
+    return (fastMultiplyBy255(c) + a - 1) / a;
+}
+
 RGBA32 makeRGB(int r, int g, int b)
 {
     return 0xFF000000 | std::max(0, std::min(r, 255)) << 16 | std::max(0, std::min(g, 255)) << 8 | std::max(0, std::min(b, 255));
@@ -57,6 +67,16 @@ RGBA32 makeRGB(int r, int g, int b)
 RGBA32 makeRGBA(int r, int g, int b, int a)
 {
     return std::max(0, std::min(a, 255)) << 24 | std::max(0, std::min(r, 255)) << 16 | std::max(0, std::min(g, 255)) << 8 | std::max(0, std::min(b, 255));
+}
+
+RGBA32 makePremultipliedRGBA(int r, int g, int b, int a)
+{
+    return makeRGBA(premultipliedChannel(r, a), premultipliedChannel(g, a), premultipliedChannel(b, a), a);
+}
+
+RGBA32 makeUnPremultipliedRGBA(int r, int g, int b, int a)
+{
+    return makeRGBA(unpremultipliedChannel(r, a), unpremultipliedChannel(g, a), unpremultipliedChannel(b, a), a);
 }
 
 static int colorFloatToRGBAByte(float f)
@@ -482,14 +502,9 @@ void Color::getHSV(double& hue, double& saturation, double& value) const
 Color colorFromPremultipliedARGB(RGBA32 pixelColor)
 {
     int alpha = alphaChannel(pixelColor);
-    if (alpha && alpha < 255) {
-        return Color::createUnchecked(
-            redChannel(pixelColor) * 255 / alpha,
-            greenChannel(pixelColor) * 255 / alpha,
-            blueChannel(pixelColor) * 255 / alpha,
-            alpha);
-    } else
-        return Color(pixelColor);
+    if (alpha && alpha < 255)
+        pixelColor = makeUnPremultipliedRGBA(redChannel(pixelColor), greenChannel(pixelColor), blueChannel(pixelColor), alpha);
+    return Color(pixelColor);
 }
 
 RGBA32 premultipliedARGBFromColor(const Color& color)
@@ -497,14 +512,10 @@ RGBA32 premultipliedARGBFromColor(const Color& color)
     unsigned pixelColor;
 
     unsigned alpha = color.alpha();
-    if (alpha < 255) {
-        pixelColor = Color::createUnchecked(
-            fastDivideBy255(color.red() * alpha + 254),
-            fastDivideBy255(color.green() * alpha + 254),
-            fastDivideBy255(color.blue() * alpha + 254),
-            alpha).rgb();
-    } else
-         pixelColor = color.rgb();
+    if (alpha < 255)
+        pixelColor = makePremultipliedRGBA(color.red(), color.green(), color.blue(), alpha);
+    else
+        pixelColor = color.rgb();
 
     return pixelColor;
 }

Source/WebCore/platform/graphics/Color.h
@@ -55,6 +55,9 @@ typedef unsigned RGBA32; // Deprecated: Type for an RGBA quadruplet. Use RGBA cl
 WEBCORE_EXPORT RGBA32 makeRGB(int r, int g, int b);
 WEBCORE_EXPORT RGBA32 makeRGBA(int r, int g, int b, int a);
 
+RGBA32 makePremultipliedRGBA(int r, int g, int b, int a);
+RGBA32 makeUnPremultipliedRGBA(int r, int g, int b, int a);
+
 WEBCORE_EXPORT RGBA32 colorWithOverrideAlpha(RGBA32 color, float overrideAlpha);
 WEBCORE_EXPORT RGBA32 colorWithOverrideAlpha(RGBA32 color, Optional<float> overrideAlpha);
 
@@ -204,6 +207,7 @@ Color blend(const Color& from, const Color& to, double progress, bool blendPremu
 
 int differenceSquared(const Color&, const Color&);
 
+uint16_t fastMultiplyBy255(uint16_t value);
 uint16_t fastDivideBy255(uint16_t);
 
 #if USE(CG)
@@ -274,6 +278,11 @@ inline uint8_t roundAndClampColorChannel(int value)
 inline uint8_t roundAndClampColorChannel(float value)
 {
     return std::max(0.f, std::min(255.f, std::round(value)));
+}
+
+inline uint16_t fastMultiplyBy255(uint16_t value)
+{
+    return (value << 8) - value;
 }
 
 inline uint16_t fastDivideBy255(uint16_t value)

Source/WebCore/platform/graphics/ImageBackingStore.h
@@ -1,0 +1,189 @@
+/*
+ * Copyright (C) 2016 Apple Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "Color.h"
+#include "IntRect.h"
+#include "IntSize.h"
+#include "NativeImage.h"
+
+#include <wtf/Vector.h>
+
+namespace WebCore {
+
+class ImageBackingStore {
+    WTF_MAKE_FAST_ALLOCATED;
+public:
+    static std::unique_ptr<ImageBackingStore> create(const IntSize& size, bool premultiplyAlpha = true)
+    {
+        return std::unique_ptr<ImageBackingStore>(new ImageBackingStore(size, premultiplyAlpha));
+    }
+
+    static std::unique_ptr<ImageBackingStore> create(const ImageBackingStore& other)
+    {
+        return std::unique_ptr<ImageBackingStore>(new ImageBackingStore(other));
+    }
+
+    NativeImagePtr image() const;
+
+    bool setSize(const IntSize& size)
+    {
+        if (size.isEmpty() || !m_pixels.tryReserveCapacity(size.area()))
+            return false;
+
+        m_pixels.resize(size.area());
+        m_pixelsPtr = m_pixels.data();
+        m_size = size;
+        m_frameRect = IntRect(IntPoint(), m_size);
+        clear();
+        return true;
+    }
+
+    void setFrameRect(const IntRect& frameRect)
+    {
+        ASSERT(!m_size.isEmpty());
+        ASSERT(inBounds(frameRect));
+        m_frameRect = frameRect;
+    }
+
+    const IntSize& size() const { return m_size; }
+    const IntRect& frameRect() const { return m_frameRect; }
+
+    void clear()
+    {
+        memset(m_pixelsPtr, 0, m_size.area() * sizeof(RGBA32));
+    }
+
+    void clearRect(const IntRect& rect)
+    {
+        if (rect.isEmpty() || !inBounds(rect))
+            return;
+
+        size_t rowBytes = rect.width() * sizeof(RGBA32);
+        RGBA32* start = pixelAt(rect.x(), rect.y());
+        for (int i = 0; i < rect.height(); ++i) {
+            memset(start, 0, rowBytes);
+            start += m_size.width();
+        }
+    }
+
+    void repeatFirstRow(const IntRect& rect)
+    {
+        if (rect.isEmpty() || !inBounds(rect))
+            return;
+
+        size_t rowBytes = rect.width() * sizeof(RGBA32);
+        RGBA32* src = pixelAt(rect.x(), rect.y());
+        RGBA32* dest = src + m_size.width();
+        for (int i = 1; i < rect.height(); ++i) {
+            memcpy(dest, src, rowBytes);
+            dest += m_size.width();
+        }
+    }
+
+    RGBA32* pixelAt(int x, int y) const
+    {
+        return m_pixelsPtr + y * m_size.width() + x;
+    }
+
+    void setPixel(int x, int y, unsigned r, unsigned g, unsigned b, unsigned a)
+    {
+        setPixel(pixelAt(x, y), r, g, b, a);
+    }
+
+    void setPixel(RGBA32* dest, unsigned r, unsigned g, unsigned b, unsigned a)
+    {
+        if (m_premultiplyAlpha && !a)
+            *dest = 0;
+        else if (m_premultiplyAlpha && a < 255)
+            *dest = makePremultipliedRGBA(r, g, b, a);
+        else
+            *dest = makeRGBA(r, g, b, a);
+    }
+
+    void blendPixel(RGBA32* dest, unsigned r, unsigned g, unsigned b, unsigned a)
+    {
+        if (!a)
+            return;
+
+        if (a >= 255 || !alphaChannel(*dest)) {
+            setPixel(dest, r, g, b, a);
+            return;
+        }
+
+        if (!m_premultiplyAlpha)
+            *dest = makePremultipliedRGBA(redChannel(*dest), greenChannel(*dest), blueChannel(*dest), alphaChannel(*dest));
+
+        unsigned d = 255 - a;
+
+        r = fastDivideBy255(r * a + redChannel(*dest) * d);
+        g = fastDivideBy255(g * a + greenChannel(*dest) * d);
+        b = fastDivideBy255(b * a + blueChannel(*dest) * d);
+        a += fastDivideBy255(d * alphaChannel(*dest));
+
+        if (m_premultiplyAlpha)
+            *dest = makeRGBA(r, g, b, a);
+        else
+            *dest = makeUnPremultipliedRGBA(r, g, b, a);
+    }
+
+    bool inBounds(const IntRect& rect) const
+    {
+        return IntRect(IntPoint(), m_size).contains(rect);
+    }
+
+    static bool isOverSize(const IntSize& size)
+    {
+        static unsigned long long MaxPixels = ((1 << 29) - 1);
+        unsigned long long pixels = static_cast<unsigned long long>(size.width()) * static_cast<unsigned long long>(size.height());
+        return pixels > MaxPixels;
+    }
+
+private:
+    ImageBackingStore(const IntSize& size, bool premultiplyAlpha = true)
+        : m_premultiplyAlpha(premultiplyAlpha)
+    {
+        ASSERT(!size.isEmpty() && !isOverSize(size));
+        setSize(size);
+    }
+
+    ImageBackingStore(const ImageBackingStore& other)
+        : m_pixels(other.m_pixels)
+        , m_size(other.m_size)
+        , m_premultiplyAlpha(other.m_premultiplyAlpha)
+    {
+        ASSERT(!m_size.isEmpty() && !isOverSize(m_size));
+        m_pixelsPtr = m_pixels.data();
+    }
+
+    Vector<RGBA32> m_pixels;
+    RGBA32* m_pixelsPtr { nullptr };
+    IntSize m_size;
+    IntRect m_frameRect; // This will always just be the entire buffer except for GIF and PNG frames
+    bool m_premultiplyAlpha { true };
+};
+
+}

Source/WebCore/PlatformEfl.cmake
@@ -183,9 +183,8 @@ list(APPEND WebCore_SOURCES
     platform/graphics/x11/PlatformDisplayX11.cpp
     platform/graphics/x11/XUniqueResource.cpp
 
+    platform/image-decoders/cairo/ImageBackingStoreCairo.cpp
     platform/image-encoders/JPEGImageEncoder.cpp
-
-    platform/image-decoders/cairo/ImageDecoderCairo.cpp
 
     platform/network/efl/NetworkStateNotifierEfl.cpp
 

Source/WebCore/PlatformGTK.cmake
@@ -155,7 +155,7 @@ list(APPEND WebCore_SOURCES
     platform/gtk/TemporaryLinkStubs.cpp
     platform/gtk/UserAgentGtk.cpp
 
-    platform/image-decoders/cairo/ImageDecoderCairo.cpp
+    platform/image-decoders/cairo/ImageBackingStoreCairo.cpp
 
     platform/mediastream/gtk/SDPProcessorScriptResourceGtk.cpp
 

Source/WebCore/PlatformWinCairo.cmake
@@ -46,7 +46,7 @@ list(APPEND WebCore_SOURCES
     platform/graphics/win/MediaPlayerPrivateMediaFoundation.cpp
     platform/graphics/win/SimpleFontDataCairoWin.cpp
 
-    platform/image-decoders/cairo/ImageDecoderCairo.cpp
+    platform/image-decoders/cairo/ImageBackingStoreCairo.cpp
 
     platform/network/NetworkStorageSessionStub.cpp
 

Source/WebCore/WebCore.xcodeproj/project.pbxproj
@@ -2275,6 +2275,7 @@
 		555B87EC1CAAF0AB00349425 /* ImageDecoderCG.cpp in Sources */ = {isa = PBXBuildFile; fileRef = 555B87EA1CAAF0AB00349425 /* ImageDecoderCG.cpp */; };
 		555B87ED1CAAF0AB00349425 /* ImageDecoderCG.h in Headers */ = {isa = PBXBuildFile; fileRef = 555B87EB1CAAF0AB00349425 /* ImageDecoderCG.h */; };
 		55A336F71D8209F40022C4C7 /* NativeImage.h in Headers */ = {isa = PBXBuildFile; fileRef = 55A336F61D8209F40022C4C7 /* NativeImage.h */; };
+		55A336F91D821E3C0022C4C7 /* ImageBackingStore.h in Headers */ = {isa = PBXBuildFile; fileRef = 55A336F81D821E3C0022C4C7 /* ImageBackingStore.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		5709E8CD1D413D47003244AC /* WebKitSubtleCrypto.cpp in Sources */ = {isa = PBXBuildFile; fileRef = 5709E8CA1D413CE3003244AC /* WebKitSubtleCrypto.cpp */; };
 		5709E8CE1D413D5B003244AC /* JSWebKitSubtleCryptoCustom.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E1FF8F661807460800132674 /* JSWebKitSubtleCryptoCustom.cpp */; };
 		5709E8CF1D413D9A003244AC /* WebKitSubtleCrypto.h in Headers */ = {isa = PBXBuildFile; fileRef = 5709E8CB1D413CE3003244AC /* WebKitSubtleCrypto.h */; };
@@ -9298,6 +9299,7 @@
 		555B87EA1CAAF0AB00349425 /* ImageDecoderCG.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = ImageDecoderCG.cpp; sourceTree = "<group>"; };
 		555B87EB1CAAF0AB00349425 /* ImageDecoderCG.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = ImageDecoderCG.h; sourceTree = "<group>"; };
 		55A336F61D8209F40022C4C7 /* NativeImage.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = NativeImage.h; sourceTree = "<group>"; };
+		55A336F81D821E3C0022C4C7 /* ImageBackingStore.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = ImageBackingStore.h; sourceTree = "<group>"; };
 		55D408F71A7C631800C78450 /* SVGImageClients.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = SVGImageClients.h; sourceTree = "<group>"; };
 		5709E8CA1D413CE3003244AC /* WebKitSubtleCrypto.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = WebKitSubtleCrypto.cpp; sourceTree = "<group>"; };
 		5709E8CB1D413CE3003244AC /* WebKitSubtleCrypto.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = WebKitSubtleCrypto.h; sourceTree = "<group>"; };
@@ -20791,6 +20793,7 @@
 				379919941200DDF400EA041C /* WOFFFileFormat.cpp */,
 				379919951200DDF400EA041C /* WOFFFileFormat.h */,
 				55A336F61D8209F40022C4C7 /* NativeImage.h */,
+				55A336F81D821E3C0022C4C7 /* ImageBackingStore.h */,
 			);
 			path = graphics;
 			sourceTree = "<group>";
@@ -26364,6 +26367,7 @@
 				582CB0531A78A14B00AFFCC4 /* SimpleLineLayoutTextFragmentIterator.h in Headers */,
 				C5A1EA7D152BCF08004D00B6 /* SimplifyMarkupCommand.h in Headers */,
 				572A7F211C6E5719009C6149 /* SimulatedClick.h in Headers */,
+				55A336F91D821E3C0022C4C7 /* ImageBackingStore.h in Headers */,
 				31741AAD16636609008A5B7E /* SimulatedClickOptions.h in Headers */,
 				FD00D7A514A3F61900734011 /* SincResampler.h in Headers */,
 				51327D6011A33A2B004F9D65 /* SinkDocument.h in Headers */,

Source/WebCore/platform/graphics/cg/NativeImageCG.cpp
@@ -47,10 +47,10 @@ IntSize nativeImageSize(const NativeImagePtr& image)
     return image ? IntSize(CGImageGetWidth(image.get()), CGImageGetHeight(image.get())) : IntSize();
 }
 
-bool nativeImageHasAlpha(const NativeImagePtr&)
+bool nativeImageHasAlpha(const NativeImagePtr& image)
 {
-    // FIXME: Answer correctly the question: does the CGImageRef have alpha channnel?
-    return true;
+    CGImageAlphaInfo info = CGImageGetAlphaInfo(image.get());
+    return (info >= kCGImageAlphaPremultipliedLast) && (info <= kCGImageAlphaFirst);
 }
 
 Color nativeImageSinglePixelSolidColor(const NativeImagePtr& image)

commit 89f4bf87cd6acad1b5e7bed006a219619b2eb131

Merge r215172 - REGRESSION(r205841): [GTK] Test fast/images/animated-png.html is failing since r205841 https://bugs.webkit.org/show_bug.cgi?id=168425
Reviewed by Said Abou-Hallawa.

Source/WebCore:

There is a problem with animations that are blending their frames into the previous frame. Due to a change
in how pixel components are premultiplied (the result is now rounded up), the parameters to the blending
operation may vary in one unit, causing the result of the blending to be different from the expected result.
In order to fix this, a new parameter is added to indicate whether we want to use rounding up when
premultiplying or not, and ImageBackingStore uses that parameter to disable rounding up.

Adjusted the expectation for fast/images/animated-png.html, as it must pass now.

* platform/graphics/Color.cpp:
(WebCore::premultipliedChannel):
(WebCore::makePremultipliedRGBA):
* platform/graphics/Color.h:
* platform/graphics/ImageBackingStore.h:
(WebCore::ImageBackingStore::blendPixel):
(WebCore::ImageBackingStore::pixelValue):

LayoutTests:

Adjusted expectation for fast/images/animated-png.html. It must pass now.

* platform/gtk/TestExpectations:

Source/WebCore/platform/graphics/Color.cpp
@@ -49,9 +49,9 @@ const RGBA32 Color::transparent;
 static const RGBA32 lightenedBlack = 0xFF545454;
 static const RGBA32 darkenedWhite = 0xFFABABAB;
 
-static inline unsigned premultipliedChannel(unsigned c, unsigned a)
+static inline unsigned premultipliedChannel(unsigned c, unsigned a, bool ceiling = true)
 {
-    return fastDivideBy255(c * a + 254);
+    return fastDivideBy255(ceiling ? c * a + 254 : c * a);
 }
 
 static inline unsigned unpremultipliedChannel(unsigned c, unsigned a)
@@ -69,9 +69,9 @@ RGBA32 makeRGBA(int r, int g, int b, int a)
     return std::max(0, std::min(a, 255)) << 24 | std::max(0, std::min(r, 255)) << 16 | std::max(0, std::min(g, 255)) << 8 | std::max(0, std::min(b, 255));
 }
 
-RGBA32 makePremultipliedRGBA(int r, int g, int b, int a)
+RGBA32 makePremultipliedRGBA(int r, int g, int b, int a, bool ceiling)
 {
-    return makeRGBA(premultipliedChannel(r, a), premultipliedChannel(g, a), premultipliedChannel(b, a), a);
+    return makeRGBA(premultipliedChannel(r, a, ceiling), premultipliedChannel(g, a, ceiling), premultipliedChannel(b, a, ceiling), a);
 }
 
 RGBA32 makeUnPremultipliedRGBA(int r, int g, int b, int a)

Source/WebCore/platform/graphics/Color.h
@@ -65,7 +65,7 @@ typedef unsigned RGBA32; // Deprecated: Type for an RGBA quadruplet. Use RGBA cl
 WEBCORE_EXPORT RGBA32 makeRGB(int r, int g, int b);
 WEBCORE_EXPORT RGBA32 makeRGBA(int r, int g, int b, int a);
 
-RGBA32 makePremultipliedRGBA(int r, int g, int b, int a);
+RGBA32 makePremultipliedRGBA(int r, int g, int b, int a, bool ceiling = true);
 RGBA32 makeUnPremultipliedRGBA(int r, int g, int b, int a);
 
 WEBCORE_EXPORT RGBA32 colorWithOverrideAlpha(RGBA32 color, float overrideAlpha);

Source/WebCore/platform/graphics/ImageBackingStore.h
@@ -151,7 +151,7 @@ public:
         }
 
         if (!m_premultiplyAlpha)
-            *dest = makePremultipliedRGBA(redChannel(*dest), greenChannel(*dest), blueChannel(*dest), alphaChannel(*dest));
+            *dest = makePremultipliedRGBA(redChannel(*dest), greenChannel(*dest), blueChannel(*dest), alphaChannel(*dest), false);
 
         unsigned d = 255 - a;
 
@@ -207,7 +207,7 @@ private:
             return 0;
 
         if (m_premultiplyAlpha && a < 255)
-            return makePremultipliedRGBA(r, g, b, a);
+            return makePremultipliedRGBA(r, g, b, a, false);
 
         return makeRGBA(r, g, b, a);
     }

LayoutTests/platform/gtk/TestExpectations
@@ -2863,8 +2863,6 @@ webkit.org/b/136109 fast/multicol/mixed-opacity-fixed-test.html [ ImageOnlyFailu
 webkit.org/b/168427 fast/scrolling/rtl-scrollbars-listbox.html [ ImageOnlyFailure ]
 webkit.org/b/168427 fast/scrolling/rtl-scrollbars-overflow-contents.html [ ImageOnlyFailure ]
 
-webkit.org/b/168425 fast/images/animated-png.html [ ImageOnlyFailure ]
-
 webkit.org/b/168428 imported/blink/css3/blending/mix-blend-mode-multiply.html [ ImageOnlyFailure ]
 
 webkit.org/b/168429 fast/text/simple-line-layout-simple-text-but-complex-font-path.html [ ImageOnlyFailure ]