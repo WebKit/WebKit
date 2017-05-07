6fed7ba08cae7c4674c4d919ed8dd228fb5a2855
Michael Catanzaro <mcatanzaro@igalia.com>
2017-05-07 09:29:45 +0000

    [GTK] Cannot sign in with new Google sign-in page https://bugs.webkit.org/show_bug.cgi?id=171770
    Reviewed by Carlos Garcia Campos.
    
    Google's new authentication page does not work with the Firefox user
    agent that's required to make various Google websites work. Special-case
    accounts.google.com so that it receives our standard user agent.
    
    * platform/UserAgentQuirks.cpp:
    (WebCore::isGoogle):
    (WebCore::urlRequiresFirefoxBrowser):
    
    
    Canonical link: https://commits.webkit.org/188699@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@216343 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/UserAgentQuirks.cpp
+++ Source/WebCore/platform/UserAgentQuirks.cpp
@@ -41,8 +41,9 @@ static bool isGoogle(const URL& url)
     // Our Google UA is *very* complicated to get right. Read
     // https://webkit.org/b/142074 carefully before changing. Test that Earth
     // view is available in Google Maps. Test Google Calendar. Test downloading
-    // the Hangouts browser plugin. Change platformVersionForUAString() to
-    // return "FreeBSD amd64" and test Maps and Calendar again.
+    // the Hangouts browser plugin. Test logging out and logging in to a Google
+    // account. Change platformVersionForUAString() to return "FreeBSD amd64"
+    // and test everything again.
     if (baseDomain.startsWith("google."))
         return true;
     if (baseDomain == "gstatic.com")
@@ -75,7 +76,7 @@ static bool urlRequiresChromeBrowser(const URL& url)
 
 static bool urlRequiresFirefoxBrowser(const URL& url)
 {
-    return isGoogle(url);
+    return isGoogle(url) && url.host() != "accounts.google.com";
 }
 
 static bool urlRequiresMacintoshPlatform(const URL& url)
