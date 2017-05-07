02fb144f551762288e29bb34b223d8f7cab798b2
Antti Koivisto <koivisto@iki.fi>
2017-05-07 09:56:48 +0000

    Merge r215347 - Don't invalidate composition for style changes in non-composited layers https://bugs.webkit.org/show_bug.cgi?id=170805 <rdar://problem/31606185>
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    Test: compositing/updates/animation-non-composited.html
    
    In most cases they can't affect composition. Composition updates are expensive, this can
    save a lot of work (tumblr.com animations hit this at the moment).
    
    * rendering/RenderElement.h:
    (WebCore::RenderElement::createsGroup):
    (WebCore::RenderElement::createsGroupForStyle):
    
        Factor to a static function so we can test style directly.
    
    * rendering/RenderLayerCompositor.cpp:
    (WebCore::RenderLayerCompositor::layerStyleChanged):
    (WebCore::RenderLayerCompositor::styleChangeMayAffectIndirectCompositingReasons):
    
        Test if style change might cause compositing change that can't be determined without compositing update.
    
    * rendering/RenderLayerCompositor.h:
    
    LayoutTests:
    
    * compositing/updates/animation-non-composited-expected.txt: Added.
    * compositing/updates/animation-non-composited.html: Added.

--- Source/WebCore/rendering/RenderElement.h
+++ Source/WebCore/rendering/RenderElement.h
@@ -139,7 +139,8 @@ public:
     bool mayCauseRepaintInsideViewport(const IntRect* visibleRect = nullptr) const;
 
     // Returns true if this renderer requires a new stacking context.
-    bool createsGroup() const { return isTransparent() || hasMask() || hasClipPath() || hasFilter() || hasBackdropFilter() || hasBlendMode(); }
+    static bool createsGroupForStyle(const RenderStyle&);
+    bool createsGroup() const { return createsGroupForStyle(style()); }
 
     bool isTransparent() const { return style().opacity() < 1.0f; }
     float opacity() const { return style().opacity(); }
@@ -426,6 +427,11 @@ inline bool RenderElement::canContainAbsolutelyPositionedObjects() const
         || (isRenderBlock() && hasTransformRelatedProperty())
         || isSVGForeignObject()
         || isRenderView();
+}
+
+inline bool RenderElement::createsGroupForStyle(const RenderStyle& style)
+{
+    return style.opacity() < 1.0f || style.hasMask() || style.clipPath() || style.hasFilter() || style.hasBackdropFilter() || style.hasBlendMode();
 }
 
 inline bool RenderObject::isRenderLayerModelObject() const
