1bff84601bfd38af943385e63cf7d8f6d340ff85
Chris Dumez <cdumez@apple.com>
2017-05-07 05:18:34 +0000

    Implement the concept of cookie-averse document https://bugs.webkit.org/show_bug.cgi?id=171746 <rdar://problem/32004466>
    Reviewed by Sam Weinig.
    
    Source/WebCore:
    
    Implement the concept of cookie-averse document:
    - https://html.spec.whatwg.org/#cookie-averse-document-object
    
    Test: fast/cookies/cookie-averse-document.html
    
    * dom/Document.cpp:
    (WebCore::Document::isCookieAverse):
    (WebCore::Document::cookie):
    (WebCore::Document::setCookie):
    * dom/Document.h:
    
    LayoutTests:
    
    Add layout test coverage.
    
    * fast/cookies/cookie-averse-document-expected.txt: Added.
    * fast/cookies/cookie-averse-document.html: Added.
    
    Canonical link: https://commits.webkit.org/188697@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@216341 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/Document.cpp
+++ Source/WebCore/dom/Document.cpp
@@ -4197,14 +4197,31 @@ HTMLFrameOwnerElement* Document::ownerElement() const
     return frame()->ownerElement();
 }
 
+// https://html.spec.whatwg.org/#cookie-averse-document-object
+bool Document::isCookieAverse() const
+{
+    // A Document that has no browsing context is cookie-averse.
+    if (!frame())
+        return true;
+
+    URL cookieURL = this->cookieURL();
+
+    // This is not part of the specification but we have historically allowed cookies over file protocol
+    // and some developers rely on this for testing.
+    if (cookieURL.isLocalFile())
+        return false;
+
+    // A Document whose URL's scheme is not a network scheme is cookie-averse (https://fetch.spec.whatwg.org/#network-scheme).
+    return !cookieURL.protocolIsInHTTPFamily() && !cookieURL.protocolIs("ftp");
+}
+
 ExceptionOr<String> Document::cookie()
 {
     if (page() && !page()->settings().cookieEnabled())
         return String();
 
-    // FIXME: The HTML5 DOM spec states that this attribute can raise an
-    // INVALID_STATE_ERR exception on getting if the Document has no
-    // browsing context.
+    if (isCookieAverse())
+        return String();
 
     if (!securityOrigin().canAccessCookies())
         return Exception { SECURITY_ERR };
@@ -4224,9 +4241,8 @@ ExceptionOr<void> Document::setCookie(const String& value)
     if (page() && !page()->settings().cookieEnabled())
         return { };
 
-    // FIXME: The HTML5 DOM spec states that this attribute can raise an
-    // INVALID_STATE_ERR exception on setting if the Document has no
-    // browsing context.
+    if (isCookieAverse())
+        return { };
 
     if (!securityOrigin().canAccessCookies())
         return Exception { SECURITY_ERR };
