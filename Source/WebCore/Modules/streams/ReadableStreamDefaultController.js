27816818d29b9208b3c49fac02a44c16118a1167
Matt Hanson <matthew_hanson@apple.com>
2017-05-10 21:23:45 +0000

    Additional ENABLE_STREAMS cleanup
    Canonical link: https://commits.webkit.org/183607.545@safari-603-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-603-branch@216622 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/streams/ReadableStreamDefaultController.js
+++ Source/WebCore/Modules/streams/ReadableStreamDefaultController.js
@@ -23,7 +23,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-// @conditional=ENABLE(READABLE_STREAM_API)
+// @conditional=ENABLE(STREAMS_API)
 
 function enqueue(chunk)
 {
--- Source/WebCore/Modules/streams/WritableStreamInternals.js
+++ Source/WebCore/Modules/streams/WritableStreamInternals.js
@@ -24,7 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-// @conditional=ENABLE(WRITABLE_STREAM_API)
+// @conditional=ENABLE(STREAMS_API)
 // @internal
 
 function isWritableStream(stream)
