5a01ce2b4e10de6e20b00aee889f82bfa87f93be
Chris Dumez <cdumez@apple.com>
2017-05-15 23:36:20 +0000

    Unreviewed, rebaseline platform/ios/ios/css/construct-WebKitCSSMatrix.html after r216881.
    * platform/ios/ios/css/construct-WebKitCSSMatrix-expected.txt:
    * platform/ios/ios/css/resources/construct-WebKitCSSMatrix.js:
    
    Canonical link: https://commits.webkit.org/189065@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@216888 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/platform/ios/ios/css/resources/construct-WebKitCSSMatrix.js
+++ LayoutTests/platform/ios/ios/css/resources/construct-WebKitCSSMatrix.js
@@ -3,12 +3,12 @@ description(
 );
 
 shouldBe("WebKitCSSMatrix.prototype.toString.call(new WebKitCSSMatrix)",
-         "'matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)'");
+         "'matrix(1, 0, 0, 1, 0, 0)'");
 
 shouldBe("WebKitCSSMatrix.prototype.toString.call(new WebKitCSSMatrix(new WebKitCSSMatrix))",
-         "'matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)'");
+         "'matrix(1, 0, 0, 1, 0, 0)'");
 
 shouldBe("WebKitCSSMatrix.prototype.toString.call(new WebKitCSSMatrix('matrix3d(1.000000, 0.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 0.000000, 1.000000)'))",
-         "'matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)'");
+         "'matrix(1, 0, 0, 1, 0, 0)'");
 
 var successfullyParsed = true;
