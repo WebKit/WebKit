8ce370cd988a436f185077962ce090afea417f2d
Yusuke Suzuki <utatane.tea@gmail.com>
2017-05-16 09:40:52 +0000

    Unreviewed, add Objective C files to CMake Mac port https://bugs.webkit.org/show_bug.cgi?id=172103
    * shell/PlatformMac.cmake: Added.
    
    Canonical link: https://commits.webkit.org/189094@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@216924 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/JavaScriptCore/shell/PlatformMac.cmake
@@ -0,0 +1,8 @@
+list(APPEND TESTAPI_SOURCES
+    ../API/tests/CurrentThisInsideBlockGetterTest.mm
+    ../API/tests/DateTests.mm
+    ../API/tests/JSExportTests.mm
+    ../API/tests/Regress141275.mm
+    ../API/tests/Regress141809.mm
+    ../API/tests/testapi.mm
+)
