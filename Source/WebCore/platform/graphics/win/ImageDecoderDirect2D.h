2e037a69998a3802c4252e6b77f1753d3e5d49fe
Said Abou-Hallawa <sabouhallawa@apple.com>
2017-05-17 20:58:42 +0000

    When the image decoding thread makes a callOnMainThread(), ensure all the objects it needs are protected https://bugs.webkit.org/show_bug.cgi?id=171614
    Patch by Said Abou-Hallawa <sabouhallawa@apple.com> on 2017-05-17
    Reviewed by David Kilzer.
    
    The asynchronous image decoding was designed to not block the main thread if
    the image is deleted. To achieve that we allow decoding the current frame
    even if it is not going to be used after closing the decoding queue. We
    protect all the objects which the decoding thread uses. But when a frame
    finishes decoding the native image frame is cached on the main thread. Not
    all of the objects are protected when the callOnMainThread() is dispatched.
    The ImageFrameCache and the ImageDecoder objects are not protected.
    
    This might lead to two kinds of crashes:
    1. A segfault inside the ImageDecoder trying to access one of its member
    2. A segfault inside the ImageFrameCache trying to access one of its frames
    
    The fix is to protect the ImageFrameCache and the ImageDecoder when the
    decoding thread makes a callOnMainThread(). Also switch all the pointers
    the decoding threads protect to be ThreadSafeRefCounted.
    
    * platform/graphics/ImageFrameCache.cpp:
    (WebCore::ImageFrameCache::startAsyncDecodingQueue):
    * platform/graphics/ImageFrameCache.h:
    * platform/graphics/cg/ImageDecoderCG.h:
    * platform/graphics/win/ImageDecoderDirect2D.h:
    * platform/image-decoders/ImageDecoder.h:
    
    Canonical link: https://commits.webkit.org/189157@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@216998 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/win/ImageDecoderDirect2D.h
+++ Source/WebCore/platform/graphics/win/ImageDecoderDirect2D.h
@@ -36,7 +36,7 @@ interface IWICImagingFactory;
 
 namespace WebCore {
 
-class ImageDecoder : public RefCounted<ImageDecoder> {
+class ImageDecoder : public ThreadSafeRefCounted<ImageDecoder> {
     WTF_MAKE_FAST_ALLOCATED;
 public:
     ImageDecoder();
