4b29d11a3fa54fabd61086824573b50ce91f8a59
Wenson Hsieh <wenson_hsieh@apple.com>
2017-05-19 01:44:56 +0000

    Attachment drag preview should not have the attachment outline https://bugs.webkit.org/show_bug.cgi?id=172327 <rdar://problem/32282831>
    Reviewed by Tim Horton.
    
    When creating a drag image for an attachment element, don't include borders around the attachment.
    
    * page/DragController.cpp:
    (WebCore::DragController::startDrag):
    * rendering/RenderAttachment.h:
    * rendering/RenderThemeIOS.mm:
    (WebCore::RenderThemeIOS::paintAttachment):
    
    
    Canonical link: https://commits.webkit.org/189225@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@217083 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/DragController.cpp
+++ Source/WebCore/page/DragController.cpp
@@ -65,6 +65,7 @@
 #include "PluginDocument.h"
 #include "PluginViewBase.h"
 #include "Position.h"
+#include "RenderAttachment.h"
 #include "RenderFileUploadControl.h"
 #include "RenderImage.h"
 #include "RenderView.h"
@@ -1056,6 +1057,10 @@ bool DragController::startDrag(Frame& src, const DragState& state, DragOperation
 
 #if ENABLE(ATTACHMENT_ELEMENT)
     if (is<HTMLAttachmentElement>(element) && m_dragSourceAction & DragSourceActionAttachment) {
+        auto* attachmentRenderer = downcast<HTMLAttachmentElement>(element).renderer();
+        if (!attachmentRenderer)
+            return false;
+
         src.editor().setIgnoreSelectionChanges(true);
         auto previousSelection = src.selection().selection();
         if (!dataTransfer.pasteboard().hasData()) {
@@ -1079,7 +1084,9 @@ bool DragController::startDrag(Frame& src, const DragState& state, DragOperation
         
         if (!dragImage) {
             TextIndicatorData textIndicator;
+            attachmentRenderer->setShouldDrawBorder(false);
             dragImage = DragImage { dissolveDragImageToFraction(createDragImageForSelection(src, textIndicator), DragImageAlpha) };
+            attachmentRenderer->setShouldDrawBorder(true);
             if (textIndicator.contentImage)
                 dragImage.setIndicatorData(textIndicator);
             dragLoc = dragLocForSelectionDrag(src);
