52a5d21cbda3cf5966b9acdcf15cef7804fad90c
Žan Doberšek <zdobersek@igalia.com>
2017-05-30 07:45:10 +0000

    [GCrypt] RSAES-PKCS1-v1_5 support https://bugs.webkit.org/show_bug.cgi?id=171219
    Reviewed by Michael Catanzaro.
    
    Source/WebCore:
    
    Add RSAES-PKCS1-v1_5 support for configurations that use libgcrypt.
    
    The encryption operation embeds the plain-text data into a data s-expression,
    specifying PKCS#1 as the padding method of choice. That's passed to the
    gcry_pk_encrypt() call together with the specified key. The returned enc-val
    expression contains the 'a' MPI from which the data is extracted and returned.
    
    The decryption operation goes in reverse -- the cipher-text data is embedded
    into an enc-val s-expression that again also specifies PKCS#1 as the padding
    method. The s-expression is passed to gcry_pk_decrypt() together with the
    specified key. The returned data s-expression contains the decrypted text
    inside the 'value' parameter, so the data is extracted from that and returned
    to the caller.
    
    No new tests -- the revelant tests are passing and are unskipped, apart from
    the ones using PKCS#8 and SPKI formats.
    
    * crypto/gcrypt/CryptoAlgorithmRSAES_PKCS1_v1_5GCrypt.cpp:
    (WebCore::mpiData):
    (WebCore::gcryptEncrypt):
    (WebCore::gcryptDecrypt):
    (WebCore::CryptoAlgorithmRSAES_PKCS1_v1_5::platformEncrypt):
    (WebCore::CryptoAlgorithmRSAES_PKCS1_v1_5::platformDecrypt):
    
    LayoutTests:
    
    * platform/gtk/TestExpectations: Enable the RSAES-PKCS1-v1_5 layout tests that we pass.
    
    
    Canonical link: https://commits.webkit.org/189596@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@217546 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/crypto/gcrypt/CryptoAlgorithmRSAES_PKCS1_v1_5GCrypt.cpp
+++ Source/WebCore/crypto/gcrypt/CryptoAlgorithmRSAES_PKCS1_v1_5GCrypt.cpp
@@ -31,17 +31,150 @@
 #include "CryptoKeyRSA.h"
 #include "ExceptionCode.h"
 #include "NotImplemented.h"
+#include "ScriptExecutionContext.h"
+#include <pal/crypto/gcrypt/Handle.h>
+#include <pal/crypto/gcrypt/Utilities.h>
 
 namespace WebCore {
 
-void CryptoAlgorithmRSAES_PKCS1_v1_5::platformEncrypt(Ref<CryptoKey>&&, Vector<uint8_t>&&, VectorCallback&&, ExceptionCallback&&, ScriptExecutionContext&, WorkQueue&)
+static std::optional<Vector<uint8_t>> mpiData(gcry_sexp_t paramSexp)
 {
-    notImplemented();
+    // Retrieve the MPI value stored in the s-expression: (name mpi-data)
+    PAL::GCrypt::Handle<gcry_mpi_t> paramMPI(gcry_sexp_nth_mpi(paramSexp, 1, GCRYMPI_FMT_USG));
+    if (!paramMPI)
+        return std::nullopt;
+
+    // Query the data length first to properly prepare the buffer.
+    size_t dataLength = 0;
+    gcry_error_t error = gcry_mpi_print(GCRYMPI_FMT_USG, nullptr, 0, &dataLength, paramMPI);
+    if (error != GPG_ERR_NO_ERROR) {
+        PAL::GCrypt::logError(error);
+        return std::nullopt;
+    }
+
+    // Finally, copy the MPI data into a properly-sized buffer.
+    Vector<uint8_t> output(dataLength);
+    error = gcry_mpi_print(GCRYMPI_FMT_USG, output.data(), output.size(), nullptr, paramMPI);
+    if (error != GPG_ERR_NO_ERROR) {
+        PAL::GCrypt::logError(error);
+        return std::nullopt;
+    }
+
+    return output;
 }
 
-void CryptoAlgorithmRSAES_PKCS1_v1_5::platformDecrypt(Ref<CryptoKey>&&, Vector<uint8_t>&&, VectorCallback&&, ExceptionCallback&&, ScriptExecutionContext&, WorkQueue&)
+static std::optional<Vector<uint8_t>> gcryptEncrypt(gcry_sexp_t keySexp, Vector<uint8_t>&& plainText)
 {
-    notImplemented();
+    // Embed the plain-text data in a data s-expression using PKCS#1 padding.
+    PAL::GCrypt::Handle<gcry_sexp_t> dataSexp;
+    gcry_error_t error = gcry_sexp_build(&dataSexp, nullptr, "(data(flags pkcs1)(value %b))",
+        plainText.size(), plainText.data());
+    if (error != GPG_ERR_NO_ERROR) {
+        PAL::GCrypt::logError(error);
+        return std::nullopt;
+    }
+
+    // Encrypt data with the provided key. The returned s-expression is of this form:
+    // (enc-val
+    //   (rsa
+    //     (a a-mpi)))
+    PAL::GCrypt::Handle<gcry_sexp_t> cipherSexp;
+    error = gcry_pk_encrypt(&cipherSexp, dataSexp, keySexp);
+    if (error != GPG_ERR_NO_ERROR) {
+        PAL::GCrypt::logError(error);
+        return std::nullopt;
+    }
+
+    // Return MPI data of the embedded a integer.
+    PAL::GCrypt::Handle<gcry_sexp_t> aSexp(gcry_sexp_find_token(cipherSexp, "a", 0));
+    if (!aSexp)
+        return std::nullopt;
+
+    return mpiData(aSexp);
+}
+
+static std::optional<Vector<uint8_t>> gcryptDecrypt(gcry_sexp_t keySexp, Vector<uint8_t>&& cipherText)
+{
+    // Embed the cipher-text data in an enc-val s-expression using PKCS#1 padding.
+    PAL::GCrypt::Handle<gcry_sexp_t> encValSexp;
+    gcry_error_t error = gcry_sexp_build(&encValSexp, nullptr, "(enc-val(flags pkcs1)(rsa(a %b)))",
+        cipherText.size(), cipherText.data());
+    if (error != GPG_ERR_NO_ERROR) {
+        PAL::GCrypt::logError(error);
+        return std::nullopt;
+    }
+
+    // Decrypt data with the provided key. The returned s-expression is of this form:
+    // (data
+    //   (flags pkcs1)
+    //   (value block))
+    PAL::GCrypt::Handle<gcry_sexp_t> plainSexp;
+    error = gcry_pk_decrypt(&plainSexp, encValSexp, keySexp);
+    if (error != GPG_ERR_NO_ERROR) {
+        PAL::GCrypt::logError(error);
+        return std::nullopt;
+    }
+
+    // Return MPI data of the embedded value integer.
+    PAL::GCrypt::Handle<gcry_sexp_t> valueSexp(gcry_sexp_find_token(plainSexp, "value", 0));
+    if (!valueSexp)
+        return std::nullopt;
+
+    return mpiData(valueSexp);
+}
+
+void CryptoAlgorithmRSAES_PKCS1_v1_5::platformEncrypt(Ref<CryptoKey>&& key, Vector<uint8_t>&& plainText, VectorCallback&& callback, ExceptionCallback&& exceptionCallback, ScriptExecutionContext& context, WorkQueue& workQueue)
+{
+    context.ref();
+    workQueue.dispatch(
+        [key = WTFMove(key), plainText = WTFMove(plainText), callback = WTFMove(callback), exceptionCallback = WTFMove(exceptionCallback), &context]() mutable {
+            auto& rsaKey = downcast<CryptoKeyRSA>(key.get());
+
+            auto output = gcryptEncrypt(rsaKey.platformKey(), WTFMove(plainText));
+            if (!output) {
+                // We should only dereference callbacks after being back to the Document/Worker threads.
+                context.postTask(
+                    [callback = WTFMove(callback), exceptionCallback = WTFMove(exceptionCallback)](ScriptExecutionContext& context) {
+                        exceptionCallback(OperationError);
+                        context.deref();
+                    });
+                return;
+            }
+
+            // We should only dereference callbacks after being back to the Document/Worker threads.
+            context.postTask(
+                [output = WTFMove(*output), callback = WTFMove(callback), exceptionCallback = WTFMove(exceptionCallback)](ScriptExecutionContext& context) mutable {
+                    callback(WTFMove(output));
+                    context.deref();
+                });
+        });
+}
+
+void CryptoAlgorithmRSAES_PKCS1_v1_5::platformDecrypt(Ref<CryptoKey>&& key, Vector<uint8_t>&& cipherText, VectorCallback&& callback, ExceptionCallback&& exceptionCallback, ScriptExecutionContext& context, WorkQueue& workQueue)
+{
+    context.ref();
+    workQueue.dispatch(
+        [key = WTFMove(key), cipherText = WTFMove(cipherText), callback = WTFMove(callback), exceptionCallback = WTFMove(exceptionCallback), &context]() mutable {
+            auto& rsaKey = downcast<CryptoKeyRSA>(key.get());
+
+            auto output = gcryptDecrypt(rsaKey.platformKey(), WTFMove(cipherText));
+            if (!output) {
+                // We should only dereference callbacks after being back to the Document/Worker threads.
+                context.postTask(
+                    [callback = WTFMove(callback), exceptionCallback = WTFMove(exceptionCallback)](ScriptExecutionContext& context) {
+                        exceptionCallback(OperationError);
+                        context.deref();
+                    });
+                return;
+            }
+
+            // We should only dereference callbacks after being back to the Document/Worker threads.
+            context.postTask(
+                [output = WTFMove(*output), callback = WTFMove(callback), exceptionCallback = WTFMove(exceptionCallback)](ScriptExecutionContext& context) mutable {
+                    callback(WTFMove(output));
+                    context.deref();
+                });
+        });
 }
 
 ExceptionOr<void> CryptoAlgorithmRSAES_PKCS1_v1_5::platformEncrypt(const CryptoKeyRSA&, const CryptoOperationData&, VectorCallback&&, VoidCallback&&)
