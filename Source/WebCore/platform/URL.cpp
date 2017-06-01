2e4276efa25211f90d55c33d6e73a31ac59a4adf
Chris Dumez <cdumez@apple.com>
2017-06-01 17:04:09 +0000

    Unreviewed, rolling out r217660.
    This should not have landed given that the new assertion is
    hit on debug EWS bots
    
    Reverted changeset:
    
    "Make sure we don't access WebCore::defaultPortForProtocol
    from non-main thread"
    https://bugs.webkit.org/show_bug.cgi?id=172797
    http://trac.webkit.org/changeset/217660
    
    Canonical link: https://commits.webkit.org/189703@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@217661 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/URL.cpp
+++ Source/WebCore/platform/URL.cpp
@@ -35,7 +35,6 @@
 #include <unicode/uidna.h>
 #include <wtf/HashMap.h>
 #include <wtf/HexNumber.h>
-#include <wtf/MainThread.h>
 #include <wtf/NeverDestroyed.h>
 #include <wtf/StdLibExtras.h>
 #include <wtf/UUID.h>
@@ -564,7 +563,6 @@ void clearDefaultPortForProtocolMapForTesting()
 
 std::optional<uint16_t> defaultPortForProtocol(StringView protocol)
 {
-    ASSERT(isMainThread());
     const auto& defaultPortForProtocolMap = defaultPortForProtocolMapForTesting();
     auto iterator = defaultPortForProtocolMap.find(protocol.toStringWithoutCopying());
     if (iterator != defaultPortForProtocolMap.end())
