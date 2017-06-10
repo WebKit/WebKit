41b02dc26418f258db0d340f6401a4d3da11e1e7
Yusuke Suzuki <utatane.tea@gmail.com>
2017-06-10 18:35:16 +0000

    Unreviewed, update test results https://bugs.webkit.org/show_bug.cgi?id=173227
    * microbenchmarks/string-prototype-search-observable-side-effects.js:
    * test262.yaml:
    
    Canonical link: https://commits.webkit.org/190065@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@218053 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/microbenchmarks/string-prototype-search-observable-side-effects.js
+++ JSTests/microbenchmarks/string-prototype-search-observable-side-effects.js
@@ -174,7 +174,7 @@ function assert(testedValue, msg) {
         assert(e.toString() == "TypeError: Builtin RegExp exec can only be called on a RegExp object",
             "Unexpected error message");
     }
-    assert(accesses == "getLastIndex,setLastIndex", "Property accesses do not match expectation");
+    assert(accesses == "getLastIndex", "Property accesses do not match expectation");
 })();
 
 // Any object with custom prototype overriding exec.
@@ -268,7 +268,7 @@ function assert(testedValue, msg) {
 
     assert(accesses == "", "unexpected call to overridden props");
     let result = "searchme".search(obj);
-    assert(accesses == "getLastIndex,setLastIndex,exec,setLastIndex", "Property accesses do not match expectation");
+    assert(accesses == "getLastIndex,exec,getLastIndex", "Property accesses do not match expectation");
     assert(result === 3, "Unexpected result");
 })();
 
@@ -374,6 +374,6 @@ function assert(testedValue, msg) {
 
     assert(accesses == "", "unexpected call to overridden props");
     let result = "searchme".search(proxy);
-    assert(accesses.toString() == "Symbol(Symbol.search),lastIndex,exec", "Proxy not able to observe some gets");
+    assert(accesses.toString() == "Symbol(Symbol.search),lastIndex,exec,lastIndex", "Proxy not able to observe some gets");
     assert(result === 3, "Unexpected result");
 })();
