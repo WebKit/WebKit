24b4326d93b11e54230d966fefaf1558b728d1f7
Carlos Garcia Campos <cgarcia@igalia.com>
2017-06-12 12:50:39 +0000

    [GTK] Move WebKit GType macros to WTF https://bugs.webkit.org/show_bug.cgi?id=173249
    Reviewed by Žan Doberšek.
    
    Source/WebKit2:
    
    Moving to a common place where they could be shared by all ports using a glib-based API.
    
    * UIProcess/API/gtk/WebKitPrivate.h:
    
    Source/WTF:
    
    They can be used by glib-based ports and eventually by JavaScriptCore glib bindings too.
    
    * wtf/glib/WTFGType.h: Copied from Source/WebKit2/UIProcess/API/gtk/WebKitPrivate.h.
    
    Canonical link: https://commits.webkit.org/190106@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@218096 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/gtk/WebKitPrivate.h
+++ Source/WebKit2/UIProcess/API/gtk/WebKitPrivate.h
@@ -23,8 +23,7 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef WebKitPrivate_h
-#define WebKitPrivate_h
+#pragma once
 
 #include "WebKitNavigationAction.h"
 #include <WebKit/WKAPICast.h>
@@ -42,74 +41,7 @@
 #include <WebKit/WebKit2_C.h>
 #include <glib.h>
 #include <wtf/Assertions.h>
-
-#define WEBKIT_PARAM_READABLE (static_cast<GParamFlags>(G_PARAM_READABLE | G_PARAM_STATIC_NAME | G_PARAM_STATIC_NICK | G_PARAM_STATIC_BLURB))
-#define WEBKIT_PARAM_WRITABLE (static_cast<GParamFlags>(G_PARAM_WRITABLE | G_PARAM_STATIC_NAME | G_PARAM_STATIC_NICK | G_PARAM_STATIC_BLURB))
-#define WEBKIT_PARAM_READWRITE (static_cast<GParamFlags>(G_PARAM_READWRITE | G_PARAM_STATIC_NAME | G_PARAM_STATIC_NICK | G_PARAM_STATIC_BLURB))
-
-#define WEBKIT_DEFINE_ASYNC_DATA_STRUCT(structName) \
-static structName* create##structName() \
-{ \
-    structName* data = static_cast<structName*>(fastZeroedMalloc(sizeof(structName))); \
-    new (data) structName(); \
-    return data; \
-} \
-static void destroy##structName(structName* data) \
-{ \
-    data->~structName(); \
-    fastFree(data); \
-}
-
-#define WEBKIT_DEFINE_TYPE(TypeName, type_name, TYPE_PARENT) _WEBKIT_DEFINE_TYPE_EXTENDED(TypeName, type_name, TYPE_PARENT, 0, { })
-#define WEBKIT_DEFINE_ABSTRACT_TYPE(TypeName, type_name, TYPE_PARENT) _WEBKIT_DEFINE_TYPE_EXTENDED(TypeName, type_name, TYPE_PARENT, G_TYPE_FLAG_ABSTRACT, { })
-#define WEBKIT_DEFINE_TYPE_WITH_CODE(TypeName, type_name, TYPE_PARENT, Code) _WEBKIT_DEFINE_TYPE_EXTENDED_BEGIN(TypeName, type_name, TYPE_PARENT, 0) {Code;} _WEBKIT_DEFINE_TYPE_EXTENDED_END()
-
-#define _WEBKIT_DEFINE_TYPE_EXTENDED(TypeName, type_name, TYPE_PARENT, flags, Code) _WEBKIT_DEFINE_TYPE_EXTENDED_BEGIN(TypeName, type_name, TYPE_PARENT, flags) {Code;} _WEBKIT_DEFINE_TYPE_EXTENDED_END()
-#define _WEBKIT_DEFINE_TYPE_EXTENDED_BEGIN(TypeName, type_name, TYPE_PARENT, flags) \
-\
-static void type_name##_class_init(TypeName##Class* klass); \
-static gpointer type_name##_parent_class = 0; \
-static void type_name##_finalize(GObject* object) \
-{ \
-    TypeName* self = (TypeName*)object; \
-    self->priv->~TypeName##Private(); \
-    G_OBJECT_CLASS(type_name##_parent_class)->finalize(object); \
-} \
-\
-static void type_name##_class_intern_init(gpointer klass) \
-{ \
-    GObjectClass* gObjectClass = G_OBJECT_CLASS(klass); \
-    g_type_class_add_private(klass, sizeof(TypeName##Private)); \
-    type_name##_parent_class = g_type_class_peek_parent(klass); \
-    type_name##_class_init((TypeName##Class*)klass); \
-    gObjectClass->finalize = type_name##_finalize; \
-} \
-\
-static void type_name##_init(TypeName* self) \
-{ \
-    TypeName##Private* priv = G_TYPE_INSTANCE_GET_PRIVATE(self, type_name##_get_type(), TypeName##Private); \
-    self->priv = priv; \
-    new (priv) TypeName##Private(); \
-}\
-GType type_name##_get_type(void) \
-{ \
-    static volatile gsize g_define_type_id__volatile = 0; \
-    if (g_once_init_enter(&g_define_type_id__volatile)) { \
-        GType g_define_type_id = \
-            g_type_register_static_simple( \
-                TYPE_PARENT, \
-                g_intern_static_string(#TypeName), \
-                sizeof(TypeName##Class), \
-                (GClassInitFunc)type_name##_class_intern_init, \
-                sizeof(TypeName), \
-                (GInstanceInitFunc)type_name##_init, \
-                (GTypeFlags)flags); \
-        // Custom code follows.
-#define _WEBKIT_DEFINE_TYPE_EXTENDED_END() \
-        g_once_init_leave(&g_define_type_id__volatile, g_define_type_id); \
-    } \
-    return g_define_type_id__volatile; \
-} // Closes type_name##_get_type().
+#include <wtf/glib/WTFGType.h>
 
 unsigned wkEventModifiersToGdkModifiers(WKEventModifiers);
 unsigned wkEventMouseButtonToWebKitMouseButton(WKEventMouseButton);
@@ -125,5 +57,3 @@ enum SnapshotRegion {
 };
 
 static const char networkCacheSubdirectory[] = "WebKitCache";
-
-#endif // WebKitPrivate_h
