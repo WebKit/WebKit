966c58b03e0c0dc129017c9228c1741ae47b570f
Dan Bernstein <mitz@apple.com>
2017-06-17 15:49:32 +0000

    Tried to fix non-iOS builds.
    * UIProcess/API/Cocoa/WKUIDelegatePrivate.h:
    
    
    
    Canonical link: https://commits.webkit.org/190404@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@218453 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit2/UIProcess/API/Cocoa/WKUIDelegatePrivate.h
+++ Source/WebKit2/UIProcess/API/Cocoa/WKUIDelegatePrivate.h
@@ -40,7 +40,7 @@
 @class _WKElementAction;
 @class _WKFrameHandle;
 
-#if TARGET_OS_IPHONE
+#if TARGET_OS_IOS
 @class UIDragItem;
 @class UITargetedDragPreview;
 @protocol UIDragSession;
@@ -110,7 +110,7 @@ struct UIEdgeInsets;
 - (void)_webView:(WKWebView *)webView dataInteraction:(id)interaction session:(id)session didEndWithOperation:(NSUInteger)operation WK_API_AVAILABLE(ios(WK_IOS_TBA));
 - (void)_webView:(WKWebView *)webView dataInteractionOperationWasHandled:(BOOL)handled forSession:(id)session itemProviders:(NSArray *)itemProviders WK_API_AVAILABLE(ios(WK_IOS_TBA));
 - (NSUInteger)_webView:(WKWebView *)webView willUpdateDataInteractionOperationToOperation:(NSUInteger)operation forSession:(id)session WK_API_AVAILABLE(ios(WK_IOS_TBA));
-#if TARGET_OS_IPHONE
+#if TARGET_OS_IOS
 - (UITargetedDragPreview *)_webView:(WKWebView *)webView previewForLiftingItem:(UIDragItem *)item session:(id <UIDragSession>)session WK_API_AVAILABLE(ios(WK_IOS_TBA));
 - (UITargetedDragPreview *)_webView:(WKWebView *)webView previewForCancellingItem:(UIDragItem *)item withDefault:(UITargetedDragPreview *)defaultPreview WK_API_AVAILABLE(ios(WK_IOS_TBA));
 - (NSArray<UIDragItem *> *)_webView:(WKWebView *)webView willPerformDropWithSession:(id <UIDropSession>)session WK_API_AVAILABLE(ios(WK_IOS_TBA));
