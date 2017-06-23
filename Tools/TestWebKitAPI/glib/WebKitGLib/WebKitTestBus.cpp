WebKit/WebKit

commit 8685926bb760c32c42904a88dba310dd420cf3eb

[WPE] Build glib API tests https://bugs.webkit.org/show_bug.cgi?id=173702
Reviewed by Žan Doberšek.

Many of the tests pass without problems, others need to be investigated, and some others are known issues. The
main difference between the GTK+ and WPE WebKitWebView is that in GTK+ it's a GtkWidget, so initially
unowned. The tests assume that webkit_web_view_new methods return a floating reference. This patch adds
Test::adoptView() method that returns a GRefPtr<WebKitWebView> with a different implementation in GTK+ and
WPE. In the case of GTK+, it sinks the floating reference, while in WPE it simply adopts the reference. Another
difference is that in GTK+ the web view is initially hidden and needs to be added to a window to make it
visible. Also, some tests need the web view to be realized to work, and others need that the view is added to a
toplevel window, instead of a popup window. All those things are not needed at all in WPE. I've added several
#ifdefs with FIXME comments for the things that we know don't work in WPE yet, for example, because we haven't
implemented the methods to send events to the web view. Those will be removed eventually when we fix the issues
and implement the missing features.

* CMakeLists.txt:
* TestWebKitAPI/Tests/WebKit2Gtk/EditorTest.cpp:
* TestWebKitAPI/Tests/WebKitGLib/FrameTest.cpp:
* TestWebKitAPI/Tests/WebKitGLib/TestAutomationSession.cpp:
(testAutomationSessionRequestSession):
* TestWebKitAPI/Tests/WebKitGLib/TestBackForwardList.cpp:
(testWebKitWebViewSessionState):
(testWebKitWebViewSessionStateWithFormData):
(testWebKitWebViewNavigationAfterSessionRestore):
* TestWebKitAPI/Tests/WebKitGLib/TestConsoleMessage.cpp:
(beforeAll):
* TestWebKitAPI/Tests/WebKitGLib/TestCookieManager.cpp:
(testCookieManagerEphemeral):
* TestWebKitAPI/Tests/WebKitGLib/TestDownloads.cpp:
(testBlobDownload):
(beforeAll):
* TestWebKitAPI/Tests/WebKitGLib/TestFrame.cpp:
* TestWebKitAPI/Tests/WebKitGLib/TestLoaderClient.cpp:

Tools/TestWebKitAPI/glib/WebKitGLib/WebKitTestBus.cpp
@@ -33,9 +33,11 @@ WebKitTestBus::~WebKitTestBus()
 bool WebKitTestBus::run()
 {
     CString display = g_getenv("DISPLAY");
+    CString runtimeDir = g_getenv("XDG_RUNTIME_DIR");
     g_test_dbus_up(m_bus.get());
     m_address = g_test_dbus_get_bus_address(m_bus.get());
     g_setenv("DISPLAY", display.data(), FALSE);
+    g_setenv("XDG_RUNTIME_DIR", runtimeDir.data(), FALSE);
     return !m_address.isNull();
 }
 

Tools/CMakeLists.txt
@@ -27,6 +27,9 @@ elseif ("${PORT}" STREQUAL "WPE")
     if (DEVELOPER_MODE)
         add_subdirectory(ImageDiff)
         add_subdirectory(WebKitTestRunner)
+        if (ENABLE_API_TESTS)
+            add_subdirectory(TestWebKitAPI/glib)
+        endif ()
     endif ()
 elseif ("${PORT}" STREQUAL "Win")
     add_subdirectory(DumpRenderTree)

Tools/TestWebKitAPI/Tests/WebKit2Gtk/EditorTest.cpp
@@ -20,7 +20,6 @@
 #include "config.h"
 
 #include "WebProcessTest.h"
-#include <webkit2/webkit-web-extension.h>
 
 #define WEBKIT_DOM_USE_UNSTABLE_API
 #include <webkitdom/WebKitDOMDOMSelection.h>

Tools/TestWebKitAPI/Tests/WebKitGLib/FrameTest.cpp
@@ -21,7 +21,6 @@
 
 #include "WebProcessTest.h"
 #include <gio/gio.h>
-#include <webkit2/webkit-web-extension.h>
 
 class WebKitFrameTest : public WebProcessTest {
 public:

Tools/TestWebKitAPI/Tests/WebKitGLib/TestAutomationSession.cpp
@@ -266,12 +266,12 @@ static void testAutomationSessionRequestSession(AutomationTest* test, gconstpoin
     g_assert(!test->createTopLevelBrowsingContext(nullptr));
 
     // Will also fail if the web view is not controlled by automation.
-    GRefPtr<WebKitWebView> webView = WEBKIT_WEB_VIEW(webkit_web_view_new_with_context(test->m_webContext.get()));
+    auto webView = Test::adoptView(webkit_web_view_new_with_context(test->m_webContext.get()));
     g_assert(!webkit_web_view_is_controlled_by_automation(webView.get()));
     g_assert(!test->createTopLevelBrowsingContext(webView.get()));
 
     // And will work with a proper web view.
-    webView = WEBKIT_WEB_VIEW(g_object_new(WEBKIT_TYPE_WEB_VIEW, "web-context", test->m_webContext.get(), "is-controlled-by-automation", TRUE, nullptr));
+    webView = Test::adoptView(g_object_new(WEBKIT_TYPE_WEB_VIEW, "web-context", test->m_webContext.get(), "is-controlled-by-automation", TRUE, nullptr));
     g_assert(webkit_web_view_is_controlled_by_automation(webView.get()));
     g_assert(test->createTopLevelBrowsingContext(webView.get()));
 

Tools/TestWebKitAPI/Tests/WebKitGLib/TestBackForwardList.cpp
@@ -21,10 +21,8 @@
 
 #include "WebKitTestServer.h"
 #include "WebViewTest.h"
-#include <gtk/gtk.h>
 #include <libsoup/soup.h>
 #include <string.h>
-#include <webkit2/webkit2.h>
 
 // Back forward list limit is 100 by default.
 static const int kBackForwardListLimit = 100;
@@ -268,7 +266,7 @@ static void testWebKitWebViewSessionState(BackForwardListTest* test, gconstpoint
 {
     WebKitWebViewSessionState* state = webkit_web_view_get_session_state(test->m_webView);
     g_assert(state);
-    GRefPtr<WebKitWebView> view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    auto view = Test::adoptView(webkit_web_view_new());
     WebKitBackForwardList* bfList = webkit_web_view_get_back_forward_list(view.get());
     g_assert_cmpuint(webkit_back_forward_list_get_length(bfList), ==, 0);
     webkit_web_view_restore_session_state(view.get(), state);
@@ -277,7 +275,7 @@ static void testWebKitWebViewSessionState(BackForwardListTest* test, gconstpoint
     g_assert(data);
     state = webkit_web_view_session_state_new(data.get());
     g_assert(state);
-    view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    view = Test::adoptView(webkit_web_view_new());
     bfList = webkit_web_view_get_back_forward_list(view.get());
     g_assert_cmpuint(webkit_back_forward_list_get_length(bfList), ==, 0);
     webkit_web_view_restore_session_state(view.get(), state);
@@ -320,7 +318,7 @@ static void testWebKitWebViewSessionState(BackForwardListTest* test, gconstpoint
     state = webkit_web_view_session_state_new(data.get());
     g_assert(state);
 
-    view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    view = Test::adoptView(webkit_web_view_new());
     bfList = webkit_web_view_get_back_forward_list(view.get());
     g_assert_cmpuint(webkit_back_forward_list_get_length(bfList), ==, 0);
     webkit_web_view_restore_session_state(view.get(), state);
@@ -349,7 +347,7 @@ static void testWebKitWebViewSessionStateWithFormData(BackForwardListTest* test,
 
     WebKitWebViewSessionState* state = webkit_web_view_get_session_state(test->m_webView);
     g_assert(state);
-    GRefPtr<WebKitWebView> view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    auto view = Test::adoptView(webkit_web_view_new());
     WebKitBackForwardList* bfList = webkit_web_view_get_back_forward_list(view.get());
     g_assert_cmpuint(webkit_back_forward_list_get_length(bfList), ==, 0);
     webkit_web_view_restore_session_state(view.get(), state);
@@ -358,7 +356,7 @@ static void testWebKitWebViewSessionStateWithFormData(BackForwardListTest* test,
     g_assert(data);
     state = webkit_web_view_session_state_new(data.get());
     g_assert(state);
-    view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    view = Test::adoptView(webkit_web_view_new());
     bfList = webkit_web_view_get_back_forward_list(view.get());
     g_assert_cmpuint(webkit_back_forward_list_get_length(bfList), ==, 0);
     webkit_web_view_restore_session_state(view.get(), state);
@@ -376,7 +374,7 @@ static void testWebKitWebViewNavigationAfterSessionRestore(BackForwardListTest* 
 {
     // This test checks that a normal load after a session restore with a BackForard list having
     // forward items doesn't produce any runtime critical warning. See https://bugs.webkit.org/show_bug.cgi?id=153233.
-    GRefPtr<WebKitWebView> view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+    auto view = Test::adoptView(webkit_web_view_new());
     g_signal_connect(view.get(), "load-changed", G_CALLBACK(viewLoadChanged), test->m_mainLoop);
 
     webkit_web_view_load_uri(view.get(), kServer->getURIForPath("/Page1").data());

Tools/TestWebKitAPI/Tests/WebKitGLib/TestConsoleMessage.cpp
@@ -20,9 +20,8 @@
 #include "config.h"
 
 #include "WebViewTest.h"
-#include <gtk/gtk.h>
-#include <webkit2/webkit2.h>
 
+#if PLATFORM(GTK)
 class ConsoleMessageTest : public WebViewTest {
 public:
     MAKE_GLIB_TEST_FIXTURE(ConsoleMessageTest);
@@ -141,13 +140,17 @@ static void testWebKitConsoleMessageSecurityError(ConsoleMessageTest* test, gcon
     test->waitUntilConsoleMessageReceived();
     g_assert(test->m_consoleMessage == referenceMessage);
 }
+#endif // PLATFORM(GTK)
 
 void beforeAll()
 {
+    // FIXME: Use JSC API to send script messages from JavaScript.
+#if PLATFORM(GTK)
     ConsoleMessageTest::add("WebKitConsoleMessage", "console-api", testWebKitConsoleMessageConsoleAPI);
     ConsoleMessageTest::add("WebKitConsoleMessage", "js-exception", testWebKitConsoleMessageJavaScriptException);
     ConsoleMessageTest::add("WebKitConsoleMessage", "network-error", testWebKitConsoleMessageNetworkError);
     ConsoleMessageTest::add("WebKitConsoleMessage", "security-error", testWebKitConsoleMessageSecurityError);
+#endif
 }
 
 void afterAll()

Tools/TestWebKitAPI/Tests/WebKitGLib/TestCookieManager.cpp
@@ -316,7 +316,7 @@ static void testCookieManagerEphemeral(CookieManagerTest* test, gconstpointer)
     g_assert(domains);
     g_assert_cmpint(g_strv_length(domains), ==, 0);
 
-    GRefPtr<WebKitWebView> webView = WEBKIT_WEB_VIEW(g_object_new(WEBKIT_TYPE_WEB_VIEW,
+    auto webView = Test::adoptView(g_object_new(WEBKIT_TYPE_WEB_VIEW,
         "web-context", webkit_web_view_get_context(test->m_webView),
         "is-ephemeral", TRUE,
         nullptr));

commit 5a1c19934989e184b5521db2bc001f59e3c29b1f

REGRESSION(r218686): [GTK] Several unit tests are failing after r218686 in the bots https://bugs.webkit.org/show_bug.cgi?id=173711
Reviewed by Carlos Alberto Lopez Perez.

I can't reproduce it locally, but it's complaining about a g_setenv() and I only added one in r218686 to recover
the XDG_RUNTIME_DIR, because it's required by Wayland to work. So, I guess that env var is not set in the bots.

* TestWebKitAPI/glib/WebKitGLib/WebKitTestBus.cpp:
(WebKitTestBus::run): Ensure value is not nullptr before calling g_setenv().

Canonical link: https://commits.webkit.org/190619@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@218736 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/TestWebKitAPI/glib/WebKitGLib/WebKitTestBus.cpp
@@ -36,8 +36,10 @@ bool WebKitTestBus::run()
     CString runtimeDir = g_getenv("XDG_RUNTIME_DIR");
     g_test_dbus_up(m_bus.get());
     m_address = g_test_dbus_get_bus_address(m_bus.get());
-    g_setenv("DISPLAY", display.data(), FALSE);
-    g_setenv("XDG_RUNTIME_DIR", runtimeDir.data(), FALSE);
+    if (!display.isNull())
+        g_setenv("DISPLAY", display.data(), FALSE);
+    if (!runtimeDir.isNull())
+        g_setenv("XDG_RUNTIME_DIR", runtimeDir.data(), FALSE);
     return !m_address.isNull();
 }
 