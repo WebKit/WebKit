31ec76a8635b7206b21f91a375e343637d607e23
Michael Saboff <msaboff@apple.com>
2017-07-01 01:20:01 +0000

    Skip a test on ARM64 platform since we run out of address space.
    Rubber stamped by Saam Barati.
    
    * stress/dont-reserve-huge-capacity-lexer.js:
    
    
    Canonical link: https://commits.webkit.org/190887@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@219032 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/dont-reserve-huge-capacity-lexer.js
+++ JSTests/stress/dont-reserve-huge-capacity-lexer.js
@@ -1,4 +1,4 @@
-//@ skip if ($architecture != "arm64" and $architecture != "x86-64")
+//@ skip if ($architecture != "x86-64")
 
 var fe="f";                                                                         
 try
