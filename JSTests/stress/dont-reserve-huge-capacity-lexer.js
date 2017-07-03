6770673ed05575f0652ee0a59fd67deb61d749b2
Yusuke Suzuki <utatane.tea@gmail.com>
2017-07-03 08:42:59 +0000

    Unreviewed, annotate dont--reserve-huge-capacity-lexer.js with $memoryLimited
    It requires too much memory.
    
    * stress/dont-reserve-huge-capacity-lexer.js:
    
    Canonical link: https://commits.webkit.org/190918@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@219065 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/dont-reserve-huge-capacity-lexer.js
+++ JSTests/stress/dont-reserve-huge-capacity-lexer.js
@@ -1,4 +1,4 @@
-//@ skip if ($architecture != "x86-64")
+//@ skip if ($architecture != "x86-64") or $memoryLimited
 
 var fe="f";                                                                         
 try
