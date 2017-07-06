61514c06bcdec4ed1f4eda9617c3e31f2822c74f
David Kilzer <ddkilzer@webkit.org>
2017-07-06 16:49:16 +0000

    Add release assert to explore crash for <rdar://problem/32908525>
    Reviewed by Brady Eidson.
    
    * Modules/indexeddb/server/UniqueIDBDatabase.cpp:
    (WebCore::IDBServer::UniqueIDBDatabase::~UniqueIDBDatabase): Add
    release assert to catch cases when the IDBBackingStore is not
    deleted before the UniqueIDBDatabase is destroyed.  The
    IDBBackingStore should always be released on the database
    thread.
    
    Canonical link: https://commits.webkit.org/191044@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@219201 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/indexeddb/server/UniqueIDBDatabase.cpp
+++ Source/WebCore/Modules/indexeddb/server/UniqueIDBDatabase.cpp
@@ -66,6 +66,7 @@ UniqueIDBDatabase::UniqueIDBDatabase(IDBServer& server, const IDBDatabaseIdentif
 UniqueIDBDatabase::~UniqueIDBDatabase()
 {
     LOG(IndexedDB, "UniqueIDBDatabase::~UniqueIDBDatabase() (%p) %s", this, m_identifier.debugString().utf8().data());
+    RELEASE_ASSERT(!m_backingStore);
     ASSERT(isMainThread());
     ASSERT(!hasAnyPendingCallbacks());
     ASSERT(!hasUnfinishedTransactions());
