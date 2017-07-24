ef34ed5580e67f9cc2744d926e8ec3b0e988ed42
Carlos Alberto Lopez Perez <clopez@igalia.com>
2017-07-24 16:01:33 +0000

    [CMake] libtasn1 should not be required when not enabling subtle_crypto https://bugs.webkit.org/show_bug.cgi?id=174785
    Reviewed by Žan Doberšek.
    
    * pal/PlatformGTK.cmake:
    * pal/PlatformWPE.cmake:
    
    Canonical link: https://commits.webkit.org/191590@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@219823 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/PAL/pal/PlatformGTK.cmake
+++ Source/WebCore/PAL/pal/PlatformGTK.cmake
@@ -1,12 +1,16 @@
 list(APPEND PAL_SOURCES
     crypto/gcrypt/CryptoDigestGCrypt.cpp
 
-    crypto/tasn1/Utilities.cpp
-
     system/gtk/SoundGtk.cpp
 
     text/KillRingNone.cpp
 )
+
+if (ENABLE_SUBTLE_CRYPTO)
+    list(APPEND PAL_SOURCES
+        crypto/tasn1/Utilities.cpp
+    )
+endif ()
 
 list(APPEND PAL_SYSTEM_INCLUDE_DIRECTORIES
     ${GDK_INCLUDE_DIRS}
--- Source/WebCore/PAL/pal/PlatformWPE.cmake
+++ Source/WebCore/PAL/pal/PlatformWPE.cmake
@@ -1,9 +1,13 @@
 list(APPEND PAL_SOURCES
     crypto/gcrypt/CryptoDigestGCrypt.cpp
 
-    crypto/tasn1/Utilities.cpp
-
     system/SoundNone.cpp
 
     text/KillRingNone.cpp
 )
+
+if (ENABLE_SUBTLE_CRYPTO)
+    list(APPEND PAL_SOURCES
+        crypto/tasn1/Utilities.cpp
+    )
+endif ()
