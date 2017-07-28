6ca9b49bd1c70eba7e88c81f4d65309e64b506db
Mathias Bynens <mathias@chromium.org>
2017-07-28 04:44:45 +0000

    Speedometer 2.0: Document Angular 4 build process https://bugs.webkit.org/show_bug.cgi?id=174896
    Patch by Mathias Bynens <mathias@chromium.org> on 2017-07-27
    Reviewed by Ryosuke Niwa.
    
    This patch documents the build process for the Angular 4 example, pins the dependencies (so
    that builds are deterministic), and updates the generated files accordingly.
    
    * Speedometer/resources/todomvc/architecture-examples/angular/README.md: Added.
    * Speedometer/resources/todomvc/architecture-examples/angular/dist/*: Update per build instructions.
    * Speedometer/resources/todomvc/architecture-examples/angular/e2e/app.e2e-spec.ts: Removed unused file.
    * Speedometer/resources/todomvc/architecture-examples/angular/e2e/app.po.ts: Removed unused file.
    * Speedometer/resources/todomvc/architecture-examples/angular/e2e/tsconfig.e2e.json: Removed unused file.
    * Speedometer/resources/todomvc/architecture-examples/angular/package.json: Remove unneeded scripts.
    * Speedometer/resources/todomvc/architecture-examples/angular/package-lock.json: Added to pin dependencies.
    * Speedometer/resources/todomvc/architecture-examples/angular/src/app/app.component.ts: Updated.
    * Speedometer/resources/todomvc/architecture-examples/angular/src/environments/environment.ts: Updated.
    * Speedometer/resources/todomvc/architecture-examples/angular/src/index.html: Made title consistent.
    
    Canonical link: https://commits.webkit.org/191722@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@219993 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- PerformanceTests/Speedometer/resources/todomvc/architecture-examples/angular/src/app/app.component.ts
+++ PerformanceTests/Speedometer/resources/todomvc/architecture-examples/angular/src/app/app.component.ts
@@ -5,7 +5,6 @@ import {TodoDataService} from './todo-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
   providers: [TodoDataService]
 })
 export class AppComponent {
