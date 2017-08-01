d0b5fb9cb8117d0cf2f463434ba9fad53d9d9020
Yoshiaki Jitsukawa <yoshiaki.jitsukawa@sony.com>
2017-08-01 22:33:18 +0000

    [PAL][Win] Copy forwarding headers https://bugs.webkit.org/show_bug.cgi?id=175012
    Patch by Yoshiaki Jitsukawa <jitsu@rd.scei.sony.co.jp> on 2017-08-01
    Reviewed by Myles C. Maxfield.
    
    * pal/PlatformAppleWin.cmake: Added.
    * pal/PlatformWin.cmake:
    * pal/PlatformWinCairo.cmake: Added.
    
    Canonical link: https://commits.webkit.org/191820@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@220113 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/PAL/pal/PlatformWin.cmake
+++ Source/WebCore/PAL/pal/PlatformWin.cmake
@@ -11,10 +11,21 @@ list(APPEND PAL_INCLUDE_DIRECTORIES
     "${CMAKE_BINARY_DIR}/../include/private"
 )
 
+list(APPEND PAL_FORWARDING_HEADERS_DIRECTORIES .)
+
+if (${WTF_PLATFORM_WIN_CAIRO})
+    include(PlatformWinCairo.cmake)
+else ()
+    include(PlatformAppleWin.cmake)
+endif ()
+
 set(PAL_OUTPUT_NAME PAL${DEBUG_SUFFIX})
 
 file(MAKE_DIRECTORY ${FORWARDING_HEADERS_DIR}/WebCore/pal)
-file(GLOB _files_PAL "${PAL_DIR}/pal/*.h")
-foreach (_file ${_files_PAL})
-    file(COPY ${_file} DESTINATION ${FORWARDING_HEADERS_DIR}/WebCore/pal/)
+foreach (_directory ${PAL_FORWARDING_HEADERS_DIRECTORIES})
+    file(MAKE_DIRECTORY ${FORWARDING_HEADERS_DIR}/WebCore/pal/${_directory})
+    file(GLOB _files "${PAL_DIR}/pal/${_directory}/*.h")
+    foreach (_file ${_files})
+        file(COPY ${_file} DESTINATION ${FORWARDING_HEADERS_DIR}/WebCore/pal/${_directory})
+    endforeach ()
 endforeach ()
