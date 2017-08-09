d5fc3bf82d198c09f35b1b5462de8922fc337244
Nikita Vasilyev <nvasilyev@apple.com>
2017-08-09 17:46:27 +0000

    Web Inspector: Styles: Add pre-populated data to spreadsheet-style view https://bugs.webkit.org/show_bug.cgi?id=174838 <rdar://problem/33523220>
    Reviewed by Matt Baker.
    
    This patch adds static content to the experimental RulesStyleSpreadsheetDetailsPanel.
    The static content will be replaced with actual data in the follow up patches.
    
    * UserInterface/Main.html:
    * UserInterface/Views/RulesStyleSpreadsheetDetailsPanel.css: Added.
    (.style-spreadsheet .style-rule):
    (.style-spreadsheet .selector-line):
    (.style-spreadsheet .selector):
    (.style-spreadsheet :matches(.selector .matched, .selector:focus)):
    (.style-spreadsheet .name):
    (.style-spreadsheet .value):
    (.style-spreadsheet :matches(.selector, .name, .value)):
    (.style-spreadsheet :matches(.selector, .name, .value):focus):
    (.style-spreadsheet .selector-inline):
    (.style-spreadsheet .property):
    (.style-spreadsheet .property-disabled,):
    (.style-spreadsheet .property-disabled :matches(.name, .value)):
    (.style-spreadsheet .styles-source):
    (.style-spreadsheet :matches(a, .node-link)):
    (.style-spreadsheet :matches(a, .node-link):hover):
    (.style-spreadsheet .inherited):
    (.style-spreadsheet .declarations):
    (.style-spreadsheet input[type="checkbox"]):
    (.style-spreadsheet input[type="checkbox"]:not(:checked)):
    (.style-spreadsheet .declarations:hover input[type="checkbox"]):
    * UserInterface/Views/RulesStyleSpreadsheetDetailsPanel.js:
    
    
    Canonical link: https://commits.webkit.org/192089@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@220462 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Views/RulesStyleSpreadsheetDetailsPanel.js
+++ Source/WebInspectorUI/UserInterface/Views/RulesStyleSpreadsheetDetailsPanel.js
@@ -28,6 +28,44 @@ WI.RulesStyleSpreadsheetDetailsPanel = class RulesStyleSpreadsheetDetailsPanel e
     constructor(delegate)
     {
         super(delegate, "rules", "rules", WI.UIString("Styles \u2014 Rules"));
+
+        let styleSpreadsheetElement = document.createElement("div");
+        styleSpreadsheetElement.classList.add("style-spreadsheet");
+        styleSpreadsheetElement.innerHTML = `
+<section class="style-rule"><span class="selector selector-inline">Style Attribute</span> { }</section>
+
+<section class="style-rule">
+<span class="styles-source"><a href="#"><span class="resource-name">styleRule.css</span><span class="line-info">:24</span></a></span>
+<span class="selector-line"><span class="selector"><span class="matched">h1</span>, h2</span> {</span>
+<div class="declarations">
+    <div class="property"><input type="checkbox" checked><span class="name">font</span>: <span class="value">14px/32px sans-serif</span>;</div>
+    <div class="property"><input type="checkbox" checked><span class="name">margin</span>: <span class="value">0</span>;</div>
+    <div class="property"><input type="checkbox" checked><span class="name">padding</span>: <span class="value">8px 16px</span>;</div>
+}</div></section>
+
+<section class="style-rule">
+<span class="styles-source"><a href="#"><span class="resource-name">webkit.org</span><span class="line-info">:110</span></a></span>
+<span class="selector-line"><span class="selector"><span class="matched">.home .page-layer</span></span> {</span>
+<div class="declarations">
+    <div class="property"><input type="checkbox" checked><span class="name">background-color</span>: <span class="value">#f7f7f7</span>;</div>
+    <div class="property"><input type="checkbox" checked><span class="name">border-top</span>: <span class="value">1px solid #e7e7e7</span>;</div>
+    <div class="property"><input type="checkbox" checked><span class="name">position</span>: <span class="value">relative</span>;</div>
+    <div class="property"><input type="checkbox" checked><span class="name">z-index</span>: <span class="value">1</span>;</div>
+}</div></section>
+
+<section class="inherited"><strong>Inherited From: </strong><span role="link" title="body" class="node-link">body</span></section>
+
+<section class="style-rule">
+<span class="styles-source"><a href="#"><span class="resource-name">style.css</span><span class="line-info">:39</span></a></span>
+<span class="selector-line"><span class="selector"><span class="matched">body</span></span> {</span>
+<div class="declarations">
+    <div class="property"><input type="checkbox" checked><span class="name">background-color</span>: <span class="value">#f7f7f7</span>;</div>
+    <div class="property"><input type="checkbox" checked><span class="name">font-size</span>: <span class="value">1.6rem</span>;</div>
+    <div class="property property-disabled"><input type="checkbox">/* <span class="name">font-weight</span>: <span class="value">400</span>; */</div>
+    <div class="property"><input type="checkbox" checked><span class="name">line-height</span>: <span class="value">1.4</span>;</div>
+}</div></section>`;
+
+        this.element.append(styleSpreadsheetElement);
     }
 
     // Public
