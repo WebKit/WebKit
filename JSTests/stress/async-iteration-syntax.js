f70e16127d528ba63be758242491b9b302fa7d74
Aleksandr Skachkov <gskachkov@gmail.com>
2017-08-14 06:00:26 +0000

    Merge r220449 - REGRESSION: 2 test262/test/language/statements/async-function failures https://bugs.webkit.org/show_bug.cgi?id=175334
    Reviewed by Yusuke Suzuki.
    
    JSTests:
    
    Add @skip parameters to tests, and remove test for async iterator from
    async await syntax test because it is already covered by async-iterator-syntax.js
    
    * stress/async-await-syntax.js:
    * stress/async-iteration-syntax.js:
    
    Source/JavaScriptCore:
    
    Switch off useAsyncIterator by default
    
    * runtime/Options.h:

--- JSTests/stress/async-iteration-syntax.js
+++ JSTests/stress/async-iteration-syntax.js
@@ -1,3 +1,7 @@
+// This test requires enableAsyncIterator to be enabled at run time.
+//@ skip
+
+
 var assert = function (result, expected, message = "") {
   if (result !== expected) {
     throw new Error('Error in assert. Expected "' + expected + '" but was "' + result + '":' + message );
