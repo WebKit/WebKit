7d86c7db1f50a0db73edc8abdd915cc52c2887af
Youenn Fablet <youenn@apple.com>
2017-08-17 16:11:07 +0000

    [Mac/iOS] Increase the audio buffer size when audio capture is on but web audio is not used https://bugs.webkit.org/show_bug.cgi?id=175631
    Patch by Youenn Fablet <youenn@apple.com> on 2017-08-17
    Reviewed by Eric Carlson.
    
    * platform/audio/cocoa/MediaSessionManagerCocoa.cpp:
    (PlatformMediaSessionManager::updateSessionState): Change value from 128 to the equivalent of 20ms when audio capture happens but not web audio.
    
    Canonical link: https://commits.webkit.org/192333@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@220859 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/audio/cocoa/MediaSessionManagerCocoa.cpp
+++ Source/WebCore/platform/audio/cocoa/MediaSessionManagerCocoa.cpp
@@ -42,8 +42,12 @@ void PlatformMediaSessionManager::updateSessionState()
 {
     LOG(Media, "PlatformMediaSessionManager::updateSessionState() - types: Video(%d), Audio(%d), WebAudio(%d)", count(PlatformMediaSession::Video), count(PlatformMediaSession::Audio), count(PlatformMediaSession::WebAudio));
 
-    if (has(PlatformMediaSession::WebAudio) || has(PlatformMediaSession::MediaStreamCapturingAudio))
+    if (has(PlatformMediaSession::WebAudio))
         AudioSession::sharedSession().setPreferredBufferSize(kWebAudioBufferSize);
+    // In case of audio capture, we want to grab 20 ms chunks to limit the latency so that it is not noticeable by users
+    // while having a large enough buffer so that the audio rendering remains stable, hence a computation based on sample rate.
+    else if (has(PlatformMediaSession::MediaStreamCapturingAudio))
+        AudioSession::sharedSession().setPreferredBufferSize(AudioSession::sharedSession().sampleRate() / 50);
     else if ((has(PlatformMediaSession::Video) || has(PlatformMediaSession::Audio)) && Settings::lowPowerVideoAudioBufferSizeEnabled()) {
         // FIXME: <http://webkit.org/b/116725> Figure out why enabling the code below
         // causes media LayoutTests to fail on 10.8.
