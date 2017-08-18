1e683b501fa0a640b89469e7935ee40a761ba51a
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2017-08-18 07:48:32 +0000

    Merge r220860 - [WPE][GTK] Ensure proper casting of data in gvariants https://bugs.webkit.org/show_bug.cgi?id=175667
    Patch by Jacobo Aragunde Pérez <jaragunde@igalia.com> on 2017-08-17
    Reviewed by Michael Catanzaro.
    
    Source/JavaScriptCore:
    
    g_variant_new requires data to have the correct width for their types, using
    casting if necessary. Some data of type `unsigned` were being saved to `guint64`
    types without explicit casting, leading to undefined behavior in some platforms.
    
    * inspector/remote/glib/RemoteInspectorGlib.cpp:
    (Inspector::RemoteInspector::listingForInspectionTarget const):
    (Inspector::RemoteInspector::listingForAutomationTarget const):
    (Inspector::RemoteInspector::sendMessageToRemote):
    
    Source/WebKit:
    
    g_variant_builder_add requires data to have the correct width for their types, using
    casting if necessary. Corrected a call where a single precision float was being put
    into a double precision parameter without a cast.
    
    * UIProcess/API/glib/WebKitWebViewSessionState.cpp:
    (encodeFrameState):

--- Source/JavaScriptCore/inspector/remote/glib/RemoteInspectorGlib.cpp
+++ Source/JavaScriptCore/inspector/remote/glib/RemoteInspectorGlib.cpp
@@ -187,14 +187,16 @@ TargetListing RemoteInspector::listingForInspectionTarget(const RemoteInspection
         return nullptr;
 
     ASSERT(target.type() == RemoteInspectionTarget::Type::Web || target.type() == RemoteInspectionTarget::Type::JavaScript);
-    return g_variant_new("(tsssb)", target.targetIdentifier(), target.type() == RemoteInspectionTarget::Type::Web ? "Web" : "JavaScript",
+    return g_variant_new("(tsssb)", static_cast<guint64>(target.targetIdentifier()),
+        target.type() == RemoteInspectionTarget::Type::Web ? "Web" : "JavaScript",
         target.name().utf8().data(), target.type() == RemoteInspectionTarget::Type::Web ? target.url().utf8().data() : "null",
         target.hasLocalDebugger());
 }
 
 TargetListing RemoteInspector::listingForAutomationTarget(const RemoteAutomationTarget& target) const
 {
-    return g_variant_new("(tsssb)", target.targetIdentifier(), "Automation", target.name().utf8().data(), "null", target.isPaired());
+    return g_variant_new("(tsssb)", static_cast<guint64>(target.targetIdentifier()),
+        "Automation", target.name().utf8().data(), "null", target.isPaired());
 }
 
 void RemoteInspector::pushListingsNow()
@@ -274,7 +276,7 @@ void RemoteInspector::sendMessageToRemote(unsigned targetIdentifier, const Strin
 
     g_dbus_connection_call(m_dbusConnection.get(), nullptr,
         INSPECTOR_DBUS_OBJECT_PATH, INSPECTOR_DBUS_INTERFACE, "SendMessageToFrontend",
-        g_variant_new("(ts)", targetIdentifier, message.utf8().data()),
+        g_variant_new("(ts)", static_cast<guint64>(targetIdentifier), message.utf8().data()),
         nullptr, G_DBUS_CALL_FLAGS_NO_AUTO_START,
         -1, m_cancellable.get(), dbusConnectionCallAsyncReadyCallback, nullptr);
 }
