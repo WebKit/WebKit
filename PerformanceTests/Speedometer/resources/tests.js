2151becde0af3b894eb43868c4ae1c8b247cf17d
Ryosuke Niwa <rniwa@webkit.org>
2017-08-25 21:21:35 +0000

    Speedometer 2.0: Duplicate the current EmberJS test as EmberJS-Debug test https://bugs.webkit.org/show_bug.cgi?id=175956
    Reviewed by Saam Barati.
    
    Made the copy of Ember.js directory at r216946 as emberjs-debug, and added a new test suite "EmberJS-Debug-TodoMVC".
    
    * Speedometer/resources/tests.js:
    (Suites.push.prepare):
    * Speedometer/resources/todomvc/architecture-examples/emberjs-debug: Copied from emberjs@r216946.
    
    
    Canonical link: https://commits.webkit.org/192653@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@221205 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- PerformanceTests/Speedometer/resources/tests.js
+++ PerformanceTests/Speedometer/resources/tests.js
@@ -194,6 +194,35 @@ Suites.push({
 });
 
 Suites.push({
+    name: 'EmberJS-Debug-TodoMVC',
+    url: 'todomvc/architecture-examples/emberjs-debug/index.html',
+    prepare: function (runner, contentWindow, contentDocument) {
+        return runner.waitForElement('#new-todo').then(function (element) {
+            element.focus();
+            return element;
+        });
+    },
+    tests: [
+        new BenchmarkTestStep('Adding' + numberOfItemsToAdd + 'Items', function (newTodo, contentWindow, contentDocument) {
+            for (var i = 0; i < numberOfItemsToAdd; i++) {
+                newTodo.value = 'Something to do ' + i;
+                triggerEnter(newTodo, 'keydown');
+            }
+        }),
+        new BenchmarkTestStep('CompletingAllItems', function (params, contentWindow, contentDocument) {
+            var checkboxes = contentDocument.querySelectorAll('.toggle');
+            for (var i = 0; i < checkboxes.length; i++)
+                checkboxes[i].click();
+        }),
+        new BenchmarkTestStep('DeletingItems', function (params, contentWindow, contentDocument) {
+            var deleteButtons = contentDocument.querySelectorAll('.destroy');
+            for (var i = 0; i < deleteButtons.length; i++)
+                deleteButtons[i].click();
+        }),
+    ]
+});
+
+Suites.push({
     name: 'BackboneJS-TodoMVC',
     url: 'todomvc/architecture-examples/backbone/index.html',
     prepare: function (runner, contentWindow, contentDocument) {
