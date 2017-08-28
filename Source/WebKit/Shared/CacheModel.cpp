3f23bc73156b0d854a3887581899c4ffe229c2f2
Charlie Turner <cturner@igalia.com>
2017-08-28 12:43:16 +0000

    Merge r220963 - MemoryCache::setCapacities assertion failure maxDeadBytes <= totalBytes https://bugs.webkit.org/show_bug.cgi?id=175571
    Patch by Charlie Turner <cturner@igalia.com> on 2017-08-21
    Reviewed by Antti Koivisto.
    
    * Shared/CacheModel.cpp:
    (WebKit::calculateMemoryCacheSizes): Ensure cacheTotalCapacity is
    set to a reasonable value even in low-memory environments.

--- Source/WebKit/Shared/CacheModel.cpp
+++ Source/WebKit/Shared/CacheModel.cpp
@@ -51,6 +51,8 @@ void calculateMemoryCacheSizes(CacheModel cacheModel, unsigned& cacheTotalCapaci
             cacheTotalCapacity = 32 * MB;
         else if (memorySize >= 512)
             cacheTotalCapacity = 16 * MB;
+        else
+            cacheTotalCapacity = 8 * MB;
 
         cacheMinDeadCapacity = 0;
         cacheMaxDeadCapacity = 0;
@@ -75,6 +77,8 @@ void calculateMemoryCacheSizes(CacheModel cacheModel, unsigned& cacheTotalCapaci
             cacheTotalCapacity = 32 * MB;
         else if (memorySize >= 512)
             cacheTotalCapacity = 16 * MB;
+        else
+            cacheTotalCapacity = 8 * MB;
 
         cacheMinDeadCapacity = cacheTotalCapacity / 8;
         cacheMaxDeadCapacity = cacheTotalCapacity / 4;
@@ -102,6 +106,8 @@ void calculateMemoryCacheSizes(CacheModel cacheModel, unsigned& cacheTotalCapaci
             cacheTotalCapacity = 64 * MB;
         else if (memorySize >= 512)
             cacheTotalCapacity = 32 * MB;
+        else
+            cacheTotalCapacity = 16 * MB;
 
         cacheMinDeadCapacity = cacheTotalCapacity / 4;
         cacheMaxDeadCapacity = cacheTotalCapacity / 2;
