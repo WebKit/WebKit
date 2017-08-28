f4b8e6c0f89566450f81f2faf7d81bf4d4b3d897
Stephan Szabo <stephan.szabo@sony.com>
2017-08-28 23:29:29 +0000

    [WinCairo] Add PlatformWin cmake for Webkit https://bugs.webkit.org/show_bug.cgi?id=176027
    Patch by Stephan Szabo <stephan.szabo@sony.com> on 2017-08-28
    Reviewed by Brent Fulgham.
    
    * PlatformWin.cmake: Added.
    
    Canonical link: https://commits.webkit.org/192709@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@221279 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit/PlatformWin.cmake
@@ -0,0 +1,101 @@
+set(WebKit2_OUTPUT_NAME WebKit2)
+set(WebKit2_WebProcess_OUTPUT_NAME WebKitWebProcess)
+set(WebKit2_NetworkProcess_OUTPUT_NAME WebKitNetworkProcess)
+set(WebKit2_PluginProcess_OUTPUT_NAME WebKitPluginProcess)
+set(WebKit2_StorageProcess_OUTPUT_NAME WebKitStorageProcess)
+
+file(MAKE_DIRECTORY ${DERIVED_SOURCES_WEBKIT2_DIR})
+
+add_definitions(-DBUILDING_WEBKIT)
+
+list(APPEND WebKit2_SOURCES
+)
+
+# DerivedSources/JavaScriptCore/inspector/InspectorBackendCommands.js is
+# expected in DerivedSources/WebInspectorUI/UserInterface/Protocol/.
+add_custom_command(
+    OUTPUT ${DERIVED_SOURCES_WEBINSPECTORUI_DIR}/UserInterface/Protocol/InspectorBackendCommands.js
+    DEPENDS ${DERIVED_SOURCES_JAVASCRIPTCORE_DIR}/inspector/InspectorBackendCommands.js
+    COMMAND cp ${DERIVED_SOURCES_JAVASCRIPTCORE_DIR}/inspector/InspectorBackendCommands.js ${DERIVED_SOURCES_WEBINSPECTORUI_DIR}/UserInterface/Protocol/InspectorBackendCommands.js
+)
+
+list(APPEND WebKit2_INCLUDE_DIRECTORIES
+    "${WEBKIT2_DIR}/NetworkProcess/win"
+    "${WEBKIT2_DIR}/Platform/classifier"
+    "${WEBKIT2_DIR}/PluginProcess/win"
+    "${WEBKIT2_DIR}/Shared/API/c/win"
+    "${WEBKIT2_DIR}/Shared/CoordinatedGraphics"
+    "${WEBKIT2_DIR}/Shared/CoordinatedGraphics/threadedcompositor"
+    "${WEBKIT2_DIR}/Shared/Plugins/win"
+    "${WEBKIT2_DIR}/Shared/unix"
+    "${WEBKIT2_DIR}/Shared/win"
+    "${WEBKIT2_DIR}/StorageProcess/win"
+    "${WEBKIT2_DIR}/UIProcess/API/C/cairo"
+    "${WEBKIT2_DIR}/UIProcess/API/C/win"
+    "${WEBKIT2_DIR}/UIProcess/API/cpp/win"
+    "${WEBKIT2_DIR}/UIProcess/API/win"
+    "${WEBKIT2_DIR}/UIProcess/Plugins/win"
+    "${WEBKIT2_DIR}/UIProcess/win"
+    "${WEBKIT2_DIR}/WebProcess/InjectedBundle/API/win"
+    "${WEBKIT2_DIR}/WebProcess/InjectedBundle/API/win/DOM"
+    "${WEBKIT2_DIR}/WebProcess/win"
+    "${WEBKIT2_DIR}/WebProcess/WebCoreSupport/win"
+    "${WEBKIT2_DIR}/WebProcess/WebPage/CoordinatedGraphics"
+    "${WEBKIT2_DIR}/WebProcess/WebPage/win"
+    "${WEBKIT2_DIR}/win"
+)
+
+list(APPEND WebKit2_SYSTEM_INCLUDE_DIRECTORIES
+    ${CAIRO_INCLUDE_DIRS}
+)
+
+set(WebKit2CommonIncludeDirectories ${WebKit2_INCLUDE_DIRECTORIES})
+set(WebKit2CommonSystemIncludeDirectories ${WebKit2_SYSTEM_INCLUDE_DIRECTORIES})
+
+list(APPEND WebProcess_SOURCES
+)
+
+list(APPEND NetworkProcess_SOURCES
+)
+
+list(APPEND StorageProcess_SOURCES
+)
+
+if (${ENABLE_PLUGIN_PROCESS})
+    list(APPEND PluginProcess_SOURCES
+    )
+endif ()
+
+if (${WTF_PLATFORM_WIN_CAIRO})
+    add_definitions(-DUSE_CAIRO=1 -DUSE_CURL=1)
+
+    list(APPEND WebKit2_LIBRARIES
+        libeay32.lib
+        mfuuid.lib
+        ssleay32.lib
+        strmiids.lib
+    )
+endif ()
+
+list(APPEND WebKit2_LIBRARIES
+    WebCoreDerivedSources${DEBUG_SUFFIX}
+)
+
+set(SharedWebKit2Libraries
+    ${WebKit2_LIBRARIES}
+)
+
+add_custom_target(WebKit2-forwarding-headers
+                  COMMAND ${PERL_EXECUTABLE}
+                  ${WEBKIT2_DIR}/Scripts/generate-forwarding-headers.pl
+                  --include-path ${WEBKIT2_DIR}
+                  --output ${FORWARDING_HEADERS_DIR}
+                  --platform win
+                  --platform curl
+                  )
+
+set(WEBKIT2_EXTRA_DEPENDENCIES
+    WebKit2-forwarding-headers
+)
+
+WEBKIT_WRAP_SOURCELIST(${WebKit2_SOURCES})
