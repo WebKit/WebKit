d0c1c591edd200b99e40d2bb972516184cacbbe9
Yusuke Suzuki <utatane.tea@gmail.com>
2017-08-30 01:22:33 +0000

    [JSC] Use table based approach for JSON.stringify's Quote https://bugs.webkit.org/show_bug.cgi?id=176044
    Reviewed by Darin Adler.
    
    We change escape operation of JSON Quote from branch-based to table-based.
    This patch partially adopts SpiderMonkey's change to StringBuilderJSON.cpp
    to optimize this escaping operation. We separate changes from StringBuilder.cpp
    to apply MPL to StringBuilderJSON.cpp file. Since WebKit already adopts MPL in
    some files (like, DateMath.h), it is acceptable.
    
    Kraken json-stringify-tinderbox shows 7.2% improvement.
    
                                       baseline                  patched
    
    json-stringify-tinderbox        40.429+-0.771      ^      37.693+-0.862         ^ definitely 1.0726x faster
    
    * WTF.xcodeproj/project.pbxproj:
    * wtf/CMakeLists.txt:
    * wtf/text/StringBuilder.cpp:
    (WTF::appendQuotedJSONStringInternalSlow): Deleted.
    (WTF::appendQuotedJSONStringInternal): Deleted.
    (WTF::StringBuilder::appendQuotedJSONString): Deleted.
    * wtf/text/StringBuilderJSON.cpp: Added.
    (WTF::appendQuotedJSONStringInternal):
    (WTF::StringBuilder::appendQuotedJSONString):
    
    Canonical link: https://commits.webkit.org/192757@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@221330 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WTF/wtf/text/StringBuilderJSON.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2010, 2013, 2016 Apple Inc. All rights reserved.
+ * Copyright (C) 2012 Google Inc. All rights reserved.
+ * Copyright (C) 2017 Yusuke Suzuki <utatane.tea@gmail.com>. All rights reserved.
+ * Copyright (C) 2017 Mozilla Foundation. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "config.h"
+#include "StringBuilder.h"
+
+#include "WTFString.h"
+
+namespace WTF {
+
+// This table driven escaping is ported from SpiderMonkey.
+static const constexpr LChar escapedFormsForJSON[0x100] = {
+    'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u',
+    'b', 't', 'n', 'u', 'f', 'r', 'u', 'u',
+    'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u',
+    'u', 'u', 'u', 'u', 'u', 'u', 'u', 'u',
+    0,   0,  '\"', 0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,  '\\', 0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   0,
+};
+
+template<typename OutputCharacterType, typename InputCharacterType>
+ALWAYS_INLINE static void appendQuotedJSONStringInternal(OutputCharacterType*& output, const InputCharacterType* input, unsigned length)
+{
+    for (auto* end = input + length; input != end; ++input) {
+        auto character = *input;
+        auto escaped = escapedFormsForJSON[character & 0xFF];
+        if (LIKELY(!escaped || character > 0xFF)) {
+            *output++ = character;
+            continue;
+        }
+
+        *output++ = '\\';
+        *output++ = escaped;
+        if (UNLIKELY(escaped == 'u')) {
+            *output++ = '0';
+            *output++ = '0';
+            *output++ = upperNibbleToLowercaseASCIIHexDigit(character);
+            *output++ = lowerNibbleToLowercaseASCIIHexDigit(character);
+        }
+    }
+}
+
+void StringBuilder::appendQuotedJSONString(const String& string)
+{
+    // Make sure we have enough buffer space to append this string without having
+    // to worry about reallocating in the middle.
+    // The 2 is for the '"' quotes on each end.
+    // The 6 is for characters that need to be \uNNNN encoded.
+    Checked<unsigned> stringLength = string.length();
+    Checked<unsigned> maximumCapacityRequired = length();
+    maximumCapacityRequired += 2 + stringLength * 6;
+    unsigned allocationSize = maximumCapacityRequired.unsafeGet();
+    // This max() is here to allow us to allocate sizes between the range [2^31, 2^32 - 2] because roundUpToPowerOfTwo(1<<31 + some int smaller than 1<<31) == 0.
+    // FIXME: roundUpToPowerOfTwo should take Checked<unsigned> and abort if it fails to round up.
+    // https://bugs.webkit.org/show_bug.cgi?id=176086
+    allocationSize = std::max(allocationSize, roundUpToPowerOfTwo(allocationSize));
+
+    if (is8Bit() && !string.is8Bit())
+        allocateBufferUpConvert(m_bufferCharacters8, allocationSize);
+    else
+        reserveCapacity(allocationSize);
+    ASSERT(m_buffer->length() >= allocationSize);
+
+    if (is8Bit()) {
+        ASSERT(string.is8Bit());
+        LChar* output = m_bufferCharacters8 + m_length;
+        *output++ = '"';
+        appendQuotedJSONStringInternal(output, string.characters8(), string.length());
+        *output++ = '"';
+        m_length = output - m_bufferCharacters8;
+    } else {
+        UChar* output = m_bufferCharacters16 + m_length;
+        *output++ = '"';
+        if (string.is8Bit())
+            appendQuotedJSONStringInternal(output, string.characters8(), string.length());
+        else
+            appendQuotedJSONStringInternal(output, string.characters16(), string.length());
+        *output++ = '"';
+        m_length = output - m_bufferCharacters16;
+    }
+    ASSERT(m_buffer->length() >= m_length);
+}
+
+} // namespace WTF
