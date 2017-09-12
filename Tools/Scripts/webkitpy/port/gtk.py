dadade1f1e8781580db834bdf4543a3251f4e5a8
Michael Catanzaro <mcatanzaro@igalia.com>
2017-09-12 16:59:24 +0000

    [WPE][GTK] Run tests with G_DEBUG=fatal-criticals https://bugs.webkit.org/show_bug.cgi?id=176755
    Reviewed by Carlos Garcia Campos.
    
    * Scripts/webkitpy/port/gtk.py:
    (GtkPort.setup_environ_for_server):
    * Scripts/webkitpy/port/wpe.py:
    (WPEPort.setup_environ_for_server):
    
    
    Canonical link: https://commits.webkit.org/193266@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@221925 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/port/gtk.py
+++ Tools/Scripts/webkitpy/port/gtk.py
@@ -116,6 +116,7 @@ class GtkPort(Port):
 
     def setup_environ_for_server(self, server_name=None):
         environment = super(GtkPort, self).setup_environ_for_server(server_name)
+        environment['G_DEBUG'] = 'fatal-criticals'
         environment['GSETTINGS_BACKEND'] = 'memory'
         environment['LIBOVERLAY_SCROLLBAR'] = '0'
         environment['TEST_RUNNER_INJECTED_BUNDLE_FILENAME'] = self._build_path('lib', 'libTestRunnerInjectedBundle.so')
@@ -138,9 +139,9 @@ class GtkPort(Port):
             else:
                     _log.warning("Can't find Gallium llvmpipe driver. Try to run update-webkitgtk-libs")
         if self.get_option("leaks"):
-            #  Turn off GLib memory optimisations https://wiki.gnome.org/Valgrind.
+            # Turn off GLib memory optimisations https://wiki.gnome.org/Valgrind.
             environment['G_SLICE'] = 'always-malloc'
-            environment['G_DEBUG'] = 'gc-friendly'
+            environment['G_DEBUG'] += ',gc-friendly'
             xmlfilename = "".join(("drt-%p-", uuid.uuid1().hex, "-leaks.xml"))
             xmlfile = os.path.join(self.results_directory(), xmlfilename)
             suppressionsfile = self.path_from_webkit_base('Tools', 'Scripts', 'valgrind', 'suppressions.txt')
--- Tools/Scripts/webkitpy/port/wpe.py
+++ Tools/Scripts/webkitpy/port/wpe.py
@@ -63,6 +63,7 @@ class WPEPort(Port):
 
     def setup_environ_for_server(self, server_name=None):
         environment = super(WPEPort, self).setup_environ_for_server(server_name)
+        environment['G_DEBUG'] = 'fatal-criticals'
         environment['GSETTINGS_BACKEND'] = 'memory'
         environment['TEST_RUNNER_INJECTED_BUNDLE_FILENAME'] = self._build_path('lib', 'libTestRunnerInjectedBundle.so')
         environment['TEST_RUNNER_TEST_PLUGIN_PATH'] = self._build_path('lib', 'plugins')
