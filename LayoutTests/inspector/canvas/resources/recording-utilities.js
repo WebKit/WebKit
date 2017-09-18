15600a63d5a15664b91736cb0c3f8c7917723cb3
Devin Rousso <webkit@devinrousso.com>
2017-09-18 22:27:05 +0000

    Web Inspector: make Canvas recording tests more human readable https://bugs.webkit.org/show_bug.cgi?id=176441
    Reviewed by Joseph Pecoraro.
    
    * TestExpectations:
    
    * inspector/canvas/resources/recording-utilities.js: Added.
    (TestPage.registerInitializer):
    Create common functions for requesting and printing a recording.
    
    * inspector/canvas/recording-2d-expected.txt:
    * inspector/canvas/recording-2d.html:
    
    * inspector/canvas/recording-webgl-expected.txt:
    * inspector/canvas/recording-webgl.html:
    
    * inspector/canvas/recording-webgl-snapshots-expected.txt:
    * inspector/canvas/recording-webgl-snapshots.html:
    
    
    Canonical link: https://commits.webkit.org/193504@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@222182 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/inspector/canvas/resources/recording-utilities.js
@@ -0,0 +1,125 @@
+TestPage.registerInitializer(() => {
+    function sanitizeURL(url) {
+        return url.replace(/^.*?LayoutTests\//, "");
+    }
+
+    function log(object, indent) {
+        for (let key of Object.keys(object)) {
+            let value = object[key];
+            if (typeof value === "string")
+                value = sanitizeURL(value);
+            InspectorTest.log(indent + key + ": " + JSON.stringify(value));
+        }
+    }
+
+    function logRecording(recording) {
+        InspectorTest.log("initialState:");
+
+        InspectorTest.log("  attributes:");
+        log(recording.initialState.attributes, "    ");
+
+        InspectorTest.log("  parameters:");
+        log(recording.initialState.parameters, "    ");
+
+        InspectorTest.log("  content: " + JSON.stringify(recording.initialState.content));
+
+        InspectorTest.log("frames:");
+        for (let i = 0; i < recording.frames.length; ++i) {
+            let frame = recording.frames[i];
+            InspectorTest.log(`  ${i}:` + (frame.incomplete ? " (incomplete)" : ""));
+
+            for (let j = 0; j < frame.actions.length; ++j) {
+                let action = frame.actions[j];
+                let actionText = `    ${j}: ${action.name}`;
+
+                let parameters = action.parameters.map((parameter) => {
+                    if (typeof parameter === "object" && !Array.isArray(parameter))
+                        return String(parameter);
+                    return JSON.stringify(parameter);
+                });
+                if (action.isFunction)
+                    actionText += "(" + parameters.join(", ") + ")";
+                else if (!action.isGetter)
+                    actionText += " = " + parameters[0];
+
+                InspectorTest.log(actionText);
+
+                if (action.swizzleTypes.length) {
+                    let swizzleNames = action.swizzleTypes.map((item, i) => {
+                        let swizzleText = WI.Recording.displayNameForSwizzleType(item);
+                        if (action.parameters[i] != action._payloadParameters[i] && Number.isInteger(action._payloadParameters[i]))
+                            swizzleText += " (" + action._payloadParameters[i] + ")";
+                        return swizzleText;
+                    });
+                    InspectorTest.log("      swizzleTypes: [" + swizzleNames.join(", ") + "]");
+                }
+
+                if (action.trace.length) {
+                    InspectorTest.log("      trace:");
+
+                    for (let k = 0; k < action.trace.length; ++k) {
+                        let callFrame = action.trace[k];
+                        let traceText = `        ${k}: `;
+                        traceText += callFrame.functionName || "(anonymous function)";
+
+                        if (callFrame.nativeCode)
+                            traceText += " - [native code]";
+                        else if (callFrame.programCode)
+                            traceText += " - [program code]";
+                        else if (callFrame.sourceCodeLocation) {
+                            let location = callFrame.sourceCodeLocation;
+                            traceText += " - " + sanitizeURL(location.sourceCode.url) + `:${location.lineNumber}:${location.columnNumber}`;
+                        }
+
+                        traceText += " (" + action._payloadTrace[k] + ")";
+                        InspectorTest.log(traceText);
+                    }
+                }
+
+                if (action.snapshot)
+                    InspectorTest.log("      snapshot: " + JSON.stringify(action.snapshot) + " (" + action._payloadSnapshot + ")");
+            }
+        }
+
+        InspectorTest.log("data:");
+        log(recording.data, "  ");
+    }
+
+    window.getCanvas = function(type) {
+        let canvases = WI.canvasManager.canvases.filter((canvas) => canvas.contextType === type);
+        InspectorTest.assert(canvases.length === 1, `There should only be one canvas with type "${type}".`);
+        if (canvases.length !== 1)
+            return null;
+        return canvases[0];
+    };
+
+    window.requestRecording = function(type, resolve, reject, {singleFrame, memoryLimit} = {}) {
+        let canvas = getCanvas(type);
+        if (!canvas) {
+            reject(`Missing canvas with type "${type}".`);
+            return;
+        }
+
+        WI.canvasManager.awaitEvent(WI.CanvasManager.Event.RecordingFinished).then((event) => {
+            InspectorTest.evaluateInPage(`cancelActions()`);
+
+            let recording = event.data.recording;
+            InspectorTest.assert(recording.source.contextType === type, `Recording should be of a canvas with type "${type}".`);
+
+            return recording.actions.then(() => {
+                logRecording(recording, type);
+            });
+        }).then(resolve, reject);
+
+        CanvasAgent.requestRecording(canvas.identifier, singleFrame, memoryLimit, (error) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            InspectorTest.evaluateInPage(`performActions()`);
+        });
+
+        return canvas;
+    };
+});
