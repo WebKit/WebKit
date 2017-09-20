61567b07e904c9bbba2e334bee2fa92ca48c4a68
Zalan Bujtas <zalan@apple.com>
2017-09-20 01:21:33 +0000

    Cherry-pick r222214. rdar://problem/34534751
    Canonical link: https://commits.webkit.org/191388.152@safari-604-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-604-branch@222240 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/line/BreakingContext.h
+++ Source/WebCore/rendering/line/BreakingContext.h
@@ -773,7 +773,7 @@ inline bool BreakingContext::handleText(WordMeasurements& wordMeasurements, bool
 
     if (renderText.style().hasTextCombine() && is<RenderCombineText>(*m_current.renderer())) {
         auto& combineRenderer = downcast<RenderCombineText>(*m_current.renderer());
-        combineRenderer.combineText();
+        combineRenderer.combineTextIfNeeded();
         // The length of the renderer's text may have changed. Increment stale iterator positions
         if (iteratorIsBeyondEndOfRenderCombineText(m_lineBreakHistory.current(), combineRenderer)) {
             ASSERT(iteratorIsBeyondEndOfRenderCombineText(m_resolver.position(), combineRenderer));
