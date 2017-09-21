54dbeb1372f21b44edee2762278b64b4598d101b
Megan Gardner <megan_gardner@apple.com>
2017-09-21 18:48:08 +0000

    Add long press and drag test https://bugs.webkit.org/show_bug.cgi?id=177289
    Reviewed by Simon Fraser and Tim Horton.
    
    Adding a test to test the long press and then drag functionality of selection.
    Also add additional helper functions to basic gestures, and clarified the existing function
    names, as to be more clear in what behavior to expect from them.
    
    * fast/events/touch/ios/long-press-then-drag-to-select-text-expected.txt: Added.
    * fast/events/touch/ios/long-press-then-drag-to-select-text.html: Added.
    * fast/events/touch/ios/resources/basic-gestures.js:
    (touchAndDragFromPointToPoint):
    
    
    
    Canonical link: https://commits.webkit.org/193644@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@222337 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/fast/events/touch/ios/resources/basic-gestures.js
+++ LayoutTests/fast/events/touch/ios/resources/basic-gestures.js
@@ -8,6 +8,14 @@ function pressAtPoint(x, y)
     })();`
 }
 
+// We should switch to this method in all tests, as it is more clear what is being done.
+// The original helper function should not be used
+function touchAndDragFromPointToPoint(startX, startY, endX, endY)
+{
+    return dragFromPointToPoint(startX, startY, endX, endY);
+}
+
+
 function dragFromPointToPoint(startX, startY, endX, endY)
 {
     return `
@@ -51,4 +59,95 @@ function dragFromPointToPoint(startX, startY, endX, endY)
     uiController.sendEventStream(JSON.stringify(eventStream), function() {});
         uiController.uiScriptComplete();
     })();`
+}
+
+function pressAndHoldAtPoint(X, Y)
+{
+    return `
+    (function() {
+     var eventStream = {
+     events : [
+         {
+             interpolate : "linear",
+             timestep: 0.1,
+             coordinateSpace : "content",
+             startEvent : {
+             inputType : "hand",
+             timeOffset : 0,
+             touches : [
+                        {
+                        inputType : "finger",
+                        phase : "began",
+                        id : 1,
+                        x : ${X},
+                        y : ${Y},
+                        pressure : 0
+                        }
+                        ]
+             },
+             endEvent : {
+             inputType : "hand",
+             timeOffset : 2.0,
+             touches : [
+                        {
+                        inputType : "finger",
+                        phase : "moved",
+                        id : 1,
+                        x : ${X},
+                        y : ${Y},
+                        pressure : 0
+                        }
+                        ]
+             }
+         }]};
+     
+         uiController.sendEventStream(JSON.stringify(eventStream), function() {});
+         uiController.uiScriptComplete();
+     })();`
+}
+
+
+function continueTouchAndDragFromPointToPoint(startX, startY, endX, endY)
+{
+    return `
+    (function() {
+     var eventStream = {
+     events : [
+         {
+             interpolate : "linear",
+             timestep: 0.1,
+             coordinateSpace : "content",
+             startEvent : {
+             inputType : "hand",
+             timeOffset : 0,
+             touches : [
+                        {
+                        inputType : "finger",
+                        phase : "moved",
+                        id : 1,
+                        x : ${startX},
+                        y : ${startY},
+                        pressure : 0
+                        }
+                        ]
+             },
+             endEvent : {
+             inputType : "hand",
+             timeOffset : 0.5,
+             touches : [
+                        {
+                        inputType : "finger",
+                        phase : "moved",
+                        id : 1,
+                        x : ${endX},
+                        y : ${endY},
+                        pressure : 0
+                        }
+                        ]
+             }
+         }]};
+     
+         uiController.sendEventStream(JSON.stringify(eventStream), function() {});
+         uiController.uiScriptComplete();
+     })();`
 }
