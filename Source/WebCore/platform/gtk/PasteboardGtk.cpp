WebKit/WebKit

commit 0d59cebc74129a59d2378d3e01349ad3a81dc323

On iOS, getData can't get text set by setData during copy event https://bugs.webkit.org/show_bug.cgi?id=176980 <rdar://problem/34453915>
Reviewed by Darin Adler.

Source/WebCore:

The bug was caused by iOS port not implementing Pasteboard::createPrivate(). Rather than implementing this in iOS,
replace its use for copy & paste events by StaticPasteboard Wenson added for input events. This makes read-write
pasteboard platform agnostic and paves the way to make writing to pasteboard in a single IPC in WebKit2.

Also fixed a bug that iOS port's Pasteboard::types returned the list of all supported types instead of ones
actually present in the pasteboard.

This patch also adds a vector of types to StaticPasteboard to maintain the type order.

Tests: editing/pasteboard/dataTransfer-types-is-initially-empty-on-copy.html
       editing/pasteboard/dataTransfer-types-pasting-plaintext.html

* dom/DataTransfer.cpp:
(WebCore::DataTransfer::createForCopyAndPaste): Make StaticPasteboard instead of a private pasteboard.
(WebCore::DataTransfer::createForInputEvent):
* editing/Editor.cpp:
(WebCore::dispatchClipboardEvent): Call newly added commitToPasteboard on StaticPasteboard.
* platform/Pasteboard.h:
(WebCore::Pasteboard::isStatic const):
* platform/PasteboardStrategy.h:
* platform/StaticPasteboard.cpp:
(WebCore::StaticPasteboard::create): Deleted.
(WebCore::StaticPasteboard::StaticPasteboard):
(WebCore::StaticPasteboard::hasData):
(WebCore::StaticPasteboard::types): Deleted. Now simply returns m_type.
(WebCore::StaticPasteboard::readString): Added.
(WebCore::StaticPasteboard::writeString): Added.
(WebCore::StaticPasteboard::clear): Added.
(WebCore::StaticPasteboard::commitToPasteboard): Added.
(isType): Added.
* platform/StaticPasteboard.h:
(WebCore::Pasteboard::isStatic const): Added.
* platform/gtk/PasteboardGtk.cpp:
(WebCore::Pasteboard::createPrivate): Deleted.
(WebCore::Pasteboard::writePasteboard): Deleted.
* platform/ios/PasteboardIOS.mm:

Source/WebCore/platform/gtk/PasteboardGtk.cpp
@@ -50,11 +50,6 @@ std::unique_ptr<Pasteboard> Pasteboard::createForGlobalSelection()
     return std::make_unique<Pasteboard>("PRIMARY");
 }
 
-std::unique_ptr<Pasteboard> Pasteboard::createPrivate()
-{
-    return std::make_unique<Pasteboard>(SelectionData::create());
-}
-
 #if ENABLE(DRAG_SUPPORT)
 std::unique_ptr<Pasteboard> Pasteboard::createForDragAndDrop()
 {
@@ -195,29 +190,6 @@ void Pasteboard::write(const PasteboardWebContent& pasteboardContent)
     m_selectionData->setText(pasteboardContent.text);
     m_selectionData->setMarkup(pasteboardContent.markup);
     m_selectionData->setCanSmartReplace(pasteboardContent.canSmartCopyOrDelete);
-
-    writeToClipboard();
-}
-
-void Pasteboard::writePasteboard(const Pasteboard& sourcePasteboard)
-{
-    const auto& sourceDataObject = sourcePasteboard.selectionData();
-    m_selectionData->clearAll();
-
-    if (sourceDataObject.hasText())
-        m_selectionData->setText(sourceDataObject.text());
-    if (sourceDataObject.hasMarkup())
-        m_selectionData->setMarkup(sourceDataObject.markup());
-    if (sourceDataObject.hasURL())
-        m_selectionData->setURL(sourceDataObject.url(), sourceDataObject.urlLabel());
-    if (sourceDataObject.hasURIList())
-        m_selectionData->setURIList(sourceDataObject.uriList());
-    if (sourceDataObject.hasImage())
-        m_selectionData->setImage(sourceDataObject.image());
-    if (sourceDataObject.hasUnknownTypeData()) {
-        for (auto& it : sourceDataObject.unknownTypes())
-            m_selectionData->setUnknownTypeData(it.key, it.value);
-    }
 
     writeToClipboard();
 }

LayoutTests/platform/ios/TestExpectations
@@ -2225,18 +2225,14 @@ editing/pasteboard/paste-wrapped-blockquote-into-nonblockquote.html [ Failure ]
 # Failing pasteboard to triage
 editing/pasteboard/4242293-1.html [ Failure ]
 editing/pasteboard/5780697-2.html [ Failure ]
-editing/pasteboard/can-read-in-copy-and-cut-events.html [ Failure ]
 editing/pasteboard/clipboard-customData.html [ Failure ]
 editing/pasteboard/copy-backslash-with-euc.html [ Failure ]
 editing/pasteboard/copy-in-password-field.html [ Failure ]
 editing/pasteboard/copy-inside-h1-preserves-h1.html [ Failure ]
 editing/pasteboard/copy-text-with-backgroundcolor.html [ Failure ]
 editing/pasteboard/copy-two-pasteboard-types-both-work.html [ Failure ]
-editing/pasteboard/copy-without-selection.html [ Failure ]
 editing/pasteboard/data-transfer-items.html [ Failure ]
 editing/pasteboard/dataTransfer-setData-getData.html [ Failure ]
-editing/pasteboard/datatransfer-getdata-plaintext.html [ Failure ]
-editing/pasteboard/datatransfer-items-copy-plaintext.html [ Failure ]
 editing/pasteboard/display-block-on-spans.html [ Failure ]
 editing/pasteboard/emacs-ctrl-k-with-move.html [ Failure ]
 editing/pasteboard/emacs-ctrl-k-y-001.html [ Failure ]
@@ -2261,7 +2257,6 @@ editing/pasteboard/paste-plaintext-user-select-none.html [ Failure ]
 editing/pasteboard/paste-sanitize-crash-1.html [ Failure ]
 editing/pasteboard/paste-sanitize-crash-2.html [ Failure ]
 editing/pasteboard/paste-text-events.html [ Failure ]
-editing/pasteboard/pasting-empty-html-falls-back-to-text.html [ Failure ]
 editing/pasteboard/smart-paste-001.html [ Failure ]
 editing/pasteboard/smart-paste-002.html [ Failure ]
 editing/pasteboard/smart-paste-003.html [ Failure ]

LayoutTests/editing/pasteboard/dataTransfer-types-is-initially-empty-on-copy-expected.txt
@@ -1,0 +1,10 @@
+Tests that DataTransfer's types is initially empty on copy. To manually test, press the button below.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS JSON.stringify(event.clipboardData.types) is "[]"
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/editing/pasteboard/dataTransfer-types-is-initially-empty-on-copy.html
@@ -1,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script src="../../resources/js-test-pre.js"></script>
+<div id="container">
+<div id="source" contenteditable="true" oncopy="check(event)">hello</div>
+<button onclick="runTest()">Start the test</button>
+</div>
+<script>
+description("Tests that DataTransfer's types is initially empty on copy. To manually test, press the button below.");
+jsTestIsAsync = true;
+
+function check(event) {
+    shouldBeEqualToString('JSON.stringify(event.clipboardData.types)', '[]');
+}
+
+function runTest()
+{
+    const source = document.getElementById('source');
+    source.focus();
+    document.execCommand('SelectAll', false, null);
+    document.execCommand('Copy', false, null);
+    source.parentNode.style.display = 'none';
+    finishJSTest();
+}
+
+if (window.testRunner)
+    runTest();
+
+successfullyParsed = true;
+
+</script>
+<script src="../../resources/js-test-post.js"></script>
+</body>
+</html>

LayoutTests/editing/pasteboard/dataTransfer-types-pasting-plaintext-expected.txt
@@ -1,0 +1,13 @@
+Tests that DataTransfer's types contains text/plain but not no image types when pasting a plain text. To manually test, click on the button below then trigger paste (Command+V or Control+V).
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS types.includes("text/plain") is true
+PASS types.find((type) => type.includes("png")) is undefined.
+PASS types.find((type) => type.includes("jpeg")) is undefined.
+PASS types.find((type) => type.includes("tiff")) is undefined.
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/editing/pasteboard/dataTransfer-types-pasting-plaintext.html
@@ -1,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script src="../../resources/js-test-pre.js"></script>
+<script>
+description("Tests that DataTransfer's types contains text/plain but not no image types when pasting a plain text. To manually test, click on the button below then trigger paste (Command+V or Control+V).");
+jsTestIsAsync = true;
+
+function check(event) {
+    types = event.clipboardData.types;
+    shouldBeTrue('types.includes("text/plain")');
+    shouldBeUndefined('types.find((type) => type.includes("png"))');
+    shouldBeUndefined('types.find((type) => type.includes("jpeg"))');
+    shouldBeUndefined('types.find((type) => type.includes("tiff"))');
+    document.getElementById('container').style.display = 'none';
+    finishJSTest();
+}
+
+function runTest()
+{
+    const source = document.getElementById('source');
+    source.focus();
+    source.contentDocument.execCommand('selectAll', false, null);
+    source.contentDocument.execCommand('copy', false, null);
+    document.getElementById('destination').focus();
+    document.getElementById('destination').textContent = 'Now, paste here';
+    document.execCommand('selectAll', false, null);
+
+    document.querySelector('button').style.display = 'none';
+    source.style.display = 'none';
+
+    document.execCommand('paste', false, null);
+}
+
+successfullyParsed = true;
+
+</script>
+<div id="container">
+<button onclick="runTest()">Copy</button><br>
+<iframe id="source" onload="window.testRunner && runTest()" src="../resources/text-pasteboard-data.txt"></iframe>
+<div id="destination" contenteditable="true" onpaste="check(event)"></div>
+</div>
+
+<script src="../../resources/js-test-post.js"></script>
+</body>
+</html>

Source/WebCore/dom/DataTransfer.cpp
@@ -74,7 +74,7 @@ DataTransfer::DataTransfer(StoreMode mode, std::unique_ptr<Pasteboard> pasteboar
 
 Ref<DataTransfer> DataTransfer::createForCopyAndPaste(StoreMode mode)
 {
-    return adoptRef(*new DataTransfer(mode, mode == StoreMode::ReadWrite ? Pasteboard::createPrivate() : Pasteboard::createForCopyAndPaste()));
+    return adoptRef(*new DataTransfer(mode, mode == StoreMode::ReadWrite ? std::make_unique<StaticPasteboard>() : Pasteboard::createForCopyAndPaste()));
 }
 
 DataTransfer::~DataTransfer()
@@ -220,8 +220,10 @@ bool DataTransfer::hasStringOfType(const String& type)
 
 Ref<DataTransfer> DataTransfer::createForInputEvent(const String& plainText, const String& htmlText)
 {
-    TypeToStringMap typeToStringMap { { ASCIILiteral("text/plain"), plainText }, { ASCIILiteral("text/html"), htmlText } };
-    return adoptRef(*new DataTransfer(StoreMode::Readonly, StaticPasteboard::create(WTFMove(typeToStringMap)), Type::InputEvent));
+    auto pasteboard = std::make_unique<StaticPasteboard>();
+    pasteboard->writeString(ASCIILiteral("text/plain"), plainText);
+    pasteboard->writeString(ASCIILiteral("text/html"), htmlText);
+    return adoptRef(*new DataTransfer(StoreMode::Readonly, WTFMove(pasteboard), Type::InputEvent));
 }
 
 #if !ENABLE(DRAG_SUPPORT)

Source/WebCore/editing/Editor.cpp
@@ -85,6 +85,7 @@
 #include "SimplifyMarkupCommand.h"
 #include "SpellChecker.h"
 #include "SpellingCorrectionCommand.h"
+#include "StaticPasteboard.h"
 #include "StyleProperties.h"
 #include "TelephoneNumberDetector.h"
 #include "Text.h"
@@ -345,7 +346,7 @@ static bool dispatchClipboardEvent(RefPtr<Element>&& target, const AtomicString&
     if (noDefaultProcessing && storeMode == DataTransfer::StoreMode::ReadWrite) {
         auto pasteboard = Pasteboard::createForCopyAndPaste();
         pasteboard->clear();
-        pasteboard->writePasteboard(dataTransfer->pasteboard());
+        downcast<StaticPasteboard>(dataTransfer->pasteboard()).commitToPasteboard(*pasteboard);
     }
 
     dataTransfer->makeInvalidForSecurity();

commit 062deeec1862601123747da51ad7fa5d667c5f1b

[GTK] Couple of pasteboard related tests are failing after r222228 https://bugs.webkit.org/show_bug.cgi?id=177295
Reviewed by Michael Catanzaro.

Source/WebCore:

Call writeToClipboard() from Pasteboard::writeString() on GTK.

When writePasteboard was replaced by commitToPasteboard in r222228,
the call to writeToClipboard() did not make it into the
platform-neutral code. This attempts to find another place in the
GTK-specific code to make the call.

Tests: editing/pasteboard/copy-without-selection.html
       editing/pasteboard/pasting-empty-html-falls-back-to-text.html

* platform/gtk/PasteboardGtk.cpp:
(WebCore::Pasteboard::writeString):

LayoutTests:

* platform/gtk/TestExpectations: mark the tests as passing again.


Canonical link: https://commits.webkit.org/193687@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@222387 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/gtk/PasteboardGtk.cpp
@@ -133,19 +133,20 @@ void Pasteboard::writeString(const String& type, const String& data)
     case ClipboardDataTypeURIList:
     case ClipboardDataTypeURL:
         m_selectionData->setURIList(data);
-        return;
+        break;
     case ClipboardDataTypeMarkup:
         m_selectionData->setMarkup(data);
-        return;
+        break;
     case ClipboardDataTypeText:
         m_selectionData->setText(data);
-        return;
+        break;
     case ClipboardDataTypeUnknown:
         m_selectionData->setUnknownTypeData(type, data);
-        return;
+        break;
     case ClipboardDataTypeImage:
         break;
     }
+    writeToClipboard();
 }
 
 void Pasteboard::writePlainText(const String& text, SmartReplaceOption smartReplaceOption)

LayoutTests/platform/gtk/TestExpectations
@@ -3466,9 +3466,6 @@ webkit.org/b/176652 http/tests/cache-storage/cache-records-persistency.https.htm
 webkit.org/b/177294 platform/gtk/fonts/fontconfig-aliasing-settings.html [ Failure ]
 webkit.org/b/177294 fast/text/fitzpatrick-combination.html [ ImageOnlyFailure ]
 
-webkit.org/b/177295 editing/pasteboard/copy-without-selection.html [ Failure ]
-webkit.org/b/177295 editing/pasteboard/pasting-empty-html-falls-back-to-text.html [ Failure ]
-
 webkit.org/b/177296 editing/deleting/smart-delete-002.html [ Failure ]
 
 webkit.org/b/177319 imported/w3c/web-platform-tests/html/semantics/forms/the-form-element/form-elements-filter.html [ Failure ]