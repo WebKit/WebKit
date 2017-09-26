b695c4f1d49f753fb775853b93cbb0be59d16ba9
Joanmarie Diggs <jdiggs@igalia.com>
2017-09-26 22:19:12 +0000

    AX: ARIA grids claim to be multiselectable even with aria-multiselectable is set to false https://bugs.webkit.org/show_bug.cgi?id=177498
    Reviewed by Chris Fleizach.
    
    Source/WebCore:
    
    AccessibilityARIAGrid::isMultiSelectable() was unconditionally returning
    true. Modify it to return false if aria-multiselectable has been set to
    false by the author.
    
    Test: accessibility/aria-multiselectable-grid.html
    
    * accessibility/AccessibilityARIAGrid.cpp:
    (WebCore::AccessibilityARIAGrid::isMultiSelectable const):
    * accessibility/AccessibilityARIAGrid.h:
    
    LayoutTests:
    
    * accessibility/aria-multiselectable-grid-expected.txt: Added.
    * accessibility/aria-multiselectable-grid.html: Added.
    
    
    Canonical link: https://commits.webkit.org/193819@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@222527 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/accessibility/AccessibilityARIAGrid.h
+++ Source/WebCore/accessibility/AccessibilityARIAGrid.h
@@ -48,7 +48,7 @@ private:
 
     // ARIA treegrids and grids support selected rows.
     bool supportsSelectedRows() override { return true; }
-    bool isMultiSelectable() const override { return true; }
+    bool isMultiSelectable() const override;
     bool computeIsTableExposableThroughAccessibility() const override { return true; }
     bool isAriaTable() const override { return true; }
     
