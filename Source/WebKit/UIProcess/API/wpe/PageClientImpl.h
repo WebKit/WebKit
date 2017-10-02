03247775503f76f9208a2e88c3a9057a5bcf44c9
Olivier Blin <olivier.blin@softathome.com>
2017-10-02 19:02:27 +0000

    [WPE] Fix UIProcess build with GStreamer and without VIDEO https://bugs.webkit.org/show_bug.cgi?id=177753
    Patch by Olivier Blin <olivier.blin@softathome.com> on 2017-10-02
    Reviewed by Michael Catanzaro.
    
    GStreamer builds fail when WebAudio is enabled but VIDEO disabled.
    This is the WPE counterpart of bug 153135.
    
    * UIProcess/API/wpe/PageClientImpl.h:
    
    Canonical link: https://commits.webkit.org/194012@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@222729 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/wpe/PageClientImpl.h
+++ Source/WebKit/UIProcess/API/wpe/PageClientImpl.h
@@ -111,7 +111,7 @@ private:
     void refView() override;
     void derefView() override;
 
-#if ENABLE(VIDEO)
+#if ENABLE(VIDEO) && USE(GSTREAMER)
     bool decidePolicyForInstallMissingMediaPluginsPermissionRequest(InstallMissingMediaPluginsPermissionRequest&) override;
 #endif
 
