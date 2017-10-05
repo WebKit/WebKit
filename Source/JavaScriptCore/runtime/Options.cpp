e9b5d2497b06efb42ee85ced1dbb3534183d5584
Saam Barati <sbarati@apple.com>
2017-10-05 18:59:58 +0000

    Unreviewed. Disable probe OSR exit on 32-bit until it's fixed.
    * runtime/Options.cpp:
    (JSC::recomputeDependentOptions):
    
    
    
    Canonical link: https://commits.webkit.org/194198@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@222925 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/Options.cpp
+++ Source/JavaScriptCore/runtime/Options.cpp
@@ -502,6 +502,12 @@ static void recomputeDependentOptions()
         Options::reservedZoneSize() = minimumReservedZoneSize;
     if (Options::softReservedZoneSize() < Options::reservedZoneSize() + minimumReservedZoneSize)
         Options::softReservedZoneSize() = Options::reservedZoneSize() + minimumReservedZoneSize;
+
+#if USE(JSVALUE32_64)
+    // FIXME: Make probe OSR exit work on 32-bit:
+    // https://bugs.webkit.org/show_bug.cgi?id=177956
+    Options::useProbeOSRExit() = false;
+#endif
 }
 
 void Options::initialize()
