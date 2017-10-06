9423d14c113a0ed98fb3935a36425816fa4005d5
Filip Pizlo <fpizlo@apple.com>
2017-10-06 16:34:41 +0000

    Use one Scavenger thread for all Heaps https://bugs.webkit.org/show_bug.cgi?id=174973
    Reviewed by JF Bastien.
    
    This combines the scavengers from all Heap instances into a single scavenger. It also combines
    the accounting for deciding when to run. Each Heap still controls what it means to scavenge
    itself (it's all in Heap::scavenge) but the policy decisions are all controlled by Scavenger.
    Because Scavenger is also the only thing that needs an AsyncTask, this removes AsyncTask and
    moves all of AsyncTask's logic into Scavenger.
    
    This appears to be a 1% progression on JetStream (with high statistical confidence: p = 0.0049).
    
    * bmalloc.xcodeproj/project.pbxproj:
    * bmalloc/AsyncTask.h: Removed.
    * bmalloc/Heap.cpp:
    (bmalloc::Heap::Heap):
    (bmalloc::Heap::allocateSmallChunk):
    (bmalloc::Heap::allocateSmallPage):
    (bmalloc::Heap::deallocateSmallLine):
    (bmalloc::Heap::splitAndAllocate):
    (bmalloc::Heap::tryAllocateLarge):
    (bmalloc::Heap::shrinkLarge):
    (bmalloc::Heap::deallocateLarge):
    (bmalloc::Heap::concurrentScavenge): Deleted.
    (bmalloc::Heap::scheduleScavengerIfUnderMemoryPressure): Deleted.
    (bmalloc::Heap::scheduleScavenger): Deleted.
    * bmalloc/Heap.h:
    * bmalloc/Scavenger.cpp:
    (bmalloc::Scavenger::Scavenger):
    (bmalloc::Scavenger::run):
    (bmalloc::Scavenger::runHoldingLock):
    (bmalloc::Scavenger::runSoon):
    (bmalloc::Scavenger::runSoonHoldingLock):
    (bmalloc::Scavenger::didStartGrowing):
    (bmalloc::Scavenger::scheduleIfUnderMemoryPressure):
    (bmalloc::Scavenger::scheduleIfUnderMemoryPressureHoldingLock):
    (bmalloc::Scavenger::schedule):
    (bmalloc::Scavenger::threadEntryPoint):
    (bmalloc::Scavenger::threadRunLoop):
    (bmalloc::Scavenger::setSelfQOSClass):
    * bmalloc/Scavenger.h:
    (bmalloc::Scavenger::willRun):
    (bmalloc::Scavenger::willRunSoon):
    
    
    
    Canonical link: https://commits.webkit.org/194254@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@222982 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/bmalloc/bmalloc/Scavenger.cpp
+++ Source/bmalloc/bmalloc/Scavenger.cpp
@@ -25,6 +25,7 @@
 
 #include "Scavenger.h"
 
+#include "AvailableMemory.h"
 #include "Heap.h"
 #include <thread>
 
@@ -41,6 +42,76 @@ Scavenger::Scavenger(std::lock_guard<StaticMutex>&)
     dispatch_resume(m_pressureHandlerDispatchSource);
     dispatch_release(queue);
 #endif
+    
+    m_thread = std::thread(&threadEntryPoint, this);
+}
+
+void Scavenger::run()
+{
+    std::lock_guard<Mutex> lock(m_mutex);
+    runHoldingLock();
+}
+
+void Scavenger::runHoldingLock()
+{
+    m_state = State::Run;
+    m_condition.notify_all();
+}
+
+void Scavenger::runSoon()
+{
+    std::lock_guard<Mutex> lock(m_mutex);
+    runSoonHoldingLock();
+}
+
+void Scavenger::runSoonHoldingLock()
+{
+    if (willRunSoon())
+        return;
+    m_state = State::RunSoon;
+    m_condition.notify_all();
+}
+
+void Scavenger::didStartGrowing()
+{
+    // We don't really need to lock here, since this is just a heuristic.
+    m_isProbablyGrowing = true;
+}
+
+void Scavenger::scheduleIfUnderMemoryPressure(size_t bytes)
+{
+    std::lock_guard<Mutex> lock(m_mutex);
+    scheduleIfUnderMemoryPressureHoldingLock(bytes);
+}
+
+void Scavenger::scheduleIfUnderMemoryPressureHoldingLock(size_t bytes)
+{
+    m_scavengerBytes += bytes;
+    if (m_scavengerBytes < scavengerBytesPerMemoryPressureCheck)
+        return;
+
+    m_scavengerBytes = 0;
+
+    if (willRun())
+        return;
+
+    if (!isUnderMemoryPressure())
+        return;
+
+    m_isProbablyGrowing = false;
+    runHoldingLock();
+}
+
+void Scavenger::schedule(size_t bytes)
+{
+    std::lock_guard<Mutex> lock(m_mutex);
+    scheduleIfUnderMemoryPressureHoldingLock(bytes);
+    
+    if (willRunSoon())
+        return;
+    
+    m_isProbablyGrowing = false;
+    runSoonHoldingLock();
 }
 
 void Scavenger::scavenge()
@@ -48,6 +119,58 @@ void Scavenger::scavenge()
     std::lock_guard<StaticMutex> lock(Heap::mutex());
     for (unsigned i = numHeaps; i--;)
         PerProcess<PerHeapKind<Heap>>::get()->at(i).scavenge(lock);
+}
+
+void Scavenger::threadEntryPoint(Scavenger* scavenger)
+{
+    scavenger->threadRunLoop();
+}
+
+void Scavenger::threadRunLoop()
+{
+    setSelfQOSClass();
+    
+    // This loop ratchets downward from most active to least active state. While
+    // we ratchet downward, any other thread may reset our state.
+    
+    // We require any state change while we are sleeping to signal to our
+    // condition variable and wake us up.
+    
+    auto truth = [] { return true; };
+    
+    while (truth()) {
+        if (m_state == State::Sleep) {
+            std::unique_lock<Mutex> lock(m_mutex);
+            m_condition.wait(lock, [&]() { return m_state != State::Sleep; });
+        }
+        
+        if (m_state == State::RunSoon) {
+            std::unique_lock<Mutex> lock(m_mutex);
+            m_condition.wait_for(lock, asyncTaskSleepDuration, [&]() { return m_state != State::RunSoon; });
+        }
+        
+        m_state = State::Sleep;
+        
+        setSelfQOSClass();
+        
+        {
+            std::unique_lock<Mutex> lock(m_mutex);
+            if (m_isProbablyGrowing && !isUnderMemoryPressure()) {
+                m_isProbablyGrowing = false;
+                runSoonHoldingLock();
+                continue;
+            }
+        }
+        
+        scavenge();
+    }
+}
+
+void Scavenger::setSelfQOSClass()
+{
+#if BOS(DARWIN)
+    pthread_set_qos_class_self_np(requestedScavengerThreadQOSClass(), 0);
+#endif
 }
 
 } // namespace bmalloc
