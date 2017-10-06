fbb4a9d5cec1658f1412fabbe931644fa9ac87bc
Youenn Fablet <youenn@apple.com>
2017-10-06 16:59:41 +0000

    Removing some dead code in RTCPeerConnection https://bugs.webkit.org/show_bug.cgi?id=178011
    Patch by Youenn Fablet <youenn@apple.com> on 2017-10-06
    Reviewed by Alejandro G. Castro.
    
    No change of behavior.
    Removing code used by non-libwebrtc WebRTC backends.
    
    * Modules/mediastream/RTCPeerConnection.cpp:
    (WebCore::RTCPeerConnection::addTrack):
    (WebCore::RTCPeerConnection::removeTrack):
    (WebCore::RTCPeerConnection::completeAddTransceiver):
    
    Canonical link: https://commits.webkit.org/194259@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@222987 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/mediastream/RTCPeerConnection.cpp
+++ Source/WebCore/Modules/mediastream/RTCPeerConnection.cpp
@@ -159,10 +159,6 @@ ExceptionOr<Ref<RTCRtpSender>> RTCPeerConnection::addTrack(Ref<MediaStreamTrack>
         m_transceiverSet->append(WTFMove(transceiver));
     }
 
-#if !USE(LIBWEBRTC)
-    m_backend->markAsNeedingNegotiation();
-#endif
-
     m_backend->notifyAddedTrack(*sender);
     return Ref<RTCRtpSender> { *sender };
 }
@@ -187,9 +183,6 @@ ExceptionOr<void> RTCPeerConnection::removeTrack(RTCRtpSender& sender)
     sender.stop();
 
     m_backend->notifyRemovedTrack(sender);
-#if !USE(LIBWEBRTC)
-    m_backend->markAsNeedingNegotiation();
-#endif
     return { };
 }
 
@@ -223,9 +216,6 @@ Ref<RTCRtpTransceiver> RTCPeerConnection::completeAddTransceiver(Ref<RTCRtpSende
     transceiver->setDirection(init.direction);
 
     m_transceiverSet->append(transceiver.copyRef());
-#if !USE(LIBWEBRTC)
-    m_backend->markAsNeedingNegotiation();
-#endif
     return transceiver;
 }
 
