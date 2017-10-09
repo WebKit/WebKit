43b76d26b9f2c9896feb9b27ad40529bb66d6630
Wenson Hsieh <wenson_hsieh@apple.com>
2017-10-09 18:04:00 +0000

    Unreviewed, another build fix attempt after r223031
    The default constructor of DragTargetResponse is also invoked via initializer list in EventHandler.cpp, so we'll
    need to explicitly declare this constructor in the header.
    
    * page/EventHandler.h:
    
    Canonical link: https://commits.webkit.org/194325@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@223054 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/EventHandler.h
+++ Source/WebCore/page/EventHandler.h
@@ -158,6 +158,7 @@ public:
 #if ENABLE(DRAG_SUPPORT)
     struct DragTargetResponse {
 #if !COMPILER_SUPPORTS(NSDMI_FOR_AGGREGATES)
+        DragTargetResponse() = default;
         DragTargetResponse(bool accept, std::optional<DragOperation> operation)
             : accept(accept)
             , operation(operation)
