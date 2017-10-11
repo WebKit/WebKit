89e158e82338da4d9bd3f63df4132d6ad5eff6e3
Chris Dumez <cdumez@apple.com>
2017-10-11 19:51:29 +0000

    Unreviewed, fix build with some SDKs.
    Stop capturing |this| unnecessarily in lambda.
    
    * Modules/entriesapi/DOMFileSystem.cpp:
    (WebCore::DOMFileSystem::getFile):
    
    Canonical link: https://commits.webkit.org/194434@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@223204 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/entriesapi/DOMFileSystem.cpp
+++ Source/WebCore/Modules/entriesapi/DOMFileSystem.cpp
@@ -336,7 +336,7 @@ void DOMFileSystem::getFile(ScriptExecutionContext& context, FileSystemFileEntry
 {
     auto virtualPath = fileEntry.virtualPath();
     auto fullPath = evaluatePath(virtualPath);
-    m_workQueue->dispatch([this, context = makeRef(context), fullPath = crossThreadCopy(fullPath), virtualPath = crossThreadCopy(virtualPath), completionCallback = WTFMove(completionCallback)]() mutable {
+    m_workQueue->dispatch([context = makeRef(context), fullPath = crossThreadCopy(fullPath), virtualPath = crossThreadCopy(virtualPath), completionCallback = WTFMove(completionCallback)]() mutable {
         auto validatedVirtualPath = validatePathIsExpectedType(fullPath, WTFMove(virtualPath), FileMetadata::Type::File);
         callOnMainThread([context = WTFMove(context), fullPath = crossThreadCopy(fullPath), validatedVirtualPath = crossThreadCopy(validatedVirtualPath), completionCallback = WTFMove(completionCallback)]() mutable {
             if (validatedVirtualPath.hasException())
