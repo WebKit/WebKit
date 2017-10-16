d27d70edfd050071c46655050fa1bf69dd9d8ff3
Wenson Hsieh <wenson_hsieh@apple.com>
2017-10-16 10:45:18 +0000

    Merge r222114 - Avoid style recomputation when forwarding a focus event to an text field's input type https://bugs.webkit.org/show_bug.cgi?id=176160 <rdar://problem/34184820>
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Currently, TextFieldInputType::forwardEvent synchronously triggers style recomputation, for the purpose of
    scrolling to the origin upon handling a blur event, and also for updating caps lock state after a blur or focus.
    In synchronously triggering style recomputation, we may end up running arbitrary JavaScript, which may change
    the HTMLInputElement's type and cause the current TextFieldInputType to be destroyed.
    
    To mitigate this, we only update caps lock state when forwarding a focus or blur event to the InputType, and
    instead scroll blurred text fields to the origin later, in HTMLInputElement::didBlur (invoked from
    Document::setFocusedElement after blur and focusout events have fired). Instead of having the InputType update
    style, lift the call to Document::updateStyleIfNeeded up into HTMLInputElement so that we gracefully handle the
    case where the page destroys and sets a new InputType within the scope of this style update.
    
    Test: fast/forms/change-input-type-in-focus-handler.html
    
    * dom/Document.cpp:
    (WebCore::Document::setFocusedElement):
    * html/HTMLInputElement.cpp:
    (WebCore::HTMLInputElement::didBlur):
    * html/HTMLInputElement.h:
    * html/InputType.h:
    (WebCore::InputType::elementDidBlur):
    * html/TextFieldInputType.cpp:
    (WebCore::TextFieldInputType::forwardEvent):
    (WebCore::TextFieldInputType::elementDidBlur):
    * html/TextFieldInputType.h:
    
    LayoutTests:
    
    Adds a new layout test verifying that we don't crash when changing the input type from within a focus event listener.
    
    * fast/forms/change-input-type-in-focus-handler-expected.txt: Added.
    * fast/forms/change-input-type-in-focus-handler.html: Added.

--- Source/WebCore/dom/Document.cpp
+++ Source/WebCore/dom/Document.cpp
@@ -3795,6 +3795,9 @@ bool Document::setFocusedElement(Element* element, FocusDirection direction, Foc
             else
                 view()->setFocus(false);
         }
+
+        if (is<HTMLInputElement>(oldFocusedElement.get()))
+            downcast<HTMLInputElement>(*oldFocusedElement).didBlur();
     }
 
     if (newFocusedElement && newFocusedElement->isFocusable()) {
