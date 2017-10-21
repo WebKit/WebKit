da60fdddb7e8e5c66c1c45e606106ae60f38957b
Mark Lam <mark.lam@apple.com>
2017-10-21 19:19:47 +0000

    Cherry-pick r223731. rdar://problem/35100279
    Canonical link: https://commits.webkit.org/191388.264@safari-604-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-604-branch@223816 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ JSTests/stress/regress-178386.js
@@ -0,0 +1,12 @@
+var str1 = String.fromCharCode(136, 115, 29, 20, 15, 155, 81);
+str3 = str1.padEnd(0x7FFFFFFC, '123');
+
+var exception;
+try {
+    JSON.stringify(str3);
+} catch (e) {
+    exception = e;
+}
+
+if (exception != "Error: Out of memory")
+    throw "FAILED";
