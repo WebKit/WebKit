cd3d85e93e74584bf84177c637740bda8e5e6814
Žan Doberšek <zdobersek@igalia.com>
2017-10-24 08:20:03 +0000

    [GTK][WPE] generate-forwarding-headers.pl should also scan derived sources in WebKit framework https://bugs.webkit.org/show_bug.cgi?id=178579
    Reviewed by Carlos Garcia Campos.
    
    * PlatformGTK.cmake: When generating forwarding headers, derived sources
    should be scanned as well. The DERIVED_SOURCES_WEBKIT_DIR value has to
    be passed as an include path to the generate-forward-headers.pl script
    to achieve that. This target then also has to depend on all the derived
    source files to be generated before this can be performed.
    * PlatformWPE.cmake: Ditto.
    
    
    Canonical link: https://commits.webkit.org/194880@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@223884 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/PlatformWPE.cmake
+++ Source/WebKit/PlatformWPE.cmake
@@ -20,7 +20,8 @@ file(REMOVE "${FORWARDING_HEADERS_DIR}/WebCore/Settings.h")
 set(WebKit_USE_PREFIX_HEADER ON)
 
 add_custom_target(webkitwpe-forwarding-headers
-    COMMAND ${PERL_EXECUTABLE} ${WEBKIT_DIR}/Scripts/generate-forwarding-headers.pl --include-path ${WEBKIT_DIR} --output ${FORWARDING_HEADERS_DIR} --platform wpe --platform soup
+    DEPENDS ${WebKit_DERIVED_SOURCES}
+    COMMAND ${PERL_EXECUTABLE} ${WEBKIT_DIR}/Scripts/generate-forwarding-headers.pl --include-path ${WEBKIT_DIR} --include-path ${DERIVED_SOURCES_WEBKIT_DIR} --output ${FORWARDING_HEADERS_DIR} --platform wpe --platform soup
 )
 
  # These symbolic link allows includes like #include <wpe/WebkitWebView.h> which simulates installed headers.
