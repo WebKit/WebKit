286c468d8994f78adbd16660d46561ce71925123
Jer Noble <jer.noble@apple.com>
2017-10-25 19:01:41 +0000

    Cherry-pick r223960. rdar://problem/35178892
    Canonical link: https://commits.webkit.org/191388.266@safari-604-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-604-branch@223969 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLVideoElement.cpp
+++ Source/WebCore/html/HTMLVideoElement.cpp
@@ -66,6 +66,7 @@ inline HTMLVideoElement::HTMLVideoElement(const QualifiedName& tagName, Document
 Ref<HTMLVideoElement> HTMLVideoElement::create(const QualifiedName& tagName, Document& document, bool createdByParser)
 {
     auto videoElement = adoptRef(*new HTMLVideoElement(tagName, document, createdByParser));
+    videoElement->finishInitialization();
     videoElement->suspendIfNeeded();
     return videoElement;
 }
