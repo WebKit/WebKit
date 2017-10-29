dce360114f4dc5b51f919d0c422ed9b4796a2193
Ryosuke Niwa <rniwa@webkit.org>
2017-10-29 10:15:55 +0000

    Assert that no script is executed during style recalc https://bugs.webkit.org/show_bug.cgi?id=178845 <rdar://problem/35106129>
    Reviewed by Antti Koivisto.
    
    This patch adds NoEventDispatchAssertion to Document::updateStyle and Document::updateStyleIfNeeded
    to make sure we don't start mutating DOM in the middle of a style update.
    
    Added NoEventDispatchAssertion::EventAllowedScope for various places in SVGUseElement to update its
    shadow tree since that happens while updating the style.
    
    No new tests since there should be no behavioral change.
    
    * dom/Document.cpp:
    (WebCore::Document::resolveStyle): Added NoEventDispatchAssertion while flushing pending stylesheets
    and calling FrameView::willRecalcStyle, and while the style tree solver is in works. Also moved in
    the code to update the selection and schedule to dispatch a fake mouse event into the same scope.
    Also increment m_styleRecalcCount in the same code since post resolution callbacks could run author
    scripts which in turn trigger another (recursive) style recalc.
    (WebCore::Document::updateStyleIfNeeded): Put everything but the call to resolveStyle in a scope with
    NoEventDispatchAssertion.
    * dom/Element.cpp:
    (WebCore::Element::cloneElementWithChildren): Added NoEventDispatchAssertion::EventAllowedScope to the
    newly cloned element for SVG use element's shadow tree.
    (WebCore::Element::cloneElementWithoutChildren): Ditto.
    * dom/EventDispatcher.cpp:
    (WebCore::EventDispatcher::dispatchEvent): Make the assertion more precise to workaround the fact SVG
    use elements update its shadow tree in the middle of style updates. Also removed a redundant assertion
    since the result of NoEventDispatchAssertion::isEventDispatchAllowedInSubtree cannot chance without
    pushing or popoing the stack frame.
    * svg/SVGUseElement.cpp:
    (WebCore::SVGUseElement::clearShadowTree):
    (WebCore::SVGUseElement::updateShadowTree): Added NoEventDispatchAssertion to the user-agent shadow root
    of a SVG use element. Since this is a newly created shadow tree which hasn't been exposed to author
    scripts, it's safe to mutate them during the style recalc even though it's not the best design.
    (WebCore::SVGUseElement::cloneTarget const): Ditto.
    (WebCore::SVGUseElement::expandUseElementsInShadowTree const): Ditto.
    (WebCore::SVGUseElement::expandSymbolElementsInShadowTree const): Ditto.
    (WebCore::SVGUseElement::transferEventListenersToShadowTree const):
    
    
    Canonical link: https://commits.webkit.org/195128@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@224159 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/Document.cpp
+++ Source/WebCore/dom/Document.cpp
@@ -1792,17 +1792,21 @@ void Document::resolveStyle(ResolveStyleType type)
     // re-attaching our containing iframe, which when asked HTMLFrameElementBase::isURLAllowed
     // hits a null-dereference due to security code always assuming the document has a SecurityOrigin.
 
-    styleScope().flushPendingUpdate();
-
-    frameView.willRecalcStyle();
+    {
+        NoEventDispatchAssertion noEventDispatchAssertion;
+        styleScope().flushPendingUpdate();
+        frameView.willRecalcStyle();
+    }
 
     InspectorInstrumentationCookie cookie = InspectorInstrumentation::willRecalculateStyle(*this);
 
-    m_inStyleRecalc = true;
     bool updatedCompositingLayers = false;
     {
         Style::PostResolutionCallbackDisabler disabler(*this);
         WidgetHierarchyUpdatesSuspensionScope suspendWidgetHierarchyUpdates;
+        NoEventDispatchAssertion noEventDispatchAssertion;
+
+        m_inStyleRecalc = true;
 
         if (m_pendingStyleRecalcShouldForce)
             type = ResolveStyleType::Rebuild;
@@ -1851,6 +1855,19 @@ void Document::resolveStyle(ResolveStyleType type)
 
         if (m_renderView->needsLayout())
             frameView.layoutContext().scheduleLayout();
+
+        // Usually this is handled by post-layout.
+        if (!frameView.needsLayout())
+            frameView.frame().selection().scheduleAppearanceUpdateAfterStyleChange();
+
+        // As a result of the style recalculation, the currently hovered element might have been
+        // detached (for example, by setting display:none in the :hover style), schedule another mouseMove event
+        // to check if any other elements ended up under the mouse pointer due to re-layout.
+        if (m_hoveredElement && !m_hoveredElement->renderer())
+            frameView.frame().mainFrame().eventHandler().dispatchFakeMouseMoveEventSoon();
+
+        ++m_styleRecalcCount;
+        // FIXME: Assert ASSERT(!needsStyleRecalc()) here. Do we still have some cases where it's not true?
     }
 
     // If we wanted to call implicitClose() during recalcStyle, do so now that we're finished.
@@ -1858,8 +1875,6 @@ void Document::resolveStyle(ResolveStyleType type)
         m_closeAfterStyleRecalc = false;
         implicitClose();
     }
-    
-    ++m_styleRecalcCount;
 
     InspectorInstrumentation::didRecalculateStyle(cookie);
 
@@ -1869,20 +1884,8 @@ void Document::resolveStyle(ResolveStyleType type)
     if (updatedCompositingLayers && !frameView.needsLayout())
         frameView.viewportContentsChanged();
 
-    // Usually this is handled by post-layout.
-    if (!frameView.needsLayout())
-        frameView.frame().selection().scheduleAppearanceUpdateAfterStyleChange();
-
-    // As a result of the style recalculation, the currently hovered element might have been
-    // detached (for example, by setting display:none in the :hover style), schedule another mouseMove event
-    // to check if any other elements ended up under the mouse pointer due to re-layout.
-    if (m_hoveredElement && !m_hoveredElement->renderer())
-        frameView.frame().mainFrame().eventHandler().dispatchFakeMouseMoveEventSoon();
-
     if (m_gotoAnchorNeededAfterStylesheetsLoad && !styleScope().hasPendingSheets())
         frameView.scrollToFragment(m_url);
-
-    // FIXME: Ideally we would ASSERT(!needsStyleRecalc()) here but we have some cases where it is not true.
 }
 
 void Document::updateTextRenderer(Text& text, unsigned offsetOfReplacedText, unsigned lengthOfReplacedText)
@@ -1920,16 +1923,19 @@ bool Document::needsStyleRecalc() const
 
 bool Document::updateStyleIfNeeded()
 {
-    ASSERT(isMainThread());
-    ASSERT(!view() || !view()->isPainting());
+    {
+        NoEventDispatchAssertion noEventDispatchAssertion;
+        ASSERT(isMainThread());
+        ASSERT(!view() || !view()->isPainting());
 
-    if (!view() || view()->layoutContext().isInRenderTreeLayout())
-        return false;
+        if (!view() || view()->layoutContext().isInRenderTreeLayout())
+            return false;
 
-    styleScope().flushPendingUpdate();
+        styleScope().flushPendingUpdate();
 
-    if (!needsStyleRecalc())
-        return false;
+        if (!needsStyleRecalc())
+            return false;
+    }
 
     resolveStyle();
     return true;
