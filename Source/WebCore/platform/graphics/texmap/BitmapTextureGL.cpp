91a867669b627c5208f1d8a75dc16afadc0e2b54
Konstantin Tokarev <annulen@yandex.ru>
2017-11-02 17:24:02 +0000

    Unreviewed, removed useless semicolon at the end of namespace
    * platform/graphics/texmap/BitmapTextureGL.cpp:
    
    
    Canonical link: https://commits.webkit.org/195287@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@224340 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/texmap/BitmapTextureGL.cpp
+++ Source/WebCore/platform/graphics/texmap/BitmapTextureGL.cpp
@@ -367,6 +367,6 @@ void BitmapTextureGL::copyFromExternalTexture(GLuint sourceTextureID)
     glDeleteFramebuffers(1, &copyFbo);
 }
 
-}; // namespace WebCore
+} // namespace WebCore
 
 #endif // USE(TEXTURE_MAPPER_GL)
