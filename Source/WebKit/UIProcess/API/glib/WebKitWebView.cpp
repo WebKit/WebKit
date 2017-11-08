WebKit/WebKit

commit 95719c33afc61c8ee7baf18253354fc2e081ece5

[WPE][GTK] Expose availability of certain editing commands in WebKitEditorState https://bugs.webkit.org/show_bug.cgi?id=168219
Reviewed by Ryosuke Niwa.

Source/WebCore:

Make Editor::canUndo and Editor::canRedo const.

Also, remove unused allowsCopy from HitTestResult.

* editing/Editor.cpp:
(WebCore::Editor::canUndo const):
(WebCore::Editor::canRedo const):
(WebCore::Editor::canUndo): Deleted.
(WebCore::Editor::canRedo): Deleted.
* editing/Editor.h:
* rendering/HitTestResult.cpp:
(WebCore::HitTestResult::allowsCopy const): Deleted.
* rendering/HitTestResult.h:
* rendering/SelectionRangeData.cpp:

Source/WebKit:

Expose canCut, canCopy, canPaste, canUndo, and canRedo in EditorState::PostLayouData.

Add corresponding new WPE/GTK API.

Remove allowsCopy from WebKitTestResultData. It's unused, does not work correctly in all
circumstances, and irritates Ryosuke.

Also, remove a bunch of #ifs guarding post layout data, since it's now used on all ports.

* Shared/EditorState.cpp:
(WebKit::EditorState::encode const):
(WebKit::EditorState::decode):
(WebKit::EditorState::PostLayoutData::encode const):
(WebKit::EditorState::PostLayoutData::decode):
* Shared/EditorState.h:
(WebKit::EditorState::postLayoutData const const):
* Shared/WebHitTestResultData.cpp:
(WebKit::WebHitTestResultData::WebHitTestResultData):
(WebKit::WebHitTestResultData::encode const):
(WebKit::WebHitTestResultData::decode):
* Shared/WebHitTestResultData.h:
* UIProcess/API/APIHitTestResult.h:
(API::HitTestResult::allowsCopy const): Deleted.
* UIProcess/API/glib/WebKitEditorState.cpp:
(webkitEditorStateChanged):
(webkit_editor_state_is_cut_available):
(webkit_editor_state_is_copy_available):
(webkit_editor_state_is_paste_available):
(webkit_editor_state_is_undo_available):
(webkit_editor_state_is_redo_available):
* UIProcess/API/glib/WebKitWebView.cpp:
(webkit_web_view_can_execute_editing_command):
* UIProcess/API/gtk/WebKitEditorState.h:
* UIProcess/API/gtk/docs/webkit2gtk-4.0-sections.txt:

Source/WebKit/UIProcess/API/glib/WebKitWebView.cpp
@@ -35,6 +35,7 @@
 #include "WebKitContextMenuItemPrivate.h"
 #include "WebKitContextMenuPrivate.h"
 #include "WebKitDownloadPrivate.h"
+#include "WebKitEditingCommands.h"
 #include "WebKitEditorStatePrivate.h"
 #include "WebKitEnumTypes.h"
 #include "WebKitError.h"
@@ -3087,9 +3088,23 @@ void webkit_web_view_can_execute_editing_command(WebKitWebView* webView, const c
     g_return_if_fail(command);
 
     GTask* task = g_task_new(webView, cancellable, callback, userData);
-    getPage(webView).validateCommand(String::fromUTF8(command), [task](const String&, bool isEnabled, int32_t, WebKit::CallbackBase::Error) {
-        g_task_return_boolean(adoptGRef(task).get(), isEnabled);        
-    });
+    WebKitEditorState* state = webkit_web_view_get_editor_state(webView);
+
+    if (!strcmp(command, WEBKIT_EDITING_COMMAND_CUT))
+        g_task_return_boolean(adoptGRef(task).get(), webkit_editor_state_is_cut_available(state));
+    else if (!strcmp(command, WEBKIT_EDITING_COMMAND_COPY))
+        g_task_return_boolean(adoptGRef(task).get(), webkit_editor_state_is_copy_available(state));
+    else if (!strcmp(command, WEBKIT_EDITING_COMMAND_PASTE))
+        g_task_return_boolean(adoptGRef(task).get(), webkit_editor_state_is_paste_available(state));
+    else if (!strcmp(command, WEBKIT_EDITING_COMMAND_UNDO))
+        g_task_return_boolean(adoptGRef(task).get(), webkit_editor_state_is_undo_available(state));
+    else if (!strcmp(command, WEBKIT_EDITING_COMMAND_REDO))
+        g_task_return_boolean(adoptGRef(task).get(), webkit_editor_state_is_redo_available(state));
+    else {
+        getPage(webView).validateCommand(String::fromUTF8(command), [task](const String&, bool isEnabled, int32_t, WebKit::CallbackBase::Error) {
+            g_task_return_boolean(adoptGRef(task).get(), isEnabled);        
+        });
+    }
 }
 
 /**

Tools/TestWebKitAPI/Tests/WebKitGtk/TestWebViewEditor.cpp
@@ -37,6 +37,27 @@ public:
         gtk_clipboard_clear(m_clipboard);
     }
 
+    static gboolean webViewDrawCallback(GMainLoop* mainLoop)
+    {
+        g_main_loop_quit(mainLoop);
+        return G_SOURCE_REMOVE;
+    }
+
+    void flushEditorState()
+    {
+        // FIXME: It would be better to call WebViewTest::showInWindowAndWaitUntilMapped
+        // at the start of the test, rather than creating and destroying temporary windows.
+        showInWindow(GTK_WINDOW_TOPLEVEL);
+
+        g_signal_connect_swapped(m_webView, "draw", G_CALLBACK(webViewDrawCallback), m_mainLoop);
+        gtk_widget_queue_draw(GTK_WIDGET(m_webView));
+        g_main_loop_run(m_mainLoop);
+
+        gtk_container_remove(GTK_CONTAINER(m_parentWindow), GTK_WIDGET(m_webView));
+        gtk_widget_destroy(m_parentWindow);
+        m_parentWindow = nullptr;
+    }
+
     static void canExecuteEditingCommandReadyCallback(GObject*, GAsyncResult* result, EditorTest* test)
     {
         GUniqueOutPtr<GError> error;
@@ -50,6 +71,20 @@ public:
         m_canExecuteEditingCommand = false;
         webkit_web_view_can_execute_editing_command(m_webView, command, 0, reinterpret_cast<GAsyncReadyCallback>(canExecuteEditingCommandReadyCallback), this);
         g_main_loop_run(m_mainLoop);
+
+        if (!strcmp(command, WEBKIT_EDITING_COMMAND_CUT))
+            g_assert(m_canExecuteEditingCommand == webkit_editor_state_is_cut_available(editorState()));
+        else if (!strcmp(command, WEBKIT_EDITING_COMMAND_COPY))
+            g_assert(m_canExecuteEditingCommand == webkit_editor_state_is_copy_available(editorState()));
+        else if (!strcmp(command, WEBKIT_EDITING_COMMAND_PASTE))
+            g_assert(m_canExecuteEditingCommand == webkit_editor_state_is_paste_available(editorState()));
+        // FIXME: Figure out how to add tests for undo and redo. It will probably require using user
+        // scripts to message the UI process when the content has been altered.
+        else if (!strcmp(command, WEBKIT_EDITING_COMMAND_UNDO))
+            g_assert(m_canExecuteEditingCommand == webkit_editor_state_is_undo_available(editorState()));
+        else if (!strcmp(command, WEBKIT_EDITING_COMMAND_REDO))
+            g_assert(m_canExecuteEditingCommand == webkit_editor_state_is_redo_available(editorState()));
+
         return m_canExecuteEditingCommand;
     }
 
@@ -140,6 +175,7 @@ static void testWebViewEditorCutCopyPasteNonEditable(EditorTest* test, gconstpoi
     GUniquePtr<char> selectedSpanHTML(g_strdup_printf(selectedSpanHTMLFormat, "false"));
     test->loadHtml(selectedSpanHTML.get(), nullptr);
     test->waitUntilLoadFinished();
+    test->flushEditorState();
 
     g_assert(test->canExecuteEditingCommand(WEBKIT_EDITING_COMMAND_COPY));
     // It's not possible to cut and paste when content is not editable
@@ -166,6 +202,7 @@ static void testWebViewEditorCutCopyPasteEditable(EditorTest* test, gconstpointe
     GUniquePtr<char> selectedSpanHTML(g_strdup_printf(selectedSpanHTMLFormat, "false"));
     test->loadHtml(selectedSpanHTML.get(), nullptr);
     test->waitUntilLoadFinished();
+    test->flushEditorState();
 
     // There's a selection.
     g_assert(test->canExecuteEditingCommand(WEBKIT_EDITING_COMMAND_CUT));
@@ -184,6 +221,7 @@ static void testWebViewEditorSelectAllNonEditable(EditorTest* test, gconstpointe
     GUniquePtr<char> selectedSpanHTML(g_strdup_printf(selectedSpanHTMLFormat, "false"));
     test->loadHtml(selectedSpanHTML.get(), nullptr);
     test->waitUntilLoadFinished();
+    test->flushEditorState();
 
     g_assert(test->canExecuteEditingCommand(WEBKIT_EDITING_COMMAND_SELECT_ALL));
 
@@ -212,6 +250,7 @@ static void testWebViewEditorSelectAllEditable(EditorTest* test, gconstpointer)
     GUniquePtr<char> selectedSpanHTML(g_strdup_printf(selectedSpanHTMLFormat, "false"));
     test->loadHtml(selectedSpanHTML.get(), nullptr);
     test->waitUntilLoadFinished();
+    test->flushEditorState();
 
     g_assert(test->canExecuteEditingCommand(WEBKIT_EDITING_COMMAND_SELECT_ALL));
 
@@ -237,10 +276,13 @@ static void loadContentsAndTryToCutSelection(EditorTest* test, bool contentEdita
     GUniquePtr<char> selectedSpanHTML(g_strdup_printf(selectedSpanHTMLFormat, contentEditable ? "true" : "false"));
     test->loadHtml(selectedSpanHTML.get(), nullptr);
     test->waitUntilLoadFinished();
+    test->flushEditorState();
 
     g_assert(!test->isEditable());
     test->setEditable(true);
     g_assert(test->isEditable());
+
+    test->flushEditorState();
 
     // Cut the selection to the clipboard to see if the view is indeed editable.
     GUniquePtr<char> clipboardText(test->cutSelection());
@@ -256,6 +298,7 @@ static void testWebViewEditorNonEditable(EditorTest* test)
     GUniquePtr<char> selectedSpanHTML(g_strdup_printf(selectedSpanHTMLFormat, "false"));
     test->loadHtml(selectedSpanHTML.get(), nullptr);
     test->waitUntilLoadFinished();
+    test->flushEditorState();
 
     g_assert(!test->isEditable());
     test->setEditable(true);
@@ -293,9 +336,9 @@ static void testWebViewEditorEditorStateTypingAttributes(EditorTest* test, gcons
         "<b><i>boldanditalic </i></b>"
         "</body></html>";
 
-    test->showInWindowAndWaitUntilMapped();
     test->loadHtml(typingAttributesHTML, nullptr);
     test->waitUntilLoadFinished();
+    test->flushEditorState();
     test->setEditable(true);
 
     unsigned typingAttributes = test->typingAttributes();
@@ -406,6 +449,7 @@ static void testWebViewEditorInsertImage(EditorTest* test, gconstpointer)
 {
     test->loadHtml("<html><body></body></html>", "file:///");
     test->waitUntilLoadFinished();
+    test->flushEditorState();
     test->setEditable(true);
 
     GUniquePtr<char> imagePath(g_build_filename(Test::getResourcesDir().data(), "blank.ico", nullptr));
@@ -423,6 +467,7 @@ static void testWebViewEditorCreateLink(EditorTest* test, gconstpointer)
 {
     test->loadHtml("<html><body onload=\"document.getSelection().selectAllChildren(document.body);\">webkitgtk.org</body></html>", nullptr);
     test->waitUntilLoadFinished();
+    test->flushEditorState();
     test->setEditable(true);
 
     static const char* webkitGTKURL = "http://www.webkitgtk.org/";

Source/WebCore/editing/Editor.cpp
@@ -1639,7 +1639,7 @@ void Editor::clearUndoRedoOperations()
         client()->clearUndoRedoOperations();
 }
 
-bool Editor::canUndo()
+bool Editor::canUndo() const
 {
     return client() && client()->canUndo();
 }
@@ -1650,7 +1650,7 @@ void Editor::undo()
         client()->undo();
 }
 
-bool Editor::canRedo()
+bool Editor::canRedo() const
 {
     return client() && client()->canRedo();
 }

Source/WebCore/editing/Editor.h
@@ -303,9 +303,9 @@ public:
     bool shouldEndEditing(Range*);
 
     void clearUndoRedoOperations();
-    bool canUndo();
+    WEBCORE_EXPORT bool canUndo() const;
     void undo();
-    bool canRedo();
+    WEBCORE_EXPORT bool canRedo() const;
     void redo();
 
     void didBeginEditing();

Source/WebCore/rendering/HitTestResult.cpp
@@ -594,21 +594,6 @@ bool HitTestResult::isOverTextInsideFormControlElement() const
     return !wordRange->text().isEmpty();
 }
 
-bool HitTestResult::allowsCopy() const
-{
-    Node* node = innerNode();
-    if (!node)
-        return false;
-
-    RenderObject* renderer = node->renderer();
-    if (!renderer)
-        return false;
-
-    bool isUserSelectNone = renderer->style().userSelect() == SELECT_NONE;
-    bool isPasswordField = is<HTMLInputElement>(node) && downcast<HTMLInputElement>(*node).isPasswordField();
-    return !isPasswordField && !isUserSelectNone;
-}
-
 URL HitTestResult::absoluteLinkURL() const
 {
     if (m_innerURLElement)

Source/WebCore/rendering/HitTestResult.h
@@ -131,7 +131,6 @@ public:
 
     WEBCORE_EXPORT bool isDownloadableMedia() const;
     WEBCORE_EXPORT bool isOverTextInsideFormControlElement() const;
-    WEBCORE_EXPORT bool allowsCopy() const;
 
     HitTestProgress addNodeToListBasedTestResult(Node*, const HitTestRequest&, const HitTestLocation& pointInContainer, const LayoutRect& = LayoutRect());
     HitTestProgress addNodeToListBasedTestResult(Node*, const HitTestRequest&, const HitTestLocation& pointInContainer, const FloatRect&);

Source/WebKit/Shared/EditorState.cpp
@@ -42,10 +42,8 @@ void EditorState::encode(IPC::Encoder& encoder) const
     encoder << hasComposition;
     encoder << isMissingPostLayoutData;
 
-#if PLATFORM(IOS) || PLATFORM(GTK) || PLATFORM(MAC)
     if (!isMissingPostLayoutData)
         m_postLayoutData.encode(encoder);
-#endif
 
 #if PLATFORM(IOS)
     encoder << firstMarkedRect;
@@ -83,12 +81,10 @@ bool EditorState::decode(IPC::Decoder& decoder, EditorState& result)
     if (!decoder.decode(result.isMissingPostLayoutData))
         return false;
 
-#if PLATFORM(IOS) || PLATFORM(GTK) || PLATFORM(MAC)
     if (!result.isMissingPostLayoutData) {
         if (!PostLayoutData::decode(decoder, result.postLayoutData()))
             return false;
     }
-#endif
 
 #if PLATFORM(IOS)
     if (!decoder.decode(result.firstMarkedRect))
@@ -102,7 +98,6 @@ bool EditorState::decode(IPC::Decoder& decoder, EditorState& result)
     return true;
 }
 
-#if PLATFORM(IOS) || PLATFORM(GTK) || PLATFORM(MAC)
 void EditorState::PostLayoutData::encode(IPC::Encoder& encoder) const
 {
     encoder << typingAttributes;
@@ -135,6 +130,11 @@ void EditorState::PostLayoutData::encode(IPC::Encoder& encoder) const
     encoder << paragraphContextForCandidateRequest;
     encoder << stringForCandidateRequest;
 #endif
+    encoder << canCut;
+    encoder << canCopy;
+    encoder << canPaste;
+    encoder << canUndo;
+    encoder << canRedo;
 }
 
 bool EditorState::PostLayoutData::decode(IPC::Decoder& decoder, PostLayoutData& result)
@@ -194,8 +194,18 @@ bool EditorState::PostLayoutData::decode(IPC::Decoder& decoder, PostLayoutData& 
         return false;
 #endif
 
+    if (!decoder.decode(result.canCut))
+        return false;
+    if (!decoder.decode(result.canCopy))
+        return false;
+    if (!decoder.decode(result.canPaste))
+        return false;
+    if (!decoder.decode(result.canUndo))
+        return false;
+    if (!decoder.decode(result.canRedo))
+        return false;
+
     return true;
 }
-#endif // PLATFORM(IOS) || PLATFORM(GTK) || PLATFORM(MAC)
 
 }

Source/WebKit/Shared/EditorState.h
@@ -77,7 +77,6 @@ struct EditorState {
     String markedText;
 #endif
 
-#if PLATFORM(IOS) || PLATFORM(GTK) || PLATFORM(MAC)
     struct PostLayoutData {
         uint32_t typingAttributes { AttributeNone };
 #if PLATFORM(IOS) || PLATFORM(GTK)
@@ -110,24 +109,26 @@ struct EditorState {
         String stringForCandidateRequest;
 #endif
 
+        bool canCut { false };
+        bool canCopy { false };
+        bool canPaste { false };
+        bool canUndo { false };
+        bool canRedo { false };
+
         void encode(IPC::Encoder&) const;
         static bool decode(IPC::Decoder&, PostLayoutData&);
     };
 
     const PostLayoutData& postLayoutData() const;
     PostLayoutData& postLayoutData();
-#endif // PLATFORM(IOS) || PLATFORM(GTK) || PLATFORM(MAC)
 
     void encode(IPC::Encoder&) const;
     static bool decode(IPC::Decoder&, EditorState&);
 
-#if PLATFORM(IOS) || PLATFORM(GTK) || PLATFORM(MAC)
 private:
     PostLayoutData m_postLayoutData;
-#endif
 };
 
-#if PLATFORM(IOS) || PLATFORM(GTK) || PLATFORM(MAC)
 inline auto EditorState::postLayoutData() -> PostLayoutData&
 {
     ASSERT_WITH_MESSAGE(!isMissingPostLayoutData, "Attempt to access post layout data before receiving it");
@@ -139,7 +140,6 @@ inline auto EditorState::postLayoutData() const -> const PostLayoutData&
     ASSERT_WITH_MESSAGE(!isMissingPostLayoutData, "Attempt to access post layout data before receiving it");
     return m_postLayoutData;
 }
-#endif
 
 }
 

commit 48ca5ca253e7be5b6250b0a4824b4b022e7a5996

REGRESSION(r224179): [GTK] Several WebViewEditor tests are failing since r224179 https://bugs.webkit.org/show_bug.cgi?id=179366
Reviewed by Michael Catanzaro.

Source/WebKit:

In r224179, webkit_web_view_can_execute_editing_command() was optimized to use the sync path for commands
supported by the WebViewEditorState, but the state requires a redraw to be up to date. We can't know if
WebViewEditorState is in sync, when webkit_web_view_can_execute_editing_command() is called, so we always need
to ask the web process.

* UIProcess/API/glib/WebKitWebView.cpp:
(webkit_web_view_can_execute_editing_command):

Tools:

Use always a toplevel window for WebViewEditor tests instead of creating and destroying one only to flush editor
state.

* TestWebKitAPI/Tests/WebKitGtk/TestWebViewEditor.cpp:

Canonical link: https://commits.webkit.org/195473@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@224567 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/API/glib/WebKitWebView.cpp
@@ -3087,24 +3087,10 @@ void webkit_web_view_can_execute_editing_command(WebKitWebView* webView, const c
     g_return_if_fail(WEBKIT_IS_WEB_VIEW(webView));
     g_return_if_fail(command);
 
-    GTask* task = g_task_new(webView, cancellable, callback, userData);
-    WebKitEditorState* state = webkit_web_view_get_editor_state(webView);
-
-    if (!strcmp(command, WEBKIT_EDITING_COMMAND_CUT))
-        g_task_return_boolean(adoptGRef(task).get(), webkit_editor_state_is_cut_available(state));
-    else if (!strcmp(command, WEBKIT_EDITING_COMMAND_COPY))
-        g_task_return_boolean(adoptGRef(task).get(), webkit_editor_state_is_copy_available(state));
-    else if (!strcmp(command, WEBKIT_EDITING_COMMAND_PASTE))
-        g_task_return_boolean(adoptGRef(task).get(), webkit_editor_state_is_paste_available(state));
-    else if (!strcmp(command, WEBKIT_EDITING_COMMAND_UNDO))
-        g_task_return_boolean(adoptGRef(task).get(), webkit_editor_state_is_undo_available(state));
-    else if (!strcmp(command, WEBKIT_EDITING_COMMAND_REDO))
-        g_task_return_boolean(adoptGRef(task).get(), webkit_editor_state_is_redo_available(state));
-    else {
-        getPage(webView).validateCommand(String::fromUTF8(command), [task](const String&, bool isEnabled, int32_t, WebKit::CallbackBase::Error) {
-            g_task_return_boolean(adoptGRef(task).get(), isEnabled);        
-        });
-    }
+    GRefPtr<GTask> task = adoptGRef(g_task_new(webView, cancellable, callback, userData));
+    getPage(webView).validateCommand(String::fromUTF8(command), [task = WTFMove(task)](const String&, bool isEnabled, int32_t, WebKit::CallbackBase::Error) {
+        g_task_return_boolean(task.get(), isEnabled);
+    });
 }
 
 /**

Tools/TestWebKitAPI/Tests/WebKitGtk/TestWebViewEditor.cpp
@@ -34,28 +34,22 @@ public:
         , m_triesCount(0)
         , m_editorState(nullptr)
     {
+        showInWindowAndWaitUntilMapped(GTK_WINDOW_TOPLEVEL);
         gtk_clipboard_clear(m_clipboard);
     }
 
     static gboolean webViewDrawCallback(GMainLoop* mainLoop)
     {
         g_main_loop_quit(mainLoop);
-        return G_SOURCE_REMOVE;
+        return FALSE;
     }
 
     void flushEditorState()
     {
-        // FIXME: It would be better to call WebViewTest::showInWindowAndWaitUntilMapped
-        // at the start of the test, rather than creating and destroying temporary windows.
-        showInWindow(GTK_WINDOW_TOPLEVEL);
-
-        g_signal_connect_swapped(m_webView, "draw", G_CALLBACK(webViewDrawCallback), m_mainLoop);
+        auto signalID = g_signal_connect_swapped(m_webView, "draw", G_CALLBACK(webViewDrawCallback), m_mainLoop);
         gtk_widget_queue_draw(GTK_WIDGET(m_webView));
         g_main_loop_run(m_mainLoop);
-
-        gtk_container_remove(GTK_CONTAINER(m_parentWindow), GTK_WIDGET(m_webView));
-        gtk_widget_destroy(m_parentWindow);
-        m_parentWindow = nullptr;
+        g_signal_handler_disconnect(m_webView, signalID);
     }
 
     static void canExecuteEditingCommandReadyCallback(GObject*, GAsyncResult* result, EditorTest* test)