3f007d980dd7518b3b07e2ac4685a4bf9329ca86
Per Arne Vollan <pvollan@apple.com>
2017-11-16 15:52:28 +0000

    Win EWS bots should reboot after N webkit-patch iterations. https://bugs.webkit.org/show_bug.cgi?id=177970
    Reviewed by Brent Fulgham.
    
    * EWSTools/start-queue-win.sh:
    
    
    Canonical link: https://commits.webkit.org/195797@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@224920 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/EWSTools/start-queue-win.sh
+++ Tools/EWSTools/start-queue-win.sh
@@ -21,44 +21,41 @@ function start_ews()
 
     "$PROGRAMFILES_X86/Microsoft Visual Studio 14.0/VC/vcvarsall.bat"
 
-    while :
-    do
-        date
-        echo "TASK: cleaning up old log files."
-        cd ~/win-ews-logs
-        rm -rf win-ews.old
-        mv win-ews.log win-ews.old
-        find ~/win-ews-logs -mtime +7 -exec rm -f {} \;
-        echo "TASK: Starting up"
-        cd ~/WebKit
-        export VSINSTALLDIR="$PROGRAMFILES_X86\Microsoft Visual Studio 14.0"
-        echo "TASK: Cleaning WebKitBuild"
-        rm -rf WebKitBuild
-        date
-        echo "TASK: Cleaning up"
-        svn cleanup
-        date
-        echo "TASK: svn revert -R *"
-        svn revert -R *
-        date
-        echo "TASK: svn status"
-        svn status | grep "?" | awk '{print $2}' | xargs rm -rf
-        date
-        echo "TASK: webkit-patch clean"
-        ~/WebKit/Tools/Scripts/webkit-patch clean
-        date
-        echo "TASK: svn up --non-interactive"
-        svn up --non-interactive
-        date
-        echo "TASK: kill orphaned tasks"
-        taskkill.exe /f /im cl.exe
-        echo "TASK: test-webkitpy"
-        ~/WebKit/Tools/Scripts/test-webkitpy
-        echo "TASK: webkit-patch win-ews"
-        ~/WebKit/Tools/Scripts/webkit-patch win-ews --bot-id=$BOT_ID --no-confirm --exit-after-iteration 10
-        echo "TASK: kill old processes"
-        ~/WebKit/Tools/BuildSlaveSupport/kill-old-processes
-    done
+    date
+    echo "TASK: cleaning up old log files."
+    cd ~/win-ews-logs
+    rm -rf win-ews.old
+    mv win-ews.log win-ews.old
+    find ~/win-ews-logs -mtime +7 -exec rm -f {} \;
+    echo "TASK: Starting up"
+    cd ~/WebKit
+    export VSINSTALLDIR="$PROGRAMFILES_X86\Microsoft Visual Studio 14.0"
+    echo "TASK: Cleaning WebKitBuild"
+    rm -rf WebKitBuild
+    date
+    echo "TASK: Cleaning up"
+    svn cleanup
+    date
+    echo "TASK: svn revert -R *"
+    svn revert -R *
+    date
+    echo "TASK: svn status"
+    svn status | grep "?" | awk '{print $2}' | xargs rm -rf
+    date
+    echo "TASK: webkit-patch clean"
+    ~/WebKit/Tools/Scripts/webkit-patch clean
+    date
+    echo "TASK: svn up --non-interactive"
+    svn up --non-interactive
+    date
+    echo "TASK: kill orphaned tasks"
+    taskkill.exe /f /im cl.exe
+    echo "TASK: test-webkitpy"
+    ~/WebKit/Tools/Scripts/test-webkitpy
+    echo "TASK: webkit-patch win-ews"
+    ~/WebKit/Tools/Scripts/webkit-patch win-ews --bot-id=$BOT_ID --no-confirm --exit-after-iteration 10
+    echo "TASK: reboot"
+    shutdown /r
 }
 
 start_ews
