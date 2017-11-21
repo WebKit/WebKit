b57c7f55c224a7ce77371aa27be8fda4a206e204
Don Olmstead <don.olmstead@sony.com>
2017-11-21 02:49:35 +0000

    Add declspec within WebKit API https://bugs.webkit.org/show_bug.cgi?id=179893
    Reviewed by Darin Adler.
    
    * Shared/API/c/WKDeclarationSpecifiers.h:
    
    
    Canonical link: https://commits.webkit.org/195933@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@225064 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/API/c/WKDeclarationSpecifiers.h
+++ Source/WebKit/Shared/API/c/WKDeclarationSpecifiers.h
@@ -27,10 +27,20 @@
 #ifndef WKDeclarationSpecifiers_h
 #define WKDeclarationSpecifiers_h
 
+#ifndef __has_declspec_attribute
+#define __has_declspec_attribute(x) 0
+#endif
+
 #undef WK_EXPORT
 #if defined(WK_NO_EXPORT)
 #define WK_EXPORT
-#elif defined(__GNUC__) && !defined(__CC_ARM) && !defined(__ARMCC__)
+#elif defined(WIN32) || defined(_WIN32) || defined(__CC_ARM) || defined(__ARMCC__) || (__has_declspec_attribute(dllimport) && __has_declspec_attribute(dllexport))
+#if BUILDING_WebKit
+#define WK_EXPORT __declspec(dllexport)
+#else
+#define WK_EXPORT __declspec(dllimport)
+#endif /* BUILDING_WebKit */
+#elif defined(__GNUC__)
 #define WK_EXPORT __attribute__((visibility("default")))
 #else /* !defined(WK_NO_EXPORT) */
 #define WK_EXPORT
