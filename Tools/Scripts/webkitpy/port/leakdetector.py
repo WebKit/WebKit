48d86a7ed6d2060ab3fde07b200dfd5c24502682
Alexey Proskuryakov <ap@apple.com>
2017-11-28 22:39:04 +0000

    Stop silencing leaks in TextCodecICU::registerCodecs, as the problem was fixed a while ago. https://bugs.webkit.org/show_bug.cgi?id=118505
    Reviewed by Joseph Pecoraro.
    
    * Scripts/valgrind/suppressions.txt:
    * Scripts/webkitpy/port/leakdetector.py:
    (LeakDetector._callstacks_to_exclude_from_leaks):
    
    
    Canonical link: https://commits.webkit.org/196101@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@225240 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/port/leakdetector.py
+++ Tools/Scripts/webkitpy/port/leakdetector.py
@@ -54,12 +54,9 @@ class LeakDetector(object):
 
     def _callstacks_to_exclude_from_leaks(self):
         callstacks = [
-            'TextCodecICU::registerCodecs',  # https://bugs.webkit.org/show_bug.cgi?id=118505
+            # List substrings here, e.g.
+            # 'TextCodecICU::registerCodecs',  # https://bugs.webkit.org/show_bug.cgi?id=118505
         ]
-        if self._port.operating_system == 'mac' and self._port.is_mavericks():
-            callstacks += [
-                'AVAssetResourceLoader _poseAuthenticationChallengeWithKey:data:requestDictionary:fallbackHandler:',  # <rdar://problem/19699887> leak in AVFoundation
-            ]
         return callstacks
 
     def _leaks_args(self, pid):
