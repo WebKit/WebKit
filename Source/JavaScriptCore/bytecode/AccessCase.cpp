a1bf6a601b4b0d0de0fd7155de2b0bd2f5be32c9
Mark Lam <mark.lam@apple.com>
2017-11-29 21:53:28 +0000

    Cherry-pick r224539. rdar://problem/35698788
    Canonical link: https://commits.webkit.org/191388.286@safari-604-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-604-branch@225295 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/bytecode/AccessCase.cpp
+++ Source/JavaScriptCore/bytecode/AccessCase.cpp
@@ -937,7 +937,9 @@ void AccessCase::generateImpl(AccessGenerationState& state)
                 state.emitExplicitExceptionHandler();
                 
                 noException.link(&jit);
-                state.restoreLiveRegistersFromStackForCall(spillState);
+                RegisterSet resultRegisterToExclude;
+                resultRegisterToExclude.set(scratchGPR);
+                state.restoreLiveRegistersFromStackForCall(spillState, resultRegisterToExclude);
             }
         }
         
