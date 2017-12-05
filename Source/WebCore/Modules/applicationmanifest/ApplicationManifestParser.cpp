ca1e8ed90d104c96f3949d250d442b317ea279b6
David Quesada <david_quesada@apple.com>
2017-12-05 00:57:33 +0000

    Add a class for parsing application manifests https://bugs.webkit.org/show_bug.cgi?id=177973 rdar://problem/34747949
    Patch by David Quesada <david_quesada@apple.com> on 2017-12-04
    Reviewed by Geoffrey Garen.
    
    Source/JavaScriptCore:
    
    * Configurations/FeatureDefines.xcconfig: Add ENABLE_APPLICATION_MANIFEST feature flag.
    
    Source/WebCore:
    
    Add a new struct ApplicationManifest, along with ApplicationManifestParser to initialize
    ApplicationManifests from JSON data, according to the App Manifest spec. Just a basic subset
    of the manifests's properties are implemented to start with.
    
    ApplicationManifestParser is tested by new unit tests in TestWebKitAPI.
    
    * Configurations/FeatureDefines.xcconfig: Add ENABLE_APPLICATION_MANIFEST feature flag.
    * Modules/applicationmanifest/ApplicationManifest.h: Added.
    * Modules/applicationmanifest/ApplicationManifestParser.cpp: Added.
    (WebCore::ApplicationManifestParser::parse):
    The main entry point for eventual clients (and currently the unit tests) to invoke
    ApplicationManifestParser. There is a variant that accepts a ScriptExecutionContext that
    uses the context exclusively for logging console warnings while parsing the manifest, and
    not for actually executing any scripts.
    (WebCore::ApplicationManifestParser::ApplicationManifestParser):
    (WebCore::ApplicationManifestParser::parseManifest):
    (WebCore::ApplicationManifestParser::logManifestPropertyNotAString):
    (WebCore::ApplicationManifestParser::logManifestPropertyInvalidURL):
    (WebCore::ApplicationManifestParser::logDeveloperWarning):
    (WebCore::ApplicationManifestParser::parseStartURL):
    (WebCore::ApplicationManifestParser::parseName):
    (WebCore::ApplicationManifestParser::parseDescription):
    (WebCore::ApplicationManifestParser::parseShortName):
    (WebCore::isInScope):
    (WebCore::ApplicationManifestParser::parseScope):
    (WebCore::ApplicationManifestParser::parseGenericString):
    * Modules/applicationmanifest/ApplicationManifestParser.h: Added.
    * Sources.txt:
    * WebCore.xcodeproj/project.pbxproj:
    * features.json: Change the Web App Manifest feature status to "In Development"
    
    Source/WebCore/PAL:
    
    * Configurations/FeatureDefines.xcconfig: Add ENABLE_APPLICATION_MANIFEST feature flag.
    
    Source/WebKit:
    
    * Configurations/FeatureDefines.xcconfig: Add ENABLE_APPLICATION_MANIFEST feature flag.
    
    Source/WebKitLegacy/mac:
    
    * Configurations/FeatureDefines.xcconfig: Add ENABLE_APPLICATION_MANIFEST feature flag.
    
    Tools:
    
    Add basic unit tests for ApplicationManifestParser. For each of the implemented top-level properties,
    attempt to parse manifests with varying types of valid and invalid data to ensure the resulting
    ApplicationManifest is configured with the appropriate values per the spec.
    
    * TestWebKitAPI/Configurations/FeatureDefines.xcconfig: Add ENABLE_APPLICATION_MANIFEST feature flag.
    * TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
    * TestWebKitAPI/Tests/WebCore/ApplicationManifestParser.cpp: Added.
    (ApplicationManifestParserTest::SetUp):
    (ApplicationManifestParserTest::parseString):
    (ApplicationManifestParserTest::parseTopLevelProperty):
    (ApplicationManifestParserTest::testStartURL):
    (ApplicationManifestParserTest::testName):
    (ApplicationManifestParserTest::testDescription):
    (ApplicationManifestParserTest::testShortName):
    (ApplicationManifestParserTest::testScope):
    (assertManifestHasDefaultValues):
    (TEST_F):
    
    Canonical link: https://commits.webkit.org/196348@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@225507 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebCore/Modules/applicationmanifest/ApplicationManifestParser.cpp
@@ -0,0 +1,219 @@
+/*
+ * Copyright (C) 2017 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "ApplicationManifestParser.h"
+
+#if ENABLE(APPLICATION_MANIFEST)
+
+#include "SecurityOrigin.h"
+#include <JavaScriptCore/ConsoleMessage.h>
+
+namespace WebCore {
+
+ApplicationManifest ApplicationManifestParser::parse(ScriptExecutionContext& scriptExecutionContext, const String& source, const URL& manifestURL, const URL& documentURL)
+{
+    ApplicationManifestParser parser { &scriptExecutionContext };
+    return parser.parseManifest(source, manifestURL, documentURL);
+}
+
+ApplicationManifest ApplicationManifestParser::parse(const String& source, const URL& manifestURL, const URL& documentURL)
+{
+    ApplicationManifestParser parser { nullptr };
+    return parser.parseManifest(source, manifestURL, documentURL);
+}
+
+ApplicationManifestParser::ApplicationManifestParser(RefPtr<ScriptExecutionContext> consoleContext)
+    : m_consoleContext(consoleContext)
+{
+}
+
+ApplicationManifest ApplicationManifestParser::parseManifest(const String& text, const URL& manifestURL, const URL& documentURL)
+{
+    m_manifestURL = manifestURL;
+
+    RefPtr<JSON::Value> jsonValue;
+    if (!JSON::Value::parseJSON(text, jsonValue)) {
+        logDeveloperWarning(ASCIILiteral("The manifest is not valid JSON data."));
+        jsonValue = JSON::Object::create();
+    }
+
+    RefPtr<JSON::Object> manifest;
+    if (!jsonValue->asObject(manifest)) {
+        logDeveloperWarning(ASCIILiteral("The manifest is not a JSON value of type \"object\"."));
+        manifest = JSON::Object::create();
+    }
+
+    ApplicationManifest parsedManifest;
+
+    parsedManifest.startURL = parseStartURL(*manifest, documentURL);
+    parsedManifest.name = parseName(*manifest);
+    parsedManifest.description = parseDescription(*manifest);
+    parsedManifest.shortName = parseShortName(*manifest);
+    parsedManifest.scope = parseScope(*manifest, documentURL, parsedManifest.startURL);
+
+    return parsedManifest;
+}
+
+void ApplicationManifestParser::logManifestPropertyNotAString(const String& propertyName)
+{
+    logDeveloperWarning("The \"" + propertyName + "\" application manifest property is not a string.");
+}
+
+void ApplicationManifestParser::logManifestPropertyInvalidURL(const String& propertyName)
+{
+    logDeveloperWarning("The \"" + propertyName + "\" application manifest property is not a valid URL.");
+}
+
+void ApplicationManifestParser::logDeveloperWarning(const String& message)
+{
+    if (m_consoleContext)
+        m_consoleContext->addConsoleMessage(std::make_unique<Inspector::ConsoleMessage>(JSC::MessageSource::Other, JSC::MessageType::Log, JSC::MessageLevel::Warning, ASCIILiteral("Parsing application manifest ") + m_manifestURL.string() + ASCIILiteral(": ") + message));
+}
+
+URL ApplicationManifestParser::parseStartURL(const JSON::Object& manifest, const URL& documentURL)
+{
+    RefPtr<JSON::Value> value;
+    if (!manifest.getValue("start_url", value))
+        return documentURL;
+
+    String stringValue;
+    if (!value->asString(stringValue)) {
+        logManifestPropertyNotAString(ASCIILiteral("start_url"));
+        return documentURL;
+    }
+
+    if (stringValue.isEmpty())
+        return documentURL;
+
+    URL startURL(m_manifestURL, stringValue);
+    if (!startURL.isValid()) {
+        logManifestPropertyInvalidURL(ASCIILiteral("start_url"));
+        return documentURL;
+    }
+
+    if (!protocolHostAndPortAreEqual(startURL, documentURL)) {
+        auto startURLOrigin = SecurityOrigin::create(startURL);
+        auto documentOrigin = SecurityOrigin::create(documentURL);
+        logDeveloperWarning(ASCIILiteral("The start_url's origin of \"") + startURLOrigin->toString() + ASCIILiteral("\" is different from the document's origin of \"") + documentOrigin->toString() + ASCIILiteral("\"."));
+        return documentURL;
+    }
+
+    return startURL;
+}
+
+String ApplicationManifestParser::parseName(const JSON::Object& manifest)
+{
+    return parseGenericString(manifest, ASCIILiteral("name"));
+}
+
+String ApplicationManifestParser::parseDescription(const JSON::Object& manifest)
+{
+    return parseGenericString(manifest, ASCIILiteral("description"));
+}
+
+String ApplicationManifestParser::parseShortName(const JSON::Object& manifest)
+{
+    return parseGenericString(manifest, ASCIILiteral("short_name"));
+}
+
+static bool isInScope(const URL& scopeURL, const URL& targetURL)
+{
+    // 1. If scopeURL is undefined (i.e., it is unbounded because of an error or it was not declared in the manifest), return true.
+    if (scopeURL.isNull() || scopeURL.isEmpty())
+        return true;
+
+    // 2. Let target be a new URL using targetURL as input. If target is failure, return false.
+    if (!targetURL.isValid())
+        return false;
+
+    // 3. Let scopePath be the elements of scopes's path, separated by U+002F (/).
+    auto scopePath = scopeURL.path();
+
+    // 4. Let targetPath be the elements of target's path, separated by U+002F (/).
+    auto targetPath = targetURL.path();
+
+    // 5. If target is same origin as scope and targetPath starts with scopePath, return true.
+    if (protocolHostAndPortAreEqual(scopeURL, targetURL) && targetPath.startsWith(scopePath))
+        return true;
+
+    // 6. Otherwise, return false.
+    return false;
+}
+
+URL ApplicationManifestParser::parseScope(const JSON::Object& manifest, const URL& documentURL, const URL& startURL)
+{
+    RefPtr<JSON::Value> value;
+    if (!manifest.getValue("scope", value))
+        return { };
+
+    String stringValue;
+    if (!value->asString(stringValue)) {
+        logManifestPropertyNotAString(ASCIILiteral("scope"));
+        return { };
+    }
+
+    if (stringValue.isEmpty())
+        return { };
+
+    URL scopeURL(m_manifestURL, stringValue);
+    if (!scopeURL.isValid()) {
+        logManifestPropertyInvalidURL(ASCIILiteral("scope"));
+        return { };
+    }
+
+    if (!protocolHostAndPortAreEqual(scopeURL, documentURL)) {
+        auto scopeURLOrigin = SecurityOrigin::create(scopeURL);
+        auto documentOrigin = SecurityOrigin::create(documentURL);
+        logDeveloperWarning(ASCIILiteral("The scope's origin of \"") + scopeURLOrigin->toString() + ASCIILiteral("\" is different from the document's origin of \"") + documentOrigin->toString() + ASCIILiteral("\"."));
+        return { };
+    }
+
+    if (!isInScope(scopeURL, startURL)) {
+        logDeveloperWarning(ASCIILiteral("The start URL is not within scope of the provided scope URL."));
+        return { };
+    }
+
+    return scopeURL;
+}
+
+String ApplicationManifestParser::parseGenericString(const JSON::Object& manifest, const String& propertyName)
+{
+    RefPtr<JSON::Value> value;
+    if (!manifest.getValue(propertyName, value))
+        return { };
+
+    String stringValue;
+    if (!value->asString(stringValue)) {
+        logManifestPropertyNotAString(propertyName);
+        return { };
+    }
+
+    return stringValue.stripWhiteSpace();
+}
+
+} // namespace WebCore
+
+#endif // ENABLE(APPLICATION_MANIFEST)
