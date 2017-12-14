0ab4538b5344e34d3e87ef05b3f887f371d2859c
Alicia Boya Garcia <aboya@igalia.com>
2017-12-14 23:13:41 +0000

    [MSE] Add isValid() check before using trackBuffer.lastEnqueuedPresentationTime https://bugs.webkit.org/show_bug.cgi?id=180258
    Patch by Alicia Boya García <aboya@igalia.com> on 2017-12-14
    Reviewed by Jer Noble.
    
    * Modules/mediasource/SourceBuffer.cpp:
    (WebCore::SourceBuffer::sourceBufferPrivateDidReceiveSample):
    
    Canonical link: https://commits.webkit.org/196734@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@225936 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/mediasource/SourceBuffer.cpp
+++ Source/WebCore/Modules/mediasource/SourceBuffer.cpp
@@ -1566,7 +1566,7 @@ void SourceBuffer::sourceBufferPrivateDidReceiveSample(MediaSample& sample)
             // Only force the TrackBuffer to re-enqueue if the removed ranges overlap with enqueued and possibly
             // not yet displayed samples.
             MediaTime currentMediaTime = m_source->currentTime();
-            if (currentMediaTime < trackBuffer.lastEnqueuedPresentationTime) {
+            if (trackBuffer.lastEnqueuedPresentationTime.isValid() && currentMediaTime < trackBuffer.lastEnqueuedPresentationTime) {
                 PlatformTimeRanges possiblyEnqueuedRanges(currentMediaTime, trackBuffer.lastEnqueuedPresentationTime);
                 possiblyEnqueuedRanges.intersectWith(erasedRanges);
                 if (possiblyEnqueuedRanges.length())
