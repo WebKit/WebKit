WebKit/WebKit

commit 3c4da69e7688646a6bc6a10ef6a6f2f2e0f2e592

[WinCairo] Move the destination of WinCairoRequirements.zip into WebKitLibraries and register it and related files as git ignore files. https://bugs.webkit.org/show_bug.cgi?id=180875
Patch by Basuke Suzuki <Basuke.Suzuki@sony.com> on 2017-12-16
Reviewed by Alex Christensen.

.:

Add files extracted from WinCairoRequirements.zip
* .gitignore:

Tools:

Bug fix. Specify the download destination correctly.

* Scripts/update-webkit-wincairo-libs.py:

Canonical link: https://commits.webkit.org/196796@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@226002 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/update-webkit-wincairo-libs.py
@@ -33,7 +33,7 @@ repo = 'WebKitForWindows/WinCairoRequirements'
 file = 'WinCairoRequirements.zip'
 output = 'WebKitLibraries/win'
 
-result = download.main([repo, file])
+result = download.main(['-o', output, repo, file])
 
 # Only unzip if required
 if result == download.Status.DOWNLOADED:

.gitignore
@@ -99,6 +99,13 @@ __cmake_systeminformation/
 /WebKitLibraries/win/lib*/pthreadVC2.lib
 /WebKitLibraries/win/lib*/zdll.lib
 
+# Ignore files installed by WinCairoRequirements
+/WebKitLibraries/win/WinCairoRequirements.zip
+/WebKitLibraries/win/WinCairoRequirements.zip.version
+/WebKitLibraries/win/etc/ssl/
+/WebKitLibraries/win/include/
+/WebKitLibraries/win/tools/
+
 # Ignore files generated by Qt Creator:
 *.pro.user
 

commit 90cdd37547e7fe06c0bc44b8d01b09aacb899c7d

[WinCairo] Move the destination of WinCairoRequirements.zip into WebKitLibraries and register it and related files as git ignore files. https://bugs.webkit.org/show_bug.cgi?id=180875 <rdar://problem/36091434>
Patch by Fujii Hironori <Hironori.Fujii@sony.com> on 2017-12-18
Reviewed by Darin Adler.

update-webkit-wincairo-libs.py gets broken by r226002. Downloaded
WinCairoRequirements.zip can not be found to unzip because the
destination was changed.

* Scripts/update-webkit-wincairo-libs.py: Specify the correct zip path to unzip.

Canonical link: https://commits.webkit.org/196824@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@226062 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/update-webkit-wincairo-libs.py
@@ -24,6 +24,7 @@
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import importlib
+import os.path
 import sys
 import zipfile
 
@@ -38,7 +39,7 @@ result = download.main(['-o', output, repo, file])
 # Only unzip if required
 if result == download.Status.DOWNLOADED:
     print('Extracting release to {}...'.format(output))
-    zip = zipfile.ZipFile(file, 'r')
+    zip = zipfile.ZipFile(os.path.join(output, file), 'r')
     zip.extractall(output)
     zip.close()
 elif result == download.Status.COULD_NOT_FIND: