2b95447bcab4fb082bdab0cd6e2fe34db6046e46
Ling Ho <lingho@apple.com>
2017-12-22 02:17:58 +0000

    "Release 32-bit Build EWS" queue should be moved up to macOS High Sierra on Bot Watcher's Dashboard https://bugs.webkit.org/show_bug.cgi?id=181071
    Reviewed by Alexey Proskuryakov.
    
    
    Canonical link: https://commits.webkit.org/196977@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@226253 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/BuildSlaveSupport/build.webkit.org-config/public_html/dashboard/Scripts/BubbleQueueServer.js
+++ Tools/BuildSlaveSupport/build.webkit.org-config/public_html/dashboard/Scripts/BubbleQueueServer.js
@@ -37,7 +37,7 @@ BubbleQueueServer = function()
         "mac-ews": {platform: Dashboard.Platform.MacOSXElCapitan, shortName: "mac", title: "WebKit1\xa0Release\xa0Tests\xa0EWS"},
         "mac-wk2-ews": {platform: Dashboard.Platform.MacOSXElCapitan, shortName: "mac-wk2", title: "WebKit2\xa0Release\xa0Tests\xa0EWS"},
         "mac-debug-ews": {platform: Dashboard.Platform.MacOSXElCapitan, shortName: "mac-debug", title: "WebKit1\xa0Debug\xa0Tests\xa0EWS"},
-        "mac-32bit-ews": {platform: Dashboard.Platform.MacOSXElCapitan, shortName: "mac-32bit", title: "Release\xa032\u2011bit\xa0Build\xa0EWS"},
+        "mac-32bit-ews": {platform: Dashboard.Platform.macOSHighSierra, shortName: "mac-32bit", title: "Release\xa032\u2011bit\xa0Build\xa0EWS"},
         "win-ews": {platform: Dashboard.Platform.Windows7, shortName: "win", title: "WebKit1\xa0Release\xa0Build\xa0EWS"}
     };
 
