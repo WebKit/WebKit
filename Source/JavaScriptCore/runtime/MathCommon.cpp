86450e26526cf02833418b1ec625fc99aa5f701a
Tim Horton <timothy_horton@apple.com>
2018-01-02 20:20:16 +0000

    Fix the MathCommon build with a recent compiler https://bugs.webkit.org/show_bug.cgi?id=181216
    Reviewed by Sam Weinig.
    
    * runtime/MathCommon.cpp:
    (JSC::fdlibmPow):
    This cast drops the 'const' qualifier from the pointer to 'one',
    but it doesn't have to, and it makes the compiler sad.
    
    
    Canonical link: https://commits.webkit.org/197055@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@226331 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/MathCommon.cpp
+++ Source/JavaScriptCore/runtime/MathCommon.cpp
@@ -179,7 +179,7 @@ static double fdlibmPow(double x, double y)
     int hx,hy,ix,iy;
     unsigned lx,ly;
 
-    i0 = ((*(int*)&one)>>29)^1; i1=1-i0;
+    i0 = ((*(const int*)&one)>>29)^1; i1=1-i0;
     hx = __HI(x); lx = __LO(x);
     hy = __HI(y); ly = __LO(y);
     ix = hx&0x7fffffff;  iy = hy&0x7fffffff;
