ed8740f73acc6610a651a8df3ff6e05a69866f5f
Michael Catanzaro <mcatanzaro@gnome.org>
2018-01-10 08:35:34 +0000

    Merge r224458 - [GTK] Fix gtk-doc generation with gtk-doc master https://bugs.webkit.org/show_bug.cgi?id=179252
    Reviewed by Carlos Garcia Campos.
    
    gtkdoc-scangobj has changed to no longer use a shell when executing its subcommands. In
    particular, this means we can no longer rely on shell stuff in our run command that we use
    to run the scanner. We currently use the RUN envvar to set LD_LIBRARY_PATH, and the end
    result is gtkdoc-scangobj now attempts to execute our environment variable instead of the
    scanner itself. We can just set LD_LIBRARY_PATH directly, instead of using RUN, in order to
    make documentation generation work again.
    
    * gtk/gtkdoc.py:
    (GTKDoc._run_gtkdoc_scangobj):

--- Tools/gtk/gtkdoc.py
+++ Tools/gtk/gtkdoc.py
@@ -319,9 +319,9 @@ class GTKDoc(object):
             ldflags = ' "-L%s" %s ' % (self.library_path, additional_ldflags) + ldflags
             current_ld_library_path = env.get('LD_LIBRARY_PATH')
             if current_ld_library_path:
-                env['RUN'] = 'LD_LIBRARY_PATH="%s:%s" ' % (self.library_path, current_ld_library_path)
+                env['LD_LIBRARY_PATH'] = '%s:%s' % (self.library_path, current_ld_library_path)
             else:
-                env['RUN'] = 'LD_LIBRARY_PATH="%s" ' % self.library_path
+                env['LD_LIBRARY_PATH'] = self.library_path
 
         if ldflags:
             env['LDFLAGS'] = '%s %s' % (ldflags, env.get('LDFLAGS', ''))
@@ -332,8 +332,6 @@ class GTKDoc(object):
             self.logger.debug('CFLAGS=%s', env['CFLAGS'])
         if 'LDFLAGS' in env:
             self.logger.debug('LDFLAGS %s', env['LDFLAGS'])
-        if 'RUN' in env:
-            self.logger.debug('RUN=%s', env['RUN'])
         self._run_command(['gtkdoc-scangobj', '--module=%s' % self.module_name],
                           env=env, cwd=self.output_dir)
 
