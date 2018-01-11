374b4e16b842018c50cb0d2a25dce0390a5501ee
Antoine Quint <graouts@webkit.org>
2018-01-11 20:18:35 +0000

    [iOS] There should be no controls markup generated in fullscreen https://bugs.webkit.org/show_bug.cgi?id=181540 <rdar://problem/35060379>
    Reviewed by Eric Carlson.
    
    Source/WebCore:
    
    We completely forgo the display of any content when fullscreen on iOS by setting the
    "visible" flag to "false" on the MediaControls, which will prevent any DOM content from
    being added.
    
    * Modules/modern-media-controls/media/media-controller.js:
    (MediaController.prototype.handleEvent):
    (MediaController.prototype._updateiOSFullscreenProperties):
    (MediaController):
    (MediaController.prototype._updateSupportingObjectsEnabledState): Deleted.
    
    LayoutTests:
    
    We update this test to simply check that we're removing all child nodes.
    
    * media/modern-media-controls/media-controller/ios/media-controller-stop-updates-in-fullscreen-expected.txt:
    * media/modern-media-controls/media-controller/ios/media-controller-stop-updates-in-fullscreen.html:
    
    Canonical link: https://commits.webkit.org/197410@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@226795 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/modern-media-controls/media/media-controller.js
+++ Source/WebCore/Modules/modern-media-controls/media/media-controller.js
@@ -158,7 +158,7 @@ class MediaController
             scheduler.flushScheduledLayoutCallbacks();
         } else if (event.currentTarget === this.media) {
             this._updateControlsIfNeeded();
-            this._updateSupportingObjectsEnabledState();
+            this._updateiOSFullscreenProperties();
             if (event.type === "webkitpresentationmodechanged")
                 this._returnMediaLayerToInlineIfNeeded();
         } else if (event.type === "keydown" && this.isFullscreen && event.key === " ") {
@@ -286,17 +286,20 @@ class MediaController
         this.host.textTrackContainer.classList.toggle("visible-controls-bar", !this.controls.faded);
     }
 
-    _updateSupportingObjectsEnabledState()
+    _updateiOSFullscreenProperties()
     {
         // On iOS, we want to make sure not to update controls when we're in fullscreen since the UI
         // will be completely invisible.
         if (!(this.layoutTraits & LayoutTraits.iOS))
             return;
 
-        if (this.isFullscreen)
+        const isFullscreen = this.isFullscreen;
+        if (isFullscreen)
             this._supportingObjects.forEach(supportingObject => supportingObject.disable());
         else
             this._supportingObjects.forEach(supportingObject => supportingObject.enable());
+
+        this.controls.visible = !isFullscreen;
     }
 
 }
