d418caa2bdcaad525fd7604a0d7658fa84d386e3
Commit Queue <commit-queue@webkit.org>
2018-01-12 15:21:46 +0000

    Cherry-pick r226771. rdar://problem/36450734
    Canonical link: https://commits.webkit.org/197228.68@safari-605-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-605-branch@226888 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/workers/service/resources/service-worker-importScript.js
+++ LayoutTests/http/tests/workers/service/resources/service-worker-importScript.js
@@ -1,11 +1,7 @@
 async function test()
 {
     try {
-        log("Registering service worker");
-        await navigator.serviceWorker.register("resources/service-worker-importScript-worker.js", { });
-        log("Registered service worker");
-
-        var frame = await interceptedFrame("resources/service-worker-fetch-worker.js", "/workers/service/resources/");
+        var frame = await interceptedFrame("resources/service-worker-importScript-worker.js", "/workers/service/resources/");
         var fetch = frame.contentWindow.fetch;
         var response = await fetch("status");
         log("Status is " + response.statusText);
