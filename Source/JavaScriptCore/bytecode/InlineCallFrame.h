51e2ab27b555f6864a4403476106607aca04305e
Saam Barati <sbarati@apple.com>
2018-01-12 06:31:16 +0000

    Cherry-pick r226811. rdar://problem/36458907
    Canonical link: https://commits.webkit.org/197228.64@safari-605-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-605-branch@226875 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/bytecode/InlineCallFrame.h
+++ Source/JavaScriptCore/bytecode/InlineCallFrame.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2011-2018 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -255,6 +255,13 @@ inline void CodeOrigin::walkUpInlineStack(const Function& function)
             break;
         codeOrigin = codeOrigin.inlineCallFrame->directCaller;
     }
+}
+
+ALWAYS_INLINE VirtualRegister remapOperand(InlineCallFrame* inlineCallFrame, VirtualRegister reg)
+{
+    if (inlineCallFrame)
+        return VirtualRegister(reg.offset() + inlineCallFrame->stackOffset);
+    return reg;
 }
 
 } // namespace JSC
