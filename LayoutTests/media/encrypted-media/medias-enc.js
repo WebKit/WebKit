0f67a8f558902cc48d9cf6a84d0692d956ac53ff
Yacine Bandou <yacine.bandou_ext@softathome.com>
2018-01-16 08:16:32 +0000

    [EME] Add layout test for the MSE playback of ClearKey encrypted audio and video https://bugs.webkit.org/show_bug.cgi?id=181594
    Patch by Yacine Bandou <yacine.bandou_ext@softathome.com> on 2018-01-16
    Reviewed by Xabier Rodriguez-Calvar.
    
    Add new layout tests in order to test the playback of ClearKey encrypted content:
    clearKey-cenc-audio-playback-mse : Tests the MSE playback of ClearKey encrypted audio track.
    clearKey-cenc-video-playback-mse : Tests the MSE playback of ClearKey encrypted video track.
    
    * media/content/encrypted/segments/AudioClearKeyCenc-seg-0.mp4: Added.
    * media/content/encrypted/segments/AudioClearKeyCenc-seg-1.mp4: Added.
    * media/content/encrypted/segments/AudioClearKeyCenc-seg-2.mp4: Added.
    * media/content/encrypted/segments/AudioClearKeyCenc-seg-3.mp4: Added.
        These are a simple encrypted audio fragments used in MSE playback.
        They have been fragmented and encrypted with BENTO4. https://www.bento4.com/
        Use full-sample encryption because they are not NAL Structured.
    * media/content/encrypted/segments/VideoClearKeyCenc-seg-1.mp4: Added.
    * media/content/encrypted/segments/VideoClearKeyCenc-seg-2.mp4: Added.
    * media/content/encrypted/segments/VideoClearKeyCenc-seg-3.mp4: Added.
        These are a simple encrypted video fragments used in MSE playback.
        They have been fragmented and encrypted with BENTO4. https://www.bento4.com/
        Use sub-sample encryption because they are NAL Structured.
    * media/encrypted-media/clearKey/clearKey-cenc-audio-playback-mse-expected.txt: Added.
    * media/encrypted-media/clearKey/clearKey-cenc-audio-playback-mse.html: Added.
    * media/encrypted-media/clearKey/clearKey-cenc-video-playback-mse-expected.txt: Added.
    * media/encrypted-media/clearKey/clearKey-cenc-video-playback-mse.html: Added.
    * media/encrypted-media/medias-enc.js:
    * platform/gtk/TestExpectations:
    * platform/mac/TestExpectations:
    * platform/wpe/TestExpectations:
        The test clearKey-cenc-audio-playback-mse crashes because the clearKey decryptor plugin doesn't
        support yet the Full-sample encryption, see bug 180080.
    
    Canonical link: https://commits.webkit.org/197518@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@226966 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/media/encrypted-media/medias-enc.js
+++ LayoutTests/media/encrypted-media/medias-enc.js
@@ -1,8 +1,21 @@
 const streamMedias = {
     "simpleClearKeyMSE" : {    video : {    initDataType : "cenc",
                                             mimeType     : 'video/mp4; codecs="avc1.64001F"',
-                                            segments     : [ "../../content/encrypted/segments/VideoClearKeyCenc-seg-0.mp4" ],
+                                            segments     : [ "../../content/encrypted/segments/VideoClearKeyCenc-seg-0.mp4",
+                                                             "../../content/encrypted/segments/VideoClearKeyCenc-seg-1.mp4",
+                                                             "../../content/encrypted/segments/VideoClearKeyCenc-seg-2.mp4",
+                                                             "../../content/encrypted/segments/VideoClearKeyCenc-seg-3.mp4",
+                                                           ],
                                             keys         : {    "d0d1d2d3d4d5d6d7d8d9dadbdcdddedf" : "c0c1c2c3c4c5c6c7c8c9cacbcccdcecf" }
+                                       },
+                               audio : {    initDataType : "cenc",
+                                            mimeType     : 'audio/mp4; codecs="mp4a.40.2"',
+                                            segments     : [ "../../content/encrypted/segments/AudioClearKeyCenc-seg-0.mp4",
+                                                             "../../content/encrypted/segments/AudioClearKeyCenc-seg-1.mp4",
+                                                             "../../content/encrypted/segments/AudioClearKeyCenc-seg-2.mp4",
+                                                             "../../content/encrypted/segments/AudioClearKeyCenc-seg-3.mp4",
+                                                           ],
+                                            keys         : {   	"b0b1b2b3b4b5b6b7b8b9babbbcbdbebf" : "a0a1a2a3a4a5a6a7a8a9aaabacadaeaf" }
                                        }
                           },
     "simpleClearKey"   :  {    video : {    initDataType : "cenc",
