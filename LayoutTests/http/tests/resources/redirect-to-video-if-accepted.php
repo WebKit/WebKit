46a848596eaf6f9f343ece5c769d07136e457e9b
Jer Noble <jer.noble@apple.com>
2018-01-18 08:33:14 +0000

    Layout Test http/tests/images/image-supports-video.html is flaky https://bugs.webkit.org/show_bug.cgi?id=181008 <rdar://problem/36143943>
    Reviewed by Youenn Fablet.
    
    Send a "Cache-control: nocache" header from the HTTP server when the image resource is
    requested.
    
    * http/tests/resources/redirect-to-video-if-accepted.php:
    * platform/mac-wk1/TestExpectations:
    
    
    Canonical link: https://commits.webkit.org/197623@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@227137 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/resources/redirect-to-video-if-accepted.php
+++ LayoutTests/http/tests/resources/redirect-to-video-if-accepted.php
@@ -2,6 +2,7 @@
 if (strpos($_SERVER['HTTP_ACCEPT'], 'video/*') !== false) {
     header('HTTP/1.1 301 Moved Permanently');
     header('Location: ' . $_GET['video']);
+    header('Cache-Control: no-cache, must-revalidate');
     return;
 }
 
