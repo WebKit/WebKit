eb375839930965849d8baa41976db831971b5f9b
Chris Dumez <cdumez@apple.com>
2018-01-24 21:26:47 +0000

    Layout Test http/tests/workers/service/basic-unregister-then-register-again-reuse.html is flaky https://bugs.webkit.org/show_bug.cgi?id=182030 <rdar://problem/36802933>
    Reviewed by Youenn Fablet.
    
    The service worker was only extending its lifetime using waitUntil() for 10 seconds. On the flakiness
    dashboard, I see that the test sometimes takes more than 10 seconds to run on some bots, which can
    cause the test to fail since the service worker is not keeping the registration alive anymore.
    To address the issue, raise the lifetime to 30 seconds.
    
    * http/tests/workers/service/resources/basic-unregister-then-register-again-reuse-worker.js:
    (event.waitUntil.new.Promise):
    
    
    Canonical link: https://commits.webkit.org/197901@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@227557 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/workers/service/resources/basic-unregister-then-register-again-reuse-worker.js
+++ LayoutTests/http/tests/workers/service/resources/basic-unregister-then-register-again-reuse-worker.js
@@ -2,7 +2,7 @@ self.addEventListener("message", function(event) {
     event.waitUntil(new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve();
-        }, 10000);
+        }, 30000);
     }));
     client = event.source;
     setTimeout(function() {
