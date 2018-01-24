601f262a7c975d11e211f8170cf02dd8ac921531
Yusuke Suzuki <utatane.tea@gmail.com>
2018-01-24 04:35:18 +0000

    Add more module scope related tests with code evaluation by string https://bugs.webkit.org/show_bug.cgi?id=181983
    Reviewed by Sam Weinig.
    
    JSTests:
    
    Add more module scope related tests. When the original tests are landed,
    we do not have browser integration. This patch adds more module scope tests
    with dynamically created script evaluation. We add tests with Function
    constructor, direct eval, indirect eval, setTimeout, setInterval, and event handlers.
    
    * modules/scopes-eval.js: Added.
    (shouldBe):
    * modules/scopes.js:
    (shouldBe):
    
    LayoutTests:
    
    * js/dom/modules/module-scope-event-handler-expected.txt: Added.
    * js/dom/modules/module-scope-event-handler.html: Added.
    * js/dom/modules/module-scope-set-interval-expected.txt: Added.
    * js/dom/modules/module-scope-set-interval.html: Added.
    * js/dom/modules/module-scope-set-timeout-expected.txt: Added.
    * js/dom/modules/module-scope-set-timeout.html: Added.
    
    Canonical link: https://commits.webkit.org/197860@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@227468 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/modules/scopes.js
+++ JSTests/modules/scopes.js
@@ -1,26 +1,36 @@
 import { Cocoa, Cappuccino, Matcha } from "./scopes/drink.js"
 import { shouldBe } from "./resources/assert.js";
 
+var global = Function("return this")();
+var globalEval = (0, eval);
+global.Cappuccino = 'Global Scope';
+
 {
     let Cocoa = 42;
     shouldBe(Cocoa, 42);
 }
 shouldBe(Cocoa, 'Cocoa');
-shouldBe(Cappuccino, 'Cappuccino');
+shouldBe(Cappuccino, 'Cappuccino'); // Module Scope.
 shouldBe(Matcha, 'Matcha');
-
-var global = Function("return this")();
 
 (function () {
     var Cocoa = 42;
-    let Cappuccino = 'CPO';
+    let Cappuccino = 'Function Scope';
     shouldBe(Cocoa, 42);
-    shouldBe(Cappuccino, 'CPO');
+    shouldBe(Cappuccino, 'Function Scope');
     shouldBe(Matcha, 'Matcha');
     {
+        let Cappuccino = 'Block Scope';
         const Matcha = 50;
         shouldBe(Matcha, 50);
         shouldBe(Object, global.Object);
+        {
+            (function () {
+                shouldBe(Cappuccino, 'Block Scope');
+                shouldBe(globalEval(`Cappuccino`), 'Global Scope');
+                shouldBe(Function(`return Cappuccino`)(), 'Global Scope');
+            }());
+        }
     }
     shouldBe(Object, global.Object);
 }());
