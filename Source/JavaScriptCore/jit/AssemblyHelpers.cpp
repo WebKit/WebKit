7fb8835150e1ac13dc02035c1fd3b5b5f85e437b
Mark Lam <mark.lam@apple.com>
2018-01-26 00:41:39 +0000

    Gardening: fix 32-bit build after r227643. https://bugs.webkit.org/show_bug.cgi?id=182086
    Not reviewed.
    
    * jit/AssemblyHelpers.cpp:
    (JSC::AssemblyHelpers::emitDynamicPoisonOnLoadedType):
    
    
    
    Canonical link: https://commits.webkit.org/197973@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@227644 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/jit/AssemblyHelpers.cpp
+++ Source/JavaScriptCore/jit/AssemblyHelpers.cpp
@@ -1046,7 +1046,7 @@ void AssemblyHelpers::emitDynamicPoisonOnLoadedType(GPRReg base, GPRReg actualTy
     emitDynamicPoison(base, actualType);
 #else
     UNUSED_PARAM(base);
-    UNUSED_PARAM(actualValue);
+    UNUSED_PARAM(actualType);
     UNUSED_PARAM(expectedType);
 #endif
 }
