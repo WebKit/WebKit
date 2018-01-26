6c2c712285d952c524fff183d34c611776d5043e
Per Arne Vollan <pvollan@apple.com>
2018-01-26 16:53:49 +0000

    Unreviewed, rolling out r224920.
    Some Win EWS bots are not coming back up after starting reboot.
    
    * EWSTools/start-queue-win.sh:
    
    
    
    Canonical link: https://commits.webkit.org/197997@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@227679 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/EWSTools/start-queue-win.sh
+++ Tools/EWSTools/start-queue-win.sh
@@ -28,40 +28,41 @@ function start_ews()
 
     "$PROGRAMFILES_X86/Microsoft Visual Studio/2017/Community/VC/Auxiliary/Build/vcvars32.bat"
 
-    date
-    echo "TASK: cleaning up old log files."
-    cd ~/win-ews-logs
-    rm -rf win-ews.old
-    mv win-ews.log win-ews.old
-    find ~/win-ews-logs -mtime +7 -exec rm -f {} \;
-    echo "TASK: Starting up"
-    cd ~/WebKit
-    echo "TASK: Cleaning WebKitBuild"
-    rm -rf WebKitBuild
-    date
-    echo "TASK: Cleaning up"
-    svn cleanup
-    date
-    echo "TASK: svn revert -R *"
-    svn revert -R *
-    date
-    echo "TASK: svn status"
-    svn status | grep "?" | awk '{print $2}' | xargs rm -rf
-    date
-    echo "TASK: webkit-patch clean"
-    ~/WebKit/Tools/Scripts/webkit-patch clean
-    date
-    echo "TASK: svn up --non-interactive"
-    svn up --non-interactive
-    date
-    echo "TASK: kill orphaned tasks"
-    taskkill.exe /f /im cl.exe
-    echo "TASK: test-webkitpy"
-    ~/WebKit/Tools/Scripts/test-webkitpy
-    echo "TASK: webkit-patch win-ews"
-    ~/WebKit/Tools/Scripts/webkit-patch win-ews --bot-id=$BOT_ID --no-confirm --exit-after-iteration $RESET_AFTER_ITERATION
-    echo "TASK: reboot"
-    shutdown /r
+    while :
+    do
+        date
+        echo "TASK: cleaning up old log files."
+        cd ~/win-ews-logs
+        rm -rf win-ews.old
+        mv win-ews.log win-ews.old
+        find ~/win-ews-logs -mtime +7 -exec rm -f {} \;
+        echo "TASK: Starting up"
+        cd ~/WebKit
+        echo "TASK: Cleaning WebKitBuild"
+        rm -rf WebKitBuild
+        date
+        echo "TASK: Cleaning up"
+        svn cleanup
+        date
+        echo "TASK: svn revert -R *"
+        svn revert -R *
+        date
+        echo "TASK: svn status"
+        svn status | grep "?" | awk '{print $2}' | xargs rm -rf
+        date
+        echo "TASK: webkit-patch clean"
+        ~/WebKit/Tools/Scripts/webkit-patch clean
+        date
+        echo "TASK: svn up --non-interactive"
+        svn up --non-interactive
+        date
+        echo "TASK: kill orphaned tasks"
+        taskkill.exe /f /im cl.exe
+        echo "TASK: test-webkitpy"
+        ~/WebKit/Tools/Scripts/test-webkitpy
+        echo "TASK: webkit-patch win-ews"
+        ~/WebKit/Tools/Scripts/webkit-patch win-ews --bot-id=$BOT_ID --no-confirm --exit-after-iteration $RESET_AFTER_ITERATION
+    done
 }
 
 start_ews
