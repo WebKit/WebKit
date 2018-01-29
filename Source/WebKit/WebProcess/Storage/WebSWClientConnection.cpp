25e19a4bc16546b0eb02d182c6c7fc6ff9e123cc
Chris Dumez <cdumez@apple.com>
2018-01-29 21:20:49 +0000

    Make sure we do not re-enter Webcore during StorageToWebProcessConnection::EstablishSWServerConnection Sync IPC https://bugs.webkit.org/show_bug.cgi?id=182256 <rdar://problem/36689233>
    Reviewed by Simon Fraser.
    
    Make sure we do not re-enter Webcore during StorageToWebProcessConnection::EstablishSWServerConnection Sync IPC as
    this can lead to crashes such as the one in <rdar://problem/36689233>.
    
    * WebProcess/Storage/WebSWClientConnection.cpp:
    (WebKit::WebSWClientConnection::WebSWClientConnection):
    
    
    Canonical link: https://commits.webkit.org/198058@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@227751 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/Storage/WebSWClientConnection.cpp
+++ Source/WebKit/WebProcess/Storage/WebSWClientConnection.cpp
@@ -53,7 +53,7 @@ WebSWClientConnection::WebSWClientConnection(IPC::Connection& connection, Sessio
     , m_connection(connection)
     , m_swOriginTable(makeUniqueRef<WebSWOriginTable>())
 {
-    bool result = sendSync(Messages::StorageToWebProcessConnection::EstablishSWServerConnection(sessionID), Messages::StorageToWebProcessConnection::EstablishSWServerConnection::Reply(m_identifier));
+    bool result = sendSync(Messages::StorageToWebProcessConnection::EstablishSWServerConnection(sessionID), Messages::StorageToWebProcessConnection::EstablishSWServerConnection::Reply(m_identifier), Seconds::infinity(), IPC::SendSyncOption::DoNotProcessIncomingMessagesWhenWaitingForSyncReply);
 
     ASSERT_UNUSED(result, result);
 }
