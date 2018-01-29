999a7b52e1bde41340e296cdae3fa7fc52484d3c
Ryosuke Niwa <rniwa@webkit.org>
2018-01-29 22:09:07 +0000

    Make StyleBench compatible with run-benchmark and run-perf-tests https://bugs.webkit.org/show_bug.cgi?id=182262
    Reviewed by Antti Koivisto.
    
    Copied resource files referenced from Speedometer directory since run-benchmark needs to be able
    to checkout each benchmark separately.
    
    Removed the code to create tests of the same name five times in makeSteps since this
    won't be compatible with either run-benchmark or run-perf-tests.
    
    * StyleBench/index.html: Removed the code to show warnings for local files since run-benchmark
    doesn't use HTTP server in WebDriver mode.
    * StyleBench/resources/benchmark-report.js: Copied from resources/benchmark-report.js.
    * StyleBench/resources/benchmark-runner.js: Copied from resources/benchmark-runner.js.
    (BenchmarkRunner.prototype._finalize): Use the correction factor of 8 instead of 5 in StyleBench.
    * StyleBench/resources/gauge.png: Copied from resources/gauge.png.
    * StyleBench/resources/gauge@2x.png: Copied from resources/gauge@2x.png.
    * StyleBench/resources/main.css: Copied from resources/main.css.
    * StyleBench/resources/main.js: Copied from resources/main.js.
    * StyleBench/resources/tests.js:
    (makeSteps): Only make each test once.
    
    
    
    Canonical link: https://commits.webkit.org/198063@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@227756 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- PerformanceTests/StyleBench/resources/tests.js
+++ PerformanceTests/StyleBench/resources/tests.js
@@ -1,20 +1,18 @@
 function makeSteps(count)
 {
-    let steps = [];
-    for (let i = 0; i < count; ++i) {
-        steps.push(new BenchmarkTestStep('Adding classes', (bench, contentWindow, contentDocument) => {
-            bench.addClasses(100);
-        }));
-        steps.push(new BenchmarkTestStep('Removing classes', (bench, contentWindow, contentDocument) => {
-            bench.removeClasses(100);
-        }));
-        steps.push(new BenchmarkTestStep('Adding leaf elements', (bench, contentWindow, contentDocument) => {
-            bench.addLeafElements(100);
-        }));
-        steps.push(new BenchmarkTestStep('Removing leaf elements', (bench, contentWindow, contentDocument) => {
-            bench.removeLeafElements(100);
-        }));
-    }
+    const steps = [];
+    steps.push(new BenchmarkTestStep('Adding classes', (bench, contentWindow, contentDocument) => {
+        bench.addClasses(100);
+    }));
+    steps.push(new BenchmarkTestStep('Removing classes', (bench, contentWindow, contentDocument) => {
+        bench.removeClasses(100);
+    }));
+    steps.push(new BenchmarkTestStep('Adding leaf elements', (bench, contentWindow, contentDocument) => {
+        bench.addLeafElements(100);
+    }));
+    steps.push(new BenchmarkTestStep('Removing leaf elements', (bench, contentWindow, contentDocument) => {
+        bench.removeLeafElements(100);
+    }));
     return steps;
 }
 
@@ -28,7 +26,7 @@ function makeSuite(configuration)
                 return contentWindow.createBenchmark(configuration);
             });
         },
-        tests: makeSteps(5),
+        tests: makeSteps(),
     };
 }
 
