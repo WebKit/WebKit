887baa6b9afb412963c0c239f073387844ab18d3
Brady Eidson <beidson@apple.com>
2018-01-29 20:34:38 +0000

    Make the API test added in r227737 be Mac-only.
    Unreviewed gardening.
    
    * TestWebKitAPI/Tests/WebKitCocoa/MessagePortProviders.mm:
    
    Canonical link: https://commits.webkit.org/198055@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@227748 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/WebKitCocoa/MessagePortProviders.mm
+++ Tools/TestWebKitAPI/Tests/WebKitCocoa/MessagePortProviders.mm
@@ -25,7 +25,7 @@
 
 #import "config.h"
 
-#if WK_API_ENABLED
+#if WK_API_ENABLED && PLATFORM(MAC)
 
 #import "PlatformUtilities.h"
 #import "TestWKWebView.h"
@@ -68,4 +68,4 @@ TEST(MessagePort, Providers)
 
 } // namespace TestWebKitAPI
 
-#endif // WK_API_ENABLED
+#endif // WK_API_ENABLED && PLATFORM(MAC)
