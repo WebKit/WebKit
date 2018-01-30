cc17bfd09470a904ee25b631475644322565b7fd
Myles C. Maxfield <mmaxfield@apple.com>
2018-01-30 21:51:18 +0000

    Test fix after r227776 https://bugs.webkit.org/show_bug.cgi?id=180951
    Unreviewed.
    
    * platform/graphics/cocoa/FontCacheCoreText.cpp:
    (WebCore::mandatoryAttributesForUserInstalledFonts):
    
    Canonical link: https://commits.webkit.org/198103@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@227848 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/cocoa/FontCacheCoreText.cpp
+++ Source/WebCore/platform/graphics/cocoa/FontCacheCoreText.cpp
@@ -1432,7 +1432,7 @@ RetainPtr<CFSetRef> mandatoryAttributesForUserInstalledFonts(AllowUserInstalledF
 {
 #if CAN_DISALLOW_USER_INSTALLED_FONTS
     if (allowUserInstalledFonts == AllowUserInstalledFonts::No) {
-        CFTypeRef mandatoryAttributesValues[] = { kCTFontUserInstalledAttribute, kCTFontFallbackOptionAttribute };
+        CFTypeRef mandatoryAttributesValues[] = { kCTFontFamilyNameAttribute, kCTFontPostScriptNameAttribute, kCTFontNameAttribute, kCTFontEnabledAttribute, kCTFontUserInstalledAttribute, kCTFontFallbackOptionAttribute };
         return adoptCF(CFSetCreate(kCFAllocatorDefault, mandatoryAttributesValues, WTF_ARRAY_LENGTH(mandatoryAttributesValues), &kCFTypeSetCallBacks));
     }
 #else
