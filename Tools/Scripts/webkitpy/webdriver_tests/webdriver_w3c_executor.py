a9cbe5f8868d2bda8138a00d4b53e8ae5879ad9f
Carlos Garcia Campos <cgarcia@igalia.com>
2018-01-31 09:08:37 +0000

    WebDriver: add domains to server config in w3c tests https://bugs.webkit.org/show_bug.cgi?id=182294
    Reviewed by Carlos Alberto Lopez Perez.
    
    Several cookie tests expect server_config to have a domains key.
    
    Fixes: imported/w3c/webdriver/tests/cookies/add_cookie.py::test_add_domain_cookie
           imported/w3c/webdriver/tests/cookies/add_cookie.py::test_add_session_cookie_with_leading_dot_character_in_domain
    
    * Scripts/webkitpy/webdriver_tests/webdriver_w3c_executor.py:
    (WebDriverW3CExecutor.__init__):
    
    Canonical link: https://commits.webkit.org/198129@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@227881 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/webdriver_tests/webdriver_w3c_executor.py
+++ Tools/Scripts/webkitpy/webdriver_tests/webdriver_w3c_executor.py
@@ -133,7 +133,7 @@ class WebDriverW3CExecutor(WdspecExecutor):
     def __init__(self, driver, server, display_driver, timeout, expectations):
         WebKitDriverServer.test_env = display_driver._setup_environ_for_test()
         WebKitDriverServer.test_env.update(driver.browser_env())
-        server_config = {'host': server.host(), 'ports': {'http': [str(server.port())]}}
+        server_config = {'host': server.host(), 'domains': {'': server.host()}, 'ports': {'http': [str(server.port())]}}
         WdspecExecutor.__init__(self, driver.browser_name(), server_config, driver.binary_path(), None, capabilities=driver.capabilities())
 
         self._timeout = timeout
