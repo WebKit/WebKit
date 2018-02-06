58322b7d05f300bb9e257e191df07ccd62db9a0b
Keith Miller <keith_miller@apple.com>
2018-02-06 22:42:26 +0000

    put_to_scope/get_from_scope should not cache lexical scopes when expecting a global object https://bugs.webkit.org/show_bug.cgi?id=182549 <rdar://problem/36189995>
    Reviewed by Saam Barati.
    
    JSTests:
    
    * stress/var-injection-cache-invalidation.js: Added.
    (allocateLotsOfThings):
    (test):
    
    Source/JavaScriptCore:
    
    Previously, the llint/baseline caching for put_to_scope and
    get_from_scope would cache lexical environments when the
    varInjectionWatchpoint had been fired for global properties. Code
    in the DFG does not follow this same assumption so we could
    potentially return the wrong result. Additionally, the baseline
    would write barrier the global object rather than the lexical
    enviroment object. This patch makes it so that we do not cache
    anything other than the global object for when the resolve type is
    GlobalPropertyWithVarInjectionChecks or GlobalProperty.
    
    * assembler/MacroAssembler.cpp:
    (JSC::MacroAssembler::jitAssert):
    * assembler/MacroAssembler.h:
    * jit/JITPropertyAccess.cpp:
    (JSC::JIT::emit_op_get_from_scope):
    (JSC::JIT::emit_op_put_to_scope):
    * runtime/CommonSlowPaths.h:
    (JSC::CommonSlowPaths::tryCachePutToScopeGlobal):
    (JSC::CommonSlowPaths::tryCacheGetFromScopeGlobal):
    * runtime/Options.h:
    
    Canonical link: https://commits.webkit.org/198344@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@228193 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ JSTests/stress/var-injection-cache-invalidation.js
@@ -0,0 +1,19 @@
+a = 0;
+
+function allocateLotsOfThings(array) {
+    for (let i = 0; i < 1e4; i++)
+        array[i] = { next: array[Math.floor(i / 2)] };
+}
+
+function test() {
+    a = 5;
+    for (var i = 0; i < 1e3; i++) {
+        allocateLotsOfThings([]);
+        edenGC();
+        eval("var a = new Int32Array(100);");
+    }
+}
+noInline(test);
+noDFG(test);
+
+test();
