a2e4bcef99953ea860d35a2c8879e9dbf304267e
Megan Gardner <megan_gardner@apple.com>
2018-02-20 20:12:09 +0000

    Switch to UIWKTextInteractionAssistant for non-editable text https://bugs.webkit.org/show_bug.cgi?id=182834
    Reviewed by Wenson Hsieh and Tim Horton.
    
    Switching to only using the UIWKTextInteractionAssistant. Only character granularity is now supported.
    Also keep the from regressing gating the keyboard bring up on user interaction. The presents of a
    textInteractionAssistant can no longer be used as proxy for the presence of the keyboard.
    
    * UIProcess/ios/WKContentViewInteraction.mm:
    (-[WKContentView useSelectionAssistantWithGranularity:]):
    (-[WKContentView _startAssistingNode:userIsInteracting:blurPreviousNode:changingActivityState:userObject:]):
    
    
    
    Canonical link: https://commits.webkit.org/198727@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@228829 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
+++ Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -1867,27 +1867,18 @@ static void cancelPotentialTapIfNecessary(WKContentView* contentView)
 
 - (void)useSelectionAssistantWithGranularity:(WKSelectionGranularity)selectionGranularity
 {
-    if (selectionGranularity == WKSelectionGranularityDynamic) {
-        if (_textSelectionAssistant) {
-            [_textSelectionAssistant deactivateSelection];
-            _textSelectionAssistant = nil;
-        }
-        if (!_webSelectionAssistant)
-            _webSelectionAssistant = adoptNS([[UIWKSelectionAssistant alloc] initWithView:self]);
-    } else if (selectionGranularity == WKSelectionGranularityCharacter) {
-        if (_webSelectionAssistant)
-            _webSelectionAssistant = nil;
+    if (_webSelectionAssistant)
+        _webSelectionAssistant = nil;
 
-        if (!_textSelectionAssistant)
-            _textSelectionAssistant = adoptNS([[UIWKTextInteractionAssistant alloc] initWithView:self]);
-        else {
-            // Reset the gesture recognizers in case editibility has changed.
-            [_textSelectionAssistant setGestureRecognizers];
-        }
-
-        if (self.isFirstResponder && !self.suppressAssistantSelectionView)
-            [_textSelectionAssistant activateSelection];
+    if (!_textSelectionAssistant)
+        _textSelectionAssistant = adoptNS([[UIWKTextInteractionAssistant alloc] initWithView:self]);
+    else {
+        // Reset the gesture recognizers in case editibility has changed.
+        [_textSelectionAssistant setGestureRecognizers];
     }
+
+    if (self.isFirstResponder && !self.suppressAssistantSelectionView)
+        [_textSelectionAssistant activateSelection];
 }
 
 - (void)clearSelection
@@ -4012,7 +4003,7 @@ static bool isAssistableInputType(InputType type)
         shouldShowKeyboard = [inputDelegate _webView:_webView focusShouldStartInputSession:focusedElementInfo.get()];
     else {
         // The default behavior is to allow node assistance if the user is interacting or the keyboard is already active.
-        shouldShowKeyboard = userIsInteracting || _textSelectionAssistant || changingActivityState;
+        shouldShowKeyboard = userIsInteracting || _isChangingFocus || changingActivityState;
 #if ENABLE(DATA_INTERACTION)
         shouldShowKeyboard |= _dragDropInteractionState.isPerformingDrop();
 #endif
