bb67a2a451aed8cb6a6a42c174f7cb5c815037e8
Carlos Garcia Campos <cgarcia@igalia.com>
2018-03-02 07:56:41 +0000

    Unreviewed. Update W3C WebDriver imported tests.
    * imported/w3c/importer.json:
    * imported/w3c/tools/webdriver/webdriver/transport.py:
    * imported/w3c/tools/wptrunner/MANIFEST.in:
    * imported/w3c/tools/wptrunner/README.rst:
    * imported/w3c/tools/wptrunner/requirements.txt:
    * imported/w3c/tools/wptrunner/requirements_chrome.txt:
    * imported/w3c/tools/wptrunner/requirements_edge.txt:
    * imported/w3c/tools/wptrunner/requirements_firefox.txt:
    * imported/w3c/tools/wptrunner/requirements_ie.txt:
    * imported/w3c/tools/wptrunner/requirements_opera.txt:
    * imported/w3c/tools/wptrunner/requirements_sauce.txt:
    * imported/w3c/tools/wptrunner/requirements_servo.txt:
    * imported/w3c/tools/wptrunner/setup.py:
    * imported/w3c/tools/wptrunner/wptrunner/browsers/chrome.py:
    * imported/w3c/tools/wptrunner/wptrunner/browsers/chrome_android.py:
    * imported/w3c/tools/wptrunner/wptrunner/browsers/edge.py:
    * imported/w3c/tools/wptrunner/wptrunner/browsers/firefox.py:
    * imported/w3c/tools/wptrunner/wptrunner/browsers/ie.py:
    * imported/w3c/tools/wptrunner/wptrunner/browsers/opera.py:
    * imported/w3c/tools/wptrunner/wptrunner/browsers/sauce.py:
    * imported/w3c/tools/wptrunner/wptrunner/browsers/server-locations.txt: Removed.
    * imported/w3c/tools/wptrunner/wptrunner/browsers/servo.py:
    * imported/w3c/tools/wptrunner/wptrunner/browsers/servodriver.py:
    * imported/w3c/tools/wptrunner/wptrunner/config.json: Removed.
    * imported/w3c/tools/wptrunner/wptrunner/environment.py:
    * imported/w3c/tools/wptrunner/wptrunner/executors/executorservo.py:
    * imported/w3c/tools/wptrunner/wptrunner/font.py:
    * imported/w3c/tools/wptrunner/wptrunner/hosts.py: Removed.
    * imported/w3c/tools/wptrunner/wptrunner/testrunner.py:
    * imported/w3c/tools/wptrunner/wptrunner/tests/browsers/__init__.py: Added.
    * imported/w3c/tools/wptrunner/wptrunner/tests/test_hosts.py: Removed.
    * imported/w3c/tools/wptrunner/wptrunner/webdriver_server.py:
    * imported/w3c/tools/wptrunner/wptrunner/wptrunner.py:
    * imported/w3c/webdriver/OWNERS:
    * imported/w3c/webdriver/tests/actions/mouse.py:
    * imported/w3c/webdriver/tests/actions/mouse_dblclick.py:
    * imported/w3c/webdriver/tests/actions/pointer_origin.py: Added.
    * imported/w3c/webdriver/tests/actions/support/mouse.py:
    * imported/w3c/webdriver/tests/element_click/bubbling.py:
    * imported/w3c/webdriver/tests/element_click/select.py:
    * imported/w3c/webdriver/tests/element_send_keys/form_controls.py:
    * imported/w3c/webdriver/tests/element_send_keys/interactability.py:
    * imported/w3c/webdriver/tests/execute_script/cyclic.py:
    * imported/w3c/webdriver/tests/fullscreen_window.py:
    * imported/w3c/webdriver/tests/interface.html:
    * imported/w3c/webdriver/tests/state/get_element_property.py:
    
    Canonical link: https://commits.webkit.org/198949@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@229166 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebDriverTests/imported/w3c/tools/wptrunner/wptrunner/browsers/sauce.py
+++ WebDriverTests/imported/w3c/tools/wptrunner/wptrunner/browsers/sauce.py
@@ -113,8 +113,7 @@ def env_extras(**kwargs):
 
 
 def env_options():
-    return {"host": "web-platform.test",
-            "bind_hostname": "true",
+    return {"bind_hostname": "true",
             "supports_debugger": False}
 
 
@@ -134,8 +133,23 @@ class SauceConnect():
         self.sauce_connect_binary = kwargs.get("sauce_connect_binary")
         self.sc_process = None
         self.temp_dir = None
+        self.env_config = None
 
-    def __enter__(self, options):
+    def __call__(self, env_options, env_config):
+        self.env_config = env_config
+
+        return self
+
+    def __enter__(self):
+        # Because this class implements the context manager protocol, it is
+        # possible for instances to be provided to the `with` statement
+        # directly. This class implements the callable protocol so that data
+        # which is not available during object initialization can be provided
+        # prior to this moment. Instances must be invoked in preparation for
+        # the context manager protocol, but this additional constraint is not
+        # itself part of the protocol.
+        assert self.env_config is not None, 'The instance has been invoked.'
+
         if not self.sauce_connect_binary:
             self.temp_dir = tempfile.mkdtemp()
             get_tar("https://saucelabs.com/downloads/sc-4.4.9-linux.tar.gz", self.temp_dir)
@@ -153,8 +167,7 @@ class SauceConnect():
             "--metrics-address=0.0.0.0:9876",
             "--readyfile=./sauce_is_ready",
             "--tunnel-domains",
-            "web-platform.test",
-            "*.web-platform.test"
+            ",".join(self.env_config['domains'].values())
         ])
 
         # Timeout config vars
@@ -181,6 +194,7 @@ class SauceConnect():
             raise SauceException("Unable to start Sauce Connect Proxy. Process exited with code %s", self.sc_process.returncode)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self.env_config = None
         self.sc_process.terminate()
         if self.temp_dir and os.path.exists(self.temp_dir):
             try:
