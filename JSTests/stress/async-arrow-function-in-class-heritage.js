88eb309d186e10bde61c9947466ae1b531760078
Yusuke Suzuki <ysuzuki@apple.com>
2018-03-05 13:16:55 +0000

    Merge r229162 - ASSERTION FAILED: matchContextualKeyword(m_vm->propertyNames->async) https://bugs.webkit.org/show_bug.cgi?id=183173
    Reviewed by Saam Barati.
    
    JSTests:
    
    * stress/async-arrow-function-in-class-heritage.js: Added.
    (testSyntax):
    (testSyntaxError):
    (SyntaxError):
    
    Source/JavaScriptCore:
    
    Classifier could propagate an error which does not occur at the first token
    of the given expression. We should check whether the given token is "async"
    instead of assertion.
    
    * parser/Parser.cpp:
    (JSC::Parser<LexerType>::parseAssignmentExpression):

---
+++ JSTests/stress/async-arrow-function-in-class-heritage.js
@@ -0,0 +1,25 @@
+function testSyntax(script) {
+    try {
+        eval(script);
+    } catch (error) {
+        if (error instanceof SyntaxError)
+            throw new Error("Bad error: " + String(error));
+    }
+}
+
+function testSyntaxError(script, message) {
+    var error = null;
+    try {
+        eval(script);
+    } catch (e) {
+        error = e;
+    }
+    if (!error)
+        throw new Error("Expected syntax error not thrown");
+
+    if (String(error) !== message)
+        throw new Error("Bad error: " + String(error));
+}
+
+testSyntaxError(`void class extends async()=>{} {}`, `SyntaxError: Unexpected token '=>'`);
+testSyntaxError(`void class extends (async()=>{}) {}`, `TypeError: The value of the superclass's prototype property is not an object.`);
