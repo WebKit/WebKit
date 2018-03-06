b2883355f87e9d4cb0514b149f88e6c9d021ff62
Michael Saboff <msaboff@apple.com>
2018-03-06 02:34:09 +0000

    Start using MAP_JIT for macOS https://bugs.webkit.org/show_bug.cgi?id=183353
    Reviewed by Filip Pizlo.
    
    Unify setting this flag for both iOS and macOS.
    
    * wtf/OSAllocatorPosix.cpp:
    (WTF::OSAllocator::reserveAndCommit):
    
    
    Canonical link: https://commits.webkit.org/199016@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@229301 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/OSAllocatorPosix.cpp
+++ Source/WTF/wtf/OSAllocatorPosix.cpp
@@ -71,7 +71,7 @@ void* OSAllocator::reserveAndCommit(size_t bytes, Usage usage, bool writable, bo
         protection |= PROT_EXEC;
 
     int flags = MAP_PRIVATE | MAP_ANON;
-#if PLATFORM(IOS)
+#if OS(DARWIN)
     if (executable)
         flags |= MAP_JIT;
 #endif
