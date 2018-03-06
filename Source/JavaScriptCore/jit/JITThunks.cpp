71588f7cd70701bd3efb9f515a219e73180eabc8
Mark Lam <mark.lam@apple.com>
2018-03-06 03:03:01 +0000

    JITThunk functions should only be called when the JIT is enabled. https://bugs.webkit.org/show_bug.cgi?id=183351 <rdar://problem/38160091>
    Reviewed by Keith Miller.
    
    * jit/JITThunks.cpp:
    (JSC::JITThunks::ctiNativeCall):
    (JSC::JITThunks::ctiNativeConstruct):
    (JSC::JITThunks::ctiInternalFunctionCall):
    (JSC::JITThunks::ctiInternalFunctionConstruct):
    * runtime/VM.cpp:
    (JSC::VM::VM):
    (JSC::VM::getCTIInternalFunctionTrampolineFor):
    
    
    
    Canonical link: https://commits.webkit.org/199017@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@229302 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/jit/JITThunks.cpp
+++ Source/JavaScriptCore/jit/JITThunks.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012, 2013, 2015-2016 Apple Inc. All rights reserved.
+ * Copyright (C) 2012-2018 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -47,15 +47,13 @@ JITThunks::~JITThunks()
 
 MacroAssemblerCodePtr JITThunks::ctiNativeCall(VM* vm)
 {
-    if (!VM::canUseJIT())
-        return MacroAssemblerCodePtr::createLLIntCodePtr(llint_native_call_trampoline);
+    ASSERT(VM::canUseJIT());
     return ctiStub(vm, nativeCallGenerator).code();
 }
 
 MacroAssemblerCodePtr JITThunks::ctiNativeConstruct(VM* vm)
 {
-    if (!VM::canUseJIT())
-        return MacroAssemblerCodePtr::createLLIntCodePtr(llint_native_construct_trampoline);
+    ASSERT(VM::canUseJIT());
     return ctiStub(vm, nativeConstructGenerator).code();
 }
 
@@ -73,15 +71,13 @@ MacroAssemblerCodePtr JITThunks::ctiNativeTailCallWithoutSavedTags(VM* vm)
 
 MacroAssemblerCodePtr JITThunks::ctiInternalFunctionCall(VM* vm)
 {
-    if (!VM::canUseJIT())
-        return MacroAssemblerCodePtr::createLLIntCodePtr(llint_internal_function_call_trampoline);
+    ASSERT(VM::canUseJIT());
     return ctiStub(vm, internalFunctionCallGenerator).code();
 }
 
 MacroAssemblerCodePtr JITThunks::ctiInternalFunctionConstruct(VM* vm)
 {
-    if (!VM::canUseJIT())
-        return MacroAssemblerCodePtr::createLLIntCodePtr(llint_internal_function_construct_trampoline);
+    ASSERT(VM::canUseJIT());
     return ctiStub(vm, internalFunctionConstructGenerator).code();
 }
 
