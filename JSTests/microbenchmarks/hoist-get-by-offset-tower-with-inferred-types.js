e6ec2817e274c8e69e3f39fb35a5f32f0df1c871
Filip Pizlo <fpizlo@apple.com>
2018-04-01 17:08:39 +0000

    JSC crash in JIT code with for-of loop and Array/Set iterators https://bugs.webkit.org/show_bug.cgi?id=183174
    Reviewed by Saam Barati.
    
    JSTests:
    
    * microbenchmarks/hoist-get-by-offset-tower-with-inferred-types.js: Added. This test shows that fixing the bug didn't break hoisting of GetByOffset with inferred types. I confirmed that if I did break it, this test slows down by >7x.
    (foo):
    * stress/hoist-get-by-offset-with-control-dependent-inferred-type.js: Added. This test shows that the bug is fixed.
    (f):
    
    Source/JavaScriptCore:
    
    * dfg/DFGSafeToExecute.h:
    (JSC::DFG::safeToExecute): Fix the bug by making GetByOffset and friends verify that they are getting the type proof they want at the desired hoisting site.
    
    
    
    Canonical link: https://commits.webkit.org/199766@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@230143 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ JSTests/microbenchmarks/hoist-get-by-offset-tower-with-inferred-types.js
@@ -0,0 +1,19 @@
+function foo(o)
+{
+    var result = 0;
+    for (var i = 0; i < 100; ++i)
+        result += o.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z;
+    return result;
+}
+
+noInline(foo);
+
+for (var i = 0; i < 1000; ++i) {
+    var o = {f:{g:{h:{i:{j:{k:{l:{m:{n:{o:{p:{q:{r:{s:{t:{u:{v:{w:{x:{y:{z:42}}}}}}}}}}}}}}}}}}}}};
+    for (var j = 0; j < 100; ++j) {
+        var result = foo(o);
+        if (result != 4200)
+            throw "Bad result in loop: " + result;
+    }
+}
+
