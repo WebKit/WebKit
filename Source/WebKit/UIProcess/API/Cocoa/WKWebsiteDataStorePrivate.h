4e6957c248bea75687aa653a74aa82c9cfbb226b
Ryan Haddad <ryanhaddad@apple.com>
2018-04-02 16:41:17 +0000

    Unreviewed build fix.
    * UIProcess/API/Cocoa/WKWebsiteDataStorePrivate.h: Update availability annotation.
    
    Canonical link: https://commits.webkit.org/199782@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@230159 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/Cocoa/WKWebsiteDataStorePrivate.h
+++ Source/WebKit/UIProcess/API/Cocoa/WKWebsiteDataStorePrivate.h
@@ -55,7 +55,7 @@ typedef NS_OPTIONS(NSUInteger, _WKWebsiteDataStoreFetchOptions) {
 
 - (void)_resourceLoadStatisticsSetShouldSubmitTelemetry:(BOOL)value WK_API_AVAILABLE(macosx(10.13), ios(11.0));
 - (void)_setResourceLoadStatisticsTestingCallback:(nullable void (^)(WKWebsiteDataStore *, NSString *))callback WK_API_AVAILABLE(macosx(10.13), ios(11.0));
-- (void)_getAllStorageAccessEntries:(void (^)(NSArray<NSString *> *domains))completionHandler WK_API_AVAILABLE(macosx(WK_MAC_TBA), ios(WK_IOS_TBA));
+- (void)_getAllStorageAccessEntries:(void (^)(NSArray<NSString *> *domains))completionHandler WK_API_AVAILABLE(macosx(10.13.4), ios(11.3));
 + (void)_allowWebsiteDataRecordsForAllOrigins WK_API_AVAILABLE(macosx(10.13.4), ios(11.3));
 - (bool)_hasRegisteredServiceWorker WK_API_AVAILABLE(macosx(WK_MAC_TBA), ios(WK_IOS_TBA));
 
