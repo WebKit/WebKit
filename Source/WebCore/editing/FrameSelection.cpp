1250c729993c001ae43e12e04462e646e5ce4a9a
Wenson Hsieh <wenson_hsieh@apple.com>
2018-04-11 03:44:00 +0000

    FrameSelection::appearanceUpdateTimerFired should be robust against layout passes underneath it https://bugs.webkit.org/show_bug.cgi?id=183395 <rdar://problem/38055732>
    Reviewed by Zalan Bujtas.
    
    Source/WebCore:
    
    In the case where a FrameSelection updates its appearance when m_appearanceUpdateTimer is fired, the
    FrameSelection's Frame is unprotected, and can be removed by arbitrary script. This patch applies a simple
    mitigation by wrapping the Frame in a Ref when firing the appearance update timer.
    
    Test: editing/selection/iframe-update-selection-appearance.html
    
    * editing/FrameSelection.cpp:
    (WebCore::FrameSelection::appearanceUpdateTimerFired):
    
    LayoutTests:
    
    Add a new layout test that passes if we didn't crash.
    
    * editing/selection/iframe-update-selection-appearance-expected.txt: Added.
    * editing/selection/iframe-update-selection-appearance.html: Added.
    
    Canonical link: https://commits.webkit.org/200038@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@230513 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/editing/FrameSelection.cpp
+++ Source/WebCore/editing/FrameSelection.cpp
@@ -2441,6 +2441,7 @@ void FrameSelection::scheduleAppearanceUpdateAfterStyleChange()
 
 void FrameSelection::appearanceUpdateTimerFired()
 {
+    Ref<Frame> protectedFrame(*m_frame);
     updateAppearanceAfterLayoutOrStyleChange();
 }
 
