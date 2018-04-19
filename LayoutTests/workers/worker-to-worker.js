1ef823d968d57da0062358446e9ba3de937ac6d5
Tadeu Zagallo <tzagallo@apple.com>
2018-04-19 17:02:38 +0000

    Fix flakiness in worker-to-worker test https://bugs.webkit.org/show_bug.cgi?id=184769 <rdar://problem/39552333>
    Patch by Tadeu Zagallo <tzagallo@apple.com> on 2018-04-19
    Reviewed by Alexey Proskuryakov.
    
    * workers/worker-to-worker-expected.txt:
    * workers/worker-to-worker.html:
    * workers/worker-to-worker.js:
    
    Canonical link: https://commits.webkit.org/200285@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@230806 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/workers/worker-to-worker.js
+++ LayoutTests/workers/worker-to-worker.js
@@ -1,11 +1,6 @@
 self.onmessage = e => {
-  const {name} = e.data;
-
   e.data.port.onmessage = e => {
-    self.postMessage(`[${name}] Received message: ${e.data}`);
+    self.postMessage(`[Worker] Received message: ${e.data}`);
   }
-
-  const sendMsg = () => e.data.port.postMessage('hello');
-  if (e.data.wait) setTimeout(sendMsg, e.data.wait);
-  else sendMsg();
+  e.data.port.postMessage('hello');
 };
