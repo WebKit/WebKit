42067abef328d345d2cf35e864f83576328e763c
Jeremy Jones <jeremyj@apple.com>
2018-04-21 01:32:18 +0000

    Disable backward and forward navigation swipes while in fullscreen. https://bugs.webkit.org/show_bug.cgi?id=184656 rdar://problem/36057535
    Patch by Jeremy Jones <jeremyj@apple.com> on 2018-04-20
    Reviewed by Tim Horton.
    
    Disable navigation swipes while in fullscreen.
    
    * UIProcess/Cocoa/ViewGestureController.cpp:
    (WebKit::ViewGestureController::canSwipeInDirection const):
    
    Canonical link: https://commits.webkit.org/200349@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@230873 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/Cocoa/ViewGestureController.cpp
+++ Source/WebKit/UIProcess/Cocoa/ViewGestureController.cpp
@@ -30,6 +30,7 @@
 #import "RemoteLayerTreeDrawingAreaProxy.h"
 #import "ViewGestureControllerMessages.h"
 #import "WebBackForwardList.h"
+#import "WebFullScreenManagerProxy.h"
 #import "WebPageProxy.h"
 #import "WebProcessProxy.h"
 #import <wtf/MathExtras.h>
@@ -115,6 +116,8 @@ void ViewGestureController::setAlternateBackForwardListSourcePage(WebPageProxy* 
     
 bool ViewGestureController::canSwipeInDirection(SwipeDirection direction) const
 {
+    if (m_webPageProxy.fullScreenManager() && m_webPageProxy.fullScreenManager()->isFullScreen())
+        return false;
     RefPtr<WebPageProxy> alternateBackForwardListSourcePage = m_alternateBackForwardListSourcePage.get();
     auto& backForwardList = alternateBackForwardListSourcePage ? alternateBackForwardListSourcePage->backForwardList() : m_webPageProxy.backForwardList();
     if (direction == SwipeDirection::Back)
