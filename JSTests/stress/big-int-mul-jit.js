1ef18934e5964dea8d87cf698d675da3e1c6356f
Caio Araujo Neponoceno de Lima <ticaiolima@gmail.com>
2018-04-28 06:11:57 +0000

    [ESNext][BigInt] Implement support for "*" operation https://bugs.webkit.org/show_bug.cgi?id=183721
    Reviewed by Saam Barati.
    
    JSTests:
    
    * bigIntTests.yaml:
    * stress/big-int-mul-jit.js: Added.
    * stress/big-int-mul-to-primitive-precedence.js: Added.
    * stress/big-int-mul-to-primitive.js: Added.
    * stress/big-int-mul-type-error.js: Added.
    * stress/big-int-mul-wrapped-value.js: Added.
    * stress/big-int-multiplication.js: Added.
    * stress/big-int-multiply-memory-stress.js: Added.
    
    Source/JavaScriptCore:
    
    Added BigInt support into times binary operator into LLInt and on
    JITOperations profiledMul and unprofiledMul. We are also replacing all
    uses of int to unsigned when there is no negative values for
    variables.
    
    * dfg/DFGConstantFoldingPhase.cpp:
    (JSC::DFG::ConstantFoldingPhase::foldConstants):
    * jit/JITOperations.cpp:
    * runtime/CommonSlowPaths.cpp:
    (JSC::SLOW_PATH_DECL):
    * runtime/JSBigInt.cpp:
    (JSC::JSBigInt::JSBigInt):
    (JSC::JSBigInt::allocationSize):
    (JSC::JSBigInt::createWithLength):
    (JSC::JSBigInt::toString):
    (JSC::JSBigInt::multiply):
    (JSC::JSBigInt::digitDiv):
    (JSC::JSBigInt::internalMultiplyAdd):
    (JSC::JSBigInt::multiplyAccumulate):
    (JSC::JSBigInt::equals):
    (JSC::JSBigInt::absoluteDivSmall):
    (JSC::JSBigInt::calculateMaximumCharactersRequired):
    (JSC::JSBigInt::toStringGeneric):
    (JSC::JSBigInt::rightTrim):
    (JSC::JSBigInt::allocateFor):
    (JSC::JSBigInt::parseInt):
    (JSC::JSBigInt::digit):
    (JSC::JSBigInt::setDigit):
    * runtime/JSBigInt.h:
    * runtime/Operations.h:
    (JSC::jsMul):
    
    
    Canonical link: https://commits.webkit.org/200597@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@231131 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ JSTests/stress/big-int-mul-jit.js
@@ -0,0 +1,19 @@
+//@ runBigIntEnabled
+
+let assert = {
+    sameValue: function(i, e, m) {
+        if (i !== e)
+            throw new Error(m);
+    }
+}
+
+function bigIntMul(x, y) {
+    return x * y;
+}
+noInline(bigIntMul);
+
+for (let i = 0; i < 10000; i++) {
+    let r = bigIntMul(3n, 10n);
+    assert.sameValue(r, 30n, 3n + " * " + 10n + " = " + r);
+}
+
---
+++ JSTests/stress/big-int-multiply-memory-stress.js
@@ -0,0 +1,15 @@
+//@ runBigIntEnabled
+
+function assert(a) {
+    if (!a)
+        throw new Error("Bad assertion");
+}
+
+let a = 0n;
+let b = 1n;
+for (let i = 0; i < 1000000; i++) {
+    a = b * 30n;
+}
+
+assert(a === 30n);
+
