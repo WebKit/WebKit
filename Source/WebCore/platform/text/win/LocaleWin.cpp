88c495f83a796ad56cd4a57d16dba887820a2f52
Yusuke Suzuki <utatane.tea@gmail.com>
2018-05-04 01:50:47 +0000

    Unreviewed, attempt to fix WinCairo build failure https://bugs.webkit.org/show_bug.cgi?id=185218
    * platform/text/win/LocaleWin.cpp:
    (WebCore::LocaleWin::getLocaleInfoString):
    
    Canonical link: https://commits.webkit.org/200777@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@231344 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/text/win/LocaleWin.cpp
+++ Source/WebCore/platform/text/win/LocaleWin.cpp
@@ -95,7 +95,7 @@ String LocaleWin::getLocaleInfoString(LCTYPE type)
     int bufferSizeWithNUL = ::GetLocaleInfo(m_lcid, type, 0, 0);
     if (bufferSizeWithNUL <= 0)
         return String();
-    StringVector<UChar> buffer(bufferSizeWithNUL);
+    Vector<UChar> buffer(bufferSizeWithNUL);
     ::GetLocaleInfo(m_lcid, type, buffer.data(), bufferSizeWithNUL);
     buffer.shrink(bufferSizeWithNUL - 1);
     return String::adopt(WTFMove(buffer));
