1a9ad2be35f67ddc6014d0e08a867933004aad02
Carlos Garcia Campos <carlosgc@webkit.org>
2018-05-07 07:46:08 +0000

    Revert "REGRESSION(r222772): [GTK][WPE] WebProcess from WebKitGtk+ 2.19.9x SIGSEVs in WebKit::WebProcess::ensureNetworkProcessConnection() at Source/WebKit/WebProcess/WebProcess.cpp:1127"
    This reverts commit r230476.

--- Source/WebKit/Platform/IPC/Connection.h
+++ Source/WebKit/Platform/IPC/Connection.h
@@ -152,7 +152,6 @@ public:
 
     void setOnlySendMessagesAsDispatchWhenWaitingForSyncReplyWhenProcessingSuchAMessage(bool);
     void setShouldExitOnSyncMessageSendFailure(bool);
-    bool shouldExitOnSyncMessageSendFailure() const { return m_shouldExitOnSyncMessageSendFailure; }
 
     // The set callback will be called on the connection work queue when the connection is closed, 
     // before didCall is called on the client thread. Must be called before the connection is opened.
