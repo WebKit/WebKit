49dc535972723c02bc74c586423190d4db432d26
Michael Catanzaro <mcatanzaro@gnome.org>
2018-05-09 23:58:53 +0000

    Merge r230450 - [WPE] Don't install JSC C API headers https://bugs.webkit.org/show_bug.cgi?id=184375
    Reviewed by Žan Doberšek.
    
    None of the functions declared in these headers are exported in WPE. Use the new jsc API
    instead.
    
    * PlatformWPE.cmake:

--- Source/JavaScriptCore/PlatformWPE.cmake
+++ Source/JavaScriptCore/PlatformWPE.cmake
@@ -24,19 +24,3 @@ list(APPEND JavaScriptCore_FORWARDING_HEADERS
     inspector/remote/glib/RemoteInspectorServer.h
     inspector/remote/glib/RemoteInspectorUtils.h
 )
-
-set(WPE_INSTALLED_JAVASCRIPTCORE_HEADERS
-    API/JSBase.h
-    API/JSContextRef.h
-    API/JSObjectRef.h
-    API/JSStringRef.h
-    API/JSTypedArray.h
-    API/JSValueRef.h
-    API/JavaScript.h
-    API/WebKitAvailability.h
-)
-
-install(FILES ${WPE_INSTALLED_JAVASCRIPTCORE_HEADERS}
-    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/wpe-${WPE_API_VERSION}/WPE/JavaScriptCore"
-    COMPONENT "Development"
-)
