aab8823a695ccc7c345b84d006baf193b8b86021
Jan Alexander Steffens <jan.steffens@gmail.com>
2018-05-10 06:44:00 +0000

    [GTK] gtk-doc installation subdir duplicated https://bugs.webkit.org/show_bug.cgi?id=185468
    Patch by Jan Alexander Steffens <jan.steffens@gmail.com> on 2018-05-09
    Reviewed by Carlos Garcia Campos.
    
    The GTK docs are installed into a duplicated subdir,
    e.g. /usr/share/doc/gtk-doc/html/webkit2gtk-4.0/webkit2gtk-4.0.
    
    * Source/PlatformGTK.cmake:
    
    Canonical link: https://commits.webkit.org/200987@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@231631 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/PlatformGTK.cmake
+++ Source/PlatformGTK.cmake
@@ -15,13 +15,13 @@ list(APPEND DocumentationDependencies
 
 if (ENABLE_GTKDOC)
     install(DIRECTORY ${CMAKE_BINARY_DIR}/Documentation/webkit2gtk-${WEBKITGTK_API_VERSION}/html/webkit2gtk-${WEBKITGTK_API_VERSION}
-            DESTINATION "${CMAKE_INSTALL_DATADIR}/gtk-doc/html/webkit2gtk-${WEBKITGTK_API_VERSION}"
+            DESTINATION "${CMAKE_INSTALL_DATADIR}/gtk-doc/html"
     )
     install(DIRECTORY ${CMAKE_BINARY_DIR}/Documentation/webkitdomgtk-${WEBKITGTK_API_VERSION}/html/webkitdomgtk-${WEBKITGTK_API_VERSION}
-            DESTINATION "${CMAKE_INSTALL_DATADIR}/gtk-doc/html/webkitdomgtk-${WEBKITGTK_API_VERSION}"
+            DESTINATION "${CMAKE_INSTALL_DATADIR}/gtk-doc/html"
     )
     install(DIRECTORY ${CMAKE_BINARY_DIR}/Documentation/jsc-glib-${WEBKITGTK_API_VERSION}/html/jsc-glib-${WEBKITGTK_API_VERSION}
-            DESTINATION "${CMAKE_INSTALL_DATADIR}/gtk-doc/html/jsc-glib-${WEBKITGTK_API_VERSION}"
+            DESTINATION "${CMAKE_INSTALL_DATADIR}/gtk-doc/html"
     )
 endif ()
 
