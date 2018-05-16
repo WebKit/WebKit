3f4c63e5de2d98f8029dee60b52436778f11fbc0
Fujii Hironori <Hironori.Fujii@sony.com>
2018-05-16 17:54:50 +0000

    [Win] Implement WebPage::handleEditingKeyboardEvent https://bugs.webkit.org/show_bug.cgi?id=185327
    Reviewed by Alexey Proskuryakov.
    
    * WebProcess/WebPage/win/WebPageWin.cpp:
    (WebKit::WebPage::handleEditingKeyboardEvent): Copied from WebKitLegacy.
    
    
    Canonical link: https://commits.webkit.org/201152@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@231853 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/WebPage/win/WebPageWin.cpp
+++ Source/WebKit/WebProcess/WebPage/win/WebPageWin.cpp
@@ -33,6 +33,7 @@
 #include "WebPageProxyMessages.h"
 #include "WebProcess.h"
 #include <WebCore/BackForwardController.h>
+#include <WebCore/Editor.h>
 #include <WebCore/EventHandler.h>
 #include <WebCore/EventNames.h>
 #include <WebCore/FocusController.h>
@@ -260,4 +261,36 @@ const char* WebPage::interpretKeyEvent(const WebCore::KeyboardEvent* evt)
     int mapKey = modifiers << 16 | evt->charCode();
     return mapKey ? keyPressCommandsMap->get(mapKey) : 0;
 }
+
+bool WebPage::handleEditingKeyboardEvent(WebCore::KeyboardEvent* event)
+{
+    auto* frame = downcast<Node>(event->target())->document().frame();
+    ASSERT(frame);
+
+    auto* keyEvent = event->underlyingPlatformEvent();
+    if (!keyEvent || keyEvent->isSystemKey()) // Do not treat this as text input if it's a system key event.
+        return false;
+
+    auto command = frame->editor().command(interpretKeyEvent(event));
+
+    if (keyEvent->type() == PlatformEvent::RawKeyDown) {
+        // WebKit doesn't have enough information about mode to decide
+        // how commands that just insert text if executed via Editor
+        // should be treated, so we leave it upon WebCore to either
+        // handle them immediately (e.g. Tab that changes focus) or
+        // let a keypress event be generated (e.g. Tab that inserts a
+        // Tab character, or Enter).
+        return !command.isTextInsertion() && command.execute(event);
+    }
+
+    if (command.execute(event))
+        return true;
+
+    // Don't insert null or control characters as they can result in unexpected behaviour.
+    if (event->charCode() < ' ')
+        return false;
+
+    return frame->editor().insertText(keyEvent->text(), event);
+}
+
 } // namespace WebKit
