a431a3f04c24d3c64143e59b41d5b256807c1a28
Caio Araujo Neponoceno de Lima <ticaiolima@gmail.com>
2018-05-17 04:27:28 +0000

    [ESNext][BigInt] Implement support for "/" operation https://bugs.webkit.org/show_bug.cgi?id=183996
    Reviewed by Yusuke Suzuki.
    
    JSTests:
    
    * bigIntTests.yaml:
    * stress/big-int-div-jit.js: Added.
    * stress/big-int-div-memory-stress.js: Added.
    * stress/big-int-div-to-primitive-precedence.js: Added.
    * stress/big-int-div-to-primitive.js: Added.
    * stress/big-int-div-type-error.js: Added.
    * stress/big-int-div-wrapped-value.js: Added.
    * stress/big-int-division.js: Added.
    
    Source/JavaScriptCore:
    
    This patch is introducing the support for BigInt into divide
    operation int LLInt and JIT layers.
    
    * dfg/DFGOperations.cpp:
    * runtime/CommonSlowPaths.cpp:
    (JSC::SLOW_PATH_DECL):
    * runtime/JSBigInt.cpp:
    (JSC::JSBigInt::divide):
    (JSC::JSBigInt::copy):
    (JSC::JSBigInt::unaryMinus):
    (JSC::JSBigInt::absoluteCompare):
    (JSC::JSBigInt::absoluteDivLarge):
    (JSC::JSBigInt::productGreaterThan):
    (JSC::JSBigInt::inplaceAdd):
    (JSC::JSBigInt::inplaceSub):
    (JSC::JSBigInt::inplaceRightShift):
    (JSC::JSBigInt::specialLeftShift):
    (JSC::JSBigInt::digit):
    (JSC::JSBigInt::setDigit):
    * runtime/JSBigInt.h:
    
    
    Canonical link: https://commits.webkit.org/201176@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@231886 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ JSTests/stress/big-int-div-to-primitive.js
@@ -0,0 +1,34 @@
+//@ runBigIntEnabled
+
+function assert(a) {
+    if (!a)
+        throw new Error("Bad assertion");
+}
+
+assert.sameValue = function (input, expected, message) {
+    if (input !== expected)
+        throw new Error(message);
+}
+
+function testDiv(x, y, z) {
+    assert.sameValue(x / y, z, x + " / " + y + " = " + z);
+}
+
+let o = {
+    [Symbol.toPrimitive]: function () { return 300000000000n; }
+}
+
+testDiv(500000000000438n, o, 1666n);
+
+o.valueOf = function () {
+    throw new Error("Should never execute it");
+};
+
+testDiv(700000000000438n, o, 2333n);
+
+o.toString = function () {
+    throw new Error("Should never execute it");
+};
+
+testDiv(700000000000438n, o, 2333n);
+
