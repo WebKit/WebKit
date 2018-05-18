2907bfe563302389cb0eb55cb36e0f118bd9a12e
Don Olmstead <don.olmstead@sony.com>
2018-05-18 00:04:59 +0000

    [Curl] Enable HTTP/2 Multiplexing https://bugs.webkit.org/show_bug.cgi?id=185746
    Reviewed by Youenn Fablet.
    
    Covered by existing tests.
    
    * platform/network/curl/CurlContext.cpp:
    (WebCore::CurlMultiHandle::CurlMultiHandle):
    
    
    Canonical link: https://commits.webkit.org/201217@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@231932 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/network/curl/CurlContext.cpp
+++ Source/WebCore/platform/network/curl/CurlContext.cpp
@@ -197,6 +197,9 @@ Lock* CurlShareHandle::mutexFor(curl_lock_data data)
 CurlMultiHandle::CurlMultiHandle()
 {
     m_multiHandle = curl_multi_init();
+
+    if (CurlContext::singleton().isHttp2Enabled())
+        curl_multi_setopt(m_multiHandle, CURLMOPT_PIPELINING, CURLPIPE_MULTIPLEX);
 }
 
 CurlMultiHandle::~CurlMultiHandle()
