8f83461b4e6565399a3c4e3e156dd40817a24edd
Saam Barati <sbarati@apple.com>
2018-05-31 03:57:59 +0000

    DFG combined liveness needs to say that the machine CodeBlock's arguments are live https://bugs.webkit.org/show_bug.cgi?id=186121 <rdar://problem/39377796>
    Reviewed by Keith Miller.
    
    JSTests:
    
    * stress/combined-liveness-needs-to-say-arguments-are-live.js: Added.
    (baz):
    (foo):
    
    Source/JavaScriptCore:
    
    DFG's combined liveness was reporting that the machine CodeBlock's |this|
    argument was dead at certain points in the program. However, a CodeBlock's
    arguments are considered live for the entire function. This fixes a bug
    where object allocation sinking phase skipped materializing an allocation
    because it thought that the argument it was associated with, |this|, was dead.
    
    * dfg/DFGCombinedLiveness.cpp:
    (JSC::DFG::liveNodesAtHead):
    
    
    Canonical link: https://commits.webkit.org/201551@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@232334 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGCombinedLiveness.cpp
+++ Source/JavaScriptCore/dfg/DFGCombinedLiveness.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2016 Apple Inc. All rights reserved.
+ * Copyright (C) 2015-2018 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -44,7 +44,7 @@ NodeSet liveNodesAtHead(Graph& graph, BasicBlock* block)
     }
     
     AvailabilityMap& availabilityMap = block->ssa->availabilityAtHead;
-    graph.forAllLocalsLiveInBytecode(
+    graph.forAllLiveInBytecode(
         block->at(0)->origin.forExit,
         [&] (VirtualRegister reg) {
             availabilityMap.closeStartingWithLocal(
