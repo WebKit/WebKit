ef6834d14cf255dd9ef4a96b05da939b9988b62c
Kocsen Chung <kocsen_chung@apple.com>
2018-06-01 00:19:40 +0000

    Revert r231595. rdar://problem/40050814
    Canonical link: https://commits.webkit.org/197228.609@safari-605-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-605-branch@232381 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/Options.h
+++ Source/JavaScriptCore/runtime/Options.h
@@ -442,6 +442,8 @@ constexpr bool enableAsyncIteration = false;
     \
     v(bool, useICStats, false, Normal, nullptr) \
     \
+    v(unsigned, prototypeHitCountForLLIntCaching, 2, Normal, "Number of prototype property hits before caching a prototype in the LLInt. A count of 0 means never cache.") \
+    \
     v(bool, dumpCompiledRegExpPatterns, false, Normal, nullptr) \
     \
     v(bool, dumpModuleRecord, false, Normal, nullptr) \
