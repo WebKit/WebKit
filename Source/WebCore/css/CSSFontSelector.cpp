9c83f46ffe02f6251b19c5506fb3a2e71442708c
Ryosuke Niwa <rniwa@webkit.org>
2018-06-02 00:21:58 +0000

    ResourceLoader::cancel() shouldn't synchronously fire load event on document https://bugs.webkit.org/show_bug.cgi?id=185284
    Revert the erroneous change to SSFontSelector::beginLoadTimerFired(), which broke CancelLoading.CancelFontSubresource.
    FrameLoader::loadDone calls checkCompleted, not checkLoadComplete so we still need to call checkLoadComplete here.
    
    * css/CSSFontSelector.cpp:
    (WebCore::CSSFontSelector::beginLoadTimerFired):
    
    
    Canonical link: https://commits.webkit.org/201624@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@232433 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/CSSFontSelector.cpp
+++ Source/WebCore/css/CSSFontSelector.cpp
@@ -365,6 +365,10 @@ void CSSFontSelector::beginLoadTimerFired()
         cachedResourceLoader.decrementRequestCount(*fontHandle);
     }
     // Ensure that if the request count reaches zero, the frame loader will know about it.
+    // New font loads may be triggered by layout after the document load is complete but before we have dispatched
+    // didFinishLoading for the frame. Make sure the delegate is always dispatched by checking explicitly.
+    if (m_document && m_document->frame())
+        m_document->frame()->loader().checkLoadComplete();
     cachedResourceLoader.loadDone(LoadCompletionType::Finish);
 }
 
