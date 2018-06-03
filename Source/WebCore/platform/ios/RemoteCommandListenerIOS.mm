031c3d5d1bc4b40c7dcdd6c2775dc4cdfda94717
Eric Carlson <eric.carlson@apple.com>
2018-06-03 20:22:05 +0000

    [iOS] MediaPlayer.framework is not always available https://bugs.webkit.org/show_bug.cgi?id=186220 <rdar://problem/40571286>
    Reviewed by Tim Horton.
    
    * platform/ios/RemoteCommandListenerIOS.mm:
    (WebCore::RemoteCommandListener::create):
    
    
    Canonical link: https://commits.webkit.org/201642@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@232453 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/ios/RemoteCommandListenerIOS.mm
+++ Source/WebCore/platform/ios/RemoteCommandListenerIOS.mm
@@ -33,7 +33,7 @@
 #import <MediaPlayer/MPRemoteCommandEvent.h>
 #import <wtf/SoftLinking.h>
 
-SOFT_LINK_FRAMEWORK(MediaPlayer)
+SOFT_LINK_FRAMEWORK_OPTIONAL(MediaPlayer)
 SOFT_LINK_CLASS(MediaPlayer, MPRemoteCommandCenter)
 SOFT_LINK_CLASS(MediaPlayer, MPSeekCommandEvent)
 SOFT_LINK_CLASS(MediaPlayer, MPChangePlaybackPositionCommandEvent)
@@ -42,6 +42,9 @@ namespace WebCore {
 
 std::unique_ptr<RemoteCommandListener> RemoteCommandListener::create(RemoteCommandListenerClient& client)
 {
+    if (!MediaPlayerLibrary())
+        return nullptr;
+
     return std::make_unique<RemoteCommandListenerIOS>(client);
 }
 
