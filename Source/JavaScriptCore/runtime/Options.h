d2e3aab8201f0dd1d368f68942aae668ac136ee1
Brent Fulgham <bfulgham@apple.com>
2018-06-05 23:31:25 +0000

    Adjust compile and runtime flags to match shippable state of features https://bugs.webkit.org/show_bug.cgi?id=186319 <rdar://problem/40352045>
    Reviewed by Maciej Stachowiak, Jon Lee, and others.
    
    Source/JavaScriptCore:
    
    This patch revises the compile time and runtime state for various features to match their
    suitability for end-user releases.
    
    * Configurations/DebugRelease.xcconfig: Update to match WebKit definition of
    WK_RELOCATABLE_FRAMEWORKS so that ENABLE(EXPERIMENTAL_FEATURES) is defined properly for
    Cocoa builds.
    * Configurations/FeatureDefines.xcconfig: Don't build ENABLE_INPUT_TYPE_COLOR
    or ENABLE_INPUT_TYPE_COLOR_POPOVER.
    * runtime/Options.h: Only enable INTL_NUMBER_FORMAT_TO_PARTS and INTL_PLURAL_RULES
    at runtime for non-production builds.
    
    Source/WebCore:
    
    * Configurations/FeatureDefines.xcconfig: Don't build ENABLE_INPUT_TYPE_COLOR
    or ENABLE_INPUT_TYPE_COLOR_POPOVER.
    
    Source/WebCore/PAL:
    
    * Configurations/DebugRelease.xcconfig: Update to match WebKit definition of
    WK_RELOCATABLE_FRAMEWORKS so that ENABLE(EXPERIMENTAL_FEATURES) is defined properly for
    Cocoa builds.
    * Configurations/FeatureDefines.xcconfig: Don't build ENABLE_INPUT_TYPE_COLOR
    or ENABLE_INPUT_TYPE_COLOR_POPOVER.
    
    Source/WebKit:
    
    Change the default state of various experimental features so that they are off by default in production
    builds, since they are still under development.
    
    Turn 'CacheAPIEnabled' and 'SubresourceIntegrityEnabled' on by default, since both features have been
    shipping for a full cycle and have proven to be stable in production.
    
    * Configurations/FeatureDefines.xcconfig: Don't build ENABLE_INPUT_TYPE_COLOR
    or ENABLE_INPUT_TYPE_COLOR_POPOVER.
    * Shared/WebPreferences.yaml:
    
    Source/WebKitLegacy/mac:
    
    * Configurations/DebugRelease.xcconfig: Don't build ENABLE_INPUT_TYPE_COLOR
    or ENABLE_INPUT_TYPE_COLOR_POPOVER.
    * Configurations/FeatureDefines.xcconfig: Update to match WebKit definition of
    WK_RELOCATABLE_FRAMEWORKS so that ENABLE(EXPERIMENTAL_FEATURES) is defined properly for
    Cocoa builds.
    
    Tools:
    
    This patch revises the compile time and runtime state for various features to match their
    suitability for end-user releases.
    
    * TestWebKitAPI/Configurations/DebugRelease.xcconfig: Update to match WebKit definition of
    WK_RELOCATABLE_FRAMEWORKS so that ENABLE(EXPERIMENTAL_FEATURES) is defined properly for
    Cocoa builds.
    * TestWebKitAPI/Configurations/FeatureDefines.xcconfig: Don't build ENABLE_INPUT_TYPE_COLOR
    or ENABLE_INPUT_TYPE_COLOR_POPOVER.
    
    LayoutTests:
    
    * platform/mac/TestExpectations:
    * platform/mac/imported/w3c/web-platform-tests/html/dom/reflection-forms-expected.txt: Added.
    * platform/mac/imported/w3c/web-platform-tests/html/semantics/forms/constraints/form-validation-validity-badInput-expected.txt: Added.
    * platform/mac/imported/w3c/web-platform-tests/html/semantics/forms/constraints/form-validation-willValidate-expected.txt: Added.
    * platform/mac/imported/w3c/web-platform-tests/html/semantics/forms/textfieldselection/selection-not-application-expected.txt: Added.
    * platform/mac/imported/w3c/web-platform-tests/html/semantics/forms/the-input-element/selection-expected.txt: Added.
    * platform/mac/imported/w3c/web-platform-tests/html/semantics/forms/the-input-element/type-change-state-expected.txt: Added.
    * platform/mac/imported/w3c/web-platform-tests/html/semantics/forms/the-input-element/valueMode-expected.txt: Added.
    * platform/mac/imported/w3c/web-platform-tests/html/semantics/selectors/pseudo-classes/readwrite-readonly-expected.txt: Added.
    
    Canonical link: https://commits.webkit.org/201707@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@232526 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/Options.h
+++ Source/JavaScriptCore/runtime/Options.h
@@ -112,13 +112,13 @@ constexpr bool enableAsyncIteration = true;
 constexpr bool enableAsyncIteration = false;
 #endif
 
-#if ENABLE(INTL_NUMBER_FORMAT_TO_PARTS)
+#if ENABLE(INTL_NUMBER_FORMAT_TO_PARTS) && (!PLATFORM(COCOA) || ENABLE(EXPERIMENTAL_FEATURES))
 constexpr bool enableIntlNumberFormatToParts = true;
 #else
 constexpr bool enableIntlNumberFormatToParts = false;
 #endif
 
-#if ENABLE(INTL_PLURAL_RULES)
+#if ENABLE(INTL_PLURAL_RULES) && (!PLATFORM(COCOA) || ENABLE(EXPERIMENTAL_FEATURES))
 constexpr bool enableIntlPluralRules = true;
 #else
 constexpr bool enableIntlPluralRules = false;
