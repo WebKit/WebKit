a45f42624c888df10f336a0fb8d51015143df388
Miguel Gomez <magomez@igalia.com>
2018-06-08 07:59:08 +0000

    [GTK][WPE] Wrong result when calling ImageBufferCairo's getImageData() https://bugs.webkit.org/show_bug.cgi?id=186384
    Reviewed by Michael Catanzaro.
    
    Fix calculations so the result is the expected one.
    
    * platform/graphics/cairo/ImageBufferCairo.cpp:
    (WebCore::getImageData):
    
    Canonical link: https://commits.webkit.org/201789@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@232618 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/cairo/ImageBufferCairo.cpp
+++ Source/WebCore/platform/graphics/cairo/ImageBufferCairo.cpp
@@ -349,11 +349,28 @@ RefPtr<cairo_surface_t> copySurfaceToImageAndAdjustRect(cairo_surface_t* surface
 template <AlphaPremultiplication premultiplied>
 RefPtr<Uint8ClampedArray> getImageData(const IntRect& rect, const IntRect& logicalRect, const ImageBufferData& data, const IntSize& size, const IntSize& logicalSize, float resolutionScale)
 {
-    auto result = Uint8ClampedArray::createUninitialized(rect.width() * rect.height() * 4);
+    // The area can overflow if the rect is too big.
+    Checked<unsigned, RecordOverflow> area = 4;
+    area *= rect.width();
+    area *= rect.height();
+    if (area.hasOverflowed())
+        return nullptr;
+
+    auto result = Uint8ClampedArray::createUninitialized(area.unsafeGet());
     if (!result)
         return nullptr;
 
-    if (rect.x() < 0 || rect.y() < 0 || (rect.x() + rect.width()) > size.width() || (rect.y() + rect.height()) > size.height())
+    // Can overflow, as we are adding 2 ints.
+    int endx = 0;
+    if (!WTF::safeAdd(rect.x(), rect.width(), endx))
+        return nullptr;
+
+    // Can overflow, as we are adding 2 ints.
+    int endy = 0;
+    if (!WTF::safeAdd(rect.y(), rect.height(), endy))
+        return nullptr;
+
+    if (rect.x() < 0 || rect.y() < 0 || endx > size.width() || endy > size.height())
         result->zeroFill();
 
     int originx = rect.x();
@@ -362,7 +379,7 @@ RefPtr<Uint8ClampedArray> getImageData(const IntRect& rect, const IntRect& logic
         destx = -originx;
         originx = 0;
     }
-    int endx = rect.maxX();
+
     if (endx > size.width())
         endx = size.width();
     int numColumns = endx - originx;
@@ -373,10 +390,14 @@ RefPtr<Uint8ClampedArray> getImageData(const IntRect& rect, const IntRect& logic
         desty = -originy;
         originy = 0;
     }
-    int endy = rect.maxY();
+
     if (endy > size.height())
         endy = size.height();
     int numRows = endy - originy;
+
+    // Nothing will be copied, so just return the result.
+    if (numColumns <= 0 || numRows <= 0)
+        return result;
 
     // The size of the derived surface is in BackingStoreCoordinateSystem.
     // We need to set the device scale for the derived surface from this ImageBuffer.
