3ef74eb1cfc7f06856738efeef458245dea89962
Žan Doberšek <zdobersek@igalia.com>
2018-06-27 08:13:10 +0000

    [GCrypt] Move definitions of GCryptUtilities helpers into a separate source file https://bugs.webkit.org/show_bug.cgi?id=187033
    Reviewed by Michael Catanzaro.
    
    Move the GCryptUtilities helpers that operate on libgcrypt values and
    constants into a separate source file. This limits a bit the amount of
    inlining the compiler might feel compelled to do, and the resulting
    shared library is 8kB smaller in size.
    
    * crypto/gcrypt/GCryptUtilities.cpp: Copied from Source/WebCore/crypto/gcrypt/GCryptUtilities.h.
    (WebCore::hmacAlgorithm):
    (WebCore::digestAlgorithm):
    (WebCore::hashCryptoDigestAlgorithm):
    (WebCore::mpiLength):
    (WebCore::mpiData):
    (WebCore::mpiZeroPrefixedData):
    (WebCore::mpiSignedData):
    * crypto/gcrypt/GCryptUtilities.h:
    (WebCore::hmacAlgorithm): Deleted.
    (WebCore::digestAlgorithm): Deleted.
    (WebCore::hashCryptoDigestAlgorithm): Deleted.
    (WebCore::mpiLength): Deleted.
    (WebCore::mpiData): Deleted.
    (WebCore::mpiZeroPrefixedData): Deleted.
    (WebCore::mpiSignedData): Deleted.
    * platform/SourcesGCrypt.txt:
    
    
    Canonical link: https://commits.webkit.org/202336@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@233247 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/crypto/gcrypt/GCryptUtilities.h
+++ Source/WebCore/crypto/gcrypt/GCryptUtilities.h
@@ -67,181 +67,19 @@ static inline bool matches(const void* lhs, size_t size, const std::array<uint8_
 
 } // namespace CryptoConstants
 
-static inline std::optional<const char*> hashAlgorithmName(CryptoAlgorithmIdentifier identifier)
-{
-    switch (identifier) {
-    case CryptoAlgorithmIdentifier::SHA_1:
-        return "sha1";
-    case CryptoAlgorithmIdentifier::SHA_224:
-        return "sha224";
-    case CryptoAlgorithmIdentifier::SHA_256:
-        return "sha256";
-    case CryptoAlgorithmIdentifier::SHA_384:
-        return "sha384";
-    case CryptoAlgorithmIdentifier::SHA_512:
-        return "sha512";
-    default:
-        return std::nullopt;
-    }
-}
+std::optional<const char*> hashAlgorithmName(CryptoAlgorithmIdentifier);
 
-static inline std::optional<int> hmacAlgorithm(CryptoAlgorithmIdentifier identifier)
-{
-    switch (identifier) {
-    case CryptoAlgorithmIdentifier::SHA_1:
-        return GCRY_MAC_HMAC_SHA1;
-    case CryptoAlgorithmIdentifier::SHA_224:
-        return GCRY_MAC_HMAC_SHA224;
-    case CryptoAlgorithmIdentifier::SHA_256:
-        return GCRY_MAC_HMAC_SHA256;
-    case CryptoAlgorithmIdentifier::SHA_384:
-        return GCRY_MAC_HMAC_SHA384;
-    case CryptoAlgorithmIdentifier::SHA_512:
-        return GCRY_MAC_HMAC_SHA512;
-    default:
-        return std::nullopt;
-    }
-}
+std::optional<int> hmacAlgorithm(CryptoAlgorithmIdentifier);
+std::optional<int> digestAlgorithm(CryptoAlgorithmIdentifier);
+std::optional<PAL::CryptoDigest::Algorithm> hashCryptoDigestAlgorithm(CryptoAlgorithmIdentifier);
 
-static inline std::optional<int> digestAlgorithm(CryptoAlgorithmIdentifier identifier)
-{
-    switch (identifier) {
-    case CryptoAlgorithmIdentifier::SHA_1:
-        return GCRY_MD_SHA1;
-    case CryptoAlgorithmIdentifier::SHA_224:
-        return GCRY_MD_SHA224;
-    case CryptoAlgorithmIdentifier::SHA_256:
-        return GCRY_MD_SHA256;
-    case CryptoAlgorithmIdentifier::SHA_384:
-        return GCRY_MD_SHA384;
-    case CryptoAlgorithmIdentifier::SHA_512:
-        return GCRY_MD_SHA512;
-    default:
-        return std::nullopt;
-    }
-}
-
-static inline std::optional<PAL::CryptoDigest::Algorithm> hashCryptoDigestAlgorithm(CryptoAlgorithmIdentifier identifier)
-{
-    switch (identifier) {
-    case CryptoAlgorithmIdentifier::SHA_1:
-        return PAL::CryptoDigest::Algorithm::SHA_1;
-    case CryptoAlgorithmIdentifier::SHA_224:
-        return PAL::CryptoDigest::Algorithm::SHA_224;
-    case CryptoAlgorithmIdentifier::SHA_256:
-        return PAL::CryptoDigest::Algorithm::SHA_256;
-    case CryptoAlgorithmIdentifier::SHA_384:
-        return PAL::CryptoDigest::Algorithm::SHA_384;
-    case CryptoAlgorithmIdentifier::SHA_512:
-        return PAL::CryptoDigest::Algorithm::SHA_512;
-    default:
-        return std::nullopt;
-    }
-}
-
-static inline std::optional<size_t> mpiLength(gcry_mpi_t paramMPI)
-{
-    // Retrieve the MPI length for the unsigned format.
-    size_t dataLength = 0;
-    gcry_error_t error = gcry_mpi_print(GCRYMPI_FMT_USG, nullptr, 0, &dataLength, paramMPI);
-    if (error != GPG_ERR_NO_ERROR) {
-        PAL::GCrypt::logError(error);
-        return std::nullopt;
-    }
-
-    return dataLength;
-}
-
-static inline std::optional<size_t> mpiLength(gcry_sexp_t paramSexp)
-{
-    // Retrieve the MPI value stored in the s-expression: (name mpi-data)
-    PAL::GCrypt::Handle<gcry_mpi_t> paramMPI(gcry_sexp_nth_mpi(paramSexp, 1, GCRYMPI_FMT_USG));
-    if (!paramMPI)
-        return std::nullopt;
-
-    return mpiLength(paramMPI);
-}
-
-static inline std::optional<Vector<uint8_t>> mpiData(gcry_mpi_t paramMPI)
-{
-    // Retrieve the MPI length.
-    auto length = mpiLength(paramMPI);
-    if (!length)
-        return std::nullopt;
-
-    // Copy the MPI data into a properly-sized buffer.
-    Vector<uint8_t> output(*length);
-    gcry_error_t error = gcry_mpi_print(GCRYMPI_FMT_USG, output.data(), output.size(), nullptr, paramMPI);
-    if (error != GPG_ERR_NO_ERROR) {
-        PAL::GCrypt::logError(error);
-        return std::nullopt;
-    }
-
-    return output;
-}
-
-static inline std::optional<Vector<uint8_t>> mpiZeroPrefixedData(gcry_mpi_t paramMPI, size_t targetLength)
-{
-    // Retrieve the MPI length. Bail if the retrieved length is longer than target length.
-    auto length = mpiLength(paramMPI);
-    if (!length || *length > targetLength)
-        return std::nullopt;
-
-    // Fill out the output buffer with zeros. Properly determine the zero prefix length,
-    // and copy the MPI data into memory area following the prefix (if any).
-    Vector<uint8_t> output(targetLength, 0);
-    size_t prefixLength = targetLength - *length;
-    gcry_error_t error = gcry_mpi_print(GCRYMPI_FMT_USG, output.data() + prefixLength, targetLength, nullptr, paramMPI);
-    if (error != GPG_ERR_NO_ERROR) {
-        PAL::GCrypt::logError(error);
-        return std::nullopt;
-    }
-
-    return output;
-}
-
-static inline std::optional<Vector<uint8_t>> mpiData(gcry_sexp_t paramSexp)
-{
-    // Retrieve the MPI value stored in the s-expression: (name mpi-data)
-    PAL::GCrypt::Handle<gcry_mpi_t> paramMPI(gcry_sexp_nth_mpi(paramSexp, 1, GCRYMPI_FMT_USG));
-    if (!paramMPI)
-        return std::nullopt;
-
-    return mpiData(paramMPI);
-}
-
-static inline std::optional<Vector<uint8_t>> mpiZeroPrefixedData(gcry_sexp_t paramSexp, size_t targetLength)
-{
-    // Retrieve the MPI value stored in the s-expression: (name mpi-data)
-    PAL::GCrypt::Handle<gcry_mpi_t> paramMPI(gcry_sexp_nth_mpi(paramSexp, 1, GCRYMPI_FMT_USG));
-    if (!paramMPI)
-        return std::nullopt;
-
-    return mpiZeroPrefixedData(paramMPI, targetLength);
-}
-
-static inline std::optional<Vector<uint8_t>> mpiSignedData(gcry_mpi_t mpi)
-{
-    auto data = mpiData(mpi);
-    if (!data)
-        return std::nullopt;
-
-    if (data->at(0) & 0x80)
-        data->insert(0, 0x00);
-
-    return data;
-}
-
-static inline std::optional<Vector<uint8_t>> mpiSignedData(gcry_sexp_t paramSexp)
-{
-    auto data = mpiData(paramSexp);
-    if (!data)
-        return std::nullopt;
-
-    if (data->at(0) & 0x80)
-        data->insert(0, 0x00);
-
-    return data;
-}
+std::optional<size_t> mpiLength(gcry_mpi_t);
+std::optional<size_t> mpiLength(gcry_sexp_t);
+std::optional<Vector<uint8_t>> mpiData(gcry_mpi_t);
+std::optional<Vector<uint8_t>> mpiZeroPrefixedData(gcry_mpi_t, size_t targetLength);
+std::optional<Vector<uint8_t>> mpiData(gcry_sexp_t);
+std::optional<Vector<uint8_t>> mpiZeroPrefixedData(gcry_sexp_t, size_t targetLength);
+std::optional<Vector<uint8_t>> mpiSignedData(gcry_mpi_t);
+std::optional<Vector<uint8_t>> mpiSignedData(gcry_sexp_t);
 
 } // namespace WebCore
