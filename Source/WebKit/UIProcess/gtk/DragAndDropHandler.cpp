4dd8672354e5ec6b7a53c3d205bcc0b3ae5c81d0
Michael Catanzaro <mcatanzaro@igalia.com>
2018-06-28 15:35:42 +0000

    [GTK] ASSERTION FAILED: !HashTranslator::equal(KeyTraits::emptyValue(), key) when dragging file into webview https://bugs.webkit.org/show_bug.cgi?id=175602
    Reviewed by Carlos Garcia Campos.
    
    We check using the GdkDragContext to ensure the DroppingContext is still alive (present in
    m_droppingContexts), but access it via the pointer to the DroppingContext that could be
    dangling. This happens on every drag. I can't actually reproduce the original assertion
    since I'm currently working with an asan build, but I imagine it's probably the same issue
    that I'm fixing here.
    
    * UIProcess/gtk/DragAndDropHandler.cpp:
    (WebKit::DragAndDropHandler::dragLeave):
    
    
    Canonical link: https://commits.webkit.org/202392@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@233305 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/gtk/DragAndDropHandler.cpp
+++ Source/WebKit/UIProcess/gtk/DragAndDropHandler.cpp
@@ -257,8 +257,8 @@ void DragAndDropHandler::dragLeave(GdkDragContext* context)
     // During a drop GTK+ will fire a drag-leave signal right before firing
     // the drag-drop signal. We want the actions for drag-leave to happen after
     // those for drag-drop, so schedule them to happen asynchronously here.
-    RunLoop::main().dispatch([this, droppingContext]() {
-        auto it = m_droppingContexts.find(droppingContext->gdkContext);
+    RunLoop::main().dispatch([this, context, droppingContext]() {
+        auto it = m_droppingContexts.find(context);
         if (it == m_droppingContexts.end())
             return;
 
