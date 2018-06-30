466cccec4c5976e6aedb971a73029eed9e71fe48
Frédéric Wang <fwang@igalia.com>
2018-06-30 06:42:04 +0000

    WTF's internal std::optional implementation should abort() on bad optional access https://bugs.webkit.org/show_bug.cgi?id=186536
    Patch by Frederic Wang <fwang@igalia.com> on 2018-06-29
    Reviewed by Michael Catanzaro.
    
    Source/WTF:
    
    Currently, some ports built with recent compilers will cause the program to abort when one
    tries to access the value of an unset std:optional (i.e. std::nullopt) as specified by C++17.
    However, most ports still use WTF's internal std::optional implementation, which does not
    verify illegal access. Hence it's not possible for developers working on these ports to
    detect issues like bugs #186189, #186535, #186752, #186753, #187139 or #187145. WTF's version
    of std::optional was introduced in bug #164199 but it was not possible to verify the
    availability of the value inside constexpr member functions because the assert might involve
    asm declarations. This commit introduces a new RELEASE_ASSERT_UNDER_CONSTEXPR_CONTEXT macro
    (a simplified version of RELEASE_ASSERT that can be used in constexpr context) and uses it in
    WTF's implementation of std::optional.
    
    * wtf/Assertions.h: Define RELEASE_ASSERT_UNDER_CONSTEXPR_CONTEXT as a version of
    RELEASE_ASSERT that can be used in constexpr context (in particular avoids asm declarations).
    * wtf/Optional.h:
    (std::optional::operator ->): Add an assert to ensure the optional value is available.
    (std::optional::operator *): Ditto.
    (std::optional::value const): Ditto.
    (std::optional::value): Ditto.
    (std::optional<T::value const): Ditto.
    
    LayoutTests:
    
    * TestExpectations: Mark one WebAnimations test as crashing (bug #187145).
    
    Canonical link: https://commits.webkit.org/202476@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@233391 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/Assertions.h
+++ Source/WTF/wtf/Assertions.h
@@ -220,6 +220,12 @@ WTF_EXPORT_PRIVATE bool WTFIsDebuggerAttached(void);
 #define WTFBreakpointTrap() WTFCrash() // Not implemented.
 #endif
 
+#if COMPILER(MSVC)
+#define WTFBreakpointTrapUnderConstexprContext() __debugbreak()
+#else
+#define WTFBreakpointTrapUnderConstexprContext() __builtin_trap()
+#endif
+
 #ifndef CRASH
 
 #if defined(NDEBUG) && OS(DARWIN)
@@ -503,6 +509,27 @@ WTF_EXPORT_PRIVATE NO_RETURN_DUE_TO_CRASH void WTFCrashWithSecurityImplication(v
 #define RELEASE_ASSERT_WITH_MESSAGE(assertion, ...) ASSERT_WITH_MESSAGE(assertion, __VA_ARGS__)
 #define RELEASE_ASSERT_WITH_SECURITY_IMPLICATION(assertion) ASSERT_WITH_SECURITY_IMPLICATION(assertion)
 #define RELEASE_ASSERT_NOT_REACHED() ASSERT_NOT_REACHED()
+#endif
+
+/* RELEASE_ASSERT_UNDER_CONSTEXPR_CONTEXT
+
+   This is a special version of RELEASE_ASSERT(assertion) that can be used in constexpr contexts.
+*/
+#if ASSERT_DISABLED
+#define RELEASE_ASSERT_UNDER_CONSTEXPR_CONTEXT(assertion) do { \
+    if (UNLIKELY(!(assertion))) { \
+        WTFBreakpointTrapUnderConstexprContext(); \
+        __builtin_unreachable(); \
+    } \
+} while (0)
+#else
+#define RELEASE_ASSERT_UNDER_CONSTEXPR_CONTEXT(assertion) do { \
+    if (!(assertion)) { \
+        WTFReportAssertionFailure(__FILE__, __LINE__, WTF_PRETTY_FUNCTION, #assertion); \
+        WTFBreakpointTrapUnderConstexprContext(); \
+        __builtin_unreachable(); \
+    } \
+} while (0)
 #endif
 
 /* UNREACHABLE_FOR_PLATFORM */
