WebKit/WebKit

commit 09c7d244971a620bc3206531dc3db0f97c5c7055

Early return when handling fetch event in case service worker origin does not match origin of a subresource load https://bugs.webkit.org/show_bug.cgi?id=187153 <rdar://problem/41329832>
Reviewed by Chris Dumez.

Stop crashing the service worker process in case a subresource load origin is not matching a service worker origin.
Instead, just return early so that the load will be handled by the network process.

Keep crashing in case a navigation load is not matching its service worker origin.
Add more logging to help with the debugging.

* WebProcess/Storage/WebSWContextManagerConnection.cpp:
(WebKit::logValidFetchError):
(WebKit::isValidFetch):
(WebKit::WebSWContextManagerConnection::startFetch):


Canonical link: https://commits.webkit.org/202422@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@233335 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/WebProcess/Storage/WebSWContextManagerConnection.cpp
@@ -189,15 +189,24 @@ static inline bool isValidFetch(const ResourceRequest& request, const FetchOptio
     if (!serviceWorkerURL.protocolIsInHTTPFamily())
         return true;
 
-    if (options.mode == FetchOptions::Mode::Navigate)
-        return protocolHostAndPortAreEqual(request.url(), serviceWorkerURL);
+    if (options.mode == FetchOptions::Mode::Navigate && !protocolHostAndPortAreEqual(request.url(), serviceWorkerURL)) {
+        RELEASE_LOG_ERROR(ServiceWorker, "Should not intercept a navigation load that is not same-origin as the service worker URL");
+        RELEASE_ASSERT_WITH_MESSAGE(request.url().host() == serviceWorkerURL.host(), "Hosts do not match");
+        RELEASE_ASSERT_WITH_MESSAGE(request.url().protocol() == serviceWorkerURL.protocol(), "Protocols do not match");
+        RELEASE_ASSERT_WITH_MESSAGE(request.url().port() == serviceWorkerURL.port(), "Ports do not match");
+        return false;
+    }
 
     String origin = request.httpOrigin();
     URL url { URL(), origin.isEmpty() ? referrer : origin };
-    if (!url.protocolIsInHTTPFamily())
-        return true;
-
-    return protocolHostAndPortAreEqual(url, serviceWorkerURL);
+    if (url.protocolIsInHTTPFamily() && !protocolHostAndPortAreEqual(url, serviceWorkerURL)) {
+        RELEASE_LOG_ERROR(ServiceWorker, "Should not intercept a non navigation load that is not originating from a same-origin context as the service worker URL");
+        ASSERT(url.host() == serviceWorkerURL.host());
+        ASSERT(url.protocol() == serviceWorkerURL.protocol());
+        ASSERT(url.port() == serviceWorkerURL.port());
+        return false;
+    }
+    return true;
 }
 
 void WebSWContextManagerConnection::cancelFetch(SWServerConnectionIdentifier serverConnectionIdentifier, ServiceWorkerIdentifier serviceWorkerIdentifier, FetchIdentifier fetchIdentifier)
@@ -214,7 +223,10 @@ void WebSWContextManagerConnection::startFetch(SWServerConnectionIdentifier serv
         return;
     }
 
-    RELEASE_ASSERT(isValidFetch(request, options, serviceWorkerThreadProxy->scriptURL(), referrer));
+    if (!isValidFetch(request, options, serviceWorkerThreadProxy->scriptURL(), referrer)) {
+        m_connectionToStorageProcess->send(Messages::StorageProcess::DidNotHandleFetch { serverConnectionIdentifier, fetchIdentifier }, 0);
+        return;
+    }
 
     auto client = WebServiceWorkerFetchTaskClient::create(m_connectionToStorageProcess.copyRef(), serviceWorkerIdentifier, serverConnectionIdentifier, fetchIdentifier);
     std::optional<ServiceWorkerClientIdentifier> clientId;

commit 2e62fee725673d831d666942c6094283187d0a5d

Fix regression introduced in r233335 https://bugs.webkit.org/show_bug.cgi?id=187282
Reviewed by Chris Dumez.

When checking for navigation loads, we need to return whether the request URL matches the service worker URL.
Before this patch, if the request URL was not matching the service worker URL, we were using the origin/referrer
which should only be used for subresource loads.

Covered by imported/w3c/web-platform-tests/service-workers/service-worker/claim-with-redirect.https.html
being no longer flaky.

* WebProcess/Storage/WebSWContextManagerConnection.cpp:
(WebKit::isValidFetch):


Canonical link: https://commits.webkit.org/202576@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@233491 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/WebProcess/Storage/WebSWContextManagerConnection.cpp
@@ -189,12 +189,15 @@ static inline bool isValidFetch(const ResourceRequest& request, const FetchOptio
     if (!serviceWorkerURL.protocolIsInHTTPFamily())
         return true;
 
-    if (options.mode == FetchOptions::Mode::Navigate && !protocolHostAndPortAreEqual(request.url(), serviceWorkerURL)) {
-        RELEASE_LOG_ERROR(ServiceWorker, "Should not intercept a navigation load that is not same-origin as the service worker URL");
-        RELEASE_ASSERT_WITH_MESSAGE(request.url().host() == serviceWorkerURL.host(), "Hosts do not match");
-        RELEASE_ASSERT_WITH_MESSAGE(request.url().protocol() == serviceWorkerURL.protocol(), "Protocols do not match");
-        RELEASE_ASSERT_WITH_MESSAGE(request.url().port() == serviceWorkerURL.port(), "Ports do not match");
-        return false;
+    if (options.mode == FetchOptions::Mode::Navigate) {
+        if (!protocolHostAndPortAreEqual(request.url(), serviceWorkerURL)) {
+            RELEASE_LOG_ERROR(ServiceWorker, "Should not intercept a navigation load that is not same-origin as the service worker URL");
+            RELEASE_ASSERT_WITH_MESSAGE(request.url().host() == serviceWorkerURL.host(), "Hosts do not match");
+            RELEASE_ASSERT_WITH_MESSAGE(request.url().protocol() == serviceWorkerURL.protocol(), "Protocols do not match");
+            RELEASE_ASSERT_WITH_MESSAGE(request.url().port() == serviceWorkerURL.port(), "Ports do not match");
+            return false;
+        }
+        return true;
     }
 
     String origin = request.httpOrigin();