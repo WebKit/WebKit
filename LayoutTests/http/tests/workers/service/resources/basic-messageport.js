712e2696c8693f3114a076a267f8dfa1cda2fb7a
Youenn Fablet <youenn@apple.com>
2018-07-06 00:43:20 +0000

    Layout Test http/tests/workers/service/basic-messageport.html is flaky https://bugs.webkit.org/show_bug.cgi?id=182630
    Reviewed by Alex Christensen.
    
    Test is flaky due to a race between serviceWorkerClient.postMessage and MessagePort.postMessage.
    Both are going through UIProcess but MessagePort events are grabbed asynchronously.
    Fixed the test to not expect order between these two message exchange channels.
    
    * http/tests/workers/service/basic-messageport-expected.txt:
    * http/tests/workers/service/resources/basic-messageport.js:
    (channel.port1.onmessage):
    (then):
    * platform/mac-wk2/TestExpectations:
    
    
    Canonical link: https://commits.webkit.org/202633@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@233554 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/workers/service/resources/basic-messageport.js
+++ LayoutTests/http/tests/workers/service/resources/basic-messageport.js
@@ -1,19 +1,31 @@
+var messages = [];
+
+function doLog(message)
+{
+    messages.push(message)
+    if (messages.length === 3) {
+        messages.sort();
+        for (let message of messages)
+            log(message);
+        finishSWTest();
+    }
+}
+
 navigator.serviceWorker.addEventListener("message", function(event) {
-    log("Message received from ServiceWorker: " + event.data);
+    doLog("Message received from ServiceWorker: " + event.data);
 });
 
 var channel = new MessageChannel;
 channel.port1.onmessage = function(event) {
-	log("Message received from MessagePort: " + event.data);
-	finishSWTest();
+    doLog("Message received from MessagePort: " + event.data);
 }
 
 navigator.serviceWorker.register("resources/messageport-echo-worker.js", { }).then(function(registration) {
-	try {
-    	registration.installing.postMessage("Here is your port", [channel.port2]);
+    try {
+        registration.installing.postMessage("Here is your port", [channel.port2]);
         channel.port1.postMessage("Message to a port!");
-	} catch(e) {
-		log("Exception: " + e);
-		finishSWTest();
-	}
+    } catch(e) {
+        doLog("Exception: " + e);
+        finishSWTest();
+    }
 });
