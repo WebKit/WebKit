eea2a4973d14ae6299df061d8748a84473729421
Chris Dumez <cdumez@apple.com>
2018-07-12 22:21:39 +0000

    Assert that the IPC::Connection is valid in Connection::dispatchMessage(Decoder&) https://bugs.webkit.org/show_bug.cgi?id=187617
    Reviewed by Youenn Fablet.
    
    Assert that the IPC::Connection is valid in Connection::dispatchMessage(Decoder&) as
    m_client would be stale otherwise.
    
    * Platform/IPC/Connection.cpp:
    (IPC::Connection::dispatchMessage):
    
    
    Canonical link: https://commits.webkit.org/202834@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@233783 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Platform/IPC/Connection.cpp
+++ Source/WebKit/Platform/IPC/Connection.cpp
@@ -936,6 +936,7 @@ void Connection::enqueueIncomingMessage(std::unique_ptr<Decoder> incomingMessage
 
 void Connection::dispatchMessage(Decoder& decoder)
 {
+    ASSERT(isValid());
     m_client.didReceiveMessage(*this, decoder);
 }
 
