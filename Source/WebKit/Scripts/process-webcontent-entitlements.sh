7c6d8bac7a3b17020c16bb63902723bd083f9e92
Dan Bernstein <mitz@apple.com>
2018-07-13 17:46:30 +0000

    [macOS] REGRESSION (r233536): Development WebContent service got opted back into Library Validation https://bugs.webkit.org/show_bug.cgi?id=187640
    Reviewed by Daniel Bates.
    
    * Scripts/process-webcontent-entitlements.sh: Add the
      com.apple.security.cs.disable-library-validation to the Development service regardless of
      whether restricted entitlements are to be used, because that entitlement is not restricted.
    
    
    Canonical link: https://commits.webkit.org/202855@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@233804 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Scripts/process-webcontent-entitlements.sh
+++ Source/WebKit/Scripts/process-webcontent-entitlements.sh
@@ -17,10 +17,10 @@ if [[ ${WK_PLATFORM_NAME} == "macosx" ]]; then
             echo "Adding domain extension entitlement for relocatable build.";
             /usr/libexec/PlistBuddy -c "Add :com.apple.private.xpc.domain-extension bool YES" "${PROCESSED_XCENT_FILE}";
         fi
+    fi
 
-        if [[ ${WK_XPC_SERVICE_VARIANT} == "Development" ]]; then
-            echo "Disabling library validation for development build.";
-            /usr/libexec/PlistBuddy -c "Add :com.apple.security.cs.disable-library-validation bool YES" "${PROCESSED_XCENT_FILE}";
-        fi
+    if [[ ${WK_XPC_SERVICE_VARIANT} == "Development" ]]; then
+        echo "Disabling library validation for development build.";
+        /usr/libexec/PlistBuddy -c "Add :com.apple.security.cs.disable-library-validation bool YES" "${PROCESSED_XCENT_FILE}";
     fi
 fi
