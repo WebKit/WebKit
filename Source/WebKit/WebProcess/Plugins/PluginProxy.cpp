b0c0db64aee4232b2bd90655c8da99abab1d88de
Chris Dumez <cdumez@apple.com>
2018-07-19 02:00:37 +0000

    Cherry-pick r233904. rdar://problem/42345140
        RELEASE_ASSERT() under IPC::Connection::sendSync() from PluginProxy::supportsSnapshotting()
        https://bugs.webkit.org/show_bug.cgi?id=187740
        <rdar://problem/41818955>
    
        Reviewed by Youenn Fablet.
    
        As per the crash trace, PluginProxy::supportsSnapshotting() can be called during layout but does synchronous
        IPC. As a result, we need to prevent WebCore re-entrancy by using DoNotProcessIncomingMessagesWhenWaitingForSyncReply
        sendOption.
    
        * WebProcess/Plugins/PluginProxy.cpp:
        (WebKit::PluginProxy::supportsSnapshotting const):
    
        Canonical link: https://commits.webkit.org/202949@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@233904 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/202900.22@safari-606-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-606-branch@233965 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/Plugins/PluginProxy.cpp
+++ Source/WebKit/WebProcess/Plugins/PluginProxy.cpp
@@ -234,7 +234,7 @@ bool PluginProxy::supportsSnapshotting() const
         return false;
 
     bool isSupported = false;
-    if (m_connection && !m_connection->connection()->sendSync(Messages::PluginControllerProxy::SupportsSnapshotting(), Messages::PluginControllerProxy::SupportsSnapshotting::Reply(isSupported), m_pluginInstanceID))
+    if (m_connection && !m_connection->connection()->sendSync(Messages::PluginControllerProxy::SupportsSnapshotting(), Messages::PluginControllerProxy::SupportsSnapshotting::Reply(isSupported), m_pluginInstanceID, Seconds::infinity(), IPC::SendSyncOption::DoNotProcessIncomingMessagesWhenWaitingForSyncReply))
         return false;
 
     return isSupported;
