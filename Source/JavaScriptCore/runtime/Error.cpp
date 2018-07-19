97ebf2624a5e81692adbd2dc772458766f531553
Commit Queue <commit-queue@webkit.org>
2018-07-19 19:43:00 +0000

    Unreviewed, rolling out r233998. https://bugs.webkit.org/show_bug.cgi?id=187815
    Not needed. (Requested by mlam|a on #webkit).
    
    Reverted changeset:
    
    "Temporarily mitigate a bug where a source provider is null
    when it shouldn't be."
    https://bugs.webkit.org/show_bug.cgi?id=187812
    https://trac.webkit.org/changeset/233998
    
    Canonical link: https://commits.webkit.org/203005@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@233999 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/Error.cpp
+++ Source/JavaScriptCore/runtime/Error.cpp
@@ -239,6 +239,7 @@ void addErrorInfo(ExecState* exec, JSObject* obj, bool useCurrentFrame)
 JSObject* addErrorInfo(CallFrame* callFrame, JSObject* error, int line, const SourceCode& source)
 {
     VM& vm = callFrame->vm();
+    const String& sourceURL = source.provider()->url();
     
     // The putDirect() calls below should really be put() so that they trigger materialization of
     // the line/sourceURL properties. Otherwise, what we set here will just be overwritten later.
@@ -255,15 +256,8 @@ JSObject* addErrorInfo(CallFrame* callFrame, JSObject* error, int line, const So
     // https://bugs.webkit.org/show_bug.cgi?id=176673
     if (line != -1)
         error->putDirect(vm, vm.propertyNames->line, jsNumber(line));
-
-    SourceProvider* provider = source.provider();
-    // FIXME: Remove this ASSERT and null check when https://webkit.org/b/187811 is fixed.
-    ASSERT(provider);
-    if (LIKELY(provider)) {
-        const String& sourceURL = provider->url();
-        if (!sourceURL.isNull())
-            error->putDirect(vm, vm.propertyNames->sourceURL, jsString(&vm, sourceURL));
-    }
+    if (!sourceURL.isNull())
+        error->putDirect(vm, vm.propertyNames->sourceURL, jsString(&vm, sourceURL));
     return error;
 }
 
