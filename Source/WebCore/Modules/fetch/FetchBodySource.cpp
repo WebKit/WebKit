46e9adfce95fe1444caac3e13bf7aba1157f4526
Youenn Fablet <youennf@gmail.com>
2018-07-20 17:17:13 +0000

    Cherry-pick r234045. rdar://problem/42434024
        FetchResponse should close its stream when loading finishes
        https://bugs.webkit.org/show_bug.cgi?id=187790
    
        Reviewed by Chris Dumez.
    
        It simplifies for a FetchResponse to push all its data into its stream if already created at end of load time.
        Did some refactoring in FetchBodyOwner to have a cleaner relationship with the stream source.
        Did a minor refactoring to expose the error description when loading fails as part of the rejected promise.
        This is consistent to errors sent back through callbacks.
    
        Covered by existing tests.
    
        * Modules/fetch/FetchBodyOwner.cpp:
        (WebCore::FetchBodyOwner::~FetchBodyOwner):
        * Modules/fetch/FetchBodyOwner.h:
        * Modules/fetch/FetchBodySource.cpp:
        (WebCore::FetchBodySource::FetchBodySource):
        (WebCore::FetchBodySource::setActive):
        (WebCore::FetchBodySource::setInactive):
        (WebCore::FetchBodySource::doStart):
        (WebCore::FetchBodySource::doPull):
        (WebCore::FetchBodySource::doCancel):
        (WebCore::FetchBodySource::cleanBodyOwner):
        * Modules/fetch/FetchBodySource.h:
        * Modules/fetch/FetchResponse.cpp:
        (WebCore::FetchResponse::BodyLoader::didSucceed):
        (WebCore::FetchResponse::BodyLoader::didFail):
    
        Canonical link: https://commits.webkit.org/203037@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@234045 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/202900.54@safari-606-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-606-branch@234050 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/fetch/FetchBodySource.cpp
+++ Source/WebCore/Modules/fetch/FetchBodySource.cpp
@@ -34,46 +34,65 @@
 namespace WebCore {
 
 FetchBodySource::FetchBodySource(FetchBodyOwner& bodyOwner)
-    : m_bodyOwner(bodyOwner)
+    : m_bodyOwner(&bodyOwner)
 {
 }
 
 void FetchBodySource::setActive()
 {
-    m_bodyOwner.setPendingActivity(&m_bodyOwner);
+    ASSERT(m_bodyOwner);
+    if (m_bodyOwner)
+        m_bodyOwner->setPendingActivity(m_bodyOwner);
 }
 
 void FetchBodySource::setInactive()
 {
-    m_bodyOwner.unsetPendingActivity(&m_bodyOwner);
+    ASSERT(m_bodyOwner);
+    if (m_bodyOwner)
+        m_bodyOwner->unsetPendingActivity(m_bodyOwner);
 }
 
 void FetchBodySource::doStart()
 {
-    m_bodyOwner.consumeBodyAsStream();
+    ASSERT(m_bodyOwner);
+    if (m_bodyOwner)
+        m_bodyOwner->consumeBodyAsStream();
 }
 
 void FetchBodySource::doPull()
 {
-    m_bodyOwner.feedStream();
+    ASSERT(m_bodyOwner);
+    if (m_bodyOwner)
+        m_bodyOwner->feedStream();
 }
 
 void FetchBodySource::doCancel()
 {
     m_isCancelling = true;
-    m_bodyOwner.cancel();
+    ASSERT(m_bodyOwner || m_isClosed);
+    if (!m_bodyOwner)
+        return;
+
+    m_bodyOwner->cancel();
+    m_bodyOwner = nullptr;
 }
 
 void FetchBodySource::close()
 {
+#ifndef NDEBUG
+    m_isClosed = true;
+#endif
+
     controller().close();
     clean();
+    m_bodyOwner = nullptr;
 }
 
 void FetchBodySource::error(const String& value)
 {
     controller().error(value);
     clean();
+    m_bodyOwner = nullptr;
 }
 
 } // namespace WebCore
--- Source/WebCore/Modules/fetch/FetchBodySource.h
+++ Source/WebCore/Modules/fetch/FetchBodySource.h
@@ -49,6 +49,7 @@ public:
     bool isCancelling() const { return m_isCancelling; }
 
     void resolvePullPromise() { pullFinished(); }
+    void detach() { m_bodyOwner = nullptr; }
 
 private:
     void doStart() final;
@@ -57,8 +58,11 @@ private:
     void setActive() final;
     void setInactive() final;
 
-    FetchBodyOwner& m_bodyOwner;
+    FetchBodyOwner* m_bodyOwner;
     bool m_isCancelling { false };
+#ifndef NDEBUG
+    bool m_isClosed { false };
+#endif
 };
 
 } // namespace WebCore
