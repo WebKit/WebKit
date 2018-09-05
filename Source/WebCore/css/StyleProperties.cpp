f7f99e664ba5e087c2c9138d8c4bd825dd31f057
Oriol Brufau <obrufau@igalia.com>
2018-09-05 05:02:53 +0000

    Check important flags when serializing shorthand with "initial" values https://bugs.webkit.org/show_bug.cgi?id=188984
    Patch by Oriol Brufau <obrufau@igalia.com> on 2018-09-04
    Reviewed by Antti Koivisto.
    
    LayoutTests/imported/w3c:
    
    The test still has some failures due to https://bugs.webkit.org/show_bug.cgi?id=185953
    but without this patch it would fail earlier.
    
    * web-platform-tests/css/cssom/shorthand-serialization-expected.txt:
    * web-platform-tests/css/cssom/shorthand-serialization.html:
    
    Source/WebCore:
    
    Test: imported/w3c/web-platform-tests/css/cssom/shorthand-serialization.html
    
    The test still has some failures due to https://bugs.webkit.org/show_bug.cgi?id=185953
    but without this patch it would fail earlier.
    
    * css/StyleProperties.cpp:
    (WebCore::StyleProperties::get4Values const):
    
    Canonical link: https://commits.webkit.org/204294@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@235658 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/StyleProperties.cpp
+++ Source/WebCore/css/StyleProperties.cpp
@@ -374,6 +374,10 @@ String StyleProperties::get4Values(const StylePropertyShorthand& shorthand) cons
     if (!top.value() || !right.value() || !bottom.value() || !left.value())
         return String();
 
+    // Important flags must be the same
+    if (top.isImportant() != right.isImportant() || right.isImportant() != bottom.isImportant() || bottom.isImportant() != left.isImportant())
+        return String();
+
     if (top.isInherited() && right.isInherited() && bottom.isInherited() && left.isInherited())
         return getValueName(CSSValueInherit);
 
@@ -384,8 +388,6 @@ String StyleProperties::get4Values(const StylePropertyShorthand& shorthand) cons
         }
         return String();
     }
-    if (top.isImportant() != right.isImportant() || right.isImportant() != bottom.isImportant() || bottom.isImportant() != left.isImportant())
-        return String();
 
     bool showLeft = !right.value()->equals(*left.value());
     bool showBottom = !top.value()->equals(*bottom.value()) || showLeft;
