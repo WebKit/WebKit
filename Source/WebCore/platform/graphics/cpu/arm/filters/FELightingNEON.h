186eba076ba0d6e1073430c62342592c4e52c998
Adrian Perez de Castro <aperez@igalia.com>
2018-09-23 04:22:50 +0000

    [ARM] Building FELightingNEON.cpp fails due to missing lightVector member https://bugs.webkit.org/show_bug.cgi?id=189890
    Reviewed by Darin Adler.
    
    No new tests needed.
    
    * platform/graphics/cpu/arm/filters/FELightingNEON.h:
    (WebCore::FELighting::platformApplyNeon): Adapt to new layout of "struct PaintingData" after r225122.
    
    
    Canonical link: https://commits.webkit.org/204844@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@236391 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/cpu/arm/filters/FELightingNEON.h
+++ Source/WebCore/platform/graphics/cpu/arm/filters/FELightingNEON.h
@@ -144,9 +144,9 @@ inline void FELighting::platformApplyNeon(const LightingData& data, const LightS
             neonData.flags |= FLAG_CONE_EXPONENT_IS_1;
     } else {
         ASSERT(m_lightSource->type() == LS_DISTANT);
-        floatArguments.lightX = paintingData.lightVector.x();
-        floatArguments.lightY = paintingData.lightVector.y();
-        floatArguments.lightZ = paintingData.lightVector.z();
+        floatArguments.lightX = paintingData.initialLightingData.lightVector.x();
+        floatArguments.lightY = paintingData.initialLightingData.lightVector.y();
+        floatArguments.lightZ = paintingData.initialLightingData.lightVector.z();
         floatArguments.padding2 = 1;
     }
 
