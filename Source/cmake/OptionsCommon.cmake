3008a74b354385c2f35f60dea67743ac42283e3b
Caio Araujo Neponoceno de Lima <ticaiolima@gmail.com>
2018-10-01 11:56:20 +0000

    'HAVE_PTHREAD_MAIN_NP' macro redefined warning when building --jsc-only on macOS https://bugs.webkit.org/show_bug.cgi?id=190118
    Reviewed by Yusuke Suzuki.
    
    We define HAVE_PTHREAD_MAIN_NP in WTF/wtf/Platform.h when the OS is
    Darwin. In such case, we don't need to check symbols for
    pthread_main_np when generating build for this system.
    
    * Source/cmake/OptionsCommon.cmake:
    
    
    Canonical link: https://commits.webkit.org/205083@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@236654 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/OptionsCommon.cmake
+++ Source/cmake/OptionsCommon.cmake
@@ -134,7 +134,9 @@ WEBKIT_CHECK_HAVE_FUNCTION(HAVE_VASPRINTF vasprintf)
 
 # Check for symbols
 WEBKIT_CHECK_HAVE_SYMBOL(HAVE_REGEX_H regexec regex.h)
+if (NOT (${CMAKE_SYSTEM_NAME} STREQUAL "Darwin"))
 WEBKIT_CHECK_HAVE_SYMBOL(HAVE_PTHREAD_MAIN_NP pthread_main_np pthread_np.h)
+endif ()
 # Windows has signal.h but is missing symbols that are used in calls to signal.
 WEBKIT_CHECK_HAVE_SYMBOL(HAVE_SIGNAL_H SIGTRAP signal.h)
 
