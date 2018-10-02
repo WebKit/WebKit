cf19ddb59e666f27aaa169b7711c6c631d0354c8
Caio Araujo Neponoceno de Lima <ticaiolima@gmail.com>
2018-10-02 13:42:37 +0000

    [BigInt] BigInt.proptotype.toString is broken when radix is power of 2 https://bugs.webkit.org/show_bug.cgi?id=190033
    Reviewed by Yusuke Suzuki.
    
    JSTests:
    
    * stress/big-int-to-string.js:
    
    Source/JavaScriptCore:
    
    The implementation of JSBigInt::toStringToGeneric doesn't handle power
    of 2 radix when JSBigInt length is >= 2. To handle such cases, we
    implemented JSBigInt::toStringBasePowerOfTwo that follows the
    algorithm that groups bits using mask of (2 ^ n) - 1 to extract every
    digit.
    
    * runtime/JSBigInt.cpp:
    (JSC::JSBigInt::toString):
    (JSC::JSBigInt::toStringBasePowerOfTwo):
    * runtime/JSBigInt.h:
    
    Source/WTF:
    
    * wtf/MathExtras.h:
    (WTF::ctz32):
    
    
    Canonical link: https://commits.webkit.org/205150@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@236737 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/JSBigInt.h
+++ Source/JavaScriptCore/runtime/JSBigInt.h
@@ -175,6 +175,7 @@ private:
     static Digit digitDiv(Digit high, Digit low, Digit divisor, Digit& remainder);
     static Digit digitPow(Digit base, Digit exponent);
 
+    static String toStringBasePowerOfTwo(ExecState*, JSBigInt*, unsigned radix);
     static String toStringGeneric(ExecState*, JSBigInt*, unsigned radix);
 
     bool isZero();
