442b578f35266994775e143638f66d60794b4932
Chris Dumez <cdumez@apple.com>
2018-10-10 16:30:07 +0000

    Unreviewed, rolling out r236802.
    Working on getting the HTML spec updated instead
    (https://github.com/whatwg/html/pull/4079)
    
    Reverted changeset:
    
    "Passing noopener=NOOPENER to window.open() should cause the
    new window to not have an opener"
    https://bugs.webkit.org/show_bug.cgi?id=190251
    https://trac.webkit.org/changeset/236802
    
    Canonical link: https://commits.webkit.org/205383@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@237002 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/WindowFeatures.cpp
+++ Source/WebCore/page/WindowFeatures.cpp
@@ -158,7 +158,7 @@ static void setWindowFeature(WindowFeatures& features, StringView key, StringVie
     else if (equalLettersIgnoringASCIICase(key, "scrollbars"))
         features.scrollbarsVisible = numericValue;
     else if (equalLettersIgnoringASCIICase(key, "noopener"))
-        features.noopener = true;
+        features.noopener = numericValue;
     else if (numericValue == 1)
         features.additionalFeatures.append(key.toString());
 }
