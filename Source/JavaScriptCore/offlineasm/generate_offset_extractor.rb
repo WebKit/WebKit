45bab40374d1e47a613645bcfc4bd2a75edafaac
Keith Miller <keith_miller@apple.com>
2018-10-17 17:49:15 +0000

    Remove debug logging from generate_offsets_extractor.rb https://bugs.webkit.org/show_bug.cgi?id=190667
    Reviewed by Mark Lam.
    
    * offlineasm/generate_offset_extractor.rb:
    
    
    Canonical link: https://commits.webkit.org/205593@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@237222 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/offlineasm/generate_offset_extractor.rb
+++ Source/JavaScriptCore/offlineasm/generate_offset_extractor.rb
@@ -121,8 +121,6 @@ File.open(outputFlnm, "w") {
         }
     }
 
-    puts configurationList.size
-    puts configurationList.display
     outp.puts "static const int64_t offsetExtractorTable[] = { };" if not configurationList.size
 
 }
