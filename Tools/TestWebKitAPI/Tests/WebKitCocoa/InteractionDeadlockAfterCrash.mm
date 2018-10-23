b0f2d4ceb74eb1c7d679aedec5bb32b14f05a7a3
Tim Horton <timothy_horton@apple.com>
2018-10-23 01:52:04 +0000

    REGRESSION (r237331): InteractionDeadlockAfterCrash API test fails https://bugs.webkit.org/show_bug.cgi?id=190801 <rdar://problem/43674361>
    * TestWebKitAPI/Tests/WebKitCocoa/InteractionDeadlockAfterCrash.mm:
    (TEST):
    Use a parented WKWebView for this test, otherwise none of the assertions
    about gesture recognizers work anymore.
    
    
    Canonical link: https://commits.webkit.org/205702@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@237346 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/WebKitCocoa/InteractionDeadlockAfterCrash.mm
+++ Tools/TestWebKitAPI/Tests/WebKitCocoa/InteractionDeadlockAfterCrash.mm
@@ -28,6 +28,7 @@
 #import "PlatformUtilities.h"
 #import "Test.h"
 #import "TestNavigationDelegate.h"
+#import "TestWKWebView.h"
 #import <WebKit/WKWebViewConfigurationPrivate.h>
 #import <WebKit/WKWebViewPrivate.h>
 #import <wtf/RetainPtr.h>
@@ -60,7 +61,7 @@ TEST(WebKit, InteractionDeadlockAfterCrash)
 {
     RetainPtr<WKWebViewConfiguration> configuration = adoptNS([[WKWebViewConfiguration alloc] init]);
 
-    RetainPtr<WKWebView> webView = adoptNS([[WKWebView alloc] initWithFrame:NSMakeRect(0, 0, 100, 100) configuration:configuration.get()]);
+    RetainPtr<WKWebView> webView = adoptNS([[TestWKWebView alloc] initWithFrame:NSMakeRect(0, 0, 100, 100) configuration:configuration.get()]);
 
     [webView loadHTMLString:@"test" baseURL:nil];
     [webView _test_waitForDidFinishNavigation];
