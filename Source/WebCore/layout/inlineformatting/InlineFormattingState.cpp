809d3b835b2befbf086333f96c7db4e4bcf0d504
Zalan Bujtas <zalan@apple.com>
2018-10-31 14:21:57 +0000

    Missing from r237634
    * layout/inlineformatting/InlineFormattingState.cpp:
    (WebCore::Layout::InlineFormattingState::formattingContext):
    
    Canonical link: https://commits.webkit.org/205918@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@237635 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/layout/inlineformatting/InlineFormattingState.cpp
+++ Source/WebCore/layout/inlineformatting/InlineFormattingState.cpp
@@ -47,7 +47,7 @@ InlineFormattingState::~InlineFormattingState()
 std::unique_ptr<FormattingContext> InlineFormattingState::formattingContext(const Box& formattingContextRoot)
 {
     ASSERT(formattingContextRoot.establishesInlineFormattingContext());
-    return std::make_unique<InlineFormattingContext>(formattingContextRoot, this);
+    return std::make_unique<InlineFormattingContext>(formattingContextRoot, *this);
 }
 
 }
