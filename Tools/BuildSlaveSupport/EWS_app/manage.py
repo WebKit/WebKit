84999c40214c9c0f969c30e157797d2c9084d40a
Aakash Jain <aakash_jain@apple.com>
2018-11-02 17:35:41 +0000

    Add Basic Django web app for OpenSource EWS https://bugs.webkit.org/show_bug.cgi?id=191091
    Reviewed by Lucas Forschler.
    
    * BuildSlaveSupport/EWS_app/manage.py: Added, auto-generated by Django.
    * BuildSlaveSupport/EWS_app/EWS_app/__init__.py: Added, auto-generated by Django.
    * BuildSlaveSupport/EWS_app/EWS_app/settings.py: Added, auto-generated by Django.
    * BuildSlaveSupport/EWS_app/EWS_app/urls.py: Added, auto-generated by Django.
    * BuildSlaveSupport/EWS_app/EWS_app/wsgi.py: Added, auto-generated by Django.
    * BuildSlaveSupport/EWS_app/ews/__init__.py: Added.
    * BuildSlaveSupport/EWS_app/ews/admin.py: Added, auto-generated by Django.
    * BuildSlaveSupport/EWS_app/ews/apps.py: Added, auto-generated by Django.
    * BuildSlaveSupport/EWS_app/ews/migrations/__init__.py: Added.
    * BuildSlaveSupport/EWS_app/ews/tests.py: Added, auto-generated by Django.
    * BuildSlaveSupport/EWS_app/ews/models/__init__.py: Added.
    * BuildSlaveSupport/EWS_app/ews/models/patch.py: Added.
    * BuildSlaveSupport/EWS_app/ews/views/__init__.py: Added.
    * BuildSlaveSupport/EWS_app/ews/views/patch.py: Added.
    
    
    Canonical link: https://commits.webkit.org/206006@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@237740 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Tools/BuildSlaveSupport/EWS_app/manage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2018 Apple Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EWS_app.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?")
+        raise
+    execute_from_command_line(sys.argv)
