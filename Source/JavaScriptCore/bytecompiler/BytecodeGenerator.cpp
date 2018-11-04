9fa08136c5c472b91e7b728ae7b7773e5dc9e574
Adrian Perez de Castro <aperez@igalia.com>
2018-11-04 15:05:43 +0000

    [GTK] Cannot make debug builds of JSC using release 2.22.3 https://bugs.webkit.org/show_bug.cgi?id=191233
    Unreviewed build fix.
    
    * bytecompiler/BytecodeGenerator.cpp:
    (JSC::BytecodeGenerator::emitGetByVal):
    Use context.asStructureForInContext() instead of explicit cast and
    remove the unneeded assertion, which is already done in the helper
    function.

--- Source/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
+++ Source/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp
@@ -2920,8 +2920,7 @@ RegisterID* BytecodeGenerator::emitGetByVal(RegisterID* dst, RegisterID* base, R
             break;
         }
 
-        ASSERT(context.type() == ForInContext::StructureForInContextType);
-        StructureForInContext& structureContext = static_cast<StructureForInContext&>(context);
+        StructureForInContext& structureContext = context.asStructureForInContext();
         UnlinkedValueProfile profile = emitProfiledOpcode(op_get_direct_pname);
         instructions().append(kill(dst));
         instructions().append(base->index());
