5ab3b99ecf48c07470060ed548b5b3f3d0590f69
Aakash Jain <aakash_jain@apple.com>
2018-11-06 23:16:38 +0000

    [ews-app] Rename EWS_app to ews-app - part 2 https://bugs.webkit.org/show_bug.cgi?id=191317
    Reviewed by Lucas Forschler.
    
    * BuildSlaveSupport/ews-app/EWS_app: Removed.
    * BuildSlaveSupport/ews-app/ews-app: Moved from BuildSlaveSupport/ews-app/EWS_app.
    * BuildSlaveSupport/ews-app/ews-app/settings.py:
    * BuildSlaveSupport/ews-app/ews-app/urls.py:
    * BuildSlaveSupport/ews-app/ews-app/wsgi.py:
    * BuildSlaveSupport/ews-app/manage.py:
    
    
    
    Canonical link: https://commits.webkit.org/206135@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@237896 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/BuildSlaveSupport/ews-app/ews-app/urls.py
+++ Tools/BuildSlaveSupport/ews-app/ews-app/urls.py
@@ -20,19 +20,24 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+"""URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-WSGI config for EWS_app project.
+from django.conf.urls import url
+from django.contrib import admin
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EWS_app.settings")
-
-application = get_wsgi_application()
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
