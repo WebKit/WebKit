0df6ee6f737ef6ce36b398f9ae7d83be4e6030e6
Brian Burg <bburg@apple.com>
2018-11-06 23:54:00 +0000

    Add missing iOS availability macro to _WKAutomationSessionConfiguration https://bugs.webkit.org/show_bug.cgi?id=191320
    Reviewed by Tim Horton.
    
    * UIProcess/API/Cocoa/_WKAutomationSessionConfiguration.h:
    
    Canonical link: https://commits.webkit.org/206137@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@237898 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/Cocoa/_WKAutomationSessionConfiguration.h
+++ Source/WebKit/UIProcess/API/Cocoa/_WKAutomationSessionConfiguration.h
@@ -31,7 +31,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-WK_CLASS_AVAILABLE(macosx(10.13.4))
+WK_CLASS_AVAILABLE(macosx(10.13.4), ios(WK_IOS_TBA))
 @interface _WKAutomationSessionConfiguration : NSObject <NSCopying>
 
 @property (nonatomic) BOOL allowsInsecureMediaCapture;
