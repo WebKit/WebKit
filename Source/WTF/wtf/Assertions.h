9ed4476e766ff454f524a5f2efd30d4371f9f98e
Michael Catanzaro <mcatanzaro@igalia.com>
2018-11-24 15:13:15 +0000

    CRASH() should call abort() except on Darwin and in developer builds https://bugs.webkit.org/show_bug.cgi?id=184408
    Reviewed by Daniel Bates.
    
    CRASH() should call abort() except on Darwin and in developer builds, as discussed on
    webkit-dev. This should be slightly nicer than dereferencing 0xbadbeef.
    
    On Darwin, CRASH() uses a breakpoint trap, which seems to corrupt the stack on Linux, so we
    can't do that.
    
    Continue to call WTFCrash() in developer mode, and make no changes to WTFCrash(), since it
    is reportedly useful in nightmare scenarios where core dumps are unavailable.
    
    * wtf/Assertions.h:
    
    
    Canonical link: https://commits.webkit.org/206638@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@238469 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/Assertions.h
+++ Source/WTF/wtf/Assertions.h
@@ -44,6 +44,7 @@
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <wtf/ExportMacros.h>
 
 #if USE(OS_LOG)
@@ -239,6 +240,8 @@ WTF_EXPORT_PRIVATE bool WTFIsDebuggerAttached(void);
     WTFBreakpointTrapUnderConstexprContext(); \
     __builtin_unreachable(); \
 } while (0)
+#elif !ENABLE(DEVELOPER_MODE) && !OS(DARWIN)
+#define CRASH() abort()
 #else
 #define CRASH() WTFCrash()
 #define CRASH_UNDER_CONSTEXPR_CONTEXT() WTFCrash()
