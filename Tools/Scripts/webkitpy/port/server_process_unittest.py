f69575973cfbb5c170f185afe56df4ec35754719
Jonathan Bedard <jbedard@apple.com>
2018-11-27 01:02:42 +0000

    webkitpy: Race condition in ServerProcess tests https://bugs.webkit.org/show_bug.cgi?id=191989 <rdar://problem/45536844>
    Reviewed by Lucas Forschler.
    
    Serializing these tests to prevent the race conditions.
    
    * Scripts/webkitpy/port/server_process_unittest.py:
    (TestServerProcess.serial_test_basic):
    (TestServerProcess.serial_test_read_after_process_exits):
    (TestServerProcess.serial_test_process_crashing):
    (TestServerProcess.serial_test_process_crashing_no_data):
    (TestServerProcess.test_basic): Deleted.
    (TestServerProcess.test_read_after_process_exits): Deleted.
    (TestServerProcess.test_process_crashing): Deleted.
    (TestServerProcess.test_process_crashing_no_data): Deleted.
    
    
    Canonical link: https://commits.webkit.org/206698@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@238534 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/port/server_process_unittest.py
+++ Tools/Scripts/webkitpy/port/server_process_unittest.py
@@ -98,7 +98,7 @@ class FakeServerProcess(server_process.ServerProcess):
 
 
 class TestServerProcess(unittest.TestCase):
-    def test_basic(self):
+    def serial_test_basic(self):
         # Give -u switch to force stdout and stderr to be unbuffered for Windows
         cmd = [sys.executable, '-uc', 'import sys; print "stdout"; print >>sys.stderr, "stderr"; sys.stdin.readline();']
         host = SystemHost()
@@ -128,7 +128,7 @@ class TestServerProcess(unittest.TestCase):
 
         proc.stop(0)
 
-    def test_read_after_process_exits(self):
+    def serial_test_read_after_process_exits(self):
         cmd = [sys.executable, '-c', 'import sys; print "stdout"; print >>sys.stderr, "stderr";']
         host = SystemHost()
         factory = PortFactory(host)
@@ -146,7 +146,7 @@ class TestServerProcess(unittest.TestCase):
 
         proc.stop(0)
 
-    def test_process_crashing(self):
+    def serial_test_process_crashing(self):
         # Give -u switch to force stdout to be unbuffered for Windows
         cmd = [sys.executable, '-uc', 'import sys; print "stdout 1"; print "stdout 2"; print "stdout 3"; sys.stdin.readline(); sys.exit(1);']
         host = SystemHost()
@@ -172,7 +172,7 @@ class TestServerProcess(unittest.TestCase):
 
         proc.stop(0)
 
-    def test_process_crashing_no_data(self):
+    def serial_test_process_crashing_no_data(self):
         cmd = [sys.executable, '-c',
                'import sys; sys.stdin.readline(); sys.exit(1);']
         host = SystemHost()
