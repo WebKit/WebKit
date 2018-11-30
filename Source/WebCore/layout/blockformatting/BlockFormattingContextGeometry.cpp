6418a4728079b4da20d956840f80332da8313a02
Zalan Bujtas <zalan@apple.com>
2018-11-30 15:24:11 +0000

    [LFC][BFC] Geometry::inFlowNonReplacedHeightAndMargin should check for empty inline formatting context. https://bugs.webkit.org/show_bug.cgi?id=192215
    Reviewed by Antti Koivisto.
    
    Check if the inline formatting context actually has any lines.
    
    * layout/blockformatting/BlockFormattingContextGeometry.cpp:
    (WebCore::Layout::BlockFormattingContext::Geometry::inFlowNonReplacedHeightAndMargin):
    
    Canonical link: https://commits.webkit.org/206880@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@238736 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/layout/blockformatting/BlockFormattingContextGeometry.cpp
+++ Source/WebCore/layout/blockformatting/BlockFormattingContextGeometry.cpp
@@ -76,8 +76,9 @@ HeightAndMargin BlockFormattingContext::Geometry::inFlowNonReplacedHeightAndMarg
         // 1. the bottom edge of the last line box, if the box establishes a inline formatting context with one or more lines
         if (layoutBox.establishesInlineFormattingContext()) {
             // This is temp and will be replaced by the correct display box once inline runs move over to the display tree.
-            auto& lastInlineRun = downcast<InlineFormattingState>(layoutState.establishedFormattingState(layoutBox)).inlineRuns().last();
-            return { lastInlineRun.logicalBottom(), nonCollapsedMargin, collapsedMargin };
+            auto& inlineRuns = downcast<InlineFormattingState>(layoutState.establishedFormattingState(layoutBox)).inlineRuns();
+            auto bottomEdge = inlineRuns.isEmpty() ? LayoutUnit() : inlineRuns.last().logicalBottom();
+            return { bottomEdge, nonCollapsedMargin, collapsedMargin };
         }
 
         // 2. the bottom edge of the bottom (possibly collapsed) margin of its last in-flow child, if the child's bottom margin...
