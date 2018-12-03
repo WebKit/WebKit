408d385986ed48a995ad2e9fb001206f1db87958
Yacine Bandou <yacine.bandou@softathome.com>
2018-12-03 11:25:08 +0000

    [EME][ClearKey] Add layout test for dynamic change of key and video resolution https://bugs.webkit.org/show_bug.cgi?id=191926
    Patch by Yacine Bandou <yacine.bandou@softathome.com> on 2018-12-03
    Reviewed by Xabier Rodriguez-Calvar.
    
           Add new layout tests in order to test the playback of ClearKey encrypted video content with
           a change of key and resolution during decoding.
           The test starts by playing a ClearKey video with a resolution of 240p, then, in the third second,
           the resolution of the video will change to 480p with a new ClearKey key.
    
    * media/content/encrypted/segments/VideoClearKeyCenc-seg-240-0.mp4: Added.
    * media/content/encrypted/segments/VideoClearKeyCenc-seg-240-1.mp4: Added.
    * media/content/encrypted/segments/VideoClearKeyCenc-seg-240-2.mp4: Added.
    * media/content/encrypted/segments/VideoClearKeyCenc-seg-240-3.mp4: Added.
    * media/content/encrypted/segments/VideoClearKeyCenc-seg-480-0.mp4: Added.
    * media/content/encrypted/segments/VideoClearKeyCenc-seg-480-4.mp4: Added.
    * media/content/encrypted/segments/VideoClearKeyCenc-seg-480-5.mp4: Added.
    * media/content/encrypted/segments/VideoClearKeyCenc-seg-480-6.mp4: Added.
    * media/encrypted-media/clearKey/clearKey-cenc-video-playback-mse-multikey-expected.txt: Added.
    * media/encrypted-media/clearKey/clearKey-cenc-video-playback-mse-multikey.html: Added.
    * media/encrypted-media/medias-enc.js:
    * platform/gtk/TestExpectations:
    * platform/mac/TestExpectations:
    
    Canonical link: https://commits.webkit.org/206937@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@238793 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/media/encrypted-media/medias-enc.js
+++ LayoutTests/media/encrypted-media/medias-enc.js
@@ -29,5 +29,21 @@ const streamMedias = {
                                             segments     : [ "../../content/encrypted/VideoClearKeyWebM.webm" ],
                                             keys         : {    "30313233343536373839303132333435" : "4d24cb6b0de1abe880e4d5983df03c93" }
                                        }
-                          }
+                          },
+     "multiKeyClearKeyVideoMSE" : { video : {    initDataType : "cenc",
+                                                 mimeType     : 'video/mp4; codecs="avc1.64001F"',
+                                                 segments     : [ "../../content/encrypted/segments/VideoClearKeyCenc-seg-240-0.mp4",
+                                                                  "../../content/encrypted/segments/VideoClearKeyCenc-seg-240-1.mp4",
+                                                                  "../../content/encrypted/segments/VideoClearKeyCenc-seg-240-2.mp4",
+                                                                  "../../content/encrypted/segments/VideoClearKeyCenc-seg-240-3.mp4",
+                                                                  "../../content/encrypted/segments/VideoClearKeyCenc-seg-480-0.mp4",
+                                                                  "../../content/encrypted/segments/VideoClearKeyCenc-seg-480-4.mp4",
+                                                                  "../../content/encrypted/segments/VideoClearKeyCenc-seg-480-5.mp4",
+                                                                  "../../content/encrypted/segments/VideoClearKeyCenc-seg-480-6.mp4",
+                                                                ],
+                                                 keys         : {    "d0d1d2d3d4d5d6d7d8d9dadbdcdddedf" : "c0c1c2c3c4c5c6c7c8c9cacbcccdcecf",
+                                                                     "b0b1b2b3b4b5b6b7b8b9babbbcbdbebf" : "a0a1a2a3a4a5a6a7a8a9aaabacadaeaf"
+                                                                }
+                                            }
+                                  }
                    };
