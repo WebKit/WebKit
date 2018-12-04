13531d81f6baa370528d20a1b1ae6d8619bc1627
YUHAN WU <yuhan_wu@apple.com>
2018-12-04 06:11:31 +0000

    Implement non-timeslice mode encoding for MediaRecorder https://bugs.webkit.org/show_bug.cgi?id=192069
    Patch by YUHAN WU <yuhan_wu@apple.com> on 2018-12-03
    Reviewed by Youenn Fablet.
    
    Source/WebCore:
    
    Implement the encoding for non-timeslice mode of MediaRecorder.
    It only supports to record MP4 file through H264 and AAC encoding, we will need to support more MIME types and encoding methods.
    Add a API in internals to allow testings to turn on the mock source.
    
    Tests: http/wpt/mediarecorder/MediaRecorder-AV-audio-only-dataavailable.html
           http/wpt/mediarecorder/MediaRecorder-AV-audio-video-dataavailable.html
           http/wpt/mediarecorder/MediaRecorder-AV-video-only-dataavailable.html
    
    * Modules/mediarecorder/MediaRecorder.cpp:
    (WebCore::MediaRecorder::create):
    (WebCore::MediaRecorder::setCustomPrivateRecorderCreator):
    (WebCore::MediaRecorder::getPrivateImpl):
    (WebCore::MediaRecorder::MediaRecorder):
    (WebCore::MediaRecorder::stopRecording):
    (WebCore::MediaRecorder::stopRecordingInternal):
    (WebCore::MediaRecorder::createRecordingDataBlob):
    (WebCore::MediaRecorder::scheduleDeferredTask):
    * Modules/mediarecorder/MediaRecorder.h:
    * Modules/mediarecorder/MediaRecorder.idl:
    * SourcesCocoa.txt:
    * WebCore.xcodeproj/project.pbxproj:
    * platform/mediarecorder/MediaRecorderPrivate.h:
    (WebCore::MediaRecorderPrivate::stopRecording):
    * platform/mediarecorder/MediaRecorderPrivateAVFImpl.cpp: Added.
    (WebCore::MediaRecorderPrivateAVFImpl::create):
    (WebCore::MediaRecorderPrivateAVFImpl::MediaRecorderPrivateAVImpl):
    (WebCore::MediaRecorderPrivateAVFImpl::sampleBufferUpdated):
    (WebCore::MediaRecorderPrivateAVFImpl::audioSamplesAvailable):
    (WebCore::MediaRecorderPrivateAVFImpl::stopRecording):
    (WebCore::MediaRecorderPrivateAVFImpl::fetchData):
    (WebCore::MediaRecorderPrivateAVFImpl::mimeType):
    * platform/mediarecorder/MediaRecorderPrivateAVFImpl.h: Added.
    * platform/mediarecorder/MediaRecorderPrivateMock.cpp:
    (WebCore::MediaRecorderPrivateMock::fetchData):
    (WebCore::MediaRecorderPrivateMock::mimeType):
    * platform/mediarecorder/MediaRecorderPrivateMock.h:
    * platform/mediarecorder/cocoa/MediaRecorderPrivateWriterCocoa.h: Added.
    * platform/mediarecorder/cocoa/MediaRecorderPrivateWriterCocoa.mm: Added.
    (WebCore::MediaRecorderPrivateWriter::setupWriter):
    (WebCore::MediaRecorderPrivateWriter::setVideoInput):
    (WebCore::MediaRecorderPrivateWriter::setAudioInput):
    (WebCore::copySampleBufferWithCurrentTimeStamp):
    (WebCore::MediaRecorderPrivateWriter::appendVideoSampleBuffer):
    (WebCore::MediaRecorderPrivateWriter::appendAudioSampleBuffer):
    (WebCore::MediaRecorderPrivateWriter::stopRecording):
    (WebCore::MediaRecorderPrivateWriter::fetchData):
    * testing/Internals.cpp:
    (WebCore::createRecorderMockSource):
    (WebCore::Internals::setCustomPrivateRecorderCreator):
    * testing/Internals.h:
    * testing/Internals.idl:
    
    LayoutTests:
    
    Create new tests for encoding of MediaRecorder. Check if the produced video and audio are correct.
    Add code to turn on the mock source of MediaRecorder for the two old tests because the real source is enabled by default.
    
    * http/wpt/mediarecorder/MediaRecorder-AV-audio-only-dataavailable-expected.txt: Added.
    * http/wpt/mediarecorder/MediaRecorder-AV-audio-only-dataavailable.html: Added.
    * http/wpt/mediarecorder/MediaRecorder-AV-audio-video-dataavailable-expected.txt: Added.
    * http/wpt/mediarecorder/MediaRecorder-AV-audio-video-dataavailable.html: Added.
    * http/wpt/mediarecorder/MediaRecorder-AV-video-only-dataavailable-expected.txt: Added.
    * http/wpt/mediarecorder/MediaRecorder-AV-video-only-dataavailable.html: Added.
    * http/wpt/mediarecorder/MediaRecorder-dataavailable.html:
    * http/wpt/mediarecorder/MediaRecorder-mock-dataavailable.html:
    
    Canonical link: https://commits.webkit.org/206985@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@238844 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebCore/platform/mediarecorder/cocoa/MediaRecorderPrivateWriterCocoa.mm
@@ -0,0 +1,287 @@
+/*
+ * Copyright (C) 2018 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "MediaRecorderPrivateWriterCocoa.h"
+
+#if ENABLE(MEDIA_STREAM) && USE(AVFOUNDATION)
+
+#include "AudioStreamDescription.h"
+#include "FileSystem.h"
+#include "Logging.h"
+#include "WebAudioBufferList.h"
+#include <AVFoundation/AVAssetWriter.h>
+#include <AVFoundation/AVAssetWriterInput.h>
+#include <pal/cf/CoreMediaSoftLink.h>
+
+typedef AVAssetWriter AVAssetWriterType;
+typedef AVAssetWriterInput AVAssetWriterInputType;
+
+SOFT_LINK_FRAMEWORK_OPTIONAL(AVFoundation)
+
+SOFT_LINK_CLASS(AVFoundation, AVAssetWriter)
+SOFT_LINK_CLASS(AVFoundation, AVAssetWriterInput)
+
+SOFT_LINK_CONSTANT(AVFoundation, AVFileTypeMPEG4, NSString *)
+SOFT_LINK_CONSTANT(AVFoundation, AVVideoCodecKey, NSString *)
+SOFT_LINK_CONSTANT(AVFoundation, AVVideoCodecH264, NSString *)
+SOFT_LINK_CONSTANT(AVFoundation, AVVideoWidthKey, NSString *)
+SOFT_LINK_CONSTANT(AVFoundation, AVVideoHeightKey, NSString *)
+SOFT_LINK_CONSTANT(AVFoundation, AVMediaTypeVideo, NSString *)
+SOFT_LINK_CONSTANT(AVFoundation, AVMediaTypeAudio, NSString *)
+SOFT_LINK_CONSTANT(AVFoundation, AVEncoderBitRatePerChannelKey, NSString *)
+SOFT_LINK_CONSTANT(AVFoundation, AVFormatIDKey, NSString *)
+SOFT_LINK_CONSTANT(AVFoundation, AVNumberOfChannelsKey, NSString *)
+SOFT_LINK_CONSTANT(AVFoundation, AVSampleRateKey, NSString *)
+
+#define AVFileTypeMPEG4 getAVFileTypeMPEG4()
+#define AVMediaTypeAudio getAVMediaTypeAudio()
+#define AVMediaTypeVideo getAVMediaTypeVideo()
+#define AVVideoCodecKey getAVVideoCodecKey()
+#define AVVideoCodecH264 getAVVideoCodecH264()
+#define AVVideoWidthKey getAVVideoWidthKey()
+#define AVVideoHeightKey getAVVideoHeightKey()
+#define AVEncoderBitRatePerChannelKey getAVEncoderBitRatePerChannelKey()
+#define AVFormatIDKey getAVFormatIDKey()
+#define AVNumberOfChannelsKey getAVNumberOfChannelsKey()
+#define AVSampleRateKey getAVSampleRateKey()
+
+using namespace WebCore;
+
+namespace WebCore {
+
+using namespace PAL;
+
+bool MediaRecorderPrivateWriter::setupWriter()
+{
+    ASSERT(!m_writer);
+    
+    NSString *directory = FileSystem::createTemporaryDirectory(@"videos");
+    NSString *filename = [NSString stringWithFormat:@"/%lld.mp4", CMClockGetTime(CMClockGetHostTimeClock()).value];
+    NSString *path = [directory stringByAppendingString:filename];
+
+    NSURL *outputURL = [NSURL fileURLWithPath:path];
+    m_path = [path UTF8String];
+    NSError *error = nil;
+    m_writer = adoptNS([allocAVAssetWriterInstance() initWithURL:outputURL fileType:AVFileTypeMPEG4 error:&error]);
+    if (error) {
+        RELEASE_LOG_ERROR(MediaStream, "create AVAssetWriter instance failed with error code %ld", (long)error.code);
+        m_writer = nullptr;
+        return false;
+    }
+    return true;
+}
+
+bool MediaRecorderPrivateWriter::setVideoInput(int width, int height)
+{
+    ASSERT(!m_videoInput);
+
+    NSDictionary *videoSettings = @{ AVVideoCodecKey: AVVideoCodecH264, AVVideoWidthKey: [NSNumber numberWithInt:width], AVVideoHeightKey: [NSNumber numberWithInt:height] };
+    m_videoInput = adoptNS([allocAVAssetWriterInputInstance() initWithMediaType:AVMediaTypeVideo outputSettings:videoSettings sourceFormatHint:nil]);
+    [m_videoInput setExpectsMediaDataInRealTime:true];
+    
+    if (![m_writer canAddInput:m_videoInput.get()]) {
+        m_videoInput = nullptr;
+        RELEASE_LOG_ERROR(MediaStream, "the video input is not allowed to add to the AVAssetWriter");
+        return false;
+    }
+    [m_writer addInput:m_videoInput.get()];
+    m_videoPullQueue = dispatch_queue_create("WebCoreVideoRecordingPullBufferQueue", DISPATCH_QUEUE_SERIAL);
+    return true;
+}
+
+bool MediaRecorderPrivateWriter::setAudioInput()
+{
+    ASSERT(!m_audioInput);
+
+    NSDictionary *audioSettings = @{ AVEncoderBitRatePerChannelKey : @(28000), AVFormatIDKey : @(kAudioFormatMPEG4AAC), AVNumberOfChannelsKey : @(1), AVSampleRateKey : @(22050) };
+
+    m_audioInput = adoptNS([allocAVAssetWriterInputInstance() initWithMediaType:AVMediaTypeAudio outputSettings:audioSettings sourceFormatHint:nil]);
+    [m_audioInput setExpectsMediaDataInRealTime:true];
+    
+    if (![m_writer canAddInput:m_audioInput.get()]) {
+        m_audioInput = nullptr;
+        RELEASE_LOG_ERROR(MediaStream, "the audio input is not allowed to add to the AVAssetWriter");
+        return false;
+    }
+    [m_writer addInput:m_audioInput.get()];
+    m_audioPullQueue = dispatch_queue_create("WebCoreAudioRecordingPullBufferQueue", DISPATCH_QUEUE_SERIAL);
+    return true;
+}
+
+static inline CMSampleBufferRef copySampleBufferWithCurrentTimeStamp(CMSampleBufferRef originalBuffer)
+{
+    CMTime startTime = CMClockGetTime(CMClockGetHostTimeClock());
+    CMItemCount count = 0;
+    CMSampleBufferGetSampleTimingInfoArray(originalBuffer, 0, nil, &count);
+    
+    Vector<CMSampleTimingInfo> timeInfo(count);
+    CMSampleBufferGetSampleTimingInfoArray(originalBuffer, count, timeInfo.data(), &count);
+    
+    for (CMItemCount i = 0; i < count; i++) {
+        timeInfo[i].decodeTimeStamp = kCMTimeInvalid;
+        timeInfo[i].presentationTimeStamp = startTime;
+    }
+    
+    CMSampleBufferRef newBuffer;
+    CMSampleBufferCreateCopyWithNewTiming(kCFAllocatorDefault, originalBuffer, count, timeInfo.data(), &newBuffer);
+    return newBuffer;
+}
+
+void MediaRecorderPrivateWriter::appendVideoSampleBuffer(CMSampleBufferRef sampleBuffer)
+{
+    ASSERT(m_videoInput);
+    if (m_isStopped)
+        return;
+
+    if (!m_hasStartedWriting) {
+        if (![m_writer startWriting]) {
+            m_isStopped = true;
+            RELEASE_LOG_ERROR(MediaStream, "create AVAssetWriter instance failed with error code %ld", (long)[m_writer error]);
+            return;
+        }
+        [m_writer startSessionAtSourceTime:CMClockGetTime(CMClockGetHostTimeClock())];
+        m_hasStartedWriting = true;
+        RefPtr<MediaRecorderPrivateWriter> protectedThis = this;
+        [m_videoInput requestMediaDataWhenReadyOnQueue:m_videoPullQueue usingBlock:[this, protectedThis] {
+            do {
+                if (![m_videoInput isReadyForMoreMediaData])
+                    break;
+                auto locker = holdLock(m_videoLock);
+                if (m_videoBufferPool.isEmpty())
+                    break;
+                auto buffer = m_videoBufferPool.takeFirst();
+                locker.unlockEarly();
+                if (![m_videoInput appendSampleBuffer:buffer.get()])
+                    break;
+            } while (true);
+            if (m_isStopped && m_videoBufferPool.isEmpty()) {
+                [m_videoInput markAsFinished];
+                m_finishWritingVideoSemaphore.signal();
+            }
+        }];
+        return;
+    }
+    CMSampleBufferRef bufferWithCurrentTime = copySampleBufferWithCurrentTimeStamp(sampleBuffer);
+    auto locker = holdLock(m_videoLock);
+    m_videoBufferPool.append(retainPtr(bufferWithCurrentTime));
+}
+
+void MediaRecorderPrivateWriter::appendAudioSampleBuffer(const PlatformAudioData& data, const AudioStreamDescription& description, const WTF::MediaTime&, size_t sampleCount)
+{
+    ASSERT(m_audioInput);
+    if ((!m_hasStartedWriting && m_videoInput) || m_isStopped)
+        return;
+    CMSampleBufferRef sampleBuffer;
+    CMFormatDescriptionRef format;
+    OSStatus error;
+    auto& basicDescription = *WTF::get<const AudioStreamBasicDescription*>(description.platformDescription().description);
+    error = CMAudioFormatDescriptionCreate(kCFAllocatorDefault, &basicDescription, 0, NULL, 0, NULL, NULL, &format);
+    if (m_isFirstAudioSample) {
+        if (!m_videoInput) {
+            // audio-only recording.
+            if (![m_writer startWriting]) {
+                m_isStopped = true;
+                return;
+            }
+            [m_writer startSessionAtSourceTime:CMClockGetTime(CMClockGetHostTimeClock())];
+            m_hasStartedWriting = true;
+        }
+        m_isFirstAudioSample = false;
+        RefPtr<MediaRecorderPrivateWriter> protectedThis = this;
+        [m_audioInput requestMediaDataWhenReadyOnQueue:m_audioPullQueue usingBlock:[this, protectedThis] {
+            do {
+                if (![m_audioInput isReadyForMoreMediaData])
+                    break;
+                auto locker = holdLock(m_audioLock);
+                if (m_audioBufferPool.isEmpty())
+                    break;
+                auto buffer = m_audioBufferPool.takeFirst();
+                locker.unlockEarly();
+                [m_audioInput appendSampleBuffer:buffer.get()];
+            } while (true);
+            if (m_isStopped && m_audioBufferPool.isEmpty()) {
+                [m_audioInput markAsFinished];
+                m_finishWritingAudioSemaphore.signal();
+            }
+        }];
+    }
+    CMTime startTime = CMClockGetTime(CMClockGetHostTimeClock());
+
+    error = CMAudioSampleBufferCreateWithPacketDescriptions(kCFAllocatorDefault, NULL, false, NULL, NULL, format, sampleCount, startTime, NULL, &sampleBuffer);
+    if (error)
+        return;
+    error = CMSampleBufferSetDataBufferFromAudioBufferList(sampleBuffer, kCFAllocatorDefault, kCFAllocatorDefault, 0, downcast<WebAudioBufferList>(data).list());
+    if (error)
+        return;
+
+    auto locker = holdLock(m_audioLock);
+    m_audioBufferPool.append(retainPtr(sampleBuffer));
+}
+
+void MediaRecorderPrivateWriter::stopRecording()
+{
+    m_isStopped = true;
+    if (!m_hasStartedWriting)
+        return;
+    ASSERT([m_writer status] == AVAssetWriterStatusWriting);
+    if (m_videoInput)
+        m_finishWritingVideoSemaphore.wait();
+
+    if (m_audioInput)
+        m_finishWritingAudioSemaphore.wait();
+
+    [m_writer finishWritingWithCompletionHandler:^{
+        m_isStopped = false;
+        m_hasStartedWriting = false;
+        m_isFirstAudioSample = true;
+        if (m_videoInput) {
+            m_videoInput.clear();
+            m_videoInput = nullptr;
+            dispatch_release(m_videoPullQueue);
+        }
+        if (m_audioInput) {
+            m_audioInput.clear();
+            m_audioInput = nullptr;
+            dispatch_release(m_audioPullQueue);
+        }
+        m_writer.clear();
+        m_writer = nullptr;
+        m_finishWritingSemaphore.signal();
+    }];
+}
+
+RefPtr<SharedBuffer> MediaRecorderPrivateWriter::fetchData()
+{
+    if ((m_path.isEmpty() && !m_isStopped) || !m_hasStartedWriting)
+        return nullptr;
+    
+    m_finishWritingSemaphore.wait();
+    return SharedBuffer::createWithContentsOfFile(m_path);
+}
+
+} // namespace WebCore
+
+#endif // ENABLE(MEDIA_STREAM) && USE(AVFOUNDATION)
