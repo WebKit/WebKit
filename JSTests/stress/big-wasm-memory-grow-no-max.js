359dc5dc1d5f438a2c92fef6219c791ad9b8a08a
Keith Miller <keith_miller@apple.com>
2018-12-06 18:59:44 +0000

    stress/big-wasm-memory tests failing on 32-bit JSC bot https://bugs.webkit.org/show_bug.cgi?id=192020
    Reviewed by Saam Barati.
    
    Not every platform has WebAssembly, e.g. 32-bit, so we should exit
    the wasm stress tests if the WebAssembly object does not exist.
    
    * stress/big-wasm-memory-grow-no-max.js:
    (test.foo):
    (test):
    (foo): Deleted.
    (catch): Deleted.
    * stress/big-wasm-memory-grow.js:
    (test.foo):
    (test):
    (foo): Deleted.
    (catch): Deleted.
    * stress/big-wasm-memory.js:
    (test.foo):
    (test):
    (foo): Deleted.
    (catch): Deleted.
    
    Canonical link: https://commits.webkit.org/207059@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@238934 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/big-wasm-memory-grow-no-max.js
+++ JSTests/stress/big-wasm-memory-grow-no-max.js
@@ -1,34 +1,45 @@
 //@ skip if $memoryLimited
-let bigArray = new Array(0x7000000);
-bigArray[0] = 1.1;
-bigArray[1] = 1.2;
 
-function foo(array) {
-    var index = array.length;
-    if (index >= bigArray.length || (index - 0x1ffdc01) < 0)
+function test() {
+
+    // We don't support WebAssembly everywhere, so check for its existance before doing anything else.
+    if (!this.WebAssembly)
         return;
-    return bigArray[index - 0x1ffdc01];
+
+    let bigArray = new Array(0x7000000);
+    bigArray[0] = 1.1;
+    bigArray[1] = 1.2;
+
+    function foo(array) {
+        var index = array.length;
+        if (index >= bigArray.length || (index - 0x1ffdc01) < 0)
+            return;
+        return bigArray[index - 0x1ffdc01];
+    }
+
+    noInline(foo);
+
+    var okArray = new Uint8Array(0x1ffdc02);
+
+    for (var i = 0; i < 10000; ++i)
+        foo(okArray);
+
+    var ok = false;
+    try {
+        var memory = new WebAssembly.Memory({ initial: 0x1000 });
+        memory.grow(0x7000);
+        var result = foo(new Uint8Array(memory.buffer));
+        if (result !== void 0)
+            throw "Error: bad result at end: " + result;
+        ok = true;
+    } catch (e) {
+        if (e.toString() != "Error: Out of memory")
+            throw e;
+    }
+
+    if (ok)
+        throw "Error: did not throw error";
+
 }
 
-noInline(foo);
-
-var okArray = new Uint8Array(0x1ffdc02);
-
-for (var i = 0; i < 10000; ++i)
-    foo(okArray);
-
-var ok = false;
-try {
-    var memory = new WebAssembly.Memory({ initial: 0x1000 });
-    memory.grow(0x7000);
-    var result = foo(new Uint8Array(memory.buffer));
-    if (result !== void 0)
-        throw "Error: bad result at end: " + result;
-    ok = true;
-} catch (e) {
-    if (e.toString() != "Error: Out of memory")
-        throw e;
-}
-
-if (ok)
-    throw "Error: did not throw error";
+test();
