9efb68b5b2372d624912560385e147dcc08fbf4c
Mark Lam <mark.lam@apple.com>
2018-12-06 04:34:27 +0000

    speculationFromCell() should speculate non-Identifier strings as SpecString instead of SpecStringVar. https://bugs.webkit.org/show_bug.cgi?id=192441 <rdar://problem/46480355>
    Reviewed by Saam Barati.
    
    JSTests:
    
    * stress/regress-192441.js: Added.
    
    Source/JavaScriptCore:
    
    This is because a regular String (non-Identifier) can be converted into an
    Identifier.  During DFG/FTL compilation, AbstractValue::checkConsistency() may
    expect a value to be of type SpecStringVar, but the mutator thread may have
    converted the string into an Identifier.  This creates a race where
    AbstractValue::checkConsistency() may fail because it sees a SpecStringIdent when
    it expects the a SpecStringVar.
    
    The fix is to speculate non-Identifier strings as type SpecString which allows it
    to be SpecStringVar or SpecStringIndent.
    
    * bytecode/SpeculatedType.cpp:
    (JSC::speculationFromCell):
    
    
    Canonical link: https://commits.webkit.org/207051@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@238923 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/bytecode/SpeculatedType.cpp
+++ Source/JavaScriptCore/bytecode/SpeculatedType.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2017 Apple Inc. All rights reserved.
+ * Copyright (C) 2011-2018 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -480,7 +480,7 @@ SpeculatedType speculationFromCell(JSCell* cell)
             if (impl->isAtomic())
                 return SpecStringIdent;
         }
-        return SpecStringVar;
+        return SpecString;
     }
     return speculationFromStructure(cell->structure());
 }
