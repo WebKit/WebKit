23dfea9963ea2fc59de4d440a22939d65fb0497f
Jonathan Bedard <jbedard@apple.com>
2018-12-07 06:18:33 +0000

    Consecutive DumpRenderTree crashes are happening again on WinCairo BuildBots since r238903 https://bugs.webkit.org/show_bug.cgi?id=192486
    Unreviewed infrastructure fix.
    
    WinCairo bots define WEBKIT_TEST_CHILD_PROCESSES to run less processes due to RAM restrictions.
    
    
    * Scripts/webkitpy/layout_tests/run_webkit_tests.py:
    (_set_up_derived_options):
    
    
    Canonical link: https://commits.webkit.org/207074@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@238950 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/run_webkit_tests.py
+++ Tools/Scripts/webkitpy/layout_tests/run_webkit_tests.py
@@ -370,6 +370,9 @@ def _print_expectations(port, options, args, logging_stream):
 
 def _set_up_derived_options(port, options):
     """Sets the options values that depend on other options values."""
+    if not options.child_processes:
+        options.child_processes = os.environ.get('WEBKIT_TEST_CHILD_PROCESSES')
+
     if not options.configuration:
         options.configuration = port.default_configuration()
 
