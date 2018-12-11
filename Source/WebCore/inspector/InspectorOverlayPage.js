bdb876a6ec8021100050e6f25c5c6525b411dc4e
Devin Rousso <drousso@apple.com>
2018-12-11 19:27:26 +0000

    Web Inspector: overlay bounds rulers don't match element when page is scrolled https://bugs.webkit.org/show_bug.cgi?id=192577
    Reviewed by Joseph Pecoraro.
    
    When drawing the highlight for a node, the canvas is translated based on the scroll position
    of the node. This translation was not applied to the bounds calculations, which meant that
    the bounds always drew where the node would have been if it wasn't scrolled.
    
    * inspector/InspectorOverlayPage.js:
    (Bounds.prototype.get minX):
    (Bounds.prototype.get minY):
    (Bounds.prototype.get maxX):
    (Bounds.prototype.get maxY):
    (Bounds.prototype.offset): Added.
    (drawNodeHighlight):
    Drive-by: draw bounds for every node being highlighted instead of just the first one.
    Drive-by: switch the bounds color to be a semi-transparent red for more visibility/contrast.
    
    
    Canonical link: https://commits.webkit.org/207173@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@239079 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/inspector/InspectorOverlayPage.js
+++ Source/WebCore/inspector/InspectorOverlayPage.js
@@ -1,4 +1,4 @@
-const boundsColor = "rgba(0,0,0,0.4)";
+const boundsColor = "rgba(255,0,0,0.6)";
 const lightGridColor = "rgba(0,0,0,0.2)";
 const darkGridColor = "rgba(0,0,0,0.5)";
 const transparentColor = "rgba(0, 0, 0, 0)";
@@ -22,14 +22,17 @@ class Bounds {
         this._minY = Number.MAX_VALUE;
         this._maxX = Number.MIN_VALUE;
         this._maxY = Number.MIN_VALUE;
+
+        this._offsetX = 0;
+        this._offsetY = 0;
     }
 
     // Public
 
-    get minX() { return this._minX; }
-    get minY() { return this._minY; }
-    get maxX() { return this._maxX; }
-    get maxY() { return this._maxY; }
+    get minX() { return this._minX + this._offsetX; }
+    get minY() { return this._minY + this._offsetY; }
+    get maxX() { return this._maxX + this._offsetX; }
+    get maxY() { return this._maxY + this._offsetY; }
 
     update(x, y)
     {
@@ -37,6 +40,12 @@ class Bounds {
         this._minY = Math.min(this._minY, y);
         this._maxX = Math.max(this._maxX, x);
         this._maxY = Math.max(this._maxY, y);
+    }
+
+    offset(x, y)
+    {
+        this._offsetX = x || 0;
+        this._offsetY = y || 0;
     }
 }
 
@@ -50,9 +59,10 @@ function drawPausedInDebuggerMessage(message)
 
 function drawNodeHighlight(allHighlights)
 {
-    let bounds = new Bounds;
-
     for (let highlight of allHighlights) {
+        let bounds = new Bounds;
+        bounds.offset(-highlight.scrollOffset.x, -highlight.scrollOffset.y);
+
         _isolateActions(() => {
             context.translate(-highlight.scrollOffset.x, -highlight.scrollOffset.y);
 
@@ -62,10 +72,10 @@ function drawNodeHighlight(allHighlights)
             if (highlight.elementData && highlight.elementData.shapeOutsideData)
                 _drawShapeHighlight(highlight.elementData.shapeOutsideData, bounds);
         });
-    }
 
-    if (DATA.showRulers)
-        _drawBounds(bounds);
+        if (DATA.showRulers)
+            _drawBounds(bounds);
+    }
 
     if (allHighlights.length === 1) {
         for (let fragment of allHighlights[0].fragments)
