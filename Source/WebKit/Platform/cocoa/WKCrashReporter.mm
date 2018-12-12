93dbb226b1f618eb1f3df9d1330f73a56d931d56
Chris Dumez <cdumez@apple.com>
2018-12-12 18:08:49 +0000

    Unreviewed, fix build with recent SDKs.
    * Platform/cocoa/WKCrashReporter.mm:
    
    Canonical link: https://commits.webkit.org/207198@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@239110 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Platform/cocoa/WKCrashReporter.mm
+++ Source/WebKit/Platform/cocoa/WKCrashReporter.mm
@@ -33,7 +33,7 @@
 CRASH_REPORTER_CLIENT_HIDDEN
 struct crashreporter_annotations_t gCRAnnotations
 __attribute__((section("__DATA," CRASHREPORTER_ANNOTATIONS_SECTION)))
-    = { CRASHREPORTER_ANNOTATIONS_VERSION, 0, 0, 0, 0, 0, 0, 0 };
+    = { {CRASHREPORTER_ANNOTATIONS_VERSION}, {0}, {0}, {0}, {0}, {0}, {0}, {0} };
 
 namespace WebKit {
 
