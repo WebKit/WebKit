WebKit/WebKit

commit 8c36afdf9b602453520ea94c5baf749bb2924acf

[iOS] Send the full list of file upload URLs and types in PasteboardItemInfo https://bugs.webkit.org/show_bug.cgi?id=192598 Work towards <rdar://problem/35626913>
Reviewed by Tim Horton.

Refactors PasteboardItemInfo to contain lists of file URLs and corresponding pasteboard types, instead of just
a "preferred" file upload URL and type. See below for more details.

* platform/PasteboardItemInfo.h:
(WebCore::PasteboardItemInfo::pathForContentType const):

Add a helper method to find a file upload URL corresponding to a given type.

(WebCore::PasteboardItemInfo::encode const):
(WebCore::PasteboardItemInfo::decode):

Change `pathForFileUpload` to `pathsForFileUpload`, and `contentTypeForFileUpload` to `contentTypesForFileUpload`.

* platform/ios/AbstractPasteboard.h:
* platform/ios/PasteboardIOS.mm:
(WebCore::Pasteboard::readRespectingUTIFidelities):

Adjust this to take the file path for the highest fidelity representation in `pathsForContentType`.

(WebCore::Pasteboard::readFilePaths):
* platform/ios/PlatformPasteboardIOS.mm:
(WebCore::PlatformPasteboard::informationForItemAtIndex):
* platform/ios/WebItemProviderPasteboard.h:
* platform/ios/WebItemProviderPasteboard.mm:
(-[NSItemProvider web_containsFileURLAndFileUploadContent]):
(-[NSItemProvider web_fileUploadContentTypes]):

Replace `web_containsFileUploadContent` with `web_fileUploadContentTypes`, which returns the full list of file
upload content types (rather than just a `BOOL` indicating whether one or more of these types exist).

(-[WebItemProviderPasteboard fileUploadURLsAtIndex:fileTypes:]):
(-[WebItemProviderPasteboard numberOfFiles]):
(-[NSItemProvider web_containsFileUploadContent]): Deleted.
(-[WebItemProviderPasteboard preferredFileUploadURLAtIndex:fileType:]): Deleted.

Replaced with `-fileUploadURLsAtIndex:fileTypes:`. This implementation currently just returns the highest
fidelity loaded type identifier, but this is wrong because it doesn't take into account inline data types that

Source/WebCore/platform/PasteboardItemInfo.h
@@ -26,6 +26,7 @@
 #pragma once
 
 #include <wtf/Optional.h>
+#include <wtf/Vector.h>
 #include <wtf/text/WTFString.h>
 
 namespace WebCore {
@@ -37,12 +38,22 @@ enum class PasteboardItemPresentationStyle {
 };
 
 struct PasteboardItemInfo {
-    String pathForFileUpload;
-    String contentTypeForFileUpload;
+    Vector<String> pathsForFileUpload;
+    Vector<String> contentTypesForFileUpload;
     String suggestedFileName;
     bool isNonTextType { false };
     bool containsFileURLAndFileUploadContent { false };
     PasteboardItemPresentationStyle preferredPresentationStyle { PasteboardItemPresentationStyle::Unspecified };
+
+    String pathForContentType(const String& type) const
+    {
+        ASSERT(pathsForFileUpload.size() == contentTypesForFileUpload.size());
+        auto index = contentTypesForFileUpload.find(type);
+        if (index == notFound)
+            return { };
+
+        return pathsForFileUpload[index];
+    }
 
     template<class Encoder> void encode(Encoder&) const;
     template<class Decoder> static std::optional<PasteboardItemInfo> decode(Decoder&);
@@ -51,7 +62,7 @@ struct PasteboardItemInfo {
 template<class Encoder>
 void PasteboardItemInfo::encode(Encoder& encoder) const
 {
-    encoder << pathForFileUpload << contentTypeForFileUpload << suggestedFileName << isNonTextType << containsFileURLAndFileUploadContent;
+    encoder << pathsForFileUpload << contentTypesForFileUpload << suggestedFileName << isNonTextType << containsFileURLAndFileUploadContent;
     encoder.encodeEnum(preferredPresentationStyle);
 }
 
@@ -59,10 +70,10 @@ template<class Decoder>
 std::optional<PasteboardItemInfo> PasteboardItemInfo::decode(Decoder& decoder)
 {
     PasteboardItemInfo result;
-    if (!decoder.decode(result.pathForFileUpload))
+    if (!decoder.decode(result.pathsForFileUpload))
         return std::nullopt;
 
-    if (!decoder.decode(result.contentTypeForFileUpload))
+    if (!decoder.decode(result.contentTypesForFileUpload))
         return std::nullopt;
 
     if (!decoder.decode(result.suggestedFileName))

Source/WebCore/platform/ios/PasteboardIOS.mm
@@ -294,9 +294,9 @@ void Pasteboard::readRespectingUTIFidelities(PasteboardWebContentReader& reader,
     for (NSUInteger index = 0, numberOfItems = strategy.getPasteboardItemsCount(m_pasteboardName); index < numberOfItems; ++index) {
 #if ENABLE(ATTACHMENT_ELEMENT)
         auto info = strategy.informationForItemAtIndex(index, m_pasteboardName);
-        bool canReadAttachment = policy == WebContentReadingPolicy::AnyType && RuntimeEnabledFeatures::sharedFeatures().attachmentElementEnabled() && !info.pathForFileUpload.isEmpty();
+        bool canReadAttachment = policy == WebContentReadingPolicy::AnyType && RuntimeEnabledFeatures::sharedFeatures().attachmentElementEnabled() && !info.pathsForFileUpload.isEmpty();
         if (canReadAttachment && info.preferredPresentationStyle == PasteboardItemPresentationStyle::Attachment) {
-            reader.readFilePaths({ info.pathForFileUpload });
+            reader.readFilePaths({ info.pathsForFileUpload.first() });
             continue;
         }
 #endif
@@ -317,7 +317,7 @@ void Pasteboard::readRespectingUTIFidelities(PasteboardWebContentReader& reader,
         }
 #if ENABLE(ATTACHMENT_ELEMENT)
         if (canReadAttachment && result == ReaderResult::DidNotReadType)
-            reader.readFilePaths({ info.pathForFileUpload });
+            reader.readFilePaths({ info.pathsForFileUpload.first() });
 #endif
     }
 }
@@ -459,7 +459,7 @@ Vector<String> Pasteboard::readFilePaths()
     auto& strategy = *platformStrategies()->pasteboardStrategy();
     for (NSUInteger index = 0, numberOfItems = strategy.getPasteboardItemsCount(m_pasteboardName); index < numberOfItems; ++index) {
         // Currently, drag and drop is the only case on iOS where the "pasteboard" may contain file paths.
-        auto filePath = strategy.informationForItemAtIndex(index, m_pasteboardName).pathForFileUpload;
+        auto filePath = strategy.informationForItemAtIndex(index, m_pasteboardName).pathsForFileUpload.first();
         if (!filePath.isEmpty())
             filePaths.append(WTFMove(filePath));
     }

Source/WebCore/platform/ios/AbstractPasteboard.h
@@ -57,7 +57,10 @@ NS_ASSUME_NONNULL_BEGIN
 - (NSArray<NSString *> *)pasteboardTypesByFidelityForItemAtIndex:(NSUInteger)index;
 @property (readonly, nonatomic) NSInteger numberOfFiles;
 @property (readonly, nonatomic) NSArray<NSURL *> *allDroppedFileURLs;
-- (nullable NSURL *)preferredFileUploadURLAtIndex:(NSUInteger)index fileType:(NSString *_Nullable *_Nullable)outFileType;
+
+// Computes lists of file URLs and types. Each file URL and type corresponds to a representation of the item provider at the given index.
+// In order from highest fidelity to lowest fidelity.
+- (NSArray<NSURL *> *)fileUploadURLsAtIndex:(NSUInteger)index fileTypes:(NSArray<NSString *> *_Nullable *_Nonnull)outFileTypes;
 - (void)updateSupportedTypeIdentifiers:(NSArray<NSString *> *)types;
 
 @end

Source/WebCore/platform/ios/PlatformPasteboardIOS.mm
@@ -144,10 +144,18 @@ PasteboardItemInfo PlatformPasteboard::informationForItemAtIndex(int index)
         return { };
 
     PasteboardItemInfo info;
-    if ([m_pasteboard respondsToSelector:@selector(preferredFileUploadURLAtIndex:fileType:)]) {
-        NSString *fileType = nil;
-        info.pathForFileUpload = [m_pasteboard preferredFileUploadURLAtIndex:index fileType:&fileType].path;
-        info.contentTypeForFileUpload = fileType;
+    if ([m_pasteboard respondsToSelector:@selector(fileUploadURLsAtIndex:fileTypes:)]) {
+        NSArray<NSString *> *fileTypes = nil;
+        NSArray *urls = [m_pasteboard fileUploadURLsAtIndex:index fileTypes:&fileTypes];
+        ASSERT(fileTypes.count == urls.count);
+
+        info.pathsForFileUpload.reserveInitialCapacity(urls.count);
+        for (NSURL *url in urls)
+            info.pathsForFileUpload.uncheckedAppend(url.path);
+
+        info.contentTypesForFileUpload.reserveInitialCapacity(fileTypes.count);
+        for (NSString *fileType : fileTypes)
+            info.contentTypesForFileUpload.uncheckedAppend(fileType);
     }
 
     NSItemProvider *itemProvider = [[m_pasteboard itemProviders] objectAtIndex:index];

Source/WebCore/platform/ios/WebItemProviderPasteboard.h
@@ -97,9 +97,6 @@ WEBCORE_EXPORT @interface WebItemProviderPasteboard : NSObject<AbstractPasteboar
 // This will only be non-empty when an operation is being performed.
 @property (readonly, nonatomic) NSArray<NSURL *> *allDroppedFileURLs;
 
-// The preferred file URL corresponds to the highest fidelity non-private UTI that was loaded.
-- (nullable NSURL *)preferredFileUploadURLAtIndex:(NSUInteger)index fileType:(NSString *_Nullable *_Nullable)outFileType;
-
 @property (readonly, nonatomic) BOOL hasPendingOperation;
 - (void)incrementPendingOperationCount;
 - (void)decrementPendingOperationCount;

Source/WebCore/platform/ios/WebItemProviderPasteboard.mm
@@ -68,21 +68,23 @@ static BOOL typeConformsToTypes(NSString *type, NSArray *conformsToTypes)
 {
     for (NSString *identifier in self.registeredTypeIdentifiers) {
         if (UTTypeConformsTo((__bridge CFStringRef)identifier, kUTTypeFileURL))
-            return self.web_containsFileUploadContent;
+            return self.web_fileUploadContentTypes.count;
     }
     return NO;
 }
 
-- (BOOL)web_containsFileUploadContent
+- (NSArray<NSString *> *)web_fileUploadContentTypes
 {
+    auto types = adoptNS([NSMutableArray new]);
     for (NSString *identifier in self.registeredTypeIdentifiers) {
         if (UTTypeConformsTo((__bridge CFStringRef)identifier, kUTTypeURL))
             continue;
 
         if (typeConformsToTypes(identifier, Pasteboard::supportedFileUploadPasteboardTypes()))
-            return YES;
+            [types addObject:identifier];
     }
-    return NO;
+
+    return types.autorelease();
 }
 
 @end
@@ -628,35 +630,27 @@ static Class classForTypeIdentifier(NSString *typeIdentifier, NSString *&outType
     return _changeCount;
 }
 
-- (NSURL *)preferredFileUploadURLAtIndex:(NSUInteger)index fileType:(NSString **)outFileType
+- (NSArray<NSURL *> *)fileUploadURLsAtIndex:(NSUInteger)index fileTypes:(NSArray<NSString *> **)outFileTypes
 {
-    if (outFileType)
-        *outFileType = nil;
+    auto fileTypes = adoptNS([NSMutableArray new]);
+    auto fileURLs = adoptNS([NSMutableArray new]);
 
     if (index >= _loadResults.size())
-        return nil;
+        return @[ ];
 
     auto result = _loadResults[index];
     if (![result canBeRepresentedAsFileUpload])
-        return nil;
+        return @[ ];
 
-    NSItemProvider *itemProvider = [result itemProvider];
-    for (NSString *registeredTypeIdentifier in itemProvider.registeredTypeIdentifiers) {
-        // Search for the highest fidelity non-private type identifier we loaded from the item provider.
-        if (!UTTypeIsDeclared((__bridge CFStringRef)registeredTypeIdentifier) && !UTTypeIsDynamic((__bridge CFStringRef)registeredTypeIdentifier))
-            continue;
-
-        for (NSString *loadedTypeIdentifier in [result loadedTypeIdentifiers]) {
-            if (!UTTypeConformsTo((__bridge CFStringRef)registeredTypeIdentifier, (__bridge CFStringRef)loadedTypeIdentifier))
-                continue;
-
-            if (outFileType)
-                *outFileType = loadedTypeIdentifier;
-            return [result fileURLForType:loadedTypeIdentifier];
+    for (NSString *contentType in [result itemProvider].web_fileUploadContentTypes) {
+        if (NSURL *url = [result fileURLForType:contentType]) {
+            [fileTypes addObject:contentType];
+            [fileURLs addObject:url];
         }
     }
 
-    return nil;
+    *outFileTypes = fileTypes.autorelease();
+    return fileURLs.autorelease();
 }
 
 - (NSArray<NSURL *> *)allDroppedFileURLs
@@ -684,7 +678,7 @@ static Class classForTypeIdentifier(NSString *typeIdentifier, NSString *&outType
         }
 #endif
         // Otherwise, fall back to examining the item's registered type identifiers.
-        if (itemProvider.web_containsFileUploadContent)
+        if (itemProvider.web_fileUploadContentTypes.count)
             ++numberOfFiles;
     }
     return numberOfFiles;

commit 0423f4ac931170654c1ff7a994a4ff3811d2d5d5

[iOS] A few API tests are failing after r239086 https://bugs.webkit.org/show_bug.cgi?id=192608
Reviewed by Zalan Bujtas.

These test failures were caused by a missing Vector size check in `Pasteboard::readFilePaths` before accessing
the first item. Fix this by adding a helper method on PasteboardItemInfo to grab the file path for the highest
fidelity pasteboard item (returning the null string if there are none), and use this in a few places that grab
the highest fidelity path using Vector::first().

While `Pasteboard::readRespectingUTIFidelities` does have a bounds check before accessing the list of paths,
this patch still replaces it with a call to `pathForHighestFidelityItem()`, so that the intent is more clear.

* platform/PasteboardItemInfo.h:
(WebCore::PasteboardItemInfo::pathForHighestFidelityItem const):
* platform/ios/PasteboardIOS.mm:
(WebCore::Pasteboard::readRespectingUTIFidelities):
(WebCore::Pasteboard::readFilePaths):


Canonical link: https://commits.webkit.org/207197@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@239107 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/PasteboardItemInfo.h
@@ -55,6 +55,15 @@ struct PasteboardItemInfo {
         return pathsForFileUpload[index];
     }
 
+    String pathForHighestFidelityItem() const
+    {
+        if (pathsForFileUpload.isEmpty())
+            return { };
+
+        ASSERT(!pathsForFileUpload.first().isEmpty());
+        return pathsForFileUpload.first();
+    }
+
     template<class Encoder> void encode(Encoder&) const;
     template<class Decoder> static std::optional<PasteboardItemInfo> decode(Decoder&);
 };

Source/WebCore/platform/ios/PasteboardIOS.mm
@@ -294,9 +294,10 @@ void Pasteboard::readRespectingUTIFidelities(PasteboardWebContentReader& reader,
     for (NSUInteger index = 0, numberOfItems = strategy.getPasteboardItemsCount(m_pasteboardName); index < numberOfItems; ++index) {
 #if ENABLE(ATTACHMENT_ELEMENT)
         auto info = strategy.informationForItemAtIndex(index, m_pasteboardName);
-        bool canReadAttachment = policy == WebContentReadingPolicy::AnyType && RuntimeEnabledFeatures::sharedFeatures().attachmentElementEnabled() && !info.pathsForFileUpload.isEmpty();
+        auto attachmentFilePath = info.pathForHighestFidelityItem();
+        bool canReadAttachment = policy == WebContentReadingPolicy::AnyType && RuntimeEnabledFeatures::sharedFeatures().attachmentElementEnabled() && !attachmentFilePath.isEmpty();
         if (canReadAttachment && info.preferredPresentationStyle == PasteboardItemPresentationStyle::Attachment) {
-            reader.readFilePaths({ info.pathsForFileUpload.first() });
+            reader.readFilePaths({ WTFMove(attachmentFilePath) });
             continue;
         }
 #endif
@@ -317,7 +318,7 @@ void Pasteboard::readRespectingUTIFidelities(PasteboardWebContentReader& reader,
         }
 #if ENABLE(ATTACHMENT_ELEMENT)
         if (canReadAttachment && result == ReaderResult::DidNotReadType)
-            reader.readFilePaths({ info.pathsForFileUpload.first() });
+            reader.readFilePaths({ WTFMove(attachmentFilePath) });
 #endif
     }
 }
@@ -459,7 +460,7 @@ Vector<String> Pasteboard::readFilePaths()
     auto& strategy = *platformStrategies()->pasteboardStrategy();
     for (NSUInteger index = 0, numberOfItems = strategy.getPasteboardItemsCount(m_pasteboardName); index < numberOfItems; ++index) {
         // Currently, drag and drop is the only case on iOS where the "pasteboard" may contain file paths.
-        auto filePath = strategy.informationForItemAtIndex(index, m_pasteboardName).pathsForFileUpload.first();
+        auto filePath = strategy.informationForItemAtIndex(index, m_pasteboardName).pathForHighestFidelityItem();
         if (!filePath.isEmpty())
             filePaths.append(WTFMove(filePath));
     }