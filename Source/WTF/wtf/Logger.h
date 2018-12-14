81e8c6285e353061d1c4f76b444895c6155a85c9
David Kilzer <ddkilzer@webkit.org>
2018-12-14 21:50:47 +0000

    clang-tidy: Fix unnecessary copy of AtomicString each time one is logged <https://webkit.org/b/192710> <rdar://problem/46738962>
    Reviewed by Eric Carlson.
    
    * wtf/Logger.h:
    (WTF::LogArgument::toString): Make argument a const reference to
    avoid the copy.
    
    
    Canonical link: https://commits.webkit.org/207298@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@239232 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/Logger.h
+++ Source/WTF/wtf/Logger.h
@@ -44,7 +44,7 @@ struct LogArgument {
     template<typename U = T> static typename std::enable_if<std::is_same<U, long>::value, String>::type toString(long argument) { return String::number(argument); }
     template<typename U = T> static typename std::enable_if<std::is_same<U, float>::value, String>::type toString(float argument) { return String::number(argument); }
     template<typename U = T> static typename std::enable_if<std::is_same<U, double>::value, String>::type toString(double argument) { return String::number(argument); }
-    template<typename U = T> static typename std::enable_if<std::is_same<typename std::remove_reference<U>::type, AtomicString>::value, String>::type toString(AtomicString argument) { return argument.string(); }
+    template<typename U = T> static typename std::enable_if<std::is_same<typename std::remove_reference<U>::type, AtomicString>::value, String>::type toString(const AtomicString& argument) { return argument.string(); }
     template<typename U = T> static typename std::enable_if<std::is_same<typename std::remove_reference<U>::type, String>::value, String>::type toString(String argument) { return argument; }
     template<typename U = T> static typename std::enable_if<std::is_same<typename std::remove_reference<U>::type, StringBuilder*>::value, String>::type toString(StringBuilder* argument) { return argument->toString(); }
     template<typename U = T> static typename std::enable_if<std::is_same<U, const char*>::value, String>::type toString(const char* argument) { return String(argument); }
