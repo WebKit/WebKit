WebKit/WebKit

commit 66ea0fe16082a976e534ec97302de967ce5ca59a

AX: Support keyboard access preference for iOS in WebKit https://bugs.webkit.org/show_bug.cgi?id=192373 <rdar://problem/46462670>
Reviewed by Tim Horton.

* Platform/spi/ios/AccessibilitySupportSPI.h:
* PlatformMac.cmake:
* SourcesCocoa.txt:
* UIProcess/Cocoa/WKFullKeyboardAccessWatcher.h: Added.
* UIProcess/Cocoa/WKFullKeyboardAccessWatcher.mm: Added.
(platformIsFullKeyboardAccessEnabled):
(-[WKFullKeyboardAccessWatcher notifyAllProcessPools]):
(-[WKFullKeyboardAccessWatcher retrieveKeyboardUIModeFromPreferences:]):
(-[WKFullKeyboardAccessWatcher init]):
(+[WKFullKeyboardAccessWatcher fullKeyboardAccessEnabled]):
* UIProcess/ios/WebProcessProxyIOS.mm:
(WebKit::WebProcessProxy::fullKeyboardAccessEnabled):
* UIProcess/mac/WKFullKeyboardAccessWatcher.h: Removed.
* UIProcess/mac/WKFullKeyboardAccessWatcher.mm: Removed.
* WebKit.xcodeproj/project.pbxproj:


Canonical link: https://commits.webkit.org/207324@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@239262 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/Platform/spi/ios/AccessibilitySupportSPI.h
@@ -38,5 +38,7 @@ extern Boolean _AXSKeyRepeatEnabled();
 extern CFTimeInterval _AXSKeyRepeatDelay();
 extern CFStringRef kAXSWebAccessibilityEventsEnabledNotification;
 extern Boolean _AXSWebAccessibilityEventsEnabled();
+extern CFStringRef kAXSFullKeyboardAccessEnabledNotification;
+extern Boolean _AXSFullKeyboardAccessEnabled();
 
 WTF_EXTERN_C_END

Source/WebKit/UIProcess/Cocoa/WKFullKeyboardAccessWatcher.h
@@ -23,10 +23,9 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef WKFullKeyboardAccessWatcher_h
-#define WKFullKeyboardAccessWatcher_h
+#pragma once
 
-#if PLATFORM(MAC)
+#if PLATFORM(COCOA)
 
 @interface WKFullKeyboardAccessWatcher : NSObject {
 @private
@@ -37,6 +36,4 @@
 
 @end;
 
-#endif // PLATFORM(MAC)
-
-#endif // WKFullKeyboardAccessWatcher_h
+#endif // PLATFORM(COCOA)

Source/WebKit/UIProcess/Cocoa/WKFullKeyboardAccessWatcher.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2011, 2018 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -26,14 +26,38 @@
 #import "config.h"
 #import "WKFullKeyboardAccessWatcher.h"
 
-#if PLATFORM(MAC)
+#if PLATFORM(COCOA)
 
+#import "AccessibilitySupportSPI.h"
 #import "WebProcessPool.h"
 
-NSString * const KeyboardUIModeDidChangeNotification = @"com.apple.KeyboardUIModeDidChange";
-const CFStringRef AppleKeyboardUIMode = CFSTR("AppleKeyboardUIMode");
+#if PLATFORM(MAC)
+static NSString * const KeyboardUIModeDidChangeNotification = @"com.apple.KeyboardUIModeDidChange";
+static const CFStringRef AppleKeyboardUIMode = CFSTR("AppleKeyboardUIMode");
+#endif
 
 @implementation WKFullKeyboardAccessWatcher
+
+static inline BOOL platformIsFullKeyboardAccessEnabled()
+{
+    BOOL fullKeyboardAccessEnabled = NO;
+#if PLATFORM(MAC)
+    CFPreferencesAppSynchronize(kCFPreferencesCurrentApplication);
+    
+    Boolean keyExistsAndHasValidFormat;
+    int mode = CFPreferencesGetAppIntegerValue(AppleKeyboardUIMode, kCFPreferencesCurrentApplication, &keyExistsAndHasValidFormat);
+    if (keyExistsAndHasValidFormat) {
+        // The keyboard access mode has two bits:
+        // Bit 0 is set if user can set the focus to menus, the dock, and various windows using the keyboard.
+        // Bit 1 is set if controls other than text fields are included in the tab order (WebKit also always includes lists).
+        fullKeyboardAccessEnabled = (mode & 0x2);
+    }
+#elif PLATFORM(IOS_FAMILY) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 130000
+    fullKeyboardAccessEnabled = _AXSFullKeyboardAccessEnabled();
+#endif
+    
+    return fullKeyboardAccessEnabled;
+}
 
 - (void)notifyAllProcessPools
 {
@@ -45,17 +69,8 @@ const CFStringRef AppleKeyboardUIMode = CFSTR("AppleKeyboardUIMode");
 {
     BOOL oldValue = fullKeyboardAccessEnabled;
 
-    CFPreferencesAppSynchronize(kCFPreferencesCurrentApplication);
-
-    Boolean keyExistsAndHasValidFormat;
-    int mode = CFPreferencesGetAppIntegerValue(AppleKeyboardUIMode, kCFPreferencesCurrentApplication, &keyExistsAndHasValidFormat);
-    if (keyExistsAndHasValidFormat) {
-        // The keyboard access mode has two bits:
-        // Bit 0 is set if user can set the focus to menus, the dock, and various windows using the keyboard.
-        // Bit 1 is set if controls other than text fields are included in the tab order (WebKit also always includes lists).
-        fullKeyboardAccessEnabled = (mode & 0x2);
-    }
-
+    fullKeyboardAccessEnabled = platformIsFullKeyboardAccessEnabled();
+    
     if (fullKeyboardAccessEnabled != oldValue)
         [self notifyAllProcessPools];
 }
@@ -68,9 +83,19 @@ const CFStringRef AppleKeyboardUIMode = CFSTR("AppleKeyboardUIMode");
 
     [self retrieveKeyboardUIModeFromPreferences:nil];
 
-    [[NSDistributedNotificationCenter defaultCenter] 
-        addObserver:self selector:@selector(retrieveKeyboardUIModeFromPreferences:) 
-        name:KeyboardUIModeDidChangeNotification object:nil];
+    NSNotificationCenter *notificationCenter = nil;
+    NSString *notitificationName = nil;
+    
+#if PLATFORM(MAC)
+    notificationCenter = [NSDistributedNotificationCenter defaultCenter];
+    notitificationName = KeyboardUIModeDidChangeNotification;
+#elif PLATFORM(IOS_FAMILY) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 130000
+    notificationCenter = [NSNotificationCenter defaultCenter];
+    notitificationName = (NSString *)kAXSFullKeyboardAccessEnabledNotification;
+#endif
+    
+    if (notitificationName)
+        [notificationCenter addObserver:self selector:@selector(retrieveKeyboardUIModeFromPreferences:) name:notitificationName object:nil];
 
     return self;
 }
@@ -83,4 +108,4 @@ const CFStringRef AppleKeyboardUIMode = CFSTR("AppleKeyboardUIMode");
 
 @end
 
-#endif // PLATFORM(MAC)
+#endif // PLATFORM(COCOA)

Source/WebKit/PlatformMac.cmake
@@ -256,6 +256,7 @@ list(APPEND WebKit_SOURCES
     UIProcess/Cocoa/UIDelegate.mm
     UIProcess/Cocoa/VersionChecks.mm
     UIProcess/Cocoa/ViewGestureController.cpp
+    UIProcess/Cocoa/WKFullKeyboardAccessWatcher.mm
     UIProcess/Cocoa/WKReloadFrameErrorRecoveryAttempter.mm
     UIProcess/Cocoa/WKWebViewContentProviderRegistry.mm
     UIProcess/Cocoa/WebPageProxyCocoa.mm
@@ -296,7 +297,6 @@ list(APPEND WebKit_SOURCES
     UIProcess/mac/TiledCoreAnimationDrawingAreaProxy.mm
     UIProcess/mac/ViewGestureControllerMac.mm
     UIProcess/mac/ViewSnapshotStore.mm
-    UIProcess/mac/WKFullKeyboardAccessWatcher.mm
     UIProcess/mac/WKFullScreenWindowController.mm
     UIProcess/mac/WKImmediateActionController.mm
     UIProcess/mac/WKInspectorViewController.mm

Source/WebKit/UIProcess/ios/WebProcessProxyIOS.mm
@@ -28,16 +28,14 @@
 
 #if PLATFORM(IOS_FAMILY)
 
-#import <WebCore/NotImplemented.h>
-
+#import "WKFullKeyboardAccessWatcher.h"
 #import "WebProcessMessages.h"
 
 namespace WebKit {
 
 bool WebProcessProxy::fullKeyboardAccessEnabled()
 {
-    notImplemented();
-    return false;
+    return [WKFullKeyboardAccessWatcher fullKeyboardAccessEnabled];
 }
 
 } // namespace WebKit

Source/WebKit/WebKit.xcodeproj/project.pbxproj
@@ -5332,6 +5332,8 @@
 				51D124321E6DE521002B2820 /* WebURLSchemeHandlerCocoa.mm */,
 				2DFC7DB91BCCC19500C1548C /* WebViewImpl.h */,
 				2DFC7DBA1BCCC19500C1548C /* WebViewImpl.mm */,
+				E1AEA22D14687BDB00804569 /* WKFullKeyboardAccessWatcher.h */,
+				E1AEA22E14687BDB00804569 /* WKFullKeyboardAccessWatcher.mm */,
 				1AD01BCB1905D54900C9C45F /* WKReloadFrameErrorRecoveryAttempter.h */,
 				1AD01BCA1905D54900C9C45F /* WKReloadFrameErrorRecoveryAttempter.mm */,
 				5CA26D80217ABBB600F97A35 /* WKSafeBrowsingWarning.h */,
@@ -8163,8 +8165,6 @@
 				51D130571382F10500351EDD /* WebProcessProxyMac.mm */,
 				868160CD18763D4B0021E79D /* WindowServerConnection.h */,
 				868160CF187645370021E79D /* WindowServerConnection.mm */,
-				E1AEA22D14687BDB00804569 /* WKFullKeyboardAccessWatcher.h */,
-				E1AEA22E14687BDB00804569 /* WKFullKeyboardAccessWatcher.mm */,
 				CDCA85C7132ABA4E00E961DF /* WKFullScreenWindowController.h */,
 				CDCA85C6132ABA4E00E961DF /* WKFullScreenWindowController.mm */,
 				9321D5851A38EE3C008052BE /* WKImmediateActionController.h */,

commit 2b1439deab5d2fd11d452101fcfef51c05c24186

Some builds are broken after r239262 https://bugs.webkit.org/show_bug.cgi?id=192777
Reviewed by Simon Fraser.

Source/WebKit:

* Platform/spi/ios/AccessibilitySupportSPI.h:
* UIProcess/Cocoa/WKFullKeyboardAccessWatcher.h:
* UIProcess/Cocoa/WKFullKeyboardAccessWatcher.mm:
(platformIsFullKeyboardAccessEnabled):
(-[WKFullKeyboardAccessWatcher init]):
Fix the build by being more clear about when it's OK to use AccessibilitySupport.

Source/WTF:

* wtf/Platform.h:


Canonical link: https://commits.webkit.org/207358@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@239311 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/Platform.h
@@ -1454,3 +1454,11 @@
 /* FIXME: Is this still necessary? CFURLConnection isn't used on Cocoa platforms any more. */
 #define ENABLE_SEC_ITEM_SHIM 1
 #endif
+
+#if (PLATFORM(IOS_FAMILY) || (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101400))
+#define HAVE_ACCESSIBILITY_SUPPORT 1
+#endif
+
+#if PLATFORM(MAC)
+#define ENABLE_FULL_KEYBOARD_ACCESS 1
+#endif

Source/WebKit/Platform/spi/ios/AccessibilitySupportSPI.h
@@ -25,20 +25,36 @@
 
 #pragma once
 
+#if HAVE(ACCESSIBILITY_SUPPORT)
+
 #if USE(APPLE_INTERNAL_SDK)
+
 // FIXME (46432011): We shouldn't need to wrap this include in extern "C".
 WTF_EXTERN_C_BEGIN
 #include <AccessibilitySupport.h>
 WTF_EXTERN_C_END
-#endif
+
+#else
 
 WTF_EXTERN_C_BEGIN
 
+#if PLATFORM(IOS_FAMILY)
 extern Boolean _AXSKeyRepeatEnabled();
 extern CFTimeInterval _AXSKeyRepeatDelay();
+#endif
+
+#if ENABLE(ACCESSIBILITY_EVENTS)
 extern CFStringRef kAXSWebAccessibilityEventsEnabledNotification;
 extern Boolean _AXSWebAccessibilityEventsEnabled();
+#endif
+
+#if PLATFORM(IOS_FAMILY) && ENABLE(FULL_KEYBOARD_ACCESS)
 extern CFStringRef kAXSFullKeyboardAccessEnabledNotification;
 extern Boolean _AXSFullKeyboardAccessEnabled();
+#endif
 
 WTF_EXTERN_C_END
+
+#endif
+
+#endif // HAVE(ACCESSIBILITY_SUPPORT)

Source/WebKit/UIProcess/Cocoa/WKFullKeyboardAccessWatcher.h
@@ -25,7 +25,7 @@
 
 #pragma once
 
-#if PLATFORM(COCOA)
+#if ENABLE(FULL_KEYBOARD_ACCESS)
 
 @interface WKFullKeyboardAccessWatcher : NSObject {
 @private
@@ -36,4 +36,4 @@
 
 @end;
 
-#endif // PLATFORM(COCOA)
+#endif // ENABLE(FULL_KEYBOARD_ACCESS)

Source/WebKit/UIProcess/Cocoa/WKFullKeyboardAccessWatcher.mm
@@ -26,7 +26,7 @@
 #import "config.h"
 #import "WKFullKeyboardAccessWatcher.h"
 
-#if PLATFORM(COCOA)
+#if ENABLE(FULL_KEYBOARD_ACCESS)
 
 #import "WebProcessPool.h"
 
@@ -55,7 +55,7 @@ static inline BOOL platformIsFullKeyboardAccessEnabled()
         // Bit 1 is set if controls other than text fields are included in the tab order (WebKit also always includes lists).
         fullKeyboardAccessEnabled = (mode & 0x2);
     }
-#elif PLATFORM(IOS_FAMILY) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 130000
+#elif PLATFORM(IOS_FAMILY)
     fullKeyboardAccessEnabled = _AXSFullKeyboardAccessEnabled();
 #endif
     
@@ -92,7 +92,7 @@ static inline BOOL platformIsFullKeyboardAccessEnabled()
 #if PLATFORM(MAC)
     notificationCenter = [NSDistributedNotificationCenter defaultCenter];
     notitificationName = KeyboardUIModeDidChangeNotification;
-#elif PLATFORM(IOS_FAMILY) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 130000
+#elif PLATFORM(IOS_FAMILY)
     notificationCenter = [NSNotificationCenter defaultCenter];
     notitificationName = (NSString *)kAXSFullKeyboardAccessEnabledNotification;
 #endif
@@ -111,4 +111,4 @@ static inline BOOL platformIsFullKeyboardAccessEnabled()
 
 @end
 
-#endif // PLATFORM(COCOA)
+#endif // ENABLE(FULL_KEYBOARD_ACCESS)