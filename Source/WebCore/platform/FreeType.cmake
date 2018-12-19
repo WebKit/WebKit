cb0d192eb89032d7896af623a3b68b4c1ae92b4f
Don Olmstead <don.olmstead@sony.com>
2018-12-19 18:23:36 +0000

    Sync some include directories in WebCore https://bugs.webkit.org/show_bug.cgi?id=192819
    Reviewed by Michael Catanzaro.
    
    Added missing include directories around features that have only been enabled on
    Apple ports. Removes obsolete directories from list and moves harfbuzz directories
    into the freetype cmake file.
    
    * CMakeLists.txt:
    * platform/FreeType.cmake:
    
    
    Canonical link: https://commits.webkit.org/207426@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@239379 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/FreeType.cmake
+++ Source/WebCore/platform/FreeType.cmake
@@ -1,6 +1,7 @@
 list(APPEND WebCore_INCLUDE_DIRECTORIES
     "${WEBCORE_DIR}/platform/graphics/freetype"
     "${WEBCORE_DIR}/platform/graphics/harfbuzz"
+    "${WEBCORE_DIR}/platform/graphics/harfbuzz/ng"
 )
 
 list(APPEND WebCore_SOURCES
