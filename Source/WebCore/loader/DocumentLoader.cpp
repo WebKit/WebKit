7a6562592ad86f547316d1324357c80ce3166c7f
Antti Koivisto <koivisto@iki.fi>
2018-12-23 01:09:21 +0000

    Merge r219121 - Synchronous media query evaluation could destroy current Frame/FrameView. https://bugs.webkit.org/show_bug.cgi?id=192781 <rdar://problem/34416793>
    Reviewed by Chris Dumez.
    
    Source/WebCore:
    
    Protect Frame and FrameView when coming back from printing and check if the current Frame/FrameView/FrameLoader objects are still valid.
    
    Test: printing/print-with-media-query-destory.html
    
    * loader/DocumentLoader.cpp:
    (WebCore::DocumentLoader::finishedLoading):
    * page/Frame.cpp:
    (WebCore::Frame::setPrinting):
    * page/FrameView.cpp:
    (WebCore::FrameView::forceLayoutForPagination):
    * page/PrintContext.cpp:
    (WebCore::PrintContext::PrintContext):
    (WebCore::PrintContext::computePageRects):
    (WebCore::PrintContext::computePageRectsWithPageSizeInternal):
    (WebCore::PrintContext::begin):
    (WebCore::PrintContext::computeAutomaticScaleFactor):
    (WebCore::PrintContext::spoolPage):
    (WebCore::PrintContext::spoolRect):
    (WebCore::PrintContext::end):
    * page/PrintContext.h:
    (WebCore::PrintContext::frame const): Deleted.
    
    LayoutTests:
    
    * printing/print-with-media-query-destory-expected.txt: Added.
    * printing/print-with-media-query-destory.html: Added.

--- Source/WebCore/loader/DocumentLoader.cpp
+++ Source/WebCore/loader/DocumentLoader.cpp
@@ -434,6 +434,8 @@ void DocumentLoader::finishedLoading()
     if (!m_mainDocumentError.isNull())
         return;
     clearMainResourceLoader();
+    if (!frameLoader())
+        return;
     if (!frameLoader()->stateMachine().creatingInitialEmptyDocument())
         frameLoader()->checkLoadComplete();
 
