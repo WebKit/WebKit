b8007511c421606ec1fcbd39ace1c4f5884278f6
Chris Dumez <cdumez@apple.com>
2019-01-15 20:04:38 +0000

    Unreviewed iOS build fix after r239993.
    * UIProcess/SuspendedPageProxy.h:
    
    Canonical link: https://commits.webkit.org/207963@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@239995 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/SuspendedPageProxy.h
+++ Source/WebKit/UIProcess/SuspendedPageProxy.h
@@ -73,7 +73,7 @@ private:
     SuspensionState m_suspensionState { SuspensionState::Suspending };
     CompletionHandler<void(SuspendedPageProxy*)> m_readyToUnsuspendHandler;
 #if PLATFORM(IOS_FAMILY)
-    ProcessThrottler::BackgroundActivityToken m_suspensionToken;
+    ProcessThrottler::ForegroundActivityToken m_suspensionToken;
 #endif
 };
 
