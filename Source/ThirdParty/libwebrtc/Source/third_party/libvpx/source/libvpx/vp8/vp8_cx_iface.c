f3d1de90684b278243d7b17963d52214ad5793f9
David Kilzer <ddkilzer@webkit.org>
2019-02-05 03:24:54 +0000

    vp8e_mr_alloc_mem() leaks LOWER_RES_FRAME_INFO if second memory allocation fails <https://webkit.org/b/194265>
    Reviewed by Youenn Fablet.
    
    * Source/third_party/libvpx/source/libvpx/vp8/vp8_cx_iface.c:
    (vp8e_mr_alloc_mem):
    - Initialize `res` to VPX_CODEC_OK instead of 0.
    - Return early if first calloc() fails instead of trying the
      second calloc().  The function would crash dereferencing
      nullptr in `shared_mem_loc->mb_info` otherwise.
    - Call free(shared_mem_loc) if the second call to calloc()
      fails.  This fixes the leak.
    * WebKit/0003-libwebrtc-fix-vp8e_mr_alloc_mem-leak.diff: Add.
    
    
    Canonical link: https://commits.webkit.org/208721@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@240961 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/ThirdParty/libwebrtc/Source/third_party/libvpx/source/libvpx/vp8/vp8_cx_iface.c
+++ Source/ThirdParty/libwebrtc/Source/third_party/libvpx/source/libvpx/vp8/vp8_cx_iface.c
@@ -577,7 +577,7 @@ static vpx_codec_err_t set_screen_content_mode(vpx_codec_alg_priv_t *ctx,
 
 static vpx_codec_err_t vp8e_mr_alloc_mem(const vpx_codec_enc_cfg_t *cfg,
                                          void **mem_loc) {
-  vpx_codec_err_t res = 0;
+  vpx_codec_err_t res = VPX_CODEC_OK;
 
 #if CONFIG_MULTI_RES_ENCODING
   LOWER_RES_FRAME_INFO *shared_mem_loc;
@@ -586,12 +586,13 @@ static vpx_codec_err_t vp8e_mr_alloc_mem(const vpx_codec_enc_cfg_t *cfg,
 
   shared_mem_loc = calloc(1, sizeof(LOWER_RES_FRAME_INFO));
   if (!shared_mem_loc) {
-    res = VPX_CODEC_MEM_ERROR;
+    return VPX_CODEC_MEM_ERROR;
   }
 
   shared_mem_loc->mb_info =
       calloc(mb_rows * mb_cols, sizeof(LOWER_RES_MB_INFO));
   if (!(shared_mem_loc->mb_info)) {
+    free(shared_mem_loc);
     res = VPX_CODEC_MEM_ERROR;
   } else {
     *mem_loc = (void *)shared_mem_loc;
