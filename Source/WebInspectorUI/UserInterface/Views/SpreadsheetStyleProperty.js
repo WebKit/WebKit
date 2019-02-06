0df359f44f246f2ecc04ca073c6fba4f80273350
Nikita Vasilyev <nvasilyev@apple.com>
2019-02-06 22:18:10 +0000

    Cherry-pick r240946. rdar://problem/47830598
        Web Inspector: Styles: fix race conditions when editing
        https://bugs.webkit.org/show_bug.cgi?id=192739
        <rdar://problem/46752925>
    
        Reviewed by Devin Rousso.
    
        Source/WebInspectorUI:
    
        Editing CSS property in the style editor syncronously updates CSSStyleDeclaration on the front-end
        and asyncronously updates the backend by calling CSSAgent.setStyleText. After the new style text is applied
        on the backend, CSSStyleDeclaration (on the front-end) gets updated.
    
        Unsure there's no race conditions by introducing `_updatesInProgressCount`:
    
          - Increment it before calling CSSAgent.setStyleText.
          - Decrement it after CSSAgent.setStyleText is finished.
    
        Prevent updates of CSSStyleDeclaration when _updatesInProgressCount isn't 0.
    
        * UserInterface/Models/CSSProperty.js:
        (WI.CSSProperty.prototype._updateOwnerStyleText):
        * UserInterface/Models/CSSStyleDeclaration.js:
        (WI.CSSStyleDeclaration):
        (WI.CSSStyleDeclaration.prototype.set text): Removed.
        (WI.CSSStyleDeclaration.prototype.setText): Added.
        Change the setter to a method since it has side effects including an asynchronous backend call.
    
        * UserInterface/Models/DOMNodeStyles.js:
        (WI.DOMNodeStyles.prototype.changeStyleText):
    
        * UserInterface/Views/SpreadsheetStyleProperty.js:
        (WI.SpreadsheetStyleProperty.prototype.get nameTextField): Removed.
        (WI.SpreadsheetStyleProperty.prototype.get valueTextField): Removed.
        Drive-by: remove unused code.
    
        LayoutTests:
    
        * inspector/css/modify-css-property-expected.txt:
        * inspector/css/modify-css-property-race-expected.txt: Added.
        * inspector/css/modify-css-property-race.html: Added.
        * inspector/css/modify-css-property.html:
    
        Canonical link: https://commits.webkit.org/208706@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@240946 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/207701.216@safari-607-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@241091 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Views/SpreadsheetStyleProperty.js
+++ Source/WebInspectorUI/UserInterface/Views/SpreadsheetStyleProperty.js
@@ -75,8 +75,6 @@ WI.SpreadsheetStyleProperty = class SpreadsheetStyleProperty extends WI.Object
 
     get element() { return this._element; }
     get property() { return this._property; }
-    get nameTextField() { return this._nameTextField; }
-    get valueTextField() { return this._valueTextField; }
     get enabled() { return this._property.enabled; }
 
     set index(index)
