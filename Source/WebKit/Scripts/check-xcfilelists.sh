bdf082236baf08afdf2b4f2bdab734d2f1da470c
Keith Rollin <krollin@apple.com>
2019-02-06 22:54:54 +0000

    Really enable the automatic checking and regenerations of .xcfilelists during builds https://bugs.webkit.org/show_bug.cgi?id=194357 <rdar://problem/47861231>
    Reviewed by Chris Dumez.
    
    Bug 194124 was supposed to enable the automatic checking and
    regenerating of .xcfilelist files during the build. While related
    changes were included in that patch, the change to actually enable the
    operation somehow was omitted. This patch actually enables the
    operation. The check-xcfilelist.sh scripts now check
    WK_DISABLE_CHECK_XCFILELISTS, and if it's "1", opts-out the developer
    from the checking.
    
    Source/JavaScriptCore:
    
    * Scripts/check-xcfilelists.sh:
    
    Source/WebCore:
    
    No new tests since there should be no observable behavior difference.
    
    * Scripts/check-xcfilelists.sh:
    
    Source/WebKit:
    
    * Scripts/check-xcfilelists.sh:
    
    Tools:
    
    * DumpRenderTree/Scripts/check-xcfilelists.sh:
    * WebKitTestRunner/Scripts/check-xcfilelists.sh:
    
    
    Canonical link: https://commits.webkit.org/208798@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@241103 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Scripts/check-xcfilelists.sh
+++ Source/WebKit/Scripts/check-xcfilelists.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ "${WK_ENABLE_CHECK_XCFILELISTS}" == "" ] && { echo "### Not running because WK_ENABLE_CHECK_XCFILELISTS is not set"; exit 0; }
+[ "${WK_DISABLE_CHECK_XCFILELISTS}" == "1" ] && { echo "### Not running because WK_DISABLE_CHECK_XCFILELISTS is set"; exit 0; }
 [ "${DEPLOYMENT_LOCATION}" == "YES" ] && { echo "### Not running because DEPLOYMENT_LOCATION is YES"; exit 0; }
 [ "${USE_INTERNAL_SDK}" == "YES" ] || { echo "### Not running because USE_INTERNAL_SDK is not YES"; exit 0; }
 
