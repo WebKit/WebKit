15c2dbb6e94b64c464b9a6c18f4534ef27f7b620
Matt Baker <mattbaker01@gmail.com>
2019-02-06 00:55:28 +0000

    Web Inspector: Elements tab: selection is broken after deleting the selected node https://bugs.webkit.org/show_bug.cgi?id=194300 <rdar://problem/47829275>
    Reviewed by Devin Rousso.
    
    Deleting a TreeElement can cause an IndexSet including indexes
    outside the deleted range to be passed to SelectionController,
    corrupting the internal selection state.
    
    * UserInterface/Views/TreeOutline.js:
    (WI.TreeOutline.prototype._indexesForSubtree.numberOfElementsInSubtree): Added.
    (WI.TreeOutline.prototype._indexesForSubtree):
    Finding the last (rightmost leaf) TreeElement in the subtree used
    TreeElement.prototype.traverseNextElement to do a depth first traversal.
    This method did not stay within the subtree rooted at `treeElement`.
    
    Canonical link: https://commits.webkit.org/208761@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@241003 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Views/TreeOutline.js
+++ Source/WebInspectorUI/UserInterface/Views/TreeOutline.js
@@ -1089,26 +1089,25 @@ WI.TreeOutline = class TreeOutline extends WI.Object
         if (!treeOutline)
             return null;
 
-        let firstChild = treeElement.children[0];
-        if (treeElement.root && !firstChild)
-            return null;
+        function numberOfElementsInSubtree(treeElement) {
+            let elements = treeElement.root ? Array.from(treeElement.children) : [treeElement];
+            let count = 0;
+            while (elements.length) {
+                let child = elements.pop();
+                if (child.hidden)
+                    continue;
 
-        let current = firstChild || treeElement;
-        let startIndex = treeOutline._indexOfTreeElement(current);
-        let endIndex = startIndex;
+                count++;
+                elements = elements.concat(child.children);
+            }
+            return count;
+        }
 
-        const skipUnrevealed = false;
-        const stayWithin = treeElement;
-        const dontPopulate = true;
-
-        while (current = current.traverseNextTreeElement(skipUnrevealed, stayWithin, dontPopulate))
-            endIndex++;
-
-        let count = endIndex - startIndex + 1;
-
+        let firstChild = treeElement.root ? treeElement.children[0] : treeElement;
+        let startIndex = treeOutline._indexOfTreeElement(firstChild);
+        let count = numberOfElementsInSubtree(treeElement);
         let indexes = new WI.IndexSet;
         indexes.addRange(startIndex, count);
-
         return indexes;
     }
 };
