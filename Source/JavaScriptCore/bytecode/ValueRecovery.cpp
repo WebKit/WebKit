fe64f68efd39570143a74fb4b234376fa50edc42
Mark Lam <mark.lam@apple.com>
2019-02-06 22:16:34 +0000

    Cherry-pick r240681. rdar://problem/47774514
        ValueRecovery::recover() should purify NaN values it recovers.
        https://bugs.webkit.org/show_bug.cgi?id=193978
        <rdar://problem/47625488>
    
        Reviewed by Saam Barati.
    
        JSTests:
    
        * stress/value-recovery-of-double-displaced-in-jsstack-should-be-purified.js: Added.
    
        Source/JavaScriptCore:
    
        According to DFG::OSRExit::executeOSRExit() and DFG::OSRExit::compileExit(),
        recovered DoubleDisplacedInJSStack values need to be purified.
        ValueRecovery::recover() should do the same.
    
        * bytecode/ValueRecovery.cpp:
        (JSC::ValueRecovery::recover const):
    
        Canonical link: https://commits.webkit.org/208476@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@240681 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/207701.187@safari-607-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@241062 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/bytecode/ValueRecovery.cpp
+++ Source/JavaScriptCore/bytecode/ValueRecovery.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011, 2013, 2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2011-2019 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -43,7 +43,7 @@ JSValue ValueRecovery::recover(ExecState* exec) const
     case StrictInt52DisplacedInJSStack:
         return jsNumber(exec->r(virtualRegister().offset()).unboxedStrictInt52());
     case DoubleDisplacedInJSStack:
-        return jsNumber(exec->r(virtualRegister().offset()).unboxedDouble());
+        return jsNumber(purifyNaN(exec->r(virtualRegister().offset()).unboxedDouble()));
     case CellDisplacedInJSStack:
         return exec->r(virtualRegister().offset()).unboxedCell();
     case BooleanDisplacedInJSStack:
