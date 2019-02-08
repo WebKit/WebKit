005a6fa35fd656f43ad4d39426b90f1ff4480c05
Jonathan Davis <jond@apple.com>
2019-02-08 00:55:08 +0000

    Disable the WP emoji polyfill https://bugs.webkit.org/show_bug.cgi?id=194394
    Reviewed by Devin Rousso.
    
    * wp-content/themes/webkit/functions.php:
    
    
    Canonical link: https://commits.webkit.org/208843@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@241174 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Websites/webkit.org/wp-content/themes/webkit/functions.php
+++ Websites/webkit.org/wp-content/themes/webkit/functions.php
@@ -10,7 +10,18 @@ add_action( 'init', function () {
     register_nav_menu('feature-subnav', __( 'Feature Page Buttons' ));
 } );
 
-//add_action( 'wp_header', 'include_invert_lightness_filter');
+// Disable WP-emoji polyfill
+add_action('init', function() {
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+    // Remove DNS prefetch
+    add_filter('emoji_svg_url', '__return_false');
+});
 
 add_action( 'wp_dashboard_setup', function () {
     $SurveyWidget = new WebKit_Nightly_Survey();
