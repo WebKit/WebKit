e915230fbfa802174f5c77705d9c878c8829f9a1
Beth Dakin <bdakin@apple.com>
2019-02-08 20:36:07 +0000

    Abstract and discussion comments needed for afterScreenUpdates https://bugs.webkit.org/show_bug.cgi?id=194442
    Reviewed by Wenson Hsieh.
    
    Forgot to add these comments with https://bugs.webkit.org/show_bug.cgi?id=194362
    * UIProcess/API/Cocoa/WKSnapshotConfiguration.h:
    
    
    
    Canonical link: https://commits.webkit.org/208867@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@241204 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/Cocoa/WKSnapshotConfiguration.h
+++ Source/WebKit/UIProcess/API/Cocoa/WKSnapshotConfiguration.h
@@ -48,6 +48,11 @@ WK_CLASS_AVAILABLE(macosx(10.13), ios(11.0))
  */
 @property (nullable, nonatomic, copy) NSNumber *snapshotWidth;
 
+/*! @abstract A Boolean value that specifies whether the snapshot should be taken after recent
+ changes have been incorporated. The value NO will capture the screen in its current state,
+ which might not include recent changes.
+ @discussion The default value is YES.
+ */
 @property (nonatomic) BOOL afterScreenUpdates WK_API_AVAILABLE(macosx(WK_MAC_TBA), ios(WK_IOS_TBA));
 
 @end
