dccbc38347027dfa56d58f82fc4d94c09ad3bdda
Mark Lam <mark.lam@apple.com>
2019-02-13 21:11:55 +0000

    Cherry-pick r241300. rdar://problem/48016008
        Add some null checks in JSNodeCustom.h's root() and generated isReachableFromOpaqueRoots() functions.
        https://bugs.webkit.org/show_bug.cgi?id=194530
        <rdar://problem/47973274>
    
        Reviewed by Chris Dumez.
    
        This is needed to fix a null pointer dereference that arises from the following scenario:
        1. a Document detaches from its StyleSheetList.
        2. the JSStyleSheetList that is associated with the detached StyleSheetList has yet
           to be scanned and collected by the GC.
        3. the GC eventually looks for the opaque root of the StyleSheetList's owner, and
           discovers a null owner pointer.
    
        This patch fixes this issue by applying the following null checks:
    
        1. Add a null check in JSNodeCustom.h's root().
    
           root() is called from a isReachableFromOpaqueRoots() generated by CodeGeneratorJS.pm.
           isReachableFromOpaqueRoots() calls a ownerNode() method and passes its result
           to root().  However, depending on which class the ownerNode() method belongs to,
           it can either return a pointer or a reference.  The null check only makes sense
           in the pointer case.
    
           To accommodate the 2 forms, root() itself is has an overload that takes a
           reference instead of a pointer.
    
           Since CodeGeneratorJS.pm can't tell what the generated class' ownerNode()
           returns, it can't discern when the result is a pointer and apply the null check.
           Instead, we just add the null check to the version of root() that takes a
           pointer.  If the node pointer is null, we'll return a null opaque root.
    
        2. Fix CodeGeneratorJS.pm to null check the opaque root before using it.
    
        * bindings/js/JSNodeCustom.h:
        (WebCore::root):
        * bindings/scripts/CodeGeneratorJS.pm:
        (GenerateImplementation):
        * bindings/scripts/test/JS/JSTestGenerateIsReachable.cpp:
        (WebCore::JSTestGenerateIsReachableOwner::isReachableFromOpaqueRoots):
    
        Canonical link: https://commits.webkit.org/208957@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@241300 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/207701.268@safari-607-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@241464 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/scripts/test/JS/JSTestGenerateIsReachable.cpp
+++ Source/WebCore/bindings/scripts/test/JS/JSTestGenerateIsReachable.cpp
@@ -204,7 +204,7 @@ bool JSTestGenerateIsReachableOwner::isReachableFromOpaqueRoots(JSC::Handle<JSC:
     TestGenerateIsReachable* root = &jsTestGenerateIsReachable->wrapped();
     if (UNLIKELY(reason))
         *reason = "Reachable from TestGenerateIsReachable";
-    return visitor.containsOpaqueRoot(root);
+    return root && visitor.containsOpaqueRoot(root);
 }
 
 void JSTestGenerateIsReachableOwner::finalize(JSC::Handle<JSC::Unknown> handle, void* context)
