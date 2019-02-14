e62aeae5d8119fe33b6a25f9a7de9aea266d4477
Ryosuke Niwa <rniwa@webkit.org>
2019-02-14 08:34:24 +0000

    Cherry-pick r241401. rdar://problem/48065612
        Crash in Page::setActivityState because m_page is null
        https://bugs.webkit.org/show_bug.cgi?id=194584
    
        Reviewed by Antti Koivisto.
    
        Add a null check to avoid the crash. Also add a debug assertion to help diagnose this in the future.
    
        * WebProcess/WebPage/WebPage.cpp:
        (WebKit::WebPage::setActivityState):
    
        Canonical link: https://commits.webkit.org/208999@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@241401 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/207701.273@safari-607-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@241503 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/WebPage/WebPage.cpp
+++ Source/WebKit/WebProcess/WebPage/WebPage.cpp
@@ -3010,7 +3010,8 @@ void WebPage::setActivityState(OptionSet<ActivityState::Flag> activityState, Act
     if (changed)
         updateThrottleState();
 
-    {
+    ASSERT_WITH_MESSAGE(m_page, "setActivityState called on %lld but WebCore page was null", pageID());
+    if (m_page) {
         SetForScope<bool> currentlyChangingActivityState { m_changingActivityState, true };
         m_page->setActivityState(activityState);
     }
