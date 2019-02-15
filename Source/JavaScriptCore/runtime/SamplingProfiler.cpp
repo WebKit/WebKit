67f946d9b35b35089d4f27dfb3b52db53c84ba8f
Mark Lam <mark.lam@apple.com>
2019-02-15 21:41:15 +0000

    SamplingProfiler::stackTracesAsJSON() should escape strings. https://bugs.webkit.org/show_bug.cgi?id=194649 <rdar://problem/48072386>
    Reviewed by Saam Barati.
    
    JSTests:
    
    * stress/sampling-profiler-stack-trace-with-double-quote-in-function-name.js: Added.
    * stress/type-profiler-with-double-quote-in-constructor-name.js: Added.
    * stress/type-profiler-with-double-quote-in-field-name.js: Added.
    * stress/type-profiler-with-double-quote-in-optional-field-name.js: Added.
    
    Source/JavaScriptCore:
    
    Ditto for TypeSet::toJSONString() and TypeSet::toJSONString().
    
    * runtime/SamplingProfiler.cpp:
    (JSC::SamplingProfiler::stackTracesAsJSON):
    * runtime/TypeSet.cpp:
    (JSC::TypeSet::toJSONString const):
    (JSC::StructureShape::toJSONString const):
    
    
    
    Canonical link: https://commits.webkit.org/209113@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@241615 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/SamplingProfiler.cpp
+++ Source/JavaScriptCore/runtime/SamplingProfiler.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2018 Apple Inc. All rights reserved.
+ * Copyright (C) 2016-2019 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -904,9 +904,7 @@ String SamplingProfiler::stackTracesAsJSON()
         loopedOnce = false;
         for (StackFrame& stackFrame : stackTrace.frames) {
             comma();
-            json.append('"');
-            json.append(stackFrame.displayNameForJSONTests(m_vm));
-            json.append('"');
+            json.appendQuotedJSONString(stackFrame.displayNameForJSONTests(m_vm));
             loopedOnce = true;
         }
         json.append(']');
