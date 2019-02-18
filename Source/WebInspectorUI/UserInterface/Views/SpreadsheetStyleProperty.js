ad4b0d1405a1f0d65bfcd1dd4c7c7124a37a74c2
Nikita Vasilyev <nvasilyev@apple.com>
2019-02-18 22:45:55 +0000

    Web Inspector: Styles: typing ";" shouldn't focus on the next property when there's open parenthesis or comment https://bugs.webkit.org/show_bug.cgi?id=194789
    Reviewed by Matt Baker.
    
    * UserInterface/Views/SpreadsheetStyleProperty.js:
    (WI.SpreadsheetStyleProperty.prototype._handleValueBeforeInput):
    
    
    Canonical link: https://commits.webkit.org/209171@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@241740 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Views/SpreadsheetStyleProperty.js
+++ Source/WebInspectorUI/UserInterface/Views/SpreadsheetStyleProperty.js
@@ -727,20 +727,8 @@ WI.SpreadsheetStyleProperty = class SpreadsheetStyleProperty extends WI.Object
         if (!selection.rangeCount || selection.getRangeAt(0).endOffset !== text.length)
             return;
 
-        // Find the first and last index (if any) of a quote character to ensure that the string
-        // doesn't contain unbalanced quotes. If so, then there's no way that the semicolon could be
-        // part of a string within the value, so we can assume that it's the property "terminator".
-        const quoteRegex = /["']/g;
-        let start = -1;
-        let end = text.length;
-        let match = null;
-        while (match = quoteRegex.exec(text)) {
-            if (start < 0)
-                start = match.index;
-            end = match.index + 1;
-        }
-
-        if (start !== -1 && !text.substring(start, end).hasMatchingEscapedQuotes())
+        let unbalancedCharacters = WI.CSSCompletions.completeUnbalancedValue(text);
+        if (unbalancedCharacters)
             return;
 
         event.preventDefault();
