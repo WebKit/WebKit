7b25e1b146828abbce62aea0729e80448eb26eed
Adrian Perez de Castro <aperez@igalia.com>
2019-02-22 09:18:24 +0000

    Unreviewed. Bump version numbers
    * Source/cmake/OptionsWPE.cmake:
    
    Canonical link: https://commits.webkit.org/209304@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@241939 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/OptionsWPE.cmake
+++ Source/cmake/OptionsWPE.cmake
@@ -1,10 +1,10 @@
 include(GNUInstallDirs)
 include(VersioningUtils)
 
-SET_PROJECT_VERSION(2 23 0)
+SET_PROJECT_VERSION(2 25 0)
 set(WPE_API_VERSION 0.1)
 
-CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT 4 0 2)
+CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT 6 0 3)
 
 # These are shared variables, but we special case their definition so that we can use the
 # CMAKE_INSTALL_* variables that are populated by the GNUInstallDirs macro.
