98e297bc64d9fe7d2b2a016e6bd7b54af9e31af2
Andy Estes <aestes@apple.com>
2019-02-23 04:50:56 +0000

    [Cocoa] Do not suppress deprecation warnings when calling sandbox_extension_issue_file https://bugs.webkit.org/show_bug.cgi?id=194971 <rdar://problem/35786264>
    Reviewed by Tim Horton.
    
    sandbox_extension_issue_file is not actually deprecated.
    
    * Shared/mac/SandboxExtensionMac.mm:
    (WebKit::SandboxExtensionImpl::sandboxExtensionForType):
    
    
    Canonical link: https://commits.webkit.org/209334@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@241985 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/mac/SandboxExtensionMac.mm
+++ Source/WebKit/Shared/mac/SandboxExtensionMac.mm
@@ -84,11 +84,9 @@ private:
     {
         switch (type) {
         case SandboxExtension::Type::ReadOnly:
-            ALLOW_DEPRECATED_DECLARATIONS_BEGIN
             return sandbox_extension_issue_file(APP_SANDBOX_READ, path, 0);
         case SandboxExtension::Type::ReadWrite:
             return sandbox_extension_issue_file(APP_SANDBOX_READ_WRITE, path, 0);
-            ALLOW_DEPRECATED_DECLARATIONS_END
         case SandboxExtension::Type::Generic:
             return sandbox_extension_issue_generic(path, 0);
         }
