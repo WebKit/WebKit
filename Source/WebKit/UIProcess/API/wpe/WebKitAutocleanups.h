ac01dd9878536e7c4115e4046fb80356b1b66d81
Adrian Perez de Castro <aperez@igalia.com>
2019-03-04 14:23:45 +0000

    [GTK][WPE] Wrong license header in WebKit{,WebExtension}Autocleanups.h https://bugs.webkit.org/show_bug.cgi?id=195245
    Reviewed by Carlos Garcia Campos.
    
    Changed license headers to contain the LGPL, which is the correct one for the GTK
    and WPE ports. The change was agreed by the contributors to the files involved.
    
    * UIProcess/API/gtk/WebKitAutocleanups.h:
    * UIProcess/API/wpe/WebKitAutocleanups.h:
    * WebProcess/InjectedBundle/API/gtk/WebKitWebExtensionAutocleanups.h:
    * WebProcess/InjectedBundle/API/wpe/WebKitWebExtensionAutocleanups.h:
    
    
    Canonical link: https://commits.webkit.org/209615@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@242351 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/wpe/WebKitAutocleanups.h
+++ Source/WebKit/UIProcess/API/wpe/WebKitAutocleanups.h
@@ -1,26 +1,20 @@
 /*
  * Copyright (C) 2015 Igalia S.L.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
 #if !defined(__WEBKIT_H_INSIDE__) && !defined(WEBKIT2_COMPILATION)
