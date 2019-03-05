e06f3dc4b5dbbe213999ffbfda45e9d4e171980b
Mark Lam <mark.lam@apple.com>
2019-03-05 17:21:19 +0000

    Merge r242114 - wasmToJS() should purify incoming NaNs. https://bugs.webkit.org/show_bug.cgi?id=194807 <rdar://problem/48189132>
    Reviewed by Saam Barati.
    
    JSTests:
    
    * wasm/regress/wasmToJS-should-purify-NaNs.js: Added.
    
    Source/JavaScriptCore:
    
    * runtime/JSCJSValue.h:
    (JSC::jsNumber):
    * runtime/TypedArrayAdaptors.h:
    (JSC::IntegralTypedArrayAdaptor::toJSValue):
    * wasm/js/WasmToJS.cpp:
    (JSC::Wasm::wasmToJS):

--- Source/JavaScriptCore/wasm/js/WasmToJS.cpp
+++ Source/JavaScriptCore/wasm/js/WasmToJS.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2018 Apple Inc. All rights reserved.
+ * Copyright (C) 2016-2019 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -249,7 +249,7 @@ Expected<MacroAssemblerCodeRef<WasmEntryPtrTag>, BindingFailure> wasmToJS(VM* vm
                         break;
                     case F32:
                     case F64:
-                        arg = jsNumber(bitwise_cast<double>(buffer[argNum]));
+                        arg = jsNumber(purifyNaN(bitwise_cast<double>(buffer[argNum])));
                         break;
                     }
                     args.append(arg);
