86ffc8dc4c752147228dd72aeb0ae714311a7a83
Dean Jackson <dino@apple.com>
2019-03-06 18:14:13 +0000

    Fix WKPasswordView compilation on iOS https://bugs.webkit.org/show_bug.cgi?id=195344 <rdar://problem/48620066>
    Reviewed by Dan Bates.
    
    Use WebKitAdditions.
    
    * UIProcess/ios/WKPasswordView.mm:
    (-[WKPasswordView showInScrollView:]):
    
    Canonical link: https://commits.webkit.org/209701@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@242552 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/ios/WKPasswordView.mm
+++ Source/WebKit/UIProcess/ios/WKPasswordView.mm
@@ -35,6 +35,16 @@
 #import <wtf/RetainPtr.h>
 #import <wtf/text/WTFString.h>
 
+#if USE(APPLE_INTERNAL_SDK) && __has_include(<WebKitAdditions/WKPasswordViewAdditions.mm>)
+#include <WebKitAdditions/WKPasswordViewAdditions.mm>
+#else
+static void configureScrollView(UIScrollView *scrollView)
+{
+    [scrollView setBackgroundColor:[UIColor groupTableViewBackgroundColor]];
+}
+#endif
+
+
 const CGFloat passwordEntryFieldPadding = 10;
 
 @interface WKPasswordView () <UIDocumentPasswordViewDelegate>
@@ -100,7 +110,8 @@ const CGFloat passwordEntryFieldPadding = 10;
     [_scrollView setMaximumZoomScale:1];
     [_scrollView setZoomScale:1];
     [_scrollView setContentSize:self.frame.size];
-    [_scrollView setBackgroundColor:[UIColor groupTableViewBackgroundColor]];
+
+    configureScrollView(_scrollView.get());
 
     [scrollView addSubview:self];
 }
