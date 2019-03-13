75767ef092a449c2f3c8dd86b9de221a9c088860
Mark Lam <mark.lam@apple.com>
2019-03-13 08:24:57 +0000

    Cherry-pick r242810. rdar://problem/48839349
        The HasIndexedProperty node does GC.
        https://bugs.webkit.org/show_bug.cgi?id=195559
        <rdar://problem/48767923>
    
        Reviewed by Yusuke Suzuki.
    
        JSTests:
    
        * stress/HasIndexedProperty-does-gc.js: Added.
    
        Source/JavaScriptCore:
    
        HasIndexedProperty can call the slow path operationHasIndexedPropertyByInt(),
        which can eventually call JSString::getIndex(), which can resolve a rope.
    
        * dfg/DFGDoesGC.cpp:
        (JSC::DFG::doesGC):
    
        Canonical link: https://commits.webkit.org/209938@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@242810 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/207701.375@safari-607-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@242862 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGDoesGC.cpp
+++ Source/JavaScriptCore/dfg/DFGDoesGC.cpp
@@ -201,7 +201,6 @@ bool doesGC(Graph& graph, Node* node)
     case GetByOffset:
     case GetGetterSetterByOffset:
     case GetEnumerableLength:
-    case HasIndexedProperty:
     case FiatInt52:
     case BooleanToNumber:
     case CheckBadCell:
@@ -288,6 +287,7 @@ bool doesGC(Graph& graph, Node* node)
     case GetDynamicVar:
     case GetMapBucket:
     case HasGenericProperty:
+    case HasIndexedProperty:
     case HasOwnProperty:
     case HasStructureProperty:
     case InById:
