5186c3d2278d971d773a3c976812b71430d9a66b
Brent Fulgham <bfulgham@apple.com>
2019-03-15 03:31:40 +0000

    Move CoreCrypto SPI declarations to an appropriate PAL/spi header https://bugs.webkit.org/show_bug.cgi?id=195754 <rdar://problem/48591957>
    Reviewed by Jiewen Tan.
    
    Move the forward declarations of various CoreCrypto SPI to an appropriate PAL/spi header.
    Update the const correctness of one function call to match new SDK declaration.
    
    No tests because there are no changes in behavior.
    
    Source/WebCore:
    
    * crypto/CommonCryptoUtilities.h:
    * crypto/mac/CryptoAlgorithmHKDFMac.cpp:
    (WebCore::CryptoAlgorithmHKDF::platformDeriveBits):
    
    Source/WebCore/PAL:
    
    * PAL.xcodeproj/project.pbxproj:
    * pal/spi/cocoa/CommonCryptoSPI.h: Copied from Source/WebCore/crypto/CommonCryptoUtilities.h.
    
    
    Canonical link: https://commits.webkit.org/210075@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@242988 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/crypto/CommonCryptoUtilities.h
+++ Source/WebCore/crypto/CommonCryptoUtilities.h
@@ -25,111 +25,16 @@
 
 #pragma once
 
-#if (PLATFORM(IOS_FAMILY) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 110000) || (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101300)
-#define HAVE_CCRSAGetCRTComponents 1
-#endif
-
 #if ENABLE(WEB_CRYPTO)
 
 #include "CryptoAlgorithmIdentifier.h"
 #include <CommonCrypto/CommonCryptor.h>
 #include <CommonCrypto/CommonRandom.h>
+#include <pal/spi/cocoa/CommonCryptoSPI.h>
 #include <wtf/Vector.h>
 
-#if USE(APPLE_INTERNAL_SDK)
-#include <CommonCrypto/CommonCryptorSPI.h>
-#include <CommonCrypto/CommonECCryptor.h>
-// FIXME: <rdar://problem/31508959>
-// #include <CommonCrypto/CommonKeyDerivationSPI.h>
-#include <CommonCrypto/CommonRSACryptor.h>
-#endif
-
-#if USE(APPLE_INTERNAL_SDK) && HAVE(CCRSAGetCRTComponents)
-#include <CommonCrypto/CommonRSACryptorSPI.h>
-#endif
-
-#ifndef _CC_RSACRYPTOR_H_
-enum {
-    kCCDigestNone = 0,
-    kCCDigestSHA1 = 8,
-    kCCDigestSHA224 = 9,
-    kCCDigestSHA256 = 10,
-    kCCDigestSHA384 = 11,
-    kCCDigestSHA512 = 12,
-};
-typedef uint32_t CCDigestAlgorithm;
-
-enum {
-    ccRSAKeyPublic = 0,
-    ccRSAKeyPrivate = 1
-};
-typedef uint32_t CCRSAKeyType;
-
-enum {
-    ccPKCS1Padding = 1001,
-    ccOAEPPadding = 1002,
-    ccRSAPSSPadding = 1005
-};
-typedef uint32_t CCAsymmetricPadding;
-#endif
-
+#if !HAVE(CCRSAGetCRTComponents)
 typedef struct _CCBigNumRef *CCBigNumRef;
-
-typedef struct _CCRSACryptor *CCRSACryptorRef;
-extern "C" CCCryptorStatus CCRSACryptorEncrypt(CCRSACryptorRef publicKey, CCAsymmetricPadding padding, const void *plainText, size_t plainTextLen, void *cipherText, size_t *cipherTextLen, const void *tagData, size_t tagDataLen, CCDigestAlgorithm digestType);
-extern "C" CCCryptorStatus CCRSACryptorDecrypt(CCRSACryptorRef privateKey, CCAsymmetricPadding padding, const void *cipherText, size_t cipherTextLen, void *plainText, size_t *plainTextLen, const void *tagData, size_t tagDataLen, CCDigestAlgorithm digestType);
-extern "C" CCCryptorStatus CCRSACryptorSign(CCRSACryptorRef privateKey, CCAsymmetricPadding padding, const void *hashToSign, size_t hashSignLen, CCDigestAlgorithm digestType, size_t saltLen, void *signedData, size_t *signedDataLen);
-extern "C" CCCryptorStatus CCRSACryptorVerify(CCRSACryptorRef publicKey, CCAsymmetricPadding padding, const void *hash, size_t hashLen, CCDigestAlgorithm digestType, size_t saltLen, const void *signedData, size_t signedDataLen);
-extern "C" CCCryptorStatus CCRSACryptorGeneratePair(size_t keysize, uint32_t e, CCRSACryptorRef *publicKey, CCRSACryptorRef *privateKey);
-extern "C" CCRSACryptorRef CCRSACryptorGetPublicKeyFromPrivateKey(CCRSACryptorRef privkey);
-extern "C" void CCRSACryptorRelease(CCRSACryptorRef key);
-extern "C" CCCryptorStatus CCRSAGetKeyComponents(CCRSACryptorRef rsaKey, uint8_t *modulus, size_t *modulusLength, uint8_t *exponent, size_t *exponentLength, uint8_t *p, size_t *pLength, uint8_t *q, size_t *qLength);
-extern "C" CCRSAKeyType CCRSAGetKeyType(CCRSACryptorRef key);
-extern "C" CCCryptorStatus CCRSACryptorImport(const void *keyPackage, size_t keyPackageLen, CCRSACryptorRef *key);
-extern "C" CCCryptorStatus CCRSACryptorExport(CCRSACryptorRef key, void *out, size_t *outLen);
-
-#if HAVE(CCRSAGetCRTComponents)
-extern "C" CCCryptorStatus CCRSAGetCRTComponentsSizes(CCRSACryptorRef rsaKey, size_t *dpSize, size_t *dqSize, size_t *qinvSize);
-extern "C" CCCryptorStatus CCRSAGetCRTComponents(CCRSACryptorRef rsaKey, void *dp, size_t dpSize, void *dq, size_t dqSize, void *qinv, size_t qinvSize);
-#endif
-
-#ifndef _CC_ECCRYPTOR_H_
-enum {
-    ccECKeyPublic = 0,
-    ccECKeyPrivate = 1,
-};
-typedef uint32_t CCECKeyType;
-
-enum {
-    kCCImportKeyBinary = 0,
-};
-typedef uint32_t CCECKeyExternalFormat;
-#endif
-
-typedef struct _CCECCryptor *CCECCryptorRef;
-extern "C" CCCryptorStatus CCECCryptorGeneratePair(size_t keysize, CCECCryptorRef *publicKey, CCECCryptorRef *privateKey);
-extern "C" void CCECCryptorRelease(CCECCryptorRef key);
-extern "C" CCCryptorStatus CCECCryptorImportKey(CCECKeyExternalFormat format, void *keyPackage, size_t keyPackageLen, CCECKeyType keyType, CCECCryptorRef *key);
-extern "C" CCCryptorStatus CCECCryptorExportKey(CCECKeyExternalFormat format, void *keyPackage, size_t *keyPackageLen, CCECKeyType keyType, CCECCryptorRef key);
-extern "C" int CCECGetKeySize(CCECCryptorRef key);
-extern "C" CCCryptorStatus CCECCryptorCreateFromData(size_t keySize, uint8_t *qX, size_t qXLength, uint8_t *qY, size_t qYLength, CCECCryptorRef *ref);
-extern "C" CCCryptorStatus CCECCryptorGetKeyComponents(CCECCryptorRef ecKey, size_t *keySize, uint8_t *qX, size_t *qXLength, uint8_t *qY, size_t *qYLength, uint8_t *d, size_t *dLength);
-extern "C" CCCryptorStatus CCECCryptorComputeSharedSecret(CCECCryptorRef privateKey, CCECCryptorRef publicKey, void *out, size_t *outLen);
-extern "C" CCCryptorStatus CCECCryptorSignHash(CCECCryptorRef privateKey, const void *hashToSign, size_t hashSignLen, void *signedData, size_t *signedDataLen);
-extern "C" CCCryptorStatus CCECCryptorVerifyHash(CCECCryptorRef publicKey, const void *hash, size_t hashLen, const void *signedData, size_t signedDataLen, uint32_t *valid);
-
-#ifndef CommonCrypto_CommonNistKeyDerivation_h
-enum {
-    kCCKDFAlgorithmHKDF = 6
-};
-typedef uint32_t CCKDFAlgorithm;
-#endif
-
-extern "C" CCStatus CCKeyDerivationHMac(CCKDFAlgorithm algorithm, CCDigestAlgorithm digest, unsigned rounds, const void *keyDerivationKey, size_t keyDerivationKeyLen, const void *label, size_t labelLen, const void *context, size_t contextLen, const void *iv, size_t ivLen, const void *salt, size_t saltLen, void *derivedKey, size_t derivedKeyLen);
-
-#if !USE(APPLE_INTERNAL_SDK)
-extern "C" CCCryptorStatus CCCryptorGCM(CCOperation op, CCAlgorithm alg, const void* key, size_t keyLength, const void* iv, size_t ivLen, const void* aData, size_t aDataLen, const void* dataIn, size_t dataInLength, void* dataOut, void* tag, size_t* tagLength);
-extern "C" CCCryptorStatus CCRSACryptorCreateFromData(CCRSAKeyType keyType, const uint8_t *modulus, size_t modulusLength, const uint8_t *exponent, size_t exponentLength, const uint8_t *p, size_t pLength, const uint8_t *q, size_t qLength, CCRSACryptorRef *ref);
 #endif
 
 namespace WebCore {
