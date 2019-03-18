ff08dc108fa1d3d82b4a8b86b6f0a73746b899a6
Adrian Perez de Castro <aperez@igalia.com>
2019-03-18 11:02:38 +0000

    [WPE] Bump dependencies to wpe-1.0 and wpebackend-fdo-1.0 https://bugs.webkit.org/show_bug.cgi?id=195786
    Reviewed by Philippe Normand.
    
    .:
    
    * Source/cmake/FindWPE.cmake: Check for the wpe-1.0 pkg-config package.
    
    Source/WebKit:
    
    * wpe/wpe-web-extension.pc.in: Change dependency to wpe-1.0
    * wpe/wpe-webkit.pc.in: Ditto.
    
    Tools:
    
    * flatpak/org.webkit.WPEModules.yaml: Use libwpe 1.1.90 and wpebackend-fdo 1.1.91.
    * wpe/jhbuild.modules: Ditto.
    
    
    Canonical link: https://commits.webkit.org/210143@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@243060 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/FindWPE.cmake
+++ Source/cmake/FindWPE.cmake
@@ -29,7 +29,7 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 find_package(PkgConfig)
-pkg_check_modules(PC_WPE QUIET wpe-0.2)
+pkg_check_modules(PC_WPE QUIET wpe-1.0)
 
 find_path(WPE_INCLUDE_DIRS
     NAMES wpe/wpe.h
@@ -37,7 +37,7 @@ find_path(WPE_INCLUDE_DIRS
 )
 
 find_library(WPE_LIBRARIES
-    NAMES wpe-0.2 wpe
+    NAMES wpe-1.0
     HINTS ${PC_WPE_LIBDIR} ${PC_WPE_LIBRARY_DIRS}
 )
 
