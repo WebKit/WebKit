c6402b0e4a66d9feced48389ae986120fbd628aa
Fujii Hironori <Hironori.Fujii@sony.com>
2019-03-18 02:08:14 +0000

    [iOS] Block the accessibility server when accessibility is not enabled. https://bugs.webkit.org/show_bug.cgi?id=195342 <rdar://problem/48615720>
    Unreviewed build fix for WinCairo.
    
    * Shared/SandboxExtension.h: Use ProcessID instead of pid_t.
    
    Canonical link: https://commits.webkit.org/210137@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@243054 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/SandboxExtension.h
+++ Source/WebKit/Shared/SandboxExtension.h
@@ -27,6 +27,7 @@
 
 #include <wtf/Forward.h>
 #include <wtf/Noncopyable.h>
+#include <wtf/ProcessID.h>
 #include <wtf/RefCounted.h>
 #include <wtf/RefPtr.h>
 #include <wtf/Vector.h>
@@ -102,7 +103,7 @@ public:
     static bool createHandleForReadWriteDirectory(const String& path, Handle&); // Will attempt to create the directory.
     static String createHandleForTemporaryFile(const String& prefix, Type, Handle&);
     static bool createHandleForGenericExtension(const String& extensionClass, Handle&);
-    static bool createHandleForMachLookupByPid(const String& service, pid_t, Handle&);
+    static bool createHandleForMachLookupByPid(const String& service, ProcessID, Handle&);
     ~SandboxExtension();
 
     bool consume();
