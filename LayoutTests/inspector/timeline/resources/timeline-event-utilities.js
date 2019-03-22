63ce7538ac36c37f12a18a978fd35e215a54aef0
Devin Rousso <drousso@apple.com>
2019-03-22 23:34:29 +0000

    Unreviewed, fix test failures after r243269.
    In debug builds, it's possible that the Web Inspector frontend is told to stop a timeline
    recording before all of the recorded records have had a chance to be completed/processed.
    
    As an example
    ```
        setTimeout(() => {
            <stop recording>
        });
    ```
    it may happen that the "stop recording" event will be dispatched before the timeout has
    finished executing, meaning that the event that contains the recorded data for that timeout
    will be ignored by the frontend.
    
    Rework the tests so that they don't dispatch the "stop recording" event until the expected
    record is received by the frontend, rather than having the test code itself say when to stop.
    
    * inspector/timeline/resources/timeline-event-utilities.js:
    (savePageData): Added.
    (TestPage.registerInitializer.InspectorTest.TimelineEvent.captureTimelineWithScript):
    (finishRecording): Deleted.
    * inspector/timeline/timeline-event-CancelAnimationFrame.html:
    * inspector/timeline/timeline-event-CancelAnimationFrame-expected.txt:
    * inspector/timeline/timeline-event-EventDispatch.html:
    * inspector/timeline/timeline-event-EventDispatch-expected.txt:
    * inspector/timeline/timeline-event-FireAnimationFrame.html:
    * inspector/timeline/timeline-event-FireAnimationFrame-expected.txt:
    * inspector/timeline/timeline-event-RequestAnimationFrame.html:
    * inspector/timeline/timeline-event-RequestAnimationFrame-expected.txt:
    * inspector/timeline/timeline-event-TimerFire.html:
    * inspector/timeline/timeline-event-TimerFire-expected.txt:
    * inspector/timeline/timeline-event-TimerInstall.html:
    * inspector/timeline/timeline-event-TimerInstall-expected.txt:
    * inspector/timeline/timeline-event-TimerRemove.html:
    * inspector/timeline/timeline-event-TimerRemove-expected.txt:
    
    Canonical link: https://commits.webkit.org/210462@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@243405 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/inspector/timeline/resources/timeline-event-utilities.js
+++ LayoutTests/inspector/timeline/resources/timeline-event-utilities.js
@@ -1,31 +1,47 @@
-function finishRecording(data) {
-    TestPage.addResult("Finish recording...");
-    TestPage.dispatchEventToFrontend("FinishRecording", data);
+function savePageData(data) {
+    TestPage.dispatchEventToFrontend("SavePageData", data);
 }
 
 TestPage.registerInitializer(() => {
     InspectorTest.TimelineEvent = {};
 
-    InspectorTest.TimelineEvent.captureTimelineWithScript = function(expression) {
+    InspectorTest.TimelineEvent.captureTimelineWithScript = function({expression, eventType}) {
         let pageRecordingData = null;
+
+        let promise = new WI.WrappedPromise;
+
+        WI.timelineManager.awaitEvent(WI.TimelineManager.Event.CapturingStopped).then((capturingStoppedEvent) => {
+            InspectorTest.assert(pageRecordingData, "savePageData should have been called in the page before capturing was stopped.");
+            promise.resolve(pageRecordingData);
+        });
+
+        InspectorTest.awaitEvent("SavePageData").then((event) => {
+            pageRecordingData = event.data;
+        });
+
+        WI.timelineManager.awaitEvent(WI.TimelineManager.Event.CapturingStarted).then((capturingStartedEvent) => {
+            let recording = WI.timelineManager.activeRecording;
+            let scriptTimeline = recording.timelines.get(WI.TimelineRecord.Type.Script);
+
+            let recordAddedListener = scriptTimeline.addEventListener(WI.Timeline.Event.RecordAdded, (recordAddedEvent) => {
+                let {record} = recordAddedEvent.data;
+                if (record.eventType !== eventType)
+                    return;
+
+                scriptTimeline.removeEventListener(WI.Timeline.Event.RecordAdded, recordAddedListener);
+
+                InspectorTest.log("Stopping Capture...");
+                WI.timelineManager.stopCapturing();
+            });
+
+            InspectorTest.log("Evaluating...");
+            return InspectorTest.evaluateInPage(expression);
+        });
 
         InspectorTest.log("Starting Capture...");
         const newRecording = true;
         WI.timelineManager.startCapturing(newRecording);
 
-        let promises = [];
-
-        promises.push(WI.timelineManager.awaitEvent(WI.TimelineManager.Event.CapturingStopped));
-
-        promises.push(InspectorTest.awaitEvent("FinishRecording").then((event) => {
-            InspectorTest.log("Stopping Capture...");
-            pageRecordingData = event.data;
-            WI.timelineManager.stopCapturing();
-        }));
-
-        InspectorTest.log("Evaluating...");
-        promises.push(InspectorTest.evaluateInPage(expression));
-
-        return Promise.all(promises).then(() => pageRecordingData);
+        return promise.promise;
     }
 });
