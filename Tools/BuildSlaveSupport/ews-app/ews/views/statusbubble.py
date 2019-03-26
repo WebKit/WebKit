8f56c4d1311e277bf3e3b68469846943301bceed
Aakash Jain <aakash_jain@apple.com>
2019-03-26 17:08:17 +0000

    [ews-app] Add webkitperl to ENABLED_QUEUES https://bugs.webkit.org/show_bug.cgi?id=196253
    Reviewed by Lucas Forschler.
    
    * BuildSlaveSupport/ews-app/ews/views/statusbubble.py:
    
    
    Canonical link: https://commits.webkit.org/210555@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@243500 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/BuildSlaveSupport/ews-app/ews/views/statusbubble.py
+++ Tools/BuildSlaveSupport/ews-app/ews/views/statusbubble.py
@@ -39,7 +39,7 @@ class StatusBubble(View):
     # FIXME: Auto-generate this list https://bugs.webkit.org/show_bug.cgi?id=195640
     ALL_QUEUES = ['api-ios', 'api-mac', 'bindings', 'gtk', 'ios', 'ios-sim', 'ios-wk2', 'jsc', 'mac', 'mac-32bit', 'mac-32bit-wk2',
                     'mac-debug', 'mac-debug-wk1', 'mac-wk1', 'mac-wk2', 'style', 'webkitperl', 'webkitpy', 'win', 'wincairo', 'wpe']
-    ENABLED_QUEUES = ['api-ios', 'api-mac']
+    ENABLED_QUEUES = ['api-ios', 'api-mac', 'webkitperl']
 
     def _build_bubble(self, patch, queue):
         bubble = {
