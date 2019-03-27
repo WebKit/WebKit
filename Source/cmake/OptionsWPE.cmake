4eb01573a03baf65247acd45c50638b3f17356b4
Adrian Perez de Castro <aperez@igalia.com>
2019-03-27 11:34:24 +0000

    Unreviewed. Update OptionsWPE.cmake and NEWS for the 2.24.0 release
    .:
    
    * Source/cmake/OptionsWPE.cmake: Bump version numbers.
    
    Source/WebKit:
    
    * wpe/NEWS: Add release notes for 2.24.0

--- Source/cmake/OptionsWPE.cmake
+++ Source/cmake/OptionsWPE.cmake
@@ -1,10 +1,10 @@
 include(GNUInstallDirs)
 include(VersioningUtils)
 
-SET_PROJECT_VERSION(2 23 91)
+SET_PROJECT_VERSION(2 24 0)
 set(WPE_API_VERSION 1.0)
 
-CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT 6 0 4)
+CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT 6 1 4)
 
 # These are shared variables, but we special case their definition so that we can use the
 # CMAKE_INSTALL_* variables that are populated by the GNUInstallDirs macro.
