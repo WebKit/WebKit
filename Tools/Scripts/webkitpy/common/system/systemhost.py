0dc548880a869e1b3b58acc0968e7b64c2aa9c1f
Jonathan Bedard <jbedard@apple.com>
2019-04-03 00:04:01 +0000

    Cherry-pick r239945. rdar://problem/49539128
        webkitpy: Expose device_type from host-like objects
        https://bugs.webkit.org/show_bug.cgi?id=193406
        <rdar://problem/47262305>
    
        Reviewed by Lucas Forschler.
    
        Devices should expose device_type. As a result, all host objects should
        provide a device_type property, even if they do not yet define a device_type.
    
        * Scripts/webkitpy/common/system/systemhost.py:
        (SystemHost):
        (SystemHost.device_type):
        * Scripts/webkitpy/common/system/systemhost_mock.py:
        (MockSystemHost):
        (MockSystemHost.device_type):
        * Scripts/webkitpy/port/device.py:
        (Device):
        (Device.device_type):
        * Scripts/webkitpy/xcode/simulated_device.py:
        (SimulatedDeviceManager._find_exisiting_device_for_request):
        (SimulatedDeviceManager._disambiguate_device_type):
        (SimulatedDeviceManager._does_fulfill_request):
        (SimulatedDeviceManager.device_count_for_type):
        (SimulatedDeviceManager.initialize_devices):
        * Scripts/webkitpy/xcode/simulated_device_unittest.py:
        (test_available_devices):
        (test_swapping_devices):
    
        Canonical link: https://commits.webkit.org/207920@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@239945 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/207701.401@safari-607-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@243770 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/system/systemhost.py
+++ Tools/Scripts/webkitpy/common/system/systemhost.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2011 Google Inc. All rights reserved.
+# Copyright (C) 2019 Apple Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -54,3 +55,7 @@ class SystemHost(object):
         if not self.platform.is_mac():
             return ''
         return self.executive.run_command(['xcrun', 'lldb', '--python-path'], return_stderr=False).rstrip()
+
+    @property
+    def device_type(self):
+        return None
