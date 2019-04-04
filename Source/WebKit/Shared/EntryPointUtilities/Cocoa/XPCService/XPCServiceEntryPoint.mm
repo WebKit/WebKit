e7f0e012dab93cd0beee524f90b8c1f6b7b606cc
Youenn Fablet <youenn@apple.com>
2019-04-04 17:44:16 +0000

    Service Worker Process does not have the right domain name https://bugs.webkit.org/show_bug.cgi?id=196585
    Reviewed by Chris Dumez.
    
    * Shared/EntryPointUtilities/Cocoa/XPCService/XPCServiceEntryPoint.mm:
    (WebKit::XPCServiceInitializerDelegate::getExtraInitializationData):
    Updated extraInitializationData to go from "security-origin" to "registrable-domain".
    
    
    Canonical link: https://commits.webkit.org/210873@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@243880 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/EntryPointUtilities/Cocoa/XPCService/XPCServiceEntryPoint.mm
+++ Source/WebKit/Shared/EntryPointUtilities/Cocoa/XPCService/XPCServiceEntryPoint.mm
@@ -118,16 +118,15 @@ bool XPCServiceInitializerDelegate::getExtraInitializationData(HashMap<String, S
 #if ENABLE(SERVICE_WORKER)
     String serviceWorkerProcess = xpc_dictionary_get_string(extraDataInitializationDataObject, "service-worker-process");
     if (!serviceWorkerProcess.isEmpty())
-        extraInitializationData.add("service-worker-process"_s, serviceWorkerProcess);
+        extraInitializationData.add("service-worker-process"_s, WTFMove(serviceWorkerProcess));
+    String registrableDomain = xpc_dictionary_get_string(extraDataInitializationDataObject, "registrable-domain");
+    if (!registrableDomain.isEmpty())
+        extraInitializationData.add("registrable-domain"_s, WTFMove(registrableDomain));
 #endif
 
     String isPrewarmedProcess = xpc_dictionary_get_string(extraDataInitializationDataObject, "is-prewarmed");
     if (!isPrewarmedProcess.isEmpty())
         extraInitializationData.add("is-prewarmed"_s, isPrewarmedProcess);
-
-    String securityOrigin = xpc_dictionary_get_string(extraDataInitializationDataObject, "security-origin");
-    if (!securityOrigin.isEmpty())
-        extraInitializationData.add("security-origin"_s, securityOrigin);
 
     if (!isClientSandboxed()) {
         String userDirectorySuffix = xpc_dictionary_get_string(extraDataInitializationDataObject, "user-directory-suffix");
