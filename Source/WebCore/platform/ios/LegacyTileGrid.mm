e40488f0fdeb703debfa6aac30734133c4ed8fa8
Chris Dumez <cdumez@apple.com>
2019-04-04 20:34:08 +0000

    Unreviewed, update r243884 to use macros in Compiler.h instead.
    * platform/ios/LegacyTileGrid.mm:
    (WebCore::LegacyTileGrid::dropDistantTiles):
    
    Canonical link: https://commits.webkit.org/210885@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@243895 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/ios/LegacyTileGrid.mm
+++ Source/WebCore/platform/ios/LegacyTileGrid.mm
@@ -130,14 +130,13 @@ bool LegacyTileGrid::dropDistantTiles(unsigned tilesNeeded, double shortestDista
         if (distance <= shortestDistance)
             continue;
         toRemove.append(std::make_pair(distance, index));
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+        ALLOW_DEPRECATED_DECLARATIONS_BEGIN
         std::push_heap(toRemove.begin(), toRemove.end(), std::ptr_fun(isFartherAway<TileIndex>));
         if (toRemove.size() > tilesToRemoveCount) {
             std::pop_heap(toRemove.begin(), toRemove.end(), std::ptr_fun(isFartherAway<TileIndex>));
-#pragma clang diagnostic pop
             toRemove.removeLast();
         }
+        ALLOW_DEPRECATED_DECLARATIONS_END
     }
     size_t removeCount = toRemove.size();
     for (size_t n = 0; n < removeCount; ++n)
