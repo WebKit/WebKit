4d9444028b4d57200d9aa7fc4d372827bbb67be1
Mark Lam <mark.lam@apple.com>
2019-04-08 12:39:23 +0000

    Merge r243280 - Cap length of an array with spread to MIN_ARRAY_STORAGE_CONSTRUCTION_LENGTH. https://bugs.webkit.org/show_bug.cgi?id=196055 <rdar://problem/49067448>
    Reviewed by Yusuke Suzuki.
    
    JSTests:
    
    * stress/new_array_with_spread-should-cap-array-size-to-MIN_ARRAY_STORAGE_CONSTRUCTION_LENGTH.js: Added.
    
    Source/JavaScriptCore:
    
    We are doing this because:
    1. We expect the array to be densely packed.
    2. SpeculativeJIT::compileAllocateNewArrayWithSize() (and the FTL equivalent)
       expects the array length to be less than MIN_ARRAY_STORAGE_CONSTRUCTION_LENGTH
       if we don't want to use an ArrayStorage shape.
    3. There's no reason why an array with spread needs to be that large anyway.
       MIN_ARRAY_STORAGE_CONSTRUCTION_LENGTH is plenty.
    
    In this patch, we also add a debug assert in compileAllocateNewArrayWithSize() and
    emitAllocateButterfly() to check for overflows.
    
    * assembler/AbortReason.h:
    * dfg/DFGOperations.cpp:
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compileCreateRest):
    (JSC::DFG::SpeculativeJIT::compileNewArrayWithSpread):
    (JSC::DFG::SpeculativeJIT::emitAllocateButterfly):
    (JSC::DFG::SpeculativeJIT::compileAllocateNewArrayWithSize):
    * ftl/FTLLowerDFGToB3.cpp:
    (JSC::FTL::DFG::LowerDFGToB3::compileNewArrayWithSpread):
    * runtime/ArrayConventions.h:
    * runtime/CommonSlowPaths.cpp:
    (JSC::SLOW_PATH_DECL):

--- Source/JavaScriptCore/assembler/AbortReason.h
+++ Source/JavaScriptCore/assembler/AbortReason.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2016 Apple Inc. All rights reserved.
+ * Copyright (C) 2014-2019 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -73,6 +73,7 @@ enum AbortReason {
     RepatchInsaneArgumentCount                        = 310,
     TGInvalidPointer                                  = 320,
     TGNotSupported                                    = 330,
+    UncheckedOverflow                                 = 335,
     YARRNoInputConsumed                               = 340,
 };
 
