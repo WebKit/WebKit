3ae4c1709a014938343df0522258346eaa9134a8
Carlos Garcia Campos <cgarcia@igalia.com>
2019-04-08 10:15:14 +0000

    Merge r242597 - REGRESSION(r242364): [WPE] Do not stop the compositing run loop update timer on suspend https://bugs.webkit.org/show_bug.cgi?id=195410
    Patch by Carlos Garcia Campos <cgarcia@igalia.com> on 2019-03-07
    Reviewed by Žan Doberšek.
    
    Calling CompositingRunLoop::stopUpdates() on suspend is leaving the threaded compositor in an inconsistent
    state, failing to resume and stopping the updates forever. This is causing timeouts in WPE layout tests. Instead
    of calling stopUpdates(), a new suspend() is called, that stops the update timer, without changing the current
    updae tha compositing state. A new method resume() is also added to schedule an update if needed.
    
    * Shared/CoordinatedGraphics/threadedcompositor/CompositingRunLoop.cpp:
    (WebKit::CompositingRunLoop::suspend): Set state as suspended and stop the update timer.
    (WebKit::CompositingRunLoop::resume): Set state as not suspended and start the update timer if it was scheduled
    while suspended.
    (WebKit::CompositingRunLoop::scheduleUpdate): Do not start the update timer when suspended.
    (WebKit::CompositingRunLoop::compositionCompleted): Ditto.
    (WebKit::CompositingRunLoop::updateCompleted): Ditto.
    (WebKit::CompositingRunLoop::updateTimerFired): Add an assert to ensure the update timer is not fired while suspended.
    * Shared/CoordinatedGraphics/threadedcompositor/CompositingRunLoop.h:
    * Shared/CoordinatedGraphics/threadedcompositor/ThreadedCompositor.cpp:
    (WebKit::ThreadedCompositor::suspend): Call CompositingRunLoop::suspend() instead of stopUpdates().
    (WebKit::ThreadedCompositor::resume): Call CompositingRunLoop::resume().

--- Source/WebKit/Shared/CoordinatedGraphics/threadedcompositor/CompositingRunLoop.cpp
+++ Source/WebKit/Shared/CoordinatedGraphics/threadedcompositor/CompositingRunLoop.cpp
@@ -94,6 +94,21 @@ void CompositingRunLoop::performTaskSync(Function<void ()>&& function)
     m_dispatchSyncCondition.wait(m_dispatchSyncConditionMutex);
 }
 
+void CompositingRunLoop::suspend()
+{
+    LockHolder stateLocker(m_state.lock);
+    m_state.isSuspended = true;
+    m_updateTimer.stop();
+}
+
+void CompositingRunLoop::resume()
+{
+    LockHolder stateLocker(m_state.lock);
+    m_state.isSuspended = false;
+    if (m_state.update == UpdateState::Scheduled)
+        m_updateTimer.startOneShot(0_s);
+}
+
 void CompositingRunLoop::scheduleUpdate()
 {
     LockHolder stateLocker(m_state.lock);
@@ -113,7 +128,8 @@ void CompositingRunLoop::scheduleUpdate(LockHolder& stateLocker)
     switch (m_state.update) {
     case UpdateState::Idle:
         m_state.update = UpdateState::Scheduled;
-        m_updateTimer.startOneShot(0_s);
+        if (!m_state.isSuspended)
+            m_updateTimer.startOneShot(0_s);
         return;
     case UpdateState::Scheduled:
         return;
@@ -156,7 +172,8 @@ void CompositingRunLoop::compositionCompleted(LockHolder& stateLocker)
         if (m_state.pendingUpdate) {
             m_state.pendingUpdate = false;
             m_state.update = UpdateState::Scheduled;
-            m_updateTimer.startOneShot(0_s);
+            if (!m_state.isSuspended)
+                m_updateTimer.startOneShot(0_s);
             return;
         }
 
@@ -188,7 +205,8 @@ void CompositingRunLoop::updateCompleted(LockHolder& stateLocker)
         if (m_state.pendingUpdate) {
             m_state.pendingUpdate = false;
             m_state.update = UpdateState::Scheduled;
-            m_updateTimer.startOneShot(0_s);
+            if (!m_state.isSuspended)
+                m_updateTimer.startOneShot(0_s);
             return;
         }
 
@@ -204,6 +222,7 @@ void CompositingRunLoop::updateTimerFired()
     {
         // Both composition and scene update are now in progress.
         LockHolder locker(m_state.lock);
+        ASSERT(!m_state.isSuspended);
         m_state.composition = CompositionState::InProgress;
         m_state.update = UpdateState::InProgress;
     }
