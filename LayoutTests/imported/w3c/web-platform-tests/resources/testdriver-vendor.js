95ae6591e8f12d43817a52ad728e9c438bf9a0cc
Antoine Quint <graouts@webkit.org>
2019-04-19 19:13:35 +0000

    [Pointer Events WPT] Provide iOS testdriver https://bugs.webkit.org/show_bug.cgi?id=197101 <rdar://problem/50044834>
    Reviewed by Dean Jackson.
    
    Add support for the "none" action source and for the "touch" pointer type. This allows us to run the touch-specific
    WPT tests for Pointer Events and add some baseline expectations for those tests.
    
    * web-platform-tests/pointerevents/pointerevent_change-touch-action-onpointerdown_touch-expected.txt: Added.
    * web-platform-tests/pointerevents/pointerevent_pointercancel_touch-expected.txt: Added.
    * web-platform-tests/pointerevents/pointerevent_pointerleave_after_pointercancel_touch-expected.txt: Added.
    * web-platform-tests/pointerevents/pointerevent_pointerout_after_pointercancel_touch-expected.txt: Added.
    * web-platform-tests/pointerevents/pointerevent_releasepointercapture_onpointercancel_touch-expected.txt: Added.
    * web-platform-tests/pointerevents/pointerevent_touch-action-auto-css_touch-expected.txt: Added.
    * web-platform-tests/pointerevents/pointerevent_touch-action-inherit_highest-parent-none_touch-expected.txt: Added.
    * web-platform-tests/pointerevents/pointerevent_touch-action-pan-x-pan-y_touch-expected.txt: Added.
    * web-platform-tests/resources/testdriver-vendor.js:
    (logDebug): Take a callback so that we don't run code for the purpose of logging unless we're in debug mode.
    (dispatchMouseActions):
    (dispatchTouchActions): Use testRunner.runUIScript() to compile an event stream based on the input from the WPT tests.
    (window.test_driver_internal.action_sequence): Handle the "none" source to insert "pause" commands in the pointer source.
    
    Canonical link: https://commits.webkit.org/211337@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@244460 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/imported/w3c/web-platform-tests/resources/testdriver-vendor.js
+++ LayoutTests/imported/w3c/web-platform-tests/resources/testdriver-vendor.js
@@ -1,10 +1,10 @@
 
 const isDebug = false;
 
-function logDebug(message)
+function logDebug(callback)
 {
     if (isDebug)
-        console.log(message);
+        console.log(callback());
 }
 
 function dispatchMouseActions(actions)
@@ -12,7 +12,7 @@ function dispatchMouseActions(actions)
     if (!window.eventSender)
         return Promise.reject(new Error("window.eventSender is undefined."));
 
-    return new Promise(function(resolve, reject) {
+    return new Promise(resolve => {
         setTimeout(() => {
             for (let action of actions) {
                 switch (action.type) {
@@ -20,21 +20,21 @@ function dispatchMouseActions(actions)
                     let origin = { x: 0, y: 0 };
                     if (action.origin instanceof Element) {
                         const bounds = action.origin.getBoundingClientRect();
-                        logDebug(`${action.origin.id} [${bounds.left}, ${bounds.top}, ${bounds.width}, ${bounds.height}]`);
+                        logDebug(() => `${action.origin.id} [${bounds.left}, ${bounds.top}, ${bounds.width}, ${bounds.height}]`);
                         origin.x = bounds.left + 1;
                         origin.y = bounds.top + 1;
                     }
-                    logDebug(`eventSender.mouseMoveTo(${action.x + origin.x}, ${action.y + origin.y})`);
+                    logDebug(() => `eventSender.mouseMoveTo(${action.x + origin.x}, ${action.y + origin.y})`);
                     eventSender.mouseMoveTo(action.x + origin.x, action.y + origin.y);
                     break;
                 case "pointerDown":
                     // FIXME: what to do with "button"?
-                    logDebug(`eventSender.mouseDown()`);
+                    logDebug(() => `eventSender.mouseDown()`);
                     eventSender.mouseDown();
                     break;
                 case "pointerUp":
                     // FIXME: what to do with "button"?
-                    logDebug(`eventSender.mouseUp()`);
+                    logDebug(() => `eventSender.mouseUp()`);
                     eventSender.mouseUp();
                     break;
                 default:
@@ -46,20 +46,119 @@ function dispatchMouseActions(actions)
     });
 }
 
+function dispatchTouchActions(actions)
+{
+    if (!window.testRunner || typeof window.testRunner.runUIScript !== "function")
+        return Promise.reject(new Error("window.testRunner.runUIScript() is undefined."));
+
+    let x = 0;
+    let y = 0;
+    let timeOffset = 0;
+
+    const events = actions.map(action => {
+        const touch = {
+            inputType : "finger",
+            id : 1,
+            x : action.x,
+            y : action.y,
+            pressure : 0
+        };
+
+        const command = {
+            inputType : "hand",
+            coordinateSpace : "content",
+            touches : [touch],
+            timeOffset
+        };
+
+        let timeOffsetIncrease = 0.05;
+
+        switch (action.type) {
+        case "pointerMove":
+            touch.phase = "moved";
+            if (action.origin instanceof Element) {
+                const bounds = action.origin.getBoundingClientRect();
+                touch.x += bounds.left + 1;
+                touch.y += bounds.top + 1;
+            }
+            break;
+        case "pointerDown":
+            touch.phase = "began";
+            break;
+        case "pointerUp":
+            touch.phase = "ended";
+            touch.x = x;
+            touch.y = y;
+            break;
+        case "pause":
+            timeOffsetIncrease = action.duration / 1000;
+            touch.phase = "stationary";
+            break;
+        default:
+            return Promise.reject(new Error(`Unknown action type "${action.type}".`));
+        }
+
+        x = touch.x;
+        y = touch.y;
+        timeOffset += timeOffsetIncrease;
+
+        return command;
+    });
+
+    const stream = JSON.stringify({ events });
+    logDebug(() => stream);
+
+    return new Promise(resolve => testRunner.runUIScript(`(function() {
+        (function() { uiController.sendEventStream('${stream}') })();
+        uiController.uiScriptComplete();
+    })();`, resolve));
+}
+
 if (window.test_driver_internal === undefined)
     window.test_driver_internal = { };
 
-window.test_driver_internal.action_sequence = function(actions)
+window.test_driver_internal.action_sequence = function(sources)
 {
-    const action = actions[0];
+    // https://w3c.github.io/webdriver/#processing-actions    
 
-    if (action.type !== "pointer")
-        return Promise.reject(new Error(`Unknown action type "${action.type}".`));
+    let noneSource;
+    let pointerSource;
+    for (let source of sources) {
+        switch (source.type) {
+        case "none":
+            noneSource = source;
+            break;
+        case "pointer":
+            pointerSource = source;
+            break;
+        default:
+            return Promise.reject(new Error(`Unknown source type "${action.type}".`));
+        }
+    }
 
-    if (action.parameters.pointerType !== "mouse")
+    if (!pointerSource)
         return Promise.reject(new Error(`Unknown pointer type pointer type "${action.parameters.pointerType}".`));
 
-    // FIXME: Handle iOS.
-    // FIXME: what do with "id" property?
-    return dispatchMouseActions(action.actions);
+    const pointerType = pointerSource.parameters.pointerType;
+    if (pointerType !== "mouse" && pointerType !== "touch")
+        return Promise.reject(new Error(`Unknown pointer type "${pointerType}".`));
+
+    // If we have a "none" source, let's inject any pause with non-zero durations into the pointer source
+    // after the matching action in the pointer source.
+    if (noneSource) {
+        let injectedActions = 0;
+        noneSource.actions.forEach((action, index) => {
+            if (action.duration > 0) {
+                pointerSource.actions.splice(index + injectedActions + 1, 0, action);
+                injectedActions++;
+            }
+        });
+    }
+
+    logDebug(() => JSON.stringify(pointerSource));
+
+    if (pointerType === "mouse")
+        return dispatchMouseActions(pointerSource.actions);
+    if (pointerType === "touch")
+        return dispatchTouchActions(pointerSource.actions);
 };
