ee586de284ba6a3edc849454cc3276a5ecde0b7b
Youenn Fablet <youenn@apple.com>
2019-04-23 15:36:58 +0000

    REGRESSION (r240727) [ Mac iOS ] Layout Test http/tests/workers/service/basic-register-exceptions.html is flaky https://bugs.webkit.org/show_bug.cgi?id=194368 <rdar://problem/47937467>
    Reviewed by Alex Christensen.
    
    Test was exiting sooner than the last registration was rejected.
    Make sure that the test is run until the end in a more ordered way.
    
    * http/tests/workers/service/basic-register-exceptions-expected.txt:
    * http/tests/workers/service/resources/basic-register-exceptions.js:
    (async.doTest):
    (then): Deleted.
    (catch): Deleted.
    * platform/mac-wk2/TestExpectations:
    
    
    Canonical link: https://commits.webkit.org/211399@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@244539 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/workers/service/resources/basic-register-exceptions.js
+++ LayoutTests/http/tests/workers/service/resources/basic-register-exceptions.js
@@ -3,95 +3,30 @@ function done()
     finishSWTest();
 }
 
-navigator.serviceWorker.register("image-mime-type.php", { })
-.then(function(r) {
-	console.log("Registered! (unexpectedly)");
-}, function(e) {
-	console.log("Registration failed with error: " + e);
-})
-.catch(function(e) {
-	console.log("Exception registering: " + e);
-});
+var registrations = [
+    ["image-mime-type.php", {}],
+    ["", {}],
+    ["http://127.0.0.1:abc", {}],
+    ["somescheme://script.js", { }],
+    ["resources/%2fscript.js", { }],
+    ["resources/script.js", { scope: "somescheme://script.js" }],
+    ["resources/script.js", { scope: "%2fscript.js" }],
+    ["http://localhost:8000/workers/service/resources/empty-worker.js", { }],
+    ["resources/empty-worker.js", { scope: "http://localhost:8000/workers/service/" }]
+];
 
-navigator.serviceWorker.register("", { })
-.then(function(r) {
-	console.log("Registered! (unexpectedly)");
-}, function(e) {
-	console.log("Registration failed with error: " + e);
-})
-.catch(function(e) {
-	console.log("Exception registering: " + e);
-});
+async function doTest()
+{
+    for (let registration of registrations) {
+        await navigator.serviceWorker.register(registration[0], registration[1]).then(function(r) {
+            console.log("Registered! (unexpectedly)");
+        }, function(e) {
+            console.log("Registration failed with error: " + e);
+        }).catch(function(e) {
+            console.log("Exception registering: " + e);i
+        });
+    }
+    finishSWTest();
+}
 
-navigator.serviceWorker.register("http://127.0.0.1:abc", { })
-.then(function(r) {
-	console.log("Registered! (unexpectedly)");
-}, function(e) {
-	console.log("Registration failed with error: " + e);
-})
-.catch(function(e) {
-	console.log("Exception registering: " + e);
-});
-
-navigator.serviceWorker.register("somescheme://script.js", { })
-.then(function(r) {
-	console.log("Registered! (unexpectedly)");
-}, function(e) {
-	console.log("Registration failed with error: " + e);
-})
-.catch(function(e) {
-	console.log("Exception registering: " + e);
-});
-
-navigator.serviceWorker.register("resources/%2fscript.js", { })
-.then(function(r) {
-	console.log("Registered! (unexpectedly)");
-}, function(e) {
-	console.log("Registration failed with error: " + e);
-})
-.catch(function(e) {
-	console.log("Exception registering: " + e);
-});
-
-navigator.serviceWorker.register("resources/script.js", { scope: "somescheme://script.js" })
-.then(function(r) {
-	console.log("Registered! (unexpectedly)");
-}, function(e) {
-	console.log("Registration failed with error: " + e);
-})
-.catch(function(e) {
-	console.log("Exception registering: " + e);
-});
-
-navigator.serviceWorker.register("resources/script.js", { scope: "%2fscript.js" })
-.then(function(r) {
-	console.log("Registered! (unexpectedly)");
-}, function(e) {
-	console.log("Registration failed with error: " + e);
-})
-.catch(function(e) {
-	console.log("Exception registering: " + e);
-});
-
-navigator.serviceWorker.register("http://localhost:8000/workers/service/resources/empty-worker.js", { })
-.then(function(r) {
-	console.log("Registered! (unexpectedly)");
-}, function(e) {
-	console.log("Registration failed with error: " + e);
-})
-.catch(function(e) {
-	console.log("Exception registering: " + e);
-});
-
-navigator.serviceWorker.register("resources/empty-worker.js", { scope: "http://localhost:8000/workers/service/" })
-.then(function(r) {
-	console.log("Registered! (unexpectedly)");
-	done();
-}, function(e) {
-	console.log("Registration failed with error: " + e);
-	done();
-})
-.catch(function(e) {
-	console.log("Exception registering: " + e);
-	done();
-});
+doTest();
