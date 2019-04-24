f728a4041c5ad3b47dee899090eb221f5a91b311
Greg V <greg@unrelenting.technology>
2019-04-24 15:51:39 +0000

    Fix -Wc++11-narrowing on unsigned char platforms like FreeBSD/aarch64 https://bugs.webkit.org/show_bug.cgi?id=197148
    Patch by Greg V <greg@unrelenting.technology> on 2019-04-24
    Reviewed by Alex Christensen.
    
    * contentextensions/DFACombiner.cpp:
    * contentextensions/NFAToDFA.cpp:
    
    Canonical link: https://commits.webkit.org/211451@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@244591 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/contentextensions/DFACombiner.cpp
+++ Source/WebCore/contentextensions/DFACombiner.cpp
@@ -37,7 +37,7 @@ namespace WebCore {
 namespace ContentExtensions {
 
 class DFAMerger {
-    typedef MutableRangeList<char, uint64_t, 128> CombinedTransitionsMutableRangeList;
+    typedef MutableRangeList<signed char, uint64_t, 128> CombinedTransitionsMutableRangeList;
 
     enum class WhichDFA {
         A,
--- Source/WebCore/contentextensions/NFAToDFA.cpp
+++ Source/WebCore/contentextensions/NFAToDFA.cpp
@@ -41,9 +41,9 @@ namespace WebCore {
 
 namespace ContentExtensions {
 
-typedef MutableRange<char, NFANodeIndexSet> NFANodeRange;
-typedef MutableRangeList<char, NFANodeIndexSet> NFANodeRangeList;
-typedef MutableRangeList<char, NFANodeIndexSet, 128> PreallocatedNFANodeRangeList;
+typedef MutableRange<signed char, NFANodeIndexSet> NFANodeRange;
+typedef MutableRangeList<signed char, NFANodeIndexSet> NFANodeRangeList;
+typedef MutableRangeList<signed char, NFANodeIndexSet, 128> PreallocatedNFANodeRangeList;
 typedef Vector<uint32_t, 0, ContentExtensionsOverflowHandler> UniqueNodeList;
 typedef Vector<UniqueNodeList, 0, ContentExtensionsOverflowHandler> NFANodeClosures;
 
