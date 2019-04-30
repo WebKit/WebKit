534c1db66a1515fd60ba58d914d23f4e68dd53ed
Aakash Jain <aakash_jain@apple.com>
2019-04-30 15:01:05 +0000

    [ews-build] Enable webkitpy queue on new EWS https://bugs.webkit.org/show_bug.cgi?id=197396
    Reviewed by Lucas Forschler.
    
    * BuildSlaveSupport/ews-app/ews/views/statusbubble.py:
    (StatusBubble):
    * BuildSlaveSupport/ews-build/config.json:
    
    
    Canonical link: https://commits.webkit.org/211595@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@244770 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/BuildSlaveSupport/ews-app/ews/views/statusbubble.py
+++ Tools/BuildSlaveSupport/ews-app/ews/views/statusbubble.py
@@ -41,7 +41,7 @@ class StatusBubble(View):
     # FIXME: Auto-generate this list https://bugs.webkit.org/show_bug.cgi?id=195640
     ALL_QUEUES = ['api-ios', 'api-mac', 'bindings', 'gtk', 'ios', 'ios-sim', 'ios-wk2', 'jsc', 'mac', 'mac-32bit', 'mac-32bit-wk2',
                     'mac-debug', 'mac-debug-wk1', 'mac-wk1', 'mac-wk2', 'style', 'webkitperl', 'webkitpy', 'win', 'wincairo', 'wpe']
-    ENABLED_QUEUES = ['api-ios', 'api-mac', 'webkitperl']
+    ENABLED_QUEUES = ['api-ios', 'api-mac', 'webkitperl', 'webkitpy']
     # FIXME: Auto-generate the queue's trigger relationship
     QUEUE_TRIGGERS = {
         'api-ios': 'ios-sim',
