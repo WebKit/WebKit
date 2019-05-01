f981bb249f847c8fff4083865335c07913ea4a33
Aakash Jain <aakash_jain@apple.com>
2019-05-01 21:40:25 +0000

    Remove webkitpy and bindings EWS queues from dashboard https://bugs.webkit.org/show_bug.cgi?id=197475
    Reviewed by Alexey Proskuryakov.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/dashboard/Scripts/BubbleQueueServer.js:
    
    
    Canonical link: https://commits.webkit.org/211654@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@244852 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/BuildSlaveSupport/build.webkit.org-config/public_html/dashboard/Scripts/BubbleQueueServer.js
+++ Tools/BuildSlaveSupport/build.webkit.org-config/public_html/dashboard/Scripts/BubbleQueueServer.js
@@ -37,8 +37,6 @@ BubbleQueueServer = function()
         "mac-ews": {platform: Dashboard.Platform.macOSHighSierra, shortName: "mac", title: "WebKit1\xa0Release\xa0Tests\xa0EWS"},
         "mac-wk2-ews": {platform: Dashboard.Platform.macOSHighSierra, shortName: "mac-wk2", title: "WebKit2\xa0Release\xa0Tests\xa0EWS"},
         "mac-debug-ews": {platform: Dashboard.Platform.macOSHighSierra, shortName: "mac-debug", title: "WebKit1\xa0Debug\xa0Tests\xa0EWS"},
-        "bindings-ews": {platform: Dashboard.Platform.macOSHighSierra, shortName: "bindings", title: "Bindings\xa0EWS"},
-        "webkitpy-ews": {platform: Dashboard.Platform.macOSHighSierra, shortName: "webkitpy", title: "Webkitpy\xa0EWS"},
         "win-ews": {platform: Dashboard.Platform.Windows7, shortName: "win", title: "WebKit1\xa0Release\xa0Build\xa0EWS"},
         "wincairo-ews": {platform: Dashboard.Platform.WinCairo, shortName: "wincairo", title: "WebKit1\xa0Release\xa0Build\xa0EWS"},
         "wpe-ews": {platform: Dashboard.Platform.LinuxWPE, shortName: "wpe", title: "WebKit2\xa0Release\xa0Build\xa0EWS"},
