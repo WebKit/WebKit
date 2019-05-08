WebKit/WebKit

commit 76ba1d773b88b42ca43bc02b18eb9f6a7220a8f0

WebDriver: evaluating javascript shouldn't fail if a dialog is shown https://bugs.webkit.org/show_bug.cgi?id=196847 <rdar://problem/49609396>
Reviewed by Brian Burg.

* UIProcess/Automation/WebAutomationSession.cpp:
(WebKit::WebAutomationSession::willShowJavaScriptDialog):


Canonical link: https://commits.webkit.org/211139@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@244230 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/Automation/WebAutomationSession.cpp
@@ -652,10 +652,9 @@ void WebAutomationSession::willShowJavaScriptDialog(WebPageProxy& page)
         }
 
         if (!m_evaluateJavaScriptFunctionCallbacks.isEmpty()) {
-            Inspector::ErrorString unexpectedAlertOpenError = STRING_FOR_PREDEFINED_ERROR_NAME(UnexpectedAlertOpen);
             for (auto key : copyToVector(m_evaluateJavaScriptFunctionCallbacks.keys())) {
                 auto callback = m_evaluateJavaScriptFunctionCallbacks.take(key);
-                callback->sendFailure(unexpectedAlertOpenError);
+                callback->sendSuccess(emptyString());
             }
         }
 

commit 09d9b0e4f988ae84a24491ab6ecbcc87d6c1f618

REGRESSION(r244230): Web Automation: use null instead of empty string as success result of scripts when alert is open https://bugs.webkit.org/show_bug.cgi?id=197655
Patch by Carlos Garcia Campos <cgarcia@igalia.com> on 2019-05-07
Reviewed by Devin Rousso.

This regressed in r244230, the case of an alert open while running a script is now handled in WebKit side, but
an empty string is returned as a result, which is not a valid JSON serialized string. That made all user prompts
tests to fail.

* UIProcess/Automation/WebAutomationSession.cpp:
(WebKit::WebAutomationSession::willShowJavaScriptDialog): Use null as script result instead of an empty string.

Canonical link: https://commits.webkit.org/211825@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@245046 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/Automation/WebAutomationSession.cpp
@@ -654,7 +654,7 @@ void WebAutomationSession::willShowJavaScriptDialog(WebPageProxy& page)
         if (!m_evaluateJavaScriptFunctionCallbacks.isEmpty()) {
             for (auto key : copyToVector(m_evaluateJavaScriptFunctionCallbacks.keys())) {
                 auto callback = m_evaluateJavaScriptFunctionCallbacks.take(key);
-                callback->sendSuccess(emptyString());
+                callback->sendSuccess("null"_s);
             }
         }
 