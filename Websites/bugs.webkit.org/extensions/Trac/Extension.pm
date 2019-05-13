2be4b9720854bf3bcb39ff7e51699e41c0b25fa5
Jer Noble <jer.noble@apple.com>
2019-05-13 23:09:05 +0000

    Bugzilla should convert "r12345" to a trac.webkit.org link https://bugs.webkit.org/show_bug.cgi?id=197790
    Reviewed by Alexey Proskuryakov.
    
    * extensions/Trac/Config.pm: Added.
    * extensions/Trac/Extension.pm: Added.
    (bug_format_comment):
    (_replace_revision):
    
    
    Canonical link: https://commits.webkit.org/211986@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@245261 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Websites/bugs.webkit.org/extensions/Trac/Extension.pm
@@ -0,0 +1,49 @@
+# Copyright (C) 2019 Apple Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package Bugzilla::Extension::Trac;
+
+use strict;
+use warnings;
+
+use parent qw(Bugzilla::Extension);
+
+our $VERSION = "1.0.0";
+
+sub bug_format_comment {
+    my ($self, $args) = @_;
+    my $regexes = $args->{'regexes'};
+
+    # Should match "r12345" and "trac.webkit.org/r12345" but not "https://trac.webkit.org/r12345"
+    push(@$regexes, { match => qr/(?<!\/)\b((r[[:digit:]]{5,}))\b/, replace => \&_replace_revision });
+    push(@$regexes, { match => qr/(?<!\/)(trac.webkit.org\/(r[[:digit:]]{5,}))\b/, replace => \&_replace_revision });
+}
+
+sub _replace_revision {
+    my $args = shift;
+    my $text = $args->{matches}->[0];
+    my $revision = $args->{matches}->[1];
+    return qq{<a href="https://trac.webkit.org/$revision">$text</a>};
+};
+
+# This must be the last line of your extension.
+__PACKAGE__->NAME;
