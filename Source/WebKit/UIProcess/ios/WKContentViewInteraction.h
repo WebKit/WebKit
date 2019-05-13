e4576c4b972c404085a4f75ff0075f92b73b08fe
Dean Jackson <dino@apple.com>
2019-05-13 06:51:08 +0000

    Cherry-pick r245153. rdar://problem/50604700
        Guard long press link preview with a USE macro rather than complicated includes
        https://bugs.webkit.org/show_bug.cgi?id=197728
        <rdar://problem/50604700>
    
        Reviewed by Wenson Hsieh.
    
        It was getting complicated to manage the coordination between WebKitAdditions
        and WebKit without having to split up the includes into multiple files
        or have lots of duplication.
    
        Instead, WebKitAdditions will now define a USE macro which can be used
        in various places through WebKit.
    
        * UIProcess/ios/WKContentViewInteraction.h:
        * UIProcess/ios/WKContentViewInteraction.mm:
        (-[WKContentView dragInteraction:itemsForBeginningSession:]):
        (-[WKContentView _contentsOfUserInterfaceItem:]):
        (previewIdentifierForElementAction):
        (-[WKContentView shouldUsePreviewForLongPress]):
        (-[WKContentView _registerPreview]):
        (-[WKContentView _unregisterPreview]):
        (shouldUsePreviewForLongPress): Deleted.
        (-[WKContentView _registerPreviewLongPress]): Deleted.
        (-[WKContentView _unregisterPreviewLongPress]): Deleted.
    
        Canonical link: https://commits.webkit.org/211895@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@245153 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/211878.6@safari-608.1.24-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-608.1.24-branch@245227 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/ios/WKContentViewInteraction.h
+++ Source/WebKit/UIProcess/ios/WKContentViewInteraction.h
@@ -61,13 +61,6 @@
 
 #if USE(APPLE_INTERNAL_SDK) && __has_include(<WebKitAdditions/WKInteractionPreviewAdditions.h>)
 #import <WebKitAdditions/WKInteractionPreviewAdditions.h>
-#else
-#ifndef ADDITIONAL_LINK_PREVIEW_MEMBERS
-#define ADDITIONAL_LINK_PREVIEW_MEMBERS
-#endif
-#ifndef ADDITIONAL_LINK_PREVIEW_PROTOCOLS
-#define ADDITIONAL_LINK_PREVIEW_PROTOCOLS
-#endif
 #endif
 
 namespace API {
@@ -255,8 +248,11 @@ struct WKAutoCorrectionData {
     RetainPtr<UIGestureRecognizer> _previewSecondaryGestureRecognizer;
     Vector<bool> _focusStateStack;
 #if HAVE(LINK_PREVIEW)
+#if USE(LONG_PRESS_FOR_LINK_PREVIEW)
+    LONG_PRESS_LINK_PREVIEW_MEMBERS
+#else
     RetainPtr<UIPreviewItemController> _previewItemController;
-    ADDITIONAL_LINK_PREVIEW_MEMBERS
+#endif
 #endif
 
     std::unique_ptr<WebKit::SmartMagnificationController> _smartMagnificationController;
@@ -527,7 +523,11 @@ FOR_EACH_PRIVATE_WKCONTENTVIEW_ACTION(DECLARE_WKCONTENTVIEW_ACTION_FOR_WEB_VIEW)
 @end
 
 #if HAVE(LINK_PREVIEW)
-@interface WKContentView (WKInteractionPreview) <UIPreviewItemDelegate ADDITIONAL_LINK_PREVIEW_PROTOCOLS>
+#if USE(LONG_PRESS_FOR_LINK_PREVIEW)
+@interface WKContentView (WKInteractionPreview) <LONG_PRESS_LINK_PREVIEW_PROTOCOL>
+#else
+@interface WKContentView (WKInteractionPreview) <UIPreviewItemDelegate>
+#endif
 
 @property (nonatomic, readonly) BOOL shouldUsePreviewForLongPress;
 
