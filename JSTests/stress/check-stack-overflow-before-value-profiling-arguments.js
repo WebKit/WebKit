2b55c66680f1098725a44387e0d7fc77105f9957
Saam Barati <sbarati@apple.com>
2019-05-15 21:44:48 +0000

    Cherry-pick r244865. rdar://problem/50753937
        Baseline JIT should do argument value profiling after checking for stack overflow
        https://bugs.webkit.org/show_bug.cgi?id=197052
        <rdar://problem/50009602>
    
        Reviewed by Yusuke Suzuki.
    
        JSTests:
    
        * stress/check-stack-overflow-before-value-profiling-arguments.js: Added.
    
        Source/JavaScriptCore:
    
        Otherwise, we may do value profiling without running a write barrier, which
        is against the rules of how we do value profiling.
    
        * jit/JIT.cpp:
        (JSC::JIT::compileWithoutLinking):
    
        Canonical link: https://commits.webkit.org/211667@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@244865 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/207701.473@safari-607-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@245350 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ JSTests/stress/check-stack-overflow-before-value-profiling-arguments.js
@@ -0,0 +1,41 @@
+//@ runDefault("--useConcurrentJIT=0", "--thresholdForJITAfterWarmUp=10", "--slowPathAllocsBetweenGCs=10", "--useConcurrentGC=0")
+
+function fullGC() {
+    for (var i = 0; i < 10; i++) {
+        new Float64Array(0x1000000);
+    }
+}
+
+function outer() {
+    function f() {
+        try {
+            const r = f();
+        } catch(e) {
+            const o = Object();
+            function inner(a1, a2, a3) {
+                try {
+                    const r1 = new Uint32Array();
+                    const r2 = r1.values();
+                } catch(e2) {
+                }
+            }
+            const result = inner();
+        }
+    }
+
+    f();
+
+    function edenGC() {
+        for (let i = 0; i < 100; i++) {
+            const floatArray = new Float64Array(0x10000);
+        }
+    }
+    edenGC();
+}
+
+for (let i = 0; i < 100; i++) {
+    const result = outer();
+}
+
+fullGC();
+
