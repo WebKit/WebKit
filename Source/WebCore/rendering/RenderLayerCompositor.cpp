9043d72b9de71130797381cbf09ec9b77f287859
Simon Fraser <simon.fraser@apple.com>
2019-05-17 10:14:14 +0000

    Merge r243786 - REGRESSION (r238266): Exchange 2013 Outlook Web Access displays partially blank page when creating new e-mail https://bugs.webkit.org/show_bug.cgi?id=196522 Source/WebCore:
    rdar://problem/49472941
    
    Reviewed by Zalan Bujtas.
    
    In this content a layer is composited to clip descendants, and has negative z-order children,
    so we compute that it "paints into ancestor", and has a foreground layer. This combination doesn't
    make sense, and when the layer becomes scrollable, we end up with bad paint phases on layers, and
    fail to paint the contents.
    
    Fix by ensuring that a layer has its own backing store if it requires a foreground layer
    by virtue of having negative z-order children.
    
    Test: compositing/backing/foreground-layer-no-paints-into-ancestor.html
    
    * rendering/RenderLayerCompositor.cpp:
    (WebCore::RenderLayerCompositor::requiresOwnBackingStore const):
    
    LayoutTests:
    
    Reviewed by Zalan Bujtas.
    
    * compositing/backing/foreground-layer-no-paints-into-ancestor-expected.html: Added.
    * compositing/backing/foreground-layer-no-paints-into-ancestor.html: Added.

--- Source/WebCore/rendering/RenderLayerCompositor.cpp
+++ Source/WebCore/rendering/RenderLayerCompositor.cpp
@@ -2198,6 +2198,7 @@ bool RenderLayerCompositor::requiresOwnBackingStore(const RenderLayer& layer, co
         || requiresCompositingForPlugin(renderer, queryData)
         || requiresCompositingForEditableImage(renderer)
         || requiresCompositingForOverflowScrolling(layer, queryData)
+        || needsContentsCompositingLayer(layer)
         || renderer.isTransparent()
         || renderer.hasMask()
         || renderer.hasReflection()
