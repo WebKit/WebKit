7bfcbd3f645d21abcafb63d602c7aaea2bcc2b0e
Keith Rollin <krollin@apple.com>
2019-05-17 19:29:17 +0000

    Re-enable generate-xcfilelists https://bugs.webkit.org/show_bug.cgi?id=197933 <rdar://problem/50831677>
    Reviewed by Jonathan Bedard.
    
    The following two tasks have been completed, and we can re-enable
    generate-xcfilelists:
    
    Bug 197619 <rdar://problem/50507392> Temporarily disable generate-xcfilelists (197619)
    Bug 197622 <rdar://problem/50508222> Rewrite generate-xcfilelists in Python (197622)
    
    Source/JavaScriptCore:
    
    * Scripts/check-xcfilelists.sh:
    
    Source/WebCore:
    
    No new tests -- no change in user-visible functionality.
    
    * Scripts/check-xcfilelists.sh:
    
    Source/WebKit:
    
    * Scripts/check-xcfilelists.sh:
    
    Tools:
    
    * DumpRenderTree/Scripts/check-xcfilelists.sh:
    * WebKitTestRunner/Scripts/check-xcfilelists.sh:
    
    
    Canonical link: https://commits.webkit.org/212098@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@245474 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Scripts/check-xcfilelists.sh
+++ Source/WebCore/Scripts/check-xcfilelists.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# FIXME: Remove this once https://bugs.webkit.org/show_bug.cgi?id=197622 is resolved
-exit 0
-
 [ "${WK_DISABLE_CHECK_XCFILELISTS}" == "1" ] && { echo "### Not running because WK_DISABLE_CHECK_XCFILELISTS is set"; exit 0; }
 [ "${DEPLOYMENT_LOCATION}" == "YES" ] && { echo "### Not running because DEPLOYMENT_LOCATION is YES"; exit 0; }
 [ "${USE_INTERNAL_SDK}" == "YES" ] || { echo "### Not running because USE_INTERNAL_SDK is not YES"; exit 0; }
