4a90bd12f1f73fe59b36de7f19500fa3a6f38fcb
Carlos Garcia Campos <cgarcia@igalia.com>
2019-05-22 08:37:30 +0000

    Unreviewed. Fix GTK unit tests after r245565
    * UIProcess/API/gtk/WebKitWebViewBase.cpp:
    (webkitWebViewBaseDispose): Null check accessible before calling webkitWebViewAccessibleSetWebView().
    
    Canonical link: https://commits.webkit.org/212187@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@245617 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/gtk/WebKitWebViewBase.cpp
+++ Source/WebKit/UIProcess/API/gtk/WebKitWebViewBase.cpp
@@ -561,7 +561,8 @@ static void webkitWebViewBaseDispose(GObject* gobject)
 {
     WebKitWebViewBase* webView = WEBKIT_WEB_VIEW_BASE(gobject);
     webkitWebViewBaseSetToplevelOnScreenWindow(webView, nullptr);
-    webkitWebViewAccessibleSetWebView(WEBKIT_WEB_VIEW_ACCESSIBLE(webView->priv->accessible.get()), nullptr);
+    if (webView->priv->accessible)
+        webkitWebViewAccessibleSetWebView(WEBKIT_WEB_VIEW_ACCESSIBLE(webView->priv->accessible.get()), nullptr);
 #if GTK_CHECK_VERSION(3, 24, 0)
     webkitWebViewBaseCompleteEmojiChooserRequest(webView, emptyString());
 #endif
