aaef6a150d5de7065e994414fbfb30440fb17a5c
Brent Fulgham <bfulgham@webkit.org>
2019-05-31 00:29:58 +0000

    Cherry-pick r245284. rdar://problem/51264857
        Protect current WebFrame during form submission
        https://bugs.webkit.org/show_bug.cgi?id=197459
        <rdar://problem/50368618>
    
        Reviewed by Alex Christensen.
    
        The 'continueWillSubmitForm' method calls a completion handler that
        might affect the state of the current frame. Ensure the frame is valid
        for the scope of the function.
    
        * WebProcess/WebPage/WebFrame.cpp:
        (WebKit::WebFrame::continueWillSubmitForm):
    
        Canonical link: https://commits.webkit.org/212006@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@245284 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/207701.494@safari-607-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@245920 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/WebPage/WebFrame.cpp
+++ Source/WebKit/WebProcess/WebPage/WebFrame.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2016 Apple Inc. All rights reserved.
+ * Copyright (C) 2010-2019 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -233,6 +233,7 @@ uint64_t WebFrame::setUpWillSubmitFormListener(CompletionHandler<void()>&& compl
 
 void WebFrame::continueWillSubmitForm(uint64_t listenerID)
 {
+    Ref<WebFrame> protectedThis(*this);
     if (auto completionHandler = m_willSubmitFormCompletionHandlers.take(listenerID))
         completionHandler();
     invalidatePolicyListener();
