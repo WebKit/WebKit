922af495a62d8a3cfff7eea5ecb27bdf106104ec
Brent Fulgham <bfulgham@webkit.org>
2019-05-31 00:30:10 +0000

    Cherry-pick r245509. rdar://problem/51264845
        Wait to get frame until after layout has been run
        https://bugs.webkit.org/show_bug.cgi?id=197999
        <rdar://problem/50800345>
    
        Reviewed by Alex Christensen.
    
        Source/WebCore:
    
        The current frame can change when layout runs, so don't bother retrieving
        the frame until the final layout pass is complete.
    
        Test: fast/dom/window-inner-width-crash.html
    
        * page/DOMWindow.cpp:
        (WebCore::DOMWindow::innerHeight const): Move frame access past the
        layout operation.
        (WebCore::DOMWindow::innerWidth const): Ditto.
        (WebCore::DOMWindow::scrollX const): Ditto.
        (WebCore::DOMWindow::scrollY const): Ditto.
    
        LayoutTests:
    
        * fast/dom/window-inner-width-crash-expected.txt: Added.
        * fast/dom/window-inner-width-crash.html: Added.
    
        Canonical link: https://commits.webkit.org/212129@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@245509 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/207701.498@safari-607-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@245924 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/DOMWindow.cpp
+++ Source/WebCore/page/DOMWindow.cpp
@@ -1236,14 +1236,17 @@ int DOMWindow::outerWidth() const
 
 int DOMWindow::innerHeight() const
 {
-    auto* frame = this->frame();
-    if (!frame)
+    if (!frame())
         return 0;
-
+    
     // Force enough layout in the parent document to ensure that the FrameView has been resized.
     if (auto* frameElement = this->frameElement())
         frameElement->document().updateLayoutIfDimensionsOutOfDate(*frameElement, HeightDimensionsCheck);
 
+    auto* frame = this->frame();
+    if (!frame)
+        return 0;
+    
     FrameView* view = frame->view();
     if (!view)
         return 0;
@@ -1253,13 +1256,16 @@ int DOMWindow::innerHeight() const
 
 int DOMWindow::innerWidth() const
 {
-    auto* frame = this->frame();
-    if (!frame)
+    if (!frame())
         return 0;
 
     // Force enough layout in the parent document to ensure that the FrameView has been resized.
     if (auto* frameElement = this->frameElement())
         frameElement->document().updateLayoutIfDimensionsOutOfDate(*frameElement, WidthDimensionsCheck);
+
+    auto* frame = this->frame();
+    if (!frame)
+        return 0;
 
     FrameView* view = frame->view();
     if (!view)
@@ -1310,7 +1316,16 @@ int DOMWindow::scrollX() const
 
     frame->document()->updateLayoutIgnorePendingStylesheets();
 
-    return view->mapFromLayoutToCSSUnits(view->contentsScrollPosition().x());
+    // Layout may have affected the current frame:
+    auto* frameAfterLayout = this->frame();
+    if (!frameAfterLayout)
+        return 0;
+    
+    FrameView* viewAfterLayout = frameAfterLayout->view();
+    if (!viewAfterLayout)
+        return 0;
+
+    return viewAfterLayout->mapFromLayoutToCSSUnits(viewAfterLayout->contentsScrollPosition().x());
 }
 
 int DOMWindow::scrollY() const
@@ -1329,7 +1344,16 @@ int DOMWindow::scrollY() const
 
     frame->document()->updateLayoutIgnorePendingStylesheets();
 
-    return view->mapFromLayoutToCSSUnits(view->contentsScrollPosition().y());
+    // Layout may have affected the current frame:
+    auto* frameAfterLayout = this->frame();
+    if (!frameAfterLayout)
+        return 0;
+    
+    FrameView* viewAfterLayout = frameAfterLayout->view();
+    if (!viewAfterLayout)
+        return 0;
+    
+    return viewAfterLayout->mapFromLayoutToCSSUnits(viewAfterLayout->contentsScrollPosition().y());
 }
 
 bool DOMWindow::closed() const
