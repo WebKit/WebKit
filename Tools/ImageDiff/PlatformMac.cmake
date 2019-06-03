cd6420e7d64e999714223b840b4d55812f8ed810
Don Olmstead <don.olmstead@sony.com>
2019-06-03 20:12:18 +0000

    [CMake] Use WEBKIT_EXECUTABLE in ImageDiff https://bugs.webkit.org/show_bug.cgi?id=198490
    Reviewed by Konstantin Tokarev.
    
    Use WEBKIT_EXECUTABLE in ImageDiff. Change CMake variables to use the proper
    convention. Add a shared CoreGraphics.cmake instead of just including PlatformMac
    for AppleWin builds.
    
    * ImageDiff/CMakeLists.txt:
    * ImageDiff/Cairo.cmake:
    * ImageDiff/CoreGraphics.cmake: Added.
    * ImageDiff/PlatformGTK.cmake:
    * ImageDiff/PlatformMac.cmake:
    * ImageDiff/PlatformWin.cmake:
    
    
    Canonical link: https://commits.webkit.org/212509@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@246044 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/ImageDiff/PlatformMac.cmake
+++ Tools/ImageDiff/PlatformMac.cmake
@@ -1,8 +1,1 @@
-list(APPEND IMAGE_DIFF_SOURCES
-    ${IMAGE_DIFF_DIR}/cg/PlatformImageCG.cpp
-)
-list(APPEND IMAGE_DIFF_LIBRARIES
-    CoreFoundation
-    CoreGraphics
-    CoreText
-)
+include(CoreGraphics.cmake)
