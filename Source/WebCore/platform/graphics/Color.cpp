d7c1aec8844f1aab59e00186cd5f11bd26bf6812
Timothy Hatcher <timothy@apple.com>
2019-06-04 05:05:14 +0000

    Cherry-pick r246042. rdar://problem/50667125
        Tweak the text and underline color for data detected text.
        https://bugs.webkit.org/show_bug.cgi?id=198487
        rdar://problem/50667125
    
        Reviewed by Devin Rousso.
    
        Source/WebCore:
    
        Tests: Color.RGBToHSL API tests
    
        * editing/cocoa/DataDetection.mm:
        (WebCore::DataDetection::detectContentInRange): Use currentcolor so semantic text colors work.
        Force the lightness of the underline color to the middle, and multiply the alpha by 38%,
        so the color will appear on light and dark backgrounds, since only one color can be specified.
        * platform/graphics/Color.cpp:
        (WebCore::Color::getHSL const): Return hue in [0...6) range to easily round-trip with makeRGBAFromHSLA().
    
        Tools:
    
        * TestWebKitAPI/Tests/WebCore/Color.cpp:
        (TestWebKitAPI::TEST): Added Color.RGBToHSL tests.
    
        Canonical link: https://commits.webkit.org/212507@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@246042 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/212437.9@safari-608.1.27-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-608.1.27-branch@246055 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/Color.cpp
+++ Source/WebCore/platform/graphics/Color.cpp
@@ -542,7 +542,7 @@ void Color::getHSL(double& hue, double& saturation, double& lightness) const
 {
     // http://en.wikipedia.org/wiki/HSL_color_space. This is a direct copy of
     // the algorithm therein, although it's 360^o based and we end up wanting
-    // [0...1) based. It's clearer if we stick to 360^o until the end.
+    // [0...6) based. It's clearer if we stick to 360^o until the end.
     double r = static_cast<double>(red()) / 255.0;
     double g = static_cast<double>(green()) / 255.0;
     double b = static_cast<double>(blue()) / 255.0;
@@ -562,8 +562,8 @@ void Color::getHSL(double& hue, double& saturation, double& lightness) const
     if (hue >= 360.0)
         hue -= 360.0;
 
-    // makeRGBAFromHSLA assumes that hue is in [0...1).
-    hue /= 360.0;
+    // makeRGBAFromHSLA assumes that hue is in [0...6).
+    hue /= 60.0;
 
     lightness = 0.5 * (max + min);
     if (!chroma)
