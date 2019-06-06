754a15d53368a2b88d2ceb11bd72bc5c7a535017
Michael Catanzaro <mcatanzaro@igalia.com>
2019-06-06 13:51:13 +0000

    [WPE][GTK] Clean up use of initiatingPageID in implementation of webkit_uri_scheme_request_get_web_view() https://bugs.webkit.org/show_bug.cgi?id=198564
    Reviewed by Carlos Garcia Campos.
    
    Note that we have to use RefPtr rather than Ref here because the priv struct has to remain
    default-constructible.
    
    * UIProcess/API/glib/WebKitURISchemeRequest.cpp:
    (webkitURISchemeRequestCreate):
    (webkit_uri_scheme_request_get_web_view):
    
    
    Canonical link: https://commits.webkit.org/212605@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@246152 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/glib/WebKitURISchemeRequest.cpp
+++ Source/WebKit/UIProcess/API/glib/WebKitURISchemeRequest.cpp
@@ -84,8 +84,12 @@ WebKitURISchemeRequest* webkitURISchemeRequestCreate(uint64_t requestID, WebKitW
     request->priv->webContext = webContext;
     request->priv->manager = &manager;
     request->priv->uri = resourceRequest.url().string().utf8();
-    request->priv->initiatingPage = WebProcessProxy::webPage(*resourceRequest.initiatingPageID());
     request->priv->requestID = requestID;
+
+    ASSERT(resourceRequest.initiatingPageID());
+    request->priv->initiatingPage = WebProcessProxy::webPage(*resourceRequest.initiatingPageID());
+    ASSERT(request->priv->initiatingPage);
+
     return request;
 }
 
@@ -166,8 +170,7 @@ WebKitWebView* webkit_uri_scheme_request_get_web_view(WebKitURISchemeRequest* re
 {
     g_return_val_if_fail(WEBKIT_IS_URI_SCHEME_REQUEST(request), 0);
 
-    // FIXME: initiatingPage is now always null, we need to re-implement this somehow.
-    return request->priv->initiatingPage ? webkitWebContextGetWebViewForPage(request->priv->webContext, request->priv->initiatingPage.get()) : nullptr;
+    return webkitWebContextGetWebViewForPage(request->priv->webContext, request->priv->initiatingPage.get());
 }
 
 static void webkitURISchemeRequestReadCallback(GInputStream* inputStream, GAsyncResult* result, WebKitURISchemeRequest* schemeRequest)
