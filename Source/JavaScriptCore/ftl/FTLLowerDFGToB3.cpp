6391f275c833dcfa129fc48e4990112c6f3cbb9e
Tadeu Zagallo <tzagallo@apple.com>
2019-06-12 22:20:54 +0000

    Cherry-pick r246071. rdar://problem/51656838
        Argument elimination should check for negative indices in GetByVal
        https://bugs.webkit.org/show_bug.cgi?id=198302
        <rdar://problem/51188095>
    
        Reviewed by Filip Pizlo.
    
        JSTests:
    
        * stress/eliminate-arguments-negative-rest-access.js: Added.
        (inlinee):
        (opt):
    
        Source/JavaScriptCore:
    
        In DFG::ArgumentEliminationPhase, the index is treated as unsigned, but there's no check
        for overflow in the addition. In compileGetMyArgumentByVal, there's a check for overflow,
        but the index is treated as signed, resulting in an index lower than numberOfArgumentsToSkip.
    
        * dfg/DFGArgumentsEliminationPhase.cpp:
        * ftl/FTLLowerDFGToB3.cpp:
        (JSC::FTL::DFG::LowerDFGToB3::compileGetMyArgumentByVal):
    
        Canonical link: https://commits.webkit.org/212532@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@246071 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/207701.521@safari-607-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@246379 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp
+++ Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp
@@ -4232,13 +4232,15 @@ private:
         
         LValue numberOfArgs = m_out.sub(numberOfArgsIncludingThis, m_out.int32One);
         LValue indexToCheck = originalIndex;
+        LValue numberOfArgumentsToSkip = m_out.int32Zero;
         if (m_node->numberOfArgumentsToSkip()) {
-            CheckValue* check = m_out.speculateAdd(indexToCheck, m_out.constInt32(m_node->numberOfArgumentsToSkip()));
+            numberOfArgumentsToSkip = m_out.constInt32(m_node->numberOfArgumentsToSkip());
+            CheckValue* check = m_out.speculateAdd(indexToCheck, numberOfArgumentsToSkip);
             blessSpeculation(check, Overflow, noValue(), nullptr, m_origin);
             indexToCheck = check;
         }
 
-        LValue isOutOfBounds = m_out.aboveOrEqual(indexToCheck, numberOfArgs);
+        LValue isOutOfBounds = m_out.bitOr(m_out.aboveOrEqual(indexToCheck, numberOfArgs), m_out.below(indexToCheck, numberOfArgumentsToSkip));
         LBasicBlock continuation = nullptr;
         LBasicBlock lastNext = nullptr;
         ValueFromBlock slowResult;
