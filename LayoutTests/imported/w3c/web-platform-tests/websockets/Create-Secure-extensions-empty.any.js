e392af1e06ed4e41392ac817d81b85f56e1a58d3
Youenn Fablet <youenn@apple.com>
2019-06-13 18:35:26 +0000

    Import WPT websockets tests https://bugs.webkit.org/show_bug.cgi?id=198774
    Reviewed by Geoffrey Garen.
    
    LayoutTests/imported/w3c:
    
    * resources/import-expectations.json:
    * resources/resource-files.json:
    * web-platform-tests/html/webappapis/dynamic-markup-insertion/opening-the-input-stream/abort.sub.window-expected.txt:
    Rebasing since we now have some websockets specific handlers.
    * web-platform-tests/websockets: Added.
    
    LayoutTests:
    
    * platform/mac-wk1/TestExpectations:
    * TestExpectations:
    * tests-options.json:
    
    
    Canonical link: https://commits.webkit.org/212808@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@246406 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/imported/w3c/web-platform-tests/websockets/Create-Secure-extensions-empty.any.js
@@ -0,0 +1,21 @@
+// META: timeout=long
+// META: script=websocket.sub.js
+
+var testOpen = async_test("Create Secure WebSocket - wsocket.extensions should be set to '' after connection is established - Connection should be opened");
+var testClose = async_test("Create Secure WebSocket - wsocket.extensions should be set to '' after connection is established - Connection should be closed");
+
+var wsocket = CreateWebSocket(true, false, false);
+var isOpenCalled = false;
+
+wsocket.addEventListener('open', testOpen.step_func(function(evt) {
+  assert_equals(wsocket.extensions, "", "extensions should be empty");
+  wsocket.close();
+  isOpenCalled = true;
+  testOpen.done();
+}), true);
+
+wsocket.addEventListener('close', testClose.step_func(function(evt) {
+  assert_true(isOpenCalled, "WebSocket connection should be closed");
+  assert_equals(evt.wasClean, true, "wasClean should be true");
+  testClose.done();
+}), true);
