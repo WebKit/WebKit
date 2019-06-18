a4b3a5fa4c2591602e93559cdf6479a5c1c8963b
Keith Miller <keith_miller@apple.com>
2019-06-18 21:11:30 +0000

    webkit-patch should allow for a bugzilla url not just bugzilla id https://bugs.webkit.org/show_bug.cgi?id=198972
    Reviewed by Dewei Zhu.
    
    When prompting for a bugzilla id or a new title we should also
    allow for a bugzilla url.
    
    * Scripts/webkitpy/tool/steps/promptforbugortitle.py:
    (PromptForBugOrTitle.run):
    
    
    Canonical link: https://commits.webkit.org/212958@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@246566 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/tool/steps/promptforbugortitle.py
+++ Tools/Scripts/webkitpy/tool/steps/promptforbugortitle.py
@@ -28,7 +28,8 @@
 
 from webkitpy.tool.steps.abstractstep import AbstractStep
 from webkitpy.tool.steps.options import Options
-
+from urlparse import urlparse
+import re
 
 class PromptForBugOrTitle(AbstractStep):
     @classmethod
@@ -41,12 +42,25 @@ class PromptForBugOrTitle(AbstractStep):
         # No need to prompt if we alrady have the bug_id.
         if state.get("bug_id"):
             return
-        user_response = self._tool.user.prompt("Please enter a bug number or a title for a new bug:\n")
-        # If the user responds with a number, we assume it's bug number.
+        user_response = self._tool.user.prompt("Please enter a bug number/bugzilla URL or a title for a new bug:\n")
+        # If the user responds with a number or a valid bugzilla URL, we assume it's bug number.
         # Otherwise we assume it's a bug subject.
         try:
             state["bug_id"] = int(user_response)
         except ValueError as TypeError:
+            parsed_url = None
+            try:
+                parsed_url = urlparse(user_response)
+            except ValueError:
+                # urlparse can throw a value error for some strings.
+                pass
+
+            if parsed_url and re.match("bugs.webkit.org", parsed_url.netloc):
+                    match = re.match("id=(?P<bug_id>\d+)", parsed_url.query)
+                    if match:
+                        state["bug_id"] = int(match.group("bug_id"))
+                        return
+
             if not self._options.non_interactive and not self._tool.user.confirm("Are you sure you want to create a new bug?", default="n"):
                 self._exit(1)
             state["bug_title"] = user_response
