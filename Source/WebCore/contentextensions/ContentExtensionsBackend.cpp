f08ea66d55ddcca0afc0fdf1fc5fe5d8a3af5398
Chris Dumez <cdumez@apple.com>
2019-06-24 22:22:42 +0000

    Pages using Google's anti-flicker optimization may take ~5 seconds to do initial paint https://bugs.webkit.org/show_bug.cgi?id=199173 <rdar://problem/45968770>
    Reviewed by Geoffrey Garen.
    
    Source/WebCore:
    
    Pages using Google's anti-flicker optimization [1] take ~5 seconds to do initial paint when
    analytics.js load is blocked by a content blocker.
    
    To address the issue, this patch introduces a quirk behind an experimental feature flag that
    calls window.dataLayer.hide.end() on the page when the load of https://www.google-analytics.com/analytics.js
    is blocked by a content blocker. Note that this is more robust than dropping the 'async-hide'
    class from document.documentElement since the class name is customizable by the caller.
    A message is logged in the console when the quirk causes window.dataLayer.hide.end() to get called
    early.
    
    [1] https://developers.google.com/optimize/
    
    * contentextensions/ContentExtensionsBackend.cpp:
    (WebCore::ContentExtensions::ContentExtensionsBackend::processContentRuleListsForLoad):
    * page/Settings.yaml:
    
    Source/WebKit:
    
    Add experimental feature for the quirk.
    
    * Shared/WebPreferences.yaml:
    
    
    Canonical link: https://commits.webkit.org/213138@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@246764 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/contentextensions/ContentExtensionsBackend.cpp
+++ Source/WebCore/contentextensions/ContentExtensionsBackend.cpp
@@ -43,6 +43,9 @@
 #include "FrameLoaderClient.h"
 #include "Page.h"
 #include "ResourceLoadInfo.h"
+#include "ScriptController.h"
+#include "ScriptSourceCode.h"
+#include "Settings.h"
 #include <wtf/URL.h>
 #include "UserContentController.h"
 #include <wtf/NeverDestroyed.h>
@@ -231,6 +234,13 @@ ContentRuleListResults ContentExtensionsBackend::processContentRuleListsForLoad(
         }
         if (results.summary.blockedLoad)
             currentDocument->addConsoleMessage(MessageSource::ContentBlocker, MessageLevel::Info, makeString("Content blocker prevented frame displaying ", mainDocumentURL.string(), " from loading a resource from ", url.string()));
+        
+            // Quirk for content-blocker interference with Google's anti-flicker optimization (rdar://problem/45968770).
+            // https://developers.google.com/optimize/
+            if (currentDocument->settings().googleAntiFlickerOptimizationQuirkEnabled() && url == "https://www.google-analytics.com/analytics.js"_str) {
+                if (auto* frame = currentDocument->frame())
+                    frame->script().evaluate(ScriptSourceCode { "try { window.dataLayer.hide.end(); console.log('Called window.dataLayer.hide.end() in frame ' + document.URL + ' because the content blocker blocked the load of the https://www.google-analytics.com/analytics.js script'); } catch (e) { }"_s });
+            }
     }
 
     return results;
