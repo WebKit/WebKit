f8fc049207905bfcd3ec7c320cc02af51f6080e4
Mike Gorse <mgorse@suse.com>
2019-07-01 08:53:06 +0000

    Merge r246710 - webkitgtk 2.24.2 fails to build w/gstreamer 1.12.5 https://bugs.webkit.org/show_bug.cgi?id=198080
    Patch by Mike Gorse <mgorse@suse.com> on 2019-06-22
    Reviewed by Philippe Normand.
    
    No new tests (build fix only).
    
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp:
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.h:
    Move includes of gst/gl/gl.h and epoxy/gl.h into
    MediaPlayerPrivateGStreamerBase.h.

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.h
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.h
@@ -37,6 +37,25 @@
 #include <wtf/RunLoop.h>
 #include <wtf/WeakPtr.h>
 
+#if USE(LIBEPOXY)
+// Include the <epoxy/gl.h> header before <gst/gl/gl.h>.
+#include <epoxy/gl.h>
+
+// Workaround build issue with RPi userland GLESv2 headers and libepoxy <https://webkit.org/b/185639>
+#if !GST_CHECK_VERSION(1, 14, 0)
+#include <gst/gl/gstglconfig.h>
+#if defined(GST_GL_HAVE_WINDOW_DISPMANX) && GST_GL_HAVE_WINDOW_DISPMANX
+#define __gl2_h_
+#undef GST_GL_HAVE_GLSYNC
+#define GST_GL_HAVE_GLSYNC 1
+#endif
+#endif // !GST_CHECK_VERSION(1, 14, 0)
+#endif // USE(LIBEPOXY)
+
+#define GST_USE_UNSTABLE_API
+#include <gst/gl/gl.h>
+#undef GST_USE_UNSTABLE_API
+
 #if USE(TEXTURE_MAPPER_GL)
 #include "TextureMapperGL.h"
 #if USE(NICOSIA)
