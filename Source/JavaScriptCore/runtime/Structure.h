f7dcadb330720ead8be0a4d381ae32ad4dad691d
Kocsen Chung <kocsen_chung@apple.com>
2019-07-02 19:51:27 +0000

    Revert r246801. rdar://problem/52505041
    Canonical link: https://commits.webkit.org/207701.543@safari-607-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@247068 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/Structure.h
+++ Source/JavaScriptCore/runtime/Structure.h
@@ -138,13 +138,11 @@ public:
         return &vm.structureSpace;
     }
 
-    JS_EXPORT_PRIVATE static bool isValidPrototype(JSValue);
-
 protected:
     void finishCreation(VM& vm)
     {
         Base::finishCreation(vm);
-        ASSERT(m_prototype.get().isEmpty() || isValidPrototype(m_prototype.get()));
+        ASSERT(m_prototype.get().isEmpty() || m_prototype.isObject() || m_prototype.isNull());
     }
 
     void finishCreation(VM& vm, const Structure* previous)
@@ -787,5 +785,17 @@ private:
 
     uint32_t m_propertyHash;
 };
+
+// We deliberately put Structure::create here in Structure.h instead of StructureInlines.h, because
+// it is used everywhere. This is so we don't have to hunt down all the places where we would need
+// to #include StructureInlines.h otherwise.
+inline Structure* Structure::create(VM& vm, JSGlobalObject* globalObject, JSValue prototype, const TypeInfo& typeInfo, const ClassInfo* classInfo, IndexingType indexingType, unsigned inlineCapacity)
+{
+    ASSERT(vm.structureStructure);
+    ASSERT(classInfo);
+    Structure* structure = new (NotNull, allocateCell<Structure>(vm.heap)) Structure(vm, globalObject, prototype, typeInfo, classInfo, indexingType, inlineCapacity);
+    structure->finishCreation(vm);
+    return structure;
+}
 
 } // namespace JSC
