abd25eeb178e488e1d774624b73543587396997a
Philippe Normand <pnormand@igalia.com>
2019-07-04 07:30:17 +0000

    Unreviewed, GTK a11y tests fix after r247071
    Patch by Philippe Normand <pnormand@igalia.com> on 2019-07-01
    
    * WebKitTestRunner/InjectedBundle/atk/AccessibilityUIElementAtk.cpp:
    (WTR::AccessibilityUIElement::popupValue const):
    
    Canonical link: https://commits.webkit.org/213401@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@247135 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/WebKitTestRunner/InjectedBundle/atk/AccessibilityUIElementAtk.cpp
+++ Tools/WebKitTestRunner/InjectedBundle/atk/AccessibilityUIElementAtk.cpp
@@ -2326,6 +2326,12 @@ bool AccessibilityUIElement::replaceTextInRange(JSStringRef, int, int)
     return false;
 }
 
+JSRetainPtr<JSStringRef> AccessibilityUIElement::popupValue() const
+{
+    notImplemented();
+    return nullptr;
+}
+
 } // namespace WTR
 
 #endif // HAVE(ACCESSIBILITY)
