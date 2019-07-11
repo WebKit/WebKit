b7e8334c08ec5d177853567afa32ecd7d39bff2a
Chris Dumez <cdumez@apple.com>
2019-07-11 20:52:06 +0000

    Pages using Google Tag Manager Google's anti-flicker optimization may take ~5 seconds to do initial paint https://bugs.webkit.org/show_bug.cgi?id=199704 <rdar://problem/52966461>
    Reviewed by Geoffrey Garen.
    
    Apply the same fix as r246764 for when "https://www.googletagmanager.com/gtm.js" load is blocked by a content
    extension to avoid delaying initial paint by ~5 seconds. This was tested on https://www.formula1.com/en.html.
    
    * contentextensions/ContentExtensionsBackend.cpp:
    (WebCore::ContentExtensions::ContentExtensionsBackend::processContentRuleListsForLoad):
    
    Canonical link: https://commits.webkit.org/213596@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@247361 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/contentextensions/ContentExtensionsBackend.cpp
+++ Source/WebCore/contentextensions/ContentExtensionsBackend.cpp
@@ -237,7 +237,9 @@ ContentRuleListResults ContentExtensionsBackend::processContentRuleListsForLoad(
         
             // Quirk for content-blocker interference with Google's anti-flicker optimization (rdar://problem/45968770).
             // https://developers.google.com/optimize/
-            if (currentDocument->settings().googleAntiFlickerOptimizationQuirkEnabled() && url == "https://www.google-analytics.com/analytics.js"_str) {
+            if (currentDocument->settings().googleAntiFlickerOptimizationQuirkEnabled()
+                && ((equalLettersIgnoringASCIICase(url.host(), "www.google-analytics.com") && equalLettersIgnoringASCIICase(url.path(), "/analytics.js"))
+                    || (equalLettersIgnoringASCIICase(url.host(), "www.googletagmanager.com") && equalLettersIgnoringASCIICase(url.path(), "/gtm.js")))) {
                 if (auto* frame = currentDocument->frame())
                     frame->script().evaluate(ScriptSourceCode { "try { window.dataLayer.hide.end(); console.log('Called window.dataLayer.hide.end() in frame ' + document.URL + ' because the content blocker blocked the load of the https://www.google-analytics.com/analytics.js script'); } catch (e) { }"_s });
             }
