3f4853855b0784dd76b78ede599221ccfb524c09
Rob Buis <rbuis@igalia.com>
2019-07-15 20:23:11 +0000

    MathML WPT test for RadicalDegreeBottomRaisePercent fails https://bugs.webkit.org/show_bug.cgi?id=183631
    Patch by Rob Buis <rbuis@igalia.com> on 2019-07-15
    Reviewed by Frédéric Wang.
    
    LayoutTests/imported/w3c:
    
    Adjust test expectation.
    
    * web-platform-tests/mathml/presentation-markup/radicals/root-parameters-1-expected.txt:
    
    Source/WebCore:
    
    When stretching the radical operator also take thickness
    and radical gap into account:
    https://mathml-refresh.github.io/mathml-core/#radicals-msqrt-mroot
    
    Test: imported/w3c/web-platform-tests/mathml/presentation-markup/radicals/root-parameters-1.html
    
    * rendering/mathml/RenderMathMLRoot.cpp:
    (WebCore::RenderMathMLRoot::layoutBlock):
    
    LayoutTests:
    
    Adjust test expectations.
    
    * platform/ios/mathml/presentation/roots-expected.txt:
    * platform/mac/mathml/presentation/roots-expected.txt:
    * platform/win/mathml/presentation/roots-expected.txt:
    
    Canonical link: https://commits.webkit.org/213674@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@247441 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/mathml/RenderMathMLRoot.cpp
+++ Source/WebCore/rendering/mathml/RenderMathMLRoot.cpp
@@ -217,7 +217,7 @@ void RenderMathMLRoot::layoutBlock(bool relayoutChildren, LayoutUnit)
 
     // Stretch the radical operator to cover the base height.
     // We can then determine the metrics of the radical operator + the base.
-    m_radicalOperator.stretchTo(style(), baseAscent + baseDescent);
+    m_radicalOperator.stretchTo(style(), baseAscent + baseDescent + vertical.verticalGap + vertical.ruleThickness);
     LayoutUnit radicalOperatorHeight = m_radicalOperator.ascent() + m_radicalOperator.descent();
     LayoutUnit indexBottomRaise { vertical.degreeBottomRaisePercent * radicalOperatorHeight };
     LayoutUnit radicalAscent = baseAscent + vertical.verticalGap + vertical.ruleThickness + vertical.extraAscender;
