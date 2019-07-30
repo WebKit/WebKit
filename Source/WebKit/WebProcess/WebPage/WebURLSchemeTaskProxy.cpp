8993a153412e93eb9cf054d6efbc9ca7a2a44f05
Brady Eidson <beidson@apple.com>
2019-07-30 03:55:29 +0000

    Cherry-pick r247787. rdar://problem/53647890
        Crash in WebContent process with custom schemes.
        <rdar://problem/52968793> and https://bugs.webkit.org/show_bug.cgi?id=200062
    
        Reviewed by Andy Estes.
    
        Almost everywhere in WebURLSchemeTaskProxy where we call processNextPendingTask() we first protect the task with a ref.
        But not in didReceiveData.
        So lets do that.
    
        * WebProcess/WebPage/WebURLSchemeTaskProxy.cpp:
        (WebKit::WebURLSchemeTaskProxy::didReceiveData): Protect this.
    
        Canonical link: https://commits.webkit.org/213917@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@247787 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/213666.116@safari-608-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-608-branch@247977 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/WebPage/WebURLSchemeTaskProxy.cpp
+++ Source/WebKit/WebProcess/WebPage/WebURLSchemeTaskProxy.cpp
@@ -125,6 +125,7 @@ void WebURLSchemeTaskProxy::didReceiveData(size_t size, const uint8_t* data)
         return;
     }
 
+    auto protectedThis = makeRef(*this);
     m_coreLoader->didReceiveData(reinterpret_cast<const char*>(data), size, 0, DataPayloadType::DataPayloadBytes);
     processNextPendingTask();
 }
