8c052e7cdb3180edfd861a10d4ec178313eb8d5e
Konstantin Tokarev <annulen@yandex.ru>
2019-08-02 08:58:35 +0000

    Remove unused constructor declaration from FontPlatformData https://bugs.webkit.org/show_bug.cgi?id=200371
    Reviewed by Carlos Garcia Campos.
    
    It is not implemented by any port.
    
    * platform/graphics/FontPlatformData.h:
    
    
    Canonical link: https://commits.webkit.org/214150@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@248151 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/FontPlatformData.h
+++ Source/WebCore/platform/graphics/FontPlatformData.h
@@ -80,7 +80,6 @@ public:
     FontPlatformData(WTF::HashTableDeletedValueType);
     FontPlatformData();
 
-    FontPlatformData(const FontDescription&, const AtomString& family);
     FontPlatformData(float size, bool syntheticBold, bool syntheticOblique, FontOrientation = FontOrientation::Horizontal, FontWidthVariant = FontWidthVariant::RegularWidth, TextRenderingMode = TextRenderingMode::AutoTextRendering);
 
 #if PLATFORM(COCOA)
