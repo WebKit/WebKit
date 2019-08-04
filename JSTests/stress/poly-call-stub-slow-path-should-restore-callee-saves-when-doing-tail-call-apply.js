b24a5a0d6312f71461775871da730f80a8157cd1
Yusuke Suzuki <ysuzuki@apple.com>
2019-08-04 03:23:23 +0000

    Merge r246372 - [JSC] Polymorphic call stub's slow path should restore callee saves before performing tail call https://bugs.webkit.org/show_bug.cgi?id=198770
    Reviewed by Saam Barati.
    
    JSTests:
    
    * stress/poly-call-stub-slow-path-should-restore-callee-saves-when-doing-tail-call.js: Added.
    (test):
    
    Source/JavaScriptCore:
    
    Polymorphic call stub is a bit specially patched in JS call site. Typical JS call site for tail calls
    are the following.
    
        if (callee == patchableCallee) {
            restore callee saves for tail call
            prepare for tail call
            jump to the target function
        }
        restore callee saves for slow path
        call the slow path function
    
    And linking patches patchableCallee, target function, and slow path function. But polymorphic call stub
    patches the above `if` statement with the jump to the stub.
    
        jump to the polymorphic call stub
    
    This is because polymorphic call stub wants to use CallFrameShuffler to get scratch registers. As a result,
    "restore callee saves for tail call" thing needs to be done in the polymorphic call stubs. While it is
    correctly done for the major cases, we have `slowPath` skips, and that path missed restoring callee saves.
    This skip happens if the callee is non JSCell or non JS function, so typically, InternalFunction is handled
    in that path.
    
    This patch does that skips after restoring callee saves.
    
    * bytecode/CallLinkInfo.cpp:
    (JSC::CallLinkInfo::CallLinkInfo):
    * bytecode/CallLinkInfo.h:
    (JSC::CallLinkInfo::setUpCall):
    (JSC::CallLinkInfo::calleeGPR):
    (JSC::CallLinkInfo::setCalleeGPR): Deleted.
    * jit/Repatch.cpp:
    (JSC::revertCall):
    (JSC::linkVirtualFor):
    (JSC::linkPolymorphicCall):
    * jit/Repatch.h:
    * jit/ThunkGenerators.cpp:
    (JSC::virtualThunkFor):

---
+++ JSTests/stress/poly-call-stub-slow-path-should-restore-callee-saves-when-doing-tail-call-apply.js
@@ -0,0 +1,17 @@
+//@ runDefault("--useConcurrentJIT=0")
+
+noInline(Function.prototype.apply);
+
+function test()
+{
+    for (var i = 0; i < 1e4; ++i) {
+        try {
+            Function.prototype.apply.call(WeakMap.bind());
+        } catch { }
+        try {
+            Function.prototype.apply.call(WeakMap);
+        } catch { }
+    }
+}
+
+test();
