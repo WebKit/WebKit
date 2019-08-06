74df30a3fe92e50766a5a652665402ab5c3761be
Keith Rollin <krollin@apple.com>
2019-08-06 21:57:52 +0000

    Enable XCBuild for `make` and `build-webkit` builds https://bugs.webkit.org/show_bug.cgi?id=200476 <rdar://problem/50771065>
    Reviewed by Jonathan Bedard.
    
    All projects have now been tested under XCBuild and successfully
    build. Enable the use of XCBuild when performing builds with `make` or
    `build-webkit` and when using a sufficient version of Xcode. We set
    this version to Xcode 11 since Xcode 10 -- even though it supports
    XCBuild -- does not support a build facility needed to build WebKit
    (Bug 197072, Bug 197116, Bug 197340).
    
    Do not enable XCBuild in the Xcode projects themselves since these
    projects may need to be used under older versions of Xcode and there's
    no way to make those project changes sensitive to the version of Xcode
    being used. This means that builds performed in the Xcode IDE will not
    use XCBuild.
    
    * Scripts/webkitdirs.pm:
    (canUseXCBuild):
    
    
    Canonical link: https://commits.webkit.org/214250@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@248317 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -903,6 +903,10 @@ sub XcodeStaticAnalyzerOption()
 
 sub canUseXCBuild()
 {
+    if (`xcodebuild -version | grep "Build version"` =~ /Build version (\d+)([a-zA-Z])(\d+)([a-zA-Z]?)/) {
+        return $1 >= 11;
+    }
+
     return 0;
 }
 
