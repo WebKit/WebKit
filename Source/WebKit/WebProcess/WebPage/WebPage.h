5326bbe61304695795c8df343b5b5e5623b2a637
Timothy Hatcher <timothy@apple.com>
2019-08-08 19:04:36 +0000

    Set WKWebView opaque based on drawsBackground in PageConfiguration. https://bugs.webkit.org/show_bug.cgi?id=200528
    Reviewed by Tim Horton.
    
    * UIProcess/API/Cocoa/WKWebView.mm:
    (-[WKWebView _initializeWithConfiguration:]): Set self.opaque = NO when !self.opaque || !pageConfiguration->drawsBackground().
    It is almost impossible to have !self.opaque be NO at this point, since we are still inside initWithFrame:. A subclass could
    override opaque and return NO, but checking pageConfiguration's drawsBackground is a good alternative.
    * WebProcess/WebPage/WebPage.h: Remove unused m_drawsBackground member.
    
    
    Canonical link: https://commits.webkit.org/214319@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@248436 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/WebPage/WebPage.h
+++ Source/WebKit/WebProcess/WebPage/WebPage.h
@@ -1631,7 +1631,6 @@ private:
     HashMap<uint64_t, RefPtr<WebCore::TextCheckingRequest>> m_pendingTextCheckingRequestMap;
 
     bool m_useFixedLayout { false };
-    bool m_drawsBackground { true };
 
     WebCore::Color m_underlayColor;
 
