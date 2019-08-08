ba8d151e4ef085c853864a3b6560b2024607feb6
Brady Eidson <beidson@apple.com>
2019-08-08 15:54:37 +0000

    Do not allow navigations of frames about to get replaced by the result of evaluating javascript: URLs <rdar://problem/53788893> and https://bugs.webkit.org/show_bug.cgi?id=198786
    Reviewed by Geoff Garen.
    
    Source/WebCore:
    
    Covered by API Test
    
    Add a "willReplaceWithResultOfExecutingJavascriptURL" flag which is respected inside FrameLoader::isNavigationAllowed
    
    * bindings/js/ScriptController.cpp:
    (WebCore::ScriptController::executeIfJavaScriptURL):
    * bindings/js/ScriptController.h:
    (WebCore::ScriptController::willReplaceWithResultOfExecutingJavascriptURL const):
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::isNavigationAllowed const):
    
    Tools:
    
    * TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
    * TestWebKitAPI/Tests/mac/JavascriptURLNavigation.mm: Added.
    
    Canonical link: https://commits.webkit.org/214308@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@248410 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/FrameLoader.cpp
+++ Source/WebCore/loader/FrameLoader.cpp
@@ -1324,7 +1324,7 @@ static void applyShouldOpenExternalURLsPolicyToNewDocumentLoader(Frame& frame, D
 
 bool FrameLoader::isNavigationAllowed() const
 {
-    return m_pageDismissalEventBeingDispatched == PageDismissalType::None && NavigationDisabler::isNavigationAllowed(m_frame);
+    return m_pageDismissalEventBeingDispatched == PageDismissalType::None && !m_frame.script().willReplaceWithResultOfExecutingJavascriptURL() && NavigationDisabler::isNavigationAllowed(m_frame);
 }
 
 bool FrameLoader::isStopLoadingAllowed() const
