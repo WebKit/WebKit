01608fbc89abdd6524ba90e618d872fe995b822a
Chris Dumez <cdumez@apple.com>
2019-08-13 20:58:19 +0000

    Fix potential thread safety issue under StorageManager::getSessionStorageOrigins() https://bugs.webkit.org/show_bug.cgi?id=200684
    Reviewed by Geoffrey Garen.
    
    Fix potential thread safety issue under StorageManager::getSessionStorageOrigins(). The origins are being
    passed from the background queue to the main thread without isolated copy.
    
    * NetworkProcess/WebStorage/StorageManager.cpp:
    (WebKit::StorageManager::getSessionStorageOrigins):
    
    
    Canonical link: https://commits.webkit.org/214428@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@248633 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/NetworkProcess/WebStorage/StorageManager.cpp
+++ Source/WebKit/NetworkProcess/WebStorage/StorageManager.cpp
@@ -180,7 +180,7 @@ void StorageManager::getSessionStorageOrigins(Function<void(HashSet<WebCore::Sec
 
         for (const auto& sessionStorageNamespace : m_sessionStorageNamespaces.values()) {
             for (auto& origin : sessionStorageNamespace->origins())
-                origins.add(origin);
+                origins.add(crossThreadCopy(origin));
         }
 
         RunLoop::main().dispatch([origins = WTFMove(origins), completionHandler = WTFMove(completionHandler)]() mutable {
