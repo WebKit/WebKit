bc2a05b2a420546b0c15161541ddc99e090223bb
Youenn Fablet <youenn@apple.com>
2019-08-14 17:10:08 +0000

    Reenable Update ContentFiltering.LazilyLoadPlatformFrameworks test for NetworkExtension https://bugs.webkit.org/show_bug.cgi?id=200706
    Reviewed by Alex Christensen.
    
    Libwebrtc is now soft-linked so we can reenable this test.
    
    * TestWebKitAPI/Tests/WebKitCocoa/ContentFiltering.mm:
    (-[LazilyLoadPlatformFrameworksController expectParentalControlsLoaded:networkExtensionLoaded:]):
    
    
    Canonical link: https://commits.webkit.org/214459@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@248673 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/WebKitCocoa/ContentFiltering.mm
+++ Tools/TestWebKitAPI/Tests/WebKitCocoa/ContentFiltering.mm
@@ -361,12 +361,7 @@ TEST(ContentFiltering, LoadAlternateAfterFinishedAddingDataWK2)
         EXPECT_EQ(static_cast<bool>(parentalControlsShouldBeLoaded), static_cast<bool>(parentalControlsLoaded));
 #endif
 #if HAVE(NETWORK_EXTENSION)
-        // FIXME: Libwebrtc is linking on some configurations to VideoProcessing framework which brings up NetworkExtension framework.
-        // Disable this test until we find a more robust test or libwebrtc weak link VideoProcessing framework.
-        // https://bugs.webkit.org/show_bug.cgi?id=180713
-#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MAX_ALLOWED < 101300
         EXPECT_EQ(static_cast<bool>(networkExtensionShouldBeLoaded), static_cast<bool>(networkExtensionLoaded));
-#endif
 #endif // HAVE(NETWORK_EXTENSION)
         isDone = true;
     }];
