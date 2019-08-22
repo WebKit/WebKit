9f57890bc3609cd04a85e4b5b83104a2e1af4603
Jer Noble <jer.noble@apple.com>
2019-08-22 15:45:33 +0000

    Cherry-pick r248997. rdar://problem/54579627
        Unreviewed build fix; add a 'final' declaration on shouldOverridePauseDuringRouteChange().
    
        * Modules/mediastream/MediaStream.h:
    
        Canonical link: https://commits.webkit.org/214735@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@248997 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/213666.287@safari-608-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-608-branch@249010 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/mediastream/MediaStream.h
+++ Source/WebCore/Modules/mediastream/MediaStream.h
@@ -165,7 +165,7 @@ private:
     bool canProduceAudio() const final;
     Document* hostingDocument() const final { return document(); }
     bool processingUserGestureForMedia() const final;
-    bool shouldOverridePauseDuringRouteChange() const { return true; }
+    bool shouldOverridePauseDuringRouteChange() const final { return true; }
 
     // ActiveDOMObject API.
     void stop() final;
