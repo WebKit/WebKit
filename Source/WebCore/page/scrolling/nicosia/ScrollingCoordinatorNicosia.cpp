a8716336d355d5ec6e79c7da5a5d29d714373813
Žan Doberšek <zdobersek@igalia.com>
2019-08-29 06:11:51 +0000

    [Nicosia] Polish ScrollingCoordinator implementation https://bugs.webkit.org/show_bug.cgi?id=201130
    Reviewed by Carlos Garcia Campos.
    
    These changes primarily mirror the Mac implementation of this class.
    
    * page/scrolling/nicosia/ScrollingCoordinatorNicosia.cpp:
    (WebCore::ScrollingCoordinatorNicosia::pageDestroyed):
    Upon releasing the ThreadedScrollingTree, it should also be invalidated
    on the scrolling thread.
    (WebCore::ScrollingCoordinatorNicosia::commitTreeState):
    Invoke willCommitTree() at the beginning of this method. Additionally
    the pending-commit-count is incremented for the ThreadedScrollingTree
    object.
    
    
    Canonical link: https://commits.webkit.org/214935@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@249241 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/scrolling/nicosia/ScrollingCoordinatorNicosia.cpp
+++ Source/WebCore/page/scrolling/nicosia/ScrollingCoordinatorNicosia.cpp
@@ -58,7 +58,9 @@ void ScrollingCoordinatorNicosia::pageDestroyed()
 
     m_scrollingStateTreeCommitterTimer.stop();
 
-    releaseScrollingTree();
+    // Invalidating the scrolling tree will break the reference cycle between the ScrollingCoordinator and ScrollingTree objects.
+    RefPtr<ThreadedScrollingTree> scrollingTree = static_pointer_cast<ThreadedScrollingTree>(releaseScrollingTree());
+    ScrollingThread::dispatch([scrollingTree] { scrollingTree->invalidate(); });
 }
 
 void ScrollingCoordinatorNicosia::commitTreeStateIfNeeded()
@@ -80,10 +82,13 @@ void ScrollingCoordinatorNicosia::scheduleTreeStateCommit()
 
 void ScrollingCoordinatorNicosia::commitTreeState()
 {
+    willCommitTree();
+
     if (!scrollingStateTree()->hasChangedProperties())
         return;
 
     RefPtr<ThreadedScrollingTree> threadedScrollingTree = downcast<ThreadedScrollingTree>(scrollingTree());
+    threadedScrollingTree->incrementPendingCommitCount();
 
     auto treeState = scrollingStateTree()->commit(LayerRepresentation::PlatformLayerRepresentation);
     ScrollingThread::dispatch([threadedScrollingTree, treeState = WTFMove(treeState)]() mutable {
