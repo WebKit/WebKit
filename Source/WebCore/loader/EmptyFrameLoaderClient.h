bf3693b8e0cae78f5e3d93bd7b03dfd8a8de819e
Chris Dumez <cdumez@apple.com>
2019-09-05 02:36:31 +0000

    Unreviewed build fix after r249501.
    r249501 seems to have exposed the fact that EmptyFrameLoaderClient.h is missing a #pragma once.
    
    * loader/EmptyFrameLoaderClient.h:
    
    Canonical link: https://commits.webkit.org/215120@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@249515 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/EmptyFrameLoaderClient.h
+++ Source/WebCore/loader/EmptyFrameLoaderClient.h
@@ -23,6 +23,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+ #pragma once
+
 #include "FrameLoaderClient.h"
 #include <WebCore/ResourceError.h>
 #include <pal/SessionID.h>
