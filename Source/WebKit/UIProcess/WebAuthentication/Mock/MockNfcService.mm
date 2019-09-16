7377d5a24452f2ff3dcc8e6599ddee2436a320f1
David Kilzer <ddkilzer@webkit.org>
2019-09-16 18:29:49 +0000

    [WebAuthn] REGRESSION (r249059): Leak of WKMockNFTag objects and WKMockNFTag instance variables <https://webkit.org/b/201813>
    Reviewed by Darin Adler.
    
    * UIProcess/WebAuthentication/Mock/MockNfcService.mm:
    (-[WKMockNFTag dealloc]): Add method and release instance
    variables to fix leaks.
    (WebKit::MockNfcService::detectTags const): Use adoptNS() to fix
    leaks of WKMockNFTag objects.
    
    
    Canonical link: https://commits.webkit.org/215461@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@249907 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/WebAuthentication/Mock/MockNfcService.mm
+++ Source/WebKit/UIProcess/WebAuthentication/Mock/MockNfcService.mm
@@ -32,6 +32,7 @@
 #import "NfcConnection.h"
 #import <WebCore/FidoConstants.h>
 #import <wtf/BlockPtr.h>
+#import <wtf/RetainPtr.h>
 #import <wtf/RunLoop.h>
 #import <wtf/Vector.h>
 
@@ -70,6 +71,18 @@
     if ((self = [super init]))
         _type = tag.type;
     return self;
+}
+
+- (void)dealloc
+{
+    [_tagID release];
+    _tagID = nil;
+    [_AppData release];
+    _AppData = nil;
+    [_UID release];
+    _UID = nil;
+
+    [super dealloc];
 }
 
 - (NSString*)description
@@ -187,12 +200,12 @@ void MockNfcService::detectTags() const
     auto callback = makeBlockPtr([configuration = m_configuration] {
         auto tags = adoptNS([[NSMutableArray alloc] init]);
         if (configuration.nfc->error == MockNfc::Error::WrongTagType || configuration.nfc->multipleTags)
-            [tags addObject:[[WKMockNFTag alloc] initWithType:NFTagTypeUnknown]];
+            [tags addObject:adoptNS([[WKMockNFTag alloc] initWithType:NFTagTypeUnknown]).get()];
         else
-            [tags addObject:[[WKMockNFTag alloc] initWithType:NFTagTypeGeneric4A]];
+            [tags addObject:adoptNS([[WKMockNFTag alloc] initWithType:NFTagTypeGeneric4A]).get()];
 
         if (configuration.nfc->multipleTags)
-            [tags addObject:[[WKMockNFTag alloc] initWithType:NFTagTypeGeneric4A]];
+            [tags addObject:adoptNS([[WKMockNFTag alloc] initWithType:NFTagTypeGeneric4A]).get()];
 
         [globalNFReaderSessionDelegate readerSession:nil didDetectTags:tags.get()];
     });
