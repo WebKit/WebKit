c6c5b630a036a97a0d4260c2dc5c62bdede62466
Antoine Quint <graouts@apple.com>
2019-09-17 22:29:36 +0000

    Cherry-pick r249757. rdar://problem/55427273
        REGRESSION: Scrubbing on ted.com does not work well
        https://bugs.webkit.org/show_bug.cgi?id=201635
        <rdar://problem/51463649>
    
        Patch by Antoine Quint <graouts@apple.com> on 2019-09-11
        Reviewed by Dean Jackson.
    
        This website uses custom media controls that simply don't work well on iOS when the User-Agent string is the desktop one, so we default to the mobile UA.
    
        * UIProcess/ios/WebPageProxyIOS.mm:
        (WebKit::desktopClassBrowsingRecommendedForRequest):
    
        Canonical link: https://commits.webkit.org/215324@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@249757 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/213666.358@safari-608-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-608-branch@249995 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/ios/WebPageProxyIOS.mm
+++ Source/WebKit/UIProcess/ios/WebPageProxyIOS.mm
@@ -1339,6 +1339,9 @@ static bool desktopClassBrowsingRecommendedForRequest(const WebCore::ResourceReq
     if (equalLettersIgnoringASCIICase(host, "trello.com") || host.endsWithIgnoringASCIICase(".trello.com"))
         return false;
 
+    if (equalLettersIgnoringASCIICase(host, "ted.com") || host.endsWithIgnoringASCIICase(".ted.com"))
+        return false;
+
     if (host.containsIgnoringASCIICase("hsbc.")) {
         if (equalLettersIgnoringASCIICase(host, "hsbc.com.au") || host.endsWithIgnoringASCIICase(".hsbc.com.au"))
             return false;
