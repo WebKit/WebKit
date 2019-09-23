c5c59f970cc522f75ff5dfeadddb9acda6d2a12e
Caio Araujo Neponoceno de Lima <ticaiolima@gmail.com>
2019-09-23 20:42:14 +0000

    Skip stress/regexp-unicode-surrogate-pair-increment-should-involve-length-check.js into ARMv7 and MIPS https://bugs.webkit.org/show_bug.cgi?id=202113
    Unreviewed test gardening, skipped test in ARMv7 and MIPS.
    
    It is going to be fixed in
    https://bugs.webkit.org/show_bug.cgi?id=202041
    
    
    * stress/regexp-unicode-surrogate-pair-increment-should-involve-length-check.js:
    
    
    Canonical link: https://commits.webkit.org/215717@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@250250 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/regexp-unicode-surrogate-pair-increment-should-involve-length-check.js
+++ JSTests/stress/regexp-unicode-surrogate-pair-increment-should-involve-length-check.js
@@ -1,3 +1,4 @@
+//@ skip if ["arm", "mips"].include?($architecture)
 // This test checks for proper incrementing around / over individual surrogates and surrogate pairs.
 // This test should run without crashing.
 
