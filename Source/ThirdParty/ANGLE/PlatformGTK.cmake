28b8d0af45b235662e82af3eef00c3d529199f2c
Adrian Perez de Castro <aperez@igalia.com>
2019-09-24 14:58:49 +0000

    [GTK] ANGLE's eglplatform.h is build broken with -DENABLE_X11_PLATFORM=OFF https://bugs.webkit.org/show_bug.cgi?id=198621
    Reviewed by Carlos Garcia Campos.
    
    * PlatformGTK.cmake: Define WL_EGL_PLATFORM to prefer Wayland type
    definitions with ENABLE_WAYLAND_TARGET=ON and ENABLE_X11_TARGET=OFF
    to avoid ANGLE's eglplatform.h from trying to include the X11 headers.
    
    
    Canonical link: https://commits.webkit.org/215747@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@250298 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/ThirdParty/ANGLE/PlatformGTK.cmake
+++ Source/ThirdParty/ANGLE/PlatformGTK.cmake
@@ -1,2 +1,15 @@
 # Enable GLSL compiler output.
 list(APPEND ANGLE_DEFINITIONS ANGLE_ENABLE_GLSL)
+
+# NOTE: When both Wayland and X11 are enabled, ANGLE_USE_X11 will be
+# defined and the X11 type definitions will be used for code involving
+# both. That works because types for both Wayland and X11 have the same
+# sizes and the code in WebKit casts the values to the proper types as
+# needed.
+
+if (ENABLE_WAYLAND_TARGET AND NOT ENABLE_X11_TARGET)
+    list(APPEND ANGLE_DEFINITIONS WL_EGL_PLATFORM)
+elseif (NOT ENABLE_X11_TARGET)
+    # Allow building ANGLE on platforms which may not provide X11 headers.
+    list(APPEND ANGLE_DEFINITIONS USE_SYSTEM_EGL)
+endif ()
