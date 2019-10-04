925c524389e1a0e0f5cedcadc6ecc9b5de85f42f
Ryosuke Niwa <rniwa@webkit.org>
2019-10-04 07:37:50 +0000

    A newly inserted element doesn't get assigned to a named slot if slot assignments had already happened https://bugs.webkit.org/show_bug.cgi?id=199733
    Reviewed by Antti Koivisto.
    
    Source/WebCore:
    
    The bug was caused by addSlotElementByName not invalidating slot assignments when a new slot element
    is inserted after corresponding elements with the slot content attribute had already been inserted.
    
    Fixed the bug by always invalidating the slot assignment when a new slot element is inserted.
    
    Tests: fast/shadow-dom/add-slotted-element-after-assignment-1.html
           fast/shadow-dom/add-slotted-element-after-assignment-2.html
           fast/shadow-dom/add-slotted-element-after-assignment-3.html
    
    * dom/SlotAssignment.cpp:
    (WebCore::SlotAssignment::addSlotElementByName):
    
    LayoutTests:
    
    Added ref tests.
    
    * fast/shadow-dom/add-slotted-element-after-assignment-1-expected.html: Added.
    * fast/shadow-dom/add-slotted-element-after-assignment-1.html: Added.
    * fast/shadow-dom/add-slotted-element-after-assignment-2-expected.html: Added.
    * fast/shadow-dom/add-slotted-element-after-assignment-2.html: Added.
    * fast/shadow-dom/add-slotted-element-after-assignment-3-expected.html: Added.
    * fast/shadow-dom/add-slotted-element-after-assignment-3.html: Added.
    
    
    Canonical link: https://commits.webkit.org/216046@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@250709 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/SlotAssignment.cpp
+++ Source/WebCore/dom/SlotAssignment.cpp
@@ -116,10 +116,7 @@ void SlotAssignment::addSlotElementByName(const AtomString& name, HTMLSlotElemen
 
     auto& slotName = slotNameFromAttributeValue(name);
     auto addResult = m_slots.ensure(slotName, [&] {
-        // Unlike named slots, assignSlots doesn't collect nodes assigned to the default slot
-        // to avoid always having a vector of all child nodes of a shadow host.
-        if (slotName == defaultSlotName())
-            m_slotAssignmentsIsValid = false;
+        m_slotAssignmentsIsValid = false;
         return makeUnique<Slot>();
     });
     auto& slot = *addResult.iterator->value;
