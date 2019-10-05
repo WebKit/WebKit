36683e24e9ef8eab0205baa2f7fa881ed09d4ac4
Ryosuke Niwa <rniwa@webkit.org>
2019-10-05 02:52:42 +0000

    Build fix for macOS Catalina.
    * wtf/spi/darwin/SandboxSPI.h:
    
    
    Canonical link: https://commits.webkit.org/216092@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@250763 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/spi/darwin/SandboxSPI.h
+++ Source/WTF/wtf/spi/darwin/SandboxSPI.h
@@ -64,6 +64,9 @@ char *sandbox_extension_issue_file(const char *extension_class, const char *path
 char *sandbox_extension_issue_generic(const char *extension_class, uint32_t flags);
 char *sandbox_extension_issue_mach_to_process_by_pid(const char *extension_class, const char *name, uint32_t flags, pid_t);
 char *sandbox_extension_issue_file_to_process_by_pid(const char *extension_class, const char *path, uint32_t flags, pid_t);
+#if (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101400) || (PLATFORM(IOS_FAMILY) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 120000)
+char *sandbox_extension_issue_file_to_process(const char *extension_class, const char *path, uint32_t flags, audit_token_t);
+#endif
 int sandbox_check(pid_t, const char *operation, enum sandbox_filter_type, ...);
 int sandbox_check_by_audit_token(audit_token_t, const char *operation, enum sandbox_filter_type, ...);
 int sandbox_container_path_for_pid(pid_t, char *buffer, size_t bufsize);
