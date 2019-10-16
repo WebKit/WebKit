6f81f17e990984a05c65d897ee4d61d4958c2b79
Dean Jackson <dino@apple.com>
2019-10-16 22:49:31 +0000

    Add Swift modulemap for WebKit Private APIs https://bugs.webkit.org/show_bug.cgi?id=203059
    Reviewed by Tim Horton.
    
    Provide a Swift modulemap for WebKit_Private, and
    ensure WKPreferencesPrivate.h is exposed.
    
    Renamed OSX.modulemap to macOS.modulemap while here.
    
    * Configurations/WebKit.xcconfig:
    * Modules/iOS.private.modulemap: Added.
    * Modules/macOS.modulemap: Renamed from Source/WebKit/Modules/OSX.modulemap.
    * Modules/macOS.private.modulemap: Added.
    * Shared/API/Cocoa/WebKitPrivate.h: Include WKPreferencesPrivate.h.
    
    Canonical link: https://commits.webkit.org/216482@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@251215 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/API/Cocoa/WebKitPrivate.h
+++ Source/WebKit/Shared/API/Cocoa/WebKitPrivate.h
@@ -25,6 +25,7 @@
 
 #import <WebKit/WKHistoryDelegatePrivate.h>
 #import <WebKit/WKNavigationPrivate.h>
+#import <WebKit/WKPreferencesPrivate.h>
 #import <WebKit/WKProcessPoolPrivate.h>
 #import <WebKit/WKUIDelegatePrivate.h>
 #import <WebKit/WKWebViewConfigurationPrivate.h>
