7136998893266eff689d69055b971ab60f61d9c9
Antoine Quint <graouts@apple.com>
2019-10-17 16:24:49 +0000

    [Web Animations] Enable the Web Animations JavaScript API by default https://bugs.webkit.org/show_bug.cgi?id=203075
    Patch by Antoine Quint <graouts@apple.com> on 2019-10-17
    Reviewed by Antti Koivisto.
    
    Source/WebCore:
    
    * page/RuntimeEnabledFeatures.h:
    
    Source/WebKit:
    
    * Shared/WebPreferences.yaml:
    
    Canonical link: https://commits.webkit.org/216503@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@251237 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/RuntimeEnabledFeatures.h
+++ Source/WebCore/page/RuntimeEnabledFeatures.h
@@ -405,7 +405,7 @@ private:
     bool m_isCustomPasteboardDataEnabled { false };
     bool m_isWebShareEnabled { false };
     bool m_inputEventsEnabled { true };
-    bool m_areWebAnimationsEnabled { false };
+    bool m_areWebAnimationsEnabled { true };
     bool m_isWebAnimationsCSSIntegrationEnabled { false };
     bool m_isImageBitmapOffscreenCanvasEnabled { true };
     bool m_isCacheAPIEnabled { false };
