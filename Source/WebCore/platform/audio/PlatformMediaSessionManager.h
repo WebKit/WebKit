99b744f22611c47040c74241b5e532ce9857e65d
Jer Noble <jer.noble@apple.com>
2019-10-20 17:32:51 +0000

    Cherry-pick r250694. rdar://problem/56061131
        [iOS] WebContent process can be interrupted during suspension; loses "Now Playing" status
            https://bugs.webkit.org/show_bug.cgi?id=202537
            <rdar://problem/55952707>
    
            Reviewed by Eric Carlson.
    
            Always deactivate the AVAudioSession when the last playing PlatformAudioSession ends playback and the application is in the background.
    
            * platform/audio/PlatformMediaSessionManager.cpp:
            (WebCore::PlatformMediaSessionManager::removeSession):
            (WebCore::PlatformMediaSessionManager::processWillSuspend):
            (WebCore::PlatformMediaSessionManager::maybeDeactivateAudioSession):
            * platform/audio/PlatformMediaSessionManager.h:
            (WebCore::PlatformMediaSessionManager::isApplicationInBackground const):
            * platform/audio/ios/MediaSessionManagerIOS.h:
            * platform/audio/ios/MediaSessionManagerIOS.mm:
            (WebCore::MediaSessionManageriOS::sessionWillEndPlayback):
    
        Canonical link: https://commits.webkit.org/216035@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@250694 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/213666.481@safari-608-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-608-branch@251338 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/audio/PlatformMediaSessionManager.h
+++ Source/WebCore/platform/audio/PlatformMediaSessionManager.h
@@ -149,6 +149,11 @@ protected:
 
     AudioHardwareListener* audioHardwareListener() { return m_audioHardwareListener.get(); }
 
+    bool isApplicationInBackground() const { return m_isApplicationInBackground; }
+#if USE(AUDIO_SESSION)
+    void maybeDeactivateAudioSession();
+#endif
+
 #if !RELEASE_LOG_DISABLED
     const Logger& logger() const final { return m_logger; }
     const void* logIdentifier() const final { return nullptr; }
