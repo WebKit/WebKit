08d866cd083bf3e6bb081724d01c00d4b1ec0631
Devin Rousso <drousso@apple.com>
2019-10-21 22:05:06 +0000

    Web Inspector: REGRESSION(r251227): Uncaught Exception: undefined is not an object (evaluating 'agent.enable') https://bugs.webkit.org/show_bug.cgi?id=203208
    Reviewed by Joseph Pecoraro.
    
    * UserInterface/Controllers/AppController.js:
    (WI.AppController.prototype.activateExtraDomains):
    
    
    Canonical link: https://commits.webkit.org/216624@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@251386 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Controllers/AppController.js
+++ Source/WebInspectorUI/UserInterface/Controllers/AppController.js
@@ -72,17 +72,18 @@ WI.AppController = class AppController extends WI.AppControllerBase
         console.assert(WI.sharedApp.debuggableType === WI.DebuggableType.JavaScript);
         console.assert(WI.targets.length === 1);
 
-        for (let domain of domains) {
-            let agent = InspectorBackend.activateDomain(domain);
+        let target = WI.mainTarget;
 
-            for (let target of WI.targets)
-                target.activateExtraDomain(domain);
+        for (let domain of domains) {
+            InspectorBackend.activateDomain(domain);
+
+            target.activateExtraDomain(domain);
 
             let manager = WI.managers.find((manager) => manager.domains && manager.domains.includes(domain));
             if (manager)
                 manager.activateExtraDomain(domain);
-            else if (agent.enable)
-                agent.enable();
+            else if (target.hasCommand(domain + ".enable"))
+                target._agents[domain].enable();
         }
 
         // FIXME: all code within WI.activateExtraDomains should be distributed elsewhere.
