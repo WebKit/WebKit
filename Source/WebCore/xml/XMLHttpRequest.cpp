8cff57c67d981d52547946af8841eb5421f81d3a
Youenn Fablet <youenn@apple.com>
2019-10-24 16:24:00 +0000

    XMLHttpRequest.responseXML url should be the HTTP response URL https://bugs.webkit.org/show_bug.cgi?id=203289
    Reviewed by Chris Dumez.
    
    LayoutTests/imported/w3c:
    
    * web-platform-tests/service-workers/service-worker/xhr-response-url.https-expected.txt:
    * web-platform-tests/xhr/responsexml-document-properties-expected.txt:
    
    Source/WebCore:
    
    Follow https://xhr.spec.whatwg.org/#document-response step 9.
    Covered by rebased tests.
    
    * xml/XMLHttpRequest.cpp:
    
    
    Canonical link: https://commits.webkit.org/216774@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@251542 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/xml/XMLHttpRequest.cpp
+++ Source/WebCore/xml/XMLHttpRequest.cpp
@@ -184,9 +184,9 @@ ExceptionOr<Document*> XMLHttpRequest::responseXML()
             m_responseDocument = nullptr;
         } else {
             if (isHTML)
-                m_responseDocument = HTMLDocument::create(nullptr, m_url);
+                m_responseDocument = HTMLDocument::create(nullptr, m_response.url());
             else
-                m_responseDocument = XMLDocument::create(nullptr, m_url);
+                m_responseDocument = XMLDocument::create(nullptr, m_response.url());
             m_responseDocument->overrideLastModified(m_response.lastModified());
             m_responseDocument->setContent(m_responseBuilder.toStringPreserveCapacity());
             m_responseDocument->setContextDocument(context);
