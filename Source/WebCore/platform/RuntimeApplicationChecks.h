c96a5679518a42c5e7d0384124b57b5c6024a6cb
Alan Coon <alancoon@apple.com>
2019-11-05 22:08:03 +0000

    Revert r252040. rdar://problem/56887517
    Canonical link: https://commits.webkit.org/213666.543@safari-608.4.9.0-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-608.4.9.0-branch@252082 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/RuntimeApplicationChecks.h
+++ Source/WebCore/platform/RuntimeApplicationChecks.h
@@ -67,7 +67,6 @@ bool isSolidStateNetworksDownloader();
 WEBCORE_EXPORT bool isVersions();
 WEBCORE_EXPORT bool isHRBlock();
 WEBCORE_EXPORT bool isIAdProducer();
-WEBCORE_EXPORT bool isEpsonSoftwareUpdater();
 
 } // MacApplication
 
