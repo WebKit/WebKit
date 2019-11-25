WebKit/WebKit

commit 02ac040d01ad20fd23ac111b811bc7275a27664a

[GTK][WPE] RemoteInspector: use sockets instead of DBus https://bugs.webkit.org/show_bug.cgi?id=204503
Reviewed by Žan Doberšek.

Source/JavaScriptCore:

It turns out DBus is event slower than expected. Using GSockets API we can simplify the code and make it a lot
more efficient. This will drastically reduce the time to run WebDriver tests in the bots.

* inspector/remote/RemoteInspector.h:
* inspector/remote/glib/RemoteInspectorGlib.cpp:
(Inspector::RemoteInspector::start):
(Inspector::RemoteInspector::stopInternal):
(Inspector::RemoteInspector::setupConnection):
(Inspector::RemoteInspector::pushListingsNow):
(Inspector::RemoteInspector::pushListingsSoon):
(Inspector::RemoteInspector::sendAutomaticInspectionCandidateMessage):
(Inspector::RemoteInspector::sendMessageToRemote):
* inspector/remote/glib/RemoteInspectorServer.cpp:
(Inspector::RemoteInspectorServer::~RemoteInspectorServer):
(Inspector::RemoteInspectorServer::start):
(Inspector::RemoteInspectorServer::incomingConnectionCallback):
(Inspector::RemoteInspectorServer::incomingConnection):
(Inspector::RemoteInspectorServer::setTargetList):
(Inspector::RemoteInspectorServer::setupInspectorClient):
(Inspector::RemoteInspectorServer::setup):
(Inspector::RemoteInspectorServer::close):
(Inspector::RemoteInspectorServer::connectionDidClose):
(Inspector::RemoteInspectorServer::sendMessageToBackend):
(Inspector::RemoteInspectorServer::sendMessageToFrontend):
(Inspector::RemoteInspectorServer::startAutomationSession):
* inspector/remote/glib/RemoteInspectorServer.h:
(Inspector::RemoteInspectorServer::isRunning const):

Source/WebDriver:

Use GSockets API instead of DBus.

* SessionHost.cpp:
(WebDriver::SessionHost::sendCommandToBackend):
* SessionHost.h:
* glib/SessionHostGlib.cpp:
(WebDriver::SessionHost::~SessionHost):
(WebDriver::SessionHost::isConnected const):
(WebDriver::ConnectToBrowserAsyncData::ConnectToBrowserAsyncData):
(WebDriver::SessionHost::launchBrowser):
(WebDriver::SessionHost::connectToBrowser):

Source/WTF/wtf/glib/SocketConnection.cpp
@@ -1,0 +1,222 @@
+/*
+ * Copyright (C) 2019 Igalia, S.L.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this library; see the file COPYING.LIB.  If not, write to
+ *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
+
+#include "config.h"
+#include "SocketConnection.h"
+
+#include <cstring>
+#include <gio/gio.h>
+#include <wtf/RunLoop.h>
+
+namespace WTF {
+
+static const unsigned defaultBufferSize = 4096;
+
+SocketConnection::SocketConnection(GRefPtr<GSocketConnection>&& connection, const MessageHandlers& messageHandlers, gpointer userData)
+    : m_connection(WTFMove(connection))
+    , m_messageHandlers(messageHandlers)
+    , m_userData(userData)
+{
+    m_readBuffer.reserveInitialCapacity(defaultBufferSize);
+    m_writeBuffer.reserveInitialCapacity(defaultBufferSize);
+
+    auto* socket = g_socket_connection_get_socket(m_connection.get());
+    g_socket_set_blocking(socket, FALSE);
+    m_readMonitor.start(socket, G_IO_IN, RunLoop::current(), [this, protectedThis = makeRef(*this)](GIOCondition condition) -> gboolean {
+        if (isClosed())
+            return G_SOURCE_REMOVE;
+
+        if (condition & G_IO_HUP || condition & G_IO_ERR || condition & G_IO_NVAL) {
+            didClose();
+            return G_SOURCE_REMOVE;
+        }
+
+        ASSERT(condition & G_IO_IN);
+        return read();
+    });
+}
+
+SocketConnection::~SocketConnection()
+{
+}
+
+bool SocketConnection::read()
+{
+    while (true) {
+        size_t previousBufferSize = m_readBuffer.size();
+        if (m_readBuffer.capacity() - previousBufferSize <= 0)
+            m_readBuffer.reserveCapacity(m_readBuffer.capacity() + defaultBufferSize);
+        m_readBuffer.grow(m_readBuffer.capacity());
+        GUniqueOutPtr<GError> error;
+        auto bytesRead = g_socket_receive(g_socket_connection_get_socket(m_connection.get()), m_readBuffer.data() + previousBufferSize, m_readBuffer.size() - previousBufferSize, nullptr, &error.outPtr());
+        if (bytesRead == -1) {
+            if (g_error_matches(error.get(), G_IO_ERROR, G_IO_ERROR_WOULD_BLOCK)) {
+                m_readBuffer.shrink(previousBufferSize);
+                break;
+            }
+
+            g_warning("Error reading from socket connection: %s\n", error->message);
+            didClose();
+            return G_SOURCE_REMOVE;
+        }
+
+        if (!bytesRead) {
+            didClose();
+            return G_SOURCE_REMOVE;
+        }
+
+        m_readBuffer.shrink(previousBufferSize + bytesRead);
+
+        while (readMessage()) { }
+        if (isClosed())
+            return G_SOURCE_REMOVE;
+    }
+    return G_SOURCE_CONTINUE;
+}
+
+bool SocketConnection::readMessage()
+{
+    if (m_readBuffer.size() < sizeof(size_t))
+        return false;
+
+    auto* messageData = m_readBuffer.data();
+    size_t bodySize;
+    memcpy(&bodySize, messageData, sizeof(size_t));
+    messageData += sizeof(size_t);
+    auto messageSize = sizeof(size_t) + bodySize;
+    if (m_readBuffer.size() < messageSize)
+        return false;
+
+    auto messageNameLength = strlen(messageData) + 1;
+    if (m_readBuffer.size() < messageNameLength) {
+        ASSERT_NOT_REACHED();
+        return false;
+    }
+
+    const auto it = m_messageHandlers.find(messageData);
+    if (it != m_messageHandlers.end()) {
+        messageData += messageNameLength;
+        GRefPtr<GVariant> parameters;
+        if (!it->value.first.isNull()) {
+            GUniquePtr<GVariantType> variantType(g_variant_type_new(it->value.first.data()));
+            parameters = g_variant_new_from_data(variantType.get(), messageData, bodySize - messageNameLength, FALSE, nullptr, nullptr);
+        }
+        it->value.second(*this, parameters.get(), m_userData);
+        if (isClosed())
+            return false;
+    }
+
+    if (m_readBuffer.size() > messageSize) {
+        std::memmove(m_readBuffer.data(), m_readBuffer.data() + messageSize, m_readBuffer.size() - messageSize);
+        m_readBuffer.shrink(m_readBuffer.size() - messageSize);
+    } else
+        m_readBuffer.shrink(0);
+
+    if (m_readBuffer.size() < defaultBufferSize)
+        m_readBuffer.shrinkCapacity(defaultBufferSize);
+
+    return true;
+}
+
+void SocketConnection::sendMessage(const char* messageName, GVariant* parameters)
+{
+    GRefPtr<GVariant> adoptedParameters = parameters;
+    size_t parametersSize = parameters ? g_variant_get_size(parameters) : 0;
+    auto messageNameLength = strlen(messageName) + 1;
+    size_t bodySize = messageNameLength + parametersSize;
+    size_t previousBufferSize = m_writeBuffer.size();
+    m_writeBuffer.grow(previousBufferSize + sizeof(size_t) + bodySize);
+
+    auto* messageData = m_writeBuffer.data() + previousBufferSize;
+    memcpy(messageData, &bodySize, sizeof(size_t));
+    messageData += sizeof(size_t);
+    memcpy(messageData, messageName, messageNameLength);
+    messageData += messageNameLength;
+    if (parameters)
+        memcpy(messageData, g_variant_get_data(parameters), parametersSize);
+
+    write();
+}
+
+void SocketConnection::write()
+{
+    if (isClosed())
+        return;
+
+    GUniqueOutPtr<GError> error;
+    auto bytesWritten = g_socket_send(g_socket_connection_get_socket(m_connection.get()), m_writeBuffer.data(), m_writeBuffer.size(), nullptr, &error.outPtr());
+    if (bytesWritten == -1) {
+        if (g_error_matches(error.get(), G_IO_ERROR, G_IO_ERROR_WOULD_BLOCK)) {
+            waitForSocketWritability();
+            return;
+        }
+
+        g_warning("Error sending message on socket connection: %s\n", error->message);
+        didClose();
+        return;
+    }
+
+    if (m_writeBuffer.size() > static_cast<size_t>(bytesWritten)) {
+        std::memmove(m_writeBuffer.data(), m_writeBuffer.data() + bytesWritten, m_writeBuffer.size() - bytesWritten);
+        m_writeBuffer.shrink(m_writeBuffer.size() - bytesWritten);
+    } else
+        m_writeBuffer.shrink(0);
+
+    if (m_writeBuffer.size() < defaultBufferSize)
+        m_writeBuffer.shrinkCapacity(defaultBufferSize);
+
+    if (!m_writeBuffer.isEmpty())
+        waitForSocketWritability();
+}
+
+void SocketConnection::waitForSocketWritability()
+{
+    if (m_writeMonitor.isActive())
+        return;
+
+    m_writeMonitor.start(g_socket_connection_get_socket(m_connection.get()), G_IO_OUT, RunLoop::current(), [this, protectedThis = makeRef(*this)] (GIOCondition condition) -> gboolean {
+        if (condition & G_IO_OUT) {
+            // We can't stop the monitor from this lambda, because stop destroys the lambda.
+            RunLoop::current().dispatch([this, protectedThis = protectedThis.copyRef()] {
+                m_writeMonitor.stop();
+                write();
+            });
+        }
+        return G_SOURCE_REMOVE;
+    });
+}
+
+void SocketConnection::close()
+{
+    m_readMonitor.stop();
+    m_writeMonitor.stop();
+    m_connection = nullptr;
+}
+
+void SocketConnection::didClose()
+{
+    if (isClosed())
+        return;
+
+    close();
+    ASSERT(m_messageHandlers.contains("DidClose"));
+    m_messageHandlers.get("DidClose").second(*this, nullptr, m_userData);
+}
+
+} // namespace WTF

Source/JavaScriptCore/inspector/remote/RemoteInspector.h
@@ -47,10 +47,9 @@ typedef RetainPtr<NSDictionary> TargetListing;
 
 #if USE(GLIB)
 #include <wtf/glib/GRefPtr.h>
+#include <wtf/glib/SocketConnection.h>
 typedef GRefPtr<GVariant> TargetListing;
 typedef struct _GCancellable GCancellable;
-typedef struct _GDBusConnection GDBusConnection;
-typedef struct _GDBusInterfaceVTable GDBusInterfaceVTable;
 #endif
 
 #if USE(INSPECTOR_SOCKET_SERVER)
@@ -178,8 +177,8 @@ private:
     void setupXPCConnectionIfNeeded();
 #endif
 #if USE(GLIB)
-    void setupConnection(GRefPtr<GDBusConnection>&&);
-    static const GDBusInterfaceVTable s_interfaceVTable;
+    void setupConnection(Ref<SocketConnection>&&);
+    static const SocketConnection::MessageHandlers s_messageHandlers;
 
     void receivedGetTargetListMessage();
     void receivedSetupMessage(TargetID);
@@ -252,7 +251,7 @@ private:
     RefPtr<RemoteInspectorXPCConnection> m_relayConnection;
 #endif
 #if USE(GLIB)
-    GRefPtr<GDBusConnection> m_dbusConnection;
+    RefPtr<SocketConnection> m_socketConnection;
     GRefPtr<GCancellable> m_cancellable;
 #endif
 

Source/JavaScriptCore/inspector/remote/glib/RemoteInspectorGlib.cpp
@@ -36,11 +36,6 @@
 #include <wtf/RunLoop.h>
 #include <wtf/glib/GUniquePtr.h>
 
-#define REMOTE_INSPECTOR_DBUS_INTERFACE "org.webkit.RemoteInspector"
-#define REMOTE_INSPECTOR_DBUS_OBJECT_PATH "/org/webkit/RemoteInspector"
-#define INSPECTOR_DBUS_INTERFACE "org.webkit.Inspector"
-#define INSPECTOR_DBUS_OBJECT_PATH "/org/webkit/Inspector"
-
 namespace Inspector {
 
 RemoteInspector& RemoteInspector::singleton()
@@ -65,22 +60,16 @@ void RemoteInspector::start()
     m_enabled = true;
     m_cancellable = adoptGRef(g_cancellable_new());
 
-    GUniquePtr<char> inspectorAddress(g_strdup(g_getenv("WEBKIT_INSPECTOR_SERVER")));
-    char* portPtr = g_strrstr(inspectorAddress.get(), ":");
-    ASSERT(portPtr);
-    *portPtr = '\0';
-    portPtr++;
-    unsigned port = g_ascii_strtoull(portPtr, nullptr, 10);
-    GUniquePtr<char> dbusAddress(g_strdup_printf("tcp:host=%s,port=%u", inspectorAddress.get(), port));
-    g_dbus_connection_new_for_address(dbusAddress.get(), G_DBUS_CONNECTION_FLAGS_AUTHENTICATION_CLIENT, nullptr, m_cancellable.get(),
-        [](GObject*, GAsyncResult* result, gpointer userData) {
+    GRefPtr<GSocketClient> socketClient = adoptGRef(g_socket_client_new());
+    g_socket_client_connect_to_host_async(socketClient.get(), g_getenv("WEBKIT_INSPECTOR_SERVER"), 0, m_cancellable.get(),
+        [](GObject* client, GAsyncResult* result, gpointer userData) {
             RemoteInspector* inspector = static_cast<RemoteInspector*>(userData);
             GUniqueOutPtr<GError> error;
-            if (GRefPtr<GDBusConnection> connection = adoptGRef(g_dbus_connection_new_for_address_finish(result, &error.outPtr())))
-                inspector->setupConnection(WTFMove(connection));
+            if (GRefPtr<GSocketConnection> connection = adoptGRef(g_socket_client_connect_to_host_finish(G_SOCKET_CLIENT(client), result, &error.outPtr())))
+                inspector->setupConnection(SocketConnection::create(WTFMove(connection), s_messageHandlers, inspector));
             else if (!g_error_matches(error.get(), G_IO_ERROR, G_IO_ERROR_CANCELLED))
                 g_warning("RemoteInspector failed to connect to inspector server at: %s: %s", g_getenv("WEBKIT_INSPECTOR_SERVER"), error->message);
-    }, this);
+        }, this);
 }
 
 void RemoteInspector::stopInternal(StopSource)
@@ -100,85 +89,59 @@ void RemoteInspector::stopInternal(StopSource)
     updateHasActiveDebugSession();
 
     m_automaticInspectionPaused = false;
-    m_dbusConnection = nullptr;
+    m_socketConnection = nullptr;
 }
 
-static const char introspectionXML[] =
-    "<node>"
-    "  <interface name='" REMOTE_INSPECTOR_DBUS_INTERFACE "'>"
-    "    <method name='GetTargetList'>"
-    "    </method>"
-    "    <method name='Setup'>"
-    "      <arg type='t' name='target' direction='in'/>"
-    "    </method>"
-    "    <method name='SendMessageToTarget'>"
-    "      <arg type='t' name='target' direction='in'/>"
-    "      <arg type='s' name='message' direction='in'/>"
-    "    </method>"
-    "    <method name='FrontendDidClose'>"
-    "      <arg type='t' name='target' direction='in'/>"
-    "    </method>"
-    "  </interface>"
-    "</node>";
-
-const GDBusInterfaceVTable RemoteInspector::s_interfaceVTable = {
-    // method_call
-    [] (GDBusConnection*, const gchar* /*sender*/, const gchar* /*objectPath*/, const gchar* /*interfaceName*/, const gchar* methodName, GVariant* parameters, GDBusMethodInvocation* invocation, gpointer userData) {
-        auto* inspector = static_cast<RemoteInspector*>(userData);
-        if (!g_strcmp0(methodName, "GetTargetList")) {
-            inspector->receivedGetTargetListMessage();
-            g_dbus_method_invocation_return_value(invocation, nullptr);
-        } else if (!g_strcmp0(methodName, "Setup")) {
+const SocketConnection::MessageHandlers RemoteInspector::s_messageHandlers = {
+    { "DidClose", std::pair<CString, SocketConnection::MessageCallback> { { },
+        [](SocketConnection&, GVariant*, gpointer userData) {
+            auto& inspector = *static_cast<RemoteInspector*>(userData);
+            inspector.stop();
+        }}
+    },
+    { "GetTargetList", std::pair<CString, SocketConnection::MessageCallback> { { },
+        [](SocketConnection&, GVariant*, gpointer userData) {
+            auto& inspector = *static_cast<RemoteInspector*>(userData);
+            inspector.receivedGetTargetListMessage();
+        }}
+    },
+    { "Setup", std::pair<CString, SocketConnection::MessageCallback> { "(t)",
+        [](SocketConnection&, GVariant* parameters, gpointer userData) {
+            auto& inspector = *static_cast<RemoteInspector*>(userData);
             guint64 targetID;
             g_variant_get(parameters, "(t)", &targetID);
-            inspector->receivedSetupMessage(targetID);
-            g_dbus_method_invocation_return_value(invocation, nullptr);
-        } else if (!g_strcmp0(methodName, "SendMessageToTarget")) {
+            inspector.receivedSetupMessage(targetID);
+        }}
+    },
+    { "SendMessageToTarget", std::pair<CString, SocketConnection::MessageCallback> { "(ts)",
+        [](SocketConnection&, GVariant* parameters, gpointer userData) {
+            auto& inspector = *static_cast<RemoteInspector*>(userData);
             guint64 targetID;
             const char* message;
             g_variant_get(parameters, "(t&s)", &targetID, &message);
-            inspector->receivedDataMessage(targetID, message);
-            g_dbus_method_invocation_return_value(invocation, nullptr);
-        } else if (!g_strcmp0(methodName, "FrontendDidClose")) {
+            inspector.receivedDataMessage(targetID, message);
+        }}
+    },
+    { "FrontendDidClose", std::pair<CString, SocketConnection::MessageCallback> { "(t)",
+        [](SocketConnection&, GVariant* parameters, gpointer userData) {
+            auto& inspector = *static_cast<RemoteInspector*>(userData);
             guint64 targetID;
             g_variant_get(parameters, "(t)", &targetID);
-            inspector->receivedCloseMessage(targetID);
-            g_dbus_method_invocation_return_value(invocation, nullptr);
-        }
-    },
-    // get_property
-    nullptr,
-    // set_property
-    nullptr,
-    // padding
-    { 0 }
+            inspector.receivedCloseMessage(targetID);
+        }}
+    }
 };
 
-void RemoteInspector::setupConnection(GRefPtr<GDBusConnection>&& connection)
+void RemoteInspector::setupConnection(Ref<SocketConnection>&& connection)
 {
     LockHolder lock(m_mutex);
 
     ASSERT(connection);
-    ASSERT(!m_dbusConnection);
-    m_dbusConnection = WTFMove(connection);
+    ASSERT(!m_socketConnection);
 
-    static GDBusNodeInfo* introspectionData = nullptr;
-    if (!introspectionData)
-        introspectionData = g_dbus_node_info_new_for_xml(introspectionXML, nullptr);
-
-    g_dbus_connection_register_object(m_dbusConnection.get(), REMOTE_INSPECTOR_DBUS_OBJECT_PATH,
-        introspectionData->interfaces[0], &s_interfaceVTable, this, nullptr, nullptr);
-
+    m_socketConnection = WTFMove(connection);
     if (!m_targetMap.isEmpty())
         pushListingsSoon();
-}
-
-static void dbusConnectionCallAsyncReadyCallback(GObject* source, GAsyncResult* result, gpointer)
-{
-    GUniqueOutPtr<GError> error;
-    GRefPtr<GVariant> resultVariant = adoptGRef(g_dbus_connection_call_finish(G_DBUS_CONNECTION(source), result, &error.outPtr()));
-    if (!resultVariant && !g_error_matches(error.get(), G_IO_ERROR, G_IO_ERROR_CANCELLED))
-        g_warning("RemoteInspector failed to send DBus message: %s", error->message);
 }
 
 TargetListing RemoteInspector::listingForInspectionTarget(const RemoteInspectionTarget& target) const
@@ -205,7 +168,7 @@ TargetListing RemoteInspector::listingForAutomationTarget(const RemoteAutomation
 
 void RemoteInspector::pushListingsNow()
 {
-    if (!m_dbusConnection)
+    if (!m_socketConnection)
         return;
 
     m_pushScheduled = false;
@@ -217,15 +180,12 @@ void RemoteInspector::pushListingsNow()
         g_variant_builder_add_value(&builder, listing.get());
     g_variant_builder_close(&builder);
     g_variant_builder_add(&builder, "b", m_clientCapabilities && m_clientCapabilities->remoteAutomationAllowed);
-    g_dbus_connection_call(m_dbusConnection.get(), nullptr,
-        INSPECTOR_DBUS_OBJECT_PATH, INSPECTOR_DBUS_INTERFACE, "SetTargetList",
-        g_variant_builder_end(&builder), nullptr, G_DBUS_CALL_FLAGS_NO_AUTO_START,
-        -1, m_cancellable.get(), dbusConnectionCallAsyncReadyCallback, nullptr);
+    m_socketConnection->sendMessage("SetTargetList", g_variant_builder_end(&builder));
 }
 
 void RemoteInspector::pushListingsSoon()
 {
-    if (!m_dbusConnection)
+    if (!m_socketConnection)
         return;
 
     if (m_pushScheduled)
@@ -246,21 +206,17 @@ void RemoteInspector::sendAutomaticInspectionCandidateMessage()
     ASSERT(m_automaticInspectionEnabled);
     ASSERT(m_automaticInspectionPaused);
     ASSERT(m_automaticInspectionCandidateTargetIdentifier);
-    ASSERT(m_dbusConnection);
+    ASSERT(m_socketConnection);
     // FIXME: Implement automatic inspection.
 }
 
 void RemoteInspector::sendMessageToRemote(TargetID targetIdentifier, const String& message)
 {
     LockHolder lock(m_mutex);
-    if (!m_dbusConnection)
+    if (!m_socketConnection)
         return;
 
-    g_dbus_connection_call(m_dbusConnection.get(), nullptr,
-        INSPECTOR_DBUS_OBJECT_PATH, INSPECTOR_DBUS_INTERFACE, "SendMessageToFrontend",
-        g_variant_new("(ts)", static_cast<guint64>(targetIdentifier), message.utf8().data()),
-        nullptr, G_DBUS_CALL_FLAGS_NO_AUTO_START,
-        -1, m_cancellable.get(), dbusConnectionCallAsyncReadyCallback, nullptr);
+    m_socketConnection->sendMessage("SendMessageToFrontend", g_variant_new("(ts)", static_cast<guint64>(targetIdentifier), message.utf8().data()));
 }
 
 void RemoteInspector::receivedGetTargetListMessage()

Source/JavaScriptCore/inspector/remote/glib/RemoteInspectorServer.h
@@ -31,14 +31,10 @@
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
 #include <wtf/glib/GRefPtr.h>
+#include <wtf/glib/SocketConnection.h>
 
-typedef struct _GCancellable GCancellable;
-typedef struct _GDBusConnection GDBusConnection;
-typedef struct _GDBusInterfaceInfo GDBusInterfaceInfo;
-typedef struct _GDBusInterfaceVTable GDBusInterfaceVTable;
-typedef struct _GDBusNodeInfo GDBusNodeInfo;
-typedef struct _GDBusServer GDBusServer;
-typedef struct _GVariant GVariant;
+typedef struct _GSocketConnection GSocketConnection;
+typedef struct _GSocketService GSocketService;
 
 namespace Inspector {
 
@@ -48,37 +44,29 @@ public:
     ~RemoteInspectorServer();
 
     bool start(const char* address, unsigned port);
-    bool isRunning() const { return !!m_dbusServer; }
+    bool isRunning() const { return !!m_service; }
 
 private:
-    GDBusInterfaceInfo* interfaceInfo();
+    static gboolean incomingConnectionCallback(GSocketService*, GSocketConnection*, GObject*, RemoteInspectorServer*);
+    void incomingConnection(Ref<SocketConnection>&&);
 
-    static gboolean newConnectionCallback(GDBusServer*, GDBusConnection*, RemoteInspectorServer*);
-    static void connectionClosedCallback(GDBusConnection*, gboolean remotePeerVanished, GError*, RemoteInspectorServer*);
-    void newConnection(GDBusConnection*);
-    void connectionClosed(GDBusConnection*);
+    static const SocketConnection::MessageHandlers s_messageHandlers;
+    void connectionDidClose(SocketConnection&);
+    void setTargetList(SocketConnection&, GVariant*);
+    GVariant* setupInspectorClient(SocketConnection&, const char* clientBackendCommandsHash);
+    void setup(SocketConnection&, uint64_t connectionID, uint64_t targetID);
+    void close(SocketConnection&, uint64_t connectionID, uint64_t targetID);
+    void sendMessageToFrontend(SocketConnection&, uint64_t target, const char*);
+    void sendMessageToBackend(SocketConnection&, uint64_t connectionID, uint64_t targetID, const char*);
+    void startAutomationSession(SocketConnection&, const char* sessionID, const RemoteInspector::Client::SessionCapabilities&);
 
-    static const GDBusInterfaceVTable s_interfaceVTable;
-    void setTargetList(GDBusConnection*, GVariant*);
-    GVariant* setupInspectorClient(GDBusConnection*, const char* clientBackendCommandsHash);
-    void setup(GDBusConnection*, uint64_t connectionID, uint64_t targetID);
-    void close(GDBusConnection*, uint64_t connectionID, uint64_t targetID);
-    void clientConnectionClosed(GDBusConnection*);
-    void sendMessageToFrontend(GDBusConnection*, uint64_t target, const char*);
-    void sendMessageToBackend(GDBusConnection*, uint64_t connectionID, uint64_t targetID, const char*);
-    void startAutomationSession(GDBusConnection*, const char* sessionID, const RemoteInspector::Client::SessionCapabilities&);
-
-    static void clientConnectionClosedCallback(GDBusConnection*, gboolean remotePeerVanished, GError*, RemoteInspectorServer*);
-
-    GDBusNodeInfo* m_introspectionData { nullptr };
-    GRefPtr<GDBusServer> m_dbusServer;
-    GRefPtr<GCancellable> m_cancellable;
-    HashSet<GRefPtr<GDBusConnection>> m_connections;
-    HashMap<GDBusConnection*, uint64_t> m_remoteInspectorConnectionToIDMap;
-    HashMap<uint64_t, GDBusConnection*> m_idToRemoteInspectorConnectionMap;
-    GRefPtr<GDBusConnection> m_clientConnection;
+    GRefPtr<GSocketService> m_service;
+    HashSet<RefPtr<SocketConnection>> m_connections;
+    HashMap<SocketConnection*, uint64_t> m_remoteInspectorConnectionToIDMap;
+    HashMap<uint64_t, SocketConnection*> m_idToRemoteInspectorConnectionMap;
+    SocketConnection* m_clientConnection { nullptr };
+    SocketConnection* m_automationConnection { nullptr };
     HashSet<std::pair<uint64_t, uint64_t>> m_inspectionTargets;
-    GRefPtr<GDBusConnection> m_automationConnection;
     HashSet<std::pair<uint64_t, uint64_t>> m_automationTargets;
 };
 

Source/WTF/wtf/PlatformGTK.cmake
@@ -4,9 +4,11 @@ list(APPEND WTF_PUBLIC_HEADERS
     glib/GLibUtilities.h
     glib/GMutexLocker.h
     glib/GRefPtr.h
+    glib/GSocketMonitor.h
     glib/GTypedefs.h
     glib/GUniquePtr.h
     glib/RunLoopSourcePriority.h
+    glib/SocketConnection.h
     glib/WTFGType.h
 )
 
@@ -24,7 +26,9 @@ list(APPEND WTF_SOURCES
     glib/FileSystemGlib.cpp
     glib/GLibUtilities.cpp
     glib/GRefPtr.cpp
+    glib/GSocketMonitor.cpp
     glib/RunLoopGLib.cpp
+    glib/SocketConnection.cpp
     glib/URLGLib.cpp
 
     posix/OSAllocatorPOSIX.cpp

Source/WTF/wtf/PlatformWPE.cmake
@@ -2,9 +2,11 @@ list(APPEND WTF_PUBLIC_HEADERS
     glib/GLibUtilities.h
     glib/GMutexLocker.h
     glib/GRefPtr.h
+    glib/GSocketMonitor.h
     glib/GTypedefs.h
     glib/GUniquePtr.h
     glib/RunLoopSourcePriority.h
+    glib/SocketConnection.h
     glib/WTFGType.h
 
     linux/ProcessMemoryFootprint.h
@@ -19,7 +21,9 @@ list(APPEND WTF_SOURCES
     glib/FileSystemGlib.cpp
     glib/GLibUtilities.cpp
     glib/GRefPtr.cpp
+    glib/GSocketMonitor.cpp
     glib/RunLoopGLib.cpp
+    glib/SocketConnection.cpp
     glib/URLGLib.cpp
 
     linux/CurrentProcessMemoryStatus.cpp

Source/WTF/wtf/glib/GSocketMonitor.cpp
@@ -29,7 +29,7 @@
 #include <gio/gio.h>
 #include <wtf/glib/RunLoopSourcePriority.h>
 
-namespace IPC {
+namespace WTF {
 
 GSocketMonitor::~GSocketMonitor()
 {
@@ -43,7 +43,7 @@ gboolean GSocketMonitor::socketSourceCallback(GSocket*, GIOCondition condition, 
     return monitor->m_callback(condition);
 }
 
-void GSocketMonitor::start(GSocket* socket, GIOCondition condition, RunLoop& runLoop, Function<gboolean (GIOCondition)>&& callback)
+void GSocketMonitor::start(GSocket* socket, GIOCondition condition, RunLoop& runLoop, Function<gboolean(GIOCondition)>&& callback)
 {
     stop();
 
@@ -68,4 +68,4 @@ void GSocketMonitor::stop()
     m_callback = nullptr;
 }
 
-} // namespace IPC
+} // namespace WTF

commit 01c02e7bcf63cb007928df727418b5440a854b41

REGRESSION(r252770): [GTK][WPE] Remove inspector broken in debug builds after r252770 https://bugs.webkit.org/show_bug.cgi?id=204569
Reviewed by Žan Doberšek.

We need to call relaxAdoptionRequirement() in SocketConnection constructor because we are taking a reference for
the read monitor lambda.

* wtf/glib/SocketConnection.cpp:
(WTF::SocketConnection::SocketConnection):

Canonical link: https://commits.webkit.org/217828@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@252853 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/glib/SocketConnection.cpp
@@ -34,6 +34,8 @@ SocketConnection::SocketConnection(GRefPtr<GSocketConnection>&& connection, cons
     , m_messageHandlers(messageHandlers)
     , m_userData(userData)
 {
+    relaxAdoptionRequirement();
+
     m_readBuffer.reserveInitialCapacity(defaultBufferSize);
     m_writeBuffer.reserveInitialCapacity(defaultBufferSize);
 