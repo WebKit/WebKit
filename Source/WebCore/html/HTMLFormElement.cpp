42a9d2df0b38188cad694d1c7935153eb4bdcad0
Alan Coon <alancoon@apple.com>
2019-12-19 19:23:56 +0000

    Revert "Revert r253493. rdar://problem/58028534"
    This reverts revision r253726.
    
    Canonical link: https://commits.webkit.org/218446.9@safari-609.1.13-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609.1.13-branch@253771 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLFormElement.cpp
+++ Source/WebCore/html/HTMLFormElement.cpp
@@ -358,7 +358,11 @@ void HTMLFormElement::submit(Event* event, bool activateSubmitButton, bool proce
     auto protectedThis = makeRef(*this); // Form submission can execute arbitary JavaScript.
 
     auto shouldLockHistory = processingUserGesture ? LockHistory::No : LockHistory::Yes;
-    frame->loader().submitForm(FormSubmission::create(*this, m_attributes, event, shouldLockHistory, formSubmissionTrigger));
+    auto formSubmission = FormSubmission::create(*this, m_attributes, event, shouldLockHistory, formSubmissionTrigger);
+    if (m_plannedFormSubmission)
+        m_plannedFormSubmission->cancel();
+    m_plannedFormSubmission = makeWeakPtr(formSubmission.get());
+    frame->loader().submitForm(WTFMove(formSubmission));
 
     if (needButtonActivation && firstSuccessfulSubmitButton)
         firstSuccessfulSubmitButton->setActivatedSubmit(false);
