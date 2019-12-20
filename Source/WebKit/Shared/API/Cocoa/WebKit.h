03fb76cbf337ee16107ab2603aec81790a57b8d1
James Savage <james.savage@apple.com>
2019-12-20 20:51:47 +0000

    Include WKPDFConfiguration, WKFindConfiguration, and WKFindResult in umbrella header https://bugs.webkit.org/show_bug.cgi?id=205432 <rdar://problem/58067946>
    Patch by James Savage <james.savage@apple.com> on 2019-12-20
    Reviewed by Wenson Hsieh.
    
    * Shared/API/Cocoa/WebKit.h: Include new headers.
    
    Canonical link: https://commits.webkit.org/218720@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@253839 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/API/Cocoa/WebKit.h
+++ Source/WebKit/Shared/API/Cocoa/WebKit.h
@@ -30,6 +30,8 @@
 #import <WebKit/WKContentRuleListStore.h>
 #import <WebKit/WKContextMenuElementInfo.h>
 #import <WebKit/WKError.h>
+#import <WebKit/WKFindConfiguration.h>
+#import <WebKit/WKFindResult.h>
 #import <WebKit/WKFoundation.h>
 #import <WebKit/WKFrameInfo.h>
 #import <WebKit/WKHTTPCookieStore.h>
@@ -38,6 +40,7 @@
 #import <WebKit/WKNavigationDelegate.h>
 #import <WebKit/WKNavigationResponse.h>
 #import <WebKit/WKOpenPanelParameters.h>
+#import <WebKit/WKPDFConfiguration.h>
 #import <WebKit/WKPreferences.h>
 #import <WebKit/WKPreviewActionItem.h>
 #import <WebKit/WKPreviewActionItemIdentifiers.h>
