2fcb3969dac044b13d07cb336029cd7034897945
Rob Buis <rbuis@igalia.com>
2020-01-04 07:57:23 +0000

    Make text track loading set same-origin fallback flag https://bugs.webkit.org/show_bug.cgi?id=205744
    Patch by Rob Buis <rbuis@igalia.com> on 2020-01-03
    Reviewed by Darin Adler.
    
    LayoutTests/imported/w3c:
    
    Adjust test result to new behavior for text track loading.
    
    * web-platform-tests/html/semantics/embedded-content/media-elements/track/track-element/cloneNode-expected.txt:
    * web-platform-tests/html/semantics/embedded-content/media-elements/track/track-element/track-data-url-expected.txt:
    * web-platform-tests/service-workers/service-worker/webvtt-cross-origin.https-expected.txt:
    
    Source/WebCore:
    
    Make text track loading set same-origin fallback flag,
    which changes text track loading to be same-origin
    when the crossorigin attribute is not specified.
    
    The new behavior matches that of Chrome and Firefox.
    
    [1] https://html.spec.whatwg.org/multipage/media.html#sourcing-out-of-band-text-tracks:create-a-potential-cors-request
    
    Tests: http/tests/security/text-track-crossorigin.html
    
    * loader/TextTrackLoader.cpp:
    (WebCore::TextTrackLoader::load):
    
    LayoutTests:
    
    Adjusts test to same-origin fallback behavior for text track
    loading by using CORS.
    
    * http/tests/security/contentSecurityPolicy/resources/track.php: Added.
    * http/tests/security/contentSecurityPolicy/track-redirect-allowed.html:
    * http/tests/security/contentSecurityPolicy/track-redirect-allowed2.html:
    * http/tests/security/contentSecurityPolicy/track-redirect-blocked.html:
    * http/tests/security/text-track-crossorigin-expected.txt:
    * http/tests/security/text-track-crossorigin.html:
    
    Canonical link: https://commits.webkit.org/218906@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@254031 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ LayoutTests/http/tests/security/contentSecurityPolicy/resources/track.php
@@ -0,0 +1,9 @@
+<?php
+header("content-type: text/vtt");
+header("Access-Control-Allow-Origin: *");
+?>
+WEBVTT
+
+1
+00:00:00.000 --> 00:00:01.000
+Sample
--- Source/WebCore/loader/TextTrackLoader.cpp
+++ Source/WebCore/loader/TextTrackLoader.cpp
@@ -152,6 +152,7 @@ bool TextTrackLoader::load(const URL& url, HTMLTrackElement& element)
     Document& document = downcast<Document>(*m_scriptExecutionContext);
 
     ResourceLoaderOptions options = CachedResourceLoader::defaultCachedResourceOptions();
+    options.sameOriginDataURLFlag = SameOriginDataURLFlag::Set;
     options.contentSecurityPolicyImposition = element.isInUserAgentShadowTree() ? ContentSecurityPolicyImposition::SkipPolicyCheck : ContentSecurityPolicyImposition::DoPolicyCheck;
 
     ResourceRequest resourceRequest(document.completeURL(url));
@@ -159,7 +160,7 @@ bool TextTrackLoader::load(const URL& url, HTMLTrackElement& element)
     if (auto mediaElement = element.mediaElement())
         resourceRequest.setInspectorInitiatorNodeIdentifier(InspectorInstrumentation::identifierForNode(*mediaElement));
 
-    auto cueRequest = createPotentialAccessControlRequest(WTFMove(resourceRequest), WTFMove(options), document, element.mediaElementCrossOriginAttribute());
+    auto cueRequest = createPotentialAccessControlRequest(WTFMove(resourceRequest), WTFMove(options), document, element.mediaElementCrossOriginAttribute(), SameOriginFlag::Yes);
     m_resource = document.cachedResourceLoader().requestTextTrack(WTFMove(cueRequest)).value_or(nullptr);
     if (!m_resource)
         return false;
