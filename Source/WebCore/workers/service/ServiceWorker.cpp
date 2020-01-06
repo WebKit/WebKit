59a2baf2184985b82b07070426a4f2db52473545
Alan Coon <alancoon@apple.com>
2020-01-06 19:54:45 +0000

    Protect ServiceWorker::postMessage from a null execState https://bugs.webkit.org/show_bug.cgi?id=205394 <rdar://problem/57392221>
    Reviewed by Chris Dumez.
    
    Crash logs indicate null pointer crashes.
    We should return early in that case.
    
    * workers/service/ServiceWorker.cpp:
    (WebCore::ServiceWorker::postMessage):
    
    Canonical link: https://commits.webkit.org/213666.575@safari-608-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-608-branch@254073 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/workers/service/ServiceWorker.cpp
+++ Source/WebCore/workers/service/ServiceWorker.cpp
@@ -102,6 +102,8 @@ ExceptionOr<void> ServiceWorker::postMessage(ScriptExecutionContext& context, JS
 
     auto* execState = context.execState();
     ASSERT(execState);
+    if (!execState)
+        return Exception { InvalidStateError };
 
     Vector<RefPtr<MessagePort>> ports;
     auto messageData = SerializedScriptValue::create(*execState, messageValue, WTFMove(transfer), ports, SerializationContext::WorkerPostMessage);
