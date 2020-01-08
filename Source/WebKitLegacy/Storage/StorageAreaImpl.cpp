893d72acb9d0d888ad3d3a099681d91004507088
Sihui Liu <sihui_liu@apple.com>
2020-01-08 00:17:34 +0000

    REGRESSION (r248734): different threads write m_storageMap of StorageAreaImpl at the same time https://bugs.webkit.org/show_bug.cgi?id=205764 <rdar://problem/58179425>
    Reviewed by Maciej Stachowiak.
    
    In StorageAreaImpl, we avoid modifying m_storageMap from different threads at the same time by blocking main
    thread access to it until the writes(importing items) of storage thread is done.
    
    In r248734 we introduced a new case where the main thread could modify m_storageMap for session change, but we
    didn't add the wait there.
    
    * Storage/StorageAreaImpl.cpp:
    (WebKit::StorageAreaImpl::importItems):
    (WebKit::StorageAreaImpl::sessionChanged):
    
    
    Canonical link: https://commits.webkit.org/219033@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@254169 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKitLegacy/Storage/StorageAreaImpl.cpp
+++ Source/WebKitLegacy/Storage/StorageAreaImpl.cpp
@@ -194,6 +194,7 @@ bool StorageAreaImpl::contains(const String& key)
 void StorageAreaImpl::importItems(HashMap<String, String>&& items)
 {
     ASSERT(!m_isShutdown);
+    ASSERT(!isMainThread());
 
     m_storageMap->importItems(WTFMove(items));
 }
@@ -292,6 +293,9 @@ void StorageAreaImpl::dispatchStorageEvent(const String& key, const String& oldV
 void StorageAreaImpl::sessionChanged(bool isNewSessionPersistent)
 {
     ASSERT(isMainThread());
+
+    // If import is not completed, background storage thread may be modifying m_storageMap.
+    blockUntilImportComplete();
 
     unsigned quota = m_storageMap->quota();
     m_storageMap = StorageMap::create(quota);
