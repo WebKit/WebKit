34162d3a08b0ef70f66fe25e35d661ab865320b5
Dean Jackson <dino@apple.com>
2020-01-15 17:14:33 +0000

    Cherry-pick r254551. rdar://problem/58508705
        Build ANGLE as a dynamic library
        https://bugs.webkit.org/show_bug.cgi?id=204708
        rdar://57349384
    
        Rolling this out for the 2nd time.
    
        Source/ThirdParty/ANGLE:
    
        - it caused issues with the shared dyld cache, because the
        cache doesn't know to include the libary until it already
        exists in the build
        - probably related to the above, we saw some performance
        regressions directly related to this change
    
        * ANGLE.xcodeproj/project.pbxproj:
        * Configurations/ANGLE.xcconfig:
        * Configurations/Base.xcconfig:
        * Configurations/DebugRelease.xcconfig:
        * include/CMakeLists.txt:
        * include/GLSLANG/ShaderLang.h:
        * include/GLSLANG/ShaderVars.h:
        * src/libANGLE/renderer/gl/cgl/DisplayCGL.mm:
        (rx::DisplayCGL::isValidNativeWindow const):
        * src/libANGLE/renderer/gl/cgl/WindowSurfaceCGL.mm:
        (rx::WindowSurfaceCGL::WindowSurfaceCGL):
        (rx::WindowSurfaceCGL::~WindowSurfaceCGL):
        * src/libANGLE/renderer/gl/eagl/DisplayEAGL.mm:
        (rx::DisplayEAGL::terminate):
        (rx::DisplayEAGL::isValidNativeWindow const):
        (rx::WorkerContextEAGL::~WorkerContextEAGL):
        * src/libANGLE/renderer/gl/eagl/WindowSurfaceEAGL.mm:
        (rx::WindowSurfaceEAGL::WindowSurfaceEAGL):
        (rx::WindowSurfaceEAGL::~WindowSurfaceEAGL):
    
        Source/WebCore:
    
        * Configurations/WebCore.xcconfig:
        * Configurations/WebCoreTestSupport.xcconfig:
        * WebCore.xcodeproj/project.pbxproj:
        * platform/graphics/ANGLEWebKitBridge.cpp:
        (WebCore::ANGLEWebKitBridge::ANGLEWebKitBridge):
        (WebCore::ANGLEWebKitBridge::cleanupCompilers):
        (WebCore::ANGLEWebKitBridge::compileShaderSource):
        (WebCore::ANGLEWebKitBridge::angleAvailable): Deleted.
        * platform/graphics/ANGLEWebKitBridge.h:
        * platform/graphics/cocoa/GraphicsContextGLOpenGLCocoa.mm:
        (WebCore::GraphicsContextGLOpenGL::GraphicsContextGLOpenGL):
    
        Canonical link: https://commits.webkit.org/219362@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@254551 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/219244.6@safari-610.1.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610.1.1-branch@254570 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/ThirdParty/ANGLE/include/GLSLANG/ShaderVars.h
+++ Source/ThirdParty/ANGLE/include/GLSLANG/ShaderVars.h
@@ -10,8 +10,6 @@
 #ifndef GLSLANG_SHADERVARS_H_
 #define GLSLANG_SHADERVARS_H_
 
-#include "export.h"
-
 #include <algorithm>
 #include <array>
 #include <string>
@@ -64,12 +62,12 @@ enum class BlockType
 // See https://code.google.com/p/angleproject/issues/detail?id=697
 struct ShaderVariable
 {
-    ANGLE_EXPORT ShaderVariable();
+    ShaderVariable();
     ShaderVariable(GLenum typeIn);
     ShaderVariable(GLenum typeIn, unsigned int arraySizeIn);
-    ANGLE_EXPORT ~ShaderVariable();
-    ANGLE_EXPORT ShaderVariable(const ShaderVariable &other);
-    ANGLE_EXPORT ShaderVariable &operator=(const ShaderVariable &other);
+    ~ShaderVariable();
+    ShaderVariable(const ShaderVariable &other);
+    ShaderVariable &operator=(const ShaderVariable &other);
     bool operator==(const ShaderVariable &other) const;
     bool operator!=(const ShaderVariable &other) const { return !operator==(other); }
 
