09e7c057be82907d85b5e00e2c6b023384dd7cc4
Wenson Hsieh <wenson_hsieh@apple.com>
2020-01-18 20:35:11 +0000

    Make pasteboard markup sanitization more robust https://bugs.webkit.org/show_bug.cgi?id=206379 <rdar://problem/58660859>
    Reviewed by Ryosuke Niwa.
    
    Makes markup sanitization when copying and pasting more robust in some circumstances (see the bug for additional
    details).
    
    * editing/markup.cpp:
    (WebCore::createPageForSanitizingWebContent):
    
    Adopt the new setting when creating the temporary web page used to sanitize markup coming from the pasteboard.
    
    * html/parser/HTMLParserOptions.cpp:
    (WebCore::HTMLParserOptions::HTMLParserOptions):
    * html/parser/HTMLParserOptions.h:
    
    Rename `scriptEnabled` to `scriptingFlag`, since parsing script elements may now be allowed even when JavaScript
    execution is disabled. The term "scripting flag" also closely matches the wording of the HTML parsing
    specification.
    
    * html/parser/HTMLTokenizer.cpp:
    (WebCore::HTMLTokenizer::updateStateFor):
    * html/parser/HTMLTreeBuilder.cpp:
    (WebCore::HTMLTreeBuilder::processStartTagForInBody):
    (WebCore::HTMLTreeBuilder::processStartTagForInHead):
    * page/Settings.yaml:
    
    Add a new setting to determine whether to consider the scripting flag on when parsing HTML. By default, we will
    only turn the scripting flag on if script execution is enabled; however, this may be set such that we may
    consider the scripting flag set, even though script execution is disabled.
    
    * page/SettingsBase.h:
    
    Canonical link: https://commits.webkit.org/219538@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@254800 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/parser/HTMLParserOptions.cpp
+++ Source/WebCore/html/parser/HTMLParserOptions.cpp
@@ -36,7 +36,7 @@
 namespace WebCore {
 
 HTMLParserOptions::HTMLParserOptions()
-    : scriptEnabled(false)
+    : scriptingFlag(false)
     , usePreHTML5ParserQuirks(false)
     , maximumDOMTreeDepth(Settings::defaultMaximumHTMLParserDOMTreeDepth)
 {
@@ -45,7 +45,10 @@ HTMLParserOptions::HTMLParserOptions()
 HTMLParserOptions::HTMLParserOptions(Document& document)
 {
     RefPtr<Frame> frame = document.frame();
-    scriptEnabled = frame && frame->script().canExecuteScripts(NotAboutToExecuteScript);
+    if (document.settings().parserScriptingFlagPolicy() == SettingsBase::ParserScriptingFlagPolicy::Enabled)
+        scriptingFlag = true;
+    else
+        scriptingFlag = frame && frame->script().canExecuteScripts(NotAboutToExecuteScript);
 
     usePreHTML5ParserQuirks = document.settings().usePreHTML5ParserQuirks();
     maximumDOMTreeDepth = document.settings().maximumHTMLParserDOMTreeDepth();
