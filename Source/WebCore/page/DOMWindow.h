3b6f9e012bb3b575d018401ff8a45f7ffa21a70f
Chris Dumez <cdumez@apple.com>
2020-01-21 17:52:52 +0000

    Unreviewed, rolling out r254753.
    It turns out this is not dead code
    
    Reverted changeset:
    
    "Drop dead code related to local storage prewarming"
    https://bugs.webkit.org/show_bug.cgi?id=206418
    https://trac.webkit.org/changeset/254753
    
    Canonical link: https://commits.webkit.org/219594@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@254857 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/DOMWindow.h
+++ Source/WebCore/page/DOMWindow.h
@@ -203,6 +203,8 @@ public:
 
     void showModalDialog(const String& urlString, const String& dialogFeaturesString, DOMWindow& activeWindow, DOMWindow& firstWindow, const WTF::Function<void(DOMWindow&)>& prepareDialogFunction);
 
+    void prewarmLocalStorageIfNecessary();
+
     void alert(const String& message = emptyString());
     bool confirmForBindings(const String& message);
     String prompt(const String& message, const String& defaultValue);
