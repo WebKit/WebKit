5d6947655c9bbde34d2c9d88f6b5e2668fae64cd
Carlos Garcia Campos <cgarcia@igalia.com>
2020-01-21 11:28:10 +0000

    [GTK][WPE] Enable WebGL by default https://bugs.webkit.org/show_bug.cgi?id=205335
    Reviewed by Adrian Perez de Castro.
    
    Source/WebKit:
    
    * UIProcess/API/glib/WebKitSettings.cpp:
    (webkit_settings_class_init):
    
    Tools:
    
    Update the unit test.
    
    * TestWebKitAPI/Tests/WebKitGLib/TestWebKitSettings.cpp:
    (testWebKitSettings):
    
    Canonical link: https://commits.webkit.org/219585@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@254848 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/glib/WebKitSettings.cpp
+++ Source/WebKit/UIProcess/API/glib/WebKitSettings.cpp
@@ -1062,17 +1062,18 @@ static void webkit_settings_class_init(WebKitSettingsClass* klass)
     /**
     * WebKitSettings:enable-webgl:
     *
-    * Enable or disable support for WebGL on pages. WebGL is an experimental
-    * proposal for allowing web pages to use OpenGL ES-like calls directly. The
-    * standard is currently a work-in-progress by the Khronos Group.
+    * Enable or disable support for WebGL on pages. WebGL enables web
+    * content to use an API based on OpenGL ES 2.0.
     */
-    g_object_class_install_property(gObjectClass,
-                                    PROP_ENABLE_WEBGL,
-                                    g_param_spec_boolean("enable-webgl",
-                                                         _("Enable WebGL"),
-                                                         _("Whether WebGL content should be rendered"),
-                                                         FALSE,
-                                                         readWriteConstructParamFlags));
+    g_object_class_install_property(
+        gObjectClass,
+        PROP_ENABLE_WEBGL,
+        g_param_spec_boolean(
+            "enable-webgl",
+            _("Enable WebGL"),
+            _("Whether WebGL content should be rendered"),
+            TRUE,
+            readWriteConstructParamFlags));
 
     /**
      * WebKitSettings:allow-modal-dialogs:
--- Tools/TestWebKitAPI/Tests/WebKitGLib/TestWebKitSettings.cpp
+++ Tools/TestWebKitAPI/Tests/WebKitGLib/TestWebKitSettings.cpp
@@ -236,10 +236,10 @@ static void testWebKitSettings(Test*, gconstpointer)
     webkit_settings_set_enable_webaudio(settings, FALSE);
     g_assert_false(webkit_settings_get_enable_webaudio(settings));
 
-    // WebGL is disabled by default.
-    g_assert_false(webkit_settings_get_enable_webgl(settings));
-    webkit_settings_set_enable_webgl(settings, TRUE);
+    // WebGL is enabled by default.
     g_assert_true(webkit_settings_get_enable_webgl(settings));
+    webkit_settings_set_enable_webgl(settings, FALSE);
+    g_assert_false(webkit_settings_get_enable_webgl(settings));
 
     // Allow Modal Dialogs is disabled by default.
     g_assert_false(webkit_settings_get_allow_modal_dialogs(settings));
