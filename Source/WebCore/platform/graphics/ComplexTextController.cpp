d214e1727ae9b57fb7760e56f8b10123960c4645
Fujii Hironori <Hironori.Fujii@sony.com>
2020-01-22 10:42:35 +0000

    Merge r254114 - ComplexTextController::offsetForPosition returns a wrong offset for a glyph boundary in a RTL text https://bugs.webkit.org/show_bug.cgi?id=205486
    Reviewed by Ross Kirsling.
    
    Source/WebCore:
    
    ComplexTextController::offsetForPosition had the following code:
    
    > unsigned hitIndex = hitGlyphStart + (hitGlyphEnd - hitGlyphStart) * (m_run.ltr() ? x / adjustedAdvance : 1 - x / adjustedAdvance);
    
    If m_run.ltr() was false and x == 0, hitIndex would become hitGlyphEnd.
    This is not expected. It expects hitIndex < hitGlyphEnd if hitGlyphStart ≠ hitGlyphEnd.
    Let hitIndex be hitGlyphStart-1 in the such condition.
    
    Above change makes fast/text/ellipsis-text-rtl.html starting to
    fail because offsetForPosition returns the character offset of the
    next glyph if the argument 'h' is in a glyph boundary. In RTL
    text, offsetForPosition should return a character offset of the
    previous glyph in case of a glyph boundary. Use '<=' instead of '<'
    for RTL text in order to select previous glyphs for glyph
    boundaries.
    
    Test: fast/dom/Document/CaretRangeFromPoint/rtl.html
    
    * platform/graphics/ComplexTextController.cpp:
    (WebCore::ComplexTextController::offsetForPosition): Compute correct hitGlyphEnd for RTL.
    
    LayoutTests:
    
    * fast/dom/Document/CaretRangeFromPoint/rtl-expected.txt: Added.
    * fast/dom/Document/CaretRangeFromPoint/rtl.html: Added.
    * platform/gtk/TestExpectations: Unmarked imported/blink/editing/selection/offset-from-point-complex-scripts.html.

--- Source/WebCore/platform/graphics/ComplexTextController.cpp
+++ Source/WebCore/platform/graphics/ComplexTextController.cpp
@@ -205,7 +205,8 @@ unsigned ComplexTextController::offsetForPosition(float h, bool includePartialGl
         for (unsigned j = 0; j < complexTextRun.glyphCount(); ++j) {
             unsigned index = offsetIntoAdjustedGlyphs + j;
             float adjustedAdvance = m_adjustedBaseAdvances[index].width();
-            if (x < adjustedAdvance) {
+            bool hit = m_run.ltr() ? x < adjustedAdvance : (x <= adjustedAdvance && adjustedAdvance);
+            if (hit) {
                 unsigned hitGlyphStart = complexTextRun.indexAt(j);
                 unsigned hitGlyphEnd;
                 if (m_run.ltr())
@@ -215,7 +216,18 @@ unsigned ComplexTextController::offsetForPosition(float h, bool includePartialGl
 
                 // FIXME: Instead of dividing the glyph's advance equally between the characters, this
                 // could use the glyph's "ligature carets". This is available in CoreText via CTFontGetLigatureCaretPositions().
-                unsigned hitIndex = hitGlyphStart + (hitGlyphEnd - hitGlyphStart) * (m_run.ltr() ? x / adjustedAdvance : 1 - x / adjustedAdvance);
+                unsigned hitIndex;
+                if (m_run.ltr())
+                    hitIndex = hitGlyphStart + (hitGlyphEnd - hitGlyphStart) * (x / adjustedAdvance);
+                else {
+                    if (hitGlyphStart == hitGlyphEnd)
+                        hitIndex = hitGlyphStart;
+                    else if (x)
+                        hitIndex = hitGlyphEnd - (hitGlyphEnd - hitGlyphStart) * (x / adjustedAdvance);
+                    else
+                        hitIndex = hitGlyphEnd - 1;
+                }
+
                 unsigned stringLength = complexTextRun.stringLength();
                 CachedTextBreakIterator cursorPositionIterator(StringView(complexTextRun.characters(), stringLength), TextBreakIterator::Mode::Caret, nullAtom());
                 unsigned clusterStart;
