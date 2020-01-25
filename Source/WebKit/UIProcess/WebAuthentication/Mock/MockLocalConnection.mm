fa445d459cc0af421324e1e6f341e1311e615f6d
Jiewen Tan <jiewen_tan@apple.com>
2020-01-25 01:28:03 +0000

    Unreviewed, a speculative test fix after r255041
    * UIProcess/WebAuthentication/Mock/MockLocalConnection.mm:
    (WebKit::MockLocalConnection::getAttestation const):
    
    Canonical link: https://commits.webkit.org/219775@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@255111 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/WebAuthentication/Mock/MockLocalConnection.mm
+++ Source/WebKit/UIProcess/WebAuthentication/Mock/MockLocalConnection.mm
@@ -100,6 +100,11 @@ void MockLocalConnection::getAttestation(const String& rpId, const String& usern
             (id)kSecValueRef: (id)key.get(),
             (id)kSecClass: (id)kSecClassKey,
             (id)kSecAttrLabel: (id)label,
+#if HAVE(DATA_PROTECTION_KEYCHAIN)
+            (id)kSecUseDataProtectionKeychain: @YES
+#else
+            (id)kSecAttrNoLegacy: @YES
+#endif
         };
         OSStatus status = SecItemAdd((__bridge CFDictionaryRef)addQuery, NULL);
         if (status) {
