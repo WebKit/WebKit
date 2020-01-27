f3b7a8ad587e56743dedaa0c72f1fa66d5467b56
Peng Liu <peng.liu6@apple.com>
2020-01-27 21:41:43 +0000

    Crash in WebCore::HTMLMediaElement::detachMediaSource() https://bugs.webkit.org/show_bug.cgi?id=206766
    Patch by Peng Liu <peng.liu6@apple.com> on 2020-01-27
    Reviewed by Jer Noble.
    
    Use WeakPtr<HTMLMediaElement> in MediaSource instead of a raw pointer.
    In addition, we need to detach a MediaSource from an HTMLMediaElement before the HTMLMediaElement forgets the reference to the MediaSource.
    
    * Modules/mediasource/MediaSource.cpp:
    (WebCore::MediaSource::attachToElement):
    * Modules/mediasource/MediaSource.h:
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::loadResource):
    
    Canonical link: https://commits.webkit.org/219834@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@255170 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/mediasource/MediaSource.h
+++ Source/WebCore/Modules/mediasource/MediaSource.h
@@ -36,14 +36,15 @@
 #include "EventTarget.h"
 #include "ExceptionOr.h"
 #include "GenericEventQueue.h"
+#include "HTMLMediaElement.h"
 #include "MediaSourcePrivateClient.h"
 #include "URLRegistry.h"
 #include <wtf/LoggerHelper.h>
+#include <wtf/WeakPtr.h>
 
 namespace WebCore {
 
 class ContentType;
-class HTMLMediaElement;
 class SourceBuffer;
 class SourceBufferList;
 class SourceBufferPrivate;
@@ -97,7 +98,7 @@ public:
     ReadyState readyState() const { return m_readyState; }
     ExceptionOr<void> endOfStream(Optional<EndOfStreamError>);
 
-    HTMLMediaElement* mediaElement() const { return m_mediaElement; }
+    HTMLMediaElement* mediaElement() const { return m_mediaElement.get(); }
 
     SourceBufferList* sourceBuffers() { return m_sourceBuffers.get(); }
     SourceBufferList* activeSourceBuffers() { return m_activeSourceBuffers.get(); }
@@ -161,7 +162,7 @@ private:
     RefPtr<SourceBufferList> m_activeSourceBuffers;
     mutable std::unique_ptr<PlatformTimeRanges> m_buffered;
     std::unique_ptr<PlatformTimeRanges> m_liveSeekable;
-    HTMLMediaElement* m_mediaElement { nullptr };
+    WeakPtr<HTMLMediaElement> m_mediaElement;
     MediaTime m_duration;
     MediaTime m_pendingSeekTime;
     ReadyState m_readyState { ReadyState::Closed };
