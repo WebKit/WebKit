47205283bb3c4fd88600a27e066cd8c6ad352263
Paulo Matos <pmatos@igalia.com>
2020-01-29 14:40:02 +0000

    Skip slow test operand-should-fit-in-abstract-heap-encoded-payload-format.js on mips and arm https://bugs.webkit.org/show_bug.cgi?id=206926
    Unreviewed test gardening.
    
    This test has quadratic behaviour with the size of a, and on mips and arm it just takes too
    long to run with such high value of a. Therefore we skip.
    
    Patch by Paulo Matos <pmatos@igalia.com> on 2020-01-29
    
    * stress/operand-should-fit-in-abstract-heap-encoded-payload-format.js:
    
    Canonical link: https://commits.webkit.org/219949@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@255355 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/operand-should-fit-in-abstract-heap-encoded-payload-format.js
+++ JSTests/stress/operand-should-fit-in-abstract-heap-encoded-payload-format.js
@@ -1,4 +1,4 @@
-//@ skip if $buildType == "debug"
+//@ skip if $buildType == "debug" or ["arm", "mips"].include?($architecture)
 //@ runDefault("--useConcurrentJIT=0", "--jitPolicyScale=0")
 const a = [];
 a.length = 65532;
