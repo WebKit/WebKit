3007a4b3965f24d208fd94a98456a76ad4b7cef4
Chris Dumez <cdumez@apple.com>
2020-02-05 00:32:48 +0000

    Cherry-pick r255329. rdar://problem/59097765
        REGRESSION (r252064): [ Mac iOS ] storage/websql/statement-error-callback.html is timing out flakily
        https://bugs.webkit.org/show_bug.cgi?id=206291
        <rdar://problem/58606666>
    
        Reviewed by Maciej Stachowiak.
    
        Source/WebCore:
    
        I found that extending the lifetime of the SQLTransaction by capturing protectedThis in the event loop
        task was the source of the flaky timeouts & crashes. Queueing this event loop task in
        notifyDatabaseThreadIsShuttingDown() is actually not necessary because calling callErrorCallbackDueToInterruption()
        directly would schedule the event loop task for us. Also, in callErrorCallbackDueToInterruption(),
        the event loop task only keeps the error callback alive, not just the SQLTransaction object.
    
        No new tests, unskipped existing test.
    
        * Modules/webdatabase/SQLTransaction.cpp:
        (WebCore::SQLTransaction::notifyDatabaseThreadIsShuttingDown):
    
        LayoutTests:
    
        Unskip test that is no longer flaky.
    
        * platform/ios/TestExpectations:
        * platform/mac/TestExpectations:
    
        Canonical link: https://commits.webkit.org/219930@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@255329 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/218903.190@safari-609-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609-branch@255755 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/webdatabase/SQLTransaction.cpp
+++ Source/WebCore/Modules/webdatabase/SQLTransaction.cpp
@@ -128,9 +128,7 @@ void SQLTransaction::performPendingCallback()
 void SQLTransaction::notifyDatabaseThreadIsShuttingDown()
 {
     callOnMainThread([this, protectedThis = makeRef(*this)]() mutable {
-        m_database->document().eventLoop().queueTask(TaskSource::Networking, [this, protectedThis = protectedThis.copyRef()]() mutable {
-            callErrorCallbackDueToInterruption();
-        });
+        callErrorCallbackDueToInterruption();
     });
 
     m_backend.notifyDatabaseThreadIsShuttingDown();
