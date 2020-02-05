56dcbdceb93fbfae1dfb0b4ee55fe041be5bccf7
repstein@apple.com <repstein@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2020-02-05 00:31:00 +0000

    Cherry-pick r255151. rdar://problem/58942731
        Correct VTT Cue Style handling to match the specification
        https://bugs.webkit.org/show_bug.cgi?id=201086
        <rdar://problem/54658121>
    
        Reviewed by Brent Fulgham.
    
        The VTT specification requires that only data-URLs are permitted in STYLE blocks.
    
        * css/CSSSelector.cpp:
        (WebCore::CSSSelector::selectorText const):
    
        Fix selectorText for function version of ::cue().
    
        * css/parser/CSSParserContext.cpp:
        (WebCore::CSSParserContext::completeURL const):
    
        Don't allow non-data URLs in WebVTT parser mode.
    
        * css/parser/CSSParserContext.h:
        (WebCore::CSSParserContext::completeURL const): Deleted.
        * css/parser/CSSParserMode.h:
        (WebCore::isStrictParserMode):
        * html/track/WebVTTParser.cpp:
        (WebCore::WebVTTParser::collectStyleSheet):
        (WebCore::WebVTTParser::checkAndStoreStyleSheet):
    
        Instead of simply validating the original stylesheet, build a new sanitized stylesheet text
        from the stylesheet parsed in WebVTT mode. This sanitized stylesheet is then used as the
        input for the style system.
    
        * html/track/WebVTTParser.h:
    
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@255151 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609-branch@255725 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/parser/CSSParserMode.h
+++ Source/WebCore/css/parser/CSSParserMode.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012 Adobe Systems Incorporated. All rights reserved.
- * Copyright (C) 2012 Apple Inc. All rights reserved.
+ * Copyright (C) 2012-2020 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -42,7 +42,9 @@ enum CSSParserMode {
     // must call parseViewportProperties so needs a special mode.
     CSSViewportRuleMode,
     // User agent stylesheets are parsed in standards mode but also allows internal properties and values.
-    UASheetMode
+    UASheetMode,
+    // WebVTT places limitations on external resources.
+    WebVTTMode
 };
 
 inline bool isQuirksModeBehavior(CSSParserMode mode)
@@ -73,7 +75,18 @@ inline CSSParserMode strictToCSSParserMode(bool inStrictMode)
 
 inline bool isStrictParserMode(CSSParserMode cssParserMode)
 {
-    return cssParserMode == UASheetMode || cssParserMode == HTMLStandardMode || cssParserMode == SVGAttributeMode;
+    switch (cssParserMode) {
+    case UASheetMode:
+    case HTMLStandardMode:
+    case SVGAttributeMode:
+    case WebVTTMode:
+        return true;
+    case HTMLQuirksMode:
+    case CSSViewportRuleMode:
+        return false;
+    }
+    ASSERT_NOT_REACHED();
+    return false;
 }
 
 } // namespace WebCore
