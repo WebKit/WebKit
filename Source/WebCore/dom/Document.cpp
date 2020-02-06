c6a237e092d5a80ad08632a7a8955153570fc74f
Antoine Quint <graouts@apple.com>
2020-02-06 18:57:49 +0000

    Cherry-pick r255953. rdar://problem/59228071
        [Web Animations] Ensure all timelines are detached from their document
        https://bugs.webkit.org/show_bug.cgi?id=207331
        <rdar://problem/59210306>
    
        Patch by Antoine Quint <graouts@apple.com> on 2020-02-06
        Reviewed by Dean Jackson.
    
        We recently added a WeakHashSet<DocumentTimeline> m_timelines member to Document and added code to ~DocumentTimeline
        to remove themselves from their Document's m_timelines. However, Document::prepareForDestruction() would call
        DocumentTimeline::detachFromDocument() only for the main timeline and neglect to do the same for any additional
        timelines that may have been created with the DocumentTimeline constructor.
    
        We now cleanly call DocumentTimeline::detachFromDocument() for all items in a Document's m_timelines, which has the
        effect of clearing the Document <> DocumentTimeline relationship since DocumentTimeline::detachFromDocument() now
        calls Document::removeTimeline().
    
        Finally, we now call DocumentTimeline::detachFromDocument() from the DocumentTimeline destructor to ensure that timelines
        that were created purely in JS but got garbage-collected are no longer referenced from the Document still.
    
        * animation/DocumentTimeline.cpp:
        (WebCore::DocumentTimeline::~DocumentTimeline):
        (WebCore::DocumentTimeline::detachFromDocument):
        (WebCore::DocumentTimeline::cacheCurrentTime):
        * dom/Document.cpp:
        (WebCore::Document::prepareForDestruction):
    
        Canonical link: https://commits.webkit.org/220319@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@255953 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/219244.98@safari-610.1.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610.1.1-branch@255965 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/Document.cpp
+++ Source/WebCore/dom/Document.cpp
@@ -2578,10 +2578,9 @@ void Document::prepareForDestruction()
 
     detachFromFrame();
 
-    if (m_timeline) {
-        m_timeline->detachFromDocument();
-        m_timeline = nullptr;
-    }
+    while (!m_timelines.computesEmpty())
+        m_timelines.begin()->detachFromDocument();
+    m_timeline = nullptr;
 
 #if ENABLE(CSS_PAINTING_API)
     for (auto& scope : m_paintWorkletGlobalScopes.values())
