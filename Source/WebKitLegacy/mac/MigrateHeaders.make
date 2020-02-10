55ce85b14f666bff1ec304f3b718321f5dd0548a
Truitt Savell <tsavell@apple.com>
2020-02-10 17:28:29 +0000

    Unreviewed, rolling out r256091.
    Broke internal builds
    
    Reverted changeset:
    
    "Move trivial definitions from FeatureDefines.xcconfig to
    PlatformEnableCocoa.h"
    https://bugs.webkit.org/show_bug.cgi?id=207155
    https://trac.webkit.org/changeset/256091
    
    Canonical link: https://commits.webkit.org/220466@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@256184 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKitLegacy/mac/MigrateHeaders.make
+++ Source/WebKitLegacy/mac/MigrateHeaders.make
@@ -62,21 +62,7 @@ HEADERS += \
 #
 endif
 
-FRAMEWORK_FLAGS = $(shell echo $(BUILT_PRODUCTS_DIR) $(FRAMEWORK_SEARCH_PATHS) $(SYSTEM_FRAMEWORK_SEARCH_PATHS) | perl -e 'print "-F " . join(" -F ", split(" ", <>));')
-HEADER_FLAGS = $(shell echo $(BUILT_PRODUCTS_DIR) $(HEADER_SEARCH_PATHS) $(SYSTEM_HEADER_SEARCH_PATHS) | perl -e 'print "-I" . join(" -I", split(" ", <>));')
-
-ifneq ($(SDKROOT),)
-    SDK_FLAGS=-isysroot $(SDKROOT)
-endif
-
-ifeq ($(USE_LLVM_TARGET_TRIPLES_FOR_CLANG),YES)
-    WK_CURRENT_ARCH=$(word 1, $(ARCHS))
-    TARGET_TRIPLE_FLAGS=-target $(WK_CURRENT_ARCH)-$(LLVM_TARGET_TRIPLE_VENDOR)-$(LLVM_TARGET_TRIPLE_OS_VERSION)$(LLVM_TARGET_TRIPLE_SUFFIX)
-endif
-
-ENABLED_FEATURES = $(shell $(CC) -std=$(CLANG_CXX_LANGUAGE_STANDARD) -x c++ -E -P -dM $(SDK_FLAGS) $(TARGET_TRIPLE_FLAGS) $(patsubst %, -D%, $(FEATURE_DEFINES)) $(FRAMEWORK_FLAGS) $(HEADER_FLAGS) -include "wtf/Platform.h" /dev/null | grep '\#define ENABLE_.* 1' | cut -d' ' -f2)
-
-ifeq ($(findstring ENABLE_IOS_TOUCH_EVENTS, $(ENABLED_FEATURES)), ENABLE_IOS_TOUCH_EVENTS)
+ifeq ($(findstring ENABLE_IOS_TOUCH_EVENTS, $(FEATURE_DEFINES)), ENABLE_IOS_TOUCH_EVENTS)
 HEADERS += \
     $(PRIVATE_HEADERS_DIR)/WebEventRegion.h
 endif
@@ -86,6 +72,9 @@ migrate_headers : $(HEADERS)
 
 WEBCORE_HEADER_REPLACE_RULES = -e 's/<WebCore\//<WebKitLegacy\//' -e "s/(^ *)WEBCORE_EXPORT /\1/"
 WEBCORE_HEADER_MIGRATE_CMD = sed -E $(WEBCORE_HEADER_REPLACE_RULES) $< > $@; touch $(PRIVATE_HEADERS_DIR)
+
+FRAMEWORK_FLAGS = $(shell echo $(BUILT_PRODUCTS_DIR) $(FRAMEWORK_SEARCH_PATHS) $(SYSTEM_FRAMEWORK_SEARCH_PATHS) | perl -e 'print "-F " . join(" -F ", split(" ", <>));')
+HEADER_FLAGS = $(shell echo $(BUILT_PRODUCTS_DIR) $(HEADER_SEARCH_PATHS) $(SYSTEM_HEADER_SEARCH_PATHS) | perl -e 'print "-I" . join(" -I", split(" ", <>));')
 
 $(PRIVATE_HEADERS_DIR)/% : % MigrateHeaders.make
 	$(WEBCORE_HEADER_MIGRATE_CMD)
--- Source/WebKitLegacy/scripts/generate-unified-sources.sh
+++ Source/WebKitLegacy/scripts/generate-unified-sources.sh
@@ -21,24 +21,4 @@ if [ $# -eq 0 ]; then
     echo "Using unified source list files: Sources.txt, SourcesCocoa.txt"
 fi
 
-if [ ! $CC ]; then
-    export CC="`xcrun -find clang`"
-fi
-
-if [ -n "$SDKROOT" ]; then
-    SDK_FLAGS="-isysroot ${SDKROOT}"
-fi
-
-if [ "${USE_LLVM_TARGET_TRIPLES_FOR_CLANG}" = "YES" ]; then
-    # FIXME: This is probably wrong for fat builds, but matches the current behavior of DerivedSources.make
-    WK_CURRENT_ARCH=$(echo ${ARCHS} | cut -d " " -f1)
-    TARGET_TRIPLE_FLAGS="-target ${WK_CURRENT_ARCH}-${LLVM_TARGET_TRIPLE_VENDOR}-${LLVM_TARGET_TRIPLE_OS_VERSION}${LLVM_TARGET_TRIPLE_SUFFIX}"
-fi
-
-FRAMEWORK_FLAGS=$(echo ${BUILT_PRODUCTS_DIR} ${FRAMEWORK_SEARCH_PATHS} ${SYSTEM_FRAMEWORK_SEARCH_PATHS} | perl -e 'print "-F " . join(" -F ", split(" ", <>));')
-HEADER_FLAGS=$(echo ${BUILT_PRODUCTS_DIR} ${HEADER_SEARCH_PATHS} ${SYSTEM_HEADER_SEARCH_PATHS} | perl -e 'print "-I" . join(" -I", split(" ", <>));')
-
-FEATURE_DEFINES_FROM_XCCONFIG=$(echo ${FEATURE_DEFINES} | perl -e 'print "-D" . join(" -D", split(" ", <>));')
-ENABLED_FEATURES=$(${CC} -std=${CLANG_CXX_LANGUAGE_STANDARD} -x c++ -E -P -dM ${SDK_FLAGS} ${TARGET_TRIPLE_FLAGS} ${FEATURE_DEFINES_FROM_XCCONFIG} ${FRAMEWORK_FLAGS} ${HEADER_FLAGS} -include "wtf/Platform.h" /dev/null | grep '\#define ENABLE_.* 1' | cut -d' ' -f2)
-
-/usr/bin/env ruby "${BUILD_SCRIPTS_DIR}/generate-unified-source-bundles.rb" "--derived-sources-path" "${BUILT_PRODUCTS_DIR}/DerivedSources/WebKitLegacy" "--source-tree-path" "${SRCROOT}" "--feature-flags" "${ENABLED_FEATURES}" "--max-cpp-bundle-count" "${UnifiedSourceCppFileCount}" "--max-obj-c-bundle-count" "${UnifiedSourceMmFileCount}" "Sources.txt" "SourcesCocoa.txt" "${ARGS[@]}" > /dev/null
+/usr/bin/env ruby "${BUILD_SCRIPTS_DIR}/generate-unified-source-bundles.rb" "--derived-sources-path" "${BUILT_PRODUCTS_DIR}/DerivedSources/WebKitLegacy" "--source-tree-path" "${SRCROOT}" "--feature-flags" "${FEATURE_DEFINES}" "--max-cpp-bundle-count" "${UnifiedSourceCppFileCount}" "--max-obj-c-bundle-count" "${UnifiedSourceMmFileCount}" "Sources.txt" "SourcesCocoa.txt" "${ARGS[@]}" > /dev/null
