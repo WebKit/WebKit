eb59e06ec7d2758c828909c199f4b2d197a25439
Alan Coon <alancoon@apple.com>
2020-02-11 23:55:30 +0000

    Unreviewed build fix. rdar://problem/59298172
    Canonical link: https://commits.webkit.org/218903.292@safari-609-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609-branch@256393 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/WebStorage/StorageAreaImpl.cpp
+++ Source/WebKit/WebProcess/WebStorage/StorageAreaImpl.cpp
@@ -46,7 +46,7 @@ StorageAreaImpl::StorageAreaImpl(Ref<StorageAreaMap>&& storageAreaMap)
     : m_identifier(Identifier::generate())
     , m_storageAreaMap(makeWeakPtr(storageAreaMap.get()))
 {
-    storageAreaMap.incrementUseCount();
+    storageAreaMap->incrementUseCount();
 }
 
 StorageAreaImpl::~StorageAreaImpl()
