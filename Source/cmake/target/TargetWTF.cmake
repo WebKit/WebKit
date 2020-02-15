ba1f05f7ce779c5c1036c05b4ad18005e96205e0
repstein@apple.com <repstein@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2020-02-15 03:02:27 +0000

    Cherry-pick r254681. rdar://problem/59474790
        [Win] Fix AppleWin build
        https://bugs.webkit.org/show_bug.cgi?id=206299
    
        Reviewed by Brent Fulgham.
    
        .:
    
        This patch has been created by don.olmstead@sony.com and pvollan@apple.com. Add target files for WTF and
        JavaScriptCore. Also, to make sure headers are copied to the forwarding headers directory, add the CMake
        keywork ALL when adding custom target for copying files.
    
        * Source/cmake/TargetJavaScriptCore.cmake: Added.
        * Source/cmake/TargetWTF.cmake: Added.
        * Source/cmake/WebKitMacros.cmake:
    
        Source/JavaScriptCore:
    
        Include required target. Build internal builds with VS2019.
    
        * CMakeLists.txt:
        * JavaScriptCore.vcxproj/JavaScriptCore.proj:
    
        Source/WebCore:
    
        Include required targets. Build internal builds with VS2019.
    
        * CMakeLists.txt:
        * WebCore.vcxproj/WebCore.proj:
    
        Source/WebKitLegacy:
    
        Include required targets. Build internal builds with VS2019.
    
        * CMakeLists.txt:
        * WebKitLegacy.vcxproj/WebKitLegacy.proj:
    
        Source/WTF:
    
        Build internal builds with VS2019.
    
        * WTF.vcxproj/WTF.proj:
    
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@254681 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609-branch@256688 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/cmake/target/TargetWTF.cmake
@@ -0,0 +1,18 @@
+if (NOT TARGET WebKit::WTF)
+    if (NOT INTERNAL_BUILD)
+        message(FATAL_ERROR "WebKit::WTF target not found")
+    endif ()
+
+    # This should be moved to an if block if the Apple Mac/iOS build moves completely to CMake
+    # Just assuming Windows for the moment
+    add_library(WebKit::WTF SHARED IMPORTED)
+    set_target_properties(WebKit::WTF PROPERTIES
+        IMPORTED_LOCATION ${WEBKIT_LIBRARIES_RUNTIME_DIR}/WTF${DEBUG_SUFFIX}.dll
+        IMPORTED_IMPLIB ${WEBKIT_LIBRARIES_LINK_DIR}/WTF${DEBUG_SUFFIX}.lib
+        # Should add Apple::CoreFoundation here when https://bugs.webkit.org/show_bug.cgi?id=205085 lands
+        INTERFACE_LINK_LIBRARIES "ICU::data;ICU::i18n;ICU::uc"
+    )
+    target_include_directories(WebKit::WTF INTERFACE
+        ${WTF_FRAMEWORK_HEADERS_DIR}
+    )
+endif ()
