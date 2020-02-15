bf2e08573b1fb1fb8da2652f8206b6b02fcba688
Per Arne Vollan <pvollan@apple.com>
2020-02-15 03:02:27 +0000

    Cherry-pick r254681. rdar://problem/59474790
        [Win] Fix AppleWin build
        https://bugs.webkit.org/show_bug.cgi?id=206299
    
        Reviewed by Brent Fulgham.
    
        .:
    
        This patch has been created by don.olmstead@sony.com and pvollan@apple.com. Add target files for WTF and
        JavaScriptCore. Also, to make sure headers are copied to the forwarding headers directory, add the CMake
        keywork ALL when adding custom target for copying files.
    
        * Source/cmake/TargetJavaScriptCore.cmake: Added.
        * Source/cmake/TargetWTF.cmake: Added.
        * Source/cmake/WebKitMacros.cmake:
    
        Source/JavaScriptCore:
    
        Include required target. Build internal builds with VS2019.
    
        * CMakeLists.txt:
        * JavaScriptCore.vcxproj/JavaScriptCore.proj:
    
        Source/WebCore:
    
        Include required targets. Build internal builds with VS2019.
    
        * CMakeLists.txt:
        * WebCore.vcxproj/WebCore.proj:
    
        Source/WebKitLegacy:
    
        Include required targets. Build internal builds with VS2019.
    
        * CMakeLists.txt:
        * WebKitLegacy.vcxproj/WebKitLegacy.proj:
    
        Source/WTF:
    
        Build internal builds with VS2019.
    
        * WTF.vcxproj/WTF.proj:
    
        Canonical link: https://commits.webkit.org/219431@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@254681 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/218903.346@safari-609-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609-branch@256688 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/cmake/target/TargetJavaScriptCore.cmake
@@ -0,0 +1,19 @@
+if (NOT TARGET WebKit::JavaScriptCore)
+    if (NOT INTERNAL_BUILD)
+        message(FATAL_ERROR "WebKit::JavaScriptCore target not found")
+    endif ()
+
+    # This should be moved to an if block if the Apple Mac/iOS build moves completely to CMake
+    # Just assuming Windows for the moment
+    add_library(WebKit::JavaScriptCore SHARED IMPORTED)
+    set_target_properties(WebKit::JavaScriptCore PROPERTIES
+        IMPORTED_LOCATION ${WEBKIT_LIBRARIES_RUNTIME_DIR}/JavaScriptCore${DEBUG_SUFFIX}.dll
+        IMPORTED_IMPLIB ${WEBKIT_LIBRARIES_LINK_DIR}/JavaScriptCore${DEBUG_SUFFIX}.lib
+        # Should add Apple::CoreFoundation here when https://bugs.webkit.org/show_bug.cgi?id=205085 lands
+        INTERFACE_LINK_LIBRARIES "WebKit::WTF;ICU::data;ICU::i18n;ICU::uc"
+    )
+    target_include_directories(WebKit::JavaScriptCore INTERFACE
+        ${JavaScriptCore_FRAMEWORK_HEADERS_DIR}
+        ${JavaScriptCore_PRIVATE_FRAMEWORK_HEADERS_DIR}
+    )
+endif ()
