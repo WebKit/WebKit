9508db04e5b5f7d9f42524a520985a9b23137e41
Stephan Szabo <stephan.szabo@sony.com>
2020-02-19 19:46:14 +0000

    [PlayStation] Get jsc test wrappers using find_package https://bugs.webkit.org/show_bug.cgi?id=207914
    Reviewed by Ross Kirsling.
    
    * shell/PlatformPlayStation.cmake:
    
    
    Canonical link: https://commits.webkit.org/220924@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@256930 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/shell/PlatformPlayStation.cmake
+++ Source/JavaScriptCore/shell/PlatformPlayStation.cmake
@@ -4,9 +4,18 @@ list(APPEND jsc_SOURCES
     ${JAVASCRIPTCORE_DIR}/shell/playstation/Initializer.cpp
 )
 
-find_library(LIBTESTWRAPPERS testwrappers PATHS ${WEBKIT_LIBRARIES_DIR}/lib)
+# Get the necessary wrappers for C functions to make jsc shell
+# able to properly run tests. Depending on version, first try
+# using find_package for new version and if that doesn't work
+# fallback to using find_library for older versions.
+find_package(libtestwrappers)
+if (TARGET libtestwrappers::testwrappers)
+    list(APPEND jsc_LIBRARIES libtestwrappers::testwrappers)
+else ()
+    find_library(LIBTESTWRAPPERS testwrappers PATHS ${WEBKIT_LIBRARIES_DIR}/lib)
+    set(PLAYSTATION_jsc_WRAP fopen getcwd chdir main)
+    list(APPEND jsc_LIBRARIES ${LIBTESTWRAPPERS})
+endif ()
 
 set(PLAYSTATION_jsc_PROCESS_NAME "JSCShell")
 set(PLAYSTATION_jsc_MAIN_THREAD_NAME "JSCShell")
-set(PLAYSTATION_jsc_WRAP fopen getcwd chdir main)
-list(APPEND jsc_LIBRARIES ${LIBTESTWRAPPERS})
