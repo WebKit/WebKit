3926266ad07a03e82c3df4e2949919acfcdc51fb
Aakash Jain <aakash_jain@apple.com>
2020-02-20 22:13:34 +0000

    Update run-webkit-tests help description for skip-failing-tests https://bugs.webkit.org/show_bug.cgi?id=208031
    Reviewed by Alexey Proskuryakov.
    
    * Scripts/webkitpy/layout_tests/run_webkit_tests.py:
    
    
    Canonical link: https://commits.webkit.org/220998@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@257087 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/layout_tests/run_webkit_tests.py
+++ Tools/Scripts/webkitpy/layout_tests/run_webkit_tests.py
@@ -180,7 +180,7 @@ def parse_args(args):
                  "option while others can have a default value that can be overridden here."),
 
         optparse.make_option("--skip-failing-tests", action="store_true",
-            default=False, help="Skip tests that are expected to fail. "
+            default=False, help="Skip tests that are marked as failing or flaky. "
                  "Note: When using this option, you might miss new crashes "
                  "in these tests."),
         optparse.make_option("--additional-drt-flag", action="append",
