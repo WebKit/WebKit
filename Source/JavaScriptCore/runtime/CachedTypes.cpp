1458a04d1cf8ac7d0715c4f19eb44f50c58b5e5a
Mark Lam <mark.lam@apple.com>
2020-02-21 16:20:55 +0000

    Make support for bytecode caching more robust against file corruption. https://bugs.webkit.org/show_bug.cgi?id=207972 <rdar://problem/59260595>
    Reviewed by Yusuke Suzuki.
    
    If a bytecode cache file is corrupted, we currently will always crash every time
    we try to read it (in perpetuity as long as the corrupted cache file continues to
    exist on disk).  To guard against this, we'll harden the bytecode caching mechanism
    as follows:
    
    1. Modify the writeCache operation to always write the cache file in a transactional
       manner i.e. we'll first write to a .tmp file, and then rename the .tmp file to
       the cache file only if the entire file has been written in completeness.
    
       This ensures that we won't get corrupted cache files due to interrupted writes.
    
    2. Modify the writeCache operation to also compute a SHA1 hash of the cache file
       and append the hash at end of the file.  Modify the readCache operation to
       first authenticate the SHA1 hash before allowing the cache file to be used.
       If the hash does not match, the file is bad, and we'll just delete it.
    
       This ensures that we won't be crashing while decoding a corrupted cache file.
    
    Manually tested with the following scenarios and ensuring that the client recovers
    with no crashes:
    
    1. no cache file on disk.
    2. a 0-sized cache file on a disk.
    3. a truncated cache file on disk.
    4. a corrupted cache file on disk.
    5. an uncorrupted cache file on disk.
    
    Also added some static_asserts in CachedTypes.cpp to document some invariants that
    the pre-existing code is dependent on.
    
    * API/JSScript.mm:
    (-[JSScript readCache]):
    (-[JSScript writeCache:]):
    * runtime/CachedTypes.cpp:
    
    
    
    Canonical link: https://commits.webkit.org/221021@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@257134 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/CachedTypes.cpp
+++ Source/JavaScriptCore/runtime/CachedTypes.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Apple Inc. All rights reserved.
+ * Copyright (C) 2019-2020 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -2318,6 +2318,8 @@ private:
     CachedCodeBlockTag m_tag;
 };
 
+static_assert(alignof(GenericCacheEntry) <= alignof(std::max_align_t));
+
 template<typename UnlinkedCodeBlockType>
 class CacheEntry : public GenericCacheEntry {
 public:
@@ -2354,6 +2356,9 @@ private:
     CachedSourceCodeKey m_key;
     CachedPtr<CachedCodeBlockType<UnlinkedCodeBlockType>> m_codeBlock;
 };
+
+static_assert(alignof(CacheEntry<UnlinkedProgramCodeBlock>) <= alignof(std::max_align_t));
+static_assert(alignof(CacheEntry<UnlinkedModuleProgramCodeBlock>) <= alignof(std::max_align_t));
 
 bool GenericCacheEntry::decode(Decoder& decoder, std::pair<SourceCodeKey, UnlinkedCodeBlock*>& result) const
 {
