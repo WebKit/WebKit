7ed9d4bc9b4d0637b8a41e7a0036df75184b64ca
Russell Epstein <repstein@apple.com>
2020-02-24 19:26:40 +0000

    Revert "Cherry-pick r257106. rdar://problem/59676862"
    This reverts commit r257231.
    
    Canonical link: https://commits.webkit.org/218903.407@safari-609.1.20.111-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609.1.20.111-branch@257233 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/ios/WebPageProxyIOS.mm
+++ Source/WebKit/UIProcess/ios/WebPageProxyIOS.mm
@@ -1391,10 +1391,6 @@ static bool desktopClassBrowsingRecommendedForRequest(const WebCore::ResourceReq
             return false;
     }
 
-    // FIXME: Remove this quirk when <rdar://problem/59480381> is complete.
-    if (equalLettersIgnoringASCIICase(host, "fidelity.com") || host.endsWithIgnoringASCIICase(".fidelity.com"))
-        return false;
-
     return true;
 }
 
