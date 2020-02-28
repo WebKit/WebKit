bfb91b93413ce2807de744c44d5ea74dd26821a7
Doug Kelly <dougk@apple.com>
2020-02-28 04:15:37 +0000

    Hit test with clipPath referencing parent element causes infinite recursion https://bugs.webkit.org/show_bug.cgi?id=208279
    Patch by Doug Kelly <dougk@apple.com> on 2020-02-27
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Add an early return for SVG hit tests which have a child element referencing a  parent clipPath.
    This change breaks the cycle, although it unfortunately runs on every hit test, for each node which
    is a child of the clipPath element.
    
    Test: svg/hittest/svg-clip-path-child-element.html
    
    * rendering/svg/RenderSVGResourceClipper.cpp:
    (WebCore::RenderSVGResourceClipper::hitTestClipContent):
    
    LayoutTests:
    
    * svg/hittest/svg-clip-path-child-element-expected.txt: Added.
    * svg/hittest/svg-clip-path-child-element.html: Added.
    
    Canonical link: https://commits.webkit.org/221285@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@257616 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/svg/RenderSVGResourceClipper.cpp
+++ Source/WebCore/rendering/svg/RenderSVGResourceClipper.cpp
@@ -277,6 +277,16 @@ bool RenderSVGResourceClipper::hitTestClipContent(const FloatRect& objectBoundin
             continue;
         if (!renderer->isSVGShape() && !renderer->isSVGText() && !childNode->hasTagName(SVGNames::useTag))
             continue;
+
+        const RenderStyle& style = renderer->style();
+        if (is<ReferenceClipPathOperation>(style.clipPath())) {
+            auto& clipPath = downcast<ReferenceClipPathOperation>(*style.clipPath());
+            AtomString id(clipPath.fragment());
+            RenderSVGResourceClipper* clipper = getRenderSVGResourceById<RenderSVGResourceClipper>(document(), id);
+            if (clipper == this)
+                continue;
+        }
+
         IntPoint hitPoint;
         HitTestResult result(hitPoint);
         constexpr OptionSet<HitTestRequest::RequestType> hitType { HitTestRequest::SVGClipContent, HitTestRequest::DisallowUserAgentShadowContent };
