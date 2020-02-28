05ee79e440f7a67cd6d8d1cf9250d792b3ca6f10
Alan Coon <alancoon@apple.com>
2020-02-28 01:32:30 +0000

    Apply patch. rdar://problem/59870336
    Canonical link: https://commits.webkit.org/218903.419@safari-609.1.20.111-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609.1.20.111-branch@257604 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ JSTests/stress/poly-proto-setter-changes-setter-2.js
@@ -0,0 +1,38 @@
+//@ requireOptions("--forcePolyProto=1", "--useLLInt=0", "--repatchBufferingCountdown=0")
+function assert_eq(a, b) {
+    
+}
+noInline(assert_eq)
+
+class A {
+    set x(v) {
+        if (v == 1) {
+            Object.defineProperty(A.prototype, "x", {
+                get: function() {
+                    return 42
+                },
+                set: undefined
+            });
+        }
+        if (v > 1)
+            throw new Error()
+    }
+}
+
+class B extends A {
+    set y(v) {}
+    set y1(v) {}
+    set y2(v) {}
+}
+
+class C extends B {
+
+}
+
+let a = new C();
+for (let i = 0; i < 30; i++) {
+    a.x = i;
+    assert_eq(a.x, i == 0 ? undefined : 42)
+}
+
+
