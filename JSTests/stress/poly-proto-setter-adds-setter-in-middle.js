674841a3390c08a7bf24b62dbba4f7bafdb47a46
Russell Epstein <repstein@apple.com>
2020-02-29 00:54:31 +0000

    Apply patch. rdar://problem/59870336
    Canonical link: https://commits.webkit.org/218903.422@safari-609.1.20.4-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609.1.20.4-branch@257671 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ JSTests/stress/poly-proto-setter-adds-setter-in-middle.js
@@ -0,0 +1,31 @@
+//@ requireOptions("--forcePolyProto=1", "--useLLInt=0", "--repatchBufferingCountdown=0")
+let correct = false
+
+class A {
+    set x(v) {
+        if (v == 1) {
+            Object.defineProperty(B.prototype, "x", {
+                set: function() {
+                    correct = true
+                }
+            });
+        }
+    }
+}
+
+class B extends A {
+    set y(v) {}
+    set y1(v) {}
+    set y2(v) {}
+}
+
+class C extends B {
+
+}
+
+let a = new C();
+for (let i = 0; i < 30; i++) {
+  a.x = i;
+}
+
+// FIXME: Check that correct is true: https://bugs.webkit.org/show_bug.cgi?id=208337
