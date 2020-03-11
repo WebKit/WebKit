b57034d47d634c8d2c68de80a592462fcae6c29b
Alex Christensen <achristensen@apple.com>
2020-03-11 20:32:07 +0000

    Fix the macCatalyst build after r258250
    * WebProcess/WebPage/Cocoa/TextCheckingControllerProxy.mm:
    (WebKit::TextCheckingControllerProxy::annotatedSubstringBetweenPositions):
    
    
    
    Canonical link: https://commits.webkit.org/221858@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@258280 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/WebPage/Cocoa/TextCheckingControllerProxy.mm
+++ Source/WebKit/WebProcess/WebPage/Cocoa/TextCheckingControllerProxy.mm
@@ -193,14 +193,14 @@ AttributedString TextCheckingControllerProxy::annotatedSubstringBetweenPositions
 
         [string appendAttributedString:[[[NSAttributedString alloc] initWithString:it.text().createNSStringWithoutCopying().get()] autorelease]];
 
-        RefPtr<Range> currentTextRange = it.range();
-        auto markers = document->markers().markersInRange(*currentTextRange, DocumentMarker::PlatformTextChecking);
+        SimpleRange currentTextRange = it.range();
+        auto markers = document->markers().markersInRange(createLiveRange(currentTextRange), DocumentMarker::PlatformTextChecking);
         for (const auto* marker : markers) {
             if (!WTF::holds_alternative<DocumentMarker::PlatformTextCheckingData>(marker->data()))
                 continue;
 
             auto& textCheckingData = WTF::get<DocumentMarker::PlatformTextCheckingData>(marker->data());
-            auto subrange = TextIterator::subrange(*currentTextRange, marker->startOffset(), marker->endOffset() - marker->startOffset());
+            auto subrange = TextIterator::subrange(createLiveRange(currentTextRange), marker->startOffset(), marker->endOffset() - marker->startOffset());
 
             size_t subrangeLocation;
             size_t subrangeLength;
