4ea1434ad63c89829e33a30e21ef28e37d3346e3
Keith Miller <keith_miller@apple.com>
2020-03-17 20:39:17 +0000

    Cherry-pick r258381. rdar://problem/60539195
        DFG nodes that take a TypedArray's storage need to keepAlive the TypedArray
        https://bugs.webkit.org/show_bug.cgi?id=209035
    
        Reviewed by Saam Barati.
    
        It might be possible to produce a graph where the last reference to a TypedArray
        is via a GetByVal or PutByVal. Since those nodes don't create any reference to the
        TypedArray in B3 we may end up not keeping the TypedArray alive until after the
        storage access.
    
        * ftl/FTLLowerDFGToB3.cpp:
        (JSC::FTL::DFG::LowerDFGToB3::compileAtomicsReadModifyWrite):
        (JSC::FTL::DFG::LowerDFGToB3::compileGetByVal):
        (JSC::FTL::DFG::LowerDFGToB3::compilePutByVal):
    
        Canonical link: https://commits.webkit.org/221938@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@258381 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/218903.430@safari-609-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609-branch@258582 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp
+++ Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp
@@ -3818,7 +3818,9 @@ private:
         // the typed array storage, since that's as precise of an abstraction as we can have of shared
         // array buffer storage.
         m_heaps.decorateFencedAccess(&m_heaps.typedArrayProperties, atomicValue);
-        
+
+        // We have to keep base alive since that keeps storage alive.
+        keepAlive(lowCell(baseEdge));
         setIntTypedArrayLoadResult(result, type);
     }
     
@@ -4632,6 +4634,7 @@ private:
         case Array::Uint32Array:
         case Array::Float32Array:
         case Array::Float64Array: {
+            LValue base = lowCell(m_graph.varArgChild(m_node, 0));
             LValue index = lowInt32(m_graph.varArgChild(m_node, 1));
             LValue storage = lowStorage(m_graph.varArgChild(m_node, 2));
             
@@ -4661,6 +4664,8 @@ private:
                     DFG_CRASH(m_graph, m_node, "Bad typed array type");
                 }
                 
+                // We have to keep base alive since that keeps storage alive.
+                keepAlive(base);
                 setDouble(result);
                 return;
             }
@@ -5027,6 +5032,8 @@ private:
                     m_out.appendTo(continuation, lastNext);
                 }
                 
+                // We have to keep base alive since that keeps storage alive.
+                keepAlive(base);
                 return;
             }
         }
