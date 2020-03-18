16ffcd5f13fb2ff3a1c8b97594598bb5c7e47456
Brent Fulgham <bfulgham@apple.com>
2020-03-18 17:24:01 +0000

    Switch from debug ASSERT to RELEASE_ASSERT in PluginQuirks.h https://bugs.webkit.org/show_bug.cgi?id=209213 <rdar://problem/59595834>
    Reviewed by Alex Christensen.
    
    * Shared/Plugins/PluginQuirks.h:
    (WebKit::PluginQuirks::add):
    
    
    Canonical link: https://commits.webkit.org/222158@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@258645 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/Plugins/PluginQuirks.h
+++ Source/WebKit/Shared/Plugins/PluginQuirks.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2010-2020 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,8 +23,7 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PluginQuirks_h
-#define PluginQuirks_h
+#pragma once
 
 namespace WebKit {
 
@@ -106,7 +105,7 @@ public:
     void add(PluginQuirk quirk)
     {
         ASSERT(quirk >= 0);
-        ASSERT_WITH_SECURITY_IMPLICATION(quirk < NumPluginQuirks);
+        RELEASE_ASSERT_WITH_SECURITY_IMPLICATION(quirk < NumPluginQuirks);
         
         m_quirks |= (1 << quirk);
     }
@@ -121,5 +120,3 @@ private:
 };
 
 } // namespace WebKit
-
-#endif // PluginQuirkSet_h
