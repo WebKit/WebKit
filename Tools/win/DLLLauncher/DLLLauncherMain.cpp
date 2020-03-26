e0093a85f9c4d8a11ab3a131ee79321dd1ecaac1
Commit Queue <commit-queue@webkit.org>
2020-03-26 14:31:28 +0000

    Unreviewed, reverting r259035. https://bugs.webkit.org/show_bug.cgi?id=209597
    broke windows layout-tests (Requested by aakashjain on
    #webkit).
    
    Reverted changeset:
    
    "[Win] lld-link: error: /manifestdependency: is not allowed in
    .drectve"
    https://bugs.webkit.org/show_bug.cgi?id=204831
    https://trac.webkit.org/changeset/259035
    
    Canonical link: https://commits.webkit.org/222521@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@259043 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/win/DLLLauncher/DLLLauncherMain.cpp
+++ Tools/win/DLLLauncher/DLLLauncherMain.cpp
@@ -37,6 +37,18 @@
 
 using namespace std;
 
+#if defined _M_IX86
+#define PROCESSORARCHITECTURE "x86"
+#elif defined _M_IA64
+#define PROCESSORARCHITECTURE "ia64"
+#elif defined _M_X64
+#define PROCESSORARCHITECTURE "amd64"
+#else
+#define PROCESSORARCHITECTURE "*"
+#endif
+
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='" PROCESSORARCHITECTURE "' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 static void enableTerminationOnHeapCorruption()
 {
     HEAP_INFORMATION_CLASS heapEnableTerminationOnCorruption = static_cast<HEAP_INFORMATION_CLASS>(1);
