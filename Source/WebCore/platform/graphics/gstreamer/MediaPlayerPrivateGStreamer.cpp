5f4c3edf7cb449d7550b4012eb8b46b16b5cfe1e
Miguel Gomez <magomez@igalia.com>
2020-03-27 13:14:01 +0000

    [WPE] Unnecessary gl synchronization when using an OpenMAX video decoder and GLES2 https://bugs.webkit.org/show_bug.cgi?id=209647
    Reviewed by Adrian Perez de Castro.
    
    Don't perform the call to gst_gl_sync_meta_wait_cpu() when using an OpenMAX decoder,
    as we don't need synchronization in that case and the internal call to glFinish()
    casues an important fps drop.
    
    * platform/graphics/gstreamer/GStreamerCommon.h:
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
    (WebCore::GstVideoFrameHolder::waitForCPUSync):
    (WebCore::MediaPlayerPrivateGStreamer::createGSTPlayBin):
    
    
    Canonical link: https://commits.webkit.org/222584@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@259113 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -276,6 +276,10 @@ public:
 #if USE(GSTREAMER_GL)
     virtual void waitForCPUSync()
     {
+        // No need for OpenGL synchronization when using the OpenMAX decoder.
+        if (m_videoDecoderPlatform == GstVideoDecoderPlatform::OpenMAX)
+            return;
+
         GstGLSyncMeta* meta = gst_buffer_get_gl_sync_meta(m_buffer.get());
         if (meta) {
             GstMemory* mem = gst_buffer_peek_memory(m_buffer.get(), 0);
@@ -2994,6 +2998,8 @@ void MediaPlayerPrivateGStreamer::createGSTPlayBin(const URL& url, const String&
             player->m_videoDecoderPlatform = GstVideoDecoderPlatform::Video4Linux;
         else if (g_str_has_prefix(elementName.get(), "imxvpudec"))
             player->m_videoDecoderPlatform = GstVideoDecoderPlatform::ImxVPU;
+        else if (g_str_has_prefix(elementName.get(), "omx"))
+            player->m_videoDecoderPlatform = GstVideoDecoderPlatform::OpenMAX;
 
 #if USE(TEXTURE_MAPPER_GL)
         player->updateTextureMapperFlags();
