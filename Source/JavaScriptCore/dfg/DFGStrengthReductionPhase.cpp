4445409d91ff5e3e0b52cc2d866ae7cb167d8c14
Ross Kirsling <ross.kirsling@sony.com>
2020-03-31 20:57:09 +0000

    REGRESSION: ASSERTION FAILED: regExpObjectNode in JSC::DFG::StrengthReductionPhase::handleNode https://bugs.webkit.org/show_bug.cgi?id=209824
    Reviewed by Mark Lam.
    
    * dfg/DFGStrengthReductionPhase.cpp:
    (JSC::DFG::StrengthReductionPhase::handleNode):
    It's true that we need to verify lastIndex even when a RegExp is neither global nor sticky,
    but if DFG's already converted RegExpExec to RegExpExecNonGlobalOrSticky, that means we've thrown away
    the RegExpObject node, so we shouldn't try to reverify lastIndex when we reconsider folding to constant.
    
    
    Canonical link: https://commits.webkit.org/222744@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@259310 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGStrengthReductionPhase.cpp
+++ Source/JavaScriptCore/dfg/DFGStrengthReductionPhase.cpp
@@ -535,33 +535,35 @@ private:
 
             ASSERT(m_node->op() != RegExpMatchFast);
 
-            // This will only work if we can prove what the value of lastIndex is. To do this
-            // safely, we need to execute the insertion set so that we see any previous strength
-            // reductions. This is needed for soundness since otherwise the effectfulness of any
-            // previous strength reductions would be invisible to us.
-            ASSERT(regExpObjectNode);
-            executeInsertionSet();
             unsigned lastIndex = UINT_MAX;
-            for (unsigned otherNodeIndex = m_nodeIndex; otherNodeIndex--;) {
-                Node* otherNode = m_block->at(otherNodeIndex);
-                if (otherNode == regExpObjectNode) {
-                    lastIndex = 0;
+            if (m_node->op() != RegExpExecNonGlobalOrSticky) {
+                // This will only work if we can prove what the value of lastIndex is. To do this
+                // safely, we need to execute the insertion set so that we see any previous strength
+                // reductions. This is needed for soundness since otherwise the effectfulness of any
+                // previous strength reductions would be invisible to us.
+                ASSERT(regExpObjectNode);
+                executeInsertionSet();
+                for (unsigned otherNodeIndex = m_nodeIndex; otherNodeIndex--;) {
+                    Node* otherNode = m_block->at(otherNodeIndex);
+                    if (otherNode == regExpObjectNode) {
+                        lastIndex = 0;
+                        break;
+                    }
+                    if (otherNode->op() == SetRegExpObjectLastIndex
+                        && otherNode->child1() == regExpObjectNode
+                        && otherNode->child2()->isInt32Constant()
+                        && otherNode->child2()->asInt32() >= 0) {
+                        lastIndex = otherNode->child2()->asUInt32();
+                        break;
+                    }
+                    if (writesOverlap(m_graph, otherNode, RegExpObject_lastIndex))
+                        break;
+                }
+                if (lastIndex == UINT_MAX) {
+                    if (verbose)
+                        dataLog("Giving up because the last index is not known.\n");
                     break;
                 }
-                if (otherNode->op() == SetRegExpObjectLastIndex
-                    && otherNode->child1() == regExpObjectNode
-                    && otherNode->child2()->isInt32Constant()
-                    && otherNode->child2()->asInt32() >= 0) {
-                    lastIndex = static_cast<unsigned>(otherNode->child2()->asInt32());
-                    break;
-                }
-                if (writesOverlap(m_graph, otherNode, RegExpObject_lastIndex))
-                    break;
-            }
-            if (lastIndex == UINT_MAX) {
-                if (verbose)
-                    dataLog("Giving up because the last index is not known.\n");
-                break;
             }
             if (!regExp->globalOrSticky())
                 lastIndex = 0;
