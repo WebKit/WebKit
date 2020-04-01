e80b0ee47a2cac8e3f79208becebaf55e210d9ca
Don Olmstead <don.olmstead@sony.com>
2020-04-01 00:12:10 +0000

    [PlayStation] Fix build breaks after r259112 https://bugs.webkit.org/show_bug.cgi?id=209830
    Unreviewed build fix.
    
    Source/WebCore:
    
    Add USE(GLIB) guards around RunLoopSourcePriority usage.
    
    
    * platform/ScrollAnimationKinetic.cpp:
    (WebCore::ScrollAnimationKinetic::ScrollAnimationKinetic):
    
    Source/WebKit:
    
    Replace PLATFORM(WPE) with USE(LIBWPE) within WebWheelEvent.
    
    
    * Shared/WebEvent.h:
    * Shared/WebWheelEvent.cpp:
    (WebKit::WebWheelEvent::encode const):
    (WebKit::WebWheelEvent::decode):
    
    
    Canonical link: https://commits.webkit.org/222757@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@259325 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/WebWheelEvent.cpp
+++ Source/WebKit/Shared/WebWheelEvent.cpp
@@ -65,7 +65,7 @@ WebWheelEvent::WebWheelEvent(Type type, const IntPoint& position, const IntPoint
 {
     ASSERT(isWheelEventType(type));
 }
-#elif PLATFORM(GTK) || PLATFORM(WPE)
+#elif PLATFORM(GTK) || USE(LIBWPE)
 WebWheelEvent::WebWheelEvent(Type type, const IntPoint& position, const IntPoint& globalPosition, const FloatSize& delta, const FloatSize& wheelTicks, Phase phase, Phase momentumPhase, Granularity granularity, OptionSet<Modifier> modifiers, WallTime timestamp)
     : WebEvent(type, modifiers, timestamp)
     , m_position(position)
@@ -91,7 +91,7 @@ void WebWheelEvent::encode(IPC::Encoder& encoder) const
     encoder << m_wheelTicks;
     encoder << m_granularity;
     encoder << m_directionInvertedFromDevice;
-#if PLATFORM(COCOA) || PLATFORM(GTK) || PLATFORM(WPE)
+#if PLATFORM(COCOA) || PLATFORM(GTK) || USE(LIBWPE)
     encoder << m_phase;
     encoder << m_momentumPhase;
 #endif
@@ -118,7 +118,7 @@ bool WebWheelEvent::decode(IPC::Decoder& decoder, WebWheelEvent& t)
         return false;
     if (!decoder.decode(t.m_directionInvertedFromDevice))
         return false;
-#if PLATFORM(COCOA) || PLATFORM(GTK) || PLATFORM(WPE)
+#if PLATFORM(COCOA) || PLATFORM(GTK) || USE(LIBWPE)
     if (!decoder.decode(t.m_phase))
         return false;
     if (!decoder.decode(t.m_momentumPhase))
