bcc21f8d8c84306742f0a7604a552f1a9394b827
Alex Christensen <achristensen@webkit.org>
2020-04-01 16:21:37 +0000

    Deprecate WKWebsiteDataStore._indexedDBDatabaseDirectory https://bugs.webkit.org/show_bug.cgi?id=209243
    Patch by Alex Christensen <achristensen@webkit.org> on 2020-04-01
    Reviewed by David Kilzer.
    
    Luckily it's read-only, but we have a way to get the configuration, so let's encourage that instead.
    
    * UIProcess/API/Cocoa/WKWebsiteDataStorePrivate.h:
    
    Canonical link: https://commits.webkit.org/222780@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@259350 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/Cocoa/WKWebsiteDataStorePrivate.h
+++ Source/WebKit/UIProcess/API/Cocoa/WKWebsiteDataStorePrivate.h
@@ -56,8 +56,8 @@ typedef NS_OPTIONS(NSUInteger, _WKWebsiteDataStoreFetchOptions) {
 @property (nonatomic, setter=_setAllowsCellularAccess:) BOOL _allowsCellularAccess WK_API_DEPRECATED_WITH_REPLACEMENT("_WKWebsiteDataStoreConfiguration.allowsCellularAccess", macos(10.13.4, WK_MAC_TBA), ios(11.3, WK_IOS_TBA));
 @property (nonatomic, setter=_setProxyConfiguration:) NSDictionary *_proxyConfiguration WK_API_DEPRECATED_WITH_REPLACEMENT("_WKWebsiteDataStoreConfiguration.proxyConfiguration", macos(10.14, WK_MAC_TBA), ios(12.0, WK_IOS_TBA));
 @property (nonatomic, setter=_setAllowsTLSFallback:) BOOL _allowsTLSFallback WK_API_AVAILABLE(macos(10.15), ios(13.0));
+@property (nonatomic, readonly) NSURL *_indexedDBDatabaseDirectory WK_API_DEPRECATED_WITH_REPLACEMENT("_WKWebsiteDataStoreConfiguration._indexedDBDatabaseDirectory", macos(10.11, WK_MAC_TBA), ios(9.0, WK_IOS_TBA));
 
-@property (nonatomic, readonly) NSURL *_indexedDBDatabaseDirectory;
 
 - (void)_setResourceLoadStatisticsTestingCallback:(nullable void (^)(WKWebsiteDataStore *, NSString *))callback WK_API_AVAILABLE(macos(10.13), ios(11.0));
 - (void)_getAllStorageAccessEntriesFor:(WKWebView *)webView completionHandler:(void (^)(NSArray<NSString *> *domains))completionHandler WK_API_AVAILABLE(macos(10.14), ios(12.0));
