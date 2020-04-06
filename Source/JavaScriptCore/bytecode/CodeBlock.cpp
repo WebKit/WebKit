7ea72f361359cca8010f8c3aaf82150c6ad0e359
Fujii Hironori <Hironori.Fujii@sony.com>
2020-04-06 20:07:45 +0000

    [Clang 10] Fix -Wimplicit-int-float-conversion compilation warnings in JavaScriptCore https://bugs.webkit.org/show_bug.cgi?id=210038
    Reviewed by Darin Adler.
    
    Clang 10 reports a compilation warning in JavaScriptCore:
    > ..\..\Source\JavaScriptCore\bytecode/CodeBlock.cpp(3002,24): warning: implicit conversion from 'unsigned long long' to 'double' changes value from 18446744073709551615 to 18446744073709551616 [-Wimplicit-int-float-conversion]
    >     if (doubleResult > std::numeric_limits<size_t>::max())
    >                      ~ ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Use a template variable maxPlusOne<T> which was added by r259537
    for the purpose.
    
    * bytecode/CodeBlock.cpp:
    (JSC::CodeBlock::predictedMachineCodeSize): Replaced '>' with '>=',
    and std::numeric_limits<size_t>::max() with maxPlusOne<size_t>.
    
    Canonical link: https://commits.webkit.org/222979@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@259588 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/bytecode/CodeBlock.cpp
+++ Source/JavaScriptCore/bytecode/CodeBlock.cpp
@@ -3001,7 +3001,7 @@ size_t CodeBlock::predictedMachineCodeSize()
     // the function is so huge that we can't even fit it into virtual memory then we
     // should probably have some other guards in place to prevent us from even getting
     // to this point.
-    if (doubleResult > std::numeric_limits<size_t>::max())
+    if (doubleResult >= maxPlusOne<size_t>)
         return 0;
     
     return static_cast<size_t>(doubleResult);
