947878dafede2c450ae87c3e23eb6a072636df61
Jer Noble <jer.noble@apple.com>
2020-04-06 20:48:29 +0000

    [ Mac wk2 ] http/tests/media/track-in-band-hls-metadata.html is flaky crashing. https://bugs.webkit.org/show_bug.cgi?id=209490 <rdar://problem/60837555>
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    To ensure the TaskDispatcher doesn't get destroyed on a background thread at the same time it's
    executing tasks on the main thread, when a GenericTaskQueue is destroyed on a background thread,
    move the TaskDispatcher into a task, and use the dispatcher itself to destroy itself on the
    main thread.
    
    * platform/GenericTaskQueue.h:
    (WebCore::GenericTaskQueue::GenericTaskQueue):
    (WebCore::GenericTaskQueue::~GenericTaskQueue):
    (WebCore::GenericTaskQueue::enqueueTask):
    
    LayoutTests:
    
    * platform/mac-wk2/TestExpectations:
    
    
    Canonical link: https://commits.webkit.org/222984@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@259593 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/GenericTaskQueue.h
+++ Source/WebCore/platform/GenericTaskQueue.h
@@ -28,6 +28,8 @@
 #include "Timer.h"
 #include <wtf/Deque.h>
 #include <wtf/Function.h>
+#include <wtf/MainThread.h>
+#include <wtf/UniqueRef.h>
 #include <wtf/WeakPtr.h>
 
 namespace WTF {
@@ -38,6 +40,7 @@ namespace WebCore {
 
 template <typename T>
 class TaskDispatcher {
+    WTF_MAKE_FAST_ALLOCATED;
 public:
     explicit TaskDispatcher(T* context)
         : m_context(context)
@@ -56,6 +59,7 @@ private:
 
 template<>
 class TaskDispatcher<Timer> : public CanMakeWeakPtr<TaskDispatcher<Timer>> {
+    WTF_MAKE_FAST_ALLOCATED;
 public:
     TaskDispatcher();
     void postTask(Function<void()>&&);
@@ -76,19 +80,25 @@ class GenericTaskQueue : public CanMakeWeakPtr<GenericTaskQueue<T>> {
     WTF_MAKE_FAST_ALLOCATED;
 public:
     GenericTaskQueue()
-        : m_dispatcher()
+        : m_dispatcher(makeUniqueRef<TaskDispatcher<T>>())
     {
     }
 
     explicit GenericTaskQueue(T& t)
-        : m_dispatcher(&t)
+        : m_dispatcher(makeUniqueRef<TaskDispatcher<T>>(&t))
     {
     }
 
     explicit GenericTaskQueue(T* t)
-        : m_dispatcher(t)
+        : m_dispatcher(makeUniqueRef<TaskDispatcher<T>>(t))
         , m_isClosed(!t)
     {
+    }
+
+    ~GenericTaskQueue()
+    {
+        if (!isMainThread())
+            m_dispatcher->postTask([dispatcher = WTFMove(m_dispatcher)] { });
     }
 
     typedef WTF::Function<void ()> TaskFunction;
@@ -99,7 +109,7 @@ public:
             return;
 
         ++m_pendingTasks;
-        m_dispatcher.postTask([weakThis = makeWeakPtr(*this), task = WTFMove(task)] {
+        m_dispatcher->postTask([weakThis = makeWeakPtr(*this), task = WTFMove(task)] {
             if (!weakThis)
                 return;
             ASSERT(weakThis->m_pendingTasks);
@@ -124,7 +134,7 @@ public:
     bool isClosed() const { return m_isClosed; }
 
 private:
-    TaskDispatcher<T> m_dispatcher;
+    UniqueRef<TaskDispatcher<T>> m_dispatcher;
     unsigned m_pendingTasks { 0 };
     bool m_isClosed { false };
 };
