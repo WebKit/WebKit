a9bd6bd598a13097bf9e566cfde272bc35933e48
Brian Burg <bburg@apple.com>
2020-04-07 20:07:46 +0000

    Web Automation: Automation.inspectBrowsingContext should bring Web Inspector to front automatically https://bugs.webkit.org/show_bug.cgi?id=210137
    Reviewed by Joseph Pecoraro.
    
    * UIProcess/Automation/mac/WebAutomationSessionMac.mm:
    (WebKit::WebAutomationSession::inspectBrowsingContext):
    Previously, calling connect() would preload Web Inspector but not show its window. This
    made it awkward to use the 'safari:automaticInspection' capability without subsequently
    evaluating a `debugger;` statement to bring Web Inspector to front.
    
    
    Canonical link: https://commits.webkit.org/223037@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@259665 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/Automation/mac/WebAutomationSessionMac.mm
+++ Source/WebKit/UIProcess/Automation/mac/WebAutomationSessionMac.mm
@@ -57,7 +57,7 @@ void WebAutomationSession::inspectBrowsingContext(const String& handle, const bo
     // Don't bring the inspector to front since this may be done automatically.
     // We just want it loaded so it can pause if a breakpoint is hit during a command.
     if (page->inspector()) {
-        page->inspector()->connect();
+        page->inspector()->show();
 
         // Start collecting profile information immediately so the entire session is captured.
         if (optionalEnableAutoCapturing && *optionalEnableAutoCapturing)
