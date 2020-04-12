b561b398f705c94d82a970a98eec2c6b6cee6a7e
Michael Catanzaro <mcatanzaro@gnome.org>
2020-04-12 13:03:07 +0000

    Merge r258808 - [GTK] Use ${PYTHON_EXECUTABLE} to run generate-gtkdoc https://bugs.webkit.org/show_bug.cgi?id=208970
    Patch by Michael Catanzaro <mcatanzaro@gnome.org> on 2020-03-21
    Reviewed by Konstantin Tokarev.
    
    .:
    
    There's no need to rely on shebangs when executing python scripts from CMake. This should
    ensure the script always works even if the shebang does not.
    
    * Source/cmake/GtkDoc.cmake:
    
    Tools:
    
    We are allowed to use /usr/bin/env in this script because it's not installed (so not
    packaged). Therefore we can make the shebang compatible with FreeBSD in this particularl
    case.
    
    * gtkdoc/generate-gtkdoc:

--- Source/cmake/GtkDoc.cmake
+++ Source/cmake/GtkDoc.cmake
@@ -4,7 +4,7 @@ macro(ADD_GTKDOC_GENERATOR _stamp_name _extra_args)
     add_custom_command(
         OUTPUT "${CMAKE_BINARY_DIR}/${_stamp_name}"
         DEPENDS ${DocumentationDependencies}
-        COMMAND ${CMAKE_COMMAND} -E env "CC=${CMAKE_C_COMPILER}" "CFLAGS=${CMAKE_C_FLAGS} -Wno-unused-parameter" "LDFLAGS=${CMAKE_EXE_LINKER_FLAGS}" ${CMAKE_SOURCE_DIR}/Tools/gtkdoc/generate-gtkdoc ${_extra_args}
+        COMMAND ${CMAKE_COMMAND} -E env "CC=${CMAKE_C_COMPILER}" "CFLAGS=${CMAKE_C_FLAGS} -Wno-unused-parameter" "LDFLAGS=${CMAKE_EXE_LINKER_FLAGS}" ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/Tools/gtkdoc/generate-gtkdoc ${_extra_args}
         COMMAND touch ${_stamp_name}
         WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
         VERBATIM
