9392d3f17546956856f863b4c3be059728f98c56
Adrian Perez de Castro <aperez@igalia.com>
2020-04-17 09:35:49 +0000

    Unreviewed build fix after r260123
    No new tests needed.
    
    * platform/gtk/CursorGtk.cpp:
    (WebCore::createCustomCursor): Pass missing pixel buffer data pointer to gdk_memory_texture_new().
    
    Canonical link: https://commits.webkit.org/223515@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@260242 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/gtk/CursorGtk.cpp
+++ Source/WebCore/platform/gtk/CursorGtk.cpp
@@ -63,7 +63,8 @@ static GRefPtr<GdkCursor> createCustomCursor(Image* image, const IntPoint& hotSp
 
 #if USE(GTK4)
     ASSERT(cairo_image_surface_get_format(surface.get()) == CAIRO_FORMAT_ARGB32);
-    auto texture = adoptGRef(gdk_memory_texture_new(cairo_image_surface_get_width(surface.get()), cairo_image_surface_get_height(surface.get()), GDK_MEMORY_A8R8G8B8_PREMULTIPLIED, cairo_image_surface_get_stride(surface.get())));
+    auto texture = adoptGRef(gdk_memory_texture_new(cairo_image_surface_get_width(surface.get()), cairo_image_surface_get_height(surface.get()),
+        GDK_MEMORY_A8R8G8B8_PREMULTIPLIED, cairo_image_surface_get_data(surface.get()), cairo_image_surface_get_stride(surface.get())));
     return adoptGRef(gdk_cursor_new_from_texture(texture.get(), effectiveHotSpot.x(), effectiveHotSpot.y(), fallbackCursor().get()));
 #else
     return adoptGRef(gdk_cursor_new_from_surface(gdk_display_get_default(), surface.get(), effectiveHotSpot.x(), effectiveHotSpot.y()));
