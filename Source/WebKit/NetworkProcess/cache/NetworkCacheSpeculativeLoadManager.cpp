011f4a8009ce0ea9b3572c688aa8b9df71fd1b8a
Alex Christensen <achristensen@webkit.org>
2020-04-20 20:04:49 +0000

    When SpeculativeLoadManager is destroyed, properly clean up its PendingFrameLoads https://bugs.webkit.org/show_bug.cgi?id=210759 <rdar://problem/62056856>
    Patch by Alex Christensen <achristensen@webkit.org> on 2020-04-20
    Reviewed by Darin Adler.
    
    Recent work on the resourceLoadStatistics layout tests increased the amount we swap out the WebsiteDataStore.
    When this happens, the NetworkSession is eventually destroyed in the NetworkProcess, sometimes when running the next test.
    An assertion was firing in the PendingFrameLoad destructor because it hadn't been marked as complete when it was destroyed.
    Rather than remove the assertion, when we destroy the SpeculativeLoadManager (which only happens when a WebsiteDataStore
    is destroyed) during a speculative pending frame load, just mark the pending frame load as complete because it is being cancelled.
    Marking the pending frame load as complete can tell the SpeculativeLoadManager to mutate m_pendingFrameLoads, which we don't want
    to do while iterating, so copy the RefPtrs into a Vector first then iterate that to get them all.
    
    This fixes an assertion that was sometimes hit in http/tests/resourceLoadStatistics/strip-referrer-to-origin-for-prevalent-subresource-redirects-database.html
    but only after running other tests that had initiated speculative pending frame loads.  This was ostensibly started by r260322 but is quite unrelated.
    
    * NetworkProcess/cache/NetworkCacheSpeculativeLoadManager.cpp:
    (WebKit::NetworkCache::SpeculativeLoadManager::~SpeculativeLoadManager):
    
    Canonical link: https://commits.webkit.org/223647@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@260390 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/NetworkProcess/cache/NetworkCacheSpeculativeLoadManager.cpp
+++ Source/WebKit/NetworkProcess/cache/NetworkCacheSpeculativeLoadManager.cpp
@@ -267,6 +267,10 @@ SpeculativeLoadManager::SpeculativeLoadManager(Cache& cache, Storage& storage)
 
 SpeculativeLoadManager::~SpeculativeLoadManager()
 {
+    for (auto& pendingFrameLoad : copyToVector(m_pendingFrameLoads.values())) {
+        if (pendingFrameLoad)
+            pendingFrameLoad->markLoadAsCompleted();
+    }
 }
 
 bool SpeculativeLoadManager::canUsePreloadedEntry(const PreloadedEntry& entry, const ResourceRequest& actualRequest)
