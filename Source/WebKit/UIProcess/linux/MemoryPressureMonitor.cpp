WebKit/WebKit

commit 3f538e9b3caa68eeef728d601dab92af95e9ab5a

[GTK][WPE] lowWatermarkPages() in MemoryPressureMonitor.cpp only searches the "low" value inside the first "Node" section https://bugs.webkit.org/show_bug.cgi?id=210345
Reviewed by Adrian Perez de Castro.

* UIProcess/linux/MemoryPressureMonitor.cpp:
(WebKit::lowWatermarkPages):


Canonical link: https://commits.webkit.org/223619@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@260359 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/linux/MemoryPressureMonitor.cpp
@@ -66,9 +66,48 @@ static const unsigned maxCgroupPath = 4096; // PATH_MAX = 4096 from (Linux) incl
 #define STRINGIFY(val) STRINGIFY_EXPANDED(val)
 #define ZONEINFO_TOKEN_BUFFER_SIZE 128
 
+// The lowWatermark is the sum of the low watermarks across all zones as the
+// MemAvailable info was implemented in /proc/meminfo since version 3.14 of the
+// kernel (added by commit 34e431b0a, source git.kernel.org):
+//
+// MemAvailable: An estimate of how much memory is available for starting new
+//               applications, without swapping. Calculated from MemFree,
+//               SReclaimable, the size of the file LRU lists, and the low
+//               watermarks in each zone.
+//               The estimate takes into account that the system needs some
+//               page cache to function well, and that not all reclaimable
+//               slab will be reclaimable, due to items being in use. The
+//               impact of those factors will vary from system to system.
+//
+// The fscanf() reads the input stream file until the argument list passed as
+// parameters is successfully filled.
+//
+// In our immplemetation the `while (!feof(zoneInfoFile))` loop follows the next
+// logic:
+//
+// - the first `fscanf(zoneInfoFile, " Node %*u, zone %...[^\n]\n", buffer);`
+//   iterates the `Node` sections.
+// - Then, when we found a Normal node, we start to read each single
+//   `fscanf(zoneInfoFile, "%...s", buffer);` until find a `low` token.
+// - We read the next token which is the actual `low` value and we add it to the
+//   `sumLow` summation.
+//
+// The second fscanf() reads tokens one by one because the format of each row is
+// not homogeneous (2, 3 or 6 values):
+//
+//   Node 0, zone   Normal
+//     pages free     27303
+//           min      20500
+//           low      24089
+//           high     27678
+//           spanned  3414016
+//           present  3414016
+//           managed  3337293
+//           protection: (0, 0, 0, 0, 0)
 static size_t lowWatermarkPages(FILE* zoneInfoFile)
 {
     size_t low = 0;
+    size_t sumLow = 0;
     char buffer[ZONEINFO_TOKEN_BUFFER_SIZE + 1];
     bool inNormalZone = false;
 
@@ -83,11 +122,13 @@ static size_t lowWatermarkPages(FILE* zoneInfoFile)
         r = fscanf(zoneInfoFile, "%" STRINGIFY(ZONEINFO_TOKEN_BUFFER_SIZE) "s", buffer);
         if (r == 1 && inNormalZone && !strcmp(buffer, "low")) {
             r = fscanf(zoneInfoFile, "%zu", &low);
-            if (r == 1)
-                break;
+            if (r == 1) {
+                sumLow += low;
+                continue;
+            }
         }
     }
-    return low;
+    return sumLow;
 }
 
 static inline size_t systemPageSize()

commit 1caf8099c50a3dab96167dc732bc97b108ca79f2

Regression after r260359 ([GTK][WPE] lowWatermarkPages() in MemoryPressureMonitor.cpp only searches the "low" value inside the first "Node" section) https://bugs.webkit.org/show_bug.cgi?id=210916
Reviewed by Carlos Alberto Lopez Perez.

Switch over to using a FileHandle type based on std::unique_ptr with a custom deleter and an
utility function for opening files. This makes easier to follow the logic inside the polling
loop for opening files and retrying when needed. This also fixes exiting the thread when the
systemMemoryUsedAsPercentage() function would return -1, to loop restart instead to keep
trying.

Thanks to Pablo Saavedra for his help in making this patch.

No new tests needed.

* UIProcess/linux/MemoryPressureMonitor.cpp:
(WebKit::FileHandleDeleter::operator()): Add deleter to use with std::unique_ptr<>.
(WebKit::tryOpeningForUnbufferedReading): Add utility function to open a file handle if
needed and configuring its buffering upon opening.
(WebKit::MemoryPressureMonitor::start): Use FileHandle to ensure that handles are always
closed properly, and fix logic retry opening files on failure.


Canonical link: https://commits.webkit.org/223819@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@260592 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/linux/MemoryPressureMonitor.cpp
@@ -297,6 +297,29 @@ MemoryPressureMonitor& MemoryPressureMonitor::singleton()
     return memoryMonitor;
 }
 
+struct FileHandleDeleter {
+    void operator()(FILE* f) { fclose(f); }
+};
+
+using FileHandle = std::unique_ptr<FILE, FileHandleDeleter>;
+
+static bool tryOpeningForUnbufferedReading(FileHandle& handle, const char* filePath)
+{
+    // Check whether the file handle is already valid.
+    if (handle)
+        return true;
+
+    // Else, try opening it and disable buffering after opening.
+    if (auto* f = fopen(filePath, "r")) {
+        setbuf(f, nullptr);
+        handle.reset(f);
+        return true;
+    }
+
+    // Could not produce a valid handle.
+    return false;
+}
+
 void MemoryPressureMonitor::start()
 {
     if (m_started)
@@ -305,31 +328,27 @@ void MemoryPressureMonitor::start()
     m_started = true;
 
     Thread::create("MemoryPressureMonitor", [] {
-        FILE* memInfoFile;
-        FILE* zoneInfoFile;
-        FILE* cgroupControllerFile;
+        FileHandle memInfoFile, zoneInfoFile, cgroupControllerFile;
         CGroupMemoryController memoryController = CGroupMemoryController();
         Seconds pollInterval = s_maxPollingInterval;
         while (true) {
             sleep(pollInterval);
 
-            memInfoFile = fopen(s_procMeminfo, "r");
-            zoneInfoFile = fopen(s_procZoneinfo, "r");
-            if (!memInfoFile || !zoneInfoFile)
+            // Cannot operate without this one, retry opening on the next iteration after sleeping.
+            if (!tryOpeningForUnbufferedReading(memInfoFile, s_procMeminfo))
                 continue;
-            setbuf(memInfoFile, nullptr);
-            setbuf(zoneInfoFile, nullptr);
 
-            cgroupControllerFile = fopen(s_procSelfCgroup, "r");
-            if (cgroupControllerFile)
-                setbuf(cgroupControllerFile, nullptr);
+            // The monitor can work without these two, but it will be more precise if thy are eventually opened: keep trying.
+            tryOpeningForUnbufferedReading(zoneInfoFile, s_procZoneinfo);
+            tryOpeningForUnbufferedReading(cgroupControllerFile, s_procSelfCgroup);
 
-            CString cgroupMemoryControllerPath = getCgroupControllerPath(cgroupControllerFile, "memory");
+            CString cgroupMemoryControllerPath = getCgroupControllerPath(cgroupControllerFile.get(), "memory");
             memoryController.setMemoryControllerPath(cgroupMemoryControllerPath);
-            int usedPercentage = systemMemoryUsedAsPercentage(memInfoFile, zoneInfoFile, &memoryController);
+            int usedPercentage = systemMemoryUsedAsPercentage(memInfoFile.get(), zoneInfoFile.get(), &memoryController);
             if (usedPercentage == -1) {
                 WTFLogAlways("Failed to get the memory usage");
-                break;
+                pollInterval = s_maxPollingInterval;
+                continue;
             }
 
             if (usedPercentage >= s_memoryPresurePercentageThreshold) {
@@ -339,12 +358,6 @@ void MemoryPressureMonitor::start()
             }
             pollInterval = pollIntervalForUsedMemoryPercentage(usedPercentage);
         }
-        if (memInfoFile)
-            fclose(memInfoFile);
-        if (zoneInfoFile)
-            fclose(zoneInfoFile);
-        if (cgroupControllerFile)
-            fclose(cgroupControllerFile);
     })->detach();
 }
 