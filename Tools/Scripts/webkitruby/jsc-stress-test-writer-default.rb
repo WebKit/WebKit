4cfe96b1492dc1bf5029c9189de731390919de4a
Yusuke Suzuki <ysuzuki@apple.com>
2020-04-24 00:12:00 +0000

    Support `--report-execution-time` to report execution time for each JSC stress test https://bugs.webkit.org/show_bug.cgi?id=210938
    Reviewed by Saam Barati.
    
    We can run `run-javascriptcore-tests` with `--report-execution-time` option to report execution time for each JSC stress test,
    to figure out which test is taking a long time. It appends execution-time to the verbose log. To see it stderr, --verbose is also
    required.
    
    $ run-javascriptcore-tests .... --verbose --report-execution-time
    
    * Scripts/run-javascriptcore-tests:
    (runJSCStressTests):
    * Scripts/run-jsc-stress-tests:
    * Scripts/webkitruby/jsc-stress-test-writer-default.rb:
    
    Canonical link: https://commits.webkit.org/223833@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@260610 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitruby/jsc-stress-test-writer-default.rb
+++ Tools/Scripts/webkitruby/jsc-stress-test-writer-default.rb
@@ -263,8 +263,12 @@ class Plan
     end
     
     def successCommand
-        if $progressMeter or $verbosity >= 2
-            "rm -f #{failFile} ; echo PASS: #{Shellwords.shellescape(@name)}"
+        executionTimeMessage = ""
+        if $reportExecutionTime
+            executionTimeMessage = " $(($SECONDS - $START_TIME))s"
+        end
+        if $progressMeter or $reportExecutionTime or $verbosity >= 2
+            "rm -f #{failFile} ; echo PASS: #{Shellwords.shellescape(@name)}#{executionTimeMessage}"
         else
             "rm -f #{failFile}"
         end
@@ -277,6 +281,9 @@ class Plan
     def writeRunScript(filename)
         File.open(filename, "w") {
             | outp |
+            if $reportExecutionTime
+                outp.puts "START_TIME=$SECONDS"
+            end
             outp.puts "echo Running #{Shellwords.shellescape(@name)}"
             cmd  = "(" + shellCommand + " || (echo $? > #{failFile})) 2>&1 "
             cmd += @outputHandler.call(@name)
