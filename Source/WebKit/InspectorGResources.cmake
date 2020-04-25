915abbb8a4d17fb270697375703349b835a506d7
Diego Pino Garcia <dpino@igalia.com>
2020-04-25 08:56:11 +0000

    Source/WebInspectorUI: REGRESSION(210942): [GTK][WPE] Unreviewed, EWS build bots fail in compile-webkit step https://bugs.webkit.org/show_bug.cgi?id=211014
    * CMakeLists.txt: Remove creation of file 'inspector-resources.stamp'.
    
    Source/WebKit:
    REGRESSION(210942): [GTK][WPE] EWS build bots fail in compile-webkit step
    https://bugs.webkit.org/show_bug.cgi?id=211014
    
    Make command that generates 'InspectorGResourceBundle.xml' depend on target
    WebInspectorUI, instead of file 'inspector-resources.stamp'.
    
    * InspectorGResources.cmake:
    
    Canonical link: https://commits.webkit.org/223897@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@260696 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/InspectorGResources.cmake
+++ Source/WebKit/InspectorGResources.cmake
@@ -1,7 +1,7 @@
 macro(WEBKIT_BUILD_INSPECTOR_GRESOURCES _derived_sources_dir)
     add_custom_command(
         OUTPUT ${_derived_sources_dir}/InspectorGResourceBundle.xml
-        DEPENDS ${CMAKE_BINARY_DIR}/inspector-resources.stamp
+        DEPENDS WebInspectorUI
                 ${TOOLS_DIR}/glib/generate-inspector-gresource-manifest.py
         COMMAND ${PYTHON_EXECUTABLE} ${TOOLS_DIR}/glib/generate-inspector-gresource-manifest.py --input=${_derived_sources_dir}/InspectorResources --output=${_derived_sources_dir}/InspectorGResourceBundle.xml
         VERBATIM
