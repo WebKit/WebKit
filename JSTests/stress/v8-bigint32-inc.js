2e141f4da7759ec4ee187ab0e36a11e12b2adec2
Caio Araujo Neponoceno de Lima <ticaiolima@gmail.com>
2020-04-30 18:41:03 +0000

    Investigate stress/v8-bitint32-inc.js failure on mips https://bugs.webkit.org/show_bug.cgi?id=210907
    Unreviewed test gardening.
    
    
    * stress/v8-bigint32-inc.js:
    
    
    Canonical link: https://commits.webkit.org/224141@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@260957 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/v8-bigint32-inc.js
+++ JSTests/stress/v8-bigint32-inc.js
@@ -1,4 +1,3 @@
-//@ skip if ["mips"].include?($architecture)
 // Copyright 2017, the V8 project authors. All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
