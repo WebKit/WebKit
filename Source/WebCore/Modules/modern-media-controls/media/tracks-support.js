71deb7976a63802311bd43cf4b19847ef6058989
Antoine Quint <graouts@apple.com>
2020-05-04 15:28:46 +0000

    Media controls tracks menu shows "Auto" selected instead of track selected via the JS API https://bugs.webkit.org/show_bug.cgi?id=211230 <rdar://problem/62648409>
    Patch by Antoine Quint <graouts@apple.com> on 2020-05-04
    Reviewed by Eric Carlson.
    
    Source/WebCore:
    
    Test: media/modern-media-controls/tracks-support/tracks-support-text-track-selected-via-media-api.html
    
    We're fixing two issues with the captions menu on macOS here.
    
    First, if a text track was marked as "showing" with the JS API, we would not show it as selected in the UI
    because MediaControlsHost would report that the captionDisplayMode was "automatic" and we'd take this as
    sufficient data to say that the "Automatic (Recommended)" item should be shown as selected. We now only
    do this if we also don't have any text tracks set as "showing".
    
    The second issue was when trying to select "Automatic (Recommended)" when a text track had been marked as
    "showing" with the JS API. Calling `setSelectedTextTrack()` on MediaControlsHost in this case was not sufficient
    because HTMLMediaElement::setSelectedTextTrack is a no-op if the automatic text track is provided but captionDisplayMode
    is still set to "automatic". To address this, we first disable all text tracks before calling `setSelectedTextTrack()`.
    
    * Modules/modern-media-controls/media/tracks-support.js:
    (TracksSupport.prototype.tracksPanelIsTrackInSectionSelected):
    (TracksSupport.prototype.tracksPanelSelectionDidChange):
    
    LayoutTests:
    
    Add a test that:
    
    - sets an embedded text track as selected via the HTMLMediaElement TextTrack APIs,
    - brings up the tracks panel,
    - dumps its initial UI state, checking that the track selected via the API shows as selected in the UI,
    - selects the "Automatic (Recommended)" track,
    - brings up the tracks panel again,
    - dumps its current UI state, checking that the "Automatic (Recommended)" shows as selected in the UI.
    
    * media/modern-media-controls/tracks-support/tracks-support-text-track-selected-via-media-api-expected.txt: Added.
    * media/modern-media-controls/tracks-support/tracks-support-text-track-selected-via-media-api.html: Added.
    
    Canonical link: https://commits.webkit.org/224259@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@261084 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/modern-media-controls/media/tracks-support.js
+++ Source/WebCore/Modules/modern-media-controls/media/tracks-support.js
@@ -101,11 +101,13 @@ class TracksSupport extends MediaControllerSupport
         if (sectionIndex == 0 && this._canPickAudioTracks())
             return this._audioTracks()[trackIndex].enabled;
 
-        const trackItem = this._textTracks()[trackIndex];
+        const textTracks = this._textTracks();
+        const trackItem = textTracks[trackIndex];
         const host = this.mediaController.host;
-        const usesAutomaticTrack = host ? host.captionDisplayMode === "automatic" : false;
+        const allTracksDisabled = textTracks.every(track => track.mode === "disabled");
+        const usesAutomaticTrack = host ? (host.captionDisplayMode === "automatic" && allTracksDisabled) : false;
 
-        if (host && trackItem === host.captionMenuOffItem && (host.captionDisplayMode === "forced-only" || host.captionDisplayMode === "manual"))
+        if (allTracksDisabled && host && trackItem === host.captionMenuOffItem && (host.captionDisplayMode === "forced-only" || host.captionDisplayMode === "manual"))
             return true;
         if (host && trackItem === host.captionMenuAutomaticItem && usesAutomaticTrack)
             return true;
@@ -116,9 +118,10 @@ class TracksSupport extends MediaControllerSupport
     {
         if (sectionIndex == 0 && this._canPickAudioTracks())
             this._audioTracks().forEach((audioTrack, index) => audioTrack.enabled = index === trackIndex);
-        else if (this.mediaController.host)
+        else if (this.mediaController.host) {
+            this._textTracks().forEach(textTrack => textTrack.mode = "disabled");
             this.mediaController.host.setSelectedTextTrack(this._textTracks()[trackIndex]);
-        else
+        } else
             this._textTracks().forEach((textTrack, index) => textTrack.mode = index === trackIndex ? "showing" : "disabled");
 
         this.mediaController.controls.hideTracksPanel();
