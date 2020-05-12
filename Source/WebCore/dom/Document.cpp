1335f0e895b0d499937c78381993ca68210cbbd7
alancoon@apple.com <alancoon@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2020-05-12 00:22:15 +0000

    Cherry-pick r258991. rdar://problem/62978907
        Fix build after https://trac.webkit.org/changeset/258977/webkit
        https://bugs.webkit.org/show_bug.cgi?id=209545
    
        Unreviewed.
    
        * dom/Document.cpp:
        (WebCore::Document::visibilityStateChanged):
        Add ENABLE(MEDIA_STREAM) compilation flag.
    
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@258991 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609-branch@261526 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/Document.cpp
+++ Source/WebCore/dom/Document.cpp
@@ -1729,13 +1729,15 @@ void Document::visibilityStateChanged()
     for (auto* client : m_visibilityStateCallbackClients)
         client->visibilityStateChanged();
 
-#if PLATFORM(IOS_FAMILY)
+#if ENABLE(MEDIA_STREAM)
     if (hidden()) {
         RealtimeMediaSourceCenter::singleton().setCapturePageState(hidden(), page()->isMediaCaptureMuted());
         return;
     }
+#if PLATFORM(IOS_FAMILY)
     if (!PlatformMediaSessionManager::sharedManager().isInterrupted())
         MediaStreamTrack::updateCaptureAccordingToMutedState(*this);
+#endif
 #endif
 }
 
