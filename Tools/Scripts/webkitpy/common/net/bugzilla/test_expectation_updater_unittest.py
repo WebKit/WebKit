1efef8c6e021423bd6ca605c334f159ae552d0f2
Carlos Alberto Lopez Perez <clopez@igalia.com>
2020-05-20 13:29:09 +0000

    update-test-expectations-from-bugzilla tool not working with new EWS https://bugs.webkit.org/show_bug.cgi?id=210975
    Reviewed by Youenn Fablet.
    
    With the new EWS, the layout test archive results are not longer
    posted to bugzilla as attachment. Now we need to inspect the EWS
    server to get the status of the runs for the patch id, and then
    query the EWS builbot server in order to get the details of each
    run to finally obtain the URL with the zip file for the results.
    
    The tool now automatically applies platform-specific and generic
    results automatically (its not longer needed to specify whether
    the result its generic or not). It uses mac-wk2 results as generic.
    
    Also now it updates the test results for tests where the result
    is MISSING.
    
    * Scripts/webkitpy/common/net/bugzilla/test_expectation_updater.py:
    (configure_logging):
    (argument_parser):
    (TestExpectationUpdater.__init__):
    (TestExpectationUpdater._platform_name):
    (TestExpectationUpdater):
    (TestExpectationUpdater._get_layout_tests_run):
    (TestExpectationUpdater._lookup_ews_results):
    (TestExpectationUpdater._tests_to_update):
    (TestExpectationUpdater._update_for_generic_bot):
    (TestExpectationUpdater._update_for_platform_specific_bot):
    (TestExpectationUpdater.do_update):
    (main):
    * Scripts/webkitpy/common/net/bugzilla/test_expectation_updater_unittest.py:
    (MockAttachment.__init__):
    (MockAttachment.is_patch):
    (MockAttachment):
    (MockAttachment.is_obsolete):
    (MockBugzilla):
    (MockBugzilla.attachments):
    (MockRequests):
    (MockRequests.__init__):
    (MockRequests.get):
    (MockRequests.content):
    (MockRequests.text):
    (MockZip.__init__):
    (TestExpectationUpdaterTest.test_update_test_expectations):
    
    
    Canonical link: https://commits.webkit.org/225000@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@261914 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/net/bugzilla/test_expectation_updater_unittest.py
+++ Tools/Scripts/webkitpy/common/net/bugzilla/test_expectation_updater_unittest.py
@@ -34,6 +34,7 @@ from webkitpy.common.system.filesystem_mock import MockFileSystem
 from webkitpy.common.system.executive_mock import MockExecutive2
 from webkitpy.common.net.bugzilla.test_expectation_updater import TestExpectationUpdater
 from webkitpy.common.net.bugzilla.attachment import Attachment
+from webkitpy.thirdparty import mock
 
 FAKE_FILES = {
     '/tests/csswg/css-fake-1/empty_dir/README.txt': '',
@@ -42,32 +43,62 @@ FAKE_FILES = {
 
 
 class MockAttachment(Attachment):
-    def __init__(self, attachment_dictionary, contents):
+    def __init__(self, attachment_dictionary, contents, is_patch, is_obsolete):
         Attachment.__init__(self, attachment_dictionary, self)
         self._contents = contents
+        self._is_patch = is_patch
+        self._is_obsolete = is_obsolete
 
     def contents(self):
         return self._contents
 
+    def is_patch(self):
+        return self._is_patch
+
+    def is_obsolete(self):
+        return self._is_obsolete
+
 
 class MockBugzilla():
-    def __init__(self, include_wk2=True, include_win_future=False):
-        self._include_wk2 = include_wk2
-        self._include_win_future = include_win_future
-
     def fetch_bug(self, id):
         return self
 
-    def attachments(self):
+    def attachments(self, include_obsolete=False):
         attachments = []
-        if self._include_wk2:
-            attachments.append(MockAttachment({"id": 1, "name": "Archive of layout-test-results from ews103 for mac-elcapitan-wk2"}, "mac-wk2"))
-        attachments.append(MockAttachment({"id": 2, "name": "Archive of layout-test-results from ews103 for mac-elcapitan"}, "mac-wk1a"))
-        attachments.append(MockAttachment({"id": 3, "name": "Archive of layout-test-results from ews104 for mac-elcapitan"}, "mac-wk1b"))
-        attachments.append(MockAttachment({"id": 4, "name": "Archive of layout-test-results from ews122 for ios-simulator-wk2"}, "ios-sim"))
-        if self._include_win_future:
-            attachments.append(MockAttachment({"id": 5, "name": "Archive of layout-test-results from ews124 for win-future"}, "win-future"))
+        attachments.append(MockAttachment({"id": 1, "name": "Test case"}, "How to reproduce the issue", False, False))
+        if include_obsolete:
+            attachments.append(MockAttachment({"id": 2, "name": "Patch"}, "Patch v1", True, True))
+            attachments.append(MockAttachment({"id": 3, "name": "Patch"}, "Patch v2", True, True))
+        attachments.append(MockAttachment({"id": 4, "name": "Patch"}, "Patch v3", True, False))
         return attachments
+
+
+class MockRequestsGet():
+    def __init__(self, url):
+        self._urls_data = {
+            'https://ews.webkit.org/status/4/': '{"mac-wk1": {"url": "https://ews-build.webkit.org/#/builders/1/builds/99", "state": 2}, "mac-wk2": {"url": "https://ews-build.webkit.org/#/builders/2/builds/99", "state": 2}, "mac-debug-wk1": {"url": "https://ews-build.webkit.org/#/builders/3/builds/99", "state": 2}, "ios-wk2": {"url": "https://ews-build.webkit.org/#/builders/4/builds/99", "state": 2}, "win": {"url": "https://ews-build.webkit.org/#/builders/5/builds/99", "state": 2}}',
+            'https://ews-build.webkit.org/api/v2/builders/1/builds/99/steps': '{ "steps": [ { "complete": true, "name": "layout-tests", "state_string": "Ran layout tests",  "urls": [ { "name": "download layout test results", "url": "/results/mac-wk1/r12345.zip" } ] } ] }',
+            'https://ews-build.webkit.org/api/v2/builders/2/builds/99/steps': '{ "steps": [ { "complete": true, "name": "layout-tests", "state_string": "Ran layout tests",  "urls": [ { "name": "download layout test results", "url": "/results/mac-wk2/r12345.zip" } ] } ] }',
+            'https://ews-build.webkit.org/api/v2/builders/3/builds/99/steps': '{ "steps": [ { "complete": true, "name": "layout-tests", "state_string": "Ran layout tests",  "urls": [ { "name": "download layout test results", "url": "/results/mac-debug-wk1/r12345.zip" } ] } ] }',
+            'https://ews-build.webkit.org/api/v2/builders/4/builds/99/steps': '{ "steps": [ { "complete": true, "name": "layout-tests", "state_string": "Ran layout tests",  "urls": [ { "name": "download layout test results", "url": "/results/ios-wk2/r12345.zip" }  ] } ] }',
+            'https://ews-build.webkit.org/api/v2/builders/5/builds/99/steps': '{ "steps": [ { "complete": true, "name": "layout-tests", "state_string": "Ran layout tests",  "urls": [ { "name": "download layout test results", "url": "/results/win/r12345.zip" }  ] } ] }',
+            "https://ews-build.webkit.org/results/mac-wk1/r12345.zip": "mac-wk1a",
+            "https://ews-build.webkit.org/results/mac-debug-wk1/r12345.zip": "mac-wk1b",
+            "https://ews-build.webkit.org/results/mac-wk2/r12345.zip": "mac-wk2",
+            "https://ews-build.webkit.org/results/ios-wk2/r12345.zip": "ios-wk2",
+            "https://ews-build.webkit.org/results/win/r12345.zip": "win"
+        }
+        self._url = url
+        if url not in self._urls_data:
+            raise ValueError("url {} not mocked".format(url))
+
+    @property
+    def content(self):
+        return self._urls_data[self._url]
+
+    @property
+    def text(self):
+        return str(self.content)
 
 
 class MockZip():
@@ -95,17 +126,17 @@ class MockZip():
             "imported/w3c/web-platform-tests/fetch/api/redirect/redirect-location-worker-actual.txt": "d",
             "imported/w3c/web-platform-tests/fetch/api/redirect/redirect-location-actual.txt": "e",
             "imported/w3c/web-platform-tests/html/browsers/windows/browsing-context-actual.txt": "f-wk1b"}
-        ios_sim_files = {"full_results.json": 'ADD_RESULTS({"tests":{"imported":{"w3c":{"web-platform-tests":{"url":{"interfaces.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}},"html":{"browsers":{"windows":{"browsing-context.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}},"the-window-object":{"apis-for-creating-and-navigating-browsing-contexts-by-name":{"open-features-tokenization-001.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}}}}},"dom":{"events":{"EventTarget-dispatchEvent.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}}}}}},"animations":{"trigger-container-scroll-empty.html":{"report":"FLAKY","expected":"PASS","actual":"TEXT PASS"}}},"skipped":9881,"num_regressions":4,"other_crashes":{},"interrupted":false,"num_missing":0,"layout_tests_dir":"/Volumes/Data/EWS/WebKit/LayoutTests","version":4,"num_passes":38225,"pixel_tests_enabled":false,"date":"07:33PM on April 08, 2017","has_pretty_patch":true,"fixable":48110,"num_flaky":1,"uses_expectations_file":true});',
+        ios_files = {"full_results.json": 'ADD_RESULTS({"tests":{"imported":{"w3c":{"web-platform-tests":{"url":{"interfaces.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}},"html":{"browsers":{"windows":{"browsing-context.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}},"the-window-object":{"apis-for-creating-and-navigating-browsing-contexts-by-name":{"open-features-tokenization-001.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}}}}},"dom":{"events":{"EventTarget-dispatchEvent.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}}}}}},"animations":{"trigger-container-scroll-empty.html":{"report":"FLAKY","expected":"PASS","actual":"TEXT PASS"}}},"skipped":9881,"num_regressions":4,"other_crashes":{},"interrupted":false,"num_missing":0,"layout_tests_dir":"/Volumes/Data/EWS/WebKit/LayoutTests","version":4,"num_passes":38225,"pixel_tests_enabled":false,"date":"07:33PM on April 08, 2017","has_pretty_patch":true,"fixable":48110,"num_flaky":1,"uses_expectations_file":true});',
             "imported/w3c/web-platform-tests/dom/events/EventTarget-dispatchEvent-actual.txt": "g",
             "imported/w3c/web-platform-tests/html/browsers/the-window-object/apis-for-creating-and-navigating-browsing-contexts-by-name/open-features-tokenization-001-actual.txt": "h",
             "imported/w3c/web-platform-tests/html/browsers/windows/browsing-context-actual.txt": "i",
             "imported/w3c/web-platform-tests/url/interfaces-actual.txt": "j"}
-        win_future_files = {"full_results.json": 'ADD_RESULTS({"tests":{"imported":{"w3c":{"web-platform-tests":{"url":{"interfaces.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}},"html":{"browsers":{"windows":{"browsing-context.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}},"the-window-object":{"apis-for-creating-and-navigating-browsing-contexts-by-name":{"open-features-tokenization-001.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}}}}},"dom":{"events":{"EventTarget-dispatchEvent.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}}}}}},"animations":{"trigger-container-scroll-empty.html":{"report":"FLAKY","expected":"PASS","actual":"TEXT PASS"}}},"skipped":9881,"num_regressions":4,"other_crashes":{},"interrupted":false,"num_missing":0,"layout_tests_dir":"/Volumes/Data/EWS/WebKit/LayoutTests","version":4,"num_passes":38225,"pixel_tests_enabled":false,"date":"07:33PM on April 08, 2017","has_pretty_patch":true,"fixable":48110,"num_flaky":1,"uses_expectations_file":true});',
+        win_files = {"full_results.json": 'ADD_RESULTS({"tests":{"imported":{"w3c":{"web-platform-tests":{"url":{"interfaces.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}},"html":{"browsers":{"windows":{"browsing-context.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}},"the-window-object":{"apis-for-creating-and-navigating-browsing-contexts-by-name":{"open-features-tokenization-001.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}}}}},"dom":{"events":{"EventTarget-dispatchEvent.html":{"report":"REGRESSION","expected":"PASS","actual":"TEXT"}}}}}},"animations":{"trigger-container-scroll-empty.html":{"report":"FLAKY","expected":"PASS","actual":"TEXT PASS"}}},"skipped":9881,"num_regressions":4,"other_crashes":{},"interrupted":false,"num_missing":0,"layout_tests_dir":"/Volumes/Data/EWS/WebKit/LayoutTests","version":4,"num_passes":38225,"pixel_tests_enabled":false,"date":"07:33PM on April 08, 2017","has_pretty_patch":true,"fixable":48110,"num_flaky":1,"uses_expectations_file":true});',
             "imported/w3c/web-platform-tests/dom/events/EventTarget-dispatchEvent-actual.txt": "g",
             "imported/w3c/web-platform-tests/html/browsers/the-window-object/apis-for-creating-and-navigating-browsing-contexts-by-name/open-features-tokenization-001-actual.txt": "h",
             "imported/w3c/web-platform-tests/html/browsers/windows/browsing-context-actual.txt": "i",
             "imported/w3c/web-platform-tests/url/interfaces-actual.txt": "j"}
-        self.files = {"mac-wk2": mac_wk2_files, "mac-wk1a": mac_wk1a_files, "mac-wk1b": mac_wk1b_files, "ios-sim": ios_sim_files, "win-future": win_future_files}
+        self.files = {"mac-wk2": mac_wk2_files, "mac-wk1a": mac_wk1a_files, "mac-wk1b": mac_wk1b_files, "ios-wk2": ios_files, "win": win_files}
 
     def unzip(self, content):
         self.content = content
@@ -133,29 +164,19 @@ class TestExpectationUpdaterTest(unittest.TestCase):
             '/mock-checkout/LayoutTests/imported/w3c/web-platform-tests/url/interfaces-expected.txt': "j-mac-wk2"})
 
         mock_zip = MockZip()
-        updater = TestExpectationUpdater(host, "123456", True, False, MockBugzilla(), lambda content: mock_zip.unzip(content))
-        updater.do_update()
-        # mac-wk2 expectation
-        self.assertTrue(self._is_matching(host, "imported/w3c/web-platform-tests/fetch/api/redirect/redirect-count-cross-origin-expected.txt", "a"))
-        # no need to add mac-wk1 specific expectation
-        self.assertFalse(self._exists(host, "platform/mac-wk1/imported/w3c/web-platform-tests/fetch/api/redirect/redirect-count-cross-origin-expected.txt"))
-        # mac-wk1/ios-simulator-wk2 specific expectation
-        self.assertTrue(self._is_matching(host, "platform/mac-wk1/imported/w3c/web-platform-tests/html/browsers/windows/browsing-context-expected.txt", "f-wk1b"))
-        self.assertTrue(self._is_matching(host, "platform/ios-wk2/imported/w3c/web-platform-tests/url/interfaces-expected.txt", "j"))
-        # removal of mac-wk1 expectation since no longer different
-        self.assertFalse(self._exists(host, "platform/mac-wk1/imported/w3c/web-platform-tests/fetch/api/redirect/redirect-location-expected.txt"))
+        with mock.patch('webkitpy.common.net.bugzilla.test_expectation_updater.requests.get', MockRequestsGet):
+            updater = TestExpectationUpdater(host, "123456", True, False, MockBugzilla(), lambda content: mock_zip.unzip(content))
+            updater.do_update()
+            # mac-wk2 expectation
+            self.assertTrue(self._is_matching(host, "imported/w3c/web-platform-tests/fetch/api/redirect/redirect-count-cross-origin-expected.txt", "a"))
+            # no need to add mac-wk1 specific expectation
+            self.assertFalse(self._exists(host, "platform/mac-wk1/imported/w3c/web-platform-tests/fetch/api/redirect/redirect-count-cross-origin-expected.txt"))
+            # mac-wk1/ios-simulator-wk2 specific expectation
+            self.assertTrue(self._is_matching(host, "platform/mac-wk1/imported/w3c/web-platform-tests/html/browsers/windows/browsing-context-expected.txt", "f-wk1b"))
+            self.assertTrue(self._is_matching(host, "platform/ios-wk2/imported/w3c/web-platform-tests/url/interfaces-expected.txt", "j"))
+            # removal of mac-wk1 expectation since no longer different
+            self.assertFalse(self._exists(host, "platform/mac-wk1/imported/w3c/web-platform-tests/fetch/api/redirect/redirect-location-expected.txt"))
+            # windows specific expectation
+            self.assertTrue(self._is_matching(host, "platform/win/imported/w3c/web-platform-tests/url/interfaces-expected.txt", "j"))
 
-    def test_update_win_future_test_expectations(self):
-        host = MockHost()
-        host.executive = MockExecutive2(exception=OSError())
-        host.filesystem = MockFileSystem(files={
-            '/mock-checkout/LayoutTests/platform/mac-wk1/imported/w3c/web-platform-tests/fetch/api/redirect/redirect-location-expected.txt': 'e-wk1',
-            '/mock-checkout/LayoutTests/imported/w3c/web-platform-tests/dom/events/EventTarget-dispatchEvent-expected.txt': "g",
-            '/mock-checkout/LayoutTests/imported/w3c/web-platform-tests/html/browsers/the-window-object/apis-for-creating-and-navigating-browsing-contexts-by-name/open-features-tokenization-001-expected.txt': "h",
-            '/mock-checkout/LayoutTests/imported/w3c/web-platform-tests/html/browsers/windows/browsing-context-expected.txt': "i",
-            '/mock-checkout/LayoutTests/imported/w3c/web-platform-tests/url/interfaces-expected.txt': "j-mac-wk2"})
 
-        mock_zip = MockZip()
-        updater = TestExpectationUpdater(host, "123456", True, False, MockBugzilla(include_wk2=False, include_win_future=True), lambda content: mock_zip.unzip(content))
-        updater.do_update()
-        self.assertTrue(self._is_matching(host, "platform/win/imported/w3c/web-platform-tests/url/interfaces-expected.txt", "j"))
