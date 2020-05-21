76c6c5170468f113bb24c3d21eb14ab8d6ba8040
Chris Dumez <cdumez@apple.com>
2020-05-21 16:08:50 +0000

    [WK2] WebKit abandons compiled sandbox profiles https://bugs.webkit.org/show_bug.cgi?id=212177 <rdar://problem/54613619>
    Reviewed by Saam Barati.
    
    WebKit generates new compiled sandbox profiles whenever sandbox profiles are updated or
    whenever webkit directories change (which is super common for WebKitTestRunner). Previous
    compiled sandbox profiles do not get deleted and therefore may accumulate.
    
    To address the issue, we now save compiled sandbox profiles under _CS_DARWIN_USER_TEMP_DIR
    instead of _CS_DARWIN_USER_CACHE_DIR. The system takes care of deleting the files under
    _CS_DARWIN_USER_TEMP_DIR upon reboot or if they have not been accessed for 3 days.
    
    * Shared/mac/AuxiliaryProcessMac.mm:
    (WebKit::sandboxDataVaultParentDirectory):
    
    
    Canonical link: https://commits.webkit.org/225074@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@262004 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/mac/AuxiliaryProcessMac.mm
+++ Source/WebKit/Shared/mac/AuxiliaryProcessMac.mm
@@ -256,7 +256,10 @@ static Optional<CString> setAndSerializeSandboxParameters(const SandboxInitializ
 static String sandboxDataVaultParentDirectory()
 {
     char temp[PATH_MAX];
-    size_t length = confstr(_CS_DARWIN_USER_CACHE_DIR, temp, sizeof(temp));
+    // We save the profiles in the user tempory directory so that they get cleaned on reboot
+    // or if they are not accessed in 3 days. This avoids accumulating profiles whenever we change
+    // our sandbox rules or webkit cache directories (rdar://problem/54613619).
+    size_t length = confstr(_CS_DARWIN_USER_TEMP_DIR, temp, sizeof(temp));
     if (!length) {
         WTFLogAlways("%s: Could not retrieve user cache directory path: %s\n", getprogname(), strerror(errno));
         exit(EX_NOPERM);
