6d02745c5cf32a8726e10428081c74e438636bf3
James Savage <james.savage@apple.com>
2020-05-23 00:52:00 +0000

    Create Swift overlay for WebKit framework https://bugs.webkit.org/show_bug.cgi?id=211270 <rdar://problem/60549174>
    Reviewed by Brady Eidson.
    
    Provides Swift-only overlay methods for our NS_REFINED_FOR_SWIFT API.
    The overlay does not provide different API than its Objective-C counterparts,
    so overlay testing focuses on ensuring that the overlay produces the
    expected interface, and provides the expected mappings to and from ObjC.
    
    The overlay is defined in a separate Xcode project (from WebKit) by
    recommendation from the Swift team. Due to build system constraints this
    project cannot be a sibbling of WebKit.xcodeproj, and must live in a
    subdirectory. However, because it generally acts as part of the WebKit
    framework, it still uses the same Version.xcconfig and source file
    locations as standard WebKit sources.
    
    * SwiftOverlay/Configurations/Base.xcconfig: Added. Set some default
        warnings and product configuration.
    * SwiftOverlay/Configurations/WebKitSwiftOverlay-maccatalyst.xcconfig: Added.
        Override SDK configurations to build for Catalyst.
    * SwiftOverlay/Configurations/WebKitSwiftOverlay.xcconfig: Added.
        These flags were all provided by the Xcode template.
    * SwiftOverlay/Configurations/WebKitSwiftOverlayTests-maccatalyst.xcconfig: Added.
        See above.
    * SwiftOverlay/Configurations/WebKitSwiftOverlayTests.xcconfig: Added.
        See above.
    * SwiftOverlay/SwiftOverlay/install-swiftmodules.sh: Added. Provided by the
        Swift team.
    * SwiftOverlay/Tests/WebKitSwiftOverlayTests-Info.plist: Added.
    * SwiftOverlay/Tests/WebKitTests.swift: Added.
    (WebKitTests.testAPI): This ensures that the function names come through as we
        expect them to. It also ensures that the functions were correctly set to
        public visiblity.
    (WebKitTests.testWKPDFConfigurationRect): This does the same as the above, but
        also exercises the null <-> nil conversion, and that regular values are
        correctly round tripped to Objective-C.
    * SwiftOverlay/WebKitSwiftOverlay.xcodeproj/project.pbxproj: Added.
    * SwiftOverlay/WebKitSwiftOverlay.xcodeproj/xcshareddata/xcschemes/Everything (Catalyst).xcscheme: Added.
    * SwiftOverlay/WebKitSwiftOverlay.xcodeproj/xcshareddata/xcschemes/Everything.xcscheme: Added.
    * UIProcess/API/Cocoa/WebKitSwiftOverlay.swift: Added.
    (WKPDFConfiguration.rect): Provide mapping of CGRect.null to nil for Swift.
    (WKWebView.callAsyncJavaScript(_:arguments:in:completion:)): Provide a Result
    in the completion handler, and default value for arguments.
    (WKWebView.evaluateJavaScript(_:in:completion:)): Ditto.
    (WKWebView.createPDF(configuration:completionHandler:)): Provide a Swift function
        that takes a Result<Data, Error> instead of (Data?, Error?) tuple. Provide
        a default value for the configuration.
    (WKWebView.createWebArchiveData(completionHandler:)): See above.
    (WKWebView.find(_:configuration:completionHandler:)): See above.
    
    
    Canonical link: https://commits.webkit.org/225152@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@262089 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit/SwiftOverlay/Tests/WebKitTests.swift
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2020 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import XCTest
+import WebKit
+
+class WebKitTests: XCTestCase {
+    /// This is a compile-time test that ensures the function names are what we expect.
+    func testAPI() {
+        _ = WKContentWorld.world(name:)
+        _ = WKWebView.callAsyncJavaScript(_:arguments:in:completion:)
+        _ = WKWebView.createPDF(configuration:completionHandler:)
+        _ = WKWebView.createWebArchiveData(completionHandler:)
+        _ = WKWebView.evaluateJavaScript(_:in:completion:)
+        _ = WKWebView.find(_:configuration:completionHandler:)
+    }
+
+    func testWKPDFConfigurationRect() {
+        let configuration = WKPDFConfiguration()
+
+        XCTAssert(type(of: configuration.rect) == Optional<CGRect>.self)
+
+        configuration.rect = nil
+        XCTAssertEqual(configuration.rect, nil)
+
+        configuration.rect = .null
+        XCTAssertEqual(configuration.rect, nil)
+
+        configuration.rect = CGRect.zero
+        XCTAssertEqual(configuration.rect, .zero)
+
+        let originalPhoneBounds = CGRect(x: 0, y: 0, width: 320, height: 480)
+        configuration.rect = originalPhoneBounds
+        XCTAssertEqual(configuration.rect, originalPhoneBounds)
+    }
+}
---
+++ Source/WebKit/UIProcess/API/Cocoa/WebKitSwiftOverlay.swift
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2020 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+@_exported import WebKit
+
+@available(iOS 14.0, macOS 10.16, *)
+extension WKPDFConfiguration {
+    public var rect: CGRect? {
+        get { __rect == .null ? nil : __rect }
+        set { __rect = newValue == nil ? .null : newValue! }
+    }
+}
+
+@available(iOS 14.0, macOS 10.16, *)
+extension WKWebView {
+    public func callAsyncJavaScript(_ functionBody: String, arguments: [String:Any] = [:], in contentWorld: WKContentWorld, completion: @escaping(Result<Any, Error>) -> Void) {
+        __callAsyncJavaScript(functionBody, arguments: arguments, in: contentWorld, completionHandler: makeResultHandler(completion))
+    }
+
+    public func createPDF(configuration: WKPDFConfiguration = .init(), completionHandler: @escaping (Result<Data, Error>) -> Void) {
+        __createPDF(with: configuration, completionHandler: makeResultHandler(completionHandler))
+    }
+
+    public func createWebArchiveData(completionHandler: @escaping (Result<Data, Error>) -> Void) {
+        __createWebArchiveData(completionHandler: makeResultHandler(completionHandler))
+    }
+
+    public func evaluateJavaScript(_ javaScript: String, in contentWorld: WKContentWorld, completion: @escaping (Result<Any, Error>) -> Void) {
+        __evaluateJavaScript(javaScript, in: contentWorld, completionHandler: makeResultHandler(completion))
+    }
+
+    public func find(_ string: String, configuration: WKFindConfiguration = .init(), completionHandler: @escaping (WKFindResult) -> Void) {
+        __find(string, with: configuration, completionHandler: completionHandler)
+    }
+}
+
+func makeResultHandler<Success, Failure>(_ handler: @escaping (Result<Success, Failure>) -> Void) -> (Success?, Failure?) -> Void {
+    return { success, failure in
+        if let success = success {
+            handler(.success(success))
+        } else if let failure = failure {
+            handler(.failure(failure))
+        } else {
+            fatalError("Bug in WebKit: Received neither result or failure.")
+        }
+    }
+}
