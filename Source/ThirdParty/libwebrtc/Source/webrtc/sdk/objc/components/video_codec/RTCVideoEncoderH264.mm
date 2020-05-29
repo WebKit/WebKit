efa15a0ab66d5c611deb52c522dd7383f55b0378
Youenn Fablet <youenn@apple.com>
2020-05-29 12:47:12 +0000

    Enable VTB required low latency code path https://bugs.webkit.org/show_bug.cgi?id=210609 <rdar://problem/61890332>
    Reviewed by Eric Carlson.
    
    * Source/webrtc/sdk/WebKit/VideoProcessingSoftLink.h:
    * Source/webrtc/sdk/objc/components/video_codec/RTCVideoEncoderH264.mm:
    Declare the new key since it is now in a private header.
    
    
    Canonical link: https://commits.webkit.org/225342@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@262290 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/ThirdParty/libwebrtc/Source/webrtc/sdk/objc/components/video_codec/RTCVideoEncoderH264.mm
+++ Source/ThirdParty/libwebrtc/Source/webrtc/sdk/objc/components/video_codec/RTCVideoEncoderH264.mm
@@ -45,6 +45,7 @@
 #import <dlfcn.h>
 #import <objc/runtime.h>
 
+VT_EXPORT const CFStringRef kVTVideoEncoderSpecification_RequiredLowLatency;
 VT_EXPORT const CFStringRef kVTVideoEncoderSpecification_Usage;
 VT_EXPORT const CFStringRef kVTCompressionPropertyKey_Usage;
 
