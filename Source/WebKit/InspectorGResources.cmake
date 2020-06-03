79ed5e0c118c45c96c604d9be08c1eee0e755ce8
Christopher Reid <chris.reid@sony.com>
2020-06-03 00:57:56 +0000

    REGRESSION[r260844]: [GTK][WPE] Inspector GResource no longer updated after WebInspectorUI file updates https://bugs.webkit.org/show_bug.cgi?id=212583
    Reviewed by Adrian Perez de Castro.
    
    Add a cmake dependency on inspector resource stamp so the Glib
    Inspector resources rebuild when the stamp is updated.
    
    * InspectorGResources.cmake:
    
    
    Canonical link: https://commits.webkit.org/225495@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@262466 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/InspectorGResources.cmake
+++ Source/WebKit/InspectorGResources.cmake
@@ -2,6 +2,7 @@ macro(WEBKIT_BUILD_INSPECTOR_GRESOURCES _derived_sources_dir)
     add_custom_command(
         OUTPUT ${_derived_sources_dir}/InspectorGResourceBundle.xml
         DEPENDS WebInspectorUI
+                ${CMAKE_BINARY_DIR}/inspector-resources.stamp
                 ${TOOLS_DIR}/glib/generate-inspector-gresource-manifest.py
         COMMAND ${PYTHON_EXECUTABLE} ${TOOLS_DIR}/glib/generate-inspector-gresource-manifest.py --input=${_derived_sources_dir}/InspectorResources --output=${_derived_sources_dir}/InspectorGResourceBundle.xml
         VERBATIM
