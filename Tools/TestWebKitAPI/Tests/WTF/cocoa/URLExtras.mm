cc8f6fc972d02c49b0c5a9e4b5e3807054b3cbc3
Alex Christensen <achristensen@apple.com>
2020-06-08 20:36:05 +0000

    Fix branch build after r262552
    * TestWebKitAPI/Tests/WTF/cocoa/URLExtras.mm:
    (TestWebKitAPI::TEST):
    The branch WTF::URL has an operator NSURL * and an operator NSString *, so we need to specify which to use.
    
    
    
    Canonical link: https://commits.webkit.org/218903.607@safari-609-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609-branch@262737 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/WTF/cocoa/URLExtras.mm
+++ Tools/TestWebKitAPI/Tests/WTF/cocoa/URLExtras.mm
@@ -205,7 +205,7 @@ TEST(WTF_URLExtras, URLExtras_ParsingError)
     EXPECT_TRUE(encodedHostName == nil);
 
     WTF::URL url2(URL(), utf16String(u"http://\u2267\u222E\uFE63"));
-    EXPECT_STREQ([[url2 absoluteString] UTF8String], "http://%E2%89%A7%E2%88%AE%EF%B9%A3");
+    EXPECT_STREQ([[(NSURL *)url2 absoluteString] UTF8String], "http://%E2%89%A7%E2%88%AE%EF%B9%A3");
 }
 
 TEST(WTF_URLExtras, URLExtras_Nil)
