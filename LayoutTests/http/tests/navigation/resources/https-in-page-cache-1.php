4cff1f79d155361a8d92a26fa9e3e0f79199c823
Chris Dumez <cdumez@apple.com>
2020-06-13 00:06:42 +0000

    Cherry-pick r262978. rdar://problem/64315997
        Stop allowing pages served over HTTPS with "Cache-Control: no-store" into the back/forward cache
        https://bugs.webkit.org/show_bug.cgi?id=213147
        <rdar://problem/64249683>
    
        Reviewed by Geoffrey Garen.
    
        Source/WebCore:
    
        Stop allowing pages served over HTTPS with "Cache-Control: no-store" into the back/forward cache.
        This is a revert of r250437 due to push back from Web developers.
    
        No new tests, updated existing tests.
    
        * history/BackForwardCache.cpp:
        (WebCore::canCacheFrame):
    
        LayoutTests:
    
        Update layout test coverage.
    
        * http/tests/navigation/https-in-page-cache-expected.txt:
        * http/tests/navigation/resources/https-in-page-cache-1.php:
    
        Canonical link: https://commits.webkit.org/225926@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@262978 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/218903.620@safari-609-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609-branch@262990 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/navigation/resources/https-in-page-cache-1.php
+++ LayoutTests/http/tests/navigation/resources/https-in-page-cache-1.php
@@ -11,19 +11,19 @@ function nextTest() {
 
 window.onpageshow = function(evt) {
 	if (evt.persisted) {
-		alert("The page was restored from the page cache. Good job. Running part 2 of the test.");
+		alert("The page was restored from the page cache. It should NOT have been. Running part 2 of the test.");
 		nextTest();
 	}
 }
 
 window.onload = function() {
 	if (window.sessionStorage.https_in_page_cache_started) {
-		alert("The page was reloaded on back, not from the page cache. It should NOT have reloaded. Running part 2 of the test.");
+		alert("The page was reloaded on back, not from the page cache. Good job. Running part 2 of the test.");
 		nextTest();
 		return;
 	}
 
-	alert("This page is https and has the no-store cache-control directive. It should go in to the page cache.");
+	alert("This page is https and has the no-store cache-control directive. It should NOT go in to the page cache.");
 	window.sessionStorage.https_in_page_cache_started = true;
 	setTimeout('window.location = "go-back.html"', 0);
 
