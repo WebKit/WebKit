badbc10eb72fbd8696104a35473562a5c17f4bf5
Andy Estes <aestes@apple.com>
2020-06-15 08:12:36 +0000

    Cherry-pick r262918. rdar://problem/64315999
        [iOS] nullptr deref in FileInputType::iconLoaded when the input's type attribute is modified by a change event listener
        https://bugs.webkit.org/show_bug.cgi?id=208244
        <rdar://problem/41855350>
    
        Reviewed by Wenson Hsieh.
    
        Source/WebCore:
    
        When an <input> element's type attribute changes, its existing InputType is detached from
        the HTMLInputElement by nulling InputType::m_element. When FileInputType::filesChosen is
        called, it dispatches the input and change events, which can run arbitrary JavaScript that
        might modify the element's type attribute. If this happens, FileInputType::m_element will be
        null after returning from FileInputType::setFiles and if there is an icon will be
        dereferenced by FileInputType::iconLoaded.
    
        Fixed this by checking for a non-null m_element before calling iconLoaded. While here, also
        fixed a bug where we sometimes checked the length of m_fileList before FileListCreator had
        finished setting m_fileList. This bug resulted in missing file icons whenever an
        <input type=file> had the webkitdirectory attribute.
    
        Tests: fast/forms/file/file-input-type-detached-on-change.html
               fast/forms/file/file-input-webkitdirectory-icon.html
    
        * html/FileInputType.cpp:
        (WebCore::FileInputType::filesChosen):
    
        Tools:
    
        * DumpRenderTree/TestRunner.cpp:
        (SetOpenPanelFilesMediaIconCallback):
        (TestRunner::staticFunctions):
        * WebKitTestRunner/InjectedBundle/TestRunner.cpp:
        (WTR::TestRunner::setOpenPanelFilesMediaIcon):
    
        LayoutTests:
    
        * fast/forms/file/file-input-type-detached-on-change-expected.txt: Added.
        * fast/forms/file/file-input-type-detached-on-change.html: Added.
        * fast/forms/file/file-input-webkitdirectory-icon-expected.html: Added.
        * fast/forms/file/file-input-webkitdirectory-icon.html: Added.
        * fast/forms/file/file-reset-in-change-using-open-panel-with-icon.html:
        * fast/forms/file/open-file-panel-crash.html:
        * fast/forms/file/resources/file-icon-bytes.js: Added.
    
        Canonical link: https://commits.webkit.org/225874@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@262918 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/218903.621@safari-609-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609-branch@263020 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/WebKitTestRunner/InjectedBundle/TestRunner.cpp
+++ Tools/WebKitTestRunner/InjectedBundle/TestRunner.cpp
@@ -2634,9 +2634,9 @@ void TestRunner::setOpenPanelFiles(JSValueRef filesValue)
     WKBundlePagePostMessage(page, messageName.get(), fileURLs.get());
 }
 
-#if PLATFORM(IOS_FAMILY)
 void TestRunner::setOpenPanelFilesMediaIcon(JSValueRef data)
 {
+#if PLATFORM(IOS_FAMILY)
     WKBundlePageRef page = InjectedBundle::singleton().page()->page();
     JSContextRef context = WKBundleFrameGetJavaScriptContext(WKBundlePageGetMainFrame(page));
 
@@ -2646,12 +2646,10 @@ void TestRunner::setOpenPanelFilesMediaIcon(JSValueRef data)
 
     static auto messageName = adoptWK(WKStringCreateWithUTF8CString("SetOpenPanelFileURLsMediaIcon"));
     WKBundlePagePostMessage(page, messageName.get(), iconData.get());
-}
 #else
-void TestRunner::setOpenPanelFilesMediaIcon(JSValueRef)
-{
+    UNUSED_PARAM(data);
+#endif
 }
-#endif // PLATFORM(IOS_FAMILY)
 
 void TestRunner::removeAllSessionCredentials(JSValueRef callback)
 {
