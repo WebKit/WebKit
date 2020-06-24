c946a8961277dde180a385a84450b1b41ad3cb67
Alexey Shvayka <shvaikalesh@gmail.com>
2020-06-24 11:11:47 +0000

    Remove [CallbackNeedsOperatorEqual] extended attribute https://bugs.webkit.org/show_bug.cgi?id=213538
    Reviewed by Chris Dumez.
    
    This change removes now unused [CallbackNeedsOperatorEqual] extended attribute.
    It was added to accommodate MediaQueryListListener, which was removed in r260243.
    
    We are safe to remove [CallbackNeedsOperatorEqual] since its implementation
    expects certain constructor signature, and callback interfaces are considered
    legacy by spec authors anyway.
    
    Also, this patch removes 2 resolved FIXMEs:
    1. OrdinaryDefineOwnProperty() is performed correctly, no dynamic dispatch there.
    2. Set() is performed correctly, with `false` as last argument.
       (changed in https://github.com/heycam/webidl/pull/832, covered by WPT)
    
    No new tests, no behavior change.
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateDefineOwnProperty):
    (GenerateAttributeSetterBodyDefinition):
    (GenerateCallbackHeaderContent):
    (GenerateCallbackImplementationContent):
    * bindings/scripts/IDLAttributes.json:
    
    
    Canonical link: https://commits.webkit.org/226345@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@263450 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -1222,7 +1222,6 @@ sub GenerateDefineOwnProperty
     }
     
     # 4. Return OrdinaryDefineOwnProperty(O, P, Desc).
-    # FIXME: Does this do the same thing?
     push(@$outputArray, "    return JSObject::defineOwnProperty(object, lexicalGlobalObject, propertyName, newPropertyDescriptor, shouldThrow);\n");
     
     push(@$outputArray, "}\n\n");
@@ -5176,9 +5175,7 @@ sub GenerateAttributeSetterBodyDefinition
         my $forwardId = $attribute->extendedAttributes->{PutForwards};
         push(@$outputArray, "    auto forwardId = Identifier::fromString(vm, reinterpret_cast<const LChar*>(\"${forwardId}\"), strlen(\"${forwardId}\"));\n");
         
-        # 3.5.9.4. Perform ? Set(Q, forwardId, V).
-        # FIXME: What should the second value to the PutPropertySlot be?
-        # (https://github.com/heycam/webidl/issues/368)
+        # 3.5.9.4. Perform ? Set(Q, forwardId, V, false).
         push(@$outputArray, "    PutPropertySlot slot(valueToForwardTo, false);\n");
         push(@$outputArray, "    asObject(valueToForwardTo)->methodTable(vm)->put(asObject(valueToForwardTo), &lexicalGlobalObject, forwardId, value, slot);\n");
         push(@$outputArray, "    RETURN_IF_EXCEPTION(throwScope, false);\n");
@@ -6168,8 +6165,6 @@ sub GenerateCallbackHeaderContent
 
     push(@$contentRef, "    static JSC::JSValue getConstructor(JSC::VM&, const JSC::JSGlobalObject*);\n") if @{$constants};
 
-    push(@$contentRef, "    virtual bool operator==(const ${name}&) const override;\n\n") if $interfaceOrCallback->extendedAttributes->{CallbackNeedsOperatorEqual};
-
     # Operations
     my $numOperations = @{$operations};
     if ($numOperations > 0) {
@@ -6229,11 +6224,7 @@ sub GenerateCallbackImplementationContent
 
     # Constructor
     push(@$contentRef, "${className}::${className}(JSObject* callback, JSDOMGlobalObject* globalObject)\n");
-    if ($interfaceOrCallback->extendedAttributes->{CallbackNeedsOperatorEqual}) {
-        push(@$contentRef, "    : ${name}(globalObject->scriptExecutionContext(), ${className}Type)\n");
-    } else {
-        push(@$contentRef, "    : ${name}(globalObject->scriptExecutionContext())\n");
-    }
+    push(@$contentRef, "    : ${name}(globalObject->scriptExecutionContext())\n");
     push(@$contentRef, "    , m_data(new ${callbackDataType}(callback, globalObject, this))\n");
     push(@$contentRef, "{\n");
     push(@$contentRef, "}\n\n");
@@ -6252,15 +6243,6 @@ sub GenerateCallbackImplementationContent
     push(@$contentRef, "    m_data = nullptr;\n");
     push(@$contentRef, "#endif\n");
     push(@$contentRef, "}\n\n");
-
-    if ($interfaceOrCallback->extendedAttributes->{CallbackNeedsOperatorEqual}) {
-        push(@$contentRef, "bool ${className}::operator==(const ${name}& other) const\n");
-        push(@$contentRef, "{\n");
-        push(@$contentRef, "    if (other.type() != type())\n");
-        push(@$contentRef, "        return false;\n");
-        push(@$contentRef, "    return static_cast<const ${className}*>(&other)->m_data->callback() == m_data->callback();\n");
-        push(@$contentRef, "}\n\n");
-    }
 
     # Constants.
     my $numConstants = @{$constants};
