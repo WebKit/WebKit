c854d9a481635628bd31f14f4f4e4beaddf14c76
Alan Coon <alancoon@apple.com>
2020-06-25 18:21:25 +0000

    Apply patch. rdar://problem/64763027
    Canonical link: https://commits.webkit.org/218903.636@safari-609.3.5.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-609.3.5.1-branch@263522 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/DocumentLoader.cpp
+++ Source/WebCore/loader/DocumentLoader.cpp
@@ -649,7 +649,7 @@ void DocumentLoader::willSendRequest(ResourceRequest&& newRequest, const Resourc
 
     if (!newRequest.url().host().isEmpty() && SecurityOrigin::shouldIgnoreHost(newRequest.url())) {
         auto url = newRequest.url();
-        url.setHostAndPort({ });
+        url.removeHostAndPort();
         newRequest.setURL(WTFMove(url));
     }
 
