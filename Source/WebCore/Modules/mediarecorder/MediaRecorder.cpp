76fbcd318e4d804332d05202722ae60e4a7eccb3
Youenn Fablet <youenn@apple.com>
2020-06-29 18:32:10 +0000

    Support MediaRecorder.onstart https://bugs.webkit.org/show_bug.cgi?id=213720
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    Fire start event if MediaRecorder.start is successful.
    Do some WebIDL clean-up, in particular change timeSlice from long to unsigned long, as per spec.
    Covered by added test.
    
    * Modules/mediarecorder/MediaRecorder.cpp:
    (WebCore::MediaRecorder::startRecording):
    * Modules/mediarecorder/MediaRecorder.h:
    * Modules/mediarecorder/MediaRecorder.idl:
    
    LayoutTests:
    
    * http/wpt/mediarecorder/MediaRecorder-start-timeSlice-expected.txt:
    * http/wpt/mediarecorder/MediaRecorder-start-timeSlice.html:
    
    
    Canonical link: https://commits.webkit.org/226536@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@263671 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/mediarecorder/MediaRecorder.cpp
+++ Source/WebCore/Modules/mediarecorder/MediaRecorder.cpp
@@ -126,7 +126,7 @@ const char* MediaRecorder::activeDOMObjectName() const
     return "MediaRecorder";
 }
 
-ExceptionOr<void> MediaRecorder::startRecording(Optional<int> timeSlice)
+ExceptionOr<void> MediaRecorder::startRecording(Optional<unsigned> timeSlice)
 {
     if (!m_isActive)
         return Exception { InvalidStateError, "The MediaRecorder is not active"_s };
@@ -141,11 +141,16 @@ ExceptionOr<void> MediaRecorder::startRecording(Optional<int> timeSlice)
         return Exception { NotSupportedError, "The MediaRecorder is unsupported on this platform"_s };
 
     m_private->startRecording([this, pendingActivity = makePendingActivity(*this)](auto&& exception) mutable {
-        if (!m_isActive || !exception)
+        if (!m_isActive)
             return;
 
-        stopRecordingInternal();
-        dispatchError(WTFMove(*exception));
+        if (exception) {
+            stopRecordingInternal();
+            dispatchError(WTFMove(*exception));
+            return;
+        }
+
+        dispatchEvent(Event::create(eventNames().startEvent, Event::CanBubble::No, Event::IsCancelable::No));
     });
 
     for (auto& track : m_tracks)
