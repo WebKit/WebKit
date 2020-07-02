0c7ae2efa4791d0b19938c93b89cca53f092fb19
Antoine Quint <graouts@webkit.org>
2020-07-02 20:35:14 +0000

    visualViewport.addEventListener("scroll"*** fires permanently after zoom or orientation change https://bugs.webkit.org/show_bug.cgi?id=211522 <rdar://problem/62939371>
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    Correctly set the flag indicating that a scroll event has been dispatched on the visual viewport.
    
    Test: fast/visual-viewport/scroll-event-fired-during-scroll-alone.html
    
    * dom/Document.cpp:
    (WebCore::Document::runScrollSteps):
    
    LayoutTests:
    
    Add a test that checks that after a call to scrollTo() no further "scroll" events are dispatched on window.visualViewport.
    
    * fast/visual-viewport/scroll-event-fired-during-scroll-alone-expected.txt: Added.
    * fast/visual-viewport/scroll-event-fired-during-scroll-alone.html: Added.
    
    
    Canonical link: https://commits.webkit.org/226699@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@263858 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/Document.cpp
+++ Source/WebCore/dom/Document.cpp
@@ -4130,7 +4130,7 @@ void Document::runScrollSteps()
     }
     if (m_needsVisualViewportScrollEvent) {
         LOG_WITH_STREAM(Events, stream << "Document" << this << "sending scroll events to visualViewport");
-        m_needsVisualViewportResizeEvent = false;
+        m_needsVisualViewportScrollEvent = false;
         if (auto* window = domWindow())
             window->visualViewport().dispatchEvent(Event::create(eventNames().scrollEvent, Event::CanBubble::No, Event::IsCancelable::No));
     }
