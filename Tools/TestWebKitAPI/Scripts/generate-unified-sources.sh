6cb9e27343d5f673f92e59980ef16dd76aa933a8
Darin Adler <darin@apple.com>
2020-07-04 17:18:45 +0000

    Make generate-unified-sources.sh not depend on features being listed in FEATURE_DEFINES environment variable https://bugs.webkit.org/show_bug.cgi?id=212420
    * Scripts/generate-unified-sources.sh: Removed --feature-flags from the
    arguments passed to generate-unified-source-bundles.rb. Also removed many
    unneeded quotes.
    
    Canonical link: https://commits.webkit.org/226761@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@263937 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Scripts/generate-unified-sources.sh
+++ Tools/TestWebKitAPI/Scripts/generate-unified-sources.sh
@@ -21,4 +21,4 @@ if [ $# -eq 0 ]; then
     echo "Using unified source list files: Sources.txt, SourcesCocoa.txt"
 fi
 
-/usr/bin/env ruby "${BUILD_SCRIPTS_DIR}/generate-unified-source-bundles.rb" "--derived-sources-path" "${BUILT_PRODUCTS_DIR}/DerivedSources/TestWebKitAPI" "--source-tree-path" "${SRCROOT}" "--feature-flags" "${FEATURE_DEFINES}" "--max-cpp-bundle-count" "${UnifiedSourceCppFileCount}" "--max-obj-c-bundle-count" "${UnifiedSourceMmFileCount}" "Sources.txt" "SourcesCocoa.txt" "${ARGS[@]}" > /dev/null
+/usr/bin/env ruby "${BUILD_SCRIPTS_DIR}/generate-unified-source-bundles.rb" --derived-sources-path "${BUILT_PRODUCTS_DIR}/DerivedSources/TestWebKitAPI" --source-tree-path "${SRCROOT}" --max-cpp-bundle-count ${UnifiedSourceCppFileCount} --max-obj-c-bundle-count ${UnifiedSourceMmFileCount} Sources.txt SourcesCocoa.txt "${ARGS[@]}" > /dev/null
