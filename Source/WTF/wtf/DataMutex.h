5b18ab6c098c514a4836ca6b974b3a420f6e07b5
Alicia Boya Garcia <aboya@igalia.com>
2020-07-08 10:06:49 +0000

    Merge r259879 - [WTF] DataMutex: Add runUnlocked() https://bugs.webkit.org/show_bug.cgi?id=209811
    Reviewed by Xabier Rodriguez-Calvar.
    
    Source/WTF:
    
    This patch introduces a runUnlocked() method in WTF::DataMutex::LockedWrapper
    to run a lambda function without the lock. This is intended to be used for
    small sections of the code that need to be unlocked, in cases where using
    scoping would prove non-ergonomic or where running the unlocked section is only
    necessary or desired when a certain condition is met -- something that cannot
    be done with C++ scoping.
    
    Safety mechanisms are provided. First, because this is used with a lambda, all
    variables to be used in the unlocked section have to be specified in the
    capture (global capture is possible but not recommended to simplify analysis).
    Second, additional checks have been added to DataMutex to detect unlocked
    accesses among other conditions. This will detect among other things naive
    access to protected members by means of capturing the LockedWrapper by
    reference.
    
    * wtf/DataMutex.h:
    (WTF::OwnerAwareLockAdapter::lock):
    (WTF::OwnerAwareLockAdapter::unlock):
    (WTF::OwnerAwareLockAdapter::tryLock):
    (WTF::OwnerAwareLockAdapter::isLocked const):
    (WTF::DataMutex::LockedWrapper::operator->):
    (WTF::DataMutex::LockedWrapper::operator*):
    (WTF::DataMutex::LockedWrapper::mutex):
    (WTF::DataMutex::LockedWrapper::lockHolder):
    (WTF::DataMutex::LockedWrapper::runUnlocked):
    
    Tools:
    
    Tests for runUnlocked() and DataMutex checks are introduced.
    
    * TestWebKitAPI/Tests/WTF/DataMutex.cpp:
    (TestWebKitAPI::TEST):

--- Source/WTF/wtf/DataMutex.h
+++ Source/WTF/wtf/DataMutex.h
@@ -21,10 +21,74 @@
 #pragma once
 
 #include <wtf/Lock.h>
+#include <wtf/Threading.h>
 
 namespace WTF {
 
-template<typename T>
+// By default invalid access checks are only done in Debug builds.
+#if !defined(ENABLE_DATA_MUTEX_CHECKS)
+#if defined(NDEBUG)
+#define ENABLE_DATA_MUTEX_CHECKS 0
+#else
+#define ENABLE_DATA_MUTEX_CHECKS 1
+#endif
+#endif
+
+#if ENABLE_DATA_MUTEX_CHECKS
+#define DATA_MUTEX_CHECK(expr) RELEASE_ASSERT(expr)
+#else
+#define DATA_MUTEX_CHECK(expr)
+#endif
+
+template<typename LockType>
+class OwnerAwareLockAdapter {
+public:
+    void lock()
+    {
+        DATA_MUTEX_CHECK(m_owner != &Thread::current()); // Thread attempted recursive lock (unsupported).
+        m_lock.lock();
+#if ENABLE_DATA_MUTEX_CHECKS
+        ASSERT(!m_owner);
+        m_owner = &Thread::current();
+#endif
+    }
+
+    void unlock()
+    {
+#if ENABLE_DATA_MUTEX_CHECKS
+        m_owner = nullptr;
+#endif
+        m_lock.unlock();
+    }
+
+    bool tryLock()
+    {
+        DATA_MUTEX_CHECK(m_owner != &Thread::current()); // Thread attempted recursive lock (unsupported).
+        if (!m_lock.tryLock())
+            return false;
+
+#if ENABLE_DATA_MUTEX_CHECKS
+        ASSERT(!m_owner);
+        m_owner = &Thread::current();
+#endif
+        return true;
+    }
+
+    bool isLocked() const
+    {
+        return m_lock.isLocked();
+    }
+
+private:
+#if ENABLE_DATA_MUTEX_CHECKS
+    Thread* m_owner { nullptr }; // Use Thread* instead of RefPtr<Thread> since m_owner thread is always alive while m_owner is set.
+#endif
+    LockType m_lock;
+};
+
+using OwnerAwareLock = OwnerAwareLockAdapter<Lock>;
+
+template<typename T, typename LockType = OwnerAwareLock>
 class DataMutex {
     WTF_MAKE_FAST_ALLOCATED;
     WTF_MAKE_NONCOPYABLE(DataMutex);
@@ -44,32 +108,44 @@ public:
 
         T* operator->()
         {
+            DATA_MUTEX_CHECK(m_mutex.isLocked());
             return &m_data;
         }
 
         T& operator*()
         {
+            DATA_MUTEX_CHECK(m_mutex.isLocked());
             return m_data;
         }
 
-        Lock& mutex()
+        LockType& mutex()
         {
             return m_mutex;
         }
 
-        LockHolder& lockHolder()
+        Locker<LockType>& lockHolder()
         {
             return m_lockHolder;
         }
 
+        // Used to avoid excessive brace scoping when only small parts of the code need to be run unlocked.
+        // Please be mindful that accessing the wrapped data from the callback is unsafe and will fail on assertions.
+        // It's helpful to use a minimal lambda capture to be conscious of what data you're having access to in these sections.
+        void runUnlocked(WTF::Function<void()> callback)
+        {
+            m_mutex.unlock();
+            callback();
+            m_mutex.lock();
+        }
+
     private:
-        Lock& m_mutex;
-        LockHolder m_lockHolder;
+        LockType& m_mutex;
+        Locker<LockType> m_lockHolder;
         T& m_data;
     };
 
 private:
-    Lock m_mutex;
+    LockType m_mutex;
     T m_data;
 };
 
