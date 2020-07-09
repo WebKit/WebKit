e0d59d29f377199c70166a697f891be04aa1a17c
Chris Dumez <cdumez@apple.com>
2020-07-09 02:49:33 +0000

    Resync web-platform-tests/websockets from upstream https://bugs.webkit.org/show_bug.cgi?id=214107
    Reviewed by Sam Weinig.
    
    Resync web-platform-tests/websockets from upstream cef93b58e7cf31f1d47cc.
    
    * web-platform-tests/websockets/*: Updated.
    
    
    Canonical link: https://commits.webkit.org/226922@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@264145 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/imported/w3c/web-platform-tests/websockets/Create-Secure-extensions-empty.any.js
+++ LayoutTests/imported/w3c/web-platform-tests/websockets/Create-Secure-extensions-empty.any.js
@@ -4,18 +4,15 @@
 var testOpen = async_test("Create Secure WebSocket - wsocket.extensions should be set to '' after connection is established - Connection should be opened");
 var testClose = async_test("Create Secure WebSocket - wsocket.extensions should be set to '' after connection is established - Connection should be closed");
 
-var wsocket = CreateWebSocket(true, false, false);
+var wsocket = new WebSocket("wss://" + __SERVER__NAME + ":" + __SECURE__PORT + "/handshake_no_extensions");
 var isOpenCalled = false;
 
-wsocket.addEventListener('open', testOpen.step_func(function(evt) {
-  assert_equals(wsocket.extensions, "", "extensions should be empty");
+wsocket.addEventListener('open', testOpen.step_func_done(function(evt) {
   wsocket.close();
   isOpenCalled = true;
-  testOpen.done();
+  assert_equals(wsocket.extensions, "", "extensions should be empty");
 }), true);
 
-wsocket.addEventListener('close', testClose.step_func(function(evt) {
+wsocket.addEventListener('close', testClose.step_func_done(function(evt) {
   assert_true(isOpenCalled, "WebSocket connection should be closed");
-  assert_equals(evt.wasClean, true, "wasClean should be true");
-  testClose.done();
 }), true);
