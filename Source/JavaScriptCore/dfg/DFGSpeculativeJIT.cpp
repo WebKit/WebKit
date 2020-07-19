6167727732fc864b3d809d96c4b2971f6e6e6f90
Mark Lam <mark.lam@apple.com>
2020-07-19 04:24:20 +0000

    Fixed regression due to r264507: Math.{min|max} inequality test should use DoubleNotEqualOrUnordered instead DoubleNotEqualAndOrdered. https://bugs.webkit.org/show_bug.cgi?id=214526 <rdar://problem/65778061>
    Reviewed by Yusuke Suzuki.
    
    JSTests:
    
    * stress/math-max-min.js: Added.
    (isNegativeZero):
    (numberAsString):
    (testMax):
    (testMin):
    (assertEq):
    
    Source/JavaScriptCore:
    
    This bug resulted in NaNs being handled by the "equal" case in some scenarios,
    which resulted in an assertion failure in a ValueRep on an internal test.
    
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compileArithMinMax):
    
    
    
    Canonical link: https://commits.webkit.org/227323@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@264575 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp
+++ Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp
@@ -6181,7 +6181,7 @@ void SpeculativeJIT::compileArithMinMax(Node* node)
         MacroAssembler::JumpList done;
 
         MacroAssembler::Jump op1Less = m_jit.branchDouble(node->op() == ArithMin ? MacroAssembler::DoubleLessThanAndOrdered : MacroAssembler::DoubleGreaterThanAndOrdered, op1FPR, op2FPR);
-        MacroAssembler::Jump opNotEqual = m_jit.branchDouble(MacroAssembler::DoubleNotEqualAndOrdered, op1FPR, op2FPR);
+        MacroAssembler::Jump opNotEqualOrUnordered = m_jit.branchDouble(MacroAssembler::DoubleNotEqualOrUnordered, op1FPR, op2FPR);
 
         // The spec for Math.min and Math.max states that +0 is considered to be larger than -0.
         if (node->op() == ArithMin)
@@ -6191,11 +6191,11 @@ void SpeculativeJIT::compileArithMinMax(Node* node)
 
         done.append(m_jit.jump());
 
-        opNotEqual.link(&m_jit);
+        opNotEqualOrUnordered.link(&m_jit);
         // op2 is either the lesser one or one of then is NaN
         MacroAssembler::Jump op2Less = m_jit.branchDouble(node->op() == ArithMin ? MacroAssembler::DoubleGreaterThanAndOrdered : MacroAssembler::DoubleLessThanAndOrdered, op1FPR, op2FPR);
 
-        // Unordered case. We don't know which of op1, op2 is NaN. Manufacture NaN by adding 
+        // Unordered case. We don't know which of op1, op2 is NaN. Manufacture NaN by adding
         // op1 + op2 and putting it into result.
         m_jit.addDouble(op1FPR, op2FPR, resultFPR);
         done.append(m_jit.jump());
