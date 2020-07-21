f9527eda2b667dae13381b4ea35d10258d31dab1
Michael Catanzaro <mcatanzaro@gnome.org>
2020-07-21 18:54:25 +0000

    REGRESSION(r264330): infinite loop in JSC stress tests using cloop https://bugs.webkit.org/show_bug.cgi?id=214491
    Patch by Michael Catanzaro <mcatanzaro@gnome.org> on 2020-07-21
    Reviewed by Saam Barati.
    
    Run the test only if JIT is enabled.
    
    * stress/validate-does-gc-with-return-early-from-infinite-loop.js:
    (vm.useJIT):
    
    Canonical link: https://commits.webkit.org/227413@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@264668 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/validate-does-gc-with-return-early-from-infinite-loop.js
+++ JSTests/stress/validate-does-gc-with-return-early-from-infinite-loop.js
@@ -1,4 +1,6 @@
 //@ skip if $architecture != "arm64" and $architecture != "x86-64"
 //@ runDefault("--returnEarlyFromInfiniteLoopsForFuzzing=true", "--earlyReturnFromInfiniteLoopsLimit=1000000", "--useConcurrentJIT=false", "--validateDoesGC=true")
 
-while(1){} while(1){}
+if ($vm.useJIT()) {
+    while(1){} while(1){}
+}
