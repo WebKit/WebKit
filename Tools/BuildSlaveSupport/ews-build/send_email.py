5c735433337cce4b98a83931db581bc9195ae5b9
Aakash Jain <aakash_jain@apple.com>
2020-07-23 10:44:27 +0000

    [ews] Add support to send emails in ews https://bugs.webkit.org/show_bug.cgi?id=214670
    Reviewed by Darin Adler.
    
    * BuildSlaveSupport/ews-build/send_email.py: Added script to send emails.
    (send_email):
    (send_email_to_bot_watchers):
    
    
    Canonical link: https://commits.webkit.org/227491@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@264752 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Tools/BuildSlaveSupport/ews-build/send_email.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2020 Apple Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import smtplib
+
+BOT_WATCHERS_EMAILS = ['webkit-bot-watchers-bugzilla@group.apple.com']
+FROM_EMAIL = 'ews@webkit.org'
+SERVER = 'localhost'
+
+
+def send_email(to_emails, subject, text):
+    email_data = """From: {}\nTo: {}\nSubject: {}\n\n{}""".format(FROM_EMAIL, ', '.join(to_emails), subject, text)
+
+    server = smtplib.SMTP(SERVER)
+    server.sendmail(FROM_EMAIL, to_emails, email_data)
+    server.quit()
+
+
+def send_email_to_bot_watchers(subject, text):
+    send_email(BOT_WATCHERS_EMAILS, subject, text)
