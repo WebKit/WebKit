64500ff940759c12a590cc7c81945b25d09f9f88
Angelos Oikonomopoulos <angelos@igalia.com>
2020-07-23 11:23:28 +0000

    Fix logic error in r264703 https://bugs.webkit.org/show_bug.cgi?id=214674
    Unreviewed Gardening.
    
    Patch by Angelos Oikonomopoulos <angelos@igalia.com> on 2020-07-23
    
    * stress/intl-displaynames.js:
    * stress/intl-parse-unicode-subtags.js:
    
    Canonical link: https://commits.webkit.org/227493@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@264754 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/intl-parse-unicode-subtags.js
+++ JSTests/stress/intl-parse-unicode-subtags.js
@@ -1,5 +1,5 @@
-//@ runDefault("--useIntlDisplayNames=1")
 //@ skip if $architecture == "arm"
+//@ runDefault("--useIntlDisplayNames=1")
 function shouldBe(actual, expected) {
     if (actual !== expected)
         throw new Error('bad value: ' + actual);
