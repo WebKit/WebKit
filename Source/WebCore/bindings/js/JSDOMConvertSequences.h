3a8a2160f4f2f886b39d9b90bbf0637bc8de9983
Mark Lam <mark.lam@apple.com>
2020-07-24 22:56:01 +0000

    Fix exception check handling below postMessage(). https://bugs.webkit.org/show_bug.cgi?id=214761 <rdar://problem/65932136>
    Reviewed by Yusuke Suzuki and Keith Miller.
    
    Source/WebCore:
    
    Test: js/dom/missing-exception-check-below-postMessage.html
    
    * bindings/js/JSDOMConvertSequences.h:
    (WebCore::Detail::SequenceConverter::convertArray):
    (WebCore::Detail::SequenceConverter::convert):
    
    LayoutTests:
    
    * js/dom/missing-exception-check-below-postMessage-expected.txt: Added.
    * js/dom/missing-exception-check-below-postMessage.html: Added.
    
    
    
    Canonical link: https://commits.webkit.org/227589@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@264866 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/js/JSDOMConvertSequences.h
+++ Source/WebCore/bindings/js/JSDOMConvertSequences.h
@@ -195,8 +195,10 @@ struct SequenceConverter {
     using GenericConverter = GenericSequenceConverter<IDLType>;
     using ReturnType = typename GenericConverter::ReturnType;
 
-    static ReturnType convertArray(JSC::JSGlobalObject& lexicalGlobalObject, JSC::ThrowScope& scope, JSC::JSArray* array)
+    static ReturnType convertArray(JSC::JSGlobalObject& lexicalGlobalObject, JSC::JSArray* array)
     {
+        auto& vm = lexicalGlobalObject.vm();
+        auto scope = DECLARE_THROW_SCOPE(vm);
         unsigned length = array->length();
 
         ReturnType result;
@@ -249,23 +251,20 @@ struct SequenceConverter {
 
         JSC::JSObject* object = JSC::asObject(value);
         if (Converter<IDLType>::conversionHasSideEffects)
-            return GenericConverter::convert(lexicalGlobalObject, object);
+            RELEASE_AND_RETURN(scope, (GenericConverter::convert(lexicalGlobalObject, object)));
 
         if (!JSC::isJSArray(object))
-            return GenericConverter::convert(lexicalGlobalObject, object);
+            RELEASE_AND_RETURN(scope, (GenericConverter::convert(lexicalGlobalObject, object)));
 
         JSC::JSArray* array = JSC::asArray(object);
         if (!array->isIteratorProtocolFastAndNonObservable())
-            return GenericConverter::convert(lexicalGlobalObject, object);
+            RELEASE_AND_RETURN(scope, (GenericConverter::convert(lexicalGlobalObject, object)));
         
-        return convertArray(lexicalGlobalObject, scope, array);
+        RELEASE_AND_RETURN(scope, (convertArray(lexicalGlobalObject, array)));
     }
 
     static ReturnType convert(JSC::JSGlobalObject& lexicalGlobalObject, JSC::JSObject* object, JSC::JSValue method)
     {
-        auto& vm = JSC::getVM(&lexicalGlobalObject);
-        auto scope = DECLARE_THROW_SCOPE(vm);
-
         if (Converter<IDLType>::conversionHasSideEffects)
             return GenericConverter::convert(lexicalGlobalObject, object, method);
 
@@ -276,7 +275,7 @@ struct SequenceConverter {
         if (!array->isIteratorProtocolFastAndNonObservable())
             return GenericConverter::convert(lexicalGlobalObject, object, method);
 
-        return convertArray(lexicalGlobalObject, scope, array);
+        return convertArray(lexicalGlobalObject, array);
     }
 };
 
