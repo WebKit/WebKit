c5e65267cd08bc22fe6d71cd342fb3bef2d90431
Eric Carlson <eric.carlson@apple.com>
2020-07-24 22:52:22 +0000

    [Cocoa] Tell AVAssetResourceLoadingContentInformationRequest that asset size is available immediately https://bugs.webkit.org/show_bug.cgi?id=214759 <rdar://problem/63751958>
    Reviewed by Jer Noble.
    
    * platform/graphics/avfoundation/objc/WebCoreAVFResourceLoader.mm:
    (WebCore::WebCoreAVFResourceLoader::responseReceived):
    
    
    Canonical link: https://commits.webkit.org/227587@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@264864 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/avfoundation/objc/WebCoreAVFResourceLoader.mm
+++ Source/WebCore/platform/graphics/avfoundation/objc/WebCoreAVFResourceLoader.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2017 Apple Inc. All rights reserved.
+ * Copyright (C) 2012-2020 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,6 +39,10 @@
 #import <objc/runtime.h>
 #import <wtf/SoftLinking.h>
 #import <wtf/text/CString.h>
+
+@interface AVAssetResourceLoadingContentInformationRequest (WebKitExtensions)
+@property (nonatomic, getter=isEntireLengthAvailableOnDemand) BOOL entireLengthAvailableOnDemand;
+@end
 
 namespace WebCore {
 
@@ -298,6 +302,9 @@ void WebCoreAVFResourceLoader::responseReceived(const ResourceResponse& response
 
         [contentInfo setContentLength:contentRange.isValid() ? contentRange.instanceLength() : response.expectedContentLength()];
         [contentInfo setByteRangeAccessSupported:YES];
+        
+        if ([contentInfo respondsToSelector:@selector(setEntireLengthAvailableOnDemand:)])
+            [contentInfo setEntireLengthAvailableOnDemand:YES];
 
         if (![m_avRequest dataRequest]) {
             [m_avRequest.get() finishLoading];
