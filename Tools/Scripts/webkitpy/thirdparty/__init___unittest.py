81b7e0d6640bdeca4c0ee6eb7bdcd511e993b44e
Jonathan Bedard <jbedard@apple.com>
2020-07-25 00:12:38 +0000

    pep8.py:110: FutureWarning: Possible nested set at position 1 (Follow-up fix) https://bugs.webkit.org/show_bug.cgi?id=214531
    Unreviewed test fix.
    
    
    * Scripts/webkitpy/thirdparty/__init___unittest.py:
    (ThirdpartyTest.test_imports): Auto-import of pep8 has been removed.
    
    
    Canonical link: https://commits.webkit.org/227595@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@264872 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/thirdparty/__init___unittest.py
+++ Tools/Scripts/webkitpy/thirdparty/__init___unittest.py
@@ -40,4 +40,3 @@ class ThirdpartyTest(unittest.TestCase):
         import webkitpy.thirdparty.autoinstalled.coverage
         import webkitpy.thirdparty.autoinstalled.mechanize
         import webkitpy.thirdparty.autoinstalled.pylint
-        import webkitpy.thirdparty.autoinstalled.pep8
