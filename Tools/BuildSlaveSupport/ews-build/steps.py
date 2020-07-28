0a3407c484c4b474bccd39ec18392d6fad922f45
Aakash Jain <aakash_jain@apple.com>
2020-07-28 23:04:21 +0000

    [ews] Add macOS Big Sur support in PrintConfiguration https://bugs.webkit.org/show_bug.cgi?id=214903
    Reviewed by Darin Adler.
    
    * BuildSlaveSupport/ews-build/steps.py:
    (PrintConfiguration.convert_build_to_os_name):
    
    
    Canonical link: https://commits.webkit.org/227719@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@265017 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/BuildSlaveSupport/ews-build/steps.py
+++ Tools/BuildSlaveSupport/ews-build/steps.py
@@ -2547,6 +2547,7 @@ class PrintConfiguration(steps.ShellSequence):
             return 'Unknown'
 
         build_to_name_mapping = {
+            '11.0': 'Big Sur',
             '10.15': 'Catalina',
             '10.14': 'Mojave',
             '10.13': 'High Sierra',
