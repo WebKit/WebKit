645bed8d236a7e89165363451446aae9dbd6c20e
Adrian Perez de Castro <aperez@igalia.com>
2020-07-29 15:33:09 +0000

    Unreviewed. Update OptionsWPE.cmake and NEWS for the 2.29.2 release
    .:
    
    * Source/cmake/OptionsWPE.cmake: Bump version numbers.
    
    Source/WebKit:
    
    * wpe/NEWS: Add release notes for 2.29.2.
    
    Canonical link: https://commits.webkit.org/227742@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@265041 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/OptionsWPE.cmake
+++ Source/cmake/OptionsWPE.cmake
@@ -1,10 +1,10 @@
 include(GNUInstallDirs)
 include(VersioningUtils)
 
-SET_PROJECT_VERSION(2 29 1)
+SET_PROJECT_VERSION(2 29 2)
 set(WPE_API_VERSION 1.0)
 
-CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT 13 0 10)
+CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT 14 0 11)
 
 # These are shared variables, but we special case their definition so that we can use the
 # CMAKE_INSTALL_* variables that are populated by the GNUInstallDirs macro.
