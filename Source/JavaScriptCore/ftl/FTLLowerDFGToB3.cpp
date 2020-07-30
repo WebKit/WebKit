cc52d1c47f86e3bde784da6579ca7f59ded9257d
Saam Barati <sbarati@apple.com>
2020-07-30 21:44:45 +0000

    Strip pointers instead of authing for byteOffset to not allow for a possible way to guess data pac https://bugs.webkit.org/show_bug.cgi?id=214952
    Reviewed by Keith Miller.
    
    In the old way of doing things, we would auth the vector pointer before subtracting
    the base from it. Since we never validated the auth, this allowed for a
    potential data-PAC bypass by just repeatedly calling byteOffset in a loop
    and observing the integer result of the operation.
    
    Since byteOffset does no loads/stores, it suffices to just strip the PAC
    bits before doing the subtraction. This eliminates any such attacks like
    the above because the PAC bits are ignored.
    
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compileGetTypedArrayByteOffset):
    * ftl/FTLLowerDFGToB3.cpp:
    (JSC::FTL::DFG::LowerDFGToB3::compileGetTypedArrayByteOffset):
    
    
    Canonical link: https://commits.webkit.org/227797@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@265097 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp
+++ Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp
@@ -4361,31 +4361,23 @@ private:
         LValue basePtr = lowCell(m_node->child1());    
 
         LBasicBlock wastefulCase = m_out.newBlock();
-        LBasicBlock notNull = m_out.newBlock();
         LBasicBlock continuation = m_out.newBlock();
         
-        ValueFromBlock nullVectorOut = m_out.anchor(m_out.constIntPtr(0));
+        ValueFromBlock nonWastefulResult = m_out.anchor(m_out.constIntPtr(0));
 
         LValue mode = m_out.load32(basePtr, m_heaps.JSArrayBufferView_mode);
         m_out.branch(
             m_out.notEqual(mode, m_out.constInt32(WastefulTypedArray)),
             unsure(continuation), unsure(wastefulCase));
 
-        LBasicBlock lastNext = m_out.appendTo(wastefulCase, notNull);
+        LBasicBlock lastNext = m_out.appendTo(wastefulCase, continuation);
 
-        LValue vector = m_out.loadPtr(basePtr, m_heaps.JSArrayBufferView_vector);
-        m_out.branch(m_out.equal(vector, m_out.constIntPtr(JSArrayBufferView::nullVectorPtr())), 
-            unsure(continuation), unsure(notNull));
-
-        m_out.appendTo(notNull, continuation);
+        LValue vectorPtr = m_out.loadPtr(basePtr, m_heaps.JSArrayBufferView_vector);
+        vectorPtr = removeArrayPtrTag(vectorPtr);
 
         LValue butterflyPtr = caged(Gigacage::JSValue, m_out.loadPtr(basePtr, m_heaps.JSObject_butterfly), basePtr);
         LValue arrayBufferPtr = m_out.loadPtr(butterflyPtr, m_heaps.Butterfly_arrayBuffer);
 
-        LValue vectorPtr = caged(Gigacage::Primitive, vector, basePtr);
-
-        // FIXME: This needs caging.
-        // https://bugs.webkit.org/show_bug.cgi?id=175515
         LValue dataPtr = m_out.loadPtr(arrayBufferPtr, m_heaps.ArrayBuffer_data);
         dataPtr = removeArrayPtrTag(dataPtr);
 
@@ -4394,7 +4386,7 @@ private:
         m_out.jump(continuation);
         m_out.appendTo(continuation, lastNext);
 
-        setInt32(m_out.castToInt32(m_out.phi(pointerType(), nullVectorOut, wastefulOut)));
+        setInt32(m_out.castToInt32(m_out.phi(pointerType(), nonWastefulResult, wastefulOut)));
     }
 
     void compileGetPrototypeOf()
