4b0b8aea71a7cb6ebc6f525ab27329949d008838
Per Arne Vollan <pvollan@apple.com>
2020-08-04 23:10:17 +0000

    [iOS] Issue a temporary extension to the MobileGestalt daemon when the MobileGestalt cache is invalid https://bugs.webkit.org/show_bug.cgi?id=214965
    Unreviewed, address review comments.
    
    Add comment explaining why a set of MobileGestalt queries are performed when starting up the WebContent process on iOS.
    
    * WebProcess/cocoa/WebProcessCocoa.mm:
    (WebKit::WebProcess::platformInitializeWebProcess):
    
    
    
    Canonical link: https://commits.webkit.org/227949@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@265267 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/cocoa/WebProcessCocoa.mm
+++ Source/WebKit/WebProcess/cocoa/WebProcessCocoa.mm
@@ -207,6 +207,12 @@ void WebProcess::platformInitializeWebProcess(WebProcessCreationParameters& para
         if (auto extension = SandboxExtension::create(WTFMove(*parameters.mobileGestaltExtensionHandle))) {
             bool ok = extension->consume();
             ASSERT_UNUSED(ok, ok);
+            // If we have an extension handle for MobileGestalt, it means the MobileGestalt cache is invalid.
+            // In this case, we perform a set of MobileGestalt queries while having access to the daemon,
+            // which will populate the MobileGestalt in-memory cache with correct values.
+            // The set of queries below was determined by finding all possible queries that have cachable
+            // values, and would reach out to the daemon for the answer. That way, the in-memory cache
+            // should be identical to a valid MobileGestalt cache after having queried all of these values.
 #if PLATFORM(IOS_FAMILY) && !PLATFORM(MACCATALYST)
             MGGetFloat32Answer(kMGQMainScreenScale, 0);
             MGGetSInt32Answer(kMGQMainScreenPitch, 0);
