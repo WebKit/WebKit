2826a6afacbd23d172d3becb30aa7cc6ba2bd446
Per Arne Vollan <pvollan@apple.com>
2020-08-17 21:54:42 +0000

    [Cocoa] Avoid waiting for Launch Services on every load https://bugs.webkit.org/show_bug.cgi?id=215569
    Reviewed by Darin Adler.
    
    Currently, we are waiting for the Launch Services database to be present on every load in the WebContent process.
    It should be sufficient to wait only on the first load. Since we now are waiting only once per WebContent process,
    increase the maximum wait time from 1s to 5s.
    
    No new tests, covered by existing tests.
    
    * WebProcess/WebPage/Cocoa/WebPageCocoa.mm:
    (WebKit::WebPage::platformDidReceiveLoadParameters):
    
    
    Canonical link: https://commits.webkit.org/228341@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@265774 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/WebPage/Cocoa/WebPageCocoa.mm
+++ Source/WebKit/WebProcess/WebPage/Cocoa/WebPageCocoa.mm
@@ -65,14 +65,18 @@ namespace WebKit {
 void WebPage::platformDidReceiveLoadParameters(const LoadParameters& parameters)
 {
 #if HAVE(LSDATABASECONTEXT)
-    auto startTime = WallTime::now();
-    bool databaseUpdated = LaunchServicesDatabaseManager::singleton().waitForDatabaseUpdate(1_s);
-    auto elapsedTime = WallTime::now() - startTime;
-    if (elapsedTime.value() > 0.5)
-        RELEASE_LOG(Loading, "Waiting for Launch Services database update took %f seconds", elapsedTime.value());
-    ASSERT_UNUSED(databaseUpdated, databaseUpdated);
-    if (!databaseUpdated)
-        RELEASE_LOG_ERROR(Loading, "Timed out waiting for Launch Services database update.");
+    static bool hasWaitedForLaunchServicesDatabase = false;
+    if (!hasWaitedForLaunchServicesDatabase) {
+        auto startTime = WallTime::now();
+        bool databaseUpdated = LaunchServicesDatabaseManager::singleton().waitForDatabaseUpdate(5_s);
+        auto elapsedTime = WallTime::now() - startTime;
+        if (elapsedTime.value() > 0.5)
+            RELEASE_LOG(Loading, "Waiting for Launch Services database update took %f seconds", elapsedTime.value());
+        ASSERT_UNUSED(databaseUpdated, databaseUpdated);
+        if (!databaseUpdated)
+            RELEASE_LOG_ERROR(Loading, "Timed out waiting for Launch Services database update.");
+        hasWaitedForLaunchServicesDatabase = true;
+    }
 #endif
 
     m_dataDetectionContext = parameters.dataDetectionContext;
