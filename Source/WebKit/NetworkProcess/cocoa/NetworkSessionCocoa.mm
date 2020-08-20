f6b34c5740366b396c523a4a8da285f16cc9a9f3
Alan Coon <alancoon@apple.com>
2020-08-20 00:30:19 +0000

    Revert r265878. rdar://problem/67364266
    Canonical link: https://commits.webkit.org/227865.91@safari-610.1.28.0-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610.1.28.0-branch@265914 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/NetworkProcess/cocoa/NetworkSessionCocoa.mm
+++ Source/WebKit/NetworkProcess/cocoa/NetworkSessionCocoa.mm
@@ -1546,12 +1546,6 @@ void NetworkSessionCocoa::continueDidReceiveChallenge(SessionWrapper& sessionWra
 #endif
         completionHandler(disposition, credential);
     };
-
-    if (negotiatedLegacyTLS == NegotiatedLegacyTLS::Yes
-        && fastServerTrustEvaluationEnabled()
-        && !networkDataTask->isTopLevelNavigation())
-        return completionHandler(AuthenticationChallengeDisposition::Cancel, { });
-
     networkDataTask->didReceiveChallenge(WTFMove(authenticationChallenge), negotiatedLegacyTLS, WTFMove(challengeCompletionHandler));
 }
 
