2d36d2ddb0fe2d889b3f7bba0e0d684952f03a6c
Per Arne Vollan <pvollan@apple.com>
2020-08-26 20:04:37 +0000

    Cherry-pick r265996. rdar://problem/67707045
        [macOS] Web pages are not responding correctly to changes in "Reduce motion" setting
        https://bugs.webkit.org/show_bug.cgi?id=215664
    
        Reviewed by Darin Adler.
    
        Source/WebCore/PAL:
    
        Declare "Reduce motion" preference key, as well as notification sent when the "Reduce motion" setting is changed.
    
        * pal/spi/mac/HIServicesSPI.h:
    
        Source/WebKit:
    
        This happens because there is a race between the preference change and notification being received in the WebContent
        process. This race is usually won by the notification, which then reads the wrong preference value when being handled.
        This bug was introduced when access to the preference daemon was closed in the WebContent process, and preferences
        were updated from the UI process. This patch addresses this issue by re-posting the notification when the preference
        value is updated in the WebContent process.
    
        API test: WebKit.AccessibilityReduceMotion
    
        * WebProcess/cocoa/WebProcessCocoa.mm:
        (WebKit::reduceMotionPreferenceKey):
        (WebKit::dispatchSimulatedNotificationsForPreferenceChange):
    
        Tools:
    
        * TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
        * TestWebKitAPI/Tests/WebKit/AccessibilityReduceMotion.mm: Added.
        (TEST):
    
        Canonical link: https://commits.webkit.org/228477@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@265996 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/227865.97@safari-610.1.28.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610.1.28.1-branch@266185 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/cocoa/WebProcessCocoa.mm
+++ Source/WebKit/WebProcess/cocoa/WebProcessCocoa.mm
@@ -126,6 +126,7 @@
 #import "WebSwitchingGPUClient.h"
 #import <WebCore/GraphicsContextGLOpenGLManager.h>
 #import <WebCore/ScrollbarThemeMac.h>
+#import <pal/spi/mac/HIServicesSPI.h>
 #import <pal/spi/mac/NSScrollerImpSPI.h>
 #endif
 
@@ -941,6 +942,12 @@ static const WTF::String& userHighlightColorPreferenceKey()
     static NeverDestroyed<WTF::String> userHighlightColorPreferenceKey(MAKE_STATIC_STRING_IMPL("AppleHighlightColor"));
     return userHighlightColorPreferenceKey;
 }
+
+static const WTF::String& reduceMotionPreferenceKey()
+{
+    static NeverDestroyed<WTF::String> key(MAKE_STATIC_STRING_IMPL("reduceMotion"));
+    return key;
+}
 #endif
 
 static void dispatchSimulatedNotificationsForPreferenceChange(const String& key)
@@ -951,15 +958,17 @@ static void dispatchSimulatedNotificationsForPreferenceChange(const String& key)
     // of the system, we must re-post the notification in the Web Content process after updating the default.
     
     if (key == userAccentColorPreferenceKey()) {
-        NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+        auto notificationCenter = [NSNotificationCenter defaultCenter];
         [notificationCenter postNotificationName:@"kCUINotificationAquaColorVariantChanged" object:nil];
         [notificationCenter postNotificationName:@"NSSystemColorsWillChangeNotification" object:nil];
         [notificationCenter postNotificationName:NSSystemColorsDidChangeNotification object:nil];
-    }
-    if (key == userHighlightColorPreferenceKey()) {
-        NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    } else if (key == userHighlightColorPreferenceKey()) {
+        auto notificationCenter = [NSNotificationCenter defaultCenter];
         [notificationCenter postNotificationName:@"NSSystemColorsWillChangeNotification" object:nil];
         [notificationCenter postNotificationName:NSSystemColorsDidChangeNotification object:nil];
+    } else if (key == reduceMotionPreferenceKey()) {
+        auto notificationCenter = CFNotificationCenterGetDistributedCenter();
+        CFNotificationCenterPostNotification(notificationCenter, kAXInterfaceReduceMotionStatusDidChangeNotification, nullptr, nullptr, true);
     }
 #endif
 }
