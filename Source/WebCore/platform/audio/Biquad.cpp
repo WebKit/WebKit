8a49d54d594b05ff1a47d81a6f8b18371b2595e7
Chris Dumez <cdumez@apple.com>
2020-09-03 20:54:28 +0000

    BiquadFilterNode.getFrequencyResponse() should return NaN for out-of-bounds frequencies https://bugs.webkit.org/show_bug.cgi?id=216130
    Reviewed by Eric Carlson.
    
    LayoutTests/imported/w3c:
    
    Rebaseline WPT test now that it is passing.
    
    * web-platform-tests/webaudio/the-audio-api/the-biquadfilternode-interface/biquad-getFrequencyResponse-expected.txt:
    
    Source/WebCore:
    
    BiquadFilterNode.getFrequencyResponse() should return NaN for out-of-bounds frequencies.
    
    No new tests, rebaselined existing test.
    
    * platform/audio/Biquad.cpp:
    (WebCore::Biquad::getFrequencyResponse):
    
    
    Canonical link: https://commits.webkit.org/228943@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@266541 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/audio/Biquad.cpp
+++ Source/WebCore/platform/audio/Biquad.cpp
@@ -576,13 +576,19 @@ void Biquad::getFrequencyResponse(unsigned nFrequencies, const float* frequency,
     double a2 = m_a2[0];
     
     for (unsigned k = 0; k < nFrequencies; ++k) {
-        double omega = -piDouble * frequency[k];
-        std::complex<double> z = std::complex<double>(cos(omega), sin(omega));
-        std::complex<double> numerator = b0 + (b1 + b2 * z) * z;
-        std::complex<double> denominator = std::complex<double>(1, 0) + (a1 + a2 * z) * z;
-        std::complex<double> response = numerator / denominator;
-        magResponse[k] = static_cast<float>(abs(response));
-        phaseResponse[k] = static_cast<float>(atan2(imag(response), real(response)));
+        if (frequency[k] < 0 || frequency[k] > 1) {
+            // Out-of-bounds frequencies should return NaN.
+            magResponse[k] = std::nanf("");
+            phaseResponse[k] = std::nanf("");
+        } else {
+            double omega = -piDouble * frequency[k];
+            std::complex<double> z = std::complex<double>(cos(omega), sin(omega));
+            std::complex<double> numerator = b0 + (b1 + b2 * z) * z;
+            std::complex<double> denominator = std::complex<double>(1, 0) + (a1 + a2 * z) * z;
+            std::complex<double> response = numerator / denominator;
+            magResponse[k] = static_cast<float>(abs(response));
+            phaseResponse[k] = static_cast<float>(atan2(imag(response), real(response)));
+        }
     }
 }
 
