0b331eccee4c06edc47a0211820a69f12d72b253
Myles C. Maxfield <mmaxfield@apple.com>
2020-09-05 07:20:02 +0000

    Fix the build after r266614 https://bugs.webkit.org/show_bug.cgi?id=216174
    Unreviewed.
    
    * testing/Internals.cpp:
    (WebCore::Internals::supportsVCPEncoder): These parentheses really should be in
    Source/ThirdParty/libwebrtc/Source/webrtc/sdk/WebKit/VideoProcessingSoftLink.h,
    but I don't know the process for updating that third-party library. This is a
    short-term build fix.
    
    Canonical link: https://commits.webkit.org/229026@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@266657 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/testing/Internals.cpp
+++ Source/WebCore/testing/Internals.cpp
@@ -5681,7 +5681,7 @@ bool Internals::capsLockIsOn()
 bool Internals::supportsVCPEncoder()
 {
 #if defined(ENABLE_VCP_ENCODER)
-    return ENABLE_VCP_ENCODER || ENABLE_VCP_VTB_ENCODER || HAVE_VTB_REQUIREDLOWLATENCY;
+    return ENABLE_VCP_ENCODER || (ENABLE_VCP_VTB_ENCODER) || HAVE_VTB_REQUIREDLOWLATENCY;
 #else
     return false;
 #endif
