aaa10b89a2f5552f71c21b5c4077827974425690
Simon Fraser <simon.fraser@apple.com>
2020-09-10 19:07:29 +0000

    Cherry-pick r266664. rdar://problem/68652524
        REGRESSION (r260571): Scrolling on weather.com in Safari causes the gradient background to flicker (fixed backgrounds)
        https://bugs.webkit.org/show_bug.cgi?id=216192
        <rdar://problem/68192010>
    
        Reviewed by Tim Horton.
    
        If a page has slow-scrolling reasons, like background-attachment:fixed on a non-root element,
        then we should never update layer positions on the scrolling thread, since this results
        in scroll position being out of sync with the painted background position.
    
        * page/scrolling/ThreadedScrollingTree.cpp:
        (WebCore::ThreadedScrollingTree::canUpdateLayersOnScrollingThread const):
        (WebCore::ThreadedScrollingTree::waitForRenderingUpdateCompletionOrTimeout):
        (WebCore::ThreadedScrollingTree::delayedRenderingUpdateDetectionTimerFired):
        (WebCore::ThreadedScrollingTree::displayDidRefreshOnScrollingThread):
        * page/scrolling/ThreadedScrollingTree.h:
    
        Canonical link: https://commits.webkit.org/229033@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@266664 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/228499.75@safari-610-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610-branch@266877 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/scrolling/ThreadedScrollingTree.cpp
+++ Source/WebCore/page/scrolling/ThreadedScrollingTree.cpp
@@ -115,6 +115,12 @@ void ThreadedScrollingTree::propagateSynchronousScrollingReasons(const HashSet<S
     }
 }
 
+bool ThreadedScrollingTree::canUpdateLayersOnScrollingThread() const
+{
+    auto* rootNode = this->rootNode();
+    return !(rootNode && rootNode->hasSynchronousScrollingReasons());
+}
+
 void ThreadedScrollingTree::scrollingTreeNodeDidScroll(ScrollingTreeScrollingNode& node, ScrollingLayerPositionAction scrollingLayerPositionAction)
 {
     if (!m_scrollingCoordinator)
@@ -257,7 +263,8 @@ void ThreadedScrollingTree::waitForRenderingUpdateCompletionOrTimeout()
         m_state = SynchronizationState::Desynchronized;
         // At this point we know the main thread is taking too long in the rendering update,
         // so we give up trying to sync with the main thread and update layers here on the scrolling thread.
-        applyLayerPositionsInternal();
+        if (canUpdateLayersOnScrollingThread())
+            applyLayerPositionsInternal();
         tracePoint(ScrollingThreadRenderUpdateSyncEnd, 1);
     } else
         tracePoint(ScrollingThreadRenderUpdateSyncEnd);
@@ -290,7 +297,8 @@ void ThreadedScrollingTree::delayedRenderingUpdateDetectionTimerFired()
     ASSERT(ScrollingThread::isCurrentThread());
 
     LockHolder treeLocker(m_treeMutex);
-    applyLayerPositionsInternal();
+    if (canUpdateLayersOnScrollingThread())
+        applyLayerPositionsInternal();
     m_state = SynchronizationState::Desynchronized;
 }
 
@@ -301,7 +309,7 @@ void ThreadedScrollingTree::displayDidRefreshOnScrollingThread()
 
     LockHolder treeLocker(m_treeMutex);
 
-    if (m_state != SynchronizationState::Idle)
+    if (m_state != SynchronizationState::Idle && canUpdateLayersOnScrollingThread())
         applyLayerPositionsInternal();
 
     switch (m_state) {
