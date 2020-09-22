0cfb6277782e996814c9530c564e920c78378435
Adrian Perez de Castro <aperez@igalia.com>
2020-09-22 06:52:15 +0000

    [CMake] Use imported targets in find module for the ATK SPI2 bridge dependency https://bugs.webkit.org/show_bug.cgi?id=216773
    Reviewed by Don Olmstead.
    
    .:
    
    * Source/cmake/FindATKBridge.cmake: Rewrite to define an ATK::Bridge imported target.
    * Source/cmake/OptionsWPE.cmake: Use the ATK::Bridge imported target.
    
    Source/WebKit:
    
    No new tests needed.
    
    * PlatformWPE.cmake: Use the ATK::Bridge imported target.
    
    Tools:
    
    * wpe/backends/CMakeLists.txt: Use the ATK::Bridge imported target.
    
    
    Canonical link: https://commits.webkit.org/229609@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@267399 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/FindATKBridge.cmake
+++ Source/cmake/FindATKBridge.cmake
@@ -1,10 +1,4 @@
-# - Try to find ATK
-# Once done, this will define
-#
-#  ATK_BRIDGE_INCLUDE_DIRS - the ATK bridge include drectories
-#  ATK_BRIDGE_LIBRARIES - link these to use ATK bridge
-#
-# Copyright (C) 2019 Igalia S.L.
+# Copyright (C) 2019, 2020 Igalia S.L.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -27,26 +21,72 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-find_package(PkgConfig QUIET)
-pkg_check_modules(PC_ATK_BRIDGE atk-bridge-2.0)
+#[=======================================================================[.rst:
+FindATKBridge
+-------------
 
-find_path(ATK_BRIDGE_INCLUDE_DIRS
+Find the ATK-SPI2 bridge headers and libraries.
+
+Imported Targets
+^^^^^^^^^^^^^^^^
+
+``ATK::Bridge``
+  The ATK SPI2 bridge library, if found.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define the following variables in your project:
+
+``ATKBridge_FOUND``
+  true if (the requested version of) the ATK SPI2 bridge is available.
+``ATKBridge_VERSION``
+  the version of the ATK SPI2 bridge.
+``ATKBridge_LIBRARIES``
+  the libraries to link against to use the ATK SPI2 bridge.
+``ATKBridge_INCLUDE_DIRS``
+  where to find the ATK SPI2 bridge headers.
+``ATKBridge_COMPILE_OPTIONS``
+  this should be passed to target_compile_options(), if the target
+  is not used for linking.
+
+#]=======================================================================]
+
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_ATK_BRIDGE QUIET atk-bridge-2.0)
+set(ATKBridge_COMPILE_OPTIONS ${PC_ATK_BRIDGE_CFLAGS_OTHER})
+set(ATKBridge_VERSION ${PC_ATK_BRIDGE_VERSION})
+
+find_path(ATKBridge_INCLUDE_DIR
     NAMES atk-bridge.h
-    HINTS ${PC_ATK_BRIDGE_INCLUDEDIR}
-          ${PC_ATK_BRIDGE_INCLUDE_DIRS}
+    HINTS ${PC_ATK_BRIDGE_INCLUDEDIR} ${PC_ATK_BRIDGE_INCLUDE_DIR}
     PATH_SUFFIXES at-spi2-atk/2.0
 )
 
-find_library(ATK_BRIDGE_LIBRARIES
-    NAMES atk-bridge-2.0
-    HINTS ${PC_ATK_BRIDGE_LIBRARY_DIRS}
-          ${PC_ATK_BRIDGE_LIBDIR}
+find_library(ATKBridge_LIBRARY
+    NAMES ${ATKBridge_NAMES} atk-bridge-2.0
+    HINTS ${PC_ATK_BRIDGE_LIBDIR} ${PC_ATK_BRIDGE_LIBRARY_DIRS}
 )
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(ATK_BRIDGE REQUIRED_VARS ATK_BRIDGE_INCLUDE_DIRS ATK_BRIDGE_LIBRARIES
-                                      VERSION_VAR   PC_ATK_BRIDGE_VERSION)
-mark_as_advanced(
-    ATK_BRIDGE_INCLUDE_DIRS
-    ATK_BRIDGE_LIBRARIES
+find_package_handle_standard_args(ATKBridge
+    FOUND_VAR ATKBridge_FOUND
+    REQUIRED_VARS ATKBridge_LIBRARY ATKBridge_INCLUDE_DIR
+    VERSION_VAR ATKBridge_VERSION
 )
+
+if (ATKBridge_LIBRARY AND NOT TARGET ATK::Bridge)
+    add_library(ATK::Bridge UNKNOWN IMPORTED GLOBAL)
+    set_target_properties(ATK::Bridge PROPERTIES
+        IMPORTED_LOCATION "${ATKBridge_LIBRARY}"
+        INTERFACE_COMPILE_OPTIONS "${ATKBridge_COMPILE_OPTIONS}"
+        INTERFACE_INCLUDE_DIRECTORIES "${ATKBridge_INCLUDE_DIR}"
+    )
+endif ()
+
+mark_as_advanced(ATKBridge_INCLUDE_DIR ATKBridge_LIBRARY)
+
+if (ATKBridge_FOUND)
+    set(ATKBridge_LIBRARIES ${ATKBridge_LIBRARY})
+    set(ATKBridge_INCLUDE_DIRS ${ATKBridge_INCLUDE_DIR})
+endif ()
