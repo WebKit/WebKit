06ac36fd7df4ae8cf394421d525909c6de0caf20
Simon Fraser <simon.fraser@apple.com>
2020-09-26 21:08:35 +0000

    Cherry-pick r266884. rdar://problem/69583178
        Revert accidental hard-coding of compositing logging from r266825.
    
        * platform/Logging.cpp:
        (WebCore::initializeLogChannelsIfNecessary):
    
        Canonical link: https://commits.webkit.org/229207@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@266884 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/228499.155@safari-610-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610-branch@267630 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/Logging.cpp
+++ Source/WebCore/platform/Logging.cpp
@@ -90,7 +90,6 @@ void initializeLogChannelsIfNecessary(Optional<String> logChannelString)
 
     String enabledChannelsString = logChannelString ? logChannelString.value() : logLevelString();
     WTFInitializeLogChannelStatesFromString(logChannels, logChannelCount, enabledChannelsString.utf8().data());
-    LogCompositing.state = WTFLogChannelState::On;
 }
 
 WTFLogChannel* getLogChannel(const String& name)
