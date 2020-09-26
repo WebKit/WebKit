463e4c2cc08f07a2f07ff8cedd814a798805a4fa
Wenson Hsieh <wenson_hsieh@apple.com>
2020-09-26 21:08:41 +0000

    Cherry-pick r266899. rdar://problem/69586659
        Address a post-commit review comment after r266887
        https://bugs.webkit.org/show_bug.cgi?id=216257
    
        Reviewed by Darin Adler.
    
        Remove a check that currently makes us conditionally set `IsComputedStyleInvalidFlag` if there is a computed
        style in rare data. There should be no change in behavior; this just makes the code a bit simpler.
    
        * dom/Element.cpp:
        (WebCore::Element::invalidateStyle):
        (WebCore::Element::storeDisplayContentsStyle):
    
        Canonical link: https://commits.webkit.org/229220@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@266899 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/228499.157@safari-610-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610-branch@267632 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/Element.cpp
+++ Source/WebCore/dom/Element.cpp
@@ -1947,8 +1947,7 @@ void Element::invalidateStyle()
 
     // FIXME: This flag should be set whenever styles are invalidated while computed styles are present,
     // not just in this codepath.
-    if (hasRareData() && elementRareData()->computedStyle())
-        setNodeFlag(NodeFlag::IsComputedStyleInvalidFlag);
+    setNodeFlag(NodeFlag::IsComputedStyleInvalidFlag);
 }
 
 void Element::invalidateStyleAndLayerComposition()
@@ -1999,6 +1998,7 @@ void Element::storeDisplayContentsStyle(std::unique_ptr<RenderStyle> style)
     ASSERT(style && style->display() == DisplayType::Contents);
     ASSERT(!renderer() || isPseudoElement());
     ensureElementRareData().setComputedStyle(WTFMove(style));
+    clearNodeFlag(NodeFlag::IsComputedStyleInvalidFlag);
 }
 
 // Returns true is the given attribute is an event handler.
