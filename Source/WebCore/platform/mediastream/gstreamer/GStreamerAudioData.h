358150738b5c7cf9508c5885a61aa44d265fccae
Adrian Perez de Castro <aperez@igalia.com>
2020-10-02 15:54:48 +0000

    [GTK] Build broken with ENABLE_MEDIA_STREAM enabled and ENABLE_WEB_RTC_DISABLED https://bugs.webkit.org/show_bug.cgi?id=217128
    Reviewed by Philippe Normand.
    
    .:
    
    * Source/cmake/GStreamerChecks.cmake: Only enable USE_LIBWEBRTC when both
    ENABLE_MEDIA_STREAM and ENABLE_WEB_RTC is defined. This prevents trying to
    build code that uses libwebrtc types when ENABLE_WEB_RTC is disabled but
    ENABLE_MEDIA_STREAM is enabled.
    
    Source/WebCore:
    
    No new tests needed.
    
    * platform/mediastream/gstreamer/GStreamerAudioData.h: Remove USE(LIBWEBRTC) guard, as
    there is nothing specific to WebRTC in this file, and it is needed when building with
    ENABLE_MEDIA_STREAM enabled.
    
    
    Canonical link: https://commits.webkit.org/230000@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@267882 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/mediastream/gstreamer/GStreamerAudioData.h
+++ Source/WebCore/platform/mediastream/gstreamer/GStreamerAudioData.h
@@ -21,7 +21,7 @@
 
 #pragma once
 
-#if ENABLE(MEDIA_STREAM) && USE(LIBWEBRTC) && USE(GSTREAMER)
+#if ENABLE(MEDIA_STREAM) && USE(GSTREAMER)
 
 #include "GRefPtrGStreamer.h"
 #include "PlatformAudioData.h"
@@ -56,4 +56,4 @@ private:
 
 } // namespace WebCore
 
-#endif // ENABLE(MEDIA_STREAM) && USE(LIBWEBRTC) && USE(GSTREAMER)
+#endif // ENABLE(MEDIA_STREAM) && USE(GSTREAMER)
