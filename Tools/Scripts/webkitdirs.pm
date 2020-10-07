5fa409a98864135ed43a01eadf899d7963a8a848
Basuke Suzuki <basuke.suzuki@sony.com>
2020-10-07 18:04:46 +0000

    [build-webkit] Compare with cmakeargs and unhandled to detect configuration change https://bugs.webkit.org/show_bug.cgi?id=207012
    Reviewed by Carlos Alberto Lopez Perez.
    
    Added command line arguments on top of @featureArgs for comparison with previous build
    options. This forces regeneration of CMakeCache.txt when any configuration is changed.
    
    * Scripts/webkitdirs.pm:
    (shouldRemoveCMakeCache):
    
    
    Canonical link: https://commits.webkit.org/230201@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@268134 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -161,6 +161,7 @@ my $shouldNotUseNinja;
 my $xcodeVersion;
 
 my $unknownPortProhibited = 0;
+my @originalArgv = @ARGV;
 
 # Variables for Win32 support
 my $programFilesPath;
@@ -2327,7 +2328,7 @@ sub cmakeFilesPath()
 
 sub shouldRemoveCMakeCache(@)
 {
-    my (@buildArgs) = @_;
+    my (@buildArgs) = sort (@_, @originalArgv);
 
     # We check this first, because we always want to create this file for a fresh build.
     my $productDir = File::Spec->catdir(baseProductDir(), configuration());
