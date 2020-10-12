792370063b5e9bda372e237d265a10d3962f0e88
Tim Horton <timothy_horton@apple.com>
2020-10-12 23:30:36 +0000

    Cursor window is transparent - but not hidden - when `cursor: none` is specified https://bugs.webkit.org/show_bug.cgi?id=217639 <rdar://problem/70006186>
    Reviewed by Simon Fraser.
    
    * Platform/spi/mac/AppKitSPI.h:
    * UIProcess/mac/PageClientImplMac.mm:
    (WebKit::PageClientImpl::setCursor):
    Adopt new AppKit SPI to actually hide the cursor when we set it to the "none" shape.
    This hiding lives until the next time the app-global cursor shape is changed by any client.]
    We'll re-set it the next time we set the "none" shape.
    
    This is important, because on some platforms, hiding the cursor is a performance improvement
    over simply making it transparent.
    
    
    Canonical link: https://commits.webkit.org/230385@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@268376 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/mac/PageClientImplMac.mm
+++ Source/WebKit/UIProcess/mac/PageClientImplMac.mm
@@ -29,6 +29,7 @@
 #if PLATFORM(MAC)
 
 #import "APIHitTestResult.h"
+#import "AppKitSPI.h"
 #import "ColorSpaceData.h"
 #import "DataReference.h"
 #import "DownloadProxy.h"
@@ -328,6 +329,11 @@ void PageClientImpl::setCursor(const WebCore::Cursor& cursor)
         return;
 
     [platformCursor set];
+
+    if (cursor.type() == WebCore::Cursor::Type::None) {
+        if ([NSCursor respondsToSelector:@selector(hideUntilChanged)])
+            [NSCursor hideUntilChanged];
+    }
 }
 
 void PageClientImpl::setCursorHiddenUntilMouseMoves(bool hiddenUntilMouseMoves)
