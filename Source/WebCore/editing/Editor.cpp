c775223f486d02297b8ad8ad63c19467acd9b372
Megan Gardner <megan_gardner@apple.com>
2020-10-13 19:35:21 +0000

    Cherry-pick r266909. rdar://problem/69101091
        Source/WebCore:
        Text replacements at the beginning of a second line are replaced too early
        https://bugs.webkit.org/show_bug.cgi?id=216327
        <rdar://problem/68170353>
    
        Reviewed by Darin Adler.
    
        In the changes in r258871, using SimpleRanges instead of Range causing some side effects
        when the replacements at the beginning of lines. The ranges that we are counting are backwards
        and the return characters are being counted instead of being ignored. There is almost
        certainly a better fix than this, but this patch restores the original logic that
        was present when Range was being used, until a better fix can be worked out.
    
        Test: editing/spelling/text-replacement-first-word-second-line.html
    
        * editing/Editor.cpp:
        (WebCore::Editor::markAndReplaceFor):
        * editing/TextCheckingHelper.cpp:
        (WebCore::TextCheckingParagraph::automaticReplacementStart const):
        (WebCore::TextCheckingParagraph::automaticReplacementLength const):
        * editing/TextCheckingHelper.h:
    
        LayoutTests:
        Overlapping text replacements at the beginning of a line are replaced too early
        https://bugs.webkit.org/show_bug.cgi?id=216327
    
        Reviewed by Darin Adler.
    
        * editing/spelling/text-replacement-first-word-second-line-expected.txt: Added.
        * editing/spelling/text-replacement-first-word-second-line.html: Added.
    
        Canonical link: https://commits.webkit.org/229229@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@266909 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/228499.212@safari-610.2-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610.2-branch@268412 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/editing/Editor.cpp
+++ Source/WebCore/editing/Editor.cpp
@@ -2802,7 +2802,8 @@ void Editor::markAndReplaceFor(const SpellCheckRequest& request, const Vector<Te
         auto resultLocation = results[i].range.location + offsetDueToReplacement;
         auto resultLength = results[i].range.length;
         auto resultEndLocation = resultLocation + resultLength;
-        auto automaticReplacementEndLocation = paragraph.automaticReplacementStart() + paragraph.automaticReplacementLength() + offsetDueToReplacement;
+        auto automaticReplacementStartLocation = paragraph.automaticReplacementStart(true);
+        auto automaticReplacementEndLocation = automaticReplacementStartLocation + paragraph.automaticReplacementLength(true) + offsetDueToReplacement;
         const String& replacement = results[i].replacement;
         bool resultEndsAtAmbiguousBoundary = useAmbiguousBoundaryOffset && selectionOffset - 1 <= resultEndLocation;
 
@@ -2827,7 +2828,7 @@ void Editor::markAndReplaceFor(const SpellCheckRequest& request, const Vector<Te
                     addMarker(badGrammarRange, DocumentMarker::Grammar, detail.userDescription);
                 }
             }
-        } else if (resultEndLocation <= automaticReplacementEndLocation && resultEndLocation >= paragraph.automaticReplacementStart()
+        } else if (automaticReplacementStartLocation <= resultEndLocation && resultEndLocation <= automaticReplacementEndLocation
             && isAutomaticTextReplacementType(resultType)) {
             // In this case the result range just has to touch the automatic replacement range, so we can handle replacing non-word text such as punctuation.
             ASSERT(resultLength > 0);
