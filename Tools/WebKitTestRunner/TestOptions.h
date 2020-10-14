1de4c528207d5f8d8b14ccf1fa7251543de8eaa3
Sam Weinig <weinig@apple.com>
2020-10-14 17:56:12 +0000

    [Testing] Support configuring any preference from test headers for WebKitTestRunner https://bugs.webkit.org/show_bug.cgi?id=217645
    Reviewed by Tim Horton.
    
    Support using any preference defined in any of the WebPreference*.yaml configuration files
    as a test header command rather than limiting it to a hard coded subset by generating the
    list of supported commands and their types from the yaml files themselves.
    
    This currently only works for WebKitTestRunner, but will be made to work with DumpRenderTree
    in subsequent changes.
    
    * WebKitTestRunner/CMakeLists.txt:
    * WebKitTestRunner/Configurations/Base.xcconfig:
    * WebKitTestRunner/DerivedSources-input.xcfilelist:
    * WebKitTestRunner/DerivedSources-output.xcfilelist:
    * WebKitTestRunner/DerivedSources.make:
    * WebKitTestRunner/WebKitTestRunner.xcodeproj/project.pbxproj:
    * WebKitTestRunner/Scripts/PreferencesTemplates: Added.
    * WebKitTestRunner/Scripts/PreferencesTemplates/TestOptionsGeneratedKeys.h.erb: Added.
    Add generation of TestOptionsGeneratedKeys.h from the WebPreference*.yaml using
    the shared GeneratePreferences.rb script in WTF.
    
    * WebKitTestRunner/TestController.cpp:
    (WTR::TestController::resetPreferencesToConsistentValues):
    Move preference setting to the bottom of the file to allow any preference
    to be overriden. Also adds in support for double, uint32_t, and string preferences
    though none of those are currently being used.
    
    * WebKitTestRunner/TestOptions.cpp:
    (WTR::TestOptions::keyTypeMapping):
    Use generated macro GENERATED_WEB_PREFERENCE_KEY_TYPE_MAPPINGS rather than hardcoding
    all the preferences types.
    
    * WebKitTestRunner/TestOptions.h:
    (WTR::TestOptions::boolWebPreferenceFeatures const):
    (WTR::TestOptions::doubleWebPreferenceFeatures const):
    (WTR::TestOptions::uint32WebPreferenceFeatures const):
    (WTR::TestOptions::stringWebPreferenceFeatures const):
    Expose accessors for preferences values for use in setting the actual preference
    values. Rather than setting all preferences, we now only set preferences that have
    been explicitly requested, using the default value for any that have not.
    
    
    Canonical link: https://commits.webkit.org/230455@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@268474 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/WebKitTestRunner/TestOptions.h
+++ Tools/WebKitTestRunner/TestOptions.h
@@ -59,9 +59,13 @@ struct ContextOptions {
 
 class TestOptions {
 public:
+    static const TestFeatures& defaults();
     static const std::unordered_map<std::string, TestHeaderKeyType>& keyTypeMapping();
 
-    explicit TestOptions(TestFeatures);
+    explicit TestOptions(TestFeatures features)
+        : m_features { std::move(features) }
+    {
+    }
 
     ContextOptions contextOptions() const
     {
@@ -74,10 +78,10 @@ public:
         };
     }
 
-    bool allowTopNavigationToDataURLs() const { return boolWebPreferenceFeatureValue("AllowTopNavigationToDataURLs"); }
-    bool enableAttachmentElement() const { return boolWebPreferenceFeatureValue("AttachmentElementEnabled"); }
-    bool punchOutWhiteBackgroundsInDarkMode() const { return boolWebPreferenceFeatureValue("PunchOutWhiteBackgroundsInDarkMode"); }
-    bool useServiceWorkerShortTimeout() const { return boolWebPreferenceFeatureValue("ShouldUseServiceWorkerShortTimeout"); }
+    bool allowTopNavigationToDataURLs() const { return boolWebPreferenceFeatureValue("AllowTopNavigationToDataURLs", true); }
+    bool enableAttachmentElement() const { return boolWebPreferenceFeatureValue("AttachmentElementEnabled", false); }
+    bool punchOutWhiteBackgroundsInDarkMode() const { return boolWebPreferenceFeatureValue("PunchOutWhiteBackgroundsInDarkMode", false); }
+    bool useServiceWorkerShortTimeout() const { return boolWebPreferenceFeatureValue("ShouldUseServiceWorkerShortTimeout", false); }
 
     bool allowsLinkPreview() const { return boolTestRunnerFeatureValue("allowsLinkPreview"); }
     bool dumpJSConsoleLogInStdErr() const { return boolTestRunnerFeatureValue("dumpJSConsoleLogInStdErr"); }
@@ -115,12 +119,15 @@ public:
     const std::unordered_map<std::string, bool>& experimentalFeatures() const { return m_features.experimentalFeatures; }
     const std::unordered_map<std::string, bool>& internalDebugFeatures() const { return m_features.internalDebugFeatures; }
 
-    std::vector<std::pair<std::string, bool>> boolWKPreferences() const;
+    const std::unordered_map<std::string, bool>& boolWebPreferenceFeatures() const { return m_features.boolWebPreferenceFeatures; }
+    const std::unordered_map<std::string, double>& doubleWebPreferenceFeatures() const { return m_features.doubleWebPreferenceFeatures; }
+    const std::unordered_map<std::string, uint32_t>& uint32WebPreferenceFeatures() const { return m_features.uint32WebPreferenceFeatures; }
+    const std::unordered_map<std::string, std::string>& stringWebPreferenceFeatures() const { return m_features.stringWebPreferenceFeatures; }
 
     bool hasSameInitializationOptions(const TestOptions&) const;
 
 private:
-    bool boolWebPreferenceFeatureValue(std::string key) const;
+    bool boolWebPreferenceFeatureValue(std::string key, bool defaultValue) const;
     bool boolTestRunnerFeatureValue(std::string key) const;
     double doubleTestRunnerFeatureValue(std::string key) const;
     std::string stringTestRunnerFeatureValue(std::string key) const;
