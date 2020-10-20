0680f099a81c7bdb6841f9b27a65b4ddf374bcf8
Sergio Villar Senin <svillar@igalia.com>
2020-10-20 12:19:27 +0000

    [css-flexbox] order property should be animatable https://bugs.webkit.org/show_bug.cgi?id=209876
    Reviewed by Antoine Quint.
    
    LayoutTests/imported/w3c:
    
    Updated ~150 FAIL expectations to PASS.
    
    * web-platform-tests/css/css-flexbox/animation/order-interpolation-expected.txt: Updated expectations.
    * web-platform-tests/web-animations/animation-model/animation-types/accumulation-per-property-002-expected.txt: Ditto.
    * web-platform-tests/web-animations/animation-model/animation-types/addition-per-property-002-expected.txt: Ditto.
    * web-platform-tests/web-animations/animation-model/animation-types/interpolation-per-property-002-expected.txt: Ditto.
    
    Source/WebCore:
    
    Make order property animatable as per spec. This allows us to pass ~150 flexbox animation subtests.
    
    * animation/CSSPropertyAnimation.cpp:
    (WebCore::CSSPropertyAnimationWrapperMap::CSSPropertyAnimationWrapperMap): Mark property as animatable.
    
    Canonical link: https://commits.webkit.org/230677@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@268726 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/animation/CSSPropertyAnimation.cpp
+++ Source/WebCore/animation/CSSPropertyAnimation.cpp
@@ -1872,6 +1872,7 @@ CSSPropertyAnimationWrapperMap::CSSPropertyAnimationWrapperMap()
 
         new PropertyWrapper<float>(CSSPropertyFlexGrow, &RenderStyle::flexGrow, &RenderStyle::setFlexGrow),
         new PropertyWrapper<float>(CSSPropertyFlexShrink, &RenderStyle::flexShrink, &RenderStyle::setFlexShrink),
+        new PropertyWrapper<int>(CSSPropertyOrder, &RenderStyle::order, &RenderStyle::setOrder),
     };
     const unsigned animatableLonghandPropertiesCount = WTF_ARRAY_LENGTH(animatableLonghandPropertyWrappers);
 
