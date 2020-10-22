WebKit/WebKit

commit fa323c74d03d0653acdce66cd27dc18b4f04aff4

Selection API: Further improvements to VisibleSelection, FrameSelection, and DOMSelection to preserve anchor and focus https://bugs.webkit.org/show_bug.cgi?id=216739
Reviewed by Ryosuke Niwa.

Source/WebCore:

If done correctly, these changes should have almost no effect unless live range selection
is enabled. This adds a feature where VisibleSelection tracks the original points,
before the VisiblePosition-style canonicalization. It also starts a transition from
the base/extent terminology to the slightly-easier-to-understand anchor/focus terminology
that is used in the selection specification. New functions name them anchor and focus,
even though we still have some old ones that call them base and extent. In addition,
made some other small related improvements to live range selection mode, without changing
behavior when not in that mode. Have been testing these locally by turning on that mode
and running various tests.

* editing/FrameSelection.cpp:
(WebCore::FrameSelection::isInDocumentTree const): Added. Used by DOMSelection to
correctly handle cases where the selection is in a shadow tree without requiring it
actually create the associated live range just to query.
(WebCore::FrameSelection::associatedLiveRange): Use the new VisibleSelection::range,
which gives us non-canonicalized points, rather than the deprecated
VisibleSelection::firstRange, which we will eventually be renaming or deleting.
(WebCore::FrameSelection::updateFromAssociatedLiveRange): Use
makeContainerOffsetPosition instead of implicitly using makeDeprecatedLegacyPosition.
Also removed the FIXME about normalization of endpoints, which is what this patch
mostly addresses; no need to track with a comment now.
(WebCore::FrameSelection::updateAssociatedLiveRange): Use VisibleSelection::range
for the same reason as above.

* editing/FrameSelection.h: Added isInDocumentTree.

* editing/VisibleSelection.cpp:
(WebCore::VisibleSelection::VisibleSelection): Updated the primary constructor to set

Source/WebCore/editing/VisibleSelection.cpp
@@ -49,50 +49,35 @@ VisibleSelection::VisibleSelection()
 {
 }
 
-VisibleSelection::VisibleSelection(const Position& position, Affinity affinity, bool isDirectional)
-    : m_base(position)
-    , m_extent(position)
+VisibleSelection::VisibleSelection(const Position& anchor, const Position& focus, Affinity affinity, bool isDirectional)
+    : m_anchor(anchor)
+    , m_focus(focus)
     , m_affinity(affinity)
     , m_isDirectional(isDirectional)
 {
     validate();
 }
 
-VisibleSelection::VisibleSelection(const Position& base, const Position& extent, Affinity affinity, bool isDirectional)
-    : m_base(base)
-    , m_extent(extent)
-    , m_affinity(affinity)
-    , m_isDirectional(isDirectional)
+VisibleSelection::VisibleSelection(const Position& position, Affinity affinity, bool isDirectional)
+    : VisibleSelection(position, position, affinity, isDirectional)
 {
-    validate();
 }
 
 VisibleSelection::VisibleSelection(const VisiblePosition& position, bool isDirectional)
-    : m_base(position.deepEquivalent())
-    , m_extent(position.deepEquivalent())
-    , m_affinity(position.affinity())
-    , m_isDirectional(isDirectional)
+    : VisibleSelection(position.deepEquivalent(), position.affinity(), isDirectional)
 {
-    validate();
+    // FIXME: Wasteful that this re-canonicalizes, but risky to change since the VisiblePosition object could be from before a mutation and its position may no longer be canonical.
 }
 
-VisibleSelection::VisibleSelection(const VisiblePosition& base, const VisiblePosition& extent, bool isDirectional)
-    : m_base(base.deepEquivalent())
-    , m_extent(extent.deepEquivalent())
-    , m_affinity(base.affinity())
-    , m_isDirectional(isDirectional)
+VisibleSelection::VisibleSelection(const VisiblePosition& anchor, const VisiblePosition& focus, bool isDirectional)
+    : VisibleSelection(anchor.deepEquivalent(), focus.deepEquivalent(), anchor.affinity(), isDirectional)
 {
-    validate();
+    // FIXME: Wasteful that this re-canonicalizes, but risky to change since the VisiblePosition objects could be from before a mutation and their positions may no longer be canonical.
 }
 
 VisibleSelection::VisibleSelection(const SimpleRange& range, Affinity affinity, bool isDirectional)
-    : m_base(createLegacyEditingPosition(&range.startContainer(), range.startOffset()))
-    , m_extent(createLegacyEditingPosition(&range.endContainer(), range.endOffset()))
-    , m_affinity(affinity)
-    , m_isDirectional(isDirectional)
+    : VisibleSelection(makeDeprecatedLegacyPosition(range.start), makeDeprecatedLegacyPosition(range.end), affinity, isDirectional)
 {
-    ASSERT(&range.startContainer().treeScope() == &range.endContainer().treeScope());
-    validate();
 }
 
 VisibleSelection VisibleSelection::selectionFromContentsOfNode(Node* node)
@@ -101,28 +86,51 @@ VisibleSelection VisibleSelection::selectionFromContentsOfNode(Node* node)
     return VisibleSelection(firstPositionInNode(node), lastPositionInNode(node));
 }
 
+Position VisibleSelection::anchor() const
+{
+    return m_anchor;
+}
+
+Position VisibleSelection::focus() const
+{
+    return m_focus;
+}
+
+Position VisibleSelection::uncanonicalizedStart() const
+{
+    return m_baseIsFirst ? m_anchor : m_focus;
+}
+
+Position VisibleSelection::uncanonicalizedEnd() const
+{
+    return m_baseIsFirst ? m_focus : m_anchor;
+}
+
+Optional<SimpleRange> VisibleSelection::range() const
+{
+    return makeSimpleRange(uncanonicalizedStart().parentAnchoredEquivalent(), uncanonicalizedEnd().parentAnchoredEquivalent());
+}
+
 void VisibleSelection::setBase(const Position& position)
 {
-    m_base = position;
+    m_anchor = position;
     validate();
 }
 
 void VisibleSelection::setBase(const VisiblePosition& visiblePosition)
 {
-    m_base = visiblePosition.deepEquivalent();
-    validate();
+    setBase(visiblePosition.deepEquivalent());
 }
 
 void VisibleSelection::setExtent(const Position& position)
 {
-    m_extent = position;
+    m_focus = position;
     validate();
 }
 
 void VisibleSelection::setExtent(const VisiblePosition& visiblePosition)
 {
-    m_extent = visiblePosition.deepEquivalent();
-    validate();
+    setExtent(visiblePosition.deepEquivalent());
 }
 
 Optional<SimpleRange> VisibleSelection::firstRange() const
@@ -210,39 +218,24 @@ void VisibleSelection::appendTrailingWhitespace()
 
 void VisibleSelection::setBaseAndExtentToDeepEquivalents()
 {
-    // Move the selection to rendered positions, if possible.
-    bool baseAndExtentEqual = m_base == m_extent;
-    if (m_base.isNotNull()) {
-        m_base = VisiblePosition(m_base, m_affinity).deepEquivalent();
-        if (baseAndExtentEqual)
-            m_extent = m_base;
-    }
-    if (m_extent.isNotNull() && !baseAndExtentEqual)
-        m_extent = VisiblePosition(m_extent, m_affinity).deepEquivalent();
+    // If only one of anchor and focus is null, convert to a caret selection.
+    // FIXME: Seems like a better rule would be to convert to no selection.
+    if (m_anchor.isNull())
+        m_anchor = m_focus;
+    if (m_focus.isNull())
+        m_focus = m_anchor;
 
-    // Make sure we do not have a dangling base or extent.
-    if (m_base.isNull() && m_extent.isNull())
-        m_baseIsFirst = true;
-    else if (m_base.isNull()) {
-        m_base = m_extent;
-        m_baseIsFirst = true;
-    } else if (m_extent.isNull()) {
+    m_base = VisiblePosition(m_anchor, m_affinity).deepEquivalent();
+    if (m_anchor == m_focus)
         m_extent = m_base;
-        m_baseIsFirst = true;
-    } else
-        m_baseIsFirst = m_base <= m_extent;
+    else
+        m_extent = VisiblePosition(m_focus, m_affinity).deepEquivalent();
+
+    m_baseIsFirst = m_base <= m_extent;
 }
 
-void VisibleSelection::setStartAndEndFromBaseAndExtentRespectingGranularity(TextGranularity granularity)
+void VisibleSelection::adjustSelectionRespectingGranularity(TextGranularity granularity)
 {
-    if (m_baseIsFirst) {
-        m_start = m_base;
-        m_end = m_extent;
-    } else {
-        m_start = m_extent;
-        m_end = m_base;
-    }
-
     switch (granularity) {
         case TextGranularity::CharacterGranularity:
             // Don't do any expansion.
@@ -286,19 +279,16 @@ void VisibleSelection::setStartAndEndFromBaseAndExtentRespectingGranularity(Text
             }
                 
             m_end = end.deepEquivalent();
+
             // End must not be before start.
-            if (m_start.deprecatedNode() == m_end.deprecatedNode() && m_start.deprecatedEditingOffset() > m_end.deprecatedEditingOffset()) {
-                Position swap(m_start);
-                m_start = m_end;    
-                m_end = swap;    
-            }
+            if (m_start.deprecatedNode() == m_end.deprecatedNode() && m_start.deprecatedEditingOffset() > m_end.deprecatedEditingOffset())
+                std::swap(m_start, m_end);
             break;
         }
-        case TextGranularity::SentenceGranularity: {
+        case TextGranularity::SentenceGranularity:
             m_start = startOfSentence(VisiblePosition(m_start, m_affinity)).deepEquivalent();
             m_end = endOfSentence(VisiblePosition(m_end, m_affinity)).deepEquivalent();
             break;
-        }
         case TextGranularity::LineGranularity: {
             m_start = startOfLine(VisiblePosition(m_start, m_affinity)).deepEquivalent();
             VisiblePosition end = endOfLine(VisiblePosition(m_end, m_affinity));
@@ -384,27 +374,47 @@ void VisibleSelection::updateSelectionType()
 void VisibleSelection::validate(TextGranularity granularity)
 {
     setBaseAndExtentToDeepEquivalents();
-    setStartAndEndFromBaseAndExtentRespectingGranularity(granularity);
+
+    m_start = m_baseIsFirst ? m_base : m_extent;
+    m_end = m_baseIsFirst ? m_extent : m_base;
+
+    auto startBeforeAdjustments = m_start;
+    auto endBeforeAdjustments = m_end;
+
+    adjustSelectionRespectingGranularity(granularity);
     adjustSelectionToAvoidCrossingShadowBoundaries();
     adjustSelectionToAvoidCrossingEditingBoundaries();
     updateSelectionType();
+
+    bool shouldUpdateAnchor = m_start != startBeforeAdjustments;
+    bool shouldUpdateFocus = m_end != endBeforeAdjustments;
 
     if (isRange()) {
         // "Constrain" the selection to be the smallest equivalent range of nodes.
         // This is a somewhat arbitrary choice, but experience shows that it is
         // useful to make to make the selection "canonical" (if only for
         // purposes of comparing selections). This is an ideal point of the code
-        // to do this operation, since all selection changes that result in a RANGE 
+        // to do this operation, since all selection changes that result in a RANGE
         // come through here before anyone uses it.
         // FIXME: Canonicalizing is good, but haven't we already done it (when we
         // set these two positions to VisiblePosition deepEquivalent()s above)?
         m_start = m_start.downstream();
         m_end = m_end.upstream();
 
-        // FIXME: Position::downstream() or Position::upStream() might violate editing boundaries
-        // if an anchor node has a Shadow DOM. So we adjust selection to avoid crossing editing
-        // boundaries again. See https://bugs.webkit.org/show_bug.cgi?id=87463
+        // Position::downstream() or Position::upstream() might violate editing boundaries
+        // if an anchor node has a Shadow DOM even though they should not. But because this
+        // happens in practice, adjust selection to avoid crossing editing boundaries again.
+        // See https://bugs.webkit.org/show_bug.cgi?id=87463.
         adjustSelectionToAvoidCrossingEditingBoundaries();
+    }
+
+    if (shouldUpdateAnchor) {
+        m_anchor = m_baseIsFirst ? m_start : m_end;
+        m_base = m_anchor;
+    }
+    if (shouldUpdateFocus) {
+        m_focus = m_baseIsFirst ? m_end : m_start;
+        m_extent = m_focus;
     }
 }
 
@@ -416,9 +426,10 @@ void VisibleSelection::validate(TextGranularity granularity)
 // class for editing to use that can manipulate selections that are not currently valid.
 void VisibleSelection::setWithoutValidation(const Position& base, const Position& extent)
 {
-    ASSERT(!base.isNull());
-    ASSERT(!extent.isNull());
+    ASSERT(base.isNull() == extent.isNull());
     ASSERT(m_affinity == Affinity::Downstream);
+    m_anchor = base;
+    m_focus = extent;
     m_base = base;
     m_extent = extent;
     m_baseIsFirst = base <= extent;
@@ -486,7 +497,7 @@ static bool isInUserAgentShadowRootOrHasEditableShadowAncestor(Node& node)
 
 void VisibleSelection::adjustSelectionToAvoidCrossingShadowBoundaries()
 {
-    if (m_base.isNull() || m_start.isNull() || m_end.isNull())
+    if (m_start.isNull() || m_end.isNull())
         return;
 
     auto startNode = makeRef(*m_start.anchorNode());
@@ -500,6 +511,7 @@ void VisibleSelection::adjustSelectionToAvoidCrossingShadowBoundaries()
             return;
     }
 
+    // Correct the focus if necessary.
     if (m_baseIsFirst) {
         m_extent = adjustPositionForEnd(m_end, m_start.containerNode());
         m_end = m_extent;
@@ -507,11 +519,12 @@ void VisibleSelection::adjustSelectionToAvoidCrossingShadowBoundaries()
         m_extent = adjustPositionForStart(m_start, m_end.containerNode());
         m_start = m_extent;
     }
+    m_focus = m_extent;
 }
 
 void VisibleSelection::adjustSelectionToAvoidCrossingEditingBoundaries()
 {
-    if (m_base.isNull() || m_start.isNull() || m_end.isNull())
+    if (m_start.isNull() || m_end.isNull())
         return;
 
     // Early return in the caret case (the state hasn't actually been set yet, so we can't use isCaret()) to avoid the
@@ -575,12 +588,7 @@ void VisibleSelection::adjustSelectionToAvoidCrossingEditingBoundaries()
             VisiblePosition previous(p);
 
             if (previous.isNull()) {
-                // The selection crosses an Editing boundary.  This is a
-                // programmer error in the editing code.  Happy debugging!
-                ASSERT_NOT_REACHED();
-                m_base = Position();
-                m_extent = Position();
-                validate();
+                *this = { };
                 return;
             }
             m_end = previous.deepEquivalent();
@@ -604,21 +612,18 @@ void VisibleSelection::adjustSelectionToAvoidCrossingEditingBoundaries()
             VisiblePosition next(p);
             
             if (next.isNull()) {
-                // The selection crosses an Editing boundary.  This is a
-                // programmer error in the editing code.  Happy debugging!
-                ASSERT_NOT_REACHED();
-                m_base = Position();
-                m_extent = Position();
-                validate();
+                *this = { };
                 return;
             }
             m_start = next.deepEquivalent();
         }
     }
     
-    // Correct the extent if necessary.
-    if (baseEditableAncestor != lowestEditableAncestor(m_extent.containerNode()))
+    // Correct the focus if necessary.
+    if (baseEditableAncestor != lowestEditableAncestor(m_extent.containerNode())) {
         m_extent = m_baseIsFirst ? m_end : m_start;
+        m_focus = m_extent;
+    }
 }
 
 bool VisibleSelection::isContentEditable() const

LayoutTests/editing/execCommand/insert-list-nested-with-orphaned-expected.txt
@@ -17,7 +17,7 @@ This tests hang when listifying nested lists with an orphaned list child in betw
         "
 |     <ol>
 |       <li>
-|         "<#selection-caret>because of you"
+|         "because of you"
 |   "
     "
 | "

Source/WebCore/editing/FrameSelection.cpp
@@ -2786,10 +2786,15 @@ static bool containsEndpoints(const WeakPtr<Document>& document, const Range& li
     return document && document->contains(liveRange.startContainer());
 }
 
+bool FrameSelection::isInDocumentTree() const
+{
+    return containsEndpoints(m_document, m_selection.range());
+}
+
 RefPtr<Range> FrameSelection::associatedLiveRange()
 {
     if (!m_associatedLiveRange) {
-        if (auto range = m_selection.firstRange(); containsEndpoints(m_document, range)) {
+        if (auto range = m_selection.range(); containsEndpoints(m_document, range)) {
             m_associatedLiveRange = createLiveRange(*range);
             m_associatedLiveRange->didAssociateWithSelection();
         }
@@ -2816,14 +2821,17 @@ void FrameSelection::updateFromAssociatedLiveRange()
     ASSERT(m_associatedLiveRange);
     if (!containsEndpoints(m_document, *m_associatedLiveRange))
         disassociateLiveRange();
-    else
-        setSelection(makeSimpleRange(*m_associatedLiveRange));
-    // FIXME: Normalization done by setSelection will be visible next time updateAssociatedLiveRange is called. Instead the Selection API specification calls for allowing non-normalized selection ranges.
+    else {
+        // Don't use VisibleSelection's constructor that takes a SimpleRange, because it uses makeDeprecatedLegacyPosition instead of makeContainerOffsetPosition.
+        auto start = makeContainerOffsetPosition(&m_associatedLiveRange->startContainer(), m_associatedLiveRange->startOffset());
+        auto end = makeContainerOffsetPosition(&m_associatedLiveRange->endContainer(), m_associatedLiveRange->endOffset());
+        setSelection({ start, end });
+    }
 }
 
 void FrameSelection::updateAssociatedLiveRange()
 {
-    auto range = m_selection.firstRange();
+    auto range = m_selection.range();
     if (!containsEndpoints(m_document, range)) {
         // The selection was cleared or is now within a shadow tree.
         disassociateLiveRange();

Source/WebCore/editing/FrameSelection.h
@@ -251,6 +251,7 @@ public:
     bool shouldShowBlockCursor() const { return m_shouldShowBlockCursor; }
     void setShouldShowBlockCursor(bool);
 
+    bool isInDocumentTree() const;
     RefPtr<Range> associatedLiveRange();
     void associateLiveRange(Range&);
     void disassociateLiveRange();

commit 032826b76fddf8f94afef40f520d94115103b00b

Stop gap patch fix for regression in r267329. https://bugs.webkit.org/show_bug.cgi?id=218020 Source/WebCore:
<rdar://problem/69542459>

Reviewed by Darin Adler.

The refactoring in https://bugs.webkit.org/show_bug.cgi?id=216739 caused selections to
expand in both directions after a double-click and drag to expand on mac. This behavior
was not observed on iOS. This removes the error-prone calculation.
Note that this bug does not occur if only testing immediatly moving the drag point to the
end of the selection. Dragging across all the words as a real user would needs to be emulated.

Test: editing/selection/double-click-and-drag-over-anchor-to-select.html

* editing/VisibleSelection.cpp:
(WebCore::VisibleSelection::validate):

LayoutTests:

Reviewed by Darin Adler.

* editing/selection/double-click-and-drag-over-anchor-to-select-expected.txt: Added.
* editing/selection/double-click-and-drag-over-anchor-to-select.html: Added.
* resources/ui-helper.js:
(window.UIHelper.doubleClickAtMouseDown):
(window.UIHelper.mouseUp):
(window.UIHelper.dragMouseAcrossElement):
(window.UIHelper.doubleClickElementMouseDown):


Canonical link: https://commits.webkit.org/230775@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@268847 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/editing/VisibleSelection.cpp
@@ -386,8 +386,8 @@ void VisibleSelection::validate(TextGranularity granularity)
     adjustSelectionToAvoidCrossingEditingBoundaries();
     updateSelectionType();
 
-    bool shouldUpdateAnchor = m_start != startBeforeAdjustments;
-    bool shouldUpdateFocus = m_end != endBeforeAdjustments;
+    bool shouldUpdateAnchor = false; // Set to false because of <rdar://problem/69542459>. Can be returned to original logic when this problem is fully fixed.
+    bool shouldUpdateFocus = false; // Ditto.
 
     if (isRange()) {
         // "Constrain" the selection to be the smallest equivalent range of nodes.

LayoutTests/editing/mac/selection/double-click-and-drag-over-anchor-to-select-expected.txt
@@ -1,0 +1,56 @@
+
+Before extending the selection:
+| "
+    "
+| <p>
+|   "The "
+|   <span>
+|     id="end"
+|     "quick"
+|   " "
+|   <span>
+|     id="middle3"
+|     "brown"
+|   " "
+|   <span>
+|     id="middle2"
+|     "fox"
+|   " "
+|   <span>
+|     id="middle1"
+|     "jumped"
+|   " "
+|   <span>
+|     id="start"
+|     "over"
+|   " the lazy dog. And then there was another sentence."
+| "
+"
+
+quick brown fox jumped over:
+| "
+    "
+| <p>
+|   "The "
+|   <span>
+|     id="end"
+|     "<#selection-focus>quick"
+|   " "
+|   <span>
+|     id="middle3"
+|     "brown"
+|   " "
+|   <span>
+|     id="middle2"
+|     "fox"
+|   " "
+|   <span>
+|     id="middle1"
+|     "jumped"
+|   " "
+|   <span>
+|     id="start"
+|     "over<#selection-anchor>"
+|   " the lazy dog. And then there was another sentence."
+| "
+"

LayoutTests/editing/mac/selection/double-click-and-drag-over-anchor-to-select.html
@@ -1,0 +1,52 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ useFlexibleViewport=true ] -->
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="../../../resources/dump-as-markup.js"></script>
+<script src="../../../resources/ui-helper.js"></script>
+<style>
+p {
+    font-size: 20px;
+}
+
+p + p {
+    margin-top: 100px;
+}
+
+#start {
+    border: 1px solid blue;
+}
+
+#end {
+    padding-right: 2px;
+    border: 1px solid tomato;
+}
+</style>
+</head>
+<body>
+<div>This test verifies that double-clicking and then dragging over the selection anchor point (in LTR languages, this is dragging left) to extend the text selection does not extend the selection from the original selection focus as well (in LRT languages, the selection should not extent to the right as well while dragging left). To test manually, load the page and double click on 'over' and drag to 'quick'. The text 'quick brown fox jumped over' should be selected, and nothing else.</div>
+<div id="container">
+    <p>The <span id="end">quick</span> <span id="middle3">brown</span> <span id="middle2">fox</span> <span id="middle1">jumped</span> <span id="start">over</span> the lazy dog. And then there was another sentence.</p>
+</div>
+<script>
+
+(async function () {
+    if (!window.testRunner)
+        return;
+
+    internals.settings.setEditingBehavior("Mac");
+    Markup.dump(document.getElementById("container"), "Before extending the selection");
+
+    UIHelper.doubleClickElementMouseDown(document.getElementById("start"));
+    UIHelper.dragMouseAcrossElement(document.getElementById("middle1"));
+    UIHelper.dragMouseAcrossElement(document.getElementById("middle2"));
+    UIHelper.dragMouseAcrossElement(document.getElementById("middle3"));
+    UIHelper.dragMouseAcrossElement(document.getElementById("end"));
+    UIHelper.mouseUp();
+
+    Markup.dump(document.getElementById("container"), document.getSelection().toString());
+    Markup.notifyDone();
+})();
+</script>
+</body>
+</html>

LayoutTests/resources/ui-helper.js
@@ -19,6 +19,19 @@ window.UIHelper = class UIHelper {
         eventSender.mouseUp();
     }
 
+    static doubleClickAtMouseDown(x1, y1)
+    {
+        eventSender.mouseMoveTo(x1, y1);
+        eventSender.mouseDown();
+        eventSender.mouseUp();
+        eventSender.mouseDown();
+    }
+
+    static mouseUp()
+    {
+        eventSender.mouseUp();
+    }
+
     static doubleClickAtThenDragTo(x1, y1, x2, y2)
     {
         eventSender.mouseMoveTo(x1, y1);
@@ -27,6 +40,28 @@ window.UIHelper = class UIHelper {
         eventSender.mouseDown();
         eventSender.mouseMoveTo(x2, y2);
         eventSender.mouseUp();
+    }
+
+    static dragMouseAcrossElement(element)
+    {
+        const x1 = element.offsetLeft + element.offsetWidth;
+        const x2 = element.offsetLeft + element.offsetWidth * .75;
+        const x3 = element.offsetLeft + element.offsetWidth / 2;
+        const x4 = element.offsetLeft + element.offsetWidth / 4;
+        const x5 = element.offsetLeft;
+        const y = element.offsetTop + element.offsetHeight / 2;
+        eventSender.mouseMoveTo(x1, y);
+        eventSender.mouseMoveTo(x2, y);
+        eventSender.mouseMoveTo(x3, y);
+        eventSender.mouseMoveTo(x4, y);
+        eventSender.mouseMoveTo(x5, y);
+    }
+
+    static doubleClickElementMouseDown(element1)
+    {
+        const x1 = element1.offsetLeft + element1.offsetWidth / 2;
+        const y1 = element1.offsetTop + element1.offsetHeight / 2;
+        return UIHelper.doubleClickAtMouseDown(x1, y1);
     }
 
     static async moveMouseAndWaitForFrame(x, y)