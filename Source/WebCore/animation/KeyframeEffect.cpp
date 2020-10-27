89cf5d4aa49555c3d4b14668e8153a10a624c160
Devin Rousso <drousso@apple.com>
2020-10-27 01:12:58 +0000

    Cherry-pick r268730. rdar://problem/70702315
        Web Inspector: REGRESSION(r260076): crash under InspectorInstrumentation::willApplyKeyframeEffect
        https://bugs.webkit.org/show_bug.cgi?id=217936
    
        Reviewed by Brian Burg.
    
        Speculative fix by null-checking `targetElementOrPseudoElement()` before using it. Before
        r260076 this was not necessary as `m_target` is null-checked earlier in the function, but
        after it can now return `nullptr` depending on the value of `m_pseudoId`.
    
        * animation/KeyframeEffect.cpp:
        (WebCore::KeyframeEffect::apply):
    
        Canonical link: https://commits.webkit.org/230681@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@268730 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/230609.15@safari-611.1.4-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-611.1.4-branch@269007 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/animation/KeyframeEffect.cpp
+++ Source/WebCore/animation/KeyframeEffect.cpp
@@ -1249,7 +1249,8 @@ void KeyframeEffect::apply(RenderStyle& targetStyle, Optional<Seconds> startTime
     auto computedTiming = getComputedTiming(startTime);
     if (!startTime) {
         m_phaseAtLastApplication = computedTiming.phase;
-        InspectorInstrumentation::willApplyKeyframeEffect(*targetElementOrPseudoElement(), *this, computedTiming);
+        if (auto* target = targetElementOrPseudoElement())
+            InspectorInstrumentation::willApplyKeyframeEffect(*target, *this, computedTiming);
     }
 
     if (!computedTiming.progress)
