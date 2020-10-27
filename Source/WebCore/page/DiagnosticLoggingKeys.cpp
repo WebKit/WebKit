ab1369386ed11ab7a3393bacb9d213d2b6bdee64
Brent Fulgham <bfulgham@apple.com>
2020-10-27 22:22:01 +0000

    Remove leftover DiagnosticLoggingKey after r268458 https://bugs.webkit.org/show_bug.cgi?id=218263 <rdar://problem/70738034>
    Reviewed by Darin Adler.
    
    Remove the leftover DiagnosticKey for ResourceLoadStatistics telemetry now that the
    actual data is no longer generated.
    
    * page/DiagnosticLoggingKeys.cpp:
    (WebCore::DiagnosticLoggingKeys::resourceLoadStatisticsTelemetryKey): Deleted.
    * page/DiagnosticLoggingKeys.h:
    
    
    Canonical link: https://commits.webkit.org/230966@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@269078 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/DiagnosticLoggingKeys.cpp
+++ Source/WebCore/page/DiagnosticLoggingKeys.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2018 Apple Inc. All rights reserved.
+ * Copyright (C) 2012-2020 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -867,11 +867,6 @@ String DiagnosticLoggingKeys::backgroundCPUUsageToDiagnosticLoggingKey(double cp
     if (cpuUsage < 70)
         return "50to70"_s;
     return "over70"_s;
-}
-
-String DiagnosticLoggingKeys::resourceLoadStatisticsTelemetryKey()
-{
-    return "resourceLoadStatisticsTelemetry"_s;
 }
     
 } // namespace WebCore
