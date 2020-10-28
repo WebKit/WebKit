3121d7b337fb7bc24fd4640e48140bc7244a9090
Aditya Keerthi <akeerthi@apple.com>
2020-10-28 17:41:12 +0000

    [Cocoa] Remove soft linking of Contacts.framework https://bugs.webkit.org/show_bug.cgi?id=218289
    Reviewed by Geoff Garen.
    
    Since Contacts.framework does not depend on WebKit and soft linking is
    discouraged, WebKit should link Contacts.framework normally.
    
    Soft linking requires running the linker at runtime, leading to an
    unwanted performance cost. By normally linking (on iOS) and weak linking
    (on macOS), the linkage is declared at build time, which enables running
    the linker at build/update/install time and avoiding the runtime cost.
    
    * Configurations/WebKit.xcconfig:
    
    On macOS, weak link the framework since Contacts.framework is not
    available on the Base System.
    
    * Platform/cocoa/PaymentAuthorizationPresenter.mm:
    (WebKit::toNSError):
    
    
    Canonical link: https://commits.webkit.org/230990@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@269109 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Platform/cocoa/PaymentAuthorizationPresenter.mm
+++ Source/WebKit/Platform/cocoa/PaymentAuthorizationPresenter.mm
@@ -38,16 +38,6 @@
 
 #import <pal/cocoa/PassKitSoftLink.h>
 
-SOFT_LINK_FRAMEWORK(Contacts);
-SOFT_LINK_CONSTANT(Contacts, CNPostalAddressCityKey, NSString *);
-SOFT_LINK_CONSTANT(Contacts, CNPostalAddressCountryKey, NSString *);
-SOFT_LINK_CONSTANT(Contacts, CNPostalAddressISOCountryCodeKey, NSString *);
-SOFT_LINK_CONSTANT(Contacts, CNPostalAddressPostalCodeKey, NSString *);
-SOFT_LINK_CONSTANT(Contacts, CNPostalAddressStateKey, NSString *);
-SOFT_LINK_CONSTANT(Contacts, CNPostalAddressStreetKey, NSString *);
-SOFT_LINK_CONSTANT(Contacts, CNPostalAddressSubAdministrativeAreaKey, NSString *);
-SOFT_LINK_CONSTANT(Contacts, CNPostalAddressSubLocalityKey, NSString *);
-
 namespace WebKit {
 
 // FIXME: Rather than having these free functions scattered about, Apple Pay data types should know
@@ -115,42 +105,42 @@ static NSError *toNSError(const WebCore::PaymentError& error)
             
         case WebCore::PaymentError::ContactField::AddressLines:
             pkContactField = PAL::get_PassKit_PKContactFieldPostalAddress();
-            postalAddressKey = getCNPostalAddressStreetKey();
+            postalAddressKey = CNPostalAddressStreetKey;
             break;
             
         case WebCore::PaymentError::ContactField::SubLocality:
             pkContactField = PAL::get_PassKit_PKContactFieldPostalAddress();
-            postalAddressKey = getCNPostalAddressSubLocalityKey();
+            postalAddressKey = CNPostalAddressSubLocalityKey;
             break;
             
         case WebCore::PaymentError::ContactField::Locality:
             pkContactField = PAL::get_PassKit_PKContactFieldPostalAddress();
-            postalAddressKey = getCNPostalAddressCityKey();
+            postalAddressKey = CNPostalAddressCityKey;
             break;
             
         case WebCore::PaymentError::ContactField::PostalCode:
             pkContactField = PAL::get_PassKit_PKContactFieldPostalAddress();
-            postalAddressKey = getCNPostalAddressPostalCodeKey();
+            postalAddressKey = CNPostalAddressPostalCodeKey;
             break;
             
         case WebCore::PaymentError::ContactField::SubAdministrativeArea:
             pkContactField = PAL::get_PassKit_PKContactFieldPostalAddress();
-            postalAddressKey = getCNPostalAddressSubAdministrativeAreaKey();
+            postalAddressKey = CNPostalAddressSubAdministrativeAreaKey;
             break;
             
         case WebCore::PaymentError::ContactField::AdministrativeArea:
             pkContactField = PAL::get_PassKit_PKContactFieldPostalAddress();
-            postalAddressKey = getCNPostalAddressStateKey();
+            postalAddressKey = CNPostalAddressStateKey;
             break;
             
         case WebCore::PaymentError::ContactField::Country:
             pkContactField = PAL::get_PassKit_PKContactFieldPostalAddress();
-            postalAddressKey = getCNPostalAddressCountryKey();
+            postalAddressKey = CNPostalAddressCountryKey;
             break;
             
         case WebCore::PaymentError::ContactField::CountryCode:
             pkContactField = PAL::get_PassKit_PKContactFieldPostalAddress();
-            postalAddressKey = getCNPostalAddressISOCountryCodeKey();
+            postalAddressKey = CNPostalAddressISOCountryCodeKey;
             break;
         }
 
