823f8db359ddbca1737fdffa11673f3f11cf86d0
James Darpinian <jdarpinian@chromium.org>
2020-11-03 19:14:11 +0000

    Cherry-pick r266364. rdar://problem/70970205
        Implement WEBGL_compressed_texture_s3tc_srgb extension
        https://bugs.webkit.org/show_bug.cgi?id=215973
    
        Patch by James Darpinian <jdarpinian@chromium.org> on 2020-08-31
        Reviewed by Dean Jackson.
    
        Source/ThirdParty/ANGLE:
    
        * src/libANGLE/renderer/gl/formatutilsgl.cpp:
        (rx::nativegl::ExtAndVersionOrExt):
        (rx::nativegl::BuildInternalFormatInfoMap):
    
        Source/WebCore:
    
        Tested by webgl/2.0.0/conformance/extensions/webgl-compressed-texture-s3tc-srgb.html
    
        * CMakeLists.txt:
        * DerivedSources.make:
        * Sources.txt:
        * WebCore.xcodeproj/project.pbxproj:
        * bindings/js/JSDOMConvertWebGL.cpp:
        (WebCore::convertToJSValue):
        * html/canvas/WebGLCompressedTextureS3TCsRGB.cpp: Added.
        (WebCore::WebGLCompressedTextureS3TCsRGB::WebGLCompressedTextureS3TCsRGB):
        (WebCore::WebGLCompressedTextureS3TCsRGB::getName const):
        (WebCore::WebGLCompressedTextureS3TCsRGB::supported):
        * html/canvas/WebGLCompressedTextureS3TCsRGB.h: Added.
        * html/canvas/WebGLCompressedTextureS3TCsRGB.idl: Added.
        * html/canvas/WebGLExtension.h:
        * html/canvas/WebGLRenderingContext.cpp:
        (WebCore::WebGLRenderingContext::getExtension):
        (WebCore::WebGLRenderingContext::getSupportedExtensions):
        * html/canvas/WebGLRenderingContextBase.cpp:
        (WebCore::WebGLRenderingContextBase::extensionIsEnabled):
        (WebCore::WebGLRenderingContextBase::validateCompressedTexFuncData):
        (WebCore::WebGLRenderingContextBase::validateCompressedTexDimensions):
        (WebCore::WebGLRenderingContextBase::validateCompressedTexSubDimensions):
        * html/canvas/WebGLRenderingContextBase.h:
        * platform/graphics/ExtensionsGL.h:
    
        Canonical link: https://commits.webkit.org/228800@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@266364 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/228499.290@safari-610-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610-branch@269317 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebCore/html/canvas/WebGLCompressedTextureS3TCsRGB.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2020 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+
+#if ENABLE(WEBGL)
+#include "WebGLCompressedTextureS3TCsRGB.h"
+
+#include "ExtensionsGL.h"
+#include "WebGLRenderingContextBase.h"
+
+namespace WebCore {
+
+WebGLCompressedTextureS3TCsRGB::WebGLCompressedTextureS3TCsRGB(WebGLRenderingContextBase& context)
+    : WebGLExtension(context)
+{
+    auto& extensions = context.graphicsContextGL()->getExtensions();
+    extensions.ensureEnabled("GL_EXT_texture_compression_s3tc_srgb");
+
+    context.addCompressedTextureFormat(ExtensionsGL::COMPRESSED_SRGB_S3TC_DXT1_EXT);
+    context.addCompressedTextureFormat(ExtensionsGL::COMPRESSED_SRGB_ALPHA_S3TC_DXT1_EXT);
+    context.addCompressedTextureFormat(ExtensionsGL::COMPRESSED_SRGB_ALPHA_S3TC_DXT3_EXT);
+    context.addCompressedTextureFormat(ExtensionsGL::COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT);
+}
+
+WebGLCompressedTextureS3TCsRGB::~WebGLCompressedTextureS3TCsRGB() = default;
+
+WebGLExtension::ExtensionName WebGLCompressedTextureS3TCsRGB::getName() const
+{
+    return WebGLCompressedTextureS3TCsRGBName;
+}
+
+bool WebGLCompressedTextureS3TCsRGB::supported(WebGLRenderingContextBase& context)
+{
+    auto& extensions = context.graphicsContextGL()->getExtensions();
+    return extensions.supports("GL_EXT_texture_compression_s3tc_srgb");
+}
+
+} // namespace WebCore
+
+#endif // ENABLE(WEBGL)
