343ee27fddbc706bfc1a0b66802ce184f359eade
Kimmo Kinnunen <kkinnunen@apple.com>
2020-11-04 22:07:04 +0000

    Cherry-pick r268386. rdar://problem/70970067
        Cocoa: Make WebGLLayer not dependent on  GraphicsContextGLOpenGL
        https://bugs.webkit.org/show_bug.cgi?id=217212
        <rdar://problem/69876022>
    
        Patch by Kimmo Kinnunen <kkinnunen@apple.com> on 2020-10-13
        Reviewed by Dean Jackson.
    
        Source/WebCore:
    
        WebGLLayer was needlessly using GraphicsContextGLOpenGL.
        This is problematic because WebGLLayer should work with
        upcoming remote GraphicsContextGL implementation.
    
        The prepare callgraph was:
            GCGLOpenGL -> WebGLLayer -> GCGLOpenGL
        Refactor it to be:
            GCGLOpenGL -> WebGLLayer
    
        Move the back buffer ownership to the GraphicsContextGLOpenGL.
        Make the front buffer ownership explicit in WebGLLayer.
        Move the EGL bindings ownerships of all buffers to
        GraphicsContextGLOpenGL.
    
        Make the WebGLLayer not use EGL or OpenGL, it does not
        need and cannot use it as not all of its clients use OpenGL
        (i.e. the above mentioned remote use-case).
    
        Improves the memory usage by not allocating front buffers
        unless needed. In case the canvas does not present, will
        not allocate front buffers at all.
    
        Improves error handling of the allocations and EGL bindings.
    
        No new tests, a refactor.
    
        * WebCore.xcodeproj/project.pbxproj:
        * platform/graphics/angle/GraphicsContextGLANGLE.cpp:
        (WebCore::GraphicsContextGLOpenGL::reshapeFBOs):
        (WebCore::GraphicsContextGLOpenGL::prepareTexture):
        (WebCore::GraphicsContextGLOpenGL::prepareTextureImpl):
        * platform/graphics/angle/GraphicsContextGLANGLEUtilities.h: Added.
        (WebCore::ScopedRestoreTextureBinding::ScopedRestoreTextureBinding):
        (WebCore::ScopedRestoreTextureBinding::~ScopedRestoreTextureBinding):
        * platform/graphics/cocoa/GraphicsContextGLOpenGLCocoa.mm:
        (WebCore::GraphicsContextGLOpenGL::GraphicsContextGLOpenGL):
        (WebCore::GraphicsContextGLOpenGL::~GraphicsContextGLOpenGL):
        (WebCore::GraphicsContextGLOpenGL::makeContextCurrent):
        (WebCore::GraphicsContextGLOpenGL::reshapeDisplayBufferBacking):
        (WebCore::GraphicsContextGLOpenGL::bindDisplayBufferBacking):
        (WebCore::GraphicsContextGLOpenGL::prepareForDisplay):
        (WebCore::GraphicsContextGLOpenGL::didDisplay):
        * platform/graphics/cocoa/WebGLLayer.h:
        * platform/graphics/cocoa/WebGLLayer.mm:
        (-[WebGLLayer initWithClient:devicePixelRatio:]):
        (-[WebGLLayer copyImageSnapshotWithColorSpace:]):
        (-[WebGLLayer recycleBuffer]):
        (-[WebGLLayer prepareForDisplayWithContents:]):
        (-[WebGLLayer display]):
        (-[WebGLLayer detachClient]):
        * platform/graphics/cocoa/WebGLLayerClient.h: Copied from Source/WebCore/platform/graphics/cocoa/WebGLLayer.h.
        (WebCore::WebGLLayerClient::~WebGLLayerClient):
        * platform/graphics/opengl/GraphicsContextGLOpenGL.h:
        * platform/graphics/opengl/GraphicsContextGLOpenGLBase.cpp:
        (WebCore::GraphicsContextGLOpenGL::reshapeFBOs):
    
        Source/WebKit:
    
        Removed unneeded inclusions of GraphicsContextGLOpenGL.h. The
        file is now using non-public headers.
    
        * WebProcess/WebPage/WebPage.cpp:
        * WebProcess/WebPage/mac/WebPageMac.mm:
    
        Canonical link: https://commits.webkit.org/230395@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@268386 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/228499.295@safari-610-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610-branch@269387 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/cocoa/WebGLLayer.h
+++ Source/WebCore/platform/graphics/cocoa/WebGLLayer.h
@@ -20,35 +20,57 @@
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #import "IOSurface.h"
-#import "IntSize.h"
 #import <QuartzCore/QuartzCore.h>
 #import <wtf/NakedPtr.h>
 
 namespace WebCore {
-class GraphicsLayer;
-class GraphicsContextGLOpenGL;
+class WebGLLayerClient;
+struct WebGLLayerBuffer {
+    std::unique_ptr<WebCore::IOSurface> surface; // The actual contents.
+    void* handle { nullptr }; // Client specific metadata handle (such as EGLSurface).
+};
 }
 
 ALLOW_DEPRECATED_DECLARATIONS_BEGIN
 
+// A layer class implementing front buffer management of a 3-buffering swap
+// chain of IOSurfaces.
+// The layer will own the IOSurfaces it uses for display.
+// The client may attach metadata to each IOSurface and will receive the metadata
+// back once the IOSurface has been displayed. However, the client may not neccessarily
+// be able to obtain the IOSurface itself for reuse.
+// Example use of the metadata is to use EGLSurface binding as the metadata. This way
+// when the WebGLLayer is done with the IOSurface display, the client can continue using
+// the existing binding obtained through the buffer recycle logic.
 @interface WebGLLayer : CALayer
 
-@property (nonatomic) NakedPtr<WebCore::GraphicsContextGLOpenGL> context;
-
-- (id)initWithGraphicsContextGL:(NakedPtr<WebCore::GraphicsContextGLOpenGL>)context;
+- (id)initWithClient:(NakedPtr<WebCore::WebGLLayerClient>)client devicePixelRatio:(float)devicePixelRatio;
 
 - (CGImageRef)copyImageSnapshotWithColorSpace:(CGColorSpaceRef)colorSpace;
 
-- (void)prepareForDisplay;
+// Returns the metadata handle of last unused contents buffer.
+// Client may recieve back also the ownership of the contents surface, in case it is available at the
+// time of the call.
+// Returns either:
+// - Empty buffer if no buffer has been submitted.
+// - Buffer with empty surface and non-empty metadata handle if the recycled buffer was available
+//   but the surface is still in use.
+// - Surface and handle.
+- (WebCore::WebGLLayerBuffer)recycleBuffer;
 
-- (bool)allocateIOSurfaceBackingStoreWithSize:(WebCore::IntSize)size usingAlpha:(BOOL)usingAlpha;
-- (void)bindFramebufferToNextAvailableSurface;
-- (void)setEGLDisplay:(void*)eglDisplay config:(void*)eglConfig;
-- (void)releaseGLResources;
+// Prepares the layer for display with a contents buffer.
+// Client transfers the ownership of the IOSurface surface in the `buffer`.
+- (void)prepareForDisplayWithContents:(WebCore::WebGLLayerBuffer)buffer;
+
+// Detaches the client and returns the current contents buffer metadata handle.
+// The if multiple buffers have been submitted, recycleBuffer must have been called before calling
+// this.
+// The client will not receive `WebGLLayerClient` notifications after calling this.
+- (void*)detachClient;
 
 @end
 
