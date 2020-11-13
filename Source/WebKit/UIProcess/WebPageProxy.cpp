07b8fc11f3ddc6137e7bf77cda4458201167f266
Per Arne Vollan <pvollan@apple.com>
2020-11-13 22:13:02 +0000

    [macOS] Issue sandbox extension to the WebContent process for com.apple.lskdd https://bugs.webkit.org/show_bug.cgi?id=218920 <rdar://problem/69168801>
    Reviewed by Geoffrey Garen.
    
    This is a Media related service, and a sandbox extension should be issued to the WebContent process based on GPU runtime settings.
    
    No new tests, covered by existing tests.
    
    * UIProcess/WebPageProxy.cpp:
    (WebKit::mediaRelatedMachServices):
    * WebProcess/com.apple.WebProcess.sb.in:
    
    
    Canonical link: https://commits.webkit.org/231565@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@269792 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/WebPageProxy.cpp
+++ Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -7749,7 +7749,7 @@ static const Vector<ASCIILiteral>& mediaRelatedMachServices()
 #if PLATFORM(MAC) || PLATFORM(MACCATALYST)
         "com.apple.coremedia.endpointstream.xpc"_s, "com.apple.coremedia.endpointplaybacksession.xpc"_s,
         "com.apple.coremedia.endpointremotecontrolsession.xpc"_s, "com.apple.coremedia.videodecoder"_s,
-        "com.apple.coremedia.videoencoder"_s, "com.apple.BluetoothServices"_s
+        "com.apple.coremedia.videoencoder"_s, "com.apple.lskdd"_s, "com.apple.BluetoothServices"_s
 #endif
     });
     return services;
