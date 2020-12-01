b9850751978f2a9ff478c9c84fafd3d4fdb326cf
Youenn Fablet <youenn@apple.com>
2020-12-01 19:29:43 +0000

    Update list of block ports according fetch spec https://bugs.webkit.org/show_bug.cgi?id=219154
    Reviewed by Sam Weinig.
    
    LayoutTests/imported/w3c:
    
    * web-platform-tests/fetch/api/request/request-bad-port.any-expected.txt:
    * web-platform-tests/fetch/api/request/request-bad-port.any.js:
    * web-platform-tests/fetch/api/request/request-bad-port.any.worker-expected.txt:
    * web-platform-tests/websockets/Create-blocked-port.any-expected.txt:
    * web-platform-tests/websockets/Create-blocked-port.any.js:
    * web-platform-tests/websockets/Create-blocked-port.any.worker-expected.txt:
    
    Source/WTF:
    
    * wtf/URL.cpp:
    (WTF::portAllowed):
    
    
    Canonical link: https://commits.webkit.org/232016@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@270321 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/URL.cpp
+++ Source/WTF/wtf/URL.cpp
@@ -909,6 +909,7 @@ bool portAllowed(const URL& url)
         42,   // name
         43,   // nicname
         53,   // domain
+        69,   // TFTP
         77,   // priv-rjs
         79,   // finger
         87,   // ttylink
@@ -926,8 +927,10 @@ bool portAllowed(const URL& url)
         119,  // nntp
         123,  // NTP
         135,  // loc-srv / epmap
+        137,  // NetBIOS
         139,  // netbios
         143,  // IMAP2
+        161,  // SNMP
         179,  // BGP
         389,  // LDAP
         427,  // SLP (Also used by Apple Filing Protocol)
@@ -942,6 +945,7 @@ bool portAllowed(const URL& url)
         532,  // netnews
         540,  // UUCP
         548,  // afpovertcp [Apple addition]
+        554,  // rtsp
         556,  // remotefs
         563,  // NNTP+SSL
         587,  // ESMTP
@@ -949,6 +953,9 @@ bool portAllowed(const URL& url)
         636,  // LDAP+SSL
         993,  // IMAP+SSL
         995,  // POP3+SSL
+        1719, // H323 (RAS)
+        1720, // H323 (Q931)
+        1723, // H323 (H245)
         2049, // NFS
         3659, // apple-sasl / PasswordServer [Apple addition]
         4045, // lockd
@@ -956,6 +963,7 @@ bool portAllowed(const URL& url)
         5060, // SIP
         5061, // SIPS
         6000, // X11
+        6566, // SANE
         6665, // Alternate IRC [Apple addition]
         6666, // Alternate IRC [Apple addition]
         6667, // Standard IRC [Apple addition]
