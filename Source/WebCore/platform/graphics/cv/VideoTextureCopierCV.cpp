426d1d74a023ae4d845274a20ce6971a78d19803
Kimmo Kinnunen <kkinnunen@apple.com>
2020-12-15 01:28:09 +0000

    Cherry-pick r269893. rdar://problem/72298580
        Textures Fail to Render in WebGL from HLS Stream on iPhone 12 [iOS 14.2]
        https://bugs.webkit.org/show_bug.cgi?id=218637
        <rdar://problem/71102126>
    
        Patch by Kimmo Kinnunen <kkinnunen@apple.com> on 2020-11-16
        Reviewed by Eric Carlson.
    
        Patch by Jer Noble.
    
        Treat internal compressed YUV pixel formats
        kCVPixelFormatType_AGX_420YpCbCr8BiPlanarVideoRange and
        kCVPixelFormatType_AGX_420YpCbCr8BiPlanarFullRange as
        kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange and
        kCVPixelFormatType_420YpCbCr8BiPlanarFullRange when
        using the OpenGL shader to convert video IOSurface to a WebGL
        texture.
    
        Fixes cases where the decoder outputs the compressed formats.
    
        No new tests, adding more comprehensive test content suite is tracked
        in another bug.
    
        Source/WebCore:
    
        * platform/graphics/cv/GraphicsContextGLCVANGLE.cpp:
        (WebCore::pixelRangeFromPixelFormat):
        (WebCore::GraphicsContextGLCVANGLE::copyPixelBufferToTexture):
    
        Source/WebCore/PAL:
    
        * PAL.xcodeproj/project.pbxproj:
        * pal/spi/cf/CoreVideoSPI.h: Added.
    
        Canonical link: https://commits.webkit.org/231646@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@269893 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/228499.323@safari-610-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610-branch@270814 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/cv/VideoTextureCopierCV.cpp
+++ Source/WebCore/platform/graphics/cv/VideoTextureCopierCV.cpp
@@ -28,6 +28,7 @@
 
 #include "FourCC.h"
 #include "Logging.h"
+#include <pal/spi/cf/CoreVideoSPI.h>
 #include <pal/spi/cocoa/IOSurfaceSPI.h>
 #include <wtf/NeverDestroyed.h>
 #include <wtf/StdMap.h>
@@ -198,6 +199,7 @@ static PixelRange pixelRangeFromPixelFormat(OSType pixelFormat)
     case kCVPixelFormatType_420YpCbCr10BiPlanarVideoRange:
     case kCVPixelFormatType_422YpCbCr10BiPlanarVideoRange:
     case kCVPixelFormatType_444YpCbCr10BiPlanarVideoRange:
+    case kCVPixelFormatType_AGX_420YpCbCr8BiPlanarVideoRange:
         return PixelRange::Video;
     case kCVPixelFormatType_420YpCbCr8PlanarFullRange:
     case kCVPixelFormatType_420YpCbCr8BiPlanarFullRange:
@@ -206,6 +208,7 @@ static PixelRange pixelRangeFromPixelFormat(OSType pixelFormat)
     case kCVPixelFormatType_420YpCbCr10BiPlanarFullRange:
     case kCVPixelFormatType_422YpCbCr10BiPlanarFullRange:
     case kCVPixelFormatType_444YpCbCr10BiPlanarFullRange:
+    case kCVPixelFormatType_AGX_420YpCbCr8BiPlanarFullRange:
         return PixelRange::Full;
     default:
         return PixelRange::Unknown;
@@ -811,7 +814,10 @@ bool VideoTextureCopierCV::copyImageToPlatformTexture(CVPixelBufferRef image, si
 
     // FIXME: This currently only supports '420v' and '420f' pixel formats. Investigate supporting more pixel formats.
     OSType pixelFormat = CVPixelBufferGetPixelFormatType(image);
-    if (pixelFormat != kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange && pixelFormat != kCVPixelFormatType_420YpCbCr8BiPlanarFullRange) {
+    if (pixelFormat != kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange
+        && pixelFormat != kCVPixelFormatType_420YpCbCr8BiPlanarFullRange
+        && pixelFormat != kCVPixelFormatType_AGX_420YpCbCr8BiPlanarVideoRange
+        && pixelFormat != kCVPixelFormatType_AGX_420YpCbCr8BiPlanarFullRange) {
         LOG(WebGL, "VideoTextureCopierCV::copyVideoTextureToPlatformTexture(%p) - Asked to copy an unsupported pixel format ('%s').", this, FourCC(pixelFormat).toString().utf8().data());
         return false;
     }
