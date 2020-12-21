723d445dafccd57a7ab225ea0fb0ca2ed86ce347
Mark Lam <mark.lam@apple.com>
2020-12-21 18:47:52 +0000

    Cherry-pick r270988. rdar://problem/72552973
        Fix MacroAssemblerARM64E::validateUntaggedPtr() to account for TBI.
        https://bugs.webkit.org/show_bug.cgi?id=220021
        <rdar://problem/72474809>
    
        Reviewed by Saam Barati.
    
        * assembler/AbstractMacroAssembler.h:
        * assembler/DisallowMacroScratchRegisterUsage.h:
        - templatized the DisallowMacroScratchRegisterUsage class so that we can #include
          it in MacroAssembler implementations.
        * assembler/MacroAssemblerARM64E.h:
        (JSC::MacroAssemblerARM64E::validateUntaggedPtr):
    
        Canonical link: https://commits.webkit.org/232599@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@270988 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/228499.341@safari-610-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610-branch@271027 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/assembler/DisallowMacroScratchRegisterUsage.h
+++ Source/JavaScriptCore/assembler/DisallowMacroScratchRegisterUsage.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2015-2020 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,10 +27,9 @@
 
 #if ENABLE(ASSEMBLER)
 
-#include "MacroAssembler.h"
-
 namespace JSC {
 
+template<typename MacroAssembler>
 class DisallowMacroScratchRegisterUsage {
 public:
     DisallowMacroScratchRegisterUsage(MacroAssembler& masm)
