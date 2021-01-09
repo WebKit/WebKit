cc8317aa54760b802bee3f6cdbde53f96bfcbb53
Mark Lam <mark.lam@apple.com>
2021-01-09 01:19:15 +0000

    Revert "Cherry-pick r271240. rdar://problem/72935325"
    This reverts commit r271315.
    
    Canonical link: https://commits.webkit.org/228499.356@safari-610.4.3.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-610.4.3.1-branch@271336 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/jit/ThunkGenerators.cpp
+++ Source/JavaScriptCore/jit/ThunkGenerators.cpp
@@ -49,7 +49,7 @@ inline void emitPointerValidation(CCallHelpers& jit, GPRReg pointerGPR, TagType 
     isNonZero.link(&jit);
     jit.pushToSave(pointerGPR);
     jit.untagPtr(tag, pointerGPR);
-    jit.validateUntaggedPtr(pointerGPR);
+    jit.validateUntaggedPtr(pointerGPR, pointerGPR);
     jit.popToRestore(pointerGPR);
 }
 
