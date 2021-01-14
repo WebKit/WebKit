ccf7bcfbc19298980570e30e07c9711477275fa6
Peng Liu <peng.liu6@apple.com>
2021-01-14 21:40:55 +0000

    [Cocoa] Add a script to generate the "Feature Flags" plist file https://bugs.webkit.org/show_bug.cgi?id=220080
    Reviewed by Daniel Bates.
    
    Add a script to combine a plist file from WebKitAdditions with the corresponding plist
    file in this repo to generate a `WebKit.plist` required by the Feature Flags interface.
    
    * FeatureFlags/WebKit-appletvos.plist: Copied from Source/WebKit/FeatureFlags/WebKit.plist.
    * FeatureFlags/WebKit-ios.plist: Copied from Source/WebKit/FeatureFlags/WebKit.plist.
    * FeatureFlags/WebKit-macos.plist: Copied from Source/WebKit/FeatureFlags/WebKit.plist.
    * FeatureFlags/WebKit-watchos.plist: Renamed from Source/WebKit/FeatureFlags/WebKit.plist.
    * Scripts/combine-feature-flags-plist.py: Added.
    (main):
    * Scripts/generate-feature-flags-plist.sh: Added.
    * WebKit.xcodeproj/project.pbxproj:
    
    
    Canonical link: https://commits.webkit.org/233050@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@271498 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Source/WebKit/Scripts/combine-feature-flags-plist.py
@@ -0,0 +1,58 @@
+#
+# Copyright (c) 2021 Apple Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+
+import plistlib
+import sys
+
+
+def main(argv):
+    if len(argv) != 4:
+        print('Usage: ', argv[0], '<feature_flags_plist> <internal_feature_flags_plist> <output_plist>')
+        return 1
+
+    feature_flags_plist_path = argv[1]
+    internal_feature_flags_plist_path = argv[2]
+    output_plist_path = argv[3]
+
+    try:
+        with open(feature_flags_plist_path, 'rb') as feature_flags_file, open(internal_feature_flags_plist_path, 'rb') as internal_feature_flags_file:
+            feature_flags = plistlib.load(feature_flags_file)
+            internal_feature_flags = plistlib.load(internal_feature_flags_file)
+            for key in internal_feature_flags:
+                if key not in feature_flags:
+                    print('Error!', key, 'does not exist in' + feature_flags_plist_path)
+                    return 1
+
+                feature_flags[key].update(internal_feature_flags[key])
+
+            with open(output_plist_path, 'wb') as output_plist_file:
+                plistlib.dump(feature_flags, output_plist_file)
+    except IOError:
+        print('File read/write error! Please make sure file names', feature_flags_plist_path, 'and', internal_feature_flags_plist_path, 'are correct.')
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
