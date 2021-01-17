48c0bbe484cd3a5f76bcee2c4b061616dd25cab0
Youenn Fablet <youenn@apple.com>
2021-01-17 12:36:52 +0000

    Add CoreMedia aavds entitlement to WebContent and GPU processes https://bugs.webkit.org/show_bug.cgi?id=220238
    Reviewed by Brent Fulgham.
    
    Manually tested.
    
    * Scripts/process-entitlements.sh:
    
    
    Canonical link: https://commits.webkit.org/233097@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@271561 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Scripts/process-entitlements.sh
+++ Source/WebKit/Scripts/process-entitlements.sh
@@ -185,6 +185,7 @@ function ios_family_process_webcontent_entitlements()
 {
     plistbuddy Add :com.apple.QuartzCore.secure-mode bool YES
     plistbuddy Add :com.apple.QuartzCore.webkit-end-points bool YES
+    plistbuddy Add :com.apple.developer.coremedia.allow-alternate-video-decoder-selection bool YES
     plistbuddy Add :com.apple.mediaremote.set-playback-state bool YES
     plistbuddy Add :com.apple.pac.shared_region_id string WebContent
     plistbuddy Add :com.apple.private.allow-explicit-graphics-priority bool YES
@@ -212,6 +213,7 @@ function ios_family_process_gpu_entitlements()
 {
     plistbuddy Add :com.apple.QuartzCore.secure-mode bool YES
     plistbuddy Add :com.apple.QuartzCore.webkit-end-points bool YES
+    plistbuddy Add :com.apple.developer.coremedia.allow-alternate-video-decoder-selection bool YES
     plistbuddy Add :com.apple.mediaremote.set-playback-state bool YES
     plistbuddy Add :com.apple.private.allow-explicit-graphics-priority bool YES
     plistbuddy Add :com.apple.private.coremedia.extensions.audiorecording.allow bool YES
