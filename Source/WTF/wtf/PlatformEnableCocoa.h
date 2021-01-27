WebKit/WebKit

commit fcb5002b310bb1a0e0f921bbc5e79bd8352e5cea

[macOS] Disable ENABLE_SET_WEBCONTENT_PROCESS_INFORMATION_IN_NETWORK_PROCESS https://bugs.webkit.org/show_bug.cgi?id=221006
Unreviewed crash fix.

Enabling this is causing a crash. Disable while investigating.


* wtf/PlatformEnableCocoa.h:


Canonical link: https://commits.webkit.org/233367@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@271907 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/PlatformEnableCocoa.h
@@ -630,7 +630,7 @@
 #define ENABLE_VORBIS 1
 #endif
 
-#if !defined(ENABLE_SET_WEBCONTENT_PROCESS_INFORMATION_IN_NETWORK_PROCESS) && PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 120000
+#if !defined(ENABLE_SET_WEBCONTENT_PROCESS_INFORMATION_IN_NETWORK_PROCESS) && PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED > 120000
 #define ENABLE_SET_WEBCONTENT_PROCESS_INFORMATION_IN_NETWORK_PROCESS 1
 #endif
 

commit 130f0e8dc863f4988d67a82af998c842b3497de4

[macOS] Re-enable ENABLE_SET_WEBCONTENT_PROCESS_INFORMATION_IN_NETWORK_PROCESS https://bugs.webkit.org/show_bug.cgi?id=221039 <rdar://problem/73665061> Source/WebCore:
Reviewed by Brent Fulgham.

The commit r271907 introduced a crash, since it denied access to the Launch Services daemon, which AppKit is using to get and set
application information, for example whether the app is foreground or not. To work around this, the WebCoreThemeWindow has been made
borderless, and the method +[NSWindow invalidateRestorableState] is overridden to do nothing. Running the default
+[NSWindow invalidateRestorableState] will exercise code which tries to reach out to the Launch Services daemon, and crashes if
unable to do so. This functionality should not be required in the WebContent process, and can be disabled.

* platform/mac/ThemeMac.mm:
(-[WebCoreThemeWindow invalidateRestorableState]):
(-[WebCoreThemeView window]):

Source/WebKit:

Reviewed by Brent Fulgham.

Work around crash introduced in r271907, by disabling persistent UI. Having persistent UI enabled will exercise code
which tries to connect with the Launch Services daemon, and crash if that fails.

* WebProcess/cocoa/WebProcessCocoa.mm:
(WebKit::WebProcess::platformInitializeWebProcess):

Source/WTF:

Reviewed by Brent Fulgham.

* wtf/PlatformEnableCocoa.h:


Canonical link: https://commits.webkit.org/233409@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@271965 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/PlatformEnableCocoa.h
@@ -630,7 +630,7 @@
 #define ENABLE_VORBIS 1
 #endif
 
-#if !defined(ENABLE_SET_WEBCONTENT_PROCESS_INFORMATION_IN_NETWORK_PROCESS) && PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED > 120000
+#if !defined(ENABLE_SET_WEBCONTENT_PROCESS_INFORMATION_IN_NETWORK_PROCESS) && PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 120000
 #define ENABLE_SET_WEBCONTENT_PROCESS_INFORMATION_IN_NETWORK_PROCESS 1
 #endif
 

Source/WebCore/platform/mac/ThemeMac.mm
@@ -67,6 +67,9 @@ static BOOL themeWindowHasKeyAppearance;
     return themeWindowHasKeyAppearance;
 }
 
+- (void)invalidateRestorableState
+{
+}
 @end
 
 @implementation WebCoreThemeView
@@ -76,7 +79,7 @@ static BOOL themeWindowHasKeyAppearance;
     // Using defer:YES prevents us from wasting any window server resources for this window, since we're not actually
     // going to draw into it. The other arguments match what you get when calling -[NSWindow init].
     static WebCoreThemeWindow *window = [[WebCoreThemeWindow alloc] initWithContentRect:NSMakeRect(100, 100, 100, 100)
-        styleMask:NSWindowStyleMaskTitled backing:NSBackingStoreBuffered defer:YES];
+        styleMask:NSWindowStyleMaskBorderless backing:NSBackingStoreBuffered defer:YES];
     return window;
 }
 

Source/WebKit/WebProcess/cocoa/WebProcessCocoa.mm
@@ -293,7 +293,7 @@ void WebProcess::platformInitializeWebProcess(WebProcessCreationParameters& para
     // We don't need to talk to the Dock.
     [NSApplication _preventDockConnections];
 
-    [[NSUserDefaults standardUserDefaults] registerDefaults:@{ @"NSApplicationCrashOnExceptions" : @YES }];
+    [[NSUserDefaults standardUserDefaults] registerDefaults:@{ @"NSApplicationCrashOnExceptions" : @YES, @"ApplePersistence" : @NO }];
 
     // rdar://9118639 accessibilityFocusedUIElement in NSApplication defaults to use the keyWindow. Since there's
     // no window in WK2, NSApplication needs to use the focused page's focused element.