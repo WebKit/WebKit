c17df51da1531e391f8b7ea01330fc8e6f3ffbc1
commit-queue@webkit.org <commit-queue@webkit.org@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2021-02-08 13:31:56 +0000

    Perform layout before running Editor::findString https://bugs.webkit.org/show_bug.cgi?id=220384
    Patch by Frédéric Wang <fwang@igalia.com> on 2021-02-08
    Reviewed by Ryosuke Niwa.
    
    This ensures that the layout is up-to-date before performing any search.
    Also disable any post-resolution that could cause more changes to happen.
    
    * editing/Editor.cpp:
    (WebCore::Editor::findString):
    
    Canonical link: https://commits.webkit.org/233802@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@272488 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/editing/Editor.cpp
+++ Source/WebCore/editing/Editor.cpp
@@ -103,6 +103,7 @@
 #include "SpellingCorrectionCommand.h"
 #include "StaticPasteboard.h"
 #include "StyleProperties.h"
+#include "StyleTreeResolver.h"
 #include "SystemSoundManager.h"
 #include "TelephoneNumberDetector.h"
 #include "Text.h"
@@ -3444,10 +3445,14 @@ void Editor::applyEditingStyleToBodyElement() const
 bool Editor::findString(const String& target, FindOptions options)
 {
     Ref<Document> protectedDocument(m_document);
+    Optional<SimpleRange> resultRange;
+    {
+        m_document.updateLayoutIgnorePendingStylesheets();
+        Style::PostResolutionCallbackDisabler disabler(m_document);
+        VisibleSelection selection = m_document.selection().selection();
+        resultRange = rangeOfString(target, selection.firstRange(), options);
+    }
 
-    VisibleSelection selection = m_document.selection().selection();
-
-    auto resultRange = rangeOfString(target, selection.firstRange(), options);
     if (!resultRange)
         return false;
 
