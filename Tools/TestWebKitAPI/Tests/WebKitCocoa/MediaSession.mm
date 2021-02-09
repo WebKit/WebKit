a1cbedefc6ed35f1624fab689aee66a26f5c8bdd
Alex Christensen <achristensen@apple.com>
2021-02-09 22:51:09 +0000

    Fix Catalina build. https://bugs.webkit.org/show_bug.cgi?id=221431
    * TestWebKitAPI/Tests/WebKitCocoa/MediaSession.mm:
    (TestWebKitAPI::TEST_F):
    
    
    
    Canonical link: https://commits.webkit.org/233878@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@272615 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/WebKitCocoa/MediaSession.mm
+++ Tools/TestWebKitAPI/Tests/WebKitCocoa/MediaSession.mm
@@ -271,7 +271,7 @@ TEST_F(MediaSessionTest, DISABLED_OnlyOneHandler)
     listenForSessionHandlerMessages({ "play", "pause", "seekto", "seekforward", "seekbackward", "previoustrack", "nexttrack" });
     listenForEventMessages({ "play", "pause", "seeked" });
 
-#if __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500
+#if __MAC_OS_X_VERSION_MIN_REQUIRED > 101500
     static Vector<MRMediaRemoteCommand> registeredCommands = { MRMediaRemoteCommandPlay };
     auto currentCommands = getSupportedCommands();
     for (MRCommandInfo *command in currentCommands.get()) {
@@ -316,7 +316,7 @@ TEST_F(MediaSessionTest, DISABLED_RemoteCommands)
     listenForSessionHandlerMessages({ "play", "pause", "seekto", "seekforward", "seekbackward", "previoustrack", "nexttrack" });
     listenForEventMessages({ "play", "pause", "seeked" });
 
-#if __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500
+#if __MAC_OS_X_VERSION_MIN_REQUIRED > 101500
     static Vector<MRMediaRemoteCommand> registeredCommands = { MRMediaRemoteCommandPlay, MRMediaRemoteCommandPause, MRMediaRemoteCommandSeekToPlaybackPosition, MRMediaRemoteCommandSkipForward, MRMediaRemoteCommandSkipBackward, MRMediaRemoteCommandPreviousTrack, MRMediaRemoteCommandNextTrack };
     auto currentCommands = getSupportedCommands();
     for (MRCommandInfo *command in currentCommands.get()) {
