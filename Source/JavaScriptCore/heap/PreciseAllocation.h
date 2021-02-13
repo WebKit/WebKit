5a376975fd0f4ae591019da4088db5f1eb01a8bb
Mark Lam <mark.lam@apple.com>
2021-02-13 01:27:47 +0000

    Remove some unused methods in MarkedBlock and PreciseAllocation. https://bugs.webkit.org/show_bug.cgi?id=221864
    Reviewed by Yusuke Suzuki.
    
    * heap/MarkedBlock.h:
    (JSC::MarkedBlock::Handle::visitWeakSet): Deleted.
    (JSC::MarkedBlock::Handle::reapWeakSet): Deleted.
    * heap/PreciseAllocation.cpp:
    (JSC::PreciseAllocation::shrink): Deleted.
    (JSC::PreciseAllocation::visitWeakSet): Deleted.
    (JSC::PreciseAllocation::reapWeakSet): Deleted.
    * heap/PreciseAllocation.h:
    
    
    
    Canonical link: https://commits.webkit.org/234061@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@272825 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/heap/PreciseAllocation.h
+++ Source/JavaScriptCore/heap/PreciseAllocation.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2019 Apple Inc. All rights reserved.
+ * Copyright (C) 2016-2021 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -79,11 +79,6 @@ public:
 
     unsigned indexInSpace() { return m_indexInSpace; }
     void setIndexInSpace(unsigned indexInSpace) { m_indexInSpace = indexInSpace; }
-    
-    void shrink();
-    
-    void visitWeakSet(SlotVisitor&);
-    void reapWeakSet();
     
     void clearNewlyAllocated() { m_isNewlyAllocated = false; }
     void flip();
