WebKit/WebKit

commit 15a4537bfaddce811d233f85d2fadc077d8acce8

REGRESSION(r269813): PLT5 regressed by 0.5% https://bugs.webkit.org/show_bug.cgi?id=220652 <rdar://problem/71517335>
Reviewed by Antti Koivisto.

In r269813 we added support for animating more pseudo-elements besides ::after and ::before. However, the only other
pseudo-element we realistically should be supporting animations for at this juncture is ::marker. So instead of
calling Style::TreeResolver::resolvePseudoStyle() for all public pseudo-elements, we call it for ::after, ::before
and ::marker alone.

* style/StyleTreeResolver.cpp:
(WebCore::Style::TreeResolver::resolveElement):


Canonical link: https://commits.webkit.org/233065@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@271515 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/style/StyleTreeResolver.cpp
@@ -249,10 +249,12 @@ ElementUpdates TreeResolver::resolveElement(Element& element)
     }
 
     PseudoIdToElementUpdateMap pseudoUpdates;
-    for (PseudoId pseudoId = PseudoId::FirstPublicPseudoId; pseudoId < PseudoId::FirstInternalPseudoId; pseudoId = static_cast<PseudoId>(static_cast<unsigned>(pseudoId) + 1)) {
-        if (auto elementUpdate = resolvePseudoStyle(element, update, pseudoId))
-            pseudoUpdates.set(pseudoId, WTFMove(*elementUpdate));
-    }
+    if (auto beforeElementUpdate = resolvePseudoStyle(element, update, PseudoId::Before))
+        pseudoUpdates.set(PseudoId::Before, WTFMove(*beforeElementUpdate));
+    if (auto afterElementUpdate = resolvePseudoStyle(element, update, PseudoId::After))
+        pseudoUpdates.set(PseudoId::After, WTFMove(*afterElementUpdate));
+    if (auto markerElementUpdate = resolvePseudoStyle(element, update, PseudoId::Marker))
+        pseudoUpdates.set(PseudoId::Marker, WTFMove(*markerElementUpdate));
 
 #if ENABLE(TOUCH_ACTION_REGIONS)
     // FIXME: Track this exactly.

commit 3460d3147844eed1d0ddd886ae0cec6833b6c321

Cherry-pick r272927. rdar://problem/74500651
    REGRESSION(r271515): ::marker fired at wrong time
    https://bugs.webkit.org/show_bug.cgi?id=221961
    <rdar://problem/74397846>

    Reviewed by Dean Jackson.

    Source/WebCore:

    Restore the order in which we generate CSS Animations for pseudo-elements as it was prior to r271515.

    * style/StyleTreeResolver.cpp:
    (WebCore::Style::TreeResolver::resolveElement):

    LayoutTests:

    Remove flakiness for this test since it didn't allow us to spot the regression introduced by r271515.

    * TestExpectations:

    Canonical link: https://commits.webkit.org/234160@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@272927 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Canonical link: https://commits.webkit.org/232923.216@safari-611-branch
git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-611-branch@273173 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/style/StyleTreeResolver.cpp
@@ -249,12 +249,12 @@ ElementUpdates TreeResolver::resolveElement(Element& element)
     }
 
     PseudoIdToElementUpdateMap pseudoUpdates;
+    if (auto markerElementUpdate = resolvePseudoStyle(element, update, PseudoId::Marker))
+        pseudoUpdates.set(PseudoId::Marker, WTFMove(*markerElementUpdate));
     if (auto beforeElementUpdate = resolvePseudoStyle(element, update, PseudoId::Before))
         pseudoUpdates.set(PseudoId::Before, WTFMove(*beforeElementUpdate));
     if (auto afterElementUpdate = resolvePseudoStyle(element, update, PseudoId::After))
         pseudoUpdates.set(PseudoId::After, WTFMove(*afterElementUpdate));
-    if (auto markerElementUpdate = resolvePseudoStyle(element, update, PseudoId::Marker))
-        pseudoUpdates.set(PseudoId::Marker, WTFMove(*markerElementUpdate));
 
 #if ENABLE(TOUCH_ACTION_REGIONS)
     // FIXME: Track this exactly.

LayoutTests/TestExpectations
@@ -3202,7 +3202,6 @@ webkit.org/b/200209 imported/w3c/web-platform-tests/css/css-images/multiple-posi
 
 # wpt css-transitions and css-animations failures and flakiness
 webkit.org/b/203296 imported/w3c/web-platform-tests/css/css-animations/keyframes-remove-documentElement-crash.html [ Skip ]
-webkit.org/b/203480 imported/w3c/web-platform-tests/css/css-animations/event-order.tentative.html [ Pass Failure ]
 webkit.org/b/203305 imported/w3c/web-platform-tests/css/css-transitions/properties-value-001.html [ Pass Failure ]
 webkit.org/b/203305 imported/w3c/web-platform-tests/css/css-transitions/properties-value-inherit-001.html [ Pass Failure ]
 webkit.org/b/203305 imported/w3c/web-platform-tests/css/css-transitions/properties-value-inherit-002.html [ Pass Failure ]