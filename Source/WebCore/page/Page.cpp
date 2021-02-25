bae38d10896a6b359f7d10a38a6745c81dc00f4b
Per Arne Vollan <pvollan@apple.com>
2021-02-25 19:55:15 +0000

    Avoid setting page muted state if unchanged https://bugs.webkit.org/show_bug.cgi?id=222395
    Reviewed by Brent Fulgham.
    
    Return early in Page::setMuted if muted state did not change.
    
    No new tests, covered by existing tests.
    
    * page/Page.cpp:
    (WebCore::Page::setMuted):
    
    
    Canonical link: https://commits.webkit.org/234576@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@273499 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/Page.cpp
+++ Source/WebCore/page/Page.cpp
@@ -2108,6 +2108,9 @@ void Page::playbackControlsMediaEngineChanged()
 
 void Page::setMuted(MediaProducer::MutedStateFlags muted)
 {
+    if (m_mutedState == muted)
+        return;
+
     m_mutedState = muted;
 
     forEachDocument([] (Document& document) {
