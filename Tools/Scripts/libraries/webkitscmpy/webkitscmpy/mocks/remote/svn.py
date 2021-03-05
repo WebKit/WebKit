d02973f57d14d5336c45db54763c1ce427b8e3ff
Jonathan Bedard <jbedard@apple.com>
2021-03-05 22:34:17 +0000

    [webkitscmpy] Standardize timezone of mock svn server (Follow-up) https://bugs.webkit.org/show_bug.cgi?id=222762 <rdar://problem/75058478>
    Unreviewed follow-up fix.
    
    * Scripts/libraries/webkitscmpy/webkitscmpy/mocks/remote/svn.py:
    (Svn.request): Only import dateutils when using it.
    
    
    Canonical link: https://commits.webkit.org/234947@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@274009 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/libraries/webkitscmpy/webkitscmpy/mocks/remote/svn.py
+++ Tools/Scripts/libraries/webkitscmpy/webkitscmpy/mocks/remote/svn.py
@@ -26,7 +26,6 @@ import re
 import xmltodict
 
 from collections import OrderedDict
-from datetime import datetime
 from dateutil.tz import tzoffset
 from webkitcorepy import mocks
 from webkitscmpy import Commit, Contributor, remote as scmremote
@@ -134,6 +133,8 @@ class Svn(mocks.Requests):
         return result
 
     def request(self, method, url, data=None, **kwargs):
+        from datetime import datetime
+
         if not url.startswith('http://') and not url.startswith('https://'):
             return mocks.Response.create404(url)
 
