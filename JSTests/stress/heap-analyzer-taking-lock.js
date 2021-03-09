b895ef2278086fc8109990a72136782cf41f0066
Mark Lam <mark.lam@apple.com>
2021-03-09 18:58:17 +0000

    Use --verifyGC=true on some JSC stress test configurations. https://bugs.webkit.org/show_bug.cgi?id=222289
    Reviewed by Filip Pizlo.
    
    JSTests:
    
    * stress/heap-analyzer-taking-lock.js:
    
    Tools:
    
    * Scripts/run-jsc-stress-tests:
    
    
    Canonical link: https://commits.webkit.org/235080@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@274159 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/heap-analyzer-taking-lock.js
+++ JSTests/stress/heap-analyzer-taking-lock.js
@@ -1,3 +1,8 @@
+//@ requireOptions("--verifyGC=false")
+// FIXME: this test intermittently fails --verifyGC. Skipping this test for --verifyGC
+// until we have found the root cause and fixed the issue.
+// https://bugs.webkit.org/show_bug.cgi?id=222947
+
 // Should not crash.
 let array = [];
 for (let i = 0; i < 10000; i++) {
