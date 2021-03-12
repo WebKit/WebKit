e5c5ac6c4ce8024bf9752ac46cf68430881f4d01
Adrian Perez de Castro <aperez@igalia.com>
2021-03-12 23:14:37 +0000

    Unreviewed. Update OptionsWPE.cmake and NEWS for the 2.31.91 release
    .:
    
    * Source/cmake/OptionsWPE.cmake: Bump version numbers.
    
    Source/WebKit:
    
    * wpe/NEWS: Add release notes for 2.31.91, and also the missing ones
    for 2.31.90.

--- Source/cmake/OptionsWPE.cmake
+++ Source/cmake/OptionsWPE.cmake
@@ -1,10 +1,10 @@
 include(GNUInstallDirs)
 include(VersioningUtils)
 
-SET_PROJECT_VERSION(2 31 1)
+SET_PROJECT_VERSION(2 31 91)
 set(WPE_API_VERSION 1.0)
 
-CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT 16 0 13)
+CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT 18 0 14)
 
 # These are shared variables, but we special case their definition so that we can use the
 # CMAKE_INSTALL_* variables that are populated by the GNUInstallDirs macro.
