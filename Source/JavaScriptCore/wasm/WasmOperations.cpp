b0346165f025511bab8aa2eaedfb95e19b3c803c
Yusuke Suzuki <ysuzuki@apple.com>
2021-03-14 18:19:00 +0000

    [JSC] wasm atomic wait offset is not index https://bugs.webkit.org/show_bug.cgi?id=223159
    Reviewed by Mark Lam.
    
    JSTests:
    
    * wasm.yaml:
    * wasm/threads-spec-tests/resources/wait-large.wast: Added.
    * wasm/threads-spec-tests/wait-large.wast.js: Added.
    
    Source/JavaScriptCore:
    
    While JS Atomics.wait's argument is "index" in the typed-array, argument of wasm wait and notify is address.
    But we are handling it as an index incorrectly.
    This patch uses it as an address.
    
    * wasm/WasmOperations.cpp:
    (JSC::Wasm::JSC_DEFINE_JIT_OPERATION):
    
    
    Canonical link: https://commits.webkit.org/235266@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@274399 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/wasm/WasmOperations.cpp
+++ Source/JavaScriptCore/wasm/WasmOperations.cpp
@@ -866,7 +866,7 @@ JSC_DEFINE_JIT_OPERATION(operationMemoryAtomicWait32, int32_t, (Instance* instan
         return -1;
     if (!vm.m_typedArrayController->isAtomicsWaitAllowedOnCurrentThread())
         return -1;
-    uint32_t* pointer = bitwise_cast<uint32_t*>(instance->memory()->memory()) + offsetInMemory;
+    uint32_t* pointer = bitwise_cast<uint32_t*>(bitwise_cast<uint8_t*>(instance->memory()->memory()) + offsetInMemory);
     return wait<uint32_t>(vm, pointer, value, timeoutInNanoseconds);
 }
 
@@ -884,7 +884,7 @@ JSC_DEFINE_JIT_OPERATION(operationMemoryAtomicWait64, int32_t, (Instance* instan
         return -1;
     if (!vm.m_typedArrayController->isAtomicsWaitAllowedOnCurrentThread())
         return -1;
-    uint64_t* pointer = bitwise_cast<uint64_t*>(instance->memory()->memory()) + offsetInMemory;
+    uint64_t* pointer = bitwise_cast<uint64_t*>(bitwise_cast<uint8_t*>(instance->memory()->memory()) + offsetInMemory);
     return wait<uint64_t>(vm, pointer, value, timeoutInNanoseconds);
 }
 
