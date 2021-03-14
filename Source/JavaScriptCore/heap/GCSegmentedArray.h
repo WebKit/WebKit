9a6e27ccb20b3ec3f35b79e658ac3a2e30fd1125
Mark Lam <mark.lam@apple.com>
2021-03-14 18:46:15 +0000

    GCSegmentedArray's size() and isEmpty() methods should be const. https://bugs.webkit.org/show_bug.cgi?id=223156
    Reviewed by Keith Miller.
    
    * heap/GCSegmentedArray.h:
    * heap/GCSegmentedArrayInlines.h:
    (JSC::GCSegmentedArray<T>::isEmpty const):
    (JSC::GCSegmentedArray<T>::size const):
    (JSC::GCSegmentedArray<T>::isEmpty): Deleted.
    (JSC::GCSegmentedArray<T>::size): Deleted.
    
    
    Canonical link: https://commits.webkit.org/235267@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@274400 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/heap/GCSegmentedArray.h
+++ Source/JavaScriptCore/heap/GCSegmentedArray.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2019 Apple Inc. All rights reserved.
+ * Copyright (C) 2014-2021 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -81,8 +81,8 @@ public:
     const T removeLast();
     bool refill();
     
-    size_t size();
-    bool isEmpty();
+    size_t size() const;
+    bool isEmpty() const;
 
     void fillVector(Vector<T>&);
     void clear();
