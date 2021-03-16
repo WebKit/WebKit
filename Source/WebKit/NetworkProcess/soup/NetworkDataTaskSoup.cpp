e352d091089213a83a5c1cf220e1b0436bc8b200
Lauro Moura <lmoura@igalia.com>
2021-03-16 09:04:21 +0000

    [SOUP] Fix SOUP3 debug build https://bugs.webkit.org/show_bug.cgi?id=223237
    Reviewed by Carlos Garcia Campos.
    
    Source/WebCore:
    
    * platform/network/soup/ResourceRequestSoup.cpp:
    (WebCore::ResourceRequest::updateSoupMessageBody const):
    
    Source/WebKit:
    
    * NetworkProcess/soup/NetworkDataTaskSoup.cpp:
    (WebKit::NetworkDataTaskSoup::acceptCertificateCallback):
    
    
    Canonical link: https://commits.webkit.org/235327@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@274471 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/NetworkProcess/soup/NetworkDataTaskSoup.cpp
+++ Source/WebKit/NetworkProcess/soup/NetworkDataTaskSoup.cpp
@@ -480,7 +480,7 @@ gboolean NetworkDataTaskSoup::acceptCertificateCallback(SoupMessage* message, GT
     if (task->state() == State::Canceling || task->state() == State::Completed || !task->m_client)
         return FALSE;
 
-    ASSERT(task->m_soupMessage.get() == soupMessage);
+    ASSERT(task->m_soupMessage.get() == message);
 
     return task->acceptCertificate(certificate, errors);
 }
