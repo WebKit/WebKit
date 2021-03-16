22f32c0057ca68056f1bc2ae01deb8922145c107
Commit Queue <commit-queue@webkit.org>
2021-03-16 19:03:28 +0000

    Unreviewed, reverting r274499. https://bugs.webkit.org/show_bug.cgi?id=223268
    Not a correct fix
    
    Reverted changeset:
    
    "[WebView _touchEventRegions] needs to take the web lock"
    https://bugs.webkit.org/show_bug.cgi?id=223266
    https://trac.webkit.org/changeset/274499
    
    Canonical link: https://commits.webkit.org/235358@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@274506 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKitLegacy/mac/WebView/WebView.mm
+++ Source/WebKitLegacy/mac/WebView/WebView.mm
@@ -4022,10 +4022,6 @@ IGNORE_WARNINGS_END
 
 - (NSArray *)_touchEventRegions
 {
-#if PLATFORM(IOS_FAMILY)
-    WebThreadLock();
-#endif
-    
     auto* frame = [self _mainCoreFrame];
     if (!frame)
         return nil;
