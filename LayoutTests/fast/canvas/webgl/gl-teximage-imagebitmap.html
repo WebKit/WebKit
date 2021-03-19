WebKit/WebKit

commit c92577e30adf2e3ac437794d2418016815e3c140

[WPE][GTK] Add support for ICC color management https://bugs.webkit.org/show_bug.cgi?id=177185
Reviewed by Adrian Perez de Castro.

.:

Add optional lcms2 dependency.

* Source/cmake/FindLCMS2.cmake: Added.
* Source/cmake/OptionsGTK.cmake:
* Source/cmake/OptionsWPE.cmake:

Source/WebCore:

Add support for ICC color profiles to JPEG and PNG decoders using lcms2 if available.

* PlatformGTK.cmake:
* PlatformWPE.cmake:
* platform/graphics/PlatformDisplay.cpp:
(WebCore::PlatformDisplay::~PlatformDisplay):
(WebCore::PlatformDisplay::colorProfile const):
* platform/graphics/PlatformDisplay.h:
* platform/graphics/x11/PlatformDisplayX11.cpp:
(WebCore::PlatformDisplayX11::colorProfile const):
* platform/graphics/x11/PlatformDisplayX11.h:
* platform/image-decoders/jpeg/JPEGImageDecoder.cpp:
(WebCore::isICCMarker):
(WebCore::readICCProfile):
(WebCore::JPEGImageReader::JPEGImageReader):
(WebCore::JPEGImageReader::decode):
(WebCore::JPEGImageDecoder::~JPEGImageDecoder):
(WebCore::JPEGImageDecoder::clear):
(WebCore::JPEGImageDecoder::setFailed):
(WebCore::JPEGImageDecoder::outputScanlines):
(WebCore::JPEGImageDecoder::decode):
(WebCore::JPEGImageDecoder::setICCProfile):
* platform/image-decoders/jpeg/JPEGImageDecoder.h:
* platform/image-decoders/png/PNGImageDecoder.cpp:
(WebCore::PNGImageDecoder::~PNGImageDecoder):
(WebCore::PNGImageDecoder::clear):
(WebCore::PNGImageDecoder::setFailed):
(WebCore::PNGImageDecoder::headerAvailable):
(WebCore::PNGImageDecoder::rowAvailable):
(WebCore::PNGImageDecoder::decode):
(WebCore::PNGImageDecoder::frameComplete):
* platform/image-decoders/png/PNGImageDecoder.h:

Canonical link: https://commits.webkit.org/235170@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@274273 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/PlatformGTK.cmake
@@ -93,6 +93,7 @@ list(APPEND WebCore_LIBRARIES
     ${GLIB_GMODULE_LIBRARIES}
     ${GLIB_GOBJECT_LIBRARIES}
     ${GLIB_LIBRARIES}
+    ${LCMS2_LIBRARIES}
     ${LIBSECRET_LIBRARIES}
     ${LIBTASN1_LIBRARIES}
     ${HYPHEN_LIBRARIES}
@@ -116,6 +117,7 @@ list(APPEND WebCore_SYSTEM_INCLUDE_DIRECTORIES
     ${ENCHANT_INCLUDE_DIRS}
     ${GIO_UNIX_INCLUDE_DIRS}
     ${GLIB_INCLUDE_DIRS}
+    ${LCMS2_INCLUDE_DIR}
     ${LIBSECRET_INCLUDE_DIRS}
     ${LIBTASN1_INCLUDE_DIRS}
     ${UPOWERGLIB_INCLUDE_DIRS}

Source/WebCore/PlatformWPE.cmake
@@ -58,6 +58,7 @@ list(APPEND WebCore_LIBRARIES
     ${GLIB_GMODULE_LIBRARIES}
     ${GLIB_GOBJECT_LIBRARIES}
     ${GLIB_LIBRARIES}
+    ${LCMS2_LIBRARIES}
     ${LIBTASN1_LIBRARIES}
     ${UPOWERGLIB_LIBRARIES}
 )
@@ -66,6 +67,7 @@ list(APPEND WebCore_SYSTEM_INCLUDE_DIRECTORIES
     ${ATK_INCLUDE_DIRS}
     ${GIO_UNIX_INCLUDE_DIRS}
     ${GLIB_INCLUDE_DIRS}
+    ${LCMS2_INCLUDE_DIR}
     ${LIBTASN1_INCLUDE_DIRS}
     ${UPOWERGLIB_INCLUDE_DIRS}
 )

Source/WebCore/platform/graphics/PlatformDisplay.cpp
@@ -80,6 +80,10 @@
 #include <wtf/NeverDestroyed.h>
 #endif
 
+#if USE(LCMS)
+#include <lcms2.h>
+#endif
+
 namespace WebCore {
 
 std::unique_ptr<PlatformDisplay> PlatformDisplay::createPlatformDisplay()
@@ -170,6 +174,10 @@ PlatformDisplay::~PlatformDisplay()
 #endif
     if (s_sharedDisplayForCompositing == this)
         s_sharedDisplayForCompositing = nullptr;
+#if USE(LCMS)
+    if (m_iccProfile)
+        cmsCloseProfile(m_iccProfile);
+#endif
 }
 
 #if USE(EGL) || USE(GLX)
@@ -264,5 +272,14 @@ void PlatformDisplay::terminateEGLDisplay()
 }
 
 #endif // USE(EGL)
+
+#if USE(LCMS)
+cmsHPROFILE PlatformDisplay::colorProfile() const
+{
+    if (!m_iccProfile)
+        m_iccProfile = cmsCreate_sRGBProfile();
+    return m_iccProfile;
+}
+#endif
 
 } // namespace WebCore

Source/WebCore/platform/graphics/PlatformDisplay.h
@@ -40,6 +40,10 @@ typedef struct _GstGLContext GstGLContext;
 typedef struct _GstGLDisplay GstGLDisplay;
 #endif // ENABLE(VIDEO) && USE(GSTREAMER_GL)
 
+#if USE(LCMS)
+typedef void* cmsHPROFILE;
+#endif
+
 namespace WebCore {
 
 class GLContext;
@@ -82,6 +86,10 @@ public:
     GstGLContext* gstGLContext() const;
 #endif
 
+#if USE(LCMS)
+    virtual cmsHPROFILE colorProfile() const;
+#endif
+
 protected:
     enum class NativeDisplayOwned { No, Yes };
     explicit PlatformDisplay(NativeDisplayOwned);
@@ -98,6 +106,10 @@ protected:
 
 #if USE(EGL) || USE(GLX)
     std::unique_ptr<GLContext> m_sharingGLContext;
+#endif
+
+#if USE(LCMS)
+    mutable cmsHPROFILE m_iccProfile { nullptr };
 #endif
 
 private:

Source/WebCore/platform/graphics/x11/PlatformDisplayX11.cpp
@@ -29,6 +29,7 @@
 #include "GLContext.h"
 
 #if PLATFORM(X11)
+#include <X11/Xatom.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/Xcomposite.h>
 #if PLATFORM(GTK)
@@ -39,6 +40,10 @@
 #if USE(EGL)
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
+#endif
+
+#if USE(LCMS)
+#include <lcms2.h>
 #endif
 
 namespace WebCore {
@@ -149,6 +154,46 @@ void* PlatformDisplayX11::visual() const
 
     return m_visual;
 }
+
+#if USE(LCMS)
+cmsHPROFILE PlatformDisplayX11::colorProfile() const
+{
+    if (m_iccProfile)
+        return m_iccProfile;
+
+    Atom iccAtom = XInternAtom(m_display, "_ICC_PROFILE", False);
+    Atom type;
+    int format;
+    unsigned long itemCount, bytesAfter;
+    unsigned char* data = nullptr;
+    auto result = XGetWindowProperty(m_display, RootWindowOfScreen(DefaultScreenOfDisplay(m_display)), iccAtom, 0L, ~0L, False, XA_CARDINAL, &type, &format, &itemCount, &bytesAfter, &data);
+    if (result == Success && type == XA_CARDINAL && itemCount > 0) {
+        unsigned long dataSize;
+        switch (format) {
+        case 8:
+            dataSize = itemCount;
+            break;
+        case 16:
+            dataSize = sizeof(short) * itemCount;
+            break;
+        case 32:
+            dataSize = sizeof(long) * itemCount;
+            break;
+        default:
+            dataSize = 0;
+            break;
+        }
+
+        if (dataSize)
+            m_iccProfile = cmsOpenProfileFromMem(data, dataSize);
+    }
+
+    if (data)
+        XFree(data);
+
+    return m_iccProfile ? m_iccProfile : PlatformDisplay::colorProfile();
+}
+#endif
 
 } // namespace WebCore
 

Source/WebCore/platform/graphics/x11/PlatformDisplayX11.h
@@ -59,6 +59,10 @@ private:
     void initializeEGLDisplay() override;
 #endif
 
+#if USE(LCMS)
+    cmsHPROFILE colorProfile() const override;
+#endif
+
     ::Display* m_display { nullptr };
     mutable Optional<bool> m_supportsXComposite;
     mutable Optional<bool> m_supportsXDamage;

Source/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.cpp
@@ -40,10 +40,12 @@
 #include "config.h"
 #include "JPEGImageDecoder.h"
 
-extern "C" {
-#if USE(ICCJPEG)
-#include <iccjpeg.h>
+#include "PlatformDisplay.h"
+#if USE(LCMS)
+#include <lcms2.h>
 #endif
+
+extern "C" {
 #include <setjmp.h>
 }
 
@@ -75,7 +77,9 @@ inline bool doFancyUpsampling() { return false; }
 inline bool doFancyUpsampling() { return true; }
 #endif
 
-const int exifMarker = JPEG_APP0 + 1;
+static const int exifMarker = JPEG_APP0 + 1;
+static const int iccMarker = JPEG_APP0 + 2;
+static const unsigned iccHeaderSize = 14;
 
 namespace WebCore {
 
@@ -194,6 +198,51 @@ static ImageOrientation readImageOrientation(jpeg_decompress_struct* info)
     return ImageOrientation::None;
 }
 
+#if USE(LCMS)
+static bool isICCMarker(jpeg_saved_marker_ptr marker)
+{
+    return marker->marker == iccMarker
+        && marker->data_length >= iccHeaderSize
+        && marker->data[0] == 'I'
+        && marker->data[1] == 'C'
+        && marker->data[2] == 'C'
+        && marker->data[3] == '_'
+        && marker->data[4] == 'P'
+        && marker->data[5] == 'R'
+        && marker->data[6] == 'O'
+        && marker->data[7] == 'F'
+        && marker->data[8] == 'I'
+        && marker->data[9] == 'L'
+        && marker->data[10] == 'E'
+        && marker->data[11] == '\0';
+}
+
+static RefPtr<SharedBuffer> readICCProfile(jpeg_decompress_struct* info)
+{
+    auto buffer = SharedBuffer::create();
+    for (jpeg_saved_marker_ptr marker = info->marker_list; marker; marker = marker->next) {
+        if (!isICCMarker(marker))
+            continue;
+
+        unsigned sequenceNumber = marker->data[12];
+        if (!sequenceNumber)
+            return nullptr;
+
+        unsigned markerCount = marker->data[13];
+        if (sequenceNumber > markerCount)
+            return nullptr;
+
+        unsigned markerSize = marker->data_length - iccHeaderSize;
+        buffer->append(reinterpret_cast<const char*>(marker->data + iccHeaderSize), markerSize);
+    }
+
+    if (buffer->isEmpty())
+        return nullptr;
+
+    return buffer;
+}
+#endif
+
 class JPEGImageReader {
     WTF_MAKE_FAST_ALLOCATED;
 public:
@@ -232,13 +281,15 @@ public:
         src->pub.term_source = term_source;
         src->decoder = this;
 
-#if USE(ICCJPEG)
-        // Retain ICC color profile markers for color management.
-        setup_read_icc_profile(&m_info);
-#endif
-
         // Keep APP1 blocks, for obtaining exif data.
         jpeg_save_markers(&m_info, exifMarker, 0xFFFF);
+
+#if USE(LCMS)
+        if (!m_decoder->ignoresGammaAndColorProfile()) {
+            // Keep APP2 blocks, for obtaining ICC profile data.
+            jpeg_save_markers(&m_info, iccMarker, 0xFFFF);
+        }
+#endif
     }
 
     ~JPEGImageReader()
@@ -316,6 +367,10 @@ public:
                 return false;
 
             m_decoder->setOrientation(readImageOrientation(info()));
+#if USE(LCMS)
+            if (!m_decoder->ignoresGammaAndColorProfile() && m_info.out_color_space == rgbOutputColorSpace())
+                m_decoder->setICCProfile(readICCProfile(&m_info));
+#endif
 
             // Don't allocate a giant and superfluous memory buffer when the
             // image is a sequential JPEG.
@@ -490,7 +545,10 @@ JPEGImageDecoder::JPEGImageDecoder(AlphaOption alphaOption, GammaAndColorProfile
 {
 }
 
-JPEGImageDecoder::~JPEGImageDecoder() = default;
+JPEGImageDecoder::~JPEGImageDecoder()
+{
+    clear();
+}
 
 ScalableImageDecoderFrame* JPEGImageDecoder::frameBufferAtIndex(size_t index)
 {
@@ -506,9 +564,24 @@ ScalableImageDecoderFrame* JPEGImageDecoder::frameBufferAtIndex(size_t index)
     return &frame;
 }
 
-bool JPEGImageDecoder::setFailed()
+void JPEGImageDecoder::clear()
 {
     m_reader = nullptr;
+#if USE(LCMS)
+    if (m_iccTransform) {
+        cmsDeleteTransform(m_iccTransform);
+        m_iccTransform = nullptr;
+    }
+    if (m_iccProfile) {
+        cmsCloseProfile(m_iccProfile);
+        m_iccProfile = nullptr;
+    }
+#endif
+}
+
+bool JPEGImageDecoder::setFailed()
+{
+    clear();
     return ScalableImageDecoder::setFailed();
 }
 
@@ -552,11 +625,17 @@ bool JPEGImageDecoder::outputScanlines(ScalableImageDecoderFrame& buffer)
         if (jpeg_read_scanlines(info, samples, 1) != 1)
             return false;
 
-        auto* currentAddress = buffer.backingStore()->pixelAt(0, sourceY);
+        auto* row = buffer.backingStore()->pixelAt(0, sourceY);
+        auto* currentAddress = row;
         for (int x = 0; x < width; ++x) {
             setPixel<colorSpace>(buffer, currentAddress, samples, x);
             ++currentAddress;
         }
+
+#if USE(LCMS)
+        if (m_iccTransform)
+            cmsDoTransform(m_iccTransform, row, row, info->output_width);
+#endif
     }
     return true;
 }
@@ -585,6 +664,11 @@ bool JPEGImageDecoder::outputScanlines()
             unsigned char* row = reinterpret_cast<unsigned char*>(buffer.backingStore()->pixelAt(0, info->output_scanline));
             if (jpeg_read_scanlines(info, &row, 1) != 1)
                 return false;
+
+#if USE(LCMS)
+            if (m_iccTransform)
+                cmsDoTransform(m_iccTransform, row, row, info->output_width);
+#endif
          }
          return true;
      }
@@ -623,8 +707,10 @@ void JPEGImageDecoder::decode(bool onlySize, bool allDataReceived)
     if (failed())
         return;
 
-    if (!m_reader)
+    if (!m_reader) {
+        clear();
         m_reader = makeUnique<JPEGImageReader>(this);
+    }
 
     // If we couldn't decode the image but we've received all the data, decoding
     // has failed.
@@ -633,7 +719,25 @@ void JPEGImageDecoder::decode(bool onlySize, bool allDataReceived)
     // If we're done decoding the image, we don't need the JPEGImageReader
     // anymore.  (If we failed, |m_reader| has already been cleared.)
     else if (!m_frameBufferCache.isEmpty() && (m_frameBufferCache[0].isComplete()))
-        m_reader = nullptr;
+        clear();
 }
+
+#if USE(LCMS)
+void JPEGImageDecoder::setICCProfile(RefPtr<SharedBuffer>&& buffer)
+{
+    if (!buffer)
+        return;
+
+    m_iccProfile = cmsOpenProfileFromMem(buffer->data(), buffer->size());
+    if (!m_iccProfile)
+        return;
+
+    auto* displayProfile = PlatformDisplay::sharedDisplay().colorProfile();
+    if (cmsGetColorSpace(m_iccProfile) != cmsSigRgbData || cmsGetColorSpace(displayProfile) != cmsSigRgbData)
+        return;
+
+    m_iccTransform = cmsCreateTransform(m_iccProfile, TYPE_BGRA_8, displayProfile, TYPE_BGRA_8, INTENT_RELATIVE_COLORIMETRIC, 0);
+}
+#endif
 
 }

Source/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.h
@@ -36,6 +36,11 @@ extern "C" {
 #include "jpeglib.h"
 }
 
+#if USE(LCMS)
+typedef void* cmsHPROFILE;
+typedef void* cmsHTRANSFORM;
+#endif
+
 namespace WebCore {
 
     class JPEGImageReader;
@@ -62,6 +67,9 @@ namespace WebCore {
         void jpegComplete();
 
         void setOrientation(ImageOrientation orientation) { m_orientation = orientation; }
+#if USE(LCMS)
+        void setICCProfile(RefPtr<SharedBuffer>&&);
+#endif
 
     private:
         JPEGImageDecoder(AlphaOption, GammaAndColorProfileOption);
@@ -78,7 +86,13 @@ namespace WebCore {
         template <J_COLOR_SPACE colorSpace, bool isScaled>
         bool outputScanlines(ScalableImageDecoderFrame& buffer);
 
+        void clear();
+
         std::unique_ptr<JPEGImageReader> m_reader;
+#if USE(LCMS)
+        cmsHPROFILE m_iccProfile { nullptr };
+        cmsHTRANSFORM m_iccTransform { nullptr };
+#endif
     };
 
 } // namespace WebCore

commit 985af5e463c82a19ca61ee9d34ea692882a5f0cb

[GLIB] REGRESSION(r274273): fast/canvas/webgl/gl-teximage-imagebitmap.html is failing https://bugs.webkit.org/show_bug.cgi?id=223297
Reviewed by Carlos Garcia Campos.

This test started failing in GTK after r274273. The test fails due to a pixel color difference of 1.
Change failing test to allow a tolerance of 1 when comparing expected and actual results.

* fast/canvas/webgl/gl-teximage-imagebitmap-expected.txt:
* fast/canvas/webgl/gl-teximage-imagebitmap.html:


Canonical link: https://commits.webkit.org/235514@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@274694 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/fast/canvas/webgl/gl-teximage-imagebitmap-expected.txt
@@ -1,14 +1,14 @@
 PASS getError was expected value: NO_ERROR : Should be no errors from setup.
 PASS getError was expected value: NO_ERROR : Should be no errors from setup
-PASS pixel 0, 149 should be 0, 0, 0, 255 was 0, 0, 0, 255
-PASS pixel 150, 149 should be 0, 0, 255, 255 was 0, 0, 255, 255
-PASS pixel 299, 149 should be 255, 0, 0, 255 was 255, 0, 0, 255
-PASS pixel 0, 75 should be 255, 0, 255, 255 was 255, 0, 255, 255
-PASS pixel 150, 75 should be 255, 0, 0, 255 was 255, 0, 0, 255
-PASS pixel 299, 75 should be 0, 255, 0, 255 was 0, 255, 0, 255
-PASS pixel 0, 0 should be 255, 0, 0, 255 was 255, 0, 0, 255
-PASS pixel 150, 0 should be 255, 255, 0, 255 was 255, 255, 0, 255
-PASS pixel 299, 0 should be 255, 0, 0, 255 was 255, 0, 0, 255
+PASS pixel 0, 149 was 0, 0, 0, 255
+PASS pixel 150, 149 was 0, 0, 255, 255
+PASS pixel 299, 149 was 255, 0, 0, 255
+PASS pixel 0, 75 was 255, 0, 255, 255
+PASS pixel 150, 75 was 255, 0, 0, 255
+PASS pixel 299, 75 was 0, 255, 0, 255
+PASS pixel 0, 0 was 255, 0, 0, 255
+PASS pixel 150, 0 was 255, 255, 0, 255
+PASS pixel 299, 0 was 255, 0, 0, 255
 PASS successfullyParsed is true
 
 TEST COMPLETE

LayoutTests/fast/canvas/webgl/gl-teximage-imagebitmap.html
@@ -19,9 +19,9 @@ function loadImage(url) {
   });
 }
 
-function checkPixel(buf, width, x, y, color) {
-  var off = (y * width + x) * 4;
-  var msg = "pixel " + x + ", " + y + " should be " +
+function checkPixel(buf, width, x, y, color, tolerance) {
+  function msgFailed() {
+    return "pixel " + x + ", " + y + " should be " +
             color[0] + ", " +
             color[1] + ", " +
             color[2] + ", " +
@@ -30,14 +30,23 @@ function checkPixel(buf, width, x, y, color) {
             buf[off + 1] + ", " +
             buf[off + 2] + ", " +
             buf[off + 3];
-
+  }
+  function msgPassed() {
+    return "pixel " + x + ", " + y + " was " +
+            color[0] + ", " +
+            color[1] + ", " +
+            color[2] + ", " +
+            color[3];
+  }
+  var off = (y * width + x) * 4;
   for (var ii = 0; ii < 4; ++ii) {
-    if (buf[off + ii] != color[ii]) {
-      testFailed(msg);
+    var delta = Math.abs(buf[off + ii] - color[ii]);
+    if (delta > tolerance) {
+      testFailed(msgFailed());
       return;
     }
   }
-  testPassed(msg);
+  testPassed(msgPassed());
 }
 
 function checkPixelRange(buf, width, x, y, color, allowedRange) {
@@ -102,6 +111,7 @@ async function test() {
   var buf = new Uint8Array(width * height * 4);
   gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, buf);
 
+  var tolerance = 1;
   var left = 0;
   var middle = Math.floor(width / 2);
   var right = width - 1;
@@ -113,7 +123,7 @@ async function test() {
   checkPixel(buf, width, right,  top,    [255,   0,   0, 255]);
   checkPixel(buf, width, left,   center, [255,   0, 255, 255]);
   checkPixel(buf, width, middle, center, [255,   0,   0, 255]);
-  checkPixel(buf, width, right,  center, [  0, 255,   0, 255]);
+  checkPixel(buf, width, right,  center, [  0, 255,   0, 255], tolerance);
   checkPixel(buf, width, left,   bottom, [255,   0,   0, 255]);
   checkPixel(buf, width, middle, bottom, [255, 255,   0, 255]);
   checkPixel(buf, width, right,  bottom, [255,   0,   0, 255]);