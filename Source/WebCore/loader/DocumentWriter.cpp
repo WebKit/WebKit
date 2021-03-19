ab486d9310ab140f9ba214aaedf80680ba0b2d23
Rob Buis <rbuis@igalia.com>
2021-03-19 16:35:43 +0000

    Protect frame before calling didBeginDocument https://bugs.webkit.org/show_bug.cgi?id=217185
    Patch by Rob Buis <rbuis@igalia.com> on 2021-03-19
    Reviewed by Alex Christensen.
    
    Protect frame before calling didBeginDocument
    since it could potentially delete the frame
    through event handling.
    
    * loader/DocumentWriter.cpp:
    (WebCore::DocumentWriter::begin):
    
    Canonical link: https://commits.webkit.org/235543@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@274723 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/loader/DocumentWriter.cpp
+++ Source/WebCore/loader/DocumentWriter.cpp
@@ -192,6 +192,8 @@ bool DocumentWriter::begin(const URL& urlReference, bool dispatch, Document* own
         document->contentSecurityPolicy()->setInsecureNavigationRequestsToUpgrade(existingDocument->contentSecurityPolicy()->takeNavigationRequestsToUpgrade());
     }
 
+    auto protectedFrame = makeRef(*m_frame);
+
     m_frame->loader().didBeginDocument(dispatch);
 
     document->implicitOpen();
