5b373bdcda4110cc40c8af30c279beb0e65f13df
Jonathan Bedard <jbedard@apple.com>
2021-03-22 19:02:52 +0000

    [webkitscmpy] Allow user to force canonicalization for a specific set of commits https://bugs.webkit.org/show_bug.cgi?id=223579 <rdar://problem/75693154>
    Reviewed by Aakash Jain.
    
    * Scripts/libraries/webkitscmpy/setup.py: Bump version.
    * Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py: Ditto.
    * Scripts/libraries/webkitscmpy/webkitscmpy/program/canonicalize/__init__.py:
    (Canonicalize.parser): Add --number argument.
    (Canonicalize.main): Use --number to set the number of commits to be canonicalized.
    * Scripts/libraries/webkitscmpy/webkitscmpy/test/canonicalize_unittest.py:
    (TestCanonicalize.test_number):
    
    
    Canonical link: https://commits.webkit.org/235580@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@274771 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/libraries/webkitscmpy/setup.py
+++ Tools/Scripts/libraries/webkitscmpy/setup.py
@@ -29,7 +29,7 @@ def readme():
 
 setup(
     name='webkitscmpy',
-    version='0.13.4',
+    version='0.13.5',
     description='Library designed to interact with git and svn repositories.',
     long_description=readme(),
     classifiers=[
