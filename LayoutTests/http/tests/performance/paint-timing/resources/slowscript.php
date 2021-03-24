a3a5fb8ca32463385dd23e1037bb1e831680abdc
Chris Gambrell <cgambrell@apple.com>
2021-03-24 19:50:29 +0000

    [LayoutTests] Convert http/tests/performance convert PHP to Python https://bugs.webkit.org/show_bug.cgi?id=223599 <rdar://problem/75707991>
    Reviewed by Jonathan Bedard.
    
    * http/tests/performance/paint-timing/performance-paint-timing-fcp-after-visually-non-empty-for-num-chars.html:
    * http/tests/performance/paint-timing/performance-paint-timing-fcp-after-visually-non-empty-for-style.html:
    * http/tests/performance/paint-timing/resources/slowscript.php: Removed.
    * http/tests/performance/paint-timing/resources/slowscript.py: Added.
    * http/tests/performance/paint-timing/resources/slowstyle.php: Removed.
    * http/tests/performance/paint-timing/resources/slowstyle.py: Added.
    
    
    Canonical link: https://commits.webkit.org/235717@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@274964 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/performance/paint-timing/resources/slowscript.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_GET["delay"]))
-    sleep($_GET["delay"]);
-header('Cache-Control: no-cache, must-revalidate');
-header('Content-Type: text/javascript');
-
-echo($_GET["script"])
-?>
-
