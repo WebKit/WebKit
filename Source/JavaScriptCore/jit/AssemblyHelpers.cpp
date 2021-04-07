4c38d0e7b9d87fb632c34ac3d4780baaa2c379e6
Mark Lam <mark.lam@apple.com>
2021-04-07 18:22:29 +0000

    Fix a typo in JITUncoughtExceptionAfterCall. https://bugs.webkit.org/show_bug.cgi?id=224290
    Reviewed by Keith Miller.
    
    * assembler/AbortReason.h:
    * jit/AssemblyHelpers.cpp:
    (JSC::AssemblyHelpers::jitReleaseAssertNoException):
    
    
    Canonical link: https://commits.webkit.org/236260@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@275615 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/jit/AssemblyHelpers.cpp
+++ Source/JavaScriptCore/jit/AssemblyHelpers.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2020 Apple Inc. All rights reserved.
+ * Copyright (C) 2011-2021 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -227,7 +227,7 @@ void AssemblyHelpers::jitReleaseAssertNoException(VM& vm)
 #elif USE(JSVALUE32_64)
     noException = branch32(Equal, AbsoluteAddress(vm.addressOfException()), TrustedImm32(0));
 #endif
-    abortWithReason(JITUncoughtExceptionAfterCall);
+    abortWithReason(JITUncaughtExceptionAfterCall);
     noException.link(this);
 }
 
