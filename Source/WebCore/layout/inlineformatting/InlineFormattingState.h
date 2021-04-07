a97851bdca71b215117fc189aced882d0bbb0764
Zalan Bujtas <zalan@apple.com>
2021-04-07 19:25:52 +0000

    [LFC][IFC] Remove InlineLines and InlineLineBoxes inline capacity of 10 https://bugs.webkit.org/show_bug.cgi?id=224296
    Reviewed by Antti Koivisto.
    
    A casual browsing session shows that the average line count is ~1.4. Having 10 as initial capacity seems a little excessive.
    (and neither PerformanceTests/Layout/line-layout-inline-block.html nor PerformanceTests/Layout/line-layout-inline-level-boxes.html microbenchmarks show regression locally)
    
    * layout/inlineformatting/InlineFormattingState.h:
    
    
    Canonical link: https://commits.webkit.org/236265@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@275620 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/layout/inlineformatting/InlineFormattingState.h
+++ Source/WebCore/layout/inlineformatting/InlineFormattingState.h
@@ -38,8 +38,8 @@ namespace WebCore {
 namespace Layout {
 
 using InlineItems = Vector<InlineItem>;
-using InlineLines = Vector<InlineLineGeometry, 10>;
-using InlineLineBoxes = Vector<LineBox, 10>;
+using InlineLines = Vector<InlineLineGeometry>;
+using InlineLineBoxes = Vector<LineBox>;
 using InlineLineRuns = Vector<LineRun>;
 
 // InlineFormattingState holds the state for a particular inline formatting context tree.
