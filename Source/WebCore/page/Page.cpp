ae65dccdbcc5ce020b688c6e7727346c9093f3a6
Ryosuke Niwa <rniwa@webkit.org>
2021-04-08 23:56:37 +0000

    Cherry-pick r273300. rdar://problem/76375521
        Handle Page::didFinishLoadingImageForElement asynchronously
        https://bugs.webkit.org/show_bug.cgi?id=221390
    
        Reviewed by Antti Koivisto.
    
        Use EventLoop instead of RunLoop::main() to be thread safe in iOS WebKit1.
    
        * page/Page.cpp:
        (WebCore::Page::didFinishLoadingImageForElement):
    
        Canonical link: https://commits.webkit.org/234460@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@273300 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/232923.316@safari-611-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-611-branch@275695 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/Page.cpp
+++ Source/WebCore/page/Page.cpp
@@ -53,6 +53,7 @@
 #include "EmptyClients.h"
 #include "Event.h"
 #include "EventHandler.h"
+#include "EventLoop.h"
 #include "EventNames.h"
 #include "ExtensionStyleSheets.h"
 #include "FocusController.h"
@@ -3286,13 +3287,14 @@ void Page::configureLoggingChannel(const String& channelName, WTFLogChannelState
 
 void Page::didFinishLoadingImageForElement(HTMLImageElement& element)
 {
-    RunLoop::main().dispatch([this, weakThis = makeWeakPtr(*this), element = makeRef(element)]() {
-        if (!weakThis)
+    element.document().eventLoop().queueTask(TaskSource::Networking, [element = makeRef(element)]() {
+        auto frame = makeRefPtr(element->document().frame());
+        if (!frame)
             return;
 
-        if (auto frame = makeRefPtr(element->document().frame()))
-            frame->editor().revealSelectionIfNeededAfterLoadingImageForElement(element);
-        chrome().client().didFinishLoadingImageForElement(element);
+        frame->editor().revealSelectionIfNeededAfterLoadingImageForElement(element);
+        if (auto* page = frame->page(); element->document().frame() == frame)
+            page->chrome().client().didFinishLoadingImageForElement(element);
     });
 }
 
