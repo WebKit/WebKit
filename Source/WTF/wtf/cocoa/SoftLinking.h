062f74413f617b996166507f6e148f80c542ae68
Jer Noble <jer.noble@apple.com>
2021-04-09 20:37:32 +0000

    Cherry-pick r275775. rdar://problem/76467684
        WTF SoftLinking macros can cause collisions with their target functions
        https://bugs.webkit.org/show_bug.cgi?id=224379
        <rdar://76434202>
    
        Reviewed by Eric Carlson.
    
        Source/WebCore:
    
        Don't include the SoftLinkLibxslt.h file in the .cpp file.
    
        * xml/SoftLinkLibxslt.cpp:
        * xml/SoftLinkLibxslt.h:
    
        Source/WebCore/PAL:
    
        * pal/cf/CoreMediaSoftLink.h:
        * pal/cocoa/MediaToolboxSoftLink.h:
        * pal/cocoa/OpenGLSoftLinkCocoa.h:
        * pal/cocoa/PassKitSoftLink.h:
        * pal/ios/UIKitSoftLink.h:
        * pal/spi/cocoa/AccessibilitySupportSoftLink.cpp:
        * pal/spi/cocoa/AccessibilitySupportSoftLink.h:
    
        Source/WTF:
    
        Do not declare an inline version of the target function, as this will collide if the target function
        becomes inlined. Instead, callers should use the explicit softLink_Library_Function() syntax, or
        the LibrarySoftLinking.h headers should use #defines to re-declare the function name.
    
        * wtf/cocoa/SoftLinking.h:
    
        Canonical link: https://commits.webkit.org/236350@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@275775 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/236111.12@safari-612.1.10-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-612.1.10-branch@275777 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/cocoa/SoftLinking.h
+++ Source/WTF/wtf/cocoa/SoftLinking.h
@@ -326,6 +326,7 @@ static void* lib##Library() \
 #define SOFT_LINK_LIBRARY_FOR_HEADER(functionNamespace, lib) \
     namespace functionNamespace { \
     extern void* lib##Library(bool isOptional = false); \
+    bool is##lib##LibraryAvailable(); \
     inline bool is##lib##LibaryAvailable() { \
         return lib##Library(true) != nullptr; \
     } \
@@ -333,7 +334,7 @@ static void* lib##Library() \
 
 #define SOFT_LINK_LIBRARY_FOR_SOURCE(functionNamespace, lib) \
     namespace functionNamespace { \
-    void* lib##Library(bool isOptional); \
+    extern void* lib##Library(bool isOptional = false); \
     void* lib##Library(bool isOptional) \
     { \
         static void* library; \
@@ -516,10 +517,6 @@ static void* lib##Library() \
         return softLink##framework##functionName parameterNames; \
     } \
     } \
-    inline __attribute__((__always_inline__)) resultType functionName parameterDeclarations \
-    {\
-        return functionNamespace::softLink##framework##functionName parameterNames; \
-    }
 
 #define SOFT_LINK_FUNCTION_FOR_SOURCE_WITH_EXPORT(functionNamespace, framework, functionName, resultType, parameterDeclarations, parameterNames, export) \
     WTF_EXTERN_C_BEGIN \
