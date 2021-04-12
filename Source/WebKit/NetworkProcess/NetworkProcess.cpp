e5d27b2bda76242c4f217ef3ef8e68bf6118a101
Sihui Liu <sihui_liu@apple.com>
2021-04-12 23:43:49 +0000

    Create WebIDBServer only when it is needed https://bugs.webkit.org/show_bug.cgi?id=224305 rdar://71962196
    Reviewed by Alex Christensen.
    
    Currently each WebIDBServer has a separate thread, so we don't want to create or keep WebIDBServer if it's not
    in use. There are two cases where network process needs a WebIDBServer:
    1. handle requests from UI process to collect or remove data
    2. handle requests from Web process to perform IDB operations
    
    Previously, we created a WebIDBServer when network process connects to a web process, but that does not mean web
    process will perform IDB operations and we may create a thread that's not used. To avoid this, add a new message
    AddIDBConnection for web process to ensure network process has WebIDBServer when it's about to perform operation.
    
    Also, previously network process removes a WebIDBServer when session is removed and WebIDBServer is not binded
    with any web process connection. Now we remove WebIDBServer when it's done handling requests, that is count of
    pending requests from UI process is 0 and WebIDBServer is not binded with web process connection. We also remove
    WebIDBServer at when network process is about to be destroyed (NetworkProcess::didClose) so we can break the
    reference cycle of NetworkProcess-WebIDBServer-IDBServer, and make sure thread exits.
    
    * NetworkProcess/IndexedDB/WebIDBServer.cpp:
    (WebKit::WebIDBServer::create):
    (WebKit::WebIDBServer::WebIDBServer):
    (WebKit::m_closeCallback):
    (WebKit::WebIDBServer::~WebIDBServer):
    (WebKit::WebIDBServer::getOrigins):
    (WebKit::WebIDBServer::closeAndDeleteDatabasesModifiedSince):
    (WebKit::WebIDBServer::closeAndDeleteDatabasesForOrigins):
    (WebKit::WebIDBServer::renameOrigin):
    (WebKit::WebIDBServer::removeConnection):
    (WebKit::WebIDBServer::close):
    (WebKit::WebIDBServer::tryClose):
    * NetworkProcess/IndexedDB/WebIDBServer.h:
    * NetworkProcess/NetworkConnectionToWebProcess.cpp:
    (WebKit::NetworkConnectionToWebProcess::addIDBConnection):
    * NetworkProcess/NetworkConnectionToWebProcess.h:
    * NetworkProcess/NetworkConnectionToWebProcess.messages.in:
    * NetworkProcess/NetworkProcess.cpp:
    (WebKit::NetworkProcess::didClose):
    (WebKit::NetworkProcess::createNetworkConnectionToWebProcess):
    (WebKit::NetworkProcess::destroySession):
    (WebKit::NetworkProcess::createWebIDBServer):
    (WebKit::NetworkProcess::connectionToWebProcessClosed):
    (WebKit::NetworkProcess::removeWebIDBServerIfPossible): Deleted. Move the removal code to WebIDBServer.
    * WebProcess/Databases/IndexedDB/WebIDBConnectionToServer.cpp:
    (WebKit::WebIDBConnectionToServer::WebIDBConnectionToServer):
    
    
    Canonical link: https://commits.webkit.org/236411@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@275846 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/NetworkProcess/NetworkProcess.cpp
+++ Source/WebKit/NetworkProcess/NetworkProcess.cpp
@@ -272,6 +272,10 @@ void NetworkProcess::didClose(IPC::Connection&)
     forEachNetworkSession([&] (auto& networkSession) {
         platformFlushCookies(networkSession.sessionID(), [callbackAggregator] { });
     });
+
+    // Make sure references to NetworkProcess in spaceRequester and closeHandler is removed.
+    for (auto& server : m_webIDBServers.values())
+        server->close();
 }
 
 void NetworkProcess::didCreateDownload()
@@ -391,8 +395,6 @@ void NetworkProcess::createNetworkConnectionToWebProcess(ProcessIdentifier ident
     connection.setOnLineState(NetworkStateNotifier::singleton().onLine());
 
     m_storageManagerSet->addConnection(connection.connection());
-
-    webIDBServer(sessionID).addConnection(connection.connection(), identifier);
 }
 
 void NetworkProcess::clearCachedCredentials(PAL::SessionID sessionID)
@@ -552,8 +554,6 @@ void NetworkProcess::destroySession(PAL::SessionID sessionID)
 #endif
 
     m_storageManagerSet->remove(sessionID);
-
-    removeWebIDBServerIfPossible(sessionID);
 }
 
 #if ENABLE(RESOURCE_LOAD_STATISTICS)
@@ -2330,10 +2330,13 @@ Ref<WebIDBServer> NetworkProcess::createWebIDBServer(PAL::SessionID sessionID)
         path = m_idbDatabasePaths.get(sessionID);
     }
 
-    return WebIDBServer::create(sessionID, path, [this, weakThis = makeWeakPtr(this), sessionID](const auto& origin, uint64_t spaceRequested) {
-        RefPtr<StorageQuotaManager> storageQuotaManager = weakThis ? this->storageQuotaManager(sessionID, origin) : nullptr;
-        return storageQuotaManager ? storageQuotaManager->requestSpaceOnBackgroundThread(spaceRequested) : StorageQuotaManager::Decision::Deny;
-    });
+    auto spaceRequester = [protectedThis = makeRef(*this), sessionID](const auto& origin, uint64_t spaceRequested) {
+        return protectedThis->storageQuotaManager(sessionID, origin)->requestSpaceOnBackgroundThread(spaceRequested);
+    };
+    auto closeHandler = [protectedThis = makeRef(*this), sessionID]() {
+        protectedThis->m_webIDBServers.remove(sessionID);
+    };
+    return WebIDBServer::create(sessionID, path, WTFMove(spaceRequester), WTFMove(closeHandler));
 }
 
 WebIDBServer& NetworkProcess::webIDBServer(PAL::SessionID sessionID)
@@ -2361,24 +2364,6 @@ void NetworkProcess::setSessionStorageQuotaManagerIDBRootPath(PAL::SessionID ses
     auto* sessionStorageQuotaManager = m_sessionStorageQuotaManagers.get(sessionID);
     ASSERT(sessionStorageQuotaManager);
     sessionStorageQuotaManager->setIDBRootPath(idbRootPath);
-}
-
-void NetworkProcess::removeWebIDBServerIfPossible(PAL::SessionID sessionID)
-{
-    ASSERT(RunLoop::isMain());
-
-    auto iterator = m_webIDBServers.find(sessionID);
-    if (iterator == m_webIDBServers.end())
-        return;
-
-    if (m_networkSessions.contains(sessionID))
-        return;
-
-    if (iterator->value->hasConnection())
-        return;
-
-    iterator->value->close();
-    m_webIDBServers.remove(iterator);
 }
 
 void NetworkProcess::syncLocalStorage(CompletionHandler<void()>&& completionHandler)
@@ -2667,10 +2652,8 @@ void NetworkProcess::connectionToWebProcessClosed(IPC::Connection& connection, P
 {
     m_storageManagerSet->removeConnection(connection);
 
-    auto* webIDBServer = m_webIDBServers.get(sessionID);
-    ASSERT(webIDBServer);
-    webIDBServer->removeConnection(connection);
-    removeWebIDBServerIfPossible(sessionID);
+    if (auto* server = m_webIDBServers.get(sessionID))
+        server->removeConnection(connection);
 }
 
 NetworkConnectionToWebProcess* NetworkProcess::webProcessConnection(ProcessIdentifier identifier) const
