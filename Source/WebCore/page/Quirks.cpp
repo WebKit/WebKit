65deaea412637f864beff6aabadb6dbec6a46c64
Peng Liu <peng.liu6@apple.com>
2021-04-14 19:58:44 +0000

    Revert "Cherry-pick r274877. rdar://problem/76373841"
    This reverts commit r275741.
    
    Canonical link: https://commits.webkit.org/232923.375@safari-611-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-611-branch@275963 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/Quirks.cpp
+++ Source/WebCore/page/Quirks.cpp
@@ -1362,9 +1362,6 @@ bool Quirks::requiresUserGestureToLoadInPictureInPicture() const
     if (!m_requiresUserGestureToLoadInPictureInPicture) {
         auto domain = RegistrableDomain(m_document->topDocument().url());
         m_requiresUserGestureToLoadInPictureInPicture = domain.string() == "twitter.com"_s;
-
-        auto domain = RegistrableDomain(m_document->topDocument().url());
-        m_shouldDisableEndFullscreenEventWhenEnteringPictureInPictureFromFullscreenQuirk = m_shouldDisableEndFullscreenEventWhenEnteringPictureInPictureFromFullscreenQuirk || domain == "espn.com"_s;
     }
 
     return *m_requiresUserGestureToLoadInPictureInPicture;
