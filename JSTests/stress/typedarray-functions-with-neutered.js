d4e6a6e9e702e3e11e34911b76bd0bc31fb1eb35
Angelos Oikonomopoulos <angelos@igalia.com>
2021-04-14 09:45:49 +0000

    [JSC] Unskip typedarray-functions-with-neutered.js on MIPS https://bugs.webkit.org/show_bug.cgi?id=224428
    Unreviewed test gardening.
    
    This appears to no longer fail.
    
    
    * stress/typedarray-functions-with-neutered.js:
    
    
    Canonical link: https://commits.webkit.org/236501@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@275939 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/typedarray-functions-with-neutered.js
+++ JSTests/stress/typedarray-functions-with-neutered.js
@@ -1,4 +1,3 @@
-//@ skip if $architecture == "mips"
 typedArrays = [Int8Array, Uint8Array, Uint8ClampedArray, Int16Array, Uint16Array, Int32Array, Uint32Array, Float32Array, Float64Array];
 
 proto = Int8Array.prototype.__proto__;
