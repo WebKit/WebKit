7892523cc9a4c5d35664b53b1e8700ec19207bf8
Mark Lam <mark.lam@apple.com>
2021-04-14 21:57:21 +0000

    Apply DeferTermination in some utility functions in the jsc shell. https://bugs.webkit.org/show_bug.cgi?id=224572 rdar://76646089
    Reviewed by Yusuke Suzuki.
    
    This is to make sure that these functions don't get in the way of testing with
    the watchdog.  Since these are only test utility functions, just doing the simple
    thing of using a DeferTermination scope is the right thing to do here.
    
    * jsc.cpp:
    
    
    
    Canonical link: https://commits.webkit.org/236524@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@275971 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/jsc.cpp
+++ Source/JavaScriptCore/jsc.cpp
@@ -31,6 +31,7 @@
 #include "CompilerTimingScope.h"
 #include "Completion.h"
 #include "ConfigFile.h"
+#include "DeferTermination.h"
 #include "DeferredWorkTimer.h"
 #include "Disassembler.h"
 #include "Exception.h"
@@ -2522,6 +2523,7 @@ JSC_DEFINE_HOST_FUNCTION(functionGenerateHeapSnapshot, (JSGlobalObject* globalOb
 {
     VM& vm = globalObject->vm();
     JSLockHolder lock(vm);
+    DeferTermination deferScope(vm);
     auto scope = DECLARE_THROW_SCOPE(vm);
 
     HeapSnapshotBuilder snapshotBuilder(vm.ensureHeapProfiler());
@@ -2537,6 +2539,7 @@ JSC_DEFINE_HOST_FUNCTION(functionGenerateHeapSnapshotForGCDebugging, (JSGlobalOb
 {
     VM& vm = globalObject->vm();
     JSLockHolder lock(vm);
+    DeferTermination deferScope(vm);
     auto scope = DECLARE_THROW_SCOPE(vm);
     String jsonString;
     {
@@ -2580,6 +2583,7 @@ JSC_DEFINE_HOST_FUNCTION(functionStartSamplingProfiler, (JSGlobalObject* globalO
 JSC_DEFINE_HOST_FUNCTION(functionSamplingProfilerStackTraces, (JSGlobalObject* globalObject, CallFrame*))
 {
     VM& vm = globalObject->vm();
+    DeferTermination deferScope(vm);
     auto scope = DECLARE_THROW_SCOPE(vm);
 
     if (!vm.samplingProfiler())
