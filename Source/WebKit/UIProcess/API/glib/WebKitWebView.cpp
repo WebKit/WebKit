32874330cbaf74e6b27f6ebd20498dfa99883d4d
Carlos Garcia Campos <carlosgc@webkit.org>
2021-04-15 19:26:15 +0000

    Cherry-pick r272301. rdar://problem/76375364
        [GTK][WPE] Reduce the use of SoupURI in preparation for libsoup3
        https://bugs.webkit.org/show_bug.cgi?id=221251
    
        Reviewed by Adrian Perez de Castro.
    
        Source/WebCore:
    
        * platform/network/soup/AuthenticationChallengeSoup.cpp:
        (WebCore::protectionSpaceServerTypeFromURL):
        (WebCore::protectionSpaceFromSoupAuthAndURL):
        (WebCore::AuthenticationChallenge::AuthenticationChallenge):
        (WebCore::protectionSpaceServerTypeFromURI): Deleted.
        (WebCore::protectionSpaceFromSoupAuthAndMessage): Deleted.
    
        Source/WebKit:
    
        * UIProcess/API/glib/WebKitURISchemeRequest.cpp:
        (webkit_uri_scheme_request_get_scheme):
        (webkit_uri_scheme_request_get_path):
        * UIProcess/API/glib/WebKitWebView.cpp:
        (webkit_web_view_load_uri):
    
        Canonical link: https://commits.webkit.org/233632@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@272301 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/232923.386@safari-611-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-611-branch@276048 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/glib/WebKitWebView.cpp
+++ Source/WebKit/UIProcess/API/glib/WebKitWebView.cpp
@@ -71,12 +71,11 @@
 #include <JavaScriptCore/JSRetainPtr.h>
 #include <jsc/JSCContextPrivate.h>
 #include <WebCore/CertificateInfo.h>
-#include <WebCore/GUniquePtrSoup.h>
 #include <WebCore/JSDOMExceptionHandling.h>
 #include <WebCore/PlatformScreen.h>
 #include <WebCore/RefPtrCairo.h>
-#include <WebCore/URLSoup.h>
 #include <glib/gi18n-lib.h>
+#include <libsoup/soup.h>
 #include <wtf/SetForScope.h>
 #include <wtf/URL.h>
 #include <wtf/glib/GRefPtr.h>
@@ -3020,8 +3019,7 @@ void webkit_web_view_load_uri(WebKitWebView* webView, const gchar* uri)
     g_return_if_fail(WEBKIT_IS_WEB_VIEW(webView));
     g_return_if_fail(uri);
 
-    GUniquePtr<SoupURI> soupURI(soup_uri_new(uri));
-    getPage(webView).loadRequest(soupURIToURL(soupURI.get()));
+    getPage(webView).loadRequest(URL({ }, String::fromUTF8(uri)));
 }
 
 /**
