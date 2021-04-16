055f1b3b24376a912b90208d8e3306af9d1982f5
Youenn Fablet <youenn@apple.com>
2021-04-16 08:39:15 +0000

    Remove a no longer necessary callOnMainThread in Worker::createRTCRtpScriptTransformer https://bugs.webkit.org/show_bug.cgi?id=224539
    Reviewed by Darin Adler.
    
    * workers/Worker.cpp:
    (WebCore::Worker::createRTCRtpScriptTransformer):
    ThreadSafeRefCounted already handles it with the DestructionThread::Main option.
    
    
    Canonical link: https://commits.webkit.org/236620@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@276124 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/workers/Worker.cpp
+++ Source/WebCore/workers/Worker.cpp
@@ -257,7 +257,6 @@ void Worker::createRTCRtpScriptTransformer(RTCRtpScriptTransform& transform, Mes
     m_contextProxy.postTaskToWorkerGlobalScope([transform = makeRef(transform), options = WTFMove(options)](auto& context) mutable {
         if (auto transformer = downcast<DedicatedWorkerGlobalScope>(context).createRTCRtpScriptTransformer(WTFMove(options)))
             transform->setTransformer(*transformer);
-        callOnMainThread([transform = WTFMove(transform)] { });
     });
 
 }
