224190496f7a1554e54c1fd0ee85279064f4988f
Chris Dumez <cdumez@apple.com>
2021-04-23 23:01:48 +0000

    http/tests/security/referrer-policy-header.html is slow https://bugs.webkit.org/show_bug.cgi?id=224998
    Reviewed by Geoffrey Garen.
    
    The test is a flaky timeout on the bots because it is slow. On my machine, it takes 17 seconds to run
    it with a debug build. To speed things up, I am now running the subtests in parallel instead of
    sequentially. I also split the test in 2 (regular response vs multipart response). Each tests now takes
    about ~5 seconds to run on my machine.
    
    * http/tests/security/referrer-policy-header-expected.txt:
    * http/tests/security/referrer-policy-header-multipart-expected.txt: Added.
    * http/tests/security/referrer-policy-header-multipart.html: Added.
    * http/tests/security/referrer-policy-header-test.js: Copied from LayoutTests/http/tests/security/referrer-policy-header.html.
    (printResults):
    (onmessage):
    (runTests):
    * http/tests/security/referrer-policy-header.html:
    * http/tests/security/resources/postReferrer.py:
    * http/tests/security/resources/serve-referrer-policy-and-test.py:
    
    
    Canonical link: https://commits.webkit.org/236979@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@276526 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/security/resources/serve-referrer-policy-and-test.py
+++ LayoutTests/http/tests/security/resources/serve-referrer-policy-and-test.py
@@ -8,6 +8,7 @@ query = parse_qs(os.environ.get('QUERY_STRING', ''), keep_blank_values=True)
 destination_origin = query.get('destinationOrigin', [''])[0]
 is_testing_multipart = query.get('isTestingMultipart', [None])[0]
 value = query.get('value', [''])[0]
+id = query.get('id', [''])[0]
 
 sys.stdout.write('Referrer-Policy: {}\r\n'.format(value))
 if is_testing_multipart is not None and is_testing_multipart == '1':
@@ -17,12 +18,12 @@ if is_testing_multipart is not None and is_testing_multipart == '1':
         'Referrer-Policy: {}\r\n'
         'Content-type: text/html\r\n'
         '\r\n'
-        '<iframe src=\'{}security/resources/postReferrer.py\'></iframe>\r\n'
-        '--boundary\r\n'.format(value, destination_origin)
+        '<iframe src=\'{}security/resources/postReferrer.py?id={}\'></iframe>\r\n'
+        '--boundary\r\n'.format(value, destination_origin, id)
     )
 else:
     sys.stdout.write(
         'Content-Type: text/html\r\n\r\n'
         '\r\n'
-        '<iframe src=\'{}security/resources/postReferrer.py\'></iframe>\r\n'.format(destination_origin)
+        '<iframe src=\'{}security/resources/postReferrer.py?id={}\'></iframe>\r\n'.format(destination_origin, id)
     )
