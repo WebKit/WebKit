1ed703b0b93edf7b700ba4cfd9c302a92b118d1a
Truitt Savell <tsavell@apple.com>
2021-04-23 18:15:55 +0000

    Unreviewed, reverting r276190.
    broke a test internally.
    
    Reverted changeset:
    
    "Metal-ANGLE: Shared memory texture tests failing in iOS
    Simulator"
    https://bugs.webkit.org/show_bug.cgi?id=222685
    https://commits.webkit.org/r276190
    
    Canonical link: https://commits.webkit.org/236964@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@276507 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/ThirdParty/ANGLE/src/libANGLE/renderer/metal/mtl_utils.mm
+++ Source/ThirdParty/ANGLE/src/libANGLE/renderer/metal/mtl_utils.mm
@@ -113,10 +113,7 @@ angle::Result InitializeTextureContents(const gl::Context *context,
 
     const angle::Format &actualAngleFormat           = textureObjFormat.actualAngleFormat();
     const gl::InternalFormat &intendedInternalFormat = textureObjFormat.intendedInternalFormat();
-    bool forceGPUInitialization = false;
-#if TARGET_OS_SIMULATOR
-    forceGPUInitialization = true;
-#endif
+
     // This function is called in many places to initialize the content of a texture.
     // So it's better we do the sanity check here instead of let the callers do it themselves:
     if (!textureObjFormat.valid() || actualAngleFormat.isBlock || actualAngleFormat.depthBits > 0 ||
@@ -151,7 +148,7 @@ angle::Result InitializeTextureContents(const gl::Context *context,
     }
 
     if (texture->isCPUAccessible() && index.getType() != gl::TextureType::_2DMultisample &&
-        index.getType() != gl::TextureType::_2DMultisampleArray && forceGPUInitialization == false)
+        index.getType() != gl::TextureType::_2DMultisampleArray)
     {
         const angle::Format &dstFormat = angle::Format::Get(textureObjFormat.actualFormatId);
         const size_t dstRowPitch       = dstFormat.pixelBytes * size.width;
@@ -201,7 +198,6 @@ angle::Result InitializeTextureContents(const gl::Context *context,
             }
         }
     }  // if (texture->isCPUAccessible())
-
     else
     {
         ANGLE_TRY(InitializeTextureContentsGPU(context, texture, textureObjFormat, index,
