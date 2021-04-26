399bccb41de090736da2c50e827b7b6e5f5102a6
Katherine Cheney <katherine_cheney@apple.com>
2021-04-26 23:42:54 +0000

    ResourceLoadStatisticsDatabaseStore::aggregatedThirdPartyData() hangs for a long time https://bugs.webkit.org/show_bug.cgi?id=225063 <rdar://problem/76248557>
    Reviewed by Brent Fulgham.
    
    Source/WebKit:
    
    Unique index values speed up sqlite database queries, and prevent
    repeated entries in tables without primary keys. Its possible to
    get into a state where some tables in the ITP database do not have index
    values. We should check on open if the database has index values, and
    create them if not. This may require migrating existing data and
    removing repeated entries.
    
    * NetworkProcess/Classifier/ResourceLoadStatisticsDatabaseStore.cpp:
    Remove semi-colons in CREATE UNIQUE INDEX queries. They aren't needed
    and if we remove them we can use these constexpr values to check for
    the correct stored index later on.
    
    (WebKit::stripIndexQueryToMatchStoredValue):
    We use CREATE UNIQUE INDEX IF NOT EXISTS to avoid an error if the
    unique index already exists, but the value is stored in the database
    without the "IF NOT EXISTS" portion. In order to check for correct
    values we need to strip this out.
    
    (WebKit::expectedTableAndIndexQueries):
    (WebKit::ResourceLoadStatisticsDatabaseStore::checkForMissingTablesInSchema):
    (WebKit::ResourceLoadStatisticsDatabaseStore::currentTableAndIndexQueries):
    Return the schema and index for a given table. If the index is empty
    that means SQLite created an automatic index and we should return
    WTF::nullopt.
    
    (WebKit::ResourceLoadStatisticsDatabaseStore::needsUpdatedPrivateClickMeasurementSchema):
    (WebKit::ResourceLoadStatisticsDatabaseStore::missingUniqueIndices):
    (WebKit::ResourceLoadStatisticsDatabaseStore::missingReferenceToObservedDomains):
    (WebKit::ResourceLoadStatisticsDatabaseStore::needsUpdatedSchema):
    (WebKit::insertDistinctValuesInTableQuery):
    Since unique indices prevent repeat entries, tables without them might
    have repeated entries. We need to select only one of each entry to
    migrate to the new tables. This picks the one with the most recent
    lastUpdated time.
    
    (WebKit::ResourceLoadStatisticsDatabaseStore::migrateDataToNewTablesIfNecessary):
    (WebKit::ResourceLoadStatisticsDatabaseStore::addMissingTablesIfNecessary):
    (WebKit::ResourceLoadStatisticsDatabaseStore::createUniqueIndices):
    This fix revealed an index that is never created. I added it here.
    
    (WebKit::ResourceLoadStatisticsDatabaseStore::isCorrectSubStatisticsCount):
    * NetworkProcess/Classifier/ResourceLoadStatisticsDatabaseStore.h:
    * UIProcess/API/Cocoa/WKWebsiteDataStore.mm:
    (-[WKWebsiteDataStore _isRelationshipOnlyInDatabaseOnce:thirdParty:completionHandler:]):
    * UIProcess/API/Cocoa/WKWebsiteDataStorePrivate.h:
    Convert SPI previously only used for layout tests to be used for an
    API test where we can load a database with repeated values to ensure
    the migration is correct after my change.
    
    Tools:
    
    Test coverage. Currently API testing is the only way to test data
    migration and schema changes in the ITP database because we can load
    a pre-seeded database and check the correct values after migration.
    We should add some ability for unit testing in the future.
    
    * TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
    * TestWebKitAPI/Tests/WebKitCocoa/ResourceLoadStatistics.mm:
    (TEST):
    
    
    Canonical link: https://commits.webkit.org/237049@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@276620 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/WebKitCocoa/ResourceLoadStatistics.mm
+++ Tools/TestWebKitAPI/Tests/WebKitCocoa/ResourceLoadStatistics.mm
@@ -1408,3 +1408,50 @@ TEST(ResourceLoadStatistics, BackForwardPerPageData)
     
     TestWebKitAPI::Util::run(&doneFlag);
 }
+
+TEST(ResourceLoadStatistics, MigrateDistinctDataFromTableWithMissingIndexes)
+{
+    auto *sharedProcessPool = [WKProcessPool _sharedProcessPool];
+
+    auto defaultFileManager = [NSFileManager defaultManager];
+    auto *dataStore = [WKWebsiteDataStore defaultDataStore];
+    NSURL *itpRootURL = [[dataStore _configuration] _resourceLoadStatisticsDirectory];
+    NSURL *fileURL = [itpRootURL URLByAppendingPathComponent:@"observations.db"];
+    [defaultFileManager removeItemAtPath:itpRootURL.path error:nil];
+    EXPECT_FALSE([defaultFileManager fileExistsAtPath:itpRootURL.path]);
+
+    // Load an incorrect database schema with pre-seeded ITP data. In this case, it contains various entries of webkit.org and apple.com
+    // repeatedly stored as subframes, subresources, and unique redirects. It also has them as repeated source and destination sites for PCM.
+    // Once we add unique indexes, each entry should be migrated exactly once. Debug asserts when running the test will indicate failure.
+    [defaultFileManager createDirectoryAtURL:itpRootURL withIntermediateDirectories:YES attributes:nil error:nil];
+    NSURL *newFileURL = [[NSBundle mainBundle] URLForResource:@"resourceLoadStatisticsMissingUniqueIndex" withExtension:@"db" subdirectory:@"TestWebKitAPI.resources"];
+    EXPECT_TRUE([defaultFileManager fileExistsAtPath:newFileURL.path]);
+    [defaultFileManager copyItemAtPath:newFileURL.path toPath:fileURL.path error:nil];
+    EXPECT_TRUE([defaultFileManager fileExistsAtPath:fileURL.path]);
+
+    auto configuration = adoptNS([[WKWebViewConfiguration alloc] init]);
+    [configuration setProcessPool: sharedProcessPool];
+    configuration.get().websiteDataStore = dataStore;
+
+    // We need an active NetworkProcess to perform ResourceLoadStatistics operations.
+    auto webView = adoptNS([[WKWebView alloc] initWithFrame:NSMakeRect(0, 0, 800, 600) configuration:configuration.get()]);
+    [dataStore _setResourceLoadStatisticsEnabled:YES];
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"about:blank"]]];
+
+    __block bool doneFlag = false;
+    // Check that the webkit.org is stored as appearing under apple.com exactly once.
+    [dataStore _isRelationshipOnlyInDatabaseOnce:[NSURL URLWithString:@"http://apple.com"] thirdParty:[NSURL URLWithString:@"http://webkit.org"] completionHandler:^void(BOOL result) {
+        EXPECT_TRUE(result);
+        doneFlag = true;
+    }];
+    TestWebKitAPI::Util::run(&doneFlag);
+
+    doneFlag = false;
+    // Check that the apple.com is stored as appearing under webkit.org exactly once.
+    [dataStore _isRelationshipOnlyInDatabaseOnce:[NSURL URLWithString:@"http://webkit.org"] thirdParty:[NSURL URLWithString:@"http://apple.com"] completionHandler:^void(BOOL result) {
+        EXPECT_TRUE(result);
+        doneFlag = true;
+    }];
+
+    TestWebKitAPI::Util::run(&doneFlag);
+}
