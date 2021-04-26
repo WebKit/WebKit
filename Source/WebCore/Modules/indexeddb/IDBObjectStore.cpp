842288744ce7ec7e44b2828c2f1d601ebe50b8e2
Sihui Liu <sihui_liu@apple.com>
2021-04-26 18:42:53 +0000

    Cherry-pick r271368. rdar://problem/77160704
        Keep newly created IDBIndex objects in deleted map when IDBTransaction is aborted
        https://bugs.webkit.org/show_bug.cgi?id=220489
        <rdar://problem/70498831>
    
        Patch by Sihui Liu <sihui_liu@appe.com> on 2021-01-11
        Reviewed by Youenn Fablet.
    
        Source/WebCore:
    
        When an upgrade transaction is aborted, we move objects from m_deletedIndexes to m_referencedIndexes to revert
        the index deletion operation. When updating m_referencedIndexes, we did not check whether key already exists.
        Therefore, some indexes in m_referencedIndexes would be replaced and destroyed (since m_referencedIndexes holds
        unique pointers) when the index is still referenced by JS.
    
        Tests: storage/indexeddb/modern/abort-index-info-private.html
               storage/indexeddb/modern/abort-index-info.html
    
        * Modules/indexeddb/IDBObjectStore.cpp:
        (WebCore::IDBObjectStore::rollbackForVersionChangeAbort):
    
        LayoutTests:
    
        * storage/indexeddb/modern/abort-index-info-expected.txt: Added.
        * storage/indexeddb/modern/abort-index-info-private-expected.txt: Added.
        * storage/indexeddb/modern/abort-index-info-private.html: Added.
        * storage/indexeddb/modern/abort-index-info.html: Added.
        * storage/indexeddb/modern/resources/abort-index-info.js: Added.
        (prepareDatabase):
        (versionChangeSuccessCallback.secondRequest.onerror):
        (versionChangeSuccessCallback):
        (secondUpgradeNeeded):
        (checkState):
    
        Canonical link: https://commits.webkit.org/232932@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@271368 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/207701.555@safari-607-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-607-branch@276605 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/indexeddb/IDBObjectStore.cpp
+++ Source/WebCore/Modules/indexeddb/IDBObjectStore.cpp
@@ -650,10 +650,14 @@ void IDBObjectStore::rollbackForVersionChangeAbort()
     Locker<Lock> locker(m_referencedIndexLock);
 
     Vector<uint64_t> identifiersToRemove;
+    Vector<std::unique_ptr<IDBIndex>> indexesToDelete;
     for (auto& iterator : m_deletedIndexes) {
         if (m_info.hasIndex(iterator.key)) {
             auto name = iterator.value->info().name();
-            m_referencedIndexes.set(name, WTFMove(iterator.value));
+            auto result = m_referencedIndexes.add(name, nullptr);
+            if (!result.isNewEntry)
+                indexesToDelete.append(std::exchange(result.iterator->value, nullptr));
+            result.iterator->value = std::exchange(iterator.value, nullptr);
             identifiersToRemove.append(iterator.key);
         }
     }
@@ -663,6 +667,12 @@ void IDBObjectStore::rollbackForVersionChangeAbort()
 
     for (auto& index : m_referencedIndexes.values())
         index->rollbackInfoForVersionChangeAbort();
+
+    for (auto& index : indexesToDelete) {
+        index->rollbackInfoForVersionChangeAbort();
+        auto indexIdentifier = index->info().identifier();
+        m_deletedIndexes.set(indexIdentifier, std::exchange(index, nullptr));
+    }
 }
 
 void IDBObjectStore::visitReferencedIndexes(SlotVisitor& visitor) const
