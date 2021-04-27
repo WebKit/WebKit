197db3099e71c09f3c6092e83cb89ba1f592d0ca
Don Olmstead <don.olmstead@sony.com>
2021-04-27 17:18:16 +0000

    [CMake] Don't use FORWARDING_HEADERS_DIR for GTK WebKit headers https://bugs.webkit.org/show_bug.cgi?id=225006
    Reviewed by Michael Catanzaro.
    
    .:
    
    Create CMake variables WebKit2Gtk_FRAMEWORK_HEADERS_DIR and WebKit2Gtk_DERIVED_SOURCES_DIR
    to represent where the WebKit2 GTK headers and derived sources will reside. The names and
    locations set then follow along with the conventions used for the other frameworks.
    
    * Source/cmake/OptionsGTK.cmake:
    
    Source/WebInspectorUI:
    
    Use WebKit2Gtk_FRAMEWORK_HEADERS_DIR and WebKit2Gtk_DERIVED_SOURCES_DIR for WebKit2 GTK
    headers instead of FORWARDING_HEADERS_DIR and DERIVED_SOURCES_DIR.
    
    * PlatformGTK.cmake:
    
    Source/WebKit:
    
    Use WebKit2Gtk_FRAMEWORK_HEADERS_DIR and WebKit2Gtk_DERIVED_SOURCES_DIR for WebKit2 GTK
    headers instead of FORWARDING_HEADERS_DIR and DERIVED_SOURCES_DIR.
    
    * PlatformGTK.cmake:
    
    Tools:
    
    Use WebKit2Gtk_FRAMEWORK_HEADERS_DIR and WebKit2Gtk_DERIVED_SOURCES_DIR for WebKit2 GTK
    headers instead of FORWARDING_HEADERS_DIR and DERIVED_SOURCES_DIR.
    
    * MiniBrowser/gtk/CMakeLists.txt:
    * TestWebKitAPI/glib/PlatformGTK.cmake:
    
    
    Canonical link: https://commits.webkit.org/237072@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@276645 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/PlatformGTK.cmake
+++ Source/WebInspectorUI/PlatformGTK.cmake
@@ -1,3 +1,3 @@
-set(WebInspectorUI_RESOURCES_DIR "${DERIVED_SOURCES_WEBKIT2GTK_DIR}/InspectorResources")
+set(WebInspectorUI_RESOURCES_DIR "${WebKit2Gtk_DERIVED_SOURCES_DIR}/InspectorResources")
 set(WebInspectorUI_LOCALIZED_STRINGS_DIR "${WebInspectorUI_RESOURCES_DIR}/WebInspectorUI/Localizations/en.lproj")
 
