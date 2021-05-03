6f09df8b02ceb03546ef24a12a84dc5b5ebefdb7
Jonathan Bedard <jbedard@apple.com>
2021-05-03 23:41:50 +0000

    [webkitpy] Support pickling platforminfo https://bugs.webkit.org/show_bug.cgi?id=225230 <rdar://problem/77384913>
    Rubber-stamped by Aakash Jain.
    
    * Scripts/webkitpy/common/system/platforminfo.py:
    (PlatformInfo.__init__): Define default arguments.
    (PlatformInfo.display_name): Use default platform module if platform_module undefined.
    (PlatformInfo._win_version_str): Ditto.
    * Scripts/webkitpy/common/system/platforminfo_unittest.py:
    (TestPlatformInfo.test_real_code):
    * Scripts/webkitpy/common/system/systemhost.py:
    (SystemHost.__init__): Use default platforminfo sys and platform modules.
    
    
    Canonical link: https://commits.webkit.org/237271@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@276935 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/common/system/platforminfo.py
+++ Tools/Scripts/webkitpy/common/system/platforminfo.py
@@ -37,7 +37,7 @@ from webkitcorepy import Version
 
 from webkitpy.common.memoized import memoized
 from webkitpy.common.version_name_map import PUBLIC_TABLE, INTERNAL_TABLE, VersionNameMap
-from webkitpy.common.system.executive import ScriptError
+from webkitpy.common.system.executive import Executive, ScriptError
 from webkitpy.port.config import apple_additions
 
 
@@ -59,8 +59,9 @@ class PlatformInfo(object):
     newer than one known to the code.
     """
 
-    def __init__(self, sys_module, platform_module, executive):
-        self._executive = executive
+    def __init__(self, sys_module=None, platform_module=None, executive=None):
+        sys_module = sys_module or sys
+        self._executive = executive or Executive()
         self._platform_module = platform_module
         self.os_name = self._determine_os_name(sys_module.platform)
         self.os_version = None
@@ -77,7 +78,7 @@ class PlatformInfo(object):
             self.os_version = self._win_version()
         else:
             # Most other platforms (namely iOS) return conforming version strings.
-            version = re.search(r'\d+.\d+(.\d+)?', platform_module.release())
+            version = re.search(r'\d+.\d+(.\d+)?', (platform_module or platform).release())
             if version:
                 self.os_version = Version.from_string(version.group(0))
             else:
@@ -134,12 +135,12 @@ class PlatformInfo(object):
     def display_name(self):
         # platform.platform() returns Darwin information for Mac, which is just confusing.
         if self.is_mac():
-            return "Mac OS X %s" % self._platform_module.mac_ver()[0]
+            return "Mac OS X %s" % (self._platform_module or platform).mac_ver()[0]
 
         # Returns strings like:
         # Linux-2.6.18-194.3.1.el5-i686-with-redhat-5.5-Final
         # Windows-2008ServerR2-6.1.7600
-        return self._platform_module.platform()
+        return (self._platform_module or platform).platform()
 
     def os_version_name(self, table=None):
         if not self.os_version:
@@ -246,7 +247,7 @@ class PlatformInfo(object):
         return Version.from_iterable(match_object.groups())
 
     def _win_version_str(self):
-        version = self._platform_module.win32_ver()[1]
+        version = (self._platform_module or platform).win32_ver()[1]
         if version:
             return version
         # Note that this should only ever be called on windows, so this should always work.
