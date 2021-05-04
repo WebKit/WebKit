de0c499f84f9dc50514e10d4ddf7150337e6569c
Diego Pino Garcia <dpino@igalia.com>
2021-05-04 08:00:03 +0000

    Unreviewed, fix GTK and WPE builds after r276947
    * Headers.cmake: Add missing header 'Modules/highlight/HighlightVisibility.h'.
    
    Canonical link: https://commits.webkit.org/237284@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@276951 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Headers.cmake
+++ Source/WebCore/Headers.cmake
@@ -51,6 +51,7 @@ set(WebCore_PRIVATE_FRAMEWORK_HEADERS
     Modules/highlight/AppHighlight.h
     Modules/highlight/Highlight.h
     Modules/highlight/HighlightRegister.h
+    Modules/highlight/HighlightVisibility.h
 
     Modules/indexeddb/IDBActiveDOMObject.h
     Modules/indexeddb/IDBDatabaseIdentifier.h
