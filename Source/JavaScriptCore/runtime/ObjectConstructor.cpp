800d33d28d0b267466a3f6daec5fa5056e72348d
Mark Lam <mark.lam@apple.com>
2021-05-06 15:47:46 +0000

    Fix missing exception check in objectConstructorGetOwnPropertyDescriptors(). https://bugs.webkit.org/show_bug.cgi?id=225413 rdar://77551530
    Reviewed by Michael Saboff.
    
    Need to handle TerminationException.
    
    * runtime/ObjectConstructor.cpp:
    (JSC::objectConstructorGetOwnPropertyDescriptors):
    
    
    Canonical link: https://commits.webkit.org/237394@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@277092 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/ObjectConstructor.cpp
+++ Source/JavaScriptCore/runtime/ObjectConstructor.cpp
@@ -1,6 +1,6 @@
 /*
  *  Copyright (C) 1999-2000 Harri Porten (porten@kde.org)
- *  Copyright (C) 2008-2019 Apple Inc. All rights reserved.
+ *  Copyright (C) 2008-2021 Apple Inc. All rights reserved.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -208,7 +208,8 @@ JSValue objectConstructorGetOwnPropertyDescriptors(JSGlobalObject* globalObject,
 
         PutPropertySlot slot(descriptors);
         descriptors->putOwnDataPropertyMayBeIndex(globalObject, propertyName, fromDescriptor, slot);
-        scope.assertNoException();
+        scope.assertNoExceptionExceptTermination();
+        RETURN_IF_EXCEPTION(scope, { });
     }
 
     return descriptors;
