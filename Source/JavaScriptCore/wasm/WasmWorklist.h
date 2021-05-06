1e80b768326cedd84da8225ecf6cb5f320380058
Filip Pizlo <fpizlo@apple.com>
2021-05-06 20:55:29 +0000

    Make some things easier to dataLog in wasm https://bugs.webkit.org/show_bug.cgi?id=225472
    Reviewed by Yusuke Suzuki.
    
    * wasm/WasmMemoryMode.cpp:
    (WTF::printInternal):
    * wasm/WasmMemoryMode.h:
    * wasm/WasmWorklist.cpp:
    (JSC::Wasm::Worklist::dump const):
    * wasm/WasmWorklist.h:
    
    
    
    Canonical link: https://commits.webkit.org/237416@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@277118 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/wasm/WasmWorklist.h
+++ Source/JavaScriptCore/wasm/WasmWorklist.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Apple Inc. All rights reserved.
+ * Copyright (C) 2017-2021 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,6 +30,7 @@
 #include <queue>
 
 #include <wtf/AutomaticThread.h>
+#include <wtf/PrintStream.h>
 #include <wtf/PriorityQueue.h>
 #include <wtf/Vector.h>
 
@@ -58,6 +59,8 @@ public:
         Preparation
     };
     const char* priorityString(Priority);
+
+    void dump(PrintStream&) const;
 
 private:
     class Thread;
