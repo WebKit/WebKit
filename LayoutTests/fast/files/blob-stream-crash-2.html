WebKit/WebKit

commit a37d972307781e91d53c4cfd7bfb2f6a251b560f

Implement Blob.stream https://bugs.webkit.org/show_bug.cgi?id=217457
Reviewed by Youenn Fablet.

LayoutTests/imported/w3c:

* web-platform-tests/FileAPI/blob/Blob-stream.any-expected.txt:
* web-platform-tests/FileAPI/blob/Blob-stream.any.worker-expected.txt:
* web-platform-tests/FileAPI/idlharness-expected.txt:
* web-platform-tests/FileAPI/idlharness.worker-expected.txt:

Source/WebCore:

It's implemented in Chrome and Firefox.

* Modules/async-clipboard/ClipboardItemBindingsDataSource.h:
* bindings/js/ReadableStream.h:
(WebCore::toJSNewlyCreated):
* bindings/scripts/CodeGeneratorJS.pm:
(AddToIncludesForIDLType):
* fileapi/Blob.cpp:
(WebCore::Blob::stream):
* fileapi/Blob.h:
* fileapi/Blob.idl:
* fileapi/FileReaderLoader.cpp:
(WebCore::FileReaderLoader::FileReaderLoader):
(WebCore::FileReaderLoader::takeRawData):
* fileapi/FileReaderLoader.h:
* fileapi/FileReaderLoaderClient.h:

LayoutTests:

* fast/files/blob-stream-error-expected.txt: Added.
* fast/files/blob-stream-error.html: Added.
* fast/files/blob-stream-frame-expected.txt: Added.
* fast/files/blob-stream-frame.html: Added.
* fast/files/resources/blob-stream-iframe.html: Added.



Canonical link: https://commits.webkit.org/230265@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@268228 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/fileapi/Blob.cpp
@@ -37,6 +37,8 @@
 #include "BlobURL.h"
 #include "File.h"
 #include "JSDOMPromiseDeferred.h"
+#include "ReadableStream.h"
+#include "ReadableStreamSource.h"
 #include "ScriptExecutionContext.h"
 #include "SharedBuffer.h"
 #include "ThreadableBlobRegistry.h"
@@ -234,6 +236,48 @@ void Blob::arrayBuffer(ScriptExecutionContext& context, Ref<DeferredPromise>&& p
         }
         promise->resolve<IDLArrayBuffer>(*arrayBuffer);
     });
+}
+
+ExceptionOr<Ref<ReadableStream>> Blob::stream(ScriptExecutionContext& scriptExecutionContext)
+{
+    class BlobStreamSource : public FileReaderLoaderClient, public ReadableStreamSource {
+    public:
+        BlobStreamSource(ScriptExecutionContext& scriptExecutionContext, Blob& blob)
+            : m_loader(makeUniqueRef<FileReaderLoader>(FileReaderLoader::ReadType::ReadAsArrayBuffer, this))
+        {
+            m_loader->start(&scriptExecutionContext, blob);
+        }
+
+    private:
+        // ReadableStreamSource
+        void setActive() final { }
+        void setInactive() final { }
+        void doStart() final { }
+        void doPull() final { }
+        void doCancel() final
+        {
+            m_loader->cancel();
+        }
+
+        // FileReaderLoaderClient
+        void didStartLoading() final { }
+        void didReceiveData() final
+        {
+            controller().enqueue(m_loader->takeRawData());
+        }
+        void didFinishLoading() final
+        {
+            controller().close();
+        }
+        void didFail(ExceptionCode exception) final
+        {
+            controller().error(Exception { exception });
+        }
+
+        UniqueRef<FileReaderLoader> m_loader;
+    };
+
+    return ReadableStream::create(*scriptExecutionContext.execState(), adoptRef(*new BlobStreamSource(scriptExecutionContext, *this)));
 }
 
 #if ASSERT_ENABLED

Source/WebCore/fileapi/FileReaderLoader.cpp
@@ -56,7 +56,7 @@ const int defaultBufferLength = 32768;
 
 FileReaderLoader::FileReaderLoader(ReadType readType, FileReaderLoaderClient* client)
     : m_readType(readType)
-    , m_client(client)
+    , m_client(makeWeakPtr(client))
     , m_isRawDataConverted(false)
     , m_stringResult(emptyString())
     , m_variableLength(false)
@@ -277,6 +277,11 @@ RefPtr<ArrayBuffer> FileReaderLoader::arrayBufferResult() const
 
     // Otherwise, return a copy.
     return ArrayBuffer::create(*m_rawData);
+}
+
+RefPtr<JSC::ArrayBuffer> FileReaderLoader::takeRawData()
+{
+    return std::exchange(m_rawData, nullptr);
 }
 
 String FileReaderLoader::stringResult()

Source/WebCore/fileapi/FileReaderLoader.h
@@ -37,6 +37,7 @@
 #include "ThreadableLoaderClient.h"
 #include <wtf/Forward.h>
 #include <wtf/Optional.h>
+#include <wtf/WeakPtr.h>
 #include <wtf/text/WTFString.h>
 
 namespace JSC {
@@ -78,6 +79,7 @@ public:
     WEBCORE_EXPORT RefPtr<JSC::ArrayBuffer> arrayBufferResult() const;
     unsigned bytesLoaded() const { return m_bytesLoaded; }
     unsigned totalBytes() const { return m_totalBytes; }
+    RefPtr<JSC::ArrayBuffer> takeRawData();
     Optional<ExceptionCode> errorCode() const { return m_errorCode; }
 
     void setEncoding(const String&);
@@ -98,7 +100,7 @@ private:
     static ExceptionCode toErrorCode(BlobResourceHandle::Error);
 
     ReadType m_readType;
-    FileReaderLoaderClient* m_client;
+    WeakPtr<FileReaderLoaderClient> m_client;
     TextEncoding m_encoding;
     String m_dataType;
 

LayoutTests/fast/files/blob-stream-error-expected.txt
@@ -1,0 +1,2 @@
+ALERT: PASS
+

LayoutTests/fast/files/blob-stream-error.html
@@ -1,0 +1,35 @@
+<script>
+    if (window.testRunner) {
+        testRunner.waitUntilDone();
+        testRunner.dumpAsText();
+    }
+    async function readUntilError(stream) {
+        const reader = stream.getReader();
+        try {
+            let read_value = await reader.read();
+            if (window.testRunner && window.testRunner.terminateNetworkProcess) {
+                alert("FAIL - successfully read from stream");
+            } else {
+                // WebKitLegacy can't terminate the network process.
+                alert("PASS");
+            }
+            if (window.testRunner) {
+                testRunner.notifyDone();
+            }
+        } catch (e) {
+            if (window.testRunner && window.testRunner.terminateNetworkProcess) {
+                alert("PASS");
+            } else {
+                alert("FAIL - error without terminating network process");
+            }
+            if (window.testRunner) {
+                testRunner.notifyDone();
+            }
+        }
+    }
+    var b = new Blob([1, 2, 3]);
+    if (window.testRunner && window.testRunner.terminateNetworkProcess) {
+        testRunner.terminateNetworkProcess();
+    }
+    readUntilError(b.stream());
+</script>

LayoutTests/fast/files/blob-stream-frame-expected.txt
@@ -1,0 +1,2 @@
+ALERT: PASS
+

LayoutTests/fast/files/blob-stream-frame.html
@@ -1,0 +1,15 @@
+<script>
+    if (window.testRunner) {
+        testRunner.dumpAsText();
+        testRunner.waitUntilDone();
+    }
+    window.addEventListener("message", function(event) {
+        event.data.stream().getReader().read().then(function({ done, value }) {
+            alert(String.fromCharCode(value[0]) == "7" ? "PASS" : "FAIL");
+            if (window.testRunner) {
+                testRunner.notifyDone();
+            }
+        });
+    });
+</script>
+<iframe src="resources/blob-stream-iframe.html"></iframe>

LayoutTests/fast/files/resources/blob-stream-iframe.html
@@ -1,0 +1,3 @@
+<script>
+    parent.postMessage(new Blob([7]), "*");
+</script>

commit a1ab0f4b8155266150d5698b3c5ce661d15ff203

Cherry-pick r271669. rdar://problem/77580999
    Fix nullptr dereference introduced in r268228
    https://bugs.webkit.org/show_bug.cgi?id=220776

    Patch by Alex Christensen <achristensen@webkit.org> on 2021-01-20
    Reviewed by Chris Dumez.

    Source/WebCore:

    Instead of taking data from the FileReaderLoader, which it assumes it still has later,
    only give the bytes to JavaScript that have been received since last time we called didReceiveData.
    This makes blob.stream correct and not crash.

    Test: fast/files/blob-stream-crash-2.html

    * fileapi/Blob.cpp:
    (WebCore::Blob::stream):
    * fileapi/FileReaderLoader.cpp:
    (WebCore::FileReaderLoader::takeRawData): Deleted.
    * fileapi/FileReaderLoader.h:
    (WebCore::FileReaderLoader::totalBytes const):

    LayoutTests:

    * fast/files/blob-stream-crash-2-expected.txt: Added.
    * fast/files/blob-stream-crash-2.html: Added.

    Canonical link: https://commits.webkit.org/233184@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@271669 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Canonical link: https://commits.webkit.org/232923.481@safari-611-branch
git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-611-branch@277184 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/fileapi/Blob.cpp
@@ -266,7 +266,17 @@ ExceptionOr<Ref<ReadableStream>> Blob::stream(ScriptExecutionContext& scriptExec
         void didStartLoading() final { }
         void didReceiveData() final
         {
-            controller().enqueue(m_loader->takeRawData());
+            auto result = m_loader->arrayBufferResult();
+            if (!result)
+                return;
+
+            if (m_loader->isCompleted() && !m_bytesRead)
+                controller().enqueue(WTFMove(result));
+            else {
+                auto bytesLoaded = m_loader->bytesLoaded();
+                controller().enqueue(result->slice(m_bytesRead, bytesLoaded));
+                m_bytesRead = bytesLoaded;
+            }
         }
         void didFinishLoading() final
         {
@@ -278,6 +288,7 @@ ExceptionOr<Ref<ReadableStream>> Blob::stream(ScriptExecutionContext& scriptExec
         }
 
         UniqueRef<FileReaderLoader> m_loader;
+        size_t m_bytesRead { 0 };
     };
 
     auto* globalObject = scriptExecutionContext.globalObject();

Source/WebCore/fileapi/FileReaderLoader.cpp
@@ -279,11 +279,6 @@ RefPtr<ArrayBuffer> FileReaderLoader::arrayBufferResult() const
     return ArrayBuffer::create(*m_rawData);
 }
 
-RefPtr<JSC::ArrayBuffer> FileReaderLoader::takeRawData()
-{
-    return std::exchange(m_rawData, nullptr);
-}
-
 String FileReaderLoader::stringResult()
 {
     ASSERT(m_readType != ReadAsArrayBuffer && m_readType != ReadAsBlob);

Source/WebCore/fileapi/FileReaderLoader.h
@@ -79,7 +79,6 @@ public:
     WEBCORE_EXPORT RefPtr<JSC::ArrayBuffer> arrayBufferResult() const;
     unsigned bytesLoaded() const { return m_bytesLoaded; }
     unsigned totalBytes() const { return m_totalBytes; }
-    RefPtr<JSC::ArrayBuffer> takeRawData();
     Optional<ExceptionCode> errorCode() const { return m_errorCode; }
 
     void setEncoding(const String&);
@@ -87,14 +86,14 @@ public:
 
     const URL& url() { return m_urlForReading; }
 
+    bool isCompleted() const;
+
 private:
     void terminate();
     void cleanup();
     void failed(ExceptionCode);
     void convertToText();
     void convertToDataURL();
-
-    bool isCompleted() const;
 
     static ExceptionCode httpStatusCodeToErrorCode(int);
     static ExceptionCode toErrorCode(BlobResourceHandle::Error);

LayoutTests/fast/files/blob-stream-crash-2-expected.txt
@@ -1,0 +1,5 @@
+ALERT: RECEIVED BYTE 'a'
+ALERT: RECEIVED BYTE 'b'
+ALERT: RECEIVED BYTE 'c'
+ALERT: RECEIVED BYTE 'd'
+

LayoutTests/fast/files/blob-stream-crash-2.html
@@ -1,0 +1,42 @@
+<script>
+
+    var bytesRead = 0;
+
+    function recursiveRead(reader) {
+        reader.read().then(function({ done, value }) {
+            for (var i = 0; i < value.length; i++) {
+                alert("RECEIVED BYTE '" + String.fromCharCode(value[i]) + "'");
+                bytesRead = bytesRead + 1;
+            }
+            if (bytesRead == 4) {
+                if (window.testRunner) {
+                    testRunner.notifyDone();
+                }
+            } else {
+                recursiveRead(reader);
+            }
+        });
+    }
+    
+    function runTest() {
+        let fileBits = [
+            'a',
+            new File([], 'x'),
+            'b',
+            new File([], 'x'),
+            'c',
+            new File([], 'x'),
+            'd'
+        ];
+        let file = new File(fileBits, 'y');
+        recursiveRead(file.stream().getReader());
+    }
+
+    if (window.testRunner) {
+        testRunner.dumpAsText();
+        testRunner.waitUntilDone();
+    }
+
+</script>
+<body onload="runTest()">
+</body>