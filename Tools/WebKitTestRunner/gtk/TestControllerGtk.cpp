ca0f8ec832c3cf82c8a4a9032da8a750198878c0
Diego Pino Garcia <dpino@igalia.com>
2021-05-11 10:39:39 +0000

    [GTK] compositing/overflow/dynamic-composited-scrolling-status.html is failing https://bugs.webkit.org/show_bug.cgi?id=225644
    Reviewed by Carlos Garcia Campos.
    
    The test started failing in r277258, which makes WebKitGTK to use
    always async scrolling in AC mode. After this change, WebKitTestRunner
    should always enable AsyncOverflowScrolling.
    
    * WebKitTestRunner/gtk/TestControllerGtk.cpp:
    (WTR::TestController::platformSpecificFeatureDefaultsForTest const): Enable AsyncOverflowScrolling.
    
    
    Canonical link: https://commits.webkit.org/237578@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@277319 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/WebKitTestRunner/gtk/TestControllerGtk.cpp
+++ Tools/WebKitTestRunner/gtk/TestControllerGtk.cpp
@@ -155,7 +155,9 @@ bool TestController::platformResetStateToConsistentValues(const TestOptions&)
 
 TestFeatures TestController::platformSpecificFeatureDefaultsForTest(const TestCommand&) const
 {
-    return { };
+    TestFeatures features;
+    features.boolWebPreferenceFeatures.insert({ "AsyncOverflowScrollingEnabled", true });
+    return features;
 }
 
 } // namespace WTR
