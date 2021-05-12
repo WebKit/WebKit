c079b650382176b9f51a8c2d69c7a3ec10f9a8b1
Alex Christensen <achristensen@webkit.org>
2021-05-12 00:05:49 +0000

    Remove xpc_connection_kill https://bugs.webkit.org/show_bug.cgi?id=225602
    Patch by Alex Christensen <achristensen@webkit.org> on 2021-05-11
    Reviewed by Alexey Proskuryakov.
    
    Source/WebKit:
    
    * UIProcess/Launcher/mac/ProcessLauncherMac.mm:
    (WebKit::terminateWithReason):
    
    Source/WTF:
    
    * wtf/PlatformHave.h:
    
    Canonical link: https://commits.webkit.org/237602@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@277344 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/Launcher/mac/ProcessLauncherMac.mm
+++ Source/WebKit/UIProcess/Launcher/mac/ProcessLauncherMac.mm
@@ -343,15 +343,15 @@ void ProcessLauncher::platformInvalidate()
 
 void terminateWithReason(xpc_connection_t connection, ReasonCode reasonCode, const char* reason)
 {
-#if PLATFORM(MACCATALYST) || PLATFORM(IOS)
-    // FIXME: Remove this once rdar://76601307 and rdar://76652763 are fixed.
+#if HAVE(SYSTEM_SANDBOXES_ALLOWING_TERMINATE_WITH_REASON)
+    terminate_with_reason(xpc_connection_get_pid(connection), OS_REASON_WEBKIT, static_cast<uint64_t>(reasonCode), reason, OS_REASON_FLAG_NO_CRASH_REPORT);
+#else
+    // We are keeping this code on iOS 14.5 because rdar://76601307 and rdar://76652763 are not fixed there.
     ALLOW_DEPRECATED_DECLARATIONS_BEGIN
     xpc_connection_kill(connection, SIGKILL);
     ALLOW_DEPRECATED_DECLARATIONS_END
     UNUSED_PARAM(reasonCode);
     UNUSED_PARAM(reason);
-#else
-    terminate_with_reason(xpc_connection_get_pid(connection), OS_REASON_WEBKIT, static_cast<uint64_t>(reasonCode), reason, OS_REASON_FLAG_NO_CRASH_REPORT);
 #endif
 }
 
