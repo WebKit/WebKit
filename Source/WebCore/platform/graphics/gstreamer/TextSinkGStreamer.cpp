087b6dc575e7879703c9162df4104150046b2ba9
eocanha@igalia.com <eocanha@igalia.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2021-05-13 13:54:43 +0000

    [GStreamer] media/track/in-band/track-in-band-srt-mkv-kind.html is a flaky crash https://bugs.webkit.org/show_bug.cgi?id=225697
    Source/WebCore:
    
    This is a tentative fix for the flaky crash (I can't reproduce it locally after 10000
    repetitions). It makes sure that mediaPlayerPrivate and any parameter passed to
    handleTextSample() can either survive or detected to be invalid when the lambda is ran in
    the main thread.
    
    Reviewed by Alicia Boya Garcia.
    
    Covered by existing tests.
    
    * platform/graphics/gstreamer/TextSinkGStreamer.cpp:
    (webkitTextSinkHandleSample): Protect mediaPlayerPrivate and streadId and check if the mediaPlayerPrivate WeakRef is still alive.
    
    LayoutTests:
    
    Reviewed by Alicia Boya Garcia.
    
    * platform/glib/TestExpectations: Unskipped test.
    
    
    Canonical link: https://commits.webkit.org/237680@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@277432 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/gstreamer/TextSinkGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/TextSinkGStreamer.cpp
@@ -61,10 +61,13 @@ static void webkitTextSinkHandleSample(WebKitTextSink* self, GRefPtr<GstSample>&
     }
 
     if (priv->streamId) {
-        // As the mediaPlayerPrivate WeakPtr is constructed from the main thread, we have to use it
-        // from the main thread as well.
-        callOnMainThread([priv, sample = WTFMove(sample)] {
-            priv->mediaPlayerPrivate->handleTextSample(sample.get(), priv->streamId);
+        // Player private methods that interact with WebCore must run from the main thread. Things can be destroyed before that
+        // code runs, including the text sink and priv, so pass everything in a safe way.
+        callOnMainThread([mediaPlayerPrivate = WeakPtr<MediaPlayerPrivateGStreamer>(priv->mediaPlayerPrivate),
+            streamId = priv->streamId, sample = WTFMove(sample)] {
+            if (!mediaPlayerPrivate)
+                return;
+            mediaPlayerPrivate->handleTextSample(sample.get(), streamId);
         });
         return;
     }
