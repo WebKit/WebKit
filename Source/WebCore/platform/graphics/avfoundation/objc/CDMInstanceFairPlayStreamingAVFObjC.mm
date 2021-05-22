WebKit/WebKit

commit e3e9cf6d20cb609b86e877cbd0e1ccf1037a3bee

Update Base64 encoding/decoding to match more modern WebKit conventions https://bugs.webkit.org/show_bug.cgi?id=225920
Reviewed by Chris Dumez.

Source/WebCore:

- Updates to use new signatures for base64 encoding/decoding including
  utilizing the string type adaptor and makeString.
- Replace out parameter based getDefaultWebCryptoMasterKey() with
  Optional<Vector<uint8_t>> defaultWebCryptoMasterKey().
- Update synchronous loading code path to use uint8_t* consistently for
  data buffer representation.
- Replace out parameters in quotedPrintableEncode/quotedPrintableDecode
  with Optional.

* Modules/encryptedmedia/InitDataRegistry.cpp:
(WebCore::extractKeyIDsKeyids):
Update to use new base64URLDecode.

(WebCore::sanitizeKeyids):
Update to use new base64URLEncodeToString()

* Modules/encryptedmedia/legacy/LegacyCDMSessionClearKey.cpp:
(WebCore::CDMSessionClearKey::update):
Update to use new base64Decode().

* Modules/plugins/QuickTimePluginReplacement.mm:
(WebCore::jsValueWithDataInContext):
(WebCore::jsValueWithAVMetadataItemInContext):
Update to use new base64EncodeToString(). Replace string concatentation
using operator+ with makeString().

* Modules/webauthn/AuthenticatorCoordinator.cpp:
(WebCore::AuthenticatorCoordinatorInternal::produceClientDataJson):
Update to use new base64URLEncodeToString().

* Modules/webauthn/PublicKeyCredential.cpp:
(WebCore::PublicKeyCredential::PublicKeyCredential):
Update to use new base64URLEncodeToString().

* Modules/websockets/WebSocketHandshake.cpp:
(WebCore::generateSecWebSocketKey):
(WebCore::WebSocketHandshake::getExpectedWebSocketAccept):
Update to use new base64URLEncodeToString().

* crypto/SerializedCryptoKeyWrap.h:
Change from outparameter based getDefaultWebCryptoMasterKey() to new
Optional<Vector<uint8_t>> defaultWebCryptoMasterKey().

* crypto/gcrypt/CryptoKeyECGCrypt.cpp:
(WebCore::CryptoKeyEC::platformAddFieldElements const):
Update to use new base64URLEncodeToString().

Source/WebCore/platform/graphics/avfoundation/objc/CDMInstanceFairPlayStreamingAVFObjC.mm
@@ -715,7 +715,7 @@ void CDMInstanceSessionFairPlayStreamingAVFObjC::requestLicense(LicenseType lice
         identifier = adoptNS([[NSString alloc] initWithData:initData->createNSData().get() encoding:NSUTF8StringEncoding]);
 #if HAVE(FAIRPLAYSTREAMING_CENC_INITDATA)
     else if (initDataType == InitDataRegistry::cencName()) {
-        String psshString = base64Encode(initData->data(), initData->size());
+        auto psshString = base64EncodeToString(initData->data(), initData->size());
         initializationData = [NSJSONSerialization dataWithJSONObject:@{ @"pssh": (NSString*)psshString } options:NSJSONWritingPrettyPrinted error:nil];
     }
 #endif
@@ -850,11 +850,11 @@ void CDMInstanceSessionFairPlayStreamingAVFObjC::updateLicense(const String&, Li
             if (!keyIDString)
                 return false;
 
-            Vector<uint8_t> keyIDVector;
-            if (!base64Decode(keyIDString, keyIDVector))
+            auto keyIDVector = base64Decode(keyIDString);
+            if (keyIDVector)
                 return false;
 
-            auto keyID = SharedBuffer::create(WTFMove(keyIDVector));
+            auto keyID = SharedBuffer::create(WTFMove(*keyIDVector));
             auto foundIndex = m_currentRequest.value().requests.findMatching([&] (auto& request) {
                 auto keyIDs = keyIDsForRequest(request.get());
                 return keyIDs.findMatching([&](const Ref<SharedBuffer>& id) {
@@ -885,10 +885,10 @@ void CDMInstanceSessionFairPlayStreamingAVFObjC::updateLicense(const String&, Li
                 auto payloadString = payloadFindResults->value->asString();
                 if (!payloadString)
                     return false;
-                Vector<uint8_t> payloadVector;
-                if (!base64Decode(payloadString, payloadVector))
+                auto payloadVector = base64Decode(payloadString);
+                if (!payloadVector)
                     return false;
-                auto payloadData = SharedBuffer::create(WTFMove(payloadVector));
+                auto payloadData = SharedBuffer::create(WTFMove(*payloadVector));
                 [request processContentKeyResponse:[PAL::getAVContentKeyResponseClass() contentKeyResponseWithFairPlayStreamingKeyResponseData:payloadData->createNSData().get()]];
             }
             return true;
@@ -1195,8 +1195,8 @@ void CDMInstanceSessionFairPlayStreamingAVFObjC::didProvideRequests(Vector<Retai
             auto entry = JSON::Object::create();
             auto& keyID = requestData.first;
             auto& payload = requestData.second;
-            entry->setString("keyID", base64Encode(keyID->data(), keyID->size()));
-            entry->setString("payload", base64Encode(payload.get().bytes, payload.get().length));
+            entry->setString("keyID", base64EncodeToString(keyID->data(), keyID->size()));
+            entry->setString("payload", base64EncodeToString(payload.get().bytes, payload.get().length));
             requestJSON->pushObject(WTFMove(entry));
         }
         auto requestBuffer = utf8Buffer(requestJSON->toJSONString());

commit 43c5f319a79b526b51d2c8a52f877dab31b6bf22

All Netflix playback fails after r277740 https://bugs.webkit.org/show_bug.cgi?id=226120 <rdar://problem/78333782>
Reviewed by Eric Carlson.

* platform/graphics/avfoundation/objc/CDMInstanceFairPlayStreamingAVFObjC.mm:
(WebCore::CDMInstanceSessionFairPlayStreamingAVFObjC::updateLicense):
Add missing !.


Canonical link: https://commits.webkit.org/238038@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@277905 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/avfoundation/objc/CDMInstanceFairPlayStreamingAVFObjC.mm
@@ -851,7 +851,7 @@ void CDMInstanceSessionFairPlayStreamingAVFObjC::updateLicense(const String&, Li
                 return false;
 
             auto keyIDVector = base64Decode(keyIDString);
-            if (keyIDVector)
+            if (!keyIDVector)
                 return false;
 
             auto keyID = SharedBuffer::create(WTFMove(*keyIDVector));