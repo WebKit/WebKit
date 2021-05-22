5946249328534696e42445d9f53ebc269c877ded
Devin Rousso <drousso@apple.com>
2021-05-22 14:16:29 +0000

    ASSERTION FAILED: Component at index 1 is 1.000000 and is greater than the allowed maximum 1.000000 https://bugs.webkit.org/show_bug.cgi?id=226118 <rdar://problem/78333590>
    Reviewed by Sam Weinig.
    
    Covered by SampledPageTopColor tests.
    
    * platform/graphics/cg/ColorCG.cpp:
    (WebCore::roundAndClampToSRGBALossy):
    Use `makeFromComponentsClamping` to actually clamp the component values instead of assuming
    that the `CGColorRef` will be sRGB (though it still assumes an RGB color space/format).
    
    
    Canonical link: https://commits.webkit.org/238052@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@277919 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/cg/ColorCG.cpp
+++ Source/WebCore/platform/graphics/cg/ColorCG.cpp
@@ -83,7 +83,7 @@ static Optional<SRGBA<uint8_t>> roundAndClampToSRGBALossy(CGColorRef color)
         ASSERT_NOT_REACHED();
     }
 
-    return convertColor<SRGBA<uint8_t>>(SRGBA<float> { r, g, b, a });
+    return convertColor<SRGBA<uint8_t>>(makeFromComponentsClamping<SRGBA<float>>(r, g, b, a ));
 }
 
 Color::Color(CGColorRef color, OptionSet<Flags> flags)
