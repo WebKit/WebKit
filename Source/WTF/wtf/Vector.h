36582be546b17e05d68968fa67f5ec0dd9a4b156
Mark Lam <mark.lam@apple.com>
2021-05-23 13:59:17 +0000

    Build fix for JSCOnly-Linux-AArch64 bot.
    Not reviewed.
    
    * wtf/Vector.h:
    (WTF::Malloc>::expandCapacity):
    (WTF::Malloc>::reserveCapacity):
    (WTF::Malloc>::constructAndAppendSlowCase):
    
    
    
    Canonical link: https://commits.webkit.org/238061@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@277930 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/Vector.h
+++ Source/WTF/wtf/Vector.h
@@ -1076,6 +1076,7 @@ NEVER_INLINE T* Vector<T, inlineCapacity, OverflowHandler, minCapacity, Malloc>:
             if (UNLIKELY(!success))
                 return nullptr;
         }
+        UNUSED_PARAM(success);
         return ptr;
     }
     size_t index = ptr - begin();
@@ -1084,6 +1085,7 @@ NEVER_INLINE T* Vector<T, inlineCapacity, OverflowHandler, minCapacity, Malloc>:
         if (UNLIKELY(!success))
             return nullptr;
     }
+    UNUSED_PARAM(success);
     return begin() + index;
 }
 
@@ -1097,6 +1099,7 @@ inline U* Vector<T, inlineCapacity, OverflowHandler, minCapacity, Malloc>::expan
         if (UNLIKELY(!success))
             return nullptr;
     }
+    UNUSED_PARAM(success);
     return ptr;
 }
 
@@ -1208,6 +1211,7 @@ bool Vector<T, inlineCapacity, OverflowHandler, minCapacity, Malloc>::reserveCap
             return false;
         }
     }
+    UNUSED_PARAM(success);
     ASSERT(begin());
 
     asanSetInitialBufferSizeTo(size());
@@ -1351,6 +1355,7 @@ bool Vector<T, inlineCapacity, OverflowHandler, minCapacity, Malloc>::constructA
         if (UNLIKELY(!success))
             return false;
     }
+    UNUSED_PARAM(success);
     ASSERT(begin());
 
     asanBufferSizeWillChangeTo(m_size + 1);
