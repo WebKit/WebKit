d80620fb644418f0e4afb2af79ccb3a1c6efb5a6
Adrian Perez de Castro <aperez@igalia.com>
2021-05-27 18:45:04 +0000

    Unreviewed. Update OptionsWPE.cmake and NEWS for the 2.33.1 release
    .:
    
    * Source/cmake/OptionsWPE.cmake: Bump version numbers.
    
    Source/WebKit:
    
    * wpe/NEWS: Add release notes for 2.33.1
    
    Canonical link: https://commits.webkit.org/238214@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@278170 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/OptionsWPE.cmake
+++ Source/cmake/OptionsWPE.cmake
@@ -1,7 +1,7 @@
 include(GNUInstallDirs)
 include(VersioningUtils)
 
-SET_PROJECT_VERSION(2 33 0)
+SET_PROJECT_VERSION(2 33 1)
 
 set(USER_AGENT_BRANDING "" CACHE STRING "Branding to add to user agent string")
 
