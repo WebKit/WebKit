b07eea11f8005da98f818efd7f907d731d4d0067
Chris Dumez <cdumez@apple.com>
2021-06-01 14:56:04 +0000

    Fix thread safety issues in MediaElementAudioSourceNode https://bugs.webkit.org/show_bug.cgi?id=226475
    Reviewed by Youenn Fablet.
    
    Adopt thread safety analysis annotations in MediaElementAudioSourceNode and fix
    bugs found by clang. In particular, the following issues were fixed:
    - setFormat() was modifying m_muted / m_sourceNumberOfChannels / m_sourceSampleRate
      on the main thread without locking, even though those data members are accessed
      from the rendering thread.
    - process() was accessing  m_muted / m_sourceNumberOfChannels / m_sourceSampleRate
      on the rendering thread *before* locking.
    
    * Modules/webaudio/MediaElementAudioSourceNode.cpp:
    (WebCore::MediaElementAudioSourceNode::setFormat):
    (WebCore::MediaElementAudioSourceNode::process):
    * Modules/webaudio/MediaElementAudioSourceNode.h:
    
    
    Canonical link: https://commits.webkit.org/238343@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@278306 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/webaudio/MediaElementAudioSourceNode.h
+++ Source/WebCore/Modules/webaudio/MediaElementAudioSourceNode.h
@@ -72,11 +72,11 @@ private:
     Ref<HTMLMediaElement> m_mediaElement;
     Lock m_processLock;
 
-    unsigned m_sourceNumberOfChannels { 0 };
-    double m_sourceSampleRate { 0 };
-    bool m_muted { false };
+    unsigned m_sourceNumberOfChannels WTF_GUARDED_BY_LOCK(m_processLock) { 0 };
+    double m_sourceSampleRate WTF_GUARDED_BY_LOCK(m_processLock) { 0 };
+    bool m_muted WTF_GUARDED_BY_LOCK(m_processLock) { false };
 
-    std::unique_ptr<MultiChannelResampler> m_multiChannelResampler;
+    std::unique_ptr<MultiChannelResampler> m_multiChannelResampler WTF_GUARDED_BY_LOCK(m_processLock);
 };
 
 } // namespace WebCore
