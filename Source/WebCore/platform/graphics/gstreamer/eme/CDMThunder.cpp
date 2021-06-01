WebKit/WebKit

commit ddf66457238035120f86d14f557fc47af0c2c9ee

Clients of optional should use has_value instead of relying on hasValue macro https://bugs.webkit.org/show_bug.cgi?id=226395
Reviewed by Chris Dumez.

Source/JavaScriptCore:

* bytecompiler/NodesCodegen.cpp:
(JSC::RegExpNode::emitBytecode): Don't use hasValue.

Source/WebCore:

In some files we are touching, moved from Optional to std::optional, but doing
that consistently in files we are not otherwise modifying is out of scope here.

In some places, use has_value instead of hasValue. In others, improved further
by not using has_value explicitly when it's clearer to do so. Wasn't 100%
consistent about that, but did aim to cover 100% of the hasValue call sites.

* Modules/webaudio/AudioContext.cpp:
(WebCore::AudioContext::create): Don't use hasValue.
* Modules/webxr/WebXRFrame.cpp:
(WebCore::WebXRFrame::getViewerPose): Ditto.
(WebCore::WebXRFrame::getPose): Ditto.
* Modules/webxr/WebXRGamepad.cpp:
(WebCore::WebXRGamepad::WebXRGamepad): Ditto.

* Modules/webxr/WebXRInputSource.cpp:
(WebCore::WebXRInputSource::requiresInputSourceChange): Use has_value.
* bindings/js/DOMPromiseProxy.h:
(WebCore::DOMPromiseProxy<IDLType>::isFulfilled const): Ditto.
(WebCore::DOMPromiseProxy<IDLUndefined>::isFulfilled const): Ditto.
(WebCore::DOMPromiseProxyWithResolveCallback<IDLType>::isFulfilled const): Ditto.
* bindings/js/SerializedScriptValue.cpp:
(WebCore::CloneDeserializer::readTerminal): Ditto.

* css/CSSFontFaceSet.cpp:
(WebCore::CSSFontFaceSet::addToFacesLookupTable): Removed unneeded double check
of optional value. !x and !x.has_value() check the same thing.
(WebCore::CSSFontFaceSet::remove): Ditto.
(WebCore::CSSFontFaceSet::fontFace): Simplified code with extra local variables
to use an idiom that does not use hasValue. Since using *, ->, or value() on an
optional already asserts, there is no need to explicitly assert in the caller.

* css/CSSGradientValue.cpp:
(WebCore::GradientStop::isSpecified const): Use has_value.

* css/CSSSegmentedFontFace.cpp:

Source/WebCore/platform/graphics/gstreamer/eme/CDMThunder.cpp
@@ -250,7 +250,7 @@ RefPtr<SharedBuffer> CDMPrivateThunder::sanitizeResponse(const SharedBuffer& res
     return response.copy();
 }
 
-Optional<String> CDMPrivateThunder::sanitizeSessionId(const String& sessionId) const
+std::optional<String> CDMPrivateThunder::sanitizeSessionId(const String& sessionId) const
 {
     return sessionId;
 }
@@ -364,7 +364,7 @@ public:
     bool hasPayload() const { return static_cast<bool>(m_payload); }
     const Ref<SharedBuffer>& payload() const& { ASSERT(m_payload); return m_payload.value(); }
     Ref<SharedBuffer>& payload() & { ASSERT(m_payload); return m_payload.value(); }
-    bool hasType() const { return m_type.hasValue(); }
+    bool hasType() const { return m_type.has_value(); }
     WebCore::MediaKeyMessageType type() const { ASSERT(m_type); return m_type.value(); }
     WebCore::MediaKeyMessageType typeOr(WebCore::MediaKeyMessageType alternate) const { return m_type ? m_type.value() : alternate; }
     explicit operator bool() const { return m_isValid; }
@@ -372,8 +372,8 @@ public:
 
 private:
     bool m_isValid { false };
-    Optional<Ref<SharedBuffer>> m_payload;
-    Optional<WebCore::MediaKeyMessageType> m_type;
+    std::optional<Ref<SharedBuffer>> m_payload;
+    std::optional<WebCore::MediaKeyMessageType> m_type;
 };
 
 void CDMInstanceSessionThunder::challengeGeneratedCallback(RefPtr<SharedBuffer>&& buffer)
@@ -469,8 +469,7 @@ void CDMInstanceSessionThunder::keysUpdateDoneCallback()
     GST_DEBUG("update done");
     if (m_doesKeyStoreNeedMerging) {
         m_doesKeyStoreNeedMerging = false;
-        auto instance = cdmInstanceThunder();
-        if (instance)
+        if (auto instance = cdmInstanceThunder())
             instance->mergeKeysFrom(m_keyStore);
     }
 
@@ -674,12 +673,9 @@ void CDMInstanceSessionThunder::storeRecordOfKeyUsage(const String&)
     notImplemented();
 }
 
-Optional<CDMInstanceThunder&> CDMInstanceSessionThunder::cdmInstanceThunder() const
+CDMInstanceThunder* CDMInstanceSessionThunder::cdmInstanceThunder() const
 {
-    auto instance = cdmInstanceProxy();
-    if (!instance)
-        return std::nullopt;
-    return static_cast<CDMInstanceThunder&>(*instance);
+    return static_cast<CDMInstanceThunder*>(cdmInstanceProxy());
 }
 
 } // namespace WebCore

commit 2bc29a911e2282de2fc736fb55a0a46e6ae10067

[EME][Thunder] Build broken since r278244 https://bugs.webkit.org/show_bug.cgi?id=226498
Patch by Philippe Normand <pnormand@igalia.com> on 2021-06-01
Reviewed by Alex Christensen.

* platform/graphics/gstreamer/eme/CDMThunder.cpp:
(WebCore::CDMInstanceSessionThunder::cdmInstanceThunder const): Cast raw pointer instead of
its smart pointer container.

Canonical link: https://commits.webkit.org/238352@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@278316 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/gstreamer/eme/CDMThunder.cpp
@@ -675,7 +675,8 @@ void CDMInstanceSessionThunder::storeRecordOfKeyUsage(const String&)
 
 CDMInstanceThunder* CDMInstanceSessionThunder::cdmInstanceThunder() const
 {
-    return static_cast<CDMInstanceThunder*>(cdmInstanceProxy());
+    auto proxy = cdmInstanceProxy();
+    return static_cast<CDMInstanceThunder*>(proxy.get());
 }
 
 } // namespace WebCore