01aa8d9d38312b263e256c21d5197f35d6921c91
Aakash Jain <aakash_jain@apple.com>
2021-06-01 01:35:29 +0000

    REGRESSION(r278261): Broke windows build - follow-up fix https://bugs.webkit.org/show_bug.cgi?id=226477
    Unreviewed build fix.
    
    
    * css/calc/CSSCalcValue.cpp:
    
    
    Canonical link: https://commits.webkit.org/238328@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@278291 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/calc/CSSCalcValue.cpp
+++ Source/WebCore/css/calc/CSSCalcValue.cpp
@@ -37,7 +37,6 @@
 #include "CSSCalcNegateNode.h"
 #include "CSSCalcOperationNode.h"
 #include "CSSCalcPrimitiveValueNode.h"
-#include "CSSCalcSymbolTable.h"
 #include "CSSParser.h"
 #include "CSSParserTokenRange.h"
 #include "CSSPrimitiveValueMappings.h"
