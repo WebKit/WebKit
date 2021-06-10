WebKit/WebKit

commit ca3a5c4d8ee76e57082744959bf231b420fa732b

Non-special URLs should have an opaque origin https://bugs.webkit.org/show_bug.cgi?id=162254
Patch by Alex Christensen <achristensen@webkit.org> on 2021-02-06
Reviewed by Geoff Garen.

LayoutTests/imported/w3c:

* web-platform-tests/url/a-element-origin-expected.txt:
* web-platform-tests/url/a-element-origin-xhtml-expected.txt:
* web-platform-tests/url/url-origin-expected.txt:

Source/WebCore:

This matches the behavior of Chrome, Firefox, and the URL specification.
In order to maintain compatibility with existing apps, we do a linkedOnOrAfter check for the new behavior.
In order to allow things like web extensions to continue to work, we allow non-null origins for schemes
for which a WKURLSchemeHandler has been registered.  We learned this lesson 4 years ago when we tried this change.
This also makes sense conceptually because those schemes will be handled by the containing application,
so they can be an "origin" for a page.

* page/SecurityOrigin.cpp:
(WebCore::shouldTreatAsUniqueOrigin):
* platform/LegacySchemeRegistry.cpp:
(WebCore::schemesHandledBySchemeHandler):
(WebCore::LegacySchemeRegistry::registerURLSchemeAsHandledBySchemeHandler):
(WebCore::LegacySchemeRegistry::schemeIsHandledBySchemeHandler):
* platform/LegacySchemeRegistry.h:
* platform/cocoa/VersionChecks.h:

Source/WebInspectorUI:

* UserInterface/Base/URLUtilities.js:
(parseURL):

Source/WebKit:

I needed to change API::SecurityOrigin from wrapping a WebCore::SecurityOrigin to wrapping a WebCore::SecurityOriginData,
otherwise it would often be null and we want it to be not null in the UI process.

* Shared/API/APISecurityOrigin.h:
(API::SecurityOrigin::createFromString):
(API::SecurityOrigin::create):
(API::SecurityOrigin::securityOrigin const):
(API::SecurityOrigin::SecurityOrigin):
* Shared/API/c/WKSecurityOriginRef.cpp:
(WKSecurityOriginCopyDatabaseIdentifier):
(WKSecurityOriginCopyProtocol):
(WKSecurityOriginCopyHost):
(WKSecurityOriginGetPort):
* UIProcess/API/C/WKWebsiteDataStoreRef.cpp:

Source/WebKit/WebProcess/WebPage/WebPage.cpp
@@ -792,6 +792,8 @@ WebPage::WebPage(PageIdentifier pageID, WebPageCreationParameters&& parameters)
 
     for (const auto& iterator : parameters.urlSchemeHandlers)
         registerURLSchemeHandler(iterator.value, iterator.key);
+    for (auto& scheme : parameters.urlSchemesWithLegacyCustomProtocolHandlers)
+        LegacySchemeRegistry::registerURLSchemeAsHandledBySchemeHandler({ scheme });
 
     m_userContentController->addContentWorlds(parameters.userContentControllerParameters.userContentWorlds);
     m_userContentController->addUserScripts(WTFMove(parameters.userContentControllerParameters.userScripts), InjectUserScriptImmediately::No);
@@ -6631,6 +6633,7 @@ void WebPage::stopAllURLSchemeTasks()
 
 void WebPage::registerURLSchemeHandler(uint64_t handlerIdentifier, const String& scheme)
 {
+    WebCore::LegacySchemeRegistry::registerURLSchemeAsHandledBySchemeHandler(scheme);
     WebCore::LegacySchemeRegistry::registerURLSchemeAsCORSEnabled(scheme);
     auto schemeResult = m_schemeToURLSchemeHandlerProxyMap.add(scheme, WebURLSchemeHandlerProxy::create(*this, handlerIdentifier));
     m_identifierToURLSchemeHandlerProxyMap.add(handlerIdentifier, schemeResult.iterator->value.get());

LayoutTests/http/tests/security/postMessage/invalid-origin-throws-exception-expected.txt
@@ -1,12 +1,10 @@
-CONSOLE MESSAGE: Unable to post message to asdf://. Recipient has origin http://localhost:8000.
-
 window.location.href = http://127.0.0.1:8000/security/postMessage/invalid-origin-throws-exception.html
 waiting...
 Encountered exception SyntaxError: The string did not match the expected pattern. while posting message to ''.
 Encountered exception SyntaxError: The string did not match the expected pattern. while posting message to 'asdf'.
 Encountered exception SyntaxError: The string did not match the expected pattern. while posting message to '/tmp/foo'.
 Encountered exception SyntaxError: The string did not match the expected pattern. while posting message to '//localhost'.
-Posted message to 'asdf:' without any exceptions.
+Encountered exception SyntaxError: The string did not match the expected pattern. while posting message to 'asdf:'.
 Encountered exception SyntaxError: The string did not match the expected pattern. while posting message to 'http:'.
 Received message: data="Received message: data="done" origin="http://127.0.0.1:8000"" origin="http://localhost:8000"
 

LayoutTests/http/tests/security/postMessage/invalid-origin-throws-exception.html
@@ -29,7 +29,7 @@ function test() {
     tryPostMessage("/tmp/foo");
     tryPostMessage("//localhost");
 
-    // URLs without an origin should fail without generating any errors.
+    // URLs without an origin should fail with a syntax error.
     tryPostMessage("asdf:");
     tryPostMessage("http:");
     

Source/WTF/wtf/URL.h
@@ -190,6 +190,8 @@ public:
     template<typename Decoder> static WARN_UNUSED_RETURN bool decode(Decoder&, URL&);
     template<typename Decoder> static Optional<URL> decode(Decoder&);
 
+    WTF_EXPORT_PRIVATE bool hasSpecialScheme() const;
+
 private:
     friend class URLParser;
 
@@ -198,7 +200,6 @@ private:
     unsigned credentialsEnd() const;
     void remove(unsigned start, unsigned length);
     void parse(const String&);
-    bool hasSpecialScheme() const;
 
     friend WTF_EXPORT_PRIVATE bool protocolHostAndPortAreEqual(const URL&, const URL&);
 

Source/WTF/wtf/spi/darwin/dyldSPI.h
@@ -65,6 +65,10 @@
 #define DYLD_IOS_VERSION_14_5 0x000E0500
 #endif
 
+#ifndef DYLD_IOS_VERSION_15_0
+#define DYLD_IOS_VERSION_15_0 0x000f0000
+#endif
+
 #ifndef DYLD_MACOSX_VERSION_10_13
 #define DYLD_MACOSX_VERSION_10_13 0x000A0D00
 #endif
@@ -93,6 +97,10 @@
 #define DYLD_MACOSX_VERSION_11_3 0x000B0300
 #endif
 
+#ifndef DYLD_MACOSX_VERSION_12_00
+#define DYLD_MACOSX_VERSION_12_00 0x000c0000
+#endif
+
 #else
 
 #define DYLD_IOS_VERSION_3_0 0x00030000
@@ -111,6 +119,7 @@
 #define DYLD_IOS_VERSION_13_4 0x000D0400
 #define DYLD_IOS_VERSION_14_0 0x000E0000
 #define DYLD_IOS_VERSION_14_5 0x000E0500
+#define DYLD_IOS_VERSION_15_0 0x000f0000
 
 #define DYLD_MACOSX_VERSION_10_11 0x000A0B00
 #define DYLD_MACOSX_VERSION_10_12 0x000A0C00
@@ -121,6 +130,7 @@
 #define DYLD_MACOSX_VERSION_10_15_4 0x000A0F04
 #define DYLD_MACOSX_VERSION_10_16 0x000A1000
 #define DYLD_MACOSX_VERSION_11_3 0x000B0300
+#define DYLD_MACOSX_VERSION_12_00 0x000c0000
 
 #endif
 

commit 66c55267ac9ea0b201f7febbd9f2383643c9d5d1

Origin is null in http requests when baseURL has custom scheme on iOS 15.0 https://bugs.webkit.org/show_bug.cgi?id=226760 Source/WebCore:
<rdar://79027280>

Reviewed by Chris Dumez.

This fixes a regression from r272469 where I made two mistakes.

1. I made schemes handled by a WKURLSchemeHandler able to create non-null origins to increase compatibility.
I forgot that one can also call loadHTMLString and pass a baseURL that can be a custom scheme.
In this case, the custom scheme is also treated as the "origin" so it should be non-null.
2. When updateRequestForAccessControl is called in the Network Process, it uses SecurityOrigins that have been
serialized and deserialized then passed through the SecurityOrigin constructor, which doesn't know about the
registry of custom schemes that are handled for that WKWebView and may deserialize to a different (null) origin.
We want to deserialize the origin having been made with the rules of SecurityOrigins in the Web Process, so
set its data to be the same as the serialized data.

I added a unit test to prevent future regression.

* page/SecurityOrigin.h:
(WebCore::SecurityOrigin::decode):

Source/WebKit:


Reviewed by Chris Dumez.

* WebProcess/WebPage/WebPage.cpp:
(WebKit::WebPage::loadData):

Tools:


Reviewed by Chris Dumez.

* TestWebKitAPI/Tests/WebKitCocoa/LoadAlternateHTMLString.mm:
(TEST):



Canonical link: https://commits.webkit.org/238699@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@278737 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/page/SecurityOrigin.h
@@ -219,7 +219,7 @@ public:
     template<class Decoder> static RefPtr<SecurityOrigin> decode(Decoder&);
 
 private:
-    SecurityOrigin();
+    WEBCORE_EXPORT SecurityOrigin();
     explicit SecurityOrigin(const URL&);
     explicit SecurityOrigin(const SecurityOrigin*);
 
@@ -276,7 +276,8 @@ template<class Decoder> inline RefPtr<SecurityOrigin> SecurityOrigin::decode(Dec
     if (!data)
         return nullptr;
 
-    auto origin = SecurityOrigin::create(data->protocol, data->host, data->port);
+    auto origin = adoptRef(*new SecurityOrigin);
+    origin->m_data = WTFMove(*data);
 
     if (!decoder.decode(origin->m_domain))
         return nullptr;

Source/WebKit/WebProcess/WebPage/WebPage.cpp
@@ -1733,7 +1733,16 @@ void WebPage::loadData(LoadParameters&& loadParameters)
     platformDidReceiveLoadParameters(loadParameters);
 
     auto sharedBuffer = SharedBuffer::create(loadParameters.data.data(), loadParameters.data.size());
-    URL baseURL = loadParameters.baseURLString.isEmpty() ? aboutBlankURL() : URL(URL(), loadParameters.baseURLString);
+
+    URL baseURL;
+    if (loadParameters.baseURLString.isEmpty())
+        baseURL = aboutBlankURL();
+    else {
+        baseURL = URL(URL(), loadParameters.baseURLString);
+        if (!baseURL.protocolIsInHTTPFamily())
+            LegacySchemeRegistry::registerURLSchemeAsHandledBySchemeHandler(baseURL.protocol().toString());
+    }
+
     loadDataImpl(loadParameters.navigationID, loadParameters.shouldTreatAsContinuingLoad, WTFMove(loadParameters.websitePolicies), WTFMove(sharedBuffer), loadParameters.MIMEType, loadParameters.encodingName, baseURL, URL(), loadParameters.userData, loadParameters.isNavigatingToAppBoundDomain, loadParameters.shouldOpenExternalURLsPolicy);
 }
 

Tools/TestWebKitAPI/Tests/WebKitCocoa/LoadAlternateHTMLString.mm
@@ -24,10 +24,12 @@
  */
 
 #import "config.h"
-#import <WebKit/WKFoundation.h>
 
+#import "HTTPServer.h"
 #import "PlatformUtilities.h"
 #import "Test.h"
+#import "TestNavigationDelegate.h"
+#import <WebKit/WKFoundation.h>
 #import <WebKit/WKWebViewPrivate.h>
 #import <wtf/RetainPtr.h>
 #import <wtf/cocoa/NSURLExtras.h>
@@ -151,4 +153,25 @@ TEST(WKWebView, LoadAlternateHTMLStringFromProvisionalLoadErrorReload)
 
     WKBackForwardList *list = [webView backForwardList];
     EXPECT_EQ((NSUInteger)0, list.backList.count);
+}
+
+TEST(WKWebView, LoadHTMLStringOrigin)
+{
+    using namespace TestWebKitAPI;
+    bool done = false;
+    HTTPServer server([&](Connection connection) {
+        connection.receiveHTTPRequest([&](Vector<char>&& request) {
+            EXPECT_TRUE(strnstr(request.data(), "Origin: custom-scheme://\r\n", request.size()));
+            done = true;
+        });
+    });
+    auto webView = adoptNS([WKWebView new]);
+    auto delegate = adoptNS([TestNavigationDelegate new]);
+    delegate.get().decidePolicyForNavigationAction = ^(WKNavigationAction *, void (^completionHandler)(WKNavigationActionPolicy)) {
+        completionHandler(WKNavigationActionPolicyAllow);
+    };
+    webView.get().navigationDelegate = delegate.get();
+    NSString *html = @"<script>var xhr = new XMLHttpRequest(); xhr.open('GET', 'http://127.0.0.1:%d/', true); xhr.send();</script>";
+    [webView loadHTMLString:[NSString stringWithFormat:html, server.port()] baseURL:[NSURL URLWithString:@"custom-scheme://"]];
+    Util::run(&done);
 }