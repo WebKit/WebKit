1195ee5ef803c2b41fdb393c79753d63ed323431
Robert Jenner <jenner@apple.com>
2021-06-11 23:38:30 +0000

    [LayoutTests] Delete unused LayoutTests/security resources https://bugs.webkit.org/show_bug.cgi?id=226839
    Reviewed by Jonathan Bedard.
    
    * security/resources/worker-isSecureContext-disabled.js: Removed.
    
    
    Canonical link: https://commits.webkit.org/238751@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@278794 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/security/resources/worker-isSecureContext-disabled.js
+++
@@ -1,7 +0,0 @@
-importScripts("../../resources/js-test.js");
-
-if (typeof(self.isSecureContext) === 'undefined')
-    testPassed("self.isSecureContext is undefined.");
-else
-    testFailed("self.isSecureContext is defined.");
-finishJSTest();
