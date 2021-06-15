01bd74a8a68d4283f851ca958e57205939714360
Michael Catanzaro <mcatanzaro@gnome.org>
2021-06-15 16:47:42 +0000

    -Warray-bounds warning in Packed.h https://bugs.webkit.org/show_bug.cgi?id=226557 <rdar://problem/79103658>
    Patch by Michael Catanzaro <mcatanzaro@gnome.org> on 2021-06-15
    Reviewed by Darin Adler.
    
    Source/JavaScriptCore:
    
    * b3/air/AirAllocateRegistersByGraphColoring.cpp:
    * jit/JITCall.cpp:
    (JSC::JIT::compileOpCall):
    
    Source/WTF:
    
    * wtf/Bitmap.h:
    (WTF::WordType>::clear): Use the newly-introduced IGNORE_ARRAY_BOUNDS_WARNINGS macros
    instead of vanilla IGNORE_WARNINGS.
    * wtf/Compiler.h: Add new IGNORE_ARRAY_BOUNDS_WARNINGS_[BEGIN,END] macros, since this
    warning is now suppressed in several different places.
    * wtf/Packed.h: Suppress the warning. Also, add a static_assert for safety.
    
    Canonical link: https://commits.webkit.org/238821@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@278878 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/Packed.h
+++ Source/WTF/wtf/Packed.h
@@ -115,6 +115,7 @@ public:
     static constexpr bool isAlignmentShiftProfitable = storageSizeWithoutAlignmentShift > storageSizeWithAlignmentShift;
     static constexpr unsigned alignmentShiftSize = isAlignmentShiftProfitable ? alignmentShiftSizeIfProfitable : 0;
     static constexpr unsigned storageSize = storageSizeWithAlignmentShift;
+    static_assert(storageSize <= sizeof(uintptr_t));
 
     constexpr PackedAlignedPtr()
         : m_storage()
@@ -136,11 +137,15 @@ public:
         // FIXME: PackedPtr<> can load memory with one mov by checking page boundary.
         // https://bugs.webkit.org/show_bug.cgi?id=197754
         uintptr_t value = 0;
+
+IGNORE_ARRAY_BOUNDS_WARNINGS_BEGIN
 #if CPU(LITTLE_ENDIAN)
         memcpy(&value, m_storage.data(), storageSize);
 #else
         memcpy(bitwise_cast<uint8_t*>(&value) + (sizeof(void*) - storageSize), m_storage.data(), storageSize);
 #endif
+IGNORE_ARRAY_BOUNDS_WARNINGS_END
+
         if (isAlignmentShiftProfitable)
             value <<= alignmentShiftSize;
 
