0f3767ac3c48d18643923ce4830d6a05f2005fde
Filip Pizlo <fpizlo@apple.com>
2021-06-16 18:08:37 +0000

    RegisterSet should be smaller https://bugs.webkit.org/show_bug.cgi?id=227078
    Reviewed by Geoff Garen.
    
    Previously, every RegisterSet would have an extra 64-bit word in it just to hold state
    relevant to hashtable keys.
    
    But RegisterSet is almost never used as a hashtable key.
    
    This patch moves the hashtable key support into a subclass, HashableRegisterSet. That class
    ends up only being used in one place.
    
    On ARM64, this makes RegisterSet use 64 bits instead of 128 bits.
    
    On X86_64, this makes RegisterSet use 32 bits instead of 64 bits.
    
    * JavaScriptCore.xcodeproj/project.pbxproj:
    * ftl/FTLSlowPathCallKey.h:
    (JSC::FTL::SlowPathCallKey::SlowPathCallKey):
    * jit/HashableRegisterSet.h: Added.
    (JSC::HashableRegisterSet::HashableRegisterSet):
    (JSC::HashableRegisterSet::isEmptyValue const):
    (JSC::HashableRegisterSet::isDeletedValue const):
    (JSC::RegisterSetHash::hash):
    (JSC::RegisterSetHash::equal):
    * jit/RegisterSet.h:
    (): Deleted.
    (JSC::RegisterSet::isEmptyValue const): Deleted.
    (JSC::RegisterSet::isDeletedValue const): Deleted.
    (JSC::RegisterSetHash::hash): Deleted.
    (JSC::RegisterSetHash::equal): Deleted.
    
    
    
    Canonical link: https://commits.webkit.org/238872@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@278942 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/jit/RegisterSet.h
+++ Source/JavaScriptCore/jit/RegisterSet.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2019 Apple Inc. All rights reserved.
+ * Copyright (C) 2013-2021 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -35,7 +35,7 @@
 
 namespace JSC {
 
-typedef Bitmap<MacroAssembler::numGPRs + MacroAssembler::numFPRs + 1> RegisterBitmap;
+typedef Bitmap<MacroAssembler::numGPRs + MacroAssembler::numFPRs> RegisterBitmap;
 class RegisterAtOffsetList;
 
 class RegisterSet {
@@ -133,30 +133,6 @@ public:
     
     JS_EXPORT_PRIVATE void dump(PrintStream&) const;
     
-    enum EmptyValueTag { EmptyValue };
-    enum DeletedValueTag { DeletedValue };
-    
-    RegisterSet(EmptyValueTag)
-    {
-        m_bits.set(hashSpecialBitIndex);
-    }
-    
-    RegisterSet(DeletedValueTag)
-    {
-        m_bits.set(hashSpecialBitIndex);
-        m_bits.set(deletedBitIndex);
-    }
-    
-    bool isEmptyValue() const
-    {
-        return m_bits.get(hashSpecialBitIndex) && !m_bits.get(deletedBitIndex);
-    }
-    
-    bool isDeletedValue() const
-    {
-        return m_bits.get(hashSpecialBitIndex) && m_bits.get(deletedBitIndex);
-    }
-    
     bool operator==(const RegisterSet& other) const { return m_bits == other.m_bits; }
     bool operator!=(const RegisterSet& other) const { return m_bits != other.m_bits; }
     
@@ -222,28 +198,10 @@ private:
     // These offsets mirror the logic in Reg.h.
     static constexpr unsigned gprOffset = 0;
     static constexpr unsigned fprOffset = gprOffset + MacroAssembler::numGPRs;
-    static constexpr unsigned hashSpecialBitIndex = fprOffset + MacroAssembler::numFPRs;
-    static constexpr unsigned deletedBitIndex = 0;
     
     RegisterBitmap m_bits;
 };
 
-struct RegisterSetHash {
-    static unsigned hash(const RegisterSet& set) { return set.hash(); }
-    static bool equal(const RegisterSet& a, const RegisterSet& b) { return a == b; }
-    static constexpr bool safeToCompareToEmptyOrDeleted = false;
-};
-
 } // namespace JSC
-
-namespace WTF {
-
-template<typename T> struct DefaultHash;
-template<> struct DefaultHash<JSC::RegisterSet> : JSC::RegisterSetHash { };
-
-template<typename T> struct HashTraits;
-template<> struct HashTraits<JSC::RegisterSet> : public CustomHashTraits<JSC::RegisterSet> { };
-
-} // namespace WTF
 
 #endif // !ENABLE(C_LOOP)
