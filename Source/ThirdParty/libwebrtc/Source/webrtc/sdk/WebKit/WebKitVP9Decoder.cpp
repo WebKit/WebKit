f655984de91d5d53d38f26d93c87cd3f4f4708f6
Alan Coon <alancoon@apple.com>
2021-06-22 17:36:55 +0000

    Revert r279011. rdar://problem/79474211
    This reverts commit r279021.
    
    Canonical link: https://commits.webkit.org/232923.551@safari-611-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-611-branch@279124 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/ThirdParty/libwebrtc/Source/webrtc/sdk/WebKit/WebKitVP9Decoder.cpp
+++ Source/ThirdParty/libwebrtc/Source/webrtc/sdk/WebKit/WebKitVP9Decoder.cpp
@@ -47,33 +47,19 @@ static OSStatus invalidateVP9Decoder(CMBaseObjectRef);
 static void finalizeVP9Decoder(CMBaseObjectRef);
 static CFStringRef copyVP9DecoderDebugDescription(CMBaseObjectRef);
 
-#if defined(__x86_64__) || defined(_M_X64)
-    constexpr size_t padSize = 4;
-#else
-    constexpr size_t padSize = 0;
-#endif
-
-#pragma pack(push, 4)
-    struct DecoderClass { uint8_t pad[padSize]; CMBaseClass alignedClass; } WebKitVP9Decoder_BaseClass { { }, {
-        kCMBaseObject_ClassVersion_1,
-        sizeof(WebKitVP9Decoder),
-        nullptr, // Comparison by pointer equality
-        invalidateVP9Decoder,
-        finalizeVP9Decoder,
-        copyVP9DecoderDebugDescription,
-        nullptr, // CopyProperty
-        nullptr, // SetProperty
-        nullptr,
-        nullptr
-    } };
-#pragma pack(pop)
-
-#if defined(__x86_64__) || defined(_M_X64)
-    static_assert(sizeof(WebKitVP9Decoder_BaseClass.alignedClass.version) != sizeof(void*), "CMBaseClass fixup is required on X86_64");
-#else
-    static_assert(sizeof(WebKitVP9Decoder_BaseClass.alignedClass.version) == sizeof(void*), "CMBaseClass fixup only required on X86_64");
-#endif
-    static_assert(alignof(DecoderClass) == 4, "CMBaseClass must have 4 byte alignment");
+static const CMBaseClass WebKitVP9Decoder_BaseClass =
+{
+    kCMBaseObject_ClassVersion_1,
+    sizeof(WebKitVP9Decoder),
+    nullptr, // Comparison by pointer equality
+    invalidateVP9Decoder,
+    finalizeVP9Decoder,
+    copyVP9DecoderDebugDescription,
+    nullptr, // CopyProperty
+    nullptr, // SetProperty
+    nullptr,
+    nullptr
+};
 
 static OSStatus startVP9DecoderSession(VTVideoDecoderRef, VTVideoDecoderSession, CMVideoFormatDescriptionRef);
 static OSStatus decodeVP9DecoderFrame(VTVideoDecoderRef, VTVideoDecoderFrame, CMSampleBufferRef, VTDecodeFrameFlags, VTDecodeInfoFlags*);
@@ -95,7 +81,7 @@ static const VTVideoDecoderClass WebKitVP9Decoder_VideoDecoderClass =
 
 static const VTVideoDecoderVTable WebKitVP9DecoderVTable =
 {
-    { nullptr, &WebKitVP9Decoder_BaseClass.alignedClass },
+    { nullptr, &WebKitVP9Decoder_BaseClass },
     &WebKitVP9Decoder_VideoDecoderClass
 };
 
