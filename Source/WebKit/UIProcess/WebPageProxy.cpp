7a642e2b50e2eb6de481afac7224ddc386332adb
Per Arne Vollan <pvollan@apple.com>
2021-06-22 21:07:14 +0000

    [macOS] Deny mach-lookup to the service 'com.apple.print.normalizerd' https://bugs.webkit.org/show_bug.cgi?id=227248 <rdar://problem/79600557>
    Reviewed by Brent Fulgham.
    
    Source/WebKit:
    
    Deny mach-lookup to the service 'com.apple.print.normalizerd' in the WebKit processes on macOS.
    
    * GPUProcess/mac/com.apple.WebKit.GPUProcess.sb.in:
    * PluginProcess/mac/com.apple.WebKit.plugin-common.sb.in:
    * UIProcess/WebPageProxy.cpp:
    (WebKit::gpuMachServices):
    * WebProcess/com.apple.WebProcess.sb.in:
    
    LayoutTests:
    
    Skip EPS test, which is failing when this service is unavailable.
    
    * platform/mac/TestExpectations:
    
    
    Canonical link: https://commits.webkit.org/239053@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@279145 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/WebPageProxy.cpp
+++ Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -7954,7 +7954,6 @@ static const Vector<ASCIILiteral>& gpuMachServices()
         "com.apple.MTLCompilerService"_s,
 #if PLATFORM(MAC) || PLATFORM(MACCATALYST)
         "com.apple.cvmsServ"_s,
-        "com.apple.print.normalizerd"_s,
 #endif
     });
     return services;
