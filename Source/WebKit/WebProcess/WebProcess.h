6f58f3ec6b2937256c1d83497dd152fd31538ab8
Chris Dumez <cdumez@apple.com>
2021-06-22 23:48:33 +0000

    [macOS] Suspend WebProcesses that are in the process cache https://bugs.webkit.org/show_bug.cgi?id=227269
    Reviewed by Geoffrey Garen.
    
    Suspend WebProcesses that are in the process cache on macOS to make sure they use no CPU.
    
    * UIProcess/WebProcessProxy.cpp:
    (WebKit::WebProcessProxy::setIsInProcessCache):
    (WebKit::WebProcessProxy::platformSuspendProcess):
    (WebKit::WebProcessProxy::platformResumeProcess):
    * UIProcess/WebProcessProxy.h:
    * UIProcess/mac/WebProcessProxyMac.mm:
    (WebKit::WebProcessProxy::platformSuspendProcess):
    (WebKit::WebProcessProxy::platformResumeProcess):
    * WebProcess/WebProcess.cpp:
    (WebKit::WebProcess::setIsInProcessCache):
    * WebProcess/WebProcess.h:
    * WebProcess/WebProcess.messages.in:
    
    
    Canonical link: https://commits.webkit.org/239059@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@279155 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/WebProcess/WebProcess.h
+++ Source/WebKit/WebProcess/WebProcess.h
@@ -420,7 +420,7 @@ private:
     void platformTerminate();
 
     void setHasSuspendedPageProxy(bool);
-    void setIsInProcessCache(bool);
+    void setIsInProcessCache(bool, CompletionHandler<void()>&&);
     void markIsNoLongerPrewarmed();
 
     void registerURLSchemeAsEmptyDocument(const String&);
