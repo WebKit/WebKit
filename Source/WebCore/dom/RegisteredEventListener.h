736f76d95206f78f5a1b20155cfa37265ba38391
Yusuke Suzuki <ysuzuki@apple.com>
2021-06-22 15:31:06 +0000

    Shrink sizeof(RegisteredEventListener) https://bugs.webkit.org/show_bug.cgi?id=227230
    Reviewed by Geoffrey Garen.
    
    This patch changes the ordering of members of RegisteredEventListener to make it small from 24 to 16.
    
    * dom/RegisteredEventListener.h:
    (WebCore::RegisteredEventListener::RegisteredEventListener):
    (): Deleted.
    
    Canonical link: https://commits.webkit.org/239034@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@279117 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/dom/RegisteredEventListener.h
+++ Source/WebCore/dom/RegisteredEventListener.h
@@ -2,7 +2,7 @@
  * Copyright (C) 2001 Peter Kelly (pmk@post.com)
  * Copyright (C) 2001 Tobias Anton (anton@stud.fbi.fh-darmstadt.de)
  * Copyright (C) 2006 Samuel Weinig (sam.weinig@gmail.com)
- * Copyright (C) 2003, 2004, 2005, 2006, 2008, 2009 Apple Inc. All rights reserved.
+ * Copyright (C) 2003-2021 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -58,18 +58,19 @@ public:
 
 private:
     RegisteredEventListener(Ref<EventListener>&& listener, const Options& options)
-        : m_callback(WTFMove(listener))
-        , m_useCapture(options.capture)
+        : m_useCapture(options.capture)
         , m_isPassive(options.passive)
         , m_isOnce(options.once)
+        , m_wasRemoved(false)
+        , m_callback(WTFMove(listener))
     {
     }
 
+    bool m_useCapture : 1;
+    bool m_isPassive : 1;
+    bool m_isOnce : 1;
+    bool m_wasRemoved : 1;
     Ref<EventListener> m_callback;
-    bool m_useCapture { false };
-    bool m_isPassive { false };
-    bool m_isOnce { false };
-    bool m_wasRemoved { false };
 };
 
 } // namespace WebCore
