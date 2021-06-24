c23f48ca1380df764b7c51943500f9e4386ce3d2
Alan Bujtas <zalan@apple.com>
2021-06-24 13:49:54 +0000

    [IFC][Integration] Text selection flashing to end of paragraph https://bugs.webkit.org/show_bug.cgi?id=227158 <rdar://79138828>
    Reviewed by Antti Koivisto.
    
    Source/WebCore:
    
    The "find the line for this position" code in RenderText::positionForPoint() expects adjoining lines boxes (i.e. no gaps between the lines). This function is called by hittesting as part of the text selection process.
    This patch implements the legacy behavior by
    1. expanding the "selection top" all the way to the (selection)bottom of the previous line.
    2. ensures that the first line's computed offset is ignored when the containing block has no border/padding.
    
    Test: fast/events/touch/ios/hidpi-selection-when-content-is-on-subpixel.html
    
    * layout/integration/LayoutIntegrationLineIteratorModernPath.h:
    (WebCore::LayoutIntegration::LineIteratorModernPath::selectionTop const):
    (WebCore::LayoutIntegration::LineIteratorModernPath::selectionTopForHitTesting const):
    
    LayoutTests:
    
    * fast/events/touch/ios/drag-to-autoscroll-in-single-line-editable.html:
    * fast/events/touch/ios/hidpi-selection-when-content-is-on-subpixel-expected.txt: Added.
    * fast/events/touch/ios/hidpi-selection-when-content-is-on-subpixel.html: Added.
    
    
    Canonical link: https://commits.webkit.org/239106@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@279220 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/layout/integration/LayoutIntegrationLineIteratorModernPath.h
+++ Source/WebCore/layout/integration/LayoutIntegrationLineIteratorModernPath.h
@@ -29,6 +29,7 @@
 
 #include "LayoutIntegrationInlineContent.h"
 #include "LayoutIntegrationRunIteratorModernPath.h"
+#include "RenderBlockFlow.h"
 
 namespace WebCore {
 
@@ -54,9 +55,11 @@ public:
     LayoutUnit lineBoxTop() const { return LayoutUnit::fromFloatRound(line().lineBoxTop()); }
     LayoutUnit lineBoxBottom() const { return LayoutUnit::fromFloatRound(line().lineBoxBottom()); }
 
-    // FIXME: What should these really be?
-    LayoutUnit selectionTop() const { return top(); }
-    LayoutUnit selectionTopForHitTesting() const { return top(); }
+    LayoutUnit selectionTop() const { return !m_lineIndex ? top() : LineIteratorModernPath(*m_inlineContent, m_lineIndex - 1).selectionBottom(); }
+    // FIXME: Remove the containingBlock().borderAndPaddingBefore() offset after retiring legacy line layout. It also requires changes in RenderText::positionForPoint to find the first line with offset.
+    // - the "before" value is already factored in to the line offset
+    // - this logic negates the first line's natural offset (e.g. block has no border/padding but the first line has a computed offset).
+    LayoutUnit selectionTopForHitTesting() const { return !m_lineIndex ? containingBlock().borderAndPaddingBefore() : selectionTop(); };
     LayoutUnit selectionBottom() const { return bottom(); }
 
     float contentLogicalLeft() const { return line().lineBoxLeft() + line().contentLeft(); }
