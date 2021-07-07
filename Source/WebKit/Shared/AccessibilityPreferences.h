c0ac6b9fc8c2180770ffe3d6257b375bdf293f8d
Per Arne Vollan <pvollan@apple.com>
2021-07-07 21:00:40 +0000

    Cherry-pick r279342. rdar://problem/80282927
        Pass correct value of AX per app settings to the WebContent process
        https://bugs.webkit.org/show_bug.cgi?id=227441
        <rdar://problem/79857797>
    
        Reviewed by Brent Fulgham.
    
        Pass value of type AXValueState instead of a boolean value for per app AX settings.
    
        * Shared/AccessibilityPreferences.h:
        * UIProcess/Cocoa/WebProcessPoolCocoa.mm:
        (WebKit::accessibilityPreferences):
        * WebProcess/cocoa/WebProcessCocoa.mm:
        (WebKit::WebProcess::accessibilityPreferencesDidChange):
    
        Canonical link: https://commits.webkit.org/239210@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@279342 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/239204.19@safari-612.1.22.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-612.1.22.1-branch@279667 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/AccessibilityPreferences.h
+++ Source/WebKit/Shared/AccessibilityPreferences.h
@@ -31,15 +31,19 @@
 #include <WebCore/CaptionUserPreferences.h>
 #endif
 
+#if HAVE(PER_APP_ACCESSIBILITY_PREFERENCES)
+#include "AccessibilitySupportSPI.h"
+#endif
+
 namespace WebKit {
 
 struct AccessibilityPreferences {
 #if HAVE(PER_APP_ACCESSIBILITY_PREFERENCES)
-    bool reduceMotionEnabled { false };
-    bool increaseButtonLegibility { false };
-    bool enhanceTextLegibility { false };
-    bool darkenSystemColors { false };
-    bool invertColorsEnabled { false };
+    AXValueState reduceMotionEnabled { AXValueStateEmpty };
+    AXValueState increaseButtonLegibility { AXValueStateEmpty };
+    AXValueState enhanceTextLegibility { AXValueStateEmpty };
+    AXValueState darkenSystemColors { AXValueStateEmpty };
+    AXValueState invertColorsEnabled { AXValueStateEmpty };
 #endif
 #if HAVE(MEDIA_ACCESSIBILITY_FRAMEWORK)
     WebCore::CaptionUserPreferences::CaptionDisplayMode captionDisplayMode;
@@ -55,3 +59,19 @@ template<> struct ArgumentCoder<WebKit::AccessibilityPreferences> {
     static std::optional<WebKit::AccessibilityPreferences> decode(Decoder&);
 };
 }
+
+#if HAVE(PER_APP_ACCESSIBILITY_PREFERENCES)
+namespace WTF {
+
+template<> struct EnumTraits<AXValueState> {
+    using values = EnumValues<
+        AXValueState,
+        AXValueState::AXValueStateInvalid,
+        AXValueState::AXValueStateEmpty,
+        AXValueState::AXValueStateOff,
+        AXValueState::AXValueStateOn
+    >;
+};
+
+} // namespace WTF
+#endif
