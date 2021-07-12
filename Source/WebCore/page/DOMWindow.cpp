6b0e5bca31881208a29705f6f2cfc171592e82ba
Chris Dumez <cdumez@apple.com>
2021-07-12 17:08:33 +0000

    Unreviewed, partial revert of r279661 to address crashes on iOS Debug.
    * page/DOMWindow.cpp:
    (WebCore::DOMWindow::removeAllEventListeners):
    
    Canonical link: https://commits.webkit.org/239593@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@279832 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/DOMWindow.cpp
+++ Source/WebCore/page/DOMWindow.cpp
@@ -2343,7 +2343,7 @@ void DOMWindow::removeAllEventListeners()
 #endif
 
 #if ENABLE(TOUCH_EVENTS)
-    if (RefPtr document = this->document())
+    if (auto* document = this->document())
         document->didRemoveEventTargetNode(*document);
 #endif
 
