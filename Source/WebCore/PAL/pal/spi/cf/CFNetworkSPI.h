2651dbf761c698c145af5db44e07a9cc580b697b
Alex Christensen <achristensen@apple.com>
2021-07-28 15:55:25 +0000

    Fix build with SDKs containing new CFNetwork SPI declaration https://bugs.webkit.org/show_bug.cgi?id=228553 <rdar://81202574>
    Reviewed by Brady Eidson.
    
    CFNSURLConnection.h also has "@interface NSURLSessionConfiguration ()"
    ObjC doesn't like it if you have the exact same thing in the parentheses and a duplicate declaration.
    I reproduced the build failure with a modified SDK and verified this fixes it.
    
    * pal/spi/cf/CFNetworkSPI.h:
    
    
    
    Canonical link: https://commits.webkit.org/240027@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@280384 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/PAL/pal/spi/cf/CFNetworkSPI.h
+++ Source/WebCore/PAL/pal/spi/cf/CFNetworkSPI.h
@@ -488,7 +488,7 @@ WTF_EXTERN_C_END
 - (void)_setMIMEType:(NSString *)type;
 @end
 
-@interface NSURLSessionConfiguration ()
+@interface NSURLSessionConfiguration (SPI)
 // FIXME: Remove this once rdar://problem/40650244 is in a build.
 @property (copy) NSDictionary *_socketStreamProperties;
 // FIXME: Remove this once rdar://80550123 is in a build.
