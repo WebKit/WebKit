97f3fdf7c639f0146bda5eaa33c78c078645c706
Chris Dumez <cdumez@apple.com>
2021-08-03 03:53:25 +0000

    [COOP] Cross-Origin-Opener-Policy header parsing fails when report-to parameter is present https://bugs.webkit.org/show_bug.cgi?id=228719
    Reviewed by Geoff Garen.
    
    LayoutTests/imported/w3c:
    
    Rebaseline WPT test that is now failing a bit later.
    
    * web-platform-tests/html/cross-origin-opener-policy/reporting/navigation-reporting/reporting-redirect-with-same-origin-allow-popups.https-expected.txt:
    
    Source/WebCore:
    
    Cross-Origin-Opener-Policy header parsing fails when report-to parameter is present, because
    parseStructuredFieldValue() doesn't handle parameters whose value is double-quoted:
    - https://datatracker.ietf.org/doc/html/rfc8941#section-4.2.3.2
    - https://datatracker.ietf.org/doc/html/rfc8941#section-4.2.3.1
    - https://datatracker.ietf.org/doc/html/rfc8941#section-4.2.5
    
    Test: http/wpt/cross-origin-opener-policy/header-parsing-with-report-to.https.html
    
    * platform/network/HTTPParsers.cpp:
    (WebCore::parseStructuredFieldValue):
    
    LayoutTests:
    
    Add layout test coverage.
    
    * http/wpt/cross-origin-opener-policy/header-parsing-with-report-to.https-expected.txt: Added.
    * http/wpt/cross-origin-opener-policy/header-parsing-with-report-to.https.html: Added.
    
    
    Canonical link: https://commits.webkit.org/240204@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@280582 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/network/HTTPParsers.cpp
+++ Source/WebCore/platform/network/HTTPParsers.cpp
@@ -38,6 +38,7 @@
 #include "ParsedContentType.h"
 #include <wtf/DateMath.h>
 #include <wtf/NeverDestroyed.h>
+#include <wtf/text/StringBuilder.h>
 #include <wtf/text/StringToIntegerConversion.h>
 #include <wtf/unicode/CharacterNames.h>
 
@@ -618,20 +619,46 @@ std::optional<std::pair<StringView, HashMap<String, String>>> parseStructuredFie
                 break;
             ++index;
         }
-        String key = header.substring(keyStart, index - keyStart).toStringWithoutCopying();
+        String key = header.substring(keyStart, index - keyStart).toString();
         String value = "true";
         if (index < header.length() && header[index] == '=') {
             ++index; // Consume '='.
-            if (!isASCIIAlpha(header[index]) && header[index] != '*')
+            if (isASCIIAlpha(header[index]) || header[index] == '*') {
+                // https://datatracker.ietf.org/doc/html/rfc8941#section-4.2.6
+                size_t valueStart = index++;
+                while (index < header.length()) {
+                    UChar c = header[index];
+                    if (!RFC7230::isTokenCharacter(c) && c != ':' && c != '/')
+                        break;
+                    ++index;
+                }
+                value = header.substring(valueStart, index - valueStart).toString();
+            } else if (header[index] == '"') {
+                // https://datatracker.ietf.org/doc/html/rfc8941#section-4.2.5
+                StringBuilder valueBuilder;
+                ++index; // Skip DQUOTE.
+                while (index < header.length()) {
+                    if (header[index] == '\\') {
+                        ++index;
+                        if (index == header.length())
+                            return std::nullopt;
+                        if (header[index] != '\\' && header[index] != '"')
+                            return std::nullopt;
+                        valueBuilder.append(header[index]);
+                    } else if (header[index] == '\"') {
+                        value = valueBuilder.toString();
+                        break;
+                    } else if ((header[index] >= 0x00 && header[index] <= 0x1F) || (header[index] >= 0x7F && header[index] <= 0xFF)) // Not in VCHAR or SP.
+                        return std::nullopt;
+                    else
+                        valueBuilder.append(header[index]);
+                    ++index;
+                }
+                if (index == header.length())
+                    return std::nullopt;
+                ++index; // Skip DQUOTE.
+            } else
                 return std::nullopt;
-            size_t valueStart = index++;
-            while (index < header.length()) {
-                UChar c = header[index];
-                if (!RFC7230::isTokenCharacter(c) && c != ':' && c != '/')
-                    break;
-                ++index;
-            }
-            value = header.substring(valueStart, index - valueStart).toStringWithoutCopying();
         }
         parameters.set(WTFMove(key), WTFMove(value));
     }
