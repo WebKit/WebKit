f9d7f6de20eda1ed5f90e229814b7c0986a27e0b
Tadeu Zagallo <tzagallo@apple.com>
2021-08-18 19:51:29 +0000

    Cherry-pick r280463. rdar://problem/82088775
        definePropertyOnReceiver should check if receiver canPerformFastPutInline
        https://bugs.webkit.org/show_bug.cgi?id=227963
        <rdar://80259710>
    
        Reviewed by Alexey Shvayka.
    
        JSTests:
    
        * stress/reflect-set-custom-value.js: Added.
    
        Source/JavaScriptCore:
    
        definePropertyOnReceiver has a fast path if the slot is not opaque and the receiver doesn't
        have a custom defineOwnProperty implementation, in which case it calls putInlineFast (and
        transitively putDirectInternal<PutModePut>). The issue is that putDirectInternal does not
        handle customValues correctly: it just overwrites the property without changing the attributes.
        To fix that, we should first check if the property might be a custom value, and if that's the case
        we now call `definePropertyOnReceiverSlow`, which has been updated to handle custom values correctly.
        I also added assertions to putInlineFastReplacingStaticPropertyIfNeeded and putDirectInternal
        to make sure we don't accidentally overwrite custom values in the future.
    
        * runtime/JSObject.cpp:
        (JSC::definePropertyOnReceiverSlow):
        (JSC::JSObject::definePropertyOnReceiver):
        (JSC::JSObject::putInlineFastReplacingStaticPropertyIfNeeded):
        * runtime/JSObjectInlines.h:
        (JSC::JSObject::putDirectInternal):
    
        Canonical link: https://commits.webkit.org/240098@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@280463 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/240203.52@safari-612.1.27.0-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-612.1.27.0-branch@281207 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/reflect-set-custom-value.js
+++ JSTests/stress/reflect-set-custom-value.js
@@ -1,14 +1,3 @@
-{
-    // reified
-    const testGetterSetter = $vm.createCustomTestGetterSetter();
-    Reflect.set({}, 'customValue', 'foo', testGetterSetter);
-    testGetterSetter.customValue = 42;
-}
-
-{
-    // non-reified
-    let tester = $vm.createStaticCustomValue();
-    Reflect.set({}, "testStaticValueSetFlag", 'foo', tester);
-    if (!tester.testStaticValueSetterCalled)
-        throw new Error('Custom value overriden');
-}
+const testGetterSetter = $vm.createCustomTestGetterSetter();
+Reflect.set({}, 'customValue', 'foo', testGetterSetter);
+testGetterSetter.customValue = 42;
