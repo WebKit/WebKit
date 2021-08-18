4a11787585164dc775c64b5cf8753e9da4eb9363
Tadeu Zagallo <tzagallo@apple.com>
2021-08-18 19:51:34 +0000

    Cherry-pick r280505. rdar://problem/82088775
        putInlineFastReplacingStaticPropertyIfNeeded should handle custom values
        https://bugs.webkit.org/show_bug.cgi?id=227963
    
        Reviewed by Alexey Shvayka.
    
        JSTests:
    
        * stress/reflect-set-custom-value.js:
    
        Source/JavaScriptCore:
    
        Follow up after r280463: as it turns out, putInlineFastReplacingStaticPropertyIfNeeded also needs to handle
        custom values, similar to how definePropertyOnReceiverSlow was updated. This function will be called when the
        target property of the receiver is a custom value and isn't reified. The previous test case was expanded to
        test both the reified and non-reified cases.
    
        * runtime/JSObject.cpp:
        (JSC::JSObject::putInlineFastReplacingStaticPropertyIfNeeded):
        * tools/JSDollarVM.cpp:
    
        Canonical link: https://commits.webkit.org/240137@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@280505 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/240203.53@safari-612.1.27.0-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-612.1.27.0-branch@281208 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/runtime/JSObject.cpp
+++ Source/JavaScriptCore/runtime/JSObject.cpp
@@ -942,8 +942,7 @@ bool JSObject::putInlineFastReplacingStaticPropertyIfNeeded(JSGlobalObject* glob
                     RELEASE_AND_RETURN(scope, customSetter(structure->globalObject(), JSValue::encode(this), JSValue::encode(value), propertyName));
             }
             // Avoid PutModePut because it fails for non-extensible structures.
-            ASSERT(!(entry->value->attributes() & PropertyAttribute::CustomValue));
-            putDirect(vm, propertyName, value, attributesForStructure(entry->value->attributes()), slot);
+            putDirect(vm, propertyName, value, attributesForStructure(entry->value->attributes()) & ~PropertyAttribute::CustomValue, slot);
             return true;
         }
     }
