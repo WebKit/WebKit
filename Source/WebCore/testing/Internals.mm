38045a526a16dcf376ae5a9914bf67d1b55a7e78
Kyle Piddington <kpiddington@apple.com>
2021-08-19 16:28:04 +0000

    WebGL via Metal experimental feature does not correctly toggle metal backend https://bugs.webkit.org/show_bug.cgi?id=229267 <rdar://81855735>
    Source/WebCore:
    
    Patch by Kyle Piddington <kpiddington@apple.com> on 2021-08-19
    Reviewed by Dean Jackson.
    
    GraphicsContextGLAttributes defines 'useMetal' as 'true' by default.
    Since this branch was only checking if Metal was enabled via the
    setting, rather than checking the status of the flag, the metal backend
    was never disabled, even when requested.
    
    Tests: webgl/webgl-metal-disabled.html
           webgl/webgl-metal-enabled.html
    
    * WebCore.xcodeproj/project.pbxproj:
    * html/canvas/WebGLRenderingContextBase.cpp:
    (WebCore::WebGLRenderingContextBase::create):
    * testing/Internals.cpp:
    (WebCore::Internals::requestedMetal):
    * testing/Internals.h:
    * testing/Internals.idl:
    * testing/Internals.mm:
    (WebCore::Internals::platformSupportsMetal):
    
    LayoutTests:
    
    Add tests to verify WebGL feature flag works as intended.
    
    Patch by Kyle Piddington <kpiddington@apple.com> on 2021-08-19
    Reviewed by Dean Jackson.
    
    * webgl/webgl-metal-disabled-expected.txt: Added.
    * webgl/webgl-metal-disabled.html: Added.
    * webgl/webgl-metal-enabled-expected.txt: Added.
    * webgl/webgl-metal-enabled.html: Added.
    
    Canonical link: https://commits.webkit.org/240682@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@281245 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/testing/Internals.mm
+++ Source/WebCore/testing/Internals.mm
@@ -38,6 +38,9 @@
 #import "SimpleRange.h"
 #import "UTIUtilities.h"
 #import <AVFoundation/AVPlayer.h>
+#if PLATFORM(COCOA)
+#import <Metal/Metal.h>
+#endif
 #import <pal/spi/cocoa/NSAccessibilitySPI.h>
 #import <wtf/cocoa/NSURLExtras.h>
 #import <wtf/spi/darwin/SandboxSPI.h>
@@ -45,6 +48,8 @@
 #if PLATFORM(IOS_FAMILY)
 #import <pal/ios/UIKitSoftLink.h>
 #endif
+
+
 
 namespace WebCore {
 
@@ -145,5 +150,26 @@ bool Internals::hasSandboxIOKitOpenAccessToClass(const String& process, const St
 
     return !sandbox_check(pid, "iokit-open", static_cast<enum sandbox_filter_type>(SANDBOX_FILTER_IOKIT_CONNECTION | SANDBOX_CHECK_NO_REPORT), ioKitClass.utf8().data());
 }
+
+#if ENABLE(WEBGL) && PLATFORM(COCOA)
+bool Internals::platformSupportsMetal(bool isWebGL2)
+{
+    auto device = MTLCreateSystemDefaultDevice();
+
+    if (device) {
+#if PLATFORM(IOS_FAMILY) && !PLATFORM(IOS_FAMILY_SIMULATOR)
+        // A8 devices (iPad Mini 4, iPad Air 2) cannot use WebGL2 via Metal.
+        // This check can be removed once they are no longer supported.
+        if (isWebGL2)
+            return [device supportsFamily:MTLGPUFamilyApple3];
+#else
+        UNUSED_PARAM(isWebGL2);
+#endif
+        return true;
+    }
+
+    return false;
+}
+#endif
 
 }
