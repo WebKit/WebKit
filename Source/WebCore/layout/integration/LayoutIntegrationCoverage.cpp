7d2be597538e034d29cab9cdc32bbc5561de85f6
Alan Bujtas <zalan@apple.com>
2021-08-20 12:44:29 +0000

    [IFC][Integration] Enable non-auto line-break values https://bugs.webkit.org/show_bug.cgi?id=228842
    Reviewed by Antti Koivisto.
    
    Source/WebCore:
    
    IFC already supports line-break.
    
    * layout/integration/LayoutIntegrationCoverage.cpp:
    (WebCore::LayoutIntegration::canUseForStyle):
    
    LayoutTests:
    
    * TestExpectations: progressions
    * platform/mac/fast/text/trailing-white-space-expected.txt: collapsed trailing whitespace cleanup.
    
    
    Canonical link: https://commits.webkit.org/240727@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@281307 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/layout/integration/LayoutIntegrationCoverage.cpp
+++ Source/WebCore/layout/integration/LayoutIntegrationCoverage.cpp
@@ -171,9 +171,6 @@ static void printReason(AvoidanceReason reason, TextStream& stream)
     case AvoidanceReason::FlowHasBorderFitLines:
         stream << "-webkit-border-fit";
         break;
-    case AvoidanceReason::FlowHasNonAutoLineBreak:
-        stream << "line-break is not auto";
-        break;
     case AvoidanceReason::FlowHasTextSecurity:
         stream << "text-security is not none";
         break;
@@ -576,8 +573,6 @@ static OptionSet<AvoidanceReason> canUseForStyle(const RenderStyle& style, Inclu
         SET_REASON_AND_RETURN_IF_NEEDED(FlowHasTextFillBox, reasons, includeReasons);
     if (style.borderFit() == BorderFit::Lines)
         SET_REASON_AND_RETURN_IF_NEEDED(FlowHasBorderFitLines, reasons, includeReasons);
-    if (style.lineBreak() != LineBreak::Auto)
-        SET_REASON_AND_RETURN_IF_NEEDED(FlowHasNonAutoLineBreak, reasons, includeReasons);
     // Special handling of text-security:disc is not yet implemented in the simple line layout code path.
     // See RenderBlock::updateSecurityDiscCharacters.
     if (style.textSecurity() != TextSecurity::None)
