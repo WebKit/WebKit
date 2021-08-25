0a92359b533738338d1f45272f001f0aeff04767
Jonathan Bedard <jbedard@apple.com>
2021-08-25 21:06:36 +0000

    [resultsdbpy] Handle E configuration comparisons https://bugs.webkit.org/show_bug.cgi?id=229477 <rdar://problem/82316775>
    Reviewed by Stephanie Lewis.
    
    * Scripts/libraries/resultsdbpy/resultsdbpy/__init__.py: Bump version.
    * Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/configuration.js:
    (Configuration.prototype.compare): Ignore "E" version names in both directions.
    * Scripts/libraries/resultsdbpy/setup.py: Bump version.
    
    
    Canonical link: https://commits.webkit.org/240945@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@281581 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/configuration.js
+++ Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/configuration.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 Apple Inc. All rights reserved.
+// Copyright (C) 2019-2021 Apple Inc. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -207,7 +207,7 @@ class Configuration {
 
                 if (this[key] !== null && configuration[key] !== null) {
                     if (typeof this[key] === 'string') {
-                        if (key === 'version_name' && this[key].startsWith(configuration[key]))
+                        if (key === 'version_name' && (this[key].startsWith(configuration[key]) || configuration[key].startsWith(this[key])))
                             result = 0;
                         else
                             result = this[key].localeCompare(configuration[key]);
--- Tools/Scripts/libraries/resultsdbpy/setup.py
+++ Tools/Scripts/libraries/resultsdbpy/setup.py
@@ -30,7 +30,7 @@ def readme():
 
 setup(
     name='resultsdbpy',
-    version='3.1.0',
+    version='3.1.1',
     description='Library for visualizing, processing and storing test results.',
     long_description=readme(),
     classifiers=[
