3ff948d2586dac5d02f2db6af53b90b052999473
Patrick Angle <pangle@apple.com>
2021-08-26 21:59:19 +0000

    [ EWS Catalina ] http/tests/websocket/tests/hybi/inspector/send-and-recieve-debugger.html is flaky timing out / failing https://bugs.webkit.org/show_bug.cgi?id=229459
    Reviewed by Devin Rousso.
    
    Increase timings slightly to widen the window during which we receive messages while paused in the debugger and
    also delay the echo server's response further.
    
    * http/tests/websocket/tests/hybi/inspector/echo-delayed_wsh.py:
    (web_socket_transfer_data):
    * http/tests/websocket/tests/hybi/inspector/send-and-recieve-debugger.html:
    * platform/mac/TestExpectations:
    
    
    Canonical link: https://commits.webkit.org/241011@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@281658 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/http/tests/websocket/tests/hybi/inspector/echo-delayed_wsh.py
+++ LayoutTests/http/tests/websocket/tests/hybi/inspector/echo-delayed_wsh.py
@@ -9,5 +9,5 @@ def web_socket_do_extra_handshake(request):
 def web_socket_transfer_data(request):
     # Echo message back
     message = msgutil.receive_message(request)
-    sleep(0.1)
+    sleep(0.25)
     msgutil.send_message(request, message)
