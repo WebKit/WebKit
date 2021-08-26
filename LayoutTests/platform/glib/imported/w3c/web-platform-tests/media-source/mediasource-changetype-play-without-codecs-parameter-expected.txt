WebKit/WebKit

commit d896681bcb48c2aabae5464f70ee5de81be071eb

[GStreamer][MSE] Implement the "sequence" mode in SourceBuffer for the GStreamer ports https://bugs.webkit.org/show_bug.cgi?id=210341
Reviewed by Alicia Boya Garcia.

LayoutTests/imported/w3c:

* web-platform-tests/media-source/mediasource-sourcebuffer-mode-timestamps-expected.txt: Added extra blank line in expectations, like in many other expectations files.

Source/WebCore:

Declare audio/mpeg support and handle it properly in AppendPipeline. This media type
doesn't have any container format, so an identity element is used in place of the
demuxer (dummy demuxer). As this element isn't an actual demuxer, the standard
no-more-pads signal isn't present, so its behaviour is simulated when the first buffer
is emitted by the element (detected by a probe).

A parser element is needed to process the data (mpegaudioparse for mpeg audio v1,
aacparse for mpeg audio v2 & v4). The existing createOptionalParserForFormat()
infrastructure is used for that after analyzing the caps, and an identity element is
now also used instead in the cases when a parser isn't needed. This simplifies the code
that links the pipeline elements, as now there aren't any optional (non existing)
elements in place, just GstIdentity instances there.

Still, the selection of the proper mpeg/aac parser requires precise caps. A new typefind
element is now used after the appsrc (when needed, and another identity element when
not).

Return TypeError in SourceBuffer::setMode(), instead of InvalidAccessError. The MSE spec
changed this behaviour at some point before June 2016 and the WebKit code never reflected
the change, still returning the deprecated InvalidAccessError when generate timestamps
flag equals true and new mode equals "segments".

Finally, the MediaSample::setTimestamps() method has been implemented for the GStreamer
port. It had an empty implementation and no problem had been detected before because
the "sequence" SourceBuffer mode had never been used until now.

Covered by existing tests.

LayoutTests/imported/w3c/web-platform-tests/media-source/mediasource-sourcebuffer-mode-timestamps-expected.txt
@@ -1,3 +1,4 @@
 
 PASS audio/aac : If generate timestamps flag equals true and new mode equals "segments", then throw a TypeError exception and abort these steps.
 PASS audio/mpeg : If generate timestamps flag equals true and new mode equals "segments", then throw a TypeError exception and abort these steps.
+

LayoutTests/platform/glib/TestExpectations
@@ -666,7 +666,6 @@ webkit.org/b/180803 imported/w3c/web-platform-tests/media-source/mediasource-dur
 webkit.org/b/226804 imported/w3c/web-platform-tests/media-source/mediasource-changetype-play.html [ Failure ]
 
 webkit.org/b/167108 imported/w3c/web-platform-tests/media-source/mediasource-trackdefaultlist.html [ Failure ]
-webkit.org/b/210341 imported/w3c/web-platform-tests/media-source/mediasource-sourcebuffer-mode-timestamps.html [ Failure ]
 webkit.org/b/210341 media/media-source/media-mp4-h264-sequence-mode.html [ Failure ]
 webkit.org/b/210342 imported/w3c/web-platform-tests/media-source/mediasource-sourcebuffer-trackdefaults.html [ Failure ]
 webkit.org/b/167108 imported/w3c/web-platform-tests/media-source/mediasource-trackdefault.html [ Failure ]

Source/WebCore/Modules/mediasource/SourceBuffer.cpp
@@ -1237,7 +1237,7 @@ ExceptionOr<void> SourceBuffer::setMode(AppendMode newMode)
     // 1. Let new mode equal the new value being assigned to this attribute.
     // 2. If generate timestamps flag equals true and new mode equals "segments", then throw an InvalidAccessError exception and abort these steps.
     if (m_shouldGenerateTimestamps && newMode == AppendMode::Segments)
-        return Exception { InvalidAccessError };
+        return Exception { TypeError };
 
     // 3. If this object has been removed from the sourceBuffers attribute of the parent media source, then throw an InvalidStateError exception and abort these steps.
     // 4. If the updating attribute equals true, then throw an InvalidStateError exception and abort these steps.

Source/WebCore/platform/graphics/gstreamer/GStreamerRegistryScanner.cpp
@@ -255,6 +255,8 @@ void GStreamerRegistryScanner::initializeDecoders(const GStreamerRegistryScanner
         m_decoderMimeTypeSet.add(AtomString("audio/aac"));
         m_decoderMimeTypeSet.add(AtomString("audio/mp4"));
         m_decoderMimeTypeSet.add(AtomString("audio/x-m4a"));
+        m_decoderMimeTypeSet.add(AtomString("audio/mpeg"));
+        m_decoderMimeTypeSet.add(AtomString("audio/x-mpeg"));
         m_decoderCodecMap.add(AtomString("mpeg"), false);
         m_decoderCodecMap.add(AtomString("mp4a*"), false);
     }

Source/WebCore/platform/graphics/gstreamer/MediaSampleGStreamer.cpp
@@ -192,6 +192,16 @@ void MediaSampleGStreamer::extendToTheBeginning()
     m_pts = MediaTime::zeroTime();
 }
 
+void MediaSampleGStreamer::setTimestamps(const MediaTime& presentationTime, const MediaTime& decodeTime)
+{
+    m_pts = presentationTime;
+    m_dts = decodeTime;
+    if (auto* buffer = gst_sample_get_buffer(m_sample.get())) {
+        GST_BUFFER_PTS(buffer) = toGstClockTime(m_pts);
+        GST_BUFFER_DTS(buffer) = toGstClockTime(m_dts);
+    }
+}
+
 void MediaSampleGStreamer::offsetTimestampsBy(const MediaTime& timestampOffset)
 {
     if (!timestampOffset)

Source/WebCore/platform/graphics/gstreamer/MediaSampleGStreamer.h
@@ -51,7 +51,7 @@ public:
     size_t sizeInBytes() const override { return m_size; }
     FloatSize presentationSize() const override { return m_presentationSize; }
     void offsetTimestampsBy(const MediaTime&) override;
-    void setTimestamps(const MediaTime&, const MediaTime&) override { }
+    void setTimestamps(const MediaTime&, const MediaTime&) override;
     bool isDivisable() const override { return false; }
     std::pair<RefPtr<MediaSample>, RefPtr<MediaSample>> divide(const MediaTime&, UseEndTime) override  { return { nullptr, nullptr }; }
     Ref<MediaSample> createNonDisplayingCopy() const override;

Source/WebCore/platform/graphics/gstreamer/mse/AppendPipeline.cpp
@@ -78,10 +78,9 @@ static GstPadProbeReturn appendPipelinePadProbeDebugInformation(GstPad*, GstPadP
 #if ENABLE(ENCRYPTED_MEDIA)
 static GstPadProbeReturn appendPipelineAppsinkPadEventProbe(GstPad*, GstPadProbeInfo*, struct PadProbeInformation*);
 #endif
-
 static GstPadProbeReturn appendPipelineDemuxerBlackHolePadProbe(GstPad*, GstPadProbeInfo*, gpointer);
-
 static GstPadProbeReturn matroskademuxForceSegmentStartToEqualZero(GstPad*, GstPadProbeInfo*, void*);
+static GRefPtr<GstElement> createOptionalParserForFormat(const AtomString&, const GstCaps*);
 
 // Wrapper for gst_element_set_state() that emits a critical if the state change fails or is not synchronous.
 static void assertedElementSetState(GstElement* element, GstState desiredState)
@@ -143,11 +142,18 @@ AppendPipeline::AppendPipeline(SourceBufferPrivateGStreamer& sourceBufferPrivate
 
     const String& type = m_sourceBufferPrivate.type().containerType();
     GST_DEBUG("SourceBuffer containerType: %s", type.utf8().data());
-    if (type.endsWith("mp4") || type.endsWith("aac"))
+    bool hasDemuxer = true;
+    if (type.endsWith("mp4") || type.endsWith("aac")) {
         m_demux = makeGStreamerElement("qtdemux", nullptr);
-    else if (type.endsWith("webm"))
+        m_typefind = makeGStreamerElement("identity", nullptr);
+    } else if (type.endsWith("webm")) {
         m_demux = makeGStreamerElement("matroskademux", nullptr);
-    else
+        m_typefind = makeGStreamerElement("identity", nullptr);
+    } else if (type == "audio/mpeg") {
+        m_demux = makeGStreamerElement("identity", nullptr);
+        m_typefind = makeGStreamerElement("typefind", nullptr);
+        hasDemuxer = false;
+    } else
         ASSERT_NOT_REACHED();
 
 #if !LOG_DISABLED
@@ -157,19 +163,35 @@ AppendPipeline::AppendPipeline(SourceBufferPrivateGStreamer& sourceBufferPrivate
     m_demuxerDataEnteringPadProbeInformation.probeId = gst_pad_add_probe(demuxerPad.get(), GST_PAD_PROBE_TYPE_BUFFER, reinterpret_cast<GstPadProbeCallback>(appendPipelinePadProbeDebugInformation), &m_demuxerDataEnteringPadProbeInformation, nullptr);
 #endif
 
-    // These signals won't outlive the lifetime of `this`.
-    g_signal_connect(m_demux.get(), "no-more-pads", G_CALLBACK(+[](GstElement*, AppendPipeline* appendPipeline) {
-        ASSERT(!isMainThread());
-        GST_DEBUG("Posting no-more-pads task to main thread");
-        appendPipeline->m_taskQueue.enqueueTaskAndWait<AbortableTaskQueue::Void>([appendPipeline]() {
-            appendPipeline->didReceiveInitializationSegment();
-            return AbortableTaskQueue::Void();
-        });
-    }), this);
+    if (hasDemuxer) {
+        // These signals won't outlive the lifetime of `this`.
+        g_signal_connect(m_demux.get(), "no-more-pads", G_CALLBACK(+[](GstElement*, AppendPipeline* appendPipeline) {
+            ASSERT(!isMainThread());
+            GST_DEBUG("Posting no-more-pads task to main thread");
+            appendPipeline->m_taskQueue.enqueueTaskAndWait<AbortableTaskQueue::Void>([appendPipeline]() {
+                appendPipeline->didReceiveInitializationSegment();
+                return AbortableTaskQueue::Void();
+            });
+        }), this);
+    } else {
+        GRefPtr<GstPad> identitySrcPad = adoptGRef(gst_element_get_static_pad(m_demux.get(), "src"));
+        gst_pad_add_probe(identitySrcPad.get(), GST_PAD_PROBE_TYPE_BUFFER, reinterpret_cast<GstPadProbeCallback>(
+            +[](GstPad *pad, GstPadProbeInfo*, AppendPipeline* appendPipeline) {
+                GRefPtr<GstCaps> caps = gst_pad_get_current_caps(pad);
+                if (!caps)
+                    return GST_PAD_PROBE_DROP;
+                appendPipeline->m_taskQueue.enqueueTaskAndWait<AbortableTaskQueue::Void>([appendPipeline]() {
+                    appendPipeline->didReceiveInitializationSegment();
+                    return AbortableTaskQueue::Void();
+                });
+                return GST_PAD_PROBE_REMOVE;
+            }
+        ), this, nullptr);
+    }
 
     // Add_many will take ownership of a reference. That's why we used an assignment before.
-    gst_bin_add_many(GST_BIN(m_pipeline.get()), m_appsrc.get(), m_demux.get(), nullptr);
-    gst_element_link(m_appsrc.get(), m_demux.get());
+    gst_bin_add_many(GST_BIN(m_pipeline.get()), m_appsrc.get(), m_typefind.get(), m_demux.get(), nullptr);
+    gst_element_link_many(m_appsrc.get(), m_typefind.get(), m_demux.get(), nullptr);
 
     assertedElementSetState(m_pipeline.get(), GST_STATE_PLAYING);
 }
@@ -605,19 +627,31 @@ createOptionalParserForFormat(const AtomString& trackId, const GstCaps* caps)
 {
     GstStructure* structure = gst_caps_get_structure(caps, 0);
     const char* mediaType = gst_structure_get_name(structure);
-
     GUniquePtr<char> parserName(g_strdup_printf("%s_parser", trackId.string().utf8().data()));
+    const gchar* elementClass = "identity";
 
-    if (!g_strcmp0(mediaType, "audio/x-opus")) {
-        GstElement* opusparse = makeGStreamerElement("opusparse", parserName.get());
-        return GRefPtr<GstElement>(opusparse);
+    if (!g_strcmp0(mediaType, "audio/x-opus"))
+        elementClass = "opusparse";
+    else if (!g_strcmp0(mediaType, "video/x-h264"))
+        elementClass = "h264parse";
+    else if (!g_strcmp0(mediaType, "audio/mpeg")) {
+        int mpegversion = 0;
+        gst_structure_get_int(structure, "mpegversion", &mpegversion);
+        switch (mpegversion) {
+        case 1:
+            elementClass = "mpegaudioparse";
+            break;
+        case 2:
+        case 4:
+            elementClass = "aacparse";
+            break;
+        default: {
+            GUniquePtr<char> capsString(gst_caps_to_string(caps));
+            GST_WARNING("Unsupported audio mpeg caps: %s", capsString.get());
+        }
+        }
     }
-    if (!g_strcmp0(mediaType, "video/x-h264")) {
-        GstElement* h264parse = makeGStreamerElement("h264parse", parserName.get());
-        return GRefPtr<GstElement>(h264parse);
-    }
-
-    return nullptr;
+    return GRefPtr<GstElement>(makeGStreamerElement(elementClass, parserName.get()));
 }
 
 AtomString AppendPipeline::generateTrackId(StreamType streamType, int padIndex)
@@ -757,7 +791,7 @@ void AppendPipeline::Track::initializeElements(AppendPipeline* appendPipeline, G
 
     gst_bin_add(GST_BIN(appendPipeline->pipeline()), appsink.get());
     gst_element_sync_state_with_parent(appsink.get());
-    entryPad = appsinkPad = adoptGRef(gst_element_get_static_pad(appsink.get(), "sink"));
+    appsinkPad = adoptGRef(gst_element_get_static_pad(appsink.get(), "sink"));
 
 #if !LOG_DISABLED
     appsinkDataEnteringPadProbeInformation.appendPipeline = appendPipeline;
@@ -774,13 +808,13 @@ void AppendPipeline::Track::initializeElements(AppendPipeline* appendPipeline, G
     // Some audio files unhelpfully omit the duration of frames in the container. We need to parse
     // the contained audio streams in order to know the duration of the frames.
     // This is known to be an issue with YouTube WebM files containing Opus audio as of YTTV2018.
-    if ((parser = createOptionalParserForFormat(trackId, caps.get()))) {
-        gst_bin_add(bin, parser.get());
-        gst_element_sync_state_with_parent(parser.get());
-        gst_element_link(parser.get(), appsink.get());
-        ASSERT(GST_PAD_IS_LINKED(appsinkPad.get()));
-        entryPad = adoptGRef(gst_element_get_static_pad(parser.get(), "sink"));
-    }
+    // If no parser is needed, a GstIdentity element will be created instead.
+    parser = createOptionalParserForFormat(trackId, caps.get());
+    gst_bin_add(bin, parser.get());
+    gst_element_sync_state_with_parent(parser.get());
+    gst_element_link(parser.get(), appsink.get());
+    ASSERT(GST_PAD_IS_LINKED(appsinkPad.get()));
+    entryPad = adoptGRef(gst_element_get_static_pad(parser.get(), "sink"));
 }
 
 void AppendPipeline::hookTrackEvents(Track& track)
@@ -841,6 +875,8 @@ const char* AppendPipeline::streamTypeToString(StreamType streamType)
         return "Invalid";
     case StreamType::Unknown:
         return "Unknown";
+    default:
+        return "(Unsupported stream type)";
     }
 }
 #endif

Source/WebCore/platform/graphics/gstreamer/mse/AppendPipeline.h
@@ -81,9 +81,10 @@ private:
         GRefPtr<GstCaps> caps;
         FloatSize presentationSize;
 
-        GRefPtr<GstPad> entryPad; // Sink pad of the parser (if any) or the appsink.
-        GRefPtr<GstElement> parser; // Optional (needed by some formats).
+        // Needed by some formats. To simplify the code, parser can be a GstIdentity when not needed.
+        GRefPtr<GstElement> parser;
         GRefPtr<GstElement> appsink;
+        GRefPtr<GstPad> entryPad; // Sink pad of the parser/GstIdentity.
         GRefPtr<GstPad> appsinkPad;
 
         RefPtr<WebCore::TrackPrivateBase> webKitTrack;
@@ -149,9 +150,11 @@ private:
     MediaPlayerPrivateGStreamerMSE* m_playerPrivate;
 
     MediaTime m_initialDuration;
-    GRefPtr<GstElement> m_appsrc;
     GRefPtr<GstElement> m_pipeline;
     GRefPtr<GstBus> m_bus;
+    GRefPtr<GstElement> m_appsrc;
+    // To simplify the code, mtypefind and m_demux can be a GstIdentity when not needed.
+    GRefPtr<GstElement> m_typefind;
     GRefPtr<GstElement> m_demux;
 
     Vector<std::unique_ptr<Track>> m_tracks;

commit 7c2584fe98e37240d81453c7275b65dd08e6f7d0

[GLIB][Media] imported/w3c/web-platform-tests/media-source/mediasource-changetype-play-without-codecs-parameter.html fails after r281617 https://bugs.webkit.org/show_bug.cgi?id=229548
Unreviewed test gardening.

The patch from https://bugs.webkit.org/show_bug.cgi?id=210341 caused a regression in
mediasource-changetype-play-without-codecs-parameter.html. The test tries to change the
media type and codecs of a SourceBuffer on the fly. That operation is unsupported in glib
ports, so there is a custom expectation file that expects failures on the transitions
between supported types. Bug 210341 adds audio/mpeg (without container) as a new supported
format, so new "failures" appear in the test. This patch adds expectations for those new
failures.


* platform/glib/imported/w3c/web-platform-tests/media-source/mediasource-changetype-play-without-codecs-parameter-expected.txt:


Canonical link: https://commits.webkit.org/240978@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@281622 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/glib/imported/w3c/web-platform-tests/media-source/mediasource-changetype-play-without-codecs-parameter-expected.txt
@@ -2,8 +2,13 @@
 PASS Check if browser supports enough test media types
 FAIL Test audio-only changeType for audio/webm; codecs="vorbis" <-> audio/webm; codecs="vorbis" (using types without codecs parameters) The operation is not supported.
 FAIL Test audio-only changeType for audio/webm; codecs="vorbis" <-> audio/mp4; codecs="mp4a.40.2" (using types without codecs parameters) The operation is not supported.
+FAIL Test audio-only changeType for audio/webm; codecs="vorbis" <-> audio/mpeg (using types without codecs parameters) The operation is not supported.
 FAIL Test audio-only changeType for audio/mp4; codecs="mp4a.40.2" <-> audio/webm; codecs="vorbis" (using types without codecs parameters) The operation is not supported.
 FAIL Test audio-only changeType for audio/mp4; codecs="mp4a.40.2" <-> audio/mp4; codecs="mp4a.40.2" (using types without codecs parameters) The operation is not supported.
+FAIL Test audio-only changeType for audio/mp4; codecs="mp4a.40.2" <-> audio/mpeg (using types without codecs parameters) The operation is not supported.
+FAIL Test audio-only changeType for audio/mpeg <-> audio/webm; codecs="vorbis" (using types without codecs parameters) The operation is not supported.
+FAIL Test audio-only changeType for audio/mpeg <-> audio/mp4; codecs="mp4a.40.2" (using types without codecs parameters) The operation is not supported.
+FAIL Test audio-only changeType for audio/mpeg <-> audio/mpeg (using types without codecs parameters) The operation is not supported.
 FAIL Test video-only changeType for video/webm; codecs="vp8" <-> video/webm; codecs="vp8" (using types without codecs parameters) The operation is not supported.
 FAIL Test video-only changeType for video/webm; codecs="vp8" <-> video/webm; codecs="vp9" (using types without codecs parameters) The operation is not supported.
 FAIL Test video-only changeType for video/webm; codecs="vp8" <-> video/mp4; codecs="avc1.4D4001" (using types without codecs parameters) The operation is not supported.