1e9a33c0efd9a07c7c9f667efc9d714c0216e384
Carlos Alberto Lopez Perez <clopez@igalia.com>
2021-09-07 14:13:50 +0000

    [GTK] The Xvfb display server may fail to start sometimes causing tests to randomly crash (v3) https://bugs.webkit.org/show_bug.cgi?id=229758
    Reviewed by Philippe Normand.
    
    Add a new function in XvfbDriver() to ensure that the display server
    at a given display_id is replying as expected. Ask it for the screen
    size at monitor 0 and compare the result with the one we expect to
    have inside Xvfb. For doing this check a external python program is
    called which does the query using GTK. Using a external program is
    more robust against possible failures calling into GTK and also will
    allow re-using this program also to check that the weston server is
    also replying as expected for the weston driver (on a future patch).
    
    If the Xvfb driver is not replying as expected then restart it and
    try again, up to 3 retries.
    
    Use this also on the weston driver to check that the Xvfb driver is
    ready.
    
    The code is both compatible with python2 and python3, when running on
    python2 it will try first to use subprocess32 if available, otherwise
    will use standard python2 subprocess without using the timeout feature.
    
    On this v3 fix an error that caused that the subprocess stderr was
    redirected to stdout by mistake.
    
    * Scripts/webkitpy/common/system/executive_mock.py:
    (MockProcess.__init__):
    (MockProcess.communicate):
    * Scripts/webkitpy/port/westondriver.py:
    (WestonDriver._setup_environ_for_test):
    * Scripts/webkitpy/port/westondriver_unittest.py:
    (WestonXvfbDriverDisplayTest._xvfb_check_if_ready):
    * Scripts/webkitpy/port/xvfbdriver.py:
    (XvfbDriver):
    (XvfbDriver.__init__):
    (XvfbDriver.check_driver):
    (XvfbDriver._xvfb_run):
    (XvfbDriver._xvfb_screen_size):
    (XvfbDriver._xvfb_stop):
    (XvfbDriver._xvfb_check_if_ready):
    (XvfbDriver._setup_environ_for_test):
    (XvfbDriver.has_crashed):
    (XvfbDriver.stop):
    * Scripts/webkitpy/port/xvfbdriver_unittest.py:
    (XvfbDriverTest.make_driver):
    (XvfbDriverTest.assertDriverStartSuccessful):
    (XvfbDriverTest.test_xvfb_start_and_ready):
    (XvfbDriverTest.test_xvfb_start_arbitrary_worker_number):
    (XvfbDriverTest.test_xvfb_not_replying):
    * gtk/print-screen-size: Added.
    
    Canonical link: https://commits.webkit.org/241382@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@282082 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitpy/port/xvfbdriver.py
+++ Tools/Scripts/webkitpy/port/xvfbdriver.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2010 Google Inc. All rights reserved.
-# Copyright (C) 2014 Igalia S.L.
+# Copyright (C) 2014-2021 Igalia S.L.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -33,6 +33,14 @@ import sys
 import re
 import time
 
+if sys.version_info[0] < 3:
+    try:
+        import subprocess32 as subprocess
+    except ImportError:
+        import subprocess
+else:
+    import subprocess
+
 from webkitcorepy import string_utils
 from webkitpy.port.server_process import ServerProcess
 from webkitpy.port.driver import Driver
@@ -41,6 +49,13 @@ _log = logging.getLogger(__name__)
 
 
 class XvfbDriver(Driver):
+
+    def __init__(self, *args, **kwargs):
+        Driver.__init__(self, *args, **kwargs)
+        self._xvfb_process = None
+        self._current_retry_start_xvfb = 0
+        self._print_screen_size_process_for_testing = None  # required for unit tests
+
     @staticmethod
     def check_driver(port):
         xvfb_findcmd = ['which', 'Xvfb']
@@ -48,7 +63,7 @@ class XvfbDriver(Driver):
                 xvfb_findcmd = port._jhbuild_wrapper + xvfb_findcmd
         xvfb_found = port.host.executive.run_command(xvfb_findcmd, return_exit_code=True) == 0
         if not xvfb_found:
-            _log.error("No Xvfb found. Cannot run layout tests.")
+            _log.error('No Xvfb found. Cannot run layout tests.')
         return xvfb_found
 
     def _xvfb_pipe(self):
@@ -86,20 +101,77 @@ class XvfbDriver(Driver):
 
     def _xvfb_run(self, environment):
         read_fd, write_fd = self._xvfb_pipe()
-        run_xvfb = ["Xvfb", "-displayfd", str(write_fd), "-screen",  "0", "1024x768x%s" % self._xvfb_screen_depth(), "-nolisten", "tcp"]
+        run_xvfb = ['Xvfb', '-displayfd', str(write_fd), '-nolisten', 'tcp',
+                    '+extension', 'GLX', '-ac', '-screen', '0',
+                    '%sx%s' % (self._xvfb_screen_size(), self._xvfb_screen_depth())]
         if self._port._should_use_jhbuild():
             run_xvfb = self._port._jhbuild_wrapper + run_xvfb
-        with open(os.devnull, 'w') as devnull:
-            # python3 will try to close the file descriptors by default
-            self._xvfb_process = self._port.host.executive.popen(run_xvfb, stderr=devnull, env=environment, close_fds=False)
-            display_id = self._xvfb_read_display_id(read_fd)
-
+        self._xvfb_process = self._port.host.executive.popen(run_xvfb, stdout=self._port.host.executive.PIPE, stderr=self._port.host.executive.PIPE, env=environment, close_fds=False)
+        display_id = self._xvfb_read_display_id(read_fd)
         self._xvfb_close_pipe((read_fd, write_fd))
-
         return display_id
 
     def _xvfb_screen_depth(self):
         return os.environ.get('XVFB_SCREEN_DEPTH', '24')
+
+    def _xvfb_screen_size(self):
+        return os.environ.get('XVFB_SCREEN_SIZE', '1024x768')
+
+    def _xvfb_stop(self):
+        if self._xvfb_process:
+            self._port.host.executive.kill_process(self._xvfb_process.pid)
+            self._xvfb_process = None
+
+    def _xvfb_check_if_ready(self, display_id):
+        environment_print_screen_size_process = super(XvfbDriver, self)._setup_environ_for_test()
+        environment_print_screen_size_process['DISPLAY'] = ':%d' % display_id
+        environment_print_screen_size_process['GDK_BACKEND'] = 'x11'
+        waited_seconds_for_xvfb_ready = 0
+        xvfb_server_replying_as_expected = False
+        while True:
+            timeout_expired = False
+            query_failed = False
+            print_screen_size_process = self._print_screen_size_process_for_testing if self._print_screen_size_process_for_testing else \
+                subprocess.Popen([self._port.path_from_webkit_base('Tools', 'gtk', 'print-screen-size')],
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=environment_print_screen_size_process)
+            # Python2 standard subprocess don't allows setting a timeout
+            if not hasattr(subprocess, 'TimeoutExpired'):
+                stdout, stderr = print_screen_size_process.communicate()
+            else:
+                try:
+                    stdout, stderr = print_screen_size_process.communicate(timeout=2)
+                except subprocess.TimeoutExpired:
+                    _log.debug('Timeout expired trying to query the Xvfb display server.')
+                    timeout_expired = True
+                    print_screen_size_process.kill()
+                    stdout, stderr = print_screen_size_process.communicate()
+                    waited_seconds_for_xvfb_ready += 2
+
+            if not timeout_expired:
+                if print_screen_size_process.returncode == 0:
+                    queried_screen_size = stdout.decode('UTF-8').strip()
+                    if queried_screen_size == self._xvfb_screen_size():
+                        xvfb_server_replying_as_expected = True
+                        _log.debug('The Xvfb display server ":%d" is ready and replying as expected.' % display_id)
+                        break
+                    else:
+                        _log.warning('The queried Xvfb screen size "%s" does not match the expectation "%s".' % (queried_screen_size, self._xvfb_screen_size()))
+                else:
+                    _log.warning('The print-screen-size tool returned non-zero status. stdout is "%s" and stderr is "%s"' % (stdout, stderr))
+                    query_failed = True
+            if timeout_expired or query_failed:
+                if self._xvfb_process.poll():
+                    xvfb_stdout, xvfb_stderr = self._xvfb_process.communicate()
+                    _log.error('The Xvfb display server has exited unexpectedly with a return code of %s. stdout is "%s" and stderr is "%s"' % (self._xvfb_process.poll(), xvfb_stdout, xvfb_stderr))
+                    break
+            if waited_seconds_for_xvfb_ready > 5:
+                _log.error('Timeout reached meanwhile waiting for the Xvfb display server to be ready')
+                break
+            _log.debug('Waiting for Xvfb display server to be ready.')
+            if not self._print_screen_size_process_for_testing:
+                time.sleep(1)  # only wait when not running unit tests
+            waited_seconds_for_xvfb_ready += 1
+        return xvfb_server_replying_as_expected
 
     def _setup_environ_for_test(self):
         port_server_environment = self._port.setup_environ_for_server(self._server_name)
@@ -111,10 +183,25 @@ class XvfbDriver(Driver):
         driver_environment['UNDER_XVFB'] = 'yes'
         driver_environment['GDK_BACKEND'] = 'x11'
         driver_environment['LOCAL_RESOURCE_ROOT'] = self._port.layout_tests_dir()
+
+        # Ensure that Xvfb is ready and replying and expected before continuing, give it 3 tries.
+        if not self._xvfb_check_if_ready(display_id):
+            self._current_retry_start_xvfb += 1
+            if self._current_retry_start_xvfb > 3:
+                _log.error('Failed to start Xvfb display server ... giving up after 3 retries.')
+                raise RuntimeError('Unable to start Xvfb display server')
+            _log.error('Failed to start Xvfb display server ... retrying [ %s of 3 ].' % self._current_retry_start_xvfb)
+            return self._setup_environ_for_test()
+
         return driver_environment
+
+    def has_crashed(self):
+        if self._xvfb_process and self._xvfb_process.poll():
+            self._crashed_process_name = 'Xvfb'
+            self._crashed_pid = self._xvfb_process.pid
+            return True
+        return super(XvfbDriver, self).has_crashed()
 
     def stop(self):
         super(XvfbDriver, self).stop()
-        if getattr(self, '_xvfb_process', None):
-            self._port.host.executive.kill_process(self._xvfb_process.pid)
-            self._xvfb_process = None
+        self._xvfb_stop()
