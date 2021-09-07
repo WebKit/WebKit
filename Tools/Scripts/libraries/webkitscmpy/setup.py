87b77cb9773c0b96398cc706bb8e57563dfa305e
Jonathan Bedard <jbedard@apple.com>
2021-09-07 22:04:55 +0000

    [git-webkit] Support callbacks as main arguments https://bugs.webkit.org/show_bug.cgi?id=229739 <rdar://problem/82597266>
    Reviewed by Dewei Zhu.
    
    * Scripts/git-webkit:
    (is_webkit_filter): Return item if repository is Webkit.
    * Scripts/libraries/webkitscmpy/setup.py: Bump version.
    * Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py: Ditto.
    * Scripts/libraries/webkitscmpy/webkitscmpy/program/__init__.py:
    (main): Pass repository to contributors, identifier_template and subversion
    to dynamically generate values if those arguments are callable.
    
    
    Canonical link: https://commits.webkit.org/241406@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@282108 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/libraries/webkitscmpy/setup.py
+++ Tools/Scripts/libraries/webkitscmpy/setup.py
@@ -29,7 +29,7 @@ def readme():
 
 setup(
     name='webkitscmpy',
-    version='2.1.1',
+    version='2.1.2',
     description='Library designed to interact with git and svn repositories.',
     long_description=readme(),
     classifiers=[
