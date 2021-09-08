292ba371d52e1291c750185a82c7ce04e231e508
Cameron McCormack <heycam@apple.com>
2021-09-08 00:08:35 +0000

    Ensure ImageBuffers are destroyed on the main thread https://bugs.webkit.org/show_bug.cgi?id=229906 <rdar://problem/82745925>
    Reviewed by Ryosuke Niwa.
    
    * platform/graphics/ImageBuffer.h:
    
    
    Canonical link: https://commits.webkit.org/241414@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@282117 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/ImageBuffer.h
+++ Source/WebCore/platform/graphics/ImageBuffer.h
@@ -42,7 +42,7 @@ class DrawingContext;
 struct ItemBufferHandle;
 }
 
-class ImageBuffer : public ThreadSafeRefCounted<ImageBuffer>, public CanMakeWeakPtr<ImageBuffer> {
+class ImageBuffer : public ThreadSafeRefCounted<ImageBuffer, WTF::DestructionThread::Main>, public CanMakeWeakPtr<ImageBuffer> {
 public:
     // Will return a null pointer on allocation failure.
     WEBCORE_EXPORT static RefPtr<ImageBuffer> create(const FloatSize&, RenderingMode, ShouldUseDisplayList, RenderingPurpose, float resolutionScale, const DestinationColorSpace&, PixelFormat, const HostWindow* = nullptr);
