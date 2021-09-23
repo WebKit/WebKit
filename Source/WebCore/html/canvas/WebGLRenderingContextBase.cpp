e0302874b705d4ccb69ee014fcdd05ec56f80adb
Alex Christensen <achristensen@webkit.org>
2021-09-23 05:13:37 +0000

    Cherry-pick r281613. rdar://problem/83429969
        Crash in GraphicsContextGLOpenGL::reshapeDisplayBufferBacking
        https://bugs.webkit.org/show_bug.cgi?id=229309
    
        Patch by Alex Christensen <achristensen@webkit.org> on 2021-08-25
        Reviewed by Myles C. Maxfield.
    
        Source/WebCore:
    
        Test: fast/canvas/xr-compatible-crash.html
    
        Null check m_swapChain because reshapeDisplayBufferBacking can be called before it is set.
    
        * html/canvas/WebGLRenderingContextBase.cpp:
        (WebCore::WebGLRenderingContextBase::addContextObject):
        * platform/graphics/angle/GraphicsContextGLANGLE.cpp:
        (WebCore::GraphicsContextGLOpenGL::reshapeFBOs):
        * platform/graphics/cocoa/GraphicsContextGLOpenGLCocoa.mm:
        (WebCore::GraphicsContextGLOpenGL::reshapeDisplayBufferBacking):
    
        LayoutTests:
    
        * fast/canvas/xr-compatible-crash.html: Added.
        * fast/canvas/xr-compatible-crash-expected.txt: Added.
    
        Canonical link: https://commits.webkit.org/240969@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@281613 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/240672.96@safari-612-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-612-branch@282929 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/canvas/WebGLRenderingContextBase.cpp
+++ Source/WebCore/html/canvas/WebGLRenderingContextBase.cpp
@@ -6336,7 +6336,6 @@ void WebGLRenderingContextBase::addContextObject(WebGLContextObject& object)
     if (m_isPendingPolicyResolution)
         return;
 
-    ASSERT(!isContextLost());
     m_contextObjects.add(&object);
 }
 
