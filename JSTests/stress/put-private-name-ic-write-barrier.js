4b1007d0d71442579ab240307427c4e54a75ca6d
Justin Michaud <justin_michaud@apple.com>
2021-09-23 05:14:20 +0000

    Cherry-pick r282663. rdar://problem/83429716
        PutByVal and PutPrivateName ICs should emit a write barrier if a butterfly might be allocated
        https://bugs.webkit.org/show_bug.cgi?id=230378
    
        Reviewed by Yusuke Suzuki.
    
        Right now, PutByVal and PutPrivateName check the value type to determine
        if a write barrier is needed. For example, putting a primitive is considered
        to not require a write barrier. This makes sense, except for the case when we
        might allocate or re-allocate a butterfly in the IC. This does not emit a write
        barrier, and so the GC might miss the new butterfly. That is somewhat undesirable.
        This is a temporary conservative fix. If we don't write to the butterfly pointer,
        then we still don't need a write barrier; this work is captured by
        https://bugs.webkit.org/show_bug.cgi?id=230377
    
        * dfg/DFGStoreBarrierInsertionPhase.cpp:
    
        Canonical link: https://commits.webkit.org/241804@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@282663 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/240672.108@safari-612-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-612-branch@282941 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ JSTests/stress/put-private-name-ic-write-barrier.js
@@ -0,0 +1,29 @@
+//@ runDefault("--collectContinuously=1", "--usePolyvariantDevirtualization=0", "--forceDebuggerBytecodeGeneration=1", "--verifyGC=1")
+// UsePolyvariantDevirtualization gives us a PutPrivateName (not byID) while still letting us generate an IC with only one AccessCase
+// DebuggerBytecodeGeneration seems to give the GC more time to interrupt the put because it forces reads from the stack 
+
+function PutPrivateNameIC() {
+    let leak = []
+
+    class A {
+        constructor() {
+            this.a = 0
+        }
+    }
+    noInline(A)
+
+    class B extends A {
+        #b
+        #c
+    }
+    noInline(B)
+
+    for (let i = 0; i < 1000000; ++i) {
+        let b1 = new B
+        let b2 = new B
+        let b3 = new B
+        leak.push(b1, b2, b3)
+    }
+}
+noInline(PutPrivateNameIC)
+PutPrivateNameIC()
