WebKit/WebKit

commit e30e21856202b1707f9b9b7f02fb81ea22cc66dd

Replace the confusing isPinnedForScrollDelta() logic with code that uses BoxSide https://bugs.webkit.org/show_bug.cgi?id=231004
Reviewed by Tim Horton.

isPinnedForScrollDelta()/isPinnedForScrollDeltaOnAxis() are ambiguous because it's
easy to interpret them as looking at the magnitude of the delta to determine whether
the delta will cause stretching on an edge. However, they don't do this; they simply
use the delta to choose which edge to look at, and the "is pinned" refers to whether
the scroller is already scrolled to that edge or stretching at that edge.

To reduce ambiguity, rewrite the code in terms of BoxSides, separating the code
that determines which BoxSide to look at for a given event delta from the code that
computed pinned state. It's now clearer that it's the caller's responsibility to
do "dominant axis" delta transformations too.

* page/EventHandler.cpp:
(WebCore::EventHandler::scrollableAreaCanHandleEvent):
* page/scrolling/ScrollingTree.cpp:
(WebCore::ScrollingTree::mainFrameCanRubberBandOnSide):
(WebCore::ScrollingTree::mainFrameCanRubberBandInDirection): Deleted.
* page/scrolling/ScrollingTree.h:
* page/scrolling/mac/ScrollingTreeScrollingNodeDelegateMac.h:
* page/scrolling/mac/ScrollingTreeScrollingNodeDelegateMac.mm:
(WebCore::ScrollingTreeScrollingNodeDelegateMac::allowsHorizontalStretching const):
(WebCore::ScrollingTreeScrollingNodeDelegateMac::allowsVerticalStretching const):
(WebCore::ScrollingTreeScrollingNodeDelegateMac::isPinnedOnSide const): Remove the old "scrollOffsetLimit" threshold. Our scroll offsets
are currently all integral so I don't think this did anything.
(WebCore::ScrollingTreeScrollingNodeDelegateMac::shouldRubberBandOnSide const):
(WebCore::ScrollingTreeScrollingNodeDelegateMac::isPinnedForScrollDeltaOnAxis const): Deleted.
(WebCore::ScrollingTreeScrollingNodeDelegateMac::isPinnedForScrollDelta const): Deleted.
(WebCore::ScrollingTreeScrollingNodeDelegateMac::shouldRubberBandInDirection const): Deleted.

Source/WebCore/platform/mac/ScrollingEffectsController.mm
@@ -83,13 +83,44 @@ void ScrollingEffectsController::stopAllTimers()
 #endif
 }
 
-
-static FloatSize convertToProminentAxisFavoringVertical(FloatSize delta)
+static ScrollEventAxis dominantAxisFavoringVertical(FloatSize delta)
 {
     if (fabsf(delta.height()) >= fabsf(delta.width()))
-        return { 0, delta.height() };
+        return ScrollEventAxis::Vertical;
 
-    return { delta.width(), 0 };
+    return ScrollEventAxis::Horizontal;
+}
+
+static FloatSize deltaAlignedToAxis(FloatSize delta, ScrollEventAxis axis)
+{
+    switch (axis) {
+    case ScrollEventAxis::Horizontal: return FloatSize { delta.width(), 0 };
+    case ScrollEventAxis::Vertical: return FloatSize { 0, delta.height() };
+    }
+
+    return { };
+}
+
+static FloatSize deltaAlignedToDominantAxis(FloatSize delta)
+{
+    auto dominantAxis = dominantAxisFavoringVertical(delta);
+    return deltaAlignedToAxis(delta, dominantAxis);
+}
+
+static std::optional<BoxSide> affectedSideOnDominantAxis(FloatSize delta)
+{
+    auto dominantAxis = dominantAxisFavoringVertical(delta);
+    return ScrollableArea::targetSideForScrollDelta(delta, dominantAxis);
+}
+
+static bool isHorizontalSide(std::optional<BoxSide> side)
+{
+    return side && (*side == BoxSide::Left || *side == BoxSide::Right);
+}
+
+static bool isVerticalSide(std::optional<BoxSide> side)
+{
+    return side && (*side == BoxSide::Top || *side == BoxSide::Bottom);
 }
 
 bool ScrollingEffectsController::handleWheelEvent(const PlatformWheelEvent& wheelEvent)
@@ -102,12 +133,12 @@ bool ScrollingEffectsController::handleWheelEvent(const PlatformWheelEvent& whee
 
     if (wheelEvent.phase() == PlatformWheelEventPhase::Began) {
         // FIXME: Trying to decide if a gesture is horizontal or vertical at the "began" phase is very error-prone.
-        auto direction = directionFromEvent(wheelEvent, ScrollEventAxis::Horizontal);
-        if (direction && m_client.isPinnedForScrollDelta(FloatSize(-wheelEvent.deltaX(), 0)) && !shouldRubberBandInDirection(direction.value()))
+        auto horizontalSide = ScrollableArea::targetSideForScrollDelta(-wheelEvent.delta(), ScrollEventAxis::Horizontal);
+        if (horizontalSide && m_client.isPinnedOnSide(*horizontalSide) && !shouldRubberBandOnSide(*horizontalSide))
             return false;
 
-        direction = directionFromEvent(wheelEvent, ScrollEventAxis::Vertical);
-        if (direction && m_client.isPinnedForScrollDelta(FloatSize(0, -wheelEvent.deltaY())) && !shouldRubberBandInDirection(direction.value()))
+        auto verticalSide = ScrollableArea::targetSideForScrollDelta(-wheelEvent.delta(), ScrollEventAxis::Vertical);
+        if (verticalSide && m_client.isPinnedOnSide(*verticalSide) && !shouldRubberBandOnSide(*verticalSide))
             return false;
 
         m_momentumScrollInProgress = false;
@@ -155,7 +186,9 @@ bool ScrollingEffectsController::handleWheelEvent(const PlatformWheelEvent& whee
     auto eventCoalescedDelta = (isVerticallyStretched || isHorizontallyStretched) ? -wheelEvent.unacceleratedScrollingDelta() : -wheelEvent.delta();
     delta += eventCoalescedDelta;
 
-    delta = convertToProminentAxisFavoringVertical(delta);
+    // FIXME: All of the code below could be simplified since predominantAxis is known, and we zero out the delta on the other axis.
+    auto affectedSide = affectedSideOnDominantAxis(delta);
+
     float deltaX = delta.width();
     float deltaY = delta.height();
 
@@ -177,7 +210,7 @@ bool ScrollingEffectsController::handleWheelEvent(const PlatformWheelEvent& whee
         }
 
         if (isVerticallyStretched) {
-            if (!isHorizontallyStretched && m_client.isPinnedForScrollDelta(FloatSize(deltaX, 0))) {
+            if (!isHorizontallyStretched && isHorizontalSide(affectedSide) && m_client.isPinnedOnSide(*affectedSide)) {
                 // Stretching only in the vertical.
                 if (deltaY && (fabsf(deltaX / deltaY) < rubberbandDirectionLockStretchRatio))
                     deltaX = 0;
@@ -189,7 +222,7 @@ bool ScrollingEffectsController::handleWheelEvent(const PlatformWheelEvent& whee
             }
         } else if (isHorizontallyStretched) {
             // Stretching only in the horizontal.
-            if (m_client.isPinnedForScrollDelta(FloatSize(0, deltaY))) {
+            if (isVerticalSide(affectedSide) && m_client.isPinnedOnSide(*affectedSide)) {
                 if (deltaX && (fabsf(deltaY / deltaX) < rubberbandDirectionLockStretchRatio))
                     deltaY = 0;
                 else if (fabsf(deltaY) < rubberbandMinimumRequiredDeltaBeforeStretch) {
@@ -200,7 +233,7 @@ bool ScrollingEffectsController::handleWheelEvent(const PlatformWheelEvent& whee
             }
         } else {
             // Not stretching at all yet.
-            if (m_client.isPinnedForScrollDelta(FloatSize(deltaX, deltaY))) {
+            if (affectedSide && m_client.isPinnedOnSide(*affectedSide)) {
                 if (fabsf(deltaY) >= fabsf(deltaX)) {
                     if (fabsf(deltaX) < rubberbandMinimumRequiredDeltaBeforeStretch) {
                         m_unappliedOverscrollDelta.expand(deltaX, 0);
@@ -233,12 +266,13 @@ bool ScrollingEffectsController::handleWheelEvent(const PlatformWheelEvent& whee
                 m_client.immediateScrollBy(FloatSize(deltaX, 0));
             }
         } else {
+            affectedSide = affectedSideOnDominantAxis({ deltaX, deltaY });
             if (deltaX) {
                 if (!m_client.allowsHorizontalStretching(wheelEvent)) {
                     deltaX = 0;
                     eventCoalescedDelta.setWidth(0);
                     handled = false;
-                } else if (!isHorizontallyStretched && !m_client.isPinnedForScrollDelta(FloatSize(deltaX, 0))) {
+                } else if (!isHorizontallyStretched && !m_client.isPinnedOnSide(*affectedSide)) {
                     deltaX *= scrollWheelMultiplier();
 
                     m_client.immediateScrollByWithoutContentEdgeConstraints(FloatSize(deltaX, 0));
@@ -251,7 +285,7 @@ bool ScrollingEffectsController::handleWheelEvent(const PlatformWheelEvent& whee
                     deltaY = 0;
                     eventCoalescedDelta.setHeight(0);
                     handled = false;
-                } else if (!isVerticallyStretched && !m_client.isPinnedForScrollDelta(FloatSize(0, deltaY))) {
+                } else if (!isVerticallyStretched && !m_client.isPinnedOnSide(*affectedSide)) {
                     deltaY *= scrollWheelMultiplier();
 
                     m_client.immediateScrollByWithoutContentEdgeConstraints(FloatSize(0, deltaY));
@@ -262,7 +296,8 @@ bool ScrollingEffectsController::handleWheelEvent(const PlatformWheelEvent& whee
             IntSize stretchAmount = m_client.stretchAmount();
 
             if (m_momentumScrollInProgress) {
-                if ((m_client.isPinnedForScrollDelta(eventCoalescedDelta) || eventCoalescedDelta.isZero()) && m_lastMomentumScrollTimestamp) {
+                auto sideAffectedByEventDelta = affectedSideOnDominantAxis(eventCoalescedDelta);
+                if ((!sideAffectedByEventDelta || m_client.isPinnedOnSide(*sideAffectedByEventDelta)) && m_lastMomentumScrollTimestamp) {
                     m_ignoreMomentumScrolls = true;
                     m_momentumScrollInProgress = false;
                     snapRubberBand();
@@ -293,53 +328,7 @@ bool ScrollingEffectsController::handleWheelEvent(const PlatformWheelEvent& whee
 
 FloatSize ScrollingEffectsController::wheelDeltaBiasingTowardsVertical(const PlatformWheelEvent& wheelEvent)
 {
-    return convertToProminentAxisFavoringVertical(wheelEvent.delta());
-}
-
-std::optional<ScrollDirection> ScrollingEffectsController::directionFromEvent(const PlatformWheelEvent& wheelEvent, std::optional<ScrollEventAxis> axis, WheelAxisBias bias)
-{
-    // FIXME: It's impossible to infer direction from a single event, since the start of a gesture is either zero or
-    // has small deltas on both axes.
-
-    auto wheelDelta = FloatSize { wheelEvent.deltaX(), wheelEvent.deltaY() };
-    if (bias == WheelAxisBias::Vertical)
-        wheelDelta = wheelDeltaBiasingTowardsVertical(wheelEvent);
-
-    if (axis) {
-        switch (axis.value()) {
-        case ScrollEventAxis::Vertical:
-            if (wheelDelta.height() < 0)
-                return ScrollDown;
-
-            if (wheelDelta.height() > 0)
-                return ScrollUp;
-            break;
-
-        case ScrollEventAxis::Horizontal:
-            if (wheelDelta.width() > 0)
-                return ScrollLeft;
-
-            if (wheelDelta.width() < 0)
-                return ScrollRight;
-        }
-
-        return std::nullopt;
-    }
-
-    // Check Y first because vertical scrolling dominates.
-    if (wheelDelta.height() < 0)
-        return ScrollDown;
-
-    if (wheelDelta.height() > 0)
-        return ScrollUp;
-
-    if (wheelDelta.width() > 0)
-        return ScrollLeft;
-
-    if (wheelDelta.width() < 0)
-        return ScrollRight;
-
-    return std::nullopt;
+    return deltaAlignedToDominantAxis(wheelEvent.delta());
 }
 
 static inline float roundTowardZero(float num)
@@ -493,18 +482,9 @@ void ScrollingEffectsController::snapRubberBand()
     startRubberbandAnimation();
 }
 
-bool ScrollingEffectsController::shouldRubberBandInHorizontalDirection(const PlatformWheelEvent& wheelEvent) const
+bool ScrollingEffectsController::shouldRubberBandOnSide(BoxSide side) const
 {
-    auto direction = directionFromEvent(wheelEvent, ScrollEventAxis::Horizontal);
-    if (direction)
-        return shouldRubberBandInDirection(direction.value());
-
-    return true;
-}
-
-bool ScrollingEffectsController::shouldRubberBandInDirection(ScrollDirection direction) const
-{
-    return m_client.shouldRubberBandInDirection(direction);
+    return m_client.shouldRubberBandOnSide(side);
 }
 
 bool ScrollingEffectsController::isRubberBandInProgressInternal() const

Source/WebCore/page/EventHandler.cpp
@@ -98,6 +98,7 @@
 #include "ScrollAnimator.h"
 #include "ScrollLatchingController.h"
 #include "Scrollbar.h"
+#include "ScrollingEffectsController.h"
 #include "SelectionRestorationMode.h"
 #include "Settings.h"
 #include "ShadowRoot.h"
@@ -3103,10 +3104,12 @@ bool EventHandler::scrollableAreaCanHandleEvent(const PlatformWheelEvent& wheelE
     auto biasedDelta = wheelEvent.delta();
 #endif
 
-    if (biasedDelta.height() && !scrollableArea.isPinnedForScrollDeltaOnAxis(-biasedDelta.height(), ScrollEventAxis::Vertical))
+    auto verticalSide = ScrollableArea::targetSideForScrollDelta(-biasedDelta, ScrollEventAxis::Vertical);
+    if (verticalSide && !scrollableArea.isPinnedOnSide(*verticalSide))
         return true;
 
-    if (biasedDelta.width() && !scrollableArea.isPinnedForScrollDeltaOnAxis(-biasedDelta.width(), ScrollEventAxis::Horizontal))
+    auto horizontalSide = ScrollableArea::targetSideForScrollDelta(-biasedDelta, ScrollEventAxis::Horizontal);
+    if (horizontalSide && !scrollableArea.isPinnedOnSide(*horizontalSide))
         return true;
 
     return false;

Source/WebCore/page/scrolling/ScrollingTree.cpp
@@ -595,18 +595,10 @@ void ScrollingTree::setMainFrameCanRubberBand(RectEdges<bool> canRubberBand)
     m_swipeState.canRubberBand = canRubberBand;
 }
 
-bool ScrollingTree::mainFrameCanRubberBandInDirection(ScrollDirection direction)
+bool ScrollingTree::mainFrameCanRubberBandOnSide(BoxSide side)
 {
     Locker locker { m_swipeStateLock };
-
-    switch (direction) {
-    case ScrollUp: return m_swipeState.canRubberBand.top();
-    case ScrollDown: return m_swipeState.canRubberBand.bottom();
-    case ScrollLeft: return m_swipeState.canRubberBand.left();
-    case ScrollRight: return m_swipeState.canRubberBand.right();
-    };
-
-    return false;
+    return m_swipeState.canRubberBand.at(side);
 }
 
 void ScrollingTree::addPendingScrollUpdate(ScrollUpdate&& update)

Source/WebCore/page/scrolling/ScrollingTree.h
@@ -173,7 +173,7 @@ public:
 
     // Can be called from any thread. Will update what edges allow rubber-banding.
     WEBCORE_EXPORT void setMainFrameCanRubberBand(RectEdges<bool>);
-    bool mainFrameCanRubberBandInDirection(ScrollDirection);
+    bool mainFrameCanRubberBandOnSide(BoxSide);
 
     bool isHandlingProgrammaticScroll() const { return m_isHandlingProgrammaticScroll; }
     void setIsHandlingProgrammaticScroll(bool isHandlingProgrammaticScroll) { m_isHandlingProgrammaticScroll = isHandlingProgrammaticScroll; }

Source/WebCore/page/scrolling/mac/ScrollingTreeScrollingNodeDelegateMac.h
@@ -68,8 +68,6 @@ public:
     void removeWheelEventTestCompletionDeferralForReason(WheelEventTestMonitor::ScrollableAreaIdentifier, WheelEventTestMonitor::DeferReason) const override;
 
 private:
-    bool isPinnedForScrollDeltaOnAxis(float scrollDelta, ScrollEventAxis, float scrollLimit = 0) const;
-
     // ScrollingEffectsControllerClient.
     std::unique_ptr<ScrollingEffectsControllerTimer> createTimer(Function<void()>&&) final;
     void startAnimationCallback(ScrollingEffectsController&) final;
@@ -78,14 +76,15 @@ private:
     bool allowsHorizontalStretching(const PlatformWheelEvent&) const final;
     bool allowsVerticalStretching(const PlatformWheelEvent&) const final;
     IntSize stretchAmount() const final;
-    bool isPinnedForScrollDelta(const FloatSize&) const final;
+    bool isPinnedOnSide(BoxSide) const final;
+
     RectEdges<bool> edgePinnedState() const final;
     bool allowsHorizontalScrolling() const final;
     bool allowsVerticalScrolling() const final;
     void setScrollBehaviorStatus(ScrollBehaviorStatus status) final { m_scrollBehaviorStatus = status; }
     ScrollBehaviorStatus scrollBehaviorStatus() const final { return m_scrollBehaviorStatus; }
 
-    bool shouldRubberBandInDirection(ScrollDirection) const final;
+    bool shouldRubberBandOnSide(BoxSide) const final;
     void immediateScrollBy(const FloatSize&) final;
     void immediateScrollByWithoutContentEdgeConstraints(const FloatSize&) final;
     void didStopRubberbandSnapAnimation() final;

Source/WebCore/page/scrolling/mac/ScrollingTreeScrollingNodeDelegateMac.mm
@@ -172,43 +172,6 @@ bool ScrollingTreeScrollingNodeDelegateMac::isScrollSnapInProgress() const
     return m_scrollController.isScrollSnapInProgress();
 }
 
-bool ScrollingTreeScrollingNodeDelegateMac::isPinnedForScrollDeltaOnAxis(float scrollDelta, ScrollEventAxis axis, float scrollLimit) const
-{
-    auto scrollPosition = currentScrollPosition();
-    switch (axis) {
-    case ScrollEventAxis::Vertical:
-        if (!allowsVerticalScrolling())
-            return true;
-
-        if (scrollDelta < 0) {
-            auto topOffset = scrollPosition.y() - minimumScrollPosition().y();
-            return topOffset <= scrollLimit;
-        }
-
-        if (scrollDelta > 0) {
-            auto bottomOffset = maximumScrollPosition().y() - scrollPosition.y();
-            return bottomOffset <= scrollLimit;
-        }
-        break;
-    case ScrollEventAxis::Horizontal:
-        if (!allowsHorizontalScrolling())
-            return true;
-
-        if (scrollDelta < 0) {
-            auto leftOffset = scrollPosition.x() - minimumScrollPosition().x();
-            return leftOffset <= scrollLimit;
-        }
-
-        if (scrollDelta > 0) {
-            auto rightOffset = maximumScrollPosition().x() - scrollPosition.x();
-            return rightOffset <= scrollLimit;
-        }
-        break;
-    }
-
-    return false;
-}
-
 std::unique_ptr<ScrollingEffectsControllerTimer> ScrollingTreeScrollingNodeDelegateMac::createTimer(Function<void()>&& function)
 {
     return WTF::makeUnique<ScrollingEffectsControllerTimer>(RunLoop::current(), [function = WTFMove(function), protectedNode = Ref { scrollingNode() }] {
@@ -244,15 +207,16 @@ bool ScrollingTreeScrollingNodeDelegateMac::allowsHorizontalStretching(const Pla
     switch (horizontalScrollElasticity()) {
     case ScrollElasticityAutomatic: {
         bool scrollbarsAllowStretching = allowsHorizontalScrolling() || !allowsVerticalScrolling();
-        bool eventPreventsStretching = wheelEvent.isGestureStart() && isPinnedForScrollDeltaOnAxis(-wheelEvent.deltaX(), ScrollEventAxis::Horizontal);
+        auto relevantSide = ScrollableArea::targetSideForScrollDelta(-wheelEvent.delta(), ScrollEventAxis::Horizontal);
+        bool eventPreventsStretching = wheelEvent.isGestureStart() && relevantSide && isPinnedOnSide(*relevantSide);
         return scrollbarsAllowStretching && !eventPreventsStretching;
     }
     case ScrollElasticityNone:
         return false;
     case ScrollElasticityAllowed: {
-        auto scrollDirection = ScrollingEffectsController::directionFromEvent(wheelEvent, ScrollEventAxis::Horizontal);
-        if (scrollDirection)
-            return shouldRubberBandInDirection(scrollDirection.value());
+        auto relevantSide = ScrollableArea::targetSideForScrollDelta(-wheelEvent.delta(), ScrollEventAxis::Horizontal);
+        if (relevantSide)
+            return shouldRubberBandOnSide(*relevantSide);
         return true;
     }
     }
@@ -266,15 +230,16 @@ bool ScrollingTreeScrollingNodeDelegateMac::allowsVerticalStretching(const Platf
     switch (verticalScrollElasticity()) {
     case ScrollElasticityAutomatic: {
         bool scrollbarsAllowStretching = allowsVerticalScrolling() || !allowsHorizontalScrolling();
-        bool eventPreventsStretching = wheelEvent.isGestureStart() && isPinnedForScrollDeltaOnAxis(-wheelEvent.deltaY(), ScrollEventAxis::Vertical);
+        auto relevantSide = ScrollableArea::targetSideForScrollDelta(-wheelEvent.delta(), ScrollEventAxis::Vertical);
+        bool eventPreventsStretching = wheelEvent.isGestureStart() && relevantSide && isPinnedOnSide(*relevantSide);
         return scrollbarsAllowStretching && !eventPreventsStretching;
     }
     case ScrollElasticityNone:
         return false;
     case ScrollElasticityAllowed: {
-        auto scrollDirection = ScrollingEffectsController::directionFromEvent(wheelEvent, ScrollEventAxis::Vertical);
-        if (scrollDirection)
-            return shouldRubberBandInDirection(scrollDirection.value());
+        auto relevantSide = ScrollableArea::targetSideForScrollDelta(-wheelEvent.delta(), ScrollEventAxis::Vertical);
+        if (relevantSide)
+            return shouldRubberBandOnSide(*relevantSide);
         return true;
     }
     }
@@ -301,17 +266,26 @@ IntSize ScrollingTreeScrollingNodeDelegateMac::stretchAmount() const
     return stretch;
 }
 
-bool ScrollingTreeScrollingNodeDelegateMac::isPinnedForScrollDelta(const FloatSize& delta) const
+bool ScrollingTreeScrollingNodeDelegateMac::isPinnedOnSide(BoxSide side) const
 {
-    // This "offset < 1" logic was added in r107488. Unclear if it's needed.
-    constexpr float scrollOffsetLimit = 1.0f - std::numeric_limits<float>::epsilon();
-
-    if (fabsf(delta.height()) >= fabsf(delta.width()))
-        return isPinnedForScrollDeltaOnAxis(delta.height(), ScrollEventAxis::Vertical, scrollOffsetLimit);
-
-    if (delta.width())
-        return isPinnedForScrollDeltaOnAxis(delta.width(), ScrollEventAxis::Horizontal, scrollOffsetLimit);
-
+    switch (side) {
+    case BoxSide::Top:
+        if (!allowsVerticalScrolling())
+            return true;
+        return currentScrollPosition().y() <= minimumScrollPosition().y();
+    case BoxSide::Bottom:
+        if (!allowsVerticalScrolling())
+            return true;
+        return currentScrollPosition().y() >= maximumScrollPosition().y();
+    case BoxSide::Left:
+        if (!allowsHorizontalScrolling())
+            return true;
+        return currentScrollPosition().x() <= minimumScrollPosition().x();
+    case BoxSide::Right:
+        if (!allowsHorizontalScrolling())
+            return true;
+        return currentScrollPosition().x() >= maximumScrollPosition().x();
+    }
     return false;
 }
 
@@ -330,17 +304,17 @@ bool ScrollingTreeScrollingNodeDelegateMac::allowsVerticalScrolling() const
     return ScrollingTreeScrollingNodeDelegate::allowsVerticalScrolling();
 }
 
-bool ScrollingTreeScrollingNodeDelegateMac::shouldRubberBandInDirection(ScrollDirection direction) const
+bool ScrollingTreeScrollingNodeDelegateMac::shouldRubberBandOnSide(BoxSide side) const
 {
     if (scrollingNode().isRootNode())
-        return scrollingTree().mainFrameCanRubberBandInDirection(direction);
+        return scrollingTree().mainFrameCanRubberBandOnSide(side);
 
-    switch (direction) {
-    case ScrollDirection::ScrollUp:
-    case ScrollDirection::ScrollDown:
+    switch (side) {
+    case BoxSide::Top:
+    case BoxSide::Bottom:
         return allowsVerticalScrolling();
-    case ScrollDirection::ScrollLeft:
-    case ScrollDirection::ScrollRight:
+    case BoxSide::Left:
+    case BoxSide::Right:
         return allowsHorizontalScrolling();
     }
     return true;

commit c48b3aad3da20e306a092c176e27039f3a220059

REGRESSION (r283335): rubber-banding no longer locks to an axis https://bugs.webkit.org/show_bug.cgi?id=231131
Reviewed by Tim Horton.

Source/WebCore:

r283335 introduced a bug where pulling down to rubber-band would result in sideways
motion even when the gesture was mostly vertical.

ScrollingEffectsController::modifyScrollDeltaForStretching() has some axis-locking behavior
that was broken by r283335, so restore the old behavior.

Test: fast/scrolling/mac/rubberband-axis-locking.html

* platform/mac/ScrollingEffectsController.mm:
(WebCore::ScrollingEffectsController::modifyScrollDeltaForStretching):
(WebCore::isHorizontalSide): Deleted.
(WebCore::isVerticalSide): Deleted.

LayoutTests:

* fast/scrolling/mac/rubberband-axis-locking-expected.txt: Added.
* fast/scrolling/mac/rubberband-axis-locking.html: Added.


Canonical link: https://commits.webkit.org/242444@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@283465 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/mac/ScrollingEffectsController.mm
@@ -113,16 +113,6 @@ static std::optional<BoxSide> affectedSideOnDominantAxis(FloatSize delta)
     return ScrollableArea::targetSideForScrollDelta(delta, dominantAxis);
 }
 
-static bool isHorizontalSide(std::optional<BoxSide> side)
-{
-    return side && (*side == BoxSide::Left || *side == BoxSide::Right);
-}
-
-static bool isVerticalSide(std::optional<BoxSide> side)
-{
-    return side && (*side == BoxSide::Top || *side == BoxSide::Bottom);
-}
-
 bool ScrollingEffectsController::handleWheelEvent(const PlatformWheelEvent& wheelEvent)
 {
     if (processWheelEventForScrollSnap(wheelEvent))
@@ -244,7 +234,7 @@ bool ScrollingEffectsController::modifyScrollDeltaForStretching(const PlatformWh
 {
     auto affectedSide = affectedSideOnDominantAxis(delta);
     if (isVerticallyStretched) {
-        if (!isHorizontallyStretched && isHorizontalSide(affectedSide) && m_client.isPinnedOnSide(*affectedSide)) {
+        if (!isHorizontallyStretched && affectedSide && m_client.isPinnedOnSide(*affectedSide)) {
             // Stretching only in the vertical.
             if (delta.height() && (fabsf(delta.width() / delta.height()) < rubberbandDirectionLockStretchRatio))
                 delta.setWidth(0);
@@ -260,7 +250,7 @@ bool ScrollingEffectsController::modifyScrollDeltaForStretching(const PlatformWh
 
     if (isHorizontallyStretched) {
         // Stretching only in the horizontal.
-        if (isVerticalSide(affectedSide) && m_client.isPinnedOnSide(*affectedSide)) {
+        if (affectedSide && m_client.isPinnedOnSide(*affectedSide)) {
             if (delta.width() && (fabsf(delta.height() / delta.width()) < rubberbandDirectionLockStretchRatio))
                 delta.setHeight(0);
             else if (fabsf(delta.height()) < rubberbandMinimumRequiredDeltaBeforeStretch) {

LayoutTests/fast/scrolling/mac/rubberband-axis-locking-expected.txt
@@ -1,0 +1,6 @@
+
+Tests axis locking while rubberbanding
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/scrolling/mac/rubberband-axis-locking.html
@@ -1,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            height: 2000px;
+            width: 200%;
+        }
+    </style>
+    <script src="../../../resources/js-test-pre.js"></script>
+    <script src="../../../resources/ui-helper.js"></script>
+    <script>
+        var jsTestIsAsync = true;
+        
+        var initialScrollX;
+        var initialScrollY;
+
+        async function resetScrollPositions(x, y)
+        {
+            window.scrollTo(x, y);
+            // Wait for scroll events to fire.
+            await UIHelper.renderingUpdate();
+        }
+        
+        async function testVerticalRubberband()
+        {
+            await resetScrollPositions(100, 0);
+            initialScrollX = window.scrollX;
+            
+            let scrollListener = () => {
+                if (window.scrollX != initialScrollX)
+                    testFailed('Horizontal scroll position ' + window.scrollX + ' is not ' + initialScrollX);
+            }
+
+            window.addEventListener('scroll', scrollListener);
+
+            eventSender.monitorWheelEvents();
+            eventSender.mouseMoveTo(100, 100);
+            // Pull down and slightly sideways
+            eventSender.mouseScrollByWithWheelAndMomentumPhases(0, 2, "began", "none");
+            eventSender.mouseScrollByWithWheelAndMomentumPhases(1, 2, "changed", "none");
+            eventSender.mouseScrollByWithWheelAndMomentumPhases(1, 2, "changed", "none");
+            eventSender.mouseScrollByWithWheelAndMomentumPhases(0, 0, "ended", "none");
+            await UIHelper.waitForScrollCompletion();
+
+            window.removeEventListener('scroll', scrollListener);
+        }
+
+        async function testHorizontalRubberband()
+        {
+            await resetScrollPositions(0, 100);
+            initialScrollY = window.scrollY;
+
+            let scrollListener = () => {
+                if (window.scrollY != initialScrollY)
+                    testFailed('Vertical scroll position ' + window.scrollY + ' is not ' + initialScrollY);
+            }
+
+            window.addEventListener('scroll', scrollListener);
+
+            eventSender.monitorWheelEvents();
+            eventSender.mouseMoveTo(100, 100);
+            // Pull left and slightly sideways
+            eventSender.mouseScrollByWithWheelAndMomentumPhases(2, 0, "began", "none");
+            eventSender.mouseScrollByWithWheelAndMomentumPhases(2, -1, "changed", "none");
+            eventSender.mouseScrollByWithWheelAndMomentumPhases(2, -1, "changed", "none");
+            eventSender.mouseScrollByWithWheelAndMomentumPhases(0, 0, "ended", "none");
+            await UIHelper.waitForScrollCompletion();
+
+            window.removeEventListener('scroll', scrollListener);
+        }
+
+        async function scrollTest()
+        {
+            debug('');
+            debug('Tests axis locking while rubberbanding');
+
+            await testVerticalRubberband();
+            await testHorizontalRubberband();
+
+            finishJSTest();
+        }
+
+        window.addEventListener('load', () => {
+            setTimeout(scrollTest, 0);
+        }, false);
+    </script>
+</head>
+<body>
+    <script src="../../../resources/js-test-post.js"></script>
+</body>
+</html>