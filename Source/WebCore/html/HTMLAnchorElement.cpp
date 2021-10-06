cf7181d14360baabf90006326cece248a8437029
John Wilander <wilander@apple.com>
2021-10-06 00:46:43 +0000

    PCM: Allow measurement of links in nested, cross-site iframes https://bugs.webkit.org/show_bug.cgi?id=229204 <rdar://problem/82310386>
    Reviewed by Alex Christensen.
    
    Developers have said it would be great to allow measurement of links served in
    nested, cross-site iframes on the click source. The click still count as if it
    happened on the first party click source site, and any attribution report still
    goes to that first party. This change enables the *serving* of measurable links
    in nested, cross-site iframes.
    
    The standards issue where this was discussed:
    https://github.com/privacycg/private-click-measurement/issues/7
    
    Source/WebCore:
    
    Test: http/tests/privateClickMeasurement/store-private-click-measurement-nested.html
    
    * html/HTMLAnchorElement.cpp:
    (WebCore::HTMLAnchorElement::parsePrivateClickMeasurement const):
        Removed the block for nested, cross-site iframes.
    
    LayoutTests:
    
    * http/tests/privateClickMeasurement/resources/nestedTargetLink.html: Added.
    * http/tests/privateClickMeasurement/store-private-click-measurement-nested-expected.txt: Added.
    * http/tests/privateClickMeasurement/store-private-click-measurement-nested.html: Added.
    
    
    Canonical link: https://commits.webkit.org/242549@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@283593 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLAnchorElement.cpp
+++ Source/WebCore/html/HTMLAnchorElement.cpp
@@ -402,8 +402,9 @@ std::optional<PrivateClickMeasurement> HTMLAnchorElement::parsePrivateClickMeasu
     using SourceSite = PrivateClickMeasurement::SourceSite;
     using AttributionDestinationSite = PrivateClickMeasurement::AttributionDestinationSite;
 
+    RefPtr<Frame> frame = document().frame();
     auto* page = document().page();
-    if (!page || page->sessionID().isEphemeral()
+    if (!frame ||!page || page->sessionID().isEphemeral()
         || !document().settings().privateClickMeasurementEnabled()
         || !UserGestureIndicator::processingUserGesture())
         return std::nullopt;
@@ -421,12 +422,6 @@ std::optional<PrivateClickMeasurement> HTMLAnchorElement::parsePrivateClickMeasu
         return std::nullopt;
     }
 
-    RefPtr<Frame> frame = document().frame();
-    if (!frame || !frame->isMainFrame()) {
-        document().addConsoleMessage(MessageSource::Other, MessageLevel::Warning, "Private Click Measurement is only supported in the main frame."_s);
-        return std::nullopt;
-    }
-    
     auto attributionSourceID = parseHTMLNonNegativeInteger(attributionSourceIDAttr);
     if (!attributionSourceID) {
         document().addConsoleMessage(MessageSource::Other, MessageLevel::Warning, "attributionsourceid is not a non-negative integer which is required for Private Click Measurement."_s);
@@ -444,8 +439,15 @@ std::optional<PrivateClickMeasurement> HTMLAnchorElement::parsePrivateClickMeasu
         return std::nullopt;
     }
 
-    RegistrableDomain documentRegistrableDomain { document().url() };
-    if (documentRegistrableDomain.matches(destinationURL)) {
+    RegistrableDomain mainDocumentRegistrableDomain;
+    if (auto mainDocument = frame->mainFrame().document())
+        mainDocumentRegistrableDomain = RegistrableDomain { mainDocument->url() };
+    else {
+        document().addConsoleMessage(MessageSource::Other, MessageLevel::Warning, "Could not find a main document to use as source site for Private Click Measurement."_s);
+        return std::nullopt;
+    }
+
+    if (mainDocumentRegistrableDomain.matches(destinationURL)) {
         document().addConsoleMessage(MessageSource::Other, MessageLevel::Warning, "attributiondestination can not be the same site as the current website."_s);
         return std::nullopt;
     }
@@ -455,7 +457,7 @@ std::optional<PrivateClickMeasurement> HTMLAnchorElement::parsePrivateClickMeasu
 #else
     String bundleID;
 #endif
-    auto privateClickMeasurement = PrivateClickMeasurement { SourceID(attributionSourceID.value()), SourceSite(WTFMove(documentRegistrableDomain)), AttributionDestinationSite(destinationURL), bundleID, WallTime::now(), PrivateClickMeasurement::AttributionEphemeral::No };
+    auto privateClickMeasurement = PrivateClickMeasurement { SourceID(attributionSourceID.value()), SourceSite(WTFMove(mainDocumentRegistrableDomain)), AttributionDestinationSite(destinationURL), bundleID, WallTime::now(), PrivateClickMeasurement::AttributionEphemeral::No };
 
     auto attributionSourceNonceAttr = attributeWithoutSynchronization(attributionsourcenonceAttr);
     if (!attributionSourceNonceAttr.isEmpty()) {
