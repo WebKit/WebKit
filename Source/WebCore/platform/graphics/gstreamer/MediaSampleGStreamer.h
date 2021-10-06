1d40c25a2a07f57ee739c87b84df29c3452723b0
Alicia Boya Garcia <aboya@igalia.com>
2021-10-06 09:48:38 +0000

    [MSE][GStreamer] Honor MP4 edit lists https://bugs.webkit.org/show_bug.cgi?id=231019
    Reviewed by Xabier Rodriguez-Calvar.
    
    Source/WebCore:
    
    This patch takes into consideration the GstSegment attached to a
    sample to offset the PTS and DTS. This ensures accurate timestamps are
    obtained for MP4 files containing edit lists (commonly necessary for
    files containing video with B frames to have PTS starting at zero).
    
    Before this was implemented, a workaround was in place based on a
    heuristic (DTS = 0 && PTS > 0 && PTS < 0.1). The workaround is
    preserved for the sake of content without proper edit lists, but
    any edit list takes preference.
    
    The time fudge factor has been modified from 0.083 seconds up to
    0.100 seconds to accomodate the size of the empty edit in test.mp4
    used by Web Platform Tests.
    
    This test fixes improves expectation results and fixes two subtests in
    imported/w3c/web-platform-tests/media-source/mediasource-remove.html.
    
    * Modules/mediasource/MediaSource.cpp:
    (WebCore::MediaSource::currentTimeFudgeFactor):
    * platform/graphics/SourceBufferPrivate.h:
    (WebCore::SourceBufferPrivate::timeFudgeFactor const):
    * platform/graphics/gstreamer/MediaSampleGStreamer.cpp:
    (WebCore::MediaSampleGStreamer::extendToTheBeginning): Deleted.
    * platform/graphics/gstreamer/MediaSampleGStreamer.h:
    * platform/graphics/gstreamer/mse/AppendPipeline.cpp:
    (WebCore::bufferTimeToStreamTimeClamped):
    (WebCore::AppendPipeline::appsinkNewSample):
    
    LayoutTests:
    
    Update expectations for mediasource-remove.html in the GStreamer
    ports, as a couple subtests get fixed.
    
    * platform/glib/imported/w3c/web-platform-tests/media-source/mediasource-remove-expected.txt:
    
    
    Canonical link: https://commits.webkit.org/242561@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@283609 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/gstreamer/MediaSampleGStreamer.h
+++ Source/WebCore/platform/graphics/gstreamer/MediaSampleGStreamer.h
@@ -42,7 +42,6 @@ public:
     static Ref<MediaSampleGStreamer> createFakeSample(GstCaps*, MediaTime pts, MediaTime dts, MediaTime duration, const FloatSize& presentationSize, const AtomString& trackId);
     static Ref<MediaSampleGStreamer> createImageSample(PixelBuffer&&, const IntSize& destinationSize = { }, double frameRate = 1);
 
-    void extendToTheBeginning();
     MediaTime presentationTime() const override { return m_pts; }
     MediaTime decodeTime() const override { return m_dts; }
     MediaTime duration() const override { return m_duration; }
