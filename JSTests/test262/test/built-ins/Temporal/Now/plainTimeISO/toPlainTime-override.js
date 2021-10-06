1b1e2a9b47b129a019484d5c5d00ca252ddd4957
Philip Chimento <pchimento@igalia.com>
2021-10-06 21:38:11 +0000

    [JSC] Update test262 https://bugs.webkit.org/show_bug.cgi?id=231201
    Add tests that were somehow cut off in the previous patch.
    
    Patch by Philip Chimento <pchimento@igalia.com> on 2021-10-06
    Reviewed by Ross Kirsling.
    
    * test262/test/built-ins/Temporal/Now/*: Added.
    
    Canonical link: https://commits.webkit.org/242606@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@283666 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ JSTests/test262/test/built-ins/Temporal/Now/plainTimeISO/toPlainTime-override.js
@@ -0,0 +1,50 @@
+// Copyright (C) 2020 Igalia, S.L. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-temporal.now.plaintimeiso
+description: PlainDateTime.toPlainTime is not observably called
+includes: [compareArray.js]
+features: [Temporal]
+---*/
+
+const actual = [];
+const expected = [
+  "has timeZone.timeZone",
+  "get timeZone.getOffsetNanosecondsFor",
+  "call timeZone.getOffsetNanosecondsFor",
+];
+
+Object.defineProperty(Temporal.PlainDateTime.prototype, "toPlainTime", {
+  get() {
+    actual.push("get Temporal.PlainDateTime.prototype.toPlainTime");
+    return function() {
+      actual.push("call Temporal.PlainDateTime.prototype.toPlainTime");
+    };
+  },
+});
+
+const timeZone = new Proxy({
+  getOffsetNanosecondsFor(instant) {
+    actual.push("call timeZone.getOffsetNanosecondsFor");
+    assert.sameValue(instant instanceof Temporal.Instant, true, "Instant");
+    return -Number(instant.epochNanoseconds % 86400_000_000_000n);
+  },
+}, {
+  has(target, property) {
+    actual.push(`has timeZone.${property}`);
+    return property in target;
+  },
+  get(target, property) {
+    actual.push(`get timeZone.${property}`);
+    return target[property];
+  },
+});
+
+const result = Temporal.Now.plainTimeISO(timeZone);
+assert.sameValue(result instanceof Temporal.PlainTime, true);
+for (const property of ["hour", "minute", "second", "millisecond", "microsecond", "nanosecond"]) {
+  assert.sameValue(result[property], 0, property);
+}
+
+assert.compareArray(actual, expected);
