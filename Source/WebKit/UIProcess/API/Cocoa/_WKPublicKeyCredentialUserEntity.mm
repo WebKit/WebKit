62d48a515a5cef6a7f2e24f2bd6c7c6cc98d5f83
David Kilzer <ddkilzer@apple.com>
2021-10-17 04:03:21 +0000

    [WebAuthn] Many Objective-C classes leak their instance variables <https://webkit.org/b/231834> <rdar://problem/84317190>
    Reviewed by Brent Fulgham.
    
    * UIProcess/API/Cocoa/_WKAuthenticationExtensionsClientInputs.mm:
    (-[_WKAuthenticationExtensionsClientInputs dealloc]):
    * UIProcess/API/Cocoa/_WKPublicKeyCredentialCreationOptions.mm:
    (-[_WKPublicKeyCredentialCreationOptions dealloc]):
    * UIProcess/API/Cocoa/_WKPublicKeyCredentialDescriptor.mm:
    (-[_WKPublicKeyCredentialDescriptor dealloc]):
    * UIProcess/API/Cocoa/_WKPublicKeyCredentialEntity.mm:
    (-[_WKPublicKeyCredentialEntity dealloc]):
    * UIProcess/API/Cocoa/_WKPublicKeyCredentialParameters.mm:
    (-[_WKPublicKeyCredentialParameters dealloc]):
    * UIProcess/API/Cocoa/_WKPublicKeyCredentialRelyingPartyEntity.mm:
    (-[_WKPublicKeyCredentialRelyingPartyEntity dealloc]):
    * UIProcess/API/Cocoa/_WKPublicKeyCredentialRequestOptions.mm:
    (-[_WKPublicKeyCredentialRequestOptions dealloc]):
    * UIProcess/API/Cocoa/_WKPublicKeyCredentialUserEntity.mm:
    (-[_WKPublicKeyCredentialUserEntity dealloc]):
    - Add -dealloc methods that release instance variables.
    
    
    Canonical link: https://commits.webkit.org/243126@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@284331 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/Cocoa/_WKPublicKeyCredentialUserEntity.mm
+++ Source/WebKit/UIProcess/API/Cocoa/_WKPublicKeyCredentialUserEntity.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Apple Inc. All rights reserved.
+ * Copyright (C) 2020-2021 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -36,6 +36,13 @@
     self.identifier = identifier;
     self.displayName = displayName;
     return self;
+}
+
+- (void)dealloc
+{
+    [_identifier release];
+    [_displayName release];
+    [super dealloc];
 }
 
 @end
