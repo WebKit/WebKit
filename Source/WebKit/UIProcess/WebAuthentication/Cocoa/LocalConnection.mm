8732bad7759da96ecef5c96e1c3e09db0e410cac
David Kilzer <ddkilzer@apple.com>
2021-10-19 16:24:46 +0000

    Follow-up #2: WebKit::LocalConnection::createCredentialPrivateKey leaks an NSMutableDictionary <https://webkit.org/b/231814> <rdar://problem/84307054>
    Unreviewed follow-up to address Darin's comment.
    
    
    * UIProcess/WebAuthentication/Cocoa/LocalConnection.mm:
    (WebKit::LocalConnection::createCredentialPrivateKey const):
    - Use WTFMove() to avoid ref count churn.
    
    
    Canonical link: https://commits.webkit.org/243218@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@284458 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/WebAuthentication/Cocoa/LocalConnection.mm
+++ Source/WebKit/UIProcess/WebAuthentication/Cocoa/LocalConnection.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Apple Inc. All rights reserved.
+ * Copyright (C) 2018=2021 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -178,7 +178,7 @@ RetainPtr<SecKeyRef> LocalConnection::createCredentialPrivateKey(LAContext *cont
     if (context) {
         auto mutableCopy = adoptNS([privateKeyAttributes mutableCopy]);
         mutableCopy.get()[(id)kSecUseAuthenticationContext] = context;
-        privateKeyAttributes = mutableCopy;
+        privateKeyAttributes = WTFMove(mutableCopy);
     }
 
     NSDictionary *attributes = @{
