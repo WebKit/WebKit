1418e1d629f30b94c91c555f3b256594b7282332
Michael Saboff <msaboff@apple.com>
2021-10-25 19:09:56 +0000

    Cherry-pick r284573. rdar://problem/84329018
        Add missing overflow checks to DFGIntegerRangeOptimizationPhase::isEquivalentTo()
        https://bugs.webkit.org/show_bug.cgi?id=232024
    
        Reviewed by Tadeu Zagallo.
    
        Added overflow check before comparing for equality.
    
        * dfg/DFGIntegerRangeOptimizationPhase.cpp:
    
        Canonical link: https://commits.webkit.org/243313@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@284573 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/240672.273@safari-612-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-612-branch@284807 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGIntegerRangeOptimizationPhase.cpp
+++ Source/JavaScriptCore/dfg/DFGIntegerRangeOptimizationPhase.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2020 Apple Inc. All rights reserved.
+ * Copyright (C) 2015-2021 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -228,8 +228,17 @@ public:
         if (*this == other)
             return true;
 
-        if (m_right->isInt32Constant() && other.m_right->isInt32Constant())
-            return (m_right->asInt32() + m_offset) == (other.m_right->asInt32() + other.m_offset);
+        if (m_right->isInt32Constant() && other.m_right->isInt32Constant()) {
+            int thisRight = m_right->asInt32();
+            int otherRight = other.m_right->asInt32();
+
+            if (sumOverflows<int>(thisRight, m_offset))
+                return false;
+            if (sumOverflows<int>(otherRight, other.m_offset))
+                return false;
+
+            return (thisRight + m_offset) == (otherRight + other.m_offset);
+        }
         return false;
     }
     
