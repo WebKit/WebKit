9d2552a16f637b110c4735d6d79b76c1ea1818e2
Saam Barati <sbarati@apple.com>
2021-10-25 21:50:57 +0000

    Cherry-pick r283600. rdar://problem/84625586
        Don't pass DontBuildStrings to next token after parsing an empty parameter list
        https://bugs.webkit.org/show_bug.cgi?id=225094
        <rdar://problem/77231778>
    
        Reviewed by Yusuke Suzuki.
    
        JSTests:
    
        * stress/dont-pass-DontBuildStrings-when-building-empty-arguments-list.js: Added.
        (main.a.prototype.g.toString.string_appeared_here):
        (main.a):
        (main):
    
        Source/JavaScriptCore:
    
        We might need the string, it turns out!
    
        * parser/Parser.cpp:
        (JSC::Parser<LexerType>::parseArguments):
    
        Canonical link: https://commits.webkit.org/242552@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@283600 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/240672.288@safari-612-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-612-branch@284830 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/parser/Parser.cpp
+++ Source/JavaScriptCore/parser/Parser.cpp
@@ -4981,7 +4981,7 @@ template <class TreeBuilder> TreeArguments Parser<LexerType>::parseArguments(Tre
     consumeOrFailWithFlags(OPENPAREN, TreeBuilder::DontBuildStrings, "Expected opening '(' at start of argument list");
     JSTokenLocation location(tokenLocation());
     if (match(CLOSEPAREN)) {
-        next(TreeBuilder::DontBuildStrings);
+        next();
         return context.createArguments();
     }
     auto argumentsStart = m_token.m_startPosition;
