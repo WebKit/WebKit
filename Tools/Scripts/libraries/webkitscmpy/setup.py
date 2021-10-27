db3b432ba7fc95bd5838df7b2fca18965cfb5b95
Jonathan Bedard <jbedard@apple.com>
2021-10-27 09:39:37 -0700

    [webkitscmpy] Ensure empty line before canonicalization https://bugs.webkit.org/show_bug.cgi?id=232103 <rdar://problem/84521382>
    Reviewed by Stephanie Lewis.
    
    * Tools/Scripts/libraries/webkitscmpy/setup.py: Bump version.
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py: Ditto.
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/canonicalize/message.py:
    (main): Handle empty lines around identifier links, add empty line before identifier links.
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/canonicalize_unittest.py:
    (TestCanonicalize.test_formated_identifier):
    (TestCanonicalize.test_existing_identifier):
    (TestCanonicalize.test_git_svn):
    (TestCanonicalize.test_git_svn_existing):
    (TestCanonicalize.test_branch_commits):
    (TestCanonicalize.test_number):

--- Tools/Scripts/libraries/webkitscmpy/setup.py
+++ Tools/Scripts/libraries/webkitscmpy/setup.py
@@ -29,7 +29,7 @@ def readme():
 
 setup(
     name='webkitscmpy',
-    version='2.2.16',
+    version='2.2.17',
     description='Library designed to interact with git and svn repositories.',
     long_description=readme(),
     classifiers=[
