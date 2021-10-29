9d5068322247cfb9e5e3c5c424a429fdc75f3f71
David Kilzer <ddkilzer@apple.com>
2021-10-29 21:08:22 +0000

    [WebKitLegacy] Enable -Wformat=2 warnings <https://webkit.org/b/232333> <rdar://problem/84681560>
    Reviewed by Darin Adler.
    
    * Configurations/Base.xcconfig:
    (WARNING_CFLAGS):
    - Replace -Wformat-security with -Wformat=2, which enables more
      warnings like -Wformat-nonliteral.
    * Misc/WebLocalizableStringsInternal.h:
    (WebLocalizedStringInternal):
    - Add NS_FORMAT_ARGUMENT() macro to fix -Wformat-nonliteral
      warnings.
    
    
    
    Canonical link: https://commits.webkit.org/243692@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@285049 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKitLegacy/mac/Misc/WebLocalizableStringsInternal.h
+++ Source/WebKitLegacy/mac/Misc/WebLocalizableStringsInternal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2011-2021 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
 extern "C" {
 #endif
 
-NSString *WebLocalizedStringInternal(const char* key);
+NSString *WebLocalizedStringInternal(const char* key) NS_FORMAT_ARGUMENT(1);
 
 #ifdef __cplusplus
 }
