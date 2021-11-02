b6d394a9d8975cf01941ab441821e5fb88cc849d
Jonathan Bedard <jbedard@apple.com>
2021-11-02 15:11:06 +0000

    [webkitscmpy] Generalize pull-request title generation. https://bugs.webkit.org/show_bug.cgi?id=232463 <rdar://problem/84784354>
    Reviewed by Dewei Zhu.
    
    * Scripts/libraries/webkitscmpy/setup.py: Bump version.
    * Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py: Ditto.
    * Scripts/libraries/webkitscmpy/webkitscmpy/program/pull_request.py:
    (PullRequest.title_for): Compute pull-request title for a set of commits.
    (PullRequest.main): Generalize pull-request title generation.
    
    
    
    Canonical link: https://commits.webkit.org/243794@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@285158 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/libraries/webkitscmpy/setup.py
+++ Tools/Scripts/libraries/webkitscmpy/setup.py
@@ -29,7 +29,7 @@ def readme():
 
 setup(
     name='webkitscmpy',
-    version='2.2.22',
+    version='2.2.23',
     description='Library designed to interact with git and svn repositories.',
     long_description=readme(),
     classifiers=[
