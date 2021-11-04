c5df6656a0188f049a961e463eed8f45fe70ac4f
Jer Noble <jer.noble@apple.com>
2021-11-04 21:19:07 +0000

    Cherry-pick r285241. rdar://problem/84990898
        [macOS] MediaSession coordinator enabled in UIProcess, disabled in WebContent
        https://bugs.webkit.org/show_bug.cgi?id=232687
        <rdar://84990898>
    
        Reviewed by Eric Carlson.
    
        Source/WebKit:
    
        The default value of defaultMediaSessionCoordinatorEnabled() will be true if the UIProcess has
        the correct entitlement. However, this value is not passed across the XPC boundary unless it has
        been overridden, which means defaultMediaSessionCoordinatorEnabled() will control the setting
        in the WebContent process, which is not entitled.
    
        Have the WebContent process check its parent process's entitlement rather than its own when
        determining the default value for defaultMediaSessionCoordinatorEnabled().
    
        * Shared/WebPreferencesDefaultValues.cpp:
        (WebKit::defaultMediaSessionCoordinatorEnabled):
    
        Source/WTF:
    
        Have the MediaSessionPlaylistEnabled preference use the same defaults as MediaSessionCoordinatorEnabled.
    
        * Scripts/Preferences/WebPreferencesExperimental.yaml:
    
        Canonical link: https://commits.webkit.org/243859@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@285241 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/240672.357@safari-612-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-612-branch@285308 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/WebPreferencesDefaultValues.cpp
+++ Source/WebKit/Shared/WebPreferencesDefaultValues.cpp
@@ -36,6 +36,7 @@
 #endif
 
 #if ENABLE(MEDIA_SESSION_COORDINATOR)
+#import "WebProcess.h"
 #import <wtf/cocoa/Entitlements.h>
 #endif
 
@@ -323,7 +324,10 @@ bool defaultMediaSessionCoordinatorEnabled()
     static dispatch_once_t onceToken;
     static bool enabled { false };
     dispatch_once(&onceToken, ^{
-        enabled = WTF::processHasEntitlement("com.apple.developer.group-session.urlactivity");
+        if (isInWebProcess())
+            enabled = WebProcess::singleton().parentProcessHasEntitlement("com.apple.developer.group-session.urlactivity");
+        else
+            enabled = WTF::processHasEntitlement("com.apple.developer.group-session.urlactivity");
     });
     return enabled;
 }
