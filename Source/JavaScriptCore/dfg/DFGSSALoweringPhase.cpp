acff015de632151e412bf576f8298d67005c04ab
Yusuke Suzuki <ysuzuki@apple.com>
2021-11-04 19:39:32 +0000

    Cherry-pick r284716. rdar://problem/84366658
        [JSC] GetTypedArrayLengthAsInt52 must be inserted only when we ensure that input is TypedArray via array-mode-based filtering
        https://bugs.webkit.org/show_bug.cgi?id=232168
        rdar://84366658
    
        Reviewed by Robin Morisset.
    
        JSTests:
    
        * stress/gettypedarraylengthasint52-must-be-emitted-for-typedarray.js: Added.
        (foo):
    
        Source/JavaScriptCore:
    
        GetTypedArrayLengthAsInt52 works only when input is TypedArray, which should be validated via array-mode (and already inserted checks in fixup).
        Accidentally we were inserting it without checking typed-array condition in SSA lowering phase. This patch adds a condition which ensures it
        is TypedArray.
    
        * dfg/DFGSSALoweringPhase.cpp:
        (JSC::DFG::SSALoweringPhase::handleNode):
        (JSC::DFG::SSALoweringPhase::lowerBoundsCheck):
    
        Canonical link: https://commits.webkit.org/243431@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@284716 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/240672.339@safari-612-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-612-branch@285287 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGSSALoweringPhase.cpp
+++ Source/JavaScriptCore/dfg/DFGSSALoweringPhase.cpp
@@ -99,7 +99,7 @@ private:
             if (lowerBoundsCheck(base, index, storage))
                 break;
             
-            if (m_node->arrayMode().typedArrayType() != NotTypedArray && m_node->arrayMode().isOutOfBounds()) {
+            if (m_node->arrayMode().isSomeTypedArrayView() && m_node->arrayMode().isOutOfBounds()) {
 #if USE(LARGE_TYPED_ARRAYS)
                 if (m_node->arrayMode().mayBeLargeTypedArray() || m_graph.hasExitSite(m_node->origin.semantic, Overflow)) {
                     Node* length = m_insertionSet.insertNode(
@@ -149,7 +149,7 @@ private:
 
         Node* checkInBounds;
 #if USE(LARGE_TYPED_ARRAYS)
-        if ((op == GetArrayLength) && (m_node->arrayMode().mayBeLargeTypedArray() || m_graph.hasExitSite(m_node->origin.semantic, Overflow))) {
+        if ((op == GetArrayLength) && m_node->arrayMode().isSomeTypedArrayView() && (m_node->arrayMode().mayBeLargeTypedArray() || m_graph.hasExitSite(m_node->origin.semantic, Overflow))) {
             Node* length = m_insertionSet.insertNode(
                 m_nodeIndex, SpecInt52Any, GetTypedArrayLengthAsInt52, m_node->origin,
                 OpInfo(m_node->arrayMode().asWord()), Edge(base.node(), KnownCellUse), storage);
