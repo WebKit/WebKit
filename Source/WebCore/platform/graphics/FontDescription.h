497fdd757c14302cb246037178dc617a923075b7
Cameron McCormack <heycam@apple.com>
2021-11-23 00:51:53 +0000

    Fix some license find/replace errors https://bugs.webkit.org/show_bug.cgi?id=233435
    Reviewed by Sam Weinig.
    
    * platform/graphics/FontCascadeDescription.h:
    * platform/graphics/FontDescription.h:
    
    
    
    Canonical link: https://commits.webkit.org/244509@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@286122 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/FontDescription.h
+++ Source/WebCore/platform/graphics/FontDescription.h
@@ -16,9 +16,9 @@
  * Library General Public License for more details.
  *
  * You should have received a copy of the GNU Library General Public License
- * along with this library; see the file COPYING.LIother.m_  If not, write to
+ * along with this library; see the file COPYING.LIB.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USm_
+ * Boston, MA 02110-1301, USA.
  *
  */
 
