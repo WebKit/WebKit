413e73561f8da3a1c9cfafe74ea1810f37e5786f
Razvan Caliman <rcaliman@apple.com>
2021-12-09 19:39:43 +0000

    Web Inspector: Enable fuzzy matching for CSS completions https://bugs.webkit.org/show_bug.cgi?id=234092
    Reviewed by Patrick Angle.
    
    Flip the flag to enable fuzzy matching when autocompleting CSS properties and values.
    
    * UserInterface/Base/Setting.js:
    
    
    
    Canonical link: https://commits.webkit.org/245033@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@286792 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Base/Setting.js
+++ Source/WebInspectorUI/UserInterface/Base/Setting.js
@@ -231,7 +231,7 @@ WI.settings = {
     experimentalEnableStylesJumpToVariableDeclaration: new WI.Setting("experimental-styles-jump-to-variable-declaration", false),
     experimentalCollapseBlackboxedCallFrames: new WI.Setting("experimental-collapse-blackboxed-call-frames", false),
     experimentalAllowInspectingInspector: new WI.Setting("experimental-allow-inspecting-inspector", false),
-    experimentalCSSCompletionFuzzyMatching: new WI.Setting("experimental-css-completion-fuzzy-matching", false),
+    experimentalCSSCompletionFuzzyMatching: new WI.Setting("experimental-css-completion-fuzzy-matching", true),
 
     // Protocol
     protocolLogAsText: new WI.Setting("protocol-log-as-text", false),
