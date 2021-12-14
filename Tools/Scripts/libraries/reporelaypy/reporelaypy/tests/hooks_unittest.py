d1b61724f4a8366c7058d2fe238a2d2eccf42c48
Jonathan Bedard <jbedard@apple.com>
2021-12-14 21:31:59 +0000

    [reporelaypy] Update checkout with hook instead of polling https://bugs.webkit.org/show_bug.cgi?id=234243 <rdar://problem/86413065>
    Reviewed by Dewei Zhu.
    
    * Tools/Scripts/libraries/reporelaypy/reporelaypy/__init__.py: Bump version,
    export HookProcessor and HookReceiver.
    * Tools/Scripts/libraries/reporelaypy/reporelaypy/checkout.py:
    (Checkout.update_for): If a branch is new, we need to track it.
    * Tools/Scripts/libraries/reporelaypy/reporelaypy/database.py:
    (Database.__init__): Use self.host and self.password.
    * Tools/Scripts/libraries/reporelaypy/reporelaypy/hooks.py: Added.
    (HookProcessor): Class to process received hooks.
    (HookReceiver): Class to receive hooks and queue them for processing.
    * Tools/Scripts/libraries/reporelaypy/reporelaypy/tests/hooks_unittest.py: Added.
    (HooksUnittest):
    (HooksUnittest.setUp):
    (HooksUnittest.test_receive):
    (HooksUnittest.test_invalid):
    (HooksUnittest.test_process):
    (HooksUnittest.test_hmac):
    (HooksUnittest.test_invalid_hmac):
    * Tools/Scripts/libraries/reporelaypy/reporelaypy/webserver.py: Add hook routes,
    if hooks are enabled.
    * Tools/Scripts/libraries/reporelaypy/run: Allow caller to enable hooks.
    * Tools/Scripts/libraries/reporelaypy/setup.py: Bump version.
    
    
    Canonical link: https://commits.webkit.org/245242@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@287045 268f45cc-cd09-0410-ab3c-d52691b4dbfc

---
+++ Tools/Scripts/libraries/reporelaypy/reporelaypy/tests/hooks_unittest.py
@@ -0,0 +1,136 @@
+# Copyright (C) 2021 Apple Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import json
+import unittest
+
+from reporelaypy import Checkout, Database, HookProcessor, HookReceiver
+from webkitcorepy import testing, OutputCapture
+from webkitflaskpy import mock_app
+from webkitscmpy import mocks, Commit
+
+
+class HooksUnittest(testing.PathTestCase):
+    basepath = 'mock/repository'
+
+    def setUp(self):
+        super(HooksUnittest, self).setUp()
+        os.mkdir(os.path.join(self.path, '.git'))
+
+    @mock_app
+    def test_receive(self, app=None, client=None):
+        app.register_blueprint(HookReceiver(debug=True))
+        response = client.get('/hooks')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [])
+
+        response = client.post('/hooks', json=dict(ref='refs/heads/main'), headers={'X-GitHub-Event': 'push'})
+        self.assertEqual(response.status_code, 202)
+        self.assertEqual(response.json(), dict(
+            status='Success',
+            message='Hook queued for processing',
+        ))
+
+        response = client.get('/hooks')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [dict(data=dict(ref='refs/heads/main'), type='push')])
+
+    @mock_app
+    def test_invalid(self, app=None, client=None):
+        app.register_blueprint(HookReceiver(debug=True))
+        response = client.get('/hooks')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), [])
+
+        response = client.post('/hooks', json=['invalid'], headers={'X-GitHub-Event': 'push'})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), dict(
+            status='Unrecognized Hook',
+            message='Provided hook is in unrecognized format',
+        ))
+
+    @mock_app
+    def test_process(self, app=None, client=None):
+        with OutputCapture(), mocks.local.Git(self.path) as repo:
+            database = Database()
+            app.register_blueprint(HookReceiver(database=database, debug=True))
+
+            response = client.post('/hooks', json=dict(ref='refs/heads/main'), headers={'X-GitHub-Event': 'push'})
+            self.assertEqual(response.status_code, 202)
+
+            processor = HookProcessor(
+                Checkout(path=self.path, url=repo.remote, sentinal=False),
+                database=database,
+            )
+            processor.process_hooks()
+
+            response = client.get('/hooks')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json(), [])
+
+    @mock_app
+    def test_process_branch(self, app=None, client=None):
+        with OutputCapture(), mocks.local.Git(self.path) as repo:
+            database = Database()
+            app.register_blueprint(HookReceiver(database=database, debug=True))
+
+            response = client.post('/hooks', json=dict(ref='refs/heads/branch-a'), headers={'X-GitHub-Event': 'push'})
+            self.assertEqual(response.status_code, 202)
+
+            processor = HookProcessor(
+                Checkout(path=self.path, url=repo.remote, sentinal=False),
+                database=database,
+            )
+            processor.process_hooks()
+
+            response = client.get('/hooks')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json(), [])
+
+    @mock_app
+    def test_hmac(self, app=None, client=None):
+        app.register_blueprint(HookReceiver(debug=True, secret='secret'))
+
+        response = client.post(
+            '/hooks', json=dict(ref='refs/heads/main'),
+            headers={
+                'X-Hub-Signature-256': 'sha256=36a089f93b92e972b714e8c0f008873a206c690a8aee946a787eeb0f23e131b2',
+                'X-GitHub-Event': 'push',
+            },
+        )
+        self.assertEqual(response.status_code, 202)
+        self.assertEqual(response.json(), dict(
+            status='Success',
+            message='Hook queued for processing',
+        ))
+
+    @mock_app
+    def test_invalid_hmac(self, app=None, client=None):
+        app.register_blueprint(HookReceiver(debug=True, secret='secret'))
+
+        response = client.post('/hooks', json=dict(ref='refs/heads/main'), headers={'X-GitHub-Event': 'push'})
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.json(), dict(
+            status='Unauthorized Hook',
+            message='HMAC verification failed failed',
+        ))
