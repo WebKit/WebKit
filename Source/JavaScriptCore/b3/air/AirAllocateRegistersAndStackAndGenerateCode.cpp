WebKit/WebKit

commit 0c3a8460bc1633c15bb22c73024d33a119d31cfa

Add support for Wasm exceptions in the Air generator https://bugs.webkit.org/show_bug.cgi?id=231211 <rdar://problem/84132861>
Reviewed by Filip Pizlo.

This patch adds support to Air for Wasm exceptions. The implementation
is very similar to how we implement it in the B3 Wasm tier. This patch
shares code with the B3 tier where it can.

This patch also fixes a bug where you the early clobbered registers
of a patchpoint could prevent the prior instruction from register allocating.
For example, you can have the instructions I1, I2. Where I2 clobbers the
entire register file. It doesn't mean I1 shouldn't be able to allocate
registers. Instead, the clobber should occur after I1 executes. This patch
fixes the issue.

* JavaScriptCore.xcodeproj/project.pbxproj:
* b3/air/AirAllocateRegistersAndStackAndGenerateCode.cpp:
(JSC::B3::Air::GenerateAndAllocateRegisters::generate):
* wasm/WasmAirIRGenerator.cpp:
(JSC::Wasm::AirIRGenerator::ControlData::ControlData):
(JSC::Wasm::AirIRGenerator::ControlData::isTry):
(JSC::Wasm::AirIRGenerator::ControlData::isCatch):
(JSC::Wasm::AirIRGenerator::ControlData::convertTryToCatch):
(JSC::Wasm::AirIRGenerator::ControlData::convertTryToCatchAll):
(JSC::Wasm::AirIRGenerator::ControlData::tryStart const):
(JSC::Wasm::AirIRGenerator::ControlData::tryEnd const):
(JSC::Wasm::AirIRGenerator::ControlData::tryDepth const):
(JSC::Wasm::AirIRGenerator::ControlData::catchKind const):
(JSC::Wasm::AirIRGenerator::ControlData::exception const):
(JSC::Wasm::AirIRGenerator::emitCallPatchpoint):
(JSC::Wasm::AirIRGenerator::addStackMap):
(JSC::Wasm::AirIRGenerator::takeStackmaps):
(JSC::Wasm::AirIRGenerator::takeExceptionHandlers):
(JSC::Wasm::AirIRGenerator::newTmp):
(JSC::Wasm::AirIRGenerator::emitPatchpoint):
(JSC::Wasm::AirIRGenerator::emitLoad):
(JSC::Wasm::AirIRGenerator::AirIRGenerator):
(JSC::Wasm::AirIRGenerator::finalizeEntrypoints):
(JSC::Wasm::AirIRGenerator::forEachLiveValue):
(JSC::Wasm::AirIRGenerator::emitLoopTierUpCheck):

Source/JavaScriptCore/b3/air/AirAllocateRegistersAndStackAndGenerateCode.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Apple Inc. All rights reserved.
+ * Copyright (C) 2019-2022 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -612,16 +612,18 @@ void GenerateAndAllocateRegisters::generate(CCallHelpers& jit)
             });
 
             RegisterSet clobberedRegisters;
+            RegisterSet earlyNextClobberedRegisters;
             {
                 Inst* nextInst = block->get(instIndex + 1);
                 if (inst.kind.opcode == Patch || (nextInst && nextInst->kind.opcode == Patch)) {
                     if (inst.kind.opcode == Patch)
                         clobberedRegisters.merge(inst.extraClobberedRegs());
                     if (nextInst && nextInst->kind.opcode == Patch)
-                        clobberedRegisters.merge(nextInst->extraEarlyClobberedRegs());
+                        earlyNextClobberedRegisters.merge(nextInst->extraEarlyClobberedRegs());
 
                     clobberedRegisters.filter(m_allowedRegisters);
                     clobberedRegisters.exclude(m_namedDefdRegs);
+                    earlyNextClobberedRegisters.filter(m_allowedRegisters);
 
                     m_namedDefdRegs.merge(clobberedRegisters);
                 }
@@ -739,30 +741,15 @@ void GenerateAndAllocateRegisters::generate(CCallHelpers& jit)
                 inst.reportUsedRegisters(registerSet);
             }
 
-            if (inst.isTerminal() && block->numSuccessors()) {
-                // By default, we spill everything between block boundaries. However, we have a small
-                // heuristic to pass along register state. We should eventually make this better.
-                // What we do now is if we have a successor with a single predecessor (us), and we
-                // haven't yet generated code for it, we give it our register state. If all our successors
-                // can take on our register state, we don't flush at the end of this block.
-
-                bool everySuccessorGetsOurRegisterState = true;
-                for (unsigned i = 0; i < block->numSuccessors(); ++i) {
-                    BasicBlock* successor = block->successorBlock(i);
-                    if (successor->numPredecessors() == 1 && !context.blockLabels[successor]->isSet())
-                        currentAllocationMap[successor] = currentAllocation;
-                    else
-                        everySuccessorGetsOurRegisterState = false;
+            auto clobber = [&] (const RegisterSet& set) {
+                for (Reg reg : set) {
+                    Tmp tmp(reg);
+                    ASSERT(currentAllocation[reg] == tmp);
+                    m_availableRegs[tmp.bank()].set(reg);
+                    m_currentAllocation->at(reg) = Tmp();
+                    m_map[tmp].reg = Reg();
                 }
-                if (!everySuccessorGetsOurRegisterState) {
-                    for (Tmp tmp : m_liveness->liveAtTail(block)) {
-                        if (tmp.isReg() && isDisallowedRegister(tmp.reg()))
-                            continue;
-                        if (Reg reg = m_map[tmp].reg)
-                            flush(tmp, reg);
-                    }
-                }
-            }
+            };
 
             if (!inst.isTerminal()) {
                 CCallHelpers::Jump jump;
@@ -770,15 +757,41 @@ void GenerateAndAllocateRegisters::generate(CCallHelpers& jit)
                     jump = inst.generate(*m_jit, context);
                 ASSERT_UNUSED(jump, !jump.isSet());
 
-                for (Reg reg : clobberedRegisters) {
-                    Tmp tmp(reg);
-                    ASSERT(currentAllocation[reg] == tmp);
-                    m_availableRegs[tmp.bank()].set(reg);
-                    m_currentAllocation->at(reg) = Tmp();
-                    m_map[tmp].reg = Reg();
-                }
+                allocNamed(earlyNextClobberedRegisters, true);
+                clobber(clobberedRegisters);
+                clobber(earlyNextClobberedRegisters);
             } else {
                 ASSERT(needsToGenerate);
+
+                clobber(clobberedRegisters);
+                ASSERT(earlyNextClobberedRegisters.isEmpty());
+
+                if (block->numSuccessors()) {
+                    // By default, we spill everything between block boundaries. However, we have a small
+                    // heuristic to pass along register state. We should eventually make this better.
+                    // What we do now is if we have a successor with a single predecessor (us), and we
+                    // haven't yet generated code for it, we give it our register state. If all our successors
+                    // can take on our register state, we don't flush at the end of this block.
+
+
+                    bool everySuccessorGetsOurRegisterState = true;
+                    for (unsigned i = 0; i < block->numSuccessors(); ++i) {
+                        BasicBlock* successor = block->successorBlock(i);
+                        if (successor->numPredecessors() == 1 && !context.blockLabels[successor]->isSet())
+                            currentAllocationMap[successor] = currentAllocation;
+                        else
+                            everySuccessorGetsOurRegisterState = false;
+                    }
+                    if (!everySuccessorGetsOurRegisterState) {
+                        for (Tmp tmp : m_liveness->liveAtTail(block)) {
+                            if (tmp.isReg() && isDisallowedRegister(tmp.reg()))
+                                continue;
+                            if (Reg reg = m_map[tmp].reg)
+                                flush(tmp, reg);
+                        }
+                    }
+                }
+
                 if (inst.kind.opcode == Jump && block->successorBlock(0) == m_code.findNextBlock(block))
                     needsToGenerate = false;
 

Source/JavaScriptCore/JavaScriptCore.xcodeproj/project.pbxproj
@@ -900,6 +900,7 @@
 		52E65A1E27682771002B4C0A /* B3EstimateStaticExecutionCounts.h in Headers */ = {isa = PBXBuildFile; fileRef = 52E65A1B27682760002B4C0A /* B3EstimateStaticExecutionCounts.h */; };
 		52EED7942492B870008F4C93 /* FunctionAllowlist.h in Headers */ = {isa = PBXBuildFile; fileRef = 52EED7932492B868008F4C93 /* FunctionAllowlist.h */; };
 		52F6C35E1E71EB080081F4CC /* WebAssemblyWrapperFunction.h in Headers */ = {isa = PBXBuildFile; fileRef = 52F6C35C1E71EB080081F4CC /* WebAssemblyWrapperFunction.h */; };
+		52FDABC32788076B00C15B59 /* WasmIRGeneratorHelpers.h in Headers */ = {isa = PBXBuildFile; fileRef = 52FDABC22788076900C15B59 /* WasmIRGeneratorHelpers.h */; };
 		530A66B91FA3E78B0026A545 /* UnifiedSource3-mm.mm in Sources */ = {isa = PBXBuildFile; fileRef = 530A66B11FA3E77A0026A545 /* UnifiedSource3-mm.mm */; };
 		530A66BA1FA3E78B0026A545 /* UnifiedSource4-mm.mm in Sources */ = {isa = PBXBuildFile; fileRef = 530A66B81FA3E77E0026A545 /* UnifiedSource4-mm.mm */; };
 		530A66BB1FA3E78B0026A545 /* UnifiedSource5-mm.mm in Sources */ = {isa = PBXBuildFile; fileRef = 530A66B51FA3E77D0026A545 /* UnifiedSource5-mm.mm */; };
@@ -3760,6 +3761,7 @@
 		52EED7932492B868008F4C93 /* FunctionAllowlist.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = FunctionAllowlist.h; sourceTree = "<group>"; };
 		52F6C35B1E71EB080081F4CC /* WebAssemblyWrapperFunction.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = WebAssemblyWrapperFunction.cpp; path = js/WebAssemblyWrapperFunction.cpp; sourceTree = "<group>"; };
 		52F6C35C1E71EB080081F4CC /* WebAssemblyWrapperFunction.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = WebAssemblyWrapperFunction.h; path = js/WebAssemblyWrapperFunction.h; sourceTree = "<group>"; };
+		52FDABC22788076900C15B59 /* WasmIRGeneratorHelpers.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = WasmIRGeneratorHelpers.h; sourceTree = "<group>"; };
 		5300740C22DD6F6600B9ACB3 /* JSFinalizationRegistry.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; path = JSFinalizationRegistry.cpp; sourceTree = "<group>"; };
 		530A63401FA3E31C0026A545 /* SourcesCocoa.txt */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text; path = SourcesCocoa.txt; sourceTree = "<group>"; };
 		530A63411FA3E31D0026A545 /* Sources.txt */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text; path = Sources.txt; sourceTree = "<group>"; };
@@ -7305,6 +7307,7 @@
 				E38350092390D9370036316D /* WasmGlobal.h */,
 				148521D526EAEBDF00CC1D1A /* WasmHandlerInfo.cpp */,
 				148521D726EAEBFE00CC1D1A /* WasmHandlerInfo.h */,
+				52FDABC22788076900C15B59 /* WasmIRGeneratorHelpers.h */,
 				AD8FF3961EB5BD850087FF82 /* WasmIndexOrName.cpp */,
 				AD8FF3951EB5BD850087FF82 /* WasmIndexOrName.h */,
 				AD5C36DE1F699EB6000BCAAF /* WasmInstance.cpp */,
@@ -11160,6 +11163,7 @@
 				E3A32BC71FC83147007D7E76 /* WeakMapImpl.h in Headers */,
 				E393ADD81FE702D00022D681 /* WeakMapImplInlines.h in Headers */,
 				A7CA3AE617DA41AE006538AF /* WeakMapPrototype.h in Headers */,
+				52FDABC32788076B00C15B59 /* WasmIRGeneratorHelpers.h in Headers */,
 				539930C822AD3B9A0051CDE2 /* WeakObjectRefConstructor.h in Headers */,
 				539BFBAE22AD3C3A0023F4C0 /* WeakObjectRefPrototype.h in Headers */,
 				14E84FA114EE1ACC00D6D5D4 /* WeakSet.h in Headers */,

Source/JavaScriptCore/wasm/WasmB3IRGenerator.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2021 Apple Inc. All rights reserved.
+ * Copyright (C) 2016-2022 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -57,6 +57,7 @@
 #include "WasmContextInlines.h"
 #include "WasmExceptionType.h"
 #include "WasmFunctionParser.h"
+#include "WasmIRGeneratorHelpers.h"
 #include "WasmInstance.h"
 #include "WasmMemory.h"
 #include "WasmOSREntryData.h"
@@ -86,16 +87,6 @@ namespace WasmB3IRGeneratorInternal {
 static constexpr bool verbose = false;
 }
 }
-
-class B3IRGenerator;
-struct PatchpointExceptionHandle {
-    void generate(CCallHelpers&, const B3::StackmapGenerationParams&, B3IRGenerator*) const;
-
-    static constexpr unsigned s_invalidCallSiteIndex =  std::numeric_limits<unsigned>::max();
-
-    unsigned m_callSiteIndex { s_invalidCallSiteIndex };
-    unsigned m_numLiveValues;
-};
 
 class B3IRGenerator {
 public:
@@ -578,21 +569,6 @@ private:
     Vector<UnlinkedHandlerInfo> m_exceptionHandlers;
 };
 
-void PatchpointExceptionHandle::generate(CCallHelpers& jit, const B3::StackmapGenerationParams& params, B3IRGenerator* generator) const
-{
-    if (m_callSiteIndex == s_invalidCallSiteIndex)
-        return;
-
-    StackMap values(m_numLiveValues);
-    unsigned paramsOffset = params.size() - m_numLiveValues;
-    unsigned childrenOffset = params.value()->numChildren() - m_numLiveValues;
-    for (unsigned i = 0; i < m_numLiveValues; ++i)
-        values[i] = OSREntryValue(params[i + paramsOffset], params.value()->child(i + childrenOffset)->type());
-
-    generator->addStackMap(m_callSiteIndex, WTFMove(values));
-    jit.store32(CCallHelpers::TrustedImm32(m_callSiteIndex), CCallHelpers::tagFor(CallFrameSlot::argumentCountIncludingThis));
-}
-
 // Memory accesses in WebAssembly have unsigned 32-bit offsets, whereas they have signed 32-bit offsets in B3.
 int32_t B3IRGenerator::fixupPointerPlusOffset(Value*& ptr, uint32_t offset)
 {
@@ -857,56 +833,13 @@ Value* B3IRGenerator::framePointer()
     return m_framePointer;
 }
 
-static void buildEntryBufferForCatch(Probe::Context& context)
-{
-    CallFrame* callFrame = context.fp<CallFrame*>();
-    CallSiteIndex callSiteIndex = callFrame->callSiteIndex();
-    OptimizingJITCallee* callee = bitwise_cast<OptimizingJITCallee*>(callFrame->callee().asWasmCallee());
-    const StackMap& stackmap = callee->stackmap(callSiteIndex);
-    VM* vm = context.gpr<VM*>(GPRInfo::regT0);
-    uint64_t* buffer = vm->wasmContext.scratchBufferForSize(stackmap.size() * 8);
-    loadValuesIntoBuffer(context, stackmap, buffer);
-
-    context.gpr(GPRInfo::argumentGPR0) = bitwise_cast<uintptr_t>(buffer);
-}
-
 void B3IRGenerator::insertEntrySwitch()
 {
     m_proc.setNumEntrypoints(m_rootBlocks.size());
 
     Ref<B3::Air::PrologueGenerator> catchPrologueGenerator = createSharedTask<B3::Air::PrologueGeneratorFunction>([] (CCallHelpers& jit, B3::Air::Code& code) {
         AllowMacroScratchRegisterUsage allowScratch(jit);
-
-        jit.emitGetFromCallFrameHeaderPtr(CallFrameSlot::callee, GPRInfo::regT0);
-        {
-            // FIXME: Handling precise allocations in WasmB3IRGenerator catch entrypoints might be unnecessary
-            // https://bugs.webkit.org/show_bug.cgi?id=231213
-            auto preciseAllocationCase = jit.branchTestPtr(CCallHelpers::NonZero, GPRInfo::regT0, CCallHelpers::TrustedImm32(PreciseAllocation::halfAlignment));
-            jit.andPtr(CCallHelpers::TrustedImmPtr(MarkedBlock::blockMask), GPRInfo::regT0);
-            jit.loadPtr(CCallHelpers::Address(GPRInfo::regT0, MarkedBlock::offsetOfFooter + MarkedBlock::Footer::offsetOfVM()), GPRInfo::regT0);
-            auto loadedCase = jit.jump();
-
-            preciseAllocationCase.link(&jit);
-            jit.loadPtr(CCallHelpers::Address(GPRInfo::regT0, PreciseAllocation::offsetOfWeakSet() + WeakSet::offsetOfVM() - PreciseAllocation::headerSize()), GPRInfo::regT0);
-
-            loadedCase.link(&jit);
-        }
-        jit.restoreCalleeSavesFromVMEntryFrameCalleeSavesBuffer(GPRInfo::regT0, GPRInfo::regT3);
-
-        jit.loadPtr(CCallHelpers::Address(GPRInfo::regT0, VM::calleeForWasmCatchOffset()), GPRInfo::regT3);
-        jit.storePtr(CCallHelpers::TrustedImmPtr(nullptr), CCallHelpers::Address(GPRInfo::regT0, VM::calleeForWasmCatchOffset()));
-        jit.emitPutToCallFrameHeader(GPRInfo::regT3, CallFrameSlot::callee);
-
-        jit.load64(CCallHelpers::Address(GPRInfo::regT0, VM::callFrameForCatchOffset()), GPRInfo::callFrameRegister);
-        jit.storePtr(CCallHelpers::TrustedImmPtr(nullptr), CCallHelpers::Address(GPRInfo::regT0, VM::callFrameForCatchOffset()));
-
-        jit.loadPtr(CCallHelpers::Address(GPRInfo::callFrameRegister, CallFrameSlot::thisArgument * sizeof(Register)), GPRInfo::regT3);
-        jit.loadPtr(CCallHelpers::Address(GPRInfo::regT3, JSWebAssemblyInstance::offsetOfInstance()), GPRInfo::regT3);
-        jit.storeWasmContextInstance(GPRInfo::regT3);
-
-        jit.probe(tagCFunction<JITProbePtrTag>(buildEntryBufferForCatch), nullptr);
-
-        jit.addPtr(CCallHelpers::TrustedImm32(-code.frameSize()), GPRInfo::callFrameRegister, CCallHelpers::stackPointerRegister);
+        emitCatchPrologueShared(code, jit);
     });
 
     for (unsigned i = 1; i < m_rootBlocks.size(); ++i)
@@ -2652,6 +2585,10 @@ Value* B3IRGenerator::emitCatchImpl(CatchKind kind, ControlType& data, unsigned 
         else
             data.convertTryToCatchAll(++m_callSiteIndex, m_proc.addVariable(pointerType()));
     }
+    // We convert from "try" to "catch" ControlType above. This doesn't
+    // happen if ControlType is already a "catch". This can happen when
+    // we have multiple catches like "try {} catch(A){} catch(B){}...CatchAll(E){}"
+    ASSERT(ControlType::isAnyCatch(data));
 
     HandlerType handlerType = kind == CatchKind::Catch ? HandlerType::Catch : HandlerType::CatchAll;
     m_exceptionHandlers.append({ handlerType, data.tryStart(), data.tryEnd(), 0, m_tryCatchDepth, exceptionIndex });
@@ -2722,30 +2659,8 @@ auto B3IRGenerator::addThrow(unsigned exceptionIndex, Vector<ExpressionType>& ar
     PatchpointExceptionHandle handle = preparePatchpointForExceptions(m_currentBlock, patch);
     patch->setGenerator([this, exceptionIndex, handle] (CCallHelpers& jit, const B3::StackmapGenerationParams& params) {
         AllowMacroScratchRegisterUsage allowScratch(jit);
-        GPRReg scratch = GPRInfo::nonPreservedNonArgumentGPR0;
         handle.generate(jit, params, this);
-
-        jit.loadPtr(CCallHelpers::Address(GPRInfo::argumentGPR0, Instance::offsetOfOwner()), scratch);
-        {
-            auto preciseAllocationCase = jit.branchTestPtr(CCallHelpers::NonZero, scratch, CCallHelpers::TrustedImm32(PreciseAllocation::halfAlignment));
-            jit.andPtr(CCallHelpers::TrustedImmPtr(MarkedBlock::blockMask), scratch);
-            jit.loadPtr(CCallHelpers::Address(scratch, MarkedBlock::offsetOfFooter + MarkedBlock::Footer::offsetOfVM()), scratch);
-            auto loadedCase = jit.jump();
-
-            preciseAllocationCase.link(&jit);
-            jit.loadPtr(CCallHelpers::Address(scratch, PreciseAllocation::offsetOfWeakSet() + WeakSet::offsetOfVM() - PreciseAllocation::headerSize()), scratch);
-
-            loadedCase.link(&jit);
-        }
-        jit.copyCalleeSavesToVMEntryFrameCalleeSavesBuffer(scratch);
-
-        jit.move(MacroAssembler::TrustedImm32(exceptionIndex), GPRInfo::argumentGPR2);
-        jit.move(MacroAssembler::stackPointerRegister, GPRInfo::argumentGPR3);
-        CCallHelpers::Call call = jit.call(OperationPtrTag);
-        jit.farJump(GPRInfo::returnValueGPR, ExceptionHandlerPtrTag);
-        jit.addLinkTask([call] (LinkBuffer& linkBuffer) {
-            linkBuffer.link(call, FunctionPtr<OperationPtrTag>(operationWasmThrow));
-        });
+        emitThrowImpl(jit, exceptionIndex); 
     });
     m_currentBlock->append(patch);
 
@@ -2763,28 +2678,8 @@ auto B3IRGenerator::addRethrow(unsigned, ControlType& data) -> PartialResult
     PatchpointExceptionHandle handle = preparePatchpointForExceptions(m_currentBlock, patch);
     patch->setGenerator([this, handle] (CCallHelpers& jit, const B3::StackmapGenerationParams& params) {
         AllowMacroScratchRegisterUsage allowScratch(jit);
-
-        GPRReg scratch = GPRInfo::nonPreservedNonArgumentGPR0;
-        jit.loadPtr(CCallHelpers::Address(GPRInfo::argumentGPR0, Instance::offsetOfOwner()), scratch);
-        {
-            auto preciseAllocationCase = jit.branchTestPtr(CCallHelpers::NonZero, scratch, CCallHelpers::TrustedImm32(PreciseAllocation::halfAlignment));
-            jit.andPtr(CCallHelpers::TrustedImmPtr(MarkedBlock::blockMask), scratch);
-            jit.loadPtr(CCallHelpers::Address(scratch, MarkedBlock::offsetOfFooter + MarkedBlock::Footer::offsetOfVM()), scratch);
-            auto loadedCase = jit.jump();
-
-            preciseAllocationCase.link(&jit);
-            jit.loadPtr(CCallHelpers::Address(scratch, PreciseAllocation::offsetOfWeakSet() + WeakSet::offsetOfVM() - PreciseAllocation::headerSize()), scratch);
-
-            loadedCase.link(&jit);
-        }
-        jit.copyCalleeSavesToVMEntryFrameCalleeSavesBuffer(scratch);
-
         handle.generate(jit, params, this);
-        CCallHelpers::Call call = jit.call(OperationPtrTag);
-        jit.farJump(GPRInfo::returnValueGPR, ExceptionHandlerPtrTag);
-        jit.addLinkTask([call] (LinkBuffer& linkBuffer) {
-            linkBuffer.link(call, FunctionPtr<OperationPtrTag>(operationWasmRethrow));
-        });
+        emitRethrowImpl(jit);
     });
     m_currentBlock->append(patch);
 
@@ -3318,26 +3213,6 @@ void computePCToCodeOriginMap(CompilationContext& context, LinkBuffer& linkBuffe
         B3::PCToOriginMap originMap = context.procedure->releasePCToOriginMap();
         context.pcToCodeOriginMap = Box<PCToCodeOriginMap>::create(PCToCodeOriginMapBuilder(PCToCodeOriginMapBuilder::WasmCodeOriginMap, WTFMove(originMap)), linkBuffer);
     }
-}
-
-void computeExceptionHandlerLocations(Vector<CodeLocationLabel<ExceptionHandlerPtrTag>>& handlers, const InternalFunction* function, const CompilationContext& context, LinkBuffer& linkBuffer)
-{
-    if (!context.procedure)
-        return;
-
-    unsigned entrypointIndex = 0;
-    unsigned numEntrypoints = context.procedure->numEntrypoints();
-    for (const UnlinkedHandlerInfo& handlerInfo : function->exceptionHandlers) {
-        RELEASE_ASSERT(entrypointIndex < numEntrypoints);
-        if (handlerInfo.m_type == HandlerType::Delegate) {
-            handlers.append({ });
-            continue;
-        }
-
-        ++entrypointIndex;
-        handlers.append(linkBuffer.locationOf<ExceptionHandlerPtrTag>(context.procedure->code().entrypointLabel(entrypointIndex)));
-    }
-    RELEASE_ASSERT(entrypointIndex == numEntrypoints - 1);
 }
 
 // Custom wasm ops. These are the ones too messy to do in wasm.json.

Source/JavaScriptCore/wasm/WasmB3IRGenerator.h
@@ -59,7 +59,6 @@ struct CompilationContext {
 
 Expected<std::unique_ptr<InternalFunction>, String> parseAndCompile(CompilationContext&, const FunctionData&, const Signature&, Vector<UnlinkedWasmToWasmCall>&, unsigned& osrEntryScratchBufferSize, const ModuleInformation&, MemoryMode, CompilationMode, uint32_t functionIndex, uint32_t loopIndexForOSREntry, TierUpCount* = nullptr);
 
-void computeExceptionHandlerLocations(Vector<CodeLocationLabel<ExceptionHandlerPtrTag>>& handlers, const InternalFunction*, const CompilationContext&, LinkBuffer&);
 void computePCToCodeOriginMap(CompilationContext&, LinkBuffer&);
 
 } } // namespace JSC::Wasm

Source/JavaScriptCore/wasm/WasmBBQPlan.cpp
@@ -36,6 +36,7 @@
 #include "WasmCallee.h"
 #include "WasmCalleeGroup.h"
 #include "WasmCalleeRegistry.h"
+#include "WasmIRGeneratorHelpers.h"
 #include "WasmSignatureInlines.h"
 #include "WasmTierUpCount.h"
 #include <wtf/DataLog.h>
@@ -202,8 +203,6 @@ std::unique_ptr<InternalFunction> BBQPlan::compileFunction(uint32_t functionInde
     // as BBQ Air bloats such lengthy Wasm code and will consume a large amount of executable memory.
     bool forceUsingB3 = false;
     if (Options::webAssemblyBBQAirModeThreshold() && m_moduleInformation->codeSectionSize >= Options::webAssemblyBBQAirModeThreshold())
-        forceUsingB3 = true;
-    else if (!m_moduleInformation->m_functionDoesNotUseExceptions.quickGet(functionIndex))
         forceUsingB3 = true;
     else if (!Options::wasmBBQUsesAir())
         forceUsingB3 = true;

commit 03456a7a629de0ed610ee2914bb6a725a5e73cd9

Unreviewed. Appease an assertion that was broken by r287801 by slightly refactoring code so we don't clobber the same named register twice.
* b3/air/AirAllocateRegistersAndStackAndGenerateCode.cpp:
(JSC::B3::Air::GenerateAndAllocateRegisters::generate):




Canonical link: https://commits.webkit.org/245858@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@287806 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/b3/air/AirAllocateRegistersAndStackAndGenerateCode.cpp
@@ -758,8 +758,8 @@ void GenerateAndAllocateRegisters::generate(CCallHelpers& jit)
                 ASSERT_UNUSED(jump, !jump.isSet());
 
                 allocNamed(earlyNextClobberedRegisters, true);
+                clobberedRegisters.merge(earlyNextClobberedRegisters);
                 clobber(clobberedRegisters);
-                clobber(earlyNextClobberedRegisters);
             } else {
                 ASSERT(needsToGenerate);
 