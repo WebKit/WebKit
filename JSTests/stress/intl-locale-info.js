5d113de9bea9aadcb30005738aaae2f0d0bbc0a2
Yusuke Suzuki <ysuzuki@apple.com>
2022-01-11 06:34:39 +0000

    Unreviewed, add a bit more Intl.Locale hourCycle tests to investigate flakiness
    * stress/intl-locale-info.js:
    (let.l.new.Intl.Locale.shouldBe):
    (shouldBe):
    
    
    Canonical link: https://commits.webkit.org/245913@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@287869 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JSTests/stress/intl-locale-info.js
+++ JSTests/stress/intl-locale-info.js
@@ -22,6 +22,7 @@ function shouldBe(actual, expected) {
     let locale = new Intl.Locale("ar")
     shouldBe(JSON.stringify(locale.calendars), `["gregory","coptic","islamic","islamic-civil","islamic-tbla"]`);
     shouldBe(JSON.stringify(locale.collations), `["compat","emoji","eor"]`);
+    shouldBe(locale.hourCycle, undefined);
     shouldBe(JSON.stringify(locale.hourCycles), `["h12"]`);
     let ns = JSON.stringify(locale.numberingSystems);
     shouldBe(ns === `["arab"]` || ns === `["latn"]`, true);
@@ -31,6 +32,7 @@ function shouldBe(actual, expected) {
     let locale = new Intl.Locale("ar-EG")
     shouldBe(JSON.stringify(locale.calendars), `["gregory","coptic","islamic","islamic-civil","islamic-tbla"]`);
     shouldBe(JSON.stringify(locale.collations), `["compat","emoji","eor"]`);
+    shouldBe(locale.hourCycle, undefined);
     shouldBe(JSON.stringify(locale.hourCycles), `["h12"]`);
     shouldBe(JSON.stringify(locale.numberingSystems), `["arab"]`);
     shouldBe(JSON.stringify(locale.timeZones), `["Africa/Cairo"]`);
@@ -40,6 +42,7 @@ function shouldBe(actual, expected) {
     let calendars = JSON.stringify(locale.calendars);
     shouldBe(calendars === `["islamic-umalqura","islamic-rgsa","islamic","gregory"]` || calendars === `["islamic-umalqura","gregory","islamic","islamic-rgsa"]`, true);
     shouldBe(JSON.stringify(locale.collations), `["compat","emoji","eor"]`);
+    shouldBe(locale.hourCycle, undefined);
     shouldBe(JSON.stringify(locale.hourCycles), `["h12"]`);
     shouldBe(JSON.stringify(locale.numberingSystems), `["arab"]`);
     shouldBe(JSON.stringify(locale.timeZones), `["Asia/Riyadh"]`);
@@ -48,6 +51,7 @@ function shouldBe(actual, expected) {
     let locale = new Intl.Locale("ja")
     shouldBe(JSON.stringify(locale.calendars), `["gregory","japanese"]`);
     shouldBe(JSON.stringify(locale.collations), `["unihan","emoji","eor"]`);
+    shouldBe(locale.hourCycle, undefined);
     shouldBe(JSON.stringify(locale.hourCycles), `["h23"]`);
     shouldBe(JSON.stringify(locale.numberingSystems), `["latn"]`);
     shouldBe(locale.timeZones, undefined);
@@ -56,6 +60,7 @@ function shouldBe(actual, expected) {
     let locale = new Intl.Locale("ja-JP")
     shouldBe(JSON.stringify(locale.calendars), `["gregory","japanese"]`);
     shouldBe(JSON.stringify(locale.collations), `["unihan","emoji","eor"]`);
+    shouldBe(locale.hourCycle, undefined);
     shouldBe(JSON.stringify(locale.hourCycles), `["h23"]`);
     shouldBe(JSON.stringify(locale.numberingSystems), `["latn"]`);
     shouldBe(JSON.stringify(locale.timeZones), `["Asia/Tokyo"]`);
@@ -64,6 +69,7 @@ function shouldBe(actual, expected) {
     let locale = new Intl.Locale("en-US")
     shouldBe(JSON.stringify(locale.calendars), `["gregory"]`);
     shouldBe(JSON.stringify(locale.collations), `["emoji","eor"]`);
+    shouldBe(locale.hourCycle, undefined);
     shouldBe(JSON.stringify(locale.hourCycles), `["h12"]`);
     shouldBe(JSON.stringify(locale.numberingSystems), `["latn"]`);
     shouldBe(JSON.stringify(locale.timeZones), `["America/Adak","America/Anchorage","America/Boise","America/Chicago","America/Denver","America/Detroit","America/Indiana/Knox","America/Indiana/Marengo","America/Indiana/Petersburg","America/Indiana/Tell_City","America/Indiana/Vevay","America/Indiana/Vincennes","America/Indiana/Winamac","America/Indianapolis","America/Juneau","America/Kentucky/Monticello","America/Los_Angeles","America/Louisville","America/Menominee","America/Metlakatla","America/New_York","America/Nome","America/North_Dakota/Beulah","America/North_Dakota/Center","America/North_Dakota/New_Salem","America/Phoenix","America/Sitka","America/Yakutat","Pacific/Honolulu"]`);
@@ -72,6 +78,7 @@ function shouldBe(actual, expected) {
     let locale = new Intl.Locale("en-NZ")
     shouldBe(JSON.stringify(locale.calendars), `["gregory"]`);
     shouldBe(JSON.stringify(locale.collations), `["emoji","eor"]`);
+    shouldBe(locale.hourCycle, undefined);
     shouldBe(JSON.stringify(locale.hourCycles), `["h12"]`);
     shouldBe(JSON.stringify(locale.numberingSystems), `["latn"]`);
     shouldBe(JSON.stringify(locale.timeZones), `["Pacific/Auckland","Pacific/Chatham"]`);
@@ -80,6 +87,7 @@ function shouldBe(actual, expected) {
     let locale = new Intl.Locale("zh")
     shouldBe(JSON.stringify(locale.calendars), `["gregory","chinese"]`);
     shouldBe(JSON.stringify(locale.collations), `["pinyin","big5han","gb2312","stroke","unihan","zhuyin","emoji","eor"]`);
+    shouldBe(locale.hourCycle, undefined);
     shouldBe(JSON.stringify(locale.hourCycles), $vm.icuVersion() >= 69 ? `["h23"]` : `["h12"]`);
     shouldBe(JSON.stringify(locale.numberingSystems), `["latn"]`);
     shouldBe(locale.timeZones, undefined);
@@ -88,6 +96,7 @@ function shouldBe(actual, expected) {
     let locale = new Intl.Locale("zh-TW")
     shouldBe(JSON.stringify(locale.calendars), `["gregory","roc","chinese"]`);
     shouldBe(JSON.stringify(locale.collations), `["stroke","big5han","gb2312","pinyin","unihan","zhuyin","emoji","eor"]`);
+    shouldBe(locale.hourCycle, undefined);
     shouldBe(JSON.stringify(locale.hourCycles), `["h12"]`);
     shouldBe(JSON.stringify(locale.numberingSystems), `["latn"]`);
     shouldBe(JSON.stringify(locale.timeZones), `["Asia/Taipei"]`);
@@ -96,6 +105,7 @@ function shouldBe(actual, expected) {
     let locale = new Intl.Locale("zh-HK")
     shouldBe(JSON.stringify(locale.calendars), `["gregory","chinese"]`);
     shouldBe(JSON.stringify(locale.collations), `["stroke","big5han","gb2312","pinyin","unihan","zhuyin","emoji","eor"]`);
+    shouldBe(locale.hourCycle, undefined);
     shouldBe(JSON.stringify(locale.hourCycles), `["h12"]`);
     shouldBe(JSON.stringify(locale.numberingSystems), `["latn"]`);
     shouldBe(JSON.stringify(locale.timeZones), `["Asia/Hong_Kong"]`);
@@ -104,6 +114,7 @@ function shouldBe(actual, expected) {
     let locale = new Intl.Locale("fa")
     shouldBe(JSON.stringify(locale.calendars), `["persian","gregory","islamic","islamic-civil","islamic-tbla"]`);
     shouldBe(JSON.stringify(locale.collations), `["emoji","eor"]`);
+    shouldBe(locale.hourCycle, undefined);
     shouldBe(JSON.stringify(locale.hourCycles), `["h23"]`);
     shouldBe(JSON.stringify(locale.numberingSystems), `["arabext"]`);
     shouldBe(locale.timeZones, undefined);
