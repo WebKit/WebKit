ad456a91f766b7445b2623a1c803adc43de63a0a
Rob Buis <rbuis@igalia.com>
2022-01-19 19:13:02 +0000

    Null check player in taintsOrigin https://bugs.webkit.org/show_bug.cgi?id=234257
    Patch by Rob Buis <rbuis@igalia.com> on 2022-01-19
    Reviewed by Youenn Fablet.
    
    Source/WebCore:
    
    Null check player in taintsOrigin.
    
    Test: fast/media/createImageBitmap-from-video-crash.html
    
    * html/ImageBitmap.cpp:
    (WebCore::taintsOrigin):
    
    LayoutTests:
    
    * fast/media/createImageBitmap-from-video-crash-expected.txt: Added.
    * fast/media/createImageBitmap-from-video-crash.html: Added.
    
    
    Canonical link: https://commits.webkit.org/246178@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@288217 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/ImageBitmap.cpp
+++ Source/WebCore/html/ImageBitmap.cpp
@@ -163,7 +163,7 @@ static bool taintsOrigin(SecurityOrigin* origin, HTMLVideoElement& video)
     if (!video.hasSingleSecurityOrigin())
         return true;
 
-    if (video.player()->didPassCORSAccessCheck())
+    if (!video.player() || video.player()->didPassCORSAccessCheck())
         return false;
 
     auto url = video.currentSrc();
