a2526260243197e570cea90de6fb8261911b0d9b
Elliott Williams <emw@apple.com>
2022-01-26 19:22:04 +0000

    Support workspace builds in build-webkit https://bugs.webkit.org/show_bug.cgi?id=235550
    On Apple platforms, `build-webkit --use-workspace` now builds using WebKit.xcworkspace and
    XCBuild. This complements the `USE_WORKSPACE=YES` Make flag added in
    https://commits.webkit.org/246232@main.
    
    At this time, there is no guarantee of correctness when building with USE_WORKSPACE=YES, and
    workspace builds are not checked by builders.
    
    Patch by Elliott Williams <emw@apple.com> on 2022-01-26
    Reviewed by Alexey Proskuryakov.
    
    * Scripts/build-webkit: Add --use-workspace flag, correct platform hints in $usage.
    * Scripts/webkitdirs.pm: Remove canUseXCBuild, the flag it powered is no longer used.
    (buildXCodeWorkspace): Added.
    
    
    Canonical link: https://commits.webkit.org/246447@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@288632 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -1079,12 +1079,6 @@ sub XcodeStaticAnalyzerOption()
     return "RUN_CLANG_STATIC_ANALYZER=YES";
 }
 
-sub canUseXCBuild()
-{
-    determineXcodeVersion();
-    return (eval "v$xcodeVersion" ge v11.4)
-}
-
 my $passedConfiguration;
 my $searchedForPassedConfiguration;
 sub determinePassedConfiguration
@@ -2111,6 +2105,15 @@ sub buildXCodeProject($$@)
         }
     }
     return system "xcodebuild", "-project", "$project.xcodeproj", @extraOptions;
+}
+
+sub buildXCodeWorkspace($$$@)
+{
+    my ($workspace, $scheme, $clean, @extraOptions) = @_;
+    if ($clean) {
+        push @extraOptions, "clean";
+    }
+    return system "xcodebuild", "-workspace", $workspace, "-scheme", $scheme, @extraOptions;
 }
 
 sub getVisualStudioToolset()
