WebKit/WebKit

commit 34127cf5279b8a091c6e89cb4f554002c2e0e587

Distinguish contiguous SharedBuffer from non-contiguous one and guarantee immutability https://bugs.webkit.org/show_bug.cgi?id=233030 rdar://85333814
Reviewed by Darin Adler.

Source/WebCore:

When calling SharedBuffer::data() the underlying data was flattened,
and so could be mutated while being shared. This was an undesirable
behaviour.
We introduce ContiguousSharedBuffer which guarantee that its content is made
of contiguous data.
All methods that would have mutated a SharedBuffer when being flattened are
moved to ContiguousSharedBuffer.

Area where SharedBuffers were always created with a single segment where
converted to a ContiguousSharedBuffer.
Previously, a SharedBuffer would have been flattened only if it needed to be
and only when the data was going to be accessed; following this change
the conversion is now explictly done through a call to makeContiguous.
This conversion is a no-op if the SharedBuffer being converted was already
a ContiguousSharedBuffer.
With the nature of a SharedBuffer being shared by potentially multiple
users it was necessary to limit the number of conversions being performed
on demand to avoid redundancy.
When the flattening could have occured, the SharedBuffer creation points were
traced back and identified to make the buffer contiguous then.
In the end, there's no more SharedBuffer flattening operations than there
would have been before this change.
There's a potential increase of temporary memory usage with the
ScriptBufferSourceProvider class; bug 233511 is tracking it.

While the change is extensive, code coverage of the modified areas is thorough
which makes undetected regressions unlikely.

In bug 233442, a SharedBuffer will be made completely immutable once created.

No observable change.
Covered by all existing tests. New API tests creatded.

* Modules/async-clipboard/ClipboardItemBindingsDataSource.cpp:
(WebCore::ClipboardItemBindingsDataSource::ClipboardItemTypeLoader::sanitizeDataIfNeeded):

Source/WebCore/loader/cache/CachedRawResource.cpp
@@ -64,24 +64,24 @@ void CachedRawResource::updateBuffer(SharedBuffer& data)
 
     CachedResourceHandle<CachedRawResource> protectedThis(this);
     ASSERT(dataBufferingPolicy() == DataBufferingPolicy::BufferData);
-    m_data = &data;
+    m_data = data.makeContiguous();
 
     auto previousDataSize = encodedSize();
-    while (data.size() > previousDataSize) {
-        auto incrementalData = data.getSomeData(previousDataSize);
+    while (m_data->size() > previousDataSize) {
+        auto incrementalData = m_data->getSomeData(previousDataSize);
         previousDataSize += incrementalData.size();
 
         SetForScope<bool> notifyScope(m_inIncrementalDataNotify, true);
         notifyClientsDataWasReceived(incrementalData.data(), incrementalData.size());
     }
-    setEncodedSize(data.size());
+    setEncodedSize(m_data->size());
 
     if (dataBufferingPolicy() == DataBufferingPolicy::DoNotBufferData) {
         if (m_loader)
             m_loader->setDataBufferingPolicy(DataBufferingPolicy::DoNotBufferData);
         clear();
     } else
-        CachedResource::updateBuffer(data);
+        CachedResource::updateBuffer(*m_data);
 
     if (m_delayedFinishLoading) {
         auto delayedFinishLoading = std::exchange(m_delayedFinishLoading, std::nullopt);
@@ -107,12 +107,15 @@ void CachedRawResource::finishLoading(SharedBuffer* data, const NetworkLoadMetri
     CachedResourceHandle<CachedRawResource> protectedThis(this);
     DataBufferingPolicy dataBufferingPolicy = this->dataBufferingPolicy();
     if (dataBufferingPolicy == DataBufferingPolicy::BufferData) {
-        m_data = data;
-
-        if (auto incrementalData = calculateIncrementalDataChunk(data)) {
-            setEncodedSize(data->size());
-            notifyClientsDataWasReceived(incrementalData->data(), incrementalData->size());
-        }
+        if (data) {
+            if (data != m_data.get())
+                m_data = data->makeContiguous();
+            if (auto incrementalData = calculateIncrementalDataChunk(data)) {
+                setEncodedSize(data->size());
+                notifyClientsDataWasReceived(incrementalData->data(), incrementalData->size());
+            }
+        } else
+            m_data = nullptr;
     }
 
 #if USE(QUICK_LOOK)

commit d41572ff12f27ae9f3ffca71dde7ad0c5177ca3e

Cherry-pick r288667. rdar://problem/87830583
    REGRESSION(r287684) speedtest.net uses many GB of memory
    https://bugs.webkit.org/show_bug.cgi?id=235615
    rdar://87830583

    Reviewed by Youenn Fablet.

    The regression was introduced with r286937 and is a good example of
    errors introduced when attempting to optimise things too early.
    CachedRawResource::updateBuffer does a search in the accumulating
    resource's SharedBuffer, search that was taking O(log(n)+1) prior r286937
    where n is the number of DataView segments in the SharedBuffer.
    This was simplified as a O(1) operation by using the combined contiguous
    SharedBuffer instead.
    However, that caused every single intermediary accumulated buffers to be
    kept referenced by the XMLHttpRequest SharedBufferBuilder leading to
    massive memory use.
    In other words:
    For each update, we did the following steps:
        - Set m_data to a new big continuous chunk of data that stores all
          received data
        - Create a view of the new data as a SharedBuffer. This SharedBuffer
          references the big continuous chunk above
        - XHR stores a ref to the view, hence keep the big chunk alive.
    Each XHR chunk, although small in data that can be accessed, is actually keeping in memory all temporary created m_data chunks.
    Following this change, XHR will now only keeps a reference to the new DataSegment added since the last run rather than the entire previous content.

    Fly-by: add some comments describing the running of the method.

    * loader/cache/CachedRawResource.cpp:
    (WebCore::CachedRawResource::updateBuffer):

    Canonical link: https://commits.webkit.org/246473@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@288667 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Canonical link: https://commits.webkit.org/245886.77@safari-613.1.16.0-branch

Source/WebCore/loader/cache/CachedRawResource.cpp
@@ -62,19 +62,24 @@ void CachedRawResource::updateBuffer(const FragmentedSharedBuffer& data)
     if (m_inIncrementalDataNotify)
         return;
 
+    // We need to keep a strong reference to both the SharedBuffer and the current CachedRawResource instance
+    // as notifyClientsDataWasReceived call may delete both.
     CachedResourceHandle<CachedRawResource> protectedThis(this);
+    auto protectedData = Ref { data };
+
     ASSERT(dataBufferingPolicy() == DataBufferingPolicy::BufferData);
     m_data = data.makeContiguous();
 
+    // Notify clients only of the newly appended content since the last run.
     auto previousDataSize = encodedSize();
-    while (m_data->size() > previousDataSize) {
-        auto incrementalData = m_data->getSomeData(previousDataSize);
+    while (data.size() > previousDataSize) {
+        auto incrementalData = data.getSomeData(previousDataSize);
         previousDataSize += incrementalData.size();
 
         SetForScope<bool> notifyScope(m_inIncrementalDataNotify, true);
         notifyClientsDataWasReceived(incrementalData.createSharedBuffer());
     }
-    setEncodedSize(m_data->size());
+    setEncodedSize(data.size());
 
     if (dataBufferingPolicy() == DataBufferingPolicy::DoNotBufferData) {
         if (m_loader)