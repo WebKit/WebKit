65ba41c35e353f47461f3407242162735a8c98be
Commit Queue <commit-queue@webkit.org>
2022-02-04 15:47:03 +0000

    Unreviewed, reverting r288996. https://bugs.webkit.org/show_bug.cgi?id=236141
    Causing debug asserts
    
    Reverted changeset:
    
    "No breakpoints hit on github.com, and some are invalid"
    https://bugs.webkit.org/show_bug.cgi?id=235607
    https://commits.webkit.org/r288996
    
    
    Canonical link: https://commits.webkit.org/246809@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@289112 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/parser/Lexer.cpp
+++ Source/JavaScriptCore/parser/Lexer.cpp
@@ -712,7 +712,6 @@ void Lexer<T>::shiftLineTerminator()
         shift();
 
     ++m_lineNumber;
-    m_lineStart = m_code;
 }
 
 template <typename T>
@@ -2473,8 +2472,6 @@ start:
         m_buffer8.shrink(0);
         break;
     case CharacterQuote: {
-        auto startLineNumber = m_lineNumber;
-        auto startLineStartOffset = currentLineStartOffset();
         StringParseResult result = StringCannotBeParsed;
         if (lexerFlags.contains(LexerFlags::DontBuildStrings))
             result = parseString<false>(tokenData, strictMode);
@@ -2487,10 +2484,8 @@ start:
         }
         shift();
         token = STRING;
-        m_atLineStart = false;
-        fillTokenInfo(tokenRecord, token, startLineNumber, currentOffset(), startLineStartOffset, currentPosition());
-        return token;
-    }
+        break;
+        }
     case CharacterIdentifierStart: {
         if constexpr (ASSERT_ENABLED) {
             UChar32 codePoint;
@@ -2511,6 +2506,7 @@ start:
         shiftLineTerminator();
         m_atLineStart = true;
         m_hasLineTerminatorBeforeToken = true;
+        m_lineStart = m_code;
         goto start;
     case CharacterHash: {
         // Hashbang is only permitted at the start of the source text.
@@ -2571,6 +2567,7 @@ inSingleLineComment:
         shiftLineTerminator();
         m_atLineStart = true;
         m_hasLineTerminatorBeforeToken = true;
+        m_lineStart = m_code;
         if (!lastTokenWasRestrKeyword())
             goto start;
 
@@ -2702,9 +2699,6 @@ JSTokenType Lexer<T>::scanTemplateString(JSToken* tokenRecord, RawStringsBuildMo
     ASSERT(!m_error);
     ASSERT(m_buffer16.isEmpty());
 
-    int startingLineStartOffset = currentLineStartOffset();
-    int startingLineNumber = lineNumber();
-
     // Leading backquote ` (for template head) or closing brace } (for template trailing) are already shifted in the previous token scan.
     // So in this re-scan phase, shift() is not needed here.
     StringParseResult result = parseTemplateLiteral(tokenData, rawStringsBuildMode);
@@ -2717,7 +2711,7 @@ JSTokenType Lexer<T>::scanTemplateString(JSToken* tokenRecord, RawStringsBuildMo
 
     // Since TemplateString always ends with ` or }, m_atLineStart always becomes false.
     m_atLineStart = false;
-    fillTokenInfo(tokenRecord, token, startingLineNumber, currentOffset(), startingLineStartOffset, currentPosition());
+    fillTokenInfo(tokenRecord, token, m_lineNumber, currentOffset(), currentLineStartOffset(), currentPosition());
     return token;
 }
 
