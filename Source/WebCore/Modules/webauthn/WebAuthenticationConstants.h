92c9c2462ad0fe4796a6d7b1d53739ed875e0ec0
Commit Queue <commit-queue@webkit.org>
2022-02-04 02:26:07 +0000

    Unreviewed, reverting r289096. https://bugs.webkit.org/show_bug.cgi?id=236114
    will break build
    
    Reverted changeset:
    
    "[WebAuthn] Remove misspelled constant
    "LocalAuthenticatiorAccessGroup""
    https://bugs.webkit.org/show_bug.cgi?id=235894
    https://commits.webkit.org/r289096
    
    
    Canonical link: https://commits.webkit.org/246796@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@289099 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/webauthn/WebAuthenticationConstants.h
+++ Source/WebCore/Modules/webauthn/WebAuthenticationConstants.h
@@ -77,6 +77,9 @@ enum class ClientDataType : bool {
     Get
 };
 
+// rdar://88104045 - Remove once staged change completed
+const char LocalAuthenticatiorAccessGroup[] = "com.apple.webkit.webauthn";
+
 constexpr const char LocalAuthenticatorAccessGroup[] = "com.apple.webkit.webauthn";
 
 // Credential serialization
