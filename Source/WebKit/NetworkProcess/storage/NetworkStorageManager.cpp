WebKit/WebKit

commit c8435bc6894ec6f116f9bc5a09b0fd10ae3124ae

Merge StorageManager with NetworkStorageManager to manage WebStorage by origin https://bugs.webkit.org/show_bug.cgi?id=234087
Reviewed by Chris Dumez.

Source/WebCore:

* platform/sql/SQLiteFileSystem.cpp:
(WebCore::SQLiteFileSystem::deleteDatabaseFile):
(WebCore::SQLiteFileSystem::moveDatabaseFile):
(WebCore::SQLiteFileSystem::databaseFileSize):
* platform/sql/SQLiteFileSystem.h:

Source/WebKit:

In network process, WebStorage data is managed by WebKit::StorageManager class. Each StorageManager owns one or
many StorageNameSpaces, and each StorageNamespace owns one or more StorageAreas. Each StorageArea correponds
to one storage map (one localStorage or sessionStorage object), using either a SQLite database or a in-memory
map as backend. Now that we have NetworkStorageManager, which manages storage by origin. We can merge
StorageManager with NetworkStorageManager, since localStorage and sessionStorage are not shared between
different origins.

The new structure is:
NetworkStorageManager (manage storage for a session, owns one or more OriginStorageManagers)
OriginStorageManager (manage storage for an origin, owns one LocalStorageManager and one SessionStorageManager)
LocalStorageManager / SessionStorageManager (manage LocalStorage and SessionStorage, owns one or more
StorageAreaBases)
MemoryStorageArea / SQLiteStorageArea (inherits StorageAreaBases; manage one local or session storage, like
Webkit::StorageArea)

StorageNamespace layer is removed. For SessionStorage, different StorageNamespaces means different web pages,
and same origin can have different sessionStorages on different pages, so we keep a StorageNamespace <=>
StorageArea map in SessionStorageManager. For LocalStorage, differnt StorageNamespaces means different page
groups. Our original plan was the same origin can have different localStorages in different page groups, but our
old implementation actually made all page groups point to the same database file. To keep existing behavior

Source/WebKit/NetworkProcess/storage/OriginStorageManager.cpp
@@ -28,19 +28,25 @@
 
 #include "FileSystemStorageHandleRegistry.h"
 #include "FileSystemStorageManager.h"
+#include "LocalStorageManager.h"
+#include "SessionStorageManager.h"
+#include "StorageAreaRegistry.h"
 #include "WebsiteDataType.h"
+#include <WebCore/SQLiteFileSystem.h>
 #include <wtf/FileSystem.h>
 
 namespace WebKit {
 
+static constexpr auto originFileName = "origin"_s;
 enum class OriginStorageManager::StorageBucketMode : bool { BestEffort, Persistent };
 
 class OriginStorageManager::StorageBucket {
     WTF_MAKE_FAST_ALLOCATED;
 public:
-    StorageBucket(const String& rootPath, const String& identifier)
+    StorageBucket(const String& rootPath, const String& identifier, const String& localStoragePath)
         : m_rootPath(rootPath)
         , m_identifier(identifier)
+        , m_localStoragePath(localStoragePath)
     {
     }
 
@@ -55,13 +61,31 @@ public:
 
     enum class StorageType : uint8_t {
         FileSystem,
+        LocalStorage,
+        SessionStorage,
     };
+
+    static std::optional<WebsiteDataType> toWebsiteDataType(const String& storageIdentifier)
+    {
+        if (storageIdentifier == "FileSystem"_s)
+            return WebsiteDataType::FileSystem;
+        if (storageIdentifier == "LocalStorage"_s)
+            return WebsiteDataType::LocalStorage;
+        if (storageIdentifier == "SessionStorage"_s)
+            return WebsiteDataType::SessionStorage;
+
+        return std::nullopt;
+    }
 
     static String toStorageIdentifier(StorageType type)
     {
         switch (type) {
         case StorageType::FileSystem:
             return "FileSystem"_s;
+        case StorageType::LocalStorage:
+            return "LocalStorage"_s;
+        case StorageType::SessionStorage:
+            return "SessionStorage"_s;
         default:
             break;
         }
@@ -86,18 +110,121 @@ public:
         return *m_fileSystemStorageManager;
     }
 
-    bool isActive()
+    LocalStorageManager& localStorageManager(StorageAreaRegistry& registry)
     {
-        return !!m_fileSystemStorageManager;
+        if (!m_localStorageManager)
+            m_localStorageManager = makeUnique<LocalStorageManager>(m_localStoragePath, registry);
+
+        return *m_localStorageManager;
+    }
+
+    LocalStorageManager* existingLocalStorageManager()
+    {
+        return m_localStorageManager.get();
+    }
+
+    SessionStorageManager& sessionStorageManager(StorageAreaRegistry& registry)
+    {
+        if (!m_sessionStorageManager)
+            m_sessionStorageManager = makeUnique<SessionStorageManager>(registry);
+
+        return *m_sessionStorageManager;
+    }
+
+    SessionStorageManager* existingSessionStorageManager()
+    {
+        return m_sessionStorageManager.get();
+    }
+
+    bool isActive() const
+    {
+        return m_fileSystemStorageManager || m_localStorageManager || m_sessionStorageManager;
+    }
+
+    bool isEmpty() const
+    {
+        auto files = FileSystem::listDirectory(m_rootPath);
+        auto hasValidFile = WTF::anyOf(files, [&](auto file) {
+            bool isInvalidFile = (file == originFileName);
+#if PLATFORM(COCOA)
+            isInvalidFile |= (file == ".DS_Store");
+#endif
+            return !isInvalidFile;
+        });
+        if (hasValidFile)
+            return false;
+
+        return !FileSystem::fileExists(m_localStoragePath);
+    }
+
+    OptionSet<WebsiteDataType> fetchDataTypesInList(OptionSet<WebsiteDataType> types)
+    {
+        auto result = fetchDataTypesInListFromMemory(types);
+        result.add(fetchDataTypesInListFromDisk(types));
+
+        return result;
     }
 
     void deleteData(OptionSet<WebsiteDataType> types, WallTime modifiedSinceTime)
     {
         if (types.contains(WebsiteDataType::FileSystem))
             deleteFileSystemStorageData(modifiedSinceTime);
+
+        if (types.contains(WebsiteDataType::LocalStorage))
+            deleteLocalStorageData(modifiedSinceTime);
+
+        if (types.contains(WebsiteDataType::SessionStorage) && modifiedSinceTime == -WallTime::infinity())
+            deleteSessionStorageData();
+    }
+
+    void moveData(const String& path, const String& localStoragePath)
+    {
+        m_fileSystemStorageManager = nullptr;
+        if (m_localStorageManager)
+            m_localStorageManager->close();
+
+        FileSystem::makeAllDirectories(FileSystem::parentPath(path));
+        FileSystem::moveFile(m_rootPath, path);
+
+        if (!m_localStoragePath.isEmpty() && !localStoragePath.isEmpty()) {
+            FileSystem::makeAllDirectories(FileSystem::parentPath(localStoragePath));
+            WebCore::SQLiteFileSystem::moveDatabaseFile(m_localStoragePath, localStoragePath);
+        }
     }
 
 private:
+    OptionSet<WebsiteDataType> fetchDataTypesInListFromMemory(OptionSet<WebsiteDataType> types)
+    {
+        OptionSet<WebsiteDataType> result;
+        if (types.contains(WebsiteDataType::LocalStorage)) {
+            if (m_localStorageManager && m_localStorageManager->hasDataInMemory())
+                result.add(WebsiteDataType::LocalStorage);
+        }
+
+        if (types.contains(WebsiteDataType::SessionStorage)) {
+            if (m_sessionStorageManager && m_sessionStorageManager->hasDataInMemory())
+                result.add(WebsiteDataType::SessionStorage);
+        }
+
+        return result;
+    }
+
+    OptionSet<WebsiteDataType> fetchDataTypesInListFromDisk(OptionSet<WebsiteDataType> types)
+    {
+        OptionSet<WebsiteDataType> result;
+        for (auto& storageType : FileSystem::listDirectory(m_rootPath)) {
+            if (auto type = toWebsiteDataType(storageType); type && types.contains(*type))
+                result.add(*type);
+        }
+
+        if (types.contains(WebsiteDataType::LocalStorage) && !result.contains(WebsiteDataType::LocalStorage)) {
+            if (FileSystem::fileExists(m_localStoragePath))
+                result.add(WebsiteDataType::LocalStorage);
+        }
+
+        return result;
+    }
+
     void deleteFileSystemStorageData(WallTime modifiedSinceTime)
     {
         m_fileSystemStorageManager = nullptr;
@@ -106,14 +233,44 @@ private:
         FileSystem::deleteAllFilesModifiedSince(fileSystemStoragePath, modifiedSinceTime);
     }
 
+    void deleteLocalStorageData(WallTime time)
+    {
+        if (auto modificationTime = FileSystem::fileModificationTime(m_localStoragePath); *modificationTime >= time) {
+            if (m_localStorageManager)
+                m_localStorageManager->clearDataOnDisk();
+            WebCore::SQLiteFileSystem::deleteDatabaseFile(m_localStoragePath);
+        }
+
+        if (!m_localStorageManager)
+            return;
+
+        m_localStorageManager->clearDataInMemory();
+        if (!m_localStorageManager->isActive())
+            m_localStorageManager = nullptr;
+    }
+
+    void deleteSessionStorageData()
+    {
+        if (!m_sessionStorageManager)
+            return;
+
+        m_sessionStorageManager->clearData();
+        if (!m_sessionStorageManager->isActive())
+            m_sessionStorageManager = nullptr;
+    }
+    
     String m_rootPath;
     String m_identifier;
     StorageBucketMode m_mode { StorageBucketMode::BestEffort };
     std::unique_ptr<FileSystemStorageManager> m_fileSystemStorageManager;
+    std::unique_ptr<LocalStorageManager> m_localStorageManager;
+    String m_localStoragePath;
+    std::unique_ptr<SessionStorageManager> m_sessionStorageManager;
 };
 
-OriginStorageManager::OriginStorageManager(String&& path)
+OriginStorageManager::OriginStorageManager(String&& path, String&& localStoragePath)
     : m_path(WTFMove(path))
+    , m_localStoragePath(WTFMove(localStoragePath))
 {
     ASSERT(!RunLoop::isMain());
 }
@@ -129,7 +286,7 @@ void OriginStorageManager::connectionClosed(IPC::Connection::UniqueID connection
 OriginStorageManager::StorageBucket& OriginStorageManager::defaultBucket()
 {
     if (!m_defaultBucket)
-        m_defaultBucket = makeUnique<StorageBucket>(m_path, "default"_s);
+        m_defaultBucket = makeUnique<StorageBucket>(m_path, "default"_s, m_localStoragePath);
 
     return *m_defaultBucket;
 }
@@ -139,15 +296,34 @@ FileSystemStorageManager& OriginStorageManager::fileSystemStorageManager(FileSys
     return defaultBucket().fileSystemStorageManager(registry);
 }
 
+LocalStorageManager& OriginStorageManager::localStorageManager(StorageAreaRegistry& registry)
+{
+    return defaultBucket().localStorageManager(registry);
+}
+
+LocalStorageManager* OriginStorageManager::existingLocalStorageManager()
+{
+    return defaultBucket().existingLocalStorageManager();
+}
+
+SessionStorageManager& OriginStorageManager::sessionStorageManager(StorageAreaRegistry& registry)
+{
+    return defaultBucket().sessionStorageManager(registry);
+}
+
+SessionStorageManager* OriginStorageManager::existingSessionStorageManager()
+{
+    return defaultBucket().existingSessionStorageManager();
+}
+
 bool OriginStorageManager::isActive()
 {
     return defaultBucket().isActive();
 }
 
-void OriginStorageManager::deleteData(OptionSet<WebsiteDataType> types, WallTime modifiedSince)
+bool OriginStorageManager::isEmpty()
 {
-    ASSERT(!RunLoop::isMain());
-    defaultBucket().deleteData(types, modifiedSince);
+    return defaultBucket().isEmpty();
 }
 
 void OriginStorageManager::setPersisted(bool value)
@@ -156,6 +332,27 @@ void OriginStorageManager::setPersisted(bool value)
 
     m_persisted = value;
     defaultBucket().setMode(value ? StorageBucketMode::Persistent : StorageBucketMode::BestEffort);
+}
+
+OptionSet<WebsiteDataType> OriginStorageManager::fetchDataTypesInList(OptionSet<WebsiteDataType> types)
+{
+    ASSERT(!RunLoop::isMain());
+
+    return defaultBucket().fetchDataTypesInList(types);
+}
+
+void OriginStorageManager::deleteData(OptionSet<WebsiteDataType> types, WallTime modifiedSince)
+{
+    ASSERT(!RunLoop::isMain());
+
+    defaultBucket().deleteData(types, modifiedSince);
+}
+
+void OriginStorageManager::moveData(const String& newPath, const String& localStoragePath)
+{
+    ASSERT(!RunLoop::isMain());
+
+    defaultBucket().moveData(newPath, localStoragePath);
 }
 
 } // namespace WebKit

Source/WebKit/NetworkProcess/storage/OriginStorageManager.h
@@ -36,20 +36,30 @@ namespace WebKit {
 
 class FileSystemStorageHandleRegistry;
 class FileSystemStorageManager;
+class LocalStorageManager;
+class SessionStorageManager;
+class StorageAreaRegistry;
 enum class WebsiteDataType : uint32_t;
 
 class OriginStorageManager {
     WTF_MAKE_FAST_ALLOCATED;
 public:
-    explicit OriginStorageManager(String&& path);
+    OriginStorageManager(String&& path, String&& localStoragePath);
     ~OriginStorageManager();
 
     void connectionClosed(IPC::Connection::UniqueID);
     bool persisted() const { return m_persisted; }
     void setPersisted(bool value);
     FileSystemStorageManager& fileSystemStorageManager(FileSystemStorageHandleRegistry&);
+    LocalStorageManager& localStorageManager(StorageAreaRegistry&);
+    LocalStorageManager* existingLocalStorageManager();
+    SessionStorageManager& sessionStorageManager(StorageAreaRegistry&);
+    SessionStorageManager* existingSessionStorageManager();
     bool isActive();
+    bool isEmpty();
+    OptionSet<WebsiteDataType> fetchDataTypesInList(OptionSet<WebsiteDataType>);
     void deleteData(OptionSet<WebsiteDataType>, WallTime);
+    void moveData(const String& newPath, const String& localStoragePath);
 
 private:
     enum class StorageBucketMode : bool;
@@ -62,6 +72,7 @@ private:
     std::unique_ptr<StorageBucket> m_defaultBucket;
     String m_path;
     bool m_persisted { false };
+    String m_localStoragePath;
 };
 
 } // namespace WebKit

Source/WebCore/platform/sql/SQLiteFileSystem.cpp
@@ -43,6 +43,8 @@
 
 namespace WebCore {
 
+static constexpr std::array<const char *, 3> databaseFileSuffixes { "", "-shm", "-wal" };
+
 SQLiteFileSystem::SQLiteFileSystem()
 {
 }
@@ -77,18 +79,22 @@ bool SQLiteFileSystem::deleteEmptyDatabaseDirectory(const String& path)
     return FileSystem::deleteEmptyDirectory(path);
 }
 
-bool SQLiteFileSystem::deleteDatabaseFile(const String& fileName)
+bool SQLiteFileSystem::deleteDatabaseFile(const String& filePath)
 {
-    auto walFileName = makeString(fileName, "-wal"_s);
-    auto shmFileName = makeString(fileName, "-shm"_s);
+    bool fileExists = false;
+    for (const auto* suffix : databaseFileSuffixes) {
+        String path = filePath + suffix;
+        FileSystem::deleteFile(path);
+        fileExists |= FileSystem::fileExists(path);
+    }
 
-    // Try to delete all three files whether or not they are there.
-    FileSystem::deleteFile(fileName);
-    FileSystem::deleteFile(walFileName);
-    FileSystem::deleteFile(shmFileName);
+    return !fileExists;
+}
 
-    // If any of the wal or shm files remain after the delete attempt, the overall delete operation failed.
-    return !FileSystem::fileExists(fileName) && !FileSystem::fileExists(walFileName) && !FileSystem::fileExists(shmFileName);
+void SQLiteFileSystem::moveDatabaseFile(const String& oldFilePath, const String& newFilePath)
+{
+    for (const auto* suffix : databaseFileSuffixes)
+        FileSystem::moveFile(makeString(oldFilePath, suffix), makeString(newFilePath, suffix));
 }
 
 #if PLATFORM(IOS_FAMILY)
@@ -98,18 +104,13 @@ bool SQLiteFileSystem::truncateDatabaseFile(sqlite3* database)
 }
 #endif
     
-uint64_t SQLiteFileSystem::databaseFileSize(const String& fileName)
-{        
+uint64_t SQLiteFileSystem::databaseFileSize(const String& filePath)
+{
     uint64_t totalSize = 0;
-
-    if (auto fileSize = FileSystem::fileSize(fileName))
-        totalSize += *fileSize;
-
-    if (auto fileSize = FileSystem::fileSize(makeString(fileName, "-wal"_s)))
-        totalSize += *fileSize;
-
-    if (auto fileSize = FileSystem::fileSize(makeString(fileName, "-shm"_s)))
-        totalSize += *fileSize;
+    for (const auto* suffix : databaseFileSuffixes) {
+        if (auto fileSize = FileSystem::fileSize(filePath + suffix))
+            totalSize += *fileSize;
+    }
 
     return totalSize;
 }

Source/WebCore/platform/sql/SQLiteFileSystem.h
@@ -78,9 +78,11 @@ public:
     // Deletes a database file.
     //
     // fileName - The file name.
-    WEBCORE_EXPORT static bool deleteDatabaseFile(const String& fileName);
+    WEBCORE_EXPORT static bool deleteDatabaseFile(const String& filePath);
 
-    WEBCORE_EXPORT static String computeHashForFileName(const String& fileName);
+    // Moves a database file to a new place.
+    WEBCORE_EXPORT static void moveDatabaseFile(const String& oldFilePath, const String& newFilePath);
+    WEBCORE_EXPORT static String computeHashForFileName(const String& filePath);
 
 #if PLATFORM(IOS_FAMILY)
     // Truncates a database file. Used when MobileSafariSettings deletes a database file,

commit 66d30f3320b227a79c7f26b17a4f6effbe8f870c

Cherry-pick r288924. rdar://problem/87163253
    Write origin file when OriginStorageManager is destroyed
    https://bugs.webkit.org/show_bug.cgi?id=235897
    rdar://87163253

    Reviewed by Darin Adler.

    The first WebStorage message sent from web process to network process is sync, and writing origin file when
    creating OriginStorageManager will delay the reply. Since we can get the origin information from in-memory map
    when OriginStorageManager is present, we may delay the write to until when OriginStorageManager is destroyed.
    This fixes the PLT regression from r286936.

    * NetworkProcess/storage/NetworkStorageManager.cpp:
    (WebKit::NetworkStorageManager::localOriginStorageManager):
    * NetworkProcess/storage/OriginStorageManager.cpp:
    (WebKit::OriginStorageManager::OriginStorageManager):
    (WebKit::OriginStorageManager::~OriginStorageManager):
    * NetworkProcess/storage/OriginStorageManager.h:

    Canonical link: https://commits.webkit.org/246658@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@288924 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Canonical link: https://commits.webkit.org/245886.156@safari-613-branch
git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-613-branch@289336 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/NetworkProcess/storage/NetworkStorageManager.cpp
@@ -210,8 +210,12 @@ OriginStorageManager& NetworkStorageManager::localOriginStorageManager(const Web
 
     return *m_localOriginStorageManagers.ensure(origin, [&] {
         auto originDirectory = originDirectoryPath(m_path, origin, m_salt);
-        writeOriginToFileIfNecessary(originFilePath(originDirectory), origin);
-        return makeUnique<OriginStorageManager>(WTFMove(originDirectory), LocalStorageManager::localStorageFilePath(m_customLocalStoragePath, origin));
+        // We write origin file at when OriginStorageManager is destroyed to avoid delay
+        // in replying sync messages for Web Storage API.
+        auto writeOriginFileFunction = [originFile = originFilePath(originDirectory), origin]() {
+            writeOriginToFileIfNecessary(originFile, origin);
+        };
+        return makeUnique<OriginStorageManager>(WTFMove(writeOriginFileFunction), WTFMove(originDirectory), LocalStorageManager::localStorageFilePath(m_customLocalStoragePath, origin));
     }).iterator->value;
 }
 

Source/WebKit/NetworkProcess/storage/OriginStorageManager.cpp
@@ -269,14 +269,19 @@ private:
     std::unique_ptr<SessionStorageManager> m_sessionStorageManager;
 };
 
-OriginStorageManager::OriginStorageManager(String&& path, String&& localStoragePath)
-    : m_path(WTFMove(path))
+OriginStorageManager::OriginStorageManager(Function<void()>&& writeOriginFileFunction, String&& path, String&& localStoragePath)
+    : m_writeOriginFileFunction(WTFMove(writeOriginFileFunction))
+    , m_path(WTFMove(path))
     , m_localStoragePath(WTFMove(localStoragePath))
 {
     ASSERT(!RunLoop::isMain());
 }
 
-OriginStorageManager::~OriginStorageManager() = default;
+OriginStorageManager::~OriginStorageManager()
+{
+    if (m_writeOriginFileFunction)
+        m_writeOriginFileFunction();
+}
 
 void OriginStorageManager::connectionClosed(IPC::Connection::UniqueID connection)
 {

Source/WebKit/NetworkProcess/storage/OriginStorageManager.h
@@ -44,7 +44,7 @@ enum class WebsiteDataType : uint32_t;
 class OriginStorageManager {
     WTF_MAKE_FAST_ALLOCATED;
 public:
-    OriginStorageManager(String&& path, String&& localStoragePath);
+    OriginStorageManager(Function<void()>&&, String&& path, String&& localStoragePath);
     ~OriginStorageManager();
 
     void connectionClosed(IPC::Connection::UniqueID);
@@ -69,6 +69,7 @@ private:
     void createOriginFileIfNecessary(const WebCore::ClientOrigin&);
     void deleteOriginFileIfNecessary();
 
+    Function<void()> m_writeOriginFileFunction;
     std::unique_ptr<StorageBucket> m_defaultBucket;
     String m_path;
     bool m_persisted { false };