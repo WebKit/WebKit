9d6d230bd27c0e9dac88a09f4a4b2258d75eceb0
Gustavo Noronha Silva <gns@gnome.org>
2022-02-15 14:20:26 +0000

    Merge r289761 - [Linux/aarch64] Move page size ceiling to 16k https://bugs.webkit.org/show_bug.cgi?id=236564
    Patch by Gustavo Noronha Silva <gustavo@noronha.dev.br> on 2022-02-14
    Reviewed by Yusuke Suzuki.
    
    On Apple Silicon, Linux needs to use 16k pages to be as fast as possible. This change has no
    visible impact on binary size for release builds, so apply it to all of aarch64 Linux.
    
    * wtf/PageBlock.h:
    
    Canonical link: https://commits.webkit.org/241176.126@webkitgtk/2.34
    git-svn-id: https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-2.34@289805 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/PageBlock.h
+++ Source/WTF/wtf/PageBlock.h
@@ -37,14 +37,15 @@ namespace WTF {
 // and recompiled. Sorry.
 //
 // macOS x86_64 uses 4 KiB, but Apple's aarch64 systems use 16 KiB. Use 16 KiB on all Apple systems
-// for consistency.
+// for consistency. Linux on Apple Silicon also needs to use 16KiB for best performance, so use that
+// for Linux on aarch64 as well.
 //
 // Most Linux and Windows systems use a page size of 4 KiB.
 //
 // On Linux, Power systems normally use 64 KiB pages.
 //
 // Use 64 KiB for any unknown CPUs to be conservative.
-#if OS(DARWIN) || PLATFORM(PLAYSTATION) || CPU(MIPS) || CPU(MIPS64)
+#if OS(DARWIN) || PLATFORM(PLAYSTATION) || CPU(MIPS) || CPU(MIPS64) || (OS(LINUX) && CPU(ARM64))
 constexpr size_t CeilingOnPageSize = 16 * KB;
 #elif CPU(PPC) || CPU(PPC64) || CPU(PPC64LE) || CPU(UNKNOWN)
 constexpr size_t CeilingOnPageSize = 64 * KB;
