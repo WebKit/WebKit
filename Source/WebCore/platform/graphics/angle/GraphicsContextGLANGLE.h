WebKit/WebKit

commit 762e09fe2f3cff85e6ca1a750e01f876b29bed25

ANGLE includes interfere with system header includes https://bugs.webkit.org/show_bug.cgi?id=236636
Patch by Kimmo Kinnunen <kkinnunen@apple.com> on 2022-02-16
Reviewed by Dean Jackson.

Source/WebCore:

Move ANGLEHeaders.h includes only to the .cpp/.mm files
that need to use ANGLE.
Use GCGL* types consistently in the GraphicsContextGLANGLE.h
declations. The WebCore types are intended to be used in place of OpenGL, EGL types
so that WebCore code does not need to include OpenGL headers.

Rename PlatformGraphicsContextGL{Display, Config}, PlatformGraphicsContextGL to
GCGLDisplay, GCGLConfig, GCGLContext to make them consistent with other
duplicated types. Declare these in GraphicsTypesGL.h, similar to others.
Remove currently unused PlatformGraphicsContextGLSurface.

Rename TemporaryANGLESetting to ScopedGLCapability, consistent with other ScopedGL*
classes. Move the implementations of ScopedGL* to ANGLEUtilities.cpp, since the
implementations need to use ANGLE and these need the ANGLE headers.

No new tests, refactor. Fixes the compile on Big Sur, internal SDK when a file add or remove
would changed unified build file ordering.

* CMakeLists.txt:
* Modules/webxr/WebXROpaqueFramebuffer.cpp:
* SourcesCocoa.txt:
* WebCore.xcodeproj/project.pbxproj:
* platform/graphics/GLContext.h:
* platform/graphics/GraphicsContextGL.h:
* platform/graphics/GraphicsTypesGL.h:
* platform/graphics/angle/ANGLEUtilities.cpp:
(WebCore::ScopedRestoreTextureBinding::ScopedRestoreTextureBinding):
(WebCore::ScopedRestoreTextureBinding::~ScopedRestoreTextureBinding):
(WebCore::ScopedBufferBinding::ScopedBufferBinding):
(WebCore::ScopedBufferBinding::~ScopedBufferBinding):
(WebCore::ScopedRestoreReadFramebufferBinding::bindFramebuffer):
(WebCore::ScopedRestoreReadFramebufferBinding::~ScopedRestoreReadFramebufferBinding):
(WebCore::ScopedPixelStorageMode::ScopedPixelStorageMode):
(WebCore::ScopedPixelStorageMode::~ScopedPixelStorageMode):
(WebCore::ScopedPixelStorageMode::pixelStore):

Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.h
@@ -68,8 +68,8 @@ class WEBCORE_EXPORT GraphicsContextGLANGLE : public GraphicsContextGL {
 public:
     virtual ~GraphicsContextGLANGLE();
 
-    PlatformGraphicsContextGLDisplay platformDisplay() const;
-    PlatformGraphicsContextGLConfig platformConfig() const;
+    GCGLDisplay platformDisplay() const;
+    GCGLConfig platformConfig() const;
     GCGLenum drawingBufferTextureTarget();
     static GCGLenum drawingBufferTextureTargetQueryForDrawingTarget(GCGLenum drawingTarget);
     static GCGLint EGLDrawingBufferTextureTargetForDrawingTarget(GCGLenum drawingTarget);
@@ -363,9 +363,9 @@ public:
     std::optional<PixelBuffer> readRenderingResultsForPainting();
     std::optional<PixelBuffer> readCompositedResultsForPainting();
 
-    constexpr static EGLNativeDisplayType defaultDisplay = EGL_DEFAULT_DISPLAY;
+    constexpr static GCGLNativeDisplayType defaultDisplay = gcGLDefaultDisplay;
 #if PLATFORM(COCOA)
-    constexpr static EGLNativeDisplayType defaultOpenGLDisplay = EGL_CAST(EGLNativeDisplayType, -1);
+    constexpr static GCGLNativeDisplayType defaultOpenGLDisplay = static_cast<GCGLNativeDisplayType>(-1);
     static_assert(defaultDisplay != defaultOpenGLDisplay);
 #endif
 
@@ -406,7 +406,7 @@ protected:
     void resolveMultisamplingIfNecessary(const IntRect& = IntRect());
     void attachDepthAndStencilBufferIfNeeded(GCGLuint internalDepthStencilFormat, int width, int height);
 #if PLATFORM(COCOA)
-    static bool makeCurrent(PlatformGraphicsContextGLDisplay, PlatformGraphicsContextGL);
+    static bool makeCurrent(GCGLDisplay, GCGLContext);
 #endif
     virtual bool reshapeDisplayBufferBacking() = 0;
 
@@ -439,7 +439,7 @@ protected:
     // Attaches m_texture when m_preserveDrawingBufferTexture is non-zero.
     GCGLuint m_preserveDrawingBufferFBO { 0 };
     // Queried at display startup.
-    EGLint m_drawingBufferTextureTarget { -1 };
+    GCGLint m_drawingBufferTextureTarget { -1 };
     // Errors raised by synthesizeGLError().
     ListHashSet<GCGLenum> m_syntheticErrors;
     bool m_isForWebGL2 { false };
@@ -454,9 +454,9 @@ protected:
 #if PLATFORM(COCOA)
     // FIXME: Move these to GraphicsContextGLCocoa.
     GraphicsContextGLIOSurfaceSwapChain m_swapChain;
-    EGLDisplay m_displayObj { nullptr };
-    PlatformGraphicsContextGL m_contextObj { nullptr };
-    PlatformGraphicsContextGLConfig m_configObj { nullptr };
+    GCGLDisplay m_displayObj { nullptr };
+    GCGLContext m_contextObj { nullptr };
+    GCGLConfig m_configObj { nullptr };
     // Backing store for the the buffer which is eventually used for display.
     // When preserveDrawingBuffer == false, this is the drawing buffer backing store.
     // When preserveDrawingBuffer == true, this is blitted to during display prepare.
@@ -473,7 +473,7 @@ protected:
     class EGLImageBacking {
     WTF_MAKE_FAST_ALLOCATED;
     public:
-        EGLImageBacking(PlatformGraphicsContextGLDisplay);
+        EGLImageBacking(GCGLDisplay);
         ~EGLImageBacking();
 
         bool reset(int width, int height, bool hasAlpha);
@@ -488,7 +488,7 @@ protected:
     private:
         void releaseResources();
 
-        PlatformGraphicsContextGLDisplay m_display;
+        GCGLDisplay m_display;
 
         gbm_bo* m_BO { nullptr };
         int m_FD { -1 };

Source/WebCore/platform/graphics/texmap/GraphicsContextGLTextureMapperANGLE.cpp
@@ -125,7 +125,7 @@ GraphicsContextGLANGLE::GraphicsContextGLANGLE(GraphicsContextGLAttributes attri
 }
 
 #if USE(NICOSIA)
-GraphicsContextGLANGLE::EGLImageBacking::EGLImageBacking(PlatformGraphicsContextGLDisplay display)
+GraphicsContextGLANGLE::EGLImageBacking::EGLImageBacking(GCGLDisplay display)
     : m_display(display)
 {
 }
@@ -231,7 +231,7 @@ GraphicsContextGLANGLE::~GraphicsContextGLANGLE()
 #endif
 }
 
-PlatformGraphicsContextGLDisplay GraphicsContextGLANGLE::platformDisplay() const
+GCGLDisplay GraphicsContextGLANGLE::platformDisplay() const
 {
 #if USE(NICOSIA)
     return m_nicosiaLayer->platformDisplay();
@@ -240,7 +240,7 @@ PlatformGraphicsContextGLDisplay GraphicsContextGLANGLE::platformDisplay() const
 #endif
 }
 
-PlatformGraphicsContextGLConfig GraphicsContextGLANGLE::platformConfig() const
+GCGLConfig GraphicsContextGLANGLE::platformConfig() const
 {
 #if USE(NICOSIA)
     return m_nicosiaLayer->platformConfig();

Source/WebCore/CMakeLists.txt
@@ -1909,7 +1909,6 @@ if (ENABLE_WEBGL)
         list(APPEND WebCore_SOURCES
             platform/graphics/angle/ANGLEUtilities.cpp
             platform/graphics/angle/GraphicsContextGLANGLE.cpp
-            platform/graphics/angle/TemporaryANGLESetting.cpp
         )
 
         list(APPEND WebCore_LIBRARIES ANGLE::GLES)

Source/WebCore/Modules/webxr/WebXROpaqueFramebuffer.cpp
@@ -52,7 +52,6 @@
 #include <wtf/Scope.h>
 
 #if USE(IOSURFACE_FOR_XR_LAYER_DATA)
-#include "ANGLEHeaders.h"
 #include "GraphicsContextGLCocoa.h"
 #endif
 

Source/WebCore/PAL/pal/spi/cg/CoreGraphicsSPI.h
@@ -40,10 +40,6 @@
 
 #if HAVE(CPP20_INCOMPATIBLE_INTERNAL_HEADERS)
 #define CGCOLORTAGGEDPOINTER_H_
-#ifndef GL_GLES_PROTOTYPES
-#define GL_GLES_PROTOTYPES 0
-#endif
-#include <ANGLE/entry_points_gles_ext_autogen.h>
 #endif
 
 #include <CoreGraphics/CGContextDelegatePrivate.h>

Source/WebCore/PAL/pal/spi/mac/HIToolboxSPI.h
@@ -29,10 +29,6 @@
 
 #if HAVE(CPP20_INCOMPATIBLE_INTERNAL_HEADERS)
 #define CGCOLORTAGGEDPOINTER_H_
-#ifndef GL_GLES_PROTOTYPES
-#define GL_GLES_PROTOTYPES 0
-#endif
-#include <ANGLE/entry_points_gles_ext_autogen.h>
 #endif
 
 #include <Carbon/CarbonPriv.h>

Source/WebCore/platform/graphics/GLContext.h
@@ -48,7 +48,7 @@ typedef uint64_t GLNativeWindowType;
 typedef struct _cairo_device cairo_device_t;
 #endif
 
-typedef void* PlatformGraphicsContextGL;
+typedef void* GCGLContext;
 
 // X11 headers define a bunch of macros with common terms, interfering with WebCore and WTF enum values.
 // As a workaround, we explicitly undef them here.
@@ -112,7 +112,7 @@ public:
 
     virtual bool isEGLContext() const = 0;
 
-    virtual PlatformGraphicsContextGL platformContext() = 0;
+    virtual GCGLContext platformContext() = 0;
 
 protected:
     GLContext(PlatformDisplay&);

Source/WebCore/platform/graphics/GraphicsContextGL.h
@@ -45,11 +45,6 @@
 #endif
 #endif
 
-typedef void* PlatformGraphicsContextGL;
-typedef void* PlatformGraphicsContextGLDisplay;
-typedef void* PlatformGraphicsContextGLSurface;
-typedef void* PlatformGraphicsContextGLConfig;
-
 namespace WebCore {
 class ExtensionsGL;
 class HostWindow;

commit 1e55cbed6db349225d30dc1b86f3eb80a756b366

[WPE] Build failure when building with ANGLE enabled since r289912 https://bugs.webkit.org/show_bug.cgi?id=236768
Reviewed by Alejandro G. Castro.

Fix build after regression caused by r289912.

* platform/graphics/angle/GraphicsContextGLANGLE.h:
* platform/graphics/texmap/GraphicsContextGLTextureMapperANGLE.cpp:
(WebCore::GraphicsContextGLANGLE::EGLImageBacking::EGLImageBacking):



Canonical link: https://commits.webkit.org/247401@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@290015 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.h
@@ -48,6 +48,8 @@ class GCGLLayer;
 
 struct gbm_device;
 struct gbm_bo;
+
+typedef void *EGLImage;
 #endif
 
 namespace WebCore {
@@ -492,7 +494,7 @@ protected:
 
         gbm_bo* m_BO { nullptr };
         int m_FD { -1 };
-        EGLImage m_image { EGL_NO_IMAGE };
+        EGLImage m_image;
     };
 
     std::unique_ptr<EGLImageBacking> m_textureBacking;

Source/WebCore/platform/graphics/texmap/GraphicsContextGLTextureMapperANGLE.cpp
@@ -127,6 +127,7 @@ GraphicsContextGLANGLE::GraphicsContextGLANGLE(GraphicsContextGLAttributes attri
 #if USE(NICOSIA)
 GraphicsContextGLANGLE::EGLImageBacking::EGLImageBacking(GCGLDisplay display)
     : m_display(display)
+    , m_image(EGL_NO_IMAGE)
 {
 }
 