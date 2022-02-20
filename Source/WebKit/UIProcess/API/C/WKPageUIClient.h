c56d8e36067e219b09027124df7b87f2c6354b37
Brandon Stewart <brandonstewart@apple.com>
2022-02-20 16:01:42 +0000

    Generate compile_commands.json on macOS Builds https://bugs.webkit.org/show_bug.cgi?id=235686
    Patch by Brandon Stewart <brandonstewart@apple.com> on 2022-02-20
    Reviewed by Saam Barati.
    
    Add support for generating compile_commands.json file to WebKit.
    
    compile_commands.json can be used by tools like clangd to provide code complete,
    refactoring, formatting, and cross references.
    
    Steps to generate the compile commands file:
    make d EXPORT_COMPILE_COMMANDS=YES
    generate-compile-commands WebKitBuild/Debug
    
    .:
    
    * Makefile.shared:
    
    Source/WebKit:
    
    * UIProcess/API/C/WKPageUIClient.h:
    
    Tools:
    
    * Scripts/build-jsc:
    * Scripts/build-webkit:
    * Scripts/generate-compile-commands: Added.
    
    
    Canonical link: https://commits.webkit.org/247553@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@290227 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/C/WKPageUIClient.h
+++ Source/WebKit/UIProcess/API/C/WKPageUIClient.h
@@ -135,8 +135,8 @@ typedef void (*WKDidExceedBackgroundResourceLimitWhileInForegroundCallback)(WKPa
 typedef void (*WKPageDidResignInputElementStrongPasswordAppearanceCallback)(WKPageRef page, WKTypeRef userData, const void *clientInfo);
 typedef bool (*WKPageShouldAllowDeviceOrientationAndMotionAccessCallback)(WKPageRef page, WKSecurityOriginRef securityOrigin, WKFrameInfoRef frame, const void *clientInfo);
 
-typedef void (*WKPageRunWebAuthenticationPanelCallback)();
-typedef void (*WKPageRequestWebAuthenticationNoGestureCallback)();
+typedef void (*WKPageRunWebAuthenticationPanelCallback)(void);
+typedef void (*WKPageRequestWebAuthenticationNoGestureCallback)(void);
 typedef void (*WKPageDecidePolicyForSpeechRecognitionPermissionRequestCallback)(WKPageRef page, WKSecurityOriginRef topOrigin, WKSpeechRecognitionPermissionCallbackRef callback);
 
 typedef void (*WKPageDecidePolicyForMediaKeySystemPermissionRequestCallback)(WKPageRef page, WKSecurityOriginRef topOrigin, WKStringRef keySystem, WKMediaKeySystemPermissionCallbackRef callback);
