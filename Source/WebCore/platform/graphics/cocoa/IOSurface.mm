6087960edc761d4725f2269ddc666d1e1fdc2fc0
Per Arne Vollan <pvollan@apple.com>
2022-02-28 23:26:38 +0000

    Remove release assert when UI process is blocking IOSurface IOKit access https://bugs.webkit.org/show_bug.cgi?id=237278
    Reviewed by Darin Adler.
    
    Remove release assert in IOSurface::bytesPerRowAlignment() when UI process is blocking IOSurface IOKit access.
    Instead, log this event, and return an appropriate value.
    
    * platform/graphics/cocoa/IOSurface.mm:
    (WebCore::IOSurface::bytesPerRowAlignment):
    
    
    
    Canonical link: https://commits.webkit.org/247891@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@290618 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/cocoa/IOSurface.mm
+++ Source/WebCore/platform/graphics/cocoa/IOSurface.mm
@@ -293,7 +293,11 @@ size_t IOSurface::bytesPerRowAlignment()
         // See https://developer.apple.com/documentation/iosurface/1419453-iosurfacegetpropertyalignment?language=objc
         // This likely means that the sandbox is blocking access to the IOSurface IOKit class,
         // and that IOSurface::bytesPerRowAlignment() has been called before IOSurface::setBytesPerRowAlignment.
-        RELEASE_ASSERT(alignment > 1);
+        if (alignment <= 1) {
+            RELEASE_LOG_FAULT(Layers, "Sandbox does not allow IOSurface IOKit access.");
+            // 64 bytes is currently the alignment on all platforms.
+            alignment = 64;
+        }
     }
     return alignment;
 }
