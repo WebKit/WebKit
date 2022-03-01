f51c9d66a5a975e550a7c3e9c07de64f609f5a20
Basuke Suzuki <basuke.suzuki@sony.com>
2022-03-01 09:36:58 +0000

    [CMake] Disabling ENABLE_WEBCORE is ignored when cmake configuration runs again. https://bugs.webkit.org/show_bug.cgi?id=237170
    Reviewed by Fujii Hironori.
    
    CMake variables which has chance to set from outside should be cached in CMake configuration cache.
    Unless cacheing, the result of building the generated project is not consisitent because the other
    configuration may run while building.
    
    To make it complete, I've changed ENABLE_JAVASCRIPTCORE, ENABLE_WEBCORE and ENABLE_WEBKIT to `option()`
    which is stored in cached and reused while building even if the confuguration runs again.
    
    * Source/cmake/OptionsPlayStation.cmake:
    * Source/cmake/WebKitCommon.cmake:
    
    
    
    Canonical link: https://commits.webkit.org/247909@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@290636 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/WebKitCommon.cmake
+++ Source/cmake/WebKitCommon.cmake
@@ -13,11 +13,16 @@ if (NOT HAS_RUN_WEBKIT_COMMON)
         message(STATUS "The CMake build type is: ${CMAKE_BUILD_TYPE}")
     endif ()
 
-    set(ENABLE_JAVASCRIPTCORE ON)
-    set(ENABLE_WEBCORE ON)
+    option(ENABLE_JAVASCRIPTCORE "Enable building JavaScriptCore" ON)
+    option(ENABLE_WEBCORE "Enable building JavaScriptCore" ON)
+    option(ENABLE_WEBKIT "Enable building WebKit" ON)
 
-    if (NOT DEFINED ENABLE_WEBKIT)
-        set(ENABLE_WEBKIT ON)
+    if (NOT ENABLE_JAVASCRIPTCORE)
+        set(ENABLE_WEBCORE OFF)
+    endif ()
+
+    if (NOT ENABLE_WEBCORE)
+        set(ENABLE_WEBKIT OFF)
     endif ()
 
     if (NOT DEFINED ENABLE_TOOLS AND EXISTS "${CMAKE_SOURCE_DIR}/Tools")
