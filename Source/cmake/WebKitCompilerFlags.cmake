77d7b77290643feb377a8ec957b2fda908d549b6
Adrian Perez de Castro <aperez@igalia.com>
2022-03-02 15:03:58 +0000

    [CMake] Build failure with libstdc++ (>= 6) when cross-compiling (fatal error: stdlib.h: No such file or directory) https://bugs.webkit.org/show_bug.cgi?id=161697 <rdar://problem/89613152>
    Reviewed by Carlos Garcia Campos.
    
    * Source/cmake/WebKitCompilerFlags.cmake: Go through CMake instead of the plain "env"
    program for the sake of portability.
    
    
    
    Canonical link: https://commits.webkit.org/247974@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@290727 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/cmake/WebKitCompilerFlags.cmake
+++ Source/cmake/WebKitCompilerFlags.cmake
@@ -278,7 +278,7 @@ endif ()
 macro(DETERMINE_GCC_SYSTEM_INCLUDE_DIRS _lang _compiler _flags _result)
     file(WRITE "${CMAKE_BINARY_DIR}/CMakeFiles/dummy" "\n")
     separate_arguments(_buildFlags UNIX_COMMAND "${_flags}")
-    execute_process(COMMAND env LANG=C ${_compiler} ${_buildFlags} -v -E -x ${_lang} -dD dummy
+    execute_process(COMMAND ${CMAKE_COMMAND} -E env LANG=C ${_compiler} ${_buildFlags} -v -E -x ${_lang} -dD dummy
                     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/CMakeFiles OUTPUT_QUIET
                     ERROR_VARIABLE _gccOutput)
     file(REMOVE "${CMAKE_BINARY_DIR}/CMakeFiles/dummy")
