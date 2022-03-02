082906a66fa4552c536a527a82c125854df8e4c9
Per Arne Vollan <pvollan@apple.com>
2022-03-02 21:25:49 +0000

    [iOS] Seatbelt sandbox profile entitlement is deprecated https://bugs.webkit.org/show_bug.cgi?id=237374
    Reviewed by Brent Fulgham.
    
    Replace with new entitlement.
    
    * Scripts/process-entitlements.sh:
    
    
    
    Canonical link: https://commits.webkit.org/247998@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@290754 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Scripts/process-entitlements.sh
+++ Source/WebKit/Scripts/process-entitlements.sh
@@ -391,8 +391,7 @@ function ios_family_process_webcontent_entitlements()
     plistbuddy Add :com.apple.tcc.delegated-services:0 string kTCCServiceCamera
     plistbuddy Add :com.apple.tcc.delegated-services:1 string kTCCServiceMicrophone
 
-    plistbuddy Add :seatbelt-profiles array
-    plistbuddy Add :seatbelt-profiles:0 string com.apple.WebKit.WebContent
+    plistbuddy Add :com.apple.private.sandbox.profile string com.apple.WebKit.WebContent
 }
 
 function ios_family_process_webcontent_captiveportal_entitlements()
@@ -427,8 +426,7 @@ function ios_family_process_webcontent_captiveportal_entitlements()
     plistbuddy Add :com.apple.tcc.delegated-services:0 string kTCCServiceCamera
     plistbuddy Add :com.apple.tcc.delegated-services:1 string kTCCServiceMicrophone
 
-    plistbuddy Add :seatbelt-profiles array
-    plistbuddy Add :seatbelt-profiles:0 string com.apple.WebKit.WebContent
+    plistbuddy Add :com.apple.private.sandbox.profile string com.apple.WebKit.WebContent
 }
 
 function ios_family_process_gpu_entitlements()
@@ -455,8 +453,7 @@ function ios_family_process_gpu_entitlements()
     plistbuddy Add :com.apple.tcc.delegated-services:0 string kTCCServiceCamera
     plistbuddy Add :com.apple.tcc.delegated-services:1 string kTCCServiceMicrophone
 
-    plistbuddy Add :seatbelt-profiles array
-    plistbuddy Add :seatbelt-profiles:0 string com.apple.WebKit.GPU
+    plistbuddy Add :com.apple.private.sandbox.profile string com.apple.WebKit.GPU
 
     plistbuddy Add :com.apple.systemstatus.activityattribution bool YES
     plistbuddy Add :com.apple.security.exception.mach-lookup.global-name array
@@ -503,14 +500,12 @@ function ios_family_process_webauthn_entitlements()
     plistbuddy Add :com.apple.springboard.remote-alert bool YES
     plistbuddy Add :com.apple.frontboard.launchapplications bool YES
 
-    plistbuddy Add :seatbelt-profiles array
-    plistbuddy Add :seatbelt-profiles:0 string com.apple.WebKit.WebAuthn
+    plistbuddy Add :com.apple.private.sandbox.profile string com.apple.WebKit.WebAuthn
 }
 
 function ios_family_process_adattributiond_entitlements()
 {
-    plistbuddy Add :seatbelt-profiles array
-    plistbuddy Add :seatbelt-profiles:0 string com.apple.WebKit.adattributiond
+    plistbuddy Add :com.apple.private.sandbox.profile string com.apple.WebKit.adattributiond
 }
 
 function ios_family_process_webpushd_entitlements()
@@ -537,8 +532,7 @@ function ios_family_process_network_entitlements()
     plistbuddy Add :com.apple.private.tcc.manager.check-by-audit-token array
     plistbuddy Add :com.apple.private.tcc.manager.check-by-audit-token:0 string kTCCServiceWebKitIntelligentTrackingPrevention
 
-    plistbuddy Add :seatbelt-profiles array
-    plistbuddy Add :seatbelt-profiles:0 string com.apple.WebKit.Networking
+    plistbuddy Add :com.apple.private.sandbox.profile string com.apple.WebKit.Networking
     plistbuddy Add :com.apple.symptom_analytics.configure bool YES
 }
 
