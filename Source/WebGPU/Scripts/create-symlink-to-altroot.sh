e9561f5e283e1ebcf2dba482b6cae86ba5e7d2d7
Michael Saboff <msaboff@apple.com>
2022-03-07 23:08:24 +0000

    Catalyst build failure in WebGPU when building with SYSTEM_CONTENT_PATH https://bugs.webkit.org/show_bug.cgi?id=237542
    Reviewed by Saam Barati.
    
    Updated create-symlink-to-altroot.sh to create the directory containing the output if is doesn't
    exist in lieu of specifying an output file in the build step.
    This is the way that other projects are configured.
    
    * Scripts/create-symlink-to-altroot.sh:
    * WebGPU.xcodeproj/project.pbxproj:
    
    
    
    Canonical link: https://commits.webkit.org/248142@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@290960 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebGPU/Scripts/create-symlink-to-altroot.sh
+++ Source/WebGPU/Scripts/create-symlink-to-altroot.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/sh
 
 set -e
 
@@ -13,6 +13,12 @@ fi
 # Convert eg. `/System/Library/PrivateFrameworks` to `../../..`
 RELATIVE_PATH_FROM_SYMLINK_TO_ROOT=$(echo "${ALTERNATE_ROOT_PATH}" | sed -E -e "s/\/[a-zA-Z0-9_]+/..\//g" -e "s/\/$//")
 SYMLINK_VALUE="${RELATIVE_PATH_FROM_SYMLINK_TO_ROOT}${INSTALL_PATH}/${FULL_PRODUCT_NAME}"
+
+OUTPUT_ALTERNATE_ROOT_DIR=`dirname ${OUTPUT_ALTERNATE_ROOT_PATH}`
+
+if [[ ! -d ${OUTPUT_ALTERNATE_ROOT_DIR} ]]; then
+    mkdir -p ${OUTPUT_ALTERNATE_ROOT_DIR}
+fi
 
 if [[ -L "${OUTPUT_ALTERNATE_ROOT_PATH}" ]]; then
     EXISTING_SYMLINK_VALUE=$(readlink "${OUTPUT_ALTERNATE_ROOT_PATH}")
