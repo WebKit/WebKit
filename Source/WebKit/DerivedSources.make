WebKit/WebKit

commit a2bed2f7903a68b74fdeb7c5a2868980cec4582f

Delete WebIDBServer and WebIDBConnectionToClient https://bugs.webkit.org/show_bug.cgi?id=237479
Reviewed by Chris Dumez.

The code is unused since r289474; remove it to avoid confusion when investigating issues including related
symbols.

* CMakeLists.txt:
* DerivedSources-input.xcfilelist:
* DerivedSources-output.xcfilelist:
* DerivedSources.make:
* NetworkProcess/IndexedDB/WebIDBConnectionToClient.cpp: Removed.
* NetworkProcess/IndexedDB/WebIDBConnectionToClient.h: Removed.
* NetworkProcess/IndexedDB/WebIDBServer.cpp: Removed.
* NetworkProcess/IndexedDB/WebIDBServer.h: Removed.
* NetworkProcess/IndexedDB/WebIDBServer.messages.in: Removed.
* NetworkProcess/NetworkProcess.cpp:
* NetworkProcess/NetworkSession.cpp:
* NetworkProcess/NetworkSession.h:
* NetworkProcess/ios/NetworkProcessIOS.mm:
* Sources.txt:
* WebKit.xcodeproj/project.pbxproj:



Canonical link: https://commits.webkit.org/248127@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@290896 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/DerivedSources-input.xcfilelist
@@ -80,7 +80,6 @@ $(PROJECT_DIR)/GPUProcess/webrtc/RemoteSampleBufferDisplayLayer.messages.in
 $(PROJECT_DIR)/GPUProcess/webrtc/RemoteSampleBufferDisplayLayerManager.messages.in
 $(PROJECT_DIR)/NetworkProcess/Cookies/WebCookieManager.messages.in
 $(PROJECT_DIR)/NetworkProcess/CustomProtocols/LegacyCustomProtocolManager.messages.in
-$(PROJECT_DIR)/NetworkProcess/IndexedDB/WebIDBServer.messages.in
 $(PROJECT_DIR)/NetworkProcess/NetworkBroadcastChannelRegistry.messages.in
 $(PROJECT_DIR)/NetworkProcess/NetworkConnectionToWebProcess.messages.in
 $(PROJECT_DIR)/NetworkProcess/NetworkContentRuleListManager.messages.in

Source/WebKit/DerivedSources.make
@@ -124,7 +124,6 @@ endif
 MESSAGE_RECEIVERS = \
 	NetworkProcess/NetworkBroadcastChannelRegistry \
 	NetworkProcess/NetworkConnectionToWebProcess \
-	NetworkProcess/IndexedDB/WebIDBServer \
 	NetworkProcess/NetworkContentRuleListManager \
 	NetworkProcess/WebStorage/StorageManagerSet \
 	NetworkProcess/cache/CacheStorageEngineConnection \

Source/WebKit/CMakeLists.txt
@@ -189,8 +189,6 @@ set(WebKit_MESSAGES_IN_FILES
 
     NetworkProcess/Cookies/WebCookieManager
 
-    NetworkProcess/IndexedDB/WebIDBServer
-
     NetworkProcess/NetworkBroadcastChannelRegistry
     NetworkProcess/NetworkConnectionToWebProcess
     NetworkProcess/NetworkContentRuleListManager

Source/WebKit/DerivedSources-output.xcfilelist
@@ -532,9 +532,6 @@ $(BUILT_PRODUCTS_DIR)/DerivedSources/WebKit/WebGeolocationManagerProxyMessagesRe
 $(BUILT_PRODUCTS_DIR)/DerivedSources/WebKit/WebIDBConnectionToServerMessageReceiver.cpp
 $(BUILT_PRODUCTS_DIR)/DerivedSources/WebKit/WebIDBConnectionToServerMessages.h
 $(BUILT_PRODUCTS_DIR)/DerivedSources/WebKit/WebIDBConnectionToServerMessagesReplies.h
-$(BUILT_PRODUCTS_DIR)/DerivedSources/WebKit/WebIDBServerMessageReceiver.cpp
-$(BUILT_PRODUCTS_DIR)/DerivedSources/WebKit/WebIDBServerMessages.h
-$(BUILT_PRODUCTS_DIR)/DerivedSources/WebKit/WebIDBServerMessagesReplies.h
 $(BUILT_PRODUCTS_DIR)/DerivedSources/WebKit/WebInspectorInterruptDispatcherMessageReceiver.cpp
 $(BUILT_PRODUCTS_DIR)/DerivedSources/WebKit/WebInspectorInterruptDispatcherMessages.h
 $(BUILT_PRODUCTS_DIR)/DerivedSources/WebKit/WebInspectorInterruptDispatcherMessagesReplies.h

Source/WebKit/NetworkProcess/IndexedDB/WebIDBConnectionToClient.cpp
@@ -1,199 +1,0 @@
-/*
- * Copyright (C) 2016-2019 Apple Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include "config.h"
-#include "WebIDBConnectionToClient.h"
-
-#include "WebCoreArgumentCoders.h"
-#include "WebIDBConnectionToServerMessages.h"
-#include "WebIDBResult.h"
-#include "WebIDBServer.h"
-#include <WebCore/IDBGetAllRecordsData.h>
-#include <WebCore/IDBGetRecordData.h>
-#include <WebCore/IDBResultData.h>
-#include <WebCore/UniqueIDBDatabaseConnection.h>
-
-namespace WebKit {
-using namespace WebCore;
-
-WebIDBConnectionToClient::WebIDBConnectionToClient(IPC::Connection& connection, WebCore::IDBConnectionIdentifier serverConnectionIdentifier)
-    : m_connection(connection)
-    , m_identifier(serverConnectionIdentifier)
-    , m_connectionToClient(IDBServer::IDBConnectionToClient::create(*this))
-{
-}
-
-WebIDBConnectionToClient::~WebIDBConnectionToClient()
-{
-    m_connectionToClient->clearDelegate();
-}
-
-IPC::Connection* WebIDBConnectionToClient::messageSenderConnection() const
-{
-    return m_connection.ptr();
-}
-
-WebCore::IDBServer::IDBConnectionToClient& WebIDBConnectionToClient::connectionToClient()
-{
-    return m_connectionToClient;
-}
-
-void WebIDBConnectionToClient::didDeleteDatabase(const WebCore::IDBResultData& resultData)
-{
-    send(Messages::WebIDBConnectionToServer::DidDeleteDatabase(resultData));
-}
-
-void WebIDBConnectionToClient::didOpenDatabase(const WebCore::IDBResultData& resultData)
-{
-    send(Messages::WebIDBConnectionToServer::DidOpenDatabase(resultData));
-}
-
-void WebIDBConnectionToClient::didAbortTransaction(const WebCore::IDBResourceIdentifier& transactionIdentifier, const WebCore::IDBError& error)
-{
-    send(Messages::WebIDBConnectionToServer::DidAbortTransaction(transactionIdentifier, error));
-}
-
-void WebIDBConnectionToClient::didCommitTransaction(const WebCore::IDBResourceIdentifier& transactionIdentifier, const WebCore::IDBError& error)
-{
-    send(Messages::WebIDBConnectionToServer::DidCommitTransaction(transactionIdentifier, error));
-}
-
-void WebIDBConnectionToClient::didCreateObjectStore(const WebCore::IDBResultData& resultData)
-{
-    send(Messages::WebIDBConnectionToServer::DidCreateObjectStore(resultData));
-}
-
-void WebIDBConnectionToClient::didDeleteObjectStore(const WebCore::IDBResultData& resultData)
-{
-    send(Messages::WebIDBConnectionToServer::DidDeleteObjectStore(resultData));
-}
-
-void WebIDBConnectionToClient::didRenameObjectStore(const WebCore::IDBResultData& resultData)
-{
-    send(Messages::WebIDBConnectionToServer::DidRenameObjectStore(resultData));
-}
-
-void WebIDBConnectionToClient::didClearObjectStore(const WebCore::IDBResultData& resultData)
-{
-    send(Messages::WebIDBConnectionToServer::DidClearObjectStore(resultData));
-}
-
-void WebIDBConnectionToClient::didCreateIndex(const WebCore::IDBResultData& resultData)
-{
-    send(Messages::WebIDBConnectionToServer::DidCreateIndex(resultData));
-}
-
-void WebIDBConnectionToClient::didDeleteIndex(const WebCore::IDBResultData& resultData)
-{
-    send(Messages::WebIDBConnectionToServer::DidDeleteIndex(resultData));
-}
-
-void WebIDBConnectionToClient::didRenameIndex(const WebCore::IDBResultData& resultData)
-{
-    send(Messages::WebIDBConnectionToServer::DidRenameIndex(resultData));
-}
-
-void WebIDBConnectionToClient::didPutOrAdd(const WebCore::IDBResultData& resultData)
-{
-    send(Messages::WebIDBConnectionToServer::DidPutOrAdd(resultData));
-}
-
-template<class MessageType> void WebIDBConnectionToClient::handleGetResult(const WebCore::IDBResultData& resultData)
-{
-    if (resultData.type() == IDBResultType::Error) {
-        send(MessageType(resultData));
-        return;
-    }
-
-    if (resultData.type() == IDBResultType::GetAllRecordsSuccess && resultData.getAllResult().type() == IndexedDB::GetAllType::Keys) {
-        send(MessageType(resultData));
-        return;
-    }
-
-    auto blobFilePaths = resultData.type() == IDBResultType::GetAllRecordsSuccess ? resultData.getAllResult().allBlobFilePaths() : resultData.getResult().value().blobFilePaths();
-    if (blobFilePaths.isEmpty()) {
-        send(MessageType(resultData));
-        return;
-    }
-
-    send(MessageType(resultData));
-}
-
-void WebIDBConnectionToClient::didGetRecord(const WebCore::IDBResultData& resultData)
-{
-    handleGetResult<Messages::WebIDBConnectionToServer::DidGetRecord>(resultData);
-}
-
-void WebIDBConnectionToClient::didGetAllRecords(const WebCore::IDBResultData& resultData)
-{
-    handleGetResult<Messages::WebIDBConnectionToServer::DidGetAllRecords>(resultData);
-}
-
-void WebIDBConnectionToClient::didGetCount(const WebCore::IDBResultData& resultData)
-{
-    send(Messages::WebIDBConnectionToServer::DidGetCount(resultData));
-}
-
-void WebIDBConnectionToClient::didDeleteRecord(const WebCore::IDBResultData& resultData)
-{
-    send(Messages::WebIDBConnectionToServer::DidDeleteRecord(resultData));
-}
-
-void WebIDBConnectionToClient::didOpenCursor(const WebCore::IDBResultData& resultData)
-{
-    handleGetResult<Messages::WebIDBConnectionToServer::DidOpenCursor>(resultData);
-}
-
-void WebIDBConnectionToClient::didIterateCursor(const WebCore::IDBResultData& resultData)
-{
-    handleGetResult<Messages::WebIDBConnectionToServer::DidIterateCursor>(resultData);
-}
-
-void WebIDBConnectionToClient::fireVersionChangeEvent(WebCore::IDBServer::UniqueIDBDatabaseConnection& connection, const WebCore::IDBResourceIdentifier& requestIdentifier, uint64_t requestedVersion)
-{
-    send(Messages::WebIDBConnectionToServer::FireVersionChangeEvent(connection.identifier(), requestIdentifier, requestedVersion));
-}
-
-void WebIDBConnectionToClient::didStartTransaction(const WebCore::IDBResourceIdentifier& transactionIdentifier, const WebCore::IDBError& error)
-{
-    send(Messages::WebIDBConnectionToServer::DidStartTransaction(transactionIdentifier, error));
-}
-
-void WebIDBConnectionToClient::didCloseFromServer(WebCore::IDBServer::UniqueIDBDatabaseConnection& connection, const WebCore::IDBError& error)
-{
-    send(Messages::WebIDBConnectionToServer::DidCloseFromServer(connection.identifier(), error));
-}
-
-void WebIDBConnectionToClient::notifyOpenDBRequestBlocked(const WebCore::IDBResourceIdentifier& requestIdentifier, uint64_t oldVersion, uint64_t newVersion)
-{
-    send(Messages::WebIDBConnectionToServer::NotifyOpenDBRequestBlocked(requestIdentifier, oldVersion, newVersion));
-}
-
-void WebIDBConnectionToClient::didGetAllDatabaseNamesAndVersions(const WebCore::IDBResourceIdentifier& requestIdentifier, const Vector<WebCore::IDBDatabaseNameAndVersion>& databases)
-{
-    send(Messages::WebIDBConnectionToServer::DidGetAllDatabaseNamesAndVersions(requestIdentifier, databases));
-}
-
-} // namespace WebKit

Source/WebKit/NetworkProcess/IndexedDB/WebIDBConnectionToClient.h
@@ -1,92 +1,0 @@
-/*
- * Copyright (C) 2016-2019 Apple Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#pragma once
-
-#include "MessageSender.h"
-#include <WebCore/IDBConnectionToClient.h>
-#include <WebCore/IndexedDB.h>
-#include <WebCore/ProcessIdentifier.h>
-
-namespace WebCore {
-namespace IDBServer {
-class IDBServer;
-}
-}
-
-namespace WebKit {
-
-class NetworkProcess;
-class WebIDBServer;
-
-class WebIDBConnectionToClient final : public WebCore::IDBServer::IDBConnectionToClientDelegate, public IPC::MessageSender {
-    WTF_MAKE_FAST_ALLOCATED;
-public:
-    WebIDBConnectionToClient(IPC::Connection&, WebCore::IDBConnectionIdentifier);
-
-    virtual ~WebIDBConnectionToClient();
-
-    WebCore::IDBServer::IDBConnectionToClient& connectionToClient();
-    WebCore::IDBConnectionIdentifier identifier() const final { return m_identifier; }
-
-private:
-    IPC::Connection* messageSenderConnection() const final;
-    uint64_t messageSenderDestinationID() const final { return 0; }
-
-    // IDBConnectionToClientDelegate
-    void didDeleteDatabase(const WebCore::IDBResultData&) final;
-    void didOpenDatabase(const WebCore::IDBResultData&) final;
-    void didAbortTransaction(const WebCore::IDBResourceIdentifier& transactionIdentifier, const WebCore::IDBError&) final;
-    void didCommitTransaction(const WebCore::IDBResourceIdentifier& transactionIdentifier, const WebCore::IDBError&) final;
-    void didCreateObjectStore(const WebCore::IDBResultData&) final;
-    void didDeleteObjectStore(const WebCore::IDBResultData&) final;
-    void didRenameObjectStore(const WebCore::IDBResultData&) final;
-    void didClearObjectStore(const WebCore::IDBResultData&) final;
-    void didCreateIndex(const WebCore::IDBResultData&) final;
-    void didDeleteIndex(const WebCore::IDBResultData&) final;
-    void didRenameIndex(const WebCore::IDBResultData&) final;
-    void didPutOrAdd(const WebCore::IDBResultData&) final;
-    void didGetRecord(const WebCore::IDBResultData&) final;
-    void didGetAllRecords(const WebCore::IDBResultData&) final;
-    void didGetCount(const WebCore::IDBResultData&) final;
-    void didDeleteRecord(const WebCore::IDBResultData&) final;
-    void didOpenCursor(const WebCore::IDBResultData&) final;
-    void didIterateCursor(const WebCore::IDBResultData&) final;
-
-    void fireVersionChangeEvent(WebCore::IDBServer::UniqueIDBDatabaseConnection&, const WebCore::IDBResourceIdentifier& requestIdentifier, uint64_t requestedVersion) final;
-    void didStartTransaction(const WebCore::IDBResourceIdentifier& transactionIdentifier, const WebCore::IDBError&) final;
-    void didCloseFromServer(WebCore::IDBServer::UniqueIDBDatabaseConnection&, const WebCore::IDBError&) final;
-    void notifyOpenDBRequestBlocked(const WebCore::IDBResourceIdentifier& requestIdentifier, uint64_t oldVersion, uint64_t newVersion) final;
-
-    void didGetAllDatabaseNamesAndVersions(const WebCore::IDBResourceIdentifier&, const Vector<WebCore::IDBDatabaseNameAndVersion>&) final;
-
-    template<class MessageType> void handleGetResult(const WebCore::IDBResultData&);
-
-    Ref<IPC::Connection> m_connection;
-    WebCore::IDBConnectionIdentifier m_identifier;
-    Ref<WebCore::IDBServer::IDBConnectionToClient> m_connectionToClient;
-};
-
-} // namespace WebKit

commit 0935c1bc383b253a9b54232939d6e68eb60f38fe

DerivedSources.make: Regenerate message receiver sources on Makefile changes https://bugs.webkit.org/show_bug.cgi?id=237543
Reviewed by Alexey Proskuryakov.

This script depends on configuration and other sources in DerivedSources.make, so it should
regenerate when they change. Fixes one-time incremental build failures caused by r290896.

* DerivedSources.make:



Canonical link: https://commits.webkit.org/248139@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@290957 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/DerivedSources-input.xcfilelist
@@ -18,6 +18,7 @@ $(JAVASCRIPTCORE_PRIVATE_HEADERS_DIR)/generator_templates.py
 $(JAVASCRIPTCORE_PRIVATE_HEADERS_DIR)/jsmin.py
 $(JAVASCRIPTCORE_PRIVATE_HEADERS_DIR)/models.py
 $(JAVASCRIPTCORE_PRIVATE_HEADERS_DIR)/xxd.pl
+$(PROJECT_DIR)/DerivedSources.make
 $(PROJECT_DIR)/GPUProcess/GPUConnectionToWebProcess.messages.in
 $(PROJECT_DIR)/GPUProcess/GPUProcess.messages.in
 $(PROJECT_DIR)/GPUProcess/graphics/RemoteDisplayListRecorder.messages.in

Source/WebKit/DerivedSources.make
@@ -316,6 +316,7 @@ GENERATE_MESSAGE_RECEIVER_SCRIPTS = \
     $(WebKit2)/Scripts/webkit/messages.py \
     $(WebKit2)/Scripts/webkit/model.py \
     $(WebKit2)/Scripts/webkit/parser.py \
+    $(WebKit2)/DerivedSources.make \
 #
 
 FRAMEWORK_FLAGS := $(shell echo $(BUILT_PRODUCTS_DIR) $(FRAMEWORK_SEARCH_PATHS) $(SYSTEM_FRAMEWORK_SEARCH_PATHS) | perl -e 'print "-F " . join(" -F ", split(" ", <>));')