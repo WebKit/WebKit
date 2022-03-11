WebKit/WebKit

commit c896ec96dc4a23c64220a079fa07cb4fa28a7b11

[GTK][WPE] Web Inspector: make it possible to use the remote inspector from other browsers https://bugs.webkit.org/show_bug.cgi?id=237601
Reviewed by Michael Catanzaro.

Source/JavaScriptCore:

Make it possible to start the inspector server with 0 port, meaning any free one will be used.

* inspector/remote/glib/RemoteInspectorServer.cpp:
(Inspector::RemoteInspectorServer::start): Pass the effective address to g_socket_listener_add_address and get the port.
* inspector/remote/glib/RemoteInspectorServer.h:
(Inspector::RemoteInspectorServer::port const): Return the port the server is listening to.

Source/WebInspectorUI:

Add Base/BrowserInspectorFrontendHost.js to be used when inspector is run and InspectorFrontendHost is not
defined in window. This new file is only included in the inspector sources for GTK and WPE ports.

* CMakeLists.txt:
* PlatformWPE: Added
* PlatformGTK:
* Scripts/combine-resources.pl: Add --input-script-name to be able to skip individual scripts too.
* Scripts/copy-user-interface-resources.pl: If inspector frontend host should not be included, remove the script
from generated files.
* UserInterface/Base/BrowserInspectorFrontendHost.js: Added.
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get supportsShowCertificate):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get isRemote):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get inspectionLevel):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get debuggableInfo):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get port):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get platform):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get platformVersionName):
(window.InspectorFrontendHost.WI.BrowserInspectorFrontendHost.prototype.get supportsDiagnosticLogging):

Source/WebKit/UIProcess/Inspector/glib/RemoteInspectorHTTPServer.cpp
@@ -1,0 +1,180 @@
+/*
+ * Copyright (C) 2022 Igalia S.L.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "RemoteInspectorHTTPServer.h"
+
+#if ENABLE(REMOTE_INSPECTOR)
+
+#include "RemoteInspectorClient.h"
+#include <WebCore/SoupVersioning.h>
+#include <wtf/FileSystem.h>
+#include <wtf/URL.h>
+#include <wtf/glib/GUniquePtr.h>
+
+namespace WebKit {
+
+RemoteInspectorHTTPServer& RemoteInspectorHTTPServer::singleton()
+{
+    static RemoteInspectorHTTPServer server;
+    return server;
+}
+
+bool RemoteInspectorHTTPServer::start(const char* address, unsigned port, unsigned inspectorPort)
+{
+    m_server = adoptGRef(soup_server_new("server-header", "WebKitInspectorHTTPServer ", nullptr));
+    GRefPtr<GSocketAddress> socketAddress = adoptGRef(g_inet_socket_address_new_from_string(address, port));
+    if (!socketAddress) {
+        g_warning("Failed to start remote inspector HTTP server on %s:%u: invalid address", address, port);
+        return false;
+    }
+
+    GUniqueOutPtr<GError> error;
+    if (!soup_server_listen(m_server.get(), socketAddress.get(), static_cast<SoupServerListenOptions>(0), &error.outPtr())) {
+        g_warning("Failed to start remote inspector HTTP server on %s:%u: %s", address, port, error->message);
+        return false;
+    }
+
+    soup_server_add_handler(m_server.get(), nullptr,
+#if USE(SOUP2)
+        [](SoupServer*, SoupMessage* message, const char* path, GHashTable*, SoupClientContext*, gpointer userData) {
+#else
+        [](SoupServer*, SoupServerMessage* message, const char* path, GHashTable*, gpointer userData) {
+#endif
+            auto& httpServer = *static_cast<RemoteInspectorHTTPServer*>(userData);
+            auto status = httpServer.handleRequest(path, soup_server_message_get_response_headers(message), soup_server_message_get_response_body(message));
+            soup_server_message_set_status(message, status, nullptr);
+        }, this, nullptr);
+
+    soup_server_add_websocket_handler(m_server.get(), "/socket", nullptr, nullptr,
+#if USE(SOUP2)
+        [](SoupServer*, SoupWebsocketConnection* connection, const char* path, SoupClientContext*, gpointer userData) {
+#else
+        [](SoupServer*, SoupServerMessage*, const char* path, SoupWebsocketConnection* connection, gpointer userData) {
+#endif
+            auto& httpServer = *static_cast<RemoteInspectorHTTPServer*>(userData);
+            httpServer.handleWebSocket(path, connection);
+        }, this, nullptr);
+
+    m_client = makeUnique<RemoteInspectorClient>(address, inspectorPort, *this);
+
+    return true;
+}
+
+unsigned RemoteInspectorHTTPServer::handleRequest(const char* path, SoupMessageHeaders* responseHeaders, SoupMessageBody* responseBody) const
+{
+    if (g_str_equal(path, "/")) {
+        auto* html = m_client->buildTargetListPage(RemoteInspectorClient::InspectorType::HTTP);
+        soup_message_headers_append(responseHeaders, "Content-Type", "text/html");
+        gsize bodyLength = html->len;
+        soup_message_body_append(responseBody, SOUP_MEMORY_TAKE, g_string_free(html, FALSE), bodyLength);
+        return SOUP_STATUS_OK;
+    }
+
+    GUniquePtr<char> resourcePath(g_build_filename("/org/webkit/inspector/UserInterface", path, nullptr));
+
+    GUniqueOutPtr<GError> error;
+    GRefPtr<GBytes> bytes = adoptGRef(g_resources_lookup_data(resourcePath.get(), G_RESOURCE_LOOKUP_FLAGS_NONE, &error.outPtr()));
+    if (bytes) {
+        gsize dataSize;
+        const auto* data = static_cast<const guchar*>(g_bytes_get_data(bytes.get(), &dataSize));
+        GUniquePtr<char> fileName(g_path_get_basename(resourcePath.get()));
+        GUniquePtr<char> contentType(g_content_type_guess(fileName.get(), data, dataSize, nullptr));
+        soup_message_headers_append(responseHeaders, "Content-Type", contentType.get());
+        soup_message_body_append(responseBody, SOUP_MEMORY_COPY, data, dataSize);
+        return SOUP_STATUS_OK;
+    }
+
+    g_warning("Failed to load inspector resource %s: %s", resourcePath.get(), error->message);
+
+    return SOUP_STATUS_NOT_FOUND;
+}
+
+void RemoteInspectorHTTPServer::handleWebSocket(const char* path, SoupWebsocketConnection* connection)
+{
+    auto items = String::fromUTF8(path).split('/');
+    if (items.size() != 4)
+        return;
+
+    uint64_t connectionID = items[1].toDouble();
+    uint64_t targetID = items[2].toDouble();
+    m_webSocketConnectionMap.set(std::make_pair(connectionID, targetID), connection);
+    m_webSocketConnectionToTargetMap.set(connection, std::make_pair(connectionID, targetID));
+    g_signal_connect(connection, "message", G_CALLBACK(+[](SoupWebsocketConnection* connection, SoupWebsocketDataType messageType, GBytes* message, gpointer userData) {
+        auto& httpServer = *static_cast<RemoteInspectorHTTPServer*>(userData);
+        gsize dataSize;
+        const auto* data = static_cast<const char*>(g_bytes_get_data(message, &dataSize));
+        httpServer.sendMessageToBackend(connection, String::fromUTF8(data, dataSize));
+    }), this);
+    g_signal_connect(connection, "closed", G_CALLBACK(+[](SoupWebsocketConnection* connection, gpointer userData) {
+        auto& httpServer = *static_cast<RemoteInspectorHTTPServer*>(userData);
+        httpServer.didCloseWebSocket(connection);
+    }), this);
+
+    m_client->inspect(connectionID, targetID, items[3], RemoteInspectorClient::InspectorType::HTTP);
+}
+
+void RemoteInspectorHTTPServer::sendMessageToFrontend(uint64_t connectionID, uint64_t targetID, const String& message) const
+{
+    auto* webSocketConnection = m_webSocketConnectionMap.get(std::make_pair(connectionID, targetID));
+    if (!webSocketConnection)
+        return;
+
+    auto utf8 = message.utf8();
+    // Soup is going to copy the data immediately, so we can use g_bytes_new_static() here to avoid more data copies.
+    GRefPtr<GBytes> bytes = adoptGRef(g_bytes_new_static(utf8.data(), utf8.length()));
+    soup_websocket_connection_send_message(webSocketConnection, SOUP_WEBSOCKET_DATA_TEXT, bytes.get());
+}
+
+void RemoteInspectorHTTPServer::targetDidClose(uint64_t connectionID, uint64_t targetID)
+{
+    auto* webSocketConnection = m_webSocketConnectionMap.get(std::make_pair(connectionID, targetID));
+    if (!webSocketConnection)
+        return;
+
+    soup_websocket_connection_close(webSocketConnection, SOUP_WEBSOCKET_CLOSE_GOING_AWAY, "Target closed");
+}
+
+void RemoteInspectorHTTPServer::sendMessageToBackend(SoupWebsocketConnection* connection, const String& message) const
+{
+    auto [connectionID, targetID] = m_webSocketConnectionToTargetMap.get(connection);
+    if (connectionID && targetID)
+        m_client->sendMessageToBackend(connectionID, targetID, message);
+}
+
+void RemoteInspectorHTTPServer::didCloseWebSocket(SoupWebsocketConnection* connection)
+{
+    auto connectionAndTarget = m_webSocketConnectionToTargetMap.take(connection);
+    if (!connectionAndTarget.first || !connectionAndTarget.second)
+        return;
+
+    g_signal_handlers_disconnect_by_data(connection, this);
+    m_webSocketConnectionMap.remove(connectionAndTarget);
+    m_client->closeFromFrontend(connectionAndTarget.first, connectionAndTarget.second);
+}
+
+} // namespace WebKit
+
+#endif // ENABLE(REMOTE_INSPECTOR)

Source/JavaScriptCore/inspector/remote/glib/RemoteInspectorServer.cpp
@@ -188,11 +188,14 @@ bool RemoteInspectorServer::start(const char* address, unsigned port)
     g_signal_connect(m_service.get(), "incoming", G_CALLBACK(incomingConnectionCallback), this);
 
     GRefPtr<GSocketAddress> socketAddress = adoptGRef(g_inet_socket_address_new_from_string(address, port));
+    GRefPtr<GSocketAddress> effectiveAddress;
     GUniqueOutPtr<GError> error;
-    if (!g_socket_listener_add_address(G_SOCKET_LISTENER(m_service.get()), socketAddress.get(), G_SOCKET_TYPE_STREAM, G_SOCKET_PROTOCOL_TCP, nullptr, nullptr, &error.outPtr())) {
+    if (!g_socket_listener_add_address(G_SOCKET_LISTENER(m_service.get()), socketAddress.get(), G_SOCKET_TYPE_STREAM, G_SOCKET_PROTOCOL_TCP, nullptr, &effectiveAddress.outPtr(), &error.outPtr())) {
         g_warning("Failed to start remote inspector server on %s:%u: %s\n", address, port, error->message);
         return false;
     }
+
+    m_port = g_inet_socket_address_get_port(G_INET_SOCKET_ADDRESS(effectiveAddress.get()));
 
     return true;
 }

Source/JavaScriptCore/inspector/remote/glib/RemoteInspectorServer.h
@@ -45,6 +45,7 @@ public:
 
     JS_EXPORT_PRIVATE bool start(const char* address, unsigned port);
     bool isRunning() const { return !!m_service; }
+    uint16_t port() const { return m_port; }
 
 private:
     static gboolean incomingConnectionCallback(GSocketService*, GSocketConnection*, GObject*, RemoteInspectorServer*);
@@ -61,6 +62,7 @@ private:
     void startAutomationSession(SocketConnection&, const char* sessionID, const RemoteInspector::Client::SessionCapabilities&);
 
     GRefPtr<GSocketService> m_service;
+    uint16_t m_port { 0 };
     HashSet<RefPtr<SocketConnection>> m_connections;
     HashMap<SocketConnection*, uint64_t> m_remoteInspectorConnectionToIDMap;
     HashMap<uint64_t, SocketConnection*> m_idToRemoteInspectorConnectionMap;

Source/WebInspectorUI/CMakeLists.txt
@@ -70,13 +70,17 @@ if (NOT DEFINED COMBINE_TEST_RESOURCES)
     endif ()
 endif ()
 
+if (NOT DEFINED INCLUDE_BROWSER_INSPECTOR_FRONTEND_HOST)
+    set(INCLUDE_BROWSER_INSPECTOR_FRONTEND_HOST NO)
+endif ()
+
 add_custom_command(
     OUTPUT ${CMAKE_BINARY_DIR}/inspector-resources.stamp
     DEPENDS ${InspectorFilesDependencies}
             ${InspectorResourceScripts}
             ${WebInspectorUI_DERIVED_SOURCES_DIR}/UserInterface/Protocol/InspectorBackendCommands.js
     COMMAND ${CMAKE_COMMAND} -E copy ${WebInspectorUI_DERIVED_SOURCES_DIR}/UserInterface/Protocol/InspectorBackendCommands.js ${JavaScriptCore_SCRIPTS_DIR}/InspectorBackendCommands.js
-    COMMAND ${CMAKE_COMMAND} -E env "DERIVED_SOURCES_DIR=${WebInspectorUI_DERIVED_SOURCES_DIR}" "SRCROOT=${WEBINSPECTORUI_DIR}" "JAVASCRIPTCORE_PRIVATE_HEADERS_DIR=${JavaScriptCore_SCRIPTS_DIR}" "TARGET_BUILD_DIR=${WebInspectorUI_RESOURCES_DIR}" "UNLOCALIZED_RESOURCES_FOLDER_PATH=WebInspectorUI" "COMBINE_INSPECTOR_RESOURCES=${COMBINE_INSPECTOR_RESOURCES}" "COMBINE_TEST_RESOURCES=${COMBINE_TEST_RESOURCES}" PYTHON=${PYTHON_EXECUTABLE} ${PERL_EXECUTABLE} ${WEBINSPECTORUI_DIR}/Scripts/copy-user-interface-resources.pl
+    COMMAND ${CMAKE_COMMAND} -E env "DERIVED_SOURCES_DIR=${WebInspectorUI_DERIVED_SOURCES_DIR}" "SRCROOT=${WEBINSPECTORUI_DIR}" "JAVASCRIPTCORE_PRIVATE_HEADERS_DIR=${JavaScriptCore_SCRIPTS_DIR}" "TARGET_BUILD_DIR=${WebInspectorUI_RESOURCES_DIR}" "UNLOCALIZED_RESOURCES_FOLDER_PATH=WebInspectorUI" "COMBINE_INSPECTOR_RESOURCES=${COMBINE_INSPECTOR_RESOURCES}" "COMBINE_TEST_RESOURCES=${COMBINE_TEST_RESOURCES}" "INCLUDE_BROWSER_INSPECTOR_FRONTEND_HOST=${INCLUDE_BROWSER_INSPECTOR_FRONTEND_HOST}" PYTHON=${PYTHON_EXECUTABLE} ${PERL_EXECUTABLE} ${WEBINSPECTORUI_DIR}/Scripts/copy-user-interface-resources.pl
     COMMAND ${CMAKE_COMMAND} -E make_directory ${WebInspectorUI_LOCALIZED_STRINGS_DIR}
     COMMAND ${CMAKE_COMMAND} -E copy "${WEBINSPECTORUI_DIR}/Localizations/en.lproj/localizedStrings.js" ${WebInspectorUI_LOCALIZED_STRINGS_DIR}/localizedStrings.js
     COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_BINARY_DIR}/inspector-resources.stamp

Source/WebInspectorUI/PlatformGTK.cmake
@@ -1,3 +1,4 @@
 set(WebInspectorUI_RESOURCES_DIR "${WebKit2Gtk_DERIVED_SOURCES_DIR}/InspectorResources")
 set(WebInspectorUI_LOCALIZED_STRINGS_DIR "${WebInspectorUI_RESOURCES_DIR}/WebInspectorUI/Localizations/en.lproj")
 
+set(INCLUDE_BROWSER_INSPECTOR_FRONTEND_HOST YES)

Source/WebInspectorUI/PlatformWPE.cmake
@@ -1,0 +1,1 @@
+set(INCLUDE_BROWSER_INSPECTOR_FRONTEND_HOST YES)

Source/WebInspectorUI/Scripts/combine-resources.pl
@@ -30,6 +30,7 @@ use File::Basename;
 use File::Path;
 
 our $inputDirectory;
+our $inputScriptName;
 our $outputDirectory;
 our $outputScriptName;
 our $outputStylesheetName;
@@ -47,6 +48,7 @@ GetOptions('output-dir=s' => \$outputDirectory,
            'input-dir=s' => \$inputDirectory,
            'input-html-dir=s' => \$htmlDirectory,
            'input-html=s' => \$htmlFile,
+           'input-script-name=s' => \$inputScriptName,
            'verbose' => \$verbose,
            'strip' => \$strip,
            'skip-concatenate-tag' => \$skipConcatenateTag);
@@ -121,7 +123,13 @@ sub stripIncludedFilesMatchingPattern($)
 }
 
 my $inputDirectoryPattern = "(?!WebKitAdditions\/)(?!External\/)(?!Workers\/)(?!NonMinified\/)[^\"]*";
-$inputDirectoryPattern = $inputDirectory . "\/[^\"]*" if $inputDirectory;
+if ($inputDirectory) {
+    if ($inputScriptName) {
+        $inputDirectoryPattern = $inputDirectory . "\/" . $inputScriptName;
+    } else {
+        $inputDirectoryPattern = $inputDirectory . "\/[^\"]*";
+    }
+}
 
 if (defined($strip)) {
     stripIncludedFilesMatchingPattern("<link rel=\"stylesheet\" href=\"($inputDirectoryPattern)\">");

Source/WebInspectorUI/Scripts/copy-user-interface-resources.pl
@@ -185,6 +185,7 @@ copy(File::Spec->catfile($ENV{'JAVASCRIPTCORE_PRIVATE_HEADERS_DIR'}, 'InspectorB
 my $forceToolInstall = defined $ENV{'FORCE_TOOL_INSTALL'} && ($ENV{'FORCE_TOOL_INSTALL'} eq 'YES');
 my $shouldCombineMain = defined $ENV{'COMBINE_INSPECTOR_RESOURCES'} && ($ENV{'COMBINE_INSPECTOR_RESOURCES'} eq 'YES');
 my $shouldCombineTest = defined $ENV{'COMBINE_TEST_RESOURCES'} && ($ENV{'COMBINE_TEST_RESOURCES'} eq 'YES');
+my $shouldIncludeBrowserInspectorFrontendHost = defined $ENV{'INCLUDE_BROWSER_INSPECTOR_FRONTEND_HOST'} && ($ENV{'INCLUDE_BROWSER_INSPECTOR_FRONTEND_HOST'} eq 'YES');
 my $combineResourcesCmd = File::Spec->catfile($scriptsRoot, 'combine-resources.pl');
 
 if ($forceToolInstall) {
@@ -201,12 +202,27 @@ if (!$shouldCombineMain) {
     # Keep the files separate for engineering builds. Copy these before altering Main.html
     # in other ways, such as combining for WebKitAdditions or inlining files.
     ditto($uiRoot, $targetResourcePath);
+
+    if (!$shouldIncludeBrowserInspectorFrontendHost) {
+        unlink File::Spec->catfile($targetResourcePath, 'Base', 'BrowserInspectorFrontendHost.js');
+    }
 }
 
 # Always refer to the copy in derived sources so the order of replacements does not matter.
 make_path($derivedSourcesDir);
 my $derivedSourcesMainHTML = File::Spec->catfile($derivedSourcesDir, 'Main.html');
 copy(File::Spec->catfile($uiRoot, 'Main.html'), File::Spec->catfile($derivedSourcesDir, 'Main.html')) or die "Copy failed: $!";
+
+if (!$shouldIncludeBrowserInspectorFrontendHost) {
+    system($perl, $combineResourcesCmd,
+        '--input-dir', 'Base',
+        '--input-html', $derivedSourcesMainHTML,
+        '--input-html-dir', $uiRoot,
+        '--input-script-name', 'BrowserInspectorFrontendHost.js',
+        '--derived-sources-dir', $derivedSourcesDir,
+        '--output-dir', $derivedSourcesDir,
+        '--strip');
+}
 
 sub webInspectorUIAdditionsDir() {
     my $webkitAdditionsDir;

commit a23024f680b6706687d307884a214a8df1cc7e61

[GLIB] Debian Stable & Ubuntu LTS build is failing after r291093 https://bugs.webkit.org/show_bug.cgi?id=237756
Reviewed by Carlos Garcia Campos.

Use `soup_websocket_connection_send_message` when SOUP < 2.67.3.

* UIProcess/Inspector/glib/RemoteInspectorHTTPServer.cpp:



Canonical link: https://commits.webkit.org/248310@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@291150 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/Inspector/glib/RemoteInspectorHTTPServer.cpp
@@ -144,8 +144,12 @@ void RemoteInspectorHTTPServer::sendMessageToFrontend(uint64_t connectionID, uin
 
     auto utf8 = message.utf8();
     // Soup is going to copy the data immediately, so we can use g_bytes_new_static() here to avoid more data copies.
+#if SOUP_CHECK_VERSION(2, 67, 3)
     GRefPtr<GBytes> bytes = adoptGRef(g_bytes_new_static(utf8.data(), utf8.length()));
     soup_websocket_connection_send_message(webSocketConnection, SOUP_WEBSOCKET_DATA_TEXT, bytes.get());
+#else
+    soup_websocket_connection_send_text(webSocketConnection, CString(reinterpret_cast<const char*>(utf8.data()), utf8.length()).data());
+#endif
 }
 
 void RemoteInspectorHTTPServer::targetDidClose(uint64_t connectionID, uint64_t targetID)