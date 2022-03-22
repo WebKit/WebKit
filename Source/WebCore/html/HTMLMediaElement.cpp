5b9a715490246c58c0957c2d1e8102c5f965d98f
Eric Carlson <eric.carlson@apple.com>
2022-03-22 17:56:11 +0000

    Cherry-pick r291029. rdar://problem/88690874
        [Cocoa] metadata cue endTime may not be updated
        https://bugs.webkit.org/show_bug.cgi?id=237630
        rdar://88690874
    
        Reviewed by Jer Noble.
    
        Source/WebCore:
    
        Data cues have a start time but not an explicit duration, a data cue ends when
        the next data cue from the same track starts. This means we don’t know the
        duration of cue #1 until cue #2 is delivered, so when cue #1 is delivered it is
        given the end time of the media file’s duration and the actual end time is updated
        when cue #2 arrives.
    
        http://webkit.org/b/229924 refactored text, audio, and video tracks to not depend
        on HTMLMediaElement. Because InbandDataTextTrack could no longer access the
        HTMLMediaElement to get its duration, a duration property was added to TextTrackList
        that InbandDataTextTrack uses to set the duration of temporary cues.
        TextTrackList.duration is set when it is created and updated when the media player
        reports a duration change.
    
        This means that if the media file’s duration is not known when the text track list
        is created, and the file's duration never changes, the text track list never has a
        valid duration and data cues were not added to the temporary list.
    
        Fix this by updating TextTrackList.duration when a HTMLMediaElement reaches HAVE_METADATA.
    
        Test: http/tests/media/hls/track-in-band-hls-metadata-cue-duration.html
    
        * html/HTMLMediaElement.cpp:
        (WebCore::HTMLMediaElement::durationChanged): Update m_textTracks.duration and post
        the 'durationchange' event.
        (WebCore::HTMLMediaElement::setReadyState): Call durationChanged.
        (WebCore::HTMLMediaElement::mediaPlayerDurationChanged): Ditto.
        * html/HTMLMediaElement.h:
    
        * html/track/InbandDataTextTrack.cpp:
        (WebCore::InbandDataTextTrack::addDataCue): Add cues to the incomplete cue map
        even if the track list doesn't have duration.
    
        LayoutTests:
    
        * http/tests/media/hls/track-in-band-hls-metadata-cue-duration-expected.txt: Added.
        * http/tests/media/hls/track-in-band-hls-metadata-cue-duration.html: Added.
    
        Canonical link: https://commits.webkit.org/248203@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@291029 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/245886.331@safari-613-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-613-branch@291658 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/HTMLMediaElement.cpp
+++ Source/WebCore/html/HTMLMediaElement.cpp
@@ -2439,6 +2439,13 @@ void HTMLMediaElement::dispatchPlayPauseEventsIfNeedsQuirks()
     scheduleEvent(eventNames().pauseEvent);
 }
 
+void HTMLMediaElement::durationChanged()
+{
+    if (m_textTracks)
+        m_textTracks->setDuration(durationMediaTime());
+    scheduleEvent(eventNames().durationchangeEvent);
+}
+
 void HTMLMediaElement::setReadyState(MediaPlayer::ReadyState state)
 {
     // Set "wasPotentiallyPlaying" BEFORE updating m_readyState, potentiallyPlaying() uses it
@@ -2499,7 +2506,7 @@ void HTMLMediaElement::setReadyState(MediaPlayer::ReadyState state)
         // HAVE_METADATA.
         if (m_readyState >= HAVE_METADATA && oldState < HAVE_METADATA) {
             prepareMediaFragmentURI();
-            scheduleEvent(eventNames().durationchangeEvent);
+            durationChanged();
             scheduleResizeEvent();
             scheduleEvent(eventNames().loadedmetadataEvent);
 
@@ -5147,7 +5154,7 @@ void HTMLMediaElement::mediaPlayerDurationChanged()
 {
     beginProcessingMediaPlayerCallback();
 
-    scheduleEvent(eventNames().durationchangeEvent);
+    durationChanged();
     mediaPlayerCharacteristicChanged();
 
     MediaTime now = currentMediaTime();
@@ -5155,9 +5162,6 @@ void HTMLMediaElement::mediaPlayerDurationChanged()
     ALWAYS_LOG(LOGIDENTIFIER, "duration = ", dur, ", current time = ", now);
     if (now > dur)
         seekInternal(dur);
-
-    if (m_textTracks)
-        m_textTracks->setDuration(dur);
 
     endProcessingMediaPlayerCallback();
 }
