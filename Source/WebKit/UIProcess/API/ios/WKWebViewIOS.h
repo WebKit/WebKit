32d4eed5e7e59eb4dcd0ce465b525748c995e1b9
Commit Queue <commit-queue@webkit.org>
2022-03-26 03:05:18 +0000

    Unreviewed, reverting r291884. https://bugs.webkit.org/show_bug.cgi?id=238405
    Broke the internal iOS build
    
    Reverted changeset:
    
    "Add Captive Portal alert to WKWebView"
    https://bugs.webkit.org/show_bug.cgi?id=238326
    https://commits.webkit.org/r291884
    
    
    Canonical link: https://commits.webkit.org/248901@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@291930 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/ios/WKWebViewIOS.h
+++ Source/WebKit/UIProcess/API/ios/WKWebViewIOS.h
@@ -25,9 +25,6 @@
 
 #import "WKWebViewInternal.h"
 #import "_WKTapHandlingResult.h"
-#if USE(APPLE_INTERNAL_SDK) && PLATFORM(IOS_FAMILY)
-#import <WebKitAdditions/WKWebViewAdditions.h>
-#endif
 
 @class UIScrollEvent;
 
