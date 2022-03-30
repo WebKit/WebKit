WebKit/WebKit

commit 1781ab0f530fe8f449f269808ce09161b5200fa2

Preserve repeat() notation for grid-templates https://bugs.webkit.org/show_bug.cgi?id=202259
Reviewed by Dean Jackson.

LayoutTests/imported/w3c:

* web-platform-tests/css/css-grid/animation/grid-template-columns-interpolation-expected.txt:
* web-platform-tests/css/css-grid/animation/grid-template-rows-interpolation-expected.txt:
* web-platform-tests/css/css-grid/parsing/grid-template-columns-computed-nogrid-expected.txt:
* web-platform-tests/css/css-grid/parsing/grid-template-rows-computed-nogrid-expected.txt:

Update expectations for existing WPT tests, now that we get the correct computed value for
grid-template-rows/columns.

Source/WebCore:

* animation/CSSPropertyAnimation.cpp:
(WebCore::blendFunc):
(WebCore::CSSPropertyAnimationWrapperMap::CSSPropertyAnimationWrapperMap):
(WebCore::GridTemplateTracksWrapper<ForColumns>::GridTemplateTracksWrapper): Deleted.
(WebCore::GridTemplateTracksWrapper<ForRows>::GridTemplateTracksWrapper): Deleted.

Replaces GridTemplateTracksWrapper with a simpler GridTemplatePropertyWrapper that just compares the
track lists. In the future we can iterate the two tracks lists in parallel and interpolate per-item,
in order to get animation correct per-spec.

* css/CSSComputedStyleDeclaration.cpp:
(WebCore::populateGridTrackList):
(WebCore::populateSubgridLineNameList):
(WebCore::valueForGridTrackList):
(WebCore::OrderedNamedLinesCollectorInsideRepeat::OrderedNamedLinesCollectorInsideRepeat): Deleted.
(WebCore::OrderedNamedLinesCollector::collectLineNamesForIndex const): Deleted.
(WebCore::OrderedNamedLinesCollectorInsideRepeat::collectLineNamesForIndex const): Deleted.

Implements getComputedStyle by iterating the track list and doing a 1:1 conversion to CSSValues, instead
of reconstructing from the processed hashtables.
This correctly preserves repeat() instances in the track list, which weren't retreivable from the processed
data.

* rendering/style/RenderStyle.h:
(WebCore::RenderStyle::gridColumns const):

Source/WebCore/rendering/style/StyleGridData.h
@@ -28,15 +28,41 @@
 #include "GridArea.h"
 #include "GridTrackSize.h"
 #include "RenderStyleConstants.h"
+#include <variant>
 #include <wtf/Ref.h>
 #include <wtf/RefCounted.h>
 #include <wtf/Vector.h>
+#include <wtf/text/TextStream.h>
 #include <wtf/text/WTFString.h>
 
 namespace WebCore {
 
 typedef HashMap<String, Vector<unsigned>> NamedGridLinesMap;
 typedef HashMap<unsigned, Vector<String>, IntHash<unsigned>, WTF::UnsignedWithZeroKeyHashTraits<unsigned>> OrderedNamedGridLinesMap;
+
+typedef std::variant<GridTrackSize, Vector<String>> RepeatEntry;
+typedef Vector<RepeatEntry> RepeatTrackList;
+
+struct GridTrackEntrySubgrid {
+    bool operator==(const GridTrackEntrySubgrid&) const = default;
+};
+struct GridTrackEntryRepeat {
+    bool operator==(const GridTrackEntryRepeat&) const = default;
+
+    unsigned repeats;
+    RepeatTrackList list;
+};
+struct GridTrackEntryAutoRepeat {
+    bool operator==(const GridTrackEntryAutoRepeat&) const = default;
+
+    AutoRepeatType type;
+    RepeatTrackList list;
+};
+typedef std::variant<GridTrackSize, Vector<String>, GridTrackEntryRepeat, GridTrackEntryAutoRepeat, GridTrackEntrySubgrid> GridTrackEntry;
+typedef Vector<GridTrackEntry> GridTrackList;
+
+WTF::TextStream& operator<<(WTF::TextStream&, const RepeatEntry& item);
+WTF::TextStream& operator<<(WTF::TextStream&, const GridTrackEntry& item);
 
 class StyleGridData : public RefCounted<StyleGridData> {
 public:
@@ -45,20 +71,7 @@ public:
 
     bool operator==(const StyleGridData& o) const
     {
-        // FIXME: comparing two hashes doesn't look great for performance. Something to keep in mind going forward.
-        return gridColumns == o.gridColumns && gridRows == o.gridRows
-            && gridAutoFlow == o.gridAutoFlow && gridAutoRows == o.gridAutoRows && gridAutoColumns == o.gridAutoColumns
-            && namedGridColumnLines == o.namedGridColumnLines && namedGridRowLines == o.namedGridRowLines
-            && autoRepeatNamedGridColumnLines == o.autoRepeatNamedGridColumnLines && autoRepeatNamedGridRowLines == o.autoRepeatNamedGridRowLines
-            && autoRepeatOrderedNamedGridColumnLines == o.autoRepeatOrderedNamedGridColumnLines && autoRepeatOrderedNamedGridRowLines == o.autoRepeatOrderedNamedGridRowLines
-            && implicitNamedGridColumnLines == o.implicitNamedGridColumnLines && implicitNamedGridRowLines == o.implicitNamedGridRowLines
-            && namedGridArea == o.namedGridArea && namedGridArea == o.namedGridArea
-            && namedGridAreaRowCount == o.namedGridAreaRowCount && namedGridAreaColumnCount == o.namedGridAreaColumnCount
-            && orderedNamedGridRowLines == o.orderedNamedGridRowLines && orderedNamedGridColumnLines == o.orderedNamedGridColumnLines
-            && gridAutoRepeatColumns == o.gridAutoRepeatColumns && gridAutoRepeatRows == o.gridAutoRepeatRows
-            && autoRepeatColumnsInsertionPoint == o.autoRepeatColumnsInsertionPoint && autoRepeatRowsInsertionPoint == o.autoRepeatRowsInsertionPoint
-            && autoRepeatColumnsType == o.autoRepeatColumnsType && autoRepeatRowsType == o.autoRepeatRowsType && subgridRows == o.subgridRows
-            && subgridColumns == o.subgridColumns;
+        return columns() == o.columns() && rows() == o.rows() && implicitNamedGridColumnLines == o.implicitNamedGridColumnLines && implicitNamedGridRowLines == o.implicitNamedGridRowLines && gridAutoFlow == o.gridAutoFlow && gridAutoRows == o.gridAutoRows && gridAutoColumns == o.gridAutoColumns && namedGridArea == o.namedGridArea && namedGridAreaRowCount == o.namedGridAreaRowCount && namedGridAreaColumnCount == o.namedGridAreaColumnCount;
     }
 
     bool operator!=(const StyleGridData& o) const
@@ -66,19 +79,37 @@ public:
         return !(*this == o);
     }
 
-    Vector<GridTrackSize> gridColumns;
-    Vector<GridTrackSize> gridRows;
+    void setRows(const GridTrackList&);
+    void setColumns(const GridTrackList&);
 
-    NamedGridLinesMap namedGridColumnLines;
-    NamedGridLinesMap namedGridRowLines;
+    const Vector<GridTrackSize>& gridColumns() const { return m_gridColumns; }
+    const Vector<GridTrackSize>& gridRows() const { return m_gridRows; }
 
-    OrderedNamedGridLinesMap orderedNamedGridColumnLines;
-    OrderedNamedGridLinesMap orderedNamedGridRowLines;
+    const NamedGridLinesMap& namedGridColumnLines() const { return m_namedGridColumnLines; };
+    const NamedGridLinesMap& namedGridRowLines() const { return m_namedGridRowLines; };
 
-    NamedGridLinesMap autoRepeatNamedGridColumnLines;
-    NamedGridLinesMap autoRepeatNamedGridRowLines;
-    OrderedNamedGridLinesMap autoRepeatOrderedNamedGridColumnLines;
-    OrderedNamedGridLinesMap autoRepeatOrderedNamedGridRowLines;
+    const OrderedNamedGridLinesMap& orderedNamedGridColumnLines() const { return m_orderedNamedGridColumnLines; }
+    const OrderedNamedGridLinesMap& orderedNamedGridRowLines() const { return m_orderedNamedGridRowLines; }
+
+    const NamedGridLinesMap& autoRepeatNamedGridColumnLines() const { return m_autoRepeatNamedGridColumnLines; }
+    const NamedGridLinesMap& autoRepeatNamedGridRowLines() const { return m_autoRepeatNamedGridRowLines; }
+    const OrderedNamedGridLinesMap& autoRepeatOrderedNamedGridColumnLines() const { return m_autoRepeatOrderedNamedGridColumnLines; }
+    const OrderedNamedGridLinesMap& autoRepeatOrderedNamedGridRowLines() const { return m_autoRepeatOrderedNamedGridRowLines; }
+
+    const Vector<GridTrackSize>& gridAutoRepeatColumns() const { return m_gridAutoRepeatColumns; }
+    const Vector<GridTrackSize>& gridAutoRepeatRows() const { return m_gridAutoRepeatRows; }
+
+    const unsigned& autoRepeatColumnsInsertionPoint() const { return m_autoRepeatColumnsInsertionPoint; }
+    const unsigned& autoRepeatRowsInsertionPoint() const { return m_autoRepeatRowsInsertionPoint; }
+
+    const AutoRepeatType& autoRepeatColumnsType() const { return m_autoRepeatColumnsType; }
+    const AutoRepeatType& autoRepeatRowsType() const { return m_autoRepeatRowsType; }
+
+    const bool& subgridRows() const { return m_subgridRows; };
+    const bool& subgridColumns() const { return m_subgridColumns; }
+
+    const GridTrackList& columns() const { return m_columns; };
+    const GridTrackList& rows() const { return m_rows; };
 
     NamedGridLinesMap implicitNamedGridColumnLines;
     NamedGridLinesMap implicitNamedGridRowLines;
@@ -94,21 +125,43 @@ public:
     unsigned namedGridAreaRowCount;
     unsigned namedGridAreaColumnCount;
 
-    Vector<GridTrackSize> gridAutoRepeatColumns;
-    Vector<GridTrackSize> gridAutoRepeatRows;
-
-    unsigned autoRepeatColumnsInsertionPoint;
-    unsigned autoRepeatRowsInsertionPoint;
-
-    AutoRepeatType autoRepeatColumnsType;
-    AutoRepeatType autoRepeatRowsType;
-
-    bool subgridRows;
-    bool subgridColumns;
-
 private:
+
+    void computeCachedTrackData(const GridTrackList&, Vector<GridTrackSize>& sizes, NamedGridLinesMap& namedLines, OrderedNamedGridLinesMap& orderedNamedLines, Vector<GridTrackSize>& autoRepeatSizes, NamedGridLinesMap& autoRepeatNamedLines, OrderedNamedGridLinesMap& autoRepeatOrderedNamedLines, unsigned& autoRepeatInsertionPoint, AutoRepeatType&, bool& subgrid);
+
+    GridTrackList m_columns;
+    GridTrackList m_rows;
+
+    // Cached data computed from m_colmns/rows;
+    Vector<GridTrackSize> m_gridColumns;
+    Vector<GridTrackSize> m_gridRows;
+
+    NamedGridLinesMap m_namedGridColumnLines;
+    NamedGridLinesMap m_namedGridRowLines;
+
+    OrderedNamedGridLinesMap m_orderedNamedGridColumnLines;
+    OrderedNamedGridLinesMap m_orderedNamedGridRowLines;
+
+    NamedGridLinesMap m_autoRepeatNamedGridColumnLines;
+    NamedGridLinesMap m_autoRepeatNamedGridRowLines;
+    OrderedNamedGridLinesMap m_autoRepeatOrderedNamedGridColumnLines;
+    OrderedNamedGridLinesMap m_autoRepeatOrderedNamedGridRowLines;
+
+    Vector<GridTrackSize> m_gridAutoRepeatColumns;
+    Vector<GridTrackSize> m_gridAutoRepeatRows;
+
+    unsigned m_autoRepeatColumnsInsertionPoint;
+    unsigned m_autoRepeatRowsInsertionPoint;
+
+    AutoRepeatType m_autoRepeatColumnsType;
+    AutoRepeatType m_autoRepeatRowsType;
+
+    bool m_subgridRows;
+    bool m_subgridColumns;
+
     StyleGridData();
     StyleGridData(const StyleGridData&);
 };
 
 } // namespace WebCore
+

LayoutTests/fast/css-grid-layout/grid-repeat-calc-expected.txt
@@ -6,7 +6,7 @@ On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE
 PASS testDiv.style['grid-template-rows'] is ""
 testDiv.style['grid-template-rows'] = 'repeat(calc(1 + 1), 18px)'
 PASS testDiv.style['grid-template-rows'] is "repeat(2, 18px)"
-PASS window.getComputedStyle(testDiv).getPropertyValue('grid-template-rows') is "18px 18px"
+PASS window.getComputedStyle(testDiv).getPropertyValue('grid-template-rows') is "repeat(2, 18px)"
 PASS successfullyParsed is true
 
 TEST COMPLETE

LayoutTests/fast/css-grid-layout/grid-repeat-calc.html
@@ -11,7 +11,7 @@ var testDiv = document.getElementById("testDiv");
 shouldBeEmptyString("testDiv.style['grid-template-rows']");
 evalAndLog("testDiv.style['grid-template-rows'] = 'repeat(calc(1 + 1), 18px)'");
 shouldBeEqualToString("testDiv.style['grid-template-rows']", "repeat(2, 18px)");
-shouldBeEqualToString("window.getComputedStyle(testDiv).getPropertyValue('grid-template-rows')", "18px 18px");
+shouldBeEqualToString("window.getComputedStyle(testDiv).getPropertyValue('grid-template-rows')", "repeat(2, 18px)");
 
 </script>
 <script src="../../resources/js-test-post.js"></script>

LayoutTests/fast/css-grid-layout/non-grid-element-repeat-get-set-expected.txt
@@ -4,24 +4,24 @@ On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE
 
 
 Test getting grid-template-columns and grid-template-rows set through CSS
-PASS window.getComputedStyle(singleSingleTrackRepeat, '').getPropertyValue('grid-template-columns') is "15%"
-PASS window.getComputedStyle(singleSingleTrackRepeat, '').getPropertyValue('grid-template-rows') is "18px"
-PASS window.getComputedStyle(twoSingleTrackRepeat, '').getPropertyValue('grid-template-columns') is "minmax(15px, 50%) minmax(15px, 50%)"
-PASS window.getComputedStyle(twoSingleTrackRepeat, '').getPropertyValue('grid-template-rows') is "auto auto"
-PASS window.getComputedStyle(twoDoubleTrackRepeat, '').getPropertyValue('grid-template-columns') is "auto minmax(100px, 120px) auto minmax(100px, 120px)"
-PASS window.getComputedStyle(twoDoubleTrackRepeat, '').getPropertyValue('grid-template-rows') is "minmax(5px, 10px) auto minmax(5px, 10px) auto"
-PASS window.getComputedStyle(twoDoubleTrackWithNamedGridLineRepeat, '').getPropertyValue('grid-template-columns') is "auto [middle] 250px [end] auto [middle] 250px [end]"
-PASS window.getComputedStyle(twoDoubleTrackWithNamedGridLineRepeat, '').getPropertyValue('grid-template-rows') is "10px [start] auto [end] 10px [start] auto [end]"
-PASS window.getComputedStyle(twoDoubleTrackWithTrailingNamedGridLineRepeat, '').getPropertyValue('grid-template-columns') is "[before] auto [before] auto"
-PASS window.getComputedStyle(twoDoubleTrackWithTrailingNamedGridLineRepeat, '').getPropertyValue('grid-template-rows') is "[before] 10px [before] 10px"
-PASS window.getComputedStyle(trailingNamedGridLineRepeat, '').getPropertyValue('grid-template-columns') is "250px [end]"
-PASS window.getComputedStyle(trailingNamedGridLineRepeat, '').getPropertyValue('grid-template-rows') is "10px [end]"
-PASS window.getComputedStyle(leadingNamedGridLineRepeat, '').getPropertyValue('grid-template-columns') is "[start] 250px 250px"
-PASS window.getComputedStyle(leadingNamedGridLineRepeat, '').getPropertyValue('grid-template-rows') is "[start] 10px 10px"
-PASS window.getComputedStyle(mixRepeatAfterNonRepeat, '').getPropertyValue('grid-template-columns') is "[start] 140px 250px 250px"
-PASS window.getComputedStyle(mixRepeatAfterNonRepeat, '').getPropertyValue('grid-template-rows') is "auto 10px 10px"
-PASS window.getComputedStyle(mixNonRepeatAfterRepeat, '').getPropertyValue('grid-template-columns') is "250px 250px 15% [last]"
-PASS window.getComputedStyle(mixNonRepeatAfterRepeat, '').getPropertyValue('grid-template-rows') is "10px 10px [end] auto"
+PASS window.getComputedStyle(singleSingleTrackRepeat, '').getPropertyValue('grid-template-columns') is "repeat(1, 15%)"
+PASS window.getComputedStyle(singleSingleTrackRepeat, '').getPropertyValue('grid-template-rows') is "repeat(1, 18px)"
+PASS window.getComputedStyle(twoSingleTrackRepeat, '').getPropertyValue('grid-template-columns') is "repeat(2, minmax(15px, 50%))"
+PASS window.getComputedStyle(twoSingleTrackRepeat, '').getPropertyValue('grid-template-rows') is "repeat(2, auto)"
+PASS window.getComputedStyle(twoDoubleTrackRepeat, '').getPropertyValue('grid-template-columns') is "repeat(2, auto minmax(100px, 120px))"
+PASS window.getComputedStyle(twoDoubleTrackRepeat, '').getPropertyValue('grid-template-rows') is "repeat(2, minmax(5px, 10px) auto)"
+PASS window.getComputedStyle(twoDoubleTrackWithNamedGridLineRepeat, '').getPropertyValue('grid-template-columns') is "repeat(2, auto [middle] 250px [end])"
+PASS window.getComputedStyle(twoDoubleTrackWithNamedGridLineRepeat, '').getPropertyValue('grid-template-rows') is "repeat(2, 10px [start] auto [end])"
+PASS window.getComputedStyle(twoDoubleTrackWithTrailingNamedGridLineRepeat, '').getPropertyValue('grid-template-columns') is "repeat(2, [before] auto)"
+PASS window.getComputedStyle(twoDoubleTrackWithTrailingNamedGridLineRepeat, '').getPropertyValue('grid-template-rows') is "repeat(2, [before] 10px)"
+PASS window.getComputedStyle(trailingNamedGridLineRepeat, '').getPropertyValue('grid-template-columns') is "repeat(1, 250px) [end]"
+PASS window.getComputedStyle(trailingNamedGridLineRepeat, '').getPropertyValue('grid-template-rows') is "repeat(1, 10px) [end]"
+PASS window.getComputedStyle(leadingNamedGridLineRepeat, '').getPropertyValue('grid-template-columns') is "[start] repeat(2, 250px)"
+PASS window.getComputedStyle(leadingNamedGridLineRepeat, '').getPropertyValue('grid-template-rows') is "[start] repeat(2, 10px)"
+PASS window.getComputedStyle(mixRepeatAfterNonRepeat, '').getPropertyValue('grid-template-columns') is "[start] 140px repeat(2, 250px)"
+PASS window.getComputedStyle(mixRepeatAfterNonRepeat, '').getPropertyValue('grid-template-rows') is "auto repeat(2, 10px)"
+PASS window.getComputedStyle(mixNonRepeatAfterRepeat, '').getPropertyValue('grid-template-columns') is "repeat(2, 250px) 15% [last]"
+PASS window.getComputedStyle(mixNonRepeatAfterRepeat, '').getPropertyValue('grid-template-rows') is "repeat(2, 10px) [end] auto"
 
 Test invalid repeat syntax.
 PASS window.getComputedStyle(element, '').getPropertyValue('grid-template-columns') is "none"

LayoutTests/fast/css-grid-layout/non-grid-element-repeat-get-set.html
@@ -66,15 +66,15 @@
     description('Test that setting and getting grid-template-columns and grid-template-rows with repeat() works as expected');
 
     debug("Test getting grid-template-columns and grid-template-rows set through CSS");
-    testGridTemplatesValues(document.getElementById("singleSingleTrackRepeat"), "15%", "18px");
-    testGridTemplatesValues(document.getElementById("twoSingleTrackRepeat"), "minmax(15px, 50%) minmax(15px, 50%)", "auto auto");
-    testGridTemplatesValues(document.getElementById("twoDoubleTrackRepeat"), "auto minmax(100px, 120px) auto minmax(100px, 120px)", "minmax(5px, 10px) auto minmax(5px, 10px) auto");
-    testGridTemplatesValues(document.getElementById("twoDoubleTrackWithNamedGridLineRepeat"), "auto [middle] 250px [end] auto [middle] 250px [end]", "10px [start] auto [end] 10px [start] auto [end]");
-    testGridTemplatesValues(document.getElementById("twoDoubleTrackWithTrailingNamedGridLineRepeat"), "[before] auto [before] auto", "[before] 10px [before] 10px");
-    testGridTemplatesValues(document.getElementById("trailingNamedGridLineRepeat"), "250px [end]", "10px [end]");
-    testGridTemplatesValues(document.getElementById("leadingNamedGridLineRepeat"), "[start] 250px 250px", "[start] 10px 10px");
-    testGridTemplatesValues(document.getElementById("mixRepeatAfterNonRepeat"), "[start] 140px 250px 250px", "auto 10px 10px");
-    testGridTemplatesValues(document.getElementById("mixNonRepeatAfterRepeat"), "250px 250px 15% [last]", "10px 10px [end] auto");
+    testGridTemplatesValues(document.getElementById("singleSingleTrackRepeat"), "repeat(1, 15%)", "repeat(1, 18px)");
+    testGridTemplatesValues(document.getElementById("twoSingleTrackRepeat"), "repeat(2, minmax(15px, 50%))", "repeat(2, auto)");
+    testGridTemplatesValues(document.getElementById("twoDoubleTrackRepeat"), "repeat(2, auto minmax(100px, 120px))", "repeat(2, minmax(5px, 10px) auto)");
+    testGridTemplatesValues(document.getElementById("twoDoubleTrackWithNamedGridLineRepeat"), "repeat(2, auto [middle] 250px [end])", "repeat(2, 10px [start] auto [end])");
+    testGridTemplatesValues(document.getElementById("twoDoubleTrackWithTrailingNamedGridLineRepeat"), "repeat(2, [before] auto)", "repeat(2, [before] 10px)");
+    testGridTemplatesValues(document.getElementById("trailingNamedGridLineRepeat"), "repeat(1, 250px) [end]", "repeat(1, 10px) [end]");
+    testGridTemplatesValues(document.getElementById("leadingNamedGridLineRepeat"), "[start] repeat(2, 250px)", "[start] repeat(2, 10px)");
+    testGridTemplatesValues(document.getElementById("mixRepeatAfterNonRepeat"), "[start] 140px repeat(2, 250px)", "auto repeat(2, 10px)");
+    testGridTemplatesValues(document.getElementById("mixNonRepeatAfterRepeat"), "repeat(2, 250px) 15% [last]", "repeat(2, 10px) [end] auto");
 
     debug("");
     debug("Test invalid repeat syntax.");

commit 263d48917a53bc1ed564093e1ef682edc81ffde0

[GCC] Ubuntu LTS build broken after r291956 https://bugs.webkit.org/show_bug.cgi?id=238481
Reviewed by Darin Adler.

Default comparisons by value (P1946R0) is only supported since GCC10.
https://en.cppreference.com/w/cpp/compiler_support/20

* rendering/style/StyleGridData.h:
(WebCore::GridTrackEntrySubgrid::operator== const):
(WebCore::GridTrackEntryRepeat::operator== const):
(WebCore::GridTrackEntryAutoRepeat::operator== const):



Canonical link: https://commits.webkit.org/249014@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@292094 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/style/StyleGridData.h
@@ -44,16 +44,25 @@ typedef std::variant<GridTrackSize, Vector<String>> RepeatEntry;
 typedef Vector<RepeatEntry> RepeatTrackList;
 
 struct GridTrackEntrySubgrid {
-    bool operator==(const GridTrackEntrySubgrid&) const = default;
+    bool operator==(const GridTrackEntrySubgrid&) const
+    {
+        return true;
+    }
 };
 struct GridTrackEntryRepeat {
-    bool operator==(const GridTrackEntryRepeat&) const = default;
+    bool operator==(const GridTrackEntryRepeat& other) const
+    {
+        return repeats == other.repeats && list == other.list;
+    }
 
     unsigned repeats;
     RepeatTrackList list;
 };
 struct GridTrackEntryAutoRepeat {
-    bool operator==(const GridTrackEntryAutoRepeat&) const = default;
+    bool operator==(const GridTrackEntryAutoRepeat& other) const
+    {
+        return type == other.type && list == other.list;
+    }
 
     AutoRepeatType type;
     RepeatTrackList list;