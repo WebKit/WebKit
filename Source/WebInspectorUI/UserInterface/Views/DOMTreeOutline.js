d57e1ce6cf97cc7808420b1c8c5d26db4ce9aa26
Patrick Angle <pangle@apple.com>
2022-04-05 23:52:51 +0000

    Web Inspector: Regression(r291729) The result of logging $0 to the console shows overlapping `$0` and `$[n]` after the node https://bugs.webkit.org/show_bug.cgi?id=238843
    Reviewed by Devin Rousso.
    
    r291729 was missing a check that the `DOMTreeOutline` is actually supposed to show an indicator for the
    inspected node, which means other uses of `DOMTreeOutline` (the console, the sources tab, etc.) were showing the
    $0 indicator as well.
    
    * UserInterface/Views/DOMTreeOutline.js:
    (WI.DOMTreeOutline.prototype.update):
    
    Canonical link: https://commits.webkit.org/249294@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@292441 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebInspectorUI/UserInterface/Views/DOMTreeOutline.js
+++ Source/WebInspectorUI/UserInterface/Views/DOMTreeOutline.js
@@ -61,7 +61,8 @@ WI.DOMTreeOutline = class DOMTreeOutline extends WI.TreeOutline
         this._hideElementsKeyboardShortcut = new WI.KeyboardShortcut(null, "H", this._hideElements.bind(this), this.element);
         this._hideElementsKeyboardShortcut.implicitlyPreventsDefault = false;
 
-        if (showInspectedNode)
+        this._showInspectedNode = !!showInspectedNode;
+        if (this._showInspectedNode)
             WI.domManager.addEventListener(WI.DOMManager.Event.InspectedNodeChanged, this._handleInspectedNodeChanged, this);
     }
 
@@ -189,7 +190,7 @@ WI.DOMTreeOutline = class DOMTreeOutline extends WI.TreeOutline
             }
         }
 
-        if (WI.domManager.inspectedNode) {
+        if (this._showInspectedNode && WI.domManager.inspectedNode) {
             let inspectedNodeTreeElement = this.findTreeElement(WI.domManager.inspectedNode);
             if (inspectedNodeTreeElement) {
                 inspectedNodeTreeElement.reveal();
