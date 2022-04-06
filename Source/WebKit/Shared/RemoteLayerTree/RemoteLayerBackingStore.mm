52d2a58c5b8b093bdd45ee7a48cf93e173891f5f
Russell Epstein <repstein@apple.com>
2022-04-06 20:47:28 +0000

    Fix the CG_DISPLAY_LIST_BACKED_IMAGE_BUFFER build https://bugs.webkit.org/show_bug.cgi?id=238783
    Unreviewed build fix.
    
    ConcreteImageBuffer<>::create() needs a default CreationContext argument.
    
    * Shared/RemoteLayerTree/RemoteLayerBackingStore.mm:
    (WebKit::RemoteLayerBackingStore::ensureFrontBuffer):
    
    Canonical link: https://commits.webkit.org/249173.5@safari-614.1.9-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-614.1.9-branch@292498 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/RemoteLayerTree/RemoteLayerBackingStore.mm
+++ Source/WebKit/Shared/RemoteLayerTree/RemoteLayerBackingStore.mm
@@ -365,7 +365,7 @@ void RemoteLayerBackingStore::ensureFrontBuffer()
 
 #if ENABLE(CG_DISPLAY_LIST_BACKED_IMAGE_BUFFER)
     if (m_includeDisplayList == IncludeDisplayList::Yes)
-        m_frontBuffer.displayListImageBuffer = WebCore::ConcreteImageBuffer<CGDisplayListImageBufferBackend>::create(m_size, m_scale, WebCore::DestinationColorSpace::SRGB(), pixelFormat());
+        m_frontBuffer.displayListImageBuffer = WebCore::ConcreteImageBuffer<CGDisplayListImageBufferBackend>::create(m_size, m_scale, WebCore::DestinationColorSpace::SRGB(), pixelFormat(), { });
 #endif
 }
 
