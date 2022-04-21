b69b3cdfe0c2e1eecc34ded012d21779ebe659ac
Chris Dumez <cdumez@apple.com>
2022-04-21 02:08:23 +0000

    Unreviewed follow-up to r293123 to address review feedback from Sam & Darin
    * css/SelectorFilter.cpp:
    (WebCore::localNameIsKnownToBeLowercase):
    (WebCore::SelectorFilter::collectElementIdentifierHashes):
    
    Canonical link: https://commits.webkit.org/249837@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@293138 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/css/SelectorFilter.cpp
+++ Source/WebCore/css/SelectorFilter.cpp
@@ -45,9 +45,16 @@ static bool isExcludedAttribute(const AtomString& name)
     return name == HTMLNames::classAttr->localName() || name == HTMLNames::idAttr->localName() || name == HTMLNames::styleAttr->localName();
 }
 
+static inline bool localNameIsKnownToBeLowercase(const Element& element)
+{
+    // Known HTML element always return a localName() that is defined inside HTMLNames.h. All known HTML
+    // tags are lowercase.
+    return element.isHTMLElement() && !element.isUnknownElement();
+}
+
 void SelectorFilter::collectElementIdentifierHashes(const Element& element, Vector<unsigned, 4>& identifierHashes)
 {
-    AtomString tagLowercaseLocalName = LIKELY(element.isHTMLElement() && !element.isUnknownElement()) ? element.localName() : element.localName().convertToASCIILowercase();
+    AtomString tagLowercaseLocalName = LIKELY(localNameIsKnownToBeLowercase(element)) ? element.localName() : element.localName().convertToASCIILowercase();
     ASSERT(tagLowercaseLocalName == tagLowercaseLocalName.convertToASCIILowercase());
     identifierHashes.append(tagLowercaseLocalName.impl()->existingHash() * TagNameSalt);
 
