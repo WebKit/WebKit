fa2a1f593e71a10e5b57fac619c1ab3d23d70b06
Mark Lam <mark.lam@apple.com>
2022-04-22 14:03:47 +0000

    Apply purifyNaN in more places. https://bugs.webkit.org/show_bug.cgi?id=239619 <rdar://problem/91924480>
    Reviewed by Yusuke Suzuki.
    
    Source/JavaScriptCore:
    
    * wasm/js/JSWebAssemblyHelpers.h:
    (JSC::toJSValue):
    
    Source/WebCore:
    
    * bindings/js/IDBBindingUtilities.cpp:
    (WebCore::toJS):
    * bindings/js/JSDOMConvertWebGL.cpp:
    (WebCore::convertToJSValue):
    * bindings/js/SerializedScriptValue.cpp:
    (WebCore::CloneDeserializer::readTerminal):
    * bridge/objc/objc_utility.mm:
    (JSC::Bindings::convertObjcValueToValue):
    
    Canonical link: https://commits.webkit.org/249889@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@293220 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/bindings/js/JSDOMConvertWebGL.cpp
+++ Source/WebCore/bindings/js/JSDOMConvertWebGL.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Apple Inc. All rights reserved.
+ * Copyright (C) 2017-2022 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -91,7 +91,7 @@ JSValue convertToJSValue(JSGlobalObject& lexicalGlobalObject, JSDOMGlobalObject&
         }, [] (long long value) -> JSValue {
             return jsNumber(value);
         }, [] (float value) -> JSValue {
-            return jsNumber(value);
+            return jsNumber(purifyNaN(value));
         }, [&] (const String& value) -> JSValue {
             return jsStringWithCache(lexicalGlobalObject.vm(), value);
         }, [&] (const Vector<bool>& values) -> JSValue {
