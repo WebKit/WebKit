758cde8da27a4a3c66d6e99f587559f944062d3b
Russell Epstein <repstein@apple.com>
2022-04-23 00:37:40 +0000

    Revert "Apply patch. rdar://problem/88904160"
    This reverts commit r293270.
    
    Canonical link: https://commits.webkit.org/245886.528@safari-613.2.7.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-613.2.7.1-branch@293273 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/GPUProcess/webrtc/LibWebRTCCodecsProxy.h
+++ Source/WebKit/GPUProcess/webrtc/LibWebRTCCodecsProxy.h
@@ -31,8 +31,7 @@
 #include "DataReference.h"
 #include "RTCDecoderIdentifier.h"
 #include "RTCEncoderIdentifier.h"
-#include <atomic>
-#include <wtf/ThreadAssertions.h>
+#include <wtf/Lock.h>
 
 namespace IPC {
 class Connection;
@@ -52,19 +51,18 @@ namespace WebKit {
 
 class GPUConnectionToWebProcess;
 
-class LibWebRTCCodecsProxy final : public IPC::Connection::ThreadMessageReceiverRefCounted {
+class LibWebRTCCodecsProxy : public IPC::Connection::ThreadMessageReceiverRefCounted {
     WTF_MAKE_FAST_ALLOCATED;
 public:
-    static Ref<LibWebRTCCodecsProxy> create(GPUConnectionToWebProcess&);
+    static Ref<LibWebRTCCodecsProxy> create(GPUConnectionToWebProcess& process) { return adoptRef(*new LibWebRTCCodecsProxy(process)); }
     ~LibWebRTCCodecsProxy();
-    void stopListeningForIPC(Ref<LibWebRTCCodecsProxy>&& refFromConnection);
+
+    void close();
+
     bool allowsExitUnderMemoryPressure() const;
 
 private:
     explicit LibWebRTCCodecsProxy(GPUConnectionToWebProcess&);
-    void initialize();
-    auto createDecoderCallback(RTCDecoderIdentifier, bool useRemoteFrames);
-    WorkQueue& workQueue() const { return m_queue; }
 
     // IPC::Connection::ThreadMessageReceiver
     void dispatchToThread(Function<void()>&&) final;
@@ -87,11 +85,13 @@ private:
 
     CFDictionaryRef ioSurfacePixelBufferCreationOptions(IOSurfaceRef);
 
+    GPUConnectionToWebProcess& m_gpuConnectionToWebProcess;
+
+    mutable Lock m_lock;
+    HashMap<RTCDecoderIdentifier, webrtc::LocalDecoder> m_decoders WTF_GUARDED_BY_LOCK(m_lock); // Only modified on the libWebRTCCodecsQueue but may get accessed from the main thread.
+    HashMap<RTCEncoderIdentifier, webrtc::LocalEncoder> m_encoders WTF_GUARDED_BY_LOCK(m_lock); // Only modified on the libWebRTCCodecsQueue but may get accessed from the main thread.
 
     Ref<WorkQueue> m_queue;
-    HashMap<RTCDecoderIdentifier, webrtc::LocalDecoder> m_decoders WTF_GUARDED_BY_LOCK(workQueue());
-    HashMap<RTCEncoderIdentifier, webrtc::LocalEncoder> m_encoders WTF_GUARDED_BY_LOCK(workQueue());
-    std::atomic<bool> m_hasEncodersOrDecoders { false };
 };
 
 }
