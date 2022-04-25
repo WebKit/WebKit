3d4238a281dd51b6431999b7cb59950d4111f678
Claudio Saavedra <csaavedra@igalia.com>
2022-04-25 15:37:33 +0000

    [GLIB] Default-handle recently-added ProcessTerminationReason values https://bugs.webkit.org/show_bug.cgi?id=239720
    Reviewed by Philippe Normand.
    
    * UIProcess/API/glib/WebKitNavigationClient.cpp: Add a couple of
    new values for ProcessTerminationReason that were added on r292912
    to silent some compile-time warnings.
    
    Canonical link: https://commits.webkit.org/249951@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@293328 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/API/glib/WebKitNavigationClient.cpp
+++ Source/WebKit/UIProcess/API/glib/WebKitNavigationClient.cpp
@@ -123,6 +123,8 @@ private:
         case ProcessTerminationReason::NavigationSwap:
         case ProcessTerminationReason::RequestedByGPUProcess:
         case ProcessTerminationReason::ExceededProcessCountLimit:
+        case ProcessTerminationReason::IdleExit:
+        case ProcessTerminationReason::Unresponsive:
             break;
         }
         return false;
