5fad2014e3ee7c2dd7bccfef9511839e2d1a8f04
Philippe Normand <pnormand@igalia.com>
2022-04-29 16:36:01 +0000

    REGRESSION(r293608): [GTK][WPE] Broke clang builds https://bugs.webkit.org/show_bug.cgi?id=239899
    Patch by Philippe Normand <pnormand@igalia.com> on 2022-04-29
    Reviewed by Michael Catanzaro.
    
    * wtf/StdLibExtras.h: Restrict std::remove_cvref hacks to GCC.
    
    Canonical link: https://commits.webkit.org/250125@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@293619 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/StdLibExtras.h
+++ Source/WTF/wtf/StdLibExtras.h
@@ -606,7 +606,7 @@ template<typename OptionalType> auto valueOrDefault(OptionalType&& optionalValue
 #define WTFMove(value) std::move<WTF::CheckMoveParameter>(value)
 
 // FIXME: Needed for GCC<=9.3. Remove it after Ubuntu 20.04 end of support (May 2023).
-#if defined(__GLIBCXX__) && !defined(HAVE_STD_REMOVE_CVREF)
+#if defined(__GLIBCXX__) && !defined(HAVE_STD_REMOVE_CVREF) && !COMPILER(CLANG)
 namespace std {
 template <typename T>
 struct remove_cvref {
