ffc556953287dea4ad616dc9326f4081bf19410a
Alan Coon <alancoon@apple.com>
2022-05-05 18:36:01 +0000

    Apply patch. rdar://problem/88904160
    Canonical link: https://commits.webkit.org/245886.553@safari-613-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-613-branch@293852 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/GPUProcess/webrtc/LibWebRTCCodecsProxy.h
+++ Source/WebKit/GPUProcess/webrtc/LibWebRTCCodecsProxy.h
@@ -31,6 +31,7 @@
 #include "DataReference.h"
 #include "RTCDecoderIdentifier.h"
 #include "RTCEncoderIdentifier.h"
+#include <WebCore/ProcessIdentity.h>
 #include <atomic>
 #include <wtf/ThreadAssertions.h>
 
@@ -63,7 +64,7 @@ public:
 private:
     explicit LibWebRTCCodecsProxy(GPUConnectionToWebProcess&);
     void initialize();
-    auto createDecoderCallback(RTCDecoderIdentifier, bool useRemoteFrames);
+    auto createDecoderCallback(RTCDecoderIdentifier);
     WorkQueue& workQueue() const { return m_queue; }
 
     // IPC::Connection::ThreadMessageReceiver
@@ -87,8 +88,10 @@ private:
 
     CFDictionaryRef ioSurfacePixelBufferCreationOptions(IOSurfaceRef);
 
-
+    Ref<IPC::Connection> m_connection;
     Ref<WorkQueue> m_queue;
+    const WebCore::ProcessIdentity m_resourceOwner;
+
     HashMap<RTCDecoderIdentifier, webrtc::LocalDecoder> m_decoders WTF_GUARDED_BY_LOCK(workQueue());
     HashMap<RTCEncoderIdentifier, webrtc::LocalEncoder> m_encoders WTF_GUARDED_BY_LOCK(workQueue());
     std::atomic<bool> m_hasEncodersOrDecoders { false };
