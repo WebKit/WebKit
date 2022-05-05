2b241680f4714fc913b7f149ce73aebbb32ec04a
Alan Coon <alancoon@apple.com>
2022-05-05 23:11:30 +0000

    Revert r293850. rdar://problem/88904160
    This reverts a patch application at r293850.
    
    Canonical link: https://commits.webkit.org/245886.556@safari-613-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-613-branch@293866 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/generic/WorkQueueGeneric.cpp
+++ Source/WTF/wtf/generic/WorkQueueGeneric.cpp
@@ -43,9 +43,6 @@ void WorkQueueBase::platformInitialize(const char* name, Type, QOS qos)
 {
     BinarySemaphore semaphore;
     Thread::create(name, [&] {
-#if ASSERT_ENABLED
-        m_threadID = Thread::current().uid();
-#endif
         m_runLoop = &RunLoop::current();
         semaphore.signal();
         m_runLoop->run();
@@ -87,12 +84,5 @@ Ref<WorkQueue> WorkQueue::constructMainWorkQueue()
 {
     return adoptRef(*new WorkQueue(RunLoop::main()));
 }
-
-#if ASSERT_ENABLED
-void WorkQueue::assertIsCurrent()
-{
-    ASSERT(m_threadID == Thread::current().uid());
-}
-#endif
 
 }
