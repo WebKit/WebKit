6b23ec6d5ecd66b8e1e400888d7053f7e5934544
Miguel Gomez <magomez@igalia.com>
2022-05-06 19:06:10 +0000

    [Nicosia] Images in webkit.org/blog/ don't show up with threaded rendering https://bugs.webkit.org/show_bug.cgi?id=238259
    Patch by Miguel Gomez <magomez@igalia.com> on 2022-05-06
    Reviewed by Carlos Garcia Campos.
    
    Implement Nicosia::CairoOperationRecorder::drawFilteredImageBuffer(), which is required
    to render images that have CSS filters applied.
    
    * platform/graphics/nicosia/cairo/NicosiaCairoOperationRecorder.cpp:
    (Nicosia::CairoOperationRecorder::drawFilteredImageBuffer):
    * platform/graphics/nicosia/cairo/NicosiaCairoOperationRecorder.h:
    
    Canonical link: https://commits.webkit.org/247617.56@webkitgtk/2.36
    git-svn-id: https://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-2.36@293915 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/nicosia/cairo/NicosiaCairoOperationRecorder.h
+++ Source/WebCore/platform/graphics/nicosia/cairo/NicosiaCairoOperationRecorder.h
@@ -65,6 +65,7 @@ private:
     void drawGlyphs(const WebCore::Font&, const WebCore::GlyphBufferGlyph*, const WebCore::GlyphBufferAdvance*, unsigned numGlyphs, const WebCore::FloatPoint&, WebCore::FontSmoothingMode) override;
 
     void drawImageBuffer(WebCore::ImageBuffer&, const WebCore::FloatRect& destination, const WebCore::FloatRect& source, const WebCore::ImagePaintingOptions&) override;
+    void drawFilteredImageBuffer(WebCore::ImageBuffer*, const WebCore::FloatRect&, WebCore::Filter&, WebCore::FilterResults&) override;
     void drawNativeImage(WebCore::NativeImage&, const WebCore::FloatSize&, const WebCore::FloatRect&, const WebCore::FloatRect&, const WebCore::ImagePaintingOptions&) override;
     void drawPattern(WebCore::NativeImage&, const WebCore::FloatRect&, const WebCore::FloatRect&, const WebCore::AffineTransform&, const WebCore::FloatPoint&, const WebCore::FloatSize&, const WebCore::ImagePaintingOptions&) override;
 
