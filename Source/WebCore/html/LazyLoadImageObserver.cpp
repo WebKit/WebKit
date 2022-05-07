3555db2ab6e5b3c6a3d5a34726865a48f4825a65
Rob Buis <rbuis@igalia.com>
2022-05-07 00:08:15 +0000

    Use correct document as root for lazy image observer https://bugs.webkit.org/show_bug.cgi?id=240083
    Patch by Rob Buis <rbuis@igalia.com> on 2022-05-06
    Reviewed by Simon Fraser.
    
    LayoutTests/imported/w3c:
    
    Add a test for scrolling a lazy loaded image into view in an iframe.
    
    * web-platform-tests/html/semantics/embedded-content/the-img-element/scrolling-below-viewport-image-lazy-loading-in-iframe-expected.txt: Added.
    * web-platform-tests/html/semantics/embedded-content/the-img-element/scrolling-below-viewport-image-lazy-loading-in-iframe.html: Added.
    
    Source/WebCore:
    
    Use correct document as root for lazy image observer, meaning
    elements in the main frame will use the main frame document and
    elements in iframes will use the iframe document as root.
    
    Test: imported/w3c/web-platform-tests/html/semantics/embedded-content/the-img-element/scrolling-below-viewport-image-lazy-loading-in-iframe.html
    
    * html/LazyLoadImageObserver.cpp:
    (WebCore::LazyLoadImageObserver::intersectionObserver):
    
    Canonical link: https://commits.webkit.org/250379@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@293933 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/html/LazyLoadImageObserver.cpp
+++ Source/WebCore/html/LazyLoadImageObserver.cpp
@@ -86,7 +86,7 @@ IntersectionObserver* LazyLoadImageObserver::intersectionObserver(Document& docu
     if (!m_observer) {
         auto callback = LazyImageLoadIntersectionObserverCallback::create(document);
         static NeverDestroyed<const String> lazyLoadingRootMarginFallback(MAKE_STATIC_STRING_IMPL("100%"));
-        IntersectionObserver::Init options { std::nullopt, lazyLoadingRootMarginFallback, { } };
+        IntersectionObserver::Init options { &document, lazyLoadingRootMarginFallback, { } };
         auto observer = IntersectionObserver::create(document, WTFMove(callback), WTFMove(options));
         if (observer.hasException())
             return nullptr;
