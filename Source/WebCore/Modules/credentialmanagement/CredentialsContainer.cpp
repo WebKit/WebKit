5f1e7a12111e01fe32d4c2ad8355216265e37e92
J Pascoe <j_pascoe@apple.com>
2022-05-13 18:25:56 +0000

    Cherry-pick r294135. rdar://problem/93201070
        [WebAuthn] Remove document focus requirement for conditional mediation requests
        https://bugs.webkit.org/show_bug.cgi?id=240361
        rdar://problem/93201070
    
        Reviewed by Brent Fulgham.
    
        This is necessary so conditional mediation requests work when you open a page in
        a new tab. Conditional mediation requests do not support security keys and the
        request will be aborted if conditional mediation is not available.
    
        * Modules/credentialmanagement/CredentialsContainer.cpp:
        (WebCore::CredentialsContainer::get):
    
        Canonical link: https://commits.webkit.org/250506@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@294135 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/250381.21@safari-7614.1.13-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-7614.1.13-branch@294168 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/Modules/credentialmanagement/CredentialsContainer.cpp
+++ Source/WebCore/Modules/credentialmanagement/CredentialsContainer.cpp
@@ -92,8 +92,8 @@ void CredentialsContainer::get(CredentialRequestOptions&& options, CredentialPro
         return;
     }
 
-    // Extra.
-    if (!m_document->hasFocus()) {
+    // The request will be aborted in WebAuthenticatorCoordinatorProxy if conditional mediation is not available.
+    if (options.mediation != CredentialRequestOptions::MediationRequirement::Conditional && !m_document->hasFocus()) {
         promise.reject(Exception { NotAllowedError, "The document is not focused."_s });
         return;
     }
