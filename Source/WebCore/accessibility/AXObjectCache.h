3d934cd81b30c00dc18ddee2aef8d760dd1db72a
Tyler Wilcock <tyler_w@apple.com>
2022-05-14 04:37:21 +0000

    Infinite recursion caused by call to accessibilityIsIgnored in the midst of AccessibilityObject::ignoredFromModalPresence https://bugs.webkit.org/show_bug.cgi?id=240365
    Reviewed by Chris Fleizach.
    
    Source/WebCore:
    
    We can get infinite recursion when accessibilityIsIgnored is called as
    part of computing AccessibilityObject::ignoredFromModalPresence. One
    example of such a cycle:
    
    AXObjectCache::currentModalNode() ->
    AccessibilityRenderObject::computeAccessibilityIsIgnored() ->
    AccessibilityRenderObject::parentObjectUnignored() ->
    AccessibilityObject::accessibilityIsIgnored() ->
    AccessibilityObject::ignoredFromModalPresence() ->
    AXObjectCache::currentModalNode() ->
    ...repeat...
    
    This patch fixes this by tracking when we start computing the current
    modal node in the AXObjectCache. Then, in AccessibilityObject::accessibilityIsIgnored(),
    we don't call AccessibilityObject::ignoredFromModalPresence() if this new state is true,
    since in this context we only need to know if the object is inherently
    ignored (i.e. ignored disregarding modal presence).
    
    Test: accessibility/aria-modal-with-text-crash.html
    
    * accessibility/AXObjectCache.cpp:
    (WebCore::AXObjectCache::currentModalNode):
    * accessibility/AXObjectCache.h:
    Add m_isRetrievingCurrentModalNode.
    (WebCore::AXObjectCache::isRetrievingCurrentModalNode): Added.
    * accessibility/AccessibilityObject.cpp:
    (WebCore::AccessibilityObject::accessibilityIsIgnored const):
    Don't call ignoredFromModalPresence if we're in the midst of computing the current modal.
    
    LayoutTests:
    
    * accessibility/aria-modal-with-text-crash-expected.txt: Added.
    * accessibility/aria-modal-with-text-crash.html: Added.
    * platform/glib/TestExpectations: Skip new test.
    * platform/ios/TestExpectations: Enable new test.
    * platform/win/TestExpectations: Skip new test.
    
    Canonical link: https://commits.webkit.org/250552@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@294186 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/accessibility/AXObjectCache.h
+++ Source/WebCore/accessibility/AXObjectCache.h
@@ -199,7 +199,8 @@ public:
     void deferNodeAddedOrRemoved(Node*);
     void handleScrolledToAnchor(const Node* anchorNode);
     void handleScrollbarUpdate(ScrollView*);
-    
+
+    bool isRetrievingCurrentModalNode() { return m_isRetrievingCurrentModalNode; }
     Node* modalNode();
 
     void deferAttributeChangeIfNeeded(const QualifiedName&, Element*);
@@ -518,6 +519,7 @@ private:
     // If that changes to require only one aria-modal we could change this to a WeakHashSet, or discard the set completely.
     ListHashSet<Element*> m_modalElementsSet;
     bool m_modalNodesInitialized { false };
+    bool m_isRetrievingCurrentModalNode { false };
 
     Timer m_performCacheUpdateTimer;
 
