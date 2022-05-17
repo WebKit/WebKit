077d3bddbd56a2b64a189dc2de7c9cf7030a6abc
Tyler Wilcock <tyler_w@apple.com>
2022-05-17 06:17:40 +0000

    Cherry-pick r294208. rdar://problem/93118830
        AX: Remove CSSDisplayContentsAXSupportEnabled flag
        https://bugs.webkit.org/show_bug.cgi?id=240409
    
        Reviewed by Chris Fleizach.
    
        Source/WebCore:
    
        We added initial support without using this flag and will continue to
        fix bugs as we find them, making this flag unnecessary.
    
        * page/RuntimeEnabledFeatures.h:
        (WebCore::RuntimeEnabledFeatures::setCSSDisplayContentsAXSupportEnabled): Deleted.
        (WebCore::RuntimeEnabledFeatures::cssDisplayContentsAXSupportEnabled const): Deleted.
    
        Source/WebKitLegacy/mac:
    
        We added initial support without using this flag and will continue to
        fix bugs as we find them, making this flag unnecessary.
    
        * WebView/WebPreferenceKeysPrivate.h:
        * WebView/WebPreferences.mm:
        (-[WebPreferences cssDisplayContentsAXSupportEnabled]): Deleted.
        (-[WebPreferences setCSSDisplayContentsAXSupportEnabled:]): Deleted.
        * WebView/WebPreferencesPrivate.h:
    
        Source/WTF:
    
        * Scripts/Preferences/WebPreferencesInternal.yaml:
    
        Tools:
    
        * DumpRenderTree/TestOptions.cpp:
        (WTR::TestOptions::defaults):
    
        Canonical link: https://commits.webkit.org/250566@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@294208 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/250381.28@safari-7614.1.14.1-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-7614.1.14.1-branch@294307 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKitLegacy/mac/WebView/WebPreferencesPrivate.h
+++ Source/WebKitLegacy/mac/WebView/WebPreferencesPrivate.h
@@ -224,7 +224,6 @@ extern NSString *WebPreferencesCacheModelChangedInternalNotification WEBKIT_DEPR
 @property (nonatomic) BOOL cacheAPIEnabled;
 @property (nonatomic) BOOL downloadAttributeEnabled;
 @property (nonatomic) BOOL directoryUploadEnabled;
-@property (nonatomic, getter=cssDisplayContentsAXSupportEnabled) BOOL CSSDisplayContentsAXSupportEnabled;
 @property (nonatomic, getter=cssLogicalEnabled) BOOL CSSLogicalEnabled;
 @property (nonatomic) BOOL lineHeightUnitsEnabled;
 @property (nonatomic) BOOL layoutFormattingContextIntegrationEnabled;
