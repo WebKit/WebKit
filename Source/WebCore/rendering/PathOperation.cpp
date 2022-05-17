1bf65c70e2558bbedf014bb8d3f37fd674934f68
Nikolaos Mouchtaris <nmouchtaris@apple.com>
2022-05-17 13:57:17 -0700

    Need a short description (OOPS!). Need the bug URL (OOPS!).
    Reviewed by NOBODY (OOPS!).
    
    No new tests (OOPS!).
    * Source/WebCore/platform/graphics/GeometryUtilities.cpp:
    (WebCore::toRelatedAcuteAngle):
    (WebCore::distanceOfPointToSidesOfRect):
    (WebCore::verticesForBox):
    * Source/WebCore/platform/graphics/GeometryUtilities.h:
    * Source/WebCore/rendering/PathOperation.cpp:
    (WebCore::RayPathOperation::lengthForPath const):
    (WebCore::RayPathOperation::lengthForContainPath const):
    (WebCore::RayPathOperation::pathForReferenceRect const):
    * Source/WebCore/rendering/PathOperation.h:
    * Source/WebCore/rendering/style/RenderStyle.cpp:
    (WebCore::getPathFromPathOperation):
    (WebCore::RenderStyle::applyMotionPathTransform const):
    * LayoutTests/TestExpectations:

--- Source/WebCore/rendering/PathOperation.cpp
+++ Source/WebCore/rendering/PathOperation.cpp
@@ -50,7 +50,7 @@ const SVGElement* ReferencePathOperation::element() const
     return m_element.get();
 }
 
-double RayPathOperation::lengthForPath() const
+float RayPathOperation::lengthForPath() const
 {
     auto boundingBox = m_containingBlockBoundingRect;
     auto distances = distanceOfPointToSidesOfRect(boundingBox, m_position);
@@ -70,13 +70,63 @@ double RayPathOperation::lengthForPath() const
     RELEASE_ASSERT_NOT_REACHED();
 }
 
-const Path RayPathOperation::pathForReferenceRect() const
+double RayPathOperation::lengthForContainPath(const FloatRect& elementRect, double computedPathLength, const FloatPoint& anchor, const OffsetRotation rotation) const
+{
+    // Construct vertices of element for determining if they are within the path length
+    // Anchor point as origin
+    Vector<FloatPoint> vertices = verticesForBox(elementRect, anchor);
+    
+    // Rotate vertices depending on offset rotate or angle
+    if (!rotation.hasAuto()) {
+        auto deg = toRelatedAcuteAngle(toPositiveAngle(m_angle - rotation.angle()));
+        auto angle = deg2rad(deg);
+        std::for_each(vertices.begin(), vertices.end(), [angle] (FloatPoint& p) {
+            p.rotate(angle);
+        });
+    }
+    
+    Vector<std::pair<double, double>> bounds;
+    
+    for (const auto& p : vertices) {
+        // Use equation for circle (offset distance + x)^2 + y^2 <= r^2 to find offset distance that satisfies equation
+        // If no solution for above equation, must minimally increase it, otherwise clamp such that
+        // every point is within path
+        double discriminant = computedPathLength * computedPathLength - p.y() * p.y();
+        if (discriminant < 0) {
+            // Need to minimally increase path length
+            break;
+        }
+        bounds.append(std::make_pair(-p.x() - std::sqrt(discriminant), -p.x() + std::sqrt(discriminant)));
+    }
+    
+    if (vertices.size() == bounds.size()) {
+        auto lowerBound = std::max_element(bounds.begin(), bounds.end(),
+            [] (std::pair<double, double> const lhs, std::pair<double, double> const rhs) {
+            return lhs.first < rhs.first;
+        })->first;
+        
+        auto upperBound = std::min_element(bounds.begin(), bounds.end(),
+            [] (std::pair<double, double> const lhs, std::pair<double, double> const rhs) {
+            return lhs.second < rhs.second;
+        })->second;
+        
+        if (lowerBound <= upperBound)
+            return std::max(lowerBound, std::min(upperBound, computedPathLength));
+    }
+    
+    // TODO: Implement minimally increasing path length to allow all vertices to be within such a path length
+    return computedPathLength;
+}
+
+const Path RayPathOperation::pathForReferenceRect(const FloatRect& elementRect, const FloatPoint& anchor, const OffsetRotation rotation) const
 {
     Path path;
     if (m_containingBlockBoundingRect.isZero())
         return path;
-    auto length = lengthForPath();
-    auto radians = deg2rad(toPositiveAngle(m_angle) - 90);
+    double length = lengthForPath();
+    if (m_isContaining)
+        length = lengthForContainPath(elementRect, length, anchor, rotation);
+    auto radians = deg2rad(toPositiveAngle(m_angle) - 90.0);
     auto point = FloatPoint(std::cos(radians) * length, std::sin(radians) * length);
     path.addLineTo(point);
     return path;
