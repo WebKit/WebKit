2990d3d7b35b1ddab31f1be5c3fcb913ea57e2a9
Alan Coon <alancoon@apple.com>
2022-05-17 06:11:01 +0000

    Cherry-pick r293981. rdar://problem/92084291
        ASSERT in WebCore::RenderTreeUpdater::updateRenderTree
        https://bugs.webkit.org/show_bug.cgi?id=240237
    
        Reviewed by Antti Koivisto.
    
        There are instances where calling findRenderingRoots() in RenderTreeUpdater::commit will
        returns two rendering roots, with one of them being an ancestor of the other.
    
        Calling updateRenderTree on the ancestor rendering root could end up removing
        the renderer of the descendant rendering root.
    
        This patch  merges findRenderingRoots() to RenderTreeUpdater::commit() which, and now we will
        ignore the nested root if there is no longer a renderer when it is being processed.
    
        * rendering/updating/RenderTreeUpdater.cpp:
    
        Canonical link: https://commits.webkit.org/250419@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@293981 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/245886.578@safari-613-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-613-branch@294303 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/rendering/updating/RenderTreeUpdater.cpp
+++ Source/WebCore/rendering/updating/RenderTreeUpdater.cpp
@@ -100,18 +100,6 @@ static ContainerNode* findRenderingRoot(ContainerNode& node)
     return nullptr;
 }
 
-static ListHashSet<ContainerNode*> findRenderingRoots(const Style::Update& update)
-{
-    ListHashSet<ContainerNode*> renderingRoots;
-    for (auto& root : update.roots()) {
-        auto* renderingRoot = findRenderingRoot(*root);
-        if (!renderingRoot)
-            continue;
-        renderingRoots.add(renderingRoot);
-    }
-    return renderingRoots;
-}
-
 void RenderTreeUpdater::commit(std::unique_ptr<const Style::Update> styleUpdate)
 {
     ASSERT(&m_document == &styleUpdate->document());
@@ -123,8 +111,12 @@ void RenderTreeUpdater::commit(std::unique_ptr<const Style::Update> styleUpdate)
 
     m_styleUpdate = WTFMove(styleUpdate);
 
-    for (auto* root : findRenderingRoots(*m_styleUpdate))
-        updateRenderTree(*root);
+    for (auto& root : m_styleUpdate->roots()) {
+        auto* renderingRoot = findRenderingRoot(*root);
+        if (!renderingRoot)
+            continue;
+        updateRenderTree(*renderingRoot);
+    }
 
     generatedContent().updateRemainingQuotes();
 
