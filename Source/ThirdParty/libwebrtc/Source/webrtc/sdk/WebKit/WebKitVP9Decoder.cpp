33d7876c7a34b242dbecced3f4b726de43c4d4fc
Alan Coon <alancoon@apple.com>
2022-05-25 23:49:29 +0000

    Cherry-pick r293643. rdar://problem/92445366
        [Mac] VTVideoDecoderClass object pointers can become unaligned on x86
        https://bugs.webkit.org/show_bug.cgi?id=239916
        <rdar://92445366>
    
        Reviewed by Eric Carlson.
    
        Both the base class and the derived class must be 4-byte aligned on x86, or there
        is a chance that a pointer member of that struct will cross a page boundary, and
        dereferencing that pointer will fail.
    
        * Source/webrtc/sdk/WebKit/WebKitVP8Decoder.cpp:
        * Source/webrtc/sdk/WebKit/WebKitVP9Decoder.cpp:
    
        Canonical link: https://commits.webkit.org/250147@main
        git-svn-id: https://svn.webkit.org/repository/webkit/trunk@293643 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Canonical link: https://commits.webkit.org/245886.582@safari-613-branch
    git-svn-id: https://svn.webkit.org/repository/webkit/branches/safari-613-branch@294837 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/ThirdParty/libwebrtc/Source/webrtc/sdk/WebKit/WebKitVP9Decoder.cpp
+++ Source/ThirdParty/libwebrtc/Source/webrtc/sdk/WebKit/WebKitVP9Decoder.cpp
@@ -55,12 +55,12 @@ static CFStringRef copyVP9DecoderDebugDescription(CMBaseObjectRef);
 #endif
 
 #pragma pack(push, 4)
-struct DecoderClass {
+struct DecoderBaseClass {
     uint8_t pad[padSize];
     CMBaseClass alignedClass;
 };
 
-static const DecoderClass WebKitVP9Decoder_BaseClass {
+static const DecoderBaseClass WebKitVP9Decoder_BaseClass {
     { },
     {
         kCMBaseObject_ClassVersion_1,
@@ -82,31 +82,41 @@ static const DecoderClass WebKitVP9Decoder_BaseClass {
 #else
     static_assert(sizeof(WebKitVP9Decoder_BaseClass.alignedClass.version) == sizeof(uintptr_t), "CMBaseClass fixup is not required!");
 #endif
-static_assert(offsetof(DecoderClass, alignedClass) == padSize, "CMBaseClass offset is incorrect!");
-static_assert(alignof(DecoderClass) == 4, "CMBaseClass must have 4 byte alignment");
+static_assert(offsetof(DecoderBaseClass, alignedClass) == padSize, "CMBaseClass offset is incorrect!");
+static_assert(alignof(DecoderBaseClass) == 4, "CMBaseClass must have 4 byte alignment");
 
 static OSStatus startVP9DecoderSession(VTVideoDecoderRef, VTVideoDecoderSession, CMVideoFormatDescriptionRef);
 static OSStatus decodeVP9DecoderFrame(VTVideoDecoderRef, VTVideoDecoderFrame, CMSampleBufferRef, VTDecodeFrameFlags, VTDecodeInfoFlags*);
 
-static const VTVideoDecoderClass WebKitVP9Decoder_VideoDecoderClass =
-{
-    kVTVideoDecoder_ClassVersion_1,
-    startVP9DecoderSession,
-    decodeVP9DecoderFrame,
-    nullptr, // VTVideoDecoderFunction_CopySupportedPropertyDictionary,
-    nullptr, // VTVideoDecoderFunction_SetProperties
-    nullptr, // VTVideoDecoderFunction_CopySerializableProperties
-    nullptr, // VTVideoDecoderFunction_CanAcceptFormatDescription
-    nullptr, // VTVideoDecoderFunction_FinishDelayedFrames
-    nullptr, // VTVideoDecoderFunction_StartTileSession
-    nullptr, // VTVideoDecoderFunction_DecodeTile
-    nullptr // VTVideoDecoderFunction_FinishDelayedTiles
+#pragma pack(push, 4)
+struct DecoderClass {
+    uint8_t pad[padSize];
+    VTVideoDecoderClass alignedClass;
 };
+
+static const DecoderClass WebKitVP9Decoder_VideoDecoderClass =
+{
+    { },
+    {
+        kVTVideoDecoder_ClassVersion_1,
+        startVP9DecoderSession,
+        decodeVP9DecoderFrame,
+        nullptr, // VTVideoDecoderFunction_CopySupportedPropertyDictionary,
+        nullptr, // VTVideoDecoderFunction_SetProperties
+        nullptr, // VTVideoDecoderFunction_CopySerializableProperties
+        nullptr, // VTVideoDecoderFunction_CanAcceptFormatDescription
+        nullptr, // VTVideoDecoderFunction_FinishDelayedFrames
+        nullptr, // VTVideoDecoderFunction_StartTileSession
+        nullptr, // VTVideoDecoderFunction_DecodeTile
+        nullptr // VTVideoDecoderFunction_FinishDelayedTiles
+    }
+};
+#pragma pack(pop)
 
 static const VTVideoDecoderVTable WebKitVP9DecoderVTable =
 {
     { nullptr, &WebKitVP9Decoder_BaseClass.alignedClass },
-    &WebKitVP9Decoder_VideoDecoderClass
+    &WebKitVP9Decoder_VideoDecoderClass.alignedClass
 };
 
 OSStatus createWebKitVP9Decoder(FigVideoCodecType, CFAllocatorRef allocator, VTVideoDecoderRef* decoderOut)
