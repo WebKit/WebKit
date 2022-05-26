6bcebc24ec21fb6df605026a9ce0d7cb7ba68a17
Patrick Angle <pangle@apple.com>
2022-05-26 23:41:11 +0000

    Web Inspector: `inspector/debugger/breakpoints/resolved-dump-all-pause-locations.html` is a flakey failure https://bugs.webkit.org/show_bug.cgi?id=239134 rdar://91639437
    Reviewed by Devin Rousso.
    
    This test was flaky because the only piece of code in the test keeping the SourceProvider alive for `dump-multiline.js`
    was the `function test()`, which is replaced by the `function test()` in the test page itself. This meant that the
    function from dump-multiline.js would be garbage collected at some point, and since it was the last possible way to
    reach source code in `dump-multiline.js`, the SourceProvider was also disposed of at the same time.
    
    Normally this would not be an issue for users because the source code itself is still viewable in Web Inspector, and
    breakpoints can still be set (and will be triggered after a reload). The issue here is that when we attach Web Inspector
    the source code is longer reachable, so the backend doesn't send information for it to the frontend for it (until a page
    reload), but this test is assuming the script will have been sent from the backend to associate with the resource.
    
    * LayoutTests/inspector/debugger/breakpoints/resolved-dump-all-pause-locations-expected.txt:
    * LayoutTests/inspector/debugger/breakpoints/resources/dump-multiline.js:
    
    Canonical link: https://commits.webkit.org/251030@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@294907 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- LayoutTests/inspector/debugger/breakpoints/resources/dump-multiline.js
+++ LayoutTests/inspector/debugger/breakpoints/resources/dump-multiline.js
@@ -1,15 +1,15 @@
-function test() {
+function multilineTest() {
     var x;
 }
 
 // Strings
 let multiline1 = "test\
-string", multiline2 = test();
+string", multiline2 = multilineTest();
 
 // Template Strings
 let multiline3 = `test
-string`, multiline4 = test();
+string`, multiline4 = multilineTest();
 
 // Comments
 /* test
-comment */ let multiline5 = test();
+comment */ let multiline5 = multilineTest();
