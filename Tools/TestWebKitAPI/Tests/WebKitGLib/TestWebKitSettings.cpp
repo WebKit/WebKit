8ee445dee4a17a0c2f733f190a8d5a3828fb2a6f
Lauro Moura <lmoura@igalia.com>
2022-05-27 17:58:08 +0000

    [GTK] Deprecate WebKitSettings:enable-java https://bugs.webkit.org/show_bug.cgi?id=239538
    Reviewed by Adrian Perez de Castro.
    
    * Tools/TestWebKitAPI/Tests/WebKitGLib/TestWebKitSettings.cpp:
    (testWebKitSettings): Move test of deprecated function to section
    disabling deprecation warnings.
    
    Canonical link: https://commits.webkit.org/251052@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@294942 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/WebKitGLib/TestWebKitSettings.cpp
+++ Tools/TestWebKitAPI/Tests/WebKitGLib/TestWebKitSettings.cpp
@@ -77,14 +77,6 @@ static void testWebKitSettings(Test*, gconstpointer)
     webkit_settings_set_enable_frame_flattening(settings, TRUE);
     g_assert_true(webkit_settings_get_enable_frame_flattening(settings));
 
-    // Java is not supported, and always disabled.
-    // Make warnings non-fatal for this test to make it pass.
-    Test::removeLogFatalFlag(G_LOG_LEVEL_WARNING);
-    g_assert_false(webkit_settings_get_enable_java(settings));
-    webkit_settings_set_enable_java(settings, FALSE);
-    g_assert_false(webkit_settings_get_enable_java(settings));
-    Test::addLogFatalFlag(G_LOG_LEVEL_WARNING);
-
     // By default, JavaScript can open windows automatically is disabled.
     g_assert_false(webkit_settings_get_javascript_can_open_windows_automatically(settings));
     webkit_settings_set_javascript_can_open_windows_automatically(settings, TRUE);
@@ -369,6 +361,14 @@ ALLOW_DEPRECATED_DECLARATIONS_BEGIN
     g_assert_false(webkit_settings_get_enable_xss_auditor(settings));
     webkit_settings_set_enable_xss_auditor(settings, TRUE);
     g_assert_false(webkit_settings_get_enable_xss_auditor(settings));
+
+    // Java is not supported, and always disabled.
+    // Make warnings non-fatal for this test to make it pass.
+    Test::removeLogFatalFlag(G_LOG_LEVEL_WARNING);
+    g_assert_false(webkit_settings_get_enable_java(settings));
+    webkit_settings_set_enable_java(settings, FALSE);
+    g_assert_false(webkit_settings_get_enable_java(settings));
+    Test::addLogFatalFlag(G_LOG_LEVEL_WARNING);
 ALLOW_DEPRECATED_DECLARATIONS_END
 
     g_object_unref(G_OBJECT(settings));
