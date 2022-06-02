4a45c9a1f6efb438b4f0fdb45212add3fe168f63
Chris Dumez <cdumez@apple.com>
2022-06-02 01:24:18 +0000

    REGRESSION (250986@main): ASSERTION FAILED: isASCII(characters[I]) in WTF::ASCIILiteral WTF::StringLiterals::operator""_s() https://bugs.webkit.org/show_bug.cgi?id=241207 <rdar://94256662>
    Unreviewed, a few URL API tests were using ""_s with non-ASCII characters, which is
    not supported. Use StringView::fromLatin1() instead to restore pre-existing behavior.
    
    * Tools/TestWebKitAPI/Tests/WTF/URL.cpp:
    (TestWebKitAPI::TEST_F):
    * Tools/TestWebKitAPI/Tests/WTF/URLParser.cpp:
    (TestWebKitAPI::TEST_F):
    
    Canonical link: https://commits.webkit.org/251205@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@295114 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/WTF/URL.cpp
+++ Tools/TestWebKitAPI/Tests/WTF/URL.cpp
@@ -151,9 +151,9 @@ TEST_F(WTF_URL, URLSetQuery)
     URL urlWithFragmentIdentifier2 = createURL("http://www.webkit.org/?#newFragment"_s);
     URL urlWithFragmentIdentifier3 = createURL("http://www.webkit.org/?test1#newFragment"_s);
 
-    urlWithFragmentIdentifier1.setQuery("test\xc3\xa5"_s);
-    urlWithFragmentIdentifier2.setQuery("test\xc3\xa5"_s);
-    urlWithFragmentIdentifier3.setQuery("test\xc3\xa5"_s);
+    urlWithFragmentIdentifier1.setQuery(StringView::fromLatin1("test\xc3\xa5"));
+    urlWithFragmentIdentifier2.setQuery(StringView::fromLatin1("test\xc3\xa5"));
+    urlWithFragmentIdentifier3.setQuery(StringView::fromLatin1("test\xc3\xa5"));
 
     EXPECT_EQ(urlWithFragmentIdentifier.string(), urlWithFragmentIdentifier1.string());
     EXPECT_EQ(urlWithFragmentIdentifier.string(), urlWithFragmentIdentifier2.string());
@@ -167,9 +167,9 @@ TEST_F(WTF_URL, URLSetFragmentIdentifier)
     URL url2 = createURL("http://www.webkit.org/#test2"_s);
     URL url3 = createURL("http://www.webkit.org/#"_s);
 
-    url1.setFragmentIdentifier("newFragment\xc3\xa5"_s);
-    url2.setFragmentIdentifier("newFragment\xc3\xa5"_s);
-    url3.setFragmentIdentifier("newFragment\xc3\xa5"_s);
+    url1.setFragmentIdentifier(StringView::fromLatin1("newFragment\xc3\xa5"));
+    url2.setFragmentIdentifier(StringView::fromLatin1("newFragment\xc3\xa5"));
+    url3.setFragmentIdentifier(StringView::fromLatin1("newFragment\xc3\xa5"));
 
     EXPECT_EQ(url.string(), url1.string());
     EXPECT_EQ(url.string(), url2.string());
