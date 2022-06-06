3a3bf0f480d5622ed92d09665b4db0493c7b1605
Per Arne Vollan <pvollan@apple.com>
2022-06-06 15:22:21 +0000

    Eagerly soft link Data Detection frameworks https://bugs.webkit.org/show_bug.cgi?id=241267
    Reviewed by Geoffrey Garen.
    
    Eagerly soft link Data Detection frameworks in the WebContent process. There are two motivations behind this change.
    First, soft linking frameworks may lead to communication with launchd. Doing the soft linking before the WebContent
    process has finished launching, will help our effort in blocking launchd post launch. Second, the data detection
    frameworks are normally used in every WebContent process, and eagerly soft linking them should be a performance
    improvement. Ideally, these frameworks should be hard linked or weak linked. However, that would most likely create
    cycles in the build dependencies.
    
    * Source/WebCore/PAL/pal/cocoa/DataDetectorsCoreSoftLink.h:
    * Source/WebCore/PAL/pal/cocoa/DataDetectorsCoreSoftLink.mm:
    * Source/WebKit/WebProcess/cocoa/WebProcessCocoa.mm:
    (WebKit::softlinkDataDetectorsFrameworks):
    (WebKit::WebProcess::platformInitializeWebProcess):
    
    Canonical link: https://commits.webkit.org/251333@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@295287 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/PAL/pal/cocoa/DataDetectorsCoreSoftLink.mm
+++ Source/WebCore/PAL/pal/cocoa/DataDetectorsCoreSoftLink.mm
@@ -30,7 +30,7 @@
 #include <pal/spi/cocoa/DataDetectorsCoreSPI.h>
 #include <wtf/SoftLinking.h>
 
-SOFT_LINK_PRIVATE_FRAMEWORK_FOR_SOURCE(PAL, DataDetectorsCore)
+SOFT_LINK_PRIVATE_FRAMEWORK_FOR_SOURCE_WITH_EXPORT(PAL, DataDetectorsCore, PAL_EXPORT)
 SOFT_LINK_CLASS_FOR_SOURCE_WITH_EXPORT(PAL, DataDetectorsCore, DDScannerResult, PAL_EXPORT)
 
 #if PLATFORM(MAC)
