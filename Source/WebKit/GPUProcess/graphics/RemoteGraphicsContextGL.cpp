WebKit/WebKit

commit b6964b958eb95f8fa418f0416b97551f4a6b41e8

[GTK][WPE][GPUProcess] Add RemoteGraphicsContextGLGBM, RemoteGraphicsContextGLProxyGBM https://bugs.webkit.org/show_bug.cgi?id=239348
Reviewed by Carlos Garcia Campos.

Provide RemoteGraphicsContextGL and RemoteGraphicsContextGLProxy
implementations guarded by and based on USE(LIBGBM) functionality,
usable by the GTK and WPE ports when jointly enabled with ANGLE
backing of WebGL execution.

RemoteGraphicsContextGLGBM is the GPUProcess-side GL executor.
Internally it spawns a GraphicsContextGLGBM instance, and once invoked,
the PrepareForDisplay() IPC message returns the reply with the DMABuf
object that was used as the rendering backing in that context instance.

RemoteGraphicsContextGLProxyGBM is the WebProcess-side counterpart.
Besides relaying the prepare-for-display request, it also manages the
GraphicsLayerContentsDisplayDelegate object which handles presenting
DMABuf objects arriving from the GPUProcess. That delegate is of the
NicosiaDisplayDelegate type, a slim class also handling a
Nicosia::ContentLayer object through which any arriving DMABuf is
presented in the TextureMapper tree.

* GPUProcess/graphics/RemoteGraphicsContextGL.cpp:
* GPUProcess/graphics/RemoteGraphicsContextGL.h:
* GPUProcess/graphics/RemoteGraphicsContextGL.messages.in:
* GPUProcess/graphics/RemoteGraphicsContextGLGBM.cpp: Added.
(WebKit::RemoteGraphicsContextGLGBM::RemoteGraphicsContextGLGBM):
(WebKit::RemoteGraphicsContextGLGBM::platformWorkQueueInitialize):
(WebKit::RemoteGraphicsContextGLGBM::prepareForDisplay):
(WebKit::RemoteGraphicsContextGL::create):
* SourcesGTK.txt:
* SourcesWPE.txt:
* WebProcess/GPU/graphics/gbm/RemoteGraphicsContextGLProxyGBM.cpp: Added.
(WebKit::NicosiaDisplayDelegate::NicosiaDisplayDelegate):
(WebKit::NicosiaDisplayDelegate::~NicosiaDisplayDelegate):
(WebKit::NicosiaDisplayDelegate::platformLayer const):
(WebKit::NicosiaDisplayDelegate::swapBuffersIfNeeded):
(WebKit::RemoteGraphicsContextGLProxyGBM::RemoteGraphicsContextGLProxyGBM):

Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.cpp
@@ -53,7 +53,7 @@ IPC::StreamConnectionWorkQueue& remoteGraphicsContextGLStreamWorkQueue()
     return instance.get();
 }
 
-#if !PLATFORM(COCOA) && !PLATFORM(WIN)
+#if !PLATFORM(COCOA) && !PLATFORM(WIN) && !USE(LIBGBM)
 Ref<RemoteGraphicsContextGL> RemoteGraphicsContextGL::create(GPUConnectionToWebProcess& gpuConnectionToWebProcess, GraphicsContextGLAttributes&& attributes, GraphicsContextGLIdentifier graphicsContextGLIdentifier, RemoteRenderingBackend& renderingBackend, IPC::StreamConnectionBuffer&& stream)
 {
     ASSERT_NOT_REACHED();
@@ -162,7 +162,7 @@ void RemoteGraphicsContextGL::reshape(int32_t width, int32_t height)
         forceContextLost();
 }
 
-#if !PLATFORM(COCOA) && !PLATFORM(WIN)
+#if !PLATFORM(COCOA) && !PLATFORM(WIN) && !USE(LIBGBM)
 void RemoteGraphicsContextGL::prepareForDisplay(CompletionHandler<void()>&& completionHandler)
 {
     assertIsCurrent(workQueue());

Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.h
@@ -41,6 +41,8 @@
 
 #if PLATFORM(COCOA)
 #include <WebCore/GraphicsContextGLCocoa.h>
+#elif USE(LIBGBM)
+#include <WebCore/GraphicsContextGLGBM.h>
 #else
 #include <WebCore/GraphicsContextGLTextureMapperANGLE.h>
 #endif
@@ -111,6 +113,8 @@ protected:
     virtual void prepareForDisplay(CompletionHandler<void(WTF::MachSendRight&&)>&&) = 0;
 #elif USE(GRAPHICS_LAYER_WC)
     virtual void prepareForDisplay(CompletionHandler<void(std::optional<WCContentBufferIdentifier>)>&&) = 0;
+#elif USE(LIBGBM)
+    virtual void prepareForDisplay(CompletionHandler<void(WebCore::DMABufObject&&)>&&) = 0;
 #else
     void prepareForDisplay(CompletionHandler<void()>&&);
 #endif
@@ -140,6 +144,8 @@ protected:
     RefPtr<IPC::StreamServerConnection> m_streamConnection;
 #if PLATFORM(COCOA)
     using GCGLContext = WebCore::GraphicsContextGLCocoa;
+#elif USE(LIBGBM)
+    using GCGLContext = WebCore::GraphicsContextGLGBM;
 #else
     using GCGLContext = WebCore::GraphicsContextGLTextureMapperANGLE;
 #endif

Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.messages.in
@@ -32,7 +32,10 @@ messages -> RemoteGraphicsContextGL NotRefCounted Stream {
 #if USE(GRAPHICS_LAYER_WC)
     void PrepareForDisplay() -> (std::optional<WebKit::WCContentBufferIdentifier> contentBuffer) Synchronous
 #endif
-#if !PLATFORM(COCOA) && !USE(GRAPHICS_LAYER_WC)
+#if USE(LIBGBM)
+    void PrepareForDisplay() -> (WebCore::DMABufObject dmabufObject) Synchronous NotStreamEncodableReply
+#endif
+#if !PLATFORM(COCOA) && !USE(GRAPHICS_LAYER_WC) && !USE(LIBGBM)
     void PrepareForDisplay() -> () Synchronous
 #endif
     void EnsureExtensionEnabled(String extension)

Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGLGBM.cpp
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2022 Metrological Group B.V.
+ * Copyright (C) 2022 Igalia S.L.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "RemoteGraphicsContextGL.h"
+
+#if ENABLE(GPU_PROCESS) && ENABLE(WEBGL) && USE(LIBGBM)
+
+namespace WebKit {
+
+class RemoteGraphicsContextGLGBM final : public RemoteGraphicsContextGL {
+public:
+    RemoteGraphicsContextGLGBM(GPUConnectionToWebProcess&, GraphicsContextGLIdentifier, RemoteRenderingBackend&, IPC::StreamConnectionBuffer&&);
+
+private:
+    void platformWorkQueueInitialize(WebCore::GraphicsContextGLAttributes&&) final;
+    void prepareForDisplay(CompletionHandler<void(WebCore::DMABufObject&&)>&&) final;
+};
+
+RemoteGraphicsContextGLGBM::RemoteGraphicsContextGLGBM(GPUConnectionToWebProcess& connection, GraphicsContextGLIdentifier identifier, RemoteRenderingBackend& renderingBackend, IPC::StreamConnectionBuffer&& stream)
+    : RemoteGraphicsContextGL(connection, identifier, renderingBackend, WTFMove(stream))
+{ }
+
+void RemoteGraphicsContextGLGBM::platformWorkQueueInitialize(WebCore::GraphicsContextGLAttributes&& attributes)
+{
+    assertIsCurrent(workQueue());
+    m_context = WebCore::GraphicsContextGLGBM::create(WTFMove(attributes));
+}
+
+void RemoteGraphicsContextGLGBM::prepareForDisplay(CompletionHandler<void(WebCore::DMABufObject&&)>&& completionHandler)
+{
+    assertIsCurrent(workQueue());
+    m_context->prepareForDisplay();
+
+    WebCore::DMABufObject dmabufObject(0);
+    auto& swapchain = m_context->swapchain();
+    if (swapchain.displayBO) {
+        uintptr_t handle = reinterpret_cast<uintptr_t>(swapchain.swapchain.get()) + swapchain.displayBO->handle();
+        dmabufObject = swapchain.displayBO->createDMABufObject(handle);
+        swapchain.displayBO = nullptr;
+    }
+
+    completionHandler(WTFMove(dmabufObject));
+}
+
+Ref<RemoteGraphicsContextGL> RemoteGraphicsContextGL::create(GPUConnectionToWebProcess& connection, WebCore::GraphicsContextGLAttributes&& attributes, GraphicsContextGLIdentifier identifier, RemoteRenderingBackend& renderingBackend, IPC::StreamConnectionBuffer&& stream)
+{
+    auto instance = adoptRef(*new RemoteGraphicsContextGLGBM(connection, identifier, renderingBackend, WTFMove(stream)));
+    instance->initialize(WTFMove(attributes));
+    return instance;
+}
+
+} // namespace WebKit
+
+#endif // ENABLE(GPU_PROCESS) && ENABLE(WEBGL) && USE(LIBGBM)

Source/WebKit/WebProcess/GPU/graphics/gbm/RemoteGraphicsContextGLProxyGBM.cpp
@@ -1,0 +1,146 @@
+/*
+ * Copyright (C) 2022 Metrological Group B.V.
+ * Copyright (C) 2022 Igalia S.L.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "RemoteGraphicsContextGLProxy.h"
+
+#if ENABLE(GPU_PROCESS) && ENABLE(WEBGL) && USE(LIBGBM)
+
+// This is a standalone check of additional requirements in this implementation,
+// avoiding having to place an overwhelming amount of build guards over the rest of the code.
+#if !USE(NICOSIA) || !USE(TEXTURE_MAPPER_DMABUF)
+#error RemoteGraphicsContextGLProxyGBM implementation also depends on USE_NICOSIA and USE_TEXTURE_MAPPER_DMABUF
+#endif
+
+#include "WebProcess.h"
+#include <WebCore/DMABufObject.h>
+#include <WebCore/GraphicsLayerContentsDisplayDelegate.h>
+#include <WebCore/NicosiaContentLayerTextureMapperImpl.h>
+#include <WebCore/TextureMapperPlatformLayerProxyDMABuf.h>
+
+namespace WebKit {
+
+class NicosiaDisplayDelegate final : public WebCore::GraphicsLayerContentsDisplayDelegate, public Nicosia::ContentLayerTextureMapperImpl::Client {
+public:
+    explicit NicosiaDisplayDelegate(bool isOpaque);
+    virtual ~NicosiaDisplayDelegate();
+
+    void present(WebCore::DMABufObject&& dmabufObject)
+    {
+        m_pending = WTFMove(dmabufObject);
+    }
+
+private:
+    // WebCore::GraphicsLayerContentsDisplayDelegate
+    Nicosia::PlatformLayer* platformLayer() const final;
+
+    // Nicosia::ContentLayerTextureMapperImpl::Client
+    void swapBuffersIfNeeded() final;
+
+    bool m_isOpaque { false };
+    RefPtr<Nicosia::ContentLayer> m_contentLayer;
+    WebCore::DMABufObject m_pending { WebCore::DMABufObject(0) };
+};
+
+NicosiaDisplayDelegate::NicosiaDisplayDelegate(bool isOpaque)
+    : m_isOpaque(isOpaque)
+{
+    m_contentLayer = Nicosia::ContentLayer::create(Nicosia::ContentLayerTextureMapperImpl::createFactory(*this, adoptRef(*new WebCore::TextureMapperPlatformLayerProxyDMABuf)));
+}
+
+NicosiaDisplayDelegate::~NicosiaDisplayDelegate()
+{ }
+
+Nicosia::PlatformLayer* NicosiaDisplayDelegate::platformLayer() const
+{
+    return m_contentLayer.get();
+}
+
+void NicosiaDisplayDelegate::swapBuffersIfNeeded()
+{
+    auto& proxy = downcast<Nicosia::ContentLayerTextureMapperImpl>(m_contentLayer->impl()).proxy();
+    ASSERT(is<WebCore::TextureMapperPlatformLayerProxyDMABuf>(proxy));
+
+    if (!!m_pending.handle) {
+        Locker locker { proxy.lock() };
+
+        WebCore::TextureMapperGL::Flags flags = WebCore::TextureMapperGL::ShouldFlipTexture;
+        if (!m_isOpaque)
+            flags |= WebCore::TextureMapperGL::ShouldBlend;
+
+        downcast<WebCore::TextureMapperPlatformLayerProxyDMABuf>(proxy).pushDMABuf(WTFMove(m_pending),
+            [](auto&& object) { return object; }, flags);
+    }
+    m_pending = WebCore::DMABufObject(0);
+}
+
+class RemoteGraphicsContextGLProxyGBM final : public RemoteGraphicsContextGLProxy {
+public:
+    RemoteGraphicsContextGLProxyGBM(GPUProcessConnection&, const WebCore::GraphicsContextGLAttributes&, RenderingBackendIdentifier);
+    virtual ~RemoteGraphicsContextGLProxyGBM() = default;
+
+private:
+    // WebCore::GraphicsContextGL
+    RefPtr<WebCore::GraphicsLayerContentsDisplayDelegate> layerContentsDisplayDelegate() final;
+    void prepareForDisplay() final;
+
+    Ref<NicosiaDisplayDelegate> m_layerContentsDisplayDelegate;
+};
+
+RemoteGraphicsContextGLProxyGBM::RemoteGraphicsContextGLProxyGBM(GPUProcessConnection& processConnection, const WebCore::GraphicsContextGLAttributes& attributes, RenderingBackendIdentifier renderingBackend)
+    : RemoteGraphicsContextGLProxy(processConnection, attributes, renderingBackend)
+    , m_layerContentsDisplayDelegate(adoptRef(*new NicosiaDisplayDelegate(!attributes.alpha)))
+{ }
+
+RefPtr<WebCore::GraphicsLayerContentsDisplayDelegate> RemoteGraphicsContextGLProxyGBM::layerContentsDisplayDelegate()
+{
+    return m_layerContentsDisplayDelegate.copyRef();
+}
+
+void RemoteGraphicsContextGLProxyGBM::prepareForDisplay()
+{
+    if (isContextLost())
+        return;
+
+    WebCore::DMABufObject dmabufObject(0);
+    auto sendResult = sendSync(Messages::RemoteGraphicsContextGL::PrepareForDisplay(), Messages::RemoteGraphicsContextGL::PrepareForDisplay::Reply(dmabufObject));
+    if (!sendResult) {
+        markContextLost();
+        return;
+    }
+
+    m_layerContentsDisplayDelegate->present(WTFMove(dmabufObject));
+    markLayerComposited();
+}
+
+RefPtr<RemoteGraphicsContextGLProxy> RemoteGraphicsContextGLProxy::create(const WebCore::GraphicsContextGLAttributes& attributes, RenderingBackendIdentifier renderingBackend)
+{
+    return adoptRef(new RemoteGraphicsContextGLProxyGBM(WebProcess::singleton().ensureGPUProcessConnection(), attributes, renderingBackend));
+}
+
+} // namespace WebKit
+
+#endif // ENABLE(GPU_PROCESS) && ENABLE(WEBGL) && USE(LIBGBM)

commit 1a12dd2e181348187468e5bf93510b1028f161f3

RemoteGraphicsContextGL autogeneration broken after 249754@main, 251204@main, 251325@main https://bugs.webkit.org/show_bug.cgi?id=241328
Patch by Kimmo Kinnunen <kkinnunen@apple.com> on 2022-06-06
Unreviewed, build fix.

Verifieable by running Tools/Scripts/generate-gpup-webgl and
observing no change.

Make generate-gpup-webgl add // NOLINT for getIntegeri_v() to skip
warning about underscores in methods.

* Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.cpp:
(WebKit::RemoteGraphicsContextGL::paintRenderingResultsToPixelBuffer):
* Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.h:
* Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.messages.in:
* Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGLFunctionsGenerated.h:
(getInternalformativ):
(paintRenderingResultsToPixelBuffer): Deleted.
* Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.cpp:
* Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.h:
* Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxyFunctionsGenerated.cpp:
(WebKit::RemoteGraphicsContextGLProxy::paintRenderingResultsToPixelBuffer): Deleted.
* Tools/Scripts/generate-gpup-webgl:

Canonical link: https://commits.webkit.org/251330@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@295284 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.cpp
@@ -363,6 +363,15 @@ void RemoteGraphicsContextGL::multiDrawElementsInstancedANGLE(uint32_t mode, IPC
     m_context->multiDrawElementsInstancedANGLE(mode, GCGLSpanTuple { counts.data(), offsets, instanceCounts.data(), counts.size() }, type);
 }
 
+void RemoteGraphicsContextGL::paintRenderingResultsToPixelBuffer(CompletionHandler<void(std::optional<IPC::PixelBufferReference>&&)>&& completionHandler)
+{
+    std::optional<IPC::PixelBufferReference> returnValue;
+    assertIsCurrent(workQueue());
+    if (auto pixelBuffer = m_context->paintRenderingResultsToPixelBuffer())
+        returnValue = pixelBuffer.releaseNonNull();
+    completionHandler(WTFMove(returnValue));
+}
+
 } // namespace WebKit
 
 #endif

Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.h
@@ -136,6 +136,7 @@ protected:
     void multiDrawArraysInstancedANGLE(uint32_t mode, IPC::ArrayReferenceTuple<int32_t, int32_t, int32_t>&& firstsCountsAndInstanceCounts);
     void multiDrawElementsANGLE(uint32_t mode, IPC::ArrayReferenceTuple<int32_t, int32_t>&& countsAndOffsets, uint32_t type);
     void multiDrawElementsInstancedANGLE(uint32_t mode, IPC::ArrayReferenceTuple<int32_t, int32_t, int32_t>&& countsOffsetsAndInstanceCounts, uint32_t type);
+    void paintRenderingResultsToPixelBuffer(CompletionHandler<void(std::optional<IPC::PixelBufferReference>&&)>&&);
 
 #include "RemoteGraphicsContextGLFunctionsGenerated.h" // NOLINT
 

Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.messages.in
@@ -57,6 +57,7 @@ messages -> RemoteGraphicsContextGL NotRefCounted Stream {
     void MultiDrawArraysInstancedANGLE(uint32_t mode, IPC::ArrayReferenceTuple<int32_t, int32_t, int32_t> firstsCountsAandInstanceCounts)
     void MultiDrawElementsANGLE(uint32_t mode, IPC::ArrayReferenceTuple<int32_t, int32_t> countsAndOffsets, uint32_t type)
     void MultiDrawElementsInstancedANGLE(uint32_t mode, IPC::ArrayReferenceTuple<int32_t, int32_t, int32_t> countsOffsetsAndInstanceCounts, uint32_t type)
+    void PaintRenderingResultsToPixelBuffer() -> (std::optional<IPC::PixelBufferReference> returnValue) Synchronous
 
     void MoveErrorsToSyntheticErrorList() -> (bool returnValue) Synchronous
     void ActiveTexture(uint32_t texture)
@@ -302,7 +303,6 @@ messages -> RemoteGraphicsContextGL NotRefCounted Stream {
     void BlendFuncSeparateiOES(uint32_t buf, uint32_t srcRGB, uint32_t dstRGB, uint32_t srcAlpha, uint32_t dstAlpha)
     void ColorMaskiOES(uint32_t buf, bool red, bool green, bool blue, bool alpha)
     void GetInternalformativ(uint32_t target, uint32_t internalformat, uint32_t pname, uint64_t paramsSize) -> (IPC::ArrayReference<int32_t> params) Synchronous
-    void PaintRenderingResultsToPixelBuffer() -> (std::optional<IPC::PixelBufferReference> returnValue) Synchronous
 }
 
 #endif

Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGLFunctionsGenerated.h
@@ -1382,12 +1382,4 @@
         m_context->getInternalformativ(target, internalformat, pname, params);
         completionHandler(IPC::ArrayReference<int32_t>(reinterpret_cast<int32_t*>(params.data()), params.size()));
     }
-    void paintRenderingResultsToPixelBuffer(CompletionHandler<void(std::optional<IPC::PixelBufferReference>&&)>&& completionHandler)
-    {
-        std::optional<IPC::PixelBufferReference> returnValue = { };
-        assertIsCurrent(workQueue());
-        if (auto pixelBuffer = m_context->paintRenderingResultsToPixelBuffer())
-            returnValue = pixelBuffer.releaseNonNull();
-        completionHandler(WTFMove(returnValue));
-    }
 

Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.cpp
@@ -317,6 +317,19 @@ void RemoteGraphicsContextGLProxy::multiDrawElementsInstancedANGLE(GCGLenum mode
     }
 }
 
+RefPtr<WebCore::PixelBuffer> RemoteGraphicsContextGLProxy::paintRenderingResultsToPixelBuffer()
+{
+    std::optional<IPC::PixelBufferReference> returnValue;
+    if (!isContextLost()) {
+        auto sendResult = sendSync(Messages::RemoteGraphicsContextGL::PaintRenderingResultsToPixelBuffer(), Messages::RemoteGraphicsContextGL::PaintRenderingResultsToPixelBuffer::Reply(returnValue));
+        if (!sendResult)
+            markContextLost();
+    }
+    if (!returnValue)
+        return nullptr;
+    return returnValue->takePixelBuffer();
+}
+
 void RemoteGraphicsContextGLProxy::wasCreated(bool didSucceed, IPC::Semaphore&& wakeUpSemaphore, IPC::Semaphore&& clientWaitSemaphore, String&& availableExtensions, String&& requestedExtensions)
 {
     if (isContextLost())

Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.h
@@ -83,6 +83,7 @@ public:
     void multiDrawArraysInstancedANGLE(GCGLenum mode, GCGLSpanTuple<const GCGLint, const GCGLsizei, const GCGLsizei> firstsCountsAndInstanceCounts) final;
     void multiDrawElementsANGLE(GCGLenum mode, GCGLSpanTuple<const GCGLsizei, const GCGLint> countsAndOffsets, GCGLenum type) final;
     void multiDrawElementsInstancedANGLE(GCGLenum mode, GCGLSpanTuple<const GCGLsizei, const GCGLint, const GCGLsizei> countsOffsetsAndInstanceCounts, GCGLenum type) final;
+    RefPtr<WebCore::PixelBuffer> paintRenderingResultsToPixelBuffer() final;
 
     // Functions with a generated implementation. This list is used by generate-gpup-webgl script.
     bool moveErrorsToSyntheticErrorList() final;
@@ -329,7 +330,6 @@ public:
     void blendFuncSeparateiOES(GCGLuint buf, GCGLenum srcRGB, GCGLenum dstRGB, GCGLenum srcAlpha, GCGLenum dstAlpha) final;
     void colorMaskiOES(GCGLuint buf, GCGLboolean red, GCGLboolean green, GCGLboolean blue, GCGLboolean alpha) final;
     void getInternalformativ(GCGLenum target, GCGLenum internalformat, GCGLenum pname, GCGLSpan<GCGLint> params) final;
-    RefPtr<WebCore::PixelBuffer> paintRenderingResultsToPixelBuffer() final;
     // End of list used by generate-gpup-webgl script.
 
     static bool handleMessageToRemovedDestination(IPC::Connection&, IPC::Decoder&);

Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxyFunctionsGenerated.cpp
@@ -2375,19 +2375,6 @@ void RemoteGraphicsContextGLProxy::getInternalformativ(GCGLenum target, GCGLenum
     }
 }
 
-RefPtr<WebCore::PixelBuffer> RemoteGraphicsContextGLProxy::paintRenderingResultsToPixelBuffer()
-{
-    std::optional<IPC::PixelBufferReference> returnValue;
-    if (!isContextLost()) {
-        auto sendResult = sendSync(Messages::RemoteGraphicsContextGL::PaintRenderingResultsToPixelBuffer(), Messages::RemoteGraphicsContextGL::PaintRenderingResultsToPixelBuffer::Reply(returnValue));
-        if (!sendResult)
-            markContextLost();
-    }
-    if (!returnValue)
-        return nullptr;
-    return returnValue->takePixelBuffer();
-}
-
 }
 
 #endif

Tools/Scripts/generate-gpup-webgl
@@ -96,7 +96,10 @@ messages -> RemoteGraphicsContextGL NotRefCounted Stream {{
 #if USE(GRAPHICS_LAYER_WC)
     void PrepareForDisplay() -> (std::optional<WebKit::WCContentBufferIdentifier> contentBuffer) Synchronous
 #endif
-#if !PLATFORM(COCOA) && !USE(GRAPHICS_LAYER_WC)
+#if USE(LIBGBM)
+    void PrepareForDisplay() -> (WebCore::DMABufObject dmabufObject) Synchronous NotStreamEncodableReply
+#endif
+#if !PLATFORM(COCOA) && !USE(GRAPHICS_LAYER_WC) && !USE(LIBGBM)
     void PrepareForDisplay() -> () Synchronous
 #endif
     void EnsureExtensionEnabled(String extension)
@@ -118,6 +121,7 @@ messages -> RemoteGraphicsContextGL NotRefCounted Stream {{
     void MultiDrawArraysInstancedANGLE(uint32_t mode, IPC::ArrayReferenceTuple<int32_t, int32_t, int32_t> firstsCountsAandInstanceCounts)
     void MultiDrawElementsANGLE(uint32_t mode, IPC::ArrayReferenceTuple<int32_t, int32_t> countsAndOffsets, uint32_t type)
     void MultiDrawElementsInstancedANGLE(uint32_t mode, IPC::ArrayReferenceTuple<int32_t, int32_t, int32_t> countsOffsetsAndInstanceCounts, uint32_t type)
+    void PaintRenderingResultsToPixelBuffer() -> (std::optional<IPC::PixelBufferReference> returnValue) Synchronous
 {}
 }}
 
@@ -825,8 +829,9 @@ class webkit_ipc_cpp_impl(object):
         self.return_value_expr = return_value_expr
 
     def __str__(self):
+        nolint = " // NOLINT" if "_" in self.name else ""
         body = "".join(f"\n        {b}" for b in self.pre_call_stmts + self.call_stmts + self.post_call_stmts)
-        return f"""    void {self.name}{self.overload_suffix}({str(self.args)})
+        return f"""    void {self.name}{self.overload_suffix}({str(self.args)}){nolint}
     {{{body}
     }}"""
 