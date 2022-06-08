215d7cd4f08afd9b35eb3a4e570282c36b279988
Ross Kirsling <ross.kirsling@sony.com>
2022-06-08 17:37:26 +0000

    Undo temporary libpas fix for PlayStation https://bugs.webkit.org/show_bug.cgi?id=241425
    Reviewed by Don Olmstead.
    
    * Source/bmalloc/libpas/src/libpas/pas_random.h:
    (pas_get_secure_random):
    Remove ifdef, as this has now been addressed downstream.
    
    Canonical link: https://commits.webkit.org/251393@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@295386 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/bmalloc/libpas/src/libpas/pas_random.h
+++ Source/bmalloc/libpas/src/libpas/pas_random.h
@@ -69,7 +69,7 @@ static inline unsigned pas_get_secure_random(unsigned upper_bound)
 
     /* Secure random is only supported on Darwin and FreeBSD at the moment due to arc4random being built into the
       stdlib. Fall back to fast behavior on other operating systems. */
-#if PAS_OS(DARWIN) || (PAS_OS(FREEBSD) && !PAS_PLATFORM(PLAYSTATION))
+#if PAS_OS(DARWIN) || PAS_OS(FREEBSD)
     rand_value = arc4random_uniform(upper_bound);
 #else
     pas_fast_random_state = pas_xorshift32(pas_fast_random_state);
