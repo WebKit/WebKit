004159faa6b11205b60479993aeb6eff84c6d7bd
Mike Wyrzykowski <mwyrzykowski@apple.com>
2022-06-08 00:26:51 +0000

    Datetime-local Field Picker Shows Incorrect Date when seconds is zero https://bugs.webkit.org/show_bug.cgi?id=239778
    Patch by Mike Wyrzykowski <mwyrzykowski@apple.com> on 2022-06-07
    Reviewed by Aditya Keerthi.
    
    In r286869, a change was made to WebCore::DateComponents::toStringForTime as
    noted in the changelog:
    
       The output will use the shortest possible string, omitting seconds or milliseconds when 0, as per
        https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-normalised-local-date-and-time-string
    
    however that broke WebDateTimePicker when seconds or milliseconds is specified yet they are zero.
    
    Since the change to WebCore::DateComponents is to spec, change WebDateTimePicker to handle this behavior
    change instead.
    
    * Source/WebKit/UIProcess/mac/WebDateTimePickerMac.mm:
    (-[WKDateTimePicker updatePicker:]):
    -[NSDataFormatter dateFromString:] will return nil if the format
    requires seconds but the seconds is omitted, so try fallback formats
    without milliseconds or seconds.
    
    (-[WKDateTimePicker dateFormatFallbackForFormat:]):
    Fallback from milliseconds -> seconds -> time without seconds.
    
    Canonical link: https://commits.webkit.org/251377@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@295369 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/UIProcess/mac/WebDateTimePickerMac.mm
+++ Source/WebKit/UIProcess/mac/WebDateTimePickerMac.mm
@@ -240,12 +240,21 @@ void WebDateTimePickerMac::didChooseDate(StringView date)
 
     NSString *currentDateValueString = _params.currentValue;
 
-    [_dateFormatter setDateFormat:[self dateFormatStringForType:_params.type]];
+    NSString *format = [self dateFormatStringForType:_params.type];
+    [_dateFormatter setDateFormat:format];
 
     if (![currentDateValueString length])
         [_datePicker setDateValue:[self initialDateForEmptyValue]];
-    else
-        [_datePicker setDateValue:[_dateFormatter dateFromString:currentDateValueString]];
+    else {
+        NSDate *dateValue = [_dateFormatter dateFromString:currentDateValueString];
+
+        while (!dateValue && (format = [self dateFormatFallbackForFormat:format])) {
+            [_dateFormatter setDateFormat:format];
+            dateValue = [_dateFormatter dateFromString:currentDateValueString];
+        }
+
+        [_datePicker setDateValue:dateValue];
+    }
 
     [_datePicker setMinDate:[NSDate dateWithTimeIntervalSince1970:_params.minimum / 1000.0]];
     [_datePicker setMaxDate:[NSDate dateWithTimeIntervalSince1970:_params.maximum / 1000.0]];
@@ -287,6 +296,16 @@ void WebDateTimePickerMac::didChooseDate(StringView date)
     }
 
     return kDateFormatString;
+}
+
+- (NSString *)dateFormatFallbackForFormat:(NSString *)format
+{
+    if ([format isEqualToString:kDateTimeWithMillisecondsFormatString])
+        return kDateTimeWithSecondsFormatString;
+    if ([format isEqualToString:kDateTimeWithSecondsFormatString])
+        return kDateTimeFormatString;
+
+    return nil;
 }
 
 - (NSDate *)initialDateForEmptyValue
