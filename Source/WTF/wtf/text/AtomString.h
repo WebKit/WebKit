573079ccb3580ce076ea5c681d2c1760808d2723
Diego Pino Garcia <dpino@igalia.com>
2022-06-09 14:08:40 +0000

    [GTK] Ubuntu LTS bot build failed after 251271@main https://bugs.webkit.org/show_bug.cgi?id=241327
    Reviewed by Darin Adler.
    
    * Source/WTF/wtf/text/AtomString.h: Implement missing operator!=(const
      AtomString& a, ASCIILiteral b).
    
    Canonical link: https://commits.webkit.org/251425@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@295419 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WTF/wtf/text/AtomString.h
+++ Source/WTF/wtf/text/AtomString.h
@@ -171,6 +171,7 @@ inline bool operator==(const String& a, const AtomString& b) { return equal(a.im
 inline bool operator==(const Vector<UChar>& a, const AtomString& b) { return b == a; }
 
 inline bool operator!=(const AtomString& a, const AtomString& b) { return a.impl() != b.impl(); }
+inline bool operator!=(const AtomString& a, ASCIILiteral b) { return !(a == b); }
 inline bool operator!=(const AtomString& a, const String& b) { return !equal(a.impl(), b.impl()); }
 inline bool operator!=(const AtomString& a, const Vector<UChar>& b) { return !(a == b); }
 inline bool operator!=(ASCIILiteral a, const AtomString& b) { return !(b == a); }
