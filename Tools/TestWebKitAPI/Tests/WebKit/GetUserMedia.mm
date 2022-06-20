1c31350c12dca88f73164c8a1c92a891fb7bccfe
Youenn Fablet <youennf@gmail.com>
2022-06-20 13:04:59 +0000

    [ iOS ] TestWebKitAPI.WebKit2.CrashGPUProcessWhileCapturing is a flaky timeout https://bugs.webkit.org/show_bug.cgi?id=239315
    Patch by Youenn Fablet <youennf@gmail.com> on 2022-06-20
    Unreviewed.
    
    Reenable tests to gather EWS results.
    We should at least reenable it in macOS if it still does not run well in iOS.
    
    * Tools/TestWebKitAPI/Tests/WebKit/GetUserMedia.mm:
    
    Canonical link: https://commits.webkit.org/251679@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@295674 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/TestWebKitAPI/Tests/WebKit/GetUserMedia.mm
+++ Tools/TestWebKitAPI/Tests/WebKit/GetUserMedia.mm
@@ -561,7 +561,7 @@ TEST(WebKit, WebAudioAndGetUserMedia)
 
 #if ENABLE(GPU_PROCESS)
 // FIXME: https://bugs.webkit.org/show_bug.cgi?id=239315
-TEST(WebKit2, DISABLED_CrashGPUProcessWhileCapturing)
+TEST(WebKit2, CrashGPUProcessWhileCapturing)
 {
     auto configuration = adoptNS([[WKWebViewConfiguration alloc] init]);
     auto preferences = [configuration preferences];
@@ -709,7 +709,7 @@ TEST(WebKit2, CrashGPUProcessAfterApplyingConstraints)
 }
 
 // FIXME: https://bugs.webkit.org/show_bug.cgi?id=239309
-TEST(WebKit2, DISABLED_CrashGPUProcessWhileCapturingAndCalling)
+TEST(WebKit2, CrashGPUProcessWhileCapturingAndCalling)
 {
     auto configuration = adoptNS([[WKWebViewConfiguration alloc] init]);
     auto preferences = [configuration preferences];
