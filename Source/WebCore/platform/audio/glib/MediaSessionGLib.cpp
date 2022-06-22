WebKit/WebKit

commit d305d6f6fb79ea3ff3091f2362bbf29b3e92d109

[GLIB] Change MPRIS bus name to be sandbox friendly https://bugs.webkit.org/show_bug.cgi?id=241807
Reviewed by Philippe Normand.

When an application has an ID this changes the name to be
org.mpris.MediaPlayer2.$APP_ID.$INSTANCE_ID which is permitted
by default with flatpak and now with our bubblewrap sandbox.

Instead of the previous org.mpris.MediaPlayer2.$APP_ID-$INSTANCE_ID
which would be blocked by sandboxes unless they allowed an
overly broad org.mpris.MediaPlayer2.* permission.

* Source/WebCore/platform/audio/glib/MediaSessionGLib.cpp:
(WebCore::MediaSessionGLib::MediaSessionGLib):

Canonical link: https://commits.webkit.org/251730@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@295725 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/audio/glib/MediaSessionGLib.cpp
@@ -222,7 +222,7 @@ MediaSessionGLib::MediaSessionGLib(MediaSessionManagerGLib& manager, GRefPtr<GDB
     }
 
     const auto& applicationID = getApplicationID();
-    m_instanceId = applicationID.isEmpty() ? makeString("org.mpris.MediaPlayer2.webkit.instance", getpid(), "-", identifier.toUInt64()) : makeString("org.mpris.MediaPlayer2.", applicationID.ascii().data(), "-", identifier.toUInt64());
+    m_instanceId = applicationID.isEmpty() ? makeString("org.mpris.MediaPlayer2.webkit.instance", getpid(), "-", identifier.toUInt64()) : makeString("org.mpris.MediaPlayer2.", applicationID.ascii().data(), ".", identifier.toUInt64());
 
     m_ownerId = g_bus_own_name_on_connection(m_connection.get(), m_instanceId.ascii().data(), G_BUS_NAME_OWNER_FLAGS_NONE, nullptr, nullptr, this, nullptr);
 }

Source/WebKit/UIProcess/Launcher/glib/XDGDBusProxy.cpp
@@ -145,7 +145,7 @@ int XDGDBusProxy::launch(bool allowPortals) const
 #if ENABLE(MEDIA_SESSION)
         if (auto* app = g_application_get_default()) {
             if (const char* appID = g_application_get_application_id(app)) {
-                auto mprisSessionID = makeString("--own=org.mpris.MediaPlayer2.", appID);
+                auto mprisSessionID = makeString("--own=org.mpris.MediaPlayer2.", appID, ".*");
                 proxyArgs.append(mprisSessionID.ascii().data());
             }
         }

commit 262ef2f34e256bc97dce7a1d5df3b82692abb677

[GLIB] Fix invalid D-Bus name for MPRIS https://bugs.webkit.org/show_bug.cgi?id=241807
Reviewed by Philippe Normand.

A mistake introduced in #1651 used a bus name where a segment started with
a number which is not allowed. Change this to `.instance-$INSTANCE_ID` to fix that.

* Source/WebCore/platform/audio/glib/MediaSessionGLib.cpp:
(WebCore::MediaSessionGLib::MediaSessionGLib):

Canonical link: https://commits.webkit.org/251744@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@295739 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/audio/glib/MediaSessionGLib.cpp
@@ -222,7 +222,7 @@ MediaSessionGLib::MediaSessionGLib(MediaSessionManagerGLib& manager, GRefPtr<GDB
     }
 
     const auto& applicationID = getApplicationID();
-    m_instanceId = applicationID.isEmpty() ? makeString("org.mpris.MediaPlayer2.webkit.instance", getpid(), "-", identifier.toUInt64()) : makeString("org.mpris.MediaPlayer2.", applicationID.ascii().data(), ".", identifier.toUInt64());
+    m_instanceId = applicationID.isEmpty() ? makeString("org.mpris.MediaPlayer2.webkit.instance", getpid(), "-", identifier.toUInt64()) : makeString("org.mpris.MediaPlayer2.", applicationID.ascii().data(), ".instance-", identifier.toUInt64());
 
     m_ownerId = g_bus_own_name_on_connection(m_connection.get(), m_instanceId.ascii().data(), G_BUS_NAME_OWNER_FLAGS_NONE, nullptr, nullptr, this, nullptr);
 }
262ef2f34e256bc97dce7a1d5df3b82692abb677
Patrick Griffis <pgriffis@igalia.com>
2022-06-22 18:12:01 +0000

    [GLIB] Fix invalid D-Bus name for MPRIS https://bugs.webkit.org/show_bug.cgi?id=241807
    Reviewed by Philippe Normand.
    
    A mistake introduced in #1651 used a bus name where a segment started with
    a number which is not allowed. Change this to `.instance-$INSTANCE_ID` to fix that.
    
    * Source/WebCore/platform/audio/glib/MediaSessionGLib.cpp:
    (WebCore::MediaSessionGLib::MediaSessionGLib):
    
    Canonical link: https://commits.webkit.org/251744@main
    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@295739 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/audio/glib/MediaSessionGLib.cpp
+++ Source/WebCore/platform/audio/glib/MediaSessionGLib.cpp
@@ -222,7 +222,7 @@ MediaSessionGLib::MediaSessionGLib(MediaSessionManagerGLib& manager, GRefPtr<GDB
     }
 
     const auto& applicationID = getApplicationID();
-    m_instanceId = applicationID.isEmpty() ? makeString("org.mpris.MediaPlayer2.webkit.instance", getpid(), "-", identifier.toUInt64()) : makeString("org.mpris.MediaPlayer2.", applicationID.ascii().data(), ".", identifier.toUInt64());
+    m_instanceId = applicationID.isEmpty() ? makeString("org.mpris.MediaPlayer2.webkit.instance", getpid(), "-", identifier.toUInt64()) : makeString("org.mpris.MediaPlayer2.", applicationID.ascii().data(), ".instance-", identifier.toUInt64());
 
     m_ownerId = g_bus_own_name_on_connection(m_connection.get(), m_instanceId.ascii().data(), G_BUS_NAME_OWNER_FLAGS_NONE, nullptr, nullptr, this, nullptr);
 }
