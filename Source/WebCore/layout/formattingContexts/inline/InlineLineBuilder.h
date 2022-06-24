0f17d96c73ab445b317aa10e55a1727903b2f202
Alan Coon <alancoon@apple.com>
2022-06-24 13:00:07 -0700

    Cherry-pick 0360d950b32f. rdar://problem/92129881
    This reverts commit 60e57aea84e5dc3b59e5bdf4e04a0b6d1e544c42.
    
    Canonical link: https://commits.webkit.org/245886.765@safari-7613.3.9.1-branch

--- Source/WebCore/layout/formattingContexts/inline/InlineLineBuilder.h
+++ Source/WebCore/layout/formattingContexts/inline/InlineLineBuilder.h
@@ -50,6 +50,14 @@ public:
         size_t start { 0 };
         size_t end { 0 };
     };
+    struct PreviousLine {
+        InlineItemRange range;
+        struct OverflowContent {
+            size_t partialContentLength { 0 };
+            std::optional<InlineLayoutUnit> width { };
+        };
+        std::optional<OverflowContent> overflowContent { };
+    };
     using FloatList = Vector<const Box*>;
     struct LineContent {
         InlineItemRange inlineItemRange;
@@ -68,14 +76,14 @@ public:
         Vector<int32_t> visualOrderList;
         const Line::RunList& runs;
     };
-    LineContent layoutInlineContent(const InlineItemRange&, size_t partialLeadingContentLength, std::optional<InlineLayoutUnit> overflowingLogicalWidth, const InlineRect& initialLineLogicalRect, bool isFirstLine);
+    LineContent layoutInlineContent(const InlineItemRange&, const InlineRect& lineLogicalRect, const std::optional<PreviousLine>&);
 
     struct IntrinsicContent {
         InlineItemRange inlineItemRange;
         InlineLayoutUnit logicalWidth { 0 };
         const FloatList& floats;
     };
-    IntrinsicContent computedIntrinsicWidth(const InlineItemRange&, bool isFirstLine);
+    IntrinsicContent computedIntrinsicWidth(const InlineItemRange&, const std::optional<PreviousLine>&);
 
 private:
     void candidateContentForLine(LineCandidate&, size_t inlineItemIndex, const InlineItemRange& needsLayoutRange, InlineLayoutUnit currentLogicalRight);
@@ -99,7 +107,7 @@ private:
     size_t rebuildLine(const InlineItemRange& needsLayoutRange, const InlineItem& lastInlineItemToAdd);
     size_t rebuildLineForTrailingSoftHyphen(const InlineItemRange& layoutRange);
     void commitPartialContent(const InlineContentBreaker::ContinuousContent::RunList&, const InlineContentBreaker::Result::PartialTrailingContent&);
-    void initialize(const UsedConstraints&, bool isFirstLine, size_t leadingInlineTextItemIndex, size_t partialLeadingContentLength, std::optional<InlineLayoutUnit> overflowingLogicalWidth);
+    void initialize(const UsedConstraints&, size_t leadingInlineTextItemIndex, const std::optional<PreviousLine>&);
     struct CommittedContent {
         size_t inlineItemCount { 0 };
         size_t partialTrailingContentLength { 0 };
