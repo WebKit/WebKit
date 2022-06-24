6f684fff369c96182fa1943ea61ccba9ae5ca2b0
Alan Coon <alancoon@apple.com>
2022-06-23 17:29:52 -0700

    Revert b308d25de831. rdar://problem/95726903
    This reverts commit 5a1b2c06c0895910c15200886ecf3d0d24e604e4.
    
    Canonical link: https://commits.webkit.org/245886.718@safari-7613.3.6.1-branch

--- Source/WebCore/editing/ReplaceSelectionCommand.cpp
+++ Source/WebCore/editing/ReplaceSelectionCommand.cpp
@@ -237,7 +237,7 @@ void ReplacementFragment::removeContentsWithSideEffects()
         }
         if (element->hasAttributes()) {
             for (auto& attribute : element->attributesIterator()) {
-                if (element->isEventHandlerAttribute(attribute) || element->attributeContainsJavaScriptURL(attribute))
+                if (element->isEventHandlerAttribute(attribute) || element->attributeContainsJavascriptURL(attribute))
                     attributesToRemove.append({ element.copyRef(), attribute.name() });
             }
         }
