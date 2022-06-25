ace70fde247b6555a298fd28711d2c01e00dbc2c
Yusuke Suzuki <ysuzuki@apple.com>
2022-06-25 11:47:42 -0700

    Cherry-pick 27d302ab3481. rdar://problem/94859656
        [JSC] Add more debug information for wasm LLIntGenerator
        https://bugs.webkit.org/show_bug.cgi?id=242004
    
        Reviewed by Mark Lam.
    
        This patch adds more debug information for wasm LLIntGenerator so that it makes wasm debugging easy.
    
        * JSTests/wasm/stress/exception-simple-delegate.js:
        (import.Builder.from.string_appeared_here.import.as.assert.from.string_appeared_here.testSimpleThrowDelegate):
        (testSimpleThrowDelegateIllegal):
        (testThrowDelegateSkip): Deleted.
        (testDelegateCaller): Deleted.
        (testSimpleDelegateMerge): Deleted.
        * Source/JavaScriptCore/wasm/WasmFunctionParser.h:
        (JSC::Wasm::FunctionParser<Context>::parseUnreachableExpression):
        * Source/JavaScriptCore/wasm/WasmLLIntGenerator.cpp:
        (JSC::Wasm::LLIntGenerator::ControlType::dump const):
        (JSC::Wasm::dumpExpressionStack):
        (JSC::Wasm::LLIntGenerator::dump):
    
        Canonical link: https://commits.webkit.org/251852@main
    
    Canonical link: https://commits.webkit.org/245886.765@safari-7613.3.9.3-branch

--- JSTests/wasm/stress/exception-simple-delegate.js
+++ JSTests/wasm/stress/exception-simple-delegate.js
@@ -15,6 +15,7 @@ function testSimpleThrowDelegate() {
                     .Try("void")
                         .Throw(0)
                     .Delegate(0)
+                    .I32Const(42)
                 .Catch(0)
                     .I32Const(2)
                 .End()
@@ -29,7 +30,67 @@ function testSimpleThrowDelegate() {
     assert.eq(instance.exports.call(), 2, "catching an exported wasm tag thrown from JS should be possible");
 }
 
+function testSimpleThrowDelegateIllegal() {
+    const b = new Builder();
+    b.Type().End()
+        .Function().End()
+        .Exception().Signature({ params: []}).End()
+        .Export().Function("call")
+            .Exception("foo", 0)
+        .End()
+        .Code()
+            .Function("call", { params: [], ret: "i32" })
+                .Try("i32")
+                    .Try("void")
+                        .Throw(0)
+                    .Delegate(0)
+                .Catch(0)
+                    .I32Const(2)
+                .End()
+            .End()
+        .End()
+
+
+    const bin = b.WebAssembly().get();
+    assert.throws(() => {
+        const module = new WebAssembly.Module(bin);
+    }, WebAssembly.CompileError, `WebAssembly.Module doesn't validate:  block with type: () -> [I32] returns: 1 but stack has: 0 values, in function at index 0 (evaluating 'new WebAssembly.Module(bin)')`);
+}
+
 function testThrowDelegateSkip() {
+    const b = new Builder();
+    b.Type().End()
+        .Function().End()
+        .Exception().Signature({ params: []}).End()
+        .Export().Function("call")
+            .Exception("foo", 0)
+        .End()
+        .Code()
+            .Function("call", { params: [], ret: "i32" })
+                .Try("i32")
+                    .Try("i32")
+                        .Try("void")
+                            .Throw(0)
+                        .Delegate(1)
+                        .I32Const(42)
+                    .Catch(0)
+                        .I32Const(1)
+                    .End()
+                .Catch(0)
+                    .I32Const(2)
+                .End()
+            .End()
+        .End()
+
+
+    const bin = b.WebAssembly().get();
+    const module = new WebAssembly.Module(bin);
+    const instance = new WebAssembly.Instance(module, { });
+
+    assert.eq(instance.exports.call(), 2, "catching an exported wasm tag thrown from JS should be possible");
+}
+
+function testThrowDelegateSkipIllegal() {
     const b = new Builder();
     b.Type().End()
         .Function().End()
@@ -55,13 +116,49 @@ function testThrowDelegateSkip() {
 
 
     const bin = b.WebAssembly().get();
+    assert.throws(() => {
+        const module = new WebAssembly.Module(bin);
+    }, WebAssembly.CompileError, `WebAssembly.Module doesn't validate:  block with type: () -> [I32] returns: 1 but stack has: 0 values, in function at index 0 (evaluating 'new WebAssembly.Module(bin)')`);
+}
+
+function testDelegateCaller() {
+    const b = new Builder();
+    b.Type().End()
+        .Function().End()
+        .Exception().Signature({ params: []}).End()
+        .Export().Function("call")
+            .Exception("foo", 0)
+        .End()
+        .Code()
+            .Function("call", { params: [], ret: "i32" })
+                .Try("i32")
+                    .Call(1)
+                .Catch(0)
+                    .I32Const(2)
+                .End()
+            .End()
+
+            .Function("call2", { params: [], ret: "i32" })
+                .Try("i32")
+                    .Try("void")
+                        .Throw(0)
+                    .Delegate(1)
+                    .I32Const(42)
+                .Catch(0)
+                    .I32Const(1)
+                .End()
+            .End()
+        .End()
+
+
+    const bin = b.WebAssembly().get();
     const module = new WebAssembly.Module(bin);
     const instance = new WebAssembly.Instance(module, { });
 
     assert.eq(instance.exports.call(), 2, "catching an exported wasm tag thrown from JS should be possible");
 }
 
-function testDelegateCaller() {
+function testDelegateCallerIllegal() {
     const b = new Builder();
     b.Type().End()
         .Function().End()
@@ -91,10 +188,9 @@ function testDelegateCaller() {
 
 
     const bin = b.WebAssembly().get();
-    const module = new WebAssembly.Module(bin);
-    const instance = new WebAssembly.Instance(module, { });
-
-    assert.eq(instance.exports.call(), 2, "catching an exported wasm tag thrown from JS should be possible");
+    assert.throws(() => {
+        const module = new WebAssembly.Module(bin);
+    }, WebAssembly.CompileError, `WebAssembly.Module doesn't validate:  block with type: () -> [I32] returns: 1 but stack has: 0 values, in function at index 1 (evaluating 'new WebAssembly.Module(bin)')`);
 }
 
 function testSimpleDelegateMerge(){
@@ -120,6 +216,9 @@ function testSimpleDelegateMerge(){
 }
 
 testSimpleThrowDelegate();
+testSimpleThrowDelegateIllegal();
 testThrowDelegateSkip();
+testThrowDelegateSkipIllegal();
 testDelegateCaller();
+testDelegateCallerIllegal();
 testSimpleDelegateMerge();
