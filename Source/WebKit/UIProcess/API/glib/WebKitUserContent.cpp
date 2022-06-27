WebKit/WebKit

commit 2ca07814300d49b49a7c76a97d6da742a86c86f8

[WPE][GTK] Enable support for CONTENT_EXTENSIONS https://bugs.webkit.org/show_bug.cgi?id=167941
Reviewed by Carlos Garcia Campos.

Source/WebCore:

* platform/gtk/po/POTFILES.in: Added WebKitUserContentFilterStore.cpp
to the list of files with translatable strings.

Source/WebKit:

Adds new API to manage a collection of content extensions on disk (including compilation
of new ones) using WebKitUserContentFilterStore; the associated WebKitUserContentFilter
type (which represents a compiled content extension); and the functions needed to enable
and disable them for a given WebKitUserContentManager.

The WebKitUserContentFilterStore public API is expressed in abstract terms of "saving"
filters into the store (which involves compiling the JSON source rule set); and "loading"
them back as main operations. This way we do not disclose to users of the API any detail
about compilation, nor how contents are laid out on disk, and the documentation explicitly
tells about only using the provided functions to manipulate the on-disk contents. This
way we allow ourselves some leeway if the implementation needs changing in the future.

* PlatformGTK.cmake: Added WebKitUserContentFilterStore.h to the list of public API headers.
* PlatformWPE.cmake: Ditto.
* SourcesGTK.txt: Added WebKitUserContentFilterStore.cpp
* SourcesWPE.txt: Ditto.
* UIProcess/API/glib/WebKitError.cpp: Add definition of webkit_user_content_filter_error_quark().
* UIProcess/API/glib/WebKitUserContent.cpp: Added WebKitUserContentFilter.
(_WebKitUserContentFilter::_WebKitUserContentFilter):
(webkit_user_content_filter_ref):
(webkit_user_content_filter_unref):
(webkit_user_content_filter_get_identifier):
(webkitUserContentFilterCreate):
(webkitUserContentFilterGetContentRuleList):
* UIProcess/API/glib/WebKitUserContentFilterStore.cpp: Added.
(toGError): Utility function to convert content extension error codes to GError.
(webkit_user_content_filter_store_class_init):
(webkit_user_content_filter_store_new):

Source/WebKit/UIProcess/API/glib/WebKitUserContent.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Igalia S.L.
+ * Copyright (C) 2014, 2018-2019 Igalia S.L.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -316,4 +316,86 @@ WebKitUserScript* webkit_user_script_new_for_world(const gchar* source, WebKitUs
 API::UserScript& webkitUserScriptGetUserScript(WebKitUserScript* userScript)
 {
     return *userScript->userScript;
+}
+
+
+struct _WebKitUserContentFilter {
+    _WebKitUserContentFilter(RefPtr<API::ContentRuleList>&& contentRuleList)
+        : identifier(contentRuleList->name().utf8())
+        , contentRuleList(WTFMove(contentRuleList))
+        , referenceCount(1)
+    {
+    }
+
+    CString identifier;
+    RefPtr<API::ContentRuleList> contentRuleList;
+    int referenceCount;
+};
+
+G_DEFINE_BOXED_TYPE(WebKitUserContentFilter, webkit_user_content_filter, webkit_user_content_filter_ref, webkit_user_content_filter_unref)
+
+/**
+ * webkit_user_content_filter_ref:
+ * @user_content_filter: A #WebKitUserContentFilter
+ *
+ * Atomically increments the reference count of @user_content_filter by one.
+ * This function is MT-safe and may be called from any thread.
+ *
+ * Since: 2.24
+ */
+WebKitUserContentFilter* webkit_user_content_filter_ref(WebKitUserContentFilter* userContentFilter)
+{
+    g_return_val_if_fail(userContentFilter, nullptr);
+    g_atomic_int_inc(&userContentFilter->referenceCount);
+    return userContentFilter;
+}
+
+/**
+ * webkit_user_content_filter_unref:
+ * @user_content_filter: A #WebKitUserContentFilter
+ *
+ * Atomically decrements the reference count of @user_content_filter by one.
+ * If the reference count drops to 0, all the memory allocated by the
+ * #WebKitUserContentFilter is released. This function is MT-safe and may
+ * be called from any thread.
+ *
+ * Since: 2.24
+ */
+void webkit_user_content_filter_unref(WebKitUserContentFilter* userContentFilter)
+{
+    g_return_if_fail(userContentFilter);
+    if (g_atomic_int_dec_and_test(&userContentFilter->referenceCount)) {
+        userContentFilter->~WebKitUserContentFilter();
+        fastFree(userContentFilter);
+    }
+}
+
+/**
+ * webkit_user_content_filter_get_identifier:
+ * @user_content_filter: A #WebKitUserContentFilter
+ *
+ * Obtain the identifier previously used to save the @user_content_filter in the
+ * #WebKitUserContentFilterStore.
+ *
+ * Returns: (transfer none): the identifier for the filter
+ *
+ * Since: 2.24
+ */
+const char* webkit_user_content_filter_get_identifier(WebKitUserContentFilter* userContentFilter)
+{
+    g_return_val_if_fail(userContentFilter, nullptr);
+    return userContentFilter->identifier.data();
+}
+
+WebKitUserContentFilter* webkitUserContentFilterCreate(RefPtr<API::ContentRuleList>&& contentRuleList)
+{
+    WebKitUserContentFilter* userContentFilter = static_cast<WebKitUserContentFilter*>(fastMalloc(sizeof(WebKitUserContentFilter)));
+    new (userContentFilter) WebKitUserContentFilter(WTFMove(contentRuleList));
+    return userContentFilter;
+}
+
+API::ContentRuleList& webkitUserContentFilterGetContentRuleList(WebKitUserContentFilter* userContentFilter)
+{
+    ASSERT(userContentFilter);
+    return *userContentFilter->contentRuleList;
 }

Source/WebKit/UIProcess/API/glib/WebKitUserContentManager.cpp
@@ -318,6 +318,56 @@ void webkit_user_content_manager_unregister_script_message_handler_in_world(WebK
     manager->priv->userContentController->removeUserMessageHandlerForName(String::fromUTF8(name), webkitUserContentWorld(worldName));
 }
 
+/**
+ * webkit_user_content_manager_add_filter:
+ * @manager: A #WebKitUserContentManager
+ * @filter: A #WebKitUserContentFilter
+ *
+ * Adds a #WebKitUserContentFilter to the given #WebKitUserContentManager.
+ * The same #WebKitUserContentFilter can be reused with multiple
+ * #WebKitUserContentManager instances.
+ *
+ * Filters need to be saved and loaded from #WebKitUserContentFilterStore.
+ *
+ * Since: 2.24
+ */
+void webkit_user_content_manager_add_filter(WebKitUserContentManager* manager, WebKitUserContentFilter* filter)
+{
+    g_return_if_fail(WEBKIT_IS_USER_CONTENT_MANAGER(manager));
+    g_return_if_fail(filter);
+    manager->priv->userContentController->addContentRuleList(webkitUserContentFilterGetContentRuleList(filter));
+}
+
+/**
+ * webkit_user_content_manager_remove_filter:
+ * @manager: A #WebKitUserContentManager
+ * @filter: A #WebKitUserContentFilter
+ *
+ * Removes a filter from the given #WebKitUserContentManager.
+ *
+ * Since 2.24
+ */
+void webkit_user_content_manager_remove_filter(WebKitUserContentManager* manager, WebKitUserContentFilter* filter)
+{
+    g_return_if_fail(WEBKIT_IS_USER_CONTENT_MANAGER(manager));
+    g_return_if_fail(filter);
+    manager->priv->userContentController->removeContentRuleList(webkitUserContentFilterGetContentRuleList(filter).name());
+}
+
+/**
+ * webkit_user_content_manager_remove_all_filters:
+ * @manager: A #WebKitUserContentManager
+ *
+ * Removes all content filters from the given #WebKitUserContentManager.
+ *
+ * Since: 2.24
+ */
+void webkit_user_content_manager_remove_all_filters(WebKitUserContentManager* manager)
+{
+    g_return_if_fail(WEBKIT_IS_USER_CONTENT_MANAGER(manager));
+    manager->priv->userContentController->removeAllContentRuleLists();
+}
+
 WebUserContentControllerProxy* webkitUserContentManagerGetUserContentControllerProxy(WebKitUserContentManager* manager)
 {
     return manager->priv->userContentController.get();

Source/WTF/wtf/glib/GRefPtr.cpp
@@ -169,6 +169,19 @@ template <> void derefGPtr(GRegex* ptr)
         g_regex_unref(ptr);
 }
 
+template <> GMappedFile* refGPtr(GMappedFile* ptr)
+{
+    if (ptr)
+        g_mapped_file_ref(ptr);
+    return ptr;
+}
+
+template <> void derefGPtr(GMappedFile* ptr)
+{
+    if (ptr)
+        g_mapped_file_unref(ptr);
+}
+
 } // namespace WTF
 
 #endif // USE(GLIB)

Source/WTF/wtf/glib/GRefPtr.h
@@ -239,6 +239,8 @@ template <> WTF_EXPORT_PRIVATE GClosure* refGPtr(GClosure*);
 template <> WTF_EXPORT_PRIVATE void derefGPtr(GClosure*);
 template <> WTF_EXPORT_PRIVATE GRegex* refGPtr(GRegex*);
 template <> WTF_EXPORT_PRIVATE void derefGPtr(GRegex*);
+template <> WTF_EXPORT_PRIVATE GMappedFile* refGPtr(GMappedFile*);
+template <> WTF_EXPORT_PRIVATE void derefGPtr(GMappedFile*);
 
 template <typename T> inline T* refGPtr(T* ptr)
 {

Source/WebCore/platform/gtk/po/POTFILES.in
@@ -19,6 +19,7 @@ LocalizedStringsGtk.cpp
 ../../../WebKit/UIProcess/API/glib/WebKitNotification.cpp
 ../../../WebKit/UIProcess/API/glib/WebKitResponsePolicyDecision.cpp
 ../../../WebKit/UIProcess/API/glib/WebKitSettings.cpp
+../../../WebKit/UIProcess/API/glib/WebKitUserContentFilterStore.cpp
 ../../../WebKit/UIProcess/API/glib/WebKitUserMediaPermissionRequest.cpp
 ../../../WebKit/UIProcess/API/glib/WebKitWebContext.cpp
 ../../../WebKit/UIProcess/API/glib/WebKitWebResource.cpp

Source/WebKit/PlatformGTK.cmake
@@ -109,6 +109,7 @@ set(WebKit2GTK_INSTALLED_HEADERS
     ${WEBKIT_DIR}/UIProcess/API/gtk/WebKitURISchemeRequest.h
     ${WEBKIT_DIR}/UIProcess/API/gtk/WebKitURIUtilities.h
     ${WEBKIT_DIR}/UIProcess/API/gtk/WebKitUserContent.h
+    ${WEBKIT_DIR}/UIProcess/API/gtk/WebKitUserContentFilterStore.h
     ${WEBKIT_DIR}/UIProcess/API/gtk/WebKitUserContentManager.h
     ${WEBKIT_DIR}/UIProcess/API/gtk/WebKitUserMediaPermissionRequest.h
     ${WEBKIT_DIR}/UIProcess/API/gtk/WebKitWebContext.h

Source/WebKit/PlatformWPE.cmake
@@ -133,6 +133,7 @@ set(WPE_API_INSTALLED_HEADERS
     ${WEBKIT_DIR}/UIProcess/API/wpe/WebKitURISchemeRequest.h
     ${WEBKIT_DIR}/UIProcess/API/wpe/WebKitURIUtilities.h
     ${WEBKIT_DIR}/UIProcess/API/wpe/WebKitUserContent.h
+    ${WEBKIT_DIR}/UIProcess/API/gtk/WebKitUserContentFilterStore.h
     ${WEBKIT_DIR}/UIProcess/API/wpe/WebKitUserContentManager.h
     ${WEBKIT_DIR}/UIProcess/API/wpe/WebKitUserMediaPermissionRequest.h
     ${WEBKIT_DIR}/UIProcess/API/wpe/WebKitWebContext.h

commit 8484c9ba1afef49e25b0ce5c57694004be423c2b

[GLib] Fix build without ContentExtensions https://bugs.webkit.org/show_bug.cgi?id=240719
Reviewed by Adrian Perez de Castro.

Build without ContentExtensions is broken since r241790 because of
API::ContentRuleList usage in WebKitUserContent* API files.

* Source/WebKit/UIProcess/API/glib/WebKitUserContent.cpp:
(_WebKitUserContentFilter::_WebKitUserContentFilter): Make identifier
conditional and reorder fields to make build checks easier to read
* Source/WebKit/UIProcess/API/glib/WebKitUserContentFilterStore.cpp:
(webkitUserContentFilterStoreConstructed):
(webkit_user_content_filter_store_save):
(webkit_user_content_filter_store_save_from_file):
(webkit_user_content_filter_store_remove):
(webkit_user_content_filter_store_load):
(webkit_user_content_filter_store_fetch_identifiers):
* Source/WebKit/UIProcess/API/glib/WebKitUserContentManager.cpp:
(webkit_user_content_manager_add_filter):
(webkit_user_content_manager_remove_filter):
(webkit_user_content_manager_remove_filter_by_id):
(webkit_user_content_manager_remove_all_filters):

Canonical link: https://commits.webkit.org/251866@main

Source/WebKit/UIProcess/API/glib/WebKitUserContent.cpp
@@ -336,14 +336,16 @@ API::UserScript& webkitUserScriptGetUserScript(WebKitUserScript* userScript)
 
 struct _WebKitUserContentFilter {
     _WebKitUserContentFilter(RefPtr<API::ContentRuleList>&& contentRuleList)
-        : identifier(contentRuleList->name().utf8())
-        , contentRuleList(WTFMove(contentRuleList))
+        : contentRuleList(WTFMove(contentRuleList))
+#if ENABLE(CONTENT_EXTENSIONS)
+        , identifier(this->contentRuleList->name().utf8())
+#endif
         , referenceCount(1)
     {
     }
 
-    CString identifier;
     RefPtr<API::ContentRuleList> contentRuleList;
+    CString identifier;
     int referenceCount;
 };
 

Source/WebKit/UIProcess/API/glib/WebKitUserContentFilterStore.cpp
@@ -70,15 +70,19 @@ enum {
     PROP_PATH,
 };
 
+#if ENABLE(CONTENT_EXTENSIONS)
 static inline GError* toGError(WebKitUserContentFilterError code, const std::error_code error)
 {
     ASSERT(error);
     return g_error_new_literal(WEBKIT_USER_CONTENT_FILTER_ERROR, code, error.message().c_str());
 }
+#endif
 
 struct _WebKitUserContentFilterStorePrivate {
     GUniquePtr<char> storagePath;
+#if ENABLE(CONTENT_EXTENSIONS)
     RefPtr<API::ContentRuleListStore> store;
+#endif
 };
 
 WEBKIT_DEFINE_TYPE(WebKitUserContentFilterStore, webkit_user_content_filter_store, G_TYPE_OBJECT)
@@ -113,8 +117,10 @@ static void webkitUserContentFilterStoreConstructed(GObject* object)
 {
     G_OBJECT_CLASS(webkit_user_content_filter_store_parent_class)->constructed(object);
 
+#if ENABLE(CONTENT_EXTENSIONS)
     WebKitUserContentFilterStore* store = WEBKIT_USER_CONTENT_FILTER_STORE(object);
     store->priv->store = adoptRef(new API::ContentRuleListStore(FileSystem::stringFromFileSystemRepresentation(store->priv->storagePath.get())));
+#endif
 }
 
 static void webkit_user_content_filter_store_class_init(WebKitUserContentFilterStoreClass* storeClass)
@@ -179,6 +185,7 @@ const char* webkit_user_content_filter_store_get_path(WebKitUserContentFilterSto
     return store->priv->storagePath.get();
 }
 
+#if ENABLE(CONTENT_EXTENSIONS)
 static void webkitUserContentFilterStoreSaveBytes(GRefPtr<GTask>&& task, String&& identifier, GRefPtr<GBytes>&& source)
 {
     size_t sourceSize;
@@ -200,6 +207,7 @@ static void webkitUserContentFilterStoreSaveBytes(GRefPtr<GTask>&& task, String&
             g_task_return_pointer(task.get(), webkitUserContentFilterCreate(WTFMove(contentRuleList)), reinterpret_cast<GDestroyNotify>(webkit_user_content_filter_unref));
     });
 }
+#endif
 
 /**
  * webkit_user_content_filter_store_save:
@@ -231,7 +239,11 @@ void webkit_user_content_filter_store_save(WebKitUserContentFilterStore* store, 
     g_return_if_fail(callback);
 
     GRefPtr<GTask> task = adoptGRef(g_task_new(store, cancellable, callback, userData));
+#if ENABLE(CONTENT_EXTENSIONS)
     webkitUserContentFilterStoreSaveBytes(WTFMove(task), String::fromUTF8(identifier), GRefPtr<GBytes>(source));
+#else
+    g_task_return_new_error(task.get(), WEBKIT_USER_CONTENT_FILTER_ERROR, WEBKIT_USER_CONTENT_FILTER_ERROR_NOT_FOUND, "Content Extensions disabled");
+#endif
 }
 
 /**
@@ -254,10 +266,12 @@ WebKitUserContentFilter* webkit_user_content_filter_store_save_finish(WebKitUser
     return static_cast<WebKitUserContentFilter*>(g_task_propagate_pointer(G_TASK(result), error));
 }
 
+#if ENABLE(CONTENT_EXTENSIONS)
 struct SaveTaskData {
     String identifier;
 };
 WEBKIT_DEFINE_ASYNC_DATA_STRUCT(SaveTaskData)
+#endif
 
 /**
  * webkit_user_content_filter_store_save_from_file:
@@ -285,7 +299,7 @@ void webkit_user_content_filter_store_save_from_file(WebKitUserContentFilterStor
     g_return_if_fail(callback);
 
     GRefPtr<GTask> task = adoptGRef(g_task_new(store, cancellable, callback, userData));
-
+#if ENABLE(CONTENT_EXTENSIONS)
     // Try mapping the file in memory first, and fall-back to reading the contents if that fails.
     if (g_file_is_native(file)) {
         GUniquePtr<char> filePath(g_file_get_path(file));
@@ -316,6 +330,9 @@ void webkit_user_content_filter_store_save_from_file(WebKitUserContentFilterStor
         } else
             g_task_return_error(task.get(), error.release());
     }, task.leakRef());
+#else
+    g_task_return_new_error(task.get(), WEBKIT_USER_CONTENT_FILTER_ERROR, WEBKIT_USER_CONTENT_FILTER_ERROR_NOT_FOUND, "Content Extensions disabled");
+#endif
 }
 
 /**
@@ -361,6 +378,7 @@ void webkit_user_content_filter_store_remove(WebKitUserContentFilterStore* store
     g_return_if_fail(callback);
 
     GRefPtr<GTask> task = adoptGRef(g_task_new(store, cancellable, callback, userData));
+#if ENABLE(CONTENT_EXTENSIONS)
     store->priv->store->removeContentRuleList(String::fromUTF8(identifier), [task = WTFMove(task)](std::error_code error) {
         if (g_task_return_error_if_cancelled(task.get()))
             return;
@@ -371,6 +389,9 @@ void webkit_user_content_filter_store_remove(WebKitUserContentFilterStore* store
         } else
             g_task_return_boolean(task.get(), TRUE);
     });
+#else
+    g_task_return_new_error(task.get(), WEBKIT_USER_CONTENT_FILTER_ERROR, WEBKIT_USER_CONTENT_FILTER_ERROR_NOT_FOUND, "Content Extensions disabled");
+#endif
 }
 
 /**
@@ -416,6 +437,7 @@ void webkit_user_content_filter_store_load(WebKitUserContentFilterStore* store, 
     g_return_if_fail(callback);
 
     GRefPtr<GTask> task = adoptGRef(g_task_new(store, cancellable, callback, userData));
+#if ENABLE(CONTENT_EXTENSIONS)
     store->priv->store->lookupContentRuleList(String::fromUTF8(identifier), [task = WTFMove(task)](RefPtr<API::ContentRuleList> contentRuleList, std::error_code error) {
         if (g_task_return_error_if_cancelled(task.get()))
             return;
@@ -427,6 +449,9 @@ void webkit_user_content_filter_store_load(WebKitUserContentFilterStore* store, 
         } else
             g_task_return_pointer(task.get(), webkitUserContentFilterCreate(WTFMove(contentRuleList)), reinterpret_cast<GDestroyNotify>(webkit_user_content_filter_unref));
     });
+#else
+    g_task_return_new_error(task.get(), WEBKIT_USER_CONTENT_FILTER_ERROR, WEBKIT_USER_CONTENT_FILTER_ERROR_NOT_FOUND, "Content Extensions disabled");
+#endif
 }
 
 /**
@@ -470,6 +495,7 @@ void webkit_user_content_filter_store_fetch_identifiers(WebKitUserContentFilterS
     g_return_if_fail(callback);
 
     GRefPtr<GTask> task = adoptGRef(g_task_new(store, cancellable, callback, userData));
+#if ENABLE(CONTENT_EXTENSIONS)
     store->priv->store->getAvailableContentRuleListIdentifiers([task = WTFMove(task)](WTF::Vector<WTF::String> identifiers) {
         if (g_task_return_error_if_cancelled(task.get()))
             return;
@@ -479,6 +505,9 @@ void webkit_user_content_filter_store_fetch_identifiers(WebKitUserContentFilterS
             result[i] = g_strdup(identifiers[i].utf8().data());
         g_task_return_pointer(task.get(), result, reinterpret_cast<GDestroyNotify>(g_strfreev));
     });
+#else
+    g_task_return_new_error(task.get(), WEBKIT_USER_CONTENT_FILTER_ERROR, WEBKIT_USER_CONTENT_FILTER_ERROR_NOT_FOUND, "Content Extensions disabled");
+#endif
 }
 
 /**

Source/WebKit/UIProcess/API/glib/WebKitUserContentManager.cpp
@@ -386,7 +386,9 @@ void webkit_user_content_manager_add_filter(WebKitUserContentManager* manager, W
 {
     g_return_if_fail(WEBKIT_IS_USER_CONTENT_MANAGER(manager));
     g_return_if_fail(filter);
+#if ENABLE(CONTENT_EXTENSIONS)
     manager->priv->userContentController->addContentRuleList(webkitUserContentFilterGetContentRuleList(filter));
+#endif
 }
 
 /**
@@ -402,7 +404,9 @@ void webkit_user_content_manager_remove_filter(WebKitUserContentManager* manager
 {
     g_return_if_fail(WEBKIT_IS_USER_CONTENT_MANAGER(manager));
     g_return_if_fail(filter);
+#if ENABLE(CONTENT_EXTENSIONS)
     manager->priv->userContentController->removeContentRuleList(webkitUserContentFilterGetContentRuleList(filter).name());
+#endif
 }
 
 /**
@@ -420,7 +424,9 @@ void webkit_user_content_manager_remove_filter_by_id(WebKitUserContentManager* m
 {
     g_return_if_fail(WEBKIT_IS_USER_CONTENT_MANAGER(manager));
     g_return_if_fail(filterId);
+#if ENABLE(CONTENT_EXTENSIONS)
     manager->priv->userContentController->removeContentRuleList(String::fromUTF8(filterId));
+#endif
 }
 
 /**
@@ -434,7 +440,9 @@ void webkit_user_content_manager_remove_filter_by_id(WebKitUserContentManager* m
 void webkit_user_content_manager_remove_all_filters(WebKitUserContentManager* manager)
 {
     g_return_if_fail(WEBKIT_IS_USER_CONTENT_MANAGER(manager));
+#if ENABLE(CONTENT_EXTENSIONS)
     manager->priv->userContentController->removeAllContentRuleLists();
+#endif
 }
 
 WebUserContentControllerProxy* webkitUserContentManagerGetUserContentControllerProxy(WebKitUserContentManager* manager)