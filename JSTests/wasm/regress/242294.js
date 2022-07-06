11024a1dd2a94ed4f31c2c1e4a34e5363dd7beb5
Justin Michaud <justin_michaud@apple.com>
2022-07-05 23:10:49 -0700

    Add tests for re-compile when doing loop osr in BBQ B3 mode. https://bugs.webkit.org/show_bug.cgi?id=242294.
    Air and B3 have two different approaches to loop osr. In BBQ Air, every
    loop is included as a potential entry point, so we only need to compile
    once. For OMG B3, we wish to compile with only one entry point at a time,
    so that we do not pessimize loops.
    
    Previously, we would always assume that BBQ generated all loop entry points,
    even when we forced the use of BBQ B3. This happens on iOS for large modules
    due to a workaround introduced to prevent executable memory exhaustion, and
    also when we manually run BBQ without air.
    
    This patch adds a test case and also adds extra test suite options to
    test this unusual mode regularly.
    
    Reviewed by Yusuke Suzuki.
    
    * JSTests/wasm/wast-tests/osr-loop-entry-force-b3.wasm: Added.
    * JSTests/wasm/wast-tests/osr-loop-entry-force-b3.wast: Added.
    * Tools/Scripts/run-jsc-stress-tests:
    
    Canonical link: https://commits.webkit.org/252171@main

---
+++ JSTests/wasm/regress/242294.js
@@ -0,0 +1,47 @@
+//@ requireOptions("--webAssemblyBBQAirModeThreshold=1")
+
+// Regression test for https://bugs.webkit.org/show_bug.cgi?id=242294
+// Run stress/osr-entry-basic.js with BBQB3 mode.
+
+import Builder from '../Builder.js'
+import * as assert from '../assert.js'
+
+{
+    const b = new Builder();
+    b.Type().End()
+        .Function().End()
+        .Export()
+            .Function("loop")
+        .End()
+        .Code()
+        .Function("loop", { params: ["i32"], ret: "i32" }, ["i32"])
+        .I32Const(0)
+        .SetLocal(1)
+        .Loop("void")
+        .Block("void", b =>
+               b.GetLocal(0)
+               .I32Const(0)
+               .I32Eq()
+               .BrIf(0)
+               .GetLocal(0)
+               .GetLocal(1)
+               .I32Add()
+               .SetLocal(1)
+               .GetLocal(0)
+               .I32Const(1)
+               .I32Sub()
+               .SetLocal(0)
+               .Br(1)
+              )
+        .End()
+        .GetLocal(1)
+        .Return()
+        .End()
+        .End()
+
+    const bin = b.WebAssembly().get();
+    const module = new WebAssembly.Module(bin);
+    const instance = new WebAssembly.Instance(module);
+
+    assert.eq(987459712, instance.exports.loop(100000000));
+}
