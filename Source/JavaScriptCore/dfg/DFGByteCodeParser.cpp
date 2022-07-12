5a8de3f50180a061d1140261ef82c825f7730952
Yusuke Suzuki <ysuzuki@apple.com>
2022-07-12 14:42:00 -0700

    Cherry-pick 1f3e8b70b999. rdar://problem/97264358
        [AArch32][Aarch64] ASSERTION FAILED variant.intrinsic() == NoIntrinsic in void JSC::DFG::ByteCodeParser::handleGetById
        https://bugs.webkit.org/show_bug.cgi?id=242599
        <rdar://96836847>
    
        Reviewed by Mark Lam.
    
        Previously all intrinsic getters are handled. So at this point, it should be NoIntrinsic.
        But after introducing 4GB TypedArray and BigInt64Array, this handling can fail.
    
        However, in this case, we should just continue using this generic path: invoking a getter.
        Thus, the current code is correct, and this assertion is stale.
    
        This patch removes this stale assertion.
    
        * JSTests/stress/bigint-array-byte-offset.js: Added.
        (__f_2):
        * Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp:
        (JSC::DFG::ByteCodeParser::handleGetById):
    
        Canonical link: https://commits.webkit.org/252391@main
    
    Canonical link: https://commits.webkit.org/245886.807@safari-7613.3.9.0-branch

--- Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp
+++ Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp
@@ -4771,8 +4771,6 @@ void ByteCodeParser::handleGetById(
         return;
     }
 
-    ASSERT(variant.intrinsic() == NoIntrinsic);
-
     // Make a call. We don't try to get fancy with using the smallest operand number because
     // the stack layout phase should compress the stack anyway.
     
