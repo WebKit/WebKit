c3670558b88295ac35a29099d23d912b8d30f748
Tim Horton <timothy_horton@apple.com>
2022-07-18 19:30:30 -0700

    Overflow assertion under `interactionRegionForRenderedRegion` on yahoo.com https://bugs.webkit.org/show_bug.cgi?id=242877 rdar://97219389
    Reviewed by Wenson Hsieh and Devin Rousso.
    
    * Source/WebCore/page/InteractionRegion.cpp:
    (WebCore::interactionRegionForRenderedRegion):
    Somehow, Yahoo's banner ad is getting an interaction region of 135800x35000,
    which overflows when area() is called. Record the overflow and bail instead of
    crashing.
    
    * LayoutTests/interaction-region/region-area-overflow-does-not-crash-expected.txt: Added.
    * LayoutTests/interaction-region/region-area-overflow-does-not-crash.html: Added.
    Add a test: the event region exists, the interaction region does not, because we bailed out.
    
    Canonical link: https://commits.webkit.org/252588@main

--- Source/WebCore/page/InteractionRegion.cpp
+++ Source/WebCore/page/InteractionRegion.cpp
@@ -72,7 +72,11 @@ std::optional<InteractionRegion> interactionRegionForRenderedRegion(RenderObject
     auto& mainFrameView = *regionRenderer.document().frame()->mainFrame().view();
     auto layoutArea = mainFrameView.layoutSize().area();
 
-    if (bounds.area() > layoutArea / 2)
+    auto checkedRegionArea = bounds.area<RecordOverflow>();
+    if (checkedRegionArea.hasOverflowed())
+        return std::nullopt;
+
+    if (checkedRegionArea.value() > layoutArea / 2)
         return std::nullopt;
 
     auto element = dynamicDowncast<Element>(regionRenderer.node());
