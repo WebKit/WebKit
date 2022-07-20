d6484ed66c865d78157f2d560ef276ada4145dde
Aakash Jain <aakash_jain@apple.com>
2022-07-20 08:37:23 -0700

    [ews-app] Add basic github support https://bugs.webkit.org/show_bug.cgi?id=242940
    Reviewed by Jonathan Bedard.
    
    Mostly copied from GitHub class in https://github.com/WebKit/WebKit/blob/main/Tools/CISupport/ews-build/steps.py
    * Tools/CISupport/ews-app/ews/common/github.py: Added.
    
    Canonical link: https://commits.webkit.org/252645@main

---
+++ Tools/CISupport/ews-app/ews/common/github.py
@@ -0,0 +1,134 @@
+# Copyright (C) 2022 Apple Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import json
+import logging
+import requests
+from requests.auth import HTTPBasicAuth
+
+_log = logging.getLogger(__name__)
+
+GITHUB_URL = 'https://github.com/'
+GITHUB_PROJECTS = ['WebKit/WebKit', 'apple/WebKit', 'WebKit/WebKit-security']
+
+
+class GitHub(object):
+    _cache = {}
+
+    @classmethod
+    def repository_urls(cls):
+        return [GITHUB_URL + project for project in GITHUB_PROJECTS]
+
+    @classmethod
+    def pr_url(cls, pr_number, repository_url=None):
+        if not repository_url:
+            repository_url = '{}{}'.format(GITHUB_URL, GITHUB_PROJECTS[0])
+
+        if repository_url not in GitHub.repository_urls():
+            return ''
+        if not pr_number or not isinstance(pr_number, int):
+            return ''
+        return '{}/pull/{}'.format(repository_url, pr_number)
+
+    @classmethod
+    def commit_url(cls, sha, repository_url=None):
+        if not repository_url:
+            repository_url = '{}{}'.format(GITHUB_URL, GITHUB_PROJECTS[0])
+        if repository_url not in GitHub.repository_urls():
+            return ''
+        if not sha:
+            return ''
+        return '{}/commit/{}'.format(repository_url, sha)
+
+    @classmethod
+    def api_url(cls, repository_url=None):
+        if not repository_url:
+            repository_url = '{}{}'.format(GITHUB_URL, GITHUB_PROJECTS[0])
+
+        if repository_url not in GitHub.repository_urls():
+            return ''
+        _, url_base = repository_url.split('://', 1)
+        host, path = url_base.split('/', 1)
+        return 'https://api.{}/repos/{}'.format(host, path)
+
+    @classmethod
+    def commit_status_url(cls, sha, repository_url=None):
+        api_url = cls.api_url(repository_url)
+        if not sha or not api_url:
+            return ''
+        return '{}/statuses/{}'.format(api_url, sha)
+
+    @classmethod
+    def credentials(cls):
+        prefix = 'GITHUB_COM_'
+
+        if prefix in cls._cache:
+            return cls._cache[prefix]
+
+        try:
+            passwords = json.load(open('passwords.json'))
+            cls._cache[prefix] = passwords.get('GITHUB_COM_USERNAME', None), passwords.get('GITHUB_COM_ACCESS_TOKEN', None)
+        except Exception as e:
+            _log.error('Error reading GitHub credentials')
+            cls._cache[prefix] = None, None
+
+        return cls._cache[prefix]
+
+    def fetch_data_from_url_with_authentication_github(self, url):
+        response = None
+        try:
+            username, access_token = GitHub.credentials()
+            auth = HTTPBasicAuth(username, access_token) if username and access_token else None
+            response = requests.get(
+                url, timeout=60, auth=auth,
+                headers=dict(Accept='application/vnd.github.v3+json'),
+            )
+            if response.status_code // 100 != 2:
+                _log.error('Accessed {url} with unexpected status code {status_code}.\n'.format(url=url, status_code=response.status_code))
+                return None
+        except Exception as e:
+            # Catching all exceptions here to safeguard access token.
+            _log.error('Failed to access {}.\n'.format(url=url))
+            return None
+        return response
+
+    def comment_on_pr(self, pr_number, content, repository_url=None):
+        api_url = GitHub.api_url(repository_url)
+        if not api_url:
+            return False
+
+        comment_url = '{api_url}/issues/{pr_number}/comments'.format(api_url=api_url, pr_number=pr_number)
+        try:
+            username, access_token = GitHub.credentials()
+            auth = HTTPBasicAuth(username, access_token) if username and access_token else None
+            response = requests.request(
+                'POST', comment_url, timeout=60, auth=auth,
+                headers=dict(Accept='application/vnd.github.v3+json'),
+                json=dict(body=content),
+            )
+            if response.status_code // 100 != 2:
+                _log.error("Failed to post comment to PR {}. Unexpected response code from GitHub: {}\n".format(pr_number, response.status_code))
+                return False
+        except Exception as e:
+            _log.error("Error in posting comment to PR {}\n".format(pr_number))
+            return False
+        return True
