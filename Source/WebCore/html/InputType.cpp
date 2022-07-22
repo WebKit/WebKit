0d146070aafd25dcc83f833619dd13fe88cad6a9
Andres Gonzalez <andresg_22@apple.com>
2022-07-22 06:56:45 -0700

    Cherry-pick 10fe8edd8ce9. rdar://problem/97399293
        AX ITM: Input elements need to notify accessibility of value changes.
        https://bugs.webkit.org/show_bug.cgi?id=243077
    
        Reviewed by Chris Fleizach.
    
        We were notifying accessibility of value changes only for range controls. We now notify for all input elements. This fixes several tests in isolated tree mode, including:
        * LayoutTests/accessibility/ax-value-with-search-expected.txt:
        * LayoutTests/accessibility/ax-value-with-search.html:
    
        * Source/WebCore/accessibility/AXObjectCache.cpp:
        (WebCore::AXObjectCache::valueChanged):
        (WebCore::AXObjectCache::shouldProcessAttributeChange):
        Minor code cleanup.
        (WebCore::AXObjectCache::handleAttributeChange):
        * Source/WebCore/accessibility/AXObjectCache.h:
        (WebCore::AXObjectCache::valueChanged):
        (WebCore::AXObjectCache::shouldProcessAttributeChange):
        * Source/WebCore/html/InputType.cpp:
        (WebCore::InputType::setValue):
        (WebCore::InputType::applyStep):
    
        Canonical link: https://commits.webkit.org/252727@main
    
    Canonical link: https://commits.webkit.org/252432.140@safari-7614-branch

--- Source/WebCore/html/InputType.cpp
+++ Source/WebCore/html/InputType.cpp
@@ -836,10 +836,8 @@ void InputType::setValue(const String& sanitizedValue, bool valueChanged, TextFi
         break;
     }
 
-    if (isRangeControl()) {
-        if (auto* cache = element()->document().existingAXObjectCache())
-            cache->postNotification(element(), AXObjectCache::AXValueChanged);
-    }
+    if (auto* cache = element()->document().existingAXObjectCache())
+        cache->valueChanged(element());
 }
 
 void InputType::willDispatchClick(InputElementClickState&)
@@ -1060,8 +1058,8 @@ ExceptionOr<void> InputType::applyStep(int count, AnyStepHandling anyStepHandlin
     if (result.hasException() || !element())
         return result;
 
-    if (AXObjectCache* cache = element()->document().existingAXObjectCache())
-        cache->postNotification(element(), AXObjectCache::AXValueChanged);
+    if (auto* cache = element()->document().existingAXObjectCache())
+        cache->valueChanged(element());
 
     return result;
 }
