7193c27b62024742bace257c9f2a87243a5f876d
Ryosuke Niwa <rniwa@webkit.org>
2022-08-02 00:13:35 -0700

    Merge 253025@main - nullptr crash in XMLDocumentParser::doEnd() https://bugs.webkit.org/show_bug.cgi?id=76199
    Reviewed by Saam Barati.
    
    Add an early return to XMLDocumentParser::doEnd for when the parser had been detached.
    
    * LayoutTests/fast/parser/xml-parser-stop-parsing-during-subtree-modified-crash-expected.txt: Added.
    * LayoutTests/fast/parser/xml-parser-stop-parsing-during-subtree-modified-crash.xhtml: Added.
    * Source/WebCore/xml/parser/XMLDocumentParserLibxml2.cpp:
    (WebCore::XMLDocumentParser::doEnd):
    
    Canonical link: https://commits.webkit.org/253025@main
    
    (cherry picked from commit 09c22643353bfa48d5a0ed470d6ccda75f712e0e)

--- Source/WebCore/xml/parser/XMLDocumentParserLibxml2.cpp
+++ Source/WebCore/xml/parser/XMLDocumentParserLibxml2.cpp
@@ -1326,6 +1326,9 @@ void XMLDocumentParser::doEnd()
     }
 
 #if ENABLE(XSLT)
+    if (isDetached())
+        return;
+
     bool xmlViewerMode = !m_sawError && !m_sawCSS && !m_sawXSLTransform && shouldRenderInXMLTreeViewerMode(*document());
     if (xmlViewerMode) {
         XMLTreeViewer xmlTreeViewer(*document());
