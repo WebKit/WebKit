WebKit/WebKit

commit 42ad6e4af024381a287ea6a587da469ef43f2819

Signal handlers should have a two phase installation. https://bugs.webkit.org/show_bug.cgi?id=213160
Reviewed by Mark Lam.

Source/JavaScriptCore:

* jsc.cpp:
(CommandLine::parseArguments):
(jscmain):
* runtime/InitializeThreading.cpp:
(JSC::initializeThreading):
* runtime/VMTraps.cpp:
* tools/SigillCrashAnalyzer.cpp:
(JSC::installCrashHandler):
* wasm/WasmFaultSignalHandler.cpp:
(JSC::Wasm::enableFastMemory):
(JSC::Wasm::prepareFastMemory):
* wasm/WasmFaultSignalHandler.h:

Source/WebKit:

Put back old WASM fast memory installation.

* WebProcess/WebProcess.cpp:
(WebKit::WebProcess::initializeWebProcess):

Source/WTF:

LLDB does not like it when we single step while there are mach exception
handlers installed because LLDB suspends all the non-active threads.
With Mach exception handlers installed, the OS will send a mach message
to our exception handler port, which is different than the active thread.
When this happens, the combination of LLDB and the process JSC is in effectively
deadlock.

Under our new approach, we go back to only telling the OS we care about
these exceptions late but lock down the function pointers early. This way
processes that benefit from our exception handler code are easier to debug.

* wtf/threads/Signals.cpp:
(WTF::addSignalHandler):
(WTF::activateSignalHandlersFor):
(WTF::installSignalHandler): Deleted.
* wtf/threads/Signals.h:

Tools:

* TestWebKitAPI/Tests/WTF/Signals.cpp:
(TEST):


Canonical link: https://commits.webkit.org/225983@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@263045 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/jsc.cpp
@@ -66,6 +66,7 @@
 #include "TypedArrayInlines.h"
 #include "VMInspector.h"
 #include "WasmCapabilities.h"
+#include "WasmFaultSignalHandler.h"
 #include "WasmMemory.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -2986,11 +2987,16 @@ void CommandLine::parseArguments(int argc, char** argv)
                 return SignalAction::ForceDefault;
             };
 
-            installSignalHandler(Signal::IllegalInstruction, SignalHandler(exit));
-            installSignalHandler(Signal::AccessFault, SignalHandler(exit));
-            installSignalHandler(Signal::FloatingPoint, SignalHandler(exit));
+            addSignalHandler(Signal::IllegalInstruction, SignalHandler(exit));
+            addSignalHandler(Signal::AccessFault, SignalHandler(exit));
+            addSignalHandler(Signal::FloatingPoint, SignalHandler(exit));
             // once we do this lldb won't work anymore because we will exit on any breakpoints it sets.
-            installSignalHandler(Signal::Breakpoint, SignalHandler(exit));
+            addSignalHandler(Signal::Breakpoint, SignalHandler(exit));
+
+            activateSignalHandlersFor(Signal::IllegalInstruction);
+            activateSignalHandlersFor(Signal::AccessFault);
+            activateSignalHandlersFor(Signal::FloatingPoint);
+            activateSignalHandlersFor(Signal::Breakpoint);
 #endif
             continue;
         }
@@ -3122,6 +3128,10 @@ int runJSC(const CommandLine& options, bool isWorker, const Func& func)
     Worker worker(Workers::singleton());
     
     VM& vm = VM::create(LargeHeap).leakRef();
+#if ENABLE(WEBASSEMBLY)
+    Wasm::enableFastMemory();
+#endif
+
     int result;
     bool success = true;
     GlobalObject* globalObject = nullptr;

Source/JavaScriptCore/runtime/InitializeThreading.cpp
@@ -95,7 +95,7 @@ void initializeThreading()
 #endif
         VMTraps::initializeSignals();
 #if ENABLE(WEBASSEMBLY)
-        Wasm::enableFastMemory();
+        Wasm::prepareFastMemory();
 #endif
 
         WTF::compilerFence();

Source/JavaScriptCore/wasm/WasmFaultSignalHandler.cpp
@@ -113,11 +113,27 @@ void enableFastMemory()
         if (!Options::useWebAssemblyFastMemory())
             return;
 
-        installSignalHandler(Signal::AccessFault, [] (Signal signal, SigInfo& sigInfo, PlatformRegisters& ucontext) {
-            return trapHandler(signal, sigInfo, ucontext);
-        });
+        activateSignalHandlersFor(Signal::AccessFault);
 
         fastHandlerInstalled = true;
+    });
+#endif
+}
+
+void prepareFastMemory()
+{
+#if ENABLE(WEBASSEMBLY_FAST_MEMORY)
+    static std::once_flag once;
+    std::call_once(once, [] {
+        if (!Wasm::isSupported())
+            return;
+
+        if (!Options::useWebAssemblyFastMemory())
+            return;
+
+        addSignalHandler(Signal::AccessFault, [] (Signal signal, SigInfo& sigInfo, PlatformRegisters& ucontext) {
+            return trapHandler(signal, sigInfo, ucontext);
+        });
     });
 #endif // ENABLE(WEBASSEMBLY_FAST_MEMORY)
 }

Source/JavaScriptCore/wasm/WasmFaultSignalHandler.h
@@ -32,6 +32,7 @@ namespace JSC {
 namespace Wasm {
 
 bool fastMemoryEnabled();
+JS_EXPORT_PRIVATE void prepareFastMemory();
 JS_EXPORT_PRIVATE void enableFastMemory();
 
 } } // namespace JSC::Wasm

Source/WebKit/WebProcess/WebProcess.cpp
@@ -83,6 +83,7 @@
 #include "WebsiteDataType.h"
 #include <JavaScriptCore/JSLock.h>
 #include <JavaScriptCore/MemoryStatistics.h>
+#include <JavaScriptCore/WasmFaultSignalHandler.h>
 #include <WebCore/AXObjectCache.h>
 #include <WebCore/ApplicationCacheStorage.h>
 #include <WebCore/AuthenticationChallenge.h>
@@ -471,6 +472,10 @@ void WebProcess::initializeWebProcess(WebProcessCreationParameters&& parameters)
 
 #if ENABLE(SERVICE_WORKER)
     ServiceWorkerProvider::setSharedProvider(WebServiceWorkerProvider::singleton());
+#endif
+
+#if ENABLE(WEBASSEMBLY)
+    JSC::Wasm::enableFastMemory();
 #endif
 
 #if ENABLE(RESOURCE_LOAD_STATISTICS) && !RELEASE_LOG_DISABLED

Source/JavaScriptCore/runtime/VMTraps.cpp
@@ -193,13 +193,15 @@ public:
     SignalSender(const AbstractLocker& locker, VM& vm)
         : Base(locker, vm.traps().m_lock, vm.traps().m_condition.copyRef())
         , m_vm(vm)
-    { }
+    {
+        activateSignalHandlersFor(Signal::AccessFault);
+    }
 
     static void initializeSignals()
     {
         static std::once_flag once;
         std::call_once(once, [] {
-            installSignalHandler(Signal::AccessFault, [] (Signal, SigInfo&, PlatformRegisters& registers) -> SignalAction {
+            addSignalHandler(Signal::AccessFault, [] (Signal, SigInfo&, PlatformRegisters& registers) -> SignalAction {
                 auto signalContext = SignalContext::tryCreate(registers);
                 if (!signalContext)
                     return SignalAction::NotHandled;

Source/JavaScriptCore/tools/SigillCrashAnalyzer.cpp
@@ -156,7 +156,7 @@ public:
 static void installCrashHandler()
 {
 #if CPU(X86_64) || CPU(ARM64)
-    installSignalHandler(Signal::IllegalInstruction, [] (Signal, SigInfo&, PlatformRegisters& registers) {
+    addSignalHandler(Signal::IllegalInstruction, [] (Signal, SigInfo&, PlatformRegisters& registers) {
         auto signalContext = SignalContext::tryCreate(registers);
         if (!signalContext)
             return SignalAction::NotHandled;
@@ -169,6 +169,7 @@ static void installCrashHandler()
         analyzer.analyze(*signalContext);
         return SignalAction::NotHandled;
     });
+    activateSignalHandlersFor(Signal::IllegalInstruction);
 #endif
 }
 

Source/WTF/wtf/threads/Signals.cpp
@@ -235,11 +235,12 @@ void handleSignalsWithMach()
     g_wtfConfig.signalHandlers.useMach = true;
 }
 
+static exception_mask_t activeExceptions;
 inline void setExceptionPorts(const AbstractLocker& threadGroupLocker, Thread& thread)
 {
     UNUSED_PARAM(threadGroupLocker);
     SignalHandlers& handlers = g_wtfConfig.signalHandlers;
-    kern_return_t result = thread_set_exception_ports(thread.machThread(), handlers.activeExceptions, handlers.exceptionPort, EXCEPTION_STATE | MACH_EXCEPTION_CODES, MACHINE_THREAD_STATE);
+    kern_return_t result = thread_set_exception_ports(thread.machThread(), handlers.addedExceptions &activeExceptions, handlers.exceptionPort, EXCEPTION_STATE | MACH_EXCEPTION_CODES, MACHINE_THREAD_STATE);
     if (result != KERN_SUCCESS) {
         dataLogLn("thread set port failed due to ", mach_error_string(result));
         CRASH();
@@ -265,10 +266,7 @@ void registerThreadForMachExceptionHandling(Thread& thread)
         setExceptionPorts(locker, thread);
 }
 
-#else
-static constexpr bool useMach = false;
 #endif // HAVE(MACH_EXCEPTIONS)
-
 
 inline size_t offsetForSystemSignal(int sig)
 {
@@ -278,14 +276,14 @@ inline size_t offsetForSystemSignal(int sig)
 
 static void jscSignalHandler(int, siginfo_t*, void*);
 
-void installSignalHandler(Signal signal, SignalHandler&& handler)
+void addSignalHandler(Signal signal, SignalHandler&& handler)
 {
     Config::AssertNotFrozenScope assertScope;
     SignalHandlers& handlers = g_wtfConfig.signalHandlers;
     ASSERT(signal < Signal::Unknown);
-#if HAVE(MACH_EXCEPTIONS)
     ASSERT(!handlers.useMach || signal != Signal::Usr);
 
+#if HAVE(MACH_EXCEPTIONS)
     if (handlers.useMach)
         startMachExceptionHandlerThread();
 #endif
@@ -293,10 +291,9 @@ void installSignalHandler(Signal signal, SignalHandler&& handler)
     static std::once_flag initializeOnceFlags[static_cast<size_t>(Signal::NumberOfSignals)];
     std::call_once(initializeOnceFlags[static_cast<size_t>(signal)], [&] {
         Config::AssertNotFrozenScope assertScope;
-#if HAVE(MACH_EXCEPTIONS)
-        bool useMach = handlers.useMach;
-#endif
-        if (!useMach) {
+
+        handlers.addedExceptions |= toMachMask(signal);
+        if (!handlers.useMach) {
             struct sigaction action;
             action.sa_sigaction = jscSignalHandler;
             auto result = sigfillset(&action.sa_mask);
@@ -314,11 +311,19 @@ void installSignalHandler(Signal signal, SignalHandler&& handler)
     });
 
     handlers.add(signal, WTFMove(handler));
+}
 
+void activateSignalHandlersFor(Signal signal)
+{
+    UNUSED_PARAM(signal);
 #if HAVE(MACH_EXCEPTIONS)
+    const SignalHandlers& handlers = g_wtfConfig.signalHandlers;
+    ASSERT(signal < Signal::Unknown);
+    ASSERT(!handlers.useMach || signal != Signal::Usr);
+
     auto locker = holdLock(activeThreads().getLock());
     if (handlers.useMach) {
-        handlers.activeExceptions |= toMachMask(signal);
+        activeExceptions |= toMachMask(signal);
 
         for (auto& thread : activeThreads().threads(locker))
             setExceptionPorts(locker, thread.get());

commit 7f676a460181db52d1b44e9b8a103f626ae90384

Cherry-pick bb92169c6b02. rdar://problem/97264328
    [JSC] Activate wasm fault handler when signaling memory is used
    https://bugs.webkit.org/show_bug.cgi?id=242358
    rdar://96056675

    Reviewed by Mark Lam.

    https://github.com/WebKit/WebKit/commit/42ad6e4af024381a287ea6a587da469ef43f2819 broke JavaScriptCore.framework's wasm signal handler
    since it is no longer installed. This patch activates that handler when wasm memory is created with signaling requirement, which is
    Signaling or Shared memory. We do not activate this in JSC::initialize since LLDB has a bug that it cannot handle mach exception.
    We defer this initialization only when we use Wasm::Memory with necessary features.

    * Source/JavaScriptCore/jsc.cpp:
    (runJSC):
    * Source/JavaScriptCore/runtime/InitializeThreading.cpp:
    (JSC::initialize):
    * Source/JavaScriptCore/shell/playstation/TestShell.cpp:
    (setupTestRun):
    * Source/JavaScriptCore/wasm/WasmFaultSignalHandler.cpp:
    (JSC::Wasm::activateSignalingMemory):
    (JSC::Wasm::initializeSignalingMemory): Deleted.
    * Source/JavaScriptCore/wasm/WasmFaultSignalHandler.h:
    * Source/JavaScriptCore/wasm/WasmMemory.cpp:
    (JSC::Wasm::MemoryHandle::MemoryHandle):
    * Source/WebKit/WebProcess/WebProcess.cpp:
    (WebKit::WebProcess::initializeWebProcess):

    Canonical link: https://commits.webkit.org/252164@main

Canonical link: https://commits.webkit.org/245886.815@safari-7613.3.9.0-branch

Source/JavaScriptCore/jsc.cpp
@@ -3617,9 +3617,6 @@ int runJSC(const CommandLine& options, bool isWorker, const Func& func)
     VM& vm = VM::create(LargeHeap).leakRef();
     if (!isWorker && options.m_canBlockIsFalse)
         vm.m_typedArrayController = adoptRef(new JSC::SimpleTypedArrayController(false));
-#if ENABLE(WEBASSEMBLY)
-    Wasm::enableFastMemory();
-#endif
 
     int result;
     bool success = true;

Source/JavaScriptCore/runtime/InitializeThreading.cpp
@@ -107,7 +107,7 @@ void initialize()
 #endif
         VMTraps::initializeSignals();
 #if ENABLE(WEBASSEMBLY)
-        Wasm::prepareFastMemory();
+        Wasm::prepareSignalingMemory();
 #endif
 
         WTF::compilerFence();

Source/JavaScriptCore/shell/playstation/TestShell.cpp
@@ -50,9 +50,6 @@ extern "C" void setupTestRun()
 
     JSC::initialize();
 
-#if ENABLE(WEBASSEMBLY)
-    JSC::Wasm::enableFastMemory();
-#endif
     Gigacage::forbidDisablingPrimitiveGigacage();
 }
 

Source/JavaScriptCore/wasm/WasmFaultSignalHandler.cpp
@@ -50,8 +50,6 @@ static constexpr bool verbose = false;
 }
 }
 
-static bool fastHandlerInstalled { false };
-
 #if ENABLE(WEBASSEMBLY_SIGNALING_MEMORY)
 
 static SignalAction trapHandler(Signal signal, SigInfo& sigInfo, PlatformRegisters& context)
@@ -107,12 +105,7 @@ static SignalAction trapHandler(Signal signal, SigInfo& sigInfo, PlatformRegiste
 
 #endif // ENABLE(WEBASSEMBLY_SIGNALING_MEMORY)
 
-bool fastMemoryEnabled()
-{
-    return fastHandlerInstalled;
-}
-
-void enableFastMemory()
+void activateSignalingMemory()
 {
 #if ENABLE(WEBASSEMBLY_SIGNALING_MEMORY)
     static std::once_flag once;
@@ -124,13 +117,11 @@ void enableFastMemory()
             return;
 
         activateSignalHandlersFor(Signal::AccessFault);
-
-        fastHandlerInstalled = true;
     });
-#endif
+#endif // ENABLE(WEBASSEMBLY_SIGNALING_MEMORY)
 }
 
-void prepareFastMemory()
+void prepareSignalingMemory()
 {
 #if ENABLE(WEBASSEMBLY_SIGNALING_MEMORY)
     static std::once_flag once;

Source/JavaScriptCore/wasm/WasmFaultSignalHandler.h
@@ -31,9 +31,8 @@ namespace JSC {
 
 namespace Wasm {
 
-bool fastMemoryEnabled();
-JS_EXPORT_PRIVATE void prepareFastMemory();
-JS_EXPORT_PRIVATE void enableFastMemory();
+void activateSignalingMemory();
+void prepareSignalingMemory();
 
 } } // namespace JSC::Wasm
 

Source/JavaScriptCore/wasm/WasmMemory.cpp
@@ -25,11 +25,12 @@
 
 #include "config.h"
 #include "WasmMemory.h"
-#include "WasmInstance.h"
 
 #if ENABLE(WEBASSEMBLY)
 
 #include "Options.h"
+#include "WasmFaultSignalHandler.h"
+#include "WasmInstance.h"
 #include <wtf/CheckedArithmetic.h>
 #include <wtf/DataLog.h>
 #include <wtf/Gigacage.h>
@@ -288,10 +289,10 @@ MemoryHandle::MemoryHandle(void* memory, size_t size, size_t mappedCapacity, Pag
     , m_initial(initial)
     , m_maximum(maximum)
 {
-#if ASSERT_ENABLED
     if (sharingMode == MemorySharingMode::Default && mode == MemoryMode::BoundsChecking)
         ASSERT(mappedCapacity == size);
-#endif
+    else
+        activateSignalingMemory();
 }
 
 MemoryHandle::~MemoryHandle()

Source/WebKit/WebProcess/WebProcess.cpp
@@ -570,10 +570,6 @@ void WebProcess::initializeWebProcess(WebProcessCreationParameters&& parameters)
     ServiceWorkerProvider::setSharedProvider(WebServiceWorkerProvider::singleton());
 #endif
 
-#if ENABLE(WEBASSEMBLY)
-    JSC::Wasm::enableFastMemory();
-#endif
-
 #if ENABLE(INTELLIGENT_TRACKING_PREVENTION) && !RELEASE_LOG_DISABLED
     WebResourceLoadObserver::setShouldLogUserInteraction(parameters.shouldLogUserInteraction);
 #endif