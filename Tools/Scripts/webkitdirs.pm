3cc8f9cb8398b3d7ddb9cc8936e8390edeaf0ad0
Robert Jenner <Jenner@apple.com>
2022-08-06 11:42:10 -0700

    Revert [253165@main] Reland "[GLIB] REGRESSION(250811@main): Undefined subroutine &Test262::Runner::isWindows" https://bugs.webkit.org/show_bug.cgi?id=243454
    Unreviewed build fix.
    
    * Tools/Scripts/do-webcore-rename:
    * Tools/Scripts/make-new-script-test:
    * Tools/Scripts/package-root:
    * Tools/Scripts/run-javascriptcore-tests:
    * Tools/Scripts/run-regexp-tests:
    * Tools/Scripts/run-sunspider:
    * Tools/Scripts/set-webkit-configuration:
    * Tools/Scripts/sunspider-compare-results:
    * Tools/Scripts/test262/Runner.pm:
    * Tools/Scripts/update-iexploder-cssproperties:
    * Tools/Scripts/update-webkit-libs-jhbuild:
    * Tools/Scripts/webkit-build-directory:
    * Tools/Scripts/webkitdirs.pm:
    (XcodeOptions):
    * Tools/Scripts/webkitperl/BuildSubproject.pm:
    * Tools/Scripts/webkitperl/FeatureList.pm:
    (getFeatureOptionList):
    
    Canonical link: https://commits.webkit.org/253181@main

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -29,8 +29,6 @@
 
 # Module to share code to get to WebKit directories.
 
-package webkitdirs;
-
 use strict;
 use version;
 use warnings;
@@ -65,142 +63,42 @@ BEGIN {
        &XcodeOptionString
        &XcodeOptionStringNoConfig
        &XcodeOptions
-       &XcodeSDKPath
        &XcodeStaticAnalyzerOption
        &appDisplayNameFromBundle
        &appendToEnvironmentVariableList
        &archCommandLineArgumentsForRestrictedEnvironmentVariables
-       &architecture
-       &architecturesForProducts
-       &argumentsForConfiguration
-       &asanIsEnabled
        &availableXcodeSDKs
        &baseProductDir
-       &buildCMakeProjectOrExit
-       &buildVisualStudioProject
-       &buildXCodeProject
-       &buildXcodeScheme
-       &builtDylibPathForName
-       &canUseNinja
        &chdirWebKit
-       &checkForArgumentAndRemoveFromARGV
-       &checkForArgumentAndRemoveFromARGVGettingValue
-       &checkForArgumentAndRemoveFromArrayRef
-       &checkForArgumentAndRemoveFromArrayRefGettingValue
        &checkFrameworks
-       &checkRequiredSystemConfig
        &cmakeArgsFromFeatures
-       &configuration
-       &configuredXcodeWorkspace
-       &coverageIsEnabled
-       &currentPerlPath
        &currentSVNRevision
-       &debugMiniBrowser
        &debugSafari
-       &debugWebKitTestRunner
-       &determineCurrentSVNRevision
-       &determineIsWin64
-       &determineXcodeSDK
        &executableProductDir
-       &exitStatus
-       &extractNonMacOSHostConfiguration
-       &forceOptimizationLevel
-       &formatBuildTime
-       &generateBuildSystemFromCMakeProject
-       &getJhbuildPath
-       &getJhbuildModulesetName
-       &inFlatpakSandbox
+       &extractNonHostConfiguration
        &iosVersion
-       &isARM64
-       &isAnyWindows
-       &isAppleCocoaWebKit
-       &isAppleMacWebKit
-       &isAppleWebKit
-       &isAppleWinWebKit
-       &isCMakeBuild
-       &isCygwin
-       &isDebianBased
-       &isFedoraBased
-       &isEmbeddedWebKit
-       &isFTW
-       &isGenerateProjectOnly
-       &isGtk
-       &isIOSWebKit
-       &isInspectorFrontend
-       &isJSCOnly
-       &isLinux
-       &isPlayStation
-       &isWPE
-       &isWinCairo
-       &isWin64
-       &isWindows
-       &isX86_64
-       &jscPath
-       &jscProductDir
-       &launcherName
-       &launcherPath
-       &ltoMode
-       &markBaseProductDirectoryAsCreatedByXcodeBuildSystem
-       &maxCPULoad
-       &nativeArchitecture
        &nmPath
-       &numberOfCPUs
-       &osXVersion
-       &overrideConfiguredXcodeWorkspace
-       &parseAvailableXcodeSDKs
-       &passedArchitecture
        &passedConfiguration
-       &plistPathFromBundle
-       &portName
        &prependToEnvironmentVariableList
        &printHelpAndExitForRunAndDebugWebKitAppIfNeeded
        &productDir
-       &prohibitUnknownPort
-       &relativeScriptsDir
-       &removeCMakeCache
-       &runGitUpdate
        &runIOSWebKitApp
-       &runInFlatpak
-       &runInFlatpakIfAvailable
        &runMacWebKitApp
-       &runMiniBrowser
-       &runSafari
-       &runSvnUpdateAndResolveChangeLogs
-       &runWebKitTestRunner
        &safariPath
        &sdkDirectory
        &sdkPlatformDirectory
-       &setArchitecture
-       &setBaseProductDir
        &setConfiguration
-       &setConfigurationProductDir
-       &setPathForRunningWebKitApp
-       &setUpGuardMallocIfNeeded
-       &setXcodeSDK
-       &setupAppleWinEnv
        &setupMacWebKitEnvironment
        &setupUnixWebKitEnvironment
        &sharedCommandLineOptions
        &sharedCommandLineOptionsUsage
        &shouldUseFlatpak
+       &runInFlatpak
        &sourceDir
-       &splitVersionString
-       &tsanIsEnabled
-       &ubsanIsEnabled
-       &willUseAppleTVDeviceSDK
-       &willUseAppleTVSimulatorSDK
        &willUseIOSDeviceSDK
        &willUseIOSSimulatorSDK
-       &willUseWatchDeviceSDK
-       &willUseWatchSimulatorSDK
-       &winVersion
-       &wrapperPrefixIfNeeded
-       &xcodeSDK
-       &xcodeSDKPlatformName
        DO_NOT_USE_OPEN_COMMAND
-       Mac
        USE_OPEN_COMMAND
-       iOS
    );
    %EXPORT_TAGS = ( );
    @EXPORT_OK   = ();
@@ -1186,7 +1084,7 @@ sub XcodeOptions
     push @options, "ARCHS=$architecture" if $architecture;
     push @options, "SDKROOT=$xcodeSDK" if $xcodeSDK;
 
-    my @features = webkitperl::FeatureList::getFeatureOptionList();
+    my @features = getFeatureOptionList();
     foreach (@features) {
         if (checkForArgumentAndRemoveFromARGV("--no-$_->{option}")) {
             push @options, "$_->{define}=";
