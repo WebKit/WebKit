22e979e0ff27fdc78866c205ff1372b344366289
Andres Gonzalez <andresg_22@apple.com>
2022-08-10 06:40:35 -0700

    AX: WebAccessibilityObjectWrapper action methods should use protectedSelf instead of self. https://bugs.webkit.org/show_bug.cgi?id=243726
    Reviewed by Chris Fleizach.
    
    We are currently capturing both self and protectedSelf unnecessarily. Changed it to capture only protectedSelf and use it in the lambda.
    
    * Source/WebCore/accessibility/mac/WebAccessibilityObjectWrapperMac.mm:
    (-[WebAccessibilityObjectWrapper accessibilityPerformPressAction]):
    (-[WebAccessibilityObjectWrapper accessibilityPerformIncrementAction]):
    (-[WebAccessibilityObjectWrapper accessibilityPerformDecrementAction]):
    
    Canonical link: https://commits.webkit.org/253291@main

--- Source/WebCore/accessibility/mac/WebAccessibilityObjectWrapperMac.mm
+++ Source/WebCore/accessibility/mac/WebAccessibilityObjectWrapperMac.mm
@@ -2892,8 +2892,8 @@ ALLOW_DEPRECATED_DECLARATIONS_BEGIN
 {
     // In case anything we do by performing the press action causes an alert or other modal
     // behaviors, we need to return now, so that VoiceOver doesn't hang indefinitely.
-    RunLoop::main().dispatch([self, protectedSelf = retainPtr(self)] {
-        [self _accessibilityPerformPressAction];
+    RunLoop::main().dispatch([protectedSelf = retainPtr(self)] {
+        [protectedSelf _accessibilityPerformPressAction];
     });
 }
 
@@ -2911,8 +2911,8 @@ ALLOW_DEPRECATED_DECLARATIONS_BEGIN
 
 - (void)accessibilityPerformIncrementAction
 {
-    RunLoop::main().dispatch([self, protectedSelf = retainPtr(self)] {
-        [self _accessibilityPerformIncrementAction];
+    RunLoop::main().dispatch([protectedSelf = retainPtr(self)] {
+        [protectedSelf _accessibilityPerformIncrementAction];
     });
 }
 
@@ -2930,8 +2930,8 @@ ALLOW_DEPRECATED_DECLARATIONS_BEGIN
 
 - (void)accessibilityPerformDecrementAction
 {
-    RunLoop::main().dispatch([self, protectedSelf = retainPtr(self)] {
-        [self _accessibilityPerformDecrementAction];
+    RunLoop::main().dispatch([protectedSelf = retainPtr(self)] {
+        [protectedSelf _accessibilityPerformDecrementAction];
     });
 }
 
