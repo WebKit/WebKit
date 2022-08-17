a7d733519a166f912e7a6aa80d8662aeb30fb352
Chris Dumez <cdumez@apple.com>
2022-08-17 15:40:56 -0700

    REGRESSION(253037@main): [ iOS macOS ] http/tests/workers/service/basic-timeout.https.html is a constant timeout https://bugs.webkit.org/show_bug.cgi?id=244001 <rdar://98739421>
    Reviewed by Youenn Fablet.
    
    After 253037@main, we no longer terminate service workers that do not respond in a timely
    fashion to loads, unless those loads are navigations.
    
    This test was testing the hung service worker termination logic using fetch requests
    and would thus no longer behave as expected after 253037@main. To address the issue,
    I rewrote the test to use iframe navigations instead of fetch requests.
    
    * LayoutTests/http/tests/workers/service/basic-timeout.https-expected.txt:
    * LayoutTests/http/tests/workers/service/basic-timeout.https.html:
    * LayoutTests/http/tests/workers/service/resources/basic-timeout.js:
    (async test.addTestFrame):
    (async test.frameText):
    (async test.testFrame4Loaded):
    (async test.frameLoaded):
    (async test):
    (async test.finishThisTest): Deleted.
    (async test.try): Deleted.
    (async test.try.checkSuccessAgain): Deleted.
    * LayoutTests/platform/glib/TestExpectations:
    * LayoutTests/platform/ios/TestExpectations:
    * LayoutTests/platform/mac-wk2/TestExpectations:
    
    Canonical link: https://commits.webkit.org/253540@main

--- LayoutTests/http/tests/workers/service/resources/basic-timeout.js
+++ LayoutTests/http/tests/workers/service/resources/basic-timeout.js
@@ -1,75 +1,79 @@
 async function test()
 {
-    var urlResults = new Array;
-    var statusResults = new Array;
-    var headerResults = new Array;
+    let frameLoadedCount = 0;
+    const expectedFrameLoadedCount = 3;
 
-    function finishThisTest()
+    function addTestFrame(url, onloadFunction)
     {
-        for (var i = 0; i < urlResults.length; ++i) {
-            log("URL " + i + " - " + urlResults[i]);
-            log("Status code " + i + " - " + statusResults[i]);
-            log("Source' header " + i + " - " + headerResults[i]);
-        }
-        finishSWTest();
+        const iframe = frame.contentDocument.createElement("iframe");
+        iframe.src = url;
+        const iframeTimeoutHandle = setInterval(() => {
+            if (iframe.contentDocument.URL != "" && iframe.contentDocument.URL != "about:blank") {
+                clearTimeout(iframeTimeoutHandle);
+                onloadFunction();
+            }
+        }, 100);
+        iframe.onload = () => {
+            clearTimeout(iframeTimeoutHandle);
+            onloadFunction();
+        };
+        frame.contentDocument.body.appendChild(iframe);
+        return iframe;
     }
-    
+
+    frameText = function(testFrame)
+    {
+        let innerText = testFrame.contentDocument.body.innerText;
+        const newLineIndex = innerText.indexOf('\n');
+        if (newLineIndex > 0)
+            innerText = innerText.slice(0, newLineIndex);
+        return innerText;
+    }
+
+    function testFrame4Loaded()
+    {
+        // Currently goes to the network because the service worker registration is inert.
+        shouldBeEqualToString("frameText(testFrame4)", "Success!");
+
+        frame.remove();
+        finishJSTest();
+    }
+
+    function frameLoaded()
+    {
+        if (++frameLoadedCount < expectedFrameLoadedCount)
+            return;
+
+        shouldBeEqualToString("frameText(testFrame1)", "Timeout resource fetched from network"); // Load should fall back to the network.
+        shouldBeEqualToString("frameText(testFrame2)", "Not Found"); // Load should have went to network and failed with 404.
+        shouldBeEqualToString("frameText(testFrame3)", "Success!"); // Load should fall back to the network.
+
+        // Now we can fetch that same URL again, which *could* relaunch the service worker and handle it there, but for now this service worker registration is inert and fetches through it will go to the network instead.
+        // I'm leaving this in to cover future cases where we do relaunch the SW to handle it.
+        if (window.testRunner)
+            testRunner.setServiceWorkerFetchTimeout(60);
+        testFrame4 = addTestFrame("succeed-fallback-check.py", testFrame4Loaded);
+    }
+
+    description("Tests various navigation time out cases for service workers.");
+    jsTestIsAsync = true;
+ 
     try {
-        var frame = await interceptedFrame("resources/basic-timeout-worker.js", "/workers/service/resources/");
-        var fetch = frame.contentWindow.fetch;
+        frame = await interceptedFrame("resources/basic-timeout-worker.js", "/workers/service/resources/");
 
         if (window.testRunner)
             testRunner.setServiceWorkerFetchTimeout(0);
-        
-        // The following two fetches should time out immediately
-        fetch("timeout-fallback.html").then(function(response) {
-            urlResults[0] = response.url;
-            statusResults[0] = response.status;
-            headerResults[0] = response.headers.get("Source");       
-        }, function(error) {
-            log(error);
-        });
+       
+        // The following two loads should time out immediately.
+        testFrame1 = addTestFrame("timeout-fallback.html", frameLoaded);
+        testFrame2 = addTestFrame("timeout-no-fallback.html", frameLoaded);
 
-        fetch("timeout-no-fallback.html").then(function(response) {
-            urlResults[1] = response.url;
-            statusResults[1] = response.status;
-            headerResults[1] = response.headers.get("Source");   
-        }, function(error) {
-            log(error);
-        });
-
-        if (window.testRunner)
-            testRunner.setServiceWorkerFetchTimeout(60);
-
-        // The service worker knows how to handle the following fetch *and* has 60 seconds to do so.
-        // But will be cancelled with the above fetches since we're terminating the service worker, and 
-        // therefore it will then fallback to the network.
-        fetch("succeed-fallback-check.py").then(function(response) {
-            urlResults[2] = response.url;
-            statusResults[2] = response.status;
-            headerResults[2] = response.headers.get("Source");   
-            setTimeout(checkSuccessAgain, 0);
-        }, function(error) {
-            log(error);
-            finishSWTest();
-        });
-        
-        // Now we can fetch that same URL again, which *could* relaunch the service worker and handle it there, but for now this service worker registration is inert and fetches through it will go to the network instead.
-        // I'm leaving this in to cover future cases where we do relaunch the SW to handle it.
-        function checkSuccessAgain() {
-            fetch("succeed-fallback-check.py").then(function(response) {
-                urlResults[3] = response.url;
-                statusResults[3] = response.status;
-                headerResults[3] = response.headers.get("Source");   
-                finishThisTest();
-            }, function(error) {
-                log(error);
-                finishSWTest();
-            });
-        }
+        // This load would have been properly handled by the service worker but will go to network anyway
+        // because the service worker was hung by the previous loads.
+        testFrame3 = addTestFrame("succeed-fallback-check.py", frameLoaded);
     } catch(e) {
         log("Got exception: " + e);
-        finishSWTest();
+        finishJSTest();
     }
 }
 
