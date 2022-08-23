65b0ecd387162580c9bdaecc4b813e2d30339741
Philippe Normand <philn@igalia.com>
2022-08-23 01:47:28 -0700

    [GStreamer][WebAudio] Misc logging improvements https://bugs.webkit.org/show_bug.cgi?id=244212
    Reviewed by Alicia Boya Garcia.
    
    Associate logs with the corresponding pipeline, which eases debugging when multiple providers are
    created from the same WebProcess.
    
    * Source/WebCore/platform/audio/gstreamer/AudioSourceProviderGStreamer.cpp:
    (WebCore::AudioSourceProviderGStreamer::AudioSourceProviderGStreamer):
    (WebCore::AudioSourceProviderGStreamer::setClient):
    (WebCore::AudioSourceProviderGStreamer::handleNewDeinterleavePad):
    
    Canonical link: https://commits.webkit.org/253674@main

--- Source/WebCore/platform/audio/gstreamer/AudioSourceProviderGStreamer.cpp
+++ Source/WebCore/platform/audio/gstreamer/AudioSourceProviderGStreamer.cpp
@@ -95,6 +95,7 @@ AudioSourceProviderGStreamer::AudioSourceProviderGStreamer(MediaStreamTrackPriva
     initializeDebugCategory();
     auto pipelineName = makeString("WebAudioProvider_MediaStreamTrack_", source.id());
     m_pipeline = gst_element_factory_make("pipeline", pipelineName.utf8().data());
+    GST_DEBUG_OBJECT(m_pipeline.get(), "MediaStream WebAudio provider created");
     auto src = webkitMediaStreamSrcNew();
     webkitMediaStreamSrcAddTrack(WEBKIT_MEDIA_STREAM_SRC(src), &source, true);
 
@@ -252,7 +253,7 @@ void AudioSourceProviderGStreamer::setClient(AudioSourceProviderClient* newClien
     if (client() == newClient)
         return;
 
-    GST_DEBUG("Setting up client %p (previous: %p)", newClient, client());
+    GST_DEBUG_OBJECT(m_pipeline.get(), "Setting up client %p (previous: %p)", newClient, client());
     bool previousClientWasValid = !!m_client;
     m_client = newClient;
 
@@ -340,7 +341,7 @@ void AudioSourceProviderGStreamer::setClient(AudioSourceProviderClient* newClien
 
 void AudioSourceProviderGStreamer::handleNewDeinterleavePad(GstPad* pad)
 {
-    GST_DEBUG("New pad %" GST_PTR_FORMAT, pad);
+    GST_DEBUG_OBJECT(m_pipeline.get(), "New pad %" GST_PTR_FORMAT, pad);
 
     // A new pad for a planar channel was added in deinterleave. Plug
     // in an appsink so we can pull the data from each
