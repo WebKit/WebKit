9bf76051236e38de9795e94bdf8646980b7c0962
Alex Christensen <achristensen@apple.com>
2022-08-30 13:24:51 -0700

    Cherry-pick 94ed12904cda. rdar://problem/99210200
        WKWebpagePreferences._allowPrivacyProxy should apply correctly to main resource loads
        https://bugs.webkit.org/show_bug.cgi?id=244506
        <rdar://99210200>
    
        Reviewed by Brady Eidson.
    
        * Source/WebKit/WebProcess/Network/WebLoaderStrategy.cpp:
        (WebKit::addParametersShared):
        (WebKit::WebLoaderStrategy::scheduleLoadFromNetworkProcess):
    
        Canonical link: https://commits.webkit.org/253956@main
    
    Canonical link: https://commits.webkit.org/252432.533@safari-7614-branch

--- Source/WebKit/WebProcess/Network/WebLoaderStrategy.cpp
+++ Source/WebKit/WebProcess/Network/WebLoaderStrategy.cpp
@@ -299,7 +299,7 @@ bool WebLoaderStrategy::tryLoadingUsingPDFJSHandler(ResourceLoader& resourceLoad
 }
 #endif
 
-static void addParametersShared(const Frame* frame, NetworkResourceLoadParameters& parameters, DocumentLoader* currentDocumentLoader = nullptr)
+static void addParametersShared(const Frame* frame, NetworkResourceLoadParameters& parameters, bool isMainFrameNavigation = false)
 {
     parameters.crossOriginAccessControlCheckEnabled = CrossOriginAccessControlCheckDisabler::singleton().crossOriginAccessControlCheckEnabled();
     parameters.hadMainFrameMainResourcePrivateRelayed = WebProcess::singleton().hadMainFrameMainResourcePrivateRelayed();
@@ -307,12 +307,17 @@ static void addParametersShared(const Frame* frame, NetworkResourceLoadParameter
     if (!frame)
         return;
 
-    // When loading the main frame, we need to get allowPrivacyProxy from currentDocumentLoader.
-    // Otherwise we need to get it from mainFrameDocumentLoader.
+    // When loading the main frame, we need to get allowPrivacyProxy from the same DocumentLoader that
+    // WebFrameLoaderClient::applyToDocumentLoader stored the value on. Otherwise, we need to get the
+    // value from the main frame's current DocumentLoader.
     auto& mainFrame = frame->mainFrame();
-    auto* mainFrameDocumentLoader = mainFrame.document() ? mainFrame.document()->loader() : nullptr;
-    parameters.allowPrivacyProxy = (currentDocumentLoader ? currentDocumentLoader->allowPrivacyProxy() : true)
-    && (mainFrameDocumentLoader ? mainFrameDocumentLoader->allowPrivacyProxy() : true);
+    auto* mainFrameDocumentLoader = mainFrame.loader().policyDocumentLoader();
+    if (!mainFrameDocumentLoader)
+        mainFrameDocumentLoader = mainFrame.loader().provisionalDocumentLoader();
+    if (!mainFrameDocumentLoader || !isMainFrameNavigation)
+        mainFrameDocumentLoader = mainFrame.loader().documentLoader();
+
+    parameters.allowPrivacyProxy = mainFrameDocumentLoader ? mainFrameDocumentLoader->allowPrivacyProxy() : true;
 
     if (auto* document = frame->document())
         parameters.crossOriginEmbedderPolicy = document->crossOriginEmbedderPolicy();
@@ -377,7 +382,8 @@ void WebLoaderStrategy::scheduleLoadFromNetworkProcess(ResourceLoader& resourceL
     loadParameters.maximumBufferingTime = maximumBufferingTime;
     loadParameters.options = resourceLoader.options();
     loadParameters.preflightPolicy = resourceLoader.options().preflightPolicy;
-    addParametersShared(frame, loadParameters, resourceLoader.documentLoader());
+    bool isMainFrameNavigation = resourceLoader.frame() && resourceLoader.frame()->isMainFrame() && resourceLoader.options().mode == FetchOptions::Mode::Navigate;
+    addParametersShared(frame, loadParameters, isMainFrameNavigation);
 
 #if ENABLE(SERVICE_WORKER)
     loadParameters.serviceWorkersMode = resourceLoader.options().loadedFromOpaqueSource == LoadedFromOpaqueSource::No ? resourceLoader.options().serviceWorkersMode : ServiceWorkersMode::None;
@@ -451,7 +457,7 @@ void WebLoaderStrategy::scheduleLoadFromNetworkProcess(ResourceLoader& resourceL
 
     loadParameters.shouldRestrictHTTPResponseAccess = shouldPerformSecurityChecks();
 
-    loadParameters.isMainFrameNavigation = resourceLoader.frame() && resourceLoader.frame()->isMainFrame() && resourceLoader.options().mode == FetchOptions::Mode::Navigate;
+    loadParameters.isMainFrameNavigation = isMainFrameNavigation;
     if (loadParameters.isMainFrameNavigation && document)
         loadParameters.sourceCrossOriginOpenerPolicy = document->crossOriginOpenerPolicy();
 
