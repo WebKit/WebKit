4f487f141e22e39a8d1425c38fff3dcf87bee1ec
Alan Coon <alancoon@apple.com>
2022-09-02 09:33:05 -0700

    Apply patch. rdar://problem/98347913
    Canonical link: https://commits.webkit.org/252432.318@safari-7614-branch

--- Source/WebCore/platform/graphics/GraphicsContextGL.cpp
+++ Source/WebCore/platform/graphics/GraphicsContextGL.cpp
@@ -576,7 +576,7 @@ bool GraphicsContextGL::extractPixelBuffer(const PixelBuffer& pixelBuffer, DataF
     return true;
 }
 
-bool GraphicsContextGL::extractTextureData(unsigned width, unsigned height, GCGLenum format, GCGLenum type, const PixelStoreParams& unpackParams, bool flipY, bool premultiplyAlpha, const void* pixels, Vector<uint8_t>& data)
+bool GraphicsContextGL::extractTextureData(unsigned width, unsigned height, GCGLenum format, GCGLenum type, const PixelStoreParams& unpackParams, bool flipY, bool premultiplyAlpha, GCGLSpan<const GCGLvoid> pixels, Vector<uint8_t>& data)
 {
     // Assumes format, type, etc. have already been validated.
     DataFormat sourceDataFormat = getDataFormat(format, type);
@@ -591,7 +591,7 @@ bool GraphicsContextGL::extractTextureData(unsigned width, unsigned height, GCGL
 
     unsigned imageSizeInBytes, skipSizeInBytes;
     computeImageSizeInBytes(format, type, width, height, 1, unpackParams, &imageSizeInBytes, nullptr, &skipSizeInBytes);
-    const uint8_t* srcData = static_cast<const uint8_t*>(pixels);
+    const uint8_t* srcData = static_cast<const uint8_t*>(pixels.data);
     if (skipSizeInBytes)
         srcData += skipSizeInBytes;
 
