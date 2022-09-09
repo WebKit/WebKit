5beda59c453e52c8f3a287e2bee02de2cecb2824
Mike Wyrzykowski <mwyrzykowski@apple.com>
2022-09-09 15:50:02 -0700

    [media-controls] allow for the fullscreen and picture-in-picture buttons to not show in fullscreen https://bugs.webkit.org/show_bug.cgi?id=242507 <rdar://96618439>
    Reviewed by Jer Noble.
    
    Hide cancel and pip buttons in fullscreen if the current playback element
    already has those buttons.
    
    * Source/WebKit/UIProcess/WebFullScreenManagerProxy.cpp:
    (WebKit::WebFullScreenManagerProxy::isVideoElement const):
    (WebKit::WebFullScreenManagerProxy::enterFullScreen):
    * Source/WebKit/UIProcess/WebFullScreenManagerProxy.h:
    State variable for tracking if this fullscreen mode is a video element.
    
    * Source/WebKit/UIProcess/WebFullScreenManagerProxy.messages.in:
    Pass the boolean in the EnterFullScreen message.
    
    * Source/WebKit/UIProcess/ios/fullscreen/WKFullScreenViewController.h:
    * Source/WebKit/UIProcess/ios/fullscreen/WKFullScreenViewController.mm:
    (-[WKFullScreenViewController initWithWebView:]):
    (-[WKFullScreenViewController videoControlsManagerDidChange]):
    (-[WKFullScreenViewController hideMediaControls:]):
    Hide and show cancel and pip buttons as needed.
    
    * Source/WebKit/UIProcess/ios/fullscreen/WKFullScreenWindowControllerIOS.mm:
    (-[WKFullScreenWindowController enterFullScreen]):
    Hide the pip and cancel buttons if the current media element already contains them.
    
    * Source/WebKit/WebProcess/FullScreen/WebFullScreenManager.cpp:
    (WebKit::WebFullScreenManager::enterFullScreenForElement):
    If the current playback element already has controls then the boolean is true, otherwise false.
    
    * Source/WebKit/WebProcess/InjectedBundle/InjectedBundlePageFullScreenClient.cpp:
    (WebKit::InjectedBundlePageFullScreenClient::enterFullScreenForElement):
    * Source/WebKit/WebProcess/InjectedBundle/InjectedBundlePageFullScreenClient.h:
    Pass along the boolean to the message.
    
    Canonical link: https://commits.webkit.org/254333@main

--- Source/WebKit/WebProcess/FullScreen/WebFullScreenManager.cpp
+++ Source/WebKit/WebProcess/FullScreen/WebFullScreenManager.cpp
@@ -169,13 +169,16 @@ void WebFullScreenManager::enterFullScreenForElement(WebCore::Element* element)
 
     setElement(*element);
 
+    bool isVideoElement = false;
 #if PLATFORM(IOS_FAMILY) || (PLATFORM(MAC) && ENABLE(VIDEO_PRESENTATION_MODE))
-    if (auto* currentPlaybackControlsElement = m_page->playbackSessionManager().currentPlaybackControlsElement())
+    if (auto* currentPlaybackControlsElement = m_page->playbackSessionManager().currentPlaybackControlsElement()) {
         currentPlaybackControlsElement->prepareForVideoFullscreenStandby();
+        isVideoElement = currentPlaybackControlsElement->controls();
+    }
 #endif
 
     m_initialFrame = screenRectOfContents(m_element.get());
-    m_page->injectedBundleFullScreenClient().enterFullScreenForElement(m_page.get(), element, m_element->document().quirks().blocksReturnToFullscreenFromPictureInPictureQuirk());
+    m_page->injectedBundleFullScreenClient().enterFullScreenForElement(m_page.get(), element, m_element->document().quirks().blocksReturnToFullscreenFromPictureInPictureQuirk(), isVideoElement);
 }
 
 void WebFullScreenManager::exitFullScreenForElement(WebCore::Element* element)
