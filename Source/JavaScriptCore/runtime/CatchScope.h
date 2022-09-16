634d22fbdc856a59f02b6527d68a415bf1d7841a
Yusuke Suzuki <ysuzuki@apple.com>
2022-09-16 01:14:56 -0700

    [JSC] Microtask should not care about exceptions https://bugs.webkit.org/show_bug.cgi?id=245199 rdar://99237677
    Reviewed by Mark Lam.
    
    Microtask execution should be done without exception, and any exceptions thrown can be cleared when running microtasks.
    We clear exceptions extensively for all cases, plus, we early return if termination exception is already thrown.
    
    * Source/JavaScriptCore/runtime/JSMicrotask.cpp:
    (JSC::runJSMicrotask):
    
    Canonical link: https://commits.webkit.org/254546@main

--- Source/JavaScriptCore/runtime/CatchScope.h
+++ Source/JavaScriptCore/runtime/CatchScope.h
@@ -45,6 +45,13 @@ public:
     JS_EXPORT_PRIVATE ~CatchScope();
 
     void clearException() { m_vm.clearException(); }
+    bool clearExceptionExceptTermination()
+    {
+        if (UNLIKELY(m_vm.hasPendingTerminationException()))
+            return false;
+        m_vm.clearException();
+        return true;
+    }
 };
 
 #define DECLARE_CATCH_SCOPE(vm__) \
@@ -61,6 +68,13 @@ public:
     CatchScope(CatchScope&&) = default;
 
     ALWAYS_INLINE void clearException() { m_vm.clearException(); }
+    ALWAYS_INLINE bool clearExceptionExceptTermination()
+    {
+        if (UNLIKELY(m_vm.hasPendingTerminationException()))
+            return false;
+        m_vm.clearException();
+        return true;
+    }
 };
 
 #define DECLARE_CATCH_SCOPE(vm__) \
