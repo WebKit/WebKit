39adafe71286c0110cc27632502ce2ad576da6c2
Xabier Rodriguez-Calvar <calvaris@igalia.com>
2022-09-16 12:49:00 +0200

    [MSE][GStreamer] Append pipeline should accept out of segment buffers https://bugs.webkit.org/show_bug.cgi?id=245272
    Reviewed by NOBODY (OOPS!).
    
    In complex streams (combining commercials and content, for example) the demuxer
    could output out of clip segments that will be corrected with timestamp offset
    later.
    
    * Source/WebCore/platform/graphics/gstreamer/mse/AppendPipeline.cpp:
    (WebCore::AppendPipeline::Track::initializeElements):

--- Source/WebCore/platform/graphics/gstreamer/mse/AppendPipeline.cpp
+++ Source/WebCore/platform/graphics/gstreamer/mse/AppendPipeline.cpp
@@ -859,6 +859,7 @@ void AppendPipeline::Track::initializeElements(AppendPipeline* appendPipeline, G
     gst_base_sink_set_async_enabled(GST_BASE_SINK(appsink.get()), FALSE); // No prerolls, no async state changes.
     gst_base_sink_set_drop_out_of_segment(GST_BASE_SINK(appsink.get()), FALSE);
     gst_base_sink_set_last_sample_enabled(GST_BASE_SINK(appsink.get()), FALSE);
+    gst_base_sink_set_drop_out_of_segment(GST_BASE_SINK(appsink.get()), FALSE);
 
     gst_bin_add(GST_BIN(appendPipeline->pipeline()), appsink.get());
     gst_element_sync_state_with_parent(appsink.get());
