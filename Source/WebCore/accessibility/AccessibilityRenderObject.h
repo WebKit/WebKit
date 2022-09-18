f0398a49b15ecfcc733a038aa8767ccc2a358c02
Tyler Wilcock <tyler_w@apple.com>
2022-09-18 15:20:12 -0700

    Cherry-pick 61b68425a2b4. rdar://problem/100055122
        AX: Focus state cannot be read or updated for AccessibilityNodeObjects
        https://bugs.webkit.org/show_bug.cgi?id=245308
        rdar://problem/100055122
    
        Reviewed by Chris Fleizach.
    
        AccessibilityRenderObject has implementations of `isFocused` and
        `setFocused`, but AccessibilityNodeObject does not. Because these
        implementations don't actually require a renderer, only a node, this
        patch moves them to AccessibilityNodeObject.
    
        This allows AX clients to read and update the focus state of
        AccessibilityNodeObjects.
    
        * LayoutTests/accessibility/basic-focusability-expected.txt: Added.
        * LayoutTests/accessibility/basic-focusability.html: Added.
        * LayoutTests/platform/glib/TestExpectations: Disable new test.
        * LayoutTests/platform/ios/TestExpectations: Enable new test.
        * LayoutTests/platform/win/TestExpectations: Disable new test.
        * Source/WebCore/accessibility/AccessibilityNodeObject.cpp:
        (WebCore::AccessibilityNodeObject::isFocused const):
        (WebCore::AccessibilityNodeObject::setFocused):
        * Source/WebCore/accessibility/AccessibilityNodeObject.h:
        * Source/WebCore/accessibility/AccessibilityRenderObject.cpp:
        (WebCore::AccessibilityRenderObject::isFocused const): Deleted.
        (WebCore::AccessibilityRenderObject::setFocused): Deleted.
        * Source/WebCore/accessibility/AccessibilityRenderObject.h:
        * Source/WebCore/accessibility/ios/WebAccessibilityObjectWrapperIOS.mm:
        (-[WebAccessibilityObjectWrapper _accessibilityIsFocusedForTesting]):
        * Tools/WebKitTestRunner/InjectedBundle/ios/AccessibilityUIElementIOS.mm:
        (WTR::AccessibilityUIElement::isFocused const):
    
        Canonical link: https://commits.webkit.org/254618@main
    
    Canonical link: https://commits.webkit.org/252432.425@safari-7614-branch

--- Source/WebCore/accessibility/AccessibilityRenderObject.h
+++ Source/WebCore/accessibility/AccessibilityRenderObject.h
@@ -58,7 +58,6 @@ public:
     
     bool isAttachment() const override;
     bool isSelected() const override;
-    bool isFocused() const override;
     bool isLoaded() const override;
     bool isOffScreen() const override;
     bool isUnvisited() const override;
@@ -134,11 +133,10 @@ public:
     Widget* widgetForAttachmentView() const override;
     AccessibilityChildrenVector documentLinks() override;
     FrameView* documentFrameView() const override;
-
+    
     void clearChildren() override;
     void updateChildrenIfNecessary() override;
     
-    void setFocused(bool) override;
     void setSelectedTextRange(const PlainTextRange&) override;
     bool setValue(const String&) override;
     void setSelectedRows(AccessibilityChildrenVector&) override;
