8cdd7d631bd4af28ac1135be960b5554f45419f2
Sihui Liu <sihui_liu@apple.com>
2022-09-19 21:51:11 -0700

    Do not launch network process for sending WebCacheStorageConnection::reference and WebCacheStorageConnection::dereference https://bugs.webkit.org/show_bug.cgi?id=245370 <rdar://100132550>
    Reviewed by Youenn Fablet.
    
    The newly launched network process will have no information about cache identifiers used in previous network process, so
    there is no point sending these messages.
    
    * Source/WebKit/WebProcess/Cache/WebCacheStorageConnection.cpp:
    (WebKit::WebCacheStorageConnection::open):
    (WebKit::WebCacheStorageConnection::reference):
    (WebKit::WebCacheStorageConnection::dereference):
    (WebKit::WebCacheStorageConnection::networkProcessConnectionClosed):
    * Source/WebKit/WebProcess/Cache/WebCacheStorageConnection.h:
    * Source/WebKit/WebProcess/Cache/WebCacheStorageProvider.cpp:
    (WebKit::WebCacheStorageProvider::networkProcessConnectionClosed):
    * Source/WebKit/WebProcess/Cache/WebCacheStorageProvider.h:
    * Source/WebKit/WebProcess/WebProcess.cpp:
    (WebKit::WebProcess::networkProcessConnectionClosed):
    
    Canonical link: https://commits.webkit.org/254660@main

--- Source/WebKit/WebProcess/Cache/WebCacheStorageProvider.h
+++ Source/WebKit/WebProcess/Cache/WebCacheStorageProvider.h
@@ -41,6 +41,7 @@ public:
     static Ref<WebCacheStorageProvider> create() { return adoptRef(*new WebCacheStorageProvider); }
 
     Ref<WebCore::CacheStorageConnection> createCacheStorageConnection() final;
+    void networkProcessConnectionClosed();
 
 private:
     WebCacheStorageProvider() = default;
