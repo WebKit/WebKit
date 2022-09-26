e362e6b6f47fb0f8682e81bf696c11c6a04406bf
Philippe Normand <philn@igalia.com>
2022-09-26 01:54:16 -0700

    Merge 254851@main - [GLib] Modernize Tools/glib/apply-build-revision-to-files.py https://bugs.webkit.org/show_bug.cgi?id=242191 <rdar://problem/96245384>
    Reviewed by Michael Catanzaro.
    
    Switch back to git log parsing, as before 252012@main. Using webkitpy was not a great idea for two
    reasons:
    
    - autoinstall kicking off during offline builds and failing
    - SSL issues in buildroot setups.
    
    * Tools/glib/apply-build-revision-to-files.py:
    (get_revision_from_most_recent_git_commit):
    (get_build_revision):
    (main):
    
    Canonical link: https://commits.webkit.org/254851@main
    
    (cherry picked from commit 792d6325aedb3d164f2f4a2d139523b0f7ffab02)

--- Tools/glib/apply-build-revision-to-files.py
+++ Tools/glib/apply-build-revision-to-files.py
@@ -16,29 +16,52 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+import os
 import sys
 from pathlib import Path
+import subprocess
+try:
+    from urllib.parse import urlparse  # pylint: disable=E0611
+except ImportError:
+    from urlparse import urlparse
 
 WEBKIT_TOP_LEVEL = Path(__file__).parent.parent.parent.resolve()
-sys.path.insert(0, str(Path(WEBKIT_TOP_LEVEL.joinpath("Tools", "Scripts"))))
 
-import webkitpy  # noqa
-from webkitscmpy import local  # noqa
 
+def get_revision_from_most_recent_git_commit():
+    with open(os.devnull, 'w') as devnull:
+        try:
+            commit_message = subprocess.check_output(("git", "log", "-1", "--pretty=%B", "origin/HEAD"), stderr=devnull)
+        except subprocess.CalledProcessError:
+            # This may happen with shallow checkouts whose HEAD has been
+            # modified; there is no origin reference anymore, and git
+            # will fail - let's pretend that this is not a repo at all
+            return None
+
+        # Commit messages tend to be huge and the metadata we're looking
+        # for is at the very end. Also a spoofed 'Canonical link' mention
+        # could appear early on. So make sure we get the right metadata by
+        # reversing the contents. And this is a micro-optimization as well.
+        for line in reversed(commit_message.splitlines()):
+            parsed = line.split(b':')
+            key = parsed[0]
+            contents = b':'.join(parsed[1:])
+            if key == b'Canonical link':
+                url = contents.decode('utf-8').strip()
+                revision = urlparse(url).path[1:]  # strip leading /
+                return revision
+    return None
 
 def get_build_revision():
-    try:
-        repository = local.Scm.from_path(str(WEBKIT_TOP_LEVEL), contributors=None)
-        return str(repository.find("HEAD", include_log=False))
-    except (local.Scm.Exception, TypeError, OSError, KeyError):
-        # This may happen with shallow checkouts whose HEAD has been
-        # modified; there is no origin reference anymore, and git
-        # will fail - let's pretend that this is not a repo at all
-        return "unknown"
+    git_path = str(WEBKIT_TOP_LEVEL.joinpath('.git'))
+    # In case of git worktrees the .git is a file.
+    if os.path.isdir(git_path) or os.path.isfile(git_path):
+        return get_revision_from_most_recent_git_commit()
 
+    return None
 
 def main(args):
-    build_revision = get_build_revision()
+    build_revision = get_build_revision() or "unknown"
 
     for in_file in args:
         file = Path(in_file)
