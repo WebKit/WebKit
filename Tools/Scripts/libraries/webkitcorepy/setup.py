5f93f28b7e67e491cdac0f84a6761a94e8c1eb56
Jonathan Bedard <jbedard@apple.com>
2022-09-28 15:35:07 -0700

    Cherry-pick 254954@main (cde78ac0d823). rdar://problem/100482618
        [webkitcorepy] Provide default argument for Environment
        https://bugs.webkit.org/show_bug.cgi?id=245759
        <rdar://100482618>
    
        Reviewed by Dewei Zhu.
    
        * Tools/Scripts/libraries/webkitcorepy/setup.py: Bump version.
        * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/__init__.py: Ditto.
        * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/environment.py:
        (Environment.get): Allow for a default argument.
    
        Canonical link: https://commits.webkit.org/254954@main
    
    Canonical link: https://commits.webkit.org/252432.493@safari-7614-branch

--- Tools/Scripts/libraries/webkitcorepy/setup.py
+++ Tools/Scripts/libraries/webkitcorepy/setup.py
@@ -30,7 +30,7 @@ def readme():
 
 setup(
     name='webkitcorepy',
-    version='0.13.14',
+    version='0.13.15',
     description='Library containing various Python support classes and functions.',
     long_description=readme(),
     classifiers=[
