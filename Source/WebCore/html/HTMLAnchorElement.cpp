99e8a6f34e14ecb081e3d096341b01010b223f14
Carlos Garcia Campos <cgarcia@igalia.com>
2022-09-30 02:20:04 -0700

    Merge 255024@main - Do not try to preconnect on link click when link preconnect setting is disabled https://bugs.webkit.org/show_bug.cgi?id=245576
    Reviewed by Michael Catanzaro.
    
    * Source/WebCore/html/HTMLAnchorElement.cpp:
    (WebCore::HTMLAnchorElement::handleClick):
    
    Canonical link: https://commits.webkit.org/255024@main
    
    (cherry picked from commit 49f6854f09b547620cb5378709aef532f639d714)

--- Source/WebCore/html/HTMLAnchorElement.cpp
+++ Source/WebCore/html/HTMLAnchorElement.cpp
@@ -617,7 +617,7 @@ void HTMLAnchorElement::handleClick(Event& event)
     sendPings(completedURL);
 
     // Preconnect to the link's target for improved page load time.
-    if (completedURL.protocolIsInHTTPFamily() && ((frame->isMainFrame() && isSelfTargetFrameName(effectiveTarget)) || isBlankTargetFrameName(effectiveTarget))) {
+    if (completedURL.protocolIsInHTTPFamily() && document().settings().linkPreconnectEnabled() && ((frame->isMainFrame() && isSelfTargetFrameName(effectiveTarget)) || isBlankTargetFrameName(effectiveTarget))) {
         auto storageCredentialsPolicy = frame->page() && frame->page()->canUseCredentialStorage() ? StoredCredentialsPolicy::Use : StoredCredentialsPolicy::DoNotUse;
         platformStrategies()->loaderStrategy()->preconnectTo(frame->loader(), completedURL, storageCredentialsPolicy, LoaderStrategy::ShouldPreconnectAsFirstParty::Yes, nullptr);
     }
