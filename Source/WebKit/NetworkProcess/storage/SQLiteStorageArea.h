0297f1253a4f4da904d242be3a70a4ba3cd9d44c
Sihui Liu <sihui_liu@apple.com>
2022-09-30 13:35:52 -0700

    REGRESSION (iOS 16): LocalStorage writes fail after ~2.5MB https://bugs.webkit.org/show_bug.cgi?id=245479 <rdar://100252023>
    Reviewed by Chris Dumez.
    
    We set the maximum size of LocalStorage database to be 5MB, because quota is 5MB. But SQLite has overhead, which means
    the file size can reach 5MB when the stored content is much smaller than that. To avoid this issue, now we let
    SQLiteStorageArea keep track of the actual content size and use that size to do quota computation, instead of setting
    hard limit on database file.
    
    * Source/WebKit/NetworkProcess/storage/SQLiteStorageArea.cpp:
    (WebKit::SQLiteStorageArea::isEmpty):
    (WebKit::SQLiteStorageArea::prepareDatabase):
    (WebKit::SQLiteStorageArea::startTransactionIfNecessary):
    (WebKit::SQLiteStorageArea::getItem):
    (WebKit::SQLiteStorageArea::getAllItemsFromDatabase):
    (WebKit::SQLiteStorageArea::initializeCache):
    (WebKit::SQLiteStorageArea::allItems):
    (WebKit::SQLiteStorageArea::setItem):
    (WebKit::SQLiteStorageArea::removeItem):
    (WebKit::SQLiteStorageArea::clear):
    * Source/WebKit/NetworkProcess/storage/SQLiteStorageArea.h:
    
    Canonical link: https://commits.webkit.org/255044@main

--- Source/WebKit/NetworkProcess/storage/SQLiteStorageArea.h
+++ Source/WebKit/NetworkProcess/storage/SQLiteStorageArea.h
@@ -69,13 +69,19 @@ private:
     WebCore::SQLiteStatementAutoResetScope cachedStatement(StatementType);
     Expected<String, StorageError> getItem(const String& key);
     Expected<String, StorageError> getItemFromDatabase(const String& key);
+    Expected<HashMap<String, String>, StorageError> getAllItemsFromDatabase();
+    void initializeCache(const HashMap<String, String>&);
 
     String m_path;
     Ref<WorkQueue> m_queue;
     std::unique_ptr<WebCore::SQLiteDatabase> m_database;
     std::unique_ptr<WebCore::SQLiteTransaction> m_transaction;
     Vector<std::unique_ptr<WebCore::SQLiteStatement>> m_cachedStatements;
-    std::optional<HashMap<String, String>> m_cache;
+    struct Cache {
+        HashMap<String, String> items;
+        uint64_t size { 0 };
+    };
+    std::optional<Cache> m_cache;
 };
 
 } // namespace WebKit
