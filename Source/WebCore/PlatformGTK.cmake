c946126c05094d5e671b28f955290e717952aaf9
Philippe Normand <philn@igalia.com>
2022-10-03 08:53:05 -0700

    [GStreamer][WebRTC] Require OpenSSL 3 https://bugs.webkit.org/show_bug.cgi?id=245150
    Reviewed by Žan Doberšek.
    
    OpenSSL 3 is now required for the GstWebRTC backend, mostly for licensing reasons. The backend code
    was ported to OpenSSL 3 as well. For now the compatibility with older OpenSSL remains but it will be
    removed later on, since it's now officially dead code.
    
    * Source/WebCore/Modules/mediastream/gstreamer/GStreamerWebRTCUtils.cpp:
    (WebCore::x509Serialize):
    (WebCore::privateKeySerialize):
    (WebCore::generateCertificate):
    * Source/WebCore/PlatformGTK.cmake:
    * Source/WebCore/PlatformWPE.cmake:
    * Source/cmake/GStreamerChecks.cmake:
    
    Canonical link: https://commits.webkit.org/255088@main

--- Source/WebCore/PlatformGTK.cmake
+++ Source/WebCore/PlatformGTK.cmake
@@ -15,6 +15,7 @@ list(APPEND WebCore_UNIFIED_SOURCE_LIST_FILES
 
 list(APPEND WebCore_PRIVATE_INCLUDE_DIRECTORIES
     "${WEBCORE_DIR}/accessibility/atspi"
+    "${WEBCORE_DIR}/crypto/openssl"
     "${WEBCORE_DIR}/page/gtk"
     "${WEBCORE_DIR}/platform/adwaita"
     "${WEBCORE_DIR}/platform/audio/glib"
