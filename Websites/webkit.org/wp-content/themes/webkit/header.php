6de86cf915b187b9df0fd8102b9d833d2d665959
Jonathan Davis <jond@apple.com>
2022-10-06 10:12:48 -0700

    Address search input and search result layout issues https://bugs.webkit.org/show_bug.cgi?id=246126
    Reviewed by Devin Rousso.
    
    * Websites/webkit.org/wp-content/themes/webkit/404.php:
    * Websites/webkit.org/wp-content/themes/webkit/header.php:
    * Websites/webkit.org/wp-content/themes/webkit/style.css:
    (.search-input):
    (header .search-input):
    (header .search-input:focus,):
    (@media only screen and (max-width: 1015px) header .search-input,):
    (@media only screen and (max-width: 1015px) header .search-input:focus,):
    (@media only screen and (max-width: 690px) .search-results h1):
    (@media only screen and (max-width: 690px) .search-results h1,):
    (#site-nav .search-input): Deleted.
    (.search-input:focus,): Deleted.
    (@media only screen and (max-width: 1015px) #site-nav .search-input,): Deleted.
    (@media only screen and (max-width: 1015px) .search-input:focus,): Deleted.
    (@media only screen and (max-width: 1015px) .search-results h1,): Deleted.
    
    Canonical link: https://commits.webkit.org/255225@main

--- Websites/webkit.org/wp-content/themes/webkit/header.php
+++ Websites/webkit.org/wp-content/themes/webkit/header.php
@@ -11,7 +11,7 @@
 
     <meta name="application-name" content="WebKit">
 
-    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>?20221005" media="all">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>?2022100501" media="all">
     <link rel="stylesheet" href="https://www.apple.com/wss/fonts?families=SF+Pro,v1" type="text/css">
     <link rel="stylesheet" href="https://www.apple.com/wss/fonts?families=SF+Mono,v2" type="text/css">
     <meta name="supported-color-schemes" content="light dark">
