WebKit/WebKit

commit 3395d79137499cc61964d7e5abd34031bc4977cc

AX: Expose suggestion, insertion, deletion roles and attributes https://bugs.webkit.org/show_bug.cgi?id=242401
Reviewed by Andres Gonzalez.

At present, screenreaders on iOS and macOS do not convey information related to elements
with an insertion, deletion, or suggestion role (or, alternatively, `<ins>` and `<del>`) besides their content.

To appropriately convey the proper annotation information and purpose, insertions, deletions,
and suggestions are now exposed via new attributes and with new methods on the Web Accessibility
 Wrapper for iOS. This allows screenreaders to appropriately label the start and end positions
 of these elements.

* LayoutTests/accessibility/ios-simulator/suggestion-insertion-deletion-attributes-expected.txt: Added.
* LayoutTests/accessibility/ios-simulator/suggestion-insertion-deletion-attributes.html: Added.
* LayoutTests/accessibility/mac/attributed-string-includes-insertion-deletion-expected.txt: Added.
* LayoutTests/accessibility/mac/attributed-string-includes-insertion-deletion.html: Added.
* LayoutTests/accessibility/mac/suggestion-subrole-and-roledescription-expected.txt: Added.
* LayoutTests/accessibility/mac/suggestion-subrole-and-roledescription.html: Added.

Tests to check for the correct subrole and attributes.

* Source/WebCore/accessibility/AccessibilityNodeObject.cpp:
(WebCore::AccessibilityNodeObject::text const):

Exposes inner content of static text methods.

* Source/WebCore/accessibility/AccessibilityObject.cpp:
(WebCore::initializeRoleMap):
(WebCore::AccessibilityObject::roleDescription const):
* Source/WebCore/accessibility/AccessibilityObjectInterface.h:
(WebCore::accessibilityRoleToString):
* Source/WebCore/accessibility/mac/AccessibilityObjectMac.mm:
(WebCore::AccessibilityObject::subrolePlatformString const):
(WebCore::Accessibility::createPlatformRoleMap):

Adds new mappings for "suggestion" role so they are recognized as roles.

* Source/WebCore/accessibility/ios/WebAccessibilityObjectWrapperIOS.mm:

Source/WebCore/accessibility/AccessibilityNodeObject.cpp
@@ -2223,6 +2223,9 @@ String AccessibilityNodeObject::text() const
             return textOrder[0].text;
     }
 
+    if (isStaticText())
+        return textUnderElement();
+    
     if (!isTextControl())
         return String();
 

Source/WebCore/accessibility/mac/WebAccessibilityObjectWrapperMac.mm
@@ -51,6 +51,7 @@
 #import "ContextMenuController.h"
 #import "Editing.h"
 #import "Editor.h"
+#import "ElementInlines.h"
 #import "Font.h"
 #import "FontCascade.h"
 #import "Frame.h"
@@ -860,6 +861,15 @@ static void AXAttributeStringSetStyle(NSMutableAttributedString* attrString, Ren
     for (Node* node = renderer->node(); node; node = node->parentNode()) {
         if (node->hasTagName(HTMLNames::markTag))
             AXAttributeStringSetNumber(attrString, @"AXHighlight", @YES, range);
+        if (auto* element = dynamicDowncast<Element>(*node)) {
+            auto& roleValue = element->attributeWithoutSynchronization(HTMLNames::roleAttr);
+            if (equalLettersIgnoringASCIICase(roleValue, "insertion"_s))
+                AXAttributeStringSetNumber(attrString, @"AXIsSuggestedInsertion", @YES, range);
+            else if (equalLettersIgnoringASCIICase(roleValue, "deletion"_s))
+                AXAttributeStringSetNumber(attrString, @"AXIsSuggestedDeletion", @YES, range);
+            else if (equalLettersIgnoringASCIICase(roleValue, "suggestion"_s))
+                AXAttributeStringSetNumber(attrString, @"AXIsSuggestion", @YES, range);
+        }
     }
 }
 
@@ -993,7 +1003,8 @@ static void AXAttributedStringAppendText(NSMutableAttributedString* attrString, 
     NSRange attrStringRange = NSMakeRange([attrString length], text.length());
     
     // append the string from this node
-    [[attrString mutableString] appendString:text.createNSStringWithoutCopying().get()];
+    NSAttributedString *stringToAppend = adoptNS([[NSAttributedString alloc] initWithString:text.createNSStringWithoutCopying().get()]).autorelease();
+    [attrString appendAttributedString:stringToAppend];
     
     // add new attributes and remove irrelevant inherited ones
     // NOTE: color attributes are handled specially because -[NSMutableAttributedString addAttribute: value: range:] does not merge
@@ -4025,6 +4036,11 @@ ALLOW_DEPRECATED_IMPLEMENTATIONS_END
         }
     }
 
+    if (backingObject->isTextControl() || backingObject->isStaticText()) {
+        if ([attribute isEqualToString:(NSString *)kAXAttributedStringForRangeParameterizedAttribute])
+            return rangeSet ? [self doAXAttributedStringForRange:range] : nil;
+    }
+
     if (backingObject->isTextControl()) {
         if ([attribute isEqualToString: (NSString *)kAXLineForIndexParameterizedAttribute]) {
             int lineNumber = backingObject->doAXLineForIndex([number intValue]);
@@ -4067,9 +4083,6 @@ ALLOW_DEPRECATED_IMPLEMENTATIONS_END
 
         if ([attribute isEqualToString: (NSString*)kAXRTFForRangeParameterizedAttribute])
             return rangeSet ? [self doAXRTFForRange:range] : nil;
-
-        if ([attribute isEqualToString: (NSString*)kAXAttributedStringForRangeParameterizedAttribute])
-            return rangeSet ? [self doAXAttributedStringForRange:range] : nil;
 
         if ([attribute isEqualToString: (NSString*)kAXStyleRangeForIndexParameterizedAttribute]) {
             PlainTextRange textRange = backingObject->doAXStyleRangeForIndex([number intValue]);

LayoutTests/accessibility/ios-simulator/suggestion-insertion-deletion-attributes-expected.txt
@@ -1,0 +1,19 @@
+This test makes sure that suggestions, insertions, and deletions expose the right attributes.
+PASS: ins1.isInsertion === true
+PASS: ins1.isFirstItemInSuggestion === true
+PASS: ins1.isLastItemInSuggestion === false
+PASS: del1.isDeletion === true
+PASS: del1.isLastItemInSuggestion === true
+PASS: del1.isFirstItemInSuggestion === false
+PASS: ins2.isInsertion === true
+PASS: ins2.isFirstItemInSuggestion === true
+PASS: ins3.isInsertion === true
+PASS: ins2.isLastItemInSuggestion === false
+PASS: del2.isLastItemInSuggestion === true
+PASS: ins3.isFirstItemInSuggestion === false
+
+PASS successfullyParsed is true
+
+TEST COMPLETE
+inserted textdeleted text
+inserted textdeleted text

LayoutTests/accessibility/ios-simulator/suggestion-insertion-deletion-attributes.html
@@ -1,0 +1,46 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<script src="../../resources/js-test.js"></script>
+<script src="../../resources/accessibility-helper.js"></script>
+</head>
+<body>
+
+<div id="sug1" role="suggestion"><span id="ins1" role="insertion">inserted text</span><span id="del1" role="deletion">deleted text</span></div>
+
+<div id="sug2" role="suggestion"><span id="ins2" role="insertion"><b>inserted</b> text</span><span id="del2" role="deletion">deleted text</span></div>
+
+
+<script>
+
+    if (window.accessibilityController) {
+
+        let output = "This test makes sure that suggestions, insertions, and deletions expose the right attributes.\n";
+
+        var ins1 = accessibilityController.accessibleElementById("ins1").childAtIndex(0);
+        output += expect("ins1.isInsertion", "true");
+        output += expect("ins1.isFirstItemInSuggestion", "true");
+        output += expect("ins1.isLastItemInSuggestion", "false");	
+
+        var del1 = accessibilityController.accessibleElementById("del1").childAtIndex(0);
+        output += expect("del1.isDeletion", "true"); 
+        output += expect("del1.isLastItemInSuggestion", "true");
+        output += expect("del1.isFirstItemInSuggestion", "false");
+		
+        var ins2 = accessibilityController.accessibleElementById("ins2").childAtIndex(0);
+        var ins3 = accessibilityController.accessibleElementById("ins2").childAtIndex(1);
+        var del2 = accessibilityController.accessibleElementById("del2").childAtIndex(0);
+        output += expect("ins2.isInsertion", "true");
+        output += expect("ins2.isFirstItemInSuggestion", "true");
+        output += expect("ins3.isInsertion", "true");
+        output += expect("ins2.isLastItemInSuggestion", "false");
+        output += expect("del2.isLastItemInSuggestion", "true");
+        output += expect("ins3.isFirstItemInSuggestion", "false");
+	
+        debug(output);
+    }
+
+</script>
+
+</body>
+</html>

LayoutTests/accessibility/mac/attributed-string-includes-insertion-deletion-expected.txt
@@ -1,0 +1,11 @@
+This verifies that using the insertion and deletion roles adds the attribute to the attributed string.
+PASS: item1.attributedStringForTextMarkerRangeContainsAttribute('AXIsSuggestedInsertion', markerRange) === true
+PASS: item2.attributedStringForTextMarkerRangeContainsAttribute('AXIsSuggestedDeletion', markerRange) === true
+PASS: item3.attributedStringForTextMarkerRangeContainsAttribute('AXIsSuggestion', markerRange) === true
+
+PASS successfullyParsed is true
+
+TEST COMPLETE
+inserted text
+deleted text
+hellohi

LayoutTests/accessibility/mac/attributed-string-includes-insertion-deletion.html
@@ -1,0 +1,34 @@
+
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<script src="../../resources/js-test.js"></script>
+<script src="../../resources/accessibility-helper.js"></script>
+</head>
+<body id="body" role="group">
+
+<div id="item1"><span role="insertion">inserted text</span></div>
+<div id="item2"><span role="deletion">deleted text</span></div>
+<div id="item3"><span role="suggestion"><span role="insertion">hello</span><span role="deletion">hi</span></span></div>
+
+<script>
+    if (window.accessibilityController) {
+        let output = "This verifies that using the insertion and deletion roles adds the attribute to the attributed string.\n";
+		
+        var item1 = accessibilityController.accessibleElementById("item1");
+        var markerRange = item1.textMarkerRangeForElement(item1);
+        output += expect("item1.attributedStringForTextMarkerRangeContainsAttribute('AXIsSuggestedInsertion', markerRange)", "true");
+
+        var item2 = accessibilityController.accessibleElementById("item2");
+        var markerRange = item2.textMarkerRangeForElement(item2);
+        output += expect("item2.attributedStringForTextMarkerRangeContainsAttribute('AXIsSuggestedDeletion', markerRange)", "true");
+
+        var item3 = accessibilityController.accessibleElementById("item3");
+        var markerRange = item3.textMarkerRangeForElement(item3);
+        output += expect("item3.attributedStringForTextMarkerRangeContainsAttribute('AXIsSuggestion', markerRange)", "true");
+        
+        debug(output);
+    }
+</script>
+</body>
+</html>

LayoutTests/accessibility/mac/suggestion-subrole-and-roledescription-expected.txt
@@ -1,0 +1,17 @@
+This tests that suggestion, insertion, and deletion elements have the right subroles.
+PASS: suggestion1.role === 'AXRole: AXGroup'
+PASS: suggestion1.subrole === 'AXSubrole: AXSuggestion'
+PASS: suggestion1.roleDescription === 'AXRoleDescription: suggestion'
+PASS: ins1.subrole === 'AXSubrole: AXInsertStyleGroup'
+PASS: ins2.subrole === 'AXSubrole: AXInsertStyleGroup'
+PASS: del1.subrole === 'AXSubrole: AXDeleteStyleGroup'
+PASS: del2.subrole === 'AXSubrole: AXDeleteStyleGroup'
+
+PASS successfullyParsed is true
+
+TEST COMPLETE
+This is some test to insert and delete
+this is inserted text.
+This is also inserted text
+This is deleted text
+this is also deleted text

LayoutTests/accessibility/mac/suggestion-subrole-and-roledescription.html
@@ -1,0 +1,43 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<script src="../../resources/js-test.js"></script>
+<script src="../../resources/accessibility-helper.js"></script>
+</head>
+<body id="body">
+
+<div id="suggestion1" role="suggestion">This is some test to <span role="insertion">insert</span> and <span role="deletion">delete</span></div>
+
+<div id="ins1" role="insertion">this is inserted text.</div>
+<ins id="ins2">This is also inserted text</ins>
+
+<div id="del1" role="deletion">This is deleted text</div>
+<del id="del2">this is also deleted text</div>
+
+<script>
+    if (window.accessibilityController) {
+        let output = "This tests that suggestion, insertion, and deletion elements have the right subroles.\n";
+
+        var suggestion1 = accessibilityController.accessibleElementById("suggestion1");
+        var ins1 = accessibilityController.accessibleElementById("ins1");
+        var ins2 = accessibilityController.accessibleElementById("ins2");         
+        var del1 = accessibilityController.accessibleElementById("del1");
+        var del2 = accessibilityController.accessibleElementById("del2");
+
+        output += expect("suggestion1.role", "'AXRole: AXGroup'");
+        output += expect("suggestion1.subrole", "'AXSubrole: AXSuggestion'");
+        output += expect("suggestion1.roleDescription", "'AXRoleDescription: suggestion'");
+
+        output += expect("ins1.subrole", "'AXSubrole: AXInsertStyleGroup'");
+        output += expect("ins2.subrole", "'AXSubrole: AXInsertStyleGroup'");
+
+        output += expect("del1.subrole", "'AXSubrole: AXDeleteStyleGroup'");
+        output += expect("del2.subrole", "'AXSubrole: AXDeleteStyleGroup'");
+        
+        debug(output);
+    }
+
+</script>
+
+</body>
+</html>

commit 206f33adf1e2d2257f48f6059260138fcaf7f027

Cherry-pick 8089baaa58f2. rdar://problem/100855009
    AX: VoiceOver double reads list markers, doesn't output braille for list items
    https://bugs.webkit.org/show_bug.cgi?id=246188
    rdar://100855009

    Reviewed by Chris Fleizach.

    This is a regression from:

    https://github.com/WebKit/WebKit/commit/3395d79137499cc61964d7e5abd34031bc4977cc
    (AX: Expose suggestion, insertion, deletion roles and attributes)

    That commit allowed static text elements to return attributed strings.
    Due to some quirks in VoiceOver (and also a different bug in
    WebKit), this causes issues that will need to be addressed at a later time.

    This patch effectively reverts the Mac portion of that commit by
    removing the code that allowed AccessibilityRole::StaticText elements
    to return attributed strings.

    * Source/WebCore/accessibility/AccessibilityNodeObject.cpp:
    (WebCore::AccessibilityNodeObject::text const):
    Stop allowing isStaticText() elements to return textUnderElement.
    * Source/WebCore/accessibility/mac/WebAccessibilityObjectWrapperMac.mm:
    (-[WebAccessibilityObjectWrapper accessibilityAttributeValue:forParameter:]):
    Stop allowing isStaticText() elements to doAXAttributedStringForRange.

    Canonical link: https://commits.webkit.org/255276@main

Canonical link: https://commits.webkit.org/252432.482@safari-7614.2.9.1-branch

Source/WebCore/accessibility/AccessibilityNodeObject.cpp
@@ -2220,9 +2220,6 @@ String AccessibilityNodeObject::text() const
             return textOrder[0].text;
     }
 
-    if (isStaticText())
-        return textUnderElement();
-    
     if (!isTextControl())
         return String();
 

Source/WebCore/accessibility/mac/WebAccessibilityObjectWrapperMac.mm
@@ -4022,11 +4022,6 @@ ALLOW_DEPRECATED_IMPLEMENTATIONS_END
         }
     }
 
-    if (backingObject->isTextControl() || backingObject->isStaticText()) {
-        if ([attribute isEqualToString:(NSString *)kAXAttributedStringForRangeParameterizedAttribute])
-            return rangeSet ? [self doAXAttributedStringForRange:range] : nil;
-    }
-
     if (backingObject->isTextControl()) {
         if ([attribute isEqualToString: (NSString *)kAXLineForIndexParameterizedAttribute]) {
             int lineNumber = backingObject->doAXLineForIndex([number intValue]);
@@ -4069,6 +4064,9 @@ ALLOW_DEPRECATED_IMPLEMENTATIONS_END
 
         if ([attribute isEqualToString: (NSString*)kAXRTFForRangeParameterizedAttribute])
             return rangeSet ? [self doAXRTFForRange:range] : nil;
+
+        if ([attribute isEqualToString:(NSString *)kAXAttributedStringForRangeParameterizedAttribute])
+            return rangeSet ? [self doAXAttributedStringForRange:range] : nil;
 
         if ([attribute isEqualToString: (NSString*)kAXStyleRangeForIndexParameterizedAttribute]) {
             PlainTextRange textRange = backingObject->doAXStyleRangeForIndex([number intValue]);