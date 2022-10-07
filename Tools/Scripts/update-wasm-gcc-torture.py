b35f8c18b7b3edf8aba6402fd5c8026dc2f5742b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-07 17:15:37 +0000

    Adding tarfile member sanitization to extractall()

--- Tools/Scripts/update-wasm-gcc-torture.py
+++ Tools/Scripts/update-wasm-gcc-torture.py
@@ -86,7 +86,26 @@ def untar_torture(args):
             shutil.rmtree(TORTURE_DIR)
         with tarfile.open(torture_file, 'r') as torture_tar:
             print('Extracting: %s -> %s' % (torture_file, TORTURE_DIR))
-            torture_tar.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(torture_tar)
     assert os.path.isdir(TORTURE_DIR)
 
 
