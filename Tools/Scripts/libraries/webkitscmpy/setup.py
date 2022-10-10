e4aabac19906ebbfa2cbcdcab1af2957562aabb8
Jonathan Bedard <jbedard@apple.com>
2022-10-10 13:36:40 -0700

    Cherry-pick 255245@main (00b8477c7f93). rdar://100412099
        [git-webkit] Pull branch name from commit message
        https://bugs.webkit.org/show_bug.cgi?id=245675
        <rdar://problem/100412099>
    
        Reviewed by Dewei Zhu.
    
        * Tools/Scripts/libraries/webkitscmpy/setup.py: Bump version.
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py: Ditto.
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/commit.py:
        (Commit.issues): Parse the commit message for issues.
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/pull_request.py:
        (PullRequest.title_for): Handle case where commit message is empty.
        (PullRequest.pull_request_branch_point): Extract the associated issue from the commit message of a local
        commit, if possible.
        (PullRequest.is_revert_commit): Handle case where commit message is empty.
        (PullRequest.add_comment_to_reverted_commit_bug_tracker): Use Commit object's issues list.
        (PullRequest.create_pull_request): Ditto.
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/commit_unittest.py:
        (TestCommit):
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/pull_request_unittest.py:
    
        Canonical link: https://commits.webkit.org/255245@main
    
    Canonical link: https://commits.webkit.org/252432.517@safari-7614-branch

--- Tools/Scripts/libraries/webkitscmpy/setup.py
+++ Tools/Scripts/libraries/webkitscmpy/setup.py
@@ -29,7 +29,7 @@ def readme():
 
 setup(
     name='webkitscmpy',
-    version='5.6.13',
+    version='5.6.14',
     description='Library designed to interact with git and svn repositories.',
     long_description=readme(),
     classifiers=[
