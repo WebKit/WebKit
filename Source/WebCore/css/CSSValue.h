143afebb28dd58d9f67f082f9c6967924f8389e7
Michael Catanzaro <mcatanzaro@redhat.com>
2022-10-12 10:09:16 -0700

    -Wfree-nonheap-object and -Wuse-after-free from CSSValue.h https://bugs.webkit.org/show_bug.cgi?id=246292
    Reviewed by Darin Adler.
    
    Suppress false-positive warnings. It seems GCC is getting confused by
    immortal refcounted CSSValues and doesn't realize they can never be
    freed.
    
    * Source/WebCore/css/CSSValue.h:
    (WebCore::CSSValue::deref const):
    
    Canonical link: https://commits.webkit.org/255439@main

--- Source/WebCore/css/CSSValue.h
+++ Source/WebCore/css/CSSValue.h
@@ -253,11 +253,17 @@ private:
 
 inline void CSSValue::deref() const
 {
+IGNORE_GCC_WARNINGS_BEGIN("use-after-free")
     unsigned tempRefCount = m_refCount - refCountIncrement;
+IGNORE_GCC_WARNINGS_END
+
     if (!tempRefCount) {
+IGNORE_GCC_WARNINGS_BEGIN("free-nonheap-object")
         delete this;
+IGNORE_GCC_WARNINGS_END
         return;
     }
+
     m_refCount = tempRefCount;
 }
 
