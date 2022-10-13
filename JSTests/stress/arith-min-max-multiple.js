9cad1e4f79ecc36ce49fbf876105e09ec3082a74
Yusuke Suzuki <ysuzuki@apple.com>
2022-10-12 19:43:57 -0700

    Cherry-pick 6e034bbd2683. rdar://problem/101048572
        [JSC] Fix ArithMin/ArithMax handling for double, and Int32 speculation
        https://bugs.webkit.org/show_bug.cgi?id=246422
        rdar://101048572
    
        Reviewed by Justin Michaud.
    
        This patch fixes two issues: one is just a bug fix and one is a bit theoretical.
    
        1. FTL ArithMin/ArithMax double case is skipping the first parameter accidentally. This patch fixes it.
        2. DFG ArithMin/ArithMax Int32 case can OSR exit after clobbering first parameter's register, which can theoretically
           puts different output if type speculation failed in the second or later parameters.
    
        * JSTests/stress/arith-min-max-multiple.js:
        (test7):
        (test8):
        * Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp:
        * Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp:
        (JSC::FTL::DFG::LowerDFGToB3::compileArithMinOrMax):
    
        Canonical link: https://commits.webkit.org/255465@main
    
    Canonical link: https://commits.webkit.org/255303.2@safari-7615.1.9-branch

--- JSTests/stress/arith-min-max-multiple.js
+++ JSTests/stress/arith-min-max-multiple.js
@@ -39,6 +39,18 @@ function test6(num)
 }
 noInline(test6);
 
+function test7(num)
+{
+    return Math.max(44.3, 0.2, 0.3, -1.3, 2.5, num);
+}
+noInline(test7);
+
+function test8(num)
+{
+    return Math.min(-44.3, 0.2, 0.3, -1.3, 2.5, num);
+}
+noInline(test8);
+
 for (let i = 0; i < 1e5; ++i) {
     shouldBe(test1(0), -4);
     shouldBe(test1(-100), -100);
@@ -50,4 +62,6 @@ for (let i = 0; i < 1e5; ++i) {
     shouldBe(test4(1e1000), 1e1000);
     shouldBe(Number.isNaN(test5(0)), true);
     shouldBe(Number.isNaN(test6(2000.1)), true);
+    shouldBe(test7(10), 44.3);
+    shouldBe(test8(10), -44.3);
 }
