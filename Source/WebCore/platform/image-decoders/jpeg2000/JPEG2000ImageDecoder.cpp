9a76ccc77f6e2708ffc18b371b5c0fe0a703489c
Adrian Perez de Castro <aperez@igalia.com>
2022-10-18 07:33:07 -0700

    [GLib] JPEG2000 produces warning about maybe uninitialized variable https://bugs.webkit.org/show_bug.cgi?id=246248
    Reviewed by Darin Adler.
    
    * Source/WebCore/platform/image-decoders/jpeg2000/JPEG2000ImageDecoder.cpp:
    (WebCore::JPEG2000ImageDecoder::decode): Initialize alpha value to 0xFF.
    
    Canonical link: https://commits.webkit.org/255674@main

--- Source/WebCore/platform/image-decoders/jpeg2000/JPEG2000ImageDecoder.cpp
+++ Source/WebCore/platform/image-decoders/jpeg2000/JPEG2000ImageDecoder.cpp
@@ -503,7 +503,7 @@ void JPEG2000ImageDecoder::decode(bool onlySize, bool allDataReceived)
     const IntRect& frameRect = buffer.backingStore()->frameRect();
     for (int y = 0; y < frameRect.height(); ++y) {
         for (int x = 0; x < frameRect.width(); ++x) {
-            int r, g, b, a;
+            int r, g, b, a = 0xFF;
 
             int offset;
             if (subsampling) {
