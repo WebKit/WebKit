5a02a23fcd7776eb83e24787354df6daf79a79cc
Said Abou-Hallawa <said@apple.com>
2022-10-20 14:27:59 -0700

    Add the class FilterTargetSwitcher https://bugs.webkit.org/show_bug.cgi?id=246822
    Reviewed by Simon Fraser.
    
    This class will manage applying a filter to a certain area of drawings with the
    minimum changes in the existing code. All it needs is a set of callbacks in the
    existing caller class. And the caller adds a line like this to the drawing
    function:
    
    	FilterTargetSwitcher targetSwitcher(*this, bounds);
    
    The constructor of FilterTargetSwitcher will ask the client if there a filter to
    apply or not. If there is a filter, it will switch the drawing to a temporary
    ImageBuffer. The client has to make sure all the drawing code from now on will
    use the context of this ImageBuffer. The destructor will apply the filter to the
    temporary ImageBuffer and then composite filtered ImageBuffer back to the client
    drawing context.
    
    * Source/WebCore/Sources.txt:
    * Source/WebCore/WebCore.xcodeproj/project.pbxproj:
    * Source/WebCore/platform/graphics/filters/FilterTargetSwitcher.cpp: Added.
    (WebCore::FilterTargetSwitcher::FilterTargetSwitcher):
    (WebCore::FilterTargetSwitcher::~FilterTargetSwitcher):
    (WebCore::FilterTargetSwitcher::drawingContext const):
    (WebCore::FilterTargetSwitcher::outsets const):
    * Source/WebCore/platform/graphics/filters/FilterTargetSwitcher.h: Added.
    
    Canonical link: https://commits.webkit.org/255802@main

---
+++ Source/WebCore/platform/graphics/filters/FilterTargetSwitcher.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2022 Apple Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "DestinationColorSpace.h"
+#include "FloatRect.h"
+
+namespace WebCore {
+
+class Filter;
+class GraphicsContext;
+class ImageBuffer;
+
+class FilterTargetSwitcher {
+public:
+    class Client {
+    public:
+        virtual ~Client() = default;
+        virtual RefPtr<Filter> createFilter(const std::function<FloatRect()>& boundsProvider) const = 0;
+        virtual IntOutsets calculateFilterOutsets(const FloatRect& bounds) const = 0;
+        virtual GraphicsContext* drawingContext() const = 0;
+        virtual void setTargetSwitcher(FilterTargetSwitcher*) = 0;
+    protected:
+        Client() = default;
+    };
+
+    FilterTargetSwitcher(Client&, const DestinationColorSpace&, const std::function<FloatRect()>& boundsProvider);
+    FilterTargetSwitcher(Client&, const DestinationColorSpace&, const FloatRect& bounds);
+    ~FilterTargetSwitcher();
+    GraphicsContext* drawingContext() const;
+    FloatBoxExtent outsets() const;
+
+private:
+    Client& m_client;
+    RefPtr<Filter> m_filter;
+    RefPtr<ImageBuffer> m_imageBuffer;
+    FloatRect m_bounds;
+};
+
+} // namespace WebCore
