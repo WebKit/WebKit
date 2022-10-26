d93ba4173c655a8e8e37e429cf787e7484dfc89b
Jonathan Bedard <jbedard@apple.com>
2022-10-26 08:54:11 -0700

    Cherry-pick 256014@main (28f6affda692). rdar://101568509
        [git-webkit] Deeply inspect potential gardening commits
        https://bugs.webkit.org/show_bug.cgi?id=247034
        rdar://101568509
    
        Reviewed by Elliott Williams.
    
        * Tools/Scripts/libraries/webkitscmpy/setup.py: Bump version.
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py: Ditto.
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/pickable.py:
        (Pickable.Filters):
        (Pickable.Filters.fuzzy): Move fuzzy_filter into scoping class.
        (Pickable.Filters.gardening): If a commit matches a gardening pattern, inspect the files
        changed to see if they are exclusively testing files.
        (Pickable.pickable): Pass commit objects into callable filters.
        (fuzzy_filter): Renamed Pickable.Filters.fuzzy.
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/pickable_unittest.py:
        (TestPickable.test_branch_gardening_exclude):
        (TestPickable.test_branch_gardening_include):
        * metadata/git_config_extension: Add API and layout test paths.
    
        Canonical link: https://commits.webkit.org/256014@main
    
    Canonical link: https://commits.webkit.org/252432.685@safari-7614-branch

--- Tools/Scripts/libraries/webkitscmpy/setup.py
+++ Tools/Scripts/libraries/webkitscmpy/setup.py
@@ -29,7 +29,7 @@ def readme():
 
 setup(
     name='webkitscmpy',
-    version='5.7.2',
+    version='5.7.3',
     description='Library designed to interact with git and svn repositories.',
     long_description=readme(),
     classifiers=[
