fb4340875878fb1d8010bc5151999336ec2a1a30
Patrick Angle <pangle@apple.com>
2022-11-04 12:48:07 -0700

    Cherry-pick c46be5c1183b. rdar://problem/102218310
        WebDriver: [Cocoa] Regression(255359@main) Exception when deleting remote automation session
        https://bugs.webkit.org/show_bug.cgi?id=247384
        rdar://101872145
    
        Reviewed by Wenson Hsieh.
    
        Revert the changes in 255359@main and instead apply a more targeted approach to fixing that specific issue. Previous
        attempts to fix this with more state management in `WKWindowVisibilityObserver` created more issues, so a more targeted
        fix it is!
    
        * Source/WebKit/UIProcess/mac/WebViewImpl.h:
        * Source/WebKit/UIProcess/mac/WebViewImpl.mm:
        (-[WKWindowVisibilityObserver startObserving:]):
        (-[WKWindowVisibilityObserver stopObserving:]):
        (WebKit::WebViewImpl::viewWillMoveToWindowImpl):
        (WebKit::WebViewImpl::viewWillMoveToWindow):
        (WebKit::WebViewImpl::prepareForMoveToWindow):
        (-[WKWindowVisibilityObserver _observeWindow:]): Deleted.
    
        Canonical link: https://commits.webkit.org/256334@main
    
    Canonical link: https://commits.webkit.org/256138.29@safari-7615.1.12.110-branch

--- Source/WebKit/UIProcess/mac/WebViewImpl.mm
+++ Source/WebKit/UIProcess/mac/WebViewImpl.mm
@@ -270,7 +270,6 @@ WTF_DECLARE_CF_TYPE_TRAIT(CGImage);
 @implementation WKWindowVisibilityObserver {
     NSView *_view;
     WebKit::WebViewImpl *_impl;
-    __weak NSWindow *_observedWindow;
 
     BOOL _didRegisterForLookupPopoverCloseNotifications;
     BOOL _shouldObserveFontPanel;
@@ -333,15 +332,14 @@ static void* keyValueObservingContext = &keyValueObservingContext;
     if (_shouldObserveFontPanel)
         [self startObservingFontPanel];
 
-    [self _observeWindow:window];
+    [window addObserver:self forKeyPath:@"contentLayoutRect" options:NSKeyValueObservingOptionInitial context:keyValueObservingContext];
+    [window addObserver:self forKeyPath:@"titlebarAppearsTransparent" options:NSKeyValueObservingOptionInitial context:keyValueObservingContext];
 }
 
 - (void)stopObserving:(NSWindow *)window
 {
     if (!window)
         return;
-
-    ASSERT_IMPLIES(_observedWindow, _observedWindow == window);
 
     NSNotificationCenter *defaultNotificationCenter = [NSNotificationCenter defaultCenter];
 
@@ -364,25 +362,8 @@ static void* keyValueObservingContext = &keyValueObservingContext;
     if (_shouldObserveFontPanel)
         [[NSFontPanel sharedFontPanel] removeObserver:self forKeyPath:@"visible" context:keyValueObservingContext];
 
-    [self _observeWindow:nil];
-}
-
-- (void)_observeWindow:(NSWindow *)window
-{
-    if (_observedWindow == window)
-        return;
-
-    if (_observedWindow) {
-        [_observedWindow removeObserver:self forKeyPath:@"contentLayoutRect" context:keyValueObservingContext];
-        [_observedWindow removeObserver:self forKeyPath:@"titlebarAppearsTransparent" context:keyValueObservingContext];
-    }
-
-    _observedWindow = window;
-
-    if (window) {
-        [window addObserver:self forKeyPath:@"contentLayoutRect" options:NSKeyValueObservingOptionInitial context:keyValueObservingContext];
-        [window addObserver:self forKeyPath:@"titlebarAppearsTransparent" options:NSKeyValueObservingOptionInitial context:keyValueObservingContext];
-    }
+    [window removeObserver:self forKeyPath:@"contentLayoutRect" context:keyValueObservingContext];
+    [window removeObserver:self forKeyPath:@"titlebarAppearsTransparent" context:keyValueObservingContext];
 }
 
 - (void)startObservingFontPanel
@@ -2128,7 +2109,7 @@ bool WebViewImpl::windowResizeMouseLocationIsInVisibleScrollerThumb(CGPoint poin
 void WebViewImpl::viewWillMoveToWindowImpl(NSWindow *window)
 {
     // If we're in the middle of preparing to move to a window, we should only be moved to that window.
-    ASSERT(!m_targetWindowForMovePreparation || (m_targetWindowForMovePreparation == window));
+    ASSERT_IMPLIES(m_targetWindowForMovePreparation, m_targetWindowForMovePreparation == window);
 
     NSWindow *currentWindow = [m_view window];
     if (window == currentWindow)
@@ -2136,9 +2117,11 @@ void WebViewImpl::viewWillMoveToWindowImpl(NSWindow *window)
 
     clearAllEditCommands();
 
-    NSWindow *stopObservingWindow = m_targetWindowForMovePreparation ? m_targetWindowForMovePreparation.get() : [m_view window];
-    [m_windowVisibilityObserver stopObserving:stopObservingWindow];
-    [m_windowVisibilityObserver startObserving:window];
+    if (!m_isPreparingToUnparentView) {
+        NSWindow *stopObservingWindow = m_targetWindowForMovePreparation.get() ?: [m_view window];
+        [m_windowVisibilityObserver stopObserving:stopObservingWindow];
+        [m_windowVisibilityObserver startObserving:window];
+    }
 
 #if HAVE(NSSCROLLVIEW_SEPARATOR_TRACKING_ADAPTER)
     if (m_isRegisteredScrollViewSeparatorTrackingAdapter) {
@@ -2152,6 +2135,7 @@ void WebViewImpl::viewWillMoveToWindow(NSWindow *window)
 {
     viewWillMoveToWindowImpl(window);
     m_targetWindowForMovePreparation = nil;
+    m_isPreparingToUnparentView = false;
 }
 
 void WebViewImpl::viewDidMoveToWindow()
@@ -2408,6 +2392,7 @@ void WebViewImpl::prepareForMoveToWindow(NSWindow *targetWindow, WTF::Function<v
 {
     m_shouldDeferViewInWindowChanges = true;
     viewWillMoveToWindowImpl(targetWindow);
+    m_isPreparingToUnparentView = !targetWindow;
     m_targetWindowForMovePreparation = targetWindow;
     viewDidMoveToWindow();
 
