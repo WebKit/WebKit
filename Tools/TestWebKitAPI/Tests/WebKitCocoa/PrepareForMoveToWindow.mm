806cf13219e90a91999a920e472c1e03fee96a34
Harrison Weinerman <hweinerman@apple.com>
2022-11-04 08:28:19 -0700

    Revert 69059cf.
    Canonical link: https://commits.webkit.org/255891.13@safari-7615.1.11-branch

--- Tools/TestWebKitAPI/Tests/WebKitCocoa/PrepareForMoveToWindow.mm
+++ Tools/TestWebKitAPI/Tests/WebKitCocoa/PrepareForMoveToWindow.mm
@@ -32,7 +32,6 @@
 #import "PlatformUtilities.h"
 #import "Test.h"
 #import "TestNavigationDelegate.h"
-#import "TestWKWebView.h"
 #import <WebKit/WKWebViewPrivate.h>
 #import <wtf/RetainPtr.h>
 #import <wtf/WeakObjCPtr.h>
@@ -56,19 +55,6 @@ TEST(WKWebView, PrepareForMoveToWindow)
     TestWebKitAPI::Util::run(&isDone);
 }
 
-TEST(WKWebView, PrepareToUnparentView)
-{
-    auto webView = adoptNS([[TestWKWebView alloc] initWithFrame:NSMakeRect(0, 0, 800, 600)]);
-    [webView synchronouslyLoadTestPageNamed:@"simple"];
-
-    __block bool done = false;
-    [webView _prepareForMoveToWindow:nil completionHandler:^{
-        [webView removeFromSuperview];
-        done = true;
-    }];
-
-    TestWebKitAPI::Util::run(&done);
-}
 
 TEST(WKWebView, PrepareForMoveToWindowThenClose)
 {
