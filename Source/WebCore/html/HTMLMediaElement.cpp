7026e87a1b8e647125a63303da6bc3c66257cd6a
Jer Noble <jer.noble@apple.com>
2022-11-07 08:46:02 -0800

    Cherry-pick 256408@main (7ff585820d47). https://bugs.webkit.org/show_bug.cgi?id=247549
        Apple.com/apple-events: Unable to enable subtitles on first watch of the keynote
        https://bugs.webkit.org/show_bug.cgi?id=247549
        rdar://101785175
    
        Reviewed by Eric Carlson.
    
        Two interlocking bugs prevent captions from being visible if tracks are added after the video is
        loaded: 1) `m_processingPreferenceChange` is set to `true` in
        `markCaptionAndSubtitleTracksAsUnconfigured()` and is only set to `false` if at least one text
        track is present and therefore `configureTextTrackGroup()` is called. 2) the MediaControlsHost is
        not notified that the video element's videoBox has changed, as it's only notified during style
        updates, and will not necessarily have a RenderVideo attached at that point.
    
        Clear `m_processingPreferenceChange` unconditionally at the end of `configureTextTracks()`. And call
        the MediaControlsHost's `updateCaptionDisplaySizes()` when the media element is resized.
    
        * LayoutTests/media/track/video-track-add-visible-expected.txt: Added.
        * LayoutTests/media/track/video-track-add-visible.html: Added.
        * Source/WebCore/html/HTMLMediaElement.cpp:
        (WebCore::HTMLMediaElement::configureTextTrackGroup):
        (WebCore::HTMLMediaElement::layoutSizeChanged):
        (WebCore::HTMLMediaElement::configureTextTracks):
    
        Canonical link: https://commits.webkit.org/256408@main

--- Source/WebCore/html/HTMLMediaElement.cpp
+++ Source/WebCore/html/HTMLMediaElement.cpp
@@ -4604,8 +4604,6 @@ void HTMLMediaElement::configureTextTrackGroup(const TrackGroup& group)
         if (!webkitClosedCaptionsVisible() && closedCaptionsVisible() && displayMode == CaptionUserPreferences::AlwaysOn)
             m_webkitLegacyClosedCaptionOverride = true;
     }
-
-    m_processingPreferenceChange = false;
 }
 
 static JSC::JSValue controllerJSValue(JSC::JSGlobalObject& lexicalGlobalObject, JSDOMGlobalObject& globalObject, HTMLMediaElement& media)
@@ -4725,6 +4723,9 @@ void HTMLMediaElement::layoutSizeChanged()
     auto task = [this] {
         if (auto root = userAgentShadowRoot())
             root->dispatchEvent(Event::create(eventNames().resizeEvent, Event::CanBubble::No, Event::IsCancelable::No));
+
+        if (m_mediaControlsHost)
+            m_mediaControlsHost->updateCaptionDisplaySizes();
     };
     queueTaskKeepingObjectAlive(*this, TaskSource::MediaElement, WTFMove(task));
 
@@ -4863,6 +4864,8 @@ void HTMLMediaElement::configureTextTracks()
         configureTextTrackGroup(metadataTracks);
     if (otherTracks.tracks.size())
         configureTextTrackGroup(otherTracks);
+
+    m_processingPreferenceChange = false;
 
     updateCaptionContainer();
     configureTextTrackDisplay();
