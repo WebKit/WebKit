00c2dde4cbe92cb880cad0fc06b766493c69c902
Aditya Keerthi <akeerthi@apple.com>
2022-11-07 17:02:35 -0800

    Cherry-pick 82deae87c0ac. rdar://problem/102218535
        Crash when displaying autofill buttons
        https://bugs.webkit.org/show_bug.cgi?id=247589
        rdar://101938935
    
        Reviewed by Wenson Hsieh.
    
        Speculative revert of 255229@main, as crashes have been observed when using
        autofill buttons, following that change.
    
        An alternate approach will need to be taken to fix webkit.org/b/245976.
    
        * LayoutTests/fast/forms/auto-fill-button/show-auto-fill-button-while-selection-inside-field-expected.txt: Removed.
        * LayoutTests/fast/forms/auto-fill-button/show-auto-fill-button-while-selection-inside-field.html: Removed.
        * Source/WebCore/html/HTMLTextFormControlElement.h:
        * Source/WebCore/html/TextFieldInputType.cpp:
        (WebCore::TextFieldInputType::createDataListDropdownIndicator):
        (WebCore::TextFieldInputType::createContainer):
        * Source/WebCore/html/TextFieldInputType.h:
    
        Canonical link: https://commits.webkit.org/256431@main
    
    Canonical link: https://commits.webkit.org/256138.46@safari-7615.1.12-branch

--- Source/WebCore/html/TextFieldInputType.cpp
+++ Source/WebCore/html/TextFieldInputType.cpp
@@ -486,7 +486,7 @@ void TextFieldInputType::createDataListDropdownIndicator()
 {
     ASSERT(!m_dataListDropdownIndicator);
     if (!m_container)
-        createContainer(PreserveSelectionRange::Yes);
+        createContainer();
 
     ScriptDisallowedScope::EventAllowedScope allowedScope(*m_container);
     m_dataListDropdownIndicator = DataListButtonElement::create(element()->document(), *this);
@@ -822,7 +822,7 @@ void TextFieldInputType::autoFillButtonElementWasClicked()
     page->chrome().client().handleAutoFillButtonClick(*element());
 }
 
-void TextFieldInputType::createContainer(PreserveSelectionRange preserveSelection)
+void TextFieldInputType::createContainer()
 {
     ASSERT(!m_container);
     ASSERT(element());
@@ -831,11 +831,6 @@ void TextFieldInputType::createContainer(PreserveSelectionRange preserveSelectio
 
     ScriptDisallowedScope::EventAllowedScope allowedScope(*element()->userAgentShadowRoot());
 
-    // FIXME: <https://webkit.org/b/245977> Suppress selectionchange events during subtree modification.
-    std::optional<std::tuple<unsigned, unsigned, TextFieldSelectionDirection>> selectionState;
-    if (preserveSelection == PreserveSelectionRange::Yes && enclosingTextFormControl(element()->document().selection().selection().start()) == element())
-        selectionState = { element()->selectionStart(), element()->selectionEnd(), element()->computeSelectionDirection() };
-
     m_container = TextControlInnerContainer::create(element()->document());
     element()->userAgentShadowRoot()->appendChild(*m_container);
     m_container->setPseudo(ShadowPseudoIds::webkitTextfieldDecorationContainer());
@@ -843,11 +838,6 @@ void TextFieldInputType::createContainer(PreserveSelectionRange preserveSelectio
     m_innerBlock = TextControlInnerElement::create(element()->document());
     m_container->appendChild(*m_innerBlock);
     m_innerBlock->appendChild(*m_innerText);
-
-    if (selectionState) {
-        auto [selectionStart, selectionEnd, selectionDirection] = *selectionState;
-        element()->setSelectionRange(selectionStart, selectionEnd, selectionDirection);
-    }
 }
 
 void TextFieldInputType::createAutoFillButton(AutoFillButtonType autoFillButtonType)
@@ -878,7 +868,7 @@ void TextFieldInputType::updateAutoFillButton()
 
     if (shouldDrawAutoFillButton()) {
         if (!m_container)
-            createContainer(PreserveSelectionRange::Yes);
+            createContainer();
 
         AutoFillButtonType autoFillButtonType = element()->autoFillButtonType();
         if (!m_autoFillButton)
