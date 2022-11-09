32b9728139794e15d690f5a3b43dbd9f8162e6ee
David Degazio <d_degazio@apple.com>
2022-11-09 14:30:14 -0800

    Segfault in JSC::IdentifierArena::makeBigIntDecimalIdentifier https://bugs.webkit.org/show_bug.cgi?id=247644 rdar://98566429
    Reviewed by Mark Lam and Yusuke Suzuki.
    
    We currently get a segfault because the parser for bigdecimal identifiers allocates a JSBigInt, which
    might cause us to run out of memory. The parser doesn't throw arbitrary exceptions elsewhere, so instead
    of throwing out-of-memory as an exception, it just produces an empty JSBigInt and crashes when using it.
    This patch addresses the issue by making the result of makeBigIntDecimalIdentifier nullable, checking for
    it in the parser, and failing with a SyntaxError if the identifier could not be created.
    
    * JSTests/stress/bigdecimal-identifiers-fail-on-oom.js: Added.
    (foo):
    * Source/JavaScriptCore/parser/Lexer.cpp:
    * Source/JavaScriptCore/parser/Parser.cpp:
    (JSC::Parser<LexerType>::parseDestructuringPattern):
    (JSC::Parser<LexerType>::parseClass):
    (JSC::Parser<LexerType>::parseClassFieldInitializerSourceElements):
    (JSC::Parser<LexerType>::parseProperty):
    (JSC::Parser<LexerType>::parseGetterSetter):
    * Source/JavaScriptCore/parser/ParserArena.cpp:
    (JSC::IdentifierArena::makeBigIntDecimalIdentifier):
    * Source/JavaScriptCore/parser/ParserArena.h:
    
    Canonical link: https://commits.webkit.org/256501@main

--- Source/JavaScriptCore/parser/Parser.cpp
+++ Source/JavaScriptCore/parser/Parser.cpp
@@ -1316,7 +1316,8 @@ template <class TreeBuilder> TreeDestructuringPattern Parser<LexerType>::parseDe
                     wasString = true;
                     break;
                 case BIGINT:
-                    propertyName = &m_parserArena.identifierArena().makeBigIntDecimalIdentifier(const_cast<VM&>(m_vm), *m_token.m_data.bigIntString, m_token.m_data.radix);
+                    propertyName = m_parserArena.identifierArena().makeBigIntDecimalIdentifier(const_cast<VM&>(m_vm), *m_token.m_data.bigIntString, m_token.m_data.radix);
+                    failIfFalse(propertyName, "Cannot parse big int property name");
                     break;
                 case OPENBRACKET:
                     next();
@@ -3054,8 +3055,8 @@ parseMethod:
             next();
             break;
         case BIGINT:
-            ident = &m_parserArena.identifierArena().makeBigIntDecimalIdentifier(const_cast<VM&>(m_vm), *m_token.m_data.bigIntString, m_token.m_data.radix);
-            ASSERT(ident);
+            ident = m_parserArena.identifierArena().makeBigIntDecimalIdentifier(const_cast<VM&>(m_vm), *m_token.m_data.bigIntString, m_token.m_data.radix);
+            failIfFalse(ident, "Cannot parse big int property name");
             next();
             break;
         case ESCAPED_KEYWORD:
@@ -3316,8 +3317,8 @@ template <class TreeBuilder> TreeSourceElements Parser<LexerType>::parseClassFie
                 next();
                 break;
             case BIGINT:
-                ident = &m_parserArena.identifierArena().makeBigIntDecimalIdentifier(const_cast<VM&>(m_vm), *m_token.m_data.bigIntString, m_token.m_data.radix);
-                ASSERT(ident);
+                ident = m_parserArena.identifierArena().makeBigIntDecimalIdentifier(const_cast<VM&>(m_vm), *m_token.m_data.bigIntString, m_token.m_data.radix);
+                failIfFalse(ident, "Cannot parse big int property name");
                 next();
                 break;
             case DOUBLE:
@@ -4613,7 +4614,8 @@ namedProperty:
         return context.createProperty(const_cast<VM&>(m_vm), m_parserArena, propertyName, node, PropertyNode::Constant, SuperBinding::NotNeeded, ClassElementTag::No);
     }
     case BIGINT: {
-        const Identifier* ident = &m_parserArena.identifierArena().makeBigIntDecimalIdentifier(const_cast<VM&>(m_vm), *m_token.m_data.bigIntString, m_token.m_data.radix);
+        const Identifier* ident = m_parserArena.identifierArena().makeBigIntDecimalIdentifier(const_cast<VM&>(m_vm), *m_token.m_data.bigIntString, m_token.m_data.radix);
+        failIfFalse(ident, "Cannot parse big int property name");
         next();
 
         if (match(OPENPAREN)) {
@@ -4705,7 +4707,8 @@ template <class TreeBuilder> TreeProperty Parser<LexerType>::parseGetterSetter(T
         numericPropertyName = m_token.m_data.doubleValue;
         next();
     } else if (match(BIGINT)) {
-        stringPropertyName = &m_parserArena.identifierArena().makeBigIntDecimalIdentifier(const_cast<VM&>(m_vm), *m_token.m_data.bigIntString, m_token.m_data.radix);
+        stringPropertyName = m_parserArena.identifierArena().makeBigIntDecimalIdentifier(const_cast<VM&>(m_vm), *m_token.m_data.bigIntString, m_token.m_data.radix);
+        failIfFalse(stringPropertyName, "Cannot parse big int property name");
         next();
     } else if (match(OPENBRACKET)) {
         next();
