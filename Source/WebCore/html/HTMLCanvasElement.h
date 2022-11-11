b61e318f043cef3f2262ee4e2004dc29a76725a9
Don Olmstead <don.olmstead@sony.com>
2022-11-11 13:41:07 -0800

    Enable HTMLCanvasElement::toVideoFrame with Web Codecs https://bugs.webkit.org/show_bug.cgi?id=247813
    Reviewed by Eric Carlson.
    
    Enable `HTMLCanvasElement::toVideoFrame` when either
    `ENABLE_MEDIA_STREAM` or `ENABLE_WEB_CODECS` is `ON`.
    
    * Source/WebCore/html/HTMLCanvasElement.cpp:
    * Source/WebCore/html/HTMLCanvasElement.h:
    
    Canonical link: https://commits.webkit.org/256588@main

--- Source/WebCore/html/HTMLCanvasElement.h
+++ Source/WebCore/html/HTMLCanvasElement.h
@@ -113,8 +113,10 @@ public:
 
     void paint(GraphicsContext&, const LayoutRect&);
 
-#if ENABLE(MEDIA_STREAM)
+#if ENABLE(MEDIA_STREAM) || ENABLE(WEB_CODECS)
     RefPtr<VideoFrame> toVideoFrame();
+#endif
+#if ENABLE(MEDIA_STREAM)
     ExceptionOr<Ref<MediaStream>> captureStream(std::optional<double>&& frameRequestRate);
 #endif
 
