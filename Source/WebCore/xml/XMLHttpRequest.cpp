b37371177a8cb1150db551c272cbe13384471090
Chris Dumez <cdumez@apple.com>
2022-11-11 13:08:19 -0800

    Revert [252611@main] Align XHR aborting with the specification and other browser engines https://bugs.webkit.org/show_bug.cgi?id=242817 rdar://101617443
    Unreviewed, revert 252611@main as it broke the Capital One app.
    
    * LayoutTests/TestExpectations:
    * LayoutTests/http/tests/navigation/page-cache-xhr-expected.txt:
    * LayoutTests/http/tests/navigation/page-cache-xhr.html:
    * LayoutTests/http/tests/navigation/resources/page-cache-xhr-in-loading-iframe.html:
    * LayoutTests/http/tests/xmlhttprequest/cross-origin-redirect-responseURL-expected.txt:
    * LayoutTests/http/tests/xmlhttprequest/frame-load-cancelled-abort-expected.txt:
    * LayoutTests/http/tests/xmlhttprequest/frame-unload-abort-crash-expected.txt:
    * LayoutTests/http/tests/xmlhttprequest/navigation-should-abort-expected.txt:
    * LayoutTests/http/tests/xmlhttprequest/navigation-should-abort.html:
    * LayoutTests/http/tests/xmlhttprequest/redirect-cross-origin-2-expected.txt:
    * LayoutTests/http/tests/xmlhttprequest/redirect-cross-origin-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/resource-timing/resource_TAO_match_origin-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/resource-timing/resource_TAO_match_wildcard-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/resource-timing/resource_TAO_multi-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/resource-timing/resource_TAO_null-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/resource-timing/resource_TAO_origin-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/resource-timing/resource_TAO_origin_uppercase-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/resource-timing/resource_TAO_space-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/resource-timing/resource_TAO_wildcard-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/resource-timing/resource_TAO_zero-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/xhr/abort-after-stop.window-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/xhr/open-after-stop.window-expected.txt:
    * LayoutTests/platform/mac-wk1/http/tests/xmlhttprequest/redirect-cross-origin-post-expected.txt:
    * LayoutTests/platform/win/http/tests/xmlhttprequest/redirect-cross-origin-post-expected.txt:
    * LayoutTests/platform/wincairo-wk1/http/tests/xmlhttprequest/redirect-cross-origin-post-expected.txt:
    * Source/WebCore/loader/FrameLoader.cpp:
    (WebCore::FrameLoader::requestFromDelegate):
    (WebCore::FrameLoader::blockedError const):
    * Source/WebCore/xml/XMLHttpRequest.cpp:
    (WebCore::XMLHttpRequest::XMLHttpRequest):
    (WebCore::XMLHttpRequest::open):
    (WebCore::XMLHttpRequest::abort):
    (WebCore::XMLHttpRequest::internalAbort):
    (WebCore::XMLHttpRequest::abortError):
    (WebCore::XMLHttpRequest::didFail):
    * Source/WebCore/xml/XMLHttpRequest.h:
    
    Canonical link: https://commits.webkit.org/256586@main

--- Source/WebCore/xml/XMLHttpRequest.cpp
+++ Source/WebCore/xml/XMLHttpRequest.cpp
@@ -113,6 +113,7 @@ XMLHttpRequest::XMLHttpRequest(ScriptExecutionContext& context)
     , m_error(false)
     , m_uploadListenerFlag(false)
     , m_uploadComplete(false)
+    , m_wasAbortedByClient(false)
     , m_responseCacheIsValid(false)
     , m_readyState(static_cast<unsigned>(UNSENT))
     , m_responseType(static_cast<unsigned>(ResponseType::EmptyString))
@@ -375,6 +376,7 @@ ExceptionOr<void> XMLHttpRequest::open(const String& method, const URL& url, boo
     m_method = normalizeHTTPMethod(method);
     m_error = false;
     m_uploadComplete = false;
+    m_wasAbortedByClient = false;
 
     // clear stuff from possible previous load
     clearResponse();
@@ -680,6 +682,7 @@ void XMLHttpRequest::abort()
 {
     Ref<XMLHttpRequest> protectedThis(*this);
 
+    m_wasAbortedByClient = true;
     if (!internalAbort())
         return;
 
@@ -698,7 +701,6 @@ void XMLHttpRequest::abort()
 
 bool XMLHttpRequest::internalAbort()
 {
-    m_pendingAbortEvent.cancel();
     m_error = true;
 
     // FIXME: when we add the support for multi-part XHR, we will have to think be careful with this initialization.
@@ -768,6 +770,7 @@ void XMLHttpRequest::networkError()
     
 void XMLHttpRequest::abortError()
 {
+    ASSERT(m_wasAbortedByClient);
     genericError();
     dispatchErrorEvents(eventNames().abortEvent);
 }
@@ -895,12 +898,10 @@ void XMLHttpRequest::didFail(const ResourceError& error)
     if (m_error)
         return;
 
-    if (error.isCancellation()) {
-        internalAbort();
-        queueCancellableTaskKeepingObjectAlive(*this, TaskSource::Networking, m_pendingAbortEvent, [this] {
-            m_exceptionCode = AbortError;
-            abortError();
-        });
+    // The XHR specification says we should only fire an abort event if the cancelation was requested by the client.
+    if (m_wasAbortedByClient && error.isCancellation()) {
+        m_exceptionCode = AbortError;
+        abortError();
         return;
     }
 
