WebKit/WebKit

commit 655a9e38ca398bcd75eb6a6dc0135fb8bc795ab4

[WK2] [Mac] Implement KeyDown function for WebKit2 EventSender. https://bugs.webkit.org/show_bug.cgi?id=57515
Source/WebKit2:

The code change in WebKit2 allows key events being sent to WebProcess from UIProcess synchronously.

Patch by Chang Shu <cshu@webkit.org> on 2011-09-13
Reviewed by Darin Adler.

* UIProcess/API/C/WKPage.cpp:
(WKPageSetShouldSendKeyboardEventSynchronously):
* UIProcess/API/C/WKPagePrivate.h:
* UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::WebPageProxy):
(WebKit::WebPageProxy::handleKeyboardEvent):
* UIProcess/WebPageProxy.h:
(WebKit::WebPageProxy::setShouldSendKeyboardEventSynchronously):
* WebProcess/WebPage/WebPage.cpp:
(WebKit::WebPage::keyEventSyncForTesting):
* WebProcess/WebPage/WebPage.h:
* WebProcess/WebPage/WebPage.messages.in:

Tools:

The code change in WebKitTestRunner first sends keydown event from InjectedBundle to
TestController synchronously. The latter then sends simulated event to WebKit2 module,
which is synchronous, too.

Patch by Chang Shu <cshu@webkit.org> on 2011-09-13
Reviewed by Darin Adler.

* WebKitTestRunner/EventSenderProxy.h: Added.
(WTR::EventSenderProxy::EventSenderProxy):
* WebKitTestRunner/InjectedBundle/Bindings/EventSendingController.idl:
* WebKitTestRunner/InjectedBundle/EventSendingController.cpp:
(WTR::EventSendingController::keyDown):
* WebKitTestRunner/InjectedBundle/EventSendingController.h:
* WebKitTestRunner/PlatformWebView.h:
(WTR::PlatformWebView::platformWindow):
* WebKitTestRunner/TestController.cpp:
(WTR::TestController::TestController):
(WTR::TestController::didReceiveSynchronousMessageFromInjectedBundle):
* WebKitTestRunner/TestController.h:
* WebKitTestRunner/WebKitTestRunner.xcodeproj/project.pbxproj:
* WebKitTestRunner/mac/EventSenderProxy.mm: Added.
(WTR::buildModifierFlags):
(WTR::EventSenderProxy::keyDown):

LayoutTests:

Unskip passed tests for Mac.

Patch by Chang Shu <cshu@webkit.org> on 2011-09-13
Reviewed by Darin Adler.

* platform/mac-wk2/Skipped:
* platform/qt-wk2/Skipped:

Tools/WebKitTestRunner/mac/EventSenderProxy.mm
@@ -1,0 +1,211 @@
+/*
+ * Copyright (C) 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "config.h"
+#import "EventSenderProxy.h"
+
+#import "StringFunctions.h"
+#import "PlatformWebView.h"
+#import "TestController.h"
+#import <WebKit2/WKPagePrivate.h>
+#import <WebKit2/WKString.h>
+
+namespace WTR {
+
+static int buildModifierFlags(WKEventModifiers modifiers)
+{
+    int flags = 0;
+    if (modifiers & kWKEventModifiersControlKey)
+        flags |= NSControlKeyMask;
+    if (modifiers & kWKEventModifiersShiftKey)
+        flags |= NSShiftKeyMask;
+    if (modifiers & kWKEventModifiersAltKey)
+        flags |= NSAlternateKeyMask;
+    if (modifiers & kWKEventModifiersMetaKey)
+        flags |= NSCommandKeyMask;
+    return flags;
+}
+
+void EventSenderProxy::keyDown(WKStringRef keyRef, WKEventModifiers modifiersRef, unsigned int keyLocation, double timestamp)
+{
+    NSString* character = [NSString stringWithCString:toSTD(keyRef).c_str() 
+                                   encoding:[NSString defaultCStringEncoding]];
+
+    NSString *eventCharacter = character;
+    unsigned short keyCode = 0;
+    if ([character isEqualToString:@"leftArrow"]) {
+        const unichar ch = NSLeftArrowFunctionKey;
+        eventCharacter = [NSString stringWithCharacters:&ch length:1];
+        keyCode = 0x7B;
+    } else if ([character isEqualToString:@"rightArrow"]) {
+        const unichar ch = NSRightArrowFunctionKey;
+        eventCharacter = [NSString stringWithCharacters:&ch length:1];
+        keyCode = 0x7C;
+    } else if ([character isEqualToString:@"upArrow"]) {
+        const unichar ch = NSUpArrowFunctionKey;
+        eventCharacter = [NSString stringWithCharacters:&ch length:1];
+        keyCode = 0x7E;
+    } else if ([character isEqualToString:@"downArrow"]) {
+        const unichar ch = NSDownArrowFunctionKey;
+        eventCharacter = [NSString stringWithCharacters:&ch length:1];
+        keyCode = 0x7D;
+    } else if ([character isEqualToString:@"pageUp"]) {
+        const unichar ch = NSPageUpFunctionKey;
+        eventCharacter = [NSString stringWithCharacters:&ch length:1];
+        keyCode = 0x74;
+    } else if ([character isEqualToString:@"pageDown"]) {
+        const unichar ch = NSPageDownFunctionKey;
+        eventCharacter = [NSString stringWithCharacters:&ch length:1];
+        keyCode = 0x79;
+    } else if ([character isEqualToString:@"home"]) {
+        const unichar ch = NSHomeFunctionKey;
+        eventCharacter = [NSString stringWithCharacters:&ch length:1];
+        keyCode = 0x73;
+    } else if ([character isEqualToString:@"end"]) {
+        const unichar ch = NSEndFunctionKey;
+        eventCharacter = [NSString stringWithCharacters:&ch length:1];
+        keyCode = 0x77;
+    } else if ([character isEqualToString:@"insert"]) {
+        const unichar ch = NSInsertFunctionKey;
+        eventCharacter = [NSString stringWithCharacters:&ch length:1];
+        keyCode = 0x72;
+    } else if ([character isEqualToString:@"delete"]) {
+        const unichar ch = NSDeleteFunctionKey;
+        eventCharacter = [NSString stringWithCharacters:&ch length:1];
+        keyCode = 0x75;
+    } else if ([character isEqualToString:@"printScreen"]) {
+        const unichar ch = NSPrintScreenFunctionKey;
+        eventCharacter = [NSString stringWithCharacters:&ch length:1];
+        keyCode = 0x0; // There is no known virtual key code for PrintScreen.
+    }
+
+    // Compare the input string with the function-key names defined by the DOM spec (i.e. "F1",...,"F24").
+    // If the input string is a function-key name, set its key code.
+    for (unsigned i = 1; i <= 24; i++) {
+        if ([character isEqualToString:[NSString stringWithFormat:@"F%u", i]]) {
+            const unichar ch = NSF1FunctionKey + (i - 1);
+            eventCharacter = [NSString stringWithCharacters:&ch length:1];
+            switch (i) {
+                case 1: keyCode = 0x7A; break;
+                case 2: keyCode = 0x78; break;
+                case 3: keyCode = 0x63; break;
+                case 4: keyCode = 0x76; break;
+                case 5: keyCode = 0x60; break;
+                case 6: keyCode = 0x61; break;
+                case 7: keyCode = 0x62; break;
+                case 8: keyCode = 0x64; break;
+                case 9: keyCode = 0x65; break;
+                case 10: keyCode = 0x6D; break;
+                case 11: keyCode = 0x67; break;
+                case 12: keyCode = 0x6F; break;
+                case 13: keyCode = 0x69; break;
+                case 14: keyCode = 0x6B; break;
+                case 15: keyCode = 0x71; break;
+                case 16: keyCode = 0x6A; break;
+                case 17: keyCode = 0x40; break;
+                case 18: keyCode = 0x4F; break;
+                case 19: keyCode = 0x50; break;
+                case 20: keyCode = 0x5A; break;
+            }
+        }
+    }
+
+    // FIXME: No keyCode is set for most keys.
+    if ([character isEqualToString:@"\t"])
+        keyCode = 0x30;
+    else if ([character isEqualToString:@" "])
+        keyCode = 0x31;
+    else if ([character isEqualToString:@"\r"])
+        keyCode = 0x24;
+    else if ([character isEqualToString:@"\n"])
+        keyCode = 0x4C;
+    else if ([character isEqualToString:@"\x8"])
+        keyCode = 0x33;
+    else if ([character isEqualToString:@"7"])
+        keyCode = 0x1A;
+    else if ([character isEqualToString:@"5"])
+        keyCode = 0x17;
+    else if ([character isEqualToString:@"9"])
+        keyCode = 0x19;
+    else if ([character isEqualToString:@"0"])
+        keyCode = 0x1D;
+    else if ([character isEqualToString:@"a"])
+        keyCode = 0x00;
+    else if ([character isEqualToString:@"b"])
+        keyCode = 0x0B;
+    else if ([character isEqualToString:@"d"])
+        keyCode = 0x02;
+    else if ([character isEqualToString:@"e"])
+        keyCode = 0x0E;
+
+    NSString *charactersIgnoringModifiers = eventCharacter;
+
+    int modifierFlags = 0;
+
+    if ([character length] == 1 && [character characterAtIndex:0] >= 'A' && [character characterAtIndex:0] <= 'Z') {
+        modifierFlags |= NSShiftKeyMask;
+        charactersIgnoringModifiers = [character lowercaseString];
+    }
+
+    modifierFlags |= buildModifierFlags(modifiersRef);
+
+    if (keyLocation == 0x03 /*DOM_KEY_LOCATION_NUMPAD*/)
+        modifierFlags |= NSNumericPadKeyMask;
+
+    // FIXME: [[[mainFrame frameView] documentView] layout];
+
+    WKPageSetShouldSendKeyboardEventSynchronously([m_testController->mainWebView()->platformView() pageRef], true);
+
+    NSEvent *event = [NSEvent keyEventWithType:NSKeyDown
+                        location:NSMakePoint(5, 5)
+                        modifierFlags:modifierFlags
+                        timestamp:timestamp//[self currentEventTime]
+                        windowNumber:[m_testController->mainWebView()->platformWindow() windowNumber]
+                        context:[NSGraphicsContext currentContext]
+                        characters:eventCharacter
+                        charactersIgnoringModifiers:charactersIgnoringModifiers
+                        isARepeat:NO
+                        keyCode:keyCode];
+
+    [[m_testController->mainWebView()->platformWindow() firstResponder] keyDown:event];
+
+    event = [NSEvent keyEventWithType:NSKeyUp
+                        location:NSMakePoint(5, 5)
+                        modifierFlags:modifierFlags
+                        timestamp:timestamp//[self currentEventTime]
+                        windowNumber:[m_testController->mainWebView()->platformWindow() windowNumber]
+                        context:[NSGraphicsContext currentContext]
+                        characters:eventCharacter
+                        charactersIgnoringModifiers:charactersIgnoringModifiers
+                        isARepeat:NO
+                        keyCode:keyCode];
+
+    [[m_testController->mainWebView()->platformWindow() firstResponder] keyUp:event];
+
+    WKPageSetShouldSendKeyboardEventSynchronously([m_testController->mainWebView()->platformView() pageRef], false);
+}
+
+} // namespace WTR

LayoutTests/platform/mac-wk2/Skipped
@@ -38,6 +38,221 @@
 ########################################
 ### START OF (1) Classified failures with bug reports
 
+# WebKitTestRunner needs an implementation of eventSender
+# <https://bugs.webkit.org/show_bug.cgi?id=42194>
+editing/deleting/delete-ligature-003.html
+editing/deleting/smart-delete-001.html
+editing/deleting/smart-delete-002.html
+editing/deleting/smart-delete-003.html
+editing/deleting/smart-delete-004.html
+editing/execCommand/findString-2.html
+editing/pasteboard/4944770-2.html
+editing/pasteboard/4947130.html
+editing/pasteboard/copy-standalone-image-crash.html
+editing/pasteboard/drag-and-drop-image-contenteditable.html
+editing/pasteboard/drag-and-drop-inputimage-contenteditable.html
+editing/pasteboard/drag-and-drop-objectimage-contenteditable.html
+editing/pasteboard/drag-drop-dead-frame.html
+editing/pasteboard/drag-drop-input-textarea.html
+editing/pasteboard/drag-drop-list.html
+editing/pasteboard/drag-drop-modifies-page.html
+editing/pasteboard/drag-drop-url-text.html
+editing/pasteboard/drag-image-in-about-blank-frame.html
+editing/pasteboard/drag-image-to-contenteditable-in-iframe.html
+editing/pasteboard/drag-selected-image-to-contenteditable.html
+editing/pasteboard/drop-file-svg.html
+editing/pasteboard/drop-inputtext-acquires-style.html
+editing/pasteboard/drop-link.html
+editing/pasteboard/drop-text-events.html
+editing/pasteboard/drop-text-without-selection.html
+editing/pasteboard/emacs-cntl-y-001.html
+editing/pasteboard/emacs-ctrl-a-k-y.html
+editing/pasteboard/emacs-ctrl-k-y-001.html
+editing/pasteboard/file-input-files-access.html
+editing/pasteboard/files-during-page-drags.html
+editing/pasteboard/get-data-text-plain-drop.html
+editing/pasteboard/smart-drag-drop.html
+editing/pasteboard/smart-paste-001.html
+editing/pasteboard/smart-paste-002.html
+editing/pasteboard/smart-paste-003.html
+editing/pasteboard/smart-paste-004.html
+editing/pasteboard/smart-paste-005.html
+editing/pasteboard/smart-paste-007.html
+editing/pasteboard/smart-paste-008.html
+editing/pasteboard/subframe-dragndrop-1.html
+editing/selection/14971.html
+editing/selection/4895428-1.html
+editing/selection/4895428-4.html
+editing/selection/5057506.html
+editing/selection/5232159.html
+editing/selection/5333725.html
+editing/selection/5354455-1.html
+editing/selection/5354455-2.html
+editing/selection/anchor-focus2.html
+editing/selection/anchor-focus3.html
+editing/selection/button-right-click.html
+editing/selection/click-in-focusable-link-should-not-clear-selection.html
+editing/selection/click-in-margins-inside-editable-div.html
+editing/selection/click-in-padding-with-multiple-line-boxes.html
+editing/selection/contains-boundaries.html
+editing/selection/context-menu-on-text.html
+editing/selection/doubleclick-beside-cr-span.html
+editing/selection/doubleclick-crash.html
+editing/selection/doubleclick-whitespace-crash.html
+editing/selection/doubleclick-whitespace-img-crash.html
+editing/selection/doubleclick-whitespace.html
+editing/selection/drag-select-1.html
+editing/selection/drag-select-rapidly.html
+editing/selection/drag-start-event-client-x-y.html
+editing/selection/drag-to-contenteditable-iframe.html
+editing/selection/empty-cell-right-click.html
+editing/selection/expanding-selections.html
+editing/selection/expanding-selections2.html
+editing/selection/extend-after-mouse-selection.html
+editing/selection/extend-selection-after-double-click.html
+editing/selection/fake-doubleclick.html
+editing/selection/fake-drag.html
+editing/selection/inline-closest-leaf-child.html
+editing/selection/last-empty-inline.html
+editing/selection/mixed-editability-10.html
+editing/selection/paragraph-granularity.html
+editing/selection/select-from-textfield-outwards.html
+editing/selection/select-out-of-editable.html
+editing/selection/select-out-of-floated-contenteditable.html
+editing/selection/select-out-of-floated-input.html
+editing/selection/select-out-of-floated-textarea.html
+editing/selection/selection-actions.html
+editing/selection/shift-click.html
+editing/selection/user-drag-element-and-user-select-none.html
+editing/selection/word-granularity.html
+editing/spelling/context-menu-suggestions.html
+editing/spelling/spellcheck-paste.html
+editing/undo/undo-deleteWord.html
+editing/undo/undo-smart-delete-reversed-selection.html
+editing/undo/undo-smart-delete-word.html
+fast/block/positioning/hittest-on-relative-positioned-children.html
+fast/css/user-drag-none.html
+fast/dom/Window/window-postmessage-clone-frames.html
+fast/dom/Window/window-postmessage-clone.html
+fast/dom/horizontal-scrollbar-in-rtl.html
+fast/dom/vertical-scrollbar-in-rtl.html
+fast/dynamic/layer-hit-test-crash.html
+fast/encoding/mailto-always-utf-8.html
+fast/events/5056619.html
+fast/events/autoscroll-in-textfield.html
+fast/events/autoscroll-nonscrollable-iframe-in-scrollable-div.html
+fast/events/autoscroll-with-non-scrollable-parent.html
+fast/events/autoscroll.html
+fast/events/bogus-dropEffect-effectAllowed.html
+fast/events/click-count.html
+fast/events/content-changed-during-drop.html
+fast/events/context-no-deselect.html
+fast/events/context-onmousedown-event.html
+fast/events/contextmenu-scrolled-page-with-frame.html
+fast/events/continuous-platform-wheelevent-in-scrolling-div.html
+fast/events/crash-on-mutate-during-drop.html
+fast/events/dblclick-addEventListener.html
+fast/events/drag-and-drop-dataTransfer-types-nocrash.html
+fast/events/drag-and-drop-fire-drag-dragover.html
+fast/events/drag-and-drop.html
+fast/events/drag-file-crash.html
+fast/events/drag-in-frames.html
+fast/events/drag-parent-node.html
+fast/events/drag-to-navigate.html
+fast/events/dropzone-001.html
+fast/events/dropzone-002.html
+fast/events/dropzone-003.html
+fast/events/dropzone-004.html
+fast/events/keydown-numpad-keys.html
+fast/events/mouse-click-events.html
+fast/events/mouseup-from-button2.html
+fast/events/node-event-anchor-lock.html
+fast/events/ondragenter.html
+fast/events/open-window-from-another-frame.html
+fast/events/platform-wheelevent-in-scrolling-div.html
+fast/events/popup-blocking-click-in-iframe.html
+fast/events/popup-when-select-change.html
+fast/events/prevent-drag-to-navigate.html
+fast/events/remove-child-onscroll.html
+fast/events/right-click-focus.html
+fast/events/scroll-after-click-on-tab-index.html
+fast/events/scroll-in-scaled-page-with-overflow-hidden.html
+fast/events/scrollbar-double-click.html
+fast/events/selectstart-by-double-triple-clicks.html
+fast/events/selectstart-by-drag.html
+fast/events/selectstart-prevent-selection-on-right-click.html
+fast/events/standalone-image-drag-to-editable.html
+fast/events/wheelevent-in-horizontal-scrollbar-in-rtl.html
+fast/events/wheelevent-in-vertical-scrollbar-in-rtl.html
+fast/events/zoom-dblclick.html
+fast/files/apply-blob-url-to-xhr.html
+fast/files/file-reader-abort.html
+fast/files/read-blob-async.html
+fast/files/read-file-async.html
+fast/forms/drag-into-textarea.html
+fast/forms/drag-out-of-textarea.html
+fast/forms/get-file-upload.html
+fast/forms/input-file-re-render.html
+fast/forms/input-readonly-autoscroll.html
+fast/forms/input-text-double-click.html
+fast/forms/input-text-drag-down.html
+fast/forms/listbox-typeahead-cyrillic.html
+fast/forms/listbox-typeahead-greek.html
+fast/forms/onselect-textarea.html
+fast/forms/onselect-textfield.html
+fast/forms/option-mouseevents.html
+fast/forms/range/slider-delete-while-dragging-thumb.html
+fast/forms/range/slider-mouse-events.html
+fast/forms/range/slider-onchange-event.html
+fast/forms/select-type-ahead-non-latin.html
+fast/history/window-open.html
+fast/lists/drag-into-marker.html
+fast/loader/policy-delegate-action-hit-test-zoomed.html
+fast/repaint/japanese-rl-selection-repaint.html
+fast/repaint/repaint-across-writing-mode-boundary.html
+fast/repaint/selection-rl.html
+fast/text/atsui-rtl-override-selection.html
+fast/text/international/khmer-selection.html
+fast/text/offsetForPosition-complex-fallback.html
+fast/text/reset-drag-on-mouse-down.html
+fast/writing-mode/horizontal-bt-replaced-selection.html
+fast/writing-mode/japanese-lr-selection.html
+fast/writing-mode/japanese-rl-selection.html
+fast/writing-mode/vertical-lr-replaced-selection.html
+fast/writing-mode/vertical-rl-replaced-selection.html
+http/tests/local/blob/send-hybrid-blob.html
+http/tests/local/drag-over-remote-content.html
+http/tests/local/fileapi/file-last-modified.html
+http/tests/local/fileapi/send-dragged-file.html
+http/tests/local/fileapi/send-sliced-dragged-file.html
+http/tests/local/formdata/form-data-with-unknown-file-extension.html
+http/tests/local/formdata/send-form-data-constructed-from-form.html
+http/tests/local/formdata/send-form-data-with-sliced-file.html
+http/tests/local/formdata/send-form-data.html
+http/tests/local/formdata/upload-events.html
+http/tests/misc/bubble-drag-events.html
+http/tests/security/clipboard/clipboard-file-access.html
+http/tests/security/dataTransfer-set-data-file-url.html
+http/tests/security/drag-drop-same-unique-origin.html
+http/tests/security/drag-over-remote-content-iframe.html
+media/controls-right-click-on-timebar.html
+platform/mac/editing/pasteboard/dataTransfer-set-data-file-url.html
+platform/mac/editing/pasteboard/drag-selections-to-contenteditable.html
+platform/mac/editing/selection/doubleclick-should-not-expand-across-lines.html
+platform/mac/editing/selection/word-thai.html
+platform/mac/fast/forms/listbox-scrollbar-hit-test.html
+platform/mac/fast/overflow/overflow-scrollbar-hit-test.html
+plugins/clicking-missing-plugin-fires-delegate.html
+scrollbars/scrollbar-iframe-click-does-not-blur-content.html
+scrollbars/scrollevent-iframe-no-scrolling-wheel.html
+
+svg/custom/foreignObject-crash-on-hover.xml
+svg/custom/mouse-move-on-svg-container-standalone.svg
+svg/custom/mouse-move-on-svg-container.xhtml
+svg/custom/mouse-move-on-svg-root-standalone.svg
+svg/custom/mouse-move-on-svg-root.xhtml
+svg/custom/use-events-crash.svg
+
 ### END OF (1) Classified failures with bug reports
 ########################################
 

Source/WebKit2/UIProcess/API/C/WKPage.cpp
@@ -581,3 +581,7 @@ WKImageRef WKPageCreateSnapshotOfVisibleContent(WKPageRef)
     return 0;
 }
 
+void WKPageSetShouldSendKeyboardEventSynchronously(WKPageRef page, bool sync)
+{
+    toImpl(page)->setShouldSendKeyboardEventSynchronously(sync);
+}

Source/WebKit2/UIProcess/API/C/WKPagePrivate.h
@@ -69,6 +69,8 @@ WK_EXPORT void WKPageDrawPagesToPDF(WKPageRef page, WKFrameRef frame, uint32_t f
 // FIXME https://bugs.webkit.org/show_bug.cgi?id=66979: Remove this sync call.
 WK_EXPORT WKImageRef WKPageCreateSnapshotOfVisibleContent(WKPageRef page);
 
+WK_EXPORT void WKPageSetShouldSendKeyboardEventSynchronously(WKPageRef page, bool sync);
+
 #ifdef __cplusplus
 }
 #endif

Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -180,6 +180,7 @@ WebPageProxy::WebPageProxy(PageClient* pageClient, PassRefPtr<WebProcessProxy> p
     , m_mainFrameIsPinnedToLeftSide(false)
     , m_mainFrameIsPinnedToRightSide(false)
     , m_renderTreeSize(0)
+    , m_shouldSendKeyboardEventSynchronously(false)
 {
 #ifndef NDEBUG
     webPageProxyCounter.increment();
@@ -860,7 +861,12 @@ void WebPageProxy::handleKeyboardEvent(const NativeWebKeyboardEvent& event)
     m_keyEventQueue.append(event);
 
     process()->responsivenessTimer()->start();
-    process()->send(Messages::WebPage::KeyEvent(event), m_pageID);
+    if (m_shouldSendKeyboardEventSynchronously) {
+        bool handled = false;
+        process()->sendSync(Messages::WebPage::KeyEventSyncForTesting(event), Messages::WebPage::KeyEventSyncForTesting::Reply(handled), m_pageID);
+        didReceiveEvent(event.type(), handled);
+    } else
+        process()->send(Messages::WebPage::KeyEvent(event), m_pageID);
 }
 
 #if ENABLE(GESTURE_EVENTS)

commit 99718f07fd9a2b5b6f9bb2e5d5f0a471d6bd7526

Fix return type of WTR::buildModifierFlags() in EventSenderProxy.mm https://bugs.webkit.org/show_bug.cgi?id=248254 <rdar://102615921>
Reviewed by Darin Adler.

Change return type of WTR::buildModifierFlags() from `int` to
`NSEventModifierFlags` since that's what it creates.  Regressed
back in 83922@main (r95039).

* Tools/WebKitTestRunner/mac/EventSenderProxy.mm:
(WTR::buildModifierFlags):

Canonical link: https://commits.webkit.org/256972@main

Tools/WebKitTestRunner/mac/EventSenderProxy.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011, 2014-2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2011-2022 Apple Inc. All rights reserved.
  * Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
  *
  * Redistribution and use in source and binary forms, with or without
@@ -276,9 +276,9 @@ static NSEventType eventTypeForMouseButtonAndAction(int button, MouseAction acti
     return static_cast<NSEventType>(0);
 }
 
-static int buildModifierFlags(WKEventModifiers modifiers)
+static NSEventModifierFlags buildModifierFlags(WKEventModifiers modifiers)
 {
-    int flags = 0;
+    NSEventModifierFlags flags = 0;
     if (modifiers & kWKEventModifiersControlKey)
         flags |= NSEventModifierFlagControl;
     if (modifiers & kWKEventModifiersShiftKey)