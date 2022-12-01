ee0a6a9a3b2b376d717a75b26e1710a8b5ede6e4
Simon Fraser <simon.fraser@apple.com>
2022-12-01 15:33:49 -0800

    Adopt HAVE(MACH_PORT_CALAYER_CONTENTS) https://bugs.webkit.org/show_bug.cgi?id=248563 rdar://102831386
    Reviewed by Tim Horton.
    
    Use HAVE(MACH_PORT_CALAYER_CONTENTS) to know when we're on a build with
    the fix for rdar://31247730.
    
    * Source/WebKit/UIProcess/RemoteLayerTree/RemoteLayerTreeHost.mm:
    (WebKit::RemoteLayerTreeHost::layerContentsType const):
    
    Canonical link: https://commits.webkit.org/257257@main

--- Source/WebKit/UIProcess/RemoteLayerTree/RemoteLayerTreeHost.mm
+++ Source/WebKit/UIProcess/RemoteLayerTree/RemoteLayerTreeHost.mm
@@ -66,10 +66,6 @@ RemoteLayerTreeHost::~RemoteLayerTreeHost()
 
 RemoteLayerBackingStore::LayerContentsType RemoteLayerTreeHost::layerContentsType() const
 {
-#if PLATFORM(MAC) || PLATFORM(MACCATALYST)
-    // CAMachPort currently does not work on macOS (or macCatalyst): rdar://problem/31247730
-    return RemoteLayerBackingStore::LayerContentsType::IOSurface;
-#else
     // If a surface will be referenced by multiple layers (as in the tile debug indicator), CAMachPort cannot be used.
     if (m_drawingArea->hasDebugIndicator())
         return RemoteLayerBackingStore::LayerContentsType::IOSurface;
@@ -78,7 +74,10 @@ RemoteLayerBackingStore::LayerContentsType RemoteLayerTreeHost::layerContentsTyp
     if (m_drawingArea->page().windowKind() == WindowKind::InProcessSnapshotting)
         return RemoteLayerBackingStore::LayerContentsType::IOSurface;
 
+#if HAVE(MACH_PORT_CALAYER_CONTENTS)
     return RemoteLayerBackingStore::LayerContentsType::CAMachPort;
+#else
+    return RemoteLayerBackingStore::LayerContentsType::IOSurface;
 #endif
 }
 
