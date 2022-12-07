e9b7bb98aeae16c1570a7090abaacad6aa9cf5e9
Karl Rackler <rackler@apple.com>
2022-12-07 10:37:03 -0800

    Revert [257463@main] Add abstract decoder support in LibWebRTCCodecsProxy because if broke the build. https://bugs.webkit.org/show_bug.cgi?id=248753 rdar://problem/102976377
    Unreviewed revert
    
    * Source/WebCore/PAL/pal/cf/CoreMediaSoftLink.cpp:
    * Source/WebCore/PAL/pal/cf/CoreMediaSoftLink.h:
    * Source/WebCore/WebCore.xcodeproj/project.pbxproj:
    * Source/WebCore/platform/mediastream/cocoa/WebRTCVideoDecoder.h: Removed.
    * Source/WebCore/platform/mediastream/cocoa/WebRTCVideoDecoder.mm: Removed.
    * Source/WebKit/GPUProcess/webrtc/LibWebRTCCodecsProxy.h:
    * Source/WebKit/GPUProcess/webrtc/LibWebRTCCodecsProxy.mm:
    (WebKit::LibWebRTCCodecsProxy::stopListeningForIPC):
    (WebKit::LibWebRTCCodecsProxy::createLocalDecoder):
    (WebKit::LibWebRTCCodecsProxy::createDecoder):
    (WebKit::LibWebRTCCodecsProxy::releaseDecoder):
    (WebKit::LibWebRTCCodecsProxy::flushDecoder):
    (WebKit::LibWebRTCCodecsProxy::setDecoderFormatDescription):
    * Source/WebKit/WebProcess/GPU/webrtc/LibWebRTCCodecs.cpp:
    
    Canonical link: https://commits.webkit.org/257493@main

--- Source/WebKit/WebProcess/GPU/webrtc/LibWebRTCCodecs.cpp
+++ Source/WebKit/WebProcess/GPU/webrtc/LibWebRTCCodecs.cpp
@@ -57,10 +57,6 @@ ALLOW_COMMA_END
 namespace WebKit {
 using namespace WebCore;
 
-#if USE(APPLE_INTERNAL_SDK)
-#include <WebKitAdditions/LibWebRTCCodecsAdditions.mm>
-#else
-
 static webrtc::WebKitVideoDecoder createVideoDecoder(const webrtc::SdpVideoFormat& format)
 {
     auto& codecs = WebProcess::singleton().libWebRTCCodecs();
@@ -95,8 +91,6 @@ std::optional<VideoCodecType> LibWebRTCCodecs::videoCodecTypeFromWebCodec(const 
     // FIXME: Expose H265 if available.
     return { };
 }
-
-#endif
 
 static int32_t releaseVideoDecoder(webrtc::WebKitVideoDecoder::Value decoder)
 {
