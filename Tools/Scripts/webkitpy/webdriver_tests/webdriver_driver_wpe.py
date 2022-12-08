c16db3c1853c3f03265f96c0d2dd9db3d56c3d0f
Lauro Moura <lmoura@igalia.com>
2022-12-08 06:03:54 -0800

    REGRESSION(256795@main) [WPE][WebDriver] Replace deleted browser environment function in tooling https://bugs.webkit.org/show_bug.cgi?id=248928
    Reviewed by Carlos Garcia Campos.
    
    * Tools/Scripts/webkitpy/webdriver_tests/webdriver_driver_wpe.py:
    (WebDriverWPE.browser_env): Replace port.browser_env with the updated
    function.
    
    Canonical link: https://commits.webkit.org/257561@main

--- Tools/Scripts/webkitpy/webdriver_tests/webdriver_driver_wpe.py
+++ Tools/Scripts/webkitpy/webdriver_tests/webdriver_driver_wpe.py
@@ -51,7 +51,7 @@ class WebDriverWPE(WebDriver):
         return args
 
     def browser_env(self):
-        return self._port.browser_env()
+        return self._port.setup_environ_for_minibrowser()
 
     def capabilities(self):
         return {'wpe:browserOptions': {
