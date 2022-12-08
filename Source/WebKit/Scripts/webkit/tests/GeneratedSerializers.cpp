WebKit/WebKit

commit 39afa29edb2601dcea29f92669101505bd491d0d

Port WebHitTestResultData to the new IPC serialization format https://bugs.webkit.org/show_bug.cgi?id=248079 rdar://102518135
Reviewed by Alex Christensen.

Port WebHitTestResultData to the new IPC serialization format and add
support for SoftLinkedClasses in the GeneratedSerializers.

* Source/WebKit/DerivedSources-input.xcfilelist:
* Source/WebKit/DerivedSources.make:
* Source/WebKit/Scripts/generate-serializers.py:
(sanitize_string_for_variable_name):
(decode_type):
(parse_serialized_types):
* Source/WebKit/Shared/WebHitTestResultData.cpp:
(WebKit::WebHitTestResultPlatformData::WebHitTestResultPlatformData):
(WebKit::WebHitTestResultData::WebHitTestResultData):
(WebKit::WebHitTestResultData::~WebHitTestResultData):
(WebKit::WebHitTestResultData::getImageSharedMemoryHandle const):
(WebKit::WebHitTestResultData::encode const): Deleted.
(WebKit::WebHitTestResultData::decode): Deleted.
(WebKit::WebHitTestResultData::platformEncode const): Deleted.
(WebKit::WebHitTestResultData::platformDecode): Deleted.
* Source/WebKit/Shared/WebHitTestResultData.h:
* Source/WebKit/Shared/WebHitTestResultData.serialization.in: Added.
* Source/WebKit/Shared/mac/WebHitTestResultData.mm: Removed.
* Source/WebKit/Shared/mac/WebHitTestResultPlatformData.serialization.in: Added.
* Source/WebKit/SourcesCocoa.txt:
* Source/WebKit/UIProcess/mac/WKImmediateActionController.mm:
(-[WKImmediateActionController _animationControllerForDataDetectedText]):
* Source/WebKit/WebKit.xcodeproj/project.pbxproj:
* Source/WebKit/WebProcess/WebPage/mac/WebPageMac.mm:
(WebKit::WebPage::performImmediateActionHitTestAtLocation):

Canonical link: https://commits.webkit.org/257087@main

Source/WebKit/Scripts/generate-serializers.py
@@ -352,12 +352,25 @@ def decode_type(type):
                     result.append('        return { ' + type.namespace_and_name() + ' { } };')
             else:
                 assert len(decodable_classes) == 0
-                result.append('    decoder >> ' + sanitize_string_for_variable_name(member.name) + ';')
-                result.append('    if (!' + sanitize_string_for_variable_name(member.name) + ')')
-                result.append('        return std::nullopt;')
-                if 'ReturnEarlyIfTrue' in member.attributes:
-                    result.append('    if (*' + sanitize_string_for_variable_name(member.name) + ')')
-                    result.append('        return { ' + type.namespace_and_name() + ' { } };')
+                r = re.compile(r"SoftLinkedClass='(.*)'")
+                soft_linked_classes = [r.match(m).groups()[0] for m in list(filter(r.match, member.attributes))]
+                if len(soft_linked_classes) == 1:
+                    match = re.search("RetainPtr<(.*)>", member.type)
+                    assert match
+                    result.append('    ' + sanitize_string_for_variable_name(member.name) + ' = IPC::decode<' + match.groups()[0] + '>(decoder, ' + soft_linked_classes[0] + ');')
+                    result.append('    if (!' + sanitize_string_for_variable_name(member.name) + ')')
+                    result.append('        return std::nullopt;')
+                    if 'ReturnEarlyIfTrue' in member.attributes:
+                        result.append('    if (*' + sanitize_string_for_variable_name(member.name) + ')')
+                        result.append('        return { ' + type.namespace_and_name() + ' { } };')
+                else:
+                    assert len(soft_linked_classes) == 0
+                    result.append('    decoder >> ' + sanitize_string_for_variable_name(member.name) + ';')
+                    result.append('    if (!' + sanitize_string_for_variable_name(member.name) + ')')
+                    result.append('        return std::nullopt;')
+                    if 'ReturnEarlyIfTrue' in member.attributes:
+                        result.append('    if (*' + sanitize_string_for_variable_name(member.name) + ')')
+                        result.append('        return { ' + type.namespace_and_name() + ' { } };')
         for attribute in member.attributes:
             match = re.search(r'Validator=\'(.*)\'', attribute)
             if match:
@@ -677,6 +690,11 @@ def parse_serialized_types(file, file_name):
             if match:
                 complete, _, allow_list, _ = match.groups()
                 member_attributes.append(allow_list)
+                member_attributes_s = member_attributes_s.replace(complete, "")
+            match = re.search(r"((, |^)+(SoftLinkedClass='.*?'))(, |$)?", member_attributes_s)
+            if match:
+                complete, _, soft_linked_class, _ = match.groups()
+                member_attributes.append(soft_linked_class)
                 member_attributes_s = member_attributes_s.replace(complete, "")
             member_attributes += [member_attribute.strip() for member_attribute in member_attributes_s.split(",")]
             members.append(MemberVariable(member_type, member_name, member_condition, member_attributes))

Source/WebKit/Shared/mac/WebHitTestResultPlatformData.serialization.in
@@ -1,0 +1,34 @@
+# Copyright (C) 2022 Apple Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+file_extension: mm
+headers: <WebCore/TextIndicator.h>
+
+#if PLATFORM(MAC)
+header: "WebHitTestResultData.h" <pal/mac/DataDetectorsSoftLink.h>
+[CustomHeader] struct WebKit::WebHitTestResultPlatformData {
+    [SoftLinkedClass='PAL::getDDActionContextClass()'] RetainPtr<DDActionContext> detectedDataActionContext;
+    WebCore::FloatRect detectedDataBoundingBox;
+    RefPtr<WebCore::TextIndicator> detectedDataTextIndicator;
+    WebCore::PageOverlay::PageOverlayID detectedDataOriginatingPageOverlay;
+}
+#endif

Source/WebKit/CMakeLists.txt
@@ -544,6 +544,7 @@ set(WebKit_SERIALIZATION_IN_FILES
     Shared/WTFArgumentCoders.serialization.in
     Shared/WebCoreArgumentCoders.serialization.in
     Shared/WebEvent.serialization.in
+    Shared/WebHitTestResultData.serialization.in
     Shared/WebPushDaemonConnectionConfiguration.serialization.in
     Shared/WebPushMessage.serialization.in
     Shared/WebsiteDataStoreParameters.serialization.in

Source/WebKit/DerivedSources-input.xcfilelist
@@ -176,6 +176,7 @@ $(PROJECT_DIR)/Shared/WebCoreArgumentCoders.serialization.in
 $(PROJECT_DIR)/Shared/WebEvent.serialization.in
 $(PROJECT_DIR)/Shared/WebExtensionContextParameters.serialization.in
 $(PROJECT_DIR)/Shared/WebExtensionControllerParameters.serialization.in
+$(PROJECT_DIR)/Shared/WebHitTestResultData.serialization.in
 $(PROJECT_DIR)/Shared/WebGPU/WebGPUBindGroupDescriptor.serialization.in
 $(PROJECT_DIR)/Shared/WebGPU/WebGPUBindGroupEntry.serialization.in
 $(PROJECT_DIR)/Shared/WebGPU/WebGPUBindGroupLayoutDescriptor.serialization.in
@@ -250,6 +251,7 @@ $(PROJECT_DIR)/Shared/XR/XRSystem.serialization.in
 $(PROJECT_DIR)/Shared/ios/InteractionInformationAtPosition.serialization.in
 $(PROJECT_DIR)/Shared/mac/SecItemRequestData.serialization.in
 $(PROJECT_DIR)/Shared/mac/SecItemResponseData.serialization.in
+$(PROJECT_DIR)/Shared/mac/WebHitTestResultPlatformData.serialization.in
 $(PROJECT_DIR)/UIProcess/Automation/Automation.json
 $(PROJECT_DIR)/UIProcess/Automation/WebAutomationSession.messages.in
 $(PROJECT_DIR)/UIProcess/Cocoa/PlaybackSessionManagerProxy.messages.in

Source/WebKit/DerivedSources.make
@@ -492,10 +492,12 @@ SERIALIZATION_DESCRIPTION_FILES = \
 	Shared/WebExtensionContextParameters.serialization.in \
 	Shared/WebEvent.serialization.in \
 	Shared/WebExtensionControllerParameters.serialization.in \
+	Shared/WebHitTestResultData.serialization.in \
 	Shared/WebPushDaemonConnectionConfiguration.serialization.in \
 	Shared/WebPushMessage.serialization.in \
 	Shared/mac/SecItemRequestData.serialization.in \
 	Shared/mac/SecItemResponseData.serialization.in \
+	Shared/mac/WebHitTestResultPlatformData.serialization.in \
 	Shared/WebsiteDataStoreParameters.serialization.in \
 	Shared/WebsiteData/WebsiteDataFetchOption.serialization.in \
 	Shared/WebGPU/WebGPUBindGroupDescriptor.serialization.in \

Source/WebKit/Shared/WebCoreArgumentCoders.serialization.in
@@ -1550,3 +1550,7 @@ enum class WebCore::PixelFormat : uint8_t {
     WebCore::PixelFormat pixelFormat;
     WebCore::DestinationColorSpace colorSpace;
 };
+
+[Return=Ref] class WebCore::TextIndicator {
+    WebCore::TextIndicatorData data();
+};

Source/WebKit/Shared/WebHitTestResultData.cpp
@@ -110,123 +110,43 @@ WebHitTestResultData::WebHitTestResultData(const HitTestResult& hitTestResult, b
     }
 }
 
+WebHitTestResultData::WebHitTestResultData(const String& absoluteImageURL, const String& absolutePDFURL, const String& absoluteLinkURL, const String& absoluteMediaURL, const String& linkLabel, const String& linkTitle, const String& linkSuggestedFilename, bool isContentEditable, const WebCore::IntRect& elementBoundingBox, const WebKit::WebHitTestResultData::IsScrollbar& isScrollbar, bool isSelected, bool isTextNode, bool isOverTextInsideFormControlElement, bool isDownloadableMedia, const String& lookupText, const String& toolTipText, const String& imageText, const std::optional<WebKit::SharedMemory::Handle>& imageHandle, const RefPtr<WebKit::ShareableBitmap>& imageBitmap, const String& sourceImageMIMEType,
+#if PLATFORM(MAC)
+    const WebHitTestResultPlatformData& platformData,
+#endif
+    const WebCore::DictionaryPopupInfo& dictionaryPopupInfo, const RefPtr<WebCore::TextIndicator>& linkTextIndicator)
+        : absoluteImageURL(absoluteImageURL)
+        , absolutePDFURL(absolutePDFURL)
+        , absoluteLinkURL(absoluteLinkURL)
+        , absoluteMediaURL(absoluteMediaURL)
+        , linkLabel(linkLabel)
+        , linkTitle(linkTitle)
+        , linkSuggestedFilename(linkSuggestedFilename)
+        , isContentEditable(isContentEditable)
+        , elementBoundingBox(elementBoundingBox)
+        , isScrollbar(isScrollbar)
+        , isSelected(isSelected)
+        , isTextNode(isTextNode)
+        , isOverTextInsideFormControlElement(isOverTextInsideFormControlElement)
+        , isDownloadableMedia(isDownloadableMedia)
+        , lookupText(lookupText)
+        , toolTipText(toolTipText)
+        , imageText(imageText)
+        , imageBitmap(imageBitmap)
+        , sourceImageMIMEType(sourceImageMIMEType)
+#if PLATFORM(MAC)
+        , platformData(platformData)
+#endif
+        , dictionaryPopupInfo(dictionaryPopupInfo)
+        , linkTextIndicator(linkTextIndicator)
+{
+    if (!imageHandle->isNull())
+        imageSharedMemory = WebKit::SharedMemory::map(*imageHandle, WebKit::SharedMemory::Protection::ReadOnly);
+}
+
 WebHitTestResultData::~WebHitTestResultData()
 {
 }
-
-void WebHitTestResultData::encode(IPC::Encoder& encoder) const
-{
-    encoder << absoluteImageURL;
-    encoder << absolutePDFURL;
-    encoder << absoluteLinkURL;
-    encoder << absoluteMediaURL;
-    encoder << linkLabel;
-    encoder << linkTitle;
-    encoder << linkSuggestedFilename;
-    encoder << isContentEditable;
-    encoder << elementBoundingBox;
-    encoder << isScrollbar;
-    encoder << isSelected;
-    encoder << isTextNode;
-    encoder << isOverTextInsideFormControlElement;
-    encoder << isDownloadableMedia;
-    encoder << lookupText;
-    encoder << toolTipText;
-    encoder << imageText;
-    encoder << dictionaryPopupInfo;
-
-    WebKit::SharedMemory::Handle imageHandle;
-    if (imageSharedMemory && imageSharedMemory->data()) {
-        if (auto handle = imageSharedMemory->createHandle(WebKit::SharedMemory::Protection::ReadOnly))
-            imageHandle = WTFMove(*handle);
-    }
-
-    encoder << imageHandle;
-
-    ShareableBitmapHandle imageBitmapHandle;
-    if (imageBitmap) {
-        if (auto handle = imageBitmap->createHandle(SharedMemory::Protection::ReadOnly))
-            imageBitmapHandle = WTFMove(*handle);
-    }
-    encoder << imageBitmapHandle;
-    encoder << sourceImageMIMEType;
-
-    bool hasLinkTextIndicator = linkTextIndicator;
-    encoder << hasLinkTextIndicator;
-    if (hasLinkTextIndicator)
-        encoder << linkTextIndicator->data();
-
-    platformEncode(encoder);
-}
-
-bool WebHitTestResultData::decode(IPC::Decoder& decoder, WebHitTestResultData& hitTestResultData)
-{
-    if (!decoder.decode(hitTestResultData.absoluteImageURL)
-        || !decoder.decode(hitTestResultData.absolutePDFURL)
-        || !decoder.decode(hitTestResultData.absoluteLinkURL)
-        || !decoder.decode(hitTestResultData.absoluteMediaURL)
-        || !decoder.decode(hitTestResultData.linkLabel)
-        || !decoder.decode(hitTestResultData.linkTitle)
-        || !decoder.decode(hitTestResultData.linkSuggestedFilename)
-        || !decoder.decode(hitTestResultData.isContentEditable)
-        || !decoder.decode(hitTestResultData.elementBoundingBox)
-        || !decoder.decode(hitTestResultData.isScrollbar)
-        || !decoder.decode(hitTestResultData.isSelected)
-        || !decoder.decode(hitTestResultData.isTextNode)
-        || !decoder.decode(hitTestResultData.isOverTextInsideFormControlElement)
-        || !decoder.decode(hitTestResultData.isDownloadableMedia)
-        || !decoder.decode(hitTestResultData.lookupText)
-        || !decoder.decode(hitTestResultData.toolTipText)
-        || !decoder.decode(hitTestResultData.imageText)
-        || !decoder.decode(hitTestResultData.dictionaryPopupInfo))
-        return false;
-
-    WebKit::SharedMemory::Handle imageHandle;
-    if (!decoder.decode(imageHandle))
-        return false;
-
-    if (!imageHandle.isNull()) {
-        hitTestResultData.imageSharedMemory = WebKit::SharedMemory::map(imageHandle, WebKit::SharedMemory::Protection::ReadOnly);
-        if (!hitTestResultData.imageSharedMemory)
-            return false;
-    }
-
-    ShareableBitmapHandle imageBitmapHandle;
-    if (!decoder.decode(imageBitmapHandle))
-        return false;
-
-    if (!imageBitmapHandle.isNull())
-        hitTestResultData.imageBitmap = ShareableBitmap::create(imageBitmapHandle, SharedMemory::Protection::ReadOnly);
-
-    if (!decoder.decode(hitTestResultData.sourceImageMIMEType))
-        return false;
-
-    bool hasLinkTextIndicator;
-    if (!decoder.decode(hasLinkTextIndicator))
-        return false;
-
-    if (hasLinkTextIndicator) {
-        std::optional<WebCore::TextIndicatorData> indicatorData;
-        decoder >> indicatorData;
-        if (!indicatorData)
-            return false;
-
-        hitTestResultData.linkTextIndicator = WebCore::TextIndicator::create(*indicatorData);
-    }
-
-    return platformDecode(decoder, hitTestResultData);
-}
-
-#if !PLATFORM(MAC)
-void WebHitTestResultData::platformEncode(IPC::Encoder& encoder) const
-{
-}
-
-bool WebHitTestResultData::platformDecode(IPC::Decoder& decoder, WebHitTestResultData& hitTestResultData)
-{
-    return true;
-}
-#endif // !PLATFORM(MAC)
 
 IntRect WebHitTestResultData::elementBoundingBoxInWindowCoordinates(const WebCore::HitTestResult& hitTestResult)
 {
@@ -247,6 +167,16 @@ IntRect WebHitTestResultData::elementBoundingBoxInWindowCoordinates(const WebCor
         return IntRect();
 
     return view->contentsToWindow(renderer->absoluteBoundingBoxRect());
+}
+
+std::optional<WebKit::SharedMemory::Handle> WebHitTestResultData::getImageSharedMemoryHandle() const
+{
+    std::optional<WebKit::SharedMemory::Handle> imageHandle = std::nullopt;
+    if (imageSharedMemory && imageSharedMemory->data()) {
+        if (auto handle = imageSharedMemory->createHandle(WebKit::SharedMemory::Protection::ReadOnly))
+            imageHandle = WTFMove(*handle);
+    }
+    return imageHandle;
 }
 
 } // WebKit

Source/WebKit/Shared/WebHitTestResultData.h
@@ -44,6 +44,15 @@ class HitTestResult;
 
 namespace WebKit {
 
+#if PLATFORM(MAC)
+struct WebHitTestResultPlatformData {
+    RetainPtr<DDActionContext> detectedDataActionContext;
+    WebCore::FloatRect detectedDataBoundingBox;
+    RefPtr<WebCore::TextIndicator> detectedDataTextIndicator;
+    WebCore::PageOverlay::PageOverlayID detectedDataOriginatingPageOverlay;
+};
+#endif
+
 struct WebHitTestResultData {
     String absoluteImageURL;
     String absolutePDFURL;
@@ -54,7 +63,7 @@ struct WebHitTestResultData {
     String linkSuggestedFilename;
     bool isContentEditable;
     WebCore::IntRect elementBoundingBox;
-    enum class IsScrollbar { No, Vertical, Horizontal };
+    enum class IsScrollbar : uint8_t { No, Vertical, Horizontal };
     IsScrollbar isScrollbar;
     bool isSelected;
     bool isTextNode;
@@ -69,12 +78,9 @@ struct WebHitTestResultData {
     String sourceImageMIMEType;
 
 #if PLATFORM(MAC)
-    RetainPtr<DDActionContext> detectedDataActionContext;
+    WebHitTestResultPlatformData platformData;
 #endif
-    WebCore::FloatRect detectedDataBoundingBox;
-    RefPtr<WebCore::TextIndicator> detectedDataTextIndicator;
-    WebCore::PageOverlay::PageOverlayID detectedDataOriginatingPageOverlay;
-
+    
     WebCore::DictionaryPopupInfo dictionaryPopupInfo;
 
     RefPtr<WebCore::TextIndicator> linkTextIndicator;
@@ -82,27 +88,16 @@ struct WebHitTestResultData {
     WebHitTestResultData();
     WebHitTestResultData(const WebCore::HitTestResult&, const String& toolTipText);
     WebHitTestResultData(const WebCore::HitTestResult&, bool includeImage);
+    WebHitTestResultData(const String& absoluteImageURL, const String& absolutePDFURL, const String& absoluteLinkURL, const String& absoluteMediaURL, const String& linkLabel, const String& linkTitle, const String& linkSuggestedFilename, bool isContentEditable, const WebCore::IntRect& elementBoundingBox, const WebKit::WebHitTestResultData::IsScrollbar&, bool isSelected, bool isTextNode, bool isOverTextInsideFormControlElement, bool isDownloadableMedia, const String& lookupText, const String& toolTipText, const String& imageText, const std::optional<WebKit::SharedMemory::Handle>& imageHandle, const RefPtr<WebKit::ShareableBitmap>& imageBitmap, const String& sourceImageMIMEType,
+#if PLATFORM(MAC)
+        const WebHitTestResultPlatformData&,
+#endif
+        const WebCore::DictionaryPopupInfo&, const RefPtr<WebCore::TextIndicator>&);
     ~WebHitTestResultData();
 
-    void encode(IPC::Encoder&) const;
-    void platformEncode(IPC::Encoder&) const;
-    static WARN_UNUSED_RETURN bool decode(IPC::Decoder&, WebHitTestResultData&);
-    static WARN_UNUSED_RETURN bool platformDecode(IPC::Decoder&, WebHitTestResultData&);
-
     WebCore::IntRect elementBoundingBoxInWindowCoordinates(const WebCore::HitTestResult&);
+
+    std::optional<WebKit::SharedMemory::Handle> getImageSharedMemoryHandle() const;
 };
 
 } // namespace WebKit
-
-namespace WTF {
-
-template<> struct EnumTraits<WebKit::WebHitTestResultData::IsScrollbar> {
-    using values = EnumValues<
-    WebKit::WebHitTestResultData::IsScrollbar,
-    WebKit::WebHitTestResultData::IsScrollbar::No,
-    WebKit::WebHitTestResultData::IsScrollbar::Vertical,
-    WebKit::WebHitTestResultData::IsScrollbar::Horizontal
-    >;
-};
-
-} // namespace WTF

commit 85272a0e3a2be85e32dd6b20b9819dbc3a1d55b8

Cherry-pick 5e92b798c222. rdar://problem/103099864
    Don't call getDDActionContextClass if we don't have a DDActionContext to decode
    https://bugs.webkit.org/show_bug.cgi?id=248931
    rdar://103099864

    Reviewed by Aditya Keerthi.

    This fixes a regression from 257087@main, before which we would only call getDDActionContextClass
    if hasActionContext was true.  This restores equivalent behavior by supporting the Nullable attribute
    when encoding soft linked ObjC classes.

    * Source/WebKit/Scripts/generate-serializers.py:
    (decode_type):
    * Source/WebKit/Scripts/webkit/tests/GeneratedSerializers.cpp:
    (IPC::ArgumentCoder<NullableSoftLinkedMember>::encode):
    (IPC::ArgumentCoder<NullableSoftLinkedMember>::decode):
    * Source/WebKit/Scripts/webkit/tests/GeneratedSerializers.h:
    * Source/WebKit/Scripts/webkit/tests/SerializedTypeInfo.cpp:
    (WebKit::allSerializedTypes):
    * Source/WebKit/Scripts/webkit/tests/TestSerializedType.serialization.in:
    * Source/WebKit/Shared/mac/WebHitTestResultPlatformData.serialization.in:

    Canonical link: https://commits.webkit.org/257575@main

Canonical link: https://commits.webkit.org/257350.7@safari-7615.1.14-branch

Source/WebKit/Scripts/generate-serializers.py
@@ -314,18 +314,7 @@ def decode_type(type):
         if member.condition is not None:
             result.append('#if ' + member.condition)
         result.append('    std::optional<' + member.type + '> ' + sanitize_string_for_variable_name(member.name) + ';')
-        if 'Nullable' in member.attributes:
-            result.append('    std::optional<bool> has' + member.name + ';')
-            result.append('    decoder >> has' + member.name + ';')
-            result.append('    if (!has' + member.name + ')')
-            result.append('        return std::nullopt;')
-            result.append('    if (*has' + member.name + ') {')
-            result.append('        decoder >> ' + member.name + ';')
-            result.append('        if (!' + member.name + ')')
-            result.append('            return std::nullopt;')
-            result.append('    } else')
-            result.append('        ' + member.name + ' = std::optional<' + member.type + '> { ' + member.type + ' { } };')
-        elif member.unique_ptr_type() is not None:
+        if member.unique_ptr_type() is not None:
             result.append('    std::optional<bool> has' + sanitize_string_for_variable_name(member.name) + ';')
             result.append('    decoder >> has' + sanitize_string_for_variable_name(member.name) + ';')
             result.append('    if (!has' + sanitize_string_for_variable_name(member.name) + ')')
@@ -357,12 +346,35 @@ def decode_type(type):
                 if len(soft_linked_classes) == 1:
                     match = re.search("RetainPtr<(.*)>", member.type)
                     assert match
-                    result.append('    ' + sanitize_string_for_variable_name(member.name) + ' = IPC::decode<' + match.groups()[0] + '>(decoder, ' + soft_linked_classes[0] + ');')
-                    result.append('    if (!' + sanitize_string_for_variable_name(member.name) + ')')
-                    result.append('        return std::nullopt;')
-                    if 'ReturnEarlyIfTrue' in member.attributes:
+                    indentation = 1
+                    if 'Nullable' in member.attributes:
+                        indentation = 2
+                        result.append('    std::optional<bool> has' + member.name + ';')
+                        result.append('    decoder >> has' + member.name + ';')
+                        result.append('    if (!has' + member.name + ')')
+                        result.append('        return std::nullopt;')
+                        result.append('    if (*has' + member.name + ') {')
+                    result.append(indent(indentation) + sanitize_string_for_variable_name(member.name) + ' = IPC::decode<' + match.groups()[0] + '>(decoder, ' + soft_linked_classes[0] + ');')
+                    result.append(indent(indentation) + 'if (!' + sanitize_string_for_variable_name(member.name) + ')')
+                    result.append(indent(indentation) + '    return std::nullopt;')
+                    if 'Nullable' in member.attributes:
+                        result.append('    } else')
+                        result.append('        ' + member.name + ' = std::optional<' + member.type + '> { ' + member.type + ' { } };')
+                    elif 'ReturnEarlyIfTrue' in member.attributes:
                         result.append('    if (*' + sanitize_string_for_variable_name(member.name) + ')')
                         result.append('        return { ' + type.namespace_and_name() + ' { } };')
+                elif 'Nullable' in member.attributes:
+                    result.append('    std::optional<bool> has' + member.name + ';')
+                    result.append('    decoder >> has' + member.name + ';')
+                    result.append('    if (!has' + member.name + ')')
+                    result.append('        return std::nullopt;')
+                    result.append('    if (*has' + member.name + ') {')
+                    # FIXME: This should be below
+                    result.append('        decoder >> ' + member.name + ';')
+                    result.append('        if (!' + member.name + ')')
+                    result.append('            return std::nullopt;')
+                    result.append('    } else')
+                    result.append('        ' + member.name + ' = std::optional<' + member.type + '> { ' + member.type + ' { } };')
                 else:
                     assert len(soft_linked_classes) == 0
                     result.append('    decoder >> ' + sanitize_string_for_variable_name(member.name) + ';')

Source/WebKit/Shared/mac/WebHitTestResultPlatformData.serialization.in
@@ -26,7 +26,7 @@ headers: <WebCore/TextIndicator.h>
 #if PLATFORM(MAC)
 header: "WebHitTestResultData.h" <pal/mac/DataDetectorsSoftLink.h>
 [CustomHeader] struct WebKit::WebHitTestResultPlatformData {
-    [SoftLinkedClass='PAL::getDDActionContextClass()'] RetainPtr<DDActionContext> detectedDataActionContext;
+    [Nullable, SoftLinkedClass='PAL::getDDActionContextClass()'] RetainPtr<DDActionContext> detectedDataActionContext;
     WebCore::FloatRect detectedDataBoundingBox;
     RefPtr<WebCore::TextIndicator> detectedDataTextIndicator;
     WebCore::PageOverlay::PageOverlayID detectedDataOriginatingPageOverlay;

Source/WebKit/Scripts/webkit/tests/GeneratedSerializers.cpp
@@ -515,6 +515,44 @@ std::optional<WebCore::FloatBoxExtent> ArgumentCoder<WebCore::FloatBoxExtent>::d
     };
 }
 
+
+void ArgumentCoder<NullableSoftLinkedMember>::encode(Encoder& encoder, const NullableSoftLinkedMember& instance)
+{
+    static_assert(std::is_same_v<std::remove_cvref_t<decltype(instance.firstMember)>, RetainPtr<DDActionContext>>);
+    static_assert(std::is_same_v<std::remove_cvref_t<decltype(instance.secondMember)>, RetainPtr<DDActionContext>>);
+    encoder << !!instance.firstMember;
+    if (!!instance.firstMember)
+        encoder << instance.firstMember;
+    encoder << instance.secondMember;
+}
+
+std::optional<NullableSoftLinkedMember> ArgumentCoder<NullableSoftLinkedMember>::decode(Decoder& decoder)
+{
+    std::optional<RetainPtr<DDActionContext>> firstMember;
+    std::optional<bool> hasfirstMember;
+    decoder >> hasfirstMember;
+    if (!hasfirstMember)
+        return std::nullopt;
+    if (*hasfirstMember) {
+        firstMember = IPC::decode<DDActionContext>(decoder, PAL::getDDActionContextClass());
+        if (!firstMember)
+            return std::nullopt;
+    } else
+        firstMember = std::optional<RetainPtr<DDActionContext>> { RetainPtr<DDActionContext> { } };
+
+    std::optional<RetainPtr<DDActionContext>> secondMember;
+    secondMember = IPC::decode<DDActionContext>(decoder, PAL::getDDActionContextClass());
+    if (!secondMember)
+        return std::nullopt;
+
+    return {
+        NullableSoftLinkedMember {
+            WTFMove(*firstMember),
+            WTFMove(*secondMember)
+        }
+    };
+}
+
 } // namespace IPC
 
 namespace WTF {

Source/WebKit/Scripts/webkit/tests/GeneratedSerializers.h
@@ -51,6 +51,7 @@ namespace WebCore {
 template<typename> class RectEdges;
 using FloatBoxExtent = RectEdges<float>;
 }
+struct NullableSoftLinkedMember;
 
 namespace IPC {
 
@@ -115,6 +116,11 @@ template<> struct ArgumentCoder<WTF::CreateUsingClass> {
 template<> struct ArgumentCoder<WebCore::FloatBoxExtent> {
     static void encode(Encoder&, const WebCore::FloatBoxExtent&);
     static std::optional<WebCore::FloatBoxExtent> decode(Decoder&);
+};
+
+template<> struct ArgumentCoder<NullableSoftLinkedMember> {
+    static void encode(Encoder&, const NullableSoftLinkedMember&);
+    static std::optional<NullableSoftLinkedMember> decode(Decoder&);
 };
 
 } // namespace IPC

Source/WebKit/Scripts/webkit/tests/SerializedTypeInfo.cpp
@@ -100,6 +100,10 @@ Vector<SerializedTypeInfo> allSerializedTypes()
             "float"_s,
             "float"_s,
         } },
+        { "NullableSoftLinkedMember"_s, {
+            "RetainPtr<DDActionContext>"_s,
+            "RetainPtr<DDActionContext>"_s,
+        } },
     };
 }
 

Source/WebKit/Scripts/webkit/tests/TestSerializedType.serialization.in
@@ -96,3 +96,8 @@ class WTF::Seconds {
     float bottom()
     float left()
 };
+
+struct NullableSoftLinkedMember {
+    [Nullable, SoftLinkedClass='PAL::getDDActionContextClass()'] RetainPtr<DDActionContext> firstMember;
+    [SoftLinkedClass='PAL::getDDActionContextClass()'] RetainPtr<DDActionContext> secondMember;
+}