679cabe97f229ee764bbe99da0c0a68183236f75
Commit Queue <commit-queue@webkit.org>
2022-12-08 11:41:46 -0800

    Unreviewed, reverting r257449@main. https://bugs.webkit.org/show_bug.cgi?id=248965
    TC39 does not have consensus on naming yet
    
    Reverted changeset:
    
    "[JSC] Update Intl.Locale info's getter to method"
    https://bugs.webkit.org/show_bug.cgi?id=248836
    https://commits.webkit.org/257449@main
    
    Canonical link: https://commits.webkit.org/257577@main

--- JSTests/stress/intl-locale-invalid-hourCycles.js
+++ JSTests/stress/intl-locale-invalid-hourCycles.js
@@ -1,12 +1,7 @@
-function touch(arg) {
-    return arg + "Hello"; // Touching.
-}
-noInline(touch);
-
 function main() {
     const v24 = new Intl.Locale("trimEnd", { 'numberingSystem': "foobar" });
-    let empty = v24.hourCycles();
-    touch(empty);
+    let empty = v24.hourCycles;
+    print(empty);
 }
 
 try {
