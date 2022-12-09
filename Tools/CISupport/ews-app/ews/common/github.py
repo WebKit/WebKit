7d063a43b838cb35afcd1a703f2194626614f62c
Aakash Jain <aakash_jain@apple.com>
2022-12-09 15:10:11 -0800

    [ews] Improve handling of non-existing status-bubbles https://bugs.webkit.org/show_bug.cgi?id=249045 rdar://103194910
    Reviewed by Ryan Haddad.
    
    * Tools/CISupport/ews-app/ews/common/github.py:
    (GitHubEWS):
    (GitHubEWS.github_status_for_queue):
    
    Canonical link: https://commits.webkit.org/257648@main

--- Tools/CISupport/ews-app/ews/common/github.py
+++ Tools/CISupport/ews-app/ews/common/github.py
@@ -201,13 +201,13 @@ class GitHubEWS(GitHub):
     STATUS_BUBBLE_START = u'<!--EWS-Status-Bubble-Start-->'
     STATUS_BUBBLE_END = u'<!--EWS-Status-Bubble-End-->'
     STATUS_BUBBLE_ROWS = [['style', 'ios', 'mac', 'wpe', 'win'],  # FIXME: generate this list dynamically to have merge queue show up on top
-                          ['bindings', 'ios-sim', 'mac-debug', 'gtk', 'wincairo'],
-                          ['webkitperl', 'ios-wk2', 'mac-AS-debug', 'gtk-wk2', ''],
-                          ['webkitpy', 'api-ios', 'api-mac', 'api-gtk', ''],
-                          ['jsc', 'tv', 'mac-wk1', 'jsc-armv7', ''],
-                          ['services', 'tv-sim', 'mac-wk2', 'jsc-armv7-tests', ''],
-                          ['merge', 'watch', 'mac-AS-debug-wk2', 'jsc-mips', ''],
-                          ['unsafe-merge', 'watch-sim', 'mac-wk2-stress', 'jsc-mips-tests', '']]
+                          ['bindings', 'ios-sim', 'mac-AS-debug', 'gtk', 'wincairo'],
+                          ['webkitperl', 'ios-wk2', 'api-mac', 'gtk-wk2', ''],
+                          ['webkitpy', 'api-ios', 'mac-wk1', 'api-gtk', ''],
+                          ['jsc', 'tv', 'mac-wk2', 'jsc-armv7', ''],
+                          ['services', 'tv-sim', 'mac-AS-debug-wk2', 'jsc-armv7-tests', ''],
+                          ['merge', 'watch', 'mac-wk2-stress', 'jsc-mips', ''],
+                          ['unsafe-merge', 'watch-sim', '', 'jsc-mips-tests', '']]
 
     @classmethod
     def generate_updated_pr_description(self, description, ews_comment):
@@ -266,6 +266,7 @@ class GitHubEWS(GitHub):
             if Buildbot.get_parent_queue(queue):
                 queue = Buildbot.get_parent_queue(queue)
             queue_full_name = Buildbot.queue_name_by_shortname_mapping.get(queue)
+            url = ''
             if queue_full_name:
                 url = 'https://{}/#/builders/{}'.format(config.BUILDBOT_SERVER_HOST, queue_full_name)
             hover_over_text = 'Waiting in queue, processing has not started yet'
