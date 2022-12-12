b0febf1cc635f1e2118218ff99b19c2fa31767c3
Ryan Haddad <ryanhaddad@apple.com>
2022-12-12 13:49:33 -0800

    [webkitpy] Increase the default number of child processes spawned for arm64 Macs https://bugs.webkit.org/show_bug.cgi?id=248625 rdar://102873021
    Reviewed by Jonathan Bedard and Geoffrey Garen.
    
    arm64 Mac hardware can handle more WebKitTestRunner processes than the
    default number we calculate. Local testing suggests that doubling the count
    yields an 18 minute speedup in overall test runtime on M1 hardware, so lets
    hardcode this increase until we implement a more sophisticated test scheduling
    mechanism.
    
    * Tools/Scripts/webkitpy/port/mac.py:
    (MacPort.default_child_processes):
    
    Canonical link: https://commits.webkit.org/257754@main

--- Tools/Scripts/webkitpy/port/mac.py
+++ Tools/Scripts/webkitpy/port/mac.py
@@ -225,6 +225,11 @@ class MacPort(DarwinPort):
     def default_child_processes(self, **kwargs):
         default_count = super(MacPort, self).default_child_processes()
 
+        # FIXME: arm64 Mac hardware can handle more processes than the default number we calculate.
+        # Double the amount of default workers until we implement more sophisticated test scheduling.
+        if self.architecture() == 'arm64':
+            default_count = default_count * 2
+
         # FIXME: https://bugs.webkit.org/show_bug.cgi?id=95906  With too many WebProcess WK2 tests get stuck in resource contention.
         # To alleviate the issue reduce the number of running processes
         # Anecdotal evidence suggests that a 4 core/8 core logical machine may run into this, but that a 2 core/4 core logical machine does not.
