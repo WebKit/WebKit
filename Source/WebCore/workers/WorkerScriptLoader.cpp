3a91df74661a1f3d5e3b98d3d0f2589c014f81d8
David Kilzer <ddkilzer@apple.com>
2022-12-12 11:36:55 -0800

    Fix use-after-move in WebCore::WorkerScriptLoader::loadAsynchronously() https://bugs.webkit.org/show_bug.cgi?id=249062 <rdar://problem/103203412>
    Reviewed by Youenn Fablet.
    
    Fix the use-after-move by using `m_url` instead of
    `scriptRequest.url()` after `request` was created using
    `WTFMove(scriptRequest)`.
    
    * Source/WebCore/workers/WorkerScriptLoader.cpp:
    (WebCore::WorkerScriptLoader::loadAsynchronously):
    
    Canonical link: https://commits.webkit.org/257748@main

--- Source/WebCore/workers/WorkerScriptLoader.cpp
+++ Source/WebCore/workers/WorkerScriptLoader.cpp
@@ -179,7 +179,7 @@ void WorkerScriptLoader::loadAsynchronously(ScriptExecutionContext& scriptExecut
 #endif
 
     if (m_destination == FetchOptions::Destination::Sharedworker)
-        m_userAgentForSharedWorker = scriptExecutionContext.userAgent(scriptRequest.url());
+        m_userAgentForSharedWorker = scriptExecutionContext.userAgent(m_url);
 
     // During create, callbacks may happen which remove the last reference to this object.
     Ref<WorkerScriptLoader> protectedThis(*this);
