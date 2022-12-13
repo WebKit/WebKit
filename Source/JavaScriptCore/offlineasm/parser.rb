b7ac5d0ccc7ca3ed6bf4f2d40fde60247ce87d3c
Dominique Leuenberger <dimstar@opensuse.org>
2022-12-12 21:24:11 -0800

    Fix build with Ruby 3.2 https://bugs.webkit.org/show_bug.cgi?id=246743
    Reviewed by Ross Kirsling.
    
    File.exists has been declared deprecated since Ruby 2.1 (2013) and with Ruby 3.2 gets removed for good.
    
    * Source/JavaScriptCore/offlineasm/config.rb:
    * Source/JavaScriptCore/offlineasm/parser.rb:
    * Source/WebInspectorUI/Scripts/update-LegacyInspectorBackendCommands.rb:
    
    Canonical link: https://commits.webkit.org/257775@main

--- Source/JavaScriptCore/offlineasm/parser.rb
+++ Source/JavaScriptCore/offlineasm/parser.rb
@@ -840,10 +840,10 @@ class Parser
                     additionsDirectoryName = "#{@buildProductsDirectory}#{@headersFolderPath}/WebKitAdditions/"
                 end
                 fileName = IncludeFile.new(moduleName, additionsDirectoryName).fileName
-                if not File.exists?(fileName)
+                if not File.exist?(fileName)
                     fileName = IncludeFile.new(moduleName, @tokens[@idx].codeOrigin.fileName.dirname).fileName
                 end
-                fileExists = File.exists?(fileName)
+                fileExists = File.exist?(fileName)
                 raise "File not found: #{fileName}" if not fileExists and not isOptional
                 list << parse(fileName, @options, @sources) if fileExists
             else
@@ -876,10 +876,10 @@ class Parser
                     additionsDirectoryName = "#{@buildProductsDirectory}#{@headersFolderPath}/WebKitAdditions/"
                 end
                 fileName = IncludeFile.new(moduleName, additionsDirectoryName).fileName
-                if not File.exists?(fileName)
+                if not File.exist?(fileName)
                     fileName = IncludeFile.new(moduleName, @tokens[@idx].codeOrigin.fileName.dirname).fileName
                 end
-                fileExists = File.exists?(fileName)
+                fileExists = File.exist?(fileName)
                 raise "File not found: #{fileName}" if not fileExists and not isOptional
                 if fileExists
                     parser = Parser.new(readTextFile(fileName), SourceFile.new(fileName), options)
