dc19977ced76d65c66f5903b23d85e391500e454
Miguel Salinas <miguel_salinas@apple.com>
2022-12-14 14:39:17 -0800

    [Cocoa] Use a link check for background web content suspension https://bugs.webkit.org/show_bug.cgi?id=249157 rdar://problem/103258911
    Reviewed by Chris Dumez.
    
    Add a link check for fully suspending background web content processes.
    
    * Source/WTF/wtf/cocoa/RuntimeApplicationChecksCocoa.h:
    * Source/WebKit/UIProcess/ProcessThrottler.cpp:
    (WebKit::ProcessThrottler::setShouldTakeSuspendedAssertion):
    
    Canonical link: https://commits.webkit.org/257878@main

--- Source/WebKit/UIProcess/ProcessThrottler.cpp
+++ Source/WebKit/UIProcess/ProcessThrottler.cpp
@@ -32,6 +32,10 @@
 #include <wtf/CompletionHandler.h>
 #include <wtf/text/TextStream.h>
 
+#if PLATFORM(COCOA)
+#include <wtf/cocoa/RuntimeApplicationChecksCocoa.h>
+#endif
+
 namespace WebKit {
     
 static const Seconds processSuspensionTimeout { 20_s };
@@ -316,6 +320,10 @@ void ProcessThrottler::setAllowsActivities(bool allow)
 
 void ProcessThrottler::setShouldTakeSuspendedAssertion(bool shouldTakeSuspendedAssertion)
 {
+#if PLATFORM(COCOA)
+    if (!linkedOnOrAfterSDKWithBehavior(SDKAlignedBehavior::FullySuspendsBackgroundContent))
+        shouldTakeSuspendedAssertion = true;
+#endif
     const bool shouldUpdateAssertion = m_shouldTakeSuspendedAssertion != shouldTakeSuspendedAssertion;
     m_shouldTakeSuspendedAssertion = shouldTakeSuspendedAssertion;
     if (shouldUpdateAssertion && m_state == ProcessThrottleState::Suspended)
