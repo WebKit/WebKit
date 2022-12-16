4db09a03542040dba6c022e7d217d3eb526654d7
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2022-12-15 16:20:22 -0800

    HTMLImageElement width/height should update renderer first
    HTMLImageElement width/height should update renderer first
    https://bugs.webkit.org/show_bug.cgi?id=249056
    
    Reviewed by Simon Fraser.
    
    This patch is to align Webkit with Gecko / Firefox and Blink / Chromium.
    
    Merge - https://chromium.googlesource.com/chromium/blink/+/d3adbd3a3f1bcbebdf515ac15a8c250873a2a1d9
    
    HTMLImageElement::width/height were not updating layout first before checking for the
    renderer, which means that we fall back to the attribute when we shouldn't. We're also
    using the renderer method instead of updateLayoutIgnorePendingStylesheets which should be
    used for all JS APIs. It also enabled us to remove unused bool.
    
    We also make sure to check if we're in the document before causing a layout so checking
    for a disconnected image's width/height before drawing to a canvas doesn't cause a sync layout.
    
    * Source/WebCore/HTMLImageElement.h: Remove unused 'boolean'
    * Source/WebCore/HTMLImageElement.cpp:
    (HTMLImageElement::width): Update to account for renderer
    (HTMLImageElement::height): Ditto
    * LayoutTests/fast/image/img-dimensions-styled.html: Add Test Case
    * LayoutTests/fast/image/img-dimensions-styled-expected.txt: Add Test Case Expectation
    
    Canonical link: https://commits.webkit.org/257961@main

--- Source/WebCore/html/HTMLImageElement.cpp
+++ Source/WebCore/html/HTMLImageElement.cpp
@@ -1,8 +1,8 @@
 /*
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
- * Copyright (C) 2004-2016 Apple Inc. All rights reserved.
- * Copyright (C) 2010 Google Inc. All rights reserved.
+ * Copyright (C) 2004-2022 Apple Inc. All rights reserved.
+ * Copyright (C) 2010-2015 Google Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -501,8 +501,11 @@ void HTMLImageElement::setPictureElement(HTMLPictureElement* pictureElement)
     m_pictureElement = pictureElement;
 }
     
-unsigned HTMLImageElement::width(bool ignorePendingStylesheets)
+unsigned HTMLImageElement::width()
 {
+    if (inRenderedDocument())
+        document().updateLayoutIgnorePendingStylesheets();
+
     if (!renderer()) {
         // check the attribute first for an explicit pixel value
         auto optionalWidth = parseHTMLNonNegativeInteger(attributeWithoutSynchronization(widthAttr));
@@ -514,11 +517,6 @@ unsigned HTMLImageElement::width(bool ignorePendingStylesheets)
             return m_imageLoader->image()->imageSizeForRenderer(renderer(), 1.0f).width().toUnsigned();
     }
 
-    if (ignorePendingStylesheets)
-        document().updateLayoutIgnorePendingStylesheets();
-    else
-        document().updateLayout();
-
     RenderBox* box = renderBox();
     if (!box)
         return 0;
@@ -526,8 +524,11 @@ unsigned HTMLImageElement::width(bool ignorePendingStylesheets)
     return adjustForAbsoluteZoom(snappedIntRect(contentRect).width(), *box);
 }
 
-unsigned HTMLImageElement::height(bool ignorePendingStylesheets)
+unsigned HTMLImageElement::height()
 {
+    if (inRenderedDocument())
+        document().updateLayoutIgnorePendingStylesheets();
+
     if (!renderer()) {
         // check the attribute first for an explicit pixel value
         auto optionalHeight = parseHTMLNonNegativeInteger(attributeWithoutSynchronization(heightAttr));
@@ -538,11 +539,6 @@ unsigned HTMLImageElement::height(bool ignorePendingStylesheets)
         if (m_imageLoader->image())
             return m_imageLoader->image()->imageSizeForRenderer(renderer(), 1.0f).height().toUnsigned();
     }
-
-    if (ignorePendingStylesheets)
-        document().updateLayoutIgnorePendingStylesheets();
-    else
-        document().updateLayout();
 
     RenderBox* box = renderBox();
     if (!box)
