ea7b3989b369d140fcf55e9962380d663a79d905
Antoine Quint <graouts@webkit.org>
2022-12-17 03:12:03 -0800

    [web-animations] add an AnimatableProperty type to encapsulate either a standard or custom property https://bugs.webkit.org/show_bug.cgi?id=249498
    Reviewed by Darin Adler.
    
    The animation code is currently heavily biased towards dealing with "standard" CSS properties, represented
    by the CSSPropertyID enum. However, with the work to add interpolation support for custom properties (see
    bug 249293) and to support CSS Transitions for custom properties (see bug 249399), we really need to deal
    with both custom properties and standard properties everywhere we currently deal with only standard
    properties.
    
    In this patch we're adding a new type, AnimatableProperty, which is an std::variant<CSSPropertyID, AtomString>.
    We currently use CSSPropertyID as a value for property lists which are HashSet<> and as keys to HashMap<>,
    so we must add the relevant traits to support this use for AnimatableProperty.
    
    * Source/WebCore/animation/WebAnimationTypes.h:
    (WTF::DefaultHash<WebCore::AnimatableProperty>::hash):
    (WTF::DefaultHash<WebCore::AnimatableProperty>::equal):
    (WTF::HashTraits<WebCore::AnimatableProperty>::constructDeletedValue):
    (WTF::HashTraits<WebCore::AnimatableProperty>::isDeletedValue):
    
    Canonical link: https://commits.webkit.org/258040@main

--- Source/WebCore/animation/WebAnimationTypes.h
+++ Source/WebCore/animation/WebAnimationTypes.h
@@ -25,10 +25,13 @@
 
 #pragma once
 
+#include "CSSPropertyNames.h"
 #include "CSSValue.h"
 #include <wtf/HashMap.h>
 #include <wtf/ListHashSet.h>
 #include <wtf/Markable.h>
+#include <wtf/text/AtomString.h>
+#include <wtf/text/AtomStringHash.h>
 
 namespace WebCore {
 
@@ -64,6 +67,45 @@ using AnimationEvents = Vector<Ref<AnimationEventBase>>;
 using PropertyToTransitionMap = HashMap<CSSPropertyID, RefPtr<CSSTransition>>;
 using CSSAnimationCollection = ListHashSet<RefPtr<CSSAnimation>>;
 
+using AnimatableProperty = std::variant<CSSPropertyID, AtomString>;
+using AnimatablePropertyToTransitionMap = HashMap<AnimatableProperty, RefPtr<CSSTransition>>;
+
 } // namespace WebCore
 
+namespace WTF {
 
+template<> struct DefaultHash<WebCore::AnimatableProperty> {
+    static unsigned hash(const WebCore::AnimatableProperty& key) {
+        return WTF::switchOn(key,
+            [] (WebCore::CSSPropertyID property) {
+                return DefaultHash<WebCore::CSSPropertyID>::hash(property);
+            },
+            [] (const AtomString& string) {
+                return DefaultHash<AtomString>::hash(string);
+            }
+        );
+    }
+    static bool equal(const WebCore::AnimatableProperty& a, const WebCore::AnimatableProperty& b) { return a == b; }
+    static const bool safeToCompareToEmptyOrDeleted = true;
+};
+
+template<> struct HashTraits<WebCore::AnimatableProperty> : GenericHashTraits<WebCore::AnimatableProperty> {
+    static const bool emptyValueIsZero = true;
+    static void constructDeletedValue(WebCore::AnimatableProperty& slot) {
+        WebCore::CSSPropertyID property;
+        HashTraits<WebCore::CSSPropertyID>::constructDeletedValue(property);
+        new (NotNull, &slot) WebCore::AnimatableProperty(property);
+    }
+    static bool isDeletedValue(const WebCore::AnimatableProperty& value) {
+        return WTF::switchOn(value,
+            [] (WebCore::CSSPropertyID property) {
+                return HashTraits<WebCore::CSSPropertyID>::isDeletedValue(property);
+            },
+            [] (const AtomString&) {
+                return false;
+            }
+        );
+    }
+};
+
+} // namespace WTF
