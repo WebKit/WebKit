b71def9bee24f243d2c26860459435c726625744
Alan Coon <alancoon@apple.com>
2022-12-20 11:30:04 -0800

    Revert 711178d9ca19. rdar://problem/103539448
    This reverts commit fb93c70dab69c3d3dfed0be6362d4a7b00bce749.
    
    Canonical link: https://commits.webkit.org/252432.1006@safari-7614-branch

--- Source/WebCore/rendering/RenderLayerScrollableArea.cpp
+++ Source/WebCore/rendering/RenderLayerScrollableArea.cpp
@@ -1663,7 +1663,7 @@ void RenderLayerScrollableArea::updateScrollableAreaSet(bool hasOverflow)
 
 #if ENABLE(IOS_TOUCH_EVENTS)
     if (addedOrRemoved) {
-        if (needsToBeRegistered && !canUseCompositedScrolling())
+        if (isScrollable && !canUseCompositedScrolling())
             registerAsTouchEventListenerForScrolling();
         else {
             // We only need the touch listener for unaccelerated overflow scrolling, so if we became
