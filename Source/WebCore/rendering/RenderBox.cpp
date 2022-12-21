8c71be5bafe9bd7fb255f44640bf1545a9b65210
Sammy Gill <sammy.gill@apple.com>
2022-12-21 13:08:53 -0800

    Cherry-pick 28c5d0e82b0f. rdar://problem/103537352
        Ensure transferred max size is floored by transferred min size in RenderReplaced::computeIntrinsicSizesConstrainedByTransferredMinMaxSizes.
        https://bugs.webkit.org/show_bug.cgi?id=249617
        rdar://103537352
    
        Reviewed by Alan Baradlay.
    
        Addresses a regression introduced by a change inside of
        computeIntrinsicSizesConstrainedByTransferredMinMaxSizes. This method is
        supposed to compute the intrinsic size and ratio of a replaced element and
        also restrict that size if there are any transferred min/max constraints
        that are violated. This code was not checking if the transferred max size
        became smaller than the transferred min size. The max size is supposed to
        be floored by the min size in order to avoid this scenario.
    
        One situation in which this can happen is when we are computing the
        min/max logical height, the containing block has an available width
        of 0px, and the max-width specified is percentage based. When the call
        to computeLogicalWidthInFragmentUsing returns, it will return a value of
        0px when it resolves the percentage based max-width. Then, any specified
        value of min-width greater than 0px will cause this issue. The new code
        added addresses this issue by setting the max size to
        max(min size, max size).
    
        Ideally, we would be able to use RenderBox::constrainLogicalMinMaxSizesByAspectRatio,
        which has this logic for us, but my attempt to do this was causing issues
        when replaced elements were inside a flexbox.
    
        Spec reference: https://www.w3.org/TR/css-sizing-4/#aspect-ratio-size-transfers
    
        * LayoutTests/imported/w3c/web-platform-tests/css/css-sizing/aspect-ratio/replaced-element-041-expected.html: Added.
        * LayoutTests/imported/w3c/web-platform-tests/css/css-sizing/aspect-ratio/replaced-element-041.html: Added.
        * Source/WebCore/rendering/RenderBox.cpp:
        (WebCore::RenderBox::computeMinMaxLogicalWidthFromAspectRatio const):
        (WebCore::RenderBox::computeMinMaxLogicalHeightFromAspectRatio const):
    
        Canonical link: https://commits.webkit.org/258210@main
    
    Canonical link: https://commits.webkit.org/252432.1009@safari-7614.4.6.1-branch

--- Source/WebCore/rendering/RenderBox.cpp
+++ Source/WebCore/rendering/RenderBox.cpp
@@ -5577,6 +5577,8 @@ std::pair<LayoutUnit, LayoutUnit> RenderBox::computeMinMaxLogicalWidthFromAspect
         if (LayoutUnit blockMaxSize = constrainLogicalHeightByMinMax(LayoutUnit::max(), std::nullopt); blockMaxSize != LayoutUnit::max())
             transferredMaxSize = inlineSizeFromAspectRatio(borderAndPaddingLogicalWidth(), borderAndPaddingLogicalHeight(), *aspectRatio, style().boxSizingForAspectRatio(), blockMaxSize, style().aspectRatioType(), isRenderReplaced());
     }
+    // Spec says the transferred max size should be floored by the transferred min size
+    transferredMaxSize = std::max(transferredMinSize, transferredMaxSize);
     return { transferredMinSize, transferredMaxSize };
 }
 
@@ -5597,6 +5599,8 @@ std::pair<LayoutUnit, LayoutUnit> RenderBox::computeMinMaxLogicalHeightFromAspec
         if (LayoutUnit inlineMaxSize = computeLogicalWidthInFragmentUsing(MaxSize, style().logicalMaxWidth(), containingBlockLogicalWidthForContent(), *containingBlock(), nullptr); inlineMaxSize != LayoutUnit::max())
             transferredMaxSize = blockSizeFromAspectRatio(borderAndPaddingLogicalWidth(), borderAndPaddingLogicalHeight(), *aspectRatio, style().boxSizingForAspectRatio(), inlineMaxSize, style().aspectRatioType(), isRenderReplaced());
     }
+    // Spec says the transferred max size should be floored by the transferred min size 
+    transferredMaxSize = std::max(transferredMinSize, transferredMaxSize);
     return { transferredMinSize, transferredMaxSize };
 }
 
