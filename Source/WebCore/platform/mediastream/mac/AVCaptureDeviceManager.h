83ba7a2701beb6dcb3a26012a9f8b2cc049be5dc
Eric Carlson <eric.carlson@apple.com>
2022-12-22 09:14:14 -0800

    Cherry-pick b96cc31eec98. rdar://problem/103003618
        [Cocoa] Softlink macros are not thread safe
        https://bugs.webkit.org/show_bug.cgi?id=249725
        rdar://103003618
    
        Reviewed by Dean Jackson.
    
        Load and cache AVCaptureDeviceType constants in the AVCaptureDeviceManager constructor,
        which is always called on the main thread, so they can be used on the dispatch queue
        used to discover the current capture devices.
    
        * Source/WebCore/platform/mediastream/mac/AVCaptureDeviceManager.h:
        * Source/WebCore/platform/mediastream/mac/AVCaptureDeviceManager.mm:
        (WebCore::AVCaptureDeviceManager::currentCameras): Use m_avCaptureDeviceTypes instead
        of the soft link functions directly.
        (WebCore::AVCaptureDeviceManager::AVCaptureDeviceManager): Cache AVCaptureDeviceType constants.
        (WebCore::currentCameras): Deleted.
    
        Canonical link: https://commits.webkit.org/258244@main
    
    Canonical link: https://commits.webkit.org/257979.11@safari-7615.1.16-branch

--- Source/WebCore/platform/mediastream/mac/AVCaptureDeviceManager.h
+++ Source/WebCore/platform/mediastream/mac/AVCaptureDeviceManager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2013-2022 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -64,10 +64,12 @@ private:
     void registerForDeviceNotifications();
     void updateCachedAVCaptureDevices();
     Vector<CaptureDevice> retrieveCaptureDevices();
+    RetainPtr<NSArray> currentCameras();
 
     RetainPtr<WebCoreAVCaptureDeviceManagerObserver> m_objcObserver;
     Vector<CaptureDevice> m_devices;
     RetainPtr<NSMutableArray> m_avCaptureDevices;
+    RetainPtr<NSArray> m_avCaptureDeviceTypes;
     bool m_isInitialized { false };
 
     Ref<WorkQueue> m_dispatchQueue;
