0a821c7d68ea240665ed1a0aaceefe1bddbebb5f
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2022-12-21 18:20:48 -0800

    Make sure rows are updated during simplified layout
    Make sure rows are updated during simplified layout
    https://bugs.webkit.org/show_bug.cgi?id=249707
    
    Reviewed by Alan Baradlay.
    
    Merge - https://src.chromium.org/viewvc/blink?revision=191146&view=revision
    
    When we are calling RenderTable::simplifiedNormalFlowLayout(), we have to
    make sure that we call section->layoutRows() before we do the
    section->computeOverflowFromCells(). The call to layoutRows is needed because
    it will reset and update the m_forceSlowPaintPathWithOverflowingCell
    flag which causes to fail the ASSERT.
    
    * Source/WebCore/rendering/RenderTable.cpp:
    (RenderTable::simplifiedNormalFlowLayout): Add "layoutRows" before "computeOverflowFromCells"
    * LayoutTests/fast/table/table-overflow-crash.html: Add Test Case
    * LayoutTests/fast/table/table-overflow-crash-expected.txt: Add Test Case Expectation
    
    Canonical link: https://commits.webkit.org/258227@main

--- Source/WebCore/rendering/RenderTable.cpp
+++ Source/WebCore/rendering/RenderTable.cpp
@@ -4,7 +4,8 @@
  *           (C) 1998 Waldo Bastian (bastian@kde.org)
  *           (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
- * Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2014 Apple Inc. All rights reserved.
+ * Copyright (C) 2003-2022 Apple Inc. All rights reserved.
+ * Copyright (C) 2015 Google Inc. All rights reserved.
  * Copyright (C) 2006 Alexey Proskuryakov (ap@nypop.com)
  *
  * This library is free software; you can redistribute it and/or
@@ -419,6 +420,7 @@ void RenderTable::simplifiedNormalFlowLayout()
         caption->layoutIfNeeded();
     for (RenderTableSection* section = topSection(); section; section = sectionBelow(section)) {
         section->layoutIfNeeded();
+        section->layoutRows();
         section->computeOverflowFromCells();
     }
 }
