WebKit/WebKit

commit a5f12d1b9232202fd842ed8b615d2baed86aa271

[GPU Process] [FormControls] Add a ControlPart for ToggleButton https://bugs.webkit.org/show_bug.cgi?id=249638 rdar://103548470
Reviewed by Aditya Keerthi.

It will handle drawing the checkbox and the radio buttons. ButtonControlMac will
be introduced as the base class of ToggleButtonMac. It will also be the base class
of the ButtonMac which will be submitted in a future patch.

* Source/WebCore/Headers.cmake:
* Source/WebCore/SourcesCocoa.txt:
* Source/WebCore/WebCore.xcodeproj/project.pbxproj:
* Source/WebCore/platform/graphics/controls/ControlFactory.h:
* Source/WebCore/platform/graphics/controls/ToggleButtonPart.h: Copied from Source/WebCore/platform/graphics/controls/ControlFactory.h.
* Source/WebCore/platform/graphics/mac/controls/ButtonControlMac.h: Copied from Source/WebCore/platform/graphics/controls/ControlFactory.h.
* Source/WebCore/platform/graphics/mac/controls/ButtonControlMac.mm: Added.
(WebCore::ButtonControlMac::ButtonControlMac):
(WebCore::ButtonControlMac::updateCellStates):
* Source/WebCore/platform/graphics/mac/controls/ControlFactoryMac.h:
* Source/WebCore/platform/graphics/mac/controls/ControlFactoryMac.mm:
(WebCore::createToggleButtonCell):
(WebCore::ControlFactoryMac::checkboxCell const):
(WebCore::ControlFactoryMac::radioCell const):
(WebCore::ControlFactoryMac::createPlatformToggleButton):
* Source/WebCore/platform/graphics/mac/controls/ControlMac.h:
(WebCore::ControlMac::cellSize const):
(WebCore::ControlMac::cellOutsets const):
* Source/WebCore/platform/graphics/mac/controls/ControlMac.mm:
(WebCore::ControlMac::calculateControlSize const):
* Source/WebCore/platform/graphics/mac/controls/ToggleButtonMac.h: Copied from Source/WebCore/platform/graphics/controls/ControlFactory.h.
* Source/WebCore/platform/graphics/mac/controls/ToggleButtonMac.mm: Added.
(WebCore::ToggleButtonMac::ToggleButtonMac):
(WebCore::ToggleButtonMac::cellSize const):
(WebCore::ToggleButtonMac::cellOutsets const):
(WebCore::ToggleButtonMac::draw):

Source/WebCore/platform/graphics/mac/controls/ButtonControlMac.mm
@@ -1,0 +1,83 @@
+/*
+ * Copyright (C) 2022 Apple Inc. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "config.h"
+#import "ButtonControlMac.h"
+
+#if PLATFORM(MAC)
+
+#import <pal/spi/cocoa/NSButtonCellSPI.h>
+
+namespace WebCore {
+
+ButtonControlMac::ButtonControlMac(ControlPart& part, ControlFactoryMac& controlFactory, NSButtonCell *buttonCell)
+    : ControlMac(part, controlFactory)
+    , m_buttonCell(buttonCell)
+{
+    ASSERT(m_buttonCell);
+}
+
+void ButtonControlMac::updateCellStates(const FloatRect& rect, const ControlStyle& style)
+{
+    const auto& states = style.states;
+
+    // Pressed state
+    bool oldPressed = [m_buttonCell isHighlighted];
+    bool pressed = states.contains(ControlStyle::State::Pressed);
+    if (pressed != oldPressed)
+        [m_buttonCell _setHighlighted:pressed animated:false];
+
+    // Enabled state
+    bool oldEnabled = [m_buttonCell isEnabled];
+    bool enabled = states.contains(ControlStyle::State::Enabled);
+    if (oldEnabled != enabled)
+        [m_buttonCell setEnabled:enabled];
+
+    // Checked and Indeterminate
+    bool oldIndeterminate = [m_buttonCell state] == NSControlStateValueMixed;
+    bool oldChecked = [m_buttonCell state] == NSControlStateValueOn;
+    bool indeterminate = states.contains(ControlStyle::State::Indeterminate);
+    bool checked = states.contains(ControlStyle::State::Checked);
+    if (oldIndeterminate != indeterminate || checked != oldChecked) {
+        NSControlStateValue newState = indeterminate ? NSControlStateValueMixed : (checked ? NSControlStateValueOn : NSControlStateValueOff);
+        [m_buttonCell _setState:newState animated:false];
+    }
+
+    // Presenting state
+    if (states.contains(ControlStyle::State::Presenting))
+        [m_buttonCell _setHighlighted:YES animated:NO];
+
+    // Window inactive state does not need to be checked explicitly, since we paint parented to
+    // a view in a window whose key state can be detected.
+
+    // Only update if we have to, since AppKit does work even if the size is the same.
+    auto controlSize = calculateControlSize(rect.size(), style);
+    if (controlSize != [m_buttonCell controlSize])
+        [m_buttonCell setControlSize:controlSize];
+}
+
+} // namespace WebCore
+
+#endif // PLATFORM(MAC)

Source/WebCore/platform/mac/ThemeMac.mm
@@ -408,74 +408,6 @@ static bool drawCellFocusRing(NSCell *cell, NSRect cellFrame, NSView *controlVie
     return false;
 }
 
-static void paintToggleButton(ControlPartType buttonType, ControlStates& controlStates, GraphicsContext& context, const FloatRect& zoomedRect, float zoomFactor, ScrollView* scrollView, float deviceScaleFactor)
-{
-    BEGIN_BLOCK_OBJC_EXCEPTIONS
-
-    RetainPtr<NSButtonCell> toggleButtonCell = static_cast<NSButtonCell *>(controlStates.platformControl());
-    IntSize zoomedRectSize = IntSize(zoomedRect.size());
-
-    if (controlStates.isDirty()) {
-        if (!toggleButtonCell)
-            toggleButtonCell = createToggleButtonCell(buttonType);
-        configureToggleButton(toggleButtonCell.get(), buttonType, controlStates, zoomedRectSize, zoomFactor, true);
-    } else {
-        if (!toggleButtonCell) {
-            if (buttonType == ControlPartType::Checkbox)
-                toggleButtonCell = sharedCheckboxCell(controlStates, zoomedRectSize, zoomFactor);
-            else {
-                ASSERT(buttonType == ControlPartType::Radio);
-                toggleButtonCell = sharedRadioCell(controlStates, zoomedRectSize, zoomFactor);
-            }
-        }
-        configureToggleButton(toggleButtonCell.get(), buttonType, controlStates, zoomedRectSize, zoomFactor, false);
-    }
-    controlStates.setDirty(false);
-
-    GraphicsContextStateSaver stateSaver(context);
-
-    NSControlSize controlSize = [toggleButtonCell controlSize];
-    IntSize zoomedSize = buttonType == ControlPartType::Checkbox ? checkboxSizes()[controlSize] : radioSizes()[controlSize];
-    zoomedSize.setWidth(zoomedSize.width() * zoomFactor);
-    zoomedSize.setHeight(zoomedSize.height() * zoomFactor);
-    const int* controlMargins = buttonType == ControlPartType::Checkbox ? checkboxMargins(controlSize) : radioMargins(controlSize);
-    FloatRect inflatedRect = inflateRect(zoomedRect, zoomedSize, controlMargins, zoomFactor);
-
-    if (zoomFactor != 1.0f) {
-        inflatedRect.setWidth(inflatedRect.width() / zoomFactor);
-        inflatedRect.setHeight(inflatedRect.height() / zoomFactor);
-        context.translate(inflatedRect.location());
-        context.scale(zoomFactor);
-        context.translate(-inflatedRect.location());
-    }
-    LocalCurrentGraphicsContext localContext(context);
-
-    NSView *view = ThemeMac::ensuredView(scrollView, controlStates, true /* useUnparentedView */);
-
-    bool needsRepaint = false;
-    bool isCellFocused = controlStates.states().contains(ControlStates::States::Focused);
-
-    if ([toggleButtonCell _stateAnimationRunning]) {
-        context.translate(inflatedRect.location());
-        context.scale(FloatSize(1, -1));
-        context.translate(0, -inflatedRect.height());
-
-        [toggleButtonCell _renderCurrentAnimationFrameInContext:context.platformContext() atLocation:NSMakePoint(0, 0)];
-        if (![toggleButtonCell _stateAnimationRunning] && isCellFocused)
-            needsRepaint = ThemeMac::drawCellOrFocusRingWithViewIntoContext(toggleButtonCell.get(), context, inflatedRect, view, false, true, deviceScaleFactor);
-    } else
-        needsRepaint = ThemeMac::drawCellOrFocusRingWithViewIntoContext(toggleButtonCell.get(), context, inflatedRect, view, true, isCellFocused, deviceScaleFactor);
-
-    [toggleButtonCell setControlView:nil];
-
-    needsRepaint |= [toggleButtonCell _stateAnimationRunning];
-    controlStates.setNeedsRepaint(needsRepaint);
-    if (needsRepaint)
-        controlStates.setPlatformControl(toggleButtonCell.get());
-
-    END_BLOCK_OBJC_EXCEPTIONS
-}
-
 // Buttons
 
 // Buttons really only constrain height. They respect width.
@@ -969,12 +901,6 @@ void ThemeMac::paint(ControlPartType type, ControlStates& states, GraphicsContex
     LocalDefaultSystemAppearance localAppearance(useDarkAppearance, tintColor);
 
     switch (type) {
-    case ControlPartType::Checkbox:
-        paintToggleButton(type, states, context, zoomedRect, zoomFactor, scrollView, deviceScaleFactor);
-        break;
-    case ControlPartType::Radio:
-        paintToggleButton(type, states, context, zoomedRect, zoomFactor, scrollView, deviceScaleFactor);
-        break;
     case ControlPartType::PushButton:
     case ControlPartType::DefaultButton:
     case ControlPartType::Button:

Source/WebCore/Headers.cmake
@@ -1744,6 +1744,7 @@ set(WebCore_PRIVATE_FRAMEWORK_HEADERS
     platform/graphics/controls/PlatformControl.h
     platform/graphics/controls/TextAreaPart.h
     platform/graphics/controls/TextFieldPart.h
+    platform/graphics/controls/ToggleButtonPart.h
 
     platform/graphics/displaylists/DisplayList.h
     platform/graphics/displaylists/DisplayListDrawingContext.h

Source/WebCore/platform/graphics/controls/ControlFactory.h
@@ -31,6 +31,7 @@ class MeterPart;
 class PlatformControl;
 class TextAreaPart;
 class TextFieldPart;
+class ToggleButtonPart;
 
 class ControlFactory {
     WTF_MAKE_FAST_ALLOCATED;
@@ -44,6 +45,7 @@ public:
     virtual std::unique_ptr<PlatformControl> createPlatformMeter(MeterPart&) = 0;
     virtual std::unique_ptr<PlatformControl> createPlatformTextArea(TextAreaPart&) = 0;
     virtual std::unique_ptr<PlatformControl> createPlatformTextField(TextFieldPart&) = 0;
+    virtual std::unique_ptr<PlatformControl> createPlatformToggleButton(ToggleButtonPart&) = 0;
 };
 
 } // namespace WebCore

Source/WebCore/platform/graphics/controls/ToggleButtonPart.h
@@ -1,0 +1,53 @@
+/*
+ * Copyright (C) 2022 Apple Inc. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "ControlFactory.h"
+#include "ControlPart.h"
+
+namespace WebCore {
+
+class ToggleButtonPart final : public ControlPart {
+public:
+    static Ref<ToggleButtonPart> create(ControlPartType type)
+    {
+        return adoptRef(*new ToggleButtonPart(type));
+    }
+
+private:
+    ToggleButtonPart(ControlPartType type)
+        : ControlPart(type)
+    {
+        ASSERT(type == ControlPartType::Checkbox || type == ControlPartType::Radio);
+    }
+
+    std::unique_ptr<PlatformControl> createPlatformControl() final
+    {
+        return controlFactory().createPlatformToggleButton(*this);
+    }
+};
+
+} // namespace WebCore

Source/WebCore/platform/graphics/mac/controls/ButtonControlMac.h
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2022 Apple Inc. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#if PLATFORM(MAC)
+
+#import "ControlMac.h"
+
+namespace WebCore {
+
+class ButtonControlMac : public ControlMac {
+public:
+    ButtonControlMac(ControlPart&, ControlFactoryMac&, NSButtonCell *);
+
+protected:
+    void updateCellStates(const FloatRect&, const ControlStyle&) override;
+
+    RetainPtr<NSButtonCell> m_buttonCell;
+};
+
+} // namespace WebCore
+
+#endif // PLATFORM(MAC)

Source/WebCore/platform/graphics/mac/controls/ControlFactoryMac.h
@@ -41,13 +41,18 @@ public:
     std::unique_ptr<PlatformControl> createPlatformMeter(MeterPart&) override;
     std::unique_ptr<PlatformControl> createPlatformTextArea(TextAreaPart&) override;
     std::unique_ptr<PlatformControl> createPlatformTextField(TextFieldPart&) override;
+    std::unique_ptr<PlatformControl> createPlatformToggleButton(ToggleButtonPart&) override;
 
 private:
+    NSButtonCell *checkboxCell() const;
+    NSButtonCell *radioCell() const;
     NSLevelIndicatorCell* levelIndicatorCell() const;
     NSTextFieldCell* textFieldCell() const;
 
     mutable RetainPtr<WebControlView> m_drawingView;
 
+    mutable RetainPtr<NSButtonCell> m_checkboxCell;
+    mutable RetainPtr<NSButtonCell> m_radioCell;
     mutable RetainPtr<NSLevelIndicatorCell> m_levelIndicatorCell;
     mutable RetainPtr<NSTextFieldCell> m_textFieldCell;
 };

Source/WebCore/platform/graphics/mac/controls/ControlFactoryMac.mm
@@ -31,6 +31,8 @@
 #import "MeterMac.h"
 #import "TextAreaMac.h"
 #import "TextFieldMac.h"
+#import "ToggleButtonMac.h"
+#import "ToggleButtonPart.h"
 #import <pal/spi/mac/NSViewSPI.h>
 #import <wtf/BlockObjCExceptions.h>
 
@@ -58,6 +60,37 @@ NSView *ControlFactoryMac::drawingView(const FloatRect& rect, const ControlStyle
     UNUSED_PARAM(style);
 #endif
     return m_drawingView.get();
+}
+
+static RetainPtr<NSButtonCell> createToggleButtonCell()
+{
+    auto buttonCell = adoptNS([[NSButtonCell alloc] init]);
+    [buttonCell setTitle:nil];
+    [buttonCell setFocusRingType:NSFocusRingTypeExterior];
+    return buttonCell;
+}
+
+NSButtonCell* ControlFactoryMac::checkboxCell() const
+{
+    if (!m_checkboxCell) {
+        BEGIN_BLOCK_OBJC_EXCEPTIONS
+        m_checkboxCell = createToggleButtonCell();
+        [m_checkboxCell setButtonType:NSButtonTypeSwitch];
+        [m_checkboxCell setAllowsMixedState:YES];
+        END_BLOCK_OBJC_EXCEPTIONS
+    }
+    return m_checkboxCell.get();
+}
+
+NSButtonCell* ControlFactoryMac::radioCell() const
+{
+    if (!m_radioCell) {
+        BEGIN_BLOCK_OBJC_EXCEPTIONS
+        m_radioCell = createToggleButtonCell();
+        [m_radioCell setButtonType:NSButtonTypeRadio];
+        END_BLOCK_OBJC_EXCEPTIONS
+    }
+    return m_radioCell.get();
 }
 
 NSLevelIndicatorCell* ControlFactoryMac::levelIndicatorCell() const
@@ -101,6 +134,11 @@ std::unique_ptr<PlatformControl> ControlFactoryMac::createPlatformTextArea(TextA
 std::unique_ptr<PlatformControl> ControlFactoryMac::createPlatformTextField(TextFieldPart& part)
 {
     return makeUnique<TextFieldMac>(part, *this, textFieldCell());
+}
+
+std::unique_ptr<PlatformControl> ControlFactoryMac::createPlatformToggleButton(ToggleButtonPart& part)
+{
+    return makeUnique<ToggleButtonMac>(part, *this, part.type() == ControlPartType::Checkbox ? checkboxCell() : radioCell());
 }
 
 } // namespace WebCore

commit 456497bb8c15309d0367d551c5c81d3ea4c62747

REGRESSION (macOS 11): Form controls don't paint with an inactive appearance in non-key windows https://bugs.webkit.org/show_bug.cgi?id=249355 rdar://103379662
Reviewed by Simon Fraser.

Before macOS 11, WebKit was able to paint form controls with an inactive
appearance by overriding `-[NSView window]` on the control view used for
painting, returning an `NSWindow` with custom return values for
`hasKeyAppearance` and `isKeyWindow`.

However, in macOS 11, AppKit began deriving the inactive state using a
"view root", which is not necessarily an `NSWindow`. The "view root" is only
computed when a view is added to the view hierarchy. Since WebKit's control
view used for painting is not a part of the view hierarchy, it ends up having
a `nil` "view root", and AppKit falls back to the active appearance.

To fix, `-[NSView _viewRoot]` is overridden on WebKit's control views to return
an `NSWindow` with the appropriate state. Additionally, a layout test is added
to detect regressions in this behavior.

* LayoutTests/fast/forms/select/mac-wk2/inactive-appearance-expected-mismatch.html: Added.
* LayoutTests/fast/forms/select/mac-wk2/inactive-appearance.html: Added.
* Source/WebCore/platform/graphics/mac/controls/ButtonControlMac.mm:
(WebCore::ButtonControlMac::updateCellStates):

Fix a regression from 258177@main, where checkboxes and radio buttons stopped
update the window active state before drawing.

* Source/WebCore/platform/graphics/mac/controls/WebControlView.mm:
(-[WebControlView _viewRoot]):
* Source/WebCore/platform/mac/ThemeMac.mm:
(-[WebCoreThemeView _viewRoot]):

Canonical link: https://commits.webkit.org/258294@main

Source/WebCore/platform/graphics/mac/controls/ButtonControlMac.mm
@@ -41,6 +41,8 @@ ButtonControlMac::ButtonControlMac(ControlPart& part, ControlFactoryMac& control
 
 void ButtonControlMac::updateCellStates(const FloatRect& rect, const ControlStyle& style)
 {
+    ControlMac::updateCellStates(rect, style);
+
     const auto& states = style.states;
 
     // Pressed state

Source/WebCore/platform/graphics/mac/controls/WebControlView.mm
@@ -130,6 +130,11 @@ static NSRect _clipBounds;
     return self;
 }
 
+- (NSWindow *)_viewRoot
+{
+    return _window.get();
+}
+
 - (void)addSubview:(NSView *)subview
 {
     // By doing nothing in this method we forbid controls from adding subviews.

Source/WebCore/platform/mac/ThemeMac.mm
@@ -118,6 +118,11 @@ static bool _useFormSemanticContext;
     return self;
 }
 
+- (NSWindow *)_viewRoot
+{
+    return _window.get();
+}
+
 - (void)addSubview:(NSView *)subview
 {
     // By doing nothing in this method we forbid controls from adding subviews.

LayoutTests/fast/forms/select/mac-wk2/inactive-appearance-expected-mismatch.html
@@ -1,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <select>
+        <option>Test</option>
+    </select>
+</body>
+</html>

LayoutTests/fast/forms/select/mac-wk2/inactive-appearance.html
@@ -1,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="../../../../resources/js-test.js"></script>
+        <script src="../../../../resources/ui-helper.js"></script>
+    </head>
+<body>
+    <select>
+        <option>Test</option>
+    </select>
+</body>
+<script>
+
+jsTestIsAsync = true;
+
+addEventListener("load", async () => {
+    if (window.internals)
+        window.internals.setPageIsFocusedAndActive(false);
+
+    await UIHelper.renderingUpdate();
+    finishJSTest();
+});
+
+</script>
+</html>