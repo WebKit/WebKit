185eb43ee5ff0b8c9c221845c1be23a56f02e9f5
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2023-01-04 01:25:27 -0800

    Remove unused "nodeUnderMouse" from EventHandler.h
    Remove unused "nodeUnderMouse" from EventHandler.h
    https://bugs.webkit.org/show_bug.cgi?id=250038
    
    Reviewed by Ryosuke Niwa.
    
    This patch removes unused "nodeUnderMouse" which seems to be defined here only
    but never used or referenced in any other .cpp & .h files.
    
    * Source/WebCore/page/EventHandler.h: Remove unused 'nodeUnderMouse'
    
    Canonical link: https://commits.webkit.org/258425@main

--- Source/WebCore/page/EventHandler.h
+++ Source/WebCore/page/EventHandler.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006-2020 Apple Inc. All rights reserved.
+ * Copyright (C) 2006-2022 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -432,8 +432,6 @@ private:
 #if ENABLE(TOUCH_EVENTS)
     bool dispatchSyntheticTouchEventIfEnabled(const PlatformMouseEvent&);
 #endif
-
-    Node* nodeUnderMouse() const;
     
     enum class FireMouseOverOut { No, Yes };
     void updateMouseEventTargetNode(const AtomString& eventType, Node*, const PlatformMouseEvent&, FireMouseOverOut);
