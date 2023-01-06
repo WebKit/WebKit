1a1e7ad8f8a74fe4c00d54149795b14ee5626e3a
Tim Horton <thorton@apple.com>
2023-01-05 17:47:04 -0800

    Live-ish resize sometimes pops to unexpected scroll position even if layout width doesn't change https://bugs.webkit.org/show_bug.cgi?id=250153 rdar://96702143
    Reviewed by Wenson Hsieh.
    
    * Source/WebKit/UIProcess/API/ios/WKWebViewIOS.mm:
    (-[WKWebView _updateScrollViewForTransaction:]):
    By default, UIScrollView pins the center of the view when you change zoomScale.
    When doing a live resize with a fixed layout width, the zoomScale changes on
    every resize. Resizes normally pin the top edge of the content (or rather,
    don't try to adjust the content offset -- at least until scroll anchoring
    comes along), so this leads to surprising jumps. To fix this, when
    web-process-originated scale changes occur, pin the vertical scroll position
    to the top edge of the content, instead of the center. I don't expect any
    clients are depending on the existing behavior, and hope that this
    will be purely an improvement.
    
    (-[WKWebView _didCommitLayerTree:]):
    Like animated resize, bail from updating scroll view parameters and various
    other incoming-commit-related things until *after* the live resize finishes.
    This avoids us calling _updateScrollViewForTransaction and adjusting zoomScale
    while the resize animation view is installed, which results in our scroll view
    thinking that the current zoomScale is 1, and throwing all the math off, causing
    further unexpected scrolling.
    
    (-[WKWebView _updateVisibleContentRects]):
    Fix this logging to indicate whether whether we're deferring geometry updates
    for any reason; an earlier patch changed the condition but not the log.
    
    Canonical link: https://commits.webkit.org/258521@main

--- Source/WebKit/UIProcess/API/ios/WKWebViewIOS.mm
+++ Source/WebKit/UIProcess/API/ios/WKWebViewIOS.mm
@@ -938,7 +938,15 @@ static void changeContentOffsetBoundedInValidRange(UIScrollView *scrollView, Web
 
     if (!layerTreeTransaction.scaleWasSetByUIProcess() && ![_scrollView isZooming] && ![_scrollView isZoomBouncing] && ![_scrollView _isAnimatingZoom] && !WebKit::scalesAreEssentiallyEqual([_scrollView zoomScale], layerTreeTransaction.pageScaleFactor())) {
         LOG_WITH_STREAM(VisibleRects, stream << " updating scroll view with pageScaleFactor " << layerTreeTransaction.pageScaleFactor());
+
+        // When web-process-originated scale changes occur, pin the
+        // vertical scroll position to the top edge of the content,
+        // instead of the center (which UIScrollView does by default).
+        CGFloat contentOffsetY = [_scrollView contentOffset].y * layerTreeTransaction.pageScaleFactor() / [_scrollView zoomScale];
+
         [_scrollView setZoomScale:layerTreeTransaction.pageScaleFactor()];
+
+        [_scrollView setContentOffset:CGPointMake([_scrollView contentOffset].x, contentOffsetY)];
     }
 }
 
@@ -1007,6 +1015,9 @@ static void changeContentOffsetBoundedInValidRange(UIScrollView *scrollView, Web
         [self _didCommitLayerTreeDuringAnimatedResize:layerTreeTransaction];
         return;
     }
+
+    if (_perProcessState.liveResizeParameters)
+        return;
 
     if (_resizeAnimationView)
         WKWEBVIEW_RELEASE_LOG("%p -[WKWebView _didCommitLayerTree:] - dynamicViewportUpdateMode is NotResizing, but still have a live resizeAnimationView (unpaired begin/endAnimatedResize?)", self);
@@ -2491,8 +2502,8 @@ static bool scrollViewCanScroll(UIScrollView *scrollView)
         || _perProcessState.currentlyAdjustingScrollViewInsetsForKeyboard) {
         _perProcessState.didDeferUpdateVisibleContentRectsForAnyReason = YES;
         _perProcessState.didDeferUpdateVisibleContentRectsForUnstableScrollView = YES;
-        WKWEBVIEW_RELEASE_LOG("%p (pageProxyID=%llu) -[WKWebView _updateVisibleContentRects:] - scroll view state is non-stable, bailing (dynamicViewportUpdateMode %d, needsResetViewStateAfterCommitLoadForMainFrame %d, sizeChangedSinceLastVisibleContentRectUpdate %d, [_scrollView isZoomBouncing] %d, currentlyAdjustingScrollViewInsetsForKeyboard %d)",
-            self, _page->identifier().toUInt64(), _perProcessState.dynamicViewportUpdateMode, _perProcessState.needsResetViewStateAfterCommitLoadForMainFrame, [_contentView sizeChangedSinceLastVisibleContentRectUpdate], [_scrollView isZoomBouncing], _perProcessState.currentlyAdjustingScrollViewInsetsForKeyboard);
+        WKWEBVIEW_RELEASE_LOG("%p (pageProxyID=%llu) -[WKWebView _updateVisibleContentRects:] - scroll view state is non-stable, bailing (shouldDeferGeometryUpdates %d, dynamicViewportUpdateMode %d, needsResetViewStateAfterCommitLoadForMainFrame %d, sizeChangedSinceLastVisibleContentRectUpdate %d, [_scrollView isZoomBouncing] %d, currentlyAdjustingScrollViewInsetsForKeyboard %d)",
+            self, _page->identifier().toUInt64(), self._shouldDeferGeometryUpdates, _perProcessState.dynamicViewportUpdateMode, _perProcessState.needsResetViewStateAfterCommitLoadForMainFrame, [_contentView sizeChangedSinceLastVisibleContentRectUpdate], [_scrollView isZoomBouncing], _perProcessState.currentlyAdjustingScrollViewInsetsForKeyboard);
         return;
     }
 
