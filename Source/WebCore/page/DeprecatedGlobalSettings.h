bf631e37e7c821aed3639dd8e7477c864b00e543
Tim Nguyen <ntim@apple.com>
2023-01-06 23:18:54 -0800

    Remove WebRTCMDNSICECandidatesEnabled from DeprecatedGlobalSettings https://bugs.webkit.org/show_bug.cgi?id=250249 rdar://103978748
    Reviewed by Simon Fraser.
    
    It seems unused aside from APIs, it might be possible to remove the preference entirely, which I've filed https://bugs.webkit.org/show_bug.cgi?id=250252 for.
    
    * Source/WTF/Scripts/Preferences/UnifiedWebPreferences.yaml:
    * Source/WebCore/page/DeprecatedGlobalSettings.h:
    (WebCore::DeprecatedGlobalSettings::setWebRTCH264LowLatencyEncoderEnabled):
    (WebCore::DeprecatedGlobalSettings::webRTCMDNSICECandidatesEnabled): Deleted.
    (WebCore::DeprecatedGlobalSettings::setWebRTCMDNSICECandidatesEnabled): Deleted.
    
    Canonical link: https://commits.webkit.org/258607@main

--- Source/WebCore/page/DeprecatedGlobalSettings.h
+++ Source/WebCore/page/DeprecatedGlobalSettings.h
@@ -126,8 +126,6 @@ public:
 #if ENABLE(WEB_RTC)
     static bool webRTCH264LowLatencyEncoderEnabled() { return shared().m_isWebRTCH264LowLatencyEncoderEnabled; }
     static void setWebRTCH264LowLatencyEncoderEnabled(bool isEnabled) { shared().m_isWebRTCH264LowLatencyEncoderEnabled = isEnabled; }
-    static bool webRTCMDNSICECandidatesEnabled() { return shared().m_isWebRTCMDNSICECandidatesEnabled; }
-    static void setWebRTCMDNSICECandidatesEnabled(bool isEnabled) { shared().m_isWebRTCMDNSICECandidatesEnabled = isEnabled; }
     static bool webRTCH264SimulcastEnabled() { return shared().m_isWebRTCH264SimulcastEnabled; }
     static void setWebRTCH264SimulcastEnabled(bool isEnabled) { shared().m_isWebRTCH264SimulcastEnabled = isEnabled; }
     static bool webRTCPlatformTCPSocketsEnabled() { return shared().m_isWebRTCPlatformTCPSocketsEnabled; }
@@ -251,7 +249,6 @@ private:
 
 #if ENABLE(WEB_RTC)
     bool m_isWebRTCH264SimulcastEnabled { true };
-    bool m_isWebRTCMDNSICECandidatesEnabled { false };
     bool m_isWebRTCH264LowLatencyEncoderEnabled { false };
     bool m_isWebRTCPlatformTCPSocketsEnabled { false };
     bool m_isWebRTCPlatformUDPSocketsEnabled { false };
