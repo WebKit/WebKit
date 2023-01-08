06479587e171c15e02394bc40840619744826116
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2023-01-07 17:10:30 -0800

    Remove the precision="float" attribute on <input type="range">
    Remove the precision="float" attribute on <input type="range">
    https://bugs.webkit.org/show_bug.cgi?id=250047
    
    Reviewed by Alexey Shvayka.
    
    This patch is to align WebKit with Gecko / Firefox, Blink / Chromium by removing non-standard 'precision' attribute.
    
    Merge - https://chromium.googlesource.com/chromium/blink/+/6b76e9c0e649bf8d4b1c6c321af513e0e0c6c1af
    
    This was added in WebKit but it was non-standard and it was never added by any other browser.
    Blink removed it in 2014 so only WebKit support it as of right now and it is broken feature
    since it does not support 'step'  with precision="float".
    
    * Source/WebCore/html/HTMLAttributeNames.in: Remove "precision"
    * Source/WebCore/html/HTMLInputElement.cpp: Remove "precisionAttr"
    * Source/WebCore/html/RangeInputType.cpp: Remove "precision='float'" support and FIXME
    
    Canonical link: https://commits.webkit.org/258625@main

--- Source/WebCore/html/HTMLInputElement.cpp
+++ Source/WebCore/html/HTMLInputElement.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2001 Dirk Mueller (mueller@kde.org)
- * Copyright (C) 2004-2021 Apple Inc. All rights reserved.
+ * Copyright (C) 2004-2023 Apple Inc. All rights reserved.
  *           (C) 2006 Alexey Proskuryakov (ap@nypop.com)
  * Copyright (C) 2007 Samuel Weinig (sam@webkit.org)
  * Copyright (C) 2010-2021 Google Inc. All rights reserved.
@@ -790,7 +790,7 @@ void HTMLInputElement::parseAttribute(const QualifiedName& name, const AtomStrin
         m_maxResults = value.isNull() ? -1 : std::min(parseHTMLInteger(value).value_or(0), maxSavedResults);
     else if (name == autosaveAttr || name == incrementalAttr)
         invalidateStyleForSubtree();
-    else if (name == maxAttr || name == minAttr || name == multipleAttr || name == patternAttr || name == precisionAttr || name == stepAttr)
+    else if (name == maxAttr || name == minAttr || name == multipleAttr || name == patternAttr || name == stepAttr)
         updateValidity();
 #if ENABLE(DATALIST_ELEMENT)
     else if (name == listAttr) {
