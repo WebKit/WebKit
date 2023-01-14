6eceb6d922f3d1ba8cd0da21fc457249938cef87
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2023-01-14 11:52:04 -0800

    Fix nit which is inefficient sequence to deleteMarkedNodes
    Fix nit which is inefficient sequence to deleteMarkedNodes
    https://bugs.webkit.org/show_bug.cgi?id=250524
    
    Reviewed by Eric Carlson.
    
    Merge - https://src.chromium.org/viewvc/blink?view=rev&revision=172279
    
    This patch is just to move "deleteMarkedNodes" just after 'm_nodesToDelete' to prevent inefficient do while loop.
    
    * Source/WebCore/Modules/webaudio/BaseAudioContext.cpp:
    (BaseAudioContext::clear): Move 'deleteMarkedNodes' down
    
    Canonical link: https://commits.webkit.org/258920@main

--- Source/WebCore/Modules/webaudio/BaseAudioContext.cpp
+++ Source/WebCore/Modules/webaudio/BaseAudioContext.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2010 Google Inc. All rights reserved.
- * Copyright (C) 2016-2021 Apple Inc. All rights reserved.
+ * Copyright (C) 2010-2014 Google Inc. All rights reserved.
+ * Copyright (C) 2016-2023 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -176,8 +176,8 @@ void BaseAudioContext::clear()
 
     // Audio thread is dead. Nobody will schedule node deletion action. Let's do it ourselves.
     do {
-        deleteMarkedNodes();
         m_nodesToDelete = std::exchange(m_nodesMarkedForDeletion, { });
+        deleteMarkedNodes();
     } while (!m_nodesToDelete.isEmpty());
 }
 
