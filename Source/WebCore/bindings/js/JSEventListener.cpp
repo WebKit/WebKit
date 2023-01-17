614ea47ae108a8f1ccc192f486ab6308f5894812
Chris Dumez <cdumez@apple.com>
2023-01-17 11:31:59 -0800

    Cherry-pick 433db4f29219. rdar://problem/100763856
        Heap use-after-free in DOMWrapperWorld::~DOMWrapperWorld
        https://bugs.webkit.org/show_bug.cgi?id=246022
        rdar://100763856
    
        Reviewed by Jonathan Bedard and Ryosuke Niwa.
    
        Right before a worker terminates, it destroys its WorkerOrWorkletScriptController,
        which destroys the JS VM. Certain objects like DOMWrapperWorld cannot outlive
        the VM since they keep a `VM&' as data member. However, DOMWrapperWorld is
        refcounted and JSEventListeners hold a strong ref to their DOMWrapperWorld. If
        JSEventListeners outlive the VM, then it would lead to a use-after free in the
        DOMWrapperWorld destructor when destroying those JSEventListeners later on.
    
        We have previously made several attempts to try and unregister all event
        listeners before destroying the VM. However, those attempts were either
        incomplete or led to other crashes. I am therefore trying a different approach
        this time.
    
        JSEventListeners now register themselves as client of the JSVMClientData (which
        is owned by the VM) and the client gets a `willDestroyVM()` call before the
        VM gets destroyed. This allows JSEventListeners to clear out their data members
        which rely on the VM (DOMWrapperWorld and JSC::Weak data members).
    
        * Source/WebCore/bindings/js/JSErrorHandler.cpp:
        (WebCore::JSErrorHandler::handleEvent):
        * Source/WebCore/bindings/js/JSEventListener.cpp:
        (WebCore::JSEventListener::JSEventListener):
        (WebCore::JSEventListener::handleEvent):
        (WebCore::JSEventListener::functionName const):
        (WebCore::JSEventListener::willDestroyVM):
        * Source/WebCore/bindings/js/JSEventListener.h:
        (WebCore::JSEventListener::isolatedWorld const):
        (WebCore::JSEventListener::ensureJSFunction const):
        * Source/WebCore/bindings/js/JSLazyEventListener.cpp:
        (WebCore::JSLazyEventListener::initializeJSFunction const):
        * Source/WebCore/bindings/js/WebCoreJSClientData.cpp:
        (WebCore::JSVMClientData::~JSVMClientData):
        * Source/WebCore/bindings/js/WebCoreJSClientData.h:
        (WebCore::JSVMClientData::addClient):
        * Source/WebCore/dom/EventTarget.cpp:
        (WebCore::EventTarget::attributeEventListener):
        * Source/WebCore/inspector/CommandLineAPIHost.cpp:
        (WebCore::CommandLineAPIHost::getEventListeners):
        * Source/WebCore/inspector/WebInjectedScriptHost.cpp:
        (WebCore::objectForEventTargetListeners):
        * Source/WebCore/inspector/agents/InspectorDOMAgent.cpp:
        (WebCore::InspectorDOMAgent::buildObjectForEventListener):
    
        Canonical link: https://commits.webkit.org/252432.1030@safari-7614-branch
    
    Canonical link: https://commits.webkit.org/259548.18@safari-7615-branch

--- Source/WebCore/bindings/js/JSEventListener.cpp
+++ Source/WebCore/bindings/js/JSEventListener.cpp
@@ -53,13 +53,14 @@ JSEventListener::JSEventListener(JSObject* function, JSObject* wrapper, bool isA
     , m_wasCreatedFromMarkup(createdFromMarkup == CreatedFromMarkup::Yes)
     , m_isInitialized(false)
     , m_wrapper(wrapper)
-    , m_isolatedWorld(isolatedWorld)
+    , m_isolatedWorld(&isolatedWorld)
 {
     if (function) {
         ASSERT(wrapper);
         m_jsFunction = JSC::Weak<JSC::JSObject>(function);
         m_isInitialized = true;
     }
+    static_cast<JSVMClientData*>(isolatedWorld.vm().clientData)->addClient(*this);
 }
 
 JSEventListener::~JSEventListener() = default;
@@ -142,7 +143,10 @@ void JSEventListener::handleEvent(ScriptExecutionContext& scriptExecutionContext
     if (!jsFunction)
         return;
 
-    auto* globalObject = toJSDOMGlobalObject(scriptExecutionContext, m_isolatedWorld);
+    if (UNLIKELY(!m_isolatedWorld))
+        return;
+
+    auto* globalObject = toJSDOMGlobalObject(scriptExecutionContext, *m_isolatedWorld);
     if (!globalObject)
         return;
 
@@ -277,7 +281,7 @@ String JSEventListener::functionName() const
     if (!m_wrapper || !m_jsFunction)
         return { };
 
-    auto& vm = isolatedWorld().vm();
+    auto& vm = m_isolatedWorld->vm();
     JSC::JSLockHolder lock(vm);
 
     auto* handlerFunction = JSC::jsDynamicCast<JSC::JSFunction*>(m_jsFunction.get());
@@ -285,6 +289,14 @@ String JSEventListener::functionName() const
         return { };
 
     return handlerFunction->name(vm);
+}
+
+void JSEventListener::willDestroyVM()
+{
+    m_jsFunction.clear();
+    m_wrapper.clear();
+    m_isInitialized = false;
+    m_isolatedWorld = nullptr;
 }
 
 } // namespace WebCore
