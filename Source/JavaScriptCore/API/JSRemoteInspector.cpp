a79898f51c577e596d64b53d834e13b6e10824ce
Elliott Williams <emw@apple.com>
2023-01-17 17:05:58 -0800

    Clean up WebKitAdditions logic in postprocess-header-rule https://bugs.webkit.org/show_bug.cgi?id=250740 rdar://104354248
    Reviewed by NOBODY (OOPS!).
    
    postprocess-framework-headers-definitions is no longer needed for
    internal builds.
    
    * Source/JavaScriptCore/Scripts/postprocess-header-rule:
    
    Reland 259000@main, which is fixed in internal builds by the underlying
    change.
    
    * Source/JavaScriptCore/API/JSRemoteInspector.cpp:
    (JSRemoteInspectorGetInspectionFollowsInternalPolicies):
    (JSRemoteInspectorSetInspectionFollowsInternalPolicies):
    * Source/JavaScriptCore/API/JSRemoteInspector.h:
    * Source/JavaScriptCore/inspector/JSGlobalObjectInspectorController.cpp:
    (Inspector::JSGlobalObjectInspectorController::developerExtrasEnabled const):
    * Source/JavaScriptCore/inspector/remote/RemoteInspectionTarget.cpp:
    (Inspector::RemoteInspectionTarget::remoteControlAllowed const):
    (Inspector::RemoteInspectionTarget::allowsInspectionByPolicy const):
    (Inspector::RemoteInspectionTarget::inspectable const):
    (Inspector::RemoteInspectionTarget::setInspectable):
    (Inspector::RemoteInspectionTarget::pauseWaitingForAutomaticInspection):
    * Source/JavaScriptCore/inspector/remote/RemoteInspectionTarget.h:
    * Source/JavaScriptCore/inspector/remote/cocoa/RemoteInspectorCocoa.mm:
    (Inspector::RemoteInspector::listingForInspectionTarget const):
    * Source/JavaScriptCore/inspector/remote/glib/RemoteInspectorGlib.cpp:
    (Inspector::RemoteInspector::listingForInspectionTarget const):
    * Source/JavaScriptCore/inspector/remote/socket/RemoteInspectorSocket.cpp:
    (Inspector::RemoteInspector::listingForInspectionTarget const):
    * Source/WebCore/bindings/js/JSDOMGlobalObject.cpp:
    (WebCore::JSDOMGlobalObject::finishCreation):
    * Source/WebKit/UIProcess/API/Cocoa/APISerializedScriptValueCocoa.mm:
    (API::SharedJSContext::ensureContext):
    
    Co-authored-by: Patrick Angle <pangle@apple.com>

--- Source/JavaScriptCore/API/JSRemoteInspector.cpp
+++ Source/JavaScriptCore/API/JSRemoteInspector.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2015-2023 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -41,6 +41,7 @@
 using namespace Inspector;
 
 static std::optional<bool> remoteInspectionEnabledByDefault = std::nullopt;
+static bool inspectionFollowsInternalPolicies = true;
 
 void JSRemoteInspectorDisableAutoStart(void)
 {
@@ -127,4 +128,14 @@ bool JSRemoteInspectorGetInspectionEnabledByDefault(void)
 void JSRemoteInspectorSetInspectionEnabledByDefault(bool enabledByDefault)
 {
     remoteInspectionEnabledByDefault = enabledByDefault;
+}
+
+bool JSRemoteInspectorGetInspectionFollowsInternalPolicies(void)
+{
+    return inspectionFollowsInternalPolicies;
+}
+
+void JSRemoteInspectorSetInspectionFollowsInternalPolicies(bool followsInternalPolicies)
+{
+    inspectionFollowsInternalPolicies = followsInternalPolicies;
 }
