WebKit/WebKit

commit 9fbb6a1b55afd7c1d3a4fff33578dd334242bb6c

[JSC] Validate funcref from JS function https://bugs.webkit.org/show_bug.cgi?id=250732 rdar://104332115
Reviewed by Justin Michaud and Mark Lam.

This patch fixes funcref validation from JS function. We need to ensure that funcref value from JS
is actually Wasm functions. Fortunately, our use of funcref from wasm is always validating them, so,
it is just a semantic bug.

* JSTests/wasm/stress/funcref-validation.js: Added.
(async let):
* JSTests/wasm/stress/resources/funcref-validation.wasm: Added.
* Source/JavaScriptCore/wasm/WasmOperations.cpp:
(JSC::Wasm::JSC_DEFINE_JIT_OPERATION):
* Source/JavaScriptCore/wasm/WasmOperations.h:
* Source/JavaScriptCore/wasm/js/WasmToJS.cpp:
(JSC::Wasm::wasmToJS):

Canonical link: https://commits.webkit.org/259011@main

Source/JavaScriptCore/wasm/WasmOperations.cpp
@@ -544,6 +544,24 @@ JSC_DEFINE_JIT_OPERATION(operationConvertToF32, float, (Instance* instance, Enco
     return static_cast<float>(JSValue::decode(v).toNumber(globalObject));
 }
 
+JSC_DEFINE_JIT_OPERATION(operationConvertToFuncref, EncodedJSValue, (Instance* instance, EncodedJSValue v))
+{
+    CallFrame* callFrame = DECLARE_WASM_CALL_FRAME(instance);
+    VM& vm = instance->vm();
+    JSGlobalObject* globalObject = instance->owner<JSWebAssemblyInstance>()->globalObject();
+    NativeCallFrameTracer tracer(vm, callFrame);
+    auto scope = DECLARE_THROW_SCOPE(vm);
+
+    JSValue value = JSValue::decode(v);
+    WebAssemblyFunction* wasmFunction = nullptr;
+    WebAssemblyWrapperFunction* wasmWrapperFunction = nullptr;
+    if (UNLIKELY(!isWebAssemblyHostFunction(value, wasmFunction, wasmWrapperFunction) && !value.isNull())) {
+        throwTypeError(globalObject, scope, "Funcref value is not a function"_s);
+        return { };
+    }
+    return v;
+}
+
 JSC_DEFINE_JIT_OPERATION(operationConvertToBigInt, EncodedJSValue, (Instance* instance, EncodedWasmValue value))
 {
     CallFrame* callFrame = DECLARE_WASM_CALL_FRAME(instance);
@@ -609,8 +627,13 @@ JSC_DEFINE_JIT_OPERATION(operationIterateResults, void, (Instance* instance, con
             unboxedValue = bitwise_cast<uint64_t>(value.toNumber(globalObject));
             break;
         default: {
-            if (isFuncref(returnType) || isExternref(returnType)) {
-                if (isFuncref(returnType) && !value.isCallable()) {
+            if (isExternref(returnType))
+                ASSERT(returnType.isNullable());
+            else if (isFuncref(returnType)) {
+                ASSERT(returnType.isNullable());
+                WebAssemblyFunction* wasmFunction = nullptr;
+                WebAssemblyWrapperFunction* wasmWrapperFunction = nullptr;
+                if (UNLIKELY(!isWebAssemblyHostFunction(value, wasmFunction, wasmWrapperFunction) && !value.isNull())) {
                     throwTypeError(globalObject, scope, "Funcref value is not a function"_s);
                     return;
                 }

JSTests/wasm/stress/funcref-validation.js
@@ -1,0 +1,22 @@
+(async function () {
+  let bytes = readFile('./resources/funcref-validation.wasm', 'binary');
+  let importObject = {
+    m: {
+      f0: new WebAssembly.Global({value: 'f32', mutable: true}, 0),
+      f1: new WebAssembly.Global({value: 'f64', mutable: true}, 0),
+      f2: 0,
+      f3: new WebAssembly.Global({value: 'f64', mutable: true}, 0),
+      f4: new WebAssembly.Global({value: 'i64', mutable: true}, 0n),
+      f5: 0,
+      f6: new WebAssembly.Global({value: 'anyfunc', mutable: true}, null),
+      f7: undefined,
+      ifn0() {},
+      ifn1() {},
+      ifn2() {},
+      t0: new WebAssembly.Table({initial: 61, element: 'anyfunc'}),
+      t1: new WebAssembly.Table({initial: 16, element: 'externref'}),
+    },
+  };
+  let i = await WebAssembly.instantiate(bytes, importObject);
+  i.instance.exports.fn3();
+})();

Source/JavaScriptCore/wasm/WasmOperations.h
@@ -59,6 +59,7 @@ JSC_DECLARE_JIT_OPERATION(operationConvertToI64, int64_t, (Instance*, EncodedJSV
 JSC_DECLARE_JIT_OPERATION(operationConvertToF64, double, (Instance*, EncodedJSValue));
 JSC_DECLARE_JIT_OPERATION(operationConvertToI32, int32_t, (Instance*, EncodedJSValue));
 JSC_DECLARE_JIT_OPERATION(operationConvertToF32, float, (Instance*, EncodedJSValue));
+JSC_DECLARE_JIT_OPERATION(operationConvertToFuncref, EncodedJSValue, (Instance*, EncodedJSValue));
 JSC_DECLARE_JIT_OPERATION(operationConvertToBigInt, EncodedJSValue, (Instance*, EncodedWasmValue));
 
 JSC_DECLARE_JIT_OPERATION(operationIterateResults, void, (Instance*, const TypeDefinition*, EncodedJSValue, uint64_t*, uint64_t*));

Source/JavaScriptCore/wasm/js/WasmToJS.cpp
@@ -451,9 +451,22 @@ Expected<MacroAssemblerCodeRef<WasmEntryPtrTag>, BindingFailure> wasmToJS(VM& vm
             break;
         }
         default:  {
-            if (Wasm::isRefType(returnType))
+            if (Wasm::isRefType(returnType)) {
+                if (Wasm::isExternref(returnType))
+                    ASSERT(returnType.isNullable());
+                else if (Wasm::isFuncref(returnType)) {
+                    ASSERT(returnType.isNullable());
+                    jit.prepareWasmCallOperation(PinnedRegisterInfo::get().wasmContextInstancePointer);
+                    jit.setupArguments<decltype(operationConvertToFuncref)>(PinnedRegisterInfo::get().wasmContextInstancePointer, JSRInfo::returnValueJSR);
+                    auto call = jit.call(OperationPtrTag);
+                    exceptionChecks.append(jit.emitJumpIfException(vm));
+                    jit.addLinkTask([=](LinkBuffer& linkBuffer) {
+                        linkBuffer.link<OperationPtrTag>(call, operationConvertToFuncref);
+                    });
+                } else if (Wasm::isI31ref(returnType))
+                    RELEASE_ASSERT_NOT_REACHED();
                 jit.moveValueRegs(JSRInfo::returnValueJSR, wasmCallInfo.results[0].location.jsr());
-            else
+            } else
                 // For the JavaScript embedding, imports with these types in their type definition return are a WebAssembly.Module validation error.
                 RELEASE_ASSERT_NOT_REACHED();
         }

commit d2e743fb9c1a3673120f1e09c30a6ba333a00242

[JSC] Fix regression from 259011@main https://bugs.webkit.org/show_bug.cgi?id=250791 rdar://104389435
Reviewed by Mark Lam, Keith Miller and Justin Michaud.

This patch fixes a regression introduced in 259011@main, accidental miss of assignment.

* JSTests/wasm/stress/externref-result-tuple.js: Added.
(shouldBe):
(async let):
(catch):
* JSTests/wasm/stress/resources/externref-result-tuple.wasm: Added.
* Source/JavaScriptCore/wasm/WasmOperations.cpp:
(JSC::Wasm::JSC_DEFINE_JIT_OPERATION):

Canonical link: https://commits.webkit.org/259053@main

JSTests/wasm/stress/externref-result-tuple.js
@@ -1,0 +1,27 @@
+//@ skip if $architecture == "arm"
+function shouldBe(actual, expected) {
+    if (actual !== expected)
+        throw new Error('bad value: ' + actual);
+}
+
+(async function () {
+  let bytes = readFile('./resources/externref-result-tuple.wasm', 'binary');
+  let importObject = {
+    m: {
+      f0: 0, f1: null, f2: 0n, f3: 0, f4: null,
+      f5: new WebAssembly.Global({value: 'externref', mutable: true}, {}),
+      ifn0() { return [0, 0, 0n]; },
+      ifn1() {},
+      ifn2() { return [undefined, null, 0, 0, undefined]; },
+      ifn3() { return [undefined, null, 0, 0, undefined]; },
+      t0: new WebAssembly.Table({initial: 49, element: 'anyfunc', maximum: 687}),
+      t1: new WebAssembly.Table({initial: 22, element: 'externref'}),
+      t2: new WebAssembly.Table({initial: 97, element: 'externref', maximum: 699}),
+    },
+    mem: {ory: new WebAssembly.Memory({initial: 40241, shared: true, maximum: 65536})},
+  };
+  let i = await WebAssembly.instantiate(bytes, importObject);
+  try { i.instance.exports.fn4(); } catch (e) { }
+  let table = i.instance.exports.table3;
+  shouldBe(table.get(0), undefined);
+})().catch($vm.abort);

Source/JavaScriptCore/wasm/WasmOperations.cpp
@@ -637,9 +637,9 @@ JSC_DEFINE_JIT_OPERATION(operationIterateResults, void, (Instance* instance, con
                     throwTypeError(globalObject, scope, "Funcref value is not a function"_s);
                     return;
                 }
-                unboxedValue = bitwise_cast<uint64_t>(value);
             } else
                 RELEASE_ASSERT_NOT_REACHED();
+            unboxedValue = bitwise_cast<uint64_t>(value);
         }
         }
         RETURN_IF_EXCEPTION(scope, void());