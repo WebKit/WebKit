5f8306750a54e227a29119f33f4a888c778d54a0
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2023-01-21 17:37:13 -0800

    Prevent overflow of width/height of layout overflow rectangle which can cause scroll bar to malfunction
    Prevent overflow of width/height of layout overflow rectangle which can cause scroll bar to malfunction
    https://bugs.webkit.org/show_bug.cgi?id=250944
    
    Reviewed by Alan Baradlay.
    
    This patch is to align WebKit behavior with Gecko / Firefox and Blink / Chromium.
    
    Merge - https://chromium.googlesource.com/chromium/blink/+/d050f9be579d02e40c8924b4153376bc32ffba1a
    
    When a child element is positioned very far away the width/height of
    layout overflow rectangle may overflow. The patch detects such cases in
    RenderOverflow::addLayoutOverflow and shift the top/left edges of the
    rectangle to avoid overflow.
    
    NOTE - The test case was already added by 138972@main from same
    commit but original patch was not added. The test case still does not show
    scrollbar like other browsers.
    
    * Source/WebCore/rendering/RenderOverflow.h:
    (RenderOverflow:addLayoutOverflow): Update to account for overflow of width/height
    
    Canonical link: https://commits.webkit.org/259178@main

--- Source/WebCore/rendering/RenderOverflow.h
+++ Source/WebCore/rendering/RenderOverflow.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009 Apple Inc. All rights reserved.
+ * Copyright (C) 2003-2023 Apple Inc. All rights reserved.
+ * Copyright (C) 2013 Google Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -77,10 +78,13 @@ inline void RenderOverflow::addLayoutOverflow(const LayoutRect& rect)
 {
     LayoutUnit maxX = std::max(rect.maxX(), m_layoutOverflow.maxX());
     LayoutUnit maxY = std::max(rect.maxY(), m_layoutOverflow.maxY());
-    m_layoutOverflow.setX(std::min(rect.x(), m_layoutOverflow.x()));
-    m_layoutOverflow.setY(std::min(rect.y(), m_layoutOverflow.y()));
-    m_layoutOverflow.setWidth(maxX - m_layoutOverflow.x());
-    m_layoutOverflow.setHeight(maxY - m_layoutOverflow.y());
+    LayoutUnit minX = std::min(rect.x(), m_layoutOverflow.x());
+    LayoutUnit minY = std::min(rect.y(), m_layoutOverflow.y());
+    // In case the width/height is larger than LayoutUnit can represent, fix the right/bottom edge and shift the top/left ones
+    m_layoutOverflow.setWidth(maxX - minX);
+    m_layoutOverflow.setHeight(maxY - minY);
+    m_layoutOverflow.setX(maxX - m_layoutOverflow.width());
+    m_layoutOverflow.setY(maxY - m_layoutOverflow.height());
 }
 
 inline void RenderOverflow::addVisualOverflow(const LayoutRect& rect)
