963f2a417e1f1ea8b4145d97fe44824109b75313
David Kilzer <ddkilzer@apple.com>
2023-01-23 16:24:52 -0800

    Fix clang-tidy bugprone-infinite-loop warnings in WebCore::AudioSampleDataSource::pullAvailableSamplesAsChunks() https://bugs.webkit.org/show_bug.cgi?id=251044 <rdar://104575403>
    Reviewed by NOBODY (OOPS!).
    
    Return early if `sampleCountPerChunk` is zero to prevent an
    infinite loop.
    
    * Source/WebCore/platform/audio/cocoa/AudioSampleDataSource.mm:
    (WebCore::AudioSampleDataSource::pullAvailableSamplesAsChunks):
    - Add debug assert, and early return if `sampleCountPerChunk` is
      zero.

--- Source/WebCore/platform/audio/cocoa/AudioSampleDataSource.mm
+++ Source/WebCore/platform/audio/cocoa/AudioSampleDataSource.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2021 Apple Inc. All rights reserved.
+ * Copyright (C) 2017-2023 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -293,6 +293,10 @@ bool AudioSampleDataSource::pullAvailableSamplesAsChunks(AudioBufferList& buffer
 {
     ASSERT(buffer.mNumberBuffers == m_ringBuffer->channelCount());
     if (buffer.mNumberBuffers != m_ringBuffer->channelCount())
+        return false;
+
+    ASSERT(sampleCountPerChunk);
+    if (!sampleCountPerChunk)
         return false;
 
     auto [startFrame, endFrame] = m_ringBuffer->getFetchTimeBounds();
