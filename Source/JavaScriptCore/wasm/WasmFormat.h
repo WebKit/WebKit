3e28bdb07aa262a34c5c9becfee6f9d36d0a1cf9
Asumu Takikawa <asumu@igalia.com>
2023-01-26 06:29:45 -0800

    [WASM-Function-References] Fix block signature parsing for reftypes https://bugs.webkit.org/show_bug.cgi?id=247383
    Reviewed by Justin Michaud.
    
    With the typed function reference proposal, block signatures can also
    have indexed reference types (and non-externref/funcref signatures in
    general). This patch adds support for parsing these cases.
    
    While for reftypes with non-index heap types it is possible to
    pre-populate the block signatures in TypeInformation, for indexed types
    the block signatures have to be created after the type section is read.
    
    This patch also makes BlockSignature a RefPtr type so that the type
    signature is kept live while the block signatures are in use in the
    validator (even if the signature is not in the ModuleInformation's type
    signature list).
    
    * JSTests/wasm/function-references/block_signature.js: Added.
    (module):
    (async blockSignatureTest):
    * JSTests/wasm/gc-spec-tests/type-equivalence.wast.js:
    * Source/JavaScriptCore/wasm/WasmFormat.h:
    * Source/JavaScriptCore/wasm/WasmFunctionParser.h:
    (JSC::Wasm::FunctionParser<Context>::unify):
    * Source/JavaScriptCore/wasm/WasmParser.h:
    (JSC::Wasm::Parser<SuccessType>::parseBlockSignature):
    
    Canonical link: https://commits.webkit.org/259421@main

--- Source/JavaScriptCore/wasm/WasmFormat.h
+++ Source/JavaScriptCore/wasm/WasmFormat.h
@@ -54,7 +54,7 @@ struct CompilationContext;
 struct ModuleInformation;
 struct UnlinkedHandlerInfo;
 
-using BlockSignature = const TypeDefinition*;
+using BlockSignature = RefPtr<const TypeDefinition>;
 
 enum class TableElementType : uint8_t {
     Externref,
