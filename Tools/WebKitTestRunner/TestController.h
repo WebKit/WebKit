WebKit/WebKit

commit 9a6f00e09dd1714190585288993b1edb9605b708

Add initial support for screen orientation locking / unlocking https://bugs.webkit.org/show_bug.cgi?id=246302
Reviewed by Darin Adler.

Add initial support for screen orientation locking / unlocking:
- https://w3c.github.io/screen-orientation/#lock-method
- https://w3c.github.io/screen-orientation/#unlock-method

Locking / Unlocking is now functional in layout tests on iOS simulator,
allowing us to run the WPT tests for this feature.

* LayoutTests/TestExpectations:
* LayoutTests/platform/ios-wk2/imported/w3c/web-platform-tests/screen-orientation/active-lock-expected.txt: Added.
* LayoutTests/platform/ios-wk2/imported/w3c/web-platform-tests/screen-orientation/event-before-promise-expected.txt: Added.
* LayoutTests/platform/ios-wk2/imported/w3c/web-platform-tests/screen-orientation/lock-basic-expected.txt: Added.
* LayoutTests/platform/ios-wk2/imported/w3c/web-platform-tests/screen-orientation/lock-unlock-check-expected.txt: Added.
* LayoutTests/platform/ios-wk2/imported/w3c/web-platform-tests/screen-orientation/onchange-event-expected.txt: Added.
* LayoutTests/platform/ios-wk2/imported/w3c/web-platform-tests/screen-orientation/onchange-event-subframe-expected.txt: Added.
* LayoutTests/platform/ios-wk2/imported/w3c/web-platform-tests/screen-orientation/orientation-reading-expected.txt: Added.
Rebaseline tests on iOS now that they are properly running.

* Source/WebCore/Sources.txt:
* Source/WebCore/WebCore.xcodeproj/project.pbxproj:
* Source/WebCore/page/ScreenOrientation.cpp:
(WebCore::ScreenOrientation::lock):
(WebCore::ScreenOrientation::unlock): Deleted.
(WebCore::ScreenOrientation::type const): Deleted.
(WebCore::ScreenOrientation::angle const): Deleted.
(WebCore::ScreenOrientation::visibilityStateChanged): Deleted.
(WebCore::ScreenOrientation::shouldListenForChangeNotification const): Deleted.
(WebCore::ScreenOrientation::screenOrientationDidChange): Deleted.
(WebCore::ScreenOrientation::activeDOMObjectName const): Deleted.

Tools/WebKitTestRunner/TestController.h
@@ -390,6 +390,11 @@ public:
 
     void handleQueryPermission(WKStringRef, WKSecurityOriginRef, WKQueryPermissionResultCallbackRef);
 
+#if PLATFORM(IOS)
+    void lockScreenOrientation(WKScreenOrientationType);
+    void unlockScreenOrientation();
+#endif
+
 private:
     WKRetainPtr<WKPageConfigurationRef> generatePageConfiguration(const TestOptions&);
     WKRetainPtr<WKContextConfigurationRef> generateContextConfiguration(const TestOptions&) const;
@@ -451,6 +456,7 @@ private:
 
 #if PLATFORM(IOS_FAMILY)
     UIPasteboardConsistencyEnforcer *pasteboardConsistencyEnforcer();
+    void restorePortraitOrientationIfNeeded();
 #endif
 
     // WKContextInjectedBundleClient

Tools/WebKitTestRunner/ios/TestControllerIOS.mm
@@ -164,32 +164,25 @@ static _WKFocusStartsInputSessionPolicy focusStartsInputSessionPolicy(const Test
     return _WKFocusStartsInputSessionPolicyAuto;
 }
 
-static void restorePortraitOrientationIfNeeded(PlatformWebView* platformWebView, Seconds timeoutDuration)
+void TestController::restorePortraitOrientationIfNeeded()
 {
-#if HAVE(UI_WINDOW_SCENE_GEOMETRY_PREFERENCES)
-    if (!platformWebView)
+#if PLATFORM(IOS)
+    auto *scene = mainWebView()->platformView().window.windowScene;
+    if (scene.interfaceOrientation == UIInterfaceOrientationPortrait)
         return;
 
-    auto *scene = platformWebView->platformView().window.windowScene;
-    if (scene.effectiveGeometry.interfaceOrientation == UIInterfaceOrientationPortrait)
-        return;
-
-    auto geometryPreferences = adoptNS([[UIWindowSceneGeometryPreferencesIOS alloc] initWithInterfaceOrientations:UIInterfaceOrientationMaskPortrait]);
-    [scene requestGeometryUpdateWithPreferences:geometryPreferences.get() errorHandler:^(NSError *error) {
-        NSLog(@"Failed to restore portrait orientation with error: %@.", error);
-    }];
+    lockScreenOrientation(kWKScreenOrientationTypePortraitPrimary);
 
     auto startTime = MonotonicTime::now();
     while ([NSRunLoop.currentRunLoop runMode:NSDefaultRunLoopMode beforeDate:NSDate.distantPast]) {
-        if (scene.effectiveGeometry.interfaceOrientation == UIInterfaceOrientationPortrait)
+        if (scene.interfaceOrientation == UIInterfaceOrientationPortrait)
             break;
 
-        if (MonotonicTime::now() - startTime >= timeoutDuration)
+        if (MonotonicTime::now() - startTime >= m_currentInvocation->shortTimeout())
             break;
     }
+    unlockScreenOrientation();
 #else
-    UNUSED_PARAM(platformWebView);
-    UNUSED_PARAM(timeoutDuration);
     [[UIDevice currentDevice] setOrientation:UIDeviceOrientationPortrait animated:NO];
 #endif
 }
@@ -204,7 +197,7 @@ bool TestController::platformResetStateToConsistentValues(const TestOptions& opt
     [[UIScreen mainScreen] _setScale:2.0];
     [[HIDEventGenerator sharedHIDEventGenerator] resetActiveModifiers];
 
-    restorePortraitOrientationIfNeeded(mainWebView(), m_currentInvocation->shortTimeout());
+    restorePortraitOrientationIfNeeded();
 
     // Ensures that only the UCB is on-screen when showing the keyboard, if the hardware keyboard is attached.
     TIPreferencesController *textInputPreferences = [getTIPreferencesControllerClass() sharedPreferencesController];
@@ -446,5 +439,38 @@ UIPasteboardConsistencyEnforcer *TestController::pasteboardConsistencyEnforcer()
         m_pasteboardConsistencyEnforcer = adoptNS([[UIPasteboardConsistencyEnforcer alloc] initWithPasteboardName:UIPasteboardNameGeneral]);
     return m_pasteboardConsistencyEnforcer.get();
 }
+
+#if PLATFORM(IOS)
+void TestController::lockScreenOrientation(WKScreenOrientationType orientation)
+{
+    TestRunnerWKWebView *webView = mainWebView()->platformView();
+    switch (orientation) {
+    case kWKScreenOrientationTypePortraitPrimary:
+        webView.supportedInterfaceOrientations = UIInterfaceOrientationMaskPortrait;
+        break;
+    case kWKScreenOrientationTypePortraitSecondary:
+        webView.supportedInterfaceOrientations = UIInterfaceOrientationMaskPortraitUpsideDown;
+        break;
+    case kWKScreenOrientationTypeLandscapePrimary:
+        webView.supportedInterfaceOrientations = UIInterfaceOrientationMaskLandscapeLeft;
+        break;
+    case kWKScreenOrientationTypeLandscapeSecondary:
+        webView.supportedInterfaceOrientations = UIInterfaceOrientationMaskLandscapeRight;
+        break;
+    }
+    [UIView performWithoutAnimation:^{
+        [webView.window.rootViewController setNeedsUpdateOfSupportedInterfaceOrientations];
+    }];
+}
+
+void TestController::unlockScreenOrientation()
+{
+    TestRunnerWKWebView *webView = mainWebView()->platformView();
+    webView.supportedInterfaceOrientations = UIInterfaceOrientationMaskAll;
+    [UIView performWithoutAnimation:^{
+        [webView.window.rootViewController setNeedsUpdateOfSupportedInterfaceOrientations];
+    }];
+}
+#endif
 
 } // namespace WTR

LayoutTests/TestExpectations
@@ -6082,9 +6082,6 @@ js/ShadowRealm-iframe-sandboxed.html [ Pass Failure ]
 js/ShadowRealm-importValue.html [ Pass Failure ]
 js/ShadowRealm-worker.html [ Pass Failure ]
 
-# We don't support screen orientation locking yet, making these tests flaky.
-imported/w3c/web-platform-tests/screen-orientation/orientation-reading.html [ Failure Pass ]
-
 # Old SVG+currentcolor tests are incorrect https://bugs.webkit.org/show_bug.cgi?id=245400
 imported/w3c/web-platform-tests/svg/import/color-prop-05-t-manual.svg [ Skip ]
 svg/W3C-SVG-1.1-SE/color-prop-05-t.svg [ Skip ]

LayoutTests/imported/w3c/web-platform-tests/screen-orientation/non-fully-active-expected.txt
@@ -1,5 +1,5 @@
 
-FAIL Attempting to lock non-fully active documents results in a InvalidStateError promise_test: Unhandled rejection with value: object "TypeError: null is not an object (evaluating 'document.body.appendChild')"
-FAIL Attempting to unlock non-fully active documents results in a InvalidStateError promise_test: Unhandled rejection with value: object "TypeError: null is not an object (evaluating 'document.body.appendChild')"
-FAIL Making a document non-fully active while locking results in an AbortError promise_test: Unhandled rejection with value: object "TypeError: null is not an object (evaluating 'document.body.appendChild')"
+PASS Attempting to lock non-fully active documents results in a InvalidStateError
+PASS Attempting to unlock non-fully active documents results in a InvalidStateError
+PASS Making a document non-fully active while locking results in an AbortError
 

LayoutTests/imported/w3c/web-platform-tests/screen-orientation/non-fully-active.html
@@ -3,6 +3,7 @@
 <script src="/resources/testharnessreport.js"></script>
 <script src="/resources/testdriver.js"></script>
 <script src="/resources/testdriver-vendor.js"></script>
+<body>
 <script>
   async function attachIFrame() {
     const iframe = document.createElement("iframe");
@@ -21,11 +22,13 @@
       ? "landscape"
       : "portrait";
 
+    const frameDOMException = iframe.contentWindow.DOMException;
     iframe.remove();
 
     await promise_rejects_dom(
       t,
       "InvalidStateError",
+      frameDOMException,
       orientation.lock(opposite)
     );
   }, "Attempting to lock non-fully active documents results in a InvalidStateError");
@@ -34,9 +37,10 @@
     const iframe = await attachIFrame();
     const { orientation } = iframe.contentWindow.screen;
 
+    const frameDOMException = iframe.contentWindow.DOMException;
     iframe.remove();
 
-    assert_throws_dom(t, "InvalidStateError", () => { orientation.unlock() });
+    assert_throws_dom("InvalidStateError", frameDOMException, () => { orientation.unlock() });
   }, "Attempting to unlock non-fully active documents results in a InvalidStateError");
 
   promise_test(async (t) => {
@@ -52,9 +56,11 @@
 
     const p = orientation.lock(opposite);
 
+    const frameDOMException = iframe.contentWindow.DOMException;
     iframe.remove();
 
-    await promise_rejects_dom(t, "AbortError", p);
-    assert_throws_dom(t, "InvalidStateError", () => { orientation.unlock() });
+    await promise_rejects_dom(t, "AbortError", frameDOMException, p);
+    assert_throws_dom("InvalidStateError", frameDOMException, () => { orientation.unlock() });
   }, "Making a document non-fully active while locking results in an AbortError");
 </script>
+</body>

LayoutTests/imported/w3c/web-platform-tests/screen-orientation/onchange-event.html
@@ -14,10 +14,10 @@ promise_test(async t => {
   await test_driver.bless("request full screen", () => {
     return document.documentElement.requestFullscreen();
   });
-  const type = screen.orientation.type.startsWith("portrait") ? "landscape" : "portrait";
+  const type = screen.orientation.type.startsWith("portrait") ? "portrait" : "landscape";
   screen.orientation.onchange = t.unreached_func("change event should not be fired");
   await screen.orientation.lock(type);
-  assert_equals(screen.orientation.type, type);
+  assert_true(screen.orientation.type.startsWith(type));
 }, "Test that orientationchange event is not fired when the orientation does not change.");
 
 promise_test(async t => {
@@ -41,12 +41,14 @@ promise_test(async t => {
 
   for (const orientation of orientations) {
     // change event is fired before resolving promise by lock.
+    let lockPromise = screen.orientation.lock(orientation);
     const result = await Promise.race([
-      screen.orientation.lock(orientation),
+      lockPromise,
       orientationWatcher.wait_for('change'),
     ]);
-    assert_equals(result instanceof Event, "The event must be fired first.");
-    assert_equals(screen.orientation.type, orientation);
+    assert_true(result instanceof Event, "The event must be fired first.");
+    assert_true(screen.orientation.type.startsWith(orientation), "The orientation must match");
+    await lockPromise;
   }
 }, "Test that orientationchange event is fired when the orientation changes.");
 </script>

LayoutTests/platform/ios/imported/w3c/web-platform-tests/screen-orientation/active-lock-expected.txt
@@ -1,0 +1,3 @@
+
+FAIL When performing a fragment navigation, the orientation must not change or unlock assert_unreached: change event must not fire Reached unreachable code
+

LayoutTests/platform/ios/imported/w3c/web-platform-tests/screen-orientation/event-before-promise-expected.txt
@@ -1,0 +1,3 @@
+
+PASS The 'change' event must fire before the [[orientationPendingPromise]] is resolved.
+

commit 30bf9df2e3d0b12f39ae0658cc9666e9cbfe6d0e

[iOS] fast/events/iOS/rotation are failing https://bugs.webkit.org/show_bug.cgi?id=251227 rdar://104203486
Reviewed by Wenson Hsieh.

This seems to have regressed with 255459@main, which updated the logic
to restore portrait orientation between tests when we added support
for the screen orientation API.

There are 2 ways to restore the orientation:
1. Call [UIScene requestGeometryUpdateWithPreferences:]
2. Lock the orientation to portrait then unlock

In 255459@main, I had replaced method 1 with method 2 because method 1
didn't work properly with the screen orientation lock tests. However,
it broke the tests in fast/events/iOS/rotation which rely on
`[UIScene requestGeometryUpdateWithPreferences:]` to simulate screen
rotation during the tests.

After several hours of trying to find a common mechanism to restore
orientation that would work for both screen rotation and orientation
lock tests, I still wasn't able to make it work.

For this reason, in my this, I am using method 2 only for tests that
lock the screen orientation and keep using the old method 1 for all
other tests. As a result, this restores pre-255459@main behavior and
fixes existing tests. It also doesn't regress the screen orientation
lock tests which were added in 255459@main.

* Tools/WebKitTestRunner/TestController.h:
* Tools/WebKitTestRunner/ios/TestControllerIOS.mm:
(-[WindowDidRotateObserver initWithCallback:]):
(-[WindowDidRotateObserver dealloc]):
(-[WindowDidRotateObserver _windowDidRotate]):
(WTR::TestController::restorePortraitOrientationIfNeeded):
(WTR::TestController::lockScreenOrientation):

Canonical link: https://commits.webkit.org/259485@main

Tools/WebKitTestRunner/TestController.h
@@ -682,6 +682,7 @@ private:
     WKRetainPtr<WKArrayRef> m_openPanelFileURLs;
 #if PLATFORM(IOS_FAMILY)
     WKRetainPtr<WKDataRef> m_openPanelFileURLsMediaIcon;
+    bool m_didLockOrientation { false };
 #endif
 
     std::unique_ptr<EventSenderProxy> m_eventSenderProxy;

Tools/WebKitTestRunner/ios/TestControllerIOS.mm
@@ -53,6 +53,44 @@
 SOFT_LINK_PRIVATE_FRAMEWORK(TextInput)
 SOFT_LINK_CLASS(TextInput, TIPreferencesController);
 
+#if HAVE(UI_WINDOW_SCENE_GEOMETRY_PREFERENCES)
+
+@interface WindowDidRotateObserver : NSObject
+@property (nonatomic, readonly) void (^callback)();
+@end
+
+@implementation WindowDidRotateObserver {
+}
+
+- (WindowDidRotateObserver *)initWithCallback:(void (^)())callback
+{
+    self = [super init];
+    if (!self)
+        return nil;
+
+    _callback = callback;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_windowDidRotate) name:UIWindowDidRotateNotification object:nil];
+    return self;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIWindowDidRotateNotification object:nil];
+    [super dealloc];
+}
+
+- (void)_windowDidRotate
+{
+    callOnMainThread([self, protectedSelf = RetainPtr<WindowDidRotateObserver>(self)] {
+        if (_callback)
+            _callback();
+    });
+}
+
+@end
+
+#endif // HAVE(UI_WINDOW_SCENE_GEOMETRY_PREFERENCES)
+
 static void overrideSyncInputManagerToAcceptedAutocorrection(id, SEL, TIKeyboardCandidate *candidate, TIKeyboardInput *input)
 {
     // Intentionally unimplemented. See usage below for more information.
@@ -166,22 +204,43 @@ static _WKFocusStartsInputSessionPolicy focusStartsInputSessionPolicy(const Test
 
 void TestController::restorePortraitOrientationIfNeeded()
 {
-#if PLATFORM(IOS)
-    auto *scene = mainWebView()->platformView().window.windowScene;
-    if (scene.interfaceOrientation == UIInterfaceOrientationPortrait)
+#if HAVE(UI_WINDOW_SCENE_GEOMETRY_PREFERENCES)
+    if (!mainWebView())
         return;
 
-    lockScreenOrientation(kWKScreenOrientationTypePortraitPrimary);
+    TestRunnerWKWebView *webView = mainWebView()->platformView();
+
+    auto *scene = webView.window.windowScene;
+    if (scene.effectiveGeometry.interfaceOrientation == UIInterfaceOrientationPortrait)
+        return;
+
+    __block bool didRotate = false;
+    auto rotationObserver = adoptNS([[WindowDidRotateObserver alloc] initWithCallback:^{
+        didRotate = true;
+    }]);
+
+    if (m_didLockOrientation)
+        lockScreenOrientation(kWKScreenOrientationTypePortraitPrimary);
+    else {
+        auto geometryPreferences = adoptNS([[UIWindowSceneGeometryPreferencesIOS alloc] initWithInterfaceOrientations:UIInterfaceOrientationMaskPortrait]);
+        [scene requestGeometryUpdateWithPreferences:geometryPreferences.get() errorHandler:^(NSError *error) {
+            NSLog(@"Failed to restore portrait orientation with error: %@.", error);
+        }];
+    }
 
     auto startTime = MonotonicTime::now();
     while ([NSRunLoop.currentRunLoop runMode:NSDefaultRunLoopMode beforeDate:NSDate.distantPast]) {
-        if (scene.interfaceOrientation == UIInterfaceOrientationPortrait)
+        if (scene.effectiveGeometry.interfaceOrientation == UIInterfaceOrientationPortrait)
             break;
 
         if (MonotonicTime::now() - startTime >= m_currentInvocation->shortTimeout())
             break;
     }
-    unlockScreenOrientation();
+    runUntil(didRotate, m_currentInvocation->shortTimeout());
+    if (m_didLockOrientation) {
+        unlockScreenOrientation();
+        m_didLockOrientation = false;
+    }
 #else
     [[UIDevice currentDevice] setOrientation:UIDeviceOrientationPortrait animated:NO];
 #endif
@@ -449,6 +508,8 @@ void TestController::lockScreenOrientation(WKScreenOrientationType orientation)
     // below won't do anything. UIKit prioritizes the top-most scene-sized window when determining interface
     // orientation.
     [webView.window makeKeyWindow];
+
+    m_didLockOrientation = true;
 
     switch (orientation) {
     case kWKScreenOrientationTypePortraitPrimary: