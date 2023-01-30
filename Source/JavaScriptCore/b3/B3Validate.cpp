WebKit/WebKit

commit 46313ff898b6af2081ff7b5ee6a73e8662c560b2

[JSC] Optimize WebAssemblyInstance#exports https://bugs.webkit.org/show_bug.cgi?id=250754 rdar://104367956
Reviewed by Mark Lam.

This patch optimizes WebAssemblyInstance#exports getter by using intrinsic getter mechanism, since this `exports` getter
can be frequently called in wasm code. This makes exports getter roughly 2x faster.

* JSTests/wasm/stress/exports-getter-fold.js: Added.
(async try):
* JSTests/wasm/stress/exports-getter.js: Added.
(bench):
(async try):
(catch):
* Source/JavaScriptCore/dfg/DFGAbstractInterpreterInlines.h:
(JSC::DFG::AbstractInterpreter<AbstractStateType>::executeEffects):
* Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp:
(JSC::DFG::ByteCodeParser::handleIntrinsicGetter):
* Source/JavaScriptCore/dfg/DFGClobberize.h:
(JSC::DFG::clobberize):
* Source/JavaScriptCore/dfg/DFGDoesGC.cpp:
(JSC::DFG::doesGC):
* Source/JavaScriptCore/dfg/DFGFixupPhase.cpp:
(JSC::DFG::FixupPhase::fixupNode):
* Source/JavaScriptCore/dfg/DFGNodeType.h:
* Source/JavaScriptCore/dfg/DFGPredictionPropagationPhase.cpp:
* Source/JavaScriptCore/dfg/DFGSafeToExecute.h:
(JSC::DFG::safeToExecute):
* Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp:
* Source/JavaScriptCore/dfg/DFGSpeculativeJIT.h:
* Source/JavaScriptCore/dfg/DFGSpeculativeJIT32_64.cpp:
(JSC::DFG::SpeculativeJIT::compile):
* Source/JavaScriptCore/dfg/DFGSpeculativeJIT64.cpp:
(JSC::DFG::SpeculativeJIT::compile):
* Source/JavaScriptCore/ftl/FTLAbstractHeapRepository.cpp:
* Source/JavaScriptCore/ftl/FTLAbstractHeapRepository.h:
* Source/JavaScriptCore/ftl/FTLCapabilities.cpp:
(JSC::FTL::canCompile):
* Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp:
(JSC::FTL::DFG::LowerDFGToB3::compileNode):
(JSC::FTL::DFG::LowerDFGToB3::compileGetWebAssemblyInstanceExports):
* Source/JavaScriptCore/jit/IntrinsicEmitter.cpp:
(JSC::IntrinsicGetterAccessCase::canEmitIntrinsicGetter):
(JSC::IntrinsicGetterAccessCase::emitIntrinsicGetter):
* Source/JavaScriptCore/runtime/CommonIdentifiers.h:

Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp
@@ -60,12 +60,14 @@
 #include "JSMapIterator.h"
 #include "JSPropertyNameEnumerator.h"
 #include "JSSetIterator.h"
+#include "JSWebAssemblyInstance.h"
 #include "LLIntThunks.h"
 #include "ProbeContext.h"
 #include "RegExpObject.h"
 #include "ScopedArguments.h"
 #include "TypeProfilerLog.h"
 #include "WeakMapImpl.h"
+#include "WebAssemblyModuleRecord.h"
 #include <wtf/BitVector.h>
 #include <wtf/Box.h>
 #include <wtf/MathExtras.h>
@@ -15819,6 +15821,24 @@ void SpeculativeJIT::compileGetPrototypeOf(Node* node)
         return;
     }
     }
+}
+
+void SpeculativeJIT::compileGetWebAssemblyInstanceExports(Node* node)
+{
+#if ENABLE(WEBASSEMBLY)
+    SpeculateCellOperand base(this, node->child1());
+    GPRTemporary result(this);
+
+    GPRReg baseGPR = base.gpr();
+    GPRReg resultGPR = result.gpr();
+
+    m_jit.loadPtr(CCallHelpers::Address(baseGPR, JSWebAssemblyInstance::offsetOfModuleRecord()), resultGPR);
+    m_jit.loadPtr(CCallHelpers::Address(resultGPR, WebAssemblyModuleRecord::offsetOfExportsObject()), resultGPR);
+
+    cellResult(resultGPR, node);
+#else
+    UNUSED_PARAM(node);
+#endif
 }
 
 void SpeculativeJIT::compileIdentity(Node* node)

Source/JavaScriptCore/dfg/DFGSpeculativeJIT.h
@@ -1306,6 +1306,7 @@ public:
     void compileLoadValueFromMapBucket(Node*);
     void compileExtractValueFromWeakMapGet(Node*);
     void compileGetPrototypeOf(Node*);
+    void compileGetWebAssemblyInstanceExports(Node*);
     void compileIdentity(Node*);
     
     void compileContiguousPutByVal(Node*);

Source/JavaScriptCore/dfg/DFGSpeculativeJIT32_64.cpp
@@ -3419,7 +3419,12 @@ void SpeculativeJIT::compile(Node* node)
         compileGetPrototypeOf(node);
         break;
     }
-        
+
+    case GetWebAssemblyInstanceExports: {
+        compileGetWebAssemblyInstanceExports(node);
+        break;
+    }
+
     case GetByOffset: {
         compileGetByOffset(node);
         break;

Source/JavaScriptCore/dfg/DFGSpeculativeJIT64.cpp
@@ -4569,7 +4569,12 @@ void SpeculativeJIT::compile(Node* node)
         compileGetPrototypeOf(node);
         break;
     }
-        
+
+    case GetWebAssemblyInstanceExports: {
+        compileGetWebAssemblyInstanceExports(node);
+        break;
+    }
+
     case GetByOffset:
     case GetGetterSetterByOffset: {
         compileGetByOffset(node);

Source/JavaScriptCore/ftl/FTLAbstractHeapRepository.h
@@ -121,6 +121,7 @@ namespace JSC { namespace FTL {
     macro(JSRopeString_fiber2, JSRopeString::offsetOfFiber2()) \
     macro(JSScope_next, JSScope::offsetOfNext()) \
     macro(JSSymbolTableObject_symbolTable, JSSymbolTableObject::offsetOfSymbolTable()) \
+    macro(JSWebAssemblyInstance_moduleRecord, JSWebAssemblyInstance::offsetOfModuleRecord()) \
     macro(NativeExecutable_asString, NativeExecutable::offsetOfAsString()) \
     macro(RegExpObject_regExpAndFlags, RegExpObject::offsetOfRegExpAndFlags()) \
     macro(RegExpObject_lastIndex, RegExpObject::offsetOfLastIndex()) \
@@ -166,6 +167,7 @@ namespace JSC { namespace FTL {
     macro(WeakMapImpl_buffer,  WeakMapImpl<WeakMapBucket<WeakMapBucketDataKey>>::offsetOfBuffer()) \
     macro(WeakMapBucket_value, WeakMapBucket<WeakMapBucketDataKeyValue>::offsetOfValue()) \
     macro(WeakMapBucket_key, WeakMapBucket<WeakMapBucketDataKeyValue>::offsetOfKey()) \
+    macro(WebAssemblyModuleRecord_exportsObject, WebAssemblyModuleRecord::offsetOfExportsObject()) \
     macro(Symbol_symbolImpl, Symbol::offsetOfSymbolImpl()) \
 
 #define FOR_EACH_INDEXED_ABSTRACT_HEAP(macro) \

Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp
@@ -1199,6 +1199,9 @@ private:
         case GetPrototypeOf:
             compileGetPrototypeOf();
             break;
+        case GetWebAssemblyInstanceExports:
+            compileGetWebAssemblyInstanceExports();
+            break;
         case AllocatePropertyStorage:
             compileAllocatePropertyStorage();
             break;
@@ -5175,6 +5178,13 @@ IGNORE_CLANG_WARNINGS_END
 
         m_out.appendTo(continuation, lastNext);
         setJSValue(m_out.phi(Int64, monoProto, polyProto, slowResult));
+    }
+
+    void compileGetWebAssemblyInstanceExports()
+    {
+        LValue base = lowCell(m_node->child1());
+        LValue moduleRecord = m_out.loadPtr(base, m_heaps.JSWebAssemblyInstance_moduleRecord);
+        setJSValue(m_out.loadPtr(moduleRecord, m_heaps.WebAssemblyModuleRecord_exportsObject));
     }
 
     LValue typedArrayLength(LValue base, bool acceptResizable, std::optional<TypedArrayType> typedArrayType)

JSTests/wasm/stress/exports-getter-fold.js
@@ -1,0 +1,21 @@
+import { instantiate } from "../wabt-wrapper.js";
+
+var object = { };
+(async function () {
+    try {
+        object.wasmInstance = await instantiate(`
+            (module
+                (func (export "fence") (atomic.fence))
+            )
+            `, {}, { threads: true });
+
+        var result = null;
+        for (var i = 0; i < 1e8; ++i)
+            result = object.wasmInstance.exports;
+        if (typeof result.fence !== 'function')
+            throw result;
+    } catch (e) {
+        print(String(e));
+        $vm.abort();
+    }
+}());

JSTests/wasm/stress/exports-getter.js
@@ -1,0 +1,27 @@
+import { instantiate } from "../wabt-wrapper.js";
+
+function bench(instance)
+{
+    var last = null;
+    for (var i = 0; i < 1e8; ++i)
+        last = instance.exports;
+    return last;
+}
+noInline(bench);
+
+(async function () {
+    try {
+        let instance = await instantiate(`
+            (module
+                (func (export "fence") (atomic.fence))
+            )
+            `, {}, { threads: true });
+
+        let result = bench(instance);
+        if (typeof result.fence !== 'function')
+            throw result;
+    } catch (e) {
+        print(String(e));
+        $vm.abort();
+    }
+}());

commit c789e1fbd78b6a29e596ec80bb8e286b9fdf795c

REGRESSION(main): [JSC] fix ENABLE(WEBASSEMBLY{_B3JIT})
https://bugs.webkit.org/show_bug.cgi?id=251357

Reviewed by NOBODY (OOPS!).

REGRESSION(257865@main) - webkitgtk-2.39.4: Source/JavaScriptCore/b3/B3Validate.cpp:

WasmTypeDefinition.h isn't included if not ENABLE(WEBASSEMBLY_B3JIT).
Also, toB3Type and simdScalarType are not defined if it is included.

REGRESSION(259017@main) - webkitgtk-2.39.5: Source/JavaScriptCore/{dfg,ftl}/*:

Commit 46313ff898b6af2081ff7b5ee6a73e8662c560b2 broke some stuff.

REGRESSION(259250@main) - webkitgtk-2.39.6: Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp:

Commit 3563dd1840c63e77d1bf18a2017edf52df32f263 broke some stuff.

Signed-off-by: Thomas Devoogdt <thomas.devoogdt@barco.com>

Source/JavaScriptCore/b3/B3Validate.cpp
@@ -47,6 +47,12 @@
 #include <wtf/StringPrintStream.h>
 #include <wtf/text/CString.h>
 
+#if ENABLE(WEBASSEMBLY) && ENABLE(WEBASSEMBLY_B3JIT)
+#define simdScalarTypeToB3Type(type) toB3Type(Wasm::simdScalarType(type))
+#else
+#define simdScalarTypeToB3Type(type) B3::Type()
+#endif
+
 namespace JSC { namespace B3 {
 
 namespace {
@@ -452,7 +458,7 @@ public:
             case VectorExtractLane:
                 VALIDATE(!value->kind().hasExtraBits(), ("At ", *value));
                 VALIDATE(value->numChildren() == 1, ("At ", *value));
-                VALIDATE(value->type() == toB3Type(Wasm::simdScalarType(value->asSIMDValue()->simdLane())), ("At ", *value));
+                VALIDATE(value->type() == simdScalarTypeToB3Type(value->asSIMDValue()->simdLane()), ("At ", *value));
                 VALIDATE(value->child(0)->type() == V128, ("At ", *value));
                 break;
             case VectorReplaceLane:
@@ -460,7 +466,7 @@ public:
                 VALIDATE(value->numChildren() == 2, ("At ", *value));
                 VALIDATE(value->type() == V128, ("At ", *value));
                 VALIDATE(value->child(0)->type() == V128, ("At ", *value));
-                VALIDATE(value->child(1)->type() == toB3Type(Wasm::simdScalarType(value->asSIMDValue()->simdLane())), ("At ", *value));
+                VALIDATE(value->child(1)->type() == simdScalarTypeToB3Type(value->asSIMDValue()->simdLane()), ("At ", *value));
                 break;
             case VectorDupElement:
                 VALIDATE(!value->kind().hasExtraBits(), ("At ", *value));
@@ -481,7 +487,7 @@ public:
                 VALIDATE(!value->kind().hasExtraBits(), ("At ", *value));
                 VALIDATE(value->numChildren() == 1, ("At ", *value));
                 VALIDATE(value->type() == V128, ("At ", *value));
-                VALIDATE(value->child(0)->type() == toB3Type(Wasm::simdScalarType(value->asSIMDValue()->simdLane())), ("At ", *value));
+                VALIDATE(value->child(0)->type() == simdScalarTypeToB3Type(value->asSIMDValue()->simdLane()), ("At ", *value));
                 break;
 
             case VectorPopcnt:

Source/JavaScriptCore/dfg/DFGSpeculativeJIT.cpp
@@ -16074,9 +16074,9 @@ void SpeculativeJIT::compileGetPrototypeOf(Node* node)
     }
 }
 
+#if ENABLE(WEBASSEMBLY)
 void SpeculativeJIT::compileGetWebAssemblyInstanceExports(Node* node)
 {
-#if ENABLE(WEBASSEMBLY)
     SpeculateCellOperand base(this, node->child1());
     GPRTemporary result(this);
 
@@ -16087,10 +16087,8 @@ void SpeculativeJIT::compileGetWebAssemblyInstanceExports(Node* node)
     loadPtr(Address(resultGPR, WebAssemblyModuleRecord::offsetOfExportsObject()), resultGPR);
 
     cellResult(resultGPR, node);
-#else
-    UNUSED_PARAM(node);
-#endif
 }
+#endif
 
 void SpeculativeJIT::compileIdentity(Node* node)
 {

Source/JavaScriptCore/dfg/DFGSpeculativeJIT.h
@@ -1303,7 +1303,9 @@ public:
     void compileLoadValueFromMapBucket(Node*);
     void compileExtractValueFromWeakMapGet(Node*);
     void compileGetPrototypeOf(Node*);
+#if ENABLE(WEBASSEMBLY)
     void compileGetWebAssemblyInstanceExports(Node*);
+#endif
     void compileIdentity(Node*);
     
     void compileContiguousPutByVal(Node*);

Source/JavaScriptCore/dfg/DFGSpeculativeJIT32_64.cpp
@@ -3441,7 +3441,11 @@ void SpeculativeJIT::compile(Node* node)
     }
 
     case GetWebAssemblyInstanceExports: {
+#if ENABLE(WEBASSEMBLY)
         compileGetWebAssemblyInstanceExports(node);
+#else
+        RELEASE_ASSERT_NOT_REACHED();
+#endif
         break;
     }
 

Source/JavaScriptCore/dfg/DFGSpeculativeJIT64.cpp
@@ -4593,7 +4593,11 @@ void SpeculativeJIT::compile(Node* node)
     }
 
     case GetWebAssemblyInstanceExports: {
+#if ENABLE(WEBASSEMBLY)
         compileGetWebAssemblyInstanceExports(node);
+#else
+        RELEASE_ASSERT_NOT_REACHED();
+#endif
         break;
     }
 

Source/JavaScriptCore/ftl/FTLAbstractHeapRepository.h
@@ -121,7 +121,6 @@ namespace JSC { namespace FTL {
     macro(JSRopeString_fiber2, JSRopeString::offsetOfFiber2()) \
     macro(JSScope_next, JSScope::offsetOfNext()) \
     macro(JSSymbolTableObject_symbolTable, JSSymbolTableObject::offsetOfSymbolTable()) \
-    macro(JSWebAssemblyInstance_moduleRecord, JSWebAssemblyInstance::offsetOfModuleRecord()) \
     macro(NativeExecutable_asString, NativeExecutable::offsetOfAsString()) \
     macro(RegExpObject_regExpAndFlags, RegExpObject::offsetOfRegExpAndFlags()) \
     macro(RegExpObject_lastIndex, RegExpObject::offsetOfLastIndex()) \
@@ -167,8 +166,15 @@ namespace JSC { namespace FTL {
     macro(WeakMapImpl_buffer,  WeakMapImpl<WeakMapBucket<WeakMapBucketDataKey>>::offsetOfBuffer()) \
     macro(WeakMapBucket_value, WeakMapBucket<WeakMapBucketDataKeyValue>::offsetOfValue()) \
     macro(WeakMapBucket_key, WeakMapBucket<WeakMapBucketDataKeyValue>::offsetOfKey()) \
-    macro(WebAssemblyModuleRecord_exportsObject, WebAssemblyModuleRecord::offsetOfExportsObject()) \
     macro(Symbol_symbolImpl, Symbol::offsetOfSymbolImpl()) \
+
+#if ENABLE(WEBASSEMBLY)
+#define FOR_EACH_ABSTRACT_FIELD_WASM(macro) \
+    macro(JSWebAssemblyInstance_moduleRecord, JSWebAssemblyInstance::offsetOfModuleRecord()) \
+    macro(WebAssemblyModuleRecord_exportsObject, WebAssemblyModuleRecord::offsetOfExportsObject())
+#else
+#define FOR_EACH_ABSTRACT_FIELD_WASM(macro)
+#endif
 
 #define FOR_EACH_INDEXED_ABSTRACT_HEAP(macro) \
     macro(ArrayStorage_vector, ArrayStorage::vectorOffset(), sizeof(WriteBarrier<Unknown>)) \
@@ -210,6 +216,7 @@ public:
 
 #define ABSTRACT_FIELD_DECLARATION(name, offset) AbstractHeap name;
     FOR_EACH_ABSTRACT_FIELD(ABSTRACT_FIELD_DECLARATION)
+    FOR_EACH_ABSTRACT_FIELD_WASM(ABSTRACT_FIELD_DECLARATION)
 #undef ABSTRACT_FIELD_DECLARATION
     
     AbstractHeap& JSCell_freeListNext;

Source/JavaScriptCore/ftl/FTLLowerDFGToB3.cpp
@@ -1204,7 +1204,11 @@ private:
             compileGetPrototypeOf();
             break;
         case GetWebAssemblyInstanceExports:
+#if ENABLE(WEBASSEMBLY)
             compileGetWebAssemblyInstanceExports();
+#else
+            RELEASE_ASSERT_NOT_REACHED();
+#endif
             break;
         case AllocatePropertyStorage:
             compileAllocatePropertyStorage();
@@ -1400,7 +1404,11 @@ private:
             compileCallDirectEval();
             break;
         case CallWasm:
+#if ENABLE(WEBASSEMBLY)
             compileCallWasm();
+#else
+            RELEASE_ASSERT_NOT_REACHED();
+#endif
             break;
         case VarargsLength:
             compileVarargsLength();
@@ -5190,12 +5198,14 @@ IGNORE_CLANG_WARNINGS_END
         setJSValue(m_out.phi(Int64, monoProto, polyProto, slowResult));
     }
 
+#if ENABLE(WEBASSEMBLY)
     void compileGetWebAssemblyInstanceExports()
     {
         LValue base = lowCell(m_node->child1());
         LValue moduleRecord = m_out.loadPtr(base, m_heaps.JSWebAssemblyInstance_moduleRecord);
         setJSValue(m_out.loadPtr(moduleRecord, m_heaps.WebAssemblyModuleRecord_exportsObject));
     }
+#endif
 
     LValue typedArrayLength(LValue base, bool acceptResizable, std::optional<TypedArrayType> typedArrayType)
     {
@@ -11627,6 +11637,7 @@ IGNORE_CLANG_WARNINGS_END
         setJSValue(patchpoint);
     }
 
+#if ENABLE(WEBASSEMBLY)
     void compileCallWasm()
     {
         Node* node = m_node;
@@ -11837,6 +11848,7 @@ IGNORE_CLANG_WARNINGS_END
             }
         }
     }
+#endif
 
     void compileVarargsLength()
     {