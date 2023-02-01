7f5f2f78dedbda857d3bfcc6594fa42d023fea5b
Kiran Pradeep <kiran.happy@gmail.com>
2023-02-01 06:30:29 -0800

    [GTK] Fix for build (linker) failure on WAYLAND target caused by dependency on version WPEBackend_fdo 1.6.0 https://bugs.webkit.org/show_bug.cgi?id=251355
    Reviewed by Michael Catanzaro.
    
    WPEBackend_fdo 1.6.0 did not export symbols to get major/minor version numbers. e.g. wpe_fdo_get_major_version.
    These symbols were exported in version 1.6.1 as shown in
    https://github.com/Igalia/WPEBackend-fdo/commit/1dda80de5372e56cdc6f818a3fca493f7cc1f9d6. Currently this
    results in undefined reference linker error on distributions installing version 1.6.0. (e.g Ubuntu 20.04)
    So these calls are guarded with check of version 1.6.1.
    
    * Source/WebKit/UIProcess/API/glib/WebKitProtocolHandler.cpp:
    (WebKit::WebKitProtocolHandler::handleGPU):
    
    Canonical link: https://commits.webkit.org/259676@main

--- Source/WebKit/UIProcess/API/glib/WebKitProtocolHandler.cpp
+++ Source/WebKit/UIProcess/API/glib/WebKitProtocolHandler.cpp
@@ -252,6 +252,7 @@ void WebKitProtocolHandler::handleGPU(WebKitURISchemeRequest* request)
             WPE_MAJOR_VERSION, WPE_MINOR_VERSION, WPE_MICRO_VERSION,
             wpe_get_major_version(), wpe_get_minor_version(), wpe_get_micro_version());
 
+#if WPE_FDO_CHECK_VERSION(1, 6, 1)
         g_string_append_printf(html,
             " <tbody><tr>"
             "  <td><div class=\"titlename\">WPEBackend-fdo version</div></td>"
@@ -259,6 +260,7 @@ void WebKitProtocolHandler::handleGPU(WebKitURISchemeRequest* request)
             " </tbody></tr>",
             WPE_FDO_MAJOR_VERSION, WPE_FDO_MINOR_VERSION, WPE_FDO_MICRO_VERSION,
             wpe_fdo_get_major_version(), wpe_fdo_get_minor_version(), wpe_fdo_get_micro_version());
+#endif
     }
 #endif
 #endif
