d2a7afb940a10cd9a2fc90d77fbd50ae12cb01ac
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2023-02-01 09:46:59 -0800

    Inline FloatSize::FloatSize(WebCore::IntSize& const) constructor
    Inline FloatSize::FloatSize(WebCore::IntSize& const) constructor
    https://bugs.webkit.org/show_bug.cgi?id=250655
    rdar://problem/104536651
    
    Reviewed by Alan Baradlay.
    
    Merge - https://chromium.googlesource.com/chromium/blink/+/8ef1d2d2935beef0baf377ee37b76f584b4db631
    
    This patch is potential optimization for scrollview code calling IntSize by inlining constructor.
    
    * Source/WebCore/platform/graphics/FloatSize.cpp:
    (FloatSize:FloatSize): Remove 'IntSize' constructor function
    * Source/WebCore/platform/graphics/FloatSize.h: Inline 'FloatSize' constructor
    
    Canonical link: https://commits.webkit.org/259693@main

--- Source/WebCore/platform/graphics/FloatSize.h
+++ Source/WebCore/platform/graphics/FloatSize.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2003-2016 Apple Inc.  All rights reserved.
+ * Copyright (C) 2003-2023 Apple Inc.  All rights reserved.
+ * Copyright (C) 2014 Google Inc.  All rights reserved.
  * Copyright (C) 2005 Nokia.  All rights reserved.
  *               2008 Eric Seidel <eric@webkit.org>
  *
@@ -59,8 +60,12 @@ class IntSize;
 class FloatSize {
 public:
     FloatSize() { }
-    FloatSize(float width, float height) : m_width(width), m_height(height) { }
-    WEBCORE_EXPORT FloatSize(const IntSize&);
+    FloatSize(float width, float height) 
+        : m_width(width)
+        , m_height(height) { }
+    FloatSize(const IntSize& size) 
+        : m_width(size.width())
+        , m_height(size.height()) { }
 
     static FloatSize narrowPrecision(double width, double height);
 
