7b91f2d38778624f81cd47f7932754a72bfef77c
Ryosuke Niwa <rniwa@webkit.org>
2023-01-31 17:01:13 -0800

    Cherry-pick 252432.1026@safari-7614-branch (2a8469e53b2f). rdar://102868995
        Remove inheritance of designMode attribute
        https://bugs.webkit.org/show_bug.cgi?id=248615
        rdar://102868995
    
        Reviewed by Wenson Hsieh and Jonathan Bedard.
    
        Stop making design mode inherit across frame boundaries.
    
        This will prevent a form element from being injected into a victim page via drag & drop
        and the new behavior matches that of Firefox and Chrome.
    
        * LayoutTests/editing/editability/design-mode-does-not-inherit-across-frames-expected.txt: Added.
        * LayoutTests/editing/editability/design-mode-does-not-inherit-across-frames.html: Added.
        * LayoutTests/fast/dom/HTMLElement/iscontenteditable-designmodeon-allinherit-subframe-expected.txt:
        * LayoutTests/fast/dom/HTMLElement/iscontenteditable-designmodeon-allinherit-subframe.html:
        * Source/WebCore/dom/Document.cpp:
        (WebCore::Document::setDesignMode):
        (WebCore::Document::inDesignMode const): Deleted.
        * Source/WebCore/dom/Document.h:
        (WebCore::Document::inDesignMode const):
    
        Canonical link: https://commits.webkit.org/252432.1026@safari-7614-branch
    
    Canonical link: https://commits.webkit.org/259548.14@safari-7615-branch

--- Source/WebCore/dom/Document.cpp
+++ Source/WebCore/dom/Document.cpp
@@ -6172,15 +6172,10 @@ void Document::setTransformSource(std::unique_ptr<TransformSource> source)
 
 #endif
 
-void Document::setDesignMode(InheritedBool value)
+void Document::setDesignMode(DesignMode value)
 {
     m_designMode = value;
-    for (RefPtr<AbstractFrame> frame = m_frame.get(); frame; frame = frame->tree().traverseNext(m_frame.get())) {
-        RefPtr localFrame = dynamicDowncast<LocalFrame>(frame.get());
-        if (!localFrame || !localFrame->document())
-            continue;
-        localFrame->document()->scheduleFullStyleRebuild();
-    }
+    scheduleFullStyleRebuild();
 }
 
 String Document::designMode() const
@@ -6190,23 +6185,8 @@ String Document::designMode() const
 
 void Document::setDesignMode(const String& value)
 {
-    InheritedBool mode;
-    if (equalLettersIgnoringASCIICase(value, "on"_s))
-        mode = on;
-    else if (equalLettersIgnoringASCIICase(value, "off"_s))
-        mode = off;
-    else
-        mode = inherit;
+    DesignMode mode = equalLettersIgnoringASCIICase(value, "on"_s) ? DesignMode::On : DesignMode::Off;
     setDesignMode(mode);
-}
-
-bool Document::inDesignMode() const
-{
-    for (const Document* d = this; d; d = d->parentDocument()) {
-        if (d->m_designMode != inherit)
-            return d->m_designMode;
-    }
-    return false;
 }
 
 Document* Document::parentDocument() const
