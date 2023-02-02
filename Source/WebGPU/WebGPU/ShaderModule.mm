53a227e4673bb88b3f34d4df6eaa8bc31886c3fc
Tadeu Zagallo <tzagallo@apple.com>
2023-02-02 02:45:49 -0800

    [WGSL] Allow early compilation without hints https://bugs.webkit.org/show_bug.cgi?id=251494 rdar://104908379
    Reviewed by Myles C. Maxfield.
    
    This is just a temporary workaround so we can start passing some CTS tests.
    For now, since we are not yet using the layout hints, we can just try to
    compile upfront even without them. Later we'll wire this up correctly so that
    if no hints are provide the compilation is deferred.
    
    * Source/WebGPU/WebGPU/ShaderModule.mm:
    (WebGPU::Device::createShaderModule):
    
    Canonical link: https://commits.webkit.org/259747@main

--- Source/WebGPU/WebGPU/ShaderModule.mm
+++ Source/WebGPU/WebGPU/ShaderModule.mm
@@ -109,7 +109,9 @@ Ref<ShaderModule> Device::createShaderModule(const WGPUShaderModuleDescriptor& d
 
     auto checkResult = WGSL::staticCheck(fromAPI(shaderModuleParameters->wgsl.code), std::nullopt, { maxBuffersPlusVertexBuffersForVertexStage() });
 
-    if (std::holds_alternative<WGSL::SuccessfulCheck>(checkResult) && shaderModuleParameters->hints && descriptor.hintCount) {
+    // FIXME: we shouldn't compile early unless hints were passed in. Remove this
+    // once we have wired up the deferred compilation.
+    if (std::holds_alternative<WGSL::SuccessfulCheck>(checkResult)) {
         if (auto result = earlyCompileShaderModule(*this, WTFMove(checkResult), descriptor, fromAPI(descriptor.label)))
             return result.releaseNonNull();
     } else {
