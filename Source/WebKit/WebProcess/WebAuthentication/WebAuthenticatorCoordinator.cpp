04c843246b51d675b302bbf2b75ed918aab0c45c
J Pascoe <j_pascoe@apple.com>
2023-02-02 08:18:13 -0800

    Cherry-pick ba76ae810b9d. rdar://problem/99535627
        [WebAuthn] Conditional passkey request presents conditional control even after AbortController.abort()
        https://bugs.webkit.org/show_bug.cgi?id=250589
        rdar://99535627
    
        Reviewed by Brent Fulgham.
    
        Currently conditional mediation requests do not get cancelled whenever the abort controller for its
        request is called. This patch changes that by calling cancel on the daemon whenever the abort controller
        is called.
    
        * Source/WebCore/Modules/webauthn/AuthenticatorCoordinator.cpp:
        (WebCore::AuthenticatorCoordinator::discoverFromExternalSource):
        * Source/WebCore/Modules/webauthn/AuthenticatorCoordinatorClient.h:
        * Source/WebKit/UIProcess/WebAuthentication/WebAuthenticatorCoordinatorProxy.cpp:
        (WebKit::WebAuthenticatorCoordinatorProxy::cancel):
        * Source/WebKit/UIProcess/WebAuthentication/WebAuthenticatorCoordinatorProxy.h:
        * Source/WebKit/UIProcess/WebAuthentication/WebAuthenticatorCoordinatorProxy.messages.in:
        * Source/WebKit/WebProcess/WebAuthentication/WebAuthenticatorCoordinator.cpp:
        (WebKit::WebAuthenticatorCoordinator::cancel):
        * Source/WebKit/WebProcess/WebAuthentication/WebAuthenticatorCoordinator.h:
    
        Canonical link: https://commits.webkit.org/259754@main
    
    Canonical link: https://commits.webkit.org/259548.53@safari-7615.1.21.10-branch

--- Source/WebKit/WebProcess/WebAuthentication/WebAuthenticatorCoordinator.cpp
+++ Source/WebKit/WebProcess/WebAuthentication/WebAuthenticatorCoordinator.cpp
@@ -96,6 +96,11 @@ void WebAuthenticatorCoordinator::isUserVerifyingPlatformAuthenticatorAvailable(
     m_webPage.sendWithAsyncReply(Messages::WebAuthenticatorCoordinatorProxy::IsUserVerifyingPlatformAuthenticatorAvailable(origin.data()), WTFMove(handler));
 }
 
+void WebAuthenticatorCoordinator::cancel()
+{
+    m_webPage.send(Messages::WebAuthenticatorCoordinatorProxy::Cancel());
+}
+
 bool WebAuthenticatorCoordinator::processingUserGesture(const Frame& frame, const FrameIdentifier& frameID)
 {
     auto processingUserGesture = UserGestureIndicator::processingUserGestureForMedia();
