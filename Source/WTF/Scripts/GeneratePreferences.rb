c4c0ad0db413cf856ad127a189abec360db15694
Brent Fulgham <bfulgham@apple.com>
2023-02-02 08:25:51 -0800

    Use 'mature' instead of 'shipping' to represent stable features https://bugs.webkit.org/show_bug.cgi?id=251545 <rdar://problem/104929045>
    Reviewed by Chris Dumez and Elliott Williams.
    
    Replace 'shipping' with 'mature' in various feature-flag places.
    
    * Source/WTF/Scripts/GeneratePreferences.rb:
    * Source/WTF/Scripts/Preferences/UnifiedWebPreferences.yaml:
    * Source/WebCore/Scripts/GenerateSettings.rb:
    * Source/WebKit/UIProcess/API/APIFeatureStatus.h:
    * Source/WebKit/UIProcess/API/Cocoa/_WKFeature.mm:
    (-[_WKFeature status]):
    * Source/WebKitLegacy/mac/WebView/WebFeature.h:
    
    Canonical link: https://commits.webkit.org/259755@main

--- Source/WTF/Scripts/GeneratePreferences.rb
+++ Source/WTF/Scripts/GeneratePreferences.rb
@@ -239,7 +239,7 @@ class Preferences
   end
 
   # Corresponds to WebFeatureStatus enum cases. "developer" and up require human-readable names.
-  STATUSES = %w{ embedder unstable internal developer testable preview stable shipping }
+  STATUSES = %w{ embedder unstable internal developer testable preview stable mature }
 
   def initializeParsedPreferences(parsedPreferences)
     result = []
