5beb5792b8372a5ff4a272069b5c020f65a8f969
Alex Christensen <achristensen@apple.com>
2023-02-02 22:46:04 -0800

    Cherry-pick 259802@main (9efb18ab3957). https://bugs.webkit.org/show_bug.cgi?id=251649
        Don't terminate web process when showing content-filter-provided data that loads subresources
        https://bugs.webkit.org/show_bug.cgi?id=251649
        rdar://104928565
    
        Reviewed by Geoffrey Garen and Andy Estes.
    
        When a content filter blocks a load, it provides HTML to load instead.
        It loads this HTML as substitute data with a base URL of blockedPageURL().
        If this HTML tries to load anything, we would terminate the web content
        process because it was claiming a firstPartyForCookies the network process
        was unaware of.  This makes the network process aware in this case.
    
        Covered by an API test.
    
        * Source/WebCore/loader/ContentFilter.cpp:
        (WebCore::ContentFilter::blockedPageURL):
        (WebCore::blockedPageURL): Deleted.
        * Source/WebCore/loader/ContentFilter.h:
        * Source/WebKit/NetworkProcess/NetworkResourceLoader.cpp:
        (WebKit::NetworkResourceLoader::handleProvisionalLoadFailureFromContentFilter):
        * Tools/TestWebKitAPI/Tests/WebKitCocoa/ContentFiltering.mm:
        (-[LoadAlternateNavigationDelegate webViewWebContentProcessDidTerminate:]):
        (TEST):
        * Tools/TestWebKitAPI/Tests/WebKitCocoa/ContentFilteringPlugIn.mm:
        (-[MockContentFilterEnabler initWithCoder:]):
    
        Canonical link: https://commits.webkit.org/259802@main

--- Source/WebKit/NetworkProcess/NetworkResourceLoader.cpp
+++ Source/WebKit/NetworkProcess/NetworkResourceLoader.cpp
@@ -1887,6 +1887,7 @@ void NetworkResourceLoader::cancelMainResourceLoadForContentFilter(const WebCore
 
 void NetworkResourceLoader::handleProvisionalLoadFailureFromContentFilter(const URL& blockedPageURL, WebCore::SubstituteData& substituteData)
 {
+    m_connection->networkProcess().addAllowedFirstPartyForCookies(m_connection->webProcessIdentifier(), RegistrableDomain { WebCore::ContentFilter::blockedPageURL() }, LoadedWebArchive::No, [] { });
     send(Messages::WebResourceLoader::ContentFilterDidBlockLoad(m_unblockHandler, m_unblockRequestDeniedScript, m_contentFilter->blockedError(), blockedPageURL, substituteData));
 }
 #endif // ENABLE(CONTENT_FILTERING_IN_NETWORKING_PROCESS)
