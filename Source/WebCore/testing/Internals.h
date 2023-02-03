3aad9165b15df302e611f79f33378137101d390f
ChangSeok Oh <changseok@webkit.org>
2023-02-03 10:14:03 -0800

    [GStreamer] Fix a failure of fast/speechsynthesis/voices-non-mock.html https://bugs.webkit.org/show_bug.cgi?id=250656
    Reviewed by Philippe Normand.
    
    The voices-non-mock.html used a mac platform-specific value to test the number
    of available voices. This change introduces a new method of Internals, which allows
    each port to decide its minimum expected voice count. Besides, this change includes
    a new expected result for ports using GStreamer since the number of available voices
    for those ports differs from that of the mac port.
    
    * LayoutTests/fast/speechsynthesis/voices-non-mock.html:
    * LayoutTests/platform/glib/TestExpectations:
    * LayoutTests/platform/glib/fast/speechsynthesis/voices-non-mock-expected.txt: Added.
    * Source/WebCore/testing/Internals.cpp:
    (WebCore::Internals::minimumExpectedVoiceCount):
    * Source/WebCore/testing/Internals.h:
    * Source/WebCore/testing/Internals.idl:
    
    Canonical link: https://commits.webkit.org/259820@main

--- Source/WebCore/testing/Internals.h
+++ Source/WebCore/testing/Internals.h
@@ -673,6 +673,7 @@ public:
     void enableMockSpeechSynthesizer();
     void enableMockSpeechSynthesizerForMediaElement(HTMLMediaElement&);
     ExceptionOr<void> setSpeechUtteranceDuration(double);
+    unsigned minimumExpectedVoiceCount();
 #endif
 
 #if ENABLE(MEDIA_STREAM)
