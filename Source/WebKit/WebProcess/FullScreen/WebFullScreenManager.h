53ce8c35db4324b63de6d0a600298eb99387395d
Jean-Yves Avenard <jya@apple.com>
2023-02-04 04:40:35 -0800

    Ensure WebFullScreenManager is removed as EventListener when page closes https://bugs.webkit.org/show_bug.cgi?id=251566 rdar://103823114
    Reviewed by Youenn Fablet.
    
    When the page close, break the cycles with WebFullScreenManager and ensure
    WebFullScreenManager is no longer registered as event listener.
    
    * Source/WebKit/WebProcess/FullScreen/WebFullScreenManager.cpp:
    (WebKit::WebFullScreenManager::~WebFullScreenManager):
    (WebKit::WebFullScreenManager::invalidate):
    (WebKit::WebFullScreenManager::element):
    * Source/WebKit/WebProcess/FullScreen/WebFullScreenManager.h:
    * Source/WebKit/WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::close):
    
    Canonical link: https://commits.webkit.org/259855@main

--- Source/WebKit/WebProcess/FullScreen/WebFullScreenManager.h
+++ Source/WebKit/WebProcess/FullScreen/WebFullScreenManager.h
@@ -58,6 +58,8 @@ public:
     static Ref<WebFullScreenManager> create(WebPage*);
     virtual ~WebFullScreenManager();
 
+    void invalidate();
+
     void didReceiveMessage(IPC::Connection&, IPC::Decoder&);
 
     bool supportsFullScreen(bool withKeyboard);
