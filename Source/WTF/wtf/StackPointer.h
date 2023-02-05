c5b1aaff095f20adff76d53864500f1e42871768
Mark Lam <mark.lam@apple.com>
2023-02-04 18:55:23 -0800

    Work around ASAN linker issue with currentStackPointer(). https://bugs.webkit.org/show_bug.cgi?id=251746 rdar://105047685
    Reviewed by Alex Christensen.
    
    For unknown reasons, the linker is not able to see exported inline asm functions
    for ASAN builds.  As a work around, we'll use the generic currentStackPointer()
    when ASAN_ENABLED.
    
    * Source/WTF/wtf/StackPointer.h:
    
    Canonical link: https://commits.webkit.org/259864@main

--- Source/WTF/wtf/StackPointer.h
+++ Source/WTF/wtf/StackPointer.h
@@ -49,7 +49,7 @@ ALWAYS_INLINE void* currentStackPointer()
     return stackPointer;
 }
 
-#elif !ENABLE(CLOOP)
+#elif !ENABLE(CLOOP) && !ASAN_ENABLED
 
 #define USE_ASM_CURRENT_STACK_POINTER 1
 extern "C" WTF_EXPORT_PRIVATE void* currentStackPointer();
