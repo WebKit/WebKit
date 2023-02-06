31cb0f30efa1a5189ed1707168c1ca059253aaa7
Jonathan Bedard <jbedard@apple.com>
2023-02-06 13:31:45 -0800

    Cherry-pick 259848@main (1b334608e224). rdar://105021853
        [run-webkit-tests] Handle diverse WatchOS device types
        https://bugs.webkit.org/show_bug.cgi?id=251723
        rdar://105021853
    
        Reviewed by Ryan Haddad.
    
        * Tools/Scripts/webkitpy/xcode/device_type.py:
        (DeviceType.standardize_hardware_type): Refactor to allow invocation without a class instance,
        add a check for WatchOS variants.
        (DeviceType.standardized_hardware_type): Invoke refactored function
        * Tools/Scripts/webkitpy/xcode/device_type_unittest.py:
        (DeviceTypeTest.test_watch_standardization):
        * Tools/Scripts/webkitpy/xcode/simulated_device.py:
        (SimulatedDeviceManager._get_device_identifier_for_type): Invoke DeviceType.standardize_hardware_type
        instead of re-implimenting the function.
    
        Canonical link: https://commits.webkit.org/259848@main
    
    Canonical link: https://commits.webkit.org/259548.53@safari-7615-branch

--- Tools/Scripts/webkitpy/xcode/simulated_device.py
+++ Tools/Scripts/webkitpy/xcode/simulated_device.py
@@ -268,9 +268,7 @@ class SimulatedDeviceManager(object):
             ' {}'.format(device_type.standardized_hardware_type.lower()) if device_type.standardized_hardware_type else '',
         )
         for type_id, type_name in SimulatedDeviceManager._device_identifier_to_name.items():
-            if type_name.lower() == type_name_for_request:
-                return type_id
-            if type_name.lower().endswith(DeviceType.FIRST_GENERATION) and type_name.lower()[:-len(DeviceType.FIRST_GENERATION)] == type_name_for_request:
+            if DeviceType.standardize_hardware_type(type_name).lower() == type_name_for_request:
                 return type_id
         return None
 
