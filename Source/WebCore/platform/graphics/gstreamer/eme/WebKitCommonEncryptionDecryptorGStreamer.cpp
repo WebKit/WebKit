213b2427ba82fc79acc9a54240fb851025167b8c
Philippe Normand <philn@igalia.com>
2023-02-07 00:53:30 -0800

    Cherry-pick 259947@main (783b37dbff01). https://bugs.webkit.org/show_bug.cgi?id=251783
        [GStreamer][EME] Misc clean-ups in decryptor base class
        https://bugs.webkit.org/show_bug.cgi?id=251783
    
        Reviewed by Xabier Rodriguez-Calvar.
    
        Chain to parent constructed vfunc and fix a mislabelled debug statement.
    
        * Source/WebCore/platform/graphics/gstreamer/eme/WebKitCommonEncryptionDecryptorGStreamer.cpp:
        (constructed):
        (attachCDMProxy):
    
        Canonical link: https://commits.webkit.org/259947@main

--- Source/WebCore/platform/graphics/gstreamer/eme/WebKitCommonEncryptionDecryptorGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/eme/WebKitCommonEncryptionDecryptorGStreamer.cpp
@@ -109,8 +109,9 @@ static void webkit_media_common_encryption_decrypt_class_init(WebKitMediaCommonE
 
 static void constructed(GObject* object)
 {
-    GstBaseTransform* base = GST_BASE_TRANSFORM(object);
+    GST_CALL_PARENT(G_OBJECT_CLASS, constructed, (object));
 
+    GstBaseTransform* base = GST_BASE_TRANSFORM(object);
     gst_base_transform_set_in_place(base, TRUE);
     gst_base_transform_set_passthrough(base, FALSE);
     gst_base_transform_set_gap_aware(base, FALSE);
@@ -391,7 +392,7 @@ static void attachCDMProxy(WebKitMediaCommonEncryptionDecrypt* self, CDMProxy* p
     WebKitMediaCommonEncryptionDecryptClass* klass = WEBKIT_MEDIA_CENC_DECRYPT_GET_CLASS(self);
 
     Locker locker { priv->lock };
-    GST_ERROR_OBJECT(self, "Attaching CDMProxy %p", proxy);
+    GST_DEBUG_OBJECT(self, "Attaching CDMProxy %p", proxy);
     priv->cdmProxy = proxy;
     klass->cdmProxyAttached(self, priv->cdmProxy);
     priv->condition.notifyOne();
