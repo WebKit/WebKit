570db2bde74d80b5aba9333e2fff3c835dedd80f
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-02-13 15:13:50 -0800

    Fix Usage of IOObjects on Mac https://bugs.webkit.org/show_bug.cgi?id=251859
    Reviewed by Brent Fulgham.
    
    IOMasterPort is deprecated, but still used up until Big Sur, so let's
    add a check for IOMainPort and then use it if the OS has it.
    
    Additionally, there is an unnecessary retain + release before
    IOServiceOpen, so let's remove that.
    
    *Source/WTF/wtf/PlatformHave.h: Add check for IOMainPort
    *Source/WebCore/platform/graphics/mac/GraphicsChecksMac.cpp: Rename
    masterPort to mainPort and remove pointless initialization. Also remove
    unneeded IOObjectRetain and IOObjectRelease calls in the same exact
    codepath. Replace calls from IOMasterPort to IOMainPort
    *Tools/DumpRenderTree/mac/LayoutTestHelper.m: Rename masterPort to
    mainPort. Replace calls from IOMasterPort to IOMainPort
    
    Canonical link: https://commits.webkit.org/260226@main

--- Tools/DumpRenderTree/mac/LayoutTestHelper.m
+++ Tools/DumpRenderTree/mac/LayoutTestHelper.m
@@ -223,15 +223,23 @@ static void simpleSignalHandler(int sig)
 
 static void lockDownDiscreteGraphics(void)
 {
-    mach_port_t masterPort;
-    kern_return_t kernResult = IOMasterPort(bootstrap_port, &masterPort);
-    if (kernResult != KERN_SUCCESS)
+    mach_port_t mainPort;
+
+#if HAVE(IOKIT_MAIN_PORT)
+    if (IOMainPort(bootstrap_port, &mainPort) != KERN_SUCCESS)
         return;
+#else
+    ALLOW_DEPRECATED_DECLARATIONS_BEGIN
+    if (IOMasterPort(bootstrap_port, &mainPort) != KERN_SUCCESS)
+        return;
+    ALLOW_DEPRECATED_DECLARATIONS_END
+#endif
+
     CFDictionaryRef classToMatch = IOServiceMatching("AppleGraphicsControl");
     if (!classToMatch)
         return;
 
-    io_service_t serviceObject = IOServiceGetMatchingService(masterPort, classToMatch);
+    io_service_t serviceObject = IOServiceGetMatchingService(mainPort, classToMatch);
     if (!serviceObject) {
         // The machine does not allow control over the choice of graphics device.
         return;
@@ -242,7 +250,7 @@ static void lockDownDiscreteGraphics(void)
     static io_connect_t permanentLockDownService = 0;
 
     // This call stalls until the graphics device lock is granted.
-    kernResult = IOServiceOpen(serviceObject, mach_task_self(), 1, &permanentLockDownService);
+    kern_return_t kernResult = IOServiceOpen(serviceObject, mach_task_self(), 1, &permanentLockDownService);
     if (kernResult != KERN_SUCCESS) {
         NSLog(@"IOServiceOpen() failed in %s with kernResult = %d", __FUNCTION__, kernResult);
         return;
