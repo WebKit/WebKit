534e11ed95f096c59f8745a9caeaa3cc451670a7
Žan Doberšek <zdobersek@igalia.com>
2023-02-14 07:02:14 -0800

    [WK2] Clean up fixed-length data encoding in Encoder, StreamConnectionEncoder https://bugs.webkit.org/show_bug.cgi?id=251671
    Reviewed by Kimmo Kinnunen.
    
    Remove the encodeFixedLengthData() methods on the Encoder and
    StreamConnectionEncoder classes, moving the logic into the corresponding
    encodeSpan() methods.
    
    One remaining encodeFixedLengthData() call-site in IPCTestingAPI is amended.
    
    * Source/WebKit/Platform/IPC/Encoder.cpp:
    (IPC::Encoder::encodeFixedLengthData): Deleted.
    * Source/WebKit/Platform/IPC/Encoder.h:
    (IPC::Encoder::encodeSpan):
    * Source/WebKit/Platform/IPC/StreamConnectionEncoder.h:
    * Source/WebKit/WebProcess/WebPage/IPCTestingAPI.cpp:
    (WebKit::IPCTestingAPI::encodeTypedArray):
    
    Canonical link: https://commits.webkit.org/260253@main

--- Source/WebKit/WebProcess/WebPage/IPCTestingAPI.cpp
+++ Source/WebKit/WebProcess/WebPage/IPCTestingAPI.cpp
@@ -1884,7 +1884,7 @@ static bool encodeTypedArray(IPC::Encoder& encoder, JSContextRef context, JSValu
     if (!data.buffer)
         return false;
 
-    encoder.encodeFixedLengthData(reinterpret_cast<const uint8_t*>(data.buffer), data.length, 1);
+    encoder.encodeSpan(Span { reinterpret_cast<const uint8_t*>(data.buffer), data.length });
     return true;
 }
 
