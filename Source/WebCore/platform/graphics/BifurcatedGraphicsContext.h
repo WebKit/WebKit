49563768826fd850e3f7dfe9bea4dbdd167fd11d
Said Abou-Hallawa <said@apple.com>
2023-02-14 08:26:48 -0800

    Make GraphicsContext::drawNativeImage() platform independent https://bugs.webkit.org/show_bug.cgi?id=252182 rdar://105409701
    Reviewed by Simon Fraser.
    
    All implementations of drawNativeImage() will be renamed drawPlatformImage(). A
    new implementation for drawNativeImage() will be added to GraphicsContext. All
    it does it calls the new method NativeImage::draw(). For now NativeImage::draw()
    will just call GraphicsContext ::drawPlatformImage() which is the original
    implementation of GraphicsContext::drawNativeImage().
    
    * Source/WebCore/platform/graphics/BifurcatedGraphicsContext.cpp:
    (WebCore::BifurcatedGraphicsContext::drawNativeImageInternal):
    (WebCore::BifurcatedGraphicsContext::drawNativeImage): Deleted.
    * Source/WebCore/platform/graphics/BifurcatedGraphicsContext.h:
    * Source/WebCore/platform/graphics/GraphicsContext.cpp:
    (WebCore::GraphicsContext::drawNativeImage):
    * Source/WebCore/platform/graphics/GraphicsContext.h:
    (WebCore::GraphicsContext::drawNativeImage):
    (WebCore::GraphicsContext::isInTransparencyLayer const):
    (WebCore::GraphicsContext::drawNativeImageInternal):
    (WebCore::GraphicsContext::supportsTransparencyLayers const): Deleted.
    * Source/WebCore/platform/graphics/NativeImage.cpp:
    (WebCore::NativeImage::draw):
    * Source/WebCore/platform/graphics/NativeImage.h:
    * Source/WebCore/platform/graphics/NullGraphicsContext.h:
    * Source/WebCore/platform/graphics/cairo/GraphicsContextCairo.cpp:
    (WebCore::GraphicsContextCairo::drawNativeImageInternal):
    (WebCore::GraphicsContextCairo::drawNativeImage): Deleted.
    * Source/WebCore/platform/graphics/cairo/GraphicsContextCairo.h:
    * Source/WebCore/platform/graphics/cg/GraphicsContextCG.cpp:
    (WebCore::GraphicsContextCG::drawNativeImageInternal):
    (WebCore::GraphicsContextCG::drawNativeImage): Deleted.
    * Source/WebCore/platform/graphics/cg/GraphicsContextCG.h:
    * Source/WebCore/platform/graphics/displaylists/DisplayListRecorder.cpp:
    (WebCore::DisplayList::Recorder::drawNativeImageInternal):
    (WebCore::DisplayList::Recorder::drawNativeImage): Deleted.
    * Source/WebCore/platform/graphics/displaylists/DisplayListRecorder.h:
    * Source/WebCore/platform/graphics/nicosia/cairo/NicosiaCairoOperationRecorder.cpp:
    (Nicosia::CairoOperationRecorder::drawNativeImageInternal):
    (Nicosia::CairoOperationRecorder::drawNativeImage): Deleted.
    * Source/WebCore/platform/graphics/nicosia/cairo/NicosiaCairoOperationRecorder.h:
    
    Canonical link: https://commits.webkit.org/260255@main

--- Source/WebCore/platform/graphics/BifurcatedGraphicsContext.h
+++ Source/WebCore/platform/graphics/BifurcatedGraphicsContext.h
@@ -97,7 +97,7 @@ public:
     void setLineJoin(LineJoin) final;
     void setMiterLimit(float) final;
 
-    void drawNativeImage(NativeImage&, const FloatSize& selfSize, const FloatRect& destRect, const FloatRect& srcRect, const ImagePaintingOptions& = { }) final;
+    void drawNativeImageInternal(NativeImage&, const FloatSize& selfSize, const FloatRect& destRect, const FloatRect& srcRect, const ImagePaintingOptions& = { }) final;
     bool needsCachedNativeImageInvalidationWorkaround(RenderingMode) final;
     void drawSystemImage(SystemImage&, const FloatRect&) final;
     void drawPattern(NativeImage&, const FloatRect& destRect, const FloatRect& tileRect, const AffineTransform& patternTransform, const FloatPoint& phase, const FloatSize& spacing, const ImagePaintingOptions& = { }) final;
--- Source/WebCore/platform/graphics/cg/GraphicsContextCG.h
+++ Source/WebCore/platform/graphics/cg/GraphicsContextCG.h
@@ -90,7 +90,6 @@ public:
     void setLineJoin(LineJoin) final;
     void setMiterLimit(float) final;
 
-    void drawNativeImage(NativeImage&, const FloatSize& selfSize, const FloatRect& destRect, const FloatRect& srcRect, const ImagePaintingOptions& = { }) final;
     void drawPattern(NativeImage&, const FloatRect& destRect, const FloatRect& tileRect, const AffineTransform& patternTransform, const FloatPoint& phase, const FloatSize& spacing, const ImagePaintingOptions& = { }) final;
     bool needsCachedNativeImageInvalidationWorkaround(RenderingMode) override;
 
@@ -133,6 +132,7 @@ protected:
 
 private:
     void convertToDestinationColorSpaceIfNeeded(RetainPtr<CGImageRef>&);
+    void drawNativeImageInternal(NativeImage&, const FloatSize& selfSize, const FloatRect& destRect, const FloatRect& srcRect, const ImagePaintingOptions& = { }) final;
 
     void clearCGShadow();
 
