7224fc38b1a67738fa802bfb75e09b22a90d02bc
Adrian Perez de Castro <aperez@igalia.com>
2023-02-15 13:06:47 +0200

    Unreviewed. Update OptionsWPE.cmake and NEWS for the 2.38.5 release
    * Source/WebKit/wpe/NEWS: Add release notes.
    * Source/cmake/OptionsWPE.cmake: Bump version numbers.

--- Source/cmake/OptionsWPE.cmake
+++ Source/cmake/OptionsWPE.cmake
@@ -1,7 +1,7 @@
 include(GNUInstallDirs)
 include(VersioningUtils)
 
-SET_PROJECT_VERSION(2 38 4)
+SET_PROJECT_VERSION(2 38 5)
 
 # This is required because we use the DEPFILE argument to add_custom_command().
 # Remove after upgrading cmake_minimum_required() to 3.20.
@@ -165,9 +165,9 @@ endif ()
 endif ()
 
 if (WPE_API_VERSION VERSION_EQUAL "1.0")
-    CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT 21 7 18)
+    CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT 21 8 18)
 else ()
-    CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT 2 7 2)
+    CALCULATE_LIBRARY_VERSIONS_FROM_LIBTOOL_TRIPLE(WEBKIT 2 8 2)
 endif ()
 
 set(CMAKE_C_VISIBILITY_PRESET hidden)
