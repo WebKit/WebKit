bad2b1b9cb893c36c7d869820a8fae97ab36356e
Brandon Stewart <brandonstewart@apple.com>
2023-02-15 22:17:48 -0800

    Integrate PGM for large heap allocations https://bugs.webkit.org/show_bug.cgi?id=249371
    Reviewed by Yusuke Suzuki.
    
    This patch turns on PGM allocations for large heaps.
    PGM will be enabled 1 in 1000 times upon process launch, and a PGM allocation
    will be performed every 4,000 to 5,000 times.
    
    This patch also adds re-allocation support to PGM. PGM allocations can be re-allocated
    to a non pgm allocation and vice versa.
    
    Based on benchmarks there was no noticable memory or performance overhead observed when enabling
    this feature.
    
    * Source/bmalloc/libpas/libpas.xcodeproj/project.pbxproj:
    * Source/bmalloc/libpas/src/libpas/bmalloc_heap_config.h:
    * Source/bmalloc/libpas/src/libpas/iso_heap_config.h:
    * Source/bmalloc/libpas/src/libpas/pas_heap.c:
    (pas_heap_create):
    * Source/bmalloc/libpas/src/libpas/pas_probabilistic_guard_malloc_allocator.c:
    (pas_probabilistic_guard_malloc_allocate):
    (pas_probabilistic_guard_malloc_return_as_large_map_entry):
    (pas_probabilistic_guard_malloc_should_call_pgm):
    (pas_probabilistic_guard_malloc_initialize_pgm):
    * Source/bmalloc/libpas/src/libpas/pas_probabilistic_guard_malloc_allocator.h:
    * Source/bmalloc/libpas/src/libpas/pas_try_allocate_common.h:
    (pas_try_allocate_common_impl_slow):
    * Source/bmalloc/libpas/src/libpas/pas_try_reallocate.h:
    (pas_try_reallocate):
    * Source/bmalloc/libpas/src/test/PGMTests.cpp:
    (std::testPGMRealloc):
    (addPGMTests):
    
    Canonical link: https://commits.webkit.org/260353@main

--- Source/bmalloc/libpas/src/libpas/pas_probabilistic_guard_malloc_allocator.h
+++ Source/bmalloc/libpas/src/libpas/pas_probabilistic_guard_malloc_allocator.h
@@ -23,18 +23,20 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-
 #ifndef PAS_PROBABILISTIC_GUARD_MALLOC_ALLOCATOR
 #define PAS_PROBABILISTIC_GUARD_MALLOC_ALLOCATOR
 
 #include "pas_utils.h"
 #include "pas_large_heap.h"
 #include <stdbool.h>
+#include <stdint.h>
 
 PAS_BEGIN_EXTERN_C;
 
-// structure for holding metadata of pgm allocations
-// FIXME : Reduce size of structure
+/*
+ * structure for holding metadata of pgm allocations
+ * FIXME : Reduce size of structure
+ */
 typedef struct pas_pgm_storage pas_pgm_storage;
 struct pas_pgm_storage {
     size_t allocation_size_requested;
@@ -44,22 +46,29 @@ struct pas_pgm_storage {
     uintptr_t start_of_data_pages;
     uintptr_t upper_guard_page;
     uintptr_t lower_guard_page;
+    pas_large_heap* large_heap;
 };
 
-// max amount of free memory that can be wasted (1MB)
+/* max amount of free memory that can be wasted (1MB) */
 #define PAS_PGM_MAX_WASTED_MEMORY (1024 * 1024)
 
-// max amount of virtual memory that can be used by PGM (1GB)
-// including guard pages and wasted memory
+/*
+ * max amount of virtual memory that can be used by PGM (1GB)
+ * including guard pages and wasted memory
+ */
 #define PAS_PGM_MAX_VIRTUAL_MEMORY (1024 * 1024 * 1024)
 
-// Probability that we should call PGM in percentage (0-100)
-#define PAS_PGM_PROBABILITY (1)
-
-/* We want a fast way to determine if we can call PGM or not.
+/*
+ * We want a fast way to determine if we can call PGM or not.
  * It would be really wasteful to recompute this answer each time we try to allocate,
- * so just update this variable each time we allocate or deallocate. */
-extern PAS_API bool pas_pgm_can_use;
+ * so just update this variable each time we allocate or deallocate.
+ */
+extern PAS_API bool pas_probabilistic_guard_malloc_can_use;
+
+extern PAS_API bool pas_probabilistic_guard_malloc_is_initialized;
+
+extern PAS_API uint16_t pas_probabilistic_guard_malloc_random;
+extern PAS_API uint16_t pas_probabilistic_guard_malloc_counter;
 
 pas_allocation_result pas_probabilistic_guard_malloc_allocate(pas_large_heap* large_heap, size_t size, const pas_heap_config* heap_config, pas_physical_memory_transaction* transaction);
 void pas_probabilistic_guard_malloc_deallocate(void* memory);
@@ -68,6 +77,23 @@ size_t pas_probabilistic_guard_malloc_get_free_virtual_memory(void);
 size_t pas_probabilistic_guard_malloc_get_free_wasted_memory(void);
 
 bool pas_probabilistic_guard_malloc_check_exists(uintptr_t mem);
+
+/*
+ * Determine whether PGM can be called at runtime.
+ * PGM will be called between every 4,000 to 5,000 times an allocation is tried.
+ */
+static PAS_ALWAYS_INLINE bool pas_probabilistic_guard_malloc_should_call_pgm(void)
+{
+    if (++pas_probabilistic_guard_malloc_counter == pas_probabilistic_guard_malloc_random) {
+        pas_probabilistic_guard_malloc_counter = 0;
+        return true;
+    }
+
+    return false;
+}
+
+extern PAS_API void pas_probabilistic_guard_malloc_initialize_pgm(void);
+pas_large_map_entry pas_probabilistic_guard_malloc_return_as_large_map_entry(uintptr_t mem);
 
 PAS_END_EXTERN_C;
 
