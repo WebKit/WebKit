WebKit/WebKit

commit fb16c32ad4fc21ecc04a45b295cdde23dd605d2a

Stop restricting iframe lazy loading to http(s) URLs https://bugs.webkit.org/show_bug.cgi?id=243225
Reviewed by Simon Fraser.

Stop restricting iframe lazy loading to http(s) URLs so we can pass a few more WPT tests.

* LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/the-iframe-element/iframe-loading-lazy-expected.txt:
* Source/WebCore/html/HTMLIFrameElement.cpp:
(WebCore::isFrameLazyLoadable):
(WebCore::HTMLIFrameElement::shouldLoadFrameLazily):

Canonical link: https://commits.webkit.org/252852@main

Source/WebCore/html/HTMLIFrameElement.cpp
@@ -166,11 +166,8 @@ void HTMLIFrameElement::setLoadingForBindings(const AtomString& value)
     setAttributeWithoutSynchronization(loadingAttr, value);
 }
 
-static bool isFrameLazyLoadable(const Document& document, const URL& completeURL, const AtomString& loadingAttributeValue)
+static bool isFrameLazyLoadable(const Document& document, const AtomString& loadingAttributeValue)
 {
-    if (!completeURL.protocolIsInHTTPFamily())
-        return false;
-
     if (!document.frame() || !document.frame()->script().canExecuteScripts(NotAboutToExecuteScript))
         return false;
 
@@ -180,9 +177,9 @@ static bool isFrameLazyLoadable(const Document& document, const URL& completeURL
 bool HTMLIFrameElement::shouldLoadFrameLazily()
 {
     if (!m_lazyLoadFrameObserver && document().settings().lazyIframeLoadingEnabled()) {
-        URL completeURL = document().completeURL(frameURL());
-        if (isFrameLazyLoadable(document(), completeURL, attributeWithoutSynchronization(HTMLNames::loadingAttr))) {
+        if (isFrameLazyLoadable(document(), attributeWithoutSynchronization(HTMLNames::loadingAttr))) {
             auto currentReferrerPolicy = referrerPolicy();
+            URL completeURL = document().completeURL(frameURL());
             lazyLoadFrameObserver().observe(AtomString { completeURL.string() }, currentReferrerPolicy);
             return true;
         }

LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/the-iframe-element/iframe-loading-lazy-expected.txt
@@ -3,7 +3,7 @@
 
 PASS In-viewport iframes load eagerly
 PASS Below-viewport iframes load lazily
-FAIL Below-viewport srcdoc iframes load lazily assert_true: The window load event should have fired before the below-viewport srcdoc iframe's subresource loads expected true got false
-FAIL Below-viewport data: url iframes load lazily assert_true: The window load event should have fired before the below-viewport data url iframe loads expected true got false
+PASS Below-viewport srcdoc iframes load lazily
+PASS Below-viewport data: url iframes load lazily
 FAIL Below-viewport blob url iframes load lazily assert_true: The window load event should have fired before the below-viewport blob url iframe loads expected true got false
 

commit b57dc1f8ff99678c9b9012ba6f89c444ad72de2f

Regression(252852@main) Games on kongregate.com are no longer loading https://bugs.webkit.org/show_bug.cgi?id=252636 rdar://104392542
Reviewed by Ryosuke Niwa.

Do not lazy load iframes that have no valid URL or "about:blank" as URL. Lazily
loading such iframes has no performance benefits and can actually cause breakage
as JS (like on kongregate.com) may expect them to load synchronously.

Note that we have a more general problem where we fail to create the Frame &
initial empty document when we decide to lazy load an iframe. This is not correct,
and we should only delay the navigation to the frame URL *after* the creation of
the initial empty document. However, fixing this is a larger change and this is
not a regression from 252852@main. As a result, I am merely adding a FIXME comment
in this patch and will address separately.

* LayoutTests/fast/frames/about-blank-frame-no-lazy-loading-expected.txt: Added.
* LayoutTests/fast/frames/about-blank-frame-no-lazy-loading.html: Added.
Add layout test coverage.

* Source/WebCore/html/HTMLFrameElementBase.cpp:
(WebCore::HTMLFrameElementBase::openURL):
* Source/WebCore/html/HTMLIFrameElement.cpp:
(WebCore::isFrameLazyLoadable):
(WebCore::HTMLIFrameElement::shouldLoadFrameLazily):

Canonical link: https://commits.webkit.org/260612@main

Source/WebCore/html/HTMLFrameElementBase.cpp
@@ -91,6 +91,8 @@ void HTMLFrameElementBase::openURL(LockHistory lockHistory, LockBackForwardList 
     if (m_frameURL.isEmpty())
         m_frameURL = AtomString { aboutBlankURL().string() };
 
+    // FIXME: This should delay the navigation to m_frameURL but shouldn't delay the creation
+    // of the Frame object or the load of the intially empty document.
     if (shouldLoadFrameLazily())
         return;
 

Source/WebCore/html/HTMLIFrameElement.cpp
@@ -167,8 +167,11 @@ void HTMLIFrameElement::setLoadingForBindings(const AtomString& value)
     setAttributeWithoutSynchronization(loadingAttr, value);
 }
 
-static bool isFrameLazyLoadable(const Document& document, const AtomString& loadingAttributeValue)
+static bool isFrameLazyLoadable(const Document& document, const URL& url, const AtomString& loadingAttributeValue)
 {
+    if (!url.isValid() || url.isAboutBlank())
+        return false;
+
     if (!document.frame() || !document.frame()->script().canExecuteScripts(NotAboutToExecuteScript))
         return false;
 
@@ -178,9 +181,9 @@ static bool isFrameLazyLoadable(const Document& document, const AtomString& load
 bool HTMLIFrameElement::shouldLoadFrameLazily()
 {
     if (!m_lazyLoadFrameObserver && document().settings().lazyIframeLoadingEnabled() && !document().quirks().shouldDisableLazyIframeLoadingQuirk()) {
-        if (isFrameLazyLoadable(document(), attributeWithoutSynchronization(HTMLNames::loadingAttr))) {
+        URL completeURL = document().completeURL(frameURL());
+        if (isFrameLazyLoadable(document(), completeURL, attributeWithoutSynchronization(HTMLNames::loadingAttr))) {
             auto currentReferrerPolicy = referrerPolicy();
-            URL completeURL = document().completeURL(frameURL());
             lazyLoadFrameObserver().observe(AtomString { completeURL.string() }, currentReferrerPolicy);
             return true;
         }

LayoutTests/fast/frames/about-blank-frame-no-lazy-loading-expected.txt
@@ -1,0 +1,10 @@
+Tests that it is possible to access frame.contentWindow synchronously on an about:blank iframe, even if marked as lazily loading.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS iframe.contentWindow.location.href is "about:blank"
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/frames/about-blank-frame-no-lazy-loading.html
@@ -1,0 +1,15 @@
+<DOCTYPE html>
+<html>
+<body>
+<script src="../../resources/js-test.js"></script>
+<script>
+description("Tests that it is possible to access frame.contentWindow synchronously on an about:blank iframe, even if marked as lazily loading.");
+
+let iframe = document.createElement("iframe");
+iframe.loading = "lazy";
+document.body.append(iframe);
+
+shouldBeEqualToString("iframe.contentWindow.location.href", "about:blank");
+</script>
+</body>
+</html>