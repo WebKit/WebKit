42fcece21e3c50244a65ae2a602803314d77de04
Said Abou-Hallawa <said@apple.com>
2023-02-21 07:20:06 -0800

    Cherry-pick fab6d531c24f. rdar://problem/105637444
        REGRESSION(260471@main): The platform graphics context colorspace should not be adopted
        https://bugs.webkit.org/show_bug.cgi?id=252629
        rdar://105637444
    
        Reviewed by Darin Adler.
    
        When storing the return raw pointer of CGIOSurfaceContextGetColorSpace() or
        CGBitmapContextGetColorSpace() in a RetainPtr, we should not be using adoptCF().
        The refcount of the colorspace should be incremented when WebKit references it.
    
        * Source/WebCore/platform/graphics/cg/GraphicsContextCG.cpp:
        (WebCore::GraphicsContextCG::colorSpace const):
    
        Canonical link: https://commits.webkit.org/260609@main
    
    Canonical link: https://commits.webkit.org/259548.273@safari-7615-branch

--- Source/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
+++ Source/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
@@ -221,9 +221,9 @@ const DestinationColorSpace& GraphicsContextCG::colorSpace() const
 
     // FIXME: Need to handle kCGContextTypePDF.
     if (CGContextGetType(context) == kCGContextTypeIOSurface)
-        colorSpace = adoptCF(CGIOSurfaceContextGetColorSpace(context));
+        colorSpace = CGIOSurfaceContextGetColorSpace(context);
     else if (CGContextGetType(context) == kCGContextTypeBitmap)
-        colorSpace = adoptCF(CGBitmapContextGetColorSpace(context));
+        colorSpace = CGBitmapContextGetColorSpace(context);
     else
         colorSpace = adoptCF(CGContextCopyDeviceColorSpace(context));
 
