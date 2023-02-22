cfc556b0bff6b0ae73916b3edcd50da0430f4ae7
Alex Christensen <achristensen@webkit.org>
2023-02-21 18:18:56 -0800

    Cherry-pick fb9f9ec928c1. rdar://problem/105728059
        Pass length as parameter with non-literal expected buffers in FragmentedSharedBufferTest
        https://bugs.webkit.org/show_bug.cgi?id=252707
    
        Reviewed by Chris Dumez.
    
        Using strlen on a non-null-terminated buffer isn't great.
    
        * Tools/TestWebKitAPI/Tests/WebCore/SharedBuffer.cpp:
        (TestWebKitAPI::checkBufferWithLength):
        (TestWebKitAPI::checkBuffer):
        (TestWebKitAPI::TEST_F):
    
        Canonical link: https://commits.webkit.org/260647@main
    
    Canonical link: https://commits.webkit.org/259548.314@safari-7615-branch

--- Tools/TestWebKitAPI/Tests/WebCore/SharedBuffer.cpp
+++ Tools/TestWebKitAPI/Tests/WebCore/SharedBuffer.cpp
@@ -195,13 +195,18 @@ TEST_F(FragmentedSharedBufferTest, builder)
     EXPECT_TRUE(builder2.isEmpty());
 }
 
+static void checkBufferWithLength(const uint8_t* buffer, size_t bufferLength, const char* expected, size_t length)
+{
+    ASSERT_EQ(length, bufferLength);
+    for (size_t i = 0; i < length; ++i)
+        EXPECT_EQ(buffer[i], expected[i]);
+}
+
 static void checkBuffer(const uint8_t* buffer, size_t bufferLength, const char* expected)
 {
     // expected is null terminated, buffer is not.
     size_t length = strlen(expected);
-    ASSERT_EQ(length, bufferLength);
-    for (size_t i = 0; i < length; ++i)
-        EXPECT_EQ(buffer[i], expected[i]);
+    checkBufferWithLength(buffer, bufferLength, expected, length);
 }
 
 TEST_F(FragmentedSharedBufferTest, getSomeData)
@@ -235,7 +240,7 @@ TEST_F(FragmentedSharedBufferTest, getSomeData)
     checkBuffer(gh2.data(), gh2.size(), "gh");
     checkBuffer(ghijkl.data(), ghijkl.size(), "ghijkl");
     EXPECT_EQ(gh1.size(), gh2.size());
-    checkBuffer(gh1.data(), gh1.size(), gh2.dataAsCharPtr());
+    checkBufferWithLength(gh1.data(), gh1.size(), gh2.dataAsCharPtr(), gh2.size());
     checkBuffer(l.data(), l.size(), "l");
 }
 
