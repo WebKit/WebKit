5b66b2604932b0682180501f451e04330be5aeb7
Etienne Segonzac <sgz@apple.com>
2023-02-27 11:11:16 -0800

    Cherry-pick b3bdeda02fd5. rdar://problem/105616856
        Adopt new subclass for InteractionRegion layers
        https://bugs.webkit.org/show_bug.cgi?id=252743
        <rdar://105616856>
    
        Reviewed by Darin Adler.
    
        Use the new extension point to get the InteractionRegion layer subclass.
    
        * Source/WebKit/UIProcess/RemoteLayerTree/RemoteLayerTreeInteractionRegionLayers.mm:
        (interactionRegionLayerClass):
        (WebKit::updateLayersForInteractionRegions):
        Remove the explicit string conversion for the element identifier.
    
        Canonical link: https://commits.webkit.org/260892@main
    
    Canonical link: https://commits.webkit.org/260848.1@safari-7616.1.4-branch

--- Source/WebKit/UIProcess/RemoteLayerTree/RemoteLayerTreeInteractionRegionLayers.mm
+++ Source/WebKit/UIProcess/RemoteLayerTree/RemoteLayerTreeInteractionRegionLayers.mm
@@ -37,6 +37,7 @@
 #if USE(APPLE_INTERNAL_SDK)
 #import <WebKitAdditions/RemoteLayerTreePropertyApplierInteractionRegionAdditions.mm>
 #else
+static Class interactionRegionLayerClass() { return [CALayer class]; }
 static void configureLayerForInteractionRegion(CALayer *, NSString *) { }
 #endif
 
@@ -176,7 +177,7 @@ void updateLayersForInteractionRegions(CALayer *layer, RemoteLayerTreeHost& host
                 interactionRegionLayer = layerIterator->value;
                 interactionLayers.remove(layerIterator);
             } else {
-                interactionRegionLayer = adoptNS([[CALayer alloc] init]);
+                interactionRegionLayer = adoptNS([[interactionRegionLayerClass() alloc] init]);
                 [interactionRegionLayer setFrame:rect];
                 [interactionRegionLayer setHitTestsAsOpaque:YES];
 
@@ -197,7 +198,7 @@ void updateLayersForInteractionRegions(CALayer *layer, RemoteLayerTreeHost& host
             }
 
             setInteractionRegion(interactionRegionLayer.get(), region);
-            configureLayerForInteractionRegion(interactionRegionLayer.get(), makeString("WKInteractionRegion-"_s, String::number(region.elementIdentifier.toUInt64())));
+            configureLayerForInteractionRegion(interactionRegionLayer.get(), makeString("WKInteractionRegion-"_s, region.elementIdentifier.toUInt64()));
             [interactionRegionLayer setCornerRadius:std::max(region.borderRadius, minimumBorderRadius)];
         }
     }
