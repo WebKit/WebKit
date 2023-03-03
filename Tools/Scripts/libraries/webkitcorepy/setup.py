c2279007a3a558da49081d5a92b0451af620d8fd
Jonathan Bedard <jbedard@apple.com>
2023-03-02 16:45:05 -0800

    Cherry-pick 261097@main (752a2873e756). rdar://106095209
        [webkitcorepy] AutoInstall unable to install pytest-asyncio (or other packages without setup.py)
        https://bugs.webkit.org/show_bug.cgi?id=253066
        rdar://106095209
    
        Reviewed by Aakash Jain.
    
        * Tools/Scripts/libraries/webkitcorepy/setup.py: Bump version.
        * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/__init__.py: Ditto.
        * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/autoinstall.py:
        (Package.install): If a project has a setup.cfg, a dummy setup.py in the same location can be
        invoked with our existing code to install the package.
    
        Canonical link: https://commits.webkit.org/261097@main
    
    Canonical link: https://commits.webkit.org/259548.354@safari-7615-branch

--- Tools/Scripts/libraries/webkitcorepy/setup.py
+++ Tools/Scripts/libraries/webkitcorepy/setup.py
@@ -30,7 +30,7 @@ def readme():
 
 setup(
     name='webkitcorepy',
-    version='0.14.1',
+    version='0.14.2',
     description='Library containing various Python support classes and functions.',
     long_description=readme(),
     classifiers=[
