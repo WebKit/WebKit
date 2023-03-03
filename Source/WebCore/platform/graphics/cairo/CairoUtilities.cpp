ef98addbf675095e3474581737c03f57d27140c8
Don Olmstead <don.olmstead@sony.com>
2023-03-02 16:06:41 -0800

    Cherry-pick 261107@main (4fac13b14ab4). https://bugs.webkit.org/show_bug.cgi?id=253254
        Network process shouldn't load Cairo
        https://bugs.webkit.org/show_bug.cgi?id=253254
    
        Reviewed by Fujii Hironori.
    
        Move creation of `s_defaultCairoFontOptions` to function scope so it is
        only created when needed.
    
        * Source/WebCore/platform/graphics/cairo/CairoUtilities.cpp:
    
        Canonical link: https://commits.webkit.org/261107@main

--- Source/WebCore/platform/graphics/cairo/CairoUtilities.cpp
+++ Source/WebCore/platform/graphics/cairo/CairoUtilities.cpp
@@ -48,19 +48,23 @@
 
 namespace WebCore {
 
-static NeverDestroyed<cairo_font_options_t*> s_defaultCairoFontOptions = cairo_font_options_create();
+static cairo_font_options_t* defaultCairoFontOptions()
+{
+    static cairo_font_options_t* s_defaultCairoFontOptions = cairo_font_options_create();
+    return s_defaultCairoFontOptions;
+}
 
 const cairo_font_options_t* getDefaultCairoFontOptions()
 {
-    return s_defaultCairoFontOptions;
+    return defaultCairoFontOptions();
 }
 
 static bool s_disableCairoFontHintingForTesting = false;
 
 void disableCairoFontHintingForTesting()
 {
-    cairo_font_options_set_hint_metrics(s_defaultCairoFontOptions, CAIRO_HINT_METRICS_ON);
-    cairo_font_options_set_hint_style(s_defaultCairoFontOptions, CAIRO_HINT_STYLE_NONE);
+    cairo_font_options_set_hint_metrics(defaultCairoFontOptions(), CAIRO_HINT_METRICS_ON);
+    cairo_font_options_set_hint_style(defaultCairoFontOptions(), CAIRO_HINT_STYLE_NONE);
 
     s_disableCairoFontHintingForTesting = true;
 }
@@ -70,14 +74,14 @@ void setDefaultCairoHintOptions(cairo_hint_metrics_t hintMetrics, cairo_hint_sty
     if (s_disableCairoFontHintingForTesting)
         return;
 
-    cairo_font_options_set_hint_metrics(s_defaultCairoFontOptions, hintMetrics);
-    cairo_font_options_set_hint_style(s_defaultCairoFontOptions, hintStyle);
+    cairo_font_options_set_hint_metrics(defaultCairoFontOptions(), hintMetrics);
+    cairo_font_options_set_hint_style(defaultCairoFontOptions(), hintStyle);
 }
 
 void setDefaultCairoAntialiasOptions(cairo_antialias_t antialias, cairo_subpixel_order_t subpixelOrder)
 {
-    cairo_font_options_set_antialias(s_defaultCairoFontOptions, antialias);
-    cairo_font_options_set_subpixel_order(s_defaultCairoFontOptions, subpixelOrder);
+    cairo_font_options_set_antialias(defaultCairoFontOptions(), antialias);
+    cairo_font_options_set_subpixel_order(defaultCairoFontOptions(), subpixelOrder);
 }
 
 void copyContextProperties(cairo_t* srcCr, cairo_t* dstCr)
