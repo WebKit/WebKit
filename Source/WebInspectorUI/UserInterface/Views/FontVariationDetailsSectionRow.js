d0dcfc4c0662f638e82d05512843ff0000a66c05
Razvan Caliman <rcaliman@apple.com>
2023-03-03 12:35:14 -0800

    Web Inspector: Add precise editing controls for variation axes in Fonts sidebar panel https://bugs.webkit.org/show_bug.cgi?id=253199
    Reviewed by Patrick Angle.
    
    Adds input fields to the Fonts siderbar panel to allow editing variation axis values.
    
    HTML number input fields accept and display any character, so we restrict keyboard
    input to just the ones that make up number values supported as axis values.
    
    The warning for an invalid input is delayed while a user is typing to allow them to finish.
    
    An input can accept and display and invalid value. It will be marked with a warning.
    However, the value written to the stylesheet will be either:
    - the default axis value when the input value is invalid;
    - the clamped min/max value when the input value is out of bounds;
    
    When focus is moved away from an invalid input,
    it will update to reflect the actual value used.
    
    Holding the ArrowUp/ArrowDown keys increments/decrements the value within
    the bounds of the min/max range with the corresponding step for that axis.
    
    Holding the Shift key will increase the step by an order of magnitude.
    Holding the Alt/Option key will decrease the step by an order of magnitude.
    
    * Source/WebInspectorUI/Localizations/en.lproj/localizedStrings.js:
    * Source/WebInspectorUI/UserInterface/Models/FontStyles.js:
    Ensure values written to CSS properties are strings.
    
    (WI.FontStyles.axisValueToFontPropertyValue):
    * Source/WebInspectorUI/UserInterface/Views/FontDetailsPanel.js:
    (WI.FontDetailsPanel.prototype.update):
    * Source/WebInspectorUI/UserInterface/Views/FontVariationDetailsSectionRow.css:
    (.details-section > .content > .group > .row.font-variation > input[type="number"]):
    (.details-section > .content > .group > .row.font-variation:has(.warning) > input[type="number"]):
    (.details-section > .content > .group > .row.font-variation > input[type="number"]::-webkit-inner-spin-button):
    (.details-section > .content > .group > .row.font-variation > .value): Deleted.
    (.details-section > .content > .group > .row.font-variation:has(.warning) > .value): Deleted.
    * Source/WebInspectorUI/UserInterface/Views/FontVariationDetailsSectionRow.js:
    (WI.FontVariationDetailsSectionRow):
    (WI.FontVariationDetailsSectionRow.prototype.set value):
    (WI.FontVariationDetailsSectionRow.prototype._getAxisResolution):
    (WI.FontVariationDetailsSectionRow.prototype._showValidity):
    (WI.FontVariationDetailsSectionRow.prototype._handleValueBlurEvent):
    (WI.FontVariationDetailsSectionRow.prototype._handleValueInputEvent):
    (WI.FontVariationDetailsSectionRow.prototype._handleValueKeydownEvent):
    
    Canonical link: https://commits.webkit.org/261162@main

--- Source/WebInspectorUI/UserInterface/Views/FontVariationDetailsSectionRow.js
+++ Source/WebInspectorUI/UserInterface/Views/FontVariationDetailsSectionRow.js
@@ -33,44 +33,55 @@ WI.FontVariationDetailsSectionRow = class FontVariationDetailsSectionRow extends
 
         let {name, tag, value, defaultValue, minimumValue, maximumValue} = fontVariationAxis;
 
+        this._tag = tag;
+        this._minimumValue = minimumValue;
+        this._maximumValue = maximumValue;
+        this._step = this._getAxisResolution();
+
         this._labelElement = this.element.appendChild(document.createElement("div"));
         this._labelElement.className = "label";
 
         this._tagElement = this.element.appendChild(document.createElement("div"));
         this._tagElement.className = "tag";
+        this._tagElement.textContent = this._tag;
+        this._tagElement.tooltip = WI.UIString("Variation axis tag", "Tag tooltip @ Font Details Sidebar", "Tooltip for a variation axis tag that explains what the 4-character label represents.");
 
         this._variationRangeElement = this.element.appendChild(document.createElement("div"));
         this._variationRangeElement.className = "variation-range";
 
-        this._inputRangeElement = this._variationRangeElement.appendChild(document.createElement("input"));
-        this._inputRangeElement.type = "range";
-        this._inputRangeElement.name = tag;
-        this._inputRangeElement.min = minimumValue;
-        this._inputRangeElement.max = maximumValue;
-        this._inputRangeElement.value = value ?? defaultValue;
-        this._inputRangeElement.step = this._getAxisResolution(minimumValue, maximumValue, tag);
+        this._valueSliderElement = this._variationRangeElement.appendChild(document.createElement("input"));
+        this._valueSliderElement.type = "range";
+        this._valueSliderElement.name = tag;
+        this._valueSliderElement.min = minimumValue;
+        this._valueSliderElement.max = maximumValue;
+        this._valueSliderElement.step = this._step;
 
         this._variationMinValueElement = this._variationRangeElement.appendChild(document.createElement("div"));
         this._variationMinValueElement.className = "variation-minvalue";
         this._variationMinValueElement.textContent = this._formatAxisValueAsString(minimumValue);
+        this._variationMinValueElement.tooltip = WI.UIString("Minimum value of variation axis", "Min axis value @ Font Details Sidebar");
 
         this._variationMaxValueElement = this._variationRangeElement.appendChild(document.createElement("div"));
         this._variationMaxValueElement.className = "variation-maxvalue";
         this._variationMaxValueElement.textContent = this._formatAxisValueAsString(maximumValue);
+        this._variationMinValueElement.tooltip = WI.UIString("Maximum value of variation axis", "Max axis value @ Font Details Sidebar");
 
-        this._inputRangeElement.addEventListener("input", (event) => {
-            this.value = event.target.value;
-            this.dispatchEventToListeners(WI.FontVariationDetailsSectionRow.Event.VariationValueChanged, {tag: event.target.name, value: event.target.value});
-        }, {signal: abortSignal});
+        this._valueTextFieldElement = this.element.appendChild(document.createElement("input"));
+        this._valueTextFieldElement.type = "text";
+        this._valueTextFieldElement.name = tag;
+        this._valueTextFieldElement.className = "value";
 
-        this._valueElement = this.element.appendChild(document.createElement("div"));
-        this._valueElement.className = "value";
+        this._valueTextFieldElement.addEventListener("keydown", this._handleValueTextFieldKeydown.bind(this), {signal: abortSignal});
+        this._valueTextFieldElement.addEventListener("input", this._handleValueTextFieldInput.bind(this), {signal: abortSignal});
+        this._valueTextFieldElement.addEventListener("blur", this._handleValueTextFieldBlur.bind(this), {signal: abortSignal});
+        this._valueSliderElement.addEventListener("input", this._handleValueSliderInput.bind(this), {signal: abortSignal});
 
-        this.tag = tag;
         this.label = name;
         this.value = value ?? defaultValue;
 
         this._defaultValue = defaultValue;
+        this._hasValidValue = true;
+        this._skipUpdatingInputValue = false;
     }
 
     // Public
@@ -111,8 +122,15 @@ WI.FontVariationDetailsSectionRow = class FontVariationDetailsSectionRow extends
     {
         this._value = value ?? this._defaultValue;
 
-        this._inputRangeElement.value = this._value;
-        this._valueElement.textContent = this._formatAxisValueAsString(this._value);
+        this._valueSliderElement.value = this._value;
+
+        // Allow an invalid value in the text field while the user is editing.
+        if (!this._hasValidValue && window.document.activeElement === this._valueTextFieldElement)
+            return;
+
+        this._valueTextFieldElement.value = this._formatAxisValueAsString(this._value);
+        this._hasValidValue = this._minimumValue <= this._value && this._value <= this._maximumValue;
+        this._showValidity();
     }
 
     // Private
@@ -127,13 +145,13 @@ WI.FontVariationDetailsSectionRow = class FontVariationDetailsSectionRow extends
         return value.toLocaleString(undefined, options);
     }
 
-    _getAxisResolution(min, max, tag)
+    _getAxisResolution()
     {
         // The `ital` variation axis acts as an on/off toggle (0 = off, 1 = on).
-        if (tag === "ital" && min === 0 && max === 1)
+        if (this._tag === "ital" && this._minimumValue === 0 && this._maximumValue === 1)
             return 1;
 
-        let delta = max - min;
+        let delta = this._maximumValue - this._minimumValue;
         if (delta <= 1)
             return 0.01;
 
@@ -141,6 +159,87 @@ WI.FontVariationDetailsSectionRow = class FontVariationDetailsSectionRow extends
             return 0.1;
 
         return 1;
+    }
+
+    _showValidity()
+    {
+        if (!this._hasValidValue)
+            this.warningMessage = WI.UIString("Axis value outside of supported range: %s – %s", "A warning that is shown in the Font Details Sidebar when the value for a variation axis is outside of the supported range of values").format(this._formatAxisValueAsString(this._minimumValue), this._formatAxisValueAsString(this._maximumValue));
+        else
+            this.warningMessage = null;
+    }
+
+    _handleValueTextFieldBlur(event)
+    {
+        this.value = this._value;
+    }
+
+    _handleValueTextFieldInput(event)
+    {
+        let valueAsString = event.target.value;
+        let valueAsNumber = parseFloat(event.target.value);
+
+        if (!/^\-?\d+(\.\d+)?$/.test(valueAsString)) {
+            this._hasValidValue = false;
+
+            // Don't warn when preparing to type a new value or starting to type a negative number.
+            if (valueAsString !== "" || valueAsString !== "-")
+                this._showValidity();
+
+            this.dispatchEventToListeners(WI.FontVariationDetailsSectionRow.Event.VariationValueChanged, {tag: this._tag, value: this._defaultValue});
+            return;
+        }
+
+        if (valueAsNumber < this._minimumValue || valueAsNumber > this._maximumValue) {
+            this._hasValidValue = false;
+
+            this._showValidity();
+            this.dispatchEventToListeners(WI.FontVariationDetailsSectionRow.Event.VariationValueChanged, {tag: this._tag, value: Number.constrain(valueAsNumber, this._minimumValue, this._maximumValue)});
+            return;
+        }
+
+        this._hasValidValue = true;
+        this.value = valueAsNumber;
+
+        this.dispatchEventToListeners(WI.FontVariationDetailsSectionRow.Event.VariationValueChanged, {tag: this._tag, value: valueAsNumber});
+    }
+
+    _handleValueTextFieldKeydown(event)
+    {
+        // The value field is a text input but is treated as a number input.
+        if (event.key.length === 1 && !/[0-9\.\-]/.test(event.key)) {
+            event.preventDefault();
+            return;
+        }
+
+        let valueAsNumber = parseFloat(event.target.value);
+        let step = this._step;
+
+        if (isNaN(valueAsNumber))
+            return;
+
+        if (event.shiftKey && event.altKey)
+            step = this._step;
+        else if (event.shiftKey)
+            step = this._step * 10;
+        else if (event.altKey)
+            step = this._step / 10;
+
+        if (event.key === "ArrowUp") {
+            this.value = Number.constrain(valueAsNumber + step, this._minimumValue, this._maximumValue);
+            event.preventDefault();
+        }
+
+        if (event.key === "ArrowDown") {
+            this.value = Number.constrain(valueAsNumber - step, this._minimumValue, this._maximumValue);
+            event.preventDefault();
+        }
+    }
+
+    _handleValueSliderInput(event)
+    {
+        this.value = event.target.valueAsNumber;
+        this.dispatchEventToListeners(WI.FontVariationDetailsSectionRow.Event.VariationValueChanged, {tag: this._tag, value: this._value});
     }
 };
 
