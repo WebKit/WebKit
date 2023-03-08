cd833210c0e7e1a3d3beac9e8d70cf02d13ee067
Sam Sneddon <gsnedders@apple.com>
2023-03-08 15:35:36 -0800

    wpt_config_json should read from original config.json https://bugs.webkit.org/show_bug.cgi?id=253602 rdar://106437906
    Reviewed by Ben Nham.
    
    This avoids any risk of a stale, copied config.json being read, as
    might happen if a prior run-webkit-tests run exited uncleanly.
    
    * Tools/Scripts/webkitpy/layout_tests/servers/web_platform_test_server.py:
    (wpt_config_json): This function now reads from the original, non-copied file and does the mutation.
    (WebPlatformTestServer._prepare_config): Make this merely call wpt_config_json and write it to the new location.
    
    Canonical link: https://commits.webkit.org/261389@main

--- Tools/Scripts/webkitpy/layout_tests/servers/web_platform_test_server.py
+++ Tools/Scripts/webkitpy/layout_tests/servers/web_platform_test_server.py
@@ -39,11 +39,16 @@ def doc_root(port_obj):
 
 
 def wpt_config_json(port_obj):
-    config_wk_filepath = port_obj._filesystem.join(port_obj.layout_tests_dir(), doc_root(port_obj), "config.json")
-    if not port_obj.host.filesystem.isfile(config_wk_filepath):
+    fs = port_obj.host.filesystem
+    config_wk_filepath = fs.join(port_obj.layout_tests_dir(), "imported", "w3c", "resources", "config.json")
+    if not fs.isfile(config_wk_filepath):
         return
-    json_data = port_obj._filesystem.read_text_file(config_wk_filepath)
-    return json.loads(json_data)
+    config = json.loads(fs.read_text_file(config_wk_filepath))
+    if port_obj.supports_localhost_aliases and not self._port_obj.get_option('disable_wpt_hostname_aliases'):
+        config['browser_host'] = 'web-platform.test'
+        config['alternate_hosts'] = {'alt': 'not-web-platform.test'}
+    config['ssl']['openssl']['base_path'] = fs.join(port_obj.results_directory(), "_wpt_certs")
+    return config
 
 
 def base_http_url(port_obj, localhost_only=False):
@@ -166,15 +171,8 @@ class WebPlatformTestServer(http_server_base.HttpServerBase):
         self._wsout = self._filesystem.open_text_file_for_writing(self._output_log_path)
 
         _log.debug('Copying WebKit web platform server config.json')
-        config_wk_filename = self._filesystem.join(self._layout_root, "imported", "w3c", "resources", "config.json")
-        if self._filesystem.isfile(config_wk_filename):
-            config = json.loads(self._filesystem.read_text_file(config_wk_filename))
-            if self._port_obj.supports_localhost_aliases and not self._port_obj.get_option('disable_wpt_hostname_aliases'):
-                _log.debug('Using aliases to web-platform.test')
-                config['browser_host'] = 'web-platform.test'
-                config['alternate_hosts'] = {'alt': 'not-web-platform.test'}
-
-            config['ssl']['openssl']['base_path'] = self._filesystem.join(self._output_dir, "_wpt_certs")
+        config = wpt_config_json(self._port_obj)
+        if config:
             self._filesystem.write_text_file(self._config_filename, json.dumps(config))
 
     def _spawn_process(self):
