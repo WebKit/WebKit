WebKit/WebKit

commit 9ed7d9c4e36677ce84c2bd2b2b225d240365fa19

[JSC] Implement RegExp Duplicate Named Capture Groups https://bugs.webkit.org/show_bug.cgi?id=252553 rdar://100335581
Reviewed by Yusuke Suzuki.

This change implements RegExp Duplicate Named Capture Groups (https://github.com/tc39/ecma262/tree/duplicate-named-capture-groups).
Duplicate named captures have a unique positive ID like subpatternIds.  When matching, either in the interpreter or JIT,
space for an unsgined is added in the "output" vector for each duplicate named capture.  When a subpattern that is one of the
duplicates for a named capture matches, that subpattern's ID is saved in its duplicate named capture location of the output
vector.  When such a subpattern doesn't match or backtrackes, a zero is written in the named capture's output location.
When either matching a backreference for a named capture or creating the match result, the participating subpattern for a
duplicate named capture is indirectly accessed via the named capture's output location.

YarrPatterns now contains a map from a duplicate named capture to a vector of unsigneds.  The first value in that vector is
that duplicate named group's unique ID.  The remaining values are the subpatternIds that are part of the named group.
There is also a vector were the value at subpatternId index contains its duplicate named group's ID.  These allow for
finding the subpatternId's for a given duplicate named capture group as well as finding the duplicated named capture ID
for a given subpatternId.

Updated existing LayoutTests/js/regexp-named-capture-groups.html to no longer fail with duplcxate capture names.
Enabled duplicate named capture tests in test262.

* JSTests/test262/config.yaml: Enabled regexp-duplicate-named-groups
* JSTests/stress/regexp-duplicate-named-captures.js: Added.
(arrayToString):
(objectToString):
(dumpValue):
(compareArray):
(compareGroups):
(testRegExp):
(testRegExpSyntaxError):
(testRegExp.x.a):
(testRegExp.a.x):

JSTests/stress/regexp-duplicate-named-captures.js
@@ -1,0 +1,189 @@
+// With verbose set to false, this test is successful if there is no output.  Set verbose to true to see expected matches.
+let verbose = false;
+
+function arrayToString(arr)
+{
+    let str = '';
+    arr.forEach(function(v, index) {
+        if (typeof v == "string")
+            str += "\"" + v + "\"";
+        else
+            str += v;
+
+        if (index != (arr.length - 1))
+            str += ',';
+      });
+  return str;
+}
+
+function objectToString(obj)
+{
+    let str = "";
+
+    firstEntry = true;
+
+    for (const [key, value] of Object.entries(obj)) {
+        if (!firstEntry)
+            str += ", ";
+
+        str += key + ": " + dumpValue(value);
+
+        firstEntry = false;
+    }
+
+    return "{ " + str + " }";
+}
+
+function dumpValue(v)
+{
+    if (v === null)
+        return "<null>";
+
+    if (v === undefined)
+        return "<undefined>";
+
+    if (typeof v == "string")
+        return "\"" + v + "\"";
+
+    let str = "";
+
+    if (v.length)
+        str += arrayToString(v);
+
+    if (v.groups) {
+        groupStr = objectToString(v.groups);
+
+        if (str.length) {
+            if ( groupStr.length)
+                str += ", " + groupStr;
+        } else
+            str = groupStr;
+    }
+
+    return "[ " + str + " ]";
+}
+
+function compareArray(expected, actual)
+{
+    if (expected === null && actual === null)
+        return true;
+
+    if (expected === null) {
+        print("### expected is null, actual is not null");
+        return false;
+    }
+
+    if (actual === null) {
+        print("### expected is not null, actual is null");
+        return false;
+    }
+
+    if (expected.length !== actual.length) {
+        print("### expected.length: " + expected.length + ", actual.length: " + actual.length);
+        return false;
+    }
+
+    for (var i = 0; i < expected.length; i++) {
+        if (expected[i] !== actual[i]) {
+            print("### expected[" + i + "]: \"" + expected[i] + "\" !== actual[" + i + "]: \"" + actual[i] + "\"");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function compareGroups(expected, actual)
+{
+    if (expected === null && actual === null)
+        return true;
+
+    if (expected === null) {
+        print("### expected group is null, actual group is not null");
+        return false;
+    }
+
+    if (actual === null) {
+        print("### expected group is not null, actual group is null");
+        return false;
+    }
+
+    for (const key in expected) {
+        if (expected[key] !== actual[key]) {
+            print("### expected." + key + ": " + dumpValue(expected[key]) + " !== actual." + key + ": " + dumpValue(actual[key]));
+            return false;
+        }
+    }
+
+    return true;
+}
+
+let testNumber = 0;
+
+function testRegExp(re, str, exp, groups)
+{
+    testNumber++;
+
+    if (groups)
+        exp.groups = groups;
+
+    let actual = re.exec(str);
+    let result = compareArray(exp, actual);;
+
+    if (exp.groups) {
+        if (!compareGroups(exp.groups, actual.groups))
+            result = false;
+    }
+
+    if (result) {
+        if (verbose)
+            print(re.toString() + ".exec(" + dumpValue(str) + "), passed ", dumpValue(exp));
+    } else
+        print(re.toString() + ".exec(" + dumpValue(str) + "), FAILED test #" + testNumber + ", Expected ", dumpValue(exp), " got ", dumpValue(actual));
+}
+
+function testRegExpSyntaxError(reString, flags, expError)
+{
+    testNumber++;
+
+    try {
+        let re = new RegExp(reString, flags);
+    } catch (e) {
+        if (e != expError)
+            print("FAILED test #" + testNumber + ", Expected /" + reString + "/" + flags + " to throw \"" + expError + "\" got \"" + e + "\"");
+        else if (verbose)
+            print("/" + reString + "/" + flags + " passed, it threw \"" + expError + "\" as expected");
+    }
+}
+
+// Test 1
+testRegExp(/(?<A>123)|(?<A>456)/, "123", ["123", "123", undefined], { A: "123" });
+testRegExp(/(?<A>123)|(?<A>456)/, "456", ["456", undefined, "456"], { A: "456" });
+testRegExp(/(?:(?<mon>[1-9]|1[0-2])\/(?<day>[1-9]|[12][0-9]|3[01])\/(?<yr>19[6-9][0-9]|20[0-2][0-9]))|(?:(?<day>[1-9]|[12][0-9]|3[01])\-(?<mon>[1-9]|1[0-2])\-(?<yr>19[6-9][0-9]|20[0-2][0-9]))/, "3/24/1992", ["3/24/1992", "3", "24", "1992", undefined, undefined, undefined], { mon: "3", day: "24", yr: "1992" });
+testRegExp(/(?:(?<mon>[1-9]|1[0-2])\/(?<day>[1-9]|[12][0-9]|3[01])\/(?<yr>19[6-9][0-9]|20[0-2][0-9]))|(?:(?<day>[1-9]|[12][0-9]|3[01])\-(?<mon>[1-9]|1[0-2])\-(?<yr>19[6-9][0-9]|20[0-2][0-9]))/, "24-3-1992", ["24-3-1992", undefined, undefined, undefined, "24", "3", "1992"], { mon: "3", day: "24", yr: "1992" });
+testRegExp(/(?<x>a)|(?<x>b)/, "bab", ["b", undefined, "b"]);
+
+// Test 6
+testRegExp(/(?:(?<x>a)|(?<x>b))\k<x>/, "bb", ["bb", undefined, "b"], { x: "b"});
+testRegExp(/(?:(?:(?<x>a)|(?<x>b))\k<x>){2}/, "aabb", ["aabb", undefined, "b"], { x: "b" });
+testRegExp(/(?<A>abc)x|(?<A>abc)y|abcz/, "abcx", ["abcx", "abc", undefined], { A: "abc" });
+testRegExp(/(?<A>abc)x|(?<A>abc)y|abcz/, "abcy", ["abcy", undefined, "abc"], { A: "abc" });
+testRegExp(/(?<A>abc)x|(?<A>abc)y|abcz/, "abcz", ["abcz", undefined, undefined], { A: undefined });
+
+// Test 11
+testRegExp(/(?<A>abc)x|(?<B>abc)y|abcz/, "abcz", ["abcz", undefined, undefined], { A: undefined, B: undefined });
+testRegExp(/(?<x>a)|(?<x>b)/, "ba", ["b", undefined, "b"], { x: "b"});
+testRegExp(/(?<=(?:\k<x>(?<x>c))|(?:\k<x>(?<x>a)))b/, "aab", ["b", undefined, "a"], { x: "a" });
+testRegExp(/(?<=(?:\k<x>(?<x>c))|(?:\k<x>))b/, "aab", ["b", undefined], { x: undefined });
+testRegExp(/(?<=(?:\k<x>(?<x>c))|(?:\k<y>(?<y>a)))b/, "aab", ["b", undefined, "a"], { x: undefined, y: "a" });
+
+// Test 16
+testRegExp(/^(?:(?<a>x)|(?<a>y)|z)\k<a>$/, "z", ["z", undefined, undefined]);
+testRegExp(/^(?:(?<a>x)|(?<a>y)|z){2}\k<a>$/, "xz", ["xz", undefined, undefined]);
+testRegExp(/(?<a>x)|(?:zy\k<a>)/, "zy", ["zy", undefined]);
+testRegExpSyntaxError("(?<A>123)(?<A>456)", "", "SyntaxError: Invalid regular expression: duplicate group specifier name");
+testRegExpSyntaxError("(?<A>123)(?:(?<A>456)|(?<A>789))", "", "SyntaxError: Invalid regular expression: duplicate group specifier name");
+
+// Test 21
+testRegExpSyntaxError("(?<=\\k<a>a)x", "", "SyntaxError: Invalid regular expression: duplicate group specifier name");
+

Source/JavaScriptCore/runtime/RegExp.cpp
@@ -163,10 +163,11 @@ void RegExp::finishCreation(VM& vm)
     }
 
     m_numSubpatterns = pattern.m_numSubpatterns;
-    if (!pattern.m_captureGroupNames.isEmpty() || !pattern.m_namedGroupToParenIndex.isEmpty()) {
+    if (!pattern.m_captureGroupNames.isEmpty() || !pattern.m_namedGroupToParenIndeces.isEmpty()) {
         m_rareData = makeUnique<RareData>();
+        m_rareData->m_numDuplicateNamedCaptureGroups = pattern.m_numDuplicateNamedCaptureGroups;
         m_rareData->m_captureGroupNames.swap(pattern.m_captureGroupNames);
-        m_rareData->m_namedGroupToParenIndex.swap(pattern.m_namedGroupToParenIndex);
+        m_rareData->m_namedGroupToParenIndeces.swap(pattern.m_namedGroupToParenIndeces);
     }
 }
 

Source/JavaScriptCore/runtime/RegExp.h
@@ -91,27 +91,43 @@ public:
     
     unsigned numSubpatterns() const { return m_numSubpatterns; }
 
-    bool hasNamedCaptures()
+    unsigned offsetVectorBaseForNamedCaptures() const
+    {
+        return (numSubpatterns() + 1) * 2;
+    }
+
+    int offsetVectorSize() const
+    {
+        if (!hasNamedCaptures())
+            return offsetVectorBaseForNamedCaptures();
+        return offsetVectorBaseForNamedCaptures() + m_rareData->m_numDuplicateNamedCaptureGroups;
+    }
+
+    bool hasNamedCaptures() const
     {
         return m_rareData && !m_rareData->m_captureGroupNames.isEmpty();
     }
 
-    String getCaptureGroupName(unsigned i)
+    String getCaptureGroupNameForSubpatternId(unsigned i) const
     {
-        if (!i || !m_rareData || m_rareData->m_captureGroupNames.size() <= i)
+        if (!i || !m_rareData || m_rareData->m_captureGroupNames.isEmpty())
             return String();
         ASSERT(m_rareData);
         return m_rareData->m_captureGroupNames[i];
     }
 
-    unsigned subpatternForName(StringView groupName)
+    template <typename Offsets>
+    unsigned subpatternIdForGroupName(StringView groupName, const Offsets ovector) const
     {
         if (!m_rareData)
             return 0;
-        auto it = m_rareData->m_namedGroupToParenIndex.find<StringViewHashTranslator>(groupName);
-        if (it == m_rareData->m_namedGroupToParenIndex.end())
+        auto it = m_rareData->m_namedGroupToParenIndeces.find<StringViewHashTranslator>(groupName);
+        if (it == m_rareData->m_namedGroupToParenIndeces.end())
             return 0;
-        return it->value;
+        if (it->value.size() == 1)
+            return it->value[0];
+
+        return ovector[offsetVectorBaseForNamedCaptures() + it->value[0] - 1];
     }
 
     bool hasCode()
@@ -185,8 +201,13 @@ private:
 
     struct RareData {
         WTF_MAKE_STRUCT_FAST_ALLOCATED;
+        unsigned m_numDuplicateNamedCaptureGroups;
         Vector<String> m_captureGroupNames;
-        HashMap<String, unsigned> m_namedGroupToParenIndex;
+
+        // This first element of the RHS vector is the subpatternId in the non-duplicate case.
+        // For the duplicate case, the first element is the namedCaptureGroupId.
+        // The remaining elements are the subpatternIds for each of the duplicate groups.
+        HashMap<String, Vector<unsigned>> m_namedGroupToParenIndeces;
     };
 
     String m_patternString;

commit 6a323d41f6f0fd052e9daf911fc4b009ad8d1213

REGRESSION(260692@main) 14 Test262 tests fail after this this change https://bugs.webkit.org/show_bug.cgi?id=253573 rdar://106430729
Reviewed by Ross Kirsling.

This fixes three issues introduced in 260692@main:
 1. Improper handling of non-duplicate named groups.  For the case where a non-duplicate
    group appears between two duplicate named groups, the non-duplicate group's match is
    accessed as a duplicate named group, by using it's subpatternId to index into the
    duplicate named group indirect area of the output vector.  This could exceed the range
    of the vector, but always provided the wrong result.  This was fixed by restructuring
    the capture group for name processing in both the renamed addCaptureGroupForName() and
    existing setupNamedCaptures().
 2. When parsing a named back reference where the references subpattern may not have been
    closed, we need to do the same analysis we find for numbered back references as found
    in atomBackReference.  That is searching for an unclosed capture parenthesis that
    contains the current back reference.
 3. We shouldn't try to convert forward references in a lookbehind when an alternative is
    complete as that is premature.  Waiting for the lookbehind assertion is sufficient.

Updated the regexp-duplicate-named-captures.js test, including the group equality checking.

Also renamed the variables *indeces* => *indices*.

* JSTests/stress/regexp-duplicate-named-captures.js:
(testRegExp):
* Source/JavaScriptCore/runtime/RegExp.cpp:
(JSC::RegExp::finishCreation):
* Source/JavaScriptCore/runtime/RegExp.h:
* Source/JavaScriptCore/yarr/YarrPattern.cpp:
(JSC::Yarr::YarrPatternConstructor::addCaptureGroupForName):
(JSC::Yarr::YarrPatternConstructor::tryConvertingForwardReferencesToBackreferences):
(JSC::Yarr::YarrPatternConstructor::atomParenthesesSubpatternBegin):
(JSC::Yarr::YarrPatternConstructor::atomNamedBackReference):
(JSC::Yarr::YarrPatternConstructor::disjunction):

JSTests/stress/regexp-duplicate-named-captures.js
@@ -124,14 +124,11 @@ function testRegExp(re, str, exp, groups)
 {
     testNumber++;
 
-    if (groups)
-        exp.groups = groups;
-
     let actual = re.exec(str);
     let result = compareArray(exp, actual);;
 
-    if (exp.groups) {
-        if (!compareGroups(exp.groups, actual.groups))
+    if (exp) {
+        if (groups && !compareGroups(groups, actual.groups))
             result = false;
     }
 
@@ -186,4 +183,12 @@ testRegExpSyntaxError("(?<A>123)(?:(?<A>456)|(?<A>789))", "", "SyntaxError: Inva
 
 // Test 21
 testRegExpSyntaxError("(?<=\\k<a>a)x", "", "SyntaxError: Invalid regular expression: duplicate group specifier name");
+testRegExp(/(?<b>.).\k<b>/u, "bab", ["bab", "b"]);
+testRegExp(/(?<b>.).\k<b>/u, "baa", null, null);
+testRegExp(/(?<a>\k<a>\w)../u, "bab", ["bab", "b"], { a: "b" });
+testRegExp(/(?:(?<x>a)|(?<y>a)(?<x>b))(?:(?<z>c)|(?<z>d))/, "abc", ["abc", undefined, "a", "b", "c", undefined], { x: "b", y: "a", z: "c" });
 
+// Test 26
+testRegExp(/(?<=(?:\1|b)(aa))./, "aaaax", ["x", "aa"]);
+testRegExp(/(?<=(?:\2|b)(?<=\1(a))(aa))./, "aaaax", ["x", "a", "aa"]);
+testRegExp(/(?<=((?:\3|b))(?<=\2(a))(aa))./, "aaaax", ["x", "aa", "a", "aa"]);

Source/JavaScriptCore/runtime/RegExp.cpp
@@ -163,11 +163,11 @@ void RegExp::finishCreation(VM& vm)
     }
 
     m_numSubpatterns = pattern.m_numSubpatterns;
-    if (!pattern.m_captureGroupNames.isEmpty() || !pattern.m_namedGroupToParenIndeces.isEmpty()) {
+    if (!pattern.m_captureGroupNames.isEmpty() || !pattern.m_namedGroupToParenIndices.isEmpty()) {
         m_rareData = makeUnique<RareData>();
         m_rareData->m_numDuplicateNamedCaptureGroups = pattern.m_numDuplicateNamedCaptureGroups;
         m_rareData->m_captureGroupNames.swap(pattern.m_captureGroupNames);
-        m_rareData->m_namedGroupToParenIndeces.swap(pattern.m_namedGroupToParenIndeces);
+        m_rareData->m_namedGroupToParenIndices.swap(pattern.m_namedGroupToParenIndices);
     }
 }
 

Source/JavaScriptCore/runtime/RegExp.h
@@ -122,8 +122,8 @@ public:
     {
         if (!m_rareData)
             return 0;
-        auto it = m_rareData->m_namedGroupToParenIndeces.find<StringViewHashTranslator>(groupName);
-        if (it == m_rareData->m_namedGroupToParenIndeces.end())
+        auto it = m_rareData->m_namedGroupToParenIndices.find<StringViewHashTranslator>(groupName);
+        if (it == m_rareData->m_namedGroupToParenIndices.end())
             return 0;
         if (it->value.size() == 1)
             return it->value[0];
@@ -208,7 +208,7 @@ private:
         // This first element of the RHS vector is the subpatternId in the non-duplicate case.
         // For the duplicate case, the first element is the namedCaptureGroupId.
         // The remaining elements are the subpatternIds for each of the duplicate groups.
-        HashMap<String, Vector<unsigned>> m_namedGroupToParenIndeces;
+        HashMap<String, Vector<unsigned>> m_namedGroupToParenIndices;
     };
 
     String m_patternString;

Source/JavaScriptCore/yarr/YarrPattern.cpp
@@ -947,41 +947,40 @@ public:
         m_pattern.m_disjunctions.append(WTFMove(body));
     }
 
-    unsigned namedCaptureGroupIdForName(const String groupName, unsigned subpatternId)
+    void addCaptureGroupForName(const String groupName, unsigned subpatternId)
     {
         ASSERT(subpatternId);
-        unsigned namedCaptureGroupId;
 
-        auto addResult = m_pattern.m_namedGroupToParenIndeces.add(groupName, Vector<unsigned>());
+        m_pattern.m_hasNamedCaptureGroups = true;
+
+        auto addResult = m_pattern.m_namedGroupToParenIndices.add(groupName, Vector<unsigned>());
         auto& thisGroupNameSubpatternIds = addResult.iterator->value;
         if (addResult.isNewEntry) {
-            namedCaptureGroupId = ++m_pattern.m_numNamedSubpatterns;
-
             while (m_pattern.m_captureGroupNames.size() < subpatternId)
                 m_pattern.m_captureGroupNames.append(String());
             m_pattern.m_captureGroupNames.append(groupName);
 
-            thisGroupNameSubpatternIds.append(namedCaptureGroupId);
-        } else
-            namedCaptureGroupId = thisGroupNameSubpatternIds[0];
+            thisGroupNameSubpatternIds.append(subpatternId);
+        } else if (thisGroupNameSubpatternIds.size() == 2) {
+            // This named group is now a duplicate.
+            thisGroupNameSubpatternIds[0] = ++m_pattern.m_numDuplicateNamedCaptureGroups;
+        }
 
         thisGroupNameSubpatternIds.append(subpatternId);
-
-        return namedCaptureGroupId;
     }
 
     void tryConvertingForwardReferencesToBackreferences()
     {
         //  There are forward references that could actually be lookbehind back references.
         for (unsigned i = 0; i < m_forwardReferencesInLookbehind.size(); ++i) {
-            auto unresolvedForwardReference = m_forwardReferencesInLookbehind[i];
+            UnresolvedForwardReference& unresolvedForwardReference = m_forwardReferencesInLookbehind[i];
             auto term = unresolvedForwardReference.term();
             if (unresolvedForwardReference.hasNamedGroup()) {
-                auto namedGroupIndecesIter = m_pattern.m_namedGroupToParenIndeces.find(unresolvedForwardReference.namedGroup());
-                if (namedGroupIndecesIter == m_pattern.m_namedGroupToParenIndeces.end())
+                auto namedGroupIndicesIter = m_pattern.m_namedGroupToParenIndices.find(unresolvedForwardReference.namedGroup());
+                if (namedGroupIndicesIter == m_pattern.m_namedGroupToParenIndices.end())
                     continue;
 
-                unsigned namedGroupSubpatternId = namedGroupIndecesIter->value.last();
+                unsigned namedGroupSubpatternId = namedGroupIndicesIter->value.last();
                 if (namedGroupSubpatternId == term->backReferenceSubpatternId) {
                     term->backReferenceSubpatternId = 0;
                     continue;
@@ -1223,7 +1222,7 @@ public:
         if (capture) {
             m_pattern.m_numSubpatterns++;
             if (optGroupName) {
-                namedCaptureGroupIdForName(optGroupName.value(), subpatternId);
+                addCaptureGroupForName(optGroupName.value(), subpatternId);
             }
         } else
             ASSERT(!optGroupName);
@@ -1331,11 +1330,39 @@ public:
 
     void atomNamedBackReference(const String& subpatternName)
     {
-        ASSERT(m_pattern.m_namedGroupToParenIndeces.find(subpatternName) != m_pattern.m_namedGroupToParenIndeces.end());
-        auto namedGroupIndeces = m_pattern.m_namedGroupToParenIndeces.get(subpatternName);
+        ASSERT(m_pattern.m_namedGroupToParenIndices.find(subpatternName) != m_pattern.m_namedGroupToParenIndices.end());
+        auto parenIndices = m_pattern.m_namedGroupToParenIndices.get(subpatternName);
+
+        if (parenIndices.size() == 2) {
+            // If this isn't a duplicate group, we need to go through the same analysis as a non-named backreferece to determine if
+            // this backreference appears in the capture itself. A duplicate could be satisfied by a prior capture and therefore doesn't
+            // need this analysis.
+            unsigned subpatternId = parenIndices.last();
+
+            PatternAlternative* currentAlternative = m_alternative;
+            ASSERT(currentAlternative);
+
+            while ((currentAlternative = currentAlternative->m_parent->m_parent)) {
+                PatternTerm& term = currentAlternative->lastTerm();
+                ASSERT((term.type == PatternTerm::Type::ParenthesesSubpattern) || (term.type == PatternTerm::Type::ParentheticalAssertion));
+
+                if ((term.type == PatternTerm::Type::ParenthesesSubpattern) && term.capture() && (subpatternId == term.parentheses.subpatternId)) {
+                    m_alternative->m_terms.append(PatternTerm::ForwardReference());
+                    return;
+                }
+
+                if (parenthesisMatchDirection() == Backward
+                    && term.type == PatternTerm::Type::ParentheticalAssertion
+                    && term.matchDirection() == Backward
+                    && subpatternId >= term.parentheses.subpatternId) {
+                    m_alternative->m_terms.append(PatternTerm::ForwardReference());
+                    return;
+                }
+            }
+        }
 
         if (parenthesisMatchDirection() == Forward) {
-            m_alternative->m_terms.append(PatternTerm(namedGroupIndeces.last()));
+            m_alternative->m_terms.append(PatternTerm(parenIndices.last()));
             PatternTerm& lastTerm = m_alternative->lastTerm();
             lastTerm.m_matchDirection = parenthesisMatchDirection();
             m_pattern.m_containsBackreferences = true;
@@ -1484,9 +1511,6 @@ public:
             // Top level alternative, record captured ranges to clear out from prior alternatives.
             m_alternative->m_lastSubpatternId = m_pattern.m_numSubpatterns;
         }
-
-        if (!m_forwardReferencesInLookbehind.isEmpty() && parenthesisMatchDirection() == Backward)
-            tryConvertingForwardReferencesToBackreferences();
 
         m_alternative = m_alternative->m_parent->addNewAlternative(m_pattern.m_numSubpatterns, parenthesisMatchDirection());
     }
@@ -1803,36 +1827,31 @@ public:
         }
     }
 
-    void setupDuplicateNamedCaptures()
+    void setupNamedCaptures()
     {
-        if (!m_pattern.m_numNamedSubpatterns)
+        if (!m_pattern.m_hasNamedCaptureGroups)
             return;
 
         // Finish padding out m_captureGroupNames vector.
         while (m_pattern.m_captureGroupNames.size() <= m_pattern.m_numSubpatterns)
             m_pattern.m_captureGroupNames.append(String());
 
-        m_pattern.m_numDuplicateNamedCaptureGroups = 0;
-
-        for (auto& namedGroupIndecies : m_pattern.m_namedGroupToParenIndeces.values()) {
-            if (namedGroupIndecies.size() > 2)
-                m_pattern.m_numDuplicateNamedCaptureGroups++;
-            else {
-                // Since this named group is only used in one place, make that subpatternId
-                // as the only value in the vector.
-                ASSERT(namedGroupIndecies.size() == 2);
-                auto onlyIndex = namedGroupIndecies.takeLast();
-                namedGroupIndecies[0] = onlyIndex;
+        for (auto& namedGroupIndicies : m_pattern.m_namedGroupToParenIndices.values()) {
+            if (namedGroupIndicies.size() == 2) {
+                // Since this named group is only used in one place, i.e. not a duplicate name,
+                // make that subpatternId as the only value in the vector.
+                ASSERT(namedGroupIndicies[0] == namedGroupIndicies[1]);
+                namedGroupIndicies.takeLast();
             }
         }
 
         if (m_pattern.m_numDuplicateNamedCaptureGroups) {
             m_pattern.m_duplicateNamedGroupForSubpatternId.fill(0, m_pattern.m_numSubpatterns + 1);
-            for (auto& namedGroupIndecies : m_pattern.m_namedGroupToParenIndeces.values()) {
-                if (namedGroupIndecies.size() > 2) {
-                    auto duplicateNamedGroupId = namedGroupIndecies[0];
-                    for (unsigned i = 1; i < namedGroupIndecies.size(); ++i) {
-                        auto subpatternId = namedGroupIndecies[i];
+            for (auto& namedGroupIndicies : m_pattern.m_namedGroupToParenIndices.values()) {
+                if (namedGroupIndicies.size() > 2) {
+                    auto duplicateNamedGroupId = namedGroupIndicies[0];
+                    for (unsigned i = 1; i < namedGroupIndicies.size(); ++i) {
+                        auto subpatternId = namedGroupIndicies[i];
                         ASSERT(!m_pattern.m_duplicateNamedGroupForSubpatternId[subpatternId]);
                         m_pattern.m_duplicateNamedGroupForSubpatternId[subpatternId] = duplicateNamedGroupId;
                     }
@@ -1996,7 +2015,7 @@ ErrorCode YarrPattern::compile(StringView patternString)
             return error;
     }
 
-    constructor.setupDuplicateNamedCaptures();
+    constructor.setupNamedCaptures();
 
     if (UNLIKELY(Options::dumpCompiledRegExpPatterns()))
         dumpPattern(patternString);
@@ -2010,6 +2029,7 @@ YarrPattern::YarrPattern(StringView pattern, OptionSet<Flags> flags, ErrorCode& 
     , m_containsLookbehinds(false)
     , m_containsUnsignedLengthPattern(false)
     , m_hasCopiedParenSubexpressions(false)
+    , m_hasNamedCaptureGroups(false)
     , m_saveInitialStartValue(false)
     , m_flags(flags)
 {

Source/JavaScriptCore/yarr/YarrPattern.h
@@ -522,7 +522,6 @@ struct YarrPattern {
     void resetForReparsing()
     {
         m_numSubpatterns = 0;
-        m_numNamedSubpatterns = 0;
         m_initialStartValueFrameLocation = 0;
         m_numDuplicateNamedCaptureGroups = 0;
 
@@ -531,6 +530,7 @@ struct YarrPattern {
         m_containsLookbehinds = false;
         m_containsUnsignedLengthPattern = false;
         m_hasCopiedParenSubexpressions = false;
+        m_hasNamedCaptureGroups = false;
         m_saveInitialStartValue = false;
 
         anycharCached = nullptr;
@@ -548,7 +548,7 @@ struct YarrPattern {
         m_disjunctions.clear();
         m_userCharacterClasses.clear();
         m_captureGroupNames.clear();
-        m_namedGroupToParenIndeces.clear();
+        m_namedGroupToParenIndices.clear();
         m_duplicateNamedGroupForSubpatternId.clear();
     }
 
@@ -701,19 +701,23 @@ struct YarrPattern {
     bool m_containsLookbehinds : 1;
     bool m_containsUnsignedLengthPattern : 1;
     bool m_hasCopiedParenSubexpressions : 1;
+    bool m_hasNamedCaptureGroups : 1;
     bool m_saveInitialStartValue : 1;
     OptionSet<Flags> m_flags;
     unsigned m_numSubpatterns { 0 };
-    unsigned m_numNamedSubpatterns { 0 };
     unsigned m_initialStartValueFrameLocation { 0 };
     unsigned m_numDuplicateNamedCaptureGroups { 0 };
     PatternDisjunction* m_body;
     Vector<std::unique_ptr<PatternDisjunction>, 4> m_disjunctions;
     Vector<std::unique_ptr<CharacterClass>> m_userCharacterClasses;
     Vector<String> m_captureGroupNames;
-    // The first vector entry of m_namedGroupToParenIndeces is the namedSubpatternId.
-    // Subsequent vector entries are the subpatternId(s) when the name is used.
-    HashMap<String, Vector<unsigned>> m_namedGroupToParenIndeces;
+    // The contents of the RHS Vector of m_namedGroupToParenIndices depends on whether the String is a
+    // duplicate named group or not.
+    // For a named group that is only used once in the pattern, the vector size is one and the only entry
+    // is the subpatterenId for a non-duplicate named group.
+    // For a duplicate named group, the size will be greater than 2. The first vector entry it is the
+    // duplicateNamedGroupId. Subsequent vector entries are the subpatternId's for that duplicateNamedGroupId.
+    HashMap<String, Vector<unsigned>> m_namedGroupToParenIndices;
     Vector<unsigned> m_duplicateNamedGroupForSubpatternId;
 
 private: