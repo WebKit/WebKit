4ae8edc354b7bfdeaedf1b945b5d12871450fe0b
Vitaly Dyachkov <vitaly@igalia.com>
2023-03-10 02:12:02 -0800

    Render control characters when taking complex text code path https://bugs.webkit.org/show_bug.cgi?id=153941
    Reviewed by Carlos Garcia Campos.
    
    The spec says: "Control characters (Unicode category Cc)—other than
    tabs (U+0009), line feeds (U+000A), carriage returns (U+000D) and
    sequences that form a segment break—must be rendered as a visible glyph"
    
    When taking simple text code path, glyphs of these characters are replaced
    with `.notdef` glyph. Let's do the same for complex text path as well.
    
    * LayoutTests/platform/gtk/TestExpectations:
    * Source/WebCore/platform/graphics/ComplexTextController.cpp:
    (WebCore::ComplexTextController::adjustGlyphsAndAdvances):
    
    Canonical link: https://commits.webkit.org/261493@main

--- Source/WebCore/platform/graphics/WidthIterator.cpp
+++ Source/WebCore/platform/graphics/WidthIterator.cpp
@@ -615,7 +615,7 @@ void WidthIterator::applyCSSVisibilityRules(GlyphBuffer& glyphBuffer, unsigned g
 
         // https://www.w3.org/TR/css-text-3/#white-space-processing
         // "Control characters (Unicode category Cc)—other than tabs (U+0009), line feeds (U+000A), carriage returns (U+000D) and sequences that form a segment break—must be rendered as a visible glyph"
-        // Also, we're omitting NULL (U+0000) from this set because Chrome and Firefox do so and it's needed for compat. See https://github.com/w3c/csswg-drafts/pull/6983.
+        // Also, we're omitting Null (U+0000) from this set because Chrome and Firefox do so and it's needed for compat. See https://github.com/w3c/csswg-drafts/pull/6983.
         if (characterResponsibleForThisGlyph != nullCharacter && isControlCharacter(characterResponsibleForThisGlyph)) {
             // Let's assume that .notdef is visible.
             GlyphBufferGlyph visibleGlyph = 0;
