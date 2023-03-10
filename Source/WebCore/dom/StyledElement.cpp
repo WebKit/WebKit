fa8dddf7984e4d3f5035805df6dfb6e075287267
Chris Dumez <cdumez@apple.com>
2023-03-10 13:19:51 -0800

    Fix undefined behavior in valueFromPool(Span<LazyNeverDestroyed<CSSPrimitiveValue>>, double) https://bugs.webkit.org/show_bug.cgi?id=253720 rdar://106522324
    Reviewed by Darin Adler.
    
    Fix undefined behavior in valueFromPool(). This appeared to be the cause of
    the crashes we started seeing on production builds yesterday. Alexey P. found
    the issue with a UBSan build.
    
    * Source/WebCore/css/CSSPrimitiveValue.cpp:
    (WebCore::valueFromPool):
    Fix undefined behavior by casting the double to a signed integer because
    casting it to an unsigned one. Casting a negative double to an unsigned
    integer is UB.
    
    * Source/WebCore/dom/StyledElement.cpp:
    (WebCore::StyledElement::setInlineStyleProperty):
    Revert temporary workaround I had landed in 261490@main.
    
    Canonical link: https://commits.webkit.org/261527@main

--- Source/WebCore/dom/StyledElement.cpp
+++ Source/WebCore/dom/StyledElement.cpp
@@ -203,9 +203,7 @@ bool StyledElement::setInlineStyleProperty(CSSPropertyID propertyID, CSSProperty
 
 bool StyledElement::setInlineStyleProperty(CSSPropertyID propertyID, double value, CSSUnitType unit, bool important)
 {
-    auto cssValue = CSSPrimitiveValue::create(value, unit);
-    auto& style = ensureMutableInlineStyle();
-    style.setProperty(propertyID, WTFMove(cssValue), important);
+    ensureMutableInlineStyle().setProperty(propertyID, CSSPrimitiveValue::create(value, unit), important);
     inlineStyleChanged();
     return true;
 }
