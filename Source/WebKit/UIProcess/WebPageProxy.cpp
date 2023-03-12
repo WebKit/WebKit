b0ab3082f7392a5df32bcea91454012d8d758fe0
Chris Dumez <cdumez@apple.com>
2023-03-11 22:10:10 -0700

    Cherry-pick d55e642abe03. rdar://problem/106597341
        Crash under ProvisionalPageProxy::initializeWebPage()
        https://bugs.webkit.org/show_bug.cgi?id=253767
        rdar://106597341
    
        Reviewed by David Kilzer.
    
        receivedNavigationPolicyDecision() calls continueNavigationInNewProcess(), which
        creates a new ProvisionalPageProxy. The ProvisionalPageProxy constructor calls
        ProvisionalPageProxy::initializeWebPage() which accessing m_page.pageClient()
        and crashes with a null dereference.
    
        receivedNavigationPolicyDecision() early returns if Page::isClosed() is true,
        which should indicate that the pageClient couldn't have been null initially.
        This means the pageClient must have been nulled out in between the isClosed()
        check and the ProvisionalPageProxy::initializeWebPage() call. To protect
        against this, I am adding a PageClientProtector to this code path, right after
        the isClosed() check.
    
        * Source/WebKit/UIProcess/WebPageProxy.cpp:
        (WebKit::WebPageProxy::receivedNavigationPolicyDecision):
    
        Canonical link: https://commits.webkit.org/261547@main
    
    Identifier: 259548.451@safari-7615-branch

--- Source/WebKit/UIProcess/WebPageProxy.cpp
+++ Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -3622,6 +3622,8 @@ void WebPageProxy::receivedNavigationPolicyDecision(PolicyAction policyAction, A
             return;
         }
 
+        PageClientProtector pageClientProtector(pageClient());
+
         bool shouldProcessSwap = processForNavigation.ptr() != sourceProcess.ptr();
         if (shouldProcessSwap) {
             policyAction = PolicyAction::StopAllLoads;
