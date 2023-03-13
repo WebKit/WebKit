37dab5e553f6d157683732b971b5ea7ae830ec66
Elliott Williams <emw@apple.com>
2023-03-13 15:30:01 -0700

    [Xcode] Support multiple PGO profiles by matching the platform target triple https://bugs.webkit.org/show_bug.cgi?id=253691 rdar://106459115
    Reviewed by Alexey Proskuryakov.
    
    Change copy-profiling-data to find a profile to use based on the active
    target triple. Using different profiles for different targets is
    valuable for making future performance improvements and addressing
    backwards compatibility.
    
    Since we may not have a profile checked in for an exact target triple,
    at build time, we pick the profile for the closest triple we can find.
    For example, for the target triple arm64e-apple-ios16.4:
    
    1. If profiles are available for arm64e-apple-ios16.4, use them.
    2. Otherwise, find a profile for a prior version of the same platform
       (e.g. arm64e-apple-ios16.0).
    3. If nothing is available for the platform, use the latest macOS
       profile.
    4. If WebKitAdditions is unavailable, fall back to the empty profile.
    
    The existing behavior (where all targets using profiles generated for
    macOS) falls under case 3. Compatibility issues with downlevels are
    handled by cases 1 and 2. And the open-source build (with no profiling
    data) is case 4.
    
    * Configurations/WebKitProjectPaths.xcconfig: Fix
      WK_WEBKITADDITIONS_HEADERS_FOLDER_PATH to work correctly in
      engineering builds. Add WK_PROFILING_DATA_FOLDER_PATH to refer to the
      Profiles directory. This needs to be a discrete build setting so that
      it can be unset in open-source builds, to satisfy XCBuild's dependency
      validation.
    * Source/JavaScriptCore/Configurations/JavaScriptCore.xcconfig: Remove
      PROFILE_DATA_PATH variable.
    * Source/JavaScriptCore/JavaScriptCore.xcodeproj/project.pbxproj: Change
      "Copy Profiling Data" build phase to depend on the entire Profiling/
      directory node. WebKitAdditions is changing to declare a directory
      reference for this path, rather than declaring individual file
      references for the profiles. Any change to the recursive contents of
      the directory will re-run the script.
    
      Note that Xcode currently has no way of knowing that the (copied)
      profile is an input of all compiler tasks, so a latent incremental
      build bug remains. We should adopt the CLANG_USE_OPTIMIZATION_PROFILE
      setting in a follow-up.
    * Source/JavaScriptCore/Scripts/copy-profiling-data.sh: The search logic
      described above.
    * Source/WebCore/Configurations/WebCore.xcconfig: Ditto xcconfig
      changes.
    * Source/WebCore/WebCore.xcodeproj/project.pbxproj: Ditto pbxproj
      changes.
    * Source/WebKit/Configurations/BaseTarget.xcconfig: Ditto xcconfig
      changes.
    * Source/WebKit/WebKit.xcodeproj/project.pbxproj: Ditto pbxproj changes.
    * Tools/Scripts/build-and-collect-pgo-profiles: Approximate the target
      triple we built for by analyzing the build products being tested. This
      is currently very Mac-specific, but this whole script woudl need to be
      changed dramatically to support collection on embedded platforms.
    
    Canonical link: https://commits.webkit.org/261593@main

--- Source/JavaScriptCore/Scripts/copy-profiling-data.sh
+++ Source/JavaScriptCore/Scripts/copy-profiling-data.sh
@@ -4,11 +4,53 @@ if [ -z "${PROFILE_DATA_FLAGS}" ]; then
     exit 0;
 fi
 
-input_profdata="${SCRIPT_INPUT_FILE_0}"
-input_profdata_type="$(file -b "${input_profdata}")"
-
+profiles="${SCRIPT_INPUT_FILE_0}"
+# The fallback profile (empty profdata) is used in open source builds, and in
+# engineering builds when the selected profile is in an unrecognized format
+# (i.e. git-lfs is not installed).
 fallback_decompressed_profdata="${SCRIPT_INPUT_FILE_1}"
 derived_decompressed_profdata="${SCRIPT_OUTPUT_FILE_0}"
+
+# Search for a PGO profile corresponding to the current target triple or a
+# previous OS version triple. In lieu of an exact match, fall back to the most
+# recent macOS profile.
+#
+# FIXME: Don't assume that profiles only exist for arm64e. This script runs as
+# a target build phase, so there is no CURRENT_ARCH, but it could process each
+# value of ARCHS.
+# FIXME: Support LLVM_TARGET_TRIPLE_SUFFIX to have separate profiles for
+# simulators and catalyst. We don't need this, but it is easy to add.
+
+function lte_os_version {
+    while read candidate_triple; do
+        printf '%s\n' ${candidate_triple#${profiles}/arm64e-apple-} ${LLVM_TARGET_TRIPLE_OS_VERSION} | sort -CV &&
+            echo ${candidate_triple}
+        done
+}
+
+pgo_target_triples=(
+    # SWIFT_PLATFORM_TARGET_PREFIX is LLVM_TARGET_TRIPLE_OS_VERSION minus the
+    # version number. If an exact match for this OS version is available, it
+    # will be chosen.
+    $(printf '%s\n' "${profiles}/arm64e-apple-${SWIFT_PLATFORM_TARGET_PREFIX}"* | sort -rV | lte_os_version)
+    # The latest macOS triple.
+    $(printf '%s\n' "${profiles}/arm64e-apple-macos"* | sort -rV | head -1)
+)
+
+for pgo_triple in "${pgo_target_triples[@]}"; do
+    [ -d "${pgo_triple}" ] && break
+done
+if [ $? -eq 0 ]; then
+    # Can't use TARGET_NAME to select which profdata to use, because this
+    # script may run as part of a "Derived Sources" target.
+    input_profdata="${pgo_triple}/${PROJECT_NAME}.profdata.compressed"
+elif [ "${USE_INTERNAL_SDK}" != YES ]; then
+    input_profdata="${fallback_decompressed_profdata}"
+else
+    echo "error: could not find a suitable profile in any of the following directories:" "${pgo_target_triples[@]}"
+    exit 1
+fi
+input_profdata_type="$(file -b "${input_profdata}")"
 
 if [[ "${input_profdata_type}" = "lzfse compressed"* ]]; then
     set -x; compression_tool -decode -i "${input_profdata}" -o "${derived_decompressed_profdata}" -a lzfse
