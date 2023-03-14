9d30cd47aaab9cda55030b7eb9ecbe8aef869564
Yusuke Suzuki <ysuzuki@apple.com>
2023-03-13 17:28:26 -0700

    Concat with a CSSStyleSheet and shadowRoot.adoptedStyleSheets returns array in array https://bugs.webkit.org/show_bug.cgi?id=253814 rdar://106667776
    Reviewed by Alexey Shvayka, Mark Lam, Keith Miller and Chris Dumez.
    
    We should bail out from the fast path of Array.prototype.concat when the parameter is Array but not JSArray.
    This is the case when the parameter is ProxyObject or DerivedArray e.g. JSObservableArray.
    This patch adds checks for both types in the fast path and returns null to go to the slow path.
    
    * LayoutTests/cssom/array-concat-adoptedStyleSheets-expected.txt: Added.
    * LayoutTests/cssom/array-concat-adoptedStyleSheets.html: Added.
    * Source/JavaScriptCore/runtime/ArrayPrototype.cpp:
    (JSC::concatAppendOne):
    (JSC::JSC_DEFINE_HOST_FUNCTION):
    
    Canonical link: https://commits.webkit.org/261604@main

--- Source/JavaScriptCore/runtime/ArrayPrototype.cpp
+++ Source/JavaScriptCore/runtime/ArrayPrototype.cpp
@@ -1449,7 +1449,7 @@ static bool moveElements(JSGlobalObject* globalObject, VM& vm, JSArray* target, 
     return true;
 }
 
-static EncodedJSValue concatAppendOne(JSGlobalObject* globalObject, VM& vm, JSArray* first, JSValue second)
+static JSValue concatAppendOne(JSGlobalObject* globalObject, VM& vm, JSArray* first, JSValue second)
 {
     auto scope = DECLARE_THROW_SCOPE(vm);
 
@@ -1462,7 +1462,15 @@ static EncodedJSValue concatAppendOne(JSGlobalObject* globalObject, VM& vm, JSAr
     checkedResultSize += 1;
     if (UNLIKELY(checkedResultSize.hasOverflowed())) {
         throwOutOfMemoryError(globalObject, scope);
-        return encodedJSValue();
+        return { };
+    }
+
+    // This code still can see concat-spreadable items. It is ProxyObject and DerivedArray.
+    // We should not use `isArray` check here since it is side-effectful for ProxyObject. Let's just bail out if it is ProxyObject or DerivedArray.
+    if (second.isObject()) {
+        JSType type = asObject(second)->type();
+        if (UNLIKELY(type == ProxyObjectType || type == DerivedArrayType))
+            return jsNull();
     }
 
     unsigned resultSize = checkedResultSize;
@@ -1475,24 +1483,23 @@ static EncodedJSValue concatAppendOne(JSGlobalObject* globalObject, VM& vm, JSAr
     JSArray* result = JSArray::tryCreate(vm, resultStructure, resultSize);
     if (UNLIKELY(!result)) {
         throwOutOfMemoryError(globalObject, scope);
-        return encodedJSValue();
+        return { };
     }
 
     bool success = result->appendMemcpy(globalObject, vm, 0, first);
     EXCEPTION_ASSERT(!scope.exception() || !success);
     if (!success) {
-        RETURN_IF_EXCEPTION(scope, encodedJSValue());
+        RETURN_IF_EXCEPTION(scope, { });
 
         bool success = moveElements(globalObject, vm, result, 0, first, firstArraySize);
         EXCEPTION_ASSERT(!scope.exception() == success);
         if (UNLIKELY(!success))
-            return encodedJSValue();
+            return { };
     }
 
     scope.release();
     result->putDirectIndex(globalObject, firstArraySize, second);
-    return JSValue::encode(result);
-
+    return result;
 }
 
 template<typename T>
@@ -1541,7 +1548,7 @@ JSC_DEFINE_HOST_FUNCTION(arrayProtoPrivateFuncConcatMemcpy, (JSGlobalObject* glo
 
     JSValue second = callFrame->uncheckedArgument(1);
     if (!isJSArray(second))
-        RELEASE_AND_RETURN(scope, concatAppendOne(globalObject, vm, firstArray, second));
+        RELEASE_AND_RETURN(scope, JSValue::encode(concatAppendOne(globalObject, vm, firstArray, second)));
 
     JSArray* secondArray = jsCast<JSArray*>(second);
     
