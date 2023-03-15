9d1936ad77d3cf5cf75f8acec942e6aa9402d54b
Philippe Normand <philn@igalia.com>
2023-03-15 05:42:22 -0700

    Cherry-pick 261684@main (8f08fa82eddb). https://bugs.webkit.org/show_bug.cgi?id=253000
        [GStreamer][EME] Added support to check support of cryptoblockformat
        https://bugs.webkit.org/show_bug.cgi?id=253000
    
        Reviewed by Xabier Rodriguez-Calvar.
    
        Inspired from downstream WPE 2.22 patch:
        https://github.com/WebPlatformForEmbedded/WPEWebKit/commit/e5b91c2bb7732e1047e3f52aa6f6651e0e1f02da
    
        Fixes YT cert test: "Format Support Tests" -> "1. isTypeSupported cryptoblockformat"
    
        Also drive-by change suggested by Xabier, remove the application/x-cbcs check in the Thunder
        decryptor as that is now cenc with a cypher-mode.
    
        * Source/WebCore/platform/graphics/gstreamer/GStreamerRegistryScanner.cpp:
        (WebCore::GStreamerRegistryScanner::isContentTypeSupported const):
    
        Canonical link: https://commits.webkit.org/261684@main

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -2694,6 +2694,8 @@ MediaPlayer::SupportsType MediaPlayerPrivateGStreamer::supportsType(const MediaE
     if (parameters.type.raw().startsWith("image"_s))
         return result;
 
+    registerWebKitGStreamerElements();
+
     auto& gstRegistryScanner = GStreamerRegistryScanner::singleton();
     result = gstRegistryScanner.isContentTypeSupported(GStreamerRegistryScanner::Configuration::Decoding, parameters.type, parameters.contentTypesRequiringHardwareSupport);
 
