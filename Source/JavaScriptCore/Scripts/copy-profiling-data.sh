99e4e950c68dd8b4fb780155b63caa3e0d70b41c
Elliott Williams <emw@apple.com>
2023-03-16 17:05:22 -0700

    [Xcode] Fix size increase due to multiple PGO profile support https://bugs.webkit.org/show_bug.cgi?id=253870 rdar://106673505
    Reviewed by Alexey Proskuryakov.
    
    The target triple selection logic in copy-profiling-data is moving to
    WebKitAdditions. Change the script on the open-source side to simply
    extract the profile that WebKitAdditions has built with.
    
    Rather than revert to the old configuration logic where we built our own
    OTHER_CFLAGS of PGO switches, use the settings that have been built-in to
    Xcode for years. AFAIK, we didn't know about these settings during the
    initial PGO work. As a result of this change, PGO profdata are correctly
    tracked as dependencies of the compile tasks that use them. We also save
    a bit of xcconfig logic.
    
    Additionally, adjust some use of GCC_PREPROCESSOR_DEFINITIONS in a
    drive-by cleanup. Feature-list variables like
    ENABLE_LLVM_PROFILE_GENERATION or ENABLE_PDFJS, which expand to their
    own name, can be used as -D flags like
    
        GCC_PREPROCESSOR_DEFINITIONS = $(ENABLE_PDFJS)
    
    instead of the more superfluous
    
        GCC_PREPROCESSOR_DEFINITIONS = GCC_PREPROCESSOR_DEFINITIONS_$(ENABLE_PDFJS)
        GCC_PREPROCESSOR_DEFINITIONS_ENABLE_PDFJS = ENABLE_PDFJS=1
    
    * Configurations/WebKitProjectPaths.xcconfig:
    * Source/JavaScriptCore/Configurations/JavaScriptCore.xcconfig:
    * Source/JavaScriptCore/JavaScriptCore.xcodeproj/project.pbxproj:
    * Source/JavaScriptCore/Scripts/copy-profiling-data.sh:
    * Source/WebCore/Configurations/WebCore.xcconfig:
    * Source/WebCore/Configurations/WebCoreTestSupport.xcconfig:
    * Source/WebCore/WebCore.xcodeproj/project.pbxproj:
    * Source/WebKit/Configurations/BaseTarget.xcconfig:
    * Source/WebKit/Configurations/MediaFormatReader.xcconfig:
    * Source/WebKit/Configurations/WebContentService.xcconfig:
    * Source/WebKit/Configurations/WebKitSwift.xcconfig:
    * Source/WebKit/Configurations/adattributiond.xcconfig:
    * Source/WebKit/Configurations/webpushd.xcconfig:
    * Source/WebKit/Configurations/webpushtool.xcconfig:
    * Source/WebKitLegacy/mac/Configurations/WebKitLegacy.xcconfig:
    
    Canonical link: https://commits.webkit.org/261778@main

--- Source/JavaScriptCore/Scripts/copy-profiling-data.sh
+++ Source/JavaScriptCore/Scripts/copy-profiling-data.sh
@@ -1,66 +1,18 @@
 #!/bin/sh
 
-if [ -z "${PROFILE_DATA_FLAGS}" ]; then
-    exit 0;
+# Decompresses and copies PGO profiles from WebKitAdditions to a derived folder.
+
+if [ "${CLANG_USE_OPTIMIZATION_PROFILE}" = YES ]; then
+    compression_tool -v -decode -i "${SCRIPT_INPUT_FILE_0}" -o "${SCRIPT_OUTPUT_FILE_0}" && exit
+    if [ "${CONFIGURATION}" = Production ]; then
+        echo "error: ${SCRIPT_INPUT_FILE_0} failed to extract"
+        exit 1
+    else
+        echo "warning: ${SCRIPT_INPUT_FILE_0} failed to extract, falling back to stub profile data"
+    fi
 fi
 
-profiles="${SCRIPT_INPUT_FILE_0}"
-# The fallback profile (empty profdata) is used in open source builds, and in
-# engineering builds when the selected profile is in an unrecognized format
-# (i.e. git-lfs is not installed).
-fallback_decompressed_profdata="${SCRIPT_INPUT_FILE_1}"
-derived_decompressed_profdata="${SCRIPT_OUTPUT_FILE_0}"
-
-# Search for a PGO profile corresponding to the current target triple or a
-# previous OS version triple. In lieu of an exact match, fall back to the most
-# recent macOS profile.
-#
-# FIXME: Don't assume that profiles only exist for arm64e. This script runs as
-# a target build phase, so there is no CURRENT_ARCH, but it could process each
-# value of ARCHS.
-# FIXME: Support LLVM_TARGET_TRIPLE_SUFFIX to have separate profiles for
-# simulators and catalyst. We don't need this, but it is easy to add.
-
-function lte_os_version {
-    while read candidate_triple; do
-        printf '%s\n' ${candidate_triple#${profiles}/arm64e-apple-} ${LLVM_TARGET_TRIPLE_OS_VERSION} | sort -CV &&
-            echo ${candidate_triple}
-        done
-}
-
-pgo_target_triples=(
-    # SWIFT_PLATFORM_TARGET_PREFIX is LLVM_TARGET_TRIPLE_OS_VERSION minus the
-    # version number. If an exact match for this OS version is available, it
-    # will be chosen.
-    $(printf '%s\n' "${profiles}/arm64e-apple-${SWIFT_PLATFORM_TARGET_PREFIX}"* | sort -rV | lte_os_version)
-    # The latest macOS triple.
-    $(printf '%s\n' "${profiles}/arm64e-apple-macos"* | sort -rV | head -1)
-)
-
-for pgo_triple in "${pgo_target_triples[@]}"; do
-    [ -d "${pgo_triple}" ] && break
-done
-if [ $? -eq 0 ]; then
-    # Can't use TARGET_NAME to select which profdata to use, because this
-    # script may run as part of a "Derived Sources" target.
-    input_profdata="${pgo_triple}/${PROJECT_NAME}.profdata.compressed"
-elif [ "${USE_INTERNAL_SDK}" != YES ]; then
-    input_profdata="${fallback_decompressed_profdata}"
-else
-    echo "error: could not find a suitable profile in any of the following directories:" "${pgo_target_triples[@]}"
-    exit 1
-fi
-input_profdata_type="$(file -b "${input_profdata}")"
-
-if [[ "${input_profdata_type}" = "lzfse compressed"* ]]; then
-    set -x; compression_tool -decode -i "${input_profdata}" -o "${derived_decompressed_profdata}" -a lzfse
-elif [[ "${input_profdata_type}" = "LLVM indexed profile data"* ]]; then
-    set -x; cp "${input_profdata}" "${derived_decompressed_profdata}"
-elif [ "${CONFIGURATION}" != Production ] && [ "${input_profdata}" != "${fallback_decompressed_profdata}" ]; then
-    echo "warning: unrecognized profiling data at ${input_profdata}, falling back to stub data"
-    set -x; cp "${fallback_decompressed_profdata}" "${derived_decompressed_profdata}"
-else
-    echo "error: unrecognized profiling data at ${input_profdata}"
-    exit 1
-fi
-
+# Copy stub data, even if CLANG_USE_OPTIMIZATION_PROFILE is off. The build
+# system needs something to be written to SCRIPT_INPUT_FILE_0 for it to skip
+# rerunning this task in subsequent builds.
+cp -vf "${SCRIPT_INPUT_FILE_1}" "${SCRIPT_OUTPUT_FILE_0}"
