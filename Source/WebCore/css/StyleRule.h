1274b2e8578441d52ca523154721d24ccd4f8335
Matthieu Dubet <m_dubet@apple.com>
2023-03-17 17:48:44 -0700

    Fix test crash because of missing copy() method https://bugs.webkit.org/show_bug.cgi?id=253723 rdar://106561354
    Reviewed by Brent Fulgham.
    
    The visitDerived() dispatcher was calling StyleRule::copy()
    because there was no implementation for StyleRuleWithNesting::copy().
    
    A follow-up patch could improve the static assertion in copy() to catch that
    at compile-time.
    
    * Source/WebCore/css/CSSStyleRule.cpp:
    (WebCore::CSSStyleRule::reattach):
    * Source/WebCore/css/StyleRule.cpp:
    (WebCore::StyleRuleWithNesting::copy const):
    (WebCore::StyleRuleWithNesting::StyleRuleWithNesting):
    (WebCore::m_originalSelectorList):
    * Source/WebCore/css/StyleRule.h:
    
    Canonical link: https://commits.webkit.org/261815@main

--- Source/WebCore/css/StyleRule.h
+++ Source/WebCore/css/StyleRule.h
@@ -158,10 +158,12 @@ class StyleRuleWithNesting final : public StyleRule {
     WTF_MAKE_STRUCT_FAST_ALLOCATED_WITH_HEAP_IDENTIFIER(StyleRuleWithNesting);
 public:
     static Ref<StyleRuleWithNesting> create(Ref<StyleProperties>&&, bool hasDocumentSecurityOrigin, CSSSelectorList&&, Vector<Ref<StyleRuleBase>>&& nestedRules);
+    Ref<StyleRuleWithNesting> copy() const;
 
     const Vector<Ref<StyleRuleBase>>& nestedRules() const { return m_nestedRules; }
     const CSSSelectorList& originalSelectorList() const { return m_originalSelectorList; }
-    StyleRuleWithNesting(const StyleRuleWithNesting&) = delete;
+    StyleRuleWithNesting(const StyleRuleWithNesting&);
+    ~StyleRuleWithNesting();
 
 private:
     StyleRuleWithNesting(Ref<StyleProperties>&&, bool hasDocumentSecurityOrigin, CSSSelectorList&&, Vector<Ref<StyleRuleBase>>&& nestedRules);
