ba39c324bac36828f7da01cba41fbca1e8e63fa6
Rose <83477269+AtariDreams@users.noreply.github.com>
2023-03-21 10:47:46 -0400

    Support constexpr std::count on non-Playstation builds https://bugs.webkit.org/show_bug.cgi?id=253157
    This does not compile on Playstation, so let us change the check
    to conditionally check for those at compile time.
    
    * Source/WebCore/PAL/pal/text/TextCodecSingleByte.cpp: Add preprocessor
      check against PlayStation before declaring size to be a const variable
      or constexpr.

--- Source/WebCore/PAL/pal/text/TextCodecSingleByte.cpp
+++ Source/WebCore/PAL/pal/text/TextCodecSingleByte.cpp
@@ -174,8 +174,13 @@ static constexpr SingleByteDecodeTable ibm866 {
 template<const SingleByteDecodeTable& decodeTable> SingleByteEncodeTable tableForEncoding()
 {
     // Allocate this at runtime because building it at compile time would make the binary much larger and this is often not used.
-    // FIXME: With the C++20 version of std::count, we should be able to change this from const to constexpr and compute the size at compile time.
+    // FIXME: Remove this check after Playstation's standard lib supports this or is dropped
+#if !PLATFORM(PLAYSTATION)
+    static constexpr auto size = std::size(decodeTable) - std::count(std::begin(decodeTable), std::end(decodeTable), replacementCharacter);
+#else
     static const auto size = std::size(decodeTable) - std::count(std::begin(decodeTable), std::end(decodeTable), replacementCharacter);
+#endif
+
     static const SingleByteEncodeTableEntry* entries;
     static std::once_flag once;
     std::call_once(once, [&] {
