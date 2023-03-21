17be066c97aff118b7793e8f1ee0e8575c7a1fe1
Jonathan Bedard <jbedard@apple.com>
2023-03-21 08:05:46 -0700

    [reporelaypy] Invoke command after cloning https://bugs.webkit.org/show_bug.cgi?id=253989 rdar://106778935
    Reviewed by Dewei Zhu.
    
    * Tools/Scripts/libraries/reporelaypy/reporelaypy/__init__.py: Bump version.
    * Tools/Scripts/libraries/reporelaypy/setup.py: Ditto.
    * Tools/Scripts/libraries/reporelaypy/reporelaypy/checkout.py:
    (Checkout.clone): Treat post_clone_commands as a list of run() kwargs and invoke each of them.
    (Checkout.__init__): Pass post_clone_commands list to clone function.
    
    Canonical link: https://commits.webkit.org/261919@main

--- Tools/Scripts/libraries/reporelaypy/reporelaypy/checkout.py
+++ Tools/Scripts/libraries/reporelaypy/reporelaypy/checkout.py
@@ -65,7 +65,7 @@ class Checkout(object):
         return cls(**data, primary=False)
 
     @staticmethod
-    def clone(url, path, remotes, credentials, sentinal_file=None, checkout_data=None, disable_origin=True):
+    def clone(url, path, remotes, credentials, sentinal_file=None, checkout_data=None, disable_origin=True, post_clone_commands=None):
         run([local.Git.executable(), 'clone', url, path], cwd=os.path.dirname(path))
         run([local.Git.executable(), 'config', 'pull.ff', 'only'], cwd=path)
         if disable_origin:
@@ -73,6 +73,9 @@ class Checkout(object):
 
         Checkout.add_remotes(local.Git(path), remotes)
         Checkout.add_credentials(local.Git(path), credentials)
+
+        for command in post_clone_commands or []:
+            run(**command)
 
         if sentinal_file:
             with open(sentinal_file, 'w') as cloned:
@@ -116,6 +119,7 @@ class Checkout(object):
         sentinal=True, fallback_url=None, primary=True,
         remotes=None, credentials=None,
         forwarding=None, disable_origin=True,
+        post_clone_commands=None,
     ):
         self.sentinal = sentinal
         self.path = path
@@ -171,11 +175,11 @@ class Checkout(object):
         if self.sentinal:
             self._child_process = multiprocessing.Process(
                 target=self.clone,
-                args=(self.url, path, self.remotes, self.credentials, self.sentinal_file, checkout_data, disable_origin),
+                args=(self.url, path, self.remotes, self.credentials, self.sentinal_file, checkout_data, disable_origin, post_clone_commands),
             )
             self._child_process.start()
         else:
-            self.clone(self.url, path, self.remotes, self.credentials, checkout_data=checkout_data, disable_origin=disable_origin)
+            self.clone(self.url, path, self.remotes, self.credentials, checkout_data=checkout_data, disable_origin=disable_origin, post_clone_commands=post_clone_commands)
 
     @property
     def sentinal_file(self):
