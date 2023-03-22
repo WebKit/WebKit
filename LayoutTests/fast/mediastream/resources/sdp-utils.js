48f79a006c6eee0497bf7deaeba7d6bf3a6b1f22
Philippe Normand <philn@igalia.com>
2023-03-22 02:00:42 -0700

    Cherry-pick 261948@main (4433b2e3d9c8). https://bugs.webkit.org/show_bug.cgi?id=254164
        [GStreamer][WebRTC] fast/mediastream/RTCPeerConnection-inspect-{offer,answer}.html fail
        https://bugs.webkit.org/show_bug.cgi?id=254164
    
        Reviewed by Xabier Rodriguez-Calvar.
    
        Make fingerprint SDP attributes predictable in fast/mediastream/RTCPeerConnection-inspect tests baselines.
    
        * LayoutTests/fast/mediastream/resources/sdp-utils.js:
        (printComparableSessionDescription):
        * LayoutTests/platform/glib/TestExpectations:
        * LayoutTests/platform/glib/fast/mediastream/RTCPeerConnection-inspect-answer-expected.txt: Added.
        * LayoutTests/platform/glib/fast/mediastream/RTCPeerConnection-inspect-offer-expected.txt: Added.
    
        Canonical link: https://commits.webkit.org/261948@main

--- LayoutTests/fast/mediastream/resources/sdp-utils.js
+++ LayoutTests/fast/mediastream/resources/sdp-utils.js
@@ -1,6 +1,6 @@
 (function (globalObject) {
     // Variable fields (e.g. generated ids) are verified and replaced with predictable
-    // lables. The result can be compared with a predefined expected output.
+    // labels. The result can be compared with a predefined expected output.
     function printComparableSessionDescription(sessionDescription, mdescVariables) {
         debug("=== RTCSessionDescription ===");
         debug("type: " + sessionDescription.type + ", sdp:");
@@ -17,6 +17,7 @@
             "iceufrag": "^a=ice-ufrag:([\\w+/]*).*$",
             "icepwd": "^a=ice-pwd:([\\w+/]*).*$",
             "bundle": "^a=group:BUNDLE .*$",
+            "fingerprint": "^a=fingerprint:sha-256 ([a-zA-Z0-9:]*).*$"
         };
 
         var mdescIndex = -1;
@@ -79,6 +80,8 @@
                     line = line.replace(iceufrag[1], verified("ice-ufrag"));
                 else if (icepwd = match(line, regexp.icepwd))
                     line = line.replace(icepwd[1], verified("ice-password"));
+                else if (fingerprint = match(line, regexp.fingerprint))
+                    line = line.replace(fingerprint[1], verified("fingerprint"));
             }
 
             if (line)
