ef3f9d54a355c4cca7532546c678b2f4f619f919
Myah Cobbs <mcobbs@apple.com>
2023-03-31 13:04:08 -0700

    Revert "Apply patch. rdar://problem/106142457"
    This reverts commit 7a49b97bd9e59bfd547d8ae8ef135d6665216f17.
    
    Identifier: 259548.394@safari-7615.1.26.11-branch

--- Source/WebCore/platform/mediastream/mac/AVVideoCaptureSource.h
+++ Source/WebCore/platform/mediastream/mac/AVVideoCaptureSource.h
@@ -119,9 +119,6 @@ private:
     void updateVerifyCapturingTimer();
     void verifyIsCapturing();
 
-    std::optional<double> computeMinZoom() const;
-    std::optional<double> computeMaxZoom(AVCaptureDeviceFormat*) const;
-
     RefPtr<VideoFrame> m_buffer;
     RetainPtr<AVCaptureVideoDataOutput> m_videoOutput;
     std::unique_ptr<ImageTransferSessionVT> m_imageTransferSession;
@@ -141,8 +138,6 @@ private:
     RetainPtr<AVFrameRateRange> m_appliedFrameRateRange;
 
     double m_currentFrameRate;
-    double m_currentZoom { 1 };
-    double m_zoomScaleFactor { 1 };
     bool m_interrupted { false };
     bool m_isRunning { false };
 
