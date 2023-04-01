c71b43dd393b72160fd23df582432f678a04a07a
Myles C. Maxfield <mmaxfield@apple.com>
2023-03-31 17:14:48 -0700

    Cherry-pick 262455@main (8778787cbaef). https://bugs.webkit.org/show_bug.cgi?id=254780
        image/jxl isn't added to the Accept: header when USE(JPEGXL) is enabled
        https://bugs.webkit.org/show_bug.cgi?id=254780
        rdar://107446115
    
        Reviewed by Said Abou-Hallawa.
    
        Just like AVIF, if an image format is enabled, it should be advertised in the Accept: header.
    
        Because every port, AFAIK, has USE(JPEGXL) disabled, I didn't add an automated test for this.
    
        * Source/WebCore/loader/cache/CachedResourceRequest.cpp:
        (WebCore::acceptHeaderValueForJPEGXLImageResource):
        (WebCore::acceptHeaderValueForImageResource):
    
        Canonical link: https://commits.webkit.org/262455@main

--- Source/WebCore/loader/cache/CachedResourceRequest.cpp
+++ Source/WebCore/loader/cache/CachedResourceRequest.cpp
@@ -143,6 +143,15 @@ static constexpr ASCIILiteral acceptHeaderValueForAVIFImageResource()
 #endif
 }
 
+static constexpr ASCIILiteral acceptHeaderValueForJPEGXLImageResource()
+{
+#if USE(JPEGXL)
+    return "image/jxl,"_s;
+#else
+    return ""_s;
+#endif
+}
+
 static String acceptHeaderValueForAdditionalSupportedImageMIMETypes()
 {
     StringBuilder sb;
@@ -162,6 +171,7 @@ static String acceptHeaderValueForImageResource()
 {
     return String(acceptHeaderValueForWebPImageResource())
         + acceptHeaderValueForAVIFImageResource()
+        + acceptHeaderValueForJPEGXLImageResource()
         + acceptHeaderValueForAdditionalSupportedImageMIMETypes()
         + acceptHeaderValueForVideoImageResource(ImageDecoder::supportsMediaType(ImageDecoder::MediaType::Video))
         + "image/png,image/svg+xml,image/*;q=0.8,*/*;q=0.5"_s;
