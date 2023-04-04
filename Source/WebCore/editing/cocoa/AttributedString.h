WebKit/WebKit

commit 391c01e102feba390f1bef16336ae485ab4022bf

Begin hardening NSAttributedString deserialization https://bugs.webkit.org/show_bug.cgi?id=254250 rdar://107033670
Reviewed by Tim Hatcher.

We get NSAttributedStrings in the web process to send to the UI process from two
different places: WKPDFLayerControllerDelegate and HTMLConverter.  After manually
looking through the code that generates those attributes, I created a structure
that more closely reflects the possible values that we might want to decode,
with more hardening possible in the future to harden the deserialization of each
value type.

A previous version of this PR broke the Catalyst build and returned an empty
NSAttributedString and NSDictionary instead of nil after serialization.  This
version fixes both issues.

* Source/WebCore/SourcesCocoa.txt:
* Source/WebCore/WebCore.xcodeproj/project.pbxproj:
* Source/WebCore/editing/cocoa/AttributedString.h:
* Source/WebCore/editing/cocoa/AttributedString.mm: Added.
(WebCore::AttributedString::rangesAreSafe):
(WebCore::toNSObject):
(WebCore::toNSDictionary):
(WebCore::AttributedString::documentAttributesAsNSDictionary const):
(WebCore::AttributedString::nsAttributedString const):
(WebCore::extractValue):
(WebCore::extractDictionary):
(WebCore::AttributedString::fromNSAttributedString):
(WebCore::AttributedString::fromNSAttributedStringAndDocumentAttributes):
* Source/WebCore/editing/cocoa/EditorCocoa.mm:
(WebCore::selectionAsAttributedString):
* Source/WebCore/editing/cocoa/HTMLConverter.mm:
(HTMLConverter::convert):
(WebCore::editingAttributedString):
* Source/WebCore/editing/mac/EditorMac.mm:
(WebCore::Editor::dataSelectionForPasteboard):
* Source/WebKit/Shared/Cocoa/ArgumentCodersCocoa.mm:
(IPC::decodeSecureCodingInternal):
* Source/WebKit/Shared/Cocoa/WebCoreArgumentCodersCocoa.serialization.in:
* Source/WebKit/Shared/DocumentEditingContext.mm:
(WebKit::DocumentEditingContext::toPlatformContext):
* Source/WebKit/UIProcess/API/Cocoa/WKWebView.mm:

Source/WebCore/editing/cocoa/AttributedString.h
@@ -26,15 +26,58 @@
 #pragma once
 
 #import <wtf/RetainPtr.h>
+#import <wtf/URL.h>
+#import <wtf/Vector.h>
 
 OBJC_CLASS NSAttributedString;
+OBJC_CLASS NSDate;
 OBJC_CLASS NSDictionary;
+OBJC_CLASS NSParagraphStyle;
+OBJC_CLASS NSShadow;
+OBJC_CLASS NSTextAttachment;
+#if PLATFORM(MAC)
+OBJC_CLASS NSColor;
+OBJC_CLASS NSFont;
+#else
+OBJC_CLASS UIColor;
+OBJC_CLASS UIFont;
+#endif
 
 namespace WebCore {
 
 struct AttributedString {
-    RetainPtr<NSAttributedString> string;
-    RetainPtr<NSDictionary> documentAttributes;
+    struct Range {
+        size_t location { 0 };
+        size_t length { 0 };
+    };
+    struct AttributeValue {
+        std::variant<
+            double,
+            String,
+            URL,
+            Vector<String>,
+            Vector<double>,
+            RetainPtr<NSParagraphStyle>,
+            RetainPtr<NSTextAttachment>,
+            RetainPtr<NSShadow>,
+            RetainPtr<NSDate>,
+#if PLATFORM(MAC)
+            RetainPtr<NSFont>, RetainPtr<NSColor>
+#else
+            RetainPtr<UIFont>, RetainPtr<UIColor>
+#endif
+        > value;
+    };
+
+    String string;
+    Vector<std::pair<Range, HashMap<String, AttributeValue>>> attributes;
+    std::optional<HashMap<String, AttributeValue>> documentAttributes;
+
+    WEBCORE_EXPORT static AttributedString fromNSAttributedStringAndDocumentAttributes(RetainPtr<NSAttributedString>&&, RetainPtr<NSDictionary>&& documentAttributes);
+    WEBCORE_EXPORT static AttributedString fromNSAttributedString(RetainPtr<NSAttributedString>&&);
+    WEBCORE_EXPORT static bool rangesAreSafe(const String&, const Vector<std::pair<Range, HashMap<String, AttributeValue>>>&);
+    WEBCORE_EXPORT RetainPtr<NSDictionary> documentAttributesAsNSDictionary() const;
+    WEBCORE_EXPORT RetainPtr<NSAttributedString> nsAttributedString() const;
 };
 
 } // namespace WebCore

commit 493b7f1a6f9d8f78fbd136ce221eb7c14b21d4d0

Serialize NSAttributedString font attributes as WebCore::Font https://bugs.webkit.org/show_bug.cgi?id=254860 rdar://107503178
Reviewed by Myles Maxfield.

This also fixes a regression from 262192@main where if you make an NSAttributedString
from an HTML string that contains a web font as a data URL, we now get an attribute
with the key of NSFontAttributeName again.

* Source/WebCore/editing/cocoa/AttributedString.h:
* Source/WebCore/editing/cocoa/AttributedString.mm:
(WebCore::toNSObject):
(WebCore::extractValue):
* Source/WebKit/Shared/Cocoa/WebCoreArgumentCodersCocoa.serialization.in:

Canonical link: https://commits.webkit.org/262556@main

Source/WebCore/editing/cocoa/AttributedString.h
@@ -29,21 +29,47 @@
 #import <wtf/URL.h>
 #import <wtf/Vector.h>
 
+#if PLATFORM(MAC)
+#define PlatformColor               NSColor
+#define PlatformColorClass          NSColor.class
+#define PlatformFont                NSFont
+#define PlatformFontClass           NSFont.class
+#define PlatformImageClass          NSImage
+#define PlatformNSColorClass        NSColor
+#define PlatformNSParagraphStyle    NSParagraphStyle.class
+#define PlatformNSShadow            NSShadow.class
+#define PlatformNSTextAttachment    NSTextAttachment.class
+#define PlatformNSTextList          NSTextList
+#define PlatformNSTextTab           NSTextTab
+#define PlatformNSTextTable         NSTextTable
+#define PlatformNSTextTableBlock    NSTextTableBlock
+#else
+#define PlatformColor               UIColor
+#define PlatformColorClass          PAL::getUIColorClass()
+#define PlatformFont                UIFont
+#define PlatformFontClass           PAL::getUIFontClass()
+#define PlatformImageClass          PAL::getUIImageClass()
+#define PlatformNSColorClass        getNSColorClass()
+#define PlatformNSParagraphStyle    PAL::getNSParagraphStyleClass()
+#define PlatformNSShadow            PAL::getNSShadowClass()
+#define PlatformNSTextAttachment    getNSTextAttachmentClass()
+#define PlatformNSTextList          getNSTextListClass()
+#define PlatformNSTextTab           getNSTextTabClass()
+#define PlatformNSTextTable         getNSTextTableClass()
+#define PlatformNSTextTableBlock    getNSTextTableBlockClass()
+#endif
+
 OBJC_CLASS NSAttributedString;
 OBJC_CLASS NSDate;
 OBJC_CLASS NSDictionary;
 OBJC_CLASS NSParagraphStyle;
 OBJC_CLASS NSShadow;
 OBJC_CLASS NSTextAttachment;
-#if PLATFORM(MAC)
-OBJC_CLASS NSColor;
-OBJC_CLASS NSFont;
-#else
-OBJC_CLASS UIColor;
-OBJC_CLASS UIFont;
-#endif
+OBJC_CLASS PlatformColor;
 
 namespace WebCore {
+
+class Font;
 
 struct AttributedString {
     struct Range {
@@ -55,17 +81,14 @@ struct AttributedString {
             double,
             String,
             URL,
+            Ref<Font>,
             Vector<String>,
             Vector<double>,
             RetainPtr<NSParagraphStyle>,
             RetainPtr<NSTextAttachment>,
             RetainPtr<NSShadow>,
             RetainPtr<NSDate>,
-#if PLATFORM(MAC)
-            RetainPtr<NSFont>, RetainPtr<NSColor>
-#else
-            RetainPtr<UIFont>, RetainPtr<UIColor>
-#endif
+            RetainPtr<PlatformColor>
         > value;
     };
 
@@ -73,6 +96,7 @@ struct AttributedString {
     Vector<std::pair<Range, HashMap<String, AttributeValue>>> attributes;
     std::optional<HashMap<String, AttributeValue>> documentAttributes;
 
+    WEBCORE_EXPORT ~AttributedString();
     WEBCORE_EXPORT static AttributedString fromNSAttributedStringAndDocumentAttributes(RetainPtr<NSAttributedString>&&, RetainPtr<NSDictionary>&& documentAttributes);
     WEBCORE_EXPORT static AttributedString fromNSAttributedString(RetainPtr<NSAttributedString>&&);
     WEBCORE_EXPORT static bool rangesAreSafe(const String&, const Vector<std::pair<Range, HashMap<String, AttributeValue>>>&);

Source/WebCore/editing/cocoa/AttributedString.mm
@@ -26,6 +26,7 @@
 #import "config.h"
 #import "AttributedString.h"
 
+#import "Font.h"
 #import "Logging.h"
 #import <Foundation/Foundation.h>
 #import <wtf/cocoa/VectorCocoa.h>
@@ -37,6 +38,8 @@
 #endif
 
 namespace WebCore {
+
+AttributedString::~AttributedString() = default;
 
 bool AttributedString::rangesAreSafe(const String& string, const Vector<std::pair<Range, HashMap<String, AttributeValue>>>& vector)
 {
@@ -75,17 +78,10 @@ static RetainPtr<NSObject> toNSObject(const AttributedString::AttributeValue& va
         return value;
     }, [] (const RetainPtr<NSDate>& value) -> RetainPtr<NSObject> {
         return value;
-#if PLATFORM(MAC)
-    }, [] (const RetainPtr<NSFont>& value) -> RetainPtr<NSObject> {
+    }, [] (const Ref<Font>& font) -> RetainPtr<NSObject> {
+        return (__bridge PlatformFont *)(font->getCTFont());
+    }, [] (const RetainPtr<PlatformColor>& value) -> RetainPtr<NSObject> {
         return value;
-    }, [] (const RetainPtr<NSColor>& value) -> RetainPtr<NSObject> {
-        return value;
-#else
-    }, [] (const RetainPtr<UIFont>& value) -> RetainPtr<NSObject> {
-        return value;
-    }, [] (const RetainPtr<UIColor>& value) -> RetainPtr<NSObject> {
-        return value;
-#endif
     });
 }
 
@@ -164,29 +160,16 @@ static std::optional<AttributedString::AttributeValue> extractValue(id value)
         return extractArray(array);
     if (auto* date = dynamic_objc_cast<NSDate>(value))
         return { { { RetainPtr { date } } } };
-#if PLATFORM(MAC)
-    if (auto* shadow = dynamic_objc_cast<NSShadow>(value))
-        return { { { RetainPtr { shadow } } } };
-    if (auto* paragraphStyle = dynamic_objc_cast<NSParagraphStyle>(value))
-        return { { { RetainPtr { paragraphStyle } } } };
-    if (auto* textAttachment = dynamic_objc_cast<NSTextAttachment>(value))
-        return { { { RetainPtr { textAttachment } } } };
-    if (auto* font = dynamic_objc_cast<NSFont>(value))
-        return { { { RetainPtr { font } } } };
-    if (auto* color = dynamic_objc_cast<NSColor>(value))
-        return { { { RetainPtr { color } } } };
-#else
-    if ([value isKindOfClass:WebCore::getNSShadowClass()])
+    if ([value isKindOfClass:PlatformNSShadow])
         return { { { RetainPtr { (NSShadow *)value } } } };
-    if ([value isKindOfClass:PAL::getNSParagraphStyleClass()])
+    if ([value isKindOfClass:PlatformNSParagraphStyle])
         return { { { RetainPtr { (NSParagraphStyle *)value } } } };
-    if ([value isKindOfClass:WebCore::getNSTextAttachmentClass()])
+    if ([value isKindOfClass:PlatformNSTextAttachment])
         return { { { RetainPtr { (NSTextAttachment *)value } } } };
-    if ([value isKindOfClass:PAL::getUIFontClass()])
-        return { { { RetainPtr { (UIFont *)value } } } };
-    if ([value isKindOfClass:PAL::getUIColorClass()])
-        return { { { RetainPtr { (UIColor *)value } } } };
-#endif
+    if ([value isKindOfClass:PlatformFontClass])
+        return { { { Font::create(FontPlatformData((__bridge CTFontRef)value, [(PlatformFont *)value pointSize])) } } };
+    if ([value isKindOfClass:PlatformColorClass])
+        return { { { RetainPtr { (PlatformColor *)value } } } };
     if (value) {
         RELEASE_LOG_ERROR(Editing, "NSAttributedString extraction failed for class <%@>", NSStringFromClass([value class]));
         ASSERT_NOT_REACHED();

Source/WebCore/editing/cocoa/HTMLConverter.mm
@@ -77,42 +77,10 @@
 #endif
 
 #if PLATFORM(IOS_FAMILY)
-
 #import "UIFoundationSoftLink.h"
 #import "WAKAppKitStubs.h"
 #import <pal/ios/UIKitSoftLink.h>
 #import <pal/spi/ios/UIKitSPI.h>
-
-#define PlatformNSShadow            getNSShadowClass()
-#define PlatformNSTextAttachment    getNSTextAttachmentClass()
-#define PlatformNSParagraphStyle    getNSParagraphStyleClass()
-#define PlatformNSTextList          getNSTextListClass()
-#define PlatformNSTextTableBlock    getNSTextTableBlockClass()
-#define PlatformNSTextTable         getNSTextTableClass()
-#define PlatformNSTextTab           getNSTextTabClass()
-#define PlatformColor               UIColor
-#define PlatformColorClass          PAL::getUIColorClass()
-#define PlatformNSColorClass        getNSColorClass()
-#define PlatformFont                UIFont
-#define PlatformFontClass           PAL::getUIFontClass()
-#define PlatformImageClass          PAL::getUIImageClass()
-
-#else
-
-#define PlatformNSShadow            NSShadow
-#define PlatformNSTextAttachment    NSTextAttachment
-#define PlatformNSParagraphStyle    NSParagraphStyle
-#define PlatformNSTextList          NSTextList
-#define PlatformNSTextTableBlock    NSTextTableBlock
-#define PlatformNSTextTable         NSTextTable
-#define PlatformNSTextTab           NSTextTab
-#define PlatformColor               NSColor
-#define PlatformColorClass          NSColor
-#define PlatformNSColorClass        NSColor
-#define PlatformFont                NSFont
-#define PlatformFontClass           NSFont
-#define PlatformImageClass          NSImage
-
 #endif
 
 using namespace WebCore;
@@ -2387,7 +2355,7 @@ AttributedString editingAttributedString(const SimpleRange& range, IncludeImages
         if (style.textDecorationsInEffect() & TextDecorationLine::LineThrough)
             [attrs setObject:[NSNumber numberWithInteger:NSUnderlineStyleSingle] forKey:NSStrikethroughStyleAttributeName];
         if (auto font = style.fontCascade().primaryFont().getCTFont())
-            [attrs setObject:toNSFont(font) forKey:NSFontAttributeName];
+            [attrs setObject:(__bridge NSFont *)font forKey:NSFontAttributeName];
         else
             [attrs setObject:[fontManager convertFont:WebDefaultFont() toSize:style.fontCascade().primaryFont().platformData().size()] forKey:NSFontAttributeName];
 

Source/WebCore/platform/graphics/FontPlatformData.h
@@ -327,21 +327,7 @@ private:
 
 #if USE(CORE_TEXT)
 bool isSystemFont(CTFontRef);
-#endif
-
-#if USE(APPKIT) && defined(__OBJC__)
-
-// NSFonts and CTFontRefs are toll-free-bridged.
-inline CTFontRef toCTFont(NSFont *font)
-{
-    return (__bridge CTFontRef)font;
-}
-
-inline NSFont *toNSFont(CTFontRef font)
-{
-    return (__bridge NSFont *)font;
-}
-
+WEBCORE_EXPORT RetainPtr<CTFontRef> createCTFont(CFDictionaryRef attributes, float size, CTFontDescriptorOptions, CFStringRef referenceURL, CFStringRef desiredPostScriptName);
 #endif
 
 #if USE(CG)

Source/WebCore/platform/graphics/coretext/FontPlatformDataCoreText.cpp
@@ -68,7 +68,7 @@ static RetainPtr<CTFontDescriptorRef> findFontDescriptor(CFStringRef referenceUR
     return nullptr;
 }
 
-static RetainPtr<CTFontRef> createCTFont(CFDictionaryRef attributes, float size, CTFontDescriptorOptions options, CFStringRef referenceURL, CFStringRef desiredPostScriptName)
+RetainPtr<CTFontRef> createCTFont(CFDictionaryRef attributes, float size, CTFontDescriptorOptions options, CFStringRef referenceURL, CFStringRef desiredPostScriptName)
 {
     auto fontDescriptor = adoptCF(CTFontDescriptorCreateWithAttributes(attributes));
     if (fontDescriptor) {

Source/WebCore/platform/ios/UIFoundationSoftLink.h
@@ -33,10 +33,8 @@
 SOFT_LINK_FRAMEWORK_FOR_HEADER(WebCore, UIFoundation)
 
 SOFT_LINK_CLASS_FOR_HEADER(WebCore, NSColor)
-SOFT_LINK_CLASS_FOR_HEADER(WebCore, NSShadow)
 SOFT_LINK_CLASS_FOR_HEADER(WebCore, NSTextAttachment)
 SOFT_LINK_CLASS_FOR_HEADER(WebCore, NSMutableParagraphStyle)
-SOFT_LINK_CLASS_FOR_HEADER(WebCore, NSParagraphStyle)
 SOFT_LINK_CLASS_FOR_HEADER(WebCore, NSTextList)
 SOFT_LINK_CLASS_FOR_HEADER(WebCore, NSTextBlock)
 SOFT_LINK_CLASS_FOR_HEADER(WebCore, NSTextTableBlock)

Source/WebCore/platform/ios/UIFoundationSoftLink.mm
@@ -34,10 +34,8 @@
 SOFT_LINK_PRIVATE_FRAMEWORK_FOR_SOURCE(WebCore, UIFoundation)
 
 SOFT_LINK_CLASS_FOR_SOURCE(WebCore, UIFoundation, NSColor)
-SOFT_LINK_CLASS_FOR_SOURCE(WebCore, UIFoundation, NSShadow)
 SOFT_LINK_CLASS_FOR_SOURCE(WebCore, UIFoundation, NSTextAttachment)
 SOFT_LINK_CLASS_FOR_SOURCE(WebCore, UIFoundation, NSMutableParagraphStyle)
-SOFT_LINK_CLASS_FOR_SOURCE(WebCore, UIFoundation, NSParagraphStyle)
 SOFT_LINK_CLASS_FOR_SOURCE(WebCore, UIFoundation, NSTextList)
 SOFT_LINK_CLASS_FOR_SOURCE(WebCore, UIFoundation, NSTextBlock)
 SOFT_LINK_CLASS_FOR_SOURCE(WebCore, UIFoundation, NSTextTableBlock)

Source/WebKit/Shared/Cocoa/WebCoreArgumentCodersCocoa.mm
@@ -477,47 +477,6 @@ void ArgumentCoder<WebCore::Font>::encodePlatformData(Encoder& encoder, const We
     }
 }
 
-static RetainPtr<CTFontDescriptorRef> findFontDescriptor(CFStringRef referenceURL, CFStringRef postScriptName)
-{
-    auto url = adoptCF(CFURLCreateWithString(kCFAllocatorDefault, referenceURL, nullptr));
-    if (!url)
-        return nullptr;
-    auto fontDescriptors = adoptCF(CTFontManagerCreateFontDescriptorsFromURL(url.get()));
-    if (!fontDescriptors || !CFArrayGetCount(fontDescriptors.get()))
-        return nullptr;
-    if (CFArrayGetCount(fontDescriptors.get()) == 1)
-        return static_cast<CTFontDescriptorRef>(CFArrayGetValueAtIndex(fontDescriptors.get(), 0));
-
-    for (CFIndex i = 0; i < CFArrayGetCount(fontDescriptors.get()); ++i) {
-        auto fontDescriptor = static_cast<CTFontDescriptorRef>(CFArrayGetValueAtIndex(fontDescriptors.get(), i));
-        auto currentPostScriptName = adoptCF(CTFontDescriptorCopyAttribute(fontDescriptor, kCTFontNameAttribute));
-        if (CFEqual(currentPostScriptName.get(), postScriptName))
-            return fontDescriptor;
-    }
-    return nullptr;
-}
-
-static RetainPtr<CTFontRef> createCTFont(CFDictionaryRef attributes, float size, CTFontDescriptorOptions options, CFStringRef referenceURL, CFStringRef desiredPostScriptName)
-{
-    auto fontDescriptor = adoptCF(CTFontDescriptorCreateWithAttributes(attributes));
-    if (fontDescriptor) {
-        auto font = adoptCF(CTFontCreateWithFontDescriptorAndOptions(fontDescriptor.get(), size, nullptr, options));
-        auto actualPostScriptName = adoptCF(CTFontCopyPostScriptName(font.get()));
-        if (CFEqual(actualPostScriptName.get(), desiredPostScriptName))
-            return font;
-    }
-
-    // CoreText couldn't round-trip the font.
-    // We can fall back to doing our best to find it ourself.
-    fontDescriptor = findFontDescriptor(referenceURL, desiredPostScriptName);
-    if (!fontDescriptor) {
-        ASSERT_NOT_REACHED();
-        fontDescriptor = adoptCF(CTFontDescriptorCreateLastResort());
-    }
-    ASSERT(fontDescriptor);
-    return adoptCF(CTFontCreateWithFontDescriptorAndOptions(fontDescriptor.get(), size, nullptr, options));
-}
-
 std::optional<WebCore::FontPlatformData> ArgumentCoder<WebCore::Font>::decodePlatformData(Decoder& decoder)
 {
     std::optional<WebCore::FontOrientation> orientation;
@@ -612,7 +571,7 @@ std::optional<WebCore::FontPlatformData> ArgumentCoder<WebCore::Font>::decodePla
     if (!postScriptName || !*postScriptName)
         return std::nullopt;
 
-    auto ctFont = createCTFont(attributes->get(), *size, *options, referenceURL->get(), postScriptName->get());
+    auto ctFont = WebCore::createCTFont(attributes->get(), *size, *options, referenceURL->get(), postScriptName->get());
     if (!ctFont)
         return std::nullopt;
 

Source/WebKit/Shared/Cocoa/WebCoreArgumentCodersCocoa.serialization.in
@@ -50,10 +50,10 @@ header: <WebCore/ResourceRequest.h>
 
 [Nested] struct WebCore::AttributedString::AttributeValue {
 #if PLATFORM(MAC)
-    std::variant<double, String, URL, Vector<String>, Vector<double>, RetainPtr<NSParagraphStyle>, RetainPtr<NSTextAttachment>, RetainPtr<NSShadow>, RetainPtr<NSDate>, RetainPtr<NSFont>, RetainPtr<NSColor>> value;
+    std::variant<double, String, URL, Ref<WebCore::Font>, Vector<String>, Vector<double>, RetainPtr<NSParagraphStyle>, RetainPtr<NSTextAttachment>, RetainPtr<NSShadow>, RetainPtr<NSDate>, RetainPtr<NSColor>> value;
 #endif
 #if PLATFORM(IOS_FAMILY)
-    std::variant<double, String, URL, Vector<String>, Vector<double>, RetainPtr<NSParagraphStyle>, RetainPtr<NSTextAttachment>, RetainPtr<NSShadow>, RetainPtr<NSDate>, RetainPtr<UIFont>, RetainPtr<UIColor>> value;
+    std::variant<double, String, URL, Ref<WebCore::Font>, Vector<String>, Vector<double>, RetainPtr<NSParagraphStyle>, RetainPtr<NSTextAttachment>, RetainPtr<NSShadow>, RetainPtr<NSDate>, RetainPtr<UIColor>> value;
 #endif
 }
 

Source/WebKitLegacy/mac/WebCoreSupport/PopupMenuMac.mm
@@ -88,7 +88,7 @@ void PopupMenuMac::populate()
                 CGFloat size = style.font().primaryFont().platformData().size();
                 font = adoptCF(CTFontCreateUIFontForLanguage(isFontWeightBold(style.font().weight()) ? kCTFontUIFontEmphasizedSystem : kCTFontUIFontSystem, size, nullptr));
             }
-            [attributes setObject:toNSFont(font.get()) forKey:NSFontAttributeName];
+            [attributes setObject:(__bridge NSFont *)(font.get()) forKey:NSFontAttributeName];
         }
 
         RetainPtr<NSMutableParagraphStyle> paragraphStyle = adoptNS([[NSParagraphStyle defaultParagraphStyle] mutableCopy]);
@@ -215,7 +215,7 @@ void PopupMenuMac::show(const IntRect& r, LocalFrameView* v, int selectedIndex)
         break;
     }
 
-    PAL::popUpMenu(menu, location, roundf(NSWidth(r)), dummyView.get(), selectedIndex, toNSFont(font), controlSize, !m_client->menuStyle().hasDefaultAppearance());
+    PAL::popUpMenu(menu, location, roundf(NSWidth(r)), dummyView.get(), selectedIndex, (__bridge NSFont *)font, controlSize, !m_client->menuStyle().hasDefaultAppearance());
 
     [m_popup dismissPopUp];
     [dummyView removeFromSuperview];

Tools/TestWebKitAPI/Tests/WebKitCocoa/NSAttributedStringWebKitAdditions.mm
@@ -96,9 +96,12 @@ TEST(NSAttributedStringWebKitAdditions, FontDataURL)
 
     __block bool done = false;
     [NSAttributedString loadFromHTMLWithString:html options:@{ } completionHandler:^(NSAttributedString *attributedString, NSDictionary<NSAttributedStringDocumentAttributeKey, id> *attributes, NSError *error) {
+        __block bool foundFont { false };
         [attributedString enumerateAttributesInRange:NSMakeRange(0, attributedString.length) options:0 usingBlock:^(NSDictionary *attributes, NSRange attributeRange, BOOL *stop) {
-            EXPECT_FALSE(attributes[NSFontAttributeName]);
+            if (attributes[NSFontAttributeName])
+                foundFont = true;
         }];
+        EXPECT_TRUE(foundFont);
         done = true;
     }];
     TestWebKitAPI::Util::run(&done);