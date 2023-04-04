3a9451dba31381654b6b9ebd65f21d083a36b474
Nikolaos Mouchtaris <nmouchtaris@apple.com>
2023-04-03 22:18:10 -0700

    Update fast/scrolling/mac/scrollbars tests to use new UIHelper scrollbar state function https://bugs.webkit.org/show_bug.cgi?id=254698 rdar://107389069
    Reviewed by Simon Fraser.
    
    Update the scrollbar tests to use the new infrastructure. When in non UI-side compositing mode
    UIHelper.horizontal/verticalScrollbarState will use the old internals function, and when in
    UI-side compositing mode, it will use the new UIScriptController function.
    
    * LayoutTests/fast/scrolling/mac/scrollbars/overflow-in-iframe-overlay-scrollbar-hovered.html:
    * LayoutTests/fast/scrolling/mac/scrollbars/overflow-in-iframe-overlay-scrollbar-reveal.html:
    * LayoutTests/fast/scrolling/mac/scrollbars/overflow-overlay-scrollbar-hovered.html:
    * LayoutTests/fast/scrolling/mac/scrollbars/overflow-overlay-scrollbar-reveal.html:
    * LayoutTests/fast/scrolling/mac/scrollbars/overlay-scrollbar-hovered.html:
    * LayoutTests/fast/scrolling/mac/scrollbars/overlay-scrollbar-reveal.html:
    * LayoutTests/fast/scrolling/mac/scrollbars/overlay-scrollbar-state-expected.txt:
    * LayoutTests/fast/scrolling/mac/scrollbars/overlay-scrollbar-state.html:
    * LayoutTests/fast/scrolling/mac/scrollbars/scrollbar-state-expected.txt:
    * LayoutTests/fast/scrolling/mac/scrollbars/scrollbar-state.html:
    * LayoutTests/fast/scrolling/mac/scrollbars/select-overlay-scrollbar-hovered.html:
    * LayoutTests/fast/scrolling/mac/scrollbars/select-overlay-scrollbar-reveal.html:
    * LayoutTests/fast/scrolling/mac/scrollbars/very-wide-overlay-scrollbar.html:
    * LayoutTests/resources/ui-helper.js:
    (window.UIHelper.async waitForConditionAsync):
    (window.UIHelper.scrollbarState.return.new.Promise.):
    (window.UIHelper.scrollbarState.return.new.Promise):
    (window.UIHelper.scrollbarState):
    (window.UIHelper.verticalScrollbarState):
    (window.UIHelper.horizontalScrollbarState):
    
    Canonical link: https://commits.webkit.org/262561@main

--- LayoutTests/resources/ui-helper.js
+++ LayoutTests/resources/ui-helper.js
@@ -178,6 +178,15 @@ window.UIHelper = class UIHelper {
         }
     }
 
+    static async waitForConditionAsync(conditionFunc)
+    {
+        var condition = await conditionFunc();
+        while (!condition) {
+            await UIHelper.animationFrame();
+            condition = await conditionFunc();
+        }
+    }
+
     static sendEventStream(eventStream)
     {
         const eventStreamAsString = JSON.stringify(eventStream);
@@ -862,20 +871,21 @@ window.UIHelper = class UIHelper {
 
     static scrollbarState(scroller, isVertical)
     {
+        var internalFunctions = scroller ? scroller.ownerDocument.defaultView.internals : internals;
         if (!this.isWebKit2() || this.isIOSFamily())
             return Promise.resolve();
         if (internals.isUsingUISideCompositing()) {
             return new Promise(resolve => {
                 testRunner.runUIScript(`(function() {
                     uiController.doAfterNextStablePresentationUpdate(function() {
-                        uiController.uiScriptComplete(uiController.scrollbarStateForScrollingNodeID(${internals.scrollingNodeIDForNode(scroller)}, ${isVertical}));
+                        uiController.uiScriptComplete(uiController.scrollbarStateForScrollingNodeID(${internalFunctions.scrollingNodeIDForNode(scroller)}, ${isVertical}));
                     });
                 })()`, state => {
                     resolve(state);
                 });
             });    
         } else {
-            return isVertical ? Promise.resolve(internals.verticalScrollbarState(scroller)) : Promise.resolve(internals.horizontalScrollbarState(scroller));
+            return isVertical ? Promise.resolve(internalFunctions.verticalScrollbarState(scroller)) : Promise.resolve(internalFunctions.horizontalScrollbarState(scroller));
         }
     }
 
