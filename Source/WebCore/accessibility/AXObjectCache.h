b459920528a607cfba5a57f126097546ff47628b
Ross Kirsling <rkirsling@gmail.com>
2023-04-04 00:44:17 -0700

    Unreviewed, fix !ENABLE(ACCESSIBILITY) build following 262566@main https://bugs.webkit.org/show_bug.cgi?id=254977
    Unreviewed PlayStation build fix.
    
    * Source/WebCore/accessibility/AXObjectCache.h:
    (WebCore::AXObjectCache::updateRelations):
    Add missing function definition.
    
    Canonical link: https://commits.webkit.org/262570@main

--- Source/WebCore/accessibility/AXObjectCache.h
+++ Source/WebCore/accessibility/AXObjectCache.h
@@ -722,6 +722,7 @@ inline void AXObjectCache::onRendererCreated(Element&) { }
 inline void AXObjectCache::updateLoadingProgress(double) { }
 inline SimpleRange AXObjectCache::rangeForNodeContents(Node& node) { return makeRangeSelectingNodeContents(node); }
 inline std::optional<Vector<AXID>> AXObjectCache::relatedObjectIDsFor(const AXCoreObject&, AXRelationType) { return std::nullopt; }
+inline void AXObjectCache::updateRelations(Element&, const QualifiedName&) { }
 inline void AXObjectCache::remove(AXID) { }
 inline void AXObjectCache::remove(RenderObject*) { }
 inline void AXObjectCache::remove(Node&) { }
