44a09b4e75e2b71681fec3205c182d28edae67b2
Tim Horton <timothy_horton@apple.com>
2023-04-05 11:48:00 -0700

    Revert 262184@main https://bugs.webkit.org/show_bug.cgi?id=255041 rdar://107624168
    Unreviewed revert.
    
    * Source/WebKit/Shared/RemoteLayerTree/RemoteLayerBackingStore.mm:
    (WebKit::RemoteLayerBackingStoreProperties::applyBackingStoreToLayer):
    
    Canonical link: https://commits.webkit.org/262626@main

--- Source/WebKit/Shared/RemoteLayerTree/RemoteLayerBackingStore.mm
+++ Source/WebKit/Shared/RemoteLayerTree/RemoteLayerBackingStore.mm
@@ -677,7 +677,6 @@ void RemoteLayerBackingStoreProperties::applyBackingStoreToLayer(CALayer *layer,
         if (!replayCGDisplayListsIntoBackingStore) {
             [layer setValue:@1 forKeyPath:WKCGDisplayListEnabledKey];
             [layer setValue:@1 forKeyPath:WKCGDisplayListBifurcationEnabledKey];
-            layer.drawsAsynchronously = (m_type == RemoteLayerBackingStore::Type::IOSurface);
         } else
             layer.opaque = m_isOpaque;
         [(WKCompositingLayer *)layer _setWKContents:contents.get() withDisplayList:WTFMove(std::get<CGDisplayList>(*m_displayListBufferHandle)) replayForTesting:replayCGDisplayListsIntoBackingStore];
