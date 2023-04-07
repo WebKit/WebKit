776f5f1ac40f7cb8e7034e6a1e0470c2087af141
Yusuke Suzuki <ysuzuki@apple.com>
2023-04-07 14:01:05 -0700

    [JSC] Restore liveness based interference analysis too https://bugs.webkit.org/show_bug.cgi?id=252798 rdar://problem/105818549
    Reviewed by Ryan Haddad and Michael Saboff.
    
    This is partial revert of https://commits.webkit.org/259548.47@safari-7615-branch.
    While the above change fixed the global interference problem, we have a problem.
    
    1. We would like to check interference on all the live place since OSR exit may need to restore
       phantomized candidate nodes.
    2. If PutStack / GetStack happens in the basic block and the stack state gets incorrect in the
       middle of basic block, we cannot know.
    
        loc0 => @b
        PutStack loc0 @a
        ...
        OSR exit
        ...
        PutStack loc0 @b
        ...
        loc0 => @b
    
    So we partially revert the change in https://commits.webkit.org/259548.47@safari-7615-branch,
    
    1. First collect all stack modification. And record which part of stack is modified for each basic block unit.
    2. Then, for each basic block
        2.1. If this basic block does not have stack modification, then just check availability for live candidates availability
             this head. Since this basic block does not have stack modification, this basic block itself never incurs interference.
             Only thing we need to check is that now the live nodes are already clobbered in terms of availability because the
             successors modified the stack.
        2.2. If this basic block does stack modification, let's take live candidates at the tail of basic block and check whether
             the basic block is clobbering this candidate's relying stack. If so, remove it from candidate.
        2.3. For each newly killed DFG node at a node-index in this basic block, check (2.2)'s stack modification and remove it
             from candidate if its stack is modified.
    
    This added part is effectively the code removed in https://commits.webkit.org/259548.47@safari-7615-branch (so, existing code before).
    Compared to the old behavior, 2.1 is added newly to cover inter-block handling.
    
    * Source/JavaScriptCore/dfg/DFGArgumentsEliminationPhase.cpp:
    * Source/JavaScriptCore/dfg/DFGAvailabilityMap.h:
    (JSC::DFG::AvailabilityMap::forEachAvailability const):
    (JSC::DFG::AvailabilityMap::closeOverNodes const):
    (JSC::DFG::AvailabilityMap::closeStartingWithLocal const):
    (JSC::DFG::AvailabilityMap::forEachAvailability): Deleted.
    (JSC::DFG::AvailabilityMap::closeOverNodes): Deleted.
    (JSC::DFG::AvailabilityMap::closeStartingWithLocal): Deleted.
    
    Cherry-pick: 259548.275@safari-7615-branch (b022d8caf560). rdar://107709855
    Canonical link: https://commits.webkit.org/262738@main

--- Source/JavaScriptCore/dfg/DFGForAllKills.h
+++ Source/JavaScriptCore/dfg/DFGForAllKills.h
@@ -181,15 +181,17 @@ void forAllKillsInBlock(
     
     LocalOSRAvailabilityCalculator localAvailability(graph);
     localAvailability.beginBlock(block);
-    // Start at the second node, because the functor is expected to only inspect nodes from the start of
+    // Start running functor at the second node, because the functor is expected to only inspect nodes from the start of
     // the block up to nodeIndex (exclusive), so if nodeIndex is zero then the functor has nothing to do.
-    for (unsigned nodeIndex = 1; nodeIndex < block->size(); ++nodeIndex) {
+    for (unsigned nodeIndex = 0; nodeIndex < block->size(); ++nodeIndex) {
         dataLogLnIf(ForAllKillsInternal::verbose, "local availability at index: ", nodeIndex, " ", localAvailability.m_availability);
-        forAllKilledNodesAtNodeIndex(
-            graph, localAvailability.m_availability, block, nodeIndex,
-            [&] (Node* node) {
-                functor(nodeIndex, node);
-            });
+        if (nodeIndex) {
+            forAllKilledNodesAtNodeIndex(
+                graph, localAvailability.m_availability, block, nodeIndex,
+                [&] (Node* node) {
+                    functor(nodeIndex, node);
+                });
+        }
         localAvailability.executeNode(block->at(nodeIndex));
     }
 }
