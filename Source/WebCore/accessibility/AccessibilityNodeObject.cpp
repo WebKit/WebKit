616b690c0767730e82d452ae6031ed2e74d61e02
Tyler Wilcock <tyler_w@apple.com>
2023-04-08 13:55:52 -0700

    Improve Ref / RefPtr hygiene in AXObjectCache::handleAttributeChange and AccessibilityNodeObject::textUnderElement rdar://problem/106557770
    Reviewed by Chris Fleizach.
    
    Bring these two functions in line with the WebKit smart pointer style
    guide (https://github.com/WebKit/WebKit/wiki/Smart-Pointer-Usage-Guidelines) by
    reference counting AccessibilityObjects before passing them into non-trivial functions.
    
    * Source/WebCore/accessibility/AXObjectCache.cpp:
    (WebCore::AXObjectCache::handleAttributeChange):
    * Source/WebCore/accessibility/AccessibilityNodeObject.cpp:
    (WebCore::AccessibilityNodeObject::textUnderElement const):
    * LayoutTests/platform/ios/TestExpectations:
    Mark new test as passing.
    * LayoutTests/accessibility/changing-aria-hidden-with-display-none-parent-expected.txt: Added.
    * LayoutTests/accessibility/changing-aria-hidden-with-display-none-parent.html: Added.
    * LayoutTests/platform/ios-simulator/accessibility/changing-aria-hidden-with-display-none-parent-expected.txt: Added.
    
    Cherry-pick: 259548.387@safari-7615-branch (dd026e944caa). rdar://problem/107755793
    Canonical link: https://commits.webkit.org/262753@main

--- Source/WebCore/accessibility/AccessibilityNodeObject.cpp
+++ Source/WebCore/accessibility/AccessibilityNodeObject.cpp
@@ -2036,7 +2036,7 @@ String AccessibilityNodeObject::textUnderElement(AccessibilityTextUnderElementMo
     }
 
     StringBuilder builder;
-    for (AccessibilityObject* child = firstChild(); child; child = child->nextSibling()) {
+    for (RefPtr child = firstChild(); child; child = child->nextSibling()) {
         if (mode.ignoredChildNode && child->node() == mode.ignoredChildNode)
             continue;
         
@@ -2046,7 +2046,7 @@ String AccessibilityNodeObject::textUnderElement(AccessibilityTextUnderElementMo
             continue;
         }
         
-        if (!shouldUseAccessibilityObjectInnerText(child, mode))
+        if (!shouldUseAccessibilityObjectInnerText(child.get(), mode))
             continue;
 
         if (is<AccessibilityNodeObject>(*child)) {
