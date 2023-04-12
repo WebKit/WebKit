695a4fb4014694df905ba03aa131308d763e56f2
Jer Noble <jer.noble@apple.com>
2023-04-12 14:21:20 -0700

    [Mac] Adopt -setEligibleForBTSmartRoutingConsideration:error: https://bugs.webkit.org/show_bug.cgi?id=255343 rdar://107144097
    Reviewed by Eric Carlson.
    
    -[AVAudioSession setEligibleForBTSmartRoutingConsideration:error:] has been
    available and in use on iOS for a while; adopt it (where available) on macOS
    as well.
    
    Add a new, common, base class to AVAudioSessionIOS and -Mac, AudioSessionCocoa.
    Move tryToSetActive() and setEligibleForSmartRouting() into that new base class.
    
    * Source/WebCore/PAL/pal/cocoa/AVFoundationSoftLink.h:
    * Source/WebCore/PAL/pal/cocoa/AVFoundationSoftLink.mm:
    * Source/WebCore/PAL/pal/spi/cocoa/AVFoundationSPI.h:
    * Source/WebCore/WebCore.xcodeproj/project.pbxproj:
    * Source/WebCore/platform/audio/cocoa/AudioSessionCocoa.h: Copied from Source/WebCore/platform/audio/ios/AudioSessionIOS.h.
    * Source/WebCore/platform/audio/cocoa/AudioSessionCocoa.mm: Added.
    (WebCore::setEligibleForSmartRouting):
    (WebCore::AudioSessionCocoa::AudioSessionCocoa):
    (WebCore::AudioSessionCocoa::~AudioSessionCocoa):
    (WebCore::AudioSessionCocoa::tryToSetActiveInternal):
    * Source/WebCore/platform/audio/ios/AudioSessionIOS.h:
    * Source/WebCore/platform/audio/ios/AudioSessionIOS.mm:
    (WebCore::AudioSessionIOS::AudioSessionIOS):
    (WebCore::setEligibleForSmartRouting): Deleted.
    (WebCore::AudioSessionIOS::tryToSetActiveInternal): Deleted.
    * Source/WebCore/platform/audio/mac/AudioSessionMac.h:
    * Source/WebCore/platform/audio/mac/AudioSessionMac.mm:
    (WebCore::AudioSessionMac::tryToSetActiveInternal): Deleted.
    
    Canonical link: https://commits.webkit.org/262886@main

---
+++ Source/WebCore/platform/audio/cocoa/AudioSessionCocoa.mm
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2023 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "config.h"
+#import "AudioSessionCocoa.h"
+
+#if USE(AUDIO_SESSION) && PLATFORM(COCOA)
+
+#import "Logging.h"
+#import "NotImplemented.h"
+#import <AVFoundation/AVAudioSession.h>
+#import <pal/spi/cocoa/AVFoundationSPI.h>
+#import <wtf/WorkQueue.h>
+
+#import <pal/cocoa/AVFoundationSoftLink.h>
+
+namespace WebCore {
+
+static void setEligibleForSmartRouting(bool eligible)
+{
+#if HAVE(AVAUDIOSESSION)
+    if (!AudioSession::shouldManageAudioSessionCategory())
+        return;
+
+    static bool supportsEligibleForBT = [PAL::getAVAudioSessionClass() instancesRespondToSelector:@selector(setEligibleForBTSmartRoutingConsideration:error:)]
+        && [PAL::getAVAudioSessionClass() instancesRespondToSelector:@selector(eligibleForBTSmartRoutingConsideration)];
+    if (!supportsEligibleForBT)
+        return;
+
+    AVAudioSession *session = [PAL::getAVAudioSessionClass() sharedInstance];
+
+    if (session.eligibleForBTSmartRoutingConsideration == eligible)
+        return;
+
+    NSError *error = nil;
+    if (![session setEligibleForBTSmartRoutingConsideration:eligible error:&error])
+        RELEASE_LOG_ERROR(Media, "failed to set eligible to %d with error: %@", eligible, error.localizedDescription);
+#else
+    UNUSED_PARAM(eligible);
+#endif
+}
+
+AudioSessionCocoa::AudioSessionCocoa()
+    : m_workQueue(WorkQueue::create("AudioSession Activation Queue"))
+{
+    m_workQueue->dispatch([] {
+        setEligibleForSmartRouting(false);
+    });
+
+}
+
+AudioSessionCocoa::~AudioSessionCocoa()
+{
+}
+
+bool AudioSessionCocoa::tryToSetActiveInternal(bool active)
+{
+#if HAVE(AVAUDIOSESSION)
+    static bool supportsSharedInstance = [PAL::getAVAudioSessionClass() respondsToSelector:@selector(sharedInstance)];
+    static bool supportsSetActive = [PAL::getAVAudioSessionClass() instancesRespondToSelector:@selector(setActive:withOptions:error:)];
+
+    if (!supportsSharedInstance)
+        return true;
+
+    // We need to deactivate the session on another queue because the AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation option
+    // means that AVAudioSession may synchronously unduck previously ducked clients. Activation needs to complete before this method
+    // returns, so do it synchronously on the same serial queue.
+    if (active) {
+        bool success = false;
+        m_workQueue->dispatchSync([&success] {
+            NSError *error = nil;
+            setEligibleForSmartRouting(true);
+            if (supportsSetActive)
+                [[PAL::getAVAudioSessionClass() sharedInstance] setActive:YES withOptions:0 error:&error];
+            success = !error;
+        });
+        return success;
+    }
+
+    m_workQueue->dispatch([] {
+        NSError *error = nil;
+        if (supportsSetActive)
+            [[PAL::getAVAudioSessionClass() sharedInstance] setActive:NO withOptions:0 error:&error];
+        setEligibleForSmartRouting(false);
+    });
+#else
+    UNUSED_PARAM(active);
+    notImplemented();
+#endif
+    return true;
+}
+
+}
+
+#endif // USE(AUDIO_SESSION) && PLATFORM(COCOA)
--- Source/WebCore/platform/audio/ios/AudioSessionIOS.mm
+++ Source/WebCore/platform/audio/ios/AudioSessionIOS.mm
@@ -110,39 +110,11 @@ void AudioSessionIOS::addAudioSessionCategoryChangedObserver(const CategoryChang
     observer(AudioSession::sharedSession(), AudioSession::sharedSession().category());
 }
 
-static void setEligibleForSmartRouting(bool eligible)
-{
-#if PLATFORM(IOS)
-    if (!AudioSession::shouldManageAudioSessionCategory())
-        return;
-
-    AVAudioSession *session = [PAL::getAVAudioSessionClass() sharedInstance];
-    if (![session respondsToSelector:@selector(setEligibleForBTSmartRoutingConsideration:error:)]
-        || ![session respondsToSelector:@selector(eligibleForBTSmartRoutingConsideration)])
-        return;
-
-    if (session.eligibleForBTSmartRoutingConsideration == eligible)
-        return;
-
-    NSError *error = nil;
-    if (![session setEligibleForBTSmartRoutingConsideration:eligible error:&error])
-        RELEASE_LOG_ERROR(Media, "failed to set eligible to %d with error: %@", eligible, error.localizedDescription);
-#else
-    UNUSED_PARAM(eligible);
-#endif
-}
-
 AudioSessionIOS::AudioSessionIOS()
-    : m_workQueue(WorkQueue::create("AudioSession Activation Queue"))
 {
     BEGIN_BLOCK_OBJC_EXCEPTIONS
     m_interruptionObserverHelper = adoptNS([[WebInterruptionObserverHelper alloc] initWithCallback:this]);
     END_BLOCK_OBJC_EXCEPTIONS
-
-    m_workQueue->dispatch([] {
-        setEligibleForSmartRouting(false);
-    });
-
 }
 
 AudioSessionIOS::~AudioSessionIOS()
@@ -334,31 +306,6 @@ size_t AudioSessionIOS::numberOfOutputChannels() const
 size_t AudioSessionIOS::maximumNumberOfOutputChannels() const
 {
     return [[PAL::getAVAudioSessionClass() sharedInstance] maximumOutputNumberOfChannels];
-}
-
-bool AudioSessionIOS::tryToSetActiveInternal(bool active)
-{
-    // We need to deactivate the session on another queue because the AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation option
-    // means that AVAudioSession may synchronously unduck previously ducked clients. Activation needs to complete before this method
-    // returns, so do it synchronously on the same serial queue.
-    if (active) {
-        bool success = false;
-        m_workQueue->dispatchSync([&success] {
-            NSError *error = nil;
-            setEligibleForSmartRouting(true);
-            [[PAL::getAVAudioSessionClass() sharedInstance] setActive:YES withOptions:0 error:&error];
-            success = !error;
-        });
-        return success;
-    }
-
-    m_workQueue->dispatch([] {
-        NSError *error = nil;
-        [[PAL::getAVAudioSessionClass() sharedInstance] setActive:NO withOptions:0 error:&error];
-        setEligibleForSmartRouting(false);
-    });
-
-    return true;
 }
 
 size_t AudioSessionIOS::preferredBufferSize() const
