WebKit/WebKit

commit f9672dbf09d92cbcc1ce47ee959a7ff55ed27f9e

[iOS] Pan gestures immediately after tap do not scroll a page with an unresponsive viewport https://bugs.webkit.org/show_bug.cgi?id=255300 rdar://106010038
Reviewed by Megan Gardner.

Currently, in a page with an unresponsive viewport, attempting to scroll the web view with a pan
gesture immediately after performing a tap does not actually result in any scrolling, since the
scroll view's pan gesture recognizer does not begin. This happens because while the pan gesture is
immediately reset to `Possible` state after the tap, it then transitions to `Failed` state right
away when subsequently panning the web view, since it's excluded by the synthetic tap (single click)
gesture which is still in `Ended` state because it hasn't been reset yet (due to the double click
gesture still being `Possible`).

To avoid this, we allow both the synthetic tap gesture to recognize simultaneously alongside pan
gestures by adding logic to `-gestureRecognizer:shouldRecognizeSimultaneouslyWithGestureRecognizer:`.
This means that instead of transitioning to `Failed` state right away when panning the web view, we
instead remain in `Possible` state, which then allows us to recognize the gesture if the user begins
to actually pan the view.

Test: fast/events/touch/ios/swipe-after-tap-scrolls-web-view-in-non-responsive-viewport.html

* LayoutTests/fast/events/touch/ios/swipe-after-tap-scrolls-web-view-in-non-responsive-viewport-expected.txt: Added.
* LayoutTests/fast/events/touch/ios/swipe-after-tap-scrolls-web-view-in-non-responsive-viewport.html: Added.

Add a layout test to exercise the fix. This test preemptively tries to mitigate any potential
flakiness as a result of either the tap or swipe gesture not getting recognized by automatically
retrying itself several times, until it's successful.

* Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm:
(-[WKContentView gestureRecognizer:shouldRecognizeSimultaneouslyWithGestureRecognizer:]):

Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -2566,6 +2566,9 @@ static inline bool isSamePair(UIGestureRecognizer *a, UIGestureRecognizer *b, UI
         return ![self shouldDeferGestureDueToImageAnalysis:gestureRecognizer];
 #endif
 
+    if (gestureRecognizer == _singleTapGestureRecognizer && [otherGestureRecognizer isKindOfClass:UIPanGestureRecognizer.class])
+        return YES;
+
     if (isSamePair(gestureRecognizer, otherGestureRecognizer, _highlightLongPressGestureRecognizer.get(), _longPressGestureRecognizer.get()))
         return YES;
 

LayoutTests/fast/events/touch/ios/swipe-after-tap-scrolls-web-view-in-non-responsive-viewport-expected.txt
@@ -1,0 +1,10 @@
+This test verifies that a pan gesture that immediately follows a tap still results in the web view being scrolled.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS Observed scrolling after a tap
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/events/touch/ios/swipe-after-tap-scrolls-web-view-in-non-responsive-viewport.html
@@ -1,0 +1,65 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ useFlexibleViewport=true AsyncOverflowScrollingEnabled=true ] -->
+<html>
+<meta name="viewport" content="width=500">
+<head>
+<script src="../../../../resources/ui-helper.js"></script>
+<script src="../../../../resources/js-test.js"></script>
+<style>
+html, body {
+    margin: 0;
+    height: 10000px;
+}
+
+#console {
+    position: fixed;
+}
+</style>
+</head>
+<body>
+<div id="console"></div>
+<script>
+jsTestIsAsync = true;
+description("This test verifies that a pan gesture that immediately follows a tap still results in the web view being scrolled.");
+
+let handledClick;
+document.body.addEventListener("click", () => handledClick = true);
+
+async function attemptTest() {
+    scrollTo(0, 0);
+    handledClick = false;
+
+    await UIHelper.ensurePresentationUpdate();
+    await UIHelper.sendEventStream(new UIHelper.EventStreamBuilder()
+        .begin(100, 100)
+        .wait(0.1)
+        .end()
+        .takeResult());
+
+    await UIHelper.sendEventStream(new UIHelper.EventStreamBuilder()
+        .begin(250, 800)
+        .move(250, 50, 0.25)
+        .end()
+        .takeResult());
+
+    await UIHelper.ensureStablePresentationUpdate();
+    return handledClick && pageYOffset > 0;
+}
+
+addEventListener("load", async () => {
+    if (!window.testRunner)
+        return;
+
+    const maximumNumberOfAttempts = 10;
+    for (let i = 0; i < maximumNumberOfAttempts; ++i) {
+        if (await attemptTest()) {
+            testPassed("Observed scrolling after a tap");
+            finishJSTest();
+            return;
+        }
+    }
+    testFailed(`Failed ${maximumNumberOfAttempts} attempts to scroll after tapping`);
+    finishJSTest();
+});
+</script>
+</body>
+</html>

commit c3c2a3059b1e60f84a97fbe84cd2aa312be48916

REGRESSION (262853@main): [iOS] 2 fast/scrolling/ios/click-events-during-momentum-scroll-* tests consistently failing https://bugs.webkit.org/show_bug.cgi?id=255415 rdar://108011669
Reviewed by Tim Horton.

A couple of iOS layout tests began failing consistently after 262853@main; these two tests:

1. Scroll in a web page (either in the mainframe or an overflow scroller).
2. Tap the page while the scroll is decelerating.
3. Verify that the tap in step (2) did not trigger any click events.

While I'm only able to rarely reproduce this on device, it appears to readily reproduce when using
the simulator; I suspect this is because the touch point doesn't change at all when using the
trackpad or synthesizing taps in the simulator/testing environment, which causes the scroll view pan
gesture to never receive any touch moves.

We can fix this by only allowing simultaneous gesture recognition between the single tap and pan
gesture, in the case where the touch isn't interrupting scroll view deceleration.

* Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm:
(-[WKContentView gestureRecognizer:shouldRecognizeSimultaneouslyWithGestureRecognizer:]):
(-[WKContentView _isInterruptingDecelerationForScrollViewOrAncestor:]):

Move this logic out of a local C++ lambda function inside of `-gestureRecognizerShouldBegin:`, and
into a separate helper method so that we can use it above as well, in
`-gestureRecognizer:shouldRecognizeSimultaneouslyWithGestureRecognizer:`.

(-[WKContentView gestureRecognizerShouldBegin:]):

Canonical link: https://commits.webkit.org/262963@main

Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -2566,7 +2566,8 @@ static inline bool isSamePair(UIGestureRecognizer *a, UIGestureRecognizer *b, UI
         return ![self shouldDeferGestureDueToImageAnalysis:gestureRecognizer];
 #endif
 
-    if (gestureRecognizer == _singleTapGestureRecognizer && [otherGestureRecognizer isKindOfClass:UIPanGestureRecognizer.class])
+    if (gestureRecognizer == _singleTapGestureRecognizer && [otherGestureRecognizer isKindOfClass:UIPanGestureRecognizer.class]
+        && ![self _isInterruptingDecelerationForScrollViewOrAncestor:[_singleTapGestureRecognizer lastTouchedScrollView]])
         return YES;
 
     if (isSamePair(gestureRecognizer, otherGestureRecognizer, _highlightLongPressGestureRecognizer.get(), _longPressGestureRecognizer.get()))
@@ -2868,29 +2869,30 @@ static inline bool isSamePair(UIGestureRecognizer *a, UIGestureRecognizer *b, UI
     return pointIsInSelectionRect;
 }
 
+- (BOOL)_isInterruptingDecelerationForScrollViewOrAncestor:(UIScrollView *)scrollView
+{
+    UIScrollView *mainScroller = self.webView.scrollView;
+    UIView *view = scrollView ?: mainScroller;
+    while (view) {
+        if (dynamic_objc_cast<UIScrollView>(view)._isInterruptingDeceleration)
+            return YES;
+
+        if (mainScroller == view)
+            break;
+
+        view = view.superview;
+    }
+    return NO;
+}
+
 - (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
 {
     CGPoint point = [gestureRecognizer locationInView:self];
 
-    auto isInterruptingDecelerationForScrollViewOrAncestor = [&] (UIScrollView *scrollView) {
-        UIScrollView *mainScroller = self.webView.scrollView;
-        UIView *view = scrollView ?: mainScroller;
-        while (view) {
-            if ([view isKindOfClass:UIScrollView.class] && [(UIScrollView *)view _isInterruptingDeceleration])
-                return YES;
-
-            if (mainScroller == view)
-                break;
-
-            view = view.superview;
-        }
-        return NO;
-    };
-
     if (gestureRecognizer == _singleTapGestureRecognizer) {
         if ([self _shouldToggleSelectionCommandsAfterTapAt:point])
             return NO;
-        return !isInterruptingDecelerationForScrollViewOrAncestor([_singleTapGestureRecognizer lastTouchedScrollView]);
+        return ![self _isInterruptingDecelerationForScrollViewOrAncestor:[_singleTapGestureRecognizer lastTouchedScrollView]];
     }
 
     if (gestureRecognizer == _doubleTapGestureRecognizerForDoubleClick) {
@@ -2919,7 +2921,7 @@ static inline bool isSamePair(UIGestureRecognizer *a, UIGestureRecognizer *b, UI
     }
 
     if (gestureRecognizer == _highlightLongPressGestureRecognizer) {
-        if (isInterruptingDecelerationForScrollViewOrAncestor([_highlightLongPressGestureRecognizer lastTouchedScrollView]))
+        if ([self _isInterruptingDecelerationForScrollViewOrAncestor:[_highlightLongPressGestureRecognizer lastTouchedScrollView]])
             return NO;
 
         if (self._hasFocusedElement) {