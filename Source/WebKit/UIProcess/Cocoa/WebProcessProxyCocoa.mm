c22ea9424e63f968123d150ececf7b19044c1f3c
Per Arne Vollan <pvollan@apple.com>
2023-04-14 07:12:50 -0700

    Fix assert failure after 262912@main https://bugs.webkit.org/show_bug.cgi?id=255413 rdar://108009928
    Unreviewed assert fix.
    
    Update the expected sandbox extension count in the assert, since 262912@main added one
    more extension.
    
    * Source/WebKit/UIProcess/Cocoa/WebProcessProxyCocoa.mm:
    (WebKit::WebProcessProxy::unblockAccessibilityServerIfNeeded):
    
    Canonical link: https://commits.webkit.org/262962@main

--- Source/WebKit/UIProcess/Cocoa/WebProcessProxyCocoa.mm
+++ Source/WebKit/UIProcess/Cocoa/WebProcessProxyCocoa.mm
@@ -252,7 +252,7 @@ void WebProcessProxy::unblockAccessibilityServerIfNeeded()
     Vector<SandboxExtension::Handle> handleArray;
 #if PLATFORM(IOS_FAMILY)
     handleArray = SandboxExtension::createHandlesForMachLookup({ "com.apple.iphone.axserver-systemwide"_s, "com.apple.frontboard.systemappservices"_s, "com.apple.CARenderServer"_s }, auditToken(), SandboxExtension::MachBootstrapOptions::EnableMachBootstrap);
-    ASSERT(handleArray.size() == 3);
+    ASSERT(handleArray.size() == 4);
 #endif
 
     send(Messages::WebProcess::UnblockServicesRequiredByAccessibility(handleArray), 0);
