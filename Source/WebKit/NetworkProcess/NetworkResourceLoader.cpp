ffa1fe87b2791fd07557e3526c61e21337158550
Alex Christensen <achristensen@apple.com>
2023-04-17 14:09:35 -0700

    Cherry-pick 259548.637@safari-7615-branch (2aa0035b7a09). <bug>
        Cherry-pick 71578313d03d. rdar://106952778
    
            Network process should only consider web archives to have been loaded if loaded using local scheme
            https://bugs.webkit.org/show_bug.cgi?id=255459
            rdar://106952778
    
            Reviewed by John Pascoe.
    
            Adding the scheme check matches the check in DocumentLoader::disallowWebArchive
    
            * Source/WebKit/NetworkProcess/NetworkResourceLoader.cpp:
            (WebKit::NetworkResourceLoader::didReceiveMainResourceResponse):
    
        Canonical link: https://commits.webkit.org/259548.637@safari-7615-branch
    
    Canonical link: https://commits.webkit.org/260527.357@webkitglib/2.40

--- Source/WebKit/NetworkProcess/NetworkResourceLoader.cpp
+++ Source/WebKit/NetworkProcess/NetworkResourceLoader.cpp
@@ -59,6 +59,8 @@
 #include <WebCore/CrossOriginEmbedderPolicy.h>
 #include <WebCore/DiagnosticLoggingKeys.h>
 #include <WebCore/HTTPParsers.h>
+#include <WebCore/LegacySchemeRegistry.h>
+#include <WebCore/LinkHeader.h>
 #include <WebCore/NetworkLoadMetrics.h>
 #include <WebCore/NetworkStorageSession.h>
 #include <WebCore/RegistrableDomain.h>
@@ -1499,7 +1501,8 @@ void NetworkResourceLoader::didReceiveMainResourceResponse(const WebCore::Resour
         speculativeLoadManager->registerMainResourceLoadResponse(globalFrameID(), originalRequest(), response);
 #endif
 #if ENABLE(WEB_ARCHIVE)
-    if (equalIgnoringASCIICase(response.mimeType(), "application/x-webarchive"_s)) {
+    if (equalIgnoringASCIICase(response.mimeType(), "application/x-webarchive"_s)
+        && LegacySchemeRegistry::shouldTreatURLSchemeAsLocal(response.url().protocol())) {
         auto& connection = connectionToWebProcess();
         connection.networkProcess().webProcessWillLoadWebArchive(connection.webProcessIdentifier());
     }
