0897dc8c4933853d3f79d4ec01e07e2ac4ce5804
Yijia Huang <yijia_huang@apple.com>
2023-04-24 13:03:05 -0700

    Add MemoryExhaustion to enable existing instead of crashing on out of memory https://bugs.webkit.org/show_bug.cgi?id=255881 rdar://108438866
    Reviewed by Yusuke Suzuki and Justin Michaud.
    
    It's intend to crash when a worker exhausting memory when constructing a VM.
    Use RELEASE_ASSERT_RESOURCE_AVAILABLE instead for these types of resource
    exhaustion to unblock fuzzer.
    
    * Source/JavaScriptCore/runtime/ResourceExhaustion.h:
    * Source/JavaScriptCore/runtime/VM.cpp:
    (JSC::VM::VM):
    
    Canonical link: https://commits.webkit.org/263328@main

--- Source/JavaScriptCore/runtime/VM.cpp
+++ Source/JavaScriptCore/runtime/VM.cpp
@@ -95,6 +95,7 @@
 #include "PropertyTable.h"
 #include "RandomizingFuzzerAgent.h"
 #include "RegExpCache.h"
+#include "ResourceExhaustion.h"
 #include "SamplingProfiler.h"
 #include "ScopedArguments.h"
 #include "ShadowChicken.h"
@@ -295,7 +296,7 @@ VM::VM(VMType vmType, HeapType heapType, WTF::RunLoop* runLoop, bool* success)
             if (success)
                 *success = false;
             else
-                RELEASE_ASSERT(bigInt);
+                RELEASE_ASSERT_RESOURCE_AVAILABLE(bigInt, MemoryExhaustion, "Crash intentionally because memory is exhausted.");
         }
     }
 
