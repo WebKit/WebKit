4f1089aa0de30ef75ae54889d960e1c231034047
Commit Queue <commit-queue@webkit.org>
2023-04-25 05:17:59 -0700

    Cherry-pick 574dcb6844bb. rdar://problem/108499789
        Unreviewed, reverting r262174@main.
        https://bugs.webkit.org/show_bug.cgi?id=255918
    
        regressions fixed
    
        Reverted changeset:
    
        "[macOS] Disable background webcontent suspension by default"
        https://bugs.webkit.org/show_bug.cgi?id=254535
        https://commits.webkit.org/262174@main
    
        Canonical link: https://commits.webkit.org/263364@main
    
    Canonical link: https://commits.webkit.org/263289.2@safari-7616.1.12-branch

--- Source/WebKit/Shared/WebPreferencesDefaultValues.cpp
+++ Source/WebKit/Shared/WebPreferencesDefaultValues.cpp
@@ -204,7 +204,12 @@ bool defaultMediaSessionCoordinatorEnabled()
 
 bool defaultRunningBoardThrottlingEnabled()
 {
+#if PLATFORM(MAC)
+    static bool newSDK = linkedOnOrAfterSDKWithBehavior(SDKAlignedBehavior::RunningBoardThrottling);
+    return newSDK;
+#else
     return false;
+#endif
 }
 
 bool defaultShouldDropSuspendedAssertionAfterDelay()
