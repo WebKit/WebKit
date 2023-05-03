2425de1c879b13f96a49d4b2cb83d587d73e536b
Jonathan Bedard <jbedard@apple.com>
2023-05-03 11:56:48 -0700

    [git-webkit] Cache filed bug https://bugs.webkit.org/show_bug.cgi?id=255007 rdar://107629444
    Reviewed by Elliott Williams.
    
    git-webkit branch will file a bug. However, that bug is only picked up
    in a commit message through environment variables, which means that
    bugs filed by `git-webkit branch` aren't very useful except when happening
    through `git-webkit pr`'s invocation of `git-webkit branch`. We have a
    similar problem when crafting cherry-picks or reverting changes.
    
    * Tools/Scripts/hooks/prepare-commit-msg: Pull title, bug and cherry-pick message from
    `git config` if they aren't available in the environment.
    * Tools/Scripts/libraries/webkitscmpy/setup.py: Bump version.
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py: Ditto.
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/mocks/local/git.py: Add ability to get
    and set `git config` values.
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/branch.py:
    (Branch.main): Save filed bug into `git config` environment.
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/cherry_pick.py:
    (CherryPick.main): Save cherry-pick pattern and bug details into `git config`, use
    COMMIT_MESSAGE_BUG instead of GIT_WEBKIT_BUG.
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/command.py:
    (Command.write_branch_variables): Write variable to `git config`'s branch specific namespace.
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/revert.py:
    (Revert.revert_commit): Save filed bug into `git config` environment.
    
    Canonical link: https://commits.webkit.org/263641@main

--- Tools/Scripts/libraries/webkitscmpy/setup.py
+++ Tools/Scripts/libraries/webkitscmpy/setup.py
@@ -29,7 +29,7 @@ def readme():
 
 setup(
     name='webkitscmpy',
-    version='6.3.1',
+    version='6.4.0',
     description='Library designed to interact with git and svn repositories.',
     long_description=readme(),
     classifiers=[
