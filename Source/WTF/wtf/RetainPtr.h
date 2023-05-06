611c1a1ceeaf4a530810d3d60c7460ff89bf1767
David Kilzer <ddkilzer@apple.com>
2023-05-05 18:57:25 -0700

    RetainPtr<>::leakRef() should have NS/CF_RETURNS_RETAINED attributes https://bugs.webkit.org/show_bug.cgi?id=256311 <rdar://108895655>
    Reviewed by Alex Christensen and Darin Adler.
    
    Use template specialization with SFINAE to add the
    CF_RETAINS_RETAINED attribute for CFType objects and the
    NS_RETURNS_RETAINED attribute for Cocoa objects.
    
    This allows the clang static analyzer to reason about the
    leakRef() method.
    
    * Source/WTF/wtf/RetainPtr.h:
    (WTF::RetainPtr<T>::leakRef):
    - Use a technique to allow for template specialization without
      using the class template type.
    - Note that this specialization must also cover the case of
      using `id` in C++ code.
    
    Canonical link: https://commits.webkit.org/263742@main

--- Source/WTF/wtf/RetainPtr.h
+++ Source/WTF/wtf/RetainPtr.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2005-2021 Apple Inc. All rights reserved.
+ *  Copyright (C) 2005-2023 Apple Inc. All rights reserved.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -99,7 +99,27 @@ public:
     ~RetainPtr();
 
     void clear();
-    PtrType leakRef() WARN_UNUSED_RETURN;
+
+#ifdef __OBJC__
+    template<typename U = T>
+    std::enable_if_t<std::is_convertible_v<U, id>, PtrType> leakRef() NS_RETURNS_RETAINED WARN_UNUSED_RETURN {
+        static_assert(std::is_same_v<T, U>, "explicit specialization not allowed");
+        return fromStorageType(std::exchange(m_ptr, nullptr));
+    }
+#else
+    template<typename U = T>
+    std::enable_if_t<std::is_same_v<U, id>, PtrType> leakRef() CF_RETURNS_RETAINED WARN_UNUSED_RETURN {
+        static_assert(std::is_same_v<T, U>, "explicit specialization not allowed");
+        return fromStorageType(std::exchange(m_ptr, nullptr));
+    }
+#endif
+
+    template<typename U = T>
+    std::enable_if_t<!std::is_convertible_v<U, id>, PtrType> leakRef() CF_RETURNS_RETAINED WARN_UNUSED_RETURN {
+        static_assert(std::is_same_v<T, U>, "explicit specialization not allowed");
+        return fromStorageType(std::exchange(m_ptr, nullptr));
+    }
+
 #if HAVE(CFAUTORELEASE)
     PtrType autorelease();
 #endif
@@ -199,11 +219,6 @@ template<typename T> inline void RetainPtr<T>::clear()
 {
     if (auto ptr = std::exchange(m_ptr, nullptr))
         CFRelease(ptr);
-}
-
-template<typename T> inline auto RetainPtr<T>::leakRef() -> PtrType
-{
-    return fromStorageType(std::exchange(m_ptr, nullptr));
 }
 
 #if HAVE(CFAUTORELEASE)
