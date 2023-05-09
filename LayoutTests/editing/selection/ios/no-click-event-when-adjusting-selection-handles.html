WebKit/WebKit

commit f9672dbf09d92cbcc1ce47ee959a7ff55ed27f9e

[iOS] Pan gestures immediately after tap do not scroll a page with an unresponsive viewport https://bugs.webkit.org/show_bug.cgi?id=255300 rdar://106010038
Reviewed by Megan Gardner.

Currently, in a page with an unresponsive viewport, attempting to scroll the web view with a pan
gesture immediately after performing a tap does not actually result in any scrolling, since the
scroll view's pan gesture recognizer does not begin. This happens because while the pan gesture is
immediately reset to `Possible` state after the tap, it then transitions to `Failed` state right
away when subsequently panning the web view, since it's excluded by the synthetic tap (single click)
gesture which is still in `Ended` state because it hasn't been reset yet (due to the double click
gesture still being `Possible`).

To avoid this, we allow both the synthetic tap gesture to recognize simultaneously alongside pan
gestures by adding logic to `-gestureRecognizer:shouldRecognizeSimultaneouslyWithGestureRecognizer:`.
This means that instead of transitioning to `Failed` state right away when panning the web view, we
instead remain in `Possible` state, which then allows us to recognize the gesture if the user begins
to actually pan the view.

Test: fast/events/touch/ios/swipe-after-tap-scrolls-web-view-in-non-responsive-viewport.html

* LayoutTests/fast/events/touch/ios/swipe-after-tap-scrolls-web-view-in-non-responsive-viewport-expected.txt: Added.
* LayoutTests/fast/events/touch/ios/swipe-after-tap-scrolls-web-view-in-non-responsive-viewport.html: Added.

Add a layout test to exercise the fix. This test preemptively tries to mitigate any potential
flakiness as a result of either the tap or swipe gesture not getting recognized by automatically
retrying itself several times, until it's successful.

* Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm:
(-[WKContentView gestureRecognizer:shouldRecognizeSimultaneouslyWithGestureRecognizer:]):

Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -2566,6 +2566,9 @@ static inline bool isSamePair(UIGestureRecognizer *a, UIGestureRecognizer *b, UI
         return ![self shouldDeferGestureDueToImageAnalysis:gestureRecognizer];
 #endif
 
+    if (gestureRecognizer == _singleTapGestureRecognizer && [otherGestureRecognizer isKindOfClass:UIPanGestureRecognizer.class])
+        return YES;
+
     if (isSamePair(gestureRecognizer, otherGestureRecognizer, _highlightLongPressGestureRecognizer.get(), _longPressGestureRecognizer.get()))
         return YES;
 

LayoutTests/fast/events/touch/ios/swipe-after-tap-scrolls-web-view-in-non-responsive-viewport-expected.txt
@@ -1,0 +1,10 @@
+This test verifies that a pan gesture that immediately follows a tap still results in the web view being scrolled.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS Observed scrolling after a tap
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/events/touch/ios/swipe-after-tap-scrolls-web-view-in-non-responsive-viewport.html
@@ -1,0 +1,65 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ useFlexibleViewport=true AsyncOverflowScrollingEnabled=true ] -->
+<html>
+<meta name="viewport" content="width=500">
+<head>
+<script src="../../../../resources/ui-helper.js"></script>
+<script src="../../../../resources/js-test.js"></script>
+<style>
+html, body {
+    margin: 0;
+    height: 10000px;
+}
+
+#console {
+    position: fixed;
+}
+</style>
+</head>
+<body>
+<div id="console"></div>
+<script>
+jsTestIsAsync = true;
+description("This test verifies that a pan gesture that immediately follows a tap still results in the web view being scrolled.");
+
+let handledClick;
+document.body.addEventListener("click", () => handledClick = true);
+
+async function attemptTest() {
+    scrollTo(0, 0);
+    handledClick = false;
+
+    await UIHelper.ensurePresentationUpdate();
+    await UIHelper.sendEventStream(new UIHelper.EventStreamBuilder()
+        .begin(100, 100)
+        .wait(0.1)
+        .end()
+        .takeResult());
+
+    await UIHelper.sendEventStream(new UIHelper.EventStreamBuilder()
+        .begin(250, 800)
+        .move(250, 50, 0.25)
+        .end()
+        .takeResult());
+
+    await UIHelper.ensureStablePresentationUpdate();
+    return handledClick && pageYOffset > 0;
+}
+
+addEventListener("load", async () => {
+    if (!window.testRunner)
+        return;
+
+    const maximumNumberOfAttempts = 10;
+    for (let i = 0; i < maximumNumberOfAttempts; ++i) {
+        if (await attemptTest()) {
+            testPassed("Observed scrolling after a tap");
+            finishJSTest();
+            return;
+        }
+    }
+    testFailed(`Failed ${maximumNumberOfAttempts} attempts to scroll after tapping`);
+    finishJSTest();
+});
+</script>
+</body>
+</html>

commit 0f118a09c40eacfb6d7ca0e8370486b10b3c6cce

REGRESSION (262853@main): Text selection sometimes disappears after interacting with selection handles https://bugs.webkit.org/show_bug.cgi?id=256542 rdar://108200433
Reviewed by Aditya Keerthi.

This is more fallout from the changes in 262853@main, which made pan gestures recognize alongside
the synthetic tap gesture; after that commit, the pan gesture for adjusting selection handles —
`UITextRangeAdjustmentGestureRecognizer` — no longer prevents the synthetic tap gesture from firing
when adjusting selection handles by panning over very short distances (i.e., under the 45pt maximum
allowable movement threshold for recognizing tap gestures).

When performing a pan gesture in this manner on a selection handle over any element with a click
event listener, this causes the selection to collapse as the synthetic click is sent to the page and
sets the selection to a caret at the tap location.

The original intent behind the changes in 262853@main was to allow tap gestures to recognize
alongside scroll view pan gestures only, so this is merely a result of that logic being overly
aggressive. Instead of allowing simultaneous recognition with all pan gestures, we fix this by
limiting it to system scroll view pan gestures only.

Test: editing/selection/ios/no-click-event-when-adjusting-selection-handles.html

* LayoutTests/editing/selection/ios/no-click-event-when-adjusting-selection-handles-expected.txt: Added.
* LayoutTests/editing/selection/ios/no-click-event-when-adjusting-selection-handles.html: Added.
* Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm:
(isBuiltInScrollViewPanGestureRecognizer):
(isBuiltInScrollViewGestureRecognizer):

Pull functionality to check whether or not a given gesture recognizer is a built-in scroll view pan
gesture out into a separate helper method; also, optimize this code slightly by caching the result
of the dynamic `NSString` -> `Class` lookup in static variables.

Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -2497,12 +2497,28 @@ static NSValue *nsSizeForTapHighlightBorderRadius(WebCore::IntSize borderRadius,
     return CGRectNull;
 }
 
+static BOOL isBuiltInScrollViewPanGestureRecognizer(UIGestureRecognizer *recognizer)
+{
+    static Class scrollViewPanGestureClass;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        scrollViewPanGestureClass = NSClassFromString(@"UIScrollViewPanGestureRecognizer");
+    });
+    return [recognizer isKindOfClass:scrollViewPanGestureClass];
+}
+
 static BOOL isBuiltInScrollViewGestureRecognizer(UIGestureRecognizer *recognizer)
 {
-    return ([recognizer isKindOfClass:NSClassFromString(@"UIScrollViewPanGestureRecognizer")]
-        || [recognizer isKindOfClass:NSClassFromString(@"UIScrollViewPinchGestureRecognizer")]
-        || [recognizer isKindOfClass:NSClassFromString(@"UIScrollViewKnobLongPressGestureRecognizer")]
-        );
+    static Class scrollViewPinchGestureClass;
+    static Class scrollViewKnobLongPressGestureRecognizerClass;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        scrollViewPinchGestureClass = NSClassFromString(@"UIScrollViewPinchGestureRecognizer");
+        scrollViewKnobLongPressGestureRecognizerClass = NSClassFromString(@"UIScrollViewKnobLongPressGestureRecognizer");
+    });
+    return isBuiltInScrollViewPanGestureRecognizer(recognizer)
+        || [recognizer isKindOfClass:scrollViewPinchGestureClass]
+        || [recognizer isKindOfClass:scrollViewKnobLongPressGestureRecognizerClass];
 }
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)preventingGestureRecognizer canPreventGestureRecognizer:(UIGestureRecognizer *)preventedGestureRecognizer
@@ -2564,7 +2580,7 @@ static inline bool isSamePair(UIGestureRecognizer *a, UIGestureRecognizer *b, UI
         return ![self shouldDeferGestureDueToImageAnalysis:gestureRecognizer];
 #endif
 
-    if (gestureRecognizer == _singleTapGestureRecognizer && [otherGestureRecognizer isKindOfClass:UIPanGestureRecognizer.class]
+    if (gestureRecognizer == _singleTapGestureRecognizer && isBuiltInScrollViewPanGestureRecognizer(otherGestureRecognizer)
         && ![self _isInterruptingDecelerationForScrollViewOrAncestor:[_singleTapGestureRecognizer lastTouchedScrollView]])
         return YES;
 

LayoutTests/editing/selection/ios/no-click-event-when-adjusting-selection-handles-expected.txt
@@ -1,0 +1,10 @@
+This test verifies that interacting with selection handles doesn't trigger a synthetic click. To manually run the test, select a word in the paragraph above and then adjust either selection handle by a single letter. The selection should not disappear.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+PASS firstSelectionRect.width > 0 is true
+PASS firstSelectionRect.height > 0 is true
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/editing/selection/ios/no-click-event-when-adjusting-selection-handles.html
@@ -1,0 +1,58 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ useFlexibleViewport=true ] -->
+<html>
+<head>
+<script src="../../../resources/ui-helper.js"></script>
+<script src="../../../resources/js-test.js"></script>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<style>
+body, html {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+}
+
+p {
+    font-size: 24px;
+}
+
+#target {
+    border: 2px solid tomato;
+    padding: 1px;
+}
+</style>
+<script>
+jsTestIsAsync = true;
+
+addEventListener("load", async () => {
+    description("This test verifies that interacting with selection handles doesn't trigger a synthetic click. To manually run the test, select a word in the paragraph above and then adjust either selection handle by a single letter. The selection should not disappear.");
+    document.body.addEventListener("click", () => testFailed("Handled unexpected click event"));
+
+    await UIHelper.longPressElement(document.getElementById("target"));
+    await UIHelper.waitForSelectionToAppear();
+
+    const rect = await UIHelper.getSelectionEndGrabberViewRect();
+    const midpoint = { x: rect.left + rect.width / 2, y: rect.top + rect.height / 2 };
+
+    await UIHelper.sendEventStream(new UIHelper.EventStreamBuilder()
+        .begin(midpoint.x, midpoint.y)
+        .move(midpoint.x - 20, midpoint.y, 0.1)
+        .end()
+        .takeResult());
+
+    await UIHelper.ensurePresentationUpdate();
+    firstSelectionRect = (await UIHelper.getUISelectionViewRects())[0];
+    shouldBeTrue("firstSelectionRect.width > 0");
+    shouldBeTrue("firstSelectionRect.height > 0");
+
+    document.querySelector("p.text").remove();
+    finishJSTest();
+});
+</script>
+</head>
+<body>
+<p class="text">Here’s to the crazy ones. The misfits. The rebels. The <span id="target">troublemakers</span>. The round pegs in the square holes. The ones who see things differently.</p>
+<pre id="description"></pre>
+<pre id="console"></pre>
+</body>
+</html>