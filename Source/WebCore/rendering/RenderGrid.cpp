4031e0e6617a18cfaf6c2c9da5d1825033e2ba5a
Russell Epstein <repstein@apple.com>
2023-05-15 15:05:35 -0700

    Revert ff409be22993. rdar://problem/109170495
    Identifier: 262728.1075@safari-7616.1.14.11-branch

--- Source/WebCore/rendering/RenderGrid.cpp
+++ Source/WebCore/rendering/RenderGrid.cpp
@@ -426,6 +426,9 @@ void RenderGrid::layoutMasonry(bool relayoutChildren)
 
         LayoutUnit availableSpaceForColumns = availableLogicalWidth();
         placeItemsOnGrid(availableSpaceForColumns);
+        // Size in the masonry axis is the masonry content size
+        if (areMasonryColumns() && style().logicalWidth().isAuto())
+            setLogicalWidth(m_masonryLayout.gridContentSize() + borderAndPaddingLogicalWidth());
 
         m_trackSizingAlgorithm.setAvailableSpace(ForColumns, availableSpaceForColumns);
         performGridItemsPreLayout(m_trackSizingAlgorithm);
