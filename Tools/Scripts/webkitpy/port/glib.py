e4a2619c7a4cedbe5fba2bd5ab5143c0e6a0617a
Philippe Normand <philn@igalia.com>
2023-05-15 01:41:29 -0700

    REGRESSION(262138@main): [GStreamer] Broke video rendering when GL is disabled https://bugs.webkit.org/show_bug.cgi?id=256757
    Reviewed by Xabier Rodriguez-Calvar.
    
    The sample received from the Cairo sink is not transfer-full, so we shouldn't adopt its reference.
    Driving by, also allow passthrough of the WEBKIT_GST_DISABLE_GL_SINK environment variable in the
    test harness.
    
    * Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
    (WebCore::MediaPlayerPrivateGStreamer::createVideoSink):
    * Tools/Scripts/webkitpy/port/glib.py:
    (GLibPort.setup_environ_for_server):
    
    Canonical link: https://commits.webkit.org/264064@main

--- Tools/Scripts/webkitpy/port/glib.py
+++ Tools/Scripts/webkitpy/port/glib.py
@@ -94,6 +94,7 @@ class GLibPort(Port):
         self._copy_value_from_environ_if_set(environment, 'WEBKIT_JHBUILD')
         self._copy_value_from_environ_if_set(environment, 'WEBKIT_TOP_LEVEL')
         self._copy_value_from_environ_if_set(environment, 'WEBKIT_DEBUG')
+        self._copy_value_from_environ_if_set(environment, 'WEBKIT_GST_DISABLE_GL_SINK')
         self._copy_value_from_environ_if_set(environment, 'WEBKIT_GST_DMABUF_SINK_DISABLED')
         self._copy_value_from_environ_if_set(environment, 'WEBKIT_GST_HARNESS_DUMP_DIR')
         self._copy_value_from_environ_if_set(environment, 'WEBKIT_GST_USE_PLAYBIN3')
