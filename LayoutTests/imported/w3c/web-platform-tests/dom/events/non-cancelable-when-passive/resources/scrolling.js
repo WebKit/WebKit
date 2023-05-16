8d6c0ab5fc5f1fd63245f951e53001d36bebacb5
Chris Dumez <cdumez@apple.com>
2023-05-16 13:48:24 -0700

    Resync web-platform-tests/dom from upstream https://bugs.webkit.org/show_bug.cgi?id=256812
    Reviewed by Tim Nguyen.
    
    Resync web-platform-tests/dom from upstream cb1e54222a206b4cbb521.
    
    * LayoutTests/imported/w3c/resources/resource-files.json:
    * LayoutTests/imported/w3c/web-platform-tests/dom/*: Updated.
    * LayoutTests/tests-options.json:
    
    Canonical link: https://commits.webkit.org/264126@main

--- LayoutTests/imported/w3c/web-platform-tests/dom/events/non-cancelable-when-passive/resources/scrolling.js
+++ LayoutTests/imported/w3c/web-platform-tests/dom/events/non-cancelable-when-passive/resources/scrolling.js
@@ -1,10 +1,21 @@
-function runTest({target, eventName, passive, expectCancelable}) {
+function raf() {
+  return new Promise((resolve) => {
+    // rAF twice.
+    window.requestAnimationFrame(() => {
+      window.requestAnimationFrame(resolve);
+    });
+  });
+}
+
+async function runTest({target, eventName, passive, expectCancelable}) {
+  await raf();
+
   let cancelable = null;
   let arrived = false;
   target.addEventListener(eventName, function (event) {
     cancelable = event.cancelable;
     arrived = true;
-  }, {passive});
+  }, {passive:passive, once:true});
 
   promise_test(async (t) => {
     t.add_cleanup(() => {
@@ -14,9 +25,10 @@ function runTest({target, eventName, passive, expectCancelable}) {
     const pos_y = Math.floor(window.innerHeight / 2);
     const delta_x = 0;
     const delta_y = 100;
+
     await new test_driver.Actions()
       .scroll(pos_x, pos_y, delta_x, delta_y).send();
-    await waitFor(() => arrived);
+    await t.step_wait(() => arrived, `Didn't get event ${eventName} on ${target.localName}`);
     assert_equals(cancelable, expectCancelable);
   });
 }
