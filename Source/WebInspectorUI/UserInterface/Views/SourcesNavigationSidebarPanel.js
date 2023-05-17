7321a4e41121936c3433c49fbf39da6321419b21
Devin Rousso <hi@devinrousso.com>
2023-05-17 10:21:53 -0700

    Web Inspector: REGRESSION(?): Sources: Worker aren't shown when switching grouping modes https://bugs.webkit.org/show_bug.cgi?id=256502
    Reviewed by Patrick Angle.
    
    * Source/WebInspectorUI/UserInterface/Views/SourcesNavigationSidebarPanel.js:
    (WI.SourcesNavigationSidebarPanel.prototype._addWorkerTargetWithMainResource):
    (WI.SourcesNavigationSidebarPanel.prototype._handleResourceGroupingModeChanged):
    
    Canonical link: https://commits.webkit.org/264158@main

--- Source/WebInspectorUI/UserInterface/Views/SourcesNavigationSidebarPanel.js
+++ Source/WebInspectorUI/UserInterface/Views/SourcesNavigationSidebarPanel.js
@@ -1311,6 +1311,9 @@ WI.SourcesNavigationSidebarPanel = class SourcesNavigationSidebarPanel extends W
     {
         console.assert(target.type === WI.TargetType.Worker || target.type === WI.TargetType.ServiceWorker);
 
+        if (this._workerTargetTreeElementMap.has(target))
+            return;
+
         let targetTreeElement = new WI.WorkerTreeElement(target);
         this._workerTargetTreeElementMap.set(target, targetTreeElement);
 
@@ -2376,6 +2379,14 @@ WI.SourcesNavigationSidebarPanel = class SourcesNavigationSidebarPanel extends W
             for (let frame of WI.networkManager.frames) {
                 if (frame !== mainFrame)
                     this._addResourcesRecursivelyForFrame(frame);
+            }
+        }
+
+        for (let target of WI.targets) {
+            if (target !== WI.pageTarget) {
+                this._addWorkerTargetWithMainResource(target);
+                this._addBreakpointsForSourceCode(target.mainResource);
+                this._addIssuesForSourceCode(target.mainResource);
             }
         }
 
