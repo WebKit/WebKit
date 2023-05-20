340b0f1155192cc2bddf1df560b345a56a4b2890
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2023-05-20 08:57:33 -0700

    [Multicolumn] Handle multicol intrinsic inline-size changes
    https://bugs.webkit.org/show_bug.cgi?id=257027
    
    Reviewed by Alan Baradlay.
    
    This patch aligns WebKit with Blink / Chromium and Gecko / Firefox.
    
    Merge: https://chromium.googlesource.com/chromium/src.git/+/209ad9840b0152a427e9d2b29318cfa47ca80d92
    
    Currently we used to store intrinsic size in column sets but if any
    change happen, 'column sets' do not get dirtied so this patch will
    reset intrinsic size of column sets to 0 to fix this.
    
    * Source/WebCore/rendering/RenderFragmentContainer.cpp:
    (RenderFragmentContainer::computeIntrinsicLogicalWidths): As above
    * LayoutTests/TestExpectations: Remove 'failing' test
    
    Canonical link: https://commits.webkit.org/264292@main

--- Source/WebCore/rendering/RenderFragmentContainer.cpp
+++ Source/WebCore/rendering/RenderFragmentContainer.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2011 Adobe Systems Incorporated. All rights reserved.
+ * Copyright (C) 2020 Google  Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -373,9 +374,8 @@ void RenderFragmentContainer::computeIntrinsicLogicalWidths(LayoutUnit& minLogic
         RenderBlockFlow::computeIntrinsicLogicalWidths(minLogicalWidth, maxLogicalWidth);
         return;
     }
-
-    minLogicalWidth = m_fragmentedFlow->minPreferredLogicalWidth();
-    maxLogicalWidth = m_fragmentedFlow->maxPreferredLogicalWidth();
+    maxLogicalWidth = { };
+    minLogicalWidth = { };
 }
 
 void RenderFragmentContainer::computePreferredLogicalWidths()
