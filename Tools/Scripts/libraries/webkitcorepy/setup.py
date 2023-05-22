bedbee4cf2dca82297c470f86bfa6e8c3abdc53f
Jonathan Bedard <jbedard@apple.com>
2023-05-22 15:59:47 -0700

    Cherry-pick 264321@main (1c10cac85558). rdar://109487762
        [webkitcorepy] Fix race condition in FileLock
        https://bugs.webkit.org/show_bug.cgi?id=256931
        rdar://109487762
    
        Reviewed by Alexey Proskuryakov.
    
        * Tools/Scripts/libraries/webkitcorepy/setup.py: Bump version.
        * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/__init__.py: Ditto.
        * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/file_lock.py:
        (FileLock.is_process_running): Check if a specific PID is running.
        (FileLock.acquire): Create and acquire lock in a single operation on Unix systems.
        Save the current PID to the file, which allows future processes to ignore this lock
        if the PID saved in it is dead. For systems which support it, use O_EXLOCK and interrupts
        instead of a spinlock. Raise exceptions when we fail to acquire the lock.
        (FileLock.release): Delete the lockfile before releasing it on Unix systems. Systems using
        O_EXLOCK simply need to close their file handle.
        (FileLock.__exit__): Don't raise a 2nd exception if __enter__ has raised one.
        * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/tests/file_lock_unittest.py:
        (action):
        (FileLockTestCase.test_locked):
        (FileLockTestCase.test_locked_timeout):
        (FileLockTestCase.test_double):
        (FileLockTestCase.test_race):
    
        Canonical link: https://commits.webkit.org/264321@main
    
    Canonical link: https://commits.webkit.org/259548.769@safari-7615-branch

--- Tools/Scripts/libraries/webkitcorepy/setup.py
+++ Tools/Scripts/libraries/webkitcorepy/setup.py
@@ -30,7 +30,7 @@ def readme():
 
 setup(
     name='webkitcorepy',
-    version='0.14.3',
+    version='0.14.4',
     description='Library containing various Python support classes and functions.',
     long_description=readme(),
     classifiers=[
