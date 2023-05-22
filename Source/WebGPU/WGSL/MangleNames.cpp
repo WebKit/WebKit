4d5fe1667498ade1a7ad5edf54d4542e852f6f15
Tadeu Zagallo <tzagallo@apple.com>
2023-05-22 00:07:18 -0700

    [WGSL] Global variables don't need to be mangled https://bugs.webkit.org/show_bug.cgi?id=257025 rdar://109564753
    Reviewed by Myles C. Maxfield.
    
    The final program does not contain any global variables,
    so we don't need to mangle them
    
    * Source/WebGPU/WGSL/MangleNames.cpp:
    (WGSL::MangledName::toString const):
    (WGSL::NameManglerVisitor::visitFunctionBody):
    (WGSL::NameManglerVisitor::visit):
    
    Canonical link: https://commits.webkit.org/264313@main

--- Source/WebGPU/WGSL/MangleNames.cpp
+++ Source/WebGPU/WGSL/MangleNames.cpp
@@ -40,7 +40,6 @@ struct MangledName {
     enum Kind : uint8_t {
         Type,
         Local,
-        Global,
         Parameter,
         Function,
         Field,
@@ -56,7 +55,6 @@ struct MangledName {
         static const ASCIILiteral prefixes[] = {
             "type"_s,
             "local"_s,
-            "global"_s,
             "parameter"_s,
             "function"_s,
             "field"_s,
@@ -138,7 +136,11 @@ void NameManglerVisitor::visitFunctionBody(AST::Function& function)
         introduceVariable(parameter.name(), MangledName::Parameter);
     }
 
+    // It's important that we call the base visitor here directly, otherwise
+    // our overwritten visitor will introduce a new ContextScope for the compound
+    // statement, which would allow shadowing the function's parameters
     AST::Visitor::visit(function.body());
+
     if (function.maybeReturnType())
         AST::Visitor::visit(*function.maybeReturnType());
 }
@@ -179,8 +181,6 @@ void NameManglerVisitor::visit(AST::Variable& variable)
             break;
         }
     }
-
-    visitVariableDeclaration(variable, MangledName::Global);
 
     const String& mangledName = variable.name();
 
