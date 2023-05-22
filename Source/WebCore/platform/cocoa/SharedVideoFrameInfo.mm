46ea28d672831cac886966dd100c4bfe4d9ae418
Youenn Fablet <youennf@gmail.com>
2023-05-22 13:54:16 -0700

    [GPUP][CoreIPC] Integer overflow in SharedVideoFrameInfo::storageSize leading to OOB read rdar://107023292
    Reviewed by Eric Carlson.
    
    Compute with safeMultitply/safeAdd the total size of the frame.
    If there is an overflow, we now fail the decoding of SharedVideoFrameInfo.
    
    Covered by provided IPC test.
    
    * LayoutTests/ipc/shared-video-frame-size-expected.txt: Added.
    * LayoutTests/ipc/shared-video-frame-size.html: Added.
    * Source/WebCore/platform/cocoa/SharedVideoFrameInfo.mm:
    (WebCore::SharedVideoFrameInfo::storageSize const):
    (WebCore::SharedVideoFrameInfo::decode):
    
    Originally-landed-as: 259548.597@safari-7615-branch (7811f6f9e18f). rdar://107023292
    Canonical link: https://commits.webkit.org/264371@main

--- Source/WebCore/platform/cocoa/SharedVideoFrameInfo.mm
+++ Source/WebCore/platform/cocoa/SharedVideoFrameInfo.mm
@@ -72,7 +72,19 @@ bool SharedVideoFrameInfo::isReadWriteSupported() const
 
 size_t SharedVideoFrameInfo::storageSize() const
 {
-    return (m_bytesPerRow * m_height) + (m_bytesPerRowPlaneB * m_heightPlaneB) + sizeof(SharedVideoFrameInfo);
+    size_t sizePlaneA;
+    if (!WTF::safeMultiply(m_bytesPerRow, m_height, sizePlaneA))
+        return 0;
+
+    size_t sizePlaneB;
+    if (!WTF::safeMultiply(m_bytesPerRowPlaneB, m_heightPlaneB, sizePlaneB))
+        return 0;
+
+    size_t size;
+    if (!WTF::safeAdd(sizePlaneA, sizePlaneB, size) || !WTF::safeAdd(size, sizeof(SharedVideoFrameInfo), size))
+        return 0;
+
+    return size;
 }
 
 void SharedVideoFrameInfo::encode(uint8_t* destination)
@@ -129,7 +141,11 @@ std::optional<SharedVideoFrameInfo> SharedVideoFrameInfo::decode(std::span<const
     if (!bytesPerRowPlaneB)
         return std::nullopt;
 
-    return SharedVideoFrameInfo { *bufferType, *width, *height, *bytesPerRow , *widthPlaneB, *heightPlaneB, *bytesPerRowPlaneB };
+    SharedVideoFrameInfo info { *bufferType, *width, *height, *bytesPerRow , *widthPlaneB, *heightPlaneB, *bytesPerRowPlaneB };
+    if (!info.storageSize())
+        return std::nullopt;
+
+    return info;
 }
 
 static const uint8_t* copyToCVPixelBufferPlane(CVPixelBufferRef pixelBuffer, size_t planeIndex, const uint8_t* source, size_t height, uint32_t bytesPerRowSource)
