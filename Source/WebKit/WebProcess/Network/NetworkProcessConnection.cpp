ec81069dd2e6d97fe79ae294b0232795384aa976
Youenn Fablet <youennf@gmail.com>
2023-05-23 22:12:50 -0700

    [root] 615.1.17: CrashTracer: com.apple.WebKit:  WTF::Detail::CallableWrapper<WebKit::NetworkProcessConnection::broadcastConsoleMessage(JSC::MessageSource, JSC::MessageLevel, WTF::String const&)::$_9, void, WebCore::Page&>::call rdar://103903569
    Reviewed by Alex Christensen.
    
    Not all pages might have a corresponding WebPage.
    iTo wrokaround this, we can directly go from Page to main frame document to print console messages.
    
    * Source/WebKit/WebProcess/Network/NetworkProcessConnection.cpp:
    (WebKit::NetworkProcessConnection::broadcastConsoleMessage):
    
    Originally-landed-as: 259548.510@safari-7615-branch (e41d370e4050). rdar://109463439
    Canonical link: https://commits.webkit.org/264459@main

--- Source/WebKit/WebProcess/Network/NetworkProcessConnection.cpp
+++ Source/WebKit/WebProcess/Network/NetworkProcessConnection.cpp
@@ -335,9 +335,10 @@ void NetworkProcessConnection::broadcastConsoleMessage(MessageSource source, Mes
 {
     FAST_RETURN_IF_NO_FRONTENDS(void());
 
-    Page::forEachPage([&] (Page& page) {
-        if (auto* webPage = WebPage::fromCorePage(page))
-            webPage->mainWebFrame().addConsoleMessage(source, level, message);
+    Page::forEachPage([&] (auto& page) {
+        if (auto* localMainFrame = dynamicDowncast<LocalFrame>(page.mainFrame()))
+            if (auto* document = localMainFrame->document())
+                document->addConsoleMessage(source, level, message);
     });
 }
 
