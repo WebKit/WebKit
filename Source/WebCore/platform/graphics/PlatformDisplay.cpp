d4b48c26573684c9cb7b5612f92b7c4f4b9fc784
Carlos Garcia Campos <cgarcia@igalia.com>
2023-06-02 01:49:34 -0700

    [GStreamer] Do not activate and fill the gst GL context https://bugs.webkit.org/show_bug.cgi?id=257599
    Reviewed by Miguel Gomez.
    
    It's only used as a sharing context, so it doesn't need to be activated
    and filled. This way we avoid making the sharing context the current
    one in the main thread forever.
    
    * Source/WebCore/platform/graphics/PlatformDisplay.cpp:
    (WebCore::PlatformDisplay::clearSharingGLContext):
    (WebCore::PlatformDisplay::terminateEGLDisplay):
    * Source/WebCore/platform/graphics/gstreamer/PlatformDisplayGStreamer.cpp:
    (WebCore::PlatformDisplay::gstGLDisplay const):
    (WebCore::PlatformDisplay::gstGLContext const):
    (createGstGLDisplay): Deleted.
    (PlatformDisplay::tryEnsureGstGLContext const): Deleted.
    (PlatformDisplay::gstGLDisplay const): Deleted.
    (PlatformDisplay::gstGLContext const): Deleted.
    
    Canonical link: https://commits.webkit.org/264826@main

--- Source/WebCore/platform/graphics/PlatformDisplay.cpp
+++ Source/WebCore/platform/graphics/PlatformDisplay.cpp
@@ -254,6 +254,9 @@ GLContext* PlatformDisplay::sharingGLContext()
 
 void PlatformDisplay::clearSharingGLContext()
 {
+#if ENABLE(VIDEO) && USE(GSTREAMER_GL)
+    m_gstGLContext = nullptr;
+#endif
     m_sharingGLContext = nullptr;
 }
 #endif
@@ -351,7 +354,6 @@ void PlatformDisplay::terminateEGLDisplay()
 {
 #if ENABLE(VIDEO) && USE(GSTREAMER_GL)
     m_gstGLDisplay = nullptr;
-    m_gstGLContext = nullptr;
 #endif
     clearSharingGLContext();
     ASSERT(m_eglDisplayInitialized);
