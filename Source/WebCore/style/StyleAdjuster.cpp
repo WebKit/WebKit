b3a3c589507eb4de25b0be22473e4bacf690922a
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2023-06-06 06:02:15 -0700

    Stop propagating text decorations on outermost SVG roots
    https://bugs.webkit.org/show_bug.cgi?id=248567
    rdar://problem/103093226
    
    Reviewed by Antti Koivisto.
    
    This patch aligns WebKit with Gecko / Firefox and Blink / Chromium.
    
    Merge: https://src.chromium.org/viewvc/blink?view=revision&revision=199132
    
    Outermost SVG roots should be consider atomic inline-level, and hence
    text decorations should not propagate into them from the outside.
    
    * Source/WebCore/style/StyleAdjuster.cpp:
    (isOutermostSVGElement): Add new static function
    (shouldInheritTextDecorationsInEffect): Update as per commit
    * LayoutTests/svg/text/text-decoration-propagation.html: Add Test Case
    * LayoutTests/svg/text/text-decoration-propagation-expected.html: Add Test Case Expectation
    * LayoutTests/svg/text/text-decoration-propagation-2.html: Add Test Case
    * LayoutTests/svg/text/text-decoration-propagation-2-expected.html: Add Test Case Expectation
    
    Canonical link: https://commits.webkit.org/264894@main

--- Source/WebCore/style/StyleAdjuster.cpp
+++ Source/WebCore/style/StyleAdjuster.cpp
@@ -8,7 +8,7 @@
  * Copyright (C) 2008, 2009 Torch Mobile Inc. All rights reserved. (http://www.torchmobile.com/)
  * Copyright (c) 2011, Code Aurora Forum. All rights reserved.
  * Copyright (C) Research In Motion Limited 2011. All rights reserved.
- * Copyright (C) 2012, 2013 Google Inc. All rights reserved.
+ * Copyright (C) 2012-2015 Google Inc. All rights reserved.
  * Copyright (C) 2014, 2020, 2022 Igalia S.L.
  *
  * This library is free software; you can redistribute it and/or
@@ -155,6 +155,11 @@ static DisplayType equivalentBlockDisplay(const RenderStyle& style)
     return DisplayType::Block;
 }
 
+static bool isOutermostSVGElement(const Element* element)
+{
+    return element && element->isSVGElement() && downcast<SVGElement>(*element).isOutermostSVGSVGElement();
+}
+
 static bool shouldInheritTextDecorationsInEffect(const RenderStyle& style, const Element* element)
 {
     if (style.isFloating() || style.hasOutOfFlowPosition())
@@ -166,6 +171,10 @@ static bool shouldInheritTextDecorationsInEffect(const RenderStyle& style, const
         auto* parentNode = element->parentNode();
         return parentNode && parentNode->isUserAgentShadowRoot();
     }();
+
+    // Outermost <svg> roots are considered to be atomic inline-level.
+    if (isOutermostSVGElement(element))
+        return false;
 
     // There is no other good way to prevent decorations from affecting user agent shadow trees.
     if (isAtUserAgentShadowBoundary)
