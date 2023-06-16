df148384b18a40059f52a6ad13f05f7c0612ade9
Nikolas Zimmermann <nzimmermann@igalia.com>
2023-06-16 04:45:24 -0700

    Mask clipping determination insufficient for <use> elements https://bugs.webkit.org/show_bug.cgi?id=258168
    Reviewed by Rob Buis.
    
    To determine if we need mask clipping or can use path clipping,
    the renderer is queried for certain information. This is not
    sufficient for <use> renderers -- the logic needs to extend to
    the referenced renderer as well.
    
    Previously when referencing e.g. a <text> element that is itself
    clipped, from an <use> element (that is a child of <clipPath>)
    we determined that path clipping is possible, leading to a
    fully clipped object (net result: nothing visible).
    
    Fix that issue, and handle <use> elements in the clip mask determination.
    
    Covered by two new tests, exercising the <use> clipping peculiarities.
    
    * LayoutTests/svg/clip-path/clip-path-use-referencing-clipped-text-expected.html: Added.
    * LayoutTests/svg/clip-path/clip-path-use-referencing-clipped-text.html: Added.
    * LayoutTests/svg/clip-path/clip-path-use-referencing-text-expected.html: Added.
    * LayoutTests/svg/clip-path/clip-path-use-referencing-text.html: Added.
    Imported from Blink.
    
    * Source/WebCore/rendering/svg/RenderSVGResourceClipper.cpp:
    (WebCore::RenderSVGResourceClipper::pathOnlyClipping):
    
    Refactor the mask clipping determination logic into a helper lambda.
    Re-use that lambda to check if the <use> referenced clip renderer
    is required to use mask clipping, or if path clipping is possible.
    
    Canonical link: https://commits.webkit.org/265238@main

--- Source/WebCore/rendering/svg/RenderSVGResourceClipper.cpp
+++ Source/WebCore/rendering/svg/RenderSVGResourceClipper.cpp
@@ -93,31 +93,46 @@ bool RenderSVGResourceClipper::pathOnlyClipping(GraphicsContext& context, const 
     WindRule clipRule = WindRule::NonZero;
     Path clipPath;
 
+    auto rendererRequiresMaskClipping = [&clipPath](RenderObject& renderer) {
+        // Only shapes or paths are supported for direct clipping. We need to fall back to masking for texts.
+        if (is<RenderSVGText>(renderer))
+            return true;
+        auto& style = renderer.style();
+        if (style.display() == DisplayType::None || style.visibility() != Visibility::Visible)
+            return false;
+        // Current shape in clip-path gets clipped too. Fall back to masking.
+        if (style.clipPath())
+            return true;
+        // Fall back to masking if there is more than one clipping path.
+        if (!clipPath.isEmpty())
+            return true;
+        return false;
+    };
+
     // If clip-path only contains one visible shape or path, we can use path-based clipping. Invisible
     // shapes don't affect the clipping and can be ignored. If clip-path contains more than one
     // visible shape, the additive clipping may not work, caused by the clipRule. EvenOdd
     // as well as NonZero can cause self-clipping of the elements.
     // See also http://www.w3.org/TR/SVG/painting.html#FillRuleProperty
     for (Node* childNode = clipPathElement().firstChild(); childNode; childNode = childNode->nextSibling()) {
-        auto* renderer = childNode->renderer();
+        auto* graphicsElement = dynamicDowncast<SVGGraphicsElement>(*childNode);
+        if (!graphicsElement)
+            continue;
+        auto* renderer = graphicsElement->renderer();
         if (!renderer)
             continue;
-        // Only shapes or paths are supported for direct clipping. We need to fall back to masking for texts.
-        if (is<RenderSVGText>(renderer))
+        if (rendererRequiresMaskClipping(*renderer))
             return false;
-        if (!is<SVGGraphicsElement>(*childNode))
-            continue;
-        auto& style = renderer->style();
-        if (style.display() == DisplayType::None || style.visibility() != Visibility::Visible)
-            continue;
-        // Current shape in clip-path gets clipped too. Fall back to masking.
-        if (style.clipPath())
-            return false;
-        // Fall back to masking if there is more than one clipping path.
-        if (!clipPath.isEmpty())
-            return false;
-        clipPath = downcast<SVGGraphicsElement>(*childNode).toClipPath();
-        clipRule = style.svgStyle().clipRule();
+
+        // For <use> elements, delegate the decision whether to use mask clipping or not to the referenced element.
+        if (auto* useElement = dynamicDowncast<SVGUseElement>(graphicsElement)) {
+            auto* clipChildRenderer = useElement->rendererClipChild();
+            if (clipChildRenderer && rendererRequiresMaskClipping(*clipChildRenderer))
+                return false;
+        }
+
+        clipPath = graphicsElement->toClipPath();
+        clipRule = renderer->style().svgStyle().clipRule();
     }
 
     // Only one visible shape/path was found. Directly continue clipping and transform the content to userspace if necessary.
