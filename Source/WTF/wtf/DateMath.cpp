1f84372b0336751caf1a2ae482a1331e754e6b51
Yusuke Suzuki <ysuzuki@apple.com>
2023-06-16 15:55:54 -0700

    [JSC] Add V8 style ICU DST offset cache https://bugs.webkit.org/show_bug.cgi?id=258186 rdar://110879575
    Reviewed by Mark Lam.
    
    This patch adds V8 style ICU DST offset cache into our DateCache.
    Previously, we had one-way cache. But in some benchmarks, we observed
    that frequent fallback to ICU due to cache miss. So we extend the cache
    to 32-way LRU, which is what V8 is doing. So long as we hit this cache,
    we do not need to query to ICU about DST offset.
    
    * Source/JavaScriptCore/runtime/JSDateMath.cpp:
    (JSC::DateCache::DSTCache::leastRecentlyUsed):
    (JSC::DateCache::DSTCache::extendTheAfterCache):
    (JSC::DateCache::DSTCache::localTimeOffset):
    (JSC::DateCache::resetIfNecessarySlow):
    (JSC::DateCache::localTimeOffset): Deleted.
    * Source/JavaScriptCore/runtime/JSDateMath.h:
    (JSC::LocalTimeOffsetCache::isEmpty):
    (JSC::DateCache::DSTCache::DSTCache):
    (JSC::DateCache::DSTCache::bumpEpoch):
    (JSC::DateCache::DSTCache::reset):
    (JSC::DateCache::localTimeOffset):
    
    Canonical link: https://commits.webkit.org/265258@main

--- Source/WTF/wtf/DateMath.cpp
+++ Source/WTF/wtf/DateMath.cpp
@@ -118,6 +118,10 @@ const int firstDayOfMonth[2][12] = {
     {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335}
 };
 
+const int8_t daysInMonths[12] = {
+    31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+};
+
 #if !OS(WINDOWS) || HAVE(TM_GMTOFF)
 static inline void getLocalTime(const time_t* localTime, struct tm* localTM)
 {
