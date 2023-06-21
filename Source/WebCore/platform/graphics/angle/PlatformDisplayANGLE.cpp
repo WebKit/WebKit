WebKit/WebKit

commit b30a03a4c8a229deacb72a6e4fa46f6469df634f

[GTK][WPE] Stop using DMA-BUF for WebGL in the web process https://bugs.webkit.org/show_bug.cgi?id=258119
Reviewed by Alejandro G. Castro.

DMA-BUF and GBM are not available in all the platforms, so we can
extract the drawing texture from the ANGLE context and pass it to the
texture mapper to be composited like we did before switching to ANGLE.

To be able to use the texture directly we need to make sure that the
context created by ANGLE uses the same EGLDisplay and sharing context as
the compositor context. To get the same EGLDisplay we only need to
ensure we pass the same parameters to EGL_GetPlatformDisplayEXT than the
ones used by the shared display for compositing. To use the same sharing
context we need to create an ANGLE context wrapping the sharing context
used by the compositor context. That's possible by using
EGL_EXTERNAL_CONTEXT_ANGLE context parameter when creating the ANGLE
sharing context and EGL_CONTEXT_VIRTUALIZATION_GROUP_ANGLE when creating
the WebGL ANGLE context.

By using the texture directly we no longer need to use DMA-BUF for WebGL
in the web process, but we keep the GBM implementation for the GPU
process.

* LayoutTests/platform/glib/TestExpectations:
* LayoutTests/platform/glib/inspector/canvas/recording-webgl-full-expected.txt: Added.
* Source/WebCore/PlatformWin.cmake:
* Source/WebCore/SourcesGTK.txt:
* Source/WebCore/SourcesWPE.txt:
* Source/WebCore/platform/TextureMapper.cmake:
* Source/WebCore/platform/graphics/PlatformDisplay.cpp:
(WebCore::PlatformDisplay::clearSharingGLContext):
* Source/WebCore/platform/graphics/PlatformDisplay.h:
* Source/WebCore/platform/graphics/angle/PlatformDisplayANGLE.cpp: Added.
(WebCore::PlatformDisplay::angleEGLDisplay const):
(WebCore::PlatformDisplay::angleSharingGLContext):
(WebCore::PlatformDisplay::clearANGLESharingGLContext):
* Source/WebCore/platform/graphics/egl/PlatformDisplaySurfaceless.cpp:
(WebCore::PlatformDisplaySurfaceless::PlatformDisplaySurfaceless):

Source/WebCore/platform/graphics/angle/PlatformDisplayANGLE.cpp
@@ -1,0 +1,127 @@
+/*
+ * Copyright (C) 2023 Igalia S.L
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * aint with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "config.h"
+#include "PlatformDisplay.h"
+
+#if ENABLE(WEBGL)
+#include "ANGLEHeaders.h"
+#include "GLContext.h"
+#include "Logging.h"
+
+namespace WebCore {
+
+EGLDisplay PlatformDisplay::angleEGLDisplay() const
+{
+#if PLATFORM(WIN)
+    return eglDisplay();
+#else
+    if (m_angleEGLDisplay != EGL_NO_DISPLAY)
+        return m_angleEGLDisplay;
+
+    auto display = eglDisplay();
+    if (display == EGL_NO_DISPLAY)
+        return EGL_NO_DISPLAY;
+
+    if (!m_anglePlatform)
+        return EGL_NO_DISPLAY;
+
+    Vector<EGLint> displayAttributes {
+        EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_OPENGLES_ANGLE,
+        EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_DEVICE_TYPE_EGL_ANGLE,
+        EGL_PLATFORM_ANGLE_NATIVE_PLATFORM_TYPE_ANGLE, m_anglePlatform.value(),
+        EGL_NONE,
+    };
+
+    auto angleDisplay = EGL_GetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, m_angleNativeDisplay ? m_angleNativeDisplay : EGL_DEFAULT_DISPLAY, displayAttributes.data());
+    if (angleDisplay == EGL_NO_DISPLAY)
+        return EGL_NO_DISPLAY;
+
+    EGLint majorVersion, minorVersion;
+    if (EGL_Initialize(angleDisplay, &majorVersion, &minorVersion) == EGL_FALSE) {
+        LOG(WebGL, "EGLDisplay Initialization failed.");
+        return EGL_NO_DISPLAY;
+    }
+    LOG(WebGL, "ANGLE initialised Major: %d Minor: %d", majorVersion, minorVersion);
+
+    m_angleEGLDisplay = angleDisplay;
+    return m_angleEGLDisplay;
+#endif
+}
+
+EGLContext PlatformDisplay::angleSharingGLContext()
+{
+#if PLATFORM(WIN)
+    return sharingGLContext();
+#else
+    if (m_angleSharingGLContext != EGL_NO_CONTEXT)
+        return m_angleSharingGLContext;
+
+    ASSERT(m_angleEGLDisplay != EGL_NO_DISPLAY);
+    auto sharingContext = sharingGLContext();
+    if (!sharingContext)
+        return EGL_NO_CONTEXT;
+
+    EGLint configAttributes[] = {
+        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+        EGL_SURFACE_TYPE, type() == Type::Surfaceless ? EGL_PBUFFER_BIT : EGL_WINDOW_BIT,
+        EGL_RED_SIZE, 8,
+        EGL_GREEN_SIZE, 8,
+        EGL_BLUE_SIZE, 8,
+        EGL_ALPHA_SIZE, 8,
+        EGL_DEPTH_SIZE, 0,
+        EGL_STENCIL_SIZE, 0,
+        EGL_NONE
+    };
+    EGLConfig config;
+    EGLint numberConfigsReturned = 0;
+    EGL_ChooseConfig(m_angleEGLDisplay, configAttributes, &config, 1, &numberConfigsReturned);
+    if (numberConfigsReturned != 1)
+        return EGL_NO_CONTEXT;
+
+    GLContext::ScopedGLContextCurrent scopedCurrent(*sharingContext);
+    EGLint contextAttributes[] = {
+        EGL_CONTEXT_CLIENT_VERSION, 2,
+        EGL_EXTERNAL_CONTEXT_ANGLE, EGL_TRUE,
+        EGL_EXTERNAL_CONTEXT_SAVE_STATE_ANGLE, EGL_TRUE,
+        EGL_NONE
+    };
+    m_angleSharingGLContext = EGL_CreateContext(m_angleEGLDisplay, config, EGL_NO_CONTEXT, contextAttributes);
+    return m_angleSharingGLContext;
+#endif
+}
+
+#if ENABLE(WEBGL) && !PLATFORM(WIN)
+void PlatformDisplay::clearANGLESharingGLContext()
+{
+    if (m_angleSharingGLContext == EGL_NO_CONTEXT)
+        return;
+
+    ASSERT(m_angleEGLDisplay);
+    ASSERT(m_sharingGLContext);
+    GLContext::ScopedGLContextCurrent scopedCurrent(*m_sharingGLContext);
+    EGL_DestroyContext(m_angleEGLDisplay, m_angleSharingGLContext);
+    m_angleSharingGLContext = EGL_NO_CONTEXT;
+}
+#endif
+
+
+} // namespace WebCore
+
+#endif // ENABLE(WEBGL)

LayoutTests/platform/glib/TestExpectations
@@ -1692,6 +1692,12 @@ webkit.org/b/251106 webgl/2.0.y/conformance/offscreencanvas/context-lost-worker.
 webkit.org/b/251106 webgl/2.0.y/conformance/offscreencanvas/methods-worker.html [ Failure Pass ]
 webkit.org/b/251106 webgl/2.0.y/conformance/offscreencanvas/offscreencanvas-timer-query.html [ Pass Timeout ]
 
+# Preserve drawing buffer enabled.
+webkit.org/b/258294 webgl/1.0.x/conformance/extensions/angle-instanced-arrays.html [ Failure ]
+webkit.org/b/258294 webgl/1.0.x/conformance/extensions/webgl-multi-draw.html [ Failure ]
+
+# Fails when using two textures.
+webkit.org/b/258296 webrtc/canvas-to-peer-connection.html [ Failure ]
 
 #////////////////////////////////////////////////////////////////////////////////////////
 # End of WebGL-related bugs

Source/WebCore/PlatformWin.cmake
@@ -51,6 +51,8 @@ list(APPEND WebCore_SOURCES
 
     platform/graphics/PlatformDisplay.cpp
 
+    platform/graphics/angle/PlatformDisplayANGLE.cpp
+
     platform/graphics/egl/GLContext.cpp
 
     platform/graphics/opentype/OpenTypeUtilities.cpp

Source/WebCore/platform/TextureMapper.cmake
@@ -136,6 +136,8 @@ endif ()
 if (ENABLE_WEBGL)
     list(APPEND WebCore_SOURCES
         platform/graphics/nicosia/texmap/NicosiaGCGLANGLELayer.cpp
+
+        platform/graphics/texmap/GraphicsContextGLTextureMapperANGLENicosia.cpp
     )
 endif ()
 
@@ -159,8 +161,6 @@ if (USE_GBM)
         platform/graphics/gbm/DMABufReleaseFlag.h
         platform/graphics/gbm/GBMBufferSwapchain.h
         platform/graphics/gbm/GBMDevice.h
-        platform/graphics/gbm/GraphicsContextGLFallback.h
         platform/graphics/gbm/GraphicsContextGLGBM.h
-        platform/graphics/gbm/GraphicsContextGLGBMTextureMapper.h
     )
 endif ()

Source/WebCore/platform/graphics/PlatformDisplay.cpp
@@ -257,6 +257,9 @@ void PlatformDisplay::clearSharingGLContext()
 #if ENABLE(VIDEO) && USE(GSTREAMER_GL)
     m_gstGLContext = nullptr;
 #endif
+#if ENABLE(WEBGL) && !PLATFORM(WIN)
+    clearANGLESharingGLContext();
+#endif
     m_sharingGLContext = nullptr;
 }
 #endif

Source/WebCore/platform/graphics/PlatformDisplay.h
@@ -122,6 +122,11 @@ public:
 #if PLATFORM(GTK)
     virtual EGLDisplay gtkEGLDisplay() { return nullptr; }
 #endif
+
+#if ENABLE(WEBGL)
+    EGLDisplay angleEGLDisplay() const;
+    EGLContext angleSharingGLContext();
+#endif
 #endif
 
 #if ENABLE(VIDEO) && USE(GSTREAMER_GL)
@@ -162,6 +167,11 @@ protected:
     std::optional<String> m_drmDeviceFile;
     std::optional<String> m_drmRenderNodeFile;
 #endif
+
+#if ENABLE(WEBGL) && !PLATFORM(WIN)
+    std::optional<int> m_anglePlatform;
+    void* m_angleNativeDisplay { nullptr };
+#endif
 #endif
 
 #if USE(LCMS)
@@ -177,6 +187,10 @@ protected:
 private:
     static std::unique_ptr<PlatformDisplay> createPlatformDisplay();
 
+#if ENABLE(WEBGL) && !PLATFORM(WIN)
+    void clearANGLESharingGLContext();
+#endif
+
 #if USE(EGL)
     void terminateEGLDisplay();
 #if USE(GBM)
@@ -187,6 +201,10 @@ private:
     int m_eglMajorVersion { 0 };
     int m_eglMinorVersion { 0 };
     EGLExtensions m_eglExtensions;
+#if ENABLE(WEBGL) && !PLATFORM(WIN)
+    mutable EGLDisplay m_angleEGLDisplay { nullptr };
+    EGLContext m_angleSharingGLContext { nullptr };
+#endif
 #endif
 
 #if ENABLE(VIDEO) && USE(GSTREAMER_GL)

Source/WebCore/platform/graphics/egl/GLContext.h
@@ -24,21 +24,12 @@
 #include "PlatformDisplay.h"
 #include <wtf/Noncopyable.h>
 
-#if !PLATFORM(GTK)
-// FIXME: For now default to the GBM EGL platform, but this should really be
-// somehow deducible from the build configuration. This is needed with libepoxy
-// as it could have been configured with X11 support enabled, resulting in
-// transitive inclusions of headers with definitions that clash with WebCore.
-#define __GBM__ 1
-#if USE(LIBEPOXY)
-#include <epoxy/egl.h>
-#else // !USE(LIBEPOXY)
+#if !PLATFORM(GTK) && !PLATFORM(WPE)
 #include <EGL/eglplatform.h>
-#endif // USE(LIBEPOXY)
 typedef EGLNativeWindowType GLNativeWindowType;
-#else // !USE(EGL) || PLATFORM(GTK)
+#else
 typedef uint64_t GLNativeWindowType;
-#endif // !PLATFORM(GTK)
+#endif
 
 #if PLATFORM(X11)
 #include "XUniqueResource.h"

commit d192a99af4640e4cd76b5429214f29252f2fa590

REGRESSION(265349@main) [Win] WebGL doesn't work https://bugs.webkit.org/show_bug.cgi?id=258367
Unreviewed fix. WebGL context creation was failing after 265349@main
for Windows port because the specified shared GL context was wrong.
It should be a EGLContext not a GLContext pointer.

* Source/WebCore/platform/graphics/angle/PlatformDisplayANGLE.cpp:
(WebCore::PlatformDisplay::angleSharingGLContext):

Canonical link: https://commits.webkit.org/265375@main

Source/WebCore/platform/graphics/angle/PlatformDisplayANGLE.cpp
@@ -68,7 +68,7 @@ EGLDisplay PlatformDisplay::angleEGLDisplay() const
 EGLContext PlatformDisplay::angleSharingGLContext()
 {
 #if PLATFORM(WIN)
-    return sharingGLContext();
+    return sharingGLContext()->platformContext();
 #else
     if (m_angleSharingGLContext != EGL_NO_CONTEXT)
         return m_angleSharingGLContext;