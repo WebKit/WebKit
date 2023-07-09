fd95536c919ad9b017d0ebd71a600b20d1768583
Mark Lam <mark.lam@apple.com>
2023-07-08 17:43:47 -0700

    Cherry-pick 7e5881c7a9e7. rdar://problem/111962239
        Idle sweeping should not schedule a timer callback for more sweeping.
        https://bugs.webkit.org/show_bug.cgi?id=259023
        rdar://111962239
    
        Reviewed by Wenson Hsieh.
    
        Idle time work should only do work during idle time.  Starting a timer as we come out of
        idle will add work that contends with non-idle activity.
    
        * Source/JavaScriptCore/heap/IncrementalSweeper.cpp:
        (JSC::IncrementalSweeper::doSweep):
    
        Canonical link: https://commits.webkit.org/265884@main
    
    Identifier: 265870.173@safari-7616-branch

--- Source/JavaScriptCore/heap/IncrementalSweeper.cpp
+++ Source/JavaScriptCore/heap/IncrementalSweeper.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2021 Apple Inc. All rights reserved.
+ * Copyright (C) 2012-2023 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -78,7 +78,8 @@ void IncrementalSweeper::doSweep(VM& vm, MonotonicTime deadline, SweepTrigger tr
         if (MonotonicTime::now() < deadline)
             continue;
 
-        scheduleTimer();
+        if (trigger == SweepTrigger::Timer)
+            scheduleTimer();
         return;
     }
 
