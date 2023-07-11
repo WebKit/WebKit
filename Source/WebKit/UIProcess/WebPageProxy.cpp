6fc3e7947b97d9485f5320b92c0cdd4cb736d785
Chris Dumez <cdumez@apple.com>
2023-07-11 13:56:23 -0700

    Cherry-pick 3f548e40249b. rdar://problem/112432636
        MESSAGE_CHECK() originalURLString in WebPageProxy::backForwardAddItemShared()
        https://bugs.webkit.org/show_bug.cgi?id=259111
        rdar://112058151
    
        Reviewed by Brent Fulgham.
    
        MESSAGE_CHECK() originalURLString in WebPageProxy::backForwardAddItemShared()
        as hardening, the same way we already do for urlString.
    
        * Source/WebKit/UIProcess/WebPageProxy.cpp:
        (WebKit::WebPageProxy::backForwardAddItemShared):
    
        Canonical link: https://commits.webkit.org/265870.12@safari-7616-branch
    Identifier: 259548.877@safari-7615.3.12.10-branch

--- Source/WebKit/UIProcess/WebPageProxy.cpp
+++ Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -7036,11 +7036,13 @@ void WebPageProxy::backForwardAddItem(BackForwardListItemState&& itemState)
 void WebPageProxy::backForwardAddItemShared(Ref<WebProcessProxy>&& process, BackForwardListItemState&& itemState)
 {
     URL itemURL { itemState.pageState.mainFrameState.urlString };
+    URL itemOriginalURL { itemState.pageState.mainFrameState.originalURLString };
 #if PLATFORM(COCOA)
     if (linkedOnOrAfterSDKWithBehavior(SDKAlignedBehavior::PushStateFilePathRestriction)) {
 #endif
-    ASSERT(!itemURL.isLocalFile() || process->wasPreviouslyApprovedFileURL(itemURL));
-    MESSAGE_CHECK(process, !itemURL.isLocalFile() || process->wasPreviouslyApprovedFileURL(itemURL));
+        ASSERT(!itemURL.protocolIsFile() || process->wasPreviouslyApprovedFileURL(itemURL));
+        MESSAGE_CHECK(process, !itemURL.protocolIsFile() || process->wasPreviouslyApprovedFileURL(itemURL));
+        MESSAGE_CHECK(process, !itemOriginalURL.protocolIsFile() || process->wasPreviouslyApprovedFileURL(itemOriginalURL));
 #if PLATFORM(COCOA)
     }
 #endif
