4fc1843e12638f0b86039d26d801e8f323727615
Youenn Fablet <youennf@gmail.com>
2023-07-13 07:41:06 -0700

    WebKit applies dynamic sandbox flags on failed navigation https://bugs.webkit.org/show_bug.cgi?id=259099 rdar://112044768
    Reviewed by Alex Christensen.
    
    In case of stopped navigation or failed navigation, we were sandboxing the current document.
    The current document was thus running but in a different configuration.
    Other browsers create a new document in that case, Firefox with the request URL and Chrome with a special error scheme URL.
    To limit the scope of changes, we are now creating a new error document, which is empty, and are sandboxing this new document.
    This gets us closer to Firefox and Safari.
    We are still calling the fail delegate in case the application wants to do additional handling on this document.
    
    * LayoutTests/http/wpt/html/browsers/browsing-the-web/navigating-across-documents/navigating-iframe-sandbox-expected.txt: Added.
    * LayoutTests/http/wpt/html/browsers/browsing-the-web/navigating-across-documents/navigating-iframe-sandbox.html: Added.
    * LayoutTests/http/wpt/html/browsers/browsing-the-web/navigating-across-documents/resources/frame-posting-messages.html: Added.
    * LayoutTests/http/wpt/html/browsers/browsing-the-web/navigating-across-documents/resources/only-same-origin-allowed.py: Added.
    (main):
    * LayoutTests/imported/w3c/web-platform-tests/html/cross-origin-embedder-policy/header-parsing.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/html/cross-origin-embedder-policy/require-corp-about-blank.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/html/cross-origin-embedder-policy/require-corp-about-srcdoc.https-expected.txt:
    * LayoutTests/platform/glib/imported/w3c/web-platform-tests/html/cross-origin-embedder-policy/header-parsing.https-expected.txt:
    * LayoutTests/platform/glib/imported/w3c/web-platform-tests/html/cross-origin-embedder-policy/require-corp-about-blank.https-expected.txt:
    * LayoutTests/platform/glib/imported/w3c/web-platform-tests/html/cross-origin-embedder-policy/require-corp-about-srcdoc.https-expected.txt:
    * Source/WebCore/loader/DocumentLoader.cpp:
    (WebCore::DocumentLoader::willSendRequest):
    (WebCore::DocumentLoader::stopLoadingAfterXFrameOptionsOrContentSecurityPolicyDenied):
    (WebCore::DocumentLoader::loadErrorDocument):
    * Source/WebCore/loader/DocumentLoader.h:
    
    Canonical link: https://commits.webkit.org/265870.62@safari-7616-branch

--- Source/WebCore/loader/DocumentLoader.h
+++ Source/WebCore/loader/DocumentLoader.h
@@ -549,6 +549,7 @@ private:
     void dataReceived(const SharedBuffer&);
 
     bool maybeLoadEmpty();
+    void loadErrorDocument();
 
     bool isMultipartReplacingLoad() const;
     bool isPostOrRedirectAfterPost(const ResourceRequest&, const ResourceResponse&);
