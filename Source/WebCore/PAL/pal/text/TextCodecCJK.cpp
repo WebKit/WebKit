16dfc4f4ab61196b4e5720dcff4f77f7f7dfad4f
Alex Christensen <achristensen@apple.com>
2023-07-19 18:11:28 -0700

    Cherry-pick 9fd5c67f7d04. rdar://problem/112507597
        Update GB18030-2022 implementation
        https://bugs.webkit.org/show_bug.cgi?id=259349
        rdar://112507597
    
        Reviewed by Myles C. Maxfield.
    
        This implements the new code points exactly as specified in GB180030-2022
        instead of Unicode's industry recommendations for migrating data between
        GB18030-2005 and GB18030-2022.
    
        * LayoutTests/imported/w3c/web-platform-tests/encoding/legacy-mb-schinese/gb18030/gb18030-decoder.any.js:
        * LayoutTests/imported/w3c/web-platform-tests/encoding/legacy-mb-schinese/gb18030/gb18030-encoder.html:
        * Source/WebCore/PAL/pal/text/TextCodecCJK.cpp:
        (PAL::gb18030_2022Encode2Byte):
        (PAL::gb18030_2022Encode4Byte):
        (PAL::gb18030_2022Decode):
        (PAL::TextCodecCJK::gb18030Decode):
        (PAL::gbEncodeShared):
        (PAL::gb18030AsymmetricEncode): Deleted.
    
        Canonical link: https://commits.webkit.org/266173@main
    
    Identifier: 265870.206@safari-7616-branch

--- Source/WebCore/PAL/pal/text/TextCodecCJK.cpp
+++ Source/WebCore/PAL/pal/text/TextCodecCJK.cpp
@@ -892,6 +892,58 @@ static const GB18030EncodeIndex& gb18030EncodeIndex()
     return *table;
 }
 
+// https://unicode-org.atlassian.net/browse/ICU-22357
+// The 2-byte values are handled correctly by values from gb18030()
+// but these need to be exceptions from gb18030Ranges().
+static std::optional<uint32_t> gb180302022Encode(UChar32 codePoint)
+{
+    switch (codePoint) {
+    case 0xE81E: return 0x82359037;
+    case 0xE826: return 0x82359038;
+    case 0xE82B: return 0x82359039;
+    case 0xE82C: return 0x82359130;
+    case 0xE832: return 0x82359131;
+    case 0xE843: return 0x82359132;
+    case 0xE854: return 0x82359133;
+    case 0xE864: return 0x82359134;
+    case 0xE78D: return 0x84318236;
+    case 0xE78F: return 0x84318237;
+    case 0xE78E: return 0x84318238;
+    case 0xE790: return 0x84318239;
+    case 0xE791: return 0x84318330;
+    case 0xE792: return 0x84318331;
+    case 0xE793: return 0x84318332;
+    case 0xE794: return 0x84318333;
+    case 0xE795: return 0x84318334;
+    case 0xE796: return 0x84318335;
+    }
+    return std::nullopt;
+}
+static std::optional<UChar32> gb180302022Decode(uint8_t first, uint8_t second, uint8_t third, uint8_t fourth)
+{
+    switch (static_cast<uint32_t>(first) << 24 | static_cast<uint32_t>(second) << 16 | static_cast<uint32_t>(third) << 8 | fourth) {
+    case 0x82359037: return 0xE81E;
+    case 0x82359038: return 0xE826;
+    case 0x82359039: return 0xE82B;
+    case 0x82359130: return 0xE82C;
+    case 0x82359131: return 0xE832;
+    case 0x82359132: return 0xE843;
+    case 0x82359133: return 0xE854;
+    case 0x82359134: return 0xE864;
+    case 0x84318236: return 0xE78D;
+    case 0x84318237: return 0xE78F;
+    case 0x84318238: return 0xE78E;
+    case 0x84318239: return 0xE790;
+    case 0x84318330: return 0xE791;
+    case 0x84318331: return 0xE792;
+    case 0x84318332: return 0xE793;
+    case 0x84318333: return 0xE794;
+    case 0x84318334: return 0xE795;
+    case 0x84318335: return 0xE796;
+    }
+    return std::nullopt;
+}
+
 // https://encoding.spec.whatwg.org/#gb18030-decoder
 String TextCodecCJK::gb18030Decode(const uint8_t* bytes, size_t length, bool flush, bool stopOnError, bool& sawError)
 {
@@ -917,6 +969,10 @@ String TextCodecCJK::gb18030Decode(const uint8_t* bytes, size_t length, bool flu
             uint8_t first = std::exchange(m_gb18030First, 0x00);
             uint8_t second = std::exchange(m_gb18030Second, 0x00);
             uint8_t third = std::exchange(m_gb18030Third, 0x00);
+            if (auto codePoint = gb180302022Decode(first, second, third, byte)) {
+                result.appendCharacter(*codePoint);
+                return SawError::No;
+            }
             if (auto codePoint = gb18030RangesCodePoint(((first - 0x81) * 10 * 126 * 10) + ((second - 0x30) * 10 * 126) + ((third - 0x81) * 10) + byte - 0x30)) {
                 result.appendCharacter(*codePoint);
                 return SawError::No;
@@ -981,52 +1037,6 @@ String TextCodecCJK::gb18030Decode(const uint8_t* bytes, size_t length, bool flu
     return result;
 }
 
-// https://www.unicode.org/L2/L2023/23003r-gb18030-recommendations.pdf
-static std::optional<uint16_t> gb18030AsymmetricEncode(UChar32 codePoint)
-{
-    switch (codePoint) {
-    case 0xE78D:
-        return 0xA6D9;
-    case 0xE78E:
-        return 0xA6DA;
-    case 0xE78F:
-        return 0xA6DB;
-    case 0xE790:
-        return 0xA6DC;
-    case 0xE791:
-        return 0xA6DD;
-    case 0xE792:
-        return 0xA6DE;
-    case 0xE793:
-        return 0xA6DF;
-    case 0xE794:
-        return 0xA6EC;
-    case 0xE795:
-        return 0xA6ED;
-    case 0xE796:
-        return 0xA6F3;
-    case 0xE81E:
-        return 0xFE59;
-    case 0xE826:
-        return 0xFE61;
-    case 0xE82B:
-        return 0xFE66;
-    case 0xE82C:
-        return 0xFE67;
-    case 0xE832:
-        return 0xFE6D;
-    case 0xE843:
-        return 0xFE7E;
-    case 0xE854:
-        return 0xFE90;
-    case 0xE864:
-        return 0xFEA0;
-    default:
-        break;
-    }
-    return std::nullopt;
-}
-
 // https://encoding.spec.whatwg.org/#gb18030-encoder
 enum class IsGBK : bool { No, Yes };
 static Vector<uint8_t> gbEncodeShared(StringView string, Function<void(UChar32, Vector<uint8_t>&)>&& unencodableHandler, IsGBK isGBK)
@@ -1050,7 +1060,9 @@ static Vector<uint8_t> gbEncodeShared(StringView string, Function<void(UChar32, 
                 result.append(0x80);
                 continue;
             }
-        } else if (auto encoded = gb18030AsymmetricEncode(codePoint)) {
+        } else if (auto encoded = gb180302022Encode(codePoint)) {
+            result.append(*encoded >> 24);
+            result.append(*encoded >> 16);
             result.append(*encoded >> 8);
             result.append(*encoded);
             continue;
