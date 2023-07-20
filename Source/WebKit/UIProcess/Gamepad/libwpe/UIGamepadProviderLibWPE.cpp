b64ff8915cec75eabd5eee7d8a4f48b4b9e17d44
Diego Pino Garcia <dpino@igalia.com>
2023-07-19 23:51:53 -0700

    Unreviewed, revert 265981@main
    The fix made several other tests fail.
    
    * LayoutTests/platform/wpe/TestExpectations:
    * Source/WebKit/UIProcess/Gamepad/libwpe/UIGamepadProviderLibWPE.cpp:
    (WebKit::UIGamepadProvider::platformWebPageProxyForGamepadInput):
    
    Canonical link: https://commits.webkit.org/266175@main

--- Source/WebKit/UIProcess/Gamepad/libwpe/UIGamepadProviderLibWPE.cpp
+++ Source/WebKit/UIProcess/Gamepad/libwpe/UIGamepadProviderLibWPE.cpp
@@ -48,9 +48,6 @@ void UIGamepadProvider::platformSetDefaultGamepadProvider()
 
 WebPageProxy* UIGamepadProvider::platformWebPageProxyForGamepadInput()
 {
-    if (GamepadProvider::singleton().isMockGamepadProvider())
-        return nullptr;
-
 #if PLATFORM(WPE)
     return WKWPE::View::platformWebPageProxyForGamepadInput();
 #else
