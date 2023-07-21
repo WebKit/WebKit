WebKit/WebKit

commit d41c610090b07554b2266f6d56435c8612c987b4

[WebXR] Use MTLTexture to attach color IOSurface https://bugs.webkit.org/show_bug.cgi?id=258598 rdar://problem/111426880
Reviewed by Kimmo Kinnunen.

There are two paths in WebXROpaqueFramebuffer for attaching compositor provided
IOSurface for rendering: 1) Via MTLSharedTexture, and 2) Via pbuffer.

Since it's possible to create a MTLTexture from an IOSurface and plane index,
switch away from using pbuffer to reduce the number of code paths.

This patch introduces USE(MTLTEXTURE_FOR_XR_LAYER_DATA) to aid the transition of
platform specific WebXR systems. Once those systems are updated,
USE(IOSURFACE_FOR_XR_LAYER_DATA) will be removed.

* Source/WebCore/Modules/webxr/WebXROpaqueFramebuffer.cpp:
(WebCore::WebXROpaqueFramebuffer::startFrame):
(WebCore::WebXROpaqueFramebuffer::endFrame):
(WebCore::WebXROpaqueFramebuffer::allocateColorStorage):
* Source/WebCore/Modules/webxr/WebXROpaqueFramebuffer.h:
* Source/WebCore/platform/graphics/cocoa/GraphicsContextGLCocoa.h:
* Source/WebCore/platform/graphics/cocoa/GraphicsContextGLCocoa.mm:
(WebCore::GraphicsContextGLCocoa::platformInitialize):
(WebCore::GraphicsContextGLCocoa::createAndBindEGLImage):
(WebCore::createAndBindSharedTextureToExternalImage): Deleted.
* Source/WebCore/platform/xr/PlatformXR.h:
(PlatformXR::Device::FrameData::LayerData::encode const):
(PlatformXR::Device::FrameData::LayerData::decode):
* Source/WebCore/testing/WebFakeXRDevice.cpp:
(WebCore::SimulatedXRDevice::frameTimerFired):

Canonical link: https://commits.webkit.org/265606@main

Source/WebCore/Modules/webxr/WebXROpaqueFramebuffer.cpp
@@ -36,7 +36,7 @@
 #include "WebGLRenderingContextBase.h"
 #include <wtf/Scope.h>
 
-#if USE(IOSURFACE_FOR_XR_LAYER_DATA)
+#if USE(IOSURFACE_FOR_XR_LAYER_DATA) || USE(MTLTEXTURE_FOR_XR_LAYER_DATA)
 #include "GraphicsContextGLCocoa.h"
 #endif
 
@@ -65,7 +65,7 @@ WebXROpaqueFramebuffer::WebXROpaqueFramebuffer(PlatformXR::LayerHandle handle, R
 WebXROpaqueFramebuffer::~WebXROpaqueFramebuffer()
 {
     if (auto gl = m_context.graphicsContextGL()) {
-#if USE(IOSURFACE_FOR_XR_LAYER_DATA)
+#if USE(IOSURFACE_FOR_XR_LAYER_DATA) || USE(MTLTEXTURE_FOR_XR_LAYER_DATA)
         m_opaqueTexture.release(*gl);
 #endif
         m_stencilBuffer.release(*gl);
@@ -76,7 +76,7 @@ WebXROpaqueFramebuffer::~WebXROpaqueFramebuffer()
     } else {
         // The GraphicsContextGL is gone, so disarm the GCGLOwned objects so
         // their destructors don't assert.
-#if USE(IOSURFACE_FOR_XR_LAYER_DATA)
+#if USE(IOSURFACE_FOR_XR_LAYER_DATA) || USE(MTLTEXTURE_FOR_XR_LAYER_DATA)
         m_opaqueTexture.leakObject();
 #endif
         m_stencilBuffer.leakObject();
@@ -92,16 +92,10 @@ void WebXROpaqueFramebuffer::startFrame(const PlatformXR::Device::FrameData::Lay
         return;
     auto& gl = *m_context.graphicsContextGL();
 
-#if USE(IOSURFACE_FOR_XR_LAYER_DATA)
-    ASSERT(data.surface);
-    auto bufferSize = data.surface->size();
-
-    auto gCGL = static_cast<GraphicsContextGLCocoa*>(m_context.graphicsContextGL());
-    auto [textureTarget, textureTargetBinding] = gl.externalImageTextureBindingPoint();
-#else
-    GCGLenum textureTarget = GL::TEXTURE_2D;
-    GCGLenum textureTargetBinding = GL::TEXTURE_BINDING_2D;
+#if USE(IOSURFACE_FOR_XR_LAYER_DATA) || USE(MTLTEXTURE_FOR_XR_LAYER_DATA)
+    IntSize bufferSize;
 #endif
+    auto [textureTarget, textureTargetBinding] = gl.externalImageTextureBindingPoint();
 
     m_framebuffer->setOpaqueActive(true);
 
@@ -123,6 +117,15 @@ void WebXROpaqueFramebuffer::startFrame(const PlatformXR::Device::FrameData::Lay
     // the textures/renderbuffers.
 
 #if USE(IOSURFACE_FOR_XR_LAYER_DATA)
+    // FIXME: This is temporary until Cocoa-specific platforms migrate to MTLTEXTURE_FOR_XR_LAYER_DATA.
+    auto colorTextureHandle = data.surface->createSendRight();
+    bool colorTextureIsShared = false;
+#elif USE(MTLTEXTURE_FOR_XR_LAYER_DATA)
+    // Tell the GraphicsContextGL to use the IOSurface as the backing store for m_opaqueTexture.
+    auto [colorTextureHandle, colorTextureIsShared] = data.colorTexture;
+#endif
+
+#if USE(IOSURFACE_FOR_XR_LAYER_DATA) || USE(MTLTEXTURE_FOR_XR_LAYER_DATA)
     m_opaqueTexture.ensure(gl);
     gl.bindTexture(textureTarget, m_opaqueTexture);
     gl.texParameteri(textureTarget, GL::TEXTURE_MAG_FILTER, GL::LINEAR);
@@ -130,28 +133,15 @@ void WebXROpaqueFramebuffer::startFrame(const PlatformXR::Device::FrameData::Lay
     gl.texParameteri(textureTarget, GL::TEXTURE_WRAP_S, GL::CLAMP_TO_EDGE);
     gl.texParameteri(textureTarget, GL::TEXTURE_WRAP_T, GL::CLAMP_TO_EDGE);
 
-    // Tell the GraphicsContextGL to use the IOSurface as the backing store for m_opaqueTexture.
-    if (data.isShared) {
-#if !PLATFORM(IOS_FAMILY_SIMULATOR)
-        auto surfaceTextureAttachment = gCGL->createAndBindEGLImage(textureTarget, data.surface.get());
-        if (!surfaceTextureAttachment) {
-            m_opaqueTexture.release(gl);
-            return;
-        }
+    auto colorTextureSource = (colorTextureIsShared) ? GL::EGLImageSource(GL::EGLImageSourceMTLSharedTextureHandle { colorTextureHandle }) : GL::EGLImageSource(GL::EGLImageSourceIOSurfaceHandle { colorTextureHandle });
 
-        auto [textureHandle, textureSize] = surfaceTextureAttachment.value();
-        m_ioSurfaceTextureHandle = textureHandle;
-        bufferSize = textureSize;
-
-        m_ioSurfaceTextureHandleIsShared = true;
-#else
-        ASSERT_NOT_REACHED();
-#endif
-    } else {
-        m_ioSurfaceTextureHandle = gCGL->createPbufferAndAttachIOSurface(textureTarget, GraphicsContextGLCocoa::PbufferAttachmentUsage::Write, GL::BGRA, bufferSize.width(), bufferSize.height(), GL::UNSIGNED_BYTE, data.surface->surface(), 0);
-        m_ioSurfaceTextureHandleIsShared = false;
+    auto colorTextureAttachment = gl.createAndBindEGLImage(textureTarget, colorTextureSource);
+    if (!colorTextureAttachment) {
+        m_opaqueTexture.release(gl);
+        return;
     }
 
+    std::tie(m_opaqueImage, bufferSize) = colorTextureAttachment.value();
     if (bufferSize.isEmpty())
         return;
 
@@ -161,11 +151,6 @@ void WebXROpaqueFramebuffer::startFrame(const PlatformXR::Device::FrameData::Lay
         m_framebufferSize = bufferSize;
         if (!setupFramebuffer())
             return;
-    }
-
-    if (!m_ioSurfaceTextureHandle) {
-        m_opaqueTexture.release(gl);
-        return;
     }
 
     // Set up the framebuffer to use the texture that points to the IOSurface. If we're not multisampling,
@@ -241,20 +226,10 @@ void WebXROpaqueFramebuffer::endFrame()
 #endif
 
 
-#if USE(IOSURFACE_FOR_XR_LAYER_DATA)
-    if (m_ioSurfaceTextureHandle) {
-        if (m_ioSurfaceTextureHandleIsShared) {
-#if !PLATFORM(IOS_FAMILY_SIMULATOR)
-            gl.destroyEGLImage(m_ioSurfaceTextureHandle);
-#else
-            ASSERT_NOT_REACHED();
-#endif
-        } else {
-            auto gCGL = static_cast<GraphicsContextGLCocoa*>(&gl);
-            gCGL->destroyPbufferAndDetachIOSurface(m_ioSurfaceTextureHandle);
-        }
-        m_ioSurfaceTextureHandle = nullptr;
-        m_ioSurfaceTextureHandleIsShared = false;
+#if USE(IOSURFACE_FOR_XR_LAYER_DATA) || USE(MTLTEXTURE_FOR_XR_LAYER_DATA)
+    if (m_opaqueImage) {
+        gl.destroyEGLImage(m_opaqueImage);
+        m_opaqueImage = nullptr;
     }
 #endif
 }
@@ -325,12 +300,7 @@ PlatformGLObject WebXROpaqueFramebuffer::allocateRenderbufferStorage(GraphicsCon
 
 PlatformGLObject WebXROpaqueFramebuffer::allocateColorStorage(GraphicsContextGL& gl, GCGLsizei samples, IntSize size)
 {
-#if USE(IOSURFACE_FOR_XR_LAYER_DATA) && !PLATFORM(IOS_FAMILY_SIMULATOR)
     constexpr auto colorFormat = GL::SRGB8_ALPHA8;
-#else
-    constexpr auto colorFormat = GL::RGBA8;
-#endif
-
     return allocateRenderbufferStorage(gl, samples, colorFormat, size);
 }
 

Source/WebCore/Modules/webxr/WebXROpaqueFramebuffer.h
@@ -81,10 +81,9 @@ private:
     GCGLOwnedRenderbuffer m_stencilBuffer;
     GCGLOwnedRenderbuffer m_multisampleColorBuffer;
     GCGLOwnedFramebuffer m_resolvedFBO;
-#if USE(IOSURFACE_FOR_XR_LAYER_DATA)
+#if USE(IOSURFACE_FOR_XR_LAYER_DATA) || USE(MTLTEXTURE_FOR_XR_LAYER_DATA)
     GCGLOwnedTexture m_opaqueTexture;
-    void* m_ioSurfaceTextureHandle { nullptr };
-    bool m_ioSurfaceTextureHandleIsShared { false };
+    GCEGLImage m_opaqueImage;
 #else
     PlatformGLObject m_opaqueTexture;
 #endif

Source/WebCore/platform/graphics/cocoa/GraphicsContextGLCocoa.h
@@ -69,10 +69,6 @@ public:
     void destroyPbufferAndDetachIOSurface(void* handle);
 
     std::optional<EGLImageAttachResult> createAndBindEGLImage(GCGLenum, EGLImageSource) final;
-#if !PLATFORM(IOS_FAMILY_SIMULATOR)
-    // Short-term support for in-process WebGL.
-    std::optional<EGLImageAttachResult> createAndBindEGLImage(GCGLenum, IOSurface*);
-#endif
 
     RetainPtr<id> newSharedEventWithMachPort(mach_port_t);
     GCEGLSync createEGLSync(ExternalEGLSyncEvent) final;

Source/WebCore/platform/graphics/cocoa/GraphicsContextGLCocoa.mm
@@ -406,7 +406,7 @@ bool GraphicsContextGLCocoa::platformInitialize()
         requiredExtensions.append("GL_EXT_texture_format_BGRA8888"_s);
     }
 #endif // PLATFORM(MAC) || PLATFORM(MACCATALYST)
-#if ENABLE(WEBXR) && !PLATFORM(IOS_FAMILY_SIMULATOR)
+#if ENABLE(WEBXR)
     if (attributes.xrCompatible) {
         requiredExtensions.append("GL_OES_EGL_image"_s);
         requiredExtensions.append("GL_EXT_sRGB"_s);
@@ -683,85 +683,67 @@ void GraphicsContextGLCocoa::destroyPbufferAndDetachIOSurface(void* handle)
     WebCore::destroyPbufferAndDetachIOSurface(m_displayObj, handle);
 }
 
-#if !PLATFORM(IOS_FAMILY_SIMULATOR)
-static std::optional<GraphicsContextGL::EGLImageAttachResult> createAndBindSharedTextureToExternalImage(GCGLDisplay platformDisplay, GCGLenum target, MTLSharedTextureHandle* handle)
+std::optional<GraphicsContextGL::EGLImageAttachResult> GraphicsContextGLCocoa::createAndBindEGLImage(GCGLenum target, EGLImageSource source)
 {
     EGLDeviceEXT eglDevice = EGL_NO_DEVICE_EXT;
-    if (!EGL_QueryDisplayAttribEXT(platformDisplay, EGL_DEVICE_EXT, reinterpret_cast<EGLAttrib*>(&eglDevice)))
+    if (!EGL_QueryDisplayAttribEXT(platformDisplay(), EGL_DEVICE_EXT, reinterpret_cast<EGLAttrib*>(&eglDevice)))
         return std::nullopt;
 
     id<MTLDevice> mtlDevice = nil;
     if (!EGL_QueryDeviceAttribEXT(eglDevice, EGL_METAL_DEVICE_ANGLE, reinterpret_cast<EGLAttrib*>(&mtlDevice)))
         return std::nullopt;
 
-    if (mtlDevice != [handle device]) {
-        LOG(WebGL, "MTLSharedTextureHandle does not have the same Metal device as platformDisplay in attachMTLTextureToExternalImage.");
-        return std::nullopt;
-    }
+    RetainPtr<id<MTLTexture>> texture = WTF::switchOn(WTFMove(source),
+    [&](EGLImageSourceIOSurfaceHandle&& ioSurface) -> RetainPtr<id> {
+        auto surface = IOSurface::createFromSendRight(WTFMove(ioSurface.handle));
+        if (!surface)
+            return { };
 
-    // Create a MTLTexture out of the MTLSharedTextureHandle.
-    auto texture = adoptNS([mtlDevice newSharedTextureWithHandle:handle]);
+        auto size = surface->size();
+        MTLTextureDescriptor *desc = [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:MTLPixelFormatBGRA8Unorm_sRGB width:size.width() height:size.height() mipmapped:NO];
+        [desc setUsage:MTLTextureUsageShaderRead | MTLTextureUsageRenderTarget];
+
+        auto tex = adoptNS([mtlDevice newTextureWithDescriptor:desc iosurface:surface->surface() plane:0]);
+        return tex;
+    },
+    [&](EGLImageSourceMTLSharedTextureHandle&& sharedTexture) -> RetainPtr<id> {
+#if PLATFORM(IOS_FAMILY_SIMULATOR)
+        UNUSED_VARIABLE(sharedTexture);
+        ASSERT_NOT_REACHED();
+        return { };
+#else
+        auto handle = adoptNS([[MTLSharedTextureHandle alloc] initWithMachPort:sharedTexture.handle.sendRight()]);
+        if (!handle)
+            return { };
+
+        if (mtlDevice != [handle device]) {
+            LOG(WebGL, "MTLSharedTextureHandle does not have the same Metal device as platformDisplay.");
+            return { };
+        }
+
+        // Create a MTLTexture out of the MTLSharedTextureHandle.
+        RetainPtr<id> texture = adoptNS([mtlDevice newSharedTextureWithHandle:handle.get()]);
+        return texture;
+        // FIXME: Does the texture have the correct usage mode?
+#endif
+    });
+
     if (!texture)
         return std::nullopt;
 
-    GCGLuint textureWidth = [texture width];
-    GCGLuint textureHeight = [texture height];
-
-    // FIXME: Does the texture have the correct usage mode?
-
     // Create an EGLImage out of the MTLTexture
     constexpr EGLint emptyAttributes[] = { EGL_NONE };
-    auto eglImage = EGL_CreateImageKHR(platformDisplay, EGL_NO_CONTEXT, EGL_METAL_TEXTURE_ANGLE, reinterpret_cast<EGLClientBuffer>(texture.get()), emptyAttributes);
+    auto eglImage = EGL_CreateImageKHR(platformDisplay(), EGL_NO_CONTEXT, EGL_METAL_TEXTURE_ANGLE, reinterpret_cast<EGLClientBuffer>(texture.get()), emptyAttributes);
     if (!eglImage)
         return std::nullopt;
 
     // Tell the currently bound texture to use the EGLImage.
     GL_EGLImageTargetTexture2DOES(target, eglImage);
 
+    GCGLuint textureWidth = [texture width];
+    GCGLuint textureHeight = [texture height];
+
     return std::make_tuple(eglImage, IntSize(textureWidth, textureHeight));
-}
-
-std::optional<GraphicsContextGL::EGLImageAttachResult> GraphicsContextGLCocoa::createAndBindEGLImage(GCGLenum target, IOSurface* surface)
-{
-    auto handle = adoptNS([[MTLSharedTextureHandle alloc] initWithIOSurface:surface->surface() label:@"WebXR"]);
-    if (!handle)
-        return std::nullopt;
-
-    return createAndBindSharedTextureToExternalImage(platformDisplay(), target, handle.get());
-}
-#endif
-
-std::optional<GraphicsContextGL::EGLImageAttachResult> GraphicsContextGLCocoa::createAndBindEGLImage(GCGLenum target, EGLImageSource source)
-{
-#if PLATFORM(IOS_FAMILY_SIMULATOR)
-    // MTLSharedTexture is unsupported on simulator
-    UNUSED_VARIABLE(target);
-    UNUSED_VARIABLE(source);
-
-    return std::nullopt;
-#else // !PLATFORM(IOS_FAMILY_SIMULATOR
-
-    RetainPtr<MTLSharedTextureHandle> handle;
-    return WTF::switchOn(WTFMove(source),
-        [&](EGLImageSourceIOSurfaceHandle&& ioSurface) -> std::optional<EGLImageAttachResult> {
-            auto surface = IOSurface::createFromSendRight(WTFMove(ioSurface.handle));
-            if (!surface)
-                return std::nullopt;
-
-            handle = adoptNS([[MTLSharedTextureHandle alloc] initWithIOSurface:surface->surface() label:@"WebXR"]);
-            if (!handle)
-                return std::nullopt;
-
-            return createAndBindSharedTextureToExternalImage(platformDisplay(), target, handle.get());
-        },
-        [&](EGLImageSourceMTLSharedTextureHandle&& sharedTexture) -> std::optional<EGLImageAttachResult> {
-            handle = adoptNS([[MTLSharedTextureHandle alloc] initWithMachPort:sharedTexture.handle.sendRight()]);
-            if (!handle)
-                return std::nullopt;
-
-            return createAndBindSharedTextureToExternalImage(platformDisplay(), target, handle.get());
-        });
-#endif
 }
 
 RetainPtr<id> GraphicsContextGLCocoa::newSharedEventWithMachPort(mach_port_t sharedEventSendRight)

Source/WebCore/platform/xr/PlatformXR.h
@@ -293,6 +293,8 @@ public:
 #if USE(IOSURFACE_FOR_XR_LAYER_DATA)
             std::unique_ptr<WebCore::IOSurface> surface;
             bool isShared { false };
+#elif USE(MTLTEXTURE_FOR_XR_LAYER_DATA)
+            std::tuple<MachSendRight, bool> colorTexture;
 #else
             PlatformGLObject opaqueTexture { 0 };
 #endif
@@ -581,6 +583,8 @@ void Device::FrameData::LayerData::encode(Encoder& encoder) const
     MachSendRight surfaceSendRight = surface ? surface->createSendRight() : MachSendRight();
     encoder << surfaceSendRight;
     encoder << isShared;
+#elif USE(MTLTEXTURE_FOR_XR_LAYER_DATA)
+    encoder << colorTexture;
 #else
     encoder << opaqueTexture;
 #endif
@@ -599,6 +603,9 @@ std::optional<Device::FrameData::LayerData> Device::FrameData::LayerData::decode
         return std::nullopt;
     layerData.surface = WebCore::IOSurface::createFromSendRight(WTFMove(surfaceSendRight));
     if (!decoder.decode(layerData.isShared))
+        return std::nullopt;
+#elif USE(MTLTEXTURE_FOR_XR_LAYER_DATA)
+    if (!decoder.decode(layerData.colorTexture))
         return std::nullopt;
 #else
     if (!decoder.decode(layerData.opaqueTexture))

Source/WebCore/testing/WebFakeXRDevice.cpp
@@ -158,6 +158,9 @@ void SimulatedXRDevice::frameTimerFired()
     for (auto& layer : m_layers) {
 #if USE(IOSURFACE_FOR_XR_LAYER_DATA)
         data.layers.add(layer.key, FrameData::LayerData { .surface = IOSurface::create(nullptr, recommendedResolution(PlatformXR::SessionMode::ImmersiveVr), DestinationColorSpace::SRGB()) });
+#elif USE(MTLTEXTURE_FOR_XR_LAYER_DATA)
+        auto surface = IOSurface::create(nullptr, recommendedResolution(PlatformXR::SessionMode::ImmersiveVr), DestinationColorSpace::SRGB());
+        data.layers.add(layer.key, FrameData::LayerData { .colorTexture = std::make_tuple(surface->createSendRight(), false) });
 #else
         data.layers.add(layer.key, FrameData::LayerData { .opaqueTexture = layer.value });
 #endif

commit 643dde579279e5eb3281493d6708556f30089fac

[WPE] Fix clang-14 build https://bugs.webkit.org/show_bug.cgi?id=259395
Unreviewed.

* Source/WebCore/Modules/webxr/WebXROpaqueFramebuffer.cpp:
(WebCore::WebXROpaqueFramebuffer::startFrame): Structured binding capture in lambda is not supported
until c++-20, the workaround is to use a dedicated initializer. This was broken since 265606@main.
* Source/bmalloc/libpas/src/libpas/pas_segregated_shared_page_directory.c: Add PAS_UNUSED_PARAM()
for unused macro result expansions.
(find_first_eligible_consider_view):
(take_last_empty_consider_view):
* Source/bmalloc/libpas/src/libpas/pas_segregated_size_directory.c: Ditto.
(take_last_empty_consider_view):

Canonical link: https://commits.webkit.org/266207@main

Source/WebCore/Modules/webxr/WebXROpaqueFramebuffer.cpp
@@ -138,7 +138,7 @@ void WebXROpaqueFramebuffer::startFrame(const PlatformXR::Device::FrameData::Lay
     GCGLint boundRenderbuffer = gl.getInteger(GL::RENDERBUFFER_BINDING);
     GCGLint boundTexture = gl.getInteger(textureTargetBinding);
 
-    auto scopedBindings = makeScopeExit([=, &gl]() {
+    auto scopedBindings = makeScopeExit([=, textureTarget = textureTarget, &gl]() {
         gl.bindFramebuffer(GL::FRAMEBUFFER, boundFBO);
         gl.bindRenderbuffer(GL::RENDERBUFFER, boundRenderbuffer);
         gl.bindTexture(textureTarget, boundTexture);

Source/bmalloc/libpas/src/libpas/pas_segregated_shared_page_directory.c
@@ -99,8 +99,9 @@ find_first_eligible_consider_view(
         <= pas_segregated_shared_page_directory_probability_of_ineligibility) {
         if (verbose)
             pas_log("cannot bump at %zu, clearing eligibility.\n", config->index);
-        PAS_SEGREGATED_DIRECTORY_BIT_REFERENCE_SET(
+        bool result = PAS_SEGREGATED_DIRECTORY_BIT_REFERENCE_SET(
             config->directory, config->bit_reference, eligible, false);
+        PAS_UNUSED_PARAM(result);
     }
 
     if (verbose)
@@ -388,7 +389,8 @@ take_last_empty_consider_view(
     }
 
     /* It's possible that the empty bit had gotten set again; clear it. */
-    PAS_SEGREGATED_DIRECTORY_BIT_REFERENCE_SET(directory, bit_reference, empty, false);
+    bool set_bit_result = PAS_SEGREGATED_DIRECTORY_BIT_REFERENCE_SET(directory, bit_reference, empty, false);
+    PAS_UNUSED_PARAM(set_bit_result);
 
     page = pas_segregated_page_for_boundary(shared_handle->page_boundary, page_config);
 

Source/bmalloc/libpas/src/libpas/pas_segregated_size_directory.c
@@ -728,7 +728,8 @@ take_last_empty_consider_view(pas_segregated_directory_iterate_config* config)
     PAS_ASSERT(!PAS_SEGREGATED_DIRECTORY_GET_BIT(directory, index, eligible));
 
     /* It's totally possible that the empty bit got set again. We should clear it for sure now. */
-    PAS_SEGREGATED_DIRECTORY_SET_BIT(directory, index, empty, false);
+    bool result = PAS_SEGREGATED_DIRECTORY_SET_BIT(directory, index, empty, false);
+    PAS_UNUSED_PARAM(result);
 
     PAS_TESTING_ASSERT(pas_segregated_page_qualifies_for_decommit(page, my_page_config));
     