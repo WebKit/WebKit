a3ead6795056e22b025fe0db430d6f6c1f24ef26
Alex Christensen <achristensen@apple.com>
2023-07-25 16:56:35 -0700

    Fix debug tests after 266301@main https://bugs.webkit.org/show_bug.cgi?id=259508 rdar://112868658
    Unreviewed.
    
    The new call to broadcastFrameRemovalToOtherProcesses was causing assertions.
    Restrict it to when site isolation is enabled to restore existing behavior when it is not enabled.
    
    * Source/WebCore/html/HTMLFrameOwnerElement.cpp:
    (WebCore::HTMLFrameOwnerElement::disconnectContentFrame):
    
    Canonical link: https://commits.webkit.org/266305@main

--- Source/WebCore/html/HTMLFrameOwnerElement.cpp
+++ Source/WebCore/html/HTMLFrameOwnerElement.cpp
@@ -80,7 +80,8 @@ void HTMLFrameOwnerElement::clearContentFrame()
 void HTMLFrameOwnerElement::disconnectContentFrame()
 {
     if (RefPtr frame = m_contentFrame.get()) {
-        frame->broadcastFrameRemovalToOtherProcesses();
+        if (frame->settings().siteIsolationEnabled())
+            frame->broadcastFrameRemovalToOtherProcesses();
         frame->frameDetached();
         frame->disconnectOwnerElement();
     }
