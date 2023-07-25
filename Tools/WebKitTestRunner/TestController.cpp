WebKit/WebKit

commit e126b11cd15986d620bb3b69a0c93dd8020a6eeb

Deprecate and disconnect all of WKPageGroupRef https://bugs.webkit.org/show_bug.cgi?id=259324 rdar://112499509
Reviewed by Tim Horton.

It is no longer used anywhere.
Its last use was just an object to get the UserContentController
or preferences object from.

* Source/WebKit/UIProcess/API/C/WKPage.cpp:
(WKPageGetPageGroup):
* Source/WebKit/UIProcess/API/C/WKPage.h:
* Source/WebKit/UIProcess/API/C/WKPageConfigurationRef.cpp:
(WKPageConfigurationGetPageGroup):
(WKPageConfigurationSetPageGroup):
* Source/WebKit/UIProcess/API/C/WKPageConfigurationRef.h:
* Source/WebKit/UIProcess/API/C/WKPageGroup.cpp:
(WKPageGroupGetTypeID):
(WKPageGroupCreateWithIdentifier):
(WKPageGroupSetPreferences):
(WKPageGroupGetPreferences):
(WKPageGroupGetUserContentController):
(WKPageGroupAddUserStyleSheet):
(WKPageGroupRemoveAllUserStyleSheets):
(WKPageGroupAddUserScript):
(WKPageGroupRemoveAllUserScripts):
* Source/WebKit/UIProcess/API/C/WKPageGroup.h:
* Source/WebKit/UIProcess/API/Cocoa/WKWebView.mm:
(-[WKWebView _setupPageConfiguration:]):
* Source/WebKit/UIProcess/API/Cocoa/WKWebViewConfiguration.mm:
(-[WKWebViewConfiguration copyWithZone:]):
(-[WKWebViewConfiguration _pageGroup]):
(-[WKWebViewConfiguration _setPageGroup:]):
* Source/WebKit/UIProcess/API/Cocoa/WKWebViewConfigurationPrivate.h:
* Tools/TestWebKitAPI/PlatformWebView.h:
* Tools/TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
* Tools/TestWebKitAPI/Tests/WebKit/DOMWindowExtensionBasic.cpp:
(TestWebKitAPI::TEST):
* Tools/TestWebKitAPI/Tests/WebKit/DOMWindowExtensionNoCache.cpp:
(TestWebKitAPI::TEST):
* Tools/TestWebKitAPI/Tests/WebKit/DocumentStartUserScriptAlertCrash.cpp:
(TestWebKitAPI::TEST):
* Tools/TestWebKitAPI/Tests/WebKit/EnumerateMediaDevices.cpp:
(TestWebKitAPI::TEST):
* Tools/TestWebKitAPI/Tests/WebKit/InjectedBundleDisableOverrideBuiltinsBehavior.cpp:
(TestWebKitAPI::TEST):
* Tools/TestWebKitAPI/Tests/WebKit/InjectedBundleMakeAllShadowRootsOpen.cpp:
(TestWebKitAPI::TEST):

Tools/WebKitTestRunner/TestController.cpp
@@ -58,7 +58,6 @@
 #include <WebKit/WKNotificationPermissionRequest.h>
 #include <WebKit/WKNumber.h>
 #include <WebKit/WKOpenPanelResultListener.h>
-#include <WebKit/WKPageGroup.h>
 #include <WebKit/WKPageInjectedBundleClient.h>
 #include <WebKit/WKPagePrivate.h>
 #include <WebKit/WKPluginInformation.h>
@@ -630,8 +629,7 @@ void TestController::initialize(int argc, const char* argv[])
     WebCoreTestSupport::installMockGamepadProvider();
 #endif
 
-    m_pageGroup.adopt(WKPageGroupCreateWithIdentifier(toWK("WebKitTestRunnerPageGroup").get()));
-
+    m_preferences = adoptWK(WKPreferencesCreate());
     m_eventSenderProxy = makeUnique<EventSenderProxy>(this);
 }
 
@@ -778,7 +776,7 @@ WKRetainPtr<WKPageConfigurationRef> TestController::generatePageConfiguration(co
 
     auto pageConfiguration = adoptWK(WKPageConfigurationCreate());
     WKPageConfigurationSetContext(pageConfiguration.get(), m_context.get());
-    WKPageConfigurationSetPageGroup(pageConfiguration.get(), m_pageGroup.get());
+    WKPageConfigurationSetPreferences(pageConfiguration.get(), m_preferences.get());
     
     if (options.useEphemeralSession()) {
         auto ephemeralDataStore = adoptWK(WKWebsiteDataStoreCreateNonPersistentDataStore());
@@ -3148,7 +3146,6 @@ UniqueRef<PlatformWebView> TestController::platformCreateOtherPage(PlatformWebVi
 
 WKContextRef TestController::platformAdjustContext(WKContextRef context, WKContextConfigurationRef)
 {
-    WKPageGroupSetPreferences(m_pageGroup.get(), adoptWK(WKPreferencesCreate()).get());
     return context;
 }
 

Source/WebKit/UIProcess/API/C/WKPage.cpp
@@ -185,7 +185,7 @@ WKContextRef WKPageGetContext(WKPageRef pageRef)
 
 WKPageGroupRef WKPageGetPageGroup(WKPageRef pageRef)
 {
-    return toAPI(&toImpl(pageRef)->pageGroup());
+    return nullptr;
 }
 
 WKPageConfigurationRef WKPageCopyPageConfiguration(WKPageRef pageRef)

Source/WebKit/UIProcess/API/C/WKPage.h
@@ -58,7 +58,7 @@ extern "C" {
 WK_EXPORT WKTypeID WKPageGetTypeID(void);
 
 WK_EXPORT WKContextRef WKPageGetContext(WKPageRef page);
-WK_EXPORT WKPageGroupRef WKPageGetPageGroup(WKPageRef page);
+WK_EXPORT WKPageGroupRef WKPageGetPageGroup(WKPageRef page) WK_C_API_DEPRECATED;
 
 WK_EXPORT WKPageConfigurationRef WKPageCopyPageConfiguration(WKPageRef page);
 

Source/WebKit/UIProcess/API/C/WKPageConfigurationRef.cpp
@@ -55,14 +55,13 @@ void WKPageConfigurationSetContext(WKPageConfigurationRef configuration, WKConte
     toImpl(configuration)->setProcessPool(toImpl(context));
 }
 
-WKPageGroupRef WKPageConfigurationGetPageGroup(WKPageConfigurationRef configuration)
+WKPageGroupRef WKPageConfigurationGetPageGroup(WKPageConfigurationRef)
 {
-    return toAPI(toImpl(configuration)->pageGroup());
+    return nullptr;
 }
 
-void WKPageConfigurationSetPageGroup(WKPageConfigurationRef configuration, WKPageGroupRef pageGroup)
+void WKPageConfigurationSetPageGroup(WKPageConfigurationRef, WKPageGroupRef)
 {
-    toImpl(configuration)->setPageGroup(toImpl(pageGroup));
 }
 
 WKUserContentControllerRef WKPageConfigurationGetUserContentController(WKPageConfigurationRef configuration)

Source/WebKit/UIProcess/API/C/WKPageConfigurationRef.h
@@ -27,6 +27,7 @@
 #define WKPageConfigurationRef_h
 
 #include <WebKit/WKBase.h>
+#include <WebKit/WKDeprecated.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -39,8 +40,8 @@ WK_EXPORT WKPageConfigurationRef WKPageConfigurationCreate(void);
 WK_EXPORT WKContextRef WKPageConfigurationGetContext(WKPageConfigurationRef configuration);
 WK_EXPORT void WKPageConfigurationSetContext(WKPageConfigurationRef configuration, WKContextRef context);
 
-WK_EXPORT WKPageGroupRef WKPageConfigurationGetPageGroup(WKPageConfigurationRef configuration);
-WK_EXPORT void WKPageConfigurationSetPageGroup(WKPageConfigurationRef configuration, WKPageGroupRef pageGroup);
+WK_EXPORT WKPageGroupRef WKPageConfigurationGetPageGroup(WKPageConfigurationRef configuration) WK_C_API_DEPRECATED;
+WK_EXPORT void WKPageConfigurationSetPageGroup(WKPageConfigurationRef configuration, WKPageGroupRef pageGroup) WK_C_API_DEPRECATED;
 
 WK_EXPORT WKUserContentControllerRef WKPageConfigurationGetUserContentController(WKPageConfigurationRef configuration);
 WK_EXPORT void WKPageConfigurationSetUserContentController(WKPageConfigurationRef configuration, WKUserContentControllerRef userContentController);

Source/WebKit/UIProcess/API/C/WKPageGroup.cpp
@@ -26,84 +26,42 @@
 #include "config.h"
 #include "WKPageGroup.h"
 
-#include "APIArray.h"
-#include "APIContentRuleList.h"
-#include "APIContentWorld.h"
-#include "APIUserScript.h"
-#include "APIUserStyleSheet.h"
-#include "InjectUserScriptImmediately.h"
-#include "WKAPICast.h"
-#include "WebPageGroup.h"
-#include "WebPreferences.h"
-#include "WebUserContentController.h"
-#include "WebUserContentControllerProxy.h"
-
-using namespace WebKit;
-
 WKTypeID WKPageGroupGetTypeID()
 {
-    return toAPI(WebPageGroup::APIType);
+    return 0;
 }
 
-WKPageGroupRef WKPageGroupCreateWithIdentifier(WKStringRef identifier)
+WKPageGroupRef WKPageGroupCreateWithIdentifier(WKStringRef)
 {
-    auto pageGroup = WebPageGroup::create(toWTFString(identifier));
-    return toAPI(&pageGroup.leakRef());
+    return nullptr;
 }
 
-void WKPageGroupSetPreferences(WKPageGroupRef pageGroupRef, WKPreferencesRef preferencesRef)
+void WKPageGroupSetPreferences(WKPageGroupRef, WKPreferencesRef)
 {
-    toImpl(pageGroupRef)->setPreferences(toImpl(preferencesRef));
 }
 
-WKPreferencesRef WKPageGroupGetPreferences(WKPageGroupRef pageGroupRef)
+WKPreferencesRef WKPageGroupGetPreferences(WKPageGroupRef)
 {
-    return toAPI(&toImpl(pageGroupRef)->preferences());
+    return nullptr;
 }
 
 WKUserContentControllerRef WKPageGroupGetUserContentController(WKPageGroupRef pageGroupRef)
 {
-    return toAPI(&toImpl(pageGroupRef)->userContentController());
+    return nullptr;
 }
 
-void WKPageGroupAddUserStyleSheet(WKPageGroupRef pageGroupRef, WKStringRef sourceRef, WKURLRef baseURLRef, WKArrayRef allowedURLPatterns, WKArrayRef blockedURLPatterns, WKUserContentInjectedFrames injectedFrames)
+void WKPageGroupAddUserStyleSheet(WKPageGroupRef, WKStringRef, WKURLRef, WKArrayRef, WKArrayRef, WKUserContentInjectedFrames)
 {
-    auto source = toWTFString(sourceRef);
-
-    if (source.isEmpty())
-        return;
-
-    auto baseURLString = toWTFString(baseURLRef);
-    auto allowlist = toImpl(allowedURLPatterns);
-    auto blocklist = toImpl(blockedURLPatterns);
-
-    Ref<API::UserStyleSheet> userStyleSheet = API::UserStyleSheet::create(WebCore::UserStyleSheet { source, baseURLString.isEmpty() ? aboutBlankURL() :  URL { baseURLString }, allowlist ? allowlist->toStringVector() : Vector<String>(), blocklist ? blocklist->toStringVector() : Vector<String>(), toUserContentInjectedFrames(injectedFrames), WebCore::UserStyleUserLevel }, API::ContentWorld::pageContentWorld());
-
-    toImpl(pageGroupRef)->userContentController().addUserStyleSheet(userStyleSheet.get());
 }
 
-void WKPageGroupRemoveAllUserStyleSheets(WKPageGroupRef pageGroup)
+void WKPageGroupRemoveAllUserStyleSheets(WKPageGroupRef)
 {
-    toImpl(pageGroup)->userContentController().removeAllUserStyleSheets();
 }
 
-void WKPageGroupAddUserScript(WKPageGroupRef pageGroupRef, WKStringRef sourceRef, WKURLRef baseURLRef, WKArrayRef allowedURLPatterns, WKArrayRef blockedURLPatterns, WKUserContentInjectedFrames injectedFrames, _WKUserScriptInjectionTime injectionTime)
+void WKPageGroupAddUserScript(WKPageGroupRef, WKStringRef, WKURLRef, WKArrayRef, WKArrayRef, WKUserContentInjectedFrames, _WKUserScriptInjectionTime)
 {
-    auto source = toWTFString(sourceRef);
-
-    if (source.isEmpty())
-        return;
-
-    auto baseURLString = toWTFString(baseURLRef);
-    auto allowlist = toImpl(allowedURLPatterns);
-    auto blocklist = toImpl(blockedURLPatterns);
-    
-    auto url = baseURLString.isEmpty() ? aboutBlankURL() : URL { baseURLString };
-    Ref<API::UserScript> userScript = API::UserScript::create(WebCore::UserScript { WTFMove(source), WTFMove(url), allowlist ? allowlist->toStringVector() : Vector<String>(), blocklist ? blocklist->toStringVector() : Vector<String>(), toUserScriptInjectionTime(injectionTime), toUserContentInjectedFrames(injectedFrames), WebCore::WaitForNotificationBeforeInjecting::No }, API::ContentWorld::pageContentWorld());
-    toImpl(pageGroupRef)->userContentController().addUserScript(userScript.get(), InjectUserScriptImmediately::No);
 }
 
-void WKPageGroupRemoveAllUserScripts(WKPageGroupRef pageGroupRef)
+void WKPageGroupRemoveAllUserScripts(WKPageGroupRef)
 {
-    toImpl(pageGroupRef)->userContentController().removeAllUserScripts();
 }

Source/WebKit/UIProcess/API/C/WKPageGroup.h
@@ -27,6 +27,7 @@
 #define WKPageGroup_h
 
 #include <WebKit/WKBase.h>
+#include <WebKit/WKDeprecated.h>
 #include <WebKit/WKUserContentInjectedFrames.h>
 #include <WebKit/WKUserScriptInjectionTime.h>
 
@@ -34,20 +35,20 @@
 extern "C" {
 #endif
 
-WK_EXPORT WKTypeID WKPageGroupGetTypeID(void);
+WK_EXPORT WKTypeID WKPageGroupGetTypeID(void) WK_C_API_DEPRECATED;
 
-WK_EXPORT WKPageGroupRef WKPageGroupCreateWithIdentifier(WKStringRef identifier);
+WK_EXPORT WKPageGroupRef WKPageGroupCreateWithIdentifier(WKStringRef identifier) WK_C_API_DEPRECATED;
 
-WK_EXPORT void WKPageGroupSetPreferences(WKPageGroupRef pageGroup, WKPreferencesRef preferences);
-WK_EXPORT WKPreferencesRef WKPageGroupGetPreferences(WKPageGroupRef pageGroup);
+WK_EXPORT void WKPageGroupSetPreferences(WKPageGroupRef pageGroup, WKPreferencesRef preferences) WK_C_API_DEPRECATED;
+WK_EXPORT WKPreferencesRef WKPageGroupGetPreferences(WKPageGroupRef pageGroup) WK_C_API_DEPRECATED;
 
-WK_EXPORT WKUserContentControllerRef WKPageGroupGetUserContentController(WKPageGroupRef pageGroup);
+WK_EXPORT WKUserContentControllerRef WKPageGroupGetUserContentController(WKPageGroupRef pageGroup) WK_C_API_DEPRECATED;
 
-WK_EXPORT void WKPageGroupAddUserStyleSheet(WKPageGroupRef pageGroup, WKStringRef source, WKURLRef baseURL, WKArrayRef allowedURLPatterns, WKArrayRef blockedURLPatterns, WKUserContentInjectedFrames);
-WK_EXPORT void WKPageGroupRemoveAllUserStyleSheets(WKPageGroupRef pageGroup);
-    
-WK_EXPORT void WKPageGroupAddUserScript(WKPageGroupRef pageGroup, WKStringRef source, WKURLRef baseURL, WKArrayRef allowedURLPatterns, WKArrayRef blockedURLPatterns, WKUserContentInjectedFrames, _WKUserScriptInjectionTime);
-WK_EXPORT void WKPageGroupRemoveAllUserScripts(WKPageGroupRef pageGroup);
+WK_EXPORT void WKPageGroupAddUserStyleSheet(WKPageGroupRef pageGroup, WKStringRef source, WKURLRef baseURL, WKArrayRef allowedURLPatterns, WKArrayRef blockedURLPatterns, WKUserContentInjectedFrames) WK_C_API_DEPRECATED;
+WK_EXPORT void WKPageGroupRemoveAllUserStyleSheets(WKPageGroupRef pageGroup) WK_C_API_DEPRECATED;
+
+WK_EXPORT void WKPageGroupAddUserScript(WKPageGroupRef pageGroup, WKStringRef source, WKURLRef baseURL, WKArrayRef allowedURLPatterns, WKArrayRef blockedURLPatterns, WKUserContentInjectedFrames, _WKUserScriptInjectionTime) WK_C_API_DEPRECATED;
+WK_EXPORT void WKPageGroupRemoveAllUserScripts(WKPageGroupRef pageGroup) WK_C_API_DEPRECATED;
 
 #ifdef __cplusplus
 }

Source/WebKit/UIProcess/API/Cocoa/WKWebView.mm
@@ -477,16 +477,9 @@ static void hardwareKeyboardAvailabilityChangedCallback(CFNotificationCenterRef,
         pageConfiguration->setWeakWebExtensionController(&controller._webExtensionController);
 #endif
 
-#if PLATFORM(MAC)
-    if (auto pageGroup = WebKit::toImpl([_configuration _pageGroup]))
-        pageConfiguration->setPageGroup(pageGroup);
-    else
-#endif
-    {
-        NSString *groupIdentifier = [_configuration _groupIdentifier];
-        if (groupIdentifier.length)
-            pageConfiguration->setPageGroup(WebKit::WebPageGroup::create(groupIdentifier).ptr());
-    }
+    NSString *groupIdentifier = [_configuration _groupIdentifier];
+    if (groupIdentifier.length)
+        pageConfiguration->setPageGroup(WebKit::WebPageGroup::create(groupIdentifier).ptr());
 
     pageConfiguration->setAdditionalSupportedImageTypes(makeVector<String>([_configuration _additionalSupportedImageTypes]));
 

commit 1cf603b84515a4e55b072398f717fa8eb41034f4

REGRESSION(266167@main): [non-Cocoa] Some http/tests/security/mixedContent tests are failing https://bugs.webkit.org/show_bug.cgi?id=259466
Reviewed by Don Olmstead.

Some http/tests/security/mixedContent tests are failing after
266167@main (bug#259324) due to a preceding tests configuration
change. 266231@main fixed the Cocoa port problem by recreating a
WKPreferencesRef object. non-Cocoa ports also need the similar fix.

* Tools/WebKitTestRunner/TestController.cpp:
(WTR::TestController::platformAdjustContext):
Recreate a WKPreferencesRef object.

Canonical link: https://commits.webkit.org/266298@main

Tools/WebKitTestRunner/TestController.cpp
@@ -3146,6 +3146,7 @@ UniqueRef<PlatformWebView> TestController::platformCreateOtherPage(PlatformWebVi
 
 WKContextRef TestController::platformAdjustContext(WKContextRef context, WKContextConfigurationRef)
 {
+    m_preferences = adoptWK(WKPreferencesCreate());
     return context;
 }
 