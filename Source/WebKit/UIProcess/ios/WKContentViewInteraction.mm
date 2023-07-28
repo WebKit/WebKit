65175743b6594e4416f01632b471641d6795bd0f
Aditya Keerthi <akeerthi@apple.com>
2023-07-28 13:57:52 -0700

    [iOS] Tap to revert does not always display an accurate prompt for multi-word autocorrections https://bugs.webkit.org/show_bug.cgi?id=259595 rdar://113036177
    Reviewed by Wenson Hsieh.
    
    UIKit requires additional information in order to detect that a tapped word is
    part of a multi-word correction. Specifically, they require the entire
    corrected string given a single tapped word, in order to look up the right
    autocorrection record.
    
    To support this requirement, add support for obtaining autocorrected ranges in
    a document editing context. WebKit will provide `autocorrectedRanges` on
    `UIWKDocumentContext`, when the `UIWKDocumentRequest` has
    `UIWKDocumentRequestAutocorrectedRanges` specified in its flags. Then, with the
    existing properties on `UIWKDocumentContext`, UIKit can determine the corrected
    string nearest to the current selection.
    
    * Source/WebCore/dom/DocumentMarkerController.cpp:
    (WebCore::DocumentMarkerController::forEach):
    
    Pass the node into the callback function so that it may be used to obtain a
    `SimpleRange` from the `DocumentMarker`.
    
    (WebCore::DocumentMarkerController::rangesForMarkersInRange):
    
    Return a `SimpleRange` for each `DocumentMarker`.
    
    (WebCore::DocumentMarkerController::hasMarkers):
    (WebCore::DocumentMarkerController::clearDescriptionOnMarkersIntersectingRange):
    * Source/WebCore/dom/DocumentMarkerController.h:
    * Source/WebKit/Platform/spi/ios/UIKitSPI.h:
    * Source/WebKit/Shared/DocumentEditingContext.h:
    * Source/WebKit/Shared/DocumentEditingContext.mm:
    (WebKit::DocumentEditingContext::toPlatformContext):
    (IPC::ArgumentCoder<WebKit::DocumentEditingContext>::encode):
    (IPC::ArgumentCoder<WebKit::DocumentEditingContext>::decode):
    * Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm:
    (toWebDocumentRequestOptions):
    * Source/WebKit/WebProcess/WebPage/ios/WebPageIOS.mm:
    
    Resolve the `SimpleRange` from each marker as a character range, using a
    `SimpleRange` representing the current context.
    
    Populate `context.autocorrectedRanges` to be sent back to the UI process and UIKit.
    
    (WebKit::WebPage::requestDocumentEditingContext):
    * Tools/TestWebKitAPI/Tests/WebKitCocoa/DocumentEditingContext.mm:
    (TEST):
    * Tools/TestWebKitAPI/ios/UIKitSPI.h:
    
    Canonical link: https://commits.webkit.org/266398@main

--- Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
+++ Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -192,6 +192,10 @@
 #import <pal/ios/QuickLookSoftLink.h>
 #import <pal/spi/ios/DataDetectorsUISoftLink.h>
 
+#if HAVE(AUTOCORRECTION_ENHANCEMENTS)
+#define UIWKDocumentRequestAutocorrectedRanges (1 << 7)
+#endif
+
 #if HAVE(LINK_PREVIEW) && USE(UICONTEXTMENU)
 static NSString * const webkitShowLinkPreviewsPreferenceKey = @"WebKitShowLinkPreviews";
 #endif
@@ -9613,6 +9617,10 @@ static inline OptionSet<WebKit::DocumentEditingContextRequest::Options> toWebDoc
         options.add(WebKit::DocumentEditingContextRequest::Options::MarkedTextRects);
     if (flags & UIWKDocumentRequestSpatialAndCurrentSelection)
         options.add(WebKit::DocumentEditingContextRequest::Options::SpatialAndCurrentSelection);
+#if HAVE(AUTOCORRECTION_ENHANCEMENTS)
+    if (flags & UIWKDocumentRequestAutocorrectedRanges)
+        options.add(WebKit::DocumentEditingContextRequest::Options::AutocorrectedRanges);
+#endif
 
     return options;
 }
