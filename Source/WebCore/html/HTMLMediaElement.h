ec1a4f3966b2baf94e25f281dcc8d31346632658
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2023-07-28 01:44:25 -0700

    Implement automatic text track selection for 'metadata' tracks
    https://bugs.webkit.org/show_bug.cgi?id=259581
    rdar://problem/113004825
    
    Reviewed by Eric Carlson.
    
    This patch aligns WebKit with Gecko / Firefox and Blink / Chromium.
    
    Merge: https://src.chromium.org/viewvc/blink?view=revision&revision=189185
    
    Previously 'metadata' tracks were being configured using the "perform
    automatic text track selection" algorithm from the the spec, while it
    should only have applied step 4 of the "honor user preferences for
    automatic text track selection" algorithm.
    
    * Source/WebCore/html/HTMLMediaElement.cpp:
    (HTMLMediaElement::configureMetadataTextTrackGroup): function to handle 'metadata' as per spec
    (HTMLMediaElement::configureTextTrackGroup): for 'metadatatracks' use new function
    * Source/WebCore/html/HTMLMediaElement.h: Add definition for new function
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/track/track-element/track-selection-metadata-expected.txt: Rebaselined
    
    Canonical link: https://commits.webkit.org/266380@main

--- Source/WebCore/html/HTMLMediaElement.h
+++ Source/WebCore/html/HTMLMediaElement.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2007-2023 Apple Inc. All rights reserved.
+ * Copyright (C) 2015 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -403,6 +404,7 @@ public:
     void scheduleConfigureTextTracks();
     void configureTextTracks();
     void configureTextTrackGroup(const TrackGroup&);
+    void configureMetadataTextTrackGroup(const TrackGroup&);
 
     void setSelectedTextTrack(TextTrack*);
 
