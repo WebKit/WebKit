7072a0ccaa41d46f8e8db3063051fea41ff5c2e7
Jonathan Bedard <jbedard@apple.com>
2023-08-02 15:29:28 -0700

    [git-webkit] no-issue should disable radar CC https://bugs.webkit.org/show_bug.cgi?id=259214 rdar://112268906
    Reviewed by Dewei Zhu and Ryan Haddad.
    
    * Tools/Scripts/libraries/webkitscmpy/setup.py: Bump version.
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py: Ditto.
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/pull_request.py:
    (PullRequest.create_pull_request): Disable radar CC when not updating an issue.
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/pull_request_unittest.py:
    
    Canonical link: https://commits.webkit.org/266526@main

--- Tools/Scripts/libraries/webkitscmpy/setup.py
+++ Tools/Scripts/libraries/webkitscmpy/setup.py
@@ -29,7 +29,7 @@ def readme():
 
 setup(
     name='webkitscmpy',
-    version='6.6.4',
+    version='6.6.5',
     description='Library designed to interact with git and svn repositories.',
     long_description=readme(),
     classifiers=[
