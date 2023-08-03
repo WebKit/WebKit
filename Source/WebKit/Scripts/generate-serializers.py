WebKit/WebKit

commit dfde447bae87cf7742a1d28613646004d33f4d86

Generate serialization for LayerProperties https://bugs.webkit.org/show_bug.cgi?id=259626
Reviewed by Brady Eidson.

LayerProperties is basically a bunch of std::optionals, but efficiently packed
with all their bools stored as a bit in an OptionSet.  So it's a tuple of optionals.

This introduces a new primitive, an OptionalTuple that is serialized by sending
a uint64_t of all the bools followed by the tuple members if the bit is 1.
This can also be used for RemoteScrollingCoordinatorTransaction.

I think OptionalTuple is a monad.  It's never been completely clear to me
what is and what is not a monad.

* Source/WebKit/Scripts/generate-serializers.py:
(SerializedType.has_optional_tuple_bits):
(MemberVariable.optional_tuple_bit):
(encode_optional_tuple):
(decode_optional_tuple):
(generate_impl):
(generate_optional_tuple_type_info):
(generate_serialized_type_info):
* Source/WebKit/Scripts/webkit/tests/GeneratedSerializers.cpp:
(IPC::ArgumentCoder<WebKit::LayerProperties>::encode):
(IPC::ArgumentCoder<WebKit::LayerProperties>::decode):
* Source/WebKit/Scripts/webkit/tests/GeneratedSerializers.h:
* Source/WebKit/Scripts/webkit/tests/SerializedTypeInfo.cpp:
(WebKit::allSerializedTypes):
* Source/WebKit/Scripts/webkit/tests/TestSerializedType.serialization.in:
* Source/WebKit/Scripts/webkit/tests/WebKitPlatformGeneratedSerializers.cpp:
* Source/WebKit/Shared/RemoteLayerTree/LayerProperties.h:
(WebKit::RemoteLayerBackingStoreOrProperties::RemoteLayerBackingStoreOrProperties):
* Source/WebKit/Shared/RemoteLayerTree/RemoteLayerBackingStore.h:
* Source/WebKit/Shared/RemoteLayerTree/RemoteLayerBackingStoreCollection.mm:
(WebKit::RemoteLayerBackingStoreCollection::didFlushLayers):
* Source/WebKit/Shared/RemoteLayerTree/RemoteLayerTree.serialization.in:
* Source/WebKit/Shared/RemoteLayerTree/RemoteLayerTreePropertyApplier.mm:
(WebKit::RemoteLayerTreePropertyApplier::applyPropertiesToLayer):
* Source/WebKit/Shared/RemoteLayerTree/RemoteLayerTreeTransaction.mm:

Source/WebKit/Scripts/generate-serializers.py
@@ -45,6 +45,8 @@ import sys
 # Validator - additional C++ to validate the value when decoding
 # NotSerialized - member is present in structure but intentionally not serialized.
 # SecureCodingAllowed - ObjC classes to allow when decoding.
+# OptionalTupleBits - This member stores bits of whether each following member is serialized. Attribute must be on first member.
+# OptionalTupleBit - The name of the bit indicating whether this member is serialized.
 
 class SerializedType(object):
     def __init__(self, struct_or_class, namespace, name, parent_class_name, members, condition, attributes, other_metadata=None):
@@ -123,6 +125,11 @@ class SerializedType(object):
     def serialized_members(self):
         return list(filter(lambda member: 'NotSerialized' not in member.attributes, self.members))
 
+    def has_optional_tuple_bits(self):
+        if len(self.members) == 0:
+            return False
+        return 'OptionalTupleBits' in self.members[0].attributes
+
 
 class SerializedEnum(object):
     def __init__(self, namespace, name, underlying_type, valid_values, condition, attributes):
@@ -168,6 +175,20 @@ class MemberVariable(object):
         self.attributes = attributes
         self.namespace = namespace
         self.is_subclass = is_subclass
+
+    def optional_tuple_bit(self):
+        for attribute in self.attributes:
+            match = re.search(r'OptionalTupleBit=(.*)', attribute)
+            if match:
+                bit, = match.groups()
+                return bit
+        return None
+
+    def optional_tuple_bits(self):
+        for attribute in self.attributes:
+            if attribute == 'OptionalTupleBits':
+                return True
+        return False
 
 
 class EnumMember(object):
@@ -368,7 +389,7 @@ def check_type_members(type, checking_parent_class):
                     result.append('#endif')
             result.append('    };')
             result.append('    static_assert(sizeof(ShouldBeSameSizeAs' + type.name + ') == sizeof(' + type.namespace_and_name() + '));')
-        result.append('    static_assert(MembersInCorrectOrder<0')
+        result.append('    static_assert(MembersInCorrectOrder < 0')
         for member in type.members:
             if 'BitField' in member.attributes:
                 continue
@@ -378,6 +399,63 @@ def check_type_members(type, checking_parent_class):
             if member.condition is not None:
                 result.append('#endif')
         result.append('    >::value);')
+    return result
+
+
+def encode_optional_tuple(type):
+    result = []
+    serialized_members = type.serialized_members()
+    result.append('    static_assert(static_cast<uint64_t>(' + serialized_members[1].optional_tuple_bit() + ') == 1);')
+    result.append('    static_assert(BitsInIncreasingOrder<')
+    for i in range(len(serialized_members)):
+        member = serialized_members[i]
+        bit = member.optional_tuple_bit()
+        if bit is not None:
+            if member.condition is not None:
+                result.append('#if ' + member.condition)
+            result.append('        ' + (', ' if i > 1 else '') + 'static_cast<uint64_t>(' + bit + ')')
+            if member.condition is not None:
+                result.append('#endif')
+    result.append('    >::value);')
+
+    for member in serialized_members:
+        if member.condition is not None:
+            result.append('#if ' + member.condition)
+        if member.optional_tuple_bits():
+            result.append('    encoder << instance.' + member.name + ';')
+            bits_variable_name = member.name
+        bit = member.optional_tuple_bit()
+        if bit is not None:
+            result.append('    if (instance.' + bits_variable_name + ' & ' + bit + ')')
+            result.append('        encoder << instance.' + member.name + ';')
+        if member.condition is not None:
+            result.append('#endif')
+    return result
+
+
+def decode_optional_tuple(type):
+    result = []
+    bits_variable_name = None
+    result.append('    ' + type.namespace_and_name() + ' result;')
+    for member in type.serialized_members():
+        if member.optional_tuple_bits():
+            result.append('    auto bits = decoder.decode<' + member.type + '>();')
+            result.append('    if (!bits)')
+            result.append('        return std::nullopt;')
+            result.append('    result.' + member.name + ' = *bits;')
+        bit = member.optional_tuple_bit()
+        if bit is not None:
+            if member.condition is not None:
+                result.append('#if ' + member.condition)
+            result.append('    if (*bits & ' + bit + ') {')
+            result.append('        if (auto deserialized = decoder.decode<' + member.type + '>())')
+            result.append('            result.' + member.name + ' = WTFMove(*deserialized);')
+            result.append('        else')
+            result.append('            return std::nullopt;')
+            result.append('    }')
+            if member.condition is not None:
+                result.append('#endif')
+        result.append('')
     return result
 
 
@@ -506,9 +584,19 @@ def generate_impl(serialized_types, serialized_enums, headers, generating_webkit
             result.append('#endif')
     result.append('')
     result.append('template<size_t...> struct MembersInCorrectOrder;')
-    result.append('template<size_t onlyOffset> struct MembersInCorrectOrder<onlyOffset> { static constexpr bool value = true; };')
+    result.append('template<size_t onlyOffset> struct MembersInCorrectOrder<onlyOffset> {')
+    result.append('    static constexpr bool value = true;')
+    result.append('};')
     result.append('template<size_t firstOffset, size_t secondOffset, size_t... remainingOffsets> struct MembersInCorrectOrder<firstOffset, secondOffset, remainingOffsets...> {')
     result.append('    static constexpr bool value = firstOffset > secondOffset ? false : MembersInCorrectOrder<secondOffset, remainingOffsets...>::value;')
+    result.append('};')
+    result.append('')
+    result.append('template<uint64_t...> struct BitsInIncreasingOrder;')
+    result.append('template<uint64_t onlyBit> struct BitsInIncreasingOrder<onlyBit> {')
+    result.append('    static constexpr bool value = true;')
+    result.append('};')
+    result.append('template<uint64_t firstBit, uint64_t secondBit, uint64_t... remainingBits> struct BitsInIncreasingOrder<firstBit, secondBit, remainingBits...> {')
+    result.append('    static constexpr bool value = firstBit == secondBit >> 1 && BitsInIncreasingOrder<secondBit, remainingBits...>::value;')
     result.append('};')
     result.append('')
     result.append('template<bool, bool> struct VirtualTableAndRefCountOverhead;')
@@ -577,7 +665,10 @@ def generate_impl(serialized_types, serialized_enums, headers, generating_webkit
             result.append('{')
             if not type.members_are_subclasses:
                 result = result + check_type_members(type, False)
-            result = result + encode_type(type)
+            if type.has_optional_tuple_bits():
+                result = result + encode_optional_tuple(type)
+            else:
+                result = result + encode_type(type)
             if type.members_are_subclasses:
                 result.append('    ASSERT_NOT_REACHED();')
             result.append('}')
@@ -587,10 +678,14 @@ def generate_impl(serialized_types, serialized_enums, headers, generating_webkit
         else:
             result.append('std::optional<' + type.namespace_and_name() + '> ArgumentCoder<' + type.namespace_and_name() + '>::decode(Decoder& decoder)')
         result.append('{')
-        result = result + decode_type(type)
+        if type.has_optional_tuple_bits():
+            result = result + decode_optional_tuple(type)
+        else:
+            result = result + decode_type(type)
         if not type.members_are_subclasses:
-            result.append('    if (UNLIKELY(!decoder.isValid()))')
-            result.append('        return std::nullopt;')
+            if not type.has_optional_tuple_bits():
+                result.append('    if (UNLIKELY(!decoder.isValid()))')
+                result.append('        return std::nullopt;')
             if type.populate_from_empty_constructor:
                 result.append('    ' + type.namespace_and_name() + ' result;')
                 for member in type.serialized_members():
@@ -599,6 +694,8 @@ def generate_impl(serialized_types, serialized_enums, headers, generating_webkit
                     result.append('    result.' + member.name + ' = WTFMove(*' + member.name + ');')
                     if member.condition is not None:
                         result.append('#endif')
+                result.append('    return { WTFMove(result) };')
+            elif type.has_optional_tuple_bits():
                 result.append('    return { WTFMove(result) };')
             else:
                 result.append('    return {')
@@ -686,6 +783,24 @@ def generate_impl(serialized_types, serialized_enums, headers, generating_webkit
     return '\n'.join(result)
 
 
+def generate_optional_tuple_type_info(type):
+    result = []
+    result.append('            {')
+    result.append('                "OptionalTuple<"')
+    serialized_members = type.serialized_members()
+    for i in range(1, len(serialized_members)):
+        member = serialized_members[i]
+        if member.condition is not None:
+            result.append('#if ' + member.condition)
+        result.append('                    "' + (', ' if i > 1 else '') + member.type + '"')
+        if member.condition is not None:
+            result.append('#endif')
+    result.append('                ">"_s,')
+    result.append('                "optionalTuple"_s')
+    result.append('            },')
+    return result
+
+
 def generate_serialized_type_info(serialized_types, serialized_enums, headers, typedefs):
     result = []
     result.append(_license_header)
@@ -708,6 +823,10 @@ def generate_serialized_type_info(serialized_types, serialized_enums, headers, t
     result.append('    return {')
     for type in serialized_types:
         result.append('        { "' + type.namespace_unless_wtf_and_name() + '"_s, {')
+        if type.has_optional_tuple_bits():
+            result = result + generate_optional_tuple_type_info(type)
+            result.append('        } },')
+            continue
         if type.members_are_subclasses:
             result.append('            { "std::variant<' + ', '.join([member.namespace + '::' + member.name for member in type.members]) + '>"_s, "subclasses"_s }')
             result.append('        } },')

Source/WebKit/Shared/RemoteLayerTree/LayerProperties.h
@@ -34,65 +34,76 @@ class RemoteLayerBackingStore;
 class RemoteLayerBackingStoreProperties;
 
 enum class LayerChange : uint64_t {
-    NameChanged                         = 1LLU << 1,
-    ChildrenChanged                     = 1LLU << 2,
-    PositionChanged                     = 1LLU << 3,
-    BoundsChanged                       = 1LLU << 4,
-    BackgroundColorChanged              = 1LLU << 5,
-    AnchorPointChanged                  = 1LLU << 6,
-    BorderWidthChanged                  = 1LLU << 7,
-    BorderColorChanged                  = 1LLU << 8,
-    OpacityChanged                      = 1LLU << 9,
-    TransformChanged                    = 1LLU << 10,
-    SublayerTransformChanged            = 1LLU << 11,
-    HiddenChanged                       = 1LLU << 12,
-    GeometryFlippedChanged              = 1LLU << 13,
-    DoubleSidedChanged                  = 1LLU << 14,
-    MasksToBoundsChanged                = 1LLU << 15,
-    OpaqueChanged                       = 1LLU << 16,
-    ContentsHiddenChanged               = 1LLU << 17,
-    MaskLayerChanged                    = 1LLU << 18,
-    ClonedContentsChanged               = 1LLU << 19,
-    ContentsRectChanged                 = 1LLU << 20,
-    ContentsScaleChanged                = 1LLU << 21,
-    CornerRadiusChanged                 = 1LLU << 22,
-    ShapeRoundedRectChanged             = 1LLU << 23,
-    ShapePathChanged                    = 1LLU << 24,
-    MinificationFilterChanged           = 1LLU << 25,
-    MagnificationFilterChanged          = 1LLU << 26,
-    BlendModeChanged                    = 1LLU << 27,
-    WindRuleChanged                     = 1LLU << 28,
-    SpeedChanged                        = 1LLU << 29,
-    TimeOffsetChanged                   = 1LLU << 30,
-    BackingStoreChanged                 = 1LLU << 31,
-    BackingStoreAttachmentChanged       = 1LLU << 32,
-    FiltersChanged                      = 1LLU << 33,
-    AnimationsChanged                   = 1LLU << 34,
-    AntialiasesEdgesChanged             = 1LLU << 35,
-    CustomAppearanceChanged             = 1LLU << 36,
+    NameChanged                         = 1LLU << 0,
+    TransformChanged                    = 1LLU << 1,
+    SublayerTransformChanged            = 1LLU << 2,
+    ShapeRoundedRectChanged             = 1LLU << 3,
+    ChildrenChanged                     = 1LLU << 4,
+    AnimationsChanged                   = 1LLU << 5,
+    PositionChanged                     = 1LLU << 6,
+    AnchorPointChanged                  = 1LLU << 7,
+    BoundsChanged                       = 1LLU << 8,
+    ContentsRectChanged                 = 1LLU << 9,
+    BackingStoreChanged                 = 1LLU << 10,
+    FiltersChanged                      = 1LLU << 11,
+    ShapePathChanged                    = 1LLU << 12,
+    MaskLayerChanged                    = 1LLU << 13,
+    ClonedContentsChanged               = 1LLU << 14,
+    TimeOffsetChanged                   = 1LLU << 15,
+    SpeedChanged                        = 1LLU << 16,
+    ContentsScaleChanged                = 1LLU << 17,
+    CornerRadiusChanged                 = 1LLU << 18,
+    BorderWidthChanged                  = 1LLU << 19,
+    OpacityChanged                      = 1LLU << 20,
+    BackgroundColorChanged              = 1LLU << 21,
+    BorderColorChanged                  = 1LLU << 22,
+    CustomAppearanceChanged             = 1LLU << 23,
+    MinificationFilterChanged           = 1LLU << 24,
+    MagnificationFilterChanged          = 1LLU << 25,
+    BlendModeChanged                    = 1LLU << 26,
+    WindRuleChanged                     = 1LLU << 27,
+    VideoGravityChanged                 = 1LLU << 28,
+    AntialiasesEdgesChanged             = 1LLU << 29,
+    HiddenChanged                       = 1LLU << 30,
+    BackingStoreAttachmentChanged       = 1LLU << 31,
+    GeometryFlippedChanged              = 1LLU << 32,
+    DoubleSidedChanged                  = 1LLU << 33,
+    MasksToBoundsChanged                = 1LLU << 34,
+    OpaqueChanged                       = 1LLU << 35,
+    ContentsHiddenChanged               = 1LLU << 36,
     UserInteractionEnabledChanged       = 1LLU << 37,
     EventRegionChanged                  = 1LLU << 38,
+#if ENABLE(SCROLLING_THREAD)
     ScrollingNodeIDChanged              = 1LLU << 39,
+#endif
 #if HAVE(CORE_ANIMATION_SEPARATED_LAYERS)
-    SeparatedChanged                    = 1LLU << 40,
+    SeparatedChanged                    = 1LLU << 39,
 #if HAVE(CORE_ANIMATION_SEPARATED_PORTALS)
-    SeparatedPortalChanged              = 1LLU << 41,
-    DescendentOfSeparatedPortalChanged  = 1LLU << 42,
+    SeparatedPortalChanged              = 1LLU << 40,
+    DescendentOfSeparatedPortalChanged  = 1LLU << 41,
 #endif
 #endif
-    VideoGravityChanged                 = 1LLU << 44,
 #if ENABLE(INTERACTION_REGIONS_IN_EVENT_REGION)
-    CoverageRectChanged                 = 1LLU << 45,
+    CoverageRectChanged                 = 1LLU << 42,
 #endif
+};
+
+struct RemoteLayerBackingStoreOrProperties {
+    RemoteLayerBackingStoreOrProperties() = default;
+    ~RemoteLayerBackingStoreOrProperties();
+    RemoteLayerBackingStoreOrProperties(RemoteLayerBackingStoreOrProperties&&) = default;
+    RemoteLayerBackingStoreOrProperties& operator=(RemoteLayerBackingStoreOrProperties&&) = default;
+    RemoteLayerBackingStoreOrProperties(std::unique_ptr<RemoteLayerBackingStoreProperties>&& properties)
+        : properties(WTFMove(properties)) { }
+
+    // Used in the WebContent process.
+    std::unique_ptr<RemoteLayerBackingStore> store;
+    // Used in the UI process.
+    std::unique_ptr<RemoteLayerBackingStoreProperties> properties;
 };
 
 struct LayerProperties {
     WTF_MAKE_STRUCT_FAST_ALLOCATED;
-    LayerProperties();
-    LayerProperties(LayerProperties&&);
-
-    void encode(IPC::Encoder&) const;
-    static WARN_UNUSED_RETURN bool decode(IPC::Decoder&, LayerProperties&);
 
     void notePropertiesChanged(OptionSet<LayerChange> changeFlags)
     {
@@ -115,28 +126,24 @@ struct LayerProperties {
 
     Vector<WebCore::PlatformLayerIdentifier> children;
 
-    Vector<std::pair<String, PlatformCAAnimationRemote::Properties>> addedAnimations;
-    HashSet<String> keysOfAnimationsToRemove;
+    struct AnimationChanges {
+        Vector<std::pair<String, PlatformCAAnimationRemote::Properties>> addedAnimations;
+        HashSet<String> keysOfAnimationsToRemove;
 #if ENABLE(THREADED_ANIMATION_RESOLUTION)
-    Vector<Ref<WebCore::AcceleratedEffect>> effects;
-    WebCore::AcceleratedEffectValues baseValues;
+        Vector<Ref<WebCore::AcceleratedEffect>> effects;
+        WebCore::AcceleratedEffectValues baseValues;
 #endif
+    } animationChanges;
 
     WebCore::FloatPoint3D position;
     WebCore::FloatPoint3D anchorPoint { 0.5, 0.5, 0 };
     WebCore::FloatRect bounds;
     WebCore::FloatRect contentsRect { 0, 0, 1, 1 };
-    // Used in the WebContent process.
-    std::unique_ptr<RemoteLayerBackingStore> backingStore;
-    // Used in the UI process.
-    std::unique_ptr<RemoteLayerBackingStoreProperties> backingStoreProperties;
+    RemoteLayerBackingStoreOrProperties backingStoreOrProperties;
     std::unique_ptr<WebCore::FilterOperations> filters;
     WebCore::Path shapePath;
     Markable<WebCore::PlatformLayerIdentifier> maskLayerID;
     Markable<WebCore::PlatformLayerIdentifier> clonedLayerID;
-#if ENABLE(SCROLLING_THREAD)
-    WebCore::ScrollingNodeID scrollingNodeID { 0 };
-#endif
     double timeOffset { 0 };
     float speed { 1 };
     float contentsScale { 1 };
@@ -162,6 +169,9 @@ struct LayerProperties {
     bool userInteractionEnabled { true };
     WebCore::EventRegion eventRegion;
 
+#if ENABLE(SCROLLING_THREAD)
+    WebCore::ScrollingNodeID scrollingNodeID { 0 };
+#endif
 #if ENABLE(INTERACTION_REGIONS_IN_EVENT_REGION)
     WebCore::FloatRect coverageRect;
 #endif

commit ac423361b4bbf1cad591675161c6d5f2cb4f7185

visionOS build broken after 266533@main https://bugs.webkit.org/show_bug.cgi?id=259788 rdar://113346360
Reviewed by Alex Christensen.

Rearrange some members. Make sure the values don't clash.

Adjust the BitsInIncreasingOrder to test relational size
rather than exact values.

* Source/WebKit/Scripts/generate-serializers.py:
(generate_impl):
* Source/WebKit/Scripts/webkit/tests/GeneratedSerializers.cpp:
* Source/WebKit/Shared/RemoteLayerTree/LayerProperties.h:
* Source/WebKit/Shared/RemoteLayerTree/RemoteLayerTree.serialization.in:
* Source/WebKit/Shared/RemoteLayerTree/RemoteLayerTreePropertyApplier.mm:
(WebKit::RemoteLayerTreePropertyApplier::applyProperties):

Canonical link: https://commits.webkit.org/266549@main

Source/WebKit/Scripts/generate-serializers.py
@@ -596,7 +596,7 @@ def generate_impl(serialized_types, serialized_enums, headers, generating_webkit
     result.append('    static constexpr bool value = true;')
     result.append('};')
     result.append('template<uint64_t firstBit, uint64_t secondBit, uint64_t... remainingBits> struct BitsInIncreasingOrder<firstBit, secondBit, remainingBits...> {')
-    result.append('    static constexpr bool value = firstBit == secondBit >> 1 && BitsInIncreasingOrder<secondBit, remainingBits...>::value;')
+    result.append('    static constexpr bool value = firstBit < secondBit && BitsInIncreasingOrder<secondBit, remainingBits...>::value;')
     result.append('};')
     result.append('')
     result.append('template<bool, bool> struct VirtualTableAndRefCountOverhead;')

Source/WebKit/Shared/RemoteLayerTree/LayerProperties.h
@@ -77,14 +77,14 @@ enum class LayerChange : uint64_t {
     ScrollingNodeIDChanged              = 1LLU << 39,
 #endif
 #if HAVE(CORE_ANIMATION_SEPARATED_LAYERS)
-    SeparatedChanged                    = 1LLU << 39,
+    SeparatedChanged                    = 1LLU << 40,
 #if HAVE(CORE_ANIMATION_SEPARATED_PORTALS)
-    SeparatedPortalChanged              = 1LLU << 40,
-    DescendentOfSeparatedPortalChanged  = 1LLU << 41,
+    SeparatedPortalChanged              = 1LLU << 41,
+    DescendentOfSeparatedPortalChanged  = 1LLU << 42,
 #endif
 #endif
 #if ENABLE(INTERACTION_REGIONS_IN_EVENT_REGION)
-    CoverageRectChanged                 = 1LLU << 42,
+    CoverageRectChanged                 = 1LLU << 43,
 #endif
 };
 
@@ -172,15 +172,15 @@ struct LayerProperties {
 #if ENABLE(SCROLLING_THREAD)
     WebCore::ScrollingNodeID scrollingNodeID { 0 };
 #endif
-#if ENABLE(INTERACTION_REGIONS_IN_EVENT_REGION)
-    WebCore::FloatRect coverageRect;
-#endif
 #if HAVE(CORE_ANIMATION_SEPARATED_LAYERS)
     bool isSeparated { false };
 #if HAVE(CORE_ANIMATION_SEPARATED_PORTALS)
     bool isSeparatedPortal { false };
     bool isDescendentOfSeparatedPortal { false };
 #endif
+#endif
+#if ENABLE(INTERACTION_REGIONS_IN_EVENT_REGION)
+    WebCore::FloatRect coverageRect;
 #endif
 };
 

Source/WebKit/Shared/RemoteLayerTree/RemoteLayerTree.serialization.in
@@ -236,14 +236,14 @@ struct WebKit::LayerProperties {
 #if ENABLE(SCROLLING_THREAD)
     [OptionalTupleBit=WebKit::LayerChange::ScrollingNodeIDChanged] WebCore::ScrollingNodeID scrollingNodeID;
 #endif
-#if ENABLE(INTERACTION_REGIONS_IN_EVENT_REGION)
-    [OptionalTupleBit=WebKit::LayerChange::CoverageRectChanged] WebCore::FloatRect coverageRect;
-#endif
 #if HAVE(CORE_ANIMATION_SEPARATED_LAYERS)
     [OptionalTupleBit=WebKit::LayerChange::SeparatedChanged] bool isSeparated;
 #if HAVE(CORE_ANIMATION_SEPARATED_PORTALS)
     [OptionalTupleBit=WebKit::LayerChange::SeparatedPortalChanged] bool isSeparatedPortal;
     [OptionalTupleBit=WebKit::LayerChange::DescendentOfSeparatedPortalChanged] bool isDescendentOfSeparatedPortal;
 #endif
+#endif
+#if ENABLE(INTERACTION_REGIONS_IN_EVENT_REGION)
+    [OptionalTupleBit=WebKit::LayerChange::CoverageRectChanged] WebCore::FloatRect coverageRect;
 #endif
 };

Source/WebKit/Shared/RemoteLayerTree/RemoteLayerTreePropertyApplier.mm
@@ -323,7 +323,7 @@ void RemoteLayerTreePropertyApplier::applyProperties(RemoteLayerTreeNode& node, 
     applyCommonPropertiesToLayer(node.interactionRegionsLayer(), properties);
     // Replicate animations on the InteractionRegion layers, the LayerTreeHost only keeps track of the original animations.
     if (properties.changedProperties & LayerChange::AnimationsChanged)
-        PlatformCAAnimationRemote::updateLayerAnimations(node.interactionRegionsLayer(), nullptr, properties.addedAnimations, properties.keysOfAnimationsToRemove);
+        PlatformCAAnimationRemote::updateLayerAnimations(node.interactionRegionsLayer(), nullptr, properties.animationChanges.addedAnimations, properties.animationChanges.keysOfAnimationsToRemove);
     if (properties.changedProperties & LayerChange::EventRegionChanged || properties.changedProperties & LayerChange::CoverageRectChanged)
         updateLayersForInteractionRegions(node);
 #endif

Source/WebKit/Scripts/webkit/tests/GeneratedSerializers.cpp
@@ -66,7 +66,7 @@ template<uint64_t onlyBit> struct BitsInIncreasingOrder<onlyBit> {
     static constexpr bool value = true;
 };
 template<uint64_t firstBit, uint64_t secondBit, uint64_t... remainingBits> struct BitsInIncreasingOrder<firstBit, secondBit, remainingBits...> {
-    static constexpr bool value = firstBit == secondBit >> 1 && BitsInIncreasingOrder<secondBit, remainingBits...>::value;
+    static constexpr bool value = firstBit < secondBit && BitsInIncreasingOrder<secondBit, remainingBits...>::value;
 };
 
 template<bool, bool> struct VirtualTableAndRefCountOverhead;