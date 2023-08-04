f9e74ee3b6cd52f21547a3b5c2b8be016ef28936
Philippe Normand <philn@igalia.com>
2023-08-04 04:09:24 -0700

    [GTK] clang warning fix for DrawingAreaCoordinatedGraphics https://bugs.webkit.org/show_bug.cgi?id=259822
    Unreviewed, the forceUpdate method is virtual in the DrawingArea class and should be marked as
    override in the sub-class.
    
    * Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/DrawingAreaCoordinatedGraphics.h:
    
    Canonical link: https://commits.webkit.org/266578@main

--- Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/DrawingAreaCoordinatedGraphics.h
+++ Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/DrawingAreaCoordinatedGraphics.h
@@ -87,7 +87,7 @@ private:
     void targetRefreshRateDidChange(unsigned rate) override;
     void displayDidRefresh() override;
     void setDeviceScaleFactor(float) override;
-    void forceUpdate();
+    void forceUpdate() override;
 
 #if PLATFORM(GTK)
     void adjustTransientZoom(double scale, WebCore::FloatPoint origin) override;
