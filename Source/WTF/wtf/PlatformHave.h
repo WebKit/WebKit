c120af9a6fcc0a48fc230c1e6980da1cfdb722cc
Tim Horton <thorton@apple.com>
2023-08-07 15:32:03 -0700

    Cocoa: Improve backtrace logging for <redacted> symbols https://bugs.webkit.org/show_bug.cgi?id=258821 rdar://111700962
    Reviewed by Wenson Hsieh.
    
    * Source/WTF/wtf/PlatformHave.h:
    * Source/WTF/wtf/StackTrace.h:
    (WTF::StackTraceSymbolResolver::forEach const):
    Enable backtrace_symbols on Cocoa, since we have it.
    If a symbol comes back from dladdr as <redacted>, fall back to the
    backtrace_symbols string, which includes:
    
    - the symbol's image's name
    - the image's DWARF UUID
    - the symbol's address
    
    ...the combination of which can be used to make either better guesses about
    the stack, or to actually symbolicate offline.
    
    The strcmp check matches what `backtrace_symbols` itself does.
    
    Canonical link: https://commits.webkit.org/266662@main

--- Source/WTF/wtf/PlatformHave.h
+++ Source/WTF/wtf/PlatformHave.h
@@ -235,9 +235,6 @@
 
 #if OS(DARWIN) || (OS(LINUX) && defined(__GLIBC__) && !defined(__UCLIBC__) && !CPU(MIPS))
 #define HAVE_BACKTRACE 1
-#endif
-
-#if (OS(DARWIN) || OS(LINUX)) && (PLATFORM(GTK) || PLATFORM(WPE)) && defined(__GLIBC__) && !defined(__UCLIBC__) && !CPU(MIPS)
 #define HAVE_BACKTRACE_SYMBOLS 1
 #endif
 
