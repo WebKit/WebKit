0d4cfda000f801690abc792ef725450b7a3a78da
Charlie Wolfe <charliew@apple.com>
2023-08-07 11:52:18 -0700

    Process Isolation Bypass via navigating to about:* context https://bugs.webkit.org/show_bug.cgi?id=257348 rdar://109853048
    Reviewed by Chris Dumez and J Pascoe.
    
    This patch fixes an issue where we will never swap processes when navigating from an
    about:* page. A compromised WebContent process could leverage this behavior to avoid
    swapping processes when navigating to a cross-origin domain.
    
    This is fixed by checking the following when navigating from an about:* page.
    - The page has not committed a provisional load to a URL that isn't about:*.
    - The source process last loaded a domain from the same origin as the domain we are navigating to.
    
    If neither of these two conditions are met, we won’t reuse the process.
    
    * Source/WebKit/UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::didExplicitOpenForFrame):
    (WebKit::WebPageProxy::didCommitLoadForFrame):
    * Source/WebKit/UIProcess/WebProcessPool.cpp:
    (WebKit::WebProcessPool::processForNavigationInternal):
    * Source/WebKit/UIProcess/WebProcessProxy.h:
    (WebKit::WebProcessProxy::didCommitMeaningfulProvisionalLoad):
    (WebKit::WebProcessProxy::hasCommittedAnyMeaningfulProvisionalLoads const):
    * Tools/TestWebKitAPI/Tests/WebKitCocoa/ProcessSwapOnNavigation.mm:
    
    Originally-landed-as: 259548.792@safari-7615-branch (f52bc7e18a7c). rdar://113174653
    Canonical link: https://commits.webkit.org/266648@main

--- Source/WebKit/UIProcess/WebPageProxy.cpp
+++ Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -5461,6 +5461,8 @@ void WebPageProxy::didExplicitOpenForFrame(FrameIdentifier frameID, URL&& url, S
 
     m_hasCommittedAnyProvisionalLoads = true;
     m_process->didCommitProvisionalLoad();
+    if (!url.protocolIsAbout())
+        m_process->didCommitMeaningfulProvisionalLoad();
 
     internals().pageLoadState.commitChanges();
 }
@@ -5702,6 +5704,8 @@ void WebPageProxy::didCommitLoadForFrame(FrameIdentifier frameID, FrameInfoData&
 
     m_hasCommittedAnyProvisionalLoads = true;
     m_process->didCommitProvisionalLoad();
+    if (!request.url().protocolIsAbout())
+        m_process->didCommitMeaningfulProvisionalLoad();
 
     if (frame->isMainFrame()) {
         m_hasUpdatedRenderingAfterDidCommitLoad = false;
