2e4cd29233b71e17121a3210a7cfe500117464d8
Fujii Hironori <Hironori.Fujii@sony.com>
2023-08-10 16:49:29 -0700

    REGRESSION(261288@main): [Win] TestWebCore.WebCoreBundleTest.BundlePathFromNameTypeDirectory is failing https://bugs.webkit.org/show_bug.cgi?id=259942
    Reviewed by Don Olmstead.
    
    Windows port no longer uses Localizable.strings and
    mediaControlsLocalizedStrings.js. Removed the test.
    
    * Source/WebCore/PlatformWin.cmake:
    * Tools/TestWebKitAPI/Tests/WebCore/win/WebCoreBundle.cpp:
    (TEST_F):
    
    Canonical link: https://commits.webkit.org/266793@main

--- Tools/TestWebKitAPI/Tests/WebCore/win/WebCoreBundle.cpp
+++ Tools/TestWebKitAPI/Tests/WebCore/win/WebCoreBundle.cpp
@@ -65,16 +65,6 @@ TEST_F(WebCoreBundleTest, BundlePathFromNameTypeDirectory)
     auto expected = FileSystem::pathByAppendingComponents(m_root, { "WebInspectorUI"_s, "Protocol"_s, "InspectorBackendCommands.js"_s });
     EXPECT_STREQ(expected.utf8().data(), actual.utf8().data());
 
-    actual = WebCore::webKitBundlePath("Localizable"_s, "strings"_s, ""_s);
-    expected = FileSystem::pathByAppendingComponents(m_root, { "en.lproj"_s, "Localizable.strings"_s });
-    EXPECT_STREQ(expected.utf8().data(), actual.utf8().data());
-
-#if !ENABLE(MODERN_MEDIA_CONTROLS)
-    actual = WebCore::webKitBundlePath("mediaControlsLocalizedStrings"_s, "js"_s, ""_s);
-    expected = FileSystem::pathByAppendingComponents(m_root, { "en.lproj"_s, "mediaControlsLocalizedStrings.js"_s });
-    EXPECT_STREQ(expected.utf8().data(), actual.utf8().data());
-#endif
-
     actual = WebCore::webKitBundlePath("file-does-not"_s, "exist"_s, "file"_s);
     expected = emptyString();
     EXPECT_STREQ(expected.utf8().data(), actual.utf8().data());
