9853d19085bb0a308a87cd21447a9504c20d5427
Antti Koivisto <antti@apple.com>
2023-08-23 09:48:17 -0700

    [CSS Container Queries] Container unit resolution should check the selected container is eligible https://bugs.webkit.org/show_bug.cgi?id=260561 rdar://problem/114291153
    Reviewed by Alan Baradlay.
    
    * LayoutTests/imported/w3c/web-platform-tests/css/css-contain/container-queries/container-units-ineligible-container-expected.txt:
    * Source/WebCore/css/CSSPrimitiveValue.cpp:
    (WebCore::CSSPrimitiveValue::computeNonCalcLengthDouble):
    
    Loop until an eligible container is found.
    
    * Source/WebCore/css/query/ContainerQueryFeatures.cpp:
    * Source/WebCore/rendering/RenderElement.cpp:
    (WebCore::RenderElement::hasEligibleContainmentForSizeQuery const):
    
    Factor into a shared function.
    
    * Source/WebCore/rendering/RenderElement.h:
    
    Canonical link: https://commits.webkit.org/267192@main

--- Source/WebCore/css/CSSPrimitiveValue.cpp
+++ Source/WebCore/css/CSSPrimitiveValue.cpp
@@ -787,17 +787,15 @@ double CSSPrimitiveValue::computeNonCalcLengthDouble(const CSSToLengthConversion
         if (!element)
             return { };
 
-        // FIXME: Use cached query containers when available.
-        auto* container = Style::ContainerQueryEvaluator::selectContainer(physicalAxis, nullString(), *element);
-        if (!container)
-            return { };
-
-        auto* containerRenderer = dynamicDowncast<RenderBox>(container->renderer());
-        if (!containerRenderer)
-            return { };
-
-        auto widthOrHeight = physicalAxis == CQ::Axis::Width ? containerRenderer->contentWidth() : containerRenderer->contentHeight();
-        return widthOrHeight * value / 100;
+        // "The query container for each axis is the nearest ancestor container that accepts container size queries on that axis."
+        while ((element = Style::ContainerQueryEvaluator::selectContainer(physicalAxis, nullString(), *element))) {
+            auto* containerRenderer = dynamicDowncast<RenderBox>(element->renderer());
+            if (containerRenderer && containerRenderer->hasEligibleContainmentForSizeQuery()) {
+                auto widthOrHeight = physicalAxis == CQ::Axis::Width ? containerRenderer->contentWidth() : containerRenderer->contentHeight();
+                return widthOrHeight * value / 100;
+            }
+        }
+        return { };
     };
 
     switch (primitiveType) {
