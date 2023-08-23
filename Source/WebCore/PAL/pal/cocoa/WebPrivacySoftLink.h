WebKit/WebKit

commit ccf8724c8c3407cb21486abe4b0c0437c8b5d31b

Remote list updates for link decoration filtering fail in Safari 17 https://bugs.webkit.org/show_bug.cgi?id=260391 rdar://114047664
Reviewed by Tim Horton.

On macOS Monterey (but not macOS Ventura or later), calling:

```
dlopen("/System/Library/PrivateFrameworks/WebPrivacy.framework/WebPrivacy", RTLD_NOW);
```

...fails to load WebPrivacy.framework from the Safari staged framework directory. This causes
`PAL::isWebPrivacyFrameworkAvailable()` to return `false`, which in turn breaks link decoration
filtering when advanced privacy protections are enabled. In comparison, the main built-in tracker
blocker loaded by Safari actually *successfully* loads, because we only use `objc_getClass` to look
up `WPResources`, and don't depend on a successful `dlopen`.

On downlevels, this call to `dlopen` is actually unnecessary, since we already link WebPrivacy via
`-weak_framework`; as such, it's sufficient to simply check whether any one of the framework API
classes (e.g. `WPResources`) have been loaded.

* Source/WebCore/PAL/pal/cocoa/WebPrivacySoftLink.h:
* Source/WebCore/PAL/pal/cocoa/WebPrivacySoftLink.mm:
* Source/WebKit/Platform/cocoa/WebPrivacyHelpers.mm:

Also, remove unnecessary soft link helpers for `WPNetworkAddressRange` (we don't need this because
we never need to create one of these in WebKit).

(WebKit::canUseWebPrivacyFramework):
(WebKit::resourceDataChangedNotificationName):
(WebKit::notificationUserInfoResourceTypeKey):
(-[WKWebPrivacyNotificationListener init]):
(-[WKWebPrivacyNotificationListener dealloc]):
(-[WKWebPrivacyNotificationListener didUpdate:]):
(WebKit::LinkDecorationFilteringController::updateStrings):
(WebKit::requestLinkDecorationFilteringData):
(WebKit::TrackerAddressLookupInfo::populateIfNeeded):
(WebKit::TrackerDomainLookupInfo::populateIfNeeded):

Canonical link: https://commits.webkit.org/267051@main

Source/WebCore/PAL/pal/cocoa/WebPrivacySoftLink.h
@@ -31,11 +31,10 @@
 #import <wtf/SoftLinking.h>
 
 SOFT_LINK_FRAMEWORK_FOR_HEADER(PAL, WebPrivacy)
-SOFT_LINK_CLASS_FOR_HEADER(PAL, WPNetworkAddressRange)
 SOFT_LINK_CLASS_FOR_HEADER(PAL, WPResourceRequestOptions)
 SOFT_LINK_CLASS_FOR_HEADER(PAL, WPResources)
 SOFT_LINK_CLASS_FOR_HEADER(PAL, WPLinkFilteringData)
-SOFT_LINK_CONSTANT_FOR_HEADER(PAL, WebPrivacy, WPNotificationUserInfoResourceTypeKey, NSString *)
-SOFT_LINK_CONSTANT_FOR_HEADER(PAL, WebPrivacy, WPResourceDataChangedNotificationName, NSNotificationName)
+SOFT_LINK_CONSTANT_MAY_FAIL_FOR_HEADER(PAL, WebPrivacy, WPNotificationUserInfoResourceTypeKey, NSString *)
+SOFT_LINK_CONSTANT_MAY_FAIL_FOR_HEADER(PAL, WebPrivacy, WPResourceDataChangedNotificationName, NSNotificationName)
 
 #endif // ENABLE(ADVANCED_PRIVACY_PROTECTIONS)

Source/WebCore/PAL/pal/cocoa/WebPrivacySoftLink.mm
@@ -31,11 +31,10 @@
 #import <wtf/SoftLinking.h>
 
 SOFT_LINK_PRIVATE_FRAMEWORK_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, PAL_EXPORT)
-SOFT_LINK_CLASS_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, WPNetworkAddressRange, PAL_EXPORT)
-SOFT_LINK_CLASS_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, WPResourceRequestOptions, PAL_EXPORT)
-SOFT_LINK_CLASS_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, WPResources, PAL_EXPORT)
-SOFT_LINK_CLASS_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, WPLinkFilteringData, PAL_EXPORT)
-SOFT_LINK_CONSTANT_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, WPNotificationUserInfoResourceTypeKey, NSString *, PAL_EXPORT)
-SOFT_LINK_CONSTANT_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, WPResourceDataChangedNotificationName, NSNotificationName, PAL_EXPORT)
+SOFT_LINK_CLASS_FOR_SOURCE_OPTIONAL_WITH_EXPORT(PAL, WebPrivacy, WPResourceRequestOptions, PAL_EXPORT)
+SOFT_LINK_CLASS_FOR_SOURCE_OPTIONAL_WITH_EXPORT(PAL, WebPrivacy, WPResources, PAL_EXPORT)
+SOFT_LINK_CLASS_FOR_SOURCE_OPTIONAL_WITH_EXPORT(PAL, WebPrivacy, WPLinkFilteringData, PAL_EXPORT)
+SOFT_LINK_CONSTANT_MAY_FAIL_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, WPNotificationUserInfoResourceTypeKey, NSString *, PAL_EXPORT)
+SOFT_LINK_CONSTANT_MAY_FAIL_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, WPResourceDataChangedNotificationName, NSNotificationName, PAL_EXPORT)
 
 #endif // ENABLE(ADVANCED_PRIVACY_PROTECTIONS)

Source/WebKit/Platform/cocoa/WebPrivacyHelpers.mm
@@ -45,6 +45,49 @@ SOFT_LINK_LIBRARY_OPTIONAL(libnetwork)
 SOFT_LINK_OPTIONAL(libnetwork, nw_context_set_tracker_lookup_callback, void, __cdecl, (nw_context_t, nw_context_tracker_lookup_callback_t))
 #endif
 
+namespace WebKit {
+
+static bool canUseWebPrivacyFramework()
+{
+#if HAVE(SYSTEM_SUPPORT_FOR_ADVANCED_PRIVACY_PROTECTIONS)
+    return PAL::isWebPrivacyFrameworkAvailable();
+#else
+    // On macOS Monterey where WebPrivacy is present as a staged framework, attempts to soft-link the framework may fail.
+    // Instead of using dlopen, we instead check for the presence of `WPResources`; the call to dlopen is not necessary because
+    // we weak-link against the framework, so the class should be present as long as the framework has been successfully linked.
+    // FIXME: This workaround can be removed once we drop support for macOS Monterey, and we can use the standard soft-linking
+    // helpers from WebPrivacySoftLink.
+    static bool hasWPResourcesClass = [&] {
+        return !!PAL::getWPResourcesClass();
+    }();
+    return hasWPResourcesClass;
+#endif
+}
+
+static NSNotificationName resourceDataChangedNotificationName()
+{
+#if HAVE(SYSTEM_SUPPORT_FOR_ADVANCED_PRIVACY_PROTECTIONS)
+    return PAL::get_WebPrivacy_WPResourceDataChangedNotificationName();
+#else
+    // FIXME: This workaround can be removed once we drop support for macOS Monterey, and we can use the standard soft-linking
+    // helpers from WebPrivacySoftLink.
+    return @"WPResourceDataChangedNotificationName";
+#endif
+}
+
+static NSString *notificationUserInfoResourceTypeKey()
+{
+#if HAVE(SYSTEM_SUPPORT_FOR_ADVANCED_PRIVACY_PROTECTIONS)
+    return PAL::get_WebPrivacy_WPNotificationUserInfoResourceTypeKey();
+#else
+    // FIXME: This workaround can be removed once we drop support for macOS Monterey, and we can use the standard soft-linking
+    // helpers from WebPrivacySoftLink.
+    return @"ResourceType";
+#endif
+}
+
+} // namespace WebKit
+
 @interface WKWebPrivacyNotificationListener : NSObject
 
 @end
@@ -58,8 +101,8 @@ SOFT_LINK_OPTIONAL(libnetwork, nw_context_set_tracker_lookup_callback, void, __c
     if (!(self = [super init]))
         return nil;
 
-    if (PAL::isWebPrivacyFrameworkAvailable())
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didUpdate:) name:PAL::get_WebPrivacy_WPResourceDataChangedNotificationName() object:nil];
+    if (WebKit::canUseWebPrivacyFramework())
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didUpdate:) name:WebKit::resourceDataChangedNotificationName() object:nil];
     return self;
 }
 
@@ -70,15 +113,15 @@ SOFT_LINK_OPTIONAL(libnetwork, nw_context_set_tracker_lookup_callback, void, __c
 
 - (void)dealloc
 {
-    if (PAL::isWebPrivacyFrameworkAvailable())
-        [[NSNotificationCenter defaultCenter] removeObserver:self name:PAL::get_WebPrivacy_WPResourceDataChangedNotificationName() object:nil];
+    if (WebKit::canUseWebPrivacyFramework())
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:WebKit::resourceDataChangedNotificationName() object:nil];
     [super dealloc];
 }
 
 - (void)didUpdate:(NSNotification *)notification
 {
-    ASSERT(PAL::isWebPrivacyFrameworkAvailable());
-    auto type = dynamic_objc_cast<NSNumber>([notification.userInfo objectForKey:PAL::get_WebPrivacy_WPNotificationUserInfoResourceTypeKey()]);
+    ASSERT(WebKit::canUseWebPrivacyFramework());
+    auto type = dynamic_objc_cast<NSNumber>([notification.userInfo objectForKey:WebKit::notificationUserInfoResourceTypeKey()]);
     if (!type)
         return;
 
@@ -120,7 +163,7 @@ void LinkDecorationFilteringController::setCachedStrings(Vector<WebCore::LinkDec
 
 void LinkDecorationFilteringController::updateStrings(CompletionHandler<void()>&& callback)
 {
-    if (!PAL::isWebPrivacyFrameworkAvailable()) {
+    if (!WebKit::canUseWebPrivacyFramework()) {
         callback();
         return;
     }
@@ -153,7 +196,7 @@ void LinkDecorationFilteringController::updateStrings(CompletionHandler<void()>&
 using LinkFilteringRulesCallback = CompletionHandler<void(Vector<WebCore::LinkDecorationFilteringData>&&)>;
 void requestLinkDecorationFilteringData(LinkFilteringRulesCallback&& callback)
 {
-    if (!PAL::isWebPrivacyFrameworkAvailable()) {
+    if (!WebKit::canUseWebPrivacyFramework()) {
         callback({ });
         return;
     }
@@ -258,7 +301,7 @@ public:
     {
         static std::once_flag onceFlag;
         std::call_once(onceFlag, [&] {
-            if (!PAL::isWebPrivacyFrameworkAvailable())
+            if (!WebKit::canUseWebPrivacyFramework())
                 return;
 
             auto options = adoptNS([PAL::allocWPResourceRequestOptionsInstance() init]);
@@ -388,7 +431,7 @@ public:
     {
         static std::once_flag onceFlag;
         std::call_once(onceFlag, [&] {
-            if (!PAL::isWebPrivacyFrameworkAvailable())
+            if (!WebKit::canUseWebPrivacyFramework())
                 return;
 
             static BOOL canRequestTrackerDomainNames = [] {

commit 546db343245acbd1394972e65da6c9056fe3b166

[iOS 17+, Sonoma] REGRESSION (267051@main): Safari no longer responds to WebPrivacy list changes https://bugs.webkit.org/show_bug.cgi?id=260569 rdar://114288616
Reviewed by Aditya Keerthi.

Several API tests in `TestWebKitAPI.AdvancedPrivacyProtections` began to fail after the changes in
267051@main; in particular, they're failing because I changed the `SOFT_LINK_CONSTANT_FOR_HEADER`
around `WPNotificationUserInfoResourceTypeKey` to `SOFT_LINK_CONSTANT_MAY_FAIL_FOR_HEADER`, the
latter of which never attempts to load will return `nil` unless we call
`PAL::canLoad_WebPrivacy_WPResourceDataChangedNotificationName()` first (the full macro expands to
the following):

```
namespace PAL {

static NSNotificationName constantWebPrivacyWPResourceDataChangedNotificationName;

bool init_WebPrivacy_WPResourceDataChangedNotificationName();
bool init_WebPrivacy_WPResourceDataChangedNotificationName()
{
    void* constant = dlsym(WebPrivacyLibrary(), "WPResourceDataChangedNotificationName");
    if (!constant)
        return false;
    constantWebPrivacyWPResourceDataChangedNotificationName = *static_cast<NSNotificationName const *>(constant);
    return true;
}

PAL_EXPORT bool canLoad_WebPrivacy_WPResourceDataChangedNotificationName();
bool canLoad_WebPrivacy_WPResourceDataChangedNotificationName()
{
    static bool loaded = init_WebPrivacy_WPResourceDataChangedNotificationName();
    return loaded;
}

PAL_EXPORT NSNotificationName get_WebPrivacy_WPResourceDataChangedNotificationName();
NSNotificationName get_WebPrivacy_WPResourceDataChangedNotificationName()
{
    return constantWebPrivacyWPResourceDataChangedNotificationName;
}

}
```

...in contrast, the original macro `SOFT_LINK_CONSTANT_FOR_HEADER` expands to:

```
namespace PAL {

PAL_EXPORT NSNotificationName get_WebPrivacy_WPResourceDataChangedNotificationName();
NSNotificationName get_WebPrivacy_WPResourceDataChangedNotificationName()
{
    static NSNotificationName constantWebPrivacyWPResourceDataChangedNotificationName;
    static dispatch_once_t once;
    dispatch_once(&once, ^{
        void* constant = dlsym(WebPrivacyLibrary(), "WPResourceDataChangedNotificationName");
        RELEASE_ASSERT_WITH_MESSAGE(constant, "%s", dlerror());
        constantWebPrivacyWPResourceDataChangedNotificationName = *static_cast<NSNotificationName const *>(constant);
    });
    return constantWebPrivacyWPResourceDataChangedNotificationName;
}

}
```

This exposes a real bug caused by 267051@main, where we no longer listen for the resource data

Source/WebCore/PAL/pal/cocoa/WebPrivacySoftLink.h
@@ -34,7 +34,7 @@ SOFT_LINK_FRAMEWORK_FOR_HEADER(PAL, WebPrivacy)
 SOFT_LINK_CLASS_FOR_HEADER(PAL, WPResourceRequestOptions)
 SOFT_LINK_CLASS_FOR_HEADER(PAL, WPResources)
 SOFT_LINK_CLASS_FOR_HEADER(PAL, WPLinkFilteringData)
-SOFT_LINK_CONSTANT_MAY_FAIL_FOR_HEADER(PAL, WebPrivacy, WPNotificationUserInfoResourceTypeKey, NSString *)
-SOFT_LINK_CONSTANT_MAY_FAIL_FOR_HEADER(PAL, WebPrivacy, WPResourceDataChangedNotificationName, NSNotificationName)
+SOFT_LINK_CONSTANT_FOR_HEADER(PAL, WebPrivacy, WPNotificationUserInfoResourceTypeKey, NSString *)
+SOFT_LINK_CONSTANT_FOR_HEADER(PAL, WebPrivacy, WPResourceDataChangedNotificationName, NSNotificationName)
 
 #endif // ENABLE(ADVANCED_PRIVACY_PROTECTIONS)

Source/WebCore/PAL/pal/cocoa/WebPrivacySoftLink.mm
@@ -34,7 +34,7 @@ SOFT_LINK_PRIVATE_FRAMEWORK_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, PAL_EXPORT)
 SOFT_LINK_CLASS_FOR_SOURCE_OPTIONAL_WITH_EXPORT(PAL, WebPrivacy, WPResourceRequestOptions, PAL_EXPORT)
 SOFT_LINK_CLASS_FOR_SOURCE_OPTIONAL_WITH_EXPORT(PAL, WebPrivacy, WPResources, PAL_EXPORT)
 SOFT_LINK_CLASS_FOR_SOURCE_OPTIONAL_WITH_EXPORT(PAL, WebPrivacy, WPLinkFilteringData, PAL_EXPORT)
-SOFT_LINK_CONSTANT_MAY_FAIL_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, WPNotificationUserInfoResourceTypeKey, NSString *, PAL_EXPORT)
-SOFT_LINK_CONSTANT_MAY_FAIL_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, WPResourceDataChangedNotificationName, NSNotificationName, PAL_EXPORT)
+SOFT_LINK_CONSTANT_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, WPNotificationUserInfoResourceTypeKey, NSString *, PAL_EXPORT)
+SOFT_LINK_CONSTANT_FOR_SOURCE_WITH_EXPORT(PAL, WebPrivacy, WPResourceDataChangedNotificationName, NSNotificationName, PAL_EXPORT)
 
 #endif // ENABLE(ADVANCED_PRIVACY_PROTECTIONS)