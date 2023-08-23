bd8988b5b5928193ec58339b63b7b4056ca561ef
Myah Cobbs <mcobbs@apple.com>
2023-08-23 14:23:46 -0700

    Revert "Cherry-pick ad4d214a77c0. rdar://110354414"
    This reverts commit a04291c8431c2b6ed7bcc23dc35b349a2c521dc2.
    
    Identifier: 265423.925@safari-7616-branch

--- Source/WebKit/UIProcess/ios/fullscreen/WKFullScreenWindowControllerIOS.mm
+++ Source/WebKit/UIProcess/ios/fullscreen/WKFullScreenWindowControllerIOS.mm
@@ -1792,8 +1792,7 @@ static constexpr NSString *kPrefersFullScreenDimmingKey = @"WebKitPrefersFullScr
             [inWindow setFrame:scene.coordinateSpace.bounds];
 
             if (enter) {
-                if ([controller _sceneAspectRatioLockingEnabled])
-                    scene.mrui_placement.preferredResizingBehavior = MRUISceneResizingBehaviorUniform;
+                scene.mrui_placement.preferredResizingBehavior = MRUISceneResizingBehaviorUniform;
                 scene.delegate = adoptNS([[WKFullscreenWindowSceneDelegate alloc] initWithController:controller.get() originalDelegate:scene.delegate]).get();
             } else {
                 scene.sizeRestrictions.minimumSize = [originalState sceneMinimumSize];
