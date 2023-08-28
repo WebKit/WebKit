504cb3567c094a4118abbe6812443cdd431fb374
Anne van Kesteren <annevk@annevk.nl>
2023-08-28 03:15:13 -0700

    Support <svg:image crossorigin> https://bugs.webkit.org/show_bug.cgi?id=260724 rdar://114462749
    Reviewed by Youenn Fablet.
    
    Add support for the crossorigin attribute to SVGImageElement in a
    manner analogous to HTMLImageElement.
    
    Also remove a duplicate parser for the crossorigin attribute in
    HTMLImageElement.
    
    * LayoutTests/imported/w3c/web-platform-tests/svg/idlharness.window-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/webcodecs/videoFrame-construction.crossOriginSource.sub.html:
    * Source/WebCore/html/HTMLImageElement.cpp:
    (WebCore::HTMLImageElement::attributeChanged):
    (): Deleted.
    (WebCore::parseCrossoriginState): Deleted.
    * Source/WebCore/svg/SVGImageElement.cpp:
    (WebCore::SVGImageElement::attributeChanged):
    (WebCore::SVGImageElement::setCrossOrigin):
    (WebCore::SVGImageElement::crossOrigin const):
    * Source/WebCore/svg/SVGImageElement.h:
    * Source/WebCore/svg/SVGImageElement.idl:
    
    Canonical link: https://commits.webkit.org/267349@main

--- Source/WebCore/html/HTMLImageElement.cpp
+++ Source/WebCore/html/HTMLImageElement.cpp
@@ -344,14 +344,6 @@ bool HTMLImageElement::hasLazyLoadableAttributeValue(StringView attributeValue)
     return equalLettersIgnoringASCIICase(attributeValue, "lazy"_s);
 }
 
-enum CrossOriginState { NotSet, UseCredentials, Anonymous };
-static CrossOriginState parseCrossoriginState(const AtomString& crossoriginValue)
-{
-    if (crossoriginValue.isNull())
-        return NotSet;
-    return equalLettersIgnoringASCIICase(crossoriginValue, "use-credentials"_s) ? UseCredentials : Anonymous;
-}
-
 void HTMLImageElement::attributeChanged(const QualifiedName& name, const AtomString& oldValue, const AtomString& newValue, AttributeModificationReason attributeModificationReason)
 {
     HTMLElement::attributeChanged(name, oldValue, newValue, attributeModificationReason);
@@ -394,7 +386,7 @@ void HTMLImageElement::attributeChanged(const QualifiedName& name, const AtomStr
         }
         break;
     case AttributeNames::crossoriginAttr:
-        if (parseCrossoriginState(oldValue) != parseCrossoriginState(newValue))
+        if (parseCORSSettingsAttribute(oldValue) != parseCORSSettingsAttribute(newValue))
             m_imageLoader->updateFromElementIgnoringPreviousError(RelevantMutation::Yes);
         break;
     case AttributeNames::nameAttr: {
