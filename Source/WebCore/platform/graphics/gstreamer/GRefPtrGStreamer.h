8f58a5833296d21aff427088a0789aaafc36bcb0
Philippe Normand <philn@igalia.com>
2023-08-28 01:34:28 -0700

    Cherry-pick 267345@main (37bc74274076). https://bugs.webkit.org/show_bug.cgi?id=260649
        [GStreamer] Prevent a crash when fetching data on stopped MediaRecorder
        https://bugs.webkit.org/show_bug.cgi?id=260649
        rdar://problem/114370120
    
        Reviewed by Xabier Rodriguez-Calvar.
    
        The backend (GStreamer transcoder) is now clearly separated from the MediaRecorderPrivate, so that
        fetchData() can create a weak pointer to be used from the main thread. If the backend was destroyed
        in-flight no unsafe memory access is performed.
    
        Test: http/wpt/mediarecorder/MediaRecorder-start-stop-crash.html
        Canonical link: https://commits.webkit.org/267345@main

--- Source/WebCore/platform/graphics/gstreamer/GRefPtrGStreamer.h
+++ Source/WebCore/platform/graphics/gstreamer/GRefPtrGStreamer.h
@@ -49,6 +49,11 @@ typedef struct _GstRTPHeaderExtension GstRTPHeaderExtension;
 typedef struct _GstWebRTCICE GstWebRTCICE;
 #endif
 
+#if USE(GSTREAMER_TRANSCODER)
+typedef struct _GstTranscoder GstTranscoder;
+typedef struct _GstTranscoderSignalAdapter GstTranscoderSignalAdapter;
+#endif
+
 namespace WTF {
 
 template<> GRefPtr<GstPlugin> adoptGRef(GstPlugin* ptr);
@@ -221,6 +226,16 @@ template<> GstWebRTCICE* refGPtr<GstWebRTCICE>(GstWebRTCICE*);
 template<> void derefGPtr<GstWebRTCICE>(GstWebRTCICE*);
 
 #endif
+
+#if USE(GSTREAMER_TRANSCODER)
+template<> GRefPtr<GstTranscoder> adoptGRef(GstTranscoder*);
+template<> GstTranscoder* refGPtr<GstTranscoder>(GstTranscoder*);
+template<> void derefGPtr<GstTranscoder>(GstTranscoder*);
+
+template<> GRefPtr<GstTranscoderSignalAdapter> adoptGRef(GstTranscoderSignalAdapter*);
+template<> GstTranscoderSignalAdapter* refGPtr<GstTranscoderSignalAdapter>(GstTranscoderSignalAdapter*);
+template<> void derefGPtr<GstTranscoderSignalAdapter>(GstTranscoderSignalAdapter*);
+#endif // USE(GSTREAMER_TRANSCODER)
 
 } // namespace WTF
 
