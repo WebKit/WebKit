ad247f639619f2ec019a41bfaea5904f80d9c6b2
Michael Catanzaro <mcatanzaro@redhat.com>
2023-08-28 10:11:04 -0500

    python autoinstaller is broken with python 3.12 https://bugs.webkit.org/show_bug.cgi?id=260726
    Reviewed by NOBODY (OOPS!).
    
    Currently git-webkit is broken with python 3.12 due to various problems
    with dependencies that are already fixed in newer upstream versions. So,
    update a few things.
    
    Unfortunately, the newer library versions are incompatible with other
    Python versions, so this means more conditional versions.
    
    * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/__init__.py:
    
    Canonical link: https://commits.webkit.org/266719.23@webkitglib/2.42

--- Tools/Scripts/libraries/webkitcorepy/webkitcorepy/__init__.py
+++ Tools/Scripts/libraries/webkitcorepy/webkitcorepy/__init__.py
@@ -53,12 +53,17 @@ version = Version(0, 16, 3)
 from webkitcorepy.autoinstall import Package, AutoInstall
 if sys.version_info > (3, 0):
     AutoInstall.register(Package('mock', Version(4)))
-    AutoInstall.register(Package('setuptools', Version(56, 0, 0)))
 else:
     AutoInstall.register(Package('mock', Version(3, 0, 5)))
-    AutoInstall.register(Package('setuptools', Version(44, 1, 1)))
     if platform.system() == 'Windows':
         AutoInstall.register(Package('win_inet_pton', Version(1, 1, 0), pypi_name='win-inet-pton'))
+
+if sys.version_info >= (3, 8):
+    AutoInstall.register(Package('setuptools', Version(68, 1, 2)))
+elif sys.version_info >= (3, 0):
+    AutoInstall.register(Package('setuptools', Version(56, 0, 0)))
+else:
+    AutoInstall.register(Package('setuptools', Version(44, 1, 1)))
 
 if sys.version_info >= (3, 6):
     AutoInstall.register(Package('certifi', Version(2022, 12, 7)))
@@ -77,12 +82,22 @@ else:
     AutoInstall.register(Package('packaging', Version(20, 4)))
 
 AutoInstall.register(Package('pyparsing', Version(2, 4, 7)))
-AutoInstall.register(Package('requests', Version(2, 24)))
+
+if sys.version_info > (3, 7):
+    AutoInstall.register(Package('requests', Version(2, 31, 0)))
+else:
+    AutoInstall.register(Package('requests', Version(2, 24)))
+
 AutoInstall.register(Package('setuptools_scm', Version(5, 0, 2), pypi_name='setuptools-scm'))
 AutoInstall.register(Package('socks', Version(1, 7, 1), pypi_name='PySocks'))
 AutoInstall.register(Package('six', Version(1, 15, 0)))
 AutoInstall.register(Package('tblib', Version(1, 7, 0)))
-AutoInstall.register(Package('urllib3', Version(1, 25, 10)))
+
+if sys.version_info > (3, 7):
+    AutoInstall.register(Package('urllib3', Version(2, 0, 4)))
+else:
+    AutoInstall.register(Package('urllib3', Version(1, 25, 10)))
+
 AutoInstall.register(Package('wheel', Version(0, 35, 1)))
 AutoInstall.register(Package('whichcraft', Version(0, 6, 1)))
 AutoInstall.register(Package('cffi', Version(1, 15, 1)))
