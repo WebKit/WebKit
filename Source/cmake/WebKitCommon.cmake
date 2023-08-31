f6b1fa7647c7e2b2f4eefbb45c79dbd27d5479ab
Don Olmstead <don.olmstead@sony.com>
2023-08-31 14:38:01 -0700

    Use generator to set CMAKE_EXPORT_COMPILE_COMMANDS https://bugs.webkit.org/show_bug.cgi?id=260666
    Reviewed by Michael Catanzaro.
    
    In 266947@main a target `RewriteCompileCommands` was added for unified
    builds where `CMAKE_EXPORT_COMPILE_COMMANDS` is `ON`. However this
    is only valid for Makefile and Ninja generators. On Visual Studio it
    is ignored so the target will always fail to build which was found out
    because its always set to `ON` within `build-webkit`.
    
    Move the logic for turning it `ON` into the CMake code and base it on
    the `CMAKE_GENERATOR`.
    
    * Source/cmake/WebKitCommon.cmake
    * Tools/Scripts/webkitdirs.pm
    
    Canonical link: https://commits.webkit.org/267521@main

--- Source/cmake/WebKitCommon.cmake
+++ Source/cmake/WebKitCommon.cmake
@@ -13,6 +13,12 @@ if (NOT HAS_RUN_WEBKIT_COMMON)
         message(STATUS "The CMake build type is: ${CMAKE_BUILD_TYPE}")
     endif ()
 
+    # Exporting compile commands is available for Ninja and Makefile generators
+    # See https://cmake.org/cmake/help/latest/variable/CMAKE_EXPORT_COMPILE_COMMANDS.html
+    if (DEVELOPER_MODE AND (CMAKE_GENERATOR MATCHES "Makefile" OR CMAKE_GENERATOR MATCHES "Ninja"))
+        set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+    endif ()
+
     option(ENABLE_JAVASCRIPTCORE "Enable building JavaScriptCore" ON)
     option(ENABLE_WEBCORE "Enable building JavaScriptCore" ON)
     option(ENABLE_WEBKIT "Enable building WebKit" ON)
