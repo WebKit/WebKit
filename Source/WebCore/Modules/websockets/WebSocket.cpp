30120862ed93a186f9e5b364f4bf090caa7f35f0
David Kilzer <ddkilzer@apple.com>
2023-09-01 20:01:49 -0700

    nullptr dereference in WebCore::WebSocket::close() https://bugs.webkit.org/show_bug.cgi?id=261037 <rdar://75425816>
    Reviewed by Alex Christensen.
    
    * Source/WebCore/Modules/websockets/WebSocket.cpp:
    (WebCore::WebSocket::close):
    - Add `nullptr` check for `m_channel`.
    
    Canonical link: https://commits.webkit.org/267575@main

--- Source/WebCore/Modules/websockets/WebSocket.cpp
+++ Source/WebCore/Modules/websockets/WebSocket.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2011 Google Inc.  All rights reserved.
- * Copyright (C) 2015-2016 Apple Inc. All rights reserved.
+ * Copyright (C) 2015-2023 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -437,7 +437,8 @@ ExceptionOr<void> WebSocket::close(std::optional<unsigned short> optionalCode, c
         return { };
     if (m_state == CONNECTING) {
         m_state = CLOSING;
-        m_channel->fail("WebSocket is closed before the connection is established."_s);
+        if (m_channel)
+            m_channel->fail("WebSocket is closed before the connection is established."_s);
         return { };
     }
     m_state = CLOSING;
