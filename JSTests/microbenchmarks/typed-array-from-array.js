0621aef584f776393f96f130107a5f7dcd7ffc9c
Mark Lam <mark.lam@apple.com>
2023-09-14 11:11:55 -0700

    Skip some tests on memory limited (aka slow) devices in lockdown test mode. https://bugs.webkit.org/show_bug.cgi?id=261558 rdar://115496412
    Reviewed by Alexey Shvayka.
    
    These tests are timing out on slower devices.
    
    * JSTests/microbenchmarks/mul-immediate-sub.js:
    * JSTests/microbenchmarks/typed-array-from-array.js:
    * JSTests/stress/array-prototype-concat-of-long-spliced-arrays.js:
    * JSTests/stress/array-prototype-concat-of-long-spliced-arrays2.js:
    
    Canonical link: https://commits.webkit.org/267998@main

--- JSTests/microbenchmarks/typed-array-from-array.js
+++ JSTests/microbenchmarks/typed-array-from-array.js
@@ -1,3 +1,6 @@
+//@ $skipModes << :lockdown if $memoryLimited
+// Here we're using $memoryLimited as a proxy for "slower device" and this test is very slow in lockdown.
+
 var a1 = new Array(1024 * 1024 * 1);
 a1.fill(99);
 for (var i = 0; i < 1e2; ++i)
