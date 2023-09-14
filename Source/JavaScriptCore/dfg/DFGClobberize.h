e960cdca544708c5eb699635438bf5e73c977c51
Keith Miller <keith_miller@apple.com>
2023-09-14 16:17:59 -0700

    Cherry-pick 265870.333@safari-7616.1.27.10-branch (ab5c6119c469). https://bugs.webkit.org/show_bug.cgi?id=261544
        Cherry-pick 47e039ffd689. rdar://115399657
    
            clobberize needs to be more precise with the *ByOffset nodes
            https://bugs.webkit.org/show_bug.cgi?id=261544
            rdar://115399657
    
            Reviewed by Yusuke Suzuki and Mark Lam.
    
            CSE phase uses clobberize to figure out if it's safe to merge two operations that
            def the same HeapLocation. Since HeapLocation does not currently have a way to
            track the offset used by the various *ByOffset nodes it can get confused and
            think that two ByOffset instructions produce the same value even if they don't
            use the same offset. This patch solves this by adding a new field to HeapLocation,
            which takes the metadata associated with the corresponding *ByOffset node. If two
            *ByOffset operations don't share the same metadata then they cannot be CSEed.
    
            * Source/JavaScriptCore/dfg/DFGCSEPhase.cpp:
            * Source/JavaScriptCore/dfg/DFGClobberize.h:
            (JSC::DFG::clobberize):
            * Source/JavaScriptCore/dfg/DFGHeapLocation.h:
            (JSC::DFG::HeapLocation::HeapLocation):
            (JSC::DFG::HeapLocation::extraState const):
            (JSC::DFG::HeapLocation::hash const):
    
            Canonical link: https://commits.webkit.org/265870.558@safari-7616-branch
        Identifier: 265423.780@safari-7616.1.27.10-branch

--- Source/JavaScriptCore/dfg/DFGClobberize.h
+++ Source/JavaScriptCore/dfg/DFGClobberize.h
@@ -1476,7 +1476,7 @@ void clobberize(Graph& graph, Node* node, const ReadFunctor& read, const WriteFu
         unsigned identifierNumber = node->storageAccessData().identifierNumber;
         AbstractHeap heap(NamedProperties, identifierNumber);
         read(heap);
-        def(HeapLocation(NamedPropertyLoc, heap, node->child2()), LazyNode(node));
+        def(HeapLocation(NamedPropertyLoc, heap, node->child2(), &node->storageAccessData()), LazyNode(node));
         return;
     }
 
@@ -1485,7 +1485,7 @@ void clobberize(Graph& graph, Node* node, const ReadFunctor& read, const WriteFu
         read(JSObject_butterfly);
         AbstractHeap heap(NamedProperties, node->multiGetByOffsetData().identifierNumber);
         read(heap);
-        def(HeapLocation(NamedPropertyLoc, heap, node->child1()), LazyNode(node));
+        def(HeapLocation(NamedPropertyLoc, heap, node->child1(), &node->multiGetByOffsetData()), LazyNode(node));
         return;
     }
         
@@ -1498,7 +1498,7 @@ void clobberize(Graph& graph, Node* node, const ReadFunctor& read, const WriteFu
             write(JSCell_structureID);
         if (node->multiPutByOffsetData().reallocatesStorage())
             write(JSObject_butterfly);
-        def(HeapLocation(NamedPropertyLoc, heap, node->child1()), LazyNode(node->child2().node()));
+        def(HeapLocation(NamedPropertyLoc, heap, node->child1(), &node->multiPutByOffsetData()), LazyNode(node->child2().node()));
         return;
     }
 
@@ -1520,7 +1520,7 @@ void clobberize(Graph& graph, Node* node, const ReadFunctor& read, const WriteFu
         unsigned identifierNumber = node->storageAccessData().identifierNumber;
         AbstractHeap heap(NamedProperties, identifierNumber);
         write(heap);
-        def(HeapLocation(NamedPropertyLoc, heap, node->child2()), LazyNode(node->child3().node()));
+        def(HeapLocation(NamedPropertyLoc, heap, node->child2(), &node->storageAccessData()), LazyNode(node->child3().node()));
         return;
     }
         
