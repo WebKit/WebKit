77d88a0725252f1b6294685fc0df14373dc0210d
Hunseop Jeong <hs85jeong@gmail.com>
2023-09-15 09:52:52 -0700

    Cherry-pick 3d6388b4ceeb. rdar://115462091
        AX: Fix to handle author error roles
        https://bugs.webkit.org/show_bug.cgi?id=261542
        rdar://problem/115462091
    
        Reviewed by Tyler Wilcock.
    
        This commit fixes the wai-aria/role/form-roles test[1]. According to the
        ARIA spec[2], in situations where an author has not specified names for
        the form and region landmarks, it is considered as authoring error. If a
        valid fallback role had been specified, or if the element had an implicit
        ARIA role, then user agents would continue to expose that role instead.
    
        [1]: https://wpt.fyi/results/wai-aria/role/form-roles.html
        [2]: https://w3c.github.io/aria/#document-handling_author-errors_roles
    
        * LayoutTests/accessibility/aria-namefrom-author.html:
        * LayoutTests/accessibility/roles-computedRoleString.html:
        * LayoutTests/imported/w3c/web-platform-tests/wai-aria/role/form-roles-expected.txt:
        * LayoutTests/platform/mac/accessibility/roles-computedRoleString-expected.txt:
        * Source/WebCore/accessibility/AccessibilityNodeObject.cpp:
        (WebCore::AccessibilityNodeObject::determineAriaRoleAttribute const):
    
        Canonical link: https://commits.webkit.org/268025@main
    
    Identifier: 266246.1705@safari-7617-branch

--- Source/WebCore/accessibility/AccessibilityNodeObject.cpp
+++ Source/WebCore/accessibility/AccessibilityNodeObject.cpp
@@ -2865,11 +2865,11 @@ AccessibilityRole AccessibilityNodeObject::determineAriaRoleAttribute() const
     if (role == AccessibilityRole::Presentational && supportsARIAAttributes())
         role = AccessibilityRole::Unknown;
     
-    // The ARIA spec states, "Authors must give each element with role region a brief label that
-    // describes the purpose of the content in the region." The Core AAM states, "Special case:
-    // if the region does not have an accessible name, do not expose the element as a landmark.
-    // Use the native host language role of the element instead."
-    if (role == AccessibilityRole::LandmarkRegion && !hasAttribute(aria_labelAttr) && !hasAttribute(aria_labelledbyAttr))
+    // https://w3c.github.io/aria/#document-handling_author-errors_roles
+    // In situations where an author has not specified names for the form and
+    // region landmarks, it is considered an authoring error. The user agent
+    // MUST treat such element as if no role had been provided.
+    if ((role == AccessibilityRole::LandmarkRegion || role == AccessibilityRole::Form) && !hasAttribute(aria_labelAttr) && !hasAttribute(aria_labelledbyAttr))
         role = AccessibilityRole::Unknown;
 
     if (static_cast<int>(role))
