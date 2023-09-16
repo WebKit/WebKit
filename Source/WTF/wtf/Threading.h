3468951fe890ae71911f72a444617be38c39c14f
Jer Noble <jer.noble@apple.com>
2023-09-15 19:28:02 -0700

    [Cocoa] RemoteAudioDestinationProxy threads are created with too low a priority for realtime audio https://bugs.webkit.org/show_bug.cgi?id=261619 rdar://115513480
    Reviewed by Chris Dumez.
    
    On macOS and iOS, when a thread is configured for a QOS, it can be de-prioritized
    by the scheduler when workloads are heavy, and will then be pre-empted by higher-priority
    threads. When a RemoteAudioDestinationProxy render thread is pre-empted, this manifests
    as an audible "stutter" or "skip" in the resulting WebAudio output.
    
    The CoreAudio Audio IO thread is configured to run as a Realtime thread, which opts out of
    the QOS de-prioritization logic. It also runs at a very high priority level, 97.
    
    Configure the RemoteAudioDestinationProxy render thread similarly to the CoreAudio IO thread.
    This requires adding a "SchedulingPolicy" enum to Thread.h and implementing that policy in
    ThreadingPOSIX.cpp. (The Windows Threading implementation already ignores QOS; it will
    further ignore SchedulingPolicy).  The Linux (POSIX) port has its own implementation of
    scheduling policy, which treats UserInteractive as Realtime. Retain this logic, and only apply
    a specific scheduling policy when it's explicitly set.
    
    * Source/WTF/wtf/PlatformHave.h:
    * Source/WTF/wtf/Threading.cpp:
    (WTF::Thread::create):
    * Source/WTF/wtf/Threading.h:
    * Source/WTF/wtf/posix/ThreadingPOSIX.cpp:
    (WTF::schedPolicy):
    (WTF::Thread::establishHandle):
    * Source/WTF/wtf/win/ThreadingWin.cpp:
    (WTF::Thread::establishHandle):
    * Source/WebKit/WebProcess/GPU/media/RemoteAudioDestinationProxy.cpp:
    (WebKit::RemoteAudioDestinationProxy::startRenderingThread):
    
    Canonical link: https://commits.webkit.org/268047@main

--- Source/WTF/wtf/Threading.h
+++ Source/WTF/wtf/Threading.h
@@ -125,13 +125,19 @@ public:
         Background
     };
 
+    enum class SchedulingPolicy : uint8_t {
+        Other = 0,
+        FIFO,
+        Realtime,
+    };
+
 #if HAVE(QOS_CLASSES)
     static dispatch_qos_class_t dispatchQOSClass(QOS);
 #endif
 
     // Returns nullptr if thread creation failed.
     // The thread name must be a literal since on some platforms it's passed in to the thread.
-    WTF_EXPORT_PRIVATE static Ref<Thread> create(const char* threadName, Function<void()>&&, ThreadType = ThreadType::Unknown, QOS = QOS::UserInitiated);
+    WTF_EXPORT_PRIVATE static Ref<Thread> create(const char* threadName, Function<void()>&&, ThreadType = ThreadType::Unknown, QOS = QOS::UserInitiated, SchedulingPolicy = SchedulingPolicy::Other);
 
     // Returns Thread object.
     static Thread& current();
@@ -293,7 +299,7 @@ protected:
     void initializeInThread();
 
     // Internal platform-specific Thread establishment implementation.
-    bool establishHandle(NewThreadContext*, std::optional<size_t> stackSize, QOS);
+    bool establishHandle(NewThreadContext*, std::optional<size_t> stackSize, QOS, SchedulingPolicy);
 
 #if USE(PTHREADS)
     void establishPlatformSpecificHandle(PlatformThreadHandle);
