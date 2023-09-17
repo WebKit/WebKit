4d995edbc44062b251be638818edcd88d7d14dd7
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2023-09-17 00:44:19 -0700

    Fix typo

--- Source/JavaScriptCore/runtime/Lookup.h
+++ Source/JavaScriptCore/runtime/Lookup.h
@@ -496,7 +496,7 @@ inline void reifyStaticProperty(VM& vm, const ClassInfo* classInfo, const Proper
         }
         #if USE(BUN_JSC_ADDITIONS)
         if (value.attributes() & PropertyAttribute::Constructable) {
-            StringImpl *name = propertyName.string();
+            StringImpl *name = propertyName.publicName();
             JSFunction* function = JSFunction::create(vm, thisObj.globalObject(), value.functionLength(), name, value.function(), ImplementationVisibility::Public, value.intrinsic(), value.function(), nullptr);
             thisObj.putDirect(vm, propertyName, function, attributesForStructure(value.attributes()));
             return;
