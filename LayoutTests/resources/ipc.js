c0a9a955b480bb69772ceccfa2eca68906532e7f
Kimmo Kinnunen <kkinnunen@apple.com>
2023-09-18 10:29:32 -0700

    jquery/attributes.html is flaky fail with StreamClientConnection  ASSERTION FAILED: !m_connection->isValid() https://bugs.webkit.org/show_bug.cgi?id=261555 rdar://115494068
    Reviewed by Antti Koivisto.
    
    Fix two tests that create StreamClientConnection instances through
    the JS IPC Testing API. The connections must be invalidated
    if they are created.
    
    Both allow and deny tests are run as normal JS business as usual, even
    though the test runner automatically ends the deny test when GPUP
    crashes. This means that the JS GC will clean up the stream connections
    during the next tests, causing unrelated tests to fail with the
    assertion.
    
    Fix the allow test to actually test what it tests, e.g. to fail if
    WebGPU is disabled.
    
    * LayoutTests/ipc/restrictedendpoints/allow-access-webGPU-expected.txt:
    * LayoutTests/ipc/restrictedendpoints/allow-access-webGPU.html:
    * LayoutTests/ipc/restrictedendpoints/deny-access-webGPU.html:
    * LayoutTests/resources/ipc.js:
    (randomID):
    (asyncFlush):
    (syncFlush): Deleted.
    * Source/WebKit/Shared/IPCTester.cpp:
    (WebKit::IPCTester::asyncPing):
    (WebKit::IPCTester::syncPing):
    * Source/WebKit/Shared/IPCTester.h:
    * Source/WebKit/Shared/IPCTester.messages.in:
    
    Canonical link: https://commits.webkit.org/268086@main

--- LayoutTests/resources/ipc.js
+++ LayoutTests/resources/ipc.js
@@ -1,14 +1,18 @@
+function randomIPCID() {
+    return Math.floor(Math.random() * 10000) + 1;
+}
+
 function asyncFlush(processTarget) {
     if (!IPC.processTargets.includes(processTarget))
         throw Error("Invalid processTarget passed to asyncFlush")
-    return IPC.sendMessage(processTarget, 0, IPC.messages.IPCTester_AsyncPing.name, [])
+    return IPC.sendMessage(processTarget, 0, IPC.messages.IPCTester_AsyncPing.name, [{type: "uint32_t", value: 88}]);
 }
 
 function syncFlush(processTarget) {
     if (!IPC.processTargets.includes(processTarget))
         throw Error("Invalid processTarget passed to syncFlush")
-    return new Promise((resolve) => {
-        IPC.sendSyncMessage(processTarget, 0, IPC.messages.IPCTester_SyncPing.name, 1000, []);
-        resolve();
-    })
+
+    let reply = IPC.sendSyncMessage(processTarget, 0, IPC.messages.IPCTester_SyncPing.name, 1000, [{type: "uint32_t", value: 77}]);
+    const firstResult = reply.arguments[0];
+    return firstResult.type == "uint32_t" && firstResult.value == 78;
 }
