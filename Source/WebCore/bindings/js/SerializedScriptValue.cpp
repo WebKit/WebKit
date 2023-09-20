466056288105e42969f244bd33862ef050925994
Mark Lam <mark.lam@apple.com>
2023-09-20 10:06:23 -0700

    Cherry-pick 265870.574@safari-7616-branch (58238f2ad1a0). https://bugs.webkit.org/show_bug.cgi?id=261801
        CloneDeserializer should always purifyNaN all double values it reads.
        https://bugs.webkit.org/show_bug.cgi?id=261801
        rdar://115756664
    
        Reviewed by Yusuke Suzuki.
    
        CloneDeserializer::read() will now invoke purifyNaN() on any double values that it reads.
        As a result, we can remove the 2 purifyNaN calls in its client that are now redundant.
    
        * Source/WebCore/bindings/js/SerializedScriptValue.cpp:
        (WebCore::CloneDeserializer::read):
        (WebCore::CloneDeserializer::readTerminal):
    
        Canonical link: https://commits.webkit.org/265870.574@safari-7616-branch

--- Source/WebCore/bindings/js/SerializedScriptValue.cpp
+++ Source/WebCore/bindings/js/SerializedScriptValue.cpp
@@ -2884,7 +2884,7 @@ private:
         } u;
         if (!readLittleEndian(u.i64))
             return false;
-        d = u.d;
+        d = purifyNaN(u.d);
         return true;
     }
 
@@ -4189,7 +4189,7 @@ private:
             double d;
             if (!read(d))
                 return JSValue();
-            return jsNumber(purifyNaN(d));
+            return jsNumber(d);
         }
         case BigIntTag:
             return readBigInt();
@@ -4197,7 +4197,7 @@ private:
             double d;
             if (!read(d))
                 return JSValue();
-            NumberObject* obj = constructNumber(m_globalObject, jsNumber(purifyNaN(d)));
+            NumberObject* obj = constructNumber(m_globalObject, jsNumber(d));
             m_gcBuffer.appendWithCrashOnOverflow(obj);
             return obj;
         }
