b379a7d75a2fe9ff06533c9013090df4849503cd
David Kilzer <ddkilzer@apple.com>
2023-09-22 14:00:47 -0700

    Cherry-pick 265870.567@safari-7616-branch (6380262a8580). rdar://115895970
        Add test function for WebCore::SincResampler
        https://bugs.webkit.org/show_bug.cgi?id=261702
        <rdar://115682448>
    
        Reviewed by Chris Dumez and Alex Christensen.
    
        Add test method that calls SincResampler::processBuffer().
    
        * Source/WebCore/testing/js/WebCoreTestSupport.cpp:
        (WebCoreTestSupport::testSincResamplerProcessBuffer): Add.
        * Source/WebCore/testing/js/WebCoreTestSupport.h:
        (WebCoreTestSupport::testSincResamplerProcessBuffer): Add.
    
        Canonical link: https://commits.webkit.org/265870.567@safari-7616-branch
    
    Canonical link: https://commits.webkit.org/267815.90@safari-7617-branch

--- Source/WebCore/testing/js/WebCoreTestSupport.h
+++ Source/WebCore/testing/js/WebCoreTestSupport.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2011, 2015 Google Inc. All rights reserved.
- * Copyright (C) 2016-2022 Apple Inc. All rights reserved.
+ * Copyright (C) 2016-2023 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -26,6 +26,7 @@
 
 #pragma once
 
+#include <span>
 #include <wtf/Forward.h>
 
 typedef const struct OpaqueJSContext* JSContextRef;
@@ -86,5 +87,9 @@ inline void populateJITOperations() { populateDisassemblyLabels(); }
 #else
 inline void populateJITOperations() { }
 #endif // ENABLE(JIT_OPERATION_VALIDATION) || ENABLE(JIT_OPERATION_DISASSEMBLY)
+
+#if ENABLE(WEB_AUDIO)
+void testSincResamplerProcessBuffer(std::span<const float> source, std::span<float> destination, double scaleFactor) TEST_SUPPORT_EXPORT;
+#endif
 
 } // namespace WebCoreTestSupport
