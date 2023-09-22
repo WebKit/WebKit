5803b1dcf449619d5dcea50cf30c4a4b7f7b4c6a
Jonathan Bedard <jbedard@apple.com>
2023-09-22 16:35:11 -0700

    Cherry-pick 268340@main (4c8538afb2f3). rdar://115912222
        [git-webkit] Reference all identity commits in pickable
        https://bugs.webkit.org/show_bug.cgi?id=261980
        rdar://115912222
    
        Reviewed by Dewei Zhu.
    
        * Tools/Scripts/libraries/webkitscmpy/setup.py: Bump version.
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py: Ditto.
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/pickable.py:
        (Pickable.pickable): Continue checking relationships if identity isn't
        found in commit story.
    
        Canonical link: https://commits.webkit.org/268340@main
    
    Canonical link: https://commits.webkit.org/267815.98@safari-7617-branch

--- Tools/Scripts/libraries/webkitscmpy/setup.py
+++ Tools/Scripts/libraries/webkitscmpy/setup.py
@@ -29,7 +29,7 @@ def readme():
 
 setup(
     name='webkitscmpy',
-    version='6.6.12',
+    version='6.6.13',
     description='Library designed to interact with git and svn repositories.',
     long_description=readme(),
     classifiers=[
