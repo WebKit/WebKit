8a00858352adede8de2078313986469f9429c3ad
Jonathan Bedard <jbedard@apple.com>
2023-09-27 14:47:40 -0700

    [commits.webkit.org] Disable crawling on commits.webkit.org https://bugs.webkit.org/show_bug.cgi?id=262213 rdar://116141929
    Reviewed by Dewei Zhu.
    
    * Tools/Scripts/libraries/reporelaypy/reporelaypy/__init__.py: Bump version.
    * Tools/Scripts/libraries/reporelaypy/setup.py: Ditto.
    * Tools/Scripts/libraries/reporelaypy/reporelaypy/webserver.py:
    (robots_txt): Return robots.txt.
    
    Canonical link: https://commits.webkit.org/268539@main

--- Tools/Scripts/libraries/reporelaypy/reporelaypy/__init__.py
+++ Tools/Scripts/libraries/reporelaypy/reporelaypy/__init__.py
@@ -44,7 +44,7 @@ except ImportError:
         "Please install webkitcorepy with `pip install webkitcorepy --extra-index-url <package index URL>`"
     )
 
-version = Version(0, 8, 2)
+version = Version(0, 8, 3)
 
 import webkitflaskpy
 
--- Tools/Scripts/libraries/reporelaypy/setup.py
+++ Tools/Scripts/libraries/reporelaypy/setup.py
@@ -30,7 +30,7 @@ def readme():
 
 setup(
     name='reporelaypy',
-    version='0.8.2',
+    version='0.8.3',
     description='Library for visualizing, processing and storing test results.',
     long_description=readme(),
     classifiers=[
