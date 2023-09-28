c124153d271be6c82398748bd9756b12b7ba7c0d
Thomas Devoogdt <thomas.devoogdt@barco.com>
2023-09-28 11:21:16 -0700

    [GTK] fix USE_EGL checks when GBM renderer is used
    https://bugs.webkit.org/show_bug.cgi?id=262169
    
    Reviewed by Carlos Garcia Campos.
    
    Regression since commit 81c064ddb4182a34a36def401025d223ca0d31ba.
    
    webkitgtk-2.42.0/Source/WebKit/UIProcess/gtk/AcceleratedBackingStoreDMABuf.cpp:39:10: fatal error: epoxy/egl.h: No such file or directory
       39 | #include <epoxy/egl.h>
          |          ^~~~~~~~~~~~~
    
    Signed-off-by: Thomas Devoogdt <thomas.devoogdt@barco.com>
    Canonical link: https://commits.webkit.org/268598@main

--- Source/WebKit/WebProcess/WebPage/AcceleratedSurface.cpp
+++ Source/WebKit/WebProcess/WebPage/AcceleratedSurface.cpp
@@ -37,7 +37,7 @@
 #include "AcceleratedSurfaceLibWPE.h"
 #endif
 
-#if PLATFORM(GTK)
+#if PLATFORM(GTK) && USE(EGL)
 #include "AcceleratedSurfaceDMABuf.h"
 #endif
 
@@ -46,7 +46,7 @@ using namespace WebCore;
 
 std::unique_ptr<AcceleratedSurface> AcceleratedSurface::create(WebPage& webPage, Client& client)
 {
-#if PLATFORM(GTK)
+#if PLATFORM(GTK) && USE(EGL)
 #if USE(GBM)
     if (PlatformDisplay::sharedDisplayForCompositing().type() == PlatformDisplay::Type::GBM)
         return AcceleratedSurfaceDMABuf::create(webPage, client);

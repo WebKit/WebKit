WebKit/WebKit

commit 81c064ddb4182a34a36def401025d223ca0d31ba

[GTK] Do not make DMA-BUF renderer depend on GBM https://bugs.webkit.org/show_bug.cgi?id=257372
Reviewed by Alejandro G. Castro.

Fallback to use shared memory buffers when GBM is not available. The env
var WEBKIT_DMABUF_RENDERER_DISABLE_GBM can be used for testing the non-GBM
code path without having to rebuild.

* Source/WebKit/PlatformGTK.cmake:
* Source/WebKit/UIProcess/API/glib/WebKitProtocolHandler.cpp:
(WebKit::WebKitProtocolHandler::handleGPU):
* Source/WebKit/UIProcess/glib/WebProcessPoolGLib.cpp:
(WebKit::WebProcessPool::platformInitializeWebProcess):
* Source/WebKit/UIProcess/gtk/AcceleratedBackingStore.cpp:
(WebKit::AcceleratedBackingStore::checkRequirements):
(WebKit::AcceleratedBackingStore::create):
* Source/WebKit/UIProcess/gtk/AcceleratedBackingStoreDMABuf.cpp:
(WebKit::AcceleratedBackingStoreDMABuf::Surface::~Surface):
(WebKit::AcceleratedBackingStoreDMABuf::Surface::swap):
(WebKit::AcceleratedBackingStoreDMABuf::Surface::prepareForRendering):
(WebKit::AcceleratedBackingStoreDMABuf::createSource):
* Source/WebKit/UIProcess/gtk/AcceleratedBackingStoreDMABuf.h:
* Source/WebKit/WebProcess/WebPage/AcceleratedSurface.cpp:
(WebKit::AcceleratedSurface::create):
* Source/WebKit/WebProcess/WebPage/gtk/AcceleratedSurfaceDMABuf.cpp:
(WebKit::AcceleratedSurfaceDMABuf::AcceleratedSurfaceDMABuf):
(WebKit::AcceleratedSurfaceDMABuf::RenderTargetColorBuffer::~RenderTargetColorBuffer):
(WebKit::AcceleratedSurfaceDMABuf::RenderTargetEGLImage::~RenderTargetEGLImage):
(WebKit::AcceleratedSurfaceDMABuf::RenderTargetTexture::create):
(WebKit::AcceleratedSurfaceDMABuf::RenderTargetTexture::RenderTargetTexture):
(WebKit::AcceleratedSurfaceDMABuf::RenderTarget::swap): Deleted.
* Source/WebKit/WebProcess/WebPage/gtk/AcceleratedSurfaceDMABuf.h:
* Source/WebKit/WebProcess/glib/WebProcessGLib.cpp:
(WebKit::WebProcess::platformInitializeWebProcess):

Canonical link: https://commits.webkit.org/265188@main

Source/WebKit/UIProcess/API/glib/WebKitProtocolHandler.cpp
@@ -45,6 +45,8 @@
 #endif
 
 #if PLATFORM(GTK)
+#include "AcceleratedBackingStoreDMABuf.h"
+#include <WebCore/PlatformDisplaySurfaceless.h>
 #include <gtk/gtk.h>
 
 #if PLATFORM(WAYLAND)
@@ -57,9 +59,7 @@
 #endif
 
 #if USE(GBM)
-#include "AcceleratedBackingStoreDMABuf.h"
 #include <WebCore/PlatformDisplayGBM.h>
-#include <WebCore/PlatformDisplaySurfaceless.h>
 #endif
 #endif
 
@@ -250,11 +250,7 @@ void WebKitProtocolHandler::handleGPU(WebKitURISchemeRequest* request)
 #if PLATFORM(GTK)
     addTableRow(versionObject, "GTK version"_s, makeString(GTK_MAJOR_VERSION, '.', GTK_MINOR_VERSION, '.', GTK_MICRO_VERSION, " (build) "_s, gtk_get_major_version(), '.', gtk_get_minor_version(), '.', gtk_get_micro_version(), " (runtime)"_s));
 
-#if USE(GBM)
     bool usingDMABufRenderer = AcceleratedBackingStoreDMABuf::checkRequirements();
-#else
-    bool usingDMABufRenderer = false;
-#endif
 
 #if PLATFORM(WAYLAND)
     if (PlatformDisplay::sharedDisplay().type() == PlatformDisplay::Type::Wayland && !usingDMABufRenderer) {
@@ -348,21 +344,24 @@ void WebKitProtocolHandler::handleGPU(WebKitURISchemeRequest* request)
 #if USE(EGL) && PLATFORM(GTK)
     if (strcmp(policy, "never")) {
         std::unique_ptr<PlatformDisplay> platformDisplay;
-#if USE(GBM)
         if (usingDMABufRenderer) {
-            if (auto* device = PlatformDisplay::sharedDisplay().gbmDevice())
-                platformDisplay = PlatformDisplayGBM::create(device);
-            else
+#if USE(GBM)
+            const char* disableGBM = getenv("WEBKIT_DMABUF_RENDERER_DISABLE_GBM");
+            if (!disableGBM || !strcmp(disableGBM, "0")) {
+                if (auto* device = PlatformDisplay::sharedDisplay().gbmDevice())
+                    platformDisplay = PlatformDisplayGBM::create(device);
+            }
+#endif
+            if (!platformDisplay)
                 platformDisplay = PlatformDisplaySurfaceless::create();
         }
-#endif
 
         if (platformDisplay || !uiProcessContextIsEGL()) {
             auto hardwareAccelerationObject = JSON::Object::create();
             startTable("Hardware Acceleration Information (Render Process)"_s);
 
             if (platformDisplay)
-                addTableRow(hardwareAccelerationObject, "Platform"_s, String::fromUTF8(platformDisplay->type() == PlatformDisplay::Type::GBM ? "GBM"_s : "Surfaceless"_s));
+                addTableRow(hardwareAccelerationObject, "Platform"_s, String::fromUTF8(platformDisplay->type() == PlatformDisplay::Type::Surfaceless ? "Surfaceless"_s : "GBM"_s));
 
             if (uiProcessContextIsEGL()) {
                 GLContext::ScopedGLContext glContext(GLContext::createOffscreen(platformDisplay ? *platformDisplay : PlatformDisplay::sharedDisplay()));

Source/WebKit/UIProcess/glib/WebProcessPoolGLib.cpp
@@ -54,9 +54,7 @@
 
 #if PLATFORM(GTK)
 #include "GtkSettingsManager.h"
-#if USE(GBM)
 #include "AcceleratedBackingStoreDMABuf.h"
-#endif
 #endif
 
 
@@ -90,7 +88,7 @@ void WebProcessPool::platformInitializeWebProcess(const WebProcessProxy& process
     parameters.renderDeviceFile = WebCore::PlatformDisplay::sharedDisplay().drmRenderNodeFile();
 #endif
 
-#if PLATFORM(GTK) && USE(GBM)
+#if PLATFORM(GTK)
     parameters.useDMABufSurfaceForCompositing = AcceleratedBackingStoreDMABuf::checkRequirements();
 #endif
 

Source/WebKit/UIProcess/gtk/AcceleratedBackingStore.cpp
@@ -40,14 +40,14 @@
 #include "AcceleratedBackingStoreX11.h"
 #endif
 
-#if USE(GBM)
+#if PLATFORM(GTK)
 #include "AcceleratedBackingStoreDMABuf.h"
 #endif
 
 namespace WebKit {
 using namespace WebCore;
 
-#if USE(GBM)
+#if PLATFORM(GTK)
 static bool gtkCanUseHardwareAcceleration()
 {
     static bool canUseHardwareAcceleration;
@@ -72,7 +72,7 @@ static bool gtkCanUseHardwareAcceleration()
 
 bool AcceleratedBackingStore::checkRequirements()
 {
-#if USE(GBM)
+#if PLATFORM(GTK)
     if (AcceleratedBackingStoreDMABuf::checkRequirements())
         return gtkCanUseHardwareAcceleration();
 #endif
@@ -93,7 +93,7 @@ std::unique_ptr<AcceleratedBackingStore> AcceleratedBackingStore::create(WebPage
     if (!HardwareAccelerationManager::singleton().canUseHardwareAcceleration())
         return nullptr;
 
-#if USE(GBM)
+#if PLATFORM(GTK)
     if (AcceleratedBackingStoreDMABuf::checkRequirements())
         return AcceleratedBackingStoreDMABuf::create(webPage);
 #endif

Source/WebKit/UIProcess/gtk/AcceleratedBackingStoreDMABuf.cpp
@@ -26,7 +26,6 @@
 #include "config.h"
 #include "AcceleratedBackingStoreDMABuf.h"
 
-#if USE(GBM)
 #include "AcceleratedBackingStoreDMABufMessages.h"
 #include "AcceleratedSurfaceDMABufMessages.h"
 #include "LayerTreeContext.h"
@@ -38,8 +37,11 @@
 #include <WebCore/IntRect.h>
 #include <WebCore/PlatformDisplay.h>
 #include <epoxy/egl.h>
-#include <gbm.h>
 #include <wtf/glib/GUniquePtr.h>
+
+#if USE(GBM)
+#include <gbm.h>
+#endif
 
 #if PLATFORM(X11) && USE(GTK4)
 #include <gdk/x11/gdkx.h>
@@ -202,6 +204,7 @@ void AcceleratedBackingStoreDMABuf::Texture::paint(GtkWidget* widget, cairo_t* c
 }
 #endif
 
+#if USE(GBM)
 AcceleratedBackingStoreDMABuf::Surface::Surface(const UnixFileDescriptor& backFD, const UnixFileDescriptor& frontFD, const WebCore::IntSize& size, uint32_t format, uint32_t offset, uint32_t stride, float deviceScaleFactor)
     : RenderSource(size, deviceScaleFactor)
 {
@@ -225,6 +228,7 @@ AcceleratedBackingStoreDMABuf::Surface::Surface(const UnixFileDescriptor& backFD
         m_backBuffer = nullptr;
     }
 }
+#endif
 
 AcceleratedBackingStoreDMABuf::Surface::Surface(RefPtr<ShareableBitmap>& backBitmap, RefPtr<ShareableBitmap>& frontBitmap, float deviceScaleFactor)
     : RenderSource(backBitmap ? backBitmap->size() : WebCore::IntSize(), deviceScaleFactor)
@@ -237,12 +241,15 @@ AcceleratedBackingStoreDMABuf::Surface::~Surface()
 {
     m_surface = nullptr;
 
+#if USE(GBM)
     if (m_backBuffer)
         gbm_bo_destroy(m_backBuffer);
     if (m_frontBuffer)
         gbm_bo_destroy(m_frontBuffer);
+#endif
 }
 
+#if USE(GBM)
 RefPtr<cairo_surface_t> AcceleratedBackingStoreDMABuf::Surface::map(struct gbm_bo* buffer) const
 {
     if (!buffer)
@@ -269,6 +276,7 @@ RefPtr<cairo_surface_t> AcceleratedBackingStoreDMABuf::Surface::map(struct gbm_b
     });
     return surface;
 }
+#endif
 
 RefPtr<cairo_surface_t> AcceleratedBackingStoreDMABuf::Surface::map(RefPtr<ShareableBitmap>& bitmap) const
 {
@@ -282,16 +290,20 @@ void AcceleratedBackingStoreDMABuf::Surface::swap()
 {
     if (m_backBitmap && m_frontBitmap)
         std::swap(m_backBitmap, m_frontBitmap);
+#if USE(GBM)
     else
         std::swap(m_backBuffer, m_frontBuffer);
+#endif
 }
 
 bool AcceleratedBackingStoreDMABuf::Surface::prepareForRendering()
 {
     if (m_backBitmap && m_frontBitmap)
         m_surface = map(m_frontBitmap);
+#if USE(GBM)
     else
         m_surface = map(m_frontBuffer);
+#endif
 
     if (m_surface) {
         cairo_surface_mark_dirty(m_surface.get());
@@ -358,8 +370,10 @@ std::unique_ptr<AcceleratedBackingStoreDMABuf::RenderSource> AcceleratedBackingS
     if (m_isSoftwareRast)
         return makeUnique<Surface>(m_surface.backBitmap, m_surface.frontBitmap, m_webPage.deviceScaleFactor());
 
+#if USE(GBM)
     if (!WebCore::PlatformDisplay::sharedDisplay().gtkEGLDisplay())
         return makeUnique<Surface>(m_surface.backFD, m_surface.frontFD, m_surface.size, m_surface.format, m_surface.offset, m_surface.stride, m_webPage.deviceScaleFactor());
+#endif
 
     ensureGLContext();
     return makeUnique<Texture>(m_gdkGLContext.get(), m_surface.backFD, m_surface.frontFD, m_surface.size, m_surface.format, m_surface.offset, m_surface.stride, m_surface.modifier, m_webPage.deviceScaleFactor());
@@ -492,5 +506,3 @@ bool AcceleratedBackingStoreDMABuf::paint(cairo_t* cr, const WebCore::IntRect& c
 #endif
 
 } // namespace WebKit
-
-#endif // USE(GBM)

Source/WebKit/UIProcess/gtk/AcceleratedBackingStoreDMABuf.h
@@ -27,7 +27,6 @@
 
 #include "AcceleratedBackingStore.h"
 
-#if USE(GBM)
 #include "MessageReceiver.h"
 #include <WebCore/IntSize.h>
 #include <WebCore/RefPtrCairo.h>
@@ -37,7 +36,10 @@
 #include <wtf/unix/UnixFileDescriptor.h>
 
 typedef void *EGLImage;
+
+#if USE(GBM)
 struct gbm_bo;
+#endif
 
 namespace WebCore {
 class IntRect;
@@ -130,7 +132,9 @@ private:
 
     class Surface final : public RenderSource {
     public:
+#if USE(GBM)
         Surface(const WTF::UnixFileDescriptor&, const WTF::UnixFileDescriptor&, const WebCore::IntSize&, uint32_t format, uint32_t offset, uint32_t stride, float deviceScaleFactor);
+#endif
         Surface(RefPtr<ShareableBitmap>&, RefPtr<ShareableBitmap>&, float deviceScaleFactor);
         ~Surface();
 
@@ -145,11 +149,15 @@ private:
         void paint(GtkWidget*, cairo_t*, const WebCore::IntRect&) const override;
 #endif
 
+#if USE(GBM)
         RefPtr<cairo_surface_t> map(struct gbm_bo*) const;
+#endif
         RefPtr<cairo_surface_t> map(RefPtr<ShareableBitmap>&) const;
 
+#if USE(GBM)
         struct gbm_bo* m_backBuffer { nullptr };
         struct gbm_bo* m_frontBuffer { nullptr };
+#endif
         RefPtr<ShareableBitmap> m_backBitmap;
         RefPtr<ShareableBitmap> m_frontBitmap;
         RefPtr<cairo_surface_t> m_surface;
@@ -179,5 +187,3 @@ private:
 };
 
 } // namespace WebKit
-
-#endif // USE(GBM)

Source/WebKit/WebProcess/WebPage/AcceleratedSurface.cpp
@@ -37,7 +37,7 @@
 #include "AcceleratedSurfaceLibWPE.h"
 #endif
 
-#if PLATFORM(GTK) && USE(GBM)
+#if PLATFORM(GTK)
 #include "AcceleratedSurfaceDMABuf.h"
 #endif
 
@@ -46,9 +46,12 @@ using namespace WebCore;
 
 std::unique_ptr<AcceleratedSurface> AcceleratedSurface::create(WebPage& webPage, Client& client)
 {
-#if PLATFORM(GTK) && USE(GBM)
-    if (PlatformDisplay::sharedDisplayForCompositing().type() == PlatformDisplay::Type::GBM
-        || PlatformDisplay::sharedDisplayForCompositing().type() == PlatformDisplay::Type::Surfaceless)
+#if PLATFORM(GTK)
+#if USE(GBM)
+    if (PlatformDisplay::sharedDisplayForCompositing().type() == PlatformDisplay::Type::GBM)
+        return AcceleratedSurfaceDMABuf::create(webPage, client);
+#endif
+    if (PlatformDisplay::sharedDisplayForCompositing().type() == PlatformDisplay::Type::Surfaceless)
         return AcceleratedSurfaceDMABuf::create(webPage, client);
 #endif
 #if PLATFORM(WAYLAND)

Source/WebKit/WebProcess/WebPage/gtk/AcceleratedSurfaceDMABuf.cpp
@@ -26,19 +26,21 @@
 #include "config.h"
 #include "AcceleratedSurfaceDMABuf.h"
 
-#if USE(GBM)
 #include "AcceleratedBackingStoreDMABufMessages.h"
 #include "AcceleratedSurfaceDMABufMessages.h"
 #include "ShareableBitmap.h"
 #include "WebPage.h"
 #include "WebProcess.h"
 #include <WebCore/DMABufFormat.h>
-#include <WebCore/GBMDevice.h>
 #include <WebCore/PlatformDisplay.h>
 #include <array>
 #include <epoxy/egl.h>
-#include <gbm.h>
 #include <wtf/SafeStrerror.h>
+
+#if USE(GBM)
+#include <WebCore/GBMDevice.h>
+#include <gbm.h>
+#endif
 
 namespace WebKit {
 
@@ -49,7 +51,6 @@ std::unique_ptr<AcceleratedSurfaceDMABuf> AcceleratedSurfaceDMABuf::create(WebPa
 
 AcceleratedSurfaceDMABuf::AcceleratedSurfaceDMABuf(WebPage& webPage, Client& client)
     : AcceleratedSurface(webPage, client)
-    , m_isSoftwareRast(WebCore::PlatformDisplay::sharedDisplayForCompositing().type() == WebCore::PlatformDisplay::Type::Surfaceless)
 {
 }
 
@@ -91,6 +92,7 @@ void AcceleratedSurfaceDMABuf::RenderTarget::swap()
     std::swap(m_backColorBuffer, m_frontColorBuffer);
 }
 
+#if USE(GBM)
 std::unique_ptr<AcceleratedSurfaceDMABuf::RenderTarget> AcceleratedSurfaceDMABuf::RenderTargetEGLImage::create(WebCore::PageIdentifier pageID, const WebCore::IntSize& size)
 {
     struct {
@@ -185,6 +187,7 @@ void AcceleratedSurfaceDMABuf::RenderTargetEGLImage::swap()
     std::swap(m_backImage, m_frontImage);
     RenderTarget::swap();
 }
+#endif
 
 std::unique_ptr<AcceleratedSurfaceDMABuf::RenderTarget> AcceleratedSurfaceDMABuf::RenderTargetSHMImage::create(WebCore::PageIdentifier pageID, const WebCore::IntSize& size)
 {
@@ -276,7 +279,19 @@ void AcceleratedSurfaceDMABuf::clientResize(const WebCore::IntSize& size)
     m_target = nullptr;
     if (size.isEmpty())
         return;
-    m_target = m_isSoftwareRast ? RenderTargetSHMImage::create(m_webPage.identifier(), size) : RenderTargetEGLImage::create(m_webPage.identifier(), size);
+
+    switch (WebCore::PlatformDisplay::sharedDisplayForCompositing().type()) {
+    case WebCore::PlatformDisplay::Type::Surfaceless:
+        m_target = RenderTargetSHMImage::create(m_webPage.identifier(), size);
+        break;
+#if USE(GBM)
+    case WebCore::PlatformDisplay::Type::GBM:
+        m_target = RenderTargetEGLImage::create(m_webPage.identifier(), size);
+        break;
+#endif
+    default:
+        break;
+    }
 }
 
 void AcceleratedSurfaceDMABuf::willRenderFrame()
@@ -309,5 +324,3 @@ void AcceleratedSurfaceDMABuf::frameDone()
 }
 
 } // namespace WebKit
-
-#endif // USE(GBM)

Source/WebKit/WebProcess/WebPage/gtk/AcceleratedSurfaceDMABuf.h
@@ -27,13 +27,14 @@
 
 #include "AcceleratedSurface.h"
 
-#if USE(GBM)
 #include "MessageReceiver.h"
 #include <WebCore/PageIdentifier.h>
 #include <wtf/unix/UnixFileDescriptor.h>
 
+#if USE(GBM)
 typedef void *EGLImage;
 struct gbm_bo;
+#endif
 
 namespace WebKit {
 
@@ -91,6 +92,7 @@ private:
         unsigned m_depthStencilBuffer { 0 };
     };
 
+#if USE(GBM)
     class RenderTargetEGLImage final : public RenderTarget {
     public:
         static std::unique_ptr<RenderTarget> create(WebCore::PageIdentifier, const WebCore::IntSize&);
@@ -103,6 +105,7 @@ private:
         EGLImage m_backImage { nullptr };
         EGLImage m_frontImage { nullptr };
     };
+#endif
 
     class RenderTargetSHMImage final : public RenderTarget {
     public:
@@ -118,11 +121,8 @@ private:
         Ref<ShareableBitmap> m_frontBitmap;
     };
 
-    bool m_isSoftwareRast { false };
     unsigned m_fbo { 0 };
     std::unique_ptr<RenderTarget> m_target;
 };
 
 } // namespace WebKit
-
-#endif // USE(GBM)

commit c124153d271be6c82398748bd9756b12b7ba7c0d

[GTK] fix USE_EGL checks when GBM renderer is used
https://bugs.webkit.org/show_bug.cgi?id=262169

Reviewed by Carlos Garcia Campos.

Regression since commit 81c064ddb4182a34a36def401025d223ca0d31ba.

webkitgtk-2.42.0/Source/WebKit/UIProcess/gtk/AcceleratedBackingStoreDMABuf.cpp:39:10: fatal error: epoxy/egl.h: No such file or directory
   39 | #include <epoxy/egl.h>
      |          ^~~~~~~~~~~~~

Signed-off-by: Thomas Devoogdt <thomas.devoogdt@barco.com>
Canonical link: https://commits.webkit.org/268598@main

Source/WebKit/UIProcess/API/glib/WebKitProtocolHandler.cpp
@@ -45,9 +45,11 @@
 #endif
 
 #if PLATFORM(GTK)
+#if USE(EGL)
 #include "AcceleratedBackingStoreDMABuf.h"
 #include "DMABufRendererBufferMode.h"
 #include <WebCore/PlatformDisplaySurfaceless.h>
+#endif
 #include <gtk/gtk.h>
 
 #if PLATFORM(WAYLAND)
@@ -164,6 +166,7 @@ static String dmabufRendererWithSupportedBuffers()
 {
     StringBuilder buffers;
     buffers.append("DMABuf (Supported buffers: "_s);
+#if USE(EGL)
     auto mode = AcceleratedBackingStoreDMABuf::rendererBufferMode();
     if (mode.contains(DMABufRendererBufferMode::Hardware))
         buffers.append("Hardware"_s);
@@ -172,6 +175,7 @@ static String dmabufRendererWithSupportedBuffers()
             buffers.append(", ");
         buffers.append("Shared Memory"_s);
     }
+#endif
     buffers.append(')');
     return buffers.toString();
 }
@@ -269,7 +273,11 @@ void WebKitProtocolHandler::handleGPU(WebKitURISchemeRequest* request)
 #if PLATFORM(GTK)
     addTableRow(versionObject, "GTK version"_s, makeString(GTK_MAJOR_VERSION, '.', GTK_MINOR_VERSION, '.', GTK_MICRO_VERSION, " (build) "_s, gtk_get_major_version(), '.', gtk_get_minor_version(), '.', gtk_get_micro_version(), " (runtime)"_s));
 
+#if USE(EGL)
     bool usingDMABufRenderer = AcceleratedBackingStoreDMABuf::checkRequirements();
+#else
+    bool usingDMABufRenderer = false;
+#endif
 
 #if PLATFORM(WAYLAND)
     if (PlatformDisplay::sharedDisplay().type() == PlatformDisplay::Type::Wayland && !usingDMABufRenderer) {

Source/WebKit/UIProcess/glib/WebProcessPoolGLib.cpp
@@ -53,7 +53,9 @@
 #endif
 
 #if PLATFORM(GTK)
+#if USE(EGL)
 #include "AcceleratedBackingStoreDMABuf.h"
+#endif
 #include "GtkSettingsManager.h"
 #include "ScreenManager.h"
 #endif
@@ -89,7 +91,7 @@ void WebProcessPool::platformInitializeWebProcess(const WebProcessProxy& process
     parameters.renderDeviceFile = WebCore::PlatformDisplay::sharedDisplay().drmRenderNodeFile();
 #endif
 
-#if PLATFORM(GTK)
+#if PLATFORM(GTK) && USE(EGL)
     parameters.dmaBufRendererBufferMode = AcceleratedBackingStoreDMABuf::rendererBufferMode();
 #endif
 

Source/WebKit/UIProcess/gtk/AcceleratedBackingStore.cpp
@@ -40,7 +40,7 @@
 #include "AcceleratedBackingStoreX11.h"
 #endif
 
-#if PLATFORM(GTK)
+#if PLATFORM(GTK) && USE(EGL)
 #include "AcceleratedBackingStoreDMABuf.h"
 #endif
 
@@ -72,7 +72,7 @@ static bool gtkCanUseHardwareAcceleration()
 
 bool AcceleratedBackingStore::checkRequirements()
 {
-#if PLATFORM(GTK)
+#if PLATFORM(GTK) && USE(EGL)
     if (AcceleratedBackingStoreDMABuf::checkRequirements())
         return gtkCanUseHardwareAcceleration();
 #endif
@@ -93,7 +93,7 @@ std::unique_ptr<AcceleratedBackingStore> AcceleratedBackingStore::create(WebPage
     if (!HardwareAccelerationManager::singleton().canUseHardwareAcceleration())
         return nullptr;
 
-#if PLATFORM(GTK)
+#if PLATFORM(GTK) && USE(EGL)
     if (AcceleratedBackingStoreDMABuf::checkRequirements())
         return AcceleratedBackingStoreDMABuf::create(webPage);
 #endif

Source/WebKit/UIProcess/gtk/AcceleratedBackingStoreDMABuf.cpp
@@ -26,6 +26,8 @@
 #include "config.h"
 #include "AcceleratedBackingStoreDMABuf.h"
 
+#if USE(EGL)
+
 #include "AcceleratedBackingStoreDMABufMessages.h"
 #include "AcceleratedSurfaceDMABufMessages.h"
 #include "DMABufRendererBufferMode.h"
@@ -519,3 +521,5 @@ bool AcceleratedBackingStoreDMABuf::paint(cairo_t* cr, const WebCore::IntRect& c
 #endif
 
 } // namespace WebKit
+
+#endif // USE(EGL)

Source/WebKit/UIProcess/gtk/AcceleratedBackingStoreDMABuf.h
@@ -25,6 +25,8 @@
 
 #pragma once
 
+#if USE(EGL)
+
 #include "AcceleratedBackingStore.h"
 
 #include "MessageReceiver.h"
@@ -232,3 +234,5 @@ private:
 };
 
 } // namespace WebKit
+
+#endif // USE(EGL)

Source/WebKit/UIProcess/gtk/AcceleratedBackingStoreDMABuf.messages.in
@@ -20,9 +20,11 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#if USE(EGL)
 messages -> AcceleratedBackingStoreDMABuf NotRefCounted {
     DidCreateBuffer(uint64_t id, UnixFileDescriptor fd, WebCore::IntSize size, uint32_t format, uint32_t offset, uint32_t stride, uint64_t modifier)
     DidCreateBufferSHM(uint64_t id, WebKit::ShareableBitmap::Handle handle)
     DidDestroyBuffer(uint64_t id)
     Frame(uint64_t id)
 }
+#endif

Source/WebKit/WebProcess/WebPage/AcceleratedSurface.cpp
@@ -37,7 +37,7 @@
 #include "AcceleratedSurfaceLibWPE.h"
 #endif
 
-#if PLATFORM(GTK)
+#if PLATFORM(GTK) && USE(EGL)
 #include "AcceleratedSurfaceDMABuf.h"
 #endif
 
@@ -46,7 +46,7 @@ using namespace WebCore;
 
 std::unique_ptr<AcceleratedSurface> AcceleratedSurface::create(WebPage& webPage, Client& client)
 {
-#if PLATFORM(GTK)
+#if PLATFORM(GTK) && USE(EGL)
 #if USE(GBM)
     if (PlatformDisplay::sharedDisplayForCompositing().type() == PlatformDisplay::Type::GBM)
         return AcceleratedSurfaceDMABuf::create(webPage, client);

Source/WebKit/WebProcess/WebPage/gtk/AcceleratedSurfaceDMABuf.cpp
@@ -26,6 +26,8 @@
 #include "config.h"
 #include "AcceleratedSurfaceDMABuf.h"
 
+#if USE(EGL)
+
 #include "AcceleratedBackingStoreDMABufMessages.h"
 #include "AcceleratedSurfaceDMABufMessages.h"
 #include "ShareableBitmap.h"
@@ -425,3 +427,5 @@ void AcceleratedSurfaceDMABuf::frameDone()
 }
 
 } // namespace WebKit
+
+#endif // USE(EGL)

Source/WebKit/WebProcess/WebPage/gtk/AcceleratedSurfaceDMABuf.h
@@ -25,6 +25,8 @@
 
 #pragma once
 
+#if USE(EGL)
+
 #include "AcceleratedSurface.h"
 
 #include "MessageReceiver.h"
@@ -173,3 +175,5 @@ private:
 };
 
 } // namespace WebKit
+
+#endif // USE(EGL)

Source/WebKit/WebProcess/WebPage/gtk/AcceleratedSurfaceDMABuf.messages.in
@@ -20,7 +20,9 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#if USE(EGL)
 messages -> AcceleratedSurfaceDMABuf NotRefCounted {
     ReleaseBuffer(uint64_t id)
     FrameDone()
 }
+#endif

Source/WebKit/WebProcess/glib/WebProcessGLib.cpp
@@ -141,7 +141,7 @@ void WebProcess::platformInitializeWebProcess(WebProcessCreationParameters& para
     WebCore::GBMDevice::singleton().initialize(parameters.renderDeviceFile);
 #endif
 
-#if PLATFORM(GTK)
+#if PLATFORM(GTK) && USE(EGL)
     m_dmaBufRendererBufferMode = parameters.dmaBufRendererBufferMode;
     if (!m_dmaBufRendererBufferMode.isEmpty()) {
 #if USE(GBM)