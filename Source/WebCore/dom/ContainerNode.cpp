61d6af34c94be20d7a6657eea4a8b2e23266a30c
Rob Buis <rbuis@igalia.com>
2023-09-29 14:35:35 +0100

    Contain intrinsic size doesn't get updated during some DOM node removals https://bugs.webkit.org/show_bug.cgi?id=261020
    Reviewed by NOBODY (OOPS!).
    
    WIP.
    
    * Source/WebCore/dom/ContainerNode.cpp:
    (WebCore::ContainerNode::removeChild):
    * Source/WebCore/dom/Element.cpp:
    (WebCore::Element::removedFromAncestor):

--- Source/WebCore/dom/ContainerNode.cpp
+++ Source/WebCore/dom/ContainerNode.cpp
@@ -685,14 +685,6 @@ ExceptionOr<void> ContainerNode::removeChild(Node& oldChild)
     rebuildSVGExtensionsElementsIfNecessary();
     dispatchSubtreeModifiedEvent();
 
-    auto* element = dynamicDowncast<Element>(oldChild);
-    if (element && (element->lastRememberedLogicalWidth() || element->lastRememberedLogicalHeight())) {
-        // The disconnected element could be unobserved because of other properties, here we need to make sure it is observed,
-        // so that deliver could be triggered and it would clear lastRememberedSize.
-        document().observeForContainIntrinsicSize(*element);
-        document().resetObservationSizeForContainIntrinsicSize(*element);
-    }
-
     return { };
 }
 
