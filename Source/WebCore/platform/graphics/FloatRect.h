4f828afc3acd38e21721463eeb2b303d2cbb1be6
Yusuke Suzuki <ysuzuki@apple.com>
2023-10-04 12:54:15 -0700

    Make FloatRect etc. constexpr and adopt nanRect to make it usable for "not-filled" FloatRect purpose https://bugs.webkit.org/show_bug.cgi?id=262636 rdar://116476334
    Reviewed by Justin Michaud and Mark Lam.
    
    Use constexpr in FloatRect and related classes, and add `nanRect` concept to make it usable for "not-filled" FloatRect.
    This is useful in subsequent SVG optimization instead of using `std::optional<FloatRect>`.
    
    * Source/WebCore/platform/graphics/FloatPoint.h:
    (WebCore::FloatPoint::FloatPoint):
    (WebCore::FloatPoint::zero):
    (WebCore::FloatPoint::isZero const):
    (WebCore::FloatPoint::x const):
    (WebCore::FloatPoint::y const):
    (WebCore::FloatPoint::scaled const):
    (WebCore::FloatPoint::dot const):
    (WebCore::FloatPoint::lengthSquared const):
    (WebCore::FloatPoint::shrunkTo const):
    (WebCore::FloatPoint::expandedTo const):
    (WebCore::FloatPoint::transposedPoint const):
    (WebCore::operator+):
    (WebCore::operator-):
    (WebCore::operator*):
    (WebCore::FloatPoint::rotate):
    * Source/WebCore/platform/graphics/FloatRect.h:
    (WebCore::FloatRect::FloatRect):
    (WebCore::FloatRect::location const):
    (WebCore::FloatRect::size const):
    (WebCore::FloatRect::x const):
    (WebCore::FloatRect::y const):
    (WebCore::FloatRect::maxX const):
    (WebCore::FloatRect::maxY const):
    (WebCore::FloatRect::width const):
    (WebCore::FloatRect::height const):
    (WebCore::FloatRect::area const):
    (WebCore::FloatRect::isEmpty const):
    (WebCore::FloatRect::isZero const):
    (WebCore::FloatRect::center const):
    (WebCore::FloatRect::minXMinYCorner const):
    (WebCore::FloatRect::maxXMinYCorner const):
    (WebCore::FloatRect::minXMaxYCorner const):
    (WebCore::FloatRect::maxXMaxYCorner const):
    (WebCore::FloatRect::contains const):
    (WebCore::FloatRect::overlapsYRange const):
    (WebCore::FloatRect::overlapsXRange const):
    (WebCore::FloatRect::transposedRect const):
    (WebCore::operator+):
    (WebCore::FloatRect::infiniteRect):
    (WebCore::FloatRect::isInfinite const):
    (WebCore::FloatRect::smallestRect):
    (WebCore::FloatRect::isSmallest const):
    (WebCore::FloatRect::nanRect):
    (WebCore::FloatRect::isNaN const):
    * Source/WebCore/platform/graphics/FloatSize.cpp:
    (WebCore::FloatSize::isZero const): Deleted.
    * Source/WebCore/platform/graphics/FloatSize.h:
    (WebCore::FloatSize::isZero const):
    * Tools/TestWebKitAPI/Tests/WebCore/FloatRectTests.cpp:
    (TestWebKitAPI::TEST):
    * Tools/TestWebKitAPI/Tests/WebCore/cg/DisplayListTestsCG.cpp:
    (TestWebKitAPI::TEST):
    
    Canonical link: https://commits.webkit.org/268866@main

--- Source/WebCore/platform/graphics/FloatRect.h
+++ Source/WebCore/platform/graphics/FloatRect.h
@@ -65,42 +65,42 @@ public:
         InsideButNotOnStroke
     };
 
-    FloatRect() { }
-    FloatRect(const FloatPoint& location, const FloatSize& size)
+    constexpr FloatRect() = default;
+    constexpr FloatRect(const FloatPoint& location, const FloatSize& size)
         : m_location(location), m_size(size) { }
-    FloatRect(float x, float y, float width, float height)
+    constexpr FloatRect(float x, float y, float width, float height)
         : m_location(FloatPoint(x, y)), m_size(FloatSize(width, height)) { }
-    FloatRect(const FloatPoint& topLeft, const FloatPoint& bottomRight)
+    constexpr FloatRect(const FloatPoint& topLeft, const FloatPoint& bottomRight)
         : m_location(topLeft), m_size(FloatSize(bottomRight.x() - topLeft.x(), bottomRight.y() - topLeft.y())) { }
     WEBCORE_EXPORT FloatRect(const IntRect&);
 
     static FloatRect narrowPrecision(double x, double y, double width, double height);
 
-    FloatPoint location() const { return m_location; }
-    FloatSize size() const { return m_size; }
+    constexpr FloatPoint location() const { return m_location; }
+    constexpr FloatSize size() const { return m_size; }
 
     void setLocation(const FloatPoint& location) { m_location = location; }
     void setSize(const FloatSize& size) { m_size = size; }
 
-    float x() const { return m_location.x(); }
-    float y() const { return m_location.y(); }
-    float maxX() const { return x() + width(); }
-    float maxY() const { return y() + height(); }
-    float width() const { return m_size.width(); }
-    float height() const { return m_size.height(); }
+    constexpr float x() const { return m_location.x(); }
+    constexpr float y() const { return m_location.y(); }
+    constexpr float maxX() const { return x() + width(); }
+    constexpr float maxY() const { return y() + height(); }
+    constexpr float width() const { return m_size.width(); }
+    constexpr float height() const { return m_size.height(); }
 
-    float area() const { return m_size.area(); }
+    constexpr float area() const { return m_size.area(); }
 
     void setX(float x) { m_location.setX(x); }
     void setY(float y) { m_location.setY(y); }
     void setWidth(float width) { m_size.setWidth(width); }
     void setHeight(float height) { m_size.setHeight(height); }
 
-    bool isEmpty() const { return m_size.isEmpty(); }
-    bool isZero() const { return m_size.isZero(); }
+    constexpr bool isEmpty() const { return m_size.isEmpty(); }
+    constexpr bool isZero() const { return m_size.isZero(); }
     bool isExpressibleAsIntRect() const;
 
-    FloatPoint center() const { return location() + size() / 2; }
+    constexpr FloatPoint center() const { return location() + size() / 2; }
 
     void move(const FloatSize& delta) { m_location += delta; } 
     void moveBy(const FloatPoint& delta) { m_location.move(delta.x(), delta.y()); }
@@ -169,10 +169,10 @@ public:
         shiftMaxYEdgeTo(maxY() + delta);
     }
 
-    FloatPoint minXMinYCorner() const { return m_location; } // typically topLeft
-    FloatPoint maxXMinYCorner() const { return FloatPoint(m_location.x() + m_size.width(), m_location.y()); } // typically topRight
-    FloatPoint minXMaxYCorner() const { return FloatPoint(m_location.x(), m_location.y() + m_size.height()); } // typically bottomLeft
-    FloatPoint maxXMaxYCorner() const { return FloatPoint(m_location.x() + m_size.width(), m_location.y() + m_size.height()); } // typically bottomRight
+    constexpr FloatPoint minXMinYCorner() const { return m_location; } // typically topLeft
+    constexpr FloatPoint maxXMinYCorner() const { return FloatPoint(m_location.x() + m_size.width(), m_location.y()); } // typically topRight
+    constexpr FloatPoint minXMaxYCorner() const { return FloatPoint(m_location.x(), m_location.y() + m_size.height()); } // typically bottomLeft
+    constexpr FloatPoint maxXMaxYCorner() const { return FloatPoint(m_location.x() + m_size.width(), m_location.y() + m_size.height()); } // typically bottomRight
 
     WEBCORE_EXPORT bool intersects(const FloatRect&) const;
     WEBCORE_EXPORT bool inclusivelyIntersects(const FloatRect&) const;
@@ -188,11 +188,11 @@ public:
 
     // Note, this doesn't match what IntRect::contains(IntPoint&) does; the int version
     // is really checking for containment of 1x1 rect, but that doesn't make sense with floats.
-    bool contains(float px, float py) const
+    constexpr bool contains(float px, float py) const
         { return px >= x() && px <= maxX() && py >= y() && py <= maxY(); }
 
-    bool overlapsYRange(float y1, float y2) const { return !isEmpty() && y2 >= y1 && y2 >= y() && y1 <= maxY(); }
-    bool overlapsXRange(float x1, float x2) const { return !isEmpty() && x2 >= x1 && x2 >= x() && x1 <= maxX(); }
+    constexpr bool overlapsYRange(float y1, float y2) const { return !isEmpty() && y2 >= y1 && y2 >= y() && y1 <= maxY(); }
+    constexpr bool overlapsXRange(float x1, float x2) const { return !isEmpty() && x2 >= x1 && x2 >= x() && x1 <= maxX(); }
 
     void inflateX(float dx) {
         m_location.setX(m_location.x() - dx);
@@ -210,7 +210,7 @@ public:
     WEBCORE_EXPORT void scale(float sx, float sy);
     void scale(FloatSize size) { scale(size.width(), size.height()); }
 
-    FloatRect transposedRect() const { return FloatRect(m_location.transposedPoint(), m_size.transposedSize()); }
+    constexpr FloatRect transposedRect() const { return FloatRect(m_location.transposedPoint(), m_size.transposedSize()); }
 
 #if USE(CG)
     WEBCORE_EXPORT FloatRect(const CGRect&);
@@ -231,11 +231,14 @@ public:
     WEBCORE_EXPORT FloatRect(const RECT&);
 #endif
 
-    static FloatRect infiniteRect();
-    bool isInfinite() const;
+    static constexpr FloatRect infiniteRect();
+    constexpr bool isInfinite() const;
 
-    static FloatRect smallestRect();
-    bool isSmallest() const;
+    static constexpr FloatRect smallestRect();
+    constexpr bool isSmallest() const;
+
+    static constexpr FloatRect nanRect();
+    constexpr bool isNaN() const;
 
     WEBCORE_EXPORT String toJSONString() const;
     WEBCORE_EXPORT Ref<JSON::Object> toJSONObject() const;
@@ -276,11 +279,14 @@ inline FloatRect& operator+=(FloatRect& a, const FloatRect& b)
     return a;
 }
 
-inline FloatRect operator+(const FloatRect& a, const FloatRect& b)
+constexpr FloatRect operator+(const FloatRect& a, const FloatRect& b)
 {
-    FloatRect c = a;
-    c += b;
-    return c;
+    return FloatRect {
+        a.x() + b.x(),
+        a.y() + b.y(),
+        a.width() + b.width(),
+        a.height() + b.height(),
+    };
 }
 
 inline bool areEssentiallyEqual(const FloatRect& a, const FloatRect& b)
@@ -288,26 +294,49 @@ inline bool areEssentiallyEqual(const FloatRect& a, const FloatRect& b)
     return areEssentiallyEqual(a.location(), b.location()) && areEssentiallyEqual(a.size(), b.size());
 }
 
-inline FloatRect FloatRect::infiniteRect()
+constexpr FloatRect FloatRect::infiniteRect()
 {
-    static FloatRect infiniteRect(-std::numeric_limits<float>::max() / 2, -std::numeric_limits<float>::max() / 2, std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
-    return infiniteRect;
+    return {
+        -std::numeric_limits<float>::max() / 2,
+        -std::numeric_limits<float>::max() / 2,
+        std::numeric_limits<float>::max(),
+        std::numeric_limits<float>::max()
+    };
 }
 
-inline bool FloatRect::isInfinite() const
+constexpr bool FloatRect::isInfinite() const
 {
     return *this == infiniteRect();
 }
 
-inline FloatRect FloatRect::smallestRect()
+constexpr FloatRect FloatRect::smallestRect()
 {
-    static FloatRect smallestRect(std::numeric_limits<float>::max() / 2, std::numeric_limits<float>::max() / 2, -std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
-    return smallestRect;
+    return {
+        std::numeric_limits<float>::max() / 2,
+        std::numeric_limits<float>::max() / 2,
+        -std::numeric_limits<float>::max(),
+        -std::numeric_limits<float>::max()
+    };
 }
 
-inline bool FloatRect::isSmallest() const
+constexpr bool FloatRect::isSmallest() const
 {
     return *this == smallestRect();
+}
+
+constexpr FloatRect FloatRect::nanRect()
+{
+    return {
+        std::numeric_limits<float>::quiet_NaN(),
+        std::numeric_limits<float>::quiet_NaN(),
+        std::numeric_limits<float>::quiet_NaN(),
+        std::numeric_limits<float>::quiet_NaN()
+    };
+}
+
+constexpr bool FloatRect::isNaN() const
+{
+    return isnan(x());
 }
 
 inline void FloatRect::inflate(float deltaX, float deltaY, float deltaMaxX, float deltaMaxY)
