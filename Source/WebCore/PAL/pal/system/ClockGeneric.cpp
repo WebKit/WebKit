4f53db6c72686f745024a8dc9b6cecb9c83d48b1
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2023-10-04 12:52:30 -0700

    Fix Clock offset bug for setPlayRate
    https://bugs.webkit.org/show_bug.cgi?id=262619
    
    Reviewed by Eric Carlson.
    
    Merge: https://src.chromium.org/viewvc/blink?view=revision&revision=159814
    
    Stopping and then restarting a WebCore::ClockGeneric resulted in junk values on subsequent currentTime() calls,
    this caused MediaController (the only user of Clock as far as I can tell) to lose the playback position when paused.
    
    * Source/WebCore/PAL/pal/system/ClockGeneric.cpp:
    (ClockGeneric::currentTime):
    (ClockGeneric::setPlayRate):
    (ClockGeneric::stop):
    (ClockGeneric::currentDelta):
    * Source/WebCore/PAL/pal/system/ClockGeneric.h:
    * LayoutTests/media/media-controller-play-then-pause.html: Add Test Case
    * LayoutTests/media/media-controller-play-then-pause-expected.txt: Add Test Case Expectation
    
    Canonical link: https://commits.webkit.org/268865@main

--- Source/WebCore/PAL/pal/system/ClockGeneric.cpp
+++ Source/WebCore/PAL/pal/system/ClockGeneric.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2011 Apple Inc.  All rights reserved.
+ * Copyright (C) 2013 Google Inc.  All rights reserved.
  * Copyright (C) 2017 Sony Interactive Entertainment Inc.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -51,14 +52,12 @@ void ClockGeneric::setCurrentTime(double time)
 
 double ClockGeneric::currentTime() const
 {
-    if (m_running)
-        m_lastTime = now();
-    return ((m_lastTime - m_startTime).seconds() * m_rate) + m_offset;
+    return currentDelta() + m_offset;
 }
 
 void ClockGeneric::setPlayRate(double rate)
 {
-    m_offset = currentTime();
+    m_offset += currentDelta();
     m_lastTime = m_startTime = now();
     m_rate = rate;
 }
@@ -77,7 +76,7 @@ void ClockGeneric::stop()
     if (!m_running)
         return;
 
-    m_offset = currentTime();
+    m_offset += currentDelta();
     m_lastTime = m_startTime = now();
     m_running = false;
 }
@@ -85,6 +84,13 @@ void ClockGeneric::stop()
 MonotonicTime ClockGeneric::now() const
 {
     return MonotonicTime::now();
+}
+
+double ClockGeneric::currentDelta() const
+{
+    if (m_running)
+        m_lastTime = now();
+    return (m_lastTime - m_startTime).seconds() * m_rate;
 }
 
 }
