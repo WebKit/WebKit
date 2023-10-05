b3c358d4525aec3be45a32e22c25b2c71fc1f3b4
Mark Lam <mark.lam@apple.com>
2023-10-04 20:53:33 -0700

    Cherry-pick 267815.202@safari-7617-branch (401705903095). https://bugs.webkit.org/show_bug.cgi?id=262616
        An Array index in CloneSerializer and CloneDeserializer can be confused for NonIndexPropertiesTag.
        https://bugs.webkit.org/show_bug.cgi?id=262616
        rdar://116034413
    
        Reviewed by Keith Miller, Sihui Liu and Chris Dumez.
    
        CloneSerializer and CloneDeserializer were previously using NonIndexPropertiesTag as the terminator of
        the indexed property section of an Array.  However, NonIndexPropertiesTag's encoding is 0xFFFFFFFD,
        which is less than MAX_ARRAY_INDEX (0xFFFFFFFE) i.e. an index of 0xFFFFFFFD can be confused for the
        NonIndexPropertiesTag, resulting type confusion.
    
        This patch changes the structure of a serialized Array to always terminate its indexed property section
        with a TerminatorTag (0xFFFFFFFF) first before looking for either a NonIndexPropertiesTag or another
        TerminatorTag.  The presence of a NonIndexPropertiesTag after the 1st TerminatorTag indicates the
        presence of a non-indexed properties section.  The presense of a TerminatorTag immediately after the
        1st TerminatorTag indicates that the non-indexed properties section is empty.
    
        Also updated the comment describing the shape of a serialized Array, and rebased a test.
    
        * LayoutTests/fast/storage/serialized-script-value.html:
        * Source/WebCore/bindings/js/SerializedScriptValue.cpp:
        (WebCore::CloneSerializer::serialize):
        (WebCore::CloneDeserializer::deserialize):
    
        Canonical link: https://commits.webkit.org/267815.202@safari-7617-branch
    
    Canonical link: https://commits.webkit.org/266719.187@webkitglib/2.42

--- Source/WebCore/bindings/js/SerializedScriptValue.cpp
+++ Source/WebCore/bindings/js/SerializedScriptValue.cpp
@@ -66,6 +66,7 @@
 #include "WebCodecsEncodedVideoChunk.h"
 #include "WebCoreJSClientData.h"
 #include <JavaScriptCore/APICast.h>
+#include <JavaScriptCore/ArrayConventions.h>
 #include <JavaScriptCore/BigIntObject.h>
 #include <JavaScriptCore/BooleanObject.h>
 #include <JavaScriptCore/CatchScope.h>
@@ -539,6 +540,8 @@ static constexpr uint32_t ImageDataPoolTag = 0xFFFFFFFE;
 // The high bit of a StringData's length determines the character size.
 static constexpr unsigned StringDataIs8BitFlag = 0x80000000;
 
+static_assert(TerminatorTag > MAX_ARRAY_INDEX);
+
 /*
  * Object serialization is performed according to the following grammar, all tags
  * are recorded as a single uint8_t.
@@ -551,7 +554,7 @@ static constexpr unsigned StringDataIs8BitFlag = 0x80000000;
  * Value :- Array | Object | Map | Set | Terminal
  *
  * Array :-
- *     ArrayTag <length:uint32_t>(<index:uint32_t><value:Value>)* TerminatorTag
+ *     ArrayTag <length:uint32_t>(<index:uint32_t><value:Value>)* TerminatorTag (NonIndexPropertiesTag (<name:StringData><value:Value>)*) TerminatorTag
  *
  * Object :-
  *     ObjectTag (<name:StringData><value:Value>)* TerminatorTag
@@ -2357,6 +2360,7 @@ SerializationReturnCode CloneSerializer::serialize(JSValue in)
                 if (index == lengthStack.last()) {
                     indexStack.removeLast();
                     lengthStack.removeLast();
+                    write(TerminatorTag); // Terminate the indexed property section.
 
                     propertyStack.append(PropertyNameArray(vm, PropertyNameMode::Strings, PrivateSymbolMode::Exclude));
                     array->getOwnNonIndexPropertyNames(m_lexicalGlobalObject, propertyStack.last(), DontEnumPropertiesMode::Exclude);
@@ -4707,12 +4711,21 @@ DeserializationResult CloneDeserializer::deserialize()
                 goto error;
             }
             if (index == TerminatorTag) {
-                JSObject* outArray = outputObjectStack.last();
-                outValue = outArray;
-                outputObjectStack.removeLast();
-                break;
-            } else if (index == NonIndexPropertiesTag) {
-                goto objectStartVisitMember;
+                // We reached the end of the indexed properties section.
+                if (!read(index)) {
+                    fail();
+                    goto error;
+                }
+                // At this point, we're either done with the array or is starting the
+                // non-indexed property section.
+                if (index == TerminatorTag) {
+                    JSObject* outArray = outputObjectStack.last();
+                    outValue = outArray;
+                    outputObjectStack.removeLast();
+                    break;
+                }
+                if (index == NonIndexPropertiesTag)
+                    goto objectStartVisitMember;
             }
 
             if (JSValue terminal = readTerminal()) {
