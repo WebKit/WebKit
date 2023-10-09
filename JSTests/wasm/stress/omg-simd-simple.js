f9a3a2147af0a89a67c74b9da7a291387c6cb39c
Justin Michaud <justin_michaud@apple.com>
2023-10-09 09:32:39 -0700

    WebAssembly SIMD results in incorrect float values and non working bitwise operations https://bugs.webkit.org/show_bug.cgi?id=258302 rdar://111050621
    Reviewed by Alexey Shvayka.
    
    We accidentally clobber our input, causing some random SIMD bugs.
    
    This was pretty difficult to isolate, even though the bug is so obvious.
    The included (larger) test case actually serves as a pretty general way
    to debug a differential jit-tier bug.
    
    * JSTests/wasm/stress/omg-simd-simple.js: Added.
    (from.string_appeared_here.import.as.assert.from.string_appeared_here.async test):
    * JSTests/wasm/stress/omg-simd-stress.js: Added.
    (from.string_appeared_here.import.as.assert.from.string_appeared_here.async test.):
    (from.string_appeared_here.import.as.assert.from.string_appeared_here.async test):
    * JSTests/wasm/stress/omg-simd-stress.wat: Added.
    * Source/JavaScriptCore/b3/air/AirLowerMacros.cpp:
    (JSC::B3::Air::lowerMacros):
    
    Canonical link: https://commits.webkit.org/269080@main

---
+++ JSTests/wasm/stress/omg-simd-simple.js
@@ -0,0 +1,33 @@
+//@ skip if !$isSIMDPlatform
+import { instantiate } from "../wabt-wrapper.js"
+import * as assert from "../assert.js"
+
+async function test() {
+    let wat = `(module
+      (func $bad (export "bad") (result i64)
+        (local $l41 v128)
+        (local.set $l41
+        (v128.const i32x4 0xFFFFFFFF 0xFFFFFFFF 0xFFFFFFFF 0xFFFFFFFF))
+
+          (if (result i64)
+            (i32.eqz
+              (v128.any_true
+                (local.get $l41)))
+            (then (i64.const 1))
+            (else
+              (i64x2.extract_lane 1
+                  (v128.const i32x4 0xFFFFFFFF 0xFFFFFFFF 0xFFFFFFFF 0xFFFFFFFF))
+            )
+          )
+      )
+)
+`
+    const instance = await instantiate(wat, { }, { simd: true, log: false })
+    const { bad } = instance.exports
+
+    for (let i = 0; i < 100000; ++i) {
+      assert.eq(bad(), -1n)
+    }
+}
+
+assert.asyncTest(test())
