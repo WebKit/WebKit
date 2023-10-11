d9d1b1d6c47b7939948980932b574b2a70e9b006
Jonathan Bedard <jbedard@apple.com>
2023-10-11 16:41:45 -0700

    [webkitcorepy] Support wheels containing a .py file https://bugs.webkit.org/show_bug.cgi?id=263042 rdar://116831655
    Reviewed by Dewei Zhu.
    
    * Tools/Scripts/libraries/webkitcorepy/setup.py: Bump version.
    * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/__init__.py: Ditto.
    * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/autoinstall.py:
    (Package.install): Handle case where a wheel contains a single .py file.
    
    Canonical link: https://commits.webkit.org/269226@main

--- Tools/Scripts/libraries/webkitcorepy/setup.py
+++ Tools/Scripts/libraries/webkitcorepy/setup.py
@@ -30,7 +30,7 @@ def readme():
 
 setup(
     name='webkitcorepy',
-    version='0.16.5',
+    version='0.16.6',
     description='Library containing various Python support classes and functions.',
     long_description=readme(),
     classifiers=[
