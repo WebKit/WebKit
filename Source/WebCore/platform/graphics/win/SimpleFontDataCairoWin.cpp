WebKit/WebKit

commit 5051fe83ad0c60f63ecc59b3cc91c4b3a7bca8ec

[Win] Reduce cairo win32 font API usage https://bugs.webkit.org/show_bug.cgi?id=262768
Reviewed by Don Olmstead.

As a preparation of migration to cairo dwrite font backend
(bug#215259), use Windows GDI API to get font information instead of
cairo win32 font API.

Windows port is still using the legacy Uniscribe API for shaping. The
legacy Windows GDI API should be replaced by DirectWrite in the
futute.

* Source/WebCore/platform/graphics/FontPlatformData.h:
* Source/WebCore/platform/graphics/win/FontPlatformDataCairoWin.cpp:
(WebCore::FontPlatformData::platformDataInit):
(WebCore::FontPlatformData::familyName const):
* Source/WebCore/platform/graphics/win/FontPlatformDataWin.cpp:
(WebCore::FontPlatformData::FontPlatformData):
* Source/WebCore/platform/graphics/win/SimpleFontDataCairoWin.cpp:
(WebCore::Font::platformInit):
* LayoutTests/platform/wincairo/TestExpectations:

Canonical link: https://commits.webkit.org/269016@main

Source/WebCore/platform/graphics/win/SimpleFontDataCairoWin.cpp
@@ -55,10 +55,8 @@ void Font::platformInit()
     HWndDC dc(0);
     SaveDC(dc);
 
-    cairo_scaled_font_t* scaledFont = m_platformData.scaledFont();
-    const double metricsMultiplier = cairo_win32_scaled_font_get_metrics_factor(scaledFont) * m_platformData.size();
-
-    cairo_win32_scaled_font_select_font(scaledFont, dc);
+    const double metricsMultiplier = 1. / cWindowsFontScaleFactor;
+    HGDIOBJ oldFont = SelectObject(dc, m_platformData.hfont());
 
     wchar_t faceName[LF_FACESIZE];
     GetTextFace(dc, LF_FACESIZE, faceName);
@@ -66,7 +64,14 @@ void Font::platformInit()
     OUTLINETEXTMETRIC metrics;
     GetOutlineTextMetrics(dc, sizeof(metrics), &metrics);
 
-    cairo_win32_scaled_font_done_font(scaledFont);
+    float xHeight = metrics.otmTextMetrics.tmAscent * 0.56f; // Best guess for xHeight if no x glyph is present.
+    GLYPHMETRICS gm;
+    static const MAT2 identity = { { 0, 1 }, { 0, 0 }, { 0, 0 }, { 0, 1 } };
+    DWORD len = GetGlyphOutline(dc, 'x', GGO_METRICS, &gm, 0, 0, &identity);
+    if (len != GDI_ERROR && gm.gmptGlyphOrigin.y > 0)
+        xHeight = gm.gmptGlyphOrigin.y;
+
+    SelectObject(dc, oldFont);
     RestoreDC(dc, -1);
 
     // Disable antialiasing when rendering with Ahem because many tests require this.
@@ -86,14 +91,9 @@ void Font::platformInit()
     m_fontMetrics.setLineGap(lineGap);
     m_fontMetrics.setLineSpacing(lroundf(ascent) + lroundf(descent) + lroundf(lineGap));
     m_fontMetrics.setUnitsPerEm(metrics.otmEMSquare);
+    m_fontMetrics.setXHeight(xHeight * metricsMultiplier);
     m_avgCharWidth = metrics.otmTextMetrics.tmAveCharWidth * metricsMultiplier;
     m_maxCharWidth = metrics.otmTextMetrics.tmMaxCharWidth * metricsMultiplier;
-
-    cairo_text_extents_t extents;
-    cairo_scaled_font_text_extents(scaledFont, "x", &extents);
-    float xHeight = -extents.y_bearing;
-
-    m_fontMetrics.setXHeight(xHeight);
 }
 
 void Font::determinePitch()

LayoutTests/platform/wincairo/TestExpectations
@@ -451,14 +451,11 @@ streams/readable-stream-byob-request.html [ Skip ]
 streams/readable-stream-byob-request-worker.html [ Skip ]
 
 # image diffs off by 0.0x%
-css2.1/20110323/c541-word-sp-000.htm [ ImageOnlyFailure ]
 css3/unicode-bidi-isolate-aharon.html [ ImageOnlyFailure ]
 fast/css/cascade/box-shadow-and-webkit-box-shadow-cascade-order.html [ ImageOnlyFailure ]
 fast/css/content-counter-010.htm [ ImageOnlyFailure ]
 fast/css/counters/counter-list-item.html [ ImageOnlyFailure ]
 fast/css/multiple-tabs.html [ ImageOnlyFailure ]
-fast/css/sticky/sticky-writing-mode-vertical-lr.html [ ImageOnlyFailure ]
-fast/css/sticky/sticky-writing-mode-vertical-rl.html [ ImageOnlyFailure ]
 fast/css/unexpected-word-wrapping-with-non-empty-spans.html [ ImageOnlyFailure ]
 fast/css-generated-content/close-quote-negative-depth.html [ ImageOnlyFailure Pass ]
 fast/css-generated-content/content-property-change.html [ ImageOnlyFailure Pass ]
@@ -478,30 +475,21 @@ imported/blink/fast/css/text-overflow-ellipsis-button.html [ ImageOnlyFailure Pa
 editing/caret/caret-in-empty-cell.html [ ImageOnlyFailure ]
 
 css3/background/background-repeat-space-border.html [ ImageOnlyFailure ]
-css3/masking/clip-path-reference-userSpaceOnUse.html [ ImageOnlyFailure ]
 css3/masking/clip-path-reference.html [ ImageOnlyFailure ]
 css3/masking/mask-repeat-space-content.html [ ImageOnlyFailure ]
 
 fast/css/absolute-child-with-percent-height-inside-relative-parent.html [ Failure ]
-fast/css/background-image-with-baseurl.html [ Failure ]
 fast/css/draggable-region-parser.html [ Failure ]
 fast/css/empty-generated-content.html [ Failure ]
 fast/css/font-face-default-font.html [ Failure ]
 fast/css/font-face-in-media-rule.html [ Failure ]
-fast/css/font-face-locally-installed.html [ Failure ]
 fast/css/font-face-multiple-ranges-for-unicode-range.html [ Failure ]
 fast/css/font-face-woff.html [ Failure ]
-fast/css/font-size-negative.html [ Failure ]
 fast/css/image-rendering.html [ Failure ]
-fast/css/imageTileOpacity.html [ Failure ]
-fast/css/import_with_baseurl.html [ Failure ]
 fast/css/line-height-determined-by-primary-font.html [ Failure ]
 fast/css/line-height-font-order.html [ Failure ]
-fast/css/line-height-negative.html [ Failure ]
-fast/css/max-height-none.html [ Failure ]
 fast/css/min-width-with-spanned-cell-fixed.html [ Failure ]
 fast/css/outline-auto-empty-rects.html [ Failure ]
-fast/css/vertical-align-lengths.html [ Failure ]
 fast/css/visibility-hit-test.html [ Failure ]
 
 fast/dom/HTMLLinkElement/preconnect-support.html [ Failure ]
@@ -611,7 +599,6 @@ crypto/subtle/rsa-pss-import-key-sign-large-salt.html [ Failure ]
 # https://bugs.webkit.org/show_bug.cgi?id=221569
 crypto/crypto-random-values-oom.html [ Skip ]
 
-crypto/subtle/ec-generate-key-malformed-parameters.html [ Failure ]
 crypto/subtle/rsa-indexeddb-non-exportable-private.html [ Skip ] # Timeout
 crypto/subtle/rsa-indexeddb-non-exportable.html [ Skip ] # Timeout
 crypto/subtle/rsa-indexeddb-private.html [ Skip ] # Timeout
@@ -747,7 +734,6 @@ http/tests/misc/authentication-redirect-3/authentication-sent-to-redirect-same-o
 http/tests/misc/favicon-loads-with-icon-loading-override.html [ Failure ]
 http/tests/misc/form-submit-file-cross-site.html [ Skip ]
 http/tests/misc/link-rel-icon-beforeload.html [ Failure ]
-http/tests/misc/object-embedding-svg-delayed-size-negotiation.xhtml [ Failure ]
 http/tests/misc/timer-vs-loading.html [ Skip ]
 http/tests/misc/webtiming-cross-origin-and-back1.html [ Failure Pass ]
 http/tests/misc/webtiming-no-origin.html [ Failure Pass ]
@@ -1087,12 +1073,9 @@ webkit.org/b/149774 css3/font-variant-small-caps-synthesis.html [ Skip ]
 # Incorrect upstream, but seem to pass on the bots, possibly with a broken baseline.
 css2.1/20110323/replaced-intrinsic-001.htm [ Pass ]
 css2.1/20110323/replaced-intrinsic-002.htm [ Pass ]
-css3/flexbox/csswg/ttwf-reftest-flex-wrap-reverse.html [ Pass ]
-css3/flexbox/csswg/ttwf-reftest-flex-wrap.html [ Pass ]
 
 fast/css/apple-system-colors.html [ Failure ]
 fast/css/paint-order-shadow.html [ ImageOnlyFailure ]
-fast/css-generated-content/initial-letter-pagination-raised.html [ ImageOnlyFailure ]
 
 # color-filters are off by default
 webkit.org/b/185076 css3/color-filters [ Skip ]
@@ -1139,9 +1122,6 @@ fast/dom/Window/slow-unload-handler-only-frame-is-stopped.html [ Failure ]
 fast/dom/timer-throttling-hidden-page.html [ Skip ] # Timeout
 fast/dom/timer-throttling-hidden-page-2.html [ Skip ] # Timeout
 
-# This test is dependent on DRT being able to open windows.
-fast/dom/Window/new-window-opener.html [ Failure ]
-
 # <rdar://5313536> Times out
 fast/dom/frame-loading-via-document-write.html [ Skip ]
 
@@ -1173,7 +1153,6 @@ webkit.org/b/203100 editing/async-clipboard [ Skip ]
 
 ###### Selection
 editing/selection/context-menu-text-selection-lookup.html [ Failure ]
-editing/selection/drag-start-event-client-x-y.html [ Failure ]
 editing/selection/extend-selection-word.html [ Failure ]
 editing/selection/move-by-word-visually-mac.html [ Failure ]
 editing/selection/move-left-right.html [ Failure ]
@@ -1219,16 +1198,9 @@ editing/secure-input [ Skip ]
 
 editing/deleting/delete-ligature-001.html [ Failure ]
 editing/deleting/delete-ligature-002.html [ Failure ]
-editing/deleting/smart-delete-001.html [ Failure ]
-editing/deleting/smart-delete-002.html [ Failure ]
-editing/deleting/smart-delete-003.html [ Failure ]
-editing/deleting/smart-delete-004.html [ Failure ]
 
 webkit.org/b/179613 editing/execCommand/underline-selection-containing-image.html [ Failure ]
 
-editing/caret/emoji.html [ Failure ]
-editing/deleting/delete-emoji.html [ Failure ]
-editing/input/caret-at-the-edge-of-input.html [ Failure ]
 webkit.org/b/150684 editing/input/reveal-caret-of-multiline-contenteditable.html [ Pass Failure ]
 
 webkit.org/b/173281 editing/style/set-foreColor-with-color-filter.html [ Skip ]
@@ -1838,7 +1810,6 @@ webxr [ Skip ]
 animations/shadow-host-child-change.html [ Skip ]
 css1/font_properties/font_family.html [ Failure Pass ]
 css2.1/t1503-c522-font-family-00-b.html [ Failure Pass ]
-css3/flexbox/flexbox-baseline-margins.html [ Failure ]
 css3/font-feature-settings-stylistic-set.html [ ImageOnlyFailure ]
 editing/selection/move-by-word-visually-across-object-element-1.html [ Failure ]
 editing/selection/move-by-word-visually-across-object-element-2.html [ Failure ]
@@ -1859,8 +1830,6 @@ fast/canvas/canvas-measureText-2.html [ Failure ]
 fast/canvas/canvas-quadratic-same-endpoint.html [ Failure ]
 fast/canvas/canvas-toDataURL-webp.html [ Failure ]
 fast/canvas/webgl/array-bounds-clamping.html [ Failure ]
-fast/canvas/webgl/css-webkit-canvas-repaint.html [ Failure ]
-fast/canvas/webgl/css-webkit-canvas.html [ Failure ]
 fast/canvas/webgl/drawingbuffer-test.html [ Skip ] # Crash
 fast/canvas/webgl/lose-context-on-status-failure.html [ Skip ] # Timeout
 fast/canvas/webgl/oes-texture-float-linear.html [ Failure ]
@@ -1874,12 +1843,9 @@ fast/canvas/webgl/webgl-compressed-texture-size-limit.html [ Failure ]
 fast/canvas/webgl/webgl-depth-texture.html [ Failure ]
 fast/canvas/webgl/webgl2-texture-upload-enums.html [ Failure ]
 fast/css-grid-layout/grid-align-baseline-vertical.html [ Failure ]
-fast/css/font-face-multiple-faces.html [ Failure ]
-fast/css/font-face-synthetic-bold-italic.html [ Failure ]
 fast/custom-elements/defined-rule.html [ ImageOnlyFailure ]
 fast/dom/call-a-constructor-as-a-function.html [ Failure ]
 fast/dynamic/float-in-trailing-whitespace-after-last-line-break-2.html [ Failure ]
-fast/dynamic/mail-autosize-with-iframe.html [ Failure ]
 fast/editing/frame-selection-is-not-caret.html [ Skip ] # Crash by assertion failure
 fast/events/attempt-scroll-with-no-scrollbars.html [ Failure ]
 fast/events/autoscroll-when-input-is-offscreen.html [ Skip ] # UIScript
@@ -1930,7 +1896,6 @@ fast/html/details-comment-crash.html [ ImageOnlyFailure ]
 fast/inline-block/hidpi-margin-top-with-subpixel-value-and-overflow-hidden.html [ ImageOnlyFailure ]
 fast/shrink-wrap/rect-shrink-wrap.html [ ImageOnlyFailure ]
 fast/text/ahom.html [ ImageOnlyFailure ]
-fast/text/backslash-to-yen-sign-euc.html [ Failure ]
 fast/text/bidi-override-isolate.html [ ImageOnlyFailure ]
 fast/text/chinese-font-name-aliases-2.html [ ImageOnlyFailure ]
 fast/text/chinese-font-name-aliases.html [ ImageOnlyFailure ]
@@ -1943,10 +1908,8 @@ fast/text/emoji-overlap.html [ ImageOnlyFailure ]
 fast/text/emoji-synthesis.html [ ImageOnlyFailure ]
 fast/text/emoji-variation-selector.html [ ImageOnlyFailure ]
 fast/text/emoji-with-joiner.html [ Failure ]
-fast/text/emoji.html [ Failure ]
 fast/text/emphasis-overlap.html [ Failure ]
 fast/text/fallback-language-han-2.html [ ImageOnlyFailure ]
-fast/text/fallback-traits-fixup.html [ Failure ]
 fast/text/flag-codepoint.html [ ImageOnlyFailure ]
 fast/text/font-collection-2.html [ ImageOnlyFailure ]
 fast/text/font-collection.html [ ImageOnlyFailure ]
@@ -1957,14 +1920,12 @@ fast/text/font-variations-feature-detection.html [ ImageOnlyFailure ]
 fast/text/hyphenate-avoid-orphaned-word.html [ Failure ]
 fast/text/image-alt-text-bidi-2.html [ ImageOnlyFailure ]
 fast/text/image-alt-text-bidi.html [ ImageOnlyFailure ]
-fast/text/international/001.html [ Failure ]
 fast/text/international/bdi-dir-default-to-auto.html [ ImageOnlyFailure ]
 fast/text/international/bdi-neutral-wrapped.html [ ImageOnlyFailure ]
 fast/text/international/cjk-segmentation.html [ Failure ]
 fast/text/international/inline-plaintext-is-isolated.html [ ImageOnlyFailure ]
 fast/text/international/kana-voiced-sound-marks-1.html [ ImageOnlyFailure ]
 fast/text/international/kana-voiced-sound-marks-2.html [ ImageOnlyFailure ]
-fast/text/international/plane2.html [ Failure ]
 fast/text/international/spaces-combined-in-vertical-text.html [ Failure ]
 fast/text/international/system-language/declarative-language.html [ Failure ]
 fast/text/international/system-language/han-quotes.html [ ImageOnlyFailure ]
@@ -1975,7 +1936,6 @@ fast/text/justify-ideograph-complex.html [ Failure ]
 fast/text/justify-ideograph-simple.html [ Failure ]
 fast/text/justify-ideograph-vertical.html [ Failure ]
 fast/text/justify-nbsp.html [ Failure ]
-fast/text/khmer-lao-font.html [ Failure ]
 fast/text/line-break-between-text-nodes-latin1.html [ ImageOnlyFailure ]
 fast/text/line-break-between-text-nodes.html [ ImageOnlyFailure ]
 fast/text/line-break-with-locale.html [ ImageOnlyFailure ]
@@ -1993,10 +1953,7 @@ fast/text/mending-heart.html [ Failure ]
 fast/text/multiple-codeunit-vertical-upright-2.html [ ImageOnlyFailure ]
 fast/text/multiple-codeunit-vertical-upright.html [ ImageOnlyFailure ]
 fast/text/multiple-feature-properties.html [ ImageOnlyFailure ]
-fast/text/orientation-sideways.html [ Failure ]
 fast/text/pua-charactersTreatedAsSpace.html [ ImageOnlyFailure ]
-fast/text/shadow-no-blur.html [ Failure ]
-fast/text/shadow-translucent-fill.html [ Failure ]
 fast/text/small-caps-web-font.html [ ImageOnlyFailure ]
 fast/text/small-line-height.html [ ImageOnlyFailure ]
 fast/text/soft-hyphen-as-first-breaking-opportunity.html [ ImageOnlyFailure ]
@@ -2006,28 +1963,21 @@ fast/text/system-font-fallback-emoji.html [ Failure ]
 fast/text/system-font-japanese-synthetic-italic.html [ ImageOnlyFailure ]
 fast/text/system-font-weight-italic.html [ Failure ]
 fast/text/system-font-weight.html [ Failure ]
-fast/text/tatechuyoko.html [ Failure ]
 fast/text/text-transform-turkish-and-azeri.html [ ImageOnlyFailure ]
 fast/text/trak-optimizeLegibility.html [ ImageOnlyFailure ]
 fast/text/updateNewFont.html [ Failure ]
 fast/text/user-installed-fonts/disable.html [ ImageOnlyFailure ]
 fast/text/user-installed-fonts/system-ui.html [ ImageOnlyFailure ]
 fast/text/wbr-pre.html [ Failure ]
-fast/text/whitespace/001.html [ Failure ]
-fast/text/whitespace/013.html [ Failure ]
-fast/text/whitespace/014.html [ Failure ]
 fast/text/whitespace/019.html [ Failure ]
-fast/text/whitespace/022.html [ Failure ]
 fast/text/whitespace/023.html [ Failure ]
 fast/text/whitespace/029.html [ Failure ]
-fast/text/whitespace/pre-newline-box-test.html [ Failure ]
 fast/text/whitespace/tab-character-basics.html [ Failure ]
 fast/text/word-space-between-inlines.html [ ImageOnlyFailure ]
 fast/text/zwj-ligature.html [ ImageOnlyFailure ]
 fast/url/data-url-mediatype.html [ Failure ]
 fast/visual-viewport/client-coordinates-relative-to-layout-viewport.html [ Failure ]
 fast/visual-viewport/rubberbanding-viewport-rects-extended-background.html [ Failure ]
-fonts/fantasy.html [ Failure ]
 inspector/cpu-profiler/threads.html [ Failure ]
 loader/reload-subresource-when-type-changes.html [ Failure ]
 loader/stateobjects/pushstate-size-iframe.html [ Skip ] # Timeout
@@ -2100,9 +2050,6 @@ fast/canvas/toDataURL-alpha-permutation.html [ ImageOnlyFailure ]
 
 fast/css/before-after-pseudo-element-case-insensitivity.html [ ImageOnlyFailure ]
 fast/dynamic/float-from-empty-line.html [ Failure ]
-fast/text/crash-complex-text-surrogate.html [ Failure ]
-
-webkit.org/b/188980 fast/css/vertical-align-block-elements.html [ ImageOnlyFailure ]
 
 imported/w3c/web-platform-tests/resource-timing/cross-origin-redirects.html [ Pass Failure ]
 imported/w3c/web-platform-tests/resource-timing/cross-origin-start-end-time-with-redirects.html [ Pass ]
@@ -2418,9 +2365,6 @@ css3/filters/backdrop/resource-use-excessive.html [ Failure ]
 css3/filters/backdrop/resource-use-ok.html [ Failure ]
 css3/filters/backdrop/resource-use-remove-some-layers.html [ Failure ]
 css3/filters/composited-during-animation-layertree.html [ Failure ]
-css3/filters/composited-during-transition-layertree.html [ Failure ]
-css3/filters/effect-brightness-clamping-hw.html [ Failure ]
-css3/filters/effect-brightness-clamping.html [ Failure ]
 css3/filters/backdrop/backdrop-filter-with-border-radius-and-reflection-add.html [ ImageOnlyFailure ]
 css3/filters/backdrop/backdrop-filter-with-border-radius-and-reflection.html [ ImageOnlyFailure ]
 css3/filters/reference-filter-color-space.html [ ImageOnlyFailure ]
@@ -2436,20 +2380,16 @@ webgl/2.0.y/conformance/extensions/webgl-multi-draw.html [ Skip ] # Timeout
 fast/canvas/webgl/colorspaces-test.html [ Failure Pass ]
 fast/canvas/webgl/context-lost-restored-p3.html [ Failure ]
 
-fast/css/child-style-can-override-visited-style.html [ Failure ]
 imported/w3c/web-platform-tests/cookies/size/attributes.www.sub.html [ Failure ]
 imported/w3c/web-platform-tests/cookies/size/name-and-value.html [ Failure ]
 
 fast/canvas/webgl/match-page-color-space-p3.html [ ImageOnlyFailure ]
 fast/css/pseudo-visited-background-color-on-input.html [ ImageOnlyFailure ]
-fast/filter-image/clipped-filter.html [ ImageOnlyFailure ]
 fast/text/initial-advance-selected-text.html [ ImageOnlyFailure ]
 
 editing/selection/user-select-all-with-shift-live-range.html [ Failure ]
 
 fast/dom/vertical-scrollbar-in-rtl.html [ Failure ]
-transforms/3d/point-mapping/3d-point-mapping-2.html [ Failure ]
-fast/css/focus-ring-continuations.html [ Failure ]
 
 editing/selection/shift-click-live-range.html [ Failure ]
 
@@ -2510,12 +2450,10 @@ streams/pipe-to.html [ Failure ]
 animations/stacking-context-not-fill-forwards.html [ ImageOnlyFailure ]
 fast/box-shadow/inset-box-shadow.html [ ImageOnlyFailure ]
 fast/canvas/webgl/move-canvas-in-document-while-clean.html [ ImageOnlyFailure ]
-fast/css-generated-content/initial-letter-pagination-raised-rl.html [ ImageOnlyFailure ]
 fast/css-generated-content/initial-letter-pagination-sunken-rl.html [ Skip ] # ImageOnlyFailure
 fast/css-generated-content/initial-letter-pagination-sunken.html [ ImageOnlyFailure ]
 fast/css/sticky/sticky-top-zoomed.html [ ImageOnlyFailure ]
 fast/text/international/bdo-bidi-width.html [ ImageOnlyFailure ]
-fast/text/simple-line-layout-fallback-space-glyph.html [ ImageOnlyFailure ]
 fast/text/simple-line-wordspacing.html [ ImageOnlyFailure ]
 fast/text/user-installed-canvas.html [ ImageOnlyFailure ]
 
@@ -2602,16 +2540,9 @@ webkit.org/b/177687 http/tests/inspector/network/resource-sizes-memory-cache.htm
 webkit.org/b/157592 fast/images/composited-animated-gif-outside-viewport.html [ Pass Timeout ]
 webkit.org/b/230504 css3/masking/reference-clip-path-animate-transform-repaint.html [ Pass Failure ]
 
-fast/css/caret-color-fallback-to-color.html [ ImageOnlyFailure ]
-fast/css/caret-color-inherit.html [ ImageOnlyFailure ]
-fast/css/caret-color-span-inside-editable-parent.html [ ImageOnlyFailure ]
-fast/css/caret-color.html [ ImageOnlyFailure ]
-
 # ImageOnlyFailure only on Buildbot
 fast/forms/placeholder-with-positioned-element.html [ ImageOnlyFailure Pass ]
 fast/forms/input-placeholder-text-indent.html [ ImageOnlyFailure Pass ]
-
-fast/forms/single-text-line-content-clipping.html [ ImageOnlyFailure ]
 
 # ImageOnlyFailure only on local PC. Depends on the graphis driver?
 compositing/overlap-blending/reflection-opacity-huge.html [ ImageOnlyFailure Pass ]
@@ -2638,3 +2569,13 @@ fast/canvas/image-buffer-backend-variants.html [ Skip ]
 webkit.org/b/238227 fast/dom/intersection-observer-document-leak.html [ Pass Failure ]
 
 webkit.org/b/262687 [ Release ] fast/workers/pending-requestAnimationFrame-upon-destruction.html [ Pass Failure ]
+
+fast/box-shadow/box-shadow-radius.html [ Failure ]
+fast/box-shadow/scaled-box-shadow.html [ Failure ]
+fast/css-generated-content/initial-letter-pagination-raised.html [ ImageOnlyFailure ]
+fast/reflections/reflection-masks-opacity.html [ Failure ]
+fast/reflections/reflection-masks.html [ Failure ]
+fast/text/ja-sans-serif.html [ ImageOnlyFailure ]
+fast/text/simple-line-hyphens-with-word-letter-spacing.html [ ImageOnlyFailure ]
+fast/text/softHyphen.html [ ImageOnlyFailure ]
+fast/text/synthetic-oblique.html [ ImageOnlyFailure ]

LayoutTests/platform/wincairo/fast/text/whitespace/nowrap-clear-float-expected.txt
@@ -5,7 +5,7 @@ layer at (0,0) size 800x600
     RenderBody {BODY} at (8,8) size 784x584
       RenderBlock {DIV} at (0,0) size 200x120
         RenderBlock (floating) {DIV} at (0,0) size 100x100 [bgcolor=#ADD8E6]
-        RenderInline {SPAN} at (0,0) size 253x19
-          RenderText {#text} at (0,100) size 253x19
-            text run at (0,100) width 253: "This text should be below the blue square."
+        RenderInline {SPAN} at (0,0) size 266x17
+          RenderText {#text} at (100,0) size 266x17
+            text run at (100,0) width 266: "This text should be below the blue square."
         RenderText {#text} at (0,0) size 0x0

Source/WebCore/platform/graphics/FontPlatformData.h
@@ -271,7 +271,7 @@ private:
 #endif
 
 #if PLATFORM(WIN)
-    void platformDataInit(HFONT, float size, HDC, WCHAR* faceName);
+    void platformDataInit(HFONT, float size, WCHAR* faceName);
 #endif
 
 #if USE(FREETYPE)

Source/WebCore/platform/graphics/win/FontPlatformDataCairoWin.cpp
@@ -39,7 +39,7 @@
 
 namespace WebCore {
 
-void FontPlatformData::platformDataInit(HFONT font, float size, HDC, WCHAR* faceName)
+void FontPlatformData::platformDataInit(HFONT font, float size, WCHAR* faceName)
 {
     cairo_font_face_t* fontFace = cairo_win32_font_face_create_for_hfont(font);
 
@@ -112,12 +112,10 @@ String FontPlatformData::description() const
 String FontPlatformData::familyName() const
 {
     HWndDC hdc(0);
-    SaveDC(hdc);
-    cairo_win32_scaled_font_select_font(scaledFont(), hdc);
+    HGDIOBJ oldFont = SelectObject(hdc, m_font.get());
     wchar_t faceName[LF_FACESIZE];
     GetTextFace(hdc, LF_FACESIZE, faceName);
-    cairo_win32_scaled_font_done_font(scaledFont());
-    RestoreDC(hdc, -1);
+    SelectObject(hdc, oldFont);
     return faceName;
 }
 

Source/WebCore/platform/graphics/win/FontPlatformDataWin.cpp
@@ -34,8 +34,6 @@
 #include <wtf/text/StringHash.h>
 #include <wtf/text/WTFString.h>
 
-using std::min;
-
 namespace WebCore {
 
 FontPlatformData::FontPlatformData(GDIObject<HFONT> font, float size, bool bold, bool oblique, const FontCustomPlatformData* customPlatformData)
@@ -50,7 +48,7 @@ FontPlatformData::FontPlatformData(GDIObject<HFONT> font, float size, bool bold,
 
     wchar_t faceName[LF_FACESIZE];
     GetTextFace(hdc, LF_FACESIZE, faceName);
-    platformDataInit(m_font->get(), size, hdc, faceName);
+    platformDataInit(m_font->get(), size, faceName);
 
     RestoreDC(hdc, -1);
 }

commit 15855ea0a70708ea758ab7917dafe852e1fbd711

REGRESSION(269016@main) [Win] fast/forms/listbox-zero-item-height.html is randomly failing https://bugs.webkit.org/show_bug.cgi?id=263101
Reviewed by Ross Kirsling.

The following tests were randomly failing after 269016@main..

  fast/css3-text/css3-text-decoration/text-decoration-skip/text-decoration-skip-ink-svg.html
  fast/css3-text/css3-text-decoration/text-decoration-thickness.html
  fast/forms/listbox-zero-item-height.html

Uninitialized OUTLINETEXTMETRIC was used if GetOutlineTextMetrics
failed. Check the return value of GetOutlineTextMetrics.

* LayoutTests/platform/wincairo/TestExpectations:
* Source/WebCore/platform/graphics/win/SimpleFontDataCairoWin.cpp:

Canonical link: https://commits.webkit.org/269354@main

Source/WebCore/platform/graphics/win/SimpleFontDataCairoWin.cpp
@@ -62,7 +62,8 @@ void Font::platformInit()
     GetTextFace(dc, LF_FACESIZE, faceName);
 
     OUTLINETEXTMETRIC metrics;
-    GetOutlineTextMetrics(dc, sizeof(metrics), &metrics);
+    if (!GetOutlineTextMetrics(dc, sizeof(metrics), &metrics))
+        return;
 
     float xHeight = metrics.otmTextMetrics.tmAscent * 0.56f; // Best guess for xHeight if no x glyph is present.
     GLYPHMETRICS gm;

LayoutTests/platform/wincairo/TestExpectations
@@ -2564,9 +2564,8 @@ animations/no-style-recalc-during-accelerated-animation.html [ Pass Failure ]
 
 webkit.org/b/133151 js/cached-window-properties.html [ Skip ] # Slow
 
-webkit.org/b/263101 fast/css3-text/css3-text-decoration/text-decoration-skip/text-decoration-skip-ink-svg.html [ ImageOnlyFailure Pass ]
-webkit.org/b/263101 fast/css3-text/css3-text-decoration/text-decoration-thickness.html [ ImageOnlyFailure Pass ]
-webkit.org/b/263101 fast/forms/listbox-zero-item-height.html [ Failure Pass ]
+fast/css3-text/css3-text-decoration/text-decoration-thickness.html [ ImageOnlyFailure ]
+fast/forms/listbox-zero-item-height.html [ Failure ]
 
 fast/css/unicode-escape-in-8bit-string.html [ ImageOnlyFailure ]
 fast/css/word-spacing-characters.html [ ImageOnlyFailure ]