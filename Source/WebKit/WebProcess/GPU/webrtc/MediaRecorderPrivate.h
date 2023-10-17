3fc2d8699c3d3a249a8cbf2d21d2b70714317c99
Dan Robson <dtr_bugzilla@apple.com>
2023-10-17 15:30:02 -0700

    Cherry-pick 5ac998d7f35a. rdar://116726041
        WebKit MediaRecorderPrivate should not be refcounted
        https://bugs.webkit.org/show_bug.cgi?id=262945
        rdar://problem/116726041
    
        Reviewed by Alex Christensen and Chris Dumez.
    
        Given MediaRecorderPrivate is stored as a std::unique_ptr, it is best to not ref/deref.
        Instead MediaRecorderPrivate stores a ref counted object that is observing GPUProcess connection closure.
    
        * Source/WebKit/WebProcess/GPU/webrtc/MediaRecorderPrivate.cpp:
        (WebKit::MediaRecorderPrivate::MediaRecorderPrivate):
        (WebKit::MediaRecorderPrivate::startRecording):
        (WebKit::MediaRecorderPrivate::gpuProcessConnectionDidClose):
        * Source/WebKit/WebProcess/GPU/webrtc/MediaRecorderPrivate.h:
    
        Canonical link: https://commits.webkit.org/269195@main
    
    Identifier: 267815.329@safari-7617-branch

--- Source/WebKit/WebProcess/GPU/webrtc/MediaRecorderPrivate.h
+++ Source/WebKit/WebProcess/GPU/webrtc/MediaRecorderPrivate.h
@@ -49,15 +49,11 @@ namespace WebKit {
 
 class MediaRecorderPrivate final
     : public WebCore::MediaRecorderPrivate
-    , public GPUProcessConnection::Client {
+    , public CanMakeWeakPtr<MediaRecorderPrivate> {
     WTF_MAKE_FAST_ALLOCATED;
 public:
     static Ref<MediaRecorderPrivate> create(WebCore::MediaStreamPrivate&, const WebCore::MediaRecorderPrivateOptions&);
     ~MediaRecorderPrivate();
-
-    void ref() const final { return WebCore::MediaRecorderPrivate::ref(); }
-    void deref() const final { return WebCore::MediaRecorderPrivate::deref(); }
-    ThreadSafeWeakPtrControlBlock& controlBlock() const final { return WebCore::MediaRecorderPrivate::controlBlock(); }
 
 private:
     MediaRecorderPrivate(WebCore::MediaStreamPrivate&, const WebCore::MediaRecorderPrivateOptions&);
@@ -72,8 +68,35 @@ private:
     void pauseRecording(CompletionHandler<void()>&&) final;
     void resumeRecording(CompletionHandler<void()>&&) final;
 
-    // GPUProcessConnection::Client
-    void gpuProcessConnectionDidClose(GPUProcessConnection&) final;
+    void gpuProcessConnectionDidClose();
+
+    class GPUProcessDidCloseObserver final
+        : public GPUProcessConnection::Client
+        , public ThreadSafeRefCountedAndCanMakeThreadSafeWeakPtr<GPUProcessDidCloseObserver> {
+    public:
+        static Ref<GPUProcessDidCloseObserver> create(MediaRecorderPrivate& recorder) { return adoptRef(*new GPUProcessDidCloseObserver(recorder)); }
+
+        // GPUProcessConnection::Client
+        void ref() const final { return ThreadSafeRefCountedAndCanMakeThreadSafeWeakPtr<GPUProcessDidCloseObserver>::ref(); }
+        void deref() const final { return ThreadSafeRefCountedAndCanMakeThreadSafeWeakPtr<GPUProcessDidCloseObserver>::deref(); }
+        ThreadSafeWeakPtrControlBlock& controlBlock() const final { return ThreadSafeRefCountedAndCanMakeThreadSafeWeakPtr<GPUProcessDidCloseObserver>::controlBlock(); }
+
+    private:
+        explicit GPUProcessDidCloseObserver(MediaRecorderPrivate& recorder)
+            : m_recorder(recorder)
+        {
+        }
+
+        void gpuProcessConnectionDidClose(GPUProcessConnection&) final
+        {
+            callOnMainRunLoop([recorder = m_recorder] {
+                if (recorder)
+                    recorder->gpuProcessConnectionDidClose();
+            });
+        }
+
+        WeakPtr<MediaRecorderPrivate> m_recorder;
+    };
 
     MediaRecorderIdentifier m_identifier;
     Ref<WebCore::MediaStreamPrivate> m_stream;
@@ -88,6 +111,7 @@ private:
     bool m_isStopped { false };
     std::optional<WebCore::IntSize> m_blackFrameSize;
 
+    Ref<GPUProcessDidCloseObserver> m_gpuProcessDidCloseObserver;
     SharedVideoFrameWriter m_sharedVideoFrameWriter;
 };
 
