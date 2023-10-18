WebKit/WebKit

commit c42aaec46c580f52c3a101b51f011a605101fbc8

Leverage fast HTML parser in DOMParser::parseFromString() https://bugs.webkit.org/show_bug.cgi?id=260076 rdar://113738031
Reviewed by Ryosuke Niwa.

Leverage the fast HTML parser in DOMParser::parseFromString(), like we already
do for HTMLElement::setInnerHTML().

* Source/WebCore/html/parser/HTMLDocumentParserFastPath.cpp:
* Source/WebCore/xml/DOMParser.cpp:
(WebCore::DOMParser::parseFromString):

Canonical link: https://commits.webkit.org/267202@main

Source/WebCore/html/parser/HTMLDocumentParserFastPath.cpp
@@ -39,6 +39,7 @@
 #include "FragmentScriptingPermission.h"
 #include "HTMLAnchorElement.h"
 #include "HTMLBRElement.h"
+#include "HTMLBodyElement.h"
 #include "HTMLButtonElement.h"
 #include "HTMLDivElement.h"
 #include "HTMLEntityParser.h"
@@ -134,6 +135,7 @@ template<typename T> static bool insertInUniquedSortedVector(Vector<T>& vector, 
 #define FOR_EACH_SUPPORTED_TAG(APPLY) \
     APPLY(a, A)                       \
     APPLY(b, B)                       \
+    APPLY(body, Body)                 \
     APPLY(br, Br)                     \
     APPLY(button, Button)             \
     APPLY(div, Div)                   \
@@ -349,6 +351,11 @@ private:
         struct Div : ContainerTag<HTMLDivElement, PermittedParents::FlowContent> {
             static constexpr ElementName tagName = ElementNames::HTML::div;
             static constexpr CharacterType tagNameCharacters[] = { 'd', 'i', 'v' };
+        };
+
+        struct Body : ContainerTag<HTMLBodyElement, PermittedParents::Special> {
+            static constexpr ElementName tagName = ElementNames::HTML::body;
+            static constexpr CharacterType tagNameCharacters[] = { 'b', 'o', 'd', 'y' };
         };
 
         struct Footer : ContainerTag<HTMLElement, PermittedParents::FlowContent> {

Source/WebCore/xml/DOMParser.cpp
@@ -20,7 +20,12 @@
 #include "DOMParser.h"
 
 #include "DOMImplementation.h"
+#include "DocumentFragment.h"
 #include "FragmentScriptingPermission.h"
+#include "HTMLBodyElement.h"
+#include "HTMLDocumentParserFastPath.h"
+#include "HTMLHeadElement.h"
+#include "HTMLHtmlElement.h"
 #include "SecurityOriginPolicy.h"
 #include "Settings.h"
 
@@ -46,11 +51,30 @@ ExceptionOr<Ref<Document>> DOMParser::parseFromString(const String& string, cons
     auto document = DOMImplementation::createDocument(contentType, nullptr, m_settings, URL { });
     if (m_contextDocument)
         document->setContextDocument(*m_contextDocument.get());
+    OptionSet<ParserContentPolicy> parsingOptions;
     if (options.includeShadowRoots && document->settings().declarativeShadowDOMInDOMParserEnabled())
-        document->setParserContentPolicy({ ParserContentPolicy::AllowScriptingContent, ParserContentPolicy::AllowPluginContent, ParserContentPolicy::AllowDeclarativeShadowDOM });
+        parsingOptions = { ParserContentPolicy::AllowScriptingContent, ParserContentPolicy::AllowPluginContent, ParserContentPolicy::AllowDeclarativeShadowDOM };
     else
-        document->setParserContentPolicy({ ParserContentPolicy::AllowScriptingContent, ParserContentPolicy::AllowPluginContent });
-    document->setContent(string);
+        parsingOptions = { ParserContentPolicy::AllowScriptingContent, ParserContentPolicy::AllowPluginContent };
+    document->setParserContentPolicy(parsingOptions);
+    bool usedFastPath = false;
+    if (contentType == "text/html"_s) {
+        auto fragment = DocumentFragment::create(document);
+        auto body = HTMLBodyElement::create(document);
+        usedFastPath = tryFastParsingHTMLFragment(string, document, fragment, body, parsingOptions);
+        if (LIKELY(usedFastPath)) {
+            auto html = HTMLHtmlElement::create(document);
+            document->appendChild(html);
+            auto head = HTMLHeadElement::create(document);
+            html->appendChild(head);
+            html->appendChild(body);
+            auto result = body->appendChild(fragment);
+            if (UNLIKELY(result.hasException()))
+                return result.releaseException();
+        }
+    }
+    if (!usedFastPath)
+        document->setContent(string);
     if (m_contextDocument) {
         document->setURL(m_contextDocument->url());
         document->setSecurityOriginPolicy(m_contextDocument->securityOriginPolicy());

commit 55a286438b239e94c5a355a1f7510cfa7b4dd033

REGRESSION (STP): Video won't play on bilibili.com https://bugs.webkit.org/show_bug.cgi?id=263196 rdar://117020123
Reviewed by Chris Dumez.

bilibili injects DOM objects in the tree by calling:
```
var t = (new DOMParser).parseFromString(e, "text/html");
return document.adoptNode(t.body.firstChild)
```

Per spec, when parsing HTML to create a document through DOMParser::parseFromString() [1]
you are to skip the whitespace tokens for the "initial" insertion mode [2].
The trailing spaces however must be kept [3]
This behaviour was regressed by 267202@main.

On this site, the observable behaviour was that the firstChild became a "#text"
node rather the expected first DIV one.

So we trim the leading whitespaces before calling the fast parser. Which
restore the original behaviour prior 267202@main

[1] https://html.spec.whatwg.org/multipage/dynamic-markup-insertion.html#parse-html-from-a-string
[2] https://html.spec.whatwg.org/multipage/parsing.html#the-initial-insertion-mode
[3] https://html.spec.whatwg.org/multipage/parsing.html#parsing-main-afterbody

* LayoutTests/fast/dom/document-contentType-DOMParser-expected.txt:
* LayoutTests/fast/dom/document-contentType-DOMParser.html: Added tests.
* Source/WebCore/html/parser/HTMLDocumentParserFastPath.cpp:
(WebCore::tryFastParsingHTMLFragment):
* Source/WebCore/html/parser/HTMLDocumentParserFastPath.h: Change type to StringView
(WebCore::requires):
* Source/WebCore/xml/DOMParser.cpp:
(WebCore::DOMParser::parseFromString):

Canonical link: https://commits.webkit.org/269457@main

Source/WebCore/html/parser/HTMLDocumentParserFastPath.cpp
@@ -895,7 +895,7 @@ static bool tryFastParsingHTMLFragmentImpl(const std::span<const CharacterType>&
     return parser.parse(contextElement);
 }
 
-bool tryFastParsingHTMLFragment(const String& source, Document& document, ContainerNode& destinationParent, Element& contextElement, OptionSet<ParserContentPolicy> policy)
+bool tryFastParsingHTMLFragment(StringView source, Document& document, ContainerNode& destinationParent, Element& contextElement, OptionSet<ParserContentPolicy> policy)
 {
     if (!canUseFastPath(contextElement, policy))
         return false;

Source/WebCore/html/parser/HTMLDocumentParserFastPath.h
@@ -41,7 +41,7 @@ class Element;
 
 enum class ParserContentPolicy : uint8_t;
 
-WEBCORE_EXPORT bool tryFastParsingHTMLFragment(const String& source, Document&, ContainerNode&, Element& contextElement, OptionSet<ParserContentPolicy>);
+WEBCORE_EXPORT bool tryFastParsingHTMLFragment(StringView source, Document&, ContainerNode&, Element& contextElement, OptionSet<ParserContentPolicy>);
 
 } // namespace WebCore
 

Source/WebCore/xml/DOMParser.cpp
@@ -60,7 +60,7 @@ ExceptionOr<Ref<Document>> DOMParser::parseFromString(const String& string, cons
     bool usedFastPath = false;
     if (contentType == "text/html"_s) {
         auto body = HTMLBodyElement::create(document);
-        usedFastPath = tryFastParsingHTMLFragment(string, document, body, body, parsingOptions);
+        usedFastPath = tryFastParsingHTMLFragment(StringView { string }.substring(string.find(isNotASCIIWhitespace<UChar>)), document, body, body, parsingOptions);
         if (LIKELY(usedFastPath)) {
             auto html = HTMLHtmlElement::create(document);
             document->appendChild(html);

LayoutTests/fast/dom/document-contentType-DOMParser-expected.txt
@@ -6,6 +6,13 @@ PASS new DOMParser().parseFromString(xslContent, "text/xsl").contentType threw e
 PASS new DOMParser().parseFromString(xmlContent, "text/dummy+xml").contentType threw exception TypeError: Type error.
 PASS new DOMParser().parseFromString(xmlContent, "text/XML").contentType threw exception TypeError: Type error.
 PASS new DOMParser().parseFromString(htmlContent, "TEXT/html").contentType threw exception TypeError: Type error.
+PASS parsedContent.body.firstChild.nodeName is "DIV"
+PASS parsedContent.body.childNodes.length is 2
+PASS parsedContent.body.childNodes[1].nodeName is "#text"
+PASS div.firstChild.nodeName is "#text"
+PASS div.childNodes.length is 3
+PASS div.childNodes[2].nodeName is "#text"
+PASS new DOMParser().parseFromString(htmlContentWithJustSpaces, "text/html").body.childNodes.length is 0
 PASS successfullyParsed is true
 
 TEST COMPLETE

LayoutTests/fast/dom/document-contentType-DOMParser.html
@@ -6,7 +6,7 @@
 </head>
 <body>
  <script>
-  
+
 var htmlContent =
                 "<html>" +
                     "<head>" +
@@ -79,6 +79,21 @@ shouldThrow('new DOMParser().parseFromString(xslContent, "text/xsl").contentType
 shouldThrow('new DOMParser().parseFromString(xmlContent, "text/dummy+xml").contentType', "'TypeError: Type error'");
 shouldThrow('new DOMParser().parseFromString(xmlContent, "text/XML").contentType', "'TypeError: Type error'");
 shouldThrow('new DOMParser().parseFromString(htmlContent, "TEXT/html").contentType', "'TypeError: Type error'");
+
+var htmlContentWithSpace = "\n        <div class=\"bpx-player-container\"></div>        \n";
+var parsedContent = new DOMParser().parseFromString(htmlContentWithSpace, "text/html");
+shouldBeEqualToString('parsedContent.body.firstChild.nodeName', 'DIV');
+shouldBe('parsedContent.body.childNodes.length', '2');
+shouldBeEqualToString('parsedContent.body.childNodes[1].nodeName', '#text');
+
+var div = document.createElement("div");
+div.innerHTML = htmlContentWithSpace;
+shouldBeEqualToString('div.firstChild.nodeName', '#text');
+shouldBe('div.childNodes.length', '3');
+shouldBeEqualToString('div.childNodes[2].nodeName', '#text');
+
+var htmlContentWithJustSpaces = "\n                \n";
+shouldBe('new DOMParser().parseFromString(htmlContentWithJustSpaces, "text/html").body.childNodes.length', '0');
 
 </script>
 <script src="../../resources/js-test-post.js"></script>