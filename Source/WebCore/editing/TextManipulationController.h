8c054af26f6d67afca68d83e3708a8e575635c2f
Commit Queue <commit-queue@webkit.org>
2023-10-23 12:34:48 -0700

    Unreviewed, reverting 269344@main. https://bugs.webkit.org/show_bug.cgi?id=263545
    Caused a Speedometer 2 regression
    
    Reverted changeset:
    
    "Adopt more smart pointers in Node.cpp"
    https://bugs.webkit.org/show_bug.cgi?id=263107
    https://commits.webkit.org/269344@main
    
    Canonical link: https://commits.webkit.org/269661@main

--- Source/WebCore/editing/TextManipulationController.h
+++ Source/WebCore/editing/TextManipulationController.h
@@ -30,7 +30,6 @@
 #include "TextManipulationControllerExclusionRule.h"
 #include "TextManipulationControllerManipulationFailure.h"
 #include "TextManipulationItem.h"
-#include <wtf/CheckedRef.h>
 #include <wtf/CompletionHandler.h>
 #include <wtf/EnumTraits.h>
 #include <wtf/ObjectIdentifier.h>
@@ -43,7 +42,7 @@ class Document;
 class Element;
 class VisiblePosition;
 
-class TextManipulationController : public CanMakeWeakPtr<TextManipulationController>, public CanMakeCheckedPtr {
+class TextManipulationController : public CanMakeWeakPtr<TextManipulationController> {
     WTF_MAKE_FAST_ALLOCATED;
 public:
     TextManipulationController(Document&);
