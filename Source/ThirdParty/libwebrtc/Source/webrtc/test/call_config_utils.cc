6da809348ebef718d9d6bd9ae835afc5a482be2a
David Kilzer <ddkilzer@apple.com>
2023-10-31 10:27:31 -0700

    [WebRTC] Add targets for vp8 and vp9 replay fuzzers https://bugs.webkit.org/show_bug.cgi?id=263912 <rdar://116139782>
    Reviewed by Youenn Fablet.
    
    * Source/ThirdParty/libwebrtc/Configurations/vp8_dec_fuzzer.xcconfig:
    * Source/ThirdParty/libwebrtc/Configurations/vp9_dec_fuzzer.xcconfig:
    - Drive-by fix to add license headers.
    * Source/ThirdParty/libwebrtc/Configurations/vp8_replay_fuzzer.xcconfig: Add.
    * Source/ThirdParty/libwebrtc/Configurations/vp9_replay_fuzzer.xcconfig: Add.
    - Add xcconfig files for each fuzzer.
    
    * Source/ThirdParty/libwebrtc/Source/third_party/json/include/json/json.h: Add.
    (Json::CharReader::parse):
    (Json::CharReaderBuilder::newCharReader):
    - Temporary workaround for jsoncpp project.
    * Source/ThirdParty/libwebrtc/Source/webrtc/test/call_config_utils.cc:
    - Temporary workaround for jsoncpp project.
    
    * Source/ThirdParty/libwebrtc/Source/webrtc/test/mac/video_renderer_mac.mm:
    (-[CocoaWindow dealloc]): Add.
    (-[CocoaWindow createWindow:]):
    (-[CocoaWindow makeCurrentContext]):
    (webrtc::test::MacRenderer::~MacRenderer):
    (webrtc::test::MacRenderer::Init):
    (webrtc::test::MacRenderer::OnFrame):
    - Fix leaks caused by autoreleased objects getting added to an
      autoreleasePool of last resort and by failing to tear down objects in
      the -[CocoaWindow dealloc] method.
    
    * Source/ThirdParty/libwebrtc/WebKit/0001-WebRTC-Add-targets-for-vp8-and-vp9-replay-fuzzers.patch: Add.
    
    * Source/ThirdParty/libwebrtc/libwebrtc.xcodeproj/project.pbxproj:
    - Add missing sources to the project for the vp{8,9}_replay_fuzzer
      targets.
    - Add vp{8,9}_replay_fuzzer targets.
    
    Canonical link: https://commits.webkit.org/270011@main

--- Source/ThirdParty/libwebrtc/Source/webrtc/test/call_config_utils.cc
+++ Source/ThirdParty/libwebrtc/Source/webrtc/test/call_config_utils.cc
@@ -25,17 +25,50 @@ VideoReceiveStreamInterface::Config ParseVideoReceiveStreamJsonConfig(
   auto receive_config = VideoReceiveStreamInterface::Config(transport);
   for (const auto& decoder_json : json["decoders"]) {
     VideoReceiveStreamInterface::Decoder decoder;
+#if WEBRTC_WEBKIT_BUILD
+    decoder.video_format =
+        SdpVideoFormat(decoder_json["payload_name"].get<std::string>());
+    decoder.payload_type = decoder_json["payload_type"].get<int32_t>();
+#else
     decoder.video_format =
         SdpVideoFormat(decoder_json["payload_name"].asString());
     decoder.payload_type = decoder_json["payload_type"].asInt64();
+#endif
     for (const auto& params_json : decoder_json["codec_params"]) {
+#if WEBRTC_WEBKIT_BUILD
+      std::vector<std::string> members(params_json.size());
+      for (const auto& [key, value] : params_json.items()) {
+        members.emplace_back(key);
+      }
+      RTC_CHECK_EQ(members.size(), 1);
+      decoder.video_format.parameters[members[0]] =
+          params_json[members[0]].get<std::string>();
+#else
       std::vector<std::string> members = params_json.getMemberNames();
       RTC_CHECK_EQ(members.size(), 1);
       decoder.video_format.parameters[members[0]] =
           params_json[members[0]].asString();
+#endif
     }
     receive_config.decoders.push_back(decoder);
   }
+#if WEBRTC_WEBKIT_BUILD
+  receive_config.render_delay_ms = json["render_delay_ms"].get<int32_t>();
+  receive_config.rtp.remote_ssrc = json["rtp"]["remote_ssrc"].get<uint32_t>();
+  receive_config.rtp.local_ssrc = json["rtp"]["local_ssrc"].get<uint32_t>();
+  receive_config.rtp.rtcp_mode =
+      json["rtp"]["rtcp_mode"].get<std::string>() == "RtcpMode::kCompound"
+          ? RtcpMode::kCompound
+          : RtcpMode::kReducedSize;
+  receive_config.rtp.lntf.enabled = json["rtp"]["lntf"]["enabled"].get<bool>();
+  receive_config.rtp.nack.rtp_history_ms =
+      json["rtp"]["nack"]["rtp_history_ms"].get<int32_t>();
+  receive_config.rtp.ulpfec_payload_type =
+      json["rtp"]["ulpfec_payload_type"].get<int32_t>();
+  receive_config.rtp.red_payload_type =
+      json["rtp"]["red_payload_type"].get<int32_t>();
+  receive_config.rtp.rtx_ssrc = json["rtp"]["rtx_ssrc"].get<uint32_t>();
+#else
   receive_config.render_delay_ms = json["render_delay_ms"].asInt64();
   receive_config.rtp.remote_ssrc = json["rtp"]["remote_ssrc"].asInt64();
   receive_config.rtp.local_ssrc = json["rtp"]["local_ssrc"].asInt64();
@@ -51,13 +84,25 @@ VideoReceiveStreamInterface::Config ParseVideoReceiveStreamJsonConfig(
   receive_config.rtp.red_payload_type =
       json["rtp"]["red_payload_type"].asInt64();
   receive_config.rtp.rtx_ssrc = json["rtp"]["rtx_ssrc"].asInt64();
+#endif
 
   for (const auto& pl_json : json["rtp"]["rtx_payload_types"]) {
+#if WEBRTC_WEBKIT_BUILD
+    std::vector<std::string> members(pl_json.size());
+    for (const auto& [key, value] : pl_json.items()) {
+        members.emplace_back(key);
+    }
+    RTC_CHECK_EQ(members.size(), 1);
+    Json::Value rtx_payload_type = pl_json[members[0]];
+    receive_config.rtp.rtx_associated_payload_types[std::stoi(members[0])] =
+        rtx_payload_type.get<int32_t>();
+#else
     std::vector<std::string> members = pl_json.getMemberNames();
     RTC_CHECK_EQ(members.size(), 1);
     Json::Value rtx_payload_type = pl_json[members[0]];
     receive_config.rtp.rtx_associated_payload_types[std::stoi(members[0])] =
         rtx_payload_type.asInt64();
+#endif
   }
   return receive_config;
 }
@@ -66,18 +111,34 @@ Json::Value GenerateVideoReceiveStreamJsonConfig(
     const VideoReceiveStreamInterface::Config& config) {
   Json::Value root_json;
 
+#if WEBRTC_WEBKIT_BUILD
+  root_json["decoders"] = Json::Value("[]");
+#else
   root_json["decoders"] = Json::Value(Json::arrayValue);
+#endif
   for (const auto& decoder : config.decoders) {
     Json::Value decoder_json;
     decoder_json["payload_type"] = decoder.payload_type;
     decoder_json["payload_name"] = decoder.video_format.name;
+#if WEBRTC_WEBKIT_BUILD
+    decoder_json["codec_params"] = Json::Value("[]");
+#else
     decoder_json["codec_params"] = Json::Value(Json::arrayValue);
+#endif
     for (const auto& codec_param_entry : decoder.video_format.parameters) {
       Json::Value codec_param_json;
       codec_param_json[codec_param_entry.first] = codec_param_entry.second;
+#if WEBRTC_WEBKIT_BUILD
+      decoder_json["codec_params"].push_back(codec_param_json);
+#else
       decoder_json["codec_params"].append(codec_param_json);
+#endif
     }
+#if WEBRTC_WEBKIT_BUILD
+    root_json["decoders"].push_back(decoder_json);
+#else
     root_json["decoders"].append(decoder_json);
+#endif
   }
 
   Json::Value rtp_json;
@@ -91,12 +152,20 @@ Json::Value GenerateVideoReceiveStreamJsonConfig(
   rtp_json["ulpfec_payload_type"] = config.rtp.ulpfec_payload_type;
   rtp_json["red_payload_type"] = config.rtp.red_payload_type;
   rtp_json["rtx_ssrc"] = config.rtp.rtx_ssrc;
+#if WEBRTC_WEBKIT_BUILD
+  rtp_json["rtx_payload_types"] = Json::Value("[]");
+#else
   rtp_json["rtx_payload_types"] = Json::Value(Json::arrayValue);
+#endif
 
   for (auto& kv : config.rtp.rtx_associated_payload_types) {
     Json::Value val;
     val[std::to_string(kv.first)] = kv.second;
+#if WEBRTC_WEBKIT_BUILD
+    rtp_json["rtx_payload_types"].push_back(val);
+#else
     rtp_json["rtx_payload_types"].append(val);
+#endif
   }
 
   root_json["rtp"] = rtp_json;
