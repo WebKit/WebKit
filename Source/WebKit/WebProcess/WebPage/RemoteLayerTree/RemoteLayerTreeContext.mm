e0e30a6cf70ce1b26ed4ecada49ec6f2175f47ea
Simon Fraser <simon.fraser@apple.com>
2023-11-01 15:00:49 -0700

    REGRESSION (Sonoma): [ Sonoma wk2 Release ] compositing/reflections/repaint-with-reflection.html is flaky https://bugs.webkit.org/show_bug.cgi?id=263347 rdar://117172843
    Reviewed by Tim Horton.
    
    Increase the scope of having a non-null `m_currentTransaction` in `RemoteLayerTreeContext::buildTransaction()`,
    since `paintReachableBackingStoreContents()` can call `PlatformCALayerRemote::copyContentsFromLayer()` for
    layers with reflections, and we need `RemoteLayerTreeContext::layerPropertyChangedWhileBuildingTransaction()`
    to see `m_currentTransaction` so we mark the clone as having changed properties.
    
    * LayoutTests/platform/mac-wk2/TestExpectations:
    * Source/WebKit/WebProcess/WebPage/RemoteLayerTree/RemoteLayerTreeContext.mm:
    (WebKit::RemoteLayerTreeContext::buildTransaction):
    
    Canonical link: https://commits.webkit.org/270082@main

--- Source/WebKit/WebProcess/WebPage/RemoteLayerTree/RemoteLayerTreeContext.mm
+++ Source/WebKit/WebProcess/WebPage/RemoteLayerTree/RemoteLayerTreeContext.mm
@@ -197,11 +197,12 @@ void RemoteLayerTreeContext::buildTransaction(RemoteLayerTreeTransaction& transa
     m_currentTransaction = &transaction;
     rootLayerRemote.recursiveBuildTransaction(*this, transaction);
     m_backingStoreCollection->prepareBackingStoresForDisplay(transaction);
-    m_currentTransaction = nullptr;
 
     bool paintedAnyBackingStore = m_backingStoreCollection->paintReachableBackingStoreContents();
     if (paintedAnyBackingStore)
         m_nextRenderingUpdateRequiresSynchronousImageDecoding = false;
+
+    m_currentTransaction = nullptr;
 
     transaction.setCreatedLayers(moveToVector(std::exchange(m_createdLayers, { }).values()));
     transaction.setDestroyedLayerIDs(WTFMove(m_destroyedLayers));
