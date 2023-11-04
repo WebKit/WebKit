3d732906995061dc7c611017fbc04e3dd344ede5
David Kilzer <ddkilzer@apple.com>
2023-11-04 03:25:42 -0700

    Build static library for JavaScriptCore before linking framework https://bugs.webkit.org/show_bug.cgi?id=263697 <rdar://113641596>
    Reviewed by Elliott Williams.
    
    This change introduces a libJavaScriptCore.a target, which is then used
    to link JavaScriptCore.framework.  By configuring the project to build
    this way, there is no need to duplicate the list of sources between both
    targets.  When JSC_USE_STATIC_LINKING=YES, the jsc binary is linked to
    libJavaScriptCore.a instead of to JavaScriptCore.framework.
    
    * Source/JavaScriptCore/API/JSWrapperMap.h:
    * Source/JavaScriptCore/API/ObjCCallbackFunction.h:
    - Fix header include paths to work with static library.
    * Source/JavaScriptCore/Configurations/BaseTarget.xcconfig: Copy from Source/JavaScriptCore/Configurations/JSC.xcconfig.
    - Extract common variables from JavaScriptCore.xcconfig that are shared
      with libJavaScriptCore.xcconfig.
    * Source/JavaScriptCore/Configurations/Base.xcconfig:
    - Extract linker variables from JavaScriptCore.xcconfig that are shared
      with JSC.xcconfig.
    - Note that `-fobjc-link-runtime` replaces
      `-lobjc -framework CoreFoundation -framework Foundation`.
    * Source/JavaScriptCore/Configurations/JSC.xcconfig:
    (JSC_USE_STATIC_LINKING): Add.
    - Set to the same boolean value as ENABLE_LIBFUZZER (which will default
      to NO), but make it a separate variable to allow this behavior to be
      overriden via xcodebuild.
    - Configure OTHER_LDFLAGS to statically link libJavaScriptCore.a when
      JSC_USE_STATIC_LINKING=YES, else use dynamic linking (default) to
      JavaScriptCore.framework.
    * Source/JavaScriptCore/Configurations/JavaScriptCore.xcconfig:
    - Extract shared variables into BaseTarget.xcconfig.
    - Move build related variables into libJavaScriptCore.xcconfig.
    * Source/JavaScriptCore/Configurations/libJavaScriptCore.xcconfig: Add.
    - Move build-related variables from JavaScriptCore.xcconfig.
    * Source/JavaScriptCore/JavaScriptCore.xcodeproj/project.pbxproj:
    - Add new xcconfig files to project.
    - Add libJavaScriptCore.a target and use libJavaScriptCore.xcconfig as
      its base.
    - Move from JavaScriptCore framework target to libJavaScriptCore target:
      - Custom Build Rules.
      - Compile Sources.
      - Target Dependencies.
      - Copy Profiling Data build phase script.
    - Add target dependency on libJavaScriptCore to JavaScriptCore.
    - Add JavaScriptCoreFramework.cpp source file to JavaScriptCore
      framework target.
    - Remove all items from "Link Binary with Libraries" for JavaScriptCore
      and jsc targets (except libedit.dylib on jsc) since this is handled
      via OTHER_LDFLAGS in xcconfig files now.
    * Source/JavaScriptCore/JavaScriptCoreFramework.cpp: Add.
    - Xcode requires at least one source file to compile to link a
      framework so create an empty one.
    
    Canonical link: https://commits.webkit.org/270226@main

--- Source/JavaScriptCore/JavaScriptCoreFramework.cpp
+++ Source/JavaScriptCore/JavaScriptCoreFramework.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2023 Apple Inc. All rights reserved.
+ * Copyright (C) 2023 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -20,29 +20,7 @@
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "JSValueInternal.h"
-#import <JavaScriptCore/JavaScriptCore.h>
-#import <objc/objc-runtime.h>
-
-#if JSC_OBJC_API_ENABLED
-
-@interface JSWrapperMap : NSObject
-
-- (instancetype)initWithGlobalContextRef:(JSGlobalContextRef)context;
-
-- (JSValue *)jsWrapperForObject:(id)object inContext:(JSContext *)context;
-
-- (JSValue *)objcWrapperForJSValueRef:(JSValueRef)value inContext:(JSContext *)context;
-
-@end
-
-id tryUnwrapObjcObject(JSGlobalContextRef, JSValueRef);
-
-bool supportsInitMethodConstructors();
-Protocol *getJSExportProtocol();
-Class getNSBlockClass();
-
-#endif
+// Used to build JavaScriptCore.framework and link libJavaScriptCore.a.
