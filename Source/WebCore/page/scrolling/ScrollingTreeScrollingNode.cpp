WebKit/WebKit

commit 9b87cf844c2f847a8222bab057eac7ba735e605e

[iOS] A scrollTo() followed by an animated scroll ends at the wrong scroll position https://bugs.webkit.org/show_bug.cgi?id=263808 rdar://117608836
Reviewed by Richard Robinson.

Programmatic scroll requests (`RequestedScrollData`) are handled in three places:
1. `RemoteLayerTreeDrawingAreaProxy::commitLayerTreeTransaction()` for iOS main frame scrolls
2. `ScrollingTreeScrollingNode::handleScrollPositionRequest()` for non-main-frame iOS scrolls
3. `RemoteScrollingTreeMac::startPendingScrollAnimations()` for all scollers on macOS

The second failed to handle the `requestedDataBeforeAnimatedScroll` data in the request, so
a sequence like `scrollTo(x, y); scrollBy({ top: z, behavior: 'smooth' })` would fail to
accumulate the two deltas.

Fix by having `ScrollingTreeScrollingNode::handleScrollPositionRequest()` process `requestedDataBeforeAnimatedScroll`
like the other call sites. Also reuse the "position or delta" logic in `RequestedScrollData::destinationPosition()`
in all these call sites for clarity.

* LayoutTests/fast/scrolling/ios/smooth-scroll-after-scrollTo-on-overflow-expected.txt: Added.
* LayoutTests/fast/scrolling/ios/smooth-scroll-after-scrollTo-on-overflow.html: Added.
* Source/WebCore/page/scrolling/ScrollingCoordinatorTypes.cpp:
(WebCore::RequestedScrollData::destinationPosition const):
(WebCore::RequestedScrollData::computeDestinationPosition):
* Source/WebCore/page/scrolling/ScrollingCoordinatorTypes.h:
* Source/WebCore/page/scrolling/ScrollingTreeScrollingNode.cpp:
(WebCore::ScrollingTreeScrollingNode::handleScrollPositionRequest):
* Source/WebKit/UIProcess/RemoteLayerTree/RemoteLayerTreeDrawingAreaProxy.mm:
(WebKit::RemoteLayerTreeDrawingAreaProxy::commitLayerTreeTransaction):
* Source/WebKit/UIProcess/RemoteLayerTree/mac/RemoteScrollingTreeMac.mm:
(WebKit::RemoteScrollingTreeMac::startPendingScrollAnimations):

Canonical link: https://commits.webkit.org/269897@main

Source/WebCore/page/scrolling/ScrollingTreeScrollingNode.cpp
@@ -313,14 +313,22 @@ void ScrollingTreeScrollingNode::handleScrollPositionRequest(const RequestedScro
     if (scrollingTree().scrollingTreeNodeRequestsScroll(scrollingNodeID(), requestedScrollData))
         return;
 
-    auto scrollToPosition = requestedScrollData.destinationPosition(currentScrollPosition());
+    auto currentScrollPosition = this->currentScrollPosition();
+
+    if (requestedScrollData.requestedDataBeforeAnimatedScroll) {
+        auto& [requestType, positionOrDeltaBeforeAnimatedScroll, scrollType, clamping] = *requestedScrollData.requestedDataBeforeAnimatedScroll;
+        auto intermediatePosition = RequestedScrollData::computeDestinationPosition(currentScrollPosition, requestType, positionOrDeltaBeforeAnimatedScroll);
+        scrollTo(intermediatePosition, scrollType, clamping);
+    }
+
+    auto destinationPosition = requestedScrollData.destinationPosition(currentScrollPosition);
 
     if (requestedScrollData.animated == ScrollIsAnimated::Yes) {
-        startAnimatedScrollToPosition(scrollToPosition);
+        startAnimatedScrollToPosition(destinationPosition);
         return;
     }
 
-    scrollTo(scrollToPosition, requestedScrollData.scrollType, requestedScrollData.clamping);
+    scrollTo(destinationPosition, requestedScrollData.scrollType, requestedScrollData.clamping);
 }
 
 FloatPoint ScrollingTreeScrollingNode::adjustedScrollPosition(const FloatPoint& scrollPosition, ScrollClamping clamping) const

Source/WebKit/UIProcess/RemoteLayerTree/mac/RemoteScrollingTreeMac.mm
@@ -161,12 +161,15 @@ void RemoteScrollingTreeMac::startPendingScrollAnimations()
         if (!targetNode)
             continue;
 
+        auto currentScrollPosition = targetNode->currentScrollPosition();
+
         if (auto previousData = std::exchange(data.requestedDataBeforeAnimatedScroll, std::nullopt)) {
-            auto& [type, positionOrDeltaBeforeAnimatedScroll, scrollType, clamping] = *previousData;
-            targetNode->scrollTo(type == ScrollRequestType::DeltaUpdate ? (targetNode->currentScrollPosition() + std::get<FloatSize>(positionOrDeltaBeforeAnimatedScroll)) : std::get<FloatPoint>(positionOrDeltaBeforeAnimatedScroll), scrollType, clamping);
+            auto& [requestType, positionOrDeltaBeforeAnimatedScroll, scrollType, clamping] = *previousData;
+            auto intermediatePosition = RequestedScrollData::computeDestinationPosition(currentScrollPosition, requestType, positionOrDeltaBeforeAnimatedScroll);
+            targetNode->scrollTo(intermediatePosition, scrollType, clamping);
         }
 
-        targetNode->startAnimatedScrollToPosition(data.destinationPosition(targetNode->currentScrollPosition()));
+        targetNode->startAnimatedScrollToPosition(data.destinationPosition(currentScrollPosition));
     }
 
     auto nodesWithPendingKeyboardScrollAnimations = std::exchange(m_nodesWithPendingKeyboardScrollAnimations, { });

LayoutTests/fast/scrolling/ios/smooth-scroll-after-scrollTo-on-overflow-expected.txt
@@ -1,0 +1,5 @@
+PASS scrollContainer.scrollTop is 700
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/scrolling/ios/smooth-scroll-after-scrollTo-on-overflow.html
@@ -1,0 +1,53 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ AsyncOverflowScrollingEnabled=true ] -->
+<html>
+<head>
+    <style>
+        .scroll-container {
+            height: 250px;
+            width: 300px;
+            border: 1px solid black;
+            overflow: scroll;
+        }
+        
+        .contents {
+            height: 500%;
+            background-image: repeating-linear-gradient(white, silver 250px);
+        }
+    </style>
+    <script src="../../../resources/ui-helper.js"></script>
+    <script src="../../../resources/js-test-pre.js"></script>
+    <script>
+        jsTestIsAsync = true;
+
+        async function runTest()
+        {
+            scrollContainer = document.querySelector('.scroll-container');
+
+            if (window.eventSender)
+                await UIHelper.startMonitoringWheelEvents();
+
+            scrollContainer.scrollTo(0, 200);
+            scrollContainer.scrollBy({
+                top: 500,
+                behavior: 'smooth'
+            });
+            
+            await UIHelper.waitForScrollCompletion();
+            
+            shouldBe('scrollContainer.scrollTop', '700');
+            finishJSTest();
+        }
+    
+        window.addEventListener('load', () => {
+            setTimeout(runTest, 0);
+        }, false);
+    </script>
+</head>
+<body>
+    <div class="scroll-container">
+        <div class="contents"></div>
+    </div>
+    <div id="console"></div>
+    <script src="../../../resources/js-test-post.js"></script>
+</body>
+</html>

Source/WebCore/page/scrolling/ScrollingCoordinatorTypes.cpp
@@ -60,9 +60,17 @@ void RequestedScrollData::merge(RequestedScrollData&& other)
     *this = WTFMove(other);
 }
 
-FloatPoint RequestedScrollData::destinationPosition(const FloatPoint& currentScrollPosition) const
+FloatPoint RequestedScrollData::destinationPosition(FloatPoint currentScrollPosition) const
 {
-    return requestType == ScrollRequestType::DeltaUpdate ? (currentScrollPosition + std::get<FloatSize>(scrollPositionOrDelta)) : std::get<FloatPoint>(scrollPositionOrDelta);
+    return computeDestinationPosition(currentScrollPosition, requestType, scrollPositionOrDelta);
+}
+
+FloatPoint RequestedScrollData::computeDestinationPosition(FloatPoint currentScrollPosition, ScrollRequestType requestType, const std::variant<FloatPoint, FloatSize>& scrollPositionOrDelta)
+{
+    if (requestType == ScrollRequestType::DeltaUpdate)
+        return currentScrollPosition + std::get<FloatSize>(scrollPositionOrDelta);
+
+    return std::get<FloatPoint>(scrollPositionOrDelta);
 }
 
 TextStream& operator<<(TextStream& ts, SynchronousScrollingReason reason)

Source/WebCore/page/scrolling/ScrollingCoordinatorTypes.h
@@ -115,7 +115,10 @@ struct RequestedScrollData {
     std::optional<std::tuple<ScrollRequestType, std::variant<FloatPoint, FloatSize>, ScrollType, ScrollClamping>> requestedDataBeforeAnimatedScroll { };
 
     void merge(RequestedScrollData&&);
-    WEBCORE_EXPORT FloatPoint destinationPosition(const FloatPoint&) const;
+
+    WEBCORE_EXPORT FloatPoint destinationPosition(FloatPoint currentScrollPosition) const;
+    WEBCORE_EXPORT static FloatPoint computeDestinationPosition(FloatPoint currentScrollPosition, ScrollRequestType, const std::variant<FloatPoint, FloatSize>& scrollPositionOrDelta);
+
     bool comparePositionOrDelta(const RequestedScrollData& other) const
     {
         if (requestType == ScrollRequestType::PositionUpdate)

Source/WebKit/UIProcess/RemoteLayerTree/RemoteLayerTreeDrawingAreaProxy.mm
@@ -279,13 +279,13 @@ void RemoteLayerTreeDrawingAreaProxy::commitLayerTreeTransaction(IPC::Connection
     // Handle requested scroll position updates from the scrolling tree transaction after didCommitLayerTree()
     // has updated the view size based on the content size.
     if (requestedScroll) {
-        auto previousScrollPosition = webPageProxy->scrollingCoordinatorProxy()->currentMainFrameScrollPosition();
+        auto currentScrollPosition = webPageProxy->scrollingCoordinatorProxy()->currentMainFrameScrollPosition();
         if (auto previousData = std::exchange(requestedScroll->requestedDataBeforeAnimatedScroll, std::nullopt)) {
-            auto& [type, positionOrDeltaBeforeAnimatedScroll, scrollType, clamping] = *previousData;
-            previousScrollPosition = type == ScrollRequestType::DeltaUpdate ? (webPageProxy->scrollingCoordinatorProxy()->currentMainFrameScrollPosition() + std::get<FloatSize>(positionOrDeltaBeforeAnimatedScroll)) : std::get<FloatPoint>(positionOrDeltaBeforeAnimatedScroll);
+            auto& [requestType, positionOrDeltaBeforeAnimatedScroll, scrollType, clamping] = *previousData;
+            currentScrollPosition = RequestedScrollData::computeDestinationPosition(currentScrollPosition, requestType, positionOrDeltaBeforeAnimatedScroll);
         }
 
-        webPageProxy->requestScroll(requestedScroll->destinationPosition(previousScrollPosition), layerTreeTransaction.scrollOrigin(), requestedScroll->animated);
+        webPageProxy->requestScroll(requestedScroll->destinationPosition(currentScrollPosition), layerTreeTransaction.scrollOrigin(), requestedScroll->animated);
     }
 #endif // ENABLE(ASYNC_SCROLLING)
 

commit a53d9e00eb65bcb56d2d24b1edde139091027c63

REGRESSION (269897@main): [ Sonoma wk2 ] 2 tests in imported/w3c/web-platform-tests/css/cssom-view/ are a constant failure https://bugs.webkit.org/show_bug.cgi?id=263925 rdar://117706782
Reviewed by Richard Robinson.

Fix a bug introduced in 269897@main, and an older bug that prevented scroll-behavior-main-frame* tests from passing.

In 269897@main the intermediate scrollTo() that we do when we have `requestedDataBeforeAnimatedScroll` data needs
to also contribute its destination position to the animated scroll that happens second, but both
`ScrollingTreeScrollingNode::handleScrollPositionRequest()` and `RemoteScrollingTreeMac::startPendingScrollAnimations()`
failed to do this.

Even wit this fix, two WPT tests were failing when they made the following sequence of programmatic scrolls:
    scrollBy(0, X, animated)
    scrollTo(0, 0)
    scrollBy(0, Y, animated)

We'd end up scrolling to X + Y because `ScrollView::setScrollPosition()` early returned if there was no position
change for the `scrollTo(0, 0)`. But we need to hit `requestScrollToPosition()` so that we have a chance to
cancel the enqueued animated scroll, so remove the `!delegatesScrollingToNativeView()` check.

* LayoutTests/platform/mac-ventura-wk2/TestExpectations:
* LayoutTests/platform/mac-wk2/TestExpectations:
* Source/WebCore/page/scrolling/ScrollingTreeScrollingNode.cpp:
(WebCore::ScrollingTreeScrollingNode::handleScrollPositionRequest):
* Source/WebCore/platform/ScrollView.cpp:
(WebCore::ScrollView::setScrollPosition):
* Source/WebKit/UIProcess/RemoteLayerTree/mac/RemoteScrollingTreeMac.mm:
(WebKit::RemoteScrollingTreeMac::startPendingScrollAnimations):

Canonical link: https://commits.webkit.org/270243@main

Source/WebCore/page/scrolling/ScrollingTreeScrollingNode.cpp
@@ -301,11 +301,11 @@ void ScrollingTreeScrollingNode::requestKeyboardScroll(const RequestedKeyboardSc
 
 void ScrollingTreeScrollingNode::handleScrollPositionRequest(const RequestedScrollData& requestedScrollData)
 {
-    LOG_WITH_STREAM(Scrolling, stream << "ScrollingTreeScrollingNode " << scrollingNodeID() << " handleScrollPositionRequest()" << " animated " << (requestedScrollData.animated == ScrollIsAnimated::Yes) << " requestedScrollData: " << requestedScrollData);
-
     stopAnimatedScroll();
 
     if (requestedScrollData.requestType == ScrollRequestType::CancelAnimatedScroll) {
+        ASSERT(!requestedScrollData.requestedDataBeforeAnimatedScroll);
+        LOG_WITH_STREAM(Scrolling, stream << "ScrollingTreeScrollingNode " << scrollingNodeID() << " handleScrollPositionRequest() - cancel animated scroll");
         scrollingTree().removePendingScrollAnimationForNode(scrollingNodeID());
         return;
     }
@@ -313,7 +313,7 @@ void ScrollingTreeScrollingNode::handleScrollPositionRequest(const RequestedScro
     if (scrollingTree().scrollingTreeNodeRequestsScroll(scrollingNodeID(), requestedScrollData))
         return;
 
-    auto currentScrollPosition = this->currentScrollPosition();
+    LOG_WITH_STREAM(Scrolling, stream << "ScrollingTreeScrollingNode " << scrollingNodeID() << " handleScrollPositionRequest() with data " << requestedScrollData);
 
     if (requestedScrollData.requestedDataBeforeAnimatedScroll) {
         auto& [requestType, positionOrDeltaBeforeAnimatedScroll, scrollType, clamping] = *requestedScrollData.requestedDataBeforeAnimatedScroll;
@@ -321,7 +321,7 @@ void ScrollingTreeScrollingNode::handleScrollPositionRequest(const RequestedScro
         switch (requestType) {
         case ScrollRequestType::PositionUpdate:
         case ScrollRequestType::DeltaUpdate: {
-            auto intermediatePosition = RequestedScrollData::computeDestinationPosition(currentScrollPosition, requestType, positionOrDeltaBeforeAnimatedScroll);
+            auto intermediatePosition = RequestedScrollData::computeDestinationPosition(currentScrollPosition(), requestType, positionOrDeltaBeforeAnimatedScroll);
             scrollTo(intermediatePosition, scrollType, clamping);
             break;
         }
@@ -331,8 +331,7 @@ void ScrollingTreeScrollingNode::handleScrollPositionRequest(const RequestedScro
         }
     }
 
-    auto destinationPosition = requestedScrollData.destinationPosition(currentScrollPosition);
-
+    auto destinationPosition = requestedScrollData.destinationPosition(currentScrollPosition());
     if (requestedScrollData.animated == ScrollIsAnimated::Yes) {
         startAnimatedScrollToPosition(destinationPosition);
         return;

Source/WebCore/platform/ScrollView.cpp
@@ -552,8 +552,10 @@ void ScrollView::setScrollPosition(const ScrollPosition& scrollPosition, const S
         return;
     }
 
-    ScrollPosition newScrollPosition = (!delegatesScrollingToNativeView() && options.clamping == ScrollClamping::Clamped) ? adjustScrollPositionWithinRange(scrollPosition) : scrollPosition;
-    if ((!delegatesScrollingToNativeView() || currentScrollType() == ScrollType::User) && newScrollPosition == this->scrollPosition()) {
+    auto newScrollPosition = (!delegatesScrollingToNativeView() && options.clamping == ScrollClamping::Clamped) ? adjustScrollPositionWithinRange(scrollPosition) : scrollPosition;
+    bool scrollPositionChanged = newScrollPosition != this->scrollPosition();
+
+    if (currentScrollType() == ScrollType::User && !scrollPositionChanged) {
         LOG_WITH_STREAM(Scrolling, stream << "ScrollView::setScrollPosition " << scrollPosition << " return for no change");
         return;
     }

Source/WebKit/UIProcess/RemoteLayerTree/mac/RemoteScrollingTreeMac.mm
@@ -153,15 +153,12 @@ void RemoteScrollingTreeMac::startPendingScrollAnimations()
     ASSERT(m_treeLock.isLocked());
 
     auto nodesWithPendingScrollAnimations = std::exchange(m_nodesWithPendingScrollAnimations, { });
-
-    LOG_WITH_STREAM(Scrolling, stream << "RemoteScrollingTreeMac::startPendingScrollAnimations() - " << nodesWithPendingScrollAnimations.size() << " nodes with pending animations");
-
     for (auto& [nodeID, data] : nodesWithPendingScrollAnimations) {
         RefPtr targetNode = dynamicDowncast<ScrollingTreeScrollingNode>(nodeForID(nodeID));
         if (!targetNode)
             continue;
 
-        auto currentScrollPosition = targetNode->currentScrollPosition();
+        LOG_WITH_STREAM(Scrolling, stream << "RemoteScrollingTreeMac::startPendingScrollAnimations() for node " << nodeID << " with data " << data);
 
         if (auto previousData = std::exchange(data.requestedDataBeforeAnimatedScroll, std::nullopt)) {
             auto& [requestType, positionOrDeltaBeforeAnimatedScroll, scrollType, clamping] = *previousData;
@@ -169,7 +166,7 @@ void RemoteScrollingTreeMac::startPendingScrollAnimations()
             switch (requestType) {
             case ScrollRequestType::PositionUpdate:
             case ScrollRequestType::DeltaUpdate: {
-                auto intermediatePosition = RequestedScrollData::computeDestinationPosition(currentScrollPosition, requestType, positionOrDeltaBeforeAnimatedScroll);
+                auto intermediatePosition = RequestedScrollData::computeDestinationPosition(targetNode->currentScrollPosition(), requestType, positionOrDeltaBeforeAnimatedScroll);
                 targetNode->scrollTo(intermediatePosition, scrollType, clamping);
                 break;
             }
@@ -179,7 +176,8 @@ void RemoteScrollingTreeMac::startPendingScrollAnimations()
             }
         }
 
-        targetNode->startAnimatedScrollToPosition(data.destinationPosition(currentScrollPosition));
+        auto finalPosition = data.destinationPosition(targetNode->currentScrollPosition());
+        targetNode->startAnimatedScrollToPosition(finalPosition);
     }
 
     auto nodesWithPendingKeyboardScrollAnimations = std::exchange(m_nodesWithPendingKeyboardScrollAnimations, { });

LayoutTests/platform/mac-ventura-wk2/TestExpectations
@@ -34,3 +34,6 @@ webkit.org/b/261444 [ Debug x86_64 ] http/tests/security/referrer-policy-header.
 
 # Asserts on pre-Sonoma macOS: rdar://116291539
 [ Debug ] http/tests/site-isolation/window-properties.html [ Skip ]
+
+# webkit.org/b/263955 Twelve css WPTs are failing with async overflow scrolling enabled on macOS
+imported/w3c/web-platform-tests/css/cssom-view/scroll-behavior-smooth-positions.html [ Failure ]

LayoutTests/platform/mac-wk2/TestExpectations
@@ -614,7 +614,6 @@ imported/w3c/web-platform-tests/css/css-ui/outline-negative-offset-composited-sc
 imported/w3c/web-platform-tests/css/css-scroll-snap/snap-inline-block.html [ Failure ]
 imported/w3c/web-platform-tests/css/css-transforms/transform3d-preserve3d-001.html [ ImageOnlyFailure ]
 imported/w3c/web-platform-tests/css/cssom-view/scroll-behavior-element.html [ Failure ]
-imported/w3c/web-platform-tests/css/cssom-view/scroll-behavior-smooth-positions.html [ Failure ]
 
 ### END OF (1) Classified failures with bug reports
 ########################################
@@ -1986,7 +1985,3 @@ http/tests/webgpu/webgpu/api/operation/render_pipeline/overrides.html [ Pass ]
 http/tests/webgpu/webgpu/api/operation/render_pipeline/primitive_topology.html [ Pass ]
 
 webkit.org/b/263920 svg/transforms/transformed-text-fill-gradient.html [ ImageOnlyFailure ]
-
-# rdar://117706782 (REGRESSION (269897@main): [ Sonoma wk2 ] 2 tests in imported/w3c/web-platform-t ests/css/cssom-view/ are a constant failure
-[ Sonoma+ ] imported/w3c/web-platform-tests/css/cssom-view/scroll-behavior-main-frame-window.html [ Failure ]
-[ Sonoma+ ] imported/w3c/web-platform-tests/css/cssom-view/scroll-behavior-main-frame-root.html [ Failure ]

LayoutTests/tiled-drawing/scrolling/clamp-out-of-bounds-scrolls-expected.txt
@@ -3,6 +3,7 @@ Attempted scroll to -5000, 0
 (Frame scrolling node
   (scrollable area size 785 585)
   (contents size 5008 5021)
+  (requested scroll position represents programmatic scroll 1)
   (scrollable area parameters
     (horizontal scroll elasticity 2)
     (vertical scroll elasticity 2)
@@ -21,6 +22,7 @@ Attempted scroll to 0, -5000
 (Frame scrolling node
   (scrollable area size 785 585)
   (contents size 5008 5021)
+  (requested scroll position represents programmatic scroll 1)
   (scrollable area parameters
     (horizontal scroll elasticity 2)
     (vertical scroll elasticity 2)
@@ -39,6 +41,7 @@ Attempted scroll to -5000, -5000
 (Frame scrolling node
   (scrollable area size 785 585)
   (contents size 5008 5021)
+  (requested scroll position represents programmatic scroll 1)
   (scrollable area parameters
     (horizontal scroll elasticity 2)
     (vertical scroll elasticity 2)

LayoutTests/tiled-drawing/scrolling/fixed/negative-scroll-offset-expected.txt
@@ -2,6 +2,7 @@
 (Frame scrolling node
   (scrollable area size 785 600)
   (contents size 785 2221)
+  (requested scroll position represents programmatic scroll 1)
   (scrollable area parameters
     (horizontal scroll elasticity 2)
     (vertical scroll elasticity 2)

LayoutTests/tiled-drawing/scrolling/fixed/negative-scroll-offset-in-view-expected.txt
@@ -2,6 +2,7 @@
 (Frame scrolling node
   (scrollable area size 785 600)
   (contents size 785 2221)
+  (requested scroll position represents programmatic scroll 1)
   (scrollable area parameters
     (horizontal scroll elasticity 2)
     (vertical scroll elasticity 2)

LayoutTests/tiled-drawing/scrolling/sticky/negative-scroll-offset-expected.txt
@@ -2,6 +2,7 @@
 (Frame scrolling node
   (scrollable area size 785 600)
   (contents size 785 2216)
+  (requested scroll position represents programmatic scroll 1)
   (scrollable area parameters
     (horizontal scroll elasticity 2)
     (vertical scroll elasticity 2)