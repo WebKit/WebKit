824e7a817082c9aa5ce165ce62bf7db87cbb3478
Fujii Hironori <Hironori.Fujii@sony.com>
2023-11-06 01:49:51 -0800

    Rewrite Python CGI under http/tests/xmlhttprequest to use binary mode for stdout for Windows Python https://bugs.webkit.org/show_bug.cgi?id=264238
    Reviewed by Ross Kirsling.
    
    Windows Python automatically converts '\n' to '\r\n' in text mode.
    Use sys.stdout.buffer.write instead of sys.stdout.write.
    
    * LayoutTests/http/tests/xmlhttprequest/resources/access-control-allow-lists.py:
    * LayoutTests/http/tests/xmlhttprequest/resources/access-control-allow-with-body.py:
    * LayoutTests/http/tests/xmlhttprequest/resources/basic-auth-default/dir1/basic-auth.py:
    * LayoutTests/http/tests/xmlhttprequest/resources/basic-auth-default/dir2/basic-auth.py:
    * LayoutTests/http/tests/xmlhttprequest/resources/basic-auth-default/dir2/catch.py:
    * LayoutTests/http/tests/xmlhttprequest/resources/chunked-transfer.py:
    * LayoutTests/http/tests/xmlhttprequest/resources/cross-origin-authorization.py:
    * LayoutTests/http/tests/xmlhttprequest/resources/cross-origin-check-cookies.py:
    * LayoutTests/http/tests/xmlhttprequest/resources/cross-origin-no-authorization.py:
    * LayoutTests/http/tests/xmlhttprequest/resources/cross-origin-preflight-get.py:
    * LayoutTests/http/tests/xmlhttprequest/resources/cross-origin-set-cookies.py:
    * LayoutTests/http/tests/xmlhttprequest/resources/logout/resource.py:
    * LayoutTests/http/tests/xmlhttprequest/resources/re-login/resource.py:
    * LayoutTests/platform/wincairo/TestExpectations:
    
    Canonical link: https://commits.webkit.org/270258@main

--- LayoutTests/http/tests/xmlhttprequest/resources/basic-auth-default/dir2/catch.py
+++ LayoutTests/http/tests/xmlhttprequest/resources/basic-auth-default/dir2/catch.py
@@ -3,21 +3,21 @@ import base64
 import os
 import sys
 
-sys.stdout.write('Content-Type: text/html\r\n')
+sys.stdout.buffer.write(b'Content-Type: text/html\r\n')
 
 credentials = base64.b64decode(os.environ.get('HTTP_AUTHORIZATION', ' Og==').split(' ')[1]).decode().split(':')
 username = credentials[0]
 password = ':'.join(credentials[1:]) if len(credentials) > 1 else ''
 
 if username:
-    sys.stdout.write(
+    sys.stdout.buffer.write(
         '\r\n'
-        'User: {}, password: {}.'.format(username, password)
+        'User: {}, password: {}.'.format(username, password).encode()
     )
 
 else:
-    sys.stdout.write(
-        'status: 500\r\n'
-        '\r\n'
-        'Where is my default auth?'
+    sys.stdout.buffer.write(
+        b'status: 500\r\n'
+        b'\r\n'
+        b'Where is my default auth?'
     )
--- LayoutTests/http/tests/xmlhttprequest/resources/chunked-transfer.py
+++ LayoutTests/http/tests/xmlhttprequest/resources/chunked-transfer.py
@@ -2,14 +2,14 @@
 import os
 import sys
 
-sys.stdout.write(
-    'Content-Type: text/html\r\n'
-    'Transfer-encoding: chunked\r\n'
-    'Cache-Control: no-cache, no-store\r\n'
-    '\r\n'
+sys.stdout.buffer.write(
+    b'Content-Type: text/html\r\n'
+    b'Transfer-encoding: chunked\r\n'
+    b'Cache-Control: no-cache, no-store\r\n'
+    b'\r\n'
 )
 
 sys.stdout.flush()
-sys.stdout.write("4\r\n<a/>\r\n")
+sys.stdout.buffer.write(b"4\r\n<a/>\r\n")
 sys.stdout.flush()
-sys.stdout.write("0\r\n\r\n")
+sys.stdout.buffer.write(b"0\r\n\r\n")
