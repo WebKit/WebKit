84aabd280f9ab479463a9701fd52a3d3c95b347d
Matt Woodrow <mattwoodrow@apple.com>
2023-11-06 15:28:37 -0800

    REGRESSION (266237@main): Poster circle animation is not continuous https://bugs.webkit.org/show_bug.cgi?id=263569 <rdar://117209302>
    Reviewed by Simon Fraser.
    
    This is the same as the existing hasBig3DRotation rotation issue, where CA animates the shortest path between 3d rotations,
    because we represent them as matrices.
    
    Previously our computed matrices for rotate(180deg) didn’t have integer values, due to a float rounding issue during deg->rad.
    
    The new change computes the rotation matrix exactly, and now there’s no longer a defined direction for a 180deg rotation and
    we sometimes end up with a ‘backwards’ rotation.
    
    This adds an option to not do the snapping when we compute matrices for CA, so that the existing behavior is preserved.
    
    * Source/WebCore/platform/graphics/transforms/RotateTransformOperation.h:
    * Source/WebCore/platform/graphics/transforms/TransformOperation.h:
    (WebCore::TransformOperation::applyUnrounded const):
    * Source/WebCore/platform/graphics/transforms/TransformationMatrix.cpp:
    (WebCore::TransformationMatrix::rotate3d):
    (WebCore::TransformationMatrix::rotate):
    (WebCore::TransformationMatrix::rotateRadians):
    * Source/WebCore/platform/graphics/transforms/TransformationMatrix.h:
    
    Canonical link: https://commits.webkit.org/270294@main

--- Source/WebCore/platform/graphics/transforms/TransformationMatrix.cpp
+++ Source/WebCore/platform/graphics/transforms/TransformationMatrix.cpp
@@ -894,7 +894,7 @@ static double roundEpsilonToZero(double val)
     return val;
 }
 
-TransformationMatrix& TransformationMatrix::rotate3d(double x, double y, double z, double angle)
+TransformationMatrix& TransformationMatrix::rotate3d(double x, double y, double z, double angle, RotationSnapping snapping)
 {
     // Normalize the axis of rotation
     double length = std::hypot(x, y, z);
@@ -910,8 +910,8 @@ TransformationMatrix& TransformationMatrix::rotate3d(double x, double y, double 
     // Angles are in degrees. Switch to radians.
     angle = deg2rad(angle);
 
-    double sinTheta = roundEpsilonToZero(sin(angle));
-    double cosTheta = roundEpsilonToZero(cos(angle));
+    double sinTheta = snapping == RotationSnapping::Snap90degRotations ? roundEpsilonToZero(sin(angle)) : sin(angle);
+    double cosTheta = snapping == RotationSnapping::Snap90degRotations ? roundEpsilonToZero(cos(angle)) : cos(angle);
 
     TransformationMatrix mat;
 
@@ -981,23 +981,23 @@ TransformationMatrix& TransformationMatrix::rotate3d(double x, double y, double 
     return *this;
 }
 
-TransformationMatrix& TransformationMatrix::rotate(double angle)
+TransformationMatrix& TransformationMatrix::rotate(double angle, RotationSnapping snapping)
 {
     if (!std::fmod(angle, 360))
         return *this;
 
-    return rotateRadians(deg2rad(angle));
+    return rotateRadians(deg2rad(angle), snapping);
 }
 
-TransformationMatrix& TransformationMatrix::rotateRadians(double angle)
+TransformationMatrix& TransformationMatrix::rotateRadians(double angle, RotationSnapping snapping)
 {
-    double sinZ = roundEpsilonToZero(sin(angle));
-    double cosZ = roundEpsilonToZero(cos(angle));
+    double sinZ = snapping == RotationSnapping::Snap90degRotations ? roundEpsilonToZero(sin(angle)) : sin(angle);
+    double cosZ = snapping == RotationSnapping::Snap90degRotations ? roundEpsilonToZero(cos(angle)) : cos(angle);
     multiply({ cosZ, sinZ, -sinZ, cosZ, 0, 0 });
     return *this;
 }
 
-TransformationMatrix& TransformationMatrix::rotate3d(double rx, double ry, double rz)
+TransformationMatrix& TransformationMatrix::rotate3d(double rx, double ry, double rz, RotationSnapping snapping)
 {
     // Angles are in degrees. Switch to radians.
     rx = deg2rad(rx);
@@ -1006,8 +1006,8 @@ TransformationMatrix& TransformationMatrix::rotate3d(double rx, double ry, doubl
 
     TransformationMatrix mat;
 
-    double sinTheta = roundEpsilonToZero(sin(rz));
-    double cosTheta = roundEpsilonToZero(cos(rz));
+    double sinTheta = snapping == RotationSnapping::Snap90degRotations ? roundEpsilonToZero(sin(rz)) : sin(rz);
+    double cosTheta = snapping == RotationSnapping::Snap90degRotations ? roundEpsilonToZero(cos(rz)) : cos(rz);
 
     mat.m_matrix[0][0] = cosTheta;
     mat.m_matrix[0][1] = sinTheta;
@@ -1024,8 +1024,8 @@ TransformationMatrix& TransformationMatrix::rotate3d(double rx, double ry, doubl
 
     TransformationMatrix rmat(mat);
 
-    sinTheta = sin(ry);
-    cosTheta = cos(ry);
+    sinTheta = snapping == RotationSnapping::Snap90degRotations ? roundEpsilonToZero(sin(ry)) : sin(ry);
+    cosTheta = snapping == RotationSnapping::Snap90degRotations ? roundEpsilonToZero(cos(ry)) : cos(ry);
 
     mat.m_matrix[0][0] = cosTheta;
     mat.m_matrix[0][1] = 0.0;
@@ -1042,8 +1042,8 @@ TransformationMatrix& TransformationMatrix::rotate3d(double rx, double ry, doubl
 
     rmat.multiply(mat);
 
-    sinTheta = sin(rx);
-    cosTheta = cos(rx);
+    sinTheta = snapping == RotationSnapping::Snap90degRotations ? roundEpsilonToZero(sin(rx)) : sin(rx);
+    cosTheta = snapping == RotationSnapping::Snap90degRotations ? roundEpsilonToZero(cos(rx)) : cos(rx);
 
     mat.m_matrix[0][0] = 1.0;
     mat.m_matrix[0][1] = 0.0;
