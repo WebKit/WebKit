5158c32444fd418c40323af93ba591b190a93980
Aditya Keerthi <akeerthi@apple.com>
2023-11-07 08:32:55 -0800

    REGRESSION (270277@main): [iOS] imported/w3c/web-platform-tests/css/css-writing-modes/forms/text-input-block-size.optional.html is a constant failure https://bugs.webkit.org/show_bug.cgi?id=264315 rdar://118035123
    Reviewed by Megan Gardner.
    
    imported/w3c/web-platform-tests/css/css-writing-modes/forms/text-input-block-size.optional.html
    tests that the block-size of inputs are equal in vertical and horizontal
    writing modes.
    
    270277@main modified the user agent stylesheet to use logical properties to
    better support vertical form controls. However,
    `RenderTextControl::computeLogicalHeight` is still incorrectly using physical
    values for padding, border, and margin. The mix of logical height with physical
    padding/border/margin results in an inconsistent block-size depending on the
    writing mode.
    
    Fix by using logical properties in `RenderTextControl::computeLogicalHeight`.
    
    * Source/WebCore/rendering/RenderTextControl.cpp:
    (WebCore::RenderTextControl::computeLogicalHeight const):
    
    Canonical link: https://commits.webkit.org/270322@main

--- Source/WebCore/rendering/RenderTextControl.cpp
+++ Source/WebCore/rendering/RenderTextControl.cpp
@@ -96,7 +96,7 @@ RenderBox::LogicalExtentComputedValues RenderTextControl::computeLogicalHeight(L
         return RenderBox::computeLogicalHeight(LayoutUnit(), LayoutUnit());
 
     if (RenderBox* innerTextBox = innerText->renderBox()) {
-        LayoutUnit nonContentHeight = innerTextBox->verticalBorderAndPaddingExtent() + innerTextBox->verticalMarginExtent();
+        LayoutUnit nonContentHeight = innerTextBox->borderAndPaddingLogicalHeight() + innerTextBox->marginLogicalHeight();
         logicalHeight = computeControlLogicalHeight(innerTextBox->lineHeight(true, HorizontalLine, PositionOfInteriorLineBoxes), nonContentHeight);
 
         // We are able to have a horizontal scrollbar if the overflow style is scroll, or if its auto and there's no word wrap.
@@ -112,7 +112,7 @@ RenderBox::LogicalExtentComputedValues RenderTextControl::computeLogicalHeight(L
         // before calling computeLogicalHeight to avoid this hack.
         cacheIntrinsicContentLogicalHeightForFlexItem(logicalHeight);
         
-        logicalHeight += verticalBorderAndPaddingExtent();
+        logicalHeight += borderAndPaddingLogicalHeight();
     }
 
     return RenderBox::computeLogicalHeight(logicalHeight, logicalTop);
