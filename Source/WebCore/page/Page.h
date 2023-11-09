a42f1a766c55e731f627f385c9b2dc0cfc994926
Dan Robson <dtr_bugzilla@apple.com>
2023-11-08 22:11:29 -0800

    Cherry-pick 38398649280b. rdar://117215059
        AX: Sometimes unable to see play/pause animation context menu item when setting is toggled
        https://bugs.webkit.org/show_bug.cgi?id=263735
        rdar://117215059
    
        Reviewed by Tyler Wilcock.
    
        When deciding whether to add the "Play/Pause all animations" or "Play/Pause animation" context menu item, we had previously
        used a softlink to reference _AXSReduceMotionAutoplayAnimatedImagesEnabled. The issue with using this from the web content
        process, however, is that distributed notifications are not permitted as per the sandbox, so updates to this setting were
        not reaching that process.
    
        To resolve this, this patch now piggybacks onto our existing cross-process update for the animation setting using the
        AccessibilityPreferencesChanged notification and WebPage::updateImageAnimationEnabled. A new flag, m_systemAllowsAnimationControls,
        now maintains the state of this setting, and allows the Page to have an up-to-date view of the setting without relying on the
        softlink.
    
        * Source/WebCore/page/ContextMenuController.cpp:
        (WebCore::ContextMenuController::populate):
        * Source/WebCore/page/Page.cpp:
        (WebCore::Page::setSystemAllowsAnimationControls):
        * Source/WebCore/page/Page.h:
        (WebCore::Page::systemAllowsAnimationControls const):
        * Source/WebKit/WebProcess/WebPage/WebPage.cpp:
        (WebKit::WebPage::updateImageAnimationEnabled):
    
        Canonical link: https://commits.webkit.org/269878@main
    
    Identifier: 267815.551@safari-7617.1.17.11-branch

--- Source/WebCore/page/Page.h
+++ Source/WebCore/page/Page.h
@@ -697,10 +697,12 @@ public:
 #if ENABLE(ACCESSIBILITY_ANIMATION_CONTROL)
     void updatePlayStateForAllAnimations();
     WEBCORE_EXPORT void setImageAnimationEnabled(bool);
+    WEBCORE_EXPORT void setSystemAllowsAnimationControls(bool isAllowed);
     void addIndividuallyPlayingAnimationElement(HTMLImageElement&);
     void removeIndividuallyPlayingAnimationElement(HTMLImageElement&);
 #endif
     bool imageAnimationEnabled() const { return m_imageAnimationEnabled; }
+    bool systemAllowsAnimationControls() const { return m_systemAllowsAnimationControls; }
 
     void userStyleSheetLocationChanged();
     const String& userStyleSheet() const;
@@ -1225,6 +1227,7 @@ private:
 
     bool m_canStartMedia { true };
     bool m_imageAnimationEnabled { true };
+    bool m_systemAllowsAnimationControls { false };
     // Elements containing animations that are individually playing (potentially overriding the page-wide m_imageAnimationEnabled state).
     WeakHashSet<HTMLImageElement, WeakPtrImplWithEventTargetData> m_individuallyPlayingAnimationElements;
 
