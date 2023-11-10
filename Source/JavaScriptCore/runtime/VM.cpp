809790625dc81779661a1edd255dbb9f9daf2ebd
Yusuke Suzuki <ysuzuki@apple.com>
2023-11-09 19:54:29 -0800

    [JSC] Prefer full GC on opportunistic GC when navigation happens https://bugs.webkit.org/show_bug.cgi?id=264492 rdar://118179290
    Reviewed by Wenson Hsieh.
    
    Previously, when navigation happens, opportunistic GC may perform full GC.
    But this happens only when GC statistics says "we should do full GC instead of eden GC
    based on memory usage".
    
    But this happens only when navigation happens, and if we have enough time, then it is
    nice if we can just do full GC instead of eden GC even though we are not reaching to
    full GC threshold since (1) there are out-living objects and they can be collected only
    in full GC and (2) it is likely that most of objects are already garbage when navigation
    happens regardless of allocation threshold.
    
    This patch changes scheduleOpportunisticFullCollectionIfNeeded to attempt to perform full
    GC always when navigation happens.
    
    * Source/JavaScriptCore/heap/Heap.cpp:
    (JSC::Heap::scheduleOpportunisticFullCollectionIfNeeded):
    
    Canonical link: https://commits.webkit.org/270502@main

--- Source/JavaScriptCore/runtime/VM.cpp
+++ Source/JavaScriptCore/runtime/VM.cpp
@@ -1693,6 +1693,7 @@ void VM::performOpportunisticallyScheduledTasks(MonotonicTime deadline, OptionSe
             auto estimatedGCDuration = (heap.lastFullGCLength() * heap.m_totalBytesVisited) / heap.m_totalBytesVisitedAfterLastFullCollect;
             if (estimatedGCDuration + extraDurationToAvoidExceedingDeadlineDuringFullGC < remainingTime) {
                 heap.collectSync(CollectionScope::Full);
+                heap.m_shouldDoOpportunisticFullCollection = false;
                 return;
             }
         }
@@ -1700,8 +1701,11 @@ void VM::performOpportunisticallyScheduledTasks(MonotonicTime deadline, OptionSe
         auto timeSinceLastGC = secondsSinceEpoch - std::max(heap.m_lastGCEndTime, heap.m_currentGCStartTime).secondsSinceEpoch();
         if (timeSinceLastGC > minimumDelayBeforeOpportunisticEdenGC && heap.m_bytesAllocatedThisCycle && heap.m_bytesAllocatedBeforeLastEdenCollect) {
             auto estimatedGCDuration = (heap.lastEdenGCLength() * heap.m_bytesAllocatedThisCycle) / heap.m_bytesAllocatedBeforeLastEdenCollect;
-            if (estimatedGCDuration + extraDurationToAvoidExceedingDeadlineDuringEdenGC < remainingTime)
+            if (estimatedGCDuration + extraDurationToAvoidExceedingDeadlineDuringEdenGC < remainingTime) {
                 heap.collectSync(CollectionScope::Eden);
+                heap.m_shouldDoOpportunisticFullCollection = false;
+                return;
+            }
         }
     }();
 
