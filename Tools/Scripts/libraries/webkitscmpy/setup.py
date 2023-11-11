f49fc0217a7340a03b7aa183b11a8fd51748a412
Jonathan Bedard <jbedard@apple.com>
2023-11-10 18:00:17 -0800

    Cherry-pick 270025@main (db7018ee6d1d). rdar://117742552
        [webkit*py] Fix excluded directories in published packages
        https://bugs.webkit.org/show_bug.cgi?id=263970
        rdar://117742552
    
        Reviewed by Elliott Williams.
    
        * Tools/Scripts/libraries/webkitscmpy/MANIFEST.in: Add json data files and testing directory.
        * Tools/Scripts/libraries/webkitcorepy/setup.py: Bump version.
        * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/__init__.py: Ditto.
        * Tools/Scripts/libraries/webkitscmpy/setup.py: Include 'tests.hooks' directory, bump version.
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py: Bump version.
    
        Canonical link: https://commits.webkit.org/270025@main
    
    Canonical link: https://commits.webkit.org/267815.560@safari-7617-branch

--- Tools/Scripts/libraries/webkitscmpy/setup.py
+++ Tools/Scripts/libraries/webkitscmpy/setup.py
@@ -29,7 +29,7 @@ def readme():
 
 setup(
     name='webkitscmpy',
-    version='6.7.0',
+    version='6.7.2',
     description='Library designed to interact with git and svn repositories.',
     long_description=readme(),
     classifiers=[
--- Tools/Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py
+++ Tools/Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py
@@ -46,7 +46,7 @@ except ImportError:
         "Please install webkitcorepy with `pip install webkitcorepy --extra-index-url <package index URL>`"
     )
 
-version = Version(6, 7, 0)
+version = Version(6, 7, 2)
 
 AutoInstall.register(Package('fasteners', Version(0, 15, 0)))
 AutoInstall.register(Package('jinja2', Version(2, 11, 3)))
