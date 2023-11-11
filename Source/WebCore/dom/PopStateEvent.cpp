f034bb9b9246d75317a80d37f3f59e6f7520938d
Rob Buis <rbuis@igalia.com>
2023-11-11 02:00:59 -0800

    Add hasUAVisualTransition attribute to popState event https://bugs.webkit.org/show_bug.cgi?id=259793
    Reviewed by Brent Fulgham.
    
    Add hasUAVisualTransition attribute to popState event as specified:
    https://html.spec.whatwg.org/#dom-popstateeventinit-hasuavisualtransition
    
    Also sync history-traversal/PopStateEvent.html *based on 17eb11d8d9) in order to test the attribute initial value.
    
    * LayoutTests/imported/w3c/web-platform-tests/html/browsers/browsing-the-web/history-traversal/PopStateEvent-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/html/browsers/browsing-the-web/history-traversal/PopStateEvent.html:
    * Source/WebCore/dom/PopStateEvent.cpp:
    (WebCore::PopStateEvent::PopStateEvent):
    * Source/WebCore/dom/PopStateEvent.h:
    * Source/WebCore/dom/PopStateEvent.idl:
    
    Canonical link: https://commits.webkit.org/270589@main

--- Source/WebCore/dom/PopStateEvent.cpp
+++ Source/WebCore/dom/PopStateEvent.cpp
@@ -39,6 +39,7 @@ WTF_MAKE_ISO_ALLOCATED_IMPL(PopStateEvent);
 PopStateEvent::PopStateEvent(const AtomString& type, const Init& initializer, IsTrusted isTrusted)
     : Event(type, initializer, isTrusted)
     , m_state(initializer.state)
+    , m_hasUAVisualTransition(initializer.hasUAVisualTransition)
 {
 }
 
