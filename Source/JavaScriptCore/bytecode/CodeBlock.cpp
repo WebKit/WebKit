fd4bac2f23415db83df6794291202caf313112b7
Dan Robson <dtr_bugzilla@apple.com>
2023-11-13 15:49:19 -0800

    Cherry-pick af7c136e799e. rdar://117838992
        Reverting https://commits.webkit.org/267815.345@safari-7617-branch
        https://bugs.webkit.org/show_bug.cgi?id=264767
        rdar://117838992
    
        Reviewed by Michael Saboff.
    
        * JSTests/stress/arrow-function-captured-arguments-aliased.js: Removed.
        * Source/JavaScriptCore/bytecode/CodeBlock.cpp:
        (JSC::CodeBlock::finishCreation):
        * Source/JavaScriptCore/bytecompiler/BytecodeGenerator.cpp:
        (JSC::BytecodeGenerator::BytecodeGenerator):
        * Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp:
        (JSC::DFG::ByteCodeParser::parseBlock):
        * Source/JavaScriptCore/llint/LowLevelInterpreter32_64.asm:
        * Source/JavaScriptCore/llint/LowLevelInterpreter64.asm:
        * Source/JavaScriptCore/runtime/GetPutInfo.h:
        (JSC::initializationModeName):
        (JSC::isInitialization):
        * Source/JavaScriptCore/runtime/ScopedArguments.h:
        * Source/JavaScriptCore/runtime/ScopedArgumentsTable.cpp:
        (JSC::ScopedArgumentsTable::tryCreate):
        (JSC::ScopedArgumentsTable::tryClone):
        (JSC::ScopedArgumentsTable::trySetLength):
        (JSC::ScopedArgumentsTable::trySetWatchpointSet): Deleted.
        * Source/JavaScriptCore/runtime/ScopedArgumentsTable.h:
        * Source/JavaScriptCore/runtime/SymbolTable.cpp:
        (JSC::SymbolTable::localToEntry):
        (JSC::SymbolTable::cloneScopePart):
        * Source/JavaScriptCore/runtime/SymbolTable.h:
    
        Canonical link: https://commits.webkit.org/267815.566@safari-7617-branch
    
    Identifier: 267815.559@safari-7617.1.17.13-branch

--- Source/JavaScriptCore/bytecode/CodeBlock.cpp
+++ Source/JavaScriptCore/bytecode/CodeBlock.cpp
@@ -587,12 +587,7 @@ bool CodeBlock::finishCreation(VM& vm, ScriptExecutable* ownerExecutable, Unlink
                     ConcurrentJSLocker locker(symbolTable->m_lock);
                     auto iter = symbolTable->find(locker, ident.impl());
                     ASSERT(iter != symbolTable->end(locker));
-                    if (bytecode.m_getPutInfo.initializationMode() == InitializationMode::ScopedArgumentInitialization) {
-                        ASSERT(bytecode.m_value.isArgument());
-                        unsigned argumentIndex = bytecode.m_value.toArgument() - 1;
-                        symbolTable->prepareToWatchScopedArgument(iter->value, argumentIndex);
-                    } else
-                        iter->value.prepareToWatch();
+                    iter->value.prepareToWatch();
                     metadata.m_watchpointSet = iter->value.watchpointSet();
                 } else
                     metadata.m_watchpointSet = nullptr;
