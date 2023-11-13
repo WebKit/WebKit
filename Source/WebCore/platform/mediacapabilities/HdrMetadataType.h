279959ad2c2cfe9b4f0dd82bfbe631ac6f56c446
Claudio Saavedra <csaavedra@igalia.com>
2023-11-13 07:36:51 -0800

    Port media capabilities enums to new serialization format https://bugs.webkit.org/show_bug.cgi?id=264679
    Reviewed by Chris Dumez.
    
    Do away with the EnumTraits for these enums and use
    the generator for their IPC serialization.
    
    * Source/WebCore/platform/mediacapabilities/ColorGamut.h:
    * Source/WebCore/platform/mediacapabilities/HdrMetadataType.h:
    * Source/WebCore/platform/mediacapabilities/TransferFunction.h:
    * Source/WebKit/Shared/WebCoreArgumentCoders.serialization.in:
    
    Canonical link: https://commits.webkit.org/270639@main

--- Source/WebCore/platform/mediacapabilities/HdrMetadataType.h
+++ Source/WebCore/platform/mediacapabilities/HdrMetadataType.h
@@ -36,16 +36,3 @@ enum class HdrMetadataType : uint8_t {
 };
 
 } // namespace WebCore
-
-namespace WTF {
-
-template<> struct EnumTraits<WebCore::HdrMetadataType> {
-    using values = EnumValues<
-        WebCore::HdrMetadataType,
-        WebCore::HdrMetadataType::SmpteSt2086,
-        WebCore::HdrMetadataType::SmpteSt209410,
-        WebCore::HdrMetadataType::SmpteSt209440
-    >;
-};
-
-}
