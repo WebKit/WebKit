d818a9114567f258212a26afc3c861439aca2812
Justin Michaud <justin_michaud@apple.com>
2023-11-14 14:46:48 -0800

    Build fix weak linking SECore https://bugs.webkit.org/show_bug.cgi?id=264822 rdar://118404992
    Reviewed by Mark Lam.
    
    We need to tell clang that this symbol can be weak-linked.
    
    * Source/JavaScriptCore/jit/ExecutableAllocator.cpp:
    (JSC::initializeJITPageReservation):
    
    Canonical link: https://commits.webkit.org/270727@main

--- Source/JavaScriptCore/jit/ExecutableAllocator.cpp
+++ Source/JavaScriptCore/jit/ExecutableAllocator.cpp
@@ -83,6 +83,11 @@ extern "C" {
 }
 #endif
 
+#if USE(INLINE_JIT_PERMISSIONS_API)
+#include <wtf/darwin/WeakLinking.h>
+WTF_WEAK_LINK_FORCE_IMPORT(se_memory_inline_jit_restrict_with_witness_supported);
+#endif
+
 namespace JSC {
 
 using namespace WTF;
@@ -412,7 +417,8 @@ static ALWAYS_INLINE JITReservation initializeJITPageReservation()
         bool fastJITPermissionsIsSupported = false;
 #if OS(DARWIN) && CPU(ARM64)
 #if USE(INLINE_JIT_PERMISSIONS_API)
-        fastJITPermissionsIsSupported = (se_memory_inline_jit_restrict_with_witness_supported != nullptr) && !!se_memory_inline_jit_restrict_with_witness_supported();
+        fastJITPermissionsIsSupported = (se_memory_inline_jit_restrict_with_witness_supported != nullptr)
+            && !!se_memory_inline_jit_restrict_with_witness_supported();
 #elif USE(PTHREAD_JIT_PERMISSIONS_API)
         fastJITPermissionsIsSupported = !!pthread_jit_write_protect_supported_np();
 #elif USE(APPLE_INTERNAL_SDK)
