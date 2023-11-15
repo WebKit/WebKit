WebKit/WebKit

commit 7a0271a0a95d61258733c44320a1163335b9dd45

[WGSL] Fix compound assignment for matrix https://bugs.webkit.org/show_bug.cgi?id=264606 rdar://118240280
Reviewed by Mike Wyrzykowski.

MSL doesn't like compound multiplication of matrices. So instead we simplify the
code and merge the implementation of CompoundAssignment and BinaryExpression by
serializing all CompoundAssignments as regular assignments plus a binary expression.
e.g. `a *= b` becomes `a = a * b`

* Source/WebGPU/WGSL/Metal/MetalFunctionWriter.cpp:
(WGSL::Metal::FunctionDefinitionWriter::serializeBinaryExpression):
(WGSL::Metal::FunctionDefinitionWriter::visit):
* Source/WebGPU/WGSL/tests/valid/overload.wgsl:

Canonical link: https://commits.webkit.org/270641@main

Source/WebGPU/WGSL/Metal/MetalFunctionWriter.cpp
@@ -120,6 +120,7 @@ private:
     void generatePackingHelpers(AST::Structure&);
     bool emitPackedVector(const Types::Vector&);
     void serializeConstant(const Type*, ConstantValue);
+    void serializeBinaryExpression(AST::Expression&, AST::BinaryOperation, AST::Expression&);
 
     StringBuilder& m_stringBuilder;
     CallGraph& m_callGraph;
@@ -1668,17 +1669,15 @@ void FunctionDefinitionWriter::visit(AST::UnaryExpression& unary)
     m_stringBuilder.append(")");
 }
 
-void FunctionDefinitionWriter::visit(AST::BinaryExpression& binary)
+void FunctionDefinitionWriter::serializeBinaryExpression(AST::Expression& lhs, AST::BinaryOperation operation, AST::Expression& rhs)
 {
-    bool isDiv = binary.operation() == AST::BinaryOperation::Divide;
-    bool isMod = !isDiv && binary.operation() == AST::BinaryOperation::Modulo;
+    bool isDiv = operation == AST::BinaryOperation::Divide;
+    bool isMod = !isDiv && operation == AST::BinaryOperation::Modulo;
 
     if (isDiv || isMod) {
-        auto* rightType = binary.rightExpression().inferredType();
+        auto* rightType = rhs.inferredType();
         if (auto* vectorType = std::get_if<Types::Vector>(rightType))
             rightType = vectorType->element;
-        if (satisfies(rightType, Constraints::Integer)) {
-        }
 
         const char* helperFunction = nullptr;
         if (satisfies(rightType, Constraints::Integer)) {
@@ -1691,17 +1690,17 @@ void FunctionDefinitionWriter::visit(AST::BinaryExpression& binary)
 
         if (helperFunction) {
             m_stringBuilder.append(helperFunction, "(");
-            visit(binary.leftExpression());
+            visit(lhs);
             m_stringBuilder.append(", ");
-            visit(binary.rightExpression());
+            visit(rhs);
             m_stringBuilder.append(")");
             return;
         }
     }
 
     m_stringBuilder.append("(");
-    visit(binary.leftExpression());
-    switch (binary.operation()) {
+    visit(lhs);
+    switch (operation) {
     case AST::BinaryOperation::Add:
         m_stringBuilder.append(" + ");
         break;
@@ -1760,8 +1759,13 @@ void FunctionDefinitionWriter::visit(AST::BinaryExpression& binary)
         m_stringBuilder.append(" || ");
         break;
     }
-    visit(binary.rightExpression());
+    visit(rhs);
     m_stringBuilder.append(")");
+}
+
+void FunctionDefinitionWriter::visit(AST::BinaryExpression& binary)
+{
+    serializeBinaryExpression(binary.leftExpression(), binary.operation(), binary.rightExpression());
 }
 
 void FunctionDefinitionWriter::visit(AST::PointerDereferenceExpression& pointerDereference)
@@ -1856,37 +1860,8 @@ void FunctionDefinitionWriter::visit(AST::CallStatement& statement)
 
 void FunctionDefinitionWriter::visit(AST::CompoundAssignmentStatement& statement)
 {
-    bool isDiv = statement.operation() == AST::BinaryOperation::Divide;
-    bool isMod = !isDiv && statement.operation() == AST::BinaryOperation::Modulo;
-
-    if (isDiv || isMod) {
-        auto* rightType = statement.rightExpression().inferredType();
-        if (auto* vectorType = std::get_if<Types::Vector>(rightType))
-            rightType = vectorType->element;
-
-        const char* helperFunction = nullptr;
-        if (satisfies(rightType, Constraints::Integer)) {
-            if (isDiv)
-                helperFunction = "__wgslDiv";
-            else
-                helperFunction = "__wgslMod";
-        } else if (isMod)
-            helperFunction = "fmod";
-
-        if (helperFunction) {
-            visit(statement.leftExpression());
-            m_stringBuilder.append(" = ", helperFunction, "(");
-            visit(statement.leftExpression());
-            m_stringBuilder.append(", ");
-            visit(statement.rightExpression());
-            m_stringBuilder.append(")");
-            return;
-        }
-    }
-
-    visit(statement.leftExpression());
-    m_stringBuilder.append(" ", toASCIILiteral(statement.operation()), "= ");
-    visit(statement.rightExpression());
+    m_stringBuilder.append(" = ");
+    serializeBinaryExpression(statement.leftExpression(), statement.operation(), statement.rightExpression());
 }
 
 void FunctionDefinitionWriter::visit(AST::CompoundStatement& statement)

Source/WebGPU/WGSL/tests/valid/overload.wgsl
@@ -141,30 +141,42 @@ fn testAddEq() {
   }
 }
 
-fn testMultiply() {
-  {
+// RUN: %metal-compile testMultiply
+@compute @workgroup_size(1)
+fn testMultiply()
+{
     _ = 0 * 0;
     _ = 0i * 0i;
     _ = 0u * 0u;
     _ = 0.0 * 0.0;
     _ = 0.0f * 0.0f;
-  }
 
-  let v2 = vec2<f32>(0, 0);
-  let v4 = vec4<f32>(0, 0, 0, 0);
-  let m = mat2x4<f32>(0, 0, 0, 0, 0, 0, 0, 0);
-  _ = m * v2;
-  _ = v4 * m;
-  _ = vec2(1, 1) * 1;
-  _ = 1 * vec2(1, 1);
-  _ = vec2(1, 1) * vec2(1, 1);
+    var v2 = vec2<f32>(0, 0);
+    var v4 = vec4<f32>(0, 0, 0, 0);
+    var m = mat2x4<f32>(0, 0, 0, 0, 0, 0, 0, 0);
+    _ = m * v2;
+    _ = v4 * m;
+    _ = vec2(1, 1) * 1;
+    _ = 1 * vec2(1, 1);
+    _ = vec2(1, 1) * vec2(1, 1);
 
-  _ = m * 2;
-  _ = 2 * m;
+    _ = m * 2;
+    _ = 2 * m;
 
-  _ = mat2x2(0, 0, 0, 0) * mat2x2(0, 0, 0, 0);
-  _ = mat2x2(0, 0, 0, 0) * mat3x2(0, 0, 0, 0, 0, 0);
-  _ = mat2x2(0, 0, 0, 0) * mat4x2(0, 0, 0, 0, 0, 0, 0, 0);
+    v2 *= v2;
+    v2 *= 2;
+
+    v4 *= v4;
+    v4 *= 2;
+
+    var m2 = mat2x2<f32>(0, 0, 0, 0);
+    m2 *= m2;
+    // FIXME: this requires type checking compound assignment
+    // m2 *= 2;
+
+    _ = mat2x2(0, 0, 0, 0) * mat2x2(0, 0, 0, 0);
+    _ = mat2x2(0, 0, 0, 0) * mat3x2(0, 0, 0, 0, 0, 0);
+    _ = mat2x2(0, 0, 0, 0) * mat4x2(0, 0, 0, 0, 0, 0, 0, 0);
 }
 
 fn testDivision() {

commit 8f82a0e1711d3a24faaba6302fd1541cbf52be03

[WGSL] Add tests for bitwise operators and fix compound assignment https://bugs.webkit.org/show_bug.cgi?id=264832 rdar://118408447
Reviewed by Mike Wyrzykowski.

Modify the existing tests to also be run through the Metal compiler, and expand the
tests to cover both constant and runtime operations. In the process the tests also
showed that I broke compound assignment in 270641@main, so I fixed that.

* Source/WebGPU/WGSL/Metal/MetalFunctionWriter.cpp:
(WGSL::Metal::FunctionDefinitionWriter::visit):
* Source/WebGPU/WGSL/tests/valid/overload.wgsl:

Canonical link: https://commits.webkit.org/270761@main

Source/WebGPU/WGSL/Metal/MetalFunctionWriter.cpp
@@ -1860,6 +1860,7 @@ void FunctionDefinitionWriter::visit(AST::CallStatement& statement)
 
 void FunctionDefinitionWriter::visit(AST::CompoundAssignmentStatement& statement)
 {
+    visit(statement.leftExpression());
     m_stringBuilder.append(" = ");
     serializeBinaryExpression(statement.leftExpression(), statement.operation(), statement.rightExpression());
 }

Source/WebGPU/WGSL/tests/valid/overload.wgsl
@@ -291,49 +291,97 @@ fn testComparison() {
 
 // 8.9. Bit Expressions (https://www.w3.org/TR/WGSL/#bit-expr)
 
+// RUN: %metal-compile testBitwise
+@compute @workgroup_size(1)
 fn testBitwise()
 {
-  {
-    _ = ~0;
-    _ = ~0i;
-    _ = ~0u;
-  }
+    {
+        var i = 0i;
+        var u = 0u;
+        const x1: u32 = ~(-1);
+        const x2: i32 = ~1i;
+        const x3: u32 = ~1u;
+        const x4: vec2<u32> = ~vec2(-1);
+        const x5: vec2<i32> = ~vec2(0i);
+        const x6: vec2<u32> = ~vec2(0u);
+        let x7: i32 = ~i;
+        let x8: u32 = ~u;
+    }
 
-  {
-    _ = 0 & 1;
-    _ = 0i & 1i;
-    _ = 0u & 1u;
-  }
+    {
+        var i = 0i;
+        var u = 0u;
+        const x1: u32 = 0 & 1;
+        const x2: i32 = 0i & 1i;
+        const x3: u32 = 0u & 1u;
+        const x4: vec2<u32> = vec2(0) & vec2(1);
+        const x5: vec2<i32> = vec2(0i) & vec2(1i);
+        const x6: vec2<u32> = vec2(0u) & vec2(1u);
+        let x7: i32 = i & 1;
+        let x8: u32 = u & 1;
+        i &= 1;
+        u &= 1;
+    }
 
-  {
-    _ = 0 | 1;
-    _ = 0i | 1i;
-    _ = 0u | 1u;
-  }
+    {
+        var i = 0i;
+        var u = 0u;
+        const x1: u32 = 0 | 1;
+        const x2: i32 = 0i | 1i;
+        const x3: u32 = 0u | 1u;
+        const x4: vec2<u32> = vec2(0) | vec2(1);
+        const x5: vec2<i32> = vec2(0i) | vec2(1);
+        const x6: vec2<u32> = vec2(0u) | vec2(1);
+        let x7: i32 = i | 1;
+        let x8: u32 = u | 1;
+        i |= 1;
+        u |= 1;
+    }
 
-  {
-    _ = 0 ^ 1;
-    _ = 0i ^ 1i;
-    _ = 0u ^ 1u;
-  }
+    {
+        var i = 0i;
+        var u = 0u;
+        const x1: u32 = 0 ^ 1;
+        const x2: i32 = 0i ^ 1i;
+        const x3: u32 = 0u ^ 1u;
+        const x4: vec2<u32> = vec2(0) ^ vec2(1);
+        const x5: vec2<i32> = vec2(0i) ^ vec2(1i);
+        const x6: vec2<u32> = vec2(0u) ^ vec2(1u);
+        let x7: i32 = i ^ 1;
+        let x8: u32 = u ^ 1;
+        i ^= 1;
+        u ^= 1;
+    }
 
-  {
-    const x: u32 = 1 << 2;
-    _ = 1i << 2u;
-    _ = 1u << 2u;
-    _ = vec2(1) << vec2(2);
-    _ = vec2(1i) << vec2(2u);
-    _ = vec2(1u) << vec2(2u);
-  }
+    {
+        var i = 0i;
+        var u = 0u;
+        const x1: u32 = 1 << 2;
+        const x2: i32 = 1i << 2u;
+        const x3: u32 = 1u << 2u;
+        const x4: vec2<u32> = vec2(1) << vec2(2);
+        const x5: vec2<i32> = vec2(1i) << vec2(2u);
+        const x6: vec2<u32> = vec2(1u) << vec2(2u);
+        let x7: i32 = i << 1;
+        let x8: u32 = u << 1;
+        i <<= 1;
+        u <<= 1;
+    }
 
-  {
-    const x: u32 = 1 >> 2;
-    _ = 1i >> 2u;
-    _ = 1u >> 2u;
-    _ = vec2(1) >> vec2(2);
-    _ = vec2(1i) >> vec2(2u);
-    _ = vec2(1u) >> vec2(2u);
-  }
+    {
+        var i = 0i;
+        var u = 0u;
+        const x: u32 = 1 >> 2;
+        const x2: i32 = 1i >> 2u;
+        const x3: u32 = 1u >> 2u;
+        const x4: vec2<u32> = vec2(1) >> vec2(2);
+        const x5: vec2<i32> = vec2(1i) >> vec2(2u);
+        const x6: vec2<u32> = vec2(1u) >> vec2(2u);
+        let x7: i32 = i >> 1;
+        let x8: u32 = u >> 1;
+        i >>= 1;
+        u >>= 1;
+    }
 }
 
 // 8.13. Address-Of Expression (https://www.w3.org/TR/WGSL/#address-of-expr)