31c70df9a06598ff2c84861112be7eece6bbb4a3
Karl Dubost <karlcow@apple.com>
2023-11-22 02:42:07 -0800

    Cherry-pick 271049@main (45154fac42da). https://bugs.webkit.org/show_bug.cgi?id=265231
        Remove Quirk shouldDispatchSimulatedMouseEvents() for airtable.com
        https://bugs.webkit.org/show_bug.cgi?id=265231
        rdar://118707570
    
        Reviewed by Antti Koivisto.
    
        This quirk was created for airtable to make it possible to navigate
        tables. It appears the Quirk is not necessary anymore. The steps to
        reproduce are described in the bug.
        Initial bug
        ​https://bugs.webkit.org/show_bug.cgi?id=201456
        <rdar://problem/51557377>
    
        * Source/WebCore/page/Quirks.cpp:
        (WebCore::Quirks::simulatedMouseEventTypeForTarget const):
    
        Canonical link: https://commits.webkit.org/271049@main

--- Source/WebCore/page/Quirks.cpp
+++ Source/WebCore/page/Quirks.cpp
@@ -477,7 +477,6 @@ bool Quirks::shouldDispatchedSimulatedMouseEventsAssumeDefaultPrevented(EventTar
 
 // maps.google.com https://bugs.webkit.org/show_bug.cgi?id=199904
 // desmos.com rdar://50925173
-// airtable.com rdar://51557377
 std::optional<Event::IsCancelable> Quirks::simulatedMouseEventTypeForTarget(EventTarget* target) const
 {
     if (!shouldDispatchSimulatedMouseEvents(target))
@@ -492,18 +491,6 @@ std::optional<Event::IsCancelable> Quirks::simulatedMouseEventTypeForTarget(Even
 
     if (isDomain("desmos.com"_s))
         return Event::IsCancelable::No;
-
-    if (isDomain("airtable.com"_s)) {
-        // We want to limit simulated mouse events to elements under <div id="paneContainer"> to allow for column re-ordering and multiple cell selection.
-        if (is<Node>(target)) {
-            auto* node = downcast<Node>(target);
-            if (auto* paneContainer = node->treeScope().getElementById(AtomString("paneContainer"_s))) {
-                if (paneContainer->contains(node))
-                    return Event::IsCancelable::Yes;
-            }
-        }
-        return { };
-    }
 
     return Event::IsCancelable::Yes;
 }
