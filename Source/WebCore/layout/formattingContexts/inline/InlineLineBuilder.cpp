93db3d3b51385837c58ee23e605c8c8ccbdcfbea
Myah Cobbs <mcobbs@apple.com>
2023-11-27 15:40:35 -0500

    Cherry-pick b741d4c7aa56. rdar://118701491
        (REGRESSION: 267587@main) Text indent is incorrect when paragraph is affected by a floating element
        https://bugs.webkit.org/show_bug.cgi?id=265216
        <rdar://problem/118701491>
    
        Reviewed by Antti Koivisto.
    
        Before 267587@main (which merged all the float constraints codepaths), we ignored the text-indent during the _initial_ constraint computation
        and adjusted the line rect later.
        At 267587@main, we started adding the text-indent value twice to the line rect; first while computing the initial constraint and
        the second time when we would normally add it.
    
        * LayoutTests/fast/text/text-indent-with-intrusive-float-expected.html: Added.
        * LayoutTests/fast/text/text-indent-with-intrusive-float.html: Added.
        * Source/WebCore/layout/formattingContexts/inline/InlineLineBuilder.cpp:
        (WebCore::Layout::LineBuilder::initialConstraintsForLine const): Let's go back to "before 267587@main" state.
        (WebCore::Layout::LineBuilder::floatConstrainedRect const): Add some comment and change variable names to reflect what they mean.
    
        Canonical link: https://commits.webkit.org/271080@main
    
    Identifier: 267815.582@safari-7617-branch

--- Source/WebCore/layout/formattingContexts/inline/InlineLineBuilder.cpp
+++ Source/WebCore/layout/formattingContexts/inline/InlineLineBuilder.cpp
@@ -493,9 +493,10 @@ LineContent LineBuilder::placeInlineAndFloatContent(const InlineItemRange& needs
 
 LineBuilder::UsedConstraints LineBuilder::initialConstraintsForLine(const InlineRect& initialLineLogicalRect, std::optional<bool> previousLineEndsWithLineBreak) const
 {
-    auto isIntrinsicWidthMode = isInIntrinsicWidthMode() ? InlineFormattingGeometry::IsIntrinsicWidthMode::Yes : InlineFormattingGeometry::IsIntrinsicWidthMode::No;
-    auto textIndent = formattingContext().formattingGeometry().computedTextIndent(isIntrinsicWidthMode, previousLineEndsWithLineBreak, initialLineLogicalRect.width());
-    return floatConstrainedRect(initialLineLogicalRect, textIndent);
+    auto isIntrinsicWidthMode = isInIntrinsicWidthMode() ? InlineFormattingUtils::IsIntrinsicWidthMode::Yes : InlineFormattingUtils::IsIntrinsicWidthMode::No;
+    auto constraints = floatConstrainedRect(initialLineLogicalRect, { });
+    constraints.marginStart = formattingContext().formattingUtils().computedTextIndent(isIntrinsicWidthMode, previousLineEndsWithLineBreak, initialLineLogicalRect.width());
+    return constraints;
 }
 
 InlineLayoutUnit LineBuilder::leadingPunctuationWidthForLineCandiate(size_t firstInlineTextItemIndex, size_t candidateContentStartIndex) const
@@ -707,21 +708,27 @@ LineBuilder::UsedConstraints LineBuilder::floatConstrainedRect(const InlineRect&
     if (!constraints.left && !constraints.right)
         return { logicalRect, marginStart, { } };
 
-    auto isConstrainedByFloat = OptionSet<UsedFloat> { };
-    auto adjustedLogicalRect = logicalRect;
-    adjustedLogicalRect.shiftLeftBy(-marginStart);
+        auto isConstrainedByFloat = OptionSet<UsedFloat> { };
+        // text-indent acts as (start)margin on the line. When looking for intrusive floats we need to check against the line's _margin_ box.
+        auto marginBoxRect = InlineRect { logicalRect.top(), logicalRect.left() - marginStart, logicalRect.width() + marginStart, logicalRect.height() };
 
-    if (constraints.left && constraints.left->x > adjustedLogicalRect.left()) {
-        adjustedLogicalRect.shiftLeftTo(constraints.left->x);
-        isConstrainedByFloat.add(UsedFloat::Left);
-    }
-    if (constraints.right && constraints.right->x < adjustedLogicalRect.right()) {
-        adjustedLogicalRect.setRight(std::max<InlineLayoutUnit>(adjustedLogicalRect.left(), constraints.right->x));
-        isConstrainedByFloat.add(UsedFloat::Right);
-    }
+        if (constraints.left && constraints.left->x > marginBoxRect.left()) {
+            marginBoxRect.shiftLeftTo(constraints.left->x);
+            isConstrainedByFloat.add(UsedFloat::Left);
+        }
+        if (constraints.right && constraints.right->x < marginBoxRect.right()) {
+            marginBoxRect.setRight(std::max<InlineLayoutUnit>(marginBoxRect.left(), constraints.right->x));
+            isConstrainedByFloat.add(UsedFloat::Right);
+        }
 
-    adjustedLogicalRect.shiftLeftBy(marginStart);
-    return { adjustedLogicalRect, marginStart, isConstrainedByFloat };
+        auto lineLogicalRect = InlineRect { marginBoxRect.top(), marginBoxRect.left() + marginStart, marginBoxRect.width() - marginStart, marginBoxRect.height() };
+        return { lineLogicalRect, marginStart, isConstrainedByFloat };
+    }();
+
+    if (auto adjustedRect = formattingContext().quirks().adjustedRectForLineGridLineAlign(constraints.logicalRect))
+        constraints.logicalRect = *adjustedRect;
+
+    return constraints;
 }
 
 LineBuilder::UsedConstraints LineBuilder::adjustedLineRectWithCandidateInlineContent(const LineCandidate& lineCandidate) const
