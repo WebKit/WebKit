dab6e63f7e12a6699f1d13913e25a08e117f95c2
Kimmo Kinnunen <kkinnunen@apple.com>
2023-11-28 07:53:03 -0800

    Cherry-pick 267815.586@safari-7617-branch (e408ece1b8a7). https://bugs.webkit.org/show_bug.cgi?id=265129
    GraphicsContextGLCocoa::setDrawingBufferColorSpace is missing makeContextCurrent()
    https://bugs.webkit.org/show_bug.cgi?id=265129
    rdar://118650182
    
    Reviewed by Dan Glastonbury.
    
    All public API entrypoints in GraphicsContextGLANGLE should call
    the makeContextCurrent().
    
    * Source/WebCore/platform/graphics/cocoa/GraphicsContextGLCocoa.mm:
    (WebCore::GraphicsContextGLCocoa::setDrawingBufferColorSpace):
    
    Canonical link: https://commits.webkit.org/267815.586@safari-7617-branch

--- Source/WebCore/platform/graphics/cocoa/GraphicsContextGLCocoa.mm
+++ Source/WebCore/platform/graphics/cocoa/GraphicsContextGLCocoa.mm
@@ -565,6 +565,8 @@ bool GraphicsContextGLCocoa::reshapeDrawingBuffer()
 
 void GraphicsContextGLCocoa::setDrawingBufferColorSpace(const DestinationColorSpace& colorSpace)
 {
+    if (!makeContextCurrent())
+        return;
     if (m_drawingBufferColorSpace == colorSpace)
         return;
     m_drawingBufferColorSpace = colorSpace;
