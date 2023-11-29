cf369d64d55e80cdb954cfabfbdb0aef31452f5f
Dan Glastonbury <djg@apple.com>
2023-11-29 13:10:37 +1030

    [WebIDL] Fine grain control of generated name for enumeration values https://bugs.webkit.org/show_bug.cgi?id=265499 rdar://118909721
    Reviewed by NOBODY (OOPS!).
    
    JS binding generation tries to generate C++ enum value names by converting kebab
    case into Pascal case. When this breaks down, adhoc fix ups are applied by
    `WK_ucfirst` and `WK_lcfirst`. These are global and non-specifc.
    
    To allow finer grain control of the name generation process, this patch extends
    IDLParser to accept the attribute `[ImplementedAs]` before each enumeration
    value name. When present, this attribute overrides the exists logic, using the
    name as specified in the attribute definition.
    
    * Source/WebCore/bindings/scripts/CodeGeneratorJS.pm:
    (GetEnumerationValueName):
    (GenerateEnumerationImplementationContent):
    (GenerateDefaultValue):
    * Source/WebCore/bindings/scripts/IDLAttributes.json:
    * Source/WebCore/bindings/scripts/IDLParser.pm:
    (parseEnumValueList):
    (parseEnumValues):
    * Source/WebCore/bindings/scripts/test/JS/JSTestEnumerationImplementedAsValue.cpp: Added.
    (WebCore::convertEnumerationToString):
    (WebCore::convertEnumerationToJS):
    (WebCore::parseEnumerationFromString<TestEnumerationImplementedAsValue>):
    (WebCore::parseEnumeration<TestEnumerationImplementedAsValue>):
    (WebCore::expectedEnumerationValues<TestEnumerationImplementedAsValue>):
    * Source/WebCore/bindings/scripts/test/JS/JSTestEnumerationImplementedAsValue.h: Added.
    * Source/WebCore/bindings/scripts/test/SupplementalDependencies.dep:
    * Source/WebCore/bindings/scripts/test/TestEnumerationImplementedAsValue.idl: Added.

--- Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -2435,8 +2435,15 @@ sub GetEnumerationClassName
 
 sub GetEnumerationValueName
 {
-    my ($name) = @_;
+    my ($value) = @_;
 
+    assert("Not an enum value") if ref($value) ne "IDLEnumValue";
+
+    if ($value->extendedAttributes->{ImplementedAs}) {
+        return $value->extendedAttributes->{ImplementedAs};
+    }
+
+    my $name = $value->name;
     return "EmptyString" if $name eq "";
     return "WebRTC" if $name eq "webrtc";
     $name = join("", map { $codeGenerator->WK_ucfirst($_) } split("-", $name));
@@ -2496,10 +2503,11 @@ sub GenerateEnumerationImplementationContent
     AddToImplIncludes("<wtf/NeverDestroyed.h>");
     $result .= "    static const NeverDestroyed<String> values[] = {\n";
     foreach my $value (@{$enumeration->values}) {
-        if ($value eq "") {
+        my $name = $value->name;
+        if ($name eq "") {
             $result .= "        emptyString(),\n";
         } else {
-            $result .= "        MAKE_STATIC_STRING_IMPL(\"$value\"),\n";
+            $result .= "        MAKE_STATIC_STRING_IMPL(\"$name\"),\n";
         }
     }
     $result .= "    };\n";
@@ -2523,8 +2531,8 @@ sub GenerateEnumerationImplementationContent
     # FIXME: Consider finding a more efficient way to match against all the strings quickly.
     $result .= "template<> std::optional<$className> parseEnumerationFromString<${className}>(const String& stringValue)\n";
     $result .= "{\n";
-    my @sortedEnumerationValues = sort @{$enumeration->values};
-    if ($sortedEnumerationValues[0] eq "") {
+    my @sortedEnumerationValues = sort { $a->name cmp $b->name } @{$enumeration->values};
+    if ($sortedEnumerationValues[0]->name eq "") {
         $result .= "    if (stringValue.isEmpty())\n";
         my $enumerationValueName = GetEnumerationValueName(shift(@sortedEnumerationValues));
         $result .= "        return ${className}::$enumerationValueName;\n";
@@ -2532,7 +2540,8 @@ sub GenerateEnumerationImplementationContent
     $result .= "    static constexpr std::pair<ComparableASCIILiteral, $className> mappings[] = {\n";
     for my $value (@sortedEnumerationValues) {
         my $enumerationValueName = GetEnumerationValueName($value);
-        $result .= "        { \"$value\", ${className}::$enumerationValueName },\n";
+        my $name = $value->name;
+        $result .= "        { \"$name\", ${className}::$enumerationValueName },\n";
     }
     $result .= "    };\n";
     $result .= "    static constexpr SortedArrayMap enumerationMapping { mappings };\n";
@@ -2548,7 +2557,7 @@ sub GenerateEnumerationImplementationContent
 
     $result .= "template<> const char* expectedEnumerationValues<$className>()\n";
     $result .= "{\n";
-    $result .= "    return \"\\\"" . join ("\\\", \\\"", @{$enumeration->values}) . "\\\"\";\n";
+    $result .= "    return \"\\\"" . join ("\\\", \\\"", map { $_->name } @{$enumeration->values}) . "\\\"\";\n";
     $result .= "}\n\n";
 
     $result .= "#endif\n\n" if $conditionalString;
@@ -2672,9 +2681,19 @@ sub GenerateDefaultValue
             }
         }
 
-        if ($codeGenerator->IsEnumType($type)) {
+        my $enumeration = $codeGenerator->GetEnumByType($type);
+        if (defined($enumeration)) {
             my $className = GetEnumerationClassName($type, $typeScope);
-            my $enumerationValueName = GetEnumerationValueName(substr($defaultValue, 1, -1));
+            my $enumValue = IDLEnumValue->new();
+            $enumValue->name(substr($defaultValue, 1, -1));
+            # Look up the value definition, replacing the default if found
+            foreach my $value (@{$enumeration->values}) {
+                if ($value->name eq $enumValue->name) {
+                    $enumValue = $value;
+                    last;
+                }
+            }
+            my $enumerationValueName = GetEnumerationValueName($enumValue);
             return $className . "::" . $enumerationValueName;
         }
     }
