f8209111eace23050256e1f89d66cc8fb5dc962b
Tim Horton <timothy_horton@apple.com>
2023-12-01 08:56:16 -0800

    Add a MiniBrowser menu item to dump a WKWebView image snapshot https://bugs.webkit.org/show_bug.cgi?id=265629
    Reviewed by Aditya Keerthi.
    
    * Tools/MiniBrowser/mac/BrowserWindowController.h:
    * Tools/MiniBrowser/mac/BrowserWindowController.m:
    (-[BrowserWindowController saveAsImage:]):
    * Tools/MiniBrowser/mac/MainMenu.xib:
    * Tools/MiniBrowser/mac/WK1BrowserWindowController.m:
    (-[WK1BrowserWindowController validateMenuItem:]):
    * Tools/MiniBrowser/mac/WK2BrowserWindowController.m:
    (-[WK2BrowserWindowController validateMenuItem:]):
    (-[WK2BrowserWindowController saveAsPDF:]):
    (-[WK2BrowserWindowController saveAsImage:]):
    (-[WK2BrowserWindowController saveAsWebArchive:]):
    Add a menu item for takeSnapshot like we have for createPDF and createWebArchive.
    
    Canonical link: https://commits.webkit.org/271383@main

--- Tools/MiniBrowser/mac/WK2BrowserWindowController.m
+++ Tools/MiniBrowser/mac/WK2BrowserWindowController.m
@@ -267,6 +267,8 @@ static BOOL areEssentiallyEqual(double a, double b)
 
     if (action == @selector(saveAsPDF:))
         return YES;
+    if (action == @selector(saveAsImage:))
+        return YES;
     if (action == @selector(saveAsWebArchive:))
         return YES;
 
@@ -975,11 +977,25 @@ static BOOL isJavaScriptURL(NSURL *url)
     panel.allowedContentTypes = @[ UTTypePDF ];
 
     [panel beginSheetModalForWindow:self.window completionHandler:^(NSInteger result) {
-        if (result == NSModalResponseOK) {
-            [self->_webView createPDFWithConfiguration:nil completionHandler:^(NSData *pdfSnapshotData, NSError *error) {
-                [pdfSnapshotData writeToURL:[panel URL] options:0 error:nil];
-            }];
-        }
+        if (result != NSModalResponseOK)
+            return;
+        [self->_webView createPDFWithConfiguration:nil completionHandler:^(NSData *pdfSnapshotData, NSError *error) {
+            [pdfSnapshotData writeToURL:[panel URL] options:0 error:nil];
+        }];
+    }];
+}
+
+- (IBAction)saveAsImage:(id)sender
+{
+    NSSavePanel *panel = [NSSavePanel savePanel];
+    panel.allowedContentTypes = @[ UTTypeTIFF ];
+
+    [panel beginSheetModalForWindow:self.window completionHandler:^(NSInteger result) {
+        if (result != NSModalResponseOK)
+            return;
+        [self->_webView takeSnapshotWithConfiguration:nil completionHandler:^(NSImage *snapshot, NSError *error) {
+            [snapshot.TIFFRepresentation writeToURL:[panel URL] options:0 error:nil];
+        }];
     }];
 }
 
@@ -989,11 +1005,11 @@ static BOOL isJavaScriptURL(NSURL *url)
     panel.allowedContentTypes = @[ UTTypeWebArchive ];
 
     [panel beginSheetModalForWindow:self.window completionHandler:^(NSInteger result) {
-        if (result == NSModalResponseOK) {
-            [self->_webView createWebArchiveDataWithCompletionHandler:^(NSData *archiveData, NSError *error) {
-                [archiveData writeToURL:[panel URL] options:0 error:nil];
-            }];
-        }
+        if (result != NSModalResponseOK)
+            return;
+        [self->_webView createWebArchiveDataWithCompletionHandler:^(NSData *archiveData, NSError *error) {
+            [archiveData writeToURL:[panel URL] options:0 error:nil];
+        }];
     }];
 }
 
