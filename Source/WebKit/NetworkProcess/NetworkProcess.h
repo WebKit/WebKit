676f48d6af5f4b9101395ad4ee76f64d23317e18
Per Arne Vollan <pvollan@apple.com>
2023-12-04 08:03:13 -0800

    Move some WebKit code to WebKit framework https://bugs.webkit.org/show_bug.cgi?id=265324 rdar://118776213
    Reviewed by Brent Fulgham.
    
    Move some WebKit code related to WebKit process extensions to WebKit framework. This change enables
    us to use new assertion API to take out an assertion on the Networking process when holding locked
    files, which is also included in this patch.
    
    * Source/WebKit/Configurations/BaseExtension.xcconfig:
    * Source/WebKit/Configurations/WebKit.xcconfig:
    * Source/WebKit/Modules/iOS_Private.modulemap:
    * Source/WebKit/NetworkProcess/NetworkProcess.cpp:
    (WebKit::NetworkProcess::setIsHoldingLockedFiles):
    * Source/WebKit/NetworkProcess/NetworkProcess.h:
    * Source/WebKit/NetworkProcess/cocoa/NetworkProcessCocoa.mm:
    (WebKit::NetworkProcess::aqcuireLockedFileGrant):
    (WebKit::NetworkProcess::invalidateGrant):
    (WebKit::NetworkProcess::hasAcquiredGrant const):
    * Source/WebKit/Shared/AuxiliaryProcessExtensions/AuxiliaryProcessExtensionBridge.h:
    * Source/WebKit/Shared/AuxiliaryProcessExtensions/NetworkingProcessExtension.swift:
    (Grant.invalidate):
    (NetworkingProcessExtension.grant(_:name:)):
    * Source/WebKit/WebKit.xcodeproj/project.pbxproj:
    
    Canonical link: https://commits.webkit.org/271478@main

--- Source/WebKit/NetworkProcess/NetworkProcess.h
+++ Source/WebKit/NetworkProcess/NetworkProcess.h
@@ -71,6 +71,10 @@
 typedef struct OpaqueCFHTTPCookieStorage*  CFHTTPCookieStorageRef;
 #endif
 
+#if USE(EXTENSIONKIT)
+OBJC_CLASS WKGrant;
+#endif
+
 namespace IPC {
 class FormDataReference;
 }
@@ -523,6 +527,11 @@ private:
 
 #if USE(RUNNINGBOARD)
     void setIsHoldingLockedFiles(bool);
+#if USE(EXTENSIONKIT)
+    bool aqcuireLockedFileGrant();
+    void invalidateGrant();
+    bool hasAcquiredGrant() const;
+#endif
 #endif
     void stopRunLoopIfNecessary();
 
@@ -559,6 +568,9 @@ private:
 
 #if USE(RUNNINGBOARD)
     WebSQLiteDatabaseTracker m_webSQLiteDatabaseTracker;
+#if USE(EXTENSIONKIT)
+    RetainPtr<WKGrant> m_holdingLockedFileGrant;
+#endif
     RefPtr<ProcessAssertion> m_holdingLockedFileAssertion;
 #endif
     
