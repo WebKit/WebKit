WebKit/WebKit

commit 0c5f458562e932b7de3f6e78471f8b727b92dd79

NetworkLoad should pass redirect completion handler to client https://bugs.webkit.org/show_bug.cgi?id=265407 rdar://112410538
Reviewed by Youenn Fablet.

This re-merges 266246@main and 266608@main, both of which were reverted to fix a perf regression.
I believe I have resolved this by not making a strong reference in the lambda I create when we
send Messages::WebResourceLoader::WillSendRequest.  The WeakPtr makes this not extend the lifetime
of the NetworkResourceLoader like the previous attempt did.
I also verified I did not re-introduce rdar://113568756.

* Source/WebKit/NetworkProcess/Downloads/DownloadManager.cpp:
(WebKit::DownloadManager::continueWillSendRequest): Deleted.
* Source/WebKit/NetworkProcess/Downloads/DownloadManager.h:
* Source/WebKit/NetworkProcess/Downloads/PendingDownload.cpp:
(WebKit::PendingDownload::willSendRedirectedRequest):
(WebKit::PendingDownload::continueWillSendRequest): Deleted.
* Source/WebKit/NetworkProcess/Downloads/PendingDownload.h:
* Source/WebKit/NetworkProcess/NetworkLoad.cpp:
(WebKit::NetworkLoad::~NetworkLoad):
(WebKit::NetworkLoad::willPerformHTTPRedirection):
(WebKit::NetworkLoad::continueWillSendRequest): Deleted.
* Source/WebKit/NetworkProcess/NetworkLoad.h:
* Source/WebKit/NetworkProcess/NetworkLoadClient.h:
* Source/WebKit/NetworkProcess/NetworkProcess.cpp:
(WebKit::NetworkProcess::continueWillSendRequest): Deleted.
* Source/WebKit/NetworkProcess/NetworkProcess.h:
* Source/WebKit/NetworkProcess/NetworkProcess.messages.in:
* Source/WebKit/NetworkProcess/NetworkResourceLoader.cpp:
(WebKit::NetworkResourceLoader::startRequest):
(WebKit::NetworkResourceLoader::willSendRedirectedRequest):
(WebKit::NetworkResourceLoader::willSendServiceWorkerRedirectedRequest):
(WebKit::NetworkResourceLoader::willSendRedirectedRequestInternal):
(WebKit::NetworkResourceLoader::continueWillSendRedirectedRequest):
(WebKit::NetworkResourceLoader::restartNetworkLoad):
(WebKit::NetworkResourceLoader::continueWillSendRequest):

Source/WebKit/NetworkProcess/cache/NetworkCacheSpeculativeLoad.cpp
@@ -88,7 +88,7 @@ void SpeculativeLoad::cancel()
     m_completionHandler(nullptr);
 }
 
-void SpeculativeLoad::willSendRedirectedRequest(ResourceRequest&& request, ResourceRequest&& redirectRequest, ResourceResponse&& redirectResponse)
+void SpeculativeLoad::willSendRedirectedRequest(ResourceRequest&& request, ResourceRequest&& redirectRequest, ResourceResponse&& redirectResponse, CompletionHandler<void(WebCore::ResourceRequest&&)>&& completionHandler)
 {
     LOG(NetworkCacheSpeculativePreloading, "Speculative redirect %s -> %s", request.url().string().utf8().data(), redirectRequest.url().string().utf8().data());
 
@@ -101,6 +101,7 @@ void SpeculativeLoad::willSendRedirectedRequest(ResourceRequest&& request, Resou
         m_cacheEntry = m_cache->makeRedirectEntry(request, redirectResponse, redirectRequest);
 
     // Don't follow the redirect. The redirect target will be registered for speculative load when it is loaded.
+    completionHandler({ });
     didComplete();
 }
 

Source/WebKit/NetworkProcess/Downloads/DownloadManager.cpp
@@ -77,14 +77,6 @@ void DownloadManager::dataTaskBecameDownloadTask(DownloadID downloadID, std::uni
     m_downloads.add(downloadID, WTFMove(download));
 }
 
-void DownloadManager::continueWillSendRequest(DownloadID downloadID, WebCore::ResourceRequest&& request)
-{
-    auto* pendingDownload = m_pendingDownloads.get(downloadID);
-    ASSERT(pendingDownload);
-    if (pendingDownload)
-        pendingDownload->continueWillSendRequest(WTFMove(request));
-}
-
 void DownloadManager::convertNetworkLoadToDownload(DownloadID downloadID, std::unique_ptr<NetworkLoad>&& networkLoad, ResponseCompletionHandler&& completionHandler, Vector<RefPtr<WebCore::BlobDataFileReference>>&& blobFileReferences, const ResourceRequest& request, const ResourceResponse& response)
 {
     ASSERT(!m_pendingDownloads.contains(downloadID));

Source/WebKit/NetworkProcess/Downloads/DownloadManager.h
@@ -84,7 +84,6 @@ public:
 
     void startDownload(PAL::SessionID, DownloadID, const WebCore::ResourceRequest&, const std::optional<WebCore::SecurityOriginData>& topOrigin, std::optional<NavigatingToAppBoundDomain>, const String& suggestedName = { });
     void dataTaskBecameDownloadTask(DownloadID, std::unique_ptr<Download>&&);
-    void continueWillSendRequest(DownloadID, WebCore::ResourceRequest&&);
     void convertNetworkLoadToDownload(DownloadID, std::unique_ptr<NetworkLoad>&&, ResponseCompletionHandler&&,  Vector<RefPtr<WebCore::BlobDataFileReference>>&&, const WebCore::ResourceRequest&, const WebCore::ResourceResponse&);
     void downloadDestinationDecided(DownloadID, Ref<NetworkDataTask>&&);
 

Source/WebKit/NetworkProcess/Downloads/PendingDownload.cpp
@@ -63,15 +63,10 @@ PendingDownload::PendingDownload(IPC::Connection* parentProcessConnection, std::
     m_networkLoad->convertTaskToDownload(*this, request, response, WTFMove(completionHandler));
 }
 
-void PendingDownload::willSendRedirectedRequest(WebCore::ResourceRequest&&, WebCore::ResourceRequest&& redirectRequest, WebCore::ResourceResponse&& redirectResponse)
+void PendingDownload::willSendRedirectedRequest(WebCore::ResourceRequest&&, WebCore::ResourceRequest&& redirectRequest, WebCore::ResourceResponse&& redirectResponse, CompletionHandler<void(WebCore::ResourceRequest&&)>&& completionHandler)
 {
-    send(Messages::DownloadProxy::WillSendRequest(WTFMove(redirectRequest), WTFMove(redirectResponse)));
+    sendWithAsyncReply(Messages::DownloadProxy::WillSendRequest(WTFMove(redirectRequest), WTFMove(redirectResponse)), WTFMove(completionHandler));
 };
-
-void PendingDownload::continueWillSendRequest(WebCore::ResourceRequest&& newRequest)
-{
-    m_networkLoad->continueWillSendRequest(WTFMove(newRequest));
-}
 
 void PendingDownload::cancel(CompletionHandler<void(const IPC::DataReference&)>&& completionHandler)
 {

Source/WebKit/NetworkProcess/Downloads/PendingDownload.h
@@ -52,7 +52,6 @@ public:
     PendingDownload(IPC::Connection*, NetworkLoadParameters&&, DownloadID, NetworkSession&, const String& suggestedName);
     PendingDownload(IPC::Connection*, std::unique_ptr<NetworkLoad>&&, ResponseCompletionHandler&&, DownloadID, const WebCore::ResourceRequest&, const WebCore::ResourceResponse&);
 
-    void continueWillSendRequest(WebCore::ResourceRequest&&);
     void cancel(CompletionHandler<void(const IPC::DataReference&)>&&);
 
 #if PLATFORM(COCOA)
@@ -65,7 +64,7 @@ private:
     void didSendData(uint64_t bytesSent, uint64_t totalBytesToBeSent) override { }
     bool isSynchronous() const override { return false; }
     bool isAllowedToAskUserForCredentials() const final { return m_isAllowedToAskUserForCredentials; }
-    void willSendRedirectedRequest(WebCore::ResourceRequest&&, WebCore::ResourceRequest&& redirectRequest, WebCore::ResourceResponse&& redirectResponse) override;
+    void willSendRedirectedRequest(WebCore::ResourceRequest&&, WebCore::ResourceRequest&& redirectRequest, WebCore::ResourceResponse&& redirectResponse, CompletionHandler<void(WebCore::ResourceRequest&&)>&&) override;
     void didReceiveResponse(WebCore::ResourceResponse&&, PrivateRelayed, ResponseCompletionHandler&&) override;
     void didReceiveBuffer(const WebCore::FragmentedSharedBuffer&, uint64_t reportedEncodedDataLength) override { };
     void didFinishLoading(const WebCore::NetworkLoadMetrics&) override { };

Source/WebKit/NetworkProcess/NetworkLoad.cpp
@@ -30,6 +30,7 @@
 #include "AuthenticationManager.h"
 #include "MessageSenderInlines.h"
 #include "NetworkDataTaskBlob.h"
+#include "NetworkLoadClient.h"
 #include "NetworkLoadScheduler.h"
 #include "NetworkProcess.h"
 #include "NetworkProcessProxyMessages.h"
@@ -84,8 +85,6 @@ NetworkLoad::~NetworkLoad()
     ASSERT(RunLoop::isMain());
     if (m_scheduler)
         m_scheduler->unschedule(*this);
-    if (m_redirectCompletionHandler)
-        m_redirectCompletionHandler({ });
     if (m_task)
         m_task->clearClient();
 }
@@ -117,24 +116,6 @@ void NetworkLoad::reprioritizeRequest(ResourceLoadPriority priority)
     m_currentRequest.setPriority(priority);
     if (m_task)
         m_task->setPriority(priority);
-}
-
-void NetworkLoad::continueWillSendRequest(WebCore::ResourceRequest&& newRequest)
-{
-    updateRequest(m_currentRequest, newRequest);
-
-    auto redirectCompletionHandler = std::exchange(m_redirectCompletionHandler, nullptr);
-    ASSERT(redirectCompletionHandler);
-    if (m_currentRequest.isNull()) {
-        NetworkLoadMetrics emptyMetrics;
-        didCompleteWithError(cancelledError(m_currentRequest), emptyMetrics);
-        if (redirectCompletionHandler)
-            redirectCompletionHandler({ });
-        return;
-    }
-
-    if (redirectCompletionHandler)
-        redirectCompletionHandler(ResourceRequest(m_currentRequest));
 }
 
 bool NetworkLoad::shouldCaptureExtraNetworkLoadMetrics() const
@@ -187,7 +168,6 @@ void NetworkLoad::willPerformHTTPRedirection(ResourceResponse&& redirectResponse
 {
     ASSERT(!redirectResponse.isNull());
     ASSERT(RunLoop::isMain());
-    ASSERT(!m_redirectCompletionHandler);
 
     if (!m_networkProcess->ftpEnabled() && request.url().protocolIsInFTPFamily()) {
         m_task->clearClient();
@@ -201,13 +181,23 @@ void NetworkLoad::willPerformHTTPRedirection(ResourceResponse&& redirectResponse
     }
     
     redirectResponse.setSource(ResourceResponse::Source::Network);
-    m_redirectCompletionHandler = WTFMove(completionHandler);
 
     auto oldRequest = WTFMove(m_currentRequest);
     request.setRequester(oldRequest.requester());
 
     m_currentRequest = request;
-    m_client.get().willSendRedirectedRequest(WTFMove(oldRequest), WTFMove(request), WTFMove(redirectResponse));
+    m_client.get().willSendRedirectedRequest(WTFMove(oldRequest), WTFMove(request), WTFMove(redirectResponse), [this, weakThis = WeakPtr<NetworkDataTaskClient> { *this }, completionHandler = WTFMove(completionHandler)] (ResourceRequest&& newRequest) mutable {
+        if (!weakThis)
+            return completionHandler({ });
+        updateRequest(m_currentRequest, newRequest);
+        if (m_currentRequest.isNull()) {
+            NetworkLoadMetrics emptyMetrics;
+            didCompleteWithError(cancelledError(m_currentRequest), emptyMetrics);
+            completionHandler({ });
+            return;
+        }
+        completionHandler(ResourceRequest(m_currentRequest));
+    });
 }
 
 void NetworkLoad::didReceiveChallenge(AuthenticationChallenge&& challenge, NegotiatedLegacyTLS negotiatedLegacyTLS, ChallengeCompletionHandler&& completionHandler)

Source/WebKit/NetworkProcess/NetworkLoad.h
@@ -27,17 +27,16 @@
 
 #include "DownloadID.h"
 #include "NetworkDataTask.h"
-#include "NetworkLoadClient.h"
 #include "NetworkLoadParameters.h"
 #include <wtf/text/WTFString.h>
 
 namespace WebCore {
 class AuthenticationChallenge;
-class BlobRegistryImpl;
 }
 
 namespace WebKit {
 
+class NetworkLoadClient;
 class NetworkLoadScheduler;
 class NetworkProcess;
 
@@ -61,8 +60,6 @@ public:
     const NetworkLoadParameters& parameters() const { return m_parameters; }
     const URL& url() const { return parameters().request.url(); }
     String attributedBundleIdentifier(WebPageProxyIdentifier);
-
-    void continueWillSendRequest(WebCore::ResourceRequest&&);
 
     void convertTaskToDownload(PendingDownload&, const WebCore::ResourceRequest&, const WebCore::ResourceResponse&, ResponseCompletionHandler&&);
     void setPendingDownloadID(DownloadID);
@@ -95,7 +92,6 @@ private:
     std::reference_wrapper<NetworkLoadClient> m_client;
     Ref<NetworkProcess> m_networkProcess;
     const NetworkLoadParameters m_parameters;
-    CompletionHandler<void(WebCore::ResourceRequest&&)> m_redirectCompletionHandler;
     RefPtr<NetworkDataTask> m_task;
     WeakPtr<NetworkLoadScheduler> m_scheduler;
 

Source/WebKit/NetworkProcess/NetworkLoadClient.h
@@ -51,7 +51,7 @@ public:
     virtual bool isAllowedToAskUserForCredentials() const = 0;
 
     virtual void didSendData(uint64_t bytesSent, uint64_t totalBytesToBeSent) = 0;
-    virtual void willSendRedirectedRequest(WebCore::ResourceRequest&&, WebCore::ResourceRequest&& redirectRequest, WebCore::ResourceResponse&& redirectResponse) = 0;
+    virtual void willSendRedirectedRequest(WebCore::ResourceRequest&&, WebCore::ResourceRequest&& redirectRequest, WebCore::ResourceResponse&& redirectResponse, CompletionHandler<void(WebCore::ResourceRequest&&)>&&) = 0;
     virtual void didReceiveInformationalResponse(WebCore::ResourceResponse&&) { };
     virtual void didReceiveResponse(WebCore::ResourceResponse&&, PrivateRelayed, ResponseCompletionHandler&&) = 0;
     virtual void didReceiveBuffer(const WebCore::FragmentedSharedBuffer&, uint64_t reportedEncodedDataLength) = 0;

commit bbe1fd08c84508af0e8824856b928da3f641879e

Fix performance regression after 271209@main https://bugs.webkit.org/show_bug.cgi?id=265826 rdar://118948980
Reviewed by Per Arne Vollan.

271209@main introduced a CompletionHandler to SpeculativeLoad::willSendRedirectedRequest.
Before that change, we would just destroy the NetworkLoad and be done.
After that change, we call the redirect completion handler with an empty ResourceRequest
before destroying the NetworkLoad, which calls updateRequest and didCompleteWithError.
This PR changes calling the completion handler to after the call to SpeculativeLoad::didComplete
which destroys the NetworkLoad.  That way, the lambda at the end of the function
NetworkLoad::willPerformHTTPRedirection just finds a null weakThis and does none of this
unnecessary work.  I verified that this is what is going on on the sites in the radar
that showed a performance regression, and I verified that this change removes that unnecessary
work when not following redirects.

* Source/WebKit/NetworkProcess/cache/NetworkCacheSpeculativeLoad.cpp:
(WebKit::NetworkCache::SpeculativeLoad::willSendRedirectedRequest):

Canonical link: https://commits.webkit.org/271526@main

Source/WebKit/NetworkProcess/cache/NetworkCacheSpeculativeLoad.cpp
@@ -101,8 +101,8 @@ void SpeculativeLoad::willSendRedirectedRequest(ResourceRequest&& request, Resou
         m_cacheEntry = m_cache->makeRedirectEntry(request, redirectResponse, redirectRequest);
 
     // Don't follow the redirect. The redirect target will be registered for speculative load when it is loaded.
-    completionHandler({ });
     didComplete();
+    completionHandler({ });
 }
 
 void SpeculativeLoad::didReceiveResponse(ResourceResponse&& receivedResponse, PrivateRelayed privateRelayed, ResponseCompletionHandler&& completionHandler)