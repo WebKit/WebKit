WebKit/WebKit

commit eec0bdebc484b8f58438640acf831ad486234388

Add a SQLite Interface for Web Extension storage https://bugs.webkit.org/show_bug.cgi?id=265830
Reviewed by Timothy Hatcher.

This patch implements an Objective-C SQLite interface to be used for managing storage for web extensions.
This interface will be used for APIs such as Storage, Scripting, and Declarative Net Request.
_WKWebExtensionSQLiteStore will contain the generic methods needed for managing this storage,
and subclasses should be made for more requests for those APIs.

Testing:
Tested adding and removing data from storage for local test web extension.

* Source/WebKit/Configurations/WebKit.xcconfig:
Add libsqlite3.dylib to the WebKit framework.

* Source/WebKit/Platform/cocoa/CocoaHelpers.h:
* Source/WebKit/Platform/cocoa/CocoaHelpers.mm:
(WebKit::ensureDirectoryExists):
(WebKit::removeFileAtURL):
(WebKit::removeDirectoryIfEmpty):
(WebKit::separateResourceForkPath):
* Source/WebKit/Shared/Extensions/SQLiteDatatypeTraits.h: Added.
(WebKit::SQLiteDatatypeTraits<int>::fetch):
(WebKit::SQLiteDatatypeTraits<int>::bind):
(WebKit::SQLiteDatatypeTraits<int64_t>::fetch):
(WebKit::SQLiteDatatypeTraits<int64_t>::bind):
(WebKit::SQLiteDatatypeTraits<double>::fetch):
(WebKit::SQLiteDatatypeTraits<double>::bind):
(WebKit::SQLiteDatatypeTraits<NSString::fetch):
(WebKit::SQLiteDatatypeTraits<NSString::bind):
(WebKit::SQLiteDatatypeTraits<NSData::fetch):
(WebKit::SQLiteDatatypeTraits<NSData::bind):
(WebKit::SQLiteDatatypeTraits<NSDate::fetch):
(WebKit::SQLiteDatatypeTraits<NSDate::bind):
(WebKit::SQLiteDatatypeTraits<NSNumber::fetch):
(WebKit::SQLiteDatatypeTraits<NSNumber::bind):
(WebKit::SQLiteDatatypeTraits<NSObject::fetch):
(WebKit::SQLiteDatatypeTraits<NSObject::bind):
(WebKit::SQLiteDatatypeTraits<std::nullptr_t>::fetch):
(WebKit::SQLiteDatatypeTraits<std::nullptr_t>::bind):
(WebKit::SQLiteDatatypeTraits<decltype):
* Source/WebKit/Shared/Extensions/SQLiteHelpers.h: Added.
(WebKit::SQLiteIsExecutionError):
(WebKit::SQLiteStatementBindAllParameters):

Source/WebKit/Platform/cocoa/CocoaHelpers.mm
@@ -30,7 +30,13 @@
 #import "config.h"
 #import "APIData.h"
 #import "CocoaHelpers.h"
+#import "Logging.h"
 #import "WKNSData.h"
+#import <wtf/FileSystem.h>
+
+#if PLATFORM(MAC)
+#import <sys/mount.h>
+#endif
 
 namespace WebKit {
 
@@ -355,6 +361,17 @@ NSString *privacyPreservingDescription(NSError *error)
         return [NSString stringWithFormat:@"Error Domain=%@ Code=%ld \"%@\"", error.domain, (long)error.code, privacyPreservingDescription];
 
     return [NSError errorWithDomain:error.domain ?: @"" code:error.code userInfo:nil].description;
+}
+
+NSURL *ensureDirectoryExists(NSURL *directory)
+{
+    ASSERT(directory.isFileURL);
+    if (!FileSystem::makeAllDirectories(directory.absoluteString)) {
+        RELEASE_LOG_ERROR(Extensions, "Failed to create directory: %{private}@", (NSString *)directory);
+        return nil;
+    }
+
+    return directory;
 }
 
 NSString *escapeCharactersInString(NSString *string, NSString *charactersToEscape)

Source/WebKit/Configurations/WebKit.xcconfig
@@ -218,7 +218,7 @@ WK_NO_STATIC_INITIALIZERS_Release_NO_ = $(WK_ERROR_WHEN_LINKING_WITH_STATIC_INIT
 WK_NO_STATIC_INITIALIZERS_Production__ = $(WK_ERROR_WHEN_LINKING_WITH_STATIC_INITIALIZERS);
 WK_NO_STATIC_INITIALIZERS_Production_NO_ = $(WK_ERROR_WHEN_LINKING_WITH_STATIC_INITIALIZERS);
 
-OTHER_LDFLAGS = $(inherited) -iframework $(SDK_DIR)$(SYSTEM_LIBRARY_DIR)/PrivateFrameworks $(UNEXPORTED_SYMBOL_LDFLAGS) $(FRAMEWORK_AND_LIBRARY_LDFLAGS) $(WK_RELOCATABLE_FRAMEWORK_LDFLAGS) $(SOURCE_VERSION_LDFLAGS) $(PROFILE_GENERATE_OR_USE_LDFLAGS) $(WK_NO_STATIC_INITIALIZERS);
+OTHER_LDFLAGS = $(inherited) -lsqlite3 -iframework $(SDK_DIR)$(SYSTEM_LIBRARY_DIR)/PrivateFrameworks $(UNEXPORTED_SYMBOL_LDFLAGS) $(FRAMEWORK_AND_LIBRARY_LDFLAGS) $(WK_RELOCATABLE_FRAMEWORK_LDFLAGS) $(SOURCE_VERSION_LDFLAGS) $(PROFILE_GENERATE_OR_USE_LDFLAGS) $(WK_NO_STATIC_INITIALIZERS);
 
 REEXPORTED_FRAMEWORK_NAMES = WebKitLegacy;
 // Needed for bincompat (rdar://117360317)

Source/WebKit/Platform/cocoa/CocoaHelpers.h
@@ -113,6 +113,8 @@ NSDictionary *mergeDictionariesAndSetValues(NSDictionary *, NSDictionary *);
 
 NSString *privacyPreservingDescription(NSError *);
 
+NSURL *ensureDirectoryExists(NSURL *directory);
+
 NSString *escapeCharactersInString(NSString *, NSString *charactersToEscape);
 
 NSDate *toAPI(const WallTime&);

Source/WebKit/Shared/Extensions/_WKWebExtensionSQLiteDatabase.h
@@ -1,0 +1,81 @@
+/*
+ * Copyright (C) 2023 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef int DatabaseResult;
+typedef int _WKSQLiteErrorCode;
+typedef int SchemaVersion;
+
+typedef NS_ENUM(NSInteger, SQLiteDatabaseAccessType) {
+    SQLiteDatabaseAccessTypeReadOnly,
+    SQLiteDatabaseAccessTypeReadWrite,
+    SQLiteDatabaseAccessTypeReadWriteCreate,
+};
+
+// This enum is only applicable on iOS and has no effect on macOS.
+// SQLiteDatabaseProtectionTypeDefault sets the protection to class C.
+typedef NS_ENUM(NSInteger, SQLiteDatabaseProtectionType) {
+    SQLiteDatabaseProtectionTypeDefault,
+    SQLiteDatabaseProtectionTypeCompleteUntilFirstUserAuthentication,
+    SQLiteDatabaseProtectionTypeCompleteUnlessOpen,
+    SQLiteDatabaseProtectionTypeComplete,
+};
+
+struct sqlite3;
+struct sqlite3_stmt;
+
+extern NSString * const _WKWebExtensionSQLiteErrorDomain;
+
+@interface _WKWebExtensionSQLiteDatabase : NSObject
+
+@property (readonly, nonatomic, nullable) sqlite3 *handle;
+@property (readonly, nonatomic) NSURL *url;
+
+@property (readonly, nonatomic) _WKSQLiteErrorCode lastErrorCode;
+@property (readonly, nonatomic, nullable) NSString *lastErrorMessage;
+
+@property (readonly, nonatomic) dispatch_queue_t queue;
+
+- (instancetype)initWithURL:(NSURL *)url queue:(dispatch_queue_t)queue;
+
+- (BOOL)enableWAL:(NSError * __autoreleasing  * _Nullable)error;
+
+- (BOOL)openWithAccessType:(SQLiteDatabaseAccessType)accessType error:(NSError **)error;
+- (BOOL)openWithAccessType:(SQLiteDatabaseAccessType)accessType vfs:(nullable NSString *)vfs error:(NSError **)error;
+- (BOOL)openWithAccessType:(SQLiteDatabaseAccessType)accessType protectionType:(SQLiteDatabaseProtectionType)protectionType vfs:(nullable NSString *)vfs error:(NSError **)error;
+
+- (BOOL)reportErrorWithCode:(_WKSQLiteErrorCode)errorCode query:(nullable NSString *)query error:(NSError **)error;
+- (BOOL)reportErrorWithCode:(_WKSQLiteErrorCode)errorCode statement:(sqlite3_stmt*)statement error:(NSError **)error;
+
+- (_WKSQLiteErrorCode)close;
+
++ (NSURL *)inMemoryDatabaseURL;
+
+@end
+
+NS_ASSUME_NONNULL_END

Source/WebKit/Shared/Extensions/_WKWebExtensionSQLiteDatabase.mm
@@ -1,0 +1,241 @@
+/*
+ * Copyright (C) 2023 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#if !__has_feature(objc_arc)
+#error This file requires ARC. Add the "-fobjc-arc" compiler flag for this file.
+#endif
+
+#import "config.h"
+#import "_WKWebExtensionSQLiteDatabase.h"
+
+#import "CocoaHelpers.h"
+#import "Logging.h"
+#import "_WKWebExtensionSQLiteHelpers.h"
+#import <sqlite3.h>
+
+NSString * const _WKWebExtensionSQLiteErrorDomain = @"com.apple.WebKit.SQLite";
+static NSString * const _WKWebExtensionSQLiteInMemoryDatabaseName = @"file::memory:";
+static NSString * const _WKWebExtensionSQLiteErrorMessageKey = @"Message";
+static NSString * const _WKWebExtensionSQLiteErrorSQLKey = @"SQL";
+
+using namespace WebKit;
+
+#if ENABLE(WK_WEB_EXTENSIONS)
+
+@implementation _WKWebExtensionSQLiteDatabase {
+    sqlite3 *_handle;
+    dispatch_queue_t _queue;
+}
+
+- (instancetype)initWithURL:(NSURL *)url queue:(dispatch_queue_t)queue
+{
+    ASSERT_ARG(url, url.isFileURL);
+
+    if (!(self = [super init]))
+        return nil;
+
+    _url = [url copy];
+    _queue = queue;
+
+    return self;
+}
+
+- (void)dealloc
+{
+    ASSERT(!_handle);
+}
+
+- (_WKSQLiteErrorCode)close
+{
+    dispatch_assert_queue(_queue);
+
+    _WKSQLiteErrorCode result = sqlite3_close_v2(_handle);
+    if (result != SQLITE_OK) {
+        RELEASE_LOG_ERROR(Extensions, "Failed to close database: %{public}@ (%d)", self.lastErrorMessage, (int)result);
+        return result;
+    }
+
+    _handle = nullptr;
+    return result;
+}
+
+- (BOOL)reportErrorWithCode:(_WKSQLiteErrorCode)errorCode query:(NSString *)query error:(NSError **)error
+{
+    dispatch_assert_queue(_queue);
+    ASSERT(errorCode != SQLITE_OK);
+
+    static const int expectedDictionarySize = 3;
+
+    NSMutableDictionary *userInfo = [NSMutableDictionary dictionaryWithCapacity:expectedDictionarySize];
+
+    if (_url)
+        userInfo[NSURLErrorKey] = [_url copy];
+
+    if (_WKSQLiteErrorCode(errorCode)) {
+        if (const char* errMsg = sqlite3_errmsg(_handle))
+            userInfo[_WKWebExtensionSQLiteErrorMessageKey] = @(errMsg);
+    }
+
+    if (query)
+        userInfo[_WKWebExtensionSQLiteErrorSQLKey] = [query copy];
+
+    NSError *errorObject = [NSError errorWithDomain:_WKWebExtensionSQLiteErrorDomain code:errorCode userInfo:userInfo];
+
+    if (error)
+        *error = errorObject;
+    else
+        RELEASE_LOG_ERROR(Extensions, "Unhandled SQLite error: %{public}@", privacyPreservingDescription(errorObject));
+
+    return NO;
+}
+
+- (BOOL)reportErrorWithCode:(_WKSQLiteErrorCode)errorCode statement:(sqlite3_stmt*)statement error:(NSError **)error
+{
+    dispatch_assert_queue(_queue);
+    ASSERT(errorCode != SQLITE_OK);
+
+    if (statement) {
+        if (char* sql = sqlite3_expanded_sql(statement)) {
+            BOOL result = [self reportErrorWithCode:errorCode query:@(sql) error:error];
+            sqlite3_free(sql);
+            return result;
+        }
+    }
+
+    return [self reportErrorWithCode:errorCode query:nil error:error];
+}
+
+- (BOOL)enableWAL:(NSError **)error
+{
+    return SQLiteDatabaseEnumerate(self, error, @"PRAGMA journal_mode = WAL", std::tie(std::ignore));
+    return SQLiteDatabaseEnumerate(self, error, @"PRAGMA journal_mode = WAL", std::tie(std::ignore));
+}
+
+- (BOOL)openWithAccessType:(SQLiteDatabaseAccessType)accessType error:(NSError **)error
+{
+    return [self openWithAccessType:accessType vfs:nil error:error];
+}
+
+- (BOOL)openWithAccessType:(SQLiteDatabaseAccessType)accessType vfs:(NSString *)vfs error:(NSError **)error
+{
+    return [self openWithAccessType:accessType protectionType:SQLiteDatabaseProtectionTypeDefault vfs:vfs error:error];
+}
+
+- (BOOL)openWithAccessType:(SQLiteDatabaseAccessType)accessType protectionType:(SQLiteDatabaseProtectionType)protectionType vfs:(NSString *)vfs error:(NSError **)error
+{
+    int flags = SQLITE_OPEN_NOMUTEX;
+
+    switch (accessType) {
+    case SQLiteDatabaseAccessTypeReadOnly:
+        flags |= SQLITE_OPEN_READONLY;
+        break;
+    case SQLiteDatabaseAccessTypeReadWrite:
+        flags |= SQLITE_OPEN_READWRITE;
+        break;
+    case SQLiteDatabaseAccessTypeReadWriteCreate:
+        flags |= SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
+        break;
+    }
+
+#if PLATFORM(IOS_FAMILY)
+    switch (protectionType) {
+    case SQLiteDatabaseProtectionTypeDefault:
+    case SQLiteDatabaseProtectionTypeCompleteUntilFirstUserAuthentication:
+        flags |= SQLITE_OPEN_FILEPROTECTION_COMPLETEUNTILFIRSTUSERAUTHENTICATION;
+        break;
+    case SQLiteDatabaseProtectionTypeCompleteUnlessOpen:
+        flags |= SQLITE_OPEN_FILEPROTECTION_COMPLETEUNLESSOPEN;
+        break;
+    case SQLiteDatabaseProtectionTypeComplete:
+        flags |= SQLITE_OPEN_FILEPROTECTION_COMPLETE;
+        break;
+    }
+#endif
+
+    return [self _openWithFlags:flags vfs:vfs error:error];
+}
+
+- (BOOL)_openWithFlags:(int)flags vfs:(NSString *)vfs error:(NSError **)error
+{
+    dispatch_assert_queue(_queue);
+    ASSERT(!_handle);
+
+    const char *databasePath;
+    if ([_url isEqual:[_WKWebExtensionSQLiteDatabase inMemoryDatabaseURL]])
+        databasePath = [_WKWebExtensionSQLiteInMemoryDatabaseName UTF8String];
+    else if ([_url isEqual:[_WKWebExtensionSQLiteDatabase privateOnDiskDatabaseURL]])
+        databasePath = "";
+    else {
+        ASSERT(_url.path.length);
+        databasePath = _url.path.fileSystemRepresentation;
+        if (!ensureDirectoryExists(_url.URLByDeletingLastPathComponent)) {
+            if (error) {
+                RELEASE_LOG_ERROR(Extensions, "Unable to create parent folder for database at path: %{private}@", _url);
+                *error = [self.class _errorWith_WKSQLiteErrorCode:SQLITE_CANTOPEN userInfo:nil];
+            }
+
+            return NO;
+        }
+    }
+
+    _WKSQLiteErrorCode result = sqlite3_open_v2(databasePath, &_handle, flags, vfs.UTF8String);
+    if (result == SQLITE_OK)
+        return YES;
+
+    // SQLite may return a valid database handle even if an error occurred. sqlite3_close silently
+    // ignores calls with a null handle so we can call it here unconditionally.
+    sqlite3_close_v2(_handle);
+    _handle = 0;
+
+    if (error)
+        *error = [[self class] _errorWith_WKSQLiteErrorCode:result userInfo:nil];
+
+    return NO;
+}
+
++ (NSURL *)inMemoryDatabaseURL
+{
+    return [NSURL URLWithString:_WKWebExtensionSQLiteInMemoryDatabaseName];
+}
+
++ (NSURL *)privateOnDiskDatabaseURL
+{
+    return [NSURL URLWithString:@"webkit::privateondisk:"];
+}
+
++ (NSError *)_errorWith_WKSQLiteErrorCode:(_WKSQLiteErrorCode)errorCode userInfo:(NSDictionary *)userInfo
+{
+    if (errorCode == SQLITE_OK)
+        return nil;
+
+    NSString *errorMessage = @(sqlite3_errstr(errorCode));
+    NSMutableDictionary *modifiedUserInfo = [userInfo mutableCopy];
+    modifiedUserInfo[_WKWebExtensionSQLiteErrorMessageKey] = errorMessage;
+    return [NSError errorWithDomain:_WKWebExtensionSQLiteErrorDomain code:errorCode userInfo:modifiedUserInfo];
+}
+
+@end
+
+#endif // ENABLE(WK_WEB_EXTENSIONS)

Source/WebKit/Shared/Extensions/_WKWebExtensionSQLiteDatatypeTraits.h
@@ -1,0 +1,247 @@
+/*
+ * Copyright (C) 2023 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "_WKWebExtensionSQLiteDatabase.h"
+#import <sqlite3.h>
+#import <tuple>
+
+NS_ASSUME_NONNULL_BEGIN
+
+namespace WebKit {
+
+template<typename Type>
+class _WKWebExtensionSQLiteDatatypeTraits {
+public:
+    static Type _Nullable fetch(sqlite3_stmt* statement, int index);
+    static _WKSQLiteErrorCode bind(sqlite3_stmt* statement, int index, const Type _Nullable &);
+};
+
+template<>
+class _WKWebExtensionSQLiteDatatypeTraits<int> {
+public:
+    static inline int fetch(sqlite3_stmt* statement, int index)
+    {
+        return sqlite3_column_int(statement, index);
+    }
+
+    static inline _WKSQLiteErrorCode bind(sqlite3_stmt* statement, int index, int value)
+    {
+        return sqlite3_bind_int(statement, index, value);
+    }
+};
+
+template<>
+class _WKWebExtensionSQLiteDatatypeTraits<int64_t> {
+public:
+    static inline int64_t fetch(sqlite3_stmt* statement, int index)
+    {
+        return sqlite3_column_int64(statement, index);
+    }
+
+    static inline _WKSQLiteErrorCode bind(sqlite3_stmt* statement, int index, int64_t value)
+    {
+        return sqlite3_bind_int64(statement, index, value);
+    }
+};
+
+template<>
+class _WKWebExtensionSQLiteDatatypeTraits<double> {
+public:
+    static inline double fetch(sqlite3_stmt* statement, int index)
+    {
+        return sqlite3_column_double(statement, index);
+    }
+
+    static inline _WKSQLiteErrorCode bind(sqlite3_stmt* statement, int index, double value)
+    {
+        return sqlite3_bind_double(statement, index, value);
+    }
+};
+
+template<>
+class _WKWebExtensionSQLiteDatatypeTraits<NSString *> {
+public:
+    static NSString * _Nullable fetch(sqlite3_stmt* statement, int index)
+    {
+        if (sqlite3_column_type(statement, index) == SQLITE_NULL)
+            return nil;
+
+        return CFBridgingRelease(CFStringCreateWithBytes(kCFAllocatorDefault, sqlite3_column_text(statement, index), sqlite3_column_bytes(statement, index), kCFStringEncodingUTF8, false));
+    }
+
+    static inline _WKSQLiteErrorCode bind(sqlite3_stmt* statement, int index, NSString * _Nullable value)
+    {
+        if (!value)
+            return sqlite3_bind_null(statement, index);
+
+        return sqlite3_bind_text(statement, index, value.UTF8String, -1, SQLITE_TRANSIENT);
+    }
+};
+
+template<>
+class _WKWebExtensionSQLiteDatatypeTraits<NSData *> {
+public:
+    static NSData * _Nullable fetch(sqlite3_stmt* statement, int index)
+    {
+        if (sqlite3_column_type(statement, index) == SQLITE_NULL)
+            return nil;
+
+        return CFBridgingRelease(CFDataCreate(kCFAllocatorDefault, (const UInt8 *)sqlite3_column_blob(statement, index), sqlite3_column_bytes(statement, index)));
+    }
+
+    static inline _WKSQLiteErrorCode bind(sqlite3_stmt* statement, int index, NSData * _Nullable value)
+    {
+        if (!value)
+            return sqlite3_bind_null(statement, index);
+
+        return sqlite3_bind_blob64(statement, index, value.bytes, value.length, SQLITE_TRANSIENT);
+    }
+};
+
+template<>
+class _WKWebExtensionSQLiteDatatypeTraits<NSDate *> {
+public:
+    static inline NSDate * _Nullable fetch(sqlite3_stmt* statement, int index)
+    {
+        if (sqlite3_column_type(statement, index) == SQLITE_NULL)
+            return nil;
+
+        return [NSDate dateWithTimeIntervalSinceReferenceDate:sqlite3_column_double(statement, index)];
+    }
+
+    static inline _WKSQLiteErrorCode bind(sqlite3_stmt* statement, int index, NSDate * _Nullable value)
+    {
+        if (!value)
+            return sqlite3_bind_null(statement, index);
+
+        return sqlite3_bind_double(statement, index, value.timeIntervalSinceReferenceDate);
+    }
+};
+
+template<>
+class _WKWebExtensionSQLiteDatatypeTraits<NSNumber *> {
+public:
+    static NSNumber * _Nullable fetch(sqlite3_stmt* statement, int index)
+    {
+        switch (sqlite3_column_type(statement, index)) {
+        case SQLITE_INTEGER:
+            return @(sqlite3_column_int64(statement, index));
+        default:
+        case SQLITE_FLOAT:
+            return @(sqlite3_column_double(statement, index));
+        case SQLITE_NULL:
+            return nil;
+        }
+    }
+
+    static _WKSQLiteErrorCode bind(sqlite3_stmt* statement, int index, NSNumber * _Nullable value)
+    {
+        if (!value)
+            return sqlite3_bind_null(statement, index);
+
+        const char* objCType = [value objCType];
+
+        if (!strcmp(objCType, @encode(double)) || !strcmp(objCType, @encode(float)))
+            return sqlite3_bind_double(statement, index, value.doubleValue);
+
+        return sqlite3_bind_int64(statement, index, value.longLongValue);
+    }
+};
+
+template<>
+class _WKWebExtensionSQLiteDatatypeTraits<NSObject *> {
+public:
+    static NSObject * _Nullable fetch(sqlite3_stmt* statement, int index)
+    {
+        switch (sqlite3_column_type(statement, index)) {
+        case SQLITE_INTEGER:
+            return @(sqlite3_column_int64(statement, index));
+        case SQLITE_FLOAT:
+            return @(sqlite3_column_double(statement, index));
+        case SQLITE_NULL:
+            return nil;
+        case SQLITE_BLOB:
+            return _WKWebExtensionSQLiteDatatypeTraits<NSData *>::fetch(statement, index);
+        case SQLITE_TEXT:
+            return _WKWebExtensionSQLiteDatatypeTraits<NSString *>::fetch(statement, index);
+        default:
+            ASSERT_NOT_REACHED();
+            return nil;
+        }
+    }
+
+    static _WKSQLiteErrorCode bind(sqlite3_stmt* statement, int index, NSObject * _Nullable value)
+    {
+        if (!value)
+            return sqlite3_bind_null(statement, index);
+
+        if ([value isKindOfClass:[NSString class]])
+            return sqlite3_bind_text(statement, index, ((NSString *)value).UTF8String, -1, SQLITE_TRANSIENT);
+
+        if ([value isKindOfClass:[NSData class]])
+            return sqlite3_bind_blob(statement, index, ((NSData *)value).bytes, (int)((NSData *)value).length, SQLITE_TRANSIENT);
+
+        if ([value isKindOfClass:[NSNumber class]])
+            return _WKWebExtensionSQLiteDatatypeTraits<NSNumber *>::bind(statement, index, (NSNumber *)value);
+
+        if ([value isKindOfClass:[NSDate class]])
+            return sqlite3_bind_double(statement, index, ((NSDate *)value).timeIntervalSinceReferenceDate);
+
+        ASSERT_NOT_REACHED();
+        return SQLITE_ERROR;
+    }
+};
+
+template<>
+class _WKWebExtensionSQLiteDatatypeTraits<std::nullptr_t> {
+public:
+    static inline std::nullptr_t fetch(sqlite3_stmt* statement, int index)
+    {
+        return std::nullptr_t();
+    }
+
+    static inline _WKSQLiteErrorCode bind(sqlite3_stmt* statement, int index, std::nullptr_t)
+    {
+        return sqlite3_bind_null(statement, index);
+    }
+};
+
+template<>
+class _WKWebExtensionSQLiteDatatypeTraits<decltype(std::ignore)> {
+public:
+    static inline decltype(std::ignore) fetch(sqlite3_stmt* statement, int index)
+    {
+        return std::ignore;
+    }
+
+    static inline _WKSQLiteErrorCode bind(sqlite3_stmt* statement, int index, decltype(std::ignore))
+    {
+        return SQLITE_OK;
+    }
+};
+
+} // namespace WebKit
+
+NS_ASSUME_NONNULL_END

commit 199f68082e5b7c4f560e6bbb147d2770463abad6

WebKit::ensureDirectoryExists should use directory path, not absoluteString. https://bugs.webkit.org/show_bug.cgi?id=265943
Reviewed by Timothy Hatcher.

Use '.path' and not '.absoluteString' when ensuring the directory exists. Fallout from initial change
in https://github.com/WebKit/WebKit/pull/21309.

* Source/WebKit/Platform/cocoa/CocoaHelpers.mm:
(WebKit::ensureDirectoryExists):

Canonical link: https://commits.webkit.org/271615@main

Source/WebKit/Platform/cocoa/CocoaHelpers.mm
@@ -34,10 +34,6 @@
 #import "WKNSData.h"
 #import <wtf/FileSystem.h>
 
-#if PLATFORM(MAC)
-#import <sys/mount.h>
-#endif
-
 namespace WebKit {
 
 static NSString * const privacyPreservingDescriptionKey = @"privacyPreservingDescription";
@@ -366,7 +362,7 @@ NSString *privacyPreservingDescription(NSError *error)
 NSURL *ensureDirectoryExists(NSURL *directory)
 {
     ASSERT(directory.isFileURL);
-    if (!FileSystem::makeAllDirectories(directory.absoluteString)) {
+    if (!FileSystem::makeAllDirectories(directory.path)) {
         RELEASE_LOG_ERROR(Extensions, "Failed to create directory: %{private}@", (NSString *)directory);
         return nil;
     }