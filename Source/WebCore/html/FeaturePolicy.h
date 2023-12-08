de238adfe0517f0f69519e4d131c3e7fe866c967
Youenn Fablet <youennf@gmail.com>
2023-12-08 00:10:47 -0800

    Cherry-pick 267815.624@safari-7617-branch (0ad98b606305). https://bugs.webkit.org/show_bug.cgi?id=265812
    Cross-origin <embed> elements can request media permission, and prompts show main-frame URL
    https://bugs.webkit.org/show_bug.cgi?id=265812
    rdar://119149318
    
    Reviewed by Chris Dumez.
    
    We should apply feature policy for all elements, including embed and frame elements.
    Since there are no allow attributes, we should use the default feature policy rules for those elements.
    Update isFeaturePolicyAllowedByDocumentAndAllOwners accordingly.
    
    Rebase tests according updated console log message.
    
    * LayoutTests/fullscreen/full-screen-enabled-expected.txt:
    * LayoutTests/fullscreen/full-screen-enabled-prefixed-expected.txt:
    * LayoutTests/fullscreen/full-screen-iframe-not-allowed-expected.txt:
    * LayoutTests/fullscreen/full-screen-iframe-without-allow-attribute-allowed-from-parent-expected.txt:
    * LayoutTests/fullscreen/full-screen-restrictions-expected.txt:
    * LayoutTests/http/tests/fullscreen/fullscreen-feature-policy-expected.txt:
    * LayoutTests/http/tests/media/media-stream/enumerate-devices-iframe-allow-attribute-expected.txt:
    * LayoutTests/http/tests/media/media-stream/get-user-media-in-embed-element-expected.txt: Added.
    * LayoutTests/http/tests/media/media-stream/get-user-media-in-embed-element.html: Added.
    * LayoutTests/http/tests/media/media-stream/resources/get-user-media-embed.html: Added.
    * LayoutTests/http/tests/paymentrequest/payment-allow-attribute.https-expected.txt:
    * LayoutTests/http/tests/security/sandboxed-iframe-geolocation-getCurrentPosition-expected.txt:
    * LayoutTests/http/tests/security/sandboxed-iframe-geolocation-watchPosition-expected.txt:
    * LayoutTests/http/tests/ssl/media-stream/get-user-media-different-host-expected.txt:
    * LayoutTests/http/tests/ssl/media-stream/get-user-media-nested-expected.txt:
    * LayoutTests/http/tests/webrtc/enumerateDevicesInFrames-expected.txt:
    * LayoutTests/http/tests/webshare/webshare-allow-attribute-canShare.https-expected.txt:
    * LayoutTests/http/tests/webshare/webshare-allow-attribute-share.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/the-iframe-element/iframe-allow-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/the-iframe-element/iframe-allowfullscreen-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/mediacapture-streams/MediaStream-feature-policy-none.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/permissions-policy/payment-allowed-by-permissions-policy-attribute-redirect-on-load.https.sub-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/screen-wake-lock/wakelock-enabled-by-feature-policy-attribute-redirect-on-load.https.sub-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/web-share/disabled-by-permissions-policy-cross-origin.https.sub-expected.txt:
    * Source/WebCore/html/FeaturePolicy.cpp:
    (WebCore::isFeaturePolicyAllowedByDocumentAndAllOwners):
    (WebCore::FeaturePolicy::parse):
    * Source/WebCore/html/FeaturePolicy.h:
    (WebCore::FeaturePolicy::defaultPolicy):
    (WebCore::FeaturePolicy::parse):
    
    Canonical link: https://commits.webkit.org/267815.624@safari-7617-branch

--- Source/WebCore/html/FeaturePolicy.h
+++ Source/WebCore/html/FeaturePolicy.h
@@ -36,7 +36,8 @@ class HTMLIFrameElement;
 
 class FeaturePolicy {
 public:
-    static FeaturePolicy parse(Document&, const HTMLIFrameElement&, StringView);
+    static FeaturePolicy defaultPolicy(Document& document) { return parse(document, nullptr, { }); }
+    static FeaturePolicy parse(Document& document, const HTMLIFrameElement& frame, StringView allow) { return parse(document, &frame, allow); }
 
     enum class Type {
         Camera,
@@ -70,6 +71,8 @@ public:
     };
 
 private:
+    static FeaturePolicy parse(Document&, const HTMLIFrameElement*, StringView);
+
     AllowRule m_cameraRule;
     AllowRule m_microphoneRule;
     AllowRule m_speakerSelectionRule;
