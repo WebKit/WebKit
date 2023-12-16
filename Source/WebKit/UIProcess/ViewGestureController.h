6c744e1658390e109a97abd5cb4e339832b25a2f
Simon Fraser <simon.fraser@apple.com>
2023-12-15 21:13:34 -0800

    [macOS WK2] Some minor smart magnify cleanup https://bugs.webkit.org/show_bug.cgi?id=266527 rdar://119746523
    Reviewed by Tim Horton, Chris Dumez and Wenson Hsieh.
    
    Rename the 'origin' argument in various ViewGestureController smart magnification functions
    to `gestureLocationInViewCoordinates`, since that's what it is.
    
    Add some more `ViewGestures` logging, and convert some `Scrolling` logging to that channel.
    
    Other minor code changes for cleanliness and readability. No behavior changes.
    
    * Source/WebKit/UIProcess/RemoteLayerTree/mac/RemoteLayerTreeDrawingAreaProxyMac.mm:
    (WebKit::RemoteLayerTreeDrawingAreaProxyMac::commitTransientZoom):
    * Source/WebKit/UIProcess/ViewGestureController.h:
    * Source/WebKit/UIProcess/mac/ViewGestureControllerMac.mm:
    (WebKit::ViewGestureController::handleSmartMagnificationGesture):
    (WebKit::ViewGestureController::didCollectGeometryForSmartMagnificationGesture):
    * Source/WebKit/WebProcess/WebPage/ViewGestureGeometryCollector.cpp:
    (WebKit::ViewGestureGeometryCollector::collectGeometryForSmartMagnificationGesture):
    * Source/WebKit/WebProcess/WebPage/ViewGestureGeometryCollector.h:
    * Source/WebKit/WebProcess/WebPage/mac/TiledCoreAnimationDrawingArea.mm:
    (WebKit::TiledCoreAnimationDrawingArea::commitTransientZoom):
    (WebKit::TiledCoreAnimationDrawingArea::applyTransientZoomToPage):
    
    Canonical link: https://commits.webkit.org/272164@main

--- Source/WebKit/UIProcess/ViewGestureController.h
+++ Source/WebKit/UIProcess/ViewGestureController.h
@@ -145,7 +145,7 @@ public:
 
 #if PLATFORM(MAC)
     void handleMagnificationGestureEvent(PlatformScrollEvent, WebCore::FloatPoint origin);
-    void handleSmartMagnificationGesture(WebCore::FloatPoint origin);
+    void handleSmartMagnificationGesture(WebCore::FloatPoint gestureLocationInViewCoordinates);
 
     void gestureEventWasNotHandledByWebCore(PlatformScrollEvent, WebCore::FloatPoint origin);
 
@@ -369,10 +369,10 @@ private:
 
     bool m_hasOutstandingRepaintRequest { false };
 
-    double m_magnification;
+    double m_magnification { 1 };
     WebCore::FloatPoint m_magnificationOrigin;
 
-    double m_initialMagnification;
+    double m_initialMagnification { 1 };
     WebCore::FloatPoint m_initialMagnificationOrigin;
 #endif
 
