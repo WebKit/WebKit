25b01ed40927622c4ea8fcfb75b8bb959627ae35
Dan Robson <dan_robson@apple.com>
2023-12-16 15:12:10 -0800

    Cherry-pick 29cbe4a37153. rdar://119339780
        Fix build issue after rename
        https://bugs.webkit.org/show_bug.cgi?id=266020
        rdar://problem/119339780
    
        Reviewed by Brent Fulgham.
    
        * Source/WebKit/Shared/AuxiliaryProcessExtensions/NetworkingProcessExtension.swift:
        (NetworkingProcessExtension.grant(_:name:)):
    
        Canonical link: https://commits.webkit.org/272110@main

--- Source/WebKit/Shared/AuxiliaryProcessExtensions/NetworkingProcessExtension.swift
+++ Source/WebKit/Shared/AuxiliaryProcessExtensions/NetworkingProcessExtension.swift
@@ -29,9 +29,9 @@ import ServiceExtensions
 @objc
 @_spi(Private)
 open class Grant: WKGrant {
-    let inner : _Capabilities.Grant
+    let inner : _Capability.Grant
 
-    init(inner: _Capabilities.Grant) {
+    init(inner: _Capability.Grant) {
         self.inner = inner
     }
 
@@ -63,7 +63,7 @@ extension NetworkingProcessExtension : NetworkingServiceExtension {
 
     override func grant(_ domain: String, name: String) -> Any {
         do {
-            let grant = try self._request(capabilities: _Capabilities.assertion(domain, name))
+            let grant = try self._request(capabilities: _Capability.assertion(domain: domain, name: name, environmentIdentifier: nil, willInvalidate: nil, didInvalidate: nil))
             return Grant(inner: grant)
         } catch {
             return WKGrant()
