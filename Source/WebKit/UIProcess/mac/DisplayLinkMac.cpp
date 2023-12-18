280cb48f7500653f70db940ceb4c3c02148b6e8c
Abrar Rahman Protyasha <a_protyasha@apple.com>
2023-12-17 22:05:06 -0800

    Unreviewed, fix the internal macOS build after CVDisplayLink deprecation
    Unreviewed build fix for macOS when building against the Apple internal
    SDK, where the CVDisplayLink interface has now been deprecated.
    
    * Source/WebCore/platform/graphics/mac/LegacyDisplayRefreshMonitorMac.cpp:
    (WebCore::LegacyDisplayRefreshMonitorMac::stop):
    (WebCore::LegacyDisplayRefreshMonitorMac::nominalFramesPerSecondFromDisplayLink):
    (WebCore::LegacyDisplayRefreshMonitorMac::ensureDisplayLink):
    (WebCore::LegacyDisplayRefreshMonitorMac::startNotificationMechanism):
    (WebCore::LegacyDisplayRefreshMonitorMac::stopNotificationMechanism):
    * Source/WebKit/UIProcess/mac/DisplayLinkMac.cpp:
    (WebKit::DisplayLink::platformInitialize):
    (WebKit::DisplayLink::platformFinalize):
    (WebKit::DisplayLink::nominalFramesPerSecondFromDisplayLink):
    (WebKit::DisplayLink::platformIsRunning const):
    (WebKit::DisplayLink::platformStart):
    (WebKit::DisplayLink::platformStop):
    
    Canonical link: https://commits.webkit.org/272201@main

--- Source/WebKit/UIProcess/mac/DisplayLinkMac.cpp
+++ Source/WebKit/UIProcess/mac/DisplayLinkMac.cpp
@@ -41,13 +41,17 @@ void DisplayLink::platformInitialize()
     // FIXME: We can get here with displayID == 0 (webkit.org/b/212120), in which case CVDisplayLinkCreateWithCGDisplay()
     // probably defaults to the main screen.
     ASSERT(hasProcessPrivilege(ProcessPrivilege::CanCommunicateWithWindowServer));
+ALLOW_DEPRECATED_DECLARATIONS_BEGIN
     CVReturn error = CVDisplayLinkCreateWithCGDisplay(m_displayID, &m_displayLink);
+ALLOW_DEPRECATED_DECLARATIONS_END
     if (error) {
         RELEASE_LOG_FAULT(DisplayLink, "Could not create a display link for display %u: error %d", m_displayID, error);
         return;
     }
 
+ALLOW_DEPRECATED_DECLARATIONS_BEGIN
     error = CVDisplayLinkSetOutputCallback(m_displayLink, displayLinkCallback, this);
+ALLOW_DEPRECATED_DECLARATIONS_END
     if (error) {
         RELEASE_LOG_FAULT(DisplayLink, "DisplayLink: Could not set the display link output callback for display %u: error %d", m_displayID, error);
         return;
@@ -63,13 +67,17 @@ void DisplayLink::platformFinalize()
     if (!m_displayLink)
         return;
 
+ALLOW_DEPRECATED_DECLARATIONS_BEGIN
     CVDisplayLinkStop(m_displayLink);
     CVDisplayLinkRelease(m_displayLink);
+ALLOW_DEPRECATED_DECLARATIONS_END
 }
 
 FramesPerSecond DisplayLink::nominalFramesPerSecondFromDisplayLink(CVDisplayLinkRef displayLink)
 {
+ALLOW_DEPRECATED_DECLARATIONS_BEGIN
     CVTime refreshPeriod = CVDisplayLinkGetNominalOutputVideoRefreshPeriod(displayLink);
+ALLOW_DEPRECATED_DECLARATIONS_END
     if (!refreshPeriod.timeValue)
         return FullSpeedFramesPerSecond;
 
@@ -79,19 +87,25 @@ FramesPerSecond DisplayLink::nominalFramesPerSecondFromDisplayLink(CVDisplayLink
 
 bool DisplayLink::platformIsRunning() const
 {
+ALLOW_DEPRECATED_DECLARATIONS_BEGIN
     return CVDisplayLinkIsRunning(m_displayLink);
+ALLOW_DEPRECATED_DECLARATIONS_END
 }
 
 void DisplayLink::platformStart()
 {
+ALLOW_DEPRECATED_DECLARATIONS_BEGIN
     CVReturn error = CVDisplayLinkStart(m_displayLink);
+ALLOW_DEPRECATED_DECLARATIONS_END
     if (error)
         RELEASE_LOG_FAULT(DisplayLink, "DisplayLink: Could not start the display link: %d", error);
 }
 
 void DisplayLink::platformStop()
 {
+ALLOW_DEPRECATED_DECLARATIONS_BEGIN
     CVDisplayLinkStop(m_displayLink);
+ALLOW_DEPRECATED_DECLARATIONS_END
 }
 
 CVReturn DisplayLink::displayLinkCallback(CVDisplayLinkRef displayLinkRef, const CVTimeStamp*, const CVTimeStamp*, CVOptionFlags, CVOptionFlags*, void* data)
