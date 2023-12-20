6c0572949bd248174e407c0c6880c72e5a98e56b
Sihui Liu <sihui_liu@apple.com>
2023-12-19 16:35:26 -0800

    Cherry-pick 272313@main (64a739754a58). https://bugs.webkit.org/show_bug.cgi?id=266603
        Ensure NetworkStorageManager gets destroyed on the main thread
        https://bugs.webkit.org/show_bug.cgi?id=266603
        rdar://117839300
    
        Reviewed by Chris Dumez and Alex Christensen.
    
        Dispatch a task to main thread to release ref of NetworkStorageManager in stopReceivingMessageFromConnection, which may
        help fix the crash under IDBStorageRegistry::removeConnectionToClient in rdar://117839300.
    
        * Source/WebKit/NetworkProcess/storage/NetworkStorageManager.cpp:
        (WebKit::NetworkStorageManager::stopReceivingMessageFromConnection):
    
        Canonical link: https://commits.webkit.org/272313@main

--- Source/WebKit/NetworkProcess/storage/NetworkStorageManager.cpp
+++ Source/WebKit/NetworkProcess/storage/NetworkStorageManager.cpp
@@ -302,6 +302,8 @@ void NetworkStorageManager::stopReceivingMessageFromConnection(IPC::Connection& 
             return shouldRemove;
         });
         m_temporaryBlobPathsByConnection.remove(connection);
+
+        RunLoop::main().dispatch([protectedThis = WTFMove(protectedThis)] { });
     });
 }
 
