a6cfb812b779ddc109abb36193c8778dd54c8877
Mark Lam <mark.lam@apple.com>
2023-12-23 07:52:53 -0800

    JSTests options need to be updated after 272208@main. https://bugs.webkit.org/show_bug.cgi?id=266837 rdar://120064300
    Reviewed by Justin Michaud.
    
    Because --validateOptions=true is not enabled, some tests were silently ignoring required
    options after the options got renamed / changed.
    
    This patch also adds --validateOptions=true as a base option for JSC stress tests.  This will
    ensure that such issues arising from options changing won't go unnoticed again.
    
    * JSTests/stress/bad-osr-exit-checkpoint-state-assertion-during-js-lock-destruction.js:
    * JSTests/stress/dfg-scan-inlined-tail-caller-frames-liveness.js:
    * JSTests/stress/early-return-from-builtin2.js:
    * JSTests/stress/eval-liveness-should-not-come-from-parser.js:
    * JSTests/stress/for-of-bad-internal-field-hoist.js:
    * JSTests/stress/inlining-for-of-should-validate.js:
    * JSTests/stress/verify-can-gc-node-index.js:
    * Tools/Scripts/run-jsc-stress-tests:
    
    Canonical link: https://commits.webkit.org/272471@main

--- JSTests/stress/dfg-scan-inlined-tail-caller-frames-liveness.js
+++ JSTests/stress/dfg-scan-inlined-tail-caller-frames-liveness.js
@@ -1,4 +1,4 @@
-//@ requireOptions("--jitPolicyScale=0", "--maximumFunctionForCallInlineCandidateInstructionCount=1000", "--validateFTLOSRExitLiveness=1")
+//@ requireOptions("--jitPolicyScale=0", "--maximumFunctionForCallInlineCandidateBytecodeCostForDFG=1000", "--maximumFunctionForCallInlineCandidateBytecodeCostForFTL=1000", "--validateFTLOSRExitLiveness=1")
 
 for (let i = 0; i < 100000; i++) {
     (function foo() { [0].concat().indexOf(0) })()
