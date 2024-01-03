80ed64c31d22f2a73adf42f4810ead01ed260778
Simon Fraser <simon.fraser@apple.com>
2024-01-02 17:05:44 -0800

    Cherry-pick 272588@main (a2e00ce3745e). https://bugs.webkit.org/show_bug.cgi?id=267002
        Null check the ScrollingCoordinator in a few places
        https://bugs.webkit.org/show_bug.cgi?id=267002
        rdar://116427384
    
        Reviewed by Alan Baradlay.
    
        We dereference the return value of RenderLayerCompositor::scrollingCoordinator() in
        a couple of places without null-checking, so fix that.
    
        * Source/WebCore/rendering/RenderLayerCompositor.cpp:
        (WebCore::RenderLayerCompositor::attachScrollingNode):
        (WebCore::RenderLayerCompositor::detachScrollCoordinatedLayer):
    
        Canonical link: https://commits.webkit.org/272588@main

--- Source/WebCore/rendering/RenderLayerCompositor.cpp
+++ Source/WebCore/rendering/RenderLayerCompositor.cpp
@@ -4709,6 +4709,9 @@ static inline ScrollCoordinationRole scrollCoordinationRoleForNodeType(Scrolling
 ScrollingNodeID RenderLayerCompositor::attachScrollingNode(RenderLayer& layer, ScrollingNodeType nodeType, ScrollingTreeState& treeState)
 {
     auto* scrollingCoordinator = this->scrollingCoordinator();
+    if (!scrollingCoordinator)
+        return 0;
+
     auto* backing = layer.backing();
     // Crash logs suggest that backing can be null here, but we don't know how: rdar://problem/18545452.
     ASSERT(backing);
@@ -4805,6 +4808,8 @@ void RenderLayerCompositor::detachScrollCoordinatedLayer(RenderLayer& layer, Opt
         return;
 
     auto* scrollingCoordinator = this->scrollingCoordinator();
+    if (!scrollingCoordinator)
+        return;
 
     if (roles.contains(ScrollCoordinationRole::Scrolling))
         detachScrollCoordinatedLayerWithRole(layer, *scrollingCoordinator, ScrollCoordinationRole::Scrolling);
