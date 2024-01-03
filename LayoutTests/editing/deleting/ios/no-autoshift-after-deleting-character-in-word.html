WebKit/WebKit

commit e6a7b742d24d37554294e235e5026cd9b86abc04

Adopt the ServiceExtensions framework for async text input support https://bugs.webkit.org/show_bug.cgi?id=266302 rdar://119568570
Reviewed by Tim Horton.

Adopt `WKSE*`-prefixed objects, which are aliased to the new classes, protocols, or types and
constants exposed from the ServiceExtensions framework (if available), or otherwise aliased to the
`UIAsync*` SPI names.

No change in behavior; see below for more details.

* Source/WebCore/WebCore.xcodeproj/project.pbxproj:
* Source/WebCore/platform/ios/WebEvent.mm:
(webEventType):
(isChangingKeyModifiers):
(-[WebEvent initWithKeyEvent:]):
(-[WebEvent originalKeyEvent]):
(-[WebEvent initWithUIKeyEvent:]): Deleted.
(-[WebEvent originalUIKeyEvent]): Deleted.
* Source/WebCore/platform/ios/WebEventPrivate.h:
* Source/WebCore/platform/ios/WebSEDefinitions.h: Copied from Source/WebKit/UIProcess/API/ios/WKSEDefinitions.h.
* Source/WebKit/Platform/spi/ios/UIKitSPI.h:
* Source/WebKit/Shared/DocumentEditingContext.h:
* Source/WebKit/Shared/DocumentEditingContext.mm:
(WebKit::setOptionalEditingContextProperties):
(WebKit::DocumentEditingContext::toLegacyPlatformContext):
(WebKit::DocumentEditingContext::toPlatformContext):
* Source/WebKit/UIProcess/API/ios/WKSEDefinitions.h:
* Source/WebKit/UIProcess/API/ios/WKWebViewPrivateForTestingIOS.h:
* Source/WebKit/UIProcess/WebPageProxy.h:
* Source/WebKit/UIProcess/ios/WKContentViewInteraction.h:
* Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm:
(-[WKContentView _updateRuntimeProtocolConformanceIfNeeded]):
(-[WKContentView textInteractionGesture:shouldBeginAtPoint:]):
(toGestureType):
(toWKSEGestureType):
(toSelectionTouch):
(toWKSESelectionTouch):
(toWKSESelectionFlags):
(toSelectionFlags):
(selectionChangedWithGesture):
(selectionChangedWithTouch):
(-[WKContentView changeSelectionWithGestureAt:withGesture:withState:]):
(-[WKContentView changeSelectionWithGestureAt:withGesture:withState:withFlags:]):

Source/WebKit/Shared/DocumentEditingContext.mm
@@ -40,7 +40,31 @@ static inline NSRange toNSRange(DocumentEditingContext::Range range)
     return NSMakeRange(range.location, range.length);
 }
 
-UIWKDocumentContext *DocumentEditingContext::toPlatformContext(OptionSet<DocumentEditingContextRequest::Options> options)
+#if HAVE(UI_WK_DOCUMENT_CONTEXT)
+
+template <typename ContextType>
+void setOptionalEditingContextProperties(const DocumentEditingContext& context, ContextType *platformContext, OptionSet<DocumentEditingContextRequest::Options> options)
+{
+    for (auto& rect : context.textRects)
+        [platformContext addTextRect:rect.rect forCharacterRange:toNSRange(rect.range)];
+
+    [platformContext setAnnotatedText:context.annotatedText.nsAttributedString().get()];
+
+#if HAVE(AUTOCORRECTION_ENHANCEMENTS)
+    if (options.contains(DocumentEditingContextRequest::Options::AutocorrectedRanges)) {
+        auto ranges = createNSArray(context.autocorrectedRanges, [&] (DocumentEditingContext::Range range) {
+            return [NSValue valueWithRange:toNSRange(range)];
+        });
+
+        if ([platformContext respondsToSelector:@selector(setAutocorrectedRanges:)])
+            [platformContext setAutocorrectedRanges:ranges.get()];
+    }
+#endif // HAVE(AUTOCORRECTION_ENHANCEMENTS)
+}
+
+#endif // HAVE(UI_WK_DOCUMENT_CONTEXT)
+
+UIWKDocumentContext *DocumentEditingContext::toLegacyPlatformContext(OptionSet<DocumentEditingContextRequest::Options> options)
 {
 #if HAVE(UI_WK_DOCUMENT_CONTEXT)
     auto platformContext = adoptNS([[UIWKDocumentContext alloc] init]);
@@ -58,24 +82,38 @@ UIWKDocumentContext *DocumentEditingContext::toPlatformContext(OptionSet<Documen
     }
 
     [platformContext setSelectedRangeInMarkedText:toNSRange(selectedRangeInMarkedText)];
-
-    for (const auto& rect : textRects)
-        [platformContext addTextRect:rect.rect forCharacterRange:toNSRange(rect.range)];
-
-    [platformContext setAnnotatedText:annotatedText.nsAttributedString().get()];
-
-#if HAVE(AUTOCORRECTION_ENHANCEMENTS)
-    if (options.contains(DocumentEditingContextRequest::Options::AutocorrectedRanges)) {
-        auto ranges = createNSArray(autocorrectedRanges, [&] (DocumentEditingContext::Range range) {
-            return [NSValue valueWithRange:toNSRange(range)];
-        });
-
-        if ([platformContext respondsToSelector:@selector(setAutocorrectedRanges:)])
-            [platformContext setAutocorrectedRanges:ranges.get()];
-    }
-#endif
+    setOptionalEditingContextProperties(*this, platformContext.get(), options);
 
     return platformContext.autorelease();
+#else
+    UNUSED_PARAM(options);
+    return nil;
+#endif
+}
+
+WKSETextDocumentContext *DocumentEditingContext::toPlatformContext(OptionSet<DocumentEditingContextRequest::Options> options)
+{
+#if HAVE(UI_WK_DOCUMENT_CONTEXT)
+#if SERVICE_EXTENSIONS_TEXT_INPUT_IS_AVAILABLE
+    RetainPtr<WKSETextDocumentContext> platformContext;
+    if (options.contains(DocumentEditingContextRequest::Options::AttributedText)) {
+        platformContext = adoptNS([[WKSETextDocumentContext alloc] initWithAttributedSelectedText:selectedText.nsAttributedString().get()
+            contextBefore:contextBefore.nsAttributedString().get()
+            contextAfter:contextAfter.nsAttributedString().get()
+            markedText:markedText.nsAttributedString().get()
+            selectedRangeInMarkedText:toNSRange(selectedRangeInMarkedText)]);
+    } else {
+        platformContext = adoptNS([[WKSETextDocumentContext alloc] initWithSelectedText:[selectedText.nsAttributedString() string]
+            contextBefore:[contextBefore.nsAttributedString() string]
+            contextAfter:[contextAfter.nsAttributedString() string]
+            markedText:[markedText.nsAttributedString() string]
+            selectedRangeInMarkedText:toNSRange(selectedRangeInMarkedText)]);
+    }
+    setOptionalEditingContextProperties(*this, platformContext.get(), options);
+    return platformContext.autorelease();
+#else
+    return toLegacyPlatformContext(options);
+#endif
 #else
     UNUSED_PARAM(options);
     return nil;

Source/WebCore/platform/ios/WebEvent.mm
@@ -48,7 +48,7 @@ using WebCore::windowsKeyCodeForCharCode;
 
 @implementation WebEvent {
 #if HAVE(UI_ASYNC_TEXT_INTERACTION)
-    RetainPtr<UIKeyEvent> _originalUIKeyEvent;
+    RetainPtr<WebSEKeyEvent> _originalKeyEvent;
 #endif
 }
 
@@ -496,14 +496,14 @@ static NSString *normalizedStringWithAppKitCompatibilityMapping(NSString *charac
 
 #if HAVE(UI_ASYNC_TEXT_INTERACTION)
 
-@implementation WebEvent (UIAsyncTextInputSupport)
+@implementation WebEvent (WebSEKeyEventSupport)
 
-static inline WebEventType webEventType(UIKeyEventType type)
+static inline WebEventType webEventType(WebSEKeyEventType type)
 {
     switch (type) {
-    case UIKeyEventKeyDown:
+    case WebSEKeyEventKeyDown:
         return WebEventKeyDown;
-    case UIKeyEventKeyUp:
+    case WebSEKeyEventKeyUp:
         return WebEventKeyUp;
     }
     ASSERT_NOT_REACHED();
@@ -526,7 +526,7 @@ static inline WebEventFlags webEventModifierFlags(UIKeyModifierFlags flags)
     return modifiers;
 }
 
-static inline bool isChangingKeyModifiers(UIKeyEvent *event)
+static inline bool isChangingKeyModifiers(WebSEKeyEvent *event)
 {
     switch (event.keyCode) {
     case VK_LWIN:
@@ -544,7 +544,7 @@ static inline bool isChangingKeyModifiers(UIKeyEvent *event)
     }
 }
 
-- (instancetype)initWithUIKeyEvent:(UIKeyEvent *)event
+- (instancetype)initWithKeyEvent:(WebSEKeyEvent *)event
 {
     if (!(self = [super init]))
         return nil;
@@ -563,14 +563,14 @@ static inline bool isChangingKeyModifiers(UIKeyEvent *event)
     _charactersIgnoringModifiers = [event.charactersIgnoringModifiers retain];
     _tabKey = NO; // FIXME: Populate this field appropriately.
     _keyRepeating = event.keyRepeating;
-    _originalUIKeyEvent = event;
+    _originalKeyEvent = event;
 
     return self;
 }
 
-- (UIKeyEvent *)originalUIKeyEvent
+- (WebSEKeyEvent *)originalKeyEvent
 {
-    return _originalUIKeyEvent.get();
+    return _originalKeyEvent.get();
 }
 
 @end

Source/WebCore/platform/ios/WebSEDefinitions.h
@@ -25,18 +25,11 @@
 
 #pragma once
 
-#if HAVE(UI_ASYNC_TEXT_INTERACTION)
+#if HAVE(SERVICEEXTENSIONS) && defined(__OBJC__) && __OBJC__
+#import <ServiceExtensions/ServiceExtensions.h>
+#import <ServiceExtensions/ServiceExtensions_Private.h>
+#endif
 
-#import "WebSEDefinitions.h"
-#import <WebCore/WebEvent.h>
-
-@class WebSEKeyEvent;
-
-@interface WebEvent (WebSEKeyEventSupport)
-
-- (instancetype)initWithKeyEvent:(WebSEKeyEvent *)event;
-@property (nonatomic, readonly) WebSEKeyEvent *originalKeyEvent;
-
-@end
-
-#endif // HAVE(UI_ASYNC_TEXT_INTERACTION)
+#if USE(APPLE_INTERNAL_SDK)
+#import <WebKitAdditions/WebSEDefinitionsAdditions.h>
+#endif

Source/WebKit/Platform/spi/ios/UIKitSPI.h
@@ -1222,27 +1222,6 @@ typedef NS_ENUM(NSUInteger, _UIScrollDeviceCategory) {
 
 @end
 
-@protocol UIAsyncTextInput_Staging_117155812 <UIAsyncTextInput>
-
-- (void)deleteInDirection:(UITextStorageDirection)direction toGranularity:(UITextGranularity)granularity;
-- (void)moveInDirection:(UITextStorageDirection)direction byGranularity:(UITextGranularity)granularity;
-- (void)extendInDirection:(UITextStorageDirection)direction byGranularity:(UITextGranularity)granularity;
-- (void)moveInLayoutDirection:(UITextLayoutDirection)direction;
-- (void)extendInLayoutDirection:(UITextLayoutDirection)direction;
-
-@end
-
-@protocol UIExtendedTextInputTraits_Staging_117880911<UITextInputTraits>
-@optional
-
-@property (nonatomic, readonly) BOOL isSingleLineDocument;
-@property (nonatomic, readonly) BOOL typingAdaptationDisabled;
-@property (nonatomic, readonly) UIColor *insertionPointColor;
-@property (nonatomic, readonly) UIColor *selectionBarColor;
-@property (nonatomic, readonly) UIColor *selectionHighlightColor;
-
-@end
-
 #if !defined(UI_DIRECTIONAL_TEXT_RANGE_STRUCT)
 
 typedef struct {
@@ -1257,9 +1236,7 @@ typedef struct {
 @end
 
 @protocol UIAsyncTextInputDelegate_Staging<UIAsyncTextInputDelegate>
-- (void)invalidateTextEntryContext; // Added in rdar://118536368.
 - (void)deferReplaceTextActionToSystem:(id)sender; // Added in rdar://118307558.
-- (void)provideCandidateUISuggestions:(NSArray<UITextSuggestion*> *)suggestions; // Added in rdar://117914235.
 @end
 
 #endif // HAVE(UI_ASYNC_TEXT_INTERACTION)

Source/WebKit/Shared/DocumentEditingContext.h
@@ -28,6 +28,7 @@
 #if PLATFORM(IOS_FAMILY)
 
 #include "ArgumentCoders.h"
+#include "WKSEDefinitions.h"
 #include <WebCore/AttributedString.h>
 #include <WebCore/ElementContext.h>
 #include <WebCore/FloatRect.h>
@@ -35,6 +36,7 @@
 #include <wtf/OptionSet.h>
 #include <wtf/Vector.h>
 
+OBJC_CLASS WKSETextDocumentContext;
 OBJC_CLASS UIWKDocumentContext;
 
 namespace WebKit {
@@ -62,7 +64,8 @@ struct DocumentEditingContextRequest {
 };
 
 struct DocumentEditingContext {
-    UIWKDocumentContext *toPlatformContext(OptionSet<WebKit::DocumentEditingContextRequest::Options>);
+    WKSETextDocumentContext *toPlatformContext(OptionSet<DocumentEditingContextRequest::Options>);
+    UIWKDocumentContext *toLegacyPlatformContext(OptionSet<DocumentEditingContextRequest::Options>);
 
     WebCore::AttributedString contextBefore;
     WebCore::AttributedString selectedText;

Source/WebKit/UIProcess/API/ios/WKSEDefinitions.h
@@ -25,6 +25,11 @@
 
 #pragma once
 
+#if HAVE(SERVICEEXTENSIONS) && defined(__OBJC__) && __OBJC__
+#import <ServiceExtensions/ServiceExtensions.h>
+#import <ServiceExtensions/ServiceExtensions_Private.h>
+#endif
+
 #if USE(APPLE_INTERNAL_SDK)
 
 #import <WebKitAdditions/WKSEDefinitionsAdditions.h>
@@ -36,4 +41,57 @@
 
 #endif
 
-
+#ifndef SERVICE_EXTENSIONS_TEXT_INPUT_IS_AVAILABLE
+#define SERVICE_EXTENSIONS_TEXT_INPUT_IS_AVAILABLE 0
+#define WKSETextSuggestion                                      UITextSuggestion
+#define WKSEAutoFillTextSuggestion                              UITextAutofillSuggestion
+#define WKSEKeyEvent                                            UIKeyEvent
+#define WKSEKeyEventContext                                     UIKeyEventContext
+#define WKSETextDocumentContext                                 UIWKDocumentContext
+#define WKSETextDocumentRequest                                 UIWKDocumentRequest
+#define WKSETextDocumentRequestFlags                            UIWKDocumentRequestFlags
+#define WKSETextDocumentRequestNone                             UIWKDocumentRequestNone
+#define WKSETextDocumentRequestText                             UIWKDocumentRequestText
+#define WKSETextDocumentRequestAttributed                       UIWKDocumentRequestAttributed
+#define WKSETextDocumentRequestRects                            UIWKDocumentRequestRects
+#define WKSETextDocumentRequestSpatial                          UIWKDocumentRequestSpatial
+#define WKSETextDocumentRequestAnnotation                       UIWKDocumentRequestAnnotation
+#define WKSETextDocumentRequestMarkedTextRects                  UIWKDocumentRequestMarkedTextRects
+#define WKSETextDocumentRequestSpatialAndCurrentSelection       UIWKDocumentRequestSpatialAndCurrentSelection
+#define WKSETextDocumentRequestAutocorrectedRanges              UIWKDocumentRequestAutocorrectedRanges
+#define WKSEDirectionalTextRange                                UIDirectionalTextRange
+#define WKSETextReplacementOptions                              UITextReplacementOptions
+#define WKSETextReplacementOptionsNone                          UITextReplacementOptionsNone
+#define WKSETextReplacementOptionsAddUnderline                  UITextReplacementOptionsAddUnderline
+#define WKSEShiftKeyState                                       UIShiftKeyState
+#define WKSEShiftKeyStateNone                                   UIShiftKeyStateNone
+#define WKSEShiftKeyStateShifted                                UIShiftKeyStateShifted
+#define WKSEShiftKeyStateCapsLocked                             UIShiftKeyStateCapsLocked
+#define WKSEExtendedTextInputTraits                             UIExtendedTextInputTraits
+#define WKSETextInput                                           UIAsyncTextInputClient
+#define WKSETextInputDelegate                                   UIAsyncTextInputDelegate
+#define WKSETextInteraction                                     UIAsyncTextInteraction
+#define WKSETextInteractionDelegate                             UIAsyncTextInteractionDelegate
+#define WKSEGestureType                                         UIWKGestureType
+#define WKSEGestureLoupe                                        UIWKGestureLoupe
+#define WKSEGestureOneFingerTap                                 UIWKGestureOneFingerTap
+#define WKSEGestureTapAndAHalf                                  UIWKGestureTapAndAHalf
+#define WKSEGestureDoubleTap                                    UIWKGestureDoubleTap
+#define WKSEGestureOneFingerDoubleTap                           UIWKGestureOneFingerDoubleTap
+#define WKSEGestureOneFingerTripleTap                           UIWKGestureOneFingerTripleTap
+#define WKSEGestureTwoFingerSingleTap                           UIWKGestureTwoFingerSingleTap
+#define WKSEGestureTwoFingerRangedSelectGesture                 UIWKGestureTwoFingerRangedSelectGesture
+#define WKSEGesturePhraseBoundary                               UIWKGesturePhraseBoundary
+#define WKSESelectionTouch                                      UIWKSelectionTouch
+#define WKSESelectionTouchStarted                               UIWKSelectionTouchStarted
+#define WKSESelectionTouchMoved                                 UIWKSelectionTouchMoved
+#define WKSESelectionTouchEnded                                 UIWKSelectionTouchEnded
+#define WKSESelectionTouchEndedMovingForward                    UIWKSelectionTouchEndedMovingForward
+#define WKSESelectionTouchEndedMovingBackward                   UIWKSelectionTouchEndedMovingBackward
+#define WKSESelectionTouchEndedNotMoving                        UIWKSelectionTouchEndedNotMoving
+#define WKSESelectionFlags                                      UIWKSelectionFlags
+#define WKSESelectionFlagsNone                                  UIWKNone
+#define WKSEWordIsNearTap                                       UIWKWordIsNearTap
+#define WKSESelectionFlipped                                    UIWKSelectionFlipped
+#define WKSEPhraseBoundaryChanged                               UIWKPhraseBoundaryChanged
+#endif // !defined(SERVICE_EXTENSIONS_TEXT_INPUT_IS_AVAILABLE)

Source/WebKit/UIProcess/API/ios/WKWebViewPrivateForTestingIOS.h
@@ -33,7 +33,7 @@
 @class _WKTextInputContext;
 @class UIEventAttribution;
 @class UIGestureRecognizer;
-@class UIWKDocumentContext;
+@class WKSEDocumentContext;
 @class UIWKDocumentRequest;
 @class UITapGestureRecognizer;
 

commit 9bf259e8d9e0193d6bbe8bd66259bf9d853dd538

[UIAsyncTextInput] Keyboard incorrectly autocapitalizes after deleting backwards https://bugs.webkit.org/show_bug.cgi?id=267003 rdar://120381357
Reviewed by Megan Gardner.

This bug was caused by the fact that autocorrection context requests with async text input enabled
broke after the changes in 272018@main — in particular, because we don't pass the `Text` request
flag when converting from `WebKit::DocumentEditingContext` to `WKSETextDocumentContext` in
`-requestTextContextForAutocorrectionWithCompletionHandler:`.

This happened to work in my testing when `SERVICE_EXTENSIONS_TEXT_INPUT_IS_AVAILABLE` is set,
because the logic in `DocumentEditingContext::toPlatformContext` assumes that `Text` is specified,
if `AttributedText` isn't set. This was an unintentional departure from the shipping behavior, which
checked:

```
    if (options.contains(DocumentEditingContextRequest::Options::AttributedText)) {

        …

    } else if (options.contains(DocumentEditingContextRequest::Options::Text)) {

        …

    }
```

As such, we fix this by making the `SERVICE_EXTENSIONS_TEXT_INPUT_IS_AVAILABLE` codepath consistent
with the above logic, and pass in `DocumentEditingContextRequest::Options::Text` when using
`toPlatformContext`, when returning an autocorrection context in
`-requestTextContextForAutocorrectionWithCompletionHandler:`.

* LayoutTests/editing/deleting/ios/no-autoshift-after-deleting-character-in-word-expected.txt: Added.
* LayoutTests/editing/deleting/ios/no-autoshift-after-deleting-character-in-word.html: Added.

Add a layout test to exercise the bug fix.

* Source/WebKit/Shared/DocumentEditingContext.mm:
(WebKit::DocumentEditingContext::toPlatformContext):

Make this only set non-attributed text context strings in the case where the `Text` request flag is
specified, to match shipping behavior.

* Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm:
(-[WKContentView requestTextContextForAutocorrectionWithCompletionHandler:]):

Source/WebKit/Shared/DocumentEditingContext.mm
@@ -102,13 +102,14 @@ WKSETextDocumentContext *DocumentEditingContext::toPlatformContext(OptionSet<Doc
             contextAfter:contextAfter.nsAttributedString().get()
             markedText:markedText.nsAttributedString().get()
             selectedRangeInMarkedText:toNSRange(selectedRangeInMarkedText)]);
-    } else {
+    } else if (options.contains(DocumentEditingContextRequest::Options::Text)) {
         platformContext = adoptNS([[WKSETextDocumentContext alloc] initWithSelectedText:[selectedText.nsAttributedString() string]
             contextBefore:[contextBefore.nsAttributedString() string]
             contextAfter:[contextAfter.nsAttributedString() string]
             markedText:[markedText.nsAttributedString() string]
             selectedRangeInMarkedText:toNSRange(selectedRangeInMarkedText)]);
-    }
+    } else
+        ASSERT_NOT_REACHED_WITH_MESSAGE("%s expected at least Options::AttributedText or Options::Text", __PRETTY_FUNCTION__);
     setOptionalEditingContextProperties(*this, platformContext.get(), options);
     return platformContext.autorelease();
 #else

Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -5925,9 +5925,9 @@ static void logTextInteraction(const char* methodName, UIGestureRecognizer *loup
 {
     [self _updateInternalStateBeforeSelectionChange];
 
-#if HAVE(UI_ASYNC_TEXT_INPUT_DELEGATE)
+#if HAVE(UI_ASYNC_TEXT_INTERACTION_DELEGATE)
     if (self.shouldUseAsyncInteractions)
-        [_asyncSystemInputDelegate selectionWillChange:self];
+        [_asyncSystemInputDelegate selectionWillChange:static_cast<id<UIAsyncTextInputClient>>(self)];
     else
 #endif
         [self.inputDelegate selectionWillChange:self];
@@ -5948,9 +5948,9 @@ static void logTextInteraction(const char* methodName, UIGestureRecognizer *loup
 
 - (void)_internalEndSelectionChange
 {
-#if HAVE(UI_ASYNC_TEXT_INPUT_DELEGATE)
+#if HAVE(UI_ASYNC_TEXT_INTERACTION_DELEGATE)
     if (self.shouldUseAsyncInteractions)
-        [_asyncSystemInputDelegate selectionDidChange:self];
+        [_asyncSystemInputDelegate selectionDidChange:static_cast<id<UIAsyncTextInputClient>>(self)];
     else
 #endif
         [self.inputDelegate selectionDidChange:self];
@@ -12627,7 +12627,7 @@ static BOOL shouldUseMachineReadableCodeMenuFromImageAnalysisResult(CocoaImageAn
             };
             break;
         }
-        completion(context.toPlatformContext({ }));
+        completion(context.toPlatformContext({ WebKit::DocumentEditingContextRequest::Options::Text }));
     }];
 }
 

Tools/TestWebKitAPI/cocoa/TestWKWebView.mm
@@ -56,6 +56,11 @@
 SOFT_LINK_FRAMEWORK(UIKit)
 SOFT_LINK_CLASS(UIKit, UIWindow)
 
+#if HAVE(UI_ASYNC_TEXT_INTERACTION)
+// FIXME: This workaround can be removed once the fix for rdar://120390585 lands in the SDK.
+SOFT_LINK_CLASS(UIKit, UIKeyEvent)
+#endif
+
 static NSString *overrideBundleIdentifier(id, SEL)
 {
     return @"com.apple.TestWebKitAPI";
@@ -252,10 +257,20 @@ static NSString *overrideBundleIdentifier(id, SEL)
 {
     std::pair<CGRect, CGRect> result;
     bool done = false;
-    [self.textInputContentView requestAutocorrectionRectsForString:string withCompletionHandler:[&] (UIWKAutocorrectionRects *rects) {
-        result = { rects.firstRect, rects.lastRect };
-        done = true;
-    }];
+#if HAVE(UI_ASYNC_TEXT_INTERACTION)
+    if (auto asyncTextInput = self.asyncTextInput) {
+        [asyncTextInput requestTextRectsForString:string withCompletionHandler:[&](NSArray<UITextSelectionRect *> *rects) {
+            result = { rects.firstObject.rect, rects.lastObject.rect };
+            done = true;
+        }];
+    } else
+#endif
+    {
+        [self.textInputContentView requestAutocorrectionRectsForString:string withCompletionHandler:[&](UIWKAutocorrectionRects *rects) {
+            result = { rects.firstRect, rects.lastRect };
+            done = true;
+        }];
+    }
     TestWebKitAPI::Util::run(&done);
     return result;
 }
@@ -264,26 +279,40 @@ static NSString *overrideBundleIdentifier(id, SEL)
 {
     TestWebKitAPI::AutocorrectionContext result;
     bool done = false;
-    [self.textInputContentView requestAutocorrectionContextWithCompletionHandler:[&] (UIWKAutocorrectionContext *context) {
-        result = { context.contextBeforeSelection, context.selectedText, context.contextAfterSelection };
-        done = true;
-    }];
+#if HAVE(UI_ASYNC_TEXT_INTERACTION)
+    if (auto asyncTextInput = self.asyncTextInput) {
+        [asyncTextInput requestTextContextForAutocorrectionWithCompletionHandler:[&](WKSETextDocumentContext *context) {
+            result = {
+                dynamic_objc_cast<NSString>(context.contextBefore),
+                dynamic_objc_cast<NSString>(context.selectedText),
+                dynamic_objc_cast<NSString>(context.contextAfter),
+            };
+            done = true;
+        }];
+    } else
+#endif // HAVE(UI_ASYNC_TEXT_INTERACTION)
+    {
+        [self.textInputContentView requestAutocorrectionContextWithCompletionHandler:[&](UIWKAutocorrectionContext *context) {
+            result = { context.contextBeforeSelection, context.selectedText, context.contextAfterSelection };
+            done = true;
+        }];
+    }
     TestWebKitAPI::Util::run(&done);
     return result;
 }
 
 - (id<UITextInputTraits_Private>)effectiveTextInputTraits
 {
-#if HAVE(UI_ASYNC_TEXT_INPUT)
+#if HAVE(UI_ASYNC_TEXT_INTERACTION)
     if (self.hasAsyncTextInput)
-        return static_cast<id<UITextInputTraits_Private>(self.extendedTextInputTraits);
+        return static_cast<id<UITextInputTraits_Private>>(self.extendedTextInputTraits);
 #endif
     return static_cast<id<UITextInputTraits_Private>>(self.textInputContentView.textInputTraits);
 }
 
 - (void)replaceText:(NSString *)input withText:(NSString *)correction shouldUnderline:(BOOL)shouldUnderline completion:(void(^)())completion
 {
-#if HAVE(UI_ASYNC_TEXT_INPUT)
+#if HAVE(UI_ASYNC_TEXT_INTERACTION)
     if (self.hasAsyncTextInput) {
         auto options = shouldUnderline ? WKSETextReplacementOptionsAddUnderline : WKSETextReplacementOptionsNone;
         [self.asyncTextInput replaceText:input withText:correction options:options withCompletionHandler:[completion = makeBlockPtr(completion)](NSArray<UITextSelectionRect *> *) {
@@ -298,11 +327,11 @@ static NSString *overrideBundleIdentifier(id, SEL)
     }];
 }
 
-#if HAVE(UI_ASYNC_TEXT_INPUT)
+#if HAVE(UI_ASYNC_TEXT_INTERACTION)
 
 static RetainPtr<WKSEKeyEvent> wrap(WebEvent *webEvent)
 {
-    auto uiKeyEvent = adoptNS([[UIKeyEvent alloc] initWithWebEvent:event]);
+    auto uiKeyEvent = adoptNS([allocUIKeyEventInstance() initWithWebEvent:webEvent]);
 #if SERVICE_EXTENSIONS_TEXT_INPUT_IS_AVAILABLE
     return adoptNS([[WKSEKeyEvent alloc] initWithUIKitKeyEvent:uiKeyEvent.get()]);
 #else
@@ -320,7 +349,7 @@ static WebEvent *unwrap(WKSEKeyEvent *event)
     return [uiEvent webEvent];
 }
 
-#endif // HAVE(UI_ASYNC_TEXT_INPUT)
+#endif // HAVE(UI_ASYNC_TEXT_INTERACTION)
 
 #if HAVE(UI_WK_DOCUMENT_CONTEXT)
 
@@ -330,10 +359,14 @@ static WebEvent *unwrap(WKSEKeyEvent *event)
     auto completion = ^{
         finished = true;
     };
-#if HAVE(UI_ASYNC_TEXT_INPUT)
-    if (self.hasAsyncTextInput)
-        [self.asyncTextInput adjustSelection:WKSEDirectionalTextRange { range.location, range.length } completion:completion];
-    else
+#if HAVE(UI_ASYNC_TEXT_INTERACTION)
+    if (self.hasAsyncTextInput) {
+        WKSEDirectionalTextRange directionalRange {
+            static_cast<NSInteger>(range.location),
+            static_cast<NSInteger>(range.length)
+        };
+        [self.asyncTextInput adjustSelection:directionalRange completionHandler:completion];
+    } else
 #endif
         [self.textInputContentView adjustSelectionWithDelta:range completionHandler:completion];
     TestWebKitAPI::Util::run(&finished);
@@ -343,9 +376,9 @@ static WebEvent *unwrap(WKSEKeyEvent *event)
 
 - (void)selectTextForContextMenuWithLocationInView:(CGPoint)locationInView completion:(void(^)(BOOL shouldPresent))completion
 {
-#if HAVE(UI_ASYNC_TEXT_INPUT)
+#if HAVE(UI_ASYNC_TEXT_INTERACTION)
     if (self.hasAsyncTextInput) {
-        [self.asyncTextInput selectTextForContextMenuWithLocationInView:locationInView completion:[completion = makeBlockPtr(completion)](BOOL shouldPresent, NSString *, NSRange) {
+        [self.asyncTextInput selectTextForContextMenuWithLocationInView:locationInView completionHandler:[completion = makeBlockPtr(completion)](BOOL shouldPresent, NSString *, NSRange) {
             completion(shouldPresent);
         }];
         return;
@@ -358,9 +391,9 @@ static WebEvent *unwrap(WKSEKeyEvent *event)
 
 - (void)handleKeyEvent:(WebEvent *)event completion:(void (^)(WebEvent *theEvent, BOOL handled))completion
 {
-#if HAVE(UI_ASYNC_TEXT_INPUT)
+#if HAVE(UI_ASYNC_TEXT_INTERACTION)
     if (self.hasAsyncTextInput) {
-        [self.asyncTextInput handleAsyncKeyEvent:wrap(event).get() completion:[completion = makeBlockPtr(completion)](WKSEKeyEvent *event, BOOL handled) {
+        [self.asyncTextInput handleAsyncKeyEvent:wrap(event).get() withCompletionHandler:[completion = makeBlockPtr(completion)](WKSEKeyEvent *event, BOOL handled) {
             completion(unwrap(event), handled);
         }];
         return;

LayoutTests/editing/deleting/ios/no-autoshift-after-deleting-character-in-word-expected.txt
@@ -1,0 +1,10 @@
+Verifies that typing a word and then deleting backwards does not cause the keyboard to automatically enter shift state.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS isAutoShifted is false
+PASS successfullyParsed is true
+
+TEST COMPLETE
+Hel

LayoutTests/editing/deleting/ios/no-autoshift-after-deleting-character-in-word.html
@@ -1,0 +1,51 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ useFlexibleViewport=true ] -->
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="../../../resources/ui-helper.js"></script>
+<script src="../../../resources/js-test.js"></script>
+<style>
+body, html {
+    font-size: 20px;
+    font-family: system-ui;
+}
+
+#editor {
+    border: solid 1px tomato;
+}
+</style>
+<script>
+jsTestIsAsync = true;
+
+addEventListener("load", async () => {
+    description("Verifies that typing a word and then deleting backwards does not cause the keyboard to automatically enter shift state.");
+
+    await UIHelper.setHardwareKeyboardAttached(false);
+
+    const editor = document.getElementById("editor");
+    await UIHelper.activateElementAndWaitForInputSession(editor);
+
+    for (let character of [..."Hello"]) {
+        await UIHelper.typeCharacter(character);
+        await UIHelper.ensurePresentationUpdate();
+    }
+
+    await UIHelper.keyDown("delete");
+    await UIHelper.ensurePresentationUpdate();
+    await UIHelper.keyDown("delete");
+    await UIHelper.ensurePresentationUpdate();
+
+    isAutoShifted = await UIHelper.keyboardIsAutomaticallyShifted();
+    shouldBeFalse("isAutoShifted");
+
+    editor.blur();
+    await UIHelper.waitForKeyboardToHide();
+
+    finishJSTest();
+});
+</script>
+</head>
+<body>
+<div contenteditable id="editor"><br></div>
+</body>
+</html>