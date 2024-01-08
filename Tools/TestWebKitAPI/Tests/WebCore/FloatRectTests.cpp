e73b8072d7b44b4246dd5a7dfdbad3de3b085d38
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2024-01-08 11:14:50 -0800

    Add more integer overflow tests for `enclosingIntRect` in FloatRectTests.cpp
    https://bugs.webkit.org/show_bug.cgi?id=267162
    
    Reviewed by Antti Koivisto.
    
    Partial Merge: https://chromium.googlesource.com/chromium/src.git/+/663b023cf7022eca119acc1b8b3a610cd8993da3
    
    This PR adds additional test from Blink commit and the underlying bug was already fixed in 189573@main.
    
    * Tools/TestWebKitAPI/Tests/WebCore/FloatRectTests.cpp:
    (EnclosingIntRect):
    
    Canonical link: https://commits.webkit.org/272779@main

--- Tools/TestWebKitAPI/Tests/WebCore/FloatRectTests.cpp
+++ Tools/TestWebKitAPI/Tests/WebCore/FloatRectTests.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 Apple Inc. All rights reserved.
+ * Copyright (C) 2017 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -722,6 +723,41 @@ TEST(FloatRect, EnclosingIntRect)
     EXPECT_EQ(INT_MIN, enclosed2.y());
     EXPECT_EQ(INT_MAX, enclosed2.width());
     EXPECT_EQ(INT_MAX, enclosed2.height());
+
+    WebCore::FloatRect smallDimensionsRect(42.5f, 84.5f, std::numeric_limits<float>::epsilon(), std::numeric_limits<float>::epsilon());
+    EXPECT_EQ(WebCore::IntRect(42, 84, 1, 1), WebCore::enclosingIntRect(smallDimensionsRect));
+
+    WebCore::FloatRect integralRect(100, 150, 200, 350);
+    EXPECT_EQ(WebCore::IntRect(100, 150, 200, 350), WebCore::enclosingIntRect(integralRect));
+
+    WebCore::FloatRect fractionalPosRect(100.6f, 150.8f, 200, 350);
+    EXPECT_EQ(WebCore::IntRect(100, 150, 201, 351), WebCore::enclosingIntRect(fractionalPosRect));
+
+    WebCore::FloatRect fractionalDimensionsRect(100, 150, 200.6f, 350.4f);
+    EXPECT_EQ(WebCore::IntRect(100, 150, 201, 351), WebCore::enclosingIntRect(fractionalDimensionsRect));
+
+    WebCore::FloatRect fractionalBothRect1(100.6f, 150.8f, 200.4f, 350.2f);
+    EXPECT_EQ(WebCore::IntRect(100, 150, 201, 351), WebCore::enclosingIntRect(fractionalBothRect1));
+
+    WebCore::FloatRect fractionalBothRect2(100.6f, 150.8f, 200.3f, 350.3f);
+    EXPECT_EQ(WebCore::IntRect(100, 150, 201, 352), WebCore::enclosingIntRect(fractionalBothRect2));
+
+    WebCore::FloatRect fractionalBothRect3(100.6f, 150.8f, 200.5f, 350.3f);
+    EXPECT_EQ(WebCore::IntRect(100, 150, 202, 352), WebCore::enclosingIntRect(fractionalBothRect3));
+
+    WebCore::FloatRect fractionalNegposRect1(-100.4f, -150.8f, 200, 350);
+    EXPECT_EQ(WebCore::IntRect(-101, -151, 201, 351), WebCore::enclosingIntRect(fractionalNegposRect1));
+
+    WebCore::FloatRect fractionalNegposRect2(-100.4f, -150.8f, 199.4f, 350.3f);
+    EXPECT_EQ(WebCore::IntRect(-101, -151, 200, 351), WebCore::enclosingIntRect(fractionalNegposRect2));
+
+    WebCore::FloatRect fractionalNegposRect3(-100.6f, -150.8f, 199.6f, 350.3f);
+    EXPECT_EQ(WebCore::IntRect(-101, -151, 201, 351), WebCore::enclosingIntRect(fractionalNegposRect3));
+
+    constexpr int intMin = std::numeric_limits<int>::min();
+    constexpr int intMax = std::numeric_limits<int>::max();
+    WebCore::FloatRect maxRect(-std::numeric_limits<float>::max() / 2, -std::numeric_limits<float>::max() / 2, std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+    EXPECT_EQ(WebCore::IntRect(intMin, intMin, intMax, intMax), WebCore::enclosingIntRect(maxRect));
 }
 
 TEST(FloatRect, RoundedIntRect)
