WebKit/WebKit

commit 0118132087d6ae990831f2b4ed72a7ff22e2e365

Pass an isolated copy of Settings to workers and worklets. https://bugs.webkit.org/show_bug.cgi?id=219688
Reviewed by Sam Weinig.

Source/JavaScriptCore:

* runtime/RuntimeFlags.h:
(JSC::RuntimeFlags::isolatedCopy const):

Source/WebCore:

Generate a Settings::Values struct containing all setting values.
Workers and worklets get that structure from their document and do a thread safe copy to use it in the background thread.
Covered by existing tests in workers.

* Modules/webaudio/AudioWorkletMessagingProxy.cpp:
(WebCore::generateWorkletParameters):
* Scripts/SettingsTemplates/Settings.cpp.erb:
* Scripts/SettingsTemplates/Settings.h.erb:
* dom/Document.h:
* dom/EmptyScriptExecutionContext.h:
* dom/ScriptExecutionContext.h:
* page/Settings.yaml:
* workers/WorkerGlobalScope.cpp:
(WebCore::WorkerGlobalScope::WorkerGlobalScope):
* workers/WorkerGlobalScope.h:
(WebCore::WorkerGlobalScope::requestAnimationFrameEnabled const):
(WebCore::WorkerGlobalScope::acceleratedCompositingEnabled const):
(WebCore::WorkerGlobalScope::webGLEnabled const):
* workers/WorkerMessagingProxy.cpp:
(WebCore::WorkerMessagingProxy::startWorkerGlobalScope):
* workers/WorkerThread.cpp:
(WebCore::WorkerParameters::isolatedCopy const):
* workers/WorkerThread.h:
* workers/service/context/ServiceWorkerThread.cpp:
(WebCore::ServiceWorkerThread::ServiceWorkerThread):
* workers/service/context/ServiceWorkerThread.h:
* workers/service/context/ServiceWorkerThreadProxy.cpp:
(WebCore::ServiceWorkerThreadProxy::ServiceWorkerThreadProxy):
* worklets/WorkletGlobalScope.cpp:
(WebCore::WorkletGlobalScope::WorkletGlobalScope):
* worklets/WorkletGlobalScope.h:
* worklets/WorkletParameters.h:
(WebCore::WorkletParameters::isolatedCopy const):

Source/WTF:

Add missing default values.

* Scripts/Preferences/WebPreferences.yaml:


Canonical link: https://commits.webkit.org/232425@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@270762 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/Scripts/SettingsTemplates/Settings.cpp.erb
@@ -66,13 +66,19 @@ Ref<Settings> Settings::create(Page* page)
 
 Settings::Settings(Page* page)
     : SettingsBase(page)
+{
+    m_values.initialize();
+}
+
+void Settings::Values::initialize()
+{
 <%- for @condition in @allSettingsSet.conditions do -%>
 <%- if @condition.nonBoolSettingsNeedingImplementation.length != 0 -%>
 <%- if @condition.condition -%>
 #if <%= @condition.condition %>
 <%- end -%>
 <%- for @setting in @condition.nonBoolSettingsNeedingImplementation -%>
-    , m_<%= @setting.name %>(SETTING_DEFAULT_VALUE_FOR_<%= @setting.name %>)
+    <%= @setting.name %> = SETTING_DEFAULT_VALUE_FOR_<%= @setting.name %>;
 <%- end -%>
 <%- if @condition.condition -%>
 #endif
@@ -85,15 +91,48 @@ Settings::Settings(Page* page)
 #if <%= @condition.condition %>
 <%- end -%>
 <%- for @setting in @condition.boolSettingsNeedingImplementation -%>
-    , m_<%= @setting.name %>(SETTING_DEFAULT_VALUE_FOR_<%= @setting.name %>)
+    <%= @setting.name %> = SETTING_DEFAULT_VALUE_FOR_<%= @setting.name %>;
 <%- end -%>
 <%- if @condition.condition -%>
 #endif
 <%- end -%>
 <%- end -%>
 <%- end -%>
+}
 
+Settings::Values Settings::Values::isolatedCopy() const
 {
+    return {
+<%- for @setting in @allSettingsSet.inspectorOverrideSettings do -%>
+        crossThreadCopy(<%= @setting.name %>InspectorOverride),
+<%- end -%>
+<%- for @condition in @allSettingsSet.conditions do -%>
+<%- if @condition.nonBoolSettingsNeedingImplementation.length != 0 -%>
+<%- if @condition.condition -%>
+#if <%= @condition.condition %>
+<%- end -%>
+<%- for @setting in @condition.nonBoolSettingsNeedingImplementation -%>
+        crossThreadCopy(<%= @setting.name %>),
+<%- end -%>
+<%- if @condition.condition -%>
+#endif
+<%- end -%>
+<%- end -%>
+<%- end -%>
+<%- for @condition in @allSettingsSet.conditions do -%>
+<%- if @condition.boolSettingsNeedingImplementation.length != 0 -%>
+<%- if @condition.condition -%>
+#if <%= @condition.condition %>
+<%- end -%>
+<%- for @setting in @condition.boolSettingsNeedingImplementation -%>
+        crossThreadCopy(<%= @setting.name %>),
+<%- end -%>
+<%- if @condition.condition -%>
+#endif
+<%- end -%>
+<%- end -%>
+<%- end -%>
+    };
 }
 
 Settings::~Settings()
@@ -109,21 +148,21 @@ Settings::~Settings()
 <%= @setting.parameterType %> Settings::<%= @setting.getterFunctionName %>() const
 {
 <%- if @setting.hasInspectorOverride? -%>
-    if (UNLIKELY(m_<%= @setting.name %>InspectorOverride)) {
+    if (UNLIKELY(m_values.<%= @setting.name %>InspectorOverride)) {
         ASSERT(InspectorInstrumentation::hasFrontends());
-        return m_<%= @setting.name %>InspectorOverride.value();
+        return m_values.<%= @setting.name %>InspectorOverride.value();
     }
 <%- end -%>
-    return m_<%= @setting.name %>;
+    return m_values.<%= @setting.name %>;
 }
 <%- end -%>
 
 <%- for @setting in @condition.settingsWithComplexSettersNeedingImplementation -%>
 void Settings::<%= @setting.setterFunctionName %>(<%= @setting.parameterType %> <%= @setting.name %>)
 {
-    if (m_<%= @setting.name %> == <%= @setting.name %>)
+    if (m_values.<%= @setting.name %> == <%= @setting.name %>)
         return;
-    m_<%= @setting.name %> = <%= @setting.name %>;
+    m_values.<%= @setting.name %> = <%= @setting.name %>;
     <%= @setting.onChange %>();
 }
 <%- end -%>
@@ -137,9 +176,9 @@ void Settings::<%= @setting.setterFunctionName %>(<%= @setting.parameterType %> 
 <%- if @setting.hasComplexSetter? -%>
 void Settings::<%= @setting.setterFunctionName %>InspectorOverride(Optional<<%= @setting.parameterType %>> <%= @setting.name %>InspectorOverride)
 {
-    if (m_<%= @setting.name %>InspectorOverride == <%= @setting.name %>InspectorOverride)
+    if (m_values.<%= @setting.name %>InspectorOverride == <%= @setting.name %>InspectorOverride)
         return;
-    m_<%= @setting.name %>InspectorOverride = <%= @setting.name %>InspectorOverride;
+        m_values.<%= @setting.name %>InspectorOverride = <%= @setting.name %>InspectorOverride;
 <%- if @setting.condition -%>
 #if <%= @setting.condition %>
 <%- end -%>

Source/JavaScriptCore/runtime/RuntimeFlags.h
@@ -90,6 +90,8 @@ public:
         );
     }
 
+    RuntimeFlags isolatedCopy() const { return RuntimeFlags(m_flags); }
+
 private:
     unsigned m_flags;
 };

Source/WTF/Scripts/Preferences/WebPreferences.yaml
@@ -600,7 +600,7 @@ DefaultTextEncodingName:
     WebKit:
       default: WebCore::defaultTextEncodingNameForSystemLanguage()
     WebCore:
-      default:
+      default: '{ }'
 
 DeferredCSSParserEnabled:
   type: bool

Source/WebCore/Modules/webaudio/AudioWorkletMessagingProxy.cpp
@@ -52,7 +52,8 @@ static WorkletParameters generateWorkletParameters(AudioWorklet& worklet)
         document->url(),
         jsRuntimeFlags,
         worklet.audioContext() ? worklet.audioContext()->sampleRate() : 0.0f,
-        worklet.identifier()
+        worklet.identifier(),
+        document->settingsValues()
     };
 }
 

Source/WebCore/Scripts/SettingsTemplates/Settings.h.erb
@@ -48,12 +48,12 @@ public:
     <%- if @setting.hasComplexGetter? -%>
     WEBCORE_EXPORT <%= @setting.parameterType %> <%= @setting.getterFunctionName %>() const;
     <%- else -%>
-    <%= @setting.parameterType %> <%= @setting.getterFunctionName %>() const { return m_<%= @setting.name %>; } 
+    <%= @setting.parameterType %> <%= @setting.getterFunctionName %>() const { return m_values.<%= @setting.name %>; }
     <%- end -%>
     <%- if @setting.hasComplexSetter? -%>
     WEBCORE_EXPORT void <%= @setting.setterFunctionName %>(<%= @setting.parameterType %>);
     <%- else -%>
-    void <%= @setting.setterFunctionName %>(<%= @setting.parameterType %> <%= @setting.name %>) { m_<%= @setting.name %> = <%= @setting.name %>; }
+    void <%= @setting.setterFunctionName %>(<%= @setting.parameterType %> <%= @setting.name %>) { m_values.<%= @setting.name %> = <%= @setting.name %>; }
     <%- end -%>
 <%- end -%>
 <%- if @condition.condition -%>
@@ -65,15 +65,16 @@ public:
     <%- if @setting.hasComplexSetter? -%>
     WEBCORE_EXPORT void <%= @setting.setterFunctionName %>InspectorOverride(Optional<<%= @setting.parameterType %>>);
     <%- else -%>
-    void <%= @setting.setterFunctionName %>InspectorOverride(Optional<<%= @setting.parameterType %>> <%= @setting.name %>InspectorOverride) { m_<%= @setting.name %>InspectorOverride = <%= @setting.name %>InspectorOverride; }
+    void <%= @setting.setterFunctionName %>InspectorOverride(Optional<<%= @setting.parameterType %>> <%= @setting.name %>InspectorOverride) { m_values.<%= @setting.name %>InspectorOverride = <%= @setting.name %>InspectorOverride; }
     <%- end -%>
 <%- end -%>
 
-private:
-    WEBCORE_EXPORT explicit Settings(Page*);
+    struct Values {
+        void initialize();
+        Values isolatedCopy() const;
 
 <%- for @setting in @allSettingsSet.inspectorOverrideSettings do -%>
-    Optional<<%= @setting.type %>> m_<%= @setting.name %>InspectorOverride;
+        Optional<<%= @setting.type %>> <%= @setting.name %>InspectorOverride;
 <%- end -%>
 
 <%- for @condition in @allSettingsSet.conditions do -%>
@@ -82,28 +83,33 @@ private:
 #if <%= @condition.condition %>
 <%- end -%>
 <%- for @setting in @condition.nonBoolSettingsNeedingImplementation -%>
-    <%= @setting.type %> m_<%= @setting.name %>;
+        <%= @setting.type %> <%= @setting.name %>;
 <%- end -%>
 <%- if @condition.condition -%>
 #endif
 <%- end -%>
 <%- end -%>
 <%- end -%>
-
 <%- for @condition in @allSettingsSet.conditions do -%>
-<%- if @condition.boolSettingsNeedingImplementation.length != 0 -%>
-<%- if @condition.condition -%>
+    <%- if @condition.settingsNeedingImplementation.length != 0 -%>
+        <%- if @condition.condition -%>
 #if <%= @condition.condition %>
-<%- end -%>
-<%- for @setting in @condition.boolSettingsNeedingImplementation -%>
-    <%= @setting.type %> m_<%= @setting.name %> : 1;
-<%- end -%>
-<%- if @condition.condition -%>
+        <%- end -%>
+        <%- for @setting in @condition.boolSettingsNeedingImplementation -%>
+        <%= @setting.type %> <%= @setting.name %> : 1;
+        <%- end -%>
+        <%- if @condition.condition -%>
 #endif
+        <%- end -%>
+    <%- end -%>
 <%- end -%>
-<%- end -%>
-<%- end -%>
+    };
+    const Values& values() const { return m_values; }
 
+private:
+    WEBCORE_EXPORT explicit Settings(Page*);
+
+    Values m_values;
 };
 
 }

Source/WebCore/dom/Document.h
@@ -624,6 +624,8 @@ public:
     void resumeActiveDOMObjects(ReasonForSuspension) final;
     void stopActiveDOMObjects() final;
 
+    const Settings::Values& settingsValues() const final { return settings().values(); }
+
     void suspendDeviceMotionAndOrientationUpdates();
     void resumeDeviceMotionAndOrientationUpdates();
 

Source/WebCore/dom/EmptyScriptExecutionContext.h
@@ -94,6 +94,8 @@ private:
     void refScriptExecutionContext() final { ref(); };
     void derefScriptExecutionContext() final { deref(); };
 
+    const Settings::Values& settingsValues() const final { return m_settingsValues; }
+
     class EmptyEventLoop final : public EventLoop {
     public:
         static Ref<EmptyEventLoop> create(JSC::VM& vm)
@@ -104,7 +106,7 @@ private:
         MicrotaskQueue& microtaskQueue() final { return m_queue; };
 
     private:
-        EmptyEventLoop(JSC::VM& vm)
+        explicit EmptyEventLoop(JSC::VM& vm)
             : m_queue(MicrotaskQueue(vm))
         {
         }
@@ -120,6 +122,7 @@ private:
     URL m_url;
     Ref<EmptyEventLoop> m_eventLoop;
     std::unique_ptr<EventLoopTaskGroup> m_eventLoopTaskGroup;
+    Settings::Values m_settingsValues;
 };
 
 } // namespace WebCore

Source/WebCore/dom/ScriptExecutionContext.h
@@ -31,6 +31,7 @@
 #include "DOMTimer.h"
 #include "SecurityContext.h"
 #include "ServiceWorkerTypes.h"
+#include "Settings.h"
 #include <JavaScriptCore/ConsoleTypes.h>
 #include <JavaScriptCore/HandleTypes.h>
 #include <wtf/CrossThreadTask.h>
@@ -102,6 +103,8 @@ public:
     virtual String userAgent(const URL&) const = 0;
 
     virtual ReferrerPolicy referrerPolicy() const = 0;
+
+    virtual const Settings::Values& settingsValues() const = 0;
 
     virtual void disableEval(const String& errorMessage) = 0;
     virtual void disableWebAssembly(const String& errorMessage) = 0;

commit 27103d255680b06fcb2ec713f2844c14ac37312e

Fix warnings found by compiling with -Wmisleading-indentation https://bugs.webkit.org/show_bug.cgi?id=267196 <rdar://120600518>
Reviewed by Darin Adler.

* Source/WebCore/Scripts/SettingsTemplates/Settings.cpp.erb:
- Fix indentation.  This regressed in 232425@main.
* Source/WebCore/platform/ios/wak/WKView.mm:
(_WKViewAutoresizeCoord):
- Fix some long-standing indentation issues that were upstreamed from
  the internal fork of iOS WebKit in 144613@main.

Canonical link: https://commits.webkit.org/272772@main

Source/WebCore/Scripts/SettingsTemplates/Settings.cpp.erb
@@ -214,7 +214,7 @@ void Settings::<%= @setting.setterFunctionName %>InspectorOverride(std::optional
 {
     if (m_values.<%= @setting.name %>InspectorOverride == <%= @setting.name %>InspectorOverride)
         return;
-        m_values.<%= @setting.name %>InspectorOverride = <%= @setting.name %>InspectorOverride;
+    m_values.<%= @setting.name %>InspectorOverride = <%= @setting.name %>InspectorOverride;
 <%- if @setting.condition -%>
 #if <%= @setting.condition %>
 <%- end -%>

Source/WebCore/platform/ios/wak/WKView.mm
@@ -229,7 +229,7 @@ static void _WKViewAutoresizeCoord(bool bByHeight, unsigned int sizingMethod, co
             widthOrHeight = newSuperFrameWidthOrHeight - (origSuperFrameWidthOrHeight - widthOrHeight);
             if (widthOrHeight < 0.0f)
                 widthOrHeight = 0.0f;
-                break;
+            break;
         case NSViewWidthSizable | NSViewMaxXMargin:
             origWidthMinusMinMargin = origSuperFrameWidthOrHeight - xOrY;
             if (widthOrHeight) {
@@ -244,7 +244,7 @@ static void _WKViewAutoresizeCoord(bool bByHeight, unsigned int sizingMethod, co
                 widthOrHeight = ((newSuperFrameWidthOrHeight - xOrY)) * prop;
             if (widthOrHeight < 0.0f)
                 widthOrHeight = 0.0f;
-                break;
+            break;
         case NSViewMinXMargin:
             xOrY = newSuperFrameWidthOrHeight - (origSuperFrameWidthOrHeight - xOrY);
             if (xOrY < 0.0f)
@@ -268,7 +268,7 @@ static void _WKViewAutoresizeCoord(bool bByHeight, unsigned int sizingMethod, co
                 xOrY = ((newSuperFrameWidthOrHeight - widthOrHeight)) * prop;
             if (xOrY < 0.0f)
                 xOrY = 0.0f;
-                break;
+            break;
         case NSViewMinXMargin | NSViewWidthSizable:
             tmp = xOrY + widthOrHeight;
             if (tmp)
@@ -279,9 +279,9 @@ static void _WKViewAutoresizeCoord(bool bByHeight, unsigned int sizingMethod, co
             widthOrHeight  = newSuperFrameWidthOrHeight - (xOrY + (origSuperFrameWidthOrHeight - tmp));
             if (xOrY < 0.0f)
                 xOrY = 0.0f;
-                if (widthOrHeight < 0.0f)
-                    widthOrHeight = 0.0f;
-                    break;
+            if (widthOrHeight < 0.0f)
+                widthOrHeight = 0.0f;
+            break;
         case NSViewMinXMargin | NSViewWidthSizable | NSViewMaxXMargin:
             if (origSuperFrameWidthOrHeight)
                 prop = xOrY / origSuperFrameWidthOrHeight;