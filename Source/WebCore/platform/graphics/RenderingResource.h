86d1981ef878451ea42fdaf4f6f5e98ac0923975
Chris Dumez <cdumez@apple.com>
2024-01-11 09:08:25 -0800

    Stop using CheckedPtr in RenderingResource::m_observers https://bugs.webkit.org/show_bug.cgi?id=267365
    Reviewed by Darin Adler.
    
    Stop using CheckedPtr in RenderingResource::m_observers. Per our recent smart
    pointer guidelines, we restrict CheckedPtr/CheckedRef usage to stack variables.
    
    Use a WeahHashSet instead.
    
    * Source/WebCore/platform/graphics/RenderingResource.h:
    (WebCore::RenderingResource::~RenderingResource):
    (WebCore::RenderingResource::addObserver):
    (WebCore::RenderingResource::removeObserver):
    
    Canonical link: https://commits.webkit.org/272916@main

--- Source/WebCore/platform/graphics/RenderingResource.h
+++ Source/WebCore/platform/graphics/RenderingResource.h
@@ -26,16 +26,15 @@
 #pragma once
 
 #include "RenderingResourceIdentifier.h"
-#include <wtf/CheckedPtr.h>
-#include <wtf/HashSet.h>
 #include <wtf/ThreadSafeWeakPtr.h>
+#include <wtf/WeakHashSet.h>
 
 namespace WebCore {
 
 class RenderingResource
     : public ThreadSafeRefCountedAndCanMakeThreadSafeWeakPtr<RenderingResource> {
 public:
-    class Observer : public CanMakeCheckedPtr {
+    class Observer : public CanMakeWeakPtr<Observer> {
     public:
         virtual ~Observer() = default;
         virtual void releaseRenderingResource(RenderingResourceIdentifier) = 0;
@@ -47,8 +46,9 @@ public:
     {
         if (!hasValidRenderingResourceIdentifier())
             return;
+
         for (auto& observer : m_observers)
-            observer->releaseRenderingResource(renderingResourceIdentifier());
+            observer.releaseRenderingResource(renderingResourceIdentifier());
     }
 
     virtual bool isNativeImage() const { return false; }
@@ -75,13 +75,13 @@ public:
     void addObserver(Observer& observer)
     {
         ASSERT(hasValidRenderingResourceIdentifier());
-        m_observers.add(&observer);
+        m_observers.add(observer);
     }
 
     void removeObserver(Observer& observer)
     {
         ASSERT(hasValidRenderingResourceIdentifier());
-        m_observers.remove(&observer);
+        m_observers.remove(observer);
     }
 
 protected:
@@ -90,7 +90,7 @@ protected:
     {
     }
 
-    HashSet<CheckedPtr<Observer>> m_observers;
+    WeakHashSet<Observer> m_observers;
     std::optional<RenderingResourceIdentifier> m_renderingResourceIdentifier;
 };
 
