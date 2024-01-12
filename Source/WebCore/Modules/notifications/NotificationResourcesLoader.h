21c1140b536ca89bfc2c3feaa1e6404d11839160
Vitaly Dyachkov <vitaly@igalia.com>
2024-01-12 05:52:42 -0800

    [GTK][WPE][Debug] imported/w3c/web-platform-tests/notifications/instance.https.html is a constant crash https://bugs.webkit.org/show_bug.cgi?id=267132
    Reviewed by Carlos Garcia Campos.
    
    Currently, when `NotificationResourcesLoader` starts downloading
    notification resources, we create a "ResourceLoader" per resource
    (we only support the icon resource for now) and add it to the set of
    running loaders `m_loaders`.
    
    When a `ResourceLoader` finishes, we check if it was the last one
    running. If yes, we run the `NotificationResourcesLoader`'s completion
    handler.
    
    But in some circumstances (e.g. when the icon URL is bogus)
    a `ResouceLoader` can execute its completion handler synchronously
    during construction.
    
    In this case, the `NotificationResourcesLoader`'s completion handler
    will be called from `didFinishLoadingResource()` because
    `m_loaders.isEmpty()` since we haven't added the loader to the set yet.
    
    To prevent it, this patch checks that `ResouceLoader` hasn't finished
    yet before adding it to the set of running loaders.
    
    * Source/WebCore/Modules/notifications/NotificationResourcesLoader.cpp:
    (WebCore::NotificationResourcesLoader::start):
    (WebCore::NotificationResourcesLoader::stop):
    (WebCore::NotificationResourcesLoader::didFinishLoadingResource):
    (WebCore::NotificationResourcesLoader::ResourceLoader::didFinishLoading):
    (WebCore::NotificationResourcesLoader::ResourceLoader::didFail):
    * Source/WebCore/Modules/notifications/NotificationResourcesLoader.h:
    
    Canonical link: https://commits.webkit.org/272970@main

--- Source/WebCore/Modules/notifications/NotificationResourcesLoader.h
+++ Source/WebCore/Modules/notifications/NotificationResourcesLoader.h
@@ -61,6 +61,8 @@ private:
 
         void cancel();
 
+        bool finished() const { return m_finished; }
+
     private:
         // ThreadableLoaderClient API.
         void didReceiveResponse(ResourceLoaderIdentifier, const ResourceResponse&) final;
@@ -68,6 +70,7 @@ private:
         void didFinishLoading(ResourceLoaderIdentifier, const NetworkLoadMetrics&) final;
         void didFail(const ResourceError&) final;
 
+        bool m_finished { false };
         SharedBufferBuilder m_buffer;
         RefPtr<BitmapImage> m_image;
         RefPtr<ThreadableLoader> m_loader;
@@ -77,6 +80,7 @@ private:
     void didFinishLoadingResource(ResourceLoader*);
 
     Notification& m_notification;
+    bool m_stopped { false };
     CompletionHandler<void(RefPtr<NotificationResources>&&)> m_completionHandler;
     HashSet<std::unique_ptr<ResourceLoader>> m_loaders;
     RefPtr<NotificationResources> m_resources;
