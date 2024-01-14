6d2f579ca0ad2281c51cfa19fce228dcb66cd2b5
Alan Baradlay <zalan@apple.com>
2024-01-14 15:52:12 -0800

    Invalidate existing inline layout content when simplified out-of-flow is sufficient https://bugs.webkit.org/show_bug.cgi?id=267487 rdar://120496542
    Reviewed by Antti Koivisto.
    
    Do not leave stale inline content around.
    
    * LayoutTests/fast/inline/dynamic-inline-content-with-out-of-flow-child-expected.txt: Added.
    * LayoutTests/fast/inline/dynamic-inline-content-with-out-of-flow-child.html: Added.
    * Source/WebCore/rendering/RenderBlockFlow.cpp:
    (WebCore::RenderBlockFlow::layoutModernLines):
    
    Canonical link: https://commits.webkit.org/272448.102@safari-7618-branch

--- Source/WebCore/rendering/RenderBlockFlow.cpp
+++ Source/WebCore/rendering/RenderBlockFlow.cpp
@@ -4031,6 +4031,8 @@ void RenderBlockFlow::layoutModernLines(bool relayoutChildren, LayoutUnit& repai
 
     if (hasSimpleOutOfFlowContentOnly) {
         // Shortcut the layout.
+        m_lineLayout = std::monostate();
+
         setStaticPositionsForSimpleOutOfFlowContent();
         setLogicalHeight(borderAndPaddingBefore() + borderAndPaddingAfter() + scrollbarLogicalHeight());
         return;
