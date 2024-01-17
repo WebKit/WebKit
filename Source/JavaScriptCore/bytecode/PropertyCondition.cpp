5c9b579ee6cefa34f8fd14ff3824bf371a2f2d60
Russell Epstein <repstein@apple.com>
2024-01-16 17:47:42 -0800

    Cherry-pick 153c0530aea6. rdar://120375878
        isStillValidAssumingImpurePropertyWatchpoint AbsenceOfSetEffect should check special property first
        https://bugs.webkit.org/show_bug.cgi?id=266985
        rdar://120375878
    
        Reviewed by Justin Michaud.
    
        * Source/JavaScriptCore/bytecode/PropertyCondition.cpp:
        (JSC::PropertyCondition::isStillValidAssumingImpurePropertyWatchpoint const):
    
        Canonical link: https://commits.webkit.org/272614@main
    
    Canonical link: https://commits.webkit.org/272448.216@safari-7618-branch

--- Source/JavaScriptCore/bytecode/PropertyCondition.cpp
+++ Source/JavaScriptCore/bytecode/PropertyCondition.cpp
@@ -175,6 +175,12 @@ bool PropertyCondition::isStillValidAssumingImpurePropertyWatchpoint(
             return false;
         }
         
+        if (structure->typeInfo().overridesPut() && JSObject::mightBeSpecialProperty(structure->vm(), structure->typeInfo().type(), uid())) {
+            if (PropertyConditionInternal::verbose)
+                dataLog("Invalid because its put() override may treat ", uid(), " property as special non-structure one.\n");
+            return false;
+        }
+
         unsigned currentAttributes;
         PropertyOffset currentOffset = structure->get(structure->vm(), concurrency, uid(), currentAttributes);
         if (currentOffset != invalidOffset) {
@@ -186,10 +192,6 @@ bool PropertyCondition::isStillValidAssumingImpurePropertyWatchpoint(
                 }
                 return false;
             }
-        } else if (structure->typeInfo().overridesPut() && JSObject::mightBeSpecialProperty(structure->vm(), structure->typeInfo().type(), uid())) {
-            if (PropertyConditionInternal::verbose)
-                dataLog("Invalid because its put() override may treat ", uid(), " property as special non-structure one.\n");
-            return false;
         } else if (structure->hasNonReifiedStaticProperties()) {
             if (auto entry = structure->findPropertyHashEntry(uid())) {
                 if (entry->value->attributes() & PropertyAttribute::ReadOnlyOrAccessorOrCustomAccessorOrValue) {
