830a3fbfc285ee62ade6802295dbf81f3f7962c2
Nikolaos Mouchtaris <nmouchtaris@apple.com>
2024-01-17 13:42:29 -0800

    [scroll-anchoring] cbssports.com: Video playing and webpage elements flickering when scrolling https://bugs.webkit.org/show_bug.cgi?id=267644 rdar://119959431
    Reviewed by Wenson Hsieh.
    
    On cbssports.com there is an undesired scroll anchoring adjustment when the user scrolls to dismiss
    the video, which cancels the user scroll and brings back up the video. Ideally, we would understand
    why this adjustment is occuring, but for now we should disable anchor adjustments during user scroll
    (like we do on iOS) to prevent them from interfering with user gestures.
    
    * Source/WebCore/page/scrolling/ScrollAnchoringController.cpp:
    (WebCore::ScrollAnchoringController::adjustScrollPositionForAnchoring):
    
    Canonical link: https://commits.webkit.org/273142@main

--- Source/WebCore/page/scrolling/ScrollAnchoringController.cpp
+++ Source/WebCore/page/scrolling/ScrollAnchoringController.cpp
@@ -361,13 +361,11 @@ void ScrollAnchoringController::adjustScrollPositionForAnchoring()
 
     FloatSize adjustment = computeOffsetFromOwningScroller(*renderer) - m_lastOffsetForAnchorElement;
     if (!adjustment.isZero()) {
-#if PLATFORM(IOS_FAMILY)
         if (m_owningScrollableArea.isUserScrollInProgress()) {
             invalidateAnchorElement();
             updateAnchorElement();
             return;
         }
-#endif
         auto newScrollPosition = m_owningScrollableArea.scrollPosition() + IntPoint(adjustment.width(), adjustment.height());
         RELEASE_LOG(ScrollAnchoring, "ScrollAnchoringController::updateScrollPosition() is main frame: %d, is main scroller: %d, adjusting from: (%d, %d) to: (%d, %d)",  frameView().frame().isMainFrame(), !m_owningScrollableArea.isRenderLayer(), m_owningScrollableArea.scrollPosition().x(), m_owningScrollableArea.scrollPosition().y(), newScrollPosition.x(), newScrollPosition.y());
         LOG_WITH_STREAM(ScrollAnchoring, stream << "ScrollAnchoringController::updateScrollPosition() for scroller element: " << ValueOrNull(elementForScrollableArea(m_owningScrollableArea)) << " anchor node: " << *m_anchorElement << "adjusting from: " << m_owningScrollableArea.scrollPosition() << " to: " << newScrollPosition);
