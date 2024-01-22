2b8eec7225d3ef8390aa6c2c06f9dbf4428cd62f
Ryosuke Niwa <rniwa@webkit.org>
2024-01-22 15:08:01 -0800

    Cherry-pick 8948469628f6. rdar://121086002
        querySelectorAll should consult document
        https://bugs.webkit.org/show_bug.cgi?id=267591
    
        Reviewed by Yusuke Suzuki.
    
        This PR makes querySelectorAll consult with Document.
    
        * Source/WebCore/dom/ContainerNode.cpp:
        (WebCore::ContainerNode::querySelectorAll):
        * Source/WebCore/dom/Document.cpp:
        (WebCore::Document::resultForSelectorAll): Added.
        (WebCore::Document::addResultForSelectorAll): Added.
        (WebCore::Document::invalidateQuerySelectorAllResults): Added.
        (WebCore::Document::clearQuerySelectorAllResults): Added.
        * Source/WebCore/dom/Document.h:
        * Source/WebCore/dom/EventTarget.h:
        (WebCore::EventTarget::hasValidQuerySelectorAllResults const): Added.
        (WebCore::EventTarget::setHasValidQuerySelectorAllResults): Added.
        * Source/WebCore/dom/Node.cpp:
        (WebCore::Node::invalidateNodeListAndCollectionCachesInAncestors):
        (WebCore::Node::invalidateNodeListAndCollectionCachesInAncestorsForAttribute):
        * Source/WebCore/dom/SelectorQuery.h:
        (WebCore::SelectorDataList::shouldStoreInDocument const): Added.
        (WebCore::SelectorQuery::shouldStoreInDocument const): Added.
        * Source/WebCore/dom/StaticNodeList.cpp:
        (WebCore::StaticWrapperNodeList::length const):
        (WebCore::StaticWrapperNodeList::item const):
        * Source/WebCore/dom/StaticNodeList.h:
        * Source/WebCore/page/MemoryRelease.cpp:
        (WebCore::releaseCriticalMemory):
    
        Canonical link: https://commits.webkit.org/273107@main
    
    Canonical link: https://commits.webkit.org/272448.284@safari-7618-branch

--- Source/WebCore/dom/EventTarget.h
+++ Source/WebCore/dom/EventTarget.h
@@ -171,6 +171,9 @@ public:
     bool isInGCReacheableRefMap() const { return hasEventTargetFlag(EventTargetFlag::IsInGCReachableRefMap); }
     void setIsInGCReacheableRefMap(bool flag) { setEventTargetFlag(EventTargetFlag::IsInGCReachableRefMap, flag); }
 
+    bool hasValidQuerySelectorAllResults() const { return hasEventTargetFlag(EventTargetFlag::HasValidQuerySelectorAllResults); }
+    void setHasValidQuerySelectorAllResults(bool flag) { setEventTargetFlag(EventTargetFlag::HasValidQuerySelectorAllResults, flag); }
+
 protected:
     enum ConstructNodeTag { ConstructNode };
     EventTarget() = default;
@@ -190,17 +193,18 @@ protected:
         IsConnected = 1 << 3,
         IsInShadowTree = 1 << 4,
         HasBeenInUserAgentShadowTree = 1 << 5,
+        HasValidQuerySelectorAllResults = 1 << 6,
         // Element bits
-        HasSyntheticAttrChildNodes = 1 << 6,
-        HasDuplicateAttribute = 1 << 7,
-        HasLangAttr = 1 << 8,
-        HasXMLLangAttr = 1 << 9,
-        HasFormAssociatedCustomElementInterface = 1 << 10,
-        HasShadowRootContainingSlots = 1 << 11,
-        IsInTopLayer = 1 << 12,
+        HasSyntheticAttrChildNodes = 1 << 7,
+        HasDuplicateAttribute = 1 << 8,
+        HasLangAttr = 1 << 9,
+        HasXMLLangAttr = 1 << 10,
+        HasFormAssociatedCustomElementInterface = 1 << 11,
+        HasShadowRootContainingSlots = 1 << 12,
+        IsInTopLayer = 1 << 13,
         // SVGElement bits
-        HasPendingResources = 1 << 13,
-        // 2 Free bits
+        HasPendingResources = 1 << 14,
+        // 1 Free bits
     };
 
     EventTargetData& ensureEventTargetData()
