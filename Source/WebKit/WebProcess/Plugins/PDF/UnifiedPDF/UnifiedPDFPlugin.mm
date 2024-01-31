WebKit/WebKit

commit 14b3ba128b7aa7dfbd192866ded19359364e1dcf

[UnifiedPDF] Text annotation HTML inputs do not get repositioned when scrolling https://bugs.webkit.org/show_bug.cgi?id=268358 rdar://121907236
Reviewed by Simon Fraser.

When the scroll offset is changed, we should make sure that the geometry
of any active annotation gets updated also. The annotation will update
its location based off the new scroll position.

* Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/UnifiedPDFPlugin.mm:
(WebKit::UnifiedPDFPlugin::didChangeScrollOffset):

Canonical link: https://commits.webkit.org/273837@main

Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/UnifiedPDFPlugin.mm
@@ -568,6 +568,9 @@ void UnifiedPDFPlugin::didChangeScrollOffset()
     else
         m_scrollContainerLayer->setBoundsOrigin(IntPoint(m_scrollOffset));
 
+    if (m_activeAnnotation)
+        m_activeAnnotation->updateGeometry();
+
     scheduleRenderingUpdate();
 }
 

commit ed7b19896be4fbd22b519f7d3e5b7b77fddce6f2

iOS build broken after 273837@main https://bugs.webkit.org/show_bug.cgi?id=268486 rdar://122029805
Unreviewed build fix.

* Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/UnifiedPDFPlugin.mm:
(WebKit::UnifiedPDFPlugin::didChangeScrollOffset):

Canonical link: https://commits.webkit.org/273846@main

Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/UnifiedPDFPlugin.mm
@@ -568,8 +568,10 @@ void UnifiedPDFPlugin::didChangeScrollOffset()
     else
         m_scrollContainerLayer->setBoundsOrigin(IntPoint(m_scrollOffset));
 
+#if PLATFORM(MAC)
     if (m_activeAnnotation)
         m_activeAnnotation->updateGeometry();
+#endif
 
     scheduleRenderingUpdate();
 }