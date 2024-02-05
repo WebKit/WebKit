91ae6a10f85ef1fb56bac952ae4615d282b68afa
Alan Baradlay <zalan@apple.com>
2024-02-05 12:47:04 -0800

    Cherry-pick 3b7a3e8caba6. rdar://121859917
        [IFC] Containing DIV with "display: inline; white-space: nowrap;" is breaking at the gap between two contained elements
        https://bugs.webkit.org/show_bug.cgi?id=268323
        <rdar://121859917>
    
        Reviewed by Antti Koivisto.
    
        Overflowing inline box decoration (e.g. <span style="border-inline-end: solid 20px blue">) is still considered part of the inline box content
        and the inline box's white-space value should apply (as opposed to its parent's style).
    
        * LayoutTests/fast/inline/overflowing-inline-box-decoration-in-nowrap-expected.html: Added.
        * LayoutTests/fast/inline/overflowing-inline-box-decoration-in-nowrap.html: Added.
        * Source/WebCore/layout/formattingContexts/inline/InlineContentBreaker.cpp:
        (WebCore::Layout::InlineContentBreaker::processOverflowingContent const):
    
        Canonical link: https://commits.webkit.org/273724@main
    
    Identifier: 272448.480@safari-7618-branch

--- Source/WebCore/layout/formattingContexts/inline/InlineContentBreaker.cpp
+++ Source/WebCore/layout/formattingContexts/inline/InlineContentBreaker.cpp
@@ -235,11 +235,12 @@ InlineContentBreaker::Result InlineContentBreaker::processOverflowingContent(con
     // Now either wrap this content over to the next line or revert back to an earlier wrapping opportunity, or not wrap at all.
     auto shouldWrapUnbreakableContentToNextLine = [&] {
         // The individual runs in this continuous content don't break, let's check if we are allowed to wrap this content to next line (e.g. pre would prevent us from wrapping).
-        // Parent style drives the wrapping behavior here.
-        // e.g. <div style="white-space: nowrap">some text<div style="display: inline-block; white-space: pre-wrap"></div></div>.
-        // While the inline-block has pre-wrap which allows wrapping, the content lives in a nowrap context.
-        auto& parentLayoutBox = continuousContent.runs()[overflowingRunIndex].inlineItem.layoutBox().parent();
-        return TextUtil::isWrappingAllowed(parentLayoutBox.style());
+        // Parent style drives the wrapping behavior here in case of non-inline-box content.
+        // e.g. <div style="white-space: nowrap">no wrap<div style="display: inline-block; white-space: normal">yes wrap</div></div>.
+        // While the inline-block has pre-wrap which allows wrapping (for its own content), the content lives in a nowrap context.
+        auto& overflowingBox = continuousContent.runs()[overflowingRunIndex].inlineItem.layoutBox();
+        auto& styleToUse = overflowingBox.isInlineBox() ? overflowingBox.style() : overflowingBox.parent().style();
+        return TextUtil::isWrappingAllowed(styleToUse);
     };
     if (shouldWrapUnbreakableContentToNextLine())
         return { Result::Action::Wrap, IsEndOfLine::Yes };
