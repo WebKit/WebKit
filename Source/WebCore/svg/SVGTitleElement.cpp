735533ad53e687295d8b34a9fbf2729c1e3d726d
Ryosuke Niwa <rniwa@webkit.org>
2024-02-05 12:55:14 -0800

    Cherry-pick ea4898b9b7ea. rdar://122164690
        SVGTitleElement should only notify Document when it's a child of document element
        https://bugs.webkit.org/show_bug.cgi?id=268578
    
        Reviewed by Yusuke Suzuki.
    
        * Source/WebCore/svg/SVGTitleElement.cpp:
        (WebCore::SVGTitleElement::insertedIntoAncestor):
        (WebCore::isTitleElementRemovedFromSVGSVGElement):
        (WebCore::SVGTitleElement::removedFromAncestor):
        (WebCore::SVGTitleElement::childrenChanged):
    
        Canonical link: https://commits.webkit.org/273974@main
    
    Identifier: 272448.491@safari-7618-branch

--- Source/WebCore/svg/SVGTitleElement.cpp
+++ Source/WebCore/svg/SVGTitleElement.cpp
@@ -24,6 +24,7 @@
 
 #include "Document.h"
 #include "SVGNames.h"
+#include "SVGSVGElement.h"
 #include <wtf/IsoMallocInlines.h>
 
 namespace WebCore {
@@ -44,20 +45,32 @@ Ref<SVGTitleElement> SVGTitleElement::create(const QualifiedName& tagName, Docum
 Node::InsertedIntoAncestorResult SVGTitleElement::insertedIntoAncestor(InsertionType insertionType, ContainerNode& parentOfInsertedTree)
 {
     auto result = SVGElement::insertedIntoAncestor(insertionType, parentOfInsertedTree);
-    document().titleElementAdded(*this);
+    if (insertionType.connectedToDocument && parentNode() == document().documentElement())
+        document().titleElementAdded(*this);
     return result;
+}
+
+static bool isTitleElementRemovedFromSVGSVGElement(SVGTitleElement& title, ContainerNode& oldParentOfRemovedTree)
+{
+    if (!title.parentNode() && is<SVGSVGElement>(oldParentOfRemovedTree) && title.document().documentElement() == &oldParentOfRemovedTree)
+        return true;
+    if (title.parentNode() && is<SVGSVGElement>(*title.parentNode()) && !title.parentNode()->parentNode() && is<Document>(oldParentOfRemovedTree))
+        return true;
+    return false;
 }
 
 void SVGTitleElement::removedFromAncestor(RemovalType removalType, ContainerNode& oldParentOfRemovedTree)
 {
     SVGElement::removedFromAncestor(removalType, oldParentOfRemovedTree);
-    document().titleElementRemoved(*this);
+    if (removalType.disconnectedFromDocument && isTitleElementRemovedFromSVGSVGElement(*this, oldParentOfRemovedTree))
+        document().titleElementRemoved(*this);
 }
 
 void SVGTitleElement::childrenChanged(const ChildChange& change)
 {
     SVGElement::childrenChanged(change);
-    document().titleElementTextChanged(*this);
+    if (isConnected() && parentNode() == document().documentElement())
+        document().titleElementTextChanged(*this);
 }
 
 }
