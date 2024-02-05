3b32ecb75fb30b487e412b988ced2df7c214b3c8
Ben Nham <nham@apple.com>
2024-02-05 12:49:20 -0800

    Cherry-pick 624a3d6250ec. rdar://120213426
        Limit size of target string in serialized session state
        https://bugs.webkit.org/show_bug.cgi?id=268410
        rdar://120213426
    
        Reviewed by Brady Eidson.
    
        We've seen some MobileSafari launches that take a long time during session restoration because the
        _WKSessionState object contains a back/forward list item with an absurdly large target string (like
        >500KB in size).
    
        To fix this, we are going to:
    
        - Arbitrarily limit the size of the persisted target string in the session state to 32KB (or 16K
          characters).
        - Tweak our session state data limit policy. Previously, if an item caused us to trip over the data
          limit, we would still serialize that potentially huge item but then avoid serializing any
          subsequent item. Now, we also avoid serializing the potentially huge item that caused us to trip
          over the data limit.
        - The first item is still exempted from the data limit on the presumption that we want session
          restoration of that item to have the maximum chance of success (e.g. in the case of a frontmost
          jetsam, we want that the first item in the restored tab to come back with as much state as
          possible).
    
        * Source/WebKit/UIProcess/mac/LegacySessionStateCoding.cpp:
        (WebKit::encodeFrameStateNode):
        (WebKit::encodeSessionHistory):
    
        Canonical link: https://commits.webkit.org/273810@main
    
    Identifier: 272448.483@safari-7618-branch

--- Source/WebKit/UIProcess/mac/LegacySessionStateCoding.cpp
+++ Source/WebKit/UIProcess/mac/LegacySessionStateCoding.cpp
@@ -68,6 +68,8 @@ static const uint32_t maximumSessionStateDataSize = 2 * 1024 * 1024;
 static const uint32_t maximumSessionStateDataSize = std::numeric_limits<uint32_t>::max();
 #endif
 
+static const uint32_t maximumFrameStateTargetLength = 16 * 1024;
+
 template<typename T> void isValidEnum(T);
 
 
@@ -346,7 +348,10 @@ static void encodeFrameStateNode(HistoryEntryDataEncoder& encoder, const FrameSt
     if (frameState.stateObjectData)
         encoder << frameState.stateObjectData.value();
 
-    encoder << frameState.target;
+    if (frameState.target.length() < maximumFrameStateTargetLength)
+        encoder << frameState.target;
+    else
+        encoder << emptyAtom();
 
 #if PLATFORM(IOS_FAMILY)
     // FIXME: iOS should not use the legacy session state encoder.
@@ -428,15 +433,21 @@ static RetainPtr<CFDictionaryRef> encodeSessionHistory(const BackForwardListStat
         auto url = item.pageState.mainFrameState.urlString.createCFString();
         auto title = item.pageState.title.createCFString();
         auto originalURL = item.pageState.mainFrameState.originalURLString.createCFString();
+
+        // We allow the first item to be unlimited in size. We refrain from serializing the data for subsequent items if they would cause us to trip over the maximumSessionStateDataSize limit.
         auto data = totalDataSize <= maximumSessionStateDataSize ? encodeSessionHistoryEntryData(item.pageState.mainFrameState) : nullptr;
+        if (data) {
+            totalDataSize += CFDataGetLength(data.get());
+            if (totalDataSize > maximumSessionStateDataSize && CFArrayGetCount(entries.get()) > 0)
+                data = nullptr;
+        }
+
         auto shouldOpenExternalURLsPolicyValue = static_cast<uint64_t>(item.pageState.shouldOpenExternalURLsPolicy);
         auto shouldOpenExternalURLsPolicy = adoptCF(CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt64Type, &shouldOpenExternalURLsPolicyValue));
 
         RetainPtr<CFDictionaryRef> entryDictionary;
 
         if (data) {
-            totalDataSize += CFDataGetLength(data.get());
-
             entryDictionary = createDictionary({
                 { sessionHistoryEntryURLKey, url.get() },
                 { sessionHistoryEntryTitleKey, title.get() },
