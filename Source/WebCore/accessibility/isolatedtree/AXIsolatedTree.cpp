f54293e5ec75fc8645c24913d9d2fa9411dfb18e
Andres Gonzalez <andresg_22@apple.com>
2024-02-07 22:12:51 -0800

    Cherry-pick ffb3f7edd603. rdar://121874043
        AX: Announcements of percentage loaded do not honor VoiceOver preference of "play tone for web page loading."
        https://bugs.webkit.org/show_bug.cgi?id=268772
        rdar://121874043
    
        Reviewed by Chris Fleizach.
    
        AXObjectCache::announce causes VoiceOver to output a spoken message regardless of the user preference to announce percentage of page loaded. This patch removes this direct announcement. In a follow up patch, we will notify VoiceOver of page processing progress with a custom notification that VoiceOver will handle according to user preferences.
    
        * Source/WebCore/accessibility/isolatedtree/AXIsolatedTree.cpp:
        (WebCore::AXIsolatedTree::reportCreationProgress):
    
        Canonical link: https://commits.webkit.org/274126@main
    
    Canonical link: https://commits.webkit.org/272448.525@safari-7618.1.15-branch

--- Source/WebCore/accessibility/isolatedtree/AXIsolatedTree.cpp
+++ Source/WebCore/accessibility/isolatedtree/AXIsolatedTree.cpp
@@ -182,7 +182,6 @@ void AXIsolatedTree::reportCreationProgress(AXObjectCache& cache, unsigned perce
 
     String percent = String::number(percentComplete) + "%"_s;
     String title = AXProcessingPage() + " "_s + percent;
-    cache.announce(percent);
     if (RefPtr axRoot = cache.get(cache.document().view())) {
         m_replacingTree->overrideNodeProperties(axRoot->objectID(), {
             { AXPropertyName::TitleAttributeValue, title },
