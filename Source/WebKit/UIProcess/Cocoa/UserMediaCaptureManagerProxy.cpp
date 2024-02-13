01389d47b6ec1b09e825757b721ee79ea259d63e
Erica Li <lerica@apple.com>
2024-02-12 21:25:03 -0800

    WTFCrashWithSecurityImplication in WebCore::RealtimeMediaSource::fitnessDistance https://bugs.webkit.org/show_bug.cgi?id=268800 rdar://122105977
    Reviewed by Youenn Fablet.
    
    This is short-term suggested fix to add isValid check to MediaTrackConstraintSetMap to ensure each incomming contraint from IPC call has the right MediaConstraintType.
    
    * LayoutTests/ipc/create-media-source-with-invalid-constraints-crash-expected.txt: Added.
    * LayoutTests/ipc/create-media-source-with-invalid-constraints-crash.html: Added.
    * Source/WebCore/platform/mediastream/MediaConstraints.cpp:
    (WebCore::MediaTrackConstraintSetMap::isValid const):
    * Source/WebCore/platform/mediastream/MediaConstraints.h:
    * Source/WebKit/UIProcess/Cocoa/UserMediaCaptureManagerProxy.cpp:
    (WebKit::UserMediaCaptureManagerProxy::createMediaSourceForCaptureDeviceWithConstraints):
    (WebKit::UserMediaCaptureManagerProxy::applyConstraints):
    
    Canonical link: https://commits.webkit.org/272448.542@safari-7618-branch

--- Source/WebKit/UIProcess/Cocoa/UserMediaCaptureManagerProxy.cpp
+++ Source/WebKit/UIProcess/Cocoa/UserMediaCaptureManagerProxy.cpp
@@ -48,6 +48,7 @@
 #include <wtf/UniqueRef.h>
 
 #define MESSAGE_CHECK(assertion) MESSAGE_CHECK_BASE(assertion, &m_connectionProxy->connection())
+#define MESSAGE_CHECK_COMPLETION(assertion, completion) MESSAGE_CHECK_COMPLETION_BASE(assertion, &m_connectionProxy->connection(), completion)
 
 namespace WebKit {
 using namespace WebCore;
@@ -545,6 +546,10 @@ void UserMediaCaptureManagerProxy::createMediaSourceForCaptureDeviceWithConstrai
         return;
     }
 
+    MESSAGE_CHECK_COMPLETION(constraints->mandatoryConstraints.isValid(), completionHandler({ "Invalid mandatoryConstraints"_s, WebCore::MediaAccessDenialReason::InvalidConstraint }, { }, { }));
+    for (const auto& advancedConstraint : constraints->advancedConstraints)
+        MESSAGE_CHECK_COMPLETION(advancedConstraint.isValid(), completionHandler({ "Invalid advancedConstraints"_s, WebCore::MediaAccessDenialReason::InvalidConstraint }, { }, { }));
+
     proxy->applyConstraints(WTFMove(const_cast<WebCore::MediaConstraints&>(*constraints)), [proxy = WTFMove(proxy), id, completionHandler = WTFMove(completionHandler), completeSetup = WTFMove(completeSetup)](auto&& result) mutable {
         if (result) {
             completionHandler({ WTFMove(result->badConstraint), WebCore::MediaAccessDenialReason::InvalidConstraint }, { }, { });
@@ -602,6 +607,10 @@ void UserMediaCaptureManagerProxy::applyConstraints(RealtimeMediaSourceIdentifie
         m_connectionProxy->connection().send(Messages::UserMediaCaptureManager::ApplyConstraintsFailed(id, { }, "Unknown source"_s), 0);
         return;
     }
+
+    MESSAGE_CHECK(constraints.mandatoryConstraints.isValid());
+    for (const auto& advancedConstraint : constraints.advancedConstraints)
+        MESSAGE_CHECK(advancedConstraint.isValid());
 
     proxy->applyConstraints(WTFMove(constraints), [this, weakThis = WeakPtr { *this }, id, proxy](auto&& result) {
 
@@ -714,6 +723,7 @@ bool UserMediaCaptureManagerProxy::hasSourceProxies() const
 
 }
 
+#undef MESSAGE_CHECK_COMPLETION
 #undef MESSAGE_CHECK
 
 #endif
