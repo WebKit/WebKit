c9b84792fc392b4d3a6d2442f902dabf493fbad2
Don Olmstead <don.olmstead@sony.com>
2024-02-16 12:14:31 -0800

    Fix Windows build after 274842@main https://bugs.webkit.org/show_bug.cgi?id=269602
    Unreviewed build fix.
    
    Add `ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)` guard.
    
    * Source/JavaScriptCore/runtime/Options.cpp:
    
    Canonical link: https://commits.webkit.org/274865@main

--- Source/JavaScriptCore/runtime/Options.cpp
+++ Source/JavaScriptCore/runtime/Options.cpp
@@ -905,8 +905,10 @@ void Options::notifyOptionsChanged()
     Options::useWebAssemblyFastMemory() = false;
 #endif
 
+#if ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
     uint8_t* reservedConfigBytes = reinterpret_cast_ptr<uint8_t*>(WebConfig::g_config + WebConfig::reservedSlotsForExecutableAllocator);
     reservedConfigBytes[WebConfig::ReservedByteForAllocationProfiling] = Options::useAllocationProfiling() ? 1 : 0;
+#endif
 
     // Do range checks where needed and make corrections to the options:
     ASSERT(Options::thresholdForOptimizeAfterLongWarmUp() >= Options::thresholdForOptimizeAfterWarmUp());
