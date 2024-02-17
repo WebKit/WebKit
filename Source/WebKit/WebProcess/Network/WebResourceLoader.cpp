0f7a4c9df827d1b1d91d73d2b9908ed75390f59d
Per Arne Vollan <pvollan@apple.com>
2024-02-17 12:45:50 -0800

    Add syscall telemetry related to QuickLook https://bugs.webkit.org/show_bug.cgi?id=269608 rdar://114835579
    Reviewed by Brent Fulgham.
    
    This patch is adding telemetry in order to see which syscalls we can block in non QuickLook mode.
    This is only telemetry, there should be no behavior change from this patch.
    
    * Source/WTF/wtf/PlatformEnableCocoa.h:
    * Source/WebKit/Resources/SandboxProfiles/ios/com.apple.WebKit.WebContent.sb.in:
    * Source/WebKit/Scripts/process-entitlements.sh:
    * Source/WebKit/WebProcess/Network/WebResourceLoader.cpp:
    (WebKit::WebResourceLoader::didReceiveResponse):
    
    Canonical link: https://commits.webkit.org/274926@main

--- Source/WebKit/WebProcess/Network/WebResourceLoader.cpp
+++ Source/WebKit/WebProcess/Network/WebResourceLoader.cpp
@@ -58,6 +58,10 @@
 #include <WebCore/SubstituteData.h>
 #include <wtf/CompletionHandler.h>
 
+#if ENABLE(QUICKLOOK_SANDBOX_RESTRICTIONS)
+#include <wtf/spi/darwin/SandboxSPI.h>
+#endif
+
 #define WEBRESOURCELOADER_RELEASE_LOG(fmt, ...) RELEASE_LOG(Network, "%p - [webPageID=%" PRIu64 ", frameID=%" PRIu64 ", resourceID=%" PRIu64 ", durationSeconds=%.3f] WebResourceLoader::" fmt, this, m_trackingParameters.pageID.toUInt64(), m_trackingParameters.frameID.object().toUInt64(), m_trackingParameters.resourceID.toUInt64(), timeSinceLoadStart().value(), ##__VA_ARGS__)
 
 namespace WebKit {
@@ -158,6 +162,13 @@ void WebResourceLoader::didReceiveResponse(ResourceResponse&& response, PrivateR
     WEBRESOURCELOADER_RELEASE_LOG("didReceiveResponse: (httpStatusCode=%d)", response.httpStatusCode());
 
     Ref<WebResourceLoader> protectedThis(*this);
+
+#if ENABLE(QUICKLOOK_SANDBOX_RESTRICTIONS)
+    if (!response.isQuickLook()) {
+        auto auditToken = WebProcess::singleton().auditTokenForSelf();
+        sandbox_enable_state_flag("BlockQuickLookSandboxResources", *auditToken);
+    }
+#endif
 
     if (metrics) {
         metrics->workerStart = m_workerStart;
