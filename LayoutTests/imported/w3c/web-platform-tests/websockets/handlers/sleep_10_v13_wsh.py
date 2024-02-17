ee1b587c42b25411944e200640a2f93f305d0b3d
Guoye Zhang <guoye_zhang@apple.com>
2024-02-16 16:26:04 -0800

    Update layout tests for the unified HTTP stack https://bugs.webkit.org/show_bug.cgi?id=268193 rdar://121281240
    Reviewed by Alex Christensen.
    
    These tests are now reporting successes in the new HTTP stack, updating expectations:
    - http/tests/inspector/network/resource-request-headers.html
    - imported/w3c/web-platform-tests/cookie-store/change_eventhandler_for_http_cookie_and_set_cookie_headers.https.window.html
    - imported/w3c/web-platform-tests/cookies/name/name-ctl.html
    - imported/w3c/web-platform-tests/cookies/value/value-ctl.html
    
    These tests are expecting failures in the new HTTP stack, pending fixes:
    - imported/w3c/web-platform-tests/html/cross-origin-embedder-policy/header-parsing.https.html
    - imported/w3c/web-platform-tests/fetch/h1-parsing/status-code.window.html
    - imported/w3c/web-platform-tests/fetch/h1-parsing/lone-cr.window.html
    
    This test script is updated:
    - imported/w3c/web-platform-tests/websockets/interfaces/WebSocket/close/close-connecting.html
    
    * LayoutTests/imported/w3c/web-platform-tests/cookie-store/change_eventhandler_for_http_cookie_and_set_cookie_headers.https.window-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/cookies/name/name-ctl-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/cookies/value/value-ctl-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/websockets/handlers/sleep_10_v13_wsh.py:
    * LayoutTests/platform/ios-wk2/TestExpectations:
    * LayoutTests/platform/mac-wk2/TestExpectations:
    * LayoutTests/platform/mac-wk2/http/tests/inspector/network/resource-request-headers-expected.txt:

--- LayoutTests/imported/w3c/web-platform-tests/websockets/handlers/sleep_10_v13_wsh.py
+++ LayoutTests/imported/w3c/web-platform-tests/websockets/handlers/sleep_10_v13_wsh.py
@@ -4,15 +4,15 @@ import sys, urllib, time
 from mod_pywebsocket import msgutil
 
 def web_socket_do_extra_handshake(request):
-    request.connection.write(b'x')
+    request.connection.write(b'H')
     time.sleep(2)
-    request.connection.write(b'x')
+    request.connection.write(b'T')
     time.sleep(2)
-    request.connection.write(b'x')
+    request.connection.write(b'T')
     time.sleep(2)
-    request.connection.write(b'x')
+    request.connection.write(b'P')
     time.sleep(2)
-    request.connection.write(b'x')
+    request.connection.write(b'/')
     time.sleep(2)
     return
 
