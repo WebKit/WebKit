56c9ce1a60cf039c616345a8c3995c1eec12352d
Nicolò Ribaudo <nribaudo@igalia.com>
2024-02-20 12:22:24 -0800

    Cherry-pick 275062@main (c1a059873be7). https://bugs.webkit.org/show_bug.cgi?id=269783
        Normalize Latin-1 characters in `String#normalize("NFKC")``
        https://bugs.webkit.org/show_bug.cgi?id=269783
    
        Reviewed by Alexey Proskuryakov.
    
        The `String.prototype.normalize` implementation has a fast-path in
        cases where normalization does not affect the string. It was incorrectly
        assumed that NFKC does not affect 8-bit strings. The only normalization
        that doesn't affect them is NFC.
    
        * JSTests/es6/String.prototype_methods_String.prototype.normalize.js:
        (test):
        * Source/JavaScriptCore/runtime/StringPrototype.cpp:
        (JSC::normalize):
        (JSC::normalizationAffects8Bit): Deleted.
    
        Canonical link: https://commits.webkit.org/275062@main
    
    Canonical link: https://commits.webkit.org/274313.164@webkitglib/2.44

--- JSTests/es6/String.prototype_methods_String.prototype.normalize.js
+++ JSTests/es6/String.prototype_methods_String.prototype.normalize.js
@@ -2,6 +2,7 @@ function test() {
 
 return typeof String.prototype.normalize === "function"
   && "c\u0327\u0301".normalize("NFC") === "\u1e09"
+  && "\u00A8".normalize("NFKC") === "\u0020\u0308"
   && "\u1e09".normalize("NFD") === "c\u0327\u0301";
       
 }
