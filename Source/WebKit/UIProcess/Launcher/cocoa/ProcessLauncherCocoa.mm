ae2a0fd6cd4382debe40c6519b387edc2856ebb0
Myah Cobbs <mcobbs@apple.com>
2024-02-21 12:40:55 -0800

    Cherry-pick 9ff5a32ea773. rdar://122995875
    Identifier: 272448.583@safari-7618.1.15.10-branch

--- Source/WebKit/UIProcess/Launcher/cocoa/ProcessLauncherCocoa.mm
+++ Source/WebKit/UIProcess/Launcher/cocoa/ProcessLauncherCocoa.mm
@@ -174,8 +174,7 @@ void ProcessLauncher::launchProcess()
 #if USE(EXTENSIONKIT)
     auto handler = [](ThreadSafeWeakPtr<ProcessLauncher> weakProcessLauncher, _SEExtensionProcess* process, ASCIILiteral name, NSError* error)
     {
-        RefPtr launcher = weakProcessLauncher.get();
-        if (!launcher) {
+        if (!weakProcessLauncher.get()) {
             [process invalidate];
             return;
         }
@@ -211,7 +210,8 @@ void ProcessLauncher::launchProcess()
 
         callOnMainRunLoop([weakProcessLauncher, name, process = retainPtr(process), launchGrant = WTFMove(launchGrant)] () mutable {
             RefPtr launcher = weakProcessLauncher.get();
-            if (!launcher) {
+            // If m_client is null, the Process launcher has been invalidated, and we should not proceed with the launch.
+            if (!launcher || !launcher->m_client) {
                 [process invalidate];
                 return;
             }
