WebKit/WebKit

commit e4117be9080c9b337a0e9753bb66916a16002669

[web-animations] don't normalize progress for `visibility` and `content-visibility` while animating https://bugs.webkit.org/show_bug.cgi?id=268366
Reviewed by Tim Nguyen.

For CSS properties using a "discrete" animation type, we normalize the animated value to be either
0 or 1 depending on which side of the 0.5 progress value we stand. However, the `visibility` and
`content-visibility` properties have a special type of discrete animation support that requires
to not perform this value normalization, as will the `display` property when we add animation
support for it (see bug 267762).

We introduce a new wrapper type `NonNormalizedDiscretePropertyWrapper` to do just that and use
it for the two properties listed above. This addresses the last remaining failure for the
`content-visibility` interpolation test.

* LayoutTests/imported/w3c/web-platform-tests/css/css-contain/content-visibility/content-visibility-interpolation-expected.txt:
* Source/WebCore/animation/CSSPropertyAnimation.cpp:
(WebCore::AnimationPropertyWrapperBase::normalizesProgressForDiscreteInterpolation const):
(WebCore::CSSPropertyAnimationWrapperMap::CSSPropertyAnimationWrapperMap):
(WebCore::blendStandardProperty):

Canonical link: https://commits.webkit.org/273742@main

Source/WebCore/animation/CSSPropertyAnimation.cpp
@@ -682,8 +682,9 @@ public:
     virtual bool requiresBlendingForAccumulativeIteration(const RenderStyle&, const RenderStyle&) const { return false; }
     virtual bool equals(const RenderStyle&, const RenderStyle&) const = 0;
     virtual bool canInterpolate(const RenderStyle&, const RenderStyle&, CompositeOperation) const { return true; }
+    virtual bool normalizesProgressForDiscreteInterpolation() const { return true; }
     virtual void blend(RenderStyle&, const RenderStyle&, const RenderStyle&, const CSSPropertyBlendingContext&) const = 0;
-    
+
 #if !LOG_DISABLED
     virtual void logBlend(const RenderStyle&, const RenderStyle&, const RenderStyle&, double) const = 0;
 #endif
@@ -3597,6 +3598,20 @@ private:
 };
 DEFINE_ALLOCATOR_WITH_HEAP_IDENTIFIER(CSSPropertyAnimationWrapperMap);
 
+template <typename T>
+class NonNormalizedDiscretePropertyWrapper final : public PropertyWrapper<T> {
+    WTF_MAKE_FAST_ALLOCATED_WITH_HEAP_IDENTIFIER(NonNormalizedDiscretePropertyWrapper);
+public:
+    NonNormalizedDiscretePropertyWrapper(CSSPropertyID property, T (RenderStyle::*getter)() const, void (RenderStyle::*setter)(T))
+        : PropertyWrapper<T>(property, getter, setter)
+    {
+    }
+
+private:
+    bool canInterpolate(const RenderStyle&, const RenderStyle&, CompositeOperation) const final { return false; }
+    bool normalizesProgressForDiscreteInterpolation() const final { return false; }
+};
+
 CSSPropertyAnimationWrapperMap::CSSPropertyAnimationWrapperMap()
 {
     // build the list of property wrappers to do the comparisons and blends
@@ -3699,7 +3714,7 @@ CSSPropertyAnimationWrapperMap::CSSPropertyAnimationWrapperMap()
         new LengthVariantPropertyWrapper<LengthSize>(CSSPropertyBorderTopRightRadius, &RenderStyle::borderTopRightRadius, &RenderStyle::setBorderTopRightRadius),
         new LengthVariantPropertyWrapper<LengthSize>(CSSPropertyBorderBottomLeftRadius, &RenderStyle::borderBottomLeftRadius, &RenderStyle::setBorderBottomLeftRadius),
         new LengthVariantPropertyWrapper<LengthSize>(CSSPropertyBorderBottomRightRadius, &RenderStyle::borderBottomRightRadius, &RenderStyle::setBorderBottomRightRadius),
-        new PropertyWrapper<Visibility>(CSSPropertyVisibility, &RenderStyle::visibility, &RenderStyle::setVisibility),
+        new NonNormalizedDiscretePropertyWrapper<Visibility>(CSSPropertyVisibility, &RenderStyle::visibility, &RenderStyle::setVisibility),
 
         new ClipWrapper,
 
@@ -3814,7 +3829,7 @@ CSSPropertyAnimationWrapperMap::CSSPropertyAnimationWrapperMap()
         new DiscretePropertyWrapper<PrintColorAdjust>(CSSPropertyPrintColorAdjust, &RenderStyle::printColorAdjust, &RenderStyle::setPrintColorAdjust),
         new DiscretePropertyWrapper<ColumnFill>(CSSPropertyColumnFill, &RenderStyle::columnFill, &RenderStyle::setColumnFill),
         new DiscretePropertyWrapper<BorderStyle>(CSSPropertyColumnRuleStyle, &RenderStyle::columnRuleStyle, &RenderStyle::setColumnRuleStyle),
-        new PropertyWrapper<ContentVisibility>(CSSPropertyContentVisibility, &RenderStyle::contentVisibility, &RenderStyle::setContentVisibility),
+        new NonNormalizedDiscretePropertyWrapper<ContentVisibility>(CSSPropertyContentVisibility, &RenderStyle::contentVisibility, &RenderStyle::setContentVisibility),
         new DiscretePropertyWrapper<CursorType>(CSSPropertyCursor, &RenderStyle::cursor, &RenderStyle::setCursor),
         new DiscretePropertyWrapper<EmptyCell>(CSSPropertyEmptyCells, &RenderStyle::emptyCells, &RenderStyle::setEmptyCells),
         new DiscretePropertyWrapper<FlexDirection>(CSSPropertyFlexDirection, &RenderStyle::flexDirection, &RenderStyle::setFlexDirection),
@@ -4237,7 +4252,7 @@ static void blendStandardProperty(const CSSPropertyBlendingClient& client, CSSPr
         // The property's values cannot be meaningfully combined, thus it is not additive and
         // interpolation swaps from Va to Vb at 50% (p=0.5).
         auto isDiscrete = !wrapper->canInterpolate(from, to, compositeOperation);
-        if (isDiscrete) {
+        if (isDiscrete && wrapper->normalizesProgressForDiscreteInterpolation()) {
             // If we want additive, we should specify progress at 0 actually and return from.
             progress = progress < 0.5 ? 0 : 1;
             compositeOperation = CompositeOperation::Replace;

LayoutTests/imported/w3c/web-platform-tests/css/css-contain/content-visibility/content-visibility-interpolation-expected.txt
@@ -47,16 +47,16 @@ PASS Web Animations: property <content-visibility> from [hidden] to [visible] at
 PASS Web Animations: property <content-visibility> from [hidden] to [visible] at (0.9) should be [visible]
 PASS Web Animations: property <content-visibility> from [hidden] to [visible] at (1) should be [visible]
 PASS Web Animations: property <content-visibility> from [hidden] to [visible] at (1.5) should be [visible]
-FAIL CSS Transitions: property <content-visibility> from [auto] to [visible] at (-0.3) should be [visible] assert_equals: expected "visible " but got "auto "
-FAIL CSS Transitions: property <content-visibility> from [auto] to [visible] at (0) should be [visible] assert_equals: expected "visible " but got "auto "
-FAIL CSS Transitions: property <content-visibility> from [auto] to [visible] at (0.3) should be [visible] assert_equals: expected "visible " but got "auto "
+PASS CSS Transitions: property <content-visibility> from [auto] to [visible] at (-0.3) should be [visible]
+PASS CSS Transitions: property <content-visibility> from [auto] to [visible] at (0) should be [visible]
+PASS CSS Transitions: property <content-visibility> from [auto] to [visible] at (0.3) should be [visible]
 PASS CSS Transitions: property <content-visibility> from [auto] to [visible] at (0.5) should be [visible]
 PASS CSS Transitions: property <content-visibility> from [auto] to [visible] at (0.6) should be [visible]
 PASS CSS Transitions: property <content-visibility> from [auto] to [visible] at (1) should be [visible]
 PASS CSS Transitions: property <content-visibility> from [auto] to [visible] at (1.5) should be [visible]
-FAIL CSS Transitions with transition: all: property <content-visibility> from [auto] to [visible] at (-0.3) should be [visible] assert_equals: expected "visible " but got "auto "
-FAIL CSS Transitions with transition: all: property <content-visibility> from [auto] to [visible] at (0) should be [visible] assert_equals: expected "visible " but got "auto "
-FAIL CSS Transitions with transition: all: property <content-visibility> from [auto] to [visible] at (0.3) should be [visible] assert_equals: expected "visible " but got "auto "
+PASS CSS Transitions with transition: all: property <content-visibility> from [auto] to [visible] at (-0.3) should be [visible]
+PASS CSS Transitions with transition: all: property <content-visibility> from [auto] to [visible] at (0) should be [visible]
+PASS CSS Transitions with transition: all: property <content-visibility> from [auto] to [visible] at (0.3) should be [visible]
 PASS CSS Transitions with transition: all: property <content-visibility> from [auto] to [visible] at (0.5) should be [visible]
 PASS CSS Transitions with transition: all: property <content-visibility> from [auto] to [visible] at (0.6) should be [visible]
 PASS CSS Transitions with transition: all: property <content-visibility> from [auto] to [visible] at (1) should be [visible]

commit bc24768393ccccdd0e231d871c04e3b4aebd8ee3

Cherry-pick 275120@main (71b8fba1401e). https://bugs.webkit.org/show_bug.cgi?id=269848
    REGRESSION(273742@main): LinkedIn "…" menu does not populate on click
    https://bugs.webkit.org/show_bug.cgi?id=269848
    rdar://123264559

    Reviewed by Cameron McCormack.

    The menu on LinkedIn transitions the `visibility` CSS property.

    Do a partial revert of 273742@main to revert only the animation wrapper change for the `visibility` property, since it has caused changes that are timing sensitive for the site.

    Imported WPT that also regressed along with 273742@main to avoid regressing this again.

    * LayoutTests/imported/w3c/resources/import-expectations.json:
    * LayoutTests/imported/w3c/web-platform-tests/css/CSS2/visufx/animation/visibility-interpolation-expected.txt: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/CSS2/visufx/animation/visibility-interpolation.html: Added.
    * Source/WebCore/animation/CSSPropertyAnimation.cpp:
    (WebCore::CSSPropertyAnimationWrapperMap::CSSPropertyAnimationWrapperMap):

    Canonical link: https://commits.webkit.org/275120@main

Source/WebCore/animation/CSSPropertyAnimation.cpp
@@ -3677,7 +3677,7 @@ CSSPropertyAnimationWrapperMap::CSSPropertyAnimationWrapperMap()
         new LengthVariantPropertyWrapper<LengthSize>(CSSPropertyBorderTopRightRadius, &RenderStyle::borderTopRightRadius, &RenderStyle::setBorderTopRightRadius),
         new LengthVariantPropertyWrapper<LengthSize>(CSSPropertyBorderBottomLeftRadius, &RenderStyle::borderBottomLeftRadius, &RenderStyle::setBorderBottomLeftRadius),
         new LengthVariantPropertyWrapper<LengthSize>(CSSPropertyBorderBottomRightRadius, &RenderStyle::borderBottomRightRadius, &RenderStyle::setBorderBottomRightRadius),
-        new NonNormalizedDiscretePropertyWrapper<Visibility>(CSSPropertyVisibility, &RenderStyle::visibility, &RenderStyle::setVisibility),
+        new PropertyWrapper<Visibility>(CSSPropertyVisibility, &RenderStyle::visibility, &RenderStyle::setVisibility),
 
         new ClipWrapper,
 

LayoutTests/imported/w3c/web-platform-tests/css/CSS2/visufx/animation/visibility-interpolation-expected.txt
@@ -1,0 +1,122 @@
+
+PASS CSS Transitions: property <visibility> from [visible] to [visible] at (-1) should be [visible]
+PASS CSS Transitions: property <visibility> from [visible] to [visible] at (0) should be [visible]
+PASS CSS Transitions: property <visibility> from [visible] to [visible] at (0.5) should be [visible]
+PASS CSS Transitions: property <visibility> from [visible] to [visible] at (1) should be [visible]
+PASS CSS Transitions: property <visibility> from [visible] to [visible] at (1.5) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [visible] to [visible] at (-1) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [visible] to [visible] at (0) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [visible] to [visible] at (0.5) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [visible] to [visible] at (1) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [visible] to [visible] at (1.5) should be [visible]
+PASS CSS Animations: property <visibility> from [visible] to [visible] at (-1) should be [visible]
+PASS CSS Animations: property <visibility> from [visible] to [visible] at (0) should be [visible]
+PASS CSS Animations: property <visibility> from [visible] to [visible] at (0.5) should be [visible]
+PASS CSS Animations: property <visibility> from [visible] to [visible] at (1) should be [visible]
+PASS CSS Animations: property <visibility> from [visible] to [visible] at (1.5) should be [visible]
+PASS Web Animations: property <visibility> from [visible] to [visible] at (-1) should be [visible]
+PASS Web Animations: property <visibility> from [visible] to [visible] at (0) should be [visible]
+PASS Web Animations: property <visibility> from [visible] to [visible] at (0.5) should be [visible]
+PASS Web Animations: property <visibility> from [visible] to [visible] at (1) should be [visible]
+PASS Web Animations: property <visibility> from [visible] to [visible] at (1.5) should be [visible]
+PASS CSS Transitions: property <visibility> from [visible] to [hidden] at (-1) should be [visible]
+PASS CSS Transitions: property <visibility> from [visible] to [hidden] at (0) should be [visible]
+PASS CSS Transitions: property <visibility> from [visible] to [hidden] at (0.1) should be [visible]
+PASS CSS Transitions: property <visibility> from [visible] to [hidden] at (0.9) should be [visible]
+PASS CSS Transitions: property <visibility> from [visible] to [hidden] at (1) should be [hidden]
+PASS CSS Transitions: property <visibility> from [visible] to [hidden] at (1.5) should be [hidden]
+PASS CSS Transitions with transition: all: property <visibility> from [visible] to [hidden] at (-1) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [visible] to [hidden] at (0) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [visible] to [hidden] at (0.1) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [visible] to [hidden] at (0.9) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [visible] to [hidden] at (1) should be [hidden]
+PASS CSS Transitions with transition: all: property <visibility> from [visible] to [hidden] at (1.5) should be [hidden]
+PASS CSS Animations: property <visibility> from [visible] to [hidden] at (-1) should be [visible]
+PASS CSS Animations: property <visibility> from [visible] to [hidden] at (0) should be [visible]
+PASS CSS Animations: property <visibility> from [visible] to [hidden] at (0.1) should be [visible]
+PASS CSS Animations: property <visibility> from [visible] to [hidden] at (0.9) should be [visible]
+PASS CSS Animations: property <visibility> from [visible] to [hidden] at (1) should be [hidden]
+PASS CSS Animations: property <visibility> from [visible] to [hidden] at (1.5) should be [hidden]
+PASS Web Animations: property <visibility> from [visible] to [hidden] at (-1) should be [visible]
+PASS Web Animations: property <visibility> from [visible] to [hidden] at (0) should be [visible]
+PASS Web Animations: property <visibility> from [visible] to [hidden] at (0.1) should be [visible]
+PASS Web Animations: property <visibility> from [visible] to [hidden] at (0.9) should be [visible]
+PASS Web Animations: property <visibility> from [visible] to [hidden] at (1) should be [hidden]
+PASS Web Animations: property <visibility> from [visible] to [hidden] at (1.5) should be [hidden]
+PASS CSS Transitions: property <visibility> from [hidden] to [visible] at (-1) should be [hidden]
+PASS CSS Transitions: property <visibility> from [hidden] to [visible] at (0) should be [hidden]
+PASS CSS Transitions: property <visibility> from [hidden] to [visible] at (0.1) should be [visible]
+PASS CSS Transitions: property <visibility> from [hidden] to [visible] at (0.9) should be [visible]
+PASS CSS Transitions: property <visibility> from [hidden] to [visible] at (1) should be [visible]
+PASS CSS Transitions: property <visibility> from [hidden] to [visible] at (1.5) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [hidden] to [visible] at (-1) should be [hidden]
+PASS CSS Transitions with transition: all: property <visibility> from [hidden] to [visible] at (0) should be [hidden]
+PASS CSS Transitions with transition: all: property <visibility> from [hidden] to [visible] at (0.1) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [hidden] to [visible] at (0.9) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [hidden] to [visible] at (1) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [hidden] to [visible] at (1.5) should be [visible]
+PASS CSS Animations: property <visibility> from [hidden] to [visible] at (-1) should be [hidden]
+PASS CSS Animations: property <visibility> from [hidden] to [visible] at (0) should be [hidden]
+PASS CSS Animations: property <visibility> from [hidden] to [visible] at (0.1) should be [visible]
+PASS CSS Animations: property <visibility> from [hidden] to [visible] at (0.9) should be [visible]
+PASS CSS Animations: property <visibility> from [hidden] to [visible] at (1) should be [visible]
+PASS CSS Animations: property <visibility> from [hidden] to [visible] at (1.5) should be [visible]
+PASS Web Animations: property <visibility> from [hidden] to [visible] at (-1) should be [hidden]
+PASS Web Animations: property <visibility> from [hidden] to [visible] at (0) should be [hidden]
+PASS Web Animations: property <visibility> from [hidden] to [visible] at (0.1) should be [visible]
+PASS Web Animations: property <visibility> from [hidden] to [visible] at (0.9) should be [visible]
+PASS Web Animations: property <visibility> from [hidden] to [visible] at (1) should be [visible]
+PASS Web Animations: property <visibility> from [hidden] to [visible] at (1.5) should be [visible]
+PASS CSS Transitions: property <visibility> from [collapse] to [visible] at (-1) should be [collapse]
+PASS CSS Transitions: property <visibility> from [collapse] to [visible] at (0) should be [collapse]
+PASS CSS Transitions: property <visibility> from [collapse] to [visible] at (0.1) should be [visible]
+PASS CSS Transitions: property <visibility> from [collapse] to [visible] at (0.9) should be [visible]
+PASS CSS Transitions: property <visibility> from [collapse] to [visible] at (1) should be [visible]
+PASS CSS Transitions: property <visibility> from [collapse] to [visible] at (1.5) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [collapse] to [visible] at (-1) should be [collapse]
+PASS CSS Transitions with transition: all: property <visibility> from [collapse] to [visible] at (0) should be [collapse]
+PASS CSS Transitions with transition: all: property <visibility> from [collapse] to [visible] at (0.1) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [collapse] to [visible] at (0.9) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [collapse] to [visible] at (1) should be [visible]
+PASS CSS Transitions with transition: all: property <visibility> from [collapse] to [visible] at (1.5) should be [visible]
+PASS CSS Animations: property <visibility> from [collapse] to [visible] at (-1) should be [collapse]
+PASS CSS Animations: property <visibility> from [collapse] to [visible] at (0) should be [collapse]
+PASS CSS Animations: property <visibility> from [collapse] to [visible] at (0.1) should be [visible]
+PASS CSS Animations: property <visibility> from [collapse] to [visible] at (0.9) should be [visible]
+PASS CSS Animations: property <visibility> from [collapse] to [visible] at (1) should be [visible]
+PASS CSS Animations: property <visibility> from [collapse] to [visible] at (1.5) should be [visible]
+PASS Web Animations: property <visibility> from [collapse] to [visible] at (-1) should be [collapse]
+PASS Web Animations: property <visibility> from [collapse] to [visible] at (0) should be [collapse]
+PASS Web Animations: property <visibility> from [collapse] to [visible] at (0.1) should be [visible]
+PASS Web Animations: property <visibility> from [collapse] to [visible] at (0.9) should be [visible]
+PASS Web Animations: property <visibility> from [collapse] to [visible] at (1) should be [visible]
+PASS Web Animations: property <visibility> from [collapse] to [visible] at (1.5) should be [visible]
+FAIL CSS Transitions: property <visibility> from [collapse] to [hidden] at (-0.3) should be [hidden] assert_equals: expected "hidden " but got "collapse "
+FAIL CSS Transitions: property <visibility> from [collapse] to [hidden] at (0) should be [hidden] assert_equals: expected "hidden " but got "collapse "
+FAIL CSS Transitions: property <visibility> from [collapse] to [hidden] at (0.3) should be [hidden] assert_equals: expected "hidden " but got "collapse "
+PASS CSS Transitions: property <visibility> from [collapse] to [hidden] at (0.5) should be [hidden]
+PASS CSS Transitions: property <visibility> from [collapse] to [hidden] at (0.6) should be [hidden]
+PASS CSS Transitions: property <visibility> from [collapse] to [hidden] at (1) should be [hidden]
+PASS CSS Transitions: property <visibility> from [collapse] to [hidden] at (1.5) should be [hidden]
+FAIL CSS Transitions with transition: all: property <visibility> from [collapse] to [hidden] at (-0.3) should be [hidden] assert_equals: expected "hidden " but got "collapse "
+FAIL CSS Transitions with transition: all: property <visibility> from [collapse] to [hidden] at (0) should be [hidden] assert_equals: expected "hidden " but got "collapse "
+FAIL CSS Transitions with transition: all: property <visibility> from [collapse] to [hidden] at (0.3) should be [hidden] assert_equals: expected "hidden " but got "collapse "
+PASS CSS Transitions with transition: all: property <visibility> from [collapse] to [hidden] at (0.5) should be [hidden]
+PASS CSS Transitions with transition: all: property <visibility> from [collapse] to [hidden] at (0.6) should be [hidden]
+PASS CSS Transitions with transition: all: property <visibility> from [collapse] to [hidden] at (1) should be [hidden]
+PASS CSS Transitions with transition: all: property <visibility> from [collapse] to [hidden] at (1.5) should be [hidden]
+PASS CSS Animations: property <visibility> from [collapse] to [hidden] at (-0.3) should be [collapse]
+PASS CSS Animations: property <visibility> from [collapse] to [hidden] at (0) should be [collapse]
+PASS CSS Animations: property <visibility> from [collapse] to [hidden] at (0.3) should be [collapse]
+PASS CSS Animations: property <visibility> from [collapse] to [hidden] at (0.5) should be [hidden]
+PASS CSS Animations: property <visibility> from [collapse] to [hidden] at (0.6) should be [hidden]
+PASS CSS Animations: property <visibility> from [collapse] to [hidden] at (1) should be [hidden]
+PASS CSS Animations: property <visibility> from [collapse] to [hidden] at (1.5) should be [hidden]
+PASS Web Animations: property <visibility> from [collapse] to [hidden] at (-0.3) should be [collapse]
+PASS Web Animations: property <visibility> from [collapse] to [hidden] at (0) should be [collapse]
+PASS Web Animations: property <visibility> from [collapse] to [hidden] at (0.3) should be [collapse]
+PASS Web Animations: property <visibility> from [collapse] to [hidden] at (0.5) should be [hidden]
+PASS Web Animations: property <visibility> from [collapse] to [hidden] at (0.6) should be [hidden]
+PASS Web Animations: property <visibility> from [collapse] to [hidden] at (1) should be [hidden]
+PASS Web Animations: property <visibility> from [collapse] to [hidden] at (1.5) should be [hidden]
+

LayoutTests/imported/w3c/web-platform-tests/css/CSS2/visufx/animation/visibility-interpolation.html
@@ -1,0 +1,70 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<title>visibility interpolation</title>
+<link rel="help" href="https://www.w3.org/TR/CSS2/visufx.html#visibility">
+<meta name="assert" content="visibility supports">
+
+<script src="/resources/testharness.js"></script>
+<script src="/resources/testharnessreport.js"></script>
+<script src="/css/support/interpolation-testcommon.js"></script>
+
+<body>
+<script>
+test_interpolation({
+  property: 'visibility',
+  from: 'visible',
+  to: 'visible'
+}, [
+  {at: -1, expect: 'visible'},
+  {at: 0, expect: 'visible'},
+  {at: 0.5, expect: 'visible'},
+  {at: 1, expect: 'visible'},
+  {at: 1.5, expect: 'visible'},
+]);
+
+test_interpolation({
+  property: 'visibility',
+  from: 'visible',
+  to: 'hidden'
+}, [
+  {at: -1, expect: 'visible'},
+  {at: 0, expect: 'visible'},
+  {at: 0.1, expect: 'visible'},
+  {at: 0.9, expect: 'visible'},
+  {at: 1, expect: 'hidden'},
+  {at: 1.5, expect: 'hidden'},
+]);
+
+test_interpolation({
+  property: 'visibility',
+  from: 'hidden',
+  to: 'visible'
+}, [
+  {at: -1, expect: 'hidden'},
+  {at: 0, expect: 'hidden'},
+  {at: 0.1, expect: 'visible'},
+  {at: 0.9, expect: 'visible'},
+  {at: 1, expect: 'visible'},
+  {at: 1.5, expect: 'visible'},
+]);
+
+test_interpolation({
+  property: 'visibility',
+  from: 'collapse',
+  to: 'visible'
+}, [
+  {at: -1, expect: 'collapse'},
+  {at: 0, expect: 'collapse'},
+  {at: 0.1, expect: 'visible'},
+  {at: 0.9, expect: 'visible'},
+  {at: 1, expect: 'visible'},
+  {at: 1.5, expect: 'visible'},
+]);
+
+test_no_interpolation({
+  property: 'visibility',
+  from: 'collapse',
+  to: 'hidden'
+});
+</script>
+</body>