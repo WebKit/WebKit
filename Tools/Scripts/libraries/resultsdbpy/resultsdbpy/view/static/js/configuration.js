4bd6185bc73f800ee801288c56beb99318ce1723
Jonathan Bedard <jbedard@apple.com>
2024-02-22 13:15:06 -0800

    [results.webkit.org] Differentiate version even when version_name is defined https://bugs.webkit.org/show_bug.cgi?id=269884 rdar://123415392
    Reviewed by Dewei Zhu.
    
    * Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/configuration.js:
    (Configuration.prototype.toParams): Set version even when version_name is defined.
    
    Canonical link: https://commits.webkit.org/275199@main

--- Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/configuration.js
+++ Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/configuration.js
@@ -287,7 +287,7 @@ class Configuration {
             version_name = this.version_name.substring(0, this.version_name.length - 2);
         return {
             platform: [this.platform],
-            version:[this.version && !this.version_name ? Configuration.integerToVersion(this.version) : null],
+            version:[this.version ? Configuration.integerToVersion(this.version) : null],
             version_name: [version_name],
             is_simulator: [this.is_simulator === null ? null : (this.is_simulator ? 'True' : 'False')],
             style: [this.style],
