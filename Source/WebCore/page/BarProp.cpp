cbcae553570809f2c48cbe4faab29e9a80ff8ba3
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2024-02-27 10:58:34 -0800

    Remove unused `type` function from `BarProp.cpp|h`
    https://bugs.webkit.org/show_bug.cgi?id=270157
    
    Reviewed by Charlie Wolfe.
    
    This patch removes unused function 'type' from `BarProp.cpp|h`.
    
    * Source/WebCore/page/BarProp.cpp:
    (BarProp::type): Deleted
    * Source/WebCore/page/BarProp.h:
    
    Canonical link: https://commits.webkit.org/275393@main

--- Source/WebCore/page/BarProp.cpp
+++ Source/WebCore/page/BarProp.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007, 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2007-2024 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -42,11 +42,6 @@ BarProp::BarProp(LocalDOMWindow& window, Type type)
     : LocalDOMWindowProperty(&window)
     , m_type(type)
 {
-}
-
-BarProp::Type BarProp::type() const
-{
-    return m_type;
 }
 
 bool BarProp::visible() const
