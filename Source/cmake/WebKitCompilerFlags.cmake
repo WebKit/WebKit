e739d6576a590c7211f9438c5fa9d54fd4cb1ee6
Adrian Perez de Castro <aperez@igalia.com>
2024-02-27 23:55:45 -0800

    Simplify compiler flags checking in Source/ThirdParty/skia/CMakeLists.txt https://bugs.webkit.org/show_bug.cgi?id=270162
    Reviewed by Michael Catanzaro.
    
    Add new WEBKIT_CHECK_COMPILER_FLAGS and WEBKIT_ADD_COMPILER_FLAGS
    functions, which split the existing functionality of the existing
    WEBKIT_ADD_TARGET_{C,CXX}_FLAGS macros, making them more generic.
    The old macros are kept for now, but implemented in terms of the
    new functions.
    
    While at it, the WEBKIT_ADD_COMPILER_FLAGS function was augmented to
    support applying flags to SOURCE files and DIRECTORY paths, and a
    tweak added to ensure results of checks for flags containing equals
    characters (=) are properly cached by CMake to save some time in CMake
    re-runs.
    
    * Source/ThirdParty/skia/CMakeLists.txt: Make use of the new
      WEBKIT_CHECK_COMPILER_FLAGS and WEBKIT_ADD_COMPILER_FLAGS functions.
    * Source/cmake/WebKitCompilerFlags.cmake: Split and improve
      functionality from the WEBKIT_ADD_TARGET_{C,CXX}_FLAGS macros into
      a new function to check flags and another generic function to apply
      flags to targets, sources, or directories.
    
    Canonical link: https://commits.webkit.org/275430@main

--- Source/cmake/WebKitCompilerFlags.cmake
+++ Source/cmake/WebKitCompilerFlags.cmake
@@ -60,17 +60,51 @@ macro(WEBKIT_APPEND_GLOBAL_COMPILER_FLAGS)
     WEBKIT_APPEND_GLOBAL_CXX_FLAGS(${ARGN})
 endmacro()
 
+# Checks whether all the given compiler flags are supported by the compiler.
+# The _compiler may be either "C" or "CXX", and the result from the check
+# will be stored in the variable named by _result.
+function(WEBKIT_CHECK_COMPILER_FLAGS _compiler _result)
+    string(TOUPPER "${_compiler}" _compiler)
+    set(${_result} FALSE PARENT_SCOPE)
+    foreach (_flag IN LISTS ARGN)
+        # If an equals (=) character is present in a variable name, it will
+        # not be cached correctly, and the check will be retried ad nauseam.
+        string(REPLACE "=" "__" _cachevar "${_compiler}_COMPILER_SUPPORTS_${_flag}")
+        if (${_compiler} STREQUAL CXX)
+            check_cxx_compiler_flag("${_flag}" "${_cachevar}")
+        elseif (${_compiler} STREQUAL C)
+            check_c_compiler_flag("${_flag}" "${_cachevar}")
+        else ()
+            set(${_cachevar} FALSE CACHE INTERNAL "" FORCE)
+            message(WARNING "WEBKIT_CHECK_COMPILER_FLAGS: unknown compiler '${_compiler}'")
+            return()
+        endif ()
+        if (NOT ${_cachevar})
+            return()
+        endif ()
+    endforeach ()
+    set(${_result} TRUE PARENT_SCOPE)
+endfunction()
+
+# Appends flags to COMPILE_OPTIONS of _subject if supported by the C
+# or CXX _compiler. The _subject argument depends on its _kind, it may be
+# a target name (with TARGET as _kind), or a path (with SOURCE or DIRECTORY
+# as _kind).
+function(WEBKIT_ADD_COMPILER_FLAGS _compiler _kind _subject)
+    foreach (_flag IN LISTS ARGN)
+        WEBKIT_CHECK_COMPILER_FLAGS(${_compiler} flag_supported "${_flag}")
+        if (flag_supported)
+            set_property(${_kind} ${_subject} APPEND PROPERTY COMPILE_OPTIONS "${_flag}")
+        endif ()
+    endforeach ()
+endfunction()
+
 # Appends flags to COMPILE_FLAGS of _target if supported by the C compiler.
 # Note that it is simply not possible to pass different C and C++ flags, unless
 # we drop support for the Visual Studio backend and use the COMPILE_LANGUAGE
 # generator expression. This is a very serious limitation.
 macro(WEBKIT_ADD_TARGET_C_FLAGS _target)
-    foreach (_flag ${ARGN})
-        check_c_compiler_flag("${_flag}" C_COMPILER_SUPPORTS_${_flag})
-        if (C_COMPILER_SUPPORTS_${_flag})
-            target_compile_options(${_target} PRIVATE ${_flag})
-        endif ()
-    endforeach ()
+    WEBKIT_ADD_COMPILER_FLAGS(C TARGET ${_target} ${ARGN})
 endmacro()
 
 # Appends flags to COMPILE_FLAGS of _target if supported by the C++ compiler.
@@ -78,12 +112,7 @@ endmacro()
 # we drop support for the Visual Studio backend and use the COMPILE_LANGUAGE
 # generator expression. This is a very serious limitation.
 macro(WEBKIT_ADD_TARGET_CXX_FLAGS _target)
-    foreach (_flag ${ARGN})
-        check_cxx_compiler_flag("${_flag}" CXX_COMPILER_SUPPORTS_${_flag})
-        if (CXX_COMPILER_SUPPORTS_${_flag})
-            target_compile_options(${_target} PRIVATE ${_flag})
-        endif ()
-    endforeach ()
+    WEBKIT_ADD_COMPILER_FLAGS(CXX TARGET ${_target} ${ARGN})
 endmacro()
 
 
