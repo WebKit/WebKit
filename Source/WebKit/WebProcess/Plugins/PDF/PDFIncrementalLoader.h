a10eb5ff15af1e45b90b7fa3c17feb42ca50a037
Chris Dumez <cdumez@apple.com>
2024-03-07 12:08:34 -0800

    Cherry-pick e604d7c70127. rdar://101147811
        PDFPlugin hangs in deallocation when data load contends with waiting for thread completion
        https://bugs.webkit.org/show_bug.cgi?id=246454
        rdar://101147811
    
        Reviewed by Tim Horton.
    
        When `PDFPlugin::teardown()` would get called (on the main thread), it would
        wait for the PDF thread to complete. However, if loading is still going on,
        the PDF thread might be blocked on `dataSemaphore.wait()` in
        PDFIncrementalLoader::dataProviderGetBytesAtPosition(). The `dataSemaphore`
        is supposed to get signaled on a task dispatched to the main thread but this
        task won't run if the main thread is blocked on waiting for the thread to
        exit.
    
        To address the issue, PDFIncrementalLoader::clear() now signals the
        pending dataSemaphores before waiting for the thread to exit and sets a
        flag indicating we want the thread to exit. This flag is then checked
        on the PDF thread to make sure we don't queue additional work / semaphores.
    
        * LayoutTests/compositing/plugins/pdf/pdf-plugin-hang-during-destruction-expected.txt: Added.
        * LayoutTests/compositing/plugins/pdf/pdf-plugin-hang-during-destruction.html: Added.
        I took the test from Erica Li's earlier PR that got reverted.
    
        * Source/WebKit/WebProcess/Plugins/PDF/PDFIncrementalLoader.h:
        (WebKit::PDFIncrementalLoader::SemaphoreWrapper::create):
        (WebKit::PDFIncrementalLoader::SemaphoreWrapper::wait):
        (WebKit::PDFIncrementalLoader::SemaphoreWrapper::signal):
        (WebKit::PDFIncrementalLoader::WTF_GUARDED_BY_LOCK):
        * Source/WebKit/WebProcess/Plugins/PDF/PDFIncrementalLoader.mm:
        (WebKit::PDFIncrementalLoader::clear):
        (WebKit::PDFIncrementalLoader::dataProviderGetBytesAtPosition):
        (WebKit::PDFIncrementalLoader::dataProviderGetByteRanges):
        (WebKit::PDFIncrementalLoader::threadEntry):
    
        Canonical link: https://commits.webkit.org/275707@main

--- Source/WebKit/WebProcess/Plugins/PDF/PDFIncrementalLoader.h
+++ Source/WebKit/WebProcess/Plugins/PDF/PDFIncrementalLoader.h
@@ -29,8 +29,10 @@
 
 #include <wtf/Ref.h>
 #include <wtf/ThreadSafeRefCounted.h>
+#include <wtf/ThreadSafeWeakHashSet.h>
 #include <wtf/ThreadSafeWeakPtr.h>
 #include <wtf/Threading.h>
+#include <wtf/threads/BinarySemaphore.h>
 
 OBJC_CLASS PDFDocument;
 
@@ -114,6 +116,27 @@ private:
     void logStreamLoader(WTF::TextStream&, WebCore::NetscapePlugInStreamLoader&);
 #endif
 
+    class SemaphoreWrapper : public ThreadSafeRefCountedAndCanMakeThreadSafeWeakPtr<SemaphoreWrapper> {
+    public:
+        static Ref<SemaphoreWrapper> create() { return adoptRef(*new SemaphoreWrapper); }
+
+        void wait() { m_semaphore.wait(); }
+        void signal()
+        {
+            m_wasSignaled = true;
+            m_semaphore.signal();
+        }
+        bool wasSignaled() const { return m_wasSignaled; }
+
+    private:
+        SemaphoreWrapper() = default;
+
+        BinarySemaphore m_semaphore;
+        std::atomic<bool> m_wasSignaled { false };
+    };
+
+    RefPtr<SemaphoreWrapper> createDataSemaphore();
+
     ThreadSafeWeakPtr<PDFPluginBase> m_plugin;
 
     RetainPtr<PDFDocument> m_backgroundThreadDocument;
@@ -123,6 +146,11 @@ private:
 
     struct RequestData;
     std::unique_ptr<RequestData> m_requestData;
+
+    ThreadSafeWeakHashSet<SemaphoreWrapper> m_dataSemaphores WTF_GUARDED_BY_LOCK(m_wasPDFThreadTerminationRequestedLock);
+
+    Lock m_wasPDFThreadTerminationRequestedLock;
+    bool m_wasPDFThreadTerminationRequested WTF_GUARDED_BY_LOCK(m_wasPDFThreadTerminationRequestedLock) { false };
 
 #if !LOG_DISABLED
     std::atomic<size_t> m_threadsWaitingOnCallback { 0 };
