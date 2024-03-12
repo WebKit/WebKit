e39892e12f8d3c3a544327c87c9904401e15ef9f
Ahmad Saleem <ahmad.saleem792@gmail.com>
2024-03-12 05:46:42 -0700

    Fix edge cases of keyboard navigations around `anchor` elements with `TabsToLink`
    https://bugs.webkit.org/show_bug.cgi?id=270713
    
    Reviewed by Ryosuke Niwa.
    
    This patch is to fix edge cases in keyboard focus navigation around
    `anchor` elements:
    
    Partial Merge: https://chromium.googlesource.com/chromium/blink/+/53f8eba14b6362795bae47c02236ca93303264a6
    
    By merging above, WebKit is trying to fix case around `empty anchor`
    while `TabsToLinks` enabled. The test case was failing currently in
    WebKit, so this fixes it.
    
    Merge: https://chromium.googlesource.com/chromium/blink/+/7ef69ef071ba2b49ece747b93518b9a1218f9c45
    
    In this merge, WebKit is aligning to allow keyboard navigation to anchors
    with `TabsToLinks` being disabled if the anchors have explicit tabindex.
    
    Beside following, this patch still retains `early return` if it is not focusable.
    
    > Changes:
    * Source/WebCore/html/HTMLAnchorElement.cpp:
    (HTMLAnchorElement::isKeyboardFocusable):
    
    > Test Cases & Expectations:
    * LayoutTests/fast/events/anchor-empty-focus-expected.txt:
    * LayoutTests/fast/events/anchor-empty-focus.html:
    * LayoutTests/fast/events/frame-tab-focus-expected.txt:
    * LayoutTests/fast/events/frame-tab-focus.html:
    * LayoutTests/fast/events/tab-focus-anchor-expected.txt:
    * LayoutTests/fast/events/tab-focus-anchor-tab-to-links-expected.txt:
    * LayoutTests/fast/events/tab-focus-anchor-tab-to-links.html:
    * LayoutTests/fast/events/tab-focus-anchor.html:
    * LayoutTests/fast/forms/focus-on-control-with-zero-size.html:
    * LayoutTests/fast/forms/focus-on-control-with-zero-size-expected.txt:
    
    > Platform Specific Test Expectation:
    * LayoutTests/platform/ios/TestExpectations: `iOS` don't have 'EventSender` keydown support
    
    Canonical link: https://commits.webkit.org/275976@main

--- Source/WebCore/html/HTMLAnchorElement.cpp
+++ Source/WebCore/html/HTMLAnchorElement.cpp
@@ -3,6 +3,7 @@
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2000 Simon Hausmann <hausmann@kde.org>
  * Copyright (C) 2003-2016 Apple Inc. All rights reserved.
+ * Copyright (C) 2013 Google Inc. All rights reserved.
  *           (C) 2006 Graham Dennis (graham.dennis@gmail.com)
  *
  * This library is free software; you can redistribute it and/or
@@ -140,20 +141,16 @@ static bool hasNonEmptyBox(RenderBoxModelObject* renderer)
 
 bool HTMLAnchorElement::isKeyboardFocusable(KeyboardEvent* event) const
 {
-    if (!isLink())
-        return HTMLElement::isKeyboardFocusable(event);
-
     if (!isFocusable())
         return false;
     
-    if (!document().frame())
-        return false;
+    // Anchor is focusable if the base element supports focus and is focusable.
+    if (isFocusable() && Element::supportsFocus())
+        return HTMLElement::isKeyboardFocusable(event);
 
-    if (!document().frame()->eventHandler().tabsToLinks(event))
+    if (isLink() && !document().frame()->eventHandler().tabsToLinks(event))
         return false;
-
-    if (!renderer() && ancestorsOfType<HTMLCanvasElement>(*this).first())
-        return true;
+    return HTMLElement::isKeyboardFocusable(event);
 
     return hasNonEmptyBox(renderBoxModelObject());
 }
