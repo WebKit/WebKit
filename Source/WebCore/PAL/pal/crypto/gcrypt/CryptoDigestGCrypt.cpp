WebKit/WebKit

commit 7263d3ab568094eb94dd4e9ec859d14a9cd871b0

Drop Vector constructor taking a raw pointer and a size https://bugs.webkit.org/show_bug.cgi?id=270944
Reviewed by Sihui Liu, Mike Wyrzykowski, Justin Michaud and Darin Adler.

Drop Vector constructor taking a raw pointer and a size and port call sites to
the constructor taking a std::span instead.

This is work towards more widespread std::span adoption in WebKit, which has
security benefits.

* Source/JavaScriptCore/runtime/ArrayBuffer.cpp:
(JSC::ArrayBuffer::tryCreate):
* Source/JavaScriptCore/runtime/ArrayBuffer.h:
* Source/JavaScriptCore/runtime/ArrayBufferView.h:
(JSC::ArrayBufferView::bytes const):
* Source/JavaScriptCore/runtime/ISO8601.cpp:
(JSC::ISO8601::parseTimeZoneBracketedAnnotation):
(JSC::ISO8601::parseCalendar):
* Source/JavaScriptCore/runtime/IntlDateTimeFormat.cpp:
(JSC::IntlDateTimeFormat::formatRange):
(JSC::IntlDateTimeFormat::formatRangeToParts):
* Source/JavaScriptCore/wasm/WasmParser.h:
(JSC::Wasm::Parser::sourceSpan const):
* Source/JavaScriptCore/wasm/WasmSectionParser.cpp:
(JSC::Wasm::SectionParser::parseInitExpr):
* Source/JavaScriptCore/wasm/WasmStreamingParser.cpp:
(JSC::Wasm::StreamingParser::consume):
* Source/JavaScriptCore/yarr/generateYarrUnicodePropertyTables.py:
(PropertyData.convertStringToCppFormat):
* Source/WTF/wtf/Algorithms.h:
(WTF::equalSpans):
* Source/WTF/wtf/Vector.h:
(WTF::Vector::Vector):
(WTF::Vector::subVector const):
* Source/WTF/wtf/cf/VectorCF.h:
(WTF::vectorFromCFData):
* Source/WTF/wtf/cocoa/VectorCocoa.h:
(WTF::vectorFromNSData):
* Source/WTF/wtf/persistence/PersistentEncoder.h:
(WTF::Persistence::Encoder::bytes const):
* Source/WTF/wtf/text/IntegerToStringConversion.h:
* Source/WTF/wtf/text/StringParsingBuffer.h:
* Source/WTF/wtf/text/WTFString.cpp:
(asciiDebug):
* Source/WTF/wtf/text/WTFString.h:
(WTF::String::fromUTF8):
* Source/WebCore/Modules/ShapeDetection/Implementation/Cocoa/FaceDetectorImplementation.mm:
(WebCore::ShapeDetection::convertLandmark):

Source/WebCore/PAL/pal/crypto/gcrypt/CryptoDigestGCrypt.cpp
@@ -88,7 +88,7 @@ Vector<uint8_t> CryptoDigest::computeHash()
     size_t digestLen = gcry_md_get_algo_dlen(m_context->algorithm);
 
     gcry_md_final(m_context->md);
-    Vector<uint8_t> result { gcry_md_read(m_context->md, 0), digestLen };
+    Vector<uint8_t> result(std::span { gcry_md_read(m_context->md, 0), digestLen });
     gcry_md_close(m_context->md);
 
     return result;
@@ -108,7 +108,7 @@ std::optional<Vector<uint8_t>> CryptoDigest::computeHash(CryptoDigest::Algorithm
 
     gcry_md_write(digest->m_context->md, input.data(), input.size());
     gcry_md_final(digest->m_context->md);
-    Vector<uint8_t> result { gcry_md_read(digest->m_context->md, 0), digestLen };
+    Vector<uint8_t> result(std::span { gcry_md_read(digest->m_context->md, 0), digestLen });
     gcry_md_close(digest->m_context->md);
 
     return result;

Source/WebCore/loader/FormSubmission.cpp
@@ -74,8 +74,7 @@ static void appendMailtoPostFormDataToURL(URL& url, const FormData& data, const 
         body = PAL::decodeURLEscapeSequences(makeStringByReplacingAll(makeStringByReplacingAll(body, '&', "\r\n"_s), '+', ' '));
     }
 
-    Vector<char> bodyData;
-    bodyData.append("body=", 5);
+    Vector<uint8_t> bodyData(std::span { "body=", 5 });
     FormDataBuilder::encodeStringAsFormData(bodyData, body.utf8());
     body = makeStringByReplacingAll(String(bodyData.data(), bodyData.size()), '+', "%20"_s);
 
@@ -223,7 +222,7 @@ Ref<FormSubmission> FormSubmission::create(HTMLFormElement& form, HTMLFormContro
 
     if (isMultiPartForm) {
         formData = FormData::createMultiPart(domFormData);
-        boundary = String::fromLatin1(formData->boundary().data());
+        boundary = String(formData->boundary());
     } else {
         formData = FormData::create(domFormData, attributes.method() == Method::Get ? FormData::EncodingType::FormURLEncoded : FormData::parseEncodingType(encodingType));
         if (copiedAttributes.method() == Method::Post && isMailtoForm) {

Source/WebCore/platform/network/FormData.cpp
@@ -55,21 +55,16 @@ Ref<FormData> FormData::create()
     return adoptRef(*new FormData);
 }
 
-Ref<FormData> FormData::create(const void* data, size_t size)
+Ref<FormData> FormData::create(std::span<const uint8_t> data)
 {
     auto result = create();
-    result->appendData(data, size);
+    result->appendData(data);
     return result;
 }
 
 Ref<FormData> FormData::create(const CString& string)
 {
-    return create(string.data(), string.length());
-}
-
-Ref<FormData> FormData::create(const Vector<char>& vector)
-{
-    return create(vector.data(), vector.size());
+    return create(string.bytes());
 }
 
 Ref<FormData> FormData::create(Vector<uint8_t>&& vector)
@@ -81,7 +76,7 @@ Ref<FormData> FormData::create(Vector<uint8_t>&& vector)
 
 Ref<FormData> FormData::create(const Vector<uint8_t>& vector)
 {
-    return create(vector.data(), vector.size());
+    return create(vector.span());
 }
 
 Ref<FormData> FormData::create(const DOMFormData& formData, EncodingType encodingType)
@@ -91,7 +86,7 @@ Ref<FormData> FormData::create(const DOMFormData& formData, EncodingType encodin
     return result;
 }
 
-Ref<FormData> FormData::create(Vector<WebCore::FormDataElement>&& elements, uint64_t identifier, bool alwaysStream, Vector<char>&& boundary)
+Ref<FormData> FormData::create(Vector<WebCore::FormDataElement>&& elements, uint64_t identifier, bool alwaysStream, Vector<uint8_t>&& boundary)
 {
     auto result = create();
     result->setAlwaysStream(alwaysStream);
@@ -164,7 +159,7 @@ FormDataElement FormDataElement::isolatedCopy() const
 {
     return WTF::switchOn(data,
         [] (const Vector<uint8_t>& bytes) {
-            return FormDataElement(Vector { bytes.data(), bytes.size() });
+            return FormDataElement(Vector { bytes });
         }, [] (const FormDataElement::EncodedFileData& fileData) {
             return FormDataElement(fileData.isolatedCopy());
         }, [] (const FormDataElement::EncodedBlobData& blobData) {
@@ -173,16 +168,16 @@ FormDataElement FormDataElement::isolatedCopy() const
     );
 }
 
-void FormData::appendData(const void* data, size_t size)
+void FormData::appendData(std::span<const uint8_t> data)
 {
     m_lengthInBytes = std::nullopt;
     if (!m_elements.isEmpty()) {
         if (auto* vector = std::get_if<Vector<uint8_t>>(&m_elements.last().data)) {
-            vector->append(static_cast<const uint8_t*>(data), size);
+            vector->append(data);
             return;
         }
     }
-    m_elements.append(Vector { static_cast<const uint8_t*>(data), size });
+    m_elements.append(Vector(data));
 }
 
 void FormData::appendFile(const String& filename)
@@ -208,7 +203,7 @@ static Vector<uint8_t> normalizeStringData(PAL::TextEncoding& encoding, const St
     return normalizeLineEndingsToCRLF(encoding.encode(value, PAL::UnencodableHandling::Entities, PAL::NFCNormalize::No));
 }
 
-void FormData::appendMultiPartFileValue(const File& file, Vector<char>& header, PAL::TextEncoding& encoding)
+void FormData::appendMultiPartFileValue(const File& file, Vector<uint8_t>& header, PAL::TextEncoding& encoding)
 {
     auto name = file.name();
 
@@ -224,7 +219,7 @@ void FormData::appendMultiPartFileValue(const File& file, Vector<char>& header, 
     FormDataBuilder::addContentTypeToMultiPartHeader(header, contentType.ascii());
 
     FormDataBuilder::finishMultiPartHeader(header);
-    appendData(header.data(), header.size());
+    appendData(header.span());
 
     if (!file.path().isEmpty())
         appendFile(file.path());
@@ -232,13 +227,13 @@ void FormData::appendMultiPartFileValue(const File& file, Vector<char>& header, 
         appendBlob(file.url());
 }
 
-void FormData::appendMultiPartStringValue(const String& string, Vector<char>& header, PAL::TextEncoding& encoding)
+void FormData::appendMultiPartStringValue(const String& string, Vector<uint8_t>& header, PAL::TextEncoding& encoding)
 {
     FormDataBuilder::finishMultiPartHeader(header);
-    appendData(header.data(), header.size());
+    appendData(header.span());
 
     auto normalizedStringData = normalizeStringData(encoding, string);
-    appendData(normalizedStringData.data(), normalizedStringData.size());
+    appendData(normalizedStringData.span());
 }
 
 void FormData::appendMultiPartKeyValuePairItems(const DOMFormData& formData)
@@ -247,31 +242,32 @@ void FormData::appendMultiPartKeyValuePairItems(const DOMFormData& formData)
 
     auto encoding = formData.encoding();
 
-    Vector<char> encodedData;
+    Vector<uint8_t> encodedData;
     for (auto& item : formData.items()) {
         auto normalizedName = normalizeStringData(encoding, item.name);
     
-        Vector<char> header;
-        FormDataBuilder::beginMultiPartHeader(header, m_boundary.data(), normalizedName);
+        Vector<uint8_t> header;
+        FormDataBuilder::beginMultiPartHeader(header, m_boundary.span(), normalizedName);
 
         if (std::holds_alternative<RefPtr<File>>(item.data))
             appendMultiPartFileValue(*std::get<RefPtr<File>>(item.data), header, encoding);
         else
             appendMultiPartStringValue(std::get<String>(item.data), header, encoding);
 
-        appendData("\r\n", 2);
+        constexpr std::array<uint8_t, 2> newline { '\r', '\n' };
+        appendData(newline);
     }
     
-    FormDataBuilder::addBoundaryToMultiPartHeader(encodedData, m_boundary.data(), true);
+    FormDataBuilder::addBoundaryToMultiPartHeader(encodedData, m_boundary.span(), true);
 
-    appendData(encodedData.data(), encodedData.size());
+    appendData(encodedData.span());
 }
 
 void FormData::appendNonMultiPartKeyValuePairItems(const DOMFormData& formData, EncodingType encodingType)
 {
     auto encoding = formData.encoding();
 
-    Vector<char> encodedData;
+    Vector<uint8_t> encodedData;
     for (auto& item : formData.items()) {
         String stringValue = WTF::switchOn(item.data,
             [](const String& string) {
@@ -286,7 +282,7 @@ void FormData::appendNonMultiPartKeyValuePairItems(const DOMFormData& formData, 
         FormDataBuilder::addKeyValuePairAsFormData(encodedData, normalizedName, normalizedStringData, encodingType);
     }
 
-    appendData(encodedData.data(), encodedData.size());
+    appendData(encodedData.span());
 }
 
 Vector<uint8_t> FormData::flatten() const
@@ -322,7 +318,7 @@ static void appendBlobResolved(BlobRegistryImpl* blobRegistry, FormData& formDat
     for (const auto& blobItem : blobData->items()) {
         if (blobItem.type() == BlobDataItem::Type::Data) {
             ASSERT(blobItem.data());
-            formData.appendData(blobItem.data()->data() + static_cast<int>(blobItem.offset()), static_cast<int>(blobItem.length()));
+            formData.appendData(blobItem.data()->bytes().subspan(blobItem.offset(), blobItem.length()));
         } else if (blobItem.type() == BlobDataItem::Type::File)
             formData.appendFileRange(blobItem.file()->path(), blobItem.offset(), blobItem.length(), blobItem.file()->expectedModificationTime());
         else
@@ -353,7 +349,7 @@ Ref<FormData> FormData::resolveBlobReferences(BlobRegistryImpl* blobRegistryImpl
     for (auto& element : m_elements) {
         switchOn(element.data,
             [&] (const Vector<uint8_t>& bytes) {
-                newFormData->appendData(bytes.data(), bytes.size());
+                newFormData->appendData(bytes.span());
             }, [&] (const FormDataElement::EncodedFileData& fileData) {
                 newFormData->appendFileRange(fileData.filename, fileData.fileStart, fileData.fileLength, fileData.expectedFileModificationTime);
             }, [&] (const FormDataElement::EncodedBlobData& blobData) {

Source/WebKit/UIProcess/API/glib/IconDatabase.cpp
@@ -638,7 +638,7 @@ void IconDatabase::setIconForPageURL(const String& iconURL, const uint8_t* iconD
         return;
     }
 
-    Vector<uint8_t> data { iconData, iconDataSize };
+    Vector<uint8_t> data(std::span { iconData, iconDataSize });
     m_workQueue->dispatch([this, protectedThis = Ref { *this }, iconURL = iconURL.isolatedCopy(), iconData = WTFMove(data), pageURL = pageURL.isolatedCopy(), completionHandler = WTFMove(completionHandler)]() mutable {
         bool result = false;
         if (m_db.isOpen()) {

Tools/TestWebKitAPI/Tests/WTF/Vector.cpp
@@ -321,7 +321,7 @@ TEST(WTF_Vector, CopyFromOtherMinCapacity)
 TEST(WTF_Vector, ConstructorOtherRawPointerTypeAndLength)
 {
     const UChar uchars[] = { 'b', 'a', 'r' };
-    Vector<LChar> vector(uchars, 3);
+    Vector<LChar> vector(std::span { uchars });
     EXPECT_EQ(vector.size(), 3U);
     EXPECT_EQ(vector[0], 'b');
     EXPECT_EQ(vector[1], 'a');
@@ -1715,9 +1715,9 @@ TEST(WTF_Vector, HashKeyString)
 TEST(WTF_Vector, ConstructorFromRawPointerAndSize)
 {
     constexpr size_t inputSize = 5;
-    uint8_t input[inputSize] = { 1, 2, 3, 4, 5 };
+    std::array<uint8_t, inputSize> input { 1, 2, 3, 4, 5 };
 
-    Vector<uint8_t> vector { input, inputSize };
+    Vector<uint8_t> vector { input };
     ASSERT_EQ(vector.size(), inputSize);
     EXPECT_EQ(vector[0], 1);
     EXPECT_EQ(vector[1], 2);

commit 2694d9fd7095bb1e77640cd3ac0b799448758ed7

[GCC] Unreviewed, build fix for Debian 11 after 276220@main
Several build bots using GCC10.2 (Debian 11) and GCC 11.4 (Ubuntu 22.04)
are failing after changes introduced in 276220@main, possibly due to
limitations in the compiler.

* Source/WebCore/PAL/pal/crypto/gcrypt/CryptoDigestGCrypt.cpp:
(PAL::CryptoDigest::computeHash):
* Source/WebCore/loader/FormSubmission.cpp:
(WebCore::appendMailtoPostFormDataToURL):
* Source/WebCore/platform/network/FormData.cpp:
(WebCore::FormDataElement::isolatedCopy const):
* Source/WebKit/UIProcess/API/glib/IconDatabase.cpp:
(WebKit::IconDatabase::setIconForPageURL):
* Tools/TestWebKitAPI/Tests/WTF/Vector.cpp:
(TestWebKitAPI::TEST):

Canonical link: https://commits.webkit.org/276293@main

Source/WebCore/PAL/pal/crypto/gcrypt/CryptoDigestGCrypt.cpp
@@ -88,7 +88,7 @@ Vector<uint8_t> CryptoDigest::computeHash()
     size_t digestLen = gcry_md_get_algo_dlen(m_context->algorithm);
 
     gcry_md_final(m_context->md);
-    Vector<uint8_t> result(std::span { gcry_md_read(m_context->md, 0), digestLen });
+    Vector<uint8_t> result(std::span<uint8_t> { gcry_md_read(m_context->md, 0), digestLen });
     gcry_md_close(m_context->md);
 
     return result;
@@ -108,7 +108,7 @@ std::optional<Vector<uint8_t>> CryptoDigest::computeHash(CryptoDigest::Algorithm
 
     gcry_md_write(digest->m_context->md, input.data(), input.size());
     gcry_md_final(digest->m_context->md);
-    Vector<uint8_t> result(std::span { gcry_md_read(digest->m_context->md, 0), digestLen });
+    Vector<uint8_t> result(std::span<uint8_t> { gcry_md_read(digest->m_context->md, 0), digestLen });
     gcry_md_close(digest->m_context->md);
 
     return result;

Source/WebCore/loader/FormSubmission.cpp
@@ -74,7 +74,7 @@ static void appendMailtoPostFormDataToURL(URL& url, const FormData& data, const 
         body = PAL::decodeURLEscapeSequences(makeStringByReplacingAll(makeStringByReplacingAll(body, '&', "\r\n"_s), '+', ' '));
     }
 
-    Vector<uint8_t> bodyData(std::span { "body=", 5 });
+    Vector<uint8_t> bodyData(std::span<const char>("body=", static_cast<size_t>(5)));
     FormDataBuilder::encodeStringAsFormData(bodyData, body.utf8());
     body = makeStringByReplacingAll(String(bodyData.data(), bodyData.size()), '+', "%20"_s);
 

Source/WebCore/platform/network/FormData.cpp
@@ -159,7 +159,7 @@ FormDataElement FormDataElement::isolatedCopy() const
 {
     return WTF::switchOn(data,
         [] (const Vector<uint8_t>& bytes) {
-            return FormDataElement(Vector { bytes });
+            return FormDataElement(Vector<uint8_t> { bytes });
         }, [] (const FormDataElement::EncodedFileData& fileData) {
             return FormDataElement(fileData.isolatedCopy());
         }, [] (const FormDataElement::EncodedBlobData& blobData) {

Source/WebKit/UIProcess/API/glib/IconDatabase.cpp
@@ -638,7 +638,7 @@ void IconDatabase::setIconForPageURL(const String& iconURL, const uint8_t* iconD
         return;
     }
 
-    Vector<uint8_t> data(std::span { iconData, iconDataSize });
+    Vector<uint8_t> data(std::span<const uint8_t> { iconData, static_cast<size_t>(iconDataSize) });
     m_workQueue->dispatch([this, protectedThis = Ref { *this }, iconURL = iconURL.isolatedCopy(), iconData = WTFMove(data), pageURL = pageURL.isolatedCopy(), completionHandler = WTFMove(completionHandler)]() mutable {
         bool result = false;
         if (m_db.isOpen()) {

Tools/TestWebKitAPI/Tests/WTF/Vector.cpp
@@ -321,7 +321,7 @@ TEST(WTF_Vector, CopyFromOtherMinCapacity)
 TEST(WTF_Vector, ConstructorOtherRawPointerTypeAndLength)
 {
     const UChar uchars[] = { 'b', 'a', 'r' };
-    Vector<LChar> vector(std::span { uchars });
+    Vector<LChar> vector(std::span(uchars, static_cast<size_t>(3)));
     EXPECT_EQ(vector.size(), 3U);
     EXPECT_EQ(vector[0], 'b');
     EXPECT_EQ(vector[1], 'a');