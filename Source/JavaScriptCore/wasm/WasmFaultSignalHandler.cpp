6c9fb3193d9dbf320747d07b485d275b4adf8f2d
Commit Queue <commit-queue@webkit.org>
2024-03-18 14:55:39 -0700

    Unreviewed, reverting 276198@main and 276218@main. https://bugs.webkit.org/show_bug.cgi?id=271182
    Broke internal Safari
    
    Reverted changesets:
    
    "new exception handler adoption"
    https://bugs.webkit.org/show_bug.cgi?id=269728
    https://commits.webkit.org/276198@main
    
    "fix darwin_arm_thread_state64 macro usage build issue"
    https://bugs.webkit.org/show_bug.cgi?id=271097
    https://commits.webkit.org/276218@main
    
    Canonical link: https://commits.webkit.org/276302@main

--- Source/JavaScriptCore/wasm/WasmFaultSignalHandler.cpp
+++ Source/JavaScriptCore/wasm/WasmFaultSignalHandler.cpp
@@ -38,29 +38,11 @@
 #include "WasmExceptionType.h"
 #include "WasmMemory.h"
 #include "WasmThunks.h"
-#include <wtf/CodePtr.h>
-#include <wtf/CryptographicallyRandomNumber.h>
 #include <wtf/HashSet.h>
 #include <wtf/Lock.h>
 #include <wtf/threads/Signals.h>
 
 namespace JSC { namespace Wasm {
-
-using WTF::CodePtr;
-
-#if CPU(ARM64E) && defined(EXCEPTION_STATE_IDENTITY_PROTECTED) && !PLATFORM(IOS_FAMILY_SIMULATOR)
-void* presignedTrampoline = { };
-
-MachExceptionSigningKey::MachExceptionSigningKey()
-{
-    // Sign the trampoline pointer using a random diversifier and stash it away before webcontent has started so that
-    // even a PAC signing gadget cannot fake this random diversifier
-    randomSigningKey = WTF::cryptographicallyRandomNumber<uint32_t>() & __DARWIN_ARM_THREAD_STATE64_USER_DIVERSIFIER_MASK;
-    uint64_t diversifier = ptrauth_blend_discriminator((void *)(unsigned long)randomSigningKey, ptrauth_string_discriminator("pc"));
-    presignedTrampoline = JSC::LLInt::getCodePtr<CFunctionPtrTag>(wasm_throw_from_fault_handler_trampoline_reg_instance).untaggedPtr();
-    presignedTrampoline = ptrauth_sign_unauthenticated(presignedTrampoline, ptrauth_key_function_pointer, diversifier);
-}
-#endif // CPU(ARM64E) && defined(EXCEPTION_STATE_IDENTITY_PROTECTED) && !PLATFORM(IOS_FAMILY_SIMULATOR)
 
 namespace {
 namespace WasmFaultSignalHandlerInternal {
@@ -114,11 +96,7 @@ static SignalAction trapHandler(Signal signal, SigInfo& sigInfo, PlatformRegiste
             };
 
             if (didFaultInWasm(faultingInstruction)) {
-#if CPU(ARM64E) && defined(EXCEPTION_STATE_IDENTITY_PROTECTED) && !PLATFORM(IOS_FAMILY_SIMULATOR)
-                MachineContext::setInstructionPointer(context, presignedTrampoline);
-#else
                 MachineContext::setInstructionPointer(context, LLInt::getCodePtr<CFunctionPtrTag>(wasm_throw_from_fault_handler_trampoline_reg_instance));
-#endif
                 return SignalAction::Handled;
             }
         }
@@ -137,7 +115,6 @@ void activateSignalingMemory()
             return;
 
         activateSignalHandlersFor(Signal::AccessFault);
-        WTF::finalizeSignalHandlers();
     });
 }
 
