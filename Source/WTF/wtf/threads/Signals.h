b4c696d154216d985cfc489c54e52cc7dd110c97
Brandon Kayes <bkayes@apple.com>
2024-03-19 00:12:40 -0700

    new exception handler adoption https://bugs.webkit.org/show_bug.cgi?id=269728 rdar://119256062
    Reviewed by NOBODY (OOPS!).
    
    This adopts the new hardened mach exception mechanism which was
    introduced in darwin recently. See the radar for more details.
    
    * Source/JavaScriptCore/jsc.cpp:
    (main):
    (CommandLine::parseArguments):
    * Source/JavaScriptCore/runtime/InitializeThreading.cpp:
    (JSC::initialize):
    * Source/JavaScriptCore/runtime/MachineContext.h:
    (JSC::MachineContext::setInstructionPointer):
    * Source/JavaScriptCore/runtime/VMTraps.cpp:
    * Source/JavaScriptCore/wasm/WasmFaultSignalHandler.cpp:
    (JSC::Wasm::MachExceptionSigningKey::MachExceptionSigningKey):
    (JSC::Wasm::trapHandler):
    (JSC::Wasm::activateSignalingMemory):
    (): Deleted.
    * Source/JavaScriptCore/wasm/WasmFaultSignalHandler.h:
    * Source/WTF/WTF.xcodeproj/project.pbxproj:
    * Source/WTF/wtf/PlatformJSCOnly.cmake:
    * Source/WTF/wtf/PlatformMac.cmake:
    * Source/WTF/wtf/PlatformRegisters.cpp:
    (WTF::threadStatePCInternal):
    * Source/WTF/wtf/PlatformRegisters.h:
    * Source/WTF/wtf/threads/Signals.cpp:
    (WTF::initMachExceptionHandlerThread):
    (WTF::setExceptionPorts):
    (WTF::activateSignalHandlersFor):
    (WTF::finalizeSignalHandlers):
    (WTF::toMachMask): Deleted.
    * Source/WTF/wtf/threads/Signals.h:
    (WTF::toMachMask):
    (WTF::initializeSignalHandling):
    (WTF::disableSignalHandling):
    * Source/WTF/wtf/win/SignalsWin.cpp:
    (WTF::finalizeSignalHandlers):
    * Source/WebKit/Resources/SandboxProfiles/ios/com.apple.WebKit.WebContent.sb.in:
    * Source/WebKit/WebProcess/com.apple.WebProcess.sb.in:
    * Tools/TestWebKitAPI/Tests/WTF/Signals.cpp:
    (TEST):

--- Source/WTF/wtf/threads/Signals.h
+++ Source/WTF/wtf/threads/Signals.h
@@ -124,18 +124,36 @@ struct SignalHandlers {
 // and once commited they can't be turned off.
 WTF_EXPORT_PRIVATE void addSignalHandler(Signal, SignalHandler&&);
 WTF_EXPORT_PRIVATE void activateSignalHandlersFor(Signal);
+WTF_EXPORT_PRIVATE void finalizeSignalHandlers();
 
+#if OS(UNIX) && HAVE(MACH_EXCEPTIONS)
+inline exception_mask_t toMachMask(Signal signal)
+{
+    switch (signal) {
+    case Signal::AccessFault: return EXC_MASK_BAD_ACCESS;
+    case Signal::IllegalInstruction: return EXC_MASK_BAD_INSTRUCTION;
+    case Signal::FloatingPoint: return EXC_MASK_ARITHMETIC;
+    case Signal::Breakpoint: return EXC_MASK_BREAKPOINT;
+    default: break;
+    }
+    RELEASE_ASSERT_NOT_REACHED();
+}
+#endif // OS(UNIX) && HAVE(MACH_EXCEPTIONS)
 
 #if HAVE(MACH_EXCEPTIONS)
 class Thread;
 void registerThreadForMachExceptionHandling(Thread&);
-WTF_EXPORT_PRIVATE void initMachExceptionHandlerThread(bool);
-inline void initializeSignalHandling() { initMachExceptionHandlerThread(true); }
-inline void disableSignalHandling() { initMachExceptionHandlerThread(false); }
+WTF_EXPORT_PRIVATE void initMachExceptionHandlerThread(bool, uint32_t, exception_mask_t);
+inline void initializeSignalHandling(uint32_t signingKey, exception_mask_t mask) { initMachExceptionHandlerThread(true, signingKey, mask); }
+inline void disableSignalHandling() { initMachExceptionHandlerThread(false, 0, 0); }
 
 void handleSignalsWithMach();
 #else
-inline void initializeSignalHandling() { }
+inline void initializeSignalHandling(uint32_t signingKey, int mask)
+{
+    UNUSED_PARAM(signingKey);
+    UNUSED_PARAM(mask);
+}
 inline void disableSignalHandling() { }
 #endif // HAVE(MACH_EXCEPTIONS)
 
@@ -152,5 +170,6 @@ using WTF::SignalAction;
 using WTF::SignalHandler;
 using WTF::addSignalHandler;
 using WTF::activateSignalHandlersFor;
+using WTF::finalizeSignalHandlers;
 using WTF::initializeSignalHandling;
 using WTF::disableSignalHandling;
