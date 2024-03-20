81b0d446a83d8bab20a4244d95b8885a53d81ad5
Erica Li <lerica@apple.com>
2024-03-20 06:47:04 -0700

    ASAN_SEGV | Hard null deref |LayoutIntegration::BoxTree::layoutBoxForRenderer; LayoutIntegration::LineLayout::enclosingBorderBoxRectFor; WebCore::RenderInline::linesBoundingBox. https://bugs.webkit.org/show_bug.cgi?id=266567. rdar://114586645.
    Reviewed by Alan Baradlay.
    
    similar to 107979394, apply handling for repainting a freshly inserted sticky inline box.
    
    * LayoutTests/fast/inline/sticky-inline-box-invalidation-repaint-crash-expected.txt: Added.
    * LayoutTests/fast/inline/sticky-inline-box-invalidation-repaint-crash.html: Added.
    * LayoutTests/platform/ios-wk2/imported/w3c/web-platform-tests/mathml/relations/html5-tree/dynamic-childlist-002-expected.txt: re-baseline for rdar://119187070.
    * Source/WebCore/rendering/RenderInline.cpp:
    (WebCore::RenderInline::linesBoundingBox const):
    
    Originally-landed-as: 272448.75@safari-7618-branch (2534e02e1983). rdar://124556813
    Canonical link: https://commits.webkit.org/276404@main

--- Source/WebCore/rendering/RenderInline.cpp
+++ Source/WebCore/rendering/RenderInline.cpp
@@ -505,14 +505,20 @@ LayoutUnit RenderInline::innerPaddingBoxHeight() const
 
 IntRect RenderInline::linesBoundingBox() const
 {
+    IntRect result;
+
     if (auto* layout = LayoutIntegration::LineLayout::containing(*this)) {
         if (!layout->contains(*this))
-            return { };
+            return result;
+
+        if (!layoutBox()) {
+            // Repaint may be issued on subtrees during content mutation with newly inserted renderers.
+            ASSERT(needsLayout());
+            return result;
+        }
         return enclosingIntRect(layout->enclosingBorderBoxRectFor(*this));
     }
 
-    IntRect result;
-    
     // See <rdar://problem/5289721>, for an unknown reason the linked list here is sometimes inconsistent, first is non-zero and last is zero.  We have been
     // unable to reproduce this at all (and consequently unable to figure ot why this is happening).  The assert will hopefully catch the problem in debug
     // builds and help us someday figure out why.  We also put in a redundant check of lastLineBox() to avoid the crash for now.
