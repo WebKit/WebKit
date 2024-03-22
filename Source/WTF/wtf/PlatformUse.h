e0a393ed719c88b2fae6e0b2ffcdaefd13d1fbad
Per Arne Vollan <pvollan@apple.com>
2024-03-22 09:09:01 -0700

    Include WebKit process extension source files for all SDK variants https://bugs.webkit.org/show_bug.cgi?id=271418 rdar://125195321
    Reviewed by Sihui Liu.
    
    * Source/WTF/wtf/PlatformUse.h:
    * Source/WebKit/Configurations/WebKit.xcconfig:
    
    Canonical link: https://commits.webkit.org/276550@main

--- Source/WTF/wtf/PlatformUse.h
+++ Source/WTF/wtf/PlatformUse.h
@@ -440,6 +440,11 @@
 #define USE_SANDBOX_VERSION_3 1
 #endif
 
+// We already enable ExtensionKit when building with the internal SDK. Also enable it in Open Source builds.
+#if !defined(USE_EXTENSIONKIT) && PLATFORM(IOS) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 170400 && !USE(APPLE_INTERNAL_SDK)
+#define USE_EXTENSIONKIT 1
+#endif
+
 #if !defined(USE_BROWSERENGINEKIT) && PLATFORM(IOS) && __has_include(<BrowserEngineKit/BETextInput.h>)
 #define USE_BROWSERENGINEKIT 1
 #endif
