178e479f109f0ad4440c1d72b1fbae3656b6abeb
Youenn Fablet <youenn@apple.com>
2024-03-28 08:00:47 -0700

    [macOS] Avoid HALOutput sample rate audio conversion rdar://116794643 https://bugs.webkit.org/show_bug.cgi?id=271821
    Reviewed by Eric Carlson.
    
    Stick to hardware sample rate to avoid sample rate audio conversion at HAL output level.
    Manually tested.
    
    * Source/WebCore/platform/mediastream/mac/CoreAudioSharedUnit.cpp:
    (WebCore::CoreAudioSharedUnit::setupAudioUnit):
    
    Canonical link: https://commits.webkit.org/276775@main

--- Source/WebCore/platform/mediastream/mac/CoreAudioSharedUnit.cpp
+++ Source/WebCore/platform/mediastream/mac/CoreAudioSharedUnit.cpp
@@ -351,6 +351,18 @@ OSStatus CoreAudioSharedUnit::setupAudioUnit()
             RELEASE_LOG_ERROR_IF(err, WebRTC, "CoreAudioSharedUnit::setupAudioUnit(%p) unable to set capture unit output device ID %d, error %d (%.4s)", this, (int)defaultOutputDeviceID, (int)err, (char*)&err);
         }
         setOutputDeviceID(!err ? defaultOutputDeviceID : 0);
+    } else {
+        // With HALOutput, we cannot rely on sample rate conversions, we stick to hardware sample rate.
+        static const AudioObjectPropertyAddress nominalSampleRateAddress = {
+            kAudioDevicePropertyNominalSampleRate,
+            kAudioObjectPropertyScopeGlobal,
+            kAudioObjectPropertyElementMain
+        };
+
+        Float64 nominalSampleRate;
+        UInt32 nominalSampleRateSize = sizeof(Float64);
+        if (AudioObjectGetPropertyData(deviceID, &nominalSampleRateAddress, 0, 0, &nominalSampleRateSize, (void*)&nominalSampleRate) == noErr)
+            setSampleRate(nominalSampleRate);
     }
 #endif
 
