b1d861f1f579588da2b6d8b13ba5aa138a7afc46
Justin Michaud <justin_michaud@apple.com>
2024-04-01 16:57:18 -0700

    (2) Adopt com.apple.developer.cs.allow-jit entitlement for iOS. rdar://122841355 https://bugs.webkit.org/show_bug.cgi?id=270723
    Reviewed by Keith Miller.
    
    * Source/JavaScriptCore/Scripts/process-entitlements.sh:
    * Source/JavaScriptCore/jit/ExecutableAllocator.cpp:
    (JSC::isJITEnabled):
    (JSC::ExecutableAllocator::disableJIT):
    * Source/WebKit/Scripts/process-entitlements.sh:
    * Tools/WebKitTestRunner/Configurations/WebKitTestRunner-internal.entitlements:
    * Tools/WebKitTestRunner/Configurations/WebKitTestRunnerApp-iOS.entitlements:
    
    Canonical link: https://commits.webkit.org/276913@main

--- Source/WebKit/Scripts/process-entitlements.sh
+++ Source/WebKit/Scripts/process-entitlements.sh
@@ -399,10 +399,17 @@ fi
 
 function ios_family_process_webcontent_entitlements()
 {
-    if [[ "${WK_PLATFORM_NAME}" != watchos ]]
-    then
+    if [[ "${PLATFORM_NAME}" != watchos ]]; then
         plistbuddy Add :com.apple.private.verified-jit bool YES
-        plistbuddy Add :dynamic-codesigning bool YES
+        if [[ "${PLATFORM_NAME}" == iphoneos ]]; then
+            if (( $(( ${SDK_VERSION_ACTUAL} )) >= 170400 )); then
+                plistbuddy Add :com.apple.developer.cs.allow-jit bool YES
+            else
+                plistbuddy Add :dynamic-codesigning bool YES
+            fi
+        else
+            plistbuddy Add :dynamic-codesigning bool YES
+        fi
     fi
     plistbuddy Add :com.apple.developer.kernel.extended-virtual-addressing bool YES
 
