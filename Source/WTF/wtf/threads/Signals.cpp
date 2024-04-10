4f6ac50c1908c99b92cc73cfe38d2579ea8d6606
Justin Michaud <justin@justinmichaud.com>
2024-04-09 18:58:10 -0700

    Cherry-pick 5144a8edcfab. rdar://126000755
        (Re-land) Don't initialize mach exceptions if we didn't register any.
        https://bugs.webkit.org/show_bug.cgi?id=272261
        rdar://126000755
    
        The original patch by Keith was reverted because it came after the enablement patch
        for the new exceptions API. It turns out that even after reverting, we still need
        this fix.
    
        Original patch:
                Don't initialize mach exceptions if we didn't register any.
                https://bugs.webkit.org/show_bug.cgi?id=272261
                rdar://126000755
    
                Reviewed by Justin Michaud.
    
                This also fixes GPU process crashing on launch because the sandbox doesn't allow `task_register_hardened_exception_handler`.
    
                * Source/WTF/wtf/threads/Signals.cpp:
                (WTF::initMachExceptionHandlerThread):
    
                Canonical link: https://commits.webkit.org/277143@main
        ----------------
    
        Canonical link: https://commits.webkit.org/277281@main

--- Source/WTF/wtf/threads/Signals.cpp
+++ Source/WTF/wtf/threads/Signals.cpp
@@ -134,6 +134,12 @@ void initMachExceptionHandlerThread(bool enable, uint32_t signingKey, exception_
         Config::AssertNotFrozenScope assertScope;
         SignalHandlers& handlers = g_wtfConfig.signalHandlers;
 
+        // We need this because some WebKit processes (e.g. GPU process) don't allow signal handling in their
+        // sandbox profiles. We don't use them there so there's no point in setting up a dispatch queue we're
+        // never going to use.
+        if (!handlers.addedExceptions)
+            return;
+
         uint16_t flags = MPO_INSERT_SEND_RIGHT;
 
 // This provisional flag can be removed once macos sonoma is no longer supported
