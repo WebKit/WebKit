84e6be5523eb608f5ad2abfbdbba3007c58d881c
Jonathan Bedard <jbedard@apple.com>
2024-04-10 15:52:25 -0700

    Cherry-pick 277213@main (3608a480f439). rdar://118901053
        [webkitscmpy] Optionally include commit message in diff
        https://bugs.webkit.org/show_bug.cgi?id=265481
        rdar://118901053
    
        Reviewed by Dewei Zhu.
    
        Programs generating diffs may want to include commit messages
        in patch format.
    
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/local/git.py:
        (Git.diff): Return lines in a diff, use format-patch if caller
        requests commit message be included.
        (Git.diff_lines): Renamed 'diff'.
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/local/scm.py:
        (Scm.diff): Added.
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/mocks/local/git.py:
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/land.py:
        (Land.main): Call 'diff' instead of 'diff_lines'
        * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/git_unittest.py:
    
        Canonical link: https://commits.webkit.org/277213@main
    
    Canonical link: https://commits.webkit.org/272448.903@safari-7618-branch

--- Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/git_unittest.py
+++ Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/git_unittest.py
@@ -491,20 +491,40 @@ CommitDate: {time_c}
             self.assertEqual(repo.rebase(target='main', base='main', head='branch-a', recommit=False), 0)
             self.assertEqual(str(repo.commit(branch='branch-a')), '5.2@branch-a')
 
-    def test_diff_lines(self):
+    def test_diff(self):
         with mocks.local.Git(self.path), OutputCapture():
             repo = local.Git(self.path)
             self.assertEqual(
-                ['--- a/ChangeLog', '+++ b/ChangeLog', '@@ -1,0 +1,0 @@', '+ Patch Series'],
-                list(repo.diff_lines(base='bae5d1e90999d4f916a8a15810ccfa43f37a2fd6'))
+                ['--- a/ChangeLog', '+++ b/ChangeLog', '@@ -1,0 +1,0 @@', '+Patch Series'],
+                list(repo.diff(base='bae5d1e90999d4f916a8a15810ccfa43f37a2fd6'))
             )
 
-    def test_diff_lines_identifier(self):
+    def test_diff_identifier(self):
+        with mocks.local.Git(self.path):
+            repo = local.Git(self.path)
+            self.assertEqual(
+                ['--- a/ChangeLog', '+++ b/ChangeLog', '@@ -1,0 +1,0 @@', '+8th commit'],
+                list(repo.diff(base='3@main', head='4@main'))
+            )
+
+    def test_diff_with_commit_message(self):
+        self.maxDiff = None
         with mocks.local.Git(self.path), OutputCapture():
             repo = local.Git(self.path)
             self.assertEqual(
-                ['--- a/ChangeLog', '+++ b/ChangeLog', '@@ -1,0 +1,0 @@', '+ 8th commit'],
-                list(repo.diff_lines(base='3@main', head='4@main'))
+                [
+                    'From bae5d1e90999d4f916a8a15810ccfa43f37a2fd6',
+                    'From: Jonathan Bedard <jbedard@apple.com>',
+                    'Date: {} +0000'.format(datetime.utcfromtimestamp(1601668000 + time.timezone).strftime('%a %b %d %H:%M:%S %Y')),
+                    'Subject: [PATCH] 8th commit',
+                    '---',
+                    'diff --git a/ChangeLog b/ChangeLog',
+                    '--- a/ChangeLog',
+                    '+++ b/ChangeLog',
+                    '@@ -1,0 +1,0 @@',
+                    '+8th commit',
+                ],
+                list(repo.diff(base='3@main', head='4@main', include_log=True))
             )
 
     def test_pull(self):
