e685b6eb7e6d74d50e053bfe6832712d30434f13
Ryosuke Niwa <rniwa@webkit.org>
2024-04-11 20:03:07 -0700

    Crash in Style::commitRelations https://bugs.webkit.org/show_bug.cgi?id=272543
    Reviewed by Yusuke Suzuki.
    
    Avoid using an extra register in the hope of fixing the crash.
    
    * Source/WebCore/cssjit/SelectorCompiler.cpp:
    (WebCore::SelectorCompiler::SelectorCodeGenerator::generateWalkToPreviousAdjacentElement):
    
    Canonical link: https://commits.webkit.org/277408@main

--- Source/WebCore/cssjit/SelectorCompiler.cpp
+++ Source/WebCore/cssjit/SelectorCompiler.cpp
@@ -2638,9 +2638,7 @@ inline void SelectorCodeGenerator::generateWalkToPreviousAdjacentElement(Assembl
 {
     Assembler::Label loopStart = m_assembler.label();
     m_assembler.loadPtr(Assembler::Address(workRegister, Node::previousSiblingMemoryOffset()), workRegister);
-    LocalRegister previousNodePointerMask(m_registerAllocator);
-    m_assembler.move(Assembler::TrustedImmPtr(Node::previousSiblingPointerMask()), previousNodePointerMask);
-    m_assembler.andPtr(previousNodePointerMask, workRegister);
+    m_assembler.andPtr(Assembler::TrustedImmPtr(Node::previousSiblingPointerMask()), workRegister);
     failureCases.append(m_assembler.branchTestPtr(Assembler::Zero, workRegister));
     DOMJIT::branchTestIsElementFlagOnNode(m_assembler, Assembler::Zero, workRegister).linkTo(loopStart, &m_assembler);
 }
