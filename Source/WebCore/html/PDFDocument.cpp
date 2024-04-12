a67b6bcf730abfb97664ddba0b4a8c14774bcd12
Michael Catanzaro <mcatanzaro@redhat.com>
2024-04-12 10:47:39 -0700

    Cherry-pick 277434@main (96e3800242ca). https://bugs.webkit.org/show_bug.cgi?id=249872
        Crash in PDFDocument::injectStyleAndContentScript when downloading PDF
        https://bugs.webkit.org/show_bug.cgi?id=249872
    
        Reviewed by Tim Nguyen.
    
        PDFDocument expects injectStyleAndContentScript to be called exactly once, but it may be called multiple times because the EventListener is never unregistered. So let's start by unregistering it when it's no longer needed (which requires saving a copy of the HTMLScriptElement). I'm not sure whether or not it's possible for a second load to start before the main resource load finishes, so I've added an extra member variable m_injectedStyleAndScript to really make sure we inject the user content only once, for good measure.
    
        This fixes a crash when attempting to download certain PDF documents, but does NOT make downloading the document actually work. It only avoids the crash.
    
        * Source/WebCore/html/PDFDocument.cpp:
        (WebCore::PDFDocumentEventListener::handleEvent):
        (WebCore::PDFDocument::finishedParsing):
        (WebCore::PDFDocument::finishLoadingPDF):
        (WebCore::PDFDocument::injectStyleAndContentScript):
        * Source/WebCore/html/PDFDocument.h:
    
        Canonical link: https://commits.webkit.org/277434@main
    
    Canonical link: https://commits.webkit.org/274313.144@webkitglib/2.44

--- Source/WebCore/html/PDFDocument.cpp
+++ Source/WebCore/html/PDFDocument.cpp
@@ -64,7 +64,7 @@ public:
     }
 
 private:
-    PDFDocumentParser(PDFDocument& document)
+    explicit PDFDocumentParser(PDFDocument& document)
         : RawDataDocumentParser(document)
     {
     }
@@ -118,7 +118,7 @@ void PDFDocumentEventListener::handleEvent(ScriptExecutionContext&, Event& event
     } else if (is<HTMLScriptElement>(event.target()) && event.type() == eventNames().loadEvent) {
         m_document->setContentScriptLoaded(true);
         if (m_document->isFinishedParsing())
-            m_document->sendPDFArrayBuffer();
+            m_document->finishLoadingPDF();
     } else
         ASSERT_NOT_REACHED();
 }
@@ -177,7 +177,7 @@ void PDFDocument::finishedParsing()
     ASSERT(m_iframe);
     m_isFinishedParsing = true;
     if (m_isContentScriptLoaded)
-        sendPDFArrayBuffer();
+        finishLoadingPDF();
 }
 
 void PDFDocument::postMessageToIframe(const String& name, JSC::JSObject* data)
@@ -219,8 +219,23 @@ void PDFDocument::sendPDFArrayBuffer()
     }
 }
 
+void PDFDocument::finishLoadingPDF()
+{
+    sendPDFArrayBuffer();
+
+    if (m_script) {
+        m_script->removeEventListener(eventNames().loadEvent, *m_listener, { });
+        m_script = nullptr;
+    }
+
+    m_listener = nullptr;
+}
+
 void PDFDocument::injectStyleAndContentScript()
 {
+    if (m_injectedStyleAndScript)
+        return;
+
     auto* contentDocument = m_iframe->contentDocument();
     ASSERT(contentDocument->head());
     auto link = HTMLLinkElement::create(HTMLNames::linkTag, *contentDocument, false);
@@ -233,11 +248,13 @@ void PDFDocument::injectStyleAndContentScript()
     contentDocument->head()->appendChild(link);
 
     ASSERT(contentDocument->body());
-    auto script = HTMLScriptElement::create(scriptTag, *contentDocument, false);
-    script->addEventListener(eventNames().loadEvent, m_listener.releaseNonNull(), false);
+    m_script = HTMLScriptElement::create(scriptTag, *contentDocument, false);
+    ASSERT(m_listener);
+    m_script->addEventListener(eventNames().loadEvent, *m_listener, false);
+    m_script->setAttribute(srcAttr, "webkit-pdfjs-viewer://pdfjs/extras/content-script.js"_s);
+    contentDocument->body()->appendChild(*m_script);
 
-    script->setAttribute(srcAttr, "webkit-pdfjs-viewer://pdfjs/extras/content-script.js"_s);
-    contentDocument->body()->appendChild(script);
+    m_injectedStyleAndScript = true;
 }
 
 } // namepsace WebCore
