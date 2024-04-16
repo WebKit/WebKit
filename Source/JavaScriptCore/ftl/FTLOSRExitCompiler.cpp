fd80b6547c5d44dbf20108f5d34e584bf7230fc0
Keith Miller <keith_miller@apple.com>
2024-04-16 10:04:50 -0700

    OSR Exit disassembly dump should convert ASCIILiteral to char* https://bugs.webkit.org/show_bug.cgi?id=272760 rdar://126560240
    Reviewed by Yusuke Suzuki.
    
    When we made ASCIILiteral a wrapper around std::span I missed there we some places that
    were passing the ASCIILiteral to a format string relying on them having the same layout.
    Instead we should just pass the characters explicitly.
    
    * Source/JavaScriptCore/dfg/DFGOSRExit.cpp:
    (JSC::DFG::JSC_DEFINE_JIT_OPERATION):
    * Source/JavaScriptCore/ftl/FTLOSRExitCompiler.cpp:
    (JSC::FTL::compileStub):
    
    Canonical link: https://commits.webkit.org/277549@main

--- Source/JavaScriptCore/ftl/FTLOSRExitCompiler.cpp
+++ Source/JavaScriptCore/ftl/FTLOSRExitCompiler.cpp
@@ -623,7 +623,7 @@ static void compileStub(VM& vm, unsigned exitID, JITCode* jitCode, OSRExit& exit
         patchBuffer, OSRExitPtrTag, nullptr,
         "FTL OSR exit #%u (D@%u, %s, %s) from %s, with operands = %s",
             exitID, exit.m_dfgNodeIndex, toCString(exit.m_codeOrigin).data(),
-            exitKindToString(exit.m_kind), toCString(*codeBlock).data(),
+            exitKindToString(exit.m_kind).characters8(), toCString(*codeBlock).data(),
             toCString(ignoringContext<DumpContext>(exit.m_descriptor->m_values)).data()
         );
 }
