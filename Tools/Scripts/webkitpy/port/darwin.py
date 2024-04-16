8df4906592e14d06bf448b40c426081479e8f931
Ryosuke Niwa <rniwa@webkit.org>
2024-04-15 22:19:01 -0700

    REGRESSION [ iOS ] 5X TestWebKitAPI.CopyRTF.Strips are crashing and failing https://bugs.webkit.org/show_bug.cgi?id=272239 rdar://125982015
    Reviewed by Wenson Hsieh, Tim Horton and Chris Dumez.
    
    Set __XPC___XPC_DYLD_LIBRARY_PATH and __XPC___XPC_DYLD_FRAMEWORK_PATH so that nsattributedstringagent
    will launch WebKit processes with _XPC_DYLD_LIBRARY_PATH and _XPC_DYLD_FRAMEWORK_PATH set.
    
    * Tools/Scripts/webkitpy/port/darwin.py:
    (DarwinPort.environment_for_api_tests):
    
    Canonical link: https://commits.webkit.org/277530@main

--- Tools/Scripts/webkitpy/port/darwin.py
+++ Tools/Scripts/webkitpy/port/darwin.py
@@ -265,7 +265,8 @@ class DarwinPort(ApplePort):
     def environment_for_api_tests(self):
         environment = super(DarwinPort, self).environment_for_api_tests()
         build_root_path = str(self._build_path())
-        for name in ['DYLD_LIBRARY_PATH', '__XPC_DYLD_LIBRARY_PATH', 'DYLD_FRAMEWORK_PATH', '__XPC_DYLD_FRAMEWORK_PATH']:
+        # nsattributedstringagent which gets launched by test runner must set __XPC_DYLD_LIBRARY_PATH and __XPC_DYLD_FRAMEWORK_PATH.
+        for name in ['DYLD_LIBRARY_PATH', '__XPC_DYLD_LIBRARY_PATH', '__XPC___XPC_DYLD_LIBRARY_PATH', 'DYLD_FRAMEWORK_PATH', '__XPC_DYLD_FRAMEWORK_PATH', '__XPC___XPC_DYLD_FRAMEWORK_PATH']:
             self._append_value_colon_separated(environment, name, build_root_path)
         return environment
 
