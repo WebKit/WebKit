9943fcd94ec5ff4b5d8ba2efea75c1acd475dcb0
Mohsin Qureshi <mohsinq@apple.com>
2024-04-17 14:35:27 -0700

    Revert rdar://126195542 (️CrashTracer: amsengagementd at com.apple.JavaScriptCore: JSC::CodeBlock::finishCreation)
    this reverts commit a2c0dd0613baee1c2e60a1d2ac8c30e162059510
    
    * Source/JavaScriptCore/CMakeLists.txt:
    * Source/JavaScriptCore/JavaScriptCore.xcodeproj/project.pbxproj:
    * Source/JavaScriptCore/Sources.txt:
    * Source/JavaScriptCore/bytecode/ExpressionInfo.h:
    * Source/JavaScriptCore/runtime/CachedTypes.cpp:
    (JSC::GenericCacheEntry::GenericCacheEntry):
    (JSC::GenericCacheEntry::isUpToDate const):
    * Source/JavaScriptCore/runtime/FileBasedFuzzerAgentBase.h:
    * Source/JavaScriptCore/runtime/JSCBytecodeCacheVersion.cpp: Removed.
    * Source/JavaScriptCore/runtime/JSCBytecodeCacheVersion.cpp.in: Added.
    * Source/JavaScriptCore/runtime/JSCBytecodeCacheVersion.h:
    * Source/JavaScriptCore/runtime/JSGlobalObjectFunctions.cpp:
    * Source/WTF/wtf/spi/darwin/dyldSPI.h:

--- Source/JavaScriptCore/runtime/JSCBytecodeCacheVersion.h
+++ Source/JavaScriptCore/runtime/JSCBytecodeCacheVersion.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2024 Apple Inc. All rights reserved.
+ * Copyright (C) 2022 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,8 +27,6 @@
 
 #include <stdint.h>
 
-namespace JSC {
-
-JS_EXPORT_PRIVATE NEVER_INLINE uint32_t computeJSCBytecodeCacheVersion();
-
-}
+// Generated during the build based on a hash of JSCBuiltins.o, CachedTypes.o, and any production
+// build source version number.
+extern const uint32_t JSCBytecodeCacheVersion;
