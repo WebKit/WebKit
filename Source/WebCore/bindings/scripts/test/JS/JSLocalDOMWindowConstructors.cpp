e32f0cd738261af59645c6910a4e26ace8f31947
Alex Christensen <achristensen@apple.com>
2024-04-18 15:11:14 -0700

    Rename some files after LocalDOMWindow.idl removal https://bugs.webkit.org/show_bug.cgi?id=272867 rdar://126655433
    Reviewed by Matthew Finkel.
    
    It's now just DOMWindow.idl, which wraps a DOMWindow, which is either a LocalDOMWindow or a RemoteDOMWindow.
    Rename some files to reflect this change.
    
    * Source/WebCore/CMakeLists.txt:
    * Source/WebCore/DerivedSources-input.xcfilelist:
    * Source/WebCore/DerivedSources-output.xcfilelist:
    * Source/WebCore/DerivedSources.make:
    * Source/WebCore/Modules/cookie-store/DOMWindow+CookieStore.idl: Renamed from Source/WebCore/Modules/cookie-store/LocalDOMWindow+CookieStore.idl.
    * Source/WebCore/Modules/speech/DOMWindow+SpeechSynthesis.idl: Renamed from Source/WebCore/Modules/speech/LocalDOMWindow+SpeechSynthesis.idl.
    * Source/WebCore/Modules/webdatabase/DOMWindow+WebDatabase.idl: Renamed from Source/WebCore/Modules/webdatabase/LocalDOMWindow+WebDatabase.idl.
    * Source/WebCore/WebCore.xcodeproj/project.pbxproj:
    * Source/WebCore/bindings/scripts/test/DOMWindowConstructors.idl: Renamed from Source/WebCore/bindings/scripts/test/LocalDOMWindowConstructors.idl.
    * Source/WebCore/bindings/scripts/test/JS/JSLocalDOMWindowConstructors.cpp: Removed.
    * Source/WebCore/bindings/scripts/test/JS/JSLocalDOMWindowConstructors.h: Removed.
    * Source/WebCore/bindings/scripts/test/SupplementalDependencies.dep:
    * Source/WebCore/page/DOMWindow+CSSOM.idl: Renamed from Source/WebCore/page/LocalDOMWindow+CSSOM.idl.
    * Source/WebCore/page/DOMWindow+CSSOMView.idl: Renamed from Source/WebCore/page/LocalDOMWindow+CSSOMView.idl.
    * Source/WebCore/page/DOMWindow+Compat.idl: Renamed from Source/WebCore/page/LocalDOMWindow+Compat.idl.
    * Source/WebCore/page/DOMWindow+DeviceMotion.idl: Renamed from Source/WebCore/page/LocalDOMWindow+DeviceMotion.idl.
    * Source/WebCore/page/DOMWindow+DeviceOrientation.idl: Renamed from Source/WebCore/page/LocalDOMWindow+DeviceOrientation.idl.
    * Source/WebCore/page/DOMWindow+RequestIdleCallback.idl: Renamed from Source/WebCore/page/LocalDOMWindow+RequestIdleCallback.idl.
    * Source/WebCore/page/DOMWindow+Selection.idl: Renamed from Source/WebCore/page/LocalDOMWindow+Selection.idl.
    * Source/WebCore/page/DOMWindow+VisualViewport.idl: Renamed from Source/WebCore/page/LocalDOMWindow+VisualViewport.idl.
    * Source/WebCore/page/DOMWindow.idl:
    * Tools/Scripts/webkitpy/bindings/main.py:
    (BindingsTests.main):
    
    Canonical link: https://commits.webkit.org/277704@main

--- Source/WebCore/bindings/scripts/test/JS/JSLocalDOMWindowConstructors.cpp
+++
@@ -1,5 +0,0 @@
-/*
-    This file is generated to inform build scripts that JSLocalDOMWindowConstructors.h and
-    JSLocalDOMWindowConstructors.cpp were created for LocalDOMWindowConstructors.idl, and prevent the build
-    scripts from trying to regenerate JSLocalDOMWindowConstructors.h and JSLocalDOMWindowConstructors.cpp on every build.
-*/
