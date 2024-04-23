375af95d19d72c0f2586d9aaff69bd85d1b1e69f
Keith Miller <keith_miller@apple.com>
2024-04-23 09:15:24 -0700

    Cherry-pick f442fbe222f3. rdar://125596635
        Make it harder to get a PAC signing gadget in JIT code.
        https://bugs.webkit.org/show_bug.cgi?id=272750
        rdar://125596635
    
        Reviewed by Yusuke Suzuki.
    
        Right now if an attacker can control where code is allocated they can overlap code to create a PAC bypass.
        This patch makes that harder (in the WebContent process) by only allowing pacibsp and pacizb. This means
        that during arity fixup we now tag the return PC with pacizb. This is ok because we don't use the zero
        diversifier for anything. For reifying inlined call frames during OSR exit things are a bit more complicated.
        First we have be careful to only move signed return addresses into lr then untag them there. Also, we have
        to shuffle SP to point to where it would in reified frame. This means that there is technically live data
        below our SP, which on many OSes causes problems. Talking to our kernel folks however this isn't a problem
        as long as we don't have any signal handlers or run lldb expressions in this window. We don't use signal
        handlers in the WebContent process and this patch tries to limit/document the window of JIT code where lldb
        would trash the stack.
    
        * Source/JavaScriptCore/assembler/MacroAssemblerARM64E.h:
        (JSC::MacroAssemblerARM64E::tagPtr):
        * Source/JavaScriptCore/dfg/DFGOSRExitCompilerCommon.cpp:
        (JSC::DFG::reifyInlinedCallFrames):
        (JSC::AssemblyHelpers::transferReturnPC):
        * Source/JavaScriptCore/jit/ThunkGenerators.cpp:
        (JSC::arityFixupGenerator):
        * Source/JavaScriptCore/llint/LLIntThunks.cpp:
        (JSC::LLInt::tagGateThunk):
        (JSC::LLInt::untagGateThunk):
        * Source/JavaScriptCore/runtime/OptionsList.h:
        * Source/WTF/wtf/PtrTag.h:
        * Source/WebKit/WebProcess/WebProcess.cpp:
        (WebKit::WebProcess::initializeProcess):
        * Tools/Scripts/run-jsc-stress-tests:
    
        Canonical link: https://commits.webkit.org/272448.948@safari-7618-branch
    
    Canonical link: https://commits.webkit.org/277790.8@safari-7619.1.10-branch

--- Source/JavaScriptCore/assembler/MacroAssemblerARM64E.h
+++ Source/JavaScriptCore/assembler/MacroAssemblerARM64E.h
@@ -65,6 +65,12 @@ public:
 
     ALWAYS_INLINE void tagPtr(PtrTag tag, RegisterID target)
     {
+        if (!tag) {
+            m_assembler.pacizb(target);
+            return;
+        }
+
+        RELEASE_ASSERT(Options::allowNonSPTagging());
         auto tagGPR = getCachedDataTempRegisterIDAndInvalidate();
         move(TrustedImm64(tag), tagGPR);
         m_assembler.pacib(target, tagGPR);
@@ -76,11 +82,17 @@ public:
             m_assembler.pacibsp();
             return;
         }
+        RELEASE_ASSERT(Options::allowNonSPTagging());
         m_assembler.pacib(target, tag);
     }
 
     ALWAYS_INLINE void untagPtr(PtrTag tag, RegisterID target)
     {
+        if (!tag) {
+            m_assembler.autizb(target);
+            return;
+        }
+
         auto tagGPR = getCachedDataTempRegisterIDAndInvalidate();
         move(TrustedImm64(tag), tagGPR);
         m_assembler.autib(target, tagGPR);
