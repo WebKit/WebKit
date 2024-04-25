5fc7a2593e690290b50e9c9f8d30aab3374798d5
Claudio Saavedra <csaavedra@igalia.com>
2024-04-24 23:13:19 -0700

    [GLIB] Modernize some argument coders https://bugs.webkit.org/show_bug.cgi?id=272983
    Reviewed by Michael Catanzaro.
    
    Move some of the argument coders uses for GLib and GTK types
    to use the modern ArgumentCoder API. Remove the GKeyFile coders,
    as one can actually use GVariant for serialization of GTK printing
    structs and we already have a GVariant serializer.
    
    Also add UNLIKELY() for the std::nullopt bailout cases that can be
    optimized.
    
    * Source/WebKit/Platform/IPC/glib/ArgumentCodersGlib.h:
    * Source/WebKit/Shared/glib/ArgumentCodersGLib.cpp:
    (IPC::ArgumentCoder<GRefPtr<GVariant>>::decode):
    (IPC::ArgumentCoder<GRefPtr<GTlsCertificate>>::decode):
    (IPC::ArgumentCoder<GTlsCertificateFlags>::decode):
    (IPC::ArgumentCoder<GRefPtr<GUnixFDList>>::decode):
    * Source/WebKit/Shared/gtk/ArgumentCodersGtk.cpp:
    (IPC::ArgumentCoder<GRefPtr<GtkPrintSettings>>::encode):
    (IPC::ArgumentCoder<GRefPtr<GtkPrintSettings>>::decode):
    (IPC::ArgumentCoder<GRefPtr<GtkPageSetup>>::encode):
    (IPC::ArgumentCoder<GRefPtr<GtkPageSetup>>::decode):
    (IPC::encodeGKeyFile): Deleted.
    (IPC::decodeGKeyFile): Deleted.
    
    Canonical link: https://commits.webkit.org/277967@main

--- Source/WebKit/Shared/gtk/ArgumentCodersGtk.cpp
+++ Source/WebKit/Shared/gtk/ArgumentCodersGtk.cpp
@@ -26,6 +26,7 @@
 #include "config.h"
 #include "ArgumentCodersGtk.h"
 
+#include "ArgumentCodersGLib.h"
 #include "WebCoreArgumentCoders.h"
 #include <WebCore/GraphicsContext.h>
 #include <WebCore/Image.h>
@@ -38,77 +39,36 @@ namespace IPC {
 using namespace WebCore;
 using namespace WebKit;
 
-static void encodeGKeyFile(Encoder& encoder, GKeyFile* keyFile)
-{
-    gsize dataSize;
-    GUniquePtr<char> data(g_key_file_to_data(keyFile, &dataSize, 0));
-    encoder << std::span<const uint8_t>(reinterpret_cast<uint8_t*>(data.get()), dataSize);
-}
-
-static WARN_UNUSED_RETURN bool decodeGKeyFile(Decoder& decoder, GUniquePtr<GKeyFile>& keyFile)
-{
-    std::span<const uint8_t> dataReference;
-    if (!decoder.decode(dataReference))
-        return false;
-
-    if (!dataReference.size())
-        return true;
-
-    keyFile.reset(g_key_file_new());
-    if (!g_key_file_load_from_data(keyFile.get(), reinterpret_cast<const gchar*>(dataReference.data()), dataReference.size(), G_KEY_FILE_NONE, 0)) {
-        keyFile.reset();
-        return false;
-    }
-
-    return true;
-}
-
 void ArgumentCoder<GRefPtr<GtkPrintSettings>>::encode(Encoder& encoder, const GRefPtr<GtkPrintSettings>& argument)
 {
     GRefPtr<GtkPrintSettings> printSettings = argument ? argument : adoptGRef(gtk_print_settings_new());
-    GUniquePtr<GKeyFile> keyFile(g_key_file_new());
-    gtk_print_settings_to_key_file(printSettings.get(), keyFile.get(), "Print Settings");
-    encodeGKeyFile(encoder, keyFile.get());
+    GRefPtr<GVariant> variant = adoptGRef(gtk_print_settings_to_gvariant(printSettings.get()));
+    encoder << variant;
 }
 
 std::optional<GRefPtr<GtkPrintSettings>> ArgumentCoder<GRefPtr<GtkPrintSettings>>::decode(Decoder& decoder)
 {
-    GUniquePtr<GKeyFile> keyFile;
-    if (!decodeGKeyFile(decoder, keyFile))
+    auto variant = decoder.decode<GRefPtr<GVariant>>();
+    if (UNLIKELY(!variant))
         return std::nullopt;
 
-    GRefPtr<GtkPrintSettings> printSettings = adoptGRef(gtk_print_settings_new());
-    if (!keyFile)
-        return printSettings;
-
-    if (!gtk_print_settings_load_key_file(printSettings.get(), keyFile.get(), "Print Settings", nullptr))
-        return std::nullopt;
-
-    return printSettings;
+    return gtk_print_settings_new_from_gvariant(variant->get());
 }
 
 void ArgumentCoder<GRefPtr<GtkPageSetup>>::encode(Encoder& encoder, const GRefPtr<GtkPageSetup>& argument)
 {
     GRefPtr<GtkPageSetup> pageSetup = argument ? argument : adoptGRef(gtk_page_setup_new());
-    GUniquePtr<GKeyFile> keyFile(g_key_file_new());
-    gtk_page_setup_to_key_file(pageSetup.get(), keyFile.get(), "Page Setup");
-    encodeGKeyFile(encoder, keyFile.get());
+    GRefPtr<GVariant> variant = adoptGRef(gtk_page_setup_to_gvariant(pageSetup.get()));
+    encoder << variant;
 }
 
 std::optional<GRefPtr<GtkPageSetup>> ArgumentCoder<GRefPtr<GtkPageSetup>>::decode(Decoder& decoder)
 {
-    GUniquePtr<GKeyFile> keyFile;
-    if (!decodeGKeyFile(decoder, keyFile))
+    auto variant = decoder.decode<GRefPtr<GVariant>>();
+    if (UNLIKELY(!variant))
         return std::nullopt;
 
-    GRefPtr<GtkPageSetup> pageSetup = adoptGRef(gtk_page_setup_new());
-    if (!keyFile)
-        return pageSetup;
-
-    if (!gtk_page_setup_load_key_file(pageSetup.get(), keyFile.get(), "Page Setup", nullptr))
-        return std::nullopt;
-
-    return pageSetup;
+    return gtk_page_setup_new_from_gvariant(variant->get());
 }
 
 }
