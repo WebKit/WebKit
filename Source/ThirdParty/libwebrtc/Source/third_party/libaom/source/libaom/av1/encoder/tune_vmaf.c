12d8ca3a4c9838d7ffb2849fdd5d981e2985f6ee
Chris Dumez <cdumez@apple.com>
2024-04-25 21:27:44 -0700

    [libwebrtc] Cherry-pick Idf6db1394f2df26e6802f13073d34cd97a7ce8dc from upstream https://bugs.webkit.org/show_bug.cgi?id=273250 rdar://126967258
    Reviewed by Jean-Yves Avenard.
    
    Cherry-pick the following change from upstream:
    https://aomedia.googlesource.com/aom/+/0dce93a8dc96eb72aa92e0a7b05ad926df641a3e%5E%21/#F0
    
    * Source/ThirdParty/libwebrtc/Source/third_party/libaom/source/libaom/av1/encoder/tune_vmaf.c:
    
    Canonical link: https://commits.webkit.org/272448.963@safari-7618-branch

--- Source/ThirdParty/libwebrtc/Source/third_party/libaom/source/libaom/av1/encoder/tune_vmaf.c
+++ Source/ThirdParty/libwebrtc/Source/third_party/libaom/source/libaom/av1/encoder/tune_vmaf.c
@@ -247,7 +247,7 @@ static AOM_INLINE void unsharp(const AV1_COMP *const cpi,
 
 // 8-tap Gaussian convolution filter with sigma = 1.0, sums to 128,
 // all co-efficients must be even.
-DECLARE_ALIGNED(16, static const int16_t, gauss_filter[8]) = { 0,  8, 30, 52,
+DECLARE_ALIGNED(16, static const int16_t, gauss_filter[9]) = { 0,  8, 30, 52,
                                                                30, 8, 0,  0 };
 static AOM_INLINE void gaussian_blur(const int bit_depth,
                                      const YV12_BUFFER_CONFIG *source,
