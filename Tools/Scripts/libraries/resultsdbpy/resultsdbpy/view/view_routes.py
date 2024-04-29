WebKit/WebKit

commit 532c42da7266c3d93a56ddf6a9c051996be83dd2

[resultsdbpy] Add dashboard on landing page https://bugs.webkit.org/show_bug.cgi?id=272034 rdar://125787240
Reviewed by Elliott Williams.

Add a configurable dashboard to the results.webkit.org landing page. This dashboard
should surface current failures and attempt to pinpoint recent regression points.

* Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/css/search.css:
(.table>thead>tr>th): Disable padding in tables.
(.table>tbody>tr>td): Ditto.
* Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/commit.js:
(_CommitBank.prototype.forBranch): Return a CommitBank object for a specific branch.
(_CommitBank.prototype.latest): Only re-define branch arguments if object is global.
* Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/common.js:
(intersection): Return a new list which is the intersection of two lists.
(mergeQueries): Combine two queries together.
* Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/dashboard.js: Added.
(Dashboard.setWillFilterExpected): Set if all dashboards will display actual or expected results.
(Dashboard.refreshCommits): Force all dashboards to re-render without reloading data.
(Dashboard.prototype.constructor): Construct a single dashboard panel.
(Dashboard.prototype.documentParams): Return arguments for the current document, with data not valid
for suite endpoints stripped.
(Dashboard.prototype.canEnable): Check if this dashboard conforms to the current query. If it doesn't,
we need to hide this dashboard.
(Dashboard.prototype.setEnabled): Set if this dashboard should be displayed or hidden, fetching suite
data for this dashboard if it should be displayed.
(Dashboard.prototype.reload): Fetch suite endpoint to populate dashboard data and re-draw dashboard
with fetched data.
(Dashboard.prototype.failureForBuild): Determine the result ID for a provided build.
(Dashboard.prototype.tileLineForBuild): Return an object to be displayed in a dashboard tile given
a build.

Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/view_routes.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019, 2020 Apple Inc. All rights reserved.
+# Copyright (C) 2019-2024 Apple Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -48,6 +48,7 @@ class ViewRoutes(AuthedBlueprint):
         tests_limits=None,
         suites_limits=None,
         commits_limits=None,
+        dashboard_queries=None,
     ):
         super(ViewRoutes, self).__init__('view', import_name, url_prefix=None, auth_decorator=auth_decorator)
         self._cache = {}
@@ -63,6 +64,7 @@ class ViewRoutes(AuthedBlueprint):
         self.tests_limits = tests_limits or dict(max=50000, default=5000)
         self.suites_limits = suites_limits or dict(max=10000, default=1000)
         self.commits_limits = commits_limits or dict(max=10000, default=1000)
+        self.dashboard_queries = dashboard_queries or {}
 
         # Protecting js and css with auth doesn't make sense
         self.add_url_rule('/library/<path:path>', 'library', self.library, authed=False, methods=('GET',))
@@ -183,5 +185,6 @@ class ViewRoutes(AuthedBlueprint):
                 tests_limits=json.dumps(self.tests_limits),
                 suites_limits=json.dumps(self.suites_limits),
                 commits_limits=json.dumps(self.commits_limits),
+                dashboard_queries=json.dumps(self.dashboard_queries),
             ), mimetype='application/javascript',
         )

Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/css/search.css
@@ -57,6 +57,12 @@
     -webkit-backdrop-filter: blur(5px);
     backdrop-filter: blur(5px);
 }
+.table>thead>tr>th {
+    padding: 0px;
+}
+.table>tbody>tr>td {
+    padding: 0px;
+}
 
 @media (prefers-color-scheme: dark) {
     .next-regress-bar button{

Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/commit.js
@@ -170,13 +170,24 @@ class Commit {
 };
 
 const COOKIE_NAME = 'commitRepresentation';
+let _banks = {};
 
 class _CommitBank {
+    forBranch(branch) {
+        let key = branch ? branch : 'main';
+        if (!_banks[key]) {
+            _banks[key] = new _CommitBank();
+            _banks[key]._branches = new Set([branch]);
+            _banks[key]._branchOverride = true;
+        }
+        return _banks[key];
+    }
     constructor() {
         this.commits = [];
 
         const params = queryToParams(document.URL.split('?')[1]);
 
+        this._branchOverride = false;
         this._branches = new Set(params.branch);
         this._repositories = new Set(params.repository_id);
         this._beginUuid = null;
@@ -223,7 +234,8 @@ class _CommitBank {
         document.cookie = `${COOKIE_NAME}=${JSON.stringify(this._representationCache)}`;
     }
     latest(params) {
-        this._branches = new Set(params.branch);
+        if (!this._branchOverride)
+            this._branches = new Set(params.branch);
         this._repositories = new Set(params.repository_id);
         this._beginUuid = null;
         this._endUuid = null;

Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/common.js
@@ -28,6 +28,10 @@ function ErrorDisplay(json) {
     </div>`;
 }
 
+function intersection(listA, listB) {
+    return listA.filter(value => listB.includes(value));
+}
+
 function queryToParams(query) {
     if (!query)
         return {};
@@ -59,6 +63,17 @@ function paramsToQuery(json) {
                 return;
             result += (result ? '&' : '') + encodeURIComponent(parameter) + '=' + encodeURIComponent(value);
         });
+    }
+    return result;
+}
+
+function mergeQueries(queryA, queryB) {
+    let result = {...queryA};
+    for (const key in queryB) {
+        if (!Object.hasOwn(result, key))
+            result[key] = queryB[key];
+        else
+            result[key] = intersection(result[key], queryB[key]);
     }
     return result;
 }
@@ -218,4 +233,4 @@ function elapsedTime(startTimestamp, endTimestamp)
     return result;
 }
 
-export {deepCompare, ErrorDisplay, queryToParams, paramsToQuery, QueryModifier, escapeHTML, escapeEndpoint, linkify, percentage, elapsedTime};
+export {deepCompare, ErrorDisplay, intersection, queryToParams, paramsToQuery, mergeQueries, QueryModifier, escapeHTML, escapeEndpoint, linkify, percentage, elapsedTime};

Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/expectations.js
@@ -35,7 +35,7 @@ class Expectations
     }
 
     static stringToStateId(string) {
-        return Expectations.stateToIdMap[string];
+        return string in Expectations.stateToIdMap ? Expectations.stateToIdMap[string] : string;
     }
     static typeForId(string) {
         const id = Expectations.stringToStateId(string);

Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/static/js/timeline.js
@@ -25,6 +25,7 @@ import {ArchiveRouter} from '/assets/js/archiveRouter.js';
 import {CommitBank} from '/assets/js/commit.js';
 import {Configuration} from '/assets/js/configuration.js';
 import {deepCompare, ErrorDisplay, escapeHTML, paramsToQuery, queryToParams, linkify, escapeEndpoint} from '/assets/js/common.js';
+import {Dashboard} from '/assets/js/dashboard.js';
 import {Expectations} from '/assets/js/expectations.js';
 import {InvestigateDrawer} from '/assets/js/investigate.js';
 import {TypeForSuite} from '/assets/js/suites.js';
@@ -1341,6 +1342,7 @@ function Legend(callback=null, plural=false, defaultWillFilterExpected=false, fl
                     InvestigateDrawer.dispatch();
                     InvestigateDrawer.select(InvestigateDrawer.selected);
                     callback(willFilterExpected);
+                    Dashboard.setWillFilterExpected(willFilterExpected);
                 };
             },
         });

Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/templates/constants.js
@@ -28,5 +28,6 @@ const DEFAULT_ARCHITECTURE = {{ default_architecture }};
 const TESTS_LIMITS = JSON.parse('{{ tests_limits|safe }}');
 const SUITES_LIMITS = JSON.parse('{{ suites_limits|safe }}');
 const COMMITS_LIMITS = JSON.parse('{{ commits_limits|safe }}');
+const DASHBOARD_QUERY = JSON.parse('{{ dashboard_queries|safe }}');
 
-export {XCODE_CLOUD_SUITES, DEFAULT_ARCHITECTURE, TESTS_LIMITS, SUITES_LIMITS, COMMITS_LIMITS}
+export {XCODE_CLOUD_SUITES, DEFAULT_ARCHITECTURE, TESTS_LIMITS, SUITES_LIMITS, COMMITS_LIMITS, DASHBOARD_QUERY}

commit fb99585b31371166f708c62167a08fbbe0c762bd

[results.webkit.org] Handle empty dashboard query https://bugs.webkit.org/show_bug.cgi?id=273436 rdar://127255746
Unreviewed infrastructure fix.

Fix a regression from 276963@main.

* Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/view_routes.py:
(ViewRoutes): Define dashboard_queries as [] if undefined.

Canonical link: https://commits.webkit.org/278126@main

Tools/Scripts/libraries/resultsdbpy/resultsdbpy/view/view_routes.py
@@ -64,7 +64,7 @@ class ViewRoutes(AuthedBlueprint):
         self.tests_limits = tests_limits or dict(max=50000, default=5000)
         self.suites_limits = suites_limits or dict(max=10000, default=1000)
         self.commits_limits = commits_limits or dict(max=10000, default=1000)
-        self.dashboard_queries = dashboard_queries or {}
+        self.dashboard_queries = dashboard_queries or []
 
         # Protecting js and css with auth doesn't make sense
         self.add_url_rule('/library/<path:path>', 'library', self.library, authed=False, methods=('GET',))