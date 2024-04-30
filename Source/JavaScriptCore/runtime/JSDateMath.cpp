511e5fff44f9949be51d903a8d1f9c851d2864e0
Chris Dumez <cdumez@apple.com>
2024-04-29 22:06:27 -0700

    Reduce use of `const char*` in <wtf/DateMath.h> https://bugs.webkit.org/show_bug.cgi?id=273409
    Reviewed by Darin Adler.
    
    Reduce use of `const char*` in <wtf/DateMath.h> by leveraging std::span.
    
    * Source/JavaScriptCore/runtime/JSDateMath.cpp:
    (JSC::DateCache::parseDate):
    * Source/WTF/wtf/DateMath.cpp:
    (WTF::skipSpacesAndComments):
    (WTF::findMonth):
    (WTF::parseInt):
    (WTF::parseLong):
    (WTF::parseDate):
    (WTF::parseDateFromNullTerminatedCharacters): Deleted.
    * Source/WTF/wtf/DateMath.h:
    * Source/WebCore/platform/network/HTTPParsers.cpp:
    (WebCore::parseHTTPDate):
    * Source/WebCore/platform/network/curl/CookieUtil.cpp:
    (WebCore::CookieUtil::parseExpiresMS):
    (WebCore::CookieUtil::parseCookieAttributes):
    
    Canonical link: https://commits.webkit.org/278153@main

--- Source/JavaScriptCore/runtime/JSDateMath.cpp
+++ Source/JavaScriptCore/runtime/JSDateMath.cpp
@@ -451,11 +451,11 @@ double DateCache::parseDate(JSGlobalObject* globalObject, VM& vm, const String& 
         return std::numeric_limits<double>::quiet_NaN();
     }
 
-    auto parseDateImpl = [this] (const char* dateString) {
+    auto parseDateImpl = [this] (const CString& dateString) {
         bool isLocalTime;
-        double value = WTF::parseES5DateFromNullTerminatedCharacters(dateString, isLocalTime);
+        double value = WTF::parseES5DateFromNullTerminatedCharacters(dateString.data(), isLocalTime);
         if (std::isnan(value))
-            value = WTF::parseDateFromNullTerminatedCharacters(dateString, isLocalTime);
+            value = WTF::parseDate(dateString.span(), isLocalTime);
 
         if (isLocalTime && std::isfinite(value))
             value -= localTimeOffset(static_cast<int64_t>(value), WTF::LocalTime).offset;
@@ -463,8 +463,7 @@ double DateCache::parseDate(JSGlobalObject* globalObject, VM& vm, const String& 
         return value;
     };
 
-    auto dateUTF8 = expectedString.value();
-    double value = parseDateImpl(dateUTF8.data());
+    double value = parseDateImpl(expectedString.value());
     m_cachedDateString = date;
     m_cachedDateStringValue = value;
     return value;
