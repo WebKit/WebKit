9f7fcac9257ebed1331734bccf454638ca63a732
Philippe Normand <philn@igalia.com>
2024-05-03 01:27:19 -0700

    [GStreamer][WebAudio] Misc improvements in playback pipeline https://bugs.webkit.org/show_bug.cgi?id=273555
    Reviewed by Xabier Rodriguez-Calvar.
    
    A small queue is needed before the sink, by design. In the webaudiosrc element timestamping was
    improved, silent buffers are now flagged as droppable and the render task starts only when going to
    PLAYING state.
    
    * Source/WebCore/platform/audio/gstreamer/AudioDestinationGStreamer.cpp:
    (WebCore::AudioDestination::create):
    (WebCore::AudioDestinationGStreamer::AudioDestinationGStreamer):
    * Source/WebCore/platform/audio/gstreamer/WebKitWebAudioSourceGStreamer.cpp:
    (webKitWebAudioSrcConstructed):
    (webKitWebAudioSrcRenderAndPushFrames):
    (webKitWebAudioSrcChangeState):
    
    Canonical link: https://commits.webkit.org/278306@main

--- Source/WebCore/platform/audio/gstreamer/AudioDestinationGStreamer.cpp
+++ Source/WebCore/platform/audio/gstreamer/AudioDestinationGStreamer.cpp
@@ -23,13 +23,10 @@
 
 #include "AudioDestinationGStreamer.h"
 
-#include "AudioChannel.h"
 #include "AudioSourceProvider.h"
 #include "AudioUtilities.h"
 #include "GStreamerCommon.h"
 #include "GStreamerQuirks.h"
-#include "Logging.h"
-#include "WebKitAudioSinkGStreamer.h"
 #include "WebKitWebAudioSourceGStreamer.h"
 #include <gst/audio/gstaudiobasesink.h>
 #include <gst/gst.h>
@@ -156,12 +153,17 @@ AudioDestinationGStreamer::AudioDestinationGStreamer(AudioIOCallback& callback, 
 
     GstElement* audioConvert = makeGStreamerElement("audioconvert", nullptr);
     GstElement* audioResample = makeGStreamerElement("audioresample", nullptr);
-    gst_bin_add_many(GST_BIN_CAST(m_pipeline.get()), m_src.get(), audioConvert, audioResample, audioSink.get(), nullptr);
 
-    // Link src pads from webkitAudioSrc to audioConvert ! audioResample ! autoaudiosink.
+    auto queue = gst_element_factory_make("queue", nullptr);
+    g_object_set(queue, "max-size-buffers", 2, "max-size-bytes", 0, "max-size-time", 0, nullptr);
+
+    gst_bin_add_many(GST_BIN_CAST(m_pipeline.get()), m_src.get(), audioConvert, audioResample, queue, audioSink.get(), nullptr);
+
+    // Link src pads from webkitAudioSrc to audioConvert ! audioResample ! queue ! autoaudiosink.
     gst_element_link_pads_full(m_src.get(), "src", audioConvert, "sink", GST_PAD_LINK_CHECK_NOTHING);
     gst_element_link_pads_full(audioConvert, "src", audioResample, "sink", GST_PAD_LINK_CHECK_NOTHING);
-    gst_element_link_pads_full(audioResample, "src", audioSink.get(), "sink", GST_PAD_LINK_CHECK_NOTHING);
+    gst_element_link_pads_full(audioResample, "src", queue, "sink", GST_PAD_LINK_CHECK_NOTHING);
+    gst_element_link_pads_full(queue, "src", audioSink.get(), "sink", GST_PAD_LINK_CHECK_NOTHING);
 }
 
 AudioDestinationGStreamer::~AudioDestinationGStreamer()
