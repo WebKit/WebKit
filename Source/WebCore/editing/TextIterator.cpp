2eee050cf9ac6cface6d3cab4355a4ab42998dc5
Ahmad Saleem <ahmad.saleem792@gmail.com>
2024-05-08 13:24:15 -0700

    innerText getter fails tests for `<p>` without margin and `<h1>`-`<h6>`
    https://bugs.webkit.org/show_bug.cgi?id=176776
    rdar://problem/98459918
    
    Reviewed by Ryosuke Niwa.
    
    Inspired by(+ more): https://chromium.googlesource.com/chromium/src.git/+/8ff781cd5c1aabca068247de9a3f143645e80422
    
    TextIterator currently decides to add two consecutive newlines
    after a P element only if it has a large margin, while the spec
    of innerText doesn't have this margin check at all. Extend it to
    heading (H1, H2...) as well.
    
    This patch removes the margin and extra line check to make TextIterator
    more spec-compliant [1]:
    
    [1] https://html.spec.whatwg.org/multipage/dom.html#the-innertext-idl-attribute
    
    * Source/WebCore/editing/TextIterator.cpp:
    (WebCore::shouldEmitExtraNewlineForNode):
    * LayoutTests/imported/w3c/web-platform-tests/innerText/getter-expected.txt: Rebaselined
    * LayoutTests/imported/w3c/web-platform-tests/html/dom/elements/the-innertext-and-outertext-properties/getter-expected.txt: Ditto
    * LayoutTests/editing/execCommand/format-block-table-expected.txt: Rebaselined
    * LayoutTests/editing/execCommand/format-block-multiple-paragraphs-in-pre-expected.txt: Ditto
    * LayoutTests/imported/w3c/web-platform-tests/editing/other/formatblock-preserving-selection.tentative_styleWithCSS=false&block=h1-expected.txt: Ditto
    * LayoutTests/imported/w3c/web-platform-tests/editing/other/formatblock-preserving-selection.tentative_styleWithCSS=true&block=h1-expected.txt: Ditto
    
    NOTE: Unfortunately, too many test rebaseline to fit in
    ChangeLog, which are mostly 'space' addition or deletion.
    
    Canonical link: https://commits.webkit.org/278522@main

--- Source/WebCore/editing/TextIterator.cpp
+++ Source/WebCore/editing/TextIterator.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2004-2023 Apple Inc. All rights reserved.
+ * Copyright (C) 2004-2024 Apple Inc. All rights reserved.
  * Copyright (C) 2015-2018 Google Inc. All rights reserved.
  * Copyright (C) 2005 Alexey Proskuryakov.
  *
@@ -918,23 +918,14 @@ static bool shouldEmitNewlineBeforeNode(Node& node)
 
 static bool shouldEmitExtraNewlineForNode(Node& node)
 {
-    // When there is a significant collapsed bottom margin, emit an extra
-    // newline for a more realistic result. We end up getting the right
-    // result even without margin collapsing. For example: <div><p>text</p></div>
-    // will work right even if both the <div> and the <p> have bottom margins.
+    // https://html.spec.whatwg.org/multipage/dom.html#the-innertext-idl-attribute
+    // Append two required linebreaks after a <p> element.
 
     CheckedPtr renderBox = dynamicDowncast<RenderBox>(node.renderer());
     if (!renderBox || !renderBox->height())
         return false;
 
-    // NOTE: We only do this for a select set of nodes, and WinIE appears not to do this at all.
-    RefPtr element = dynamicDowncast<HTMLElement>(node);
-    if (!element || (!hasHeaderTag(*element) && !is<HTMLParagraphElement>(*element)))
-        return false;
-
-    auto bottomMargin = renderBox->collapsedMarginAfter();
-    auto fontSize = renderBox->style().fontDescription().computedSize();
-    return bottomMargin * 2 >= fontSize;
+    return node.hasTagName(pTag);
 }
 
 static int collapsedSpaceLength(RenderText& renderer, int textEnd)
