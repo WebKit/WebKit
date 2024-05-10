4b588046ceb574e6c5399952d275e4d2bcce0fd3
Dan Glastonbury <djg@apple.com>
2024-05-09 22:31:29 -0700

    [WebXR] Split WebXROpaqueFramebuffer implementation https://bugs.webkit.org/show_bug.cgi?id=273987 rdar://127855785
    Reviewed by Mike Wyrzykowski.
    
    Cocoa and non-Cocoa implementations of WebXROpaqueFramebuffer have diverged
    greatly. Instead of using #ifdef/#endif to work around the differences, split
    the implementation into Cocoa and non-Cocoa specific implementation files.
    
    * Source/WebCore/Modules/webxr/WebXROpaqueFramebuffer.cpp:
    (WebCore::WebXROpaqueFramebuffer::~WebXROpaqueFramebuffer):
    (WebCore::WebXROpaqueFramebuffer::startFrame):
    (WebCore::WebXROpaqueFramebuffer::endFrame):
    (WebCore::WebXROpaqueFramebuffer::blitShared):
    (WebCore::WebXROpaqueFramebuffer::blitSharedToLayered):
    (WebCore::WebXROpaqueFramebuffer::supportsDynamicViewportScaling const):
    (WebCore::WebXROpaqueFramebuffer::drawViewport const):
    (WebCore::WebXROpaqueFramebuffer::calculateViewportShared):
    (WebCore::ensure): Deleted.
    (WebCore::createAndBindCompositorBuffer): Deleted.
    (WebCore::makeExternalImageSource): Deleted.
    (WebCore::convertViewportToPhysicalCoordinates): Deleted.
    (WebCore::displayLayout): Deleted.
    (WebCore::WebXROpaqueFramebuffer::setupFramebuffer): Deleted.
    * Source/WebCore/Modules/webxr/WebXROpaqueFramebufferCocoa.cpp: Copied from Source/WebCore/Modules/webxr/WebXROpaqueFramebuffer.cpp.
    (WebCore::ensure):
    (WebCore::createAndBindCompositorBuffer):
    (WebCore::makeExternalImageSource):
    (WebCore::WebXROpaqueFramebuffer::create):
    (WebCore::WebXROpaqueFramebuffer::WebXROpaqueFramebuffer):
    (WebCore::WebXROpaqueFramebuffer::~WebXROpaqueFramebuffer):
    (WebCore::WebXROpaqueFramebuffer::startFrame):
    (WebCore::WebXROpaqueFramebuffer::endFrame):
    (WebCore::WebXROpaqueFramebuffer::usesLayeredMode const):
    (WebCore::WebXROpaqueFramebuffer::resolveMSAAFramebuffer):
    (WebCore::WebXROpaqueFramebuffer::blitShared):
    (WebCore::WebXROpaqueFramebuffer::blitSharedToLayered):
    (WebCore::WebXROpaqueFramebuffer::supportsDynamicViewportScaling const):
    (WebCore::WebXROpaqueFramebuffer::displayFramebufferSize const):
    (WebCore::WebXROpaqueFramebuffer::drawFramebufferSize const):
    (WebCore::convertViewportToPhysicalCoordinates):
    (WebCore::WebXROpaqueFramebuffer::drawViewport const):
    (WebCore::displayLayout):
    (WebCore::toIntSize):
    (WebCore::WebXROpaqueFramebuffer::setupFramebuffer):
    (WebCore::WebXROpaqueFramebuffer::allocateRenderbufferStorage):
    (WebCore::WebXROpaqueFramebuffer::allocateAttachments):
    (WebCore::WebXROpaqueFramebuffer::bindAttachments):
    (WebCore::WebXROpaqueFramebuffer::calculateViewportShared):
    (WebCore::WebXRExternalRenderbuffer::destroyImage):
    (WebCore::WebXRExternalRenderbuffer::release):
    (WebCore::WebXRExternalRenderbuffer::leakObject):
    * Source/WebCore/SourcesCocoa.txt:
    * Source/WebCore/WebCore.xcodeproj/project.pbxproj:
    
    Canonical link: https://commits.webkit.org/278600@main

--- Source/WebCore/Modules/webxr/WebXROpaqueFramebufferCocoa.cpp
+++ Source/WebCore/Modules/webxr/WebXROpaqueFramebufferCocoa.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2020 Igalia S.L. All rights reserved.
- * Copyright (C) 2021-2024 Apple, Inc. All rights reserved.
+ * Copyright (C) 2024 Apple, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,7 +26,7 @@
 #include "config.h"
 #include "WebXROpaqueFramebuffer.h"
 
-#if ENABLE(WEBXR) && !PLATFORM(COCOA)
+#if ENABLE(WEBXR) && PLATFORM(COCOA)
 
 #include "IntSize.h"
 #include "WebGLFramebuffer.h"
@@ -38,9 +37,47 @@
 #include <wtf/Scope.h>
 #include <wtf/SystemTracing.h>
 
+#if PLATFORM(COCOA)
+#include "GraphicsContextGLCocoa.h"
+#endif
+
 namespace WebCore {
 
 using GL = GraphicsContextGL;
+
+static void ensure(GL& gl, GCGLOwnedFramebuffer& framebuffer)
+{
+    if (!framebuffer) {
+        auto object = gl.createFramebuffer();
+        if (!object)
+            return;
+        framebuffer.adopt(gl, object);
+    }
+}
+
+static void createAndBindCompositorBuffer(GL& gl, WebXRExternalRenderbuffer& buffer, GCGLenum internalFormat, GL::ExternalImageSource source, GCGLint layer)
+{
+    if (!buffer.renderBufferObject) {
+        auto object = gl.createRenderbuffer();
+        if (!object)
+            return;
+        buffer.renderBufferObject.adopt(gl, object);
+    }
+    gl.bindRenderbuffer(GL::RENDERBUFFER, buffer.renderBufferObject);
+    auto image = gl.createExternalImage(WTFMove(source), internalFormat, layer);
+    if (!image)
+        return;
+    gl.bindExternalImage(GL::RENDERBUFFER, image);
+    buffer.image.adopt(gl, image);
+}
+
+static GL::ExternalImageSource makeExternalImageSource(std::tuple<WTF::MachSendRight, bool>&& imageSource)
+{
+    auto [imageHandle, isSharedTexture] = WTFMove(imageSource);
+    if (isSharedTexture)
+        return GraphicsContextGLExternalImageSourceMTLSharedTextureHandle { WTFMove(imageHandle) };
+    return GraphicsContextGLExternalImageSourceIOSurfaceHandle { WTFMove(imageHandle) };
+}
 
 std::unique_ptr<WebXROpaqueFramebuffer> WebXROpaqueFramebuffer::create(PlatformXR::LayerHandle handle, WebGLRenderingContextBase& context, Attributes&& attributes, IntSize framebufferSize)
 {
@@ -62,6 +99,8 @@ WebXROpaqueFramebuffer::WebXROpaqueFramebuffer(PlatformXR::LayerHandle handle, R
 WebXROpaqueFramebuffer::~WebXROpaqueFramebuffer()
 {
     if (RefPtr gl = m_context.graphicsContextGL()) {
+        for (auto& layer : m_displayAttachments)
+            layer.release(*gl);
         m_drawAttachments.release(*gl);
         m_resolveAttachments.release(*gl);
         m_resolvedFBO.release(*gl);
@@ -69,6 +108,8 @@ WebXROpaqueFramebuffer::~WebXROpaqueFramebuffer()
     } else {
         // The GraphicsContextGL is gone, so disarm the GCGLOwned objects so
         // their destructors don't assert.
+        for (auto& layer : m_displayAttachments)
+            layer.leakObject();
         m_drawAttachments.leakObject();
         m_resolveAttachments.leakObject();
         m_displayFBO.leakObject();
@@ -100,8 +141,28 @@ void WebXROpaqueFramebuffer::startFrame(const PlatformXR::FrameData::LayerData& 
     // FIXME: Actually do the clearing (not using invalidateFramebuffer). This will have to be done after we've attached
     // the textures/renderbuffers.
 
-    m_framebufferSize = data.framebufferSize;
-    m_colorTexture = data.opaqueTexture;
+    if (data.layerSetup) {
+        // The drawing target can change size at any point during the session. If this happens, we need
+        // to recreate the framebuffer.
+        if (!setupFramebuffer(*gl, *data.layerSetup))
+            return;
+
+        m_completionSyncEvent = MachSendRight(data.layerSetup->completionSyncEvent);
+    }
+
+    int layerCount = (m_displayLayout == PlatformXR::Layout::Layered) ? 2 : 1;
+    for (int layer = 0; layer < layerCount; ++layer) {
+        auto colorTextureSource = makeExternalImageSource(std::tuple<MachSendRight, bool> { data.colorTexture });
+        createAndBindCompositorBuffer(*gl, m_displayAttachments[layer].colorBuffer, GL::BGRA_EXT, WTFMove(colorTextureSource), layer);
+        ASSERT(m_displayAttachments[layer].colorBuffer.image);
+        if (!m_displayAttachments[layer].colorBuffer.image)
+            return;
+
+        auto depthStencilBufferSource = makeExternalImageSource(std::tuple<MachSendRight, bool> { data.depthStencilBuffer });
+        createAndBindCompositorBuffer(*gl, m_displayAttachments[layer].depthStencilBuffer, GL::DEPTH24_STENCIL8, WTFMove(depthStencilBufferSource), layer);
+    }
+
+    m_renderingFrameIndex = data.renderingFrameIndex;
 
     // WebXR must always clear for the rAF of the session. Currently we assume content does not do redundant initial clear,
     // as the spec says the buffer always starts cleared.
@@ -143,11 +204,21 @@ void WebXROpaqueFramebuffer::endFrame()
         break;
     }
 
-    // FIXME: We have to call finish rather than flush because we only want to disconnect
-    // the IOSurface and signal the DeviceProxy when we know the content has been rendered.
-    // It might be possible to set this up so the completion of the rendering triggers
-    // the endFrame call.
-    gl->finish();
+    if (m_completionSyncEvent) {
+        auto completionSync = gl->createExternalSync(std::tuple(m_completionSyncEvent, m_renderingFrameIndex));
+        ASSERT(completionSync);
+        if (completionSync) {
+            gl->flush();
+            gl->deleteExternalSync(completionSync);
+        }
+    } else
+        gl->finish();
+
+    int layerCount = (m_displayLayout == PlatformXR::Layout::Layered) ? 2 : 1;
+    for (int layer = 0; layer < layerCount; ++layer) {
+        m_displayAttachments[layer].colorBuffer.destroyImage(*gl);
+        m_displayAttachments[layer].depthStencilBuffer.destroyImage(*gl);
+    }
 }
 
 bool WebXROpaqueFramebuffer::usesLayeredMode() const
@@ -176,20 +247,66 @@ void WebXROpaqueFramebuffer::resolveMSAAFramebuffer(GraphicsContextGL& gl)
 
 void WebXROpaqueFramebuffer::blitShared(GraphicsContextGL& gl)
 {
-    gl.bindFramebuffer(GL::FRAMEBUFFER, m_drawFramebuffer->object());
-    gl.framebufferTexture2D(GL::FRAMEBUFFER, GL::COLOR_ATTACHMENT0, GL::TEXTURE_2D, m_colorTexture, 0);
+    ASSERT(!m_resolvedFBO, "blitShared should not require intermediate resolve buffers");
+
+    ensure(gl, m_displayFBO);
+    gl.bindFramebuffer(GL::FRAMEBUFFER, m_displayFBO);
+    gl.framebufferRenderbuffer(GL::FRAMEBUFFER, GL::COLOR_ATTACHMENT0, GL::RENDERBUFFER, m_displayAttachments[0].colorBuffer.renderBufferObject);
     ASSERT(gl.checkFramebufferStatus(GL::FRAMEBUFFER) == GL::FRAMEBUFFER_COMPLETE);
+    resolveMSAAFramebuffer(gl);
 }
 
 void WebXROpaqueFramebuffer::blitSharedToLayered(GraphicsContextGL& gl)
 {
-    UNUSED_PARAM(gl);
-    ASSERT_NOT_REACHED();
+    ensure(gl, m_displayFBO);
+
+    PlatformGLObject readFBO = (m_resolvedFBO && m_attributes.antialias) ? m_resolvedFBO : m_drawFramebuffer->object();
+    ASSERT(readFBO, "readFBO shouldn't be the default framebuffer");
+    PlatformGLObject drawFBO = m_displayFBO;
+    ASSERT(drawFBO, "drawFBO shouldn't be the default framebuffer");
+
+    IntSize phyiscalSize = m_leftPhysicalSize;
+    IntRect viewport = m_leftViewport;
+
+    if (m_resolvedFBO && m_attributes.antialias)
+        resolveMSAAFramebuffer(gl);
+
+    for (int layer = 0; layer < 2; ++layer) {
+        gl.bindFramebuffer(GL::READ_FRAMEBUFFER, readFBO);
+        gl.bindFramebuffer(GL::DRAW_FRAMEBUFFER, drawFBO);
+
+        GCGLbitfield buffers = GL::COLOR_BUFFER_BIT;
+        gl.framebufferRenderbuffer(GL::DRAW_FRAMEBUFFER, GL::COLOR_ATTACHMENT0, GL::RENDERBUFFER, m_displayAttachments[layer].colorBuffer.renderBufferObject);
+
+        if (m_displayAttachments[layer].depthStencilBuffer.image) {
+            buffers |= GL::DEPTH_BUFFER_BIT;
+            gl.framebufferRenderbuffer(GL::DRAW_FRAMEBUFFER, GL::DEPTH_STENCIL_ATTACHMENT, GL::RENDERBUFFER, m_displayAttachments[layer].depthStencilBuffer.renderBufferObject);
+        }
+        ASSERT(gl.checkFramebufferStatus(GL::DRAW_FRAMEBUFFER) == GL::FRAMEBUFFER_COMPLETE);
+
+        int horizontalOffset = viewport.x() / static_cast<double>(m_screenSize.width()) * phyiscalSize.width();
+        int verticalOffset = viewport.y() / static_cast<double>(m_screenSize.height()) * phyiscalSize.height();
+        int adjustedWidth = viewport.width() / static_cast<double>(m_screenSize.width()) * phyiscalSize.width();
+        int adjustedHeight = viewport.height() / static_cast<double>(m_screenSize.height()) * phyiscalSize.height();
+
+        gl.blitFramebuffer(horizontalOffset, verticalOffset, horizontalOffset + adjustedWidth, verticalOffset + adjustedHeight, 0, 0, adjustedWidth, adjustedHeight, buffers, GL::NEAREST);
+
+        // FIXME: https://bugs.webkit.org/show_bug.cgi?id=272104 - [WebXR] Compositor expects reverse-Z values
+        gl.clearDepth(FLT_MIN);
+        gl.clear(GL::DEPTH_BUFFER_BIT | GL::STENCIL_BUFFER_BIT);
+
+        phyiscalSize = m_rightPhysicalSize;
+        viewport = m_rightViewport;
+    }
 }
 
 bool WebXROpaqueFramebuffer::supportsDynamicViewportScaling() const
 {
+#if PLATFORM(VISION)
+    return false;
+#else
     return true;
+#endif
 }
 
 IntSize WebXROpaqueFramebuffer::displayFramebufferSize() const
@@ -207,15 +324,90 @@ IntSize WebXROpaqueFramebuffer::drawFramebufferSize() const
     }
 }
 
+static IntRect convertViewportToPhysicalCoordinates(const IntRect& viewport, const IntSize& screenSize, const IntSize& phyiscalSize)
+{
+    return IntRect(
+        viewport.x() / static_cast<double>(screenSize.width()) * phyiscalSize.width(),
+        viewport.y() / static_cast<double>(screenSize.height()) * phyiscalSize.height(),
+        viewport.width() / static_cast<double>(screenSize.width()) * phyiscalSize.width(),
+        viewport.height() / static_cast<double>(screenSize.height()) * phyiscalSize.height());
+}
+
 IntRect WebXROpaqueFramebuffer::drawViewport(PlatformXR::Eye eye) const
 {
-    UNUSED_PARAM(eye);
-    return IntRect(IntPoint::zero(), drawFramebufferSize());
+    switch (eye) {
+    case PlatformXR::Eye::None:
+        return IntRect(IntPoint::zero(), drawFramebufferSize());
+    case PlatformXR::Eye::Left:
+        return m_usingFoveation ? convertViewportToPhysicalCoordinates(m_leftViewport, m_screenSize, m_leftPhysicalSize) : m_leftViewport;
+    case PlatformXR::Eye::Right:
+        return m_usingFoveation ? convertViewportToPhysicalCoordinates(m_rightViewport, m_screenSize, m_rightPhysicalSize) : m_rightViewport;
+    }
+}
+
+static PlatformXR::Layout displayLayout(const PlatformXR::FrameData::LayerSetupData& data)
+{
+    return data.physicalSize[1][0] > 0 ? PlatformXR::Layout::Layered : PlatformXR::Layout::Shared;
 }
 
 static IntSize toIntSize(const auto& size)
 {
     return IntSize(size[0], size[1]);
+}
+
+bool WebXROpaqueFramebuffer::setupFramebuffer(GraphicsContextGL& gl, const PlatformXR::FrameData::LayerSetupData& data)
+{
+    auto framebufferSize = IntSize(data.framebufferSize[0], data.framebufferSize[1]);
+    bool framebufferResize = m_framebufferSize != framebufferSize || m_displayLayout != displayLayout(data);
+    bool foveationChange = !data.horizontalSamples[0].empty() && !data.verticalSamples.empty() && !data.horizontalSamples[1].empty();
+    m_usingFoveation = foveationChange;
+
+    m_framebufferSize = framebufferSize;
+    m_displayLayout = displayLayout(data);
+    m_leftPhysicalSize = toIntSize(data.physicalSize[0]);
+    m_rightPhysicalSize = toIntSize(data.physicalSize[1]);
+    m_screenSize = data.screenSize;
+
+    const bool layeredLayout = m_displayLayout == PlatformXR::Layout::Layered;
+    const bool needsIntermediateResolve = m_attributes.antialias && layeredLayout;
+
+    // Set up recommended samples for WebXR.
+    auto sampleCount = m_attributes.antialias ? std::min(4, m_context.maxSamples()) : 0;
+
+    IntSize size = drawFramebufferSize();
+
+    // Drawing target
+    if (framebufferResize) {
+        // FIXME: We always allocate a new drawing target
+        allocateAttachments(gl, m_drawAttachments, sampleCount, size);
+
+        gl.bindFramebuffer(GL::FRAMEBUFFER, m_drawFramebuffer->object());
+        bindAttachments(gl, m_drawAttachments);
+        ASSERT(gl.checkFramebufferStatus(GL::FRAMEBUFFER) == GL::FRAMEBUFFER_COMPLETE);
+    }
+
+    // Calculate viewports of each eye
+    if (foveationChange) {
+        if (!gl.addFoveation(toIntSize(data.physicalSize[0]), toIntSize(data.physicalSize[1]), data.screenSize, data.horizontalSamples[0], data.verticalSamples, data.horizontalSamples[1]))
+            return false;
+        gl.enableFoveation(m_drawAttachments.colorBuffer);
+    }
+
+    m_leftViewport = calculateViewportShared(PlatformXR::Eye::Left, foveationChange, data.viewports[0], data.viewports[1]);
+    m_rightViewport = calculateViewportShared(PlatformXR::Eye::Right, foveationChange, data.viewports[0], data.viewports[1]);
+    // Intermediate resolve target
+    if ((!m_resolvedFBO || framebufferResize) && needsIntermediateResolve) {
+        allocateAttachments(gl, m_resolveAttachments, 0, size);
+
+        ensure(gl, m_resolvedFBO);
+        gl.bindFramebuffer(GL::FRAMEBUFFER, m_resolvedFBO);
+        bindAttachments(gl, m_resolveAttachments);
+        ASSERT(gl.checkFramebufferStatus(GL::FRAMEBUFFER) == GL::FRAMEBUFFER_COMPLETE);
+        if (gl.checkFramebufferStatus(GL::FRAMEBUFFER) != GL::FRAMEBUFFER_COMPLETE)
+            return false;
+    }
+
+    return gl.checkFramebufferStatus(GL::FRAMEBUFFER) == GL::FRAMEBUFFER_COMPLETE;
 }
 
 void WebXROpaqueFramebuffer::allocateRenderbufferStorage(GraphicsContextGL& gl, GCGLOwnedRenderbuffer& buffer, GCGLsizei samples, GCGLenum internalFormat, IntSize size)
@@ -245,6 +437,10 @@ void WebXROpaqueFramebuffer::bindAttachments(GraphicsContextGL& gl, WebXRAttachm
 
 IntRect WebXROpaqueFramebuffer::calculateViewportShared(PlatformXR::Eye eye, bool isFoveated, const IntRect& leftViewport, const IntRect& rightViewport)
 {
+#if !PLATFORM(VISION)
+    RELEASE_ASSERT(!isFoveated, "Foveated rendering is not supported");
+#endif
+
     switch (eye) {
     case PlatformXR::Eye::None:
         ASSERT_NOT_REACHED();
@@ -277,4 +473,4 @@ void WebXRExternalRenderbuffer::leakObject()
 
 } // namespace WebCore
 
-#endif // ENABLE(WEBXR) && !PLATFORM(COCOA)
+#endif // ENABLE(WEBXR) && PLATFORM(COCOA)
