283802946dcc7e82c929ea0b24c132dee97b0c58
Alexey Shvayka <ashvayka@apple.com>
2024-05-10 15:32:21 -0700

    [JSC] putDirectWithReify() should not cache putting `prototype` to a JSFunction https://bugs.webkit.org/show_bug.cgi?id=273580 <rdar://125602977>
    Reviewed by Yusuke Suzuki.
    
    Before this change, putDirectWithReify() used for direct op_put_by_id could still cache putting of
    `prototype` properties. Leveraging the cache, one could put a configurable `prototype` property onto
    a JSFunction with mayHaveNonReifiedPrototype() returning true. And because of configurability, it could
    have been turned into a GetterSetter and leaked via getDirect() in JSFunction::getOwnPropertySlot(),
    breaking the method's assumption that the `prototype` a JSFunction with thruthy mayHaveNonReifiedPrototype()
    must be a value.
    
    This change prevents put slot from being cached, and clears FunctionRareData like it's done in other
    methods that put `prototype` property.
    
    * JSTests/stress/regress-125602977.js: Added.
    * Source/JavaScriptCore/runtime/CommonSlowPaths.h:
    (JSC::CommonSlowPaths::putDirectWithReify):
    (JSC::CommonSlowPaths::putDirectAccessorWithReify):
    
    Canonical link: https://commits.webkit.org/272448.1012@safari-7618-branch

--- Source/JavaScriptCore/runtime/CommonSlowPaths.h
+++ Source/JavaScriptCore/runtime/CommonSlowPaths.h
@@ -180,7 +180,15 @@ static ALWAYS_INLINE void putDirectWithReify(VM& vm, JSGlobalObject* globalObjec
     auto scope = DECLARE_THROW_SCOPE(vm);
     bool isJSFunction = baseObject->inherits<JSFunction>();
     if (isJSFunction) {
-        jsCast<JSFunction*>(baseObject)->reifyLazyPropertyIfNeeded<>(vm, globalObject, propertyName);
+        JSFunction* jsFunction = jsCast<JSFunction*>(baseObject);
+
+        if (propertyName == vm.propertyNames->prototype) {
+            slot.disableCaching();
+            if (FunctionRareData* rareData = jsFunction->rareData())
+                rareData->clear("Store to prototype property of a function");
+        }
+
+        jsFunction->reifyLazyPropertyIfNeeded<>(vm, globalObject, propertyName);
         RETURN_IF_EXCEPTION(scope, void());
     }
 
@@ -201,16 +209,18 @@ static ALWAYS_INLINE void putDirectWithReify(VM& vm, JSGlobalObject* globalObjec
 
 static ALWAYS_INLINE void putDirectAccessorWithReify(VM& vm, JSGlobalObject* globalObject, JSObject* baseObject, PropertyName propertyName, GetterSetter* accessor, unsigned attribute)
 {
+    // baseObject is either JSFinalObject during object literal construction, or a userland JSFunction class
+    // constructor, both of which are guaranteed to be extensible and without non-configurable |propertyName|.
+    // Please also note that static "prototype" accessor in a `class` literal is a syntax error.
+
     auto scope = DECLARE_THROW_SCOPE(vm);
     bool isJSFunction = baseObject->inherits<JSFunction>();
     if (isJSFunction) {
+        ASSERT(propertyName != vm.propertyNames->prototype);
         jsCast<JSFunction*>(baseObject)->reifyLazyPropertyIfNeeded<>(vm, globalObject, propertyName);
         RETURN_IF_EXCEPTION(scope, void());
     }
 
-    // baseObject is either JSFinalObject during object literal construction, or a userland JSFunction class
-    // constructor, both of which are guaranteed to be extensible and without non-configurable |propertyName|.
-    // Please also note that static "prototype" accessor in a `class` literal is a syntax error.
     ASSERT(canPutDirectFast(vm, originalStructureBeforePut(baseObject), propertyName, isJSFunction));
     scope.release();
     baseObject->putDirectAccessor(globalObject, propertyName, accessor, attribute);
