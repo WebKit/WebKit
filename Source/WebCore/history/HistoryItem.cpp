WebKit/WebKit

commit b903f425a02b341d81be1131835fd495ae5e100c

Change `FrameTree::uniqueName()` to generate a unique name when called https://bugs.webkit.org/show_bug.cgi?id=272062 rdar://125808920
Reviewed by Alex Christensen.

Unique frame names currently use a counter to increment a unique frame index each time
`FrameTree::setSpecifiedName` is called. This will not work with site isolation. Instead, we should
change `FrameTree::uniqueName()` to create the unique frame name based on the frame’s position in the
frame tree to mimic previous behavior . Other than layout test output, there are two users of unique
frame names. `HistoryItem` relies on them for session state and to give a consistent identifier for
frame positions after a navigation. Web archives output them in their saved state. Both should continue
to work after this change.

Some tests that output unique frame names needed to be rebased to accommodate this change.

* LayoutTests/fast/forms/form-and-frame-interaction-retains-values-expected.txt:
* LayoutTests/fast/frames/iframe-reparenting-unique-name-expected.txt:
* LayoutTests/fast/frames/reparent-in-unload-contentdocument-expected.txt:
* LayoutTests/http/tests/security/dataURL/xss-DENIED-from-data-url-in-foreign-domain-subframe-expected.txt:
* LayoutTests/http/tests/site-isolation/notify-done-expected.txt:
* LayoutTests/platform/mac-site-isolation/TestExpectations:
* Source/WebCore/page/FrameTree.cpp:
(WebCore::FrameTree::setSpecifiedName):
(WebCore::FrameTree::clearName):
(WebCore::FrameTree::childBySpecifiedName const):
(WebCore::FrameTree::find const):
(WebCore::FrameTree::findByUniqueName const):
(WebCore::FrameTree::uniqueName const):
(WebCore::FrameTree::uniqueChildName const): Deleted.
(WebCore::FrameTree::generateUniqueName const): Deleted.
* Source/WebCore/page/FrameTree.h:
(WebCore::FrameTree::uniqueName const): Deleted.
(WebCore::FrameTree::resetFrameIdentifiers): Deleted.
* Source/WebCore/page/LocalFrame.cpp:
(WebCore::LocalFrame::setDocument):

Canonical link: https://commits.webkit.org/277393@main

Source/WebCore/page/FrameTree.cpp
@@ -51,18 +51,11 @@ FrameTree::~FrameTree()
 void FrameTree::setSpecifiedName(const AtomString& specifiedName)
 {
     m_specifiedName = specifiedName;
-    if (!parent()) {
-        m_uniqueName = specifiedName;
-        return;
-    }
-    m_uniqueName = nullAtom(); // Remove our old frame name so it's not considered in uniqueChildName.
-    m_uniqueName = parent()->tree().uniqueChildName(specifiedName);
 }
 
 void FrameTree::clearName()
 {
     m_specifiedName = nullAtom();
-    m_uniqueName = nullAtom();
 }
 
 Frame* FrameTree::parent() const
@@ -98,26 +91,6 @@ void FrameTree::removeChild(Frame& child)
     newLocationForNext = WTFMove(child.tree().m_nextSibling);
 
     m_scopedChildCount = invalidCount;
-}
-
-AtomString FrameTree::uniqueChildName(const AtomString& requestedName) const
-{
-    // If the requested name (the frame's "name" attribute) is unique, just use that.
-    if (!requestedName.isEmpty() && !childByUniqueName(requestedName) && !isBlankTargetFrameName(requestedName))
-        return requestedName;
-
-    // The "name" attribute was not unique or absent. Generate a name based on a counter on the main frame that gets reset
-    // on navigation. The name uses HTML comment syntax to avoid collisions with author names.
-    return generateUniqueName();
-}
-
-AtomString FrameTree::generateUniqueName() const
-{
-    auto& top = this->top();
-    if (&top.tree() != this)
-        return top.tree().generateUniqueName();
-
-    return makeAtomString("<!--frame", ++m_frameIDGenerator, "-->");
 }
 
 Ref<Frame> FrameTree::protectedThisFrame() const
@@ -250,6 +223,15 @@ Frame* FrameTree::childByUniqueName(const AtomString& name) const
     return nullptr;
 }
 
+Frame* FrameTree::childBySpecifiedName(const AtomString& name) const
+{
+    for (auto* child = firstChild(); child; child = child->tree().nextSibling()) {
+        if (child->tree().specifiedName() == name)
+            return child;
+    }
+    return nullptr;
+}
+
 // FrameTree::find() only returns frames in pages that are related to the active
 // page by an opener <-> openee relationship.
 static bool isFrameFamiliarWith(Frame& frameA, Frame& frameB)
@@ -264,7 +246,8 @@ static bool isFrameFamiliarWith(Frame& frameA, Frame& frameB)
         || (frameAOpener && frameBOpener && frameAOpener->page() == frameBOpener->page());
 }
 
-inline Frame* FrameTree::find(const AtomString& name, const Function<const AtomString&(const FrameTree&)>& nameGetter, Frame& activeFrame) const
+template<typename F>
+inline Frame* FrameTree::find(const AtomString& name, F&& nameGetter, Frame& activeFrame) const
 {
     if (isSelfTargetFrameName(name))
         return m_thisFrame.ptr();
@@ -312,7 +295,7 @@ inline Frame* FrameTree::find(const AtomString& name, const Function<const AtomS
 
 Frame* FrameTree::findByUniqueName(const AtomString& uniqueName, Frame& activeFrame) const
 {
-    return find(uniqueName, [&](auto& frameTree) -> const AtomString& {
+    return find(uniqueName, [&](auto& frameTree) -> AtomString {
         return frameTree.uniqueName();
     }, activeFrame);
 }
@@ -535,6 +518,26 @@ unsigned FrameTree::depth() const
     for (auto* parent = m_thisFrame.ptr(); parent; parent = parent->tree().parent())
         depth++;
     return depth;
+}
+
+AtomString FrameTree::uniqueName() const
+{
+    if (!parent())
+        return m_specifiedName;
+
+    auto frameIndex { 0u };
+    for (RefPtr frame = top().tree().firstChild(); frame; frame = frame->tree().traverseNext()) {
+        bool frameMatch = frame->frameID() == m_thisFrame->frameID();
+        auto frameName = frame->tree().specifiedName();
+        if (frameName.isEmpty() || isBlankTargetFrameName(frameName) || frame->tree().childBySpecifiedName(frameName)) {
+            frameIndex++;
+            if (frameMatch)
+                return makeAtomString("<!--frame"_s, frameIndex, "-->"_s);
+        }
+        if (frameMatch)
+            return frameName;
+    }
+    return nullAtom();
 }
 
 ASCIILiteral blankTargetFrameName()

Source/WebCore/page/FrameTree.h
@@ -42,7 +42,7 @@ public:
     ~FrameTree();
 
     const AtomString& specifiedName() const { return m_specifiedName; }
-    const AtomString& uniqueName() const { return m_uniqueName; }
+    WEBCORE_EXPORT AtomString uniqueName() const;
     WEBCORE_EXPORT void setSpecifiedName(const AtomString&);
     WEBCORE_EXPORT void clearName();
     WEBCORE_EXPORT Frame* parent() const;
@@ -71,6 +71,7 @@ public:
     WEBCORE_EXPORT void removeChild(Frame&);
 
     Frame* child(unsigned index) const;
+    Frame* childBySpecifiedName(const AtomString& name) const;
     Frame* childByUniqueName(const AtomString& name) const;
     WEBCORE_EXPORT Frame* findByUniqueName(const AtomString&, Frame& activeFrame) const;
     WEBCORE_EXPORT Frame* findBySpecifiedName(const AtomString&, Frame& activeFrame) const;
@@ -85,8 +86,6 @@ public:
     Frame* scopedChildBySpecifiedName(const AtomString& name) const;
     unsigned scopedChildCount() const;
 
-    void resetFrameIdentifiers() { m_frameIDGenerator = 0; }
-
 private:
     Frame* deepFirstChild() const;
     Frame* deepLastChild() const;
@@ -96,10 +95,7 @@ private:
     Frame* scopedChild(const Function<bool(const FrameTree&)>& isMatch, TreeScope*) const;
     unsigned scopedChildCount(TreeScope*) const;
 
-    Frame* find(const AtomString& name, const Function<const AtomString&(const FrameTree&)>& nameGetter, Frame& activeFrame) const;
-
-    AtomString uniqueChildName(const AtomString& requestedName) const;
-    AtomString generateUniqueName() const;
+    template<typename F> Frame* find(const AtomString& name, F&& nameGetter, Frame& activeFrame) const;
 
     Ref<Frame> protectedThisFrame() const;
 
@@ -107,14 +103,12 @@ private:
 
     WeakPtr<Frame> m_parent;
     AtomString m_specifiedName; // The actual frame name (may be empty).
-    AtomString m_uniqueName;
 
     RefPtr<Frame> m_nextSibling;
     WeakPtr<Frame> m_previousSibling;
     RefPtr<Frame> m_firstChild;
     WeakPtr<Frame> m_lastChild;
     mutable unsigned m_scopedChildCount { invalidCount };
-    mutable uint64_t m_frameIDGenerator { 0 };
 };
 
 ASCIILiteral blankTargetFrameName();

LayoutTests/fast/forms/form-and-frame-interaction-retains-values-expected.txt
@@ -1,7 +1,7 @@
 
 
 --------
-Frame: '<!--frame4-->'
+Frame: '<!--frame1-->'
 --------
 PASS
 
@@ -13,6 +13,6 @@ In this test the main page is loaded in a frameset, which sets the value of an i
 
 
 --------
-Frame: '<!--frame3-->'
+Frame: '<!--frame2-->'
 --------
 

LayoutTests/fast/frames/iframe-reparenting-unique-name-expected.txt
@@ -1,18 +1,18 @@
 
 
 --------
-Frame: '<!--frame2-->'
+Frame: '<!--frame1-->'
 --------
 This test PASSED if DRT outputs two <iframe>s with unique names.
 
 
 
 --------
-Frame: '<!--frame3-->'
+Frame: '<!--frame2-->'
 --------
 Frame 0
 
 --------
-Frame: '<!--frame4-->'
+Frame: '<!--frame3-->'
 --------
 Frame 1

LayoutTests/fast/frames/reparent-in-unload-contentdocument-expected.txt
@@ -6,6 +6,6 @@ TEST COMPLETE
 
 
 --------
-Frame: '<!--frame2-->'
+Frame: '<!--frame1-->'
 --------
 Should be visible.

LayoutTests/http/tests/security/dataURL/xss-DENIED-from-data-url-in-foreign-domain-subframe-expected.txt
@@ -7,7 +7,7 @@ Pass: Cross frame access from a data: URL on a different domain was denied.
 
 
 --------
-Frame: 'aFrame'
+Frame: '<!--frame1-->'
 --------
 Inner iframe on a foreign domain.
 

LayoutTests/http/tests/site-isolation/notify-done-expected.txt
@@ -3,6 +3,6 @@ This test passes if it doesn't time out.
 
 
 --------
-Frame: '<!--frame2-->'
+Frame: '<!--frame1-->'
 --------
 I am a simple document that calls notifyDone when done loading.

commit 686240cb98d8182dc675f6a2aac9abd2704984ec

Replace unique frame names with frame identifiers in the history controller https://bugs.webkit.org/show_bug.cgi?id=273995 rdar://127870100
Reviewed by Alex Christensen.

With site isolation, unique frame names should not be used to identify frames. This patch replaces most
of their usage in the history controller with frame identifiers. We cannot remove unique frame names from
`HistoryItem` because logic in `FrameLoader::loadURLIntoChildFrame` requires us to match current frames
with frames from a previous page. Unique frame names are also still used from session state and history
test output.

This also fixes a bug introduced by 277393@main, where shifting frames after a navigation could cause
the back/forward list to target an incorrect frame.

* Source/WebCore/history/HistoryItem.cpp:
(WebCore::HistoryItem::HistoryItem):
(WebCore::HistoryItem::reset):
(WebCore::HistoryItem::addChildItem):
(WebCore::HistoryItem::childItemWithFrameID):
* Source/WebCore/history/HistoryItem.h:
(WebCore::HistoryItem::frameID const):
(WebCore::HistoryItem::setFrameID):
* Source/WebCore/loader/HistoryController.cpp:
(WebCore::HistoryController::initializeItem):
(WebCore::HistoryController::recursiveSetProvisionalItem):
(WebCore::HistoryController::recursiveGoToItem):
* Source/WebCore/page/FrameTree.cpp:
(WebCore::FrameTree::childByFrameID const):
(WebCore::FrameTree::childByUniqueName const): Deleted.
* Source/WebCore/page/FrameTree.h:
* Source/WebCore/testing/Internals.cpp:
(WebCore::Internals::formControlStateOfPreviousHistoryItem):
(WebCore::Internals::setFormControlStateOfPreviousHistoryItem):
* Source/WebKit/Shared/SessionState.h:
* Source/WebKit/Shared/SessionState.serialization.in:
* Source/WebKit/WebProcess/WebCoreSupport/SessionStateConversion.cpp:
(WebKit::toFrameState):
(WebKit::applyFrameState):
* Tools/TestWebKitAPI/Tests/WebKit/WKBackForwardListTests.mm:
(TEST(WKBackForwardList, BackForwardListRemoveAndAddSubframes)):

Source/WebCore/history/HistoryItem.cpp
@@ -71,6 +71,7 @@ HistoryItem::HistoryItem(const HistoryItem& item)
     , m_originalURLString(item.m_originalURLString)
     , m_referrer(item.m_referrer)
     , m_target(item.m_target)
+    , m_frameID(item.m_frameID)
     , m_scrollPosition(item.m_scrollPosition)
     , m_pageScaleFactor(item.m_pageScaleFactor)
     , m_children(item.m_children.map([](auto& child) { return child->copy(); }))
@@ -102,6 +103,7 @@ void HistoryItem::reset()
     m_originalURLString = String();
     m_referrer = String();
     m_target = nullAtom();
+    m_frameID = std::nullopt;
 
     m_lastVisitWasFailure = false;
     m_isTargetItem = false;
@@ -287,7 +289,7 @@ void HistoryItem::setNavigationAPIStateObject(RefPtr<SerializedScriptValue>&& ob
 
 void HistoryItem::addChildItem(Ref<HistoryItem>&& child)
 {
-    ASSERT(!childItemWithTarget(child->target()));
+    ASSERT(!child->frameID() || !childItemWithFrameID(*child->frameID()));
     m_children.append(WTFMove(child));
 }
 
@@ -310,6 +312,15 @@ HistoryItem* HistoryItem::childItemWithTarget(const AtomString& target)
     unsigned size = m_children.size();
     for (unsigned i = 0; i < size; ++i) {
         if (m_children[i]->target() == target)
+            return m_children[i].ptr();
+    }
+    return nullptr;
+}
+
+HistoryItem* HistoryItem::childItemWithFrameID(FrameIdentifier frameID)
+{
+    for (unsigned i = 0; i < m_children.size(); ++i) {
+        if (m_children[i]->frameID() == frameID)
             return m_children[i].ptr();
     }
     return nullptr;

Source/WebCore/history/HistoryItem.h
@@ -28,6 +28,7 @@
 
 #include "BackForwardItemIdentifier.h"
 #include "FloatRect.h"
+#include "FrameIdentifier.h"
 #include "FrameLoaderTypes.h"
 #include "IntPoint.h"
 #include "IntRect.h"
@@ -97,6 +98,7 @@ public:
     WEBCORE_EXPORT URL originalURL() const;
     WEBCORE_EXPORT const String& referrer() const;
     WEBCORE_EXPORT const AtomString& target() const;
+    std::optional<FrameIdentifier> frameID() const { return m_frameID; }
     WEBCORE_EXPORT bool isTargetItem() const;
     
     WEBCORE_EXPORT FormData* formData();
@@ -126,6 +128,7 @@ public:
     WEBCORE_EXPORT void setOriginalURLString(const String&);
     WEBCORE_EXPORT void setReferrer(const String&);
     WEBCORE_EXPORT void setTarget(const AtomString&);
+    void setFrameID(std::optional<FrameIdentifier> frameID) { m_frameID = frameID; }
     WEBCORE_EXPORT void setIsTargetItem(bool);
     
     WEBCORE_EXPORT void setStateObject(RefPtr<SerializedScriptValue>&&);
@@ -149,6 +152,7 @@ public:
     WEBCORE_EXPORT void addChildItem(Ref<HistoryItem>&&);
     void setChildItem(Ref<HistoryItem>&&);
     WEBCORE_EXPORT HistoryItem* childItemWithTarget(const AtomString&);
+    WEBCORE_EXPORT HistoryItem* childItemWithFrameID(FrameIdentifier);
     HistoryItem* childItemWithDocumentSequenceNumber(long long number);
     WEBCORE_EXPORT const Vector<Ref<HistoryItem>>& children() const;
     void clearChildren();
@@ -227,6 +231,7 @@ private:
     String m_originalURLString;
     String m_referrer;
     AtomString m_target;
+    std::optional<FrameIdentifier> m_frameID;
     
     IntPoint m_scrollPosition;
     float m_pageScaleFactor { 0 }; // 0 indicates "unset".

Source/WebCore/loader/HistoryController.cpp
@@ -715,6 +715,7 @@ void HistoryController::initializeItem(HistoryItem& item)
     
     item.setURL(url);
     item.setTarget(frame->tree().uniqueName());
+    item.setFrameID(frame->frameID());
     item.setOriginalURLString(originalURL.string());
 
     if (!unreachableURL.isEmpty() || documentLoader->response().httpStatusCode() >= 400)
@@ -779,13 +780,15 @@ void HistoryController::recursiveSetProvisionalItem(HistoryItem& item, HistoryIt
     m_provisionalItem = &item;
 
     for (Ref childItem : item.children()) {
-        auto& childFrameName = childItem->target();
+        auto frameID = childItem->frameID();
+        if (!frameID)
+            continue;
 
-        RefPtr fromChildItem = fromItem->childItemWithTarget(childFrameName);
+        RefPtr fromChildItem = fromItem->childItemWithFrameID(*frameID);
         if (!fromChildItem)
             continue;
 
-        if (RefPtr childFrame = dynamicDowncast<LocalFrame>(m_frame->tree().childByUniqueName(childFrameName)))
+        if (RefPtr childFrame = m_frame->tree().childByFrameID(*frameID))
             childFrame->checkedHistory()->recursiveSetProvisionalItem(childItem, fromChildItem.get());
     }
 }
@@ -802,13 +805,15 @@ void HistoryController::recursiveGoToItem(HistoryItem& item, HistoryItem* fromIt
 
     // Just iterate over the rest, looking for frames to navigate.
     for (Ref childItem : item.children()) {
-        auto& childFrameName = childItem->target();
+        auto frameID = childItem->frameID();
+        if (!frameID)
+            continue;
 
-        RefPtr fromChildItem = fromItem->childItemWithTarget(childFrameName);
+        RefPtr fromChildItem = fromItem->childItemWithFrameID(*frameID);
         if (!fromChildItem)
             continue;
 
-        if (RefPtr childFrame = dynamicDowncast<LocalFrame>(m_frame->tree().childByUniqueName(childFrameName)))
+        if (RefPtr childFrame = m_frame->tree().childByFrameID(*frameID))
             childFrame->checkedHistory()->recursiveGoToItem(childItem, fromChildItem.get(), type, shouldTreatAsContinuingLoad);
     }
 }

Source/WebCore/page/FrameTree.cpp
@@ -214,10 +214,10 @@ Frame* FrameTree::child(unsigned index) const
     return result;
 }
 
-Frame* FrameTree::childByUniqueName(const AtomString& name) const
+Frame* FrameTree::childByFrameID(FrameIdentifier frameID) const
 {
     for (auto* child = firstChild(); child; child = child->tree().nextSibling()) {
-        if (child->tree().uniqueName() == name)
+        if (child->frameID() == frameID)
             return child;
     }
     return nullptr;

Source/WebCore/page/FrameTree.h
@@ -19,6 +19,7 @@
 
 #pragma once
 
+#include "FrameIdentifier.h"
 #include <wtf/Forward.h>
 #include <wtf/WeakPtr.h>
 #include <wtf/text/AtomString.h>
@@ -72,7 +73,7 @@ public:
 
     Frame* child(unsigned index) const;
     Frame* childBySpecifiedName(const AtomString& name) const;
-    Frame* childByUniqueName(const AtomString& name) const;
+    Frame* childByFrameID(FrameIdentifier) const;
     WEBCORE_EXPORT Frame* findByUniqueName(const AtomString&, Frame& activeFrame) const;
     WEBCORE_EXPORT Frame* findBySpecifiedName(const AtomString&, Frame& activeFrame) const;
     WEBCORE_EXPORT unsigned childCount() const;

Source/WebKit/Shared/SessionState.h
@@ -28,6 +28,7 @@
 #include "ViewSnapshotStore.h"
 #include <WebCore/BackForwardItemIdentifier.h>
 #include <WebCore/FloatRect.h>
+#include <WebCore/FrameIdentifier.h>
 #include <WebCore/FrameLoaderTypes.h>
 #include <WebCore/IntRect.h>
 #include <WebCore/SerializedScriptValue.h>
@@ -80,6 +81,7 @@ public:
     String originalURLString;
     String referrer;
     AtomString target;
+    std::optional<WebCore::FrameIdentifier> frameID;
 
     std::optional<Vector<uint8_t>> stateObjectData;
 

Source/WebKit/Shared/SessionState.serialization.in
@@ -42,6 +42,7 @@ header: "SessionState.h"
     String originalURLString;
     String referrer;
     AtomString target;
+    std::optional<WebCore::FrameIdentifier> frameID;
 
     std::optional<Vector<uint8_t>> stateObjectData;
 

Source/WebKit/WebProcess/WebCoreSupport/SessionStateConversion.cpp
@@ -72,6 +72,7 @@ static FrameState toFrameState(const HistoryItem& historyItem)
     frameState.originalURLString = historyItem.originalURLString();
     frameState.referrer = historyItem.referrer();
     frameState.target = historyItem.target();
+    frameState.frameID = historyItem.frameID();
 
     frameState.setDocumentState(historyItem.documentState());
     if (RefPtr<SerializedScriptValue> stateObject = historyItem.stateObject())
@@ -141,6 +142,7 @@ static void applyFrameState(WebCore::HistoryItemClient& client, HistoryItem& his
     historyItem.setOriginalURLString(frameState.originalURLString);
     historyItem.setReferrer(frameState.referrer);
     historyItem.setTarget(frameState.target);
+    historyItem.setFrameID(frameState.frameID);
 
     historyItem.setDocumentState(frameState.documentState());
 

Source/WebCore/testing/Internals.cpp
@@ -1631,10 +1631,10 @@ ExceptionOr<Vector<AtomString>> Internals::formControlStateOfPreviousHistoryItem
     HistoryItem* mainItem = frame()->history().previousItem();
     if (!mainItem)
         return Exception { ExceptionCode::InvalidAccessError };
-    auto uniqueName = frame()->tree().uniqueName();
-    if (mainItem->target() != uniqueName && !mainItem->childItemWithTarget(uniqueName))
+    auto frameID = frame()->frameID();
+    if (mainItem->frameID() != frameID && !mainItem->childItemWithFrameID(frameID))
         return Exception { ExceptionCode::InvalidAccessError };
-    return Vector<AtomString> { mainItem->target() == uniqueName ? mainItem->documentState() : mainItem->childItemWithTarget(uniqueName)->documentState() };
+    return Vector<AtomString> { mainItem->frameID() == frameID ? mainItem->documentState() : mainItem->childItemWithFrameID(frameID)->documentState() };
 }
 
 ExceptionOr<void> Internals::setFormControlStateOfPreviousHistoryItem(const Vector<AtomString>& state)
@@ -1642,10 +1642,10 @@ ExceptionOr<void> Internals::setFormControlStateOfPreviousHistoryItem(const Vect
     HistoryItem* mainItem = frame()->history().previousItem();
     if (!mainItem)
         return Exception { ExceptionCode::InvalidAccessError };
-    auto uniqueName = frame()->tree().uniqueName();
-    if (mainItem->target() == uniqueName)
+    auto frameID = frame()->frameID();
+    if (mainItem->frameID() == frameID)
         mainItem->setDocumentState(state);
-    else if (HistoryItem* subItem = mainItem->childItemWithTarget(uniqueName))
+    else if (HistoryItem* subItem = mainItem->childItemWithFrameID(frameID))
         subItem->setDocumentState(state);
     else
         return Exception { ExceptionCode::InvalidAccessError };

Tools/TestWebKitAPI/Tests/WebKit/WKBackForwardListTests.mm
@@ -29,12 +29,14 @@
 #import "PlatformUtilities.h"
 #import "Test.h"
 #import "TestNavigationDelegate.h"
+#import "TestUIDelegate.h"
 #import <WebKit/WKBackForwardListItemPrivate.h>
 #import <WebKit/WKBackForwardListPrivate.h>
 #import <WebKit/WKNavigationDelegatePrivate.h>
 #import <WebKit/WKNavigationPrivate.h>
 #import <WebKit/WKWebViewPrivate.h>
 #import <WebKit/WKWebViewPrivateForTesting.h>
+#import <WebKit/_WKFrameTreeNode.h>
 #import <WebKit/_WKSessionState.h>
 #import <wtf/RetainPtr.h>
 #import <wtf/text/WTFString.h>
@@ -807,4 +809,51 @@ TEST(WKBackForwardList, BackNavigationHijacking)
     TestWebKitAPI::Util::spinRunLoop(10);
 
     EXPECT_STREQ([webView URL].absoluteString.UTF8String, url1.absoluteString.UTF8String);
+}
+
+TEST(WKBackForwardList, BackForwardListRemoveAndAddSubframes)
+{
+    auto indexHTML = "<iframe id='1' src='/frame1'></iframe>"
+        "<iframe id='2' src='/frame2'></iframe>"_s;
+    TestWebKitAPI::HTTPServer server({
+        { "/index"_s, { indexHTML } },
+        { "/frame1"_s, { ""_s } },
+        { "/frame2"_s, { "<script> alert('frame2'); </script>"_s } },
+        { "/frame3"_s, { "<script> alert('frame3'); </script>"_s } },
+    }, TestWebKitAPI::HTTPServer::Protocol::Https);
+    auto webView = adoptNS([[WKWebView alloc] init]);
+    auto navigationDelegate = adoptNS([WKBackForwardNavigationDelegate new]);
+    webView.get().navigationDelegate = navigationDelegate.get();
+    auto uiDelegate = adoptNS([TestUIDelegate new]);
+    webView.get().UIDelegate = uiDelegate.get();
+
+    [webView loadRequest:server.request("/index"_s)];
+    EXPECT_WK_STREQ([uiDelegate waitForAlert], "frame2");
+
+    [webView _frames:^(_WKFrameTreeNode *mainFrame) {
+        [webView evaluateJavaScript:@"location.href = '/frame3'" inFrame:mainFrame.childFrames[1].info inContentWorld:WKContentWorld.pageWorld completionHandler:nil];
+    }];
+    EXPECT_WK_STREQ([uiDelegate waitForAlert], "frame3");
+
+    auto removeAndAddFrame = @"let frame = document.getElementById('1');"
+        "frame.parentNode.removeChild(frame);"
+        "let newFrame = document.createElement('iframe');"
+        "newFrame.src = '/frame1';"
+        "document.body.appendChild(newFrame);";
+    __block bool done = false;
+    [webView evaluateJavaScript:removeAndAddFrame completionHandler:^(id, NSError *) {
+        done = true;
+    }];
+    TestWebKitAPI::Util::run(&done);
+    done = false;
+
+    [webView goBack];
+    EXPECT_WK_STREQ([uiDelegate waitForAlert], "frame2");
+
+    __block auto expectedFrameURL = server.request("/frame2"_s).URL.absoluteString.UTF8String;
+    [webView evaluateJavaScript:@"document.getElementById('2').contentWindow.location.href" completionHandler:^(id result, NSError *) {
+        EXPECT_WK_STREQ(expectedFrameURL, [result UTF8String]);
+        done = true;
+    }];
+    TestWebKitAPI::Util::run(&done);
 }