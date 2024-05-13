WebKit/WebKit

commit 6ae622978d28c12e50253fab9c4cfc08bcffff1c

Sanitize email input when multiple attribute is set https://bugs.webkit.org/show_bug.cgi?id=256137
Reviewed by Tim Nguyen and Chris Dumez.

The sanitizeValue function is now called when the multiple attribute is
changed. As per the spec https://html.spec.whatwg.org/#email-state-(type=email)

* LayoutTests/imported/w3c/web-platform-tests/html/semantics/forms/the-input-element/email-expected.txt:
* LayoutTests/imported/w3c/web-platform-tests/html/semantics/forms/the-input-element/email.html:
* Source/WebCore/html/EmailInputType.cpp:
(WebCore::EmailInputType::attributeChanged):
* Source/WebCore/html/EmailInputType.h:

Canonical link: https://commits.webkit.org/263555@main

Source/WebCore/html/EmailInputType.cpp
@@ -26,6 +26,7 @@
 #include "EmailInputType.h"
 
 #include "HTMLInputElement.h"
+#include "HTMLNames.h"
 #include "HTMLParserIdioms.h"
 #include "InputTypeNames.h"
 #include "LocalizedStrings.h"
@@ -34,6 +35,8 @@
 #include <wtf/text/StringBuilder.h>
 
 namespace WebCore {
+
+using namespace HTMLNames;
 
 // From https://html.spec.whatwg.org/#valid-e-mail-address.
 static constexpr ASCIILiteral emailPattern = "^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"_s;
@@ -86,6 +89,14 @@ String EmailInputType::typeMismatchText() const
 bool EmailInputType::supportsSelectionAPI() const
 {
     return false;
+}
+
+void EmailInputType::attributeChanged(const QualifiedName& name)
+{
+    if (name == multipleAttr)
+        element()->setValueFromRenderer(sanitizeValue(element()->value()));
+
+    BaseTextInputType::attributeChanged(name);
 }
 
 String EmailInputType::sanitizeValue(const String& proposedValue) const

LayoutTests/imported/w3c/web-platform-tests/html/semantics/forms/the-input-element/email-expected.txt
@@ -7,4 +7,5 @@ PASS When the multiple attribute is removed, the user agent must run the value s
 PASS multiple_email has the multiple attribute
 PASS run the value sanitization algorithm after setting a new value
 PASS valid value is a set of valid email addresses separated by a single ','
+PASS When the multiple attribute is set, the user agent must run the value sanitization algorithm
 

LayoutTests/imported/w3c/web-platform-tests/html/semantics/forms/the-input-element/email.html
@@ -55,4 +55,12 @@
     mult.value = 'u,ser1@example.com,user2@test.com,user3@test.com';
     assert_false(mult.validity.valid);
   }, "valid value is a set of valid email addresses separated by a single ','");
+    
+  test(function(){
+    mult.removeAttribute('multiple');
+    mult.value = 'user1@example.com , user2@example.com';
+    assert_equals(mult.value, 'user1@example.com , user2@example.com');
+    mult.setAttribute('multiple', true);
+    assert_equals(mult.value, 'user1@example.com,user2@example.com');
+  }, 'When the multiple attribute is set, the user agent must run the value sanitization algorithm');
 </script>

Source/WebCore/html/EmailInputType.h
@@ -54,6 +54,7 @@ private:
     String typeMismatchText() const final;
     bool supportsSelectionAPI() const final;
     String sanitizeValue(const String&) const final;
+    void attributeChanged(const QualifiedName&) final;
 };
 
 } // namespace WebCore

commit 208d709bf7604891c1b3f8a082cd29b95f83fef9

Cherry-pick 276895@main (b52f2b8abac2). https://bugs.webkit.org/show_bug.cgi?id=271043
    REGRESSION (273461@main): The value attribute is not displayed in an input field with type="email" and multiple attributes
    https://bugs.webkit.org/show_bug.cgi?id=271043
    rdar://125221858

    Reviewed by Ryosuke Niwa.

    273461@main deferred input element shadow tree construction outside of attribute
    parsing. However, when combined with an earlier change to sanitize the value of
    email inputs when the multiple attribute is set (263555@main), the deferral of
    shadow tree construction results in the value not being displayed at all.

    The root cause of the issue is the use of `setValueFromRenderer` introduced by
    263555@main. The use of that method in `EmailInputType::attributeChanged` has
    always been inappropriate, as it is specifically meant for user modifications.
    However, in this case, the problem arises from the fact that `setValueFromRenderer`
    calls `setFormControlValueMatchesRenderer(true)`. This results in the elision
    of shadow tree construction in `TextFieldInputType::updateInnerTextValue`, as
    `HTMLTextFormControlElement::setInnerTextValue` is only called if
    `formControlValueMatchesRenderer()` returns false. Before 273461@main, this elision
    was benign, as the shadow tree was constructed at attribute parse time.

    Fix by using a more appropriate method to update the email input's value when
    the multiple attribute is changed.

    A new regression test is added. The coverage for the issue fixed by 263555@main
    continues to be provided by the WPT: html/semantics/forms/the-input-element/email.html.

    * LayoutTests/fast/forms/email-input-type-value-and-multiple-attributes-set-expected.html: Added.
    * LayoutTests/fast/forms/email-input-type-value-and-multiple-attributes-set.html: Added.
    * Source/WebCore/html/EmailInputType.cpp:
    (WebCore::EmailInputType::attributeChanged):

Source/WebCore/html/EmailInputType.cpp
@@ -94,7 +94,7 @@ bool EmailInputType::supportsSelectionAPI() const
 void EmailInputType::attributeChanged(const QualifiedName& name)
 {
     if (name == multipleAttr)
-        element()->setValueFromRenderer(sanitizeValue(element()->value()));
+        element()->setValueInternal(sanitizeValue(element()->value()), TextFieldEventBehavior::DispatchNoEvent);
 
     BaseTextInputType::attributeChanged(name);
 }

LayoutTests/fast/forms/email-input-type-value-and-multiple-attributes-set-expected.html
@@ -1,0 +1,1 @@
+<input type="email" value="test@example.com">

LayoutTests/fast/forms/email-input-type-value-and-multiple-attributes-set.html
@@ -1,0 +1,1 @@
+<input type="email" value="test@example.com" multiple>