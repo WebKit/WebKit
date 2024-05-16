ab0d7793f15cf087af2e8d0f1508152881a2187b
Kimmo Kinnunen <kkinnunen@apple.com>
2024-05-15 17:24:16 -0700

    GraphicsContextGLANGLE does not validate clearBuffers value length https://bugs.webkit.org/show_bug.cgi?id=271634 rdar://125222153
    Reviewed by Dan Glastonbury.
    
    Avoid passing too long or small arrays as GL_clearBuffer*v values.
    
    * Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.cpp:
    (WebCore::GraphicsContextGLANGLE::clearBufferiv):
    (WebCore::GraphicsContextGLANGLE::clearBufferuiv):
    (WebCore::GraphicsContextGLANGLE::clearBufferfv):
    (WebCore::GraphicsContextGLANGLE::validateClearBufferv):
    * Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.h:
    * Tools/TestWebKitAPI/Tests/WebCore/cocoa/TestGraphicsContextGLCocoa.mm:
    (TestWebKitAPI::TEST_F):
    
    Originally-landed-as: 272448.803@safari-7618-branch (89ee93bd2ea4). rdar://128087675
    Canonical link: https://commits.webkit.org/278841@main

--- Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.cpp
+++ Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.cpp
@@ -2629,7 +2629,8 @@ void GraphicsContextGLANGLE::clearBufferiv(GCGLenum buffer, GCGLint drawbuffer, 
 {
     if (!makeContextCurrent())
         return;
-
+    if (!validateClearBufferv(buffer, values.size()))
+        return;
     GL_ClearBufferiv(buffer, drawbuffer, values.data());
     checkGPUStatus();
 }
@@ -2638,7 +2639,8 @@ void GraphicsContextGLANGLE::clearBufferuiv(GCGLenum buffer, GCGLint drawbuffer,
 {
     if (!makeContextCurrent())
         return;
-
+    if (!validateClearBufferv(buffer, values.size()))
+        return;
     GL_ClearBufferuiv(buffer, drawbuffer, values.data());
     checkGPUStatus();
 }
@@ -2647,7 +2649,8 @@ void GraphicsContextGLANGLE::clearBufferfv(GCGLenum buffer, GCGLint drawbuffer, 
 {
     if (!makeContextCurrent())
         return;
-
+    if (!validateClearBufferv(buffer, values.size()))
+        return;
     GL_ClearBufferfv(buffer, drawbuffer, values.data());
     checkGPUStatus();
 }
@@ -3336,6 +3339,26 @@ void GraphicsContextGLANGLE::requestExtension(const String& name)
         m_webglColorBufferFloatRGB = true;
 }
 
+bool GraphicsContextGLANGLE::validateClearBufferv(GCGLenum buffer, size_t valuesSize)
+{
+    // ClearBuffersfv, iv, uiv are missing ANGLE Robust* entry-point. Make the call act similar way as other
+    // calls that validate buffer sizes by validating it here.
+    switch (buffer) {
+    case GraphicsContextGL::COLOR:
+        if (valuesSize == 4)
+            return true;
+        break;
+    case GraphicsContextGL::DEPTH:
+    case GraphicsContextGL::STENCIL:
+        if (valuesSize == 1)
+            return true;
+        break;
+    default:
+        break;
+    }
+    addError(GCGLErrorCode::InvalidOperation);
+    return false;
+}
 }
 
 #endif // ENABLE(WEBGL)
