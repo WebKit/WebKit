04a4f4adf3f1272fe288b310cdcca6cc8f96c956
Philippe Normand <philn@igalia.com>
2024-05-20 03:42:25 -0700

    [GStreamer] Minor cleanups in GstMappedFrame class https://bugs.webkit.org/show_bug.cgi?id=274312
    Reviewed by Xabier Rodriguez-Calvar.
    
    There is no need for a m_isValid boolean attribute because the buffer field on the GstVideoFrame can
    be checked instead.
    
    * Source/WebCore/platform/graphics/gstreamer/GStreamerCommon.h:
    (WebCore::GstMappedFrame::GstMappedFrame):
    (WebCore::GstMappedFrame::~GstMappedFrame):
    (WebCore::GstMappedFrame::get):
    (WebCore::GstMappedFrame::ComponentData const):
    (WebCore::GstMappedFrame::ComponentStride const):
    (WebCore::GstMappedFrame::info):
    (WebCore::GstMappedFrame::width const):
    (WebCore::GstMappedFrame::height const):
    (WebCore::GstMappedFrame::format const):
    (WebCore::GstMappedFrame::planeData const):
    (WebCore::GstMappedFrame::planeStride const):
    (WebCore::GstMappedFrame::isValid const):
    (WebCore::GstMappedFrame::operator bool const):
    (WebCore::GstMappedFrame::operator! const):
    
    Canonical link: https://commits.webkit.org/278992@main

--- Source/WebCore/platform/graphics/gstreamer/GStreamerCommon.h
+++ Source/WebCore/platform/graphics/gstreamer/GStreamerCommon.h
@@ -223,90 +223,87 @@ class GstMappedFrame {
     WTF_MAKE_FAST_ALLOCATED;
     WTF_MAKE_NONCOPYABLE(GstMappedFrame);
 public:
-
     GstMappedFrame(GstBuffer* buffer, GstVideoInfo* info, GstMapFlags flags)
     {
-        m_isValid = gst_video_frame_map(&m_frame, info, buffer, flags);
+        gst_video_frame_map(&m_frame, info, buffer, flags);
     }
 
     GstMappedFrame(const GRefPtr<GstSample>& sample, GstMapFlags flags)
     {
         GstVideoInfo info;
-
         if (!gst_video_info_from_caps(&info, gst_sample_get_caps(sample.get())))
             return;
 
-        m_isValid = gst_video_frame_map(&m_frame, &info, gst_sample_get_buffer(sample.get()), flags);
+        gst_video_frame_map(&m_frame, &info, gst_sample_get_buffer(sample.get()), flags);
+    }
+
+    ~GstMappedFrame()
+    {
+        // FIXME: Make this un-conditional when the minimum GStreamer dependency version is >= 1.22.
+        if (m_frame.buffer)
+            gst_video_frame_unmap(&m_frame);
     }
 
     GstVideoFrame* get()
     {
-        RELEASE_ASSERT(m_isValid);
+        RELEASE_ASSERT(isValid());
         return &m_frame;
     }
 
     uint8_t* ComponentData(int comp) const
     {
-        RELEASE_ASSERT(m_isValid);
+        RELEASE_ASSERT(isValid());
         return GST_VIDEO_FRAME_COMP_DATA(&m_frame, comp);
     }
 
     int ComponentStride(int stride) const
     {
-        RELEASE_ASSERT(m_isValid);
+        RELEASE_ASSERT(isValid());
         return GST_VIDEO_FRAME_COMP_STRIDE(&m_frame, stride);
     }
 
     GstVideoInfo* info()
     {
-        RELEASE_ASSERT(m_isValid);
+        RELEASE_ASSERT(isValid());
         return &m_frame.info;
     }
 
     int width() const
     {
-        RELEASE_ASSERT(m_isValid);
+        RELEASE_ASSERT(isValid());
         return GST_VIDEO_FRAME_WIDTH(&m_frame);
     }
 
     int height() const
     {
-        RELEASE_ASSERT(m_isValid);
+        RELEASE_ASSERT(isValid());
         return GST_VIDEO_FRAME_HEIGHT(&m_frame);
     }
 
     int format() const
     {
-        RELEASE_ASSERT(m_isValid);
+        RELEASE_ASSERT(isValid());
         return GST_VIDEO_FRAME_FORMAT(&m_frame);
     }
 
     void* planeData(uint32_t planeIndex) const
     {
-        RELEASE_ASSERT(m_isValid);
+        RELEASE_ASSERT(isValid());
         return GST_VIDEO_FRAME_PLANE_DATA(&m_frame, planeIndex);
     }
 
     int planeStride(uint32_t planeIndex) const
     {
-        RELEASE_ASSERT(m_isValid);
+        RELEASE_ASSERT(isValid());
         return GST_VIDEO_FRAME_PLANE_STRIDE(&m_frame, planeIndex);
     }
 
-    ~GstMappedFrame()
-    {
-        if (m_isValid)
-            gst_video_frame_unmap(&m_frame);
-        m_isValid = false;
-    }
-
-    bool isValid() const { return m_isValid; }
-    explicit operator bool() const { return m_isValid; }
-    bool operator!() const { return !m_isValid; }
+    bool isValid() const { return m_frame.buffer; }
+    explicit operator bool() const { return m_frame.buffer; }
+    bool operator!() const { return !m_frame.buffer; }
 
 private:
     GstVideoFrame m_frame;
-    bool m_isValid { false };
 };
 
 class GstMappedAudioBuffer {
