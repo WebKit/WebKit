3a3bb5a411b2eb2da92e4f4a4a9f263f52c1a0e4
Diego Pino Garcia <dpino@igalia.com>
2024-05-21 01:30:29 -0700

    [GCC] Unreviewed, build fix for Debian 11 after 278995@main https://bugs.webkit.org/show_bug.cgi?id=267827
    GCC10 requires compiler flag '-fcoroutines'.
    
    * Source/cmake/WebKitCompilerFlags.cmake:
    
    Canonical link: https://commits.webkit.org/279046@main

--- Source/cmake/WebKitCompilerFlags.cmake
+++ Source/cmake/WebKitCompilerFlags.cmake
@@ -149,6 +149,7 @@ if (COMPILER_IS_GCC_OR_CLANG)
     else ()
         WEBKIT_APPEND_GLOBAL_COMPILER_FLAGS(-fno-exceptions)
         WEBKIT_APPEND_GLOBAL_CXX_FLAGS(-fno-rtti)
+        WEBKIT_APPEND_GLOBAL_CXX_FLAGS(-fcoroutines)
 
         WEBKIT_PREPEND_GLOBAL_COMPILER_FLAGS(-Wno-tautological-compare)
 
