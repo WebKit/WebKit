WebKit/WebKit

commit ba786023a496b001be96a51f56d52977d1617d05

Add release assertion to verify public suffix added to cache is correct https://bugs.webkit.org/show_bug.cgi?id=271811 rdar://125417343
Reviewed by Brady Eidson.

To help debug a crash found in ToT WebKit.

* Source/WebCore/platform/PublicSuffixStore.h:
* Source/WebCore/platform/cocoa/PublicSuffixStoreCocoa.mm:
(WebCore::PublicSuffixStore::enablePublicSuffixCache):
(WebCore::PublicSuffixStore::addPublicSuffix):
* Tools/TestWebKitAPI/Tests/WebCore/PublicSuffix.cpp:
(TestWebKitAPI::TEST_F):

Canonical link: https://commits.webkit.org/276834@main

Source/WebCore/platform/PublicSuffixStore.h
@@ -43,7 +43,8 @@ public:
     WEBCORE_EXPORT void clearHostTopPrivatelyControlledDomainCache();
 
 #if PLATFORM(COCOA)
-    WEBCORE_EXPORT void enablePublicSuffixCache();
+    enum class CanAcceptCustomPublicSuffix : bool { No, Yes };
+    WEBCORE_EXPORT void enablePublicSuffixCache(CanAcceptCustomPublicSuffix = CanAcceptCustomPublicSuffix::No);
     WEBCORE_EXPORT void addPublicSuffix(const String& publicSuffix);
 #endif
 
@@ -59,6 +60,7 @@ private:
 #if PLATFORM(COCOA)
     mutable Lock m_publicSuffixCacheLock;
     std::optional<HashSet<String, ASCIICaseInsensitiveHash>> m_publicSuffixCache WTF_GUARDED_BY_LOCK(m_publicSuffixCacheLock);
+    bool m_canAcceptCustomPublicSuffix { false };
 #endif
 };
 

Source/WebCore/platform/cocoa/PublicSuffixStoreCocoa.mm
@@ -60,11 +60,12 @@ String PublicSuffixStore::platformTopPrivatelyControlledDomain(const String& hos
     return { };
 }
 
-void PublicSuffixStore::enablePublicSuffixCache()
+void PublicSuffixStore::enablePublicSuffixCache(CanAcceptCustomPublicSuffix canAcceptCustomPublicSuffix)
 {
     Locker locker { m_publicSuffixCacheLock };
     ASSERT(!m_publicSuffixCache);
 
+    m_canAcceptCustomPublicSuffix = (canAcceptCustomPublicSuffix == CanAcceptCustomPublicSuffix::Yes);
     m_publicSuffixCache = HashSet<String, ASCIICaseInsensitiveHash> { };
 }
 
@@ -74,6 +75,12 @@ void PublicSuffixStore::addPublicSuffix(const String& publicSuffix)
         return;
 
     Locker locker { m_publicSuffixCacheLock };
+
+    if (!m_publicSuffixCache)
+        return;
+
+    if (!m_canAcceptCustomPublicSuffix)
+        RELEASE_ASSERT(isPublicSuffixCF(publicSuffix));
 
     if (m_publicSuffixCache)
         m_publicSuffixCache->add(crossThreadCopy(publicSuffix));

Tools/TestWebKitAPI/Tests/WebCore/PublicSuffix.cpp
@@ -195,7 +195,7 @@ TEST_F(PublicSuffix, PublicSuffixCache)
     EXPECT_EQ(String("example.example"_s), publicSuffixStore.topPrivatelyControlledDomain("a.b.example.example"_s));
     EXPECT_FALSE(publicSuffixStore.isPublicSuffix(""_s));
 
-    publicSuffixStore.enablePublicSuffixCache();
+    publicSuffixStore.enablePublicSuffixCache(PublicSuffixStore::CanAcceptCustomPublicSuffix::Yes);
     publicSuffixStore.clearHostTopPrivatelyControlledDomainCache();
     publicSuffixStore.addPublicSuffix("example.example"_s);
     publicSuffixStore.addPublicSuffix(""_s);

commit 4031410eeeb157432f0a6e0ca143e54d02665096

ASSERTION FAILED: isPublicSuffixCF(publicSuffix) in PublicSuffixStore::addPublicSuffix https://bugs.webkit.org/show_bug.cgi?id=274418 rdar://128255984
Reviewed by Chris Dumez.

The assertion was introduced in 276834@main to help debug rdar://125417343, where we suspected the cause is unexpected
entry being added to public suffix cache, like 276635@main. However, because UI process and web process have different
sandbox access, isPublicSuffixCF (_CFHostIsDomainTopLevel) might return different values on the same string. That means,
in web process, the assertion would be hit even though the string is a valid public suffix according to UI process's
check.

As the original goal is to ensure UI process only sends valid public suffix (or empty string) to web process, the patch
does the hardening by introducing a new class PublicSuffix. UI process needs to explicity contruct it from URL with
PublicSuffixStore and send PublicSuffix object to web process, which is less error-prone than allowing UI process sends
public suffix in string. With this change, we no longer need the assertion.

* Source/WebCore/Headers.cmake:
* Source/WebCore/WebCore.xcodeproj/project.pbxproj:
* Source/WebCore/platform/PublicSuffix.h: Added.
(WebCore::PublicSuffix::fromRawString):
(WebCore::PublicSuffix::isValid const):
(WebCore::PublicSuffix::string const):
(WebCore::PublicSuffix::isolatedCopy const):
(WebCore::PublicSuffix::PublicSuffix):
(WebCore::PublicSuffix::operator== const):
(WebCore::PublicSuffix::isHashTableDeletedValue const):
(WebCore::PublicSuffix::hash const):
(WebCore::PublicSuffix::PublicSuffixHash::hash):
(WebCore::PublicSuffix::PublicSuffixHash::equal):
* Source/WebCore/platform/PublicSuffixStore.cpp:
(WebCore::PublicSuffixStore::publicSuffix const):
* Source/WebCore/platform/PublicSuffixStore.h:
(): Deleted.
* Source/WebCore/platform/cocoa/PublicSuffixStoreCocoa.mm:
(WebCore::PublicSuffixStore::platformIsPublicSuffix const):
(WebCore::PublicSuffixStore::enablePublicSuffixCache):

Source/WebCore/Headers.cmake
@@ -1759,6 +1759,7 @@ set(WebCore_PRIVATE_FRAMEWORK_HEADERS
     platform/ProcessIdentity.h
     platform/ProcessQualified.h
     platform/PromisedAttachmentInfo.h
+    platform/PublicSuffix.h
     platform/PublicSuffixStore.h
     platform/RectEdges.h
     platform/ReferrerPolicy.h

Source/WebCore/platform/PublicSuffix.h
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2024 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <wtf/CrossThreadCopier.h>
+#include <wtf/HashTraits.h>
+#include <wtf/text/StringHash.h>
+
+namespace WebCore {
+
+class PublicSuffix {
+public:
+    static PublicSuffix fromRawString(String&& string) { return PublicSuffix(WTFMove(string)); }
+    PublicSuffix() = default;
+    bool isValid() const { return !m_string.isEmpty(); }
+    const String& string() const { return m_string; }
+    PublicSuffix isolatedCopy() const { return fromRawString(crossThreadCopy(m_string)); }
+
+    PublicSuffix(WTF::HashTableDeletedValueType)
+        : m_string(WTF::HashTableDeletedValue) { }
+    friend bool operator==(const PublicSuffix&, const PublicSuffix&) = default;
+    bool operator==(ASCIILiteral other) const { return m_string == other; }
+    bool isHashTableDeletedValue() const { return m_string.isHashTableDeletedValue(); }
+    unsigned hash() const { return m_string.hash(); }
+    struct PublicSuffixHash {
+        static unsigned hash(const PublicSuffix& publicSuffix) { return ASCIICaseInsensitiveHash::hash(publicSuffix.m_string.impl()); }
+        static bool equal(const PublicSuffix& a, const PublicSuffix& b) { return equalIgnoringASCIICase(a.string(), b.string()); }
+        static const bool safeToCompareToEmptyOrDeleted = false;
+    };
+
+private:
+    explicit PublicSuffix(String&& string) : m_string(WTFMove(string)) { }
+
+    String m_string;
+};
+
+} // namespace WebCore
+
+namespace WTF {
+
+template<> struct DefaultHash<WebCore::PublicSuffix> : WebCore::PublicSuffix::PublicSuffixHash { };
+template<> struct HashTraits<WebCore::PublicSuffix> : SimpleClassHashTraits<WebCore::PublicSuffix> { };
+
+} // namespace WTF

Source/WebCore/platform/PublicSuffixStore.cpp
@@ -52,7 +52,7 @@ bool PublicSuffixStore::isPublicSuffix(StringView domain) const
     return platformIsPublicSuffix(domain);
 }
 
-String PublicSuffixStore::publicSuffix(const URL& url) const
+PublicSuffix PublicSuffixStore::publicSuffix(const URL& url) const
 {
     if (!url.isValid())
         return { };
@@ -65,7 +65,7 @@ String PublicSuffixStore::publicSuffix(const URL& url) const
     for (unsigned labelStart = 0; (separatorPosition = host.find('.', labelStart)) != notFound; labelStart = separatorPosition + 1) {
         auto candidate = host.substring(separatorPosition + 1);
         if (isPublicSuffix(candidate))
-            return candidate.toString();
+            return PublicSuffix::fromRawString(candidate.toString());
     }
 
     return { };

Source/WebCore/platform/PublicSuffixStore.h
@@ -25,6 +25,7 @@
 
 #pragma once
 
+#include "PublicSuffix.h"
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
 #include <wtf/text/StringHash.h>
@@ -38,14 +39,13 @@ public:
 
     // https://url.spec.whatwg.org/#host-public-suffix
     WEBCORE_EXPORT bool isPublicSuffix(StringView domain) const;
-    WEBCORE_EXPORT String publicSuffix(const URL&) const;
+    WEBCORE_EXPORT PublicSuffix publicSuffix(const URL&) const;
     WEBCORE_EXPORT String topPrivatelyControlledDomain(StringView host) const;
     WEBCORE_EXPORT void clearHostTopPrivatelyControlledDomainCache();
 
 #if PLATFORM(COCOA)
-    enum class CanAcceptCustomPublicSuffix : bool { No, Yes };
-    WEBCORE_EXPORT void enablePublicSuffixCache(CanAcceptCustomPublicSuffix = CanAcceptCustomPublicSuffix::No);
-    WEBCORE_EXPORT void addPublicSuffix(const String& publicSuffix);
+    WEBCORE_EXPORT void enablePublicSuffixCache();
+    WEBCORE_EXPORT void addPublicSuffix(const PublicSuffix&);
 #endif
 
 private:
@@ -59,8 +59,7 @@ private:
     mutable HashMap<String, String, ASCIICaseInsensitiveHash> m_hostTopPrivatelyControlledDomainCache WTF_GUARDED_BY_LOCK(m_HostTopPrivatelyControlledDomainCacheLock);
 #if PLATFORM(COCOA)
     mutable Lock m_publicSuffixCacheLock;
-    std::optional<HashSet<String, ASCIICaseInsensitiveHash>> m_publicSuffixCache WTF_GUARDED_BY_LOCK(m_publicSuffixCacheLock);
-    bool m_canAcceptCustomPublicSuffix { false }; // Only used on the main thread.
+    std::optional<HashSet<PublicSuffix>> m_publicSuffixCache WTF_GUARDED_BY_LOCK(m_publicSuffixCacheLock);
 #endif
 };
 

Source/WebCore/platform/cocoa/PublicSuffixStoreCocoa.mm
@@ -42,8 +42,11 @@ bool PublicSuffixStore::platformIsPublicSuffix(StringView domain) const
     auto domainString = domain.toStringWithoutCopying();
     {
         Locker locker { m_publicSuffixCacheLock };
-        if (m_publicSuffixCache && m_publicSuffixCache->contains(domainString))
-            return true;
+        if (m_publicSuffixCache) {
+            auto publicSuffix = PublicSuffix::fromRawString(String { domainString });
+            if (m_publicSuffixCache->contains(publicSuffix))
+                return true;
+        }
     }
 
     return isPublicSuffixCF(domainString);
@@ -60,24 +63,21 @@ String PublicSuffixStore::platformTopPrivatelyControlledDomain(StringView host) 
     return { };
 }
 
-void PublicSuffixStore::enablePublicSuffixCache(CanAcceptCustomPublicSuffix canAcceptCustomPublicSuffix)
+void PublicSuffixStore::enablePublicSuffixCache()
 {
     RELEASE_ASSERT(isMainThread());
-
-    m_canAcceptCustomPublicSuffix = canAcceptCustomPublicSuffix == CanAcceptCustomPublicSuffix::Yes;
 
     Locker locker { m_publicSuffixCacheLock };
     ASSERT(!m_publicSuffixCache);
-    m_publicSuffixCache = HashSet<String, ASCIICaseInsensitiveHash> { };
+    m_publicSuffixCache = HashSet<PublicSuffix> { };
 }
 
-void PublicSuffixStore::addPublicSuffix(const String& publicSuffix)
+void PublicSuffixStore::addPublicSuffix(const PublicSuffix& publicSuffix)
 {
-    if (publicSuffix.isEmpty())
-        return;
-
     RELEASE_ASSERT(isMainThread());
-    RELEASE_ASSERT(m_canAcceptCustomPublicSuffix || isPublicSuffixCF(publicSuffix));
+
+    if (!publicSuffix.isValid())
+        return;
 
     Locker locker { m_publicSuffixCacheLock };
     ASSERT(m_publicSuffixCache);

Source/WebKit/Shared/GoToBackForwardItemParameters.h
@@ -30,6 +30,7 @@
 #include "WebsitePoliciesData.h"
 #include <WebCore/BackForwardItemIdentifier.h>
 #include <WebCore/FrameLoaderTypes.h>
+#include <WebCore/PublicSuffix.h>
 #include <WebCore/ShouldTreatAsContinuingLoad.h>
 #include <wtf/text/WTFString.h>
 
@@ -43,7 +44,7 @@ struct GoToBackForwardItemParameters {
     std::optional<WebsitePoliciesData> websitePolicies;
     bool lastNavigationWasAppInitiated;
     std::optional<NetworkResourceLoadIdentifier> existingNetworkResourceLoadIdentifierToResume;
-    String publicSuffix;
+    WebCore::PublicSuffix publicSuffix;
     SandboxExtension::Handle sandboxExtensionHandle;
 };
 

Source/WebKit/Shared/GoToBackForwardItemParameters.serialization.in
@@ -28,6 +28,6 @@
     std::optional<WebKit::WebsitePoliciesData> websitePolicies;
     bool lastNavigationWasAppInitiated;
     std::optional<WebKit::NetworkResourceLoadIdentifier> existingNetworkResourceLoadIdentifierToResume;
-    String publicSuffix;
+    WebCore::PublicSuffix publicSuffix;
     WebKit::SandboxExtensionHandle sandboxExtensionHandle;
 };

Source/WebKit/Shared/LoadParameters.h
@@ -33,6 +33,7 @@
 #include <WebCore/AdvancedPrivacyProtections.h>
 #include <WebCore/FrameIdentifier.h>
 #include <WebCore/FrameLoaderTypes.h>
+#include <WebCore/PublicSuffixStore.h>
 #include <WebCore/ResourceRequest.h>
 #include <WebCore/ShouldTreatAsContinuingLoad.h>
 #include <WebCore/SubstituteData.h>
@@ -51,7 +52,7 @@ using SandboxFlags = int;
 namespace WebKit {
 
 struct LoadParameters {
-    String publicSuffix;
+    WebCore::PublicSuffix publicSuffix;
 
     uint64_t navigationID { 0 };
     std::optional<WebCore::FrameIdentifier> frameIdentifier;

Source/WebKit/Shared/LoadParameters.serialization.in
@@ -25,7 +25,7 @@ enum class WebCore::LockBackForwardList : bool;
 enum class WebKit::NavigatingToAppBoundDomain : bool;
 
 [RValue] struct WebKit::LoadParameters {
-    String publicSuffix;
+    WebCore::PublicSuffix publicSuffix;
     uint64_t navigationID;
     std::optional<WebCore::FrameIdentifier> frameIdentifier;
     [EncodeRequestBody] WebCore::ResourceRequest request;

Source/WebKit/Shared/WebCoreArgumentCoders.serialization.in
@@ -8155,3 +8155,7 @@ using WebCore::IDBKeyPath = std::variant<String, Vector<String>>;
 using WebCore::ServiceWorkerOrClientData = std::variant<WebCore::ServiceWorkerData, WebCore::ServiceWorkerClientData>
 using WebCore::MediaPlayer::VideoFullscreenMode = uint32_t
 enum class WebCore::CrossOriginMode : bool
+
+[CreateUsing=fromRawString] class WebCore::PublicSuffix {
+    String string();
+};

Tools/TestWebKitAPI/Tests/WebCore/PublicSuffix.cpp
@@ -192,17 +192,17 @@ TEST_F(PublicSuffix, PublicSuffixCache)
     WTF::URL abExampleURL { "http://a.b.example.example"_s };
     auto& publicSuffixStore = PublicSuffixStore::singleton();
     EXPECT_FALSE(publicSuffixStore.isPublicSuffix("example.example"_s));
-    EXPECT_EQ(String("example"_s), publicSuffixStore.publicSuffix(abExampleURL));
+    EXPECT_EQ(String("example"_s), publicSuffixStore.publicSuffix(abExampleURL).string());
     // Non-cocoa platforms currently do not use public suffix cache for topPrivatelyControlledDomain().
     EXPECT_EQ(String("example.example"_s), publicSuffixStore.topPrivatelyControlledDomain("a.b.example.example"_s));
     EXPECT_FALSE(publicSuffixStore.isPublicSuffix(""_s));
 
-    publicSuffixStore.enablePublicSuffixCache(PublicSuffixStore::CanAcceptCustomPublicSuffix::Yes);
+    publicSuffixStore.enablePublicSuffixCache();
     publicSuffixStore.clearHostTopPrivatelyControlledDomainCache();
-    publicSuffixStore.addPublicSuffix("example.example"_s);
-    publicSuffixStore.addPublicSuffix(""_s);
+    publicSuffixStore.addPublicSuffix(WebCore::PublicSuffix::fromRawString("example.example"_s));
+    publicSuffixStore.addPublicSuffix(WebCore::PublicSuffix { });
     EXPECT_TRUE(publicSuffixStore.isPublicSuffix("example.example"_s));
-    EXPECT_EQ(String("example.example"_s), publicSuffixStore.publicSuffix(abExampleURL));
+    EXPECT_EQ(String("example.example"_s), publicSuffixStore.publicSuffix(abExampleURL).string());
     EXPECT_EQ(String("b.example.example"_s), publicSuffixStore.topPrivatelyControlledDomain("a.b.example.example"_s));
     EXPECT_FALSE(publicSuffixStore.isPublicSuffix(""_s));
 }