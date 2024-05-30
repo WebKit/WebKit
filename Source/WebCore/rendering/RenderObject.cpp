a2fd007b306c7afe6bee7a3789887c08432d1df2
Alan Baradlay <zalan@apple.com>
2024-05-30 12:14:58 -0700

    <input> field changes sizes briefly while typing https://bugs.webkit.org/show_bug.cgi?id=274904 <rdar://128298014>
    Reviewed by Antti Koivisto.
    
    * LayoutTests/fast/dynamic/input-box-inside-flex-grid-expected.html: Added.
    * LayoutTests/fast/dynamic/input-box-inside-flex-grid.html: Added.
    * Source/WebCore/rendering/RenderObject.cpp:
    (WebCore::objectIsRelayoutBoundary):
    
    Canonical link: https://commits.webkit.org/279536@main

--- Source/WebCore/rendering/RenderObject.cpp
+++ Source/WebCore/rendering/RenderObject.cpp
@@ -69,6 +69,7 @@
 #include "RenderSVGModelObject.h"
 #include "RenderScrollbarPart.h"
 #include "RenderTableRow.h"
+#include "RenderTextControl.h"
 #include "RenderTheme.h"
 #include "RenderTreeBuilder.h"
 #include "RenderView.h"
@@ -535,8 +536,12 @@ static inline bool objectIsRelayoutBoundary(const RenderElement* object)
     if (object->isRenderView())
         return true;
 
-    if (object->isRenderTextControl())
-        return true;
+    if (auto* textControl = dynamicDowncast<RenderTextControl>(*object)) {
+        if (!textControl->isFlexItem() && !textControl->isGridItem()) {
+            // Flexing type of layout systems may compute different size than what input's preferred width is which won't happen unless they run their layout as well.
+            return true;
+        }
+    }
 
     if (object->shouldApplyLayoutContainment() && object->shouldApplySizeContainment())
         return true;
