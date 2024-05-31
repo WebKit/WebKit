4b149650aebe9972cad909d83279d198dda6cb47
Philippe Normand <philn@igalia.com>
2024-05-31 00:45:48 -0700

    [GStreamer][MediaRecorder] Pipeline remains in global registry if stopRecording is called https://bugs.webkit.org/show_bug.cgi?id=274893
    Reviewed by Xabier Rodriguez-Calvar.
    
    Clearing the GStreamer pipeline without un-registering it from the global registry would result in a
    memory leak.
    
    * Source/WebCore/platform/mediarecorder/MediaRecorderPrivateGStreamer.cpp:
    (WebCore::MediaRecorderPrivateBackend::stopRecording):
    
    Canonical link: https://commits.webkit.org/279567@main

--- Source/WebCore/platform/mediarecorder/MediaRecorderPrivateGStreamer.cpp
+++ Source/WebCore/platform/mediarecorder/MediaRecorderPrivateGStreamer.cpp
@@ -138,6 +138,7 @@ void MediaRecorderPrivateBackend::stopRecording(CompletionHandler<void()>&& comp
     GST_DEBUG_OBJECT(m_transcoder.get(), "Stop requested, pushing EOS event");
 
     auto scopeExit = makeScopeExit([this, completionHandler = WTFMove(completionHandler)]() mutable {
+        unregisterPipeline(m_pipeline);
         m_pipeline.clear();
         GST_DEBUG_OBJECT(m_transcoder.get(), "Stopping");
         m_transcoder.clear();
