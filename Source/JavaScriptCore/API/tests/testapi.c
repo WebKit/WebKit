WebKit/WebKit

commit f9a7aaf14df9fe74f266abc8fbb70cf5201e3b00

JSStringGetUTF8CString writes beyond the provided buffer size https://bugs.webkit.org/show_bug.cgi?id=275073 rdar://122388595
Reviewed by Yusuke Suzuki.

Patch created by Justin Michaud (referring to the original commit in the last line).
ToT already fixed the issue. Only landing the test (with newer semantics adjustment) is necessary.

* Source/JavaScriptCore/API/tests/testapi.c:
(checkJSStringOOBUTF8):
(checkJSStringOOBUTF16):
(checkJSStringOOBUTF16AtEnd):
(checkJSStringOOB):
(main):

Originally-landed-as: 272448.625@safari-7618-branch (800c12a28dea). rdar://128091153
Canonical link: https://commits.webkit.org/279673@main

Source/JavaScriptCore/API/tests/testapi.c
@@ -1190,6 +1190,117 @@ void JSSynchronousGarbageCollectForDebugging(JSContextRef);
 }
 #endif
 
+static void checkJSStringOOBUTF8(void)
+{
+    const size_t sourceCStringSize = 200;
+    const size_t outCStringSize = 10;
+
+    char sourceCString[sourceCStringSize];
+    memset(sourceCString, 0, sizeof(sourceCString));
+    for (size_t i = 0; i < sourceCStringSize - 1; ++i)
+        sourceCString[i] = '0' + (i%10);
+
+    char outCString[outCStringSize + sourceCStringSize];
+    memset(outCString, 0x13, sizeof(outCString));
+
+    JSStringRef str = JSStringCreateWithUTF8CString(sourceCString);
+    size_t bytesWritten = JSStringGetUTF8CString(str, outCString, outCStringSize);
+
+    assertTrue(bytesWritten == 10, "we report 10 bytes written precisely");
+
+    for (size_t i = 0; i < sizeof(outCString); ++i) {
+        if (i == outCStringSize - 1)
+            assertTrue(outCString[i] == '\0', "string terminated");
+        else if (i < outCStringSize - 1)
+            assertTrue(outCString[i] == sourceCString[i], "string copied");
+        else
+            assertTrue(outCString[i] == 0x13, "did not write past the end");
+    }
+
+    JSStringRelease(str);
+}
+
+static void checkJSStringOOBUTF16(void)
+{
+    const size_t sourceCStringSize = 22;
+    const size_t outCStringSize = 20;
+
+    char sourceCString[sourceCStringSize];
+    memset(sourceCString, 0, sizeof(sourceCString));
+    for (size_t i = 0; i < sourceCStringSize - 1; ++i)
+        sourceCString[i] = '0' + (i%10);
+
+    sourceCString[3] = '\xF0';
+    sourceCString[4] = '\x9F';
+    sourceCString[5] = '\x98';
+    sourceCString[6] = '\x81';
+
+    char outCString[outCStringSize + sourceCStringSize];
+    memset(outCString, 0x13, sizeof(outCString));
+
+    JSStringRef str = JSStringCreateWithUTF8CString(sourceCString);
+    size_t bytesWritten = JSStringGetUTF8CString(str, outCString, outCStringSize);
+
+    assertTrue(bytesWritten == 20, "we report 20 bytes written precisely");
+
+    for (size_t i = 0; i < sizeof(outCString); ++i) {
+        if (i == outCStringSize - 1)
+            assertTrue(outCString[i] == '\0', "string terminated");
+        else if (i < outCStringSize - 1)
+            assertTrue(outCString[i] == sourceCString[i], "string copied");
+        else
+            assertTrue(outCString[i] == 0x13, "did not write past the end");
+    }
+
+    JSStringRelease(str);
+}
+
+static void checkJSStringOOBUTF16AtEnd(void)
+{
+    const size_t sourceCStringSize = 22;
+    const size_t outCStringSize = 20;
+
+    char sourceCString[sourceCStringSize];
+    memset(sourceCString, 0, sizeof(sourceCString));
+    for (size_t i = 0; i < sourceCStringSize - 1; ++i)
+        sourceCString[i] = '0' + (i%10);
+
+    sourceCString[17] = '\xF0';
+    sourceCString[18] = '\x9F';
+    sourceCString[19] = '\x98';
+    sourceCString[20] = '\x81';
+
+    char outCString[outCStringSize + sourceCStringSize];
+    memset(outCString, 0x13, sizeof(outCString));
+
+    JSStringRef str = JSStringCreateWithUTF8CString(sourceCString);
+    size_t bytesWritten = JSStringGetUTF8CString(str, outCString, outCStringSize);
+
+    assertTrue(bytesWritten == 18, "we report 18 bytes written precisely");
+
+    for (size_t i = 0; i < sizeof(outCString); ++i) {
+        if (i == 17)
+            assertTrue(outCString[i] == '\0', "string terminated");
+        else if (i < 17)
+            assertTrue(outCString[i] == sourceCString[i], "string copied");
+        else
+            assertTrue(outCString[i] == 0x13, "did not write past the end");
+    }
+
+    JSStringRelease(str);
+}
+
+static void checkJSStringOOB(void)
+{
+    printf("Test: checkJSStringOOB\n");
+    checkJSStringOOBUTF8();
+    printf(".\n");
+    checkJSStringOOBUTF16();
+    printf(".\n");
+    checkJSStringOOBUTF16AtEnd();
+    printf("PASS: checkJSStringOOB\n");
+}
+
 static const unsigned numWeakRefs = 10000;
 
 static void markingConstraint(JSMarkerRef marker, void *userData)
@@ -1763,7 +1874,7 @@ int main(int argc, char* argv[])
     assertEqualsAsCharactersPtr(jsOneThird, "0.3333333333333333");
     assertEqualsAsCharactersPtr(jsEmptyString, "");
     assertEqualsAsCharactersPtr(jsOneString, "1");
-    
+
     assertEqualsAsUTF8String(jsUndefined, "undefined");
     assertEqualsAsUTF8String(jsNull, "null");
     assertEqualsAsUTF8String(jsTrue, "true");
@@ -1773,9 +1884,11 @@ int main(int argc, char* argv[])
     assertEqualsAsUTF8String(jsOneThird, "0.3333333333333333");
     assertEqualsAsUTF8String(jsEmptyString, "");
     assertEqualsAsUTF8String(jsOneString, "1");
-    
+
+    checkJSStringOOB();
+
     checkConstnessInJSObjectNames();
-    
+
     ASSERT(JSValueIsStrictEqual(context, jsTrue, jsTrue));
     ASSERT(!JSValueIsStrictEqual(context, jsOne, jsOneString));
 

commit b39622eb0b6b8c1d0b248d189986100521b139e7

Enable -Wvla when building WebKit <https://bugs.webkit.org/show_bug.cgi?id=275081> <rdar://129190911>
Reviewed by Yusuke Suzuki.

* Configurations/CommonBase.xcconfig:
(WK_COMMON_WARNING_CFLAGS): Add -Wvla.
* Source/JavaScriptCore/API/tests/testapi.c:
(checkJSStringOOBUTF8):
(checkJSStringOOBUTF16):
(checkJSStringOOBUTF16AtEnd):
- Stop using VLAs.  Since this is C, we use malloc()/free() instead.
  Regressed recently in 279673@main.
* Source/ThirdParty/libwebrtc/Configurations/Base.xcconfig:
(WK_FIXME_WARNING_CFLAGS): Add -Wno-vla.
- The opus project and one pffft source file use VLAs.
* Tools/DumpRenderTree/cg/PixelDumpSupportCG.cpp:
(computeSHA1HashStringForBitmapContext):
- Use a Vector<uint32_t> instead of a VLA.
* Tools/DumpRenderTree/mac/AccessibilityNotificationHandler.mm:
(makeArrayRefForArray):
- Ignore -Wvla warnings for JSValueRef objects that must be allocated on
  the stack.
* Tools/WebKitTestRunner/InjectedBundle/cocoa/AccessibilityCommonCocoa.mm:
(WTR::makeJSArray): Ditto.
* Tools/WebKitTestRunner/ios/HIDEventGenerator.mm:
(-[HIDEventGenerator touchDown:touchCount:]):
(-[HIDEventGenerator liftUp:touchCount:]):
(-[HIDEventGenerator moveToPoints:touchCount:duration:]):
- Use a Vector<CGPoint> instead of a VLA.

Canonical link: https://commits.webkit.org/279702@main

Configurations/CommonBase.xcconfig
@@ -55,7 +55,7 @@ OTHER_LDFLAGS = $(inherited) $(WK_COMMON_OTHER_LDFLAGS);
 WK_COMMON_OTHER_TAPI_FLAGS = -x objective-c++ -std=c++2a -fno-rtti $(WK_SANITIZER_OTHER_TAPI_FLAGS);
 OTHER_TAPI_FLAGS = $(inherited) $(WK_COMMON_OTHER_TAPI_FLAGS);
 
-WK_COMMON_WARNING_CFLAGS = -Wall -Wc99-designator -Wconditional-uninitialized -Wextra -Wdeprecated-enum-enum-conversion -Wdeprecated-enum-float-conversion -Wenum-float-conversion -Wfinal-dtor-non-final-class -Wformat=2 -Wmisleading-indentation -Wreorder-init-list -Wundef;
+WK_COMMON_WARNING_CFLAGS = -Wall -Wc99-designator -Wconditional-uninitialized -Wextra -Wdeprecated-enum-enum-conversion -Wdeprecated-enum-float-conversion -Wenum-float-conversion -Wfinal-dtor-non-final-class -Wformat=2 -Wmisleading-indentation -Wreorder-init-list -Wundef -Wvla;
 WARNING_CFLAGS = $(inherited) $(WK_COMMON_WARNING_CFLAGS) $(WK_SANITIZER_WARNING_CFLAGS);
 
 CLANG_CXX_STANDARD_LIBRARY_HARDENING = extensive;

Source/ThirdParty/libwebrtc/Configurations/Base.xcconfig
@@ -77,7 +77,7 @@ WARNING_CFLAGS = $(inherited) -Wthread-safety $(WK_FIXME_WARNING_CFLAGS) -Wexit-
 
 // Remove WK_FIXME_WARNING_CFLAGS once all warnings are fixed.
 // -Wno-unknown-warning-option added for -Wno-unused-but-set-parameter.
-WK_FIXME_WARNING_CFLAGS = -Wno-conditional-uninitialized -Wno-missing-field-initializers -Wno-sign-compare -Wno-undef -Wno-unknown-warning-option -Wno-unused-but-set-parameter -Wno-unused-parameter -Wno-array-parameter -Wno-unused-but-set-variable -Wno-thread-safety-reference-return;
+WK_FIXME_WARNING_CFLAGS = -Wno-conditional-uninitialized -Wno-missing-field-initializers -Wno-sign-compare -Wno-undef -Wno-unknown-warning-option -Wno-unused-but-set-parameter -Wno-unused-parameter -Wno-array-parameter -Wno-unused-but-set-variable -Wno-thread-safety-reference-return -Wno-vla;
 
 ENTITLEMENTS_REQUIRED = $(ENTITLEMENTS_REQUIRED_USE_INTERNAL_SDK_$(USE_INTERNAL_SDK))
 ENTITLEMENTS_REQUIRED_USE_INTERNAL_SDK_ = NO;

Tools/DumpRenderTree/cg/PixelDumpSupportCG.cpp
@@ -40,6 +40,7 @@
 #include <wtf/RefPtr.h>
 #include <wtf/RetainPtr.h>
 #include <wtf/SHA1.h>
+#include <wtf/Vector.h>
 
 #if PLATFORM(IOS_FAMILY)
 #include <MobileCoreServices/UTCoreTypes.h>
@@ -89,10 +90,10 @@ void computeSHA1HashStringForBitmapContext(BitmapContext* context, char hashStri
     unsigned char* bitmapData = static_cast<unsigned char*>(CGBitmapContextGetData(bitmapContext));
     if ((CGBitmapContextGetBitmapInfo(bitmapContext) & kCGBitmapByteOrderMask) == kCGBitmapByteOrder32Big) {
         for (unsigned row = 0; row < pixelsHigh; row++) {
-            uint32_t buffer[pixelsWide];
+            Vector<uint32_t> buffer(pixelsWide);
             for (unsigned column = 0; column < pixelsWide; column++)
                 buffer[column] = OSReadLittleInt32(bitmapData, 4 * column);
-            sha1.addBytes(std::span { reinterpret_cast<const uint8_t*>(buffer), 4 * pixelsWide });
+            sha1.addBytes(asBytes(buffer.span()));
             bitmapData += bytesPerRow;
         }
     } else {

Tools/DumpRenderTree/mac/AccessibilityNotificationHandler.mm
@@ -122,7 +122,9 @@ static JSValueRef makeValueRefForValue(JSContextRef context, id value)
 static JSObjectRef makeArrayRefForArray(JSContextRef context, NSArray *array)
 {
     NSUInteger count = array.count;
+IGNORE_WARNINGS_BEGIN("vla")
     JSValueRef arguments[count];
+IGNORE_WARNINGS_END
     for (NSUInteger i = 0; i < count; i++)
         arguments[i] = makeValueRefForValue(context, [array objectAtIndex:i]);
     return JSObjectMakeArray(context, count, arguments, nullptr);

Tools/WebKitTestRunner/InjectedBundle/cocoa/AccessibilityCommonCocoa.mm
@@ -66,7 +66,9 @@ Class webAccessibilityObjectWrapperClass()
 JSObjectRef makeJSArray(JSContextRef context, NSArray *array)
 {
     NSUInteger count = array.count;
+IGNORE_WARNINGS_BEGIN("vla")
     JSValueRef arguments[count];
+IGNORE_WARNINGS_END
     for (NSUInteger i = 0; i < count; i++)
         arguments[i] = makeValueRefForValue(context, [array objectAtIndex:i]);
     return JSObjectMakeArray(context, count, arguments, nullptr);

Tools/WebKitTestRunner/ios/HIDEventGenerator.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2015-2024 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,6 +34,7 @@
 #import <wtf/Assertions.h>
 #import <wtf/BlockPtr.h>
 #import <wtf/SoftLinking.h>
+#import <wtf/Vector.h>
 
 SOFT_LINK_PRIVATE_FRAMEWORK(BackBoardServices)
 SOFT_LINK(BackBoardServices, BKSHIDEventSetDigitizerInfo, void, (IOHIDEventRef digitizerEvent, uint32_t contextID, uint8_t systemGestureisPossible, uint8_t isSystemGestureStateChangeEvent, CFStringRef displayUUID, CFTimeInterval initialTouchTimestamp, float maxForce), (digitizerEvent, contextID, systemGestureisPossible, isSystemGestureStateChangeEvent, displayUUID, initialTouchTimestamp, maxForce));
@@ -610,12 +611,12 @@ static InterpolationType interpolationFromString(NSString *string)
 {
     touchCount = std::min(touchCount, HIDMaxTouchCount);
 
-    CGPoint locations[touchCount];
+    Vector<CGPoint> locations(touchCount);
 
     for (NSUInteger index = 0; index < touchCount; ++index)
         locations[index] = location;
     
-    [self touchDownAtPoints:locations touchCount:touchCount];
+    [self touchDownAtPoints:&locations[0] touchCount:touchCount];
 }
 
 - (void)touchDown:(CGPoint)location
@@ -646,12 +647,12 @@ static InterpolationType interpolationFromString(NSString *string)
 {
     touchCount = std::min(touchCount, HIDMaxTouchCount);
 
-    CGPoint locations[touchCount];
+    Vector<CGPoint> locations(touchCount);
 
     for (NSUInteger index = 0; index < touchCount; ++index)
         locations[index] = location;
     
-    [self liftUpAtPoints:locations touchCount:touchCount];
+    [self liftUpAtPoints:&locations[0] touchCount:touchCount];
 }
 
 - (void)liftUp:(CGPoint)location
@@ -663,8 +664,8 @@ static InterpolationType interpolationFromString(NSString *string)
 {
     touchCount = std::min(touchCount, HIDMaxTouchCount);
 
-    CGPoint startLocations[touchCount];
-    CGPoint nextLocations[touchCount];
+    Vector<CGPoint> startLocations(touchCount);
+    Vector<CGPoint> nextLocations(touchCount);
 
     CFAbsoluteTime startTime = CFAbsoluteTimeGetCurrent();
     CFTimeInterval elapsed = 0;
@@ -680,7 +681,7 @@ static InterpolationType interpolationFromString(NSString *string)
 
             nextLocations[i] = calculateNextCurveLocation(startLocations[i], newLocations[i], interval);
         }
-        [self _updateTouchPoints:nextLocations count:touchCount];
+        [self _updateTouchPoints:&nextLocations[0] count:touchCount];
 
         delayBetweenMove(eventIndex++, elapsed);
     }

Source/JavaScriptCore/API/tests/testapi.c
@@ -1195,13 +1195,13 @@ static void checkJSStringOOBUTF8(void)
     const size_t sourceCStringSize = 200;
     const size_t outCStringSize = 10;
 
-    char sourceCString[sourceCStringSize];
-    memset(sourceCString, 0, sizeof(sourceCString));
+    char* sourceCString = (char*)malloc(sourceCStringSize);
+    memset(sourceCString, 0, sourceCStringSize);
     for (size_t i = 0; i < sourceCStringSize - 1; ++i)
         sourceCString[i] = '0' + (i%10);
 
-    char outCString[outCStringSize + sourceCStringSize];
-    memset(outCString, 0x13, sizeof(outCString));
+    char* outCString = (char*)malloc(outCStringSize + sourceCStringSize);
+    memset(outCString, 0x13, outCStringSize + sourceCStringSize);
 
     JSStringRef str = JSStringCreateWithUTF8CString(sourceCString);
     size_t bytesWritten = JSStringGetUTF8CString(str, outCString, outCStringSize);
@@ -1218,6 +1218,8 @@ static void checkJSStringOOBUTF8(void)
     }
 
     JSStringRelease(str);
+    free(outCString);
+    free(sourceCString);
 }
 
 static void checkJSStringOOBUTF16(void)
@@ -1225,8 +1227,8 @@ static void checkJSStringOOBUTF16(void)
     const size_t sourceCStringSize = 22;
     const size_t outCStringSize = 20;
 
-    char sourceCString[sourceCStringSize];
-    memset(sourceCString, 0, sizeof(sourceCString));
+    char* sourceCString = (char*)malloc(sourceCStringSize);
+    memset(sourceCString, 0, sourceCStringSize);
     for (size_t i = 0; i < sourceCStringSize - 1; ++i)
         sourceCString[i] = '0' + (i%10);
 
@@ -1235,8 +1237,8 @@ static void checkJSStringOOBUTF16(void)
     sourceCString[5] = '\x98';
     sourceCString[6] = '\x81';
 
-    char outCString[outCStringSize + sourceCStringSize];
-    memset(outCString, 0x13, sizeof(outCString));
+    char* outCString = (char*)malloc(outCStringSize + sourceCStringSize);
+    memset(outCString, 0x13, outCStringSize + sourceCStringSize);
 
     JSStringRef str = JSStringCreateWithUTF8CString(sourceCString);
     size_t bytesWritten = JSStringGetUTF8CString(str, outCString, outCStringSize);
@@ -1253,6 +1255,8 @@ static void checkJSStringOOBUTF16(void)
     }
 
     JSStringRelease(str);
+    free(outCString);
+    free(sourceCString);
 }
 
 static void checkJSStringOOBUTF16AtEnd(void)
@@ -1260,8 +1264,8 @@ static void checkJSStringOOBUTF16AtEnd(void)
     const size_t sourceCStringSize = 22;
     const size_t outCStringSize = 20;
 
-    char sourceCString[sourceCStringSize];
-    memset(sourceCString, 0, sizeof(sourceCString));
+    char* sourceCString = (char*)malloc(sourceCStringSize);
+    memset(sourceCString, 0, sourceCStringSize);
     for (size_t i = 0; i < sourceCStringSize - 1; ++i)
         sourceCString[i] = '0' + (i%10);
 
@@ -1270,8 +1274,8 @@ static void checkJSStringOOBUTF16AtEnd(void)
     sourceCString[19] = '\x98';
     sourceCString[20] = '\x81';
 
-    char outCString[outCStringSize + sourceCStringSize];
-    memset(outCString, 0x13, sizeof(outCString));
+    char* outCString = (char*)malloc(outCStringSize + sourceCStringSize);
+    memset(outCString, 0x13, outCStringSize + sourceCStringSize);
 
     JSStringRef str = JSStringCreateWithUTF8CString(sourceCString);
     size_t bytesWritten = JSStringGetUTF8CString(str, outCString, outCStringSize);
@@ -1288,6 +1292,8 @@ static void checkJSStringOOBUTF16AtEnd(void)
     }
 
     JSStringRelease(str);
+    free(outCString);
+    free(sourceCString);
 }
 
 static void checkJSStringOOB(void)