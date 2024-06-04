e3d83fa746241a811bbe944cf7dc437e5599871b
Adrian Perez de Castro <aperez@igalia.com>
2024-06-04 18:13:37 +0200

    [CMake][GLib] Symbol WebKit::GPUProcessMain should be public https://bugs.webkit.org/show_bug.cgi?id=275110
    Reviewed by NOBODY (OOPS!).
    
    Add a new GENERATE_LINKER_SCRIPT() CMake function which sets up rules to
    process a template file through unifdef, sets the needed linker flags,
    and arranges dependencies to workaround the fact that CMake does not
    materialize build rules for files listed in a target's LINK_DEPENDS
    property. This allows to set WebKit::GPUProcessMain() as public with
    ENABLE_GPU_PROCESS=ON.
    
    * Source/WebKit/PlatformGTK.cmake: Use GENERATE_LINKER_SCRIPT().
    * Source/WebKit/PlatformWPE.cmake: Ditto.
    * Source/WebKit/webkitglib-symbols.map.in: Renamed from Source/WebKit/webkitglib-symbols.map.
    * Source/cmake/GLibMacros.cmake: Add GENERATE_LINKER_SCRIPT() function.

--- Source/cmake/GLibMacros.cmake
+++ Source/cmake/GLibMacros.cmake
@@ -13,3 +13,32 @@ macro(GENERATE_GLIB_API_HEADERS _framework _input_header_templates _derived_sour
 
     list(APPEND ${_framework}_SOURCES ${${_output_headers}})
 endmacro()
+
+function(GENERATE_LINKER_SCRIPT target template)
+    if (DEVELOPER_MODE OR CMAKE_SYSTEM_NAME MATCHES "Darwin")
+        return()
+    endif ()
+
+    set(linker_script_target "${target}-linker-script")
+    set(linker_script "${CMAKE_CURRENT_BINARY_DIR}/${target}-symbols.map")
+
+    add_custom_command(
+        OUTPUT "${linker_script}"
+        DEPENDS "${template}"
+        COMMAND "${UNIFDEF_EXECUTABLE}" -x1 -t "-o${linker_script}"
+            ${ARGN} "${template}"
+        VERBATIM
+    )
+    target_link_options(${target} PRIVATE "-Wl,--version-script,${linker_script}")
+
+    #
+    # There is a LINK_DEPENDS target property, but the files listed do not
+    # nudge CMake into materializing the build rules for custom commands;
+    # it only works with preexisting files. Workaround by adding a dummy
+    # custom target and depend on that to force the rules to be generated.
+    #
+    # See https://discourse.cmake.org/t/link-depends-on-output-of-add-custom-command-no-rule-to-make-target/10179
+    #
+    add_custom_target(${linker_script_target} DEPENDS "${linker_script}")
+    add_dependencies(${target} ${linker_script_target})
+endfunction()
