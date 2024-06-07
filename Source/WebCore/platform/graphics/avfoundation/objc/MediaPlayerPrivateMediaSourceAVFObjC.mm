WebKit/WebKit

commit 02bb8a509f08174d0673b37f1ceef03cfd689824

[Cocoa][MSE] Fix canvas painting on older builds https://bugs.webkit.org/show_bug.cgi?id=243163 <rdar://97532406>
Reviewed by Eric Carlson and Wenson Hsieh.

After 252183@main, an alternate path was provided for older builds that
didn't support AVSBDL `-copyDisplayedPixelBuffer` by calling `willBeAskedToPaintGL`
before a canvas paint is attempted. The problem is that the location in
which `willBeAskedToPaintGL` was called is too late since valid frame metadata
is checked before `willBeAskedToPaintGL` is reached, however, on older builds
this method needs to be called in order to valid frame metadata to be generated.

The fix is to simply enable the decompression session if frame metadata
is being requested and notify WebGL is about to paint for the SW path as well.

* Source/WebCore/html/canvas/WebGLRenderingContextBase.cpp:
(WebCore::WebGLRenderingContextBase::texImageSourceHelper):
* Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateMediaSourceAVFObjC.mm:
(WebCore::MediaPlayerPrivateMediaSourceAVFObjC::shouldEnsureLayer const):
(WebCore::MediaPlayerPrivateMediaSourceAVFObjC::startVideoFrameMetadataGathering):
(WebCore::MediaPlayerPrivateMediaSourceAVFObjC::stopVideoFrameMetadataGathering):
* Source/WebCore/platform/graphics/cocoa/MediaPlayerPrivateWebM.mm:
(WebCore::MediaPlayerPrivateWebM::shouldEnsureLayer const):
(WebCore::MediaPlayerPrivateWebM::startVideoFrameMetadataGathering):
(WebCore::MediaPlayerPrivateWebM::stopVideoFrameMetadataGathering):

* LayoutTests/platform/mac/TestExpectations:

Canonical link: https://commits.webkit.org/252893@main

Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateMediaSourceAVFObjC.mm
@@ -788,7 +788,7 @@ bool MediaPlayerPrivateMediaSourceAVFObjC::shouldEnsureLayer() const
         return !m_player->playerContentBoxRect().isEmpty();
     }();
 #else
-    return !m_hasBeenAskedToPaintGL;
+    return !m_hasBeenAskedToPaintGL && !m_isGatheringVideoFrameMetadata;
 #endif
 }
 
@@ -1446,6 +1446,7 @@ void MediaPlayerPrivateMediaSourceAVFObjC::startVideoFrameMetadataGathering()
 {
     ASSERT(!m_videoFrameMetadataGatheringObserver || m_synchronizer);
     m_isGatheringVideoFrameMetadata = true;
+    acceleratedRenderingStateChanged();
 
     // FIXME: We should use a CADisplayLink to get updates on rendering, for now we emulate with addPeriodicTimeObserverForInterval.
     m_videoFrameMetadataGatheringObserver = [m_synchronizer addPeriodicTimeObserverForInterval:PAL::CMTimeMake(1, 60) queue:dispatch_get_main_queue() usingBlock:[weakThis = WeakPtr { *this }](CMTime currentTime) {
@@ -1477,6 +1478,7 @@ void MediaPlayerPrivateMediaSourceAVFObjC::checkNewVideoFrameMetadata(CMTime cur
 void MediaPlayerPrivateMediaSourceAVFObjC::stopVideoFrameMetadataGathering()
 {
     m_isGatheringVideoFrameMetadata = false;
+    acceleratedRenderingStateChanged();
     m_videoFrameMetadata = { };
 
     ASSERT(m_videoFrameMetadataGatheringObserver);

LayoutTests/platform/mac/TestExpectations
@@ -2278,9 +2278,6 @@ webkit.org/b/240989 http/tests/media/hls/hls-webvtt-flashing.html [ Pass Failure
 
 webkit.org/b/242249 [ BigSur+ ] webanimations/accelerated-animation-after-forward-filling-animation.html [ Pass ImageOnlyFailure ]
 
-# Requires platform support (see rdar://94324932).
-[ BigSur Monterey ] http/tests/media/media-source/mediasource-rvfc.html [ Timeout ]
-
 webkit.org/b/242481 fast/css/direct-adjacent-style-sharing-3.html [ Pass ImageOnlyFailure ]
 
 webkit.org/b/242484 fast/css/display-contents-all.html [ Pass ImageOnlyFailure ]

Source/WebCore/html/canvas/WebGLRenderingContextBase.cpp
@@ -5164,6 +5164,10 @@ ExceptionOr<void> WebGLRenderingContextBase::texImageSourceHelper(TexImageFuncti
         // elements' size for WebGL 1.0 as late as possible.
         bool sourceImageRectIsDefault = inputSourceImageRect == sentinelEmptyRect() || inputSourceImageRect == IntRect(0, 0, video->videoWidth(), video->videoHeight());
 
+#if PLATFORM(COCOA) && !HAVE(AVSAMPLEBUFFERDISPLAYLAYER_COPYDISPLAYEDPIXELBUFFER)
+        if (auto player = video->player())
+            player->willBeAskedToPaintGL();
+#endif
         // Go through the fast path doing a GPU-GPU textures copy without a readback to system memory if possible.
         // Otherwise, it will fall back to the normal SW path.
         // FIXME: The current restrictions require that format shoud be RGB or RGBA,
@@ -5173,9 +5177,6 @@ ExceptionOr<void> WebGLRenderingContextBase::texImageSourceHelper(TexImageFuncti
             && type == GraphicsContextGL::UNSIGNED_BYTE
             && !level) {
             if (auto player = video->player()) {
-#if PLATFORM(COCOA) && !HAVE(AVSAMPLEBUFFERDISPLAYLAYER_COPYDISPLAYEDPIXELBUFFER)
-                player->willBeAskedToPaintGL();
-#endif
                 if (m_context->copyTextureFromMedia(*player, texture->object(), target, level, internalformat, format, type, m_unpackPremultiplyAlpha, m_unpackFlipY)) {
 #if !USE(ANGLE)
                     texture->setLevelInfo(target, level, internalformat, video->videoWidth(), video->videoHeight(), type);

Source/WebCore/platform/graphics/cocoa/MediaPlayerPrivateWebM.mm
@@ -610,7 +610,7 @@ bool MediaPlayerPrivateWebM::shouldEnsureLayer() const
         && ((m_displayLayer && !CGRectIsEmpty([m_displayLayer bounds]))
             || !m_player->playerContentBoxRect().isEmpty());
 #else
-    return !m_hasBeenAskedToPaintGL;
+    return !m_hasBeenAskedToPaintGL && !m_isGatheringVideoFrameMetadata;
 #endif
 }
 
@@ -1394,6 +1394,7 @@ void MediaPlayerPrivateWebM::startVideoFrameMetadataGathering()
 {
     ASSERT(!m_videoFrameMetadataGatheringObserver || m_synchronizer);
     m_isGatheringVideoFrameMetadata = true;
+    acceleratedRenderingStateChanged();
 
     // FIXME: We should use a CADisplayLink to get updates on rendering, for now we emulate with addPeriodicTimeObserverForInterval.
     m_videoFrameMetadataGatheringObserver = [m_synchronizer addPeriodicTimeObserverForInterval:PAL::CMTimeMake(1, 60) queue:dispatch_get_main_queue() usingBlock:[weakThis = WeakPtr { *this }](CMTime currentTime) {
@@ -1407,6 +1408,7 @@ void MediaPlayerPrivateWebM::startVideoFrameMetadataGathering()
 void MediaPlayerPrivateWebM::stopVideoFrameMetadataGathering()
 {
     m_isGatheringVideoFrameMetadata = false;
+    acceleratedRenderingStateChanged();
     m_videoFrameMetadata = { };
 
     ASSERT(m_videoFrameMetadataGatheringObserver);

commit bff029f2d8a30fbcd16d5d02dd9dd3cbfd18cb96

Cherry-pick 37d3475208d6. rdar://127264745
    [Monterey] Videos don't fully render in LinkedIn news feed
    https://bugs.webkit.org/show_bug.cgi?id=274812
    rdar://127264745

    Reviewed by Youenn Fablet.

    Video frames were not presented as when a requestVideoFrameCallback operation is pending we do
    not create the required AVSampleBufferDisplayLayer.
    This is a regression from 252893@main.
    On OS that do not support [AVSBDL copyDisplayedPixelBuffer] (Monterey and earlier), we create
    a WebCoreDecompressionSession in place of a AVSBDL to decode the video frames. But those frames
    are never rendered to screen. The (incorrect) assumption was that when using rVFC we would always
    use a canvas or WebGL context to paint the video frames.
    For now we revert the line introduced by 252893@main that while fixing rVFC broke playback.
    rVFC was broken anyway on Monterey and earlier (you never get the expected frame).

    No test, as we no longer have infrastructure in place to check on specific Monterey code path.

    * Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateMediaSourceAVFObjC.mm:
    (WebCore::MediaPlayerPrivateMediaSourceAVFObjC::shouldEnsureLayerOrVideoRenderer const):

    Canonical link: https://commits.webkit.org/279474@main

Canonical link: https://commits.webkit.org/272448.1075@safari-7618-branch

Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateMediaSourceAVFObjC.mm
@@ -811,7 +811,7 @@ bool MediaPlayerPrivateMediaSourceAVFObjC::shouldEnsureLayer() const
         return player && player->renderingCanBeAccelerated();
     }();
 #else
-    return !m_hasBeenAskedToPaintGL && !m_isGatheringVideoFrameMetadata;
+    return !m_hasBeenAskedToPaintGL;
 #endif
 }
 