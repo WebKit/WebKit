4f58dca1efb9e8012bc157eed7738e23b93a2d71
Kimmo Kinnunen <kkinnunen@apple.com>
2024-06-14 10:23:12 -0700

    Cherry-pick 280022@main (5554cf00d1f4). https://bugs.webkit.org/show_bug.cgi?id=275435
        UI process crash when network process connection request arrives before settings have been initialized
        https://bugs.webkit.org/show_bug.cgi?id=275435
        rdar://129587532
    
        Reviewed by Cameron McCormack.
    
        Mark sendSync of GetNetworkProcessConnection as
        MaintainOrderingWithAsyncMessages so that previous messages are
        guaranteed to be delivered. The previous messages might initialize the
        network process settings that are needed to establish the connection.
    
        * Source/WebKit/WebProcess/WebProcess.cpp:
        (WebKit::getNetworkProcessConnection):
    
        Canonical link: https://commits.webkit.org/280022@main
    
    Canonical link: https://commits.webkit.org/274313.362@webkitglib/2.44

--- Source/WebKit/WebProcess/WebProcess.cpp
+++ Source/WebKit/WebProcess/WebProcess.cpp
@@ -1180,7 +1180,7 @@ static NetworkProcessConnectionInfo getNetworkProcessConnection(IPC::Connection&
 {
     NetworkProcessConnectionInfo connectionInfo;
     auto requestConnection = [&]() -> bool {
-        auto sendResult = connection.sendSync(Messages::WebProcessProxy::GetNetworkProcessConnection(), 0);
+        auto sendResult = connection.sendSync(Messages::WebProcessProxy::GetNetworkProcessConnection(), 0, IPC::Timeout::infinity(), IPC::SendSyncOption::MaintainOrderingWithAsyncMessages);
         if (!sendResult.succeeded()) {
             RELEASE_LOG_ERROR(Process, "getNetworkProcessConnection: Failed to send message or receive invalid message: error %" PUBLIC_LOG_STRING, IPC::errorAsString(sendResult.error()));
             failedToGetNetworkProcessConnection();
