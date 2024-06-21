d8035e9cf316ae2ac0458fa0563e4f8354cd99ff
Jer Noble <jer.noble@apple.com>
2024-06-21 09:25:01 -0700

    [Cocoa] CRASH in HTMLMediaElement::loadResource() https://bugs.webkit.org/show_bug.cgi?id=275718 rdar://129570289
    Reviewed by Eric Carlson.
    
    From crashlogs, loadResource() is being called when m_player is null. This should not be possible,
    as the only site that clears m_player is clearMediaPlayer(), which cancels the task which calls
    loadResource().
    
    Null-check m_player, but leave an ASSERT_NOT_REACHED() in its place, to hopefully catch this state
    in debug builds and layout tests.
    
    * Source/WebCore/html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::loadResource):
    
    Canonical link: https://commits.webkit.org/280251@main

--- Source/WebCore/html/HTMLMediaElement.cpp
+++ Source/WebCore/html/HTMLMediaElement.cpp
@@ -1687,6 +1687,12 @@ void HTMLMediaElement::loadResource(const URL& initialURL, const ContentType& in
         return;
     }
 
+    if (!m_player) {
+        ASSERT_NOT_REACHED("It should not be possible to enter loadResource without a valid m_player object");
+        mediaLoadingFailed(MediaPlayer::NetworkState::FormatError);
+        return;
+    }
+
     URL url = initialURL;
 #if PLATFORM(COCOA)
     if (url.protocolIsFile() && !frame->loader().willLoadMediaElementURL(url, *this)) {
