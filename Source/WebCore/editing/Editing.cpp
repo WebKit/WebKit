4ebb97decaa7afc69c850ae5056bb9aa6642855f
Ahmad Saleem <52317531+Ahmad-S792@users.noreply.github.com>
2024-06-24 11:03:56 +0100

    document.execCommand('insertOrderedList'); can hang browser on removing list
    https://bugs.webkit.org/show_bug.cgi?id=258254
    rdar://problem/111272008
    
    Reviewed by NOBODY (OOPS!).
    
    Merge: https://chromium.googlesource.com/chromium/src.git/+/f15d4cc339737df653274e39726ae0d14d409b18
    
    While firstEditablePositionAfterPositionInRoot() is running,
    we can get the next editable paragraph under the highest editable
    node by skipping non-editable paragraph.
    
    However, if an editable paragraph has a non-editable child, the next
    paragraph can be the same editable paragraph that is the parent of the
    non-editable child. In this case, we can't get the next editable
    paragraph in InsertListCommand::doApply's while loop. As a result,
    the while loop can't break, which makes the tab hang.
    
    This patch allows to get the next editable paragraph by considering the
    non-editable child element.
    
    * Source/WebCore/editing/Editing.cpp:
    (WebCore::firstEditablePositionAfterPositionInRoot):
    * LayoutTests/editing/selection/insert-list-over-uneditable-in-contenteditable.html: Add Test Case
    * LayoutTests/editing/selection/insert-list-over-uneditable-in-contenteditable-expected.txt: Add Test Case Expectation

--- Source/WebCore/editing/Editing.cpp
+++ Source/WebCore/editing/Editing.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2004-2022 Apple Inc. All rights reserved.
- * Copyright (C) 2015 Google Inc. All rights reserved.
+ * Copyright (C) 2004-2024 Apple Inc. All rights reserved.
+ * Copyright (C) 2015-2016 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -275,12 +275,19 @@ Position firstEditablePositionAfterPositionInRoot(const Position& position, Cont
         candidate = positionAfterNode(shadowAncestor.get());
     }
 
-    while (candidate.deprecatedNode() && !isEditablePosition(candidate) && candidate.protectedDeprecatedNode()->isDescendantOf(*highestRoot))
+    Node* nonEditableNode = nullptr;
+    while (candidate.deprecatedNode() && !isEditablePosition(candidate) && candidate.protectedDeprecatedNode()->isDescendantOf(*highestRoot)) {
+        nonEditableNode = candidate.deprecatedNode();
         candidate = isAtomicNode(candidate.deprecatedNode()) ? positionInParentAfterNode(candidate.protectedDeprecatedNode().get()) : nextVisuallyDistinctCandidate(candidate);
+    }
 
     if (candidate.deprecatedNode() && candidate.deprecatedNode() != highestRoot && !candidate.protectedDeprecatedNode()->isDescendantOf(*highestRoot))
         return { };
 
+    // If 'editablePosition' has the non-editable child skipped, get the next sibling position.
+    // If not, we can't get the next paragraph in InsertListCommand::doApply's while loop.
+    if (nonEditableNode && nonEditableNode->isDescendantOf(candidate.deprecatedNode()))
+        candidate = nextVisuallyDistinctCandidate(candidate);
     return candidate;
 }
 
