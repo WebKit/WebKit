1870b96c36f08cd1aadb6699b77098acfc179b10
Ahmad Saleem <52317531+Ahmad-S792@users.noreply.github.com>
2024-06-24 13:54:03 +0100

    execCommand("outdent") on a list containing a table duplicates the content
    https://bugs.webkit.org/show_bug.cgi?id=24249
    rdar://problem/129093516
    
    Reviewed by NOBODY (OOPS!).
    
    Merge: https://chromium.googlesource.com/chromium/blink/+/0cd4b3864db4fc9c0abfeeee0775c5fcbbca0184
    
    This patch is for cases of indent/outdent for 'li' elements containing children
    spanning across multiple lines (paragraphs).
    For such cases the selection only encompasses children contained within the
    first paragraph (within the li), thereby causing incorrect behavior when indenting.
    Thus we need to ensure while inserting a new li element and cloning the original,
    that all the children are considered.
    
    With this change, an existing test: indent-pre-list.html fails because of the
    previous incorrect behavior. Two 'li`s' were created for the indented 'li'
    with the text contained within the first paragraph forming the first and the
    remaining content moving to the second 'li' element.
    
    This has now been fixed resulting in only a single indented 'li'.
    
    * Source/WebCore/editing/IndentOutdentCommand.cpp:
    (WebCore::IndentOutdentCommand::tryIndentingAsListItem):
    * LayoutTests/editing/execCommand/indent-pre-list-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/editing/run/indent-expected.txt:
    * LayoutTests/editing/execCommand/indent-list-item-with-children.html: Add Test Case
    * LayoutTests/editing/execCommand/indent-list-item-with-children-expected.txt: Add Test Case Expectation

--- Source/WebCore/editing/IndentOutdentCommand.cpp
+++ Source/WebCore/editing/IndentOutdentCommand.cpp
@@ -81,7 +81,15 @@ bool IndentOutdentCommand::tryIndentingAsListItem(const Position& start, const P
         newList = HTMLOListElement::create(protectedDocument());
     insertNodeBefore(*newList, *selectedListItem);
 
-    moveParagraphWithClones(start, end, newList.get(), selectedListItem.get());
+    // We should clone all the children of the list item for indenting purposes. However, in case the current
+    // selection does not encompass all its children, we need to explicitally handle the same. The original
+    // list item too would require proper deletion in that case.
+    if (end.anchorNode() == selectedListItem.get() || end.anchorNode()->isDescendantOf(selectedListItem->lastChild()))
+        moveParagraphWithClones(start, end, newList.get(), selectedListItem.get());
+    else {
+        moveParagraphWithClones(start, positionAfterNode(selectedListItem->lastChild()), newList.get(), selectedListItem.get());
+        removeNode(*selectedListItem);
+    }
 
     if (canMergeLists(previousList.get(), newList.get()))
         mergeIdenticalElements(*previousList, *newList);
