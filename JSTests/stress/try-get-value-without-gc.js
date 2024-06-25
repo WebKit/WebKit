b173743e8ef1a0d5c60cdd8321603475b35e6924
Yusuke Suzuki <ysuzuki@apple.com>
2024-06-25 09:44:17 -0700

    Unreviewed, attach memoryLimited restriction for memory consuming test https://bugs.webkit.org/show_bug.cgi?id=275861 rdar://130443221
    * JSTests/stress/try-get-value-without-gc.js:
    
    Canonical link: https://commits.webkit.org/280339@main

--- JSTests/stress/try-get-value-without-gc.js
+++ JSTests/stress/try-get-value-without-gc.js
@@ -1,3 +1,4 @@
+//@ skip if $memoryLimited
 //@ runDefault("--watchdog=1000", "--watchdog-exception-ok")
 function main() {
     error = (new Function(`return (function () { arguments.callee.displayName = 'a'.repeat(0x100000) + 'b'; `.repeat(100) + `return new Error();` + ` })();`.repeat(100)))();
