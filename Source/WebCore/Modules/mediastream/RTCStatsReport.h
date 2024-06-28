01d51aa70bfe9753dd1347e339bdfa8b70b3d34a
Ahmad Saleem <ahmad.saleem792@gmail.com>
2024-06-28 11:10:36 -0700

    Update `RTCAudioPlayoutStats` as per web-specification
    https://bugs.webkit.org/show_bug.cgi?id=275832
    
    Reviewed by Darin Adler and Youenn Fablet.
    
    This patch aligns WebKit with web specification [1]:
    
    [1] https://w3c.github.io/webrtc-stats/#dom-rtcaudiosourcestats
    
    It aligns by removing dropped metrics [2]:
    
    [2] https://github.com/w3c/webrtc-stats/pull/773
    
    * Source/WebCore/Modules/mediastream/RTCStatsReport.h:
    * Source/WebCore/Modules/mediastream/RTCStatsReport.idl:
    
    Canonical link: https://commits.webkit.org/280461@main

--- Source/WebCore/Modules/mediastream/RTCStatsReport.h
+++ Source/WebCore/Modules/mediastream/RTCStatsReport.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012 Google Inc. All rights reserved.
- * Copyright (C) 2017 Apple Inc. All rights reserved.
+ * Copyright (C) 2017-2024 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -492,10 +492,6 @@ public:
         std::optional<double> totalSamplesDuration;
         std::optional<double> echoReturnLoss;
         std::optional<double> echoReturnLossEnhancement;
-        std::optional<double> droppedSamplesDuration;
-        std::optional<uint64_t> droppedSamplesEvents;
-        std::optional<double> totalCaptureDelay;
-        std::optional<uint64_t> totalSamplesCaptured;
     };
     static_assert(!std::is_default_constructible_v<AudioSourceStats>);
 
