WebKit/WebKit

commit 3830822c935e6fa0e851781b1cf7def6d3f66f66

Navigation loads that go through a service worker should finish in case of service worker termination rdar://130079675 https://bugs.webkit.org/show_bug.cgi?id=275613
Reviewed by Chris Dumez.

We now store navigation fetch tasks in WebSWContextManagerConnection.
This allows to directly ask them to continueDidReceiveFetchResponse or convertFetchToDownload without hopping to the worker thread.
This is useful as the worker thread may be dead by the time we hop to it, which will prevent the load to finish or fail.

Since we are now calling continueDidReceiveFetchResponse or convertFetchToDownload from WebSWContextManagerConnection queue, we add a connection lock to WebServiceWorkerFetchTaskClient.

When a service worker gets stopped, we are now explicitly cancelling the ongoing fetch tasks by calling WebServiceWorkerFetchTaskClient::contextIsStopping.
It will cancel the load except if the load is already terminated but waiting to proceed (which happens for navigation loads waiting for a response check).

* LayoutTests/http/wpt/service-workers/navigation-fetch-worker-terminate-worker.js: Added.
(doTest):
* LayoutTests/http/wpt/service-workers/navigation-fetch-worker-terminate.https-expected.txt: Added.
* LayoutTests/http/wpt/service-workers/navigation-fetch-worker-terminate.https.html: Added.
* Source/WebCore/workers/service/ServiceWorkerGlobalScope.cpp:
(WebCore::ServiceWorkerGlobalScope::prepareForDestruction):
* Source/WebCore/workers/service/context/SWContextManager.cpp:
(WebCore::SWContextManager::removeFetch):
* Source/WebCore/workers/service/context/SWContextManager.h:
* Source/WebCore/workers/service/context/ServiceWorkerFetch.h:
* Source/WebCore/workers/service/context/ServiceWorkerThreadProxy.cpp:
(WebCore::ServiceWorkerThreadProxy::cancelFetch):
(WebCore::ServiceWorkerThreadProxy::convertFetchToDownload): Deleted.
(WebCore::ServiceWorkerThreadProxy::continueDidReceiveFetchResponse): Deleted.
* Source/WebCore/workers/service/context/ServiceWorkerThreadProxy.h:

Source/WebKit/NetworkProcess/ServiceWorker/ServiceWorkerFetchTask.messages.in
@@ -29,4 +29,5 @@ messages -> ServiceWorkerFetchTask NotRefCounted {
     DidReceiveFormData(IPC::FormDataReference data)
     DidFinish(WebCore::NetworkLoadMetrics metrics)
     UsePreload()
+    ContextClosed()
 }

Source/WebKit/WebProcess/Storage/WebServiceWorkerFetchTaskClient.cpp
@@ -53,8 +53,12 @@ WebServiceWorkerFetchTaskClient::WebServiceWorkerFetchTaskClient(Ref<IPC::Connec
 
 void WebServiceWorkerFetchTaskClient::didReceiveRedirection(const WebCore::ResourceResponse& response)
 {
+    Locker lock(m_connectionLock);
+
     if (!m_connection)
         return;
+
+    m_didSendResponse = true;
     m_connection->send(Messages::ServiceWorkerFetchTask::DidReceiveRedirectResponse { response }, m_fetchIdentifier);
 
     cleanup();
@@ -62,9 +66,12 @@ void WebServiceWorkerFetchTaskClient::didReceiveRedirection(const WebCore::Resou
 
 void WebServiceWorkerFetchTaskClient::didReceiveResponse(const ResourceResponse& response)
 {
+    Locker lock(m_connectionLock);
+
     if (!m_connection)
         return;
 
+    m_didSendResponse = true;
     if (m_needsContinueDidReceiveResponseMessage)
         m_waitingForContinueDidReceiveResponseMessage = true;
 
@@ -72,6 +79,12 @@ void WebServiceWorkerFetchTaskClient::didReceiveResponse(const ResourceResponse&
 }
 
 void WebServiceWorkerFetchTaskClient::didReceiveData(const SharedBuffer& buffer)
+{
+    Locker lock(m_connectionLock);
+    didReceiveDataInternal(buffer);
+}
+
+void WebServiceWorkerFetchTaskClient::didReceiveDataInternal(const SharedBuffer& buffer)
 {
     if (!m_connection)
         return;
@@ -91,9 +104,15 @@ void WebServiceWorkerFetchTaskClient::didReceiveData(const SharedBuffer& buffer)
 
 void WebServiceWorkerFetchTaskClient::didReceiveFormDataAndFinish(Ref<FormData>&& formData)
 {
+    Locker lock(m_connectionLock);
+    didReceiveFormDataAndFinishInternal(WTFMove(formData));
+}
+
+void WebServiceWorkerFetchTaskClient::didReceiveFormDataAndFinishInternal(Ref<FormData>&& formData)
+{
     if (auto sharedBuffer = formData->asSharedBuffer()) {
-        didReceiveData(sharedBuffer.releaseNonNull());
-        didFinish({ });
+        didReceiveDataInternal(sharedBuffer.releaseNonNull());
+        didFinishInternal({ });
         return;
     }
 
@@ -137,6 +156,7 @@ void WebServiceWorkerFetchTaskClient::didReceiveFormDataAndFinish(Ref<FormData>&
 
 void WebServiceWorkerFetchTaskClient::didReceiveBlobChunk(const SharedBuffer& buffer)
 {
+    Locker lock(m_connectionLock);
     if (!m_connection)
         return;
 
@@ -154,6 +174,12 @@ void WebServiceWorkerFetchTaskClient::didFinishBlobLoading()
 }
 
 void WebServiceWorkerFetchTaskClient::didFail(const ResourceError& error)
+{
+    Locker lock(m_connectionLock);
+    didFailInternal(error);
+}
+
+void WebServiceWorkerFetchTaskClient::didFailInternal(const ResourceError& error)
 {
     if (!m_connection)
         return;
@@ -174,6 +200,12 @@ void WebServiceWorkerFetchTaskClient::didFail(const ResourceError& error)
 }
 
 void WebServiceWorkerFetchTaskClient::didFinish(const NetworkLoadMetrics& metrics)
+{
+    Locker lock(m_connectionLock);
+    didFinishInternal(metrics);
+}
+
+void WebServiceWorkerFetchTaskClient::didFinishInternal(const NetworkLoadMetrics& metrics)
 {
     if (!m_connection)
         return;
@@ -196,6 +228,12 @@ void WebServiceWorkerFetchTaskClient::didFinish(const NetworkLoadMetrics& metric
 
 void WebServiceWorkerFetchTaskClient::didNotHandle()
 {
+    Locker lock(m_connectionLock);
+    didNotHandleInternal();
+}
+
+void WebServiceWorkerFetchTaskClient::didNotHandleInternal()
+{
     if (!m_connection)
         return;
 
@@ -206,6 +244,8 @@ void WebServiceWorkerFetchTaskClient::didNotHandle()
 
 void WebServiceWorkerFetchTaskClient::cancel()
 {
+    Locker lock(m_connectionLock);
+
     ASSERT(!isMainRunLoop());
     m_connection = nullptr;
     if (m_cancelledCallback)
@@ -226,6 +266,8 @@ void WebServiceWorkerFetchTaskClient::setCancelledCallback(Function<void()>&& ca
 
 void WebServiceWorkerFetchTaskClient::usePreload()
 {
+    Locker lock(m_connectionLock);
+
     if (!m_connection)
         return;
 
@@ -236,7 +278,9 @@ void WebServiceWorkerFetchTaskClient::usePreload()
 
 void WebServiceWorkerFetchTaskClient::continueDidReceiveResponse()
 {
-    RELEASE_LOG(ServiceWorker, "ServiceWorkerFrameLoaderClient::continueDidReceiveResponse, has connection %d, didFinish %d, response type %ld", !!m_connection, m_didFinish, static_cast<long>(m_responseData.index()));
+    Locker lock(m_connectionLock);
+
+    RELEASE_LOG(ServiceWorker, "ServiceWorkerFrameLoaderClient::continueDidReceiveResponse, has connection %d, didFinish %d, response type %ld", !!m_connection, !!m_didFinish, static_cast<long>(m_responseData.index()));
 
     if (!m_connection)
         return;
@@ -244,16 +288,20 @@ void WebServiceWorkerFetchTaskClient::continueDidReceiveResponse()
     m_waitingForContinueDidReceiveResponseMessage = false;
 
     switchOn(m_responseData, [this](std::nullptr_t&) {
+        assertIsHeld(m_connectionLock);
         if (m_didFinish)
-            didFinish(m_networkLoadMetrics);
+            didFinishInternal(m_networkLoadMetrics);
     }, [this](const SharedBufferBuilder& buffer) {
-        didReceiveData(buffer.copy()->makeContiguous());
+        assertIsHeld(m_connectionLock);
+        didReceiveDataInternal(buffer.copy()->makeContiguous());
         if (m_didFinish)
-            didFinish(m_networkLoadMetrics);
+            didFinishInternal(m_networkLoadMetrics);
     }, [this](Ref<FormData>& formData) {
-        didReceiveFormDataAndFinish(WTFMove(formData));
+        assertIsHeld(m_connectionLock);
+        didReceiveFormDataAndFinishInternal(WTFMove(formData));
     }, [this](UniqueRef<ResourceError>& error) {
-        didFail(error.get());
+        assertIsHeld(m_connectionLock);
+        didFailInternal(error.get());
     });
     m_responseData = nullptr;
 }
@@ -261,10 +309,30 @@ void WebServiceWorkerFetchTaskClient::continueDidReceiveResponse()
 void WebServiceWorkerFetchTaskClient::cleanup()
 {
     m_connection = nullptr;
-    ensureOnMainRunLoop([serviceWorkerIdentifier = m_serviceWorkerIdentifier, serverConnectionIdentifier = m_serverConnectionIdentifier, fetchIdentifier = m_fetchIdentifier] {
-        if (auto* proxy = SWContextManager::singleton().serviceWorkerThreadProxy(serviceWorkerIdentifier))
-            proxy->removeFetch(serverConnectionIdentifier, fetchIdentifier);
+    ensureOnMainRunLoop([serviceWorkerIdentifier = m_serviceWorkerIdentifier, serverConnectionIdentifier = m_serverConnectionIdentifier, fetchIdentifier = m_fetchIdentifier, needsContinueDidReceiveResponseMessage = m_needsContinueDidReceiveResponseMessage] {
+        SWContextManager::singleton().removeFetch(serviceWorkerIdentifier, serverConnectionIdentifier, fetchIdentifier, needsContinueDidReceiveResponseMessage);
     });
+}
+
+void WebServiceWorkerFetchTaskClient::contextIsStopping()
+{
+    Locker lock(m_connectionLock);
+
+    if (!m_connection)
+        return;
+
+    if (!m_didSendResponse) {
+        didNotHandleInternal();
+        return;
+    }
+
+    if (m_didFinish) {
+        ASSERT(m_needsContinueDidReceiveResponseMessage);
+        return;
+    }
+
+    m_connection->send(Messages::ServiceWorkerFetchTask::ContextClosed { }, m_fetchIdentifier);
+    cleanup();
 }
 
 } // namespace WebKit

LayoutTests/http/wpt/service-workers/navigation-fetch-worker-terminate-worker.js
@@ -1,0 +1,8 @@
+function doTest(event)
+{
+    event.respondWith(new Response("<html><body>PASS</body></html>", { status: 200, headers: [["Content-Type", "text/html"]] }));
+    if (self.internals)
+        setTimeout(() => internals.terminate(), 0);
+}
+
+self.addEventListener("fetch", doTest);

LayoutTests/http/wpt/service-workers/navigation-fetch-worker-terminate.https-expected.txt
@@ -1,0 +1,5 @@
+
+
+PASS Setup worker
+PASS Make sure a navigation load answered by a synthetic response succeeds even if worker terminates
+

LayoutTests/http/wpt/service-workers/navigation-fetch-worker-terminate.https.html
@@ -1,0 +1,32 @@
+<html>
+<head>
+<script src="/resources/testharness.js"></script>
+<script src="/resources/testharnessreport.js"></script>
+<script src="/service-workers/service-worker/resources/test-helpers.sub.js"></script>
+</head>
+<body>
+<script>
+var scope = "resources";
+var activeWorker;
+
+promise_test(async (test) => {
+    var registration = await navigator.serviceWorker.register("navigation-fetch-worker-terminate-worker.js", { scope : scope });
+    activeWorker = registration.active;
+    if (activeWorker)
+        return;
+    activeWorker = registration.installing;
+    return new Promise(resolve => {
+        activeWorker.addEventListener('statechange', () => {
+            if (activeWorker.state === "activated")
+                resolve();
+        });
+    });
+}, "Setup worker");
+
+promise_test(async (test) => {
+    const iframe = await with_iframe(scope);
+    assert_equals(iframe.contentWindow.document.body.innerHTML, "PASS");
+}, "Make sure a navigation load answered by a synthetic response succeeds even if worker terminates");
+</script>
+</body>
+</html>

Source/WebCore/workers/service/ServiceWorkerGlobalScope.cpp
@@ -178,7 +178,10 @@ void ServiceWorkerGlobalScope::prepareForDestruction()
     // Make sure we destroy fetch events objects before the VM goes away, since their
     // destructor may access the VM.
     m_extendedEvents.clear();
-    m_ongoingFetchTasks.clear();
+
+    auto ongoingFetchTasks = std::exchange(m_ongoingFetchTasks, { });
+    for (auto& task : ongoingFetchTasks.values())
+        task.client->contextIsStopping();
 
     WorkerGlobalScope::prepareForDestruction();
 }

Source/WebCore/workers/service/context/SWContextManager.cpp
@@ -337,4 +337,13 @@ void SWContextManager::setRegistrationUpdateViaCache(ServiceWorkerRegistrationId
     });
 }
 
+void SWContextManager::removeFetch(ServiceWorkerIdentifier serviceWorkerIdentifier, SWServerConnectionIdentifier serverConnectionIdentifier, FetchIdentifier fetchIdentifier, bool isNavigationFetch)
+{
+    ASSERT(isMainThread());
+    if (auto* proxy = serviceWorkerThreadProxy(serviceWorkerIdentifier))
+        proxy->removeFetch(serverConnectionIdentifier, fetchIdentifier);
+    if (isNavigationFetch && m_connection)
+        m_connection->removeNavigationFetch(serverConnectionIdentifier, fetchIdentifier);
+}
+
 } // namespace WebCore

Source/WebCore/workers/service/context/SWContextManager.h
@@ -90,6 +90,8 @@ public:
 
         bool isClosed() const { return m_isClosed; }
 
+        virtual void removeNavigationFetch(SWServerConnectionIdentifier, FetchIdentifier) = 0;
+
     protected:
         void setAsClosed() { m_isClosed = true; }
 
@@ -133,6 +135,7 @@ public:
     WEBCORE_EXPORT void fireUpdateFoundEvent(ServiceWorkerRegistrationIdentifier);
     WEBCORE_EXPORT void setRegistrationLastUpdateTime(ServiceWorkerRegistrationIdentifier, WallTime);
     WEBCORE_EXPORT void setRegistrationUpdateViaCache(ServiceWorkerRegistrationIdentifier, ServiceWorkerUpdateViaCache);
+    WEBCORE_EXPORT void removeFetch(ServiceWorkerIdentifier, SWServerConnectionIdentifier, FetchIdentifier, bool isNavigationFetch);
 
 private:
     SWContextManager() = default;

commit 821d1ca973031360e9f5327c49f57fed1c0c38d7

Crash under ServiceWorkerFetchTask::contextClosed() https://bugs.webkit.org/show_bug.cgi?id=276079 rdar://130856919
Reviewed by David Kilzer and Youenn Fablet.

This crash is a regression from 280246@main. When ServiceWorkerFetchTask::contextClosed()
was called due to receiving the ServiceWorkerFetchTask::ContextClosed IPC, the task
would fail to unregister itself from the WebSWServerToContextConnection, leaving a null
WebPtr in the map. We would then dereference this null WeakPtr in the WebSWServerToContextConnection
destructor.

To address the issue, I have renamed the IPC to WorkerClosed, and the implementation now
unregisters the task from the WebSWServerToContextConnection before calling contextClosed().

* Source/WebKit/NetworkProcess/ServiceWorker/ServiceWorkerFetchTask.cpp:
(WebKit::ServiceWorkerFetchTask::workerClosed):
* Source/WebKit/NetworkProcess/ServiceWorker/ServiceWorkerFetchTask.h:
* Source/WebKit/NetworkProcess/ServiceWorker/ServiceWorkerFetchTask.messages.in:
* Source/WebKit/NetworkProcess/ServiceWorker/WebSWServerToContextConnection.cpp:
(WebKit::WebSWServerToContextConnection::~WebSWServerToContextConnection):

Canonical link: https://commits.webkit.org/280600@main

Source/WebKit/NetworkProcess/ServiceWorker/ServiceWorkerFetchTask.cpp
@@ -156,6 +156,13 @@ void ServiceWorkerFetchTask::start(WebSWServerToContextConnection& serviceWorker
     startFetch();
 }
 
+void ServiceWorkerFetchTask::workerClosed()
+{
+    if (CheckedPtr serviceWorkerConnection = m_serviceWorkerConnection.get())
+        serviceWorkerConnection->unregisterFetch(*this);
+    contextClosed();
+}
+
 void ServiceWorkerFetchTask::contextClosed()
 {
     SWFETCH_RELEASE_LOG("contextClosed: (m_isDone=%d, m_wasHandled=%d)", m_isDone, m_wasHandled);

Source/WebKit/NetworkProcess/ServiceWorker/ServiceWorkerFetchTask.h
@@ -117,6 +117,8 @@ private:
     NetworkSession* session();
     void preloadResponseIsReady();
 
+    void workerClosed();
+
     template<typename Message> bool sendToServiceWorker(Message&&);
     template<typename Message> bool sendToClient(Message&&);
 

Source/WebKit/NetworkProcess/ServiceWorker/ServiceWorkerFetchTask.messages.in
@@ -29,5 +29,5 @@ messages -> ServiceWorkerFetchTask NotRefCounted {
     DidReceiveFormData(IPC::FormDataReference data)
     DidFinish(WebCore::NetworkLoadMetrics metrics)
     UsePreload()
-    ContextClosed()
+    WorkerClosed()
 }

Source/WebKit/NetworkProcess/ServiceWorker/WebSWServerToContextConnection.cpp
@@ -61,11 +61,11 @@ WebSWServerToContextConnection::~WebSWServerToContextConnection()
 {
     auto fetches = WTFMove(m_ongoingFetches);
     for (auto& fetch : fetches.values())
-        fetch->contextClosed();
+        Ref { fetch.get() }->contextClosed();
 
     auto downloads = WTFMove(m_ongoingDownloads);
     for (auto& weakPtr : downloads.values()) {
-        if (auto download = weakPtr.get())
+        if (RefPtr download = weakPtr.get())
             download->contextClosed();
     }
 

Source/WebKit/WebProcess/Storage/WebServiceWorkerFetchTaskClient.cpp
@@ -331,7 +331,7 @@ void WebServiceWorkerFetchTaskClient::contextIsStopping()
         return;
     }
 
-    m_connection->send(Messages::ServiceWorkerFetchTask::ContextClosed { }, m_fetchIdentifier);
+    m_connection->send(Messages::ServiceWorkerFetchTask::WorkerClosed { }, m_fetchIdentifier);
     cleanup();
 }
 