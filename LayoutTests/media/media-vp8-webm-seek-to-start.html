WebKit/WebKit

commit 925970b7979d9517eebb9014b76ef399c9572f4a

MediaPlayerPrivateWebM caused the seeked event to be fired before the seek has completed. https://bugs.webkit.org/show_bug.cgi?id=267326 rdar://120776794
Reviewed by Youenn Fablet.

MediaPlayerPrivateWebM only implemented the most basic of seeking operation.
We copy the logic from the MediaPlayerPrivateMediaSourceAVFObjC into the WebM
player.

Now the seeked event will only be fired once the seek operation has completed
and once the frame at the seek operation has completed.
The WebM player would also always start playback after a seek regardless
of the element playing prior the operation, only resume playback if we
were seeking before.

Following a call to flush() WebCoreDecompressionSession could continue
delivering in-flight decompressed frame. We modify it so that we can guarantee
that no frames enqueued before the call to flush() can get delivered.
This is done by using NativePromise to track the actual decoding and storing
the flushId value at the start of the decoding, and to stop delivering the
frame if that flushId has changed.

Add tests.
On macOS < 14 and iOS < 17; the required `isReadyForDisplay` API to accurately
determine that a frame got painted doesn't exist.

* LayoutTests/media/content/test-vp8.webm: Added.
* LayoutTests/media/content/test-vp8.webm.png: Added.
* LayoutTests/media/media-vp8-webm-expected.html: Added.
* LayoutTests/media/media-vp8-webm.html: Added.
* LayoutTests/platform/mac-wk1/TestExpectations:
* Source/WebCore/platform/graphics/cocoa/MediaPlayerPrivateWebM.h:
* Source/WebCore/platform/graphics/cocoa/MediaPlayerPrivateWebM.mm:
(WebCore::MediaPlayerPrivateWebM::MediaPlayerPrivateWebM):
(WebCore::MediaPlayerPrivateWebM::~MediaPlayerPrivateWebM):
(WebCore::MediaPlayerPrivateWebM::play):
(WebCore::MediaPlayerPrivateWebM::pause):
(WebCore::MediaPlayerPrivateWebM::seekToTarget):
(WebCore::MediaPlayerPrivateWebM::seekInternal):
(WebCore::MediaPlayerPrivateWebM::maybeCompleteSeek):
(WebCore::MediaPlayerPrivateWebM::seeking const):

Source/WebCore/platform/graphics/cocoa/MediaPlayerPrivateWebM.mm
@@ -89,9 +89,32 @@ MediaPlayerPrivateWebM::MediaPlayerPrivateWebM(MediaPlayer* player)
     , m_logIdentifier(player->mediaPlayerLogIdentifier())
     , m_videoLayerManager(makeUnique<VideoLayerManagerObjC>(m_logger, m_logIdentifier))
     , m_listener(WebAVSampleBufferListener::create(*this))
+    , m_seekTimer(*this, &MediaPlayerPrivateWebM::seekInternal)
 {
     ALWAYS_LOG(LOGIDENTIFIER);
     m_parser->setLogger(m_logger, m_logIdentifier);
+
+    // addPeriodicTimeObserverForInterval: throws an exception if you pass a non-numeric CMTime, so just use
+    // an arbitrarily large time value of once an hour:
+    __block WeakPtr weakThis { *this };
+    m_timeJumpedObserver = [m_synchronizer addPeriodicTimeObserverForInterval:PAL::toCMTime(MediaTime::createWithDouble(3600)) queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
+#if LOG_DISABLED
+        UNUSED_PARAM(time);
+#endif
+
+        if (!weakThis)
+            return;
+
+        auto clampedTime = CMTIME_IS_NUMERIC(time) ? clampTimeToLastSeekTime(PAL::toMediaTime(time)) : MediaTime::zeroTime();
+        ALWAYS_LOG(LOGIDENTIFIER, "synchronizer fired: time clamped = ", clampedTime, ", seeking = ", m_isSynchronizerSeeking, ", pending = ", !!m_pendingSeek);
+
+        if (m_isSynchronizerSeeking && !m_pendingSeek) {
+            m_isSynchronizerSeeking = false;
+            maybeCompleteSeek();
+        }
+    }];
+    ALWAYS_LOG(LOGIDENTIFIER, "synchronizer initial rate:", [m_synchronizer rate]);
+    [m_synchronizer setRate:0];
 }
 
 MediaPlayerPrivateWebM::~MediaPlayerPrivateWebM()
@@ -102,6 +125,8 @@ MediaPlayerPrivateWebM::~MediaPlayerPrivateWebM()
         [m_synchronizer removeTimeObserver:m_durationObserver.get()];
     if (m_videoFrameMetadataGatheringObserver)
         [m_synchronizer removeTimeObserver:m_videoFrameMetadataGatheringObserver.get()];
+    if (m_timeJumpedObserver)
+        [m_synchronizer removeTimeObserver:m_timeJumpedObserver.get()];
 
     destroyLayer();
     destroyDecompressionSession();
@@ -235,6 +260,10 @@ void MediaPlayerPrivateWebM::play()
     flushIfNeeded();
 #endif
 
+    m_isPlaying = true;
+    if (!shouldBePlaying())
+        return;
+
     [m_synchronizer setRate:m_rate];
 
     if (currentMediaTime() >= durationMediaTime())
@@ -243,6 +272,7 @@ void MediaPlayerPrivateWebM::play()
 
 void MediaPlayerPrivateWebM::pause()
 {
+    m_isPlaying = false;
     [m_synchronizer setRate:0];
 }
 
@@ -262,31 +292,101 @@ void MediaPlayerPrivateWebM::setPageIsVisible(bool visible, String&&)
 
 MediaTime MediaPlayerPrivateWebM::currentMediaTime() const
 {
-    MediaTime synchronizerTime = PAL::toMediaTime(PAL::CMTimebaseGetTime([m_synchronizer timebase]));
+    MediaTime synchronizerTime = clampTimeToLastSeekTime(PAL::toMediaTime(PAL::CMTimebaseGetTime([m_synchronizer timebase])));
     if (synchronizerTime < MediaTime::zeroTime())
         return MediaTime::zeroTime();
-    if (synchronizerTime > durationMediaTime())
-        return durationMediaTime();
 
     return synchronizerTime;
 }
 
 void MediaPlayerPrivateWebM::seekToTarget(const SeekTarget& target)
 {
-    ALWAYS_LOG(LOGIDENTIFIER, "target = ", target);
-    [m_synchronizer setRate:0 time:PAL::toCMTime(target.time)];
+    ALWAYS_LOG(LOGIDENTIFIER, "time = ", target.time, ", negativeThreshold = ", target.negativeThreshold, ", positiveThreshold = ", target.positiveThreshold);
+
+    m_pendingSeek = target;
+
+    if (m_seekTimer.isActive())
+        m_seekTimer.stop();
+    m_seekTimer.startOneShot(0_s);
+}
+
+void MediaPlayerPrivateWebM::seekInternal()
+{
+    if (!m_pendingSeek)
+        return;
+
+    auto pendingSeek = std::exchange(m_pendingSeek, { }).value();
+    m_lastSeekTime = pendingSeek.time;
+    [m_synchronizer setRate:0 time:PAL::toCMTime(m_lastSeekTime)];
+
+    m_seekState = Seeking;
+
+    MediaTime synchronizerTime = PAL::toMediaTime([m_synchronizer currentTime]);
+
+    m_isSynchronizerSeeking = synchronizerTime != m_lastSeekTime;
+    ALWAYS_LOG(LOGIDENTIFIER, "seekedTime = ", m_lastSeekTime, ", synchronizerTime = ", synchronizerTime, "synchronizer seeking = ", m_isSynchronizerSeeking);
+
+    if (!m_isSynchronizerSeeking) {
+        // In cases where the destination seek time precisely matches the synchronizer's existing time
+        // no time jumped notification will be issued. In this case, just notify the MediaPlayer that
+        // the seek completed successfully.
+        maybeCompleteSeek();
+        return;
+    }
+
+    flush();
+
     for (auto& trackBufferPair : m_trackBufferMap) {
         TrackBuffer& trackBuffer = trackBufferPair.second;
         auto trackId = trackBufferPair.first;
 
         trackBuffer.setNeedsReenqueueing(true);
-        reenqueueMediaForTime(trackBuffer, trackId, target.time);
+        reenqueueMediaForTime(trackBuffer, trackId, m_lastSeekTime);
     }
-    [m_synchronizer setRate:m_rate];
+
+    maybeCompleteSeek();
+}
+
+void MediaPlayerPrivateWebM::maybeCompleteSeek()
+{
+    if (m_seekState == SeekCompleted)
+        return;
+    if (hasVideo() && !m_hasAvailableVideoFrame) {
+        ALWAYS_LOG(LOGIDENTIFIER, "waiting for video frame");
+        m_seekState = WaitingForAvailableFame;
+        return;
+    }
+    m_seekState = Seeking;
+    ALWAYS_LOG(LOGIDENTIFIER);
+    if (m_isSynchronizerSeeking) {
+        ALWAYS_LOG(LOGIDENTIFIER, "Synchronizer still seeking, bailing out");
+        return;
+    }
+    m_seekState = SeekCompleted;
+    if (shouldBePlaying())
+        [m_synchronizer setRate:m_rate];
     if (auto player = m_player.get()) {
-        player->seeked(target.time);
+        player->seeked(m_lastSeekTime);
         player->timeChanged();
     }
+}
+
+bool MediaPlayerPrivateWebM::seeking() const
+{
+    return m_pendingSeek || m_seekState != SeekCompleted;
+}
+
+MediaTime MediaPlayerPrivateWebM::clampTimeToLastSeekTime(const MediaTime& time) const
+{
+    if (m_lastSeekTime.isFinite() && time < m_lastSeekTime)
+        return m_lastSeekTime;
+
+    return time;
+}
+
+bool MediaPlayerPrivateWebM::shouldBePlaying() const
+{
+    return m_isPlaying && !seeking();
 }
 
 void MediaPlayerPrivateWebM::setRateDouble(double rate)
@@ -296,7 +396,7 @@ void MediaPlayerPrivateWebM::setRateDouble(double rate)
 
     m_rate = std::max<double>(rate, 0);
 
-    if (!paused())
+    if (shouldBePlaying())
         [m_synchronizer setRate:m_rate];
 
     if (auto player = m_player.get())
@@ -561,6 +661,10 @@ void MediaPlayerPrivateWebM::setHasAvailableVideoFrame(bool hasAvailableVideoFra
 
     if (auto player = m_player.get())
         player->firstVideoFrameAvailable();
+
+    if (m_seekState == WaitingForAvailableFame)
+        maybeCompleteSeek();
+
     setReadyState(MediaPlayer::ReadyState::HaveEnoughData);
 }
 
@@ -768,15 +872,38 @@ void MediaPlayerPrivateWebM::enqueueSample(Ref<MediaSample>&& sample, TrackID tr
             setNaturalSize(formatSize);
 
         if (m_decompressionSession)
-            m_decompressionSession->enqueueSample(platformSample.sample.cmSampleBuffer);
-
-        if (!hasAvailableVideoFrame() && !sample->isNonDisplaying())
-            setHasAvailableVideoFrame(true);
+            m_decompressionSession->enqueueSample(platformSample.sample.cmSampleBuffer, !sample->isNonDisplaying());
 
         if (!m_videoLayer)
             return;
 
         m_videoLayer->enqueueSample(platformSample.sample.cmSampleBuffer, !sample->isNonDisplaying());
+        AVSampleBufferDisplayLayer* displayLayer = m_videoLayer->displayLayer();
+#if HAVE(AVSAMPLEBUFFERDISPLAYLAYER_READYFORDISPLAY)
+        // FIXME (117934497): Remove staging code once -[AVSampleBufferDisplayLayer isReadyForDisplay] is available in SDKs used by WebKit builders
+        if ([displayLayer respondsToSelector:@selector(isReadyForDisplay)])
+            return;
+#endif
+        if (m_hasAvailableVideoFrame || sample->isNonDisplaying())
+            return;
+
+        DEBUG_LOG(LOGIDENTIFIER, "adding buffer attachment");
+
+        [displayLayer prerollDecodeWithCompletionHandler:[this, weakThis = WeakPtr { *this }, logSiteIdentifier = LOGIDENTIFIER] (BOOL success) mutable {
+            ensureOnMainThread([this, weakThis = WTFMove(weakThis), logSiteIdentifier, success] () {
+                RefPtr protectedThis = weakThis.get();
+                if (!protectedThis)
+                    return;
+
+                if (!success || !m_videoLayer) {
+                    ERROR_LOG(logSiteIdentifier, "prerollDecodeWithCompletionHandler failed");
+                    return;
+                }
+
+                layerReadyForDisplayChanged(m_videoLayer->displayLayer(), true);
+            });
+        }];
+
         return;
     }
     // AVSampleBufferAudioRenderer will throw an un-documented exception if passed a sample
@@ -1404,7 +1531,7 @@ void MediaPlayerPrivateWebM::destroyDecompressionSession()
 {
     if (!m_decompressionSession)
         return;
-    
+
     m_decompressionSession->invalidate();
     m_decompressionSession = nullptr;
     m_hasAvailableVideoFrameSemaphore = nullptr;
@@ -1583,6 +1710,17 @@ void MediaPlayerPrivateWebM::rendererDidReceiveError(AVSampleBufferAudioRenderer
     setNetworkState(MediaPlayer::NetworkState::DecodeError);
     setReadyState(MediaPlayer::ReadyState::HaveNothing);
     m_errored = true;
+}
+
+void MediaPlayerPrivateWebM::layerReadyForDisplayChanged(AVSampleBufferDisplayLayer* layer, bool isReadyForDisplay)
+{
+    if (!m_videoLayer || layer != m_videoLayer->displayLayer() || !isReadyForDisplay)
+        return;
+
+    auto currentTime = PAL::CMTimebaseGetTime([layer timebase]);
+    ALWAYS_LOG(LOGIDENTIFIER, "m_isSynchronizerSeeking:", m_isSynchronizerSeeking, " layer.basetime:", PAL::toMediaTime(currentTime));
+
+    setHasAvailableVideoFrame(true);
 }
 
 } // namespace WebCore

LayoutTests/platform/mac-wk1/TestExpectations
@@ -1022,6 +1022,7 @@ media/W3C/video/canPlayType/canPlayType_two_implies_one_1.html [ Failure ]
 media/W3C/video/canPlayType/canPlayType_two_implies_one_2.html [ Failure ]
 media/media-can-play-webm.html [ Failure ]
 media/media-vp8-webm-error.html [ Failure ]
+media/media-vp8-webm.html [ Skip ]
 media/track/media-audio-track.html [ Failure ]
 
 # VP9/VP8 are not supported on WK1 and turned off by default

LayoutTests/platform/mac-wk2/TestExpectations
@@ -1422,7 +1422,7 @@ webkit.org/b/229166 webrtc/utf8-sdp.html [ Pass Failure ]
 #rdar://82032946 ([Mac wk2] webanimations/translate-property-and-translate-animation-with-delay-on-forced-layer.html is a flaky image-only fail)
 [ Monterey+ ] webanimations/translate-property-and-translate-animation-with-delay-on-forced-layer.html [ Pass Failure ]
 
-# webkit.org/b/228200 Setting multiple test expectations for Monetery on OpenSource:
+# webkit.org/b/228200 Setting multiple test expectations for Montery on OpenSource:
 [ Monterey Release ] platform/mac-wk2/plugins/asychronous-deadlock-with-timer-and-evaluate.html [ Pass Crash ]
 [ Monterey Release ] media/media-fragments/TC0004.html [ Pass Timeout ]
 
@@ -1879,6 +1879,8 @@ webkit.org/b/262411 [ Sonoma+ ] fast/scrolling/scroll-to-anchor-zoomed-header.ht
 
 # rdar://114452082 (REGRESSION (262124@main?): [ Sonoma wk2 ] imported/w3c/web-platform-tests/mimesniff/media/media-sniff.window.html is a consistent failure)
 [ Sonoma+ ] imported/w3c/web-platform-tests/mimesniff/media/media-sniff.window.html [ Failure ]
+
+[ Monterey Ventura ] media/media-vp8-webm.html [ ImageOnlyFailure ] # Missing AVSampleBufferDisplayLayer's ReadyForDisplayDidChangeNotification
 
 # rdar://114294654 (REGRESSION (265615@main): [ Sonoma ] fast/attachment/cocoa/wide-attachment-rendering.html is a constant failure)
 [ Sonoma+ ] fast/attachment/cocoa/wide-attachment-rendering.html [ Failure ]

LayoutTests/media/media-vp8-webm-expected.html
@@ -1,0 +1,5 @@
+<html>
+  <body>
+    <img src="content/test-vp8.webm.png"/>
+  </body>
+</html>

LayoutTests/media/media-vp8-webm.html
@@ -1,0 +1,31 @@
+<html>
+<meta name="fuzzy" content="maxDifference=0-55; totalPixels=0-76781" />
+<head>
+<title>webm file with vp8</title>
+<script src="../resources/testharness.js"></script>
+<script src="utilities.js"></script>
+<script>
+    // content encoded with ffmpeg -i test.mp4 -c:v libvpx -vf "scale=in_range=tv:out_range=tv" test-vp8.webm
+    // image: ffmpeg -sseof -3 -i test-vp8.webm -pred mixed -pix_fmt rgb24 -sws_flags +accurate_rnd+full_chroma_int -update 1 -q:v 1 -vf "scale=in_range=tv:out_range=tv" test-vp8.webm.png
+    // tv color range required until https://bugs.webkit.org/show_bug.cgi?id=267400
+    async function init()
+    {
+        if (window.testRunner)
+            testRunner.waitUntilDone();
+
+        let v = document.getElementsByTagName('video')[0];
+        v.src = "content/test-vp8.webm";
+        await once(v, 'canplay');
+        v.play();
+        // duration of the last frame.
+        v.currentTime = v.duration - 0.02;
+        await Promise.all([ once(v, 'seeked'), once(v, 'ended') ]);
+        if (window.testRunner)
+            testRunner.notifyDone();
+    }
+</script>
+</head>
+<body onload="init();">
+<video/>
+</body>
+</html>

Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateMediaSourceAVFObjC.h
@@ -341,8 +341,8 @@ ALLOW_NEW_API_WITHOUT_GUARDS_END
     MediaTime m_lastSeekTime;
     FloatSize m_naturalSize;
     double m_rate { 1 };
-    bool m_playing { false };
-    bool m_synchronizerSeeking { false };
+    bool m_isPlaying { false };
+    bool m_isSynchronizerSeeking { false };
     SeekState m_seekState { SeekCompleted };
     mutable bool m_loadingProgressed { false };
 #if !HAVE(AVSAMPLEBUFFERDISPLAYLAYER_COPYDISPLAYEDPIXELBUFFER)

commit e5a7357eaea5e5d45b18a74ebb0d6a0c5299fbb4

[WEBM] Seeking is broken https://bugs.webkit.org/show_bug.cgi?id=276171 rdar://128620836
Reviewed by Jer Noble and Youenn Fablet.

Regression was introduced in 273109@main
We never flushed the AVSampleBufferDisplayLayer nor re-enqueued new frames to be displayed
when seeking backward.

Added test

* LayoutTests/media/media-vp8-webm-seek-to-start-expected.html: Added.
* LayoutTests/media/media-vp8-webm-seek-to-start.html: Added.
* LayoutTests/platform/mac-wk2/TestExpectations:
* Source/WebCore/platform/graphics/cocoa/MediaPlayerPrivateWebM.mm:
(WebCore::MediaPlayerPrivateWebM::seekInternal):

Canonical link: https://commits.webkit.org/280648@main

Source/WebCore/platform/graphics/cocoa/MediaPlayerPrivateWebM.mm
@@ -368,7 +368,6 @@ void MediaPlayerPrivateWebM::seekInternal()
 
     auto pendingSeek = std::exchange(m_pendingSeek, { }).value();
     m_lastSeekTime = pendingSeek.time;
-    [m_synchronizer setRate:0 time:PAL::toCMTime(m_lastSeekTime)];
 
     m_seekState = Seeking;
 
@@ -386,6 +385,7 @@ void MediaPlayerPrivateWebM::seekInternal()
     }
 
     flush();
+    [m_synchronizer setRate:0 time:PAL::toCMTime(m_lastSeekTime)];
 
     for (auto& trackBufferPair : m_trackBufferMap) {
         TrackBuffer& trackBuffer = trackBufferPair.second;

LayoutTests/media/media-vp8-webm-seek-to-start-expected.html
@@ -1,0 +1,8 @@
+<html>
+  <body>
+    <video src="content/test-vp8.webm"></video>
+    <div>
+      <span>END OF TEST<br></span>
+    </div>
+  </body>
+</html>

LayoutTests/media/media-vp8-webm-seek-to-start.html
@@ -1,0 +1,26 @@
+<html>
+<meta name="fuzzy" content="maxDifference=0-2; totalPixels=0-275" />
+<head>
+<title>webm file with vp8</title>
+<script>window.requirePixelDump = true</script>
+<script src="video-test.js"></script>
+<script>
+    async function init()
+    {
+        let v = document.getElementsByTagName('video')[0];
+        v.src = "content/test-vp8.webm";
+        await waitFor(v, 'canplay', true);
+        v.play();
+        // duration of the last frame.
+        v.currentTime = v.duration - 0.02;
+        await Promise.all([ waitFor(v, 'seeked', true), waitFor(v, 'ended', true) ]);
+        v.currentTime = 0;
+        await waitFor(v, 'seeked', true);
+        endTest();
+    }
+</script>
+</head>
+<body onload="init();">
+<video/>
+</body>
+</html>

LayoutTests/platform/mac-site-isolation/TestExpectations
@@ -10815,6 +10815,7 @@ media/media-source/media-managedmse-seek.html [ ImageOnlyFailure ]
 media/media-source/media-source-video-renders.html [ ImageOnlyFailure ]
 media/media-sources-selection.html [ ImageOnlyFailure ]
 media/media-vp8-webm.html [ ImageOnlyFailure ]
+media/media-vp8-webm-seek-to-start.html [ ImageOnlyFailure ]
 media/track/track-webvtt-no-snap-to-lines-overlap.html [ ImageOnlyFailure ]
 media/track/track-webvtt-snap-to-lines-inline-style.html [ ImageOnlyFailure ]
 media/track/track-webvtt-snap-to-lines-left-right.html [ ImageOnlyFailure ]

LayoutTests/platform/mac-wk1/TestExpectations
@@ -1075,6 +1075,7 @@ media/W3C/video/canPlayType/canPlayType_two_implies_one_2.html [ Failure ]
 media/media-can-play-webm.html [ Failure ]
 media/media-vp8-webm-error.html [ Failure ]
 media/media-vp8-webm.html [ Skip ]
+media/media-vp8-webm-seek-to-start.html [ Skip ]
 media/video-src-webm-blob.html [ Failure ]
 media/track/media-audio-track.html [ Failure ]
 media/media-sources-selection.html [ ImageOnlyFailure ]

LayoutTests/platform/mac-wk2/TestExpectations
@@ -1618,7 +1618,8 @@ http/tests/contentextensions/block-everything-unless-domain-redirect.py [ Pass F
 
 webkit.org/b/262411 [ Sonoma+ ] fast/scrolling/scroll-to-anchor-zoomed-header.html [ Pass Crash ]
 
-[ Ventura+ ] media/media-vp8-webm.html [ ImageOnlyFailure ] # Missing AVSampleBufferDisplayLayer's ReadyForDisplayDidChangeNotification
+[ Ventura ] media/media-vp8-webm.html [ ImageOnlyFailure ] # Missing AVSampleBufferDisplayLayer's ReadyForDisplayDidChangeNotification
+[ Ventura ] media/media-vp8-webm-seek-to-start.html [ ImageOnlyFailure ] # Missing AVSampleBufferDisplayLayer's ReadyForDisplayDidChangeNotification
 
 webkit.org/b/263136 [ Sonoma+ ] imported/w3c/web-platform-tests/css/css-text/line-break/line-break-anywhere-overrides-uax-behavior-011.html [ ImageOnlyFailure ]
 