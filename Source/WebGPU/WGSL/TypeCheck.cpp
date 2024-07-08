3b1cf88ddf2b118045096099a656ced989c7546d
Tadeu Zagallo <tzagallo@apple.com>
2024-07-08 05:22:22 -0700

    [WGSL] Offset parameter to texture functions should be a const-expression https://bugs.webkit.org/show_bug.cgi?id=276226 rdar://130227871
    Reviewed by Mike Wyrzykowski.
    
    According to the spec, the `offset` argument to texture functions must be a
    const-expression, and the values must be between -8 and 7.
    
    * Source/WebGPU/WGSL/TypeCheck.cpp:
    (WGSL::TypeChecker::visit):
    * Source/WebGPU/WGSL/tests/invalid/texture-offset.wgsl: Added.
    
    Canonical link: https://commits.webkit.org/280726@main

--- Source/WebGPU/WGSL/TypeCheck.cpp
+++ Source/WebGPU/WGSL/TypeCheck.cpp
@@ -1393,16 +1393,47 @@ void TypeChecker::visit(AST::CallExpression& call)
                 m_shaderModule.setUsesDot4U8Packed();
             else if (targetName == "extractBits"_s)
                 m_shaderModule.setUsesExtractBits();
-            else if (targetName == "textureGather"_s) {
-                auto& component = call.arguments()[0];
-                if (satisfies(component.inferredType(), Constraints::ConcreteInteger)) {
-                    auto& constant = component.constantValue();
-                    if (!constant)
-                        typeError(InferBottom::No, component.span(), "the component argument must be a const-expression"_s);
-                    else {
-                        auto componentValue = constant->integerValue();
-                        if (componentValue < 0 || componentValue > 3)
-                            typeError(InferBottom::No, component.span(), "the component argument must be at least 0 and at most 3. component is "_s, String::number(componentValue));
+            else if (
+                targetName == "textureGather"_s
+                || targetName == "textureGatherCompare"_s
+                || targetName == "textureSample"_s
+                || targetName == "textureSampleBias"_s
+                || targetName == "textureSampleCompare"_s
+                || targetName == "textureSampleCompareLevel"_s
+                || targetName == "textureSampleGrad"_s
+                || targetName == "textureSampleLevel"_s
+            ) {
+                if (targetName == "textureGather"_s) {
+                    auto& component = call.arguments()[0];
+                    if (satisfies(component.inferredType(), Constraints::ConcreteInteger)) {
+                        auto& constant = component.constantValue();
+                        if (!constant)
+                            typeError(InferBottom::No, component.span(), "the component argument must be a const-expression"_s);
+                        else {
+                            auto componentValue = constant->integerValue();
+                            if (componentValue < 0 || componentValue > 3)
+                                typeError(InferBottom::No, component.span(), "the component argument must be at least 0 and at most 3. component is "_s, String::number(componentValue));
+                        }
+                    }
+                }
+
+                auto& lastArg = call.arguments().last();
+                auto* vectorType = std::get_if<Types::Vector>(lastArg.inferredType());
+                if (!vectorType || vectorType->size != 2 || vectorType->element != m_types.i32Type())
+                    return;
+
+                auto& maybeConstant = lastArg.constantValue();
+                if (!maybeConstant.has_value()) {
+                    typeError(InferBottom::No, lastArg.span(), "the offset argument must be a const-expression"_s);
+                    return;
+                }
+
+                auto& vector = std::get<ConstantVector>(*maybeConstant);
+                for (unsigned i = 0; i < 2; ++i) {
+                    auto& i32 = std::get<int32_t>(vector.elements[i]);
+                    if (i32 < -8 || i32 > 7) {
+                        typeError(InferBottom::No, lastArg.span(), "each component of the offset argument must be at least -8 and at most 7. offset component "_s, String::number(i), " is "_s, String::number(i32));
+                        break;
                     }
                 }
             }
