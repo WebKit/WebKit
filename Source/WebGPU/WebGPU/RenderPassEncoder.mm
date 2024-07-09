c354673f409eb379afab8a289de881ad2fa968e3
Mike Wyrzykowski <mwyrzykowski@apple.com>
2024-07-09 12:24:43 -0700

    [WebGPU] OOB buffer access as offset is not considered in max vertex counts https://bugs.webkit.org/show_bug.cgi?id=276368 <radar://131387875>
    Reviewed by Tadeu Zagallo.
    
    Vertex / instance count calculations failed to account for the buffer offset.
    
    * Source/WebGPU/WebGPU/RenderPassEncoder.mm:
    (WebGPU::RenderPassEncoder::computeMininumVertexInstanceCount const):
    
    Canonical link: https://commits.webkit.org/280789@main

--- Source/WebGPU/WebGPU/RenderPassEncoder.mm
+++ Source/WebGPU/WebGPU/RenderPassEncoder.mm
@@ -583,7 +583,7 @@ std::pair<uint32_t, uint32_t> RenderPassEncoder::computeMininumVertexInstanceCou
     auto& requiredBufferIndices = m_pipeline->requiredBufferIndices();
     for (auto& [bufferIndex, bufferData] : requiredBufferIndices) {
         auto it = m_vertexBuffers.find(bufferIndex);
-        auto bufferSize = it == m_vertexBuffers.end() ? 0 : it->value.buffer.length;
+        auto bufferSize = it == m_vertexBuffers.end() || (it->value.buffer.length < it->value.offset) ? 0 : (it->value.buffer.length - it->value.offset);
         auto stride = bufferData.stride;
         auto lastStride = bufferData.lastStride;
         if (!stride)
