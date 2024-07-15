ce7c640b3ef889c94846bee7e4e9fcdb7f00a704
AlexsanderDamaceno <aemgbo@gmail.com>
2024-07-15 04:36:55 -0300

    Values other than list-item should hide marker. https://bugs.webkit.org/show_bug.cgi?id=264381.
    Reviewed by NOBODY (OOPS!).
    
    Make the marker appear on the summary element when we define only a list-item display mode.
    
    * Source/WebCore/html/shadow/DetailsMarkerControl.cpp:
    (WebCore::DetailsMarkerControl::rendererIsNeeded):

--- Source/WebCore/html/shadow/DetailsMarkerControl.cpp
+++ Source/WebCore/html/shadow/DetailsMarkerControl.cpp
@@ -61,7 +61,7 @@ RenderPtr<RenderElement> DetailsMarkerControl::createElementRenderer(RenderStyle
 
 bool DetailsMarkerControl::rendererIsNeeded(const RenderStyle& style)
 {
-    return downcast<HTMLSummaryElement>(shadowHost())->isActiveSummary() && HTMLDivElement::rendererIsNeeded(style);
+    return downcast<HTMLSummaryElement>(shadowHost())->isActiveSummary() && HTMLDivElement::rendererIsNeeded(style) && downcast<HTMLSummaryElement>(shadowHost())->computedStyle()->display() == DisplayType::ListItem;
 }
 
 }
