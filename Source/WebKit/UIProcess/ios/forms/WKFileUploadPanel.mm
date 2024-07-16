6050042505a6733698f01da528796c4542e41d1e
Aditya Keerthi <akeerthi@apple.com>
2024-07-15 17:24:40 -0700

    [tvOS] File upload panel is invisible and cannot be dismissed https://bugs.webkit.org/show_bug.cgi?id=276627 rdar://127010503
    Reviewed by Richard Robinson and Wenson Hsieh.
    
    * Source/WebKit/UIProcess/ios/forms/WKFileUploadPanel.mm:
    
    Disable presentation of the file upload panel on tvOS.
    
    Canonical link: https://commits.webkit.org/280994@main

--- Source/WebKit/UIProcess/ios/forms/WKFileUploadPanel.mm
+++ Source/WebKit/UIProcess/ios/forms/WKFileUploadPanel.mm
@@ -1338,7 +1338,7 @@ static NSString *displayStringForDocumentsAtURLs(NSArray<NSURL *> *urls)
 
 - (BOOL)platformSupportsPickerViewController
 {
-#if PLATFORM(WATCHOS)
+#if PLATFORM(WATCHOS) || PLATFORM(APPLETV)
     return NO;
 #else
     return YES;
