0c2fef17e043af0f0b820d1e1d39d720ba1b6508
Youenn Fablet <youenn@apple.com>
2024-07-22 05:41:04 -0700

    Remove unneeded protobuf cmake generated files https://bugs.webkit.org/show_bug.cgi?id=276879 rdar://132227290
    Reviewed by Jean-Yves Avenard.
    
    These files were generated locally as part of building protoc.
    Removing them since they are not useful in the repository.
    
    * Source/ThirdParty/libwebrtc/Source/third_party/protobuf/cmake/CMakeFiles: Removed.
    * Source/ThirdParty/libwebrtc/Source/third_party/protobuf/cmake/CMakeLists.txt: Removed.
    * Source/ThirdParty/libwebrtc/Source/third_party/protobuf/cmake/extract_includes.bat.in: Removed.
    * Source/ThirdParty/libwebrtc/Source/third_party/protobuf/cmake/lib: Removed.
    
    Canonical link: https://commits.webkit.org/281188@main

--- Source/ThirdParty/libwebrtc/Source/third_party/protobuf/cmake/CMakeFiles/libprotobuf.dir/Users/youenn/dev/Apple/OpenSource/Source/ThirdParty/libwebrtc/Source/third_party/protobuf/src/google/protobuf/io/io_win32.cc.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/libprotobuf.dir/Users/youenn/dev/Apple/OpenSource/Source/ThirdParty/libwebrtc/Source/third_party/protobuf/src/google/protobuf/io/io_win32.cc.o: \
-  /Users/youenn/dev/Apple/OpenSource/Source/ThirdParty/libwebrtc/Source/third_party/protobuf/src/google/protobuf/io/io_win32.cc
