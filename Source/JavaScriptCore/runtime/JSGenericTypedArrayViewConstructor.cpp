d4f01e6351bd87baeee94ed9ed5c6042a40f145f
Jarred Sumner <jarred@jarredsumner.com>
2024-07-27 21:24:43 -0700

    Add missing includes + disable macOS LTO build for now

--- Source/JavaScriptCore/runtime/JSGenericTypedArrayViewConstructor.cpp
+++ Source/JavaScriptCore/runtime/JSGenericTypedArrayViewConstructor.cpp
@@ -24,7 +24,9 @@
  */
 
 #include "config.h"
+#include "JSGenericTypedArrayView.h"
 #include "JSGenericTypedArrayViewConstructor.h"
+#include "JSGenericTypedArrayViewConstructorInlines.h"
 
 #include "ParseInt.h"
 #include <wtf/text/Base64.h>
