1b25554251447179a862eb544b6323a473b9afc6
Aditya Keerthi <akeerthi@apple.com>
2024-07-29 08:40:20 -0700

    REGRESSION (281060@main): [iOS] Crash when entering Japanese text into a text box https://bugs.webkit.org/show_bug.cgi?id=277251 rdar://132702363
    Reviewed by Wenson Hsieh.
    
    281060@main introduced an unconditional call to
    `-[NSAttributedString attributesAtIndex:effectiveRange:]` in the marked text
    codepath, which is used for both IME and inline text predictions.
    
    This is unsafe, as an empty string is inserted when accepting an IME composition.
    Fix by eliding the call to `-[NSAttributedString attributesAtIndex:effectiveRange:]`
    when the text is empty.
    
    * Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm:
    (-[WKContentView setAttributedMarkedText:selectedRange:]):
    * Tools/TestWebKitAPI/Tests/ios/KeyboardInputTestsIOS.mm:
    (TestWebKitAPI::TEST(KeyboardInputTests, NoCrashWithEmptyAttributedMarkedText)):
    
    Reproducing the issue in a test requires the use of `NSMutableAttributedString`.
    Specifically, the crash is in the run-length encoded array used to keep track
    of attributes. The issue only reproduces when that array is modified. Simply
    creating an empty attributed string is insufficient to reproduce the crash.
    
    Canonical link: https://commits.webkit.org/281501@main

--- Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
+++ Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -6307,6 +6307,9 @@ static Vector<WebCore::CompositionHighlight> compositionHighlights(NSAttributedS
 - (void)setAttributedMarkedText:(NSAttributedString *)markedText selectedRange:(NSRange)selectedRange
 {
     BOOL hasTextCompletion = ^{
+        if (!markedText.length)
+            return NO;
+
         // UIKit doesn't include the `NSTextCompletionAttributeName`, so the next best way to detect if this method
         // is being used for a text completion is to check if the attributes match these hard-coded ones.
         RetainPtr textCompletionAttributes = @{
