659fdc463755f03380a39d10cd0bcb995a7a404f
sosukesuzuki <aosukeke@gmail.com>
2024-08-01 00:17:14 +0900

    [JSC] Enable `Promise.try` and `RegExp.escape` https://bugs.webkit.org/show_bug.cgi?id=277406
    Reviewed by NOBODY (OOPS!).
    
    `Promise.try` proposal[1] reached Stage 3 in June 2024[2]. `RegExp.escape` proposal[3] reached
    Stage 3 in July 2024[4].
    
    This patch enables those proposals.
    
    [1]: https://github.com/tc39/proposal-promise-try
    [2]: https://github.com/tc39/agendas/blob/main/2024/06.md
    [3]: https://github.com/tc39/proposal-regex-escaping
    [4]: https://github.com/tc39/agendas/blob/main/2024/07.md
    
    * LayoutTests/js/Object-getOwnPropertyNames-expected.txt:
    * LayoutTests/js/dom/regexp-caching-expected.txt:
    * LayoutTests/js/script-tests/Object-getOwnPropertyNames.js:
    * Source/JavaScriptCore/runtime/OptionsList.h:

--- Source/JavaScriptCore/runtime/OptionsList.h
+++ Source/JavaScriptCore/runtime/OptionsList.h
@@ -591,8 +591,8 @@ bool hasCapacityToUseLargeGigacage();
     v(Bool, useImportAttributes, true, Normal, "Enable import attributes."_s) \
     v(Bool, useIntlDurationFormat, true, Normal, "Expose the Intl DurationFormat."_s) \
     v(Bool, usePromiseWithResolversMethod, true, Normal, "Expose the Promise.withResolvers() method."_s) \
-    v(Bool, usePromiseTryMethod, false, Normal, "Expose the Promise.try() method."_s) \
-    v(Bool, useRegExpEscape, false, Normal, "Expose RegExp.escape feature."_s) \
+    v(Bool, usePromiseTryMethod, true, Normal, "Expose the Promise.try() method."_s) \
+    v(Bool, useRegExpEscape, true, Normal, "Expose RegExp.escape feature."_s) \
     v(Bool, useResizableArrayBuffer, true, Normal, "Expose ResizableArrayBuffer feature."_s) \
     v(Bool, useSharedArrayBuffer, false, Normal, nullptr) \
     v(Bool, useShadowRealm, false, Normal, "Expose the ShadowRealm object."_s) \
