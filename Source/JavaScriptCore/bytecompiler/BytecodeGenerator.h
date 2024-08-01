e62dd90f8a790c36c09758e89f3701a07cabc547
Dylan Conway <dylan.conway567@gmail.com>
2024-08-01 12:33:57 -0700

    use default

--- Source/JavaScriptCore/bytecompiler/BytecodeGenerator.h
+++ Source/JavaScriptCore/bytecompiler/BytecodeGenerator.h
@@ -1060,7 +1060,7 @@ namespace JSC {
 
         CodeType codeType() const { return m_codeType; }
 
-        bool shouldBeConcernedWithCompletionValue() const { return m_defaultAllowCallIgnoreResultOptimization ? m_codeType != FunctionCode : false; }
+        bool shouldBeConcernedWithCompletionValue() const { return !m_defaultAllowCallIgnoreResultOptimization; }
 
         bool shouldEmitDebugHooks() const { return m_codeGenerationMode.contains(CodeGenerationMode::Debugger) && !m_isBuiltinFunction; }
         bool shouldEmitTypeProfilerHooks() const { return m_codeGenerationMode.contains(CodeGenerationMode::TypeProfiler); }
