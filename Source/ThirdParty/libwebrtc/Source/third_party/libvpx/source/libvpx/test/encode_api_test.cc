0203417948253baadd130950e5549dd6cb6e73c9
Mohsin Qureshi <mohsinq@apple.com>
2024-08-02 12:12:53 -0700

    Revert c143dc71ee90. rdar://132736239

--- Source/ThirdParty/libwebrtc/Source/third_party/libvpx/source/libvpx/test/encode_api_test.cc
+++ Source/ThirdParty/libwebrtc/Source/third_party/libvpx/source/libvpx/test/encode_api_test.cc
@@ -20,7 +20,6 @@
 #include "third_party/googletest/src/include/gtest/gtest.h"
 #include "test/acm_random.h"
 #include "test/video_source.h"
-include "test/y4m_video_source.h"
 
 #include "./vpx_config.h"
 #include "vpx/vp8cx.h"
@@ -476,109 +475,6 @@ TEST(EncodeAPI, VP8GlobalHeaders) {
   EXPECT_NO_FATAL_FAILURE(EncodeWithConfig(cfg, &enc.ctx));
   EXPECT_EQ(vpx_codec_get_global_headers(&enc.ctx), nullptr);
 }
-
-TEST(EncodeAPI, OssFuzz69100) {
-  // Initialize libvpx encoder.
-  vpx_codec_iface_t *const iface = vpx_codec_vp8_cx();
-  vpx_codec_ctx_t enc;
-  vpx_codec_enc_cfg_t cfg;
-
-  ASSERT_EQ(vpx_codec_enc_config_default(iface, &cfg, 0), VPX_CODEC_OK);
-
-  cfg.g_w = 64;
-  cfg.g_h = 64;
-  cfg.g_lag_in_frames = 25;
-  cfg.g_timebase.num = 1;
-  cfg.g_timebase.den = 6240592;
-  cfg.rc_target_bitrate = 1202607620;
-  cfg.kf_max_dist = 24377;
-
-  ASSERT_EQ(vpx_codec_enc_init(&enc, iface, &cfg, 0), VPX_CODEC_OK);
-
-  ASSERT_EQ(vpx_codec_control(&enc, VP8E_SET_CPUUSED, 1), VPX_CODEC_OK);
-  ASSERT_EQ(vpx_codec_control(&enc, VP8E_SET_ARNR_MAXFRAMES, 0), VPX_CODEC_OK);
-  ASSERT_EQ(vpx_codec_control(&enc, VP8E_SET_ARNR_STRENGTH, 3), VPX_CODEC_OK);
-  ASSERT_EQ(vpx_codec_control_(&enc, VP8E_SET_ARNR_TYPE, 3),
-            VPX_CODEC_OK);  // deprecated
-  ASSERT_EQ(vpx_codec_control(&enc, VP8E_SET_NOISE_SENSITIVITY, 0),
-            VPX_CODEC_OK);
-  ASSERT_EQ(vpx_codec_control(&enc, VP8E_SET_TOKEN_PARTITIONS, 0),
-            VPX_CODEC_OK);
-  ASSERT_EQ(vpx_codec_control(&enc, VP8E_SET_STATIC_THRESHOLD, 0),
-            VPX_CODEC_OK);
-
-  libvpx_test::RandomVideoSource video;
-  video.set_limit(30);
-  video.SetSize(cfg.g_w, cfg.g_h);
-  video.SetImageFormat(VPX_IMG_FMT_I420);
-  video.Begin();
-  do {
-    ASSERT_EQ(vpx_codec_encode(&enc, video.img(), video.pts(), video.duration(),
-                               /*flags=*/0, VPX_DL_GOOD_QUALITY),
-              VPX_CODEC_OK);
-    video.Next();
-  } while (video.img() != nullptr);
-
-  ASSERT_EQ(vpx_codec_destroy(&enc), VPX_CODEC_OK);
-}
-
-void EncodeOssFuzz69906(int cpu_used, vpx_enc_deadline_t deadline) {
-  char str[80];
-  snprintf(str, sizeof(str), "cpu_used: %d deadline: %d", cpu_used,
-           static_cast<int>(deadline));
-  SCOPED_TRACE(str);
-
-  // Initialize libvpx encoder.
-  vpx_codec_iface_t *const iface = vpx_codec_vp8_cx();
-  vpx_codec_ctx_t enc;
-  vpx_codec_enc_cfg_t cfg;
-
-  ASSERT_EQ(vpx_codec_enc_config_default(iface, &cfg, 0), VPX_CODEC_OK);
-
-  cfg.g_w = 4097;
-  cfg.g_h = 16;
-  cfg.rc_target_bitrate = 1237084865;
-  cfg.kf_max_dist = 4336;
-
-  ASSERT_EQ(vpx_codec_enc_init(&enc, iface, &cfg, 0), VPX_CODEC_OK);
-
-  ASSERT_EQ(vpx_codec_control(&enc, VP8E_SET_CPUUSED, cpu_used), VPX_CODEC_OK);
-  ASSERT_EQ(vpx_codec_control(&enc, VP8E_SET_ARNR_MAXFRAMES, 0), VPX_CODEC_OK);
-  ASSERT_EQ(vpx_codec_control(&enc, VP8E_SET_ARNR_STRENGTH, 3), VPX_CODEC_OK);
-  ASSERT_EQ(vpx_codec_control_(&enc, VP8E_SET_ARNR_TYPE, 3),
-            VPX_CODEC_OK);  // deprecated
-  ASSERT_EQ(vpx_codec_control(&enc, VP8E_SET_NOISE_SENSITIVITY, 0),
-            VPX_CODEC_OK);
-  ASSERT_EQ(vpx_codec_control(&enc, VP8E_SET_TOKEN_PARTITIONS, 0),
-            VPX_CODEC_OK);
-  ASSERT_EQ(vpx_codec_control(&enc, VP8E_SET_STATIC_THRESHOLD, 0),
-            VPX_CODEC_OK);
-
-  libvpx_test::Y4mVideoSource video("repro-oss-fuzz-69906.y4m", /*start=*/0,
-                                    /*limit=*/3);
-  video.Begin();
-  do {
-    ASSERT_EQ(vpx_codec_encode(&enc, video.img(), video.pts(), video.duration(),
-                               /*flags=*/0, deadline),
-              VPX_CODEC_OK);
-    video.Next();
-  } while (video.img() != nullptr);
-
-  ASSERT_EQ(vpx_codec_destroy(&enc), VPX_CODEC_OK);
-}
-
-TEST(EncodeAPI, OssFuzz69906) {
-  // Note the original bug report was for speed 1, good quality. The remainder
-  // of the settings are for added coverage.
-  for (int cpu_used = 0; cpu_used <= 5; ++cpu_used) {
-    EncodeOssFuzz69906(cpu_used, VPX_DL_GOOD_QUALITY);
-  }
-
-  for (int cpu_used = -16; cpu_used <= -5; ++cpu_used) {
-    EncodeOssFuzz69906(cpu_used, VPX_DL_REALTIME);
-  }
-}
-
 #endif  // CONFIG_VP8_ENCODER
 
 // Set up 2 spatial streams with 2 temporal layers per stream, and generate
