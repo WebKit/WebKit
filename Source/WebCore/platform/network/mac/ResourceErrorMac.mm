d60a7835829969b56d2276a4210cb60f0636a4b4
Guoye Zhang <guoye_zhang@apple.com>
2024-08-06 09:55:30 -0700

    Cherry-pick d89cd1cf5382. rdar://132952350
        DNS filtering not flagging expected domains
        https://bugs.webkit.org/show_bug.cgi?id=277515
        rdar://132952350 (DNS filtering not flagging expected domains)
    
        Reviewed by Alex Christensen.
    
        The new network loader puts information in the top level user info dictionary instead of the underlying error's user info dictionary.
    
        * Source/WebCore/platform/network/mac/ResourceErrorMac.mm:
        (WebCore::ResourceError::blockedKnownTracker const):
        (WebCore::ResourceError::blockedTrackerHostName const):
        * Source/WebKit/NetworkProcess/cocoa/NetworkSessionCocoa.mm:
        (-[WKNetworkSessionDelegate URLSession:task:didCompleteWithError:]):
    
        Canonical link: https://commits.webkit.org/281735@main
    
    Canonical link: https://commits.webkit.org/280938.199@safari-7619-branch

--- Source/WebCore/platform/network/mac/ResourceErrorMac.mm
+++ Source/WebCore/platform/network/mac/ResourceErrorMac.mm
@@ -278,6 +278,9 @@ ResourceError::operator CFErrorRef() const
 
 bool ResourceError::blockedKnownTracker() const
 {
+    if (id blockedTrackerFailure = nsError().userInfo[@"_NSURLErrorBlockedTrackerFailureKey"])
+        return [blockedTrackerFailure boolValue];
+    // This loop can be removed when the CFNetwork loader is no longer in use
     for (NSError *underlyingError in nsError().underlyingErrors) {
         if ([underlyingError.userInfo[@"_NSURLErrorBlockedTrackerFailureKey"] boolValue])
             return true;
@@ -289,6 +292,13 @@ String ResourceError::blockedTrackerHostName() const
 {
     ASSERT(blockedKnownTracker());
 
+    if (id failingPath = nsError().userInfo[@"_NSURLErrorNWPathKey"]) {
+        auto failingEndpoint = adoptNS(nw_path_copy_effective_remote_endpoint(failingPath));
+        if (auto* hostName = nw_endpoint_get_known_tracker_name(failingEndpoint.get()))
+            return String::fromUTF8(hostName);
+        return { };
+    }
+    // This loop can be removed when the CFNetwork loader is no longer in use
     for (NSError *underlyingError in nsError().underlyingErrors) {
         if (id failingPath = underlyingError.userInfo[@"_NSURLErrorNWPathKey"]) {
             auto failingEndpoint = adoptNS(nw_path_copy_effective_remote_endpoint(failingPath));
