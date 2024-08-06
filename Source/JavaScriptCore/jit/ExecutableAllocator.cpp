WebKit/WebKit

commit e41732697b999464e80dc3fe1c95900c6c438888

Implement JITless JS entry thunk to Wasm https://bugs.webkit.org/show_bug.cgi?id=277266 rdar://113768974
Reviewed by Yusuke Suzuki.

This patch builds on Justin's previous patch (https://commits.webkit.org/280485@main) to fully implement JITless entry to Wasm  code. This now allows us to run WebAssembly without enabling JIT. The lack of generated JIT thunks should significantly improve memory performance on programs with many call signatures.

Performance Testing: (N=10)
---------------------------

JetStream 2.0: 431.3 +/- 0.96 pt; up from 429.6 +/- 0.77 pt (0.40% significant progression)
Membuster 5: 1.357 GB +/- 3 MB; up from 1.354 GB +/- 3 MB (0.18% insignificant regression)

* JSTests/microbenchmarks/wasm-cc-int-to-int.js:
(wasm_instance.exports):
* JSTests/wasm/lowExecutableMemory/executable-memory-oom.js:
* JSTests/wasm/lowExecutableMemory/exports-oom.js:
(failCount.0.catch): Deleted.
(failCount.0.module.undefined.catch): Deleted.
* JSTests/wasm/noJIT/noJIT.js:
* JSTests/wasm/stress/cc-double-to-double.js: Copied from JSTests/wasm/stress/cc-int-to-int.js.
(from.string_appeared_here.import.as.assert.from.string_appeared_here.let.wat.module.type.sig_test.func.param.f64.result.f64.table.t.1.funcref.elem.i32.const.0.test.func.test.export.string_appeared_here.param.x.f64.result.f64.f64.add.local.x.f64.const.42.func.export.string_appeared_here.param.x.f64.result.f64.f64.add.local.x.call.test.f64.const.1337.func.export.string_appeared_here.param.x.f64.result.f64.local.x.f64.const.98.call_indirect.t.type.sig_test.i32.const.0.f64.add.async test):
* JSTests/wasm/stress/cc-f32-kitchen-sink.js: Added.

Source/JavaScriptCore/llint/LowLevelInterpreter.asm
@@ -2766,6 +2766,10 @@ _wasmLLIntPCRangeEnd:
 else
 
 # These need to be defined even when WebAssembly is disabled
+op(js_to_wasm_wrapper_entry_crash_for_simd_parameters, macro ()
+    crash()
+end)
+
 op(js_to_wasm_wrapper_entry, macro ()
     crash()
 end)
@@ -2775,6 +2779,10 @@ op(wasm_function_prologue_trampoline, macro ()
 end)
 
 op(wasm_function_prologue, macro ()
+    crash()
+end)
+
+op(wasm_function_prologue_simd_trampoline, macro ()
     crash()
 end)
 

commit f0f17a7e056d8b93169187544432a702623e08ca

[Win] Enable UNIFIED_AND_FREEZABLE_CONFIG_RECORD https://bugs.webkit.org/show_bug.cgi?id=275859
Reviewed by Yusuke Suzuki.

Freezing hasn't been implemented, but after this change we can remove
the X86_64_WIN backend from offlineasm.

This also fixes wasm on Windows, which was broken by
e41732697b999464e80dc3fe1c95900c6c438888

* Source/JavaScriptCore/jit/ExecutableAllocator.cpp:
(JSC::initializeJITPageReservation):
* Source/JavaScriptCore/llint/LowLevelInterpreter.asm:
* Source/JavaScriptCore/llint/LowLevelInterpreter.cpp:
* Source/JavaScriptCore/llint/LowLevelInterpreter32_64.asm:
* Source/JavaScriptCore/llint/LowLevelInterpreter64.asm:
* Source/JavaScriptCore/llint/WebAssembly.asm:
* Source/JavaScriptCore/runtime/InitializeThreading.cpp:
(JSC::initialize):
* Source/JavaScriptCore/runtime/JSCConfig.cpp:
* Source/JavaScriptCore/runtime/JSCConfig.h:
* Source/JavaScriptCore/runtime/Options.cpp:
(JSC::Options::notifyOptionsChanged):
* Source/WTF/wtf/PlatformEnable.h:
* Source/WTF/wtf/WTFConfig.cpp:
(WTF::setPermissionsOfConfigPage):
(WTF::Config::permanentlyFreeze):
* Source/WTF/wtf/WTFConfig.h:
(WTF::setPermissionsOfConfigPage): Deleted.
* Source/bmalloc/bmalloc/BPlatform.h:
* Source/bmalloc/bmalloc/Gigacage.cpp:
(Gigacage::ensureGigacage):
* Source/bmalloc/bmalloc/GigacageConfig.h:
* Source/bmalloc/bmalloc/mbmalloc.cpp:
* Source/cmake/OptionsWin.cmake:

Canonical link: https://commits.webkit.org/281876@main

Source/JavaScriptCore/jit/ExecutableAllocator.cpp
@@ -442,7 +442,7 @@ static ALWAYS_INLINE JITReservation initializeJITPageReservation()
         g_jscConfig.startExecutableMemory = reservation.base;
         g_jscConfig.endExecutableMemory = reservationEnd;
 
-#if !USE(SYSTEM_MALLOC) && ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
+#if !USE(SYSTEM_MALLOC)
         static_assert(WebConfig::reservedSlotsForExecutableAllocator >= 2);
         WebConfig::g_config[0] = bitwise_cast<uintptr_t>(reservation.base);
         WebConfig::g_config[1] = bitwise_cast<uintptr_t>(reservationEnd);

Source/JavaScriptCore/llint/LowLevelInterpreter.asm
@@ -318,16 +318,10 @@ const OpcodeIDWide32SizeJS = 2 # Wide32 Prefix + OpcodeID
 const OpcodeIDWide16SizeWasm = 2 # Wide16 Prefix + OpcodeID(1 byte)
 const OpcodeIDWide32SizeWasm = 2 # Wide32 Prefix + OpcodeID(1 byte)
 
-if X86_64_WIN or C_LOOP_WIN
-    const WTFConfig = _g_wtfConfigForLLInt
-    const GigacageConfig = _g_gigacageConfig
-    const JSCConfig = _g_jscConfig
-else
-    const WTFConfig = _g_config + constexpr WTF::startOffsetOfWTFConfig
-    const GigacageConfig = _g_config + constexpr Gigacage::startOffsetOfGigacageConfig
-    const JSCConfigOffset = constexpr WTF::offsetOfWTFConfigExtension
-    const JSCConfigGateMapOffset = JSCConfigOffset + constexpr JSC::offsetOfJSCConfigGateMap
-end
+const WTFConfig = _g_config + constexpr WTF::startOffsetOfWTFConfig
+const GigacageConfig = _g_config + constexpr Gigacage::startOffsetOfGigacageConfig
+const JSCConfigOffset = constexpr WTF::offsetOfWTFConfigExtension
+const JSCConfigGateMapOffset = JSCConfigOffset + constexpr JSC::offsetOfJSCConfigGateMap
 
 macro nextInstruction()
     loadb [PB, PC, 1], t0
@@ -1945,13 +1939,8 @@ macro entry(kind, initialize)
         # Include generated bytecode initialization file.
         includeEntriesAtOffset(kind, initialize)
 
-        if X86_64_WIN or C_LOOP_WIN
-            leap JSCConfig + constexpr JSC::offsetOfJSCConfigInitializeHasBeenCalled, t3
-            bbeq [t3], 0, .notFrozen
-        else
-            leap _g_config, t3
-            bbeq JSCConfigOffset + constexpr JSC::offsetOfJSCConfigInitializeHasBeenCalled[t3], 0, .notFrozen
-        end
+        leap _g_config, t3
+        bbeq JSCConfigOffset + constexpr JSC::offsetOfJSCConfigInitializeHasBeenCalled[t3], 0, .notFrozen
         crash()
     .notFrozen:
 

Source/JavaScriptCore/llint/LowLevelInterpreter.cpp
@@ -88,9 +88,7 @@ using namespace JSC::LLInt;
 // Define the opcode dispatch mechanism when using the C loop:
 //
 
-#if ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
 using WebConfig::g_config;
-#endif
 
 // These are for building a C Loop interpreter:
 #define OFFLINE_ASM_BEGIN

Source/JavaScriptCore/llint/LowLevelInterpreter32_64.asm
@@ -2369,13 +2369,8 @@ macro callHelper(opcodeName, opcodeStruct, dispatchAfterCall, valueProfileName, 
         storep 0, address
     end)
     addp %opcodeStruct%::Metadata::m_callLinkInfo, t5, t2 # CallLinkInfo* in t2
-    if X86_64_WIN or C_LOOP_WIN
-        leap JSCConfig + constexpr JSC::offsetOfJSCConfigDefaultCallThunk, t5
-        loadp [t5], t5
-    else
-        leap _g_config, t5
-        loadp JSCConfigOffset + constexpr JSC::offsetOfJSCConfigDefaultCallThunk[t5], t5
-    end
+    leap _g_config, t5
+    loadp JSCConfigOffset + constexpr JSC::offsetOfJSCConfigDefaultCallThunk[t5], t5
     jmp .dispatch
 end
         
@@ -2459,13 +2454,8 @@ macro doCallVarargs(opcodeName, size, get, opcodeStruct, valueProfileName, dstVi
                 storep 0, address
             end)
             addp %opcodeStruct%::Metadata::m_callLinkInfo, t5, t2 # CallLinkInfo* in t2
-            if X86_64_WIN or C_LOOP_WIN
-                leap JSCConfig + constexpr JSC::offsetOfJSCConfigDefaultCallThunk, t5
-                loadp [t5], t5
-            else
-                leap _g_config, t5
-                loadp JSCConfigOffset + constexpr JSC::offsetOfJSCConfigDefaultCallThunk[t5], t5
-            end
+            leap _g_config, t5
+            loadp JSCConfigOffset + constexpr JSC::offsetOfJSCConfigDefaultCallThunk[t5], t5
             jmp .dispatch
         .dontUpdateSP:
             jmp _llint_throw_from_slow_path_trampoline

Source/JavaScriptCore/llint/LowLevelInterpreter64.asm
@@ -693,13 +693,8 @@ macro structureIDToStructureWithScratch(structureIDThenStructure, scratch)
         lshiftp (constexpr StructureID::encodeShiftAmount), structureIDThenStructure
     elsif ADDRESS64
         andq (constexpr StructureID::structureIDMask), structureIDThenStructure
-        if X86_64_WIN or C_LOOP_WIN
-            leap JSCConfig + constexpr JSC::offsetOfJSCConfigStartOfStructureHeap, scratch
-            loadp [scratch], scratch
-        else
-            leap _g_config, scratch
-            loadp JSCConfigOffset + constexpr JSC::offsetOfJSCConfigStartOfStructureHeap[scratch], scratch
-        end
+        leap _g_config, scratch
+        loadp JSCConfigOffset + constexpr JSC::offsetOfJSCConfigStartOfStructureHeap[scratch], scratch
         addp scratch, structureIDThenStructure
     end
 end
@@ -2531,13 +2526,8 @@ macro callHelper(opcodeName, opcodeStruct, dispatchAfterCall, valueProfileName, 
         storep 0, address
     end)
     addp %opcodeStruct%::Metadata::m_callLinkInfo, t5, t2 # CallLinkInfo* in t2
-    if X86_64_WIN or C_LOOP_WIN
-        leap JSCConfig + constexpr JSC::offsetOfJSCConfigDefaultCallThunk, t5
-        loadp [t5], t5
-    else
-        leap _g_config, t5
-        loadp JSCConfigOffset + constexpr JSC::offsetOfJSCConfigDefaultCallThunk[t5], t5
-    end
+    leap _g_config, t5
+    loadp JSCConfigOffset + constexpr JSC::offsetOfJSCConfigDefaultCallThunk[t5], t5
     jmp .dispatch
 end
 
@@ -2620,13 +2610,8 @@ macro doCallVarargs(opcodeName, size, get, opcodeStruct, valueProfileName, dstVi
                 storep 0, address
             end)
             addp %opcodeStruct%::Metadata::m_callLinkInfo, t5, t2 # CallLinkInfo* in t2
-            if X86_64_WIN or C_LOOP_WIN
-                leap JSCConfig + constexpr JSC::offsetOfJSCConfigDefaultCallThunk, t5
-                loadp [t5], t5
-            else
-                leap _g_config, t5
-                loadp JSCConfigOffset + constexpr JSC::offsetOfJSCConfigDefaultCallThunk[t5], t5
-            end
+            leap _g_config, t5
+            loadp JSCConfigOffset + constexpr JSC::offsetOfJSCConfigDefaultCallThunk[t5], t5
             jmp .dispatch
         .dontUpdateSP:
             jmp _llint_throw_from_slow_path_trampoline

Source/JavaScriptCore/llint/WebAssembly.asm
@@ -653,7 +653,7 @@ end
         if ARM64 or ARM64E
             storepairq memoryBase, boundsCheckingSize, -2 * SlotSize[cfr]
             storep wasmInstance, -3 * SlotSize[cfr]
-        elsif X86_64
+        elsif X86_64 or X86_64_WIN
             # These must match the wasmToJS thunk, since the unwinder won't be able to tell who made this frame.
             storep boundsCheckingSize, -1 * SlotSize[cfr]
             storep memoryBase, -2 * SlotSize[cfr]
@@ -667,7 +667,7 @@ end
         if ARM64 or ARM64E
             loadpairq -2 * SlotSize[cfr], memoryBase, boundsCheckingSize
             loadp -3 * SlotSize[cfr], wasmInstance
-        elsif X86_64
+        elsif X86_64 or X86_64_WIN
             loadp -1 * SlotSize[cfr], boundsCheckingSize
             loadp -2 * SlotSize[cfr], memoryBase
             loadp -3 * SlotSize[cfr], wasmInstance
@@ -716,7 +716,7 @@ end
     # Memory
     if ARM64 or ARM64E
         loadpairq JSWebAssemblyInstance::m_cachedMemory[wasmInstance], memoryBase, boundsCheckingSize
-    elsif X86_64
+    elsif X86_64 or X86_64_WIN
         loadp JSWebAssemblyInstance::m_cachedMemory[wasmInstance], memoryBase
         loadp JSWebAssemblyInstance::m_cachedBoundsCheckingSize[wasmInstance], boundsCheckingSize
     end

Source/JavaScriptCore/runtime/InitializeThreading.cpp
@@ -138,9 +138,6 @@ void initialize()
         WTF::compilerFence();
         RELEASE_ASSERT(!g_jscConfig.initializeHasBeenCalled);
         g_jscConfig.initializeHasBeenCalled = true;
-#if OS(WINDOWS) && ENABLE(WEBASSEMBLY)
-        g_wtfConfigForLLInt = g_wtfConfig;
-#endif
     });
 }
 

Source/JavaScriptCore/runtime/JSCConfig.cpp
@@ -29,14 +29,6 @@
 
 namespace JSC {
 
-#if !ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-Config g_jscConfig;
-#endif
-
-#if OS(WINDOWS) && ENABLE(WEBASSEMBLY)
-WTF::Config g_wtfConfigForLLInt = g_wtfConfig;
-#endif
-
 Config& Config::singleton()
 {
     return g_jscConfig;

Source/JavaScriptCore/runtime/JSCConfig.h
@@ -115,23 +115,12 @@ struct Config {
 #endif
 };
 
-#if ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
 constexpr size_t alignmentOfJSCConfig = std::alignment_of<JSC::Config>::value;
 
 static_assert(WTF::offsetOfWTFConfigExtension + sizeof(JSC::Config) <= WTF::ConfigSizeToProtect);
 static_assert(roundUpToMultipleOf<alignmentOfJSCConfig>(WTF::offsetOfWTFConfigExtension) == WTF::offsetOfWTFConfigExtension);
 
 #define g_jscConfig (*bitwise_cast<JSC::Config*>(&g_wtfConfig.spaceForExtensions))
-
-#else // not ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
-extern "C" JS_EXPORT_PRIVATE Config g_jscConfig;
-#if OS(WINDOWS) && ENABLE(WEBASSEMBLY)
-extern "C" JS_EXPORT_PRIVATE WTF::Config g_wtfConfigForLLInt;
-#endif
-
-#endif // ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
 
 constexpr size_t offsetOfJSCConfigInitializeHasBeenCalled = offsetof(JSC::Config, initializeHasBeenCalled);
 constexpr size_t offsetOfJSCConfigGateMap = offsetof(JSC::Config, llint.gateMap);
@@ -144,7 +133,3 @@ ALWAYS_INLINE PURE_FUNCTION uintptr_t startOfStructureHeap()
 }
 
 } // namespace JSC
-
-#if !ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-using JSC::g_jscConfig;
-#endif

Source/JavaScriptCore/runtime/Options.cpp
@@ -924,10 +924,8 @@ void Options::notifyOptionsChanged()
     Options::useWasmFastMemory() = false;
 #endif
 
-#if ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
     uint8_t* reservedConfigBytes = reinterpret_cast_ptr<uint8_t*>(WebConfig::g_config + WebConfig::reservedSlotsForExecutableAllocator);
     reservedConfigBytes[WebConfig::ReservedByteForAllocationProfiling] = Options::useAllocationProfiling() ? 1 : 0;
-#endif
 
     // Do range checks where needed and make corrections to the options:
     ASSERT(Options::thresholdForOptimizeAfterLongWarmUp() >= Options::thresholdForOptimizeAfterWarmUp());

Source/WTF/wtf/PlatformEnable.h
@@ -865,24 +865,6 @@
 #define ENABLE_SIGNAL_BASED_VM_TRAPS 1
 #endif
 
-/* The unified Config record feature is not available for Windows because the
-   Windows port puts WTF in a separate DLL, and the offlineasm code accessing
-   the config record expects the config record to be directly accessible like
-   a global variable (and not have to go thru DLL shenanigans). C++ code would
-   resolve these DLL bindings automatically, but offlineasm does not.
-
-   The permanently freezing feature also currently relies on the Config records
-   being unified, and the Windows port also does not currently have an
-   implementation for the freezing mechanism anyway. For simplicity, we just
-   disable both the use of unified Config record and config freezing for the
-   Windows port.
-*/
-#if OS(WINDOWS)
-#define ENABLE_UNIFIED_AND_FREEZABLE_CONFIG_RECORD 0
-#else
-#define ENABLE_UNIFIED_AND_FREEZABLE_CONFIG_RECORD 1
-#endif
-
 #if !defined(ENABLE_MPROTECT_RX_TO_RWX)
 #define ENABLE_MPROTECT_RX_TO_RWX 0
 #endif

Source/WTF/wtf/WTFConfig.cpp
@@ -48,8 +48,6 @@
 
 #include <mutex>
 
-#if ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
 namespace WebConfig {
 
 alignas(WTF::ConfigAlignment) Slot g_config[WTF::ConfigSizeToProtect / sizeof(Slot)];
@@ -60,19 +58,8 @@ alignas(WTF::ConfigAlignment) Slot g_config[WTF::ConfigSizeToProtect / sizeof(Sl
 static_assert(Gigacage::startSlotOfGigacageConfig == WebConfig::reservedSlotsForExecutableAllocator + WebConfig::additionalReservedSlots);
 #endif
 
-#else // not ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
 namespace WTF {
 
-Config g_wtfConfig;
-
-} // namespace WTF
-
-#endif // ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
-namespace WTF {
-
-#if ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
 void setPermissionsOfConfigPage()
 {
 #if PLATFORM(COCOA)
@@ -96,7 +83,6 @@ void setPermissionsOfConfigPage()
     });
 #endif // PLATFORM(COCOA)
 }
-#endif // ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
 
 void Config::initialize()
 {
@@ -152,7 +138,6 @@ void Config::permanentlyFreeze()
 
     int result = 0;
 
-#if ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
 #if PLATFORM(COCOA)
     enum {
         DontUpdateMaximumPermission = false,
@@ -164,14 +149,9 @@ void Config::permanentlyFreeze()
 #elif OS(LINUX)
     result = mprotect(&WebConfig::g_config, ConfigSizeToProtect, PROT_READ);
 #elif OS(WINDOWS)
-    // FIXME: Implement equivalent, maybe with VirtualProtect.
+    // FIXME: Implement equivalent for Windows, maybe with VirtualProtect.
     // Also need to fix WebKitTestRunner.
-
-    // Note: the Windows port also currently does not support a unified Config
-    // record, which is needed for the current form of the freezing feature to
-    // work. See comments in PlatformEnable.h for UNIFIED_AND_FREEZABLE_CONFIG_RECORD.
 #endif
-#endif // ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
 
     RELEASE_ASSERT(!result);
     RELEASE_ASSERT(g_wtfConfig.isPermanentlyFrozen);

Source/WTF/wtf/WTFConfig.h
@@ -42,7 +42,6 @@ constexpr size_t reservedBytesForGigacageConfig = 0;
 #include <bmalloc/GigacageConfig.h>
 #endif
 
-#if ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
 namespace WebConfig {
 
 using Slot = uint64_t;
@@ -59,7 +58,6 @@ enum ReservedConfigByteOffset {
 static_assert(NumberOfReservedConfigBytes <= sizeof(Slot) * additionalReservedSlots);
 
 } // namespace WebConfig
-#endif // ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
 
 namespace WTF {
 
@@ -102,8 +100,6 @@ struct Config {
     uint64_t spaceForExtensions[1];
 };
 
-#if ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
 constexpr size_t startSlotOfWTFConfig = Gigacage::reservedSlotsForGigacageConfig;
 constexpr size_t startOffsetOfWTFConfig = startSlotOfWTFConfig * sizeof(WebConfig::Slot);
 
@@ -117,14 +113,6 @@ static_assert(roundUpToMultipleOf<alignmentOfWTFConfig>(startOffsetOfWTFConfig) 
 WTF_EXPORT_PRIVATE void setPermissionsOfConfigPage();
 
 #define g_wtfConfig (*bitwise_cast<WTF::Config*>(&WebConfig::g_config[WTF::startSlotOfWTFConfig]))
-
-#else // not ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
-inline void setPermissionsOfConfigPage() { }
-
-extern "C" WTF_EXPORT_PRIVATE Config g_wtfConfig;
-
-#endif // ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
 
 constexpr size_t offsetOfWTFConfigLowestAccessibleAddress = offsetof(WTF::Config, lowestAccessibleAddress);
 
@@ -141,7 +129,3 @@ ALWAYS_INLINE Config::AssertNotFrozenScope::~AssertNotFrozenScope()
 };
 
 } // namespace WTF
-
-#if !ENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-using WTF::g_wtfConfig;
-#endif

Source/bmalloc/bmalloc/BPlatform.h
@@ -365,24 +365,6 @@
 #define BUSE_PRECOMPUTED_CONSTANTS_VMPAGE16K 1
 #endif
 
-/* The unified Config record feature is not available for Windows because the
-   Windows port puts WTF in a separate DLL, and the offlineasm code accessing
-   the config record expects the config record to be directly accessible like
-   a global variable (and not have to go thru DLL shenanigans). C++ code would
-   resolve these DLL bindings automatically, but offlineasm does not.
-
-   The permanently freezing feature also currently relies on the Config records
-   being unified, and the Windows port also does not currently have an
-   implementation for the freezing mechanism anyway. For simplicity, we just
-   disable both the use of unified Config record and config freezing for the
-   Windows port.
-*/
-#if BOS(WINDOWS)
-#define BENABLE_UNIFIED_AND_FREEZABLE_CONFIG_RECORD 0
-#else
-#define BENABLE_UNIFIED_AND_FREEZABLE_CONFIG_RECORD 1
-#endif
-
 /* We only export the mallocSize and mallocGoodSize APIs if they're supported by the DebugHeap allocator (currently only Darwin) and the current bmalloc allocator (currently only libpas). */
 #if BUSE(LIBPAS) && BOS(DARWIN)
 #define BENABLE_MALLOC_SIZE 1

Source/bmalloc/bmalloc/Gigacage.cpp
@@ -47,10 +47,6 @@
 
 namespace Gigacage {
 
-#if !BENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-Config g_gigacageConfig;
-#endif
-
 struct Callback {
     Callback() { }
     
@@ -98,11 +94,9 @@ void ensureGigacage()
             if (!shouldBeEnabled())
                 return;
 
-#if BENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
             // We might only get page size alignment, but that's also the minimum
             // alignment we need for freezing the Config.
             RELEASE_BASSERT(!(reinterpret_cast<size_t>(&WebConfig::g_config) & (vmPageSize() - 1)));
-#endif
 
             Kind shuffledKinds[NumberOfKinds];
             for (unsigned i = 0; i < NumberOfKinds; ++i)

Source/bmalloc/bmalloc/GigacageConfig.h
@@ -30,16 +30,12 @@
 #include "StdLibExtras.h"
 #include <inttypes.h>
 
-#if BENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
 namespace WebConfig {
 
 using Slot = uint64_t;
 extern "C" Slot g_config[];
 
 } // namespace WebConfig
-
-#endif // BENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
 
 namespace Gigacage {
 
@@ -102,8 +98,6 @@ struct Config {
     size_t allocSizes[NumberOfKinds];
 };
 
-#if BENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
 // The first 4 slots are reserved for the use of the ExecutableAllocator.
 constexpr size_t startSlotOfGigacageConfig = 4;
 constexpr size_t startOffsetOfGigacageConfig = startSlotOfGigacageConfig * sizeof(WebConfig::Slot);
@@ -118,14 +112,4 @@ static_assert(bmalloc::roundUpToMultipleOf<alignmentOfGigacageConfig>(startOffse
 
 #define g_gigacageConfig (*bmalloc::bitwise_cast<Gigacage::Config*>(&WebConfig::g_config[Gigacage::startSlotOfGigacageConfig]))
 
-#else // not BENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
-extern "C" BEXPORT Config g_gigacageConfig;
-
-#endif // BENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
 } // namespace Gigacage
-
-#if !BENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-using Gigacage::g_gigacageConfig;
-#endif

Source/bmalloc/bmalloc/mbmalloc.cpp
@@ -28,8 +28,6 @@
 #include "BExport.h"
 #include "GigacageConfig.h"
 
-#if BENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
 namespace WebConfig {
 
 // FIXME: Other than OS(DARWIN) || PLATFORM(PLAYSTATION), CeilingOnPageSize is
@@ -40,16 +38,6 @@ constexpr size_t ConfigSizeToProtect = 16 * bmalloc::Sizes::kB;
 alignas(ConfigAlignment) BEXPORT Slot g_config[ConfigSizeToProtect / sizeof(Slot)];
 
 } // namespace WebConfig
-
-#else // !BENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
-
-namespace Gigacage {
-
-Config g_gigacageConfig;
-
-} // namespace Gigacage
-
-#endif // BENABLE(UNIFIED_AND_FREEZABLE_CONFIG_RECORD)
 
 extern "C" {
 

Source/cmake/OptionsWin.cmake
@@ -193,7 +193,7 @@ if (USE_WOFF2)
 endif ()
 
 set(bmalloc_LIBRARY_TYPE OBJECT)
-set(WTF_LIBRARY_TYPE SHARED)
+set(WTF_LIBRARY_TYPE OBJECT)
 set(JavaScriptCore_LIBRARY_TYPE SHARED)
 set(PAL_LIBRARY_TYPE OBJECT)
 set(WebCore_LIBRARY_TYPE SHARED)