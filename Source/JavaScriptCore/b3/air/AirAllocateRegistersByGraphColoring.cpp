WebKit/WebKit

commit f5716c3f9c9a8b19d82c02df456588ea62767c1e

Add mechanism to automatically convert Enums to Strings for printing https://bugs.webkit.org/show_bug.cgi?id=271967 rdar://125723369
Reviewed by Yusuke Suzuki.

This patch adds support a set of new functions that return a `std::span<const char>`
with the stringification of an enum's type or members. Members are determined by
reflecting on the compiler's result for `__PRETTY_FUNCTION__`. This doesn't seem to
significantly increase compile times as a Debug build of JSC was 272.5s with this
change vs 268.8s without (~1% compile time increase).

In order to limit compile time overhead, the number of enum values stringified is
limited to 256 by default.

This patch also integrates this new name reflection into `dataLog` so it will
automatically print the enum's value strigified into the log. There's also a
`ScopedEnumDump` that includes the enum's name and `EnumDumpWithDefault` that
takes a default string if a stringification for the enum value can't be deduced.

* JSTests/wasm/wabt-wrapper.js:
* Source/JavaScriptCore/bytecode/ExitKind.cpp:
(JSC::exitKindToString): Deleted.
(WTF::printInternal): Deleted.
* Source/JavaScriptCore/bytecode/ExitKind.h:
* Source/JavaScriptCore/bytecode/ExpressionInfo.cpp:
(WTF::printInternal): Deleted.
* Source/JavaScriptCore/bytecode/ExpressionInfo.h:
* Source/JavaScriptCore/dfg/DFGArrayMode.cpp:
(WTF::printInternal):
(JSC::DFG::arrayActionToString): Deleted.
(JSC::DFG::arrayTypeToString): Deleted.
(JSC::DFG::arrayClassToString): Deleted.
(JSC::DFG::arraySpeculationToString): Deleted.
(JSC::DFG::arrayConversionToString): Deleted.
* Source/JavaScriptCore/dfg/DFGArrayMode.h:
* Source/JavaScriptCore/dfg/DFGBranchDirection.h:
(JSC::DFG::branchDirectionToString): Deleted.
(WTF::printInternal): Deleted.
* Source/JavaScriptCore/dfg/DFGOSRExit.cpp:
(JSC::DFG::JSC_DEFINE_JIT_OPERATION):
* Source/JavaScriptCore/ftl/FTLOSRExitCompiler.cpp:
(JSC::FTL::compileStub):
* Source/JavaScriptCore/heap/GCLogging.cpp:
(JSC::GCLogging::levelAsString): Deleted.

Source/JavaScriptCore/dfg/DFGOSRExit.cpp
@@ -207,7 +207,7 @@ JSC_DEFINE_NOEXCEPT_JIT_OPERATION(operationCompileOSRExit, void, (CallFrame* cal
             patchBuffer, OSRExitPtrTag, nullptr,
             "DFG OSR exit #%u (D@%u, %s, %s) from %s, with operands = %s",
                 exitIndex, exit.m_dfgNodeIndex, toCString(exit.m_codeOrigin).data(),
-                exitKindToString(exit.m_kind).characters(), toCString(*codeBlock).data(),
+                exit.m_kind, toCString(*codeBlock).data(),
                 toCString(ignoringContext<DumpContext>(operands)).data());
         codeBlock->dfgJITData()->setExitCode(exitIndex, exitCode);
     }
@@ -881,7 +881,7 @@ JSC_DEFINE_NOEXCEPT_JIT_OPERATION(operationDebugPrintSpeculationFailure, void, (
     NativeCallFrameTracer tracer(vm, callFrame);
 
     dataLog("Speculation failure in ", *codeBlock);
-    dataLog(" @ exit #", debugInfo->exitIndex, " (", debugInfo->bytecodeIndex, ", ", exitKindToString(debugInfo->kind), ") with ");
+    dataLog(" @ exit #", debugInfo->exitIndex, " (", debugInfo->bytecodeIndex, ", ", debugInfo->kind, ") with ");
     if (alternative) {
         dataLog(
             "executeCounter = ", alternative->jitExecuteCounter(),

Source/JavaScriptCore/ftl/FTLOSRExitCompiler.cpp
@@ -624,7 +624,7 @@ static void compileStub(VM& vm, unsigned exitID, JITCode* jitCode, OSRExit& exit
         patchBuffer, OSRExitPtrTag, nullptr,
         "FTL OSR exit #%u (D@%u, %s, %s) from %s, with operands = %s",
             exitID, exit.m_dfgNodeIndex, toCString(exit.m_codeOrigin).data(),
-            exitKindToString(exit.m_kind).characters(), toCString(*codeBlock).data(),
+            exit.m_kind, toCString(*codeBlock).data(),
             toCString(ignoringContext<DumpContext>(exit.m_descriptor->m_values)).data()
         );
 }

JSTests/wasm/wabt-wrapper.js
@@ -1,4 +1,4 @@
-load("../libwabt.js");
+load("libwabt.js", "caller relative");
 
 export async function compile(wat, options = {}) {
     const wabtModule = await WabtModule();

Source/JavaScriptCore/bytecode/ExitKind.cpp
@@ -32,74 +32,6 @@
 
 namespace JSC {
 
-ASCIILiteral exitKindToString(ExitKind kind)
-{
-    switch (kind) {
-    case ExitKindUnset:
-        return "Unset"_s;
-    case BadType:
-        return "BadType"_s;
-    case BadConstantValue:
-        return "BadConstantValue"_s;
-    case BadIdent:
-        return "BadIdent"_s;
-    case BadExecutable:
-        return "BadExecutable"_s;
-    case BadCache:
-        return "BadCache"_s;
-    case BadConstantCache:
-        return "BadConstantCache"_s;
-    case BadIndexingType:
-        return "BadIndexingType"_s;
-    case BadTypeInfoFlags:
-        return "BadTypeInfoFlags"_s;
-    case Overflow:
-        return "Overflow"_s;
-    case NegativeZero:
-        return "NegativeZero"_s;
-    case NegativeIndex:
-        return "NegativeIndex"_s;
-    case Int52Overflow:
-        return "Int52Overflow"_s;
-    case StoreToHole:
-        return "StoreToHole"_s;
-    case LoadFromHole:
-        return "LoadFromHole"_s;
-    case OutOfBounds:
-        return "OutOfBounds"_s;
-    case InadequateCoverage:
-        return "InadequateCoverage"_s;
-    case ArgumentsEscaped:
-        return "ArgumentsEscaped"_s;
-    case ExoticObjectMode:
-        return "ExoticObjectMode"_s;
-    case VarargsOverflow:
-        return "VarargsOverflow"_s;
-    case TDZFailure:
-        return "TDZFailure"_s;
-    case HoistingFailed:
-        return "HoistingFailed"_s;
-    case Uncountable:
-        return "Uncountable"_s;
-    case UncountableInvalidation:
-        return "UncountableInvalidation"_s;
-    case WatchdogTimerFired:
-        return "WatchdogTimerFired"_s;
-    case DebuggerEvent:
-        return "DebuggerEvent"_s;
-    case ExceptionCheck:
-        return "ExceptionCheck"_s;
-    case GenericUnwind:
-        return "GenericUnwind"_s;
-    case BigInt32Overflow:
-        return "BigInt32Overflow"_s;
-    case UnexpectedResizableArrayBufferView:
-        return "UnexpectedResizableArrayBufferView"_s;
-    }
-    RELEASE_ASSERT_NOT_REACHED();
-    return "Unknown"_s;
-}
-
 bool exitKindMayJettison(ExitKind kind)
 {
     switch (kind) {
@@ -112,13 +44,3 @@ bool exitKindMayJettison(ExitKind kind)
 }
 
 } // namespace JSC
-
-namespace WTF {
-
-void printInternal(PrintStream& out, JSC::ExitKind kind)
-{
-    out.print(exitKindToString(kind));
-}
-
-} // namespace WTF
-

Source/JavaScriptCore/bytecode/ExitKind.h
@@ -62,14 +62,6 @@ enum ExitKind : uint8_t {
     UnexpectedResizableArrayBufferView, // We exited because we made an incorrect assumption about what type of ArrayBufferView we would see.
 };
 
-ASCIILiteral exitKindToString(ExitKind);
 bool exitKindMayJettison(ExitKind);
 
 } // namespace JSC
-
-namespace WTF {
-
-class PrintStream;
-void printInternal(PrintStream&, JSC::ExitKind);
-
-} // namespace WTF

Source/JavaScriptCore/bytecode/ExpressionInfo.cpp
@@ -1059,23 +1059,3 @@ void ExpressionInfo::dumpEncodedInfo(ExpressionInfo::EncodedInfo* start, Express
 }
 
 } // namespace JSC
-
-namespace WTF {
-
-void printInternal(PrintStream& out, JSC::ExpressionInfo::FieldID fieldID)
-{
-    auto name = [] (auto fieldID) {
-        switch (fieldID) {
-        case JSC::ExpressionInfo::FieldID::InstPC: return "Inst";
-        case JSC::ExpressionInfo::FieldID::Divot: return "Divot";
-        case JSC::ExpressionInfo::FieldID::Start: return "Start";
-        case JSC::ExpressionInfo::FieldID::End: return "End";
-        case JSC::ExpressionInfo::FieldID::Line: return "Line";
-        case JSC::ExpressionInfo::FieldID::Column: return "Column";
-        }
-        return ""; // placate GCC.
-    };
-    out.print(name(fieldID));
-}
-
-} // namespace WTF

Source/JavaScriptCore/bytecode/ExpressionInfo.h
@@ -332,9 +332,3 @@ private:
 static_assert(roundUpToMultipleOf<sizeof(unsigned)>(sizeof(ExpressionInfo)) == sizeof(ExpressionInfo), "CachedExpressionInfo relies on this invariant");
 
 } // namespace JSC
-
-namespace WTF {
-
-JS_EXPORT_PRIVATE void printInternal(PrintStream&, JSC::ExpressionInfo::FieldID);
-
-} // namespace WTF

Source/JavaScriptCore/bytecode/Watchpoint.h
@@ -48,7 +48,9 @@ class FireDetail {
 public:
     FireDetail() = default;
     virtual ~FireDetail() = default;
-    virtual void dump(PrintStream&) const = 0;
+    // This can't be pure virtual as it breaks our Dumpable concept.
+    // FIXME: Make this virtual after we stop suppporting the Montery Clang.
+    virtual void dump(PrintStream&) const { }
 };
 
 class StringFireDetail final : public FireDetail {

commit 2ea46ab90e6511139bfb94415205038b672381e0

[JSC] Attempt to convert Move32 to Move in ARM64 https://bugs.webkit.org/show_bug.cgi?id=277744 rdar://133394615
Reviewed by Keith Miller.

We found that we are emitting Move32 unnecessarily when the destination said that the value is ZeroDefed.
We are doing so since x86_64 prefers Move32. But on ARM64, Move32 is more costly than Move. So we add opposite
optimization which attempts to make Move32 to Move (we already have optimization making Move to Move32 for x86_64).
We also fixes an issue in FTLOSRExitCompiler's logging introduced in 281831@main.

* Source/JavaScriptCore/b3/air/AirAllocateRegistersByGraphColoring.cpp:
* Source/JavaScriptCore/ftl/FTLOSRExitCompiler.cpp:
(JSC::FTL::compileStub):

Canonical link: https://commits.webkit.org/281959@main

Source/JavaScriptCore/b3/air/AirAllocateRegistersByGraphColoring.cpp
@@ -1967,16 +1967,22 @@ private:
                 // complete register allocation. So, we record this before starting.
                 bool mayBeCoalescable = allocator.mayBeCoalescable(inst);
 
-                // Move32 is cheaper if we know that it's equivalent to a Move in x86_64. It's
-                // equivalent if the destination's high bits are not observable or if the source's high
-                // bits are all zero. Note that we don't have the opposite optimization for other
-                // architectures, which may prefer Move over Move32, because Move is canonical already.
                 if constexpr (isX86_64()) {
-                    if (bank == GP && inst.kind.opcode == Move
-                        && inst.args[0].isTmp() && inst.args[1].isTmp()) {
-                        if (m_tmpWidth.useWidth(inst.args[1].tmp()) <= Width32
-                            || m_tmpWidth.defWidth(inst.args[0].tmp()) <= Width32)
+                    // Move32 is cheaper if we know that it's equivalent to a Move in x86_64. It's
+                    // equivalent if the destination's high bits are not observable or if the source's high
+                    // bits are all zero.
+                    if (bank == GP && inst.kind.opcode == Move && inst.args[0].isTmp() && inst.args[1].isTmp()) {
+                        if (m_tmpWidth.useWidth(inst.args[1].tmp()) <= Width32 || m_tmpWidth.defWidth(inst.args[0].tmp()) <= Width32)
                             inst.kind.opcode = Move32;
+                    }
+                }
+                if constexpr (isARM64()) {
+                    // On the other hand, on ARM64, Move is cheaper than Move32. We would like to use Move instead of Move32.
+                    // Move32 on ARM64 is explicitly selected in B3LowerToAir for ZExt32 for example. But using ZDef information
+                    // here can optimize it from Move32 to Move.
+                    if (bank == GP && inst.kind.opcode == Move32 && inst.args[0].isTmp() && inst.args[1].isTmp()) {
+                        if (m_tmpWidth.defWidth(inst.args[0].tmp()) <= Width32)
+                            inst.kind.opcode = Move;
                     }
                 }
 

Source/JavaScriptCore/dfg/DFGOSRExit.cpp
@@ -207,7 +207,7 @@ JSC_DEFINE_NOEXCEPT_JIT_OPERATION(operationCompileOSRExit, void, (CallFrame* cal
             patchBuffer, OSRExitPtrTag, nullptr,
             "DFG OSR exit #%u (D@%u, %s, %s) from %s, with operands = %s",
                 exitIndex, exit.m_dfgNodeIndex, toCString(exit.m_codeOrigin).data(),
-                exit.m_kind, toCString(*codeBlock).data(),
+                toCString(exit.m_kind).data(), toCString(*codeBlock).data(),
                 toCString(ignoringContext<DumpContext>(operands)).data());
         codeBlock->dfgJITData()->setExitCode(exitIndex, exitCode);
     }

Source/JavaScriptCore/ftl/FTLOSRExitCompiler.cpp
@@ -624,7 +624,7 @@ static void compileStub(VM& vm, unsigned exitID, JITCode* jitCode, OSRExit& exit
         patchBuffer, OSRExitPtrTag, nullptr,
         "FTL OSR exit #%u (D@%u, %s, %s) from %s, with operands = %s",
             exitID, exit.m_dfgNodeIndex, toCString(exit.m_codeOrigin).data(),
-            exit.m_kind, toCString(*codeBlock).data(),
+            toCString(exit.m_kind).data(), toCString(*codeBlock).data(),
             toCString(ignoringContext<DumpContext>(exit.m_descriptor->m_values)).data()
         );
 }