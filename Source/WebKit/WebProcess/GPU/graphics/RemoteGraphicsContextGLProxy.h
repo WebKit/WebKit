e2badf99cf105b84f65f170f796d2508650ceaf4
Mike Wyrzykowski <mwyrzykowski@apple.com>
2024-08-07 11:51:14 -0700

    [WebXR] EnableRequiredWebXRExtensions can't be behind the prompt https://bugs.webkit.org/show_bug.cgi?id=277745 <radar://133317583>
    Unreviewed partial revert of 281114@main
    
    The website may enable the extensions prior to entering immersive mode, so this
    message can not be behind the UI prompt.
    
    * Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.messages.in:
    * Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.h:
    
    Canonical link: https://commits.webkit.org/281947@main

--- Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.h
+++ Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.h
@@ -368,7 +368,7 @@ public:
     void deleteExternalSync(GCGLExternalSync) IPC_MESSAGE_ATTRIBUTE(EnabledIf='webXRPromptAccepted()') final;
 
 #if ENABLE(WEBXR)
-    bool enableRequiredWebXRExtensions() IPC_MESSAGE_ATTRIBUTE(EnabledIf='webXRPromptAccepted()') final;
+    bool enableRequiredWebXRExtensions() IPC_MESSAGE_ATTRIBUTE(EnabledIf='webXREnabled()') final;
     bool addFoveation(WebCore::IntSize physicalSizeLeft, WebCore::IntSize physicalSizeRight, WebCore::IntSize screenSize, std::span<const GCGLfloat> horizontalSamplesLeft, std::span<const GCGLfloat> verticalSamples, std::span<const GCGLfloat> horizontalSamplesRight) IPC_MESSAGE_ATTRIBUTE(EnabledIf='webXRPromptAccepted()') final;
     void enableFoveation(GCGLuint) IPC_MESSAGE_ATTRIBUTE(EnabledIf='webXRPromptAccepted()') final;
     void disableFoveation() IPC_MESSAGE_ATTRIBUTE(EnabledIf='webXRPromptAccepted()') final;
