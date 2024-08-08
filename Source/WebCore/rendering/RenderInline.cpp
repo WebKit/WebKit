182f3b9c6e50140ba370be855368724005d414c5
Commit Queue <commit-queue@webkit.org>
2024-08-08 10:01:22 -0700

    Cherry-pick 6ae7550718f6. rdar://133402480
        Unreviewed, reverting 280578@main.
        https://bugs.webkit.org/show_bug.cgi?id=277798
    
        Causes repaint issues on <http://music.youtube.com|music.youtube.com>
    
        Reverted changeset:
    
        "RenderElement::dirtyLinesFromChangedChild should only be called on svg content"
        https://bugs.webkit.org/show_bug.cgi?id=276103
        https://commits.webkit.org/280578@main
    
        Canonical link: https://commits.webkit.org/281989@main
    
    Canonical link: https://commits.webkit.org/280938.210@safari-7619-branch

--- Source/WebCore/rendering/RenderInline.cpp
+++ Source/WebCore/rendering/RenderInline.cpp
@@ -109,8 +109,8 @@ void RenderInline::willBeDestroyed()
                 for (auto* box = firstLineBox(); box; box = box->nextLineBox())
                     box->removeFromParent();
             }
-        } else if (auto* parent = this->parent(); parent && parent->isSVGRenderer())
-            parent->dirtyLinesFromChangedChild(*this);
+        } else if (parent())
+            parent()->dirtyLinesFromChangedChild(*this);
     }
 
     m_lineBoxes.deleteLineBoxes();
