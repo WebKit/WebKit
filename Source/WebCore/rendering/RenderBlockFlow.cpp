6ae7550718f6e94f9807c7615bdd624ff3cda995
Commit Queue <commit-queue@webkit.org>
2024-08-08 07:24:21 -0700

    Unreviewed, reverting 280578@main. https://bugs.webkit.org/show_bug.cgi?id=277798
    Causes repaint issues on <http://music.youtube.com|music.youtube.com>
    
    Reverted changeset:
    
    "RenderElement::dirtyLinesFromChangedChild should only be called on svg content"
    https://bugs.webkit.org/show_bug.cgi?id=276103
    https://commits.webkit.org/280578@main
    
    Canonical link: https://commits.webkit.org/281989@main

--- Source/WebCore/rendering/RenderBlockFlow.cpp
+++ Source/WebCore/rendering/RenderBlockFlow.cpp
@@ -168,8 +168,8 @@ void RenderBlockFlow::willBeDestroyed()
                         childBox->removeFromParent();
                 }
             }
-        } else if (auto* parent = this->parent(); parent && parent->isSVGRenderer())
-            parent->dirtyLinesFromChangedChild(*this);
+        } else if (parent())
+            parent()->dirtyLinesFromChangedChild(*this);
     }
 
     if (legacyLineLayout())
