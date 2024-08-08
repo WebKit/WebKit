fb9c68512622c1af0a2636faf675adfc4d5d7db3
Gerald Squelart <g_squelart@apple.com>
2024-08-08 14:49:53 +1000

    WIP: Premultiply color conversion matrices Need the bug URL (OOPS!). Include a Radar link (OOPS!).
    Reviewed by NOBODY (OOPS!).
    
    Explanation of why this fixes the bug (OOPS!).
    
    * Source/WebCore/platform/graphics/ColorConversion.h:
    (WebCore::ColorConversion::handleMatrixConversion):
    * Source/WebCore/platform/graphics/ColorMatrix.h:
    (WebCore::premultiply):

--- Source/WebCore/platform/graphics/ColorMatrix.h
+++ Source/WebCore/platform/graphics/ColorMatrix.h
@@ -32,28 +32,49 @@ namespace WebCore {
 
 template<typename, size_t> struct ColorComponents;
 
-template<size_t ColumnCount, size_t RowCount>
+template<size_t ColumnCount, size_t RowCount, typename MatrixValueType = float>
 class ColorMatrix {
 public:
-    template<typename ...Ts>
+    template<typename ...Ts, typename = std::enable_if_t<(std::is_convertible_v<Ts, MatrixValueType> && ...)>>
     explicit constexpr ColorMatrix(Ts ...input)
-        : m_matrix {{ static_cast<float>(input) ... }}
+        : m_matrix { { static_cast<MatrixValueType>(input) ... } }
     {
         static_assert(sizeof...(Ts) == RowCount * ColumnCount);
     }
 
-    template<size_t NumberOfComponents>
-    constexpr ColorComponents<float, NumberOfComponents> transformedColorComponents(const ColorComponents<float, NumberOfComponents>&) const;
+    template<typename ColorValueType, size_t NumberOfComponents>
+    constexpr ColorComponents<ColorValueType, NumberOfComponents> transformedColorComponents(const ColorComponents<ColorValueType, NumberOfComponents>&) const;
 
-    constexpr float at(size_t row, size_t column) const
+    constexpr MatrixValueType at(size_t row, size_t column) const
     {
         return m_matrix[(row * ColumnCount) + column];
     }
 
     friend bool operator==(const ColorMatrix&, const ColorMatrix&) = default;
 
+    template <typename TargetValueType>
+    constexpr ColorMatrix<ColumnCount, RowCount, TargetValueType> toValueType() const
+    {
+        if constexpr(std::is_same_v<TargetValueType, MatrixValueType>)
+            return *this;
+        else
+            return ColorMatrix<ColumnCount, RowCount, TargetValueType> { *this };
+    }
+
 private:
-    std::array<float, RowCount * ColumnCount> m_matrix;
+    template <size_t OtherColumnCount, size_t OtherRowCount, typename OtherMatrixValueType>
+    friend class ColorMatrix;
+
+    template <typename FromValueType>
+    constexpr ColorMatrix(const ColorMatrix<ColumnCount, RowCount, FromValueType>& input)
+    {
+        for (size_t row = 0; row < RowCount; ++row) {
+            for (size_t column = 0; column < ColumnCount; ++column)
+                m_matrix[(row * ColumnCount) + column] = input.at(row, column);
+        }
+    }
+
+    std::array<MatrixValueType, RowCount * ColumnCount> m_matrix;
 };
 
 constexpr ColorMatrix<3, 3> brightnessColorMatrix(float amount)
@@ -156,26 +177,27 @@ inline ColorMatrix<3, 3> hueRotateColorMatrix(float angleInDegrees)
     };
 }
 
-template<size_t ColumnCount, size_t RowCount>
-template<size_t NumberOfComponents>
-constexpr auto ColorMatrix<ColumnCount, RowCount>::transformedColorComponents(const ColorComponents<float, NumberOfComponents>& inputVector) const -> ColorComponents<float, NumberOfComponents>
+template<size_t ColumnCount, size_t RowCount, typename MatrixValueType>
+template<typename ColorValueType, size_t NumberOfComponents>
+constexpr auto ColorMatrix<ColumnCount, RowCount, MatrixValueType>::transformedColorComponents(const ColorComponents<ColorValueType, NumberOfComponents>& inputVector) const -> ColorComponents<ColorValueType, NumberOfComponents>
 {
-    static_assert(ColorComponents<float, NumberOfComponents>::Size >= RowCount);
+    using ValueType = std::conditional_t<sizeof(MatrixValueType) >= sizeof(ColorValueType), MatrixValueType, ColorValueType>;
+    static_assert(ColorComponents<ValueType, NumberOfComponents>::Size >= RowCount);
     
-    ColorComponents<float, NumberOfComponents> result;
+    ColorComponents<ValueType, NumberOfComponents> result;
     for (size_t row = 0; row < RowCount; ++row) {
-        if constexpr (ColumnCount <= ColorComponents<float, NumberOfComponents>::Size) {
+        if constexpr (ColumnCount <= ColorComponents<ColorValueType, NumberOfComponents>::Size) {
             for (size_t column = 0; column < ColumnCount; ++column)
                 result[row] += at(row, column) * inputVector[column];
-        } else if constexpr (ColumnCount > ColorComponents<float, NumberOfComponents>::Size) {
-            for (size_t column = 0; column < ColorComponents<float, NumberOfComponents>::Size; ++column)
+        } else if constexpr (ColumnCount > ColorComponents<ColorValueType, NumberOfComponents>::Size) {
+            for (size_t column = 0; column < ColorComponents<ColorValueType, NumberOfComponents>::Size; ++column)
                 result[row] += at(row, column) * inputVector[column];
-            for (size_t additionalColumn = ColorComponents<float, NumberOfComponents>::Size; additionalColumn < ColumnCount; ++additionalColumn)
+            for (size_t additionalColumn = ColorComponents<ColorValueType, NumberOfComponents>::Size; additionalColumn < ColumnCount; ++additionalColumn)
                 result[row] += at(row, additionalColumn);
         }
     }
-    if constexpr (ColorComponents<float, NumberOfComponents>::Size > RowCount) {
-        for (size_t additionalRow = RowCount; additionalRow < ColorComponents<float, NumberOfComponents>::Size; ++additionalRow)
+    if constexpr (ColorComponents<ColorValueType, NumberOfComponents>::Size > RowCount) {
+        for (size_t additionalRow = RowCount; additionalRow < ColorComponents<ColorValueType, NumberOfComponents>::Size; ++additionalRow)
             result[additionalRow] = inputVector[additionalRow];
     }
 
@@ -190,6 +212,27 @@ template<typename T, typename M> inline constexpr auto applyMatricesToColorCompo
 template<typename T, typename M, typename... Matrices> inline constexpr auto applyMatricesToColorComponents(const ColorComponents<T, 4>& components, M matrix, Matrices... matrices) -> ColorComponents<T, 4>
 {
     return applyMatricesToColorComponents(matrix.transformedColorComponents(components), matrices...);
+}
+
+template<size_t ColumnCount, size_t RowCount, typename MatrixValueType>
+consteval ColorMatrix<ColumnCount, RowCount, MatrixValueType> premultiply(const ColorMatrix<ColumnCount, RowCount, MatrixValueType>& m1, const ColorMatrix<ColumnCount, RowCount, MatrixValueType>& m2)
+{
+    static_assert(ColumnCount == 3);
+    static_assert(ColumnCount == 3);
+
+    float a = m1.at(0, 0), b = m1.at(0, 1), c = m1.at(0, 2);
+    float d = m1.at(1, 0), e = m1.at(1, 1), f = m1.at(1, 2);
+    float g = m1.at(2, 0), h = m1.at(2, 1), i = m1.at(2, 2);
+
+    float j = m2.at(0, 0), k = m2.at(0, 1), l = m2.at(0, 2);
+    float m = m2.at(1, 0), n = m2.at(1, 1), o = m2.at(1, 2);
+    float p = m2.at(2, 0), q = m2.at(2, 1), r = m2.at(2, 2);
+
+    return ColorMatrix<ColumnCount, RowCount, MatrixValueType> {
+        a*j + d*k + g*l, b*j + e*k + h*l, c*j + f*k + i*l,
+        a*m + d*n + g*o, b*m + e*n + h*o, c*m + f*n + i*o,
+        a*p + d*q + g*r, b*p + e*q + h*r, c*p + f*q + i*r
+    };
 }
 
 } // namespace WebCore
