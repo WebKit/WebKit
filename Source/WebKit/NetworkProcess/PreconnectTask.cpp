e3442d872260cfae9378878e3e895e7fe36a9557
Lingfeng Hou <fyomtain@163.com>
2024-08-08 13:33:17 +0800

    The preconnect to an HTTP/1.1 origin blocks a main resource request. https://bugs.webkit.org/show_bug.cgi?id=275467
    Reviewed by NOBODY (OOPS!).
    
    The root cause of this issue is that the curl request had been canceled
    but the PreconnectTask was still in running till the time out (10
    seconds), which blocked the main resource request.
    
    * Source/WebKit/NetworkProcess/PreconnectTask.cpp:
    (WebKit::PreconnectTask::didReceiveResponse):

--- Source/WebKit/NetworkProcess/PreconnectTask.cpp
+++ Source/WebKit/NetworkProcess/PreconnectTask.cpp
@@ -82,6 +82,10 @@ void PreconnectTask::willSendRedirectedRequest(ResourceRequest&&, ResourceReques
 
 void PreconnectTask::didReceiveResponse(ResourceResponse&& response, PrivateRelayed, ResponseCompletionHandler&& completionHandler)
 {
+#if !USE(CURL)
+    ASSERT_NOT_REACHED();
+#endif
+
     completionHandler(PolicyAction::Ignore);
     didFinish(ResourceError { ResourceError::Type::Cancellation }, NetworkLoadMetrics::emptyMetrics());
 }
