a27fb4bae52ef8b9ab9339790660631f387df44f
David Kilzer <ddkilzer@apple.com>
2024-08-14 10:04:56 -0700

    [WebKitLegacy] Fix -Wenum-compare in WebDefaultPolicyDelegate.m <https://bugs.webkit.org/show_bug.cgi?id=278073> <rdar://133809285>
    Reviewed by Alex Christensen.
    
    Use WTF::enumToUnderlyingType() to compare
    WebNavigationTypePlugInRequest to a WebNavigationType enum.
    
    Also convert WebDefaultPolicyDelegate.m to Obj-C++ source file so that
    WTF::enumToUnderlyingType() can be used.
    
    * Source/WebKitLegacy/WebKitLegacy.xcodeproj/project.pbxproj:
    - Rename WebDefaultPolicyDelegate.m to WebDefaultPolicyDelegate.mm.
    * Source/WebKitLegacy/mac/DefaultDelegates/WebDefaultPolicyDelegate.mm: Rename from Source/WebKitLegacy/mac/DefaultDelegates/WebDefaultPolicyDelegate.m.
    (-[WebDefaultPolicyDelegate webView:decidePolicyForNavigationAction:request:frame:decisionListener:]):
    - Use WTF::enumToUnderlyingType() to fix the warning.
    * Tools/DumpRenderTree/DefaultPolicyDelegate.mm:
    (-[DefaultPolicyDelegate webView:decidePolicyForNavigationAction:request:frame:decisionListener:]):
    - Use WTF::enumToUnderlyingType() instead of static_cast<unsigned>()
      to match code in WebDefaultPolicyDelegate.mm.
    
    Canonical link: https://commits.webkit.org/282241@main

--- Source/WebKitLegacy/mac/DefaultDelegates/WebDefaultPolicyDelegate.mm
+++ Source/WebKitLegacy/mac/DefaultDelegates/WebDefaultPolicyDelegate.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2005-2024 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -36,6 +36,7 @@
 #import <Foundation/NSURLRequest.h>
 #import <Foundation/NSURLResponse.h>
 #import <wtf/Assertions.h>
+#import <wtf/EnumTraits.h>
 
 @implementation WebDefaultPolicyDelegate
 
@@ -85,7 +86,7 @@ static WebDefaultPolicyDelegate *sharedDelegate = nil;
 
     if ([WebView _canHandleRequest:request forMainFrame:frame == [wv mainFrame]]) {
         [listener use];
-    } else if (navType == WebNavigationTypePlugInRequest) {
+    } else if (enumToUnderlyingType(navType) == enumToUnderlyingType(WebNavigationTypePlugInRequest)) {
         [listener use];
     } else {
         // A file URL shouldn't fall through to here, but if it did,
