93f73ae142691349752f1f0b0e9852c950383d92
Aditya Keerthi <akeerthi@apple.com>
2024-08-14 10:04:01 -0700

    ::target-text cannot be applied on visited links https://bugs.webkit.org/show_bug.cgi?id=278081 rdar://133814103
    Reviewed by Tim Nguyen and Richard Robinson.
    
    * LayoutTests/TestExpectations:
    
    The fact that these styles are not web-exposed is covered by the already
    passing WPT `css/css-pseudo/highlight-pseudos-visited-computed-001.html`.
    
    * Source/WebCore/rendering/StyledMarkedText.cpp:
    (WebCore::computeStyleForPseudoElementStyle):
    
    Use `visitedDependentColorWithColorFilter` to account for visited styles.
    
    Canonical link: https://commits.webkit.org/282240@main

--- Source/WebCore/rendering/StyledMarkedText.cpp
+++ Source/WebCore/rendering/StyledMarkedText.cpp
@@ -40,7 +40,7 @@ static void computeStyleForPseudoElementStyle(StyledMarkedText::Style& style, co
     if (!pseudoElementStyle)
         return;
 
-    style.backgroundColor = pseudoElementStyle->colorResolvingCurrentColor(pseudoElementStyle->backgroundColor());
+    style.backgroundColor = pseudoElementStyle->visitedDependentColorWithColorFilter(CSSPropertyBackgroundColor, paintInfo.paintBehavior);
     style.textStyles.fillColor = pseudoElementStyle->computedStrokeColor();
     style.textStyles.strokeColor = pseudoElementStyle->computedStrokeColor();
     style.textStyles.hasExplicitlySetFillColor = pseudoElementStyle->hasExplicitlySetColor();
