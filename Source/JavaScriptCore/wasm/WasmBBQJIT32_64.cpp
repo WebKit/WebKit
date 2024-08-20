WebKit/WebKit

commit b7de8d58745394254579958957e487a7a38b4b0b

Merge Wasm::Instance into JSWebAssemblyInstance https://bugs.webkit.org/show_bug.cgi?id=276963 rdar://problem/132330783
Reviewed by Yusuke Suzuki.

This patch combines `Wasm::Instance` into `JSWebAssemblyInstance` by mostly just merging the two together.
In order to make this work a couple interesting changes had to made:

1) IsoSubspaces now support variable-sized precise-only configurations. In this configuration an IsoSubspace can support variable sized objects
   that are always allocated as precise allocations. This is ok for JSWebAssemblyInstances because they are both relatively rare and typically
   quite large. One thing to note is that IsoSubpsaces in this configuration don't (currently) support IsoCellSets since those expect a max of
   eight precise allocations.

2) Wasm::Memory used to have a `HashSet<WeakPtr<Wasm::Instance>>` in order to update the cached memory bounds/base but now uses a
   `WeakGCSet<JSWebAssemblyInstance>` instead. This works because `Wasm::Memory`s are essentially tied to a single VM anyway.

3) WeakGCSet now works out of the box without a custom hasher. It uses the underlying value as the key, which is probably what people expect.
   The only constraint is that `WeakHandleOwner`s are not supported, currently. It's a little unclear how we would support more than one anyway
   since a `Weak` only supports one `WeakHandleOwner` in its `WeakImpl`

4) the `current_memory` instruction now has one more dependent load since `JSWebAssemblyInstance` doesn't hold a direct pointer to `Wasm::Memory`
   and only points to `JSWebAssemblyMemory` but `current_memory` isn't a hot instruction and hopefully we'll merge `Wasm::Memory` into `JSWebAssemblyMemory`
   in the near future.

Some other cleanup changes:

1) Add a copyright to the IPInt .asm files
2) capitalize `arraySegmentOperation` to `ArraySegmentOperation` correctly for a type per WebKit style.

In the future we should:

Source/JavaScriptCore/wasm/WasmBBQJIT32_64.cpp
@@ -51,7 +51,6 @@
 #include "WasmFormat.h"
 #include "WasmFunctionParser.h"
 #include "WasmIRGeneratorHelpers.h"
-#include "WasmInstance.h"
 #include "WasmMemoryInformation.h"
 #include "WasmModule.h"
 #include "WasmModuleInformation.h"
@@ -237,7 +236,7 @@ PartialResult WARN_UNUSED_RETURN BBQJIT::getGlobal(uint32_t index, Value& result
     const Wasm::GlobalInformation& global = m_info.globals[index];
     Type type = global.type;
 
-    int32_t offset = Instance::offsetOfGlobalPtr(m_info.importFunctionCount(), m_info.tableCount(), index);
+    int32_t offset = JSWebAssemblyInstance::offsetOfGlobalPtr(m_info.importFunctionCount(), m_info.tableCount(), index);
     Value globalValue = Value::pinned(type.kind, Location::fromGlobal(offset));
 
     switch (global.bindingMode) {
@@ -305,17 +304,15 @@ PartialResult WARN_UNUSED_RETURN BBQJIT::setGlobal(uint32_t index, Value value)
     const Wasm::GlobalInformation& global = m_info.globals[index];
     Type type = global.type;
 
-    int32_t offset = Instance::offsetOfGlobalPtr(m_info.importFunctionCount(), m_info.tableCount(), index);
+    int32_t offset = JSWebAssemblyInstance::offsetOfGlobalPtr(m_info.importFunctionCount(), m_info.tableCount(), index);
     Location valueLocation = locationOf(value);
 
     switch (global.bindingMode) {
     case Wasm::GlobalInformation::BindingMode::EmbeddedInInstance: {
         emitMove(value, Location::fromGlobal(offset));
         consume(value);
-        if (isRefType(type)) {
-            m_jit.load32(Address(GPRInfo::wasmContextInstancePointer, Instance::offsetOfOwner()), wasmScratchGPR);
+        if (isRefType(type))
             emitWriteBarrier(wasmScratchGPR);
-        }
         break;
     }
     case Wasm::GlobalInformation::BindingMode::Portable: {
@@ -3493,8 +3490,7 @@ PartialResult WARN_UNUSED_RETURN BBQJIT::addCallRef(const TypeDefinition& origin
 
         m_jit.loadPtr(MacroAssembler::Address(calleePtr, WebAssemblyFunctionBase::offsetOfInstance()), jsCalleeAnchor);
         m_jit.loadPtr(MacroAssembler::Address(calleePtr, WebAssemblyFunctionBase::offsetOfEntrypointLoadLocation()), calleeCode);
-        m_jit.loadPtr(MacroAssembler::Address(jsCalleeAnchor, JSWebAssemblyInstance::offsetOfInstance()), calleeInstance);
-
+        m_jit.move(jsCalleeAnchor, calleeInstance);
     }
 
     emitIndirectCall("CallRef", callee, calleeInstance, calleeCode, jsCalleeAnchor, signature, args, results, CallType::Call);

commit 5e9259f3fd5d24d7d5ccb011e7ceae6990469a51

[JSC] Fix regression in Wasm::BBQJIT::setGlobal on armv7 https://bugs.webkit.org/show_bug.cgi?id=277413
Reviewed by Justin Michaud.

Fixes a regression introduced in b7de8d58745394254579958957e487a7a38b4b0b.

* Source/JavaScriptCore/wasm/WasmBBQJIT32_64.cpp:
(JSC::Wasm::BBQJITImpl::BBQJIT::setGlobal):

Canonical link: https://commits.webkit.org/282521@main

Source/JavaScriptCore/wasm/WasmBBQJIT32_64.cpp
@@ -312,7 +312,7 @@ PartialResult WARN_UNUSED_RETURN BBQJIT::setGlobal(uint32_t index, Value value)
         emitMove(value, Location::fromGlobal(offset));
         consume(value);
         if (isRefType(type))
-            emitWriteBarrier(wasmScratchGPR);
+            emitWriteBarrier(GPRInfo::wasmContextInstancePointer);
         break;
     }
     case Wasm::GlobalInformation::BindingMode::Portable: {