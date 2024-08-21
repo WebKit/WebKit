3357b1f7da58d86da53df06eb36f75c858ebe1b0
Per Arne Vollan <pvollan@apple.com>
2024-08-21 09:35:25 -0700

    Assert Lockdown Mode flag has been set before queried https://bugs.webkit.org/show_bug.cgi?id=278128 rdar://133884110
    Reviewed by Chris Dumez.
    
    Assert that Lockdown Mode flag has been set before queried in the WebContent process. This patch also
    initializes the Lockdown Mode flag in WebProcess earlier by using the value from the XPC initialization
    message, which is sent in the XPC extra initialization data. This patch also asserts that JSC options
    are set before JSC::initialize() is called. Setting JSC options after this call can lead to crashes.
    
    * Source/WebKit/Shared/EntryPointUtilities/Cocoa/XPCService/XPCServiceEntryPoint.h:
    (WebKit::XPCServiceInitializer):
    * Source/WebKit/Shared/EntryPointUtilities/Cocoa/XPCService/XPCServiceEntryPoint.mm:
    (WebKit::XPCServiceInitializerDelegate::getExtraInitializationData):
    (WebKit::setJSCOptions):
    * Source/WebKit/Shared/WebProcessCreationParameters.h:
    * Source/WebKit/Shared/WebProcessCreationParameters.serialization.in:
    * Source/WebKit/UIProcess/Launcher/cocoa/ProcessLauncherCocoa.mm:
    (WebKit::ProcessLauncher::finishLaunchingProcess):
    * Source/WebKit/UIProcess/WebProcessPool.cpp:
    (WebKit::WebProcessPool::initializeNewWebProcess):
    * Source/WebKit/UIProcess/WebProcessProxy.cpp:
    (WebKit::WebProcessProxy::getLaunchOptions):
    * Source/WebKit/WebProcess/WebProcess.cpp:
    (WebKit::WebProcess::initializeProcess):
    (WebKit::WebProcess::initializeWebProcess):
    * Source/WebKit/WebProcess/WebProcess.h:
    (WebKit::WebProcess::isLockdownModeEnabled const):
    
    Canonical link: https://commits.webkit.org/282562@main

--- Source/WebKit/WebProcess/WebProcess.cpp
+++ Source/WebKit/WebProcess/WebProcess.cpp
@@ -360,6 +360,8 @@ WebProcess::~WebProcess()
 
 void WebProcess::initializeProcess(const AuxiliaryProcessInitializationParameters& parameters)
 {
+    m_isLockdownModeEnabled = parameters.extraInitializationData.get<HashTranslatorASCIILiteral>("enable-lockdown-mode"_s) == "1"_s;
+
     WTF::setProcessPrivileges({ });
 
     {
@@ -603,8 +605,7 @@ void WebProcess::initializeWebProcess(WebProcessCreationParameters&& parameters,
     commonVM().setGlobalConstRedeclarationShouldThrow(parameters.shouldThrowExceptionForGlobalConstantRedeclaration);
 
     ScriptExecutionContext::setCrossOriginMode(parameters.crossOriginMode);
-    m_isLockdownModeEnabled = parameters.isLockdownModeEnabled;
-    DeprecatedGlobalSettings::setArePDFImagesEnabled(!m_isLockdownModeEnabled);
+    DeprecatedGlobalSettings::setArePDFImagesEnabled(!isLockdownModeEnabled());
 
 #if ENABLE(SERVICE_CONTROLS)
     setEnabledServices(parameters.hasImageServices, parameters.hasSelectionServices, parameters.hasRichContentServices);
