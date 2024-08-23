031367be5b9a5f52893f49535fdf503efc95196e
Ryosuke Niwa <rniwa@webkit.org>
2024-08-23 13:05:36 -0700

    Cherry-pick c213f1482344. rdar://134538423
        Crash in TimerBase::~TimerBase via DataDetection::detectContentInFrame
        https://bugs.webkit.org/show_bug.cgi?id=278259
        <rdar://133488287>
    
        Reviewed by Wenson Hsieh.
    
        Speculative fix. Use a WeakPtr instead of RefPtr in detectContentInFrame so that
        we don't attempt to destruct Document in a background thread.
    
        * Source/WebCore/editing/cocoa/DataDetection.mm:
        (WebCore::DataDetection::detectContentInFrame):
    
        Canonical link: https://commits.webkit.org/282380@main
    
    Canonical link: https://commits.webkit.org/280938.262@safari-7619-branch

--- Source/WebCore/editing/cocoa/DataDetection.mm
+++ Source/WebCore/editing/cocoa/DataDetection.mm
@@ -726,17 +726,20 @@ void DataDetection::detectContentInFrame(LocalFrame* frame, OptionSet<DataDetect
     if (types.contains(DataDetectorType::LookupSuggestion))
         PAL::softLink_DataDetectorsCore_DDScannerEnableOptionalSource(scanner.get(), DDScannerSourceSpotlight, true);
 
-    workQueue().dispatch([scanner = WTFMove(scanner), types, referenceDateFromContext, scanQuery = WTFMove(scanQuery), document = Ref { *document }, fragments = WTFMove(fragments), completionHandler = WTFMove(completionHandler)]() mutable {
+    workQueue().dispatch([scanner = WTFMove(scanner), types, referenceDateFromContext, scanQuery = WTFMove(scanQuery), weakDocument = WeakPtr { *document }, fragments = WTFMove(fragments), completionHandler = WTFMove(completionHandler)]() mutable {
         if (!PAL::softLink_DataDetectorsCore_DDScannerScanQuery(scanner.get(), scanQuery.get())) {
-            callOnMainRunLoop([scanner = WTFMove(scanner), scanQuery = WTFMove(scanQuery), document = WTFMove(document), fragments = WTFMove(fragments), completionHandler = WTFMove(completionHandler)]() mutable {
+            callOnMainRunLoop([scanner = WTFMove(scanner), scanQuery = WTFMove(scanQuery), weakDocument = WTFMove(weakDocument), fragments = WTFMove(fragments), completionHandler = WTFMove(completionHandler)]() mutable {
                 completionHandler(nil);
             });
             return;
         }
 
-        callOnMainRunLoop([scanner = WTFMove(scanner), types, referenceDateFromContext, scanQuery = WTFMove(scanQuery), document = WTFMove(document), fragments = WTFMove(fragments), completionHandler = WTFMove(completionHandler)]() mutable {
-            auto contextRange = makeRangeSelectingNodeContents(document);
+        callOnMainRunLoop([scanner = WTFMove(scanner), types, referenceDateFromContext, scanQuery = WTFMove(scanQuery), weakDocument = WTFMove(weakDocument), fragments = WTFMove(fragments), completionHandler = WTFMove(completionHandler)]() mutable {
+            RefPtr document = weakDocument.get();
+            if (!document)
+                return;
 
+            auto contextRange = makeRangeSelectingNodeContents(*document);
             completionHandler(processDataDetectorScannerResults(scanner.get(), types, referenceDateFromContext, scanQuery.get(), contextRange, fragments));
         });
     });
