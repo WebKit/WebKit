f95652711d7e02dc8fb2005baf8dd36c1239e2e3
David Degazio <d_degazio@apple.com>
2024-08-23 16:47:52 -0700

    [WASM] Check subtyping in both directions for mutable globals and tables https://bugs.webkit.org/show_bug.cgi?id=278497 rdar://problem/134450707
    Reviewed by Keith Miller and Yijia Huang.
    
    Replaces our existing typechecking for imported mutable globals and tables to
    check subtyping both ways. This ensures the type we import a global value or
    table as is not a supertype of the imported value's innate type - we don't want
    to be able to import a non-nullable (ref func) as a nullable (ref null func)
    and violate the original type by assigning it to a null value.
    
    * Source/JavaScriptCore/wasm/js/WebAssemblyModuleRecord.cpp:
    (JSC::WebAssemblyModuleRecord::initializeImports):
    
    Canonical link: https://commits.webkit.org/282682@main

--- JSTests/wasm/gc/linking.js
+++ JSTests/wasm/gc/linking.js
@@ -23,38 +23,12 @@ function testLinking() {
     const m0 = instantiate(`
       (module
         (type (func))
-        (table (export "t0") 10 (ref null 0)))
-    `);
-
-    const m1 = instantiate(`
-      (module
-        (table (import "m" "t1") 10 (ref null func)))
-    `, { m: { t1: m0.exports.t0 } });
-  }
-
-  {
-    const m0 = instantiate(`
-      (module
-        (type (func))
         (global (export "g0") (ref null 0) (ref.null 0)))
     `);
 
     const m1 = instantiate(`
       (module
         (global (import "m" "g1") (ref null func)))
-    `, { m: { g1: m0.exports.g0 } });
-  }
-
-  {
-    const m0 = instantiate(`
-      (module
-        (type (func))
-        (global (export "g0") (mut (ref null 0)) (ref.null 0)))
-    `);
-
-    const m1 = instantiate(`
-      (module
-        (global (import "m" "g1") (mut (ref null func))))
     `, { m: { g1: m0.exports.g0 } });
   }
 }
