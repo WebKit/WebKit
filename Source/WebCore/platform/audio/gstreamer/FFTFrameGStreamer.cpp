c438aed87c1082a36b7cda3ffae1b4eb93dd832f
Philippe Normand <philn@igalia.com>
2024-08-23 13:00:51 +0100

    [WebAudio][GStreamer] Fix null pointer dereference in FFT implementation
    Reviewed by NOBODY (OOPS!).
    
    This was spotted by ASan, the real and imaginary AudioFloatArrays end-up using aligned_alloc() (when
    bmalloc is disabled) for their storage, which expects a power-of-two size. Using fftSize / 2 + 1
    makes the size non-power-of-two, the full fftSize being power-of-two (we now have an ASSERT for
    this).
    
    Driving-by, the GstFFTF32 handling has been modernized using GUniquePtr.
    
    ==1733723==ERROR: AddressSanitizer: SEGV on unknown address 0x000000000000 (pc 0x7fc75c438bca bp 0x7ffed612c860 sp 0x7ffed612c028 T0)
    ==1733723==The signal is caused by a WRITE memory access.
    ==1733723==Hint: address points to the zero page.
        #0 0x7fc75c438bca in __memset_avx2_unaligned_erms (/lib64/libc.so.6+0x16dbca) (BuildId: 77c77fee058b19c6f001cf2cb0371ce3b8341211)
        #1 0x2fc4ce in __asan_memset (/var/home/phil/WebKit/local-build-gtk/WebKitBuild/GTK/Release/bin/WebKitWebProcess+0x2fc4ce) (BuildId: e2b0aff0c8fcab48026d63cf711cd70d0aeceb79)
        #2 0x7fc77047599e in WebCore::FFTFrame::FFTFrame(unsigned int) UnifiedSource-3c72abbe-20.cpp
        #3 0x7fc76d41c08b in WebCore::PeriodicWave::createBandLimitedTables(float const*, float const*, unsigned int, WebCore::ShouldDisableNormalization) UnifiedSource-f8afad56-52.cpp
        #4 0x7fc76d41f371 in WebCore::PeriodicWave::generateBasicWaveform(WebCore::PeriodicWave::Type) UnifiedSource-f8afad56-52.cpp
        #5 0x7fc76d41e633 in WebCore::PeriodicWave::createSine(float) UnifiedSource-f8afad56-52.cpp
        #6 0x7fc76d3c2352 in WebCore::BaseAudioContext::periodicWave(WebCore::OscillatorType) UnifiedSource-f8afad56-49.cpp
        #7 0x7fc76d410dbf in WebCore::OscillatorNode::setTypeForBindings(WebCore::OscillatorType) UnifiedSource-f8afad56-52.cpp
        #8 0x7fc76d4102f4 in WebCore::OscillatorNode::create(WebCore::BaseAudioContext&, WebCore::OscillatorOptions const&) UnifiedSource-f8afad56-52.cpp
        #9 0x7fc76d3bc210 in WebCore::BaseAudioContext::createOscillator() UnifiedSource-f8afad56-49.cpp
        #10 0x7fc76acbc743 in WebCore::jsBaseAudioContextPrototypeFunction_createOscillator(JSC::JSGlobalObject*, JSC::CallFrame*) UnifiedSource-3a52ce78-11.cpp
        #11 0x7fc705c10037  (<unknown module>)
    
    * Source/WebCore/platform/audio/FFTFrame.h:
    * Source/WebCore/platform/audio/gstreamer/FFTFrameGStreamer.cpp:
    (WebCore::FFTFrame::FFTFrame):
    (WebCore::FFTFrame::doFFT):
    (WebCore::FFTFrame::doInverseFFT):
    (WebCore::FFTFrame::~FFTFrame): Deleted.
    * Source/WebCore/platform/graphics/gstreamer/GUniquePtrGStreamer.h:

--- Source/WebCore/platform/audio/gstreamer/FFTFrameGStreamer.cpp
+++ Source/WebCore/platform/audio/gstreamer/FFTFrameGStreamer.cpp
@@ -25,19 +25,12 @@
 #include "FFTFrame.h"
 
 #include "VectorMath.h"
-#include <wtf/FastMalloc.h>
-#include <wtf/StdLibExtras.h>
 
 namespace {
 
 // FIXME: It is unclear what the limits are for gst_fft so we use the same ones as on macOS for now.
 const int kMinFFTPow2Size = 2;
 const int kMaxFFTPow2Size = 24;
-
-size_t unpackedFFTDataSize(unsigned fftSize)
-{
-    return fftSize / 2 + 1;
-}
 
 } // anonymous namespace
 
@@ -47,13 +40,17 @@ namespace WebCore {
 FFTFrame::FFTFrame(unsigned fftSize)
     : m_FFTSize(fftSize)
     , m_log2FFTSize(static_cast<unsigned>(log2(fftSize)))
-    , m_complexData(makeUniqueArray<GstFFTF32Complex>(unpackedFFTDataSize(m_FFTSize)))
-    , m_realData(unpackedFFTDataSize(m_FFTSize))
-    , m_imagData(unpackedFFTDataSize(m_FFTSize))
+    , m_complexData(makeUniqueArray<GstFFTF32Complex>(m_FFTSize))
 {
+    // We only allow power of two.
+    ASSERT(1UL << m_log2FFTSize == m_FFTSize);
+
+    m_realData.resize(m_FFTSize);
+    m_imagData.resize(m_FFTSize);
+
     int fftLength = gst_fft_next_fast_length(m_FFTSize);
-    m_fft = gst_fft_f32_new(fftLength, FALSE);
-    m_inverseFft = gst_fft_f32_new(fftLength, TRUE);
+    m_fft.reset(gst_fft_f32_new(fftLength, FALSE));
+    m_inverseFft.reset(gst_fft_f32_new(fftLength, TRUE));
 }
 
 // Creates a blank/empty frame (interpolate() must later be called).
@@ -62,21 +59,21 @@ FFTFrame::FFTFrame()
     , m_log2FFTSize(0)
 {
     int fftLength = gst_fft_next_fast_length(m_FFTSize);
-    m_fft = gst_fft_f32_new(fftLength, FALSE);
-    m_inverseFft = gst_fft_f32_new(fftLength, TRUE);
+    m_fft.reset(gst_fft_f32_new(fftLength, FALSE));
+    m_inverseFft.reset(gst_fft_f32_new(fftLength, TRUE));
 }
 
 // Copy constructor.
 FFTFrame::FFTFrame(const FFTFrame& frame)
     : m_FFTSize(frame.m_FFTSize)
     , m_log2FFTSize(frame.m_log2FFTSize)
-    , m_complexData(makeUniqueArray<GstFFTF32Complex>(unpackedFFTDataSize(m_FFTSize)))
-    , m_realData(unpackedFFTDataSize(frame.m_FFTSize))
-    , m_imagData(unpackedFFTDataSize(frame.m_FFTSize))
+    , m_complexData(makeUniqueArray<GstFFTF32Complex>(m_FFTSize))
+    , m_realData(frame.m_FFTSize)
+    , m_imagData(frame.m_FFTSize)
 {
     int fftLength = gst_fft_next_fast_length(m_FFTSize);
-    m_fft = gst_fft_f32_new(fftLength, FALSE);
-    m_inverseFft = gst_fft_f32_new(fftLength, TRUE);
+    m_fft.reset(gst_fft_f32_new(fftLength, FALSE));
+    m_inverseFft.reset(gst_fft_f32_new(fftLength, TRUE));
 
     // Copy/setup frame data.
     memcpy(realData().data(), frame.realData().data(), sizeof(float) * realData().size());
@@ -87,25 +84,15 @@ void FFTFrame::initialize()
 {
 }
 
-FFTFrame::~FFTFrame()
-{
-    if (!m_fft)
-        return;
-
-    gst_fft_f32_free(m_fft);
-    m_fft = 0;
-
-    gst_fft_f32_free(m_inverseFft);
-    m_inverseFft = 0;
-}
+FFTFrame::~FFTFrame() = default;
 
 void FFTFrame::doFFT(const float* data)
 {
-    gst_fft_f32_fft(m_fft, data, m_complexData.get());
+    gst_fft_f32_fft(m_fft.get(), data, m_complexData.get());
 
     float* imagData = m_imagData.data();
     float* realData = m_realData.data();
-    for (unsigned i = 0; i < unpackedFFTDataSize(m_FFTSize); ++i) {
+    for (unsigned i = 0; i < m_FFTSize; ++i) {
         imagData[i] = m_complexData[i].i;
         realData[i] = m_complexData[i].r;
     }
@@ -117,12 +104,12 @@ void FFTFrame::doInverseFFT(float* data)
     float* realData = m_realData.data();
     float* imagData = m_imagData.data();
 
-    for (size_t i = 0; i < unpackedFFTDataSize(m_FFTSize); ++i) {
+    for (size_t i = 0; i < m_FFTSize; ++i) {
         m_complexData[i].i = imagData[i];
         m_complexData[i].r = realData[i];
     }
 
-    gst_fft_f32_inverse_fft(m_inverseFft, m_complexData.get(), data);
+    gst_fft_f32_inverse_fft(m_inverseFft.get(), m_complexData.get(), data);
 
     // Scale so that a forward then inverse FFT yields exactly the original data.
     VectorMath::multiplyByScalar(data, 1.0 / m_FFTSize, data, m_FFTSize);
