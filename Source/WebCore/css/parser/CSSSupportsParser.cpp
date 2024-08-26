ee62852a23217119046e38cf1033d0ba8176194f
Alexsander Borges Damaceno <alexbdamac@gmail.com>
2024-08-26 13:33:16 -0700

    Convert ClauseType to an enum class https://bugs.webkit.org/show_bug.cgi?id=278670
    Reviewed by Tim Nguyen.
    
    Convert ClauseType to an enum class.
    
    * Source/WebCore/css/parser/CSSSupportsParser.cpp:
    (WebCore::CSSSupportsParser::consumeCondition):
    
    Canonical link: https://commits.webkit.org/282763@main

--- Source/WebCore/css/parser/CSSSupportsParser.cpp
+++ Source/WebCore/css/parser/CSSSupportsParser.cpp
@@ -57,7 +57,7 @@ CSSSupportsParser::SupportsResult CSSSupportsParser::supportsCondition(CSSParser
     return supportsParser.consumeSupportsFeatureOrGeneralEnclosed(range);
 }
 
-enum ClauseType { Unresolved, Conjunction, Disjunction };
+enum class ClauseType { Unresolved, Conjunction, Disjunction };
 
 CSSSupportsParser::SupportsResult CSSSupportsParser::consumeCondition(CSSParserTokenRange range)
 {
@@ -67,7 +67,7 @@ CSSSupportsParser::SupportsResult CSSSupportsParser::consumeCondition(CSSParserT
     }
 
     bool result = false;
-    ClauseType clauseType = Unresolved;
+    auto clauseType = ClauseType::Unresolved;
 
     auto previousTokenType = IdentToken;
 
@@ -76,9 +76,9 @@ CSSSupportsParser::SupportsResult CSSSupportsParser::consumeCondition(CSSParserT
         if (nextResult == Invalid)
             return Invalid;
         bool nextSupported = nextResult;
-        if (clauseType == Unresolved)
+        if (clauseType == ClauseType::Unresolved)
             result = nextSupported;
-        else if (clauseType == Conjunction)
+        else if (clauseType == ClauseType::Conjunction)
             result &= nextSupported;
         else
             result |= nextSupported;
@@ -95,10 +95,10 @@ CSSSupportsParser::SupportsResult CSSSupportsParser::consumeCondition(CSSParserT
 
         previousTokenType = token.type();
 
-        if (clauseType == Unresolved)
-            clauseType = token.value().length() == 3 ? Conjunction : Disjunction;
-        if ((clauseType == Conjunction && !equalLettersIgnoringASCIICase(token.value(), "and"_s))
-            || (clauseType == Disjunction && !equalLettersIgnoringASCIICase(token.value(), "or"_s)))
+        if (clauseType == ClauseType::Unresolved)
+            clauseType = token.value().length() == 3 ? ClauseType::Conjunction : ClauseType::Disjunction;
+        if ((clauseType == ClauseType::Conjunction && !equalLettersIgnoringASCIICase(token.value(), "and"_s))
+            || (clauseType == ClauseType::Disjunction && !equalLettersIgnoringASCIICase(token.value(), "or"_s)))
             return Invalid;
 
         range.consume();
