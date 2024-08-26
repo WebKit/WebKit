b64b05a02992c1687fbc6e09e0f95c603140fe7f
Abrar Rahman Protyasha <a_protyasha@apple.com>
2024-08-26 15:33:50 -0700

    Fix the visionOS build after 282761@main https://bugs.webkit.org/show_bug.cgi?id=278680 rdar://134730443
    Unreviewed build fix.
    
    The _WKApplicationManifestDirection declaration introduced in 282761@main
    did not use the correct availability annotation for the visionOS
    platform, and as such broke the build for some visionOS configurations.
    
    * Source/WebKit/UIProcess/API/Cocoa/_WKApplicationManifest.h:
    
    Canonical link: https://commits.webkit.org/282767@main

--- Source/WebKit/UIProcess/API/Cocoa/_WKApplicationManifest.h
+++ Source/WebKit/UIProcess/API/Cocoa/_WKApplicationManifest.h
@@ -40,7 +40,7 @@ typedef NS_ENUM(NSInteger, _WKApplicationManifestDirection) {
     _WKApplicationManifestDirectionAuto,
     _WKApplicationManifestDirectionLTR,
     _WKApplicationManifestDirectionRTL,
-} WK_API_AVAILABLE(macos(WK_MAC_TBA), ios(WK_IOS_TBA));
+} WK_API_AVAILABLE(macos(WK_MAC_TBA), ios(WK_IOS_TBA), visionos(WK_XROS_TBA));
 
 typedef NS_ENUM(NSInteger, _WKApplicationManifestDisplayMode) {
     _WKApplicationManifestDisplayModeBrowser,
