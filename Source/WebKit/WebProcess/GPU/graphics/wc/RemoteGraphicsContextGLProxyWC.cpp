396c3b575c9db7bd5cf30bb688ac6cf394a4172a
Commit Queue <commit-queue@webkit.org>
2024-08-28 14:14:19 -0700

    Unreviewed, reverting 282863@main. https://bugs.webkit.org/show_bug.cgi?id=278815
    broke Windows port
    
    Reverted changeset:
    
    "REGRESSION(282843@main): [Win] ASSERTION FAILED: sizeof(RemoteGraphicsContextGLProxy) == size"
    https://bugs.webkit.org/show_bug.cgi?id=278812
    https://commits.webkit.org/282863@main
    
    Canonical link: https://commits.webkit.org/282864@main

--- Source/WebKit/WebProcess/GPU/graphics/wc/RemoteGraphicsContextGLProxyWC.cpp
+++ Source/WebKit/WebProcess/GPU/graphics/wc/RemoteGraphicsContextGLProxyWC.cpp
@@ -63,8 +63,6 @@ private:
 };
 
 class RemoteGraphicsContextGLProxyWC final : public RemoteGraphicsContextGLProxy {
-    WTF_MAKE_FAST_ALLOCATED;
-    WTF_OVERRIDE_DELETE_FOR_CHECKED_PTR(RemoteGraphicsContextGLProxyWC);
 public:
     // RemoteGraphicsContextGLProxy overrides.
     void prepareForDisplay() final;
