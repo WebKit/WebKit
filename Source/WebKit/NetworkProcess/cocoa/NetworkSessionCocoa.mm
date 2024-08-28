eb349e95072b59e560c0358da0611f6440855549
Matthew Finkel <sysrqb@apple.com>
2024-08-28 11:51:52 -0700

    Cherry-pick 1abc3386be80. rdar://134845097
        Revert "[cocoa] Improve respecting X-Content-Type-Options: nosniff"
        rdar://134845097
    
        This reverts commit f6fd96e4f3a29cd55a3bf351660298e6dddc0b9a.
    
        Caused regression on some high profile site.
    
    Canonical link: https://commits.webkit.org/280938.218@safari-7619.1.26.30-branch

--- Source/WebKit/NetworkProcess/cocoa/NetworkSessionCocoa.mm
+++ Source/WebKit/NetworkProcess/cocoa/NetworkSessionCocoa.mm
@@ -1100,13 +1100,10 @@ ALLOW_DEPRECATED_DECLARATIONS_END
         ALLOW_DEPRECATED_DECLARATIONS_END
 
         // Avoid MIME type sniffing if the response comes back as 304 Not Modified.
-        auto isNSHTTPURLResponseClass = [response isKindOfClass:NSHTTPURLResponse.class];
-        int statusCode = isNSHTTPURLResponseClass ? [(NSHTTPURLResponse *)response statusCode] : 0;
-        NSString *xContentTypeOptions = isNSHTTPURLResponseClass ? [(NSHTTPURLResponse *)response valueForHTTPHeaderField:@"X-Content-Type-Options"] : nil;
-        bool isNoSniff = xContentTypeOptions && [xContentTypeOptions caseInsensitiveCompare:@"nosniff"] == NSOrderedSame;
+        int statusCode = [response isKindOfClass:NSHTTPURLResponse.class] ? [(NSHTTPURLResponse *)response statusCode] : 0;
         if (statusCode != httpStatus304NotModified) {
             bool isMainResourceLoad = networkDataTask->firstRequest().requester() == WebCore::ResourceRequestRequester::Main;
-            WebCore::adjustMIMETypeIfNecessary(response._CFURLResponse, isMainResourceLoad ? WebCore::IsMainResourceLoad::Yes : WebCore::IsMainResourceLoad::No, isNoSniff ? WebCore::IsNoSniffSet::Yes : WebCore::IsNoSniffSet::No);
+            WebCore::adjustMIMETypeIfNecessary(response._CFURLResponse, isMainResourceLoad);
         }
 
         WebCore::ResourceResponse resourceResponse(response);
