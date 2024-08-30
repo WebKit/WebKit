7b711fb51888e9785b998dd368d394b7180a41d0
Alan Baradlay <zalan@apple.com>
2024-08-30 13:10:39 -0700

    Hovering on CBC homepage causes weird scaling to happen as you move https://bugs.webkit.org/show_bug.cgi?id=278894 <rdar://problem/134986382>
    Reviewed by Antti Koivisto.
    
    Preferred width size should never be affected by override size (set by containing boxes e.g. flex, grid).
    
    Override size (as its name implies) is meant to force child boxes use pre-computed widths in flexing context (again e.g. flex, grid)
    However this override size, in many cases is result of a previously computed intrinsic sizes coming from child content.
    
    Here the first set of preferred width computation finishes with the correct geometry as there's no override size yet (nothing to set), while
    any subsequent layout would force this previously computed override size on preferred with computation (hence the growing content).
    
    This change was introduced as part of r276572 with no explanation of why it was required ("Take the overriding width into account when computing the preferred min/max values for the element" <- not helpful).
    r276572 fixed a few WPTS and those still pass after undoing the preferred with size override.
    
    * LayoutTests/fast/table/expanding-table-with-grid-cell-expected.html: Added.
    * LayoutTests/fast/table/expanding-table-with-grid-cell.html: Added.
    * Source/WebCore/rendering/RenderTable.cpp:
    (WebCore::RenderTable::computePreferredLogicalWidths):
    
    Canonical link: https://commits.webkit.org/282988@main

--- Source/WebCore/rendering/RenderTable.cpp
+++ Source/WebCore/rendering/RenderTable.cpp
@@ -911,11 +911,6 @@ void RenderTable::computePreferredLogicalWidths()
     for (unsigned i = 0; i < m_captions.size(); i++)
         m_minPreferredLogicalWidth = std::max(m_minPreferredLogicalWidth, m_captions[i]->minPreferredLogicalWidth());
 
-    if (auto overridingLogicalWidth = this->overridingLogicalWidth()) {
-        m_minPreferredLogicalWidth = std::max(m_minPreferredLogicalWidth, *overridingLogicalWidth);
-        m_maxPreferredLogicalWidth = std::max(m_maxPreferredLogicalWidth, *overridingLogicalWidth);
-    }
-
     auto& styleToUse = style();
     // FIXME: This should probably be checking for isSpecified since you should be able to use percentage or calc values for min-width.
     if (styleToUse.logicalMinWidth().isFixed() && styleToUse.logicalMinWidth().value() > 0) {
