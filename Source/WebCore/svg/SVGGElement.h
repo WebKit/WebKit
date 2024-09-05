a4e4545085fa041626131f04be20cc284426e420
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2024-09-05 10:38:31 -0700

    [SVG2] Fix `presentational hints` special cases where supported
    https://bugs.webkit.org/show_bug.cgi?id=279144
    rdar://135298525
    
    Reviewed by Simon Fraser.
    
    This patch aligns WebKit with Gecko / Firefox, Blink / Chromium and
    Web Specification [1]:
    
    [1] https://svgwg.org/svg2-draft/styling.html#PresentationAttributes
    
    This patch ensures that `gradientTransform`, `patternTransformAttribute`
    and `transform` are mapped to transform property on supported elements.
    
    This patch also sync particular test from WPT upstream as well.
    
    Upstream commit: https://github.com/web-platform-tests/wpt/commit/3fed65b68731adff5b419980ad6c02bf83159252
    
    * LayoutTests/imported/w3c/web-platform-tests/svg/styling/presentation-attributes-relevant-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/svg/styling/presentation-attributes-special-cases-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/svg/styling/presentation-attributes-special-cases.html:
    * LayoutTests/imported/w3c/web-platform-tests/svg/styling/presentation-attributes.js:
    (presentationAttributeIsSupported):
    * Source/WebCore/svg/SVGGElement.cpp:
    (WebCore::SVGGElement::collectPresentationalHintsForAttribute):
    * Source/WebCore/svg/SVGGElement.h:
    * Source/WebCore/svg/SVGGradientElement.cpp:
    (WebCore::SVGGradientElement::collectPresentationalHintsForAttribute):
    * Source/WebCore/svg/SVGGradientElement.h:
    * Source/WebCore/svg/SVGPatternElement.cpp:
    (WebCore::SVGPatternElement::collectPresentationalHintsForAttribute):
    * Source/WebCore/svg/SVGPatternElement.h:
    * LayoutTests/platform/glib/svg/batik/text/textProperties-expected.txt:
    * LayoutTests/platform/ios/svg/batik/text/textProperties-expected.txt:
    * LayoutTests/platform/mac/svg/batik/text/textProperties-expected.txt:
    
    Canonical link: https://commits.webkit.org/283223@main

--- Source/WebCore/svg/SVGGElement.h
+++ Source/WebCore/svg/SVGGElement.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2004, 2005, 2007, 2008 Nikolas Zimmermann <zimmermann@kde.org>
  * Copyright (C) 2004, 2005, 2006 Rob Buis <buis@kde.org>
- * Copyright (C) 2018-2019 Apple Inc. All rights reserved.
+ * Copyright (C) 2018-2024 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -34,6 +34,8 @@ public:
     static Ref<SVGGElement> create(Document&);
 
     using PropertyRegistry = SVGPropertyOwnerRegistry<SVGGElement, SVGGraphicsElement>;
+
+    void collectPresentationalHintsForAttribute(const QualifiedName&, const AtomString&, MutableStyleProperties&) override;
 
 private:
     SVGGElement(const QualifiedName&, Document&);
