59e06f7486a4de4137166a91662c879cd21a7cfa
Rose <gfunni234@gmail.com>
2024-09-05 19:59:19 -0400

    [JSC] Fix initWithValue antipattern in JSManagedValue https://bugs.webkit.org/show_bug.cgi?id=279235
    Reviewed by NOBODY (OOPS!).
    
    The idea of calling an initWithValue and passing nil is weird.
    Just call [super init] at that point and call it a day.
    This will also confuse analyzers less.
    
    * Source/JavaScriptCore/API/JSManagedValue.mm:
      (-[JSManagedValue init]): Call [super init] directly.

--- Source/JavaScriptCore/API/JSManagedValue.mm
+++ Source/JavaScriptCore/API/JSManagedValue.mm
@@ -73,7 +73,8 @@ static JSManagedValueHandleOwner& managedValueHandleOwner()
 
 - (instancetype)init
 {
-    return [self initWithValue:nil];
+    self = [super init];
+    return self;
 }
 
 - (instancetype)initWithValue:(JSValue *)value
