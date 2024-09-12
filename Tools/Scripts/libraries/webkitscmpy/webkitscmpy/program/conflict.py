c2e76f6579ddf2b7c0424c7b23405c1a5c1aff38
Alex Atwater <alexandera_22@apple.com>
2024-09-12 10:49:56 -0700

    Update git-webkit conflict to support additional integration/ branches https://bugs.webkit.org/show_bug.cgi?id=278562 rdar://129681381
    Reviewed by Ryan Haddad.
    
    This will now search for updated integration branches when running git-webkit conflict.
    
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/conflict.py:
    (Conflict):
    (Conflict.parser):
    (Conflict.find_conflict_pr):
    (Conflict.get_open_integration_prs):
    (Conflict.main):
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/conflict_unittest.py:
    (TestConflict._mock_radar_response):
    (TestConflict.test_conflict_not_found):
    (TestConflict.test_conflict_found):
    
    Canonical link: https://commits.webkit.org/283569@main

--- Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/conflict.py
+++ Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/conflict.py
@@ -22,26 +22,54 @@
 
 import sys
 
+from webkitbugspy import Tracker, radar
 from .command import Command
 from .. import local
+from ..commit import Commit
 
 
 class Conflict(Command):
     name = 'conflict'
     help = "Given the representative Radar ID of a conflicting merge, checkout the branch."
+    INTEGRATION_BRANCH_PREFIX = 'integration'
 
     @classmethod
     def parser(cls, parser, loggers=None):
         parser.add_argument(
-            'radar_id',
-            type=int, default=None,
-            help='Radar ID that caused a merge conflict.',
+            'radar',
+            type=str, default=None,
+            help='Radar ID that caused a merge conflict. Ex. rdar://problem/123, rdar://123, 123',
         )
 
     @classmethod
-    def find_conflict_pr(cls, remote, branch):
-        for pr in remote.pull_requests.find(head=branch, opened=True):
-            return pr
+    def find_conflict_pr(cls, remote, radar_id):
+        """
+        Because we don't know what the target branch was just given the radar id,
+        we need to search for PRs with branches that start with the integration prefix
+        and have the first and last sha.
+        """
+        radar_obj = Tracker.from_string(f'rdar://{radar_id}')
+        assert radar_obj, 'Could not fetch radar object for id {}'.format(radar_id)
+        shas = []
+
+        repo_name = remote.name if '/' not in remote.name else remote.name.split('/', 1)[-1]
+        for entry in radar_obj.source_changes:
+            repo, action, sha = entry.split(', ')
+            if repo == repo_name:
+                shas.append(sha)
+
+        integration_branches = []
+        for prefix in ('ci', 'conflict'):
+            integration_branches.append("{}/{}/{}_{}".format(cls.INTEGRATION_BRANCH_PREFIX, prefix, shas[0][:Commit.HASH_LABEL_SIZE], shas[-1][:Commit.HASH_LABEL_SIZE]))
+
+        for pr in cls.get_open_integration_prs(remote):
+            for branch in integration_branches:
+                if pr.head.startswith(branch):
+                    return pr
+
+    @classmethod
+    def get_open_integration_prs(cls, remote):
+        return remote.pull_requests.find(head=cls.INTEGRATION_BRANCH_PREFIX, opened=True)
 
     @classmethod
     def main(cls, args, repository, **kwargs):
@@ -52,11 +80,13 @@ class Conflict(Command):
             sys.stderr.write('Cannot checkout conflict, must be in a local git repository\n')
             return 1
 
-        expected_branch = 'integration/conflict/{}'.format(args.radar_id)
+        # This is to remove any extra inputs like rdar://problem/
+        radar_id = ''.join(i for i in args.radar if i.isdigit())
+        radar_obj = Tracker.from_string(f'rdar://{radar_id}')
+        expected_branch = 'integration/conflict/{}'.format(radar_obj.id)
         for source_remote in repository.source_remotes():
             rmt = repository.remote(name=source_remote)
-
-            conflict_pr = cls.find_conflict_pr(rmt, expected_branch)
+            conflict_pr = cls.find_conflict_pr(rmt, radar_obj.id)
             if conflict_pr:
                 return repository.checkout('{}:{}'.format(conflict_pr._metadata['full_name'], conflict_pr.head))
 
