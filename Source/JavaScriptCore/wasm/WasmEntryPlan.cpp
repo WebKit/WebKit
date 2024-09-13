90792d022bbe9119984cb53379925d6ba4b5ac51
Ross Kirsling <ross.kirsling@sony.com>
2024-09-13 14:12:31 -0700

    Remove leftover ENABLE(JIT) guards from WasmEntryPlan https://bugs.webkit.org/show_bug.cgi?id=279651
    Reviewed by Yusuke Suzuki.
    
    282172@main implemented !ENABLE(JIT) paths for compileFunctions and generateStubsIfNecessary,
    but the calls themselves were left guarded, causing segfaults when building (and not simply running) without JIT.
    
    * Source/JavaScriptCore/wasm/WasmEntryPlan.cpp:
    (JSC::Wasm::EntryPlan::compileFunctions):
    (JSC::Wasm::EntryPlan::generateStubsIfNecessary):
    Remove leftover ENABLE(JIT) guards.
    
    Canonical link: https://commits.webkit.org/283651@main

--- Source/JavaScriptCore/wasm/WasmEntryPlan.cpp
+++ Source/JavaScriptCore/wasm/WasmEntryPlan.cpp
@@ -218,23 +218,19 @@ void EntryPlan::compileFunctions(CompilationEffort effort)
             compileFunction(index);
 
         if (!areWasmToWasmStubsCompiled) {
-#if ENABLE(JIT)
             if (UNLIKELY(!generateWasmToWasmStubs())) {
                 Locker locker { m_lock };
                 fail(makeString("Out of executable memory at stub generation"_s));
                 return;
             }
-#endif
         }
 
         if (!areWasmToJSStubsCompiled) {
-#if ENABLE(JIT)
             if (UNLIKELY(!generateWasmToJSStubs())) {
                 Locker locker { m_lock };
                 fail(makeString("Out of executable memory at stub generation"_s));
                 return;
             }
-#endif
         }
     }
 }
@@ -272,21 +268,17 @@ bool EntryPlan::completeSyncIfPossible()
 void EntryPlan::generateStubsIfNecessary()
 {
     if (!std::exchange(m_areWasmToWasmStubsCompiled, true)) {
-#if ENABLE(JIT)
         if (UNLIKELY(!generateWasmToWasmStubs())) {
             fail(makeString("Out of executable memory at stub generation"_s));
             return;
         }
-#endif
     }
 
     if (!std::exchange(m_areWasmToJSStubsCompiled, true)) {
-#if ENABLE(JIT)
         if (UNLIKELY(!generateWasmToJSStubs())) {
             fail(makeString("Out of executable memory at stub generation"_s));
             return;
         }
-#endif
     }
 }
 
