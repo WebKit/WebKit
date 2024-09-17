20cfad1c39ebdd0b4cbfb648f70812774e3c9802
Nicole Rosario <nicole_rosario@apple.com>
2024-09-17 09:38:18 -0700

    Cherry-pick 283780@main (16d8e9346f0f). https://bugs.webkit.org/show_bug.cgi?id=276637
        Hitting RELEASE_ASSERT(amount <= bitfields.connectedSubframeCount) in decrement subframe
        https://bugs.webkit.org/show_bug.cgi?id=276637
        rdar://130091196
    
        Reviewed by Ryosuke Niwa.
    
        Over decrements connectedSubframeCount (tries to decrease when it's 0)
        due to calling `frame->disconnectOwnerElement()` even if it was already
        processed
    
        * Source/WebCore/html/HTMLFrameOwnerElement.cpp:
        (WebCore::HTMLFrameOwnerElement::disconnectContentFrame):
        * LayoutTests/fast/html/decrement-subframes-expected.txt
        * LayoutTests/fast/html/decrement-subframes.html
    
        Originally-landed-as: 280938.90@safari-7619-branch (eb3508a3bd08). rdar://136110756
        Canonical link: https://commits.webkit.org/283780@main
    
    Canonical link: https://commits.webkit.org/274313.437@webkitglib/2.44

--- Source/WebCore/html/HTMLFrameOwnerElement.cpp
+++ Source/WebCore/html/HTMLFrameOwnerElement.cpp
@@ -81,7 +81,9 @@ void HTMLFrameOwnerElement::disconnectContentFrame()
         if (frame->settings().siteIsolationEnabled())
             frame->broadcastFrameRemovalToOtherProcesses();
         frame->frameDetached();
-        frame->disconnectOwnerElement();
+        if (frame == m_contentFrame.get())
+            frame->disconnectOwnerElement();
+        RELEASE_ASSERT_WITH_SECURITY_IMPLICATION(frame != m_contentFrame.get());
     }
 }
 
