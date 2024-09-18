6837cf249f33c48e9a165a0a155072392adf4185
Philippe Normand <philn@igalia.com>
2024-09-18 02:30:05 -0700

    Cherry-pick 283833@main (8a57ce995163). https://bugs.webkit.org/show_bug.cgi?id=279819
        [GStreamer][DMABuf] Disable DMABuf video sink by default
        https://bugs.webkit.org/show_bug.cgi?id=279819
    
        Reviewed by Xabier Rodriguez-Calvar.
    
        In its current form this sink and the associated rendering logic in the TextureMapper are prone to
        memory leaks and increased file descriptors usage. There is a plan to rework this but it was too
        late for the 2.46 release.
    
        * Source/WebCore/platform/graphics/gstreamer/DMABufVideoSinkGStreamer.cpp:
        (webKitDMABufVideoSinkIsEnabled):
        * Tools/Scripts/webkitpy/port/glib.py:
        (GLibPort.setup_environ_for_server):
    
        Canonical link: https://commits.webkit.org/283833@main
    
    Canonical link: https://commits.webkit.org/274313.431@webkitglib/2.44

--- Source/WebCore/platform/graphics/gstreamer/DMABufVideoSinkGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/DMABufVideoSinkGStreamer.cpp
@@ -175,21 +175,21 @@ static void webkit_dmabuf_video_sink_class_init(WebKitDMABufVideoSinkClass* klas
 
 bool webKitDMABufVideoSinkIsEnabled()
 {
-    static bool s_disabled = false;
+    static bool s_enabled = false;
 #if USE(GBM)
     static std::once_flag s_flag;
     std::call_once(s_flag, [&] {
-        const char* value = g_getenv("WEBKIT_GST_DMABUF_SINK_DISABLED");
-        s_disabled = value && (equalLettersIgnoringASCIICase(value, "true"_s) || equalLettersIgnoringASCIICase(value, "1"_s));
-        if (!s_disabled && !GBMDevice::singleton().device()) {
+        const char* value = g_getenv("WEBKIT_GST_DMABUF_SINK_ENABLED");
+        s_enabled = value && (equalLettersIgnoringASCIICase(value, "true"_s) || equalLettersIgnoringASCIICase(value, "1"_s));
+        if (s_enabled && !GBMDevice::singleton().device()) {
             WTFLogAlways("Unable to access the GBM device, disabling DMABuf video sink.");
-            s_disabled = true;
+            s_enabled = false;
         }
     });
 #else
-    s_disabled = true;
+    s_enabled = false;
 #endif
-    return !s_disabled;
+    return s_enabled;
 }
 
 bool webKitDMABufVideoSinkProbePlatform()
