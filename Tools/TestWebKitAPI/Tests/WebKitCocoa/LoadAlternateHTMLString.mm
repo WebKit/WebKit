b6ec328bc3968dc46b1558969fdc6bbb1916534a
Dawn Morningstar <morningstar@apple.com>
2024-09-19 12:55:18 -0700

    Unreviewed, reverting 283937@main (f6696434ef62) https://bugs.webkit.org/show_bug.cgi?id=280007 rdar://136310996
    REVERT 283937@main Broke builds
    
    Reverted change:
    
        [ Gardening ] TestWebKitAPI.WebKit.LoadMoreThan4GB always times out on Debug builds
        https://bugs.webkit.org/show_bug.cgi?id=278563
        rdar://134569278
        283937@main (f6696434ef62)
    
    Canonical link: https://commits.webkit.org/283940@main

--- Tools/TestWebKitAPI/Tests/WebKitCocoa/LoadAlternateHTMLString.mm
+++ Tools/TestWebKitAPI/Tests/WebKitCocoa/LoadAlternateHTMLString.mm
@@ -214,9 +214,6 @@ TEST(WebKit, LoadHTMLStringWithInvalidBaseURL)
     EXPECT_FALSE(didCrash);
 }
 
-#if !defined(NDEBUG)
-TEST(WebKit, DISABLED_LoadMoreThan4GB)
-#else
 TEST(WebKit, LoadMoreThan4GB)
 {
     constexpr auto html = "<script>"
