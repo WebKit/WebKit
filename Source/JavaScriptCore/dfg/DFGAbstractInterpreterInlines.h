494de07b887e4a18400603824d12353e82754b1d
Yusuke Suzuki <ysuzuki@apple.com>
2024-09-25 15:25:17 -0700

    [JSC] Convert CheckStructure with String Structure etc. to Check(StringUse) https://bugs.webkit.org/show_bug.cgi?id=280327 <rdar://problem/136659524>
    Reviewed by Yijia Huang.
    
    We found that CheckStructure(String structure) is not effectively
    removed when abstract value is saying it is SpecString. We should
    convert it to Check(StringUse) so that we can remove this based on more
    coarse grained type information in abstract value. Also this patch does
    it for SymbolUse and HeapBigIntUse too.
    
    * Source/JavaScriptCore/dfg/DFGAbstractInterpreterInlines.h:
    (JSC::DFG::AbstractInterpreter<AbstractStateType>::executeEffects):
    * Source/JavaScriptCore/dfg/DFGConstantFoldingPhase.cpp:
    (JSC::DFG::ConstantFoldingPhase::foldConstants):
    
    Canonical link: https://commits.webkit.org/284249@main

--- Source/JavaScriptCore/dfg/DFGAbstractInterpreterInlines.h
+++ Source/JavaScriptCore/dfg/DFGAbstractInterpreterInlines.h
@@ -4031,6 +4031,12 @@ bool AbstractInterpreter<AbstractStateType>::executeEffects(unsigned clobberLimi
         
         if (value.m_structure.isSubsetOf(set))
             m_state.setShouldTryConstantFolding(true);
+        else if (value.m_type) {
+            if (set.onlyStructure().get() == m_vm.stringStructure.get()
+                || set.onlyStructure().get() == m_vm.symbolStructure.get()
+                || set.onlyStructure().get() == m_vm.bigIntStructure.get())
+                m_state.setShouldTryConstantFolding(true);
+        }
 
         SpeculatedType admittedTypes = SpecNone;
         switch (node->child1().useKind()) {
