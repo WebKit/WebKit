77875945873424054d51fa338846c4b70968923f
Anil Hebbar <anil_hebbar@apple.com>
2024-09-25 14:54:22 -0700

    [b6b120387cbd43a7] ASAN_SEGV | WebKit::UnifiedPDFPlugin::didChangeSettings; WebKit::PluginView::didChangeSettings; WebKit::WebPage::updatePreferences
    https://bugs.webkit.org/show_bug.cgi?id=280291
    
    rdar://135586580
    
    Reviewed by NOBODY (OOPS!).
    
    THe crash was because of referencing NULL pointers. The fix was to do a NULL check on the pointers befoer they are used
    
    * Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/PDFScrollingPresentationController.mm:
    (WebKit::PDFScrollingPresentationController::updateDebugBorders):
    * Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/UnifiedPDFPlugin.mm:
    (WebKit::UnifiedPDFPlugin::didChangeSettings):
    
    Changes to be committed:
    Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/PDFScrollingPresentationController.mm
    Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/UnifiedPDFPlugin.mm

--- Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/UnifiedPDFPlugin.mm
+++ Source/WebKit/WebProcess/Plugins/PDF/UnifiedPDF/UnifiedPDFPlugin.mm
@@ -610,9 +610,14 @@ void UnifiedPDFPlugin::didChangeSettings()
         layer.setShowDebugBorder(showDebugBorders);
         layer.setShowRepaintCounter(showRepaintCounter);
     };
-    propagateSettingsToLayer(*m_rootLayer);
-    propagateSettingsToLayer(*m_scrollContainerLayer);
-    propagateSettingsToLayer(*m_scrolledContentsLayer);
+    if (m_rootLayer)
+        propagateSettingsToLayer(*m_rootLayer);
+
+    if (m_scrollContainerLayer)
+        propagateSettingsToLayer(*m_scrollContainerLayer);
+
+    if (m_scrolledContentsLayer)
+        propagateSettingsToLayer(*m_scrolledContentsLayer);
 
     if (m_layerForHorizontalScrollbar)
         propagateSettingsToLayer(*m_layerForHorizontalScrollbar);
