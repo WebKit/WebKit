91c3660a4cbc627b2a9b27476bcaf4dac7b8c72c
Brianna Fan <bfan2@apple.com>
2024-09-27 14:46:23 -0700

    The jsc-armv7 and jsc-armv7-tests EWS bots should not block merging https://bugs.webkit.org/show_bug.cgi?id=280541 rdar://136542647
    Reviewed by Ryan Haddad.
    
    Remove tests from the safe-merge-queue status check.
    
    * Tools/CISupport/ews-build/steps.py:
    (CheckStatusOfPR):
    
    Canonical link: https://commits.webkit.org/284392@main

--- Tools/CISupport/ews-build/steps.py
+++ Tools/CISupport/ews-build/steps.py
@@ -2522,7 +2522,7 @@ class CheckStatusOfPR(buildstep.BuildStep, GitHubMixin, AddToLogMixin):
     haltOnFailure = False
     EMBEDDED_CHECKS = ['ios', 'ios-sim', 'ios-wk2', 'ios-wk2-wpt', 'api-ios', 'vision', 'vision-sim', 'vision-wk2', 'tv', 'tv-sim', 'watch', 'watch-sim']
     MACOS_CHECKS = ['mac', 'mac-AS-debug', 'api-mac', 'mac-wk1', 'mac-wk2', 'mac-AS-debug-wk2', 'mac-wk2-stress', 'jsc', 'jsc-arm64']
-    LINUX_CHECKS = ['gtk', 'gtk-wk2', 'api-gtk', 'wpe', 'wpe-cairo', 'wpe-wk2', 'api-wpe', 'jsc-armv7', 'jsc-armv7-tests']
+    LINUX_CHECKS = ['gtk', 'gtk-wk2', 'api-gtk', 'wpe', 'wpe-cairo', 'wpe-wk2', 'api-wpe']
     WINDOWS_CHECKS = ['win']
     EWS_WEBKIT_FAILED = 0
     EWS_WEBKIT_PASSED = 1
