6b59562904cd85cebdd91106308d35c5e112858d
Olivier Blin <olivier.blin@softathome.com>
2024-10-10 08:42:52 -0700

    Cherry-pick 284970@main (7ffe7f5c6b9c). https://bugs.webkit.org/show_bug.cgi?id=281159
        [GTK][WPE] Build broken with ENABLE_WEB_AUDIO=OFF
        https://bugs.webkit.org/show_bug.cgi?id=281159
    
        Reviewed by Miguel Gomez.
    
        Since 280453@main, default values from UnifiedWebPreferences.yaml are used.
        Do not try to use WebAudioEnabled when undefined.
    
        * Source/WebKit/UIProcess/API/glib/WebKitSettings.cpp:
        (webkit_settings_class_init):
    
        Canonical link: https://commits.webkit.org/284970@main
    
    Canonical link: https://commits.webkit.org/282416.204@webkitglib/2.46

--- Source/WebKit/UIProcess/API/glib/WebKitSettings.cpp
+++ Source/WebKit/UIProcess/API/glib/WebKitSettings.cpp
@@ -1105,7 +1105,11 @@ static void webkit_settings_class_init(WebKitSettingsClass* klass)
             "enable-webaudio",
             _("Enable WebAudio"),
             _("Whether WebAudio content should be handled"),
+#if ENABLE(WEB_AUDIO)
             FEATURE_DEFAULT(WebAudioEnabled),
+#else
+            FALSE,
+#endif
             readWriteConstructParamFlags);
 
     /**
