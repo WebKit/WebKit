233c7a6a0a0eac5b1c599eed7c11d34aab342387
Carlos Alberto Lopez Perez <clopez@igalia.com>
2024-10-11 06:28:12 -0700

    [Tools][GTK][WPE] generate-bundle: Add a test suite to test the bundle with webdriver and docker https://bugs.webkit.org/show_bug.cgi?id=280835
    Reviewed by Carlos Garcia Campos.
    
    This adds a test suite to automatically check that the generated bundled works correctly.
    It runs several tests with WebDriver that exercise advanced parts of the engine: webgl, wasm,
    video playback, css animations, etc.
    
    And it runs all those tests automatically in very different distros, from ancient distros like
    centos/rockylinux to bleeding edge distros like fedora/arch/gentoo to even non-standard distros
    with different libc like Alpine or distros like Nixos or Gentoo.
    
    This test suite will be executed on the bot generating the universal bundles to validate that
    the universal bundle works in all of those distros before uploading it.
    
    * Tools/Scripts/test-bundle: Added.
    (run_cmd):
    (tests_webdriver_minibroser):
    (main):
    * Tools/Scripts/webkitpy/binary_bundling/tests/webdriver/install_deps_docker_start_test.sh: Added.
    * Tools/Scripts/webkitpy/binary_bundling/tests/webdriver/run-webdriver-tests-bundle-docker.sh: Added.
    * Tools/Scripts/webkitpy/binary_bundling/tests/webdriver/test_css_animations.html: Added.
    * Tools/Scripts/webkitpy/binary_bundling/tests/webdriver/test_dynamic_rendering.html: Added.
    * Tools/Scripts/webkitpy/binary_bundling/tests/webdriver/test_video_playback.html: Added.
    * Tools/Scripts/webkitpy/binary_bundling/tests/webdriver/test_webdriver_bundle.py: Added.
    (callfunc):
    (WebDriverTests):
    (WebDriverTests.__init__):
    (WebDriverTests.__del__):
    (WebDriverTests.test_dynamic_rendering):
    (WebDriverTests.test_video_playback):
    (WebDriverTests.test_webgl_support):
    (WebDriverTests.test_css_animations):
    (WebDriverTests.test_webgl_rainbow):
    (WebDriverTests.test_webgl_rainbow.get_average_color):
    (WebDriverTests.test_webgl_rainbow.test_color_filling):
    (WebDriverTests.test_ssl_errors):
    (WebDriverTests.test_wasm_doom_load):
    (WebDriverTests.do_test):
    (WebDriverTests.do_local_test):
    (run_all_tests):
    (main):
    * Tools/Scripts/webkitpy/binary_bundling/tests/webdriver/test_webgl_rainbow.html: Added.
    * Tools/Scripts/webkitpy/binary_bundling/tests/webdriver/test_webgl_support.html: Added.
    
    Canonical link: https://commits.webkit.org/285014@main

---
+++ Tools/Scripts/webkitpy/binary_bundling/tests/webdriver/install_deps_docker_start_test.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+# Note: keep this script POSIX-shell compatible as some
+# distros like Alpine do not ship by default with bash
+set -eux
+
+# Determine current distro
+if [ -f /etc/os-release ]; then
+    CURRENT_DISTRO="$(grep ^ID= /etc/os-release | cut -d= -f2 | sed 's/"//g' | tr '[:upper:]' '[:lower:]')"
+elif [ -f /etc/nix/nix.conf -a -d /nix/store ]; then
+    CURRENT_DISTRO="nixos"
+else
+    echo "Can not determine current distro type"
+    exit 1
+fi
+
+# Install needed packages
+case "${CURRENT_DISTRO}" in
+    alpine)
+        apk update
+        apk upgrade
+        apk add py3-pip py3-pillow py3-numpy
+        python3 -m pip config set global.break-system-packages true
+        pip3 install selenium==4.24.0
+    ;;
+    ubuntu|debian)
+        export DEBIAN_FRONTEND=noninteractive
+        apt update
+        apt upgrade -y
+        apt install -y python3-pil python3-numpy python3-pip
+        python3 -m pip config set global.break-system-packages true
+        pip3 install selenium==4.24.0
+    ;;
+    fedora|amzn)
+        dnf -y update
+        dnf -y upgrade
+        dnf -y install  python3-pip python3-pillow python3-numpy tar xz
+        python3 -m pip config set global.break-system-packages true
+        pip3 install selenium==4.24.0
+    ;;
+    rocky)
+        dnf -y update
+        dnf -y upgrade
+        dnf -y install python3-pip tar xz
+        python3 -m pip config set global.break-system-packages true
+        pip3 install selenium==4.24.0 pillow numpy
+    ;;
+    arch)
+        pacman --noconfirm -Syu
+        pacman --noconfirm -Syu python-pip python-pillow python-numpy
+        python3 -m pip config set global.break-system-packages true
+        pip3 install selenium==4.24.0
+    ;;
+    mageia)
+        urpmi --auto --auto-update
+        urpmi --auto python3-pip tar xz python3-pillow python3-numpy
+        python3 -m pip config set global.break-system-packages true
+        pip3 install selenium==4.24.0
+    ;;
+    nixos)
+        nix-env -iA nixpkgs.python3Packages.numpy nixpkgs.python3Packages.pillow nixpkgs.python3Packages.pip nixpkgs.xz
+        pip3 install --break-system-packages selenium==4.24.0
+    ;;
+    gentoo)
+        emaint --auto sync
+        emerge dev-python/pip
+        python3 -m pip config set global.break-system-packages true
+        pip3 install selenium==4.24.0 pillow numpy
+    ;;
+    *)
+        echo "Unknown distro: ${CURRENT_DISTRO}"
+        exit 1
+    ;;
+esac
+
+# prepare for testing
+mkdir /testbundle
+tar xfav /testbundle.tar.xz -C /testbundle
+echo "Starting tests on distro: ${CURRENT_DISTRO}"
+[ -f /etc/os-release ] && cat /etc/os-release
+# run the tests
+if [ "${CURRENT_DISTRO}" = "nixos" ]; then
+    nix-shell -p python3 python3Packages.pip python3Packages.pillow python3Packages.numpy --run "/testdata/test_webdriver_bundle.py --platform=$1 /testbundle"
+else
+    /testdata/test_webdriver_bundle.py "--platform=$1" /testbundle
+fi
---
+++ Tools/Scripts/webkitpy/binary_bundling/tests/webdriver/run-webdriver-tests-bundle-docker.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+function error_help() {
+    echo "ERROR: ${@}"
+    echo "Use: ${0} --platform {gtk,wpe} /path/to/MiniBrowser-bundle.tar.xz" 1>&2
+    exit 1
+}
+
+[[ "${#}" -ge 2 ]] || error_help "Not enough parameters"
+if [[ "${1}" == "--platform=gtk" ]] || [[ "${1}" == "--platform=wpe" ]]; then
+    PORT="$(echo ${1}|cut -d= -f2)"
+elif [[ "${1}" == "--platform" ]]; then
+    if [[ "${2}" == "gtk" ]] || [[ "${2}" == "wpe" ]]; then
+        PORT="${2}"
+        shift 1
+    else
+        error_help "Unknown platform: ${2}"
+    fi
+else
+    error_help "Unexpected parameter: ${1}"
+fi
+
+[[ -f "${2}" ]] || error_help "Bundle file does not exist: ${2}"
+echo "${2}" | grep -qE "\.tar\.xz$" || error_help "Bundle file does not end in .tar.xz: ${2}"
+FBUNDLE="$(readlink -f ${2})"
+
+MYDIR="$(dirname $(readlink -f ${0}))"
+
+TESTINITSCRIPT="install_deps_docker_start_test.sh"
+
+set -eux
+
+DISTROS_TO_TEST=(
+    alpine:latest
+    amazonlinux:latest
+    archlinux:latest
+    debian:11
+    debian:12
+    debian:testing
+    fedora:latest
+    gentoo/stage3:latest
+    mageia:latest
+    nixos/nix:latest
+    rockylinux:9
+    ubuntu:20.04
+    ubuntu:22.04
+    ubuntu:24.04
+    ubuntu:latest
+)
+
+test -x "${MYDIR}/${TESTINITSCRIPT}"
+HOSTNAME="$(hostname)"
+
+I=0
+for DISTRO in ${DISTROS_TO_TEST[@]}; do
+    echo "[INFO] Start testing on distro: ${DISTRO}"
+    docker pull "${DISTRO}"
+    docker run -it --rm \
+                -v "${MYDIR}:/testdata:ro" \
+                -v "${FBUNDLE}:/testbundle.tar.xz:ro" \
+                -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+                -v "${XAUTHORITY}:${XAUTHORITY}" \
+                -e "XAUTHORITY=${XAUTHORITY}" \
+                -e "DISPLAY=${DISPLAY}" \
+                -h "${HOSTNAME}" \
+                "${DISTRO}" \
+                "/testdata/${TESTINITSCRIPT}" "${PORT}"
+    echo "[PASS $((++I))/${#DISTROS_TO_TEST[@]}] Finish testing on distro: ${DISTRO}"
+done
+
+echo "[PASS][ALL OK] Bundle passed tests on all this ${#DISTROS_TO_TEST[@]} distros: $(echo ${DISTROS_TO_TEST[@]}|sed 's/ /, /g')"
+exit 0
