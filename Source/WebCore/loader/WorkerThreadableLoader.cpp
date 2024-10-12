5108dfdca1e5cc3775152b75af4b27883b7ceb20
Alexsander Damaceno <aemgbo@gmail.com>
2024-10-11 23:45:27 -0300

    Deploy smart pointers in WorkerThreadableLoader https://bugs.webkit.org/show_bug.cgi?id=281359
    Reviewed by NOBODY (OOPS!).
    
    Deploy smart pointers in WorkerThreadableLoader
    
    * Source/WebCore/loader/WorkerThreadableLoader.cpp:
    (WebCore::WorkerThreadableLoader::bridge):
    (WebCore::WorkerThreadableLoader::cancel):
    (WebCore::WorkerThreadableLoader::computeIsDone):
    (WebCore::m_contextIdentifier):
    (WebCore::WorkerThreadableLoader::MainThreadBridge::destroy):
    (WebCore::WorkerThreadableLoader::MainThreadBridge::cancel):
    (WebCore::WorkerThreadableLoader::MainThreadBridge::computeIsDone):
    * Source/WebCore/loader/WorkerThreadableLoader.h:
    (WebCore::WorkerThreadableLoader::WorkerThreadableLoader):
    (WebCore::WorkerThreadableLoader::~WorkerThreadableLoader): Deleted.

--- Source/WebCore/loader/WorkerThreadableLoader.cpp
+++ Source/WebCore/loader/WorkerThreadableLoader.cpp
@@ -58,13 +58,15 @@ namespace WebCore {
 
 WorkerThreadableLoader::WorkerThreadableLoader(WorkerOrWorkletGlobalScope& workerOrWorkletGlobalScope, ThreadableLoaderClient& client, const String& taskMode, ResourceRequest&& request, const ThreadableLoaderOptions& options, const String& referrer)
     : m_workerClientWrapper(ThreadableLoaderClientWrapper::create(client, options.initiatorType))
-    , m_bridge(*new MainThreadBridge(m_workerClientWrapper.get(), workerOrWorkletGlobalScope.workerOrWorkletThread()->workerLoaderProxy(), workerOrWorkletGlobalScope.identifier(), taskMode, WTFMove(request), options, referrer.isEmpty() ? workerOrWorkletGlobalScope.url().strippedForUseAsReferrer().string : referrer, workerOrWorkletGlobalScope))
+    , m_bridge(adoptRef(*new MainThreadBridge(m_workerClientWrapper.get(), workerOrWorkletGlobalScope.workerOrWorkletThread()->workerLoaderProxy(), workerOrWorkletGlobalScope.identifier(), taskMode, WTFMove(request), options, referrer.isEmpty() ? workerOrWorkletGlobalScope.url().strippedForUseAsReferrer().string : referrer, workerOrWorkletGlobalScope)))
 {
 }
 
-WorkerThreadableLoader::~WorkerThreadableLoader()
+WorkerThreadableLoader::~WorkerThreadableLoader() = default;
+
+WorkerThreadableLoader::MainThreadBridge& WorkerThreadableLoader::bridge()
 {
-    m_bridge.destroy();
+    return m_bridge;
 }
 
 void WorkerThreadableLoader::loadResourceSynchronously(WorkerOrWorkletGlobalScope& workerOrWorkletGlobalScope, ResourceRequest&& request, ThreadableLoaderClient& client, const ThreadableLoaderOptions& options)
@@ -85,12 +87,12 @@ void WorkerThreadableLoader::loadResourceSynchronously(WorkerOrWorkletGlobalScop
 
 void WorkerThreadableLoader::cancel()
 {
-    m_bridge.cancel();
+    bridge().cancel();
 }
 
 void WorkerThreadableLoader::computeIsDone()
 {
-    m_bridge.computeIsDone();
+    bridge().computeIsDone();
 }
 
 struct LoaderTaskOptions {
