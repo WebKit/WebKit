48e051485568bf5a206d50a450a002f9a640bd96
Mark Lam <mark.lam@apple.com>
2024-10-14 09:46:27 -0700

    Change the StringBuilder in stringReplaceAllStringString to RecordOverflow. https://bugs.webkit.org/show_bug.cgi?id=281412 rdar://137230737
    Reviewed by Yijia Huang.
    
    The function was already checking for resultBuilder.hasOverflowed(), but without this
    RecordOverflow handler being explicitly specified, the StringBuilder will crash eagerly
    on detected overflow.
    
    * JSTests/stress/string-replaceAll-string-string-overflow.js: Added.
    (catch):
    * Source/JavaScriptCore/runtime/StringPrototypeInlines.h:
    (JSC::stringReplaceAllStringString):
    
    Canonical link: https://commits.webkit.org/285126@main

--- Source/JavaScriptCore/runtime/StringPrototypeInlines.h
+++ Source/JavaScriptCore/runtime/StringPrototypeInlines.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Apple, Inc. All rights reserved.
+ * Copyright (C) 2019-2024 Apple, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -271,7 +271,7 @@ ALWAYS_INLINE JSString* stringReplaceAllStringString(JSGlobalObject* globalObjec
 
     auto resultLength = CheckedSize { string.length() + matchStarts.size() * (replacement.length() - searchLength) };
 
-    StringBuilder resultBuilder;
+    StringBuilder resultBuilder(StringBuilder::OverflowHandler::RecordOverflow);
     resultBuilder.reserveCapacity(resultLength);
 
     size_t lastMatchEnd = 0;
