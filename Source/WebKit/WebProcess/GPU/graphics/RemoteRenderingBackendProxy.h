06d849415c31fc9b3d9b32eded8b601692390e4a
Rupin Mittal <rupin@apple.com>
2024-10-16 11:42:16 -0700

    [IsDeprecatedWeakRefSmartPointerException] Remove exception from RemoteRenderingBackendProxy https://bugs.webkit.org/show_bug.cgi?id=281558 rdar://138016364
    Reviewed by Chris Dumez and Geoffrey Garen.
    
    Since RemoteRenderingBackendProxy does not inherit from `CanMakeWeakPtr`,
    it does not need IsDeprecatedWeakRefSmartPointerException.
    
    * Source/WebKit/WebProcess/GPU/graphics/RemoteRenderingBackendProxy.h:
    
    Canonical link: https://commits.webkit.org/285283@main

--- Source/WebKit/WebProcess/GPU/graphics/RemoteRenderingBackendProxy.h
+++ Source/WebKit/WebProcess/GPU/graphics/RemoteRenderingBackendProxy.h
@@ -53,15 +53,6 @@
 #include <wtf/WeakPtr.h>
 #include <wtf/WorkQueue.h>
 
-namespace WebKit {
-class TimerAlignment;
-}
-
-namespace WTF {
-template<typename T> struct IsDeprecatedWeakRefSmartPointerException;
-template<> struct IsDeprecatedWeakRefSmartPointerException<WebKit::RemoteRenderingBackendProxy> : std::true_type { };
-}
-
 namespace WebCore {
 
 class DestinationColorSpace;
