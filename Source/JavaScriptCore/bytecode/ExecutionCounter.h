35ad07fd5f68a3eaaed90f20177747fe6de6fc79
Dan Hecht <dan.hecht@apple.com>
2024-10-18 08:06:36 -0700

    [JSC] Remove some dead code from ExecutionCounter https://bugs.webkit.org/show_bug.cgi?id=281688 rdar://138139166
    Reviewed by Yijia Huang and Keith Miller.
    
    Cleanup some dead code to make the code a tiny bit easier to read.
    
    * Source/JavaScriptCore/bytecode/ExecutionCounter.h:
    (JSC::ExecutionCounter::hasCrossedThreshold const): Deleted.
    (JSC::ExecutionCounter::setNewThresholdForOSRExit): Deleted.
    
    Canonical link: https://commits.webkit.org/285405@main

--- Source/JavaScriptCore/bytecode/ExecutionCounter.h
+++ Source/JavaScriptCore/bytecode/ExecutionCounter.h
@@ -61,18 +61,10 @@ public:
     ExecutionCounter();
     void forceSlowPathConcurrently(); // If you use this, checkIfThresholdCrossedAndSet() may still return false.
     bool checkIfThresholdCrossedAndSet(CodeBlock*);
-    bool hasCrossedThreshold() const { return m_counter >= 0; }
     void setNewThreshold(int32_t threshold, CodeBlock* = nullptr);
     void deferIndefinitely();
     double count() const { return static_cast<double>(m_totalCount) + m_counter; }
     void dump(PrintStream&) const;
-    
-    void setNewThresholdForOSRExit(uint32_t activeThreshold, double memoryUsageAdjustedThreshold)
-    {
-        m_activeThreshold = activeThreshold;
-        m_counter = static_cast<int32_t>(-memoryUsageAdjustedThreshold);
-        m_totalCount = memoryUsageAdjustedThreshold;
-    }
 
     template<typename T>
     static T clippedThreshold(CodeBlock* codeBlock, T threshold)
