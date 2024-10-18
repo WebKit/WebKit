4ef4201d4ec0b0a13fa5f26b153e6937b6a8c91b
Wenson Hsieh <wenson_hsieh@apple.com>
2024-10-18 12:31:01 -0700

    Unreviewed, add a missing header include (due to unified sources)
    * Source/WebKit/UIProcess/Cocoa/UserMediaPermissionRequestManagerProxy.mm:
    (WebKit::UserMediaPermissionRequestManagerProxy::startMonitoringCaptureDeviceRotation):
    
    Import `WKWebView.h`, so that the call to `-layer` isn't ambiguous.
    
    Canonical link: https://commits.webkit.org/285424@main

--- Source/WebKit/UIProcess/Cocoa/UserMediaPermissionRequestManagerProxy.mm
+++ Source/WebKit/UIProcess/Cocoa/UserMediaPermissionRequestManagerProxy.mm
@@ -29,6 +29,7 @@
 #import "MediaPermissionUtilities.h"
 #import "SandboxUtilities.h"
 #import "UserMediaCaptureManagerProxy.h"
+#import "WKWebView.h"
 #import "WebPageProxy.h"
 #import "WebPreferences.h"
 #import <WebCore/VideoFrame.h>
@@ -197,7 +198,7 @@ void UserMediaPermissionRequestManagerProxy::startMonitoringCaptureDeviceRotatio
     if (!page)
         return;
 
-    auto webView = page->cocoaView();
+    RetainPtr webView = page->cocoaView();
     auto *layer = [webView layer];
     if (!layer) {
         RELEASE_LOG_ERROR(WebRTC, "UserMediaPermissionRequestManagerProxy unable to start monitoring capture device rotation");
