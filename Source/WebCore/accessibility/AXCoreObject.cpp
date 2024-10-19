0d6c0e3075ab65448c3731b132c99f47da4faa4a
Darin Adler <darin@apple.com>
2024-10-18 17:37:16 -0700

    Reduce allocation and deallocation in AXCoreObject::value https://bugs.webkit.org/show_bug.cgi?id=281777 rdar://138208818
    Reviewed by Darin Adler and Tyler Wilcock.
    
    This should allocate a lot less string objects that then get
    deallocated.
    
    * Source/WebCore/accessibility/AXCoreObject.cpp:
    (WebCore::AXCoreObject::value):
    
    Canonical link: https://commits.webkit.org/285447@main

--- Source/WebCore/accessibility/AXCoreObject.cpp
+++ Source/WebCore/accessibility/AXCoreObject.cpp
@@ -298,7 +298,12 @@ AXCoreObject::AXValue AXCoreObject::value()
 
     if (isColorWell()) {
         auto color = convertColor<SRGBA<float>>(colorValue()).resolved();
-        return makeString("rgb "_s, String::numberToStringFixedPrecision(color.red, 6, TrailingZerosPolicy::Keep), ' ', String::numberToStringFixedPrecision(color.green, 6, TrailingZerosPolicy::Keep), ' ', String::numberToStringFixedPrecision(color.blue, 6, TrailingZerosPolicy::Keep), ' ', color.alpha == 1.0f ? "1"_s : String::numberToStringFixedPrecision(color.alpha, 6, TrailingZerosPolicy::Keep));
+        auto channel = [](float number) {
+            return FormattedNumber::fixedPrecision(number, 6, TrailingZerosPolicy::Keep);
+        };
+        return color.alpha == 1
+            ? makeString("rgb "_s, channel(color.red), ' ', channel(color.green), ' ', channel(color.blue), " 1"_s)
+            : makeString("rgb "_s, channel(color.red), ' ', channel(color.green), ' ', channel(color.blue), ' ', channel(color.alpha));
     }
 
     return stringValue();
