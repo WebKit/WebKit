90197d85aeb25925f035661de711ebc933e2db6c
Sosuke Suzuki <aosukeke@gmail.com>
2024-10-26 17:20:06 -0700

    [JSC] Enable Iterator Helpers https://bugs.webkit.org/show_bug.cgi?id=281636
    Reviewed by Alexey Shvayka.
    
    Iterator Helpers Proposal[1] reached stage 4 in Oct 2024[2].
    
    This patch enables it by default.
    
    [1]: https://github.com/tc39/proposal-iterator-helpers
    [2]: https://github.com/tc39/agendas/blob/main/2024/10.md
    
    * Source/JavaScriptCore/runtime/OptionsList.h:
    
    Canonical link: https://commits.webkit.org/285734@main

--- Source/JavaScriptCore/runtime/OptionsList.h
+++ Source/JavaScriptCore/runtime/OptionsList.h
@@ -587,7 +587,7 @@ bool hasCapacityToUseLargeGigacage();
     v(Bool, useErrorIsError, false, Normal, "Expose Error.isError feature."_s) \
     v(Bool, useFloat16Array, true, Normal, "Expose Float16Array."_s) \
     v(Bool, useIteratorChunking, false, Normal, "Expose the Iterator.prototype.chunks and Iterator.prototype.windows methods."_s) \
-    v(Bool, useIteratorHelpers, false, Normal, "Expose the Iterator Helpers."_s) \
+    v(Bool, useIteratorHelpers, true, Normal, "Expose the Iterator Helpers."_s) \
     v(Bool, useIteratorSequencing, false, Normal, "Expose the Iterator.concat method."_s) \
     v(Bool, useMathSumPreciseMethod, false, Normal, "Expose the Math.sumPrecise() method."_s) \
     v(Bool, usePromiseTryMethod, true, Normal, "Expose the Promise.try() method."_s) \
