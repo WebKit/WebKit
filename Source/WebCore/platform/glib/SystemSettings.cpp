WebKit/WebKit

commit 8b7209ba250c22f2dfb21d681ca8d706ea3a6c43

[GTK] Make SystemSettingsProxy a web process supplement and rename it as SystemSettingsManager https://bugs.webkit.org/show_bug.cgi?id=281567
Reviewed by Adrian Perez de Castro.

SystemSettingsProxy doesn't need to be a singleton, it can be a web
process supplement. The proxy is actually in the UI process, sending the
settings to all web processes, so this patch renames SystemSettingsProxy
as SystemSettingsManager and SystemSettingsManager as
SystemSettingsManagerProxy. The web process supplement now connects the
observer in the constructor and initializes the settings from web
process creation parameters later, fixing the initial settings not being
applied before.

* Source/WebCore/platform/glib/SystemSettings.cpp:
(WebCore::SystemSettings::updateSettings):
* Source/WebCore/platform/glib/SystemSettings.h:
* Source/WebKit/PlatformGTK.cmake:
* Source/WebKit/PlatformWPE.cmake:
* Source/WebKit/SourcesGTK.txt:
* Source/WebKit/SourcesWPE.txt:
* Source/WebKit/UIProcess/glib/SystemSettingsManagerProxy.cpp: Renamed from Source/WebKit/UIProcess/glib/SystemSettingsManager.cpp.
(WebKit::SystemSettingsManagerProxy::themeName const):
(WebKit::SystemSettingsManagerProxy::darkMode const):
(WebKit::SystemSettingsManagerProxy::fontName const):
(WebKit::SystemSettingsManagerProxy::xftAntialias const):
(WebKit::SystemSettingsManagerProxy::xftHinting const):
(WebKit::SystemSettingsManagerProxy::xftHintStyle const):
(WebKit::SystemSettingsManagerProxy::xftRGBA const):
(WebKit::SystemSettingsManagerProxy::xftDPI const):
(WebKit::SystemSettingsManagerProxy::cursorBlink const):
(WebKit::SystemSettingsManagerProxy::cursorBlinkTime const):
(WebKit::SystemSettingsManagerProxy::primaryButtonWarpsSlider const):
(WebKit::SystemSettingsManagerProxy::overlayScrolling const):
(WebKit::SystemSettingsManagerProxy::enableAnimations const):
(WebKit::SystemSettingsManagerProxy::initialize):
(WebKit::SystemSettingsManagerProxy::settingsDidChange):

Source/WebCore/platform/glib/SystemSettings.cpp
@@ -38,7 +38,7 @@ SystemSettings& SystemSettings::singleton()
 
 SystemSettings::SystemSettings() = default;
 
-void SystemSettings::updateSettings(SystemSettings::State&& state)
+void SystemSettings::updateSettings(const SystemSettings::State& state)
 {
     if (state.themeName)
         m_state.themeName = state.themeName;

Source/WebCore/platform/glib/SystemSettings.h
@@ -59,7 +59,7 @@ public:
 
     using State = SystemSettingsState;
 
-    void updateSettings(SystemSettings::State&&);
+    void updateSettings(const SystemSettings::State&);
 
     const State& settingsState() const { return m_state; }
 

Source/WebKit/PlatformGTK.cmake
@@ -64,7 +64,7 @@ list(APPEND WebKit_MESSAGES_IN_FILES
 
     WebProcess/WebPage/dmabuf/AcceleratedSurfaceDMABuf
 
-    WebProcess/glib/SystemSettingsProxy
+    WebProcess/glib/SystemSettingsManager
 )
 
 if (USE_GBM)

Source/WebKit/PlatformWPE.cmake
@@ -487,7 +487,7 @@ else ()
 endif ()
 
 list(APPEND WebKit_MESSAGES_IN_FILES
-    WebProcess/glib/SystemSettingsProxy
+    WebProcess/glib/SystemSettingsManager
 )
 
 if (ENABLE_WPE_PLATFORM)

Source/WebKit/UIProcess/glib/SystemSettingsManagerProxy.cpp
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2024 Igalia S.L.
  * Copyright (C) 2021 Purism SPC
  *
  * Redistribution and use in source and binary forms, with or without
@@ -24,91 +25,92 @@
  */
 
 #include "config.h"
-#include "SystemSettingsManager.h"
+#include "SystemSettingsManagerProxy.h"
 
-#include "SystemSettingsProxyMessages.h"
+#include "SystemSettingsManagerMessages.h"
 #include "WebProcessPool.h"
+#include <WebCore/SystemSettings.h>
 
 namespace WebKit {
 using namespace WebCore;
 
 #if !PLATFORM(GTK)
 
-SystemSettingsManager::SystemSettingsManager() = default;
+SystemSettingsManagerProxy::SystemSettingsManagerProxy() = default;
 
-String SystemSettingsManager::themeName() const
+String SystemSettingsManagerProxy::themeName() const
 {
     return emptyString();
 }
 
-bool SystemSettingsManager::darkMode() const
+bool SystemSettingsManagerProxy::darkMode() const
 {
     return false;
 }
 
-String SystemSettingsManager::fontName() const
+String SystemSettingsManagerProxy::fontName() const
 {
     return "Sans 10"_s;
 }
 
-int SystemSettingsManager::xftAntialias() const
+int SystemSettingsManagerProxy::xftAntialias() const
 {
     return -1;
 }
 
-int SystemSettingsManager::xftHinting() const
+int SystemSettingsManagerProxy::xftHinting() const
 {
     return -1;
 }
 
-String SystemSettingsManager::xftHintStyle() const
+String SystemSettingsManagerProxy::xftHintStyle() const
 {
     return emptyString();
 }
 
-String SystemSettingsManager::xftRGBA() const
+String SystemSettingsManagerProxy::xftRGBA() const
 {
     return "rgb"_s;
 }
 
-int SystemSettingsManager::xftDPI() const
+int SystemSettingsManagerProxy::xftDPI() const
 {
     return -1;
 }
 
-bool SystemSettingsManager::cursorBlink() const
+bool SystemSettingsManagerProxy::cursorBlink() const
 {
     return true;
 }
 
-int SystemSettingsManager::cursorBlinkTime() const
+int SystemSettingsManagerProxy::cursorBlinkTime() const
 {
     return 1200;
 }
 
-bool SystemSettingsManager::primaryButtonWarpsSlider() const
+bool SystemSettingsManagerProxy::primaryButtonWarpsSlider() const
 {
     return true;
 }
 
-bool SystemSettingsManager::overlayScrolling() const
+bool SystemSettingsManagerProxy::overlayScrolling() const
 {
     return true;
 }
 
-bool SystemSettingsManager::enableAnimations() const
+bool SystemSettingsManagerProxy::enableAnimations() const
 {
     return true;
 }
 
 #endif // !PLATFORM(GTK)
 
-void SystemSettingsManager::initialize()
+void SystemSettingsManagerProxy::initialize()
 {
-    static NeverDestroyed<SystemSettingsManager> manager;
+    static NeverDestroyed<SystemSettingsManagerProxy> manager;
 }
 
-void SystemSettingsManager::settingsDidChange()
+void SystemSettingsManagerProxy::settingsDidChange()
 {
     auto& oldState = SystemSettings::singleton().settingsState();
     SystemSettings::State changedState;
@@ -166,9 +168,9 @@ void SystemSettingsManager::settingsDidChange()
         changedState.enableAnimations = enableAnimations;
 
     for (auto& processPool : WebProcessPool::allProcessPools())
-        processPool->sendToAllProcesses(Messages::SystemSettingsProxy::DidChange(changedState));
+        processPool->sendToAllProcesses(Messages::SystemSettingsManager::DidChange(changedState));
 
-    SystemSettings::singleton().updateSettings(WTFMove(changedState));
+    SystemSettings::singleton().updateSettings(changedState);
 }
 
 } // namespace WebKit

Source/WebKit/UIProcess/glib/SystemSettingsManagerProxy.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2021 Purism SPC
  * Copyright (C) 2024 Igalia S.L.
+ * Copyright (C) 2021 Purism SPC
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -26,20 +26,28 @@
 
 #pragma once
 
-#include <WebCore/SystemSettings.h>
 #include <wtf/NeverDestroyed.h>
+#include <wtf/Noncopyable.h>
+#include <wtf/text/WTFString.h>
+
+#if PLATFORM(GTK)
+typedef struct _GtkSettings GtkSettings;
+using PlatformSettings = GtkSettings;
+#else
+using PlatformSettings = void;
+#endif
 
 namespace WebKit {
 
-class SystemSettingsManager {
-    WTF_MAKE_NONCOPYABLE(SystemSettingsManager);
-    friend NeverDestroyed<SystemSettingsManager>;
+class SystemSettingsManagerProxy {
+    WTF_MAKE_NONCOPYABLE(SystemSettingsManagerProxy);
+    friend NeverDestroyed<SystemSettingsManagerProxy>;
 
 public:
     static void initialize();
 
 private:
-    SystemSettingsManager();
+    SystemSettingsManagerProxy();
 
     void settingsDidChange();
 
@@ -57,7 +65,7 @@ private:
     bool overlayScrolling() const;
     bool enableAnimations() const;
 
-    void* m_settings = nullptr;
+    PlatformSettings* m_settings { nullptr };
 };
 
 } // namespace WebKit

Source/WebKit/UIProcess/gtk/Display.cpp
@@ -26,7 +26,7 @@
 #include "config.h"
 #include "Display.h"
 
-#include "SystemSettingsManager.h"
+#include "SystemSettingsManagerProxy.h"
 #include <WebCore/GLContext.h>
 #include <WebCore/GLDisplay.h>
 #include <WebCore/GtkVersioning.h>
@@ -55,7 +55,7 @@ Display::Display()
         return;
 
     // As soon as gtk is initialized we listen to GtkSettings.
-    SystemSettingsManager::initialize();
+    SystemSettingsManagerProxy::initialize();
 
     m_gdkDisplay = gdk_display_manager_get_default_display(gdk_display_manager_get());
     if (!m_gdkDisplay)

Source/WebKit/UIProcess/gtk/SystemSettingsManagerProxyGtk.cpp
@@ -24,15 +24,14 @@
  */
 
 #include "config.h"
-#include "SystemSettingsManager.h"
+#include "SystemSettingsManagerProxy.h"
 
 #include <gtk/gtk.h>
 #include <wtf/glib/GUniquePtr.h>
 
 namespace WebKit {
-using namespace WebCore;
 
-String SystemSettingsManager::themeName() const
+String SystemSettingsManagerProxy::themeName() const
 {
     if (auto* themeNameEnv = g_getenv("GTK_THEME")) {
         String name = String::fromUTF8(themeNameEnv);
@@ -49,7 +48,7 @@ String SystemSettingsManager::themeName() const
     return name;
 }
 
-bool SystemSettingsManager::darkMode() const
+bool SystemSettingsManagerProxy::darkMode() const
 {
     gboolean preferDarkTheme;
     g_object_get(m_settings, "gtk-application-prefer-dark-theme", &preferDarkTheme, nullptr);
@@ -69,87 +68,87 @@ bool SystemSettingsManager::darkMode() const
     return false;
 }
 
-String SystemSettingsManager::fontName() const
+String SystemSettingsManagerProxy::fontName() const
 {
     GUniqueOutPtr<char> fontNameSetting;
     g_object_get(m_settings, "gtk-font-name", &fontNameSetting.outPtr(), nullptr);
     return String::fromUTF8(fontNameSetting.get());
 }
 
-int SystemSettingsManager::xftAntialias() const
+int SystemSettingsManagerProxy::xftAntialias() const
 {
     int antialiasSetting;
     g_object_get(m_settings, "gtk-xft-antialias", &antialiasSetting, nullptr);
     return antialiasSetting;
 }
 
-int SystemSettingsManager::xftHinting() const
+int SystemSettingsManagerProxy::xftHinting() const
 {
     int hintingSetting;
     g_object_get(m_settings, "gtk-xft-hinting", &hintingSetting, nullptr);
     return hintingSetting;
 }
 
-String SystemSettingsManager::xftHintStyle() const
+String SystemSettingsManagerProxy::xftHintStyle() const
 {
     GUniqueOutPtr<char> hintStyleSetting;
     g_object_get(m_settings, "gtk-xft-hintstyle", &hintStyleSetting.outPtr(), nullptr);
     return String::fromUTF8(hintStyleSetting.get());
 }
 
-String SystemSettingsManager::xftRGBA() const
+String SystemSettingsManagerProxy::xftRGBA() const
 {
     GUniqueOutPtr<char> rgbaSetting;
     g_object_get(m_settings, "gtk-xft-rgba", &rgbaSetting.outPtr(), nullptr);
     return String::fromUTF8(rgbaSetting.get());
 }
 
-int SystemSettingsManager::xftDPI() const
+int SystemSettingsManagerProxy::xftDPI() const
 {
     int dpiSetting;
     g_object_get(m_settings, "gtk-xft-dpi", &dpiSetting, nullptr);
     return dpiSetting;
 }
 
-bool SystemSettingsManager::cursorBlink() const
+bool SystemSettingsManagerProxy::cursorBlink() const
 {
     gboolean cursorBlinkSetting;
     g_object_get(m_settings, "gtk-cursor-blink", &cursorBlinkSetting, nullptr);
     return cursorBlinkSetting ? true : false;
 }
 
-int SystemSettingsManager::cursorBlinkTime() const
+int SystemSettingsManagerProxy::cursorBlinkTime() const
 {
     int cursorBlinkTimeSetting;
     g_object_get(m_settings, "gtk-cursor-blink-time", &cursorBlinkTimeSetting, nullptr);
     return cursorBlinkTimeSetting;
 }
 
-bool SystemSettingsManager::primaryButtonWarpsSlider() const
+bool SystemSettingsManagerProxy::primaryButtonWarpsSlider() const
 {
     gboolean buttonSetting;
     g_object_get(m_settings, "gtk-primary-button-warps-slider", &buttonSetting, nullptr);
     return buttonSetting ? true : false;
 }
 
-bool SystemSettingsManager::overlayScrolling() const
+bool SystemSettingsManagerProxy::overlayScrolling() const
 {
     gboolean overlayScrollingSetting;
     g_object_get(m_settings, "gtk-overlay-scrolling", &overlayScrollingSetting, nullptr);
     return overlayScrollingSetting ? true : false;
 }
 
-bool SystemSettingsManager::enableAnimations() const
+bool SystemSettingsManagerProxy::enableAnimations() const
 {
     gboolean enableAnimationsSetting;
     g_object_get(m_settings, "gtk-enable-animations", &enableAnimationsSetting, nullptr);
     return enableAnimationsSetting ? true : false;
 }
 
-SystemSettingsManager::SystemSettingsManager()
+SystemSettingsManagerProxy::SystemSettingsManagerProxy()
     : m_settings(gtk_settings_get_default())
 {
-    auto settingsChangedCallback = +[](SystemSettingsManager* settingsManager) {
+    auto settingsChangedCallback = +[](SystemSettingsManagerProxy* settingsManager) {
         settingsManager->settingsDidChange();
     };
 

commit 1f3ae07b8e51f7746581f12af28f9728cdaefe68

REGRESSION(285320@main): Crash in SystemSettings::updateSettings https://bugs.webkit.org/show_bug.cgi?id=282156
Reviewed by Adrian Perez de Castro.

The problem is that SystemSettings::updateSettings() doesn't allow
changes in the observers HashMap while it's being iterated. This
regressed in 285320@main because now we are properly applying the initial
settings on web process initialization, and RenderTheme singleton is
created from the web process settings observer, which adds its own
observer. We need to use copyToVector when iterating the observers, but
the values are non copyable, so we copy the keys instead and get the
value to call the observer function.

* Source/WebCore/platform/glib/SystemSettings.cpp:
(WebCore::SystemSettings::updateSettings):

Canonical link: https://commits.webkit.org/285761@main

Source/WebCore/platform/glib/SystemSettings.cpp
@@ -82,8 +82,12 @@ void SystemSettings::updateSettings(const SystemSettings::State& state)
     if (state.enableAnimations)
         m_state.enableAnimations = state.enableAnimations;
 
-    for (const auto& observer : m_observers.values())
-        observer(state);
+    for (auto* context : copyToVector(m_observers.keys())) {
+        const auto it = m_observers.find(context);
+        if (it == m_observers.end())
+            continue;
+        it->value(state);
+    }
 }
 
 std::optional<FontRenderOptions::Hinting> SystemSettings::hintStyle() const