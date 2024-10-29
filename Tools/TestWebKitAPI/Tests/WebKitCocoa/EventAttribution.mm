2d255415cfa5f6217ee45b2663e4b20f8415337c
Sihui Liu <sihui_liu@apple.com>
2024-10-28 19:27:55 -0700

    [ macOS ] TestWebKitAPI.PrivateClickMeasurement.EphemeralWithAttributedBundleIdentifier is a flaky timeout https://bugs.webkit.org/show_bug.cgi?id=280348 rdar://136703173
    Reviewed by Alex Christensen.
    
    WKWebsiteDataStore creates and resolves its directories on a background thread, and the test tries to clear data by
    manually deleting directory on the main thread, which leads to unexpected behavior. To fix this, using
    WKWebsiteDataStore's API to clear data instead.
    
    * Tools/TestWebKitAPI/Tests/WebKitCocoa/EventAttribution.mm:
    (TestWebKitAPI::runBasicPCMTest):
    (TestWebKitAPI::TEST(PrivateClickMeasurement, EphemeralWithAttributedBundleIdentifier)):
    (TestWebKitAPI::clearState): Deleted.
    
    Canonical link: https://commits.webkit.org/285808@main

--- Tools/TestWebKitAPI/Tests/WebKitCocoa/EventAttribution.mm
+++ Tools/TestWebKitAPI/Tests/WebKitCocoa/EventAttribution.mm
@@ -121,11 +121,6 @@ static NSURL *exampleURL()
     return [NSURL URLWithString:@"https://example.com/"];
 }
 
-static void clearState()
-{
-    [[NSFileManager defaultManager] removeItemAtURL:adoptNS([[_WKWebsiteDataStoreConfiguration alloc] init]).get()._resourceLoadStatisticsDirectory error:nil];
-}
-
 static RetainPtr<WKWebViewConfiguration> configurationWithoutUsingDaemon()
 {
     auto dataStoreConfiguration = adoptNS([_WKWebsiteDataStoreConfiguration new]);
@@ -142,7 +137,6 @@ static RetainPtr<WKWebView> webViewWithoutUsingDaemon()
 
 void runBasicPCMTest(WKWebViewConfiguration *configuration, Function<void(WKWebView *, const HTTPServer&)>&& addAttributionToWebView, bool setTestAppBundleID = true)
 {
-    clearState();
     [WKWebsiteDataStore _setNetworkProcessSuspensionAllowedForTesting:NO];
     bool done = false;
     HTTPServer server([&done, connectionCount = 0] (Connection connection) mutable {
@@ -181,6 +175,12 @@ void runBasicPCMTest(WKWebViewConfiguration *configuration, Function<void(WKWebV
     addAttributionToWebView(webView.get(), server);
     [[webView configuration].websiteDataStore _setResourceLoadStatisticsEnabled:YES];
     [[webView configuration].websiteDataStore _trustServerForLocalPCMTesting:secTrustFromCertificateChain(@[(id)testCertificate().get()]).get()];
+    // Clear existing data.
+    __block bool cleared = false;
+    [[webView configuration].websiteDataStore removeDataOfTypes:[WKWebsiteDataStore _allWebsiteDataTypesIncludingPrivate] modifiedSince:[NSDate distantPast] completionHandler:^() {
+        cleared = true;
+    }];
+    Util::run(&cleared);
     [webView _setPrivateClickMeasurementAttributionReportURLsForTesting:serverURL destinationURL:exampleURL() completionHandler:^{
         [webView _setPrivateClickMeasurementOverrideTimerForTesting:YES completionHandler:^{
             NSString *html = [NSString stringWithFormat:@"<script>fetch('%@conversionRequestBeforeRedirect',{mode:'no-cors'})</script>", serverURL];
@@ -386,12 +386,7 @@ TEST(PrivateClickMeasurement, Basic)
     });
 }
 
-// rdar://136703173
-#if PLATFORM(MAC)
-TEST(PrivateClickMeasurement, DISABLED_EphemeralWithAttributedBundleIdentifier)
-#else
 TEST(PrivateClickMeasurement, EphemeralWithAttributedBundleIdentifier)
-#endif
 {
     auto configuration = configurationWithoutUsingDaemon();
     configuration.get()._attributedBundleIdentifier = @"other.test.bundle.id";
