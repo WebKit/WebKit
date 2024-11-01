831c463d4efcdcb1f6ba0679c63facfe67efc235
Sosuke Suzuki <aosukeke@gmail.com>
2024-10-31 19:33:14 -0700

    [JSC] Refactor `Element` in `IntlDurationFormat` https://bugs.webkit.org/show_bug.cgi?id=281895
    Reviewed by Yusuke Suzuki.
    
    The `Element` struct in `IntlDurationFormat`'s `collectElements` function holds the value of each
    duration unit as the `m_value` field. The `m_value` field is used to pass following two pieces of
    information to `NumberFormat`:
    
    1. The sign bit
    2. The value's type (`infinity`, `NaN`, or `integer`)
    
    The type of `m_value` is always `integer`. Therefore, the `Element` struct only needs to hold the
    sign bit of `m_value` rather than `m_value` itself.
    
    This patch removes the `m_value` field from the `Element` struct and adds an `m_valueSign` field
    instead.
    
    * Source/JavaScriptCore/runtime/IntlDurationFormat.cpp:
    (JSC::collectElements):
    (JSC::IntlDurationFormat::formatToParts const):
    
    Canonical link: https://commits.webkit.org/285992@main

--- Source/JavaScriptCore/runtime/IntlDurationFormat.cpp
+++ Source/JavaScriptCore/runtime/IntlDurationFormat.cpp
@@ -304,9 +304,9 @@ enum class ElementType : uint8_t {
 
 struct Element {
     ElementType m_type;
+    bool m_valueSignBit;
     TemporalUnit m_unit;
     String m_string;
-    double m_value;
     std::unique_ptr<UFormattedNumber, ICUDeleter<unumf_closeResult>> m_formattedNumber;
 };
 
@@ -388,6 +388,8 @@ static String buildDecimalFormat(TemporalUnit unit, Int128 ns)
 static Vector<Element> collectElements(JSGlobalObject* globalObject, const IntlDurationFormat* durationFormat, ISO8601::Duration duration)
 {
     // https://tc39.es/proposal-intl-duration-format/#sec-partitiondurationformatpattern
+
+    ASSERT(isValidDuration(duration));
 
     VM& vm = globalObject->vm();
     auto scope = DECLARE_THROW_SCOPE(vm);
@@ -578,13 +580,13 @@ static Vector<Element> collectElements(JSGlobalObject* globalObject, const IntlD
                     auto formatted = formatToString(formattedNumber.get());
                     RETURN_IF_EXCEPTION(scope, { });
 
-                    elements.append({ ElementType::Element, unit, WTFMove(formatted), value, WTFMove(formattedNumber) });
+                    elements.append({ ElementType::Element, std::signbit(value), unit, WTFMove(formatted), WTFMove(formattedNumber) });
                 }
 
                 if (needsSeparator) {
                     if (separator.isNull())
                         separator = retrieveSeparator(durationFormat->dataLocaleWithExtensions(), durationFormat->numberingSystem());
-                    elements.append({ ElementType::Literal, unit, separator, value, nullptr });
+                    elements.append({ ElementType::Literal, std::signbit(value), unit, separator, nullptr });
                 }
 
                 break;
@@ -610,7 +612,7 @@ static Vector<Element> collectElements(JSGlobalObject* globalObject, const IntlD
                 auto formatted = formatToString(formattedNumber.get());
                 RETURN_IF_EXCEPTION(scope, { });
 
-                elements.append({ ElementType::Element, unit, WTFMove(formatted), value, WTFMove(formattedNumber) });
+                elements.append({ ElementType::Element, std::signbit(value), unit, WTFMove(formatted), WTFMove(formattedNumber) });
                 break;
             }
             }
@@ -772,7 +774,7 @@ JSValue IntlDurationFormat::formatToParts(JSGlobalObject* globalObject, ISO8601:
                     }
                     IntlFieldIterator iterator(*fieldItr.get());
                     JSString* type = jsString(vm, String(temporalUnitSingularPropertyName(vm, element.m_unit).uid()));
-                    IntlNumberFormat::formatToPartsInternal(globalObject, IntlNumberFormat::Style::Unit, std::signbit(element.m_value), IntlMathematicalValue::numberTypeFromDouble(element.m_value), element.m_string, iterator, parts, nullptr, type);
+                    IntlNumberFormat::formatToPartsInternal(globalObject, IntlNumberFormat::Style::Unit, element.m_valueSignBit, IntlMathematicalValue::NumberType::Integer, element.m_string, iterator, parts, nullptr, type);
                     RETURN_IF_EXCEPTION(scope, void());
                     break;
                 }
