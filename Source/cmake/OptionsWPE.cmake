4b4a965bed28c9aa620d4245e643cf00b0624c2e
Adrian Perez de Castro <aperez@igalia.com>
2024-11-04 04:45:00 -0800

    Unreviewed. Update OptionsWPE.cmake and NEWS for the 2.47.1 release
    * Source/WebKit/wpe/NEWS: Add release notes.
    * Source/cmake/OptionsWPE.cmake: Bump version number.
    
    Canonical link: https://commits.webkit.org/286077@main

--- Source/cmake/OptionsWPE.cmake
+++ Source/cmake/OptionsWPE.cmake
@@ -1,7 +1,7 @@
 include(GNUInstallDirs)
 include(VersioningUtils)
 
-SET_PROJECT_VERSION(2 47 0)
+SET_PROJECT_VERSION(2 47 1)
 
 set(USER_AGENT_BRANDING "" CACHE STRING "Branding to add to user agent string")
 
