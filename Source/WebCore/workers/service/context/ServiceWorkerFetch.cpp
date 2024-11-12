8aeae6caf5398bb8369bc280e2acab4b8554c0af
Youenn Fablet <youenn@apple.com>
2024-11-11 23:48:21 -0800

    Service Worker can't correctly redirect relative URLs rdar://139498279 https://bugs.webkit.org/show_bug.cgi?id=282427
    Reviewed by Per Arne Vollan.
    
    Set the URL of the service worker response slightly before so that it is taken into account for redirections.
    One of the WPT test is changing from PASS to FAIL, which aligns with Chrome and Firefox.
    This WPT test should probably be fixed or removed upstream.
    
    * LayoutTests/imported/w3c/web-platform-tests/service-workers/service-worker/navigation-redirect.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/service-workers/service-worker/navigation-redirect.https_client-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/service-workers/service-worker/redirected-response.https-expected.txt:
    * Source/WebCore/workers/service/context/ServiceWorkerFetch.cpp:
    (WebCore::ServiceWorkerFetch::processResponse):
    
    Canonical link: https://commits.webkit.org/286470@main

--- Source/WebCore/workers/service/context/ServiceWorkerFetch.cpp
+++ Source/WebCore/workers/service/context/ServiceWorkerFetch.cpp
@@ -104,6 +104,10 @@ static void processResponse(Ref<Client>&& client, Expected<Ref<FetchResponse>, s
         return;
     }
 
+    // As per https://fetch.spec.whatwg.org/#main-fetch step 9, copy request's url list in response's url list if empty.
+    if (resourceResponse.url().isNull())
+        resourceResponse.setURL(requestURL);
+
     if (resourceResponse.isRedirection() && resourceResponse.httpHeaderFields().contains(HTTPHeaderName::Location)) {
         client->didReceiveRedirection(resourceResponse);
         return;
@@ -120,10 +124,6 @@ static void processResponse(Ref<Client>&& client, Expected<Ref<FetchResponse>, s
         if (!resourceResponse.certificateInfo())
             resourceResponse.setCertificateInfo(WTFMove(certificateInfo));
     }
-
-    // As per https://fetch.spec.whatwg.org/#main-fetch step 9, copy request's url list in response's url list if empty.
-    if (resourceResponse.url().isNull())
-        resourceResponse.setURL(requestURL);
 
     client->didReceiveResponse(resourceResponse);
 
