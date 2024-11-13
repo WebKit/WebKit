WebKit/WebKit

commit e9ced931afc738c9413a26562ad2dd1d7fec4cd2

GC Wasm BBQ/OMG-OSR code https://bugs.webkit.org/show_bug.cgi?id=280896 rdar://131411963
Reviewed by Yusuke Suzuki and David Degazio.

This patch enables GCing BBQ/OMG-OSR code. We don't reclaim OMG code
as it's the highest tier so there's nothing to replace it or LLInt/IPInt
because LLInt is on its way out and it's not clear how profitable it will
be to GC IPInt code.

To make this work a couple significant changes had to be made:
* NativeCallees are now ThreadSafeRefCountedAndCanMakeThreadSafeWeakPtr so CalleeGroup can retain weak references to them.

* When doing updateCallsitesToCallUs we get the callsites from Wasm::JITCallees directly rather than from anywhere else.
  In order to quickly find the potential callees we record a FixedBitVector of the FunctionCodeIndices that could have
  a direct asm callsite to us. This also makes CallsiteCollection obsolete thus is removed in this patch.

* CalleeGroup now holds ThreadSafeWeakOrStrongPtr to BBQCallees and a HashMap of ThreadSafeWeakPtrs to OSREntryCallees.
  When the BBQCallee is the current highest tier the BBQCallee is retained as a strong reference. Once OMG code is compiled
  that reference is made weak. This allows us to continue to update the outgoing calls of this callee while it is still reachable.
  If we didn't do this then we could fail to update the callsite and be left with a dangling "pointer" to other code that's
  already been collected.

* Once a Wasm::Callee is ready to be released we iterate all the VMs in the process and give them a copy of the Callee. If they
  don't see it on a stack scan it can be released as that VM isn't referencing it.

* ConservativeRoots now knows that it needs to also look for boxed Wasm::Callees on the stack. In order to avoid potentially
  regressing performance when not running wasm the stack scan is now templated on whether or not to look for boxed Wasm::Callees
  on the stack.

There are a few other additions in WTF:

Source/JavaScriptCore/CMakeLists.txt
@@ -1273,7 +1273,6 @@ set(JavaScriptCore_PRIVATE_FRAMEWORK_HEADERS
     wasm/WasmCallee.h
     wasm/WasmCalleeGroup.h
     wasm/WasmCallingConvention.h
-    wasm/WasmCallsiteCollection.h
     wasm/WasmCapabilities.h
     wasm/WasmCompilationContext.h
     wasm/WasmCompilationMode.h

commit f3fb9b07e5316590aebe5d7be10ea2d9dbb0ca4f

ifdef out bookkeeping to GC wasm code https://bugs.webkit.org/show_bug.cgi?id=283050 rdar://139208625
Reviewed by Mark Lam.

There still seems to be some unaccounted for memory regression with 284867@main even
after the other fixes. Let's ifdef out the bookkeeping for now.

* Source/JavaScriptCore/JavaScriptCore.xcodeproj/project.pbxproj:
* Source/JavaScriptCore/Sources.txt:
* Source/JavaScriptCore/heap/ConservativeRoots.h:
* Source/JavaScriptCore/runtime/NativeCallee.h:
(JSC::NativeCallee::category const): Deleted.
(JSC::NativeCallee::implementationVisibility const): Deleted.
* Source/JavaScriptCore/wasm/WasmBBQPlan.cpp:
(JSC::Wasm::BBQPlan::work):
* Source/JavaScriptCore/wasm/WasmCallee.cpp:
(JSC::Wasm::Callee::Callee):
* Source/JavaScriptCore/wasm/WasmCallee.h:
* Source/JavaScriptCore/wasm/WasmCalleeGroup.cpp:
(JSC::Wasm::CalleeGroup::CalleeGroup):
(JSC::Wasm::CalleeGroup::tryGetBBQCalleeForLoopOSR):
* Source/JavaScriptCore/wasm/WasmCalleeGroup.h:
* Source/JavaScriptCore/wasm/WasmCallsiteCollection.cpp: Added.
(JSC::Wasm::CallsiteCollection::addCallsites):
(JSC::Wasm::CallsiteCollection::addCalleeGroupCallsites):
(JSC::Wasm::CallsiteCollection::updateCallsitesToCallUs):
* Source/JavaScriptCore/wasm/WasmCallsiteCollection.h: Copied from Source/JavaScriptCore/runtime/NativeCallee.h.
(JSC::Wasm::CallsiteCollection::CallsiteCollection):
(JSC::Wasm::CallsiteCollection::calleeGroupCallsites const):
* Source/JavaScriptCore/wasm/WasmOMGPlan.cpp:
(JSC::Wasm::OMGPlan::work):
* Source/JavaScriptCore/wasm/WasmOSREntryPlan.cpp:
(JSC::Wasm::OSREntryPlan::work):
* Source/JavaScriptCore/wasm/WasmOperations.cpp:
(JSC::Wasm::assertCalleeIsReferenced):
* Source/WTF/wtf/RefPtr.h:
(WTF::RefPtr::ptr const):

Canonical link: https://commits.webkit.org/286543@main

Source/JavaScriptCore/CMakeLists.txt
@@ -1274,6 +1274,7 @@ set(JavaScriptCore_PRIVATE_FRAMEWORK_HEADERS
     wasm/WasmCallee.h
     wasm/WasmCalleeGroup.h
     wasm/WasmCallingConvention.h
+    wasm/WasmCallsiteCollection.h
     wasm/WasmCapabilities.h
     wasm/WasmCompilationContext.h
     wasm/WasmCompilationMode.h

Source/JavaScriptCore/JavaScriptCore.xcodeproj/project.pbxproj
@@ -1189,6 +1189,7 @@
 		539BFBB022AD3CDC0023F4C0 /* JSWeakObjectRef.h in Headers */ = {isa = PBXBuildFile; fileRef = 539BFBAF22AD3CDC0023F4C0 /* JSWeakObjectRef.h */; };
 		539DD7F523C1BBB500905F13 /* JSArrayIterator.h in Headers */ = {isa = PBXBuildFile; fileRef = 539DD7F423C1BBA900905F13 /* JSArrayIterator.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		539FB8BA1C99DA7C00940FA1 /* JSArrayInlines.h in Headers */ = {isa = PBXBuildFile; fileRef = 539FB8B91C99DA7C00940FA1 /* JSArrayInlines.h */; settings = {ATTRIBUTES = (Private, ); }; };
+		53A3FF322CE41924009AABCF /* WasmCallsiteCollection.h in Headers */ = {isa = PBXBuildFile; fileRef = 53FB0D282CE40D11008A3331 /* WasmCallsiteCollection.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		53B4BD121F68B32500D2BEA3 /* WasmOps.h in Headers */ = {isa = PBXBuildFile; fileRef = 533B15DE1DC7F463004D500A /* WasmOps.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		53B601EC2034B8C5006BE667 /* JSCast.h in Headers */ = {isa = PBXBuildFile; fileRef = 53B601EB2034B8C5006BE667 /* JSCast.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		53C2CE9C22FCC3D6008B2853 /* AirHelpers.h in Headers */ = {isa = PBXBuildFile; fileRef = 53C2CE9B22FCC3D6008B2853 /* AirHelpers.h */; };
@@ -4315,6 +4316,8 @@
 		53F9DD762BEFD76D004A17B7 /* GCOwnedDataScope.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = GCOwnedDataScope.h; sourceTree = "<group>"; };
 		53FA2AE01CF37F3F0022711D /* LLIntPrototypeLoadAdaptiveStructureWatchpoint.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = LLIntPrototypeLoadAdaptiveStructureWatchpoint.h; sourceTree = "<group>"; };
 		53FA2AE21CF380390022711D /* LLIntPrototypeLoadAdaptiveStructureWatchpoint.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = LLIntPrototypeLoadAdaptiveStructureWatchpoint.cpp; sourceTree = "<group>"; };
+		53FB0D282CE40D11008A3331 /* WasmCallsiteCollection.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = WasmCallsiteCollection.h; sourceTree = "<group>"; };
+		53FB0D292CE40D11008A3331 /* WasmCallsiteCollection.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; path = WasmCallsiteCollection.cpp; sourceTree = "<group>"; };
 		53FD04D11D7AB187003287D3 /* WasmCallingConvention.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = WasmCallingConvention.cpp; sourceTree = "<group>"; };
 		53FD04D21D7AB187003287D3 /* WasmCallingConvention.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = WasmCallingConvention.h; sourceTree = "<group>"; };
 		554C418529B14CA5003C9F71 /* WebAssemblyGCObjectBase.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = WebAssemblyGCObjectBase.cpp; path = js/WebAssemblyGCObjectBase.cpp; sourceTree = "<group>"; };
@@ -7743,6 +7746,8 @@
 				526AC4B51E977C5D003500E1 /* WasmCalleeGroup.h */,
 				53FD04D11D7AB187003287D3 /* WasmCallingConvention.cpp */,
 				53FD04D21D7AB187003287D3 /* WasmCallingConvention.h */,
+				53FB0D292CE40D11008A3331 /* WasmCallsiteCollection.cpp */,
+				53FB0D282CE40D11008A3331 /* WasmCallsiteCollection.h */,
 				E337B966224324E50093A820 /* WasmCapabilities.h */,
 				E32D51A62C6D0FD600B013D1 /* WasmCompilationContext.cpp */,
 				E32D51A52C6D0FD600B013D1 /* WasmCompilationContext.h */,
@@ -11913,6 +11918,7 @@
 				525C0DDA1E935847002184CD /* WasmCallee.h in Headers */,
 				526AC4B71E977C5D003500E1 /* WasmCalleeGroup.h in Headers */,
 				53FD04D41D7AB291003287D3 /* WasmCallingConvention.h in Headers */,
+				53A3FF322CE41924009AABCF /* WasmCallsiteCollection.h in Headers */,
 				E337B967224324EA0093A820 /* WasmCapabilities.h in Headers */,
 				E32D51AF2C6D0FF200B013D1 /* WasmCompilationContext.h in Headers */,
 				E3BD2B7622F275020011765C /* WasmCompilationMode.h in Headers */,
@@ -12563,7 +12569,7 @@
 		};
 		5D5D8ABF0E0D0B0300F9C692 /* Create /usr/local/bin/jsc symlink */ = {
 			isa = PBXShellScriptBuildPhase;
-			buildActionMask = 2147483647;
+			buildActionMask = 8;
 			files = (
 			);
 			inputPaths = (
@@ -12764,7 +12770,7 @@
 		};
 		F4CDF3C927E9147500191928 /* Copy Profiling Data */ = {
 			isa = PBXShellScriptBuildPhase;
-			buildActionMask = 12;
+			buildActionMask = 2147483647;
 			files = (
 			);
 			inputFileListPaths = (

Source/JavaScriptCore/heap/ConservativeRoots.h
@@ -34,6 +34,7 @@ class HeapCell;
 class JITStubRoutineSet;
 
 class ConservativeRoots {
+    WTF_FORBID_HEAP_ALLOCATION;
 public:
     ConservativeRoots(Heap&);
     ~ConservativeRoots();

Source/JavaScriptCore/wasm/WasmBBQPlan.cpp
@@ -136,7 +136,9 @@ void BBQPlan::work(CompilationEffort)
 
         m_calleeGroup->setBBQCallee(locker, m_functionIndex, callee.copyRef());
         ASSERT(m_calleeGroup->replacement(locker, callee->index()) == callee.ptr());
+#if ENABLE(WASM_CODE_RECLAIMATION)
         m_calleeGroup->reportCallees(locker, callee.ptr(), function->outgoingJITDirectCallees);
+#endif
 
         for (auto& call : callee->wasmToWasmCallsites()) {
             CodePtr<WasmEntryPtrTag> entrypoint;
@@ -152,7 +154,12 @@ void BBQPlan::work(CompilationEffort)
             MacroAssembler::repatchPointer(call.calleeLocation, CalleeBits::boxNativeCalleeIfExists(calleeCallee));
         }
 
+#if ENABLE(WASM_CODE_RECLAIMATION)
         m_calleeGroup->updateCallsitesToCallUs(locker, CodeLocationLabel<WasmEntryPtrTag>(entrypoint), m_functionIndex);
+#else
+        m_calleeGroup->callsiteCollection().addCallsites(locker, m_calleeGroup.get(), callee->wasmToWasmCallsites());
+        m_calleeGroup->callsiteCollection().updateCallsitesToCallUs(locker, m_calleeGroup.get(), CodeLocationLabel<WasmEntryPtrTag>(entrypoint), m_functionIndex, functionIndexSpace);
+#endif
 
         {
             WTF::storeStoreFence();

Source/JavaScriptCore/wasm/WasmCalleeGroup.cpp
@@ -59,10 +59,15 @@ CalleeGroup::CalleeGroup(MemoryMode mode, const CalleeGroup& other)
     , m_mode(mode)
     , m_llintCallees(other.m_llintCallees)
     , m_jsEntrypointCallees(other.m_jsEntrypointCallees)
+#if ENABLE(WASM_CODE_RECLAIMATION)
     , m_callers(m_calleeCount)
+#endif
     , m_wasmIndirectCallEntryPoints(other.m_wasmIndirectCallEntryPoints)
     , m_wasmIndirectCallWasmCallees(other.m_wasmIndirectCallWasmCallees)
     , m_wasmToWasmExitStubs(other.m_wasmToWasmExitStubs)
+#if !ENABLE(WASM_CODE_RECLAIMATION)
+    , m_callsiteCollection(m_calleeCount)
+#endif
 {
     Locker locker { m_lock };
     setCompilationFinished();
@@ -72,7 +77,12 @@ CalleeGroup::CalleeGroup(VM& vm, MemoryMode mode, ModuleInformation& moduleInfor
     : m_calleeCount(moduleInformation.internalFunctionCount())
     , m_mode(mode)
     , m_llintCallees(llintCallees)
+#if ENABLE(WASM_CODE_RECLAIMATION)
     , m_callers(m_calleeCount)
+#else
+    , m_callsiteCollection(m_calleeCount)
+#endif
+
 {
     RefPtr<CalleeGroup> protectedThis = this;
     m_plan = adoptRef(*new LLIntPlan(vm, moduleInformation, m_llintCallees->data(), createSharedTask<Plan::CallbackType>([this, protectedThis = WTFMove(protectedThis)] (Plan&) {
@@ -97,6 +107,9 @@ CalleeGroup::CalleeGroup(VM& vm, MemoryMode mode, ModuleInformation& moduleInfor
         }
 
         m_wasmToWasmExitStubs = m_plan->takeWasmToWasmExitStubs();
+#if !ENABLE(WASM_CODE_RECLAIMATION)
+        m_callsiteCollection.addCalleeGroupCallsites(locker, *this, m_plan->takeWasmToWasmCallsites());
+#endif
         m_jsEntrypointCallees = static_cast<LLIntPlan*>(m_plan.get())->takeJSCallees();
 
         setCompilationFinished();
@@ -117,7 +130,11 @@ CalleeGroup::CalleeGroup(VM& vm, MemoryMode mode, ModuleInformation& moduleInfor
     : m_calleeCount(moduleInformation.internalFunctionCount())
     , m_mode(mode)
     , m_ipintCallees(ipintCallees)
+#if ENABLE(WASM_CODE_RECLAIMATION)
     , m_callers(m_calleeCount)
+#else
+    , m_callsiteCollection(m_calleeCount)
+#endif
 {
     RefPtr<CalleeGroup> protectedThis = this;
     m_plan = adoptRef(*new IPIntPlan(vm, moduleInformation, m_ipintCallees->data(), createSharedTask<Plan::CallbackType>([this, protectedThis = WTFMove(protectedThis)] (Plan&) {
@@ -137,6 +154,9 @@ CalleeGroup::CalleeGroup(VM& vm, MemoryMode mode, ModuleInformation& moduleInfor
         }
 
         m_wasmToWasmExitStubs = m_plan->takeWasmToWasmExitStubs();
+#if !ENABLE(WASM_CODE_RECLAIMATION)
+        m_callsiteCollection.addCalleeGroupCallsites(locker, *this, m_plan->takeWasmToWasmCallsites());
+#endif
         m_jsEntrypointCallees = static_cast<IPIntPlan*>(m_plan.get())->takeJSCallees();
 
         setCompilationFinished();
@@ -199,6 +219,7 @@ BBQCallee* CalleeGroup::tryGetBBQCalleeForLoopOSR(const AbstractLocker&, VM& vm,
         return nullptr;
 
     auto& maybeCallee = m_bbqCallees[functionIndex];
+#if ENABLE(WASM_CODE_RECLAIMATION)
     if (maybeCallee.isStrong())
         return maybeCallee.ptr();
 
@@ -211,8 +232,13 @@ BBQCallee* CalleeGroup::tryGetBBQCalleeForLoopOSR(const AbstractLocker&, VM& vm,
     BBQCallee* result = bbqCallee.get();
     vm.heap.reportWasmCalleePendingDestruction(bbqCallee.releaseNonNull());
     return result;
+#else
+    UNUSED_PARAM(vm);
+    return maybeCallee.get();
+#endif
 }
 
+#if ENABLE(WASM_CODE_RECLAIMATION)
 void CalleeGroup::releaseBBQCallee(const AbstractLocker&, FunctionCodeIndex functionIndex)
 {
     if (!Options::freeRetiredWasmCode())
@@ -230,7 +256,9 @@ void CalleeGroup::releaseBBQCallee(const AbstractLocker&, FunctionCodeIndex func
     bbqCallee->reportToVMsForDestruction();
 }
 #endif
+#endif
 
+#if ENABLE(WASM_CODE_RECLAIMATION)
 #if ENABLE(WEBASSEMBLY_OMGJIT) || ENABLE(WEBASSEMBLY_BBQJIT)
 void CalleeGroup::updateCallsitesToCallUs(const AbstractLocker& locker, CodeLocationLabel<WasmEntryPtrTag> entrypoint, FunctionCodeIndex functionIndex)
 {
@@ -349,7 +377,7 @@ void CalleeGroup::reportCallees(const AbstractLocker&, JITCallee* caller, const 
         );
     }
 }
-#endif
+#endif // ENABLE(WEBASSEMBLY_OMGJIT) || ENABLE(WEBASSEMBLY_BBQJIT)
 
 TriState CalleeGroup::calleeIsReferenced(const AbstractLocker&, Wasm::Callee* callee) const
 {
@@ -389,6 +417,7 @@ TriState CalleeGroup::calleeIsReferenced(const AbstractLocker&, Wasm::Callee* ca
         RELEASE_ASSERT_NOT_REACHED();
     }
 }
+#endif // ENABLE(WASM_CODE_RECLAIMATION)
 
 bool CalleeGroup::isSafeToRun(MemoryMode memoryMode)
 {

Source/JavaScriptCore/wasm/WasmCalleeGroup.h
@@ -30,6 +30,7 @@
 #include "MacroAssemblerCodeRef.h"
 #include "MemoryMode.h"
 #include "WasmCallee.h"
+#include "WasmCallsiteCollection.h"
 #include "WasmJS.h"
 #include <wtf/CrossThreadCopier.h>
 #include <wtf/FixedBitVector.h>
@@ -52,6 +53,7 @@ struct UnlinkedWasmToWasmCall;
 
 class CalleeGroup final : public ThreadSafeRefCounted<CalleeGroup> {
 public:
+    friend class CallsiteCollection;
     typedef void CallbackType(Ref<CalleeGroup>&&, bool);
     using AsyncCompilationCallback = RefPtr<WTF::SharedTask<CallbackType>>;
     static Ref<CalleeGroup> createFromLLInt(VM&, MemoryMode, ModuleInformation&, RefPtr<LLIntCallees>);
@@ -151,7 +153,7 @@ public:
     void setBBQCallee(const AbstractLocker&, FunctionCodeIndex functionIndex, Ref<BBQCallee>&& callee) WTF_REQUIRES_LOCK(m_lock)
     {
         if (m_bbqCallees.isEmpty())
-            m_bbqCallees = FixedVector<ThreadSafeWeakOrStrongPtr<BBQCallee>>(m_calleeCount);
+            m_bbqCallees = FixedVector<BBQSmartPtrType>(m_calleeCount);
         m_bbqCallees[functionIndex] = WTFMove(callee);
     }
 
@@ -174,11 +176,13 @@ public:
         m_omgCallees[functionIndex] = WTFMove(callee);
     }
 
+#if ENABLE(WASM_CODE_RECLAIMATION)
     void recordOMGOSREntryCallee(const AbstractLocker&, FunctionCodeIndex functionIndex, OMGOSREntryCallee& callee) WTF_REQUIRES_LOCK(m_lock)
     {
         auto result = m_osrEntryCallees.add(functionIndex, callee);
         ASSERT_UNUSED(result, result.isNewEntry);
     }
+#endif
 #endif
 
     CodePtr<WasmEntryPtrTag>* entrypointLoadLocationFromFunctionIndexSpace(FunctionSpaceIndex functionIndexSpace)
@@ -205,6 +209,7 @@ public:
 
     MemoryMode mode() const { return m_mode; }
 
+#if ENABLE(WASM_CODE_RECLAIMATION)
 #if ENABLE(WEBASSEMBLY_OMGJIT) || ENABLE(WEBASSEMBLY_BBQJIT)
     void updateCallsitesToCallUs(const AbstractLocker&, CodeLocationLabel<WasmEntryPtrTag> entrypoint, FunctionCodeIndex functionIndex) WTF_REQUIRES_LOCK(m_lock);
     void reportCallees(const AbstractLocker&, JITCallee* caller, const FixedBitVector& callees) WTF_REQUIRES_LOCK(m_lock);
@@ -212,6 +217,12 @@ public:
 
     // TriState::Indeterminate means weakly referenced.
     TriState calleeIsReferenced(const AbstractLocker&, Wasm::Callee*) const WTF_REQUIRES_LOCK(m_lock);
+
+#else
+
+    CallsiteCollection& callsiteCollection() { return m_callsiteCollection; }
+    const CallsiteCollection& callsiteCollection() const { return m_callsiteCollection; }
+#endif
 
     ~CalleeGroup();
 private:
@@ -235,12 +246,19 @@ private:
     FixedVector<RefPtr<OMGCallee>> m_omgCallees;
 #endif
 #if ENABLE(WEBASSEMBLY_BBQJIT)
-    FixedVector<ThreadSafeWeakOrStrongPtr<BBQCallee>> m_bbqCallees;
+#if ENABLE(WASM_CODE_RECLAIMATION)
+    using BBQSmartPtrType = ThreadSafeWeakOrStrongPtr<BBQCallee>;
+#else
+    using BBQSmartPtrType = RefPtr<BBQCallee>;
+#endif
+
+    FixedVector<BBQSmartPtrType> m_bbqCallees;
 #endif
     RefPtr<IPIntCallees> m_ipintCallees;
     RefPtr<LLIntCallees> m_llintCallees;
     UncheckedKeyHashMap<uint32_t, RefPtr<JSEntrypointCallee>, DefaultHash<uint32_t>, WTF::UnsignedWithZeroKeyHashTraits<uint32_t>> m_jsEntrypointCallees;
-#if ENABLE(WEBASSEMBLY_BBQJIT) || ENABLE(WEBASSEMBLY_OMGJIT)
+#if ENABLE(WASM_CODE_RECLAIMATION)
+#if (ENABLE(WEBASSEMBLY_BBQJIT) || ENABLE(WEBASSEMBLY_OMGJIT))
     // FIXME: We should probably find some way to prune dead entries periodically.
     UncheckedKeyHashMap<uint32_t, ThreadSafeWeakPtr<OMGOSREntryCallee>, DefaultHash<uint32_t>, WTF::UnsignedWithZeroKeyHashTraits<uint32_t>> m_osrEntryCallees;
 #endif
@@ -252,10 +270,14 @@ private:
     using SparseCallers = HashSet<uint32_t, DefaultHash<uint32_t>, WTF::UnsignedWithZeroKeyHashTraits<uint32_t>>;
     using DenseCallers = BitVector;
     FixedVector<std::variant<SparseCallers, DenseCallers>> m_callers;
+#endif // ENABLE(WASM_CODE_RECLAIMATION)
     FixedVector<CodePtr<WasmEntryPtrTag>> m_wasmIndirectCallEntryPoints;
     FixedVector<RefPtr<Wasm::Callee>> m_wasmIndirectCallWasmCallees;
     FixedVector<MacroAssemblerCodeRef<WasmEntryPtrTag>> m_wasmToWasmExitStubs;
     RefPtr<EntryPlan> m_plan;
+#if !ENABLE(WASM_CODE_RECLAIMATION)
+    CallsiteCollection m_callsiteCollection;
+#endif
     std::atomic<bool> m_compilationFinished { false };
     String m_errorMessage;
 public:

Source/JavaScriptCore/wasm/WasmCallsiteCollection.cpp
@@ -1,0 +1,101 @@
+/*
+ * Copyright (C) 2023 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "WasmCallsiteCollection.h"
+
+#if ENABLE(WEBASSEMBLY)
+
+#include "WasmCallee.h"
+#include "WasmCalleeGroup.h"
+#include "WasmMachineThreads.h"
+#include <wtf/DataLog.h>
+#include <wtf/Locker.h>
+#include <wtf/StdLibExtras.h>
+
+namespace JSC { namespace Wasm {
+
+#if ENABLE(WEBASSEMBLY_OMGJIT) || ENABLE(WEBASSEMBLY_BBQJIT)
+namespace WasmCallsiteCollectionInternal {
+static constexpr bool verbose = false;
+}
+#endif
+
+void CallsiteCollection::addCallsites(const AbstractLocker& calleeGroupLocker, CalleeGroup& calleeGroup, const FixedVector<UnlinkedWasmToWasmCall>& callsites)
+{
+    UNUSED_PARAM(calleeGroupLocker);
+    unsigned functionImportCount = calleeGroup.functionImportCount();
+    for (auto& callsite : callsites) {
+        unsigned calleeIndex = callsite.functionIndexSpace - functionImportCount;
+        m_callsites[calleeIndex].append(Callsite {
+            callsite.callLocation,
+            { }
+        });
+    }
+}
+
+void CallsiteCollection::addCalleeGroupCallsites(const AbstractLocker& calleeGroupLocker, CalleeGroup& calleeGroup, Vector<Vector<UnlinkedWasmToWasmCall>>&& callsitesList)
+{
+    UNUSED_PARAM(calleeGroupLocker);
+    unsigned functionImportCount = calleeGroup.functionImportCount();
+    for (auto& callsites : callsitesList) {
+        for (auto& callsite : callsites) {
+            unsigned calleeIndex = callsite.functionIndexSpace - functionImportCount;
+            m_callsites[calleeIndex].append(Callsite {
+                callsite.callLocation,
+                { }
+            });
+        }
+    }
+    m_calleeGroupCallsites = WTFMove(callsitesList);
+}
+
+#if ENABLE(WEBASSEMBLY_OMGJIT) || ENABLE(WEBASSEMBLY_BBQJIT)
+void CallsiteCollection::updateCallsitesToCallUs(const AbstractLocker& calleeGroupLocker, CalleeGroup& calleeGroup, CodeLocationLabel<WasmEntryPtrTag> entrypoint, uint32_t functionIndex, uint32_t functionIndexSpace)
+{
+    UNUSED_PARAM(calleeGroupLocker);
+    UNUSED_PARAM(functionIndexSpace);
+
+    for (auto& callsite : m_callsites[functionIndex])
+        callsite.m_target = MacroAssembler::prepareForAtomicRepatchNearCallConcurrently(callsite.m_callLocation, entrypoint);
+
+    // It's important to make sure we do this before we make any of the code we just compiled visible. If we didn't, we could end up
+    // where we are tiering up some function A to A' and we repatch some function B to call A' instead of A. Another CPU could see
+    // the updates to B but still not have reset its cache of A', which would lead to all kinds of badness.
+    resetInstructionCacheOnAllThreads();
+    WTF::storeStoreFence(); // This probably isn't necessary but it's good to be paranoid.
+
+    calleeGroup.m_wasmIndirectCallEntryPoints[functionIndex] = entrypoint;
+
+    for (auto& callsite : m_callsites[functionIndex]) {
+        dataLogLnIf(WasmCallsiteCollectionInternal::verbose, "Repatching call at: ", RawPointer(callsite.m_callLocation.dataLocation()), " to ", RawPointer(entrypoint.taggedPtr()));
+        MacroAssembler::repatchNearCall(callsite.m_callLocation, callsite.m_target);
+    }
+}
+#endif
+
+} } // namespace JSC::Wasm
+
+#endif // ENABLE(WEBASSEMBLY)

Source/JavaScriptCore/wasm/WasmCallsiteCollection.h
@@ -25,39 +25,40 @@
 
 #pragma once
 
-#include "ImplementationVisibility.h"
-#include <wtf/TZoneMalloc.h>
-#include <wtf/ThreadSafeWeakPtr.h>
+#include "WasmFormat.h"
+#include <wtf/FixedVector.h>
+#include <wtf/Lock.h>
+#include <wtf/Vector.h>
 
-namespace JSC {
+#if ENABLE(WEBASSEMBLY)
 
-class LLIntOffsetsExtractor;
+namespace JSC { namespace Wasm {
 
-#if ENABLE(WASM_CODE_RECLAIMATION)
-class NativeCallee : public ThreadSafeRefCountedAndCanMakeThreadSafeWeakPtr<NativeCallee> {
-#else
-class NativeCallee : public ThreadSafeRefCounted<NativeCallee> {
-#endif
-    WTF_MAKE_COMPACT_TZONE_ALLOCATED(NativeCallee);
+class CalleeGroup;
+
+class CallsiteCollection {
 public:
-    enum class Category : uint8_t {
-        InlineCache,
-        Wasm,
-    };
+    CallsiteCollection(unsigned functionsExcudingImport)
+        : m_callsites(functionsExcudingImport)
+    {
+    }
 
-    Category category() const { return m_category; }
-    ImplementationVisibility implementationVisibility() const { return m_implementationVisibility; }
+    void addCalleeGroupCallsites(const AbstractLocker& calleeGroupLocker, CalleeGroup&, Vector<Vector<UnlinkedWasmToWasmCall>>&&);
+    void addCallsites(const AbstractLocker& calleeGroupLocker, CalleeGroup&, const FixedVector<UnlinkedWasmToWasmCall>&);
 
-    void dump(PrintStream&) const;
+    void updateCallsitesToCallUs(const AbstractLocker& calleeGroupLocker, CalleeGroup&, CodeLocationLabel<WasmEntryPtrTag> entrypoint, uint32_t functionIndex, uint32_t functionIndexSpace);
 
-    JS_EXPORT_PRIVATE void operator delete(NativeCallee*, std::destroying_delete_t);
-
-protected:
-    JS_EXPORT_PRIVATE NativeCallee(Category, ImplementationVisibility);
+    const Vector<Vector<UnlinkedWasmToWasmCall>>& calleeGroupCallsites() const { return m_calleeGroupCallsites; }
 
 private:
-    Category m_category;
-    ImplementationVisibility m_implementationVisibility { ImplementationVisibility::Public };
+    struct Callsite {
+        CodeLocationNearCall<WasmEntryPtrTag> m_callLocation;
+        CodeLocationLabel<WasmEntryPtrTag> m_target;
+    };
+    Vector<Vector<UnlinkedWasmToWasmCall>> m_calleeGroupCallsites;
+    FixedVector<Vector<Callsite>> m_callsites;
 };
 
-} // namespace JSC
+} } // namespace JSC::Wasm
+
+#endif // ENABLE(WEBASSEMBLY)

Source/JavaScriptCore/wasm/WasmOMGPlan.cpp
@@ -185,7 +185,9 @@ void OMGPlan::work(CompilationEffort)
 
         m_calleeGroup->setOMGCallee(locker, m_functionIndex, callee.copyRef());
         ASSERT(m_calleeGroup->replacement(locker, callee->index()) == callee.ptr());
+#if ENABLE(WASM_CODE_RECLAIMATION)
         m_calleeGroup->reportCallees(locker, callee.ptr(), internalFunction->outgoingJITDirectCallees);
+#endif
 
         for (auto& call : callee->wasmToWasmCallsites()) {
             CodePtr<WasmEntryPtrTag> entrypoint;
@@ -203,8 +205,13 @@ void OMGPlan::work(CompilationEffort)
             MacroAssembler::repatchPointer(call.calleeLocation, CalleeBits::boxNativeCalleeIfExists(calleeCallee));
         }
 
+#if ENABLE(WASM_CODE_RECLAIMATION)
         m_calleeGroup->updateCallsitesToCallUs(locker, CodeLocationLabel<WasmEntryPtrTag>(entrypoint), m_functionIndex);
         ASSERT(*m_calleeGroup->entrypointLoadLocationFromFunctionIndexSpace(functionIndexSpace) == entrypoint);
+#else
+        m_calleeGroup->callsiteCollection().addCallsites(locker, m_calleeGroup.get(), callee->wasmToWasmCallsites());
+        m_calleeGroup->callsiteCollection().updateCallsitesToCallUs(locker, m_calleeGroup, CodeLocationLabel<WasmEntryPtrTag>(entrypoint), m_functionIndex, functionIndexSpace);
+#endif
 
         {
             WTF::storeStoreFence();
@@ -229,11 +236,13 @@ void OMGPlan::work(CompilationEffort)
     if (jsEntrypointCallee)
         jsEntrypointCallee->setReplacementTarget(entrypoint);
 
+#if ENABLE(WASM_CODE_RECLAIMATION)
     if (Options::freeRetiredWasmCode()) {
         WTF::storeStoreFence();
         Locker locker { m_calleeGroup->m_lock };
         m_calleeGroup->releaseBBQCallee(locker, m_functionIndex);
     }
+#endif
 
     dataLogLnIf(WasmOMGPlanInternal::verbose, "Finished OMG ", m_functionIndex);
     Locker locker { m_lock };

Source/JavaScriptCore/wasm/WasmOSREntryPlan.cpp
@@ -140,8 +140,10 @@ void OSREntryPlan::work(CompilationEffort)
     callee->setEntrypoint(WTFMove(omgEntrypoint), internalFunction->osrEntryScratchBufferSize, WTFMove(unlinkedCalls), WTFMove(internalFunction->stackmaps), WTFMove(internalFunction->exceptionHandlers), WTFMove(exceptionHandlerLocations));
     {
         Locker locker { m_calleeGroup->m_lock };
+#if ENABLE(WASM_CODE_RECLAIMATION)
         m_calleeGroup->recordOMGOSREntryCallee(locker, m_functionIndex, callee.get());
         m_calleeGroup->reportCallees(locker, callee.ptr(), internalFunction->outgoingJITDirectCallees);
+#endif
 
         for (auto& call : callee->wasmToWasmCallsites()) {
             CodePtr<WasmEntryPtrTag> entrypoint;

Source/JavaScriptCore/wasm/WasmOperations.cpp
@@ -1198,7 +1198,7 @@ JSC_DEFINE_NOEXCEPT_JIT_OPERATION(operationWasmLoopOSREnterBBQJIT, void, (Probe:
 
 ALWAYS_INLINE void assertCalleeIsReferenced(CallFrame* frame, JSWebAssemblyInstance* instance)
 {
-#if ASSERT_ENABLED
+#if ASSERT_ENABLED && ENABLE(WASM_CODE_RECLAIMATION)
     CalleeGroup& calleeGroup = *instance->calleeGroup();
     Wasm::Callee* callee = static_cast<Wasm::Callee*>(frame->callee().asNativeCallee());
     TriState status;

Source/WTF/wtf/RefPtr.h
@@ -60,6 +60,7 @@ public:
     ALWAYS_INLINE ~RefPtr() { RefDerefTraits::derefIfNotNull(PtrTraits::exchange(m_ptr, nullptr)); }
 
     T* get() const { return PtrTraits::unwrap(m_ptr); }
+    T* ptr() const { return get(); }
 
     Ref<T> releaseNonNull() { ASSERT(m_ptr); Ref<T> tmp(adoptRef(*m_ptr)); m_ptr = nullptr; return tmp; }
 