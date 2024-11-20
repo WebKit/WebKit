20f5df0feb7da718bfd61e59b1decfbcdaf02bbf
Carlos Bentzen <cadubentzen@igalia.com>
2024-11-20 01:18:28 -0800

    [GStreamer][LibWebRTC] No H.264 decoder found when using broadcom and westeros quirks and no FFmpeg installed https://bugs.webkit.org/show_bug.cgi?id=283359
    Reviewed by Philippe Normand.
    
    Use GStreamerRegistryScanner::isCodecSupport() instead of the
    hand-written implementation of GstDecoderFactory(), because this is more
    robust to detect decoders in hardware-accelerated scenarios with custom
    elements and GStreamer quirks.
    
    This is cherry-pick of https://github.com/WebPlatformForEmbedded/WPEWebKit/commit/d7a7509280358127a6ac75d9ba2242aa9b88150e
    Original author: Vivek.A <Vivek_Arumugam@comcast.com>
    
    * Source/WebCore/platform/mediastream/libwebrtc/gstreamer/GStreamerVideoDecoderFactory.cpp:
    (WebCore::GStreamerWebRTCVideoDecoder::GstDecoderFactory):
    
    Canonical link: https://commits.webkit.org/286848@main

--- Source/WebCore/platform/mediastream/libwebrtc/gstreamer/GStreamerVideoDecoderFactory.cpp
+++ Source/WebCore/platform/mediastream/libwebrtc/gstreamer/GStreamerVideoDecoderFactory.cpp
@@ -24,6 +24,7 @@
 #include "GStreamerVideoDecoderFactory.h"
 
 #include "GStreamerQuirks.h"
+#include "GStreamerRegistryScanner.h"
 #include "GStreamerVideoCommon.h"
 #include "GStreamerVideoFrameLibWebRTC.h"
 #include "webrtc/modules/video_coding/codecs/h264/include/h264.h"
@@ -277,19 +278,7 @@ public:
 
     static GRefPtr<GstElementFactory> GstDecoderFactory(const char* capsStr)
     {
-        auto allDecoders = gst_element_factory_list_get_elements(GST_ELEMENT_FACTORY_TYPE_DECODER,
-            GST_RANK_MARGINAL);
-        auto caps = adoptGRef(gst_caps_from_string(capsStr));
-        auto decoders = gst_element_factory_list_filter(allDecoders,
-            caps.get(), GST_PAD_SINK, FALSE);
-
-        gst_plugin_feature_list_free(allDecoders);
-        GRefPtr<GstElementFactory> res;
-        if (decoders)
-            res = GST_ELEMENT_FACTORY(decoders->data);
-        gst_plugin_feature_list_free(decoders);
-
-        return res;
+        return GStreamerRegistryScanner::singleton().isCodecSupported(GStreamerRegistryScanner::Configuration::Decoding, String::fromUTF8(capsStr), false).factory;
     }
 
     bool HasGstDecoder()
