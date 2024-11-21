81b3ff8571fb7219854a4cae1c450f896b50864d
David Kilzer <ddkilzer@apple.com>
2024-11-20 17:29:40 -0800

    [Media] WebCore::WebVTTCueData needs to use WTF_MAKE_TZONE_ALLOCATED_EXPORT() <https://bugs.webkit.org/show_bug.cgi?id=283449> <rdar://140309828>
    Unreviewed build fix.
    
    * Source/WebCore/html/track/WebVTTParser.h:
    (WebCore::WebVTTCueData):
    - Change WTF_MAKE_TZONE_ALLOCATED() to WTF_MAKE_TZONE_ALLOCATED_EXPORT()
      so that "WebCore::WebVTTCueData::operator delete(void*)" is exported.
    
    Canonical link: https://commits.webkit.org/286887@main

--- Source/WebCore/html/track/WebVTTParser.h
+++ Source/WebCore/html/track/WebVTTParser.h
@@ -63,7 +63,7 @@ public:
 };
 
 class WebVTTCueData final : public RefCounted<WebVTTCueData> {
-    WTF_MAKE_TZONE_ALLOCATED(WebVTTCueData);
+    WTF_MAKE_TZONE_ALLOCATED_EXPORT(WebVTTCueData, WEBCORE_EXPORT);
 public:
 
     static Ref<WebVTTCueData> create() { return adoptRef(*new WebVTTCueData()); }
