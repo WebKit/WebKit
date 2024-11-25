81ca2e9fb66634cea993ad9619ba51d0f0854476
Claudio Saavedra <csaavedra@igalia.com>
2024-11-25 03:47:00 -0800

    [SOUP] Drop WTF_ALLOW_UNSAFE_BUFFER_USAGE in PublicSuffixStoreSoup https://bugs.webkit.org/show_bug.cgi?id=283669
    Reviewed by Adrian Perez de Castro.
    
    Use StringView API instead of raw char* manipulation.
    
    * Source/WebCore/platform/soup/PublicSuffixStoreSoup.cpp:
    (WebCore::PublicSuffixStore::platformTopPrivatelyControlledDomain const):
    
    Canonical link: https://commits.webkit.org/287061@main

--- Source/WebCore/platform/soup/PublicSuffixStoreSoup.cpp
+++ Source/WebCore/platform/soup/PublicSuffixStoreSoup.cpp
@@ -60,21 +60,17 @@ static String permissiveTopPrivateDomain(StringView domain)
 
 String PublicSuffixStore::platformTopPrivatelyControlledDomain(StringView domain) const
 {
-    CString domainUTF8 = domain.utf8();
-
     // This function is expected to work with the format used by cookies, so skip any leading dots.
-    WTF_ALLOW_UNSAFE_BUFFER_USAGE_BEGIN // GLib port
     unsigned position = 0;
-    while (domainUTF8.data()[position] == '.')
+    while (domain[position] == '.')
         position++;
 
-    if (position == domainUTF8.length())
+    if (position == domain.length())
         return String();
 
     GUniqueOutPtr<GError> error;
-    if (const char* baseDomain = soup_tld_get_base_domain(domainUTF8.data() + position, &error.outPtr()))
+    if (const char* baseDomain = soup_tld_get_base_domain(domain.substring(position).utf8().data(), &error.outPtr()))
         return String::fromUTF8(baseDomain);
-    WTF_ALLOW_UNSAFE_BUFFER_USAGE_END
 
     if (g_error_matches(error.get(), SOUP_TLD_ERROR, SOUP_TLD_ERROR_NO_BASE_DOMAIN)) {
         if (domain.endsWithIgnoringASCIICase("web-platform.test"_s))
