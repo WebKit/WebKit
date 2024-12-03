6c52b67944ce03ceb3e10d318479bc7e4e0806c0
Sam Sneddon <gsnedders@apple.com>
2024-12-03 11:34:37 -0800

    webkitcorepy.Terminal should allow any file-like object https://bugs.webkit.org/show_bug.cgi?id=283250
    Reviewed by Jonathan Bedard.
    
    Previously, we strictly required arguments to
    Terminal.assert_writeable_stream and Terminal.override_atty to be
    specific types of file-like objects (specifically: those inheriting
    from io.IOBase, as on Python 3 all three options are either io.IOBase
    or subclasses thereof).
    
    This unfortunately means that they then throw when passed another
    file-like object (e.g., something inheriting from the typing.IO ABC).
    
    The status of file-like objects in the Python system is a bit of a
    mess, and requiring them to be specific types is almost certainly
    unworkable, thus we should just rely on duck-typing and checking we
    have something that looks like a file-like object.
    
    * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/terminal.py:
    (Terminal.assert_writeable_stream):
    (Terminal.override_atty):
    * Tools/Scripts/libraries/webkitcorepy/webkitcorepy/tests/terminal_unittest.py:
    (TerminalTests.test_interrupt_decorator):
    (TerminalTests):
    (TerminalTests.test_assert_writeable_stream):
    (TerminalTests.test_override_atty):
    
    Canonical link: https://commits.webkit.org/287297@main

--- Tools/Scripts/libraries/webkitcorepy/webkitcorepy/tests/terminal_unittest.py
+++ Tools/Scripts/libraries/webkitcorepy/webkitcorepy/tests/terminal_unittest.py
@@ -20,12 +20,15 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import io
 import logging
 import sys
+import typing
 import unittest
 
 from mock import patch
-from webkitcorepy import mocks, OutputCapture, Terminal
+
+from webkitcorepy import OutputCapture, Terminal, mocks
 
 
 class TerminalTests(unittest.TestCase):
@@ -95,8 +98,6 @@ class TerminalTests(unittest.TestCase):
         self.assertEqual(captured.stdout.getvalue(), 'Pick:\n    1) Alpha\n    2) Beta\n    3) Charlie\n    4) Delta\n: \n')
 
     def test_interrupt(self):
-        from mock import patch
-
         def do_interrupt(output):
             print(output)
             raise KeyboardInterrupt
@@ -123,3 +124,40 @@ class TerminalTests(unittest.TestCase):
         with self.assertRaises(KeyboardInterrupt):
             with Terminal.disable_keyboard_interrupt_stacktracktrace(logging.root.level):
                 raise KeyboardInterrupt
+
+    def test_assert_writeable_stream(self):
+        for file_like in (
+            io.BytesIO(),
+            io.StringIO(),
+            sys.stdout,
+            sys.stderr,
+        ):
+            Terminal.assert_writeable_stream(file_like)
+
+        for file_like in (
+            sys.stdin,
+            typing.IO(),
+        ):
+            with self.assertRaises(ValueError):
+                Terminal.assert_writeable_stream(file_like)
+
+    def test_override_atty(self):
+        for file_like in (
+            io.BytesIO(),
+            io.StringIO(),
+            sys.stdin,
+            sys.stdout,
+            sys.stderr,
+            typing.IO(),
+        ):
+            original = Terminal.isatty(file_like)
+
+            with Terminal.override_atty(file_like, isatty=False):
+                self.assertFalse(Terminal.isatty(file_like))
+
+            self.assertEqual(original, Terminal.isatty(file_like))
+
+            with Terminal.override_atty(file_like, isatty=True):
+                self.assertTrue(Terminal.isatty(file_like))
+
+            self.assertEqual(original, Terminal.isatty(file_like))
