4fb2286de6a966512b42fdde27d65f4822a28b0f
Alexsander Damaceno <aemgbo@gmail.com>
2024-12-17 00:26:11 -0300

    Apply text-justify: none on lines https://bugs.webkit.org/show_bug.cgi?id=241304
    Reviewed by NOBODY (OOPS!).
    
    Verify if text-justify is set to none, and do not compute justification on lines.
    
    * LayoutTests/fast/css3-text/css3-text-justify/text-justify-none-01-expected.txt: Added.
    * LayoutTests/fast/css3-text/css3-text-justify/text-justify-none-01.html: Added.
    * Source/WTF/Scripts/Preferences/UnifiedWebPreferences.yaml:
    * Source/WebCore/layout/formattingContexts/inline/InlineLineBuilder.cpp:
    (WebCore::Layout::LineBuilder::placeInlineAndFloatContent):

--- Source/WebCore/layout/formattingContexts/inline/InlineLineBuilder.cpp
+++ Source/WebCore/layout/formattingContexts/inline/InlineLineBuilder.cpp
@@ -582,7 +582,7 @@ UniqueRef<LineContent> LineBuilder::placeInlineAndFloatContent(const InlineItemR
                 // in order to exactly fill the line box. Unless otherwise specified by text-align-last,
                 // the last line before a forced break or the end of the block is start-aligned.
                 auto hasTextAlignJustify = (isLastInlineContent || m_line.runs().last().isLineBreak()) ? rootStyle.textAlignLast() == TextAlignLast::Justify : rootStyle.textAlign() == TextAlignMode::Justify;
-                if (hasTextAlignJustify) {
+                if (hasTextAlignJustify && rootStyle.textJustify() != TextJustify::None) {
                     auto additionalSpaceForAlignedContent = InlineContentAligner::applyTextAlignJustify(m_line.runs(), spaceToDistribute, m_line.hangingTrailingWhitespaceLength());
                     m_line.inflateContentLogicalWidth(additionalSpaceForAlignedContent);
                 }
