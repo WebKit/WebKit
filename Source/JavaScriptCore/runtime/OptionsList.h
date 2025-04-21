afe30fb89038d6b3404c2a5ee23d8b0a4b1b0206
Marcus Plutowski <marcus_plutowski@apple.com>
2025-04-21 10:21:40 -0700

    [JSC] Disable DFG loop unrolling https://bugs.webkit.org/show_bug.cgi?id=291852 rdar://147263376
    Reviewed by Yijia Huang
    
    This is known to cause compiler crashes; while a fix is in the pipeline,
    we should disable it to prevent issues downstream.
    
    * Source/JavaScriptCore/runtime/OptionsList.h:
    
    Originally-landed-as: 289651.443@safari-7621.2.5.10-branch (4f4f4d8c6646). rdar://151714471

--- Source/JavaScriptCore/runtime/OptionsList.h
+++ Source/JavaScriptCore/runtime/OptionsList.h
@@ -593,7 +593,7 @@ bool hasCapacityToUseLargeGigacage();
     v(Unsigned, allocationProfilingMode, 0, Normal, "Allows custom arguments to be passed to bmalloc/libPAS allocation profiling features at JSC launch."_s) \
     v(Bool, dumpBaselineJITSizeStatistics, false, Normal, nullptr) \
     v(Bool, dumpDFGJITSizeStatistics, false, Normal, nullptr) \
-    v(Bool, useLoopUnrolling, true, Normal, nullptr) \
+    v(Bool, useLoopUnrolling, false, Normal, nullptr) \
     v(Bool, usePartialLoopUnrolling, true, Normal, nullptr) \
     v(Bool, verboseLoopUnrolling, false, Normal, nullptr) \
     v(Bool, disallowLoopUnrollingForNonInnermost, true, Normal, nullptr) \
