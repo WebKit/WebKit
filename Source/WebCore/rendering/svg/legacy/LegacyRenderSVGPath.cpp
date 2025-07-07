2c604325ff8e06c4861033a09f0081e4273a6779
Adan Lopez <ja_lopezlozoya@apple.com>
2025-07-07 11:57:16 -0700

    ASAN_TRAP in WebCore::LegacyRenderSVGShape::strokeBoundingBox https://bugs.webkit.org/show_bug.cgi?id=295535 rdar://153080312
    Reviewed by NOBODY (OOPS!).
    
    Adding checks for invalid bounding box leading marker to NaN.
    
    * Source/WebCore/rendering/svg/legacy/LegacyRenderSVGPath.cpp:
    (WebCore::LegacyRenderSVGPath::adjustStrokeBoundingBoxForMarkersAndZeroLengthLinecaps const):
    * Source/WebCore/rendering/svg/legacy/LegacyRenderSVGShape.cpp:
    (WebCore::LegacyRenderSVGShape::calculateStrokeBoundingBox const):

--- Source/WebCore/rendering/svg/legacy/LegacyRenderSVGPath.cpp
+++ Source/WebCore/rendering/svg/legacy/LegacyRenderSVGPath.cpp
@@ -86,8 +86,12 @@ FloatRect LegacyRenderSVGPath::adjustStrokeBoundingBoxForMarkersAndZeroLengthLin
 {
     float strokeWidth = this->strokeWidth();
 
-    if (!m_markerPositions.isEmpty())
-        strokeBoundingBox.unite(markerRect(repaintRectCalculation, strokeWidth));
+    if (!m_markerPositions.isEmpty()) {
+        FloatRect markerRect = this->markerRect(repaintRectCalculation, strokeWidth);
+        if (!markerRect.isNaN())
+            strokeBoundingBox.unite(markerRect);
+
+    }
 
     if (style().svgStyle().hasStroke()) {
         // FIXME: zero-length subpaths do not respect vector-effect = non-scaling-stroke.
--- Source/WebCore/rendering/svg/legacy/LegacyRenderSVGShape.cpp
+++ Source/WebCore/rendering/svg/legacy/LegacyRenderSVGShape.cpp
@@ -402,9 +402,11 @@ FloatRect LegacyRenderSVGShape::calculateStrokeBoundingBox() const
                     strokeBoundingBox.unite(strokeBoundingRect);
             }
         } else {
-            strokeBoundingBox.unite(path().strokeBoundingRect(Function<void(GraphicsContext&)> { [this] (GraphicsContext& context) {
+            FloatRect strokeBoundingRect = path().strokeBoundingRect(Function<void(GraphicsContext&)> { [this] (GraphicsContext& context) {
                 SVGRenderSupport::applyStrokeStyleToContext(context, style(), *this);
-            } }));
+            } });
+            if (!strokeBoundingRect.isNaN())
+                strokeBoundingBox.unite(strokeBoundingRect);
         }
     }
 
