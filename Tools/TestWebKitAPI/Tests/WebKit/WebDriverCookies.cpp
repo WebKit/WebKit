e60c7c521af51dac1579f8385e44a6209c9c0d56
Kirk Elliott <elliott@ionq.co>
2025-07-25 09:43:03 -0400

    Fix style issues in WebDriver cookie tests
    - Remove trailing whitespace
    - Fix include order
    - Add missing newlines at end of files
    - Fix indentation issues

--- Tools/TestWebKitAPI/Tests/WebKit/WebDriverCookies.cpp
+++ Tools/TestWebKitAPI/Tests/WebKit/WebDriverCookies.cpp
@@ -31,15 +31,14 @@
 #include <WebKit/WKContext.h>
 #include <WebKit/WKPageConfigurationRef.h>
 #include <WebKit/WKRetainPtr.h>
-#include <WebKit/WKWebsiteDataStoreRef.h>
 #include <WebKit/WKWebsiteDataStoreConfigurationRef.h>
-#include <WebKit/WebKitAutomationSession.h>
+#include <WebKit/WKWebsiteDataStoreRef.h>
 #include <wtf/FileSystem.h>
 
 #if PLATFORM(GTK) || PLATFORM(WPE)
 #include <WebKit/WebKitAutomationSession.h>
-#include <WebKit/WebKitWebsiteDataManager.h>
 #include <WebKit/WebKitCookieManager.h>
+#include <WebKit/WebKitWebsiteDataManager.h>
 #include <WebKit/webkit.h>
 #endif
 
@@ -55,10 +54,10 @@ public:
         m_automationSession = webkit_automation_session_new("WebDriverCookieTest", "1.0");
         m_websiteDataManager = webkit_web_context_get_website_data_manager(m_webContext);
         m_cookieManager = webkit_website_data_manager_get_cookie_manager(m_websiteDataManager);
-        
+
         // Connect to cookie change signal to verify signal emission
         g_signal_connect(m_cookieManager, "changed", G_CALLBACK(onCookieChanged), this);
-        
+
         m_cookieChangedSignalReceived = false;
 #endif
     }
@@ -91,22 +90,21 @@ public:
 TEST_F(WebDriverCookieTest, BasicWebDriverCookieStorage)
 {
     // Test 1: Basic WebDriver cookie setting through automation session
-    
+
     // Create a WebDriver cookie directly
-    SoupCookie* cookie = soup_cookie_new("webdriver_test_cookie", "webdriver_test_value", 
+    SoupCookie* cookie = soup_cookie_new("webdriver_test_cookie", "webdriver_test_value",
                                          "127.0.0.1", "/", -1);
-    
+
     // Set cookie through WebKit cookie manager (simulates WebDriver path)
     webkit_cookie_manager_add_cookie(m_cookieManager, cookie, nullptr, nullptr, nullptr);
-    
+
     // Wait for signal emission
     auto startTime = g_get_monotonic_time();
-    while (!m_cookieChangedSignalReceived && (g_get_monotonic_time() - startTime) < 5000000) {
+    while (!m_cookieChangedSignalReceived && (g_get_monotonic_time() - startTime) < 5000000)
         g_main_context_iteration(nullptr, FALSE);
-    }
-    
+
     EXPECT_TRUE(m_cookieChangedSignalReceived);
-    
+
     soup_cookie_free(cookie);
 }
 
@@ -114,9 +112,9 @@ TEST_F(WebDriverCookieTest, WebDriverCookieSignalEmission)
 {
     // Test 2: Verify that WebDriver cookie operations emit the 'changed' signal
     // This tests the core fix in Bug 279079
-    
+
     m_cookieChangedSignalReceived = false;
-    
+
     // Create multiple cookies to test signal emission for each
     std::vector<SoupCookie*> cookies;
     for (int i = 0; i < 3; ++i) {
@@ -126,69 +124,65 @@ TEST_F(WebDriverCookieTest, WebDriverCookieSignalEmission)
         g_free(name);
         g_free(value);
     }
-    
+
     int signalCount = 0;
     auto originalHandler = G_CALLBACK(onCookieChanged);
     g_signal_handlers_disconnect_by_func(m_cookieManager, originalHandler, this);
-    
+
     // Connect a counting handler
     g_signal_connect(m_cookieManager, "changed", G_CALLBACK(+[](WebKitCookieManager*, gpointer userData) {
         (*static_cast<int*>(userData))++;
     }), &signalCount);
-    
+
     // Add cookies through WebDriver path
-    for (auto* cookie : cookies) {
+    for (auto* cookie : cookies)
         webkit_cookie_manager_add_cookie(m_cookieManager, cookie, nullptr, nullptr, nullptr);
-    }
-    
+
     // Wait for signals
     auto startTime = g_get_monotonic_time();
-    while (signalCount < 3 && (g_get_monotonic_time() - startTime) < 5000000) {
+    while (signalCount < 3 && (g_get_monotonic_time() - startTime) < 5000000)
         g_main_context_iteration(nullptr, FALSE);
-    }
-    
+
     EXPECT_EQ(signalCount, 3);
-    
+
     // Cleanup
-    for (auto* cookie : cookies) {
+    for (auto* cookie : cookies)
         soup_cookie_free(cookie);
-    }
 }
 
 TEST_F(WebDriverCookieTest, WebDriverCookieDOMConsistency)
 {
     // Test 3: Verify WebDriver-set cookies are visible in DOM
-    
+
     // Create a simple WebView to test DOM visibility
     auto webView = webkit_web_view_new_with_context(m_webContext);
-    
+
     // Set a cookie through WebDriver path
-    SoupCookie* cookie = soup_cookie_new("webdriver_dom_test", "dom_test_value", 
+    SoupCookie* cookie = soup_cookie_new("webdriver_dom_test", "dom_test_value",
                                          "127.0.0.1", "/", -1);
     webkit_cookie_manager_add_cookie(m_cookieManager, cookie, nullptr, nullptr, nullptr);
-    
+
     // Load a page and check if cookie is accessible via document.cookie
-    webkit_web_view_load_html(WEBKIT_WEB_VIEW(webView), 
+    webkit_web_view_load_html(WEBKIT_WEB_VIEW(webView),
         "<html><body><script>"
         "window.cookieTestResult = document.cookie.includes('webdriver_dom_test=dom_test_value');"
-        "</script></body></html>", 
+        "</script></body></html>",
         "http://127.0.0.1/");
-    
+
     // Wait for page load
     bool loadFinished = false;
     g_signal_connect(webView, "load-finished", G_CALLBACK(+[](WebKitWebView*, WebKitLoadEvent, gpointer userData) {
         *static_cast<bool*>(userData) = true;
     }), &loadFinished);
-    
+
     auto startTime = g_get_monotonic_time();
-    while (!loadFinished && (g_get_monotonic_time() - startTime) < 5000000) {
+    while (!loadFinished && (g_get_monotonic_time() - startTime) < 5000000)
         g_main_context_iteration(nullptr, FALSE);
-    }
-    
+
     EXPECT_TRUE(loadFinished);
-    
+
     // Check if cookie is visible in DOM
-    webkit_web_view_run_javascript(WEBKIT_WEB_VIEW(webView), "window.cookieTestResult", nullptr, 
+    webkit_web_view_run_javascript(WEBKIT_WEB_VIEW(webView), "window.cookieTestResult", nullptr,
         +[](GObject*, GAsyncResult* result, gpointer userData) {
             WebKitJavascriptResult* jsResult = webkit_web_view_run_javascript_finish(WEBKIT_WEB_VIEW(userData), result, nullptr);
             if (jsResult) {
@@ -200,19 +194,18 @@ TEST_F(WebDriverCookieTest, WebDriverCookieDOMConsistency)
             bool* finished = static_cast<bool*>(g_object_get_data(G_OBJECT(userData), "finished"));
             *finished = true;
         }, webView);
-    
+
     bool testResult = false;
     bool jsFinished = false;
     g_object_set_data(G_OBJECT(webView), "test-result", &testResult);
     g_object_set_data(G_OBJECT(webView), "finished", &jsFinished);
-    
+
     startTime = g_get_monotonic_time();
-    while (!jsFinished && (g_get_monotonic_time() - startTime) < 5000000) {
+    while (!jsFinished && (g_get_monotonic_time() - startTime) < 5000000)
         g_main_context_iteration(nullptr, FALSE);
-    }
-    
+
     EXPECT_TRUE(testResult);
-    
+
     // Cleanup
     soup_cookie_free(cookie);
     g_object_unref(webView);
@@ -221,43 +214,41 @@ TEST_F(WebDriverCookieTest, WebDriverCookieDOMConsistency)
 TEST_F(WebDriverCookieTest, WebDriverCookieStressTest)
 {
     // Test 4: Stress test - multiple rapid WebDriver cookie operations
-    
+
     m_cookieChangedSignalReceived = false;
     int signalCount = 0;
-    
+
     g_signal_handlers_disconnect_by_data(m_cookieManager, this);
     g_signal_connect(m_cookieManager, "changed", G_CALLBACK(+[](WebKitCookieManager*, gpointer userData) {
         (*static_cast<int*>(userData))++;
     }), &signalCount);
-    
+
     // Rapidly add multiple cookies
     const int numCookies = 20;
     std::vector<SoupCookie*> cookies;
-    
+
     for (int i = 0; i < numCookies; ++i) {
         auto name = g_strdup_printf("webdriver_stress_%d", i);
         auto value = g_strdup_printf("stress_value_%d", i);
         auto* cookie = soup_cookie_new(name, value, "127.0.0.1", "/", -1);
         cookies.push_back(cookie);
-        
+
         webkit_cookie_manager_add_cookie(m_cookieManager, cookie, nullptr, nullptr, nullptr);
-        
+
         g_free(name);
         g_free(value);
     }
-    
+
     // Wait for all signals
     auto startTime = g_get_monotonic_time();
-    while (signalCount < numCookies && (g_get_monotonic_time() - startTime) < 10000000) {
+    while (signalCount < numCookies && (g_get_monotonic_time() - startTime) < 10000000)
         g_main_context_iteration(nullptr, FALSE);
-    }
-    
+
     EXPECT_EQ(signalCount, numCookies);
-    
+
     // Cleanup
-    for (auto* cookie : cookies) {
+    for (auto* cookie : cookies)
         soup_cookie_free(cookie);
-    }
 }
 
 #else
@@ -272,4 +263,4 @@ TEST_F(WebDriverCookieTest, PlatformNotSupported)
 
 #endif // PLATFORM(GTK) || PLATFORM(WPE)
 
-} // namespace TestWebKitAPI 
+} // namespace TestWebKitAPI
--- Tools/TestWebKitAPI/Tests/WebKitCocoa/WebDriverCookies.mm
+++ Tools/TestWebKitAPI/Tests/WebKitCocoa/WebDriverCookies.mm
@@ -35,10 +35,10 @@
 #import <WebKit/WKHTTPCookieStorePrivate.h>
 #import <WebKit/WKProcessPoolPrivate.h>
 #import <WebKit/WKWebsiteDataStorePrivate.h>
-#import <WebKit/_WKProcessPoolConfiguration.h>
-#import <WebKit/_WKWebsiteDataStoreConfiguration.h>
 #import <WebKit/_WKAutomationSession.h>
 #import <WebKit/_WKAutomationSessionDelegate.h>
+#import <WebKit/_WKProcessPoolConfiguration.h>
+#import <WebKit/_WKWebsiteDataStoreConfiguration.h>
 #import <wtf/BlockPtr.h>
 #import <wtf/ProcessPrivilege.h>
 #import <wtf/RetainPtr.h>
@@ -92,29 +92,29 @@ public:
     {
         // Clean up any existing cookies
         auto dataStore = [WKWebsiteDataStore defaultDataStore];
-        [dataStore removeDataOfTypes:[WKWebsiteDataStore allWebsiteDataTypes] 
-                       modifiedSince:[NSDate distantPast] 
+        [dataStore removeDataOfTypes:[WKWebsiteDataStore allWebsiteDataTypes]
+                       modifiedSince:[NSDate distantPast]
                    completionHandler:^{
                        webDriverCookieOperationCompleted = true;
                    }];
-        
+
         Util::run(&webDriverCookieOperationCompleted);
         webDriverCookieOperationCompleted = false;
-        
+
         // Set up automation session for WebDriver testing
         auto processPoolConfiguration = adoptNS([[_WKProcessPoolConfiguration alloc] init]);
         processPoolConfiguration.get().automationAllowed = YES;
-        
+
         m_processPool = adoptNS([[WKProcessPool alloc] _initWithConfiguration:processPoolConfiguration.get()]);
-        
+
         m_automationDelegate = adoptNS([[WebDriverAutomationDelegate alloc] init]);
         m_automationSession = adoptNS([[_WKAutomationSession alloc] init]);
         [m_automationSession setDelegate:m_automationDelegate.get()];
         [m_processPool _setAutomationSession:m_automationSession.get()];
-        
+
         m_observer = adoptNS([[WebDriverCookieObserver alloc] init]);
         [m_observer setCallbackCount:0];
-        
+
         cookieStoreObserverCallbackWasCalled = false;
     }
 
@@ -133,12 +133,12 @@ protected:
 TEST_F(WebDriverCookieTest, BasicWebDriverCookieStorage)
 {
     // Test 1: Basic WebDriver cookie storage through automation session
-    
+
     auto dataStore = [WKWebsiteDataStore defaultDataStore];
     auto cookieStore = dataStore.httpCookieStore;
-    
+
     [cookieStore addObserver:m_observer.get()];
-    
+
     // Create a cookie that would be set by WebDriver
     auto cookie = [NSHTTPCookie cookieWithProperties:@{
         NSHTTPCookieName: @"webdriver_test_cookie",
@@ -146,19 +146,19 @@ TEST_F(WebDriverCookieTest, BasicWebDriverCookieStorage)
         NSHTTPCookieDomain: @"localhost",
         NSHTTPCookiePath: @"/",
     }];
-    
+
     // Set cookie through WebDriver path (simulated via cookie store)
     [cookieStore setCookie:cookie completionHandler:^{
         webDriverCookieOperationCompleted = true;
     }];
-    
+
     Util::run(&webDriverCookieOperationCompleted);
     webDriverCookieOperationCompleted = false;
-    
+
     // Verify cookie was stored and observer was called
     EXPECT_TRUE(cookieStoreObserverCallbackWasCalled);
     EXPECT_GT([m_observer callbackCount], 0U);
-    
+
     // Verify cookie is accessible
     [cookieStore getAllCookies:^(NSArray<NSHTTPCookie *> *cookies) {
         EXPECT_EQ(cookies.count, 1U);
@@ -168,10 +168,10 @@ TEST_F(WebDriverCookieTest, BasicWebDriverCookieStorage)
         }
         webDriverCookieOperationCompleted = true;
     }];
-    
+
     Util::run(&webDriverCookieOperationCompleted);
     webDriverCookieOperationCompleted = false;
-    
+
     [cookieStore removeObserver:m_observer.get()];
 }
 
@@ -179,14 +179,14 @@ TEST_F(WebDriverCookieTest, WebDriverCookieObserverSignaling)
 {
     // Test 2: Verify that WebDriver cookie operations trigger observer callbacks
     // This tests the equivalent of libsoup 'changed' signal emission on Apple platforms
-    
+
     auto dataStore = [WKWebsiteDataStore defaultDataStore];
     auto cookieStore = dataStore.httpCookieStore;
-    
+
     [cookieStore addObserver:m_observer.get()];
     [m_observer setCallbackCount:0];
     cookieStoreObserverCallbackWasCalled = false;
-    
+
     // Create multiple cookies to test observer notification for each
     NSArray *cookieProperties = @[
         @{
@@ -196,21 +196,21 @@ TEST_F(WebDriverCookieTest, WebDriverCookieObserverSignaling)
             NSHTTPCookiePath: @"/",
         },
         @{
-            NSHTTPCookieName: @"webdriver_observer_test_2", 
+            NSHTTPCookieName: @"webdriver_observer_test_2",
             NSHTTPCookieValue: @"observer_value_2",
             NSHTTPCookieDomain: @"localhost",
             NSHTTPCookiePath: @"/",
         },
         @{
             NSHTTPCookieName: @"webdriver_observer_test_3",
-            NSHTTPCookieValue: @"observer_value_3", 
+            NSHTTPCookieValue: @"observer_value_3",
             NSHTTPCookieDomain: @"localhost",
             NSHTTPCookiePath: @"/",
         }
     ];
-    
+
     __block NSUInteger expectedCallbacks = cookieProperties.count;
-    
+
     // Set cookies through WebDriver-like path
     for (NSDictionary *properties : cookieProperties) {
         auto cookie = [NSHTTPCookie cookieWithProperties:properties];
@@ -221,28 +221,28 @@ TEST_F(WebDriverCookieTest, WebDriverCookieObserverSignaling)
             }
         }];
     }
-    
+
     Util::run(&webDriverCookieOperationCompleted);
     webDriverCookieOperationCompleted = false;
-    
+
     // Verify observer was called for each cookie
     EXPECT_TRUE(cookieStoreObserverCallbackWasCalled);
     EXPECT_EQ([m_observer callbackCount], cookieProperties.count);
-    
+
     [cookieStore removeObserver:m_observer.get()];
 }
 
 TEST_F(WebDriverCookieTest, WebDriverCookieDOMConsistency)
 {
     // Test 3: Verify WebDriver-set cookies are visible in DOM
-    
+
     auto configuration = adoptNS([[WKWebViewConfiguration alloc] init]);
     configuration.get().processPool = m_processPool.get();
     auto webView = adoptNS([[TestWKWebView alloc] initWithFrame:NSMakeRect(0, 0, 800, 600) configuration:configuration.get()]);
-    
+
     auto dataStore = configuration.get().websiteDataStore;
     auto cookieStore = dataStore.httpCookieStore;
-    
+
     // Set a cookie through WebDriver path
     auto cookie = [NSHTTPCookie cookieWithProperties:@{
         NSHTTPCookieName: @"webdriver_dom_consistency_test",
@@ -250,24 +250,24 @@ TEST_F(WebDriverCookieTest, WebDriverCookieDOMConsistency)
         NSHTTPCookieDomain: @"localhost",
         NSHTTPCookiePath: @"/",
     }];
-    
+
     [cookieStore setCookie:cookie completionHandler:^{
         webDriverCookieOperationCompleted = true;
     }];
-    
+
     Util::run(&webDriverCookieOperationCompleted);
     webDriverCookieOperationCompleted = false;
-    
+
     // Load a page and check if cookie is accessible via document.cookie
-    [webView loadHTMLString:@"<html><body><script>window.testResult = document.cookie.includes('webdriver_dom_consistency_test=dom_consistency_value');</script></body></html>" 
+    [webView loadHTMLString:@"<html><body><script>window.testResult = document.cookie.includes('webdriver_dom_consistency_test=dom_consistency_value');</script></body></html>"
                     baseURL:[NSURL URLWithString:@"http://localhost:8080/"]];
     [webView _test_waitForDidFinishNavigation];
-    
+
     // Check if cookie is visible in DOM
     id result = [webView objectByEvaluatingJavaScript:@"window.testResult"];
     EXPECT_TRUE([result isKindOfClass:[NSNumber class]]);
     EXPECT_TRUE([(NSNumber *)result boolValue]);
-    
+
     // Also verify through document.cookie directly
     NSString *documentCookie = [webView stringByEvaluatingJavaScript:@"document.cookie"];
     EXPECT_TRUE([documentCookie containsString:@"webdriver_dom_consistency_test=dom_consistency_value"]);
@@ -276,17 +276,17 @@ TEST_F(WebDriverCookieTest, WebDriverCookieDOMConsistency)
 TEST_F(WebDriverCookieTest, WebDriverCookieStressTest)
 {
     // Test 4: Stress test - multiple rapid WebDriver cookie operations
-    
+
     auto dataStore = [WKWebsiteDataStore defaultDataStore];
     auto cookieStore = dataStore.httpCookieStore;
-    
+
     [cookieStore addObserver:m_observer.get()];
     [m_observer setCallbackCount:0];
     cookieStoreObserverCallbackWasCalled = false;
-    
+
     const NSUInteger numCookies = 20;
     __block NSUInteger operationsCompleted = 0;
-    
+
     // Rapidly add multiple cookies
     for (NSUInteger i = 0; i < numCookies; ++i) {
         auto cookie = [NSHTTPCookie cookieWithProperties:@{
@@ -295,27 +295,27 @@ TEST_F(WebDriverCookieTest, WebDriverCookieStressTest)
             NSHTTPCookieDomain: @"localhost",
             NSHTTPCookiePath: @"/",
         }];
-        
+
         [cookieStore setCookie:cookie completionHandler:^{
             operationsCompleted++;
             if (operationsCompleted == numCookies) {
-                webDriverCookieOperationCompleted = true;  
+                webDriverCookieOperationCompleted = true;
             }
         }];
     }
-    
+
     Util::run(&webDriverCookieOperationCompleted);
     webDriverCookieOperationCompleted = false;
-    
+
     // Verify all operations triggered observer callbacks
     EXPECT_TRUE(cookieStoreObserverCallbackWasCalled);
     EXPECT_EQ([m_observer callbackCount], numCookies);
-    
+
     // Verify all cookies were stored
     [cookieStore getAllCookies:^(NSArray<NSHTTPCookie *> *cookies) {
         // Should have at least our stress test cookies (may have others from other tests)
         EXPECT_GE(cookies.count, numCookies);
-        
+
         NSUInteger stressCookieCount = 0;
         for (NSHTTPCookie *cookie in cookies) {
             if ([cookie.name hasPrefix:@"webdriver_stress_"]) {
@@ -323,13 +323,13 @@ TEST_F(WebDriverCookieTest, WebDriverCookieStressTest)
             }
         }
         EXPECT_EQ(stressCookieCount, numCookies);
-        
+
         webDriverCookieOperationCompleted = true;
     }];
-    
+
     Util::run(&webDriverCookieOperationCompleted);
-    
+
     [cookieStore removeObserver:m_observer.get()];
 }
 
-} // namespace TestWebKitAPI 
+} // namespace TestWebKitAPI
