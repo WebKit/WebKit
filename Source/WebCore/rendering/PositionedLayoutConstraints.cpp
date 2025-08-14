4f6214591145e54a19c6aee8f85872b9883190b4
Alexsander Damaceno <aborges@igalia.com>
2025-08-14 03:22:51 -0300

    [css-anchor-position-1] Support safe with anchor-center https://bugs.webkit.org/show_bug.cgi?id=295503
    Reviewed by NOBODY (OOPS!).
    
    Make safe keyword work with anchor-aligned element as specified in CSS Anchor Positioning spec:
    https://drafts.csswg.org/css-anchor-position-1/#anchor-center
    
    "When using anchor-center, by default if the anchor is too close to the edge of the box’s original containing block,
    it will "shift" from being purely centered, in order to remain within the original containing block.
    See CSS Box Alignment 3 §4.4 Overflow Alignment:
    the safe and unsafe keywords and scroll safety limits for more details."
    
    * Source/WebCore/rendering/PositionedLayoutConstraints.cpp:
    (WebCore::PositionedLayoutConstraints::resolveAlignmentShift const):
    * LayoutTests/imported/w3c/web-platform-tests/css/css-anchor-position/anchor-center-safe-expected.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-anchor-position/anchor-center-safe.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-anchor-position/anchor-center-safe-ref.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-anchor-position/anchor-center-safe-rtl-expected.html: Copied from LayoutTests/imported/w3c/web-platform-tests/css/css-anchor-position/anchor-center-safe-expected.html.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-anchor-position/anchor-center-safe-rtl-ref.html: Copied from LayoutTests/imported/w3c/web-platform-tests/css/css-anchor-position/anchor-center-safe-expected.html.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-anchor-position/anchor-center-safe-rtl.html: Copied from LayoutTests/imported/w3c/web-platform-tests/css/css-anchor-position/anchor-center-safe.html.

--- Source/WebCore/rendering/PositionedLayoutConstraints.cpp
+++ Source/WebCore/rendering/PositionedLayoutConstraints.cpp
@@ -353,8 +353,17 @@ LayoutUnit PositionedLayoutConstraints::resolveAlignmentShift(LayoutUnit unusedS
 
     LayoutUnit shift;
     if (ItemPosition::AnchorCenter == resolvedAlignment) {
-        auto anchorCenterPosition = m_anchorArea.min() + (m_anchorArea.size() - itemSize) / 2;
+        LayoutUnit anchorCenterPosition = m_anchorArea.min() + (m_anchorArea.size() - itemSize) / 2;
         shift = anchorCenterPosition - m_insetModifiedContainingRange.min();
+        if (m_alignment.overflow() == OverflowAlignment::Safe) {
+            if (startIsBefore) {
+                if (shift < 0)
+                    shift = 0;
+            } else {
+                if (shift > unusedSpace)
+                    shift = unusedSpace;
+            }
+        }
         if (!isOverflowing && OverflowAlignment::Default == m_alignment.overflow()) {
             // Avoid introducing overflow of the IMCB.
             if (shift < 0)
