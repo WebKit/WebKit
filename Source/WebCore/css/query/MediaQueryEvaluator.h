0fcfceb939b3ce0d7d7145fdbe377ba32ff6ad55
Quang D. Nguyen <quang13012005@gmail.com>
2025-09-01 01:57:55 -0400

    marked for deletion

--- Source/WebCore/css/query/MediaQueryEvaluator.h
+++ Source/WebCore/css/query/MediaQueryEvaluator.h
@@ -50,7 +50,7 @@ public:
 private:
     AtomString m_mediaType;
     WeakPtr<const Document, WeakPtrImplWithEventTargetData> m_document;
-    const RenderStyle* m_rootElementStyle { nullptr }; // FIXME: Switch to a smart pointer.
+//    const RenderStyle* m_rootElementStyle { nullptr }; // FIXME: Switch to a smart pointer.
     EvaluationResult m_staticMediaConditionResult { EvaluationResult::Unknown };
 };
 
