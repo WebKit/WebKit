7c16338c3f17c3344403d77e3457949541b270c2
Quang D. Nguyen <quang13012005@gmail.com>
2025-09-01 01:57:55 -0400

    added get() for m_rootElementStyle

--- Source/WebCore/css/query/MediaQueryEvaluator.cpp
+++ Source/WebCore/css/query/MediaQueryEvaluator.cpp
@@ -83,7 +83,7 @@ bool MediaQueryEvaluator::evaluate(const MediaQuery& query) const
             return EvaluationResult::Unknown;
 
 //        FeatureEvaluationContext context { *document, { *m_rootElementStyle, m_rootElementStyle, nullptr, document->renderView() }, nullptr };
-        FeatureEvaluationContext
+        FeatureEvaluationContext context { *document, {}}
         return evaluateCondition(*query.condition, context);
     }();
 
