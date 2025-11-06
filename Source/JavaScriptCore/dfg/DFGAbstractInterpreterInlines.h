5d72ba88558e20b8cb03012b59d586329879ed90
Sosuke Suzuki <sosuke@bun.sh>
2025-11-06 13:10:24 +0900

    [JSC] Fold `StringAt` like `StringCharAt` https://bugs.webkit.org/show_bug.cgi?id=302062
    Reviewed by NOBODY (OOPS!).
    
    This patch changes to fold `StringAt` like `StringCharAt`.
    
    * JSTests/stress/string-indexed-fold.js:
    (test2):
    * Source/JavaScriptCore/dfg/DFGAbstractInterpreterInlines.h:
    (JSC::DFG::AbstractInterpreter<AbstractStateType>::executeEffects):

--- Source/JavaScriptCore/dfg/DFGAbstractInterpreterInlines.h
+++ Source/JavaScriptCore/dfg/DFGAbstractInterpreterInlines.h
@@ -2614,6 +2614,7 @@ bool AbstractInterpreter<AbstractStateType>::executeEffects(unsigned clobberLimi
         setTypeForNode(node, SpecStringResolved);
         break;
 
+    case StringAt:
     case StringCharAt: {
         auto& value = forNode(node->child1());
         JSValue constant = value.value();
@@ -2624,7 +2625,10 @@ bool AbstractInterpreter<AbstractStateType>::executeEffects(unsigned clobberLimi
                 break;
             }
         }
-        setTypeForNode(node, SpecStringResolved);
+        if (node->op() == StringAt && node->arrayMode().isOutOfBounds())
+            setTypeForNode(node, SpecStringResolved | SpecOther);
+        else
+            setTypeForNode(node, SpecStringResolved);
         break;
     }
 
@@ -2639,14 +2643,6 @@ bool AbstractInterpreter<AbstractStateType>::executeEffects(unsigned clobberLimi
             }
         }
         setNonCellTypeForNode(node, SpecInt32Only);
-        break;
-    }
-
-    case StringAt: {
-        if (node->arrayMode().isOutOfBounds())
-            setTypeForNode(node, SpecString | SpecOther);
-        else
-            setTypeForNode(node, SpecString);
         break;
     }
 
