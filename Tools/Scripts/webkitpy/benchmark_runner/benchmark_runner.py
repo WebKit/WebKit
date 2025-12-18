WebKit/WebKit

commit 58666b0ba7853872a47bb239514a14cbc4486ff7

'run-benchmark' should launch browsers in a relative clean state. https://bugs.webkit.org/show_bug.cgi?id=234107
Reviewed by Stephanie Lewis.

Add code to ensure Chrome and Firefox launches in a relative clean state.
Refactor browser drivers for macOS Chrome and Firefox to share more code.

* Scripts/webkitpy/benchmark_runner/browser_driver/osx_chrome_driver.py:
Added '--no-first-run', '--no-default-browser-check', '--disable-extensions' flags.
(OSXChromeDriverBase):
(OSXChromeDriverBase.launch_url):
(OSXChromeDriverBase.launch_driver):
(OSXChromeDriverBase._create_chrome_options):
(OSXChromeDriverBase._window_size_arg):
(OSXChromeDriverBase._set_chrome_binary_location):
(OSXChromeDriver):
(OSXChromeDriver._set_chrome_binary_location):
(OSXChromeCanaryDriver._set_chrome_binary_location):
(OSXChromeDriver.launch_url): Deleted.
(OSXChromeDriver.launch_driver): Deleted.
(OSXChromeCanaryDriver.launch_url): Deleted.
(OSXChromeCanaryDriver.launch_driver): Deleted.
(create_args): Deleted.
(create_chrome_options): Deleted.
(create_window_size_arg): Deleted.
* Scripts/webkitpy/benchmark_runner/browser_driver/osx_firefox_driver.py:
Added code to use profiles to suppress default browser check and privacy notice tab.
(OSXFirefoxDriverBase):
(OSXFirefoxDriverBase.__init__):
(OSXFirefoxDriverBase.prepare_env):
(OSXFirefoxDriverBase.restore_env):
(OSXFirefoxDriverBase.launch_url):
(OSXFirefoxDriverBase.launch_driver):
(OSXFirefoxDriverBase._setup_temporary_profile_directory):
(OSXFirefoxDriverBase._teardown_temporary_profile_directory):
(OSXFirefoxDriverBase._set_firefox_binary_location):
(OSXFirefoxDriver):
(OSXFirefoxDriver._set_firefox_binary_location):
(OSXFirefoxNightlyDriver):
(OSXFirefoxNightlyDriver._set_firefox_binary_location):
(OSXFirefoxDriver.launch_url): Deleted.
(OSXFirefoxDriver.launch_driver): Deleted.
(OSXFirefoxNightlyDriver.launch_url): Deleted.
(OSXFirefoxNightlyDriver.launch_driver): Deleted.
(create_args): Deleted.

Tools/Scripts/webkitpy/benchmark_runner/browser_driver/osx_chrome_driver.py
@@ -9,28 +9,53 @@ from webkitpy.benchmark_runner.browser_driver.osx_browser_driver import OSXBrows
 _log = logging.getLogger(__name__)
 
 
-class OSXChromeDriver(OSXBrowserDriver):
+class OSXChromeDriverBase(OSXBrowserDriver):
+    app_name = None
+
+    def launch_url(self, url, options, browser_build_path, browser_path):
+        # FIXME: handle self._browser_path.
+        args_with_url = ['--args', '--homepage', url, self._window_size_arg(), '--no-first-run',
+                         '--no-default-browser-check', '--disable-extensions']
+        self._launch_process(build_dir=browser_build_path, app_name=self.app_name, url=url, args=args_with_url)
+
+    def launch_driver(self, url, options, browser_build_path):
+        from selenium import webdriver
+        chrome_options = self._create_chrome_options(browser_build_path)
+        driver_executable = self.webdriver_binary_path
+        driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=driver_executable)
+        self._launch_webdriver(url=url, driver=driver)
+        return driver
+
+    def _create_chrome_options(self, browser_build_path):
+        from selenium.webdriver.chrome.options import Options
+        chrome_options = Options()
+        chrome_options.add_argument("--disable-web-security")
+        chrome_options.add_argument("--user-data-dir")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument(self._window_size_arg())
+        self._set_chrome_binary_location(chrome_options, browser_build_path)
+        return chrome_options
+
+    def _window_size_arg(self):
+        screen_size = self._screen_size()
+        return '--window-size={width},{height}'.format(width=int(screen_size.width), height=int(screen_size.height))
+
+    def _set_chrome_binary_location(self, options, browser_build_path):
+        pass
+
+
+class OSXChromeDriver(OSXChromeDriverBase):
     process_name = 'Google Chrome'
     browser_name = 'chrome'
     app_name = 'Google Chrome.app'
     bundle_id = 'com.google.Chrome'
 
-    def launch_url(self, url, options, browser_build_path, browser_path):
-        # FIXME: handle self._browser_path.
-        args_with_url = self._insert_url(create_args(), 2, url)
-        self._launch_process(build_dir=browser_build_path, app_name=self.app_name, url=url, args=args_with_url)
-
-    def launch_driver(self, url, options, browser_build_path):
-        from selenium import webdriver
-        chrome_options = create_chrome_options()
-        if browser_build_path:
-            app_path = os.path.join(browser_build_path, self.app_name)
-            binary_path = os.path.join(app_path, "Contents/MacOS", self.process_name)
-            chrome_options.binary_location = binary_path
-        driver_executable = self.webdriver_binary_path
-        driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=driver_executable)
-        self._launch_webdriver(url=url, driver=driver)
-        return driver
+    def _set_chrome_binary_location(self, options, browser_build_path):
+        if not browser_build_path:
+            return
+        app_path = os.path.join(browser_build_path, self.app_name)
+        binary_path = os.path.join(app_path, "Contents/MacOS", self.process_name)
+        options.binary_location = binary_path
 
 
 class OSXChromeCanaryDriver(OSXBrowserDriver):
@@ -39,40 +64,9 @@ class OSXChromeCanaryDriver(OSXBrowserDriver):
     app_name = 'Google Chrome Canary.app'
     bundle_id = 'com.google.Chrome.canary'
 
-    def launch_url(self, url, options, browser_build_path, browser_path):
-        # FIXME: handle self._browser_path.
-        args_with_url = self._insert_url(create_args(), 2, url)
-        self._launch_process(build_dir=browser_build_path, app_name=self.app_name, url=url, args=args_with_url)
-
-    def launch_driver(self, url, options, browser_build_path):
-        from selenium import webdriver
-        chrome_options = create_chrome_options()
+    def _set_chrome_binary_location(self, options, browser_build_path):
         if not browser_build_path:
             browser_build_path = '/Applications/'
         app_path = os.path.join(browser_build_path, self.app_name)
         binary_path = os.path.join(app_path, "Contents/MacOS", self.process_name)
-        chrome_options.binary_location = binary_path
-        driver_executable = self.webdriver_binary_path
-        driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=driver_executable)
-        self._launch_webdriver(url=url, driver=driver)
-        return driver
-
-
-def create_args():
-    args = ['--args', '--homepage', create_window_size_arg()]
-    return args
-
-
-def create_chrome_options():
-    from selenium.webdriver.chrome.options import Options
-    chrome_options = Options()
-    chrome_options.add_argument("--disable-web-security")
-    chrome_options.add_argument("--user-data-dir")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument(create_window_size_arg())
-    return chrome_options
-
-
-def create_window_size_arg():
-    window_size_arg = '--window-size={width},{height}'.format(width=int(OSXBrowserDriver._screen_size().width), height=int(OSXBrowserDriver._screen_size().height))
-    return window_size_arg
+        options.binary_location = binary_path

Tools/Scripts/webkitpy/benchmark_runner/browser_driver/osx_firefox_driver.py
@@ -2,65 +2,85 @@
 
 import logging
 import os
+import shutil
+import tempfile
 
 from webkitpy.benchmark_runner.browser_driver.osx_browser_driver import OSXBrowserDriver
 
 
 _log = logging.getLogger(__name__)
+test_profile_directory = os.path.join(os.path.dirname(__file__), '../data/firefox_profile/')
 
 
-class OSXFirefoxDriver(OSXBrowserDriver):
+class OSXFirefoxDriverBase(OSXBrowserDriver):
+    app_name = None
+
+    def __init__(self, *args, **kwargs):
+        super(OSXFirefoxDriverBase, self).__init__(*args, **kwargs)
+        self._profile_directory = None
+
+    def prepare_env(self, config):
+        super(OSXFirefoxDriverBase, self).prepare_env(config)
+        self._setup_temporary_profile_directory()
+
+    def restore_env(self):
+        super(OSXFirefoxDriverBase, self).restore_env()
+        self._teardown_temporary_profile_directory()
+
+    def launch_url(self, url, options, browser_build_path, browser_path):
+        # FIXME: handle self._browser_path.
+        screen_size = self._screen_size()
+        args_with_url = ['--args', '-width', str(int(screen_size.width)), '-height', str(int(screen_size.height))]
+        if self._profile_directory and os.path.exists(self._profile_directory):
+            args_with_url.extend(['--profile', self._profile_directory])
+        args_with_url.append(url)
+        self._launch_process(build_dir=browser_build_path, app_name=self.app_name, url=url, args=args_with_url)
+
+    def launch_driver(self, url, options, browser_build_path):
+        from selenium import webdriver
+        from selenium.webdriver.firefox.options import Options
+        firefox_options = Options()
+        self._set_firefox_binary_location(options, browser_build_path)
+        driver_executable = self.webdriver_binary_path
+        driver = webdriver.Firefox(firefox_options=firefox_options, executable_path=driver_executable)
+        self._launch_webdriver(url=url, driver=driver)
+        return driver
+
+    def _setup_temporary_profile_directory(self):
+        self._profile_directory = os.path.join(tempfile.mkdtemp(), 'firefox_profile')
+        shutil.copytree(test_profile_directory, self._profile_directory)
+
+    def _teardown_temporary_profile_directory(self):
+        if self._profile_directory and os.path.exists(self._profile_directory):
+            shutil.rmtree(os.path.dirname(self._profile_directory))
+
+    def _set_firefox_binary_location(self, options, browser_build_path):
+        pass
+
+
+class OSXFirefoxDriver(OSXFirefoxDriverBase):
     process_name = 'firefox'
     browser_name = 'firefox'
     app_name = 'Firefox.app'
     bundle_id = 'org.mozilla.firefox'
 
-    def launch_url(self, url, options, browser_build_path, browser_path):
-        # FIXME: handle self._browser_path.
-        args_with_url = self._insert_url(create_args(), 0, url)
-        self._launch_process(build_dir=browser_build_path, app_name=self.app_name, url=url, args=args_with_url)
-
-    def launch_driver(self, url, options, browser_build_path):
-        from selenium import webdriver
-        from selenium.webdriver.firefox.options import Options
-        firefox_options = Options()
-        if browser_build_path:
-            app_path = os.path.join(browser_build_path, self.app_name)
-            binary_path = os.path.join(app_path, "Contents/MacOS", self.process_name)
-            firefox_options.binary_location = binary_path
-        driver_executable = self.webdriver_binary_path
-        driver = webdriver.Firefox(firefox_options=firefox_options, executable_path=driver_executable)
-        self._launch_webdriver(url=url, driver=driver)
-        return driver
+    def _set_firefox_binary_location(self, options, browser_build_path):
+        if not browser_build_path:
+            return
+        app_path = os.path.join(browser_build_path, self.app_name)
+        binary_path = os.path.join(app_path, "Contents/MacOS", self.process_name)
+        options.binary_location = binary_path
 
 
-class OSXFirefoxNightlyDriver(OSXBrowserDriver):
+class OSXFirefoxNightlyDriver(OSXFirefoxDriverBase):
     process_name = 'firefox'
     browser_name = 'firefox-nightly'
     app_name = 'FirefoxNightly.app'
     bundle_id = 'org.mozilla.firefox'
 
-
-    def launch_url(self, url, options, browser_build_path, browser_path):
-        # FIXME: handle self._browser_path.
-        args_with_url = self._insert_url(create_args(), 0, url)
-        self._launch_process(build_dir=browser_build_path, app_name=self.app_name, url=url, args=args_with_url)
-
-    def launch_driver(self, url, options, browser_build_path):
-        from selenium import webdriver
-        from selenium.webdriver.firefox.options import Options
-        firefox_options = Options()
+    def _set_firefox_binary_location(self, options, browser_build_path):
         if not browser_build_path:
             browser_build_path = '/Applications/'
         app_path = os.path.join(browser_build_path, self.app_name)
         binary_path = os.path.join(app_path, "Contents/MacOS", self.process_name)
-        firefox_options.binary_location = binary_path
-        driver_executable = self.webdriver_binary_path
-        driver = webdriver.Firefox(firefox_options=firefox_options, executable_path=driver_executable)
-        self._launch_webdriver(url=url, driver=driver)
-        return driver
-
-
-def create_args():
-    args = ['--args', '-width', str(int(OSXBrowserDriver._screen_size().width)), '-height', str(int(OSXBrowserDriver._screen_size().height))]
-    return args
+        options.binary_location = binary_path

Tools/Scripts/webkitpy/benchmark_runner/data/firefox_profile/user.js
@@ -1,0 +1,9 @@
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.startup.couldRestoreSession.count", -1);
+user_pref("browser.webapps.checkForUpdates", 0);
+user_pref("startup.homepage_welcome_url", "");
+user_pref("startup.homepage_welcome_url.additional", "");
+user_pref("app.update.checkInstallTime", false);
+user_pref("app.update.disabledForTesting", true);
+user_pref("extensions.update.enabled", false);
+user_pref("datareporting.policy.dataSubmissionPolicyBypassNotification", true);

commit 52cb4ab5e9c3373a6b1f2e7f8928d923e39a3223

[run-benchmark] Fix crashes when using --driver=webdriver https://bugs.webkit.org/show_bug.cgi?id=304414
Reviewed by NOBODY (OOPS!).

The WebDriver benchmark runner was crashing due to two issues:

1. BenchmarkRunner.__init__() did not accept http_server_type parameter,
   causing "takes from 10 to 20 positional arguments but 21 were given".

2. launch_driver() methods did not accept browser_path parameter,
   causing "takes 4 positional arguments but 5 were given".

The WebDriver benchmark runner now passes browser_path to launch_driver(),
matching the existing launch_url() signature. This allows specifying a custom
browser binary path when using the WebDriver runner.

This patch also fixes a regression from 245441@main where osx_firefox_driver.py
was passing the wrong options object to _set_firefox_binary_location(),
causing custom browser builds to not work correctly.

Additionally, this modifies Firefox drivers to pass the profile directory to the
WebDriver Service, ensuring Firefox launches with the correct profile settings
that suppress the default browser check and privacy notice.
To do so, update drivers to use the modern Selenium 4 API (Service) instead of
deprecated executable_path and firefox_options/chrome_options parameters.

* Tools/Scripts/webkitpy/benchmark_runner/benchmark_runner.py:
(BenchmarkRunner.__init__): Accept http_server_type parameter for interface
compatibility with WebServerBenchmarkRunner.

* Tools/Scripts/webkitpy/benchmark_runner/browser_driver/linux_chrome_driver.py:
(LinuxChromeDriver.launch_driver): Add browser_path parameter and use Service class.

* Tools/Scripts/webkitpy/benchmark_runner/browser_driver/linux_cog_driver.py:
(CogBrowserDriver.launch_driver): Add browser_path parameter.

* Tools/Scripts/webkitpy/benchmark_runner/browser_driver/linux_epiphany_driver.py:
(EpiphanyBrowserDriver.launch_driver): Add browser_path parameter.

* Tools/Scripts/webkitpy/benchmark_runner/browser_driver/linux_firefox_driver.py:

Tools/Scripts/webkitpy/benchmark_runner/benchmark_runner.py
@@ -23,7 +23,7 @@ class BenchmarkRunner(object):
     def __init__(self, plan_file, local_copy, count_override, timeout_override, build_dir, output_file, platform,
                  browser, browser_path, subtests=None, scale_unit=True, show_iteration_values=False, device_id=None,
                  diagnose_dir=None, generate_pgo_profiles=False, profile_output_dir=None, trace_type=None,
-                 profiling_interval=None, browser_args=None):
+                 profiling_interval=None, browser_args=None, http_server_type=None):
         self._plan_name, self._plan = BenchmarkRunner._load_plan_data(plan_file)
         if 'options' not in self._plan:
             self._plan['options'] = {}

Tools/Scripts/webkitpy/benchmark_runner/browser_driver/linux_chrome_driver.py
@@ -43,17 +43,21 @@ class LinuxChromeDriver(LinuxBrowserDriver):
         self._default_browser_arguments += ['--homepage', url]
         super().launch_url(url, options, browser_build_path, browser_path)
 
-    def launch_driver(self, url, options, browser_build_path):
+    def launch_driver(self, url, options, browser_build_path, browser_path):
         from selenium import webdriver
         from selenium.webdriver.chrome.options import Options
+        from selenium.webdriver.chrome.service import Service
         options = Options()
         for option_switch in self._default_browser_arguments:
             options.add_argument(option_switch)
         if browser_build_path:
             binary_path = os.path.join(browser_build_path, 'chromium-browser')
             options.binary_location = binary_path
+        elif browser_path:
+            options.binary_location = browser_path
         driver_executable = self.webdriver_binary_path
-        driver = webdriver.Chrome(chrome_options=options, executable_path=driver_executable)
+        service = Service(executable_path=driver_executable)
+        driver = webdriver.Chrome(service=service, options=options)
         super().launch_webdriver(url, driver)
         return driver
 

Tools/Scripts/webkitpy/benchmark_runner/browser_driver/linux_cog_driver.py
@@ -44,7 +44,7 @@ class CogBrowserDriver(LinuxBrowserDriver):
         self._default_browser_arguments.append(url)
         super(CogBrowserDriver, self).launch_url(url, options, browser_build_path, browser_path)
 
-    def launch_driver(self, url, options, browser_build_path):
+    def launch_driver(self, url, options, browser_build_path, browser_path):
         raise ValueError("Browser {browser} is not available with webdriver".format(browser=self.browser_name))
 
     def prepare_env(self, config):

Tools/Scripts/webkitpy/benchmark_runner/browser_driver/linux_epiphany_driver.py
@@ -37,5 +37,5 @@ class EpiphanyBrowserDriver(LinuxBrowserDriver):
                                    url]
         super(EpiphanyBrowserDriver, self).launch_url(url, options, browser_build_path, browser_path)
 
-    def launch_driver(self, url, options, browser_build_path):
+    def launch_driver(self, url, options, browser_build_path, browser_path):
         raise ValueError("Browser {browser} is not available with webdriver".format(browser=self.browser_name))

Tools/Scripts/webkitpy/benchmark_runner/browser_driver/linux_firefox_driver.py
@@ -40,14 +40,18 @@ class LinuxFirefoxDriver(LinuxBrowserDriver):
                                    url]
         super(LinuxFirefoxDriver, self).launch_url(url, options, browser_build_path, browser_path)
 
-    def launch_driver(self, url, options, browser_build_path):
+    def launch_driver(self, url, options, browser_build_path, browser_path):
         from selenium import webdriver
         from selenium.webdriver.firefox.options import Options
+        from selenium.webdriver.firefox.service import Service
         options = Options()
         if browser_build_path:
             binary_path = os.path.join(browser_build_path, 'firefox-bin')
             options.binary_location = binary_path
+        elif browser_path:
+            options.binary_location = browser_path
         driver_executable = self.webdriver_binary_path
-        driver = webdriver.Firefox(firefox_options=options, executable_path=driver_executable)
+        service = Service(executable_path=driver_executable, service_args=["--profile-root", self._temp_profiledir])
+        driver = webdriver.Firefox(service=service, options=options)
         super(LinuxFirefoxDriver, self).launch_webdriver(url, driver)
         return driver

Tools/Scripts/webkitpy/benchmark_runner/browser_driver/linux_minibrowsergtk_driver.py
@@ -42,5 +42,5 @@ class GTKMiniBrowserDriver(LinuxBrowserDriver):
         self._default_browser_arguments.append(url)
         super(GTKMiniBrowserDriver, self).launch_url(url, options, browser_build_path, browser_path)
 
-    def launch_driver(self, url, options, browser_build_path):
+    def launch_driver(self, url, options, browser_build_path, browser_path):
         raise ValueError("Browser {browser} is not available with webdriver".format(browser=self.browser_name))

Tools/Scripts/webkitpy/benchmark_runner/browser_driver/linux_minibrowserwpe_driver.py
@@ -44,7 +44,7 @@ class WPEMiniBrowserBaseDriver(LinuxBrowserDriver):
         self._default_browser_arguments.append(url)
         super().launch_url(url, options, browser_build_path, browser_path)
 
-    def launch_driver(self, url, options, browser_build_path):
+    def launch_driver(self, url, options, browser_build_path, browser_path):
         raise ValueError("Browser {browser} is not available with webdriver".format(browser=self.browser_name))
 
     def prepare_env(self, config):

Tools/Scripts/webkitpy/benchmark_runner/browser_driver/linux_servo_driver.py
@@ -31,5 +31,5 @@ class ServoBrowserDriver(LinuxBrowserDriver):
     browser_name = 'servo'
     process_search_list = ['servo']
 
-    def launch_driver(self, url, options, browser_build_path):
+    def launch_driver(self, url, options, browser_build_path, browser_path):
         raise ValueError("Browser {browser} is not available with webdriver".format(browser=self.browser_name))

Tools/Scripts/webkitpy/benchmark_runner/browser_driver/osx_chrome_driver.py
@@ -17,22 +17,25 @@ class OSXChromeDriverBase(OSXBrowserDriver):
     def launch_url(self, url, options, browser_build_path, browser_path):
         self._launch_process(build_dir=browser_build_path, app_name=self.app_name, url=url, args=self.launch_args_with_url(url))
 
-    def launch_driver(self, url, options, browser_build_path):
+    def launch_driver(self, url, options, browser_build_path, browser_path):
         from selenium import webdriver
-        chrome_options = self._create_chrome_options(browser_build_path)
+        chrome_options = self._create_chrome_options(browser_build_path, browser_path)
         driver_executable = self.webdriver_binary_path
         driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=driver_executable)
         self._launch_webdriver(url=url, driver=driver)
         return driver
 
-    def _create_chrome_options(self, browser_build_path):
+    def _create_chrome_options(self, browser_build_path, browser_path):
         from selenium.webdriver.chrome.options import Options
         chrome_options = Options()
         chrome_options.add_argument("--disable-web-security")
         chrome_options.add_argument("--user-data-dir")
         chrome_options.add_argument("--disable-extensions")
         chrome_options.add_argument(self._window_size_arg())
-        self._set_chrome_binary_location(chrome_options, browser_build_path)
+        if browser_build_path:
+            self._set_chrome_binary_location(chrome_options, browser_build_path)
+        elif browser_path:
+            chrome_options.binary_location = browser_path
         return chrome_options
 
     def _window_size_arg(self):

Tools/Scripts/webkitpy/benchmark_runner/browser_driver/osx_firefox_driver.py
@@ -34,13 +34,18 @@ class OSXFirefoxDriverBase(OSXBrowserDriver):
         args_with_url.append(url)
         self._launch_process(build_dir=browser_build_path, app_name=self.app_name, url=url, args=args_with_url)
 
-    def launch_driver(self, url, options, browser_build_path):
+    def launch_driver(self, url, options, browser_build_path, browser_path):
         from selenium import webdriver
         from selenium.webdriver.firefox.options import Options
+        from selenium.webdriver.firefox.service import Service
         firefox_options = Options()
-        self._set_firefox_binary_location(options, browser_build_path)
+        if browser_build_path:
+            self._set_firefox_binary_location(firefox_options, browser_build_path)
+        elif browser_path:
+            firefox_options.binary_location = browser_path
         driver_executable = self.webdriver_binary_path
-        driver = webdriver.Firefox(firefox_options=firefox_options, executable_path=driver_executable)
+        service = Service(executable_path=driver_executable, service_args=["--profile-root", self._profile_directory])
+        driver = webdriver.Firefox(service=service, options=firefox_options)
         self._launch_webdriver(url=url, driver=driver)
         return driver
 

Tools/Scripts/webkitpy/benchmark_runner/browser_driver/osx_safari_driver.py
@@ -104,7 +104,7 @@ class OSXSafariDriver(OSXBrowserDriver):
 
         subprocess.Popen(['open', '-a', safari_app_path, url], env=env)
 
-    def launch_driver(self, url, options, browser_build_path):
+    def launch_driver(self, url, options, browser_build_path, browser_path):
         from selenium import webdriver
         driver = webdriver.Safari(quiet=False)
         self._launch_webdriver(url=url, driver=driver)