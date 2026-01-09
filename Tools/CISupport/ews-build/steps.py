WebKit/WebKit

commit 7ed5d8bb6c92ebf91ae3705bc86fcce6fb92a013

[Win] Skip CompileWebKitWithoutChange on Win-Tests-EWS https://bugs.webkit.org/show_bug.cgi?id=300919
Reviewed by Jonathan Bedard.

It submits an auto-retry if compiling without the patch fails on that
box. Very occasionally saves a manual step for that PR at the expense
of delaying results for the other PRs waiting in the queue.

Given the limited worker resources I don't think that trade-off makes
sense for Win-Tests-EWS, we'd rather free up the worker faster to work
on another job.

Canonical link: https://commits.webkit.org/305111@main

Tools/CISupport/ews-build/steps.py
@@ -4308,6 +4308,7 @@ class RunWebKitTests(shell.Test, AddToLogMixin, ShellMixin):
     def evaluateCommand(self, cmd):
         rc = self.evaluateResult(cmd)
         previous_build_summary = self.getProperty('build_summary', '')
+        platform = self.getProperty('platform')
         steps_to_add = []
 
         if SHOULD_FILTER_LOGS is True:
@@ -4353,15 +4354,16 @@ class RunWebKitTests(shell.Test, AddToLogMixin, ShellMixin):
                     ReRunWebKitTests(),
                 ]
             else:
-                steps_to_add += [
-                    RevertAppliedChanges(),
-                    CleanWorkingDirectory(),
-                    ValidateChange(verifyBugClosed=False, addURLs=False),
-                    CompileWebKitWithoutChange(retry_build_on_failure=True),
-                    ValidateChange(verifyBugClosed=False, addURLs=False),
-                    KillOldProcesses(),
-                    RunWebKitTestsWithoutChange(),
-                ]
+                if platform not in ('win'):
+                    steps_to_add += [
+                        RevertAppliedChanges(),
+                        CleanWorkingDirectory(),
+                        ValidateChange(verifyBugClosed=False, addURLs=False),
+                        CompileWebKitWithoutChange(retry_build_on_failure=True),
+                        ValidateChange(verifyBugClosed=False, addURLs=False),
+                        KillOldProcesses(),
+                        RunWebKitTestsWithoutChange(),
+                    ]
         self.build.addStepsAfterCurrentStep(steps_to_add)
 
         return rc
@@ -4467,6 +4469,7 @@ class ReRunWebKitTests(RunWebKitTests):
         flaky_failures = sorted(list(flaky_failures))[:self.NUM_FAILURES_TO_DISPLAY]
         flaky_failures_string = ', '.join(flaky_failures)
         previous_build_summary = self.getProperty('build_summary', '')
+        platform = self.getProperty('platform')
         steps_to_add = []
 
         if SHOULD_FILTER_LOGS is True:
@@ -4521,19 +4524,23 @@ class ReRunWebKitTests(RunWebKitTests):
                 steps_to_add += [ArchiveTestResults(), UploadTestResults(identifier='rerun'), ExtractTestResults(identifier='rerun')]
                 self.build.addStepsAfterCurrentStep(steps_to_add)
                 return WARNINGS
-            steps_to_add += [
-                ArchiveTestResults(),
-                UploadTestResults(identifier='rerun'),
-                ExtractTestResults(identifier='rerun'),
-                RevertAppliedChanges(),
-                CleanWorkingDirectory(),
-                ValidateChange(verifyBugClosed=False, addURLs=False),
-                CompileWebKitWithoutChange(retry_build_on_failure=True),
-                ValidateChange(verifyBugClosed=False, addURLs=False),
-                KillOldProcesses(),
-                RunWebKitTestsWithoutChange()
-            ]
-            self.build.addStepsAfterCurrentStep(steps_to_add)
+
+            # The significant additional build time isn't worth it on Windows, we'd rather
+            # the worker start on another job in the queue.
+            if platform not in ('win'):
+                steps_to_add += [
+                    ArchiveTestResults(),
+                    UploadTestResults(identifier='rerun'),
+                    ExtractTestResults(identifier='rerun'),
+                    RevertAppliedChanges(),
+                    CleanWorkingDirectory(),
+                    ValidateChange(verifyBugClosed=False, addURLs=False),
+                    CompileWebKitWithoutChange(retry_build_on_failure=True),
+                    ValidateChange(verifyBugClosed=False, addURLs=False),
+                    KillOldProcesses(),
+                    RunWebKitTestsWithoutChange()
+                ]
+                self.build.addStepsAfterCurrentStep(steps_to_add)
         return rc
 
     @defer.inlineCallbacks

commit 284fa089fb60d33ff16273e7b863c9c420e790c2

REGRESSION(305111@main): TypeError: 'in <string>' requires string as left operand, not NoneType rdar://167867009 https://bugs.webkit.org/show_bug.cgi?id=305223
Reviewed by NOBODY (OOPS!).

Resolve an error introduced in 305111@main that broke our services/ EWS unit tests.

* Tools/CISupport/ews-build/steps.py:
(RunWebKitTests.evaluateCommand):
(ReRunWebKitTests.evaluateCommand):

Tools/CISupport/ews-build/steps.py
@@ -4354,7 +4354,7 @@ class RunWebKitTests(shell.Test, AddToLogMixin, ShellMixin):
                     ReRunWebKitTests(),
                 ]
             else:
-                if platform not in ('win'):
+                if platform not in ('win',):
                     steps_to_add += [
                         RevertAppliedChanges(),
                         CleanWorkingDirectory(),
@@ -4527,7 +4527,7 @@ class ReRunWebKitTests(RunWebKitTests):
 
             # The significant additional build time isn't worth it on Windows, we'd rather
             # the worker start on another job in the queue.
-            if platform not in ('win'):
+            if platform not in ('win',):
                 steps_to_add += [
                     ArchiveTestResults(),
                     UploadTestResults(identifier='rerun'),