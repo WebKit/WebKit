589e9b9e1414da6f333be3d682c4da4f588c77c7
ronan-apple <ronan.turner@apple.com>
2026-01-13 12:49:32 +0000

    Fix ContentRuleList HTTPS upgrade behaviour with Enhanced Security https://bugs.webkit.org/show_bug.cgi?id=305379 rdar://168058731
    Reviewed by NOBODY (OOPS!).
    
    When the Enhanced Security heuristics flag is enabled by default, a test
    failure occurred in:
    
      * WebKit.RedirectToPlaintextHTTPSUpgrade
    
    This test ensures that a HTTPS site performing a same-site redirect to
    plaintext HTTP does not get upgraded by the ContentRuleList rules, as this
    is an explicit HTTP redirect.
    
    When the Enhanced Security heuristics flag is enabled, the redirect to the
    HTTP site causes us to process swap and load this in an Enhanced Security
    process. In doing so, we change the load code path taken and lost information
    that this explicit redirect had occurred.
    
    To address this, this change explicitly checks for us having continued the
    load due to a navigation policy change, and uses the original request URL,
    if different, when processing the ContentRuleList for this load.
    
    We also now have an explicit test that checks this case.
    
    Test: Tools/TestWebKitAPI/Tests/WebKitCocoa/EnhancedSecurityPolicies.mm
    
    * Source/WebCore/loader/DocumentLoader.h:
    (WebCore::DocumentLoader::isContinuingLoadAfterNavigationPolicyDecision const):
    (WebCore::DocumentLoader::setIsContinuingLoadAfterNavigationPolicyDecision):
    * Source/WebCore/loader/FrameLoader.cpp:
    (WebCore::FrameLoader::load):
    * Source/WebCore/loader/cache/CachedResourceLoader.cpp:
    (WebCore::CachedResourceLoader::requestResource):
    * Tools/TestWebKitAPI/Tests/WebKitCocoa/EnhancedSecurityPolicies.mm:
    (runHttpsToSameSiteHttpExplicitRedirect):

--- Source/WebCore/loader/FrameLoader.cpp
+++ Source/WebCore/loader/FrameLoader.cpp
@@ -1750,6 +1750,7 @@ void FrameLoader::load(FrameLoadRequest&& request, std::optional<NavigationReque
     loader->setIsContentRuleListRedirect(request.isContentRuleListRedirect());
     loader->setIsRequestFromClientOrUserInput(request.isRequestFromClientOrUserInput());
     loader->setIsContinuingLoadAfterProvisionalLoadStarted(request.shouldTreatAsContinuingLoad() == ShouldTreatAsContinuingLoad::YesAfterProvisionalLoadStarted);
+    loader->setIsContinuingLoadAfterNavigationPolicyDecision(request.shouldTreatAsContinuingLoad() == ShouldTreatAsContinuingLoad::YesAfterNavigationPolicyDecision);
     if (crossSiteRequester)
         loader->setCrossSiteRequester(WTF::move(*crossSiteRequester));
 
