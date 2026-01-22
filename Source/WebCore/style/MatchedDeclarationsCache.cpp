70cb4c44ac6de4fcad64cb8550fc444611819a40
David Kilzer <ddkilzer@apple.com>
2026-01-22 10:45:32 -0800

    Cherry-pick 45684cc85f9f. rdar://168674879
        [CSS] Crash due to release assert in HashTable when MatchedDeclarationsCache::computeHash() returns 0xFFFFFFFF
        <https://bugs.webkit.org/show_bug.cgi?id=305864>
        <rdar://118164567>
    
        Reviewed by Chris Dumez.
    
        MatchedDeclarationsCache::computeHash() calls avalancheBits(hash) to
        compute a hash, which may return the deleted value.  This would lead to
        the release assert firing when attempting to add the deleted value to a
        HashTable.
    
        The fix is to call AlreadyHashed::avoidDeletedValue() to avoid using the
        deleted value as a hash key.
    
        No new tests since this hash key is computed using the value of pointers
        to memory addresses, and thus is nondeterministic.
    
        * Source/WebCore/style/MatchedDeclarationsCache.cpp:
        (WebCore::Style::MatchedDeclarationsCache::computeHash):
        - Use WTF::AlreadyHashed::avoidDeletedValue() to avoid returning the
          value of a deleted hash table entry.
    
        Canonical link: https://commits.webkit.org/305989@main
    
    Identifier: 305413.87@safari-7624-branch

--- Source/WebCore/style/MatchedDeclarationsCache.cpp
+++ Source/WebCore/style/MatchedDeclarationsCache.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  * Copyright (C) 2004-2005 Allan Sandfeld Jensen (kde@carewolf.com)
  * Copyright (C) 2006, 2007 Nicholas Shanks (webkit@nickshanks.com)
- * Copyright (C) 2005-2019 Apple Inc. All rights reserved.
+ * Copyright (C) 2005-2026 Apple Inc. All rights reserved.
  * Copyright (C) 2007 Alexey Proskuryakov <ap@webkit.org>
  * Copyright (C) 2007, 2008 Eric Seidel <eric@webkit.org>
  * Copyright (C) 2008, 2009 Torch Mobile Inc. All rights reserved. (http://www.torchmobile.com/)
@@ -133,7 +133,7 @@ unsigned MatchedDeclarationsCache::computeHash(const MatchResult& matchResult, c
         if (allNonCacheable)
             return 0;
     }
-    return WTF::computeHash(matchResult, &inheritedCustomProperties);
+    return AlreadyHashed::avoidDeletedValue(WTF::computeHash(matchResult, &inheritedCustomProperties));
 }
 
 std::optional<MatchedDeclarationsCache::Result> MatchedDeclarationsCache::find(unsigned hash, const MatchResult& matchResult, const Style::CustomPropertyData& inheritedCustomProperties, const RenderStyle& parentStyle)
