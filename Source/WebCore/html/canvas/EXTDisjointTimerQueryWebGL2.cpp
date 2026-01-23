02a742c41b1cfd33d1dce5bd444490f915756aaf
Chris Dumez <cdumez@apple.com>
2026-01-23 15:35:40 -0800

    Cherry-pick 301765.347@safari-7623-branch (fabe69c2dcb4). rdar://168334301
        Unsafe argument capture in EXTDisjointTimerQueryWebGL2::queryCounterEXT()
        https://bugs.webkit.org/show_bug.cgi?id=303348
        rdar://165125247
    
        Reviewed by Brent Fulgham.
    
        The lambda runs asynchronously and it is thus unsafe to capture by reference
        here. We need to capture a smart pointer to the query to make sure it is kept
        alive.
    
        * Source/WebCore/html/canvas/EXTDisjointTimerQueryWebGL2.cpp:
        (WebCore::EXTDisjointTimerQueryWebGL2::queryCounterEXT):
    
        Identifier: 301765.347@safari-7623-branch
    
    Identifier: 305413.123@safari-7624-branch

--- Source/WebCore/html/canvas/EXTDisjointTimerQueryWebGL2.cpp
+++ Source/WebCore/html/canvas/EXTDisjointTimerQueryWebGL2.cpp
@@ -77,8 +77,8 @@ void EXTDisjointTimerQueryWebGL2::queryCounterEXT(WebGLQuery& query, GCGLenum ta
     context->graphicsContextGL()->queryCounterEXT(query.object(), target);
 
     // A query's result must not be made available until control has returned to the user agent's main loop.
-    context->protectedScriptExecutionContext()->checkedEventLoop()->queueMicrotask([&] {
-        query.makeResultAvailable();
+    context->protectedScriptExecutionContext()->checkedEventLoop()->queueMicrotask([query = Ref { query }] {
+        query->makeResultAvailable();
     });
 }
 
