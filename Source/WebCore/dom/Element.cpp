a1d9944acf9d1eb1f3c06e8d55be052127e9099e
Timothy Hatcher <timothy@apple.com>
2026-01-23 13:01:38 -0800

    REGRESSION(305413.31@safari-7624-branch): Broke editing/pasteboard/img-srcset-copy-paste-canonicalization.html https://webkit.org/b/306036 rdar://168512100
    Reviewed by Brian Weinstein.
    
    ResolveURLs::YesExcludingURLsForPrivacy was incorrectly not completing file URLs
    like ResolveURLs::Yes does. Drop the extra branch to match Yes.
    
    * LayoutTests/TestExpectations: Remove Failure exception.
    * Source/WebCore/dom/Element.cpp:
    (WebCore::Element::resolveURLStringIfNeeded const):
    
    Identifier: 305413.117@safari-7624-branch

--- Source/WebCore/dom/Element.cpp
+++ Source/WebCore/dom/Element.cpp
@@ -5934,9 +5934,7 @@ String Element::resolveURLStringIfNeeded(const String& urlString, ResolveURLs re
     case ResolveURLs::YesExcludingURLsForPrivacy: {
         if (document().shouldMaskURLForBindings(completeURL))
             return maskedURLStringForBindings.get();
-        if (!document().url().protocolIsFile())
-            return completeURL.string();
-        break;
+        return completeURL.string();
     }
 
     case ResolveURLs::NoExcludingURLsForPrivacy:
