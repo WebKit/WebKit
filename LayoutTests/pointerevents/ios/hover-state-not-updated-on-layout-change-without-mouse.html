WebKit/WebKit

commit edb7dbd59d799e010a5acdcea6a0821df9f14a79

Incorrect boundary events behavior when hit test target changes under stationary pointing device https://bugs.webkit.org/show_bug.cgi?id=298763 rdar://160147423
Reviewed by Abrar Rahman Protyasha.

We usually fail to send boundary events when the hit test target underneath a stationary pointing
device changes due to layout updates. The only exception is when the pointer first moves into an
element, and that element is then hidden. If the element is then shown and hidden again while
the pointer remains stationary, boundary events will only fire on the first hide. Boundary
events do eventually fire, but only when the mouse moves.

During post layout tasks, we now schedule mouse event target updates which checks if the mouse
is still over the same element it was previously. If the element has changed, we call
`updateMouseEventTargetNode` with a synthetic `PlatformMouseEvent` so that the boundary events are
dispatched, and we update the hover state of the element the mouse is over as well.

As a result of these changes, we no longer need to call `dispatchFakeMouseMoveEventSoon` during
style changes.

The web-platform-test `pointerevent_pointerout_no_pointer_movement.html` now passes when run using
webdriver as a result, but the test still times out in WKTR ever since
https://github.com/web-platform-tests/wpt/commit/01186a0a3449e8c7f89553abe2524eced3bc30a5 for
reasons not yet determined. A new layout test has been added to test this behavior in the meantime:
pointerevents/mouse/pointer-boundary-events.html

Test: pointerevents/mouse/pointer-boundary-events.html
* LayoutTests/fast/events/ghostly-mousemoves-in-subframe-expected.txt: Renamed from LayoutTests/platform/glib/fast/events/ghostly-mousemoves-in-subframe-expected.txt.
* LayoutTests/imported/w3c/web-platform-tests/pointerevents/pointerevent_boundary_events_attributes_during_drag-expected.txt:
* LayoutTests/imported/w3c/web-platform-tests/pointerevents/pointerevent_pointerout_no_pointer_movement-expected.txt:

Source/WebCore/page/EventHandler.cpp
@@ -408,6 +408,8 @@ EventHandler::EventHandler(LocalFrame& frame)
 #if ENABLE(IMAGE_ANALYSIS)
     , m_textRecognitionHoverTimer(*this, &EventHandler::textRecognitionHoverTimerFired, 250_ms)
 #endif
+    , m_mouseEventTargetUpdateTimer(*this, &EventHandler::updateMouseEventTargetAfterLayoutIfNeeded)
+    , m_mouseEventTargetFinalUpdateTimer(*this, &EventHandler::updateMouseEventTargetAfterLayoutIfNeeded)
     , m_autoscrollController(makeUniqueRef<AutoscrollController>())
 #if !ENABLE(IOS_TOUCH_EVENTS)
     , m_fakeMouseMoveEventTimer(*this, &EventHandler::fakeMouseMoveEventTimerFired)
@@ -426,6 +428,8 @@ EventHandler::~EventHandler()
 #if ENABLE(CURSOR_VISIBILITY)
     ASSERT(!m_autoHideCursorTimer.isActive());
 #endif
+    ASSERT(!m_mouseEventTargetUpdateTimer.isActive());
+    ASSERT(!m_mouseEventTargetFinalUpdateTimer.isActive());
 }
     
 #if ENABLE(DRAG_SUPPORT)
@@ -461,6 +465,8 @@ void EventHandler::clear()
 #if ENABLE(IMAGE_ANALYSIS)
     m_textRecognitionHoverTimer.stop();
 #endif
+    m_mouseEventTargetUpdateTimer.stop();
+    m_mouseEventTargetFinalUpdateTimer.stop();
     m_resizeLayer = nullptr;
     clearElementUnderMouse();
     m_lastElementUnderMouse = nullptr;
@@ -2936,6 +2942,20 @@ RefPtr<Element> EventHandler::textRecognitionCandidateElement() const
 
 #endif // ENABLE(IMAGE_ANALYSIS)
 
+static RefPtr<Element> getElementOrAncestorElementForNode(Node* targetNode)
+{
+    RefPtr<Element> targetElement;
+    while (targetNode) {
+        if (RefPtr asElement = dynamicDowncast<Element>(*targetNode)) {
+            targetElement = asElement;
+            break;
+        }
+        targetNode = targetNode->parentInComposedTree();
+    }
+
+    return targetElement;
+}
+
 void EventHandler::updateMouseEventTargetNode(const AtomString& eventType, Node* targetNode, const PlatformMouseEvent& platformMouseEvent, FireMouseOverOut fireMouseOverOut)
 {
     Ref frame = m_frame.get();
@@ -2946,13 +2966,7 @@ void EventHandler::updateMouseEventTargetNode(const AtomString& eventType, Node*
         targetElement = m_capturingMouseEventsElement.get();
     else {
         // If the target node is a non-element, dispatch on the parent. <rdar://problem/4196646>
-        while (targetNode) {
-            if (auto* asElement = dynamicDowncast<Element>(*targetNode)) {
-                targetElement = asElement;
-                break;
-            }
-            targetNode = targetNode->parentInComposedTree();
-        }
+        targetElement = getElementOrAncestorElementForNode(targetNode);
     }
 
     m_elementUnderMouse = targetElement;
@@ -3050,6 +3064,75 @@ void EventHandler::clearElementUnderMouse()
         return;
 
     imageOverlayController->elementUnderMouseDidChange(protectedFrame(), nullptr);
+}
+
+void EventHandler::scheduleMouseEventTargetUpdateAfterLayout()
+{
+    // If the element underneath the mouse is changing more frequently than every 200ms for
+    // a period of time, we want to fire boundary events both during the period of updates,
+    // and afterwards. For this reason, two timers are used.
+    if (!m_mouseEventTargetUpdateTimer.isActive())
+        m_mouseEventTargetUpdateTimer.startOneShot(200_ms);
+
+    m_mouseEventTargetFinalUpdateTimer.startOneShot(200_ms);
+}
+
+static constexpr OptionSet hoverTimerHitType { HitTestRequest::Type::Move, HitTestRequest::Type::DisallowUserAgentShadowContent };
+
+void EventHandler::updateMouseEventTargetAfterLayoutIfNeeded()
+{
+    if (!m_lastKnownMousePosition.has_value())
+        return;
+
+    Ref frame = m_frame.get();
+    RefPtr view = frame->view();
+    if (!view || !m_elementUnderMouse)
+        return;
+
+    if (!m_elementUnderMouse)
+        return;
+
+    RefPtr document = frame->document();
+
+    // Clear element tracking if there's a document mismatch.
+    if ((&m_elementUnderMouse->document() != document)
+        || (m_lastElementUnderMouse && &m_lastElementUnderMouse->document() != document)) {
+        m_elementUnderMouse = nullptr;
+        m_lastElementUnderMouse = nullptr;
+        return;
+    }
+
+    RefPtr<Element> targetElement;
+
+    // If pointer capture isn't active, we'll need to hit test to determine our target element.
+    // If it is active, our target is the capturing element.
+    if (!m_capturingMouseEventsElement) {
+        const auto mousePosition = view->windowToContents(valueOrDefault(m_lastKnownMousePosition));
+        HitTestResult hitResult((LayoutPoint(mousePosition)));
+
+        // We use the same hit test as the hover timer because we may need to update
+        // the hover state in this method as well.
+        if (document)
+            document->hitTest(hoverTimerHitType, hitResult);
+
+        targetElement = getElementOrAncestorElementForNode(hitResult.innerNode());
+    } else
+        targetElement = m_capturingMouseEventsElement.get();
+
+    if (targetElement && (targetElement != m_elementUnderMouse)) {
+        // Create a synthetic mouse event and update the mouse event target node in order to trigger
+        // boundary event processing.
+        auto modifiers = PlatformKeyboardEvent::currentStateOfModifierKeys();
+        PlatformMouseEvent syntheticEvent(valueOrDefault(m_lastKnownMousePosition), m_lastKnownMouseGlobalPosition,
+            MouseButton::None, PlatformEvent::Type::NoType, 0, modifiers, MonotonicTime::now(), 0, SyntheticClickType::NoTap);
+
+        // EventHandler updates scrollable areas when the element under the mouse changes as a result of the
+        // mouse moving. In this case, the mouse did not move, but the element under the mouse still changed,
+        // so we treat it internally as a mousemove event.
+        updateMouseEventTargetNode(eventNames().mousemoveEvent, targetElement.get(), syntheticEvent, FireMouseOverOut::Yes);
+        if (document)
+            document->updateHoverActiveState(hoverTimerHitType, targetElement.get());
+    }
 }
 
 void EventHandler::notifyScrollableAreasOfMouseEvents(const AtomString& eventType, Element* lastElementUnderMouse, Element* elementUnderMouse)
@@ -3882,9 +3965,8 @@ void EventHandler::hoverTimerFired()
     if (RefPtr document = frame->document()) {
         if (RefPtr view = frame->view()) {
             HitTestResult result(view->windowToContents(flooredIntPoint(valueOrDefault(m_lastKnownMousePosition))));
-            constexpr OptionSet<HitTestRequest::Type> hitType { HitTestRequest::Type::Move, HitTestRequest::Type::DisallowUserAgentShadowContent };
-            document->hitTest(hitType, result);
-            document->updateHoverActiveState(hitType, result.targetElement());
+            document->hitTest(hoverTimerHitType, result);
+            document->updateHoverActiveState(hoverTimerHitType, result.targetElement());
         }
     }
 }

Source/WebCore/page/EventHandler.h
@@ -407,6 +407,9 @@ public:
 
     std::optional<RemoteUserInputEventData> userInputEventDataForRemoteFrame(const RemoteFrame*, const IntPoint&);
 
+    WEBCORE_EXPORT void updateMouseEventTargetAfterLayoutIfNeeded();
+    WEBCORE_EXPORT void scheduleMouseEventTargetUpdateAfterLayout();
+
 private:
 #if ENABLE(DRAG_SUPPORT)
     static DragState& dragState();
@@ -643,6 +646,8 @@ private:
 #if ENABLE(IMAGE_ANALYSIS)
     DeferrableOneShotTimer m_textRecognitionHoverTimer;
 #endif
+    Timer m_mouseEventTargetUpdateTimer;
+    Timer m_mouseEventTargetFinalUpdateTimer;
     const UniqueRef<AutoscrollController> m_autoscrollController;
     SingleThreadWeakPtr<RenderLayer> m_resizeLayer;
 

LayoutTests/fast/events/ghostly-mousemoves-in-subframe-expected.txt
@@ -10,7 +10,6 @@ clickable_div_in_subframe got mousemove at 14,14
 overlapping_div_in_main_frame got mousemove at 16,16
 main_frame got mousemove at 400,400
 main_frame got click at 400,400
-main_frame got mousemove at 400,400
 PASS successfullyParsed is true
 
 TEST COMPLETE

commit 2f0a2363ac5696adc6f3c971851101aed5173a58

Cherry-pick fb2234a1edb8. rdar://168270308
    REGRESSION(300277@main): [iOS] music.apple.com: Hover effect on playlist rolls with scrolling
    https://bugs.webkit.org/show_bug.cgi?id=304955
    rdar://165386975

    Reviewed by Wenson Hsieh and Lily Spiniolas.

    Scrolling on sites like music.apple.com caused hover states to
    inappropriately change as elements moved under the stored "mouse
    position" from wheel events. This made it appear as if different items
    were being "selected" while scrolling.

    The regression was introduced in 300277@main, which added
    updateMouseEventTargetAfterLayoutIfNeeded() to fire boundary events when
    layout changes cause different elements to be under the pointer. However,
    on iOS (or other touch-primary platforms), if we interact on the web
    page through touch, this sets m_lastKnownMousePosition because of the
    synthetic click codepath. Once set, even though there is no real pointer
    to track, we trigger hover semantics (since we hit test over regular web
    content).

    The fix adds an early return when the last known mouse position is
    sourced from touch, rather than a pointing device or wheel event
    handling. This preserves correct behavior on iPads with connected
    mice or trackpads while preventing inappropriate hover updates on
    touch-only devices.

    Test: pointerevents/ios/hover-state-not-updated-on-layout-change-without-mouse.html

    * LayoutTests/pointerevents/ios/hover-state-not-updated-on-layout-change-without-mouse-expected.txt: Added.
    * LayoutTests/pointerevents/ios/hover-state-not-updated-on-layout-change-without-mouse.html: Added.
    * Source/WebCore/page/EventHandler.cpp:
    (WebCore::mousePositionSource):
    (WebCore::EventHandler::handleMousePressEvent):
    (WebCore::EventHandler::handleMouseDoubleClickEvent):
    (WebCore::EventHandler::handleMouseMoveEvent):
    (WebCore::EventHandler::handleMouseReleaseEvent):

Source/WebCore/page/EventHandler.cpp
@@ -1973,6 +1973,12 @@ static Scrollbar* scrollbarForMouseEvent(const MouseEventWithHitTestResults& mou
 
 }
 
+static LastKnownMousePositionSource mousePositionSource(const PlatformMouseEvent& event)
+{
+    using enum LastKnownMousePositionSource;
+    return event.syntheticClickType() == SyntheticClickType::NoTap ? Mouse : Touch;
+}
+
 HandleUserInputEventResult EventHandler::handleMousePressEvent(const PlatformMouseEvent& platformMouseEvent)
 {
     Ref frame = m_frame.get();
@@ -2016,7 +2022,7 @@ HandleUserInputEventResult EventHandler::handleMousePressEvent(const PlatformMou
 
     m_mousePressed = true;
     m_capturesDragging = true;
-    setLastKnownMousePosition(platformMouseEvent.position(), platformMouseEvent.globalPosition());
+    setLastKnownMousePosition(platformMouseEvent.position(), platformMouseEvent.globalPosition(), mousePositionSource(platformMouseEvent));
     m_mouseDownTimestamp = platformMouseEvent.timestamp();
 #if ENABLE(DRAG_SUPPORT)
     m_mouseDownMayStartDrag = false;
@@ -2159,7 +2165,7 @@ bool EventHandler::handleMouseDoubleClickEvent(const PlatformMouseEvent& platfor
 
     // We get this instead of a second mouse-up 
     m_mousePressed = false;
-    setLastKnownMousePosition(platformMouseEvent.position(), platformMouseEvent.globalPosition());
+    setLastKnownMousePosition(platformMouseEvent.position(), platformMouseEvent.globalPosition(), mousePositionSource(platformMouseEvent));
 
     constexpr OptionSet<HitTestRequest::Type> hitType { HitTestRequest::Type::Release, HitTestRequest::Type::DisallowUserAgentShadowContent };
     MouseEventWithHitTestResults mouseEvent = prepareMouseEvent(hitType, platformMouseEvent);
@@ -2314,7 +2320,7 @@ HandleUserInputEventResult EventHandler::handleMouseMoveEvent(const PlatformMous
     }
 #endif
 
-    setLastKnownMousePosition(platformMouseEvent.position(), platformMouseEvent.globalPosition());
+    setLastKnownMousePosition(platformMouseEvent.position(), platformMouseEvent.globalPosition(), mousePositionSource(platformMouseEvent));
 
     if (m_hoverTimer.isActive())
         m_hoverTimer.stop();
@@ -2540,7 +2546,7 @@ HandleUserInputEventResult EventHandler::handleMouseReleaseEvent(const PlatformM
 #endif
 
     m_mousePressed = false;
-    setLastKnownMousePosition(platformMouseEvent.position(), platformMouseEvent.globalPosition());
+    setLastKnownMousePosition(platformMouseEvent.position(), platformMouseEvent.globalPosition(), mousePositionSource(platformMouseEvent));
 
     if (m_svgPan) {
         m_svgPan = false;
@@ -2619,7 +2625,7 @@ bool EventHandler::handleMouseForceEvent(const PlatformMouseEvent& event)
     }
 #endif
 
-    setLastKnownMousePosition(event.position(), event.globalPosition());
+    setLastKnownMousePosition(event.position(), event.globalPosition(), mousePositionSource(event));
 
     OptionSet<HitTestRequest::Type> hitType { HitTestRequest::Type::DisallowUserAgentShadowContent };
 
@@ -3152,6 +3158,9 @@ void EventHandler::updateMouseEventTargetAfterLayoutIfNeeded()
     if (!view || !m_elementUnderMouse)
         return;
 
+    if (m_lastKnownMousePositionSource == LastKnownMousePositionSource::Touch)
+        return;
+
     RefPtr document = frame->document();
 
     // Clear element tracking if there's a document mismatch.
@@ -3519,7 +3528,7 @@ HandleUserInputEventResult EventHandler::handleWheelEventInternal(const Platform
     auto allowsScrollingState = SetForScope(m_currentWheelEventAllowsScrolling, processingSteps.contains(WheelEventProcessingSteps::SynchronousScrolling));
     
     setFrameWasScrolledByUser();
-    setLastKnownMousePosition(event.position(), event.globalPosition());
+    setLastKnownMousePosition(event.position(), event.globalPosition(), LastKnownMousePositionSource::Wheel);
 
     if (m_frame->isMainFrame()) {
         RefPtr page = m_frame->page();
@@ -5605,10 +5614,11 @@ bool EventHandler::dispatchSyntheticTouchEventIfEnabled(const PlatformMouseEvent
 }
 #endif // ENABLE(TOUCH_EVENTS)
 
-void EventHandler::setLastKnownMousePosition(const DoublePoint& position, const DoublePoint& globalPosition)
+void EventHandler::setLastKnownMousePosition(const DoublePoint& position, const DoublePoint& globalPosition, std::optional<LastKnownMousePositionSource>&& source)
 {
     m_lastKnownMousePosition = position;
     m_lastKnownMouseGlobalPosition = globalPosition;
+    m_lastKnownMousePositionSource = WTF::move(source);
 }
 
 void EventHandler::setImmediateActionStage(ImmediateActionStage stage)

Source/WebCore/page/EventHandler.h
@@ -140,6 +140,7 @@ extern const unsigned InvalidTouchIdentifier;
 
 enum AppendTrailingWhitespace { ShouldAppendTrailingWhitespace, DontAppendTrailingWhitespace };
 enum CheckDragHysteresis { ShouldCheckDragHysteresis, DontCheckDragHysteresis };
+enum class LastKnownMousePositionSource : uint8_t { Mouse, Wheel, Touch };
 
 class EventHandler final : public CanMakeCheckedPtr<EventHandler> {
     WTF_MAKE_TZONE_ALLOCATED(EventHandler);
@@ -242,7 +243,7 @@ public:
     void defaultWheelEventHandler(Node*, WheelEvent&);
     void wheelEventWasProcessedByMainThread(const PlatformWheelEvent&, OptionSet<EventHandling>);
 
-    WEBCORE_EXPORT void setLastKnownMousePosition(const DoublePoint& position, const DoublePoint& globalPosition);
+    WEBCORE_EXPORT void setLastKnownMousePosition(const DoublePoint& position, const DoublePoint& globalPosition, std::optional<LastKnownMousePositionSource>&& = std::nullopt);
 
     bool handlePasteGlobalSelection();
 
@@ -702,6 +703,7 @@ private:
 
     std::optional<DoublePoint> m_lastKnownMousePosition; // Same coordinates as PlatformMouseEvent::position().
     DoublePoint m_lastKnownMouseGlobalPosition;
+    std::optional<LastKnownMousePositionSource> m_lastKnownMousePositionSource;
     IntPoint m_mouseDownContentsPosition;
     MonotonicTime m_mouseDownTimestamp;
     PlatformMouseEvent m_mouseDownEvent;

Source/WebKit/Scripts/webkit/messages.py
@@ -1234,6 +1234,7 @@ def headers_for_type(type, for_implementation_file=False):
         'WebCore::IDBResourceObjectIdentifier': ['<WebCore/IDBResourceIdentifier.h>'],
         'WebCore::ISOWebVTTCue': ['<WebCore/ISOVTTCue.h>'],
         'WebCore::KeypressCommand': ['<WebCore/KeyboardEvent.h>'],
+        'WebCore::LastKnownMousePositionSource': ['<WebCore/EventHandler.h>'],
         'WebCore::LastNavigationWasAppInitiated': ['<WebCore/ServiceWorkerClientData.h>'],
         'WebCore::LegacyCDMSessionClient::MediaKeyErrorCode': ['<WebCore/LegacyCDMSession.h>'],
         'WebCore::LineCap': ['<WebCore/GraphicsTypes.h>'],

Source/WebKit/Shared/WebCoreArgumentCoders.serialization.in
@@ -9220,3 +9220,10 @@ header: <WebCore/ResolvedCaptionDisplaySettingsOptions.h>
     std::optional<WebCore::ResolvedCaptionDisplaySettingsOptionsXPositionArea> xPositionArea;
     std::optional<WebCore::ResolvedCaptionDisplaySettingsOptionsYPositionArea> yPositionArea;
 };
+
+header: <WebCore/EventHandler.h>
+[CustomHeader] enum class WebCore::LastKnownMousePositionSource : uint8_t {
+    Mouse,
+    Wheel,
+    Touch
+};

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -231,6 +231,7 @@
 #include <WebCore/DragController.h>
 #include <WebCore/DragData.h>
 #include <WebCore/ElementContext.h>
+#include <WebCore/EventHandler.h>
 #include <WebCore/EventNames.h>
 #include <WebCore/ExceptionCode.h>
 #include <WebCore/ExceptionData.h>
@@ -4297,7 +4298,7 @@ void WebPageProxy::continueWheelEventHandling(const WebWheelEvent& wheelEvent, c
     if (!result.needsMainThreadProcessing()) {
         if (m_mainFrame && wheelEvent.phase() == WebWheelEvent::Phase::Began) {
             // When wheel events are handled entirely in the UI process, we still need to tell the web process where the mouse is for cursor updates.
-            sendToProcessContainingFrame(m_mainFrame->frameID(), Messages::WebPage::SetLastKnownMousePosition(m_mainFrame->frameID(), wheelEvent.position(), wheelEvent.globalPosition()));
+            sendToProcessContainingFrame(m_mainFrame->frameID(), Messages::WebPage::SetLastKnownMousePosition(m_mainFrame->frameID(), wheelEvent.position(), wheelEvent.globalPosition(), WebCore::LastKnownMousePositionSource::Wheel));
         }
 
         wheelEventHandlingCompleted(result.wasHandled);

Source/WebKit/WebProcess/WebPage/WebPage.cpp
@@ -3685,13 +3685,13 @@ void WebPage::mouseEvent(FrameIdentifier frameID, const WebMouseEvent& mouseEven
 #endif
 }
 
-void WebPage::setLastKnownMousePosition(WebCore::FrameIdentifier frameID, const DoublePoint& eventPoint, const DoublePoint& globalPoint)
+void WebPage::setLastKnownMousePosition(WebCore::FrameIdentifier frameID, const DoublePoint& eventPoint, const DoublePoint& globalPoint, std::optional<WebCore::LastKnownMousePositionSource>&& source)
 {
     RefPtr frame = WebProcess::singleton().webFrame(frameID);
     if (!frame || !frame->coreLocalFrame() || !frame->coreLocalFrame()->view())
         return;
 
-    frame->coreLocalFrame()->eventHandler().setLastKnownMousePosition(eventPoint, globalPoint);
+    frame->coreLocalFrame()->eventHandler().setLastKnownMousePosition(eventPoint, globalPoint, WTF::move(source));
 }
 
 void WebPage::startDeferringResizeEvents()

Source/WebKit/WebProcess/WebPage/WebPage.h
@@ -245,6 +245,7 @@ enum class HighlightRequestOriginatedInApp : bool;
 enum class ImageDecodingError : uint8_t;
 enum class InputMode : uint8_t;
 enum class IsLoggedIn : uint8_t;
+enum class LastKnownMousePositionSource : uint8_t;
 enum class LayerTreeAsTextOptions : uint16_t;
 enum class LayoutMilestone : uint16_t;
 enum class LinkDecorationFilteringTrigger : uint8_t;
@@ -2278,7 +2279,7 @@ private:
     void mouseEvent(WebCore::FrameIdentifier, const WebMouseEvent&, std::optional<Vector<SandboxExtensionHandle>>&& sandboxExtensions);
     void keyEvent(WebCore::FrameIdentifier, const WebKeyboardEvent&);
 
-    void setLastKnownMousePosition(WebCore::FrameIdentifier, const WebCore::DoublePoint&, const WebCore::DoublePoint&);
+    void setLastKnownMousePosition(WebCore::FrameIdentifier, const WebCore::DoublePoint&, const WebCore::DoublePoint&, std::optional<WebCore::LastKnownMousePositionSource>&& = std::nullopt);
 
 #if ENABLE(IOS_TOUCH_EVENTS)
     void touchEventSync(const WebTouchEvent&, CompletionHandler<void(bool)>&&);

Source/WebKit/WebProcess/WebPage/WebPage.messages.in
@@ -59,7 +59,7 @@ messages -> WebPage WantsAsyncDispatchMessage {
     ExecuteEditCommandWithCallback(String name, String argument) -> ()
     KeyEvent(WebCore::FrameIdentifier frameID, WebKit::WebKeyboardEvent event)
     MouseEvent(WebCore::FrameIdentifier frameID, WebKit::WebMouseEvent event, std::optional<Vector<WebKit::SandboxExtensionHandle>> sandboxExtensions)
-    SetLastKnownMousePosition(WebCore::FrameIdentifier frameID, WebCore::DoublePoint eventPoint, WebCore::DoublePoint globalPoint);
+    SetLastKnownMousePosition(WebCore::FrameIdentifier frameID, WebCore::DoublePoint eventPoint, WebCore::DoublePoint globalPoint, enum:uint8_t std::optional<WebCore::LastKnownMousePositionSource> source);
 
 #if PLATFORM(IOS_FAMILY)
     SetSceneIdentifier(String sceneIdentifier)

LayoutTests/pointerevents/ios/hover-state-not-updated-on-layout-change-without-mouse-expected.txt
@@ -1,0 +1,12 @@
+Test that hover states are not updated after layout changes on iOS when no mouse device is connected.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+
+Triggering layout change (expanding spacer by 120px)...
+PASS Hover states correctly remained unchanged during layout change without mouse device
+PASS successfullyParsed is true
+
+TEST COMPLETE
+Item 1Item 2

LayoutTests/pointerevents/ios/hover-state-not-updated-on-layout-change-without-mouse.html
@@ -1,0 +1,102 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ useFlexibleViewport=true ] -->
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="../../resources/js-test.js"></script>
+<script src="../../resources/ui-helper.js"></script>
+<style>
+#spacer {
+    height: 0px;
+}
+
+#item1, #item2 {
+    position: absolute;
+    left: 100px;
+    width: 200px;
+    height: 100px;
+    background-color: lightblue;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+}
+
+#item1 {
+    top: 100px;
+}
+
+#item2 {
+    top: 220px;
+}
+
+#item1:hover, #item2:hover {
+    background-color: yellow;
+}
+</style>
+</head>
+<body>
+<div id="spacer"></div>
+<div id="item1">Item 1</div>
+<div id="item2">Item 2</div>
+
+<script>
+description("Test that hover states are not updated after layout changes on iOS when no mouse device is connected.");
+window.jsTestIsAsync = true;
+
+if (!window.testRunner || !window.eventSender) {
+    testFailed("This test requires testRunner and eventSender");
+    finishJSTest();
+}
+
+function checkHovered(elementId) {
+    const elem = document.getElementById(elementId);
+    const bgColor = window.getComputedStyle(elem).backgroundColor;
+    return bgColor === 'rgb(255, 255, 0)';
+}
+
+async function runTest() {
+    try {
+        testRunner.setHasMouseDeviceForTesting(false);
+        await UIHelper.renderingUpdate();
+
+        const item1 = document.getElementById('item1');
+        const rect1 = item1.getBoundingClientRect();
+        const pointerX = rect1.left + rect1.width / 2;
+        const pointerY = rect1.top + rect1.height / 2;
+
+        await eventSender.asyncMouseMoveTo(pointerX, pointerY);
+        await UIHelper.renderingUpdate();
+
+        debug("\nTriggering layout change (expanding spacer by 120px)...");
+        const spacer = document.getElementById('spacer');
+        spacer.style.height = '120px';
+        window.internals?.updateLayoutAndStyleForAllFrames();
+        await UIHelper.renderingUpdate();
+
+        const item1HoveredAfter = checkHovered('item1');
+        const item2HoveredAfter = checkHovered('item2');
+
+        if (!item1HoveredAfter && !item2HoveredAfter) {
+            testPassed("Hover states correctly remained unchanged during layout change without mouse device");
+        } else {
+            if (item2HoveredAfter) {
+                testFailed("item2 incorrectly gained hover state after moving under pointer position");
+            }
+            if (item1HoveredAfter) {
+                testFailed("item1 unexpectedly hovered");
+            }
+        }
+
+    } catch (error) {
+        testFailed(`Test error: ${error}`);
+    } finally {
+        finishJSTest();
+    }
+}
+
+window.addEventListener('load', async () => {
+    runTest();
+});
+</script>
+</body>
+</html>