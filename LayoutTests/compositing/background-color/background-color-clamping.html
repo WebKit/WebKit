WebKit/WebKit

commit fd22b8420d86a73fc777a825132d29f1ebb3b85d

Extended colors should be clamped so they look the same in SDR and HDR layers. https://bugs.webkit.org/show_bug.cgi?id=292226 <rdar://150231788>
Reviewed by Mike Wyrzykowski.

When drawing to an extended color space destination, manually convert colors
into the standard range to get the same output as would have happened with a
standard range destination.

* Source/WebCore/platform/graphics/Color.h:
* Source/WebCore/platform/graphics/cg/ColorCG.cpp:
(WTF::RetainPtr<CGColorRef>>::createValueForKey):
(WebCore::createSDRCGColorForColorspace):
(WebCore::cachedSDRCGColorForColorspace):
* Source/WebCore/platform/graphics/cg/GraphicsContextCG.cpp:
(WebCore::setCGFillColor):
(WebCore::GraphicsContextCG::drawRect):
(WebCore::GraphicsContextCG::drawLine):
(WebCore::GraphicsContextCG::fillRect):
(WebCore::GraphicsContextCG::fillRoundedRectImpl):
(WebCore::GraphicsContextCG::setCGShadow):
(WebCore::GraphicsContextCG::didUpdateState):
(WebCore::GraphicsContextCG::drawLinesForText):
* Tools/TestWebKitAPI/Tests/WebCore/cg/GraphicsContextCGTests.mm:
(TestWebKitAPI::TEST(GraphicsContextTests, OutOfGamutSRGBNotDrawn)):

Canonical link: https://commits.webkit.org/297279@main

Source/WebCore/platform/graphics/Color.h
@@ -310,6 +310,7 @@ bool outOfLineComponentsEqualIgnoringSemanticColor(const Color&, const Color&);
 
 #if USE(CG)
 WEBCORE_EXPORT RetainPtr<CGColorRef> cachedCGColor(const Color&);
+WEBCORE_EXPORT RetainPtr<CGColorRef> cachedSDRCGColorForColorspace(const Color&, const DestinationColorSpace&);
 WEBCORE_EXPORT ColorComponents<float, 4> platformConvertColorComponents(ColorSpace, ColorComponents<float, 4>, const DestinationColorSpace&);
 WEBCORE_EXPORT std::optional<SRGBA<uint8_t>> roundAndClampToSRGBALossy(CGColorRef);
 #endif

Source/WebCore/platform/graphics/cg/ColorCG.cpp
@@ -40,6 +40,7 @@
 
 namespace WebCore {
 static RetainPtr<CGColorRef> createCGColor(const Color&);
+static RetainPtr<CGColorRef> createSDRCGColorForColorspace(const Color&, const DestinationColorSpace&);
 }
 
 namespace WTF {
@@ -48,6 +49,12 @@ template<>
 RetainPtr<CGColorRef> TinyLRUCachePolicy<WebCore::Color, RetainPtr<CGColorRef>>::createValueForKey(const WebCore::Color& color)
 {
     return WebCore::createCGColor(color);
+}
+
+template<>
+RetainPtr<CGColorRef> TinyLRUCachePolicy<std::pair<WebCore::Color, std::optional<WebCore::DestinationColorSpace>>, RetainPtr<CGColorRef>>::createValueForKey(const std::pair<WebCore::Color, std::optional<WebCore::DestinationColorSpace>>& colorAndColorSpace)
+{
+    return WebCore::createSDRCGColorForColorspace(colorAndColorSpace.first, *colorAndColorSpace.second);
 }
 
 } // namespace WTF
@@ -210,6 +217,25 @@ RetainPtr<CGColorRef> cachedCGColor(const Color& color)
 
     static NeverDestroyed<TinyLRUCache<Color, RetainPtr<CGColorRef>, 32>> cache;
     return cache.get().get(color);
+}
+
+RetainPtr<CGColorRef> createSDRCGColorForColorspace(const Color& color, const DestinationColorSpace& colorSpace)
+{
+    RetainPtr result = cachedCGColor(color);
+    RetainPtr standardRangeColorSpace = adoptCF(CGColorSpaceCreateCopyWithStandardRange(colorSpace.platformColorSpace()));
+    return adoptCF(CGColorCreateCopyByMatchingToColorSpace(standardRangeColorSpace.get(), kCGRenderingIntentDefault, result.get(), nullptr));
+}
+
+RetainPtr<CGColorRef> cachedSDRCGColorForColorspace(const Color& color, const DestinationColorSpace& colorSpace)
+{
+    if (!colorSpace.usesExtendedRange() || color.tryGetAsSRGBABytes())
+        return cachedCGColor(color);
+
+    static Lock cachedSDRColorLock;
+    Locker locker { cachedSDRColorLock };
+
+    static NeverDestroyed<TinyLRUCache<std::pair<Color, std::optional<DestinationColorSpace>>, RetainPtr<CGColorRef>, 32>> cache;
+    return cache.get().get(std::make_pair(color, colorSpace));
 }
 
 ColorComponents<float, 4> platformConvertColorComponents(ColorSpace inputColorSpace, ColorComponents<float, 4> inputColorComponents, const DestinationColorSpace& outputColorSpace)

commit 29d8af1fc776dc172b5e629db59f5633ee60b55f

Cherry-pick d6203e5d590d. rdar://169022562
    REGRESSION (297279@main): P3 colors can be mismatched between painted content, and layer backgrounds (see on book.stevejobsarchive.com).
    https://bugs.webkit.org/show_bug.cgi?id=305683
    rdar://166725679

    Reviewed by Sam Weinig.

    https://book.stevejobsarchive.com used a display-p3 color in CSS, and this was used both
    for painted backgrounds, and for "simple container" layers where we set it as layer.backgroundColor.

    This bug was introduced by 297279@main, which added clamping of the colors in the painting
    path to sRGB, but not in the layer backgroundColor path, which also caused colors to no longer
    paint as P3.

    The fix is to convert out-of-line colors into a clamping colorspace, which is DisplayP3 if
    the output colorspace is wide gamut, or SRGB. This happens only before colors go into the cache,
    so is not super hot code. We also need to used the clamped color for layer background colors in
    `RemoteLayerTreePropertyApplier::applyPropertiesToLayer()` so that they match the paint-time clamp.

    Rename `cachedSDRCGColorForColorspace()` to `createCGColorInDestinationStandardRange()` to be more
    accurate.

    We also need a fix in `DrawGlyphsRecorder`, so that text being rendered in P3 respects the
    colorspace of the target context to avoid clamping. We implement the `deGetColorSpace` callback,
    have `GraphicsContextCG::colorSpace()` use `CGContextGetColorSpace()`, and have the buffer in
    `DrawGlyphsRecorder::drawOTSVGRun()` use the right colorspace.

    On iOS we need to get to the display colorspace in `RemoteLayerTreePropertyApplier::applyPropertiesToLayer()`,
    so expose `WebPageProxy::colorSpace()` for Cocoa platforms, and cache it in PageClientImpl() because
    fetching it involves calling `MediaToolbox_MTShouldPlayHDRVideo()`.

    * LayoutTests/TestExpectations:

Source/WebCore/Configurations/AllowedSPI-legacy.toml
@@ -528,7 +528,6 @@ symbols = [
     "CGColorSpaceEqualToColorSpace",
     "CGColorTransformConvertColorComponents",
     "CGColorTransformCreate",
-    "CGContextCopyDeviceColorSpace",
     "CGContextCreateWithDelegate",
     "CGContextDelegateCreate",
     "CGContextDelegateGetInfo",

Source/WebCore/Configurations/AllowedSPI.toml
@@ -75,6 +75,13 @@ symbols = [
 ]
 
 [[temporary-usage]]
+request = "rdar://168834967"
+cleanup = "rdar://168834815"
+symbols = [
+    "CGContextGetColorSpace",
+]
+
+[[temporary-usage]]
 request = "rdar://161402565"
 cleanup = "rdar://161402756"
 symbols = [

Source/WebCore/PAL/pal/spi/cg/CoreGraphicsSPI.h
@@ -85,6 +85,7 @@ typedef CF_ENUM (int32_t, CGContextDelegateCallbackName)
     deDrawGlyphs = 8,
     deBeginLayer = 17,
     deEndLayer = 18,
+    deGetColorSpace = 30,
 };
 
 typedef const struct CGColorTransform* CGColorTransformRef;
@@ -396,7 +397,7 @@ typedef bool (^CGPDFAnnotationDrawCallbackType)(CGContextRef context, CGPDFPageR
 void CGContextDrawPDFPageWithAnnotations(CGContextRef, CGPDFPageRef, CGPDFAnnotationDrawCallbackType);
 void CGContextDrawPathDirect(CGContextRef, CGPathDrawingMode, CGPathRef, const CGRect* boundingBox);
 
-CGColorSpaceRef CGContextCopyDeviceColorSpace(CGContextRef);
+CGColorSpaceRef CGContextGetColorSpace(CGContextRef);
 CFPropertyListRef CGColorSpaceCopyPropertyList(CGColorSpaceRef);
 CGError CGSNewRegionWithRect(const CGRect*, CGRegionRef*);
 CGError CGSPackagesEnableConnectionOcclusionNotifications(CGSConnectionID, bool flag, bool* outCurrentVisibilityState);

Source/WebCore/platform/graphics/Color.h
@@ -309,7 +309,7 @@ bool outOfLineComponentsEqualIgnoringSemanticColor(const Color&, const Color&);
 
 #if USE(CG)
 WEBCORE_EXPORT RetainPtr<CGColorRef> cachedCGColor(const Color&);
-WEBCORE_EXPORT RetainPtr<CGColorRef> cachedSDRCGColorForColorspace(const Color&, const DestinationColorSpace&);
+WEBCORE_EXPORT RetainPtr<CGColorRef> cachedCGColorInDestinationStandardRange(const Color&, const DestinationColorSpace&);
 WEBCORE_EXPORT ColorComponents<float, 4> platformConvertColorComponents(ColorSpace, ColorComponents<float, 4>, const DestinationColorSpace&);
 WEBCORE_EXPORT std::optional<SRGBA<uint8_t>> roundAndClampToSRGBALossy(CGColorRef);
 #endif

Source/WebCore/platform/graphics/ColorConversion.h
@@ -46,7 +46,6 @@ template<typename Output, typename Input> Output convertColorCarryingForwardMiss
 ColorComponents<float, 4> convertAndResolveColorComponents(ColorSpace inputColorSpace, ColorComponents<float, 4> inputColorComponents, ColorSpace outputColorSpace);
 ColorComponents<float, 4> convertAndResolveColorComponents(ColorSpace inputColorSpace, ColorComponents<float, 4> inputColorComponents, const DestinationColorSpace& outputColorSpace);
 
-
 // All color types, other than XYZA or those inheriting from RGBType, must implement
 // the following conversions to and from their "Reference" color.
 //

Source/WebCore/platform/graphics/cg/ColorCG.cpp
@@ -40,7 +40,7 @@
 
 namespace WebCore {
 static RetainPtr<CGColorRef> createCGColor(const Color&);
-static RetainPtr<CGColorRef> createSDRCGColorForColorspace(const Color&, const DestinationColorSpace&);
+static RetainPtr<CGColorRef> createCGColorInDestinationStandardRange(const Color&, const DestinationColorSpace&);
 }
 
 namespace WTF {
@@ -54,7 +54,7 @@ RetainPtr<CGColorRef> TinyLRUCachePolicy<WebCore::Color, RetainPtr<CGColorRef>>:
 template<>
 RetainPtr<CGColorRef> TinyLRUCachePolicy<std::pair<WebCore::Color, std::optional<WebCore::DestinationColorSpace>>, RetainPtr<CGColorRef>>::createValueForKey(const std::pair<WebCore::Color, std::optional<WebCore::DestinationColorSpace>>& colorAndColorSpace)
 {
-    return WebCore::createSDRCGColorForColorspace(colorAndColorSpace.first, *colorAndColorSpace.second);
+    return WebCore::createCGColorInDestinationStandardRange(colorAndColorSpace.first, *colorAndColorSpace.second);
 }
 
 } // namespace WTF
@@ -201,16 +201,18 @@ RetainPtr<CGColorRef> cachedCGColor(const Color& color)
     return cache.get().get(color);
 }
 
-RetainPtr<CGColorRef> createSDRCGColorForColorspace(const Color& color, const DestinationColorSpace& colorSpace)
+RetainPtr<CGColorRef> createCGColorInDestinationStandardRange(const Color& color, const DestinationColorSpace& colorSpace)
 {
-    RetainPtr result = cachedCGColor(color);
-    RetainPtr standardRangeColorSpace = adoptCF(CGColorSpaceCreateCopyWithStandardRange(colorSpace.protectedPlatformColorSpace().get()));
-    return adoptCF(CGColorCreateCopyByMatchingToColorSpace(standardRangeColorSpace.get(), kCGRenderingIntentDefault, result.get(), nullptr));
+    auto clampingSpace = CGColorSpaceIsWideGamutRGB(colorSpace.protectedPlatformColorSpace().get()) ? ColorSpace::DisplayP3 : ColorSpace::SRGB;
+    auto [r, g, b, a] = color.toResolvedColorComponentsInColorSpace(clampingSpace);
+
+    std::array<CGFloat, 4> sourceComponents { r, g, b, a };
+    return adoptCF(CGColorCreate(cachedNullableCGColorSpaceSingleton(clampingSpace), sourceComponents.data()));
 }
 
-RetainPtr<CGColorRef> cachedSDRCGColorForColorspace(const Color& color, const DestinationColorSpace& colorSpace)
+RetainPtr<CGColorRef> cachedCGColorInDestinationStandardRange(const Color& color, const DestinationColorSpace& colorSpace)
 {
-    if (!colorSpace.usesExtendedRange() || color.tryGetAsSRGBABytes())
+    if (color.tryGetAsSRGBABytes())
         return cachedCGColor(color);
 
     static Lock cachedSDRColorLock;

Source/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
@@ -56,7 +56,7 @@ WTF_MAKE_TZONE_ALLOCATED_IMPL(GraphicsContextCG);
 
 static void setCGFillColor(CGContextRef context, const Color& color, const DestinationColorSpace& colorSpace)
 {
-    CGContextSetFillColorWithColor(context, cachedSDRCGColorForColorspace(color, colorSpace).get());
+    CGContextSetFillColorWithColor(context, cachedCGColorInDestinationStandardRange(color, colorSpace).get());
 }
 
 inline CGAffineTransform getUserToBaseCTM(CGContextRef context)
@@ -236,12 +236,13 @@ const DestinationColorSpace& GraphicsContextCG::colorSpace() const
     RetainPtr<CGColorSpaceRef> colorSpace;
 
     // FIXME: Need to handle kCGContextTypePDF.
-    if (CGContextGetType(context) == kCGContextTypeIOSurface)
+    auto contextType = CGContextGetType(context);
+    if (contextType == kCGContextTypeIOSurface)
         colorSpace = CGIOSurfaceContextGetColorSpace(context);
-    else if (CGContextGetType(context) == kCGContextTypeBitmap)
+    else if (contextType == kCGContextTypeBitmap)
         colorSpace = CGBitmapContextGetColorSpace(context);
     else
-        colorSpace = adoptCF(CGContextCopyDeviceColorSpace(context));
+        colorSpace = CGContextGetColorSpace(context);
 
     // FIXME: Need to ASSERT(colorSpace). For now fall back to sRGB if colorSpace is nil.
     m_colorSpace = colorSpace ? DestinationColorSpace(colorSpace) : DestinationColorSpace::SRGB();
@@ -1112,7 +1113,7 @@ void GraphicsContextCG::setCGDropShadow(const std::optional<GraphicsDropShadow>&
 
     CGContextSetAlpha(context, shadow->opacity);
 
-    auto style = adoptCF(CGStyleCreateShadow2(offset, blurRadius, cachedSDRCGColorForColorspace(shadow->color, colorSpace()).get()));
+    auto style = adoptCF(CGStyleCreateShadow2(offset, blurRadius, cachedCGColorInDestinationStandardRange(shadow->color, colorSpace()).get()));
     CGContextSetStyle(context, style.get());
 }
 
@@ -1198,7 +1199,7 @@ void GraphicsContextCG::didUpdateState(GraphicsContextState& state)
             break;
 
         case GraphicsContextState::Change::StrokeBrush:
-            CGContextSetStrokeColorWithColor(context, cachedSDRCGColorForColorspace(state.strokeBrush().color(), colorSpace()).get());
+            CGContextSetStrokeColorWithColor(context, cachedCGColorInDestinationStandardRange(state.strokeBrush().color(), colorSpace()).get());
             break;
 
         case GraphicsContextState::Change::CompositeMode:

Source/WebCore/platform/graphics/coretext/DrawGlyphsRecorder.cpp
@@ -79,6 +79,12 @@ static CGError drawPath(CGContextDelegateRef delegate, CGRenderingStateRef rstat
     return kCGErrorSuccess;
 }
 
+static CGColorSpaceRef getColorSpace(CGContextDelegateRef delegate, CGRenderingStateRef, CGGStateRef)
+{
+    DrawGlyphsRecorder& recorder = *static_cast<DrawGlyphsRecorder*>(CGContextDelegateGetInfo(delegate));
+    return recorder.colorSpace();
+}
+
 UniqueRef<GraphicsContext> DrawGlyphsRecorder::createInternalContext()
 {
     auto contextDelegate = adoptCF(CGContextDelegateCreate(this));
@@ -87,6 +93,8 @@ UniqueRef<GraphicsContext> DrawGlyphsRecorder::createInternalContext()
     CGContextDelegateSetCallback(contextDelegate.get(), deDrawGlyphs, reinterpret_cast<CGContextDelegateCallback>(&WebCore::drawGlyphs));
     CGContextDelegateSetCallback(contextDelegate.get(), deDrawImage, reinterpret_cast<CGContextDelegateCallback>(&drawImage));
     CGContextDelegateSetCallback(contextDelegate.get(), deDrawPath, reinterpret_cast<CGContextDelegateCallback>(&drawPath));
+    CGContextDelegateSetCallback(contextDelegate.get(), deGetColorSpace, reinterpret_cast<CGContextDelegateCallback>(&getColorSpace));
+
     auto contextType = kCGContextTypeUnknown;
     auto context = adoptCF(CGContextCreateWithDelegate(contextDelegate.get(), contextType, nullptr, nullptr));
     return makeUniqueRef<GraphicsContextCG>(context.get());
@@ -418,6 +426,11 @@ void DrawGlyphsRecorder::recordDrawPath(CGRenderingStateRef, CGGStateRef gstate,
     }
 }
 
+CGColorSpaceRef DrawGlyphsRecorder::colorSpace() const
+{
+    return m_owner.colorSpace().platformColorSpace();
+}
+
 void DrawGlyphsRecorder::drawOTSVGRun(const Font& font, std::span<const GlyphBufferGlyph> glyphs, std::span<const GlyphBufferAdvance> advances, const FloatPoint& startPoint, FontSmoothingMode smoothingMode)
 {
     FloatPoint penPosition = startPoint;
@@ -434,7 +447,7 @@ void DrawGlyphsRecorder::drawOTSVGRun(const Font& font, std::span<const GlyphBuf
 #endif
 
         // Create a local ImageBuffer because decoding the SVG fonts has to happen in WebProcess.
-        if (auto imageBuffer = m_owner.createAlignedImageBuffer(bounds, DestinationColorSpace::SRGB(), RenderingMethod::Local)) {
+        if (auto imageBuffer = m_owner.createAlignedImageBuffer(bounds, m_owner.colorSpace(), RenderingMethod::Local)) {
             FontCascade::drawGlyphs(imageBuffer->context(), font, glyphs.subspan(i, 1), advances.subspan(i, 1), FloatPoint(), smoothingMode);
 
             FloatRect destinationRect = enclosingIntRect(bounds);

Source/WebCore/platform/graphics/coretext/DrawGlyphsRecorder.h
@@ -63,6 +63,8 @@ public:
     void recordDrawImage(CGRenderingStateRef, CGGStateRef, CGRect, CGImageRef);
     void recordDrawPath(CGRenderingStateRef, CGGStateRef, CGPathDrawingMode, CGPathRef);
 
+    CGColorSpaceRef colorSpace() const;
+
 private:
     UniqueRef<GraphicsContext> createInternalContext();
 

Source/WebKit/Shared/RemoteLayerTree/RemoteLayerTreePropertyApplier.mm
@@ -29,9 +29,11 @@
 #import "Logging.h"
 #import "PlatformCAAnimationRemote.h"
 #import "PlatformCALayerRemote.h"
+#import "RemoteLayerTreeDrawingAreaProxy.h"
 #import "RemoteLayerTreeHost.h"
 #import "RemoteLayerTreeInteractionRegionLayers.h"
 #import "WKVideoView.h"
+#import "WebPageProxy.h"
 #import <QuartzCore/QuartzCore.h>
 #import <WebCore/ContentsFormatCocoa.h>
 #import <WebCore/GraphicsLayerEnums.h>
@@ -425,8 +427,18 @@ void RemoteLayerTreePropertyApplier::applyPropertiesToLayer(CALayer *layer, Remo
     if (properties.changedProperties & LayerChange::NameChanged)
         layer.name = properties.name.createNSString().get();
 
-    if (properties.changedProperties & LayerChange::BackgroundColorChanged)
-        layer.backgroundColor = cgColorFromColor(properties.backgroundColor).get();
+    if (properties.changedProperties & LayerChange::BackgroundColorChanged) {
+        auto colorSpace = [&]() {
+            Ref drawingArea = layerTreeHost->drawingArea();
+            if (RefPtr webPage = drawingArea->page())
+                return webPage->colorSpace();
+
+            return DestinationColorSpace::SRGB();
+        }();
+
+        RetainPtr cgColor = cachedCGColorInDestinationStandardRange(properties.backgroundColor, colorSpace);
+        layer.backgroundColor = cgColor.get();
+    }
 
     if (properties.changedProperties & LayerChange::BorderColorChanged)
         layer.borderColor = cgColorFromColor(properties.borderColor).get();

Source/WebKit/UIProcess/API/Cocoa/WKWebViewTesting.mm
@@ -52,6 +52,7 @@
 #import "_WKFrameHandleInternal.h"
 #import "_WKInspectorInternal.h"
 #import <WebCore/BoxSides.h>
+#import <WebCore/Color.h>
 #import <WebCore/NowPlayingInfo.h>
 #import <WebCore/ScrollingNodeID.h>
 #import <WebCore/ValidationBubble.h>
@@ -208,6 +209,9 @@ static void dumpCALayer(TextStream& ts, CALayer *layer, bool traverse)
 
     if (layer.opacity != 1.0)
         ts.dumpProperty("layer opacity"_s, makeString(layer.opacity));
+
+    if (layer.backgroundColor)
+        ts.dumpProperty("layer background color"_s, WebCore::Color::createAndPreserveColorSpace(RetainPtr { layer.backgroundColor }.get()));
 
     if (layer.cornerRadius != 0.0)
         ts.dumpProperty("layer cornerRadius"_s, makeString(layer.cornerRadius));

Source/WebKit/UIProcess/Cocoa/WebPageProxyCocoa.mm
@@ -194,6 +194,14 @@ void WebPageProxy::didCommitLayerTree(const RemoteLayerTreeTransaction& layerTre
     }
 }
 
+WebCore::DestinationColorSpace WebPageProxy::colorSpace() const
+{
+    if (RefPtr pageClient = this->pageClient())
+        return pageClient->colorSpace();
+
+    return WebCore::DestinationColorSpace::SRGB();
+}
+
 void WebPageProxy::didCommitMainFrameData(const MainFrameData& mainFrameData, const TransactionID& transactionID)
 {
     themeColorChanged(mainFrameData.themeColor);

Source/WebKit/UIProcess/PageClient.h
@@ -527,8 +527,6 @@ public:
 
     virtual CGRect boundsOfLayerInLayerBackedWindowCoordinates(CALayer *) const = 0;
 
-    virtual WebCore::DestinationColorSpace colorSpace() = 0;
-
     virtual bool useFormSemanticContext() const = 0;
     
     virtual NSView *viewForPresentingRevealPopover() const = 0;
@@ -557,6 +555,8 @@ public:
     virtual void scrollingNodeScrollViewDidScroll(WebCore::ScrollingNodeID) = 0;
 
     virtual CocoaWindow *platformWindow() const = 0;
+
+    virtual WebCore::DestinationColorSpace colorSpace() = 0;
 #endif
 
     virtual void reconcileEnclosingScrollViewContentOffset(EditorState&) { };

Source/WebKit/UIProcess/WebPageProxy.h
@@ -1600,8 +1600,6 @@ public:
 #if PLATFORM(MAC)
     bool useFormSemanticContext() const;
     void semanticContextDidChange();
-
-    WebCore::DestinationColorSpace colorSpace();
 #endif
 
     void effectiveAppearanceDidChange();
@@ -1623,6 +1621,8 @@ public:
     RefPtr<WebCore::SharedBuffer> dataSelectionForPasteboard(const String& pasteboardType);
     void makeFirstResponder();
     void assistiveTechnologyMakeFirstResponder();
+
+    WebCore::DestinationColorSpace colorSpace() const;
 
 #if ENABLE(MULTI_REPRESENTATION_HEIC)
     void insertMultiRepresentationHEIC(NSData *, NSString *);

Source/WebKit/UIProcess/ios/PageClientImplIOS.h
@@ -40,6 +40,7 @@ OBJC_CLASS WKContentView;
 OBJC_CLASS WKEditorUndoTarget;
 
 namespace WebCore {
+class DestinationColorSpace;
 enum class DOMPasteAccessCategory : uint8_t;
 enum class DOMPasteAccessResponse : uint8_t;
 struct PromisedAttachmentInfo;
@@ -80,6 +81,8 @@ private:
     bool isViewInWindow() override;
     bool isViewVisibleOrOccluded() override;
     bool isVisuallyIdle() override;
+    WebCore::DestinationColorSpace colorSpace() override;
+
     void processDidExit() override;
     void processWillSwap() override;
     void didRelaunchProcess() override;
@@ -392,6 +395,7 @@ private:
 
     WeakObjCPtr<WKContentView> m_contentView;
     RetainPtr<WKEditorUndoTarget> m_undoTarget;
+    std::optional<WebCore::DestinationColorSpace> m_colorSpace;
 };
 } // namespace WebKit
 

Source/WebKit/UIProcess/ios/PageClientImplIOS.mm
@@ -225,6 +225,14 @@ bool PageClientImpl::isVisuallyIdle()
     return !isActiveViewVisible();
 }
 
+WebCore::DestinationColorSpace PageClientImpl::colorSpace()
+{
+    if (!m_colorSpace)
+        m_colorSpace = screenColorSpace(nullptr);
+
+    return *m_colorSpace;
+}
+
 void PageClientImpl::processDidExit()
 {
     [contentView() _processDidExit];

Source/WebKit/UIProcess/mac/WebPageProxyMac.mm
@@ -382,11 +382,6 @@ void WebPageProxy::semanticContextDidChange()
     protectedLegacyMainFrameProcess()->send(Messages::WebPage::SemanticContextDidChange(useFormSemanticContext()), webPageIDInMainFrameProcess());
 }
 
-WebCore::DestinationColorSpace WebPageProxy::colorSpace()
-{
-    return protectedPageClient()->colorSpace();
-}
-
 void WebPageProxy::registerUIProcessAccessibilityTokens(WebCore::AccessibilityRemoteToken elementToken, WebCore::AccessibilityRemoteToken windowToken)
 {
     if (!hasRunningProcess())

Source/WebKit/WebProcess/WebPage/RemoteLayerTree/RemoteLayerTreeContext.mm
@@ -95,7 +95,7 @@ std::optional<WebCore::DestinationColorSpace> RemoteLayerTreeContext::displayCol
 {
     if (RefPtr drawingArea = m_webPage->drawingArea())
         return drawingArea->displayColorSpace();
-    
+
     return { };
 }
 

LayoutTests/TestExpectations
@@ -293,6 +293,9 @@ http/tests/app-privacy-report/ [ Skip ]
 # Only partial support on Cocoa platforms.
 imported/w3c/web-platform-tests/speech-api/ [ Skip ]
 
+# Requires caLayerTreeAsText
+compositing/background-color/background-color-clamping.html [ Skip ]
+
 # Support for COEP violation reporting is missing.
 imported/w3c/web-platform-tests/html/cross-origin-opener-policy/reporting/access-reporting/
 imported/w3c/web-platform-tests/html/cross-origin-opener-policy/reporting/document-reporting/

LayoutTests/compositing/background-color/background-color-clamping-expected.txt
@@ -1,0 +1,37 @@
+
+(CALayer tree root
+  (layer bounds [x: 0 y: 0 width: 769 height: 830])
+  (layer position [x: 392.5 y: 392.5])
+  (sublayers
+    (
+      (layer bounds [x: 0 y: 0 width: 200 height: 200])
+      (layer position [x: 110 y: 110])
+      (sublayers
+        (
+          (layer bounds [x: 0 y: 0 width: 200 height: 200])
+          (layer anchorPoint [x: 0 y: 0])
+          (layer background color rgb(255, 255, 255)))))
+    (
+      (layer bounds [x: 0 y: 0 width: 200 height: 200])
+      (layer position [x: 110 y: 110])
+      (sublayers
+        (
+          (layer bounds [x: 0 y: 0 width: 200 height: 200])
+          (layer anchorPoint [x: 0 y: 0])
+          (layer background color rgb(0, 140, 214)))))
+    (
+      (layer bounds [x: 0 y: 0 width: 200 height: 200])
+      (layer position [x: 110 y: 110])
+      (sublayers
+        (
+          (layer bounds [x: 0 y: 0 width: 200 height: 200])
+          (layer anchorPoint [x: 0 y: 0])
+          (layer background color rgb(0, 255, 184)))))
+    (
+      (layer bounds [x: 0 y: 0 width: 200 height: 200])
+      (layer position [x: 110 y: 110])
+      (sublayers
+        (
+          (layer bounds [x: 0 y: 0 width: 200 height: 200])
+          (layer anchorPoint [x: 0 y: 0])
+          (layer background color rgb(0, 255, 166)))))))

LayoutTests/compositing/background-color/background-color-clamping.html
@@ -1,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .box {
+            margin: 10px;
+            width: 200px;
+            height: 200px;
+            will-change: transform;
+        }
+        
+        #layers {
+            will-change: transform;
+        }
+        
+        .unclampedrgb {
+            background-color: color(srgb 5 5 5);
+        }
+        
+        .displayp3 {
+            background-color: color(display-p3 0 0.537 0.815);
+        }
+
+        .displayp3hdr {
+            background-color: color(display-p3 0 1.537 0.815);
+        }
+
+        .xyz {
+            background-color: color(xyz-d50 0.2005 1.14089 0.4472);
+        }
+    </style>
+    <script src="../../resources/ui-helper.js"></script>
+    <script>
+        if (window.testRunner) {
+            testRunner.dumpAsText();
+            testRunner.waitUntilDone();
+        }
+
+        window.addEventListener('load', async () => {
+            const results = document.getElementById('results');
+            results.textContent = await UIHelper.getCALayerTreeForCompositedElement(document.getElementById('layers'));
+
+            if (window.testRunner)
+                testRunner.notifyDone();
+        }, false);
+    </script>
+</head>
+<body>
+    <section id="layers">
+        <div class="unclampedrgb box"></div>
+        <div class="displayp3 box"></div>
+        <div class="displayp3hdr box"></div>
+        <div class="xyz box"></div>
+    </section>
+<pre id="results"></pre>
+</body>
+</html>

LayoutTests/platform/ios/TestExpectations
@@ -154,6 +154,9 @@ http/tests/media/modern-media-controls/overflow-support [ Pass ]
 # media/modern-media-controls/overflow-support [ Pass ]
 # media/modern-media-controls/tracks-support [ Pass ]
 
+# Uses caLayerTreeAsText
+compositing/background-color/background-color-clamping.html [ Pass ]
+
 media/media-webm-av1.html [ Skip ]
 
 media/media-source/media-managedmse-airplay.html [ Pass ]

LayoutTests/platform/ios/compositing/background-color/background-color-clamping-expected.txt
@@ -1,0 +1,37 @@
+
+(CALayer tree root
+  (layer bounds [x: 0 y: 0 width: 784 height: 830])
+  (layer position [x: 400 y: 400])
+  (sublayers
+    (
+      (layer bounds [x: 0 y: 0 width: 200 height: 200])
+      (layer position [x: 110 y: 110])
+      (sublayers
+        (
+          (layer bounds [x: 0 y: 0 width: 200 height: 200])
+          (layer anchorPoint [x: 0 y: 0])
+          (layer background color rgb(255, 255, 255)))))
+    (
+      (layer bounds [x: 0 y: 0 width: 200 height: 200])
+      (layer position [x: 110 y: 110])
+      (sublayers
+        (
+          (layer bounds [x: 0 y: 0 width: 200 height: 200])
+          (layer anchorPoint [x: 0 y: 0])
+          (layer background color rgb(0, 140, 214)))))
+    (
+      (layer bounds [x: 0 y: 0 width: 200 height: 200])
+      (layer position [x: 110 y: 110])
+      (sublayers
+        (
+          (layer bounds [x: 0 y: 0 width: 200 height: 200])
+          (layer anchorPoint [x: 0 y: 0])
+          (layer background color rgb(0, 255, 184)))))
+    (
+      (layer bounds [x: 0 y: 0 width: 200 height: 200])
+      (layer position [x: 110 y: 110])
+      (sublayers
+        (
+          (layer bounds [x: 0 y: 0 width: 200 height: 200])
+          (layer anchorPoint [x: 0 y: 0])
+          (layer background color rgb(0, 255, 166)))))))

LayoutTests/platform/mac/TestExpectations
@@ -65,6 +65,9 @@ imported/w3c/web-platform-tests/pointerevents [ Pass ]
 
 imported/w3c/web-platform-tests/speech-api [ Pass ]
 
+# Uses caLayerTreeAsText
+compositing/background-color/background-color-clamping.html [ Pass ]
+
 media/media-webm-av1.html [ Skip ]
 media/track/track-description-cue.html [ Pass ]
 media/track/track-extended-descriptions.html [ Pass ]