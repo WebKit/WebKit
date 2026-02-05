cd84ea89b20411d138eab1ee1969e6f8a6b5d262
Chris Dumez <cdumez@apple.com>
2026-02-05 13:16:07 -0800

    Cherry-pick b2191c46ed4e. rdar://169372533
        Take a process assertion on the destination process when sending a sync IPC to it
        https://bugs.webkit.org/show_bug.cgi?id=306417
        rdar://168622072
    
        Reviewed by Per Arne Vollan.
    
        Take a process assertion on the destination process when sending a sync
        IPC to it, similarly to what we already do when calling
        `sendWithAsyncReply()`. Similarly as `sendWithAsyncReply()`, we're
        expecting a response from the destination process. Also, if the
        destination process is suspended, the sender will actually hang.
    
        * Source/WebKit/UIProcess/AuxiliaryProcessProxy.h:
        (WebKit::AuxiliaryProcessProxy::sendSync):
    
        Canonical link: https://commits.webkit.org/306586@main
    
    * Source/WebKit/UIProcess/AuxiliaryProcessProxy.h:
    (WebKit::AuxiliaryProcessProxy::sendSync):
    
    Identifier: 305413.246@safari-7624-branch

--- Source/WebKit/UIProcess/AuxiliaryProcessProxy.h
+++ Source/WebKit/UIProcess/AuxiliaryProcessProxy.h
@@ -110,10 +110,11 @@ public:
 
     template<typename T> bool send(T&& message, uint64_t destinationID, OptionSet<IPC::SendOption> sendOptions = { });
 
-    template<typename T> using SendSyncResult = IPC::Connection::SendSyncResult<T>;
-    template<typename T> SendSyncResult<T> sendSync(T&& message, uint64_t destinationID, IPC::Timeout = 1_s, OptionSet<IPC::SendSyncOption> sendSyncOptions = { });
-
     enum class ShouldStartProcessThrottlerActivity : bool { No, Yes };
+
+    template<typename T> using SendSyncResult = IPC::Connection::SendSyncResult<T>;
+    template<typename T> SendSyncResult<T> sendSync(T&& message, uint64_t destinationID, IPC::Timeout = 1_s, OptionSet<IPC::SendSyncOption> sendSyncOptions = { }, ShouldStartProcessThrottlerActivity = ShouldStartProcessThrottlerActivity::Yes);
+
     using AsyncReplyID = IPC::Connection::AsyncReplyID;
     template<typename T, typename C> std::optional<AsyncReplyID> sendWithAsyncReply(T&&, C&&, uint64_t destinationID = 0, OptionSet<IPC::SendOption> = { }, ShouldStartProcessThrottlerActivity = ShouldStartProcessThrottlerActivity::Yes);
 
@@ -130,9 +131,9 @@ public:
     }
 
     template<typename T, typename RawValue>
-    SendSyncResult<T> sendSync(T&& message, const ObjectIdentifierGenericBase<RawValue>& destinationID, IPC::Timeout timeout = 1_s, OptionSet<IPC::SendSyncOption> sendSyncOptions = { })
+    SendSyncResult<T> sendSync(T&& message, const ObjectIdentifierGenericBase<RawValue>& destinationID, IPC::Timeout timeout = 1_s, OptionSet<IPC::SendSyncOption> sendSyncOptions = { }, ShouldStartProcessThrottlerActivity shouldStartProcessThrottlerActivity = ShouldStartProcessThrottlerActivity::Yes)
     {
-        return sendSync<T>(std::forward<T>(message), destinationID.toUInt64(), timeout, sendSyncOptions);
+        return sendSync<T>(std::forward<T>(message), destinationID.toUInt64(), timeout, sendSyncOptions, shouldStartProcessThrottlerActivity);
     }
 
     IPC::Connection& connection() const
@@ -360,7 +361,7 @@ bool AuxiliaryProcessProxy::send(T&& message, uint64_t destinationID, OptionSet<
 }
 
 template<typename T>
-AuxiliaryProcessProxy::SendSyncResult<T> AuxiliaryProcessProxy::sendSync(T&& message, uint64_t destinationID, IPC::Timeout timeout, OptionSet<IPC::SendSyncOption> sendSyncOptions)
+AuxiliaryProcessProxy::SendSyncResult<T> AuxiliaryProcessProxy::sendSync(T&& message, uint64_t destinationID, IPC::Timeout timeout, OptionSet<IPC::SendSyncOption> sendSyncOptions, ShouldStartProcessThrottlerActivity shouldStartProcessThrottlerActivity)
 {
     static_assert(T::isSync, "Sync message expected");
 
@@ -369,6 +370,10 @@ AuxiliaryProcessProxy::SendSyncResult<T> AuxiliaryProcessProxy::sendSync(T&& mes
         return { IPC::Error::InvalidConnection };
 
     TraceScope scope(SyncMessageStart, SyncMessageEnd);
+
+    RefPtr<ProcessThrottlerActivity> activity;
+    if (shouldStartProcessThrottlerActivity == ShouldStartProcessThrottlerActivity::Yes)
+        activity = protectedThrottler()->quietBackgroundActivity(description(T::name()));
 
     return connection->sendSync(std::forward<T>(message), destinationID, timeout, sendSyncOptions);
 }
