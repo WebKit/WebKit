49ccbc26efdbaab9fb05e5a4c1172842548e5b50
Marcus Plutowski <marcus_plutowski@apple.com>
2026-02-16 16:50:50 -0800

    [WTF] Refactor StackSwitch to accomodate multiple trampolines https://bugs.webkit.org/show_bug.cgi?id=307997 rdar://170492474
    Reviewed by NOBODY (OOPS!).
    
    In order to prevent these trampolines from being used as general gadgets
    for attackers, we want each to be wired to a specific callee.
    However, we don't want to be copying these around every time we need
    to stack-switch inside a given function, esp. given all the assembly.
    These macros allow us to more easily abstract over the interface and
    make creating new wired trampolines easier.
    
    The reason I have this "callExpression" in there is to allow for e.g.
    calling member functions using the parameter, which is something we will
    likely need soon.
    If we come to need multi-argument trampolines, then we can add support for
    that when we get there.

--- Source/WTF/wtf/StackSwitch.cpp
+++ Source/WTF/wtf/StackSwitch.cpp
@@ -33,69 +33,72 @@
 
 namespace WTF {
 
-// C-linkage wrapper for Thread::entryPointFinishSetup so it can be called from assembly
-extern "C" void threadEntryPointFinishSetupWrapper(void* context);
-extern "C" void threadEntryPointFinishSetupWrapper(void* context)
-{
-    Thread::entryPointFinishSetup(context);
-}
+// C-linkage wrappers for target functions so they can be called from assembly
+#define DEFINE_STACK_SWITCH_WRAPPER(trampolineName, argType, argName, callExpression) \
+    extern "C" void trampolineName##Wrapper(argType argName); \
+    extern "C" void trampolineName##Wrapper(argType argName) \
+    { \
+        callExpression; \
+    }
+
+WTF_FOR_EACH_STACK_SWITCH_TRAMPOLINE(DEFINE_STACK_SWITCH_WRAPPER)
+#undef DEFINE_STACK_SWITCH_WRAPPER
 
 #if CPU(ARM64E)
 
 // Trampoline that switches from the OS-provided stack to a custom sequestered stack.
 // Parameters:
-//   x0 = context pointer (passed as argument to Thread::entryPointFinishSetup)
+//   x0 = context pointer (passed as argument to target function, or 'this' pointer for member functions)
 //   x1 = new stack pointer (top of stack, since stacks grow down)
 //
 // This function:
 // 1. Saves callee-saved registers on the current (OS) stack
 // 2. Switches to the new stack
 // 3. Sets up the rest of its frame (saved sp, lr) on the new stack
-// 4. Calls Thread::entryPointFinishSetup
+// 4. Calls the target function via its wrapper
 // 5. When function returns, restores the OS stack and all saved registers
 // 6. Returns to the original caller
-__asm__(
-    ".text" "\n"
-    ".balign 16" "\n"
-    ".globl " SYMBOL_STRING(callThreadEntryPointFinishSetupWithNewStack) "\n"
-    SYMBOL_STRING(callThreadEntryPointFinishSetupWithNewStack) ":" "\n"
+#define DEFINE_STACK_SWITCH_TRAMPOLINE_ARM64E(trampolineName, argType, argName, callExpression) \
+    __asm__( \
+        ".text" "\n" \
+        ".balign 16" "\n" \
+        ".globl " SYMBOL_STRING(trampolineName) "\n" \
+        SYMBOL_STRING(trampolineName) ":" "\n" \
+        \
+        "pacibsp" "\n" \
+        "stp x19, x20, [sp, #-16]!" "\n" \
+        "mov x19, sp" "\n" \
+        \
+        "mov sp, x1" "\n" \
+        "stp x29, x30, [sp, #-16]!" "\n" \
+        "mov x29, sp" "\n" \
+        \
+        "bl " SYMBOL_STRING(trampolineName##Wrapper) "\n" \
+        \
+        "mov sp, x19" "\n" \
+        "ldp x29, x30, [sp], #16" "\n" \
+        "ldp x19, x20, [sp], #16" "\n" \
+        "retab" "\n" \
+        \
+        ".previous" "\n" \
+    );
 
-    // Prologue
-    "pacibsp" "\n"
-    "stp x19, x20, [sp, #-16]!" "\n"
-    // Save away old sp in callee-saved x19
-    "mov x19, sp" "\n"
-
-    // Switch to new stack: x1 contains new stack pointer)
-    "mov sp, x1" "\n"
-    // Create a proper frame on the new stack
-    // This frame chains back to the OS stack frame
-    // of our caller
-    "stp x29, x30, [sp, #-16]!" "\n"
-    "mov x29, sp" "\n"
-
-    // Call Thread::entryPointFinishSetup
-    // x0 contains the context-pointer argument
-    "bl " SYMBOL_STRING(threadEntryPointFinishSetupWrapper) "\n"
-
-    // When function returns, restore OS stack from saved register
-    "mov sp, x19" "\n" // Restore sp for OS-stack from x19
-
-    // Unwind frame and return
-    "ldp x29, x30, [sp], #16" "\n"
-    "ldp x19, x20, [sp], #16" "\n"
-    "retab" "\n"
-
-    ".previous" "\n"
-);
+WTF_FOR_EACH_STACK_SWITCH_TRAMPOLINE(DEFINE_STACK_SWITCH_TRAMPOLINE_ARM64E)
+#undef DEFINE_STACK_SWITCH_TRAMPOLINE_ARM64E
 
 #else // !CPU(ARM64E)
 
 IGNORE_WARNINGS_BEGIN("missing-noreturn")
-extern "C" void callThreadEntryPointFinishSetupWithNewStack(void*, void*)
-{
-    RELEASE_ASSERT_NOT_REACHED();
-}
+
+#define DEFINE_STACK_SWITCH_STUB(trampolineName, argType, argName, callExpression) \
+    extern "C" void trampolineName(argType, void*) \
+    { \
+        RELEASE_ASSERT_NOT_REACHED(); \
+    }
+
+WTF_FOR_EACH_STACK_SWITCH_TRAMPOLINE(DEFINE_STACK_SWITCH_STUB)
+#undef DEFINE_STACK_SWITCH_STUB
+
 IGNORE_WARNINGS_END
 
 #endif // CPU(ARM64E)
