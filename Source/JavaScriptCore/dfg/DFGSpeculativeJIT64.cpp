a9eb4a8d4cf518575e244fcb46353814bb542276
Sosuke Suzuki <sosuke@bun.com>
2026-02-22 16:24:43 +0900

    [JSC] Reduce register pressure for TypeOfIsUndefined in DFG https://bugs.webkit.org/show_bug.cgi?id=308422
    Reviewed by NOBODY (OOPS!).
    
    The localGlobalObject and remoteGlobalObject GPRTemporaries were
    unconditionally allocated, but they are only used when the
    masqueradesAsUndefined watchpoint is invalid. In the common case where
    the watchpoint is still valid, these two registers were locked
    unnecessarily, increasing register pressure for surrounding nodes.
    
    Use std::optional<GPRTemporary> to defer allocation to the case where
    the registers are actually needed, reducing locked GPRs from 4 to 2 in
    the common path.
    
    * Source/JavaScriptCore/dfg/DFGSpeculativeJIT64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):

--- Source/JavaScriptCore/dfg/DFGSpeculativeJIT64.cpp
+++ Source/JavaScriptCore/dfg/DFGSpeculativeJIT64.cpp
@@ -5163,17 +5163,23 @@ void SpeculativeJIT::compile(Node* node)
     case TypeOfIsUndefined: {
         JSValueOperand value(this, node->child1());
         GPRTemporary result(this);
-        GPRTemporary localGlobalObject(this);
-        GPRTemporary remoteGlobalObject(this);
+        std::optional<GPRTemporary> localGlobalObject;
+        std::optional<GPRTemporary> remoteGlobalObject;
+
+        bool watchpointValid = masqueradesAsUndefinedWatchpointSetIsStillValid();
+        if (!watchpointValid) {
+            localGlobalObject.emplace(this);
+            remoteGlobalObject.emplace(this);
+        }
 
         Jump isCell = branchIfCell(value.jsValueRegs());
 
         compare64(Equal, value.gpr(), TrustedImm32(JSValue::ValueUndefined), result.gpr());
         Jump done = jump();
-        
+
         isCell.link(this);
         Jump notMasqueradesAsUndefined;
-        if (masqueradesAsUndefinedWatchpointSetIsStillValid()) {
+        if (watchpointValid) {
             move(TrustedImm32(0), result.gpr());
             notMasqueradesAsUndefined = jump();
         } else {
@@ -5185,8 +5191,8 @@ void SpeculativeJIT::compile(Node* node)
             notMasqueradesAsUndefined = jump();
 
             isMasqueradesAsUndefined.link(this);
-            GPRReg localGlobalObjectGPR = localGlobalObject.gpr();
-            GPRReg remoteGlobalObjectGPR = remoteGlobalObject.gpr();
+            GPRReg localGlobalObjectGPR = localGlobalObject->gpr();
+            GPRReg remoteGlobalObjectGPR = remoteGlobalObject->gpr();
             loadLinkableConstant(LinkableConstant::globalObject(*this, node), localGlobalObjectGPR);
             emitLoadStructure(vm(), value.gpr(), result.gpr());
             loadPtr(Address(result.gpr(), Structure::globalObjectOffset()), remoteGlobalObjectGPR);
