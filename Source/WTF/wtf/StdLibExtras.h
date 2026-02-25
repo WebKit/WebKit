e98b8cffd72724947de83ede12029ec5ed30250e
Dan Hecht <dan.hecht@apple.com>
2026-02-25 12:07:11 -0800

    Revert "Clean up opaque atomic variables"
    This reverts commit dd44d905386dafa40def5c8c60a6e1181df80a00.

--- Source/WTF/wtf/StdLibExtras.h
+++ Source/WTF/wtf/StdLibExtras.h
@@ -1560,6 +1560,16 @@ struct SizedUnsignedTrait<8> {
 template<typename T>
 using SameSizeUnsignedInteger = SizedUnsignedTrait<sizeof(T)>::Type;
 
+// We use this primitive on ARM to express memory ordering efficiently.
+template<typename T>
+inline T* addOpaqueZero(T* pointer, unsigned opaqueZero)
+{
+WTF_ALLOW_UNSAFE_BUFFER_USAGE_BEGIN
+    // This is bounds-safe because opaqueZero is always zero.
+    return std::bit_cast<T*>(std::bit_cast<char*>(pointer) + opaqueZero);
+WTF_ALLOW_UNSAFE_BUFFER_USAGE_END
+}
+
 namespace Views {
 
 static constexpr auto dereferenceView = std::views::transform([](auto&& x) -> decltype(auto) { return *x; });
@@ -1582,6 +1592,7 @@ template<typename T, typename U> constexpr auto forward_like_preserving_const(U&
 using WTF::GB;
 using WTF::KB;
 using WTF::MB;
+using WTF::addOpaqueZero;
 using WTF::approximateBinarySearch;
 using WTF::asBytes;
 using WTF::asByteSpan;
