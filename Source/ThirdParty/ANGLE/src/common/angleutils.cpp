d017c45b0855e30f8f51ecf91eec7f4fe09d697a
Ruthvik Konda <rkonda2@apple.com>
2026-03-11 12:42:48 -0700

    Cherry-pick 309080@main (3df36642bc36). https://bugs.webkit.org/show_bug.cgi?id=309609
        ANGLE: Add mutex to MakeStaticString to fix data race crash
        https://bugs.webkit.org/show_bug.cgi?id=309609
        rdar://171048125
    
        Reviewed by Kimmo Kinnunen.
    
        MakeStaticString() in angleutils.cpp uses a function-local static
        std::set<std::string> without synchronization. When multiple
        SharedWorker threads simultaneously initialize WebGL contexts via
        OffscreenCanvas, concurrent find() and insert() operations corrupt
        the red-black tree, causing NULL pointer dereference crashes in
        __tree_balance_after_insert.
    
        Add angle::SimpleMutex to protect the static set, following the
        same heap-allocated mutex pattern used by debug.cpp's g_debugMutex.
    
        * Source/ThirdParty/ANGLE/src/common/angleutils.cpp:
        (MakeStaticString):
        * Source/ThirdParty/ANGLE/src/common/angleutils_unittest.cpp:
        (MakeStaticString_ThreadSafety_Test::TestBody):
    
        Canonical link: https://commits.webkit.org/309080@main
    
    Canonical link: https://commits.webkit.org/298234.493@webkitglib/2.50

--- Source/ThirdParty/ANGLE/src/common/angleutils.cpp
+++ Source/ThirdParty/ANGLE/src/common/angleutils.cpp
@@ -5,6 +5,7 @@
 //
 
 #include "common/angleutils.h"
+#include "common/SimpleMutex.h"
 #include "common/debug.h"
 
 #include <stdio.h>
@@ -132,7 +133,9 @@ const char *MakeStaticString(const std::string &str)
 {
     // On the heap so that no destructor runs on application exit.
     static std::set<std::string> *strings = new std::set<std::string>;
-    std::set<std::string>::iterator it    = strings->find(str);
+    static angle::SimpleMutex *mutex      = new angle::SimpleMutex;
+    std::lock_guard<angle::SimpleMutex> lock(*mutex);
+    std::set<std::string>::iterator it = strings->find(str);
     if (it != strings->end())
     {
         return it->c_str();
