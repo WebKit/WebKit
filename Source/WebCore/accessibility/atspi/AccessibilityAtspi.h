45d479cc4bea61d203925061f5895870aaa317b4
Lauro Moura <lmoura@igalia.com>
2026-03-12 17:54:52 -0700

    Cherry-pick 309174@main (57438b0e6983). https://bugs.webkit.org/show_bug.cgi?id=309693
        [GLIB] WebProcess critical assertion during D-Bus RequestName completion on process shutdown
        https://bugs.webkit.org/show_bug.cgi?id=309693
    
        Reviewed by Claudio Saavedra.
    
        While rare, there's a chance that the WebProcess exits while the
        AccessibilityAtspi dbus name callbacks are still in flight. If we don't
        close them gracefully, we risk critical errors when the reply arrives.
        This is likely related to 284894@main, which added the support for
        well-known bus name to AccessibilityAtspi.
    
        This happens, for example, in some WebDriver tests that quickly open and
        close browsing contexts. A run with release asserts is reporting almost
        30 crashes.
    
        This commit addresses this issue by adding a graceful cleanup path to
        AccessibilityAtspi.
    
        * Source/WebCore/accessibility/atspi/AccessibilityAtspi.cpp:
        (WebCore::AccessibilityAtspi::didConnect):
        (WebCore::AccessibilityAtspi::disconnect):
        * Source/WebCore/accessibility/atspi/AccessibilityAtspi.h:
        * Source/WebKit/WebProcess/glib/WebProcessGLib.cpp:
        (WebKit::WebProcess::stopRunLoop):
    
        Canonical link: https://commits.webkit.org/309174@main
    
    Canonical link: https://commits.webkit.org/298234.494@webkitglib/2.50

--- Source/WebCore/accessibility/atspi/AccessibilityAtspi.h
+++ Source/WebCore/accessibility/atspi/AccessibilityAtspi.h
@@ -53,6 +53,7 @@ public:
     void deref() const { }
 
     void connect(const String&, const String&);
+    WEBCORE_EXPORT void disconnect();
 
     const char* uniqueName() const;
     GVariant* nullReference() const;
@@ -135,6 +136,7 @@ private:
 
     String m_busName;
     bool m_isConnecting { false };
+    unsigned m_nameOwnerId { 0 };
     GRefPtr<GDBusConnection> m_connection;
     GRefPtr<GDBusProxy> m_registry;
     Vector<PendingRootRegistration> m_pendingRootRegistrations;
