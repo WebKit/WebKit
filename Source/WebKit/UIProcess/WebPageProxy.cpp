d1551df53d970b9881712c93ae4eabbb9cb31e9f
Chris Dumez <cdumez@apple.com>
2026-03-18 06:15:22 -0700

    didSameDocumentNavigationForFrame accepts arbitrary URL, enabling address bar spoofing https://bugs.webkit.org/show_bug.cgi?id=310073 rdar://172567659
    Reviewed by Ryosuke Niwa.
    
    Add a MESSAGE_CHECK to validate that the URL's protocol/host/port match
    the current frame's URL.
    
    * Source/WebKit/UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::didSameDocumentNavigationForFrameViaJS):
    
    Identifier: 305413.512@safari-7624-branch

--- Source/WebKit/UIProcess/WebPageProxy.cpp
+++ Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -8102,6 +8102,7 @@ void WebPageProxy::didSameDocumentNavigationForFrameViaJS(IPC::Connection& conne
 
     Ref process = WebProcessProxy::fromConnection(connection);
     MESSAGE_CHECK_URL(process, url);
+    MESSAGE_CHECK(process, url.protocolIsFile() || frame->url().isEmpty() || protocolHostAndPortAreEqual(url, frame->url()));
 
     WEBPAGEPROXY_RELEASE_LOG(Loading, "didSameDocumentNavigationForFrameViaJS: frameID=%" PRIu64 ", isMainFrame=%d, type=%u", frameID.toUInt64(), frame->isMainFrame(), enumToUnderlyingType(navigationType));
 
