e557102b0f7767f8c8983041d5bd6d666f760177
Said Abou-Hallawa <said@apple.com>
2026-03-18 17:45:29 -0700

    FEMorphologySoftwareApplier is not thread safe https://bugs.webkit.org/show_bug.cgi?id=308910 rdar://168776587
    Reviewed by Mike Wyrzykowski.
    
    The software morphology applier relies on threading the calculations using
    ParallelJobs. It splits the source image into vertical blocks and working on each
    block separately. All the jobs access the same source and the destination
    PixelBuffer. The problem is the PixelBuffer is not thread safe.
    
    To fix this issue, FEMorphologySoftwareApplier will create a source and a
    destination PixelBuffers for each job. Before executing the parallel jobs, it
    copies bytes from its source PixelBuffer to the jobs source PixelBuffers. After
    the calculation finishes, FEMorphologySoftwareApplier will collect the results
    from the destination PixelBuffers of the jobs to its destination PixelBuffer.
    
    * LayoutTests/svg/filters/feMorphology-radius-cases.svg:
    * Source/WebCore/WebCore.xcodeproj/project.pbxproj:
    * Source/WebCore/platform/graphics/filters/software/FEMorphologySoftwareApplier.cpp:
    (WebCore::FEMorphologySoftwareApplier::columnExtremum):
    (WebCore::FEMorphologySoftwareApplier::applyPlatformGeneric):
    (WebCore::FEMorphologySoftwareApplier::applyPlatformWorker):
    (WebCore::FEMorphologySoftwareApplier::applyPlatform):
    (WebCore::FEMorphologySoftwareApplier::apply const):
    * Source/WebCore/platform/graphics/filters/software/FEMorphologySoftwareApplier.h:
    * Source/WebCore/platform/graphics/filters/software/FilterEffectSoftwareParallelApplier.h: Added.
    (WebCore::applyPlatformParallel):
    
    Identifier: 305413.516@safari-7624-branch

--- Source/WebCore/platform/graphics/filters/software/FEMorphologySoftwareApplier.h
+++ Source/WebCore/platform/graphics/filters/software/FEMorphologySoftwareApplier.h
@@ -2,7 +2,7 @@
  * Copyright (C) 2004, 2005, 2006, 2007 Nikolas Zimmermann <zimmermann@kde.org>
  * Copyright (C) 2004, 2005 Rob Buis <buis@kde.org>
  * Copyright (C) 2005 Eric Seidel <eric@webkit.org>
- * Copyright (C) 2021-2022 Apple, Inc. All rights reserved.
+ * Copyright (C) 2021-2026 Apple, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -46,20 +46,14 @@ private:
 
     using ColumnExtrema = Vector<ColorComponents<uint8_t, 4>, 16>;
 
-    struct PaintingData {
-        MorphologyOperatorType type;
-        int radiusX;
-        int radiusY;
-        const PixelBuffer* srcPixelBuffer;
-        PixelBuffer* dstPixelBuffer;
-        int width;
-        int height;
-    };
-
     struct ApplyParameters {
-        const PaintingData* paintingData;
-        int startY;
-        int endY;
+        RefPtr<PixelBuffer> sourceBuffer;
+        RefPtr<PixelBuffer> destinationBuffer;
+        IntSize sourceSize;
+        IntRect destinationRect;
+
+        MorphologyOperatorType type;
+        IntSize radius;
     };
 
     static inline int pixelArrayIndex(int x, int y, int width) { return (y * width + x) * 4; }
@@ -67,12 +61,12 @@ private:
 
     static inline ColorComponents<uint8_t, 4> makeColorComponentsfromPixelValue(PackedColor::RGBA pixel) { return asColorComponents(asSRGBA(pixel).resolved()); }
     static inline ColorComponents<uint8_t, 4> minOrMax(const ColorComponents<uint8_t, 4>& a, const ColorComponents<uint8_t, 4>& b, MorphologyOperatorType);
-    static inline ColorComponents<uint8_t, 4> columnExtremum(const PixelBuffer& srcPixelBuffer, int x, int yStart, int yEnd, int width, MorphologyOperatorType);
+    static inline ColorComponents<uint8_t, 4> columnExtremum(const PixelBuffer& sourceBuffer, int x, int yStart, int yEnd, int width, MorphologyOperatorType);
     static inline ColorComponents<uint8_t, 4> kernelExtremum(const ColumnExtrema& kernel, MorphologyOperatorType);
 
-    static void applyPlatformGeneric(const PaintingData&, int startY, int endY);
+    static void applyPlatformGeneric(PixelBuffer& sourceBuffer, PixelBuffer& destinationBuffer, const IntSize& sourceSize, const IntRect& destinationRect, MorphologyOperatorType, const IntSize& radius);
     static void applyPlatformWorker(ApplyParameters*);
-    static void applyPlatform(const PaintingData&);
+    static bool applyPlatform(PixelBuffer& sourceBuffer, PixelBuffer& destinationBuffer, MorphologyOperatorType, const IntSize& radius);
 };
 
 } // namespace WebCore
