7bbbcf272deabe27253fda6af59971f342a5cb6e
Roberto Rodriguez <roberto_rodriguez2@apple.com>
2026-03-23 11:43:45 -0700

    Inherit creating document's CSP for SharedWorkers loaded from headerless URLs https://bugs.webkit.org/show_bug.cgi?id=308765 rdar://171287542
    Reviewed by Ryan Reno.
    
    SharedWorkerScriptLoader::notifyFinished() does not inherit the creating
    document's CSP when the worker URL carries no HTTP headers.
    Worker::didReceiveResponse() already handles this for DedicatedWorkers by
    checking for blob:, file:, and opaque-origin URLs.
    
    Apply the same check in SharedWorkerScriptLoader so that the creating
    context's CSP flows into the WorkerFetchResult before it reaches
    SharedWorkerGlobalScope.
    
    Tests: http/tests/security/contentSecurityPolicy/shared-worker-blob-url-inherits-csp-importScripts-blocked.html
           http/tests/security/contentSecurityPolicy/shared-worker-data-url-inherits-csp-importScripts-blocked.html
    
    * LayoutTests/http/tests/security/contentSecurityPolicy/shared-worker-blob-url-inherits-csp-importScripts-blocked-expected.txt: Added.
    * LayoutTests/http/tests/security/contentSecurityPolicy/shared-worker-blob-url-inherits-csp-importScripts-blocked.html: Added.
    * LayoutTests/http/tests/security/contentSecurityPolicy/shared-worker-data-url-inherits-csp-importScripts-blocked-expected.txt: Added.
    * LayoutTests/http/tests/security/contentSecurityPolicy/shared-worker-data-url-inherits-csp-importScripts-blocked.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/content-security-policy/script-src/worker-data-set-timeout.sub-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/upgrade-insecure-requests/gen/sharedworker-classic-data.http-rp/upgrade/fetch.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/upgrade-insecure-requests/gen/sharedworker-classic-data.http-rp/upgrade/websocket.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/upgrade-insecure-requests/gen/sharedworker-classic-data.http-rp/upgrade/xhr.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/upgrade-insecure-requests/gen/sharedworker-classic-data.meta/upgrade/fetch.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/upgrade-insecure-requests/gen/sharedworker-classic-data.meta/upgrade/websocket.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/upgrade-insecure-requests/gen/sharedworker-classic-data.meta/upgrade/xhr.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/upgrade-insecure-requests/gen/sharedworker-module-data.http-rp/upgrade/fetch.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/upgrade-insecure-requests/gen/sharedworker-module-data.http-rp/upgrade/websocket.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/upgrade-insecure-requests/gen/sharedworker-module-data.http-rp/upgrade/xhr.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/upgrade-insecure-requests/gen/sharedworker-module-data.meta/upgrade/fetch.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/upgrade-insecure-requests/gen/sharedworker-module-data.meta/upgrade/websocket.https-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/upgrade-insecure-requests/gen/sharedworker-module-data.meta/upgrade/xhr.https-expected.txt:
    * Source/WebCore/workers/shared/SharedWorkerScriptLoader.cpp:
    (WebCore::SharedWorkerScriptLoader::notifyFinished):
    
    Identifier: 305413.556@safari-7624-branch

--- Source/WebCore/workers/shared/SharedWorkerScriptLoader.cpp
+++ Source/WebCore/workers/shared/SharedWorkerScriptLoader.cpp
@@ -29,6 +29,7 @@
 #include "ContextDestructionObserverInlines.h"
 #include "EventNames.h"
 #include "InspectorInstrumentation.h"
+#include "SecurityOrigin.h"
 #include "SharedWorker.h"
 #include "WorkerFetchResult.h"
 #include "WorkerInitializationData.h"
@@ -84,6 +85,15 @@ void SharedWorkerScriptLoader::notifyFinished(std::optional<ScriptExecutionConte
     }
 
     auto fetchResult = m_loader->fetchResult();
+
+    // These URLs carry no HTTP headers; inherit the creating document's
+    // CSP, matching Worker::didReceiveResponse().
+    if (scriptExecutionContext) {
+        const auto& responseURL = fetchResult.responseURL;
+        if (responseURL.protocolIsBlob() || responseURL.protocolIsFile() || SecurityOrigin::create(responseURL)->isOpaque())
+            fetchResult.contentSecurityPolicy = scriptExecutionContext->checkedContentSecurityPolicy()->responseHeaders();
+    }
+
     if (fetchResult.referrerPolicy.isNull() && scriptExecutionContext)
         fetchResult.referrerPolicy = referrerPolicyToString(scriptExecutionContext->referrerPolicy());
     m_completionHandler(WTF::move(fetchResult), WorkerInitializationData {
