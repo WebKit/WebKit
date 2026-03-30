75f5535a9a3b2b6e50bfb22edd7b4ff06b99a160
Chris Dumez <cdumez@apple.com>
2026-03-30 05:41:10 -0700

    Cherry-pick 310216@main (6821aea7edf7). https://bugs.webkit.org/show_bug.cgi?id=311081
        Fix use-after-move in IndexSparseSet::remove()
        https://bugs.webkit.org/show_bug.cgi?id=311081
    
        Reviewed by Ryosuke Niwa.
    
        * Source/WTF/wtf/IndexSparseSet.h:
        (WTF::OverflowHandler>::remove):
    
        Canonical link: https://commits.webkit.org/310216@main
    
    Canonical link: https://commits.webkit.org/298234.518@webkitglib/2.50

--- Source/WTF/wtf/IndexSparseSet.h
+++ Source/WTF/wtf/IndexSparseSet.h
@@ -152,9 +152,8 @@ inline bool IndexSparseSet<EntryType, EntryTypeTraits, OverflowHandler>::remove(
         return false;
 
     if (EntryTypeTraits::key(m_values[position]) == value) {
-        EntryType lastValue = m_values.last();
-        m_values[position] = WTFMove(lastValue);
-        m_map[EntryTypeTraits::key(lastValue)] = position;
+        m_map[EntryTypeTraits::key(m_values.last())] = position;
+        m_values[position] = WTFMove(m_values.last());
         m_values.removeLast();
         return true;
     }
