034987454f853536715921b9af8b073390d9dbb0
Vitor Roriz <vitor.roriz@apple.com>
2026-03-30 16:09:38 -0700

    Cherry-pick a46cecace5fb. rdar://173518431
    CSS Zoom: Use Evaluation<LineHeight> for lh unit resolution
    https://bugs.webkit.org/show_bug.cgi?id=310379
    rdar://173021603
    
    Reviewed by Sammy Gill.
    
    Replace the tryFixed workaround in lh unit resolution with evaluate<float>
    using conversionData.zoom(), by using the Evaluation<LineHeight>
    specialization introduced in https://commits.webkit.org/309625@main.
    
    * Source/WebCore/style/values/primitives/StyleLengthResolution.cpp:
    (WebCore::Style::computeNonCalcLengthDouble):
    
    Canonical link: https://commits.webkit.org/309647@main
    Identifier: 305413.595@safari-7624-branch

--- Source/WebCore/style/values/primitives/StyleLengthResolution.cpp
+++ Source/WebCore/style/values/primitives/StyleLengthResolution.cpp
@@ -310,13 +310,11 @@ double computeNonCalcLengthDouble(double value, CSS::LengthUnit lengthUnit, cons
         if (conversionData.computingLineHeight() || conversionData.computingFontSize()) {
             // Try to get the parent's computed line-height, or fall back to the initial line-height of this element's font spacing.
             value *= conversionData.parentStyle() ? conversionData.parentStyle()->computedLineHeight() : conversionData.fontCascadeForFontUnits().metricsOfPrimaryFont().intLineSpacing();
-        } else if (auto fixedLineHeight = conversionData.style()->lineHeight().tryFixed()) {
-            // We can't use computedLineHeightForFontUnits if the line height is fixed since
-            // that will apply the usedZoomFactor. We probably should refactor it so that
-            // does not happen and we don't have to special case this scenario.
-            value *= Style::evaluate<LayoutUnit>(*fixedLineHeight, Style::ZoomFactor { conversionData.zoom() }).toFloat();
-        } else
-            value *= conversionData.computedLineHeightForFontUnits();
+        } else {
+            auto* style = conversionData.style();
+            Style::LineHeightEvaluationContext context { style->computedFontSize(), style->metricsOfPrimaryFont().lineSpacing() };
+            value *= Style::evaluate<float>(style->lineHeight(), context, Style::ZoomFactor { conversionData.zoom() });
+        }
         break;
 
     // MARK: "root font dependent" resolution
