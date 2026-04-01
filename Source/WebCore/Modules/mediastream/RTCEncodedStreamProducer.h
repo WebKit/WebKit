8d45e135c17e5ac5b9f40f0352df5896236693a3
Youenn Fablet <youenn@apple.com>
2026-04-01 12:54:19 -0700

    Type Confusion in RTCEncodedStreamProducer.cpp Results in OOB Read https://bugs.webkit.org/show_bug.cgi?id=311131 rdar://173718825
    Reviewed by Eric Carlson.
    
    We add a check in RTCEncodedStreamProducer::writeFrame that we can only enqueue a video frame on a video sender/receiver,
    and audio frame on an audio sender/receiver.
    We should also allow write frames in a WritableStream that are generated from the corresponding ReadableStream.
    We add the check in RTCEncodedStreamProducer::writeFrame for RTCRtpScriptTransformer and
    
    Covered by updated test.
    
    * LayoutTests/http/wpt/webrtc/audio-video-transform.js:
    (AudioVideoRTCRtpTransformer):
    (AudioVideoRTCRtpTransformer.prototype.process):
    (onrtctransform): Deleted.
    * LayoutTests/http/wpt/webrtc/audiovideo-script-transform-expected.txt:
    * LayoutTests/http/wpt/webrtc/audiovideo-script-transform.html:
    * Source/WebCore/Modules/mediastream/RTCEncodedStreamProducer.cpp:
    (WebCore::RTCEncodedStreamProducer::start):
    (WebCore::RTCEncodedStreamProducer::enqueueFrame):
    (WebCore::RTCEncodedStreamProducer::writeFrame):
    * Source/WebCore/Modules/mediastream/RTCEncodedStreamProducer.h:
    * Source/WebCore/Modules/mediastream/RTCRtpScriptTransformer.cpp:
    (WebCore::RTCRtpScriptTransformer::start):
    * Source/WebCore/Modules/mediastream/RTCRtpTransformableFrame.h:
    (WebCore::RTCRtpTransformableFrame::isFromTransformer const):
    (WebCore::RTCRtpTransformableFrame::setTransformer):
    
    Identifier: 305413.606@safari-7624-branch

--- Source/WebCore/Modules/mediastream/RTCEncodedStreamProducer.h
+++ Source/WebCore/Modules/mediastream/RTCEncodedStreamProducer.h
@@ -53,7 +53,7 @@ public:
     static ExceptionOr<Ref<RTCEncodedStreamProducer>> create(ScriptExecutionContext&);
     ~RTCEncodedStreamProducer();
 
-    void start(Ref<RTCRtpTransformBackend>&&, bool isVideo);
+    void start(Ref<RTCRtpTransformBackend>&&, bool isVideo, RTCRtpScriptTransformer* = nullptr);
     void clear(bool shouldClearCallback);
 
     void generateKeyFrame(ScriptExecutionContext&, const String&, Ref<DeferredPromise>&&);
@@ -82,6 +82,8 @@ private:
     RefPtr<RTCRtpTransformBackend> m_transformBackend;
     Vector<Ref<DeferredPromise>> m_pendingKeyFramePromises;
     bool m_isVideo { false };
+    bool m_hasTransformer { false };
+    WeakPtr<RTCRtpScriptTransformer> m_transformer;
 
 #if !RELEASE_LOG_DISABLED
     bool m_enableAdditionalLogging { false };
