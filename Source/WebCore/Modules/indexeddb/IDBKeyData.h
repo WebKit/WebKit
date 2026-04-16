39624d0524162c5f9705e4db0da807bf3251a000
Sihui Liu <sihui_liu@apple.com>
2026-04-15 22:06:43 -0700

    Cherry-pick 3efb02c6bee1. rdar://174327363
        [IndexedDB] IDBKeyData operator== and operator<=> define different equivalence classes
        rdar://174327363
    
        Reviewed by Brady Eidson.
    
        IDBKeyData::operator== and IDBKeyData::operator<=> currently disagree in two cases:
        1. String-typed keys: operator== (via WTF::String::operator==) distinguishes null String from empty String, but
        operator<=> (via codePointCompare) treats them as equivalent.
        2. Invalid-typed keys: operator== checks isNull() to distinguish nullptr_t from Invalid{}, but operator<=> treats all
        Invalid-typed keys as equivalent.
    
        This could cause inconsistency in containers of MemoryIDBBackingStore, specifically HashMap keyed by operator== and
        std::set ordered by operator<=>, and defeats cursor-invalidation guards (which compare with operator== while erases use
        operator<). This patch makes sure IDBKeyData::operator<=> match IDBKeyData::operator== for equality check.
    
        API tests: IDBKeyData.NullAndInvalidAreNotEquivalentInvalidKey
                   IDBKeyData.NullStringAndEmptyStringAreNotEquivalentStringKey
                   IDBKeyData.SetDistinguishesNullAndInvalid
                   IDBKeyData.SetDistinguishesNullStringAndEmptyString
    
        * Source/WebCore/Modules/indexeddb/IDBKeyData.cpp:
        (WebCore::operator<=>):
        * Source/WebCore/Modules/indexeddb/IDBKeyData.h:
        * Tools/TestWebKitAPI/CMakeLists.txt:
        * Tools/TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
        * Tools/TestWebKitAPI/Tests/WebCore/IDBKeyData.cpp: Added.
        (TestWebKitAPI::TEST(IDBKeyData, NullAndInvalidAreNotEquivalentInvalidKey)):
        (TestWebKitAPI::TEST(IDBKeyData, NullStringAndEmptyStringAreNotEquivalentStringKey)):
        (TestWebKitAPI::TEST(IDBKeyData, SetDistinguishesNullAndInvalid)):
        (TestWebKitAPI::TEST(IDBKeyData, SetDistinguishesNullStringAndEmptyString)):
    
        Identifier: 305413.663@safari-7624-branch
    
    Canonical link: https://commits.webkit.org/305413.666@safari-7624.2.5.11-branch

--- Source/WebCore/Modules/indexeddb/IDBKeyData.h
+++ Source/WebCore/Modules/indexeddb/IDBKeyData.h
@@ -99,11 +99,11 @@ public:
     bool isNull() const { return std::holds_alternative<std::nullptr_t>(m_value); }
     bool isValid() const;
     WEBCORE_EXPORT static bool isValidValue(const ValueVariant&);
-    IndexedDB::KeyType type() const;
+    WEBCORE_EXPORT IndexedDB::KeyType type() const;
 
     WEBCORE_EXPORT friend std::weak_ordering operator<=>(const IDBKeyData&, const IDBKeyData&);
 
-    bool operator==(const IDBKeyData& other) const;
+    WEBCORE_EXPORT bool operator==(const IDBKeyData& other) const;
 
     String string() const
     {
