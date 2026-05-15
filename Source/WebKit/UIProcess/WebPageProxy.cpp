61687f3bd8d1c6a1b7e12122018d681ac656b37f
Franco Vieira de Souza <franco_vieira@apple.com>
2026-05-15 14:25:38 -0700

    Cherry-pick 53ed150fb399. rdar://177192024
        Don't block page loading due pending SafeBrowsing result
        https://bugs.webkit.org/show_bug.cgi?id=313163
        rdar://165058397
    
        Reviewed by Pascoe.
    
        WebPageProxy::decidePolicyForResponseShared() no longer waits for
        the SafeBrowsing request to be completed before responding. This
        unblocks the WebContent process to proceed with the load. This is not
        a bypass of the SafeBrowsing logic since a warning that arrives later
        is still handled and shown.
    
        * Source/WebKit/UIProcess/API/APINavigation.h:
        (API::Navigation::setSafeBrowsingCheckTimedOut): Deleted.
        (API::Navigation::safeBrowsingCheckTimedOut): Deleted.
        * Source/WebKit/UIProcess/WebPageProxy.cpp:
        (WebKit::WebPageProxy::decidePolicyForResponseShared):
    
        Identifier: 305413.801@safari-7624-branch
    
    
    (cherry picked from commit 06292549834a740104f05a2566fcb9ad7a75a955)
    
    Canonical link: https://commits.webkit.org/305413.683@safari-7624.4-branch

--- Source/WebKit/UIProcess/WebPageProxy.cpp
+++ Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -8906,14 +8906,6 @@ void WebPageProxy::decidePolicyForResponseShared(Ref<WebProcessProxy>&& process,
         m_openedMainFrameName = { };
     }
 
-    auto expectSafeBrowsing = ShouldExpectSafeBrowsingResult::No;
-    MonotonicTime requestStart;
-
-    if (navigation && navigation->safeBrowsingCheckOngoing()) {
-        expectSafeBrowsing = ShouldExpectSafeBrowsingResult::Yes;
-        requestStart = navigation->requestStart();
-    }
-
     Ref listener = frame->setUpPolicyListenerProxy([
         this,
         protectedThis = Ref { *this },
@@ -9042,14 +9034,7 @@ void WebPageProxy::decidePolicyForResponseShared(Ref<WebProcessProxy>&& process,
         }
 #endif
         completionHandlerWrapper(policyAction);
-    }, expectSafeBrowsing , ShouldExpectAppBoundDomainResult::No, ShouldWaitForInitialLinkDecorationFilteringData::No, ShouldWaitForSiteHasStorageCheck::No);
-    if (expectSafeBrowsing == ShouldExpectSafeBrowsingResult::Yes && navigation) {
-        Seconds timeout = (MonotonicTime::now() - requestStart) * 1.5 + 0.25_s;
-        RunLoop::mainSingleton().dispatchAfter(timeout, [listener, navigation] mutable {
-            listener->didReceiveSafeBrowsingResults({ });
-            navigation->setSafeBrowsingCheckTimedOut();
-        });
-    }
+    }, ShouldExpectSafeBrowsingResult::No, ShouldExpectAppBoundDomainResult::No, ShouldWaitForInitialLinkDecorationFilteringData::No, ShouldWaitForSiteHasStorageCheck::No);
 
     if (m_policyClient)
         m_policyClient->decidePolicyForResponse(*this, *frame, response, request, canShowMIMEType, WTF::move(listener));
