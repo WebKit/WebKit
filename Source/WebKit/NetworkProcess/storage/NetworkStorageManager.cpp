d1eaa8cf28cb1d3cbd206bef919948234a255569
Chris Dumez <cdumez@apple.com>
2026-05-21 05:46:30 +0000

    Bump QoS of the work queue during service worker registrations import https://bugs.webkit.org/show_bug.cgi?id=309626 rdar://172236600
    Reviewed by Sihui Liu.
    
    Bump QoS of the work queue during service worker registrations import
    from disk. Navigations are delayed until the import is complete and it
    is thus important to treat the import with high (UserInitiated) priority.
    
    * Source/WTF/wtf/SuspendableWorkQueue.cpp:
    (WTF::SuspendableWorkQueue::dispatchWithQOS):
    * Source/WTF/wtf/SuspendableWorkQueue.h:
    * Source/WebKit/NetworkProcess/storage/NetworkStorageManager.cpp:
    (WebKit::NetworkStorageManager::importServiceWorkerRegistrations):
    
    Originally-landed-as: 305413.439@rapid/safari-7624.2.5.110-branch (33ae1ab583c7). rdar://176067106

--- Source/WebKit/NetworkProcess/storage/NetworkStorageManager.cpp
+++ Source/WebKit/NetworkProcess/storage/NetworkStorageManager.cpp
@@ -2447,9 +2447,15 @@ void NetworkStorageManager::importServiceWorkerRegistrationsForOrigin(const WebC
     if (m_closed)
         return completionHandler(std::nullopt);
 
+<<<<<<< HEAD
     RELEASE_LOG(Storage, "%p - NetworkStorageManager::importServiceWorkerRegistrationsForOrigin: Starting import", this);
     auto startTime = MonotonicTime::now();
     workQueue().dispatchWithQOS([this, protectedThis = Ref { *this }, topOrigin = crossThreadCopy(topOrigin), startTime, completionHandler = WTF::move(completionHandler)]() mutable {
+=======
+    RELEASE_LOG(Storage, "%p - NetworkStorageManager::importServiceWorkerRegistrations: Starting import", this);
+    auto startTime = MonotonicTime::now();
+    workQueue().dispatchWithQOS([this, protectedThis = Ref { *this }, startTime, completionHandler = WTF::move(completionHandler)]() mutable {
+>>>>>>> 33ae1ab583c7 (Bump QoS of the work queue during service worker registrations import)
         assertIsCurrent(workQueue());
 
         std::optional<Vector<WebCore::ServiceWorkerContextData>> result;
@@ -2470,6 +2476,7 @@ void NetworkStorageManager::importServiceWorkerRegistrationsForOrigin(const WebC
         }
 
         RunLoop::mainSingleton().dispatch([protectedThis = WTF::move(protectedThis), startTime, result = crossThreadCopy(WTF::move(result)), completionHandler = WTF::move(completionHandler)]() mutable {
+<<<<<<< HEAD
 #if !RELEASE_LOG_DISABLED
             auto elapsed = MonotonicTime::now() - startTime;
             RELEASE_LOG(Storage, "%p - NetworkStorageManager::importServiceWorkerRegistrationsForOrigin: Imported %zu registrations in %.0f ms", protectedThis.ptr(), result ? result->size() : 0, elapsed.milliseconds());
@@ -2519,6 +2526,16 @@ void NetworkStorageManager::importServiceWorkerOriginList(CompletionHandler<void
             completionHandler(WTF::move(result));
         });
     }, WorkQueue::QOS::UserInitiated);
+=======
+            auto elapsed = MonotonicTime::now() - startTime;
+            if (elapsed > 2_s)
+                RELEASE_LOG_ERROR(Storage, "%p - NetworkStorageManager::importServiceWorkerRegistrations: Imported %zu registrations in %.0f ms", protectedThis.ptr(), result ? result->size() : 0, elapsed.milliseconds());
+            else
+                RELEASE_LOG(Storage, "%p - NetworkStorageManager::importServiceWorkerRegistrations: Imported %zu registrations in %.0f ms", protectedThis.ptr(), result ? result->size() : 0, elapsed.milliseconds());
+            completionHandler(WTF::move(result));
+        });
+    }, WorkQueue::QOS::UserInitiated);
+>>>>>>> 33ae1ab583c7 (Bump QoS of the work queue during service worker registrations import)
 }
 
 void NetworkStorageManager::updateServiceWorkerRegistrations(Vector<WebCore::ServiceWorkerContextData>&& registrationsToUpdate, Vector<WebCore::ServiceWorkerRegistrationKey>&& registrationsToDelete, CompletionHandler<void(std::optional<Vector<WebCore::ServiceWorkerScripts>>)>&& completionHandler)
