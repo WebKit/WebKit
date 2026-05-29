58e96970567d335bdacf060ba089f1e017a69128
Ryosuke Niwa <rniwa@webkit.org>
2026-05-29 06:21:46 +0000

    Use-after-free in Element::dispatchBlurEvent https://bugs.webkit.org/show_bug.cgi?id=312365 rdar://174646151
    Reviewed by Rupin Mittal, Chris Dumez, and Anne van Kesteren.
    
    Deployed more smart pointers to fix the bug.
    
    Test: fast/events/blur-window-with-focus-in-shadow-tree-crash.html
    
    * LayoutTests/fast/events/blur-window-with-focus-in-shadow-tree-crash-expected.txt: Added.
    * LayoutTests/fast/events/blur-window-with-focus-in-shadow-tree-crash.html: Added.
    * Source/WebCore/page/FocusController.cpp:
    (WebCore::dispatchEventsOnWindowAndFocusedElement):
    
    Originally-landed-as: 305413.673@safari-7624-branch (f045966f731c). rdar://176058798

--- Source/WebCore/page/FocusController.cpp
+++ Source/WebCore/page/FocusController.cpp
@@ -418,26 +418,30 @@ static inline void dispatchEventsOnWindowAndFocusedElement(Document* document, b
             return;
     }
 
-    if (!focused && document->focusedElement()) {
-        if (document->focusedElement()->transferredFocusToPicker()) {
+    if (RefPtr focusedElement = document->focusedElement(); !focused && focusedElement) {
+        if (focusedElement->transferredFocusToPicker()) {
             // The webpage lost focus because the focused element transferred focus to
             // a non-web-content picker when it was activated. We don't want to post any
             // web-exposed events (e.g. blur) in these cases, so return.
-            document->focusedElement()->didSuppressBlurDueToPickerFocusTransfer();
+            focusedElement->didSuppressBlurDueToPickerFocusTransfer();
             return;
         }
 
+<<<<<<< HEAD
         if (RefPtr formControlElement = dynamicDowncast<HTMLFormControlElement>(*document->focusedElement())) {
             if (formControlElement->wasChangedSinceLastFormControlChangeEvent())
                 formControlElement->dispatchFormControlChangeEvent();
         }
 
         document->focusedElement()->dispatchBlurEvent(nullptr);
+=======
+        focusedElement->dispatchBlurEvent(nullptr);
+>>>>>>> f045966f731c (Use-after-free in Element::dispatchBlurEvent)
     }
 
     document->dispatchWindowEvent(Event::create(focused ? eventNames().focusEvent : eventNames().blurEvent, Event::CanBubble::No, Event::IsCancelable::No));
-    if (focused && document->focusedElement())
-        document->focusedElement()->dispatchFocusEvent(nullptr, { });
+    if (RefPtr focusedElement = document->focusedElement(); focused && focusedElement)
+        focusedElement->dispatchFocusEvent(nullptr, { });
 }
 
 static inline bool isFocusableElementOrScopeOwner(Element& element, const FocusEventData& focusEventData)
