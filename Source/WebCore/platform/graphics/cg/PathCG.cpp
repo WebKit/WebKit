9767704e16593f56a6ec79889e5781ebd94a318b
Said Abou-Hallawa <said@apple.com>
2026-05-31 05:54:20 -0700

    Cherry-pick 232dada9ad29. rdar://176138322
        PathCG::strokeContains() is not thread safe
        https://bugs.webkit.org/show_bug.cgi?id=313935
        rdar://176138322
    
        Reviewed by Darin Adler.
    
        PathCG::strokeContains() is not thread-safe because it accesses a global
        CGContextRef managed by NeverDestroyed in scratchContext(). If multiple offscreen
        canvases call CanvasRenderingContext2D:isPointInStroke() on worker threads, they
        are going to call PathCG::strokeContains(). This means the same CGContextRef will
        be used by these workers. This lake of global variable synchronization can lead
        to all sort of security and correctness bugs depending on what was set in the
        context when it is referenced.
    
        The fix is to protect scratchContext() by a locker. This will allow only one caller
        to have access to the underlying CGContextRef.
    
        Test: fast/canvas/offscreen-worker-unsafe-isPointInStroke-call.html
    
        * LayoutTests/fast/canvas/offscreen-worker-unsafe-isPointInStroke-call-expected.txt: Added.
        * LayoutTests/fast/canvas/offscreen-worker-unsafe-isPointInStroke-call.html: Added.
        * Source/WebCore/platform/graphics/cg/PathCG.cpp:
        (WebCore::PathCG::strokeContains const):
        (WebCore::PathCG::strokeBoundingRect const):
    
        Identifier: 305413.891@safari-7624-branch
    
    Identifier: 305413.784@safari-7624.4-branch

--- Source/WebCore/platform/graphics/cg/PathCG.cpp
+++ Source/WebCore/platform/graphics/cg/PathCG.cpp
@@ -605,6 +605,9 @@ bool PathCG::strokeContains(const FloatPoint& point, NOESCAPE const Function<voi
 {
     ASSERT(strokeStyleApplier);
 
+    static Lock scratchContextLock;
+    Locker locker { scratchContextLock };
+
     CGContextRef context = scratchContext();
 
     CGContextSaveGState(context);
@@ -640,6 +643,9 @@ FloatRect PathCG::boundingRect() const
 
 FloatRect PathCG::strokeBoundingRect(NOESCAPE const Function<void(GraphicsContext&)>& strokeStyleApplier) const
 {
+    static Lock scratchContextLock;
+    Locker locker { scratchContextLock };
+
     CGContextRef context = scratchContext();
 
     CGContextSaveGState(context);
