5329e9b444e178516b88d520b6a555ad035f664f
Justin Michaud <jmichaud@igalia.com>
2026-06-03 16:19:02 -0600

    Add FTL-only benchmark mode for JetStream3 https://bugs.webkit.org/show_bug.cgi?id=316258
    Reviewed by NOBODY (OOPS!).
    
    This patch adds new metrics to measure FTL compile times in JS3,
    as well as peak performance.
    
    The motivation is that it can be difficult to separate the effect
    of FTL changes that improve generated code, but hurt compile times.
    
    This can cause us to make decisions about which optimizations to keep
    or remove based on sensitive peculiarites of how JS3 tiers up. This lets
    us be sure.
    
    These changes were generated by LLM tooling. If it proves valuable,
    I will look into cleaning it up further.
    
    * PerformanceTests/JetStream3/ftl-only-cli.js: Added.
    (reportUnsupported):
    (const.dfgFtlCompileMs):
    (const.ftlDfgCount):
    (__benchmark.prepareForNextIteration.__benchmark.prepareForNextIteration):
    (warmupIterations.WARMUP_BUDGET.v.const.start.performance.now):
    (top.currentResolve):
    (async try):
    (catch):
    * Source/JavaScriptCore/bytecode/CodeBlock.cpp:
    (JSC::CodeBlock::shouldJettisonDueToOldAge):
    * Source/JavaScriptCore/bytecode/CodeBlock.h:
    (JSC::CodeBlock::resetOSRExitCounter):
    * Source/JavaScriptCore/jit/JIT.cpp:
    (JSC::JIT::resetCompileTimeStats):
    (JSC::JIT::totalDFGCompileTimeValue):
    (JSC::JIT::totalFTLCompileTimeValue):
    (JSC::JIT::totalFTLDFGCompileTimeValue):
    (JSC::JIT::totalFTLB3CompileTimeValue):
    * Source/JavaScriptCore/jit/JIT.h:
    * Source/JavaScriptCore/runtime/OptionsList.h:
    * Source/JavaScriptCore/tools/CompilerTimingScope.cpp:
    (JSC::phaseTimeTotals):
    (JSC::resetPhaseTimeTotals):
    * Source/JavaScriptCore/tools/CompilerTimingScope.h:
    * Source/JavaScriptCore/tools/JSDollarVM.cpp:
    (JSC::JSC_DEFINE_HOST_FUNCTION):
    (JSC::JSDollarVM::finishCreation):
    * Tools/Scripts/compare-ftl-only-results: Added.
    (_load_compare_results):
    (load):
    (single_metric_values):
    (breakdown):
    (median):
    (subtest_keys):
    (shared_subtests):
    (restrict):
    (suite_summary):
    (suite_summary.series):
    (check_function_counts):
    (main):
    * Tools/Scripts/compare-results:
    (computeMultipleHypothesesSignificance):
    * Tools/Scripts/run-ftl-only-jsc-benchmarks: Added.
    (load_subtests):
    (to_score):
    (median):
    (run_one):
    (collect):
    (_nested):
    (build_score_json):
    (build_compile_json):
    (build_phase_json):
    (build_osrexits_json):
    (build_funcs_json):
    (write_json):
    (main):

--- Source/JavaScriptCore/bytecode/CodeBlock.cpp
+++ Source/JavaScriptCore/bytecode/CodeBlock.cpp
@@ -1297,12 +1297,15 @@ ALWAYS_INLINE bool CodeBlock::shouldJettisonDueToOldAge(const ConcurrentJSLocker
     if (visitor.isMarked(this))
         return false;
 
+    if (Options::disableCodeBlockJettisonDueToOldAge()) [[unlikely]]
+        return false;
+
     if (Options::forceCodeBlockToJettisonDueToOldAge()) [[unlikely]]
         return true;
-    
+
     if (timeSinceCreation() < timeToLive(jitType()))
         return false;
-    
+
     return true;
 }
 
