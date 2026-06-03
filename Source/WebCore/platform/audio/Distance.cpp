24f1c91b75334dd093f32ac65d9a69991696a689
Chris Dumez <cdumez@apple.com>
2026-06-03 13:04:20 +0800

    PannerNode produces non-finite samples for edge-case distance parameters https://bugs.webkit.org/show_bug.cgi?id=316174
    Reviewed by NOBODY (OOPS!).
    
    The Web Audio specification permits configurations that make the distance
    gain formulas degenerate:
    - Linear model with refDistance == maxDistance: divides by zero in
      1 - rolloff * (d - ref) / (max - ref).
    - Inverse and exponential models with refDistance == 0: divide by zero
      in ref / (ref + ...) and pow(d / ref, ...) respectively.
    
    These produced NaN / Inf gains that propagated into the rendered audio.
    Guard each formula by returning the well-defined limit, matching the
    behavior of Blink and Gecko:
    - linearGain() returns 1 - rolloffFactor when refDistance == maxDistance.
    - inverseGain() and exponentialGain() return 0 when refDistance == 0.
    
    Test: imported/w3c/web-platform-tests/webaudio/the-audio-api/the-pannernode-interface/panner-non-finite-distance-params.html
    This test is passing in Chrome and Firefox, but failing in shipping Safari.
    
    * LayoutTests/imported/w3c/web-platform-tests/webaudio/the-audio-api/the-pannernode-interface/panner-non-finite-distance-params-expected.txt: Added.
    * LayoutTests/imported/w3c/web-platform-tests/webaudio/the-audio-api/the-pannernode-interface/panner-non-finite-distance-params.html: Added.
    * Source/WebCore/platform/audio/Distance.cpp:
    (WebCore::DistanceEffect::linearGain const):
    (WebCore::DistanceEffect::inverseGain const):
    (WebCore::DistanceEffect::exponentialGain const):

--- Source/WebCore/platform/audio/Distance.cpp
+++ Source/WebCore/platform/audio/Distance.cpp
@@ -66,6 +66,9 @@ double DistanceEffect::gain(double distance) const
 double DistanceEffect::linearGain(double distance) const
 {
     auto clampedRolloffFactor = std::clamp(m_rolloffFactor, 0.0, 1.0);
+    // The spec permits refDistance == maxDistance; avoid the resulting division by zero.
+    if (m_refDistance == m_maxDistance)
+        return 1.0 - clampedRolloffFactor;
     // We want a gain that decreases linearly from m_refDistance to
     // m_maxDistance. The gain is 1 at m_refDistance.
     return (1.0 - clampedRolloffFactor * (distance - m_refDistance) / (m_maxDistance - m_refDistance));
@@ -73,11 +76,15 @@ double DistanceEffect::linearGain(double distance) const
 
 double DistanceEffect::inverseGain(double distance) const
 {
+    if (!m_refDistance)
+        return 0;
     return m_refDistance / (m_refDistance + m_rolloffFactor * (distance - m_refDistance));
 }
 
 double DistanceEffect::exponentialGain(double distance) const
 {
+    if (!m_refDistance)
+        return 0;
     return pow(distance / m_refDistance, -m_rolloffFactor);
 }
 
