1dffbce416c9b693e10282ff56d24e9d3dfaf454
Brent Fulgham <bfulgham@apple.com>
2026-06-09 20:54:53 -0700

    Cherry-pick a9cd5d298884. rdar://174857144
        REGRESSION (Safari 26.4): Scaling the page resulting in length unit `ic` error
        https://bugs.webkit.org/show_bug.cgi?id=310580
        rdar://173198587
    
        Reviewed by Vitor Roriz.
    
        This bug report was already a FIXME in the code.
    
        In computeUnzoomedNonCalcLengthDouble, raw font metrics (ideogramWidth, xHeight, zeroWidth,
        capHeight) are measured from the zoomed rendered font. In the new evaluation-time zoom
        path, these metrics must be divided by usedZoomFactor to match how em strips zoom via
        computedSizeForRangeZoomOption(Unzoomed). Without this, 1ic (and 1ex, 1ch, 1cap) would
        scale with CSS zoom while 1em did not.
    
        This work exposed the presence of an existing bug in the ic unit: We were falling back to
        0, rather than 1em when the font lacked the relevant ideograph glyph. This was fixed, and
        a new WPT created to guard against this regressing in the future.
    
        This patch converts the existing fast/css/ic-unit-zoom.html test into a WPT that covers
        the four length types under CSS Zoom beheavior. It also adds a new WPT to confirm that
        the engine properly handles the case of a font lacking the ideograph glyph.
    
        Tests: imported/w3c/web-platform-tests/css/css-values/ic-unit-016.html
               imported/w3c/web-platform-tests/css/css-viewport/zoom/ic-unit.html
    
        * LayoutTests/imported/w3c/web-platform-tests/css/css-values/ic-unit-016-expected.html: Added.
        * LayoutTests/imported/w3c/web-platform-tests/css/css-values/ic-unit-016.html: Added.
        * LayoutTests/imported/w3c/web-platform-tests/css/css-values/reference/ic-unit-016-ref.html: Added.
        * LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/ic-unit-expected.txt: Added.
        * LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/ic-unit.html: Added.
        * Source/WebCore/rendering/mathml/RenderMathMLBlock.cpp:
        (WebCore::toUserUnits): Correct regression with evaluation time zoom (avoid double-zoom).
        * Source/WebCore/style/values/primitives/StyleLengthResolution.cpp:
        (WebCore::Style::unzoomFontMetricIfNeeded): Added.
        (WebCore::Style::computeUnzoomedNonCalcLengthDouble): Adopt unzoomFontMetricIfNeeded to use
        correct zoom state. Also fix a fallback bug for the ic unit when a font lacks the necessary
        glyphs.
        (WebCore::Style::computeNonCalcLengthDouble): Remove unnecessary comment.
    
        Canonical link: https://commits.webkit.org/311238@main
    
    Canonical link: https://commits.webkit.org/305413.892@safari-7624.4-branch

--- Source/WebCore/style/values/primitives/StyleLengthResolution.cpp
+++ Source/WebCore/style/values/primitives/StyleLengthResolution.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2025 Samuel Weinig <sam@webkit.org>
+ * Copyright (C) 2026 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -85,6 +86,18 @@ static double lengthOfViewportPhysicalAxisForLogicalAxis(LogicalBoxAxis logicalA
     return lengthOfViewportPhysicalAxisForLogicalAxis(logicalAxis, size, rootElement->renderStyle());
 }
 
+// Raw font metrics include the usedZoomFactor. In evaluation-time zoom mode with the Unzoomed
+// range option, normalize by usedZoomFactor so that font-relative units (ex, cap, ch, ic) scale
+// consistently with em.
+static double unzoomFontMetricIfNeeded(double metric, const FontDescription& fontDescription, CSS::RangeZoomOptions rangeZoomOption)
+{
+    if (fontDescription.evaluationTimeZoomEnabled() && rangeZoomOption == CSS::RangeZoomOptions::Unzoomed) {
+        if (auto usedZoomFactor = fontDescription.usedZoomFactor(); usedZoomFactor > 0)
+            return metric / usedZoomFactor;
+    }
+    return metric;
+}
+
 double computeUnzoomedNonCalcLengthDouble(double value, CSS::LengthUnit lengthUnit, CSSPropertyID propertyToCompute, const FontCascade* fontCascadeForUnit, CSS::RangeZoomOptions rangeZoomOption, const RenderView* renderView)
 {
     using enum CSS::LengthUnit;
@@ -117,28 +130,36 @@ double computeUnzoomedNonCalcLengthDouble(double value, CSS::LengthUnit lengthUn
     case Ex:
     case Rex: {
         ASSERT(fontCascadeForUnit);
+        auto& fontDescription = fontCascadeForUnit->fontDescription();
         auto& fontMetrics = fontCascadeForUnit->metricsOfPrimaryFont();
         if (fontMetrics.xHeight())
-            return fontMetrics.xHeight().value() * value;
-        auto& fontDescription = fontCascadeForUnit->fontDescription();
+            return unzoomFontMetricIfNeeded(fontMetrics.xHeight().value(), fontDescription, rangeZoomOption) * value;
         return ((propertyToCompute == CSSPropertyFontSize) ? fontDescription.specifiedSize() : fontDescription.computedSizeForRangeZoomOption(rangeZoomOption)) / 2.0 * value;
     }
     case Cap:
     case Rcap: {
         ASSERT(fontCascadeForUnit);
+        auto& fontDescription = fontCascadeForUnit->fontDescription();
         auto& fontMetrics = fontCascadeForUnit->metricsOfPrimaryFont();
         if (fontMetrics.capHeight())
-            return fontMetrics.capHeight().value() * value;
-        return fontMetrics.intAscent() * value;
+            return unzoomFontMetricIfNeeded(fontMetrics.capHeight().value(), fontDescription, rangeZoomOption) * value;
+        return unzoomFontMetricIfNeeded(fontMetrics.intAscent(), fontDescription, rangeZoomOption) * value;
     }
     case Ch:
-    case Rch:
+    case Rch: {
         ASSERT(fontCascadeForUnit);
-        return fontCascadeForUnit->zeroWidth() * value;
+        auto& fontDescription = fontCascadeForUnit->fontDescription();
+        return unzoomFontMetricIfNeeded(fontCascadeForUnit->zeroWidth(), fontDescription, rangeZoomOption) * value;
+    }
     case Ic:
-    case Ric:
+    case Ric: {
         ASSERT(fontCascadeForUnit);
-        return fontCascadeForUnit->metricsOfPrimaryFont().ideogramWidth().value_or(0) * value;
+        auto& fontDescription = fontCascadeForUnit->fontDescription();
+        auto ideogramWidth = fontCascadeForUnit->metricsOfPrimaryFont().ideogramWidth();
+        if (!ideogramWidth)
+            return fontDescription.computedSizeForRangeZoomOption(rangeZoomOption) * value;
+        return unzoomFontMetricIfNeeded(ideogramWidth.value(), fontDescription, rangeZoomOption) * value;
+    }
 
     // MARK: "viewport percentage" resolution
 
@@ -299,9 +320,6 @@ double computeNonCalcLengthDouble(double value, CSS::LengthUnit lengthUnit, cons
     case Cap:
     case Ch:
     case Ic:
-        // FIXME: We have a bug right now where the zoom will be applied twice to EX units.
-        // We really need to compute EX using fontMetrics for the original specifiedSize and not use
-        // our actual constructed rendering font.
         value = computeUnzoomedNonCalcLengthDouble(value, lengthUnit, conversionData.propertyToCompute(), &conversionData.fontCascadeForFontUnits(), conversionData.rangeZoomOption());
         value = adjustZoomStateForFontRelativeUnitsIfNeeded(value, conversionData);
         break;
