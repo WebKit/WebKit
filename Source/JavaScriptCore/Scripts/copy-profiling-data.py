5060901e267ff7d54f0cd0c9ec2638c0f97e4cac
Elliott Williams <emw@apple.com>
2026-06-09 22:08:30 -0700

    Cherry-pick cf72c5b0d395. rdar://175716744
        [Xcode] Use different PGO profiles for each architecture
        https://bugs.webkit.org/show_bug.cgi?id=312619
        rdar://172932216
    
        Reviewed by Alexey Proskuryakov.
    
        Relanding with a env var lookup fix in `copy-profiling-data.py`.
    
        Internal infrastructure now generates per-architecture PGO profiles at
        paths like:
    
            ./usr/local/include/WebKitAdditions/Profiling/arm64e/JavaScriptCore.profdata.compressed
            ./usr/local/include/WebKitAdditions/Profiling/x86_64/JavaScriptCore.profdata.compressed
    
        and so on. Update the build system logic that decompresses these
        profiles to take active architecture(s) into account.
    
        The architecture-specific profiles will be decompressed to paths like:
    
            WebKitBuild/Release/DerivedSources/JavaScriptCore/Profiling/arm64e.profdata
            WebKitBuild/Release/DerivedSources/JavaScriptCore/Profiling/x86_64.profdata
    
        Change the xcconfig logic that computes -fprofile-instr-use flags to
        reflect the new input paths.
    
        * Source/JavaScriptCore/Configurations/libJavaScriptCore.xcconfig:
        * Source/JavaScriptCore/JavaScriptCore.xcodeproj/project.pbxproj:
        * Source/JavaScriptCore/Scripts/copy-profiling-data.py: Added, rewrite
          of the shell script.
        (checked_decompress):
        * Source/JavaScriptCore/Scripts/copy-profiling-data.sh:
        * Source/WebCore/Configurations/WebCore.xcconfig:
        * Source/WebCore/WebCore.xcodeproj/project.pbxproj:
        * Source/WebKit/Configurations/BaseTarget.xcconfig:
        * Source/WebKit/WebKit.xcodeproj/project.pbxproj:
    
        Canonical link: https://commits.webkit.org/311878@main
    
    Identifier: 305413.798@safari-7624-branch
    Canonical link: https://commits.webkit.org/305413.919@safari-7624.4-branch

---
+++ Source/JavaScriptCore/Scripts/copy-profiling-data.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import os
+import shlex
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+
+profiles_folder = Path(os.environ['SCRIPT_INPUT_FILE_0'])
+empty_profdata = Path(os.environ['SCRIPT_INPUT_FILE_1'])
+output_folder = Path('{BUILT_PRODUCTS_DIR}/DerivedSources/{PROJECT_NAME}/Profiling'.format_map(os.environ))
+depfile_path = Path('{DERIVED_FILES_DIR}/copy-profiling-data.d'.format_map(os.environ))
+
+# Some 26.x versions of Xcode do not expose ARCHS_BASE, but can safely fall
+# back to ARCHS.
+archs = os.environ.get('ARCHS_BASE', os.environ['ARCHS'])
+project_name = os.environ['PROJECT_NAME']
+
+
+def checked_decompress(src, dst):
+    if src.stat().st_size < 1024:
+        if os.environ['CONFIGURATION'] == 'Production':
+            raise SystemExit(
+                f'error: {src} is <1KB, is it a Git LFS stub? '
+                'Ensure this file was checked out on a machine with git-lfs installed.'
+            )
+        else:
+            print(
+                f'warning: {src} is <1KB, is it a Git LFS stub? '
+                'To build with production optimizations, ensure this file was '
+                'checked out on a machine with git-lfs installed. Falling '
+                'back to stub profile data.',
+                file=sys.stderr
+            )
+            shutil.copy(src, dst)
+    else:
+        subprocess.check_call(
+            ('compression_tool', '-v', '-decode', '-i', src, '-o', dst)
+        )
+
+
+inputs = []
+if os.environ.get('WK_ENABLE_PGO_USE') == 'YES':
+    for arch in archs.split():
+        src = profiles_folder / f'{arch}/{project_name}.profdata.compressed'
+        dst = output_folder / f'{arch}.profdata'
+        checked_decompress(src, dst)
+        inputs.append(src)
+
+depfile_path.write_text(
+    'dependencies: ' + '\\\n    '.join(
+        shlex.quote(str(path)) for path in inputs
+    )
+)
