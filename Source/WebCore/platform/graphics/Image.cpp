9cd327e6f60c9ff9757049760d34708a9c1464bf
Charlie Wolfe <charliew@apple.com>
2026-06-13 13:55:14 -0700

    Cherry-pick 314937@main (6a12936c2791). https://bugs.webkit.org/show_bug.cgi?id=316767
        Null deref in Image::toShareableBitmap when ShareableBitmap::create() fails
        https://bugs.webkit.org/show_bug.cgi?id=316767
        rdar://179212122
    
        Reviewed by Pascoe.
    
        ShareableBitmap::create() can return null, but the result was dereferenced without a check. Return
        nullptr when creation fails.
    
        * Source/WebCore/platform/graphics/Image.cpp:
        (WebCore::Image::toShareableBitmap const):
    
        Canonical link: https://commits.webkit.org/314937@main
    
    Canonical link: https://commits.webkit.org/305877.743@webkitglib/2.52

--- Source/WebCore/platform/graphics/Image.cpp
+++ Source/WebCore/platform/graphics/Image.cpp
@@ -424,6 +424,8 @@ DestinationColorSpace Image::colorSpace()
 RefPtr<ShareableBitmap> Image::toShareableBitmap() const
 {
     RefPtr bitmap = ShareableBitmap::create({ IntSize(size()) });
+    if (!bitmap)
+        return nullptr;
     std::unique_ptr graphicsContext = bitmap->createGraphicsContext();
     if (!graphicsContext)
         return nullptr;
