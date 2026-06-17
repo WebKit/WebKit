d74d692503fce0f3b5da859c7feaec7c48c1e0e6
Kai Tamkun <k_tamkun@apple.com>
2026-06-16 17:58:49 -0700

    [JSC] Validate RegExps in DFG strength reduction phase https://bugs.webkit.org/show_bug.cgi?id=316723 rdar://178446883
    Reviewed by Yusuke Suzuki.
    
    This patch prevents conversion to NewRegExpUntyped if the regexp in question is invalid.
    
    Test: JSTests/stress/new-regexp-untyped-soft-invalid-stale-captures.js
    
    * JSTests/stress/new-regexp-untyped-soft-invalid-stale-captures.js: Added.
    (atDepth):
    (const.make.eval):
    (catch):
    (try.catch):
    (try.i.catch):
    (exposed.i.catch):
    * Source/JavaScriptCore/dfg/DFGStrengthReductionPhase.cpp:
    (JSC::DFG::StrengthReductionPhase::handleNode):
    * Source/JavaScriptCore/runtime/RegExp.cpp:
    (JSC::RegExp::finishCreation):
    (JSC::RegExp::updateMetadataFromPattern):
    (JSC::RegExp::byteCodeCompileIfNecessary):
    (JSC::RegExp::compile):
    (JSC::RegExp::compileMatchOnly):
    * Source/JavaScriptCore/runtime/RegExp.h:
    * Source/JavaScriptCore/runtime/RegExpCache.cpp:
    (JSC::RegExpCache::lookupOrCreate):
    
    Identifier: 305413.975@safari-7624.5-branch

--- Source/JavaScriptCore/dfg/DFGStrengthReductionPhase.cpp
+++ Source/JavaScriptCore/dfg/DFGStrengthReductionPhase.cpp
@@ -716,6 +716,9 @@ private:
             if (!regExp)
                 break;
 
+            if (!regExp->isValid())
+                break;
+
             m_node->convertToNewRegExp(m_graph.freezeStrong(regExp), m_insertionSet.insertConstantForUse(m_nodeIndex, m_node->origin, jsNumber(0), UntypedUse));
             m_changed = true;
             break;
