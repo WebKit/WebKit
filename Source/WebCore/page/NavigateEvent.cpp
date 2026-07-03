7b7429195b075c5c4332d5a436cf605516912c23
Ryosuke Niwa <rniwa@webkit.org>
2026-07-03 16:43:10 -0700

    Use-after-free in LocalFrameView::scrollToAnchorFragment via NavigateEvent:finish https://bugs.webkit.org/show_bug.cgi?id=313606
    Reviewed by Anne van Kesteren and Rupin Mittal.
    
    Deployed more smart pointers to fix the bug.
    
    Test: navigation-api/navigation-api-fragment-intercept-crash.html
    
    * LayoutTests/navigation-api/navigation-api-fragment-intercept-crash-expected.txt: Added.
    * LayoutTests/navigation-api/navigation-api-fragment-intercept-crash.html: Added.
    * LayoutTests/navigation-api/resources/navigation-api-fragment-intercept-crash-inner.html: Added.
    
    Originally-landed-as: 305413.777@safari-7624-branch (0e113df3124c). rdar://180428609
    Canonical link: https://commits.webkit.org/316500@main

--- Source/WebCore/page/NavigateEvent.cpp
+++ Source/WebCore/page/NavigateEvent.cpp
@@ -154,7 +154,6 @@ void NavigateEvent::processScrollBehavior(Document& document)
 
     if (m_navigationType == NavigationNavigationType::Traverse) {
         document.frame()->loader().history().restoreScrollPositionAndViewState();
-
         return;
     }
 
@@ -162,8 +161,7 @@ void NavigateEvent::processScrollBehavior(Document& document)
         if (m_navigationType == NavigationNavigationType::Reload)
             document.frame()->loader().history().restoreScrollPositionAndViewState();
         else
-            protect(document)->frame()->view()->setScrollPosition({ 0, 0 });
-
+            protect(protect(document)->frame()->view())->setScrollPosition({ 0, 0 });
         return;
     }
 
@@ -172,14 +170,13 @@ void NavigateEvent::processScrollBehavior(Document& document)
     if (!document.findAnchor(document.url().fragmentIdentifier())) {
         if (m_navigationType == NavigationNavigationType::Reload)
             document.frame()->loader().history().restoreScrollPositionAndViewState();
-
         return;
     }
 
     if (!document.haveStylesheetsLoaded())
         document.setGotoAnchorNeededAfterStylesheetsLoad(true);
     else
-        protect(document)->frame()->view()->scrollToFragment(document.url());
+        protect(protect(document)->frame()->view())->scrollToFragment(document.url());
 }
 
 // https://html.spec.whatwg.org/multipage/nav-history-apis.html#dom-navigateevent-scroll
