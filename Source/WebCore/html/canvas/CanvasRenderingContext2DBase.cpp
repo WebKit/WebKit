1519d5331824bb48ef17ace35406effa1eacdb77
Claudio Saavedra <csaavedra@igalia.com>
2026-07-05 00:29:23 -0700

    Cherry-pick 308314@main (b5da4b256925). https://bugs.webkit.org/show_bug.cgi?id=308741
        REGRESSION(307450@main): CanvasRenderingContext2D does not account for unrealized saves when resetting the context
        https://bugs.webkit.org/show_bug.cgi?id=308741
    
        Reviewed by Kimmo Kinnunen.
    
        When canvas size dimensions are modified the rendering context stack
        needs to be restored. However, if there are unrealized saves, the
        check at the beginning of CanvasRenderingContext2DBase::restore()
        will short-circuit and leave the state inconsistent after the image
        buffer is reset or removed.
    
        Resetting the counter for unrealized saves before the restore call
        loop prevents this.
    
        Test: compositing/canvas/resize-canvas-with-unrealized-saves-crash.html
    
        * LayoutTests/compositing/canvas/resize-canvas-with-unrealized-saves-crash-expected.txt: Added.
        * LayoutTests/compositing/canvas/resize-canvas-with-unrealized-saves-crash.html: Added.
        * Source/WebCore/html/canvas/CanvasRenderingContext2DBase.cpp:
        (WebCore::CanvasRenderingContext2DBase::~CanvasRenderingContext2DBase):
        (WebCore::CanvasRenderingContext2DBase::didUpdateCanvasSizeProperties):
    
        Canonical link: https://commits.webkit.org/308314@main
    
    Canonical link: https://commits.webkit.org/305877.945@webkitglib/2.52

--- Source/WebCore/html/canvas/CanvasRenderingContext2DBase.cpp
+++ Source/WebCore/html/canvas/CanvasRenderingContext2DBase.cpp
@@ -262,6 +262,7 @@ CanvasRenderingContext2DBase::CanvasRenderingContext2DBase(CanvasBase& canvas, C
 CanvasRenderingContext2DBase::~CanvasRenderingContext2DBase()
 {
 #if ASSERT_ENABLED
+    m_unrealizedSaveCount = 0;
     size_t restoreCount = m_stateStack.size() - 1;
     for (size_t i = 0; i < restoreCount; ++i)
         restore();
@@ -322,12 +323,12 @@ void CanvasRenderingContext2DBase::reset()
 
 void CanvasRenderingContext2DBase::didUpdateCanvasSizeProperties(bool sizeChanged)
 {
+    m_unrealizedSaveCount = 0;
     size_t restoreCount = m_stateStack.size() - 1;
     for (size_t i = 0; i < restoreCount; ++i)
         restore();
     m_stateStack.first() = State();
     m_path.clear();
-    m_unrealizedSaveCount = 0;
     m_cachedContents.emplace<CachedContentsTransparent>();
     m_hasDeferredOperations = false;
     clearAccumulatedDirtyRect();
