50232798d9518e9d7fd72f681149410324103c81
Youenn Fablet <youenn@apple.com>
2026-07-06 11:39:12 -0700

    Race on RemoteVideoDecoderCallbacks::m_timestampToDuration leads to use-after-free rdar://177096608
    Reviewed by Jean-Yves Avenard.
    
    RemoteVideoDecoderCallbacks::addDuration is called from the JS thread that invoked VideoDecoder.decode.
    RemoteVideoDecoderCallbacks::notifyDecodingResult is called from the LibWebRTCCodecs work queue.
    Both mutate m_timestampToDuration so we need to add a lock to ensure synchronization.
    We also make m_isClosed atomic on both the decoder and encoder callback objects as it is written in a thread and read in another.
    
    Patch mostly written by Simon Lewis.
    
    * LayoutTests/http/wpt/webcodecs/videoDecoder-concurrent-decode-output-no-crash-expected.txt: Added.
    * LayoutTests/http/wpt/webcodecs/videoDecoder-concurrent-decode-output-no-crash.html: Added.
    * Source/WebKit/WebProcess/GPU/media/RemoteVideoCodecFactory.cpp:
    (WebKit::RemoteVideoDecoderCallbacks::addDuration):
    (WebKit::RemoteVideoDecoderCallbacks::takeDuration):
    (WebKit::RemoteVideoDecoderCallbacks::notifyDecodingResult):
    
    Identifier: 305413.1093@safari-7624.5-branch

--- Source/WebKit/WebProcess/GPU/media/RemoteVideoCodecFactory.cpp
+++ Source/WebKit/WebProcess/GPU/media/RemoteVideoCodecFactory.cpp
@@ -42,14 +42,19 @@ public:
     void notifyDecodingResult(RefPtr<WebCore::VideoFrame>&&, int64_t timestamp);
 
     void close() { m_isClosed = true; }
-    void addDuration(int64_t timestamp, uint64_t duration) { m_timestampToDuration.insert_or_assign(timestamp, duration); }
+    void addDuration(int64_t timestamp, uint64_t duration)
+    {
+        Locker locker { m_timestampToDurationLock };
+        m_timestampToDuration.insert_or_assign(timestamp, duration);
+    }
 
 private:
     explicit RemoteVideoDecoderCallbacks(WebCore::VideoDecoder::OutputCallback&&);
 
     WebCore::VideoDecoder::OutputCallback m_outputCallback;
-    bool m_isClosed { false };
-    StdUnorderedMap<int64_t, uint64_t> m_timestampToDuration;
+    std::atomic<bool> m_isClosed { false };
+    Lock m_timestampToDurationLock;
+    StdUnorderedMap<int64_t, uint64_t> m_timestampToDuration WTF_GUARDED_BY_LOCK(m_timestampToDurationLock);
 };
 
 class RemoteVideoDecoder : public WebCore::VideoDecoder {
@@ -84,7 +89,7 @@ private:
 
     WebCore::VideoEncoder::DescriptionCallback m_descriptionCallback;
     WebCore::VideoEncoder::OutputCallback m_outputCallback;
-    bool m_isClosed { false };
+    std::atomic<bool> m_isClosed { false };
 };
 
 class RemoteVideoEncoder : public WebCore::VideoEncoder {
@@ -238,10 +243,13 @@ void RemoteVideoDecoderCallbacks::notifyDecodingResult(RefPtr<WebCore::VideoFram
         return;
     }
     uint64_t duration = 0;
-    auto iterator = m_timestampToDuration.find(timestamp);
-    if (iterator != m_timestampToDuration.end()) {
-        duration = iterator->second;
-        m_timestampToDuration.erase(iterator);
+    {
+        Locker locker { m_timestampToDurationLock };
+        auto iterator = m_timestampToDuration.find(timestamp);
+        if (iterator != m_timestampToDuration.end()) {
+            duration = iterator->second;
+            m_timestampToDuration.erase(iterator);
+        }
     }
     m_outputCallback(WebCore::VideoDecoder::DecodedFrame { frame.releaseNonNull(), static_cast<int64_t>(timestamp), duration });
 }
