WebKit/WebKit

commit 7768ef95fbb7da97cc764fa81978681994ce8c74

[Build] Reenable JSC and WebCore module verifier on public SDK builds https://bugs.webkit.org/show_bug.cgi?id=317876 rdar://177493372
Reviewed by Richard Robinson.

Reland of 316413@main with a fix for the SaferCPP EWS build, which still
uses Xcode 26.2.

Our `AvailabilityProhibitedInternal.h` injected header does not work
when building with modules. Modules are by design self-contained, and
the `os_availability` modulemap in the SDK compiles without finding and
including our injected header.

We need to silence availability errors in the public build in order to
bind to SPI on the platform that is otherwise marked API_UNAVAILABLE.

Instead, replace the technique with a new approach based on stubbing out
the entire Availability library, replacing <Availability.h> and
<os/availability.h> with our own versions of the headers.

The OS modulemap containing the availability project has other headers
in it too, so we can't just ship our own modulemap referencing the
stubs. Instead, generate a VFS overlay which maps our headers onto the
correct SDK paths.

* Configurations/CommonBase.xcconfig:
* Configurations/SDKAdditions.xcconfig:
* Source/JavaScriptCore/Configurations/JavaScriptCore.xcconfig:

In third-party code, add "Setup" targets to Xcode projects which depend
on the overlay YAML bring produced. These gate compilation of any of the
third-party libraries until bmalloc has run and WTF has staged public
SDK headers.

(In CMake, we're able to generate the overlay at configure time without
disturbing the dependency graph, so this issue does not exist.)

* Source/ThirdParty/ANGLE/ANGLE.xcodeproj/project.pbxproj:
* Source/ThirdParty/gmock/gmock.xcodeproj/project.pbxproj:
* Source/ThirdParty/gtest/xcode/gtest.xcodeproj/project.pbxproj:
* Source/ThirdParty/libwebrtc/libwebrtc.xcodeproj/project.pbxproj:
* Source/WTF/Configurations/WTF.xcconfig:
* Source/WebCore/Configurations/WebCore.xcconfig:
* Source/WebCore/PAL/Configurations/PAL.xcconfig:

Source/cmake/OptionsCocoa.cmake
@@ -58,45 +58,33 @@ set(CMAKE_HAVE_PTHREAD_H 1 CACHE INTERNAL "")
 set(CMAKE_HAVE_LIBC_PTHREAD 1 CACHE INTERNAL "")
 find_package(Threads REQUIRED)
 
-string(REGEX MATCH "^[0-9]+" _sdk_major "${_sdk_version}")
-set(_additions_candidates
-    "${CMAKE_SOURCE_DIR}/WebKitLibraries/SDKs/${WEBKIT_SDK_NAME}${_sdk_major}.0-additions.sdk/usr/local/include"
-    "${CMAKE_SOURCE_DIR}/WebKitLibraries/SDKs/${WEBKIT_SDK_NAME}${_sdk_major_minor}-additions.sdk/usr/local/include"
-)
-set(_additions_found FALSE)
-foreach (_additions IN LISTS _additions_candidates)
-    if (EXISTS "${_additions}/AvailabilityProhibitedInternal.h")
-        add_compile_options("$<$<NOT:$<COMPILE_LANGUAGE:Swift>>:-isystem${_additions}>")
-        message(STATUS "SDK additions overlay: ${_additions} (disables API_UNAVAILABLE for SPI code)")
-        set(_additions_found TRUE)
-        break ()
-    endif ()
-endforeach ()
-if (NOT _additions_found)
-    file(GLOB _all_additions "${CMAKE_SOURCE_DIR}/WebKitLibraries/SDKs/${WEBKIT_SDK_NAME}*-additions.sdk")
-    list(SORT _all_additions)
-    list(REVERSE _all_additions)
-    foreach (_additions_sdk IN LISTS _all_additions)
-        set(_additions "${_additions_sdk}/usr/local/include")
-        if (EXISTS "${_additions}/AvailabilityProhibitedInternal.h")
-            add_compile_options("$<$<NOT:$<COMPILE_LANGUAGE:Swift>>:-isystem${_additions}>")
-            message(STATUS "SDK additions overlay (fallback): ${_additions}")
-            set(_additions_found TRUE)
-            break ()
-        endif ()
-    endforeach ()
-    unset(_all_additions)
-    unset(_additions_sdk)
-endif ()
-if (NOT _additions_found)
-    message(WARNING "No SDK additions overlay found -- API_UNAVAILABLE classes (AVAudioSession etc.) will fail to compile")
-endif ()
-unset(_sdk_version)
-unset(_sdk_major)
-unset(_sdk_major_minor)
-unset(_additions_candidates)
-unset(_additions)
-unset(_additions_found)
+# Replace the SDK's availability headers with stubs that defuse availability
+# checks.
+set(_availability_overlay_dir "${CMAKE_SOURCE_DIR}/WebKitLibraries/AvailabilityOverlay")
+set(_availability_overlay_yaml "${CMAKE_BINARY_DIR}/availability-overlay.yaml")
+file(WRITE "${_availability_overlay_yaml}.tmp"
+"{
+  \"version\": 0,
+  \"case-sensitive\": false,
+  \"roots\": [
+    {
+      \"name\": \"${CMAKE_OSX_SYSROOT}/usr/include/os/availability.h\",
+      \"type\": \"file\",
+      \"external-contents\": \"${_availability_overlay_dir}/usr/include/os/availability.h\"
+    },
+    {
+      \"name\": \"${CMAKE_OSX_SYSROOT}/usr/include/Availability.h\",
+      \"type\": \"file\",
+      \"external-contents\": \"${_availability_overlay_dir}/usr/include/Availability.h\"
+    }
+  ]
+}
+")
+file(COPY_FILE "${_availability_overlay_yaml}.tmp" ${_availability_overlay_yaml} ONLY_IF_DIFFERENT)
+add_compile_options("$<$<NOT:$<COMPILE_LANGUAGE:Swift>>:SHELL:-ivfsoverlay ${_availability_overlay_yaml}>")
+add_compile_options("$<$<COMPILE_LANGUAGE:Swift>:SHELL:-vfsoverlay ${_availability_overlay_yaml}>")
+unset(_availability_overlay_dir)
+unset(_availability_overlay_yaml)
 
 if (EXISTS "/usr/local/include/WebKitAdditions" AND NOT EXISTS "/usr/local/include/AppleFeatures/AppleFeatures.h")
     set(_apple_features_stub "${CMAKE_BINARY_DIR}/generated-stubs/AppleFeatures")

Configurations/CommonBase.xcconfig
@@ -59,7 +59,7 @@ WK_GCC_PREPROCESSOR_DEFINITIONS = $(WK_GCC_PREPROCESSOR_DEFINITIONS_$(USE_INTERN
 WK_GCC_PREPROCESSOR_DEFINITIONS_YES = $(WK_OS_UNFAIR_LOCK_INLINE_CFLAGS) __ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES=0;
 GCC_PREPROCESSOR_DEFINITIONS = $(inherited) $(WK_GCC_PREPROCESSOR_DEFINITIONS);
 
-WK_COMMON_OTHER_CFLAGS = $(WK_SANITIZER_OTHER_CFLAGS) $(WK_COMMON_OTHER_CFLAGS_PGO_$(WK_OR_$(WK_ENABLE_PGO_USE)_$(ENABLE_LLVM_PROFILE_GENERATION)));
+WK_COMMON_OTHER_CFLAGS = $(WK_AVAILABILITY_OVERLAY_FLAGS) $(WK_SANITIZER_OTHER_CFLAGS) $(WK_COMMON_OTHER_CFLAGS_PGO_$(WK_OR_$(WK_ENABLE_PGO_USE)_$(ENABLE_LLVM_PROFILE_GENERATION)));
 // Work around rdar://141760829 until the fix is available on all clang-1700
 // based toolchains.
 // Fix from rdar://178983382 to avoid symbol module prefix differences causing performance loss.
@@ -67,7 +67,7 @@ WK_COMMON_OTHER_CFLAGS_PGO_YES = -mllvm -system-headers-coverage -mllvm -static-
 
 OTHER_CFLAGS = $(inherited) $(WK_COMMON_OTHER_CFLAGS);
 
-WK_COMMON_OTHER_CPLUSPLUSFLAGS = $(WK_LIBCPP_ASSERTIONS_CFLAGS);
+WK_COMMON_OTHER_CPLUSPLUSFLAGS = $(WK_AVAILABILITY_OVERLAY_FLAGS) $(WK_LIBCPP_ASSERTIONS_CFLAGS);
 OTHER_CPLUSPLUSFLAGS = $(inherited) $(WK_COMMON_OTHER_CPLUSPLUSFLAGS);
 
 _EXPERIMENTAL_SWIFT_EXPLICIT_MODULES = YES;
@@ -83,7 +83,7 @@ WK_COMMON_OTHER_LDFLAGS = $(WK_SANITIZER_OTHER_LDFLAGS) $(WK_COMMON_OTHER_LDFLAG
 WK_COMMON_OTHER_LDFLAGS_INSTRUMENTATION_YES = -Wl,-rename_section,__DATA,__llvm_prf_cnts,__MMAP_DATA,__llvm_prf_cnts;
 OTHER_LDFLAGS = $(inherited) $(WK_COMMON_OTHER_LDFLAGS);
 
-WK_COMMON_OTHER_TAPI_FLAGS = -x objective-c++ -std=c++2b -fno-rtti $(WK_SANITIZER_OTHER_TAPI_FLAGS);
+WK_COMMON_OTHER_TAPI_FLAGS = -x objective-c++ -std=c++2b -fno-rtti $(WK_SANITIZER_OTHER_TAPI_FLAGS) $(WK_AVAILABILITY_OVERLAY_FLAGS);
 OTHER_TAPI_FLAGS = $(inherited) $(WK_COMMON_OTHER_TAPI_FLAGS);
 
 WK_DEFAULT_WK_AUDIT_SPI[sdk=iphoneos*] = $(WK_AND_$(WK_NOT_$(WK_ANY_SANITIZER_ENABLED))_$(OVERRIDE_SUPPORTS_TEXT_BASED_API:default=YES));
@@ -130,7 +130,7 @@ WK_SWIFT_CLANG_DEPLOYMENT_TARGET = $(WK_SWIFT_CLANG_DEPLOYMENT_TARGET_$(WK_PLATF
 WK_SWIFT_CLANG_DEPLOYMENT_TARGET_macosx = -clang-target $(CURRENT_ARCH)-apple-macos$(MACOSX_DEPLOYMENT_TARGET);
 
 // rdar://170129992: remove -track-system-dependencies when resolved
-OTHER_SWIFT_FLAGS = $(inherited) -Xcc -fvisibility=hidden -Werror ExistentialAny -Werror NoUseUnstructuredThrowingTask -Werror NoUsage -enable-upcoming-feature ExistentialAny $(WK_SWIFT_MEMORY_SAFETY_FLAGS) $(WK_SWIFT_MEMORY_SAFETY_ERROR_FLAGS) $(WK_SWIFT_CLANG_DEPLOYMENT_TARGET) $(WK_SANITIZER_OTHER_SWIFT_FLAGS) -enable-upcoming-feature InternalImportsByDefault -enable-upcoming-feature MemberImportVisibility -track-system-dependencies -enable-experimental-feature ImportCxxMembersLazily;
+OTHER_SWIFT_FLAGS = $(inherited) $(WK_AVAILABILITY_OVERLAY_SWIFT_FLAGS) -Xcc -fvisibility=hidden -Werror ExistentialAny -Werror NoUseUnstructuredThrowingTask -Werror NoUsage -enable-upcoming-feature ExistentialAny $(WK_SWIFT_MEMORY_SAFETY_FLAGS) $(WK_SWIFT_MEMORY_SAFETY_ERROR_FLAGS) $(WK_SWIFT_CLANG_DEPLOYMENT_TARGET) $(WK_SANITIZER_OTHER_SWIFT_FLAGS) -enable-upcoming-feature InternalImportsByDefault -enable-upcoming-feature MemberImportVisibility -track-system-dependencies -enable-experimental-feature ImportCxxMembersLazily;
 // Work around rdar://157581667 on affected toolchains.
 OTHER_SWIFT_FLAGS[sdk=*26.2*] = $(inherited) -Xcc -fno-modulemap-allow-subdirectory-search;
 OTHER_SWIFT_FLAGS[sdk=*26.3*] = $(inherited) -Xcc -fno-modulemap-allow-subdirectory-search;

Configurations/SDKAdditions.xcconfig
@@ -40,3 +40,14 @@ WK_PRIVATE_SDK_DIR = $(WK_PRIVATE_SDK_DIR_$(USE_INTERNAL_SDK));
 WK_PRIVATE_SDK_DIR_ = $(SDK_DIR_WebKitSDKAdditions_$(PLATFORM_NAME));
 WK_PRIVATE_SDK_DIR_[sdk=macos*] = $(SDK_DIR);
 WK_PRIVATE_SDK_DIR_YES = $(SDK_DIR);
+
+// Clang VFS overlay that replaces the SDK's <os/availability.h> and
+// <Availability.h> with WebKit-supplied stubs that defuse availability
+// checks.
+WK_AVAILABILITY_OVERLAY_YAML = $(BUILD_DIR)/availability-overlay.yaml;
+
+WK_AVAILABILITY_OVERLAY_FLAGS = -ivfsoverlay $(WK_AVAILABILITY_OVERLAY_YAML);
+WK_AVAILABILITY_OVERLAY_FLAGS[sdk=*.internal] = ;
+
+WK_AVAILABILITY_OVERLAY_SWIFT_FLAGS = -vfsoverlay $(WK_AVAILABILITY_OVERLAY_YAML);
+WK_AVAILABILITY_OVERLAY_SWIFT_FLAGS[sdk=*.internal] = ;

Source/JavaScriptCore/Configurations/JavaScriptCore.xcconfig
@@ -24,11 +24,10 @@
 #include "BaseTarget.xcconfig"
 
 DEFINES_MODULE = YES;
-// FIXME: (rdar://177493372) Re-enable the JSC and WebCore module verifier on public SDK builds
-ENABLE_MODULE_VERIFIER = $(WK_ENABLE_MODULE_VERIFIER_$(USE_INTERNAL_SDK))
-WK_ENABLE_MODULE_VERIFIER_YES = $(OVERRIDE_ENABLE_MODULE_VERIFIER:default=YES);
+ENABLE_MODULE_VERIFIER = $(WK_ENABLE_MODULE_VERIFIER)
+WK_ENABLE_MODULE_VERIFIER = $(OVERRIDE_ENABLE_MODULE_VERIFIER:default=YES);
 
-OTHER_MODULE_VERIFIER_FLAGS = -- -iframework $(BUILT_PRODUCTS_DIR)/SDKAdditions/System/Library/Frameworks -I$(BUILT_PRODUCTS_DIR)$(WK_LIBRARY_HEADERS_FOLDER_PATH) -I$(WK_ADDITIONAL_SDKS)$(WK_LIBRARY_HEADERS_FOLDER_PATH) $(OTHER_MODULE_VERIFIER_FLAGS_$(WK_EMPTY_$(WK_OVERRIDE_FRAMEWORKS_DIR)));
+OTHER_MODULE_VERIFIER_FLAGS = -- $(WK_AVAILABILITY_OVERLAY_FLAGS) -iframework $(BUILT_PRODUCTS_DIR)/SDKAdditions/System/Library/Frameworks -I$(BUILT_PRODUCTS_DIR)$(WK_LIBRARY_HEADERS_FOLDER_PATH) -I$(WK_ADDITIONAL_SDKS)$(WK_LIBRARY_HEADERS_FOLDER_PATH) $(OTHER_MODULE_VERIFIER_FLAGS_$(WK_EMPTY_$(WK_OVERRIDE_FRAMEWORKS_DIR)));
 OTHER_MODULE_VERIFIER_FLAGS_ = -iframework "$(WK_PRIVATE_SDK_DIR)$(WK_OVERRIDE_FRAMEWORKS_DIR)" -isystem "$(WK_PRIVATE_SDK_DIR)$(WK_ALTERNATE_WEBKIT_SDK_PATH)$(WK_LIBRARY_HEADERS_FOLDER_PATH)";
 
 MODULEMAP_FILE = $(SRCROOT)/JavaScriptCore.modulemap;

Source/ThirdParty/ANGLE/ANGLE.xcodeproj/project.pbxproj
@@ -52,6 +52,19 @@
 			name = "Tools (ANGLE)";
 			productName = "Tools (ANGLE)";
 		};
+		DDAD70112FF59CB70054DF83 /* Setup */ = {
+			isa = PBXAggregateTarget;
+			buildConfigurationList = DDAD70152FF59CB70054DF83 /* Build configuration list for PBXAggregateTarget "Setup" */;
+			buildPhases = (
+				DDAD70162FF59CC60054DF83 /* Public SDK gate */,
+			);
+			dependencies = (
+			);
+			name = Setup;
+			packageProductDependencies = (
+			);
+			productName = Setup;
+		};
 /* End PBXAggregateTarget section */
 
 /* Begin PBXBuildFile section */
@@ -2715,6 +2728,41 @@
 			proxyType = 1;
 			remoteGlobalIDString = A3E045FAB988D2834BEDCF23;
 			remoteInfo = ANGLEDeqpGLES2Tests;
+		};
+		DDAD70172FF59CF40054DF83 /* PBXContainerItemProxy */ = {
+			isa = PBXContainerItemProxy;
+			containerPortal = FB39D0701200ED9200088E69 /* Project object */;
+			proxyType = 1;
+			remoteGlobalIDString = DDAD70112FF59CB70054DF83;
+			remoteInfo = Setup;
+		};
+		DDAD70192FF59CF70054DF83 /* PBXContainerItemProxy */ = {
+			isa = PBXContainerItemProxy;
+			containerPortal = FB39D0701200ED9200088E69 /* Project object */;
+			proxyType = 1;
+			remoteGlobalIDString = DDAD70112FF59CB70054DF83;
+			remoteInfo = Setup;
+		};
+		DDAD701B2FF59CFA0054DF83 /* PBXContainerItemProxy */ = {
+			isa = PBXContainerItemProxy;
+			containerPortal = FB39D0701200ED9200088E69 /* Project object */;
+			proxyType = 1;
+			remoteGlobalIDString = DDAD70112FF59CB70054DF83;
+			remoteInfo = Setup;
+		};
+		DDAD701D2FF59CFD0054DF83 /* PBXContainerItemProxy */ = {
+			isa = PBXContainerItemProxy;
+			containerPortal = FB39D0701200ED9200088E69 /* Project object */;
+			proxyType = 1;
+			remoteGlobalIDString = DDAD70112FF59CB70054DF83;
+			remoteInfo = Setup;
+		};
+		DDAD701F2FF59D000054DF83 /* PBXContainerItemProxy */ = {
+			isa = PBXContainerItemProxy;
+			containerPortal = FB39D0701200ED9200088E69 /* Project object */;
+			proxyType = 1;
+			remoteGlobalIDString = DDAD70112FF59CB70054DF83;
+			remoteInfo = Setup;
 		};
 		FF194FF52744331A006A97A3 /* PBXContainerItemProxy */ = {
 			isa = PBXContainerItemProxy;
@@ -8192,6 +8240,7 @@
 				31A9E9A0249AA04200C7E243 /* PBXBuildRule */,
 			);
 			dependencies = (
+				DDAD70182FF59CF40054DF83 /* PBXTargetDependency */,
 				FF194FF62744331A006A97A3 /* PBXTargetDependency */,
 			);
 			name = "ANGLE (dynamic)";
@@ -8248,6 +8297,7 @@
 			buildRules = (
 			);
 			dependencies = (
+				DDAD701A2FF59CF70054DF83 /* PBXTargetDependency */,
 				7BFAF9372DE6FDD800327F7F /* PBXTargetDependency */,
 				7B820C582DE992CD00DAC5E4 /* PBXTargetDependency */,
 			);
@@ -8313,6 +8363,7 @@
 			buildRules = (
 			);
 			dependencies = (
+				DDAD701C2FF59CFA0054DF83 /* PBXTargetDependency */,
 			);
 			name = translator;
 			productName = translator;
@@ -8419,6 +8470,7 @@
 			buildRules = (
 			);
 			dependencies = (
+				DDAD701E2FF59CFD0054DF83 /* PBXTargetDependency */,
 			);
 			name = utils;
 			productName = translator;
@@ -8513,6 +8565,7 @@
 				FF194FFA27443C07006A97A3 /* PBXBuildRule */,
 			);
 			dependencies = (
+				DDAD70202FF59D000054DF83 /* PBXTargetDependency */,
 			);
 			name = ANGLEMetalLib;
 			productName = ANGLEMetalLib;
@@ -8555,6 +8608,9 @@
 					7BF554B52C81EEDB002B101E = {
 						CreatedOnToolsVersion = 16.0;
 					};
+					DDAD70112FF59CB70054DF83 = {
+						CreatedOnToolsVersion = 26.5;
+					};
 					FFDA50C4269F845100AE11E2 = {
 						CreatedOnToolsVersion = 13.0;
 						ProvisioningStyle = Automatic;
@@ -8574,6 +8630,7 @@
 			projectRoot = "";
 			targets = (
 				31DB794F2491C33300982878 /* ANGLE */,
+				DDAD70112FF59CB70054DF83 /* Setup */,
 				31CDFDF32491819E00486F27 /* ANGLE (dynamic) */,
 				7BB971FF2DE47B8A00455D13 /* ANGLE (static) */,
 				7BF5549A2C81EDE0002B101E /* translator */,
@@ -8753,6 +8810,27 @@
 			shellPath = /bin/sh;
 			shellScript = "set -e\nD=\"$TARGET_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH\"; S=\"$SRCROOT\"\nmkdir -p \"$D/vk_gl_cts_data/data\"\n/usr/bin/rsync -a --delete \"$S/third_party/VK-GL-CTS/src/data/gles3/\" \"$D/vk_gl_cts_data/data/gles3/\"\nmkdir -p \"$D/third_party/VK-GL-CTS/src/external/openglcts/data/gl_cts/data/mustpass/gles/aosp_mustpass/main\" \"$D/src/tests/deqp_support\"\n/bin/cp \"$S/third_party/VK-GL-CTS/src/external/openglcts/data/gl_cts/data/mustpass/gles/aosp_mustpass/main/gles3-main.txt\" \"$D/third_party/VK-GL-CTS/src/external/openglcts/data/gl_cts/data/mustpass/gles/aosp_mustpass/main/\"\n/bin/cp \"$S/src/tests/deqp_support/deqp_gles3_test_expectations.txt\" \"$D/src/tests/deqp_support/\"";
 			showEnvVarsInLog = 0;
+		};
+		DDAD70162FF59CC60054DF83 /* Public SDK gate */ = {
+			isa = PBXShellScriptBuildPhase;
+			buildActionMask = 2147483647;
+			files = (
+			);
+			inputFileListPaths = (
+			);
+			inputPaths = (
+				"$(WK_DERIVED_SDK_HEADERS_DIR)",
+				"$(WK_AVAILABILITY_OVERLAY_YAML)",
+			);
+			name = "Public SDK gate";
+			outputFileListPaths = (
+			);
+			outputPaths = (
+				"$(DERIVED_FILE_DIR)/public-sdk-ready.timestamp",
+			);
+			runOnlyForDeploymentPostprocessing = 0;
+			shellPath = /bin/sh;
+			shellScript = "touch \"${SCRIPT_OUTPUT_FILE_0}\"\n";
 		};
 		FFDA50D5269F895400AE11E2 /* Bake Metal Library to NSData */ = {
 			isa = PBXShellScriptBuildPhase;
@@ -11042,6 +11120,31 @@
 			target = 9FDB44EAA70594111A0ED0EA /* ANGLEDeqpGLES3Tests */;
 			targetProxy = 31294C019492778D9D742220 /* PBXContainerItemProxy */;
 		};
+		DDAD70182FF59CF40054DF83 /* PBXTargetDependency */ = {
+			isa = PBXTargetDependency;
+			target = DDAD70112FF59CB70054DF83 /* Setup */;
+			targetProxy = DDAD70172FF59CF40054DF83 /* PBXContainerItemProxy */;
+		};
+		DDAD701A2FF59CF70054DF83 /* PBXTargetDependency */ = {
+			isa = PBXTargetDependency;
+			target = DDAD70112FF59CB70054DF83 /* Setup */;
+			targetProxy = DDAD70192FF59CF70054DF83 /* PBXContainerItemProxy */;
+		};
+		DDAD701C2FF59CFA0054DF83 /* PBXTargetDependency */ = {
+			isa = PBXTargetDependency;
+			target = DDAD70112FF59CB70054DF83 /* Setup */;
+			targetProxy = DDAD701B2FF59CFA0054DF83 /* PBXContainerItemProxy */;
+		};
+		DDAD701E2FF59CFD0054DF83 /* PBXTargetDependency */ = {
+			isa = PBXTargetDependency;
+			target = DDAD70112FF59CB70054DF83 /* Setup */;
+			targetProxy = DDAD701D2FF59CFD0054DF83 /* PBXContainerItemProxy */;
+		};
+		DDAD70202FF59D000054DF83 /* PBXTargetDependency */ = {
+			isa = PBXTargetDependency;
+			target = DDAD70112FF59CB70054DF83 /* Setup */;
+			targetProxy = DDAD701F2FF59D000054DF83 /* PBXContainerItemProxy */;
+		};
 		FF194FF62744331A006A97A3 /* PBXTargetDependency */ = {
 			isa = PBXTargetDependency;
 			target = FFDA50C4269F845100AE11E2 /* ANGLEMetalLib */;
@@ -11500,6 +11603,30 @@
 			};
 			name = Production;
 		};
+		DDAD70122FF59CB70054DF83 /* Debug */ = {
+			isa = XCBuildConfiguration;
+			buildSettings = {
+				CODE_SIGN_STYLE = Automatic;
+				PRODUCT_NAME = "$(TARGET_NAME)";
+			};
+			name = Debug;
+		};
+		DDAD70132FF59CB70054DF83 /* Release */ = {
+			isa = XCBuildConfiguration;
+			buildSettings = {
+				CODE_SIGN_STYLE = Automatic;
+				PRODUCT_NAME = "$(TARGET_NAME)";
+			};
+			name = Release;
+		};
+		DDAD70142FF59CB70054DF83 /* Production */ = {
+			isa = XCBuildConfiguration;
+			buildSettings = {
+				CODE_SIGN_STYLE = Automatic;
+				PRODUCT_NAME = "$(TARGET_NAME)";
+			};
+			name = Production;
+		};
 		FB39D0711200ED9200088E69 /* Debug */ = {
 			isa = XCBuildConfiguration;
 			baseConfigurationReference = 5D7C59C71208C68B001C873E /* DebugRelease.xcconfig */;
@@ -11743,6 +11870,16 @@
 				3332450181D6667B032A60AA /* Debug */,
 				6FAC471F99BD174315920DDD /* Release */,
 				AF6640E096DC860854C31233 /* Production */,
+			);
+			defaultConfigurationIsVisible = 0;
+			defaultConfigurationName = Production;
+		};
+		DDAD70152FF59CB70054DF83 /* Build configuration list for PBXAggregateTarget "Setup" */ = {
+			isa = XCConfigurationList;
+			buildConfigurations = (
+				DDAD70122FF59CB70054DF83 /* Debug */,
+				DDAD70132FF59CB70054DF83 /* Release */,
+				DDAD70142FF59CB70054DF83 /* Production */,
 			);
 			defaultConfigurationIsVisible = 0;
 			defaultConfigurationName = Production;

Source/ThirdParty/gmock/gmock.xcodeproj/project.pbxproj
@@ -3,7 +3,7 @@
 	archiveVersion = 1;
 	classes = {
 	};
-	objectVersion = 55;
+	objectVersion = 54;
 	objects = {
 
 /* Begin PBXAggregateTarget section */
@@ -321,6 +321,7 @@
 			isa = PBXNativeTarget;
 			buildConfigurationList = 44E750C62AC7291000828AC4 /* Build configuration list for PBXNativeTarget "gmock" */;
 			buildPhases = (
+				DDAD7E562FF6456F0054DF83 /* Public SDK gate */,
 				448631492AE2D94C00F9BC42 /* Product Dependencies */,
 				44E750BE2AC7291000828AC4 /* Headers */,
 				44E750BF2AC7291000828AC4 /* Sources */,
@@ -371,6 +372,30 @@
 			);
 		};
 /* End PBXProject section */
+
+/* Begin PBXShellScriptBuildPhase section */
+		DDAD7E562FF6456F0054DF83 /* Public SDK gate */ = {
+			isa = PBXShellScriptBuildPhase;
+			buildActionMask = 2147483647;
+			files = (
+			);
+			inputFileListPaths = (
+			);
+			inputPaths = (
+				"$(WK_DERIVED_SDK_HEADERS_DIR)",
+				"$(WK_AVAILABILITY_OVERLAY_YAML)",
+			);
+			name = "Public SDK gate";
+			outputFileListPaths = (
+			);
+			outputPaths = (
+				"$(DERIVED_FILE_DIR)/public-sdk-ready.timestamp",
+			);
+			runOnlyForDeploymentPostprocessing = 0;
+			shellPath = /bin/sh;
+			shellScript = "touch \"${SCRIPT_OUTPUT_FILE_0}\"\n";
+		};
+/* End PBXShellScriptBuildPhase section */
 
 /* Begin PBXSourcesBuildPhase section */
 		44E750BF2AC7291000828AC4 /* Sources */ = {

Source/ThirdParty/gtest/xcode/gtest.xcodeproj/project.pbxproj
@@ -22,6 +22,19 @@
 			name = Check;
 			productName = Check;
 		};
+		DDAD7E502FF6450E0054DF83 /* Setup */ = {
+			isa = PBXAggregateTarget;
+			buildConfigurationList = DDAD7E542FF6450E0054DF83 /* Build configuration list for PBXAggregateTarget "Setup" */;
+			buildPhases = (
+				DDAD7E552FF645450054DF83 /* Public SDK gate */,
+			);
+			dependencies = (
+			);
+			name = Setup;
+			packageProductDependencies = (
+			);
+			productName = Setup;
+		};
 /* End PBXAggregateTarget section */
 
 /* Begin PBXBuildFile section */
@@ -212,6 +225,20 @@
 			proxyType = 1;
 			remoteGlobalIDString = 40C84989101A36A60083642A;
 			remoteInfo = "sample1_unittest-static";
+		};
+		DDAD7E572FF645B30054DF83 /* PBXContainerItemProxy */ = {
+			isa = PBXContainerItemProxy;
+			containerPortal = 0867D690FE84028FC02AAC07 /* Project object */;
+			proxyType = 1;
+			remoteGlobalIDString = DDAD7E502FF6450E0054DF83;
+			remoteInfo = Setup;
+		};
+		DDAD7E592FF645B60054DF83 /* PBXContainerItemProxy */ = {
+			isa = PBXContainerItemProxy;
+			containerPortal = 0867D690FE84028FC02AAC07 /* Project object */;
+			proxyType = 1;
+			remoteGlobalIDString = DDAD7E502FF6450E0054DF83;
+			remoteInfo = Setup;
 		};
 /* End PBXContainerItemProxy section */
 
@@ -621,6 +648,7 @@
 				3171513E2D8250F500D53C18 /* PBXBuildRule */,
 			);
 			dependencies = (
+				DDAD7E582FF645B30054DF83 /* PBXTargetDependency */,
 			);
 			name = "gtest-static";
 			productName = "gtest-static";
@@ -636,6 +664,7 @@
 			buildRules = (
 			);
 			dependencies = (
+				DDAD7E5A2FF645B60054DF83 /* PBXTargetDependency */,
 			);
 			name = "gtest_main-static";
 			productName = "gtest_main-static";
@@ -708,6 +737,11 @@
 			attributes = {
 				LastSwiftUpdateCheck = 0700;
 				LastUpgradeCheck = 2700;
+				TargetAttributes = {
+					DDAD7E502FF6450E0054DF83 = {
+						CreatedOnToolsVersion = 26.5;
+					};
+				};
 			};
 			buildConfigurationList = 4FADC24608B4156D00ABE55E /* Build configuration list for PBXProject "gtest" */;
 			compatibilityVersion = "Xcode 3.2";
@@ -730,6 +764,7 @@
 				4089A0120FFACEFC000B29AE /* sample1_unittest-framework */,
 				40C84989101A36A60083642A /* sample1_unittest-static */,
 				3B238F5F0E828B5400846E11 /* Check */,
+				DDAD7E502FF6450E0054DF83 /* Setup */,
 			);
 		};
 /* End PBXProject section */
@@ -760,6 +795,27 @@
 			runOnlyForDeploymentPostprocessing = 0;
 			shellPath = /bin/sh;
 			shellScript = "# Remember, this \"Run Script\" build phase will be executed from $SRCROOT\n/bin/bash Scripts/runtests.sh\n";
+		};
+		DDAD7E552FF645450054DF83 /* Public SDK gate */ = {
+			isa = PBXShellScriptBuildPhase;
+			buildActionMask = 2147483647;
+			files = (
+			);
+			inputFileListPaths = (
+			);
+			inputPaths = (
+				"$(WK_DERIVED_SDK_HEADERS_DIR)",
+				"$(WK_AVAILABILITY_OVERLAY_YAML)",
+			);
+			name = "Public SDK gate";
+			outputFileListPaths = (
+			);
+			outputPaths = (
+				"$(DERIVED_FILE_DIR)/public-sdk-ready.timestamp",
+			);
+			runOnlyForDeploymentPostprocessing = 0;
+			shellPath = /bin/sh;
+			shellScript = "touch \"${SCRIPT_OUTPUT_FILE_0}\"\n";
 		};
 /* End PBXShellScriptBuildPhase section */
 
@@ -884,6 +940,16 @@
 			isa = PBXTargetDependency;
 			target = 40C84989101A36A60083642A /* sample1_unittest-static */;
 			targetProxy = 40C849F8101A43490083642A /* PBXContainerItemProxy */;
+		};
+		DDAD7E582FF645B30054DF83 /* PBXTargetDependency */ = {
+			isa = PBXTargetDependency;
+			target = DDAD7E502FF6450E0054DF83 /* Setup */;
+			targetProxy = DDAD7E572FF645B30054DF83 /* PBXContainerItemProxy */;
+		};
+		DDAD7E5A2FF645B60054DF83 /* PBXTargetDependency */ = {
+			isa = PBXTargetDependency;
+			target = DDAD7E502FF6450E0054DF83 /* Setup */;
+			targetProxy = DDAD7E592FF645B60054DF83 /* PBXContainerItemProxy */;
 		};
 /* End PBXTargetDependency section */
 
@@ -1125,6 +1191,30 @@
 			};
 			name = Release;
 		};
+		DDAD7E512FF6450E0054DF83 /* Debug */ = {
+			isa = XCBuildConfiguration;
+			buildSettings = {
+				CODE_SIGN_STYLE = Automatic;
+				PRODUCT_NAME = "$(TARGET_NAME)";
+			};
+			name = Debug;
+		};
+		DDAD7E522FF6450E0054DF83 /* Release */ = {
+			isa = XCBuildConfiguration;
+			buildSettings = {
+				CODE_SIGN_STYLE = Automatic;
+				PRODUCT_NAME = "$(TARGET_NAME)";
+			};
+			name = Release;
+		};
+		DDAD7E532FF6450E0054DF83 /* Production */ = {
+			isa = XCBuildConfiguration;
+			buildSettings = {
+				CODE_SIGN_STYLE = Automatic;
+				PRODUCT_NAME = "$(TARGET_NAME)";
+			};
+			name = Production;
+		};
 /* End XCBuildConfiguration section */
 
 /* Begin XCConfigurationList section */
@@ -1214,6 +1304,16 @@
 				4FADC24708B4156D00ABE55E /* Debug */,
 				4FADC24808B4156D00ABE55E /* Release */,
 				44C48E0414A409DE00A2D9C7 /* Production */,
+			);
+			defaultConfigurationIsVisible = 0;
+			defaultConfigurationName = Production;
+		};
+		DDAD7E542FF6450E0054DF83 /* Build configuration list for PBXAggregateTarget "Setup" */ = {
+			isa = XCConfigurationList;
+			buildConfigurations = (
+				DDAD7E512FF6450E0054DF83 /* Debug */,
+				DDAD7E522FF6450E0054DF83 /* Release */,
+				DDAD7E532FF6450E0054DF83 /* Production */,
 			);
 			defaultConfigurationIsVisible = 0;
 			defaultConfigurationName = Production;

commit 20f0fc462d9b0d2d606fbd22fe9e9f79d7eb4e85

[CMake] Only use vfsoverlay stub for public SDK builds https://bugs.webkit.org/show_bug.cgi?id=318802 rdar://181623820
Unreviewed build fix.

Match the availability-silencing behavior of the Xcode build, which
regressed in 316493@main. We should only use it when building against
the public SDK.

* Source/cmake/OptionsCocoa.cmake:

Canonical link: https://commits.webkit.org/316658@main

Source/cmake/OptionsCocoa.cmake
@@ -60,31 +60,33 @@ find_package(Threads REQUIRED)
 
 # Replace the SDK's availability headers with stubs that defuse availability
 # checks.
-set(_availability_overlay_dir "${CMAKE_SOURCE_DIR}/WebKitLibraries/AvailabilityOverlay")
-set(_availability_overlay_yaml "${CMAKE_BINARY_DIR}/availability-overlay.yaml")
-file(WRITE "${_availability_overlay_yaml}.tmp"
-"{
-  \"version\": 0,
-  \"case-sensitive\": false,
-  \"roots\": [
-    {
-      \"name\": \"${CMAKE_OSX_SYSROOT}/usr/include/os/availability.h\",
-      \"type\": \"file\",
-      \"external-contents\": \"${_availability_overlay_dir}/usr/include/os/availability.h\"
-    },
-    {
-      \"name\": \"${CMAKE_OSX_SYSROOT}/usr/include/Availability.h\",
-      \"type\": \"file\",
-      \"external-contents\": \"${_availability_overlay_dir}/usr/include/Availability.h\"
+if (NOT USE_APPLE_INTERNAL_SDK)
+    set(_availability_overlay_dir "${CMAKE_SOURCE_DIR}/WebKitLibraries/AvailabilityOverlay")
+    set(_availability_overlay_yaml "${CMAKE_BINARY_DIR}/availability-overlay.yaml")
+    file(WRITE "${_availability_overlay_yaml}.tmp"
+    "{
+      \"version\": 0,
+      \"case-sensitive\": false,
+      \"roots\": [
+        {
+          \"name\": \"${CMAKE_OSX_SYSROOT}/usr/include/os/availability.h\",
+          \"type\": \"file\",
+          \"external-contents\": \"${_availability_overlay_dir}/usr/include/os/availability.h\"
+        },
+        {
+          \"name\": \"${CMAKE_OSX_SYSROOT}/usr/include/Availability.h\",
+          \"type\": \"file\",
+          \"external-contents\": \"${_availability_overlay_dir}/usr/include/Availability.h\"
+        }
+      ]
     }
-  ]
-}
-")
-file(COPY_FILE "${_availability_overlay_yaml}.tmp" ${_availability_overlay_yaml} ONLY_IF_DIFFERENT)
-add_compile_options("$<$<NOT:$<COMPILE_LANGUAGE:Swift>>:SHELL:-ivfsoverlay ${_availability_overlay_yaml}>")
-add_compile_options("$<$<COMPILE_LANGUAGE:Swift>:SHELL:-vfsoverlay ${_availability_overlay_yaml}>")
-unset(_availability_overlay_dir)
-unset(_availability_overlay_yaml)
+    ")
+    file(COPY_FILE "${_availability_overlay_yaml}.tmp" ${_availability_overlay_yaml} ONLY_IF_DIFFERENT)
+    add_compile_options("$<$<NOT:$<COMPILE_LANGUAGE:Swift>>:SHELL:-ivfsoverlay ${_availability_overlay_yaml}>")
+    add_compile_options("$<$<COMPILE_LANGUAGE:Swift>:SHELL:-vfsoverlay ${_availability_overlay_yaml}>")
+    unset(_availability_overlay_dir)
+    unset(_availability_overlay_yaml)
+endif ()
 
 if (EXISTS "/usr/local/include/WebKitAdditions" AND NOT EXISTS "/usr/local/include/AppleFeatures/AppleFeatures.h")
     set(_apple_features_stub "${CMAKE_BINARY_DIR}/generated-stubs/AppleFeatures")