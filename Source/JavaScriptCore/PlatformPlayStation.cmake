1e6cdb5a88d5bc752543d25f203ed3f83e8ddb70
Stephan Szabo <stephan.szabo@sony.com>
2026-07-08 14:39:35 -0700

    Remove stubs for missing ICU functions on PlayStation https://bugs.webkit.org/show_bug.cgi?id=318880
    Reviewed by Yusuke Suzuki.
    
    Remove the stub file and reference for building it.
    Functions should now be available in dev SDKs for PlayStation.
    
    * Source/JavaScriptCore/PlatformPlayStation.cmake:
    * Source/JavaScriptCore/runtime/temporal/core/ICUCalendarStubs.cpp: Removed.
    
    Canonical link: https://commits.webkit.org/316762@main

--- Source/JavaScriptCore/PlatformPlayStation.cmake
+++ Source/JavaScriptCore/PlatformPlayStation.cmake
@@ -7,14 +7,6 @@ list(APPEND JavaScriptCore_PRIVATE_DEFINITIONS
 
 list(APPEND JavaScriptCore_PRIVATE_INCLUDE_DIRECTORIES ${MEMORY_EXTRA_INCLUDE_DIR})
 
-# PlayStation's libSceIcu_stub_weak.a does not export UCal write/mutation APIs
-# (ucal_set, ucal_add, ucal_getMillis, ucal_getTimeZoneTransitionDate, udat_clone, etc.)
-# that the Temporal ICU bridge requires. Provide stub implementations so the
-# build links successfully; stubs set U_UNSUPPORTED_ERROR so callers degrade gracefully.
-list(APPEND JavaScriptCore_SOURCES
-    runtime/temporal/core/ICUCalendarStubs.cpp
-)
-
 target_link_libraries(LLIntSettingsExtractor PRIVATE ${MEMORY_EXTRA_LIB})
 target_link_libraries(LLIntOffsetsExtractor PRIVATE ${MEMORY_EXTRA_LIB})
 
