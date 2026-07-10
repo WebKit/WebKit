d20df22f308ad7367044b996c0d75d592f1a84ef
Richard Robinson <richard_robinson2@apple.com>
2026-07-09 22:30:10 -0700

    Cherry-pick fd3b2868954a. rdar://182105479
    Follow-up to 316796@main and subsequent build fixes to document safety invariants and FIXMEs
    https://bugs.webkit.org/show_bug.cgi?id=319024
    rdar://181879513
    
    Reviewed by Abrar Rahman Protyasha.
    
    Swift 6.4 has the safe `Span(viewing:)` initializer API (https://developer.apple.com/documentation/swift/span/init(viewing:)-9d31g) implemented in https://github.com/swiftlang/swift/commit/8e5539f6199458258d508210ed2f2256911c6ca3 .
    
    However, some configurations of WebKit use an old Swift 6.4 version which does not yet have such
    API. Therefore, a build fix was landed to use the pre-API version of the initializer, which has
    an identical implementation but is marked as unsafe since its constraints on the `Span.Element`
    type aren't as strict.
    
    Document this safety reasoning and add a FIXME.
    
    * Source/WebKit/UIProcess/API/Cocoa/WKUserContentController.swift:
    (WKUserContentController.addBuffer(_:name:to:)):
    
    Canonical link: https://commits.webkit.org/316606.53@safari-7625-branch

--- Source/WebKit/UIProcess/API/Cocoa/WKUserContentController.swift
+++ Source/WebKit/UIProcess/API/Cocoa/WKUserContentController.swift
@@ -40,7 +40,10 @@ extension WKUserContentController {
     ///     The buffer will only be visible to JavaScript executing in that content world.
     ///
     public func addBuffer(_ buffer: RawSpan, name: Swift.String, to contentWorld: WKContentWorld) {
+        // Safety: This is safe because it's just the pre-API version of the safe `Span(viewing:)` API in Swift 6.4.
+        // FIXME: (rdar://181879532) Adopt `Span(viewing:)` initializer instead of `Span(_bytes:)` in `WKUserContentController/addBuffer` implementation.
         let typedSpan = unsafe Span<UInt8>(_bytes: buffer)
+
         // This use of unsafe is necessary to wrap the RawSpan for immediate processing by WebKit,
         // unknowledging that the safety of the RawSpan passed in by the client cannot be guaranteed.
         // This is fine becuase WebKit is going to immediately make a copy of the passed-in bytes
