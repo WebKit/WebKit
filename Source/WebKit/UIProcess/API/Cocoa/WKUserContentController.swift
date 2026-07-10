cb21f712f67ec442c320923526efa2de5aa62092
Ryosuke Niwa <rniwa@webkit.org>
2026-07-09 18:32:21 -0700

    Cherry-pick 4e6aeaa03e7a3. rdar://182237879
    REGRESSION(316796@main): Safer C++ builds are broken
    https://bugs.webkit.org/show_bug.cgi?id=318999
    
    Reviewed by Wenson Hsieh.
    
    Compile out the offending code in Safer C++ bots for now.
    
    * Source/WebKit/UIProcess/API/Cocoa/WKUserContentController.swift:
    
    Canonical link: https://commits.webkit.org/316606.52@safari-7625-branch

--- Source/WebKit/UIProcess/API/Cocoa/WKUserContentController.swift
+++ Source/WebKit/UIProcess/API/Cocoa/WKUserContentController.swift
@@ -21,7 +21,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 // THE POSSIBILITY OF SUCH DAMAGE.
 
-#if compiler(>=6.4)
+#if compiler(>=6.4) && !SWIFT_WEBKIT_TOOLCHAIN
 
 import Foundation
 import WebKit_Internal
