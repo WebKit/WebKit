6675202f43106524639134d4c80ad9bae4a7cd2c
Richard Robinson <richard_robinson2@apple.com>
2026-07-10 09:16:24 -0700

    Speculatively fix the build for some macOS configurations after 316796@main https://bugs.webkit.org/show_bug.cgi?id=319027 rdar://181881668
    Unreviewed build fix.
    
    * Source/WebKit/UIProcess/API/Cocoa/WKContentWorld.swift:
    (WKContentWorld.world(with:)):
    
    Canonical link: https://commits.webkit.org/316907@main

--- Source/WebKit/UIProcess/API/Cocoa/WKContentWorld.swift
+++ Source/WebKit/UIProcess/API/Cocoa/WKContentWorld.swift
@@ -23,11 +23,16 @@
 
 import Foundation
 
+// SPI typealias to keep others building for now
+// swift-format-ignore: AllPublicDeclarationsHaveDocumentation
+@_spi(_)
+public typealias WKContentWorldConfiguration = WKContentWorld.Configuration
+
 extension WKContentWorld {
     // SPI factory to keep others building for now
     // swift-format-ignore: AllPublicDeclarationsHaveDocumentation
     @_spi(APIAdoptionStaging)
-    public static func world(with configuration: WKContentWorld.Configuration) -> WKContentWorld {
+    public static func world(with configuration: WKContentWorldConfiguration) -> WKContentWorld {
         WKContentWorld(configuration: configuration)
     }
 }
