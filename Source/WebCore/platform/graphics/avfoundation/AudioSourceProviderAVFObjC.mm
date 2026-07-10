f84f369decbcb181621c2603bef8445e85ffa158
Eric Carlson <eric.carlson@apple.com>
2026-07-09 18:54:46 -0700

    AudioSourceProviderAVFObjC should only allow callbacks to be set once rdar://177358137
    Reviewed by Jean-Yves Avenard
    
    AudioSourceProviderAVFObjC::setAudioCallback() and AudioSourceProviderAVFObjC::setConfigureAudioStorageCallback
    ASSERT(m_avAudioMix) in a debug build so enforce this condition in a release build by
    returning early.
    
    * Source/WebCore/platform/graphics/avfoundation/AudioSourceProviderAVFObjC.mm:
    (WebCore::AudioSourceProviderAVFObjC::setAudioCallback):
    (WebCore::AudioSourceProviderAVFObjC::setConfigureAudioStorageCallback):
    
    Identifier: 305413.1109@safari-7624.5-branch

--- Source/WebCore/platform/graphics/avfoundation/AudioSourceProviderAVFObjC.mm
+++ Source/WebCore/platform/graphics/avfoundation/AudioSourceProviderAVFObjC.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014, 2015 Apple Inc. All rights reserved.
+ * Copyright (C) 2014-2026 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -419,12 +419,16 @@ void AudioSourceProviderAVFObjC::process(MTAudioProcessingTapRef tap, CMItemCoun
 void AudioSourceProviderAVFObjC::setAudioCallback(AudioCallback&& callback)
 {
     ASSERT(!m_avAudioMix);
+    if (m_avAudioMix)
+        return;
     m_audioCallback = WTF::move(callback);
 }
 
 void AudioSourceProviderAVFObjC::setConfigureAudioStorageCallback(ConfigureAudioStorageCallback&& callback)
 {
     ASSERT(!m_avAudioMix);
+    if (m_avAudioMix)
+        return;
     m_configureAudioStorageCallback = WTF::move(callback);
 }
 
