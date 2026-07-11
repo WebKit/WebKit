0e86b49069a5e3737c1cc7af554833d06580cc9a
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2026-07-10 18:00:10 -0700

    Reduce VTTCue size by regrouping m_displayDirection with adjacent small enums https://bugs.webkit.org/show_bug.cgi?id=319026 rdar://181880375
    Reviewed by Eric Carlson.
    
    m_displayDirection (CSSValueID, a uint16_t enum) was declared immediately
    before the double m_displaySize. Sitting on an 8-byte-aligned offset, its
    2 bytes forced 6 bytes of padding before the double, wasting a full word.
    
    Move it up next to m_writingDirection and m_cueAlignment (both 1-byte
    enums), which already left 6 bytes of padding before the following
    RefPtr<VTTRegion>. The enum now folds into that existing hole at no cost,
    and m_displaySize leads its group without a preceding gap. This shrinks
    the object by 8 bytes with no behavioral change.
    
    * Source/WebCore/html/track/VTTCue.h:
    
    Canonical link: https://commits.webkit.org/316950@main

--- Source/WebCore/html/track/VTTCue.h
+++ Source/WebCore/html/track/VTTCue.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2011, 2013 Google Inc. All rights reserved.
- * Copyright (C) 2012-2023 Apple Inc. All rights reserved.
+ * Copyright (C) 2012-2026 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -273,6 +273,7 @@ private:
 
     DirectionSetting m_writingDirection { DirectionSetting::Horizontal };
     AlignSetting m_cueAlignment { AlignSetting::Center };
+    CSSValueID m_displayDirection { CSSValueLtr };
 
     RefPtr<VTTRegion> m_region;
     String m_parsedRegionId;
@@ -286,7 +287,6 @@ private:
     RefPtr<SpeechSynthesisUtterance> m_speechUtterance;
 #endif
 
-    CSSValueID m_displayDirection { CSSValueLtr };
     double m_displaySize { 0 };
     DisplayPosition m_displayPosition;
 
