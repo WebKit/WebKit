b42421de79d1c2daf9b4c26119113cf9926f6260
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2026-07-11 00:19:55 -0700

    Use `using` alias instead of `typedef` in WebVTTToken https://bugs.webkit.org/show_bug.cgi?id=319154 rdar://181976250
    Reviewed by Tim Nguyen.
    
    Replace the C-style `typedef WebVTTTokenTypes Type;` with a modern
    `using Type = WebVTTTokenTypes;` alias. No behavior change.
    
    * Source/WebCore/html/track/WebVTTToken.h:
    
    Canonical link: https://commits.webkit.org/316958@main

--- Source/WebCore/html/track/WebVTTToken.h
+++ Source/WebCore/html/track/WebVTTToken.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2011, 2013 Google Inc. All rights reserved.
- * Copyright (C) 2014 Apple Inc. All rights reserved.
+ * Copyright (C) 2014-2026 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -51,7 +51,7 @@ public:
 
 class WebVTTToken {
 public:
-    typedef WebVTTTokenTypes Type;
+    using Type = WebVTTTokenTypes;
 
     WebVTTToken()
         : m_type(Type::Uninitialized) { }
