050117b8ea1118fbd13e6fa410bbd723ec2bf972
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2026-07-12 23:41:08 -0700

    AudioParamTimeline::cancelAndHoldAtTime discards exception from second insertEvent https://bugs.webkit.org/show_bug.cgi?id=319186 rdar://182035131
    Reviewed by Chris Dumez.
    
    When cancelAndHoldAtTime() splits a SetValueCurve, it inserts a
    replacement curve event and then a trailing SetValue event. The second
    insertEvent() call ignored its own result and re-checked the already-
    validated result from the first insert, so any exception from the
    second insert would be silently swallowed.
    
    Capture and check the second insert's own result.
    
    No new tests: the trailing SetValue lands exactly at the truncated
    curve's end (a non-overlap) with all later events already removed, so
    the second insert cannot throw today. This is a latent correctness fix,
    not a web-observable bug.
    
    * Source/WebCore/Modules/webaudio/AudioParamTimeline.cpp:
    (WebCore::AudioParamTimeline::cancelAndHoldAtTime):
    
    Canonical link: https://commits.webkit.org/317019@main

--- Source/WebCore/Modules/webaudio/AudioParamTimeline.cpp
+++ Source/WebCore/Modules/webaudio/AudioParamTimeline.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2011 Google Inc. All rights reserved.
- * Copyright (C) 2022-2024 Apple Inc. All rights reserved.
+ * Copyright (C) 2022-2026 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -314,9 +314,9 @@ ExceptionOr<void> AudioParamTimeline::cancelAndHoldAtTime(Seconds cancelTime)
         if (result.hasException())
             return result.releaseException();
         if (newSetValueEvent) {
-            insertEvent(WTF::move(*newSetValueEvent));
-            if (result.hasException())
-                return result.releaseException();
+            auto setValueResult = insertEvent(WTF::move(*newSetValueEvent));
+            if (setValueResult.hasException())
+                return setValueResult.releaseException();
         }
     }
 
