71b801ce6146294598f75bcb683bfb62f09e99cc
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2026-07-15 14:00:31 -0700

    Cherry-pick 317026@main (986e5185e1c1). https://bugs.webkit.org/show_bug.cgi?id=319222
        IndexedDB getCount() truncates record counts above INT_MAX
        https://bugs.webkit.org/show_bug.cgi?id=319222
        rdar://182075709
    
        Reviewed by Chris Dumez.
    
        SQLiteIDBBackingStore::getCount() writes the record count into a
        uint64_t out-parameter, and SQLite's COUNT(*) is a 64-bit integer,
        but the result was read with columnInt(), which returns a 32-bit int.
        A count above INT_MAX would be truncated (and sign-extended into a
        bogus large value). Read the column with columnInt64() instead,
        matching uncheckedGetKeyGeneratorValue() just below it.
    
        No new tests; triggering truncation requires more than 2^31 records
        in a single object store, which is not practical to exercise in a
        test.
    
        * Source/WebCore/Modules/indexeddb/server/SQLiteIDBBackingStore.cpp:
        (WebCore::IDBServer::SQLiteIDBBackingStore::getCount):
    
        Canonical link: https://commits.webkit.org/317026@main
    
    Canonical link: https://commits.webkit.org/305877.979@webkitglib/2.52

--- Source/WebCore/Modules/indexeddb/server/SQLiteIDBBackingStore.cpp
+++ Source/WebCore/Modules/indexeddb/server/SQLiteIDBBackingStore.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2021 Apple Inc. All rights reserved.
+ * Copyright (C) 2016-2026 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -2496,7 +2496,7 @@ IDBError SQLiteIDBBackingStore::getCount(const IDBResourceIdentifier& transactio
     if (statement->step() != SQLITE_ROW)
         return IDBError { ExceptionCode::UnknownError, "Unable to count records"_s };
 
-    outCount = statement->columnInt(0);
+    outCount = statement->columnInt64(0);
     return IDBError { };
 }
 
