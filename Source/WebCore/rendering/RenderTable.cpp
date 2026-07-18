27059d44ca3687792473995c99ee26b7fb9040d3
Zak Ridouh <zakr@apple.com>
2026-07-18 15:55:16 -0700

    Improve readability of StyleLogicalHeight by adopting a switch on statement. https://bugs.webkit.org/show_bug.cgi?id=303746 rdar://166529986
    Reviewed by Sam Weinig.
    
    Replace the if/else chain in RenderTable::convertStyleLogicalHeightToComputedHeight()
    with WTF::switchOn() for improved readability, matching the pattern already used in
    RenderFlexibleBox and other rendering code.
    
    The intrinsic and stretch sizing keywords are coalesced behind a new
    Style::IsIntrinsicOrStretchSizeKeyword concept (the compile-time analog of the sizing
    types' isIntrinsicOrStretch() predicate), placed in a new
    style/values/sizing/StyleSizing.h header. The remaining keywords are enumerated
    explicitly rather than caught by a generic handler, so that adding a new sizing keyword
    fails to compile here and forces this switch to be revisited.
    
    No new tests, no change of functionality.
    
    * Source/WebCore/rendering/RenderTable.cpp:
    (WebCore::RenderTable::convertStyleLogicalHeightToComputedHeight):
    * Source/WebCore/style/values/sizing/StyleSizing.h: Added.
    * Source/WebCore/WebCore.xcodeproj/project.pbxproj:
    
    Canonical link: https://commits.webkit.org/317491@main

--- Source/WebCore/rendering/RenderTable.cpp
+++ Source/WebCore/rendering/RenderTable.cpp
@@ -64,6 +64,7 @@
 #include "StyleComputedStyle+GettersInlines.h"
 #include "StylePrimitiveNumericTypes+Evaluation.h"
 #include "StylePrimitiveNumericTypes+EvaluationMinimum.h"
+#include "StyleSizing.h"
 #include <wtf/SetForScope.h>
 #include <wtf/StackStats.h>
 #include <wtf/TZoneMallocInlines.h>
@@ -359,24 +360,48 @@ template<typename SizeType> LayoutUnit RenderTable::convertStyleLogicalWidthToCo
 
 template<typename SizeType> LayoutUnit RenderTable::convertStyleLogicalHeightToComputedHeight(const SizeType& styleLogicalHeight)
 {
+    CheckedRef checkedThis { *this };
     LayoutUnit borderAndPaddingBefore = borderBefore() + (collapseBorders() ? 0_lu : paddingBefore());
     LayoutUnit borderAndPaddingAfter = borderAfter() + (collapseBorders() ? 0_lu : paddingAfter());
     LayoutUnit borderAndPadding = borderAndPaddingBefore + borderAndPaddingAfter;
-    if (auto fixedStyleLogicalHeight =  styleLogicalHeight.tryFixed()) {
-        // HTML tables size as though CSS height includes border/padding, CSS tables do not.
-        LayoutUnit borders;
-        // FIXME: We cannot apply box-sizing: content-box on <table> which other browsers allow.
-        if (is<HTMLTableElement>(element()) || style().boxSizing() == BoxSizing::BorderBox) {
-            borders = borderAndPadding;
+    return WTF::switchOn(styleLogicalHeight,
+        [&](const typename SizeType::Fixed& fixedStyleLogicalHeight) {
+            // HTML tables size as though CSS height includes border/padding, CSS tables do not.
+            LayoutUnit borders;
+            // FIXME: We cannot apply box-sizing: content-box on <table> which other browsers allow.
+            if (is<HTMLTableElement>(checkedThis->element()) || checkedThis->style().boxSizing() == BoxSizing::BorderBox)
+                borders = borderAndPadding;
+            return Style::evaluate<LayoutUnit>(fixedStyleLogicalHeight, checkedThis->style().usedZoomForLength()) - borders;
+        },
+        [&](const typename SizeType::Percentage&) {
+            return checkedThis->computePercentageLogicalHeight(styleLogicalHeight).value_or(0_lu);
+        },
+        [&](const typename SizeType::Calc& calc) {
+            return checkedThis->computePercentageLogicalHeight(calc).value_or(0_lu);
+        },
+        [&](Style::IsIntrinsicOrStretchSizeKeyword auto const&) {
+            return checkedThis->computeSizingKeywordLogicalContentHeightUsing(styleLogicalHeight, checkedThis->logicalHeight() - borderAndPadding, borderAndPadding).value_or(0_lu);
+        },
+        // The remaining keywords are not valid computed values for a table's logical height. They are
+        // enumerated explicitly (rather than caught by a generic handler) so that adding a new keyword
+        // to any of the sizing types fails to compile here and forces this switch to be revisited.
+        [&](const CSS::Keyword::Auto&) {
+            ASSERT_NOT_REACHED();
+            return 0_lu;
+        },
+        [&](const CSS::Keyword::None&) {
+            ASSERT_NOT_REACHED();
+            return 0_lu;
+        },
+        [&](const CSS::Keyword::Intrinsic&) {
+            ASSERT_NOT_REACHED();
+            return 0_lu;
+        },
+        [&](const CSS::Keyword::MinIntrinsic&) {
+            ASSERT_NOT_REACHED();
+            return 0_lu;
         }
-        return Style::evaluate<LayoutUnit>(*fixedStyleLogicalHeight, style().usedZoomForLength()) - borders;
-    } else if (styleLogicalHeight.isPercentOrCalculated())
-        return computePercentageLogicalHeight(styleLogicalHeight).value_or(0);
-    else if (styleLogicalHeight.isIntrinsicOrStretch())
-        return computeSizingKeywordLogicalContentHeightUsing(styleLogicalHeight, logicalHeight() - borderAndPadding, borderAndPadding).value_or(0);
-    else
-        ASSERT_NOT_REACHED();
-    return 0_lu;
+    );
 }
 
 void RenderTable::layoutCaption(RenderTableCaption& caption)
