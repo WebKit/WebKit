WebKit/WebKit

commit 165c11e88f109c66d6b3edb325dd67660c1c650f

[iOS] Video playback remains active on a wireless device after switching back to local playback https://bugs.webkit.org/show_bug.cgi?id=319638 rdar://182125629
Reviewed by Jer Noble.

Two bugs led to video playback remaining active on a wireless device after the user switches back
to local playback in Safari:

1. On routes that support video URL casting but not real-time audio streaming, an
   AVAudioSessionRouteChangeNotification notification is not dispatched when that route is selected
   or deselected. This bypassed the logic in -[WebMediaSessionHelper activeOutputDeviceDidChange:]
   that would reconfigure the media engine for local playback.
2. In MediaPlayerPrivateWirelessPlayback::setWirelessPlaybackTarget, we would not disconnect the
   old playback target's route session prior to switching to the new playback target. This resulted
   in playback continuing on the wireless route.

Resolved (1) by consolidating the logic for switching between wireless and local routes into
MediaSessionHelper::activeRoutesDidChange, since that function is called whether or not the
wireless route supports real-time audio streaming. Resolved (2) by disconnecting the outgoing route
from its playback session and clearing ourselves as the route client in
MediaPlayerPrivateWirelessPlayback::setWirelessPlaybackTarget.

Tests: media/wireless-playback-media-player/route-deactivate-pauses-playback.html
       media/wireless-playback-media-player/route-switch-disconnects-previous-route.html

* LayoutTests/media/wireless-playback-media-player/route-deactivate-pauses-playback-expected.txt: Added.
* LayoutTests/media/wireless-playback-media-player/route-deactivate-pauses-playback.html: Added.
* LayoutTests/media/wireless-playback-media-player/route-switch-disconnects-previous-route-expected.txt: Added.
* LayoutTests/media/wireless-playback-media-player/route-switch-disconnects-previous-route.html: Added.
* Source/WebCore/PAL/pal/spi/cocoa/AVFoundationSPI.h:

Source/WebCore/platform/graphics/avfoundation/MediaPlaybackTargetCocoa.mm
@@ -30,10 +30,6 @@
 
 #import <pal/spi/cocoa/AVFoundationSPI.h>
 
-#if HAVE(AVROUTING_FRAMEWORK)
-#import <WebKitAdditions/MediaPlaybackTargetCocoaAdditions.mm>
-#endif
-
 #import <pal/cocoa/AVFoundationSoftLink.h>
 
 namespace WebCore {
@@ -78,6 +74,18 @@ bool MediaPlaybackTargetCocoa::hasActiveRoute() const
             return outputDevice.deviceFeatures & (AVOutputDeviceFeatureVideo | AVOutputDeviceFeatureAudio);
     }
     return m_outputContext.get().deviceName;
+}
+
+// FIXME: This check is only needed temporarily. Once rdar://181927768 is resolved,
+// AVSystemRouting will not even notify us of routes that do no support the video feature.
+bool MediaPlaybackTargetCocoa::supportsCustomProtocolVideoPlayback() const
+{
+    for (AVOutputDevice *outputDevice in [m_outputContext outputDevices]) {
+        if (outputDevice.deviceType == AVOutputDeviceTypeCustomProtocol && outputDevice.deviceFeatures & AVOutputDeviceFeatureVideo)
+            return true;
+    }
+
+    return false;
 }
 
 bool MediaPlaybackTargetCocoa::supportsRemoteVideoPlayback() const

LayoutTests/media/wireless-playback-media-player/route-deactivate-pauses-playback-expected.txt
@@ -1,0 +1,17 @@
+
+Test that deactivating the active MediaDeviceRoute pauses the video element.
+
+RUN(internals.mockMediaDeviceRouteController.activateRoute(route))
+RUN(video.src = findMediaFile('video', '../content/test'))
+RUN(route.playbackRate = 1)
+RUN(route.timeRange = { start: 0, duration: 60 })
+RUN(video.play())
+RUN(route.ready = true)
+EVENT(playing)
+RUN(route.currentPlaybackPosition = 10)
+RUN(internals.mockMediaDeviceRouteController.deactivateRoute(route))
+EVENT(pause)
+EXPECTED (video.paused == 'true') OK
+EXPECTED (internals.elementEffectivePlaybackRate(video) == '0') OK
+END OF TEST
+

LayoutTests/media/wireless-playback-media-player/route-deactivate-pauses-playback.html
@@ -1,0 +1,52 @@
+<!DOCTYPE html><!-- webkit-test-runner [ WirelessPlaybackMediaPlayerEnabled=true ] -->
+<html>
+    <head>
+        <script src='../media-file.js'></script>
+        <script src='../video-test.js'></script>
+        <script>
+            var route;
+            var video;
+
+            async function start()
+            {
+                if (window.internals)
+                    internals.mockMediaDeviceRouteController.enabled = true;
+
+                route = internals.mockMediaDeviceRouteController.createMockMediaDeviceRoute();
+                run(`internals.mockMediaDeviceRouteController.activateRoute(route)`);
+
+                const urlPromise = new Promise((resolve) => {
+                    route.setURLCallback(async (url) => {
+                        resolve(url);
+                    });
+                });
+
+                video = document.getElementsByTagName('video')[0];
+                run(`video.src = findMediaFile('video', '../content/test')`);
+                run(`route.playbackRate = 1`);
+                run(`route.timeRange = { start: 0, duration: 60 }`);
+                run(`video.play()`);
+                await urlPromise;
+
+                const playingPromise = waitFor(video, 'playing');
+                run(`route.ready = true`);
+                await playingPromise;
+
+                run(`route.currentPlaybackPosition = 10`);
+                await waitForConditionOrTimeout('video.currentTime >= 10', true, 3000);
+
+                const pausePromise = waitFor(video, 'pause');
+                run(`internals.mockMediaDeviceRouteController.deactivateRoute(route)`);
+                await pausePromise;
+                testExpected(`video.paused`, true);
+                testExpected(`internals.elementEffectivePlaybackRate(video)`, 0);
+
+                endTest();
+            }
+        </script>
+    </head>
+    <body onload='start()'>
+        <video controls></video>
+        <p>Test that deactivating the active MediaDeviceRoute pauses the video element.</p>
+    </body>
+</html>

LayoutTests/media/wireless-playback-media-player/route-switch-disconnects-previous-route-expected.txt
@@ -1,0 +1,22 @@
+
+Test that activating a new MediaDeviceRoute disconnects the previously active route.
+
+RUN(internals.mockMediaDeviceRouteController.activateRoute(route1))
+RUN(video.src = findMediaFile('video', '../content/test'))
+RUN(route1.playbackRate = 1)
+RUN(route1.timeRange = { start: 0, duration: 60 })
+RUN(route1.audioOptions = [{ displayName: 'Audio', identifier: 'audio' }])
+RUN(video.play())
+RUN(route1.ready = true)
+EVENT(playing)
+EXPECTED (route1.connected == 'true') OK
+EXPECTED (internals.mediaUsageState(video).hasAudio == 'true') OK
+RUN(route2.playbackRate = 1)
+RUN(route2.timeRange = { start: 0, duration: 60 })
+RUN(route2.audioOptions = [{ displayName: 'Audio', identifier: 'audio' }])
+RUN(internals.mockMediaDeviceRouteController.activateRoute(route2))
+RUN(route2.ready = true)
+EXPECTED (route1.connected == 'false') OK
+EXPECTED (route2.connected == 'true') OK
+END OF TEST
+

LayoutTests/media/wireless-playback-media-player/route-switch-disconnects-previous-route.html
@@ -1,0 +1,69 @@
+<!DOCTYPE html><!-- webkit-test-runner [ WirelessPlaybackMediaPlayerEnabled=true ] -->
+<html>
+    <head>
+        <script src='../media-file.js'></script>
+        <script src='../video-test.js'></script>
+        <script>
+            var route1;
+            var route2;
+            var video;
+
+            function nextURLCallback(route)
+            {
+                return new Promise((resolve) => {
+                    route.setURLCallback(async (url) => {
+                        resolve(url);
+                    });
+                });
+            }
+
+            async function start()
+            {
+                if (window.internals)
+                    internals.mockMediaDeviceRouteController.enabled = true;
+
+                route1 = internals.mockMediaDeviceRouteController.createMockMediaDeviceRoute();
+                let urlPromise = nextURLCallback(route1);
+                run(`internals.mockMediaDeviceRouteController.activateRoute(route1)`);
+
+                video = document.getElementsByTagName('video')[0];
+                run(`video.src = findMediaFile('video', '../content/test')`);
+                run(`route1.playbackRate = 1`);
+                run(`route1.timeRange = { start: 0, duration: 60 }`);
+                run(`route1.audioOptions = [{ displayName: 'Audio', identifier: 'audio' }]`);
+                run(`video.play()`);
+                await urlPromise;
+
+                const playingPromise = waitFor(video, 'playing');
+                run(`route1.ready = true`);
+                await playingPromise;
+
+                await waitForConditionOrTimeout('route1.connected', true);
+                testExpected(`route1.connected`, true);
+
+                await testExpectedEventually('internals.mediaUsageState(video).hasAudio', true);
+
+                route2 = internals.mockMediaDeviceRouteController.createMockMediaDeviceRoute();
+                urlPromise = nextURLCallback(route2);
+                run(`route2.playbackRate = 1`);
+                run(`route2.timeRange = { start: 0, duration: 60 }`);
+                run(`route2.audioOptions = [{ displayName: 'Audio', identifier: 'audio' }]`);
+                run(`internals.mockMediaDeviceRouteController.activateRoute(route2)`);
+                await urlPromise;
+
+                run(`route2.ready = true`);
+                await waitForConditionOrTimeout('route2.connected', true);
+
+                await waitForConditionOrTimeout('!route1.connected', true);
+                testExpected(`route1.connected`, false);
+                testExpected(`route2.connected`, true);
+
+                endTest();
+            }
+        </script>
+    </head>
+    <body onload='start()'>
+        <video controls></video>
+        <p>Test that activating a new MediaDeviceRoute disconnects the previously active route.</p>
+    </body>
+</html>

Source/WebCore/PAL/pal/spi/cocoa/AVFoundationSPI.h
@@ -134,10 +134,15 @@ typedef NS_OPTIONS(NSUInteger, AVOutputDeviceFeatures) {
     AVOutputDeviceFeatureVideo = (1UL << 2),
 };
 
+typedef NS_ENUM(NSInteger, AVOutputDeviceType) {
+    AVOutputDeviceTypeCustomProtocol = 5,
+};
+
 @interface AVOutputDevice : NSObject
 @property (nonatomic, readonly) NSString *name;
 @property (nonatomic, readonly) NSString *deviceName;
 @property (nonatomic, readonly) AVOutputDeviceFeatures deviceFeatures;
+@property (nonatomic, readonly) AVOutputDeviceType deviceType;
 @end
 
 #if !PLATFORM(IOS_FAMILY)

Source/WebCore/platform/audio/ios/MediaSessionHelperIOS.mm
@@ -260,27 +260,27 @@ void MediaSessionHelper::activeRoutesDidChange(MediaDeviceRouteController& route
     ASSERT(&MediaDeviceRouteController::singleton() == &routeController);
 
     Ref target = MediaPlaybackTargetCocoa::create();
+    RefPtr mostRecentActiveRoute = routeController.mostRecentActiveRoute();
 
-    if (RefPtr mostRecentActiveRoute = routeController.mostRecentActiveRoute()) {
-#if HAVE(AVROUTING_FRAMEWORK)
-        bool hasAirPlayDevice = target->hasAirPlayDevice();
-        RELEASE_LOG(Media, "MediaSessionHelper::activeRoutesDidChange: hasAirPlayDevice = %d", hasAirPlayDevice);
-        if (!hasAirPlayDevice)
-            return;
-
-        auto wirelessPlaybackTarget = dynamicDowncast<MediaPlaybackTargetWirelessPlayback>(m_playbackTarget.get());
-        if (wirelessPlaybackTarget && wirelessPlaybackTarget->route() == mostRecentActiveRoute.get())
-            return;
+    bool supportsVideo = [&] {
+#if PLATFORM(IOS_FAMILY_SIMULATOR)
+        return true;
+#else
+        return target->supportsCustomProtocolVideoPlayback();
 #endif
+    }();
 
-        activeVideoRouteDidChange(SupportsAirPlayVideo::Yes, MediaPlaybackTargetWirelessPlayback::create(*mostRecentActiveRoute));
+    RELEASE_LOG(Media, "MediaSessionHelper::activeRoutesDidChange: mostRecentActiveRoute=%d, supportsVideo=%d", !!mostRecentActiveRoute, supportsVideo);
+
+    if (!mostRecentActiveRoute || !supportsVideo) {
+        activeAudioRouteDidChange(ShouldPause::Yes);
+        activeVideoRouteDidChange(SupportsAirPlayVideo::No, WTF::move(target));
         return;
     }
 
-#if PLATFORM(IOS_FAMILY_SIMULATOR)
-    activeAudioRouteDidChange(ShouldPause::Yes);
-    activeVideoRouteDidChange(SupportsAirPlayVideo::No, WTF::move(target));
-#endif
+    auto wirelessPlaybackTarget = dynamicDowncast<MediaPlaybackTargetWirelessPlayback>(m_playbackTarget.get());
+    if (!wirelessPlaybackTarget || wirelessPlaybackTarget->route() != mostRecentActiveRoute.get())
+        activeVideoRouteDidChange(SupportsAirPlayVideo::Yes, MediaPlaybackTargetWirelessPlayback::create(*mostRecentActiveRoute));
 }
 #endif // ENABLE(WIRELESS_PLAYBACK_MEDIA_PLAYER)
 
@@ -602,22 +602,7 @@ void MediaSessionHelperIOS::externalOutputDeviceAvailableDidChange()
 
         callback->updateCarPlayIsConnected();
         callback->activeAudioRouteDidChange(shouldPause);
-
-        Ref target = MediaPlaybackTargetCocoa::create();
-
-#if HAVE(AVROUTING_FRAMEWORK)
-        bool hasAirPlayDevice = target->hasAirPlayDevice();
-        RELEASE_LOG(Media, "-[WebMediaSessionHelper activeOutputDeviceDidChange:]: hasAirPlayDevice = %d", hasAirPlayDevice);
-        if (hasAirPlayDevice) {
-            RefPtr mostRecentActiveRoute = MediaDeviceRouteController::singleton().mostRecentActiveRoute();
-            auto wirelessPlaybackTarget = dynamicDowncast<MediaPlaybackTargetWirelessPlayback>(callback->playbackTarget());
-            if (!wirelessPlaybackTarget || wirelessPlaybackTarget->route() != mostRecentActiveRoute.get())
-                callback->activeVideoRouteDidChange(MediaPlaybackTargetWirelessPlayback::create(*mostRecentActiveRoute));
-            return;
-        }
-#endif
-
-        callback->activeVideoRouteDidChange(WTF::move(target));
+        callback->activeVideoRouteDidChange(MediaPlaybackTargetCocoa::create());
     });
 }
 

Source/WebCore/platform/graphics/MediaPlayerPrivateWirelessPlayback.cpp
@@ -185,6 +185,11 @@ void MediaPlayerPrivateWirelessPlayback::setWirelessPlaybackTarget(Ref<MediaPlay
 
     ALWAYS_LOG(LOGIDENTIFIER, playbackTarget->type());
 
+    if (RefPtr route = this->route()) {
+        route->disconnectFromSession();
+        route->setClient(nullptr);
+    }
+
     m_playbackTarget = WTF::move(playbackTarget);
 
     if (!wirelessPlaybackTarget())

commit d8474d43be34f4568b8cfc5ce3895ff20ac8e1fe

REGRESSION (317433@main): Build failure: AVOutputDeviceTypeCustomProtocol is only available on macOS 27.0 or newer <https://bugs.webkit.org/show_bug.cgi?id=319732> <rdar://182570316>
Unreviewed build fix.

Fix the `-Wunguarded-availability-new` error introduced by 317433@main.
The AVRouting SDK annotates the `AVOutputDeviceType` enumerator
`AVOutputDeviceTypeCustomProtocol` as `SPI_AVAILABLE(macos(27.0),
ios(27.0), tvos(27.0), watchos(27.0), visionos(27.0))`, so referencing
it in `supportsCustomProtocolVideoPlayback()` fails to build for
deployment targets earlier than 27.0.

Suppress the availability warning rather than adding a runtime
`@available` guard.  The enumerator is a compile-time constant and
`-[AVOutputDevice deviceType]` has been available since macOS 10.12, so
no runtime API is absent on older systems.  On those systems, no output
device reports the custom-protocol type, so the loop already returns
false.

No new tests since this change is not directly testable.

* Source/WebCore/platform/graphics/avfoundation/MediaPlaybackTargetCocoa.mm:
(WebCore::MediaPlaybackTargetCocoa::supportsCustomProtocolVideoPlayback const):

Canonical link: https://commits.webkit.org/317450@main

Source/WebCore/platform/graphics/avfoundation/MediaPlaybackTargetCocoa.mm
@@ -81,8 +81,10 @@ bool MediaPlaybackTargetCocoa::hasActiveRoute() const
 bool MediaPlaybackTargetCocoa::supportsCustomProtocolVideoPlayback() const
 {
     for (AVOutputDevice *outputDevice in [m_outputContext outputDevices]) {
+ALLOW_NEW_API_WITHOUT_GUARDS_BEGIN
         if (outputDevice.deviceType == AVOutputDeviceTypeCustomProtocol && outputDevice.deviceFeatures & AVOutputDeviceFeatureVideo)
             return true;
+ALLOW_NEW_API_WITHOUT_GUARDS_END
     }
 
     return false;