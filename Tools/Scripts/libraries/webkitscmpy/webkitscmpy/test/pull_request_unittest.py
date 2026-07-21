b560bf8e54109b9e46d737bf1c4b0d90324f6401
Ryan Haddad <ryanhaddad@apple.com>
2026-07-21 12:10:33 -0700

    [webkitscmpy] Changes to tests shouldn't be considered "Gardening" https://bugs.webkit.org/show_bug.cgi?id=319847 rdar://182469134
    Reviewed by Aakash Jain.
    
    The "Gardening" commit class matched any change whose files were all located in LayoutTests,
    so a PR that fixed test code would be classified as "Gardening", resulting in git-webkit
    commenting as much in the associated bugzilla. Beyond potential confusion, it could also result
    in silently skipping bug assignment and reopening.
    
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/classify_unittest.py:
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/pull_request_unittest.py:
    * metadata/commit_classes.json: Narrow the Gardening paths so a gardening change may only
    touch test expectations and baseline/result files, never a test driver. A change that edits
    an actual test now falls through to the Tools class and is handled like any other pull request.
    Pure skip and rebaseline changes continue to be classified as Gardening.
    
    Canonical link: https://commits.webkit.org/317644@main

--- Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/pull_request_unittest.py
+++ Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/pull_request_unittest.py
@@ -1472,6 +1472,47 @@ No pre-PR checks to run""")
             ],
         )
 
+    def test_github_bugzilla_gardening_with_test_fix(self):
+        with OutputCapture(level=logging.INFO) as captured, mocks.remote.GitHub(projects=bmocks.PROJECTS) as remote, bmocks.Bugzilla(
+            self.BUGZILLA.split('://')[-1],
+            projects=bmocks.PROJECTS, issues=bmocks.ISSUES,
+            environment=Environment(
+                BUGS_EXAMPLE_COM_USERNAME='tcontributor@example.com',
+                BUGS_EXAMPLE_COM_PASSWORD='password',
+            )), patch(
+                'webkitbugspy.Tracker._trackers', [bugzilla.Tracker(self.BUGZILLA)],
+        ), mocks.local.Git(
+            self.path, remote='https://{}'.format(remote.remote),
+            remotes=dict(fork='https://{}/Contributor/WebKit'.format(remote.hosts[0])),
+        ) as repo, mocks.local.Svn():
+
+            repo.commits['eng/pr-branch'] = [
+                repo.commits[repo.default_branch][-1],
+                Commit(
+                    hash='06de5d56554e693db72313f4ca1fb969c30b8ccb',
+                    branch='eng/pr-branch',
+                    author=dict(name='Tim Contributor', emails=['tcontributor@example.com']),
+                    identifier="5.1@eng/pr-branch",
+                    timestamp=int(time.time()),
+                    message='[GARDENING] Existing commit\nbugs.example.com/show_bug.cgi?id=1'
+                )
+            ]
+            repo.head = repo.commits['eng/pr-branch'][-1]
+            self.assertEqual(0, program.main(
+                args=('pull-request', '-v', '--no-history'),
+                path=self.path,
+                classifier=CommitClassifier([CommitClassifier.CommitClass(
+                    name='Gardening',
+                    headers=[{'value': 'GARDENING', 'ratio': 85}],
+                    paths=['LayoutTests/TestExpectations', 'LayoutTests/.+-expected(-mismatch)?\\.'],
+                )]),
+            ))
+
+            self.assertEqual(
+                Tracker.instance().issue(1).comments[-1].content,
+                'Pull request: https://github.example.com/WebKit/WebKit/pull/1',
+            )
+
     def test_github_branch_secondary_redacted(self):
         with OutputCapture(level=logging.INFO) as captured, mocks.remote.GitHub(projects=bmocks.PROJECTS) as remote, bmocks.Bugzilla(
             self.BUGZILLA.split('://')[-1],
