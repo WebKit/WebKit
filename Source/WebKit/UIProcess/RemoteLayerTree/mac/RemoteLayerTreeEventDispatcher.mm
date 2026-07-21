049c752f490d12c4c758cba27029093984bf100c
Kiet Ho <kiet.ho@apple.com>
2026-07-21 16:38:15 -0700

    Cherry-pick d586a22ef92e. rdar://182773013
        RemoteLayerTreeEventDispatcher: add lock to guard access to m_momentumEventDispatcher
        https://bugs.webkit.org/show_bug.cgi?id=318487
        rdar://174662710
    
        Reviewed by Simon Fraser.
    
        m_momentumEventDispatcher can be accessed from two threads, which could lead to
        a race condition:
        * On the scrolling thread, RemoteLayerTreeEventDispatcher::didRefreshDisplay
          gets called. It grabs the pointer of m_momentumEventDispatcher to call into it
        * Just before m_momentumEventDispatcher->displayDidRefresh is called,
          the scrolling thread gets pre-empted for the main thread. It calls
          RemoteLayerTreeEventDispatcher::invalidate, which sets m_momentumEventDispatcher
          to nullptr, freeing the object
        * Then the scrolling thread resumes execution at
          RemoteLayerTreeEventDispatcher::didRefreshDisplay, which calls
          calls m_momentumEventDispatcher->displayDidRefresh on the freed object
    
        Note the race window is *very* small - didRefreshDisplay needs to be preempted
        at the exact instruction that calls m_momentumEventDispatcher->displayDidRefresh.
    
        263321@main tweaked the order of operations to avoid needing a lock and null
        check around m_momentumEventDispatcher:
    
        > Rather than adding a lock to use around accesses to m_momentumEventDispatcher
        > and adding null checks, fix the teardown order to ensure that m_momentumEventDispatcher
        > is always non-null in the scrolling thread code.
    
        But it's almost too easy to accidentally change the order of operations, so this
        patch properly fixes it by guarding m_momentumEventDispatcher with a lock. Any
        operations involving it needs to acquire the lock first.
    
        No tests - tiny race window makes it almost impossible to write a test that
        reliably triggers the race condition.
    
        * Source/WebKit/UIProcess/RemoteLayerTree/mac/RemoteLayerTreeEventDispatcher.h:
        * Source/WebKit/UIProcess/RemoteLayerTree/mac/RemoteLayerTreeEventDispatcher.mm:
        (WebKit::RemoteLayerTreeEventDispatcher::invalidate):
        (WebKit::RemoteLayerTreeEventDispatcher::cacheWheelEventScrollingAccelerationCurve):
        (WebKit::RemoteLayerTreeEventDispatcher::scrollingThreadHandleWheelEvent):
        (WebKit::RemoteLayerTreeEventDispatcher::didRefreshDisplay):
        (WebKit::RemoteLayerTreeEventDispatcher::windowScreenDidChange):
        (WebKit::RemoteLayerTreeEventDispatcher::stopDisplayDidRefreshCallbacks):
        (WebKit::RemoteLayerTreeEventDispatcher::flushMomentumEventLoggingSoon):
    
        Identifier: 305413.1103@safari-7624.5-branch
    
    Identifier: 316606.145@safari-7625-branch

--- Source/WebKit/UIProcess/RemoteLayerTree/mac/RemoteLayerTreeEventDispatcher.mm
+++ Source/WebKit/UIProcess/RemoteLayerTree/mac/RemoteLayerTreeEventDispatcher.mm
@@ -159,7 +159,10 @@ void RemoteLayerTreeEventDispatcher::invalidate()
     });
 
 #if ENABLE(MOMENTUM_EVENT_DISPATCHER)
-    m_momentumEventDispatcher = nullptr;
+    {
+        Locker locker { m_momentumEventDispatcherLock };
+        m_momentumEventDispatcher = nullptr;
+    }
 #endif
 
     m_displayLinkClient = nullptr;
@@ -212,7 +215,12 @@ void RemoteLayerTreeEventDispatcher::cacheWheelEventScrollingAccelerationCurve(c
         });
         return curve;
     });
-    m_momentumEventDispatcher->setScrollingAccelerationCurve(m_pageIdentifier, WTF::move(curve));
+
+    {
+        Locker locker { m_momentumEventDispatcherLock };
+        if (m_momentumEventDispatcher)
+            m_momentumEventDispatcher->setScrollingAccelerationCurve(m_pageIdentifier, WTF::move(curve));
+    }
 #endif
 }
 
@@ -275,10 +283,13 @@ void RemoteLayerTreeEventDispatcher::scrollingThreadHandleWheelEvent(const WebWh
     }
 
 #if ENABLE(MOMENTUM_EVENT_DISPATCHER)
-    if (m_momentumEventDispatcher->handleWheelEvent(m_pageIdentifier, webWheelEvent, rubberBandableEdges)) {
-        continueEventHandlingOnMainThread(WheelEventHandlingResult::handled(processingSteps));
-        [CATransaction flush];
-        return;
+    {
+        Locker locker { m_momentumEventDispatcherLock };
+        if (m_momentumEventDispatcher && m_momentumEventDispatcher->handleWheelEvent(m_pageIdentifier, webWheelEvent, rubberBandableEdges)) {
+            continueEventHandlingOnMainThread(WheelEventHandlingResult::handled(processingSteps));
+            [CATransaction flush];
+            return;
+        }
     }
 #endif
 
@@ -488,7 +499,9 @@ void RemoteLayerTreeEventDispatcher::didRefreshDisplay(PlatformDisplayID display
     {
         // Make sure the lock is held for the handleSyntheticWheelEvent callback.
         ScrollingTree::HitTestLocker locker { *scrollingTree };
-        m_momentumEventDispatcher->displayDidRefresh(displayID);
+        Locker momentumEventDispatcherLocker { m_momentumEventDispatcherLock };
+        if (m_momentumEventDispatcher)
+            m_momentumEventDispatcher->displayDidRefresh(displayID);
     }
 #endif
 
@@ -781,7 +794,11 @@ void RemoteLayerTreeEventDispatcher::windowScreenWillChange()
 void RemoteLayerTreeEventDispatcher::windowScreenDidChange(PlatformDisplayID displayID, std::optional<FramesPerSecond> nominalFramesPerSecond)
 {
 #if ENABLE(MOMENTUM_EVENT_DISPATCHER)
-    m_momentumEventDispatcher->pageScreenDidChange(m_pageIdentifier, displayID, nominalFramesPerSecond);
+    {
+        Locker locker { m_momentumEventDispatcherLock };
+        if (m_momentumEventDispatcher)
+            m_momentumEventDispatcher->pageScreenDidChange(m_pageIdentifier, displayID, nominalFramesPerSecond);
+    }
 #else
     UNUSED_PARAM(displayID);
     UNUSED_PARAM(nominalFramesPerSecond);
@@ -863,8 +880,7 @@ void RemoteLayerTreeEventDispatcher::stopDisplayDidRefreshCallbacks(PlatformDisp
 {
     ASSERT(m_momentumEventDispatcherNeedsDisplayLink);
     m_momentumEventDispatcherNeedsDisplayLink = false;
-    if (m_momentumEventDispatcher)
-        startOrStopDisplayLink();
+    startOrStopDisplayLink();
 }
 
 void RemoteLayerTreeEventDispatcher::didEndSyntheticMomentumScrolling()
@@ -879,7 +895,9 @@ void RemoteLayerTreeEventDispatcher::didEndSyntheticMomentumScrolling()
 void RemoteLayerTreeEventDispatcher::flushMomentumEventLoggingSoon()
 {
     RunLoop::currentSingleton().dispatchAfter(1_s, [protectedThis = Ref { *this }] {
-        protectedThis->m_momentumEventDispatcher->flushLog();
+        Lock locker { protectedThis->m_momentumEventDispatcherLock };
+        if (protectedThis->m_momentumEventDispatcher)
+            protectedThis->m_momentumEventDispatcher->flushLog();
     });
 }
 #endif // ENABLE(MOMENTUM_EVENT_DISPATCHER_TEMPORARY_LOGGING)
