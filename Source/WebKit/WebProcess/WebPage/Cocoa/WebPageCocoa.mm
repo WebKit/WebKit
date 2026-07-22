ceee5e9070b100c496086afc6da497fe5d60235e
Abrar Rahman Protyasha <a_protyasha@apple.com>
2026-07-22 16:46:30 -0700

    [AppKit Gestures] Bring up support for DOM dblclick and smart magnification https://bugs.webkit.org/show_bug.cgi?id=319832 rdar://177010342
    Reviewed by Richard Robinson and Wenson Hsieh.
    
    This patch reworks WKAppKitGestureController's click handling so that
    a double click either produces a DOM dblclick or smart magnification
    depending on the content under the interaciton.
    
    Some salient parts to this change:
    
    - Move the start of a potential click (in the two-phase click pipeline)
      at pressDown instead of the single click's .began action. This
      addresses latency concerns that could drop the click on a plain single
      click (now that the double click recognizer's failure requirements are
      active).
    
    - Add a non-blocking double-click recognizer and a doubleClickForDouble-
      click recognizer and drive their enablement by deliberating about the
      web content under some interaction.
    
    - Wire the new recognizers in the gesture controller (simultaneity,
      canPrevent, and a listener-gated single-click failure requirement)
      and, in the web content process, feed the decision by hit-testing the
      node under the press point when there is no click responder.
    
    * Source/WebKit/UIProcess/mac/WKAppKitGestureController.h:
    * Source/WebKit/UIProcess/mac/WKAppKitGestureController.mm:
    (-[WKAppKitGestureController singleClickGestureRecognizer]):
    (-[WKAppKitGestureController setSingleClickGestureRecognizer:]):
    (-[WKAppKitGestureController setUpGestureRecognizers]):
    (-[WKAppKitGestureController setUpDoubleClickForDoubleClickGestureRecognizer]):
    (-[WKAppKitGestureController setUpNonBlockingDoubleClickGestureRecognizer]):
    (-[WKAppKitGestureController addGesturesToWebView]):
    (-[WKAppKitGestureController enableGesturesIfNeeded]):
    (-[WKAppKitGestureController ensureGesturesAreNotArchived]):
    (-[WKAppKitGestureController panGestureRecognized:]):
    (-[WKAppKitGestureController singleClickGestureRecognized:]):
    (-[WKAppKitGestureController doubleClickGestureRecognized:]):
    (-[WKAppKitGestureController doubleClickForDoubleClickGestureRecognized:]):
    (-[WKAppKitGestureController nonBlockingDoubleClickGestureRecognized:]):
    (-[WKAppKitGestureController _doubleClickForDoubleClickShouldBeginAtLocation:]):
    (-[WKAppKitGestureController _doubleClickForZoomShouldBeginAtLocation:]):
    (-[WKAppKitGestureController handleClickBegan:]):
    (-[WKAppKitGestureController _resetClickGestureRecognizersForNextClick]):
    (-[WKAppKitGestureController _handleClickEnded:]):
    (-[WKAppKitGestureController _setDoubleClickGesturesEnabled:]):
    (-[WKAppKitGestureController _updateDoubleClickGestureRecognizerEnablement]):
    (-[WKAppKitGestureController wouldSignificantlyZoomForRenderRect:]):
    (-[WKAppKitGestureController handleSmartMagnificationInformationForPotentialClick:renderRect:fitEntireRect:viewportMinimumScale:viewportMaximumScale:nodeIsRootLevel:nodeIsPluginElement:]):
    (-[WKAppKitGestureController didNotHandleClickAsClick:]):
    (-[WKAppKitGestureController gestureRecognizer:shouldRecognizeSimultaneouslyWithGestureRecognizer:]):
    (-[WKAppKitGestureController gestureRecognizer:shouldRequireFailureOfGestureRecognizer:]):
    (-[WKAppKitGestureController gestureRecognizerShouldBegin:]):
    (-[WKAppKitGestureController _gestureRecognizer:canPreventGestureRecognizer:]):
    (-[WKAppKitGestureController setUpSingleClickGestureRecognizer]): Deleted.
    (-[WKAppKitGestureController _handleClickBegan:]): Deleted.
    * Source/WebKit/UIProcess/mac/WKAppKitGestureController.swift:
    (WKClickPressGestureRecognizer.controller):
    (WKAppKitGestureController.setUpSingleClickGestureRecognizer):
    * Source/WebKit/WebProcess/WebPage/Cocoa/WebPageCocoa.mm:
    (WebKit::WebPage::potentialTapAtPosition):
    (WebKit::WebPage::commitPotentialTap):
    * Tools/TestWebKitAPI/Tests/WebKit/WebPage/AppKit Gesture Tests/EmbeddedAppKitGesturesTests.swift:
    (AppKitGesturesTests.doubleClickingSelectsWordWhenScrolledToTopWithObscuredContentInset):
    
    Canonical link: https://commits.webkit.org/317742@main

--- Source/WebKit/WebProcess/WebPage/Cocoa/WebPageCocoa.mm
+++ Source/WebKit/WebProcess/WebPage/Cocoa/WebPageCocoa.mm
@@ -3152,28 +3152,35 @@ Awaitable<std::optional<WebCore::RemoteUserInputEventData>> WebPage::potentialTa
 
     m_wasShowingInputViewForFocusedElementDuringLastPotentialTap = m_isShowingInputViewForFocusedElement;
 
-    RefPtr viewGestureGeometryCollector = m_viewGestureGeometryCollector;
+    sendTapHighlightForNodeIfNecessary(requestID, m_potentialTapNode.get(), position);
+#endif // PLATFORM(IOS_FAMILY)
 
-    if (shouldRequestMagnificationInformation && m_potentialTapNode && viewGestureGeometryCollector) {
+#if ENABLE(TWO_PHASE_CLICKS)
+    RefPtr<WebCore::Node> magnificationNode = m_potentialTapNode;
+    if (!magnificationNode && localMainFrame) {
+        if (RefPtr frameView = localMainFrame->view()) {
+            constexpr OptionSet hitType { HitTestRequest::Type::ReadOnly, HitTestRequest::Type::DisallowUserAgentShadowContent, HitTestRequest::Type::AllowVisibleChildFrameContentOnly };
+            auto result = localMainFrame->eventHandler().hitTestResultAtPoint(frameView->windowToContents(roundedIntPoint(position)), hitType);
+            magnificationNode = result.innerNode();
+        }
+    }
+    if (RefPtr viewGestureGeometryCollector = m_viewGestureGeometryCollector; shouldRequestMagnificationInformation && magnificationNode && viewGestureGeometryCollector) {
         FloatPoint origin = position;
         FloatRect absoluteBoundingRect;
         bool fitEntireRect;
         double viewportMinimumScale;
         double viewportMaximumScale;
 
-        viewGestureGeometryCollector->computeZoomInformationForNode(*protect(m_potentialTapNode), origin, absoluteBoundingRect, fitEntireRect, viewportMinimumScale, viewportMaximumScale);
+        viewGestureGeometryCollector->computeZoomInformationForNode(*magnificationNode, origin, absoluteBoundingRect, fitEntireRect, viewportMinimumScale, viewportMaximumScale);
 
-        bool nodeIsRootLevel = is<WebCore::Document>(*m_potentialTapNode) || is<WebCore::HTMLBodyElement>(*m_potentialTapNode);
-        bool nodeIsPluginElement = is<WebCore::HTMLPlugInElement>(*m_potentialTapNode);
+        bool nodeIsRootLevel = is<WebCore::Document>(*magnificationNode) || is<WebCore::HTMLBodyElement>(*magnificationNode);
+        bool nodeIsPluginElement = is<WebCore::HTMLPlugInElement>(*magnificationNode);
         send(Messages::WebPageProxy::HandleSmartMagnificationInformationForPotentialTap(requestID, absoluteBoundingRect, fitEntireRect, viewportMinimumScale, viewportMaximumScale, nodeIsRootLevel, nodeIsPluginElement));
     }
 
-    sendTapHighlightForNodeIfNecessary(requestID, m_potentialTapNode.get(), position);
-#if ENABLE(TWO_PHASE_CLICKS)
     if (RefPtr potentialTapNode = m_potentialTapNode; potentialTapNode && !potentialTapNode->allowsDoubleTapGesture())
         send(Messages::WebPageProxy::DisableDoubleTapGesturesDuringTapIfNecessary(requestID));
-#endif
-#endif // PLATFORM(IOS_FAMILY)
+#endif // ENABLE(TWO_PHASE_CLICKS)
 
     co_return std::nullopt;
 }
@@ -3212,23 +3219,19 @@ Awaitable<std::optional<WebCore::FrameIdentifier>> WebPage::commitPotentialTap(s
     };
 
     if (invalidTargetForSingleClick) {
-#if PLATFORM(IOS_FAMILY)
         if (localRootFrame) {
             constexpr OptionSet hitType { HitTestRequest::Type::ReadOnly, HitTestRequest::Type::Active, HitTestRequest::Type::AllowVisibleChildFrameContentOnly };
             auto roundedPoint = IntPoint { m_potentialTapLocation };
             auto result = localRootFrame->eventHandler().hitTestResultAtPoint(roundedPoint, hitType);
             localRootFrame->eventHandler().setLastTouchedNode(protect(result.innerNode()));
         }
-#endif
 
         reportFailedTap();
         co_return std::nullopt;
     }
 
-#if PLATFORM(IOS_FAMILY)
     if (localRootFrame)
         localRootFrame->eventHandler().setLastTouchedNode(nullptr);
-#endif
 
     FloatPoint adjustedPoint;
     RefPtr nodeRespondingToClick = localRootFrame ? localRootFrame->nodeRespondingToClickEvents(m_potentialTapLocation, adjustedPoint, m_potentialTapSecurityOrigin.get()) : nullptr;
