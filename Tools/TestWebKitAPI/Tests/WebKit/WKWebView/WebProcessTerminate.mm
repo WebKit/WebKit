WebKit/WebKit

commit 9935051dcd8ebb29a4eaf54129ef77ecf9c50aea

Crash under WebProcessPool::pageBeginUsingWebsiteDataStore() https://bugs.webkit.org/show_bug.cgi?id=222574 <rdar://68340471>
Reviewed by Geoffrey Garen.

Source/WebKit:

The issue was that when WebProcessProxy::requestTermination() was called (e.g. process is killed by WebKit
for using too much memory), we would fail to remove the process from the WebProcessCache. Because the
terminated would stay in the cache (even though WebProcessProxy::shutDown() was called), we could potentially
try and use it later on for a navigation to the same domain. This would lead to crashes because
WebProcessProxy::shutDown() has already been called.

Note that we were previously correctly removing the process from the cache in case of a proper crash, inside
WebProcessProxy::processDidTerminateOrFailedToLaunch(). I have moved the logic to remove from the cache
from processDidTerminateOrFailedToLaunch() to shutDown() to avoid similar issues in the future.

* UIProcess/API/Cocoa/WKProcessPool.mm:
(-[WKProcessPool _requestWebProcessTermination:]):
* UIProcess/API/Cocoa/WKProcessPoolPrivate.h:
* UIProcess/WebProcessProxy.cpp:
(WebKit::WebProcessProxy::shutDown):
(WebKit::WebProcessProxy::processDidTerminateOrFailedToLaunch):

Tools:

Add API test coverage.

* TestWebKitAPI/Tests/WebKitCocoa/ProcessSwapOnNavigation.mm:


Canonical link: https://commits.webkit.org/234716@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@273695 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/API/Cocoa/WKProcessPool.mm
@@ -387,6 +387,16 @@ static RetainPtr<WKProcessPool>& sharedProcessPool()
 #endif
 }
 
+- (BOOL)_requestWebProcessTermination:(pid_t)pid
+{
+    for (auto& process : _processPool->processes()) {
+        if (process->processIdentifier() == pid)
+            process->requestTermination(WebKit::ProcessTerminationReason::RequestedByClient);
+        return YES;
+    }
+    return NO;
+}
+
 - (void)_makeNextWebProcessLaunchFailForTesting
 {
     _processPool->setShouldMakeNextWebProcessLaunchFailForTesting(true);

Source/WebKit/UIProcess/API/Cocoa/WKProcessPoolPrivate.h
@@ -110,6 +110,7 @@
 - (void)_clearWebProcessCache WK_API_AVAILABLE(macos(10.15.4), ios(13.4));
 - (void)_setUseSeparateServiceWorkerProcess:(BOOL)forceServiceWorkerProcess WK_API_AVAILABLE(macos(10.15.4), ios(13.4));
 - (pid_t)_gpuProcessIdentifier WK_API_AVAILABLE(macos(WK_MAC_TBA), ios(WK_IOS_TBA));
+- (BOOL)_requestWebProcessTermination:(pid_t)pid WK_API_AVAILABLE(macos(WK_MAC_TBA), ios(WK_IOS_TBA));
 
 // Test only. Returns web processes running web pages (does not include web processes running service workers)
 - (size_t)_webPageContentProcessCount WK_API_AVAILABLE(macos(10.13.4), ios(11.3));

Source/WebKit/UIProcess/WebProcessProxy.cpp
@@ -448,6 +448,11 @@ void WebProcessProxy::shutDown()
 {
     RELEASE_ASSERT(isMainThreadOrCheckDisabled());
 
+    if (m_isInProcessCache) {
+        processPool().webProcessCache().removeProcess(*this, WebProcessCache::ShouldShutDownProcess::No);
+        ASSERT(!m_isInProcessCache);
+    }
+
     shutDownProcess();
 
     if (m_webConnection) {
@@ -866,11 +871,6 @@ void WebProcessProxy::processDidTerminateOrFailedToLaunch()
     auto isResponsiveCallbacks = WTFMove(m_isResponsiveCallbacks);
     for (auto& callback : isResponsiveCallbacks)
         callback(false);
-
-    if (m_isInProcessCache) {
-        processPool().webProcessCache().removeProcess(*this, WebProcessCache::ShouldShutDownProcess::No);
-        ASSERT(!m_isInProcessCache);
-    }
 
     if (isStandaloneServiceWorkerProcess())
         processPool().serviceWorkerProcessCrashed(*this);

Tools/TestWebKitAPI/Tests/WebKitCocoa/ProcessSwapOnNavigation.mm
@@ -3690,6 +3690,104 @@ TEST(ProcessSwap, UseWebProcessCacheAfterTermination)
     EXPECT_EQ(1U, [processPool _webProcessCountIgnoringPrewarmed]);
 }
 
+TEST(ProcessSwap, ProcessCrashedWhileInTheCache)
+{
+    auto processPoolConfiguration = psonProcessPoolConfiguration();
+    auto processPool = adoptNS([[WKProcessPool alloc] _initWithConfiguration:processPoolConfiguration.get()]);
+
+    auto webViewConfiguration = adoptNS([[WKWebViewConfiguration alloc] init]);
+    [webViewConfiguration setProcessPool:processPool.get()];
+    auto handler = adoptNS([[PSONScheme alloc] init]);
+    [webViewConfiguration setURLSchemeHandler:handler.get() forURLScheme:@"PSON"];
+
+    auto navigationDelegate = adoptNS([[TestNavigationDelegate alloc] init]);
+    [navigationDelegate setDidFinishNavigation:^(WKWebView *, WKNavigation *) {
+        done = true;
+    }];
+
+    int webkitPID = 0;
+
+    @autoreleasepool {
+        auto webView = adoptNS([[WKWebView alloc] initWithFrame:NSMakeRect(0, 0, 800, 600) configuration:webViewConfiguration.get()]);
+        [webView setNavigationDelegate:navigationDelegate.get()];
+
+        NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"pson://www.webkit.org/main.html"]];
+
+        [webView loadRequest:request];
+        TestWebKitAPI::Util::run(&done);
+        done = false;
+        webkitPID = [webView _webProcessIdentifier];
+    }
+
+    while ([processPool _processCacheSize] != 1)
+        TestWebKitAPI::Util::sleep(0.1);
+
+    kill(webkitPID, 9);
+
+    while ([processPool _processCacheSize])
+        TestWebKitAPI::Util::sleep(0.1);
+
+    auto webView = adoptNS([[WKWebView alloc] initWithFrame:NSMakeRect(0, 0, 800, 600) configuration:webViewConfiguration.get()]);
+    [webView setNavigationDelegate:navigationDelegate.get()];
+
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"pson://www.webkit.org/main.html"]];
+
+    [webView loadRequest:request];
+    TestWebKitAPI::Util::run(&done);
+    done = false;
+
+    EXPECT_NE(webkitPID, [webView _webProcessIdentifier]);
+}
+
+TEST(ProcessSwap, ProcessTerminatedWhileInTheCache)
+{
+    auto processPoolConfiguration = psonProcessPoolConfiguration();
+    auto processPool = adoptNS([[WKProcessPool alloc] _initWithConfiguration:processPoolConfiguration.get()]);
+
+    auto webViewConfiguration = adoptNS([[WKWebViewConfiguration alloc] init]);
+    [webViewConfiguration setProcessPool:processPool.get()];
+    auto handler = adoptNS([[PSONScheme alloc] init]);
+    [webViewConfiguration setURLSchemeHandler:handler.get() forURLScheme:@"PSON"];
+
+    auto navigationDelegate = adoptNS([[TestNavigationDelegate alloc] init]);
+    [navigationDelegate setDidFinishNavigation:^(WKWebView *, WKNavigation *) {
+        done = true;
+    }];
+
+    int webkitPID = 0;
+
+    @autoreleasepool {
+        auto webView = adoptNS([[WKWebView alloc] initWithFrame:NSMakeRect(0, 0, 800, 600) configuration:webViewConfiguration.get()]);
+        [webView setNavigationDelegate:navigationDelegate.get()];
+
+        NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"pson://www.webkit.org/main.html"]];
+
+        [webView loadRequest:request];
+        TestWebKitAPI::Util::run(&done);
+        done = false;
+        webkitPID = [webView _webProcessIdentifier];
+    }
+
+    while ([processPool _processCacheSize] != 1)
+        TestWebKitAPI::Util::sleep(0.1);
+
+    EXPECT_TRUE([processPool _requestWebProcessTermination:webkitPID]);
+    TestWebKitAPI::Util::spinRunLoop(100);
+
+    EXPECT_EQ(0U, [processPool _processCacheSize]);
+
+    auto webView = adoptNS([[WKWebView alloc] initWithFrame:NSMakeRect(0, 0, 800, 600) configuration:webViewConfiguration.get()]);
+    [webView setNavigationDelegate:navigationDelegate.get()];
+
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"pson://www.webkit.org/main.html"]];
+
+    [webView loadRequest:request];
+    TestWebKitAPI::Util::run(&done);
+    done = false;
+
+    EXPECT_NE(webkitPID, [webView _webProcessIdentifier]);
+}
+
 TEST(ProcessSwap, UseWebProcessCacheForLoadInNewView)
 {
     auto processPoolConfiguration = psonProcessPoolConfiguration();

commit 00bfe4a98d52a1792e3f82e01c079f782a560295

[WKProcessPool _requestWebProcessTermination:] only terminates the first web process in the pool https://bugs.webkit.org/show_bug.cgi?id=319973
Reviewed by Per Arne Vollan.

[WKProcessPool _requestWebProcessTermination:] walks the pool's web content
processes looking for the one whose PID matches the argument, but a misplaced
`return YES;` sat inside the loop and outside the `if` that tests for a match.
As a result the method only ever examined the *first* process in the list: it
terminated that process if its PID matched, did nothing for any other PID, and
in every case unconditionally returned YES after the first iteration -- which
also made the trailing `return NO` (the "no such process" path) dead code.
Requesting termination of any process that was not first in the pool's list
therefore silently failed while still reporting success.

The pool stores its processes in creation order, so any process created after
the first can never be terminated through this SPI. This has been broken since
the method was introduced in 234716@main.

Move the `return YES;` inside the matching branch so the loop keeps scanning
until it finds the process with the requested PID, terminates it, and returns
YES, and so the method returns NO when no process matches.

Test: Tools/TestWebKitAPI/Tests/WebKit/WKWebView/WebProcessTerminate.mm

* Source/WebKit/UIProcess/API/Cocoa/WKProcessPool.mm:
(-[WKProcessPool _requestWebProcessTermination:]):
* Tools/TestWebKitAPI/Tests/WebKit/WKWebView/WebProcessTerminate.mm:
(-[WebProcessTerminationSchemeHandler webView:startURLSchemeTask:]):
(-[WebProcessTerminationSchemeHandler webView:stopURLSchemeTask:]):
(TEST(WebKit, RequestWebProcessTerminationForNonFirstProcess)):

Canonical link: https://commits.webkit.org/317756@main

Source/WebKit/UIProcess/API/Cocoa/WKProcessPool.mm
@@ -437,9 +437,10 @@ ALLOW_DEPRECATED_IMPLEMENTATIONS_END
 - (BOOL)_requestWebProcessTermination:(pid_t)pid
 {
     for (Ref process : borrow(_processPool->processes()).get()) {
-        if (process->processID() == pid)
+        if (process->processID() == pid) {
             process->requestTermination(WebKit::ProcessTerminationReason::RequestedByClient);
-        return YES;
+            return YES;
+        }
     }
     return NO;
 }

Tools/TestWebKitAPI/Tests/WebKit/WKWebView/WebProcessTerminate.mm
@@ -32,6 +32,7 @@
 #import <WebKit/WKProcessPoolPrivate.h>
 #import <WebKit/WKWebViewPrivate.h>
 #import <WebKit/WebKit.h>
+#import <WebKit/_WKProcessPoolConfiguration.h>
 #import <wtf/RetainPtr.h>
 
 TEST(WebKit, WebProcessTerminate)
@@ -48,6 +49,83 @@ TEST(WebKit, WebProcessTerminate)
 
     auto pid2 = [webView _webProcessIdentifier];
     EXPECT_TRUE(pid != pid2);
+}
+
+@interface WebProcessTerminationSchemeHandler : NSObject <WKURLSchemeHandler>
+@end
+
+@implementation WebProcessTerminationSchemeHandler
+- (void)webView:(WKWebView *)webView startURLSchemeTask:(id<WKURLSchemeTask>)task
+{
+    RetainPtr data = [@"<html><body>hello</body></html>" dataUsingEncoding:NSUTF8StringEncoding];
+    RetainPtr response = adoptNS([[NSURLResponse alloc] initWithURL:task.request.URL MIMEType:@"text/html" expectedContentLength:[data length] textEncodingName:nil]);
+    [task didReceiveResponse:response.get()];
+    [task didReceiveData:data.get()];
+    [task didFinish];
+}
+
+- (void)webView:(WKWebView *)webView stopURLSchemeTask:(id<WKURLSchemeTask>)task
+{
+}
+@end
+
+// The pool keeps its web content processes in creation order, so a process created later is never
+// first in the list. -[WKProcessPool _requestWebProcessTermination:] must still terminate it.
+TEST(WebKit, RequestWebProcessTerminationForNonFirstProcess)
+{
+    RetainPtr processPoolConfiguration = adoptNS([[_WKProcessPoolConfiguration alloc] init]);
+    processPoolConfiguration.get().processSwapsOnNavigation = YES;
+    processPoolConfiguration.get().processSwapsOnNavigationWithinSameNonHTTPFamilyProtocol = YES;
+    RetainPtr processPool = adoptNS([[WKProcessPool alloc] _initWithConfiguration:processPoolConfiguration.get()]);
+
+    RetainPtr handler = adoptNS([[WebProcessTerminationSchemeHandler alloc] init]);
+    RetainPtr configuration = adoptNS([[WKWebViewConfiguration alloc] init]);
+    [configuration setProcessPool:processPool.get()];
+    [configuration setURLSchemeHandler:handler.get() forURLScheme:@"termtest"];
+
+    RetainPtr navigationDelegate = adoptNS([[TestNavigationDelegate alloc] init]);
+    __block bool didFinishNavigation = false;
+    [navigationDelegate setDidFinishNavigation:^(WKWebView *, WKNavigation *) {
+        didFinishNavigation = true;
+    }];
+
+    // First web view -> first web content process in the pool's list.
+    RetainPtr firstWebView = adoptNS([[WKWebView alloc] initWithFrame:NSMakeRect(0, 0, 800, 600) configuration:configuration.get()]);
+    [firstWebView setNavigationDelegate:navigationDelegate.get()];
+    [firstWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"termtest://first.example/"]]];
+    TestWebKitAPI::Util::run(&didFinishNavigation);
+    didFinishNavigation = false;
+    auto firstPID = [firstWebView _webProcessIdentifier];
+
+    // Second web view (different site) -> a second, non-first web content process.
+    RetainPtr secondWebView = adoptNS([[WKWebView alloc] initWithFrame:NSMakeRect(0, 0, 800, 600) configuration:configuration.get()]);
+    [secondWebView setNavigationDelegate:navigationDelegate.get()];
+    [secondWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"termtest://second.example/"]]];
+    TestWebKitAPI::Util::run(&didFinishNavigation);
+    didFinishNavigation = false;
+    auto secondPID = [secondWebView _webProcessIdentifier];
+
+    // Sanity: two distinct web content processes exist, and the second was created after the first.
+    EXPECT_NE(firstPID, secondPID);
+    EXPECT_EQ(2U, [processPool _webProcessCountIgnoringPrewarmed]);
+
+    WKWebView *secondWebViewToMatch = secondWebView.get();
+    __block bool secondWebViewProcessTerminated = false;
+    [navigationDelegate setWebContentProcessDidTerminate:^(WKWebView *webView, _WKProcessTerminationReason) {
+        if (webView == secondWebViewToMatch)
+            secondWebViewProcessTerminated = true;
+    }];
+
+    EXPECT_TRUE([processPool _requestWebProcessTermination:secondPID]);
+
+    // The (non-first) second process must actually be terminated within a bounded time. With the bug,
+    // only the first process is examined, so this never happens and the wait times out.
+    TestWebKitAPI::Util::runFor(&secondWebViewProcessTerminated, 5_s);
+    EXPECT_TRUE(secondWebViewProcessTerminated);
+    EXPECT_EQ(0, [secondWebView _webProcessIdentifier]);
+
+    // The first process must be left running.
+    EXPECT_EQ(firstPID, [firstWebView _webProcessIdentifier]);
 }
 
 TEST(WebKit, TerminateAllProcessesDuringLaunch)