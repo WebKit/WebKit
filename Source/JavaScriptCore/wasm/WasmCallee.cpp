4bfd6c750726a25617c844de7be79c4400f3c088
Sergey Rubanov <chi187@gmail.com>
2026-07-23 12:43:55 -0700

    [JSC][Wasm] Fix OMG inlined stack origin lookup skipping deeper frames https://bugs.webkit.org/show_bug.cgi?id=270832
    Reviewed by Yusuke Suzuki.
    
    getCodeOrigin used a broken lower_bound comparator on lastInlineCSI
    (integer subtraction coerced to bool), so it could start mid-list and
    skip deeper postorder origins that still contain the call site.
    Error.stack then dropped nested inlined frames (seen as a flaky wasm
    stress failure on x86/EWS). Use a proper lower_bound for the first
    origin with lastInlineCSI >= csi, then walk only that suffix matching
    firstInlineCSI and depth.
    
    Add a focused OMG-inlining stress test for nested catch/throw stacks.
    
    * Source/JavaScriptCore/wasm/WasmCallee.cpp:
    (JSC::Wasm::OptimizingJITCallee::getCodeOrigin):
    * JSTests/wasm/stress/nested-inline-stacktrace-omg.js: Added.
    
    Canonical link: https://commits.webkit.org/317800@main

--- Source/JavaScriptCore/wasm/WasmCallee.cpp
+++ Source/JavaScriptCore/wasm/WasmCallee.cpp
@@ -443,19 +443,18 @@ void OptimizingJITCallee::addCodeOrigin(unsigned firstInlineCSI, unsigned lastIn
 const WasmCodeOrigin* OptimizingJITCallee::getCodeOrigin(unsigned csi, unsigned depth, bool& isInlined) const
 {
     isInlined = false;
-    auto iter = std::lower_bound(codeOrigins.begin(), codeOrigins.end(), WasmCodeOrigin { 0, csi, 0, 0 }, [&](const auto& a, const auto& b) {
-        return b.lastInlineCSI - a.lastInlineCSI;
+    auto iter = std::lower_bound(codeOrigins.begin(), codeOrigins.end(), csi, [](const WasmCodeOrigin& origin, unsigned value) {
+        return origin.lastInlineCSI < value;
     });
-    if (!iter || iter == codeOrigins.end())
-        iter = codeOrigins.begin();
-    while (iter != codeOrigins.end()) {
-        if (iter->firstInlineCSI <= csi && iter->lastInlineCSI >= csi && !(depth--)) {
+    for (; iter != codeOrigins.end(); ++iter) {
+        if (iter->firstInlineCSI > csi)
+            continue;
+        if (!depth) {
             isInlined = true;
             return iter;
         }
-        ++iter;
+        --depth;
     }
-
     return nullptr;
 }
 
