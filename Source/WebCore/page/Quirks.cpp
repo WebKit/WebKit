216217f5e2217211e3fdb6da5af6c96954ec8f59
Jean-Yves Avenard <jya@apple.com>
2026-07-22 22:42:57 -0700

    Cherry-pick 0aac2890d97a. rdar://182972132
        open.spotify.com: returning to Home auto-plays multiple song previews / overlapping audio in Safari
        https://bugs.webkit.org/show_bug.cgi?id=319271
        rdar://179910591
    
        Reviewed by Jer Noble.
    
        Spotify start several audio elements at once when navigating within the page
        (e.g. returning to a landing view). Each play() is authorized by the transient
        user activation produced by the navigation click: that activation is
        document-scoped and stays valid for several seconds, so every play() issued
        during the window is permitted no matter how many elements are involved. The
        result is several tracks playing on top of each other.
    
        Add a site quirk, ShouldBlockAudiblePlaybackWhileAudioIsPlaying, that denies an
        audible element from starting while another element in the same document is
        already playing audio, unless a user gesture is being directly processed (so
        deliberately choosing a track is unaffected). Enable it for open.spotify.com.
    
        To distinguish a directly-processed gesture from transient activation, add
        Document::mediaUserGestureReason(), reporting which branch of
        processingUserGestureForMedia() authorized playback; the latter is refactored to
        use it and is otherwise unchanged.
    
        * Source/WebCore/dom/Document.cpp:
        (WebCore::Document::processingUserGestureForMedia const):
        (WebCore::Document::mediaUserGestureReason const):
        * Source/WebCore/dom/Document.h:
        * Source/WebCore/html/MediaElementSession.cpp:
        (WebCore::mediaGestureReasonString):
        (WebCore::MediaElementSession::playbackStateChangePermitted const):
        * Source/WebCore/page/Quirks.cpp:
        (WebCore::Quirks::shouldBlockAudiblePlaybackWhileAudioIsPlaying const):
        (WebCore::handleSpotifyQuirks):
        * Source/WebCore/page/Quirks.h:
        * Source/WebCore/page/QuirksData.h:
    
        Canonical link: https://commits.webkit.org/317159@main
    
    Identifier: 316606.215@safari-7625-branch

--- Source/WebCore/page/Quirks.cpp
+++ Source/WebCore/page/Quirks.cpp
@@ -2452,6 +2452,14 @@ bool Quirks::shouldSuppressHLSSubtitles() const
     return m_quirksData.quirkIsEnabled(QuirksData::SiteSpecificQuirk::ShouldSuppressHLSSubtitles);
 }
 
+// spotify.com: block additive audible playback (e.g. Home-page track previews) while another
+// audible media element is already playing in the document.
+bool Quirks::shouldBlockAudiblePlaybackWhileAudioIsPlaying() const
+{
+    QUIRKS_EARLY_RETURN_IF_DISABLED_WITH_VALUE(false);
+    return m_quirksData.quirkIsEnabled(QuirksData::SiteSpecificQuirk::ShouldBlockAudiblePlaybackWhileAudioIsPlaying);
+}
+
 bool Quirks::shouldSuppressMediaSessionPauseActionOnInterruption() const
 {
     QUIRKS_EARLY_RETURN_IF_DISABLED_WITH_VALUE(false);
@@ -3873,6 +3881,7 @@ static void handleSpotifyQuirks(QuirksData& quirksData, const URL& quirksURL, co
         QuirksData::SiteSpecificQuirk::ShouldLimitHLSPlaybackRate,
         QuirksData::SiteSpecificQuirk::NeedsWebKitMediaTextTrackDisplayQuirk,
         QuirksData::SiteSpecificQuirk::ShouldDeferIntersectionObserversDuringResize,
+        QuirksData::SiteSpecificQuirk::ShouldBlockAudiblePlaybackWhileAudioIsPlaying,
     });
 }
 
