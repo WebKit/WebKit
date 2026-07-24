WebKit/WebKit

commit c51ab53c5b09430364f2c2b278e88650aa327a8c

Avoid loading AVFoundation to check supported MIME types if possible https://bugs.webkit.org/show_bug.cgi?id=185839 <rdar://problem/40182010>
Reviewed by Jer Noble.
Source/WebCore:

Avoid loading AVFoundation to call +[AVURLAssetClass audiovisualMIMETypes] as long as possible,
and when they are loaded send the list to the UI process so it can pass it to all extant
and all new web processes so they can won't have call it at all.

* WebCore.xcodeproj/project.pbxproj:
* platform/graphics/ImageDecoder.cpp:
(WebCore::ImageDecoder::create): Don't call ImageDecoderAVFObjC::canDecodeType if
ImageDecoderCG can decode the type so we don't have to load AVFoundation.
(WebCore::ImageDecoder::supportsMediaType): Return as soon as a decoder class says
it supports a media type to avoid calling more than one. Call ImageDecoderAVFObjC last.

* platform/graphics/avfoundation/objc/AVFoundationMIMETypeCache.h:
(WebCore::AVFoundationMIMETypeCache::setCacheMIMETypesCallback):
* platform/graphics/avfoundation/objc/AVFoundationMIMETypeCache.mm:
(WebCore::AVFoundationMIMETypeCache::singleton): Simplify.
(WebCore::AVFoundationMIMETypeCache::setSupportedTypes): Cache the supplied list of types
so we won't have to load AVFoundation when asked for types later.
(WebCore::AVFoundationMIMETypeCache::types):
(WebCore::AVFoundationMIMETypeCache::supportsContentType): New convenience routine.
(WebCore::AVFoundationMIMETypeCache::canDecodeType): Ditto.
(WebCore::AVFoundationMIMETypeCache::isAvailable const): New, check to see if AVFoundation.framework
is available without actually loading it.
(WebCore::AVFoundationMIMETypeCache::loadMIMETypes): Load types if possible.
(WebCore::AVFoundationMIMETypeCache::AVFoundationMIMETypeCache): Deleted.
(WebCore::AVFoundationMIMETypeCache::loadTypes): Deleted.

* platform/graphics/avfoundation/objc/ImageDecoderAVFObjC.mm:
(WebCore::ImageDecoderAVFObjC::create): Use AVFoundationMIMETypeCache::isAvailable instead
of loading the frameworks.

Source/WebKit/UIProcess/Cocoa/WebProcessProxyCocoa.mm
@@ -31,6 +31,8 @@
 #import "WKBrowsingContextControllerInternal.h"
 #import "WKBrowsingContextHandleInternal.h"
 #import "WKTypeRefWrapper.h"
+#import "WebProcessMessages.h"
+#import "WebProcessPool.h"
 #import <sys/sysctl.h>
 #import <wtf/NeverDestroyed.h>
 
@@ -135,6 +137,39 @@ bool WebProcessProxy::platformIsBeingDebugged() const
         return false;
 
     return info.kp_proc.p_flag & P_TRACED;
+}
+
+static Vector<String>& mediaTypeCache()
+{
+    ASSERT(RunLoop::isMain());
+    static NeverDestroyed<Vector<String>> typeCache;
+    return typeCache;
+}
+
+void WebProcessProxy::cacheMediaMIMETypes(const Vector<String>& types)
+{
+    if (!mediaTypeCache().isEmpty())
+        return;
+
+    mediaTypeCache() = types;
+    for (auto& process : processPool().processes()) {
+        if (process != this)
+            cacheMediaMIMETypesInternal(types);
+    }
+}
+
+void WebProcessProxy::cacheMediaMIMETypesInternal(const Vector<String>& types)
+{
+    if (!mediaTypeCache().isEmpty())
+        return;
+
+    mediaTypeCache() = types;
+    send(Messages::WebProcess::SetMediaMIMETypes(types), 0);
+}
+
+Vector<String> WebProcessProxy::mediaMIMETypes()
+{
+    return mediaTypeCache();
 }
 
 }

Source/WebKit/UIProcess/WebProcessProxy.h
@@ -221,6 +221,11 @@ public:
     bool isInPrewarmedPool() const { return m_isInPrewarmedPool; }
     void setIsInPrewarmedPool(bool isInPrewarmedPool) { m_isInPrewarmedPool = isInPrewarmedPool; }
 
+#if PLATFORM(COCOA)
+    Vector<String> mediaMIMETypes();
+    void cacheMediaMIMETypes(const Vector<String>&);
+#endif
+
 protected:
     static uint64_t generatePageID();
     WebProcessProxy(WebProcessPool&, WebsiteDataStore&, IsInPrewarmedPool);
@@ -232,6 +237,10 @@ protected:
 
     // ProcessLauncher::Client
     void didFinishLaunching(ProcessLauncher*, IPC::Connection::Identifier) override;
+
+#if PLATFORM(COCOA)
+    void cacheMediaMIMETypesInternal(const Vector<String>&);
+#endif
 
 private:
     // Called when the web process has crashed or we know that it will terminate soon.

Source/WTF/wtf/cocoa/SoftLinking.h
@@ -63,6 +63,13 @@
         return frameworkLibrary; \
     }
 
+#define SOFT_LINK_FRAMEWORK_OPTIONAL_PREFLIGHT(framework) \
+    static bool framework##LibraryIsAvailable() \
+    { \
+        static bool frameworkLibraryIsAvailable = dlopen_preflight("/System/Library/Frameworks/" #framework ".framework/" #framework); \
+        return frameworkLibraryIsAvailable; \
+    }
+
 #define SOFT_LINK_FRAMEWORK_OPTIONAL(framework) \
     static void* framework##Library() \
     { \

Source/WebCore/WebCore.xcodeproj/project.pbxproj
@@ -219,6 +219,7 @@
 		07EE76EC1BE96DB000F89133 /* MockRealtimeVideoSource.h in Headers */ = {isa = PBXBuildFile; fileRef = 07EE76EA1BE96DB000F89133 /* MockRealtimeVideoSource.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		07EE76EF1BEA619800F89133 /* MockRealtimeVideoSourceMac.h in Headers */ = {isa = PBXBuildFile; fileRef = 07EE76ED1BEA619800F89133 /* MockRealtimeVideoSourceMac.h */; };
 		07F04A942006B1E300AE2A0A /* ApplicationStateChangeListener.h in Headers */ = {isa = PBXBuildFile; fileRef = 07F04A92200684BC00AE2A0A /* ApplicationStateChangeListener.h */; settings = {ATTRIBUTES = (Private, ); }; };
+		07F4E93320B3587F002E3803 /* AVFoundationMIMETypeCache.h in Headers */ = {isa = PBXBuildFile; fileRef = 07C8AD121D073D630087C5CE /* AVFoundationMIMETypeCache.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		07F876841AD580F900905849 /* MediaPlaybackTargetContext.h in Headers */ = {isa = PBXBuildFile; fileRef = 07F876831AD4A94500905849 /* MediaPlaybackTargetContext.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		07F944161864D046005D31CB /* PlatformMediaSessionManager.h in Headers */ = {isa = PBXBuildFile; fileRef = CDAE8C081746B95700532D78 /* PlatformMediaSessionManager.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		07FE99DD18807A7D00256648 /* MediaElementSession.h in Headers */ = {isa = PBXBuildFile; fileRef = 07FE99DB18807A7D00256648 /* MediaElementSession.h */; settings = {ATTRIBUTES = (Private, ); }; };
@@ -27120,6 +27121,7 @@
 				CDC675271EAEA9D400727C84 /* AVAudioSessionCaptureDevice.h in Headers */,
 				CDC675231EAEA9B700727C84 /* AVAudioSessionCaptureDeviceManager.h in Headers */,
 				070363E2181A1CDC00C074A5 /* AVCaptureDeviceManager.h in Headers */,
+				07F4E93320B3587F002E3803 /* AVFoundationMIMETypeCache.h in Headers */,
 				070363E4181A1CDC00C074A5 /* AVMediaCaptureSource.h in Headers */,
 				CD336F6217F9F64700DDDCD0 /* AVTrackPrivateAVFObjCImpl.h in Headers */,
 				070363E6181A1CDC00C074A5 /* AVVideoCaptureSource.h in Headers */,

Source/WebCore/platform/graphics/ImageDecoder.cpp
@@ -42,11 +42,11 @@ namespace WebCore {
 
 RefPtr<ImageDecoder> ImageDecoder::create(SharedBuffer& data, const String& mimeType, AlphaOption alphaOption, GammaAndColorProfileOption gammaAndColorProfileOption)
 {
-#if HAVE(AVASSETREADER)
-    if (ImageDecoderAVFObjC::canDecodeType(mimeType))
-        return ImageDecoderAVFObjC::create(data, mimeType, alphaOption, gammaAndColorProfileOption);
-#else
     UNUSED_PARAM(mimeType);
+
+#if HAVE(AVASSETREADER)
+    if (!ImageDecoderCG::canDecodeType(mimeType) && ImageDecoderAVFObjC::canDecodeType(mimeType))
+        return ImageDecoderAVFObjC::create(data, mimeType, alphaOption, gammaAndColorProfileOption);
 #endif
 
 #if USE(CG)
@@ -60,24 +60,23 @@ RefPtr<ImageDecoder> ImageDecoder::create(SharedBuffer& data, const String& mime
 
 bool ImageDecoder::supportsMediaType(MediaType type)
 {
-    bool supports = false;
-#if HAVE(AVASSETREADER)
-    if (ImageDecoderAVFObjC::supportsMediaType(type))
-        supports = true;
-#endif
-
 #if USE(CG)
     if (ImageDecoderCG::supportsMediaType(type))
-        supports = true;
+        return true;
 #elif USE(DIRECT2D)
     if (ImageDecoderDirect2D::supportsMediaType(type))
-        supports = true;
+        return true;
 #else
     if (ScalableImageDecoder::supportsMediaType(type))
-        supports = true;
+        return true;
 #endif
 
-    return supports;
+#if HAVE(AVASSETREADER)
+    if (ImageDecoderAVFObjC::supportsMediaType(type))
+        return true;
+#endif
+
+    return false;
 }
 
 }

Source/WebCore/platform/graphics/avfoundation/objc/AVFoundationMIMETypeCache.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Apple Inc. All rights reserved.
+ * Copyright (C) 2016-2018 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,10 +23,9 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#ifndef AVFoundationMIMETypeCache_h
-#define AVFoundationMIMETypeCache_h
+#pragma once
 
-#if ENABLE(VIDEO) && USE(AVFOUNDATION)
+#if PLATFORM(COCOA)
 
 #include <wtf/Forward.h>
 #include <wtf/HashSet.h>
@@ -34,24 +33,34 @@
 
 namespace WebCore {
 
+class ContentType;
+
 class AVFoundationMIMETypeCache {
 public:
-    static AVFoundationMIMETypeCache& singleton();
+    WEBCORE_EXPORT static AVFoundationMIMETypeCache& singleton();
 
-    AVFoundationMIMETypeCache();
+    bool supportsContentType(const ContentType&);
+    bool canDecodeType(const String&);
 
-    void loadTypes();
     const HashSet<String, ASCIICaseInsensitiveHash>& types();
+    bool isEmpty();
+    bool isAvailable() const;
+
+    using CacheMIMETypesCallback = std::function<void(const Vector<String>&)>;
+    void setCacheMIMETypesCallback(CacheMIMETypesCallback&& callback) { m_cacheTypeCallback = WTFMove(callback); }
+
+    WEBCORE_EXPORT void setSupportedTypes(const Vector<String>&);
 
 private:
-    enum MIMETypeLoadStatus { NotLoaded, Loading, Loaded };
+    friend NeverDestroyed<AVFoundationMIMETypeCache>;
+    AVFoundationMIMETypeCache() = default;
 
-    MIMETypeLoadStatus m_status { NotLoaded };
-    dispatch_queue_t m_loaderQueue;
-    Lock m_lock;
-    HashSet<String, ASCIICaseInsensitiveHash> m_cache;
+    void loadMIMETypes();
+
+    std::optional<HashSet<String, ASCIICaseInsensitiveHash>> m_cache;
+    CacheMIMETypesCallback m_cacheTypeCallback;
 };
 
 }
-#endif
+
 #endif

Source/WebCore/platform/graphics/avfoundation/objc/AVFoundationMIMETypeCache.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Apple Inc. All rights reserved.
+ * Copyright (C) 2016-2018 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -26,70 +26,95 @@
 #import "config.h"
 #import "AVFoundationMIMETypeCache.h"
 
-#if ENABLE(VIDEO) && USE(AVFOUNDATION)
+#if PLATFORM(COCOA)
 
+#import "ContentType.h"
 #import <AVFoundation/AVAsset.h>
 #import <wtf/HashSet.h>
-#import <wtf/Locker.h>
-#import <wtf/NeverDestroyed.h>
 
 #import <pal/cf/CoreMediaSoftLink.h>
 
+#if ENABLE(VIDEO) && USE(AVFOUNDATION)
+SOFT_LINK_FRAMEWORK_OPTIONAL_PREFLIGHT(AVFoundation)
 SOFT_LINK_FRAMEWORK_OPTIONAL(AVFoundation)
 SOFT_LINK_CLASS(AVFoundation, AVURLAsset)
+#endif
 
 namespace WebCore {
-
-using namespace PAL;
-
-AVFoundationMIMETypeCache::AVFoundationMIMETypeCache()
-{
-    m_loaderQueue = dispatch_queue_create("WebCoreAudioVisualMIMETypes loader queue", DISPATCH_QUEUE_SERIAL);
-}
-
-void AVFoundationMIMETypeCache::loadTypes()
-{
-    {
-        Locker<Lock> lock(m_lock);
-
-        if (m_status != NotLoaded)
-            return;
-
-        if (!AVFoundationLibrary()) {
-            m_status = Loaded;
-            return;
-        }
-
-        m_status = Loading;
-    }
-
-    dispatch_async(m_loaderQueue, [this] {
-        for (NSString *type in [getAVURLAssetClass() audiovisualMIMETypes])
-            m_cache.add(type);
-        m_lock.lock();
-        m_status = Loaded;
-        m_lock.unlock();
-    });
-}
-
-const HashSet<String, ASCIICaseInsensitiveHash>& AVFoundationMIMETypeCache::types()
-{
-    m_lock.lock();
-    MIMETypeLoadStatus status = m_status;
-    m_lock.unlock();
-
-    if (status != Loaded) {
-        loadTypes();
-        dispatch_sync(m_loaderQueue, ^{ });
-    }
-
-    return m_cache;
-}
 
 AVFoundationMIMETypeCache& AVFoundationMIMETypeCache::singleton()
 {
     static NeverDestroyed<AVFoundationMIMETypeCache> cache;
     return cache.get();
+}
+
+void AVFoundationMIMETypeCache::setSupportedTypes(const Vector<String>& types)
+{
+    if (m_cache)
+        return;
+
+    m_cache = HashSet<String, ASCIICaseInsensitiveHash>();
+    for (auto& type : types)
+        m_cache->add(type);
+}
+
+const HashSet<String, ASCIICaseInsensitiveHash>& AVFoundationMIMETypeCache::types()
+{
+    if (!m_cache)
+        loadMIMETypes();
+
+    return *m_cache;
+}
+
+bool AVFoundationMIMETypeCache::supportsContentType(const ContentType& contentType)
+{
+    if (contentType.isEmpty())
+        return false;
+
+    return types().contains(contentType.containerType());
+}
+
+bool AVFoundationMIMETypeCache::canDecodeType(const String& mimeType)
+{
+    if (mimeType.isEmpty())
+        return false;
+
+    if (!isAvailable() || !types().contains(ContentType { mimeType }.containerType()))
+        return false;
+
+#if ENABLE(VIDEO) && USE(AVFOUNDATION)
+    return [getAVURLAssetClass() isPlayableExtendedMIMEType:mimeType];
+#endif
+
+    return false;
+}
+
+bool AVFoundationMIMETypeCache::isAvailable() const
+{
+#if ENABLE(VIDEO) && USE(AVFOUNDATION)
+    return AVFoundationLibraryIsAvailable();
+#else
+    return false;
+#endif
+}
+
+void AVFoundationMIMETypeCache::loadMIMETypes()
+{
+    m_cache = HashSet<String, ASCIICaseInsensitiveHash>();
+
+#if ENABLE(VIDEO) && USE(AVFOUNDATION)
+    static std::once_flag onceFlag;
+    std::call_once(onceFlag, [this] {
+        if (!AVFoundationLibrary())
+            return;
+
+        for (NSString* type in [getAVURLAssetClass() audiovisualMIMETypes])
+            m_cache->add(type);
+
+        if (m_cacheTypeCallback)
+            m_cacheTypeCallback(copyToVector(*m_cache));
+    });
+#endif
 }
 
 }

Source/WebCore/platform/graphics/avfoundation/objc/ImageDecoderAVFObjC.mm
@@ -319,7 +319,7 @@ ImageDecoderAVFObjCSample* toSample(Iterator iter)
 RefPtr<ImageDecoderAVFObjC> ImageDecoderAVFObjC::create(SharedBuffer& data, const String& mimeType, AlphaOption alphaOption, GammaAndColorProfileOption gammaAndColorProfileOption)
 {
     // AVFoundation may not be available at runtime.
-    if (!getAVURLAssetClass())
+    if (!AVFoundationMIMETypeCache::singleton().isAvailable())
         return nullptr;
 
     if (!canLoad_VideoToolbox_VTCreateCGImageFromCVPixelBuffer())
@@ -350,24 +350,17 @@ ImageDecoderAVFObjC::~ImageDecoderAVFObjC() = default;
 
 bool ImageDecoderAVFObjC::supportsMediaType(MediaType type)
 {
-    if (type == MediaType::Video)
-        return getAVURLAssetClass() && canLoad_VideoToolbox_VTCreateCGImageFromCVPixelBuffer();
-    return false;
+    return type == MediaType::Video && AVFoundationMIMETypeCache::singleton().isAvailable();
 }
 
 bool ImageDecoderAVFObjC::supportsContentType(const ContentType& type)
 {
-    if (getAVURLAssetClass() && canLoad_VideoToolbox_VTCreateCGImageFromCVPixelBuffer())
-        return AVFoundationMIMETypeCache::singleton().types().contains(type.containerType());
-    return false;
+    return AVFoundationMIMETypeCache::singleton().supportsContentType(type);
 }
 
 bool ImageDecoderAVFObjC::canDecodeType(const String& mimeType)
 {
-    if (!supportsMediaType(MediaType::Video))
-        return nullptr;
-
-    return [getAVURLAssetClass() isPlayableExtendedMIMEType:mimeType];
+    return AVFoundationMIMETypeCache::singleton().canDecodeType(mimeType);
 }
 
 AVAssetTrack *ImageDecoderAVFObjC::firstEnabledTrack()

commit 75c66e5982a9258fa7019336c884473a1cabf393

WebProcessProxy::cacheMediaMIMETypes never broadcasts cached types to other web processes https://bugs.webkit.org/show_bug.cgi?id=320070
Reviewed by Jean-Yves Avenard.

When a web process lazily computes the supported media MIME types (by loading
AVFoundation and querying [AVURLAsset audiovisualMIMETypes]), it reports them
back to the UI process via CacheMediaMIMETypes. The UI process caches them in a
process-global and is supposed to push them to every other existing web process
(via SetMediaMIMETypes) so those processes can avoid recomputing them.

This broadcast was broken in two ways, both present since the machinery was
introduced in 201372@main (2018):

  1. cacheMediaMIMETypes() iterated over the other processes but called
     cacheMediaMIMETypesInternal(types) on `this` rather than on the loop
     variable `process`, so the message would have targeted the wrong process.

  2. Even ignoring (1), cacheMediaMIMETypesInternal() guarded on
     `!mediaTypeCache().isEmpty()` and returned early. Since mediaTypeCache() is
     a UI-process global that cacheMediaMIMETypes() populates before the loop,
     that guard was always true by the time the helper ran, so the
     SetMediaMIMETypes IPC was never sent to any process.

The net effect was that already-running web processes never received the cached
types over IPC and had to recompute them independently on first use.

Fix this by inlining the broadcast into the loop so it sends SetMediaMIMETypes
directly to each peer process, and remove the now-unused
cacheMediaMIMETypesInternal() helper.

Note that the practical value of this broadcast is limited today:

  - New web processes were never affected: they already receive the cached types
    at launch via WebProcessCreationParameters (WebProcessPoolCocoa.mm), which
    seeds both AVAssetMIMETypeCache and RemoteMediaPlayerMIMETypeCache. That leg
    has always worked; only the broadcast to already-running peers was broken.

Source/WebKit/UIProcess/Cocoa/WebProcessProxyCocoa.mm
@@ -125,17 +125,8 @@ void WebProcessProxy::cacheMediaMIMETypes(const Vector<String>& types)
     mediaTypeCache() = types;
     for (Ref process : processPool().processes()) {
         if (process.ptr() != this)
-            cacheMediaMIMETypesInternal(types);
+            process->send(Messages::WebProcess::SetMediaMIMETypes(types), 0);
     }
-}
-
-void WebProcessProxy::cacheMediaMIMETypesInternal(const Vector<String>& types)
-{
-    if (!mediaTypeCache().isEmpty())
-        return;
-
-    mediaTypeCache() = types;
-    send(Messages::WebProcess::SetMediaMIMETypes(types), 0);
 }
 
 const Vector<String>& WebProcessProxy::mediaMIMETypes()

Source/WebKit/UIProcess/WebProcessProxy.h
@@ -664,8 +664,6 @@ private:
 
 #if PLATFORM(COCOA)
     bool handleRemoteObjectRegistryMessage(IPC::Connection&, IPC::Decoder&);
-
-    void cacheMediaMIMETypesInternal(const Vector<String>&);
 #endif
 
     // ProcessLauncher::Client