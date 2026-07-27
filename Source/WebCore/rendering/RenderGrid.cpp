9bb7a30a2ff78ca1732dd096e6c14c054677f615
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2026-07-27 14:59:50 -0700

    Subgrid with large column-gap overflows max-width-constrained parent https://bugs.webkit.org/show_bug.cgi?id=280613 rdar://137422489
    Reviewed by Sammy Gill.
    
    This patch aligns WebKit with Blink / Chromium.
    
    Per CSS Grid Level 2 [1], a subgrid "acts as if it was completely empty
    for track sizing purposes in the subgridded dimension" and "its track
    sizes are governed by the parent grid." So a subgrid must not contribute
    its own gap to that axis. WebKit did, twice, overflowing a
    max-width-constrained parent and any item spanning the subgrid:
    
        parent grid (max-width) |--------------|
        +---------------------------------------+
        | subgrid  [t0].gap.[t1].gap.[t2]... ----+--> overflow: + 12 x 10rem
        +---------------------------------------+   (subgrid's own gap, wrongly
          tracks governed by parent --+             added on top of parent tracks)
    
    1. computeTrackSizesForIndefiniteSize() unconditionally added
       guttersSize() to the container's min/max-content width. In a
       subgridded axis this injected the subgrid's own gap (13 inherited
       tracks, gap: 10rem -> +12 x 10rem), forcing the parent wider than its
       containing block. Fix: skip gutters when isSubgrid(direction).
    
    2. gridAreaBreadthForGridItemIncludingAlignmentOffsets() used
       tracks[end-1]->baseSize(). To realize [1]'s rule that "half the
       difference between the subgrid's gutters and its parent grid's
       gutters is applied as an extra layer of (potentially negative)
       margin," copyUsedTrackSizesForSubgrid() stores negative base sizes;
       baseSize() clamps them to zero, overstating the breadth. Fix: use
       unclampedBaseSize(), matching populateGridPositionsForDirection().
    
    [1] https://drafts.csswg.org/css-grid-2/#subgrids
    
    Test: imported/w3c/web-platform-tests/css/css-grid/subgrid/subgrid-own-column-gap-does-not-inflate-intrinsic-width.html
    
    * LayoutTests/imported/w3c/web-platform-tests/css/css-grid/subgrid/subgrid-own-column-gap-does-not-inflate-intrinsic-width-expected.txt: Added.
    * LayoutTests/imported/w3c/web-platform-tests/css/css-grid/subgrid/subgrid-own-column-gap-does-not-inflate-intrinsic-width.html: Added.
    * Source/WebCore/rendering/RenderGrid.cpp:
    (WebCore::RenderGrid::computeTrackSizesForIndefiniteSize const):
    (WebCore::RenderGrid::gridAreaBreadthForGridItemIncludingAlignmentOffsets const):
    
    Canonical link: https://commits.webkit.org/318009@main

--- Source/WebCore/rendering/RenderGrid.cpp
+++ Source/WebCore/rendering/RenderGrid.cpp
@@ -882,7 +882,7 @@ std::pair<LayoutUnit, LayoutUnit> RenderGrid::computeTrackSizesForIndefiniteSize
     algorithm.run(direction, numTracks(direction), SizingOperation::IntrinsicSizeComputation, std::nullopt, gridLayoutState);
 
     size_t numberOfTracks = algorithm.tracks(direction).size();
-    auto totalGuttersSize = direction == Style::GridTrackSizingDirection::Columns && explicitIntrinsicInnerLogicalSize(direction).has_value() ? 0_lu : guttersSize(direction, 0, numberOfTracks, std::nullopt);
+    auto totalGuttersSize = (direction == Style::GridTrackSizingDirection::Columns && explicitIntrinsicInnerLogicalSize(direction).has_value()) || isSubgrid(direction) ? 0_lu : guttersSize(direction, 0, numberOfTracks, std::nullopt);
 
     ASSERT(algorithm.tracksAreWiderThanMinTrackBreadth());
     return { algorithm.minContentSize() + totalGuttersSize, algorithm.maxContentSize() + totalGuttersSize };
@@ -1720,8 +1720,10 @@ LayoutUnit RenderGrid::gridAreaBreadthForGridItemIncludingAlignmentOffsets(const
     auto initialTrackPosition = positions[span.startLine()];
     auto finalTrackPosition = positions[span.endLine() - 1];
 
-    // Track Positions vector stores the 'start' grid line of each track, so we have to add last track's baseSize.
-    return finalTrackPosition - initialTrackPosition + tracks[span.endLine() - 1]->baseSize();
+    // Track Positions vector stores the 'start' grid line of each track, so we have to add last track's
+    // unclamped base size, matching populateGridPositionsForDirection() (a subgrid track's base size can
+    // be negative; clamping it here would overstate the breadth).
+    return finalTrackPosition - initialTrackPosition + tracks[span.endLine() - 1]->unclampedBaseSize();
 }
 
 void RenderGrid::populateGridPositionsForDirection(const GridTrackSizingAlgorithm& algorithm, Style::GridTrackSizingDirection direction)
