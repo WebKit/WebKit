34520da7d0fa1404787d3060c7c0aa1d449ec335
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2026-07-26 18:47:05 -0700

    registerProcessor() error messages report the processor name instead of the offending AudioParam name https://bugs.webkit.org/show_bug.cgi?id=320266 rdar://183189781
    Reviewed by Chris Dumez.
    
    registerProcessor()'s parameterDescriptors validation errors (duplicate
    name, defaultValue out of [minValue, maxValue] range) interpolated the
    worklet processor's name into the message instead of the name of the
    AudioParamDescriptor that actually failed validation, making it
    impossible to tell which param was at fault when a processor declares
    multiple parameters.
    
    Fix by using descriptor.name instead of name in the three
    Exception messages.
    
    * Source/WebCore/Modules/webaudio/AudioWorkletGlobalScope.cpp:
    (WebCore::AudioWorkletGlobalScope::registerProcessor):
    
    Canonical link: https://commits.webkit.org/317948@main

--- Source/WebCore/Modules/webaudio/AudioWorkletGlobalScope.cpp
+++ Source/WebCore/Modules/webaudio/AudioWorkletGlobalScope.cpp
@@ -113,11 +113,11 @@ ExceptionOr<void> AudioWorkletGlobalScope::registerProcessor(String&& name, Ref<
         for (auto& descriptor : parameterDescriptors) {
             auto addResult = paramNames.add(descriptor.name);
             if (!addResult.isNewEntry)
-                return Exception { ExceptionCode::NotSupportedError, makeString("parameterDescriptors contain duplicate AudioParam name: "_s, name) };
+                return Exception { ExceptionCode::NotSupportedError, makeString("parameterDescriptors contain duplicate AudioParam name: "_s, descriptor.name) };
             if (descriptor.defaultValue < descriptor.minValue)
-                return Exception { ExceptionCode::InvalidStateError, makeString("AudioParamDescriptor with name '"_s, name, "' has a defaultValue that is less than the minValue"_s) };
+                return Exception { ExceptionCode::InvalidStateError, makeString("AudioParamDescriptor with name '"_s, descriptor.name, "' has a defaultValue that is less than the minValue"_s) };
             if (descriptor.defaultValue > descriptor.maxValue)
-                return Exception { ExceptionCode::InvalidStateError, makeString("AudioParamDescriptor with name '"_s, name, "' has a defaultValue that is greater than the maxValue"_s) };
+                return Exception { ExceptionCode::InvalidStateError, makeString("AudioParamDescriptor with name '"_s, descriptor.name, "' has a defaultValue that is greater than the maxValue"_s) };
         }
     }
 
