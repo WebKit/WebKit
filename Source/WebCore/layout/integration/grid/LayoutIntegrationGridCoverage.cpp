690d4e5d080bf880c5cb73d2e777fc5de3d3ac05
Sammy Gill <sammy.gill@apple.com>
2026-07-28 17:32:44 -0700

    [GFC][Integration] Bail out of GFC for relatively positioned grid items with a percentage offset https://bugs.webkit.org/show_bug.cgi?id=320492 rdar://problem/183458460
    Reviewed by Taher Ali.
    
    When a grid item is relatively positioned and has a percentage inset we
    need to resolve the percent against the item's grid area. This
    information is needed after layout but we currently have no way to
    recompute this and it is not stored anywhere so let's not run GFC while
    we figure this out.
    
    Canonical link: https://commits.webkit.org/318115@main

--- Source/WebCore/layout/integration/grid/LayoutIntegrationGridCoverage.cpp
+++ Source/WebCore/layout/integration/grid/LayoutIntegrationGridCoverage.cpp
@@ -81,6 +81,7 @@ enum class GridAvoidanceReason : uint8_t {
     GridItemHasNonVisibleOverflow,
     GridItemHasContainsSize,
     GridItemNeedsSecondColumnSizingPass,
+    RelativeGridItemHasPercentageInset,
 
     GridItemColumnStartHasLineName,
     GridItemColumnStartHasNegativeLineNumber,
@@ -612,6 +613,18 @@ static EnumSet<GridAvoidanceReason> gridLayoutAvoidanceReason(const RenderGrid& 
         if (gridItem->isOutOfFlowPositioned())
             ADD_REASON_AND_RETURN_IF_NEEDED(GridAvoidanceReason::GridHasOutOfFlowChild, reasons, reasonCollectionMode);
 
+        // A relatively (or sticky) positioned grid item resolves percentage inset offsets against
+        // its containing block, which for a grid item is its grid area. GFC does not yet set the
+        // grid-area size on the item, so such percentages would incorrectly resolve against the
+        // grid container's content box. Keep these items on the legacy path.
+        if (gridItemStyle->position() == PositionType::Relative || gridItemStyle->position() == PositionType::Sticky) {
+            auto hasPercentageInsetOffset = gridItemStyle->insetBox().anyOf([](const Style::InsetEdge& insetEdge) {
+                return insetEdge.isPercentOrCalculated();
+            });
+            if (hasPercentageInsetOffset)
+                ADD_REASON_AND_RETURN_IF_NEEDED(GridAvoidanceReason::RelativeGridItemHasPercentageInset, reasons, reasonCollectionMode);
+        }
+
         if (gridItemStyle->aspectRatio().hasRatio())
             ADD_REASON_AND_RETURN_IF_NEEDED(GridAvoidanceReason::GridItemHasAspectRatio, reasons, reasonCollectionMode);
 
@@ -787,6 +800,9 @@ static void printReason(GridAvoidanceReason reason, TextStream& stream)
         break;
     case GridAvoidanceReason::GridItemNeedsSecondColumnSizingPass:
         stream << "grid item needs second column sizing support";
+        break;
+    case GridAvoidanceReason::RelativeGridItemHasPercentageInset:
+        stream << "relative grid item has percentage inset";
         break;
     case GridAvoidanceReason::GridItemColumnStartHasLineName:
         stream << "grid item column start has line name";
