49300a9c6739d3435bf9a6f1ca83d3c44b92f7af
Adrian Perez de Castro <aperez@igalia.com>
2026-08-03 09:28:55 -0700

    Cherry-pick 318441@main (de831160fc15). https://bugs.webkit.org/show_bug.cgi?id=320840
        [Swift] LTO linker flags unrecognezed by swiftc
        https://bugs.webkit.org/show_bug.cgi?id=320840
    
        Reviewed by Adrian Taylor and Nikolas Zimmermann.
    
        * Tools/Scripts/swift/swiftc-wrapper.sh: Prefix LTO flags (both plain
          -flto and -flto=<param>) with -Xcc to have those passed to clang and
          prevent swiftc from trying to use them.
    
        Canonical link: https://commits.webkit.org/318441@main
    
    Canonical link: https://commits.webkit.org/317695.27@webkitglib/2.54

--- Tools/Scripts/swift/swiftc-wrapper.sh
+++ Tools/Scripts/swift/swiftc-wrapper.sh
@@ -69,6 +69,9 @@ for arg in "$@"; do
             fi
             ;;
         "-include") skip_next=1 ;;
+        "-flto" | "-flto="*)
+            args+=("-Xcc" "$arg")
+            ;;
         "-fuse-ld="*)
             args+=("-Xcc" "$arg")
             ;;
