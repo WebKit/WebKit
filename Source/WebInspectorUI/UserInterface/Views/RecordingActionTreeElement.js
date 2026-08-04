83b815141e0695e015410b3e774e2f4a0c7e3f3a
Devin Rousso <hi@devinrousso.com>
2026-08-03 21:39:15 -0700

    Web Inspector: Canvas: add icons for recorded WebGL and WebGPU actions https://bugs.webkit.org/show_bug.cgi?id=176560
    Reviewed by Mike Wyrzykowski.
    
    * Source/WebInspectorUI/UserInterface/Views/RecordingActionTreeElement.js:
    (WI.RecordingActionTreeElement._classNameForAction):
    * Source/WebInspectorUI/UserInterface/Views/RecordingActionTreeElement.css:
    (.item.recording-action.bind > .icon): Added.
    (.item.recording-action.copy > .icon): Added.
    (.item.recording-action.object > .icon): Added.
    (.item.recording-action.draw > .icon): Added.
    (.item.recording-action.execute > .icon): Added.
    (.item.recording-action.query > .icon): Added.
    (.item.recording-action.read > .icon): Added.
    (.item.recording-action.show > .icon): Added.
    (.item.recording-action.source > .icon): Added.
    (.item.recording-action.uniform > .icon): Added.
    (.item.recording-action.begin > .icon): Added.
    (.item.recording-action.end > .icon): Added.
    (.item.recording-action.viewport > .icon): Added.
    (.item.recording-action.write > .icon): Added.
    
    * Source/WebInspectorUI/UserInterface/Images/Begin.svg: Added.
    * Source/WebInspectorUI/UserInterface/Images/Bind.svg: Added.
    * Source/WebInspectorUI/UserInterface/Images/Copy.svg: Added.
    * Source/WebInspectorUI/UserInterface/Images/End.svg: Added.
    * Source/WebInspectorUI/UserInterface/Images/Execute.svg: Added.
    * Source/WebInspectorUI/UserInterface/Images/Object.svg: Added.
    * Source/WebInspectorUI/UserInterface/Images/Read.svg: Added.
    * Source/WebInspectorUI/UserInterface/Images/Viewport.svg: Added.
    * Source/WebInspectorUI/UserInterface/Images/Write.svg: Added.
    
    Canonical link: https://commits.webkit.org/318517@main

--- Source/WebInspectorUI/UserInterface/Views/RecordingActionTreeElement.js
+++ Source/WebInspectorUI/UserInterface/Views/RecordingActionTreeElement.js
@@ -264,36 +264,390 @@ WI.RecordingActionTreeElement = class RecordingActionTreeElement extends WI.Gene
     static _classNameForAction(recordingAction)
     {
         switch (recordingAction.name) {
+        case "activeTexture":
+        case "attachShader":
+        case "bindAttribLocation":
+        case "bindBuffer":
+        case "bindBufferBase":
+        case "bindBufferRange":
+        case "bindFramebuffer":
+        case "bindRenderbuffer":
+        case "bindSampler":
+        case "bindTexture":
+        case "bindTransformFeedback":
+        case "bindVertexArray":
+        case "bindVertexArrayOES":
+        case "compileShader":
+        case "detachShader":
+        case "framebufferRenderbuffer":
+        case "framebufferTexture2D":
+        case "framebufferTextureLayer":
+        case "getBindGroupLayout":
+        case "linkProgram":
+        case "setBindGroup":
+        case "setIndexBuffer":
+        case "setPipeline":
+        case "setVertexBuffer":
+        case "useProgram":
+        case "validateProgram":
+            return "bind";
+
+        case "blitFramebuffer":
+        case "copyBufferSubData":
+        case "copyBufferToBuffer":
+        case "copyBufferToTexture":
+        case "copyElementImageToTexture":
+        case "copyExternalImageToTexture":
+        case "copyTexImage2D":
+        case "copyTexSubImage2D":
+        case "copyTexSubImage3D":
+        case "copyTextureToBuffer":
+        case "copyTextureToTexture":
+            return "copy";
+
+        case "createBindGroup":
+        case "createBindGroupLayout":
+        case "createBuffer":
+        case "createCommandEncoder":
+        case "createComputePipeline":
+        case "createComputePipelineAsync":
+        case "createFramebuffer":
+        case "createPipelineLayout":
+        case "createProgram":
+        case "createQuery":
+        case "createQueryEXT":
+        case "createQuerySet":
+        case "createRenderbuffer":
+        case "createRenderBundleEncoder":
+        case "createRenderPipeline":
+        case "createRenderPipelineAsync":
+        case "createSampler":
+        case "createShader":
+        case "createShaderModule":
+        case "createTexture":
+        case "createTransformFeedback":
+        case "createVertexArray":
+        case "createVertexArrayOES":
+        case "createView":
+        case "deleteBuffer":
+        case "deleteFramebuffer":
+        case "deleteProgram":
+        case "deleteQuery":
+        case "deleteQueryEXT":
+        case "deleteRenderbuffer":
+        case "deleteSampler":
+        case "deleteShader":
+        case "deleteSync":
+        case "deleteTexture":
+        case "deleteTransformFeedback":
+        case "deleteVertexArray":
+        case "deleteVertexArrayOES":
+        case "destroy":
+        case "fenceSync":
+        case "importExternalTexture":
+        case "invalidateFramebuffer":
+        case "invalidateSubFramebuffer":
+        case "renderbufferStorage":
+        case "renderbufferStorageMultisample":
+        case "texStorage2D":
+        case "texStorage3D":
+            return "object";
+
+        case "draw":
+        case "drawArrays":
+        case "drawArraysInstanced":
+        case "drawArraysInstancedANGLE":
+        case "drawArraysInstancedBaseInstanceWEBGL":
+        case "drawElements":
+        case "drawElementsInstanced":
+        case "drawElementsInstancedANGLE":
+        case "drawElementsInstancedBaseVertexBaseInstanceWEBGL":
+        case "drawIndexed":
+        case "drawIndexedIndirect":
+        case "drawIndirect":
+        case "drawRangeElements":
+        case "multiDrawArraysInstancedBaseInstanceWEBGL":
+        case "multiDrawArraysInstancedWEBGL":
+        case "multiDrawArraysWEBGL":
+        case "multiDrawElementsInstancedBaseVertexBaseInstanceWEBGL":
+        case "multiDrawElementsInstancedWEBGL":
+        case "multiDrawElementsWEBGL":
+            return "draw";
+
+        case "dispatchWorkgroups":
+        case "dispatchWorkgroupsIndirect":
+        case "executeBundles":
+        case "flush":
+        case "makeXRCompatible":
+        case "submit":
+            return "execute";
+
+        case "beginOcclusionQuery":
+        case "beginQuery":
+        case "beginQueryEXT":
+        case "clientWaitSync":
+        case "count":
+        case "endOcclusionQuery":
+        case "endQuery":
+        case "endQueryEXT":
+        case "getQuery":
+        case "getQueryEXT":
+        case "getQueryObjectEXT":
+        case "getQueryParameter":
+        case "getSyncParameter":
+        case "isQuery":
+        case "isQueryEXT":
+        case "isSync":
+        case "lost":
+        case "mapState":
+        case "onSubmittedWorkDone":
+        case "queryCounterEXT":
+        case "resolveQuerySet":
+        case "type":
+        case "waitSync":
+            return "query";
+
+        case "adapterInfo":
+        case "canvas":
+        case "checkFramebufferStatus":
+        case "features":
+        case "getBufferParameter":
+        case "getBufferSubData":
+        case "getContextAttributes":
+        case "getEffectiveRenderingModeForTesting":
+        case "getError":
+        case "getExtension":
+        case "getFramebufferAttachmentParameter":
+        case "getIndexedParameter":
+        case "getInternalformatParameter":
+        case "getMappedRange":
+        case "getParameter":
+        case "getRenderbufferParameter":
+        case "getSupportedExtensions":
+        case "isBuffer":
+        case "isContextLost":
+        case "isEnabled":
+        case "isFramebuffer":
+        case "isProgram":
+        case "isRenderbuffer":
+        case "isSampler":
+        case "isShader":
+        case "isTexture":
+        case "isTransformFeedback":
+        case "isVertexArray":
+        case "isVertexArrayOES":
+        case "label":
+        case "limits":
+        case "onuncapturederror":
+        case "queue":
+        case "readBuffer":
+        case "readPixels":
+        case "size":
+        case "usage":
+            return "read";
+
+        case "popDebugGroup":
+        case "popErrorScope":
+        case "reset":
+        case "restore":
+        case "unmap":
+            return "restore";
+
+        case "mapAsync":
+        case "pushDebugGroup":
+        case "pushErrorScope":
+        case "save":
+            return "save";
+
+        case "getActiveAttrib":
+        case "getActiveUniform":
+        case "getActiveUniformBlockName":
+        case "getActiveUniformBlockParameter":
+        case "getActiveUniforms":
+        case "getAttachedShaders":
+        case "getAttribLocation":
+        case "getCompilationInfo":
+        case "getFragDataLocation":
+        case "getProgramInfoLog":
+        case "getProgramParameter":
+        case "getShaderInfoLog":
+        case "getShaderParameter":
+        case "getShaderPrecisionFormat":
+        case "getShaderSource":
+        case "getTransformFeedbackVarying":
+        case "getTranslatedShaderSource":
+        case "getUniform":
+        case "getUniformBlockIndex":
+        case "getUniformIndices":
+        case "getUniformLocation":
+            return "show";
+
+        case "shaderSource":
+            return "source";
+
+        case "transformFeedbackVaryings":
+        case "uniform1f":
+        case "uniform1fv":
+        case "uniform1i":
+        case "uniform1iv":
+        case "uniform1ui":
+        case "uniform1uiv":
+        case "uniform2f":
+        case "uniform2fv":
+        case "uniform2i":
+        case "uniform2iv":
+        case "uniform2ui":
+        case "uniform2uiv":
+        case "uniform3f":
+        case "uniform3fv":
+        case "uniform3i":
+        case "uniform3iv":
+        case "uniform3ui":
+        case "uniform3uiv":
+        case "uniform4f":
+        case "uniform4fv":
+        case "uniform4i":
+        case "uniform4iv":
+        case "uniform4ui":
+        case "uniform4uiv":
+        case "uniformBlockBinding":
+        case "uniformMatrix2fv":
+        case "uniformMatrix2x3fv":
+        case "uniformMatrix2x4fv":
+        case "uniformMatrix3fv":
+        case "uniformMatrix3x2fv":
+        case "uniformMatrix3x4fv":
+        case "uniformMatrix4fv":
+        case "uniformMatrix4x2fv":
+        case "uniformMatrix4x3fv":
+            return "uniform";
+
+        case "beginComputePass":
+        case "beginLayer":
+        case "beginRenderPass":
+        case "beginTransformFeedback":
+        case "restoreContext":
+        case "resumeTransformFeedback":
+            return "begin";
+
+        case "end":
+        case "endLayer":
+        case "endTransformFeedback":
+        case "finish":
+        case "loseContext":
+        case "pauseTransformFeedback":
+            return "end";
+
+        case "depthRange":
+        case "drawingBufferHeight":
+        case "drawingBufferWidth":
+        case "height":
+        case "setViewport":
+        case "viewport":
+        case "width":
+            return "viewport";
+
+        case "bufferData":
+        case "bufferSubData":
+        case "writeBuffer":
+        case "writeTexture":
+            return "write";
+
         case "arc":
         case "arcTo":
             return "arc";
 
+        case "blendColor":
+        case "blendEquation":
+        case "blendEquationiOES":
+        case "blendEquationSeparate":
+        case "blendEquationSeparateiOES":
+        case "blendFunc":
+        case "blendFunciOES":
+        case "blendFuncSeparate":
+        case "blendFuncSeparateiOES":
+        case "colorMask":
+        case "colorMaskiOES":
+        case "cullFace":
+        case "depthFunc":
+        case "depthMask":
+        case "disable":
+        case "disableiOES":
+        case "drawBuffers":
+        case "drawBuffersWEBGL":
+        case "enable":
+        case "enableiOES":
+        case "frontFace":
         case "globalAlpha":
         case "globalCompositeOperation":
+        case "hint":
+        case "sampleCoverage":
         case "setAlpha":
-        case "setGlobalAlpha":
+        case "setBlendConstant":
         case "setCompositeOperation":
+        case "setGlobalAlpha":
         case "setGlobalCompositeOperation":
+        case "setStencilReference":
+        case "stencilFunc":
+        case "stencilFuncSeparate":
+        case "stencilMask":
+        case "stencilMaskSeparate":
+        case "stencilOp":
+        case "stencilOpSeparate":
             return "composite";
 
         case "bezierCurveTo":
         case "quadraticCurveTo":
             return "curve";
 
+        case "clear":
+        case "clearBuffer":
+        case "clearBufferfi":
+        case "clearBufferfv":
+        case "clearBufferiv":
+        case "clearBufferuiv":
+        case "clearColor":
+        case "clearDepth":
         case "clearRect":
+        case "clearStencil":
         case "fill":
         case "fillRect":
         case "fillText":
             return "fill";
 
+        case "compressedTexImage2D":
+        case "compressedTexImage3D":
+        case "compressedTexSubImage2D":
+        case "compressedTexSubImage3D":
         case "createImageData":
+        case "depthOrArrayLayers":
+        case "dimension":
+        case "drawElementImage":
         case "drawFocusIfNeeded":
         case "drawImage":
+        case "drawingBufferColorSpace":
         case "filter":
+        case "format":
+        case "generateMipmap":
         case "getImageData":
+        case "getSamplerParameter":
+        case "getSupportedProfiles":
+        case "getTexParameter":
         case "imageSmoothingEnabled":
         case "imageSmoothingQuality":
+        case "mipLevelCount":
+        case "pixelStorei":
         case "putImageData":
+        case "sampleCount":
+        case "samplerParameterf":
+        case "samplerParameteri":
+        case "texElementImage2D":
+        case "texImage2D":
+        case "texImage3D":
+        case "texParameterf":
+        case "texParameteri":
+        case "texSubImage2D":
+        case "texSubImage3D":
         case "transferFromImageBitmap":
         case "webkitImageSmoothingEnabled":
             return "image";
@@ -352,26 +706,58 @@ WI.RecordingActionTreeElement = class RecordingActionTreeElement extends WI.Gene
 
         case "direction":
         case "font":
+        case "letterSpacing":
         case "measureText":
         case "textAlign":
         case "textBaseline":
+        case "wordSpacing":
             return "text";
 
+        case "disableVertexAttribArray":
+        case "enableVertexAttribArray":
         case "getTransform":
+        case "getVertexAttrib":
+        case "getVertexAttribOffset":
+        case "polygonOffset":
+        case "polygonModeWEBGL":
+        case "polygonOffsetClampEXT":
+        case "provokingVertexWEBGL":
         case "resetTransform":
         case "rotate":
         case "scale":
         case "setTransform":
         case "transform":
         case "translate":
+        case "vertexAttrib1f":
+        case "vertexAttrib1fv":
+        case "vertexAttrib2f":
+        case "vertexAttrib2fv":
+        case "vertexAttrib3f":
+        case "vertexAttrib3fv":
+        case "vertexAttrib4f":
+        case "vertexAttrib4fv":
+        case "vertexAttribDivisor":
+        case "vertexAttribDivisorANGLE":
+        case "vertexAttribI4i":
+        case "vertexAttribI4iv":
+        case "vertexAttribI4ui":
+        case "vertexAttribI4uiv":
+        case "vertexAttribIPointer":
+        case "vertexAttribPointer":
             return "transform";
 
         case "clip":
+        case "clipControlEXT":
+        case "scissor":
+        case "setScissorRect":
+            return "clip";
+
         case "ellipse":
+            return "ellipse";
+
         case "rect":
-        case "restore":
-        case "save":
-            return recordingAction.name;
+        case "roundRect":
+            return "rect";
         }
 
         return "name-unknown";
