82c30dd925e1726383e951ede5e761a56c991665
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2026-08-04 01:16:39 -0700

    Remove unreachable null check in XMLHttpRequestUpload::scriptExecutionContext() https://bugs.webkit.org/show_bug.cgi?id=320891 rdar://183909422
    Reviewed by Chris Dumez.
    
    WeakRef::ptr() ends in RELEASE_ASSERT(ptr), so it never returns null and the
    early return was dead code. Use protect() to get the counted reference in a
    single expression, matching eventListenersDidChange() just above; dereferencing
    the WeakRef directly would pass an uncounted 'this' and trip Safer C++.
    
    No change in behavior.
    
    * Source/WebCore/xml/XMLHttpRequestUpload.cpp:
    (WebCore::XMLHttpRequestUpload::scriptExecutionContext const):
    
    Canonical link: https://commits.webkit.org/318532@main

--- Source/WebCore/xml/XMLHttpRequestUpload.cpp
+++ Source/WebCore/xml/XMLHttpRequestUpload.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2008-2026 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -70,9 +70,7 @@ void XMLHttpRequestUpload::dispatchProgressEvent(const AtomString& type, unsigne
 
 ScriptExecutionContext* XMLHttpRequestUpload::scriptExecutionContext() const
 {
-    if (RefPtr request = m_request.ptr())
-        return request->scriptExecutionContext();
-    return nullptr;
+    return protect(m_request)->scriptExecutionContext();
 }
 
 WebCoreOpaqueRoot root(XMLHttpRequestUpload* upload)
