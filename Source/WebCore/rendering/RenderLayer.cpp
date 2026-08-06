d102c8a92c1d4475635f175e361eede57c27fffc
Carlos Alberto Lopez Perez <clopez@igalia.com>
2026-08-06 09:49:29 -0700

    Cherry-pick 318651@main (447c453338cc). https://bugs.webkit.org/show_bug.cgi?id=321111
    Unreviewed backport.
    
        [WPE][Clang] Build failing since switch to Clang due to new unused-function warnings
        https://bugs.webkit.org/show_bug.cgi?id=321111
    
        Unreviewed build fix.
    
        The EWS WPE Build queue is having a hard time since we switched the
        bots to use Clang due to new unused-function warnings.
    
        Fix that by making layersLogEnabled() and compositingLogEnabledRenderLayer()
        only defined when ENABLE(TREE_DEBUGGING) because all the callers of that
        functions are guarded behind this.
    
        * Source/WebCore/rendering/RenderLayer.cpp:
        * Source/WebCore/rendering/RenderLayerCompositor.cpp:
    
        Canonical link: https://commits.webkit.org/318651@main
    
    Canonical link: https://commits.webkit.org/305877.1053@webkitglib/2.52

--- Source/WebCore/rendering/RenderLayer.cpp
+++ Source/WebCore/rendering/RenderLayer.cpp
@@ -1088,7 +1088,7 @@ void RenderLayer::willUpdateLayerPositions()
         markers->invalidateRectsForAllMarkers();
 }
 
-#if !LOG_DISABLED || ENABLE(TREE_DEBUGGING)
+#if ENABLE(TREE_DEBUGGING)
 static inline bool compositingLogEnabledRenderLayer()
 {
     return LogCompositing.state == WTFLogChannelState::On;
