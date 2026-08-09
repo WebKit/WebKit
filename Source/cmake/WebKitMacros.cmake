97b45927eca5408ac86edc492f6116af436bd3d4
Fujii Hironori <fujii@igalia.com>
2026-08-09 13:57:35 -0700

    Cherry-pick 318630@main (4bfeff8c67ad). https://bugs.webkit.org/show_bug.cgi?id=321081
        [CMake 4.4][WPE] _WEBKIT_TARGET_LINK_FRAMEWORK reports "Unknown arguments specified" error
        https://bugs.webkit.org/show_bug.cgi?id=321081
    
        Reviewed by Adrian Perez de Castro.
    
        CMake 4.4 reports the following error for WPE.
    
        > -- Using platform-specific CMakeLists: /run/build/webkitwpe/Source/JavaScriptCore/shell/PlatformWPE.cmake
        > CMake Error at Source/cmake/WebKitMacros.cmake:554 (if):
        >   if given arguments:
        >
        >     "(" "NOT" "_linked_into" ")" "OR" "(" "JavaScriptCore" "STREQUAL" ")" "OR" "(" "NOT" "IN_LIST" "jsc_FRAMEWORKS" ")"
        >
        >   Unknown arguments specified
        > Call Stack (most recent call first):
        >   Source/cmake/WebKitMacros.cmake:756 (_WEBKIT_TARGET_LINK_FRAMEWORK)
        >   Source/JavaScriptCore/shell/CMakeLists.txt:126 (WEBKIT_EXECUTABLE)
    
        Replaced `${_linked_into}` with `_linked_into`.
    
        * Source/cmake/WebKitMacros.cmake(_WEBKIT_TARGET_LINK_FRAMEWORK):
    
        Canonical link: https://commits.webkit.org/318630@main
    
    Canonical link: https://commits.webkit.org/305877.1072@webkitglib/2.52

--- Source/cmake/WebKitMacros.cmake
+++ Source/cmake/WebKitMacros.cmake
@@ -308,7 +308,7 @@ macro(_WEBKIT_TARGET_LINK_FRAMEWORK _target)
         get_property(_linked_into GLOBAL PROPERTY ${framework}_LINKED_INTO)
 
         # See if the target is linking a framework that the specified framework is already linked into
-        if ((NOT _linked_into) OR (${framework} STREQUAL ${_linked_into}) OR (NOT ${_linked_into} IN_LIST ${_target}_FRAMEWORKS))
+        if ((NOT _linked_into) OR (framework STREQUAL _linked_into) OR (NOT _linked_into IN_LIST ${_target}_FRAMEWORKS))
             list(APPEND ${_target}_PRIVATE_LIBRARIES WebKit::${framework})
 
             # The WebKit:: alias targets do not propagate OBJECT libraries so the
