26695efdefc6076049ec50713a2c4e54b90b4d47
Matthew Finkel <m_finkel@apple.com>
2026-08-12 15:36:43 -0400

    Cherry-pick ca088424bb54. rdar://184669124
        [cocoa] WebTransport is not marking connections
        https://bugs.webkit.org/show_bug.cgi?id=321065
        rdar://184076013
    
        Reviewed by Rupin Mittal and Brady Eidson.
    
        WebTransport connections aren't correctly marked in two ways:
        1) correctly delegating the connection from the application
        2) marking connections to known tracking domains
    
        Therefore, the networking stack can't make informed decisions about how the
        connection should be routed. For example, in some cases, these connections
        should be proxied instead of allowing a direct connection to the server.
    
        WebRTC already correctly supports this behavior, so this patch simply leverages
        those existing helper functions. I adjusted the LibWebRTC guard so it only
        protects the WebRTC-specific behavior.
    
        * Source/WebKit/NetworkProcess/webrtc/NetworkRTCUtilitiesCocoa.h:
        * Source/WebKit/NetworkProcess/webrtc/NetworkRTCUtilitiesCocoa.mm:
        (WebKit::setNWParametersTrackerOptions):
        * Source/WebKit/NetworkProcess/webtransport/cocoa/NetworkTransportSessionCocoa.mm:
        (WebKit::createParameters):
    
        Identifier: 316606.331@safari-7625-branch
    
    Canonical link: https://commits.webkit.org/305413.1135@safari-7624.5.1.11-branch

--- Source/WebKit/NetworkProcess/webtransport/cocoa/NetworkTransportSessionCocoa.mm
+++ Source/WebKit/NetworkProcess/webtransport/cocoa/NetworkTransportSessionCocoa.mm
@@ -31,6 +31,7 @@
 #import "MessageSenderInlines.h"
 #import "NetworkConnectionToWebProcess.h"
 #import "NetworkProcess.h"
+#import "NetworkRTCUtilitiesCocoa.h"
 #import "NetworkSessionCocoa.h"
 #import "NetworkTransportStream.h"
 #import "WebTransportSessionMessages.h"
@@ -40,6 +41,7 @@
 #import <WebCore/Exception.h>
 #import <WebCore/ExceptionCode.h>
 #import <WebCore/RFC8941.h>
+#import <WebCore/RegistrableDomain.h>
 #import <WebCore/WebTransportConnectionInfo.h>
 #import <WebCore/WebTransportConnectionStats.h>
 #import <WebCore/WebTransportReceiveStreamStats.h>
@@ -253,7 +255,12 @@ static RetainPtr<nw_parameters_t> createParameters(NetworkConnectionToWebProcess
 
     auto configureTCP = options.requireUnreliable ? NW_PARAMETERS_DISABLE_PROTOCOL : NW_PARAMETERS_DEFAULT_CONFIGURATION;
 
-    return adoptNS(nw_parameters_create_webtransport_http(configureWebTransport, configureTLS, configureQUIC, configureTCP));
+    RetainPtr parameters = adoptNS(nw_parameters_create_webtransport_http(configureWebTransport, configureTLS, configureQUIC, configureTCP));
+    setNWParametersApplicationIdentifiers(parameters.get(), connectionToWebProcess.networkProcess().uiProcessBundleIdentifier().utf8().data(), connectionToWebProcess.networkProcess().sourceApplicationAuditToken(), emptyString());
+    bool isTracker = isKnownTracker(WebCore::RegistrableDomain { url });
+    bool isFirstParty = WebCore::RegistrableDomain { clientOrigin.clientOrigin } == WebCore::RegistrableDomain { clientOrigin.topOrigin };
+    setNWParametersTrackerOptions(parameters.get(), false, isFirstParty, isTracker, IsRTC::No);
+    return parameters;
 }
 #endif // HAVE(WEB_TRANSPORT)
 
