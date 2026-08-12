d72bc6e85623bfb052307b0b5ea167ebe91889e1
Adrian Taylor <adrian_taylor@apple.com>
2026-08-12 14:25:38 +0100

    Force-enable Swift on Windows [DO NOT MERGE]
    For testing purposes on EWS, force-enable our Swift features on Windows.
    
    Reviewed by NOBODY (OOPS!).
    
    Explanation of why this fixes the bug (OOPS!).
    
    * Source/WTF/wtf/MachSendRightAnnotated.h:
    * Source/cmake/OptionsWin.cmake:
    * Tools/Scripts/swift/swiftc-wrapper.py:
    (quote_response_file_token):
    (write_response_file):
    (main):

--- Source/cmake/OptionsWin.cmake
+++ Source/cmake/OptionsWin.cmake
@@ -110,11 +110,9 @@ WEBKIT_OPTION_DEFAULT_PORT_VALUE(ENABLE_WEBDRIVER PRIVATE ON)
 
 # No support planned
 
-# The Swift prototype features default on for any Clang host with a new-enough
-# swiftc, which now includes this port. They can't build here yet; Windows
-# Swift support is in progress.
-WEBKIT_OPTION_DEFAULT_PORT_VALUE(ENABLE_BACK_FORWARD_LIST_SWIFT PRIVATE OFF)
-WEBKIT_OPTION_DEFAULT_PORT_VALUE(ENABLE_SWIFT_DEMO_URI_SCHEME PRIVATE OFF)
+# Force Swift features on just for testing purposes - do not merge
+WEBKIT_OPTION_DEFAULT_PORT_VALUE(ENABLE_BACK_FORWARD_LIST_SWIFT PRIVATE ON)
+WEBKIT_OPTION_DEFAULT_PORT_VALUE(ENABLE_SWIFT_DEMO_URI_SCHEME PRIVATE ON)
 
 # WebDriver options
 SET_AND_EXPOSE_TO_BUILD(ENABLE_WEBDRIVER_KEYBOARD_INTERACTIONS ON)
