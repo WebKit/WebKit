750cd66f31bc665a4cc3088d339a85e05bcb787e
Antti Koivisto <antti@apple.com>
2026-08-12 06:49:09 -0700

    [css-mixins-1] Function arguments invoking the function should not create a cycle https://bugs.webkit.org/show_bug.cgi?id=321592 rdar://184711017
    Reviewed by Alan Baradlay.
    
    * LayoutTests/imported/w3c/web-platform-tests/css/css-mixins/dashed-function-cycles-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/css/css-mixins/dashed-function-cycles.html:
    * Source/WebCore/style/StyleSubstitutionResolver.cpp:
    (WebCore::Style::SubstitutionResolver::substituteDashedFunction):
    
    Move cycle detection after argument substitution.
    
    Canonical link: https://commits.webkit.org/319034@main

--- Source/WebCore/style/StyleSubstitutionResolver.cpp
+++ Source/WebCore/style/StyleSubstitutionResolver.cpp
@@ -488,14 +488,10 @@ bool SubstitutionResolver::substituteDashedFunction(StringView functionName, CSS
 
     auto& [customFunction, foundScopeOrdinal] = *resolved;
 
-    auto guard = m_styleBuilder.state().guardSubstitutionContext({ SubstitutionContext::Type::Function, scopedFunctionName.name, foundScopeOrdinal });
-
-    if (guard.isCyclicContext())
-        return false;
-
     auto& parameters = customFunction->parameters;
 
-    // Parse and substitute arguments.
+    // Arguments are substituted before the context is guarded, so an argument that calls the same
+    // function is not a cycle. https://drafts.csswg.org/css-mixins/#replace-a-dashed-function
     auto substitutedArguments = [&] -> std::optional<Vector<Vector<CSSParserToken>>> {
         Vector<Vector<CSSParserToken>> result;
         for (unsigned i = 0; !range.atEnd(); ++i) {
@@ -513,6 +509,13 @@ bool SubstitutionResolver::substituteDashedFunction(StringView functionName, CSS
     }();
 
     if (!substitutedArguments)
+        return false;
+
+    // Parameter default values and the body are resolved inside the guard, so a function that reaches
+    // itself through either is still cyclic.
+    auto guard = m_styleBuilder.state().guardSubstitutionContext({ SubstitutionContext::Type::Function, scopedFunctionName.name, foundScopeOrdinal });
+
+    if (guard.isCyclicContext())
         return false;
 
     // "Let registrations be an initially empty set of custom property registrations."
