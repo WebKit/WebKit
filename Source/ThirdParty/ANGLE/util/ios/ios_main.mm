db35c2d759d938c8eec9af1c4069e43bdecfba0d
Jon Butler <jonbutler@apple.com>
2026-08-18 06:30:23 -0700

    Update the ANGLEEnd2EndTests binary plist with the required UIApplicationSceneManifest https://bugs.webkit.org/show_bug.cgi?id=320996
    Reviewed by Kimmo Kinnunen.
    
    Recent Apple SDK versions introduced a requirement for the UIApplicationSceneManifest key
    in the app's plist to avoid crashing on launch. This change adds this plist key, as well
    as changing the startup code to use the new scene-based design.
    
    * Source/ThirdParty/ANGLE/WebKit/ANGLEEnd2EndTestsAppInfo.plist:
    * Source/ThirdParty/ANGLE/util/ios/ios_main.mm:
    (-[AngleUtilSceneDelegate scene:willConnectToSession:options:]):
    (-[AngleUtilAppDelegate application:didFinishLaunchingWithOptions:]):
    (-[AngleUtilAppDelegate runMain]): Deleted.
    
    Canonical link: https://commits.webkit.org/319366@main

--- Source/ThirdParty/ANGLE/util/ios/ios_main.mm
+++ Source/ThirdParty/ANGLE/util/ios/ios_main.mm
@@ -19,11 +19,19 @@ int main(int argc, char *_Nullable *_Nullable argv);
 
 @interface AngleUtilAppDelegate : UIResponder <UIApplicationDelegate>
 
+// IOSWindow.mm reaches the test's layer through the application delegate's window, so the scene
+// delegate publishes its window here once the scene connects.
 @property(nullable, nonatomic, strong) UIWindow *window;
 
 @end
 
-@implementation AngleUtilAppDelegate
+@interface AngleUtilSceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property(nullable, nonatomic, strong) UIWindow *window;
+
+@end
+
+@implementation AngleUtilSceneDelegate
 
 @synthesize window;
 
@@ -33,19 +41,32 @@ int main(int argc, char *_Nullable *_Nullable argv);
     exit(main(original_argc, original_argv));
 }
 
-- (BOOL)application:(UIApplication *_Nonnull)application
-    didFinishLaunchingWithOptions:(NSDictionary *_Nullable)launchOptions
+- (void)scene:(UIScene *_Nonnull)scene
+    willConnectToSession:(UISceneSession *_Nonnull)session
+                 options:(UISceneConnectionOptions *_Nonnull)connectionOptions
 {
-    self.window                    = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window = [[UIWindow alloc] initWithWindowScene:(UIWindowScene *)scene];
     self.window.rootViewController = [[UIViewController alloc] initWithNibName:nil bundle:nil];
     [self.window makeKeyAndVisible];
-    // We need to return from this function before the app finishes launching, so call main in a
+    [UIApplication sharedApplication].delegate.window = self.window;
+    // We need to return from this function before the scene finishes connecting, so call main in a
     // timer callback afterward.
     [NSTimer scheduledTimerWithTimeInterval:0
                                      target:self
                                    selector:@selector(runMain)
                                    userInfo:nil
                                     repeats:NO];
+}
+
+@end
+
+@implementation AngleUtilAppDelegate
+
+@synthesize window;
+
+- (BOOL)application:(UIApplication *_Nonnull)application
+    didFinishLaunchingWithOptions:(NSDictionary *_Nullable)launchOptions
+{
     return YES;
 }
 
