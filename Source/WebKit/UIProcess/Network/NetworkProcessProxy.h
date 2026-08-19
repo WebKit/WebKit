WebKit/WebKit

commit 8c4517f0f7c512fe8ace111510e3068f745ebcd2

Use CertificateInfo from network process instead of web content process rdar://174079071
Reviewed by Chris Dumez.

Using the CertificateInfo from the web content process allows a compromised
web content process to tamper with the certificates and their properties.
This PR makes us use the CertificateInfo from the network process instead.
We send the CertificateInfo when we receive a response.  Thereafter, when the
first HTTP body data is received, it is sent to the web content process which
tells the UI process that the load was committed.  Since the messages coming
from the network process directly to the UI process are on a separate queue
at the kernel level, there is no guarantee of the order messages are received
in.  In the very unlikely case that the certificate info has not yet arrived,
process the incoming messages from the network process to find the certificate
info.  To avoid race conditions between multiple navigations committing in
quick succession, we check the host and port for correctness before using the
certificate info.  This allows a compromised web content process to either
match or not match, which limits its ability to just using or not using the
certificate info from the network process.

To fix the regression from the original commit, I include rdar://177670634
in this upstreaming too.

Test: Tools/TestWebKitAPI/Tests/WebKit/WKWebView/WKWebViewServerTrustKVC.mm

* Source/WebKit/NetworkProcess/NetworkResourceLoader.cpp:
(WebKit::NetworkResourceLoader::didReceiveMainResourceResponse):
* Source/WebKit/UIProcess/API/Cocoa/WKWebView.mm:
(-[WKWebView _certificateChain]):
(-[WKWebView certificateChain]):
* Source/WebKit/UIProcess/Network/NetworkProcessProxy.cpp:
(WebKit::NetworkProcessProxy::receivedMainResourceResponseWithCertificateInfo):
* Source/WebKit/UIProcess/Network/NetworkProcessProxy.h:
* Source/WebKit/UIProcess/Network/NetworkProcessProxy.messages.in:
* Source/WebKit/UIProcess/ProvisionalPageProxy.cpp:

Source/WebKit/UIProcess/Network/NetworkProcessProxy.h
@@ -71,6 +71,7 @@ class SessionID;
 
 namespace WebCore {
 class AuthenticationChallenge;
+class CertificateInfo;
 class SharedBuffer;
 class ProtectionSpace;
 class ResourceRequest;
@@ -343,6 +344,8 @@ public:
     void openWindowFromServiceWorker(PAL::SessionID, const String& urlString, const WebCore::SecurityOriginData& serviceWorkerOrigin, CompletionHandler<void(std::optional<WebCore::PageIdentifier>&&)>&&);
 
     void navigateServiceWorkerClient(WebCore::FrameIdentifier, WebCore::ScriptExecutionContextIdentifier, const URL&, CompletionHandler<void(std::optional<WebCore::PageIdentifier>, std::optional<WebCore::FrameIdentifier>)>&&);
+
+    void receivedMainResourceResponseWithCertificateInfo(WebCore::FrameIdentifier, String&& hostAndPort, WebCore::CertificateInfo&&);
 
     void cookiesDidChange(PAL::SessionID);
 

Source/WebKit/UIProcess/ProvisionalPageProxy.cpp
@@ -485,7 +485,7 @@ void ProvisionalPageProxy::didFailProvisionalLoadForFrame(FrameInfoData&& frameI
     page->didFailProvisionalLoadForFrameShared(protect(process()), *frame, WTF::move(frameInfo), WTF::move(request), navigationID, WTF::move(provisionalURL), WTF::move(error), willContinueLoading, userData, willInternallyHandleFailure); // May delete |this|.
 }
 
-void ProvisionalPageProxy::didCommitLoadForFrame(IPC::Connection& connection, FrameIdentifier frameID, FrameInfoData&& frameInfo, ResourceRequest&& request, std::optional<WebCore::NavigationIdentifier> navigationID, String&& mimeType, bool frameHasCustomContentProvider, FrameLoadType frameLoadType, const CertificateInfo& certificateInfo, bool usedLegacyTLS, bool privateRelayed, String&& proxyName, WebCore::ResourceResponseSource source, bool containsPluginDocument, HasInsecureContent hasInsecureContent, MouseEventPolicy mouseEventPolicy, DocumentSecurityPolicy&& documentSecurityPolicy, HashSet<WebCore::SecurityOriginData>&& cspOriginsThatUpgradeInsecureNavigations, const UserData& userData, RestoredFromBackForwardCache restoredFromBackForwardCache, RefPtr<FrameState>&& redirectReplaceFrameState)
+void ProvisionalPageProxy::didCommitLoadForFrame(IPC::Connection& connection, FrameIdentifier frameID, FrameInfoData&& frameInfo, ResourceRequest&& request, std::optional<WebCore::NavigationIdentifier> navigationID, String&& mimeType, bool frameHasCustomContentProvider, FrameLoadType frameLoadType, bool usedLegacyTLS, bool privateRelayed, String&& proxyName, WebCore::ResourceResponseSource source, bool containsPluginDocument, HasInsecureContent hasInsecureContent, MouseEventPolicy mouseEventPolicy, DocumentSecurityPolicy&& documentSecurityPolicy, HashSet<WebCore::SecurityOriginData>&& cspOriginsThatUpgradeInsecureNavigations, const UserData& userData, RestoredFromBackForwardCache restoredFromBackForwardCache, RefPtr<FrameState>&& redirectReplaceFrameState)
 {
     if (!validateInput(frameID, navigationID))
         return;
@@ -511,7 +511,7 @@ void ProvisionalPageProxy::didCommitLoadForFrame(IPC::Connection& connection, Fr
     m_messageReceiverRegistration.stopReceivingMessages();
 
     m_wasCommitted = true;
-    page->commitProvisionalPage(connection, frameID, WTF::move(frameInfo), WTF::move(request), navigationID, WTF::move(mimeType), frameHasCustomContentProvider, frameLoadType, certificateInfo, usedLegacyTLS, privateRelayed, WTF::move(proxyName), source, containsPluginDocument, hasInsecureContent, mouseEventPolicy, WTF::move(documentSecurityPolicy), WTF::move(cspOriginsThatUpgradeInsecureNavigations), userData, restoredFromBackForwardCache, WTF::move(redirectReplaceFrameState)); // Will delete |this|.
+    page->commitProvisionalPage(connection, frameID, WTF::move(frameInfo), WTF::move(request), navigationID, WTF::move(mimeType), frameHasCustomContentProvider, frameLoadType, usedLegacyTLS, privateRelayed, WTF::move(proxyName), source, containsPluginDocument, hasInsecureContent, mouseEventPolicy, WTF::move(documentSecurityPolicy), WTF::move(cspOriginsThatUpgradeInsecureNavigations), userData, restoredFromBackForwardCache, WTF::move(redirectReplaceFrameState)); // Will delete |this|.
 }
 
 void ProvisionalPageProxy::didNavigateWithNavigationData(const WebNavigationDataStore& store, FrameIdentifier frameID)

Source/WebKit/UIProcess/WebFrameProxy.cpp
@@ -41,6 +41,8 @@
 #include "Logging.h"
 #include "MessageSenderInlines.h"
 #include "NetworkProcessMessages.h"
+#include "NetworkProcessProxyMessages.h"
+#include "PageLoadState.h"
 #include "ProvisionalFrameCreationParameters.h"
 #include "ProvisionalFrameProxy.h"
 #include "ProvisionalPageProxy.h"
@@ -58,6 +60,7 @@
 #include "WebProcessPool.h"
 #include "WebsiteDataStore.h"
 #include "WebsitePoliciesData.h"
+#include <WebCore/CertificateInfo.h>
 #include <WebCore/DocumentSyncData.h>
 #include <WebCore/FloatRect.h>
 #include <WebCore/FocusController.h>
@@ -340,7 +343,7 @@ void WebFrameProxy::didFailProvisionalLoad()
         m_navigateCallback({ }, { });
 }
 
-void WebFrameProxy::didCommitLoad(const String& contentType, const WebCore::CertificateInfo& certificateInfo, bool containsPluginDocument, DocumentSecurityPolicy&& documentSecurityPolicy, HashSet<WebCore::SecurityOriginData>&& cspOriginsThatUpgradeInsecureNavigations)
+void WebFrameProxy::didCommitLoad(const String& contentType, bool containsPluginDocument, DocumentSecurityPolicy&& documentSecurityPolicy, HashSet<WebCore::SecurityOriginData>&& cspOriginsThatUpgradeInsecureNavigations)
 {
     m_frameLoadState.didCommitLoad();
 
@@ -349,7 +352,6 @@ void WebFrameProxy::didCommitLoad(const String& contentType, const WebCore::Cert
 
     m_title = String();
     m_MIMEType = contentType;
-    m_certificateInfo = certificateInfo;
     m_containsPluginDocument = containsPluginDocument;
     m_documentSecurityPolicy = WTF::move(documentSecurityPolicy);
     m_cspOriginsThatUpgradeInsecureNavigations = WTF::move(cspOriginsThatUpgradeInsecureNavigations);
@@ -618,7 +620,7 @@ void WebFrameProxy::prepareForProvisionalLoadInProcess(WebProcessProxy& process,
         continuation();
 }
 
-void WebFrameProxy::commitProvisionalFrame(IPC::Connection& connection, FrameIdentifier frameID, FrameInfoData&& frameInfo, ResourceRequest&& request, std::optional<WebCore::NavigationIdentifier> navigationID, String&& mimeType, bool frameHasCustomContentProvider, FrameLoadType frameLoadType, const CertificateInfo& certificateInfo, bool usedLegacyTLS, bool privateRelayed, String&& proxyName, WebCore::ResourceResponseSource source, bool containsPluginDocument, HasInsecureContent hasInsecureContent, MouseEventPolicy mouseEventPolicy, DocumentSecurityPolicy&& documentSecurityPolicy, HashSet<WebCore::SecurityOriginData>&& cspOriginsThatUpgradeInsecureNavigations, const UserData& userData, RestoredFromBackForwardCache restoredFromBackForwardCache, RefPtr<FrameState>&& redirectReplaceFrameState)
+void WebFrameProxy::commitProvisionalFrame(IPC::Connection& connection, FrameIdentifier frameID, FrameInfoData&& frameInfo, ResourceRequest&& request, std::optional<WebCore::NavigationIdentifier> navigationID, String&& mimeType, bool frameHasCustomContentProvider, FrameLoadType frameLoadType, bool usedLegacyTLS, bool privateRelayed, String&& proxyName, WebCore::ResourceResponseSource source, bool containsPluginDocument, HasInsecureContent hasInsecureContent, MouseEventPolicy mouseEventPolicy, DocumentSecurityPolicy&& documentSecurityPolicy, HashSet<WebCore::SecurityOriginData>&& cspOriginsThatUpgradeInsecureNavigations, const UserData& userData, RestoredFromBackForwardCache restoredFromBackForwardCache, RefPtr<FrameState>&& redirectReplaceFrameState)
 {
     ASSERT(m_page);
     if (m_provisionalFrame) {
@@ -635,7 +637,7 @@ void WebFrameProxy::commitProvisionalFrame(IPC::Connection& connection, FrameIde
             page->inspectorController().didCommitProvisionalFrame(*this, oldProcessID, oldPageID, newProcessID);
     }
 
-    protect(page())->didCommitLoadForFrame(connection, frameID, WTF::move(frameInfo), WTF::move(request), navigationID, WTF::move(mimeType), frameHasCustomContentProvider, frameLoadType, certificateInfo, usedLegacyTLS, privateRelayed, WTF::move(proxyName), source, containsPluginDocument, hasInsecureContent, mouseEventPolicy, WTF::move(documentSecurityPolicy), WTF::move(cspOriginsThatUpgradeInsecureNavigations), userData, restoredFromBackForwardCache, WTF::move(redirectReplaceFrameState));
+    protect(page())->didCommitLoadForFrame(connection, frameID, WTF::move(frameInfo), WTF::move(request), navigationID, WTF::move(mimeType), frameHasCustomContentProvider, frameLoadType, usedLegacyTLS, privateRelayed, WTF::move(proxyName), source, containsPluginDocument, hasInsecureContent, mouseEventPolicy, WTF::move(documentSecurityPolicy), WTF::move(cspOriginsThatUpgradeInsecureNavigations), userData, restoredFromBackForwardCache, WTF::move(redirectReplaceFrameState));
 }
 
 void WebFrameProxy::getFrameInfo(CompletionHandler<void(std::optional<FrameInfoData>&&)>&& completionHandler)
@@ -1225,6 +1227,58 @@ void WebFrameProxy::updateDocumentSecurityOrigin(WebFrameProxy* creator, ForInit
     }
 
     m_documentSecurityOrigin = SecurityOrigin::create(url());
+}
+
+void WebFrameProxy::waitForCertificateInfoFromNetworkProcess(const String& hostAndPort)
+{
+    RefPtr page = m_page.get();
+    if (!page)
+        return;
+    RefPtr networkProcess = page->websiteDataStore().networkProcessIfExists();
+    if (!networkProcess)
+        return;
+    RefPtr connection = networkProcess->connection();
+    if (!connection)
+        return;
+    // The message should already be queued at this point. Drain ReceivedMainResourceResponseWithCertificateInfo
+    // entries until our frame's hostAndPort appears in the map, since the singleton receiver no longer
+    // filters by frame destination.
+    while (!m_hostAndPortToCertificateInfo.contains(hostAndPort)) {
+        if (connection->waitForAndDispatchImmediately<Messages::NetworkProcessProxy::ReceivedMainResourceResponseWithCertificateInfo>(0, 0_s) != IPC::Error::NoError) {
+            RELEASE_LOG_ERROR(Network, "Unexpectedly missing certificate info from IPC");
+            return;
+        }
+    }
+}
+
+void WebFrameProxy::commitCertificateInfo(const URL& url)
+{
+    RefPtr page = m_page.get();
+    if (!page)
+        return;
+
+    String hostAndPort = url.hostAndPort();
+    CertificateInfo certificateInfo;
+    if (decltype(m_hostAndPortToCertificateInfo)::isValidKey(hostAndPort)) {
+        certificateInfo = m_hostAndPortToCertificateInfo.get(hostAndPort);
+        if (certificateInfo.isEmpty() && url.protocolIsSecure()) {
+            waitForCertificateInfoFromNetworkProcess(hostAndPort);
+            certificateInfo = m_hostAndPortToCertificateInfo.get(hostAndPort);
+            if (certificateInfo.isEmpty())
+            RELEASE_LOG_ERROR(Network, "Unexpectedly missing certificate info");
+        }
+    }
+
+    m_certificateInfo = WTF::move(certificateInfo);
+}
+
+void WebFrameProxy::receivedMainResourceResponseWithCertificateInfo(String&& hostAndPort, WebCore::CertificateInfo&& certificateInfo)
+{
+    // FIXME: This has no corresponding remove call. If we know a host is not being used by the back/forward
+    // cache and is not being navigated to, we could clean up this map. Since we don't re-use main frames for
+    // multiple hosts, this would only affect iframes that navigate to many hosts.
+    if (decltype(m_hostAndPortToCertificateInfo)::isValidKey(hostAndPort))
+        m_hostAndPortToCertificateInfo.set(WTF::move(hostAndPort), WTF::move(certificateInfo));
 }
 
 } // namespace WebKit

Source/WebKit/UIProcess/WebFrameProxy.h
@@ -184,6 +184,9 @@ public:
 
     const WebCore::CertificateInfo& certificateInfo() const LIFETIME_BOUND { return m_certificateInfo; }
 
+    void commitCertificateInfo(const URL&);
+    void receivedMainResourceResponseWithCertificateInfo(String&&, WebCore::CertificateInfo&&);
+
     bool canProvideSource() const;
 
     bool isSameOriginAs(const WebFrameProxy&) const;
@@ -201,7 +204,7 @@ public:
     void didExplicitOpen(URL&&, String&& mimeType);
     void didReceiveServerRedirectForProvisionalLoad(URL&&);
     void didFailProvisionalLoad();
-    void didCommitLoad(const String& contentType, const WebCore::CertificateInfo&, bool containsPluginDocument, WebCore::DocumentSecurityPolicy&&, HashSet<WebCore::SecurityOriginData>&& cspOriginsThatUpgradeInsecureNavigations);
+    void didCommitLoad(const String& contentType, bool containsPluginDocument, WebCore::DocumentSecurityPolicy&&, HashSet<WebCore::SecurityOriginData>&& cspOriginsThatUpgradeInsecureNavigations);
     void didFinishLoad();
     void didFailLoad();
     void didSameDocumentNavigation(URL&&); // eg. anchor navigation, session state change.
@@ -230,7 +233,7 @@ public:
     ProcessID NODELETE processID() const;
     void prepareForProvisionalLoadInProcess(WebProcessProxy&, API::Navigation&, BrowsingContextGroup&, std::optional<WebCore::SecurityOriginData>, CompletionHandler<void(std::optional<WebCore::PageIdentifier>)>&&);
 
-    void commitProvisionalFrame(IPC::Connection&, WebCore::FrameIdentifier, FrameInfoData&&, WebCore::ResourceRequest&&, std::optional<WebCore::NavigationIdentifier>, String&& mimeType, bool frameHasCustomContentProvider, WebCore::FrameLoadType, const WebCore::CertificateInfo&, bool usedLegacyTLS, bool privateRelayed, String&& proxyName, WebCore::ResourceResponseSource, bool containsPluginDocument, WebCore::HasInsecureContent, WebCore::MouseEventPolicy, WebCore::DocumentSecurityPolicy&&, HashSet<WebCore::SecurityOriginData>&& cspOriginsThatUpgradeInsecureNavigations, const UserData&, WebCore::RestoredFromBackForwardCache, RefPtr<FrameState>&& redirectReplaceFrameState);
+    void commitProvisionalFrame(IPC::Connection&, WebCore::FrameIdentifier, FrameInfoData&&, WebCore::ResourceRequest&&, std::optional<WebCore::NavigationIdentifier>, String&& mimeType, bool frameHasCustomContentProvider, WebCore::FrameLoadType, bool usedLegacyTLS, bool privateRelayed, String&& proxyName, WebCore::ResourceResponseSource, bool containsPluginDocument, WebCore::HasInsecureContent, WebCore::MouseEventPolicy, WebCore::DocumentSecurityPolicy&&, HashSet<WebCore::SecurityOriginData>&& cspOriginsThatUpgradeInsecureNavigations, const UserData&, WebCore::RestoredFromBackForwardCache, RefPtr<FrameState>&& redirectReplaceFrameState);
 
     void getFrameTree(CompletionHandler<void(std::optional<FrameTreeNodeData>&&)>&&);
     void getFrameInfo(CompletionHandler<void(std::optional<FrameInfoData>&&)>&&);
@@ -336,6 +339,7 @@ private:
     WebFrameProxy(WebPageProxy&, FrameProcess&, WebCore::FrameIdentifier, WebCore::SandboxFlags, WebCore::ReferrerPolicy, WebCore::ScrollbarMode, WebFrameProxy*, WebFrameProxy*, IsMainFrame, std::optional<URL>&&);
 
     std::optional<SharedPreferencesForWebProcess> NODELETE sharedPreferencesForWebProcess() const;
+    void waitForCertificateInfoFromNetworkProcess(const String& hostAndPort);
 
     std::optional<WebCore::PageIdentifier> NODELETE pageIdentifier() const;
 
@@ -360,6 +364,7 @@ private:
     String m_frameName;
     bool m_containsPluginDocument { false };
     WebCore::CertificateInfo m_certificateInfo;
+    HashMap<String, WebCore::CertificateInfo> m_hostAndPortToCertificateInfo;
     RefPtr<WebFramePolicyListenerProxy> m_activeListener;
     WebCore::FrameIdentifier m_frameID;
     ListHashSet<Ref<WebFrameProxy>> m_childFrames;

commit d81a5452943f1083f1f939685afb47a8dc7957c2

Cherry-pick 318495@main (702c6b7899c5). https://bugs.webkit.org/show_bug.cgi?id=320765
    Fix imported/w3c/web-platform-tests/html/cross-origin-opener-policy/resource-popup.https.html to have correct certificate info
    https://bugs.webkit.org/show_bug.cgi?id=320765
    rdar://183768315

    Reviewed by Chris Dumez.

    317375@main had a few issues.

    It introduced a bool to prevent waitForAndDispatchImmediately re-entry,
    but at the cost of sometimes not having the correct certificate info.
    The real bug was that after a COOP-initiated process swap, we weren't
    taking the certificate info received by the old main frame and giving
    it to the new main frame.

    I added a unit test that times out waiting for a server trust that
    never appears before this fix, showing clearly the functional regression
    from 316854@main that this PR fixes and 317375@main did not.

    * Source/WebKit/UIProcess/Network/NetworkProcessProxy.h:
    * Source/WebKit/UIProcess/ProvisionalPageProxy.cpp:
    (WebKit::ProvisionalPageProxy::ProvisionalPageProxy):
    * Source/WebKit/UIProcess/WebFrameProxy.cpp:
    (WebKit::WebFrameProxy::certificateInfoFromNetworkProcess const):
    (WebKit::WebFrameProxy::commitCertificateInfo):
    (WebKit::WebFrameProxy::copyCertificateInfoForProcessSwapOnNavigationResponse):
    (WebKit::WebFrameProxy::waitForCertificateInfoFromNetworkProcess):
    * Source/WebKit/UIProcess/WebFrameProxy.h:
    * Tools/TestWebKitAPI/Tests/WebKit/WKWebView/WKWebViewServerTrustKVC.mm:
    (TEST(WKWebView, ServerTrustKVCWithCOOP)):

    Canonical link: https://commits.webkit.org/318495@main

Canonical link: https://commits.webkit.org/317695.109@webkitglib/2.54

Source/WebKit/UIProcess/Network/NetworkProcessProxy.h
@@ -369,9 +369,6 @@ public:
 
     void flushNetworkProcessIPC(CompletionHandler<void()>&&);
 
-    bool isWaitingForCertificateInfo() const { return m_isWaitingForCertificateInfo; }
-    void setIsWaitingForCertificateInfo(bool waiting) { m_isWaitingForCertificateInfo = waiting; }
-
 private:
     explicit NetworkProcessProxy();
 
@@ -506,7 +503,6 @@ private:
     RetainPtr<id> m_backgroundObserver;
     RetainPtr<id> m_foregroundObserver;
 #endif
-    bool m_isWaitingForCertificateInfo { false };
 };
 
 } // namespace WebKit

Source/WebKit/UIProcess/ProvisionalPageProxy.cpp
@@ -143,6 +143,9 @@ ProvisionalPageProxy::ProvisionalPageProxy(WebPageProxy& page, Ref<FrameProcess>
         previousMainFrame->transferNavigationCallbackToFrame(mainFrame);
     }
 
+    if (isProcessSwappingOnNavigationResponse && previousMainFrame)
+        protect(m_mainFrame)->copyCertificateInfoForProcessSwapOnNavigationResponse(m_provisionalLoadURL, *previousMainFrame);
+
     // Normally, notification of a server redirect comes from the WebContent process.
     // If we are process swapping in response to a server redirect then that notification will not come from the new WebContent process.
     // In this case we have the UIProcess synthesize the redirect notification at the appropriate time.

Source/WebKit/UIProcess/WebFrameProxy.cpp
@@ -1230,50 +1230,39 @@ void WebFrameProxy::updateDocumentSecurityOrigin(WebFrameProxy* creator, ForInit
     m_documentSecurityOrigin = SecurityOrigin::create(url());
 }
 
-void WebFrameProxy::waitForCertificateInfoFromNetworkProcess(const String& hostAndPort)
+WebCore::CertificateInfo WebFrameProxy::certificateInfoFromNetworkProcess(const URL& url) const
 {
+    String hostAndPort = url.hostAndPort();
+    if (!decltype(m_hostAndPortToCertificateInfo)::isValidKey(hostAndPort))
+        return { };
+
+    if (!url.protocolIsSecure())
+        return { };
+
+    if (CertificateInfo certificateInfo = m_hostAndPortToCertificateInfo.get(hostAndPort); !certificateInfo.isEmpty())
+        return certificateInfo;
+
     RefPtr page = m_page.get();
     if (!page)
-        return;
+        return { };
     RefPtr networkProcess = page->websiteDataStore().networkProcessIfExists();
     if (!networkProcess)
-        return;
+        return { };
     RefPtr connection = networkProcess->connection();
     if (!connection)
-        return;
+        return { };
 
-    // waitForAndDispatchImmediately dispatches messages besides the one being waited for.
-    // If another Messages::WebPageProxy::DidCommitLoadForFrame is received, we can get Error::MultipleWaitingClients.
-    // To prevent this, only be in one waitForAndDispatchImmediately call at a time per connection for this.
-    // If the next frame commit is also missing certificate info after this, then it will wait too.
-    if (networkProcess->isWaitingForCertificateInfo())
-        return;
-    networkProcess->setIsWaitingForCertificateInfo(true);
-    if (connection->waitForAndDispatchImmediately<Messages::WebFrameProxyFromNetworkProcess::ReceivedMainResourceResponseWithCertificateInfo>(frameID(), 0_s) != IPC::Error::NoError) {
-        RELEASE_LOG_ERROR(Network, "Unexpectedly missing certificate info from IPC");
-    }
-    networkProcess->setIsWaitingForCertificateInfo(false);
+    connection->waitForAndDispatchImmediately<Messages::WebFrameProxyFromNetworkProcess::ReceivedMainResourceResponseWithCertificateInfo>(frameID(), 0_s);
+
+    CertificateInfo certificateInfo = m_hostAndPortToCertificateInfo.get(hostAndPort);
+    if (certificateInfo.isEmpty())
+        RELEASE_LOG_ERROR(Network, "Unexpectedly missing certificate info");
+    return certificateInfo;
 }
 
 void WebFrameProxy::commitCertificateInfo(const URL& url)
 {
-    RefPtr page = m_page.get();
-    if (!page)
-        return;
-
-    String hostAndPort = url.hostAndPort();
-    CertificateInfo certificateInfo;
-    if (decltype(m_hostAndPortToCertificateInfo)::isValidKey(hostAndPort)) {
-        certificateInfo = m_hostAndPortToCertificateInfo.get(hostAndPort);
-        if (certificateInfo.isEmpty() && url.protocolIsSecure()) {
-            waitForCertificateInfoFromNetworkProcess(hostAndPort);
-            certificateInfo = m_hostAndPortToCertificateInfo.get(hostAndPort);
-            if (certificateInfo.isEmpty())
-                RELEASE_LOG_ERROR(Network, "Unexpectedly missing certificate info");
-        }
-    }
-
-    m_certificateInfo = WTF::move(certificateInfo);
+    m_certificateInfo = certificateInfoFromNetworkProcess(url);
 }
 
 void WebFrameProxy::receivedMainResourceResponseWithCertificateInfo(String&& hostAndPort, WebCore::CertificateInfo&& certificateInfo)
@@ -1283,6 +1272,15 @@ void WebFrameProxy::receivedMainResourceResponseWithCertificateInfo(String&& hos
     // multiple hosts, this would only affect iframes that navigate to many hosts.
     if (decltype(m_hostAndPortToCertificateInfo)::isValidKey(hostAndPort))
         m_hostAndPortToCertificateInfo.set(WTF::move(hostAndPort), WTF::move(certificateInfo));
+}
+
+void WebFrameProxy::copyCertificateInfoForProcessSwapOnNavigationResponse(const URL& url, const WebFrameProxy& oldMainFrame)
+{
+    ASSERT(isMainFrame());
+    ASSERT(oldMainFrame.isMainFrame());
+    ASSERT(m_hostAndPortToCertificateInfo.isEmpty());
+
+    m_hostAndPortToCertificateInfo.set(url.hostAndPort(), oldMainFrame.certificateInfoFromNetworkProcess(url));
 }
 
 } // namespace WebKit

Source/WebKit/UIProcess/WebFrameProxy.h
@@ -186,6 +186,7 @@ public:
 
     void commitCertificateInfo(const URL&);
     void receivedMainResourceResponseWithCertificateInfo(String&&, WebCore::CertificateInfo&&);
+    void copyCertificateInfoForProcessSwapOnNavigationResponse(const URL&, const WebFrameProxy&);
 
     bool canProvideSource() const;
 
@@ -340,7 +341,7 @@ private:
     WebFrameProxy(WebPageProxy&, FrameProcess&, WebCore::FrameIdentifier, WebCore::SandboxFlags, WebCore::ReferrerPolicy, WebCore::ScrollbarMode, WebFrameProxy*, WebFrameProxy*, IsMainFrame, std::optional<URL>&&);
 
     std::optional<SharedPreferencesForWebProcess> NODELETE sharedPreferencesForWebProcess() const;
-    void waitForCertificateInfoFromNetworkProcess(const String& hostAndPort);
+    WebCore::CertificateInfo certificateInfoFromNetworkProcess(const URL&) const;
 
     std::optional<WebCore::PageIdentifier> NODELETE pageIdentifier() const;
 

Tools/TestWebKitAPI/Tests/WebKit/WKWebView/WKWebViewServerTrustKVC.mm
@@ -29,6 +29,7 @@
 #import "Helpers/Test.h"
 #import "Helpers/cocoa/HTTPServer.h"
 #import "Helpers/cocoa/TestNavigationDelegate.h"
+#import "Helpers/cocoa/TestUIDelegate.h"
 #import <wtf/RetainPtr.h>
 
 @interface TrustObserver : NSObject
@@ -90,4 +91,39 @@ TEST(WKWebView, ServerTrustKVC)
     [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"about:blank"]]];
     [observer waitUntilServerTrustChanged];
     EXPECT_NULL([webView serverTrust]);
+}
+
+TEST(WKWebView, ServerTrustKVCWithCOOP)
+{
+    using namespace TestWebKitAPI;
+    HTTPServer server({
+        { "/path1"_s, { "hi"_s } },
+        { "/path2"_s, { { { "Cross-Origin-Opener-Policy"_s, "same-origin"_s } }, "hi"_s } }
+    }, HTTPServer::Protocol::HttpsProxy);
+
+    RetainPtr webView = adoptNS([[WKWebView alloc] initWithFrame:CGRectZero configuration:server.httpsProxyConfiguration()]);
+    RetainPtr uiDelegate = adoptNS([TestUIDelegate new]);
+    webView.get().UIDelegate = uiDelegate.get();
+    RetainPtr navigationDelegate = adoptNS([TestNavigationDelegate new]);
+    [navigationDelegate allowAnyTLSCertificate];
+    webView.get().navigationDelegate = navigationDelegate.get();
+
+    RetainPtr observer = adoptNS([TrustObserver new]);
+    __block RetainPtr<WKWebView> opened;
+    uiDelegate.get().createWebViewWithConfiguration = ^(WKWebViewConfiguration *configuration, WKNavigationAction *, WKWindowFeatures *) {
+        opened = adoptNS([[WKWebView alloc] initWithFrame:CGRectZero configuration:configuration]);
+        opened.get().navigationDelegate = navigationDelegate.get();
+        [opened addObserver:observer.get() forKeyPath:@"serverTrust" options:NSKeyValueObservingOptionNew context:nil];
+        return opened.get();
+    };
+    [webView loadURL:[NSURL URLWithString:@"https://webkit.org/path1"]];
+    [navigationDelegate waitForDidFinishNavigation];
+    [webView evaluateJavaScript:@"window.open('https://webkit.org/path2')" completionHandler:nil];
+    [observer waitUntilServerTrustChanged];
+
+    // FIXME: This should be null. The web process should say whether to include a cert,
+    // it just shouldn't provide the cert.
+    [webView loadHTMLString:@"<script>alert('loaded')</script>" baseURL:[NSURL URLWithString:@"https://webkit.org/path1"]];
+    EXPECT_WK_STREQ([uiDelegate waitForAlert], "loaded");
+    EXPECT_NOT_NULL(webView.get().serverTrust);
 }