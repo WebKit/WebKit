f2d69489c66bb72533d44612fff3fd09e68a632e
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2026-08-20 00:57:18 -0700

    Remove unused `tryAllocateBuffer` from `Vector.h` https://bugs.webkit.org/show_bug.cgi?id=271125 rdar://125315707
    Reviewed by Chris Dumez.
    
    This patch removes unused code, I tried to find any call sites
    but couldn't.
    
    * Source/WTF/wtf/Vector.h:
    (WTF::VectorBufferBase::allocateBuffer):
    (WTF::VectorBuffer::allocateBuffer):
    (WTF::VectorBufferBase::tryAllocateBuffer): Deleted.
    (WTF::VectorBuffer::tryAllocateBuffer): Deleted.
    
    Canonical link: https://commits.webkit.org/319514@main

--- Source/WTF/wtf/Vector.h
+++ Source/WTF/wtf/Vector.h
@@ -245,7 +245,6 @@ public:
     }
 
     ALWAYS_INLINE void allocateBuffer(size_t newCapacity) { allocateBuffer<FailureAction::Crash>(newCapacity); }
-    ALWAYS_INLINE bool tryAllocateBuffer(size_t newCapacity) { return allocateBuffer<FailureAction::Report>(newCapacity); }
 
     bool shouldReallocateBuffer(size_t newCapacity) const
     {
@@ -391,7 +390,6 @@ public:
 #endif
 
     using Base::allocateBuffer;
-    using Base::tryAllocateBuffer;
     using Base::shouldReallocateBuffer;
     using Base::reallocateBuffer;
     using Base::deallocateBuffer;
@@ -468,7 +466,6 @@ public:
     }
 
     ALWAYS_INLINE void allocateBuffer(size_t newCapacity) { allocateBuffer<FailureAction::Crash>(newCapacity); }
-    ALWAYS_INLINE bool tryAllocateBuffer(size_t newCapacity) { return allocateBuffer<FailureAction::Report>(newCapacity); }
 
     void deallocateBuffer(T* bufferToDeallocate)
     {
@@ -1014,7 +1011,6 @@ private:
     using Base::swap;
     using Base::allocateBuffer;
     using Base::deallocateBuffer;
-    using Base::tryAllocateBuffer;
     using Base::shouldReallocateBuffer;
     using Base::reallocateBuffer;
     using Base::restoreInlineBufferIfNeeded;
