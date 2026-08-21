2f072fbfecffc76a8567e87be967fbb8653e9472
Franco Vieira de Souza <franco_vieira@apple.com>
2026-08-20 19:10:19 -0700

    [CSS] Set CSSUserSelectEnabled to stable https://bugs.webkit.org/show_bug.cgi?id=321825 rdar://184964807
    Reviewed by Tim Nguyen and Megan Gardner.
    
    Set CSSUserSelectEnabled to stable.
    
    This also adapts a pair of API test that specifically exercises both
    values of this flag, with no change of behavior.
    
    * Source/WTF/Scripts/Preferences/UnifiedWebPreferences.yaml:
    * Tools/TestWebKitAPI/Tests/WebKit/WKWebView/CopyRTF.mm:
    (copyAttributedStringFromHTML):
    (TEST(CopyRTF, StripsUserSelectNone)): now assumes the flag is true
    (TEST(CopyRTF, StripsUserSelectNonewithLegacyUserSelectBehavior)): flag off behavior
    (TEST(CopyRTF, StripsUserSelectNoneWithUnprefixedUserSelectEnabled)): Deleted.
    
    Canonical link: https://commits.webkit.org/319562@main

--- Tools/TestWebKitAPI/Tests/WebKit/WKWebView/CopyRTF.mm
+++ Tools/TestWebKitAPI/Tests/WebKit/WKWebView/CopyRTF.mm
@@ -62,15 +62,15 @@ static NSData *readRTFDataFromPasteboard()
 }
 #endif
 
-static RetainPtr<NSAttributedString> copyAttributedStringFromHTML(NSString *htmlString, bool forceDarkMode, bool enableUnprefixedUserSelect = false)
+static RetainPtr<NSAttributedString> copyAttributedStringFromHTML(NSString *htmlString, bool forceDarkMode, bool disableUnprefixedUserSelect = false)
 {
     RetainPtr webView = adoptNS([[TestWKWebView alloc] initWithFrame:NSMakeRect(0, 0, 400, 400)]);
 
     auto preferences = (__bridge WKPreferencesRef)[[webView configuration] preferences];
     WKPreferencesSetWriteRichTextDataWhenCopyingOrDragging(preferences, true);
 
-    if (enableUnprefixedUserSelect)
-        WKPreferencesSetBoolValueForKeyForTesting(preferences, true, WKStringCreateWithUTF8CString("CSSUserSelectEnabled"));
+    if (disableUnprefixedUserSelect)
+        WKPreferencesSetBoolValueForKeyForTesting(preferences, false, WKStringCreateWithUTF8CString("CSSUserSelectEnabled"));
 
     if (forceDarkMode)
         [webView forceDarkMode];
@@ -178,19 +178,18 @@ TEST(CopyRTF, StripsUserSelectNone)
         "<span style='-webkit-user-select: initial; user-select: initial;'>WebKit </span></span>"
         "<div style='-webkit-user-select: none; user-select: none;'>some<br>user-select-none<br>content</div><span inert>foo </span>bar", false);
 
-    EXPECT_WK_STREQ([attributedString string].UTF8String, "hello WebKit bar");
+    EXPECT_WK_STREQ([attributedString string].UTF8String, "hello bar");
 }
 
-TEST(CopyRTF, StripsUserSelectNoneWithUnprefixedUserSelectEnabled)
+TEST(CopyRTF, StripsUserSelectNonewithLegacyUserSelectBehavior)
 {
-    // Same content as StripsUserSelectNone. With the unprefixed property enabled, 'initial' no
-    // longer counts as explicitly set for either spelling, so the inner span falls back to
-    // 'user-select: auto', resolves to the surrounding 'none', and is stripped too.
+    // Same content as StripsUserSelectNone. On legacy behavior,
+    // 'initial' is resolved to 'text':
     auto attributedString = copyAttributedStringFromHTML(@"hello <span style='-webkit-user-select: none; user-select: none;'>world "
         "<span style='-webkit-user-select: initial; user-select: initial;'>WebKit </span></span>"
         "<div style='-webkit-user-select: none; user-select: none;'>some<br>user-select-none<br>content</div><span inert>foo </span>bar", false, true);
 
-    EXPECT_WK_STREQ([attributedString string].UTF8String, "hello bar");
+    EXPECT_WK_STREQ([attributedString string].UTF8String, "hello WebKit bar");
 }
 
 TEST(CopyRTF, StripsUserSelectNoneQuirks)
