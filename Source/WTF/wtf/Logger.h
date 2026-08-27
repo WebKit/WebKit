d3699b58389400a9357d99a530102b809da42d6e
Andy Estes <aestes@apple.com>
2026-08-26 21:11:38 -0700

    MediaSource should remove itself as a Logger observer on destruction https://bugs.webkit.org/show_bug.cgi?id=318837 rdar://177356913
    Reviewed by Eric Carlson.
    
    Logger stores raw references to observers in Logger::observers(), so the observer is responsible
    for removing itself from Logger::observers() prior to destruction. Modified MediaSource's
    destructor do call Logger::removeObserver, and modified Logger::Observer's destructor to
    RELEASE_ASSERT that the observer has been removed.
    
    Test: media/media-source/worker/media-source-worker-logger-observer-crash.html
    
    * LayoutTests/media/media-source/worker/media-source-worker-logger-observer-crash-expected.txt: Added.
    * LayoutTests/media/media-source/worker/media-source-worker-logger-observer-crash.html: Added.
    * Source/WTF/wtf/Logger.cpp:
    (WTF::Logger::Observer::assertIsNotRegistered const):
    * Source/WTF/wtf/Logger.h:
    (WTF::Logger::Observer::~Observer):
    * Source/WebCore/Modules/mediasource/MediaSource.cpp:
    (WebCore::MediaSource::~MediaSource):
    * Source/WebCore/testing/Internals.cpp:
    (WebCore::Internals::configureLoggingChannel):
    * Source/WebCore/testing/Internals.h:
    * Source/WebCore/testing/Internals.idl:
    
    Originally-landed-as: 305413.1106@safari-7624.5-branch (ed0a2c844690). rdar://185368565
    Canonical link: https://commits.webkit.org/319926@main

--- Source/WTF/wtf/Logger.h
+++ Source/WTF/wtf/Logger.h
@@ -138,9 +138,11 @@ public:
 
     class Observer {
     public:
-        virtual ~Observer() = default;
+        virtual ~Observer() { assertIsNotRegistered(); }
         // Can be called on any thread.
         virtual void didLogMessage(const WTFLogChannel&, WTFLogLevel, std::optional<WTFLogLocation>, Vector<JSONLogValue>&&) = 0;
+
+        WTF_EXPORT_PRIVATE void assertIsNotRegistered() const;
     };
 
     class MessageHandlerObserver {
