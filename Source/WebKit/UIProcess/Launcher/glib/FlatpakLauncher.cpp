1c3038513722ea1712ed251cbfd98dcdea63322e
Michael Catanzaro <mcatanzaro@gnome.org>
2026-08-27 19:31:53 -0700

    Cherry-pick 319841@main (60477f5f174c). https://bugs.webkit.org/show_bug.cgi?id=321951
        [WPE][GTK] FlatpakLauncher should not attempt to expose paths under /app or /usr
        https://bugs.webkit.org/show_bug.cgi?id=321951
    
        Reviewed by Patrick Griffis.
    
        The auxiliary process always has the same view of /app and /usr as the
        UI process, so trying to expose them to the sandbox is pointless at
        best. In the past, it has uncovered bugs in Flatpak. See:
        https://github.com/flatpak/flatpak/issues/6584
    
        * Source/WebKit/UIProcess/Launcher/glib/FlatpakLauncher.cpp:
        (WebKit::canPossiblyExposePath):
        (WebKit::flatpakSpawn):
    
        Canonical link: https://commits.webkit.org/319841@main
    
    Canonical link: https://commits.webkit.org/317695.158@webkitglib/2.54

--- Source/WebKit/UIProcess/Launcher/glib/FlatpakLauncher.cpp
+++ Source/WebKit/UIProcess/Launcher/glib/FlatpakLauncher.cpp
@@ -36,6 +36,12 @@
 
 namespace WebKit {
 
+static bool canPossiblyExposePath(const String& path)
+{
+    // The child process's /app and /usr will both be identical to the parent process's.
+    return !path.startsWith("/app"_s) && !path.startsWith("/usr"_s);
+}
+
 GRefPtr<GSubprocess> flatpakSpawn(GSubprocessLauncher* launcher, const WebKit::ProcessLauncher::LaunchOptions& launchOptions, Vector<char*>& argv, int childProcessSocket, GError** error)
 {
     ASSERT(launcher);
@@ -66,22 +72,28 @@ GRefPtr<GSubprocess> flatpakSpawn(GSubprocessLauncher* launcher, const WebKit::P
         // GST_DEBUG_FILE points to an absolute file path, so we need write permissions for its parent directory.
         if (const char* debugFilePath = g_getenv("GST_DEBUG_FILE")) {
             auto parentDir = FileSystem::parentPath(FileSystem::stringFromFileSystemRepresentation(debugFilePath));
-            GUniquePtr<gchar> pathArg(g_strdup_printf("--sandbox-expose-path=%s", parentDir.utf8().data()));
-            flatpakArgs.append(pathArg.get());
+            if (canPossiblyExposePath(parentDir)) {
+                GUniquePtr<gchar> pathArg(g_strdup_printf("--sandbox-expose-path=%s", parentDir.utf8().data()));
+                flatpakArgs.append(pathArg.get());
+            }
         }
 
         // GST_DEBUG_DUMP_DOT_DIR might not exist when the application starts, so we need write
         // permissions for its parent directory.
         if (const char* dotDir = g_getenv("GST_DEBUG_DUMP_DOT_DIR")) {
             auto parentDir = FileSystem::parentPath(FileSystem::stringFromFileSystemRepresentation(dotDir));
-            GUniquePtr<gchar> pathArg(g_strdup_printf("--sandbox-expose-path=%s", parentDir.utf8().data()));
-            flatpakArgs.append(pathArg.get());
+            if (canPossiblyExposePath(parentDir)) {
+                GUniquePtr<gchar> pathArg(g_strdup_printf("--sandbox-expose-path=%s", parentDir.utf8().data()));
+                flatpakArgs.append(pathArg.get());
+            }
         }
 
         for (const auto& pathAndPermission : launchOptions.extraSandboxPaths) {
-            const char* formatString = pathAndPermission.value == SandboxPermission::ReadOnly ? "--sandbox-expose-path-ro=%s": "--sandbox-expose-path=%s";
-            GUniquePtr<gchar> pathArg(g_strdup_printf(formatString, pathAndPermission.key.data()));
-            flatpakArgs.append(pathArg.get());
+            if (canPossiblyExposePath(String { pathAndPermission.key.span() })) {
+                const char* formatString = pathAndPermission.value == SandboxPermission::ReadOnly ? "--sandbox-expose-path-ro=%s": "--sandbox-expose-path=%s";
+                GUniquePtr<gchar> pathArg(g_strdup_printf(formatString, pathAndPermission.key.data()));
+                flatpakArgs.append(pathArg.get());
+            }
         }
 
 #if USE(ATSPI)
