141ea152cf214cd8426f0711efc6eac7209bd0ef
Youenn Fablet <youenn@apple.com>
2026-09-02 16:33:00 -0700

    Out-of-bounds read in -[RTCVideoEncoderH265/H264 frameWasEncoded:] — AppendData uses total CMBlockBuffer size instead of chunk length rdar://176886407
    Reviewed by Eric Carlson.
    
    We were copying size bytes of data while we should only copy length bytes of data, which is an issue if VT is generating non contiguous encoded frames.
    
    * Source/ThirdParty/libwebrtc/Source/webrtc/webkit_sdk/objc/components/video_codec/RTCVideoEncoderH264.mm:
    (-[RTCVideoEncoderH264 frameWasEncoded:flags:sampleBuffer:codecSpecificInfo:width:height:renderTimeMs:timestamp:duration:rotation:isKeyFrameRequired:]):
    * Source/ThirdParty/libwebrtc/Source/webrtc/webkit_sdk/objc/components/video_codec/RTCVideoEncoderH265.mm:
    (-[RTCVideoEncoderH265 frameWasEncoded:flags:sampleBuffer:width:height:renderTimeMs:timestamp:rotation:]):
    
    Originally-landed-as: 305413.998@safari-7624.5-branch (34598221929f). rdar://185368333
    Canonical link: https://commits.webkit.org/320365@main

--- Source/ThirdParty/libwebrtc/Source/webrtc/webkit_sdk/objc/components/video_codec/RTCVideoEncoderH265.mm
+++ Source/ThirdParty/libwebrtc/Source/webrtc/webkit_sdk/objc/components/video_codec/RTCVideoEncoderH265.mm
@@ -594,8 +594,8 @@ void compressionOutputCallback(void* encoder,
         }
         return;
       }
-      buffer->AppendData(data, size);
-      currentStart += size;
+      buffer->AppendData(data, length);
+      currentStart += length;
     }
     if (_descriptionCallback && _needsToSendDescription) {
       auto formatDescription = CMSampleBufferGetFormatDescription(sampleBuffer);
