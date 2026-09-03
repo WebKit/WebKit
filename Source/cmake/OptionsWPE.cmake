37e8b8b7168a1bb74906e56deae7c5e696a041c2
Adrian Perez de Castro <aperez@igalia.com>
2026-09-03 01:16:38 -0700

    Unreviewed. [WPE] Properly set version number for the 2.53.92 release
    * Source/cmake/OptionsWPE.cmake: The previous commit updated the library
      soname version and added the release notes, but missed setting the
      package version. Do it now.
    
    Canonical link: https://commits.webkit.org/317695.185@webkitglib/2.54

--- Source/cmake/OptionsWPE.cmake
+++ Source/cmake/OptionsWPE.cmake
@@ -1,7 +1,7 @@
 include(GNUInstallDirs)
 include(VersioningUtils)
 
-SET_PROJECT_VERSION(2 53 91)
+SET_PROJECT_VERSION(2 53 92)
 
 set(USER_AGENT_BRANDING "" CACHE STRING "Branding to add to user agent string")
 
