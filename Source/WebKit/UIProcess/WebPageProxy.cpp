abb5bc1797daf480d7f429d0d846aa6e1ea34da3
Jeffrey Xu <jeffrey_xu2@apple.com>
2026-09-08 17:55:16 -0700

    Prevent marking back/forward navigation as redirect. https://bugs.webkit.org/show_bug.cgi?id=301806 rdar://164320103
    Reviewed by NOBODY (OOPS!).
    
    Any top-frame navigation within 5s without a user gesture is marked as a redirect for ITP. However, clicking the back button (or other similar interactions with the browser) does not count as a user gesture because it doesn't interact with the page itself, but this navigation is clearly user-initiated.
    
    Test: http/tests/resourceLoadStatistics/do-not-log-back-navigation-as-redirect.html
    
    * LayoutTests/http/tests/resourceLoadStatistics/do-not-log-back-navigation-as-redirect-expected.txt: Added.
    * LayoutTests/http/tests/resourceLoadStatistics/do-not-log-back-navigation-as-redirect.html: Added.
    * Source/WebKit/UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::decidePolicyForNavigationAction):

--- Source/WebKit/UIProcess/WebPageProxy.cpp
+++ Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -10293,7 +10293,7 @@ void WebPageProxy::decidePolicyForNavigationAction(Ref<WebProcessProxy>&& proces
     }
 #endif
 
-    auto wasPotentiallyInitiatedByUser = navigation->isLoadedWithNavigationShared() || navigation->wasUserInitiated();
+    auto wasPotentiallyInitiatedByUser = navigation->isLoadedWithNavigationShared() || navigation->wasUserInitiated() || navigationAction->data().navigationType == WebCore::NavigationType::BackForward;
     if (!sessionID().isEphemeral())
         logFrameNavigation(frame, internals().pageLoadState.url(), request, navigationAction->data().redirectResponse.url(), wasPotentiallyInitiatedByUser);
 
