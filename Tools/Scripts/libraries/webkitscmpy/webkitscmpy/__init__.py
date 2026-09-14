de20262c2e5261feaaa8f56e785a2212ba7c15bc
ozcoder <ozcoder@gmail.com>
2026-09-14 03:57:07 -1000

    Web Inspector: Improve interoperability of HAR file import and export - stage 1 https://bugs.webkit.org/show_bug.cgi?id=319491
    Reviewed by NOBODY (OOPS!).
    
    Fixed up casing.
    
    No new tests (OOPS!).
    
    * Source/WebInspectorUI/UserInterface/Controllers/HARBuilder.js:
    (WI.HARBuilder.priority):
    (WI.HARBuilder.networkPriorityFromHARPriority):
    (WI.HARBuilder):
    * Source/WebInspectorUI/UserInterface/Models/Resource.js:
    (WI.Resource.comparePriority):
    (WI.Resource.displayNameForPriority):
    (WI.Resource.networkPriorityFromPayload):
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py:

--- Tools/Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py
+++ Tools/Scripts/libraries/webkitscmpy/webkitscmpy/__init__.py
@@ -60,7 +60,7 @@ AutoInstall.register(Package('xmltodict', Version(0, 11, 0)))
 AutoInstall.register(Package('webkitbugspy', Version(0, 15, 4)), local=True)
 
 if sys.version_info >= (3, 10):
-    AutoInstall.register(Package('rapidfuzz', Version(3, 14, 5), wheel=True))
+    AutoInstall.register(Package('rapidfuzz', Version(3, 14, 6), wheel=True))
 else:
     AutoInstall.register(Package('rapidfuzz', Version(3, 4, 0)))
 
