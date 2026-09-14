6e50c074a8444f18a5ca43c9cccefb594355f2dc
Nathan Solomon <nathan_solomon@apple.com>
2026-09-14 14:40:45 -0700

    Store RenderBox overriding sizes in a per-box struct instead of in global weak hash maps https://bugs.webkit.org/show_bug.cgi?id=324187 rdar://187407218
    Reviewed by NOBODY (OOPS!).
    
    Overriding logical sizes for flex and grid items were previously stored in six global
    SingleThreadWeakHashMaps keyed on the RenderBox. Every get/set/clear was a hash + weak-ptr
    probe, plus occasional removeNullReferences() cleanup and rehashing. This was an inlined
    cost for a few getters/setters/clearers related to logical width and height during layout.
    These were per-box values read via RenderBox identity.
    
    To improve performance, move this per RenderBox-data to a struct that each RenderBox holds a
    handle to directly. get/set/clear operations now become a pointer-null-check + struct field
    access. This makes the map access and maintenance work go away at the tradeoff of adding 8
    bytes to RenderBox.
    
    No new tests since there is no behavior change, just changing implementation details.
    
    * Source/WebCore/rendering/RenderBox.cpp:
    (WebCore::RenderBox::ensureOverridingSizes):
    (WebCore::RenderBox::setOverridingBorderBoxLogicalHeight):
    (WebCore::RenderBox::setOverridingBorderBoxLogicalWidth):
    (WebCore::RenderBox::clearOverridingBorderBoxLogicalHeight):
    (WebCore::RenderBox::clearOverridingBorderBoxLogicalWidth):
    (WebCore::RenderBox::overridingBorderBoxLogicalWidth const):
    (WebCore::RenderBox::overridingBorderBoxLogicalHeight const):
    (WebCore::RenderBox::gridAreaContentLogicalWidth const):
    (WebCore::RenderBox::gridAreaContentLogicalHeight const):
    (WebCore::RenderBox::setGridAreaContentLogicalWidth):
    (WebCore::RenderBox::setGridAreaContentLogicalHeight):
    (WebCore::RenderBox::clearGridAreaContentSize):
    (WebCore::RenderBox::clearGridAreaContentLogicalHeight):
    (WebCore::RenderBox::overridingLogicalHeightForFlexBasisComputation const):
    (WebCore::RenderBox::setOverridingBorderBoxLogicalHeightForFlexBasisComputation):
    (WebCore::RenderBox::clearOverridingLogicalHeightForFlexBasisComputation):
    (WebCore::RenderBox::overridingLogicalWidthForFlexBasisComputation const):
    (WebCore::RenderBox::setOverridingBorderBoxLogicalWidthForFlexBasisComputation):
    (WebCore::RenderBox::clearOverridingLogicalWidthForFlexBasisComputation):
    * Source/WebCore/rendering/RenderBox.h:

--- Source/WebCore/rendering/RenderBox.h
+++ Source/WebCore/rendering/RenderBox.h
@@ -43,6 +43,7 @@ class RenderBoxFragmentInfo;
 class RenderFragmentContainer;
 class PositionedLayoutConstraints;
 struct PaintInfo;
+struct RenderBoxOverridingSizes;
 
 enum class AvailableLogicalHeightType : bool { ExcludeMarginBorderPadding, IncludeMarginBorderPadding };
 
@@ -740,7 +741,11 @@ protected:
     // Our overflow information.
     std::unique_ptr<RenderOverflow> m_overflow;
 
+    std::unique_ptr<RenderBoxOverridingSizes> m_overridingSizes;
+
 private:
+    RenderBoxOverridingSizes& ensureOverridingSizes();
+
     // Used to store state between styleWillChange and styleDidChange
     static bool s_hadNonVisibleOverflow;
 };
