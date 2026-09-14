8e61c5532c480af7af198a9cc4d233ff81970442
Diego Pino Garcia <dpino@igalia.com>
2026-09-14 07:04:22 -0700

    Cherry-pick 321054@main (d2e0c4ababbe). https://bugs.webkit.org/show_bug.cgi?id=323663
        [GLIB] Build fix for Ubuntu 24.04 after 320820@main
        https://bugs.webkit.org/show_bug.cgi?id=323663
    
        Unreviewed.
    
        Follow-up on 321047@main.
    
        Macro 'MANETTE_CHECK_VERSION' is not defined in the libmanette version
        shipped on Ubuntu 24.04 (v0.2.7).
    
        * Source/WebCore/platform/gamepad/manette/ManetteGamepad.cpp:
        (WebCore::ManetteGamepad::startRumble):
    
        Canonical link: https://commits.webkit.org/321054@main
    
    Canonical link: https://commits.webkit.org/317695.304@webkitglib/2.54

--- Source/WebKit/WPEPlatform/wpe/WPEGamepadManette.cpp
+++ Source/WebKit/WPEPlatform/wpe/WPEGamepadManette.cpp
@@ -173,8 +173,12 @@ static gboolean wpeGamepadManetteHasRumble(WPEGamepad* gamepad)
 static gboolean wpeGamepadManetteRumble(WPEGamepad* gamepad, gdouble strongMagnitude, gdouble weakMagnitude, guint durationMs)
 {
     auto* priv = WPE_GAMEPAD_MANETTE(gamepad)->priv;
-#if defined(MANETTE_CHECK_VERSION) && MANETTE_CHECK_VERSION(1, 0, 0)
+#if defined(MANETTE_CHECK_VERSION)
+#if MANETTE_CHECK_VERSION(1, 0, 0)
     return manette_device_rumble(priv->device.get(), strongMagnitude, weakMagnitude, durationMs);
+#else
+    return manette_device_rumble(priv->device.get(), strongMagnitude * G_MAXUINT16, weakMagnitude * G_MAXUINT16, durationMs);
+#endif
 #else
     return manette_device_rumble(priv->device.get(), strongMagnitude * G_MAXUINT16, weakMagnitude * G_MAXUINT16, durationMs);
 #endif
