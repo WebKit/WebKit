666e05b6c6bc13a19091a31a3db1329f9c6924dd
tannal <mtan@igalia.com>
2026-09-14 20:32:58 +0800

    [Refactoring] Extract common anchor element logic into AnchorElementUtils https://bugs.webkit.org/show_bug.cgi?id=324146
    Reviewed by NOBODY (OOPS!).
    
    Factored out common hyperlink behavior (rel attributes parsing, ping sending,
    download attribute sanitization, and hyperlink navigation) from HTMLAnchorElement
    and SVGAElement into a shared helper class `AnchorElementUtils`.
    
    Also exposed `KeyboardEvent::isEnterKeyKeydownEvent` for reuse.
    
    No behavior change, in preparation for supporting the MathMLAnchorElement IDL. [1]
    
    [1] https://github.com/WebKit/WebKit/pull/71827
    
    * Source/WebCore/Headers.cmake:
    * Source/WebCore/Sources.txt:
    * Source/WebCore/WebCore.xcodeproj/project.pbxproj:
    * Source/WebCore/dom/KeyboardEvent.cpp:
    (WebCore::KeyboardEvent::isEnterKeyKeydownEvent):
    * Source/WebCore/dom/KeyboardEvent.h:
    * Source/WebCore/html/HTMLAnchorElement.cpp:
    (WebCore::HTMLAnchorElement::defaultEventHandler):
    (WebCore::HTMLAnchorElement::setActive):
    (WebCore::HTMLAnchorElement::attributeChanged):
    (WebCore::HTMLAnchorElement::relList):
    (WebCore::HTMLAnchorElement::isSystemPreviewLink):
    (WebCore::HTMLAnchorElement::sendPings): Deleted.
    * Source/WebCore/html/HTMLAnchorElement.h:
    * Source/WebCore/mathml/MathMLElement.cpp:
    (WebCore::MathMLElement::defaultEventHandler):
    * Source/WebCore/svg/SVGAElement.cpp:
    (WebCore::SVGAElement::attributeChanged):
    (WebCore::SVGAElement::defaultEventHandler):
    (WebCore::SVGAElement::relList):
    * Source/WebCore/html/AnchorElementUtils.cpp: Added.
    (WebCore::AnchorElementUtils::relationsForRelAttribute):
    (WebCore::AnchorElementUtils::isSupportedRelToken):
    (WebCore::AnchorElementUtils::hasRel):
    (WebCore::AnchorElementUtils::parseDownloadAttribute):
    (WebCore::AnchorElementUtils::sendPings):
    (WebCore::AnchorElementUtils::effectiveReferrerPolicy):
    (WebCore::AnchorElementUtils::navigateHyperlink):
    * Source/WebCore/html/AnchorElementUtils.h: Added.
    (WebCore::AnchorElementUtils::navigateHyperlink):

--- Source/WebCore/Headers.cmake
+++ Source/WebCore/Headers.cmake
@@ -1560,6 +1560,7 @@ set(WebCore_PRIVATE_FRAMEWORK_HEADERS
     history/ProcessSwapDisposition.h
 
     html/Allowlist.h
+    html/AnchorElementUtils.h
     html/AttachmentAssociatedElement.h
     html/Autocapitalize.h
     html/AutocapitalizeTypes.h
