9d94c4a1b1dfb70159f036f89e9390ac1ddddae3
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2026-09-13 21:48:05 -0700

    GraphicsLayerCA::m_tileCoverage is read uninitialized by changeLayerTypeTo() https://bugs.webkit.org/show_bug.cgi?id=324097 rdar://187313650
    Reviewed by Chris Dumez.
    
    m_tileCoverage (a plain unsigned TileCoverage) had no in-class initializer
    and was absent from the constructor's member-init list, so changeLayerTypeTo()
    reads an indeterminate value at GraphicsLayerCA.cpp:4947 and forwards it to a
    freshly-created TiledBacking. This is undefined behavior.
    
    Initialize m_tileCoverage to TiledBacking::CoverageForVisibleArea in the
    GraphicsLayerCA constructor, matching TileController's own default. The
    constructor lives in the .cpp, which already includes TiledBacking.h, so we
    can use the named enumerator directly rather than a bare 0 in the header
    (GraphicsLayerCA.h does not include TiledBacking.h).
    
    * Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp:
    (WebCore::GraphicsLayerCA::GraphicsLayerCA):
    
    Canonical link: https://commits.webkit.org/321036@main

--- Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
+++ Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2023 Apple Inc. All rights reserved.
+ * Copyright (C) 2010-2026 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -401,6 +401,7 @@ static LayerDisplayListHashMap& NODELETE layerDisplayListMap()
 
 GraphicsLayerCA::GraphicsLayerCA(Type layerType, GraphicsLayerClient& client)
     : GraphicsLayer(layerType, client)
+    , m_tileCoverage(TiledBacking::CoverageForVisibleArea)
     , m_needsFullRepaint(false)
     , m_allowsBackingStoreDetaching(true)
     , m_intersectsCoverageRect(false)
