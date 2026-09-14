17dc676b794b482367a5565815003d32b237b216
ozcoder <ozcoder@gmail.com>
2026-09-14 03:57:07 -1000

    Web Inspector: Improve interoperability of HAR file import and export - stage 1 https://bugs.webkit.org/show_bug.cgi?id=319491
    Reviewed by NOBODY (OOPS!).
    
    Use a RefPtr when reading AdditionalNetworkLoadMetricsForWebInspector instead
    of an uncounted raw pointer from RefPtr::get(), matching Safer CPP guidelines.
    
    No new tests (OOPS!).
    
    * Source/WebKit/UIProcess/Inspector/Agents/ProxyingNetworkAgent.cpp:
    (Inspector::buildObjectForMetrics):

--- Source/WebKit/UIProcess/Inspector/Agents/ProxyingNetworkAgent.cpp
+++ Source/WebKit/UIProcess/Inspector/Agents/ProxyingNetworkAgent.cpp
@@ -162,7 +162,7 @@ static Ref<Inspector::Protocol::Network::Metrics> buildObjectForMetrics(const Ne
 
     if (!networkLoadMetrics.protocol.isNull())
         metrics->setProtocol(networkLoadMetrics.protocol);
-    if (auto* additionalMetrics = networkLoadMetrics.additionalNetworkLoadMetricsForWebInspector.get()) {
+    if (RefPtr additionalMetrics = networkLoadMetrics.additionalNetworkLoadMetricsForWebInspector) {
         metrics->setInitialPriority(Inspector::Protocol::Network::toProtocol(additionalMetrics->initialPriority));
 
         if (additionalMetrics->priority != WebCore::NetworkLoadPriority::Unknown)
@@ -190,7 +190,7 @@ static Ref<Inspector::Protocol::Network::Metrics> buildObjectForMetrics(const Ne
     auto connectionPayload = Inspector::Protocol::Security::Connection::create()
         .release();
 
-    if (auto* additionalMetrics = networkLoadMetrics.additionalNetworkLoadMetricsForWebInspector.get()) {
+    if (RefPtr additionalMetrics = networkLoadMetrics.additionalNetworkLoadMetricsForWebInspector) {
         if (!additionalMetrics->tlsProtocol.isEmpty())
             connectionPayload->setProtocol(additionalMetrics->tlsProtocol);
         if (!additionalMetrics->tlsCipher.isEmpty())
