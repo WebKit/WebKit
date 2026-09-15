5c0c8d0350ab4cc975d3817ffb536e8c4cd0aa5a
Sergio Villar Senin <svillar@igalia.com>
2026-09-15 14:19:21 +0200

    [WebXR] Discard MSAA attachments after rendering https://bugs.webkit.org/show_bug.cgi?id=324239
    Reviewed by NOBODY (OOPS!).
    
    In order to implement MSAA in WebXR, WebKit uses a multisampled
    renderbuffer. Render buffers are write-only objects, nobody is going to
    read from them after the contents are resolved. On tile-based rendering
    GPUs, after a render pass is completed, the contents of the renderbuffer
    are saved into RAM because the OpenGL driver does not know whether it
    can simply discard them. For a 3200*1600 framebuffer with 4x MSAA that's
    82Mb written per frame (actually 82 + 82 if depth/stencil are used too).
    That's a very expensive operation that adds nothing and should be
    avoided.
    
    The fix is to do the same as VisionOS, discard the render buffer
    attachments before finishing the render pass. VisionOS does not even
    need the blit as that port uses a special ANGLE extension for Metal
    backend that gives an FBO a resolve destination alongside its
    multisampled attachment.
    
    No new tests as this operates at OpenGLES driver level.
    
    * Source/WebCore/Modules/webxr/WebXROpaqueFramebuffer.cpp:
    (WebCore::WebXROpaqueFramebuffer::resolveMSAAFramebuffer):
    * Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.cpp:
    (WebCore::GraphicsContextGLANGLE::framebufferDiscard):
    * Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.h:
    * Source/WebCore/platform/graphics/android/GraphicsContextGLTextureMapperAndroid.cpp:
    (WebCore::GraphicsContextGLTextureMapperAndroid::enableRequiredWebXRExtensionsImpl):
    * Source/WebCore/platform/graphics/cocoa/GraphicsContextGLCocoa.h:
    * Source/WebCore/platform/graphics/cocoa/GraphicsContextGLCocoa.mm:
    (WebCore::GraphicsContextGLCocoa::framebufferDiscard): Deleted.
    * Source/WebCore/platform/graphics/gbm/GraphicsContextGLTextureMapperGBM.cpp:
    (WebCore::GraphicsContextGLTextureMapperGBM::enableRequiredWebXRExtensionsImpl):

--- Source/WebCore/platform/graphics/gbm/GraphicsContextGLGBM.cpp
+++ Source/WebCore/platform/graphics/gbm/GraphicsContextGLGBM.cpp
@@ -328,7 +328,8 @@ bool GraphicsContextGLGBM::enableRequiredWebXRExtensionsImpl()
 {
     return enableExtensionsImpl({
         "GL_OES_EGL_image"_s,
-        "GL_OES_EGL_image_external"_s
+        "GL_OES_EGL_image_external"_s,
+        "GL_EXT_discard_framebuffer"_s
     });
 }
 #endif // ENABLE(WEBXR)
