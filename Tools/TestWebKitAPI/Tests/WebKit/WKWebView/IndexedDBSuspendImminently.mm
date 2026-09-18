WebKit/WebKit

commit cd1990281851f02d502f18377b4355dcd1e5f2bf

[iOS] Network process unresponsive due to suspension https://bugs.webkit.org/show_bug.cgi?id=255417 rdar://105621258
Reviewed by Chris Dumez.

If a WebContent process is a standalone worker process with a foreground
assertion/activity (e.g. a worker for a foreground view) and then a page
gets added to the process it will never take an assertion on the network
process as it should. We need to make sure we call
WebProcessProxy::didChangeThrottleState() when a standalone worker
process becomes a web content process with a page.
WebProcessProxy::didChangeThrottleState() checks
isStandaloneServiceWorkerProcess() and drops all network assertions if
it returns true.

* Source/WebKit/UIProcess/ProcessThrottler.h:
(WebKit::ProcessThrottler::currentState):
* Source/WebKit/UIProcess/WebProcessProxy.cpp:
(WebKit::WebProcessProxy::addExistingWebPage):

Canonical link: https://commits.webkit.org/263031@main

Source/WebKit/UIProcess/ProcessThrottler.h
@@ -121,6 +121,7 @@ public:
     void setShouldTakeSuspendedAssertion(bool);
     void delaySuspension();
     bool isSuspended() const { return m_processIdentifier && !m_assertion; }
+    ProcessThrottleState currentState() const { return m_state; }
 
 private:
     friend class ProcessThrottlerActivity;

Source/WebKit/UIProcess/WebProcessProxy.cpp
@@ -719,6 +719,8 @@ void WebProcessProxy::addExistingWebPage(WebPageProxy& webPage, BeginsUsingDataS
     RELEASE_ASSERT(!m_isInProcessCache);
     ASSERT(!m_websiteDataStore || websiteDataStore() == &webPage.websiteDataStore());
 
+    bool wasStandaloneServiceWorkerProcess = isStandaloneServiceWorkerProcess();
+
     if (beginsUsingDataStore == BeginsUsingDataStore::Yes) {
         RELEASE_ASSERT(m_processPool);
         m_processPool->pageBeginUsingWebsiteDataStore(webPage.identifier(), webPage.websiteDataStore());
@@ -738,6 +740,12 @@ void WebProcessProxy::addExistingWebPage(WebPageProxy& webPage, BeginsUsingDataS
     updateRegistrationWithDataStore();
     updateBackgroundResponsivenessTimer();
     updateBlobRegistryPartitioningState();
+
+    // If this was previously a standalone worker process with no pages we need to call didChangeThrottleState()
+    // to update our process assertions on the network process since standalone worker processes do not hold
+    // assertions on the network process
+    if (wasStandaloneServiceWorkerProcess)
+        didChangeThrottleState(throttler().currentState());
 }
 
 void WebProcessProxy::markIsNoLongerInPrewarmedPool()

commit 497098e97dcc2b822e87cfb6b678a33757db96c9

IndexedDB transactions stall when reusing a service worker process without RunningBoard https://bugs.webkit.org/show_bug.cgi?id=324058 <rdar://problem/187349702>
Reviewed by Sihui Liu.

Without RunningBoard `ProcessThrottler::didConnectToProcess` is never called so `ProcessThrottler::currentState` keeps reporting its initial `ProcessThrottleState::Suspended` value.
When navigation reuses a standalone `ServiceWorker` WebProcess, `WebProcessProxy::addExistingWebPage` passes that value to `WebProcessProxy::didChangeThrottleState` to refresh NetworkProcess assertions.
This incorrectly marks the IndexedDB client suspended with no subsequent resume notification to restart its transactions.

315609@main added the storage suspension notifications and 319873@main made the scheduler defer queued transactions while the client is suspended.
The incorrect state also affects other readers, including the foreground flag reported by `WebProcessProxy::didExceedMemoryFootprintThreshold`.
Correct the state at its source rather than making one caller ignore it, preserving the assertion refresh introduced by 263031@main when a `ServiceWorker` WebProcess gains a page without changing `ProcessThrottleState`.

* Source/WebKit/UIProcess/ProcessThrottler.h:
* Source/WebKit/UIProcess/ProcessThrottler.cpp:
(WebKit::ProcessThrottler::setThrottleState):

* Tools/TestWebKitAPI/Tests/WebKit/WKWebView/IndexedDBSuspendImminently.mm:
(testTransactionAfterCrossSiteNavigationWithServiceWorker): Added.
(TEST(IndexedDB, TransactionAfterCrossSiteNavigationWithServiceWorker)): Added.
(TEST(IndexedDB, TransactionAfterCrossSiteNavigationWithServiceWorkerWithoutPageCache)): Added.

Canonical link: https://commits.webkit.org/321356@main

Source/WebKit/UIProcess/ProcessThrottler.cpp
@@ -294,8 +294,15 @@ ProcessAssertionType ProcessThrottler::assertionTypeForState(ProcessThrottleStat
     RELEASE_ASSERT_NOT_REACHED();
 }
 
+#if !USE(RUNNINGBOARD)
+NO_RETURN_DUE_TO_ASSERT
+#endif
 void ProcessThrottler::setThrottleState(ProcessThrottleState newState)
 {
+#if !USE(RUNNINGBOARD)
+    ASSERT_NOT_REACHED();
+#endif
+
     assertIfCalledFromBackgroundThread();
 
     bool didChange = m_state != newState;

Source/WebKit/UIProcess/ProcessThrottler.h
@@ -187,7 +187,11 @@ private:
     WeakHashSet<Activity> m_foregroundActivities;
     WeakHashSet<Activity> m_backgroundActivities;
     std::optional<uint64_t> m_pendingRequestToSuspendID;
+#if USE(RUNNINGBOARD)
     ProcessThrottleState m_state { ProcessThrottleState::Suspended };
+#else
+    ProcessThrottleState m_state { ProcessThrottleState::Foreground };
+#endif
     bool m_shouldDropNearSuspendedAssertionAfterDelay { false };
     const bool m_shouldTakeUIBackgroundAssertion { false };
     bool m_shouldTakeNearSuspendedAssertion { true };

Tools/TestWebKitAPI/Tests/WebKit/WKWebView/IndexedDBSuspendImminently.mm
@@ -28,6 +28,7 @@
 #import "Helpers/DeprecatedGlobalValues.h"
 #import "Helpers/PlatformUtilities.h"
 #import "Helpers/Test.h"
+#import "Helpers/cocoa/HTTPServer.h"
 #import "Helpers/cocoa/TestScriptMessageHandler.h"
 #import "TestNavigationDelegate.h"
 #import "TestURLSchemeHandler.h"
@@ -802,4 +803,88 @@ TEST(IndexedDB, TransactionOfSuspendedProcessIsAbortedWhenAnotherSuspendedProces
     [waiterWebView _setThrottleStateForTesting:2];
     EXPECT_WK_STREQ([waiterHandler waitForMessage].body, @"waiter transaction completed");
     EXPECT_WK_STREQ([holderHandler waitForMessage].body, @"holder transaction aborted");
+}
+
+enum class UsePageCache : bool { No, Yes };
+
+static void testTransactionAfterCrossSiteNavigationWithServiceWorker(UsePageCache usePageCache)
+{
+    static constexpr auto page = R"HTML(
+        <script>
+        function post(message)
+        {
+            window.webkit.messageHandlers.testHandler.postMessage(message);
+        }
+        const database = new Promise((resolve, reject) => {
+            const request = indexedDB.open("BackNavigationDatabase", 1);
+            request.onupgradeneeded = () => request.result.createObjectStore("store");
+            request.onsuccess = () => resolve(request.result);
+            request.onerror = () => reject(request.error);
+        });
+        async function transaction(value)
+        {
+            const connection = await database;
+            return new Promise((resolve, reject) => {
+                const transaction = connection.transaction("store", "readwrite");
+                const store = transaction.objectStore("store");
+                const request = value ? store.put(value, "key") : store.get("key");
+                transaction.oncomplete = () => resolve(request.result);
+                transaction.onabort = transaction.onerror = () => reject(transaction.error);
+            });
+        }
+        async function read()
+        {
+            await navigator.serviceWorker.register("/worker.js");
+            await navigator.serviceWorker.ready;
+            if (!navigator.serviceWorker.controller)
+                await new Promise(resolve => navigator.serviceWorker.addEventListener("controllerchange", resolve, { once: true }));
+            const value = await transaction();
+            post(value === undefined ? "empty" : value);
+        }
+        addEventListener("pageshow", () => read().catch(error => post(String(error))));
+        </script>
+    )HTML"_s;
+
+    TestWebKitAPI::HTTPServer server({
+        { "/"_s, { page } },
+        { "/worker.js"_s, { {{ "Content-Type"_s, "application/javascript"_s }}, "addEventListener('activate', event => event.waitUntil(clients.claim()));"_s } },
+        { "/other.html"_s, { "<p>Other site</p>"_s } },
+    });
+
+    RetainPtr processPoolConfiguration = adoptNS([[_WKProcessPoolConfiguration alloc] init]);
+    processPoolConfiguration.get().pageCacheEnabled = usePageCache == UsePageCache::Yes;
+    processPoolConfiguration.get().processSwapsOnNavigation = YES;
+
+    RetainPtr configuration = adoptNS([[WKWebViewConfiguration alloc] init]);
+    configuration.get().processPool = adoptNS([[WKProcessPool alloc] _initWithConfiguration:processPoolConfiguration.get()]).get();
+    configuration.get().websiteDataStore = [WKWebsiteDataStore nonPersistentDataStore];
+    RetainPtr handler = adoptNS([TestScriptMessageHandler new]);
+    [configuration.get().userContentController addScriptMessageHandler:handler.get() name:@"testHandler"];
+
+    RetainPtr navigationDelegate = adoptNS([TestNavigationDelegate new]);
+    RetainPtr webView = adoptNS([[WKWebView alloc] initWithFrame:CGRectMake(0, 0, 800, 600) configuration:configuration.get()]);
+    webView.get().navigationDelegate = navigationDelegate.get();
+    [webView loadRequest:server.request()];
+    EXPECT_WK_STREQ([handler waitForMessage].body, @"empty");
+
+    [webView evaluateJavaScript:@"transaction('TestValue').then(() => post('stored')).catch(error => post(String(error)))" completionHandler:nil];
+    EXPECT_WK_STREQ([handler waitForMessage].body, @"stored");
+    auto initialProcessIdentifier = [webView _webProcessIdentifier];
+
+    [webView loadRequest:server.requestWithLocalhost("/other.html"_s)];
+    [navigationDelegate waitForDidFinishNavigation];
+    EXPECT_NE(initialProcessIdentifier, [webView _webProcessIdentifier]);
+
+    [webView goBack];
+    EXPECT_WK_STREQ([handler waitForMessage].body, @"TestValue");
+}
+
+TEST(IndexedDB, TransactionAfterCrossSiteNavigationWithServiceWorker)
+{
+    testTransactionAfterCrossSiteNavigationWithServiceWorker(UsePageCache::Yes);
+}
+
+TEST(IndexedDB, TransactionAfterCrossSiteNavigationWithServiceWorkerWithoutPageCache)
+{
+    testTransactionAfterCrossSiteNavigationWithServiceWorker(UsePageCache::No);
 }