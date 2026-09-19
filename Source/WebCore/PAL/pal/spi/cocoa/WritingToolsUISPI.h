f25af0d17d4645c298e29b4a3bd3b1979f973abf
Megan Gardner <megan_gardner@apple.com>
2026-09-18 21:01:57 -0700

    Remove SPI for WritingToolsUI proofreading animations. https://bugs.webkit.org/show_bug.cgi?id=323921 rdar://173046822
    Reviewed by Elliott Williams.
    
    Move to API. No behavior change.
    
    * Source/WebCore/Configurations/AllowedSPI.toml:
    * Source/WebCore/PAL/pal/cocoa/WritingToolsUISoftLink.h:
    * Source/WebCore/PAL/pal/cocoa/WritingToolsUISoftLink.mm:
    * Source/WebCore/PAL/pal/spi/cocoa/WritingToolsUISPI.h:
    * Source/WebKit/Configurations/AllowedSPI.toml:
    * Source/WebKit/UIProcess/Cocoa/WKTextEffectManager.mm:
    (toNSWritingDirection):
    (forEachTextPreviewImage):
    (textPreviewFromIndicator):
    (-[WKTextEffectManager initWithWebView:]):
    (-[WKTextEffectManager dealloc]):
    (-[WKTextEffectManager _effectContainerView]):
    (-[WKTextEffectManager addTextEffectForID:withData:]):
    (-[WKTextEffectManager removeTextEffectForID:]):
    (-[WKTextEffectManager removeAllTextEffects]):
    (-[WKTextEffectManager forgetEffectForID:]):
    (-[WKTextEffectManager effectIDForContext:]):
    (-[WKTextEffectManager setUnderlyingTextVisibility:forEffectID:completion:]):
    (-[WKTextEffectManager previewForContext:textDecoration:completion:]):
    (-[WKTextEffectManager writingToolsCoordinator:prepareForTextAnimation:forRange:inContext:completion:]):
    (-[WKTextEffectManager writingToolsCoordinator:finishTextAnimation:forRange:inContext:completion:]):
    (-[WKTextEffectManager writingToolsCoordinator:requestsPreviewForTextAnimation:ofRange:inContext:completion:]):
    (-[WKTextEffectManager writingToolsCoordinator:requestsPreviewForTextAnimation:ofRange:inContext:textDecoration:completion:]):
    (-[WKTextEffectManager writingToolsCoordinator:requestsContextsForScope:completion:]):
    (-[WKTextEffectManager writingToolsCoordinator:replaceRange:inContext:proposedText:reason:animationParameters:completion:]):
    (-[WKTextEffectManager writingToolsCoordinator:selectRanges:inContext:completion:]):
    (-[WKTextEffectManager writingToolsCoordinator:requestsBoundingBezierPathsForRange:inContext:completion:]):
    (-[WKTextEffectManager writingToolsCoordinator:requestsUnderlinePathsForRange:inContext:completion:]):
    (-[WKTextEffectManager writingToolsCoordinator:requestsPreviewForRect:inContext:completion:]):
    (toTextEffectWritingDirection): Deleted.
    (-[WKTextEffectManager hideTextForSuggestionWithUUID:completion:]): Deleted.
    (-[WKTextEffectManager showTextForSuggestionWithUUID:completion:]): Deleted.
    (-[WKTextEffectManager containerViewForSuggestionWithUUID:completion:]): Deleted.
    (textPreviewsFromIndicator): Deleted.
    (-[WKTextEffectManager previewsForSuggestionWithUUID:completion:]): Deleted.
    
    Canonical link: https://commits.webkit.org/321434@main

--- Source/WebCore/PAL/pal/spi/cocoa/WritingToolsUISPI.h
+++ Source/WebCore/PAL/pal/spi/cocoa/WritingToolsUISPI.h
@@ -32,21 +32,7 @@ DECLARE_SYSTEM_HEADER
 
 #if ENABLE(WRITING_TOOLS)
 
-#import <CoreGraphics/CoreGraphics.h>
 #import <Foundation/Foundation.h>
-
-#if USE(APPLE_INTERNAL_SDK)
-
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wnon-modular-include-in-module"
-#import <WebKitAdditions/WTTextEffectManagerEffectTypeAdditions.h>
-#pragma clang diagnostic pop
-
-#else // !USE(APPLE_INTERNAL_SDK)
-#define WTTextEffectManagerEffectTypeDefault ((WTTextEffectManagerEffectType)0)
-#endif
-
-// MARK: Forward-declared Writing Tools types
 
 @class WTSession;
 @class WTContext;
@@ -54,6 +40,17 @@ DECLARE_SYSTEM_HEADER
 @interface NSObject (WritingToolsSPI)
 - (void)willBeginWritingToolsSession:(WTSession *)session forProofreadingReview:(BOOL)proofreadingReview requestContexts:(void (^)(NSArray<WTContext *> *))completion;
 @end
+
+#if PLATFORM(MAC)
+
+#import <AppKit/AppKit.h>
+#import <CoreGraphics/CoreGraphics.h>
+
+// MARK: Forward-declared Writing Tools types
+
+typedef NS_ENUM(NSInteger, WTRequestedTool);
+
+@protocol WTWritingToolsDelegate;
 
 // MARK: _WTTextPreview
 
@@ -80,47 +77,6 @@ DECLARE_SYSTEM_HEADER
 - (instancetype)initWithSnapshotImage:(CGImageRef)snapshotImage presentationFrame:(CGRect)presentationFrame backgroundColor:(CGColorRef)backgroundColor clippingPath:(CGPathRef)clippingPath scale:(CGFloat)scale candidateRects:(NSArray <NSValue *> *)candidateRects;
 
 @end
-
-typedef NS_ENUM(NSInteger, WTTextEffectManagerEffectType) {
-    WTTextEffectManagerEffectTypeNone = 0
-};
-
-typedef NS_ENUM(NSInteger, WTTextEffectManagerWritingDirection) {
-    WTTextEffectManagerWritingDirectionLeftToRight = 0,
-    WTTextEffectManagerWritingDirectionRightToLeft = 1,
-};
-
-// MARK: _WTTextEffectManagerDelegate
-
-@protocol _WTTextEffectManagerDelegate <NSObject>
-
-- (void)hideTextForSuggestionWithUUID:(NSUUID *)uuid completion:(void(^)(void))completion;
-- (void)showTextForSuggestionWithUUID:(NSUUID *)uuid completion:(void(^)(void))completion;
-
-@optional
-- (void)previewForSuggestionWithUUID:(NSUUID *)uuid completion:(void(^)(NSArray<_WTTextPreview *> * _Nullable textPreviews))completion;
-- (void)previewAndUnderlineForSuggestionWithUUID:(NSUUID *)uuid completion:(void(^)(NSArray<_WTTextPreview *> * _Nullable textPreviews, NSArray<_WTTextPreview *> * _Nullable underlinePreviews))completion;
-
-@end
-
-// MARK: _WTTextEffectManager
-
-@interface _WTTextEffectManager : NSObject
-@property (weak, readonly) id <_WTTextEffectManagerDelegate> delegate;
-
-- (instancetype)initWithDelegate:(id <_WTTextEffectManagerDelegate>)delegate;
-- (void)startAnimationForSuggestionWithUUID:(NSUUID *)uuid writingDirection:(WTTextEffectManagerWritingDirection)direction effectType:(WTTextEffectManagerEffectType)effectType completion:(void(^)(NSUUID * uuid))completion;
-- (void)cancelAnimationForSuggestionWithUUID:(NSUUID *)uuid;
-- (void)cancelAllAnimations;
-@end
-
-#if PLATFORM(MAC)
-
-#import <AppKit/AppKit.h>
-
-typedef NS_ENUM(NSInteger, WTRequestedTool);
-
-@protocol WTWritingToolsDelegate;
 
 // MARK: _WTTextChunk
 
@@ -260,6 +216,6 @@ NS_ASSUME_NONNULL_END
 
 @end
 
-#endif
+#endif // PLATFORM(MAC)
 
-#endif
+#endif // ENABLE(WRITING_TOOLS)
