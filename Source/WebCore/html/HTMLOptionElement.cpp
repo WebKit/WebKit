2ebb616ec0c28ada924fefb1034a711b1ace801c
Anne van Kesteren <annevk@annevk.nl>
2026-09-19 09:28:57 +0200

    Rename usesMenuListDeprecated() to isSingleSelectDropDownBox() https://bugs.webkit.org/show_bug.cgi?id=324620
    Reviewed by NOBODY (OOPS!).
    
    The semantics of this method always were about select elements that allow
    for a singular selection and use a drop-down box.
    
    Bug 324529 will make this method depend on the appearance value which is
    why a change from "menu list" to "drop down" is made. Drop down is the
    terminology the standard uses and is renderer-agnostic.
    
    usesMenuList() remains as-is as it cannot be made to depend on the
    appearance value due to :-internal-uses-menulist also depending on it.

--- Source/WebCore/html/HTMLOptionElement.cpp
+++ Source/WebCore/html/HTMLOptionElement.cpp
@@ -300,15 +300,15 @@ void HTMLOptionElement::setText(String&& text)
     Ref protectedThis { *this };
 
     // Changing the text causes a recalc of a select's items, which will reset the selected
-    // index to the first item if the select is single selection with a menu list. We attempt to
+    // index to the first item if the select is a single select drop-down box. We attempt to
     // preserve the selected item.
     RefPtr select = ownerSelectElement();
-    bool selectIsMenuList = select && select->usesMenuListDeprecated();
-    int oldSelectedIndex = selectIsMenuList ? select->selectedIndex() : -1;
+    bool isSingleSelectDropDownBox = select && select->isSingleSelectDropDownBox();
+    int oldSelectedIndex = isSingleSelectDropDownBox ? select->selectedIndex() : -1;
 
     setTextContent(WTF::move(text));
-    
-    if (selectIsMenuList && select->selectedIndex() != oldSelectedIndex)
+
+    if (isSingleSelectDropDownBox && select->selectedIndex() != oldSelectedIndex)
         select->setSelectedIndex(oldSelectedIndex);
 }
 
