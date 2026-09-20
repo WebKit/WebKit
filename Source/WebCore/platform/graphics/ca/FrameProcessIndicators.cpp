810adaa1b184c5d22f6b8da53aab75aa7c6d42a0
Per Arne Vollan <pvollan@apple.com>
2026-09-19 18:11:51 -0700

    [Site Isolation] The text in frame process indicators is not centered https://bugs.webkit.org/show_bug.cgi?id=324447 rdar://187689047
    Reviewed by Sihui Liu.
    
    Fix this by adjusting the area filled with the background layer, since the text
    is already centered in the underlying platform layer.
    
    Also use the the height of the text to compute the stagger in the vertical
    direction, to avoid overlapping layers.
    
    The font size is also increased a little to improve readability.
    
    * Source/WebCore/platform/graphics/ca/FrameProcessIndicators.cpp:
    (WebCore::FrameProcessIndicators::updateGeometry):
    (WebCore::FrameProcessIndicators::platformCALayerPaintContents):
    
    Canonical link: https://commits.webkit.org/321458@main

--- Source/WebCore/platform/graphics/ca/FrameProcessIndicators.cpp
+++ Source/WebCore/platform/graphics/ca/FrameProcessIndicators.cpp
@@ -42,7 +42,7 @@
 namespace WebCore {
 
 static constexpr float padding = 3;
-static constexpr float fontSize = 12;
+static constexpr float fontSize = 14;
 static constexpr float borderWidth = 4;
 static constexpr float depthStaggerStep = 12;
 
@@ -90,8 +90,10 @@ void FrameProcessIndicators::updateGeometry(const FloatRect& bounds)
 
     // Stagger indicators by frame nesting depth so co-located frames (e.g. a mainframe and an
     // iframe both anchored at the page origin) don't draw their labels directly on top of each other.
-    float stagger = m_graphicsLayer ? m_graphicsLayer->frameProcessIndicatorDepth() * depthStaggerStep : 0;
-    m_indicatorLayer->setPosition({ location.x() + stagger, location.y() + stagger, 1 });
+    unsigned depth = m_graphicsLayer ? m_graphicsLayer->frameProcessIndicatorDepth() : 0;
+    float staggerX = borderWidth + depth * depthStaggerStep;
+    float staggerY = borderWidth + depth * m_indicatorLayer->bounds().height();
+    m_indicatorLayer->setPosition({ location.x() + staggerX, location.y() + staggerY, 1 });
 }
 
 void FrameProcessIndicators::updateContentsScale(float contentsScale)
@@ -144,7 +146,7 @@ void FrameProcessIndicators::platformCALayerPaintContents(PlatformCALayer* layer
     auto indicatorSize = layer->bounds().size();
 
     context.setFillColor(m_backgroundColor);
-    context.fillRect(FloatRect { 0, borderWidth, indicatorSize.width() - borderWidth, indicatorSize.height() - borderWidth });
+    context.fillRect(FloatRect { { }, indicatorSize });
     context.setFillColor(textColor);
     context.drawText(font, TextRun(m_text), { padding, padding - textTop });
 }
