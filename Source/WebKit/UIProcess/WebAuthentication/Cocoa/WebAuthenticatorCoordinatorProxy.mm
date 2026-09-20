3607298361ab70a28420a6549feecfa8b39955fb
Pascoe <pascoe@apple.com>
2026-09-20 15:28:47 -0700

    Guard setPrf: on the security key registration request https://bugs.webkit.org/show_bug.cgi?id=324148 rdar://187379888
    Reviewed by Abrar Rahman Protyasha and Aditya Keerthi.
    
    prf on ASAuthorizationSecurityKeyPublicKeyCredentialRegistrationRequest is only
    available in iOS 26.4, but HAVE(WEB_AUTHN_PRF_API) is satisfied by an iOS 18
    deployment target, where only the platform request implements it. Setting it
    there throws an unrecognized selector exception.
    
    Check respondsToSelector: first, as we already do in requestsForAssertion.
    
    * Source/WebKit/UIProcess/WebAuthentication/Cocoa/WebAuthenticatorCoordinatorProxy.mm:
    (WebKit::WebAuthenticatorCoordinatorProxy::requestsForRegistration):
    
    Canonical link: https://commits.webkit.org/321476@main

--- Source/WebKit/UIProcess/WebAuthentication/Cocoa/WebAuthenticatorCoordinatorProxy.mm
+++ Source/WebKit/UIProcess/WebAuthentication/Cocoa/WebAuthenticatorCoordinatorProxy.mm
@@ -363,7 +363,7 @@ RetainPtr<NSArray> WebAuthenticatorCoordinatorProxy::requestsForRegistration(con
             request = [provider createCredentialRegistrationRequestWithChallenge:toNSData(options.challenge).get() displayName:options.user.displayName.createNSString().get() name:options.user.name.createNSString().get() userID:toNSData(options.user.id).get()];
         }
 #if HAVE(WEB_AUTHN_PRF_API)
-        if (options.extensions && options.extensions->prf) {
+        if (options.extensions && options.extensions->prf && [request respondsToSelector:@selector(setPrf:)]) {
             auto prf = options.extensions->prf;
             if (prf->eval)
                 request.get().prf = adoptNS([allocASAuthorizationPublicKeyCredentialPRFRegistrationInputInstance() initWithInputValues:toASAssertionPRFInputValue(prf->eval).get()]).get();
