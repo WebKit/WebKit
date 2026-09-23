0181cdc42aac7336ec66b3734c0d681953f6b0c2
Adrian Perez de Castro <aperez@igalia.com>
2026-09-23 04:39:19 -0700

    REGRESSION(316784@main): [WebDriver] Build fails with logging disabled https://bugs.webkit.org/show_bug.cgi?id=324949
    Reviewed by Claudio Saavedra.
    
    Based on a patch by Nina <q66@chimera-linux.org>.
    
    * Source/WebDriver/WebDriverService.cpp:
    (WebDriver::WebDriverService::handleRequest): Guard usage of
    LOG_CHANNEL() to ensure that the code is only built when logging
    is enabled.
    
    Canonical link: https://commits.webkit.org/321686@main

--- Source/WebDriver/WebDriverService.cpp
+++ Source/WebDriver/WebDriverService.cpp
@@ -381,6 +381,8 @@ bool WebDriverService::findCommand(HTTPMethod method, const String& path, Comman
 void WebDriverService::handleRequest(HTTPRequestHandler::Request&& request, Function<void (HTTPRequestHandler::Response&&)>&& replyHandler)
 {
     Function<void (HTTPRequestHandler::Response&&)> actualReplyHandler = WTF::move(replyHandler);
+
+#if !RELEASE_LOG_DISABLED
     if (LOG_CHANNEL(WebDriverClassic).state != WTFLogChannelState::Off) {
         RELEASE_LOG_INFO(WebDriverClassic, "HTTP request %s %s (body=%zu bytes)", request.method.utf8(), request.path.utf8(), request.dataLength);
         actualReplyHandler = [startTime = MonotonicTime::now(), replyHandler = WTF::move(actualReplyHandler)](HTTPRequestHandler::Response&& response) mutable {
@@ -388,6 +390,7 @@ void WebDriverService::handleRequest(HTTPRequestHandler::Request&& request, Func
             replyHandler(WTF::move(response));
         };
     }
+#endif
 
     auto method = toCommandHTTPMethod(request.method);
     if (!method) {
