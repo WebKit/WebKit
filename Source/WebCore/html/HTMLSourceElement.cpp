02ffdaba68d418dd449d613c96e76cd57f7e3c9a
Luke Warlow <lwarlow@igalia.com>
2026-09-26 00:30:15 -0700

    Source element moving steps doesn't handle media and model elements https://bugs.webkit.org/show_bug.cgi?id=320463
    Reviewed by Anne van Kesteren.
    
    Updates the moving steps for HTMLSourceElement to handle media elements and the model element.
    
    The HTML spec doesn't include the model element but this approximates what happens when removing
    and inserting a source.
    
    Spec: https://html.spec.whatwg.org/multipage/embedded-content.html#the-source-element:html-element-moving-steps
    
    Tests: imported/w3c/web-platform-tests/dom/nodes/moveBefore/resource-selection-move-source-between-media-elements.html
           imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-audio.html
           imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-in-div.html
           imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-in-namespace.html
           imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-not-in-document.html
           imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source.html
    
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-audio-expected.txt: Added.
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-audio.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-expected.txt: Added.
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-in-div-expected.txt: Added.
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-in-div.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-in-namespace-expected.txt: Added.
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-in-namespace.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-not-in-document-expected.txt: Added.
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-not-in-document.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-same-parent-expected.txt: Added.
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source-same-parent.html: Added.
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/media-elements/loading-the-media-resource/resource-selection-invoke-move-source.html: Added.
    * LayoutTests/model-element/model-element-source-expected.txt:
    * LayoutTests/model-element/model-element-source.html:
    * Source/WebCore/html/HTMLSourceElement.cpp:
    (WebCore::HTMLSourceElement::movingSteps):
    
    Canonical link: https://commits.webkit.org/321973@main

--- Source/WebCore/html/HTMLSourceElement.cpp
+++ Source/WebCore/html/HTMLSourceElement.cpp
@@ -130,8 +130,23 @@ void HTMLSourceElement::movingSteps(IsSubtreeRoot isSubtreeRoot, ContainerNode& 
     if (isSubtreeRoot == IsSubtreeRoot::No)
         return;
 
+    RefPtr newParent { parentElement() };
+
+#if ENABLE(VIDEO)
+    if (RefPtr newMediaElement = dynamicDowncast<HTMLMediaElement>(newParent))
+        newMediaElement->sourceWasAdded(*this);
+#endif
+
+#if ENABLE(MODEL_ELEMENT)
+    if (RefPtr oldModelElement = dynamicDowncast<HTMLModelElement>(oldParent))
+        oldModelElement->sourcesChanged();
+
+    if (RefPtr newModelElement = dynamicDowncast<HTMLModelElement>(newParent))
+        newModelElement->sourcesChanged();
+#endif
+
     RefPtr oldParentPicture = dynamicDowncast<HTMLPictureElement>(oldParent);
-    RefPtr parentPicture = dynamicDowncast<HTMLPictureElement>(parentElement());
+    RefPtr parentPicture = dynamicDowncast<HTMLPictureElement>(newParent);
 
     m_shouldCallSourcesChanged = false;
     if (parentPicture) {
